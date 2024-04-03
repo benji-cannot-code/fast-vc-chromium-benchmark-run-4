5b01411fe67405de14327193e93acef5b189b629
Xiaodan Zhu <zxdan@chromium.org>
2024-04-03 01:09:42 +0000

    oak birch: create a base class for birch chip
    To create a chip loader for birch bar animation, this cl creates a
    BirchChipButtonBase. In the following cl, we will create the loader
    inheriting from this base class.
    
    Bug: b:332379877
    Change-Id: Ibc4d3dd3d78b35d377946251e889559a6162baf5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5417413
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281587}

--- ash/wm/overview/birch/birch_chip_button.cc
+++ ash/wm/overview/birch/birch_chip_button.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/birch/birch_item.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/pill_button.h"
-#include "ash/style/style_util.h"
 #include "ash/style/typography.h"
 #include "ash/wm/overview/birch/birch_bar_context_menu_model.h"
 #include "ash/wm/overview/birch/birch_bar_controller.h"
@@ -17,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/events/types/event_type.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
-#include "ui/views/background.h"
 #include "ui/views/context_menu_controller.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -25,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_runner.h"
 #include "ui/views/controls/menu/menu_types.h"
-#include "ui/views/highlight_border.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/flex_layout_types.h"
@@ -41,9 +38,6 @@ namespace {
 constexpr gfx::Insets kInteriorMarginsNoAddon =
     gfx::Insets::TLBR(12, 0, 12, 20);
 constexpr gfx::Insets kInteriorMarginsWithAddon = gfx::Insets::VH(12, 0);
-constexpr int kRoundedCornerRadius = 12;
-constexpr ui::ColorId kBackgroundColorId =
-    cros_tokens::kCrosSysSystemOnBaseOpaque;
 
 // The layout parameters of icon.
 constexpr gfx::Size kItemIconSize = gfx::Size(20, 20);
@@ -95,13 +89,8 @@ BirchChipButton::BirchChipButton()
   raw_ptr<views::BoxLayoutView> titles_container = nullptr;
 
   // Build up the chip's contents.
-  views::Builder<views::Button>(this)
+  views::Builder<BirchChipButtonBase>(this)
       .SetLayoutManager(std::move(flex_layout))
-      .SetBorder(std::make_unique<views::HighlightBorder>(
-          kRoundedCornerRadius,
-          views::HighlightBorder::Type::kHighlightBorderNoShadow))
-      .SetBackground(views::CreateThemedRoundedRectBackground(
-          kBackgroundColorId, kRoundedCornerRadius))
       // TODO(zxdan): verbalize all the contents in following changes.
       .SetAccessibleName(u"Birch Chip")
       .AddChildren(
@@ -138,11 +127,6 @@ BirchChipButton::BirchChipButton()
 
   // Add removal chip panel.
   set_context_menu_controller(chip_menu_controller_.get());
-
-  // Install and stylize the focus ring.
-  StyleUtil::InstallRoundedCornerHighlightPathGenerator(
-      this, gfx::RoundedCornersF(kRoundedCornerRadius));
-  StyleUtil::SetUpFocusRingForView(this);
 }
 
 BirchChipButton::~BirchChipButton() = default;
@@ -164,6 +148,14 @@ void BirchChipButton::Init(BirchItem* item) {
   }
   item_->LoadIcon(base::BindOnce(&BirchChipButton::SetIconImage,
                                  weak_factory_.GetWeakPtr()));
+}
+
+const BirchItem* BirchChipButton::GetItem() const {
+  return item_.get();
+}
+
+BirchItem* BirchChipButton::GetItem() {
+  return item_.get();
 }
 
 void BirchChipButton::Shutdown() {
--- ash/wm/overview/birch/birch_chip_button.h
+++ ash/wm/overview/birch/birch_chip_button.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_OVERVIEW_BIRCH_BIRCH_CHIP_BUTTON_H_
 #define ASH_WM_OVERVIEW_BIRCH_BIRCH_CHIP_BUTTON_H_
 
+#include "ash/wm/overview/birch/birch_chip_button_base.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/base/models/simple_menu_model.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/metadata/view_factory.h"
 
 namespace views {
@@ -25,9 +25,9 @@ class PillButton;
 
 // A compact view of an app, displaying its icon, name, a brief description, and
 // an optional call to action.
-class BirchChipButton : public views::Button,
+class BirchChipButton : public BirchChipButtonBase,
                         public ui::SimpleMenuModel::Delegate {
-  METADATA_HEADER(BirchChipButton, views::Button)
+  METADATA_HEADER(BirchChipButton, BirchChipButtonBase)
 
  public:
   BirchChipButton();
@@ -35,13 +35,8 @@ class BirchChipButton : public views::Button,
   BirchChipButton& operator=(const BirchChipButton&) = delete;
   ~BirchChipButton() override;
 
-  BirchItem* item() { return item_.get(); }
-
   // Chip configuration methods.
   void Init(BirchItem* item);
-
-  // Clears the BirchItem pointer to avoid dangling pointers.
-  void Shutdown();
 
   template <typename T>
   T* SetAddon(std::unique_ptr<T> addon_view) {
@@ -50,7 +45,10 @@ class BirchChipButton : public views::Button,
     return ptr;
   }
 
-  // views::Button:
+  // BirchChipButtonBase:
+  const BirchItem* GetItem() const override;
+  BirchItem* GetItem() override;
+  void Shutdown() override;
   void OnGestureEvent(ui::GestureEvent* event) override;
 
   // ui::SimpleMenuModel::Delegate:
@@ -83,7 +81,7 @@ class BirchChipButton : public views::Button,
   base::WeakPtrFactory<BirchChipButton> weak_factory_{this};
 };
 
-BEGIN_VIEW_BUILDER(/*no export*/, BirchChipButton, views::Button)
+BEGIN_VIEW_BUILDER(/*no export*/, BirchChipButton, BirchChipButtonBase)
 VIEW_BUILDER_METHOD(Init, BirchItem*)
 VIEW_BUILDER_VIEW_TYPE_PROPERTY(views::View, Addon)
 END_VIEW_BUILDER
