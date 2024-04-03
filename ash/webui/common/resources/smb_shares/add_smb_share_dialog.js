cf473152b64e825ac33188ea47369b5bc8de21b7
Wes Okuhara <wesokuhara@google.com>
2024-04-03 23:15:49 +0000

    Jelly: Remove Jelly flag from add SMB share dialog
    Jelly has launched and this conditional code can be cleaned up.
    
    Bug: b:319740292
    Test: Manually verify dynamic colors supported in dialog
    Change-Id: I55dd9b1bd8b85118f969c91dc618b6d442142133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5421244
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282204}

--- ash/webui/common/resources/smb_shares/add_smb_share_dialog.js
+++ ash/webui/common/resources/smb_shares/add_smb_share_dialog.js
@@ -402,10 +402,6 @@ Polymer({
     return SMB_SHARE_URL_REGEX.test(this.mountUrl_);
   },
 
-  isJellyEnabled_() {
-    return !!loadTimeData.getBoolean('isJellyEnabled');
-  },
-
   isCrosComponentsEnabled_() {
     return !!loadTimeData.getBoolean('isCrosComponentsEnabled');
   },
