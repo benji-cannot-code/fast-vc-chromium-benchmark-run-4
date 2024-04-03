e45cb479b47492235373c51f38e363c5dc23caee
Li Lin <llin@chromium.org>
2024-04-03 01:34:57 +0000

    Don't cancel nudge if nudge widget is activated.
    Bug: b/332453677
    Change-Id: I41cfade84d790796581e3b221ec9f048a92098f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5416616
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281596}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -1381,6 +1381,11 @@ BASE_FEATURE(kGrowthFramework,
              "GrowthFramework",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Close the nudge when targeted app window is inactive.
+BASE_FEATURE(kGrowthCampaignsCloseNudgeWhenTargetInactivated,
+             "GrowthCampaignsCloseNudgeWhenTargetInactivated",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 // Enables CrOS events recording with growth campaigns.
 BASE_FEATURE(kGrowthCampaignsCrOSEvents,
              "GrowthCampaignsCrOSEvents",
@@ -3611,6 +3616,11 @@ bool IsFeatureManagementGrowthFrameworkEnabled() {
 
 bool IsGrowthFrameworkEnabled() {
   return base::FeatureList::IsEnabled(kGrowthFramework);
+}
+
+bool IsGrowthCampaignsCloseNudgeWhenTargetInactivated() {
+  return base::FeatureList::IsEnabled(
+      kGrowthCampaignsCloseNudgeWhenTargetInactivated);
 }
 
 bool IsGrowthCampaignsCrOSEventsEnabled() {
