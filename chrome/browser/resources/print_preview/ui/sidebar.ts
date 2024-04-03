ef46d8fbb8426d80a103fb7ebb0d018101dd315a
dpapad <dpapad@chromium.org>
2024-04-03 16:55:29 +0000

    PrintPreview: Improve type checking when referring to Settings object.
    Specifically using 'keyof Settings' instead of 'string' as the
    TypeScript type whenever dereferencing the global Settings object, which
    surfaces any typos, or references to missing keys. For example it was
    revealed that 'recentDestinations' was erroneously not declared as a
    key.
    
    Bug: 331681689
    Change-Id: I5868aadedd6179787889556872426e6a554340c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5416454
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281919}

--- chrome/browser/resources/print_preview/ui/sidebar.ts
+++ chrome/browser/resources/print_preview/ui/sidebar.ts
@@ -39,6 +39,7 @@ import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bu
 
 import {DarkModeMixin} from '../dark_mode_mixin.js';
 import type {Destination} from '../data/destination.js';
+import type {Settings} from '../data/model.js';
 import type {Error} from '../data/state.js';
 import {State} from '../data/state.js';
 import {MetricsContext, PrintSettingsUiBucket} from '../metrics.js';
@@ -212,7 +213,7 @@ export class PrintPreviewSidebarElement extends PrintPreviewSidebarElementBase {
   private computeShouldShowMoreSettings_(): boolean {
     // Destination settings is always available. See if the total number of
     // available sections exceeds the maximum number to show.
-    return [
+    const keys: Array<keyof Settings> = [
       'pages',
       'copies',
       'layout',
@@ -226,7 +227,8 @@ export class PrintPreviewSidebarElement extends PrintPreviewSidebarElementBase {
       'duplex',
       'otherOptions',
       'vendorItems',
-    ].reduce((count, setting) => {
+    ];
+    return keys.reduce((count, setting) => {
       return this.getSetting(setting).available ? count + 1 : count;
     }, 1) > MAX_SECTIONS_TO_SHOW;
   }
