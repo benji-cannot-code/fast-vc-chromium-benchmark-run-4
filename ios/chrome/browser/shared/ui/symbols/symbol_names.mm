a1fc42424ce0a284ae3b6bf7d59bc5987d36d205
adamta <adamta@google.com>
2024-04-03 19:03:32 +0000

    [iOS] Adds icons to feed header context menu
    Change-Id: I2db67203ccd993a8ec189028d46d1f63024326e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5415733
    Auto-Submit: Adam Arcaro <adamta@google.com>
    Commit-Queue: Adam Arcaro <adamta@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282027}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.mm
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.mm
@@ -91,7 +91,7 @@ NSString* const kRefineQuerySymbol = @"arrow.up.backward";
 NSString* const kLinkActionSymbol = @"link";
 NSString* const kQRCodeFinderActionSymbol = @"qrcode.viewfinder";
 NSString* const kNewTabActionSymbol = @"plus.square";
-NSString* const kNewTabCircleActionSymbol = @"plus.circle";
+NSString* const kPlusInCircleSymbol = @"plus.circle";
 NSString* const kClipboardActionSymbol = @"doc.on.clipboard";
 NSString* const kDeleteActionSymbol = @"trash";
 NSString* const kEditActionSymbol = @"pencil";
@@ -205,6 +205,7 @@ NSString* const kMoveTabToGroupActionSymbol = @"arrow.up.right.square";
 NSString* const kClockSymbol = @"clock";
 NSString* const kUngroupTabGroupSymbol = @"viewfinder";
 NSString* const kPlusInSquareSymbol = @"plus.square";
+NSString* const kMinusInCircleSymbol = @"minus.circle";
 
 // Names of the default symbol being non-monochrome by default. When using them,
 // you probably want to set their color to monochrome.
