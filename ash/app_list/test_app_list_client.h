bd76505a5d6c77d54c77b1f1f876e415180a563a
Ana Salazar <anasalazar@chromium.org>
2024-04-03 02:39:33 +0000

    Verify OnFirstSync if a user is new to ChromeOS
    Previously, the UserEducationController created a class member value for
    the AppsSyncableService to confirm if the user had previously  logged on
    a different ChromeOS device. The main purpose is to identify new users
    in the ChromeOS ecosystem so that Welcome Tour targets users
    appropriately.
    
    Since AppsCollections attempts to target the same subset of users as
    Welcome Tour, expose the same variable for the app list from the browser
    by using the AppListClient as a bridge to help verify the status of a
    new user in a device within the chromeos ecosystem.
    
    Bug: b:325640336
    Change-Id: I29944d51c119263a0d9bcb955b6e38a84527280a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399488
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281617}

--- ash/app_list/test_app_list_client.h
+++ ash/app_list/test_app_list_client.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_client.h"
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "base/memory/weak_ptr.h"
+#include "components/account_id/account_id.h"
 
 namespace ash {
 
@@ -64,6 +65,7 @@ class TestAppListClient : public AppListClient {
   std::unique_ptr<ScopedIphSession> CreateLauncherSearchIphSession() override;
   void LoadIcon(int profile_id, const std::string& app_id) override;
   ash::AppListSortOrder GetPermanentSortingOrder() const override;
+  std::optional<bool> IsNewUser(const AccountId& account_id) const override;
 
   int start_zero_state_search_count() const {
     return start_zero_state_search_count_;
@@ -110,6 +112,10 @@ class TestAppListClient : public AppListClient {
     search_callback_ = std::move(callback);
   }
 
+  void set_is_new_user(std::optional<bool> is_new_user) {
+    is_new_user_ = is_new_user;
+  }
+
  private:
   // Called in response to StartZeroStateSearch() when
   // `run_zero_state_callback_immediately_` is false. Counts calls via
@@ -127,6 +133,7 @@ class TestAppListClient : public AppListClient {
   std::vector<std::string> loaded_icon_app_ids_;
 
   std::vector<AppListSearchControlCategory> toggleable_categories_for_test_;
+  std::optional<bool> is_new_user_;
 
   // If not null, callback that will be run on each search request. It can be
   // used by tests to inject results to search model in response to search
