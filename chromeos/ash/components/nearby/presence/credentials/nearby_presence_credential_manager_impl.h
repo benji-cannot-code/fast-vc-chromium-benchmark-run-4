d1ad332eb33747c7c8ee575aae113dec8522deaa
Ryan Hansberry <hansberry@chromium.org>
2024-04-03 23:25:11 +0000

    [NearbyPresence] Clean up CredentialManager after vending a test instance.
    Without this fix, the following invocation was failing:
    
    $ ./out/Default/chromeos_unittests --gtest_filter=*NearbyPresence*
    
    because NearbyPresenceServiceImplTest inadvertently left
    NearbyPresenceCredentialManagerImpl::Creator in a bad state with its
    static g_is_credential_manager_set_for_testing_ as true, eventually
    causing NearbyPresenceCredentialManagerImplTest to fail.
    
    This fix makes NearbyPresenceCredentialManagerImpl::Creator
    implicitly clean up its internal state each time it vends a test
    instance (i.e., reset g_is_credential_manager_set_for_testing).
    
    Test: unit tests and running the above target
    Change-Id: I208b3aab480e328aea149741e2c3f3a30eb61a56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5418790
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282211}

--- chromeos/ash/components/nearby/presence/credentials/nearby_presence_credential_manager_impl.h
+++ chromeos/ash/components/nearby/presence/credentials/nearby_presence_credential_manager_impl.h
@@ -78,7 +78,7 @@ class NearbyPresenceCredentialManagerImpl
     Creator& operator=(Creator&) = delete;
 
     static Creator* Get();
-    static void SetCredentialManagerForTesting(
+    static void SetNextCredentialManagerInstanceForTesting(
         std::unique_ptr<NearbyPresenceCredentialManager> credential_manager);
 
     void Create(
