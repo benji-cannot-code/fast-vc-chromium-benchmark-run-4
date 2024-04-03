d9fb271bba43b038d6c5bdaaf486dbf99dd3922e
Ashley Prasad <ashleydp@google.com>
2024-04-03 20:50:05 +0000

    cros-preview: Use destination-row in dropdown
    - Add destination property to destination-row.
    - Display destination display name in destination-row.
    - Use destination-row as select visual for destination-dropdown.
    - Test destination label correctly displayed.
    - Update dropdown test to check row label.
    
    Screenshot: http://screen/DUFQMP4PeT6qtFs
    
    Bug: b:323421684
    Test: browser_test --gtest_filter=*PrintPreviewCros*
    Change-Id: Ia7ed41619fc25cb81f38d01a7bdb0ec61fce628c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405711
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282100}

--- ash/webui/print_preview_cros/resources/js/destination_dropdown.ts
+++ ash/webui/print_preview_cros/resources/js/destination_dropdown.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '../css/print_preview_cros_shared.css.js';
+import './destination_row.js';
 
 import {EventTracker} from 'chrome://resources/js/event_tracker.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
