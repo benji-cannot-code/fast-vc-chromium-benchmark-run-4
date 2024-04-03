ae70556c1087f431e6fe8df22acdfcf04e27d296
Marke Hallowell <wry@chromium.org>
2024-04-03 18:05:03 +0000

    Add a content notification client.
    Implements handling logic for content notifications in Bling.
    
    Bug: b/315148990
    Change-Id: Ie93d5bb6c5dc6292fde5896816b459e54f5f1628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5313925
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281979}

--- ios/chrome/browser/push_notification/model/push_notification_client_manager.mm
+++ ios/chrome/browser/push_notification/model/push_notification_client_manager.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/optimization_guide/core/optimization_guide_features.h"
 #import "ios/chrome/browser/commerce/model/push_notification/commerce_push_notification_client.h"
 #import "ios/chrome/browser/commerce/model/push_notification/push_notification_feature.h"
+#import "ios/chrome/browser/content_notification/model/content_notification_client.h"
 #import "ios/chrome/browser/push_notification/model/constants.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_util.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
@@ -26,6 +27,10 @@ PushNotificationClientManager::PushNotificationClientManager() {
 
   if (IsIOSTipsNotificationsEnabled()) {
     AddPushNotificationClient(std::make_unique<TipsNotificationClient>());
+  }
+
+  if (IsContentPushNotificationsEnabled()) {
+    AddPushNotificationClient(std::make_unique<ContentNotificationClient>());
   }
 }
 PushNotificationClientManager::~PushNotificationClientManager() = default;
