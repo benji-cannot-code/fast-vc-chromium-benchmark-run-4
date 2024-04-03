088319e7ab48d04d4e732268e4ad48648a85764f
Nate Chapin <japhet@chromium.org>
2024-04-03 16:36:18 +0000

    Reapply "Use typed promises/resolvers for ReadableStream and related classes"
    This converts IDL-exposed promises in ReadableStream,
    ReadableStreamBYOBReader, ReadableStreamDefaultReader, and
    ReadableStreamGenericReader to use typed ScriptPromiseResolver
    instead of StreamPromiseResolver and to return typed
    ScriptPromises.
    
    Bug: 329702363
    Change-Id: I6dad0ce6902c2dd5411185800bdd5a6dd8585df8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410703
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1281906}

--- third_party/blink/renderer/core/streams/readable_stream_byob_reader.h
+++ third_party/blink/renderer/core/streams/readable_stream_byob_reader.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_READABLE_STREAM_BYOB_READER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_READABLE_STREAM_BYOB_READER_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/streams/readable_stream_generic_reader.h"
@@ -18,9 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 class ExceptionState;
-class ScriptPromiseUntyped;
 class ScriptState;
 class ReadableStream;
+class ReadableStreamReadResult;
 class ReadIntoRequest;
 class DOMArrayBufferView;
 
@@ -43,9 +44,10 @@ class CORE_EXPORT ReadableStreamBYOBReader
   bool IsBYOBReader() const override { return true; }
 
   // https://streams.spec.whatwg.org/#byob-reader-read
-  ScriptPromiseUntyped read(ScriptState*,
-                            NotShared<DOMArrayBufferView> view,
-                            ExceptionState&);
+
+  ScriptPromise<ReadableStreamReadResult> read(ScriptState*,
+                                               NotShared<DOMArrayBufferView>,
+                                               ExceptionState&);
 
   // https://streams.spec.whatwg.org/#byob-reader-release-lock
   void releaseLock(ScriptState*, ExceptionState&);
