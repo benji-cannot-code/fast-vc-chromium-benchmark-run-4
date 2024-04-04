4bba71791bc7b73d892fa5b097f7950e7540a44d
Sam Maier <smaier@chromium.org>
2024-04-04 18:32:47 +0000

    jni_zero: Fix and add test for placeholders overwriting each other
    There was a bug where if you imported a class which had @NativeMethods
    it would overwrite the entry in the placeholders jar. This fixes it
    by just keeping a set tracking what we've currently added.
    
    I also had to change the golden tests to support multiple files.
    
    Bug: 330173100
    Change-Id: Id7161c5606efde2a1edda67f5124d31ed3941fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420173
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282644}

---
+++ third_party/jni_zero/test/java/src/org/jni_zero/extrapackage/ImportsTinySample.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+package org.jni_zero.extrapackage;
+
+import org.jni_zero.TinySample;
+
+class ImportsTinySample {
+    TinySample a;
+
+    @NativeMethods()
+    interface Natives {
+        void asdf();
+    }
+}
