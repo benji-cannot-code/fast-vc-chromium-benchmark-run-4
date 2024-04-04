cdbefb368507908af3f3f494d8b22863034d2fca
canalun <i.am.kanaru.sato@gmail.com>
2024-04-04 20:06:41 +0000

    Made TextDecorationPainter use draw looper instead of filter when painting shadow.
    This CL is a refactor pre-patch for the below bugfix:
    Ie973d8e78381356d4d4518cb44113836dff7d393.
    
    Bug: 40829719
    Change-Id: I0fba60677e8a3557cced19456efb1032d3e1dcae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5380302
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282707}

--- third_party/blink/renderer/core/paint/decoration_line_painter.h
+++ third_party/blink/renderer/core/paint/decoration_line_painter.h
@@ -28,9 +28,8 @@ class DecorationLinePainter final {
 
   void Paint(const Color& color, const cc::PaintFlags* flags = nullptr);
 
-  static void DrawLineForText(GraphicsContext&,
-                              const gfx::PointF& pt,
-                              float width,
+  static void DrawLineForText(GraphicsContext& context,
+                              const gfx::RectF& line_rect,
                               const StyledStrokeData& styled_stroke,
                               const AutoDarkMode& auto_dark_mode,
                               const cc::PaintFlags* paint_flags = nullptr);
@@ -38,6 +37,10 @@ class DecorationLinePainter final {
                                  float width,
                                  float stroke_thickness,
                                  StrokeStyle stroke_style);
+
+  static Vector<gfx::RectF> GetRectsForTextLine(
+      const gfx::RectF line_rect_without_intercepts,
+      const Vector<gfx::RectF>& stripe_intercepts);
 
  private:
   void PaintWavyTextDecoration(const AutoDarkMode&);
