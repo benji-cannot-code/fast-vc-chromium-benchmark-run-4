b39f414495e2fb96e2de0267bbea5bc9e13bb7d8
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-04-04 20:12:55 +0000

    Triage dangling pointers in ChromeOS [4/N]
    Those are blocking the patch:
    https://chromium-review.googlesource.com/c/chromium/src/+/5385328/5
    enabling the dangling pointer detector on ChromeOS, no matter the bot.
    
    DanglingUntriaged-notes: Annotating pre-existing dangling pointers.
    Bug: chromium:328104161
    Change-Id: I8df51a5f3d8f588c65accf8cdd52919b6a011ed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401849
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282713}

--- media/gpu/test/local_gpu_memory_buffer_manager.cc
+++ media/gpu/test/local_gpu_memory_buffer_manager.cc
@@ -125,7 +125,7 @@ class GpuMemoryBufferImplGbm : public gfx::GpuMemoryBuffer {
       Unmap();
     }
 
-    gbm_bo_destroy(buffer_object_);
+    gbm_bo_destroy(buffer_object_.ExtractAsDangling());
   }
 
   bool Map() override {
