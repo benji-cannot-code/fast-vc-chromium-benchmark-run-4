cfe74b961e2cc900ef477b7bc1964c9c19232bb6
Philip Rogers <pdr@chromium.org>
2024-04-04 18:12:35 +0000

    [css-anchor-position] Support CSS visibility for anchors-visible
    We previously supported position-visibility: anchors-visible only for
    the "intersection visibility" of the anchor element. This patch extends
    anchors-visible to also be based on the CSS visibility of the anchor
    element.
    
    Implementation notes:
    * CSS visibility of anchor elements can change without notifying the
      anchored element. To catch these style changes, we loop over all used
      anchor elements every lifecycle update with
      `LocalFrame::CheckPositionAnchorsForCssVisibilityChanges`.
    * LayerPositionVisibility has been introduced to support the two aspects
      of anchors-visible visibility. These can be tracked independently.
    
    Spec:
    https://github.com/w3c/csswg-drafts/issues/7758#issuecomment-1965540529
    
    Bug: 332663437
    Change-Id: Id043217fb33308c7af35a3d9fbe103a5c552a34a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425736
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282626}

--- third_party/blink/renderer/core/frame/local_frame.cc
+++ third_party/blink/renderer/core/frame/local_frame.cc
@@ -171,6 +171,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/inspector/inspector_trace_events.h"
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer_controller.h"
 #include "third_party/blink/renderer/core/layout/anchor_position_scroll_data.h"
+#include "third_party/blink/renderer/core/layout/anchor_position_visibility_observer.h"
 #include "third_party/blink/renderer/core/layout/hit_test_result.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/text_autosizer.h"
@@ -3877,6 +3878,20 @@ void LocalFrame::ScheduleNextServiceForScrollSnapshotClients() {
     if (client->ShouldScheduleNextService()) {
       View()->ScheduleAnimation();
       return;
+    }
+  }
+}
+
+void LocalFrame::CheckPositionAnchorsForCssVisibilityChanges() {
+  if (!RuntimeEnabledFeatures::CSSPositionVisibilityEnabled()) {
+    return;
+  }
+  for (auto& client : scroll_snapshot_clients_) {
+    if (AnchorPositionScrollData* scroll_data =
+            DynamicTo<AnchorPositionScrollData>(client.Get())) {
+      if (auto* observer = scroll_data->GetAnchorPositionVisibilityObserver()) {
+        observer->UpdateForCssAnchorVisibility();
+      }
     }
   }
 }
