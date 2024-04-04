ff46637a9300fe3b2804b8edd5d9f61283fe2f17
Li Lin <llin@chromium.org>
2024-04-04 16:50:53 +0000

    Add chrome flags for customizing growth and demo mode Omaha cohort.
    Bug: b/332782700
    Change-Id: I437ec618c6168aadcc32fb5686740cb41dbe7962
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422369
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282563}

--- components/component_updater/component_updater_switches.h
+++ components/component_updater/component_updater_switches.h
@@ -12,6 +12,8 @@ namespace switches {
 // alongside the definition of their values in the .cc file.
 extern const char kComponentUpdater[];
 extern const char kComponentUpdaterTrustTokensComponentPath[];
+extern const char kCampaignsTestTag[];
+extern const char kDemoModeTestTag[];
 
 }  // namespace switches
 
