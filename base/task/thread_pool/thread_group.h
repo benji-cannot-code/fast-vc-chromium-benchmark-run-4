01cf3f50b8978785c0a3f73e9e8ca4604eeb5292
Bartek Nowierski <bartekn@chromium.org>
2024-04-04 21:53:05 +0000

    [BRP] Un-rewrite couple raw_ptrs in thread_group.h that are stack-scoped
    BaseScopedCommandsExecutor showed up in profiler data [1]. Upon a closer
    look, it's short-lived (stack-scoped), and we have a rule to avoid
    rewriting these. We can't use STACK_ALLOCATED on the embedding class,
    however, because it needs to be a pointer for polymorphism purposes.
    
    When inspecting the rest of the file, turned out that
    ScopedReenqueueExecutor::destination_thread_group_ is also
    stack-scoped. In this case, the embedding class can easily be marked
    as STACK_ALLOCATED (hence RAW_PTR_EXCLUSION isn't needed there).
    
    ThreadGroup::replacement_thread_group_ appears to be redundant (never
    changes), so removing.
    
    [1] https://pprofng.corp.google.com/?id=a918472fab52ee23c3cf1f8bb500ab6c&filter=focus:RawPtrBackupRefImpl&tab=bottomup
    
    Bug: 332743723
    Change-Id: I62a2992e1bff77a8bea204de88b97a00507f8636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5423055
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282764}

--- base/task/thread_pool/thread_group.h
+++ base/task/thread_pool/thread_group.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_export.h"
 #include "base/dcheck_is_on.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/stack_allocated.h"
 #include "base/task/common/checked_lock.h"
 #include "base/task/thread_pool/priority_queue.h"
 #include "base/task/thread_pool/task.h"
@@ -235,7 +237,9 @@ class BASE_EXPORT ThreadGroup {
    protected:
     explicit BaseScopedCommandsExecutor(ThreadGroup* outer);
 
-    raw_ptr<ThreadGroup> outer_;
+    // RAW_PTR_EXCLUSION: Performance: visible in sampling profiler and stack
+    // scoped, also a back-pointer to the owning object.
+    RAW_PTR_EXCLUSION ThreadGroup* outer_;
 
    protected:
     // Performs BaseScopedCommandsExecutor-related tasks, must be called in this
@@ -251,6 +255,8 @@ class BASE_EXPORT ThreadGroup {
   // Allows a task source to be pushed to a ThreadGroup's PriorityQueue at the
   // end of a scope, when all locks have been released.
   class ScopedReenqueueExecutor {
+    STACK_ALLOCATED();
+
    public:
     ScopedReenqueueExecutor();
     ScopedReenqueueExecutor(const ScopedReenqueueExecutor&) = delete;
@@ -268,7 +274,7 @@ class BASE_EXPORT ThreadGroup {
     // should be enqueued.
     std::optional<RegisteredTaskSourceAndTransaction>
         transaction_with_task_source_;
-    raw_ptr<ThreadGroup> destination_thread_group_ = nullptr;
+    ThreadGroup* destination_thread_group_ = nullptr;
   };
 
   ThreadGroup(TrackedRef<TaskTracker> task_tracker,
@@ -466,11 +472,6 @@ class BASE_EXPORT ThreadGroup {
   // is nonetheless always safe to read it without the lock (since it's atomic).
   std::atomic<YieldSortKey> max_allowed_sort_key_ GUARDED_BY(lock_){
       kMaxYieldSortKey};
-
-  // If |replacement_thread_group_| is non-null, this ThreadGroup is invalid and
-  // all task sources should be scheduled on |replacement_thread_group_|. Used
-  // to support the UseNativeThreadPool experiment.
-  raw_ptr<ThreadGroup> replacement_thread_group_ = nullptr;
 
   const std::string histogram_label_;
   const std::string thread_group_label_;
