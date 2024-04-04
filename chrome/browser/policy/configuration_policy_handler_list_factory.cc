3cdfa7cbd58ffc1af5e0e95416ecf0b3951f0b2b
Shunya Shishido <sisidovski@chromium.org>
2024-04-04 21:52:08 +0000

    Reland "[Product specifications] Add enterprise policy"
    This reverts commit de47a3bbb4c1f4e12db1f8c4ace1af644e4a2178.
    
    Reason for revert: tools/traffic_annotation/scripts/auditor/auditor.py script fails by this change.
    
    Original change's description:
    > Revert "[Product specifications] Add enterprise policy"
    >
    > This reverts commit 259f96f21613a4bf0427b51b911884c08d8934e2.
    >
    > Reason for revert: Policy format and name incorrect for feature
    >
    > Original change's description:
    > > [Product specifications] Add enterprise policy
    > >
    > > This patch adds the declaration for the enterprise policy for product
    > > specifications. The policy is not checked anywhere yet, but is needed
    > > for network annotation definitions in subsequent patches.
    > >
    > > Bug: b:325109916
    > > Change-Id: If3191068904cbf22449183db63d463d1426be2d1
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297236
    > > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > > Reviewed-by: Pavol Marko <pmarko@chromium.org>
    > > Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1265934}
    >
    > BYPASS_POLICY_COMPATIBILITY_CHECK=Policy never launched in-code
    >
    > Bug: b:325109916
    > Change-Id: I0e12808c00dc1804f212d5443f8356e556209032
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405992
    > Reviewed-by: Owen Min <zmin@chromium.org>
    > Commit-Queue: Matthew Jones <mdjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1282148}
    
    Bug: b:325109916
    Change-Id: I5f8a4d69209e1f273dcf070f8c60acaf0c5f35f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426630
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282763}

--- chrome/browser/policy/configuration_policy_handler_list_factory.cc
+++ chrome/browser/policy/configuration_policy_handler_list_factory.cc
@@ -2168,6 +2168,12 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
     client_certificates::prefs::kProvisionManagedClientCertificateForUserPrefs,
     base::Value::Type::INTEGER },
 #endif  //
+
+#if !BUILDFLAG(IS_FUCHSIA)
+  { key::kProductSpecificationsEnabled,
+    commerce::kProductSpecificationsEnabledPrefName,
+    base::Value::Type::BOOLEAN},
+#endif  // !BUILDFLAG(IS_FUCHSIA)
 };
 // clang-format on
 
