46e6eaa76a6dd720347d8b1ab18bd13d39c1eddf
Ankush Singh <ankushkush@google.com>
2024-04-04 14:02:27 +0000

    Revert "Make LoginManagerMixin::LoginAsNew*User use FUDAC by default"
    This reverts commit c2e7d745720621531ea4db7d97873236ec1998e3.
    
    Reason for revert:  linux-chromeos-chrome:UserAddedRemovedReporterBrowserTest.ReportRemovedAffiliatedUser is crashing. See linked issue.
    
    Original change's description:
    > Make LoginManagerMixin::LoginAsNew*User use FUDAC by default
    >
    > This change ensures that all invariants are correctly satisfied
    > for subsequent onboarding process (e.g. that there is a correctly
    > established authsession).
    >
    > Also:
    >  * updated PasswordSelectionScreen to support repeated MaybeSkip;
    >  * rewrote RecoveryEligibilityScreen test to use actual
    >    login/policies;
    >  * skip SyncConsentTest*.AbortedTest if local passwords are enabled,
    >    as with local passwords feature aborting at this stage would
    >    trigger user cleanup;
    >
    >
    > Bug: b:325279785
    > Change-Id: If0a3c197b917073bc4bee865b00f743c510d60b2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402868
    > Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    > Reviewed-by: Istvan Nagy <iscsi@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1282399}
    
    Bug: b:325279785, 332875146
    Change-Id: Ia813dbbd7610cb459ac20701d98877da38a07892
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424890
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282466}

--- chrome/browser/ash/login/screens/sync_consent_browsertest.cc
+++ chrome/browser/ash/login/screens/sync_consent_browsertest.cc
@@ -371,9 +371,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentTest, SkippedSyncDisabledByPolicy) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentTest, PRE_AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   LoginAndShowSyncConsentScreenWithCapability();
   WaitForScreenShown();
   test::OobeJS().CreateVisibilityWaiter(true, {kSyncConsent})->Wait();
@@ -381,9 +378,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentTest, PRE_AbortedSetup) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentTest, AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   EXPECT_EQ(session_manager::SessionState::LOGIN_PRIMARY,
             session_manager::SessionManager::Get()->session_state());
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
@@ -783,9 +777,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, Decline) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, PRE_AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   LoginAndShowSyncConsentScreenWithCapability();
   WaitForScreenShown();
   test::OobeJS().CreateVisibilityWaiter(true, {kSyncConsent})->Wait();
@@ -793,9 +784,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, PRE_AbortedSetup) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   EXPECT_EQ(session_manager::SessionState::LOGIN_PRIMARY,
             session_manager::SessionManager::Get()->session_state());
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
