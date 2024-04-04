ec5a79129f135cc0dbce2bf6c39e30417f050708
Noémie St-Onge <noemies@google.com>
2024-04-04 17:03:09 +0000

    [iOSKAUpgrade] Make ChipButton an ExtendedTouchTargetButton
    This CL makes the ChipButton class inherit the
    ExtendedTouchTargetButton class, so that respecting the touch target
    size guideline of 44px is handled automatically (without having to add
    empty space around the button). This change will facilitate laying out
    ChipButtons because we won't have to take into account the extra space
    when applying constraints.
    
    The empty space is kept when the Keyboard Accessory Upgrade feature is
    disabled so that this change doesn't visually impact the current UI:
    https://screenshot.googleplex.com/9F5JqvW7VUwXbFp.png
    
    Bug: 326406091
    Change-Id: Ie77b320b9e72057347289de0226ca921f09c3bc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420157
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282574}

--- ios/chrome/browser/ui/autofill/manual_fill/chip_button.h
+++ ios/chrome/browser/ui/autofill/manual_fill/chip_button.h
@@ -6,10 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CHIP_BUTTON_H_
 #define IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CHIP_BUTTON_H_
 
-#import <UIKit/UIKit.h>
+#import "ios/chrome/browser/shared/ui/elements/extended_touch_target_button.h"
 
 // Buttons with a rounded border and gray background in highlighted state.
-@interface ChipButton : UIButton
+@interface ChipButton : ExtendedTouchTargetButton
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_AUTOFILL_MANUAL_FILL_CHIP_BUTTON_H_
