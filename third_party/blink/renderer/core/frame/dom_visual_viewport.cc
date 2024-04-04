de761a7ddfd3da8fccf50a34d5bd498c65956ba6
Menard, Alexis <alexis.menard@intel.com>
2024-04-04 00:10:22 +0000

    [Device Posture][Viewport Segments] Add a global use counter to track foldable APIs.
    For the Origin Trial we need a global use counter for the foldable APIs,
    this CL add the new counter where all the two APIs could be used.
    
    Also make sure we track the usage of the JS APIs.
    
    Bug: 40124716, 40113439
    Change-Id: Ia18fefdf811071e844f8c06ffe65dfe392ac7523
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422208
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282239}

--- third_party/blink/renderer/core/frame/dom_visual_viewport.cc
+++ third_party/blink/renderer/core/frame/dom_visual_viewport.cc
@@ -202,6 +202,8 @@ std::optional<HeapVector<Member<DOMRect>>> DOMVisualViewport::segments() const {
     return std::nullopt;
   }
 
+  UseCounter::Count(frame->GetDocument(), WebFeature::kFoldableAPIs);
+
   WebVector<gfx::Rect> web_segments =
       frame->GetWidgetForLocalRoot()->ViewportSegments();
 
