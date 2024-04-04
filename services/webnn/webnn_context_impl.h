04f5f21886cf01d4608a5a8dcee8241ba3687dd2
Bryan Bernhart <bryan.bernhart@intel.com>
2024-04-04 01:54:21 +0000

    WebNN: start creating graphs owned by context impl
    Instead of creating WebNNGraphImpls as standalone objects,
    this change creates them as regular receivers which are owned
    by the WebNNContextImpls used to create them.
    After this change, graph operations can use the context safely via raw_ptr.
    
    Bug: 1472888
    Change-Id: I1168bd105b9633536b788c95fa365a07f1a6edc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5416733
    Commit-Queue: Bryan Bernhart <bryan.bernhart@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1282278}

--- services/webnn/webnn_context_impl.h
+++ services/webnn/webnn_context_impl.h
@@ -11,14 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/unique_receiver_set.h"
 #include "services/webnn/public/mojom/webnn_buffer.mojom.h"
 #include "services/webnn/public/mojom/webnn_context_provider.mojom.h"
+#include "services/webnn/public/mojom/webnn_graph.mojom.h"
 #include "services/webnn/webnn_object_impl.h"
 
 namespace webnn {
 
 class WebNNBufferImpl;
 class WebNNContextProviderImpl;
+class WebNNGraphImpl;
 
 class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextImpl
     : public mojom::WebNNContext {
@@ -46,6 +49,14 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextImpl
   // buffer to be written to then execute the write buffer operation.
   void WriteBuffer(const WebNNBufferImpl& dst_buffer,
                    mojo_base::BigBuffer src_buffer);
+
+  // This method will be called once `WebNNGraph::CreateGraph()` completes
+  // initialization to associate the `WebNNGraph` instance and receiver to
+  // this context. Once called, the `WebNNGraph` instance can safely access the
+  // `WebNNContext` instance in graph operations.
+  void OnWebNNGraphImplCreated(
+      mojo::PendingReceiver<mojom::WebNNGraph> receiver,
+      std::unique_ptr<WebNNGraphImpl> graph_impl);
 
  protected:
   void OnConnectionError();
@@ -102,6 +113,10 @@ class COMPONENT_EXPORT(WEBNN_SERVICE) WebNNContextImpl
   // Determines if a WebNNBuffer is still connected with this context so WebNN
   // operations can use it.
   bool IsWebNNBufferValid(const base::UnguessableToken& handle) const;
+
+  // GraphsImpls which are stored on the context to allow graph
+  // operations to use this context safely via a raw_ptr.
+  mojo::UniqueReceiverSet<mojom::WebNNGraph> graph_impls_;
 };
 
 }  // namespace webnn
