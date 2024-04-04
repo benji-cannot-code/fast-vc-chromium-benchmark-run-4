c72c00de08ae1104d4700babfe6d08fd41e79490
Jonathan Lee <jonathanjlee@google.com>
2024-04-04 01:21:08 +0000

    [wpt-importer] Clean up CL descriptions
    * Shorten the WPT GitHub link so that it fits under 72 characters. The
      9-digit SHA is almost certainly still unique.
    * Link the import CL in the bug fixup CL description.
    * Remove extra newline above "Note to gardeners: ...", since `git` adds
      its own (e.g., https://crrev.com/c/5419750).
    
    Bug: 40631540
    Change-Id: If816c62b3ac085e2fde2c9be0eadebaddf5b8b7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420804
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282267}

--- third_party/blink/tools/blinkpy/w3c/test_importer_unittest.py
+++ third_party/blink/tools/blinkpy/w3c/test_importer_unittest.py
@@ -469,11 +469,12 @@ class TestImporterTest(LoggingTestCase):
     def test_commit_message(self):
         importer = self._get_test_importer(self.mock_host())
         self.assertEqual(
-            importer.commit_message('aaaa', CommitRange('0000', '1111')),
+            importer.commit_message('aaaa',
+                                    CommitRange('0123456789', 'a123456789')),
             textwrap.dedent("""\
-                Import wpt@1111
+                Import wpt@a123456789
 
-                https://github.com/web-platform-tests/wpt/compare/0000...1111
+                https://github.com/web-platform-tests/wpt/compare/012345678...a12345678
 
                 Using wpt-import in Chromium aaaa.
                 """))
@@ -504,7 +505,7 @@ class TestImporterTest(LoggingTestCase):
 
     def test_cl_description_with_empty_environ(self):
         host = self.mock_host()
-        host.executive = MockExecutive(output='Last commit message\n')
+        host.executive = MockExecutive(output='Last commit message\n\n')
         importer = self._get_test_importer(host)
         description = importer.cl_description(directory_owners={})
         self.assertEqual(
@@ -528,7 +529,7 @@ class TestImporterTest(LoggingTestCase):
 
     def test_cl_description_with_directory_owners(self):
         host = self.mock_host()
-        host.executive = MockExecutive(output='Last commit message\n')
+        host.executive = MockExecutive(output='Last commit message\n\n')
         importer = self._get_test_importer(host)
         description = importer.cl_description(
             directory_owners={
@@ -665,7 +666,7 @@ class TestImporterTest(LoggingTestCase):
         git.commit_locally_with_message(f'Import wpt@{"f" * 40}')
 
         local_wpt = MockLocalWPT()
-        gerrit_cl = mock.Mock(messages=[])
+        gerrit_cl = mock.Mock(messages=[], number=999)
         gerrit_api = mock.Mock()
         gerrit_api.query_cls.return_value = [gerrit_cl]
         self.buganizer_client.NewIssue.side_effect = lambda issue: BuganizerIssue(
@@ -698,7 +699,7 @@ class TestImporterTest(LoggingTestCase):
                 crbug.com/444 external/wpt/foo/do-not-modify.html [ Failure ]
                 """))
         expected_message = textwrap.dedent("""\
-            [wpt-import] Update `TestExpectations` with bugs for new failures
+            Update `TestExpectations` with bugs filed for crrev.com/c/999
 
             Bug: 111
             """)
@@ -780,7 +781,7 @@ class TestImporterTest(LoggingTestCase):
         }
         notifier.main.return_value = {
             'external/wpt/foo': BuganizerIssue('New failures', '', '', 111),
-        }
+        }, mock.Mock()
 
         with importer:
             importer.file_and_record_bugs(notifier)
