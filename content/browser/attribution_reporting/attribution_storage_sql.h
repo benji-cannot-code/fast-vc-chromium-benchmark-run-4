99602f1b7002217a7c3e0e12a34180db832baa89
Nan Lin <linnan@chromium.org>
2024-04-04 21:10:16 +0000

    Inline CheckDestinationRateLimit
    Change-Id: I15bd61ffd453e64b594652312fad25432be096c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426735
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282725}

--- content/browser/attribution_reporting/attribution_storage_sql.h
+++ content/browser/attribution_reporting/attribution_storage_sql.h
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/attribution_reporting/attribution_storage.h"
 #include "content/browser/attribution_reporting/attribution_trigger.h"
 #include "content/browser/attribution_reporting/rate_limit_table.h"
-#include "content/browser/attribution_reporting/store_source_result.h"
 #include "content/browser/attribution_reporting/stored_source.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/attribution_data_model.h"
@@ -39,6 +38,7 @@ namespace content {
 
 class AttributionStorageDelegate;
 class StorableSource;
+class StoreSourceResult;
 struct AttributionInfo;
 
 enum class RateLimitResult : int;
@@ -177,10 +177,6 @@ class CONTENT_EXPORT AttributionStorageSql : public AttributionStorage {
                  StoragePartition::StorageKeyMatcherFunction filter,
                  bool delete_rate_limit_data) override;
   void SetDelegate(std::unique_ptr<AttributionStorageDelegate>) override;
-
-  [[nodiscard]] StoreSourceResult::Result CheckDestinationRateLimit(
-      const StorableSource& source,
-      base::Time source_time);
 
   void ClearAllDataAllTime(bool delete_rate_limit_data)
       VALID_CONTEXT_REQUIRED(sequence_checker_);
