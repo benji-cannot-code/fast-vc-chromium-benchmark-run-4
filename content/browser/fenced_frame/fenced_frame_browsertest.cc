18c302b6c9e2608b5403c849f8cfc025a122f459
Joanne de Abreu <jdeabreu@google.com>
2024-04-04 14:08:08 +0000

    Ship Cross App and Web Attribution Measurement
    Enable AttributionReportingCrossAppWeb feature by default. Note that this also enables Attribution
    Reporting web interfaces which are gated by this feature.
    
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/lG0O_jXbiws/m/_JLJIl6oAAAJ
    
    Bug: 331926920
    Change-Id: I1184f4d547249e0e1582506fafe52eaa98d9a4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402829
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Joanne de Abreu <jdeabreu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282470}

--- content/browser/fenced_frame/fenced_frame_browsertest.cc
+++ content/browser/fenced_frame/fenced_frame_browsertest.cc
@@ -5459,12 +5459,18 @@ class FencedFrameReportEventBrowserTest
           ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
               response.http_request()->headers.at(
                   "Attribution-Reporting-Eligible"));
+          ExpectValidAttributionReportingSupportHeader(
+              response.http_request()->headers.at(
+                  "Attribution-Reporting-Support"),
+              /*web_expected=*/true,
+              /*os_expected=*/false);
         } else {
           EXPECT_FALSE(base::Contains(response.http_request()->headers,
                                       "Attribution-Reporting-Eligible"));
+          EXPECT_FALSE(base::Contains(response.http_request()->headers,
+                                      "Attribution-Reporting-Support"));
         }
-        EXPECT_FALSE(base::Contains(response.http_request()->headers,
-                                    "Attribution-Reporting-Support"));
+
         // TODO(crbug.com/1496395): Remove this check after 3PCD.
         EXPECT_EQ(0U, response.http_request()->headers.count("Cookie"));
         response.Done();
@@ -6118,8 +6124,11 @@ IN_PROC_BROWSER_TEST_F(FencedFrameReportEventBrowserTest,
   ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
       reporting_response.http_request()->headers.at(
           "Attribution-Reporting-Eligible"));
-  EXPECT_FALSE(base::Contains(reporting_response.http_request()->headers,
-                              "Attribution-Reporting-Support"));
+  ExpectValidAttributionReportingSupportHeader(
+      reporting_response.http_request()->headers.at(
+          "Attribution-Reporting-Support"),
+      /*web_expected=*/true,
+      /*os_expected=*/false);
 }
 
 // The reportEvent beacon is a POST request. Upon receiving a 302 redirect
@@ -6205,13 +6214,15 @@ IN_PROC_BROWSER_TEST_F(FencedFrameReportEventBrowserTest,
               net::test_server::HttpMethod::METHOD_POST);
     ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
         response.http_request()->headers.at("Attribution-Reporting-Eligible"));
+    ExpectValidAttributionReportingSupportHeader(
+        response.http_request()->headers.at("Attribution-Reporting-Support"),
+        /*web_expected=*/true,
+        /*os_expected=*/false);
     EXPECT_TRUE(
         base::Contains(response.http_request()->headers, "Content-Length"));
     EXPECT_TRUE(
         base::Contains(response.http_request()->headers, "Content-Type"));
     EXPECT_TRUE(base::Contains(response.http_request()->headers, "Origin"));
-    EXPECT_FALSE(base::Contains(response.http_request()->headers,
-                                "Attribution-Reporting-Support"));
 
     // Send 302 redirect response.
     GURL redirect_url = https_server()->GetURL("a.test", "/redirect.html");
@@ -6242,8 +6253,10 @@ IN_PROC_BROWSER_TEST_F(FencedFrameReportEventBrowserTest,
     ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
         redirect_response.http_request()->headers.at(
             "Attribution-Reporting-Eligible"));
-    EXPECT_FALSE(base::Contains(response.http_request()->headers,
-                                "Attribution-Reporting-Support"));
+    ExpectValidAttributionReportingSupportHeader(
+        response.http_request()->headers.at("Attribution-Reporting-Support"),
+        /*web_expected=*/true,
+        /*os_expected=*/false);
   }
 }
 
@@ -6336,8 +6349,11 @@ IN_PROC_BROWSER_TEST_F(FencedFrameReportEventBrowserTest,
     ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
         reporting_response.http_request()->headers.at(
             "Attribution-Reporting-Eligible"));
-    EXPECT_FALSE(base::Contains(reporting_response.http_request()->headers,
-                                "Attribution-Reporting-Support"));
+    ExpectValidAttributionReportingSupportHeader(
+        reporting_response.http_request()->headers.at(
+            "Attribution-Reporting-Support"),
+        /*web_expected=*/true,
+        /*os_expected=*/false);
 
     // Send 302 redirect response, with "Access-Control-Allow-Origin" header.
     // This header is needed to get the redirect through.
@@ -6367,8 +6383,11 @@ IN_PROC_BROWSER_TEST_F(FencedFrameReportEventBrowserTest,
     ExpectValidAttributionReportingEligibleHeaderForEventBeacon(
         redirect_response.http_request()->headers.at(
             "Attribution-Reporting-Eligible"));
-    EXPECT_FALSE(base::Contains(reporting_response.http_request()->headers,
-                                "Attribution-Reporting-Support"));
+    ExpectValidAttributionReportingSupportHeader(
+        redirect_response.http_request()->headers.at(
+            "Attribution-Reporting-Support"),
+        /*web_expected=*/true,
+        /*os_expected=*/false);
   }
 }
 
@@ -7393,8 +7412,10 @@ class FencedFrameAutomaticBeaconBrowserTest
     // Verify the request contains the eligibility header.
     ExpectValidAttributionReportingEligibleHeaderForNavigation(
         response.http_request()->headers.at("Attribution-Reporting-Eligible"));
-    EXPECT_FALSE(base::Contains(response.http_request()->headers,
-                                "Attribution-Reporting-Support"));
+    ExpectValidAttributionReportingSupportHeader(
+        response.http_request()->headers.at("Attribution-Reporting-Support"),
+        /*web_expected=*/true,
+        /*os_expected=*/false);
 
     // Verify the request has credentials attached.
     // TODO(crbug.com/1496395): Remove this block after 3PCD.
