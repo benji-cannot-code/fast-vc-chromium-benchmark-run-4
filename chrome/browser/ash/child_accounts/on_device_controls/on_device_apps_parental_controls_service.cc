6050477d88c057bafe2cbb91f1ff0434c284a76a
Courtney Wong <courtneywong@chromium.org>
2024-04-04 23:13:10 +0000

    Create factory to fetch on device apps parental controls service
    Bug: b:332925160
    Change-Id: Ia856c804be4c25b109f91162cbdb46b194b6d861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425695
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282817}

---
+++ chrome/browser/ash/child_accounts/on_device_controls/on_device_apps_parental_controls_service.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/child_accounts/on_device_controls/on_device_apps_parental_controls_service.h"
+
+namespace ash {
+
+OnDeviceAppsParentalControlsService::OnDeviceAppsParentalControlsService() =
+    default;
+
+OnDeviceAppsParentalControlsService::~OnDeviceAppsParentalControlsService() =
+    default;
+
+}  // namespace ash
---
+++ chrome/browser/ash/child_accounts/on_device_controls/on_device_apps_parental_controls_service_factory.h
@@ -0,0 +1,55 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_CHILD_ACCOUNTS_ON_DEVICE_CONTROLS_ON_DEVICE_APPS_PARENTAL_CONTROLS_SERVICE_FACTORY_H_
+#define CHROME_BROWSER_ASH_CHILD_ACCOUNTS_ON_DEVICE_CONTROLS_ON_DEVICE_APPS_PARENTAL_CONTROLS_SERVICE_FACTORY_H_
+
+#include "chrome/browser/profiles/profile_keyed_service_factory.h"
+
+namespace base {
+template <typename T>
+class NoDestructor;
+}  // namespace base
+
+namespace content {
+class BrowserContext;
+}  // namespace content
+
+namespace ash {
+class OnDeviceAppsParentalControlsService;
+
+class OnDeviceAppsParentalControlsServiceFactory
+    : public ProfileKeyedServiceFactory {
+ public:
+  OnDeviceAppsParentalControlsServiceFactory(
+      const OnDeviceAppsParentalControlsServiceFactory&) = delete;
+  OnDeviceAppsParentalControlsServiceFactory& operator=(
+      const OnDeviceAppsParentalControlsServiceFactory&) = delete;
+
+  // Returns singleton instance of OnDeviceAppsParentalControlsServiceFactory.
+  static OnDeviceAppsParentalControlsServiceFactory* GetInstance();
+
+  // Returns whether or not on-device apps parental controls is supported for
+  // |context|.
+  static bool IsOnDeviceAppsParentalControlsAvailable(
+      content::BrowserContext* context);
+
+  // Returns the OnDeviceAppsParentalControlsService associated with |context|.
+  static OnDeviceAppsParentalControlsService* GetForBrowserContext(
+      content::BrowserContext* context);
+
+ private:
+  friend base::NoDestructor<OnDeviceAppsParentalControlsServiceFactory>;
+
+  OnDeviceAppsParentalControlsServiceFactory();
+  ~OnDeviceAppsParentalControlsServiceFactory() override;
+
+  // BrowserContextKeyedServiceFactory:
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
+      content::BrowserContext* context) const override;
+};
+}  // namespace ash
+
+#endif
