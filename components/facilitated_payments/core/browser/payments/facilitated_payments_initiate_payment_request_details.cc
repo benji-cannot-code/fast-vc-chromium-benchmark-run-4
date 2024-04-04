c172f801654bb3f3cdc6238d2ddedf544671f6a4
Vishwas Uppoor <vishwasuppoor@google.com>
2024-04-04 14:17:17 +0000

    [PIX] Initiate payment request skeleton code
    This CL creates:
    1. A PaymentsNetworkInterface implementation for non-form based FOPs
    2. A new PaymentsRequest for PIX payment
    
    Bug: b/300334562
    Change-Id: Ifbd2acbb08fae1eafae54f75d3855d940a1aa989
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410667
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282475}

---
+++ components/facilitated_payments/core/browser/payments/facilitated_payments_initiate_payment_request_details.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/facilitated_payments/core/browser/payments/facilitated_payments_initiate_payment_request_details.h"
+
+namespace payments::facilitated {
+
+FacilitatedPaymentsInitiatePaymentRequestDetails::
+    FacilitatedPaymentsInitiatePaymentRequestDetails() = default;
+
+}  // namespace payments::facilitated
---
+++ components/facilitated_payments/core/browser/payments/facilitated_payments_initiate_payment_response_details.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FACILITATED_PAYMENTS_CORE_BROWSER_PAYMENTS_FACILITATED_PAYMENTS_INITIATE_PAYMENT_RESPONSE_DETAILS_H_
+#define COMPONENTS_FACILITATED_PAYMENTS_CORE_BROWSER_PAYMENTS_FACILITATED_PAYMENTS_INITIATE_PAYMENT_RESPONSE_DETAILS_H_
+
+namespace payments::facilitated {
+
+// Contains information retrieved from an
+// `FacilitatedPaymentsInitiatePaymentRequest`.
+class FacilitatedPaymentsInitiatePaymentResponseDetails {
+ public:
+  FacilitatedPaymentsInitiatePaymentResponseDetails();
+  FacilitatedPaymentsInitiatePaymentResponseDetails(
+      const FacilitatedPaymentsInitiatePaymentResponseDetails&) = delete;
+  FacilitatedPaymentsInitiatePaymentResponseDetails& operator=(
+      const FacilitatedPaymentsInitiatePaymentResponseDetails&) = delete;
+  ~FacilitatedPaymentsInitiatePaymentResponseDetails() = default;
+};
+
+}  // namespace payments::facilitated
+
+#endif  // COMPONENTS_FACILITATED_PAYMENTS_CORE_BROWSER_PAYMENTS_FACILITATED_PAYMENTS_INITIATE_PAYMENT_RESPONSE_DETAILS_H_
