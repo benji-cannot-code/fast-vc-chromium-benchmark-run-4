86326d34c84d5d37c441df5dd4ea245098fa6689
rudranshd <rudranshd@google.com>
2024-04-04 21:48:41 +0000

    [Instant Hotspot] Rename Network to Internet in Settings
    Renames Network to Internet in Settings when instant-hotspot-rebrand
    flag is enabled
    
    Screenshot: https://screenshot.googleplex.com/7FSB7LBDHD8NtZT.jpg
    Bug: b/290370273
    Change-Id: I59fd6326c88cb7765cb5fd5834ec35ca7b6da14b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385710
    Auto-Submit: Rudransh Dikshit <rudranshd@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1282757}

--- chrome/browser/ui/webui/ash/settings/pages/internet/internet_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/internet/internet_section.cc
@@ -795,7 +795,9 @@ void InternetSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
       {"internetJoinType", IDS_SETTINGS_INTERNET_JOIN_TYPE},
       {"internetKnownNetworksPageTitle", IDS_SETTINGS_INTERNET_KNOWN_NETWORKS},
       {"internetNoNetworks", IDS_SETTINGS_INTERNET_NO_NETWORKS},
-      {"internetPageTitle", IDS_SETTINGS_INTERNET},
+      {"internetPageTitle", features::IsInstantHotspotRebrandEnabled()
+                                ? IDS_SETTINGS_INTERNET
+                                : IDS_SETTINGS_INTERNET_LEGACY},
       {"internetSummaryButtonA11yLabel",
        IDS_SETTINGS_INTERNET_SUMMARY_BUTTON_ACCESSIBILITY_LABEL},
       {"internetToggleTetherA11yLabel",
@@ -1228,7 +1230,9 @@ void InternetSection::AddHandlers(content::WebUI* web_ui) {
 }
 
 int InternetSection::GetSectionNameMessageId() const {
-  return IDS_SETTINGS_INTERNET;
+  return features::IsInstantHotspotRebrandEnabled()
+             ? IDS_SETTINGS_INTERNET
+             : IDS_SETTINGS_INTERNET_LEGACY;
 }
 
 mojom::Section InternetSection::GetSection() const {
