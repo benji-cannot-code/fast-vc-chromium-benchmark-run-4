ab4e302e06ed18fde945645ad26b422022455b39
Cheick Cisse <cheickcisse@google.com>
2024-04-05 18:13:58 +0000

    [WhatsNew][iOS] Convert User Actions to Histograms for some metrics
    This CL converts primary tap and learn more actions to histograms. This will allow us to not worry about updating actions.xml whenever we have new features. In addition, this CL cleans up the old actions from actions.xml.
    
    Bug: b/329248901
    Change-Id: If7a690cade71802cfec66f158f8408e677ef9ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424814
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283270}

--- ios/chrome/browser/ui/whats_new/whats_new_mediator.mm
+++ ios/chrome/browser/ui/whats_new/whats_new_mediator.mm
@@ -50,14 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)didTapActionButton:(WhatsNewType)type
              primaryAction:(WhatsNewPrimaryAction)primaryAction {
-  const char* type_str = WhatsNewTypeToString(type);
-  if (!type_str) {
-    return;
-  }
-
-  std::string metric =
-      base::StrCat({"WhatsNew.", type_str, ".PrimaryActionTapped"});
-  base::RecordAction(base::UserMetricsAction(metric.c_str()));
+  base::UmaHistogramEnumeration("IOS.WhatsNew.PrimaryActionTapped", type);
 
   switch (primaryAction) {
     case WhatsNewPrimaryAction::kIOSSettings:
@@ -101,31 +94,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   UrlLoadParams params = UrlLoadParams::InNewTab(learnMoreURL);
   params.web_params.transition_type = ui::PAGE_TRANSITION_AUTO_BOOKMARK;
   self.urlLoadingAgent->Load(params);
-  [self recordLearnMoreInteraction:type];
+  base::UmaHistogramEnumeration("IOS.WhatsNew.LearnMoreTapped", type);
 }
 
 - (void)didTapInstructions:(WhatsNewType)type {
-  const char* type_str = WhatsNewTypeToString(type);
-  if (!type_str) {
-    return;
-  }
-
-  std::string metric =
-      base::StrCat({"WhatsNew.", type_str, ".InstructionsTapped"});
-  base::RecordAction(base::UserMetricsAction(metric.c_str()));
   base::UmaHistogramEnumeration("IOS.WhatsNew.InstructionsShown", type);
 }
 
 #pragma mark - WhatsNewTableViewActionHandler
 
 - (void)recordWhatsNewInteraction:(WhatsNewItem*)item {
-  const char* type = WhatsNewTypeToString(item.type);
-  if (!type) {
-    return;
-  }
-
-  std::string metric = base::StrCat({"WhatsNew.", type});
-  base::RecordAction(base::UserMetricsAction(metric.c_str()));
   base::UmaHistogramEnumeration("IOS.WhatsNew.Shown", item.type);
 }
 
@@ -176,18 +154,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)updateConsumer {
   [self.consumer setWhatsNewProperties:[self whatsNewChromeTipItem]
                           featureItems:[self whatsNewFeatureItems]];
-}
-
-// Record when a user tap on learn more.
-- (void)recordLearnMoreInteraction:(WhatsNewType)type {
-  const char* type_str = WhatsNewTypeToString(type);
-  if (!type_str) {
-    return;
-  }
-
-  std::string metric =
-      base::StrCat({"WhatsNew.", type_str, ".LearnMoreTapped"});
-  base::RecordAction(base::UserMetricsAction(metric.c_str()));
 }
 
 @end
