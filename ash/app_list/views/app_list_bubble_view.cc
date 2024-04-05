11689ccd03018518c4d57e542e7320ebb20c2102
Ana Salazar <anasalazar@chromium.org>
2024-04-05 18:57:28 +0000

    Set correct apps page after query changed to empty
    Use the AppsCollectionsController method to select is the bubble view
    shows AppsCollections or AppsPage after a query changed.
    
    Bug: b:332606516
    Change-Id: Ic455fa66cb33c940974ebdfd9841f50a16342984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426930
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283299}

--- ash/app_list/views/app_list_bubble_view.cc
+++ ash/app_list/views/app_list_bubble_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/accessibility_controller.h"
 #include "ash/app_list/app_list_model_provider.h"
 #include "ash/app_list/app_list_util.h"
+#include "ash/app_list/apps_collections_controller.h"
 #include "ash/app_list/model/app_list_folder_item.h"
 #include "ash/app_list/views/app_list_a11y_announcer.h"
 #include "ash/app_list/views/app_list_bubble_apps_collections_page.h"
@@ -747,7 +748,7 @@ void AppListBubbleView::QueryChanged(const std::u16string& trimmed_query,
     search_page_->search_view()->UpdateForNewSearch(!trimmed_query.empty());
     if (!trimmed_query.empty()) {
       ShowPage(AppListBubblePage::kSearch);
-    } else if (app_list_features::IsAppsCollectionsEnabled()) {
+    } else if (AppsCollectionsController::Get()->ShouldShowAppsCollection()) {
       ShowPage(AppListBubblePage::kAppsCollections);
     } else {
       ShowPage(AppListBubblePage::kApps);
