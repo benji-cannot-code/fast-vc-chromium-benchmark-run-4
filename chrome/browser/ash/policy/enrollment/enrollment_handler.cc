6304597f849dde69e0698055c32a17e73f683519
Yves Arrouye <drcrash@google.com>
2024-04-05 18:26:13 +0000

    [fre-on-flex] Do not request state keys if FRE is not enabled
    Currently, we do not request state keys if on Flex, but what we
    really want is to not request them if FRE is not enabled/supported.
    This CL makes that trivial change, paving the way for doing the
    right thing once Flex supports FRE.
    
    Bug: b:332709076
    Test: browser_tests --gtest_filter=EnrollmentScreenTest
    test: unit_tests --gtest_filter=*DeviceCloudPolicyManagerAsh*
    Change-Id: I6907e7964ece7c5e68d9463c5b344dc7b48da1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420534
    Auto-Submit: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Yves Arrouye <drcrash@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283279}

--- chrome/browser/ash/policy/enrollment/enrollment_handler.cc
+++ chrome/browser/ash/policy/enrollment/enrollment_handler.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/constants/ash_features.h"
-#include "ash/constants/ash_switches.h"
 #include "base/base64.h"
 #include "base/command_line.h"
 #include "base/functional/bind.h"
@@ -268,19 +267,26 @@ void EnrollmentHandler::StartEnrollment() {
     return;
   }
 
-  // Currently reven devices don't support server-backed state keys, but they
-  // also don't support FRE/AutoRE so don't block enrollment on the
-  // availability of state keys.
-  // TODO(b/208705225): Remove this special case when reven supports state keys.
-  if (ash::switches::IsRevenBranding()) {
+  // Request state keys if FRE is enabled.
+  if (AutoEnrollmentTypeChecker::IsFREEnabled()) {
+    LOG(WARNING) << "Requesting state keys.";
+    state_keys_broker_->RequestStateKeys(base::BindOnce(
+        // This simply allows the keys to be wrapped in an std::optional, which
+        // the compiler can't do if we just bind
+        // &EnrollmentHandler::HandleStateKeys.
+        [](base::WeakPtr<EnrollmentHandler> self,
+           const std::vector<std::string>& keys) {
+          if (self) {
+            self->HandleStateKeys(keys);
+          }
+        },
+        weak_ptr_factory_.GetWeakPtr()));
+  } else {
+    // Skip the request for state keys, but handle their absence and move to the
+    // next step.
     LOG(WARNING) << "Skipping state keys.";
-    HandleStateKeysResult({});
-    return;
+    HandleStateKeys(std::nullopt);
   }
-  LOG(WARNING) << "Requesting state keys.";
-  state_keys_broker_->RequestStateKeys(
-      base::BindOnce(&EnrollmentHandler::HandleStateKeysResult,
-                     weak_ptr_factory_.GetWeakPtr()));
 }
 
 std::unique_ptr<CloudPolicyClient> EnrollmentHandler::ReleaseClient() {
@@ -400,12 +406,13 @@ void EnrollmentHandler::OnStoreError(CloudPolicyStore* store) {
                                                store_->validation_status()));
 }
 
-void EnrollmentHandler::HandleStateKeysResult(
-    const std::vector<std::string>& state_keys) {
+void EnrollmentHandler::HandleStateKeys(
+    std::optional<std::vector<std::string>> opt_state_keys) {
   DCHECK_EQ(STEP_STATE_KEYS, enrollment_step_);
 
-  // Make sure state keys are available if forced re-enrollment is on.
-  if (AutoEnrollmentTypeChecker::IsFREEnabled()) {
+  if (opt_state_keys.has_value()) {
+    auto state_keys = opt_state_keys.value();
+
     client_->SetStateKeysToUpload(state_keys);
     register_params_->current_state_key =
         state_keys_broker_->current_state_key();
@@ -415,11 +422,13 @@ void EnrollmentHandler::HandleStateKeysResult(
           EnrollmentStatus::Code::kNoStateKeys));
       return;
     }
+
+    // Logging as "WARNING" to make sure it's preserved in the logs.
+    LOG(WARNING) << "State keys generated, success=" << !state_keys.empty();
+  } else {
+    LOG(WARNING) << "State keys are not used.";
   }
 
-  // TODO(crbug.com/1271134): Logging as "WARNING" to make sure it's preserved
-  // in the logs.
-  LOG(WARNING) << "State keys generated, success=" << !state_keys.empty();
   SetStep(STEP_LOADING_STORE);
   StartRegistration();
 }
