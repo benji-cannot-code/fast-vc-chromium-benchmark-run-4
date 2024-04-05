10284421271ca6baf96c453454049636ee5e6b6e
Slobodan Pejic <slobodan@chromium.org>
2024-04-05 20:55:07 +0000

    Implement swipe to dismiss in the card unmask authentication selection on iOS.
    When unmasking payment cards: Before, the authentication selection sheet
    could be dismissed by swiping; however, this did not stop the
    coordinator for the card unmasking flow. Now, swiping the sheet stops
    the coordinator (and also logs metrics, the same as tapping the cancel
    button.)
    
    Bug: 40282545, b/324886800
    Change-Id: If28ec268995d5a55dccce25c9d0d286ceeb9ddde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422788
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283377}

--- ios/chrome/browser/ui/autofill/authentication/card_unmask_authentication_selection_view_controller.h
+++ ios/chrome/browser/ui/autofill/authentication/card_unmask_authentication_selection_view_controller.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // cards.
 @interface CardUnmaskAuthenticationSelectionViewController
     : ChromeTableViewController <CardUnmaskAuthenticationSelectionConsumer,
+                                 UIAdaptivePresentationControllerDelegate,
                                  UITableViewDelegate>
 
 // The delegate for user actions.
