da8db41cab36bb88b078aa5ba525b059c21425ea
Aliona DANGLA <alionadangla@google.com>
2024-04-05 18:03:37 +0000

    [iOS] Close tab in group view
    This CL ensures to update the tab group view and the grid when a tab in
    a groups is closed.
    
    See video:
    https://drive.google.com/file/d/1z25R18bnSbA9i5RZ8TnskQu-tmbeBSAv/view?usp=drive_link
    
    Bug: 331926148
    Fixed: 331898820, 332842951
    Change-Id: I0c4969ed0a72b1742306522d204613ce03623200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424390
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283258}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "ios/chrome/browser/shared/model/web_state_list/web_state_list_observer_bridge.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_collection_drag_drop_handler.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator_items_provider.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_commands.h"
@@ -27,6 +28,10 @@ class Browser;
 @protocol TabGroupsCommands;
 @protocol TabPresentationDelegate;
 class WebStateList;
+
+namespace web {
+class WebState;
+}
 
 // Mediates between model layer and tab grid UI layer.
 @interface BaseGridMediator : NSObject <BaseGridMediatorItemProvider,
@@ -61,7 +66,7 @@ class WebStateList;
 
 @end
 
-@interface BaseGridMediator (Subclassing)
+@interface BaseGridMediator (Subclassing) <WebStateListObserving>
 
 // Current mode.
 @property(nonatomic, assign) TabGridMode currentMode;
@@ -83,6 +88,15 @@ class WebStateList;
 
 // Calls `-populateItems:selectedItemID:` on the consumer.
 - (void)populateConsumerItems;
+
+// Adds an observation to every non-pinned WebState. Subclasses can override
+// this to observe a different set of `WebState`s. It's not necessary to call
+// the parent class implementation if not desired.
+- (void)addWebStateObservations;
+
+// Adds or removes observation to the given `webState`.
+- (void)addObservationForWebState:(web::WebState*)webState;
+- (void)removeObservationForWebState:(web::WebState*)webState;
 
 @end
 
--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // No-op
 }
 
+// Overrides the parent to only display tabs from the group.
 - (void)populateConsumerItems {
   if (!self.webStateList || !_tabGroup) {
     return;
@@ -106,6 +107,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.consumer populateItems:CreateTabItems(self.webStateList,
                                               _tabGroup->range())
         selectedItemIdentifier:identifier];
+}
+
+// Overrides the parent observations: only observe the group `WebState`s.
+- (void)addWebStateObservations {
+  for (int index : _tabGroup->range()) {
+    web::WebState* webState = self.webStateList->GetWebStateAt(index);
+    [self addObservationForWebState:webState];
+  }
+}
+
+#pragma mark - WebStateListObserving override
+
+// Overrides the parent observations. The parent treats a group as one cell,
+// whereas this TabGroupMediator only cares about one group, and shows grouped
+// tabs as many cells.
+- (void)willChangeWebStateList:(WebStateList*)webStateList
+                        change:(const WebStateListChangeDetach&)detachChange
+                        status:(const WebStateListStatus&)status {
+  DCHECK_EQ(self.webStateList, webStateList);
+  if (webStateList->IsBatchInProgress()) {
+    return;
+  }
+  CHECK(detachChange.group() == _tabGroup);
+
+  web::WebState* detachedWebState = detachChange.detached_web_state();
+  GridItemIdentifier* identifierToRemove =
+      [GridItemIdentifier tabIdentifier:detachedWebState];
+  GridItemIdentifier* selectedIdentifier;
+  if (self.webStateList->GetGroupOfWebStateAt(webStateList->active_index()) ==
+      _tabGroup) {
+    selectedIdentifier =
+        [GridItemIdentifier tabIdentifier:webStateList->GetActiveWebState()];
+  }
+  [self.consumer removeItemWithIdentifier:identifierToRemove
+                   selectedItemIdentifier:selectedIdentifier];
+  [self removeObservationForWebState:detachedWebState];
 }
 
 @end
