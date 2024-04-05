3853597c303e0718d8d2db9f93cd6c4e53e868fa
rbpotter <rbpotter@chromium.org>
2024-04-05 15:58:29 +0000

    WebUI: Remove some unnecessary Polymer dependencies in cr_elements
    - Change cr-toolbar-selection-overlay to use CrA11yAnnouncer instead of
      IronA11yAnnouncer
    - Remove imports of paper-styles/color.js from places where no paper-
      colors are actually used.
    
    Bug: 40943652
    Change-Id: I622b8d270d2e6f9dcb1d28003c5d2cd393e60ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425423
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283174}

--- ui/webui/resources/cr_elements/cr_profile_avatar_selector/cr_profile_avatar_selector.ts
+++ ui/webui/resources/cr_elements/cr_profile_avatar_selector/cr_profile_avatar_selector.ts
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../cr_button/cr_button.js';
 import '../cr_grid/cr_grid.js';
-import '//resources/polymer/v3_0/paper-styles/color.js';
 import '//resources/polymer/v3_0/paper-tooltip/paper-tooltip.js';
 
 import {assert} from '//resources/js/assert.js';
