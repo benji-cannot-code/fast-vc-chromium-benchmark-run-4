65f03c100e8a9b7efa05ffffd0109f9cff49987d
Guoxing Zhao <charleszhao@chromium.org>
2024-04-05 00:13:28 +0000

    [VC] Integration test For Background replace buttons.
    Bug=b:325147805
    TEST="autoninja -C  out/Test chrome/test:browser_tests && out/Test/browser_tests --gtest_filter=VideoConferenceIntegrationTest.*"
    
    Change-Id: I6677b3004b84f3cc1cee16da7219bc0b6976259c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401305
    Reviewed-by: CJ Huang <chenjih@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282852}

--- ash/system/video_conference/effects/video_conference_tray_effects_manager_types.h
+++ ash/system/video_conference/effects/video_conference_tray_effects_manager_types.h
@@ -44,7 +44,8 @@ class ASH_EXPORT VcEffectState {
                 const std::u16string& label_text,
                 int accessible_name_id,
                 base::RepeatingClosure button_callback,
-                std::optional<int> state_value = std::nullopt);
+                std::optional<int> state_value = std::nullopt,
+                int view_id = -1);
 
   VcEffectState(const VcEffectState&) = delete;
   VcEffectState& operator=(const VcEffectState&) = delete;
@@ -55,6 +56,7 @@ class ASH_EXPORT VcEffectState {
   const gfx::VectorIcon* icon() const { return icon_; }
   const std::u16string& label_text() const { return label_text_; }
   int accessible_name_id() const { return accessible_name_id_; }
+  int view_id() const { return view_id_; }
   const base::RepeatingClosure& button_callback() const {
     return button_callback_;
   }
@@ -77,6 +79,9 @@ class ASH_EXPORT VcEffectState {
 
   // The state value.
   std::optional<int> state_value_;
+
+  // Optional id used to identify the state view.
+  const int view_id_;
 };
 
 // Designates the type of user-adjustments made to this effect.
