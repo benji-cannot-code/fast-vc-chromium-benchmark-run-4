584bdf791ae5777d496215849445f1d6fa3f917c
Dominic Farolino <dom@chromium.org>
2024-04-05 08:44:21 +0000

    DOM: Introduce state-preserving atomic move flag
    As of this CL, the flag does nothing, but it gives us a gate behind
    which we can start implementing SPAM (state-preserving move) behaviors.
    
    R=nrosenthal@chromium.org
    
    Bug: 40150299
    Change-Id: I01b2ed52117d7f9fc2b734ceda2939ba24e4f0d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425852
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Auto-Submit: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283004}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -10638,6 +10638,10 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kObservableAPIDescription, kOsAll,
      FEATURE_VALUE_TYPE(blink::features::kObservableAPI)},
 
+    {"atomic-move-override", flag_descriptions::kAtomicMoveOverrideName,
+     flag_descriptions::kAtomicMoveOverrideDescription, kOsAll,
+     FEATURE_VALUE_TYPE(blink::features::kAtomicMoveOverride)},
+
 #if BUILDFLAG(IS_ANDROID)
     {"android-hub", flag_descriptions::kAndroidHubName,
      flag_descriptions::kAndroidHubDescription, kOsAndroid,
