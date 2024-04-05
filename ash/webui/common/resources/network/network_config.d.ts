7a9c9689883c727714d8fee5f0c5ccc5f5b52f64
Gavin Williams <gavinwill@chromium.org>
2024-04-05 22:54:20 +0000

    Shimless: Convert network_page test to ts
    - Fix all ts errors and use ts styling.
    - Include missing attributes to the NetworkConfig and NetworkList
      elements.
    
    Test: browser_tests --gtest_filter=ShimlessRmaApp.OnboardingNetworkPage
    Bug: 315002705
    Change-Id: I5b89e63825248d46d1cacfd5d2e1e066dbb8c3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422051
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283445}

--- ash/webui/common/resources/network/network_config.d.ts
+++ ash/webui/common/resources/network/network_config.d.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {LegacyElementMixin} from '//resources/polymer/v3_0/polymer/lib/legacy/legacy-element-mixin.js';
 interface NetworkConfigElement extends LegacyElementMixin, HTMLElement {
+  enableConnect: boolean;
   init(): void;
   connect(): void;
   save(): void;
--- ash/webui/common/resources/network/network_list.d.ts
+++ ash/webui/common/resources/network/network_list.d.ts
@@ -11,6 +11,7 @@ import {OncMojo} from './onc_mojo.js';
 interface NetworkListElement extends LegacyElementMixin, HTMLElement {
   networks: OncMojo.NetworkStateProperties[];
   customItems: NetworkList.CustomItemState[];
+  disabled: boolean;
 }
 
 declare global {
