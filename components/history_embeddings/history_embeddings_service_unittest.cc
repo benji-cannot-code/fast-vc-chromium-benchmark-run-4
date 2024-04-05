d923a1e874d6a47149ec16ecf80ad5fdced12e7d
Sophie Chang <sophiechang@chromium.org>
2024-04-05 18:49:47 +0000

    Incorporate content visibility calculation into search results for history embeddings
    Bug: 331260830
    Change-Id: Ifbb788929b8909c48e56f3ddf5ad57fd16f9ded8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426879
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283295}

--- components/history_embeddings/history_embeddings_service_unittest.cc
+++ components/history_embeddings/history_embeddings_service_unittest.cc
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "components/history/core/browser/history_service.h"
 #include "components/history/core/test/history_service_test_util.h"
+#include "components/optimization_guide/core/test_optimization_guide_model_provider.h"
+#include "components/page_content_annotations/core/test_page_content_annotations_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace history_embeddings {
@@ -24,6 +26,14 @@ class HistoryEmbeddingsTest : public testing::Test {
     history_service_ =
         history::CreateHistoryService(history_dir_.GetPath(), true);
     CHECK(history_service_);
+
+    optimization_guide_model_provider_ = std::make_unique<
+        optimization_guide::TestOptimizationGuideModelProvider>();
+
+    page_content_annotations_service_ =
+        page_content_annotations::TestPageContentAnnotationsService::Create(
+            optimization_guide_model_provider_.get(), history_service_.get());
+    CHECK(page_content_annotations_service_);
   }
 
   void TearDown() override {}
@@ -34,11 +44,15 @@ class HistoryEmbeddingsTest : public testing::Test {
 
   base::ScopedTempDir history_dir_;
   std::unique_ptr<history::HistoryService> history_service_;
+  std::unique_ptr<optimization_guide::TestOptimizationGuideModelProvider>
+      optimization_guide_model_provider_;
+  std::unique_ptr<page_content_annotations::TestPageContentAnnotationsService>
+      page_content_annotations_service_;
 };
 
 TEST_F(HistoryEmbeddingsTest, ConstructsAndInvalidatesWeakPtr) {
-  auto service =
-      std::make_unique<HistoryEmbeddingsService>(history_service_.get());
+  auto service = std::make_unique<HistoryEmbeddingsService>(
+      history_service_.get(), page_content_annotations_service_.get());
   auto weak_ptr = service->AsWeakPtr();
   EXPECT_TRUE(weak_ptr);
   service.reset();
