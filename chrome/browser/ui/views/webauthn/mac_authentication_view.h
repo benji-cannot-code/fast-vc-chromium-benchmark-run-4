6597a42702b0cb9b97faacebc0732e1a6cd8a1a6
Yuki Shiino <yukishiino@chromium.org>
2024-04-05 00:49:30 +0000

    Revert "[webauthn] Polish Touch ID UI"
    This reverts commit 3e9b1bb614ed93e729aa553af32454fc81944e4f.
    
    Reason for revert: Suspicious about causing test failures of
    GPMPasskeysAuthenticatorDialogTest.InvokeUi_touchid
    at
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac11%20Tests/26633/overview
    
    Original change's description:
    > [webauthn] Polish Touch ID UI
    >
    > Polish the Touch ID GPM UI:
    > * Move the UI to a sheet.
    > * Wait for the animation to finish before proceeding.
    > * Add text displaying the account details (for get assertion & make
    >   credential)
    > * Catch Touch ID being locked and defer to a password if that happens.
    > * Remove old bubble code.
    >
    > Bug: 40274370
    > Change-Id: I73cb9470b1a0a4d2531e11cfce96ab99632f1921
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403928
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1282777}
    
    Bug: 40274370
    Change-Id: Ibb80e45d6a58e7cd3b3d283719eb9bea0f71f967
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427371
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1282874}

--- chrome/browser/ui/views/webauthn/mac_authentication_view.h
+++ chrome/browser/ui/views/webauthn/mac_authentication_view.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/timer/timer.h"
 #include "crypto/scoped_lacontext.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
@@ -44,12 +43,10 @@ class API_AVAILABLE(macos(12)) MacAuthenticationView : public views::View {
   struct ObjCStorage;
 
   void OnAuthenticationComplete(bool success);
-  void OnTouchIDAnimationComplete(bool success);
 
   Callback callback_;
   std::unique_ptr<ObjCStorage> storage_;
   bool evaluation_requested_ = false;
-  base::OneShotTimer touch_id_animation_timer_;
 
   base::WeakPtrFactory<MacAuthenticationView> weak_factory_{this};
 };
