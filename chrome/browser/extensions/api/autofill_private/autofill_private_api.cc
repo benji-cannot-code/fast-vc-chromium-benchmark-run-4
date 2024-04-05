c1e190b7498bb55b50f57bed432ce97cd471815d
Florian Leimgruber <fleimgruber@google.com>
2024-04-05 11:25:52 +0000

    Move payments mandatory re-auth logic to PayDM
    Moves the following functions without changes from PDM to PayDM:
    - SetPaymentMethodsMandatoryReauthEnabled();
    - IsPaymentMethodsMandatoryReauthEnabled()
    - ShouldShowPaymentMethodsMandatoryReauthPromo()
    - IncrementPaymentMethodsMandatoryReauthPromoShownCounter()
    
    All callers, except for a few ones from ios/ are rewritten. The latter
    two functions are not called from ios/ and thus even removed from the
    PDM.
    
    Unit tests and the overrides in the TestPDM are moved as well.
    
    Bug: b/322170538
    Change-Id: I5d6e856fa8a35d49f952f447e68467d5c1b16d61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424658
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283060}

--- chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
+++ chrome/browser/extensions/api/autofill_private/autofill_private_api.cc
@@ -849,7 +849,8 @@ AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction::Run() {
   LogMandatoryReauthOptInOrOutUpdateEvent(
       MandatoryReauthOptInOrOutSource::kSettingsPage,
       /*opt_in=*/
-      !personal_data_manager->IsPaymentMethodsMandatoryReauthEnabled(),
+      !personal_data_manager->payments_data_manager()
+           .IsPaymentMethodsMandatoryReauthEnabled(),
       MandatoryReauthAuthenticationFlowEvent::kFlowStarted);
   client->GetOrCreatePaymentsMandatoryReauthManager()->AuthenticateWithMessage(
       l10n_util::GetStringUTF16(IDS_PAYMENTS_AUTOFILL_MANDATORY_REAUTH_PROMPT),
@@ -884,8 +885,8 @@ void AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction::
   // mandatory reauth feature. If the mandatory reauth toggle on the settings is
   // currently enabled, then the `opt_in` bool will be false because the user is
   // opting-out, otherwise the `opt_in` bool will be true.
-  const bool opt_in =
-      !personal_data_manager->IsPaymentMethodsMandatoryReauthEnabled();
+  const bool opt_in = !personal_data_manager->payments_data_manager()
+                           .IsPaymentMethodsMandatoryReauthEnabled();
   LogMandatoryReauthOptInOrOutUpdateEvent(
       MandatoryReauthOptInOrOutSource::kSettingsPage, opt_in,
       reauth_succeeded ? MandatoryReauthAuthenticationFlowEvent::kFlowSucceeded
@@ -893,7 +894,8 @@ void AutofillPrivateAuthenticateUserAndFlipMandatoryAuthToggleFunction::
   if (reauth_succeeded) {
     base::RecordAction(base::UserMetricsAction(
         "PaymentsUserAuthSuccessfulForMandatoryAuthToggle"));
-    personal_data_manager->SetPaymentMethodsMandatoryReauthEnabled(opt_in);
+    personal_data_manager->payments_data_manager()
+        .SetPaymentMethodsMandatoryReauthEnabled(opt_in);
   }
 #endif
 }
@@ -913,7 +915,8 @@ ExtensionFunction::ResponseAction AutofillPrivateGetLocalCardFunction::Run() {
   if (!personal_data_manager || !personal_data_manager->IsDataLoaded()) {
     return RespondNow(Error(kErrorDataUnavailable));
   }
-  if (personal_data_manager->IsPaymentMethodsMandatoryReauthEnabled()) {
+  if (personal_data_manager->payments_data_manager()
+          .IsPaymentMethodsMandatoryReauthEnabled()) {
     base::RecordAction(base::UserMetricsAction(
         "PaymentsUserAuthTriggeredToShowEditLocalCardDialog"));
     LogMandatoryReauthSettingsPageEditCardEvent(
