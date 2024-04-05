af8c55245d596b5c90cc06b26145f8c375a3c856
David Padlipsky <dpad@google.com>
2024-04-05 21:48:51 +0000

    evdev: Identify fn key via hid report
    Fixed: b/332607741
    Change-Id: I779275afd83a6fc1f8c02132d9e2dde45def6ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384018
    Reviewed-by: Sean O'Brien <seobrien@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283411}

--- ui/events/devices/keyboard_device.cc
+++ ui/events/devices/keyboard_device.cc
@@ -13,8 +13,11 @@ KeyboardDevice::KeyboardDevice() = default;
 KeyboardDevice::KeyboardDevice(int id,
                                InputDeviceType type,
                                const std::string& name,
-                               bool has_assistant_key)
-    : InputDevice(id, type, name), has_assistant_key(has_assistant_key) {}
+                               bool has_assistant_key,
+                               bool has_function_key)
+    : InputDevice(id, type, name),
+      has_assistant_key(has_assistant_key),
+      has_function_key(has_function_key) {}
 
 KeyboardDevice::KeyboardDevice(int id,
                                InputDeviceType type,
@@ -24,12 +27,17 @@ KeyboardDevice::KeyboardDevice(int id,
                                uint16_t vendor,
                                uint16_t product,
                                uint16_t version,
-                               bool has_assistant_key)
+                               bool has_assistant_key,
+                               bool has_function_key)
     : InputDevice(id, type, name, phys, sys_path, vendor, product, version),
       has_assistant_key(has_assistant_key) {}
 
-KeyboardDevice::KeyboardDevice(InputDevice input_device, bool has_assistant_key)
-    : InputDevice(input_device), has_assistant_key(has_assistant_key) {}
+KeyboardDevice::KeyboardDevice(InputDevice input_device,
+                               bool has_assistant_key,
+                               bool has_function_key)
+    : InputDevice(input_device),
+      has_assistant_key(has_assistant_key),
+      has_function_key(has_function_key) {}
 
 KeyboardDevice::KeyboardDevice(const KeyboardDevice& other) = default;
 
--- ui/events/ozone/evdev/event_converter_evdev.cc
+++ ui/events/ozone/evdev/event_converter_evdev.cc
@@ -164,6 +164,10 @@ bool EventConverterEvdev::HasAssistantKey() const {
   return false;
 }
 
+bool EventConverterEvdev::HasFunctionKey() const {
+  return false;
+}
+
 bool EventConverterEvdev::HasCapsLockLed() const {
   return false;
 }
