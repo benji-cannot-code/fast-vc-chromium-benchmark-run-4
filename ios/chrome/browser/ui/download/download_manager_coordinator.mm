1cdcfe58271d5170f913bf210762dbfcd039580d
Quentin Pubert <qpubert@google.com>
2024-04-05 13:52:35 +0000

    [iOS] Add IOSDownloadOpenInDriveApp user action
    This CL adds a `IOSDownloadOpenInDriveApp` user action triggered when
    the user taps "OPEN" in the download manager UI after a download to
    Drive successfully completed.
    
    Bug: 333039131
    Change-Id: I33c914c1fb5acab7d6a661180f557340be667bde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424797
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283103}

--- ios/chrome/browser/ui/download/download_manager_coordinator.mm
+++ ios/chrome/browser/ui/download/download_manager_coordinator.mm
@@ -371,6 +371,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)downloadManagerViewControllerDidOpenInDriveApp:
     (UIViewController*)controller {
   CHECK(base::FeatureList::IsEnabled(kIOSSaveToDrive));
+  base::RecordAction(base::UserMetricsAction("IOSDownloadOpenInDriveApp"));
   UploadTask* uploadTask = _mediator.GetUploadTask();
   CHECK(uploadTask);
   std::optional<GURL> openFileInDriveURL =
