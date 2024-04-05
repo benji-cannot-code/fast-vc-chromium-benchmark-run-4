6c741ab69116009c539550a1440767fc2366fe20
Adem Derinel <derinel@google.com>
2024-04-05 13:42:43 +0000

    WebAuthn: Replace "Create passkey in GPM" bubble with a dialog
    This CL removes the bubble and introduces the dialog as per the mocks.
    The row with the username and the passkey icon will be added later.
    
    Bug: 40274370
    Change-Id: I4d1e6ad9484e145a3ec746757b343a71757fd6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428095
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283099}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -406,7 +406,6 @@ StepUIType step_ui_type(AuthenticatorRequestDialogModel::Step step) {
     case AuthenticatorRequestDialogModel::Step::kGPMReauthAccount:
       return StepUIType::WINDOW;
 
-    case AuthenticatorRequestDialogModel::Step::kGPMCreatePasskey:
     case AuthenticatorRequestDialogModel::Step::kGPMTouchID:
     case AuthenticatorRequestDialogModel::Step::kGPMOnboarding:
     case AuthenticatorRequestDialogModel::Step::kGPMPasskeySaved:
