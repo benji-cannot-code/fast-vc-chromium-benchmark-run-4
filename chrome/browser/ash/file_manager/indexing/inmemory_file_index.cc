c7e6b025a42e91a8a2e8e5858be4094f65a3ff3c
Bo Majewski <majewski@chromium.org>
2024-04-05 05:50:39 +0000

    [Files]: Adds table for storing file info
    Adds a new table that stores file info. File info is stored by the URL
    ID. Adds APIs for getting, putting, and deleting file info.
    
    Bug: b:327535200
    Change-Id: I9dc6c6e9c15d59eb6db8307f775334527ca077da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5409883
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282976}

--- chrome/browser/ash/file_manager/indexing/inmemory_file_index.cc
+++ chrome/browser/ash/file_manager/indexing/inmemory_file_index.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cstdint>
-#include "file_index.h"
 
 namespace file_manager {
 
@@ -48,7 +47,8 @@ OpResults InmemoryFileIndex::AugmentFile(const std::vector<Term>& terms,
     return OpResults::kSuccess;
   }
 
-  int64_t url_id = GetOrCreateUrlId(info);
+  int64_t url_id = GetOrCreateUrlId(info.file_url);
+  DCHECK(PutFileInfo(info) == url_id);
   DCHECK(url_id >= 0);
 
   auto term_ids_by_field = ConvertToTermIds(terms);
@@ -80,8 +80,9 @@ OpResults InmemoryFileIndex::SetFileTerms(const std::vector<Term>& terms,
 
   // Arrange terms by field and remove duplicates and convert to internal IDs.
   auto term_ids_by_field = ConvertToTermIds(terms);
-  int64_t url_id = GetOrCreateUrlId(info);
+  int64_t url_id = GetOrCreateUrlId(info.file_url);
   DCHECK(url_id >= 0);
+  DCHECK(PutFileInfo(info) == url_id);
 
   // If the given url_id already had some terms associated with it, remove terms
   // not specified in terms vector. Say, if url_id had terms {t1, t3, t8}
@@ -225,15 +226,23 @@ int64_t InmemoryFileIndex::GetUrlId(const GURL& url) {
   return (it != url_to_id_.end()) ? it->second : -1;
 }
 
-int64_t InmemoryFileIndex::GetOrCreateUrlId(const FileInfo& info) {
-  int64_t url_id = GetUrlId(info.file_url);
+int64_t InmemoryFileIndex::GetOrCreateUrlId(const GURL& url) {
+  int64_t url_id = GetUrlId(url);
   if (url_id >= 0) {
     return url_id;
   }
   int64_t this_url_id = url_id_++;
-  url_to_id_.emplace(std::make_pair(info.file_url, this_url_id));
-  url_id_to_file_info_.emplace(std::make_pair(this_url_id, info));
+  url_to_id_.emplace(std::make_pair(url, this_url_id));
   return this_url_id;
+}
+
+int64_t InmemoryFileIndex::PutFileInfo(const FileInfo& file_info) {
+  int64_t url_id = GetUrlId(file_info.file_url);
+  if (url_id == -1) {
+    return -1;
+  }
+  url_id_to_file_info_.emplace(std::make_pair(url_id, file_info));
+  return url_id;
 }
 
 // Searches the index for file info matching the specified query.
