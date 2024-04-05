35aab9e8d344cae846bb0e0181ca6e7c3610d4df
Liam Brady <lbrady@google.com>
2024-04-05 15:44:46 +0000

    Reland "Cross-origin support for window.fence.reportEvent()."
    This is a reland of commit 280fe80c08b919b9cb7ba8228b1b0323ce5bceea.
    
    There is a known flakiness issue with tests that involve loading
    cross-origin subframes with remote context executors. The way we
    historically have fixed that is by splitting up each promise_test into a
    separate file. This CL attempts to fix that flakiness by doing just
    that.
    
    Original change's description:
    > Cross-origin support for window.fence.reportEvent().
    >
    > Third-party ad serving (3PAS) lets advertisers manage ads across
    > different bidding platforms. However, the event-level reporting API
    > (reportEvent()) currently prevents direct reporting from documents
    > that are cross-origin to the root ad frame's fenced frame config's
    > mapped URL, limiting this functionality for the 3PAS use case. This CL
    > extends the reportEvent() API to support this use case.
    >
    > To allow cross-origin reporting while still preserving privacy, both
    > the ad frame and the 3PAS subframe must explicitly opt in. The ad
    > frame opts in using a new `Allow-Cross-Origin-Event-Reporting`
    > response header, while the 3PAS subframe opts in by calling
    > reportEvent() with `crossOriginExposed=true`. If both opt-ins are
    > present, then the reportEvent() call from the 3PAS document will
    > succeed.
    >
    > This CL adds histograms to track how many beacons are sent from
    > documents that are same-origin and cross-origin to the fenced frame
    > config's mapped url.
    >
    > This CL also expands the fenced frame WPT system to support
    > content-initiated navigations of fenced frame contexts to cross-origin
    > URLs.
    >
    > Change-Id: I4e895bec4cb77604e2017cfc67b496d59863a30b
    > Bug: 331844131
    
    Bug: 331844131
    Change-Id: Iacf17786ba5471faff15252c87e1924698ab4583
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425335
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Reviewed-by: Tarun Bansal <tbansal@chromium.org>
    Commit-Queue: Liam Brady <lbrady@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283165}

--- third_party/blink/public/common/fenced_frame/fenced_frame_utils.h
+++ third_party/blink/public/common/fenced_frame/fenced_frame_utils.h
@@ -52,6 +52,12 @@ inline constexpr char kAutomaticBeaconEventTypeHistogram[] =
 inline constexpr char kFencedFrameBeaconReportingHttpResultUMA[] =
     "Blink.FencedFrame.BeaconReportingHttpResult";
 
+inline constexpr char kFencedFrameBeaconReportingCountUMA[] =
+    "Navigation.FencedFrameBeaconReportingCountSameOrigin";
+
+inline constexpr char kFencedFrameBeaconReportingCountCrossOriginUMA[] =
+    "Navigation.FencedFrameBeaconReportingCountCrossOrigin";
+
 // Corresponds to the "FencedFrameCreationOutcome" histogram enumeration type in
 // tools/metrics/histograms/enums.xml.
 //
