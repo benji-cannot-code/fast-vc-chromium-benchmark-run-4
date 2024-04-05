b006849238c94f1b8b2f2d616b3981342ed658b3
David Tseng <dtseng@google.com>
2024-04-05 16:57:38 +0000

    [a11y]: eliminate unneeded copies in AXTreeCombiner flow
    This change adds the minimal copy to move logic, performance histograms,
    and experiment field trial.
    
    Process:
    - s/= default/= delete for selected a11y structs
    - audit compile failures
    
    The patterns seen in this change, and measured are:
    1. passing of
    const T&
    and eventually doing
    items.push_back(std::move(item))
    where `item` is a const T& and
    `items` is a vector of Ts.
    This triggers a copy even though a `std::move` is called.
    Fixed by a const cast which takes the underlying resources of `item`.
    
    2. for (auto foo : bar) (copy)
    Fixed by doing
    for (auto& foo : bar) (by ref)
    
    Bug: 332302437
    Change-Id: Ie5cda01c2e41398f12b10fa49f19042031f3b8bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5410696
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283204}

--- content/browser/accessibility/accessibility_tree_snapshot_combiner.cc
+++ content/browser/accessibility/accessibility_tree_snapshot_combiner.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "content/browser/accessibility/accessibility_tree_snapshot_combiner.h"
+
 #include "content/browser/renderer_host/render_frame_host_impl.h"
+#include "ui/accessibility/accessibility_features.h"
 
 namespace content {
 
@@ -36,8 +38,19 @@ void AccessibilityTreeSnapshotCombiner::ReceiveSnapshotFromRenderFrameHost(
 // ReceiveSnapshotFromRenderFrameHost when there are no more references to this
 // object.
 AccessibilityTreeSnapshotCombiner::~AccessibilityTreeSnapshotCombiner() {
+  SCOPED_UMA_HISTOGRAM_TIMER_MICROS(
+      "Accessibility.Performance.AccessibilityTreeSnapshotCombiner::"
+      "~AccessibilityTreeSnapshotCombiner");
   combiner_.Combine();
-  std::move(callback_).Run(combiner_.combined());
+  if (features::IsUseMoveNotCopyInAXTreeCombinerEnabled()) {
+    // This ensures a move of `combiner_.combined()`. It should be safe to steal
+    // `combiner_`'s resources since we're being destroyed.
+    std::move(callback_).Run(
+        const_cast<ui::AXTreeUpdate&>(combiner_.combined()));
+  } else {
+    // This is a copy since combiner_.combined() is a const AXTreeUpdate&.
+    std::move(callback_).Run(combiner_.combined());
+  }
 }
 
 }  // namespace content
