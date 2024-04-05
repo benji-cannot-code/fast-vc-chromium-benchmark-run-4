de318627f493480ab7cf40f47950d00b039f69c0
Phillis Tang <phillis@chromium.org>
2024-04-05 01:06:58 +0000

    webnn: coreml: better type check with base::span<DataType> instead of bytes
    Instead of passing generic byte span to `AddConstantImmediateValue`,
    pass in actual base::span<const DataType> and infer the MIL datatype
    from it to avoid bugs with mismatching types.
    
    Change-Id: I290645327e7ee208e6e712714e156b729aa93d89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422149
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282882}

--- services/webnn/coreml/graph_builder.h
+++ services/webnn/coreml/graph_builder.h
@@ -20,6 +20,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn::coreml {
 
+struct Float16 {
+  uint16_t data;
+};
+
+namespace internal {
+// Supported tensor types for immediate values. The list can be expanded as
+// needed.
+template <typename T, typename... U>
+concept IsAnyOf = (std::same_as<T, U> || ...);
+template <typename T>
+concept IsSupportedTensorType = IsAnyOf<T,
+                                        Float16,
+                                        float,
+                                        int8_t,
+                                        uint8_t,
+                                        int32_t,
+                                        uint32_t,
+                                        int64_t,
+                                        uint64_t,
+                                        char>;
+}  // namespace internal
+
 inline constexpr char kPlaceholderInputName[] = "placeholder";
 
 // Get name identifiers used in CoreML model files for input/output operands.
@@ -100,12 +122,14 @@ class GraphBuilder {
   [[nodiscard]] base::expected<void, mojom::ErrorPtr> AddOperationForConv2d(
       const mojom::Conv2d& operation,
       CoreML::Specification::MILSpec::Block& block);
+
   // Add constants as immediate values in the model file.
+  template <typename DataType>
+    requires internal::IsSupportedTensorType<DataType>
   void AddConstantImmediateValue(CoreML::Specification::MILSpec::Block& block,
                                  std::string_view name,
-                                 CoreML::Specification::MILSpec::DataType,
                                  base::span<const uint32_t> dimensions,
-                                 base::span<const uint8_t> value);
+                                 base::span<const DataType> value);
   void AddConstantImmediateValue(uint32_t constant_id,
                                  CoreML::Specification::MILSpec::Block& block);
   // Add constants to weight file.
