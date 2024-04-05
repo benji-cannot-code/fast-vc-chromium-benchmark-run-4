5363ca3945d940a5d9e32173044118557773c70c
Ewann Pelle <ewannpv@chromium.org>
2024-04-05 15:55:38 +0000

    [iOS][TG] Rename methods
    This CL renames methods to make their use of WebState indexes explicit.
    
    Change-Id: I83e8222e068880e0b16b9227fc1c77700028a974
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428415
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283170}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
@@ -436,7 +436,7 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
         } else {
           [self moveItem:[GridItemIdentifier
                              tabIdentifier:moveChange.moved_web_state()]
-              beforeIndex:moveChange.moved_to_index() + 1];
+              beforeWebStateIndex:moveChange.moved_to_index() + 1];
         }
       }
       break;
@@ -472,7 +472,7 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
       } else {
         web::WebState* insertedWebState = insertChange.inserted_web_state();
         [self insertItem:[GridItemIdentifier tabIdentifier:insertedWebState]
-             beforeIndex:insertChange.index() + 1];
+            beforeWebStateIndex:insertChange.index() + 1];
 
         _scopedWebStateObservation->AddObservation(insertedWebState);
       }
@@ -488,9 +488,9 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
                              withWebStateList:webStateList];
 
       [self insertItem:groupItemIdentifier
-           beforeIndex:groupItemIdentifier.tabGroupItem.tabGroup->range()
-                           .range_end() +
-                       1];
+          beforeWebStateIndex:groupItemIdentifier.tabGroupItem.tabGroup->range()
+                                  .range_end() +
+                              1];
       break;
     }
     case WebStateListChange::Type::kGroupVisualDataUpdate: {
@@ -510,7 +510,7 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
       [self moveItem:[GridItemIdentifier
                           groupIdentifier:groupMoveChange.moved_group()
                          withWebStateList:webStateList]
-          beforeIndex:groupMoveChange.moved_to_range().range_end() + 1];
+          beforeWebStateIndex:groupMoveChange.moved_to_range().range_end() + 1];
       break;
     }
     case WebStateListChange::Type::kGroupDelete: {
@@ -1240,7 +1240,7 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
     _scopedWebStateObservation->RemoveObservation(webState);
   } else {
     [self insertItem:[GridItemIdentifier tabIdentifier:webState]
-         beforeIndex:index + 1];
+        beforeWebStateIndex:index + 1];
     _scopedWebStateObservation->AddObservation(webState);
   }
 }
@@ -1382,19 +1382,20 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
   }
 }
 
-// Inserts `item` before the item at `nextItemIndex`.
-- (void)insertItem:(GridItemIdentifier*)item beforeIndex:(int)nextItemIndex {
+// Inserts `item` before the WebState at `nextWebStateIndex`.
+- (void)insertItem:(GridItemIdentifier*)item
+    beforeWebStateIndex:(int)nextWebStateIndex {
   GridItemIdentifier* nextItemIdentifier;
-  if (self.webStateList->ContainsIndex(nextItemIndex)) {
+  if (self.webStateList->ContainsIndex(nextWebStateIndex)) {
     const TabGroup* group =
-        self.webStateList->GetGroupOfWebStateAt(nextItemIndex);
+        self.webStateList->GetGroupOfWebStateAt(nextWebStateIndex);
     if (group) {
       nextItemIdentifier =
           [GridItemIdentifier groupIdentifier:group
                              withWebStateList:self.webStateList];
     } else {
       nextItemIdentifier = [GridItemIdentifier
-          tabIdentifier:self.webStateList->GetWebStateAt(nextItemIndex)];
+          tabIdentifier:self.webStateList->GetWebStateAt(nextWebStateIndex)];
     }
   }
   [self.consumer insertItem:item
@@ -1411,18 +1412,19 @@ GridItemIdentifier* GetActiveNonPinnedIdentifier(WebStateList* web_state_list) {
          withReplacementItem:groupIdentifier];
 }
 
-// Moves `item` before the item at `nextItemIndex`.
-- (void)moveItem:(GridItemIdentifier*)item beforeIndex:(int)nextItemIndex {
+// Moves `item` before the WebState at `nextWebStateIndex`.
+- (void)moveItem:(GridItemIdentifier*)item
+    beforeWebStateIndex:(int)nextWebStateIndex {
   GridItemIdentifier* nextItem = nil;
-  if (self.webStateList->ContainsIndex(nextItemIndex)) {
+  if (self.webStateList->ContainsIndex(nextWebStateIndex)) {
     const TabGroup* group =
-        self.webStateList->GetGroupOfWebStateAt(nextItemIndex);
+        self.webStateList->GetGroupOfWebStateAt(nextWebStateIndex);
     if (group) {
       nextItem = [GridItemIdentifier groupIdentifier:group
                                     withWebStateList:self.webStateList];
     } else {
       nextItem = [GridItemIdentifier
-          tabIdentifier:self.webStateList->GetWebStateAt(nextItemIndex)];
+          tabIdentifier:self.webStateList->GetWebStateAt(nextWebStateIndex)];
     }
   }
 
