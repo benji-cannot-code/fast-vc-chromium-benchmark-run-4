0bae5efb8e8b2c10e28a008c03d19670a3617d9a
Ahmed Elwasefi <a.m.elwasefi@gmail.com>
2024-04-05 23:28:46 +0000

    [a11y] Remove need_to_send_location_changes and just depend on changed_bounds_ids_
    The need_to_send_location_changes variable is useless. Later CLs will also investigate the marking of objects dirty when locations change as it could be meaningless.
    
    Change-Id: I3470e8fcff28b3be44d45103e973fbfd03054393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424805
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Ahmed Elwasefi (Ahmad45123) <a.m.elwasefi@gmail.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1283457}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -3434,10 +3434,6 @@ bool AXObjectCacheImpl::SerializeUpdatesAndEvents() {
 
   bool had_end_of_test_event = false;
 
-  // If there's a layout complete or a scroll changed message, we need to send
-  // location changes.
-  bool need_to_send_location_changes = false;
-
   // Keep track of load complete messages. When a load completes, it's a good
   // time to inject a stylesheet for image annotation debugging.
   bool had_load_complete_messages = false;
@@ -3449,8 +3445,7 @@ bool AXObjectCacheImpl::SerializeUpdatesAndEvents() {
   // either when the queue is empty, or the number of remaining objects to
   // serialize has been reached.
   GetUpdatesAndEventsForSerialization(updates, events, had_end_of_test_event,
-                                      had_load_complete_messages,
-                                      need_to_send_location_changes);
+                                      had_load_complete_messages);
 
   /* Clear the pending updates and events as they're about to be serialized */
   dirty_objects_.clear();
@@ -3487,7 +3482,7 @@ bool AXObjectCacheImpl::SerializeUpdatesAndEvents() {
     // really occur and thus the function will return false.
     // Cancel serialization to avoid stalling pipeline.
     OnSerializationCancelled();
-  } else if (need_to_send_location_changes) {
+  } else if (!changed_bounds_ids_.empty()) {
     SerializeLocationChanges();
   }
 
@@ -5362,8 +5357,7 @@ Element* AXObjectCacheImpl::GetActiveAriaModalDialog() const {
 
 void AXObjectCacheImpl::SerializeLocationChanges() {
   CHECK(GetDocument().IsActive());
-  if (changed_bounds_ids_.empty())
-    return;
+  DCHECK(!changed_bounds_ids_.empty());
 
   TRACE_EVENT0("accessibility", "SerializeLocationChanges");
   SCOPED_UMA_HISTOGRAM_TIMER_MICROS(
@@ -5468,8 +5462,7 @@ void AXObjectCacheImpl::GetUpdatesAndEventsForSerialization(
     std::vector<ui::AXTreeUpdate>& updates,
     std::vector<ui::AXEvent>& events,
     bool& had_end_of_test_event,
-    bool& had_load_complete_messages,
-    bool& need_to_send_location_changes) {
+    bool& had_load_complete_messages) {
   TRACE_EVENT0("accessibility", "GetUpdatesAndEventsForSerialization");
   SCOPED_UMA_HISTOGRAM_TIMER_MICROS(
       "Accessibility.Performance.GetUpdatesAndEventsForSerialization");
@@ -5569,11 +5562,6 @@ void AXObjectCacheImpl::GetUpdatesAndEventsForSerialization(
         "Accessibility.Performance.AXObjectCacheImpl.Incremental",
         base::saturated_cast<int>(node_data_size.ByteSize()), 1, kSizeGb,
         kBucketCount);
-  }
-
-  if (need_to_send_location_changes_) {
-    need_to_send_location_changes_ = false;  // Class member is now clear.
-    need_to_send_location_changes = true;    // Reference parameter.
   }
 
   // Loop over each event and generate an updated event message.
@@ -5944,7 +5932,6 @@ void AXObjectCacheImpl::HandleLayoutComplete(Document* document) {
     return;
   }
 
-  need_to_send_location_changes_ = true;
   DeferTreeUpdate(TreeUpdateReason::kMarkDirtyFromHandleLayout, document);
 }
 
@@ -5980,7 +5967,6 @@ void AXObjectCacheImpl::HandleScrollPositionChanged(
   InvalidateBoundingBoxForFixedOrStickyPosition();
   Node* node = GetClosestNodeForLayoutObject(layout_object);
   if (node) {
-    need_to_send_location_changes_ = true;
     DeferTreeUpdate(TreeUpdateReason::kMarkDirtyFromHandleScroll, node);
   }
 }
