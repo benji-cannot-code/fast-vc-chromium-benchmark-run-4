0cb49904829bb75da43a6c85f3d986a7f88e32d7
Yann Dago <ydago@chromium.org>
2024-04-05 16:23:58 +0000

    Use new policy to enable enterprise badging
    The new EnterpriseBadingTemporarySetting policy is use to control who
    the visibility of the enterprise profile badging.
    
    Bug: 331619808
    Change-Id: Ifd3769ae565118efda2302bcf1d7a19af5e5442d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402175
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283185}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_delegate.cc
@@ -99,7 +99,7 @@ bool IsErrorSyncPaused(Profile* profile) {
 // - true for Work.
 // - false for School.
 bool IsManagementWork(Profile* profile) {
-  CHECK(base::FeatureList::IsEnabled(features::kEnterpriseProfileBadging));
+  CHECK(chrome::enterprise_util::CanShowEnterpriseBadging(profile));
   auto* identity_manager = IdentityManagerFactory::GetForProfile(profile);
   auto management_environment =
       chrome::enterprise_util::GetManagementEnvironment(
@@ -535,14 +535,16 @@ class ManagementStateProvider : public StateProvider,
       const AvatarToolbarButton& avatar_toolbar_button)
       : StateProvider(state_observer),
         profile_(profile),
-        avatar_toolbar_button_(avatar_toolbar_button),
-        user_accepted_account_management_(
-            chrome::enterprise_util::UserAcceptedAccountManagement(
-                &profile_.get())) {
+        avatar_toolbar_button_(avatar_toolbar_button) {
     BrowserList::AddObserver(this);
     profile_observation_.Observe(&GetProfileAttributesStorage());
 
     pref_change_registrar_.Init(profile_->GetPrefs());
+    pref_change_registrar_.Add(
+        prefs::kEnterpriseBadgingTemporarySetting,
+        base::BindRepeating(&ManagementStateProvider::RequestUpdate,
+                            weak_ptr_factory_.GetWeakPtr(),
+                            ElementToUpdate::kText));
     pref_change_registrar_.Add(
         prefs::kCustomProfileLabel,
         base::BindRepeating(&ManagementStateProvider::RequestUpdate,
@@ -559,7 +561,7 @@ class ManagementStateProvider : public StateProvider,
 
   // StateProvider:
   bool IsActive() const override {
-    return user_accepted_account_management_ &&
+    return chrome::enterprise_util::CanShowEnterpriseBadging(&profile_.get()) &&
            (!IsTransient() || temporarily_showing_);
   }
 
@@ -573,9 +575,7 @@ class ManagementStateProvider : public StateProvider,
   // ProfileAttributesStorage::Observer:
   void OnProfileUserManagementAcceptanceChanged(
       const base::FilePath& profile_path) override {
-    user_accepted_account_management_ =
-        chrome::enterprise_util::UserAcceptedAccountManagement(&profile_.get());
-    if (!user_accepted_account_management_) {
+    if (!chrome::enterprise_util::CanShowEnterpriseBadging(&profile_.get())) {
       RequestUpdate(ElementToUpdate::kAll);
       return;
     }
