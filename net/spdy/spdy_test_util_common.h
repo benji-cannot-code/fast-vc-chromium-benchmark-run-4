36dc8bdddc436ec27823411f82d60e4f8a161aff
Matt Menke <mmenke@chromium.org>
2024-04-05 00:22:39 +0000

    Pass in post-host-mapping URLs to HttpServeProperties::MaybeForceHTTP11
    SpdySession is passing post-host-mapping SchemeHostPorts to the
    MaybeForceHTTP11() family of HttpServeProperties methods, as is
    ConnectJobFactory, while HttpNetworkTransaction and
    HttpStreamFactory::Job are passing in pre-host-mapping values to them.
    
    This inconsistency can cause breakages and retry-loops in rare cases,
    like NTLM auth over a host-mapped origin that supports HTTP/2.
    
    This CL makes ConnectJobFactory and HttpStreamFactory::Job use host
    mapped origins, and adds a test case for one of the cases that would
    have failed before.
    
    Bug: 332724851
    Change-Id: Ia7255ba734ddd9d1422ceee2f4a6c66180e3ad62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426392
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1282859}

--- net/spdy/spdy_test_util_common.h
+++ net/spdy/spdy_test_util_common.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "crypto/ec_private_key.h"
 #include "net/base/completion_once_callback.h"
+#include "net/base/host_mapping_rules.h"
 #include "net/base/proxy_server.h"
 #include "net/base/request_priority.h"
 #include "net/base/test_completion_callback.h"
@@ -196,6 +197,7 @@ struct SpdySessionDependencies {
   std::unique_ptr<ReportingService> reporting_service;
   std::unique_ptr<NetworkErrorLoggingService> network_error_logging_service;
 #endif
+  HostMappingRules host_mapping_rules;
   bool enable_ip_pooling = true;
   bool enable_ping = false;
   bool enable_user_alternate_protocol_ports = false;
