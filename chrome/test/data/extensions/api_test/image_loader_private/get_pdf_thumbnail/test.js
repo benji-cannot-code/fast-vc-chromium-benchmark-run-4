3923c95fefe45cf749103e0d8dbe3c9e12fae388
Lei Zhang <thestig@chromium.org>
2024-04-06 00:07:55 +0000

    Add API test for chrome.imageLoaderPrivate.getPdfThumbnail()
    Currently, this API only has unit tests. Add an end-to-end
    FileManagerPrivateApiTest to make sure it works in a full browser
    environment. The C++ test runs 3 JS test cases to make sure the good
    path and some easy to test bad paths work as expected. This ensures
    code refactors in the near future that affect this API will not break
    it.
    
    To make the test work, change the imageLoaderPrivate API to not have
    an allowlist on the "trunk" channel.
    
    Bug: 332743737
    Change-Id: Ia1dc2bffae1e3ca3d8bb36ca73432485bb4e5192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5431014
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283472}

---
+++ chrome/test/data/extensions/api_test/image_loader_private/get_pdf_thumbnail/test.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function testGetPdfThumbnail() {
+  chrome.imageLoaderPrivate.getPdfThumbnail(
+      'filesystem:chrome-extension://dflhahjnheihhcfhnhcflfdgacjoocip/' +
+          'external/Downloads-user/test.pdf',
+      100, 100, (thumbnailDataUrl) => {
+        chrome.test.assertTrue(
+            thumbnailDataUrl.startsWith('data:image/png;base64,'));
+        chrome.test.succeed();
+      });
+}
+
+function testNonExistentFile() {
+  chrome.imageLoaderPrivate.getPdfThumbnail(
+      'filesystem:chrome-extension://dflhahjnheihhcfhnhcflfdgacjoocip/' +
+          'external/Downloads-user/no_such.pdf',
+      100, 100, () => {
+        chrome.test.assertLastError('Failed to read PDF file');
+        chrome.test.succeed();
+      });
+}
+
+function testWrongUrlScheme() {
+  chrome.imageLoaderPrivate.getPdfThumbnail(
+      'https://localhost/test.pdf', 100, 100, () => {
+        chrome.test.assertLastError('Expected a native local URL');
+        chrome.test.succeed();
+      });
+}
+
+chrome.test.runTests(
+    [testGetPdfThumbnail, testNonExistentFile, testWrongUrlScheme]);
