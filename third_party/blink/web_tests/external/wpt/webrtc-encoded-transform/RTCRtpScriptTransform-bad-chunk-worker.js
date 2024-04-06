9d849dadf7a8b2a35bd3ba6aeace4e04ac5a2b45
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-04-06 04:23:49 +0000

    Import wpt@f8fb78a2c237eeb8064d2c94a2f3ac9842bd348b
    https://github.com/web-platform-tests/wpt/compare/043469746...f8fb78a2c
    
    Using wpt-import in Chromium 3f2d64993916ff4633bdb81ef66441d06d3aa087.
    With Chromium commits locally applied on WPT:
      5fdbc88b41 "Convert all layer WPT tests to synchronous [...]
      256090d9b2 "Reland "Cross-origin support for [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    drott@chromium.org, kojii@chromium.org:
      external/wpt/css/css-fonts
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I5646c067258a13003c827366507d55d286e93e09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5431313
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1283531}

---
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/RTCRtpScriptTransform-bad-chunk-worker.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+onrtctransform = async (event) => {
+  const { port } = event.transformer.options;
+  port.postMessage("started");
+
+  const reader = event.transformer.readable.getReader();
+  const writer = event.transformer.writable.getWriter();
+
+  const { done, value } = await reader.read();
+
+  writer.write(null).catch(err => port.postMessage([err.name, 'null']));
+  writer.write(value).catch(err => port.postMessage([err.name, value.constructor.name]));
+};
+self.postMessage('registered');
---
+++ third_party/blink/web_tests/external/wpt/webtransport/sendstream-bad-chunk.https.any.js
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,worker
+// META: script=/common/get-host-info.sub.js
+// META: script=resources/webtransport-test-helpers.sub.js
+// META: script=/common/utils.js
+
+promise_test(async t => {
+  // Establish a WebTransport session.
+  const wt = new WebTransport(webtransport_url('echo.py'));
+  t.add_cleanup(() => wt.close());
+  await wt.ready;
+
+  // Create a bidirectional stream
+  const {writable} = await wt.createBidirectionalStream();
+
+  const writer = writable.getWriter();
+  await promise_rejects_js(t, TypeError, writer.write("foo"));
+  await promise_rejects_js(t, TypeError, writer.write(new Uint8Array(0)));
+}, 'WebTransportSendStream should reject when non-buffer-source data is written');
