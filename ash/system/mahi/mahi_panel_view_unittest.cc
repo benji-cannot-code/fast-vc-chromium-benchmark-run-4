b9f7bad80903c05f85286c2645e513dd87b44c50
andrewxu <andrewxu@chromium.org>
2024-04-06 03:00:32 +0000

    [Mahi] Set the error status view image
    This CL sets the error status view's image. This CL adds a pixel
    test to protect this feature.
    
    Bug: b/319731862
    Change-Id: I6ffa345adcce87e55aef004be1295f033b330187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5409617
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283516}

--- ash/system/mahi/mahi_panel_view_unittest.cc
+++ ash/system/mahi/mahi_panel_view_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/system_textfield.h"
 #include "ash/system/mahi/mahi_constants.h"
 #include "ash/system/mahi/mahi_ui_controller.h"
+#include "ash/system/mahi/mahi_utils.h"
 #include "ash/system/mahi/test/mock_mahi_manager.h"
 #include "ash/test/ash_test_base.h"
 #include "base/strings/strcat.h"
@@ -838,8 +839,10 @@ TEST_F(MahiPanelViewTest, FailToGetAnswer) {
     EXPECT_FALSE(summary_outlines_section->GetVisible());
     EXPECT_TRUE(error_status_view->GetVisible());
 
-    // TODO(http://b/319731862): Check the label contents.
-    EXPECT_FALSE(error_status_label->GetText().empty());
+    // Check the contents of `error_status_label`.
+    EXPECT_EQ(
+        error_status_label->GetText(),
+        l10n_util::GetStringUTF16(mahi_utils::GetErrorStatusViewTextId(error)));
 
     CreatePanelWidget();
   }
@@ -894,8 +897,10 @@ TEST_F(MahiPanelViewTest, FailToGetOutlines) {
     EXPECT_FALSE(summary_outlines_section->GetVisible());
     EXPECT_TRUE(error_status_view->GetVisible());
 
-    // TODO(http://b/319731862): Check the label contents.
-    EXPECT_FALSE(error_status_label->GetText().empty());
+    // Check the contents of `error_status_label`.
+    EXPECT_EQ(
+        error_status_label->GetText(),
+        l10n_util::GetStringUTF16(mahi_utils::GetErrorStatusViewTextId(error)));
   }
 }
 
@@ -948,8 +953,10 @@ TEST_F(MahiPanelViewTest, FailToGetSummary) {
     EXPECT_FALSE(summary_outlines_section->GetVisible());
     EXPECT_TRUE(error_status_view->GetVisible());
 
-    // TODO(http://b/319731862): Check the label contents.
-    EXPECT_FALSE(error_status_label->GetText().empty());
+    // Check the contents of `error_status_label`.
+    EXPECT_EQ(
+        error_status_label->GetText(),
+        l10n_util::GetStringUTF16(mahi_utils::GetErrorStatusViewTextId(error)));
   }
 }
 
---
+++ ash/system/mahi/mahi_utils.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_MAHI_MAHI_UTILS_H_
+#define ASH_SYSTEM_MAHI_MAHI_UTILS_H_
+
+#include "ash/ash_export.h"
+
+namespace chromeos {
+enum class MahiResponseStatus;
+}  // namespace chromeos
+
+namespace ash::mahi_utils {
+
+// Returns the text ID of the `error` description on `MahiErrorStatusView`.
+// NOTE: This function should be called only if the `error` should be presented
+// on `MahiErrorStatusView`.
+ASH_EXPORT int GetErrorStatusViewTextId(chromeos::MahiResponseStatus error);
+
+}  // namespace ash::mahi_utils
+
+#endif  // ASH_SYSTEM_MAHI_MAHI_UTILS_H_
