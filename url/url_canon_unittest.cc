6e1d297e87c6135a8b6858dac4fc52b77ccb8ffc
Jan Keitel <jkeitel@google.com>
2024-04-07 13:51:54 +0000

    Use std::string_view in ParseFileSystemURL.
    Bug: 325408566
    Change-Id: Idf694657ef8be3a98ce297e6316cfcf0df7170df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5374208
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283621}

--- url/url_canon_unittest.cc
+++ url/url_canon_unittest.cc
@@ -2104,9 +2104,7 @@ TEST(URLCanonTest, ReplaceFileSystemURL) {
 
   for (const auto& replace_case : replace_cases) {
     const ReplaceCase& cur = replace_case;
-    int base_len = static_cast<int>(strlen(cur.base));
-    Parsed parsed;
-    ParseFileSystemURL(cur.base, base_len, &parsed);
+    Parsed parsed = ParseFileSystemURL(cur.base);
 
     Replacements<char> r;
     typedef Replacements<char> R;  // Clean up syntax.
@@ -2368,9 +2366,7 @@ TEST(URLCanonTest, CanonicalizeFileSystemURL) {
   };
 
   for (const auto& i : cases) {
-    int url_len = static_cast<int>(strlen(i.input));
-    Parsed parsed;
-    ParseFileSystemURL(i.input, url_len, &parsed);
+    Parsed parsed = ParseFileSystemURL(i.input);
 
     Parsed out_parsed;
     std::string out_str;
