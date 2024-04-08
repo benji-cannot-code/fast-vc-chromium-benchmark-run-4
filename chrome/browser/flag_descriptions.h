ae8df0bd84a75f316a7ad07212f51f1a2ecc168e
Aliaa Alborhamy <aliaaborhamy@google.com>
2024-04-08 15:39:30 +0000

    [iOS Promo] Feature flag with params for iOS Save Address promo.
    Bug: 331409520
    Change-Id: I337256c4bee5dea9e67d1de5e7923b6ea9968aa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398076
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283901}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2955,6 +2955,9 @@ extern const char kSmartCardWebApiDescription[];
 extern const char kSettingsEnableGetTheMostOutOfChromeName[];
 extern const char kSettingsEnableGetTheMostOutOfChromeDescription[];
 
+extern const char kIOSPromoAddressBubbleName[];
+extern const char kIOSPromoAddressBubbleDescription[];
+
 extern const char kIOSPromoBookmarkBubbleName[];
 extern const char kIOSPromoBookmarkBubbleDescription[];
 #endif
