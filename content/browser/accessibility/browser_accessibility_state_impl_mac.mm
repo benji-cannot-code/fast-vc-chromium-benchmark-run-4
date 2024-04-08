5e87b30ff027d2ca05484e9374378ec02c71d7c4
Jayson Adams <shrike@chromium.org>
2024-04-08 19:15:05 +0000

    Revert "[Mac A11y Sonoma] Activate VoiceOver KVO monitoring later in start-up"
    This reverts commit bbbd8ff3c57c31c65406517c0dfbd6d00413c613.
    
    Reason for revert: breaks KVO notifications for things like dark/light theme changes (see b/332743720).
    
    Original change's description:
    > [Mac A11y Sonoma] Activate VoiceOver KVO monitoring later in start-up
    >
    > The VoiceOver KVO monitoring setup code has a dependency on the
    > existence of the global accessibility state object. This cl ensures the
    > KVO code runs only after the global a11y object has been created.
    >
    > Bug: 330759711
    >
    > Test: out/debug/browser_tests --gtest_filter=ChromeBrowserAppMac*
    >       out/default/browser_tests \
    >         --gtest_filter=*PromotionalTabsEnabledPolicy*
    >
    >
    > Include-Ci-Only-Tests: true
    > Change-Id: Ieb1425c7bb8ab3c1a373a04d23100ad19ab0778d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5385095
    > Commit-Queue: Jayson Adams <shrike@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1280877}
    
    Bug: 330759711,332743720
    Change-Id: Id80c0ca875e0eaa6d3659674e962ae10d147bb6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436073
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284027}

--- content/browser/accessibility/browser_accessibility_state_impl_mac.mm
+++ content/browser/accessibility/browser_accessibility_state_impl_mac.mm
@@ -9,17 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
-#import "base/mac/mac_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/browser_thread.h"
-#include "content/public/common/content_features.h"
 #include "ui/gfx/animation/animation.h"
 
 namespace content {
 
 namespace {
-void SetUpAccessibilityNotifications() {
+void SetupAccessibilityDisplayOptionsNotifier() {
   // We need to call into gfx::Animation and WebContentsImpl on the UI thread,
   // so ensure that we setup the notification on the correct thread.
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -41,18 +39,6 @@ void SetUpAccessibilityNotifications() {
                   wc->OnWebPreferencesChanged();
                 }
               }];
-
-  if (base::mac::MacOSVersion() >= 14'00'00 &&
-      base::FeatureList::IsEnabled(
-          features::kSonomaAccessibilityActivationRefinements)) {
-    // Set up KVO monitoring of VoiceOver state changes.
-    [[NSWorkspace sharedWorkspace]
-        addObserver:NSApp
-         forKeyPath:@"voiceOverEnabled"
-            options:(NSKeyValueObservingOptionInitial |
-                     NSKeyValueObservingOptionNew)
-            context:nil];
-  }
 }
 }  // namespace
 
@@ -71,7 +57,7 @@ void BrowserAccessibilityStateImplMac::InitBackgroundTasks() {
   BrowserAccessibilityStateImpl::InitBackgroundTasks();
 
   GetUIThreadTaskRunner({})->PostTask(
-      FROM_HERE, base::BindOnce(&SetUpAccessibilityNotifications));
+      FROM_HERE, base::BindOnce(&SetupAccessibilityDisplayOptionsNotifier));
 }
 
 void BrowserAccessibilityStateImplMac::UpdateHistogramsOnOtherThread() {
