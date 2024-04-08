ff4000d66264145a9c3d1d07a43818f824338956
Timofey Chudakov <tchudakov@google.com>
2024-04-08 19:52:26 +0000

    [Mullhet] Process PasswordStore operations in access task tests.
    credential_manager_pending_prevent_silent_access_task_unittest
    uses TestPasswordStore but ignores any delayed operations this
    password store posts to the execution sequence.
    
    The tests verify class behavior by calling the member methods which
    should be called by the PasswordStore. This is done because the tests
    do not interact with the message loop.
    
    Since tests should execute real life production scenarios, this CL
    makes the OnGetPasswordStoreResults and OnGetPasswordStoreResultsFrom
    member methods private and re-implements tests by processing password
    store operations.
    
    Bug: b/331409076
    Change-Id: I7b8b37f260a931936374d3eadc7e4474f67ad44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5432260
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284045}

--- components/password_manager/core/browser/credential_manager_pending_prevent_silent_access_task_unittest.cc
+++ components/password_manager/core/browser/credential_manager_pending_prevent_silent_access_task_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/credential_manager_pending_prevent_silent_access_task.h"
 
 #include "base/test/task_environment.h"
+#include "components/password_manager/core/browser/form_parsing/form_data_parser.h"
 #include "components/password_manager/core/browser/password_store/test_password_store.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -13,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager {
 
 namespace {
+
+constexpr const char kUrl[] = "https://www.example.com";
 
 class CredentialManagerPendingPreventSilentAccessTaskDelegateMock
     : public CredentialManagerPendingPreventSilentAccessTaskDelegate {
@@ -46,8 +49,10 @@ class CredentialManagerPendingPreventSilentAccessTaskTest
     account_store_->ShutdownOnUIThread();
     profile_store_->ShutdownOnUIThread();
     // It's needed to cleanup the password store asynchronously.
-    task_environment_.RunUntilIdle();
+    ProcessPasswordStoreUpdates();
   }
+
+  void ProcessPasswordStoreUpdates() { task_environment_.RunUntilIdle(); }
 
  protected:
   testing::NiceMock<CredentialManagerPendingPreventSilentAccessTaskDelegateMock>
@@ -56,49 +61,45 @@ class CredentialManagerPendingPreventSilentAccessTaskTest
   scoped_refptr<TestPasswordStore> account_store_;
 
  private:
-  base::test::TaskEnvironment task_environment_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
-TEST_F(CredentialManagerPendingPreventSilentAccessTaskTest, ProfileStoreOnly) {
+// Verify that the delegate is notified when the credentials are fetched from
+// the password store.
+TEST_F(CredentialManagerPendingPreventSilentAccessTaskTest,
+       NotifiesDelegate_ProfileStoreOnly) {
   ON_CALL(delegate_mock_, GetProfilePasswordStore)
       .WillByDefault(testing::Return(profile_store_.get()));
   ON_CALL(delegate_mock_, GetAccountPasswordStore)
       .WillByDefault(testing::Return(nullptr));
 
-  CredentialManagerPendingPreventSilentAccessTask task(&delegate_mock_);
-
-  task.AddOrigin(PasswordFormDigest(PasswordForm::Scheme::kHtml,
-                                    /*signon_realm=*/"www.example.com",
-                                    GURL("www.example.com")));
-
   // We are expecting results from only one store, delegate should be called
   // upon getting a response from the store.
   EXPECT_CALL(delegate_mock_, DoneRequiringUserMediation);
-  task.OnGetPasswordStoreResultsFrom(profile_store_.get(), {});
+
+  CredentialManagerPendingPreventSilentAccessTask task(&delegate_mock_);
+  task.AddOrigin(PasswordFormDigest(PasswordForm::Scheme::kHtml,
+                                    GetSignonRealm(GURL(kUrl)), GURL(kUrl)));
+  ProcessPasswordStoreUpdates();
 }
 
+// Verify that the delegate is nofitied only once when credentials are fetched
+// from both the profile and account password stores.
 TEST_F(CredentialManagerPendingPreventSilentAccessTaskTest,
-       ProfileAndAccountStores) {
+       NotifiesDelegate_ProfileAndAccountStores) {
   ON_CALL(delegate_mock_, GetProfilePasswordStore)
       .WillByDefault(testing::Return(profile_store_.get()));
   ON_CALL(delegate_mock_, GetAccountPasswordStore)
       .WillByDefault(testing::Return(account_store_.get()));
 
-  CredentialManagerPendingPreventSilentAccessTask task(&delegate_mock_);
-
-  task.AddOrigin(PasswordFormDigest(PasswordForm::Scheme::kHtml,
-                                    /*signon_realm=*/"www.example.com",
-                                    GURL("www.example.com")));
-
-  // We are expecting results from 2 stores, the delegate shouldn't be called
-  // until both stores respond.
-  EXPECT_CALL(delegate_mock_, DoneRequiringUserMediation).Times(0);
-  task.OnGetPasswordStoreResultsFrom(profile_store_.get(), {});
-
-  testing::Mock::VerifyAndClearExpectations(&delegate_mock_);
-
+  // We are expecting results from 2 stores, the delegate should be called only
+  // once after both stores return logins.
   EXPECT_CALL(delegate_mock_, DoneRequiringUserMediation);
-  task.OnGetPasswordStoreResultsFrom(account_store_.get(), {});
+
+  CredentialManagerPendingPreventSilentAccessTask task(&delegate_mock_);
+  task.AddOrigin(PasswordFormDigest(PasswordForm::Scheme::kHtml,
+                                    GetSignonRealm(GURL(kUrl)), GURL(kUrl)));
+  ProcessPasswordStoreUpdates();
 }
 
 }  // namespace password_manager
