195c2a2f832e05943de1c918dae9e804d9d25484
Ryan Tarpine <rtarpine@chromium.org>
2024-04-08 17:51:58 +0000

    BrowsingDataFilterBuilder: Replace IsCrossSiteClearSiteDataForCookies().
    Rename SetPartitionedStateAllowedOnly() to SetPartitionedCookiesOnly().
    
    Replace IsCrossSiteClearSiteDataForCookies() with
    PartitionedCookiesOnly().
    
    Bug: 331733538
    Change-Id: I827b460fb12e09d6c158aaa9d22c32cb68cdaac6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372996
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283933}

--- content/browser/browsing_data/browsing_data_filter_builder_impl.h
+++ content/browser/browsing_data/browsing_data_filter_builder_impl.h
@@ -37,7 +37,6 @@ class CONTENT_EXPORT BrowsingDataFilterBuilderImpl
   void SetCookiePartitionKeyCollection(
       const net::CookiePartitionKeyCollection& cookie_partition_key_collection)
       override;
-  bool IsCrossSiteClearSiteDataForCookies() const override;
   void SetStorageKey(
       const std::optional<blink::StorageKey>& storage_key) override;
   bool HasStorageKey() const override;
@@ -45,7 +44,8 @@ class CONTENT_EXPORT BrowsingDataFilterBuilderImpl
       const blink::StorageKey& other_key) const override;
   bool MatchesAllOriginsAndDomains() override;
   bool MatchesNothing() override;
-  void SetPartitionedStateAllowedOnly(bool value) override;
+  void SetPartitionedCookiesOnly(bool value) override;
+  bool PartitionedCookiesOnly() const override;
   void SetStoragePartitionConfig(
       const StoragePartitionConfig& storage_partition_config) override;
   std::optional<StoragePartitionConfig> GetStoragePartitionConfig() override;
@@ -70,8 +70,6 @@ class CONTENT_EXPORT BrowsingDataFilterBuilderImpl
   const net::CookiePartitionKeyCollection&
   GetCookiePartitionKeyCollectionForTesting() const;
 
-  bool PartitionedStateAllowedOnlyForTesting() const;
-
  private:
   bool IsEqual(const BrowsingDataFilterBuilder& other) const override;
 
@@ -83,7 +81,7 @@ class CONTENT_EXPORT BrowsingDataFilterBuilderImpl
   net::CookiePartitionKeyCollection cookie_partition_key_collection_ =
       net::CookiePartitionKeyCollection::ContainsAll();
   std::optional<blink::StorageKey> storage_key_ = std::nullopt;
-  bool partitioned_state_only_ = false;
+  bool partitioned_cookies_only_ = false;
   std::optional<StoragePartitionConfig> storage_partition_config_ =
       std::nullopt;
 };
