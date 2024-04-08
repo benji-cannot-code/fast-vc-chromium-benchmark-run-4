2b048a534faaf1c60a73bf27083f97a007933921
Tim Sergeant <tsergeant@chromium.org>
2024-04-08 08:33:19 +0000

    [Mall] Add device context to launches of the Mall app
    Whenever the Mall app is launched through the App Service publisher,
    intercept it and add a base64-encoded proto to the URL containing
    device information. This is a simple substitute for being able to use a
    mojo-style API (e.g. in an SWA).
    
    Bug: b/330246426
    Change-Id: I40964dab1658d85b49fefc7761a60d0f37fdb553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5401495
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283742}

---
+++ chrome/browser/ash/mall/mall_url.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_MALL_MALL_URL_H_
+#define CHROME_BROWSER_ASH_MALL_MALL_URL_H_
+
+class GURL;
+
+namespace apps {
+struct DeviceInfo;
+}
+
+namespace ash {
+
+// Returns a URL to launch the mall app. Includes the `info` as additional
+// context.
+GURL GetMallLaunchUrl(const apps::DeviceInfo& info);
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_MALL_MALL_URL_H_
--- chrome/browser/web_applications/preinstalled_web_apps/app_mall.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/app_mall.cc
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "chromeos/constants/url_constants.h"
+#include "url/gurl.h"
 
 namespace web_app {
 
 ExternalInstallOptions GetConfigForAppMall() {
   ExternalInstallOptions options(
-      /*install_url=*/GURL("https://discover.apps.chrome/install/"),
+      /*install_url=*/GURL(chromeos::kAppMallBaseUrl).Resolve("install/"),
       /*user_display_mode=*/mojom::UserDisplayMode::kStandalone,
       /*install_source=*/ExternalInstallSource::kExternalDefault);
 
