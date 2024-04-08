39923dbc2cbcee4e92ca617935e4c256d59f8deb
Cici Ruan <cuicuiruan@google.com>
2024-04-08 19:47:52 +0000

    gd: add uma metrics 3/n
    This CL adds metrics for:
    - Toggle toolbar to expanded or collapsed.
    - Toolbar changed to new position.
    - Function triggered on the main menu.
    - Toggle the welcome dialog notification on or off.
    - Toggle game controls hint on or off.
    - Toggle game controls feature on or off.
    
    Details: go/gd-metrics.
    
    Bug: b/303308615
    Test: manual test
    Test: ash_unittests --gtest_filter="*GameDashboardContext*"
    Change-Id: I77d9b5e96325972b6f3f83a2a9202a423763a04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428132
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284041}

--- ash/game_dashboard/game_dashboard_context_test_api.cc
+++ ash/game_dashboard/game_dashboard_context_test_api.cc
@@ -255,7 +255,7 @@ IconButton* GameDashboardContextTestApi::GetToolbarScreenshotButton() {
           GameDashboardToolbarView::ToolbarViewId::kScreenshotButton)));
 }
 
-GameDashboardContext::ToolbarSnapLocation
+GameDashboardToolbarSnapLocation
 GameDashboardContextTestApi::GetToolbarSnapLocation() const {
   return context_->toolbar_snap_location_;
 }
