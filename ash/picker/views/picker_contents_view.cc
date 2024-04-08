eff87aed6822603535229c3ed4d3a8117745c32a
Darren Shen <shend@chromium.org>
2024-04-08 08:26:30 +0000

    Temporarily remove gradient helper to fix flickering.
    Bug: b/330785264
    Change-Id: Id3a701e1a4ab55b35c6d70de5c6689b0d541b125
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5433576
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283730}

--- ash/picker/views/picker_contents_view.cc
+++ ash/picker/views/picker_contents_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/controls/rounded_scroll_bar.h"
-#include "ash/controls/scroll_view_gradient_helper.h"
 #include "ash/picker/views/picker_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -27,8 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 namespace {
-
-constexpr int kScrollViewGradientHeight = 16;
 
 constexpr auto kScrollViewContentsBorderInsets = gfx::Insets::TLBR(0, 0, 8, 0);
 
@@ -54,24 +51,13 @@ class PickerScrollView : public views::ScrollView {
         .SetBackgroundColor(std::nullopt)
         .SetHorizontalScrollBarMode(views::ScrollView::ScrollBarMode::kDisabled)
         .BuildChildren();
-
-    // Paint to layer so that we can apply a gradient mask.
-    SetPaintToLayer(ui::LAYER_NOT_DRAWN);
-    gradient_helper_ = std::make_unique<ScrollViewGradientHelper>(
-        this, kScrollViewGradientHeight);
   }
   PickerScrollView(const PickerScrollView&) = delete;
   PickerScrollView& operator=(const PickerScrollView&) = delete;
   ~PickerScrollView() override = default;
 
-  void OnBoundsChanged(const gfx::Rect& previous_bounds) override {
-    gradient_helper_->UpdateGradientMask();
-  }
-
- private:
-  // Applies fade in / fade out gradients at the top and bottom of the scroll
-  // view to indicate when the contents can be scrolled.
-  std::unique_ptr<ScrollViewGradientHelper> gradient_helper_;
+  // TODO: b/330785264 - Add back gradient helper once the flickering issue is
+  // resolved.
 };
 
 BEGIN_METADATA(PickerScrollView)
