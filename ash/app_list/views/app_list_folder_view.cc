37b38db6203a4f09f898f77936bcaeba03230a57
Toni Barzic <tbarzic@chromium.org>
2024-04-08 21:47:00 +0000

    Fix shutdown crash when orphaning folder shadow layer
    Correctly updates the app list folder background view shadow when the
    shadow layers get recreated. The folder shadow is added to the
    background view's below region - recreating the shadow layer (e.g.
    during "SYNC" rotation animation) would leave the old shadow layer in
    the background view's below region, which would cause mismatch between
    the shadow layer parent and the background layer parent during the time
    the shadow layer gets recreated, and the original layer gets deleted.
    If the app list view gets deleted during this time (e.g. due to
    shutdown), this would cause a crash when orphaning layers in the folder
    background view's below region.
    
    Another symptom of the issue is that the new, recreated layer does not
    get added to the background view's below region, making it not track the
    folder background view visibility - so if the layers get recreated while
    the  folder view is visible, this leaves the folder shadow permanently
    shown in the  app list folder view.
    
    The issue is addressed by adding a observer callback to
    SystemShadowOnNinePatchLayerImpl that gets called whenever shadow layer
    gets recreated, and having folder view update the background view's
    layer regions accordingly.
    
    BUG=b/319941708
    
    Change-Id: I6455765f40f99bb090a31887d1631b431599fcb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5432374
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284115}

--- ash/app_list/views/app_list_folder_view.cc
+++ ash/app_list/views/app_list_folder_view.cc
@@ -703,7 +703,9 @@ AppListFolderView::AppListFolderView(AppListFolderController* folder_controller,
 
   // Create a shadow under `background_view_`.
   shadow_ = SystemShadow::CreateShadowOnNinePatchLayer(
-      SystemShadow::Type::kElevation12);
+      SystemShadow::Type::kElevation12,
+      base::BindRepeating(&AppListFolderView::OnShadowLayerRecreated,
+                          base::Unretained(this)));
   background_view_->AddLayerToRegion(shadow_->GetLayer(),
                                      views::LayerRegion::kBelow);
 
@@ -1042,6 +1044,12 @@ void AppListFolderView::UpdatePreferredBounds() {
 
 void AppListFolderView::UpdateShadowBounds() {
   shadow_->SetContentBounds(background_view_->layer()->bounds());
+}
+
+void AppListFolderView::OnShadowLayerRecreated(ui::Layer* old_layer,
+                                               ui::Layer* new_layer) {
+  background_view_->RemoveLayerFromRegions(old_layer);
+  background_view_->AddLayerToRegion(new_layer, views::LayerRegion::kBelow);
 }
 
 int AppListFolderView::GetYOffsetForFolder() {
