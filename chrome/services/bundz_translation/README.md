da32296c1428cb2e68ca3b3f89d131f9601e869d
Mingyu Lei <leimy@chromium.org>
2024-04-08 15:44:34 +0000

    Bundz: create bundz translation service with mock translator
    A bundz translation service is created under //chrome/services to
    provide translation for the blink API or maybe other components like
    speech.
    
    In this CL, only the mock translator is implemented. It can only
    "translate" the text to the same language by echoing the input.
    
    The service should be launched in the utility process and the other
    component would communicate with it via mojo to check if translation
    between a pair of language is supported, and create translator.
    
    Bug: 331733060
    
    Change-Id: Ic4c6ce79daf845741e110868249299f9b8fc2eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5382015
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283903}

---
+++ chrome/services/bundz_translation/README.md
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+The BundzTranslationService provides functionalities to check
+if translation between a pair of source and target languages is
+supported, and create translator for the pair. The translator
+could be used by the Web Translation blink API.
+
+
+The service runs in a utility process and it should be only launched
+once.
---
+++ chrome/services/bundz_translation/mock_translator.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/bundz_translation/mock_translator.h"
+
+#include "chrome/services/bundz_translation/public/mojom/translator.mojom.h"
+
+namespace bundz_translation {
+
+MockTranslator::~MockTranslator() = default;
+
+// static
+bool MockTranslator::CanTranslate(const std::string& source_lang,
+                                  const std::string& target_lang) {
+  return source_lang == target_lang;
+}
+
+void MockTranslator::Translate(const std::string& input,
+                               TranslateCallback callback) {
+  std::move(callback).Run(input);
+}
+
+}  // namespace bundz_translation
