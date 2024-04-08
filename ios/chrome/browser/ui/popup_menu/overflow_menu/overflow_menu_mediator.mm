f78fadf4209eb91dec15e716f923a9ffb03e24b1
Filipa Senra <fsenra@google.com>
2024-04-08 15:21:41 +0000

    [Revert][iOS][PageInfo] Remove the new badge on the entry point
    In a previous CL (crrev.com/c/5349506), a new badge was added to iOS'
    Page Info, also known as Site Information, chip on the three dot menu.
    Since UX no longer wants to have the new badge on Page Info's entry
    point to promote its revamp, this CL reverts the changes of adding the
    new badge.
    
    Bug: 1512580
    Change-Id: I9cfcb1d0f47ff9e440e67a084ee30f347d8407ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419978
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283894}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/translate/model/chrome_ios_translate_client.h"
 #import "ios/chrome/browser/ui/ntp/metrics/feed_metrics_recorder.h"
-#import "ios/chrome/browser/ui/page_info/features.h"
 #import "ios/chrome/browser/ui/policy/user_policy_util.h"
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/destination_usage_history/constants.h"
 #import "ios/chrome/browser/ui/popup_menu/overflow_menu/destination_usage_history/destination_usage_history.h"
@@ -292,11 +291,6 @@ bool IsBookmarked(const GURL& url,
         _engagementTracker->Dismissed(
             feature_engagement::kIPHWhatsNewUpdatedFeature);
       }
-    }
-
-    if (self.siteInfoDestination.badge != BadgeTypeNone) {
-      _engagementTracker->Dismissed(
-          feature_engagement::kIPHiOSPageInfoRevampFeature);
     }
 
     _engagementTracker = nullptr;
@@ -1809,16 +1803,8 @@ bool IsBookmarked(const GURL& url,
     case overflow_menu::Destination::RecentTabs:
       return self.isIncognito ? nil : self.recentTabsDestination;
     case overflow_menu::Destination::SiteInfo:
-      if (![self currentWebPageSupportsSiteInfo]) {
-        return nil;
-      }
-      if (IsRevampPageInfoIosEnabled() && self.engagementTracker &&
-          self.engagementTracker->ShouldTriggerHelpUI(
-              feature_engagement::kIPHiOSPageInfoRevampFeature)) {
-        self.siteInfoDestination.badge = BadgeTypeNew;
-      }
-      return self.siteInfoDestination;
-
+      return ([self currentWebPageSupportsSiteInfo]) ? self.siteInfoDestination
+                                                     : nil;
     case overflow_menu::Destination::Settings:
       if ([self shouldIndicateIdentityError]) {
         self.settingsDestination.badge = BadgeTypeError;
