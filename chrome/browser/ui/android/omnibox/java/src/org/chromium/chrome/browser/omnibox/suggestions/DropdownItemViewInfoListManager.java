2a3d69ee3579ef7f79de8b726bb8e03816c3100d
Tomasz Wiszkowski <ender@google.com>
2024-04-08 18:08:20 +0000

    Complete clean up of the DropdownItemViewInfoListManager.
    Change-Id: Id5bce92b6a8fc96b00eb64054696ca1399fb424e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430479
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Fred Mello <fredmello@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283959}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManager.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManager.java
@@ -10,9 +10,6 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
-import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
-import org.chromium.chrome.browser.omnibox.R;
-import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
 import org.chromium.components.omnibox.GroupsProto.GroupSection;
 import org.chromium.ui.base.DeviceFormFactor;
@@ -32,11 +29,6 @@ class DropdownItemViewInfoListManager {
     private @BrandedColorScheme int mBrandedColorScheme;
     private List<DropdownItemViewInfo> mSourceViewInfoList;
 
-    private final int mListActiveOmniboxTopSmallMargin;
-    private final int mListActiveOmniboxTopBigMargin;
-    private final int mListNonActiveOmniboxTopSmallMargin;
-    private final int mListNonActiveOmniboxTopBigMargin;
-
     DropdownItemViewInfoListManager(@NonNull ModelList managedModel, @NonNull Context context) {
         assert managedModel != null : "Must specify a non-null model.";
         mContext = context;
@@ -44,21 +36,6 @@ class DropdownItemViewInfoListManager {
         mBrandedColorScheme = BrandedColorScheme.LIGHT_BRANDED_THEME;
         mSourceViewInfoList = Collections.emptyList();
         mManagedModel = managedModel;
-
-        mListActiveOmniboxTopSmallMargin =
-                OmniboxResourceProvider.getActiveOmniboxTopSmallMargin(context);
-        mListActiveOmniboxTopBigMargin =
-                mContext.getResources()
-                        .getDimensionPixelSize(
-                                R.dimen.omnibox_suggestion_list_active_top_big_margin);
-        mListNonActiveOmniboxTopSmallMargin =
-                mContext.getResources()
-                        .getDimensionPixelSize(
-                                R.dimen.omnibox_suggestion_list_non_active_top_small_margin);
-        mListNonActiveOmniboxTopBigMargin =
-                mContext.getResources()
-                        .getDimensionPixelSize(
-                                R.dimen.omnibox_suggestion_list_non_active_top_big_margin);
     }
 
     /**
@@ -116,8 +93,6 @@ class DropdownItemViewInfoListManager {
         DropdownItemViewInfo previousItem = null;
         GroupSection previousSection = null;
         GroupSection currentSection;
-        boolean shouldShowModernizeVisualUpdate =
-                OmniboxFeatures.shouldShowModernizeVisualUpdate(mContext);
 
         for (int i = 0; i < mSourceViewInfoList.size(); i++) {
             final DropdownItemViewInfo item = mSourceViewInfoList.get(i);
@@ -126,7 +101,7 @@ class DropdownItemViewInfoListManager {
             model.set(SuggestionCommonProperties.COLOR_SCHEME, mBrandedColorScheme);
             model.set(SuggestionCommonProperties.DEVICE_FORM_FACTOR, deviceType);
 
-            if (shouldShowModernizeVisualUpdate && item.processor.allowBackgroundRounding()) {
+            if (item.processor.allowBackgroundRounding()) {
                 currentSection = item.groupConfig.getSection();
                 var applyRounding = currentSection != previousSection;
 
