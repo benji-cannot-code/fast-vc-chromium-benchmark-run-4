1e43d90e0a334dec2bf1c2c63fbfa8e170b07f60
James Cook <jamescook@chromium.org>
2024-04-08 18:43:09 +0000

    birch: Add metrics for birch bar, part 7
    See go/birch-metrics. This CL adds:
    Ash.Birch.Ranking.Total
    Ash.Birch.Ranking.0000to0500
    Ash.Birch.Ranking.0500to1200
    Ash.Birch.Ranking.1200to1700
    Ash.Birch.Ranking.1700to0000
    
    Bug: b:330188422
    Test: added to ash_unittests
    Change-Id: I0b8091d62f0e27f9ca5a9fc1c03aa086b37a9368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430478
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283970}

--- ash/wm/overview/birch/birch_bar_unittest.cc
+++ ash/wm/overview/birch/birch_bar_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/test/scoped_mock_clock_override.h"
 #include "base/types/cxx23_to_underlying.h"
 #include "ui/display/manager/display_manager.h"
 #include "ui/display/test/display_manager_test_api.h"
@@ -341,6 +342,8 @@ class BirchBarTest : public AshTestBase {
 
  private:
   base::test::ScopedFeatureList feature_list_;
+  // Ensure base::Time::Now() is a fixed value.
+  base::ScopedMockClockOverride mock_clock_override_;
 };
 
 // Tests that the birch bar will be shown in the normal Overview.
@@ -376,6 +379,14 @@ TEST_F(BirchBarTest, RecordsHistogramWhenChipsShown) {
                                BirchItemType::kFile, 1);
   histograms.ExpectBucketCount("Ash.Birch.Chip.Impression",
                                BirchItemType::kCalendar, 1);
+
+  // Two rankings were recorded for the current time slot histogram.
+  histograms.ExpectBucketCount("Ash.Birch.Ranking.1200to1700", 1, 1);
+  histograms.ExpectBucketCount("Ash.Birch.Ranking.1200to1700", 9, 1);
+
+  // The same ranking were recorded for the all-day total histogram.
+  histograms.ExpectBucketCount("Ash.Birch.Ranking.Total", 1, 1);
+  histograms.ExpectBucketCount("Ash.Birch.Ranking.Total", 9, 1);
 }
 
 // Tests that the birch bar will be hidden in the partial Overview with a split
