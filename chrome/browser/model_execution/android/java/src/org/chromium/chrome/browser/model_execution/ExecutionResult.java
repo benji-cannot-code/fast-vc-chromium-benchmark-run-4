52bd06734407023f0109cb7d49550b96fef2f900
Salvador Guerrero <salg@google.com>
2024-04-08 19:22:17 +0000

    Added additional enum values to execution result interface
    Bug: b/2013979485
    Change-Id: I185ce3fcc133391add70709f2fec01f751da16d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435902
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284032}

--- chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ExecutionResult.java
+++ chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ExecutionResult.java
@@ -24,6 +24,8 @@ public class ExecutionResult {
         ExecutionError.NOT_AVAILABLE,
         ExecutionError.BUSY,
         ExecutionError.FILTERED,
+        ExecutionError.INPUT_FILTERED,
+        ExecutionError.REQUEST_TOO_LARGE,
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface ExecutionError {
@@ -31,6 +33,8 @@ public class ExecutionResult {
         public static final int NOT_AVAILABLE = 1;
         public static final int BUSY = 2;
         public static final int FILTERED = 3;
+        public static final int INPUT_FILTERED = 4;
+        public static final int REQUEST_TOO_LARGE = 5;
     }
 
     /**
