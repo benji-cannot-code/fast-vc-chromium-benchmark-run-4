636f77baa675e76ab5ca28e365f73acb7d3682a7
Matt Giuca <mgiuca@chromium.org>
2024-04-08 06:52:00 +0000

    Update strings in app intent picker dialog.
    1. Changes "Open in app" to "Select an app to open".
    2. Removes the verbose subtitle.
    
    Affects all platforms, but on Windows/Mac/Linux only applies when Link
    Capturing is enabled.
    
    Bug: b/332984653
    Change-Id: I479c4f036dd1967bf4e9150a999c62e95ccb3dec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428271
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1283724}

--- chrome/browser/ui/views/intent_picker_dialog_browsertest.cc
+++ chrome/browser/ui/views/intent_picker_dialog_browsertest.cc
@@ -76,7 +76,7 @@ class IntentPickerDialogTest : public DialogBrowserTest {
 #define MAYBE_InvokeUi_default InvokeUi_default
 #endif
 IN_PROC_BROWSER_TEST_F(IntentPickerDialogTest, MAYBE_InvokeUi_default) {
-  set_baseline("3742640");
+  set_baseline("5428271");
   ShowAndVerifyUi();
 }
 
@@ -113,6 +113,6 @@ class IntentPickerDialogGridViewTest : public IntentPickerDialogTest {
 };
 
 IN_PROC_BROWSER_TEST_F(IntentPickerDialogGridViewTest, InvokeUi_default) {
-  set_baseline("3742640");
+  set_baseline("5428271");
   ShowAndVerifyUi();
 }
