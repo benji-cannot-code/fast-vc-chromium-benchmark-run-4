12d12d216c62c9fc56270fdd9c5b396d2f80907e
rgod <rgod@google.com>
2024-04-08 23:58:43 +0000

    [WebAuthn] Replace GPM onboarding bubble with a dialog
    The mocks have been updated to capture this view in a dialog.
    Step content (3 description rows) will be added in a follow-up.
    
    Bug: 40274370
    Change-Id: I8725c5ef34b10c338c6e0f199ff83e6b7434a824
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435003
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284180}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -407,7 +407,6 @@ StepUIType step_ui_type(AuthenticatorRequestDialogModel::Step step) {
       return StepUIType::WINDOW;
 
     case AuthenticatorRequestDialogModel::Step::kGPMTouchID:
-    case AuthenticatorRequestDialogModel::Step::kGPMOnboarding:
     case AuthenticatorRequestDialogModel::Step::kGPMPasskeySaved:
       return StepUIType::BUBBLE;
 
