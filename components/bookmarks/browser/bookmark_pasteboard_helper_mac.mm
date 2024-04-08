81104688893cd44dca7a1a624436b4c6c8955781
Anupam Snigdha <snianu@microsoft.com>
2024-04-08 19:01:35 +0000

    Clipboard: Set copied bookmark as private in incognito window on Mac.
    This is a continuation of crrev.com/c/5422175.
    
    Improve privacy of the user data by marking as current host only in
    pasteboard if bookmark data is copied from an incognito window.
    This prevents the data from being shared across different hosts.
    
    Bug: 40945200
    Change-Id: I6d13baa7ab8c4a85643f118569942b0e6352c565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427290
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1284003}

--- components/bookmarks/browser/bookmark_pasteboard_helper_mac.mm
+++ components/bookmarks/browser/bookmark_pasteboard_helper_mac.mm
@@ -268,7 +268,8 @@ NSArray<NSPasteboardItem*>* PasteboardItemsFromBookmarks(
 void WriteBookmarksToPasteboard(
     NSPasteboard* pb,
     const std::vector<BookmarkNodeData::Element>& elements,
-    const base::FilePath& profile_path) {
+    const base::FilePath& profile_path,
+    bool is_off_the_record) {
   if (elements.empty()) {
     return;
   }
@@ -276,6 +277,10 @@ void WriteBookmarksToPasteboard(
   NSArray<NSPasteboardItem*>* items =
       PasteboardItemsFromBookmarks(elements, profile_path);
   [pb clearContents];
+  if (is_off_the_record) {
+    // Make the pasteboard content current host only.
+    [pb prepareForNewContentsWithOptions:NSPasteboardContentsCurrentHostOnly];
+  }
   [pb writeObjects:items];
 }
 
