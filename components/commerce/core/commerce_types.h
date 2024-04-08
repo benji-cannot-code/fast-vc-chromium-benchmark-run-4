bdb286bd42431aa83380532d8ddda278612ae6ef
Yue Zhang <yuezhanggg@chromium.org>
2024-04-08 23:47:00 +0000

    [Product specifications] Add product category info
    Bug: b/332725713
    Change-Id: I323a5be62715862aabf4651cc21928221efe68c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425731
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284171}

--- components/commerce/core/commerce_types.h
+++ components/commerce/core/commerce_types.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/tuple.h"
 #include "components/commerce/core/proto/parcel.pb.h"
+#include "components/commerce/core/proto/product_category.pb.h"
 #include "url/gurl.h"
 
 namespace commerce {
@@ -120,6 +121,7 @@ struct ProductInfo {
   int64_t amount_micros{0};
   std::optional<int64_t> previous_amount_micros;
   std::string country_code;
+  CategoryData category_data;
 
  private:
   friend class ShoppingService;
