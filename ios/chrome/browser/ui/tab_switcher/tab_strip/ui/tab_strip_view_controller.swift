57fbd75960ad621a5099d4b4dae4090ff6c8f7ef
Quentin Pubert <qpubert@google.com>
2024-04-08 17:20:07 +0000

    [iOS][TabStrip] Only reconfigure items in the snapshot
    This CL ensures only items from the data source snapshot can be reloaded
    through `reconfigureItems`.
    
    Bug: 333377248
    Change-Id: I9f9264c10ebe48b16d4412929714a933a79c14dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435279
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1283921}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_view_controller.swift
@@ -248,7 +248,9 @@ class TabStripViewController: UIViewController, TabStripTabCellDelegate,
 
   func reconfigureItems(_ items: [TabStripItemIdentifier]) {
     var snapshot = dataSource.snapshot()
-    snapshot.reconfigureItems(items)
+    let itemsInSnapshot = Set(snapshot.itemIdentifiers)
+    let itemsToReconfigure = Set(items).intersection(itemsInSnapshot)
+    snapshot.reconfigureItems(Array(itemsToReconfigure))
     dataSource.apply(snapshot)
   }
 
