6ce6d4b756c1e330fe379e9d87b4fcd4b772d608
Zachary Tan <tanzachary@chromium.org>
2024-04-09 01:57:13 +0000

    [FedCM] Minor UI fixes for button flow modal
    1. Fix incorrect button row between child spacing
    2. Fix missing space above disclosure text
    3. Fix avatar being fetched at lower resolution than modal avatar size
    
    Bug: 41491333
    Change-Id: I938158bf337fc95be03f6c5cbbca2bba44f8d099
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434445
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284244}

--- chrome/browser/ui/views/webid/account_selection_view_base.cc
+++ chrome/browser/ui/views/webid/account_selection_view_base.cc
@@ -170,7 +170,8 @@ class AccountImageView : public views::ImageView {
 
   // Fetch image and set it on AccountImageView.
   void FetchAccountImage(const content::IdentityRequestAccount& account,
-                         image_fetcher::ImageFetcher& image_fetcher) {
+                         image_fetcher::ImageFetcher& image_fetcher,
+                         int image_size) {
     image_fetcher::ImageFetcherParams params(kTrafficAnnotation,
                                              kImageFetcherUmaClient);
 
@@ -181,7 +182,8 @@ class AccountImageView : public views::ImageView {
         account.picture,
         base::BindOnce(&AccountImageView::OnAccountImageFetched,
                        weak_ptr_factory_.GetWeakPtr(),
-                       base::UTF8ToUTF16(account.name), &image_fetcher),
+                       base::UTF8ToUTF16(account.name), &image_fetcher,
+                       image_size),
         std::move(params));
   }
 
@@ -208,6 +210,7 @@ class AccountImageView : public views::ImageView {
  private:
   void OnAccountImageFetched(const std::u16string& account_name,
                              image_fetcher::ImageFetcher* image_fetcher,
+                             int image_size,
                              const gfx::Image& image,
                              const image_fetcher::RequestMetadata& metadata) {
     gfx::ImageSkia avatar;
@@ -217,11 +220,11 @@ class AccountImageView : public views::ImageView {
         letter = base::i18n::ToUpper(account_name.substr(0, 1));
       }
       avatar = gfx::CanvasImageSource::MakeImageSkia<
-          LetterCircleCroppedImageSkiaSource>(letter, kDesiredAvatarSize);
+          LetterCircleCroppedImageSkiaSource>(letter, image_size);
     } else {
       avatar =
           gfx::CanvasImageSource::MakeImageSkia<CircleCroppedImageSkiaSource>(
-              image.AsImageSkia(), std::nullopt, kDesiredAvatarSize);
+              image.AsImageSkia(), std::nullopt, image_size);
     }
     if (idp_image_) {
       SetBadgedImage(avatar, *idp_image_);
@@ -354,7 +357,7 @@ void AccountSelectionViewBase::SetLabelProperties(views::Label* label) {
       views::kFlexBehaviorKey,
       views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToZero,
                                views::MaximumFlexSizeRule::kUnbounded,
-                               /*adjust_height_for_width =*/true));
+                               /*adjust_height_for_width=*/true));
 }
 
 std::unique_ptr<views::View> AccountSelectionViewBase::CreateAccountRow(
@@ -380,7 +383,8 @@ std::unique_ptr<views::View> AccountSelectionViewBase::CreateAccountRow(
       ConfigureBadgeIdp(*account_image_view,
                         idp_display_data.idp_metadata.brand_icon_url);
     }
-    account_image_view->FetchAccountImage(account, *image_fetcher_);
+    account_image_view->FetchAccountImage(account, *image_fetcher_,
+                                          avatar_size);
 
     std::unique_ptr<views::ImageView> arrow_icon_view = nullptr;
     if (is_modal_dialog) {
@@ -420,7 +424,7 @@ std::unique_ptr<views::View> AccountSelectionViewBase::CreateAccountRow(
     }
     return row;
   }
-  account_image_view->FetchAccountImage(account, *image_fetcher_);
+  account_image_view->FetchAccountImage(account, *image_fetcher_, avatar_size);
   auto row = std::make_unique<views::View>();
   row->SetLayoutManager(std::make_unique<views::BoxLayout>(
       views::BoxLayout::Orientation::kHorizontal,
