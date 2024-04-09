ba288f6b45642b1cb1eadf1490f59cb179bad175
Michele Fan <michelefan@chromium.org>
2024-04-09 18:51:36 +0000

    snap-group: Fix divider exit overview animation regression
    This cl fixed the regression of the divider exit overview animation.
    `divider_window->Show()` will be called in `RefreshStackingOrder()`
    prior to `RefreshDividerState()` causing the divider to appear
    precipitously before the windows animation.
    
    Demo: http://b/333465871#comment2
    Fixed: b/333465871
    Test: Added regression test (fail w/o the change) + manually
    Change-Id: Ifc0028eb07d1045615ea419a40643db56dc80a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436091
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284613}

--- ash/wm/splitview/split_view_divider.cc
+++ ash/wm/splitview/split_view_divider.cc
@@ -590,7 +590,8 @@ void SplitViewDivider::RefreshStackingOrder() {
 
   base::AutoReset<bool> lock(&is_refreshing_stacking_order_, true);
 
-  if (observed_windows_.empty() || !divider_widget_) {
+  if (observed_windows_.empty() || !divider_widget_ ||
+      !divider_widget_->IsVisible()) {
     return;
   }
 
