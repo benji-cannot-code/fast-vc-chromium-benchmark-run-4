202bc917ab2c112bbb29640952c22f16bbe20110
Matt Reynolds <mattreynolds@chromium.org>
2024-04-09 00:45:54 +0000

    usb: Migrate UsbDeviceHandleWin from base::Owned to std::unique_ptr
    Bug: 332935372
    Change-Id: Ic68b3f0e6395e30af280cfbdc9e9a39074a87753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430662
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284210}

--- services/device/usb/usb_device_handle_win.h
+++ services/device/usb/usb_device_handle_win.h
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/device/usb/scoped_winusb_handle.h"
 #include "services/device/usb/usb_device_handle.h"
 
+struct _USB_NODE_CONNECTION_INFORMATION_EX;
+
 namespace base {
 class RefCountedBytes;
 class SequencedTaskRunner;
@@ -192,7 +194,7 @@ class UsbDeviceHandleWin : public UsbDeviceHandle {
   std::unique_ptr<Request> UnlinkRequest(Request* request);
   void GotNodeConnectionInformation(
       TransferCallback callback,
-      void* node_connection_info,
+      std::unique_ptr<_USB_NODE_CONNECTION_INFORMATION_EX> node_connection_info,
       scoped_refptr<base::RefCountedBytes> buffer,
       std::pair<DWORD, DWORD> result_and_bytes_transferred);
   void GotDescriptorFromNodeConnection(
