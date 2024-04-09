ecc4c5a3269fdffd9f7271a708071652630a8840
Gang Wu <gangwu@chromium.org>
2024-04-09 15:51:40 +0000

    Clean up UX revamp flag in ToolbarPhone
    This cl only clean up the flag in ToolbarPhone. Renaming location_bar_start_padding_modern to location_bar_start_padding will be in the follow up CL.
    
    
    
    Change-Id: Ic937fd02475eecc066180fc20c0fff9c608c8b40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435183
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284498}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java
@@ -57,7 +57,6 @@ import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.omnibox.LocationBar;
 import org.chromium.chrome.browser.omnibox.LocationBarCoordinator;
 import org.chromium.chrome.browser.omnibox.NewTabPageDelegate;
-import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.omnibox.SearchEngineUtils;
 import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
@@ -142,7 +141,6 @@ public class ToolbarPhone extends ToolbarLayout
     protected View mUrlActionContainer;
     protected ImageView mToolbarShadow;
     private OptionalButtonCoordinator mOptionalButtonCoordinator;
-    private boolean mShouldShowModernizeVisualUpdate;
 
     @ViewDebug.ExportedProperty(category = "chrome")
     protected int mTabSwitcherState;
@@ -322,7 +320,6 @@ public class ToolbarPhone extends ToolbarLayout
      */
     public ToolbarPhone(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mShouldShowModernizeVisualUpdate = OmniboxFeatures.shouldShowModernizeVisualUpdate(context);
         mIsSurfacePolishEnabled = ChromeFeatureList.sSurfacePolish.isEnabled();
         mIsSurfacePolishOmniboxColorEnabled =
                 mIsSurfacePolishEnabled
@@ -442,9 +439,6 @@ public class ToolbarPhone extends ToolbarLayout
     }
 
     private void updateModernLocationBarCorners() {
-        if (!mShouldShowModernizeVisualUpdate) {
-            return;
-        }
         int nonFocusedRadius =
                 getResources()
                         .getDimensionPixelSize(R.dimen.modern_toolbar_background_corner_radius);
@@ -479,8 +473,7 @@ public class ToolbarPhone extends ToolbarLayout
      * @param shouldUseFocusColor True if should return the color for focus state.
      */
     private @ColorInt int getToolbarDefaultColor(boolean shouldUseFocusColor) {
-        if (mShouldShowModernizeVisualUpdate
-                && (mLocationBar.getPhoneCoordinator().hasFocus() || shouldUseFocusColor)) {
+        if (mLocationBar.getPhoneCoordinator().hasFocus() || shouldUseFocusColor) {
             if (mDropdownListScrolled) {
                 return isIncognito()
                         ? getContext().getColor(R.color.default_bg_color_dark_elev_2_baseline)
@@ -503,8 +496,7 @@ public class ToolbarPhone extends ToolbarLayout
      */
     private @ColorInt int getLocationBarDefaultColorForToolbarColor(
             @ColorInt int toolbarColor, boolean shouldUseFocusColor) {
-        if (mShouldShowModernizeVisualUpdate
-                && (mLocationBar.getPhoneCoordinator().hasFocus() || shouldUseFocusColor)) {
+        if (mLocationBar.getPhoneCoordinator().hasFocus() || shouldUseFocusColor) {
 
             // Omnibox has same background as the Omnibox suggestion.
             return mLocationBar.getSuggestionBackgroundColor(isIncognito());
@@ -889,8 +881,7 @@ public class ToolbarPhone extends ToolbarLayout
             case VisualState.INCOGNITO:
                 return ChromeColors.getDefaultThemeColor(getContext(), true);
             case VisualState.BRAND_COLOR:
-                if (mShouldShowModernizeVisualUpdate
-                        && mLocationBar.getPhoneCoordinator().hasFocus()) {
+                if (mLocationBar.getPhoneCoordinator().hasFocus()) {
                     return getToolbarDefaultColor(/* shouldUseFocusColor= */ false);
                 }
                 if (mIsSurfacePolishEnabled && mIsShowingStartSurfaceHomepage) {
@@ -1201,8 +1192,7 @@ public class ToolbarPhone extends ToolbarLayout
             // focusing on the NTP. In NTP, toolbar and locationbar need to transite color only when
             // the omnibox is focused. When the fake omnibox is scrolled, the color should not
             // change.
-            if (((mShouldShowModernizeVisualUpdate && mLocationBar.getPhoneCoordinator().hasFocus())
-                            || !isLocationBarShownInNtp)
+            if ((mLocationBar.getPhoneCoordinator().hasFocus() || !isLocationBarShownInNtp)
                     && mTabSwitcherState == STATIC_TAB) {
                 boolean usePolishedLocationBar = isLocationBarShownInGeneralNtpOrStartSurface();
                 // Add a special case for general NTP and Start Surface to the defaultColor to
@@ -3079,11 +3069,7 @@ public class ToolbarPhone extends ToolbarLayout
 
     private int getAdditionalOffsetForNtp() {
         return getResources().getDimensionPixelSize(R.dimen.fake_search_box_lateral_padding)
-                - getResources()
-                        .getDimensionPixelSize(
-                                mShouldShowModernizeVisualUpdate
-                                        ? R.dimen.location_bar_start_padding_modern
-                                        : R.dimen.location_bar_start_padding);
+                - getResources().getDimensionPixelSize(R.dimen.location_bar_start_padding_modern);
     }
 
     @Override
@@ -3104,20 +3090,12 @@ public class ToolbarPhone extends ToolbarLayout
 
     @Override
     public void onSuggestionDropdownScroll() {
-        if (!mShouldShowModernizeVisualUpdate) {
-            return;
-        }
-
         mDropdownListScrolled = true;
         updateToolbarAndLocationBarColor();
     }
 
     @Override
     public void onSuggestionDropdownOverscrolledToTop() {
-        if (!mShouldShowModernizeVisualUpdate) {
-            return;
-        }
-
         mDropdownListScrolled = false;
         updateToolbarAndLocationBarColor();
     }
