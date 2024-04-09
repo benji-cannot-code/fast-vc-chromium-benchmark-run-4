980c89894b3d89dffb72a6fc4dade166c5359b20
Scott Yoder <scottyoder@google.com>
2024-04-09 21:21:59 +0000

    [ios] Make all Snackbars with actions use legacy dismissal behavior
    Because something is wrong with the autoroller for MDC, the property
    useLegacyDismissalBehavior had to be declared in a category in order to
    access it. A further run-time check is added to ensure it is there. A
    killswitch is also added for safety.
    
    Fixed: 332491188
    Change-Id: I1bb0e8768cf3e8b6e7d74c007ed2cd9680c0474e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5438456
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284708}

--- ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
+++ ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
@@ -7,11 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <MaterialComponents/MaterialSnackbar.h>
 
+#import "base/metrics/field_trial_params.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/public/provider/chrome/browser/material/material_branding_api.h"
+
+BASE_FEATURE(kSnackbarUseLegacyDismissalBehavior,
+             "SnackbarUseLegacyDismissalBehavior",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Allow access to `usesLegacyDismissalBehavior` since the autoroller to update
+// the header is broken.
+@interface MDCSnackbarMessage (UsesLegacyDismissalBehavior)
+@property(nonatomic) BOOL usesLegacyDismissalBehavior;
+@end
 
 @interface SnackbarCoordinator () <MDCSnackbarManagerDelegate>
 
@@ -95,6 +106,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [MDCSnackbarMessage messageWithText:messageText];
   message.action = action;
   message.completionHandler = completionAction;
+
+  // TODO:(crbug.com/333567367) Clean up the killswitch.
+  if (base::FeatureList::IsEnabled(kSnackbarUseLegacyDismissalBehavior)) {
+    if ([message
+            respondsToSelector:@selector(setUsesLegacyDismissalBehavior:)]) {
+      message.usesLegacyDismissalBehavior = YES;
+    }
+  }
 
   [self showSnackbarMessage:message];
 }
