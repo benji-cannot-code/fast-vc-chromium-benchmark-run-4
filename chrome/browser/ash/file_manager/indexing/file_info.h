59238f080d8d63f47eebe72a9ac352fff6dd5b3f
Bo Majewski <majewski@chromium.org>
2024-04-09 00:05:37 +0000

    [Files]: Simplifies in-memory index by using augmented terms.
    Rather than having separate maps one for global terms (not prefixed with
    a field) and one for field specific terms, this CL introduces a concept
    of a augmented term. A term such as label:starred previously was treated
    as a pair of field name (e.g., "label") and term (e.g., "starred"). Only
    the term was converted to an ID. This required then having two separate
    maps: from field name to posting list and a global posting list. In this
    CL each term is converted to an augmented term ID, one with an empty
    field and one with the field specified by the user. This allows for
    terms to be intermixed in the single posting list. No changes to the
    unit tests are necessary.
    
    Bug: b:327535200, b:333029290
    Change-Id: Ib1679186751a6f96a356e6987c140859db2d9308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427492
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284185}

--- chrome/browser/ash/file_manager/indexing/file_info.h
+++ chrome/browser/ash/file_manager/indexing/file_info.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_FILE_MANAGER_INDEXING_FILE_INFO_H_
 #define CHROME_BROWSER_ASH_FILE_MANAGER_INDEXING_FILE_INFO_H_
 
+#include <ostream>
+
 #include "base/time/time.h"
 #include "url/gurl.h"
 
@@ -34,6 +36,10 @@ struct FileInfo {
            file_url == other.file_url && size == other.size &&
            last_modified == other.last_modified;
   }
+
+  // Human readable output of this structure.
+  friend std::ostream& operator<<(std::ostream& stream,
+                                  const FileInfo& file_info);
 
   // The URL of the file in the form filesystem:<origin>:<type>:/path/to/entry.
   // For example, for a file stored on the local file system the URL would be
