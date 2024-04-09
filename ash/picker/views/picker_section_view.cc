f0fa7c7fd0d75fd646e77771cd65afc845f817f7
Darren Shen <shend@chromium.org>
2024-04-09 08:11:42 +0000

    Trailing link should not steal focus.
    Bug: b/332991506
    Change-Id: Ia5b8f1cdb91aaf89d039714089570040d336b6d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5431695
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284334}

--- ash/picker/views/picker_section_view.cc
+++ ash/picker/views/picker_section_view.cc
@@ -84,6 +84,7 @@ void PickerSectionView::AddTitleTrailingLink(
           .SetEnabledColorId(cros_tokens::kCrosSysPrimary)
           .SetForceUnderline(false)
           .SetProperty(views::kMarginsKey, kSectionTitleTrailingLinkMargins)
+          .SetFocusBehavior(views::View::FocusBehavior::ACCESSIBLE_ONLY)
           .Build());
 }
 
