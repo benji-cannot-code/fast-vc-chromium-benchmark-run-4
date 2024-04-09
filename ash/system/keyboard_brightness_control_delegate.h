e1e08a3eb372c3b5193a345f754f3da02b997a67
Longbo Wei <longbowei@google.com>
2024-04-09 21:55:51 +0000

    kbd-backlight: Add HandleGetKeyboardBrightness in ash
    * This method will be used to get the initial brightness for the
      keyboard brightness slider.
    
    Bug: b/326009607
    Change-Id: I306144338a8bdd702f05663e661dfe4b01b888ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430919
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284727}

--- ash/system/keyboard_brightness_control_delegate.h
+++ ash/system/keyboard_brightness_control_delegate.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_KEYBOARD_BRIGHTNESS_CONTROL_DELEGATE_H_
 #define ASH_SYSTEM_KEYBOARD_BRIGHTNESS_CONTROL_DELEGATE_H_
 
+#include <optional>
+
+#include "base/functional/callback_forward.h"
+
 namespace ash {
 
 // Delegate for controlling the keyboard brightness.
@@ -13,12 +17,23 @@ class KeyboardBrightnessControlDelegate {
  public:
   virtual ~KeyboardBrightnessControlDelegate() {}
 
-  // Handles the request to decrease or increase the keyboard brightness, or
-  // toggle the backlight itself on/off, or set brightness to a specific level.
+  // Handles an accelerator-driven request to decrease or increase the keyboard
+  // brightness.
   virtual void HandleKeyboardBrightnessDown() = 0;
   virtual void HandleKeyboardBrightnessUp() = 0;
+
+  // Request that to turn keyboard brightness on or off.
   virtual void HandleToggleKeyboardBacklight() = 0;
+
+  // Requests that the keyboard brightness be set to |percent|, in the range
+  // [0.0, 100.0].  |gradual| specifies whether the transition to the new
+  // brightness should be animated or instantaneous.
   virtual void HandleSetKeyboardBrightness(double percent, bool gradual) = 0;
+
+  // Asynchronously invokes |callback| with the current brightness, in the range
+  // [0.0, 100.0]. In case of error, it is called with nullopt.
+  virtual void HandleGetKeyboardBrightness(
+      base::OnceCallback<void(std::optional<double>)> callback) = 0;
 };
 
 }  // namespace ash
