cc5d7622177b5f8f17d89bb870b48986ac4d1f19
Chris Fredrickson <cfredric@chromium.org>
2024-04-09 19:43:18 +0000

    Move FedCM/SAA autogrants to consider RP embedder instead of requester
    Bug: 330574529
    Change-Id: Id9d92989564ef35db82965ab2a543aee455ee2ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440112
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284642}

--- chrome/browser/webid/federated_identity_account_keyed_permission_context_unittest.cc
+++ chrome/browser/webid/federated_identity_account_keyed_permission_context_unittest.cc
@@ -295,11 +295,17 @@ TEST_F(FederatedIdentityAccountKeyedPermissionContextTest,
   context()->GrantPermission(relying_party_requester, relying_party_embedder,
                              identity_provider, "my_account");
   EXPECT_TRUE(
-      context()->HasPermission(net::SchemefulSite(relying_party_requester),
+      context()->HasPermission(net::SchemefulSite(relying_party_embedder),
                                net::SchemefulSite(identity_provider)));
   EXPECT_FALSE(
       context()->HasPermission(net::SchemefulSite(identity_provider),
+                               net::SchemefulSite(relying_party_embedder)));
+  EXPECT_FALSE(
+      context()->HasPermission(net::SchemefulSite(identity_provider),
                                net::SchemefulSite(relying_party_requester)));
+  EXPECT_FALSE(
+      context()->HasPermission(net::SchemefulSite(relying_party_requester),
+                               net::SchemefulSite(identity_provider)));
 }
 
 TEST_F(FederatedIdentityAccountKeyedPermissionContextTest, RevokeNoMatch) {
@@ -350,7 +356,7 @@ TEST_F(FederatedIdentityAccountKeyedPermissionContextTest,
   context()->GrantPermission(relying_party_requester, relying_party_embedder,
                              identity_provider, "my_account");
   ASSERT_TRUE(
-      context()->HasPermission(net::SchemefulSite(relying_party_requester),
+      context()->HasPermission(net::SchemefulSite(relying_party_embedder),
                                net::SchemefulSite(identity_provider)));
 
   EXPECT_THAT(context()->GetSharingPermissionGrantsAsContentSettings(),
@@ -372,7 +378,7 @@ TEST_F(FederatedIdentityAccountKeyedPermissionContextTest,
   context()->GrantPermission(relying_party_requester, relying_party_embedder,
                              identity_provider, account_id);
   ASSERT_TRUE(
-      context()->HasPermission(net::SchemefulSite(relying_party_requester),
+      context()->HasPermission(net::SchemefulSite(relying_party_embedder),
                                net::SchemefulSite(identity_provider)));
 
   EXPECT_THAT(context()->GetSharingPermissionGrantsAsContentSettings(),
@@ -380,7 +386,7 @@ TEST_F(FederatedIdentityAccountKeyedPermissionContextTest,
                   ContentSettingsPattern::FromURLToSchemefulSitePattern(
                       identity_provider.GetURL()),
                   ContentSettingsPattern::FromURLToSchemefulSitePattern(
-                      relying_party_requester.GetURL()),
+                      relying_party_embedder.GetURL()),
                   content_settings::ContentSettingToValue(
                       ContentSetting::CONTENT_SETTING_ALLOW),
                   /*source=*/"", /*incognito=*/false)));
