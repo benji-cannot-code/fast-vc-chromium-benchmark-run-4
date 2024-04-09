d188795e75ff58e59c113acc548da28e5d8114fa
Ivana Žužić <izuzic@google.com>
2024-04-09 13:46:09 +0000

    [Tab Group Parity][Gardener] Close soft keyboard to accept empty title
    The CL was https://crrev.com/c/5419812, caused this test to be flaky:
    TabSwitcherLayoutTest#testTabGroupCreation_acceptNullTitle
    
    Explanation of the fix:
    The CL enabled autofocusing on the text input field as soon as the
    dialog to input the tab group name shows up and it was showing soft
    keyboard. That made the test flaky because sometimes the keyboard was
    late enough so it was possible to click on the positive button on the
    dialog and sometimes the keyboard was shown before Espresso click so
    Espresso could not find the positive button, even though it was on the
    screen. As explained here:
    https://yaqs.corp.google.com/eng/q/8966007151117991936, the soft
    keyboard needs to be dismissed before clicking.
    
    Bug: b/333521103, b/41490324
    Change-Id: I1f7bc0aae067f9c6eef7abbefa7e84b140d24c40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439274
    Owners-Override: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284441}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.tasks.tab_management;
 import static android.os.Build.VERSION_CODES.O_MR1;
 import static android.os.Build.VERSION_CODES.Q;
 
+import static androidx.test.espresso.Espresso.closeSoftKeyboard;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
@@ -1755,6 +1756,9 @@ public class TabSwitcherLayoutTest {
         verifyModalDialogShowingAnimationCompleteInTabSwitcher();
         onViewWaiting(withId(R.id.creation_dialog_layout), /* checkRootDialog= */ true)
                 .check(matches(isDisplayed()));
+
+        // Close the soft keyboard that appears when the dialog is shown.
+        closeSoftKeyboard();
 
         // Accept without changing the title.
         onView(withId(R.id.positive_button)).perform(click());
