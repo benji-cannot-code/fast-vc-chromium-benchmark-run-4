d941f4e33d247520272ee6c96fd45ca1cfe23740
Sarvesh Kalwit <sarveshkalwit@google.com>
2024-04-09 16:17:56 +0000

    floss: Support GATT characteristic start/stop notification callbacks
    Allow Floss to recognize when local Client Characteristic Configuration
    Descriptors are written to, so notifications about these events can be
    sent to upper browser layers.
    
    BUG=b:323943663
    TEST=passing unit tests
    
    Change-Id: I5fb8617a12876b1bcc17bd0c5e3ff1ff8c5c46dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412331
    Auto-Submit: Sarvesh Kalwit <sarveshkalwit@google.com>
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    Commit-Queue: Sarvesh Kalwit <sarveshkalwit@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284517}

--- device/bluetooth/floss/bluetooth_local_gatt_descriptor_floss.h
+++ device/bluetooth/floss/bluetooth_local_gatt_descriptor_floss.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace floss {
 
+inline constexpr uint16_t CCCD_STOP_ALL = 0x0000;
+inline constexpr uint16_t CCCD_START_NOTIFY = 0x0001;
+inline constexpr uint16_t CCCD_START_INDICATE = 0x0002;
+
 class BluetoothLocalGattCharacteristicFloss;
 
 // The BluetoothLocalGattDescriptorFloss class implements
@@ -93,6 +97,10 @@ class DEVICE_BLUETOOTH_EXPORT BluetoothLocalGattDescriptorFloss
                               std::vector<uint8_t>& value,
                               bool needs_response,
                               bool success);
+
+  // Notify browser clients if there was a change to the CCCD.
+  GattStatus HandleCccDescriptor(std::string address,
+                                 std::vector<uint8_t>& value);
 
   // Cached instance of the latest pending read/write request, if one exists.
   std::optional<GattRequest> pending_request_;
