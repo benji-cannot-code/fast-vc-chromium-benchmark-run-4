9f46430dc9adcb7a0c29e1ddbeb29e9a3c8ac26d
Steven Holte <holte@google.com>
2024-04-09 04:09:06 +0000

    Fail request checks for unsupported language.
    Bug: 332358363
    
    Change-Id: I506cb2c2691879724bca0dfe1ce9e89cc74c4f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5437159
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284275}

--- components/optimization_guide/core/model_execution/session_impl.cc
+++ components/optimization_guide/core/model_execution/session_impl.cc
@@ -453,7 +453,8 @@ void SessionImpl::OnRequestSafetyResult(
   const auto& thresholds = check.safety_category_thresholds().empty()
                                ? safety_config_->safety_category_thresholds()
                                : check.safety_category_thresholds();
-  bool is_unsafe = HasUnsafeScores(thresholds, safety_info);
+  bool is_unsafe = IsTextInUnsupportedOrUndeterminedLanguage(safety_info) ||
+                   HasUnsafeScores(thresholds, safety_info);
 
   // Log the check execution.
   auto* response_log =
