c05525f750a0457567878d2fcb5b4256f3508ac5
Karol Sygiet <sygiet@google.com>
2024-04-09 17:51:41 +0000

    [Mullhet] Add PotentialBestMatchFormType metric
    This CL introduces a new metric that will record the match form type
    for the credentials that should be used as a preferred match. That
    does not mean that the value recorded here will be used, since some
    consumers don't use credentials with `kGrouped` match type.
    This will cause the discrepancy between existing
    `PasswordManager.MatchedFormType` and the new metric.
    
    Bug: 332673987
    Change-Id: Ia81138b7bce3b0c64551f472dbcb9f5cf520557a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5431235
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1284574}

--- components/password_manager/core/browser/fake_form_fetcher.cc
+++ components/password_manager/core/browser/fake_form_fetcher.cc
@@ -133,6 +133,10 @@ FakeFormFetcher::GetAccountStoreBackendError() const {
   return account_store_backend_error_;
 }
 
+bool FakeFormFetcher::WereGroupedCredentialsAvailable() const {
+  return false;
+}
+
 void FakeFormFetcher::SetProfileStoreBackendError(
     std::optional<PasswordStoreBackendError> error) {
   profile_store_backend_error_ = error;
