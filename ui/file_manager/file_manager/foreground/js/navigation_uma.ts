1ff7015867b346e6339d4503ddd314e049275c0f
Luciano Pacheco <lucmult@chromium.org>
2024-04-09 08:38:32 +0000

    Files app: Add support to change directory for FileData without Entry
    Test: Integration test will come in a follow up CL after adding support for new scanner
    Bug: b:327533814
    Change-Id: I20ef5fd77ca16f075818c73499e3b80088c39978
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5432714
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284347}

--- ui/file_manager/file_manager/foreground/js/navigation_uma.ts
+++ ui/file_manager/file_manager/foreground/js/navigation_uma.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {VolumeManager} from '../../background/js/volume_manager.js';
+import type {UniversalDirectory} from '../../common/js/files_app_entry_types.js';
 import {recordEnum} from '../../common/js/metrics.js';
 import {RootTypesForUMA} from '../../common/js/volume_manager_types.js';
 
@@ -18,7 +19,12 @@ export class NavigationUma {
    *
    * @param entry the new directory
    */
-  onDirectoryChanged(entry: Entry) {
+  onDirectoryChanged(entry: UniversalDirectory|undefined) {
+    // TOOD(b/327533814): Refactor to work with FileData.
+    if (!entry) {
+      return;
+    }
+
     const locationInfo = this.volumeManager_.getLocationInfo(entry);
     if (locationInfo) {
       recordEnum(
