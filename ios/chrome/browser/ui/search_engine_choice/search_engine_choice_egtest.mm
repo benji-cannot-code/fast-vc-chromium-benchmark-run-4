779b1c2abe262c70041893bc31eafccb1254c89d
Asami Doi <asamidoi@chromium.org>
2024-04-09 13:38:40 +0000

    [ios] Disable testUserActionWhenExpandingSnippetChevron
    Bug: 333519732
    Change-Id: I6e15c6b07b8722eaf6f86075281944c1ce5106e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439553
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Owners-Override: Asami Doi <asamidoi@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284437}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_egtest.mm
@@ -160,7 +160,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 // Test that the snippet can be expanded or collapsed.
-- (void)testUserActionWhenExpandingSnippetChevron {
+// TODO(crbug.com/333519732): Fix this flaky test.
+- (void)FLAKY_testUserActionWhenExpandingSnippetChevron {
   // Checks that the choice screen is shown
   [SearchEngineChoiceEarlGreyUI verifySearchEngineChoiceScreenIsDisplayed];
   if ([ChromeEarlGrey isIPadIdiom]) {
