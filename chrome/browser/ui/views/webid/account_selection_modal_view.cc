9a09ca6d9a5c617eefeb22d5ad5c52f64d0bfd0b
Zachary Tan <tanzachary@chromium.org>
2024-04-09 21:23:29 +0000

    [FedCM] Focus on continue button in request permission modal dialog
    Bug: 41491333
    Change-Id: Ia386e0500554e441b704e6ac103bb38b4c98857c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440117
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284710}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/i18n/case_conversion.h"
 #include "chrome/app/vector_icons/vector_icons.h"
+#include "chrome/browser/accessibility/accessibility_state_utils.h"
 #include "chrome/browser/image_fetcher/image_decoder_impl.h"
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/ui/monogram_utils.h"
@@ -532,6 +533,14 @@ void AccountSelectionModalView::ShowRequestPermissionDialog(
           base::Unretained(observer_))));
 
   InitDialogWidget();
+
+  // Focusing `continue_button_` without screen reader on makes the UI look
+  // awkward, so we only want to do so when screen reader is enabled.
+  if (accessibility_state_utils::IsScreenReaderEnabled()) {
+    CHECK(continue_button_);
+    continue_button_->RequestFocus();
+  }
+  SendAccessibilityEvent(GetWidget());
 }
 
 std::unique_ptr<views::View>
