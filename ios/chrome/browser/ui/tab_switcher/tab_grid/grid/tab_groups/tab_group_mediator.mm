6b19903b883e106381f4632c426d8cbae73667bc
Gauthier Ambard <gambard@chromium.org>
2024-04-09 11:11:28 +0000

    [iOS] Highlight the Group if the selected cell is inside
    Change the way the selected item is found: return a group if needed for
    the main TabGrid. It is also taking into consideration the pinned
    elements as they were taken into account when the selected web state
    was updated in WebStateListDidChange.
    Make sure the Group view only return web state from itself.
    
    https://screencast.googleplex.com/cast/NjQzMzI3MTk4MTI3NzE4NHw5YjE5MTczZi03MA
    
    Fixed: 333387787
    Change-Id: I616c48f5d3016d6596f796e67c47046d7beda1e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5438393
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284388}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
@@ -109,6 +109,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         selectedItemIdentifier:identifier];
 }
 
+// Override the parent to only show individual web state in the group.
+- (GridItemIdentifier*)activeIdentifier {
+  WebStateList* webStateList = self.webStateList;
+  if (!webStateList) {
+    return nil;
+  }
+
+  int webStateIndex = webStateList->active_index();
+  if (webStateIndex == WebStateList::kInvalidIndex) {
+    return nil;
+  }
+
+  if (!_tabGroup->range().contains(webStateIndex)) {
+    return nil;
+  }
+
+  return [GridItemIdentifier
+      tabIdentifier:webStateList->GetWebStateAt(webStateIndex)];
+}
+
 // Overrides the parent observations: only observe the group `WebState`s.
 - (void)addWebStateObservations {
   for (int index : _tabGroup->range()) {
