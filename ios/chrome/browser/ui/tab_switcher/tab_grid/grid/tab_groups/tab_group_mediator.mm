c54fff5406a88db4c975254742d57a2dad96b4f6
Aliona DANGLA <alionadangla@google.com>
2024-04-09 11:21:42 +0000

    [iOS] Update tab group view after updating visual data
    See video:
    https://drive.google.com/file/d/1Fa3kY-ingH2TaFJyOIRBO5aYOjHiA3eb/view?usp=drive_link
    
    Fixed: 331913371
    Bug: 40942154
    Change-Id: Ic18a9713fa82bddce1f75fb2c09c6867588c8593
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434996
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284395}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
@@ -165,4 +165,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self removeObservationForWebState:detachedWebState];
 }
 
+// Overrides the parent observations. The parent treats a group as one cell and
+// just update it, whereas this TabGroupMediator treats them as multiples cell,
+// so this overrides manages notifications accordingly.
+- (void)didChangeWebStateList:(WebStateList*)webStateList
+                       change:(const WebStateListChange&)change
+                       status:(const WebStateListStatus&)status {
+  DCHECK_EQ(self.webStateList, webStateList);
+  if (webStateList->IsBatchInProgress()) {
+    return;
+  }
+
+  switch (change.type()) {
+    case WebStateListChange::Type::kGroupVisualDataUpdate: {
+      const WebStateListChangeGroupVisualDataUpdate& visualDataChange =
+          change.As<WebStateListChangeGroupVisualDataUpdate>();
+      const TabGroup* tabGroup = visualDataChange.updated_group();
+      if (_tabGroup != tabGroup) {
+        break;
+      }
+      [_groupConsumer setGroupTitle:tabGroup->GetTitle()];
+      [_groupConsumer setGroupColor:tabGroup->GetColor()];
+      break;
+    }
+    default:
+      [super didChangeWebStateList:webStateList change:change status:status];
+      break;
+  }
+}
+
 @end
