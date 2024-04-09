08993a5a386fc967781ab6f84dfd32c4d4a64ce3
Xiyuan Xia <xiyuan@chromium.org>
2024-04-09 18:57:45 +0000

    ash: Add a DWC to investigate unexpected LoggedInStateChanged
    b/333262357 shows a case that `LoggedInStateChanged` could be
    called unexpectedly to start the login animation timeout timer.
    Add a DWC to investigate how it happens. This should suppress
    the DWC in OnLoginAnimationFinishedTimerFired.
    
    Bug: 333262357
    Change-Id: I8d3484a5fce28a423407e7382dd5d7399369c77d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436995
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284617}

--- ash/metrics/login_unlock_throughput_recorder.cc
+++ ash/metrics/login_unlock_throughput_recorder.cc
@@ -245,6 +245,7 @@ void LoginUnlockThroughputRecorder::OnAuthSuccess() {
 }
 
 void LoginUnlockThroughputRecorder::OnAshRestart() {
+  is_ash_restart_ = true;
   login_animation_finished_timer_.Stop();
   if (!post_login_deferred_task_runner_->Started()) {
     post_login_deferred_task_runner_->Start();
@@ -268,6 +269,20 @@ void LoginUnlockThroughputRecorder::LoggedInStateChanged() {
   if (logged_in_user != LoginState::LOGGED_IN_USER_OWNER &&
       logged_in_user != LoginState::LOGGED_IN_USER_REGULAR) {
     // Kiosk users fall here.
+    return;
+  }
+
+  // On ash restart, `SessionManager::CreateSessionForRestart` should happen
+  // and trigger `LoggedInStateChanged` here to set `user_logged_in_` flag
+  // before `OnAshRestart` is called. So `is_ash_restart_` should never be true
+  // here. Otherwise, we have unexpected sequence of events and login metrics
+  // would not be correctly reported.
+  //
+  // It seems somehow happening in b/333262357. Adding a DumpWithoutCrashing
+  // to capture the offending stack.
+  // TODO(b/333262357): Remove `DumpWithoutCrashing`.
+  if (is_ash_restart_) {
+    base::debug::DumpWithoutCrashing();
     return;
   }
 
