ae917ebe2f49ef109af578e42d5f757a5a239062
jhtin <jhtin@chromium.org>
2024-04-09 06:52:51 +0000

    input method settings: Add a helper to set multiple prefs at once
    Bug: b/333041130
    Change-Id: Ia8e24c9acc05667da0431ab7dd6654b2769b9463
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427498
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284313}

--- chrome/browser/ash/input_method/input_method_settings.cc
+++ chrome/browser/ash/input_method/input_method_settings.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/autocorrect_enums.h"
 #include "chrome/browser/ash/input_method/autocorrect_prefs.h"
 #include "chrome/common/pref_names.h"
+#include "components/prefs/scoped_user_pref_update.h"
 
 namespace ash {
 namespace input_method {
@@ -412,6 +413,19 @@ mojom::InputMethodSettingsPtr CreateSettingsFromPrefs(
   // This will be something like InputMethodSettings::NewJapaneseSettings(...)
 
   return nullptr;
+}
+
+void SetLanguageInputMethodSpecificSetting(PrefService& prefs,
+                                           const std::string& engine_id,
+                                           const base::Value::Dict& values) {
+  // This creates a dictionary where any changes to the dictionary will notify
+  // the prefs service (and its observers).
+  ScopedDictPrefUpdate update = ScopedDictPrefUpdate(
+      &prefs, ::prefs::kLanguageInputMethodSpecificSettings);
+
+  for (const auto [key, value] : values) {
+    update->FindDict(engine_id)->Set(key, value.Clone());
+  }
 }
 
 bool IsAutocorrectSupported(const std::string& engine_id) {
