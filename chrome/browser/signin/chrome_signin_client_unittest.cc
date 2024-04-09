7ad583bff6681db2ce489ba3fd1c27b822ac28cf
Mikel Astiz <mastiz@chromium.org>
2024-04-09 17:57:20 +0000

    [Profiles] Clear primary account upon profile deletion
    ...instead of stopping sync-the-feature specifically.
    
    This is an improvement over the ancient
    http://codereview.chromium.org/8772028.
    
    The original requirements are captured in https://crbug.com/105813 and
    the TEST line in the patch describes some of the requirements, quoted
    here for convenience: "Create a new profile and enable sync, then delete
    that profile. Profile deletion should succeed, sync should continue to
    operate for other accounts and sync data from the deleted account
    shouldn't be affected.". Those steps continue to work after this patch.
    
    Many things have changed throughout the years and it is no longer clear
    if this call does anything useful. SyncService could immediately restart
    in transport mode and, worst case, propagate deletions to other devices,
    namely deletions triggered by Clear Browsing Data (exercised in the same
    function).
    
    A more reliable way to make sure nothing gets propagated to the server
    is to revoke sign-in credentials.
    
    On Ash, clearing the primary account isn't possible, but it is believed
    that this codepath is never used on Ash (e.g.
    DeleteProfileHelper::MaybeScheduleProfileForDeletion() is never
    invoked). As a temporary step, it is instrumented with a non-fatal
    CHECK_IS_TEST, in combination with falling back to the legacy logic for
    the unlikely case that it is reachable in the wild.
    
    Change-Id: Ib3486016e0e3c824fd1150912f6f2ab3762cd639
    Bug: 40797392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5071555
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284576}

--- chrome/browser/signin/chrome_signin_client_unittest.cc
+++ chrome/browser/signin/chrome_signin_client_unittest.cc
@@ -216,6 +216,7 @@ bool IsAlwaysAllowedSignoutSources(
     case signin_metrics::ProfileSignout::kUserClickedRevokeSyncConsentSettings:
     case signin_metrics::ProfileSignout::
         kUserClickedSignoutFromUserPolicyNotificationDialog:
+    case signin_metrics::ProfileSignout::kSignoutDuringProfileDeletion:
       return true;
   }
 }
@@ -351,7 +352,9 @@ const signin_metrics::ProfileSignout kSignoutSources[] = {
     signin_metrics::ProfileSignout::kIdleTimeoutPolicyTriggeredSignOut,
     signin_metrics::ProfileSignout::kCancelSyncConfirmationRemoveAccount,
     signin_metrics::ProfileSignout::kMovePrimaryAccount,
+    signin_metrics::ProfileSignout::kSignoutDuringProfileDeletion,
 };
+
 // kNumberOfObsoleteSignoutSources should be updated when a ProfileSignout
 // value is deprecated.
 const int kNumberOfObsoleteSignoutSources = 6;
