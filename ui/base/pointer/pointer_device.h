a7c05fab4047a023bd6fab9e303e189fd6e6bdd4
Jose Dapena Paz <jdapena@igalia.com>
2024-04-09 17:59:37 +0000

    IWYU: usage of uint8_t and uintptr_t in pointer_device.h require include
    Bug: 41455655
    Change-Id: I82934f886a36fcf6ee23bce289fb781f892620d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440253
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1284580}

--- ui/base/pointer/pointer_device.h
+++ ui/base/pointer/pointer_device.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_BASE_POINTER_POINTER_DEVICE_H_
 #define UI_BASE_POINTER_POINTER_DEVICE_H_
 
+#include <cstdint>
 #include <optional>
 #include <tuple>
 #include <vector>
