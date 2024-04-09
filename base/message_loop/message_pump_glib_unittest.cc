a8a7bd8b7c25611744899be91a2f6b1e4b12e16b
Helmut Januschka <helmut@januschka.com>
2024-04-09 01:42:16 +0000

    linux_util: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I71022d35671a6c2c882c0509f8ca57b3078bc1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405644
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284240}

--- base/message_loop/message_pump_glib_unittest.cc
+++ base/message_loop/message_pump_glib_unittest.cc
@@ -7,9 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <glib.h>
 #include <math.h>
-#include "build/build_config.h"
 
 #include <algorithm>
+#include <string_view>
 #include <vector>
 
 #include "base/files/file_util.h"
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/test/trace_event_analyzer.h"
 #include "base/threading/thread.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
@@ -761,7 +762,7 @@ void WriteFDWrapper(const int fd,
                     const char* buf,
                     int size,
                     WaitableEvent* event) {
-  ASSERT_TRUE(WriteFileDescriptor(fd, StringPiece(buf, size)));
+  ASSERT_TRUE(WriteFileDescriptor(fd, std::string_view(buf, size)));
 }
 
 }  // namespace
--- base/message_loop/message_pump_libevent_unittest.cc
+++ base/message_loop/message_pump_libevent_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 
 #include <memory>
+#include <string_view>
 #include <utility>
 
 #include "base/containers/span.h"
@@ -284,7 +285,7 @@ void WriteFDWrapper(const int fd,
                     const char* buf,
                     int size,
                     WaitableEvent* event) {
-  ASSERT_TRUE(WriteFileDescriptor(fd, StringPiece(buf, size)));
+  ASSERT_TRUE(WriteFileDescriptor(fd, std::string_view(buf, size)));
 }
 
 // Tests that MessagePumpLibevent quits immediately when it is quit from
