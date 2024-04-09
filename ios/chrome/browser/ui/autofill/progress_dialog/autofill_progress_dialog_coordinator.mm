22d4f61af0e60e1c99c737e181f87a9118891116
Siyu An <siyua@chromium.org>
2024-04-09 18:45:22 +0000

    [VCN] Finish the progress dialog with some updates
    * Implements the close button in the progress dialog alert.
    * Sets the modal presentation style to
    UIModalPresentationOverFullScreen.
    
    Screen recording: https://drive.google.com/file/d/1oNslxEDvvtpGXYf3AotZoXYoMoIL4Hi6/view?usp=sharing
    
    FWIW I noticed the BottomSheet seems to incorrectly give the focus back
    to the form after I select a card. This will cause the keyboard
    accessory showing up when the progress dialog is visible. And in the
    screen recording the second time I selected the card from KA there is
    no such issue. Shall we fix it somehow?
    
    Bug: 324606723
    Change-Id: I99684f6309f4410fe321c0bbba3466c846d8d75f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425421
    Commit-Queue: Siyu An <siyua@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284606}

--- ios/chrome/browser/ui/autofill/progress_dialog/autofill_progress_dialog_coordinator.mm
+++ ios/chrome/browser/ui/autofill/progress_dialog/autofill_progress_dialog_coordinator.mm
@@ -56,6 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   AlertViewController* alertViewController = [[AlertViewController alloc] init];
+  alertViewController.modalPresentationStyle =
+      UIModalPresentationOverFullScreen;
   _alertViewController = alertViewController;
   _mediator->SetConsumer(alertViewController);
   [self.baseViewController presentViewController:alertViewController
