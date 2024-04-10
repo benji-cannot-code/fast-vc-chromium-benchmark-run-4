6697723344e1573f07c4bf62efd3c3ae9161ef2f
John Lee <johntlee@chromium.org>
2024-04-10 21:03:01 +0000

    WebUI: Update cr-button to have simpler, more specific vars
    - Update cr-button's CSS vars to be prefix with `--cr-button-`.
    - Simplify some of the vars instead of having separate vars for each
    theme (eg. merge --text-color, text--color-action into a singular
    --cr-button-text-color).
    - Update usages of cr-button vars.
    - Remove GM2 styling.
    
    Bug: 40256212, 326116364
    Change-Id: I49b16678cbe47f40480f458e3e3f880e233e63aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5298591
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285401}

--- chrome/test/data/webui/cr_elements/cr_button_test.ts
+++ chrome/test/data/webui/cr_elements/cr_button_test.ts
@@ -148,11 +148,7 @@ suite('cr-button', function() {
     await whenPrefixSlotchange;
 
     assertEquals('8px', buttonStyle.gap);
-    assertEquals('8px', buttonStyle.padding);
-
-    document.documentElement.toggleAttribute('chrome-refresh-2023', true);
     assertEquals('8px 16px 8px 12px', buttonStyle.padding);
-    document.documentElement.removeAttribute('chrome-refresh-2023');
 
     const whenPrefixSlotRemoved =
         eventToPromise('slotchange', button.$.prefixIcon);
@@ -169,10 +165,6 @@ suite('cr-button', function() {
     await whenSuffixSlotchange;
 
     assertEquals('8px', buttonStyle.gap);
-    assertEquals('8px', buttonStyle.padding);
-
-    document.documentElement.toggleAttribute('chrome-refresh-2023', true);
     assertEquals('8px 12px 8px 16px', buttonStyle.padding);
-    document.documentElement.removeAttribute('chrome-refresh-2023');
   });
 });
