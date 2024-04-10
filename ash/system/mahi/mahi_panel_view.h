63fc11784dedadb70c138bbf4fe57421edbb511e
Kevin Radtke <kradtke@chromium.org>
2024-04-10 00:26:21 +0000

    mahi: Disable send button when processing question
    Disable the panel send button and further user input while the backend
    is processing a question.
    
    Bug: b:331808008
    Change-Id: I4645f13afe19ec9cd1fbb35a62e4fa859048aa24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5408262
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284806}

--- ash/system/mahi/mahi_panel_view.h
+++ ash/system/mahi/mahi_panel_view.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_MAHI_MAHI_PANEL_VIEW_H_
 #define ASH_SYSTEM_MAHI_MAHI_PANEL_VIEW_H_
 
+#include <optional>
+#include <string>
+
 #include "ash/ash_export.h"
 #include "ash/system/mahi/mahi_ui_controller.h"
 #include "base/memory/raw_ptr.h"
@@ -22,6 +25,7 @@ class Textfield;
 
 namespace ash {
 
+class IconButton;
 class MahiQuestionAnswerView;
 class SummaryOutlinesSection;
 
@@ -44,7 +48,10 @@ class ASH_EXPORT MahiPanelView : public views::FlexLayoutView,
                       const ui::KeyEvent& key_event) override;
 
   // MahiUiController::Observer:
+  void OnAnswerLoaded(const std::u16string& answer) override;
   void OnContentsRefreshInitiated() override;
+  void OnStateChanged(MahiUiController::State new_state,
+                      const std::optional<PayloadType>& payload) override;
 
   // Creates the header row, which includes a back button (visible only
   // in the Q&A view), the panel title, an experiment badge and a close button.
@@ -60,12 +67,13 @@ class ASH_EXPORT MahiPanelView : public views::FlexLayoutView,
   const raw_ptr<MahiUiController> ui_controller_;
 
   // Owned by the views hierarchy.
-  raw_ptr<views::View> back_button_;
-  raw_ptr<views::ImageView> content_icon_;
-  raw_ptr<views::Label> content_title_;
-  raw_ptr<MahiQuestionAnswerView> question_answer_view_;
-  raw_ptr<SummaryOutlinesSection> summary_outlines_section_;
-  raw_ptr<views::Textfield> question_textfield_;
+  raw_ptr<views::View> back_button_ = nullptr;
+  raw_ptr<views::ImageView> content_icon_ = nullptr;
+  raw_ptr<views::Label> content_title_ = nullptr;
+  raw_ptr<MahiQuestionAnswerView> question_answer_view_ = nullptr;
+  raw_ptr<SummaryOutlinesSection> summary_outlines_section_ = nullptr;
+  raw_ptr<views::Textfield> question_textfield_ = nullptr;
+  raw_ptr<IconButton> send_button_ = nullptr;
 
   base::WeakPtrFactory<MahiPanelView> weak_ptr_factory_{this};
 };
