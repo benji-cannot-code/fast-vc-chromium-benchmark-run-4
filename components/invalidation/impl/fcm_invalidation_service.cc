d429e303c5c69e33ad576a55c34caba13fa74f3e
Roland Bock <rbock@google.com>
2024-04-10 14:24:50 +0000

    Drop invalidation::TopicData
    Invalidation code is using differente data structures to represent
    topics. This CL drops TopicData to make the overall code a bit more
    uniform.
    
    Bug: b:333354294
    Change-Id: I5c4ca923acb1b96126f52b34d1923d5e696d77eb
    Tests: Existing unit tests
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434558
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Roland Bock <rbock@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285111}

--- components/invalidation/impl/fcm_invalidation_service.cc
+++ components/invalidation/impl/fcm_invalidation_service.cc
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/gcm_driver/instance_id/instance_id_driver.h"
 #include "components/invalidation/impl/fcm_network_handler.h"
 #include "components/invalidation/impl/invalidation_prefs.h"
+#include "components/invalidation/public/invalidation_util.h"
 #include "components/invalidation/public/invalidator_state.h"
-#include "components/invalidation/public/topic_data.h"
 #include "components/prefs/scoped_user_pref_update.h"
 
 namespace invalidation {
@@ -94,18 +94,18 @@ bool FCMInvalidationService::HasObserver(
 
 bool FCMInvalidationService::UpdateInterestedTopics(
     InvalidationHandler* handler,
-    const TopicSet& legacy_topic_set) {
+    const TopicSet& topic_set) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   update_was_requested_ = true;
-  DVLOG(2) << "Subscribing to topics: " << legacy_topic_set.size();
-  std::set<TopicData> topic_set;
-  for (const auto& topic_name : legacy_topic_set) {
-    topic_set.insert(TopicData(topic_name, handler->IsPublicTopic(topic_name)));
+  DVLOG(2) << "Subscribing to topics: " << topic_set.size();
+  TopicMap topic_map;
+  for (const auto& topic_name : topic_set) {
+    topic_map[topic_name] = TopicMetadata(handler->IsPublicTopic(topic_name));
   }
   // TODO(crbug.com/1054404): UpdateRegisteredTopics() should be renamed to
   // clarify that it actually updates whether topics need subscription (aka
   // interested).
-  if (!invalidator_registrar_.UpdateRegisteredTopics(handler, topic_set)) {
+  if (!invalidator_registrar_.UpdateRegisteredTopics(handler, topic_map)) {
     return false;
   }
   DoUpdateSubscribedTopicsIfNeeded();
