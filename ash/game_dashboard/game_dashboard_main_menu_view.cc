c0dcdfc8e4769516f7f855a3d807bace034fbff3
Prameet Shah <phshah@google.com>
2024-04-10 01:21:57 +0000

    gd: Set the accessibility role and title to the main menu view.
    Sets the accessibility role for the GameDashboardMainMenuView to
    `ax::mojom::Role::kDialog` and the accessibility title to "Game
    dashboard". When opening the main menu, the screen reader will announce
    "Game dashboard" instead of all the views in the dialog.
    
          dashboard" instead of all the view.
    
    Bug: b:331641112
    Test: Ran on DUT with CVox enabled, and verified CVox announces "Game
    Change-Id: I85638be7942e043c83c1870f50992951801a77e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441174
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Prameet Shah <phshah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284830}

--- ash/game_dashboard/game_dashboard_main_menu_view.cc
+++ ash/game_dashboard/game_dashboard_main_menu_view.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
+#include "ui/accessibility/ax_enums.mojom-shared.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -696,6 +697,13 @@ GameDashboardMainMenuView::GameDashboardMainMenuView(
   AddMainMenuViews();
 
   SizeToPreferredSize();
+
+  // We set the dialog role because views::BubbleDialogDelegate defaults this to
+  // an alert dialog. This would make screen readers announce every view in the
+  // main menu, which is undesirable.
+  SetAccessibleWindowRole(ax::mojom::Role::kDialog);
+  SetAccessibleTitle(l10n_util::GetStringUTF16(
+      IDS_ASH_GAME_DASHBOARD_GAME_DASHBOARD_BUTTON_TITLE));
 }
 
 GameDashboardMainMenuView::~GameDashboardMainMenuView() = default;
