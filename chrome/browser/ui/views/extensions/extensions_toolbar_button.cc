6698024b141321fbed2b728c5a4db93430b696d3
EmiliaPaz <emiliapaz@chromium.org>
2024-04-10 20:27:43 +0000

    [Extensions] Add ChromeRefresh values directly in extensions toolbar
    IsChromeRefresh() is enabled by default. Thus, we can directly add the
    GM3 values to the extensions extensions toolbar.
    
    Bug: 326156510
    
    Change-Id: I150afa4d2d344babc7b9a0aff98dfa12be6ae605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444096
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285375}

--- chrome/browser/ui/views/extensions/extensions_toolbar_button.cc
+++ chrome/browser/ui/views/extensions/extensions_toolbar_button.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/pointer/touch_ui_controller.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/button_controller.h"
@@ -31,11 +30,7 @@ namespace {
 const gfx::VectorIcon& GetIcon(ExtensionsToolbarButton::State state) {
   switch (state) {
     case ExtensionsToolbarButton::State::kDefault:
-      return (features::IsChromeRefresh2023() ||
-              base::FeatureList::IsEnabled(
-                  extensions_features::kExtensionsMenuAccessControl))
-                 ? vector_icons::kExtensionChromeRefreshIcon
-                 : vector_icons::kExtensionIcon;
+      return vector_icons::kExtensionChromeRefreshIcon;
     case ExtensionsToolbarButton::State::kAllExtensionsBlocked:
       return vector_icons::kExtensionOffIcon;
     case ExtensionsToolbarButton::State::kAnyExtensionHasAccess:
@@ -211,11 +206,7 @@ int ExtensionsToolbarButton::GetIconSize() const {
     return kDefaultTouchableIconSize;
   }
 
-  return features::IsChromeRefresh2023() ||
-                 base::FeatureList::IsEnabled(
-                     extensions_features::kExtensionsMenuAccessControl)
-             ? kDefaultIconSizeChromeRefresh
-             : kDefaultIconSize;
+  return kDefaultIconSizeChromeRefresh;
 }
 
 std::u16string ExtensionsToolbarButton::GetTooltipText(
