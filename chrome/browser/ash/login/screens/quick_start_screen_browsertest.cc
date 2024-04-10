9f8a12f1ad3a9b11d5a8a96cd46e84cb4b17e3f6
Brittany Hartmire <bhartmire@google.com>
2024-04-10 18:10:42 +0000

    [QuickStart] Record ScreenOpened for OOBE screens in middle of QS
    Test: Existing unittests pass, updated browsertest
    Change-Id: I071f4da27dcd83c790efa922867812df30027d40
    Bug: b/298042953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430480
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285271}

--- chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
@@ -576,6 +576,9 @@ IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, EndToEndWithMetrics) {
       kScreenOpenedHistogram,
       quick_start::QuickStartMetrics::ScreenName::kQSWifiCredentialsReceived,
       0);
+  histogram_tester.ExpectBucketCount(
+      kScreenOpenedHistogram,
+      quick_start::QuickStartMetrics::ScreenName::kChooseChromebookSetup, 0);
 
   EnterQuickStartFlowFromWelcomeScreen();
   histogram_tester.ExpectBucketCount(
@@ -599,6 +602,10 @@ IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, EndToEndWithMetrics) {
   test::WaitForNetworkSelectionScreen();
   SkipUpdateScreenOnBrandedBuilds();
   WaitForUserCreationAndTriggerPersonalFlow();
+
+  histogram_tester.ExpectBucketCount(
+      kScreenOpenedHistogram,
+      quick_start::QuickStartMetrics::ScreenName::kChooseChromebookSetup, 1);
 
   // The flow continues to QuickStart upon reaching the GaiaInfoScreen or
   // GaiaScreen.
