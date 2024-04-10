2c5c3b3b0121fbb15d143124ea30c10a5f29e714
Regan Hsu <hsuregan@google.com>
2024-04-10 23:25:52 +0000

    [CrOS Cellular] Add finalized strings for APN discovery dialog.
    Following discussion with string writer, APN discovery dialog strings
    were updated.
    
    Also adds a learn more link that was added during the session.
    
    Bug: b/331224974
    Test: browser_test *ApnDialog* and manually
    Change-Id: I200395884a7e43fb24056bf638bd020cae0dfd63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444376
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285502}

--- ash/webui/common/resources/network/apn_selection_dialog.js
+++ ash/webui/common/resources/network/apn_selection_dialog.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Dialog that displays APNs for a user to select to be attempted to be used.
  */
 
+import '//resources/ash/common/cr_elements/localized_link/localized_link.js';
 import '//resources/ash/common/cr_elements/cr_button/cr_button.js';
 import '//resources/ash/common/cr_elements/cr_dialog/cr_dialog.js';
 import '//resources/ash/common/cr_elements/cr_shared_style.css.js';
@@ -51,6 +52,11 @@ export class ApnSelectionDialog extends ApnSelectionDialogElementBase {
 
       /** The GUID of the network to select known APNs for. */
       guid: String,
+
+      shouldOmitLinks: {
+        type: Boolean,
+        value: false,
+      },
 
       /** @type {ApnProperties} */
       selectedApn_: {
