646e705cfb1fb42c16dd98e7eeb3591cb1844ef8
Darren Shen <shend@chromium.org>
2024-04-10 08:51:38 +0000

    Don't truncate results when searching only a specific category.
    Bug: b/333645232
    Change-Id: I00b6e3bc9750df9c2bcf64e235d9d2f6ef605c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442156
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284974}

--- ash/picker/search/picker_search_request.cc
+++ ash/picker/search/picker_search_request.cc
@@ -54,7 +54,8 @@ PickerSearchRequest::PickerSearchRequest(
     PickerClient* client,
     emoji::EmojiSearch* emoji_search,
     base::span<const PickerCategory> available_categories)
-    : client_(CHECK_DEREF(client)),
+    : is_category_specific_search_(category.has_value()),
+      client_(CHECK_DEREF(client)),
       emoji_search_(CHECK_DEREF(emoji_search)),
       current_callback_(std::move(callback)),
       gif_search_debouncer_(kGifDebouncingDelay) {
@@ -159,8 +160,9 @@ void PickerSearchRequest::HandleCrosSearchResults(
       }
 
       // TODO: b/333295235 - Check if Omnibox results were truncated.
-      HandleSearchSourceResults(PickerSearchSource::kOmnibox,
-                                std::move(results), /*has_more_results=*/true);
+      HandleSearchSourceResults(
+          PickerSearchSource::kOmnibox, std::move(results),
+          /*has_more_results=*/!is_category_specific_search_);
       break;
     case AppListSearchResultType::kDriveSearch: {
       if (cros_search_start_.has_value()) {
@@ -168,7 +170,9 @@ void PickerSearchRequest::HandleCrosSearchResults(
         base::UmaHistogramTimes("Ash.Picker.Search.DriveProvider.QueryTime",
                                 elapsed);
       }
-      size_t files_to_remove = std::max<size_t>(results.size(), 3) - 3;
+      size_t files_to_remove = is_category_specific_search_
+                                   ? 0
+                                   : std::max<size_t>(results.size(), 3) - 3;
       results.erase(results.end() - files_to_remove, results.end());
 
       HandleSearchSourceResults(PickerSearchSource::kDrive, std::move(results),
@@ -181,7 +185,9 @@ void PickerSearchRequest::HandleCrosSearchResults(
         base::UmaHistogramTimes("Ash.Picker.Search.FileProvider.QueryTime",
                                 elapsed);
       }
-      size_t files_to_remove = std::max<size_t>(results.size(), 3) - 3;
+      size_t files_to_remove = is_category_specific_search_
+                                   ? 0
+                                   : std::max<size_t>(results.size(), 3) - 3;
       results.erase(results.end() - files_to_remove, results.end());
 
       HandleSearchSourceResults(PickerSearchSource::kLocalFile,
