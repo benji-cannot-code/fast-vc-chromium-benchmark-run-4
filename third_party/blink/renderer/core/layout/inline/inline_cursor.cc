52ac6320ffd837335491900c00a8d44508ff1381
Mason Freed <masonf@chromium.org>
2024-04-10 18:01:33 +0000

    Add some DCHECKs for item_index going negative
    crbug.com/332736862 says it can go negative somewhere, so this
    adds a few more DCHECKs to catch where.
    
    Bug: 332736862
    Change-Id: I92a008889ff026b6a46f8cca54e3ac4de5caf6a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439597
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285264}

--- third_party/blink/renderer/core/layout/inline/inline_cursor.cc
+++ third_party/blink/renderer/core/layout/inline/inline_cursor.cc
@@ -1432,6 +1432,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
     return;
   }
   // |FirstInlineFragmentItemIndex| is 1-based. Convert to 0-based index.
+  DCHECK_GT(item_index, 0UL);
   --item_index;
 
   // Find |FragmentItems| that contains |item_index|.
@@ -1443,6 +1444,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
       if (!Current())
         return;
     }
+    DCHECK_GE(item_index, fragment_items_->SizeOfEarlierFragments());
     item_index -= fragment_items_->SizeOfEarlierFragments();
 #if EXPENSIVE_DCHECKS_ARE_ON()
     InlineCursor check_cursor(*root_block_flow_);
@@ -1454,6 +1456,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
     // If |this| is not rooted at |LayoutBlockFlow|, iterate |FragmentItems|
     // from |LayoutBlockFlow|.
     if (fragment_items_->HasItemIndex(item_index)) {
+      DCHECK_GE(item_index, fragment_items_->SizeOfEarlierFragments());
       item_index -= fragment_items_->SizeOfEarlierFragments();
     } else {
       InlineCursor cursor;
@@ -1465,6 +1468,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
           return;
         }
         if (cursor.fragment_items_ == fragment_items_) {
+          DCHECK_GE(cursor.Current().Item(), fragment_items_->Items().data());
           item_index = base::checked_cast<wtf_size_t>(
               cursor.Current().Item() - fragment_items_->Items().data());
           break;
@@ -1497,6 +1501,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
         MakeNull();
         return;
       }
+      DCHECK_GE(item_index, span_begin_item_index);
       item_index -= span_begin_item_index;
     }
   }
