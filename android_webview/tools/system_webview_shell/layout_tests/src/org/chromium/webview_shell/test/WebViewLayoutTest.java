e1b366cee7aa493dd6b3e79314b9434f70fd3f7c
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-04-10 08:34:29 +0000

    [wv-codestyle] Chain append calls on StringBuilder
    Change-Id: Ibcb8bdb43cc5167a149bba61df2ee597e034a6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440033
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284967}

--- android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
+++ android_webview/tools/system_webview_shell/layout_tests/src/org/chromium/webview_shell/test/WebViewLayoutTest.java
@@ -165,7 +165,7 @@ public class WebViewLayoutTest {
         // Check that each current webview interface is one of webview expected interfaces.
         for (String interfaceS : webviewInterfacesMap.keySet()) {
             if (webviewExpectedInterfacesMap.get(interfaceS) == null) {
-                newInterfaces.append(interfaceS + "\n");
+                newInterfaces.append(interfaceS).append("\n");
             }
         }
 
@@ -242,7 +242,7 @@ public class WebViewLayoutTest {
                             NOT_WEBVIEW_EXPOSED_CHROMIUM_PATH));
             interfacesNotExposedInWebview.forEach(
                     illegallyExposedInterface -> {
-                        errorMessage.append("\t- " + illegallyExposedInterface + "\n");
+                        errorMessage.append("\t- ").append(illegallyExposedInterface).append("\n");
                     });
         }
         String errorTemplate =
@@ -266,7 +266,7 @@ public class WebViewLayoutTest {
                                     NOT_WEBVIEW_EXPOSED_CHROMIUM_PATH));
                     propertiesNotExposed.forEach(
                             propertyNotExposed -> {
-                                errorMessage.append("\t- " + propertyNotExposed + "\n");
+                                errorMessage.append("\t- ").append(propertyNotExposed).append("\n");
                             });
                 });
         if (errorMessage.length() > 0) {
@@ -352,7 +352,8 @@ public class WebViewLayoutTest {
                             """,
                             NOT_WEBVIEW_EXPOSED_CHROMIUM_PATH));
             missingInterfaces.forEach(
-                    (missingInterface) -> errorMessage.append("\t- " + missingInterface + "\n"));
+                    (missingInterface) ->
+                            errorMessage.append("\t- ").append(missingInterface).append("\n"));
         }
         String errorTemplate =
                 """
@@ -372,7 +373,10 @@ public class WebViewLayoutTest {
                                     NOT_WEBVIEW_EXPOSED_CHROMIUM_PATH));
                     missingProperties.forEach(
                             (missingProperty) ->
-                                    errorMessage.append("\t- " + missingProperty + "\n"));
+                                    errorMessage
+                                            .append("\t- ")
+                                            .append(missingProperty)
+                                            .append("\n"));
                 });
         Assert.assertTrue(errorMessage.toString(), errorMessage.length() == 0);
     }
