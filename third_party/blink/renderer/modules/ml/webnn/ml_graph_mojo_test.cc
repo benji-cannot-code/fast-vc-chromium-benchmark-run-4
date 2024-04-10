12f7b2acaa19dd95c61f2b912f8afe946e8c571d
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-04-10 08:36:26 +0000

    Revert "webnn: Fall back to WebNN service if built without XNNPACK"
    This reverts commit 4e4028aa878e864c4b887a32ec343057c32c58c7.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5651734418423808
    
    Sample build with failed test: https://ci.chromium.org/b/8751077078639134417
    Affected test(s):
    [ninja://:blink_wpt_tests/external/wpt/webnn/conformance_tests/pad.https.any.html](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_wpt_tests%2Fexternal%2Fwpt%2Fwebnn%2Fconformance_tests%2Fpad.https.any.html?q=VHash%3Aee7ed2231ecfebc6)
    [ninja://:blink_wpt_tests/external/wpt/webnn/conformance_tests/pad.https.any.worker.html](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_wpt_tests%2Fexternal%2Fwpt%2Fwebnn%2Fconformance_tests%2Fpad.https.any.worker.html?q=VHash%3Aee7ed2231ecfebc6)
    [ninja://:blink_wpt_tests/external/wpt/webnn/conformance_tests/softmax.https.any.html](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_wpt_tests%2Fexternal%2Fwpt%2Fwebnn%2Fconformance_tests%2Fsoftmax.https.any.html?q=VHash%3Aee7ed2231ecfebc6)
    [ninja://:blink_wpt_tests/external/wpt/webnn/conformance_tests/softmax.https.any.worker.html](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2F:blink_wpt_tests%2Fexternal%2Fwpt%2Fwebnn%2Fconformance_tests%2Fsoftmax.https.any.worker.html?q=VHash%3Aee7ed2231ecfebc6)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5651734418423808&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5439712&type=BUG
    
    Original change's description:
    > webnn: Fall back to WebNN service if built without XNNPACK
    >
    > Currently, the developer must specify a "gpu" or "npu" device type to
    > target the WebNN Service. This CL allows for targeting the WebNN Service
    > regardless of the device type (i.e. "cpu", too)
    >
    > Note that the WebNN Service must still be explicitly enabled to be used
    >
    > Bug: 325612086, 41486052
    > Change-Id: I3b7471b4f2f7f27a7f732406f8249deac42f5a51
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439712
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Austin Sullivan <asully@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1284808}
    >
    
    Bug: 325612086, 41486052
    Change-Id: Id3e1d4c936823a5b54133ef5e4e38afc7a037f87
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442549
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Owners-Override: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284968}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_mojo_test.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_mojo_test.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/memory/raw_ref.h"
 #include "base/test/scoped_feature_list.h"
-#include "components/ml/webnn/features.mojom-blink.h"
+#include "components/ml/webnn/features.mojom-features.h"
 #include "mojo/public/cpp/base/big_buffer.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
+#include "third_party/blink/public/common/features.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_tester.h"
@@ -39,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 namespace blink_mojom = webnn::mojom::blink;
+namespace webnn_features = webnn::mojom::features;
 
 // Helper struct to create faked mojom result of inference.
 struct ComputeResult {
@@ -50,7 +52,8 @@ class FakeWebNNBuffer;
 class MLGraphTestMojo : public MLGraphTestBase {
  public:
   MLGraphTestMojo()
-      : scoped_feature_list_(webnn::mojom::features::kWebMachineLearningNeuralNetwork) {}
+      : scoped_feature_list_(webnn_features::kWebMachineLearningNeuralNetwork) {
+  }
 
   void SetGraphInfo(blink_mojom::GraphInfoPtr graph_info) {
     graph_info_ = std::move(graph_info);
