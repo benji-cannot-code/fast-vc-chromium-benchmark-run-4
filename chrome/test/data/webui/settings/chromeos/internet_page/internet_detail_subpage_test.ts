f6df59712e53d5460bda17c961a98972f0da5903
Gordon Seto <gordonseto@google.com>
2024-04-10 19:54:28 +0000

    [CrOS Cellular] Expose AllowApnModification in CrosNetworkConfig
    Add allow_apn_modification to CrosNetworkConfig's mojo interface and
    populate the property.
    
    Bug: b:333100319
    Test: *CrosNetworkConfig*
    Change-Id: I6a8c720123b95d4267101b6dccd536072a030b15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441651
    Auto-Submit: Gordon Seto <gordonseto@google.com>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Regan Hsu <hsuregan@chromium.org>
    Commit-Queue: Regan Hsu <hsuregan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285340}

--- chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_test.ts
+++ chrome/test/data/webui/settings/chromeos/internet_page/internet_detail_subpage_test.ts
@@ -238,6 +238,7 @@ suite('<settings-internet-detail-subpage>', () => {
 
   function getDefaultGlobalPolicy(): GlobalPolicy {
     return {
+      allowApnModification: false,
       allowOnlyPolicyWifiNetworksToConnect: false,
       allowCellularSimLock: false,
       allowCellularHotspot: false,
--- chromeos/ash/services/network_config/cros_network_config_unittest.cc
+++ chromeos/ash/services/network_config/cros_network_config_unittest.cc
@@ -4160,6 +4160,7 @@ TEST_F(CrosNetworkConfigTest, GetGlobalPolicy) {
   base::RunLoop().RunUntilIdle();
   mojom::GlobalPolicyPtr policy = GetGlobalPolicy();
   ASSERT_TRUE(policy);
+  EXPECT_TRUE(policy->allow_apn_modification);
   EXPECT_TRUE(policy->allow_cellular_sim_lock);
   EXPECT_FALSE(policy->allow_only_policy_cellular_networks);
   EXPECT_TRUE(policy->allow_only_policy_networks_to_autoconnect);
@@ -4180,6 +4181,7 @@ TEST_F(CrosNetworkConfigTest, GlobalPolicyApplied) {
   EXPECT_EQ(0, observer()->GetPolicyAppliedCount(/*userhash=*/std::string()));
 
   base::Value::Dict global_config;
+  global_config.Set(::onc::global_network_config::kAllowAPNModification, false);
   global_config.Set(::onc::global_network_config::kAllowCellularSimLock, false);
   global_config.Set(::onc::global_network_config::kAllowCellularHotspot, false);
   global_config.Set(
@@ -4192,6 +4194,7 @@ TEST_F(CrosNetworkConfigTest, GlobalPolicyApplied) {
   base::RunLoop().RunUntilIdle();
   mojom::GlobalPolicyPtr policy = GetGlobalPolicy();
   ASSERT_TRUE(policy);
+  EXPECT_TRUE(policy->allow_apn_modification);
   EXPECT_FALSE(policy->allow_cellular_sim_lock);
   EXPECT_FALSE(policy->allow_cellular_hotspot);
   EXPECT_TRUE(policy->allow_only_policy_cellular_networks);
@@ -4205,6 +4208,10 @@ TEST_F(CrosNetworkConfigTest, GlobalPolicyApplied) {
   EXPECT_EQ(mojom::SuppressionType::kUnset, policy->allow_text_messages);
 
   EXPECT_EQ(1, observer()->GetPolicyAppliedCount(/*userhash=*/std::string()));
+
+  feature_list.InitAndEnableFeature(features::kApnPolicies);
+  policy = GetGlobalPolicy();
+  EXPECT_FALSE(policy->allow_apn_modification);
 }
 
 TEST_F(CrosNetworkConfigTest,
