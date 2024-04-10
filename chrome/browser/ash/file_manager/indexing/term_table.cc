52ca44eff79575ac69bb69a368d1d8968fadac6c
Bo Majewski <majewski@chromium.org>
2024-04-10 06:57:19 +0000

    Reland "[Files]: Adds table for storing file info"
    This is a reland of commit 18169b92ade14a403e094ae9ddfc504daebaee78
    
    Based on a simpler implementation of the in-memory index.
    Added destruction of the index after each test.
    
    Original change's description:
    > [Files]: Adds table for storing file info
    >
    > Adds a new table that stores file info. File info is stored by the URL
    > ID. Adds APIs for getting, putting, and deleting file info.
    >
    > Bug: b:327535200
    > Change-Id: I9dc6c6e9c15d59eb6db8307f775334527ca077da
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5409883
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Bo Majewski <majewski@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1282976}
    
    Bug: b:327535200
    Change-Id: Id78a1d7535507076b1e3ff1f2b2734a712d04683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5433174
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284935}

--- chrome/browser/ash/file_manager/indexing/term_table.cc
+++ chrome/browser/ash/file_manager/indexing/term_table.cc
@@ -54,7 +54,10 @@ int64_t TermTable::DeleteTerm(const std::string& term) {
 }
 
 int64_t TermTable::GetTermId(const std::string& term, bool create) {
-  return GetValueId(term, create);
+  if (create) {
+    return GetOrCreateValueId(term);
+  }
+  return GetValueId(term);
 }
 
 std::unique_ptr<sql::Statement> TermTable::MakeGetStatement() const {
