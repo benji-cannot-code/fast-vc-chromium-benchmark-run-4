94c5fa4a0722a3180d21ae916974079bc2d138b8
Fredrik Söderquist <fs@opera.com>
2024-04-10 13:34:51 +0000

    Rework SVGResourceDocumentContent to be the ResourceClient
    This separates the load-cycle of the underlying Resource from the load-
    cycle of the SVGResourceDocumentContent. In a future CL they will no
    longer be the same because additional steps need to be taken before
    loading can be considered completed in some cases (c.f SVGImage and the
    loading of data URL resources).
    
    A new TextResource resource subclass is introduced -
    SVGDocumentResource - to manage the loading and essentially do the work
    of the ResourceClient. It feeds data and status updates into the
    associated SVGResourceDocumentContent.
    
    Bug: 40134477
    Change-Id: Ide967d0d4a315c9205bdc86ece646c5726e8ab19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419515
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285089}

--- third_party/blink/renderer/core/svg/svg_use_element.h
+++ third_party/blink/renderer/core/svg/svg_use_element.h
@@ -27,9 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/svg/svg_geometry_element.h"
 #include "third_party/blink/renderer/core/svg/svg_graphics_element.h"
+#include "third_party/blink/renderer/core/svg/svg_resource_document_observer.h"
 #include "third_party/blink/renderer/core/svg/svg_uri_reference.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/loader/fetch/resource_client.h"
 #include "third_party/blink/renderer/platform/scheduler/public/post_cancellable_task.h"
 
 namespace blink {
@@ -39,7 +39,7 @@ class SVGResourceDocumentContent;
 
 class SVGUseElement final : public SVGGraphicsElement,
                             public SVGURIReference,
-                            public ResourceClient {
+                            public SVGResourceDocumentObserver {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -105,8 +105,11 @@ class SVGUseElement final : public SVGGraphicsElement,
                               const SVGElement& new_target) const;
 
   void QueueOrDispatchPendingEvent(const AtomicString&);
-  void NotifyFinished(Resource*) override;
-  String DebugName() const override;
+
+  // SVGResourceDocumentObserver:
+  void ResourceNotifyFinished(SVGResourceDocumentContent*) override;
+
+  void UpdateDocumentContent(SVGResourceDocumentContent*);
   void UpdateTargetReference();
 
   SVGAnimatedPropertyBase* PropertyFromAttribute(
