412ce08df5785639c34e2e109a139e44dca4adcf
Greg Thompson <grt@chromium.org>
2024-04-10 07:44:14 +0000

    [debug] Use cstring_view in base::debug::StackTrace
    Bug: None
    Change-Id: I1d8e06d7f5d7b901418399c633fab8c92124250d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439432
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284953}

--- base/debug/stack_trace_unittest.cc
+++ base/debug/stack_trace_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_handle.h"
 #include "base/profiler/stack_buffer.h"
 #include "base/profiler/stack_copier.h"
+#include "base/strings/cstring_view.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -134,14 +135,14 @@ TEST_F(StackTraceTest, DebugPrintWithPrefixBacktrace) {
 // Make sure nullptr prefix doesn't crash. Output not examined, much
 // like the DebugPrintBacktrace test above.
 TEST_F(StackTraceTest, DebugPrintWithNullPrefixBacktrace) {
-  StackTrace().PrintWithPrefix(nullptr);
+  StackTrace().PrintWithPrefix({});
 }
 
 // Test OutputToStreamWithPrefix, mainly to make sure it doesn't
 // crash. Any "real" stack trace testing happens above.
 TEST_F(StackTraceTest, DebugOutputToStreamWithPrefix) {
   StackTrace trace;
-  const char* prefix_string = "[test]";
+  cstring_view prefix_string = "[test]";
   std::ostringstream os;
   trace.OutputToStreamWithPrefix(&os, prefix_string);
   std::string backtrace_message = os.str();
@@ -155,8 +156,8 @@ TEST_F(StackTraceTest, DebugOutputToStreamWithPrefix) {
 TEST_F(StackTraceTest, DebugOutputToStreamWithNullPrefix) {
   StackTrace trace;
   std::ostringstream os;
-  trace.OutputToStreamWithPrefix(&os, nullptr);
-  trace.ToStringWithPrefix(nullptr);
+  trace.OutputToStreamWithPrefix(&os, {});
+  trace.ToStringWithPrefix({});
 }
 
 #endif  // !defined(__UCLIBC__) && !defined(_AIX)
