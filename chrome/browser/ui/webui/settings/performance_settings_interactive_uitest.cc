93f3a4a5f9eb3197d25a524aa4a3a4ccf1f4b4bb
Alison Gale <agale@chromium.org>
2024-04-10 01:06:41 +0000

    Move webui test mixin to shared directory for broader use
    This will be used for some infobar interactive tests so it makes sense
    for it to not be in the performance controls directory.
    
    Change-Id: Ic1759fbe76569148faa2a5c2ecb2912be6d3f1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436137
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284822}

--- chrome/browser/ui/webui/settings/performance_settings_interactive_uitest.cc
+++ chrome/browser/ui/webui/settings/performance_settings_interactive_uitest.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/performance_controls/test_support/battery_saver_browser_test_mixin.h"
 #include "chrome/browser/ui/performance_controls/test_support/memory_saver_interactive_test_mixin.h"
-#include "chrome/browser/ui/performance_controls/test_support/webui_interactive_test_mixin.h"
+#include "chrome/browser/ui/webui/test_support/webui_interactive_test_mixin.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/feedback/feedback_dialog.h"
