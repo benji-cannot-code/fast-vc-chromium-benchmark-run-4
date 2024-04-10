1747da042576a2fb61825bc5f28d2d14b526f2cd
Christy Chen <chrche@microsoft.com>
2024-04-10 00:16:22 +0000

    Add WebSocketSend events to DevTools tracing
    Include WebSocketSend events to DevTools tracing, so we can display
    WebSocket message activities in the DevTools timeline.
    
    Design Doc: https://bit.ly/3VvcBP7
    
    Bug: 331351979
    Change-Id: If5ce12fa177b357032083017af64471df10d6be7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399496
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Christy Chen <chrche@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1284795}

--- third_party/blink/renderer/modules/websockets/inspector_websocket_events.cc
+++ third_party/blink/renderer/modules/websockets/inspector_websocket_events.cc
@@ -17,15 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void InspectorWebSocketCreateEvent::Data(perfetto::TracedValue context,
-                                         ExecutionContext* execution_context,
-                                         uint64_t identifier,
-                                         const KURL& url,
-                                         const String& protocol) {
-  auto dict = std::move(context).WriteDictionary();
+void AddCommonData(ExecutionContext* execution_context,
+                   uint64_t identifier,
+                   perfetto::TracedDictionary& dict) {
   DCHECK(execution_context->IsContextThread());
   dict.Add("identifier", identifier);
-  dict.Add("url", url.GetString());
   if (auto* window = DynamicTo<LocalDOMWindow>(execution_context)) {
     dict.Add("frame", IdentifiersFactory::FrameId(window->GetFrame()));
   } else if (auto* scope = DynamicTo<WorkerGlobalScope>(execution_context)) {
@@ -35,6 +31,16 @@ void InspectorWebSocketCreateEvent::Data(perfetto::TracedValue context,
     NOTREACHED()
         << "WebSocket is available only in Window and WorkerGlobalScope";
   }
+}
+
+void InspectorWebSocketCreateEvent::Data(perfetto::TracedValue context,
+                                         ExecutionContext* execution_context,
+                                         uint64_t identifier,
+                                         const KURL& url,
+                                         const String& protocol) {
+  auto dict = std::move(context).WriteDictionary();
+  AddCommonData(execution_context, identifier, dict);
+  dict.Add("url", url.GetString());
   if (!protocol.IsNull())
     dict.Add("webSocketProtocol", protocol);
   SetCallStack(execution_context->GetIsolate(), dict);
@@ -43,18 +49,18 @@ void InspectorWebSocketCreateEvent::Data(perfetto::TracedValue context,
 void InspectorWebSocketEvent::Data(perfetto::TracedValue context,
                                    ExecutionContext* execution_context,
                                    uint64_t identifier) {
-  DCHECK(execution_context->IsContextThread());
   auto dict = std::move(context).WriteDictionary();
-  dict.Add("identifier", identifier);
-  if (auto* window = DynamicTo<LocalDOMWindow>(execution_context)) {
-    dict.Add("frame", IdentifiersFactory::FrameId(window->GetFrame()));
-  } else if (auto* scope = DynamicTo<WorkerGlobalScope>(execution_context)) {
-    dict.Add("workerId", IdentifiersFactory::IdFromToken(
-                             scope->GetThread()->GetDevToolsWorkerToken()));
-  } else {
-    NOTREACHED()
-        << "WebSocket is available only in Window and WorkerGlobalScope";
-  }
+  AddCommonData(execution_context, identifier, dict);
+  SetCallStack(execution_context->GetIsolate(), dict);
+}
+
+void InspectorWebSocketTransferEvent::Data(perfetto::TracedValue context,
+                                           ExecutionContext* execution_context,
+                                           uint64_t identifier,
+                                           uint64_t data_length) {
+  auto dict = std::move(context).WriteDictionary();
+  AddCommonData(execution_context, identifier, dict);
+  dict.Add("dataLength", data_length);
   SetCallStack(execution_context->GetIsolate(), dict);
 }
 
