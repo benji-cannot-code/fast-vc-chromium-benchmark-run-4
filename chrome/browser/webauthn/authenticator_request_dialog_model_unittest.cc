chromium/chromium

commit 5a4f4fb43d7597f178d453c003d869fc34c59bff

webauthn: splitting the model, step 3.
Update views to only look at the model, and update tests to know the
difference.

Bug: 330750582
Change-Id: I1932201001b51cd29235cb1024cec73d7bccf5b3
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5384592
Reviewed-by: Nina Satragno <nsatragno@chromium.org>
Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
Commit-Queue: Adam Langley <agl@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1279569}

chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -447,7 +447,7 @@ AuthenticatorRequestDialogModel::AuthenticatorRequestDialogModel(
 
 AuthenticatorRequestDialogModel::~AuthenticatorRequestDialogModel() {
   for (auto& observer : observers) {
-    observer.OnModelDestroyed(/*should be `this`*/ controller_);
+    observer.OnModelDestroyed(this);
   }
 }
 
@@ -465,15 +465,15 @@ void AuthenticatorRequestDialogModel::SetStep(Step step) {
         break;
 
       case StepUIType::DIALOG:
-        ShowAuthenticatorRequestDialog(web_contents, controller_);
+        ShowAuthenticatorRequestDialog(web_contents, this);
         break;
 
       case StepUIType::BUBBLE:
-        ShowAuthenticatorRequestBubble(web_contents, controller_);
+        ShowAuthenticatorRequestBubble(web_contents, this);
         break;
 
       case StepUIType::WINDOW:
-        ShowAuthenticatorRequestWindow(controller_);
+        ShowAuthenticatorRequestWindow(this);
         break;
     }
   }
@@ -505,6 +505,14 @@ bool AuthenticatorRequestDialogModel::should_dialog_be_closed() const {
 
 bool AuthenticatorRequestDialogModel::should_bubble_be_closed() const {
   return step_ui_type(step_) != StepUIType::BUBBLE;
+}
+
+std::optional<std::u16string>
+AuthenticatorRequestDialogModel::GetPriorityPhoneName() const {
+  if (!priority_phone_index) {
+    return std::nullopt;
+  }
+  return base::UTF8ToUTF16(paired_phone_names.at(*priority_phone_index));
 }
 
 #define AUTHENTICATOR_REQUEST_EVENT_0(name)      \
@@ -725,7 +733,7 @@ AuthenticatorRequestDialogController::paired_phone_names() const {
 #endif
 
 void AuthenticatorRequestDialogController::OnModelDestroyed(
-    AuthenticatorRequestDialogController* controller) {
+    AuthenticatorRequestDialogModel* model) {
   // This stops the destructor of this object from trying to remove itself from
   // the list of observers. But this is not a valid state for this object to be
   // in: many functions will crash. So this is just to make destroying the two
@@ -1696,14 +1704,6 @@ void AuthenticatorRequestDialogController::ContactPhoneForTesting(
   ContactPhone(name);
 }
 
-std::optional<std::u16string>
-AuthenticatorRequestDialogController::GetPriorityPhoneName() const {
-  if (!model_->priority_phone_index) {
-    return std::nullopt;
-  }
-  return base::UTF8ToUTF16(paired_phones_[*model_->priority_phone_index]->name);
-}
-
 void AuthenticatorRequestDialogController::StartTransportFlowForTesting(
     AuthenticatorTransport transport) {
   StartGuidedFlowForTransport(transport);
@@ -2318,7 +2318,8 @@ void AuthenticatorRequestDialogController::PopulateMechanisms() {
   const bool is_get_assertion = transport_availability_.request_type ==
                                 device::FidoRequestType::kGetAssertion;
   model_->priority_phone_index = GetIndexOfMostRecentlyUsedPhoneFromSync();
-  std::optional<std::u16string> priority_phone_name = GetPriorityPhoneName();
+  std::optional<std::u16string> priority_phone_name =
+      model_->GetPriorityPhoneName();
   bool list_phone_passkeys =
       is_get_assertion && model_->priority_phone_index &&
       base::FeatureList::IsEnabled(syncer::kSyncWebauthnCredentials);

chrome/browser/webauthn/authenticator_request_dialog_model.h
@@ -171,7 +171,7 @@ class AuthenticatorRequestDialogController;
   AUTHENTICATOR_REQUEST_EVENT_1(OnHavePIN, std::u16string)                       \
   /* Called just before the model is destructed. */                              \
   AUTHENTICATOR_REQUEST_EVENT_1(OnModelDestroyed,                                \
-                                AuthenticatorRequestDialogController*)
+                                AuthenticatorRequestDialogModel*)
 
 struct AuthenticatorRequestDialogModel {
   // Each Step defines a unique UI state. Setting a Step causes the matching
@@ -426,6 +426,11 @@ struct AuthenticatorRequestDialogModel {
   // Similar to above, but for bubbles.
   bool should_bubble_be_closed() const;
 
+  // Returns the name of the "priority" paired phone. This is the phone from
+  // sync if there are a priori discovered GPM passkeys, or the first phone on
+  // the list otherwise.
+  std::optional<std::u16string> GetPriorityPhoneName() const;
+
   const std::optional<content::GlobalRenderFrameHostId> frame_host_id;
   device::FidoRequestType request_type = device::FidoRequestType::kGetAssertion;
   device::ResidentKeyRequirement resident_key_requirement =
@@ -576,7 +581,7 @@ class AuthenticatorRequestDialogController
   std::vector<std::string> paired_phone_names() const;
 #endif
 
-  void OnModelDestroyed(AuthenticatorRequestDialogController* model) override;
+  void OnModelDestroyed(AuthenticatorRequestDialogModel* model) override;
 
   // Hides the dialog. A subsequent call to SetCurrentStep() will unhide it.
   void HideDialog();
@@ -823,11 +828,6 @@ class AuthenticatorRequestDialogController
   // Only for unittests. UI should use |mechanisms()| to enumerate the
   // user-visible mechanisms and use the callbacks therein.
   void ContactPhoneForTesting(const std::string& name);
-
-  // Returns the name of the "priority" paired phone. This is the phone from
-  // sync if there are a priori discovered GPM passkeys, or the first phone on
-  // the list otherwise.
-  virtual std::optional<std::u16string> GetPriorityPhoneName() const;
 
   // StartTransportFlowForTesting moves the UI to focus on the given transport.
   // UI should use |mechanisms()| to enumerate the user-visible mechanisms and

chrome/browser/ui/webauthn/sheet_models_unittest.cc
@@ -5,40 +5,28 @@
 #include "chrome/browser/ui/webauthn/sheet_models.h"
 
 #include "base/functional/callback_helpers.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
 #include "chrome/browser/webauthn/authenticator_transport.h"
 #include "chrome/grit/generated_resources.h"
 #include "device/fido/fido_types.h"
-#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gmock/include/gmock/gmock-matchers.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
 
-using Mechanism = AuthenticatorRequestDialogController::Mechanism;
+using Mechanism = AuthenticatorRequestDialogModel::Mechanism;
 using MechanismVisibility =
     AuthenticatorGenericErrorSheetModel::OtherMechanismButtonVisibility;
 
 class AuthenticatorSheetBaseTest : public testing::Test {};
 
-class MockDialogModel : public AuthenticatorRequestDialogController {
- public:
-  MockDialogModel()
-      : AuthenticatorRequestDialogController(
-            static_cast<content::RenderFrameHost*>(nullptr)) {}
-
-  MOCK_METHOD(base::span<const Mechanism>, mechanisms, (), (const));
-  MOCK_METHOD(std::optional<std::u16string>,
-              GetPriorityPhoneName,
-              (),
-              (const override));
-};
-
 class TestAuthenticatorSheetModel : public AuthenticatorSheetModelBase {
  public:
   TestAuthenticatorSheetModel(
-      AuthenticatorRequestDialogController* dialog_model,
+      AuthenticatorRequestDialogModel* dialog_model,
       OtherMechanismButtonVisibility other_mechanism_button_visibility)
       : AuthenticatorSheetModelBase(dialog_model,
                                     other_mechanism_button_visibility) {
@@ -58,49 +46,47 @@ class TestAuthenticatorSheetModel : public AuthenticatorSheetModelBase {
 };
 
 TEST_F(AuthenticatorSheetBaseTest, IsOtherMechanismButtonVisible) {
-  MockDialogModel dialog_model;
+  AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
 
   // No mechanisms present.
   {
     TestAuthenticatorSheetModel sheet_model(&dialog_model,
                                             MechanismVisibility::kVisible);
-    std::vector<Mechanism> mechanisms;
-    EXPECT_CALL(dialog_model, mechanisms)
-        .WillOnce(testing::Return(base::make_span(mechanisms)));
+    dialog_model.mechanisms.clear();
     EXPECT_FALSE(sheet_model.IsOtherMechanismButtonVisible());
-    testing::Mock::VerifyAndClearExpectations(&dialog_model);
   }
 
   // Two mechanisms present.
   {
     TestAuthenticatorSheetModel sheet_model(&dialog_model,
                                             MechanismVisibility::kVisible);
-    std::vector<Mechanism> mechanisms;
-    mechanisms.emplace_back(Mechanism::Phone("phone"), u"phone", u"ph",
-                            kPasskeyAoaIcon, base::DoNothing());
-    mechanisms.emplace_back(
+    dialog_model.mechanisms.clear();
+    dialog_model.mechanisms.emplace_back(Mechanism::Phone("phone"), u"phone",
+                                         u"ph", kPasskeyAoaIcon,
+                                         base::DoNothing());
+    dialog_model.mechanisms.emplace_back(
         Mechanism::Transport(AuthenticatorTransport::kUsbHumanInterfaceDevice),
         u"security key", u"usb", kPasskeyAoaIcon, base::DoNothing());
-    EXPECT_CALL(dialog_model, mechanisms)
-        .WillOnce(testing::Return(base::make_span(mechanisms)));
     EXPECT_TRUE(sheet_model.IsOtherMechanismButtonVisible());
-    testing::Mock::VerifyAndClearExpectations(&dialog_model);
   }
 
   // Hidden button.
   {
     TestAuthenticatorSheetModel sheet_model(&dialog_model,
                                             MechanismVisibility::kHidden);
-    EXPECT_CALL(dialog_model, mechanisms).Times(0);
+    dialog_model.mechanisms.clear();
+    dialog_model.mechanisms.emplace_back(Mechanism::Phone("phone"), u"phone",
+                                         u"ph", kPasskeyAoaIcon,
+                                         base::DoNothing());
     EXPECT_FALSE(sheet_model.IsOtherMechanismButtonVisible());
-    testing::Mock::VerifyAndClearExpectations(&dialog_model);
   }
 }
 
 // Regression test for crbug.com/1408492.
 TEST_F(AuthenticatorSheetBaseTest,
        IsOtherMechanismButtonVisible_NoDialogModel) {
-  auto dialog_model = std::make_unique<MockDialogModel>();
+  auto dialog_model = std::make_unique<AuthenticatorRequestDialogModel>(
+      /*render_frame_host=*/nullptr);
   TestAuthenticatorSheetModel sheet_model(dialog_model.get(),
                                           MechanismVisibility::kVisible);
   dialog_model.reset();
@@ -114,21 +100,18 @@ constexpr char16_t kPasskeyName2[] = u"kodai";
 constexpr char16_t kPhoneName[] = u"pixel 7";
 
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
-  MockDialogModel dialog_model;
-  std::vector<Mechanism> mechanisms;
-  EXPECT_CALL(dialog_model, GetPriorityPhoneName)
-      .WillRepeatedly(testing::Return(kPhoneName));
-  mechanisms.emplace_back(
+  AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
+  dialog_model.paired_phone_names = {base::UTF16ToUTF8(kPhoneName)};
+  dialog_model.priority_phone_index = 0;
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
       kPasskeyName1, kPasskeyName1, kPasskeyPhoneIcon, base::DoNothing());
-  mechanisms.emplace_back(
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {1}}),
       kPasskeyName2, kPasskeyName2, kPasskeyPhoneIcon, base::DoNothing());
-  mechanisms.emplace_back(
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Transport(AuthenticatorTransport::kUsbHumanInterfaceDevice),
       u"security key", u"usb", kPasskeyAoaIcon, base::DoNothing());
-  EXPECT_CALL(dialog_model, mechanisms)
-      .WillRepeatedly(testing::Return(base::make_span(mechanisms)));
 
   AuthenticatorMultiSourcePickerSheetModel model(&dialog_model);
   EXPECT_THAT(model.primary_passkey_indices(), testing::ElementsAre(0, 1));
@@ -139,21 +122,18 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
 }
 
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
-  MockDialogModel dialog_model;
-  std::vector<Mechanism> mechanisms;
-  EXPECT_CALL(dialog_model, GetPriorityPhoneName)
-      .WillRepeatedly(testing::Return(kPhoneName));
-  mechanisms.emplace_back(
+  AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
+  dialog_model.paired_phone_names = {base::UTF16ToUTF8(kPhoneName)};
+  dialog_model.priority_phone_index = 0;
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
       kPasskeyName1, kPasskeyName1, kPasskeyPhoneIcon, base::DoNothing());
-  mechanisms.emplace_back(
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kTouchID, {1}}),
       kPasskeyName2, kPasskeyName2, kPasskeyAoaIcon, base::DoNothing());
-  mechanisms.emplace_back(
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Transport(AuthenticatorTransport::kUsbHumanInterfaceDevice),
       u"security key", u"usb", kPasskeyAoaIcon, base::DoNothing());
-  EXPECT_CALL(dialog_model, mechanisms)
-      .WillRepeatedly(testing::Return(base::make_span(mechanisms)));
 
   AuthenticatorMultiSourcePickerSheetModel model(&dialog_model);
   EXPECT_THAT(model.primary_passkey_indices(), testing::ElementsAre(1));
@@ -163,13 +143,10 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
 }
 
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, NoDiscoveredPasskeys) {
-  MockDialogModel dialog_model;
-  std::vector<Mechanism> mechanisms;
-  mechanisms.emplace_back(
+  AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
+  dialog_model.mechanisms.emplace_back(
       Mechanism::Transport(AuthenticatorTransport::kUsbHumanInterfaceDevice),
       u"security key", u"usb", kPasskeyAoaIcon, base::DoNothing());
-  EXPECT_CALL(dialog_model, mechanisms)
-      .WillRepeatedly(testing::Return(base::make_span(mechanisms)));
 
   AuthenticatorMultiSourcePickerSheetModel model(&dialog_model);
   EXPECT_TRUE(model.primary_passkey_indices().empty());

chrome/browser/password_manager/chrome_webauthn_credentials_delegate.cc
@@ -41,7 +41,8 @@ void ChromeWebAuthnCredentialsDelegate::LaunchWebAuthnFlow() {
   if (!authenticator_delegate) {
     return;
   }
-  authenticator_delegate->dialog_model()->TransitionToModalWebAuthnRequest();
+  authenticator_delegate->dialog_controller()
+      ->TransitionToModalWebAuthnRequest();
 #endif  // !BUILDFLAG(IS_ANDROID)
 }
 
@@ -69,7 +70,7 @@ void ChromeWebAuthnCredentialsDelegate::SelectPasskey(
     std::move(callback).Run();
     return;
   }
-  authenticator_delegate->dialog_model()->OnAccountPreselected(
+  authenticator_delegate->dialog_controller()->OnAccountPreselected(
       *selected_credential_id);
   // TODO(crbug.com/40274370): Update the OnAccountPreselected to run the
   // callback.

chrome/browser/password_manager/chrome_webauthn_credentials_delegate_unittest.cc
@@ -102,8 +102,8 @@ class ChromeWebAuthnCredentialsDelegateTest
 #if !BUILDFLAG(IS_ANDROID)
     AuthenticatorRequestDialogController::TransportAvailabilityInfo tai;
     tai.recognized_credentials = std::move(creds);
-    dialog_model()->StartFlow(std::move(tai),
-                              /*is_conditional_mediation=*/true);
+    dialog_controller()->StartFlow(std::move(tai),
+                                   /*is_conditional_mediation=*/true);
 #else
     delegate_->OnWebAuthnRequestPending(
         main_rfh(), creds, /*is_conditional_request=*/true,
@@ -115,8 +115,8 @@ class ChromeWebAuthnCredentialsDelegateTest
   }
 
 #if !BUILDFLAG(IS_ANDROID)
-  AuthenticatorRequestDialogController* dialog_model() {
-    return authenticator_request_delegate_->GetDialogModelForTesting();
+  AuthenticatorRequestDialogController* dialog_controller() {
+    return authenticator_request_delegate_->dialog_controller();
   }
 #endif
 
@@ -224,7 +224,7 @@ TEST_F(ChromeWebAuthnCredentialsDelegateTest, SelectCredential) {
 
 #if !BUILDFLAG(IS_ANDROID)
   base::RunLoop run_loop;
-  dialog_model()->SetAccountPreselectedCallback(base::BindLambdaForTesting(
+  dialog_controller()->SetAccountPreselectedCallback(base::BindLambdaForTesting(
       [&](device::DiscoverableCredentialMetadata cred) {
         EXPECT_THAT(cred.cred_id, testing::ElementsAreArray(kCredId2));
         run_loop.Quit();

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 1ec3338e1d4b8fee1ea0ab487c7c13ea33919b1a

webauthn: fix crash bug.
5a4f4fb43d75 introduced this crash bug because it assumed that the
`priority_phone_index` indexed the phone names, but it never did.
Instead, keep the index private to the controller, since it indexes the
paired phones that the controller holds. Instead put the name of the
priority phone in the model since that's what the UI needs to display.

Fixed: 333592767
Change-Id: I20b2a9c82e4b015aab2113d6d7d4f07372a6a11b
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441832
Auto-Submit: Adam Langley <agl@chromium.org>
Commit-Queue: Adam Langley <agl@chromium.org>
Commit-Queue: Nina Satragno <nsatragno@chromium.org>
Reviewed-by: Nina Satragno <nsatragno@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1285352}

chrome/browser/ui/webauthn/sheet_models.cc
@@ -1548,11 +1548,11 @@ AuthenticatorMultiSourcePickerSheetModel::
     return;
   }
 
-  std::optional<std::u16string> phone_name =
-      dialog_model->GetPriorityPhoneName();
+  const std::optional<std::string>& phone_name =
+      dialog_model->priority_phone_name;
   if (phone_name) {
-    primary_passkeys_label_ =
-        l10n_util::GetStringFUTF16(IDS_WEBAUTHN_FROM_PHONE_LABEL, *phone_name);
+    primary_passkeys_label_ = l10n_util::GetStringFUTF16(
+        IDS_WEBAUTHN_FROM_PHONE_LABEL, base::UTF8ToUTF16(*phone_name));
   }
   for (size_t i = 0; i < dialog_model->mechanisms.size(); ++i) {
     const AuthenticatorRequestDialogModel::Mechanism& mech =

chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -248,10 +248,10 @@ const gfx::VectorIcon& GetCredentialIcon(device::AuthenticatorType type) {
 
 std::u16string GetMechanismDescription(
     device::AuthenticatorType type,
-    const std::optional<std::u16string>& priority_phone_name) {
+    const std::optional<std::string>& priority_phone_name) {
   if (type == device::AuthenticatorType::kPhone) {
     return l10n_util::GetStringFUTF16(IDS_WEBAUTHN_SOURCE_PHONE,
-                                      *priority_phone_name);
+                                      base::UTF8ToUTF16(*priority_phone_name));
   }
   int message;
   switch (type) {
@@ -497,14 +497,6 @@ bool AuthenticatorRequestDialogModel::should_dialog_be_closed() const {
 
 bool AuthenticatorRequestDialogModel::should_bubble_be_closed() const {
   return step_ui_type(step_) != StepUIType::BUBBLE;
-}
-
-std::optional<std::u16string>
-AuthenticatorRequestDialogModel::GetPriorityPhoneName() const {
-  if (!priority_phone_index) {
-    return std::nullopt;
-  }
-  return base::UTF8ToUTF16(paired_phone_names.at(*priority_phone_index));
 }
 
 #define AUTHENTICATOR_REQUEST_EVENT_0(name)      \
@@ -1500,7 +1492,7 @@ void AuthenticatorRequestDialogController::OnAccountPreselected(
         break;
 
       case AccountState::kRecoverable:
-        if (model_->priority_phone_index) {
+        if (priority_phone_index_) {
           SetCurrentStep(Step::kTrustThisComputer);
         } else {
           SetCurrentStep(Step::kRecoverSecurityDomain);
@@ -1515,7 +1507,7 @@ void AuthenticatorRequestDialogController::OnAccountPreselected(
 
       case AccountState::kNone:
       case AccountState::kIrrecoverable:
-        if (model_->priority_phone_index) {
+        if (priority_phone_index_) {
           ContactPriorityPhone();
         } else {
           NOTIMPLEMENTED();
@@ -1525,13 +1517,13 @@ void AuthenticatorRequestDialogController::OnAccountPreselected(
       case AccountState::kEmpty:
         if (transport_availability_.request_type ==
             device::FidoRequestType::kMakeCredential) {
-          if (model_->priority_phone_index) {
+          if (priority_phone_index_) {
             SetCurrentStep(Step::kTrustThisComputer);
           } else {
             SetCurrentStep(Step::kRecoverSecurityDomain);
           }
         } else {
-          if (model_->priority_phone_index) {
+          if (priority_phone_index_) {
             ContactPriorityPhone();
           } else {
             // TODO(enclave): the security domain is empty but there were
@@ -1559,7 +1551,7 @@ void AuthenticatorRequestDialogController::SetSelectedAuthenticatorForTesting(
 }
 
 void AuthenticatorRequestDialogController::ContactPriorityPhone() {
-  ContactPhone(paired_phones_[*model_->priority_phone_index]->name);
+  ContactPhone(paired_phones_[*priority_phone_index_]->name);
 }
 
 void AuthenticatorRequestDialogController::ContactPhoneForTesting(
@@ -1568,6 +1560,16 @@ void AuthenticatorRequestDialogController::ContactPhoneForTesting(
   // screen right away.
   model_->ble_adapter_is_powered = true;
   ContactPhone(name);
+}
+
+void AuthenticatorRequestDialogController::SetPriorityPhoneIndex(
+    std::optional<size_t> index) {
+  if (index) {
+    model_->priority_phone_name = paired_phones_.at(*index)->name;
+  } else {
+    model_->priority_phone_name.reset();
+  }
+  priority_phone_index_ = index;
 }
 
 void AuthenticatorRequestDialogController::StartTransportFlowForTesting(
@@ -2206,11 +2208,9 @@ void AuthenticatorRequestDialogController::SortRecognizedCredentials() {
 void AuthenticatorRequestDialogController::PopulateMechanisms() {
   const bool is_get_assertion = transport_availability_.request_type ==
                                 device::FidoRequestType::kGetAssertion;
-  model_->priority_phone_index = GetIndexOfMostRecentlyUsedPhoneFromSync();
-  std::optional<std::u16string> priority_phone_name =
-      model_->GetPriorityPhoneName();
+  SetPriorityPhoneIndex(GetIndexOfMostRecentlyUsedPhoneFromSync());
   bool list_phone_passkeys =
-      is_get_assertion && model_->priority_phone_index &&
+      is_get_assertion && priority_phone_index_ &&
       base::FeatureList::IsEnabled(syncer::kSyncWebauthnCredentials);
   bool specific_phones_listed = false;
   bool specific_local_passkeys_listed = false;
@@ -2239,7 +2239,7 @@ void AuthenticatorRequestDialogController::PopulateMechanisms() {
               &AuthenticatorRequestDialogController::OnAccountPreselected,
               base::Unretained(this), cred.cred_id));
       mechanism.description =
-          GetMechanismDescription(cred.source, priority_phone_name);
+          GetMechanismDescription(cred.source, model_->priority_phone_name);
     }
   }
 
@@ -2381,7 +2381,7 @@ void AuthenticatorRequestDialogController::PopulateMechanisms() {
         paired_phones_.size() == 1 && !use_conditional_mediation_ &&
         transport_availability_.is_only_hybrid_or_internal;
     if (skip_to_phone_confirmation) {
-      model_->priority_phone_index = 0;
+      SetPriorityPhoneIndex(0);
       pending_step_ = Step::kPhoneConfirmationSheet;
     }
   }

chrome/browser/webauthn/authenticator_request_dialog_model.h
@@ -429,11 +429,6 @@ struct AuthenticatorRequestDialogModel {
   // Similar to above, but for bubbles.
   bool should_bubble_be_closed() const;
 
-  // Returns the name of the "priority" paired phone. This is the phone from
-  // sync if there are a priori discovered GPM passkeys, or the first phone on
-  // the list otherwise.
-  std::optional<std::u16string> GetPriorityPhoneName() const;
-
   const std::optional<content::GlobalRenderFrameHostId> frame_host_id;
   device::FidoRequestType request_type = device::FidoRequestType::kGetAssertion;
   device::ResidentKeyRequirement resident_key_requirement =
@@ -467,9 +462,10 @@ struct AuthenticatorRequestDialogModel {
   std::optional<int> pin_attempts;
   std::optional<int> uv_attempts;
   device::pin::PINEntryError pin_error = device::pin::PINEntryError::kNoError;
-  std::optional<size_t> priority_phone_index;
   // A sorted, unique list of the names of paired phones.
   std::vector<std::string> paired_phone_names;
+  // The name of the priority phone, if any.
+  std::optional<std::string> priority_phone_name;
 
   // cable_ui_type contains the type of UI to display for a caBLE transaction.
   std::optional<CableUIType> cable_ui_type;
@@ -797,6 +793,10 @@ class AuthenticatorRequestDialogController
   // user-visible mechanisms and use the callbacks therein.
   void ContactPhoneForTesting(const std::string& name);
 
+  // Sets `priority_phone_index_` and updates the name of the priority phone in
+  // `model_` accordingly.
+  void SetPriorityPhoneIndex(std::optional<size_t> index);
+
   // StartTransportFlowForTesting moves the UI to focus on the given transport.
   // UI should use |mechanisms()| to enumerate the user-visible mechanisms and
   // use the callbacks therein.
@@ -1039,6 +1039,9 @@ class AuthenticatorRequestDialogController
   // paired_phones_ contains details of caBLEv2-paired phones from both Sync and
   // QR-based pairing. The entries are sorted by name.
   std::vector<std::unique_ptr<device::cablev2::Pairing>> paired_phones_;
+
+  // The index, into `paired_phones_`, for the top-priority phone.
+  std::optional<size_t> priority_phone_index_;
 
   // paired_phones_contacted_ is the same length as |paired_phones_| and
   // contains true whenever the corresponding phone as already been contacted.

chrome/browser/ui/webauthn/sheet_models_unittest.cc
@@ -103,7 +103,7 @@ constexpr char16_t kPhoneName[] = u"pixel 7";
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
   AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
   dialog_model.paired_phone_names = {base::UTF16ToUTF8(kPhoneName)};
-  dialog_model.priority_phone_index = 0;
+  dialog_model.priority_phone_name = dialog_model.paired_phone_names.at(0);
   dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
       kPasskeyName1, kPasskeyName1, kPasskeyPhoneIcon, base::DoNothing());
@@ -125,7 +125,7 @@ TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMPasskeysOnly) {
 TEST_F(AuthenticatorMultiSourcePickerSheetModelTest, GPMAndLocalPasskeys) {
   AuthenticatorRequestDialogModel dialog_model(/*render_frame_host=*/nullptr);
   dialog_model.paired_phone_names = {base::UTF16ToUTF8(kPhoneName)};
-  dialog_model.priority_phone_index = 0;
+  dialog_model.priority_phone_name = dialog_model.paired_phone_names.at(0);
   dialog_model.mechanisms.emplace_back(
       Mechanism::Credential({device::AuthenticatorType::kPhone, {0}}),
       kPasskeyName1, kPasskeyName1, kPasskeyPhoneIcon, base::DoNothing());

chrome/browser/webauthn/authenticator_request_dialog_model_unittest.cc
@@ -1555,6 +1555,33 @@ TEST_F(AuthenticatorRequestDialogControllerTest, Cable2ndFactorFlows) {
   }
 }
 
+TEST_F(AuthenticatorRequestDialogControllerTest, CrBug333592767) {
+  AuthenticatorRequestDialogModel model(main_rfh());
+  AuthenticatorRequestDialogController controller(&model);
+
+  auto phone1 = std::make_unique<device::cablev2::Pairing>();
+  phone1->name = "test";
+  phone1->from_sync_deviceinfo = true;
+  phone1->last_updated = base::Time::FromTimeT(1);
+  auto phone2 = std::make_unique<device::cablev2::Pairing>(*phone1);
+  phone2->last_updated = base::Time::FromTimeT(2);
+
+  std::vector<std::unique_ptr<device::cablev2::Pairing>> phones;
+  phones.emplace_back(std::move(phone1));
+  phones.emplace_back(std::move(phone2));
+
+  controller.set_cable_transport_info(/*extension_is_v2=*/false,
+                                      std::move(phones), base::DoNothing(),
+                                      std::nullopt);
+  TransportAvailabilityInfo transports_info;
+  transports_info.request_type = RequestType::kMakeCredential;
+  transports_info.make_credential_attachment =
+      device::AuthenticatorAttachment::kAny;
+  transports_info.available_transports = kAllTransportsWithoutCable;
+  controller.StartFlow(std::move(transports_info),
+                       /*is_conditional_mediation=*/false);
+}
+
 TEST_F(AuthenticatorRequestDialogControllerTest, AwaitingAcknowledgement) {
   const struct {
     void (AuthenticatorRequestDialogController::*event)();
@@ -2207,7 +2234,7 @@ TEST_F(AuthenticatorRequestDialogControllerTest, ContactPriorityPhone_NoSync) {
   controller.StartFlow(std::move(transports_info),
                        /*is_conditional_mediation=*/false);
   EXPECT_EQ(model.step(), Step::kPhoneConfirmationSheet);
-  EXPECT_EQ(model.GetPriorityPhoneName(), u"Phone from QR");
+  EXPECT_EQ(model.priority_phone_name, "Phone from QR");
   model.ContactPriorityPhone();
   EXPECT_EQ(model.step(), Step::kCableActivate);
   EXPECT_EQ(model.selected_phone_name, "Phone from QR");
@@ -2241,7 +2268,7 @@ TEST_F(AuthenticatorRequestDialogControllerTest,
   controller.StartFlow(std::move(transports_info),
                        /*is_conditional_mediation=*/false);
   EXPECT_EQ(model.step(), Step::kPhoneConfirmationSheet);
-  EXPECT_EQ(model.GetPriorityPhoneName(), u"Phone from sync");
+  EXPECT_EQ(model.priority_phone_name, "Phone from sync");
   model.ContactPriorityPhone();
   EXPECT_EQ(model.step(), Step::kCableActivate);
   EXPECT_EQ(model.selected_phone_name, "Phone from sync");