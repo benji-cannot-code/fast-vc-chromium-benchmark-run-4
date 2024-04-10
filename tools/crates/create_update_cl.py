3f88b38c50cb51b77badeb265fd3642d77715da5
Lukasz Anforowicz <lukasza@chromium.org>
2024-04-10 20:38:12 +0000

    Unit test coverage for `//tools/crates/create_update_cl.py`.
    Bug: 40221377
    Change-Id: I6174a3d444bb406e4324fd13d2c1254583ff56b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430735
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285386}

--- tools/crates/create_update_cl.py
+++ tools/crates/create_update_cl.py
@@ -101,7 +101,7 @@ def GetCurrentCrateIds() -> Set[str]:
     return result
 
 
-@dataclass
+@dataclass(eq=True, order=True)
 class UpdatedCrate:
     old_crate_id: str
     new_crate_id: str
@@ -179,7 +179,8 @@ def DiffCrateIds(old_crate_ids: Set[str],
         if key not in old_dict:
             added_crate_ids.append(new_crate_id)
 
-    return CratesDiff(updates, removed_crate_ids, added_crate_ids)
+    return CratesDiff(sorted(updates), sorted(removed_crate_ids),
+                      sorted(added_crate_ids))
 
 
 def GetEpoch(crate_version: str) -> str:
