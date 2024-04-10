e17b4f0ee94442388dfb485c0d405f0ef8217d55
Takashi Toyoshima <toyoshim@chromium.org>
2024-04-10 16:42:19 +0000

    LinkPreview: speculative crash fix and inject debug code
    Ensure the actual crash reason as the current code injection point
    was optimized out in the production build, and the reason wasn't
    clear enough to know what happened, or where it happened.
    
    Bug: 330324142
    Change-Id: I648689985645286e815709466782ba18f1d721e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439083
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285193}

--- components/page_load_metrics/browser/metrics_web_contents_observer.cc
+++ components/page_load_metrics/browser/metrics_web_contents_observer.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
@@ -535,7 +537,15 @@ void MetricsWebContentsObserver::OnCookiesAccessedImpl(
 
 void MetricsWebContentsObserver::DidActivatePreviewedPage(
     base::TimeTicks activation_time) {
-  primary_page_->DidActivatePreviewedPage(activation_time);
+  if (primary_page_) {
+    primary_page_->DidActivatePreviewedPage(activation_time);
+  } else {
+    // TODO(b:330324142): Speculative fix to avoid crash cases. Need extra
+    // investigation why this happens once we ensure the case.
+    SCOPED_CRASH_KEY_STRING32("b/330324142", "Null",
+                              "MetricsWebContentsObserver::primary_page_");
+    base::debug::DumpWithoutCrashing();
+  }
 }
 
 void MetricsWebContentsObserver::OnStorageAccessed(
