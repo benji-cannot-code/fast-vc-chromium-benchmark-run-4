266d39e356e17cb614779e7e244e27239febdd80
Kristi Saney <kristislee@google.com>
2024-04-10 19:30:40 +0000

    Ensure selecting text that's been read aloud selects the main panel
    Expected behavior of reading mode in general is when you select text
    in the side panel, the same text in the main panel is selected. However,
    right now selecting text from the side panel that was read aloud does
    not correctly propagate to the main panel because of the formatting we
    add for the text that's been read aloud.
    
    Also updated the node text used in tests since the previous text was
    very similar to each other and made it hard to debug.
    
    Bug: 332589789
    Change-Id: Ie624a788fb5edfae8088a2e679b92a728d04b9a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5416915
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285324}

--- chrome/test/data/webui/side_panel/read_anything/read_aloud_update_content_selection.ts
+++ chrome/test/data/webui/side_panel/read_anything/read_aloud_update_content_selection.ts
@@ -134,10 +134,12 @@ suite('ReadAloud_UpdateContentSelection', () => {
       assertFalse(app.speechPlayingState.paused);
       assertTrue(app.speechPlayingState.speechStarted);
       // The expected HTML with the current highlights.
-      const expected =
-          '<div><p><span><span class="current-read-highlight">World</span>' +
-          '</span></p><p><span><span class="current-read-highlight">Friend' +
-          '</span></span><span><span class="current-read-highlight">!</span>' +
+      const expected = '<div><p><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">World</span>' +
+          '</span></p><p><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">Friend' +
+          '</span></span><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">!</span>' +
           '</span></p></div>';
       const innerHTML = app.$.container.innerHTML;
       assertEquals(innerHTML, expected);
@@ -171,10 +173,12 @@ suite('ReadAloud_UpdateContentSelection', () => {
       assertTrue(app.speechPlayingState.paused);
       assertTrue(app.speechPlayingState.speechStarted);
       // The expected HTML with the current highlights.
-      const expected =
-          '<div><p><span><span class="current-read-highlight">World</span>' +
-          '</span></p><p><span><span class="current-read-highlight">Friend' +
-          '</span></span><span><span class="current-read-highlight">!</span>' +
+      const expected = '<div><p><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">World</span>' +
+          '</span></p><p><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">Friend' +
+          '</span></span><span class="parent-of-highlight">' +
+          '<span class="current-read-highlight">!</span>' +
           '</span></p></div>';
       const innerHTML = app.$.container.innerHTML;
       assertEquals(innerHTML, expected);
