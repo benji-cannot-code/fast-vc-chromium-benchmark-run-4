3104466f3274f817b77d62aa3e37610b2960ed38
Min Qin <qinmin@chromium.org>
2024-04-10 23:51:58 +0000

    Adding empty ClusterManager implementation
    Bug: 333741496
    Change-Id: Ib71cf3bb702fef3058378d4e8154e0faed047d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445246
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285523}

--- components/commerce/core/shopping_service.cc
+++ components/commerce/core/shopping_service.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/commerce_constants.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/commerce/core/commerce_utils.h"
+#include "components/commerce/core/compare/cluster_manager.h"
 #include "components/commerce/core/compare/product_specifications_server_proxy.h"
 #include "components/commerce/core/discounts_storage.h"
 #include "components/commerce/core/feature_utils.h"
@@ -218,6 +219,8 @@ ShoppingService::ShoppingService(
   product_specs_server_proxy_ =
       std::make_unique<ProductSpecificationsServerProxy>(
           account_checker_.get(), identity_manager, url_loader_factory);
+
+  cluster_manager_ = std::make_unique<ClusterManager>();
 }
 
 AccountChecker* ShoppingService::GetAccountChecker() {
@@ -231,6 +234,7 @@ void ShoppingService::WebWrapperCreated(WebWrapper* web) {
 void ShoppingService::DidNavigatePrimaryMainFrame(WebWrapper* web) {
   HandleDidNavigatePrimaryMainFrameForProductInfo(web);
   HandleDidNavigatePrimaryMainFrameForPriceInsightsInfo(web);
+  cluster_manager_->DidNavigatePrimaryMainFrame(web->GetLastCommittedURL());
 }
 
 void ShoppingService::HandleDidNavigatePrimaryMainFrameForProductInfo(
@@ -275,6 +279,7 @@ void ShoppingService::HandleDidNavigatePrimaryMainFrameForProductInfo(
 void ShoppingService::DidNavigateAway(WebWrapper* web, const GURL& from_url) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   commerce_info_cache_.RemoveRef(web->GetLastCommittedURL());
+  cluster_manager_->DidNavigateAway(web->GetLastCommittedURL(), from_url);
 }
 
 void ShoppingService::DidStopLoading(WebWrapper* web) {
@@ -492,6 +497,7 @@ void ShoppingService::WebWrapperDestroyed(WebWrapper* web) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   open_web_wrappers_.erase(web);
   commerce_info_cache_.RemoveRef(web->GetLastCommittedURL());
+  cluster_manager_->WebWrapperDestroyed(web->GetLastCommittedURL());
 }
 
 void ShoppingService::UpdateProductInfoCache(
--- components/commerce/core/shopping_service.h
+++ components/commerce/core/shopping_service.h
@@ -109,6 +109,7 @@ class ScheduledMetricsManager;
 }  // namespace metrics
 
 class BookmarkUpdateManager;
+class ClusterManager;
 class DiscountsStorage;
 class ParcelsManager;
 class ProductSpecificationsServerProxy;
@@ -706,6 +707,9 @@ class ShoppingService : public KeyedService,
   // A list of UrlInfo ordered by most recently viewed. This is based on
   // selected tab (not necessarily navigation).
   std::vector<UrlInfo> recently_visited_tabs_;
+
+  // Class for clustering products.
+  std::unique_ptr<ClusterManager> cluster_manager_;
 
   // TODO(crbug.com/40067058): Delete this when ConsentLevel::kSync is deleted.
   //     See ConsentLevel::kSync documentation for details.
