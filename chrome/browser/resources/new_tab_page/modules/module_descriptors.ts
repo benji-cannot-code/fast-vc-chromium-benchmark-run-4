23bd49795be5d962f16d39d398e529551a4b4afa
Riley Tatum <rtatum@google.com>
2024-04-10 20:01:30 +0000

    [NTP][Enterprise] Initialize base calendar card
    The goal here is just to get the calendar card files set up and show a
    basic calendar card on the NTP (Just a card with a header).
    
    Other changes:
    - There was an error because of new_tab_page_modules having the wrong
      id for file_suggestion. Fixed that.
    - Updated the outlook calendar card flag name so it wasn't the exact
      same as the google calendar one.
    
    Screenshot: http://screenshot/7iiSDs6rUrSjG89
    
    Bug: 333560689
    Change-Id: Ibc12d74c7c809d0f521dd9fa1417ba7ea3096321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441659
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285351}

--- chrome/browser/resources/new_tab_page/modules/module_descriptors.ts
+++ chrome/browser/resources/new_tab_page/modules/module_descriptors.ts
@@ -19,6 +19,7 @@ import type {ModuleDescriptor} from './module_descriptor.js';
 import {ModuleRegistry} from './module_registry.js';
 import {photosDescriptor} from './photos/module.js';
 import {recipeTasksDescriptor} from './recipes/module.js';
+import {googleCalendarDescriptor} from './v2/calendar/module.js';
 // <if expr="not is_official_build">
 import {dummyV2Descriptor} from './v2/dummy/module.js';
 // </if>
@@ -40,6 +41,7 @@ descriptors.push(
                                historyClustersDescriptor);
 
 descriptors.push(tabResumptionDescriptor);
+descriptors.push(googleCalendarDescriptor);
 
 // <if expr="not is_official_build">
 if (modulesRedesignedEnabled) {
---
+++ chrome/browser/resources/new_tab_page/modules/v2/calendar/module.ts
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../module_header.js';
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {I18nMixin} from '../../../i18n_setup.js';
+import {ModuleDescriptor} from '../../module_descriptor.js';
+
+import {getTemplate} from './module.html.js';
+
+/**
+ * The Calendar module, which serves as an inside look in to upcoming events on
+ * a user's Google Calendar or Microsoft Outlook.
+ */
+export class CalendarModuleElement extends I18nMixin
+(PolymerElement) {
+  static get is() {
+    return 'ntp-calendar-module';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {};
+  }
+}
+
+customElements.define(CalendarModuleElement.is, CalendarModuleElement);
+
+async function createCalendarElement(): Promise<CalendarModuleElement|null> {
+  return new Promise<CalendarModuleElement>(
+      (resolve) => resolve(new CalendarModuleElement()));
+}
+
+export const googleCalendarDescriptor: ModuleDescriptor = new ModuleDescriptor(
+    /*id*/ 'google_calendar', createCalendarElement);
