d83b642413eb196b698494673b14d4e5e0bd4deb
Tommy C. Li <tommycli@chromium.org>
2024-04-10 23:35:58 +0000

    History Embeddings: Implement OnHistoryDeletions, add integration test
    Bug: b/329495955
    Change-Id: I53a28581629044738843d2c98c351b9aee25127e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5437009
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285507}

--- components/history_embeddings/history_embeddings_service.h
+++ components/history_embeddings/history_embeddings_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/threading/sequence_bound.h"
 #include "components/history/core/browser/history_service.h"
@@ -81,8 +82,8 @@ class HistoryEmbeddingsService : public KeyedService,
 
  private:
   friend class HistoryEmbeddingsBrowserTest;
-  FRIEND_TEST_ALL_PREFIXES(HistoryEmbeddingsTest,
-                           ConstructsAndComputesEmbeddings);
+  friend class HistoryEmbeddingsServiceTest;
+  FRIEND_TEST_ALL_PREFIXES(HistoryEmbeddingsServiceTest, OnHistoryDeletions);
 
   // A utility container to wrap anything that should be accessed on
   // the separate storage worker sequence.
@@ -99,6 +100,11 @@ class HistoryEmbeddingsService : public KeyedService,
         size_t query_id,
         Embedding query_embedding,
         size_t count);
+
+    // Handles the History deletions on the worker thread.
+    void HandleHistoryDeletions(bool for_all_history,
+                                history::URLRows deleted_rows,
+                                std::set<history::VisitID> deleted_visit_ids);
 
     // A VectorDatabase implementation that holds data in memory.
     VectorDatabaseInMemory vector_database;
