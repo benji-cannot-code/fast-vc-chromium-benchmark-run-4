dae9283f33a3b8c70a3146d51c304586a5d6e19e
Pavol Marko <pmarko@google.com>
2024-04-10 09:13:55 +0000

    Fix access to ClientCertPattern in enrollment_dialog_view
    Fix network policy access in enrollment_dialog_view.cc and add an
    integration test for the "EnrollmentURI" feature (browsertest in
    chrome/browser/ash/policy/networking/network_policy_application_browsertest.cc).
    
    Background:
    enrollment_dialog_view.cc's CreateEnrollmentDialog needs to access an
    ONC policy provided ClientCertPattern. This regressed in CL:3634816.
    That CL changed retrieval of network policies to be able to request
    "original" policies (as they came from the policy server) or "policies
    with runtime values" where things like placeholders and client
    certificate patterns are replaced with their runtime resolved values.
    
    CreateEnrollmentDialog should have been requesting the network policy
    with PolicyType::kOriginal, but it was implicitly using
    PolicyType::kWithRuntimeValues. There was no integration test for this
    feature.
    
    Bug: b:319188170
    Change-Id: I54435a7f2f087eaefa41d39382ece50316895a45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425011
    Reviewed-by: Oleksandr Sarapulov <olsa@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Commit-Queue: Pavol Marko <pmarko@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284984}

--- chrome/browser/ui/ash/network/enrollment_dialog_view.h
+++ chrome/browser/ui/ash/network/enrollment_dialog_view.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::enrollment {
 
+inline constexpr char kWidgetName[] = "EnrollmentDialogWidget";
+
 // Creates and shows the dialog for certificate-based network enrollment.
 bool CreateEnrollmentDialog(const std::string& network_id);
 
