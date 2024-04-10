6f92abe8dce90157d19a46c5354d32cde9b27870
selya <selya@chromium.org>
2024-04-10 17:10:32 +0000

    Update factory methods in CookiePartitionKey to remove default parameter values and replace with expected inputs and values
    The implementation of the ancestor chain bit required changes to the factory methods FromWire and FromNetworkIsolationKey. To reduce the size of the CL for the ancestor chain bit implementation, those methods were given default parameter values for the parameters added. This CL removes the default values and updates the call sites to use the expected inputs or values.
    
    Bug: 331945202
    Change-Id: Ic6b0d5d31fe48c3e4520bac4c93d59f8ebadeead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405466
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Auto-Submit: Aaron Selya <selya@google.com>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285226}

--- content/browser/devtools/protocol/network_handler.cc
+++ content/browser/devtools/protocol/network_handler.cc
@@ -231,6 +231,7 @@ class CookieRetrieverNetworkService
   static void Retrieve(network::mojom::CookieManager* cookie_manager,
                        const std::vector<GURL> urls,
                        const net::NetworkIsolationKey& network_isolation_key,
+                       const net::SiteForCookies& site_for_cookies,
                        std::unique_ptr<GetCookiesCallback> callback) {
     scoped_refptr<CookieRetrieverNetworkService> self =
         new CookieRetrieverNetworkService(std::move(callback));
@@ -240,7 +241,8 @@ class CookieRetrieverNetworkService
           url, cookie_options,
           net::CookiePartitionKeyCollection::FromOptional(
               net::CookiePartitionKey::FromNetworkIsolationKey(
-                  network_isolation_key)),
+                  network_isolation_key, site_for_cookies,
+                  net::SchemefulSite(url))),
           base::BindOnce(&CookieRetrieverNetworkService::GotCookies, self));
     }
   }
@@ -1613,7 +1615,8 @@ void NetworkHandler::GetCookies(Maybe<Array<String>> protocol_urls,
 
   CookieRetrieverNetworkService::Retrieve(
       storage_partition_->GetCookieManagerForBrowserProcess(), urls,
-      host_->GetNetworkIsolationKey(), std::move(callback));
+      host_->GetNetworkIsolationKey(), host_->ComputeSiteForCookies(),
+      std::move(callback));
 }
 
 void NetworkHandler::GetAllCookies(
--- content/browser/fenced_frame/fenced_frame_browsertest.cc
+++ content/browser/fenced_frame/fenced_frame_browsertest.cc
@@ -3011,7 +3011,8 @@ IN_PROC_BROWSER_TEST_F(FencedFrameParameterizedBrowserTest,
   EXPECT_TRUE(isolation_info.nonce());
   std::optional<net::CookiePartitionKey> partition_key =
       net::CookiePartitionKey::FromNetworkIsolationKey(
-          isolation_info.network_isolation_key());
+          isolation_info.network_isolation_key(),
+          isolation_info.site_for_cookies(), net::SchemefulSite(https_url));
   EXPECT_TRUE(partition_key && partition_key->nonce());
   net::CookiePartitionKeyCollection cookie_partition_key_collection =
       net::CookiePartitionKeyCollection::FromOptional(partition_key);
@@ -3024,6 +3025,8 @@ IN_PROC_BROWSER_TEST_F(FencedFrameParameterizedBrowserTest,
     EXPECT_TRUE(cookie.IsPartitioned());
     EXPECT_TRUE(cookie.PartitionKey() && cookie.PartitionKey()->nonce());
     EXPECT_EQ(cookie.PartitionKey()->nonce(), partition_key->nonce());
+    EXPECT_EQ(cookie.PartitionKey()->IsThirdParty(),
+              partition_key->IsThirdParty());
     EXPECT_EQ("3", cookie.Value());
   }
 
@@ -3117,7 +3120,8 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_TRUE(isolation_info.nonce());
   std::optional<net::CookiePartitionKey> partition_key =
       net::CookiePartitionKey::FromNetworkIsolationKey(
-          isolation_info.network_isolation_key());
+          isolation_info.network_isolation_key(),
+          isolation_info.site_for_cookies(), net::SchemefulSite(https_url));
   EXPECT_TRUE(partition_key && partition_key->nonce());
   net::CookiePartitionKeyCollection cookie_partition_key_collection =
       net::CookiePartitionKeyCollection::FromOptional(partition_key);
@@ -3130,6 +3134,8 @@ IN_PROC_BROWSER_TEST_F(
     EXPECT_TRUE(cookie.IsPartitioned());
     EXPECT_TRUE(cookie.PartitionKey() && cookie.PartitionKey()->nonce());
     EXPECT_EQ(cookie.PartitionKey()->nonce(), partition_key->nonce());
+    EXPECT_EQ(cookie.PartitionKey()->IsThirdParty(),
+              partition_key->IsThirdParty());
     EXPECT_EQ("3", cookie.Value());
   }
 
