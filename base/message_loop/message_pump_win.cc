aa3378bf7d65be949d24f1b610c9c880c540f1d0
Andrew Williams <awillia@chromium.org>
2024-04-10 18:33:53 +0000

    Fix more instances where ::GetLastError() won't be logged as expected
    `LOG(INFO) << ::GetLastError()` and similar will always output 0 instead
    of the previously set ::GetLastError() (due to [1]), so update a
    instances of this to show the correct error reason.
    
    Found with:
    case:yes pcre:true (?ms)CHECK[^;]+<<[^;]+GetLastError -path:third_party -path:.js -path:out path:.cc
    case:yes pcre:true (?ms)NOTREACHED[^;]+<<[^;]+GetLastError -path:third_party -path:.js -path:out path:.cc
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:base/logging.h;l=643-646;drc=9d2b795f72832aa7216d7c84bdbf5d2358346548
    
    Bug: 333445539
    Change-Id: I6594265b46f6ac2b779ec71dd5f883dec2874dc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436937
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Henrik Andreasson <henrika@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285285}

--- base/message_loop/message_pump_win.cc
+++ base/message_loop/message_pump_win.cc
@@ -265,6 +265,7 @@ void MessagePumpForUI::WaitForWork(Delegate::NextWorkInfo next_work_info) {
     base::debug::Alias(&wait_flags);
     DWORD result = MsgWaitForMultipleObjectsEx(0, nullptr, delay, QS_ALLINPUT,
                                                wait_flags);
+    DPCHECK(WAIT_FAILED != result);
 
     if (WAIT_OBJECT_0 == result) {
       // A WM_* message is available.
@@ -311,8 +312,6 @@ void MessagePumpForUI::WaitForWork(Delegate::NextWorkInfo next_work_info) {
                 ->set_wait_for_object_result(result);
           });
     }
-
-    DCHECK_NE(WAIT_FAILED, result) << GetLastError();
   }
 }
 
