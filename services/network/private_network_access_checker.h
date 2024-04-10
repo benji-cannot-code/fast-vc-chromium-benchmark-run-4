7811fd41197a92c4b1e543461c406ae3afe1ab78
Yifan Luo <lyf@chromium.org>
2024-04-10 12:22:30 +0000

    [Private Network Access] Public HTTP pages with required address space should be blocked.
    Mixed content request with private/local required IP address space (or less public required IP address space than client's IP address space ) is allowed for private network access. We should double check it in the PNA check if the required IP address space is different with the resource IP address space.
    
    Resource IP address space is the real IP address space we just got. We compared resource IP address space and target IP address space here and set target IP address space to be the resource IP address space later.
    
    If required IP address space doesn't meet the real IP address space (aka the resource IP address space), we block the request, as it should be blocked by mixed content checker if there's no required IP address space.
    
    Bug: 332848993
    Change-Id: Idb8a7aa277f1e7a8ad0a086844c90fd6a4c12019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440194
    Reviewed-by: Yifan Luo <lyf@chromium.org>
    Reviewed-by: Jonathan Hao <phao@chromium.org>
    Commit-Queue: Yifan Luo <lyf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285059}

--- services/network/private_network_access_checker.h
+++ services/network/private_network_access_checker.h
@@ -84,6 +84,10 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) PrivateNetworkAccessChecker {
     return target_address_space_;
   }
 
+  mojom::IPAddressSpace RequiredAddressSpace() const {
+    return required_address_space_;
+  }
+
   // Clears state from all checks this instance has performed, and sets the
   // request URL to `new_url`.
   //
@@ -185,6 +189,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) PrivateNetworkAccessChecker {
 
   // The request is from/to a potentially trustworthy and same origin.
   bool is_potentially_trustworthy_same_origin_;
+
+  // IP address space required for permission prompt.
+  mojom::IPAddressSpace required_address_space_;
 };
 
 }  // namespace network
