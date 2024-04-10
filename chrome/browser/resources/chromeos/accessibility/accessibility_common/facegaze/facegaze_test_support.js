c2c6cb463af174ecaeb2a0ceda65ec3bf2455b5d
Akihiro Ota <akihiroota@chromium.org>
2024-04-10 21:08:20 +0000

    FaceGaze: Refactor and cleanup C++ tests
    This change cleans up the FaceGaze C++ tests by doing the following:
    1. Changing forehead location from two doubles to a pair of doubles
    2. Changing most instances of "mouse" to "cursor"
    3. Making some properties in Config optional. However, no Configs were
    updated in this change to allow for smoother review.
    4. AssertCursorAt() was added as a convenience way to confirm the
    cursor position
    
    Bug: b/309121742
    Change-Id: Ib3f56e143daa04ac069bddb3d8f031684d34feb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441413
    Reviewed-by: Anastasia Helfinstein <anastasi@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285406}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_support.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test_support.js
@@ -66,7 +66,7 @@ class FaceGazeTestSupport {
    * @param {number} x
    * @param {number} y
    */
-  async waitForMouseLocation(x, y) {
+  async waitForCursorLocation(x, y) {
     const desktop = await new Promise(resolve => {
       chrome.automation.getDesktop(d => resolve(d));
     });
