68fe6cee80ade3a8f2840d46315dc8497f30182c
Will Harris <wfh@chromium.org>
2024-04-10 17:36:02 +0000

    Add fuzzer for caller validation
    BUG=40227925
    
    Change-Id: I69960c5ab1708226949e85bebdfaf55a30bb712b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5443624
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285243}

---
+++ chrome/elevation_service/caller_validation_fuzzer.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/elevation_service/caller_validation.h"
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "base/containers/span.h"
+#include "base/process/process.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  std::ignore = elevation_service::ValidateData(
+      base::Process::Current(), base::span<const uint8_t>(data, size));
+  return 0;
+}
