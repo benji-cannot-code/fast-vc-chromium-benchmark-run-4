49538be38c54ef5544d68370dc0dc6afc2b205ca
Daniil Sakhapov <sakhapov@chromium.org>
2024-04-10 10:02:56 +0000

    Replace GetIntValue by ComputeInteger for numeric value custom text
    Bug: 40946458
    Change-Id: I06563f12a75b2433c6f32962299aea6812bdeaf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435017
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285011}

--- third_party/blink/renderer/core/css/css_numeric_literal_value.cc
+++ third_party/blink/renderer/core/css/css_numeric_literal_value.cc
@@ -216,7 +216,7 @@ String CSSNumericLiteralValue::CustomCSSText() const {
       // FIXME
       break;
     case UnitType::kInteger:
-      text = String::Number(GetIntValue());
+      text = String::Number(ComputeInteger());
       break;
     case UnitType::kNumber:
     case UnitType::kPercentage:
