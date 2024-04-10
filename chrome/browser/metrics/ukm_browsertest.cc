dcd185aad56f9f75ba4e41b3f8c0043f4532a397
Friedrich Horschig <fhorschig@chromium.org>
2024-04-10 07:45:13 +0000

    Revert "[declutter] Add core archive tabmodel classes"
    This reverts commit d7a4c3abe246d18bbed123542f9835b55705c69e.
    
    Reason for revert: One of the new test consistently fails on
    https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests/18206/test-results?sortby=&groupby=
    See the original bug for details.
    
    Original change's description:
    > [declutter] Add core archive tabmodel classes
    >
    > Add core classes to manage the lifetime/persistence of the archived
    > tab model. These classes are wired into ChromeTabbedActivity behind
    > a flag, but likely will need to always be wired up so that that
    > archived tabs can be migrated back if the feature is off. Will address
    > that in a later CL.
    >
    > Bug: 331412481
    > Change-Id: I894fa058016f1bcf94f0e7cbe99b85536200b502
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5399363
    > Reviewed-by: Sky Malice <skym@chromium.org>
    > Commit-Queue: Brandon Wylie <wylieb@google.com>
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1284764}
    
    Bug: 331412481
    Change-Id: I422a85d373f4aa177250b87ad6a844723a2f9d84
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442772
    Auto-Submit: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284954}

--- chrome/browser/metrics/ukm_browsertest.cc
+++ chrome/browser/metrics/ukm_browsertest.cc
@@ -72,7 +72,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #else
-#include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/flags/android/chrome_session_state.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
@@ -356,11 +355,6 @@ class UkmBrowserTest : public UkmBrowserTestBase {
     // would need to remove the pre-existing TabModel and add a new one.
     // Having an empty TabModelList allows us to simply add the appropriate
     // TabModel.
-    if (base::FeatureList::IsEnabled(chrome::android::kAndroidTabDeclutter)) {
-      EXPECT_EQ(2U, TabModelList::models().size());
-      TabModelList::RemoveTabModel(TabModelList::models()[1]);
-    }
-    EXPECT_EQ(1U, TabModelList::models().size());
     TabModelList::RemoveTabModel(TabModelList::models()[0]);
     EXPECT_EQ(0U, TabModelList::models().size());
   }
