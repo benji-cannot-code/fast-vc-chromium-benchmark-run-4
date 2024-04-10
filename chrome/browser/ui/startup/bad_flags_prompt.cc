82f22395ae8ddeb6118c5b0ebc84b12422894959
Nathan Hebert <nhebert@chromium.org>
2024-04-10 01:24:06 +0000

    cast certificate: enable --cast-developer-certificate-path switch
    Previously the Chrome command line switch,
    --cast-developer-certificate-path, was only used if the build flag,
    cast_allow_developer_certificate was set to true. This CL enables the
    command line switch always. Doing so helps unblock testing on the CQ
    and on ChromeOS. The change is in a few parts.
    
    One change is to use the platform independent command line parsing
    method: base::CommandLine::GetSwitchValuePath(). Doing so fixes
    compilation on Windows where FilePath is constructed from std::wstring.
    
    Another change is to gate starting a task to load the certificate file
    by the presence of the command line switch. This avoids an issue where
    the CastAuthUtilTest lacks a task environment. It is also a
    micro-optimization.
    
    Since build flag cast_allow_developer_certificate is no longer used, the
    certificate BUILD.gn no longer include its definition from
    third_party/openscreen.
    
    This change also adds the --cast-developer-certificate-path switch to
    the "bad flags" list, so a user will see a warning in the info bar if
    the flag has been set.
    
    A security review of always enabling the switch has been approved.
    
    Bug: b:320187114
    Test: Streamed to standalone cast_receiver with developer certificates
    
    Change-Id: I5b8e70dd42e6fd39bd379213c6518ac66a3c9c32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407786
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1284833}

--- chrome/browser/ui/startup/bad_flags_prompt.cc
+++ chrome/browser/ui/startup/bad_flags_prompt.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/history_clusters/core/file_clustering_backend.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/infobars/core/infobar_delegate.h"
+#include "components/media_router/common/providers/cast/certificate/switches.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/startup_metric_utils/browser/startup_metric_utils.h"
 #include "components/translate/core/common/translate_switches.h"
@@ -179,6 +180,10 @@ const char* const kBadFlags[] = {
     // in tests and performance benchmarks. Using it could allow faking user
     // interaction across origins.
     cc::switches::kEnableGpuBenchmarking,
+
+    // This flag enables loading a developer-signed certificate for Cast
+    // streaming receivers and should only be used for testing purposes.
+    cast_certificate::switches::kCastDeveloperCertificatePath,
 };
 #endif  // !BUILDFLAG(IS_ANDROID)
 
