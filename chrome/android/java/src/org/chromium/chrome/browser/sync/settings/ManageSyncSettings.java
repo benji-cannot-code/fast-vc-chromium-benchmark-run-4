f4eeff6aa8b077ae08248b7e1e0531a5cc0e1615
Mahmoud Rashad <mmrashad@google.com>
2024-04-11 09:34:10 +0000

    Add the "Data in your account" preference to the unified settings panel
    Screenshot: http://screen/66EDknPkc7zvMcM
    Mock: http://docs/presentation/d/1jfWN1ti53cu-JmkXIvZK0EWFADoGjHC1Hw3A0DbYA9Q?pli=1#slide=id.g2bc865286e3_0_0
    
    Bug: 326574743
    Change-Id: I73b4c4758c607dd664b5d480a4cfae2c634299d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439275
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285698}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettings.java
@@ -248,11 +248,6 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
                 findPreference(PREF_ADVANCED_CATEGORY).setVisible(true);
             }
 
-            Preference reviewSyncData = findPreference(PREF_SYNC_REVIEW_DATA);
-            reviewSyncData.setOnPreferenceClickListener(
-                    SyncSettingsUtils.toOnClickListener(
-                            this, () -> SyncSettingsUtils.openSyncDashboard(getActivity())));
-
             mSyncTypeCheckBoxPreferencesMap = new HashMap<>();
             mSyncTypeCheckBoxPreferencesMap.put(
                     UserSelectableType.AUTOFILL, findPreference(PREF_SYNC_AUTOFILL));
@@ -316,6 +311,11 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
         mSyncEncryption = findPreference(PREF_ENCRYPTION);
         mSyncEncryption.setOnPreferenceClickListener(
                 SyncSettingsUtils.toOnClickListener(this, this::onSyncEncryptionClicked));
+
+        Preference reviewSyncData = findPreference(PREF_SYNC_REVIEW_DATA);
+        reviewSyncData.setOnPreferenceClickListener(
+                SyncSettingsUtils.toOnClickListener(
+                        this, () -> SyncSettingsUtils.openSyncDashboard(getActivity())));
     }
 
     @Override
