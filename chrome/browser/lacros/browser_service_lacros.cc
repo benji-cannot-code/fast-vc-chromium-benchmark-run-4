dbf7bc6504610e890e38ef33014391ab886311bb
Takuto Ikuta <tikuta@chromium.org>
2024-04-11 10:29:01 +0000

    crosapi: remove unused response from removed method
    This is to remove circular dependency between BrowserService and
    Crosapi.
    
    Then we can move BrowserService related types to different files and
    refer that without increasing compile time much.
    
    NOPRESUBMIT=true
    
    Bug: 329182208
    Change-Id: Id693a4082c8b9729952244a03d797ada90125516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442512
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285723}

--- chrome/browser/lacros/browser_service_lacros.cc
+++ chrome/browser/lacros/browser_service_lacros.cc
@@ -196,7 +196,7 @@ BrowserServiceLacros::~BrowserServiceLacros() {
   BrowserList::RemoveObserver(this);
 }
 
-void BrowserServiceLacros::REMOVED_0(REMOVED_0Callback callback) {
+void BrowserServiceLacros::REMOVED_0() {
   NOTIMPLEMENTED();
 }
 
