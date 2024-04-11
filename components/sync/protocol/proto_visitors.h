48bf5bc5f066ad04c3982c44b7340acf1c5e83cd
Yuheng Huang <yuhengh@chromium.org>
2024-04-11 15:42:43 +0000

    [tab group v2] Add pinned_position field to SavedTabGroup
    In saved_tab_group_specifics.proto,
    pinned_position is used by tab group v2 to replace position.
    This can prevent tab group v1 to change the position of v2.
    For more information please see the doc[1]
    
    [1]https://docs.google.com/document/d/1NCC65ij297_vCAOWpF1vy2HMPxucu1dFz3wXIpIMLIM/edit?usp=sharing&resourcekey=0-8EwabFjqcKs6frBvbDTg2w
    
    Bug: 327629525
    Change-Id: Ibd19ad9ed2bb490bc3968e940a2733be44bb6259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436078
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285858}

--- components/sync/protocol/proto_visitors.h
+++ components/sync/protocol/proto_visitors.h
@@ -1151,6 +1151,7 @@ VISIT_PROTO_FIELDS(const sync_pb::SavedTabGroup& proto) {
   VISIT(position);
   VISIT(title);
   VISIT_ENUM(color);
+  VISIT(pinned_position);
 }
 
 VISIT_PROTO_FIELDS(const sync_pb::SavedTabGroupTab& proto) {
