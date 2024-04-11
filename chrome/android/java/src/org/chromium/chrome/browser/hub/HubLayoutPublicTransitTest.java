7335543172988793b284593c0b6894e0824e5e51
Henrique Nakashima <hnakashima@chromium.org>
2024-04-11 21:07:50 +0000

    [Android] Split NewTabPageStation and IncognitoNewTabPageStation
    The contents are fundamentally different, so split them into two
    stations.
    
    Bug: 328275619
    Change-Id: Idd6d12b3a6c600af05939e265b1f250220a6bd31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445153
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286096}

--- chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutPublicTransitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutPublicTransitTest.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.test.transit.ChromeTabbedActivityPublicTransitEntryPo
 import org.chromium.chrome.test.transit.HubIncognitoTabSwitcherStation;
 import org.chromium.chrome.test.transit.HubTabSwitcherAppMenuFacility;
 import org.chromium.chrome.test.transit.HubTabSwitcherStation;
+import org.chromium.chrome.test.transit.IncognitoNewTabPageStation;
 import org.chromium.chrome.test.transit.NewTabPageStation;
 import org.chromium.chrome.test.transit.PageAppMenuFacility;
 import org.chromium.chrome.test.transit.PageStation;
@@ -101,7 +102,7 @@ public class HubLayoutPublicTransitTest {
 
         HubTabSwitcherAppMenuFacility appMenu = tabSwitcher.openAppMenu();
 
-        NewTabPageStation newIncognitoTab = appMenu.openNewIncognitoTab();
+        IncognitoNewTabPageStation newIncognitoTab = appMenu.openNewIncognitoTab();
 
         // Reset to original state for batching.
         HubIncognitoTabSwitcherStation incognitoTabSwitcher =
@@ -118,7 +119,7 @@ public class HubLayoutPublicTransitTest {
         PageStation page = mTransitEntryPoints.startOnBlankPageBatched(mBatchedRule);
 
         PageAppMenuFacility appMenu = page.openAppMenu();
-        NewTabPageStation incognitoNewTabPage = appMenu.openNewIncognitoTab();
+        IncognitoNewTabPageStation incognitoNewTabPage = appMenu.openNewIncognitoTab();
 
         HubIncognitoTabSwitcherStation incognitoTabSwitcher =
                 incognitoNewTabPage.openHub(HubIncognitoTabSwitcherStation.class);
