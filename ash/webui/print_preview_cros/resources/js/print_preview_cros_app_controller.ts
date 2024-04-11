0ecf986ec1d0c0265fa22c271fe745d557095d2c
Ashley Prasad <ashleydp@google.com>
2024-04-11 20:19:50 +0000

    cros-preview: Add skeleton controller to app
    - Add skeleton implementation of controller to print-preview-cros-app to
      handle starting print preview session and passing session context to
      data manager(s).
    - Test controller can be created and exists in element.
    
    Bug: b:323421684
    Test: browser_test --gtest_filter=*PrintPreviewCros*
    Change-Id: I2b84cd732e519f79c334574a2ae85b23564e4294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425579
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286058}

---
+++ ash/webui/print_preview_cros/resources/js/print_preview_cros_app_controller.ts
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'print-preview-cros-app-controller' is responsible for starting the print
+ * preview session and providing the session context to the data managers.
+ * Session context provides the unique ID necessary for communicating with
+ * the CrOS preview backend and required initialization information.
+ */
+
+export class PrintPreviewCrosAppController extends EventTarget {}
---
+++ chrome/test/data/webui/chromeos/print_preview_cros/print_preview_cros_app_controller_test.ts
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://os-print/js/print_preview_cros_app_controller.js';
+
+import {PrintPreviewCrosAppController} from 'chrome://os-print/js/print_preview_cros_app_controller.js';
+import {assertTrue} from 'chrome://webui-test/chromeos/chai_assert.js';
+
+suite('PrintPreviewCrosAppController', () => {
+  let controller: PrintPreviewCrosAppController;
+
+  setup(() => {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+
+    controller = new PrintPreviewCrosAppController();
+  });
+
+  // Verify controller is an event target.
+  test('controller is an event target', () => {
+    assertTrue(
+        controller instanceof EventTarget, 'Controller is an event target');
+  });
+});
