0bc1aebfae63439c6c128586bc61570cf7344442
Ken Rockot <rockot@google.com>
2024-04-11 14:13:52 +0000

    Mojo: Introduce MojoIpczMemV2 feature
    This feature will enable the IPCZ_FEATURE_MEM_V2 feature within ipcz,
    which cannot itself use base::FeatureList. The feature is currently a
    no-op, so this CL is just plumbing feature state with no behavioral
    changes.
    
    Bug: 332719403
    Change-Id: Ibaed2d37e66d6817f15a19cbda9f5a28ea6fd533
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444216
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Oksana Zhuravlova <oksamyt@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285806}

--- mojo/core/embedder/embedder.cc
+++ mojo/core/embedder/embedder.cc
@@ -51,6 +51,8 @@ std::atomic<bool> g_mojo_ipcz_enabled{false};
 std::atomic<bool> g_mojo_ipcz_enabled{true};
 #endif
 
+bool g_enable_memv2 = false;
+
 std::optional<std::string> GetMojoIpczEnvVar() {
   std::string value;
   auto env = base::Environment::Create();
@@ -105,6 +107,8 @@ void InitFeatures() {
   } else {
     g_mojo_ipcz_enabled.store(false, std::memory_order_release);
   }
+
+  g_enable_memv2 = base::FeatureList::IsEnabled(kMojoIpczMemV2);
 }
 
 void EnableMojoIpcz() {
@@ -129,6 +133,7 @@ void Init(const Configuration& configuration) {
         .use_local_shared_memory_allocation =
             configuration.is_broker_process ||
             configuration.force_direct_shared_memory_allocation,
+        .enable_memv2 = g_enable_memv2,
     }));
     MojoEmbedderSetSystemThunks(GetMojoIpczImpl());
   } else {
