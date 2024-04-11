2b509e0d646934bd6d2b86dc896da2c4cb20469d
Chris Fredrickson <cfredric@chromium.org>
2024-04-11 18:12:15 +0000

    Store SAA-eligibility in FedCM sharing permission contexts
    This fixes a security flaw whereby a compromised renderer could bypass
    the permissions policy requirement in SAA (by claiming to have invoked
    SAA successfully already). After this change, the browser process will
    not send "unmarked" FedCM permission grants, so the network service
    can identify when a renderer shouldn't really have cookie access.
    
    Tested=manually
    
    Bug: 330574529
    Change-Id: I8f1009099963234b0f43979f65e817dfcee9fc02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435996
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285962}

--- chrome/browser/webid/federated_identity_account_keyed_permission_context.h
+++ chrome/browser/webid/federated_identity_account_keyed_permission_context.h
@@ -6,15 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEBID_FEDERATED_IDENTITY_ACCOUNT_KEYED_PERMISSION_CONTEXT_H_
 #define CHROME_BROWSER_WEBID_FEDERATED_IDENTITY_ACCOUNT_KEYED_PERMISSION_CONTEXT_H_
 
+#include <string>
+
 #include "components/content_settings/core/common/content_settings_types.h"
 #include "components/permissions/object_permission_context_base.h"
 #include "components/webid/federated_identity_data_model.h"
 
-#include <string>
-
 namespace content {
 class BrowserContext;
 class ContentSettingsForOneType;
+}
+
+namespace net {
+class SchemefulSite;
 }
 
 namespace url {
@@ -38,6 +42,8 @@ class FederatedIdentityAccountKeyedPermissionContext
       const FederatedIdentityAccountKeyedPermissionContext&) = delete;
   FederatedIdentityAccountKeyedPermissionContext& operator=(
       const FederatedIdentityAccountKeyedPermissionContext&) = delete;
+
+  ~FederatedIdentityAccountKeyedPermissionContext() override;
 
   // Returns whether the given relying party has any FedCM permission.
   bool HasPermission(const url::Origin& relying_party_requester);
@@ -88,6 +94,14 @@ class FederatedIdentityAccountKeyedPermissionContext
                         const std::string& account_id,
                         base::OnceClosure callback);
 
+  // Marks the given (site, site) pair as eligible to use FedCM sharing
+  // permission as a signal for the Storage Access API. This is only valid to
+  // call for pairs that already have sharing permission.
+  void MarkStorageAccessEligible(
+      const net::SchemefulSite& relying_party_embedder,
+      const net::SchemefulSite& identity_provider,
+      base::OnceClosure callback);
+
   // permissions::ObjectPermissionContextBase:
   std::string GetKeyForObject(const base::Value::Dict& object) override;
 
@@ -99,7 +113,8 @@ class FederatedIdentityAccountKeyedPermissionContext
       base::OnceClosure callback);
 
   // Converts existing sharing permission grants into (site, site)-keyed content
-  // settings.
+  // settings. The returned value only includes permissions for (site, site)
+  // pairs that were marked "eligible" via `MarkStorageAccessEligible`.
   ContentSettingsForOneType GetSharingPermissionGrantsAsContentSettings();
 
  private:
@@ -118,6 +133,11 @@ class FederatedIdentityAccountKeyedPermissionContext
   base::raw_ref<content::BrowserContext> browser_context_;
 
   raw_ptr<base::Clock> clock_;
+
+  // The set of pairs that are eligible for Storage-Access autogrants.
+  // Keyed by (embedder, identity-provider) pairs.
+  base::flat_set<std::pair<net::SchemefulSite, net::SchemefulSite>>
+      storage_access_eligible_connections_;
 };
 
 #endif  // CHROME_BROWSER_WEBID_FEDERATED_IDENTITY_ACCOUNT_KEYED_PERMISSION_CONTEXT_H_
