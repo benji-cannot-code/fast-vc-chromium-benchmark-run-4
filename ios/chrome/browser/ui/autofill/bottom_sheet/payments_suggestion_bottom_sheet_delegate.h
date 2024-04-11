82f09381d80429107ced6349f8e8d6f69b959e20
Alexis Hetu <sugoi@chromium.org>
2024-04-11 17:52:58 +0000

    [iOS] Fix payments bottom sheet refocus logic
    The Payments Bottom Sheet could have been refocusing the triggering
    field, possibly causing the keyboard to open, even when selecting a
    credit card. To prevent this from happening, the logic has been
    changed to not disable the bottom sheet on exit in this scenario and
    explicitly disable the bottom sheet just before performing the
    suggestion selection process.
    
    Bug: 324606723
    Change-Id: I2c5d292a5eb99d607e92aa2153e9128c24c6a0c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444966
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285944}

--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_delegate.h
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_delegate.h
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Request to disable the bottom sheet, potentially refocusing the field which
 // originally triggered the bottom sheet after the bottom sheet has been
 // disabled.
-- (void)disableBottomSheet;
+- (void)disableBottomSheetAndRefocus:(BOOL)refocus;
 
 @end
 
--- ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/payments_suggestion_bottom_sheet_mediator.mm
@@ -71,10 +71,6 @@ using PaymentsSuggestionBottomSheetExitReason::kBadProvider;
                               autofill::PersonalDataManagerObserver>>
       _scopedPersonalDataManagerObservation;
 
-  // Whether the field that triggered the bottom sheet will need to refocus when
-  // the bottom sheet is dismissed. Default is true.
-  bool _needsRefocus;
-
   // Information regarding the triggering form for this bottom sheet.
   autofill::FormActivityParams _params;
 }
@@ -90,7 +86,6 @@ using PaymentsSuggestionBottomSheetExitReason::kBadProvider;
                  personalDataManager:
                      (autofill::PersonalDataManager*)personalDataManager {
   if (self = [super init]) {
-    _needsRefocus = true;
     _params = params;
     _hasCreditCards = NO;
     _webStateList = webStateList;
@@ -233,12 +228,11 @@ using PaymentsSuggestionBottomSheetExitReason::kBadProvider;
     // Last resort safety exit: On the unlikely event that the provider was set
     // incorrectly (for example if local predictions and server predictions are
     // different), simply exit and open the keyboard.
-    _needsRefocus = true;
-    [self disableBottomSheet];
+    [self disableBottomSheetAndRefocus:YES];
     [self logExitReason:kBadProvider];
     return;
   }
-  _needsRefocus = false;
+  [self disableBottomSheetAndRefocus:NO];
 
   // Create a form suggestion containing the selected credit card's backend id
   // so that the suggestion provider can properly fill the form.
@@ -264,11 +258,11 @@ using PaymentsSuggestionBottomSheetExitReason::kBadProvider;
   [provider didSelectSuggestion:suggestion params:_params];
 }
 
-- (void)disableBottomSheet {
+- (void)disableBottomSheetAndRefocus:(BOOL)refocus {
   if (_webStateList) {
     web::WebState* activeWebState = _webStateList->GetActiveWebState();
     AutofillBottomSheetTabHelper::FromWebState(activeWebState)
-        ->DetachPaymentsListenersForAllFrames(_needsRefocus);
+        ->DetachPaymentsListenersForAllFrames(refocus);
   }
 }
 
