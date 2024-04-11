61cc248267079e62a48221abff3ae832cf32cfc8
Hailey Wang <haileywang@google.com>
2024-04-11 20:34:13 +0000

    [Gardener] Disable failing OmniboxPedalsTest
    Bug: b/333905073
    Change-Id: I28cd4561637b3d36fa2069c26ecd4d54d493538f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448287
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Owners-Override: Hailey Wang <haileywang@google.com>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Auto-Submit: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286069}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/suggestions/OmniboxPedalsTest.java
@@ -42,6 +42,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -214,6 +215,7 @@ public class OmniboxPedalsTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "b/333905073")
     public void testManagePasswords() throws InterruptedException {
         setSuggestions(createPedalSuggestion(OmniboxPedalId.MANAGE_PASSWORDS));
         clickOnPedalToSettings(() -> mOmniboxUtils.clickOnAction(0, 0), PasswordSettings.class);
