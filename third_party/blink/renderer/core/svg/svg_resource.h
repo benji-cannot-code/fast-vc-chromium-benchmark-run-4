864ed37900024efa4f5400cd6424c7cc9860d581
Fredrik Söderquist <fs@opera.com>
2024-04-11 14:39:31 +0000

    Reland "Rework SVGResourceDocumentContent to be the ResourceClient"
    This is a reland of commit 4fd78d8e5ba3a306d8447b75b293acbad6e2ebfd
    
    Reland change: Don't add SVGDocumentResources to the MemoryCache at all
    rather than reloading them. This improves the handling of static data
    resources since it allows them to complete synchronously.
    
    Original change's description:
    > Rework SVGResourceDocumentContent to be the ResourceClient
    >
    > This separates the load-cycle of the underlying Resource from the load-
    > cycle of the SVGResourceDocumentContent. In a future CL they will no
    > longer be the same because additional steps need to be taken before
    > loading can be considered completed in some cases (c.f SVGImage and the
    > loading of data URL resources).
    >
    > A new TextResource resource subclass is introduced -
    > SVGDocumentResource - to manage the loading and essentially do the work
    > of the ResourceClient. It feeds data and status updates into the
    > associated SVGResourceDocumentContent.
    >
    > Bug: 40134477
    > Change-Id: Ide967d0d4a315c9205bdc86ece646c5726e8ab19
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5419515
    > Commit-Queue: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1285089}
    
    Bug: 40134477
    Change-Id: I3cb949a517591edbe3a6a4e9f7911b1987698998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444825
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285817}

--- third_party/blink/renderer/core/svg/svg_resource.h
+++ third_party/blink/renderer/core/svg/svg_resource.h
@@ -7,9 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_SVG_SVG_RESOURCE_H_
 
 #include "third_party/blink/renderer/core/loader/resource/image_resource_observer.h"
+#include "third_party/blink/renderer/core/svg/svg_resource_document_observer.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_map.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
-#include "third_party/blink/renderer/platform/loader/fetch/resource_client.h"
+#include "third_party/blink/renderer/platform/heap/prefinalizer.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
@@ -149,7 +150,8 @@ class LocalSVGResource final : public SVGResource {
 };
 
 // External resource reference (see SVGResource).
-class ExternalSVGResource final : public SVGResource, public ResourceClient {
+class ExternalSVGResource final : public SVGResource,
+                                  public SVGResourceDocumentObserver {
  public:
   explicit ExternalSVGResource(const KURL&);
 
@@ -161,9 +163,8 @@ class ExternalSVGResource final : public SVGResource, public ResourceClient {
  private:
   Element* ResolveTarget();
 
-  // ResourceClient implementation
-  void NotifyFinished(Resource*) override;
-  WTF::String DebugName() const override;
+  // SVGResourceDocumentObserver:
+  void ResourceNotifyFinished(SVGResourceDocumentContent*) override;
 
   Member<SVGResourceDocumentContent> document_content_;
   KURL url_;
