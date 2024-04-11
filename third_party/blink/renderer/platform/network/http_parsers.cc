22dbd259f17318190118022a68447895f44cba73
Psychpsyo <psychpsyo@gmail.com>
2024-04-11 06:18:42 +0000

    Disallow fractions in http-equiv=refresh
    The HTML Standard says that http-equiv=refresh only accepts integer
    time values and the spec maintainers have also expressed that they
    think http-equiv=refresh is "not a great feature" and they "don't
    think we should add to it".
    For those reasons, I have removed that functionality.
    This means Chromium's behavior now matches that of other browsers,
    such as Firefox.
    
    References:
    - Spec
        https://html.spec.whatwg.org/#pragma-directives
    - WHATWG comment:
        https://github.com/whatwg/html/issues/3266#issuecomment-348683670
    Change-Id: I01e02ecf3e23b6e7c0b2612d95208c187573cb95
    
    Bug: 329866189
    Change-Id: I01e02ecf3e23b6e7c0b2612d95208c187573cb95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5371947
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285634}

--- third_party/blink/renderer/platform/network/http_parsers.cc
+++ third_party/blink/renderer/platform/network/http_parsers.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/resource_response.h"
 #include "third_party/blink/renderer/platform/network/header_field_tokenizer.h"
 #include "third_party/blink/renderer/platform/network/http_names.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/date_math.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
@@ -371,8 +372,6 @@ bool ParseRefreshTime(const String& source, base::TimeDelta& delay) {
   for (unsigned i = 0; i < source.length(); ++i) {
     UChar ch = source[i];
     if (ch == kFullstopCharacter) {
-      // TODO(tkent): According to the HTML specification, we should support
-      // only integers. However we support fractional numbers.
       if (++full_stop_count == 2)
         number_end = i;
     } else if (!IsASCIIDigit(ch)) {
@@ -381,6 +380,9 @@ bool ParseRefreshTime(const String& source, base::TimeDelta& delay) {
   }
   bool ok;
   double time = source.Left(number_end).ToDouble(&ok);
+  if (RuntimeEnabledFeatures::MetaRefreshNoFractionalEnabled()) {
+    time = floor(time);
+  }
   if (!ok)
     return false;
   delay = base::Seconds(time);
