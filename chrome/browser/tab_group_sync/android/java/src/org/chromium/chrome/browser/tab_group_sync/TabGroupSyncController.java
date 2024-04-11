ebd69a35189ad8a168594b9d8525486b0ba20998
Shakti Sahu <shaktisahu@chromium.org>
2024-04-11 23:14:05 +0000

    [Tab Group Sync] Propagating remote updates to local
    This CL
    1. Adds the remote observer that observes TabGroupSyncService and
    invokes LocalTabGroupMutationHelper to apply the changes.
    2. LocalTabGroupMutationHelper to help with the process of
    creating and updating a local tab group.
    
    Bug: b:333721527
    Change-Id: Icdc2134167f143ec3ed4fb984d01805939317708
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444563
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286171}

---
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncController.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_group_sync;
+
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.url.GURL;
+
+/**
+ * Central class responsible for making things happen. i.e. apply remote changes to local and local
+ * changes to remote.
+ */
+public final class TabGroupSyncController {
+    /**
+     * A delegate in helping out with creating and navigating tabs in response to remote updates
+     * from sync. The tab will be created in a background state and will not be navigated
+     * immediately. The navigation will happen only when the tab becomes active such as user
+     * switches to the tab.
+     */
+    public interface TabCreationDelegate {
+
+        /**
+         * Creates a tab in background in the local tab model. The tab will be created at the given
+         * position and will be loaded with the given URL. The URL will not be loaded right away.
+         *
+         * @param url The URL to load.
+         * @param parent The parent of the tab.
+         * @param position The position of the tab in the tab model.
+         * @return The tab created.
+         */
+        Tab createBackgroundTab(GURL url, Tab parent, int position);
+    }
+}
