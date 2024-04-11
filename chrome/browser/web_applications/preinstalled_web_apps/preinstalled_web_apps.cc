0664678a1553d88ebedf40b0d7af940143c1e834
David Black <dmblack@google.com>
2024-04-11 21:18:19 +0000

    [4] Provide device info to container app preinstallation config.
    This CL provides device info the the container app preinstallation
    config on ChromeOS so that it can dynamically adjust its external
    install options based on device info state. See planned sequence of
    CLs below:
    
    [1] https://crrev.com/c/chromium/src/+/5439611
    [2] http://shortn/_GKBRFkYWOR
    [3] Roll-deps
    [4] This CL
    [5] http://shortn/_h6yi8jwdk2
    [6] Roll-deps
    
    Bug: b:331212317, b:333583704
    Change-Id: I78b673e8fab678f2e6c705eed863399e940827f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441179
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286103}

--- chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
+++ chrome/browser/web_applications/preinstalled_web_apps/preinstalled_web_apps.cc
@@ -85,7 +85,7 @@ std::vector<ExternalInstallOptions> GetChromeBrandedApps(
 #if BUILDFLAG(IS_CHROMEOS)
       GetConfigForAppMall(),
       GetConfigForCalculator(),
-      GetConfigForContainer(), // TODO(http://b/331212317): Pass `device_info`.
+      GetConfigForContainer(device_info),
       GetConfigForGoogleCalendar(),
       GetConfigForGoogleChat(),
       GetConfigForGoogleMeet(),
