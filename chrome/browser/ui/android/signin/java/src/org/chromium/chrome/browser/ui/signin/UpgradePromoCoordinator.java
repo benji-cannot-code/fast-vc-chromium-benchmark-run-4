1e0ef8127c9a3459f6936a5df6517b85670975f2
Samar <samarchehade@google.com>
2024-04-11 13:57:42 +0000

    [UNOp2][Android] Implement add accounts flow for re-FRE
    Bug: b/41493788
    Change-Id: I21721090847bfee336bd8f5a20d2b805915a0c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434556
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285791}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/UpgradePromoCoordinator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/UpgradePromoCoordinator.java
@@ -35,7 +35,7 @@ public final class UpgradePromoCoordinator
                 FullscreenSigninCoordinator.Delegate {
     public interface Delegate {
         /** Notifies when the user clicked the "add account" button. */
-        void addAccountInUpgradePromo();
+        void addAccount();
 
         /**
          * The supplier that supplies whether reading policy value is necessary. See {@link
@@ -124,7 +124,7 @@ public final class UpgradePromoCoordinator
     /** Implements {@link FullscreenSigninCoordinator.Delegate} */
     @Override
     public void addAccount() {
-        mDelegate.addAccountInUpgradePromo();
+        mDelegate.addAccount();
     }
 
     /** Implements {@link FullscreenSigninCoordinator.Delegate} */
@@ -235,6 +235,10 @@ public final class UpgradePromoCoordinator
             return;
         }
         advanceToNextPage();
+    }
+
+    public void onAccountSelected(String accountName) {
+        mSigninCoordinator.onAccountSelected(accountName);
     }
 
     private void inflateViewSwitcher() {
