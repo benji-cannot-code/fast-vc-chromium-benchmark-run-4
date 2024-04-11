4a895d4006e6306e4de07801b2167ae78ab43b1f
EmiliaPaz <emiliapaz@chromium.org>
2024-04-11 00:10:19 +0000

    [Extensions] Remove unnecessary method from ExtensionActionTestHelper
    ExtensionActionTestHelper was added before there was a platform-abstract
    ExtensionsContainer class. Use extensions container's
    IsActionVisibleOnToolbar() instead of VisibleBrowserActions() util
    and remove such method.
    
    Bug: 40889653
    Change-Id: Ic81ed26218012c59a7baeffe17919762a85fc04b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446086
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285529}

--- chrome/browser/extensions/extension_keybinding_apitest.cc
+++ chrome/browser/extensions/extension_keybinding_apitest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/extensions/extension_action_test_helper.h"
+#include "chrome/browser/ui/extensions/extensions_container.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/test/base/interactive_test_utils.h"
@@ -380,7 +381,8 @@ IN_PROC_BROWSER_TEST_F(CommandsApiTest, UnpinnedPageActionTriggers) {
   std::unique_ptr<ExtensionActionTestHelper> test_helper =
       ExtensionActionTestHelper::Create(browser());
   RunScheduledLayouts();
-  EXPECT_EQ(0, test_helper->VisibleBrowserActions());
+  EXPECT_FALSE(test_helper->GetExtensionsContainer()->IsActionVisibleOnToolbar(
+      extension->id()));
 
   const int tab_id = NavigateToTestURLAndReturnTabId();
   SetActionVisibleOnTab(profile(), *extension, tab_id);
