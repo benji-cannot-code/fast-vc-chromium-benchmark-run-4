9a7580ef8c5cb32dfc622e9b835c5f123f7ad42d
Aliona DANGLA <alionadangla@google.com>
2024-04-11 13:36:04 +0000

    [iOS] Update group view when a tab is moved out of the group
    See video:
    https://drive.google.com/file/d/1r21nEek7qGhjIaGA13DcM94x_WHl8ntO/view?usp=drive_link
    
    Bug: 40942154
    Change-Id: If6b14efc22ce17e68bec857d4f679d738ed08c59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444432
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285778}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
@@ -209,14 +209,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   web::WebState* detachedWebState = detachChange.detached_web_state();
   GridItemIdentifier* identifierToRemove =
       [GridItemIdentifier tabIdentifier:detachedWebState];
-  GridItemIdentifier* selectedIdentifier;
-  if (self.webStateList->GetGroupOfWebStateAt(webStateList->active_index()) ==
-      _tabGroup.get()) {
-    selectedIdentifier =
-        [GridItemIdentifier tabIdentifier:webStateList->GetActiveWebState()];
-  }
   [self.consumer removeItemWithIdentifier:identifierToRemove
-                   selectedItemIdentifier:selectedIdentifier];
+                   selectedItemIdentifier:[self activeIdentifier]];
   [self removeObservationForWebState:detachedWebState];
 }
 
@@ -258,9 +252,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       if (moveChange.old_group() != _tabGroup.get()) {
         break;
       }
-      [self moveItem:[GridItemIdentifier
-                         tabIdentifier:moveChange.moved_web_state()]
-          beforeWebStateIndex:moveChange.moved_to_index() + 1];
+      GridItemIdentifier* item =
+          [GridItemIdentifier tabIdentifier:moveChange.moved_web_state()];
+      if (moveChange.new_group() == _tabGroup.get()) {
+        [self moveItem:item
+            beforeWebStateIndex:moveChange.moved_to_index() + 1];
+      } else {
+        [self.consumer removeItemWithIdentifier:item
+                         selectedItemIdentifier:[self activeIdentifier]];
+      }
       break;
     }
     default:
