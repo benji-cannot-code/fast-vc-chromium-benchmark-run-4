4bca5eb1ba3e6784662ec5f986d53d2d41159f02
Evan Stade <estade@chromium.org>
2024-04-11 19:05:59 +0000

    Storage buckets: throw TypeError when window context is destroyed.
    This was recently changed from returning undefined to throwing
    (rejecting with) a SecurityError, but the spec says to throw a TypeError
    https://wicg.github.io/storage-buckets/#storage-bucket-open
    
    Bug: none
    Change-Id: Ib4437f18709ce206be5c37b3a1d1fd2e61866f5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445109
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286008}

--- third_party/blink/renderer/modules/buckets/storage_bucket_manager.cc
+++ third_party/blink/renderer/modules/buckets/storage_bucket_manager.cc
@@ -109,8 +109,13 @@ ScriptPromise<StorageBucket> StorageBucketManager::open(
   auto promise = resolver->Promise();
 
   ExecutionContext* context = ExecutionContext::From(script_state);
-  if (context->IsContextDestroyed() ||
-      !context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
+
+  if (context->IsContextDestroyed()) {
+    exception_state.ThrowTypeError("The window/worker has been destroyed.");
+    return promise;
+  }
+
+  if (!context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
     exception_state.ThrowSecurityError(
         "Access to Storage Buckets API is denied in this context.");
     return promise;
@@ -148,8 +153,12 @@ ScriptPromise<IDLSequence<IDLString>> StorageBucketManager::keys(
   auto promise = resolver->Promise();
 
   ExecutionContext* context = ExecutionContext::From(script_state);
-  if (context->IsContextDestroyed() ||
-      !context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
+  if (context->IsContextDestroyed()) {
+    exception_state.ThrowTypeError("The window/worker has been destroyed.");
+    return promise;
+  }
+
+  if (!context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
     exception_state.ThrowSecurityError(
         "Access to Storage Buckets API is denied in this context.");
     return promise;
@@ -170,8 +179,12 @@ ScriptPromise<IDLUndefined> StorageBucketManager::Delete(
   auto promise = resolver->Promise();
 
   ExecutionContext* context = ExecutionContext::From(script_state);
-  if (context->IsContextDestroyed() ||
-      !context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
+  if (context->IsContextDestroyed()) {
+    exception_state.ThrowTypeError("The window/worker has been destroyed.");
+    return promise;
+  }
+
+  if (!context->GetSecurityOrigin()->CanAccessStorageBuckets()) {
     exception_state.ThrowSecurityError(
         "Access to Storage Buckets API is denied in this context.");
     return promise;
