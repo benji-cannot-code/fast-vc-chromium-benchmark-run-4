c996cc46f8359debe0d466db18adc5d1adec5b79
Ioana Pandele <ioanap@chromium.org>
2024-04-11 11:32:21 +0000

    [Gardener] Disable flaky startup paint preview test
    StartupPaintPreviewHelperTest#testDisplayOnStar
    
    Bug: 333779543
    Change-Id: Ibf4705bc1b2bbc824b56473c5b7e5364d0a136c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444828
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Owners-Override: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285736}

--- chrome/android/javatests/src/org/chromium/chrome/browser/paint_preview/StartupPaintPreviewHelperTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/paint_preview/StartupPaintPreviewHelperTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.base.ColdStartTracker;
@@ -92,6 +93,7 @@ public class StartupPaintPreviewHelperTest {
     @Test
     @MediumTest
     @Restriction(StartupPaintPreviewHelperTestRunner.RESTRICTION_TYPE_KEEP_ACTIVITIES)
+    @DisabledTest(message = "Very flaky. See crbug.com/333779543.")
     public void testDisplayOnStartup() throws ExecutionException {
         mActivityTestRule.startMainActivityWithURL(
                 mActivityTestRule.getTestServer().getURL(TEST_URL));
