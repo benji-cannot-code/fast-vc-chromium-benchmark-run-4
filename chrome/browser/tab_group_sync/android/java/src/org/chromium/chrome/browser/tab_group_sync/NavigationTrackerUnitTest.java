af3483f197cacc241204711ec5c5d4159107c89e
Shakti Sahu <shaktisahu@chromium.org>
2024-04-11 18:36:32 +0000

    [Tab Group Sync] Added navigation observer and tracker
    This CL
    1. Adds navigation observer that listens to every navigation in
    the tab model and propagates them to sync after applying a filter.
    2. The navigation filtering contains: ignore incognito, tabs not
    in tab groups, redirects, and sync initiated navigations.
    3. Adds a naviation tracker to mark which navigations were
    started from sync so that we can ignore them during subsequent
    updates.
    
    Bug: b:331127308
    Change-Id: Id2b35e76b3c8f955a1f9c1986f53df9f3ee1db59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444305
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285990}

---
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/NavigationTrackerUnitTest.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_group_sync;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.UserDataHost;
+import org.chromium.base.test.BaseRobolectricTestRunner;
+
+/** Unit tests for the {@link NavigationTracker}. */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class NavigationTrackerUnitTest {
+    private NavigationTracker mNavigationTracker = new NavigationTracker();
+
+    @Test
+    public void testSyncInitiatedNavigation() {
+        UserDataHost userDataHost = new UserDataHost();
+        mNavigationTracker.setNavigationWasFromSync(userDataHost);
+        Assert.assertTrue(mNavigationTracker.wasNavigationFromSync(userDataHost));
+    }
+
+    @Test
+    public void testNotSyncInitiatedNavigation() {
+        UserDataHost userDataHost = new UserDataHost();
+        Assert.assertFalse(mNavigationTracker.wasNavigationFromSync(userDataHost));
+    }
+
+    @Test
+    public void testTrackingResetAfterQuery() {
+        UserDataHost userDataHost = new UserDataHost();
+        mNavigationTracker.setNavigationWasFromSync(userDataHost);
+        Assert.assertTrue(mNavigationTracker.wasNavigationFromSync(userDataHost));
+        Assert.assertFalse(mNavigationTracker.wasNavigationFromSync(userDataHost));
+    }
+}
