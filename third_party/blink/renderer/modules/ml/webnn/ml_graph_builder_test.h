ad5200b86dfc6b986049e46c861cdb5b874a0d00
mei1127 <shanxing.mei@intel.com>
2024-04-11 07:57:45 +0000

    webnn: Migrate transpose validation tests to WPTs
    This CL adds WPT tests for transpose and removes the unit tests
    `MLGraphBuilderTest.TransposeTest`, `MLGraphTestMojo.TransposeTest` and
    `MLGraphTest.TransposeTest`.
    
    Bug: 327337526, 328026885
    Change-Id: I438cd463d3a9e60042bb02fb97797771b0450075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5437146
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Shanxing Mei <shanxing.mei@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1285657}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_reduce_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_softplus_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_split_options.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_transpose_options.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_utils.h"
@@ -126,13 +125,6 @@ MLOperand* BuildSoftplus(
     MLGraphBuilder* builder,
     const MLOperand* input,
     const MLSoftplusOptions* options = MLSoftplusOptions::Create());
-
-MLOperand* BuildTranspose(
-    V8TestingScope& scope,
-    MLGraphBuilder* builder,
-    const MLOperand* input,
-    const MLTransposeOptions* options = MLTransposeOptions::Create());
-
 }  // namespace blink
 
 #endif  // THIRD_PARTY_BLINK_RENDERER_MODULES_ML_WEBNN_ML_GRAPH_BUILDER_TEST_H_
