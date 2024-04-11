548f34c02d4e323268ae4126a9c9c856b70e4f12
Artsiom Mitrokhin <amitrokhin@google.com>
2024-04-11 17:06:11 +0000

    Add `ContextualGoogleIntegrations(Enabled|Configuration)` policies
    `ContextualGoogleIntegrationsEnabled` is an umbrella bool policy
    that turns off all integrations listed in the `...Configuration`.
    
    https://docs.google.com/document/d/1r3z2-a5DQXFtA-tiT2puNnK9ax8A-z1yzK6ut9UNA-0/edit?tab=t.0#heading=h.wrvfadawnn4h
    
    Bug: b/319251265, b/296286730
    Change-Id: I5e9f182e00ff115026b30578d218fe9ae65b10fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5420096
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285915}

--- ash/glanceables/glanceables_controller.cc
+++ ash/glanceables/glanceables_controller.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/glanceables/glanceables_controller.h"
 
+#include <utility>
+
 #include "ash/api/tasks/tasks_client.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/glanceables/classroom/glanceables_classroom_client.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/session/session_controller.h"
 #include "base/check.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "components/account_id/account_id.h"
 #include "components/prefs/pref_registry_simple.h"
 
@@ -33,6 +36,14 @@ GlanceablesController::~GlanceablesController() {
 void GlanceablesController::RegisterUserProfilePrefs(
     PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(prefs::kGlanceablesEnabled, true);
+
+  base::Value::List default_integrations;
+  default_integrations.Append(prefs::kGoogleCalendarIntegrationName);
+  default_integrations.Append(prefs::kGoogleClassroomIntegrationName);
+  default_integrations.Append(prefs::kGoogleTasksIntegrationName);
+  registry->RegisterListPref(prefs::kContextualGoogleIntegrationsConfiguration,
+                             std::move(default_integrations));
+
   GlanceablesClassroomStudentView::RegisterUserProfilePrefs(registry);
   GlanceablesTasksComboboxModel::RegisterUserProfilePrefs(registry);
 }
---
+++ chrome/browser/ash/policy/handlers/contextual_google_integrations_policies_handler.h
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_POLICY_HANDLERS_CONTEXTUAL_GOOGLE_INTEGRATIONS_POLICIES_HANDLER_H_
+#define CHROME_BROWSER_ASH_POLICY_HANDLERS_CONTEXTUAL_GOOGLE_INTEGRATIONS_POLICIES_HANDLER_H_
+
+#include "components/policy/core/browser/configuration_policy_handler.h"
+
+class PrefValueMap;
+
+namespace policy {
+
+class PolicyErrorMap;
+class PolicyMap;
+class Schema;
+
+// Handles `ContextualGoogleIntegrationsEnabled` and
+// `ContextualGoogleIntegrationsConfiguration` policies.
+class ContextualGoogleIntegrationsPoliciesHandler
+    : public SchemaValidatingPolicyHandler {
+ public:
+  explicit ContextualGoogleIntegrationsPoliciesHandler(const Schema& schema);
+  ContextualGoogleIntegrationsPoliciesHandler(
+      const ContextualGoogleIntegrationsPoliciesHandler&) = delete;
+  ContextualGoogleIntegrationsPoliciesHandler& operator=(
+      const ContextualGoogleIntegrationsPoliciesHandler&) = delete;
+  ~ContextualGoogleIntegrationsPoliciesHandler() override;
+
+  // SchemaValidatingPolicyHandler:
+  bool CheckPolicySettings(const PolicyMap& policies,
+                           PolicyErrorMap* errors) override;
+
+ protected:
+  // SchemaValidatingPolicyHandler:
+  void ApplyPolicySettings(const PolicyMap& policies,
+                           PrefValueMap* prefs) override;
+};
+
+}  // namespace policy
+
+#endif  // CHROME_BROWSER_ASH_POLICY_HANDLERS_CONTEXTUAL_GOOGLE_INTEGRATIONS_POLICIES_HANDLER_H_
