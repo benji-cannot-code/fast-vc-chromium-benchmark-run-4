0b06dbb833f630dacb343072e0585ccf0af5afbf
Alison Gale <agale@chromium.org>
2024-04-11 17:48:55 +0000

    Migrate TODOs referencing old crbug IDs to the new issue tracker IDs
    The canonical bug format is TODO(crbug.com/<id>). TODOs of the
    following forms will all be migrated to the new format:
    
    - TODO(crbug.com/<old id>)
    - TODO(https://crbug.com/<old id>)
    - TODO(crbug/<old id>)
    - TODO(crbug/monorail/<old id>)
    - TODO(<old id>)
    
    Bug id mapping is sourced from go/chrome-on-buganizer-prod-issues.
    See go/crbug-todo-migration for details.
    
    #crbug-todo-migration
    
    Bug: b/321899722
    Change-Id: Ide7663da5de0a300c894cb149a035748c937aead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447228
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285937}

--- chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller_interactive_uitest.cc
@@ -3240,7 +3240,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTestWithTabbedWebApp,
 }
 
 // Home tab can't be detached.
-// TODO(crbug.com/1381358): Enable this test for Linux and Lacros.
+// TODO(crbug.com/40245163): Enable this test for Linux and Lacros.
 #if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS))
 #define MAYBE_CantDragHomeTab DISABLED_CantDragHomeTab
 #else
@@ -3506,7 +3506,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
 
 // Creates two browsers, drags from first into the second in such a way that
 // no detaching should happen.
-// TODO(crbug.com/1509717): Reenable flaky test.
+// TODO(crbug.com/41482323): Reenable flaky test.
 IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
                        DISABLED_DragDirectlyToSecondWindow) {
   // TODO(pkasting): Crashes when detaching browser.  https://crbug.com/918733
@@ -3756,7 +3756,7 @@ void DragWindowAndVerifyOffset(DetachToBrowserTabDragControllerTest* test,
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
 // TODO(mukai): enable this test on Windows and Linux.
-// TODO(crbug.com/979013): flaky on Mac
+// TODO(crbug.com/41468034): flaky on Mac
 #define MAYBE_OffsetForDraggingTab DISABLED_OffsetForDraggingTab
 #else
 #define MAYBE_OffsetForDraggingTab OffsetForDraggingTab
@@ -3768,7 +3768,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
   ASSERT_FALSE(TabDragController::IsActive());
 }
 
-// TODO(960915): fix flakiness and re-enable this test on mac/linux.
+// TODO(crbug.com/41457552): fix flakiness and re-enable this test on mac/linux.
 IN_PROC_BROWSER_TEST_P(DetachToBrowserTabDragControllerTest,
                        DISABLED_OffsetForDraggingDetachedTab) {
   AddTabsAndResetBrowser(browser(), 1);
@@ -4229,7 +4229,7 @@ void DragSingleTabToSeparateWindowInSecondDisplayStep2(
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 
 // Drags from browser to a second display and releases input.
-// TODO(crbug.com/1499240): Test is flaky on multiple bots.
+// TODO(crbug.com/40940016): Test is flaky on multiple bots.
 IN_PROC_BROWSER_TEST_P(DetachToBrowserInSeparateDisplayTabDragControllerTest,
                        DISABLED_DragSingleTabToSeparateWindowInSecondDisplay) {
   AddTabsAndResetBrowser(browser(), 1);
@@ -4245,7 +4245,7 @@ IN_PROC_BROWSER_TEST_P(DetachToBrowserInSeparateDisplayTabDragControllerTest,
                           start.y() + GetDetachY(tab_strip));
   ASSERT_TRUE(second_display.bounds().Contains(target));
 
-  // TODO(crbug.com/990589): Unit tests should be able to simulate mouse input
+  // TODO(crbug.com/40638870): Unit tests should be able to simulate mouse input
   // without having to call |CursorManager::SetDisplay|.
   ash::Shell::Get()->cursor_manager()->SetDisplay(second_display);
   DragTabAndNotify(
@@ -5151,7 +5151,7 @@ INSTANTIATE_TEST_SUITE_P(
         ::testing::Values("mouse")));
 #endif  // BUILDFLAG(IS_CHROMEOS)
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-// TODO(crbug.com/1488094): Enable Multi Display Test on lacros
+// TODO(crbug.com/40283516): Enable Multi Display Test on lacros
 INSTANTIATE_TEST_SUITE_P(
     TabDragging,
     DifferentDeviceScaleFactorDisplayTabDragControllerTest,
