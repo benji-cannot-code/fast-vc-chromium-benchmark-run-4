d78e6d74370d707304f22f3a3cc598b03d113b9b
Lambros Lambrou <lambroslambrou@chromium.org>
2024-04-11 03:38:02 +0000

    [remoting] Remove SelectSource() call in multi-stream mode.
    In multi-stream mode, ClientSession needed to call SelectSource() on
    each video-stream, just to plumb the correct screen-id through
    WebrtcVideoStream::Core for sending per-frame stats reports. This had
    an unwanted side-effect of also calling SelectSource() on the capturer,
    even though the capturer was already created with that screen-id.
    
    This CL addresses a TODO in DesktopSessionProxy by removing the
    unwanted call. Instead, the screen_id is passed through to Core via
    WebrtcVideoStream::Start(). To facilitate this, the interface method
    ConnectionToClient::StartVideoStream() is changed to accept a
    screen-id instead of a stream-name.
    
    Bug: 331679617
    Change-Id: I9a90ec82b94022e1a4a8d465d62c5816f2c55d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446303
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Auto-Submit: Lambros Lambrou <lambroslambrou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285597}

--- remoting/protocol/connection_unittest.cc
+++ remoting/protocol/connection_unittest.cc
@@ -561,7 +561,7 @@ TEST_P(ConnectionTest, MAYBE_Video) {
 
   std::unique_ptr<VideoStream> video_stream =
       host_connection_->StartVideoStream(
-          "screen_stream", std::make_unique<TestScreenCapturer>());
+          0, std::make_unique<TestScreenCapturer>());
 
   // Receive 5 frames.
   for (int i = 0; i < 5; ++i) {
@@ -586,7 +586,7 @@ TEST_P(ConnectionTest, MAYBE_VideoWithSlowSignaling) {
 
   std::unique_ptr<VideoStream> video_stream =
       host_connection_->StartVideoStream(
-          "screen_stream", base::WrapUnique(new TestScreenCapturer()));
+          0, base::WrapUnique(new TestScreenCapturer()));
 
   WaitNextVideoFrame();
 }
@@ -637,7 +637,7 @@ TEST_P(ConnectionTest, DISABLED_VideoStats) {
 
   std::unique_ptr<VideoStream> video_stream =
       host_connection_->StartVideoStream(
-          "screen_stream", std::make_unique<TestScreenCapturer>());
+          0, std::make_unique<TestScreenCapturer>());
   video_stream->SetEventTimestampsSource(input_event_timestamps_source);
 
   WaitNextVideoFrame();
@@ -700,7 +700,7 @@ TEST_P(ConnectionTest, DISABLED_FirstCaptureFailed) {
   auto capturer = std::make_unique<TestScreenCapturer>();
   capturer->FailNthFrame(0);
   auto video_stream =
-      host_connection_->StartVideoStream("screen_stream", std::move(capturer));
+      host_connection_->StartVideoStream(0, std::move(capturer));
 
   WaitNextVideoFrame();
 }
@@ -713,7 +713,7 @@ TEST_P(ConnectionTest, DISABLED_SecondCaptureFailed) {
   auto capturer = std::make_unique<TestScreenCapturer>();
   capturer->FailNthFrame(1);
   auto video_stream =
-      host_connection_->StartVideoStream("screen_stream", std::move(capturer));
+      host_connection_->StartVideoStream(0, std::move(capturer));
 
   WaitNextVideoFrame();
   WaitNextVideoFrame();
