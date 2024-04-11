f76ff3a049d481f576b2fff8b602cf8f9f9c1160
Joe Laughlin <joel@microsoft.com>
2024-04-11 14:58:49 +0000

    Use PostTaskAndReply to move the PreReadFile call to a different worker thread.
    Change-Id: I74f220127101b544643ddf78f2cfee65dfde5908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5430242
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285829}

--- components/performance_manager/graph/policies/prefetch_virtual_memory_policy.h
+++ components/performance_manager/graph/policies/prefetch_virtual_memory_policy.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERFORMANCE_MANAGER_GRAPH_POLICIES_PREFETCH_VIRTUAL_MEMORY_POLICY_H_
 #define COMPONENTS_PERFORMANCE_MANAGER_GRAPH_POLICIES_PREFETCH_VIRTUAL_MEMORY_POLICY_H_
 
+#include "base/memory/weak_ptr.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/graph/process_node.h"
 
@@ -38,6 +39,8 @@ class PrefetchVirtualMemoryPolicy : public GraphOwned,
 
   base::FilePath file_to_prefetch_;
   base::TimeTicks last_prefetch_time_;
+  bool ongoing_preread_;
+  base::WeakPtrFactory<PrefetchVirtualMemoryPolicy> weak_ptr_factory_;
 };
 
 }  // namespace performance_manager::policies
