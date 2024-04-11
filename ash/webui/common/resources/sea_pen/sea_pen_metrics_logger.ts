be2e481f3716da2e584b8b397690a87617009dce
Jerry Liu <pzliu@google.com>
2024-04-11 15:01:29 +0000

    p13n: log metrics when the user clicks SeaPen thumbnails
    Log the metric Ash.SeaPen.ThumbnailClicked with template ID when the
    user clicks a SeaPen thumbnail.
    
    BUG=b:333462286
    TEST=Manually
    
    Change-Id: Ic90f13a1a64490a3b54e6b0a3b9b201c5e2027af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5440462
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Auto-Submit: Jerry Liu <pzliu@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285831}

--- ash/webui/common/resources/sea_pen/sea_pen_metrics_logger.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_metrics_logger.ts
@@ -8,6 +8,7 @@ import {isPersonalizationApp} from './sea_pen_utils.js';
 
 const enum HistogramName {
   SEA_PEN_TEMPLATE_SUBPAGE = 'Ash.SeaPen.Template',
+  SEA_PEN_THUMBNAIL_CLICKED = 'Ash.SeaPen.ThumbnailClicked',
   SEA_PEN_CREATE_BUTTON = 'Ash.SeaPen.CreateButton',
 }
 
@@ -48,4 +49,10 @@ export function logRecentImageActionMenuItemClick(
 export function logSeaPenVisited() {
   const appName = isPersonalizationApp() ? 'Wallpaper' : 'VcBackground';
   chrome.metricsPrivate.recordBoolean(`Ash.SeaPen.${appName}.Visited`, true);
+}
+
+export function logSeaPenThumbnailClicked(templateId: SeaPenTemplateId) {
+  chrome.metricsPrivate.recordEnumerationValue(
+      HistogramName.SEA_PEN_THUMBNAIL_CLICKED, templateId,
+      SeaPenTemplateId.MAX_VALUE + 1);
 }
