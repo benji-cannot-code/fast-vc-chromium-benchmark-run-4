3808d4cd97609afc159f8992868e1804a8a1c5b5
David Tseng <dtseng@google.com>
2024-04-11 14:08:44 +0000

    [a11y]: eliminate unneeded copies in ui::MergeTreeUpdates flow
    In BrowserAccessibilityManager and ReadAnythingAppModel, we attempt to
    merge tree updates. This results in copying.
    
    Prevent this by:
    - passing non-const references
    - emplacing/moving tree update resources from their source
    
    In read anything, strip calls to std::move:
    https://learn.microsoft.com/en-us/cpp/code-quality/c26478?view=msvc-170
    which advises to not apply std::move to const lvalue references.
    
    R=aleventhal@chromium.org, mschillaci@google.com
    
    Bug: 332302437
    Change-Id: Ie8b911ec0a4c0f1d55d88caf4daabdbb76fe121c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5413915
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285799}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -966,6 +966,9 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature(
                 "UseMoveNotCopyInAXTreeCombiner",
                 "Enables moves instead of copies of snapshot tree data when combining updates."),
+        Flag.baseFeature(
+                "UseMoveNotCopyInMergeTreeUpdate",
+                "Enables moves instead of copies of snapshot tree data when merging udpates."),
         // Add new commandline switches and features above. The final entry should have a
         // trailing comma for cleaner diffs.
     };
