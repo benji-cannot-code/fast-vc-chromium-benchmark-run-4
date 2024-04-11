770c203018718209af13ffdc4dfbe869b1689230
Matt Mueller <mattm@chromium.org>
2024-04-11 18:38:43 +0000

    Support reading test root CAs from a PEM file for ChromeOS testing.
    This replaces the NSS-based one that no longer works.
    
    Bug: 303020351,41468842
    Change-Id: Iaa6037034645197f4b46a0686b2ede152fc4fafa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425584
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285991}

--- base/threading/thread_restrictions.h
+++ base/threading/thread_restrictions.h
@@ -363,6 +363,7 @@ class ProxyConfigServiceWin;
 class ScopedAllowBlockingForSettingGetter;
 namespace internal {
 class AddressTrackerLinux;
+class PemFileCertStore;
 }
 }  // namespace net
 namespace printing {
@@ -634,6 +635,7 @@ class BASE_EXPORT [[maybe_unused, nodiscard]] ScopedAllowBlocking {
   friend class net::ProxyConfigServiceWin;  // http://crbug.com/61453
   friend class net::
       ScopedAllowBlockingForSettingGetter;  // http://crbug.com/69057
+  friend class net::internal::PemFileCertStore;
   friend class printing::LocalPrinterHandlerDefault;
   friend class printing::PrintBackendServiceManager;
   friend class printing::PrintPreviewUIUntrusted;
