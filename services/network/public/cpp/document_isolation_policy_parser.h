9b5dd1a789d2584e73f41bb1ead968dd04fed929
Camille Lamy <clamy@chromium.org>
2024-04-11 13:39:06 +0000

    [DIP] Document-Isolation-Policy definition and parser
    This CL adds the definition of DocumentIsolationPolicy, and a parser to
    parse the Document-Isolation_policy header for navigation requests.
    
    Bug: 332883907
    Change-Id: I033e3f1ccdb7da38f59cfd666788db0655c0ae12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428412
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Auto-Submit: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285782}

---
+++ services/network/public/cpp/document_isolation_policy_parser.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_NETWORK_PUBLIC_CPP_DOCUMENT_ISOLATION_POLICY_PARSER_H_
+#define SERVICES_NETWORK_PUBLIC_CPP_DOCUMENT_ISOLATION_POLICY_PARSER_H_
+
+#include "base/component_export.h"
+
+namespace net {
+class HttpResponseHeaders;
+}
+
+namespace network {
+
+struct DocumentIsolationPolicy;
+
+// Parses the Document-Isolation-Policy and
+// Document-Isolation-Policy-Report-Only headers.
+COMPONENT_EXPORT(NETWORK_CPP)
+DocumentIsolationPolicy ParseDocumentIsolationPolicy(
+    const net::HttpResponseHeaders& headers);
+
+}  // namespace network
+
+#endif  // SERVICES_NETWORK_PUBLIC_CPP_DOCUMENT_ISOLATION_POLICY_PARSER_H_
--- services/network/public/cpp/parsed_headers.cc
+++ services/network/public/cpp/parsed_headers.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/content_security_policy/content_security_policy.h"
 #include "services/network/public/cpp/cross_origin_embedder_policy_parser.h"
 #include "services/network/public/cpp/cross_origin_opener_policy_parser.h"
+#include "services/network/public/cpp/document_isolation_policy_parser.h"
 #include "services/network/public/cpp/features.h"
 #include "services/network/public/cpp/link_header_parser.h"
 #include "services/network/public/cpp/no_vary_search_header_parser.h"
@@ -51,6 +52,11 @@ mojom::ParsedHeadersPtr PopulateParsedHeaders(
       ParseCrossOriginEmbedderPolicy(*headers);
   parsed_headers->cross_origin_opener_policy =
       ParseCrossOriginOpenerPolicy(*headers);
+
+  if (base::FeatureList::IsEnabled(features::kDocumentIsolationPolicy)) {
+    parsed_headers->document_isolation_policy =
+        ParseDocumentIsolationPolicy(*headers);
+  }
 
   std::string origin_agent_cluster;
   headers->GetNormalizedHeader("Origin-Agent-Cluster", &origin_agent_cluster);
