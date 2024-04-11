0a39d906ae25fbfef4e28f04150bdd02a84a8cb7
John Stiles <johnstiles@google.com>
2024-04-11 17:35:17 +0000

    Disallow usage of canvas GPUTexture after endWebGPUAccess.
    After endWebGPUAccess, the GPUTexture is conceptually owned by the
    canvas--and eventually, it should _actually_ have its ownership
    transferred to the canvas.
    
    We can enforce that the GPUTexture is no longer accessible from
    JavaScript by calling destroy() on it. Unfortunately, there is (by
    design) no way to directly test that a GPUTexture has been
    destroyed, so we test this indirectly instead. The test attempts
    to read and write from the texture after endWebGPUAccess is called,
    and expects GPUValidationErrors to occur.
    
    Bug: 41490345
    Change-Id: I6501e64c73e679a572eeb2a994ff51920539a9c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439858
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1285935}

---
+++ third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_access/endWebGPUAccess-destroys-texture.https.worker.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=worker
+
+// ============================================================================
+
+importScripts("/resources/testharness.js");
+importScripts("./webgpu-helpers.js");
+
+// This test parallels endWebGPUAccess-destroys-texture.https.html.
+promise_test(() => {
+    return with_webgpu((adapter, adapterInfo, device) => {
+      return test_endWebGPUAccess_destroys_texture(
+          device,
+          new OffscreenCanvas(50, 50),
+          {});
+    });
+  },
+  'endWebGPUAccess() on a worker should destroy the associated GPUTexture.'
+);
+
+done();
