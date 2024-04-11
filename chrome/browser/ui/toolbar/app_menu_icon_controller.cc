d8b9dfad7a19853b8ef2deb01e5025553c9b7b16
Alison Gale <agale@chromium.org>
2024-04-11 23:08:53 +0000

    Show default browser chip when observer fires
    Moving the default browser prompt manager to be visible to ChromeOS
    to simplify logic in the app menu icon controller.
    
    Bug: 331956101
    Change-Id: If688dc14a758022c6a18a26047d962baeb7bd43d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445714
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286166}

--- chrome/browser/ui/toolbar/app_menu_icon_controller.cc
+++ chrome/browser/ui/toolbar/app_menu_icon_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
+#include "chrome/browser/ui/startup/default_browser_prompt_manager.h"
 #include "chrome/browser/upgrade_detector/upgrade_detector.h"
 #include "chrome/common/channel_info.h"
 #include "components/version_info/channel.h"
@@ -96,6 +97,10 @@ AppMenuIconController::AppMenuIconController(UpgradeDetector* upgrade_detector,
 
   global_error_observation_.Observe(
       GlobalErrorServiceFactory::GetForProfile(profile_));
+#if !BUILDFLAG(IS_CHROMEOS)
+  default_browser_prompt_observation_.Observe(
+      DefaultBrowserPromptManager::GetInstance());
+#endif
 
   upgrade_detector_->AddObserver(this);
 }
@@ -136,6 +141,11 @@ AppMenuIconController::GetTypeAndSeverity() const {
     return {IconType::GLOBAL_ERROR, Severity::MEDIUM};
   }
 #endif
+#if !BUILDFLAG(IS_CHROMEOS)
+  if (DefaultBrowserPromptManager::GetInstance()->get_show_app_menu_prompt()) {
+    return {IconType::DEFAULT_BROWSER_PROMPT, Severity::LOW};
+  }
+#endif
   return {IconType::NONE, Severity::NONE};
 }
 
@@ -153,5 +163,9 @@ void AppMenuIconController::OnGlobalErrorsChanged() {
 }
 
 void AppMenuIconController::OnUpgradeRecommended() {
+  UpdateDelegate();
+}
+
+void AppMenuIconController::OnShowAppMenuPromptChanged() {
   UpdateDelegate();
 }
