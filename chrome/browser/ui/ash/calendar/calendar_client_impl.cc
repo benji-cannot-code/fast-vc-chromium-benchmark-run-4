dbefce9e087f7c00aff7ea2089d3dc507478e927
Artsiom Mitrokhin <amitrokhin@google.com>
2024-04-11 17:10:11 +0000

    Update `CalendarClient::IsDisabledByAdmin` to check another pref
    This change checks another `kContextualGoogleIntegrationsConfiguration`
    pref that contains a list of enabled API integrations.
    
    This new pref/policy will eventually replace the existing one,
    but for now it's enabled (populated with the Calendar entry) by default.
    
    Bug: b/332284872
    Change-Id: I7897b24372b0b44b8041defe61c732f4fee5949c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5436975
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285919}

--- chrome/browser/ui/ash/calendar/calendar_client_impl.cc
+++ chrome/browser/ui/ash/calendar/calendar_client_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/constants/ash_pref_names.h"
+#include "base/containers/contains.h"
 #include "base/functional/callback_helpers.h"
 #include "base/time/time.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
@@ -40,7 +41,10 @@ bool CalendarClientImpl::IsDisabledByAdmin() const {
   // 1) Check the Calendar pref.
   const auto* const pref_service = profile_->GetPrefs();
   if (!pref_service ||
-      !pref_service->GetBoolean(prefs::kCalendarIntegrationEnabled)) {
+      !pref_service->GetBoolean(prefs::kCalendarIntegrationEnabled) ||
+      !base::Contains(pref_service->GetList(
+                          prefs::kContextualGoogleIntegrationsConfiguration),
+                      prefs::kGoogleCalendarIntegrationName)) {
     return true;
   }
 
