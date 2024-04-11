3b2fb80ca38ae08fc3df9b98cbecb51c25ff22ef
Bao-Duy Tran <tranbaoduy@chromium.org>
2024-04-11 05:34:53 +0000

    Graduate and dismantle kHandwriting{LegacyRecognition,LibraryDlc} flags.
    (This must ONLY be submitted once http://cl/621775162 has landed
    in CrOS; the latter landed in CrOS R125-15849.0.0 @
    https://crrev.com/i/7157957)
    
    Both have been enabled-by-default for long enough (since
    https://crrev.com/c/5028535 and https://crrev.com/c/4904452).
    
    Two Virtual Keyboard flags are partially based off them, but have
    since been replaceable with an equivalent but simplified one
    (https://crrev.com/c/5422990) that's ultimately adopted by CrOS 1P
    Virtual Keyboard (in internal repo) at http://cl/621775162.
    
    As of now, CrOS 1P Virtual Keyboard no longer references those
    old Virtual Keyboard flags, and consequently the underlying Cr
    flags are fully obsolete and can be thoroughly dismantled.
    
    Bug: b:319365786, b:316429185
    Change-Id: I393fe96dbffad0fcd089756715aff82e986a2ddf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5422833
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285624}

--- chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
+++ chrome/browser/extensions/api/virtual_keyboard_private/chrome_virtual_keyboard_delegate.cc
@@ -189,21 +189,6 @@ bool IsOndeviceHandwritingEnabledViaCommandLine() {
              ::switches::kOndeviceHandwritingSwitch) == "use_rootfs";
 }
 
-bool IsHandwritingLegacyRecognitionEnabled() {
-  // Disable handwriting DLC flags if device does not have on-device handwriting
-  // (see b/316981973).
-  return IsOndeviceHandwritingEnabledViaCommandLine() &&
-         base::FeatureList::IsEnabled(
-             ash::features::kHandwritingLegacyRecognition);
-}
-
-bool IsHandwritingLibraryDlcEnabled() {
-  // Disable handwriting DLC flags if device does not have on-device handwriting
-  // (see b/316981973).
-  return IsOndeviceHandwritingEnabledViaCommandLine() &&
-         base::FeatureList::IsEnabled(ash::features::kHandwritingLibraryDlc);
-}
-
 }  // namespace
 
 namespace extensions {
@@ -525,8 +510,6 @@ void ChromeVirtualKeyboardDelegate::OnHasInputDevices(
   features.Append(GenerateFeatureFlag("autocorrect", config.auto_correct));
   features.Append(GenerateFeatureFlag("spellcheck", config.spell_check));
   features.Append(GenerateFeatureFlag("handwriting", config.handwriting));
-  features.Append(GenerateFeatureFlag("handwritinglegacyrecognition",
-                                      IsHandwritingLegacyRecognitionEnabled()));
   features.Append(GenerateFeatureFlag(
       "hindiinscriptlayout",
       base::FeatureList::IsEnabled(ash::features::kHindiInscriptLayout)));
@@ -549,8 +532,6 @@ void ChromeVirtualKeyboardDelegate::OnHasInputDevices(
   features.Append(GenerateFeatureFlag(
       "autocorrectparamstuning",
       base::FeatureList::IsEnabled(ash::features::kAutocorrectParamsTuning)));
-  features.Append(GenerateFeatureFlag("handwritinglibrarydlc",
-                                      IsHandwritingLibraryDlcEnabled()));
   features.Append(
       GenerateFeatureFlag("jelly", chromeos::features::IsJellyEnabled()));
   features.Append(GenerateFeatureFlag(
