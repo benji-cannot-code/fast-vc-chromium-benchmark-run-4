7e504e118ebbbd56677da4961014ecc5e42c27c4
Curtis McMullan <curtismcmullan@chromium.org>
2024-04-11 08:01:52 +0000

    editor: announce editor card when shown
    This CL adds a new announcement for the editor feature. It announces the
    card's visibility when it is shown, and informs the user of the keyboard
    interaction required to move focus to the editor card.
    
    BUG=b:331968669
    
    Change-Id: I64d3c99613d756fafcdd0ef85fd4bd7eef3b7270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446825
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285661}

--- chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.cc
+++ chrome/browser/ui/views/editor_menu/editor_menu_promo_card_view.cc
@@ -43,6 +43,8 @@ namespace chromeos::editor_menu {
 namespace {
 
 constexpr char kWidgetName[] = "EditorMenuPromoCardViewWidget";
+constexpr char16_t kCardShownAnnouncement[] =
+    u"Help Me Write, press tab to focus the Help Me Write card.";
 
 constexpr int kPromoCardIconSizeDip = 20;
 
@@ -159,6 +161,14 @@ bool EditorMenuPromoCardView::AcceleratorPressed(
   CHECK(GetWidget());
   GetWidget()->Close();
   return true;
+}
+
+void EditorMenuPromoCardView::OnWidgetVisibilityChanged(views::Widget* widget,
+                                                        bool visible) {
+  if (visible && !queued_announcement_) {
+    GetViewAccessibility().AnnounceAlert(kCardShownAnnouncement);
+    queued_announcement_ = true;
+  }
 }
 
 void EditorMenuPromoCardView::UpdateBounds(
