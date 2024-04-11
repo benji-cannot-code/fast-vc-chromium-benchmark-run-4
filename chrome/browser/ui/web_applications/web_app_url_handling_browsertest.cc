42b7e204f8de2cef361ff08d84a8cbb8cb32ea93
Daniel Murphy <dmurph@chromium.org>
2024-04-11 01:24:04 +0000

    Reland "[dPWA] Enable os integration for tests that specify override."
    This is a reland of commit 3bca1b556e8952fc17ce82723484ef94ad5432c8
    
    See the diff from patchset 1 for the change. I needed to reduce the #
    of apps being installed, as os integration is triggered for all
    of them now. The tests were failing due to timeout.
    
    Original change's description:
    > [dPWA] Enable os integration for tests that specify override.
    >
    > This test ensure that, if a test (or the FakeOsIntegrationManager)
    > creates a ScopedSuppressForTesting, and the test also has created a
    > OsIntegrationTestOverride, then the os integration is executed.
    >
    > This change allows us to gradually phase out the ScopedSuppressForTesting.
    >
    > Change-Id: If67ecb7329ba9ec6a4d104fe424ef53aa7d58043
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434852
    > Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    > Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1284529}
    
    Change-Id: Iae9a1d68567fcbdf64b65323823c8658627aa853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441553
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1285558}

--- chrome/browser/ui/web_applications/web_app_url_handling_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_url_handling_browsertest.cc
@@ -64,7 +64,6 @@ class WebAppUrlHandlingBrowserTest : public WebAppNavigationBrowserTest {
   base::HistogramTester histogram_tester_;
 
  private:
-  OsIntegrationManager::ScopedSuppressForTesting os_hooks_supress_;
   base::test::ScopedFeatureList scoped_feature_list_{
       blink::features::kWebAppEnableUrlHandlers};
 };
