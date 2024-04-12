5e71e1b49128a456f99ef352f9f01c54b0689c8d
Andre Le <leandre@chromium.org>
2024-04-12 18:45:51 +0000

    Mahi: Add loading animated image when waiting for answer a question
    When waiting for an answer, we should display an animated image for
    loading animation.
    
    Fixed: b:330413864
    Change-Id: I2315cee055812d2b966b5cb0b98a1a0b1a7c930d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444809
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286653}

--- ash/system/mahi/mahi_constants.h
+++ ash/system/mahi/mahi_constants.h
@@ -32,6 +32,7 @@ enum ViewId {
   kOutlinesContainer,
   kSummaryLoadingAnimatedImage,
   kOutlinesLoadingAnimatedImage,
+  kAnswerLoadingAnimatedImage,
   kPanelContentsContainer,
   kQuestionTextfield,
   // Since every text bubble label has this ID, the view lookup will
@@ -63,7 +64,7 @@ inline constexpr int kRefreshBannerStackDepth = 25;
 inline constexpr int kPanelCornerRadius = 16;
 
 // Delays used in `FakeMahiManager` for testing.
-inline constexpr int kFakeMahiManagerLoadAnswerDelaySeconds = 1;
+inline constexpr int kFakeMahiManagerLoadAnswerDelaySeconds = 3;
 inline constexpr int kFakeMahiManagerLoadSummaryDelaySeconds = 4;
 inline constexpr int kFakeMahiManagerLoadOutlinesDelaySeconds = 6;
 
