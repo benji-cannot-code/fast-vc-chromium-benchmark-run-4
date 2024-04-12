web-platform-tests/wpt

commit a1a458bfd3fc357dd8eccf4f3f3f06efc5a9a268

update tests which are generic across all TypedArrays for Float16Array

IndexedDB/structured-clone.any.js
@@ -179,6 +179,7 @@ cloneObjectTest(new Uint8Array([0, 1, 254, 255]).buffer, (orig, clone) => {
   new Int16Array([0x0000, 0x0001, 0xFFFE, 0xFFFF]),
   new Int32Array([0x00000000, 0x00000001, 0xFFFFFFFE, 0xFFFFFFFF]),
   new Uint8ClampedArray([0, 1, 254, 255]),
+  new Float16Array([-Infinity, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Infinity, NaN]),
   new Float32Array([-Infinity, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Infinity, NaN]),
   new Float64Array([-Infinity, -Number.MAX_VALUE, -Number.MIN_VALUE, 0,
                     Number.MIN_VALUE, Number.MAX_VALUE, Infinity, NaN])

FileAPI/blob/Blob-constructor.any.js
@@ -290,10 +290,11 @@ test_blob(function() {
     new Int16Array([0x4150, 0x5353]),
     new Uint32Array([0x53534150]),
     new Int32Array([0x53534150]),
+    new Float16Array([2.65625, 58.59375]),
     new Float32Array([0xD341500000])
   ]);
 }, {
-  expected: "PASSPASSPASSPASSPASSPASSPASS",
+  expected: "PASSPASSPASSPASSPASSPASSPASSPASS",
   type: "",
   desc: "Passing typed arrays as elements of the blobParts array should work."
 });

IndexedDB/idb-binary-key-roundtrip.htm
@@ -83,6 +83,7 @@ function view_type_test(type) {
   'Int16Array',
   'Uint32Array',
   'Int32Array',
+  'Float16Array',
   'Float32Array',
   'Float64Array'
 ].forEach((type) => { view_type_test(type); });

WebCryptoAPI/getRandomValues.any.js
@@ -1,12 +1,19 @@
 // Step 1.
 test(function() {
     assert_throws_dom("TypeMismatchError", function() {
+        self.crypto.getRandomValues(new Float16Array(6))
+    }, "Float16Array")
+    assert_throws_dom("TypeMismatchError", function() {
         self.crypto.getRandomValues(new Float32Array(6))
     }, "Float32Array")
     assert_throws_dom("TypeMismatchError", function() {
         self.crypto.getRandomValues(new Float64Array(6))
     }, "Float64Array")
 
+    assert_throws_dom("TypeMismatchError", function() {
+        const len = 65536 / Float16Array.BYTES_PER_ELEMENT + 1;
+        self.crypto.getRandomValues(new Float16Array(len));
+    }, "Float16Array (too long)")
     assert_throws_dom("TypeMismatchError", function() {
         const len = 65536 / Float32Array.BYTES_PER_ELEMENT + 1;
         self.crypto.getRandomValues(new Float32Array(len));

compression/decompression-buffersource.tentative.any.js
@@ -48,6 +48,10 @@ const bufferSourceChunksForDeflate = [
     value: new Uint32Array(new Uint8Array(compressedBytesWithDeflate).buffer)
   },
   {
+    name: 'Float16Array',
+    value: new Float16Array(new Uint8Array(compressedBytesWithDeflate).buffer)
+  },
+  {
     name: 'Float32Array',
     value: new Float32Array(new Uint8Array(compressedBytesWithDeflate).buffer)
   },
@@ -95,6 +99,10 @@ const bufferSourceChunksForGzip = [
     value: new Uint32Array(new Uint8Array(compressedBytesWithGzip).buffer)
   },
   {
+    name: 'Float16Array',
+    value: new Float16Array(new Uint8Array(compressedBytesWithGzip).buffer)
+  },
+  {
     name: 'Float32Array',
     value: new Float32Array(new Uint8Array(compressedBytesWithGzip).buffer)
   },
@@ -140,6 +148,10 @@ const bufferSourceChunksForDeflateRaw = [
   {
     name: 'Uint32Array',
     value: new Uint32Array(new Uint8Array(compressedBytesWithDeflateRaw).buffer)
+  },
+  {
+    name: 'Float16Array',
+    value: new Float16Array(new Uint8Array(compressedBytesWithDeflateRaw).buffer)
   },
   {
     name: 'Float32Array',

encoding/encodeInto.any.js
@@ -129,6 +129,7 @@
  "Uint8ClampedArray",
  "BigInt64Array",
  "BigUint64Array",
+ "Float16Array",
  "Float32Array",
  "Float64Array"].forEach(type => {
   ["ArrayBuffer", "SharedArrayBuffer"].forEach((arrayBufferOrSharedArrayBuffer) => {

fetch/api/basic/request-headers.any.js
@@ -54,6 +54,7 @@ requestHeaders("Fetch with POST with Blob body", url, "POST", new Blob(["Test"])
 requestHeaders("Fetch with POST with ArrayBuffer body", url, "POST", new ArrayBuffer(4), location.origin, "4");
 requestHeaders("Fetch with POST with Uint8Array body", url, "POST", new Uint8Array(4), location.origin, "4");
 requestHeaders("Fetch with POST with Int8Array body", url, "POST", new Int8Array(4), location.origin, "4");
+requestHeaders("Fetch with POST with Float16Array body", url, "POST", new Float16Array(1), location.origin, "2");
 requestHeaders("Fetch with POST with Float32Array body", url, "POST", new Float32Array(1), location.origin, "4");
 requestHeaders("Fetch with POST with Float64Array body", url, "POST", new Float64Array(1), location.origin, "8");
 requestHeaders("Fetch with POST with DataView body", url, "POST", new DataView(new ArrayBuffer(8), 0, 4), location.origin, "4");

fetch/api/basic/request-upload.any.js
@@ -60,6 +60,10 @@ testUpload("Fetch with POST with Int8Array body", url,
   "POST",
   () => new Int8Array(4),
   "\0\0\0\0");
+testUpload("Fetch with POST with Float16Array body", url,
+  "POST",
+  () => new Float16Array(2),
+  "\0\0\0\0");
 testUpload("Fetch with POST with Float32Array body", url,
   "POST",
   () => new Float32Array(1),

benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 110d41099c09b7e8795e888a53861ae93f6d2bd1

IndexedDB: fix structured-clone.any.js WPT
This entire test suite was broken by
https://github.com/web-platform-tests/wpt/pull/45483/commits/a1a458bfd3fc357dd8eccf4f3f3f06efc5a9a268

To restore IDB test coverage, the new (tentative and unimplemented,
AFAICT), type is only tested if it exists.

The other IDB test that started failing with this change,
idb-binary-key-roundtrip.htm, is unchanged because in that case only
one new test is failing rather than the entire suite, i.e. overall
test coverage is not affected.

Bug: 333225369
Change-Id: I45732d279274561b3b2117ac7cede78fe408cdb9
Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448168
Reviewed-by: Ayu Ishii <ayui@chromium.org>
Commit-Queue: Evan Stade <estade@chromium.org>
Cr-Commit-Position: refs/heads/main@{#1286580}

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.js
@@ -171,7 +171,7 @@ cloneObjectTest(new Uint8Array([0, 1, 254, 255]).buffer, (orig, clone) => {
 // TODO SharedArrayBuffer
 
 // Array Buffer Views
-[
+let byteArrays = [
   new Uint8Array([]),
   new Uint8Array([0, 1, 254, 255]),
   new Uint16Array([0x0000, 0x0001, 0xFFFE, 0xFFFF]),
@@ -180,11 +180,17 @@ cloneObjectTest(new Uint8Array([0, 1, 254, 255]).buffer, (orig, clone) => {
   new Int16Array([0x0000, 0x0001, 0xFFFE, 0xFFFF]),
   new Int32Array([0x00000000, 0x00000001, 0xFFFFFFFE, 0xFFFFFFFF]),
   new Uint8ClampedArray([0, 1, 254, 255]),
-  new Float16Array([-Infinity, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Infinity, NaN]),
   new Float32Array([-Infinity, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Infinity, NaN]),
   new Float64Array([-Infinity, -Number.MAX_VALUE, -Number.MIN_VALUE, 0,
                     Number.MIN_VALUE, Number.MAX_VALUE, Infinity, NaN])
-].forEach(value => cloneObjectTest(value, (orig, clone) => {
+]
+
+if (typeof Float16Array !== 'undefined') {
+  byteArrays.push(
+      new Float16Array([-Infinity, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, Infinity, NaN]));
+}
+
+byteArrays.forEach(value => cloneObjectTest(value, (orig, clone) => {
   assert_array_equals(orig, clone);
 }));
 

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_1-20-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Error in remote http://web-platform.test:8001/IndexedDB/structured-clone.any.js: Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_101-last-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_21-40-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Error in remote http://web-platform.test:8001/IndexedDB/structured-clone.any.js: Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_41-60-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Error in remote http://web-platform.test:8001/IndexedDB/structured-clone.any.js: Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_61-80-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Error in remote http://web-platform.test:8001/IndexedDB/structured-clone.any.js: Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any.worker_81-100-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_1-20-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_101-last-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_21-40-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_41-60-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_61-80-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-

third_party/blink/web_tests/external/wpt/IndexedDB/structured-clone.any_81-100-expected.txt
@@ -1,5 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-This is a testharness.js-based test.
-Harness Error. harness_status.status = 1 , harness_status.message = Uncaught ReferenceError: Float16Array is not defined
-Harness: the test ran to completion.
-