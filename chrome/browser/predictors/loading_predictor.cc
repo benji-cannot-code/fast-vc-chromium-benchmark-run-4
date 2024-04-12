ea984aaf8ead1146bec25b1c3cb1420ceb9c3f4b
Minoru Chikamune <chikamune@chromium.org>
2024-04-12 01:52:14 +0000

    Avoid prefetching during browser startup
    Running PrefetchManager during the browser startup could be harmful to
    Startup.Android.Cold.TimeToFirstVisibleContent2. This CL avoids it.
    
    kAvoidLoadingPredictorPrefetchDuringBrowserStartup is enabled by default
    in this CL because this feature flag is intended to be a kill switch.
    
    Bug: 40134562
    Change-Id: I4ec4ff5971cb45348b5dc221b59d79ebd72fd9f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442692
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286245}

--- chrome/browser/predictors/loading_predictor.cc
+++ chrome/browser/predictors/loading_predictor.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram_functions.h"
 #include "build/build_config.h"
+#include "chrome/browser/after_startup_task_utils.h"
 #include "chrome/browser/predictors/lcp_critical_path_predictor/lcp_critical_path_predictor_util.h"
 #include "chrome/browser/predictors/lcp_critical_path_predictor/prewarm_http_disk_cache_manager.h"
 #include "chrome/browser/predictors/loading_data_collector.h"
@@ -403,7 +404,10 @@ void LoadingPredictor::CleanupAbandonedHintsAndNavigations(
 void LoadingPredictor::MaybeAddPreconnect(const GURL& url,
                                           PreconnectPrediction prediction) {
   CHECK(!shutdown_);
-  if (!prediction.prefetch_requests.empty()) {
+  if (!prediction.prefetch_requests.empty() &&
+      (AfterStartupTaskUtils::IsBrowserStartupComplete() ||
+       !base::FeatureList::IsEnabled(
+           features::kAvoidLoadingPredictorPrefetchDuringBrowserStartup))) {
     CHECK(base::FeatureList::IsEnabled(features::kLoadingPredictorPrefetch));
     prefetch_manager()->Start(url, std::move(prediction.prefetch_requests));
   }
