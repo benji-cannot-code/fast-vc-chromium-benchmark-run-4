b6299e0641e31045a8dbc00540db3bf1b8d13acc
Elie Maamari <emaamari@google.com>
2024-04-12 11:44:20 +0000

    ChromeOS Settings WebUI shows AuthPanel based on FF state
    Bug: b:271248452
    Change-Id: Iab016f6bcf822fc245e452237af7cb5efb111e6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366925
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286406}

--- chrome/browser/ui/webui/ash/settings/pages/privacy/privacy_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/privacy/privacy_section.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/no_destructor.h"
+#include "base/strings/utf_string_conversions.h"
 #include "build/branding_buildflags.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_utils.h"
@@ -664,6 +665,11 @@ void PrivacySection::AddLoadTimeData(content::WebUIDataSource* html_source) {
                          chrome::kPrivacyHubGeolocationLearnMoreURL);
 
   html_source->AddString("osSettingsAppId", web_app::kOsSettingsAppId);
+
+  html_source->AddString("authPrompt",
+                         l10n_util::GetStringFUTF16(
+                             IDS_SETTINGS_IN_SESSION_AUTH_ORIGIN_NAME_PROMPT,
+                             u"ChromeOS Settings"));
 
   html_source->AddBoolean("showSecureDnsSetting", IsSecureDnsAvailable());
   html_source->AddBoolean("showSecureDnsOsSettingLink", false);
