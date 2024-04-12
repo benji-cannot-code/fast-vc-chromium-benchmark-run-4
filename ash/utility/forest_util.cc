42f895a60ba35d0c4abbcd15951f3fe941d39bd7
Sammie Quon <sammiequon@chromium.org>
2024-04-12 15:44:08 +0000

    forest: Introduce a utility function that checks for Google emails
    Test: none
    Bug: b:333952534
    Change-Id: I3f1d0eafe1d0f5ccdd69d9e7e9887b9a65882793
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448707
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286508}

---
+++ ash/utility/forest_util.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/utility/forest_util.h"
+
+#include "ash/constants/ash_features.h"
+#include "ash/constants/ash_switches.h"
+#include "ash/session/session_controller_impl.h"
+#include "ash/shell.h"
+#include "google_apis/gaia/gaia_auth_util.h"
+
+namespace ash {
+
+bool IsForestFeatureEnabled() {
+  if (!base::FeatureList::IsEnabled(features::kForestFeature)) {
+    return false;
+  }
+
+  // TODO(http://b/333952534): Remove the google api DEPS changes.
+  if (gaia::IsGoogleInternalAccountEmail(Shell::Get()
+                                             ->session_controller()
+                                             ->GetActiveAccountId()
+                                             .GetUserEmail())) {
+    return true;
+  }
+
+  return switches::IsForestSecretKeyMatched();
+}
+
+}  // namespace ash
