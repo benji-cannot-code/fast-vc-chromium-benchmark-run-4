0f919d735d7327393bdb321082b384b0bedc6f37
Duncan Mercer <mercerd@google.com>
2024-04-12 19:39:29 +0000

    Render bounding box after the user makes a selection
    Use the CSS Houdini Paint API to render a bounding box that
    shows the user their selection. The corners of the bounding box are
    rendered using the Paint API, which will allow us to more easily and
    performantly animate the corners. In addition, the PostSelectionRenderer
    is a separate element as it will be in charge of letting the user grab
    a corner to adjust the region, and since there will be multiple ways to
    make a selection, I decided to have one element handle rendering the
    post selection.
    
    Demo: https://screencast.googleplex.com/cast/NDc0NTcxMTQ4MzYxNzI4MHw4NzUzOWMyMC04Mw
    
    Bug: b/332894403
    Change-Id: I777ca459b9602643aa8faa818ad88adeac0dfd44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5429683
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286694}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './object_layer.js';
 import './text_layer.js';
 import './region_selection.js';
+import './post_selection_renderer.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import type {ObjectLayerElement} from './object_layer.js';
+import type {PostSelectionRendererElement} from './post_selection_renderer.js';
 import type {RegionSelectionElement} from './region_selection.js';
 import {getTemplate} from './selection_overlay.html.js';
 import {DRAG_THRESHOLD, DragFeature, emptyGestureEvent, type GestureEvent, GestureState} from './selection_utils.js';
@@ -17,10 +19,11 @@ import type {TextLayerElement} from './text_layer.js';
 
 export interface SelectionOverlayElement {
   $: {
+    objectSelectionLayer: ObjectLayerElement,
+    postSelectionRenderer: PostSelectionRendererElement,
     regionSelectionLayer: RegionSelectionElement,
     selectionOverlay: HTMLElement,
     textSelectionLayer: TextLayerElement,
-    objectSelectionLayer: ObjectLayerElement,
   };
 }
 
@@ -83,6 +86,7 @@ export class SelectionOverlayElement extends PolymerElement {
     }
 
     this.addDragListeners();
+    this.$.postSelectionRenderer.clearSelection();
 
     this.currentGesture = {
       state: GestureState.STARTING,
