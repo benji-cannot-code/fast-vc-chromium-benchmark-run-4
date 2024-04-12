5737139f43a9c4e7f08d6c8dc23c19841fe19a13
Nan Lin <linnan@chromium.org>
2024-04-12 14:35:19 +0000

    Improve null reports unittests coverage
    Bug: 333746224
    Change-Id: Ic80ba83b8255bb0b69e1a0b6edd97cadf45c5d56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447959
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286470}

--- components/attribution_reporting/aggregatable_utils_unittest.cc
+++ components/attribution_reporting/aggregatable_utils_unittest.cc
@@ -23,6 +23,8 @@ using ::attribution_reporting::mojom::SourceRegistrationTimeConfig;
 
 using ::testing::IsEmpty;
 using ::testing::SizeIs;
+using ::testing::UnorderedElementsAre;
+using ::testing::UnorderedElementsAreArray;
 
 TEST(AggregatableUtilsTest, RoundDownToWholeDaySinceUnixEpoch) {
   const struct {
@@ -73,27 +75,35 @@ TEST(AggregatableUtilsTest,
 
   const auto selective = [](int day) { return day == 0 || day == 5; };
 
+  std::vector<NullAggregatableReport> expected_all;
+  expected_all.reserve(31);
+  for (int i = 0; i < 31; i++) {
+    expected_all.emplace_back(now - base::Days(i));
+  }
   EXPECT_THAT(GetNullAggregatableReports(
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, always_true),
-              SizeIs(31));
+              UnorderedElementsAreArray(expected_all));
   EXPECT_THAT(GetNullAggregatableReports(
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, always_false),
               IsEmpty());
-  EXPECT_THAT(GetNullAggregatableReports(
-                  config,
-                  /*trigger_time=*/now,
-                  /*attributed_source_time=*/std::nullopt, selective),
-              SizeIs(2));
+  EXPECT_THAT(
+      GetNullAggregatableReports(config,
+                                 /*trigger_time=*/now,
+                                 /*attributed_source_time=*/std::nullopt,
+                                 selective),
+      UnorderedElementsAre(NullAggregatableReport(now),
+                           NullAggregatableReport(now - base::Days(5))));
 
+  expected_all.erase(expected_all.begin());
   EXPECT_THAT(
       GetNullAggregatableReports(config,
                                  /*trigger_time=*/now,
                                  /*attributed_source_time=*/now, always_true),
-      SizeIs(30));
+      UnorderedElementsAreArray(expected_all));
   EXPECT_THAT(
       GetNullAggregatableReports(config,
                                  /*trigger_time=*/now,
@@ -103,7 +113,7 @@ TEST(AggregatableUtilsTest,
       GetNullAggregatableReports(config,
                                  /*trigger_time=*/now,
                                  /*attributed_source_time=*/now, selective),
-      SizeIs(1));
+      UnorderedElementsAre(NullAggregatableReport(now - base::Days(5))));
 }
 
 TEST(AggregatableUtilsTest,
@@ -123,7 +133,7 @@ TEST(AggregatableUtilsTest,
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, always_true),
-              SizeIs(1));
+              UnorderedElementsAre(NullAggregatableReport(now)));
   EXPECT_THAT(GetNullAggregatableReports(
                   config,
                   /*trigger_time=*/now,
@@ -133,7 +143,7 @@ TEST(AggregatableUtilsTest,
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, selective),
-              SizeIs(1));
+              UnorderedElementsAre(NullAggregatableReport(now)));
 
   EXPECT_THAT(
       GetNullAggregatableReports(config,
@@ -152,6 +162,39 @@ TEST(AggregatableUtilsTest,
       IsEmpty());
 }
 
+TEST(AggregatableUtilsTest, GetNullAggregatableReports_RoundedTime) {
+  auto config =
+      *AggregatableTriggerConfig::Create(SourceRegistrationTimeConfig::kInclude,
+                                         /*trigger_context_id=*/std::nullopt);
+  const auto generate_func = [](int day) {
+    return day == 3 || day == 4 || day == 5;
+  };
+
+  const base::Time whole_day = base::Time::UnixEpoch() + base::Days(14288);
+
+  const std::vector<base::TimeDelta> time_deltas = {
+      base::TimeDelta(), base::Seconds(1), base::Hours(12),
+      base::Days(1) - base::Seconds(1)};
+
+  for (base::TimeDelta source_time_delta : time_deltas) {
+    SCOPED_TRACE(source_time_delta);
+    for (base::TimeDelta trigger_time_delta : time_deltas) {
+      SCOPED_TRACE(trigger_time_delta);
+
+      // Rounded down to `whole_day` + base::Days(4).
+      base::Time trigger_time = whole_day + base::Days(4) + trigger_time_delta;
+
+      EXPECT_THAT(GetNullAggregatableReports(config, trigger_time,
+                                             // Rounded down to `whole_day`.
+                                             whole_day + source_time_delta,
+                                             generate_func),
+                  UnorderedElementsAre(
+                      NullAggregatableReport(trigger_time - base::Days(3)),
+                      NullAggregatableReport(trigger_time - base::Days(5))));
+    }
+  }
+}
+
 TEST(AggregatableUtilsTest, GetNullAggregatableReports_TriggerContextId) {
   auto config =
       *AggregatableTriggerConfig::Create(SourceRegistrationTimeConfig::kExclude,
@@ -168,17 +211,17 @@ TEST(AggregatableUtilsTest, GetNullAggregatableReports_TriggerContextId) {
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, always_true),
-              SizeIs(1));
+              UnorderedElementsAre(NullAggregatableReport(now)));
   EXPECT_THAT(GetNullAggregatableReports(
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, always_false),
-              SizeIs(1));
+              UnorderedElementsAre(NullAggregatableReport(now)));
   EXPECT_THAT(GetNullAggregatableReports(
                   config,
                   /*trigger_time=*/now,
                   /*attributed_source_time=*/std::nullopt, selective),
-              SizeIs(1));
+              UnorderedElementsAre(NullAggregatableReport(now)));
 
   EXPECT_THAT(
       GetNullAggregatableReports(config,
