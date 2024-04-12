734808cd9dddc37a1d1a529fb43fbc49951a7cf2
Noémie St-Onge <noemies@google.com>
2024-04-12 21:07:36 +0000

    [iOSKAUpgrade] Adjust vertical spacing in manual fill cells
    Currently, the vertical spacing between the UI elements of manual fill
    cells is always the same. With the Keyboard Accessory Upgrade feature,
    this is not the case. Chip buttons are grouped together by a smaller
    spacing, which differs depending on whether chip buttons are labeled or
    not. In the address manual fill cells, there are multiple groups of chip
    buttons:
    https://docs.google.com/presentation/d/1A-Fjiiz9R06GuhWjRrp2KxjlkK_v56seaboqq-ax0Fg/edit#slide=id.g2529a841833_0_51
    
    This CL sets the right spacing above each of the UI elements by:
    - Introducing the ManualFillCellView struct to be able to associate UI
    elements with a type (ElementType enum).
    - Associating a spacing value with each ElementType.
    - Grouping vertical lead chip buttons that should be vertically closer.
    - Creating a ManualFillCellView for each vertical lead views and adding
    them to the different `verticalLeadViews` arrays.
    - Using the view-type bundles from the ManualFillCellView variables to
    create vertical constraints.
    
    Before/After comparison when KA Upgrade is disabled (nothing should have
    changed): https://screenshot.googleplex.com/7PVvmk4VEk3xJNT.png
    
    Before/After comparison when KA Upgrade is enabled:
    https://screenshot.googleplex.com/3yeHHDAQ6hPqLe5.png
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 326406091
    Fixed: 326413069
    Change-Id: If737d50ef2ea3a6b995cf021d4686929dd1b92a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405611
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286750}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_address_cell.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_address_cell.mm
@@ -130,7 +130,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   self.contentInjector = contentInjector;
 
-  NSMutableArray<UIView*>* verticalLeadViews = [[NSMutableArray alloc] init];
+  // Holds the views whose leading anchor is constrained relative to the cell's
+  // leading anchor.
+  std::vector<ManualFillCellView> verticalLeadViews;
 
   NSString* blackText = nil;
   NSString* grayText = nil;
@@ -175,7 +177,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   if (attributedString) {
     self.addressLabel.attributedText = attributedString;
-    [verticalLeadViews addObject:self.addressLabel];
+    AddViewToVerticalLeadViews(self.addressLabel,
+                               ManualFillCellView::ElementType::kOther,
+                               verticalLeadViews);
   }
 
   self.dynamicConstraints = [[NSMutableArray alloc] init];
@@ -183,13 +187,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   BOOL largeTypes = UIContentSizeCategoryIsAccessibilityCategory(
       UIScreen.mainScreen.traitCollection.preferredContentSizeCategory);
 
-  // Name line, first middle and last.
+  // First, middle and last names are presented on the same line when possible.
   NSMutableArray<UIView*>* nameLineViews = [[NSMutableArray alloc] init];
 
   bool showFirstName = address.firstName.length;
   bool showMiddleName = address.middleNameOrInitial.length;
   bool showLastName = address.lastName.length;
 
+  // Holds the chip buttons related to the name that are vertical leads.
+  NSMutableArray<UIView*>* nameGroupVerticalLeadChips =
+      [[NSMutableArray alloc] init];
+
+  // First name chip button.
   if (showFirstName) {
     [self.firstNameButton setTitle:address.firstName
                           forState:UIControlStateNormal];
@@ -199,6 +208,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.firstNameButton.hidden = YES;
   }
 
+  // Middle name chip button.
   if (showMiddleName) {
     [self.middleNameButton setTitle:address.middleNameOrInitial
                            forState:UIControlStateNormal];
@@ -208,6 +218,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.middleNameButton.hidden = YES;
   }
 
+  // Last name chip button.
   if (showLastName) {
     [self.lastNameButton setTitle:address.lastName
                          forState:UIControlStateNormal];
@@ -220,38 +231,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self layMultipleViews:nameLineViews
           withLargeTypes:largeTypes
                  onGuide:self.layoutGuide
-      addFirstLineViewTo:verticalLeadViews];
+      addFirstLineViewTo:nameGroupVerticalLeadChips];
 
-  // Company line.
+  // Holds the chip buttons related to the company name that are vertical leads.
+  NSMutableArray<UIView*>* companyGroupVerticalLeadChips =
+      [[NSMutableArray alloc] init];
+
+  // Company line chip button.
   if (address.company.length) {
     [self.companyButton setTitle:address.company forState:UIControlStateNormal];
-    [verticalLeadViews addObject:self.companyButton];
+    [companyGroupVerticalLeadChips addObject:self.companyButton];
     self.companyButton.hidden = NO;
   } else {
     self.companyButton.hidden = YES;
   }
 
-  // Address line 1.
+  // Holds the chip buttons related to the address that are vertical leads.
+  NSMutableArray<UIView*>* addressGroupVerticalLeadChips =
+      [[NSMutableArray alloc] init];
+
+  // Address line 1 chip button.
   if (address.line1.length) {
     [self.line1Button setTitle:address.line1 forState:UIControlStateNormal];
-    [verticalLeadViews addObject:self.line1Button];
+    [addressGroupVerticalLeadChips addObject:self.line1Button];
     self.line1Button.hidden = NO;
   } else {
     self.line1Button.hidden = YES;
   }
 
-  // Address line 2.
+  // Address line 2 chip button.
   if (address.line2.length) {
     [self.line2Button setTitle:address.line2 forState:UIControlStateNormal];
-    [verticalLeadViews addObject:self.line2Button];
+    [addressGroupVerticalLeadChips addObject:self.line2Button];
     self.line2Button.hidden = NO;
   } else {
     self.line2Button.hidden = YES;
   }
 
-  // Zip and city line.
+  // ZIP code and city are presented on the same line when possible.
   NSMutableArray<UIView*>* zipCityLineViews = [[NSMutableArray alloc] init];
 
+  // ZIP code chip button.
   if (address.zip.length) {
     [self.zipButton setTitle:address.zip forState:UIControlStateNormal];
     [zipCityLineViews addObject:self.zipButton];
@@ -260,6 +280,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.zipButton.hidden = YES;
   }
 
+  // City chip button.
   if (address.city.length) {
     [self.cityButton setTitle:address.city forState:UIControlStateNormal];
     [zipCityLineViews addObject:self.cityButton];
@@ -271,12 +292,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self layMultipleViews:zipCityLineViews
           withLargeTypes:largeTypes
                  onGuide:self.layoutGuide
-      addFirstLineViewTo:verticalLeadViews];
+      addFirstLineViewTo:addressGroupVerticalLeadChips];
 
-  // State and country line.
+  // State and country are presented on the same line when possible.
   NSMutableArray<UIView*>* stateCountryLineViews =
       [[NSMutableArray alloc] init];
 
+  // State chip button.
   if (address.state.length) {
     [self.stateButton setTitle:address.state forState:UIControlStateNormal];
     [stateCountryLineViews addObject:self.stateButton];
@@ -285,6 +307,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.stateButton.hidden = YES;
   }
 
+  // Country chip button.
   if (address.country.length) {
     [self.countryButton setTitle:address.country forState:UIControlStateNormal];
     [stateCountryLineViews addObject:self.countryButton];
@@ -296,26 +319,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self layMultipleViews:stateCountryLineViews
           withLargeTypes:largeTypes
                  onGuide:self.layoutGuide
-      addFirstLineViewTo:verticalLeadViews];
+      addFirstLineViewTo:addressGroupVerticalLeadChips];
 
+  // Holds the chip buttons related to the contact info that are vertical leads.
+  NSMutableArray<UIView*>* contactInfoGroupVerticalLeadChips =
+      [[NSMutableArray alloc] init];
+
+  // Phone number chip button.
   if (address.phoneNumber.length) {
     [self.phoneNumberButton setTitle:address.phoneNumber
                             forState:UIControlStateNormal];
-    [verticalLeadViews addObject:self.phoneNumberButton];
+    [contactInfoGroupVerticalLeadChips addObject:self.phoneNumberButton];
     self.phoneNumberButton.hidden = NO;
   } else {
     self.phoneNumberButton.hidden = YES;
   }
 
+  // Email address chip button.
   if (address.emailAddress.length) {
     [self.emailAddressButton setTitle:address.emailAddress
                              forState:UIControlStateNormal];
-    [verticalLeadViews addObject:self.emailAddressButton];
+    [contactInfoGroupVerticalLeadChips addObject:self.emailAddressButton];
     self.emailAddressButton.hidden = NO;
   } else {
     self.emailAddressButton.hidden = YES;
   }
 
+  AddChipGroupsToVerticalLeadViews(
+      @[
+        nameGroupVerticalLeadChips, companyGroupVerticalLeadChips,
+        addressGroupVerticalLeadChips, contactInfoGroupVerticalLeadChips
+      ],
+      verticalLeadViews);
+
+  // Set and activate constraints.
   AppendVerticalConstraintsSpacingForViews(self.dynamicConstraints,
                                            verticalLeadViews, self.layoutGuide);
   [NSLayoutConstraint activateConstraints:self.dynamicConstraints];
@@ -324,7 +361,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - Private
 
 // Dynamically lay givens `views` on `guide`, adding first view of every
-// generated line to `addFirstLineViewTo`. If `largeTypes` is true, fields are
+// generated line to `verticalLeadViews`. If `largeTypes` is true, fields are
 // laid out vertically one per line, otherwise horizontally on one line.
 // Constraints are added to `self.dynamicConstraints` property.
 - (void)layMultipleViews:(NSArray<UIView*>*)views
