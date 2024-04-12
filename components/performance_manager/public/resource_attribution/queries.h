97cac8b657ab727a59c2df3374e285063d4dbc19
Joe Mason <joenotcharles@google.com>
2024-04-12 02:20:11 +0000

    ResourceAttribution: Add a QueryId for scoped queries
    QueryId's are assigned by QueryScheduler when a ScopedResourceUsageQuery
    starts. They'll be used to map results to queries. This will be needed
    when more flexible query schedules are implemented, but in the short
    term will be used to track which queries have received results for a
    PM node after it's deleted, so that the corresponding ResourceContext
    data can be freed once all queries are notified.
    
    R=fdoray
    
    Bug: 333112603
    Change-Id: Icd12e472da4b89abbd08563f4427136fc1966954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446063
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286255}

--- components/performance_manager/public/resource_attribution/queries.h
+++ components/performance_manager/public/resource_attribution/queries.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace resource_attribution {
 
 namespace internal {
-struct QueryParams;
+class QueryParams;
 class QueryScheduler;
 }
 
