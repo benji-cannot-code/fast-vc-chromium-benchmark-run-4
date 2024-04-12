ff966876766ee8e31daa1477b2f0d80b3fdde6de
Elie Maamari <emaamari@google.com>
2024-04-12 16:22:25 +0000

    Add user avatar icon to in-session auth dialog
    Bug: b:271248452
    Change-Id: I3550ef12c190e8be6fd074afe85032e632c95386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5377810
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286526}

--- ash/in_session_auth/in_session_auth_dialog_contents_view.h
+++ ash/in_session_auth/in_session_auth_dialog_contents_view.h
@@ -6,12 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_IN_SESSION_AUTH_IN_SESSION_AUTH_DIALOG_CONTENTS_VIEW_H_
 #define ASH_IN_SESSION_AUTH_IN_SESSION_AUTH_DIALOG_CONTENTS_VIEW_H_
 
-#include "chromeos/ash/components/auth_panel/impl/auth_panel.h"
+#include "base/memory/raw_ptr.h"
 #include "chromeos/ash/components/auth_panel/public/shared_types.h"
-#include "ui/views/controls/label.h"
 #include "ui/views/view.h"
 
+namespace views {
+
+class Label;
+
+}  // namespace views
+
 namespace ash {
+
+class AnimatedRoundedImageView;
+class AuthHubConnector;
+class AuthPanel;
 
 // The parent view for in-session auth dialogs. This gets created,
 // injected into a widget and shown as part of
@@ -36,10 +45,16 @@ class InSessionAuthDialogContentsView : public views::View {
 
  private:
   void AddVerticalSpacing(int height);
+  void AddUserAvatar();
+  void AddTitle();
   void AddPrompt(const std::string& prompt);
   void AddAuthPanel(base::OnceClosure on_end_authentication,
                     base::RepeatingClosure on_ui_initialized,
                     AuthHubConnector* connector);
+
+  raw_ptr<AnimatedRoundedImageView> avatar_view_ = nullptr;
+
+  raw_ptr<views::Label> title_ = nullptr;
 
   raw_ptr<AuthPanel> auth_panel_;
 
