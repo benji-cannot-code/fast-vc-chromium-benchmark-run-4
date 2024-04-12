46c12a36dc11921efb05ab75872ac05005b6258f
Artsiom Mitrokhin <amitrokhin@google.com>
2024-04-12 22:25:10 +0000

    Delete logic for fetching teacher courses
    Bug: b/333576791
    Change-Id: I3342f6b706f95096d9e8e4b744401e16884d1bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450525
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286804}

--- google_apis/classroom/classroom_api_list_courses_request_unittest.cc
+++ google_apis/classroom/classroom_api_list_courses_request_unittest.cc
@@ -116,13 +116,14 @@ TEST_F(ClassroomApiListCoursesRequestsTest, ListCoursesRequest) {
           Field(&HttpRequest::relative_url,
                 Eq("/v1/courses"
                    "?fields=courses(id%2Cname%2CcourseState)%2CnextPageToken"
+                   "&studentId=test1%40test.com"
                    "&courseStates=ACTIVE")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateSuccessfulResponse())));
 
   base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
       future;
   auto request = std::make_unique<ListCoursesRequest>(
-      request_sender(), /*student_id=*/"", /*teacher_id=*/"",
+      request_sender(), /*student_id=*/"test1@test.com",
       /*page_token=*/"", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
@@ -142,7 +143,6 @@ TEST_F(ClassroomApiListCoursesRequestsTest,
                 Eq("/v1/courses"
                    "?fields=courses(id%2Cname%2CcourseState)%2CnextPageToken"
                    "&studentId=test1%40test.com"
-                   "&teacherId=test2%40test.com"
                    "&courseStates=ACTIVE"
                    "&pageToken=qwerty")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateSuccessfulResponse())));
@@ -151,7 +151,6 @@ TEST_F(ClassroomApiListCoursesRequestsTest,
       future;
   auto request = std::make_unique<ListCoursesRequest>(
       request_sender(), /*student_id=*/"test1@test.com",
-      /*teacher_id=*/"test2@test.com",
       /*page_token=*/"qwerty", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
@@ -169,13 +168,14 @@ TEST_F(ClassroomApiListCoursesRequestsTest, ListCoursesRequestHandlesError) {
           Field(&HttpRequest::relative_url,
                 Eq("/v1/courses"
                    "?fields=courses(id%2Cname%2CcourseState)%2CnextPageToken"
+                   "&studentId=test1%40test.com"
                    "&courseStates=ACTIVE")))))
       .WillOnce(Return(ByMove(TestRequestHandler::CreateFailedResponse())));
 
   base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
       future;
   auto request = std::make_unique<ListCoursesRequest>(
-      request_sender(), /*student_id=*/"", /*teacher_id=*/"",
+      request_sender(), /*student_id=*/"test1@test.com",
       /*page_token=*/"", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
