060f83f25c0e032428bf48d82bcdbc10cee8bdda
Roger Tawa <rogerta@chromium.org>
2024-04-12 02:37:17 +0000

    Read screenshot protection from data controls.
    Adds support for screenshot protection by reading rules set in data
    controls.  Screenshot protection will also be retrieved via a RT URL
    lookup that is not implemented yet.
    
    Bug: 322519161
    Change-Id: Ibda5dc578eee71603d363baff758a2efba667bc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426746
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Roger Tawa <rogerta@chromium.org>
    Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286261}

--- chrome/browser/enterprise/data_protection/data_protection_navigation_observer.cc
+++ chrome/browser/enterprise/data_protection/data_protection_navigation_observer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
+#include "chrome/browser/enterprise/data_controls/rules_service.h"
 #include "chrome/browser/interstitials/enterprise_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/chrome_enterprise_url_lookup_service.h"
@@ -78,6 +79,10 @@ void OnDoLookupComplete(
     return;
   }
 
+  // TODO: This function runs after data protections that come from data
+  // controls have already been saved in page user data.  This means RT UTL
+  // lookup results will override data controls when the protections they
+  // control overlap.  Is that right?
   DataProtectionPageUserData::UpdateRTLookupResponse(
       GetPageFromWebContents(web_contents.get()), identifier,
       std::move(rt_lookup_response));
@@ -150,6 +155,13 @@ void LogVerdictSource(
   base::UmaHistogramEnumeration(kURLVerdictSourceHistogram, verdict_source);
 }
 
+bool IsScreenshotAllowedByDataControls(content::BrowserContext* context,
+                                       const GURL& url) {
+  auto* rules =
+      data_controls::RulesServiceFactory::GetForBrowserContext(context);
+  return rules ? !rules->BlockScreenshots(url) : true;
+}
+
 }  // namespace
 
 // static
@@ -205,7 +217,12 @@ void DataProtectionNavigationObserver::GetDataProtectionSettings(
     return;
   }
 
+  GURL url = web_contents->GetLastCommittedURL();
   std::string identifier = GetIdentifier(profile);
+
+  DataProtectionPageUserData::UpdateScreenshotState(
+      GetPageFromWebContents(web_contents), identifier,
+      IsScreenshotAllowedByDataControls(profile, url));
 
   auto* lookup_service =
       g_lookup_service
@@ -232,7 +249,9 @@ void DataProtectionNavigationObserver::GetDataProtectionSettings(
     DoLookup(lookup_service, web_contents->GetLastCommittedURL(),
              GetIdentifier(profile), std::move(lookup_callback), web_contents);
   } else {
-    std::move(callback).Run(UrlSettings::None());
+    ud = GetUserData(web_contents);
+    DCHECK(ud);
+    std::move(callback).Run(ud->settings());
   }
 }
 
@@ -255,6 +274,8 @@ DataProtectionNavigationObserver::DataProtectionNavigationObserver(
   DCHECK(lookup_service_);
 
   identifier_ = GetIdentifier(web_contents->GetBrowserContext());
+  allow_screenshot_ = IsScreenshotAllowedByDataControls(
+      web_contents->GetBrowserContext(), navigation_handle.GetURL());
 
   // When serving from cache, we expect to find a page user data. So this code
   // skips the call to DoLookup() to prevent an unneeded network request.
@@ -286,6 +307,11 @@ void DataProtectionNavigationObserver::DidRedirectNavigation(
     content::NavigationHandle* navigation_handle) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(!is_from_cache_);
+
+  allow_screenshot_ = allow_screenshot_ && IsScreenshotAllowedByDataControls(
+      navigation_handle->GetWebContents()->GetBrowserContext(),
+      navigation_handle->GetURL());
+
   DoLookup(
       lookup_service_, navigation_handle->GetURL(),
       GetIdentifier(navigation_handle->GetWebContents()->GetBrowserContext()),
@@ -323,7 +349,14 @@ void DataProtectionNavigationObserver::DidFinishNavigation(
     LogVerdictSource(URLVerdictSource::kPageUserData);
     RunPendingNavigationCallback(web_contents(),
                                  std::move(pending_navigation_callback_));
-  } else if (rt_lookup_response_) {
+    return;
+  }
+
+  DataProtectionPageUserData::UpdateScreenshotState(
+      GetPageFromWebContents(navigation_handle->GetWebContents()), identifier_,
+      allow_screenshot_);
+
+  if (rt_lookup_response_.get()) {
     LogVerdictSource(URLVerdictSource::kCachedLookupResult);
     OnDoLookupComplete(web_contents()->GetWeakPtr(),
                        std::move(pending_navigation_callback_), identifier_,
