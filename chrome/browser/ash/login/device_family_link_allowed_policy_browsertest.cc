66345eb3cc714fc61f123e5b1089f464b7354d26
Steven Bennetts <stevenjb@chromium.org>
2024-04-12 23:49:42 +0000

    CrOS: Use NetworkState in delay_network_call.cc
    NetworkPortalDetector::CaptivePortalStatus is being deprecated.
    This removes the dependency from delay_network_call.cc and uses
    NetworkState::connection_state instead.
    
    BUG=b:331264838
    TEST=unit_tests, browser_tests, ash_unittests, chromeos_unittests
    
    Change-Id: I9d7b4b1c01bdbb98f509356cd09f7098f19f8b10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434847
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286860}

--- chrome/browser/ash/login/device_family_link_allowed_policy_browsertest.cc
+++ chrome/browser/ash/login/device_family_link_allowed_policy_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/user_auth_config.h"
 #include "chrome/browser/ash/login/test/user_policy_mixin.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
+#include "chrome/browser/ash/net/delay_network_call.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/lifetime/application_lifetime_chromeos.h"
 #include "chrome/browser/lifetime/termination_notification.h"
@@ -92,6 +93,9 @@ class DeviceFamilyLinkAllowedPolicyTest : public LoginManagerTest {
   }
 
   void LoginFamilyLinkUser() {
+    // TODO(b/333450354): Determine why this is necessary and fix.
+    SetDelayNetworkCallsForTesting(true);
+
     login_manager_.SkipPostLoginScreens();
     UserContext user_context =
         LoginManagerMixin::CreateDefaultUserContext(family_link_user_);
