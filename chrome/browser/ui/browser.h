c22c5c3aeb1557f20f26da243097371559638a5b
Frank Liberato <liberato@chromium.org>
2024-04-12 19:05:02 +0000

    [pip] Associate pip window with web app
    Document Picture in Picture windows weren't properly setting their
    application name.  On Mac, this caused a random browser window to
    receive the focus when the Document PiP window is closed, rather than
    the PWA.
    
    Bug: b/320527670
    Change-Id: I6f184242794392b6e224bbd8cd3fed3fe6f4c440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448181
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286665}

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -271,6 +271,11 @@ class Browser : public TabStripModelObserver,
                                           Profile* profile,
                                           bool user_gesture);
 
+    static CreateParams CreateForPictureInPicture(const std::string& app_name,
+                                                  bool trusted_source,
+                                                  Profile* profile,
+                                                  bool user_gesture);
+
     static CreateParams CreateForDevTools(Profile* profile);
 
     // The browser type.
