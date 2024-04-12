79b844b33aadef90e7fd98ab910acad87186bc24
Wei-Luan Wang <weiluanwang@google.com>
2024-04-12 16:15:36 +0000

    [Telemetry] Use ignoreAdditionalProperties for input union types
    Use [ignoreAdditionalProperties] so that we can use union types directly
    as the input type. In the past, we use object type to avoid parser
    errors.
    
    NO_IFTTT=No behavior change
    
    Bug: b/320394027
    Test: unit_tests -ozone-platform=headless --gtest_filter="*Diagnostics*"
    Test: browser_tests -ozone-platform=headless --gtest_filter="*Telemetry*"
    Change-Id: Ib11120092022b5646f8f1840dc0d68b94ea7d069
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447820
    Reviewed-by: Byron Lee <byronlee@chromium.org>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Reviewed-by: Denny Huang <dennyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286523}

--- chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api_v2_browsertest.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api_v2_browsertest.cc
@@ -101,7 +101,7 @@ IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticsApiV2BrowserTest,
         await chrome.test.assertPromiseRejects(
             chrome.os.diagnostics.isRoutineArgumentSupported({
               memory: {
-                maxTestingMemKib: "more",
+                maxTestingMemKib: -1,
               },
             }),
             'Error: Routine arguments are invalid.'
@@ -249,6 +249,24 @@ IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticsApiV2BrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticsApiV2BrowserTest,
+                       CreateRoutineSuccessWithUnrecognizedRoutineName) {
+  OpenAppUiAndMakeItSecure();
+
+  CreateExtensionAndRunServiceWorker(R"(
+    chrome.test.runTests([
+      async function createUnrecognizedRoutine() {
+        const result = await chrome.os.diagnostics.createRoutine({
+          newRoutine: {},
+        });
+
+        chrome.test.assertTrue(result !== undefined);
+        chrome.test.succeed();
+      }
+    ]);
+  )");
+}
+
+IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticsApiV2BrowserTest,
                        CreateRoutineParseArgumentsError) {
   OpenAppUiAndMakeItSecure();
 
@@ -258,7 +276,7 @@ IN_PROC_BROWSER_TEST_F(TelemetryExtensionDiagnosticsApiV2BrowserTest,
         await chrome.test.assertPromiseRejects(
             chrome.os.diagnostics.createRoutine({
               memory: {
-                maxTestingMemKib: "more",
+                maxTestingMemKib: -1,
               },
             }),
             'Error: Routine arguments are invalid.'
