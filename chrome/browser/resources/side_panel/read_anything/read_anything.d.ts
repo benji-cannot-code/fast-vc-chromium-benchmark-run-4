f27507eb1271b4ba2de5f63aec1b486e09a147cf
Lauren Winston <lwinston@google.com>
2024-04-12 19:57:10 +0000

    Add some more metrics for tracking latency.
    This will help give us more granular data on where any latency delays
    may be coming from.
    
    Bug: 329291869
    Change-Id: I76286ff9d71f0df7735226ad5f54bb8c5a31f734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447881
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286708}

--- chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
@@ -299,5 +299,8 @@ declare namespace chrome {
     // Gets the readable name for a locale code
     function getDisplayNameForLocale(locale: string, displayLocale: string):
         string;
+
+    // Log UmaHistogramLong
+    function logMetric(time: number, metricName: string): void;
   }
 }
--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -758,7 +758,9 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
                  &ReadAnythingAppController::RequestImageDataUrl)
       .SetMethod("getImageDataUrl", &ReadAnythingAppController::GetImageDataUrl)
       .SetMethod("getDisplayNameForLocale",
-                 &ReadAnythingAppController::GetDisplayNameForLocale);
+                 &ReadAnythingAppController::GetDisplayNameForLocale)
+      .SetMethod("logMetric",
+                 &ReadAnythingAppController::LogUmaHistogramLongTimes);
 }
 
 ui::AXNodeID ReadAnythingAppController::RootId() const {
@@ -1443,4 +1445,9 @@ int ReadAnythingAppController::GetAccessibleBoundary(const std::u16string& text,
       shorter_string.length() - 1, ax::mojom::MoveDirection::kBackward,
       ax::mojom::TextAffinity::kDefaultValue);
   return word_ends;
+}
+
+void ReadAnythingAppController::LogUmaHistogramLongTimes(int64_t time,
+                                                         std::string metric) {
+  base::UmaHistogramTimes(metric, base::Milliseconds(time));
 }
