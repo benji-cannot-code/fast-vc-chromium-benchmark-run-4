89713d547719ed377b462412f0e00c16a3f539a5
Jonathan Ross <jonross@chromium.org>
2024-04-12 16:54:20 +0000

    Update FrameSequenceTracker to handle Backfill Frames
    Earlier this year we worked on fixing the termination of traces in
    benchmarks:
      https://chromium-review.googlesource.com/c/chromium/src/+/5212207
    
    Since they ChromeOS identified that their benchmarks can generate a
    large number of Backfill frames. These are for VSyncs where we skipped
    producing a frame, because another was still in process. However we were
    expecting to produce for this VSync.
    
    This means we can process `sequence_number` for Backfills that are newer
    than the `last_ended_frame`.
    
    This change moves the handling of out-of-order drops to be only during
    termination, to address the tracing bug. While restoring the processing
    during the sequence.
    
    We are adding a feature to run a holdback to confirm the shift
    
    Bug: 329877366
    Change-Id: I2211679610a3a6359627c1e22ad0ea2587619af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447595
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286550}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -194,4 +194,8 @@ BASE_FEATURE(kPaintWithGlobalToneMapFilter,
              "PaintWithGlobalToneMapFilter",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kMetricsBackfillAdjustmentHoldback,
+             "MetricsBackfillAdjustmentHoldback",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 }  // namespace features
