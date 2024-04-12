8aa84f2512890d32b127c2db74cbe2299270f222
Shakti Sahu <shaktisahu@chromium.org>
2024-04-12 22:48:01 +0000

    [Tab Group Sync] OnTabGroupRemoved with sync ID
    This CL adds a OnTabGroupRemoved method with sync ID. This will
    be used by the revisit surface.
    
    Bug: b:331449778
    Change-Id: I6a3d8791a79b27e70eddd8393c3f521bda08d080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450204
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286815}

--- chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncRemoteObserver.java
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncRemoteObserver.java
@@ -97,6 +97,9 @@ public final class TabGroupSyncRemoteObserver implements TabGroupSyncService.Obs
         mEnableLocalObserverCallback.onResult(true);
     }
 
+    @Override
+    public void onTabGroupRemoved(String syncId) {}
+
     private TabModel getTabModel() {
         return mTabGroupModelFilter.getTabModel();
     }
