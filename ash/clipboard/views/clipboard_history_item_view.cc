ca28e15adeb66c4bcffe43ecfde18503223fe1e5
andrewxu <andrewxu@chromium.org>
2024-04-12 04:02:21 +0000

    [Excellence] A quick fix on the clipboard history bitmap view
    It is a regression caused by a recent box layout code refactoring
    https://crrev.com/c/5301223
    
    This CL uses a box layout flex specification instead of INT_MAX.
    
    This CL adds a pixel test to protect the clipboard history bitmap
    view. The benchmarks are attached to the issue.
    
    Bug: b/331681809
    Change-Id: I8fd58f78bb1afb960687933a7611b9d8ff688469
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5439797
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: David Black <dmblack@google.com>
    Commit-Queue: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286292}

--- ash/clipboard/views/clipboard_history_item_view.cc
+++ ash/clipboard/views/clipboard_history_item_view.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
 #include "ui/views/layout/fill_layout.h"
+#include "ui/views/layout/layout_types.h"
 #include "ui/views/metadata/view_factory_internal.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/view_targeter_delegate.h"
@@ -141,6 +142,8 @@ class ClipboardHistoryItemView::DisplayView
       AddChildView(
           views::Builder<views::View>()
               .SetLayoutManager(std::make_unique<views::FillLayout>())
+              .SetProperty(views::kBoxLayoutFlexKey,
+                           views::BoxLayoutFlexSpecification())
               .AddChildren(
                   views::Builder<views::BoxLayoutView>()
                       .SetOrientation(views::BoxLayout::Orientation::kVertical)
@@ -171,9 +174,12 @@ class ClipboardHistoryItemView::DisplayView
     } else {
       // Add the item's contents and a delete button that, when visible, takes
       // away some of the contents' horizontal space.
-      AddChildView(views::Builder<views::View>(container->CreateContentsView())
+      AddChildView(views::Builder<views::BoxLayoutView>()
+                       .SetOrientation(views::LayoutOrientation::kVertical)
                        .AddChild(views::Builder<views::View>(
-                           container->CreateDeleteButton()))
+                                     container->CreateContentsView())
+                                     .AddChild(views::Builder<views::View>(
+                                         container->CreateDeleteButton())))
                        .Build());
     }
   }
