1ca6d51665a0a90524bfb0a147de29cfe2789e36
Michele Fan <michelefan@chromium.org>
2024-04-12 19:16:48 +0000

    snap-group: [Reland] Do not show snap groups in partial overview
    Demo: http://b/302212786#comment2
    Fixed: b/302212786
    Test: Added new unit test + manually
    Change-Id: I981a9b6c8aa559643eccd206242d66e50c6ef50d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449871
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286670}

--- ash/wm/snap_group/snap_group_controller.h
+++ ash/wm/snap_group/snap_group_controller.h
@@ -88,6 +88,7 @@ class ASH_EXPORT SnapGroupController : public OverviewObserver,
 
   // OverviewObserver:
   void OnOverviewModeStarting() override;
+  void OnOverviewModeEnding(OverviewSession* overview_session) override;
   void OnOverviewModeEndingAnimationComplete(bool canceled) override;
 
   // display::DisplayObserver:
