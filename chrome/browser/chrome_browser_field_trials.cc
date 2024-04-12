4c2b2b76334d83e29ad82e44d895e5086a6444e0
Eshwar Stalin <estalin@chromium.org>
2024-04-12 20:21:57 +0000

    Moving some functionality out of default browser prompt manager class
    Introducing a new class to move some of the functionality out of the
    default browser prompt manager class
    
    Change-Id: I34cd7d08f0a981a76965591f7ebc85703666d075
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448861
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286724}

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
-#include "chrome/browser/ui/startup/default_browser_prompt_manager.h"
+#include "chrome/browser/ui/startup/default_browser_prompt_trial.h"
 #endif
 
 ChromeBrowserFieldTrials::ChromeBrowserFieldTrials(PrefService* local_state)
@@ -155,6 +155,6 @@ void ChromeBrowserFieldTrials::RegisterSyntheticTrials() {
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_CHROMEOS)
-  DefaultBrowserPromptManager::EnsureStickToDefaultBrowserPromptCohort();
+  DefaultBrowserPromptTrial::EnsureStickToDefaultBrowserPromptCohort();
 #endif
 }
--- chrome/browser/ui/startup/default_browser_prompt_manager.cc
+++ chrome/browser/ui/startup/default_browser_prompt_manager.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/first_run/first_run.h"
-#include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/startup/default_browser_infobar_delegate.h"
@@ -29,43 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 DefaultBrowserPromptManager* DefaultBrowserPromptManager::GetInstance() {
   return base::Singleton<DefaultBrowserPromptManager>::get();
-}
-
-// static
-void DefaultBrowserPromptManager::MaybeJoinDefaultBrowserPromptCohort() {
-  PrefService* local_state = g_browser_process->local_state();
-  if (!local_state) {
-    return;  // Can be null in unit tests;
-  }
-
-  std::string active_study_group =
-      features::kDefaultBrowserPromptRefreshStudyGroup.Get();
-  // If the study group isn't set, don't add the user to the cohort.
-  if (active_study_group.empty()) {
-    return;
-  }
-
-  local_state->SetString(prefs::kDefaultBrowserPromptRefreshStudyGroup,
-                         active_study_group);
-  DefaultBrowserPromptManager::RegisterSyntheticFieldTrial(active_study_group);
-}
-
-// static
-void DefaultBrowserPromptManager::EnsureStickToDefaultBrowserPromptCohort() {
-  PrefService* local_state = g_browser_process->local_state();
-  if (!local_state) {
-    return;  // Can be null in unit tests;
-  }
-
-  auto enrolled_study_group =
-      local_state->GetString(prefs::kDefaultBrowserPromptRefreshStudyGroup);
-  if (enrolled_study_group.empty()) {
-    // The user was not enrolled or exited the study at some point.
-    return;
-  }
-
-  DefaultBrowserPromptManager::RegisterSyntheticFieldTrial(
-      enrolled_study_group);
 }
 
 // static
@@ -202,16 +163,6 @@ void DefaultBrowserPromptManager::OnDismiss() {
 DefaultBrowserPromptManager::DefaultBrowserPromptManager() = default;
 
 DefaultBrowserPromptManager::~DefaultBrowserPromptManager() = default;
-
-// static
-void DefaultBrowserPromptManager::RegisterSyntheticFieldTrial(
-    const std::string& group_name) {
-  CHECK(!group_name.empty());
-
-  ChromeMetricsServiceAccessor::RegisterSyntheticFieldTrial(
-      "DefaultBrowserPromptRefreshSynthetic", group_name,
-      variations::SyntheticTrialAnnotationMode::kCurrentLog);
-}
 
 // static
 bool DefaultBrowserPromptManager::ShouldShowPrompts() {
