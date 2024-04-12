3a49655f3d2bf1e2765fe64e923da2fb52afa522
Salome <salsi@google.com>
2024-04-12 07:12:12 +0000

    [AutofillDataModel] Add new field_type - ADDRESS_HOME_HOUSE_NUMBER_AND_APT.
    As part of our i18n project in Autofill, this CL introduces a new field_type
    ADDRESS_HOME_HOUSE_NUMBER_AND_APT, which is a union of house number and apartment number.
    
    Change-Id: I80319c119459788184e21160942553382845e72a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445173
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Salsi Shekiladze <salsi@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286339}

--- components/autofill/core/browser/field_types.cc
+++ components/autofill/core/browser/field_types.cc
@@ -56,6 +56,8 @@ static constexpr auto kTypeNameToFieldType =
          {"ADDRESS_HOME_APT", ADDRESS_HOME_APT},
          {"ADDRESS_HOME_APT_NUM", ADDRESS_HOME_APT_NUM},
          {"ADDRESS_HOME_APT_TYPE", ADDRESS_HOME_APT_TYPE},
+         {"ADDRESS_HOME_HOUSE_NUMBER_AND_APT",
+          ADDRESS_HOME_HOUSE_NUMBER_AND_APT},
          {"ADDRESS_HOME_CITY", ADDRESS_HOME_CITY},
          {"ADDRESS_HOME_STATE", ADDRESS_HOME_STATE},
          {"ADDRESS_HOME_ZIP", ADDRESS_HOME_ZIP},
@@ -171,6 +173,7 @@ bool IsFillableFieldType(FieldType field_type) {
     case ADDRESS_HOME_APT:
     case ADDRESS_HOME_APT_NUM:
     case ADDRESS_HOME_APT_TYPE:
+    case ADDRESS_HOME_HOUSE_NUMBER_AND_APT:
     case ADDRESS_HOME_CITY:
     case ADDRESS_HOME_STATE:
     case ADDRESS_HOME_ZIP:
@@ -422,6 +425,8 @@ std::string_view FieldTypeToDeveloperRepresentationString(FieldType type) {
       return "Address street location and landmark";
     case ADDRESS_HOME_DEPENDENT_LOCALITY_AND_LANDMARK:
       return "Address locality and landmark";
+    case ADDRESS_HOME_HOUSE_NUMBER_AND_APT:
+      return "House number and apartment number";
     case DELIVERY_INSTRUCTIONS:
       return "Delivery instructions";
     case CREDIT_CARD_NAME_FULL:
@@ -529,6 +534,7 @@ FieldTypeGroup GroupTypeOfFieldType(FieldType field_type) {
     case ADDRESS_HOME_STREET_LOCATION_AND_LANDMARK:
     case ADDRESS_HOME_DEPENDENT_LOCALITY_AND_LANDMARK:
     case DELIVERY_INSTRUCTIONS:
+    case ADDRESS_HOME_HOUSE_NUMBER_AND_APT:
       return FieldTypeGroup::kAddress;
 
     case CREDIT_CARD_NAME_FULL:
--- components/autofill/core/browser/form_parsing/address_field_parser_ng.cc
+++ components/autofill/core/browser/form_parsing/address_field_parser_ng.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/cxx23_to_underlying.h"
 #include "components/autofill/core/browser/autofill_field.h"
 #include "components/autofill/core/browser/data_model/autofill_i18n_api.h"
+#include "components/autofill/core/browser/field_types.h"
 #include "components/autofill/core/browser/form_parsing/autofill_scanner.h"
 
 namespace autofill {
@@ -488,9 +489,11 @@ std::optional<double> AddressFieldParserNG::FindScoreOfBestMatchingRule(
       return Match("ADDRESS_HOME_APT_NUM", 1.0);
     case ADDRESS_HOME_APT:
     case ADDRESS_HOME_APT_TYPE:
-      // ADDRESS_HOME_APT and ADDRESS_HOME_APT_TYPE are currently internal nodes
-      // of the address hierarchy that only exist to parse and format an
-      // address. They don't exist as recognized field types.
+    case ADDRESS_HOME_HOUSE_NUMBER_AND_APT:
+      // ADDRESS_HOME_APT, ADDRESS_HOME_APT_TYPE and
+      // ADDRESS_HOME_HOUSE_NUMBER_AND_APT are currently internal nodes of the
+      // address hierarchy that only exist to parse and format an address. They
+      // don't exist as recognized field types.
       return std::nullopt;
     case ADDRESS_HOME_CITY:
       return Match("CITY", 1.0);
