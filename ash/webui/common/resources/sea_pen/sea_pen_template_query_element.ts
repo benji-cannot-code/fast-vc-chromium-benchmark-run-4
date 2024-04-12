5d77c995a6b16d6f03ade7f410549f712685d7d5
Jason Thai <jasontt@chromium.org>
2024-04-12 01:52:54 +0000

    p13n: reset loading state when template changes
    http://go/scrcast/NTUxODk1NzQ5NzgxMDk0NHxjMTc1ZTgwMS02OA
    http://go/scrcast/NjU4MTk5ODEwODgwMzA3MnwxMDk3ZTIwZC02NQ
    
    Updates the UI to stop showing the loading state when the user switches
    to a new template. Add some logic to handle new requests when the user
    switches template.
    
    BUG=b:331826412
    TEST=browser_tests --gtest_filter="*SeaPen*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: Iff7b60d8633f6531e2c0b8d9561d835bde6fd274
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445201
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286246}

--- ash/webui/common/resources/sea_pen/sea_pen_template_query_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_template_query_element.ts
@@ -154,7 +154,9 @@ export class SeaPenTemplateQueryElement extends WithSeaPenStore {
     beforeNextRender(this, () => {
       this.inspireMeAnimation_ =
           this.shadowRoot?.querySelector<LottieRenderer>('#inspireMeAnimation');
-      this.inspireMeAnimation_!.autoplay = false;
+      if (this.inspireMeAnimation_) {
+        this.inspireMeAnimation_.autoplay = false;
+      }
     });
   }
 
