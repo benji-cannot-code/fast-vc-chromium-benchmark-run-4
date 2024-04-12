af1218c0a36c961fefd48a349f52d02e1d9fb5e8
David Black <dmblack@google.com>
2024-04-12 20:03:32 +0000

    Add command-line switch for container app activation time threshold.
    Adds a command-line switch for the container app activation time
    threshold. Note that this switch will only be used for testing purposes.
    
    See: http://shortn/_Sma6xUWAxA
    
    Bug: b:331212317
    Change-Id: I2a138b3d5676a4980bf8242c39065f92de8598c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449554
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286713}

--- chromeos/constants/chromeos_switches.h
+++ chromeos/constants/chromeos_switches.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_CONSTANTS_CHROMEOS_SWITCHES_H_
 #define CHROMEOS_CONSTANTS_CHROMEOS_SWITCHES_H_
 
+#include <optional>
 #include <string>
 
 #include "base/component_export.h"
+#include "base/time/time.h"
 #include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
 
@@ -17,10 +19,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // in //ash/constants/ash_switches.h.
 namespace chromeos::switches {
 
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+extern const char kContainerAppPreinstallActivationTimeThreshold[];
+
 #if !BUILDFLAG(IS_CHROMEOS_LACROS)
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
 extern const char kContainerAppPreinstallKey[];
+#endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
+COMPONENT_EXPORT(CHROMEOS_CONSTANTS)
+std::optional<base::Time> GetContainerAppPreinstallActivationTimeThreshold();
+
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
 COMPONENT_EXPORT(CHROMEOS_CONSTANTS) std::string GetContainerAppPreinstallKey();
 #endif  // !BUILDFLAG(IS_CHROMEOS_LACROS)
 
