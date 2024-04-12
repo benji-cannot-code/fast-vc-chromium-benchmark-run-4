5bb03b9b88c2acbbaa780f196115729853635a68
Tom Burgin <bur@google.com>
2024-04-12 19:05:44 +0000

    immersive fullscreen: fix oom on window restore
    Restoring a fullscreen window that has an NSToolbar and an
    NSTitlebarAccessoryViewController causes the key view loop to become
    corrupted. The NSToolbar's view points to the
    NSTitlebarAccessoryViewController's view in the key view loop. Under
    certain circumstances this can cause an infinite loop leading to an OOM.
    
    This CL calls `-[NSWindow recalculateKeyViewLoop]` on the browser window
    after a restore. This fixes the key view loop setting the NSToolbar's
    view `-nextKeyView` to nil.
    
    Note: This bug stems from a workaround added because of FB12010731
    (NSTitlebarAccessoryViewController fullScreenMinHeight ignored). Once
    that bug is fixed this workaround can be removed.
    
    For good completeness I have also reported this bug to Apple under
    FB13724144 (restoring a fullscreen window that has an NSToolbar and an
    NSTitlebarAccessoryViewController causes the key view loop to become
    corrupted). Though we would prefer a fix for FB12010731.
    
    Bug: 324812653
    Change-Id: I813b049ec844e26c343395b7e99ff05808b3ffe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445152
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Commit-Queue: Tom Burgin <bur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286666}

--- components/remote_cocoa/app_shim/immersive_mode_tabbed_controller_cocoa.mm
+++ components/remote_cocoa/app_shim/immersive_mode_tabbed_controller_cocoa.mm
@@ -8,15 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/foundation_util.h"
 #include "base/debug/dump_without_crashing.h"
 #include "base/functional/callback_forward.h"
+#import "components/remote_cocoa/app_shim/NSToolbar+Private.h"
 #import "components/remote_cocoa/app_shim/bridged_content_view.h"
-
-// Access the private view that backs the toolbar.
-// TODO(http://crbug.com/40261565): Remove when FB12010731 is fixed in AppKit.
-@interface NSToolbar (ToolbarView)
-// The current usage of this property is readonly. Mark it as such here so we
-// don't invite other usages without a thoughtful change.
-@property(readonly) NSView* _toolbarView;
-@end
 
 namespace remote_cocoa {
 
@@ -211,8 +204,7 @@ void ImmersiveModeTabbedControllerCocoa::TitlebarReveal() {
   // and re-add the tab controller so its view is z-order above the toolbar
   // view. See http://crbug/40283902 for details.
   // TODO(http://crbug.com/40261565): Remove when FB12010731 is fixed in AppKit.
-  if ([toolbar respondsToSelector:@selector(_toolbarView)]) {
-    NSView* toolbar_view = toolbar._toolbarView;
+  if (NSView* toolbar_view = toolbar.privateToolbarView) {
     [toolbar_view.superview addSubview:toolbar_view
                             positioned:NSWindowBelow
                             relativeTo:nil];
