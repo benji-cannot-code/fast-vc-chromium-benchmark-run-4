007151f43060354a1a4a386dbaf1cc73e1c3abef
Livia Seibert <livseibert@gmail.com>
2024-04-13 00:13:39 +0000

    Porting Oak attestation to Chrome.
    Copying over some of the data structures from https://github.com/project-oak/oak/blob/main/oak_attestation_verification/src/claims.rs#L135 to use in https://github.com/project-oak/oak/blob/main/oak_attestation_verification/src/endorsement.rs
    
    Bug: b/324141620
    Change-Id: I61796d724b33d702b12a16db234344d42d736957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448152
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Livia Seibert <livseibert@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286870}

---
+++ device/fido/enclave/verify/claim.h
@@ -0,0 +1,68 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_FIDO_ENCLAVE_VERIFY_CLAIM_H_
+#define DEVICE_FIDO_ENCLAVE_VERIFY_CLAIM_H_
+
+#include <map>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "base/time/time.h"
+
+namespace device::enclave {
+
+struct Subject {
+  Subject(std::string name, std::map<std::string, std::string> digest);
+  Subject();
+  ~Subject();
+
+  std::string name;
+  std::map<std::string, std::string> digest;
+};
+
+template <typename T>
+struct Statement {
+  std::string type;
+  std::string predicate_type;
+  std::vector<Subject> subject;
+  T predicate;
+};
+
+struct ClaimEvidence {
+  ClaimEvidence(std::optional<std::string> role,
+                std::string uri,
+                std::vector<uint8_t> digest);
+  ClaimEvidence();
+  ~ClaimEvidence();
+
+  std::optional<std::string> role;
+  std::string uri;
+  std::vector<uint8_t> digest;
+};
+
+struct ClaimValidity {
+  base::Time not_before;
+  base::Time not_after;
+};
+
+template <typename T>
+struct ClaimPredicate {
+  std::string claim_type;
+  std::optional<T> claim_spec;
+  std::string usage;
+  base::Time issued_on;
+  std::optional<ClaimValidity> validity;
+  std::vector<ClaimEvidence> evidence;
+};
+
+struct Claimless {};
+
+typedef Statement<ClaimPredicate<Claimless>> EndorsementStatement;
+
+}  // namespace device::enclave
+
+#endif  // DEVICE_FIDO_ENCLAVE_VERIFY_CLAIM_H_
