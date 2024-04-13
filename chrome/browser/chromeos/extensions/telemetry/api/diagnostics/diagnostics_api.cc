36756073467fa1948222cb72d9354e9867512db0
Byron Lee <byronlee@google.com>
2024-04-13 01:40:26 +0000

    [Telemetry] Remove feature flag for network bandwidth routine API
    BUG: b/320631510
    TEST: unit_tests --gtest_filter="*TelemetryExtension*" \
          -ozone-platform=headless
    TEST: unit_tests --gtest_filter="*TelemetryDiagnostic*" \
          -ozone-platform=headless
    TEST: browser_tests --gtest_filter="*TelemetryExtension*" \
          -ozone-platform=headless
    
    Change-Id: I846298b5d993324bc75a4e3fa9a7db229f59df64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445332
    Reviewed-by: Denny Huang <dennyh@google.com>
    Commit-Queue: Byron Lee <byronlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1286904}

--- chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api.cc
@@ -66,7 +66,7 @@ ParseRoutineArgumentSupportResult(
 
 bool IsPendingApprovalRoutine(
     const crosapi::mojom::TelemetryDiagnosticRoutineArgumentPtr& arg) {
-  return arg->is_network_bandwidth();
+  return false;
 }
 
 }  // namespace
