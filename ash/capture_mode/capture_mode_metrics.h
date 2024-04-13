c317f07bfe056c48747b0e8f408ecae21a4b7601
Cici Ruan <cuicuiruan@google.com>
2024-04-13 03:03:59 +0000

    gd: Add histogram enum for game dashboard
    This CL adds an enum item in EndRecordingReason.
    
    Bug: b/316036118
    Test: manual test
    Test: ash_unittests --gtest_filter="*GameDashboardCaptureMode*"
    Change-Id: I91bc188558c5b81005d4ce9df14456b4ba29dee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450804
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Cici Ruan <cuicuiruan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1286925}

--- ash/capture_mode/capture_mode_metrics.h
+++ ash/capture_mode/capture_mode_metrics.h
@@ -43,7 +43,8 @@ enum class EndRecordingReason {
   kKeyboardShortcut,
   kGameDashboardStopRecordingButton,
   kGameToolbarStopRecordingButton,
-  kMaxValue = kGameToolbarStopRecordingButton,
+  kGameDashboardTabletMode,
+  kMaxValue = kGameDashboardTabletMode,
 };
 
 // Enumeration of capture bar buttons that can be pressed while in capture mode.
