534b8863c980cab436f827899fdd2fc10e315ba7
Etienne Pierre-doray <etiennep@chromium.org>
2024-04-15 18:41:13 +0000

    [tasks] Make idle request timeout cancelable
    Using a timer makes tasks cancelable, which should reduce
    OOM related to delayed tasks queue.
    
    Change-Id: I6ba4b7101f2ff694303a366d7f1d4b882a668da9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449850
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287514}

--- third_party/blink/renderer/core/dom/scripted_idle_task_controller.cc
+++ third_party/blink/renderer/core/dom/scripted_idle_task_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/probe/core_probes.h"
 #include "third_party/blink/renderer/platform/instrumentation/tracing/trace_event.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread_scheduler.h"
+#include "third_party/blink/renderer/platform/timer.h"
 #include "third_party/blink/renderer/platform/wtf/functional.h"
 #include "third_party/blink/renderer/platform/wtf/ref_counted.h"
 
@@ -32,9 +33,14 @@ class IdleRequestCallbackWrapper
   }
   virtual ~IdleRequestCallbackWrapper() = default;
 
+  void ScheduleTimeout(base::TimeDelta delay) {
+    timeout_.StartOneShot(delay, FROM_HERE);
+  }
+
   static void IdleTaskFired(
       scoped_refptr<IdleRequestCallbackWrapper> callback_wrapper,
       base::TimeTicks deadline) {
+    callback_wrapper->timeout_.Stop();
     if (ScriptedIdleTaskController* controller =
             callback_wrapper->Controller()) {
       // If we are going to yield immediately, reschedule the callback for
@@ -50,14 +56,12 @@ class IdleRequestCallbackWrapper
     callback_wrapper->Cancel();
   }
 
-  static void TimeoutFired(
-      scoped_refptr<IdleRequestCallbackWrapper> callback_wrapper) {
-    if (ScriptedIdleTaskController* controller =
-            callback_wrapper->Controller()) {
-      controller->CallbackFired(callback_wrapper->Id(), base::TimeTicks::Now(),
+  void TimeoutFired(TimerBase*) {
+    if (ScriptedIdleTaskController* controller = Controller()) {
+      controller->CallbackFired(Id(), base::TimeTicks::Now(),
                                 IdleDeadline::CallbackType::kCalledByTimeout);
     }
-    callback_wrapper->Cancel();
+    Cancel();
   }
 
   void Cancel() { controller_ = nullptr; }
@@ -68,10 +72,16 @@ class IdleRequestCallbackWrapper
  private:
   IdleRequestCallbackWrapper(ScriptedIdleTaskController::CallbackId id,
                              ScriptedIdleTaskController* controller)
-      : id_(id), controller_(controller) {}
+      : id_(id),
+        controller_(controller),
+        timeout_(controller->GetExecutionContext()->GetTaskRunner(
+                     TaskType::kIdleTask),
+                 this,
+                 &IdleRequestCallbackWrapper::TimeoutFired) {}
 
   ScriptedIdleTaskController::CallbackId id_;
   WeakPersistent<ScriptedIdleTaskController> controller_;
+  TaskRunnerTimer<IdleRequestCallbackWrapper> timeout_;
 };
 
 }  // namespace internal
@@ -132,13 +142,7 @@ void ScriptedIdleTaskController::ScheduleCallback(
       WTF::BindOnce(&internal::IdleRequestCallbackWrapper::IdleTaskFired,
                     callback_wrapper));
   if (timeout_millis > 0) {
-    GetExecutionContext()
-        ->GetTaskRunner(TaskType::kIdleTask)
-        ->PostDelayedTask(
-            FROM_HERE,
-            WTF::BindOnce(&internal::IdleRequestCallbackWrapper::TimeoutFired,
-                          callback_wrapper),
-            base::Milliseconds(timeout_millis));
+    callback_wrapper->ScheduleTimeout(base::Milliseconds(timeout_millis));
   }
 }
 
@@ -242,7 +246,7 @@ void ScriptedIdleTaskController::ContextUnpaused() {
         ->PostTask(
             FROM_HERE,
             WTF::BindOnce(&internal::IdleRequestCallbackWrapper::TimeoutFired,
-                          callback_wrapper));
+                          callback_wrapper, nullptr));
   }
   pending_timeouts_.clear();
 
