75c7e8128714f1b21d2233a88e84f1deec6dbed1
punithbnayak <punithbnayak@chromium.org>
2024-04-15 15:40:52 +0000

    Replace std::unique_ptr<T[]> with HeapArray for: components/password_manager/core/browser/password_store/login_database_ios.cc
    Bug: 326458832
    Change-Id: Ic0e6f1f592e85965392bdfc0224459b2a3e7384c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446777
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Punith Nayak <punithbnayak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287373}

--- components/password_manager/core/browser/password_store/login_database_ios.cc
+++ components/password_manager/core/browser/password_store/login_database_ios.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/apple/osstatus_logging.h"
 #include "base/apple/scoped_cftyperef.h"
 #include "base/base64.h"
+#include "base/containers/heap_array.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/sys_string_conversions.h"
@@ -127,11 +128,11 @@ OSStatus GetTextFromKeychainIdentifier(const std::string& keychain_identifier,
 
   CFDataRef data = base::apple::CFCast<CFDataRef>(data_cftype.get());
   const size_t size = CFDataGetLength(data);
-  std::unique_ptr<UInt8[]> buffer(new UInt8[size]);
-  CFDataGetBytes(data, CFRangeMake(0, size), buffer.get());
+  auto buffer = base::HeapArray<UInt8>::Uninit(size);
+  CFDataGetBytes(data, CFRangeMake(0, size), buffer.data());
 
   *plain_text = base::UTF8ToUTF16(
-      std::string(static_cast<char*>(static_cast<void*>(buffer.get())),
+      std::string(static_cast<char*>(static_cast<void*>(buffer.data())),
                   static_cast<size_t>(size)));
   return errSecSuccess;
 }
