b57c2596e1edcf4d9ecf70d1f59398b11cba9c91
Adrian Taylor <adetaylor@chromium.org>
2024-04-15 17:21:57 +0000

    Roll getrandom: 0.2.12 => 0.2.14 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * getrandom: 0.2.12 => 0.2.14
    
    The following `cargo vet` audit criteria are required:
    
    * getrandom@0.2.14: does-not-implement-crypto, safe-to-run
    
    Bug: None
    Change-Id: Idf161c68359e941f65d412c0c79da0903053d755
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453435
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Auto-Submit: Adrian Taylor <adetaylor@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287437}

--- third_party/rust/chromium_crates_io/vendor/getrandom-0.2.14/src/error_impls.rs
+++ third_party/rust/chromium_crates_io/vendor/getrandom-0.2.14/src/error_impls.rs
@@ -1,9 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#![cfg_attr(docsrs, doc(cfg(feature = "std")))]
 extern crate std;
 
 use crate::Error;
-use core::convert::From;
 use std::io;
 
 impl From<Error> for io::Error {
