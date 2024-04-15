1612b8c0171c71379276cf2938b92870818aae9d
Joey Arhar <jarhar@chromium.org>
2024-04-15 20:35:21 +0000

    Fix anchor attribute in XML crash
    I added a failure expectation because we still need to figure out if the
    anchor element should work in XML documents or not, but this patch still
    fixes a crash when the anchor attribute is set in XML.
    
    If we decide that the anchor attribute shouldn't work in XML, then I'll
    have to move the IDL and some methods from Element to HTMLElement which
    will be a bigger patch.
    
    Fixed: 334368377
    Change-Id: Id3a016a838a5ce5b3adac445b7bcf7be4bbc3160
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455393
    Commit-Queue: David Baron <dbaron@chromium.org>
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287589}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2736,7 +2736,8 @@ void Element::AttributeChanged(const AttributeModificationParams& params) {
       }
     }
   } else if (params.name == html_names::kAnchorAttr) {
-    if (RuntimeEnabledFeatures::CSSAnchorPositioningEnabled()) {
+    if (IsA<HTMLElement>(this) &&
+        RuntimeEnabledFeatures::CSSAnchorPositioningEnabled()) {
       EnsureAnchorElementObserver().Notify();
       return;
     }
