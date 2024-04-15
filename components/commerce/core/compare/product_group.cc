ab214038172c993acca9ccd62e69feb3e8663f30
Min Qin <qinmin@chromium.org>
2024-04-15 16:54:21 +0000

    Add ProductGroup implementation to ClusterManager
    This CL adds ProductGroup implementation to ClusterManager. When
    a ProductGroup is added, existing candidate products will try to
    add to that group. Similarly, when a new candidate product is added,
    Chrome will try to add it to suitable product groups.
    
    Bug: 333741496
    Change-Id: Ice471d8093da2ccc93f8bbff6680610741334629
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450509
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287415}

---
+++ components/commerce/core/compare/product_group.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/commerce/core/compare/product_group.h"
+
+namespace commerce {
+
+ProductGroup::ProductGroup(const std::string& group_id,
+                           const std::string& title)
+    : group_id(group_id), title(title) {
+  creation_time = base::Time::Now();
+  update_time = creation_time;
+}
+
+ProductGroup::~ProductGroup() = default;
+
+}  // namespace commerce
