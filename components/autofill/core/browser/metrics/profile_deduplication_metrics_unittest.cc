626ae0845a8da5c8fd5d190dbbb7eb4f1beca4aa
Florian Leimgruber <fleimgruber@google.com>
2024-04-15 10:46:29 +0000

    Add TypeOfQuasiDuplicateToken metric
    As defined in the deduplication strategy doc. The metric emits the
    types that make up the profile's duplication rank. As such, these types
    are always setting-visible types.
    
    Bug: b/325452461
    Change-Id: Ic938cd4aee239d2ce909234e17afc777452544b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402644
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287248}

--- components/autofill/core/browser/metrics/profile_deduplication_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/profile_deduplication_metrics_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace autofill::autofill_metrics {
@@ -54,6 +56,28 @@ TEST_F(ProfileDeduplicationMetricsTest,
       "Autofill.Deduplication.ExistingProfiles."
       "RankOfStoredQuasiDuplicateProfiles",
       0);
+}
+
+TEST_F(ProfileDeduplicationMetricsTest, TypeOfQuasiDuplicateToken) {
+  // `a` differs from `b` and `c` only in a single type.
+  AutofillProfile a = test::GetFullProfile();
+  AutofillProfile b = test::GetFullProfile();
+  b.SetRawInfo(COMPANY_NAME, u"different company");
+  AutofillProfile c = test::GetFullProfile();
+  c.SetRawInfo(EMAIL_ADDRESS, u"different-email@gmail.com");
+  base::HistogramTester histogram_tester;
+  LogDeduplicationStartupMetrics({&a, &b, &c}, kLocale);
+  RunUntilIdle();
+  // Expect two samples for `kCompany` and `kEmailAddress`, since:
+  // - `kCompany` and `kEmailAddress` are each emitted once by `a`.
+  // - `kCompany` is emitted once by `b`.
+  // - `kEmailAddress` is emitted once by `c`.
+  EXPECT_THAT(
+      histogram_tester.GetAllSamples("Autofill.Deduplication.ExistingProfiles."
+                                     "TypeOfQuasiDuplicateToken.1"),
+      base::BucketsAre(
+          base::Bucket(SettingsVisibleFieldTypeForMetrics::kCompany, 2),
+          base::Bucket(SettingsVisibleFieldTypeForMetrics::kEmailAddress, 2)));
 }
 
 }  // namespace
