899b8ecae2cc0d51ca43aaf57198c6e2c12d7ce6
seannli <seannli@google.com>
2024-04-15 16:29:04 +0000

    camera: Add run-video-capture-service-in-browser to Ash's flag
    Add run-video-capture-service-in-browser to Ash's flag so that video
    capture service can run on the utility process by disabling it.
    
    Bug: b:333929189
    Test: Flag works well.
    Change-Id: Ieb42960417cec3026209b50fa6d4ac906b49287c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444705
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Sean Li <seannli@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287399}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4083,6 +4083,9 @@ extern const char kReleaseNotesNotificationAlwaysEligibleDescription[];
 extern const char kRenderArcNotificationsByChromeName[];
 extern const char kRenderArcNotificationsByChromeDescription[];
 
+extern const char kRunVideoCaptureServiceInBrowserProcessName[];
+extern const char kRunVideoCaptureServiceInBrowserProcessDescription[];
+
 extern const char kArcWindowPredictorName[];
 extern const char kArcWindowPredictorDescription[];
 
