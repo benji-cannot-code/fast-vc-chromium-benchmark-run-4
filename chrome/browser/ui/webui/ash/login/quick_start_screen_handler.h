eb7f51c5f627f991f98e2d8657ca8af492905d23
Michael Hansen <hansenmichael@google.com>
2024-04-15 19:25:23 +0000

    [QuickStart] Dynamic subtitle on success screen.
    This changes the setup complete screen to show one of two messages
    depending on whether we transferred Wi-Fi credentials as part of the
    Quick Start flow.
    
    "You signed in...":
    https://screenshot.googleplex.com/ApG76dnJy8EBTCE.png
    
    "You connected to Wi-Fi":
    https://screenshot.googleplex.com/8fCYyiJKyMxSCuY.png
    
    Test: Manually tested the applicable scenarios, including forced update.
    
    Bug: b:328681282
    Change-Id: I09122678707cc8806d734616b9a51e36b2bdc066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5451107
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287555}

--- chrome/browser/ui/webui/ash/login/quick_start_screen_handler.h
+++ chrome/browser/ui/webui/ash/login/quick_start_screen_handler.h
@@ -37,7 +37,7 @@ class QuickStartView {
   virtual void ShowConfirmGoogleAccount() = 0;
   virtual void ShowSigningInStep() = 0;
   virtual void ShowCreatingAccountStep() = 0;
-  virtual void ShowSetupCompleteStep() = 0;
+  virtual void ShowSetupCompleteStep(const bool did_transfer_wifi) = 0;
   virtual void SetUserEmail(const std::string email) = 0;
   virtual void SetUserFullName(const std::string full_name) = 0;
   virtual void SetUserAvatar(const std::string avatar_url) = 0;
@@ -70,7 +70,7 @@ class QuickStartScreenHandler final : public QuickStartView,
   void ShowConfirmGoogleAccount() override;
   void ShowSigningInStep() override;
   void ShowCreatingAccountStep() override;
-  void ShowSetupCompleteStep() override;
+  void ShowSetupCompleteStep(const bool did_transfer_wifi) override;
   void SetUserEmail(const std::string email) override;
   void SetUserFullName(const std::string full_name) override;
   void SetUserAvatar(const std::string avatar_url) override;
