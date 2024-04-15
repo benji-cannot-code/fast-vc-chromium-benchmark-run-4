3d7a872fe02f684233878cb2f1e22a41a8de76e6
Florian Leimgruber <fleimgruber@google.com>
2024-04-15 10:35:37 +0000

    Add RankOfStoredQuasiDuplicateProfiles metric
    Adds the Autofill.Deduplication.ExistingProfiles.
    RankOfStoredQuasiDuplicateProfiles metric, as defined in the
    deduplication strategy doc, using the infrastructure built in the prior
    CLs.
    
    Bug: b/325452461
    Change-Id: I2a334b76217cc3f3a6908b8316bd317682f37063
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402636
    Reviewed-by: Matthias Körber <koerber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287244}

--- components/autofill/core/browser/metrics/profile_deduplication_metrics.cc
+++ components/autofill/core/browser/metrics/profile_deduplication_metrics.cc
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/profile_deduplication_metrics.h"
 
 #include <algorithm>
+#include <limits>
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "base/functional/bind.h"
+#include "base/metrics/histogram_functions.h"
+#include "base/strings/strcat.h"
 #include "base/task/thread_pool.h"
 #include "components/autofill/core/browser/address_data_cleaner.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
@@ -20,10 +24,21 @@ namespace autofill::autofill_metrics {
 
 namespace {
 
+constexpr std::string_view kHistogramPrefix =
+    "Autofill.Deduplication.ExistingProfiles.";
+
 void LogDeduplicationStartupMetricsForProfile(
     const AutofillProfile& profile,
     std::vector<FieldTypeSet> min_incompatible_sets) {
-  // TODO(b/325452461): Implement metrics.
+  // The minimum number of fields whose removal makes `profile` a duplicate.
+  const int duplication_rank = min_incompatible_sets.empty()
+                                   ? std::numeric_limits<int>::max()
+                                   : min_incompatible_sets.back().size();
+
+  base::UmaHistogramCounts100(
+      base::StrCat({kHistogramPrefix, "RankOfStoredQuasiDuplicateProfiles"}),
+      duplication_rank);
+  // TODO(b/325452461): Implement more metrics.
 }
 
 }  // namespace
@@ -31,6 +46,10 @@ void LogDeduplicationStartupMetricsForProfile(
 void LogDeduplicationStartupMetrics(
     const std::vector<AutofillProfile*>& profiles,
     const std::string& app_locale) {
+  if (profiles.size() <= 1) {
+    // Don't pollute metrics with cases where obviously no duplicates exists.
+    return;
+  }
   auto log_metrics = [](std::vector<AutofillProfile> profiles,
                         const std::string& app_locale) {
     AutofillProfileComparator comparator(app_locale);
