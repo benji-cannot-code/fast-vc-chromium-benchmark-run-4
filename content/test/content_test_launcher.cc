f606342c181440b926f12143cf9fa9c7cbedbcef
Dave Tapuska <dtapuska@chromium.org>
2024-04-15 19:16:22 +0000

    [ios] Resurrect content_browsertests with BrowserEngineKit APIs
    Reorganize how plist and entitlements are assigned
    to the subprocesses. This allows sharing amongst content shell,
    content_browsertests and component_browsertests. Add an embedder
    hook for the extension processes for content/test.
    
    Change-Id: If7152281d8e57065075b7198983ee1dc93eeb0bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447502
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287540}

--- content/test/content_test_launcher.cc
+++ content/test/content_test_launcher.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/public/test/test_launcher.h"
-
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/i18n/icu_util.h"
@@ -14,9 +12,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_suite.h"
 #include "base/test/test_timeouts.h"
 #include "build/build_config.h"
+#include "build/ios_buildflags.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/content_browser_test_shell_main_delegate.h"
 #include "content/public/test/content_test_suite_base.h"
+#include "content/public/test/test_launcher.h"
 #include "content/shell/common/shell_switches.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/buildflags.h"
@@ -80,7 +80,12 @@ class ContentTestLauncherDelegate : public TestLauncherDelegate {
 
 }  // namespace content
 
-int main(int argc, char** argv) {
+#if BUILDFLAG(IS_IOS) && BUILDFLAG(IS_IOS_APP_EXTENSION)
+extern "C" int ContentProcessMain(int argc, const char** argv)
+#else
+int main(int argc, char** argv)
+#endif
+{
   base::CommandLine::Init(argc, argv);
   size_t parallel_jobs = base::NumParallelJobs(/*cores_per_job=*/2);
   if (parallel_jobs == 0U)
@@ -92,5 +97,6 @@ int main(int argc, char** argv) {
   base::win::PinUser32();
 #endif  // BUILDFLAG(IS_WIN)
   content::ContentTestLauncherDelegate launcher_delegate;
-  return LaunchTests(&launcher_delegate, parallel_jobs, argc, argv);
+  return LaunchTests(&launcher_delegate, parallel_jobs, argc,
+                     const_cast<char**>(argv));
 }
