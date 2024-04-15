14170ff5463a28a7c392a0ecf8cf43a306bf9d61
Luc Nguyen <lucnguyen@google.com>
2024-04-15 21:54:20 +0000

    Populate session hash in system profile
    I also manually verified that independent logs and stability logs
    have the session hash from which the session came from.
    
    Bug: b/333576235
    Change-Id: I7cfa688b807c9b4c17b34f40c2ce652512bc7822
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449684
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287659}

--- components/metrics/metrics_log.cc
+++ components/metrics/metrics_log.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cstring>
 #include <string>
+#include <vector>
 
 #include "base/build_time.h"
 #include "base/command_line.h"
@@ -40,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/pref_service.h"
 #include "components/variations/hashing.h"
+#include "crypto/random.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "third_party/metrics_proto/histogram_event.pb.h"
 #include "third_party/metrics_proto/system_profile.pb.h"
@@ -201,6 +203,14 @@ metrics::SystemProfileProto::OS::XdgCurrentDesktop ToProtoCurrentDesktop(
   return metrics::SystemProfileProto::OS::OTHER;
 }
 #endif  // BUILDFLAG(IS_LINUX)
+
+// Gets the hash of this session. A random hash is generated the first time this
+// is called (which is cached and returned for the remainder of the session).
+uint64_t GetSessionHash() {
+  static const std::vector<uint8_t> session_hash =
+      crypto::RandBytesAsVector(/*length=*/8);
+  return *reinterpret_cast<const uint64_t*>(session_hash.data());
+}
 
 }  // namespace
 
@@ -375,6 +385,8 @@ void MetricsLog::RecordCoreSystemProfile(
   // Set if a build is instrumented (e.g. built with ASAN, or with DCHECKs).
   system_profile->set_is_instrumented_build(true);
 #endif
+
+  system_profile->set_session_hash(GetSessionHash());
 
   metrics::SystemProfileProto::Hardware* hardware =
       system_profile->mutable_hardware();
