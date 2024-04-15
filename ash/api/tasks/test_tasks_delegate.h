6e372c5b43296fe566f3e4dafede89f6a353f5af
Alexander Bolodurin <alexbn@chromium.org>
2024-04-15 01:23:03 +0000

    Revert "Tasks-Api: Fill in TestTasksDelegate"
    This reverts commit 96e14ab43b4458774c8ead1c724842a57e750e62.
    
    Reason for revert: failures on multiple bots for multiple tests.
    
    DoNotDisturbNotificationControllerWithFocusModeTest.*
    FocusModeDetailedViewTest.*
    FocusModeChipCarouselTest.*
    FocusModeControllerMultiUserTest.*
    FocusModeCountdownViewTest.*
    FocusModeFeaturePodControllerTest.*
    
    Bug: b/334223570
    
    Original change's description:
    > Tasks-Api: Fill in TestTasksDelegate
    >
    > This CL fills in the body of TestTasksDelegate so that we can use it for
    > testing in Focus Mode.
    >
    > Bug: b/319307346
    > Change-Id: I7e7985d082227caf410087ad626fb60ae3f072d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5319140
    > Reviewed-by: Hongyu Long <hongyulong@chromium.org>
    > Reviewed-by: Richard Chui <richui@chromium.org>
    > Reviewed-by: Sean Kau <skau@chromium.org>
    > Commit-Queue: Daniel Andersson <dandersson@google.com>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1286608}
    
    Bug: b/319307346
    Change-Id: I21c54e0bc9aade565e46eef117470ddca6049668
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446778
    Owners-Override: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Bolodurin <alexbn@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287122}

--- ash/api/tasks/test_tasks_delegate.h
+++ ash/api/tasks/test_tasks_delegate.h
@@ -8,12 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/api/tasks/tasks_delegate.h"
 #include "ash/ash_export.h"
-#include "base/containers/flat_map.h"
-#include "components/account_id/account_id.h"
 
 namespace ash::api {
-
-class FakeTasksClient;
 
 class ASH_EXPORT TestTasksDelegate : public TasksDelegate {
  public:
@@ -37,20 +33,6 @@ class ASH_EXPORT TestTasksDelegate : public TasksDelegate {
                   const std::string& title,
                   bool completed,
                   TasksClient::OnTaskSavedCallback callback) override;
-
-  // Helper function for adding pre-build `FakeTasksClient` objects.
-  void AddFakeTasksClient(const AccountId& account_id,
-                          std::unique_ptr<FakeTasksClient> tasks_client);
-
-  // Switches the active account for the delegate.
-  void SetActiveAccount(const AccountId& account_id);
-
- private:
-  // The client for the active account.
-  raw_ptr<FakeTasksClient> active_client_ = nullptr;
-
-  // Maps account IDs to clients.
-  base::flat_map<AccountId, std::unique_ptr<FakeTasksClient>> clients_;
 };
 
 }  // namespace ash::api
