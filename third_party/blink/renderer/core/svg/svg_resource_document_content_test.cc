f7535e9b3ad0ca418beb337659eda1dae48d8b43
Sébastien Lalancette <seblalancette@chromium.org>
2024-04-15 18:06:05 +0000

    Revert "Restore check for zero-length payload in SVGDocumentResource"
    This reverts commit 05863a1a7bfc53999fe8395628265a196f50345d.
    
    Reason for revert: New test is failing consistently in many builders, e.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/11392/overview
    
    Original change's description:
    > Restore check for zero-length payload in SVGDocumentResource
    >
    > Was lost in 5cf4c81eec1566f43833f7c23a6f6abc5e9505c6.
    >
    > Fixed: 334840707
    > Bug: 40134477
    > Change-Id: I253179a1e5ffb682f04e8480ef3cb53483e33e6b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453656
    > Auto-Submit: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1287381}
    
    Bug: 40134477
    Change-Id: I80c86250369ece951d68fa1dbb1c4bb83c81b7cd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455053
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Owners-Override: Sébastien Lalancette <seblalancette@chromium.org>
    Owners-Override: Matt Jones <mdjones@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1287481}

--- third_party/blink/renderer/core/svg/svg_resource_document_content_test.cc
+++ third_party/blink/renderer/core/svg/svg_resource_document_content_test.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
-#include "third_party/blink/renderer/core/testing/page_test_base.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_request.h"
 #include "third_party/blink/renderer/core/testing/sim/sim_test.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_initiator_type_names.h"
@@ -15,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-class SVGResourceDocumentContentSimTest : public SimTest {};
+class SVGResourceDocumentContentTest : public SimTest {};
 
-TEST_F(SVGResourceDocumentContentSimTest, GetDocumentBeforeLoadComplete) {
+TEST_F(SVGResourceDocumentContentTest, GetDocumentBeforeLoadComplete) {
   SimRequest main_resource("https://example.com/test.html", "text/html");
   LoadURL("https://example.com/test.html");
   main_resource.Complete("<html><body></body></html>");
@@ -44,20 +43,6 @@ TEST_F(SVGResourceDocumentContentSimTest, GetDocumentBeforeLoadComplete) {
   // Finish the response, the Document should now be accessible.
   svg_resource.Complete("g></svg>");
   EXPECT_NE(nullptr, entry->GetDocument());
-}
-
-class SVGResourceDocumentContentTest : public PageTestBase {};
-
-TEST_F(SVGResourceDocumentContentTest, EmptyDataUrl) {
-  const char kEmptySVGImageDataUrl[] = "data:image/svg+xml,";
-  ExecutionContext* execution_context = GetDocument().GetExecutionContext();
-  ResourceLoaderOptions options(execution_context->GetCurrentWorld());
-  options.initiator_info.name = fetch_initiator_type_names::kCSS;
-  FetchParameters params(ResourceRequest(kEmptySVGImageDataUrl), options);
-  auto* content = SVGResourceDocumentContent::Fetch(params, GetDocument());
-
-  EXPECT_TRUE(content->IsLoaded());
-  EXPECT_TRUE(content->ErrorOccurred());
 }
 
 }  // namespace blink
