22da58eb099a8d6e1eac45c8e8503fa98771f690
Elad Alon <eladalon@chromium.org>
2024-04-15 17:55:01 +0000

    Refactor stop-button code into TabSharingInfoBarDelegate::StopButton
    By placing the code related to the stop-button into its own class,
    we bring the code around this button in alignment with the other
    buttons in the tab-sharing infobar. This allows a follow-up CL to
    consolidate some of the logic behind a helper, GetButton().
    
    DanglingUntriaged-notes: The new instance of a dangling pointer
    is actually just a replication of the dangling pointer in the
    owning class. Both dangling pointers have the same lifetime,
    so the problem is not aggravated, it just changes in appearance.
    
    Bug: None
    Change-Id: Ibe80c53bedb13ab131f787e488bf7ec95845dbc2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450017
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287469}

--- chrome/browser/ui/tab_sharing/tab_sharing_infobar_delegate.h
+++ chrome/browser/ui/tab_sharing/tab_sharing_infobar_delegate.h
@@ -80,6 +80,7 @@ class TabSharingInfoBarDelegate : public infobars::InfoBarDelegate {
   };
 
   class TabSharingInfoBarDelegateButton;
+  class StopButton;
   class ShareTabInsteadButton;
   class SwitchToTabButton;
   class CscIndicatorButton;
@@ -161,6 +162,7 @@ class TabSharingInfoBarDelegate : public infobars::InfoBarDelegate {
   // Indicates whether this instance is used for casting or capturing.
   const TabShareType capture_type_;
 
+  std::unique_ptr<StopButton> stop_button_;
   std::unique_ptr<ShareTabInsteadButton> share_this_tab_instead_button_;
   std::unique_ptr<SwitchToTabButton> quick_nav_button_;
   std::unique_ptr<CscIndicatorButton> csc_indicator_button_;
