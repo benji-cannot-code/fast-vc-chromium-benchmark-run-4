7332cc40c538ba0454aceb2814d1a47d67b1fe65
Duong Dac <ddac@google.com>
2024-04-15 08:44:44 +0000

    [iOS] Record Capabilities Fetching latency metrics
    This CL migrates metrics recording from the History Sync screen (where
    we no longer wait for capabilities since M124), to the capabilities
    fetcher.
    
    Bug: 333859353, 333858998
    
    Change-Id: I48f4beedccef73abe5460a52f3d6d1b188617f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444629
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287195}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_view_controller.mm
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/feature_list.h"
 #import "base/metrics/histogram_functions.h"
-#import "base/timer/elapsed_timer.h"
 #import "components/signin/public/base/signin_metrics.h"
 #import "components/signin/public/base/signin_switches.h"
 #import "ios/chrome/browser/shared/ui/elements/activity_overlay_view.h"
@@ -30,11 +29,7 @@ constexpr base::TimeDelta kAnimationDuration = base::Milliseconds(200);
 
 @end
 
-@implementation HistorySyncViewController {
-  // Tracks the duration between when the view appeared with hidden buttons
-  // and when the buttons are shown.
-  std::unique_ptr<base::ElapsedTimer> _userVisibileLatency;
-}
+@implementation HistorySyncViewController
 
 @dynamic delegate;
 
@@ -73,11 +68,6 @@ constexpr base::TimeDelta kAnimationDuration = base::Milliseconds(200);
       [self.view addSubview:self.overlay];
       AddSameConstraints(self.view, self.overlay);
       [self.overlay.indicator startAnimating];
-
-      // Record availability metrics and start the latency timer.
-      base::UmaHistogramBoolean(
-          "Signin.AccountCapabilities.ImmediatelyAvailable", false);
-      _userVisibileLatency = std::make_unique<base::ElapsedTimer>();
     }
   }
 }
@@ -108,15 +98,7 @@ constexpr base::TimeDelta kAnimationDuration = base::Milliseconds(200);
 - (void)displayButtonsWithRestrictionStatus:(BOOL)isRestricted {
   if (base::FeatureList::IsEnabled(
           switches::kMinorModeRestrictionsForHistorySyncOptIn)) {
-    if (self.actionButtonsVisibility == ActionButtonsVisibility::kDefault) {
-      // Buttons are updated without ever being hidden when capabilities are
-      // immediately available.
-      base::UmaHistogramBoolean(
-          "Signin.AccountCapabilities.ImmediatelyAvailable", true);
-      base::UmaHistogramTimes("Signin.AccountCapabilities.UserVisibleLatency",
-                              base::Seconds(0));
-    } else if (self.actionButtonsVisibility ==
-               ActionButtonsVisibility::kHidden) {
+    if (self.actionButtonsVisibility == ActionButtonsVisibility::kHidden) {
       // Fade out the spinner while fading in the title and subtitle.
       // The buttons will be shown simultaneously.
       __weak __typeof(self) weakSelf = self;
@@ -132,7 +114,6 @@ constexpr base::TimeDelta kAnimationDuration = base::Milliseconds(200);
           }
           completion:^(BOOL finished) {
             [weakSelf.overlay removeFromSuperview];
-            [weakSelf recordLatencyMetrics];
           }];
     }
 
@@ -145,17 +126,6 @@ constexpr base::TimeDelta kAnimationDuration = base::Milliseconds(200);
             ? signin_metrics::SyncButtonsType::kHistorySyncEqualWeighted
             : signin_metrics::SyncButtonsType::kHistorySyncNotEqualWeighted;
     base::UmaHistogramEnumeration("Signin.SyncButtons.Shown", buttonType);
-  }
-}
-
-#pragma mark - HistorySyncConsumer
-
-- (void)recordLatencyMetrics {
-  if (_userVisibileLatency) {
-    base::TimeDelta elapsed = _userVisibileLatency->Elapsed();
-    base::UmaHistogramTimes("Signin.AccountCapabilities.UserVisibleLatency",
-                            elapsed);
-    base::UmaHistogramTimes("Signin.AccountCapabilities.FetchLatency", elapsed);
   }
 }
 
