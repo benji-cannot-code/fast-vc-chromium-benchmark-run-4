fd1227cdb851e55856ead448a690bf84c5821065
Katie Dektar <katie@chromium.org>
2024-04-15 04:33:35 +0000

    [A11y] Large cursor pref shouldn't reset after disabling feature
    Users want to toggle large cursor on and off and have the "on" state
    remember what cursor size they'd previously chosen.
    
    Test: New, manual
    Bug: b/333895941
    Change-Id: Idbfed596a8264d6bce20af946214a51f69479d01
    AX-Relnotes: Fixes bug where large cursor size was reset after disable
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449447
    Reviewed-by: Gina Domergue <gdomergue@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287142}

--- ash/accessibility/accessibility_controller.cc
+++ ash/accessibility/accessibility_controller.cc
@@ -2623,14 +2623,10 @@ FloatingMenuPosition AccessibilityController::GetFloatingMenuPosition() {
 
 void AccessibilityController::UpdateLargeCursorFromPref() {
   DCHECK(active_user_prefs_);
-  const bool enabled =
-      active_user_prefs_->GetBoolean(prefs::kAccessibilityLargeCursorEnabled);
-  // Reset large cursor size to the default size when large cursor is disabled.
-  if (!enabled) {
-    active_user_prefs_->ClearPref(prefs::kAccessibilityLargeCursorDipSize);
-  }
-  const int size =
-      active_user_prefs_->GetInteger(prefs::kAccessibilityLargeCursorDipSize);
+  const bool enabled = large_cursor().enabled();
+  const int size = enabled ? active_user_prefs_->GetInteger(
+                                 prefs::kAccessibilityLargeCursorDipSize)
+                           : kDefaultLargeCursorSize;
 
   if (large_cursor_size_in_dip_ == size) {
     return;
@@ -2641,9 +2637,8 @@ void AccessibilityController::UpdateLargeCursorFromPref() {
   NotifyAccessibilityStatusChanged();
 
   Shell* shell = Shell::Get();
-  shell->cursor_manager()->SetCursorSize(large_cursor().enabled()
-                                             ? ui::CursorSize::kLarge
-                                             : ui::CursorSize::kNormal);
+  shell->cursor_manager()->SetCursorSize(enabled ? ui::CursorSize::kLarge
+                                                 : ui::CursorSize::kNormal);
   shell->SetLargeCursorSizeInDip(large_cursor_size_in_dip_);
   shell->UpdateCursorCompositingEnabled();
 }
@@ -3108,10 +3103,6 @@ void AccessibilityController::UpdateFeatureFromPref(FeatureType feature) {
           enabled);
       break;
     case FeatureType::kLargeCursor:
-      if (!enabled) {
-        active_user_prefs_->ClearPref(prefs::kAccessibilityLargeCursorDipSize);
-      }
-
       Shell::Get()->cursor_manager()->SetCursorSize(
           large_cursor().enabled() ? ui::CursorSize::kLarge
                                    : ui::CursorSize::kNormal);
