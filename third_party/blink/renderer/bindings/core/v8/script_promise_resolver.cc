41676bb534ce0979b72d1d2e0d4fd4c6e9d717df
Nate Chapin <japhet@chromium.org>
2024-04-15 22:45:32 +0000

    Don't Detach() when trying to resolve a ScriptPromiseResolver in a destroyed context
    Change-Id: I34f30d2e8c7eb58c701374f3fae2cd8a437eca36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454036
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287692}

--- third_party/blink/renderer/bindings/core/v8/script_promise_resolver.cc
+++ third_party/blink/renderer/bindings/core/v8/script_promise_resolver.cc
@@ -200,7 +200,6 @@ void ScriptPromiseResolverBase::ScheduleResolveOrReject() {
 void ScriptPromiseResolverBase::ResolveOrRejectDeferred() {
   DCHECK(state_ == kResolving || state_ == kRejecting);
   if (!GetExecutionContext()) {
-    Detach();
     return;
   }
 
