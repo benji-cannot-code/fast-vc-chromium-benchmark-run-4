209308a053d08789ad58a3ea8c2f8ed011fa014a
Samuel Huang <huangs@chromium.org>
2024-04-15 19:10:07 +0000

    [Android TRM] Add and use LocalTabSuggestionEntry.
    A SuggestionEntry in Tab Resumption Module represents one of two case:
    * Foreign Session: This came from a tab from a foreign device. Clicking
      would open the stored URL in a new tab.
    * (WIP) Local Tab: This tracks an existing tab. Click would navigate to
      the existing tab (and close the NTP).
    
    Previously both use cases SuggestionEntry, and are distinguished by the
    nullity of |tab| field (if null then Foreign Session; else Local Tab).
    
    This CL refactors the TRM by deriving LocalTabSuggestionEntry from
    SuggestionEntry (which represent the Foreign Session case for now). This
    leads to:
    * Removal of "/* tab= */ null" when making new SuggestionEntry, esp. for
      tests, which makes tests less verbose.
    * Better case detection via "entry instanceof LocalTabSuggestionEntry".
    * Simplified CTOR for LocalTabSuggestionEntry: Only Tab is needed.
    
    Also change TabResumptionTileView.bindSuggestionClickCallback() to take
    param SuggestionEntry instead of GURL and Tab.
    
    Bug: 332588018
    Change-Id: I9c69e26e54e32ef0ed2a8e8ef4d031cb35b15863
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454952
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287536}

--- chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionTileView.java
+++ chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionTileView.java
@@ -14,10 +14,8 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab_resumption.TabResumptionModuleMetricsUtils.ClickInfo;
 import org.chromium.chrome.browser.tab_resumption.TabResumptionModuleUtils.SuggestionClickCallbacks;
-import org.chromium.url.GURL;
 
 /**
  * The view for a tab suggestion tile. These tile comes in two variants: A larger one for the
@@ -68,8 +66,7 @@ public class TabResumptionTileView extends RelativeLayout {
     /** Binds the click handler with an associated URL. */
     public void bindSuggestionClickCallback(
             SuggestionClickCallbacks callbacks,
-            @Nullable GURL url,
-            @Nullable Tab tab,
+            SuggestionEntry entry,
             int tileCount,
             int tileIndex) {
         setOnClickListener(
@@ -78,10 +75,11 @@ public class TabResumptionTileView extends RelativeLayout {
                     int clickInfo =
                             TabResumptionModuleMetricsUtils.computeClickInfo(tileCount, tileIndex);
                     TabResumptionModuleMetricsUtils.recordClickInfo(clickInfo);
-                    if (tab != null) {
-                        callbacks.onSuggestionClickByTabId(tab.getId());
+                    if (entry instanceof LocalTabSuggestionEntry) {
+                        callbacks.onSuggestionClickByTabId(
+                                ((LocalTabSuggestionEntry) entry).tab.getId());
                     } else {
-                        callbacks.onSuggestionClickByUrl(url);
+                        callbacks.onSuggestionClickByUrl(entry.url);
                     }
                 });
         // Handle and return false to avoid obstructing long click handling of containing Views.
