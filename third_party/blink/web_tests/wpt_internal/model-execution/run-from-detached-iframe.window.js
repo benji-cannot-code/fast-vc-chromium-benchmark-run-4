316d0e65b108269e965e260a1333667c42e5518f
Mingyu Lei <leimy@chromium.org>
2024-04-15 10:33:28 +0000

    Bundz: add WPT to test the execution API
    This CL adds more WPT to test the model execution API with the mock
    implementation. It also remove the entry from VirtualTestSuites and use
    configuration in fieldtrial_testing_config.json to enable the feature.
    
    Bug: 330833021
    Change-Id: I8bf81ef7530a438d67ab59f326860afab836e956
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5423054
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287241}

---
+++ third_party/blink/web_tests/wpt_internal/model-execution/run-from-detached-iframe.window.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+promise_test(async t => {
+  // Make sure the model api is enabled.
+  assert_true(!!model);
+  // Create the iframe and append it to the document.
+  let iframe = document.createElement("iframe");
+  document.childNodes[document.childNodes.length - 1].appendChild(iframe);
+  let iframeWindow = iframe.contentWindow;
+  let iframeDOMException = iframeWindow.DOMException;
+  // Detach the iframe.
+  iframe.remove();
+  // Calling `model.defaultGenericSessionOptions()` from an invalid script
+  // state will trigger the "The execution context is not valid." exception.
+  await promise_rejects_dom(
+    t, 'InvalidStateError', iframeDOMException, iframeWindow.model.defaultGenericSessionOptions(),
+    "The promise should be rejected with InvalidStateError if the execution context is invalid."
+  );
+});
