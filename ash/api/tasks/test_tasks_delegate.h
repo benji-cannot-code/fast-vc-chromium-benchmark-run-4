da62a809616946119ed3ed5048a20385054d825f
Daniel Andersson <dandersson@chromium.org>
2024-04-15 20:50:36 +0000

    Tasks-Api: Fill in TestTasksDelegate
    This CL fills in the body of TestTasksDelegate so that we can use it for
    testing in Focus Mode.
    
    Note: this is essentially the same as https://crrev.com/c/5319140, but
    with an implementation that hopefully won't have to be reverted.
    
    BUG=b:319307346
    
    Change-Id: I00a49f6e1035bba6b8e3956f55edacdcb619b12c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5452732
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287598}

--- ash/api/tasks/test_tasks_delegate.h
+++ ash/api/tasks/test_tasks_delegate.h
@@ -6,10 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_API_TASKS_TEST_TASKS_DELEGATE_H_
 #define ASH_API_TASKS_TEST_TASKS_DELEGATE_H_
 
+#include <optional>
+
 #include "ash/api/tasks/tasks_delegate.h"
 #include "ash/ash_export.h"
+#include "base/containers/flat_map.h"
+#include "components/account_id/account_id.h"
 
 namespace ash::api {
+
+class TasksClient;
 
 class ASH_EXPORT TestTasksDelegate : public TasksDelegate {
  public:
@@ -33,6 +39,24 @@ class ASH_EXPORT TestTasksDelegate : public TasksDelegate {
                   const std::string& title,
                   bool completed,
                   TasksClient::OnTaskSavedCallback callback) override;
+
+  // Helper function for adding pre-build `TasksClient` objects.
+  void AddFakeTasksClient(const AccountId& account_id,
+                          std::unique_ptr<TasksClient> tasks_client);
+
+  // Switches the active account for the delegate.
+  void SetActiveAccount(const AccountId& account_id);
+
+ private:
+  // Returns a reference to the client registered for a current
+  // account. CHECK-fails if no client is registered for it.
+  TasksClient& GetClientForActiveAccount();
+
+  // The currently active account.
+  std::optional<AccountId> active_account_id_;
+
+  // Maps account IDs to clients.
+  base::flat_map<AccountId, std::unique_ptr<TasksClient>> clients_;
 };
 
 }  // namespace ash::api
