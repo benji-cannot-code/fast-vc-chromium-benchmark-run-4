080783a2c937936e2bbec7f0d6ac73eae9b229b1
Sebastien Lalancette <seblalancette@chromium.org>
2024-04-15 21:49:54 +0000

    Move OpenSSLPrivateKey Out of Test Utilities
    This class can be used to wrap a EVP_PKEY held in a crypto::ECPrivateKey
    into a SSLPrivateKey instance.
    
    Also use this new utility in the client_certificates::SSLKeyConverter.
    
    Bug: b:330750994
    Change-Id: I05590daca069ba027c0bb78b8698c4e6e5cebeb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435514
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287656}

--- net/ssl/client_cert_identity_test_util.cc
+++ net/ssl/client_cert_identity_test_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "net/ssl/openssl_private_key.h"
 #include "net/ssl/ssl_private_key.h"
 #include "net/ssl/test_ssl_private_key.h"
 #include "net/test/cert_test_util.h"
--- net/test/key_util.cc
+++ net/test/key_util.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/logging.h"
+#include "net/ssl/openssl_private_key.h"
 #include "net/ssl/ssl_private_key.h"
-#include "net/ssl/test_ssl_private_key.h"
 #include "third_party/boringssl/src/include/openssl/bio.h"
 #include "third_party/boringssl/src/include/openssl/evp.h"
 #include "third_party/boringssl/src/include/openssl/pem.h"
