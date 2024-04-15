f8447d8b209ae9127031a07ec3d89a18e4ef7078
Etienne Pierre-doray <etiennep@chromium.org>
2024-04-15 20:21:23 +0000

    [tracing] Update TriggersDataSource
    To emit the new packet from https://android-review.git.corp.google.com/c/platform/external/perfetto/+/3031643
    
    Bug: b/40257548
    Change-Id: I6a293eae17a27ca01e0ead3edfee7b8f6841565c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5443537
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287578}

--- services/tracing/public/cpp/triggers_data_source.cc
+++ services/tracing/public/cpp/triggers_data_source.cc
@@ -5,10 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/tracing/public/cpp/triggers_data_source.h"
 
+#include "base/metrics/metrics_hashes.h"
 #include "base/trace_event/trace_event.h"
 #include "base/tracing/trace_time.h"
 #include "third_party/perfetto/protos/perfetto/common/data_source_descriptor.gen.h"
-#include "third_party/perfetto/protos/perfetto/trace/trigger.pbzero.h"
+#include "third_party/perfetto/protos/perfetto/trace/chrome/chrome_trigger.pbzero.h"
 
 namespace tracing {
 
@@ -24,8 +25,9 @@ void TriggersDataSource::EmitTrigger(const std::string& trigger_name) {
     packet->set_timestamp(
         TRACE_TIME_TICKS_NOW().since_origin().InNanoseconds());
     packet->set_timestamp_clock_id(base::tracing::kTraceClockId);
-    auto* trigger = packet->set_trigger();
+    auto* trigger = packet->set_chrome_trigger();
     trigger->set_trigger_name(trigger_name);
+    trigger->set_trigger_name_hash(base::HashFieldTrialName(trigger_name));
   });
 }
 
