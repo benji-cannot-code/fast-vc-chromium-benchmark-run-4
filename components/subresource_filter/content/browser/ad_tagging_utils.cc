dfee04f2e93be0760e3bf7e9d46762442de70075
Martin Verde <thesalsa@google.com>
2024-04-15 18:21:54 +0000

    [FPF] Move activation computing throttle to content/shared/browser
    Also refactors utility functions for checking for root/child frames
    out of ContentSubresourceFilterWebContentsHelper to prevent the
    content/shared/browser directory from depending on content/browser.
    
    DanglingUntriaged-notes: refactor
    
    Bug: 40280666
    Change-Id: Ib479973259e203c34468ea4a8e56e06d0137c7e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5435061
    Reviewed-by: Patricia Alfonso <trishalfonso@google.com>
    Commit-Queue: Patricia Alfonso <trishalfonso@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287493}

--- components/subresource_filter/content/browser/ad_tagging_utils.cc
+++ components/subresource_filter/content/browser/ad_tagging_utils.cc
@@ -3,15 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/subresource_filter/content/common/subresource_filter_utils.h"
+#include "components/subresource_filter/content/browser/ad_tagging_utils.h"
 
-#include "content/public/common/url_utils.h"
+#include <optional>
+
+#include "components/subresource_filter/core/common/load_policy.h"
+#include "third_party/blink/public/mojom/ad_tagging/ad_evidence.mojom-shared.h"
 
 namespace subresource_filter {
-
-bool ShouldInheritActivation(const GURL& url) {
-  return !content::IsURLHandledByNetworkStack(url);
-}
 
 blink::mojom::FilterListResult InterpretLoadPolicyAsEvidence(
     const std::optional<LoadPolicy>& load_policy) {
