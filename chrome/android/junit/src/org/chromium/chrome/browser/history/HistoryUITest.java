d83c47ca063325ab95c654c9941e75995b35df01
Zach Katz <katzz@google.com>
2024-04-16 01:36:11 +0000

    [ASH] Filter by app chip
    - Chip that appears when using history search in Chrome browser. Toggling this chip will cause a bottom sheet to appear whereby the history can be filtered.
    - Currently just clicking the chip toggles the check mark and blue background color, but when the bottom sheet code is hooked into it this should only happen once a filter is selected.
    
    Bug: 331856224
    Change-Id: I06f084763209754bdc660820fc963d37470829f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425758
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Zach Katz <katzz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287786}

--- chrome/android/junit/src/org/chromium/chrome/browser/history/HistoryUITest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/history/HistoryUITest.java
@@ -20,6 +20,7 @@ import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build.VERSION_CODES;
 import android.provider.Browser;
 import android.view.MenuItem;
 import android.view.View;
@@ -45,11 +46,13 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 import org.robolectric.shadows.ShadowLooper;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Features.DisableFeatures;
+import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.R;
@@ -123,6 +126,7 @@ public class HistoryUITest {
     @Mock private SigninManager mSigninManager;
     @Mock private PrefChangeRegistrar.Natives mPrefChangeRegistrarJni;
     @Mock private TemplateUrlService mTemplateUrlService;
+    @Mock private HistoryAdapter mMockAdapter;
 
     public static Matcher<Intent> hasData(GURL uri) {
         return IntentMatchers.hasData(uri.getSpec());
@@ -426,6 +430,19 @@ public class HistoryUITest {
         toolbar.onSearchNavigationBack();
         Assert.assertEquals(View.GONE, toolbarShadow.getVisibility());
         Assert.assertEquals(View.GONE, toolbarSearchView.getVisibility());
+    }
+
+    @EnableFeatures(ChromeFeatureList.APP_SPECIFIC_HISTORY)
+    @Config(sdk = VERSION_CODES.UPSIDE_DOWN_CAKE)
+    @Test
+    @SmallTest
+    public void testSearch_AppFilterChipVisible() {
+        mAdapter.setClearBrowsingDataButtonVisibilityForTest(false);
+        mAdapter.setPrivacyDisclaimer();
+        Assert.assertFalse(mAdapter.hasListHeader());
+        performMenuAction(R.id.search_menu_id);
+        // The 'Filter by app' chip is now visible in the header.
+        Assert.assertTrue(mAdapter.hasListHeader());
     }
 
     @Test
