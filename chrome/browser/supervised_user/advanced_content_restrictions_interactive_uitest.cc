4c54dcdac92015a08dceff7ca6b6c31a3f00fca7
anthie@google.com <anthie@google.com>
2024-04-16 16:18:32 +0000

    Rename test accounts with typo
    Rename the last remaining references of the test configs
    with a typo.
    Follow up: remove the old/typo entries from internal
    and bump the dependency in chromium.
    
    Change-Id: I341706429e4eaa5bfb7fd1e60902953ed92f11be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453865
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288086}

--- chrome/browser/supervised_user/advanced_content_restrictions_interactive_uitest.cc
+++ chrome/browser/supervised_user/advanced_content_restrictions_interactive_uitest.cc
@@ -160,7 +160,7 @@ INSTANTIATE_TEST_SUITE_P(
     SupervisedUserFamilyLinkCookiesSwitchUiTest,
     testing::Combine(
         testing::Values(FamilyIdentifier("FAMILY_DMA_ELIGIBLE_WITH_CONSENT"),
-                        FamilyIdentifier("FAMILY_DMA_ELIGIBILE_NO_CONSENT"),
+                        FamilyIdentifier("FAMILY_DMA_ELIGIBLE_NO_CONSENT"),
                         FamilyIdentifier("FAMILY_DMA_INELIGIBLE")),
         /*cookies_switch_value=*/testing::Bool()),
     [](const auto& info) {
