25a0d8097350417ffc2fbe1afb2416b50c2ebffa
Adem Derinel <derinel@google.com>
2024-04-16 17:30:29 +0000

    WebAuthn: Implement reset PIN flow using reauth
    Demo: go/scrcast/NTkzMTE1ODM5NDEwOTk1MnxhZjgxYWRjNS1jNw
    
    There will be a crash if the user repeats the flow after a successful
    reauth. I believe this is due to using the incorrect reauth URL, which
    will be replaced in a future CL.
    
    Bug: 331611435
    Change-Id: I4573fd702d7cc77b7b952c325519cdbf09ddeb92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453443
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288152}

--- chrome/browser/webauthn/change_pin_controller_impl.cc
+++ chrome/browser/webauthn/change_pin_controller_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
@@ -19,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "device/fido/features.h"
+
+using Step = AuthenticatorRequestDialogModel::Step;
 
 ChangePinControllerImpl::ChangePinControllerImpl(
     content::WebContents* web_contents)
@@ -35,6 +38,7 @@ ChangePinControllerImpl::ChangePinControllerImpl(
                       : nullptr;
   model_ = std::make_unique<AuthenticatorRequestDialogModel>(
       web_contents->GetPrimaryMainFrame());
+  model_observation_.Observe(&model_->observers);
 }
 
 ChangePinControllerImpl::~ChangePinControllerImpl() = default;
@@ -69,6 +73,46 @@ bool ChangePinControllerImpl::StartChangePin() {
   if (!IsChangePinFlowAvailable()) {
     return false;
   }
-  model_->SetStep(AuthenticatorRequestDialogModel::Step::kGPMReauthAccount);
+  // TODO(enclave): use local UV instead of GPM reauth when available.
+  model_->SetStep(Step::kGPMReauthAccount);
   return true;
+}
+
+void ChangePinControllerImpl::CancelAuthenticatorRequest() {
+  // User clicked "Cancel" in the GPM dialog.
+  Reset();
+}
+
+void ChangePinControllerImpl::OnReauthComplete(std::string rapt) {
+  rapt_ = std::move(rapt);
+  model_->SetStep(Step::kGPMCreatePin);
+}
+
+void ChangePinControllerImpl::OnRecoverSecurityDomainClosed() {
+  // User closed the reauth window.
+  Reset();
+}
+
+void ChangePinControllerImpl::OnGPMPinEntered(const std::u16string& pin) {
+  CHECK(rapt_.has_value() && (model_->step() == Step::kGPMCreatePin ||
+                              model_->step() == Step::kGPMCreateArbitraryPin));
+  enclave_manager_->ChangePIN(
+      base::UTF16ToUTF8(pin), std::move(rapt_),
+      base::BindOnce(&ChangePinControllerImpl::OnGpmPinChanged,
+                     weak_ptr_factory_.GetWeakPtr()));
+}
+
+void ChangePinControllerImpl::Reset() {
+  model_observation_.Reset();
+  model_->SetStep(Step::kNotStarted);
+  rapt_.reset();
+}
+
+void ChangePinControllerImpl::OnGpmPinChanged(bool success) {
+  if (!success) {
+    model_->SetStep(Step::kGPMError);
+    return;
+  }
+  // TODO(derinel): Display success UI
+  Reset();
 }
