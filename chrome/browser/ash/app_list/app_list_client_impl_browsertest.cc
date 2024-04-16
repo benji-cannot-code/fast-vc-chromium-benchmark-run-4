2c7ed1f6e9f7119b1ffd16f3627bcad4cab29f8c
Takuto Ikuta <tikuta@chromium.org>
2024-04-16 08:39:21 +0000

    ash: remove unused include from app_list_client_impl_browsertest.cc
    This makes compile time of the file faster from 29.6s to 23.1s.
    
    Compile trace is changed like below.
    Before: https://ui.perfetto.dev/#!/?s=b80f9a408fb7f97844304da7b35dc78117844ca959113199b3750b443a246a4a
    After:  https://ui.perfetto.dev/#!/?s=e73a769d6f157c8b9992456d6390c69474e52440bf6cd2449b780138f546a4a1
    
    Bug: b/324519459
    Change-Id: I315f9ef2eb8f656c11b34e7e16c8fce5f2ed15e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456953
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Junji Watanabe <jwata@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287859}

--- chrome/browser/ash/app_list/app_list_client_impl_browsertest.cc
+++ chrome/browser/ash/app_list/app_list_client_impl_browsertest.cc
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
 #include "chrome/browser/ash/login/ui/user_adding_screen.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
-#include "chrome/browser/ash/login/wizard_controller.h"
 #include "chrome/browser/ash/system_web_apps/system_web_app_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
