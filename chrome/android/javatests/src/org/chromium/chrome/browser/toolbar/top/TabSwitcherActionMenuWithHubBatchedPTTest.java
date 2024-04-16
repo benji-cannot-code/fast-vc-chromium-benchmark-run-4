8b3155491ac7790df3f09de9aebc4fc394beece8
Henrique Nakashima <hnakashima@chromium.org>
2024-04-16 22:31:48 +0000

    [Android] Support BlankCTATabInitialStateRule in Public Transit
    Convert HubLayoutPublicTransitTest to use it and remove most of the
    state cleanup code from it.
    
    Bug: 41495174
    Change-Id: Ica63ae6e0fa3a3ec4de9162cbf7ae87d245293d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5460161
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288350}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuWithHubBatchedPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuWithHubBatchedPTTest.java
@@ -51,7 +51,7 @@ public class TabSwitcherActionMenuWithHubBatchedPTTest {
 
     @Rule
     public BatchedPublicTransitRule<PageStation> mBatchedRule =
-            new BatchedPublicTransitRule<>(PageStation.class);
+            new BatchedPublicTransitRule<>(PageStation.class, /* expectResetByTest= */ true);
 
     @Rule
     public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
