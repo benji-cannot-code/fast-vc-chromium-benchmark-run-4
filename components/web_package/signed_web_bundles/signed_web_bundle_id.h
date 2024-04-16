a88338acde4419da3b4baef0e392ff5d274025c3
Andrew Rayskiy <greengrape@google.com>
2024-04-16 21:23:55 +0000

    [web_package] Integrate ECDSA with SignedWebBundleId
    Bug: 330378455
    Change-Id: I00eff846847be075f9cf11c12d8e22fcc5999fb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5457435
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288307}

--- components/web_package/signed_web_bundles/signed_web_bundle_id.h
+++ components/web_package/signed_web_bundles/signed_web_bundle_id.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/strings/string_piece.h"
 #include "base/types/expected.h"
+#include "components/web_package/signed_web_bundles/ecdsa_p256_public_key.h"
 #include "components/web_package/signed_web_bundles/ed25519_public_key.h"
 
 namespace web_package {
@@ -21,6 +22,7 @@ namespace web_package {
 // types of IDs:
 //   * IDs used for development and testing (the so-called proxy mode);
 //   * IDs based on an Ed25519 public key.
+//   * IDs based on an ECDSA P-256 public key.
 //
 // IDs are base32-encoded (without padding), and then transformed to lowercase.
 //
@@ -42,6 +44,8 @@ class SignedWebBundleId {
       kProxyModeKeyLength + kTypeSuffixLength;
   static constexpr size_t kEd25519DecodedIdLength =
       Ed25519PublicKey::kLength + kTypeSuffixLength;
+  static constexpr size_t kEcdsaP256DecodedIdLength =
+      EcdsaP256PublicKey::kLength + kTypeSuffixLength;
   static_assert(kProxyModeDecodedIdLength == kEd25519DecodedIdLength);
 
   // The encoded ID is a lower ASCII base32-encoded concatenation of
@@ -51,12 +55,15 @@ class SignedWebBundleId {
       (kEd25519DecodedIdLength * 8 + 4) / 5;
   static constexpr size_t kProxyModeEncodedIdLength =
       (kProxyModeDecodedIdLength * 8 + 4) / 5;
+  static constexpr size_t kEcdsaP256EncodedIdLength =
+      (kEcdsaP256DecodedIdLength * 8 + 4) / 5;
   static_assert(kEd25519EncodedIdLength == kProxyModeEncodedIdLength);
 
   using TypeSuffix = std::array<uint8_t, kTypeSuffixLength>;
 
   static constexpr TypeSuffix kTypeProxyMode = {0x00, 0x00, 0x02};
   static constexpr TypeSuffix kTypeEd25519PublicKey = {0x00, 0x01, 0x02};
+  static constexpr TypeSuffix kTypeEcdsaP256PublicKey = {0x00, 0x02, 0x02};
 
  public:
   enum class Type {
@@ -65,6 +72,7 @@ class SignedWebBundleId {
     // to a development HTTP(s) server.
     kProxyMode,
     kEd25519PublicKey,
+    kEcdsaP256PublicKey,
   };
 
   // Attempts to parse a a Signed Web Bundle ID, and returns an instance of
@@ -74,7 +82,10 @@ class SignedWebBundleId {
       base::StringPiece base32_encoded_id);
 
   static SignedWebBundleId CreateForEd25519PublicKey(
-      Ed25519PublicKey public_key);
+      const Ed25519PublicKey& public_key);
+
+  static SignedWebBundleId CreateForEcdsaP256PublicKey(
+      const EcdsaP256PublicKey& public_key);
 
   static SignedWebBundleId CreateForProxyMode(
       base::span<const uint8_t, kProxyModeKeyLength> data);
