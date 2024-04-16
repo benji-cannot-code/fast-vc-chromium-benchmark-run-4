138cb9e7f897686b2bc26dc02dc3519ebccaf2ca
Minoru Chikamune <chikamune@chromium.org>
2024-04-16 02:45:36 +0000

    [LCPP] Skip AdjustImagePriority when there is LCP hint
    This CL adds kLCPCriticalPathAdjustImageLoadPriorityOverrideFirstNBoost
    feature param to override the first N image prioritization when there is
    LCP hint.
    
    Bug: 40063266
    Change-Id: I14cd29045e1acdd3f77f2743f4eb40c6cabe4259
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448036
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287802}

--- third_party/blink/renderer/core/loader/frame_fetch_context.h
+++ third_party/blink/renderer/core/loader/frame_fetch_context.h
@@ -106,6 +106,8 @@ class CORE_EXPORT FrameFetchContext final : public BaseFetchContext,
 
   bool DoesLCPPHaveAnyHintData() override;
 
+  bool DoesLCPPHaveLcpElementLocatorHintData() override;
+
   // Exposed for testing.
   void ModifyRequestForCSP(ResourceRequest&);
   void AddClientHintsIfNecessary(const std::optional<float> resource_width,
