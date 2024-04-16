0a67a93a2013d01bb897a29a3009fb6cda146627
Helmut Januschka <helmut@januschka.com>
2024-04-16 01:23:09 +0000

    media/fuchsia: base::StringPiece -> std::string_view
    Bug: 691162
    Change-Id: I3611f48a204e1f5e5ad74b8a8a6b916e807316ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5405582
    Commit-Queue: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287782}

--- media/fuchsia/common/sysmem_client.cc
+++ media/fuchsia/common/sysmem_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <zircon/rights.h>
 
 #include <algorithm>
+#include <string_view>
 
 #include "base/fuchsia/fuchsia_logging.h"
 #include "base/fuchsia/process_context.h"
