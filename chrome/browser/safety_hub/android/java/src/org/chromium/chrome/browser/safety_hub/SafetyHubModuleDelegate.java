fd8155a591469caf2c7d3435aad1fce7037bd689
Zaina Al-Mashni <zalmashni@google.com>
2024-04-16 17:58:29 +0000

    [SHoA] Integrate the password check count with Safety Hub UI
    This CL adds the password module to the Safety Hub settings page and
    updates the UI state based on the cached password check result count.
    
    Bug: 324562205
    Change-Id: I804bc5bfb5c7368b5cdd8c7d01d89e43be51e81b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434941
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288174}

---
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegate.java
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.safety_hub;
+
+import org.chromium.chrome.browser.profiles.Profile;
+
+/** A delegate for Safety Hub to handle UI related behaviour. */
+public interface SafetyHubModuleDelegate {
+
+    boolean shouldShowPasswordCheckModule(Profile profile);
+}
