62dd3e1e1970d6c6cdf3ecee90a2fb690108b76a
Ashley Prasad <ashleydp@google.com>
2024-04-16 20:36:18 +0000

    cros-preview: Trigger start session from  controller
    - Add PrintPreviewPageHandler to app controller.
    - Add start session functionality to page handler to get
      initialization information.
    - Refactor FakePrintPreviewPageHandler method invocation to use
      helper function to update method call count and trigger method.
    - Test expected function called.
    
    Bug: b:323421684
    Test: browser_test --gtest_filter=*PrintPreviewCros*
    Change-Id: I38801a873df9ed6f65c1deb0b10088d5829b55b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5425743
    Auto-Submit: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288282}

--- chrome/test/data/webui/chromeos/print_preview_cros/fake_print_preview_page_handler_test.ts
+++ chrome/test/data/webui/chromeos/print_preview_cros/fake_print_preview_page_handler_test.ts
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-print/js/fakes/fake_print_preview_page_handler.js';
 
-import {FAKE_PRINT_REQUEST_FAILURE_INVALID_SETTINGS_ERROR, FAKE_PRINT_REQUEST_SUCCESSFUL, FakePrintPreviewPageHandler} from 'chrome://os-print/js/fakes/fake_print_preview_page_handler.js';
+import {FAKE_PRINT_REQUEST_FAILURE_INVALID_SETTINGS_ERROR, FAKE_PRINT_REQUEST_SUCCESSFUL, FAKE_PRINT_SESSION_CONTEXT_SUCCESSFUL, FakePrintPreviewPageHandler} from 'chrome://os-print/js/fakes/fake_print_preview_page_handler.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {assertEquals} from 'chrome://webui-test/chromeos/chai_assert.js';
 import {MockController} from 'chrome://webui-test/chromeos/mock_controller.m.js';
 
-suite('PrintPreviewCrosApp', () => {
+suite('FakePrintPreviewPageHandler', () => {
   let printPreviewPageHandler: FakePrintPreviewPageHandler;
 
   setup(() => {
@@ -22,6 +22,7 @@ suite('PrintPreviewCrosApp', () => {
   test('call count zero', () => {
     assertEquals(0, printPreviewPageHandler.getCallCount('print'));
     assertEquals(0, printPreviewPageHandler.getCallCount('cancel'));
+    assertEquals(0, printPreviewPageHandler.getCallCount('startSession'));
   });
 
   // Verify the fake PrintPreviewPageHandler returns a successful response by
@@ -81,4 +82,11 @@ suite('PrintPreviewCrosApp', () => {
         mockController.verifyMocks();
         mockController.reset();
       });
+
+  // Verify the fake PrintPreviewHandler returns a successful SessionContext by
+  // default.
+  test('start session returns SessionContext', async () => {
+    const result = await printPreviewPageHandler.startSession();
+    assertEquals(FAKE_PRINT_SESSION_CONTEXT_SUCCESSFUL, result);
+  });
 });
