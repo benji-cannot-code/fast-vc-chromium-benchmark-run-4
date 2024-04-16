7c3d655833afabf1bc231f45ffce15eb6eee0822
Antonio Rivera <antoniori@google.com>
2024-04-16 23:30:59 +0000

    Copy starboard cast code into chromium (part 1 of n).
    Contains starboard adapter code.
    
    This is part of the work to get cast on starboard building out of
    chromium. See go/moving-cwr-to-chromium for more information on the
    high-level goal.
    
    Bug: b:333571227
    Change-Id: I726331f4c72a32cc7b4b1e84ca1b983212f62950
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441442
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288395}

---
+++ chromecast/starboard/chromecast/starboard_adapter/src/cast_starboard_api_adapter_dummy.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// A dummy implementation of egl_starboard.h. This can be used to compile
+// without starboard headers. It should never be used in production.
+//
+// TODO(b/333131992): remove this
+
+#include <iostream>
+
+#include "chromecast/starboard/chromecast/starboard_adapter/public/cast_starboard_api_adapter.h"
+
+namespace chromecast {
+
+// static
+CastStarboardApiAdapter* CastStarboardApiAdapter::GetInstance() {
+  std::cerr << "Returning a null CastStarboardApiAdapter" << std::endl;
+  return nullptr;
+}
+
+}  // namespace chromecast
