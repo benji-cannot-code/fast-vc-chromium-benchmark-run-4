5f64ab504f14a477eb6597940ab41a34f33c68d7
Longbo Wei <longbowei@google.com>
2024-04-16 23:44:59 +0000

    Cleanup: Fix typo in comment
    Change-Id: Ia93751f582ce72aea5f58c6215361aab9b2d6234
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5460428
    Auto-Submit: Longbo Wei <longbowei@google.com>
    Reviewed-by: Danny Wang <wangdanny@google.com>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288409}

--- components/peripherals/logging/logging.h
+++ components/peripherals/logging/logging.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "components/peripherals/logging/log_buffer.h"
 
-// Use the PR_LOG() macro for all logging related to Cross Device Features so
+// Use the PR_LOG() macro for all logging related to Peripherals Features so
 // the debug page can reflect all logs related to this feature in the internal
 // debug WebUI (chrome://nearby-internals).
 #define PR_LOG(severity, feature)                                              \
