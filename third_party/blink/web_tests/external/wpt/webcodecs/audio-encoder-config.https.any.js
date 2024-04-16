8235096edce5233e6c57a24c32e705f6404d67cd
Thomas Guilbert <tguilbert@chromium.org>
2024-04-16 16:52:17 +0000

    Add OpusEncoderConfig.[signal/application]
    This CL adds optional configuration flags to the OpusEncoderConfig.
    
    See matching spec change: https://github.com/w3c/webcodecs/pull/777
    
    Bug: 330949853
    Change-Id: Iaa39ba0a710ffb482aa767deb8992f67c52ebb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5387232
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288112}

--- third_party/blink/web_tests/external/wpt/webcodecs/audio-encoder-config.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/audio-encoder-config.https.any.js
@@ -273,6 +273,8 @@ const validConfigs = [
     numberOfChannels: 2,
     opus: {
       complexity: 5,
+      signal: 'music',
+      application: 'audio',
       frameDuration: 20000,
       packetlossperc: 10,
       useinbandfec: true,
@@ -284,6 +286,8 @@ const validConfigs = [
     numberOfChannels: 2,
     opus: {
       format: 'opus',
+      signal: 'voice',
+      application: 'lowdelay',
       complexity: 10,
       frameDuration: 60000,
       packetlossperc: 20,  // Irrelevant without useinbandfec, but still valid.
