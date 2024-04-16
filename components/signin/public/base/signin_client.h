a3bebeb929cd3b3f0f3784a450b306b674ead42b
Hans Wennborg <hans@chromium.org>
2024-04-16 10:32:24 +0000

    Replace include of network_context.mojom.h with forward declaration
    The definition wasn't required. Removing the include should save ca
    1.5GB of preprocessed source size for the 'chrome' build target [1].
    
     1. https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2024-04-16_065141.html#view=edges&includer=%5Ecomponents%2Fsignin%2Fpublic%2Fbase%2Fsignin_client%5C.h%24
    
    Bug: 242216
    Change-Id: Id7cba820d170faddbd3ee9eec3a5d4566d371fcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453576
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287929}

--- components/signin/public/base/signin_client.h
+++ components/signin/public/base/signin_client.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_metrics.h"
 #include "google_apis/gaia/core_account_id.h"
 #include "google_apis/gaia/gaia_auth_fetcher.h"
-#include "services/network/public/mojom/network_context.mojom.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "url/gurl.h"
 
@@ -46,6 +45,7 @@ class SharedURLLoaderFactory;
 
 namespace mojom {
 class CookieManager;
+class NetworkContext;
 }
 }  // namespace network
 
