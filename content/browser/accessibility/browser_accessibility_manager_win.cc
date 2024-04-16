308d65958cb97b130e8aa4bffb026ad419b4a361
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-04-16 00:39:39 +0000

    [UIA] Don't fire a live region changed event on creation
    With IA2, we only fire a live region changed event when the live
    region node's content changes. With UIA, we're also firing a live
    region changed event when the node gets created. This leads to unexpected behaviors with NVDA and JAWS when UIA is enabled, such as live region announced when they shouldn't be, or other text being interrupted.
    
    This CL fixes that by omitting the live region changed event we were
    doing with UIA when the node gets created.
    
    Bug: 334991033
    Change-Id: Ic6cbf7b03592132b29db00d29905a2f267ebf286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456111
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1287754}

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -389,9 +389,6 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
     case ui::AXEventGenerator::Event::LANGUAGE_CHANGED:
       FireUiaPropertyChangedEvent(UIA_CulturePropertyId, wrapper);
       break;
-    case ui::AXEventGenerator::Event::LIVE_REGION_CREATED:
-      FireUiaAccessibilityEvent(UIA_LiveRegionChangedEventId, wrapper);
-      break;
     case ui::AXEventGenerator::Event::LIVE_REGION_CHANGED:
       // This event is redundant with the IA2_EVENT_TEXT_INSERTED events;
       // however, JAWS 2018 and earlier do not process the text inserted
@@ -557,6 +554,7 @@ void BrowserAccessibilityManagerWin::FireGeneratedEvent(
     case ui::AXEventGenerator::Event::DETAILS_CHANGED:
     case ui::AXEventGenerator::Event::DOCUMENT_TITLE_CHANGED:
     case ui::AXEventGenerator::Event::FOCUS_CHANGED:
+    case ui::AXEventGenerator::Event::LIVE_REGION_CREATED:
     case ui::AXEventGenerator::Event::LIVE_REGION_NODE_CHANGED:
     case ui::AXEventGenerator::Event::MENU_ITEM_SELECTED:
     case ui::AXEventGenerator::Event::ORIENTATION_CHANGED:
