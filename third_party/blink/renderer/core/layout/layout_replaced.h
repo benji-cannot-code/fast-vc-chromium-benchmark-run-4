292d26a9f5f9a8491698a11dac62e8a9b1dc1038
Traian Captan <tcaptan@chromium.org>
2024-04-16 00:51:51 +0000

    object-fit should use the "real" natural aspect ratio
    The resolution of issue 10116 [1]
    "Should contain-intrinsic-size affect object-fit?" was that:
    "`contain` removes the natural aspect ratio / width / height only for
    the purposes of sizing and layout of the box (and object-fit is
    therefore not affected)".
    
    Therefore, `LayoutReplaced::ComputeObjectFitAndPositionRect` should
    use the natural size rather than the overridden intrinsic size. After
    analyzing all callsites of `LayoutReplaced::IntrinsicSize`, it was
    found that they all should use the natural size rather than the
    overridden intrinsic size, so this core function was updated.
    
    [1]
    https://github.com/w3c/csswg-drafts/issues/10116
    
    Low-Coverage-Reason: TRIVIAL_CHANGE - no effective change
    
    Bug: 332775468
    Change-Id: I9cbdcb45a392145586a3ee7b3f06f46ce2301931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5442533
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Traian Captan <tcaptan@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287761}

--- third_party/blink/renderer/core/layout/layout_replaced.h
+++ third_party/blink/renderer/core/layout/layout_replaced.h
@@ -141,8 +141,14 @@ class CORE_EXPORT LayoutReplaced : public LayoutBox {
 
   void WillBeDestroyed() override;
 
+  // See: intrinsic_size_.
   PhysicalSize IntrinsicSize() const {
     NOT_DESTROYED();
+
+    if (RuntimeEnabledFeatures::NoIntrinsicSizeOverrideEnabled()) {
+      return intrinsic_size_;
+    }
+
     auto width_override = IntrinsicWidthOverride();
     auto height_override = IntrinsicHeightOverride();
     return PhysicalSize(width_override.value_or(intrinsic_size_.width),
@@ -195,6 +201,8 @@ class CORE_EXPORT LayoutReplaced : public LayoutBox {
       const PhysicalRect& base_content_rect,
       const PhysicalSize* overridden_intrinsic_size) const;
 
+  // TODO(crbug.com/335003884): remove IntrinsicWidthOverride and
+  // IntrinsicHeightOverride when removing the NoIntrinsicSizeOverride flag.
   std::optional<LayoutUnit> IntrinsicWidthOverride() const {
     NOT_DESTROYED();
     if (HasOverrideIntrinsicContentWidth())
