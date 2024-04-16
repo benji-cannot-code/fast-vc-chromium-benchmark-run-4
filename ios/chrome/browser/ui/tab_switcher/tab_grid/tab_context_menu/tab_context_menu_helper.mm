488865f26a2afc45baf5949ef0e28c9ca169e4a9
Ewann Pelle <ewannpv@chromium.org>
2024-04-16 10:27:35 +0000

    [iOS][TG] Fix group view context menu
    Hides the select and share action for tabs in groups.
    
    Fixed: 334879558, 334879114
    Change-Id: I76297dd735ecdef1a1438e1eedf16dd31d2c5a52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455774
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287924}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_helper.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_helper.mm
@@ -222,23 +222,25 @@ using PinnedState = WebStateSearchCriteria::PinnedState;
       [weakSelf handleAddWebState:tabID toGroup:group];
     };
 
-    const TabGroup* tabGroup = [self groupForWebState:tabID];
+    const TabGroup* currentTabGroup = [self groupForWebState:tabID];
     UIMenuElement* groupAction;
-    if (tabGroup) {
+    if (currentTabGroup) {
       ProceduralBlock removeBlock = ^{
         [weakSelf handleRemoveWebStateFromGroup:tabID];
       };
-      groupAction = [actionFactory menuToMoveTabToGroupWithGroups:groups
-                                                     currentGroup:tabGroup
-                                                        moveBlock:actionResult
-                                                      removeBlock:removeBlock];
+      groupAction =
+          [actionFactory menuToMoveTabToGroupWithGroups:groups
+                                           currentGroup:currentTabGroup
+                                              moveBlock:actionResult
+                                            removeBlock:removeBlock];
     } else {
       groupAction = [actionFactory menuToAddTabToGroupWithGroups:groups
                                                     numberOfTabs:1
                                                            block:actionResult];
     }
 
-    if (shareAction) {
+    // Hide the `shareAction` for tabs in groups.
+    if (shareAction && !currentTabGroup) {
       UIMenu* shareMenu = [UIMenu menuWithTitle:@""
                                           image:nil
                                      identifier:nil
@@ -262,7 +264,8 @@ using PinnedState = WebStateSearchCriteria::PinnedState;
     if (bookmarkAction) {
       [collectionsActions addObject:bookmarkAction];
     }
-    if (selectAction) {
+    // Hide the `selectAction` for tabs in groups.
+    if (selectAction && !currentTabGroup) {
       [collectionsActions addObject:selectAction];
     }
     if (closeTabAction) {
