007146e92c3ae6a127d7b17a83b9b7ba50120eda
jiawei wang <hellojw513@gmail.com>
2024-04-16 15:50:32 +0000

    Bubble views: Remove PageInfo's SizeToContents()
    First remove SizetoContents() in PageInfo and modify ui/views. Because
    title() will return nullptr when SetTitleView() calls
    InvalidateLayout().
    
    Bug: 41493925
    Change-Id: I998b9661649a06b7fe51d9bcb01f84fe6f578d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5446490
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: jiawei wang <hellojw513@gmail.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288071}

--- chrome/browser/ui/views/page_info/page_info_bubble_view.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view.cc
@@ -115,8 +115,6 @@ InternalPageInfoBubbleView::InternalPageInfoBubbleView(
   title_label->SetFontList(views::Label::GetDefaultFontList());
   title_label->SetMultiLine(true);
   title_label->SetElideBehavior(gfx::NO_ELIDE);
-
-  SizeToContents();
 }
 
 InternalPageInfoBubbleView::~InternalPageInfoBubbleView() = default;
@@ -304,7 +302,6 @@ gfx::Size PageInfoBubbleView::CalculatePreferredSize() const {
 
 void PageInfoBubbleView::ChildPreferredSizeChanged(views::View* child) {
   DeprecatedLayoutImmediately();
-  SizeToContents();
 }
 
 void PageInfoBubbleView::AnnouncePageOpened(std::u16string announcement) {
