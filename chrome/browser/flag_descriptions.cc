3031820fe446dbfe25a8944364e1a5f05d69e930
Michael Tang <tangm@microsoft.com>
2024-04-16 16:43:36 +0000

    gpu: win: Delegated compositing on Windows
    This CL implements the `DelegatedCompositing` feature for Windows. It
    allows OverlayProcessorWin to promote all quads from the root pass to
    overlay. It current comes with a GPU memory cost since we are copying
    renderer DrawQuads to DComp surfaces in the SkiaOutputDeviceDComp. This
    will be mitigated with the use of DComp textures on Win11 >=23H2.
    
    We intend to land "partial" delegated compositing, which will keep the
    web contents surface in a swap chain, similar to how the root surface
    is handled today.
    
    Bug: 1132392
    Change-Id: I53986cecc325077af157cb54926526f8156bd216
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3790233
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1288105}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1023,7 +1023,8 @@ const char kEnableRawDrawDescription[] =
 
 const char kEnableDelegatedCompositingName[] = "Enable delegated compositing";
 const char kEnableDelegatedCompositingDescription[] =
-    "When enabled and applicable, the act of compositing is delegated to Ash.";
+    "When enabled and applicable, the act of compositing is delegated to the "
+    "system compositor.";
 
 const char kEnableRenderPassDrawnRectName[] =
     "Enable drawn rect for render passes";
