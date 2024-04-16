40e62d06dc3a4c41702360c25d90377e29763dd0
Thorsten Sideboard <sideboard@google.com>
2024-04-16 23:25:44 +0000

    Alternative approach to custom allocator.
    Rather than adding to each parser, add the alloc logic to
    StreamParserBuffer itself.
    
    Bug: b/321805728
    Change-Id: I7ec2cb619163ddd861f3564a656437b8abd5147b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458839
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Thorsten Sideboard <sideboard@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288392}

--- media/base/stream_parser_buffer.cc
+++ media/base/stream_parser_buffer.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/memory/ptr_util.h"
+#include "media/base/media_client.h"
 #include "media/base/timestamp_constants.h"
 
 namespace media {
@@ -24,6 +25,14 @@ scoped_refptr<StreamParserBuffer> StreamParserBuffer::CopyFrom(
     bool is_key_frame,
     Type type,
     TrackId track_id) {
+  if (auto* media_client = GetMediaClient()) {
+    if (auto* alloc = media_client->GetMediaAllocator()) {
+      auto data_span = UNSAFE_BUFFERS(
+          base::span(data, base::checked_cast<size_t>(data_size)));
+      return StreamParserBuffer::FromExternalMemory(
+          alloc->CopyFrom(data_span), is_key_frame, type, track_id);
+    }
+  }
   return base::WrapRefCounted(
       new StreamParserBuffer(data, data_size, is_key_frame, type, track_id));
 }
