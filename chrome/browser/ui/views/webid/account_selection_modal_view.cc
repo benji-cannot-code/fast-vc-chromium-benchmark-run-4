f895751fbd28ecb0ab763c3a4e37293b091730cf
Nicolás Peña <npm@chromium.org>
2024-04-16 17:47:13 +0000

    [FedCM] Fix multi IDP account badges
    A similar approach to the modal dialog is used to achieve badging the
    IDP image on top of the account image in the account's HoverButton.
    
    Demo: https://issues.chromium.org/issues/325503352#comment5
    
    Bug: 325503352
    Change-Id: Ie36e1428b8ff194a22cba78b6750a5af287d4233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454601
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288167}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -367,9 +367,7 @@ void AccountSelectionModalView::ShowMultiAccountPicker(
   RemoveNonHeaderChildViews();
 
   ConfigureBrandImageView(brand_icon_,
-                          idp_display_data_list[0].idp_metadata.brand_icon_url,
-                          kModalIdpIconSize,
-                          /*should_circle_crop=*/false);
+                          idp_display_data_list[0].idp_metadata.brand_icon_url);
   account_chooser_ =
       AddChildView(CreateMultipleAccountChooser(idp_display_data_list));
 
@@ -488,9 +486,7 @@ void AccountSelectionModalView::ShowSingleAccountConfirmDialog(
   RemoveNonHeaderChildViews();
 
   ConfigureBrandImageView(brand_icon_,
-                          idp_display_data.idp_metadata.brand_icon_url,
-                          kModalIdpIconSize,
-                          /*should_circle_crop=*/false);
+                          idp_display_data.idp_metadata.brand_icon_url);
   account_chooser_ =
       AddChildView(CreateSingleAccountChooser(idp_display_data, account,
                                               /*should_hover=*/true,
@@ -548,9 +544,7 @@ void AccountSelectionModalView::ShowRequestPermissionDialog(
   RemoveNonHeaderChildViews();
 
   ConfigureBrandImageView(brand_icon_,
-                          idp_display_data.idp_metadata.brand_icon_url,
-                          kModalIdpIconSize,
-                          /*should_circle_crop=*/false);
+                          idp_display_data.idp_metadata.brand_icon_url);
   account_chooser_ = AddChildView(CreateSingleAccountChooser(
       idp_display_data, account,
       /*should_hover=*/false,
@@ -597,9 +591,7 @@ AccountSelectionModalView::CreateBrandIconImageView(
   brand_icon_image_view->SetImageSize(
       gfx::Size(kModalIdpIconSize, kModalIdpIconSize));
   if (brand_icon_url.is_valid()) {
-    ConfigureBrandImageView(brand_icon_image_view.get(), brand_icon_url,
-                            kModalIdpIconSize,
-                            /*should_circle_crop=*/false);
+    ConfigureBrandImageView(brand_icon_image_view.get(), brand_icon_url);
   } else {
     brand_icon_image_view->SetImage(ui::ImageModel::FromVectorIcon(
         kWebidGlobeIcon, ui::kColorIconSecondary, kModalIdpIconSize));
