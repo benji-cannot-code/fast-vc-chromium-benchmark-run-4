5d478d63dec8e13705ed8740c57ca0f512c0ab0e
Michele Fan <michelefan@chromium.org>
2024-04-16 01:02:31 +0000

    snap-group: Fix the header view issue with undo
    Demo: http://b/334217916#comment5
    Fixed: b/334217916
    Test: Manually
    Change-Id: I257dcdd529aa0a0b0f5235081b5287cff765632d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456093
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287768}

--- ash/wm/overview/overview_group_item.cc
+++ ash/wm/overview/overview_group_item.cc
@@ -322,6 +322,8 @@ void OverviewGroupItem::SetShouldUseSpawnAnimation(bool value) {
   for (const auto& item : overview_items_) {
     item->SetShouldUseSpawnAnimation(value);
   }
+
+  should_use_spawn_animation_ = value;
 }
 
 void OverviewGroupItem::OnStartingAnimationComplete() {
