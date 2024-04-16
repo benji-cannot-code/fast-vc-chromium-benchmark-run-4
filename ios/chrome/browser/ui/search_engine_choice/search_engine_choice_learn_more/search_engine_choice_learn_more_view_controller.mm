3cb7b8ac338afe35a89db06d515ef4d0fc366263
Jérôme Lebel <jlebel@chromium.org>
2024-04-16 18:35:54 +0000

    [iOS][Waffle] Update the learn more margins
    This patch updates the horizontal margins for the learn more from
    the choice screen dialog.
    
    This patch should be merged to M125.
    
    Before:
    https://drive.google.com/file/d/1Qr5fmKLIhpc1GMLqEYosEXIe5PbUnW-i/view?resourcekey=0-8CWch7unYx0zzo4EfpAdWQ
    After:
    https://drive.google.com/file/d/1Tz1vP524WtNEcNCzTTAR7ZdM-3AzLf7X/view?resourcekey=0-HnIXixVPysYOUKnelj1bsQ
    
    Fixed: 329415142
    Change-Id: Ieac89772d649dd6d45ad7583fc26567ef41be368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458412
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288198}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_learn_more/search_engine_choice_learn_more_view_controller.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_learn_more/search_engine_choice_learn_more_view_controller.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
 #import "ios/chrome/common/string_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
+#import "ios/chrome/common/ui/promo_style/utils.h"
 #import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 #import "ios/chrome/common/ui/util/text_view_util.h"
 #import "ui/base/l10n/l10n_util_mac.h"
@@ -133,8 +134,10 @@ UITextView* SecondParagraph() {
 - (void)viewDidLoad {
   [super viewDidLoad];
 
+  UIView* view = self.view;
+
   // Navigation.
-  self.view.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
+  view.backgroundColor = [UIColor colorNamed:kSecondaryBackgroundColor];
   self.navigationItem.title =
       l10n_util::GetNSString(IDS_SEARCH_ENGINE_CHOICE_INFO_DIALOG_TITLE_IOS);
   UIBarButtonItem* doneButton = [[UIBarButtonItem alloc]
@@ -145,7 +148,7 @@ UITextView* SecondParagraph() {
 
   // Scroll view.
   UIScrollView* scrollView = [[UIScrollView alloc] init];
-  [self.view addSubview:scrollView];
+  [view addSubview:scrollView];
   scrollView.translatesAutoresizingMaskIntoConstraints = NO;
 
   // Scrollable content.
@@ -183,16 +186,19 @@ UITextView* SecondParagraph() {
   secondParagraphTextView.delegate = self;
   [scrollContentView addSubview:secondParagraphTextView];
 
+  // Create a layout guide to constrain the width of the content, while still
+  // allowing the scroll view to take the full screen width.
+  UILayoutGuide* widthLayoutGuide = AddPromoStyleWidthLayoutGuide(view);
   [NSLayoutConstraint activateConstraints:@[
     // Frame layout.
     [scrollView.frameLayoutGuide.topAnchor
-        constraintEqualToAnchor:self.view.topAnchor],
+        constraintEqualToAnchor:view.topAnchor],
     [scrollView.frameLayoutGuide.bottomAnchor
-        constraintEqualToAnchor:self.view.bottomAnchor],
+        constraintEqualToAnchor:view.bottomAnchor],
     [scrollView.frameLayoutGuide.leadingAnchor
-        constraintEqualToAnchor:self.view.leadingAnchor],
+        constraintEqualToAnchor:view.leadingAnchor],
     [scrollView.frameLayoutGuide.trailingAnchor
-        constraintEqualToAnchor:self.view.trailingAnchor],
+        constraintEqualToAnchor:view.trailingAnchor],
 
     // Content layout.
     [scrollView.contentLayoutGuide.trailingAnchor
@@ -207,12 +213,10 @@ UITextView* SecondParagraph() {
     [scrollContentView.bottomAnchor
         constraintEqualToAnchor:scrollView.contentLayoutGuide.bottomAnchor
                        constant:-kTableViewVerticalSpacing],
-    [scrollContentView.leadingAnchor
-        constraintEqualToAnchor:scrollView.contentLayoutGuide.leadingAnchor
-                       constant:kTableViewHorizontalSpacing],
-    [scrollContentView.trailingAnchor
-        constraintEqualToAnchor:scrollView.contentLayoutGuide.trailingAnchor
-                       constant:-kTableViewHorizontalSpacing],
+    [scrollContentView.widthAnchor
+        constraintEqualToAnchor:widthLayoutGuide.widthAnchor],
+    [scrollContentView.centerXAnchor
+        constraintEqualToAnchor:widthLayoutGuide.centerXAnchor],
 
     // first paragraph.
     [firstParagraphTextView.topAnchor
