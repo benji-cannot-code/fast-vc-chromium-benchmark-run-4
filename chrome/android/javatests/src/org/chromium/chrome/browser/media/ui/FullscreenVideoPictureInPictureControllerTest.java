3cb77bc82c368dfdd31a6e00bea88e196533eceb
Zach Katz <katzz@google.com>
2024-04-16 23:01:24 +0000

    Disable FullscreenVideoPictureInPictureControllerTest#testFullscreenVideoDetectedOnlyWhenPlaying
    Bug: 335305496
    Change-Id: I620eb2530358b781c4846345dbf89139395c2618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5457943
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Zach Katz <katzz@google.com>
    Owners-Override: Zach Katz <katzz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288384}

--- chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/ui/FullscreenVideoPictureInPictureControllerTest.java
@@ -86,6 +86,7 @@ public class FullscreenVideoPictureInPictureControllerTest {
     @Test
     @MediumTest
     @MinAndroidSdkLevel(Build.VERSION_CODES.O)
+    @DisabledTest(message = "https://crbug.com/335305496")
     public void testFullscreenVideoDetectedOnlyWhenPlaying() throws Throwable {
         enterFullscreen();
 
