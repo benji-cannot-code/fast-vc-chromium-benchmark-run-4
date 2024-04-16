a60bb033a07fc7725be161c8dc103d48e124e557
Jeroen Dhollander <jeroendh@google.com>
2024-04-16 08:57:24 +0000

    Simplify dependencies of DeviceLocalAccountExternalCache
    This is a cleanup with no behavioral change, that removes the
    dependencies from DeviceLocalAccountExternalCache on both
    DeviceLocalAccountExternalPolicyLoader and Crosapi.
    
    This makes adding new tests easier (which will happen in a follow-up
    CL).
    
    Bug: b:329582435
    Change-Id: I6e398b69f04218c098bfd763c4f5863af363cbdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444329
    Commit-Queue: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Edman Anjos <edman@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287891}

--- chrome/browser/ash/policy/core/device_local_account_external_cache.cc
+++ chrome/browser/ash/policy/core/device_local_account_external_cache.cc
@@ -6,20 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/device_local_account_external_cache.h"
 
 #include <memory>
+#include <string>
+#include <utility>
 
-#include "base/check_is_test.h"
+#include "base/check.h"
 #include "base/files/file_path.h"
+#include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
-#include "chrome/browser/ash/crosapi/crosapi_ash.h"
-#include "chrome/browser/ash/crosapi/crosapi_manager.h"
-#include "chrome/browser/ash/crosapi/device_local_account_extension_service_ash.h"
 #include "chrome/browser/ash/extensions/external_cache_impl.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/extensions/device_local_account_external_policy_loader.h"
-#include "chrome/browser/extensions/external_loader.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
@@ -27,11 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 DeviceLocalAccountExternalCache::DeviceLocalAccountExternalCache(
+    ExtensionListCallback ash_loader,
+    ExtensionListCallback lacros_loader,
     const std::string& user_id,
     const base::FilePath& cache_dir)
-    : user_id_(user_id), cache_dir_(cache_dir) {
-  loader_ = base::MakeRefCounted<DeviceLocalAccountExternalPolicyLoader>();
-}
+    : cache_dir_(cache_dir),
+      ash_loader_(ash_loader),
+      lacros_loader_(lacros_loader) {}
 
 DeviceLocalAccountExternalCache::~DeviceLocalAccountExternalCache() = default;
 
@@ -64,7 +65,8 @@ void DeviceLocalAccountExternalCache::StopCache(base::OnceClosure callback) {
   }
 
   base::Value::Dict empty_prefs;
-  loader_->OnExtensionListsUpdated(empty_prefs);
+  ash_loader_.Run(user_id_, empty_prefs.Clone());
+  lacros_loader_.Run(user_id_, empty_prefs.Clone());
 }
 
 bool DeviceLocalAccountExternalCache::IsCacheRunning() const {
@@ -73,15 +75,8 @@ bool DeviceLocalAccountExternalCache::IsCacheRunning() const {
 
 void DeviceLocalAccountExternalCache::OnExtensionListsUpdated(
     const base::Value::Dict& prefs) {
-  if (crosapi::CrosapiManager::IsInitialized()) {
-    crosapi::CrosapiManager::Get()
-        ->crosapi_ash()
-        ->device_local_account_extension_service()
-        ->SetForceInstallExtensionsFromCache(user_id_, prefs.Clone());
-  } else {
-    CHECK_IS_TEST();
-  }
-  loader_->OnExtensionListsUpdated(prefs);
+  lacros_loader_.Run(user_id_, prefs.Clone());
+  ash_loader_.Run(user_id_, prefs.Clone());
 }
 
 bool DeviceLocalAccountExternalCache::IsRollbackAllowed() const {
@@ -95,11 +90,6 @@ bool DeviceLocalAccountExternalCache::CanRollbackNow() const {
     return user_id_ != user->GetAccountId().GetUserEmail();
   }
   return true;
-}
-
-scoped_refptr<extensions::ExternalLoader>
-DeviceLocalAccountExternalCache::GetExtensionLoader() {
-  return loader_;
 }
 
 base::Value::Dict DeviceLocalAccountExternalCache::GetCachedExtensions() const {
