9af7a1807dc0d21730bfda001ac949e3170e324b
Brittany Hartmire <bhartmire@google.com>
2024-04-16 16:18:52 +0000

    [QuickStart]: Add new fallback URL UI to list of screen names
    Test: manually tested fallback url with pixel8 and atlas
    Change-Id: If969d860fbf76819a749cec6f02f5a72658a5458
    Bug: b/298042953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454943
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288087}

--- chromeos/ash/components/quick_start/quick_start_metrics.cc
+++ chromeos/ash/components/quick_start/quick_start_metrics.cc
@@ -125,6 +125,8 @@ constexpr const char kScreenClosedQSGettingGoogleAccountInfo[] =
     "QuickStart.ScreenClosed.QSGettingGoogleAccountInfo";
 constexpr const char kScreenClosedQSCreatingAccount[] =
     "QuickStart.ScreenClosed.QSCreatingAccount";
+constexpr const char kScreenClosedQSFallbackURL[] =
+    "QuickStart.ScreenClosed.QSFallbackURL";
 
 std::string MapMessageTypeToMetric(
     QuickStartMetrics::MessageType message_type) {
@@ -191,6 +193,8 @@ std::string MapScreenNameToMetric(QuickStartMetrics::ScreenName screen_name) {
       return kScreenClosedQSSelectGoogleAccount;
     case QuickStartMetrics::ScreenName::kQSCreatingAccount:
       return kScreenClosedQSCreatingAccount;
+    case QuickStartMetrics::ScreenName::kQSFallbackURL:
+      return kScreenClosedQSFallbackURL;
     case QuickStartMetrics::ScreenName::kOther:
       [[fallthrough]];
     default:
@@ -531,6 +535,9 @@ std::ostream& operator<<(
       break;
     case QuickStartMetrics::ScreenName::kQSCreatingAccount:
       stream << "[QS creating account]";
+      break;
+    case QuickStartMetrics::ScreenName::kQSFallbackURL:
+      stream << "[QS fallback URL]";
       break;
   }
 
