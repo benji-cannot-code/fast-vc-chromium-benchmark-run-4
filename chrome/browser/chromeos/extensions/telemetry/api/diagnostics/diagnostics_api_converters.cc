f12c705430ba385ffa939d4dbaabd1a0e2c8d45d
Wei-Luan Wang <weiluanwang@google.com>
2024-04-16 03:51:39 +0000

    [Telemetry] Fix typos
    Bug: none
    Test: CQ
    Change-Id: I0f7b00b09e0270038c0cf2b6831178931a0b85d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449227
    Reviewed-by: Kerker Yang <kerker@chromium.org>
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Commit-Queue: Wei-Luan Wang <weiluanwang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1287814}

--- chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api_converters.cc
+++ chrome/browser/chromeos/extensions/telemetry/api/diagnostics/diagnostics_api_converters.cc
@@ -414,9 +414,9 @@ ConvertRoutineArgumentsUnion(
   if (result) {
     return result;
   }
-  // When extension is newer than the brwowser, extension might pass in a
-  // routine argument that cannot be recognized by the browser. For better
-  // developer experience, don't treat it as an invalid union.
+  // When extension is newer than the browser, extension might pass in a routine
+  // argument that cannot be recognized by the browser. For better developer
+  // experience, don't treat it as an invalid union.
   return crosapi::TelemetryDiagnosticRoutineArgument::NewUnrecognizedArgument(
       false);
 }
@@ -437,9 +437,9 @@ ConvertRoutineInquiryReplyUnion(
   if (result) {
     return result;
   }
-  // When extension is newer than the brwowser, extension might pass in a
-  // reply that cannot be recognized by the browser. For better developer
-  // experience, don't treat it as an invalid union.
+  // When extension is newer than the browser, extension might pass in a reply
+  // that cannot be recognized by the browser. For better developer experience,
+  // don't treat it as an invalid union.
   return crosapi::TelemetryDiagnosticRoutineInquiryReply::NewUnrecognizedReply(
       false);
 }
