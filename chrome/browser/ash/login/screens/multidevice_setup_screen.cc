af6e200fdd79d078809ca4683b89845161014c5a
Brittany Hartmire <bhartmire@google.com>
2024-04-16 16:06:51 +0000

    [QuickStart] Record ScreenClosed.{ScreenName}.Reason
    additions to existing browsertests
    
    Test: manually tested different scenarios with atlas and pixel8, made
    Change-Id: I65218e5d3308a15c21cc52c756ba103cf2958955
    Bug: b/298042953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447677
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288080}

--- chrome/browser/ash/login/screens/multidevice_setup_screen.cc
+++ chrome/browser/ash/login/screens/multidevice_setup_screen.cc
@@ -285,7 +285,8 @@ void MultiDeviceSetupScreen::OnGetGroupPrivateKeyStatus(
 void MultiDeviceSetupScreen::MaybeRecordQuickStartScreenClosed() {
   if (quick_start_metrics_ != nullptr) {
     quick_start_metrics_->RecordScreenClosed(
-        quick_start::QuickStartMetrics::ScreenName::kUnifiedSetup);
+        quick_start::QuickStartMetrics::ScreenName::kUnifiedSetup,
+        quick_start::QuickStartMetrics::ScreenClosedReason::kAdvancedInFlow);
   }
 }
 void MultiDeviceSetupScreen::RecordOobeMultideviceScreenSkippedReasonHistogram(
