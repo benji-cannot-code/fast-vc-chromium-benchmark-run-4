7a7eb6620c6880415181291a222723f7f47ddecc
Siyu An <siyua@chromium.org>
2024-04-16 00:13:44 +0000

    [VCN] Fix VCN OTP auth issue.
    Happens when user clicks on the link to get a new code. In this case,
    we still call showDialog which will reset the controller and break the
    connection between the controller and the delegate
    (credit_card_otp_authenticator).
    
    This bug starts on 04/10 due to the refactor to reset controller
    eveytime the show is called. Need to merge to M125
    
    Screen recording: https://drive.google.com/file/d/11SWoPzC6fXedXEppbdjA26OjmlVaukqa/view?usp=sharing
    
    Bug: 334732832
    Change-Id: Icd7b5bc8f955b2a396cecbe59cd345d900f293b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5452852
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1287748}

--- components/autofill/core/browser/payments/credit_card_otp_authenticator_unittest.cc
+++ components/autofill/core/browser/payments/credit_card_otp_authenticator_unittest.cc
@@ -312,7 +312,8 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthServerVcnError) {
     EXPECT_FALSE(OtpAuthenticatorContextToken().empty());
     EXPECT_NE(OtpAuthenticatorContextToken(),
               "context_token_from_previous_unmask_response");
-    // TODO(crbug.com/1243475): Verify the otp dialog is shown.
+    EXPECT_TRUE(
+        autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
 
     // Simulate user provides the OTP and clicks 'Confirm' in the OTP dialog.
     // TestPaymentsNetworkInterface just stores the unmask request detail, won't
@@ -372,7 +373,8 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthServerNonVcnError) {
   EXPECT_FALSE(OtpAuthenticatorContextToken().empty());
   EXPECT_NE(OtpAuthenticatorContextToken(),
             "context_token_from_previous_unmask_response");
-  // TODO(crbug.com/1243475): Verify the otp dialog is shown.
+  EXPECT_TRUE(
+      autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
 
   // Simulate user provides the OTP and clicks 'Confirm' in the OTP dialog.
   // TestPaymentsNetworkInterface just stores the unmask request detail, won't
@@ -419,7 +421,9 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthMismatchThenRetry) {
   EXPECT_FALSE(OtpAuthenticatorContextToken().empty());
   EXPECT_NE(OtpAuthenticatorContextToken(),
             "context_token_from_previous_unmask_response");
-  // TODO(crbug.com/1243475): Verify the otp dialog is shown.
+  EXPECT_TRUE(
+      autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
+  autofill_client_.GetPaymentsAutofillClient()->ResetShowOtpInputDialog();
 
   // Simulate user provides the OTP and clicks 'Confirm' in the OTP dialog.
   // TestPaymentsNetworkInterface just stores the unmask request detail, won't
@@ -443,8 +447,6 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthMismatchThenRetry) {
       /*context_token=*/"context_token_from_incorrect_otp");
   // Verify the context token is updated with unmask response.
   EXPECT_EQ(OtpAuthenticatorContextToken(), "context_token_from_incorrect_otp");
-  // TODO(crbug.com/1243475): Verify the otp dialog is updated with the mismatch
-  // info.
 
   // Simulate user types in another otp and click 'Confirm' again.
   authenticator_->OnUnmaskPromptAccepted(/*otp=*/u"333333");
@@ -462,6 +464,8 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthMismatchThenRetry) {
   ASSERT_TRUE(requester_->did_succeed().has_value());
   EXPECT_TRUE(*(requester_->did_succeed()));
   EXPECT_EQ(kTestNumber16, requester_->number());
+  EXPECT_FALSE(
+      autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
 
   // Ensures the metrics have been logged correctly.
   histogram_tester.ExpectUniqueSample(
@@ -498,7 +502,9 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthExpiredThenResendOtp) {
   EXPECT_FALSE(OtpAuthenticatorContextToken().empty());
   EXPECT_NE(OtpAuthenticatorContextToken(),
             "context_token_from_previous_unmask_response");
-  // TODO(crbug.com/1243475): Verify the otp dialog is shown.
+  EXPECT_TRUE(
+      autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
+  autofill_client_.GetPaymentsAutofillClient()->ResetShowOtpInputDialog();
 
   // Simulate user provides the OTP and clicks 'Confirm' in the OTP dialog.
   // TestPaymentsNetworkInterface just stores the unmask request detail, won't
@@ -522,18 +528,18 @@ TEST_P(CreditCardOtpAuthenticatorTest, OtpAuthExpiredThenResendOtp) {
       /*context_token=*/"context_token_from_expired_otp");
   // Verify the context token is updated with unmask response.
   EXPECT_EQ(OtpAuthenticatorContextToken(), "context_token_from_expired_otp");
-  // TODO(crbug.com/1243475): Verify the otp dialog is updated with the otp
-  // expired info.
 
   // Simulate user clicks "Get new code" from the UI, which calls
   // SendSelectChallengeOptionRequest() again. This will send the same selected
   // challenge option with the new context token.
-  authenticator_->SendSelectChallengeOptionRequest();
+  authenticator_->OnNewOtpRequested();
   // Verify the second SelectChallengeRequest is correctly set, the only
   // difference from the previous call is the context_token.
   verifySelectChallengeOptionRequest(
       /*context_token=*/"context_token_from_expired_otp",
       kTestBillingCustomerNumber);
+  EXPECT_FALSE(
+      autofill_client_.GetPaymentsAutofillClient()->show_otp_input_dialog());
 
   // Simulate user receives the new otp and types in the new otp.
   authenticator_->OnUnmaskPromptAccepted(/*otp=*/u"555555");
