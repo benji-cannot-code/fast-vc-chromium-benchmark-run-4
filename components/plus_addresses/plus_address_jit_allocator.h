cf8c006bb8f892b1cbcfae5b28d48541be9bfc28
Jan Keitel <jkeitel@google.com>
2024-04-17 14:36:45 +0000

    Add error handling for plus address refresh.
    If a refresh request receives a 429 response, then the refresh quota
    is exhausted. This CL adds support to disable refreshing for 24h after
    receiving a 429 response.
    
    Bug: 324557932
    Change-Id: I2da44e7d7d1c1424a21e273e82dd69de1163114e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458755
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288696}

--- components/plus_addresses/plus_address_jit_allocator.h
+++ components/plus_addresses/plus_address_jit_allocator.h
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_JIT_ALLOCATOR_H_
 #define COMPONENTS_PLUS_ADDRESSES_PLUS_ADDRESS_JIT_ALLOCATOR_H_
 
-#include "components/plus_addresses/plus_address_allocator.h"
-
 #include "base/containers/flat_map.h"
 #include "base/memory/raw_ref.h"
+#include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
+#include "components/plus_addresses/plus_address_allocator.h"
+#include "components/plus_addresses/plus_address_types.h"
 #include "url/origin.h"
 
 namespace plus_addresses {
@@ -29,6 +31,12 @@ class PlusAddressJitAllocator : public PlusAddressAllocator {
   bool IsRefreshingSupported(const url::Origin& origin) const override;
 
  private:
+  // Checks the `profile_or_error` response before passing it on to `callback`.
+  // If it receives a 429 code, it disables refreshing until a cooldown period
+  // has expired.
+  void HandleRefreshResponse(PlusAddressRequestCallback callback,
+                             const PlusProfileOrError& profile_or_error);
+
   // Responsible for server communication. Owned by the `PlusAddressService` and
   // outlives `this`.
   const raw_ref<PlusAddressHttpClient> http_client_;
@@ -36,6 +44,11 @@ class PlusAddressJitAllocator : public PlusAddressAllocator {
   // Counts how many refresh attempts where made for an `Origin`. Serves to
   // limit the number of refresh requests per session.
   base::flat_map<url::Origin, int> refresh_attempts_;
+
+  // The last known time that the user reached the refresh limit.
+  base::TimeTicks time_refresh_limit_reached_;
+
+  base::WeakPtrFactory<PlusAddressJitAllocator> weak_ptr_factory_{this};
 };
 
 }  // namespace plus_addresses
