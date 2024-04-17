ad16410f64a28f99b8e999397cdc47cfa0afb9f0
Hirthanan Subenderan <hirthanan@google.com>
2024-04-17 18:01:05 +0000

    Decouple CrOS secondary user UMA consent from device owner consent.
    This change ensures that regardless of the Device Owner's UMA enabled
    state, secondary users can manage their UMA consent (during OOBE, or
    from chrome://settings toggle).
    
    During the OOBE consent flow, secondary users will continue to
    be opt-in by default.
    
    The scope of this change is for unmanaged ChromeOS devices. Managed
    devices do not allow users to change consent, as it is managed by the
    admins enterprise policy.
    
    Prior to this change, the OOBE flow was confusing. For example,
    if the Device Owner turns off UMA, subsequent users didn't have the
    option to enable consent. When the Device Owner consents to UMA,
    secondary users have the ability to toggle their consent.
    
    This previous behaviour was confusing because a user with
    multiple profiles on the same CrOS device might incorrectly think that
    turning off UMA for their secondary profile will turn off UMA for
    everyone on the device.
    
    Test plan & new OOBE flows: go/cros-per-user-consent-flow-changes
    
    BUG=b/283174830
    
    Change-Id: Ife60d6f2fb0fe7961038006c6bf9a417a515d3fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5297698
    Reviewed-by: Osama Fathy <osamafathy@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288835}

--- chrome/browser/ash/login/shill_profile_loading_browsertest.cc
+++ chrome/browser/ash/login/shill_profile_loading_browsertest.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/login_manager_test.h"
 #include "chrome/browser/ash/login/startup_utils.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
+#include "chrome/browser/ash/login/test/oobe_screens_utils.h"
 #include "chrome/browser/ash/login/test/user_policy_mixin.h"
 #include "chrome/browser/ash/login/ui/user_adding_screen.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -238,9 +239,14 @@ IN_PROC_BROWSER_TEST_F(ShillProfileLoadingGuestLoginTest, GuestLogin) {
   LoadShillProfileWaiter load_shill_profile_waiter(
       FakeSessionManagerClient::Get());
 
-  // Mark EULA accepted to skip the guest ToS screen.
+  // The ToS screen is shown to guest users regardless of EULA being accepted
+  // previously by the device owner.
   StartupUtils::MarkEulaAccepted();
   ASSERT_TRUE(LoginScreenTestApi::ClickGuestButton());
+
+  // Accept guest ToS consent screen.
+  ash::test::WaitForGuestTosScreen();
+  ash::test::TapGuestTosAccept();
 
   restart_job_waiter.Run();
 
--- chrome/browser/ui/ash/login_screen_client_impl.cc
+++ chrome/browser/ui/ash/login_screen_client_impl.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/lock_screen_reauth/lock_screen_reauth_dialogs.h"
 #include "chrome/browser/ui/webui/ash/login/l10n_util.h"
 #include "chrome/common/webui_url_constants.h"
+#include "chromeos/ash/components/install_attributes/install_attributes.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/components/settings/cros_settings_provider.h"
 #include "components/session_manager/core/session_manager.h"
@@ -358,16 +359,15 @@ void LoginScreenClientImpl::LoginAsGuest() {
 }
 
 void LoginScreenClientImpl::ShowGuestTosScreen() {
-  // Guet ToS screen is only shown if EULA was not already accepted.
-  if (ash::StartupUtils::IsEulaAccepted()) {
+  // EULA is already accepted on enterprise managed devices.
+  // Unmanaged guests on managed devices should login directly without seeing
+  // the ToS screen. Managed guest sessions are handled separately.
+  if (ash::InstallAttributes::Get()->IsEnterpriseManaged()) {
     LoginAsGuest();
     return;
   }
 
-  DCHECK(!ash::ScreenLocker::default_screen_locker());
-  if (ash::LoginDisplayHost::default_host()) {
-    ash::LoginDisplayHost::default_host()->ShowGuestTosScreen();
-  }
+  ash::LoginDisplayHost::default_host()->ShowGuestTosScreen();
 }
 
 void LoginScreenClientImpl::OnMaxIncorrectPasswordAttempted(
