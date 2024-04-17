949f7f22881f993dff896b69ed824579edb1dc68
Livia Seibert <livseibert@gmail.com>
2024-04-17 18:23:31 +0000

    Return status instead of bool, add comments, and add to claim header.
    Part of porting Oak attestation to C++.
    
    Change-Id: I7ab5a27c55932950f559389bcccec99e0cd707d9
    Bug: b/324141620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458148
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Livia Seibert <livseibert@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288858}

--- device/fido/enclave/verify/claim.h
+++ device/fido/enclave/verify/claim.h
@@ -11,10 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/time/time.h"
+#include "base/types/expected.h"
 
 namespace device::enclave {
 
+// A software artifact identified by its name and a set of artifacts.
 struct Subject {
   Subject(std::string name, std::map<std::string, std::string> digest);
   Subject();
@@ -24,6 +27,7 @@ struct Subject {
   std::map<std::string, std::string> digest;
 };
 
+// Represents a generic statement that binds a predicate to a subject.
 template <typename T>
 struct Statement {
   std::string type;
@@ -32,6 +36,8 @@ struct Statement {
   T predicate;
 };
 
+// Metadata about an artifact that serves as the evidence for the truth of a
+// claim.
 struct ClaimEvidence {
   ClaimEvidence(std::optional<std::string> role,
                 std::string uri,
@@ -44,11 +50,13 @@ struct ClaimEvidence {
   std::vector<uint8_t> digest;
 };
 
+// Validity time range of an issued claim.
 struct ClaimValidity {
   base::Time not_before;
   base::Time not_after;
 };
 
+// Detailed content of a claim.
 template <typename T>
 struct ClaimPredicate {
   std::string claim_type;
@@ -59,9 +67,30 @@ struct ClaimPredicate {
   std::vector<ClaimEvidence> evidence;
 };
 
+// Inner type for a simple claim with no further fields.
 struct Claimless {};
 
 typedef Statement<ClaimPredicate<Claimless>> EndorsementStatement;
+
+// Converts the given byte array into an endorsement statement.
+base::expected<EndorsementStatement, std::string> ParseEndorsementStatement(
+    base::span<const uint8_t> bytes);
+
+// Checks that the given statement is a valid claim:
+// - has valid Statement and Predicate types, and
+// - has a valid validity duration.
+template <typename T>
+bool ValidateClaim(const Statement<ClaimPredicate<T>>& claim);
+
+// Checks that the input claim has a validity duration, and that the specified
+// time is inside the validity period.
+template <typename T>
+bool VerifyValidityDuration(base::Time now,
+                            const Statement<ClaimPredicate<T>>& claim);
+
+// Checks that the given endorsement statement is a valid and has the correct
+// claim type.
+bool ValidateEndorsement(const EndorsementStatement& claim);
 
 }  // namespace device::enclave
 
