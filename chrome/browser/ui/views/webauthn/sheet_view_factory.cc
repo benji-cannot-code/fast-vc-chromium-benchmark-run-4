01f8f1a8cd9518e9d48198a65f7cead9288c81b7
Adem Derinel <derinel@google.com>
2024-04-17 07:15:16 +0000

    WebAuthn: Add row with username and icon in Create GPM Passkey dialog
    Impl: screen/BU7JCeS6ufEZJAm
    
    Bug: 40274370
    Change-Id: I69d204f153944cdd98e7e7d1e4c4783bcf9e9eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5428619
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288544}

--- chrome/browser/ui/views/webauthn/sheet_view_factory.cc
+++ chrome/browser/ui/views/webauthn/sheet_view_factory.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_model.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_bio_enrollment_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_client_pin_entry_sheet_view.h"
+#include "chrome/browser/ui/views/webauthn/authenticator_create_gpm_passkey_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_arbitrary_pin_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_onboarding_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_pin_sheet_view.h"
@@ -383,9 +384,7 @@ std::unique_ptr<AuthenticatorRequestSheetView> CreateSheetViewForCurrentStepOf(
               dialog_model));
       break;
     case Step::kGPMCreatePasskey:
-      // TODO(derinel): use a specific view to include the row with the passkey
-      // icon and the credential name.
-      sheet_view = std::make_unique<AuthenticatorRequestSheetView>(
+      sheet_view = std::make_unique<AuthenticatorCreateGpmPasskeySheetView>(
           std::make_unique<AuthenticatorCreateGpmPasskeySheetModel>(
               dialog_model));
       break;
