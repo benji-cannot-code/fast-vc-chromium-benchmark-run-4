d15daf7f0be9a30a2f2692d49e299f06b9857d8e
Hayato Ito <hayato@chromium.org>
2024-04-17 01:40:50 +0000

    [URL] Don't navigate to an invalid "javascript:" scheme URL
    fast/loader/javascript-url-hierarchical-execution.html is one of the
    tests which would fail if kStandardCompliandNonSpecialSchemeURLParser
    flag is enabled [^1] [^2].
    
    It seems a browser navigates to "javascript://Spaceballs:" URL, and
    the navigation hits
    `DCHECK(!info.url_request.Url().ProtocolIs("javascript"))` [^3]:
    
    ```
    bool WebLocalFrameImpl::WillStartNavigation(const WebNavigationInfo& info) {
      DCHECK(!info.url_request.IsNull());
      DCHECK(!info.url_request.Url().ProtocolIs("javascript"));
      return GetFrame()->Loader().WillStartNavigation(info);
    }
    ```
    
    The test itself also fails because the custom policy delegate
    indicates that a load was attempted.
    
    Background / Investigation:
    
    1. When the feature is enabled, "javascript:" scheme URLs can be an
       invalid URL.
    
       e.g. "javascript://a b", which is a hierarchical non-special URL,
       but its host part contains a space, which is not allowed.
    
       Currently, without the flag, any "javascript:" scheme URL is
       considered as a valid URL because it is parsed as an *opaque path*
       URL.
    
    2. The current JavaScript execution code uses
       `KURL::ProtocolIsJavaScript()` to process a URL as "javascript:"
       URL, but it returns false for an invalid URL, even if the URL starts
       with "javascript:".
    
       So invalid "javascript:" URLs is not executed in a renderer and
       the URL is passed to navigation code. That upsets browser
       navigation code.
    
    Fix:
    
    I investigated the standard compliant behavior, and found
    wpt/url/javascript-urls.window.js. It seems the HTML and URL Standard
    requires that invalid "javascript:" URLs should NOT be executed.
    
    So the fix is to prevent a navigation for an invalid "javascript:"
    URL. See `FrameLoader::StartNavigation`.
    
    The CL also updates the description of
    `fast/loader/javascript-url-hierarchical-execution.html` to explain the intention of the test well. The previous description seems a bit outdated.
    
    Now both `fast/loader/javascript-url-hierarchical-execution.html` and
    `wpt/url/javascript-urls.window.js' tests in virtual/url-non-special
    pass. Neither JavaScript execution nor navigation happens for invalid
    "javascript:" URLs.
    
    Impacts:
    
    The following explanation is not directly related to this CL, but for
    the record, let me note the impact:
    
    The StandardCompliandNonSpecialSchemeURLParser feature affects URLs
    which start with "javascript://" or "javascript:/". Usually, such a
    "javascript:" URL contains only a comment. So there is no practical
    changes in most cases, unless a URL contains a new line ('%0a'),
    such as, "javascript://comment hello/%0aglobal_variable_a=1".
    
    [^1]: https://ci.chromium.org/ui/p/chromium/builders/try/linux-rel/1752691/test-results?sortby=&groupby=
    [^2]: https://docs.google.com/spreadsheets/d/1Pqw1iKXK_lxHj-kLIAeRFs-khFz-BPZDio1W7SgEVE4/edit?usp=sharing
    [^3]: Why a browser receives "javascript://Spaceballs:", instead of
        "javascript://Spaceballs: The Comment!", is another interesting
        topic, but I omit the details in this CL. In short, invalid URL's
        string representation is not guaranteed.
    
    Bug: 40063064
    Change-Id: I914be8060166084815ba5186a106ecff00c179e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454874
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288459}

--- third_party/blink/renderer/core/loader/frame_loader.cc
+++ third_party/blink/renderer/core/loader/frame_loader.cc
@@ -134,6 +134,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/weborigin/security_policy.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_utf8_adaptor.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "url/url_features.h"
 
 namespace blink {
 
@@ -793,6 +794,18 @@ void FrameLoader::StartNavigation(FrameLoadRequest& request,
       frame_->GetDocument()->ProcessJavaScriptUrl(url,
                                                   request.JavascriptWorld());
     }
+    return;
+  }
+  // If kStandardCompliantNonSpecialSchemeURLParsing feature is enabled,
+  // "javascript:" scheme URL can be a invalid URL. e.g. "javascript://a b".
+  //
+  // We shouldn't navigate to such an invalid "javascript:" scheme URL.
+  //
+  // See wpt/url/javascript-urls.window.js test for the standard compliant
+  // behaviors.
+  if (url::IsUsingStandardCompliantNonSpecialSchemeURLParsing() &&
+      ProtocolIsJavaScript(url.GetString())) {
+    DCHECK(!url.IsValid());
     return;
   }
 
