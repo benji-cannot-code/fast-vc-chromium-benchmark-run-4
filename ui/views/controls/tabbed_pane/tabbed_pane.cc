5365db1226e90c9b9de5e7744b05934436947c7d
weidongliu <weidongliu@chromium.org>
2024-04-17 23:23:41 +0000

    use CalculatePreferredSize(SizeBounds) in //ui/views
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: Id40aa2bb592e144ddc7c14588352ada8481245c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458613
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289043}

--- ui/views/controls/tabbed_pane/tabbed_pane.cc
+++ ui/views/controls/tabbed_pane/tabbed_pane.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/i18n/rtl.h"
+#include "base/notreached.h"
 #include "build/build_config.h"
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkPath.h"
@@ -132,19 +133,17 @@ void TabbedPaneTab::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 gfx::Size TabbedPaneTab::CalculatePreferredSize() const {
-  int width = preferred_title_width_ + GetInsets().width();
-  if (tabbed_pane_->GetStyle() == TabbedPane::TabStripStyle::kHighlight &&
-      tabbed_pane_->GetOrientation() == TabbedPane::Orientation::kVertical)
-    width = std::max(width, 192);
-  return gfx::Size(width, 32);
+  NOTREACHED_NORETURN() << "Use CalculatePreferredSize(SizeBounds)";
 }
 
-int TabbedPaneTab::GetHeightForWidth(int w) const {
-  // Because we set the LayoutManager, it will use
-  // LayoutManager::GetPreferredHeightForWidth by default, but this is not
-  // consistent with the fixed height desired by CalculatePreferredSize, so we
-  // override it and call it manually.
-  return CalculatePreferredSize().height();
+gfx::Size TabbedPaneTab::CalculatePreferredSize(
+    const SizeBounds& available_size) const {
+  int width = preferred_title_width_ + GetInsets().width();
+  if (tabbed_pane_->GetStyle() == TabbedPane::TabStripStyle::kHighlight &&
+      tabbed_pane_->GetOrientation() == TabbedPane::Orientation::kVertical) {
+    width = std::max(width, 192);
+  }
+  return gfx::Size(width, 32);
 }
 
 void TabbedPaneTab::GetAccessibleNodeData(ui::AXNodeData* data) {
