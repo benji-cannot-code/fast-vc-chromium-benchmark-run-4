f8b7e3708f9ae96e1acc0734a98f31b28449f99d
Calder Kitagawa <ckitagawa@chromium.org>
2024-04-17 21:53:12 +0000

    [Hub] Default enable on ToT
    Enable on ToT after metrics green light.
    
    Also deflake two tests that have block submit.
    
    Bug: 335250391
    Change-Id: Ia655c4d509f22a7e04214280da7b0db33720c2d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459887
    Commit-Queue: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289003}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -425,7 +425,7 @@ BASE_FEATURE(kAndroidHatsRefactor,
              "AndroidHatsRefactor",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAndroidHub, "AndroidHub", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kAndroidHub, "AndroidHub", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidImprovedBookmarks,
              "AndroidImprovedBookmarks",
