c0c9747f31b1fe1a2720b974c21ddda6bd9554f4
Akihiro Ota <akihiroota@chromium.org>
2024-04-17 19:21:18 +0000

    FaceGaze: Consolidate test setup methods
    Currently three methods need to be called to initialize FaceGaze for
    tests. This change consolidates these methods so that devs only need
    to call one method, EnableFaceGaze(config), to setup FaceGaze.
    
    Bug: b/309121742
    Change-Id: I26caab3829c20be11e9cf75f4b7bf0111cb5aa46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455258
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288900}

--- chrome/browser/ash/accessibility/facegaze_browsertest.cc
+++ chrome/browser/ash/accessibility/facegaze_browsertest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "ui/accessibility/accessibility_features.h"
-#include "ui/display/test/display_manager_test_api.h"
 #include "ui/events/event.h"
 #include "ui/events/event_handler.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
@@ -26,8 +25,6 @@ using MediapipeGesture = FaceGazeTestUtils::MediapipeGesture;
 using MockFaceLandmarkerResult = FaceGazeTestUtils::MockFaceLandmarkerResult;
 
 namespace {
-
-const char* kDefaultDisplaySize = "1200x800";
 
 aura::Window* GetRootWindow() {
   auto* root_window = Shell::GetRootWindowForNewWindows();
@@ -95,12 +92,6 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
   void SetUpOnMainThread() override {
     InProcessBrowserTest::SetUpOnMainThread();
     GetRootWindow()->AddPreTargetHandler(&event_handler_);
-    display::test::DisplayManagerTestApi(Shell::Get()->display_manager())
-        .UpdateDisplay(kDefaultDisplaySize);
-
-    // Initialize FaceGaze.
-    utils_->EnableFaceGaze();
-    utils_->CreateFaceLandmarker();
   }
 
   void TearDownOnMainThread() override {
@@ -130,7 +121,7 @@ class FaceGazeIntegrationTest : public AccessibilityFeatureBrowserTest {
 };
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, UpdateCursorLocation) {
-  utils()->ConfigureFaceGaze(Config().Default());
+  utils()->EnableFaceGaze(Config().Default());
   event_handler().ClearEvents();
 
   utils()->ProcessFaceLandmarkerResult(
@@ -152,7 +143,7 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, UpdateCursorLocation) {
 }
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, ResetCursor) {
-  utils()->ConfigureFaceGaze(
+  utils()->EnableFaceGaze(
       Config()
           .Default()
           .WithGesturesToMacros(
@@ -184,7 +175,7 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, ResetCursor) {
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
                        IgnoreGesturesWithLowConfidence) {
-  utils()->ConfigureFaceGaze(
+  utils()->EnableFaceGaze(
       Config()
           .Default()
           .WithGesturesToMacros(
@@ -209,7 +200,7 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
                        UpdateCursorLocationWithSpeed1) {
-  utils()->ConfigureFaceGaze(Config().Default().WithCursorSpeeds(
+  utils()->EnableFaceGaze(Config().Default().WithCursorSpeeds(
       {/*up=*/1, /*down=*/1, /*left=*/1, /*right=*/1}));
 
   // With cursor acceleration off and buffer size 1, one-pixel head movements
@@ -226,7 +217,7 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest,
 }
 
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, SpaceKeyEvents) {
-  utils()->ConfigureFaceGaze(
+  utils()->EnableFaceGaze(
       Config()
           .Default()
           .WithGesturesToMacros(
@@ -258,7 +249,7 @@ IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, SpaceKeyEvents) {
 // ensures that the associated action is performed if either of the gestures is
 // detected.
 IN_PROC_BROWSER_TEST_F(FaceGazeIntegrationTest, BrowsDownGesture) {
-  utils()->ConfigureFaceGaze(
+  utils()->EnableFaceGaze(
       Config()
           .Default()
           .WithCursorLocation(gfx::Point(0, 0))
