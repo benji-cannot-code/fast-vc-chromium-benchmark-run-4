c20b831f8af447139f0bf8ddf57161b731c20149
Mike Wasserman <msw@chromium.org>
2024-04-17 23:03:30 +0000

    Fullscreen: Remove unused params from CanEnterFullscreenMode[ForTab]
    Bug: None
    Change-Id: I3bd1380037f9feb92752931a9dad9a0ddefc7ec3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463960
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289033}

--- chrome/browser/ui/exclusive_access/fullscreen_controller.h
+++ chrome/browser/ui/exclusive_access/fullscreen_controller.h
@@ -115,9 +115,7 @@ class FullscreenController : public ExclusiveAccessControllerBase {
 
   // Returns whether entering fullscreen with |EnterFullscreenModeForTab()| is
   // allowed.
-  bool CanEnterFullscreenModeForTab(
-      content::RenderFrameHost* requesting_frame,
-      const int64_t display_id = display::kInvalidDisplayId);
+  bool CanEnterFullscreenModeForTab(content::RenderFrameHost* requesting_frame);
 
   // Enter tab-initiated fullscreen mode. FullscreenController decides whether
   // to also fullscreen the browser window. See 'FullscreenWithinTab Note'.
--- content/public/browser/web_contents_delegate.cc
+++ content/public/browser/web_contents_delegate.cc
@@ -189,8 +189,7 @@ FullscreenState WebContentsDelegate::GetFullscreenState(
 }
 
 bool WebContentsDelegate::CanEnterFullscreenModeForTab(
-    RenderFrameHost* requesting_frame,
-    const blink::mojom::FullscreenOptions& options) {
+    RenderFrameHost* requesting_frame) {
   return true;
 }
 
