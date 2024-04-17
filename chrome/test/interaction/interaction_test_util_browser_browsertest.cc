0f9d06076455d6f3869724ddf79f3b63c6162353
Nicolás Peña <npm@chromium.org>
2024-04-17 19:19:49 +0000

    Disable InteractionTestUtilBrowserTest.CompareScreenshot_Surface on Win
    Bug: b/335464453
    Change-Id: I24ab809a611d45c67fff69064c0801acc8ff241f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463521
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288898}

--- chrome/test/interaction/interaction_test_util_browser_browsertest.cc
+++ chrome/test/interaction/interaction_test_util_browser_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/interaction/interaction_test_util_browser.h"
 
 #include "base/functional/bind.h"
+#include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -68,8 +69,13 @@ IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest, CompareScreenshot_View) {
                              /*baseline_cl=*/"3924454"));
 }
 
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_CompareScreenshot_Surface DISABLED_CompareScreenshot_Surface
+#else
+#define MAYBE_CompareScreenshot_Surface CompareScreenshot_Surface
+#endif
 IN_PROC_BROWSER_TEST_F(InteractionTestUtilBrowserTest,
-                       CompareScreenshot_Surface) {
+                       MAYBE_CompareScreenshot_Surface) {
   const auto lock =
       user_education::NewBadgeController::DisableNewBadgesForTesting();
   RunTestSequence(SetOnIncompatibleAction(OnIncompatibleAction::kSkipTest,
