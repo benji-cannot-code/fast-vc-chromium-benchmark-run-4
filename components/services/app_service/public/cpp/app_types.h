ff7e6d4dcd3f77bec5729edfe4f588f526f107ba
Jose Dapena Paz <jdapena@igalia.com>
2024-04-17 08:19:26 +0000

    IWYU: missing include for std::optional usage in app_types.h
    Bug: 40939315
    Change-Id: I845f4dd1598e38cbe5b22fe472079c41d24a8bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456358
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1288572}

--- components/services/app_service/public/cpp/app_types.h
+++ components/services/app_service/public/cpp/app_types.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_APP_TYPES_H_
 #define COMPONENTS_SERVICES_APP_SERVICE_PUBLIC_CPP_APP_TYPES_H_
 
+#include <optional>
+
 #include "base/component_export.h"
 #include "components/services/app_service/public/cpp/macros.h"
 #include "components/services/app_service/public/protos/app_types.pb.h"
