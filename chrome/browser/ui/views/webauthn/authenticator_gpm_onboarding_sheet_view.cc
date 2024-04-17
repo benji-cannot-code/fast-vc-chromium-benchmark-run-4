0eb748011aea04a3a16cfc0a4c3a216a48d8e94a
Adem Derinel <derinel@google.com>
2024-04-17 17:33:27 +0000

    [WebAuthn] Add username label to GPM Create Sheets
    Implementations:
     onboarding: screen/7CFwTmKUq3FbhAQ
     create: screen/5F8Q9PP5MbWtiaE
    
    Bug: 40274370
    Change-Id: Ic653b1e8b8b68ab90c11240d6ef87ca7c75d4db7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459133
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288813}

--- chrome/browser/ui/views/webauthn/authenticator_gpm_onboarding_sheet_view.cc
+++ chrome/browser/ui/views/webauthn/authenticator_gpm_onboarding_sheet_view.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_onboarding_sheet_view.h"
 
+#include <memory>
+
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
+#include "chrome/browser/ui/views/webauthn/authenticator_common_views.h"
 #include "ui/gfx/vector_icon_types.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -15,18 +18,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_provider.h"
 #include "ui/views/layout/layout_types.h"
 #include "ui/views/style/typography.h"
+#include "ui/views/view.h"
 
 namespace {
 
 constexpr int kIconSize = 20;
+constexpr int kGapSize = 8;
 
 std::unique_ptr<views::View> CreateIconWithLabelRow(
     const gfx::VectorIcon& icon,
     const std::u16string& label) {
   auto row = std::make_unique<views::BoxLayoutView>();
   row->SetOrientation(views::BoxLayout::Orientation::kHorizontal);
-  row->SetBetweenChildSpacing(views::LayoutProvider::Get()->GetDistanceMetric(
-      views::DISTANCE_RELATED_CONTROL_HORIZONTAL));
+  row->SetBetweenChildSpacing(kGapSize);
 
   row->AddChildView(std::make_unique<views::ImageView>(
       ui::ImageModel::FromVectorIcon(icon, ui::kColorIcon, kIconSize)));
@@ -60,6 +64,11 @@ AuthenticatorGpmOnboardingSheetView::BuildStepSpecificContent() {
   container->AddChildView(CreateIconWithLabelRow(
       // TODO(rgod): Add correct icon.
       kKeyIcon, u"One less password to manage or remember (UT)"));
+
+  auto* sheet_model = static_cast<AuthenticatorSheetModelBase*>(model());
+  std::u16string username = base::UTF8ToUTF16(
+      sheet_model->dialog_model()->user_entity.name.value_or(""));
+  container->AddChildView(CreatePasskeyWithUsernameLabel(username));
 
   return std::make_pair(std::move(container), AutoFocus::kNo);
 }
--- chrome/browser/ui/views/webauthn/sheet_view_factory.cc
+++ chrome/browser/ui/views/webauthn/sheet_view_factory.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/payments/webauthn_dialog_model.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_bio_enrollment_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_client_pin_entry_sheet_view.h"
-#include "chrome/browser/ui/views/webauthn/authenticator_create_gpm_passkey_sheet_view.h"
+#include "chrome/browser/ui/views/webauthn/authenticator_create_user_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_arbitrary_pin_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_onboarding_sheet_view.h"
 #include "chrome/browser/ui/views/webauthn/authenticator_gpm_pin_sheet_view.h"
@@ -379,12 +379,12 @@ std::unique_ptr<AuthenticatorRequestSheetView> CreateSheetViewForCurrentStepOf(
               dialog_model));
       break;
     case Step::kTrustThisComputerCreation:
-      sheet_view = std::make_unique<AuthenticatorRequestSheetView>(
+      sheet_view = std::make_unique<AuthenticatorCreateUserSheetView>(
           std::make_unique<AuthenticatorTrustThisComputerCreationSheetModel>(
               dialog_model));
       break;
     case Step::kGPMCreatePasskey:
-      sheet_view = std::make_unique<AuthenticatorCreateGpmPasskeySheetView>(
+      sheet_view = std::make_unique<AuthenticatorCreateUserSheetView>(
           std::make_unique<AuthenticatorCreateGpmPasskeySheetModel>(
               dialog_model));
       break;
