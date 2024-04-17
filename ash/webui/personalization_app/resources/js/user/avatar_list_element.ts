66fe484d8e4a1078b0a6174c0e22269302ea3ebb
Jason Thai <jasontt@chromium.org>
2024-04-17 21:49:46 +0000

    p13n: avatar sanitized image url update
    Update `getAvatarUrl` to partially match the logic in `cr-auto-img` for
    handling staticEncode param. This fixes the loading errors on the
    avatar page due to malformed sanitized urls.
    
    BUG=b:327032051
    TEST=browser_tests --gtest_filter="*UserPreview*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I3c01fa73402533e67647539eea5ca10c186dd937
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463425
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289000}

--- ash/webui/personalization_app/resources/js/user/avatar_list_element.ts
+++ ash/webui/personalization_app/resources/js/user/avatar_list_element.ts
@@ -430,7 +430,8 @@ export class AvatarListElement extends WithPersonalizationStore {
       assert(
           !url.startsWith('chrome://image/'),
           'The URL shouldn\'t be sanitized');
-      return `background-image: url('${getAvatarUrl(url)}&staticEncode=true')`;
+      return `background-image: url('${
+          getAvatarUrl(url, /*staticEncode=*/ true)}')`;
     }
     return '';
   }
