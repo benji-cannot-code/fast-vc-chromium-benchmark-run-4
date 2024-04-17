f11db4e4add374a8d66d8eefdcf66d3545f66290
Nicolás Peña <npm@chromium.org>
2024-04-17 19:36:09 +0000

    Revert "exo/base-client: Improve compatibility with common compositors"
    This reverts commit c29b483607b98c9f3d2c87dafac6dbb199473b47.
    
    Reason for revert: WaylandClientPerfTests.Simple is very flaky
    
    Bug: b/335466746
    
    Original change's description:
    > exo/base-client: Improve compatibility with common compositors
    >
    > - Use xdg-shell by default as wl-shell is deprecated and support has
    >   been dropped from some common compositors.
    > - Only ask for xdg-shell version 1. Unconditionally using the server
    >   version may cause crashes and no functionality from higher versions is
    >   implemented yet.
    > - Make the simple client fail on protocol errors.
    > - Fix a protocol error in the simple client triggered by creating
    >   multiple `wp_viewport`s.
    > - Remove `features::kOnBeginFrameAcks` workaround for the simple client
    >   in perftests.
    >
    > These changes are meant to make development and debugging easier.
    >
    > Bug: b:289488725
    > Change-Id: Ib727420664cacfed4204563e252a54aaaaaacfb4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4808103
    > Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    > Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1288073}
    
    Bug: b:289488725
    Change-Id: I006eea11b2733b139a7e06f4447f95886964705f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463182
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1288910}

--- components/exo/wayland/clients/simple.cc
+++ components/exo/wayland/clients/simple.cc
@@ -133,11 +133,7 @@ void Simple::Run(int frames,
   bool frame_callback_pending = false;
   wp_viewport* viewport =
       wp_viewporter_get_viewport(globals_.wp_viewporter.get(), surface_.get());
-
-  int dispatch_res = 0;
   do {
-    DCHECK(dispatch_res != -1);
-
     if (frame_callback_pending)
       continue;
 
@@ -203,9 +199,7 @@ void Simple::Run(int frames,
 
     wl_surface_commit(surface_.get());
     wl_display_flush(display_.get());
-  } while ((dispatch_res = wl_display_dispatch(display_.get())));
-
-  wp_viewport_destroy(viewport);
+  } while (wl_display_dispatch(display_.get()) != -1);
 
   if (feedback)
     *feedback = presentation.feedback;
