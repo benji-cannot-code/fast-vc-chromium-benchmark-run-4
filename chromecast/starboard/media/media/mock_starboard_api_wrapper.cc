ea83c5717b30ed8c36bd8ea3e65ca0a256ff6ff3
Antonio Rivera <antoniori@google.com>
2024-04-17 00:34:46 +0000

    Copy starboard cast code into chromium (part 4 of n).
    Contains starboard media API wrappers.
    
    This is part of the work to get cast on starboard building out of
    chromium. See go/moving-cwr-to-chromium for more information on the
    high-level goal.
    
    Bug: b:333571227
    Change-Id: Ie96a196c4e421acd2c61dfbb5514f306c2e2335a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447678
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288433}

---
+++ chromecast/starboard/media/media/mock_starboard_api_wrapper.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/starboard/media/media/mock_starboard_api_wrapper.h"
+
+namespace chromecast {
+namespace media {
+
+MockStarboardApiWrapper::MockStarboardApiWrapper() = default;
+MockStarboardApiWrapper::~MockStarboardApiWrapper() = default;
+
+}  // namespace media
+}  // namespace chromecast
