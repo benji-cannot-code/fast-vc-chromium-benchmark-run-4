eefaf1fff26c181291764004e354235caf38db71
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-04-17 21:15:25 +0000

    [views_ax] Introduce private `SetState` method to `ViewAccessibility`
    This CL introduces the `SetState` method to `ViewAccessibility`.
    
    Even though ax::mojom::State don't have an associated "value" like
    `SetState(state, value)` seems to infer, the `value` is used as
    convenience to indicate whether we want to add or remove the state.
    
    The CL based on top of this one as well as upcoming CLs will make
    use of this function.
    
    Bug: 325137417
    Change-Id: I556fc53c9d81aecc9600313baba9251838fe6159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5461388
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1288981}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -823,4 +823,12 @@ void ViewAccessibility::UnpruneSubtree() {
     child->UnpruneVirtualSubtree();
   }
 }
+
+void ViewAccessibility::SetState(ax::mojom::State state, bool is_enabled) {
+  if (is_enabled) {
+    data_.AddState(state);
+  } else {
+    data_.RemoveState(state);
+  }
+}
 }  // namespace views
