e80d1ee41b15824c23799f98eee1c4c641f9db70
Antonio Rivera <antoniori@google.com>
2024-04-17 00:33:48 +0000

    Copy starboard cast code into chromium (part 3 of n).
    Contains graphics code.
    
    This is part of the work to get cast on starboard building out of
    chromium. See go/moving-cwr-to-chromium for more information on the
    high-level goal.
    
    Bug: b:333571227
    Change-Id: Idb04303414cc9710c2100d9848c295435e1322a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448392
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Shawn Quereshi <shawnq@google.com>
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288432}

---
+++ chromecast/starboard/graphics/graphics_properties_default.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/public/graphics_properties_shlib.h"
+
+namespace chromecast {
+
+bool GraphicsPropertiesShlib::IsSupported(
+    Resolution resolution,
+    const std::vector<std::string>& argv) {
+  switch (resolution) {
+    case Resolution::k1080p:
+      return true;
+    case Resolution::kUHDTV:
+      return false;
+    default:
+      return false;
+  }
+}
+
+}  // namespace chromecast
