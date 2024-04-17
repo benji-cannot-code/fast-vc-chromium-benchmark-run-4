2f29618b125a5a649569a21d29acb5cc0d88f2b3
Jack Yammine <jyammine@google.com>
2024-04-17 14:49:13 +0000

    [waffle]Change button state when needed on resize
    The "More" button is displayed when the list of search engines doesn't
    fit in the screen. The button should change to "Set as default" when
    the screen is resized enough to fit the whole content.
    
    We also re-enable the SearchEngineChoiceUIPixelTest pixel tests in this
    change and fix a flakiness that was happening due to a hover color.
    
    Recording: https://crbug.com/333787236#comment5
    Bug: b:333787236
    Change-Id: Ief1fd62f43a88a2b391c0d3e5f5d783d860b0364
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453877
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288703}

--- chrome/browser/resources/search_engine_choice/app.ts
+++ chrome/browser/resources/search_engine_choice/app.ts
@@ -135,6 +135,7 @@ export class SearchEngineChoiceAppElement extends
         document.addEventListener('scroll', this.onPageScroll_.bind(this));
       }
 
+      window.addEventListener('resize', this.onPageResize_.bind(this));
       this.pageHandler_.displayDialog();
     });
   }
@@ -247,10 +248,21 @@ export class SearchEngineChoiceAppElement extends
     this.showSearchEngineSnippet_(parseInt(newPrepopulatedId));
   }
 
-  private onPageScroll_() {
+  private wasPageContentFullyDisplayed_() {
     // The value is checked against `< 1` instead of `=== 0` to keep a margin of
     // error.
-    if (document.body.scrollHeight - window.innerHeight - window.scrollY < 1) {
+    return document.body.scrollHeight - window.innerHeight - window.scrollY < 1;
+  }
+
+  private onPageResize_() {
+    if (this.wasPageContentFullyDisplayed_()) {
+      this.hasUserScrolledToTheBottom_ = true;
+      window.removeEventListener('resize', this.onPageScroll_.bind(this));
+    }
+  }
+
+  private onPageScroll_() {
+    if (this.wasPageContentFullyDisplayed_()) {
       this.hasUserScrolledToTheBottom_ = true;
       document.removeEventListener('scroll', this.onPageScroll_.bind(this));
     }
