534332f08db456e32216c18b47fd2e7e3525b379
Brian Osman <brianosman@google.com>
2024-04-17 19:06:20 +0000

    [blink] Remove use of SkPathBuilder
    This was an API refactor that never worked out. Switching back to
    SkPath, so we can delete the code from Skia.
    
    Bug: b/335431546
    Change-Id: I12a6e55000d84b473da94c467f0b00d14c94aa3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463021
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288885}

--- third_party/blink/renderer/core/paint/styleable_marker_painter.cc
+++ third_party/blink/renderer/core/paint/styleable_marker_painter.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/graphics_context_state_saver.h"
 #include "third_party/blink/renderer/platform/graphics/paint/paint_shader.h"
 #include "third_party/blink/renderer/platform/graphics/styled_stroke_data.h"
-#include "third_party/skia/include/core/SkPathBuilder.h"
+#include "third_party/skia/include/core/SkPath.h"
 
 namespace blink {
 
@@ -37,7 +37,7 @@ PaintRecord RecordMarker(Color blink_color) {
 
   // Adjust the phase such that f' == 0 is "pixel"-centered
   // (for optimal rasterization at native rez).
-  SkPathBuilder path;
+  SkPath path;
   path.moveTo(kMarkerWidth * -3 / 8, kMarkerHeight * 3 / 4);
   path.cubicTo(kMarkerWidth * -1 / 8, kMarkerHeight * 3 / 4,
                kMarkerWidth * -1 / 8, kMarkerHeight * 1 / 4,
@@ -57,7 +57,7 @@ PaintRecord RecordMarker(Color blink_color) {
 
   PaintRecorder recorder;
   recorder.beginRecording();
-  recorder.getRecordingCanvas()->drawPath(path.detach(), flags);
+  recorder.getRecordingCanvas()->drawPath(path, flags);
 
   return recorder.finishRecordingAsPicture();
 }
