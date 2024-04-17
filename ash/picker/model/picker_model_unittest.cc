4930b1a976999255350424dc93494e4a9a9c1e14
Darren Shen <shend@chromium.org>
2024-04-17 06:00:27 +0000

    Enable or disable Editor in Picker depending on availability.
    CacheEditorContext returns a valid callback if Editor is available.
    
    Bug: b/334703181
    Change-Id: I209f7189f1feee930293843df6b834622a1cc6ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5452831
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288530}

--- ash/picker/model/picker_model_unittest.cc
+++ ash/picker/model/picker_model_unittest.cc
@@ -17,10 +17,12 @@ namespace {
 
 using ::testing::Contains;
 using ::testing::ElementsAre;
+using ::testing::Not;
 
 TEST(PickerModel, AvailableCategoriesWithNoFocusHasCorrectOrdering) {
   input_method::FakeImeKeyboard fake_ime_keyboard;
-  PickerModel model(/*focused_client=*/nullptr, &fake_ime_keyboard);
+  PickerModel model(/*focused_client=*/nullptr, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_THAT(
       model.GetAvailableCategories(),
       ElementsAre(PickerCategory::kCapsOn, PickerCategory::kEditor,
@@ -35,7 +37,8 @@ TEST(PickerModel, AvailableCategoriesWithNoSelectedTextHasCorrectOrdering) {
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
   client.SetTextAndSelection(u"a", gfx::Range(0));
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_THAT(
       model.GetAvailableCategories(),
       ElementsAre(PickerCategory::kCapsOn, PickerCategory::kEditor,
@@ -50,7 +53,8 @@ TEST(PickerModel, AvailableCategoriesWithSelectedTextHasCorrectOrdering) {
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
   client.SetTextAndSelection(u"a", gfx::Range(0, 1));
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_THAT(
       model.GetAvailableCategories(),
       ElementsAre(PickerCategory::kEditor, PickerCategory::kUpperCase,
@@ -63,7 +67,8 @@ TEST(PickerModel, AvailableCategoriesShowsCapsOffWhenCapsIsOn) {
   fake_ime_keyboard.SetCapsLockEnabled(true);
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_THAT(model.GetAvailableCategories(),
               Contains(PickerCategory::kCapsOff));
 }
@@ -73,9 +78,32 @@ TEST(PickerModel, AvailableCategoriesShowsCapsOnWhenCapsIsOff) {
   fake_ime_keyboard.SetCapsLockEnabled(false);
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_THAT(model.GetAvailableCategories(),
               Contains(PickerCategory::kCapsOn));
+}
+
+TEST(PickerModel, AvailableCategoriesEnablesEditorWhenEnabled) {
+  input_method::FakeImeKeyboard fake_ime_keyboard;
+  fake_ime_keyboard.SetCapsLockEnabled(false);
+  ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
+
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
+  EXPECT_THAT(model.GetAvailableCategories(),
+              Contains(PickerCategory::kEditor));
+}
+
+TEST(PickerModel, AvailableCategoriesDisablesEditorWhenDisabled) {
+  input_method::FakeImeKeyboard fake_ime_keyboard;
+  fake_ime_keyboard.SetCapsLockEnabled(false);
+  ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
+
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kDisabled);
+  EXPECT_THAT(model.GetAvailableCategories(),
+              Not(Contains(PickerCategory::kEditor)));
 }
 
 TEST(PickerModel, GetsEmptySelectedText) {
@@ -83,7 +111,8 @@ TEST(PickerModel, GetsEmptySelectedText) {
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
   client.SetTextAndSelection(u"abcd", gfx::Range(1, 1));
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_FALSE(model.HasSelectedText());
   EXPECT_EQ(model.selected_text(), u"");
 }
@@ -93,7 +122,8 @@ TEST(PickerModel, GetsNonEmptySelectedText) {
   ui::FakeTextInputClient client({.type = ui::TEXT_INPUT_TYPE_TEXT});
   client.SetTextAndSelection(u"abcd", gfx::Range(1, 3));
 
-  PickerModel model(&client, &fake_ime_keyboard);
+  PickerModel model(&client, &fake_ime_keyboard,
+                    PickerModel::EditorStatus::kEnabled);
   EXPECT_TRUE(model.HasSelectedText());
   EXPECT_EQ(model.selected_text(), u"bc");
 }
--- ash/picker/picker_controller_unittest.cc
+++ ash/picker/picker_controller_unittest.cc
@@ -39,7 +39,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
+using ::testing::Contains;
 using ::testing::NiceMock;
+using ::testing::Not;
+using ::testing::Return;
 
 bool CopyTextToClipboard() {
   base::test::TestFuture<bool> copy_confirmed_future;
@@ -116,17 +119,8 @@ class TestPickerClient : public MockPickerClient {
   }
   ~TestPickerClient() override { controller_->SetClient(nullptr); }
 
-  base::test::TestFuture<void>& show_editor_future() {
-    return show_editor_future_;
-  }
-
-  ShowEditorCallback CacheEditorContext() override {
-    return show_editor_future_.GetCallback();
-  }
-
  private:
   raw_ptr<PickerController> controller_ = nullptr;
-  base::test::TestFuture<void> show_editor_future_;
 };
 
 TEST_F(PickerControllerTest, ToggleWidgetShowsWidgetIfClosed) {
@@ -384,11 +378,39 @@ TEST_F(PickerControllerTest, ShowingAndClosingWidgetRecordsUsageMetrics) {
 TEST_F(PickerControllerTest, ShowEditorCallsCallbackFromClient) {
   PickerController controller;
   NiceMock<TestPickerClient> client(&controller);
-  controller.ToggleWidget();
+  base::test::TestFuture<void> show_editor_future;
+  EXPECT_CALL(client, CacheEditorContext)
+      .WillOnce(Return(show_editor_future.GetCallback()));
 
+  controller.ToggleWidget();
   controller.ShowEditor();
 
-  EXPECT_TRUE(client.show_editor_future().Wait());
+  EXPECT_TRUE(show_editor_future.Wait());
+}
+
+TEST_F(PickerControllerTest, AvailableCategoriesContainsEditorWhenEnabled) {
+  PickerController controller;
+  NiceMock<TestPickerClient> client(&controller);
+  EXPECT_CALL(client, CacheEditorContext).WillOnce(Return(base::DoNothing()));
+
+  controller.ToggleWidget();
+  controller.ShowEditor();
+
+  EXPECT_THAT(controller.GetAvailableCategories(),
+              Contains(PickerCategory::kEditor));
+}
+
+TEST_F(PickerControllerTest,
+       AvailableCategoriesDoesNotContainEditorWhenDisabled) {
+  PickerController controller;
+  NiceMock<TestPickerClient> client(&controller);
+  EXPECT_CALL(client, CacheEditorContext)
+      .WillOnce(Return(base::NullCallback()));
+
+  controller.ToggleWidget();
+
+  EXPECT_THAT(controller.GetAvailableCategories(),
+              Not(Contains(PickerCategory::kEditor)));
 }
 
 TEST_F(PickerControllerTest, GetUpperCaseSelectedText) {
