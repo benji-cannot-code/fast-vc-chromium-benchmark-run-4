0ba15d10968207816104a54a00e646379658f318
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-04-17 07:43:19 +0000

    [iOS] Open link in tab group
    The aim of this CL is to add to option of opening a link in a new or an
    existing tab group.
    
    Demo: https://drive.google.com/file/d/18SY_jkRaaJG0l65P432ArjxSP_2zAyrE/view?usp=sharing&resourcekey=0-ZGUgUWyLKKVR3dB3H0iYQg
    
    Bug: 328356222
    Change-Id: Ie423814306dd7c595dfbc23132f60c1b215954f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5452874
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288556}

--- ios/chrome/browser/url_loading/model/url_loading_params.mm
+++ ios/chrome/browser/url_loading/model/url_loading_params.mm
@@ -68,6 +68,8 @@ UrlLoadParams UrlLoadParams::SwitchToTab(
 
 UrlLoadParams::UrlLoadParams() = default;
 
+UrlLoadParams::~UrlLoadParams() = default;
+
 UrlLoadParams::UrlLoadParams(const UrlLoadParams& other) = default;
 
 UrlLoadParams& UrlLoadParams::operator=(const UrlLoadParams& other) = default;
