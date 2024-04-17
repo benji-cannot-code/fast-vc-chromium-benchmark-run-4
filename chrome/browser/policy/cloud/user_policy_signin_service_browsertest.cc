3ff9b923498dd2ed612b01c425b411c3a9c0f157
Nicolás Peña <npm@chromium.org>
2024-04-17 17:52:52 +0000

    Revert "[Uno-D] Enable fieldtrial_testing_config for full phase"
    This reverts commit 3b980d0bd50a84f848f2bf420646bf9fbcbb1b98.
    
    Reason for revert: TurnSyncOnHelperBrowserTestWithParam.PrimaryAccountResetAfterSyncOptInFlowAborted/2 consistently failing
    
    Bug: b/335453969
    
    Original change's description:
    > [Uno-D] Enable fieldtrial_testing_config for full phase
    >
    > Followup to https://crrev.com/c/5397876.
    > Adapted some remaining tests.
    >
    > Bug: b/326204909
    > Change-Id: I4e86958bb2d815d4c3db59be78689bbe276e81bf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5407891
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Reviewed-by: Monica Basta <msalama@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1288640}
    
    Bug: b/326204909
    Change-Id: I691f90a285fa2cc814a747181d0bbac1e01b48c2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462900
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288829}

--- chrome/browser/policy/cloud/user_policy_signin_service_browsertest.cc
+++ chrome/browser/policy/cloud/user_policy_signin_service_browsertest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/accounts_mutator.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
-#include "components/signin/public/identity_manager/primary_account_mutator.h"
 #include "components/sync/base/command_line_switches.h"
 #include "content/public/test/browser_test.h"
 #include "google_apis/gaia/fake_gaia.h"
@@ -526,8 +525,10 @@ IN_PROC_BROWSER_TEST_P(UserPolicySigninServiceTest,
 
   if (!base::FeatureList::IsEnabled(kDisallowManagedProfileSignout)) {
     // Signout
-    identity_manager()->GetPrimaryAccountMutator()->ClearPrimaryAccount(
-        signin_metrics::ProfileSignout::kTest);
+    auto* accounts_mutator = identity_manager()->GetAccountsMutator();
+    accounts_mutator->RemoveAccount(
+        account_id(), signin_metrics::SourceForRefreshTokenOperation::
+                          kDiceResponseHandler_Signout);
     EXPECT_TRUE(signin_client()->IsClearPrimaryAccountAllowed(
         /*has_sync_account=*/false));
     EXPECT_EQ(std::nullopt,
