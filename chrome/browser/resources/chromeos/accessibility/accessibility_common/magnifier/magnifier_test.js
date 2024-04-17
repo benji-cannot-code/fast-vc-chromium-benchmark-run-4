9897a47727fa2c55146060775835188aeb8ade69
Katie Dektar <katie@chromium.org>
2024-04-17 16:30:34 +0000

    Reland "[a11y extensions] Remove importModules from test base"
    This reverts commit 717acb7812aeb9063e795a004d68b1e4fde55cf7.
    
    Reason for revert: Speculative revert of the CL didn't fix
    the flakes, so we can reland.
    
    Original change's description:
    > Revert "Reland "[a11y extensions] Remove importModules from test base""
    >
    > This reverts commit a7c0a485aa5e5caa3ae71f1dbeeea4384569faa4.
    >
    > Reason for revert: linux-chromeos-chrome-with-lacros is failing on CI. Speculatively revert this change to see if it fixes it.
    >
    > Original change's description:
    > > Reland "[a11y extensions] Remove importModules from test base"
    > >
    > > This is a reland of commit 468a0c11995d1a1625f449bc8f4def7a3a2c0095
    > >
    > > Was reverted because test SwitchAccessTabNodeTest.FindCloseButton was failing after upload on browser_tests_with_lacros.
    > >
    > > Original change's description:
    > > > [a11y extensions] Remove importModules from test base
    > > >
    > > > In addition to removing calls to importModules, we need to add a few imports to select_to_speak_main. This is because the tests rely on some infrastructure from common/ that Select-to-Speak does not otherwise use, and with the updated import method modules need to be loaded before they can be imported in tests.
    > > >
    > > > AX-Relnotes: n/a.
    > > > Bug: b/260590502
    > > > Change-Id: I94f76553c1c9852c683937008f0da67dec3d996e
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5321574
    > > > Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    > > > Reviewed-by: Katie Dektar <katie@chromium.org>
    > > > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1272501}
    > >
    > > AX-Relnotes: n/a.
    > > Cq-Include-Trybots: luci.chromium.try:chromeos-amd64-generic-lacros-dbg,lacros-amd64-generic-rel-gtest,lacros-arm-generic-rel
    > > Bug: b/260590502
    > > Change-Id: I85f07e073b073467d04318feff83a4b71437482e
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5372890
    > > Reviewed-by: Katie Dektar <katie@chromium.org>
    > > Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    > > Commit-Queue: Katie Dektar <katie@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1275903}
    >
    > Bug: b/260590502, b/333055971
    > Change-Id: I0d279227eed916ed20426aabdfb9dbafba211355
    > Cq-Include-Trybots: luci.chromium.try:chromeos-amd64-generic-lacros-dbg,lacros-amd64-generic-rel-gtest,lacros-arm-generic-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455361
    > Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    > Reviewed-by: Katie Dektar <katie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1288176}
    
    Bug: b/260590502, b/333055971
    Change-Id: I06299501b64bfaf136844c82267afb200de82115
    Cq-Include-Trybots: luci.chromium.try:chromeos-amd64-generic-lacros-dbg,lacros-amd64-generic-rel-gtest,lacros-arm-generic-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5461379
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1288770}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier_test.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/magnifier/magnifier_test.js
@@ -220,10 +220,11 @@ TEST_F(
     });
 
 AX_TEST_F('MagnifierE2ETest', 'IgnoresRootNodeFocus', async function() {
+  await this.runWithLoadedTree('');
+
   const magnifier = accessibilityCommon.getMagnifierForTest();
   magnifier.setIsInitializingForTest(false);
 
-  await this.runWithLoadedTree('');
   chrome.accessibilityPrivate.onMagnifierBoundsChanged.addListener(
       (newBounds) => {
         throw new Error(
@@ -326,9 +327,6 @@ AX_TEST_F('MagnifierE2ETest', 'OnCaretBoundsChanged', async function() {
 
 TEST_F('MagnifierE2ETest', 'ScreenMagnifierFocusFollowingPref', function() {
   this.newCallback(async () => {
-    await importModule(
-        'Magnifier', '/accessibility_common/magnifier/magnifier.js');
-
     // Disable focus following for full screen magnifier, and verify prefs and
     // state.
     await this.setPref(Magnifier.Prefs.SCREEN_MAGNIFIER_FOCUS_FOLLOWING, false);
@@ -351,8 +349,6 @@ TEST_F(
     'MagnifierE2ETest', 'ScreenMagnifierSelectToSpeakFollowingPref',
     function() {
       this.newCallback(async () => {
-        await importModule(
-            'Magnifier', '/accessibility_common/magnifier/magnifier.js');
         // Disable select to speak following for full screen magnifier, and
         // verify prefs and state.
         await this.setPref(
@@ -379,8 +375,6 @@ TEST_F(
     'MagnifierE2ETest', 'FullscreenMagnifierDoesNotFollowStsWhenPrefOff',
     function() {
       this.newCallback(async () => {
-        await importModule(
-            'Magnifier', '/accessibility_common/magnifier/magnifier.js');
         // Disable select to speak following for full screen magnifier, and
         // verify prefs and state.
         await this.setPref(
@@ -407,8 +401,6 @@ TEST_F(
 TEST_F(
     'MagnifierE2ETest', 'FullscreenMagnifierFollowsStsWhenPrefOn', function() {
       this.newCallback(async () => {
-        await importModule(
-            'Magnifier', '/accessibility_common/magnifier/magnifier.js');
         // Disable select to speak following for full screen magnifier, and
         // verify prefs and state.
         await this.setPref(
