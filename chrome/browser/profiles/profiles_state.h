bdbc755dd5dea9881b8768ba2b6641f32114eccb
Tomasz Moniuszko <tmoniuszko@opera.com>
2024-04-17 10:35:31 +0000

    Remove redundant include from profiles_state.h
    Bug: 1270673
    Change-Id: Ib1270fea7ab77a135c70063e34d0514f77c29580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459662
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: Tomasz Moniuszko <tmoniuszko@opera.com>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288622}

--- chrome/browser/profiles/profiles_state.h
+++ chrome/browser/profiles/profiles_state.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
 #include <vector>
-
-#include "chrome/browser/profiles/avatar_menu.h"
 #endif
 
 struct AccountInfo;
