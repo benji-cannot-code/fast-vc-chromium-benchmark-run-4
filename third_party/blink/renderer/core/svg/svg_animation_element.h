e56d40390da0029249704801d35a190c42495904
Fredrik Söderquist <fs@opera.com>
2024-04-17 18:18:38 +0000

    Split CheckAnimationParameters() into multiple pieces
    Split out the value update into UpdateAnimationValues(). Fold
    CalculateValuesAnimation() into it. Move the (lazy) update of animation
    parameters into UpdateAnimationParameters() and keep parameter checking
    in CheckAnimationParameters(). Mark CheckAnimationParameters() const.
    Fold SetAnimationMode() into the only caller.
    
    Bug: 40774066
    Change-Id: Iac256b02e3ff4791c3efb4286ce1cc31bd83b478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458559
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288855}

--- third_party/blink/renderer/core/svg/svg_animation_element.h
+++ third_party/blink/renderer/core/svg/svg_animation_element.h
@@ -123,7 +123,10 @@ class CORE_EXPORT SVGAnimationElement : public SVGSMILElement {
   String ByValue() const;
   String FromValue() const;
 
-  bool CheckAnimationParameters();
+  bool UpdateAnimationParameters();
+  bool CheckAnimationParameters() const;
+  bool UpdateAnimationValues();
+
   virtual bool CalculateToAtEndOfDurationValue(
       const String& to_at_end_of_duration_string) = 0;
   virtual bool CalculateFromAndToValues(const String& from_string,
@@ -138,7 +141,6 @@ class CORE_EXPORT SVGAnimationElement : public SVGSMILElement {
     return -1.f;
   }
 
-  bool CalculateValuesAnimation();
   float CurrentValuesForValuesAnimation(float percent,
                                         String& from,
                                         String& to) const;
@@ -160,9 +162,6 @@ class CORE_EXPORT SVGAnimationElement : public SVGSMILElement {
   unsigned CalculateKeyTimesIndex(float percent) const;
 
   void SetCalcMode(const AtomicString&);
-  void SetAnimationMode(AnimationMode animation_mode) {
-    animation_mode_ = animation_mode;
-  }
 
   enum class AnimationValidity : unsigned char {
     kUnknown,
