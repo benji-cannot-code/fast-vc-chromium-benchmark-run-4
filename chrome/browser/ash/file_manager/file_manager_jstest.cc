bfaef3e39270c4e65f7a128f2edbea50f7c94162
Luciano Pacheco <lucmult@chromium.org>
2024-04-17 04:49:31 +0000

    Files app: Re-enable test on MSAN bots
    Tests were flaking because the action clearCachedEntries() is called
    via requestIdleCallback() which didn't have a chance to run on MSAN.
    
    Fix the flakiness by adding a timeout that forces the action to run at
    least in 50 milliseconds.
    
    Bug: b:334908136
    Change-Id: I60a8c187d4c73cd87d28fa985eaa8dec80fc9ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5460876
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Auto-Submit: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Commit-Queue: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288510}

--- chrome/browser/ash/file_manager/file_manager_jstest.cc
+++ chrome/browser/ash/file_manager/file_manager_jstest.cc
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
 #include "chrome/browser/ash/file_manager/file_manager_jstest_base.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "content/public/test/browser_test.h"
@@ -321,8 +320,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerJsTest, Selector) {
   RunTestURL("lib/selector_unittest.js");
 }
 
-// TODO(crbug.com/335318101): re-enable this test.
-IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DISABLED_DucksAllEntries) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DucksAllEntries) {
   RunTestURL("state/ducks/all_entries_unittest.js");
 }
 
@@ -362,12 +360,7 @@ IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DucksUiEntries) {
   RunTestURL("state/ducks/ui_entries_unittest.js");
 }
 
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_DucksVolumes DISABLED_DucksVolumes
-#else
-#define MAYBE_DucksVolumes DucksVolumes
-#endif  // defined(MEMORY_SANITIZER)
-IN_PROC_BROWSER_TEST_F(FileManagerJsTest, MAYBE_DucksVolumes) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DucksVolumes) {
   RunTestURL("state/ducks/volumes_unittest.js");
 }
 
--- ui/file_manager/base/js/test_harness.js
+++ ui/file_manager/base/js/test_harness.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Mark the test as fully loaded. The Browser Test reads this.
 window.__TEST_LOADED__ = false;
+window.IN_TEST = true;
 
 import 'chrome://webui-test/strings.m.js';
 
