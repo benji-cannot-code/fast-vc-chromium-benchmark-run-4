549cc65742a09ec4c0e2ee25690ad20e4627aab0
Fredrik Söderquist <fs@opera.com>
2024-04-17 18:16:14 +0000

    Signal decode error for SVGResourceDocumentContents with non-<svg> roots
    Bug: 40134477
    Change-Id: I4748162f3c21cda6bd6b5e00ab53fce12866901f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462359
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1288851}

--- third_party/blink/renderer/core/svg/svg_resource_document_content.cc
+++ third_party/blink/renderer/core/svg/svg_resource_document_content.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/loader/resource/svg_document_resource.h"
 #include "third_party/blink/renderer/core/svg/svg_resource_document_observer.h"
+#include "third_party/blink/renderer/core/svg/svg_svg_element.h"
 #include "third_party/blink/renderer/platform/loader/fetch/fetch_parameters.h"
 #include "third_party/blink/renderer/platform/loader/fetch/memory_cache.h"
 #include "third_party/blink/renderer/platform/supplementable.h"
@@ -173,10 +174,10 @@ void SVGResourceDocumentContent::UpdateStatus(ResourceStatus new_status) {
   status_ = new_status;
 }
 
-void SVGResourceDocumentContent::UpdateDocument(const String& content,
+bool SVGResourceDocumentContent::UpdateDocument(const String& content,
                                                 const KURL& request_url) {
   if (content.empty()) {
-    return;
+    return false;
   }
   url_ = request_url;
   document_ = XMLDocument::CreateSVG(DocumentInit::Create()
@@ -184,6 +185,11 @@ void SVGResourceDocumentContent::UpdateDocument(const String& content,
                                          .WithExecutionContext(context_)
                                          .WithAgent(*context_->GetAgent()));
   document_->SetContent(content);
+  // Drop documents with a non-SVG root element.
+  if (!IsA<SVGSVGElement>(document_->documentElement())) {
+    document_.Clear();
+  }
+  return document_;
 }
 
 void SVGResourceDocumentContent::ClearDocument() {
