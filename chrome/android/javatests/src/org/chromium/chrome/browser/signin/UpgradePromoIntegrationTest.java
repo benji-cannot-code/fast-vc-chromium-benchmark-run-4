f3171642bf3726bedf73dcd6bedba22e06bf06dd
Samar <samarchehade@google.com>
2024-04-17 08:41:31 +0000

    [UNOp2][Android] Disable failing testAddAccount
    Looks like it still doesn't work.
    
    Bug: b/333908211
    Change-Id: I1c4119e2c5338de5b3a05395484c139c35c0ba11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458127
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1288577}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/UpgradePromoIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/UpgradePromoIntegrationTest.java
@@ -36,6 +36,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -218,6 +219,7 @@ public class UpgradePromoIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "b/333908211")
     public void testAddAccount() {
         String secondAccountEmail = "jane.doe@gmail.com";
         mSigninTestRule.setResultForNextAddAccountFlow(Activity.RESULT_OK, secondAccountEmail);
