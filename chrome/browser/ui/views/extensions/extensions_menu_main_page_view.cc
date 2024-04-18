3df75bc474be5bca7688874c7fd3591ddf20bd7c
EmiliaPaz <emiliapaz@chromium.org>
2024-04-18 04:02:15 +0000

    [Extensions-c2s] Customize allow and dismiss request buttons
    Configure buttons of extension entry in message section to have kText
    style and same background as the container.
    
    Constraints:
      - Adding extension entries happen at construction and while the menu
        is opened.
      - Background color override takes a SkColor, which needs color
        provider
    
    Instead of setting the background SkColor at construction and after,
    we can add a new SetBgColorIdOverride() that takes a color id. That
    way, we only need to set it at construction.
    
    This allows us to:
      - Use SetBgColorIdOverride() for other calls
      - Deprecate SetBgColorOverride(), since it's only used in one place
        (which is complicated to migrate because value is read from
        value::Dict to SKColor).
    
    Screenshot: https://drive.google.com/file/d/11Ijn_cx1YPqJmoIKLw4zKvQSKafF9aW5/view?usp=sharing
    
    Bug: 40879945
    Change-Id: I3550d598f23a944f4319d7c263f6cc897795e54d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5441894
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Elaine Chien <elainechien@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289133}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/extension_id.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
+#include "ui/base/ui_base_types.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/gfx/vector_icon_types.h"
@@ -419,12 +420,18 @@ void MessageSection::AddOrUpdateExtension(const extensions::ExtensionId& id,
                                      views::MaximumFlexSizeRule::kUnbounded)),
                 views::Builder<views::MdTextButton>()
                     .SetCallback(base::BindRepeating(dismiss_callback_, id))
+                    .SetStyle(ui::ButtonStyle::kText)
+                    .SetBgColorIdOverride(
+                        kColorExtensionsMenuHighlightedBackground)
                     .SetText(l10n_util::GetStringUTF16(
                         IDS_EXTENSIONS_MENU_REQUESTS_ACCESS_SECTION_DISMISS_BUTTON_TEXT))
                     .SetTooltipText(l10n_util::GetStringUTF16(
                         IDS_EXTENSIONS_MENU_REQUESTS_ACCESS_SECTION_DISMISS_BUTTON_TOOLTIP)),
                 views::Builder<views::MdTextButton>()
                     .SetCallback(base::BindRepeating(allow_callback_, id))
+                    .SetStyle(ui::ButtonStyle::kText)
+                    .SetBgColorIdOverride(
+                        kColorExtensionsMenuHighlightedBackground)
                     .SetText(l10n_util::GetStringUTF16(
                         IDS_EXTENSIONS_MENU_REQUESTS_ACCESS_SECTION_ALLOW_BUTTON_TEXT))
                     .SetTooltipText(l10n_util::GetStringUTF16(
