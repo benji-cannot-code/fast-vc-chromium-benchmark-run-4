b6cabd677769aca67fb9047cc11a7614d7a2200c
Feras Aldahlawi <feras@chromium.org>
2024-04-18 21:52:15 +0000

    [media] Convert std::unique_ptr<T[]> to HeapArray<T> in media/base/...
    See
    https://docs.google.com/document/d/1YsPR8GoN8VTP1ABKCISaQkuBif1Cn80cTxTjsM8QT4s
    for an explanation on why this is favorable.
    
    Bug: b/326459492, b/326459327, b/326459127, b/326459386, b/326459268
    Change-Id: I88db2042b67ff3706c5ee2c122c18dcd80682cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5450966
    Commit-Queue: Feras Aldahlawi <feras@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289603}

--- media/base/user_input_monitor_win.cc
+++ media/base/user_input_monitor_win.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stddef.h>
 #include <stdint.h>
+
 #include <memory>
 
+#include "base/containers/heap_array.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -257,10 +259,10 @@ LRESULT UserInputMonitorWinCore::OnInput(HRAWINPUT input_handle) {
   DCHECK_EQ(0u, result);
 
   // Retrieve the input record itself.
-  std::unique_ptr<uint8_t[]> buffer(new uint8_t[size]);
-  RAWINPUT* input = reinterpret_cast<RAWINPUT*>(buffer.get());
-  result = GetRawInputData(
-      input_handle, RID_INPUT, buffer.get(), &size, sizeof(RAWINPUTHEADER));
+  auto buffer = base::HeapArray<uint8_t>::Uninit(size);
+  RAWINPUT* input = reinterpret_cast<RAWINPUT*>(buffer.data());
+  result = GetRawInputData(input_handle, RID_INPUT, buffer.data(), &size,
+                           sizeof(RAWINPUTHEADER));
   if (result == static_cast<UINT>(-1)) {
     PLOG(ERROR) << "GetRawInputData() failed";
     return 0;
