dc668b1a33a6f1403ce6fb06081de55deb24e588
Alvin Lam <alvingo@google.com>
2024-04-18 19:48:08 +0000

    [3PCD Reminders] Clean up IPH reminder for Clank
    Bug: b/332761678
    Change-Id: I39725aa0b966e8536f0b28d82feefc51439d23fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458733
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Alvin Lam <alvingo@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289516}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/status/StatusMediator.java
@@ -22,7 +22,6 @@ import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.supplier.Supplier;
-import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.merchant_viewer.MerchantTrustSignalsCoordinator;
 import org.chromium.chrome.browser.omnibox.LocationBarDataProvider;
 import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
@@ -46,8 +45,6 @@ import org.chromium.components.content_settings.ContentSettingsType;
 import org.chromium.components.content_settings.CookieBlocking3pcdStatus;
 import org.chromium.components.content_settings.CookieControlsBridge;
 import org.chromium.components.content_settings.CookieControlsObserver;
-import org.chromium.components.feature_engagement.FeatureConstants;
-import org.chromium.components.feature_engagement.Tracker;
 import org.chromium.components.page_info.PageInfoController;
 import org.chromium.components.permissions.PermissionDialogController;
 import org.chromium.components.search_engines.TemplateUrlService;
@@ -855,14 +852,7 @@ public class StatusMediator
                 return;
             }
 
-            Tracker tracker = TrackerFactory.getTrackerForProfile(profile);
-            if (!tracker.wouldTriggerHelpUI(FeatureConstants.COOKIE_CONTROLS_3PCD_FEATURE)) return;
-
-            animateCookieControlsIcon(
-                    () ->
-                            mPageInfoIPHController.showCookieControlsReminderIPH(
-                                    getIPHTimeout(),
-                                    R.string.cookie_controls_reminder_iph_message));
+            animateCookieControlsIcon(() -> {});
         }
     }
 
