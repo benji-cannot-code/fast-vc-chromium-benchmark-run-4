7d8e7f7f74191b9e5ae918a79b98c273e2d26c56
Nate Chapin <japhet@chromium.org>
2024-04-18 17:03:55 +0000

    Use typed promises/resolvers for ReadableStream and related classes, attempt #3
    This converts IDL-exposed promises in ReadableStream,
    ReadableStreamBYOBReader, ReadableStreamDefaultReader, and
    ReadableStreamGenericReader to use typed ScriptPromiseResolver
    instead of StreamPromiseResolver and to return typed
    ScriptPromises.
    
    Bug: 329702363
    Change-Id: I8ad1af1a7c9c909d711881ce7621c6c9fac58931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5429731
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289397}

--- third_party/blink/renderer/core/streams/pipe_to_engine.h
+++ third_party/blink/renderer/core/streams/pipe_to_engine.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_PIPE_TO_ENGINE_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_STREAMS_PIPE_TO_ENGINE_H_
 
+#include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
+#include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -20,7 +22,6 @@ class PipeOptions;
 class ReadableStream;
 class ReadableStreamDefaultReader;
 class ScriptState;
-class StreamPromiseResolver;
 class WritableStream;
 class WritableStreamDefaultWriter;
 
@@ -42,9 +43,9 @@ class PipeToEngine final : public GarbageCollected<PipeToEngine> {
   PipeToEngine& operator=(const PipeToEngine&) = delete;
 
   // This is the main entrypoint for ReadableStreamPipeTo().
-  ScriptPromiseUntyped Start(ReadableStream* readable,
-                             WritableStream* destination,
-                             ExceptionState&);
+  ScriptPromise<IDLUndefined> Start(ReadableStream* readable,
+                                    WritableStream* destination,
+                                    ExceptionState&);
 
   void Trace(Visitor* visitor) const {
     visitor->Trace(script_state_);
@@ -189,7 +190,7 @@ class PipeToEngine final : public GarbageCollected<PipeToEngine> {
   Member<PipeOptions> pipe_options_;
   Member<ReadableStreamDefaultReader> reader_;
   Member<WritableStreamDefaultWriter> writer_;
-  Member<StreamPromiseResolver> promise_;
+  Member<ScriptPromiseResolver<IDLUndefined>> promise_;
   Member<AbortSignal::AlgorithmHandle> abort_handle_;
   TraceWrapperV8Reference<v8::Promise> last_write_;
   Action shutdown_action_;
