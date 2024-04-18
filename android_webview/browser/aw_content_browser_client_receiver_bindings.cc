31d51a714f14ccc23f323cd85c5b583e6db81ab2
Ashley Newson <ashleynewson@chromium.org>
2024-04-18 16:31:54 +0000

    Restrict AWMI to trustworthy HTTP/HTTPS origins
    - Only expose the API on SecureContexts.
    - Only allow calling the API from potentially trustworthy HTTP/HTTPS
      origins. Render will verify this and throw a helpful error if on an
      inappropriate origin. The browser enforces this condition before
      binding the Mojo receiver.
    - Enforce in the browser-side implementation that the API only works on
      true HTTPS origins (without supporting loadDataWithBaseURL) by
      checking that the last committed origin and URL agree on scheme.
    
    Bug: 330151742
    Change-Id: I6abde47b0cb0edfe06c0e0a2cfcb32ffb5c7a194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455111
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289369}

--- android_webview/browser/aw_content_browser_client_receiver_bindings.cc
+++ android_webview/browser/aw_content_browser_client_receiver_bindings.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/mojo/buildflags.h"
 #include "mojo/public/cpp/bindings/binder_map.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "services/network/public/cpp/is_potentially_trustworthy.h"
 #include "services/service_manager/public/cpp/binder_registry.h"
 #include "services/service_manager/public/cpp/interface_provider.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_registry.h"
@@ -151,6 +152,25 @@ void ForwardToJavaFrame(content::RenderFrameHost* render_frame_host,
   render_frame_host->GetJavaInterfaces()->GetInterface(std::move(receiver));
 }
 
+void BindMediaIntegrityServiceReceiver(
+    content::RenderFrameHost* render_frame_host,
+    mojo::PendingReceiver<blink::mojom::WebViewMediaIntegrityService>
+        receiver) {
+  const url::Origin& origin = render_frame_host->GetLastCommittedOrigin();
+  // Note that this particular check respects the origin of the base URL
+  // supplied by loadDataWithBaseURL.
+  if ((origin.scheme() != url::kHttpScheme &&
+       origin.scheme() != url::kHttpsScheme) ||
+      !network::IsOriginPotentiallyTrustworthy(origin)) {
+    mojo::ReportBadMessage(
+        "Attempted to access WebView Media Integrity service for a "
+        "non-trustworthy or non-HTTP/HTTPS origin.");
+    return;
+  };
+  ForwardToJavaFrame<blink::mojom::WebViewMediaIntegrityService>(
+      render_frame_host, std::move(receiver));
+}
+
 }  // anonymous namespace
 
 void AwContentBrowserClient::BindMediaServiceReceiver(
@@ -260,8 +280,8 @@ void AwContentBrowserClient::RegisterBrowserInterfaceBindersForFrame(
   if (base::FeatureList::IsEnabled(
           features::kWebViewMediaIntegrityApiBlinkExtension) &&
       !base::FeatureList::IsEnabled(features::kWebViewMediaIntegrityApi)) {
-    map->Add<blink::mojom::WebViewMediaIntegrityService>(base::BindRepeating(
-        &ForwardToJavaFrame<blink::mojom::WebViewMediaIntegrityService>));
+    map->Add<blink::mojom::WebViewMediaIntegrityService>(
+        base::BindRepeating(&BindMediaIntegrityServiceReceiver));
   }
 }
 
