15e1c17d71565fb1b84abe1045a00cd3afe78eae
dljames <dljames@chromium.org>
2024-04-18 18:04:31 +0000

    [ TabRestore ] Use TabRestoreTypes in LiveTabContext
    Because of the changes in this cl: https://chromium-review.googlesource.com/c/chromium/src/+/5449978, we are able to use the TabRestoreTypes directly in LiveTabContext. This reduces the parameters we need to pass to functions such as AddRestoredTab and ReplaceRestoredTab.
    
    We also no longer need to independently update the parameters if an addition is made to sessions::tab_restore::Tab.
    
    Change-Id: I46594b45a2dab5e1ac8eacea7b44ea7b5552a209
    Bug: 335281443
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458785
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289448}

--- chrome/browser/ui/android/tab_model/android_live_tab_context_wrapper.cc
+++ chrome/browser/ui/android/tab_model/android_live_tab_context_wrapper.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "components/sessions/content/content_live_tab.h"
+#include "components/sessions/core/tab_restore_types.h"
 
 AndroidLiveTabContextCloseWrapper::AndroidLiveTabContextCloseWrapper(
     TabModel* tab_model,
@@ -91,28 +92,17 @@ void AndroidLiveTabContextRestoreWrapper::SetVisualDataForGroup(
 }
 
 sessions::LiveTab* AndroidLiveTabContextRestoreWrapper::AddRestoredTab(
-    const std::vector<sessions::SerializedNavigationEntry>& navigations,
+    const sessions::tab_restore::Tab& tab,
     int tab_index,
-    int selected_navigation,
-    const std::string& extension_app_id,
-    std::optional<tab_groups::TabGroupId> group,
-    const tab_groups::TabGroupVisualData& group_visual_data,
-    bool select,
-    bool pin,
-    const sessions::tab_restore::PlatformSpecificTabData* storage_namespace,
-    const sessions::SerializedUserAgentOverride& user_agent_override,
-    const std::map<std::string, std::string>& extra_data,
-    const SessionID* tab_id) {
-  auto* live_tab = AndroidLiveTabContext::AddRestoredTab(
-      navigations, tab_index, selected_navigation, extension_app_id, group,
-      group_visual_data, select, pin, storage_namespace, user_agent_override,
-      extra_data, tab_id);
-  if (group) {
-    TabAndroid* tab = TabAndroid::FromWebContents(
+    bool select) {
+  auto* live_tab =
+      AndroidLiveTabContext::AddRestoredTab(tab, tab_index, select);
+  if (tab.group) {
+    TabAndroid* restored_tab = TabAndroid::FromWebContents(
         static_cast<sessions::ContentLiveTab*>(live_tab)->web_contents());
-    DCHECK(tab);
-    tab_groups_[*group].visual_data = group_visual_data;
-    tab_groups_[*group].tab_ids.push_back(tab->GetAndroidId());
+    DCHECK(restored_tab);
+    tab_groups_[*tab.group].visual_data = *tab.group_visual_data;
+    tab_groups_[*tab.group].tab_ids.push_back(restored_tab->GetAndroidId());
   }
   return live_tab;
 }
