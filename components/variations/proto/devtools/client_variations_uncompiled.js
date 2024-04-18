ace68802fc879095b98b94c0437f94507fdbce0b
Ilya Sherman <isherman@chromium.org>
2024-04-18 14:02:04 +0000

    Add more details to the client_variations.proto inline docs
    The documentation in this file was pretty sparse. Provide some more context, and
    regularize terminology with other parts of the codebase.
    
    R=asvitkine@chromium.org
    
    Bug: none
    Change-Id: I224ae342e6d9831c81aef8c0b88d7d2ce960bd62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464602
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Auto-Submit: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289285}

--- components/variations/proto/devtools/client_variations_uncompiled.js
+++ components/variations/proto/devtools/client_variations_uncompiled.js
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const ClientVariations = goog.require('proto.variations.ClientVariations');
 
-const VARIATION_IDS_COMMENT = 'Active client experiment variation IDs.';
+const VARIATION_IDS_COMMENT =
+      'Active Google-visible variation IDs on this client. These are ' +
+      'reported for analysis, but do not directly affect any server-side ' +
+      'behavior.';
 const TRIGGER_VARIATION_IDS_COMMENT =
-    'Active client experiment variation IDs that trigger server-side behavior.';
+      'Active Google-visible variation IDs on this client that trigger ' +
+      'server-side behavior. These are reported for analysis *and* directly ' +
+      'affect server-side behavior.';
 
 /**
  * Parses a serialized ClientVariations proto into a human-readable format.
