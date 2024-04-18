c7b801a2afac0c7cbc70aa14aebfd9b4568adb6a
Ted Choc <tedchoc@chromium.org>
2024-04-18 20:31:47 +0000

    Remove unused Profile provider in ChromeAppModule.
    Bug: 40254448
    Change-Id: Ia6fa1003d3eedf231fa9205f038c16d6b5a45eb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466225
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289549}

--- chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppModule.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dependency_injection/ChromeAppModule.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.dependency_injection;
 
 import static org.chromium.chrome.browser.dependency_injection.ChromeCommonQualifiers.APP_CONTEXT;
-import static org.chromium.chrome.browser.dependency_injection.ChromeCommonQualifiers.LAST_USED_REGULAR_PROFILE;
 
 import android.content.Context;
 
@@ -25,8 +24,6 @@ import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
 import org.chromium.chrome.browser.night_mode.SystemNightModeMonitor;
 import org.chromium.chrome.browser.notifications.channels.SiteChannelsManager;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
-import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.browser.tabmodel.AsyncTabParamsManager;
 import org.chromium.chrome.browser.webapps.WebappRegistry;
 
@@ -39,12 +36,6 @@ public class ChromeAppModule {
     /** See {@link ModuleFactoryOverrides} */
     public interface Factory {
         ChromeAppModule create();
-    }
-
-    @Provides
-    @Named(LAST_USED_REGULAR_PROFILE)
-    public Profile provideLastUsedRegularProfile() {
-        return ProfileManager.getLastUsedRegularProfile();
     }
 
     @Provides
