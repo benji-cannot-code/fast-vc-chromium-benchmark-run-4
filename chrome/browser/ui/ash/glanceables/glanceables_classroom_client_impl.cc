d323543d278d44aa4cd33283bc4abf3893f8f2bf
Artsiom Mitrokhin <amitrokhin@google.com>
2024-04-18 16:54:33 +0000

    Update traffic annotations for calendar, classroom, tasks integrations
    Bug: b/319251265
    Change-Id: I6620885ec9de5b7588592e32f1229f80dcef08f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463019
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289390}

--- chrome/browser/ui/ash/glanceables/glanceables_classroom_client_impl.cc
+++ chrome/browser/ui/ash/glanceables/glanceables_classroom_client_impl.cc
@@ -91,8 +91,8 @@ constexpr net::NetworkTrafficAnnotationTag kTrafficAnnotationTag =
           cookies_allowed: NO
           setting: "This feature cannot be disabled in settings"
           chrome_policy {
-            GlanceablesEnabled {
-              GlanceablesEnabled: false
+            ContextualGoogleIntegrationsEnabled {
+              ContextualGoogleIntegrationsEnabled: false
             }
           }
         }
