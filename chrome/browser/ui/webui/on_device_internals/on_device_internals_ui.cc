4be8008c0ec83f1351551ab2c31e5668e7f0a154
Yi Chou <yich@google.com>
2024-04-18 23:31:42 +0000

    on_device_model: Use UUID to load model in testing page
    We need an automatic integration test to verify the ODML inference
    stack behavior on ChromeOS.
    
    To accomplish that, we can leverage the existing
    chrome://on-device-internals page.
    
    BUG=b:335518557
    TEST=Verify the behavior via crrev.com/i/7157216
    
    Change-Id: Ie100f5871e2ca6aa7d2ca1e02ef0548bb2f9b0a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5406657
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Yi Chou <yich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289672}

--- chrome/browser/ui/webui/on_device_internals/on_device_internals_ui.cc
+++ chrome/browser/ui/webui/on_device_internals/on_device_internals_ui.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
-
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
 on_device_model::ModelAssets LoadModelAssets(const base::FilePath& model_path) {
   // This WebUI currently provides no way to dynamically configure the expected
   // output dimension of the TS model. Since the model is in flux and its output
@@ -41,7 +41,7 @@ on_device_model::ModelAssets LoadModelAssets(const base::FilePath& model_path) {
   }
   return on_device_model::LoadModelAssets(model_paths);
 }
-
+#endif
 }  // namespace
 
 OnDeviceInternalsUI::OnDeviceInternalsUI(content::WebUI* web_ui)
@@ -68,6 +68,16 @@ void OnDeviceInternalsUI::LoadModel(
     const base::FilePath& model_path,
     mojo::PendingReceiver<on_device_model::mojom::OnDeviceModel> model,
     LoadModelCallback callback) {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // We treat the file path as a UUID on ChromeOS.
+  base::Uuid uuid = base::Uuid::ParseLowercase(model_path.value());
+  if (!uuid.is_valid()) {
+    std::move(callback).Run(
+        on_device_model::mojom::LoadModelResult::kFailedToLoadLibrary);
+    return;
+  }
+  GetService().LoadPlatformModel(uuid, std::move(model), std::move(callback));
+#else
   // Warm the service while assets load in the background.
   std::ignore = GetService();
   base::ThreadPool::PostTaskAndReplyWithResult(
@@ -76,6 +86,7 @@ void OnDeviceInternalsUI::LoadModel(
       base::BindOnce(&OnDeviceInternalsUI::OnModelAssetsLoaded,
                      weak_ptr_factory_.GetWeakPtr(), std::move(model),
                      std::move(callback)));
+#endif
 }
 
 on_device_model::mojom::OnDeviceModelService&
