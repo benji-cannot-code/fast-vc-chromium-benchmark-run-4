754a47d7bbd88ac3d3b263736def726b4b6120cb
Carlos IL <carlosil@chromium.org>
2024-04-18 18:52:22 +0000

    Make SCT hashdance tests stop depending on Argon2023 being in the log list
    This adds a fake log list update to the test setup so Argon2023 is
    included.
    
    Bug: 335630147
    Change-Id: I40125e81079132a12eb019173c1a8f57a0dd073f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463907
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Auto-Submit: Carlos IL <carlosil@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289475}

--- chrome/browser/ssl/sct_reporting_service_browsertest.cc
+++ chrome/browser/ssl/sct_reporting_service_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ssl/sct_reporting_service.h"
+
 #include <memory>
 #include <tuple>
 
@@ -24,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ssl/cert_verifier_browser_test.h"
-#include "chrome/browser/ssl/sct_reporting_service.h"
 #include "chrome/browser/ssl/sct_reporting_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_constants.h"
@@ -60,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/traffic_annotation/network_traffic_annotation_test_helper.h"
 #include "services/network/public/cpp/features.h"
+#include "services/network/public/mojom/ct_log_info.mojom.h"
 #include "services/network/public/mojom/network_service.mojom.h"
 #include "services/network/public/proto/sct_audit_report.pb.h"
 #include "services/network/test/test_url_loader_factory.h"
@@ -869,6 +871,18 @@ class SCTHashdanceBrowserTest : public SCTReportingServiceBrowserTest {
     mock_cert_verifier()->AddResultForCertAndHost(
         https_server()->GetCertificate().get(), "hashdance.test", verify_result,
         net::OK);
+    network::mojom::CTLogInfoPtr log(std::in_place);
+    std::string googleLogIdAsString(
+        reinterpret_cast<const char*>(kTestGoogleLogId),
+        sizeof(kTestGoogleLogId));
+    log->id = googleLogIdAsString;
+    log->mmd = base::Seconds(86400);
+    std::vector<network::mojom::CTLogInfoPtr> log_list;
+    log_list.emplace_back(std::move(log));
+    base::RunLoop run_loop;
+    content::GetNetworkService()->UpdateCtLogList(std::move(log_list),
+                                                  run_loop.QuitClosure());
+    run_loop.Run();
   }
 
  private:
