36092b25ef0bded5a2821ec48fd11d814b151c4b
Yu Zhong <yuzhong@google.com>
2024-04-18 20:46:03 +0000

    [Read aloud] Add language menu dialog into toolbar navigation
    Stage 1 language menu:
    In Reading mode users can select from a list of voices for TTS, we are adding a dialog to allow users to turn on/off certain languages per their preferences.
    This dialog is opened from bottom of voice menu.
    Currently it shows all languages of installed voices and supports search in the language list.
    Follow up CLs will save the preferences and use them to filter voice menu list.
    https://screenshot.googleplex.com/6QKD6UwAw69grVG
    
    Bug: 333143002
    Fixed: 333143002
    Change-Id: Ie9d748598f8e8a315d754958daf470966cf90618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5416690
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Yu Zhong <yuzhong@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289560}

--- chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
+++ chrome/test/data/webui/side_panel/read_anything/read_anything_browsertest.cc
@@ -88,6 +88,11 @@ IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, AppReceivesToolbarChanges) {
       "mocha.run()");
 }
 
+IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, LanguageMenu) {
+  RunSidePanelTest("side_panel/read_anything/language_menu_test.js",
+                   "mocha.run()");
+}
+
 IN_PROC_BROWSER_TEST_F(ReadAnythingMochaTest, LinksToggle) {
   RunSidePanelTest("side_panel/read_anything/links_toggle_button_test.js",
                    "mocha.run()");
