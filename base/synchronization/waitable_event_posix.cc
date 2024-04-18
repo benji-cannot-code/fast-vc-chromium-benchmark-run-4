a81e7dcdc4f0b4996df47362d187dee2b7a98ea5
Bartek Nowierski <bartekn@chromium.org>
2024-04-18 13:49:58 +0000

    [BRP] Initialize pointers un-rewritten from raw_ptr with nullptr
    This is a follow up to crrev.com/c/5446500, crrev.com/c/5444919 and crrev.com/c/5383151, which unrewrote a bunch of pointers. Unlike raw_ptr, raw pointers aren't default-initialized. Initialize them to avoid subtle behavior differences. Plus it's a good practice anyway.
    
    Change-Id: Ib58325ad4a06ae313f431537ffde1744c11ee05d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464681
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289281}

--- base/synchronization/waitable_event_posix.cc
+++ base/synchronization/waitable_event_posix.cc
@@ -150,7 +150,7 @@ class SyncWaiter : public WaitableEvent::Waiter {
 
  private:
   bool fired_;
-  WaitableEvent* signaling_event_;  // The WaitableEvent which woke us
+  WaitableEvent* signaling_event_ = nullptr;  // The WaitableEvent which woke us
   base::Lock lock_;
   base::ConditionVariable cv_;
 };
