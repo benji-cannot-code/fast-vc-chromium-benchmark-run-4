b59592593a647ee9a3e8d41cb5633eceef1cafcd
Bao-Duy Tran <tranbaoduy@chromium.org>
2024-04-18 04:36:21 +0000

    Dismantle obsolete ash::features::kMultilingualTyping flag.
    Bug: b:308726198
    Change-Id: I4c31ca76c1a95f0e9ac0922644bbda6c8737bb97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5325919
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289137}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4029,9 +4029,6 @@ extern const char kMultiCalendarSupportDescription[];
 extern const char kMultiZoneRgbKeyboardName[];
 extern const char kMultiZoneRgbKeyboardDescription[];
 
-extern const char kMultilingualTypingName[];
-extern const char kMultilingualTypingDescription[];
-
 extern const char kNearbySharingSelfShareName[];
 extern const char kNearbySharingSelfShareDescription[];
 
