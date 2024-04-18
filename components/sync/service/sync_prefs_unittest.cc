01239d27fd45d7a1fef22a3b9489833c54aa9c36
Jood Hajeer <jood@google.com>
2024-04-18 17:08:57 +0000

    [sync] Remove Set/Get EncryptionBootstrapToken from SyncPrefs
    Set/Get EncryptionBootstrapToken in SyncPrefs are only used in
    SyncPrefsMigrationTest after launching
    SyncRememberCustomPassphraseAfterSignout. It is safe now to remove these
    methods and have the pref directly accessed in tests.
    
    Bug: 325201878
    Change-Id: I74b10a4fc652e191d71a77745dc4173549aca89c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465643
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289403}

--- components/sync/service/sync_prefs_unittest.cc
+++ components/sync/service/sync_prefs_unittest.cc
@@ -1002,20 +1002,27 @@ TEST_F(SyncPrefsMigrationTest, LeavesPasswordsAloneIfDisabledByPolicy) {
 TEST_F(SyncPrefsMigrationTest, NoPassphraseMigrationForSignoutUsers) {
   SyncPrefs prefs(&pref_service_);
   // Passphrase is not set.
-  ASSERT_TRUE(prefs.GetEncryptionBootstrapToken().empty());
+  ASSERT_TRUE(
+      pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken)
+          .empty());
 
   auto gaia_id_hash_empty = signin::GaiaIdHash::FromGaiaId("");
   prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_empty);
-  EXPECT_TRUE(prefs.GetEncryptionBootstrapToken().empty());
+  EXPECT_TRUE(
+      pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken)
+          .empty());
   EXPECT_TRUE(
       prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_empty).empty());
 }
 
 TEST_F(SyncPrefsMigrationTest, PassphraseMigrationDone) {
   SyncPrefs prefs(&pref_service_);
-  prefs.SetEncryptionBootstrapToken("token");
+  pref_service_.SetString(prefs::internal::kSyncEncryptionBootstrapToken,
+                          "token");
   prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_);
-  EXPECT_EQ(prefs.GetEncryptionBootstrapToken(), "token");
+  EXPECT_EQ(
+      pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken),
+      "token");
   EXPECT_EQ(prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_),
             "token");
   signin::GaiaIdHash gaia_id_hash_2 =
@@ -1026,17 +1033,23 @@ TEST_F(SyncPrefsMigrationTest, PassphraseMigrationDone) {
 
 TEST_F(SyncPrefsMigrationTest, PassphraseMigrationOnlyOnce) {
   SyncPrefs prefs(&pref_service_);
-  prefs.SetEncryptionBootstrapToken("token");
+  pref_service_.SetString(prefs::internal::kSyncEncryptionBootstrapToken,
+                          "token");
   prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_);
-  EXPECT_EQ(prefs.GetEncryptionBootstrapToken(), "token");
+  EXPECT_EQ(
+      pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken),
+      "token");
   EXPECT_EQ(prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_),
             "token");
 
   // Force old pref to change for testing purposes.
-  prefs.SetEncryptionBootstrapToken("token2");
+  pref_service_.SetString(prefs::internal::kSyncEncryptionBootstrapToken,
+                          "token2");
   prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_);
   // The migration should not run again.
-  EXPECT_EQ(prefs.GetEncryptionBootstrapToken(), "token2");
+  EXPECT_EQ(
+      pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken),
+      "token2");
   EXPECT_EQ(prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_),
             "token");
 }
@@ -1044,13 +1057,17 @@ TEST_F(SyncPrefsMigrationTest, PassphraseMigrationOnlyOnce) {
 TEST_F(SyncPrefsMigrationTest, PassphraseMigrationOnlyOnceWithBrowserRestart) {
   {
     SyncPrefs prefs(&pref_service_);
-    prefs.SetEncryptionBootstrapToken("token");
+    pref_service_.SetString(prefs::internal::kSyncEncryptionBootstrapToken,
+                            "token");
     prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_);
-    EXPECT_EQ(prefs.GetEncryptionBootstrapToken(), "token");
+    EXPECT_EQ(
+        pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken),
+        "token");
     EXPECT_EQ(prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_),
               "token");
     // Force old pref to change for testing purposes.
-    prefs.SetEncryptionBootstrapToken("token2");
+    pref_service_.SetString(prefs::internal::kSyncEncryptionBootstrapToken,
+                            "token2");
   }
 
   // The browser is restarted.
@@ -1058,7 +1075,9 @@ TEST_F(SyncPrefsMigrationTest, PassphraseMigrationOnlyOnceWithBrowserRestart) {
     SyncPrefs prefs(&pref_service_);
     prefs.MaybeMigrateCustomPassphrasePref(gaia_id_hash_);
     // No migration should run.
-    EXPECT_EQ(prefs.GetEncryptionBootstrapToken(), "token2");
+    EXPECT_EQ(
+        pref_service_.GetString(prefs::internal::kSyncEncryptionBootstrapToken),
+        "token2");
     EXPECT_EQ(prefs.GetEncryptionBootstrapTokenForAccount(gaia_id_hash_),
               "token");
   }
