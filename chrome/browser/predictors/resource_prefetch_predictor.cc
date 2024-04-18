eb664efd00c6c1a550474a167cf76752e5004ef7
Yoichi Osato <yoichio@chromium.org>
2024-04-18 23:43:02 +0000

    [LCPP] Refactor GetLCPPDatabaseKey and add tests
    This CL refactors GetLCPPDatabaseKey to GetFirstLevelPath and creates
    small GetLCPPDatabaseKey in resource_prefetch_predictor.cc using
    the function while considering flags.
    This is preparation for new implementation of Multiple Key where
    new GetLCPPDatabaseKey would consider another flag.
    
    This CL also adds a test about max_hosts_to_track_for_lcpp.
    
    Design doc(Google internal): https://docs.google.com/document/d/1hHckrLMvM_lQiMARgNn94HqkP3c1wfkr0_dWQaxWLsY/edit?pli=1&tab=t.0
    
    Bug: 328487899
    Change-Id: I1ef0d470922162985252874534b8032898154270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456651
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289675}

--- chrome/browser/predictors/resource_prefetch_predictor.cc
+++ chrome/browser/predictors/resource_prefetch_predictor.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "net/base/url_util.h"
 #include "services/network/public/mojom/fetch_api.mojom.h"
+#include "third_party/blink/public/common/features.h"
 #include "url/origin.h"
 
 using content::BrowserThread;
@@ -69,6 +70,23 @@ GURL CreateRedirectURL(const std::string& scheme,
                        const std::string& host,
                        std::uint16_t port) {
   return GURL(scheme + "://" + host + ":" + base::NumberToString(port));
+}
+
+std::string GetLCPPDatabaseKey(const GURL& url) {
+  CHECK(IsURLValidForLcpp(url));
+
+  if (!base::FeatureList::IsEnabled(blink::features::kLCPPMultipleKey)) {
+    return url.host();
+  }
+  const std::string first_level_path = GetFirstLevelPath(url);
+  const size_t key_length = url.host().length() + first_level_path.length();
+  if (key_length > ResourcePrefetchPredictorTables::kMaxStringLength) {
+    // The key must not be longer than `kMaxStringLength`.
+    // Note that we confirmed that url.host() is less than the limit in
+    // `IsURLValidForLcpp()`.
+    return url.host();
+  }
+  return url.host() + first_level_path;
 }
 
 }  // namespace
