a66eb11cd972e482748b40ca05e5a571803f2a98
Chuong Ho <hdchuong@google.com>
2024-04-18 22:46:29 +0000

    editor: clean up unused function
    Bug:NONE
    
    Change-Id: I9fbf517fc368da619319ae83cd78d64477dce331
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5432119
    Auto-Submit: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289634}

--- chrome/browser/ash/input_method/editor_switch.h
+++ chrome/browser/ash/input_method/editor_switch.h
@@ -48,8 +48,6 @@ class EditorSwitch {
 
   void OnTextSelectionLengthChanged(size_t new_length);
 
-  void SetProfile(Profile* profile);
-
   EditorMode GetEditorMode() const;
 
   EditorOpportunityMode GetEditorOpportunityMode() const;
