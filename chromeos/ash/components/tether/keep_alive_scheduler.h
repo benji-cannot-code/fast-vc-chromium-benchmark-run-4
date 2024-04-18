ca365f419684d85fc9451d5393d1bbe8f27ad415
Joe Antonetti <joeantonetti@google.com>
2024-04-18 16:23:49 +0000

    [Tether] Remove RemoteDeviceRef from KeepAliveOperation::Observer
    This change continues to remove RemoteDeviceRef from the Tether code
    base by refactoring KeepAliveOperation's observer to no longer
    return a RemoteDeviceRef. As KeepAliveOperation can only run for
    a single device, this is not an issue.
    
    Test = Updated unit tests.
    
    Change-Id: Ia77d7c83487443b60edc0f993de14325f3da10d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5361537
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289364}

--- chromeos/ash/components/tether/keep_alive_scheduler.h
+++ chromeos/ash/components/tether/keep_alive_scheduler.h
@@ -54,7 +54,6 @@ class KeepAliveScheduler : public ActiveHost::Observer,
 
   // KeepAliveOperation::Observer:
   void OnOperationFinished(
-      multidevice::RemoteDeviceRef remote_device,
       std::unique_ptr<DeviceStatus> device_status) override;
 
  private:
