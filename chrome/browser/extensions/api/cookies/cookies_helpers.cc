00a0a09ea568a61dd96930c9d6e8711b40348bf2
Ari Chivukula <arichiv@chromium.org>
2024-04-18 19:52:45 +0000

    [CookiePartitionKey] Count well formatted extensions keys
    The extensions cookie API requires partitionKeys to be coercible, but
    doesn't require them to be well formatted. Let's count how many that are
    coercible are or aren't well formatted. Depending on the results, it may
    be possible to get more strict on parsing of the partition key passed
    in.
    
    Bug: 41486025
    Change-Id: I90e368d725ed530f9f18b481206d510a9e23b8c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464208
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289520}

--- chrome/browser/extensions/api/cookies/cookies_helpers.cc
+++ chrome/browser/extensions/api/cookies/cookies_helpers.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -242,6 +243,12 @@ bool ValidateCookieApiPartitionKey(
       return false;
     }
     net_partition_key = key.value();
+    // Record 'well formatted' uma here so that we count only coercible
+    // partition keys.
+    base::UmaHistogramBoolean(
+        "Extensions.CookieAPIPartitionKeyWellFormatted",
+        net::SchemefulSite::Deserialize(partition_key->top_level_site.value())
+                .Serialize() == partition_key->top_level_site.value());
   }
   return true;
 }
