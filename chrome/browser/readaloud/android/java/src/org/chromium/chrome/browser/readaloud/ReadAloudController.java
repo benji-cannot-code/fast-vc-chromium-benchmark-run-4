c256998d862db47849ac0c43d9a64afe0d4ef04a
Andrea Gomez <andreaxg@google.com>
2024-04-18 22:15:32 +0000

    Use mProfileSupplier and remove mProfile from ReadAloudController
    Simplifying the code
    
    
    Bug: 335680343
    Change-Id: I489baf7f100ec911b9bc1955fde0cde37f6a61bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464137
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289615}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudController.java
@@ -129,9 +129,6 @@ public class ReadAloudController
     // Playback for voice previews
     @Nullable private Playback mVoicePreviewPlayback;
 
-    // TODO(b/322052505): Remove this and just observe mProfileSupplier.
-    @Nullable private Profile mProfile;
-
     private boolean mOnUserLeaveHint;
     private boolean mRestoringPlayer;
 
@@ -427,13 +424,12 @@ public class ReadAloudController
 
     @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
     public void onProfileAvailable(Profile profile) {
-        mProfile = profile;
         mReadabilityHooks =
                 sReadabilityHooksForTesting != null
                         ? sReadabilityHooksForTesting
                         : new ReadAloudReadabilityHooksImpl(mActivity, profile);
         if (mReadabilityHooks.isEnabled()) {
-            boolean isAllowed = ReadAloudFeatures.isAllowed(mProfileSupplier.get());
+            boolean isAllowed = ReadAloudFeatures.isAllowed(profile);
             ReadAloudMetrics.recordIsUserEligible(isAllowed);
             if (!isAllowed) {
                 ReadAloudMetrics.recordIneligibilityReason(
@@ -612,7 +608,7 @@ public class ReadAloudController
         if (mReadabilityHooks == null) {
             return;
         }
-        if (mProfile == null || !mProfile.isNativeInitialized()) {
+        if (mProfileSupplier.get() == null || !mProfileSupplier.get().isNativeInitialized()) {
             return;
         }
         if (!isURLReadAloudSupported(url)) {
@@ -680,8 +676,8 @@ public class ReadAloudController
         if (tab == null
                 || tab.getUrl() == null
                 || tab.getWebContents() == null
-                || mProfile == null
-                || !mProfile.isNativeInitialized()) {
+                || mProfileSupplier.get() == null
+                || !mProfileSupplier.get().isNativeInitialized()) {
             return false;
         }
 
@@ -1193,7 +1189,7 @@ public class ReadAloudController
 
     private Promise<Playback> createPlayback(PlaybackArgs args) {
         final var promise = new Promise<Playback>();
-        if (mProfile == null || !mProfile.isNativeInitialized()) {
+        if (mProfileSupplier.get() == null || !mProfileSupplier.get().isNativeInitialized()) {
             promise.reject(new Exception("missing profile"));
             return promise;
         }
