cd8328c71da5d81918e71cbafb86ae0a8b39ed9a
Quentin Pubert <qpubert@google.com>
2024-04-19 11:17:12 +0000

    [iOS][TabStrip] Re-enable MoveGroupBeforeItemDifferentBrowser unit test
    This CL re-enables this test which used to cause a use-after-free. To
    prevent this type of error down the road, the `const TabGroup*` pointer
    in TabSwitcherItem is replaced with a `base::WeakPtr<const TabGroup>`
    instead.
    
    Fixed: 335581919
    Change-Id: Idd064e10dcf7dffc10101acb74d792880566c799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463591
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289856}

--- ios/chrome/browser/ui/tab_switcher/tab_group_item.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_group_item.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation TabGroupItem {
   WebStateList* _webStateList;
   NSMutableArray<GroupTabInfo*>* _tabGroupInfos;
+  base::WeakPtr<const TabGroup> _tabGroup;
 }
 
 - (instancetype)initWithTabGroup:(const TabGroup*)tabGroup
@@ -23,50 +24,54 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CHECK(webStateList->ContainsGroup(tabGroup));
   self = [super init];
   if (self) {
-    _tabGroup = tabGroup;
+    _tabGroup = tabGroup->GetWeakPtr();
     _webStateList = webStateList;
     _tabGroupInfos = [[NSMutableArray alloc] init];
   }
   return self;
 }
 
+- (const TabGroup*)tabGroup {
+  return _tabGroup.get();
+}
+
 - (NSString*)title {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     return nil;
   }
   return _tabGroup->GetTitle();
 }
 
 - (NSString*)rawTitle {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     return nil;
   }
   return _tabGroup->GetRawTitle();
 }
 
 - (UIColor*)groupColor {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     return nil;
   }
   return _tabGroup->GetColor();
 }
 
 - (NSInteger)numberOfTabsInGroup {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     return 0;
   }
   return _tabGroup->range().count();
 }
 
 - (BOOL)collapsed {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     return NO;
   }
   return _tabGroup->visual_data().is_collapsed();
 }
 
 - (void)fetchGroupTabInfos:(GroupTabInfosFetchingCompletionBlock)completion {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     __weak TabGroupItem* weakSelf = self;
     base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
         FROM_HERE, base::BindOnce(^{
@@ -111,7 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // completion if there is no new snapshot or favicon to save.
 
 - (void)notifyCompletion:(GroupTabInfosFetchingCompletionBlock)completion {
-  if (!_webStateList->ContainsGroup(_tabGroup)) {
+  if (!_webStateList->ContainsGroup(self.tabGroup)) {
     completion(self, @[]);
     return;
   }
