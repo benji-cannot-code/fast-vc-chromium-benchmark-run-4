305ed221d45884dd8b97589709bafd9f747a47b2
Fiona Macintosh <fmacintosh@google.com>
2024-04-19 17:50:18 +0000

    Remove unused CookieSettingsUiAlignment feature
    Change-Id: I9d362c8c99e17e2b35978a1b66aadb63d10b9f14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466058
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290043}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -166,10 +166,6 @@ BASE_FEATURE(kIpProtectionUx,
              "IpProtectionUx",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kCookieSettingsUiAlignment,
-             "CookieSettingsUiAlignment",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kTrackingProtectionSettingsLaunch,
              "TrackingProtectionSettingsLaunch",
              base::FEATURE_DISABLED_BY_DEFAULT);
