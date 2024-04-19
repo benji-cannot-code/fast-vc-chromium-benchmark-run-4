b2a222ad0086df74698708dec1862264cf56cfa6
Austin Sullivan <asully@chromium.org>
2024-04-19 17:11:03 +0000

    webnn: Enable services unit test for the leakyRelu operator in CoreML
    Forgot to include this in https://crrev.com/c/5464381
    
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel,mac14.arm64-blink-rel
    Bug: 41481333
    Change-Id: Ibfd09b21c9da08c16e7ebab699af05fb74f7aa83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466658
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290016}

--- services/webnn/webnn_graph_impl_backend_test.cc
+++ services/webnn/webnn_graph_impl_backend_test.cc
@@ -350,6 +350,7 @@ void WebNNGraphImplBackendTest::SetUp() {
       "BuildAndComputeConcatWithConstants",
       "BuildAndComputeSingleOperatorElementWiseBinary",
       "BuildAndComputeSingleOperatorElementWiseUnary",
+      "BuildAndComputeSingleOperatorLeakyRelu",
       "BuildAndComputeSingleOperatorAveragePool2d",
       "BuildAndComputeSingleOperatorL2Pool2d",
       "BuildAndComputeSingleOperatorMaxPool2d",
