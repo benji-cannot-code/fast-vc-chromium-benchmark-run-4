e980bd6bd92ded14992ba01348e7db84e5247329
stluong <stluong@chromium.org>
2024-04-19 17:39:06 +0000

    Implement Performance Detection Take Action
    Implements the PerformanceDetectionManager::DiscardTab() so that when
    observers receive a notification that a certain health status is not
    healthy but actionable, callers can take action to improve the health
    status by discarding the specified pages.
    
    Fixed: 324261604
    Change-Id: I1440257da8cb6ca9c4101a20c8ba2dfe28a9481c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5415496
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290033}

--- chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h
+++ chrome/browser/performance_manager/public/user_tuning/performance_detection_manager.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/enum_set.h"
 #include "base/containers/flat_map.h"
+#include "base/functional/callback_forward.h"
+#include "base/functional/callback_helpers.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
@@ -64,6 +66,13 @@ class PerformanceDetectionManager {
   void AddActionableTabsObserver(ResourceTypeSet resource_types,
                                  ActionableTabsObserver* new_observer);
   void RemoveActionableTabsObserver(ActionableTabsObserver* o);
+
+  // Discards all eligible pages in `tabs` and runs `post_discard_cb`
+  // after the discard finishes. `post_discard_cb` must be valid to
+  // run on the UI sequence.
+  void DiscardTabs(std::vector<resource_attribution::PageContext> tabs,
+                   base::OnceCallback<void(bool)> post_discard_cb =
+                       base::OnceCallback<void(bool)>());
 
   // Returns whether a PerformanceDetectionManager was created and installed.
   // Should only return false in unit tests.
