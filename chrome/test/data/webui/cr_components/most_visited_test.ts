2f035318b3ae5f55019be08fe34f7071779e1dee
HuanPo Lin <robertlin@chromium.org>
2024-04-19 07:46:22 +0000

    preload: Test Prerender2NewTabPageTrigger re-trigger cases
    This CL adds coverage for Prerender2NewTabPageTrigger re-trigger cases
    in the existing tests.
    
    Bug: 40275094, 335517465
    Change-Id: Iaa31de0609312486cf27f0f519b840a5d0fd5bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465232
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289790}

--- chrome/test/data/webui/cr_components/most_visited_test.ts
+++ chrome/test/data/webui/cr_components/most_visited_test.ts
@@ -1296,7 +1296,7 @@ suite('Prerendering', () => {
     await handler.whenCalled('prerenderMostVisitedTile');
   });
 
-  test('prerender cancelation', async () => {
+  test('prerender cancelation and retrigger', async () => {
     // Arrange.
     await addTiles(1);
 
@@ -1314,6 +1314,16 @@ suite('Prerendering', () => {
 
     const mouseExitEvent = document.createEvent('MouseEvents');
     mouseExitEvent.initEvent('mouseleave', true, true);
+    tileLink.dispatchEvent(mouseExitEvent);
+
+    // Make sure Prerendering has been canceled.
+    await handler.whenCalled('cancelPrerender');
+
+    tileLink.dispatchEvent(mouseEnterEvent);
+
+    // Make sure Prerendering can be re-triggered
+    await handler.whenCalled('prerenderMostVisitedTile');
+
     tileLink.dispatchEvent(mouseExitEvent);
 
     // Make sure Prerendering has been canceled.
