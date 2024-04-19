c1654e6fbd8a5a96c643c3b52bb9439989a81ede
Eitan Goldberger <eitang@google.com>
2024-04-19 18:38:50 +0000

    [Read aloud] Auto install the voice pack of the browser/page language.
    Listen to the `languagesChanged` call. The Page Handler decides
    whether to send the browser or page language, depending on whether
    it detects the page language.
    
    This CL auto-installs the languages sent from `languagesChanged` event.
    
    Bug: 332323711
    Change-Id: I03eda34842b4430102f759c36b8d86fede5c7c2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467220
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290079}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -21,8 +21,7 @@ import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.m
 import {getTemplate} from './app.html.js';
 import {validatedFontName} from './common.js';
 import type {ReadAnythingToolbarElement} from './read_anything_toolbar.js';
-import type {VoicePackStatus} from './voice_language_util.js';
-import {convertLangOrLocaleForVoicePackManager, mojoVoicePackStatusToVoicePackStatusEnum} from './voice_language_util.js';
+import {convertLangOrLocaleForVoicePackManager, mojoVoicePackStatusToVoicePackStatusEnum, VoicePackStatus} from './voice_language_util.js';
 
 const ReadAnythingElementBase = WebUiListenerMixin(PolymerElement);
 
@@ -274,6 +273,10 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
   private localeToDisplayName: {[locale: string]: string};
 
   private voicePackInstallStatus: {[language: string]: VoicePackStatus} = {};
+
+  // Set of languages of the browser and/or of the pages navigated to that we
+  // need to download Natural voices for automatically
+  private languagesForVoiceDownloads: Set<string> = new Set();
 
   // State for speech synthesis paused/play state needs to be tracked explicitly
   // because there are bugs with window.speechSynthesis.paused and
@@ -760,6 +763,11 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
       return;
     }
     const voicePackStatus = mojoVoicePackStatusToVoicePackStatusEnum(status);
+    if (voicePackStatus === VoicePackStatus.NOT_INSTALLED &&
+        this.languagesForVoiceDownloads.has(lang)) {
+      chrome.readingMode.sendInstallVoicePackRequest(lang);
+    }
+
     this.voicePackInstallStatus =
         {...this.voicePackInstallStatus, [lang]: voicePackStatus};
     // TODO (b/335472298) Handle voice menu downloading voice spinners
@@ -1658,6 +1666,16 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
   languageChanged() {
     this.$.toolbar.updateFonts();
     this.selectPreferredVoice_();
+
+    const baseLang = chrome.readingMode.baseLanguageForSpeech;
+    const langCodeForVoicePackManager =
+        convertLangOrLocaleForVoicePackManager(baseLang);
+    if (langCodeForVoicePackManager) {
+      this.languagesForVoiceDownloads.add(langCodeForVoicePackManager);
+      // Inquire if the voice pack is downloaded. If not, it'll trigger a
+      // download when we get the response in updateVoicePackStatus()
+      this.sendGetVoicePackInfoRequest(langCodeForVoicePackManager);
+    }
   }
 
   private onKeyDown_(e: KeyboardEvent) {
