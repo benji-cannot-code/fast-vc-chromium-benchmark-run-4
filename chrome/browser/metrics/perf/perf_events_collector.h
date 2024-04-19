cbaa9964223ab54423647c8dd3baa5c7e4b8bf4c
Tuo Shan <shantuo@google.com>
2024-04-19 17:43:52 +0000

    Differentiate zero frequency due to file not existing
    We see sometimes base::SysInfo::NumberOfProcessors returns more logical
    cores than the actual possible numbers on the devices. This adds a check
    on the existence of /sys/devices/system/cpu/cpu* to differentiate this
    case and report to a different histogram bucket.
    
    Bug: b:324649047
    Change-Id: Ibae2575fb78d12216444aab6b4e1e98381558eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466483
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: Tuo Shan <shantuo@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Tuo Shan <shantuo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290041}

--- chrome/browser/metrics/perf/perf_events_collector.h
+++ chrome/browser/metrics/perf/perf_events_collector.h
@@ -109,8 +109,9 @@ class PerfCollector : public internal::MetricCollector {
     kSomeZeroCPUFrequencies,
     kAllZeroCPUFrequencies,
     kSuccessOnRetry,
+    kNumCPUsMoreThanPossible,
     // Magic constant used by the histogram macros.
-    kMaxValue = kSuccessOnRetry,
+    kMaxValue = kNumCPUsMoreThanPossible,
   };
 
   // Extracts the |lacros_channel| and |lacros_version| from |lacros_path|.
