a1dc65ff94ee3ffef92433d7a99ed7828d8e182a
Michael Slutskii <slutskii@google.com>
2024-04-19 13:57:03 +0000

    Allow generating symmetric keys for signing in platform keys service
    GenerateSymKey now requires key type to generate either a key that
    can be used only for signing or a key for encrypting/decrypting.
    
    Bug: b:288880151
    Change-Id: I7f143277080db895b03b7fbb9e117f4bec3be301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5398198
    Reviewed-by: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Commit-Queue: Michael Slutskii <slutskii@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289905}

--- chrome/browser/chromeos/platform_keys/platform_keys.h
+++ chrome/browser/chromeos/platform_keys/platform_keys.h
@@ -23,6 +23,9 @@ namespace chromeos::platform_keys {
 // Supported key types.
 enum class KeyType { kRsassaPkcs1V15, kEcdsa };
 
+// Supported symmetric key types.
+enum class SymKeyType { kAesCbc, kHmac };
+
 // Supported key attribute types.
 enum class KeyAttributeType { kCertificateProvisioningId, kKeyPermissions };
 
