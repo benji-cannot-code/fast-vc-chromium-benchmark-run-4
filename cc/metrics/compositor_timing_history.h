b26b97cb0b141dc14fd590e64074d27506b30e95
Jonathan Ross <jonross@chromium.org>
2024-04-19 21:19:56 +0000

    Remove Histogram Scheduling.Renderer.DeadlineMode
    We want to update the
    cc::SchedulerStateMachine::BeginImplFrameDeadlineMode enum. In order
    to add support for a new mode.
    
    The enum is currently mapped to an expired histogram. Let's remove it.
    
    OBSOLETE_HISTOGRAM[Scheduling.Renderer.DeadlineMode] = determined what
    we needed. This has expired and is no longer desired.
    
    Bug: 329346768
    Change-Id: Id0eeff3e38179553831e866ee21203d0d335bbdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468421
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290189}

--- cc/metrics/compositor_timing_history.h
+++ cc/metrics/compositor_timing_history.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/rolling_time_delta_history.h"
 #include "cc/cc_export.h"
 #include "cc/metrics/event_metrics.h"
-#include "cc/scheduler/scheduler.h"
 #include "cc/tiles/tile_priority.h"
 #include "components/viz/common/frame_sinks/begin_frame_args.h"
 
@@ -86,10 +85,6 @@ class CC_EXPORT CompositorTimingHistory {
   void WillDraw();
   void DidDraw();
   void WillInvalidateOnImplSide();
-
-  // Record the scheduler's deadline mode and send to UMA.
-  using DeadlineMode = SchedulerStateMachine::BeginImplFrameDeadlineMode;
-  void RecordDeadlineMode(DeadlineMode deadline_mode);
 
   base::TimeTicks begin_main_frame_sent_time() const {
     return begin_main_frame_sent_time_;
