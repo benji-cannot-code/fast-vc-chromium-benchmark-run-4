ce6ffcce947e1be4f60449cab062ec4bcffbef2c
Adem Derinel <derinel@google.com>
2024-04-19 10:49:55 +0000

    [Passwords][WebAuthn] Show a toast after successful PIN change
    Mocks: https://www.figma.com/file/rOiROnrQQN36fTAVdHMrzm/GPM-Passkeys-UX?type=design&node-id=5511-55610&mode=design&t=ORTtwO1UjEE3OWi8-0
    Impl: screen/BFjnJMBy5sipwJY
    
    Bug: 331611435
    Change-Id: I93da822c25513dbe0927818b0575382f37bd09d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463500
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289847}

--- chrome/browser/resources/password_manager/settings_section.ts
+++ chrome/browser/resources/password_manager/settings_section.ts
@@ -13,6 +13,7 @@ import './dialogs/move_passwords_dialog.js';
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {HelpBubbleMixin} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin.js';
 import type {CrLinkRowElement} from 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
+import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
 import {assert, assertNotReached} from 'chrome://resources/js/assert.js';
@@ -42,6 +43,7 @@ export interface SettingsSectionElement {
     passwordToggle: PrefToggleButtonElement,
     trustedVaultBanner: CrLinkRowElement,
     accountStorageToggle: PrefToggleButtonElement,
+    toast: CrToastElement,
   };
 }
 
@@ -228,6 +230,7 @@ export class SettingsSectionElement extends SettingsSectionElementBase {
     PasswordManagerImpl.getInstance().removeSavedPasswordListChangedListener(
         this.setCredentialsChangedListener_);
     this.setCredentialsChangedListener_ = null;
+    this.$.toast.hide();
   }
 
   override currentRouteChanged(route: Route): void {
@@ -400,7 +403,15 @@ export class SettingsSectionElement extends SettingsSectionElementBase {
   }
 
   private onChangePasswordManagerPinRowClick_() {
-    PasswordManagerImpl.getInstance().changePasswordManagerPin();
+    PasswordManagerImpl.getInstance().changePasswordManagerPin().then(
+        this.showToastForPasswordChange_.bind(this));
+  }
+
+  private showToastForPasswordChange_(success: boolean): void {
+    if (!success) {
+      return;
+    }
+    this.$.toast.show();
   }
 }
 
