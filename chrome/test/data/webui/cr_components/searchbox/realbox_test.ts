937885d78d11e672a7712d107493b8147fd9b297
Justin Donnelly <jdonnelly@chromium.org>
2024-04-19 16:53:56 +0000

    [lensoverlay] Test that searchbox navigation clears inline autocomplete.
    Also updates and adds a comment to an existing test.
    
    This is a follow up to crrev.com/c/5448814.
    
    Bug: b:332787629
    Change-Id: Id87e849adb68e7bce92d2038d1c96c447791d6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464631
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290001}

--- chrome/test/data/webui/cr_components/searchbox/realbox_test.ts
+++ chrome/test/data/webui/cr_components/searchbox/realbox_test.ts
@@ -938,12 +938,13 @@ suite('NewTabPageRealboxTest', () => {
     // Input is expected to have been focused before any navigation.
     realbox.$.input.dispatchEvent(new Event('focus'));
 
-    realbox.$.input.value = '  hello  ';
+    realbox.$.input.value = 'hello ';
     realbox.$.input.dispatchEvent(new InputEvent('input'));
 
     const matches = [
       createSearchMatch({
         allowedToBeDefaultMatch: true,
+        inlineAutocompletion: mojoString16('world'),
       }),
       createUrlMatch(),
     ];
@@ -961,6 +962,13 @@ suite('NewTabPageRealboxTest', () => {
     // First match is selected.
     assertTrue(matchEls[0]!.hasAttribute(Attributes.SELECTED));
 
+    // Before navigation, input should be inline autocompleted.
+    assertEquals('hello world', realbox.$.input.value);
+    let start = realbox.$.input.selectionStart!;
+    let end = realbox.$.input.selectionEnd!;
+    assertEquals('world', realbox.$.input.value.substring(start, end));
+
+    // Pressing enter...
     const shiftEnter = new KeyboardEvent('keydown', {
       bubbles: true,
       cancelable: true,
@@ -981,6 +989,13 @@ suite('NewTabPageRealboxTest', () => {
 
     // Navigation should close the dropdown.
     assertFalse(await areMatchesShowing());
+
+    // After navigation, the inline autocompletion should be applied to the text
+    // shown in the input and there should be no visible selection.
+    assertEquals('hello world', realbox.$.input.value);
+    start = realbox.$.input.selectionStart!;
+    end = realbox.$.input.selectionEnd!;
+    assertEquals('', realbox.$.input.value.substring(start, end));
   });
 
   test(
@@ -1216,6 +1231,9 @@ suite('NewTabPageRealboxTest', () => {
       matches,
       suggestionGroupsMap: {},
     });
+    // As soon as the new matches arrive, the pending enter triggers a
+    // navigation, which closes the dropdown.
+    assertFalse(await areMatchesShowing());
 
     // First match is selected.
     assertTrue(matchEls[0]!.hasAttribute(Attributes.SELECTED));
@@ -1227,9 +1245,6 @@ suite('NewTabPageRealboxTest', () => {
     assertTrue(args.areMatchesShowing);
     assertTrue(args.shiftKey);
     assertEquals(1, testProxy.handler.getCallCount('openAutocompleteMatch'));
-
-    // Navigation should close the dropdown.
-    assertFalse(await areMatchesShowing());
   });
 
   test('pressing Enter on the selected match navigates to it', async () => {
