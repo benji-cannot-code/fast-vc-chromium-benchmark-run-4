db1b23bee5ee7c9166c2ea95794abb4a91cf6fa9
Josh Hilke <jrhilke@google.com>
2024-04-19 17:08:31 +0000

    Remove fatal_crash_events_observer_save_file_paths_provider.
    Instead of using a global instance object to provide paths, just pass
    the paths directly to the constructor. This uses less code and will also
    make it easier to create other types of crash observers (for instance
    Chrome crash observer) that may need to use different paths.
    
    Change-Id: I3c3fdfdd8bdae1661c42044a2acce0aff47c62ba
    Bug: b:332614580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465812
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290014}

--- chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_browsertest.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_browsertest.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/affiliation/affiliation_test_helper.h"
 #include "chrome/browser/ash/policy/core/device_policy_cros_browser_test.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer.h"
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_save_file_paths_provider.h"
-#include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer_test_util.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.h"
 #include "chrome/browser/ash/settings/scoped_testing_cros_settings.h"
 #include "chrome/browser/ash/settings/stub_cros_settings_provider.h"
@@ -54,20 +52,6 @@ bool IsRecordCrashEvent(const ::reporting::Record& record) {
       record_data.event_data().type() == MetricEventType::FATAL_CRASH;
 }
 
-// Browser test environment for fatal crash events.
-class [[maybe_unused]] BrowserTestEnvironment final
-    : public FatalCrashEventsObserver::TestEnvironment::SaveFilePathsProvider {
- public:
-  BrowserTestEnvironment() = default;
-  ~BrowserTestEnvironment() override = default;
-  BrowserTestEnvironment(const BrowserTestEnvironment&) = delete;
-  BrowserTestEnvironment& operator=(const BrowserTestEnvironment&) = delete;
-
- private:
-  // Reset g_default_save_file_paths_provider_ during the test.
-  base::AutoReset<decltype(g_default_save_file_paths_provider_)> auto_reset_{
-      &g_default_save_file_paths_provider_, this};
-};
 
 class FatalCrashEventsBrowserTest
     : public ::policy::DevicePolicyCrosBrowserTest,
@@ -110,7 +94,6 @@ class FatalCrashEventsBrowserTest
   }
 
  private:
-  BrowserTestEnvironment fatal_crash_events_browser_test_environment_;
   ::policy::DevicePolicyCrosTestHelper test_helper_;
   // Set up device affiliation. No need to set up or log in as an affiliated
   // user, because reporting fatal crash events is controlled by a device
