c8c9cb6875776debee4ee273a53e8d3cd78ceb72
Benjamin Williams <bwwilliams@google.com>
2024-04-19 19:13:47 +0000

    [iOS] Refactor Docking Promo inactivity thresholds to use TimeDelta.
    The Docking Promo project currently uses integer values (in hours) for
    its feature parameters related to inactivity thresholds. This approach
    limits flexibility, as any changes to the time units (e.g., switching
    from hours to minutes or days) require code modifications and
    deployment.
    
    This change refactors the Docking Promo feature params; using TimeDelta
    instead of int (hours) for inactivity thresholds, enabling dynamic time
    unit configuration via Finch.
    
    Change-Id: If4bc28170de46209dc6168952e6d5a31df0b0f90
    Bug: 335663309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462487
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290101}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -77,6 +77,10 @@ const char kIOSDockingPromoNewUserInactiveThresholdHours[] =
     "IOSDockingPromoNewUserInactiveThresholdHours";
 const char kIOSDockingPromoOldUserInactiveThresholdHours[] =
     "IOSDockingPromoOldUserInactiveThresholdHours";
+const char kIOSDockingPromoNewUserInactiveThreshold[] =
+    "IOSDockingPromoNewUserInactiveThreshold";
+const char kIOSDockingPromoOldUserInactiveThreshold[] =
+    "IOSDockingPromoOldUserInactiveThreshold";
 
 BASE_FEATURE(kIOSDockingPromo,
              "IOSDockingPromo",
@@ -447,6 +451,20 @@ DockingPromoDisplayTriggerArm DockingPromoExperimentTypeEnabled() {
       base::GetFieldTrialParamByFeatureAsInt(
           kIOSDockingPromo, kIOSDockingPromoExperimentType,
           /*default_value=*/(int)DockingPromoDisplayTriggerArm::kAfterFRE));
+}
+
+const base::TimeDelta InactiveThresholdForNewUsersUntilDockingPromoShown() {
+  return base::GetFieldTrialParamByFeatureAsTimeDelta(
+      kIOSDockingPromo, kIOSDockingPromoNewUserInactiveThreshold,
+      /*default_value=*/
+      base::Hours(HoursInactiveForNewUsersUntilShowingDockingPromo()));
+}
+
+const base::TimeDelta InactiveThresholdForOldUsersUntilDockingPromoShown() {
+  return base::GetFieldTrialParamByFeatureAsTimeDelta(
+      kIOSDockingPromo, kIOSDockingPromoOldUserInactiveThreshold,
+      /*default_value=*/
+      base::Hours(HoursInactiveForOldUsersUntilShowingDockingPromo()));
 }
 
 int HoursInactiveForNewUsersUntilShowingDockingPromo() {
