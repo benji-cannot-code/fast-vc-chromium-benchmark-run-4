ae452beb438528b3129fcded531de62f2fc4152e
Thomas Nguyen <tungnh@google.com>
2024-04-19 21:43:40 +0000

    [PEPC] Not allow PEPC in cross-origin subframe without frame-ancestors
    The frame-ancestors CSP directive must be explicitly included to allow a
    document using PEPC to be embedded cross-origin (to the top level
    frame). That's a security measure outlined in DD
    https://docs.google.com/document/d/1a1gjlJ4VkAWoG8AeGKZDcQXm_c0q-cFTs_5MxmjWVYI/edit?tab=t.0
    
    Explainer PR: https://github.com/WICG/PEPC/pull/15/commits
    
    Fixed: 335834282
    Bug: 1462930
    Change-Id: Iacdf80a425498f0ee3e3a068f583446a333e4592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5453616
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290205}

--- third_party/blink/renderer/core/frame/csp/content_security_policy.h
+++ third_party/blink/renderer/core/frame/csp/content_security_policy.h
@@ -417,6 +417,9 @@ class CORE_EXPORT ContentSecurityPolicy final
   // - https://*:*
   bool AllowFencedFrameOpaqueURL() const;
 
+  // Returns whether enforcing frame-ancestors CSP directives are present.
+  bool HasEnforceFrameAncestorsDirectives();
+
   void Count(WebFeature feature) const;
 
  private:
--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/space_split_string.h"
 #include "third_party/blink/renderer/core/events/mouse_event.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
+#include "third_party/blink/renderer/core/frame/csp/content_security_policy.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/frame/local_frame_ukm_aggregator.h"
 #include "third_party/blink/renderer/core/frame/visual_viewport.h"
@@ -303,6 +304,17 @@ void HTMLPermissionElement::AttachLayoutTree(AttachContext& context) {
   if (GetDocument().GetFrame()->IsInFencedFrameTree()) {
     AddConsoleError(
         String::Format("The permission '%s' is not allowed in fenced frame",
+                       GetType().Utf8().c_str()));
+    return;
+  }
+
+  if (GetDocument().GetFrame()->IsCrossOriginToOutermostMainFrame() &&
+      !GetExecutionContext()
+           ->GetContentSecurityPolicy()
+           ->HasEnforceFrameAncestorsDirectives()) {
+    AddConsoleError(
+        String::Format("The permission '%s' is not allowed without the CSP "
+                       "'frame-ancestors' directive present.",
                        GetType().Utf8().c_str()));
     return;
   }
