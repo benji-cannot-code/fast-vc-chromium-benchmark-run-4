40a587bc1db1ab52289180a4beeeb1e2a5deebec
Samar <samarchehade@google.com>
2024-04-19 14:48:17 +0000

    [UNOp2][Android] Suppress history sync in the re-FRE when appropriate
    Bug: b/41493788
    Change-Id: I0e40727bd6269ded9420886826a4da76a3b2a5bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465636
    Reviewed-by: Menghan Yang <myuu@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289930}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncHelperTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncHelperTest.java
@@ -56,22 +56,26 @@ public class HistorySyncHelperTest {
     @Test
     @SmallTest
     public void testDidAlreadyOptIn() {
+        Assert.assertFalse(mHistorySyncHelper.shouldSuppressHistorySync());
         Assert.assertFalse(mHistorySyncHelper.didAlreadyOptIn());
 
         when(mSyncServiceMock.getSelectedTypes())
                 .thenReturn(Set.of(UserSelectableType.HISTORY, UserSelectableType.TABS));
 
         Assert.assertTrue(mHistorySyncHelper.didAlreadyOptIn());
+        Assert.assertTrue(mHistorySyncHelper.shouldSuppressHistorySync());
     }
 
     @Test
     @SmallTest
     public void testIsHistorySyncDisabledByPolicy_syncDisabledByPolicy() {
+        Assert.assertFalse(mHistorySyncHelper.shouldSuppressHistorySync());
         Assert.assertFalse(mHistorySyncHelper.isHistorySyncDisabledByPolicy());
 
         when(mSyncServiceMock.isSyncDisabledByEnterprisePolicy()).thenReturn(true);
 
         Assert.assertTrue(mHistorySyncHelper.isHistorySyncDisabledByPolicy());
+        Assert.assertTrue(mHistorySyncHelper.shouldSuppressHistorySync());
     }
 
     @Test
@@ -82,6 +86,7 @@ public class HistorySyncHelperTest {
         when(mSyncServiceMock.isTypeManagedByPolicy(anyInt())).thenReturn(true);
 
         Assert.assertTrue(mHistorySyncHelper.isHistorySyncDisabledByPolicy());
+        Assert.assertTrue(mHistorySyncHelper.shouldSuppressHistorySync());
     }
 
     @Test
@@ -92,6 +97,18 @@ public class HistorySyncHelperTest {
         when(mSyncServiceMock.isTypeManagedByCustodian(anyInt())).thenReturn(true);
 
         Assert.assertTrue(mHistorySyncHelper.isHistorySyncDisabledByCustodian());
+        Assert.assertTrue(mHistorySyncHelper.shouldSuppressHistorySync());
+    }
+
+    @Test
+    @SmallTest
+    public void testShouldSuppressHistorySync() {
+        when(mSyncServiceMock.getSelectedTypes()).thenReturn(Set.of());
+        when(mSyncServiceMock.isTypeManagedByCustodian(anyInt())).thenReturn(false);
+        when(mSyncServiceMock.isTypeManagedByPolicy(anyInt())).thenReturn(false);
+        when(mSyncServiceMock.isSyncDisabledByEnterprisePolicy()).thenReturn(false);
+
+        Assert.assertFalse(mHistorySyncHelper.shouldSuppressHistorySync());
     }
 
     @Test
