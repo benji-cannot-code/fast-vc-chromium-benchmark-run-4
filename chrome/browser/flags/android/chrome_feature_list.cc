c6ccddc1a82bdb66ee6fd4b29771afde5ee248ae
Christian Dullweber <dullweber@chromium.org>
2024-04-19 14:04:38 +0000

    eCCT: Add flag
    Add feature and flag for ephemeral CCTs.
    
    Bug: 335607734
    Change-Id: I3f295bde3fa2326baa9517bcfe4cd4acd9a8092c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465853
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289907}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -185,6 +185,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCCTEmbedderSpecialBehaviorTrigger,
     &kCCTExtendTrustedCdnPublisher,
     &kCCTFeatureUsage,
+    &kCCTEphemeralMode,
     &kCCTIncognitoAvailableToThirdParty,
     &kCCTIntentFeatureOverrides,
     &kCCTMinimized,
@@ -504,6 +505,10 @@ BASE_FEATURE(kCCTClientDataHeader,
 
 BASE_FEATURE(kCCTEmbedderSpecialBehaviorTrigger,
              "CCTEmbedderSpecialBehaviorTrigger",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kCCTEphemeralMode,
+             "CCTEphemeralMode",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTExtendTrustedCdnPublisher,
