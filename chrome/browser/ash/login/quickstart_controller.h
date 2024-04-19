7eca46d5e855b10550e6054d64b497591335900c
Brittany Hartmire <bhartmire@google.com>
2024-04-19 15:47:42 +0000

    [QuickStart] Add QuickStart.FlowAborted.Reason metric
    Test: additions to QuickStartBrowserTest
    Change-Id: I878e3330e73b8f17414d0bff2c701011b52d7235
    Bug: b/335026165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463269
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289961}

--- chrome/browser/ash/login/quickstart_controller.h
+++ chrome/browser/ash/login/quickstart_controller.h
@@ -36,6 +36,8 @@ class QuickStartController
       public TargetDeviceBootstrapController::Observer,
       public bluetooth_config::mojom::SystemPropertiesObserver {
  public:
+  using AbortFlowReason = QuickStartMetrics::AbortFlowReason;
+
   // QuickStart flow entry point locations.
   enum class EntryPoint {
     WELCOME_SCREEN,
@@ -58,16 +60,6 @@ class QuickStartController
     CONTINUING_AFTER_ENROLLMENT_CHECKS,
     FALLBACK_URL_FLOW_ON_GAIA_SCREEN,
     SETUP_COMPLETE,
-  };
-
-  enum class AbortFlowReason {
-    USER_CLICKED_BACK,
-    USER_CLICKED_CANCEL,
-    SIGNIN_SCHOOL,
-    ENTERPRISE_ENROLLMENT,
-    ERROR,
-    // Child accounts are not yet supported.
-    ADD_CHILD,
   };
 
   // Implemented by the QuickStartScreen
--- chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/quick_start_screen_browsertest.cc
@@ -76,6 +76,8 @@ constexpr char kScreenClosedNetworkScreen[] =
     "QuickStart.ScreenClosed.NetworkScreen";
 constexpr char kAuthenticationMethodHistogram[] =
     "QuickStart.AuthenticationMethod";
+constexpr char kFlowAbortedReason[] = "QuickStart.FlowAborted.Reason";
+
 constexpr test::UIPath kQuickStartEntryPointPath = {
     WelcomeView::kScreenId.name, kWelcomeScreen, kQuickStartEntryPoint};
 constexpr test::UIPath kQuickStartButtonPath = {
@@ -530,6 +532,9 @@ IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, ClickingCancelReturnsToWelcome) {
   histogram_tester_.ExpectBucketCount(
       std::string{kScreenClosedQSSetUpWithAndroidPhone} + kReasonHistogram,
       quick_start::QuickStartMetrics::ScreenClosedReason::kUserCancelled, 0);
+  histogram_tester_.ExpectBucketCount(
+      kFlowAbortedReason,
+      quick_start::QuickStartMetrics::AbortFlowReason::USER_CLICKED_CANCEL, 0);
 
   // Cancel button must be present.
   test::OobeJS()
@@ -542,6 +547,9 @@ IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, ClickingCancelReturnsToWelcome) {
   histogram_tester_.ExpectBucketCount(
       std::string{kScreenClosedQSSetUpWithAndroidPhone} + kReasonHistogram,
       quick_start::QuickStartMetrics::ScreenClosedReason::kUserCancelled, 1);
+  histogram_tester_.ExpectBucketCount(
+      kFlowAbortedReason,
+      quick_start::QuickStartMetrics::AbortFlowReason::USER_CLICKED_CANCEL, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(QuickStartBrowserTest, CancelOnQRCode) {
