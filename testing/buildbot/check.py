267f5fba92920a9b451a86065ff547f6eaf050d0
Garrett Beaty <gbeaty@chromium.org>
2024-04-19 18:56:42 +0000

    Remove stray character from docstring in check.py.
    Change-Id: I54d230fd737d1ee9cc6631b9e4638673028eb2e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468292
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Struan Shrimpton <sshrimp@google.com>
    Commit-Queue: Struan Shrimpton <sshrimp@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290088}

--- testing/buildbot/check.py
+++ testing/buildbot/check.py
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 """Runs checks on the files defining tests.
-d
+
 This performs the following checks:
 * Checks that any entry in gn_isolate_map.pyl is referenced by some
   builder (modulo targets known to be used by builders in other projects
