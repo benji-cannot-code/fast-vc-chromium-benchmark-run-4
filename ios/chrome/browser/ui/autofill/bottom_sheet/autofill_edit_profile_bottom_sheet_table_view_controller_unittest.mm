74e11e22219c2b8fbd8690625ba82b7ca31381a1
Vidhan Jain <vidhanj@google.com>
2024-04-19 09:53:16 +0000

    [iOS] Update AutofillEditProfileBottomSheetTableViewControllerTest
    This CL updates the unit test AutofillEditProfileBottomSheetTableViewControllerTest to be a parameterized unit test, potentially covering the test cases
    for the following scenarios:
    1. When the bottomsheet is presented for just saving the profile.
    2. When the bottomsheet is presented for the case of updating the
    profile.
    3. When the bottomsheet is presented for the case of migration of
    profile to the account.
    
    Bug: 1482269
    Change-Id: I40bfc566e7db8ca5fb16e4449dc2c7f04773636f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459119
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1289832}

--- ios/chrome/browser/ui/autofill/bottom_sheet/autofill_edit_profile_bottom_sheet_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/autofill_edit_profile_bottom_sheet_table_view_controller_unittest.mm
@@ -28,7 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 class AutofillEditProfileBottomSheetTableViewControllerTest
-    : public LegacyChromeTableViewControllerTest {
+    : public LegacyChromeTableViewControllerTest,
+      public ::testing::WithParamInterface<AutofillSaveProfilePromptMode> {
  protected:
   void SetUp() override {
     LegacyChromeTableViewControllerTest::SetUp();
@@ -46,11 +47,11 @@ class AutofillEditProfileBottomSheetTableViewControllerTest
     AutofillEditProfileBottomSheetTableViewController* viewController =
         [[AutofillEditProfileBottomSheetTableViewController alloc]
             initWithDelegate:nil
-               editSheetMode:AutofillSaveProfilePromptMode::kNewProfile];
+               editSheetMode:GetParam()];
     autofill_profile_edit_table_view_controller_ =
         [[AutofillProfileEditTableViewController alloc]
             initWithDelegate:delegate_mock_
-                   userEmail:nil
+                   userEmail:@"test@gmail.com"
                   controller:viewController
                 settingsView:NO];
     viewController.handler = autofill_profile_edit_table_view_controller_;
@@ -103,9 +104,18 @@ class AutofillEditProfileBottomSheetTableViewControllerTest
   id delegate_mock_;
 };
 
-// Tests that there are no requirement checks for the profiles saved to sync.
-TEST_F(AutofillEditProfileBottomSheetTableViewControllerTest,
-       TestNoRequirements) {
+INSTANTIATE_TEST_SUITE_P(
+    /* No InstantiationName */,
+    AutofillEditProfileBottomSheetTableViewControllerTest,
+    testing::Values(AutofillSaveProfilePromptMode::kNewProfile,
+                    AutofillSaveProfilePromptMode::kUpdateProfile,
+                    AutofillSaveProfilePromptMode::kMigrateProfile));
+
+}  // namespace
+
+// Tests that there are requirement checks for the profiles to be saved.
+TEST_P(AutofillEditProfileBottomSheetTableViewControllerTest,
+       TestRequirements) {
   [autofill_profile_edit_table_view_controller_ setLine1Required:YES];
   [autofill_profile_edit_table_view_controller_ setCityRequired:YES];
   [autofill_profile_edit_table_view_controller_ setStateRequired:YES];
@@ -136,7 +146,6 @@ TEST_F(AutofillEditProfileBottomSheetTableViewControllerTest,
   city_item.textFieldValue = @"";
   [autofill_profile_edit_table_view_controller_
       tableViewItemDidChange:city_item];
-  EXPECT_EQ(button_item.enabled, YES);
+  EXPECT_EQ(button_item.enabled,
+            GetParam() != AutofillSaveProfilePromptMode::kMigrateProfile);
 }
-
-}  // namespace
