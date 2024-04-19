1f5d751845671123b9257a5e1e1dc48e2f893055
Stephen McGruer <smcgruer@chromium.org>
2024-04-19 17:33:31 +0000

    Clean up expired flag 'add-identity-in-can-make-payment'
    This flag expired back in M115. It was part of an opt-out origin
    trial to restore certain features to the CanMakePaymentEvent IDL
    file. Now that it has expired, those features can be removed entirely.
    
    We may re-introduce a similar flag in future if/when we tackle
    native app IS_READY_TO_PAY, but there is no point in keeping this
    code/flag around for that potential future.
    
    Bug: 40909272, 40212375
    
    Change-Id: I31aa01b9406685b88de16cec5a789d21f53b2815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5457560
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290028}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2102,9 +2102,6 @@ extern const char kWebGpuDeveloperFeaturesDescription[];
 extern const char kWebPaymentsExperimentalFeaturesName[];
 extern const char kWebPaymentsExperimentalFeaturesDescription[];
 
-extern const char kAddIdentityInCanMakePaymentEventName[];
-extern const char kAddIdentityInCanMakePaymentEventDescription[];
-
 extern const char kAppStoreBillingDebugName[];
 extern const char kAppStoreBillingDebugDescription[];
 
