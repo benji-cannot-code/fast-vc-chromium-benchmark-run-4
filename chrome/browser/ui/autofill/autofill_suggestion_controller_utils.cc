d8948066b0a0718edcc2ddb0c97f9004fe0844b7
Jan Keitel <jkeitel@google.com>
2024-04-19 12:56:59 +0000

    Move suggestion rotation logic into the KA controller.
    This CL is another step towards eliminating the
    AutofillKeyboardAccessoryAdapter.
    
    The AutofillKeyboardAccessoryAdapter roughly does 4 things:
    1) It changes the order of the suggestions in the controller by moving
       "clear form" suggestions to the front.
    2) It provides its own suggestion labels.
    3) It has custom deletion logic.
    4) It has logic to manage its own lifetime and that of the AccessoryView
       that it owns.
    
    This CL moves 1) and 2) from the adapter into the
    AutofillKeyboardAccessoryController. 3) and 4) remain in the adapter.
    
    Bug: 333316034
    Change-Id: I0ff1596c0b96524368ee99225a79138f2b735f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465840
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1289886}

--- chrome/browser/ui/autofill/autofill_suggestion_controller_utils.cc
+++ chrome/browser/ui/autofill/autofill_suggestion_controller_utils.cc
@@ -83,8 +83,9 @@ void NotifyIphAboutAcceptedSuggestion(content::BrowserContext* browser_context,
   }
 }
 
-void UpdateSuggestionsFromDataList(base::span<const SelectOption> options,
-                                   std::vector<Suggestion>& suggestions) {
+std::vector<Suggestion> UpdateSuggestionsFromDataList(
+    base::span<const SelectOption> options,
+    std::vector<Suggestion> suggestions) {
   // Remove all the old data list values, which should always be at the top of
   // the list if they are present.
   std::erase_if(suggestions, [](const Suggestion& suggestion) {
@@ -98,8 +99,7 @@ void UpdateSuggestionsFromDataList(base::span<const SelectOption> options,
         suggestions[0].popup_item_id == PopupItemId::kSeparator) {
       suggestions.erase(suggestions.begin());
     }
-
-    return;
+    return suggestions;
   }
 
   // Add a separator if there are any other values.
@@ -117,6 +117,7 @@ void UpdateSuggestionsFromDataList(base::span<const SelectOption> options,
     suggestions[i].labels = {{Suggestion::Text(options[i].content)}};
     suggestions[i].popup_item_id = PopupItemId::kDatalistEntry;
   }
+  return suggestions;
 }
 
 }  // namespace autofill
