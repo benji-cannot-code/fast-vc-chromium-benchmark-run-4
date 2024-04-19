296df749fe0bfd9485b6eb08c3b7541775a49091
Alison Gale <agale@chromium.org>
2024-04-19 17:13:31 +0000

    Fix flaky default browser infobar tests
    This uses the same check that is used elsewhere in the startup logic
    to prevent the infobars from being shown in tests.
    
    Change-Id: Ie738ad0a3088407a243c8131145c7b0feb98f1fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466808
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290018}

--- chrome/browser/ui/startup/default_browser_infobar_delegate.cc
+++ chrome/browser/ui/startup/default_browser_infobar_delegate.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/command_line.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/metrics/histogram_macros.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar.h"
 #include "components/prefs/pref_service.h"
 #include "components/vector_icons/vector_icons.h"
+#include "content/public/common/content_switches.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace chrome {
@@ -130,11 +132,15 @@ bool DefaultBrowserInfoBarDelegate::Accept() {
                             ACCEPT_INFO_BAR,
                             NUM_INFO_BAR_USER_INTERACTION_TYPES);
 
-  // The worker pointer is reference counted. While it is running, the
-  // message loops of the FILE and UI thread will hold references to it
-  // and it will be automatically freed once all its tasks have finished.
-  base::MakeRefCounted<shell_integration::DefaultBrowserWorker>()
-      ->StartSetAsDefault(base::DoNothing());
+  // Only change the default browser when not in a test environment.
+  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kTestType)) {
+    // The worker pointer is reference counted. While it is running, the
+    // message loops of the FILE and UI thread will hold references to
+    // it and it will be automatically freed once all its tasks have
+    // finished.
+    base::MakeRefCounted<shell_integration::DefaultBrowserWorker>()
+        ->StartSetAsDefault(base::DoNothing());
+  }
 
   return ConfirmInfoBarDelegate::Accept();
 }
