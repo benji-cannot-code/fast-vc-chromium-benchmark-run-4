889c99215e37f86eb9cfae735444ff627a2cb6f1
Juliet Levesque <julietlevesque@google.com>
2024-04-19 20:03:08 +0000

    [Nearby Presence] Discover service in DiscoverServiceAndCharacteristics
    Implements the first half of
    BleV2GattClient::DiscoverServiceAndCharacteristics() to discover GATT
    services on the remote device, and check for a match with the passed
    in service UUID. A follow up CL wil implement the second half of this
    method, which discovers the characteristics. See context in
    go/cros-nearby-ble-v2.
    
    implemented, more e2e manual verification will follow.
    
    Tested: Prototyping + unit tests. Once all of BleV2GattClient is
    Bug: b/311430390
    Change-Id: I1a642f82a87b12b09408097a980d071153a4d207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5445883
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290137}

--- chrome/services/sharing/nearby/platform/ble_v2_gatt_client.h
+++ chrome/services/sharing/nearby/platform/ble_v2_gatt_client.h
@@ -8,8 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/public/mojom/device.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
-#include "mojo/public/cpp/bindings/remote.h"
+#include "mojo/public/cpp/bindings/shared_remote.h"
 #include "third_party/nearby/src/internal/platform/implementation/ble_v2.h"
+
+namespace base {
+class SequencedTaskRunner;
+class WaitableEvent;
+}  // namespace base
 
 namespace nearby::chrome {
 
@@ -43,7 +48,32 @@ class BleV2GattClient : public ::nearby::api::ble_v2::GattClient {
   void Disconnect() override;
 
  private:
-  mojo::Remote<bluetooth::mojom::Device> remote_device_;
+  struct GattService {
+    GattService();
+    ~GattService();
+
+    bluetooth::mojom::ServiceInfoPtr service_info;
+  };
+
+  void DoDiscoverServices(
+      base::WaitableEvent* discover_services_waitable_event);
+  void OnGetGattServices(
+      base::WaitableEvent* discover_services_waitable_event,
+      std::vector<bluetooth::mojom::ServiceInfoPtr> services);
+
+  void Shutdown(base::WaitableEvent* shutdown_waitable_event);
+
+  bool have_gatt_services_been_discovered_ = false;
+  std::map<std::string, std::unique_ptr<GattService>>
+      uuid_to_discovered_gatt_service_map_;
+
+  // Track all pending tasks in case the object is invalidated while
+  // waiting.
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+  base::flat_set<raw_ptr<base::WaitableEvent>>
+      pending_discover_services_waitable_events_;
+
+  mojo::SharedRemote<bluetooth::mojom::Device> remote_device_;
 };
 
 }  // namespace nearby::chrome
