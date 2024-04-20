e3d683690403846f5a5ad2da1982d3f076293bb5
Orin Jaworski <orinj@google.com>
2024-04-20 01:25:18 +0000

    Record and test history embeddings histograms for vector database search
    Bug: 335897915
    Change-Id: If2029555262e741d8969718f59ab339b57c8f53a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468330
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290283}

--- components/history_embeddings/history_embeddings_service.cc
+++ components/history_embeddings/history_embeddings_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/time/time.h"
+#include "base/timer/elapsed_timer.h"
 #include "components/history/core/browser/history_types.h"
 #include "components/history/core/browser/url_database.h"
 #include "components/history/core/browser/url_row.h"
@@ -227,7 +228,8 @@ std::vector<ScoredUrl> HistoryEmbeddingsService::Storage::Search(
     Embedding query_embedding,
     std::optional<base::Time> time_range_start,
     size_t count) {
-  std::vector<ScoredUrl> scored_urls = sql_database.FindNearest(
+  base::ElapsedTimer timer;
+  SearchInfo search_info = sql_database.FindNearest(
       time_range_start, count, std::move(query_embedding),
       base::BindRepeating(
           [](base::WeakPtr<std::atomic<size_t>> weak_latest_query_id,
@@ -238,9 +240,17 @@ std::vector<ScoredUrl> HistoryEmbeddingsService::Storage::Search(
             return !weak_latest_query_id || *weak_latest_query_id != query_id;
           },
           std::move(weak_latest_query_id), query_id));
+  base::UmaHistogramTimes("History.Embeddings.Search.Duration",
+                          timer.Elapsed());
+  base::UmaHistogramCounts1M("History.Embeddings.Search.UrlCount",
+                             search_info.searched_url_count);
+  base::UmaHistogramCounts10M("History.Embeddings.Search.EmbeddingCount",
+                              search_info.searched_embedding_count);
+  base::UmaHistogramBoolean("History.Embeddings.Search.Completed",
+                            search_info.completed);
 
   // Populate source passages.
-  for (ScoredUrl& scored_url : scored_urls) {
+  for (ScoredUrl& scored_url : search_info.scored_urls) {
     std::optional<proto::PassagesValue> value =
         sql_database.GetPassages(scored_url.url_id);
     if (value &&
@@ -249,7 +259,7 @@ std::vector<ScoredUrl> HistoryEmbeddingsService::Storage::Search(
     }
   }
 
-  return scored_urls;
+  return std::move(search_info.scored_urls);
 }
 
 void HistoryEmbeddingsService::Storage::HandleHistoryDeletions(
