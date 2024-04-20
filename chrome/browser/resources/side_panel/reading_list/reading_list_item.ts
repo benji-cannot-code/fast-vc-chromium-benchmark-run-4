0c42391d87d57950a96088efd5ed7cc61f43bc7d
dpapad <dpapad@chromium.org>
2024-04-20 00:41:11 +0000

    SidePanel: Migrate c/b/r/side_panel/reading_list to Lit, part 4.
    In this part migrating reading_list_app.ts and icons.html. As part of
    the migration also:
    
     - Updated CrSelectableMixin to accommodate the case where selectable
       items reside in the shadowRoot, and not in a <slot>.
     - Ported sp_shared_style.css and mwb_element_shared_style_lit.css to
       Lit.
     - Migrated sp_footer.ts and sp_empty_state.ts to Lit.
    
    Bug: 40943652
    Change-Id: I4eb527a6c28acfaadebfd4a29ed539ac2b3e82eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463215
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290271}

--- chrome/browser/resources/side_panel/reading_list/reading_list_item.ts
+++ chrome/browser/resources/side_panel/reading_list/reading_list_item.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_url_list_item/cr_url_list_item.js';
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
-import 'chrome://resources/cr_elements/icons.html.js';
+import 'chrome://resources/cr_elements/icons_lit.html.js';
 import './icons.html.js';
 
 import type {CrUrlListItemElement} from 'chrome://resources/cr_elements/cr_url_list_item/cr_url_list_item.js';
