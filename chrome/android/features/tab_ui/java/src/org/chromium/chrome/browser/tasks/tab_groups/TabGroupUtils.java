cd67ab8f6432b7117ff8c765915a05ffea4d4b0d
Ted Choc <tedchoc@chromium.org>
2024-04-20 00:05:56 +0000

    Remove getLastUsedRegularProfile from TabGroupUtils
    Bug: 40254448
    Change-Id: I35c3c68928158cdf7289b43721bf414e3aed9727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468154
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290261}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_groups/TabGroupUtils.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_groups/TabGroupUtils.java
@@ -14,7 +14,7 @@ import androidx.annotation.StringRes;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
-import org.chromium.chrome.browser.profiles.ProfileManager;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
@@ -38,6 +38,7 @@ public class TabGroupUtils {
     private static TabModelSelectorTabObserver sTabModelSelectorTabObserver;
 
     public static void maybeShowIPH(
+            Profile profile,
             @FeatureConstants String featureName,
             View view,
             @Nullable BottomSheetController bottomSheetController) {
@@ -64,8 +65,7 @@ public class TabGroupUtils {
                 return;
         }
 
-        final Tracker tracker =
-                TrackerFactory.getTrackerForProfile(ProfileManager.getLastUsedRegularProfile());
+        final Tracker tracker = TrackerFactory.getTrackerForProfile(profile);
         if (!tracker.isInitialized()) return;
         if (!tracker.shouldTriggerHelpUI(featureName)) return;
 
@@ -134,6 +134,7 @@ public class TabGroupUtils {
                                 || (navigationHandle.pageTransition() & PageTransition.CORE_MASK)
                                         == PageTransition.GENERATED) {
                             maybeShowIPH(
+                                    tab.getProfile(),
                                     FeatureConstants.TAB_GROUPS_QUICKLY_COMPARE_PAGES_FEATURE,
                                     tab.getView(),
                                     null);
