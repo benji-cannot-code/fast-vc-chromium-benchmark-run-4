2af5fa43b8278d37c3dd83893055b9f490876198
weidongliu <weidongliu@chromium.org>
2024-04-20 19:05:33 +0000

    Reland "Reland "use CalculatePreferredSize(SizeBounds) in //ui/views""
    Respect ProportionallySizedView's preferred_width_ first.
    
    Original change's description:
    > Reland "use CalculatePreferredSize(SizeBounds) in //ui/views"
    >
    > Because TabbedPaneTab has LayoutManager, it will use
    > LayoutManager::GetPreferredHeightForWidth by default, so can't delete
    > GetHeightForWidth now.
    >
    > Original change's description:
    > > use CalculatePreferredSize(SizeBounds) in //ui/views
    > >
    > > In order to ensure implementation consistency and avoid oversights, we
    > > should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    > > them with the CalculatePreferredSize(SizeBounds) overload.
    > >
    > > Bug: 40232718
    > > Change-Id: Id40aa2bb592e144ddc7c14588352ada8481245c4
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458613
    > > Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    > > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1289043}
    >
    > Bug: 40232718
    > Change-Id: I79a0993b2aebf930ab701e68a8d7cbd05d411b8f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465280
    > Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    > Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1289734}
    
    Bug: 40232718, 335769951
    Change-Id: I4e696f3bfdd7e1d7445521b81caf5ab7dac7511c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467482
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290370}

--- ui/views/controls/tabbed_pane/tabbed_pane.cc
+++ ui/views/controls/tabbed_pane/tabbed_pane.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "base/i18n/rtl.h"
+#include "base/notreached.h"
 #include "build/build_config.h"
 #include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkPath.h"
@@ -132,10 +133,16 @@ void TabbedPaneTab::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 gfx::Size TabbedPaneTab::CalculatePreferredSize() const {
+  NOTREACHED_NORETURN() << "Use CalculatePreferredSize(SizeBounds)";
+}
+
+gfx::Size TabbedPaneTab::CalculatePreferredSize(
+    const SizeBounds& available_size) const {
   int width = preferred_title_width_ + GetInsets().width();
   if (tabbed_pane_->GetStyle() == TabbedPane::TabStripStyle::kHighlight &&
-      tabbed_pane_->GetOrientation() == TabbedPane::Orientation::kVertical)
+      tabbed_pane_->GetOrientation() == TabbedPane::Orientation::kVertical) {
     width = std::max(width, 192);
+  }
   return gfx::Size(width, 32);
 }
 
@@ -144,7 +151,7 @@ int TabbedPaneTab::GetHeightForWidth(int w) const {
   // LayoutManager::GetPreferredHeightForWidth by default, but this is not
   // consistent with the fixed height desired by CalculatePreferredSize, so we
   // override it and call it manually.
-  return CalculatePreferredSize().height();
+  return CalculatePreferredSize(SizeBounds(w, {})).height();
 }
 
 void TabbedPaneTab::GetAccessibleNodeData(ui::AXNodeData* data) {
