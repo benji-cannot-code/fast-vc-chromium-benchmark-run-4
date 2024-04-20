ea2f596789a51a5ed0686f590e4b5fe8a2759ef4
jiawei wang <hellojw513@gmail.com>
2024-04-20 17:59:16 +0000

    Bubble: remove all SizeToContents() in /components
    Remove all SizeToContents() and add InvalidateLayout() when necessary.
    This changes is used to make the bubbles using autosize mode` via
    autosize parameter.(autosize mode: When the preferred size of bubble
    changes,its widget automatically resizes itself to meet the content's
    size)
    
    The ultimate goal of this bug is to remove all SizeToContents() to
    reduce developer confusion about when SizeToContents() needs to be
    called.
    
    This CL was uploaded by git cl split.
    
    R=dpenning@chromium.org, evliu@google.com
    
    Bug: 41493925
    Change-Id: I7853e7f6e5674faf58dc35885141ac8f62350bf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464567
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Auto-Submit: jiawei wang <hellojw513@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1290363}

--- components/live_caption/views/caption_bubble.cc
+++ components/live_caption/views/caption_bubble.cc
@@ -537,7 +537,11 @@ class CaptionBubbleLabelAXModeObserver : public ui::AXModeObserver {
 CaptionBubble::CaptionBubble(PrefService* profile_prefs,
                              const std::string& application_locale,
                              base::OnceClosure destroyed_callback)
-    : profile_prefs_(profile_prefs),
+    : views::BubbleDialogDelegateView(nullptr,
+                                      views::BubbleBorder::TOP_LEFT,
+                                      views::BubbleBorder::DIALOG_SHADOW,
+                                      true),
+      profile_prefs_(profile_prefs),
       destroyed_callback_(std::move(destroyed_callback)),
       application_locale_(application_locale),
       is_expanded_(
@@ -1518,7 +1522,6 @@ void CaptionBubble::UpdateContentSize() {
 void CaptionBubble::Redraw() {
   UpdateBubbleAndTitleVisibility();
   UpdateContentSize();
-  SizeToContents();
 }
 
 void CaptionBubble::ShowInactive() {
