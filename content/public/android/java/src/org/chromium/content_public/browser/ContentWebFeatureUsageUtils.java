8afac8622b9ea9b425c33b53bca75c6892c4c8a8
Peter Kotwicz <pkotwicz@chromium.org>
2024-04-20 02:07:31 +0000

    [RWI] Add UseCounter when user clicks on mdoc:// deep link
    This CL:
    - Adds a use counter for when user clicks on mdoc:// deep link (vs
      going through navigator.identity API)
    - Adds a use counter for when user clicks on openid4vp:// deep link
    - Adds Android.TabNavigationInterceptResult.ForMdoc and
      Android.TabNavigationInterceptResult.ForOpenId4Vp histograms. The
      purpose of the histograms is to track how frequently the external
      app launch triggered by mdoc:// and opendi4vp:// deep links is
      mediated by a Chrome dialog and thus can be canceled.
    
    BUG=334965867
    
    Change-Id: Idf83abd33a710e35619e75863045274fc7b70701
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458335
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290289}

---
+++ content/public/android/java/src/org/chromium/content_public/browser/ContentWebFeatureUsageUtils.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content_public.browser;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.blink.mojom.WebFeature;
+
+/** A Java API for calling ContentBrowserClient::LogWebFeatureForCurrentPage(). */
+@JNINamespace("content")
+public class ContentWebFeatureUsageUtils {
+    public static void logWebFeatureForCurrentPage(
+            WebContents webContents, @WebFeature.EnumType int webFeature) {
+        ContentWebFeatureUsageUtilsJni.get().logWebFeatureForCurrentPage(webContents, webFeature);
+    }
+
+    @NativeMethods
+    public interface Natives {
+        void logWebFeatureForCurrentPage(
+                WebContents webContents, @WebFeature.EnumType int webFeature);
+    }
+}
