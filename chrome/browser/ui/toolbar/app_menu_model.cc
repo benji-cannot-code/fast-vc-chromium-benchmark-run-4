62be6b9126ef1dfea366f7a886e1254f5a7dafa8
Emily Shack <emshack@chromium.org>
2024-04-20 17:02:46 +0000

    Add new tab organization feature flags
    Adds flags for disabling app menu item and adding new tab divider
    
    Bug: b/335921332
    Change-Id: Ifff00b1b03f19f8fdec1de11568e9e0d344702cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5469083
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290358}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -1804,7 +1804,8 @@ void AppMenuModel::Build() {
           kShowSearchCompanion);
     }
 #endif
-    if (TabOrganizationUtils::GetInstance()->IsEnabled(browser_->profile())) {
+    if (base::FeatureList::IsEnabled(features::kTabOrganizationAppMenuItem) &&
+        TabOrganizationUtils::GetInstance()->IsEnabled(browser_->profile())) {
       auto* const tab_organization_service =
           TabOrganizationServiceFactory::GetForProfile(browser_->profile());
       if (tab_organization_service) {
