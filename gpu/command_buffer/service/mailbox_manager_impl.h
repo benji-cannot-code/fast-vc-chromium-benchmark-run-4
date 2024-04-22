868ebb83fa87d319cbce74eebbf97d1a730b3872
Colin Blundell <blundell@chromium.org>
2024-04-22 17:04:06 +0000

    [//gpu] Remove MailboxManager::ProduceTexture()
    This function has no production clients anymore \o/. We also remove
    the entire mailbox_manager_unittest.cc, since all of its tests are of
    production followed by consumption.
    
    Tearing down of consumption will follow, but requires slightly more work
    as there is currently still dead client code that reaches it.
    
    Bug: 40057997
    Change-Id: I9600f5f66871d7ce07443b43ecba86b18d9ec21e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471977
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290730}

--- gpu/command_buffer/service/mailbox_manager_impl.h
+++ gpu/command_buffer/service/mailbox_manager_impl.h
@@ -29,7 +29,6 @@ class GPU_GLES2_EXPORT MailboxManagerImpl : public MailboxManager {
 
   // MailboxManager implementation:
   TextureBase* ConsumeTexture(const Mailbox& mailbox) override;
-  void ProduceTexture(const Mailbox& mailbox, TextureBase* texture) override;
   void TextureDeleted(TextureBase* texture) override;
 
  private:
