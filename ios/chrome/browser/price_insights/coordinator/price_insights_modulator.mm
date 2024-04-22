2cf4746448396434bd228221e7a613ddaeef903a
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-04-22 08:37:15 +0000

    Revert "[PriceInsights][iOS] Add Price History to Price Insights"
    This reverts commit c977f3c714fb6d4b35214610effde08b044f8de9.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5749914871005184
    
    Sample build with failed test: https://ci.chromium.org/b/8750206938930220785
    Affected test(s):
    [ninja://ios/chrome/test/earl_grey2:ios_chrome_bookmarks_eg2tests_module/BookmarksAccessibilityTestCase/testAccessibilityOnBookmarksDeleteUndo](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2:ios_chrome_bookmarks_eg2tests_module%2FBookmarksAccessibilityTestCase%2FtestAccessibilityOnBookmarksDeleteUndo?q=VHash%3A119cb1b045047867)
    [ninja://ios/chrome/test/earl_grey2:ios_chrome_bookmarks_eg2tests_module/BookmarksAccessibilityTestCase/testAccessibilityOnBookmarksEditPage](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2:ios_chrome_bookmarks_eg2tests_module%2FBookmarksAccessibilityTestCase%2FtestAccessibilityOnBookmarksEditPage?q=VHash%3A119cb1b045047867)
    [ninja://ios/chrome/test/earl_grey2:ios_chrome_bookmarks_eg2tests_module/BookmarksAccessibilityTestCase/testAccessibilityOnBookmarksFolderEditPage](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2:ios_chrome_bookmarks_eg2tests_module%2FBookmarksAccessibilityTestCase%2FtestAccessibilityOnBookmarksFolderEditPage?q=VHash%3A119cb1b045047867)
    [ninja://ios/chrome/test/earl_grey2:ios_chrome_bookmarks_eg2tests_module/BookmarksAccessibilityTestCase/testAccessibilityOnBookmarksLandingPage](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2:ios_chrome_bookmarks_eg2tests_module%2FBookmarksAccessibilityTestCase%2FtestAccessibilityOnBookmarksLandingPage?q=VHash%3A119cb1b045047867)
    [ninja://ios/chrome/test/earl_grey2:ios_chrome_bookmarks_eg2tests_module/BookmarksAccessibilityTestCase/testAccessibilityOnBookmarksMovePage](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2:ios_chrome_bookmarks_eg2tests_module%2FBookmarksAccessibilityTestCase%2FtestAccessibilityOnBookmarksMovePage?q=VHash%3A119cb1b045047867)
    and 11 more ...
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5749914871005184&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5454599&type=BUG
    
    Original change's description:
    > [PriceInsights][iOS] Add Price History to Price Insights
    >
    > This CL:
    > - Adds adds a skeleton swiftUI view for price history graph
    > - Hooks up the swift UI view created to Price Insights Cell
    > - Make some small refactor to PriceInsightsCell
    > - Adds priceHistory to PriceInsightsItem
    >
    > Bug: b/333894143
    > Change-Id: I314526038a9c3430d0957d64c85cc79a5d632787
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454599
    > Reviewed-by: Ayman Almadhoun <ayman@chromium.org>
    > Reviewed-by: Gayane Petrosyan <gayane@google.com>
    > Auto-Submit: Cheick Cisse <cheickcisse@google.com>
    > Commit-Queue: Ayman Almadhoun <ayman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1290005}
    >
    
    Bug: b/333894143
    Change-Id: If28ce6a8e5f044cad408742f229bdf646912f3d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5469071
    Reviewed-by: Elmehdi Rahmaoui <erahmaoui@google.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Owners-Override: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290536}

--- ios/chrome/browser/price_insights/coordinator/price_insights_modulator.mm
+++ ios/chrome/browser/price_insights/coordinator/price_insights_modulator.mm
@@ -202,7 +202,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Cell configuration handler helper.
 - (void)configureCell:(PriceInsightsCell*)cell {
-  cell.viewController = self.viewController;
   PriceInsightsItem* item = [[PriceInsightsItem alloc] init];
   [cell configureWithItem:item];
 }
