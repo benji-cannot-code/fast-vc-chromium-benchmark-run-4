d4eb98ffe313fe76f2f4269bb91991080a0f63e6
dpapad <dpapad@chromium.org>
2024-04-22 23:44:01 +0000

    SidePanel: Migrate shared/sp_heading.ts to Lit.
    This is the last dependency of c/b/r/side_panel/reading_list/ that
    transitively pulls in Polymer as a dependency.
    
    Bug: 40943652
    Change-Id: If8cf77d24f85cc7f6e5af8d414dd400c85449b30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5474175
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290962}

--- chrome/browser/resources/side_panel/customize_chrome/themes.ts
+++ chrome/browser/resources/side_panel/customize_chrome/themes.ts
@@ -12,7 +12,7 @@ import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
 import './check_mark_wrapper.js';
 
-import type {SpHeading} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
+import type {SpHeadingElement} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
 import type {HelpBubbleMixinInterface} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin.js';
 import {HelpBubbleMixin} from 'chrome://resources/cr_components/help_bubble/help_bubble_mixin.js';
 import type {CrToggleElement} from 'chrome://resources/cr_elements/cr_toggle/cr_toggle.js';
@@ -38,7 +38,7 @@ const ThemesElementBase = HelpBubbleMixin(PolymerElement) as
 export interface ThemesElement {
   $: {
     refreshDailyToggle: CrToggleElement,
-    heading: SpHeading,
+    heading: SpHeadingElement,
   };
 }
 
