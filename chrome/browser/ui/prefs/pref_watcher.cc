715f8f568826605c9174c0c7973ceae43cd603af
Katie Dektar <katie@chromium.org>
2024-04-22 15:05:59 +0000

    [a11y] Update caret blink interval immediately
    Adds a native theme observer to the pref watcher that
    will be notified when the caret blink interval changes.
    
    Bug: b/259374492
    Change-Id: I4f3097c115399bd848b250e1690cbc2a4b351495
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467975
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290671}

--- chrome/browser/ui/prefs/pref_watcher.cc
+++ chrome/browser/ui/prefs/pref_watcher.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/live_caption/pref_names.h"
 #include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
+#include "ui/native_theme/native_theme.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_pref_names.h"
@@ -85,6 +86,7 @@ PrefWatcher::PrefWatcher(Profile* profile)
   CHECK(tracking_protection_settings_);
   tracking_protection_settings_observation_.Observe(
       tracking_protection_settings_);
+  native_theme_observation_.Observe(ui::NativeTheme::GetInstanceForWeb());
 
   profile_pref_change_registrar_.Init(profile_->GetPrefs());
 
@@ -148,6 +150,11 @@ void PrefWatcher::RegisterRendererPreferenceWatcher(
 void PrefWatcher::Shutdown() {
   profile_pref_change_registrar_.RemoveAll();
   local_state_pref_change_registrar_.RemoveAll();
+}
+
+void PrefWatcher::OnNativeThemeUpdated(
+    ui::NativeTheme* observed_theme) {
+  UpdateRendererPreferences();
 }
 
 void PrefWatcher::OnDoNotTrackEnabledChanged() {
--- chrome/browser/ui/prefs/pref_watcher.h
+++ chrome/browser/ui/prefs/pref_watcher.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/singleton.h"
+#include "base/scoped_observation.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/privacy_sandbox/tracking_protection_settings_observer.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
 #include "third_party/blink/public/mojom/renderer_preference_watcher.mojom.h"
+#include "ui/native_theme/native_theme_observer.h"
 
 class Profile;
 class PrefsTabHelper;
@@ -22,7 +24,8 @@ class PrefsTabHelper;
 // Watches updates in WebKitPreferences and blink::RendererPreferences, and
 // notifies tab helpers and registered watchers of those updates.
 class PrefWatcher : public KeyedService,
-                    public privacy_sandbox::TrackingProtectionSettingsObserver {
+                    public privacy_sandbox::TrackingProtectionSettingsObserver,
+                    public ui::NativeThemeObserver {
  public:
   explicit PrefWatcher(Profile* profile);
   ~PrefWatcher() override;
@@ -37,6 +40,9 @@ class PrefWatcher : public KeyedService,
  private:
   // KeyedService overrides:
   void Shutdown() override;
+
+  // ui::NativeThemeObserver:
+  void OnNativeThemeUpdated(ui::NativeTheme* observed_theme) override;
 
   void OnDoNotTrackEnabledChanged() override;
 
@@ -63,6 +69,9 @@ class PrefWatcher : public KeyedService,
   // preference changes, use |tab_helpers_|.
   mojo::RemoteSet<blink::mojom::RendererPreferenceWatcher>
       renderer_preference_watchers_;
+
+    base::ScopedObservation<ui::NativeTheme, ui::NativeThemeObserver>
+      native_theme_observation_{this};
 };
 
 class PrefWatcherFactory : public ProfileKeyedServiceFactory {
