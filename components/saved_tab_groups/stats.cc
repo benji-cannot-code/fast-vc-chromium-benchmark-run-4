4df8beb1f2d07c0f6dfbb6a099e20c320ed7fa4b
Yuheng Huang <yuhengh@chromium.org>
2024-04-22 23:45:45 +0000

    [tab group v2] Add metrics for pin/unpin group
    New Actions:
    TabGroups_SavedTabGroups_Unpinned
    TabGroups_SavedTabGroups_Pinned
    
    New metrics:
    TabGroups.SavedTabGroupPinnedCount
    
    Bug: 333792982
    Change-Id: If66c0f59ef58dec97f9f6feca012f55aadf7ed65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5456000
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Reviewed-by: pengchao Cai <pengchaocai@chromium.org>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290964}

--- components/saved_tab_groups/stats.cc
+++ components/saved_tab_groups/stats.cc
@@ -21,6 +21,8 @@ void RecordSavedTabGroupMetrics(SavedTabGroupModel* model) {
 
   const base::Time current_time = base::Time::Now();
 
+  int pinned_group_count = 0;
+
   for (const SavedTabGroup& group : model->saved_tab_groups()) {
     base::UmaHistogramCounts10000("TabGroups.SavedTabGroupTabCount",
                                   group.saved_tabs().size());
@@ -50,7 +52,14 @@ void RecordSavedTabGroupMetrics(SavedTabGroupModel* model) {
           "TabGroups.SavedTabGroupTabTimeSinceModification",
           duration_since_tab_modification.InMinutes());
     }
+
+    if (group.is_pinned()) {
+      ++pinned_group_count;
+    }
   }
+
+  base::UmaHistogramCounts10000("TabGroups.SavedTabGroupPinnedCount",
+                                pinned_group_count);
 }
 
 void RecordTabCountMismatchOnConnect(size_t tabs_in_saved_group,
