6f43795e0f5ed17dff36ecd1e2e25b74380b9a08
Md Hasibul Hasan <hasibulhasan873@gmail.com>
2024-04-22 11:15:24 +0000

    [media] Convert base::StringPiece to std::string_view
    Bug: 40506050
    Change-Id: I6c42192408ce94c51f408a2743c8657b03d90570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466388
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290593}

--- media/capture/video/fake_video_capture_device_factory.cc
+++ media/capture/video/fake_video_capture_device_factory.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/fake_video_capture_device_factory.h"
 
 #include <array>
+#include <string_view>
 
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
@@ -294,7 +295,7 @@ void FakeVideoCaptureDeviceFactory::ParseFakeDevicesConfigFromOptionsString(
       FakeVideoCaptureDevice::DisplayMediaType::ANY;
 
   while (option_tokenizer.GetNext()) {
-    std::vector<base::StringPiece> param = base::SplitStringPiece(
+    std::vector<std::string_view> param = base::SplitStringPiece(
         option_tokenizer.token_piece(), "=", base::TRIM_WHITESPACE,
         base::SPLIT_WANT_NONEMPTY);
 
