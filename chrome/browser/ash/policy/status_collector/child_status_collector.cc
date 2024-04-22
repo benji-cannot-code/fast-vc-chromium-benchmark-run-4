fc0511df9cba3d2c75bfe8f17c809304c4689ef2
Henrique Ferreiro <hferreiro@igalia.com>
2024-04-22 08:47:46 +0000

    Remove unused policy::IntervalMap
    Change-Id: I236452b3d3f609e06c6bac6c919a7c247699185e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465715
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Henrique Ferreiro <hferreiro@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1290544}

--- chrome/browser/ash/policy/status_collector/child_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/child_status_collector.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "base/values.h"
 #include "chrome/browser/ash/policy/status_collector/child_activity_storage.h"
-#include "chrome/browser/ash/policy/status_collector/interval_map.h"
 #include "chrome/browser/ash/policy/status_collector/status_collector_state.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
--- chrome/browser/ash/policy/status_collector/device_status_collector.cc
+++ chrome/browser/ash/policy/status_collector/device_status_collector.cc
@@ -59,7 +59,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/device_local_account.h"
 #include "chrome/browser/ash/policy/core/reporting_user_tracker.h"
 #include "chrome/browser/ash/policy/status_collector/enterprise_activity_storage.h"
-#include "chrome/browser/ash/policy/status_collector/interval_map.h"
 #include "chrome/browser/ash/policy/status_collector/status_collector_state.h"
 #include "chrome/browser/ash/policy/status_collector/tpm_status_combiner.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
