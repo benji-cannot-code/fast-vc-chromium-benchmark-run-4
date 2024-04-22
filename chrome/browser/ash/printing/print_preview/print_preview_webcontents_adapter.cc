dab2f16bd9eb7da4a5831b03bbe21d7056e41ee6
Jimmy <jimmyxgong@chromium.org>
2024-04-22 21:23:14 +0000

    cros-preview: Add PrintPreviewDelegate and stub impl
    PrintPreviewWebcontentsAdapter implements PrintPreviewDelegate
    which allows //ash to be able to reach //chrome/browser/ash.
    
    This is an initial stub implementation.
    
    Bug: 334993067
    Change-Id: I7f91e8421d913ece281d2856b0d4c90f8cbe17fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468319
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290893}

---
+++ chrome/browser/ash/printing/print_preview/print_preview_webcontents_adapter.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/printing/print_preview/print_preview_webcontents_adapter.h"
+
+#include "base/unguessable_token.h"
+
+namespace ash::printing {
+
+// TODO(jimmyxgong): Implement stub.
+void PrintPreviewWebcontentsAdapter::StartGetPreview(
+    base::UnguessableToken token) {}
+
+}  // namespace ash::printing
