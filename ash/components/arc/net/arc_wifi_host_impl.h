aa276e2c09793b4769b8e003677e0069c9651b81
Chuwei He <chuweih@google.com>
2024-04-22 03:30:37 +0000

    ARC: Add implementation for ArcWifiHostImpl methods
    This patch added implementation for initialization related methods and
    several WiFi state related methods (Get/Set/Notify Change) in
    ArcWifiHostImpl.
    
    Bug: b/329552433
    Test: build
    Change-Id: I00676433996c7522c19d403fae161b5686fd6356
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394535
    Commit-Queue: Chuwei He <chuweih@google.com>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290485}

--- ash/components/arc/net/arc_wifi_host_impl.h
+++ ash/components/arc/net/arc_wifi_host_impl.h
@@ -51,6 +51,8 @@ class ArcWifiHostImpl : public KeyedService,
  private:
   const raw_ptr<ArcBridgeService>
       arc_bridge_service_;  // Owned by ArcServiceManager.
+
+  THREAD_CHECKER(thread_checker_);
 };
 
 }  // namespace arc
--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/metrics/arc_metrics_service.h"
 #include "ash/components/arc/midis/arc_midis_bridge.h"
 #include "ash/components/arc/net/arc_net_host_impl.h"
+#include "ash/components/arc/net/arc_wifi_host_impl.h"
 #include "ash/components/arc/obb_mounter/arc_obb_mounter_bridge.h"
 #include "ash/components/arc/pay/arc_digital_goods_bridge.h"
 #include "ash/components/arc/pay/arc_payment_app_bridge.h"
@@ -327,6 +328,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcVolumeMounterBridge::GetForBrowserContext(profile);
   ArcWakeLockBridge::GetForBrowserContext(profile);
   ArcWallpaperService::GetForBrowserContext(profile);
+  ArcWifiHostImpl::GetForBrowserContext(profile);
   GpuArcVideoKeyedService::GetForBrowserContext(profile);
   CertStoreService::GetForBrowserContext(profile);
   apps::ArcAppsFactory::GetForProfile(profile);
@@ -510,6 +512,7 @@ void ArcServiceLauncher::EnsureFactoriesBuilt() {
   ArcVolumeMounterBridge::EnsureFactoryBuilt();
   ArcWakeLockBridge::EnsureFactoryBuilt();
   ArcWallpaperService::EnsureFactoryBuilt();
+  ArcWifiHostImpl::EnsureFactoryBuilt();
   CertStoreService::EnsureFactoryBuilt();
   GpuArcVideoKeyedService::EnsureFactoryBuilt();
   input_overlay::ArcInputOverlayManager::EnsureFactoryBuilt();
