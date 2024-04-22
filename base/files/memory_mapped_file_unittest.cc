f4595b7d89ba863f2127000e0be05683ef0157a7
Jack Yammine <jyammine@google.com>
2024-04-22 07:56:40 +0000

    [CodeHealth]Use HeapArray in base/files/memory_mapped_file_unittest.cc
    Fixed: b:326458552
    Change-Id: I74dd1e35a8c3bb7cf82eb3452d44d8c96098b519
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465562
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290531}

--- base/files/memory_mapped_file_unittest.cc
+++ base/files/memory_mapped_file_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -20,17 +22,17 @@ namespace base {
 namespace {
 
 // Create a temporary buffer and fill it with a watermark sequence.
-std::unique_ptr<uint8_t[]> CreateTestBuffer(size_t size, size_t offset) {
-  std::unique_ptr<uint8_t[]> buf(new uint8_t[size]);
+base::HeapArray<uint8_t> CreateTestBuffer(size_t size, size_t offset) {
+  auto buf = base::HeapArray<uint8_t>::Uninit(size);
   for (size_t i = 0; i < size; ++i)
-    buf.get()[i] = static_cast<uint8_t>((offset + i) % 253);
+    buf[i] = static_cast<uint8_t>((offset + i) % 253);
   return buf;
 }
 
 // Check that the watermark sequence is consistent with the |offset| provided.
 bool CheckBufferContents(span<const uint8_t> bytes, size_t offset) {
-  std::unique_ptr<uint8_t[]> test_data(CreateTestBuffer(bytes.size(), offset));
-  return memcmp(test_data.get(), bytes.data(), bytes.size()) == 0;
+  base::HeapArray<uint8_t> test_data(CreateTestBuffer(bytes.size(), offset));
+  return test_data.as_span() == bytes;
 }
 
 class MemoryMappedFileTest : public PlatformTest {
@@ -47,9 +49,8 @@ class MemoryMappedFileTest : public PlatformTest {
               File::FLAG_CREATE_ALWAYS | File::FLAG_READ | File::FLAG_WRITE);
     EXPECT_TRUE(file.IsValid());
 
-    std::unique_ptr<uint8_t[]> test_data(CreateTestBuffer(size, 0));
-    size_t bytes_written =
-        file.Write(0, reinterpret_cast<char*>(test_data.get()), size);
+    base::HeapArray<uint8_t> test_data(CreateTestBuffer(size, 0));
+    size_t bytes_written = file.Write(0, test_data.as_span()).value();
     EXPECT_EQ(size, bytes_written);
     file.Close();
   }
