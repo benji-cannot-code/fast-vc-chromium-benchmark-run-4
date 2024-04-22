96ee0d786699602db63269b87d59dfa3018f8fb1
mei1127 <shanxing.mei@intel.com>
2024-04-22 01:57:54 +0000

    Reland "webnn: Migrate Conv2d validation tests to WPTs"
    This is a reland of commit 93d2ab44dcde13d2b3ba9c408c75877489e03d49
    
    The failing tests have been disabled because:
    - I suspect testing output overflow requires setting the large input
    - value according to the system.
    
    Original change's description:
    > webnn: Migrate Conv2d validation tests to WPTs
    >
    > This CL adds WPT tests for Conv2d and removes the unit tests
    > `MLGraphBuilderTest.Conv2dTest`, `MLGraphTestMojo.Conv2dTest` and
    > `MLGraphTest.Conv2dTest`.
    >
    > Bug: 327337526, 328026885
    > Change-Id: I2328bd2c9f9056d8cef562f45d479606545e41ba
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5413394
    > Reviewed-by: Austin Sullivan <asully@chromium.org>
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Commit-Queue: Shanxing Mei <shanxing.mei@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1287852}
    
    Bug: 327337526, 328026885
    Change-Id: If067dbd8d825163a67907ea1995ea9ff1043904f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5460412
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Shanxing Mei <shanxing.mei@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1290469}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder.cc
@@ -963,8 +963,7 @@ MLOperand* MLGraphBuilder::conv2d(const MLOperand* input,
 
   auto conv2d_attributes = ConvertToConv2dAttributes(options);
   if (!conv2d_attributes.has_value()) {
-    exception_state.ThrowDOMException(DOMExceptionCode::kDataError,
-                                      conv2d_attributes.error());
+    exception_state.ThrowTypeError(conv2d_attributes.error());
     return nullptr;
   }
 
@@ -972,9 +971,7 @@ MLOperand* MLGraphBuilder::conv2d(const MLOperand* input,
       ConvertToComponentOperand(input), ConvertToComponentOperand(filter),
       std::move(conv2d_attributes.value()));
   if (!validated_output.has_value()) {
-    exception_state.ThrowDOMException(
-        DOMExceptionCode::kDataError,
-        WTF::String::FromUTF8(validated_output.error()));
+    exception_state.ThrowTypeError(String::FromUTF8(validated_output.error()));
     return nullptr;
   }
   // Create conv2d operator and its output operand. Connect the conv2d operator
@@ -986,8 +983,7 @@ MLOperand* MLGraphBuilder::conv2d(const MLOperand* input,
       this, ComponentOperandTypeToBlink(validated_output.value().data_type),
       Vector<uint32_t>(validated_output.value().dimensions), conv2d);
   if (!output.has_value()) {
-    exception_state.ThrowDOMException(DOMExceptionCode::kDataError,
-                                      output.error());
+    exception_state.ThrowTypeError(output.error());
     return nullptr;
   }
   conv2d->Connect(std::move(inputs), {output.value()});
