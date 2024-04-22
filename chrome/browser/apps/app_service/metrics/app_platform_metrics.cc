04633fd5071f07f4d33ee5b12001f7d290e096f9
Joel Hockey <joelhockey@chromium.org>
2024-04-22 06:34:52 +0000

    Add Mall as a DefaultAppName
    Add Mall as a DefaultAppName and remove web_applications import from
    app_platform_metrics
    
    Bug: 330641558
    Change-Id: Ifee53cb8dab5451ab510b90672f530a8c3283ea8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5469933
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290516}

--- chrome/browser/apps/app_service/metrics/app_platform_metrics.cc
+++ chrome/browser/apps/app_service/metrics/app_platform_metrics.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/guest_os/guest_os_shelf_utils.h"
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/web_applications/web_app_id_constants.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chromeos/components/mgs/managed_guest_session_utils.h"
 #include "components/app_constants/constants.h"
@@ -1217,7 +1216,7 @@ void AppPlatformMetrics::RecordAppsUsageTimeUkm() {
       }
 
       // UMA for Mall app.
-      if (it.second.app_id == web_app::kMallAppId) {
+      if (AppIdToName(it.second.app_id) == DefaultAppName::kMall) {
         base::UmaHistogramCustomTimes("Apps.AppDiscovery.MallUsageTime",
                                       it.second.running_time, base::Seconds(1),
                                       base::Hours(2), 100);
