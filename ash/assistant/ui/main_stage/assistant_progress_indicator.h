43377d270c65908aaa55234743e864139333f4de
weidongliu <weidongliu@chromium.org>
2024-04-22 15:34:07 +0000

    views: use CalculatePreferredSize(SizeBounds) in //ash/assistant.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: I4ac688522f72684f51cf4a6d3dc55fa81984a349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465194
    Reviewed-by: Eric Sum <esum@google.com>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290683}

--- ash/assistant/ui/main_stage/assistant_progress_indicator.h
+++ ash/assistant/ui/main_stage/assistant_progress_indicator.h
@@ -26,7 +26,8 @@ class COMPONENT_EXPORT(ASSISTANT_UI) AssistantProgressIndicator
   ~AssistantProgressIndicator() override;
 
   // views::View:
-  gfx::Size CalculatePreferredSize() const override;
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override;
   int GetHeightForWidth(int width) const override;
   void AddedToWidget() override;
   void RemovedFromWidget() override;
