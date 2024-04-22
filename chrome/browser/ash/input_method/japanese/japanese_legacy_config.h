70197a2b41446a9f8fc3893d22dcad1e2a9cd570
jhtin <jhtin@chromium.org>
2024-04-22 08:39:59 +0000

    Jp SystemPk: Make japanese legacy config write all fields to prefs
    On Activate if the JpSystemPk flag is enabled, we need to write all
    japanese legacy config fields to prefs to migrate the old settings over.
    This is already done but only for one field (InputMode). I added a
    helper function to add the rest of the fields.
    
    Bug: b/333041130
    Change-Id: Ibefb6be56158c0a6b633a88ee5d925d432d56a6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465546
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Bao-Duy Tran <tranbaoduy@chromium.org>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290538}

---
+++ chrome/browser/ash/input_method/japanese/japanese_legacy_config.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_INPUT_METHOD_JAPANESE_JAPANESE_LEGACY_CONFIG_H_
+#define CHROME_BROWSER_ASH_INPUT_METHOD_JAPANESE_JAPANESE_LEGACY_CONFIG_H_
+
+#include "base/containers/fixed_flat_map.h"
+#include "base/values.h"
+#include "chromeos/ash/services/ime/public/mojom/user_data/japanese_legacy_config.mojom.h"
+
+namespace ash::input_method {
+
+// Fills a dictionary with all the prefs that are set in the
+// JapaneseLegacyConfig using the prefs constants expected by the settings app.
+base::Value::Dict CreatePrefsDictFromJapaneseLegacyConfig(
+    ash::ime::mojom::JapaneseLegacyConfigPtr legacy_config);
+
+}  // namespace ash::input_method
+
+#endif  // CHROME_BROWSER_ASH_INPUT_METHOD_JAPANESE_JAPANESE_LEGACY_CONFIG_H_
--- chrome/browser/ash/input_method/native_input_method_engine_observer.cc
+++ chrome/browser/ash/input_method/native_input_method_engine_observer.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/autocorrect_prefs.h"
 #include "chrome/browser/ash/input_method/input_method_quick_settings_helpers.h"
 #include "chrome/browser/ash/input_method/input_method_settings.h"
+#include "chrome/browser/ash/input_method/japanese/japanese_legacy_config.h"
 #include "chrome/browser/ash/input_method/suggestion_enums.h"
 #include "chrome/browser/ash/input_method/ui/input_method_menu_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -776,21 +777,14 @@ void NativeInputMethodEngineObserver::OnFocusAck(
 
 void NativeInputMethodEngineObserver::SetJapanesePrefsFromLegacyConfig(
     mojom::JapaneseLegacyConfigResponsePtr response) {
-  // TODO(b/333041130): Add remaining values.
-  // Move this setting prefs logic to its own separate file.
-  base::Value::Dict values;
-
-  mojom::JapaneseLegacyConfig::PreeditMethod preedit =
-      response->get_response()->preedit_method;
-  if (preedit == mojom::JapaneseLegacyConfig::PreeditMethod::kKana) {
-    values.Set("JapaneseInputMode", "Kana");
-  }
-  if (preedit == mojom::JapaneseLegacyConfig::PreeditMethod::kRomaji) {
-    values.Set("JapaneseInputMode", "Romaji");
+  if (!response->is_response()) {
+    return;
   }
 
-  SetLanguageInputMethodSpecificSetting(*prefs_, kJapanesePrefsEngineId,
-                                        values);
+  SetLanguageInputMethodSpecificSetting(
+      *prefs_, kJapanesePrefsEngineId,
+      CreatePrefsDictFromJapaneseLegacyConfig(
+          std::move(response->get_response())));
 }
 
 void NativeInputMethodEngineObserver::OnActivate(const std::string& engine_id) {
