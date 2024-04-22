38f2b0e332a96f2a573105ff62a4159190e617f7
Ben Reich <benreich@chromium.org>
2024-04-22 23:59:21 +0000

    [FspCache] Plumb the whole EntryMetadata via OpenFileCallback
    In preparation for passing the size via the `OpenFileCallback`, update
    the existing to not just retrieve the `CloudFileInfo` but to pass the
    whole `EntryMetadata` via the `OpenFileCallback`. The IDL was updated in
    CL:5343674 to include `EntryMetadata` already, however, only the
    `CloudFileInfo` was respected.
    
    Bug: b/335548899
    Change-Id: If4e9c8a886a4bde3a2b40cba93153a1b9e31c5ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465158
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290971}

--- chrome/browser/ash/file_system_provider/operations/open_file_unittest.cc
+++ chrome/browser/ash/file_system_provider/operations/open_file_unittest.cc
@@ -38,10 +38,10 @@ class CallbackLogger {
    public:
     Event(int file_handle,
           base::File::Error result,
-          std::unique_ptr<CloudFileInfo> cloud_file_info)
+          std::unique_ptr<EntryMetadata> metadata)
         : file_handle_(file_handle),
           result_(result),
-          cloud_file_info_(std::move(cloud_file_info)) {}
+          metadata_(std::move(metadata)) {}
 
     Event(const Event&) = delete;
     Event& operator=(const Event&) = delete;
@@ -50,12 +50,12 @@ class CallbackLogger {
 
     int file_handle() { return file_handle_; }
     base::File::Error result() { return result_; }
-    CloudFileInfo* cloud_file_info() { return cloud_file_info_.get(); }
+    EntryMetadata* metadata() { return metadata_.get(); }
 
    private:
     int file_handle_;
     base::File::Error result_;
-    std::unique_ptr<CloudFileInfo> cloud_file_info_;
+    std::unique_ptr<EntryMetadata> metadata_;
   };
 
   CallbackLogger() = default;
@@ -67,9 +67,9 @@ class CallbackLogger {
 
   void OnOpenFile(int file_handle,
                   base::File::Error result,
-                  std::unique_ptr<CloudFileInfo> cloud_file_info) {
-    events_.push_back(std::make_unique<Event>(file_handle, result,
-                                              std::move(cloud_file_info)));
+                  std::unique_ptr<EntryMetadata> metadata) {
+    events_.push_back(
+        std::make_unique<Event>(file_handle, result, std::move(metadata)));
   }
 
   std::vector<std::unique_ptr<Event>>& events() { return events_; }
