03ce3e9228b4b46d423514553fc467088eb1eac9
Jack Yammine <jyammine@google.com>
2024-04-22 15:28:29 +0000

    [CodeHealth]Use HeapArray in components/zucchini/fuzzers/raw_gen_fuzzer.cc
    Fixed: b:326458310
    Change-Id: I75a26f877967dfab082f27339c474cab8ae8a2f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463510
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290682}

--- components/zucchini/fuzzers/raw_gen_fuzzer.cc
+++ components/zucchini/fuzzers/raw_gen_fuzzer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iostream>
 #include <memory>
 
+#include "base/containers/heap_array.h"
 #include "base/environment.h"
 #include "base/logging.h"
 #include "components/zucchini/buffer_sink.h"
@@ -67,7 +68,7 @@ DEFINE_BINARY_PROTO_FUZZER(const zucchini::fuzzers::FilePair& file_pair) {
   CHECK_LE(patch_size, kMaxImageSize * 2);
 
   // Write to buffer to avoid IO.
-  std::unique_ptr<uint8_t[]> patch_data(new uint8_t[patch_size]);
-  zucchini::BufferSink patch(patch_data.get(), patch_size);
+  auto patch_data = base::HeapArray<uint8_t>::Uninit(patch_size);
+  zucchini::BufferSink patch(patch_data.data(), patch_data.size());
   patch_writer.SerializeInto(patch);
 }
