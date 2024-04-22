bed2b88e04b034f5ee8268662aaf3222020ee36a
Katie Dektar <katie@chromium.org>
2024-04-22 15:37:07 +0000

    [A11y] Add UMA metrics for caret blink interval
    AX-Relnotes: N/A
    Bug: b/259374492
    Change-Id: Iec13927ed696b4a422da6631df3fce260f8024d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464000
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290685}

--- chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
@@ -1367,6 +1367,10 @@ bool AccessibilitySection::LogMetric(mojom::Setting setting,
           "ChromeOS.Settings.Accessibility.ColorCorrection.FilterAmount",
           value.GetInt());
       return true;
+    case mojom::Setting::kCaretBlinkInterval:
+      base::UmaHistogramSparse(
+          "ChromeOS.Settings.Accessibility.CaretBlinkInterval", value.GetInt());
+      return true;
     case mojom::Setting::kLiveCaption:
       base::UmaHistogramBoolean(
           "ChromeOS.Settings.Accessibility.LiveCaption.Enabled",
