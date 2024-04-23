4c01717d531e8beb6ac93542d3f80d14e6e50094
Jose Dapena Paz <jdapena@igalia.com>
2024-04-23 16:20:20 +0000

    IWYU: missing include for std::optional usage in property_tree_manager.h
    Bug: 41455655
    Change-Id: I03e47559778570c4eee44f8268daa13c545fcca6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5475523
    Reviewed-by: Aaron Krajeski <aaronhk@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1291329}

--- third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.h
+++ third_party/blink/renderer/platform/graphics/compositing/property_tree_manager.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_COMPOSITING_PROPERTY_TREE_MANAGER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_COMPOSITING_PROPERTY_TREE_MANAGER_H_
 
+#include <optional>
+
 #include "base/memory/raw_ptr_exclusion.h"
 #include "cc/layers/layer_collections.h"
 #include "third_party/blink/renderer/platform/graphics/compositor_element_id.h"
