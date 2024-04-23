c0da24a7dac29ae7ee852a3634bad42aa7b785d3
Brandon Wylie <wylieb@chromium.org>
2024-04-23 21:38:15 +0000

    [declutter] Disable killswitch before fixing native tests
    Bug: 336581953,335817729
    Change-Id: I391740d1d641cea91d3b1a90d2b49f65e9245360
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478649
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Owners-Override: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291551}

--- chrome/android/junit/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreIntegrationTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/app/tabmodel/TabPersistentStoreIntegrationTest.java
@@ -37,8 +37,10 @@ import org.chromium.base.task.PostTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features;
+import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.homepage.HomepageManager;
 import org.chromium.chrome.browser.ntp.RecentlyClosedBridge;
 import org.chromium.chrome.browser.ntp.RecentlyClosedBridgeJni;
@@ -75,6 +77,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 /** Tests for TabPersistentStore reacting to events from TabModel and Tab. */
 @RunWith(BaseRobolectricTestRunner.class)
 @LooperMode(Mode.PAUSED)
+@DisableFeatures({
+    ChromeFeatureList.ANDROID_TAB_DECLUTTER,
+    ChromeFeatureList.ANDROID_TAB_DECLUTTER_RESCUE_KILLSWITCH
+})
 public class TabPersistentStoreIntegrationTest {
     /** Shadow for {@link HomepageManager}. */
     @Implements(HomepageManager.class)
@@ -157,7 +163,6 @@ public class TabPersistentStoreIntegrationTest {
         TabWindowManagerSingleton.resetTabModelSelectorFactoryForTesting();
 
         TabStateExtractor.resetTabStatesForTesting();
-        ArchivedTabModelOrchestrator.destroyProfileKeyedMapForTesting();
     }
 
     @Test
