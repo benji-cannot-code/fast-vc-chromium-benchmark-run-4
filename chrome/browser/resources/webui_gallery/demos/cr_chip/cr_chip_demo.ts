4096677e981b449f88abd5f43093d23e72b95fec
rbpotter <rbpotter@chromium.org>
2024-04-23 21:22:25 +0000

    WebUI Gallery: Use cr-icon/icons_lit
    Encourage the use of cr-icon rather than iron-icon and icons_lit.html.js
    rather than icons.html.js by using these consistently throughout the
    WebUI gallery demos.
    
    Bug: 40943652
    Change-Id: I9a5465d7c2cd6e4933d555f1b982b3a8037b667d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477587
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291542}

--- chrome/browser/resources/webui_gallery/demos/cr_chip/cr_chip_demo.ts
+++ chrome/browser/resources/webui_gallery/demos/cr_chip/cr_chip_demo.ts
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '//resources/cr_elements/cr_chip/cr_chip.js';
-import '//resources/cr_elements/icons.html.js';
-import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/cr_elements/cr_icon/cr_icon.js';
+import '//resources/cr_elements/icons_lit.html.js';
 import '../demo.css.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
