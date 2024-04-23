f90e89f5d08d11422b2e3bea4ea344aa9855babd
Andrew Rayskiy <greengrape@google.com>
2024-04-23 18:01:33 +0000

    [web_package] Support ECDSA P-256 SHA-256 signing scheme in tests
    This CL introduces a new EcdsaP256KeyPair subtype to WebBundleSigner to
    simplify end-to-end testing for web bundles signed using ECDSA P-256
    SHA-256.
    
    To make test results reproducible, the signing process relies on a
    predefined nonce instead of generating a random one each time.
    
    DD: go/ecdsa-p256-swbn
    
    Bug: 330378455
    Change-Id: I5b006726ae5cb3a82abea9e61ae6278407ecee11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5457394
    Reviewed-by: Giovanni Pezzino <giovax@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Christian Flach <cmfcmf@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291419}

--- components/web_package/test_support/signed_web_bundles/web_bundle_signer.h
+++ components/web_package/test_support/signed_web_bundles/web_bundle_signer.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/enum_set.h"
 #include "base/containers/span.h"
 #include "components/cbor/values.h"
+#include "components/web_package/signed_web_bundles/ecdsa_p256_public_key.h"
 #include "components/web_package/signed_web_bundles/ed25519_public_key.h"
 
 namespace web_package {
@@ -85,7 +86,31 @@ class WebBundleSigner {
     bool produce_invalid_signature;
   };
 
-  using KeyPair = absl::variant<Ed25519KeyPair>;
+  struct EcdsaP256KeyPair {
+    static EcdsaP256KeyPair CreateRandom(
+        bool produce_invalid_signature = false);
+
+    EcdsaP256KeyPair(
+        base::span<const uint8_t, EcdsaP256PublicKey::kLength> public_key_bytes,
+        base::span<const uint8_t, 32> private_key_bytes,
+        bool produce_invalid_signature = false);
+
+    EcdsaP256KeyPair(const EcdsaP256KeyPair&);
+    EcdsaP256KeyPair& operator=(const EcdsaP256KeyPair&);
+
+    EcdsaP256KeyPair(EcdsaP256KeyPair&&) noexcept;
+    EcdsaP256KeyPair& operator=(EcdsaP256KeyPair&&) noexcept;
+
+    ~EcdsaP256KeyPair();
+
+    EcdsaP256PublicKey public_key;
+    // We don't have a wrapper for private keys since they are only used in
+    // tests.
+    std::array<uint8_t, 32> private_key;
+    bool produce_invalid_signature;
+  };
+
+  using KeyPair = absl::variant<Ed25519KeyPair, EcdsaP256KeyPair>;
 
   // Creates an integrity block with the given signature stack entries.
   static cbor::Value CreateIntegrityBlock(
