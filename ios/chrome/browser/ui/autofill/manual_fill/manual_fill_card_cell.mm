3ef8d9e0896616b86d755667f005513b0a547242
Noémie St-Onge <noemies@google.com>
2024-04-23 20:11:32 +0000

    [iOSKAUpgrade] Add new manual fill card cell header
    This CL adds the new Keyboard Accessory Upgrade header to the manual
    fill card cells. A separator is also added to delimit the header from
    the rest of the content.
    
    Before/After comparison when the KA Upgrade feature is disabled (there
    should be no changes):
    https://screenshot.googleplex.com/5Feri5vzJ5tbh4b.png
    
    Before/After comparison when the KA Upgrade feature is enabled:
    https://screenshot.googleplex.com/B6ijzPLEiXVjNGZ.png
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 326412939, 326414019
    Change-Id: If95d112be220671ab0e6f9cb407e01d8b207a143
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473964
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291495}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_card_cell.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_card_cell.mm
@@ -78,6 +78,10 @@ using base::SysNSStringToUTF8;
 @property(nonatomic, strong)
     NSMutableArray<NSLayoutConstraint*>* dynamicConstraints;
 
+// The view displayed at the top the cell containing the card icon, the card
+// label and a 3-dot menu button.
+@property(nonatomic, strong) UIView* headerView;
+
 // The label with bank name and network.
 @property(nonatomic, strong) UILabel* cardLabel;
 
@@ -119,6 +123,11 @@ using base::SysNSStringToUTF8;
 
 // Layout guide for the cell's content.
 @property(nonatomic, strong) UILayoutGuide* layoutGuide;
+
+// Separator line. When the Keyboard Accessory Upgrade feature is enbaled, used
+// to delimit the header from the rest of the cell. When disabled, used to
+// delimit cells.
+@property(nonatomic, strong) UIView* grayLine;
 
 @end
 
@@ -190,10 +199,14 @@ using base::SysNSStringToUTF8;
 
   // Create the UIViews, add them to the contentView.
   self.cardLabel = CreateLabel();
-  [self.contentView addSubview:self.cardLabel];
   self.cardIcon = [[UIImageView alloc] init];
   self.cardIcon.translatesAutoresizingMaskIntoConstraints = NO;
-  [self.contentView addSubview:self.cardIcon];
+  [self.cardIcon setContentHuggingPriority:UILayoutPriorityDefaultHigh
+                                   forAxis:UILayoutConstraintAxisHorizontal];
+  self.headerView = CreateHeaderView(self.cardIcon, self.cardLabel);
+  [self.contentView addSubview:self.headerView];
+  self.grayLine = CreateGraySeparatorForContainer(self.contentView);
+
   UILabel* expirationDateSeparatorLabel;
 
   // If Virtual Cards are enabled, create UIViews with the labeled chips,
@@ -243,16 +256,10 @@ using base::SysNSStringToUTF8;
 
 // Horizontally positions the UIViews.
 - (void)horizontallyArrangeViews:(UILabel*)expirationDateSeparatorLabel {
-  CreateGraySeparatorForContainer(self.contentView);
-
   NSMutableArray<NSLayoutConstraint*>* staticConstraints =
       [[NSMutableArray alloc] init];
-  AppendHorizontalConstraintsForViews(
-      staticConstraints, @[ self.cardIcon, self.cardLabel ], self.layoutGuide);
-  [NSLayoutConstraint activateConstraints:@[
-    [self.cardIcon.centerYAnchor
-        constraintEqualToAnchor:self.cardLabel.centerYAnchor]
-  ]];
+  AppendHorizontalConstraintsForViews(staticConstraints, @[ self.headerView ],
+                                      self.layoutGuide);
 
   // If Virtual Cards are enabled, position the labeled chips, else position the
   // regular buttons.
@@ -367,9 +374,14 @@ using base::SysNSStringToUTF8;
   // Holds the views whose leading anchor is constrained relative to the cell's
   // leading anchor.
   std::vector<ManualFillCellView> verticalLeadViews;
-  AddViewToVerticalLeadViews(self.cardLabel,
+  AddViewToVerticalLeadViews(self.headerView,
                              ManualFillCellView::ElementType::kOther,
                              verticalLeadViews);
+  if (IsKeyboardAccessoryUpgradeEnabled()) {
+    AddViewToVerticalLeadViews(self.grayLine,
+                               ManualFillCellView::ElementType::kSeparator,
+                               verticalLeadViews);
+  }
 
   // Holds the chip buttons related to the card that are vertical leads.
   NSMutableArray<UIView*>* cardInfoGroupVerticalLeadChips =
