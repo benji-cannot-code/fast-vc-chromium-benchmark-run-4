fdafbdddce9bc72b153dc2e77a8ec44c84293e6d
Thomas Lukaszewicz <tluk@chromium.org>
2024-04-23 09:26:01 +0000

    [lacros] Ensure display for new window set correctly for profile picker
    LacrosService takes a display_id argument when creating profile picker
    windows, however currently this information is discarded when creating
    new profile picker windows.
    
    This CL ensures the display for new windows is set correctly and new
    windows are positioned on the correct display.
    
    Bug: 333312496
    Change-Id: I9239fa8f318d3442bb79cae968162ce9a3f66683
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473097
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291173}

--- chrome/browser/lacros/browser_service_lacros.cc
+++ chrome/browser/lacros/browser_service_lacros.cc
@@ -126,7 +126,8 @@ Browser* FindBrowserWithTabId(const std::string& tab_id_str) {
 }
 
 // The return value indicates whether the profile picker was shown.
-bool ShowProfilePickerIfNeeded(bool incognito) {
+bool ShowProfilePickerIfNeeded(bool incognito,
+                               std::optional<int64_t> target_display_id) {
   if (StartupProfileModeFromReason(ProfilePicker::GetStartupModeReason()) ==
           StartupProfileMode::kProfilePicker &&
       chrome::GetTotalBrowserCount() == 0 && !incognito) {
@@ -137,6 +138,10 @@ bool ShowProfilePickerIfNeeded(bool incognito) {
     // default behavior for the first browser window supports session restore,
     // additional windows are opened blank and thus it works reasonably well for
     // BrowserServiceLacros.
+    std::optional<display::ScopedDisplayForNewWindows> scoped_display;
+    if (target_display_id.has_value()) {
+      scoped_display.emplace(target_display_id.value());
+    }
     ProfilePicker::Show(ProfilePicker::Params::FromEntryPoint(
         ProfilePicker::EntryPoint::kNewSessionOnExistingProcess));
     return true;
@@ -224,7 +229,7 @@ void BrowserServiceLacros::NewWindow(bool incognito,
     return;
   }
 
-  if (ShowProfilePickerIfNeeded(incognito)) {
+  if (ShowProfilePickerIfNeeded(incognito, target_display_id)) {
     std::move(callback).Run(
         crosapi::mojom::CreationResult::kBrowserWindowUnavailable);
     return;
@@ -315,7 +320,9 @@ void BrowserServiceLacros::NewTab(std::optional<uint64_t> profile_id,
     return;
   }
 
-  if (ShowProfilePickerIfNeeded(false)) {
+  // TODO: crbug.com/333312496 - Update newtab to pass the target display id
+  // through the crosapi.
+  if (ShowProfilePickerIfNeeded(false, std::nullopt)) {
     std::move(callback).Run(
         crosapi::mojom::CreationResult::kBrowserWindowUnavailable);
     return;
@@ -349,7 +356,7 @@ void BrowserServiceLacros::Launch(int64_t target_display_id,
     return;
   }
 
-  if (ShowProfilePickerIfNeeded(false)) {
+  if (ShowProfilePickerIfNeeded(false, target_display_id)) {
     std::move(callback).Run(
         crosapi::mojom::CreationResult::kBrowserWindowUnavailable);
     return;
