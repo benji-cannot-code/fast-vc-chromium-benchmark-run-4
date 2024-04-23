0b5d93996a5dca6f0c4b5e8acaa28814e5348ad2
Fiona Macintosh <fmacintosh@google.com>
2024-04-23 18:35:37 +0000

    [TP] Add API to TPS for when 3PC are allowed by enterprise policy
    Bug: b:335427614
    Change-Id: I886827a6cab3b57e25169330338c21b67f84b76c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468191
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291449}

--- components/content_settings/core/browser/cookie_settings.cc
+++ components/content_settings/core/browser/cookie_settings.cc
@@ -370,10 +370,11 @@ bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
     return true;
   }
 
-  // Cookies should always be blocked in 3PCD experiment.
   if (tracking_protection_settings_ &&
       tracking_protection_settings_->IsTrackingProtection3pcdEnabled()) {
-    return true;
+    // 3PCs are blocked by default post-3PCD unless an enterprise policy is set.
+    return !pref_change_registrar_->prefs()->GetBoolean(
+        prefs::kAllowAll3pcToggleEnabled);
   }
 
   CookieControlsMode mode = static_cast<CookieControlsMode>(
@@ -394,8 +395,10 @@ bool CookieSettings::ShouldBlockThirdPartyCookiesInternal() {
 bool CookieSettings::MitigationsEnabledFor3pcdInternal() {
   if (tracking_protection_settings_ &&
       tracking_protection_settings_->IsTrackingProtection3pcdEnabled()) {
-    // Mitigations should be on iff we are not blocking all 3PC in 3PCD.
-    return !tracking_protection_settings_->AreAllThirdPartyCookiesBlocked();
+    // Mitigations should be on iff we are not blocking or allowing all 3PC.
+    return !tracking_protection_settings_->AreAllThirdPartyCookiesBlocked() &&
+           !tracking_protection_settings_
+                ->AreThirdPartyCookiesAllowedByEnterprise();
   }
 
   if (net::cookie_util::IsForceThirdPartyCookieBlockingEnabled()) {
@@ -422,6 +425,8 @@ void CookieSettings::OnContentSettingChanged(
 }
 
 void CookieSettings::OnBlockAllThirdPartyCookiesChanged() {
+  OnCookiePreferencesChanged();
+
   bool new_mitigations_enabled_for_3pcd = MitigationsEnabledFor3pcdInternal();
   {
     base::AutoLock auto_lock(lock_);
@@ -440,6 +445,7 @@ void CookieSettings::OnTrackingProtection3pcdChanged() {
   DCHECK(pref_change_registrar_);
 
   bool new_tracking_protection_enabled_for_3pcd =
+      tracking_protection_settings_ &&
       tracking_protection_settings_->IsTrackingProtection3pcdEnabled();
   {
     base::AutoLock auto_lock(lock_);
