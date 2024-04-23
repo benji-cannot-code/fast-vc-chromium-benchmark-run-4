3388433732af57662980d946a5d451fc5b7f7f94
Jimmy <jimmyxgong@chromium.org>
2024-04-23 01:53:12 +0000

    cros-preview: Add PrintPreviewWebcontentsManager stub
    PrintPreviewWebcontentsManager serves as the chrome endpoint for
    crosapi communication with ash. It will maintain a mapping of
    UUID tokens and webcontents. This is only a stub impl.
    
    Bug: 334993067
    Change-Id: I53f7b3048b18b594a26b774c92a29037b2862796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467874
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291023}

---
+++ chrome/browser/chromeos/printing/print_preview/print_preview_webcontents_manager.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/printing/print_preview/print_preview_webcontents_manager.h"
+
+#include "base/unguessable_token.h"
+
+namespace chromeos {
+
+// TODO(jimmyxgong): implement stub.
+void PrintPreviewWebcontentsManager::GetPreview(base::UnguessableToken token) {}
+
+}  // namespace chromeos
---
+++ chrome/browser/chromeos/printing/print_preview/print_preview_webcontents_manager.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_PRINTING_PRINT_PREVIEW_PRINT_PREVIEW_WEBCONTENTS_MANAGER_H_
+#define CHROME_BROWSER_CHROMEOS_PRINTING_PRINT_PREVIEW_PRINT_PREVIEW_WEBCONTENTS_MANAGER_H_
+
+#include "base/unguessable_token.h"
+
+namespace chromeos {
+
+// Manages a 1:1 relationship between a printing source's webcontents and a
+// base::UnguessableToken. Each token represent a webcontent and is used as a
+// proxy for determining which print preview is relevant.
+// Communicates to ash via crosapi.
+class PrintPreviewWebcontentsManager {
+ public:
+  PrintPreviewWebcontentsManager() = default;
+  PrintPreviewWebcontentsManager(const PrintPreviewWebcontentsManager&) =
+      delete;
+  PrintPreviewWebcontentsManager& operator=(
+      const PrintPreviewWebcontentsManager&) = delete;
+  ~PrintPreviewWebcontentsManager() = default;
+
+  void GetPreview(base::UnguessableToken token);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_PRINTING_PRINT_PREVIEW_PRINT_PREVIEW_WEBCONTENTS_MANAGER_H_
