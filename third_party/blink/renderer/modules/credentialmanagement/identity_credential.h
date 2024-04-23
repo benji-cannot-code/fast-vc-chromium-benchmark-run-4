b058147fdc0c7dddf8e8081d593a896798523f3f
Nicolás Peña <npm@chromium.org>
2024-04-23 00:29:19 +0000

    [FedCM] Add configURL to IdentityCredential under the multi IDP API
    This allows the caller to know which IDP the token comes from.
    
    Fixed: 336298058
    
    Change-Id: Id598686e63bcefc61d54c190d7071de82a529c24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472945
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290991}

--- third_party/blink/renderer/modules/credentialmanagement/identity_credential.h
+++ third_party/blink/renderer/modules/credentialmanagement/identity_credential.h
@@ -21,14 +21,18 @@ class MODULES_EXPORT IdentityCredential final : public Credential {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static IdentityCredential* Create(const String& token, bool is_auto_selected);
+  static IdentityCredential* Create(
+      const String& token,
+      bool is_auto_selected = false,
+      const String& config_url = WTF::g_empty_string);
 
   static bool IsRejectingPromiseDueToCSP(ContentSecurityPolicy* policy,
                                          ScriptPromiseResolverBase* resolver,
                                          const KURL& provider_url);
 
   explicit IdentityCredential(const String& token,
-                              bool is_auto_selected = false);
+                              bool is_auto_selected,
+                              const String& config_url);
 
   // Credential:
   bool IsIdentityCredential() const override;
@@ -36,6 +40,7 @@ class MODULES_EXPORT IdentityCredential final : public Credential {
   // IdentityCredential.idl
   const String& token() const { return token_; }
   const bool& isAutoSelected() const { return is_auto_selected_; }
+  const String& configURL() const { return config_url_; }
 
   static ScriptPromise<IDLUndefined> disconnect(
       ScriptState*,
@@ -45,6 +50,7 @@ class MODULES_EXPORT IdentityCredential final : public Credential {
  private:
   const String token_;
   const bool is_auto_selected_{false};
+  const String config_url_;
 };
 
 }  // namespace blink
--- third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
+++ third_party/blink/web_tests/external/wpt/credential-management/support/fedcm-helper.sub.js
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 export const manifest_origin = "https://{{host}}:{{ports[https][0]}}";
 export const alt_manifest_origin = 'https://{{hosts[alt][]}}:{{ports[https][0]}}';
 export const same_site_manifest_origin = 'https://{{hosts[][www1]}}:{{ports[https][0]}}';
+export const default_manifest_path = '/credential-management/support/fedcm/manifest.py';
 
 export function open_and_wait_for_popup(origin, path) {
   return new Promise(resolve => {
@@ -101,6 +102,25 @@ credential-management/support/fedcm/${manifest_filename}`;
   };
 }
 
+export function request_options_with_two_idps(mediation = 'required') {
+  const first_config = `${manifest_origin}${default_manifest_path}`;
+  const second_config = `${alt_manifest_origin}${default_manifest_path}`;
+  return {
+    identity: {
+      providers: [{
+        configURL: first_config,
+        clientId: 'C1',
+        nonce: 'N1'
+      },
+      {
+        configURL: second_config,
+        clientId: 'C2',
+        nonce: 'N2'
+      }],
+    },
+    mediation: mediation
+  };
+}
 
 // Test wrapper which does FedCM-specific setup.
 export function fedcm_test(test_func, test_name) {
