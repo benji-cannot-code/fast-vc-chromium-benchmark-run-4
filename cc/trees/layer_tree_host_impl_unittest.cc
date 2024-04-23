252e0cf67caac545251146dff1e2760023c363cf
Xianzhu Wang <wangxianzhu@chromium.org>
2024-04-23 21:18:12 +0000

    Fall back to main thread if scroll hit test is affected by rounded corners
    We had two issues:
    1.  Before we had fast rounded corners, we always created mask layers
    for rounded corner clips, and the mask layer made the scroll begin
    unreliable and fall back to the main thread. With fast rounded corners,
    the scrolls were treated as reliable without checking if the point is
    in or out of the rounded corners.
    2. If the scroller has a rounded corner by itself (instead of from an
    ancestor), as we only create InnerBorderRadiusClip for the contents,
    the compositor doesn't actually know which part of the layer bounds
    is transparent to hit test (e.g. if the scroller has a border which
    is outside of the InnerBorderRadiusClip). Now with HitTestOpaqueness,
    such layers have HitTestOpaqueness::kMixed.
    
    This CL changes the behavior of
    LayerTreeImpl::FindLayersUpToFirstOpaqueToHitTest (renamed from
    FindLayerUpToFirstScrollableOrOpaqueToHitTest):
    - For issue #1: LayerImpl::OpaqueToHitTest() also checks whether the
      layer is affected by any fast rounded corners;
    - For issue #2: FindLayerUpToFirstOpaqueToHitTest checks only
      OpaqueToHitTest() (without checking IsScrollerOrScrollbar())
      because a hit test on a scrollable layer is reliable only if it's
      opaque to hit test.
    
    Bug: 40277896
    Change-Id: I1acb16f2c6790760661e8239ea1599035f83ea51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466909
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291538}

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -167,7 +167,7 @@ class LayerTreeHostImplTest : public testing::Test,
     media::InitializeMediaLibrary();
   }
 
-  LayerTreeSettings DefaultSettings() {
+  virtual LayerTreeSettings DefaultSettings() {
     LayerListSettings settings;
     settings.minimum_occlusion_tracking_size = gfx::Size();
     settings.enable_smooth_scroll = true;
@@ -17924,6 +17924,12 @@ class UnifiedScrollingTest : public LayerTreeHostImplTest {
  public:
   using ScrollStatus = InputHandler::ScrollStatus;
 
+  LayerTreeSettings DefaultSettings() override {
+    auto settings = LayerTreeHostImplTest::DefaultSettings();
+    settings.enable_hit_test_opaqueness = true;
+    return settings;
+  }
+
   void SetUp() override {
     LayerTreeHostImplTest::SetUp();
 
@@ -17950,7 +17956,9 @@ class UnifiedScrollingTest : public LayerTreeHostImplTest {
     DrawFrame();
   }
 
-  void CreateScroller(uint32_t main_thread_scrolling_reasons) {
+  void CreateScroller(
+      uint32_t main_thread_scrolling_reasons,
+      HitTestOpaqueness hit_test_opaqueness = HitTestOpaqueness::kOpaque) {
     // Creates a regular compositeds scroller that comes with a ScrollNode and
     // Layer.
     gfx::Size scrollable_content_bounds(100, 100);
@@ -17959,6 +17967,7 @@ class UnifiedScrollingTest : public LayerTreeHostImplTest {
     LayerImpl* layer =
         AddScrollableLayer(OuterViewportScrollLayer(), container_bounds,
                            scrollable_content_bounds);
+    layer->SetHitTestOpaqueness(hit_test_opaqueness);
     scroller_layer_ = layer;
     GetScrollNode(layer)->main_thread_scrolling_reasons =
         main_thread_scrolling_reasons;
@@ -18363,6 +18372,18 @@ TEST_F(UnifiedScrollingTest, MainThreadScrollingReasonsScrollOnCompositor) {
     ScrollStatus status = ScrollBegin(gfx::Vector2d(0, 10));
     EXPECT_EQ(ScrollThread::kScrollOnImplThread, status.thread);
     EXPECT_EQ(MainThreadScrollingReason::kNotScrollingOnMain,
+              status.main_thread_hit_test_reasons);
+  }
+}
+
+TEST_F(UnifiedScrollingTest, UnreliableHitTestOnNonOpaqueToHitTestScroller) {
+  CreateScroller(MainThreadScrollingReason::kNotScrollingOnMain,
+                 HitTestOpaqueness::kMixed);
+
+  {
+    ScrollStatus status = ScrollBegin(gfx::Vector2d(0, 10));
+    EXPECT_EQ(ScrollThread::kScrollOnImplThread, status.thread);
+    EXPECT_EQ(MainThreadScrollingReason::kFailedHitTest,
               status.main_thread_hit_test_reasons);
   }
 }
