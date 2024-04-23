85d31b50ddfebc6e6914c3506582fb7bff4d78b1
Tom Anderson <thomasanderson@chromium.org>
2024-04-23 17:45:54 +0000

    Fix ozone platform initialization when set via chrome://flags
    This change that fixes ozone platform selection.  It's intended to be
    minimal so it can be merged to beta and stable.  The newly added
    --eye-dropper-not-supported flag should be removed in a followup.
    
    R=sky
    CC=​nickdiego
    
    Fixed: 329678163
    Change-Id: Ie06da22447740bdeef8a50216bd1d1769c03588d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5474983
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291406}

--- ui/base/ui_base_features.cc
+++ ui/base/ui_base_features.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdlib.h>
 
+#include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "build/build_config.h"
@@ -346,8 +347,12 @@ BASE_FEATURE(kEyeDropper,
 #endif
 );
 
+const char kEyeDropperNotSupported[] = "eye-dropper-not-supported";
+
 bool IsEyeDropperEnabled() {
-  return base::FeatureList::IsEnabled(features::kEyeDropper);
+  return base::FeatureList::IsEnabled(features::kEyeDropper) &&
+         !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             kEyeDropperNotSupported);
 }
 
 // Used to enable keyboard accessible tooltips in in-page content
