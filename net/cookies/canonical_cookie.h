17ea615fbe71d1ffe291f66602589fbc3e1ca616
Lily Chen <chlily@chromium.org>
2024-04-23 14:35:56 +0000

    [Cookies] Make Lax-allowing-unsafe behavior virtual
    The Lax-allowing-unsafe (a.k.a. Lax+POST) behavior of CanonicalCookie
    allows a cookie with SameSite unspecified (defaulting to Lax) to
    actually be sent on cross-site requests with unsafe methods (POST, etc.)
    within 2 minutes of their creation time (which doesn't usually happen
    for a Lax cookie). This was introduced as a web compatibility measure
    to avoid breakage from SameSite=Lax-by-default.
    
    Since CanonicalCookie is now a subclass of CookieBase, and other
    CookieBase subclasses will exist soon that may not want
    Lax-allowing-unsafe behavior, this CL makes that behavior customizable
    by the subclass.
    
    The default behavior of CookieBase does NOT permit
    Lax-allowing-unsafe; it is instead implemented by CanonicalCookie
    overriding it. Lax-allowing-unsafe was intended to be a temporary
    mitigation, and while there has not been much progress towards removing
    it because the web platform still relies on it, we should default
    towards not having it in the base class because that's the desired end
    state.
    
    Bug: 324405105
    Change-Id: I69d647a043ce8683df0bf7a0b2a950242431101e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464071
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291260}

--- net/cookies/canonical_cookie.h
+++ net/cookies/canonical_cookie.h
@@ -410,6 +410,7 @@ class NET_EXPORT CanonicalCookie : public CookieBase {
   static bool HasHiddenPrefixName(const std::string_view cookie_value);
 
   // CookieBase:
+  base::TimeDelta GetLaxAllowUnsafeThresholdAge() const override;
   void PostIncludeForRequestURL(
       const CookieAccessResult& access_result,
       const CookieOptions& options_used,
