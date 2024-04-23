d1625ca415bb0dd60f2bb24e03911ce9570021c3
Nathan Hebert <nhebert@chromium.org>
2024-04-23 18:19:35 +0000

    [Cast Certificate] Fix command line null dereference
    This checks that the command line is initialized before using it in
    VerifyDeviceCertUsingCustomTrustStore().
    
    In the browser, the command line is always initialized, but when a
    fuzzer runs, it does not initialize the command line. This caused a
    null dereference.
    
    Bug: 336352741
    Change-Id: I179a0851ade574aaa187b54e94d7a95c0fb78279
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478268
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291436}

--- components/media_router/common/providers/cast/certificate/cast_cert_validator.cc
+++ components/media_router/common/providers/cast/certificate/cast_cert_validator.cc
@@ -262,7 +262,8 @@ CastCertError VerifyDeviceCertUsingCustomTrustStore(
     const CastCRL* fallback_crl,
     CRLPolicy crl_policy,
     bssl::TrustStore* trust_store) {
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+  if (base::CommandLine::InitializedForCurrentProcess() &&
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kCastLogDeviceCertChain)) {
     VLOG(3) << "Cast Cert Chain for validation:\n"
             << CastCertificateChainAsPEM(certs);
