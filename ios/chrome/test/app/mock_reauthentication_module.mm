bd6a1861ceef8a22f0b32dc882b3794ac6e703ae
Ernesto Izquierdo Clua <eic@google.com>
2024-04-23 18:25:09 +0000

    [ios] Fix mocked authentication race condition
    Fixes crashes in tests mocking local authentication. Tests delivering
    async mocked authentication results were failing because the
    application logic requesting authentication could run after the test
    code triggering the delivery of the mocked authentication results.
    
    The root cause of the issue is that authentication requests can be
    triggered after some async UI events (e.g. animations). The fix is
    simply spin the run loop until the UI events are done before
    delivering any mocked authentication results.
    
    See bug for examples of failures.
    
    Added failing try-bots:
    ios18-beta-device
    ios17-beta-device
    
    Fixed: 333889854
    Change-Id: I1300aaf04831244ef68443538721d957031ec9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462966
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291441}

--- ios/chrome/test/app/mock_reauthentication_module.mm
+++ ios/chrome/test/app/mock_reauthentication_module.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/app/mock_reauthentication_module.h"
 
 #import "base/check.h"
+#import "base/test/ios/wait_util.h"
 
 // Type of the block used for delivering mocked reauth results.
 typedef void (^ReauthenticationResultHandler)(ReauthenticationResult success);
@@ -69,8 +70,15 @@ typedef void (^ReauthenticationResultHandler)(ReauthenticationResult success);
 }
 
 - (void)returnMockedReauthenticationResult {
-  DCHECK(!self.shouldReturnSynchronously);
-  DCHECK(self.reauthResultHandler);
+  CHECK(!self.shouldReturnSynchronously);
+
+  // Some test cases set the handler after UI events (e.g. animations).
+  // Avoid race conditions by waiting until the handler is set.
+  __weak __typeof(self) weakSelf = self;
+  CHECK(base::test::ios::WaitUntilConditionOrTimeout(
+      base::test::ios::kWaitForUIElementTimeout, ^{
+        return [weakSelf reauthResultHandler] != nullptr;
+      }));
 
   self.reauthResultHandler(_expectedResult);
   self.reauthResultHandler = nil;
