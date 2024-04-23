36057d63b9ef683eed185562c7b6fa01c8e0688a
Jimmy <jimmyxgong@chromium.org>
2024-04-23 00:23:03 +0000

    cros-preview: Add PrintPreviewDialogControllerCros skeleton
    PrintPreviewDialogControllerCros maintains a 1:1 mapping of a print
    preview dialog and its printing source. This is only a skeleton impl.
    
    Bug: 334993067
    Change-Id: I572394fa5605dd63208b56996098abe7e8722845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468880
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1290984}

---
+++ chrome/browser/ash/printing/print_preview/print_preview_dialog_controller_cros.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_PRINTING_PRINT_PREVIEW_PRINT_PREVIEW_DIALOG_CONTROLLER_CROS_H_
+#define CHROME_BROWSER_ASH_PRINTING_PRINT_PREVIEW_PRINT_PREVIEW_DIALOG_CONTROLLER_CROS_H_
+
+#include "base/unguessable_token.h"
+#include "components/printing/common/print.mojom.h"
+
+namespace ash {
+
+// For ChromeOS print preview, this is a singleton class that is responsible for
+// creation and destruction of print preview dialogs. It maintains a 1:1
+// relationship between the base::UnguessableToken representation of the
+// source's Webcontent and its print dialog.
+class PrintPreviewDialogControllerCros {
+ public:
+  PrintPreviewDialogControllerCros() = default;
+  PrintPreviewDialogControllerCros(const PrintPreviewDialogControllerCros&) =
+      delete;
+  PrintPreviewDialogControllerCros& operator=(
+      const PrintPreviewDialogControllerCros&) = delete;
+  ~PrintPreviewDialogControllerCros() = default;
+
+  // TODO(jimmyxgong): Implement these stubs.
+  void CreatePrintPreview(base::UnguessableToken token,
+                          printing::mojom::RequestPrintPreviewParams params);
+  void RemovePrintPreview(base::UnguessableToken token);
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_PRINTING_PRINT_PREVIEW_DIALOG_CONTROLLER_H_
