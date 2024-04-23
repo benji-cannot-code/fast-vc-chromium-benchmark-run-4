aba62ba7bd2dd5f23fd13f285e17e18ca414ab41
Ewann Pellé <ewannpv@chromium.org>
2024-04-23 09:05:09 +0000

    Revert "Remove deleted groups from _selectionEditingItems"
    This reverts commit a0afc1f36f33a4b9b90428f3b686b566aedf9dc4.
    
    Reason for revert:
    BaseGridMediatorTest.CloseSelectedGroup/0
    BaseGridMediatorTest.CloseSelectedGroup/1
    
    are failing on ios-asan bot.
    
    Example of failure: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-asan/72758/
    
    Original change's description:
    > Remove deleted groups from _selectionEditingItems
    >
    > When a group is deleted, the corresponding item must be removed from the
    > set of selected items.
    > - After a batch operation, the set is cleared entirely.
    > - After a deletion outside of a batch operation, only the relevant item
    >   is removed from the set.
    >
    > Fixed: 335813669
    > Change-Id: I911439e19e7626078366a73e99f57939ec2ba1fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465727
    > Commit-Queue: Louis Romero <lpromero@google.com>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Louis Romero <lpromero@google.com>
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1290654}
    
    Change-Id: Ib3ca8cbc0e308be9270990e8a3e0f1449475ebc3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471833
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: David Jean <djean@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Commit-Queue: David Jean <djean@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291163}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
@@ -650,7 +650,6 @@ Browser* GetBrowserForTabWithId(BrowserList* browser_list,
       GridItemIdentifier* groupItemIdentifier =
           [GridItemIdentifier groupIdentifier:groupDeleteChange.deleted_group()
                              withWebStateList:_webStateList];
-      [_selectedEditingItems removeItem:groupItemIdentifier];
       [self.consumer removeItemWithIdentifier:groupItemIdentifier
                        selectedItemIdentifier:[self activeIdentifier]];
       break;
@@ -669,9 +668,6 @@ Browser* GetBrowserForTabWithId(BrowserList* browser_list,
 
 - (void)webStateListBatchOperationEnded:(WebStateList*)webStateList {
   DCHECK_EQ(_webStateList, webStateList);
-
-  // Clear selections.
-  [_selectedEditingItems removeAllItems];
 
   [self addWebStateObservations];
   [self populateConsumerItems];
