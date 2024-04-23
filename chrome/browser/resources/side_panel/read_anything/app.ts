22ba6c3fc176d91ff86c68868f94fbd579bf7d9e
Kristi Saney <kristislee@google.com>
2024-04-23 00:43:53 +0000

    Store NONE status if no voice pack is available for a given language
    Bug: 336279361
    Change-Id: If10af3604a4da215f141936657a4a5d3dd82fdbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472170
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1290995}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -1704,9 +1704,16 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
     const langCodeForVoicePackManager =
         convertLangOrLocaleForVoicePackManager(langOrLocale);
 
-    if (langCodeForVoicePackManager &&
-        this.voicePackInstallStatus[langCodeForVoicePackManager] !==
-            VoicePackStatus.INSTALLED) {
+    if (!langCodeForVoicePackManager) {
+      this.voicePackInstallStatus = {
+        ...this.voicePackInstallStatus,
+        [langOrLocale]: VoicePackStatus.NONE,
+      };
+      return;
+    }
+
+    if (this.voicePackInstallStatus[langCodeForVoicePackManager] !==
+        VoicePackStatus.INSTALLED) {
       this.languagesForVoiceDownloads.add(langCodeForVoicePackManager);
       // Inquire if the voice pack is downloaded. If not, it'll trigger a
       // download when we get the response in updateVoicePackStatus()
