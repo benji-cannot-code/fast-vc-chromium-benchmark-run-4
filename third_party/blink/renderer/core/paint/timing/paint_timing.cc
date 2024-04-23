82ce99bef5eedc6165d2de83f29beb08ce4de226
Khushal Sagar <khushalsagar@chromium.org>
2024-04-23 21:51:37 +0000

    navigation: Fix logging of SurfaceEmbedToFCPFrameSubmission
    This is a follow up to [1] which added a new metric but missed
    serializing it from the GPU to renderer process.
    
    [1]: https://chromium-review.googlesource.com/c/chromium/src/+/5318409
    
    Bug: 325798142
    Change-Id: I29ccbc69c571313b4c73df4a06bc416734437bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478888
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291564}

--- third_party/blink/renderer/core/paint/timing/paint_timing.cc
+++ third_party/blink/renderer/core/paint/timing/paint_timing.cc
@@ -360,6 +360,7 @@ void PaintTiming::RecordFirstContentfulPaintTimingMetrics(
        frame_timing_details.embedded_frame_timestamp);
   base::UmaHistogramBoolean("Navigation.FCPFrameSubmittedBeforeSurfaceEmbed",
                             frame_submitted_before_embed);
+
   if (frame_submitted_before_embed) {
     base::UmaHistogramCustomTimes(
         "Navigation.FCPFrameSubmissionToSurfaceEmbed",
