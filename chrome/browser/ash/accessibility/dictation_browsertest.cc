7d63a6331ba01244f56ca58a19b0a4a578575743
Rob Schonberger <robsc@chromium.org>
2024-04-23 03:03:29 +0000

    soda: Update browser tests to remove monolingual assumptions.
    Many of the browser tests assumed that only en-US has dictation, and
    this change updates to:
    
    1. No longer test that assumption.
    2. Modify the name of some assumed-web languages here and there.
    
    BUG=b/257166700
    
    Change-Id: I6ce59cfaf5dcfcfdcde9a3d7752cf99126ae38ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459072
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291047}

--- chrome/browser/ash/accessibility/dictation_browsertest.cc
+++ chrome/browser/ash/accessibility/dictation_browsertest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/notification_center/notification_center_test_api.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/system/status_area_widget_test_helper.h"
+#include "base/containers/fixed_flat_set.h"
 #include "base/functional/bind.h"
 #include "base/metrics/metrics_hashes.h"
 #include "base/metrics/statistics_recorder.h"
@@ -122,6 +123,9 @@ static const char* kEnglishDictationCommands[] = {
     "select from phrase to another phrase",
     "move to the next sentence",
     "move to the previous sentence"};
+
+constexpr auto kOfflineNotSupportedLocaleSet =
+    base::MakeFixedFlatSet<std::string_view>({"af-ZA", "kn-IN"});
 
 PrefService* GetActiveUserPrefs() {
   return ProfileManager::GetActiveUserProfile()->GetPrefs();
@@ -287,7 +291,6 @@ class DictationTestBase : public AccessibilityFeatureBrowserTest,
   }
 
   DictationTestUtils* utils() { return utils_.get(); }
-
  private:
   std::unique_ptr<DictationTestUtils> utils_;
   base::test::ScopedFeatureList scoped_feature_list_;
@@ -330,13 +333,16 @@ IN_PROC_BROWSER_TEST_P(DictationTest, GetAllSupportedLocales) {
     bool installed = it.second.installed;
     if (speech_recognition_type() == speech::SpeechRecognitionType::kOnDevice &&
         locale == speech::kUsEnglishLocale) {
-      // Currently, the only locale supported by SODA is en-US. It should work
-      // offline and be installed.
+      // We are certain that en_US works offline, so that must be true. There
+      // are others too (depending on flags) but we can't test them.  We can be
+      // certain that we haven't installed them though.
       EXPECT_TRUE(works_offline);
       EXPECT_TRUE(installed);
     } else {
-      EXPECT_FALSE(works_offline);
-      EXPECT_FALSE(installed);
+      EXPECT_FALSE(installed) << " for locale " << locale;
+      if (base::Contains(kOfflineNotSupportedLocaleSet, locale)) {
+        EXPECT_FALSE(works_offline) << " for locale " << locale;
+      }
     }
   }
 
@@ -353,13 +359,16 @@ IN_PROC_BROWSER_TEST_P(DictationTest, GetAllSupportedLocales) {
     bool works_offline = it.second.works_offline;
     bool installed = it.second.installed;
     if (locale == speech::kUsEnglishLocale) {
-      // en-US should be marked as "works offline", but it shouldn't be
-      // installed.
+      // We are certain that en_US works offline, so that must be true. There
+      // are others too (depending on flags) but we can't test them.  We can be
+      // certain that we haven't installed them though.
       EXPECT_TRUE(works_offline);
       EXPECT_FALSE(installed);
     } else {
-      EXPECT_FALSE(works_offline);
-      EXPECT_FALSE(installed);
+      EXPECT_FALSE(installed) << " for locale " << locale;
+      if (base::Contains(kOfflineNotSupportedLocaleSet, locale)) {
+        EXPECT_FALSE(works_offline) << " for locale " << locale;
+      }
     }
   }
 }
