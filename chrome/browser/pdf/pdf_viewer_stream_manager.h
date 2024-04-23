d98039183b403a1389e230209a2e3a0a59cf62db
Andy Phan <andyphan@chromium.org>
2024-04-23 21:40:27 +0000

    [OOPIF PDF] Move PDF extension navigation into a helper method
    Move the PDF extension URL navigation code from
    PdfViewerStreamManager::DidFinishNavigation() to
    PdfViewerStreamManager::NavigateToPdfExtensionUrl(). This makes
    DidFinishNavigation() easier to read, but it also allows
    TestPdfViewerStreamManager to override it for testing loading PDFs
    before the PDF extension loads. See https://crrev.com/c/5455932.
    
    Add an additional parameter to NavigateToPdfExtensionUrl() that can be
    used by subclasses to get the other parameters safely in callbacks. This
    parameter is only used in subclasses and not PdfViewerStreamManager
    itself.
    
    There should be no visible behavioral change in this CL.
    
    Bug: 324636880
    Change-Id: I4c838cec27c3452c463648209a6d1951e831fa95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455370
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291553}

--- chrome/browser/pdf/pdf_viewer_stream_manager.h
+++ chrome/browser/pdf/pdf_viewer_stream_manager.h
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 struct GlobalRenderFrameHostId;
 class NavigationHandle;
+class RenderFrameHost;
+class SiteInstance;
 class WebContents;
 }  // namespace content
 
@@ -297,6 +299,21 @@ class PdfViewerStreamManager
   // Returns the stream info for a PDF content navigation.
   StreamInfo* GetClaimedStreamInfoFromPdfContentNavigation(
       content::NavigationHandle* navigation_handle);
+
+  // Navigates the FrameTreeNode with ID `extension_host_frame_tree_node_id` to
+  // the PDF extension URL. Marks the PDF extension as navigated in
+  // `stream_info`, which must be non-null. `source_site_instance` should be the
+  // `content::SiteInstance` of the PDF embedder frame that will be initiating
+  // the navigation.
+  //
+  // Subclasses may override this for use in callbacks. If so, `global_id`,
+  // which is the ID for the intermediate about:blank host for the PDF extension
+  // frame, can be used to get the other parameters safely.
+  virtual void NavigateToPdfExtensionUrl(
+      int extension_host_frame_tree_node_id,
+      StreamInfo* stream_info,
+      content::SiteInstance* source_site_instance,
+      content::GlobalRenderFrameHostId global_id);
 
  private:
   FRIEND_TEST_ALL_PREFIXES(PdfViewerStreamManagerTest,
