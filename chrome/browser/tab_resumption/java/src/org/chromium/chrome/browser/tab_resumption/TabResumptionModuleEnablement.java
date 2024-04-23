3ce8e67a720dd10b5905f0118ff221a00470154d
Samuel Huang <huangs@chromium.org>
2024-04-23 20:07:52 +0000

    [Android TRM] Add Provider for Mixed suggestions.
    We'd like to show both Local Tab and Foreign Session suggestions in the
    Tab Resumption Module. This CL adds the "Mixed suggestions Provider" to
    do this.
    
    Local Tab suggestion:
    * This intends to replace the Single Tab Module:
    * Requirements:
      * Flag #enable-magic-stack-android="Enable Show tabs in one module".
      * Have "Continue with this tab" in "New tab card settings".
        * We might need to revisit this?
    * This should only appear on a Start-triggered NTP, suggesting the last
      active tab.
      * Work is sill needed to reconcile how Start-triggered NTP should take
        over an existing NTP, but *somehow* manage to show the TRM even if
        it were already hidden by the Magic Stack.
    * Clicking on the suggestion closes the NTP and focuses on the local
      tab, which should already be open.
    * Closing the suggested tab while the Local Tab suggestion is shown
      would trigger TRM update and cause the suggestion to disappear.
    
    Foreign Session suggestions (recap):
    * Requirements:
      * Flag #enable-tab-resumption-module="Enabled".
      * Have "Continue with this tab" in "New tab card settings".
      * User signed in.
      * Sync enabled ("Sync everything").
    * Picks the 2 most recent active tabs among all Foreign Sessions.
    * Clicking on the suggestion uses the current NTP and opens the link.
    * Affecting requirement (e.g., disable sync) causes the suggestions to
      disappear.
    
    Mixed suggestions:
    * The Local Tab suggestion has higher priority over Foreign Sessions.
    * If the Local Tab suggestion is shown, then use LocalTileView, which
      shows a thumbnail and a favicon overlay.
    * Otherwise (Local Tab suggestion as one of two tiles, or
      Foreign Session) use TabResumptionTileView, which uses only favicon.
    * Retain individual click behavior of suggestions.
    * Retain update conditions.
      * Caveat: Recency text (e.g., "1 min ago") gets updated.
    * Complexity arises from delays inherent in Foreign Session suggestions,
      mixing with Local Tab's lack of delay.
      * The "suggestion strength" approach with [TENTATIVE=0, STABLE=1,
        FORCED_NULL=2] works: Mixed suggestion only needs to take the
        minimum.
    
    Details:
    * Add SuggestionsResult.size() for mixing convenience.
    * Add MixedTabResumptionDataProvider.
      * Wraps around {LocalTab,ForeignSession}TabResumptionDataProvider
        instances, taken by CTOR (these are "sub-Providers"). Either but not
        both can be null.
      * Add inner class ResultMixer to get results from both sub-Providers
        (if both exists) and generate mixed SuggestionsResult.
      * Directly forward callback from setStatusChangedCallback() to
        sub-Providers.
    * Enablement:
      * LocalTab: Add and use isAllowedByConfig() and make it same as
        ForeignSession's, to better reflect Magic Stack's current module
        availability check.
      * Update isFeatureEnabled() and computeModuleNotShownReason() to
        include LocalTab. This causes the condition for the TRM to show to
        broaden (since we have a new data source).
    * Builder:
      * In build(), conditionally instantiate sub-Providers. Always wrap
        with new Mixed provider.
    
    Testing:
    * Add MixedTabResumptionDataProviderTest with 9 tests. We need to
      account for more combinations, now that we have a Mixed Provider with
      two sub-Providers, either can be null.
    
    Bug: 332588018
    Change-Id: Ic30eb2118c7ef1c9f534ccbda6774b51d03d51d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477687
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291494}

--- chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleEnablement.java
+++ chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleEnablement.java
@@ -26,12 +26,17 @@ public class TabResumptionModuleEnablement {
             return HomeModulesMetricsUtils.HOME_MODULES_COMBINE_TABS.getValue();
         }
 
+        static boolean isAllowedByConfig() {
+            return HomeModulesConfigManager.getInstance()
+                    .getPrefModuleTypeEnabled(ModuleType.TAB_RESUMPTION);
+        }
+
         static boolean hasData(ModuleDelegate moduleDelegate) {
             return moduleDelegate.getTrackingTab() != null;
         }
 
         static boolean shouldMakeProvider(ModuleDelegate moduleDelegate) {
-            return isFeatureEnabled() && hasData(moduleDelegate);
+            return isFeatureEnabled() && isAllowedByConfig() && hasData(moduleDelegate);
         }
     }
 
@@ -68,8 +73,7 @@ public class TabResumptionModuleEnablement {
      * feature enablement, ignoring other user settings and data availability.
      */
     static boolean isFeatureEnabled() {
-        // TODO(b/332588018): Also consider LocalTab.isFeatureEnabled().
-        return ForeignSession.isFeatureEnabled();
+        return LocalTab.isFeatureEnabled() || ForeignSession.isFeatureEnabled();
     }
 
     /**
@@ -81,7 +85,7 @@ public class TabResumptionModuleEnablement {
      */
     static @Nullable Integer computeModuleNotShownReason(
             ModuleDelegate moduleDelegate, Profile profile) {
-        // TODO(b/332588018): Consider LocalTab.
+        if (LocalTab.isFeatureEnabled() && LocalTab.hasData(moduleDelegate)) return null;
 
         if (!ForeignSession.isFeatureEnabled()) return ModuleNotShownReason.FEATURE_DISABLED;
 
