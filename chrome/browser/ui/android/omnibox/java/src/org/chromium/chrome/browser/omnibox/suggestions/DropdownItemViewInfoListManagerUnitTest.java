92faf83e196a7331baad7dd047c5414695bfc51f
Gang Wu <gangwu@chromium.org>
2024-04-23 23:29:49 +0000

    clean up of the DropdownItemViewInfoListManagerUnitTest
    This is follow up of https://crrev.com/c/5430479.
    
    Change-Id: I8b440ea9f4c2f1f1cd827a72bd3d37d26d41543b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5469901
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291609}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManagerUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/DropdownItemViewInfoListManagerUnitTest.java
@@ -28,9 +28,7 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
-import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.chrome.browser.omnibox.OmniboxFeatures;
 import org.chromium.chrome.browser.ui.theme.BrandedColorScheme;
 import org.chromium.components.omnibox.suggestions.OmniboxSuggestionUiType;
 import org.chromium.ui.modelutil.ListObservable.ListObserver;
@@ -44,7 +42,6 @@ import java.util.List;
 /** Tests for {@link DropdownItemViewInfoListManager}. */
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
-@EnableFeatures({ChromeFeatureList.OMNIBOX_MODERNIZE_VISUAL_UPDATE})
 public class DropdownItemViewInfoListManagerUnitTest {
     public @Rule MockitoRule mockitoRule = MockitoJUnit.rule();
     public @Rule TestRule mProcessor = new Features.JUnitProcessor();
@@ -73,8 +70,6 @@ public class DropdownItemViewInfoListManagerUnitTest {
         mContext = ContextUtils.getApplicationContext();
         mManager = new DropdownItemViewInfoListManager(mSuggestionModels, mContext);
         mManager.onNativeInitialized();
-
-        Assert.assertTrue(OmniboxFeatures.shouldShowModernizeVisualUpdate(mContext));
     }
 
     /**
