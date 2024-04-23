f035d84f032bfef23706fc6e92fb986403d0f02b
Curtis McMullan <curtismcmullan@chromium.org>
2024-04-23 05:07:43 +0000

    editor: refactor text query provider
    This CL completes a quick refactor of EditorTextQueryProvider to make it
    easier to test. Currently it is bound to the concrete impl of
    OrcaProvider. The refactor updates the class to allow for injection of an OrcaProvider-like object.
    
    BUG=b:335358077
    
    Change-Id: I23b085a69060bbe8ed50d38f4c97a35e62fc02c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467481
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291086}

---
+++ chrome/browser/ash/input_method/editor_text_query_from_manta.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_TEXT_QUERY_FROM_MANTA_H_
+#define CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_TEXT_QUERY_FROM_MANTA_H_
+
+#include <map>
+#include <string>
+
+#include "chrome/browser/ash/input_method/editor_text_query_provider.h"
+#include "chrome/browser/manta/manta_service_factory.h"
+#include "components/manta/manta_service_callbacks.h"
+#include "components/manta/orca_provider.h"
+
+namespace ash::input_method {
+
+class EditorTextQueryFromManta : public EditorTextQueryProvider::MantaProvider {
+ public:
+  explicit EditorTextQueryFromManta(Profile* profile);
+  ~EditorTextQueryFromManta() override;
+
+  // EditorTextQueryProvider::MantaProvider overrides
+  void Call(const std::map<std::string, std::string> params,
+            manta::MantaGenericCallback callback) override;
+
+ private:
+  std::unique_ptr<manta::OrcaProvider> provider_;
+};
+
+}  // namespace ash::input_method
+
+#endif  // CHROME_BROWSER_ASH_INPUT_METHOD_EDITOR_TEXT_QUERY_FROM_MANTA_H_
