6ca016d6b1c3156d2bb41cd5468e46095fd6fb5b
Chidera Olibie <colibie@google.com>
2024-04-23 17:01:21 +0000

    [Cronet] Implement setThreadPriority via HttpEngine wrapper
    This wraps the executor provided by the user with an inline
    executor that tries to setThreadPriority if not already set,
    before proceeding to execute the user's executor.
    
    We do not need to allowDirectExecutor for this wrapped executor
    because the inline execution check happens in the runnable
    that is executed on the user's executor.
    
    Bug: b/333345941
    Change-Id: Ia8436fb840538673ece316f0721fce6f65a5c187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5444503
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291363}

---
+++ components/cronet/android/test/javatests/src/org/chromium/net/impl/AndroidHttpEngineWrapperTest.java
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net.impl;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.TruthJUnit.assume;
+
+import android.os.Build;
+import android.os.Process;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.test.util.Batch;
+
+/** Tests wrapper specific features. */
+@Batch(Batch.UNIT_TESTS)
+@RunWith(AndroidJUnit4.class)
+public class AndroidHttpEngineWrapperTest {
+    @Before
+    public void setUp() {
+        assume().that(Build.VERSION.SDK_INT).isAtLeast(Build.VERSION_CODES.UPSIDE_DOWN_CAKE);
+    }
+
+    @Test
+    public void testSetThreadPriorityMethod_runsOnce() {
+        AndroidHttpEngineWrapper engine =
+                new AndroidHttpEngineWrapper(null, Process.THREAD_PRIORITY_FOREGROUND);
+
+        assertThat(engine.setThreadPriority()).isTrue();
+        assertThat(engine.setThreadPriority()).isFalse();
+    }
+}
