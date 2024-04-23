dfe68552ddb7d022e2f4ccbabc4615a2dd3ce3d9
Michael Lippautz <mlippautz@chromium.org>
2024-04-23 16:02:41 +0000

    fieldtrial: Enable V8's --idle-gc-on-context-disposal
    Bug: chromium:333423696
    Change-Id: I8082668bbdab45cc7aebd517674cf0706e202966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464468
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Omer Katz <omerkatz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291319}

--- gin/gin_features.cc
+++ gin/gin_features.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "gin/gin_features.h"
+
 #include "base/metrics/field_trial_params.h"
 
 namespace features {
@@ -210,6 +211,10 @@ BASE_FEATURE(kV8UseLibmTrigFunctions,
 
 BASE_FEATURE(kV8UseOriginalMessageForStackTrace,
              "V8UseOriginalMessageForStackTrace",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kV8IdleGcOnContextDisposal,
+             "V8IdleGcOnContextDisposal",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Elide redundant TDZ hole checks in bytecode. This only sets the V8 flag when
