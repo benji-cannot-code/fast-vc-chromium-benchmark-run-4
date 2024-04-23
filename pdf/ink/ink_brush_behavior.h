996c7aeebc3154817900a2a162cb686c53533256
Jose Dapena Paz <jdapena@igalia.com>
2024-04-23 14:43:56 +0000

    IWYU: missing include for usage of int8_t in ink_brush_behavior.h
    Bug: 41455655
    Change-Id: Ibb1ca76cd7b1aa437d91060b5c83af15c3fb9ceb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5475207
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291265}

--- pdf/ink/ink_brush_behavior.h
+++ pdf/ink/ink_brush_behavior.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_INK_INK_BRUSH_BEHAVIOR_H_
 #define PDF_INK_INK_BRUSH_BEHAVIOR_H_
 
+#include <cstdint>
 #include <optional>
 
 namespace chrome_pdf {
