f1be2423fb3575c3b59434357604ebea291b8491
Rouslan Solomakhin <rouslan@chromium.org>
2024-04-23 17:09:07 +0000

    [PIX] Make PIX code validator into a class
    This patch changes makes the pix_code_validator.h contain a class that
    will be a mojom interface implementer. As a result, the
    IsValidPixCode(string) standalone function is changed to be a static
    method and all of its callers have been updated.
    
    Bug: 334121577
    Change-Id: I793071f53d0cdd4a54cdec63be1045179ea36d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462331
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291372}

--- components/facilitated_payments/content/renderer/facilitated_payments_agent.cc
+++ components/facilitated_payments/content/renderer/facilitated_payments_agent.cc
@@ -54,7 +54,7 @@ void FacilitatedPaymentsAgent::TriggerPixCodeDetection(
           return false;
         }
 
-        result = IsValidPixCode(trimmed_result)
+        result = PixCodeValidator::IsValidPixCode(trimmed_result)
                      ? mojom::PixCodeDetectionResult::kValidPixCodeFound
                      : mojom::PixCodeDetectionResult::kInvalidPixCodeFound;
 
--- components/facilitated_payments/core/util/pix_code_validator_fuzzer.cc
+++ components/facilitated_payments/core/util/pix_code_validator_fuzzer.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace payments::facilitated {
 
 void IsValidPixCodeCanParseAnyString(const std::string& input) {
-  IsValidPixCode(input);
+  PixCodeValidator::IsValidPixCode(input);
 }
 
 FUZZ_TEST(IsValidPixCodeTest, IsValidPixCodeCanParseAnyString)
