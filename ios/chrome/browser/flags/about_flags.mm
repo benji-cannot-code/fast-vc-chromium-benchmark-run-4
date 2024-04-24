83de023b081ee103a8ef90d20c9ebcf0408d2cdc
Vipul Vinod Koul <koulvipul@google.com>
2024-04-24 00:04:00 +0000

    [Verve]Adding flag to support Verve for payments autofill on Chrome
    This CL adds the flags for Verve on Desktop, Clank and iOS clients.
    
    Design doc- go/verve-autofill-dd
    
    Bug: 336380175
    Change-Id: I590d0b9b7eddce54080d028a67801840f54cdaab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468152
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vipul Koul <koulvipul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291624}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1588,6 +1588,11 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
     {"ios-quick-delete", flag_descriptions::kIOSQuickDeleteName,
      flag_descriptions::kIOSQuickDeleteDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kIOSQuickDelete)},
+    {"autofill-enable-verve-card-support",
+     flag_descriptions::kAutofillEnableVerveCardSupportName,
+     flag_descriptions::kAutofillEnableVerveCardSupportDescription,
+     flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(autofill::features::kAutofillEnableVerveCardSupport)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
