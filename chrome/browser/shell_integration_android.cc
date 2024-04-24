27dea43305757deab4318779eeddd8de0df1e050
Alexei Svitkine <asvitkine@chromium.org>
2024-04-24 15:52:15 +0000

    Remove NOTIMPLEMENTED() macros in shell_integration_android.cc.
    These seem to spam logs (e.g. browser tests) and given we've not had a need to implement these for a decade, seems we could spare the spam.
    
    Change-Id: I2448ee55546229d7e4aac6de51bdeeace6e01c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472797
    Auto-Submit: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291895}

--- chrome/browser/shell_integration_android.cc
+++ chrome/browser/shell_integration_android.cc
@@ -3,29 +3,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/notreached.h"
 #include "chrome/browser/shell_integration.h"
 
 namespace shell_integration {
 
 // TODO: crbug/115375 to track implementation for following methods.
 bool SetAsDefaultBrowser() {
-  NOTIMPLEMENTED();
   return false;
 }
 
 bool SetAsDefaultClientForScheme(const std::string& scheme) {
-  NOTIMPLEMENTED();
   return false;
 }
 
 std::u16string GetApplicationNameForScheme(const GURL& url) {
-  NOTIMPLEMENTED();
   return std::u16string();
 }
 
 DefaultWebClientState GetDefaultBrowser() {
-  NOTIMPLEMENTED();
   return UNKNOWN_DEFAULT;
 }
 
@@ -34,7 +29,6 @@ bool IsFirefoxDefaultBrowser() {
 }
 
 DefaultWebClientState IsDefaultClientForScheme(const std::string& scheme) {
-  NOTIMPLEMENTED();
   return UNKNOWN_DEFAULT;
 }
 
@@ -42,7 +36,6 @@ namespace internal {
 
 DefaultWebClientSetPermission GetPlatformSpecificDefaultWebClientSetPermission(
     WebClientSetMethod method) {
-  NOTIMPLEMENTED();
   return SET_DEFAULT_NOT_ALLOWED;
 }
 
