f4821df6c6a8d4410a4c6879eb0af2efec927614
Florian Leimgruber <fleimgruber@google.com>
2024-04-24 08:34:29 +0000

    Simplify PersonalDataManagerTestBase
    Bug: b/322170538
    Change-Id: I57b0538619700173fe1be1d0f77c9f6563a50ed6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477248
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291761}

--- components/autofill/core/browser/address_data_manager_unittest.cc
+++ components/autofill/core/browser/address_data_manager_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/with_feature_override.h"
+#include "base/uuid.h"
 #include "build/buildflag.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
