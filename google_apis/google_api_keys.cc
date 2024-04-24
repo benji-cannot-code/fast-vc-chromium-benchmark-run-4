d2a631e5aee65897f0634806943511485fc41c26
Mihai Sardarescu <msarda@chromium.org>
2024-04-24 13:40:40 +0000

    [google_apis] Allow overriding main API key via a feature.
    This CLs adds the capability to override the API key via a feature
    parameter. This takes precedence over the baked-in value of the APIKey
    (including the value in the plist file on Apple platforms).
    
    Note: This CL also removes the google_apis/google_api_keys.cc and
    instead simply includes google_apis/google_api_keys-inc.cc in the
    source files in google_apis/BUILD. This ensures that the file is
    properly tracked by the build system and appears in IDEs (e.g. Xcode)
    and thus can be used during bebugging.
    
    Bug: 333064918
    
    Change-Id: Id146d73ca51bba49f8d7680d80cb2c13cc005225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424132
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291846}

--- google_apis/google_api_keys.cc
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2012 The Chromium Authors
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Note: The implementation of the google_apis_keys is in file
-// "google_apis/google_api_keys-inc.cc" as it uses macros to define API keys
-// and it needs to be included several times for the unit test.
-#include "google_apis/google_api_keys-inc.cc"
---
+++ google_apis/google_api_keys_utils.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GOOGLE_APIS_GOOGLE_API_KEYS_UTILS_H_
+#define GOOGLE_APIS_GOOGLE_API_KEYS_UTILS_H_
+
+#include <string>
+
+#include "base/component_export.h"
+
+namespace google_apis {
+
+// Returns an API key that can be used to override the API key and that is
+// configured via and experimental feature.
+COMPONENT_EXPORT(GOOGLE_APIS)
+std::string GetAPIKeyOverrideViaFeature();
+
+COMPONENT_EXPORT(GOOGLE_APIS)
+void LogAPIKeysMatchHistogram(bool match);
+
+}  // namespace google_apis
+
+#endif  // GOOGLE_APIS_GOOGLE_API_KEYS_UTILS_H_
