59acae5874f9b70bd57c42e9199f74331447446b
weidongliu <weidongliu@chromium.org>
2024-04-24 23:29:48 +0000

    views: use CalculatePreferredSize(SizeBounds) in //ash/app_list.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: I5b0317f088cebcd3763ae3d28a3765dc17a5570d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465408
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292176}

--- ash/app_list/views/search_result_view.cc
+++ ash/app_list/views/search_result_view.cc
@@ -1302,7 +1302,8 @@ void SearchResultView::OnSelectedResultChanged() {
   }
 }
 
-gfx::Size SearchResultView::CalculatePreferredSize() const {
+gfx::Size SearchResultView::CalculatePreferredSize(
+    const views::SizeBounds& available_size) const {
   return gfx::Size(kPreferredWidth, PreferredHeight());
 }
 
--- ash/app_list/views/top_icon_animation_view.h
+++ ash/app_list/views/top_icon_animation_view.h
@@ -74,7 +74,8 @@ class TopIconAnimationView : public views::View,
 
  private:
   // views::View overrides:
-  gfx::Size CalculatePreferredSize() const override;
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override;
   void Layout(PassKey) override;
   void OnThemeChanged() override;
 
