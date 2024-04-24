07fb8bec1a3acac8706162f837ed66dc7b4231bb
Scott Violet <sky@chromium.org>
2024-04-24 18:07:39 +0000

    [a11y]: this reverts three patches that lead to renderer crashes
    Specifically these patches:
    https://chromium-review.googlesource.com/c/chromium/src/+/5370683
    https://chromium-review.googlesource.com/c/chromium/src/+/5388968
    https://chromium-review.googlesource.com/c/chromium/src/+/5468155
    
    Bug: 335553723
    Change-Id: I3c6e65290021dd8f732b7175b9625cb72fbb1ef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484367
    Reviewed-by: Tzarial Kuznia <zork@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291985}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -2899,16 +2899,7 @@ String AXObject::GetValueForControl() const {
   return String();
 }
 
-String AXObject::GetValueForControl(AXObjectSet& visited) const {
-  return String();
-}
-
 String AXObject::SlowGetValueForControlIncludingContentEditable() const {
-  return String();
-}
-
-String AXObject::SlowGetValueForControlIncludingContentEditable(
-    AXObjectSet& visited) const {
   return String();
 }
 
