9055f0ad44d6dcec84cc5cc22de5b5c53db92bf7
Annie Sullivan <sullivan@chromium.org>
2024-04-24 13:54:49 +0000

    LoAF: add trace events for all animation frames, long or short.
    Bug: 333906584
    Change-Id: Icfa976673f9ea3cd5b4238513f93fd5afd10d1e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476429
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291852}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -115,8 +115,8 @@ void AnimationFrameTimingMonitor::DidBeginMainFrame() {
     current_frame_timing_info_->SetTotalBlockingDuration(blocking_duration);
 
     client_.ReportLongAnimationFrameTiming(current_frame_timing_info_);
-    RecordLongAnimationFrameUKMAndTrace(*current_frame_timing_info_);
   }
+  RecordLongAnimationFrameUKMAndTrace(*current_frame_timing_info_);
 
   first_ui_event_timestamp_ = base::TimeTicks();
   current_frame_timing_info_.Clear();
@@ -213,7 +213,11 @@ void AnimationFrameTimingMonitor::OnTaskCompleted(
     return;
   }
 
-  if (!frame || (task_duration < kLongAnimationFrameDuration)) {
+  bool tracing_enabled;
+  TRACE_EVENT_CATEGORY_GROUP_ENABLED("devtools.timeline", &tracing_enabled);
+
+  if (!frame ||
+      (task_duration < kLongAnimationFrameDuration && !tracing_enabled)) {
     return;
   }
 
@@ -228,7 +232,8 @@ void AnimationFrameTimingMonitor::OnTaskCompleted(
   }
 
   if (RuntimeEnabledFeatures::LongAnimationFrameTimingEnabled(
-          frame->DomWindow())) {
+          frame->DomWindow()) &&
+      task_duration >= kLongAnimationFrameDuration) {
     DOMWindowPerformance::performance(*frame->DomWindow())
         ->ReportLongAnimationFrameTiming(timing_info);
   }
@@ -268,17 +273,21 @@ void RecordLongAnimationFrameTrace(const AnimationFrameTimingInfo& info,
   uint64_t trace_id = base::trace_event::GetNextGlobalTraceId();
 
   TRACE_EVENT_NESTABLE_ASYNC_BEGIN_WITH_TIMESTAMP1(
-      "devtools.timeline", "LongAnimationFrame", trace_id,
-      info.FrameStartTime(), "data", std::move(traced_value));
-  TRACE_EVENT_NESTABLE_ASYNC_END_WITH_TIMESTAMP0("devtools.timeline",
-                                                 "LongAnimationFrame", trace_id,
-                                                 info.RenderEndTime());
+      "devtools.timeline", "AnimationFrame", trace_id, info.FrameStartTime(),
+      "data", std::move(traced_value));
+  TRACE_EVENT_NESTABLE_ASYNC_END_WITH_TIMESTAMP0(
+      "devtools.timeline", "AnimationFrame", trace_id, info.RenderEndTime());
 }
 }  // namespace
 
 void AnimationFrameTimingMonitor::RecordLongAnimationFrameUKMAndTrace(
     const AnimationFrameTimingInfo& info) {
+  // Record all animation frames to traces, but only long ones to UKM.
   RecordLongAnimationFrameTrace(info, this);
+  if (info.Duration() < kLongAnimationFrameDuration) {
+    return;
+  }
+
   ukm::UkmRecorder* recorder = client_.MainFrameUkmRecorder();
   ukm::SourceId source_id = client_.MainFrameUkmSourceId();
   if (!recorder || source_id == ukm::kInvalidSourceId) {
