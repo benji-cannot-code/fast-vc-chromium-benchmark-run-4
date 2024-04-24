6d7020d50204d3ddf60bc2ca55265720cc2ab2eb
Chidera Olibie <colibie@google.com>
2024-04-24 16:35:51 +0000

    Add arm config to cr_cronet
    Change-Id: I4bb01ba26cf284fe60a1639cf17ff9fc064ee0a6
    Bug: b/331170780
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5483506
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Chidera Olibie <colibie@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291923}

--- components/cronet/tools/cr_cronet.py
+++ components/cronet/tools/cr_cronet.py
@@ -79,6 +79,9 @@ def main():
   parser.add_argument('--x64',
                       action='store_true',
                       help='build for Intel x86_64 architecture')
+  parser.add_argument('--arm',
+                      action='store_true',
+                      help='build for arm architecture')
   parser.add_argument('-R',
                       '--riscv64',
                       action='store_true',
@@ -107,6 +110,9 @@ def main():
   elif options.riscv64:
     target_cpu = 'riscv64'
     out_dir_suffix = '-riscv64'
+  elif options.arm:
+    target_cpu = 'arm'
+    out_dir_suffix = '-arm'
   else:
     target_cpu = 'arm64'
     out_dir_suffix = '-arm64'
