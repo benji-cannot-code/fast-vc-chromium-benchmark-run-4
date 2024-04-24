a4db0ebe4bc55244098d5b0cac744c8655133876
mlcui <mlcui@google.com>
2024-04-24 06:05:23 +0000

    language_packs: Rename font service callbacks to reflect use
    This should purely be a refactoring change, and will make the following
    changes (which adds a callback to the install at login) easier to
    understand.
    
    Bug: b:331667343
    Change-Id: Id46c39b5fdbe0a75239d5b9f47a97d159705585d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479619
    Reviewed-by: Zac Partridge <zacpartridge@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291715}

--- chrome/browser/ash/language_packs/language_pack_font_service.cc
+++ chrome/browser/ash/language_packs/language_pack_font_service.cc
@@ -35,7 +35,7 @@ LanguagePackFontService::LanguagePackFontService(PrefService* prefs,
     : prefs_(CHECK_DEREF(prefs)), add_font_dir_(std::move(add_font_dir)) {
   pref_accept_language_.Init(
       language::prefs::kPreferredLanguages, &*prefs_,
-      base::BindRepeating(&LanguagePackFontService::InstallFontDlcs,
+      base::BindRepeating(&LanguagePackFontService::OnAcceptLanguageChanged,
                           weak_factory_.GetWeakPtr()));
 
   // Add installed fonts to fontconfig.
@@ -47,9 +47,8 @@ LanguagePackFontService::LanguagePackFontService(PrefService* prefs,
   for (std::string& language_pack : GetLanguagePacksForAcceptLanguage()) {
     LanguagePackManager::GetPackState(
         kFontsFeatureId, language_pack,
-        base::BindOnce(
-            &LanguagePackFontService::AddDlcFontDirsToFontConfigPackCallback,
-            weak_factory_.GetWeakPtr()));
+        base::BindOnce(&LanguagePackFontService::GetPackStateOnInitCallback,
+                       weak_factory_.GetWeakPtr()));
   }
 }
 
@@ -74,14 +73,14 @@ LanguagePackFontService::GetLanguagePacksForAcceptLanguage() {
   return language_packs;
 }
 
-void LanguagePackFontService::InstallFontDlcs() {
+void LanguagePackFontService::OnAcceptLanguageChanged() {
   for (std::string& language_pack : GetLanguagePacksForAcceptLanguage()) {
     LanguagePackManager::InstallPack(kFontsFeatureId, language_pack,
                                      base::DoNothing());
   }
 }
 
-void LanguagePackFontService::AddDlcFontDirsToFontConfigPackCallback(
+void LanguagePackFontService::GetPackStateOnInitCallback(
     const PackResult& result) {
   if (result.pack_state != PackResult::StatusCode::kInstalled &&
       !result.language_code.empty()) {
