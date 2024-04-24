26f79e77d3c8dd44b6c2c892f17dafe9bc0e6c92
Jihad Hanna <jihadghanna@google.com>
2024-04-24 14:25:55 +0000

    Revert "[iOS] Autofill: Refactor Filling & DidFill..."
    This reverts commit 21028f8ccc2c5b4c0747f973dd5bf61d4c099666.
    
    Reason for revert: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-device/356100/overview
    
    Original change's description:
    > [iOS] Autofill: Refactor Filling & DidFill...
    >
    > This CL refactors the process of filling to more closely match the
    > content implementation of this flow. Specifically:
    >
    > 1. fillFormData:inFrame: in AutofillAgent now uses a FillData object
    > 2. DidFillAutofillFormData is no longer invoked synchronously with the
    >    form object that was passed to fillFormData. Instead, the JS code
    >    waits for all the filling events to be triggered, then re-extracts
    >    the form, and sends it back to AutofillJavaScriptFeature, which
    >    parses the message and passes the result to the Driver.
    >
    > This change is needed to allow the AutofillDriverRouter to interact with
    > this flow. That will be introduced in a follow-up CL.
    >
    > Bug: 324931644
    > Change-Id: Id283338d7897d1dbd7c0bca09de66f52f91141fc
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5311137
    > Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    > Reviewed-by: Vincent Boisselle <vincb@google.com>
    > Commit-Queue: Tommy Martino <tmartino@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1291857}
    
    Bug: 324931644
    Change-Id: Ia1dc5c18399854403a395858b4775f72609b267f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482015
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Owners-Override: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291863}

--- components/autofill/ios/browser/autofill_driver_ios_bridge.h
+++ components/autofill/ios/browser/autofill_driver_ios_bridge.h
@@ -6,15 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_IOS_BROWSER_AUTOFILL_DRIVER_IOS_BRIDGE_H_
 #define COMPONENTS_AUTOFILL_IOS_BROWSER_AUTOFILL_DRIVER_IOS_BRIDGE_H_
 
-#import <stdint.h>
+#include <stdint.h>
 
-#import <vector>
+#include <vector>
 
-#import "components/autofill/core/common/form_data_predictions.h"
-#import "components/autofill/core/common/form_field_data.h"
-#import "components/autofill/core/common/unique_ids.h"
+#include "components/autofill/core/common/form_data_predictions.h"
 
 namespace autofill {
+struct FormData;
 class FormStructure;
 }
 
@@ -26,8 +25,8 @@ class WebFrame;
 // Interface used to pipe form data from AutofillDriverIOS to the embedder.
 @protocol AutofillDriverIOSBridge
 
-- (void)fillData:(const std::vector<autofill::FormFieldData::FillData>&)form
-         inFrame:(web::WebFrame*)frame;
+- (void)fillFormData:(const autofill::FormData&)form
+             inFrame:(web::WebFrame*)frame;
 
 - (void)fillSpecificFormField:(const autofill::FieldRendererId&)field
                     withValue:(const std::u16string)value
