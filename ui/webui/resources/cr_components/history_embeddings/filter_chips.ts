eb7bd35050c6fe126281e6e97276cdba776a31d7
John Lee <johntlee@chromium.org>
2024-04-24 03:10:13 +0000

    Update history "show by" button to use a select menu instead of a chip
    http://screen/YmX37AW435pq2NG
    
    Bug: 328648133
    Change-Id: Ic14b86e4108ee7a5cb96170032a3402c7d05cbd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468627
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291683}

--- ui/webui/resources/cr_components/history_embeddings/filter_chips.ts
+++ ui/webui/resources/cr_components/history_embeddings/filter_chips.ts
@@ -5,12 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '//resources/cr_elements/cr_chip/cr_chip.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
-import '//resources/cr_elements/icons.html.js';
-import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
-import './icons.html.js';
+import '//resources/cr_elements/md_select.css.js';
 
 import {loadTimeData} from '//resources/js/load_time_data.js';
-import type {IronIconElement} from '//resources/polymer/v3_0/iron-icon/iron-icon.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import type {DomRepeatEvent} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
@@ -50,8 +47,7 @@ function generateSuggestions(): Suggestion[] {
 
 export interface HistoryEmbeddingsFilterChips {
   $: {
-    byGroupChip: HTMLElement,
-    byGroupChipIcon: IronIconElement,
+    showByGroupSelectMenu: HTMLSelectElement,
   };
 }
 export class HistoryEmbeddingsFilterChips extends PolymerElement {
@@ -98,8 +94,8 @@ export class HistoryEmbeddingsFilterChips extends PolymerElement {
     return this.selectedSuggestion === suggestion;
   }
 
-  private onByGroupClick_() {
-    this.showResultsByGroup = !this.showResultsByGroup;
+  private onShowByGroupSelectMenuChanged_() {
+    this.showResultsByGroup = this.$.showByGroupSelectMenu.value === 'true';
   }
 
   private onTimeRangeStartChanged_() {
