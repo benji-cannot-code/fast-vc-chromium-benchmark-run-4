af8c5225dfbd7edf76e2c1d1da7aa8572f2954a5
Vidhan Jain <vidhanj@google.com>
2024-04-24 08:37:01 +0000

    [iOS] Replace AutofillUIType in AutofillProfileEditItem with autofill field type
    Autofill field types are a string version of the AutofillType. This will
    help us in getting rid of autofillUIType from AutofillProfileEditTableViewController to make way for country specific fields.
    
    Bug: 1482269
    Change-Id: I36b870de4bb8862252ceae6bdf240b29240c087f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476469
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291763}

--- ios/chrome/browser/ui/autofill/autofill_profile_edit_table_view_controller_delegate.h
+++ ios/chrome/browser/ui/autofill/autofill_profile_edit_table_view_controller_delegate.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Notifies the class that conforms this delegate to update the profile
 // `serverFieldType` with `value`.
 - (void)updateProfileMetadataWithValue:(NSString*)value
-                     forAutofillUIType:(AutofillUIType)autofillUIType;
+                  forAutofillFieldType:(NSString*)autofillUIType;
 
 // Notifies the class that conforms this delegate that the view has moved out of
 // the view hierarchy.
