e0fcfb457128a1b7b1f0f2d651012d42870bd25e
Adriana Ixba <aixba@google.com>
2024-04-24 06:21:43 +0000

    Reland "Add displayName to animation trace event"
    This is a reland of commit 7e1f73fef48baa28dd62ec73857ea25964e50457
    
    Wait until animation starts with onceAnimationStarted to avoid flakiness.
    From failures, it looks like only some of the animation
    events get caught with the status animation events missing.
    
    Original change's description:
    > Add displayName to animation trace event
    >
    > We use this property for auditing in Lighthouse by using a cdp call.
    > We want to avoid making cdp calls in our transition to RPP,
    > so adding this information in through the trace
    >
    > Bug: 331429737,335215001
    > Change-Id: I08533ae28c2738fddc5c0f5c14a29e505fa3f5c3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448403
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1288539}
    
    Bug: 331429737,335215001
    Change-Id: I9d4b7685373c6d858cfb0ce2aad357bdc3e2f227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473867
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291718}

--- third_party/blink/renderer/core/inspector/inspector_animation_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_animation_agent.h
@@ -69,6 +69,7 @@ class CORE_EXPORT InspectorAnimationAgent final
   protocol::Response AssertAnimation(const String& id,
                                      blink::Animation*& result);
 
+  static String AnimationDisplayName(const Animation& animation);
   void Trace(Visitor*) const override;
 
  private:
