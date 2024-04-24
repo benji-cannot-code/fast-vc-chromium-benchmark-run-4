49cd9dfb8f0a4a3055527ad3eeb05f7ed26d2cc5
Wolfgang Beyer <wolfi@chromium.org>
2024-04-24 19:38:19 +0000

    [DevTools] Console insights: handle "allow, but no logging" enterprise policy
    DevTools CL: https://crrev.com/c/5471891
    
    Bug: chromium:299423255, b/313052094
    Change-Id: I0583434561d584d07ea7769c3936f77a55cff456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471796
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292026}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -1333,6 +1333,9 @@ GURL DevToolsWindow::GetDevToolsURL(Profile* profile,
       if (blocked_reason.blocked_by_enterprise_policy) {
         url += "&ci_blockedByEnterprisePolicy=true";
       }
+      if (blocked_reason.disallow_logging) {
+        url += "&ci_disallowLogging=true";
+      }
       if (blocked_reason.blocked_by_geo) {
         url += "&ci_blockedByGeo=true";
       }
