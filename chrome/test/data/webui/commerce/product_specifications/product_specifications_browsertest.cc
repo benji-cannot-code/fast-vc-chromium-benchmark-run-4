2095de0f94a6c44a737ee26648b53c4e38a69a4b
Kevin McNee <mcnee@chromium.org>
2024-04-24 16:21:57 +0000

    Disable ProductSpecificationsTest.ProductSelector for flakiness
    Bug: 336414352
    Change-Id: Ibc348585f28ac29149ff38a3392ede314fde5b96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482689
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291913}

--- chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
+++ chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
@@ -30,7 +30,8 @@ IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, Table) {
   RunTest("commerce/product_specifications/table_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, ProductSelector) {
+// TODO(https://crbug.com/336414352): Flaky.
+IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, DISABLED_ProductSelector) {
   RunTest("commerce/product_specifications/product_selector_test.js",
           "mocha.run()");
 }
