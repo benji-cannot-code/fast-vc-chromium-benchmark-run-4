a60cb25d0fa881994d115019bebc63fb55bc2d8b
Duncan Mercer <mercerd@google.com>
2024-04-24 18:44:07 +0000

    [lensoverlay] Move toPercent to a common file
    Bug: b/333620724
    Change-Id: I3de82e441a9a4e3599ce816ebe8c2395a70d53cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478269
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292008}

---
+++ chrome/browser/resources/lens/overlay/values_converter.ts
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// File to store common utils relating to converting values used across the Lens
+// overlay.
+
+// Takes the value between 0-1 and returns a string in the form '__%';
+export function toPercent(value: number): string {
+  return `${value * 100}%`;
+}
