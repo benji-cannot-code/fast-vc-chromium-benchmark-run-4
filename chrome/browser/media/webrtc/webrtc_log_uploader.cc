9d189c16dd73b5f741025e406666e4b2425f5978
Tony Herre <toprice@chromium.org>
2024-04-24 13:48:20 +0000

    Bump max number of concurrent webrtc debug log collections
    WebRTC usage is now much more common, across multiple different
    products. This is leading to a rise in the number of logging start
    attempts failing due to too many concurrent debugging sessions seen.
    
    Also add UMA monitoring of how many concurrent logs are happening, to
    guide setting this limit in future.
    
    Bug: 313592212
    Change-Id: I856426e52508daa8167639e953fb402b5a01095a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482190
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Reviewed-by: Tomas Gunnarsson <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291849}

--- chrome/browser/media/webrtc/webrtc_log_uploader.cc
+++ chrome/browser/media/webrtc/webrtc_log_uploader.cc
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kLogCountLimit = 5;
+const int kLogCountLimit = 20;
 const uint32_t kIntermediateCompressionBufferBytes = 256 * 1024;  // 256 KB
 const int kLogListLimitLines = 50;
 
@@ -156,11 +156,16 @@ WebRtcLogUploader::~WebRtcLogUploader() {
 
 bool WebRtcLogUploader::ApplyForStartLogging() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(main_sequence_checker_);
+  bool success = false;
   if (log_count_ < kLogCountLimit && !shutdown_) {
     ++log_count_;
-    return true;
+    base::UmaHistogramCounts100("WebRtcTextLogging.ConcurrentLogCount",
+                                log_count_);
+    success = true;
   }
-  return false;
+  base::UmaHistogramBoolean("WebRtcTextLogging.ApplyForStartLoggingSuccess",
+                            success);
+  return success;
 }
 
 void WebRtcLogUploader::LoggingStoppedDontUpload() {
