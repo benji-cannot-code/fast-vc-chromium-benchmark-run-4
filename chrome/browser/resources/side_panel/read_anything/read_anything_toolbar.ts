0f4c9f49a7075a601162599b90c3a0f85380ae18
Kristi Saney <kristislee@google.com>
2024-04-24 00:34:17 +0000

    Wrap the toolbar when page zoom is between 125 and 200
    demo: https://photos.app.goo.gl/JnPNxH2MbCmmovBB9
    
    Still todo: Handle page zoom 110 & 125 when "play" is pressed as that's
    still broken after this change. Ideally we can put more things in the
    more options menu in these cases but I may eventually give up and just
    overflow then too.
    
    Bug: 308467782
    Change-Id: I64c639e5aaddd93bddc8f01d26c477decdcb37e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5480075
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291638}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -333,7 +333,7 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
   private colorSuffix_: string = '';
 
   private toolbarContainerObserver_: ResizeObserver|null;
-  private dragResizeCallback_: () => void;
+  private windowResizeCallback_: () => void;
 
   // If Read Aloud is in the paused state. This is set from the parent element
   // via one way data binding.
@@ -382,9 +382,8 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
       this.toolbarContainerObserver_ =
           new ResizeObserver(this.onToolbarResize_);
       this.toolbarContainerObserver_.observe(this.$.toolbarContainer);
-
-      this.dragResizeCallback_ = this.onDragResize_.bind(this);
-      window.addEventListener('resize', this.dragResizeCallback_);
+      this.windowResizeCallback_ = this.onWindowResize_.bind(this);
+      window.addEventListener('resize', this.windowResizeCallback_);
     }
     this.textStyleOptions_ =
         this.textStyleOptions_.concat(this.moreOptionsButtons_);
@@ -397,8 +396,8 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
 
   override disconnectedCallback() {
     super.disconnectedCallback();
-    if (this.dragResizeCallback_) {
-      window.removeEventListener('resize', this.dragResizeCallback_);
+    if (this.windowResizeCallback_) {
+      window.removeEventListener('resize', this.windowResizeCallback_);
     }
     this.toolbarContainerObserver_?.disconnect();
   }
@@ -428,7 +427,7 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
     this.areFontsLoaded_ = true;
   }
 
-  private onDragResize_() {
+  private onWindowResize_() {
     ReadAnythingToolbarElement.maybeUpdateMoreOptions(this.$.toolbarContainer);
   }
 
