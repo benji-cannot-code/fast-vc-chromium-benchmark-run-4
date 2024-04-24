778a5f30ca9c3b4df1a53466a90b9d6ae1febe13
Takumi Fujimoto <takumif@chromium.org>
2024-04-24 00:15:59 +0000

    Allow reshowing pointer lock bubble (toast) immediately after hiding
    When the bubble is hidden, it is destroyed asynchronously afterwards.
    If we try to reshow the bubble right before it's destroyed, the
    destruction still happens and it isn't reshown. This CL fixes that
    issue.
    
    Bug: 335009803, 334994009
    Change-Id: Ic9daa1f3a40a0059cec69f9e67184a05823a8a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5461034
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291631}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/task/cancelable_task_tracker.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "build/branding_buildflags.h"
@@ -110,7 +111,7 @@ enum class Channel;
 namespace views {
 class ExternalFocusTracker;
 class WebView;
-}
+}  // namespace views
 
 namespace webapps {
 enum class InstallableWebAppCheckResult;
@@ -982,9 +983,8 @@ class BrowserView : public BrowserWindow,
 
   // Calls |method| which is either WebContents::Cut, ::Copy, or ::Paste on
   // the given WebContents, returning true if it consumed the event.
-  bool DoCutCopyPasteForWebContents(
-      content::WebContents* contents,
-      void (content::WebContents::*method)());
+  bool DoCutCopyPasteForWebContents(content::WebContents* contents,
+                                    void (content::WebContents::*method)());
 
   // Shows the next app-modal dialog box, if there is one to be shown, or moves
   // an existing showing one to the front.
@@ -1270,6 +1270,10 @@ class BrowserView : public BrowserWindow,
   bool in_process_fullscreen_ = false;
 
   std::unique_ptr<ExclusiveAccessBubbleViews> exclusive_access_bubble_;
+  // Tracks the task to asynchronously destroy the exclusive access bubble.
+  base::CancelableTaskTracker exclusive_access_bubble_cancelable_task_tracker_;
+  std::optional<base::CancelableTaskTracker::TaskId>
+      exclusive_access_bubble_destruction_task_id_;
 
   // True when we do not want to allow exiting fullscreen, e.g. in Chrome OS
   // Kiosk session.
