c23b0a7e719695ff6b522f51e7b2f09872a2eb41
thefrog <thefrog@chromium.org>
2024-04-24 20:58:28 +0000

    Use latest instead of stored permanent country for HPRT eligibility
    Some implementation details:
     - This CL modifies OHTTP key service to start listening to local state
       pref changes as well as the current profile pref changes, since the
       VariationsService uses local state prefs for the latest country. Note
       that crrev.com/c/5445492 technically should already have done this
       split in GetHashRealTimeSelectionConfiguringPrefs for the variations
       prefs (it is a bug that it did not), but  there is no impact for that
       bug since the stored_permanent_country  prefs don’t change
       mid-session.
     - References to the stored_permanent_country are removed since they are
       no longer used.
     - A simple browser test is added that runs through both the factory
       code creating the service and the service itself to test the local
       state pref changes end to end.
    
    Deprecated histogram:
    SafeBrowsing.HPRT.WouldBeIneligibleForSessionOrLatestCountry
    
    OBSOLETE_HISTOGRAM[SafeBrowsing.HPRT.WouldBeIneligibleForSessionOrLatestCountry]=No longer needed.
    
    Bug: 330364321
    Change-Id: Ie44cc0f91d097c7ed011e097dc38879dee1366ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478889
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292081}

--- ios/chrome/browser/safe_browsing/model/ohttp_key_service_factory_unittest.mm
+++ ios/chrome/browser/safe_browsing/model/ohttp_key_service_factory_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/safe_browsing/model/ohttp_key_service_factory.h"
 
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 
@@ -14,6 +15,7 @@ class OhttpKeyServiceFactoryTest : public PlatformTest {
   OhttpKeyServiceFactoryTest() = default;
 
   web::WebTaskEnvironment task_environment_;
+  IOSChromeScopedTestingLocalState scoped_testing_local_state_;
   std::unique_ptr<ChromeBrowserState> browser_state_;
 
  private:
