11d5e045beeeba48d00e3e19e3b4b6d101e81b39
Cam Bickel <cambickel@google.com>
2024-04-24 16:43:34 +0000

    settings: Add click handler + actionable styles to startup sound toggle
    This CL applies the new attribute 'actionable-row' added in
    https://crrev.com/c/5463288 to the audio setting
    "Device startup sound enabled". This CL also adds a click handler to
    the setting row to match the behavior of settings-toggle-button.
    
    Demo videos:
    (note: the cursor position in the videos are slightly offset,
    seems like an issue with the screen recording, not sure what happened)
    
    Before:
    https://screencast.googleplex.com/cast/NTIwNjE3OTQyMzcxNTMyOHwwMTk3MWQ0ZS02ZQ
    
    After:
    https://screencast.googleplex.com/cast/NTA5MzM4MzMxNDg2NjE3Nnw1YzFmYzU1MS1lZQ
    
    Test: Verified on DUT
    Test: browser_tests --gtest_filter=*AudioPage*
    Bug: b/335686680
    Change-Id: I576b337b2d670e973605c566568b3f83796ca395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472337
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1291930}

---
+++ chrome/test/data/webui/chromeos/settings/device_page/test_audio_and_captions_page_browser_proxy.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {AudioAndCaptionsPageBrowserProxy} from 'chrome://os-settings/lazy_load.js';
+import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+export class TestAudioAndCaptionsPageBrowserProxy extends TestBrowserProxy
+    implements AudioAndCaptionsPageBrowserProxy {
+  constructor() {
+    super([
+      'setStartupSoundEnabled',
+      'audioAndCaptionsPageReady',
+      'getStartupSoundEnabled',
+    ]);
+  }
+
+  setStartupSoundEnabled(enabled: boolean): void {
+    this.methodCalled('setStartupSoundEnabled', enabled);
+    webUIListenerCallback('startup-sound-setting-retrieved', enabled);
+  }
+
+  audioAndCaptionsPageReady(): void {
+    this.methodCalled('audioAndCaptionsPageReady');
+  }
+
+  getStartupSoundEnabled(): void {
+    this.methodCalled('getStartupSoundEnabled');
+  }
+}
