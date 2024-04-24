5e707bc5411799a582509e7c7dd305d851852ec7
Jian Li <jianli@chromium.org>
2024-04-24 20:48:37 +0000

    Add reliability logging interface for user tapping a card
    Bug: 336152471
    Change-Id: I89cebbe0fa2aceda87c5cc26237b45339d0e4f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5469091
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292074}

--- chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/feed/FeedSurfaceScope.java
+++ chrome/browser/xsurface/android/java/src/org/chromium/chrome/browser/xsurface/feed/FeedSurfaceScope.java
@@ -29,4 +29,8 @@ public interface FeedSurfaceScope extends SurfaceScope {
     default FeedUserInteractionReliabilityLogger getUserInteractionReliabilityLogger() {
         return new FeedUserInteractionReliabilityLogger() {};
     }
+
+    default FeedCardOpeningReliabilityLogger getCardOpeningReliabilityLogger() {
+        return new FeedCardOpeningReliabilityLogger() {};
+    }
 }
