736d156beb59ca8b9d465dcc274915fcd7bd1b04
Duong Dac <ddac@google.com>
2024-04-24 12:52:17 +0000

    [iOS] Variable kMinorModeRestrictionsFetchDeadlineMs
    The fixed value of kMinorModeRestrictionsFetchDeadlineMs might be too
    short, causing system capability values to be ignored.
    
    Bug: 336535566
    Change-Id: I7096c924dac76d24f1eabddc04a404111088ae51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477787
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291831}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_capabilities_fetcher_unittest.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_capabilities_fetcher_unittest.mm
@@ -33,11 +33,6 @@ namespace {
 
 const char kTestEmail[] = "test@gmail.com";
 
-// Short timeout to wait for asynchronously fetching already available system
-// capabilities.
-constexpr base::TimeDelta kFetchImmediatelyAvailableCapabilityDeadline =
-    base::Milliseconds(20);
-
 class HistorySyncCapabilitiesFetcherTest
     : public PlatformTest,
       public ::testing::WithParamInterface<std::tuple<bool, bool>> {
@@ -307,7 +302,8 @@ TEST_P(HistorySyncCapabilitiesFetcherTest, TestCapabilityFetchDeadline) {
   if (IsFetchingImmediatelyAvailableCapabilities()) {
     [fetcher_ fetchImmediatelyAvailableRestrictionCapabilityWithCallback:
                   std::move(callback)];
-    scoped_clock.Advance(kFetchImmediatelyAvailableCapabilityDeadline);
+    scoped_clock.Advance(base::Milliseconds(
+        switches::kFetchImmediatelyAvailableCapabilityDeadlineMs.Get()));
     run_loop.Run();
 
     histogram_tester.ExpectTotalCount(
