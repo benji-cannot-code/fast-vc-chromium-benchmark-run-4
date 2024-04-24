25a73be11504eb0eaca6ba6cd5d7f80a66cb3ab5
weidongliu <weidongliu@chromium.org>
2024-04-24 02:39:05 +0000

    views: use CalculatePreferredSize(SizeBounds) in //ash/shelf.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    This CL was uploaded by git cl split.
    
    Bug: 40232718
    Change-Id: Id8cca39cfa353ae668943045062aa1979f6c19c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471089
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291668}

--- ash/shelf/scrollable_shelf_view.cc
+++ ash/shelf/scrollable_shelf_view.cc
@@ -224,7 +224,7 @@ void ScrollableShelfContainerView::Layout(PassKey) {
   // CalculateIdealSize. Because ShelfView::CalculatePreferredSize relies on the
   // bounds of app icon. Meanwhile, the icon's bounds may be updated by
   // animation.
-  const gfx::Rect ideal_bounds = gfx::Rect(CalculatePreferredSize());
+  const gfx::Rect ideal_bounds = gfx::Rect(CalculatePreferredSize({}));
 
   const gfx::Rect local_bounds = GetLocalBounds();
   gfx::Rect shelf_view_bounds =
@@ -563,7 +563,7 @@ gfx::Insets ScrollableShelfView::CalculateMirroredEdgePadding(
       available_local_bounds.width(), available_local_bounds.height());
 
   int gap = CanFitAllAppsWithoutScrolling(available_local_bounds.size(),
-                                          CalculatePreferredSize())
+                                          CalculatePreferredSize({}))
                 ? available_size_for_app_icons - icons_size
                 : 0;  // overflow
 
@@ -713,8 +713,9 @@ const Shelf* ScrollableShelfView::GetShelf() const {
   return shelf_view_->shelf();
 }
 
-gfx::Size ScrollableShelfView::CalculatePreferredSize() const {
-  return shelf_container_view_->GetPreferredSize();
+gfx::Size ScrollableShelfView::CalculatePreferredSize(
+    const views::SizeBounds& available_size) const {
+  return shelf_container_view_->GetPreferredSize(available_size);
 }
 
 void ScrollableShelfView::Layout(PassKey) {
