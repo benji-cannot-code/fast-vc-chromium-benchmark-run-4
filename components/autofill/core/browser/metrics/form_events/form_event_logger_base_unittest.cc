fd8580d1076bf8be6c06a2d2f01a006855f478fc
Norge Vizcay <vizcay@google.com>
2024-04-24 21:15:08 +0000

    Remove kAutofillEnableEmailHeuristicOnlyAddressForms flag from tests.
    The flag has been default enabled in a previous CL.
    
    Bug: b/322286677
    Change-Id: Iec8b98388c55ce772566a1393ca07f503b95656a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482072
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292092}

--- components/autofill/core/browser/metrics/form_events/form_event_logger_base_unittest.cc
+++ components/autofill/core/browser/metrics/form_events/form_event_logger_base_unittest.cc
@@ -555,8 +555,6 @@ class FormEventLoggerBaseEmailHeuristicOnlyMetricsTest
 
   // Fillable form.
   FormData form_;
-  base::test::ScopedFeatureList features_{
-      features::kAutofillEnableEmailHeuristicOnlyAddressForms};
 };
 
 void FormEventLoggerBaseEmailHeuristicOnlyMetricsTest::SetUp() {
