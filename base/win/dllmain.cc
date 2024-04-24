cd8fef5368231f30fdb839106d594e1075ae21b6
danakj <danakj@chromium.org>
2024-04-24 18:31:43 +0000

    Opt //base into unsafe-buffer-usage warnings, except PA and third_party
    Any files that have warnings are marked for now with
    `#pragma allow_unsafe_buffers` to allow them to compile. We can remove
    the pragmas as we make progress through //base.
    
    This will prevent new code from being added that is not bounds-safe.
    
    When clang plugins are not enabled use -Wno-unknown-pragmas to
    prevent errors on the pragmas we use to control the plugin. We
    avoid adding a define telling code if plugins are enabled, which
    would be needed to put the pragma behind a macro instead.
    
    Bug: 40284755
    Change-Id: If3ca901c5895dbc2f99dee63c5d350f44cb46029
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5457820
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291999}

--- base/win/dllmain.cc
+++ base/win/dllmain.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
+#pragma allow_unsafe_buffers
+#endif
+
 // Windows doesn't support pthread_key_create's destr_function, and in fact
 // it's a bit tricky to get code to run when a thread exits.  This is
 // cargo-cult magic from http://www.codeproject.com/threads/tls.asp.
