7adaaf7da3326299a9710af67a45764d19f4aa6c
Shakti Sahu <shaktisahu@chromium.org>
2024-04-24 22:26:04 +0000

    [Tab Group Sync] Moving feature utils to a separate class
    This CL moves the features and params related logic for tab
    group sync to a separate class. Expected to have more logic
    here.
    
    Also, disabling the feature temporarily to complete a refactor.
    
    Bug: b/336385437
    Change-Id: Iff2d528bb1528414f048643cff54dd4d5c87656d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5480002
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292130}

---
+++ chrome/browser/tab_group_sync/android/java/src/org/chromium/chrome/browser/tab_group_sync/TabGroupSyncFeatures.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_group_sync;
+
+/** Utility class for defining features and params related to tab group sync. */
+public final class TabGroupSyncFeatures {
+
+    /** Whether tab group sync is enabled. */
+    public static boolean isTabGroupSyncEnabled() {
+        // TODO(b/336385437): Disabling the feature to do a refactor. We can't have the feature
+        // running during the transition since it will break or crash.
+        // return ChromeFeatureList.isEnabled(ChromeFeatureList.TAB_GROUP_SYNC_ANDROID);
+        return false;
+    }
+}
