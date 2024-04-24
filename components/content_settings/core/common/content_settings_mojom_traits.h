fd16faeb0fdaad59d1caaeeb3ab80ca48092ccb1
Jonathan Njeunje <njeunje@chromium.org>
2024-04-24 18:12:45 +0000

    Adds a field trial testing config for net::features:: kTpcdMetadataStagedRollback
    This CL initiates the preliminary steps needed to launch the
    ThirdPartyCookieDeprecationMetadataStageControl Finch Study in Dev.
    
    The experiment is intended at helping us understand the chrome client
    stability and performance impact of this feature.
    
    This change uncovered some mojo lagging configurations: this CL also adds those configs.
    
    
    Bug: b:336588050, b:336776248
    Change-Id: I79c71b605d6998b6250121b35b73371f31b1efd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478129
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Commit-Queue: Jonathan Njeunje <njeunje@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291990}

--- components/content_settings/core/common/content_settings_mojom_traits.h
+++ components/content_settings/core/common/content_settings_mojom_traits.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.h"
 #include "components/content_settings/core/common/content_settings.mojom-shared.h"
 #include "components/content_settings/core/common/content_settings_constraints.h"
+#include "components/content_settings/core/common/content_settings_enums.mojom-shared.h"
 #include "components/content_settings/core/common/content_settings_metadata.h"
 #include "mojo/public/cpp/base/time_mojom_traits.h"
 #include "mojo/public/cpp/base/values_mojom_traits.h"
@@ -122,6 +123,11 @@ struct StructTraits<content_settings::mojom::RuleMetaDataDataView,
   static const content_settings::mojom::TpcdMetadataRuleSource&
   tpcd_metadata_rule_source(const content_settings::RuleMetaData& r) {
     return r.tpcd_metadata_rule_source_;
+  }
+
+  static const content_settings::mojom::TpcdMetadataCohort&
+  tpcd_metadata_cohort(const content_settings::RuleMetaData& r) {
+    return r.tpcd_metadata_cohort_;
   }
 
   static bool Read(content_settings::mojom::RuleMetaDataDataView data,
--- components/content_settings/core/common/content_settings_mojom_traits_unittest.cc
+++ components/content_settings/core/common/content_settings_mojom_traits_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings.mojom-shared.h"
 #include "components/content_settings/core/common/content_settings.mojom.h"
 #include "components/content_settings/core/common/content_settings_constraints.h"
+#include "components/content_settings/core/common/content_settings_enums.mojom-shared.h"
 #include "components/content_settings/core/common/content_settings_metadata.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "mojo/public/cpp/test_support/test_utils.h"
@@ -76,6 +77,8 @@ TEST(ContentSettingsTraitsTest, Roundtrips_RuleMetadata) {
       content_settings::mojom::SessionModel::USER_SESSION);
   original.set_tpcd_metadata_rule_source(
       content_settings::mojom::TpcdMetadataRuleSource::SOURCE_TEST);
+  original.set_tpcd_metadata_cohort(
+      content_settings::mojom::TpcdMetadataCohort::GRACE_PERIOD_FORCED_OFF);
 
   content_settings::RuleMetaData round_tripped;
 
