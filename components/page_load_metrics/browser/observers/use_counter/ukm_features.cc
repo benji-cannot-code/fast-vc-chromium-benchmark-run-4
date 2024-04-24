6edcfb005ff99021c4d6a661403c550d850de098
Annie Sullivan <sullivan@chromium.org>
2024-04-24 17:35:35 +0000

    Add isInputPending use counter to UKM
    Bug: 336566553
    Change-Id: I6bbc99f401551894414477ebb3091516f87ad0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482364
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291967}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -400,7 +400,8 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kServiceWorkerStaticRouter_Evaluate,
           WebFeature::kNavigatorCookieEnabledThirdParty,
           WebFeature::kSimplifyLoadingTransparentPlaceholderImage,
-          WebFeature::kFedCmContinueOnResponse
+          WebFeature::kFedCmContinueOnResponse,
+          WebFeature::kSchedulingIsInputPending
       }));
   return *opt_in_features;
 }
