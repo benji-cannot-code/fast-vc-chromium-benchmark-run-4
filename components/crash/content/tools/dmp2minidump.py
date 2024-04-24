9103ec5afb5e414a645d0f6e643588580ad31e7f
Peter Wen <wnwen@chromium.org>
2024-04-24 15:16:41 +0000

    components/crash/content/tools: Format with yapf
    Added .style.yapf file to specify consistent yapf formatting for Python.
    
    Unnecessary import removed from dmp2minidump.py.
    
    Ran:
    $ git cl format --full
    
    Bug: None
    Change-Id: Ic82051bdc8b625870bed9b259e7593b7ffd52176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5477689
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Peter Wen <wnwen@chromium.org>
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291881}

--- components/crash/content/tools/dmp2minidump.py
+++ components/crash/content/tools/dmp2minidump.py
@@ -3,11 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2013 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """A tool to extract minidumps from dmp crash dumps."""
 
 from __future__ import print_function
-import os
 import sys
 from cgi import parse_multipart
 
