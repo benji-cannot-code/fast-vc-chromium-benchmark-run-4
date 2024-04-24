386ae4ddaa945867b32937dfc3e24e7cb4324dfc
Anders Hartvoll Ruud <andruud@chromium.org>
2024-04-24 17:31:21 +0000

    [scroll-animations] Remove runtime flag CSSViewTimelineInsetShorthand
    This has been enabled since M118.
    
    Bug: 40925527
    Change-Id: I48891420d06259605af5e5260f5624d2204630f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476470
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291963}

--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -1238,7 +1238,6 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
     case CSSPropertyID::kTextSpacing:
     case CSSPropertyID::kTransition:
     case CSSPropertyID::kViewTimeline:
-    case CSSPropertyID::kAlternativeViewTimelineWithInset:
     case CSSPropertyID::kWebkitColumnBreakAfter:
     case CSSPropertyID::kWebkitColumnBreakBefore:
     case CSSPropertyID::kWebkitColumnBreakInside:
