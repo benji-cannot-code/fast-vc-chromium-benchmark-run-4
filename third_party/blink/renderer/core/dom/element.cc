f01f3715c1e65333c156a7cb77ded99608d5afdd
Noam Rosenthal <nrosenthal@chromium.org>
2024-04-24 16:04:05 +0000

    Keep pseudo-elements during moveBefore
    This happens by default since we don't detach the layout,
    but this CL removes the assertions that the psuedo-elements are gone.
    
    Bug: 40150299
    Change-Id: I6200dd8ff0902b1e0d2950009ce46f51f53eda59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481991
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1291905}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -2952,7 +2952,7 @@ Node::InsertionNotificationRequest Element::InsertedInto(
   // by the time we reach updateId
   ContainerNode::InsertedInto(insertion_point);
 
-  DCHECK(!GetElementRareData() || !GetElementRareData()->HasPseudoElements());
+  DCHECK(!GetElementRareData() || !GetElementRareData()->HasPseudoElements() || GetDocument().StatePreservingAtomicMoveInProgress());
 
   RecomputeDirectionFromParent();
 
@@ -3127,7 +3127,7 @@ void Element::RemovedFrom(ContainerNode& insertion_point) {
       context->ElementDisconnected();
     }
 
-    DCHECK(!data->HasPseudoElements());
+    DCHECK(!data->HasPseudoElements() || GetDocument().StatePreservingAtomicMoveInProgress());
 
     if (AccessibleNode* accessible_node = ExistingAccessibleNode()) {
       accessible_node->DetachedFromDocument();
