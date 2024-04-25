a807d9e8379d6a2e6112e53443e31b560a69c823
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-04-25 21:49:55 +0000

    Do not restore layer tree of a window to a stale state
    ScopedWindowTreeSynchroizer stores the state of layer tree as it was
    just before entering overview. However in overview, the window can
    transition to a window state in which the cached layer tree state is
    becomes invalid.
    
    In this CL, we identify and reset the cached layer information in such
    cases.
    
    Bug: b/336626787
    Change-Id: I26c4894b297818d679fd26e37c3b283c92f97f13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486579
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292716}

--- ash/wm/scoped_layer_tree_synchronizer.cc
+++ ash/wm/scoped_layer_tree_synchronizer.cc
@@ -410,6 +410,10 @@ ScopedLayerTreeSynchronizerBase::ScopedLayerTreeSynchronizerBase(
 
 ScopedLayerTreeSynchronizerBase::~ScopedLayerTreeSynchronizerBase() = default;
 
+void ScopedLayerTreeSynchronizerBase::ResetCachedLayerInfo() {
+  original_layers_info_.clear();
+}
+
 bool ScopedLayerTreeSynchronizerBase::SynchronizeLayerTreeRoundedCorners(
     ui::Layer* layer,
     bool consider_curvature,
@@ -525,10 +529,6 @@ void ScopedLayerTreeSynchronizerBase::RestoreLayerTreeImpl(ui::Layer* layer) {
   for (ui::Layer* child : layer->children()) {
     RestoreLayerTreeImpl(child);
   }
-}
-
-void ScopedLayerTreeSynchronizerBase::ResetCachedLayerInfo() {
-  original_layers_info_.clear();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
