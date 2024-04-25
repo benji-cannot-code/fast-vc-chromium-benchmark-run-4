628356fb2605b25518fed426e79382caaec3712d
Vladimir Levin <vmpstr@chromium.org>
2024-04-25 22:12:55 +0000

    VT: Use BlitRequests in the gpu path for capturing textures
    For now, we still wait for the CopyOutput ack before using the textures
    but at least it's exercising the right path. Since software BlitRequests
    path is not yet available, we would need to maintain two paths.
    
    This is sufficient evidence that we can use it in ipif paint holding
    though
    
    R=khushalsagar@chromium.org, vasilyt@chromium.org
    
    Change-Id: I473b8b22377ac2ce10eb52e2a91810bad28220a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484787
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292733}

--- components/viz/service/surfaces/surface_saved_frame.h
+++ components/viz/service/surfaces/surface_saved_frame.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/quads/compositor_render_pass.h"
 #include "components/viz/common/resources/release_callback.h"
 #include "components/viz/service/viz_service_export.h"
+#include "gpu/command_buffer/client/client_shared_image.h"
 
 namespace gpu {
 class SharedImageInterface;
@@ -106,7 +107,7 @@ class VIZ_SERVICE_EXPORT SurfaceSavedFrame {
 
   std::unique_ptr<CopyOutputRequest> CreateCopyRequestIfNeeded(
       const CompositorRenderPass& render_pass,
-      const CompositorRenderPassList& render_pass_list) const;
+      const CompositorRenderPassList& render_pass_list);
 
   void NotifyCopyOfOutputComplete(size_t shared_index,
                                   const RenderPassDrawData& info,
@@ -145,6 +146,10 @@ class VIZ_SERVICE_EXPORT SurfaceSavedFrame {
   CompositorFrameTransitionDirective directive_;
   raw_ptr<gpu::SharedImageInterface> shared_image_interface_;
   TransitionDirectiveCompleteCallback directive_finished_callback_;
+
+  // Store the blit images while the copy output request is ongoing.
+  base::flat_map<size_t, scoped_refptr<gpu::ClientSharedImage>>
+      blit_shared_images_;
 
   std::optional<FrameResult> frame_result_;
 
