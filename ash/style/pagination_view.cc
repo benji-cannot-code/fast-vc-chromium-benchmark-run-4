3fff72aa4e73c9d15874feb0ef36f22fd4783bfc
weidongliu <weidongliu@chromium.org>
2024-04-25 01:51:44 +0000

    views: use CalculatePreferredSize(SizeBounds) in //ash/style.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    This CL was uploaded by git cl split.
    
    Bug: 40232718
    Change-Id: I9a5f4a662b904635a10308d6fde5d8df46bb8335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467276
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292242}

--- ash/style/pagination_view.cc
+++ ash/style/pagination_view.cc
@@ -95,7 +95,8 @@ class IndicatorButton : public views::Button {
   }
 
   // views::Button:
-  gfx::Size CalculatePreferredSize() const override {
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override {
     return gfx::Size(kIndicatorButtonSize, kIndicatorButtonSize);
   }
 
@@ -341,7 +342,8 @@ PaginationView::PaginationView(PaginationModel* model, Orientation orientation)
 
 PaginationView::~PaginationView() = default;
 
-gfx::Size PaginationView::CalculatePreferredSize() const {
+gfx::Size PaginationView::CalculatePreferredSize(
+    const views::SizeBounds& available_size) const {
   const int total_pages = model_->total_pages();
   if (total_pages < kMinNumPages) {
     return gfx::Size();
