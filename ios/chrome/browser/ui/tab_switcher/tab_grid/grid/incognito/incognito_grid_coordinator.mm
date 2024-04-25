56bb1adad460bab340ffb4f306578936eb43c1ad
Gauthier Ambard <gambard@chromium.org>
2024-04-25 10:07:15 +0000

    [iOS] Fix incognito transition animation
    Previous CL forgot to check for the presence of TabGroup.
    
    Fixed: 336962210
    Change-Id: Ifaaa24f8f88b8daaff35facd3c73947d093c115b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5488117
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292352}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/incognito/incognito_grid_coordinator.mm
@@ -100,8 +100,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (LegacyGridTransitionLayout*)transitionLayout {
   if (IsTabGroupInGridEnabled()) {
-    return [self.tabGroupCoordinator.viewController
-                .gridViewController transitionLayout];
+    if (self.tabGroupCoordinator) {
+      return [self.tabGroupCoordinator.viewController
+                  .gridViewController transitionLayout];
+    }
   }
   return [self.gridViewController transitionLayout];
 }
