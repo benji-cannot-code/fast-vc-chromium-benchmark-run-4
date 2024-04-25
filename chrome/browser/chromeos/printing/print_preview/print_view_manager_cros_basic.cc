a1a64d313d57fdcbaed5e5be1585a35dfb38313d
Jimmy <jimmyxgong@chromium.org>
2024-04-25 23:22:37 +0000

    cros-preview: Add PrintViewManagerCros* skeleton
    PrintViewManagerCros is responsible for handling printing
    commands for a webcontent. This is only a skeleton impl.
    
    Bug: 334993067
    Change-Id: I63201f117a91d888c1564844611ef9b7d3be8c14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472461
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292757}

---
+++ chrome/browser/chromeos/printing/print_preview/print_view_manager_cros_basic.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/printing/print_preview/print_view_manager_cros_basic.h"
+
+#include "content/public/browser/web_contents.h"
+
+namespace chromeos {
+
+PrintViewManagerCrosBasic::PrintViewManagerCrosBasic(
+    content::WebContents* web_contents)
+    : PrintViewManagerCrosBase(web_contents) {}
+
+}  // namespace chromeos
