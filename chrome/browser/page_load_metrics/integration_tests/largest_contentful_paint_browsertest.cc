663e3b151f0b22319d8571bcee324927c94febab
François Doray <fdoray@chromium.org>
2024-04-25 16:00:02 +0000

    [gardener] Disable some LcpBreakdownTimingsTest* tests on ASAN.
    Bug: 337012486
    Change-Id: Id2db7e4508ff3912ab3b87b2c91b2d856e9f99c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5489880
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292480}

--- chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
+++ chrome/browser/page_load_metrics/integration_tests/largest_contentful_paint_browsertest.cc
@@ -1357,7 +1357,8 @@ IN_PROC_BROWSER_TEST_F(LcpBreakdownTimingsTest, MAYBE_WrittenAsOuterHtmlImage) {
   Validate();
 }
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
+// Flaky timeout with ASAN (crbug.com/337012486)
+#if BUILDFLAG(IS_CHROMEOS_LACROS) || defined(ADDRESS_SANITIZER)
 #define MAYBE_DocumentWrittenImage DISABLED_DocumentWrittenImage
 #else
 #define MAYBE_DocumentWrittenImage DocumentWrittenImage
@@ -1369,7 +1370,8 @@ IN_PROC_BROWSER_TEST_F(LcpBreakdownTimingsTest, MAYBE_DocumentWrittenImage) {
   Validate();
 }
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
+// Flaky timeout with ASAN (crbug.com/337012486)
+#if BUILDFLAG(IS_CHROMEOS_LACROS) || defined(ADDRESS_SANITIZER)
 #define MAYBE_SrcSetImage DISABLED_SrcSetImage
 #else
 #define MAYBE_SrcSetImage SrcSetImage
