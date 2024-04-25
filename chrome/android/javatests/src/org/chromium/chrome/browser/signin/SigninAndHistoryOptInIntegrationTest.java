e7444889dc9bdc8425d292e329853fb8e69409a2
Menghan Yang <myuu@google.com>
2024-04-25 13:17:13 +0000

    [Android] Fix failing SigninAndHistoryOptInIntegrationTest
    In tests, AccountInfoServiceProvider.getAccountInfoByEmail() returns a
    null CoreAccountInfo just after an account is added, if the
    SEED_ACCOUNTS_REVAMP flag is disabled.
    
    Given the SEED_ACCOUNT_REVAMP experiment is being launched, this CL
    enables the flag for the failing tests.
    
    Bug: 336371182, 335867572
    Change-Id: Ib02de52c0f4cd491723ab3d65edd409964c7e139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5488079
    Auto-Submit: Menghan Yang <myuu@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292405}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninAndHistoryOptInIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninAndHistoryOptInIntegrationTest.java
@@ -41,7 +41,6 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.util.ApplicationTestUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -59,6 +58,7 @@ import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
 import org.chromium.chrome.test.util.browser.signin.SigninTestRule;
 import org.chromium.chrome.test.util.browser.sync.SyncTestUtil;
+import org.chromium.components.signin.SigninFeatures;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.metrics.SigninAccessPoint;
@@ -363,9 +363,11 @@ public class SigninAndHistoryOptInIntegrationTest {
         verifySigninCancelled();
     }
 
+    // Enabling SEED_ACCOUNTS_REVAMP to avoid failure due to the deprecated seed account flow. See
+    // https://crbug.com/336371182.
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/335867572")
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     public void testWithNoAccount_bottomSheetSignin_requiredHistorySync() {
         launchActivity(
                 NoAccountSigninMode.BOTTOM_SHEET,
@@ -376,9 +378,11 @@ public class SigninAndHistoryOptInIntegrationTest {
         acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ false);
     }
 
+    // Enabling SEED_ACCOUNTS_REVAMP to avoid failure due to the deprecated seed account flow. See
+    // https://crbug.com/336371182.
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/336371182")
+    @EnableFeatures(SigninFeatures.SEED_ACCOUNTS_REVAMP)
     public void testWithNoAccount_instantSignin_requiredHistorySync() {
         CoreAccountInfo accountInfo = AccountManagerTestRule.TEST_ACCOUNT_1;
         mSigninTestRule.setResultForNextAddAccountFlow(Activity.RESULT_OK, accountInfo.getEmail());
