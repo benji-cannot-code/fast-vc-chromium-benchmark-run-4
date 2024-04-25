0ce8c8c56433f3e4d84fc60a9530c491e48ccf7e
gupta.h <gupta.h@samsung.com>
2024-04-25 17:38:45 +0000

    Set touch handle area to be excluded for system gestures
    When a user swipes from the edge such that the swipe
    comes in contact with text selection handle, handle was moving
    and back gesture was also triggered. Both operations must not
    trigger in this scenario.
    
    Added implementation to create exclusion rects which
    tells platform to block the back gesture for such rects.
    Reference:
    https://developer.android.com/develop/ui/views/touch-and-input/gestures/gesturenav#back-gestures
    
    Bug: 41492419
    Change-Id: Idb5de70d87dea4b8e47961f04d79b786b91c1f72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5461516
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Mahesh Machavolu <mahesh.ma@samsung.com>
    Commit-Queue: Mahesh Machavolu <mahesh.ma@samsung.com>
    Cr-Commit-Position: refs/heads/main@{#1292543}

--- content/browser/android/selection/selection_popup_controller.h
+++ content/browser/android/selection/selection_popup_controller.h
@@ -40,6 +40,10 @@ class SelectionPopupController : public RenderWidgetHostConnector {
       const base::android::JavaParamRef<jobject>& obj,
       jboolean hidden);
 
+  base::android::ScopedJavaLocalRef<jobjectArray> GetTouchHandleRects(
+      JNIEnv* env,
+      const base::android::JavaParamRef<jobject>& obj);
+
   // RendetWidgetHostConnector implementation.
   void UpdateRenderProcessConnection(
       RenderWidgetHostViewAndroid* old_rwhva,
