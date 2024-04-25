baa8cadda901c2aa54ea57222a5acb11cdd5540f
Mikihito Matsuura <mikt@google.com>
2024-04-25 05:30:38 +0000

    [Garden] Disable NewTabPageModulesHistoryClustersInteractive... on Linux
    Disable followings:
    - All/NewTabPageModulesHistoryClustersInteractiveUiTest.
      ClickingHistoryClustersCartTileNavigatesToCorrectPage/1
    - All/NewTabPageModulesHistoryClustersInteractiveUiTest.\
      ClickingHistoryClustersRelatedSearchNavigatesToCorrectPage/1
    
    Bug: 335214502
    Change-Id: I53ef337656532ddb9f9077d77a11ab2765e000e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5487317
    Owners-Override: Mikihito Matsuura <mikt@google.com>
    Auto-Submit: Mikihito Matsuura <mikt@google.com>
    Commit-Queue: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292279}

--- chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
+++ chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
@@ -395,7 +395,7 @@ class NewTabPageModulesHistoryClustersInteractiveUiTest
 };
 
 // TODO(crbug.com/335214502): Flaky on Linux/ChromeOS Tests.
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 #define MAYBE_ClickingHistoryClustersRelatedSearchNavigatesToCorrectPage \
   DISABLED_ClickingHistoryClustersRelatedSearchNavigatesToCorrectPage
 #else
@@ -424,7 +424,7 @@ IN_PROC_BROWSER_TEST_P(
 }
 
 // TODO(crbug.com/335214502): Flaky on Linux/ChromeOS Tests.
-#if BUILDFLAG(IS_CHROMEOS)
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 #define MAYBE_ClickingHistoryClustersCartTileNavigatesToCorrectPage \
   DISABLED_ClickingHistoryClustersCartTileNavigatesToCorrectPage
 #else
