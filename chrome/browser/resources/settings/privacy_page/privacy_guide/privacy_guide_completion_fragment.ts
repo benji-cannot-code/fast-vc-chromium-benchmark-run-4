e038131f91f3dccb8010e2d0366d1c2ea9f3198e
Fiona Macintosh <fmacintosh@google.com>
2024-04-25 17:59:23 +0000

    Update privacy guide completion link icons
    Before dark: https://screenshot.googleplex.com/376frgPDYEAxyDK.png
    After dark: https://screenshot.googleplex.com/8LLXcnWFaLsmh55.png
    Before light: https://screenshot.googleplex.com/A2sncajcpQ5tntA.png
    After light: https://screenshot.googleplex.com/B2Y3UGbRVYGZk4Q.png
    
    Bug: b:324565877
    Change-Id: I86604e77e767544323923f2910bffe092749dade
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482726
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292561}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import 'chrome://resources/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/cr_elements/cr_link_row/cr_link_row.js';
-import './privacy_guide_completion_link_row.js';
 import './privacy_guide_fragment_shared.css.js';
+import '../../settings_shared.css.js';
 
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
