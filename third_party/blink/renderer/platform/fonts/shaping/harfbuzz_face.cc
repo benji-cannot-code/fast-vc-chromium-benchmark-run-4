319e156d9efb325b862c4fa16b4d638c64e7b249
Munira Tursunova <moonira@google.com>
2024-04-25 13:23:12 +0000

    Add variation sequences support in text shaping
    More information can be found in design doc [0].
    New text shaping logic:
    -If no variation sequences were found in the segment, no changes should
    be performed to text shaping cycle.
    -If there are variation sequences and all of them were shaped in the
    first fallback list pass, we should finish shaping.
    -If some of the variation sequences were unshaped, but there are fonts
    in fallback list that have a glyph for a base character (ignoring
    variation selector), we should perform second pass through fallback
    list, but this time we should ignore variation selectors in HarBuzz
    callbacks.
    
    Tested with tests from WPT PR [1].
    
    Neutral performance impact on the pages with no variation selectors.
    Compare the pinpoint results on Speedometer3 [2] and
    blink_perf.layout [3] and [4].
    
    [0] https://docs.google.com/document/d/1NyIKm0PnWUwX6j0smDwxDAPSUoiyBHPL95oH2lvjjpc/edit?usp=sharing
    [1] https://github.com/web-platform-tests/wpt/pull/45681
    [2] https://pinpoint-dot-chromeperf.appspot.com/job/128da31bbe0000
    [3] https://pinpoint-dot-chromeperf.appspot.com/job/13e58bc1be0000
    [4] https://pinpoint-dot-chromeperf.appspot.com/job/13bed05dbe0000
    
    Bug: 40628044
    Change-Id: I95c57a66b25de63150f2deba01f2207c21aa484b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424774
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292409}

--- third_party/blink/renderer/platform/fonts/shaping/harfbuzz_face.cc
+++ third_party/blink/renderer/platform/fonts/shaping/harfbuzz_face.cc
@@ -56,6 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/hash_map.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
+#include "third_party/blink/renderer/platform/wtf/thread_specific.h"
+#include "third_party/blink/renderer/platform/wtf/wtf.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkPoint.h"
@@ -76,7 +78,20 @@ void HarfBuzzFace::Trace(Visitor* visitor) const {
   visitor->Trace(harfbuzz_font_data_);
 }
 
-bool HarfBuzzFace::ignore_variation_selectors_ = false;
+bool& GetIgnoreVariationSelectors() {
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(WTF::ThreadSpecific<bool>,
+                                  ignore_variation_selectors, ());
+  return *ignore_variation_selectors;
+}
+
+bool HarfBuzzFace::ShouldIgnoreVariationSelectors() {
+  return GetIgnoreVariationSelectors();
+}
+
+void HarfBuzzFace::SetIgnoreVariationSelectors(bool value) {
+  DCHECK(RuntimeEnabledFeatures::FontVariationSequencesEnabled() || value);
+  GetIgnoreVariationSelectors() = value;
+}
 
 static hb_bool_t HarfBuzzGetGlyph(hb_font_t* hb_font,
                                   void* font_data,
@@ -111,7 +126,8 @@ static hb_bool_t HarfBuzzGetGlyph(hb_font_t* hb_font,
   // `kUnmatchedVSGlyphId`.
   bool consider_variation_selector =
       RuntimeEnabledFeatures::FontVariationSequencesEnabled() &&
-      !HarfBuzzFace::GetIgnoreVariationSelectors() &&
+      !HarfBuzzFace::ShouldIgnoreVariationSelectors() &&
+      Character::IsUnicodeVariationSelector(variation_selector) &&
       Character::IsVariationSequence(unicode, variation_selector);
 
   if (consider_variation_selector) {
