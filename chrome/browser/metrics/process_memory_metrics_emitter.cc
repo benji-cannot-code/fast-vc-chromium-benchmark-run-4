45c14c63193e8d4983488410f8d2be25ef5df976
Andrew Grieve <agrieve@chromium.org>
2024-04-25 20:21:53 +0000

    Android: Disable memory dumps on emulator with 16k page size
    This is a work-around to make chrome & webview not crash in this
    environment. It's not clear whether the crash is due to a chrome bug, or
    an emulator bug (does not happen on real devices), so applying this
    (hopefully temporary) work-around.
    
    Memory dumps can still be enabled (to test the crash) via:
    bin/chrome_public_apk run --args=--test-memory-log-delay-in-minutes=-1
    
    Also changes --test-memory-log-delay-in-minutes=0 to use a delay of
    1 second rather than 0 seconds (or else chrome just freezes up).
    
    Bug: 330751658
    Change-Id: I77e54abba8f3922c9f4824f4fbb0ea75607dc010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486189
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292662}

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -12,10 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/allocator/buildflags.h"
 #include "base/allocator/partition_allocator/src/partition_alloc/partition_alloc_buildflags.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
+#include "base/logging.h"
+#include "base/memory/page_size.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/process/process_metrics.h"
@@ -26,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/tab_footprint_aggregator.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/common/chrome_switches.h"
 #include "components/metrics/metrics_data_validation.h"
 #include "components/performance_manager/public/graph/frame_node.h"
 #include "components/performance_manager/public/graph/graph.h"
@@ -1141,6 +1145,27 @@ ProcessMemoryMetricsEmitter::ProcessMemoryMetricsEmitter(
 
 void ProcessMemoryMetricsEmitter::FetchAndEmitProcessMemoryMetrics() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+// https://crbug.com/330751658 (hopefully temporary).
+#if BUILDFLAG(IS_ANDROID) && defined(ARCH_CPU_X86_64)
+  // Do not skip when command-line is used to specifically test it.
+  if (base::GetPageSize() == 16 * 1024) {
+    const base::CommandLine* command_line =
+        base::CommandLine::ForCurrentProcess();
+    int test_delay_in_minutes = 0;
+    if (command_line->HasSwitch(switches::kTestMemoryLogDelayInMinutes)) {
+      base::StringToInt(command_line->GetSwitchValueASCII(
+                            switches::kTestMemoryLogDelayInMinutes),
+                        &test_delay_in_minutes);
+    }
+    // Allow a negative value to test the crashing scenario.
+    if (test_delay_in_minutes >= 0) {
+      LOG(WARNING) << "Ignoring dump request to avoid emulator crash. "
+                   << "https://crbug.com/330751658";
+      return;
+    }
+  }
+#endif
 
   MarkServiceRequestsInProgress();
 
