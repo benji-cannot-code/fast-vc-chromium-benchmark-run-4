9ddd6dd3c186bca17f1228067eb003ffb1d05fbc
Jose Dapena Paz <jdapena@igalia.com>
2024-04-25 11:41:13 +0000

    IWYU: missing include for usage of std::array in feature_keys.h
    Bug: 41455655
    Change-Id: I14e87e12a5dc029fbf8db4f3bef5475d498be135
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472627
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1292376}

--- components/optimization_guide/core/model_execution/feature_keys.h
+++ components/optimization_guide/core/model_execution/feature_keys.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_FEATURE_KEYS_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_FEATURE_KEYS_H_
 
+#include <array>
 #include <optional>
 
 #include "base/notreached.h"
