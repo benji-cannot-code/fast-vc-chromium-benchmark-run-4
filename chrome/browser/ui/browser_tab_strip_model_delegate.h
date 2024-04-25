80dbc3056991944e274dda7a911b07f0a8ddf0de
David Pennington <dpenning@chromium.org>
2024-04-25 20:34:53 +0000

    [STGV2] Implement "close tabs" commands on the tab context menu
    Change-Id: I562018707fc0195873d156200b3de03474e1ed03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5454940
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292672}

--- chrome/browser/ui/browser_tab_strip_model_delegate.h
+++ chrome/browser/ui/browser_tab_strip_model_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BROWSER_TAB_STRIP_MODEL_DELEGATE_H_
 #define CHROME_BROWSER_UI_BROWSER_TAB_STRIP_MODEL_DELEGATE_H_
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_delegate.h"
@@ -67,6 +68,9 @@ class BrowserTabStripModelDelegate : public TabStripModelDelegate {
   bool CanGoBack(content::WebContents* web_contents) override;
   bool IsNormalWindow() override;
   BrowserWindowInterface* GetBrowserWindowInterface() override;
+  bool ConfirmDestroyingGroups(
+      const std::vector<tab_groups::TabGroupId>& group_ids,
+      base::OnceCallback<void()> callback) override;
 
   void CloseFrame();
 
