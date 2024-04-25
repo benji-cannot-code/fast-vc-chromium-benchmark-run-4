0556ccf9d66cf8d5067f189431f5412bd8df828b
Brian Osman <brianosman@google.com>
2024-04-25 15:57:37 +0000

    Remove support for closed arcs from ArcBuilder
    These kinds of arcs (closed, but not involving the circle center) are
    very rare, and also not handled directly by Skia. We just end up turning
    them into paths anyway.
    
    Change-Id: Id53641f3351c1bf3fe824d8ea5d9da9fac06d2da
    Bug: b/330506337
    Bug: b/336822959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484567
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292478}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_path.h
@@ -62,8 +62,6 @@ class MODULES_EXPORT CanvasPath : public GarbageCollectedMixin,
     float radius;
     float start_angle_radians;
     float sweep_angle_radians;
-    // Whether the path was closed.
-    bool closed;
   };
 
   virtual ~CanvasPath() = default;
@@ -147,7 +145,7 @@ class MODULES_EXPORT CanvasPath : public GarbageCollectedMixin,
   // Returns true if the CanvasPath represents an arc. In some cases (such as
   // using constructor that takes a Path) this can return false even though the
   // path is an arc
-  bool IsArc() const { return arc_builder_.HasArcOrIsClosed(); }
+  bool IsArc() const { return arc_builder_.HasArc(); }
 
   // Returns the points that make up the line. Only valid if IsLine() is true.
   const Line& line() const {
@@ -262,36 +260,21 @@ class MODULES_EXPORT CanvasPath : public GarbageCollectedMixin,
 
     ALWAYS_INLINE void Clear() { state_ = State::kEmpty; }
 
-    // Returns true if ArcTo() may be called.
-    bool CanCreateArcTo() const { return state_ == State::kEmpty; }
-
-    bool IsClosed() const { return state_ == State::kClosed; }
-
-    void Close() {
-      // Close() may be called multiple times.
-      DCHECK(state_ == State::kClosed || state_ == State::kArc);
-      state_ = State::kClosed;
-      arc_.closed = true;
-    }
-
     void ArcTo(float x,
                float y,
                float radius,
                float start_angle_radians,
                float sweep_angle_radians) {
-      DCHECK(CanCreateArcTo());
+      DCHECK(IsEmpty());
       state_ = State::kArc;
       arc_.x = x;
       arc_.y = y;
       arc_.radius = radius;
       arc_.start_angle_radians = start_angle_radians;
       arc_.sweep_angle_radians = sweep_angle_radians;
-      arc_.closed = false;
     }
 
-    bool HasArcOrIsClosed() const {
-      return state_ == State::kArc || state_ == State::kClosed;
-    }
+    bool HasArc() const { return state_ == State::kArc; }
 
     gfx::RectF BoundingRect() const;
 
@@ -306,7 +289,6 @@ class MODULES_EXPORT CanvasPath : public GarbageCollectedMixin,
     enum class State {
       kEmpty,
       kArc,     // ArcTo() was called.
-      kClosed,  // Close() was called after ArcTo().
     };
 
     State state_ = State::kEmpty;
