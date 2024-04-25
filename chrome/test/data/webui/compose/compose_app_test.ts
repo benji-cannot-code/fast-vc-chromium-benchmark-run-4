2738d953532b35fecb37468823bd067edfa3b9b8
Justin DeWitt <dewittj@chromium.org>
2024-04-25 17:38:32 +0000

    [Compose] Default-disable the dogfood footer for on device.
    Is not desirable in external usage of this feature.
    
    Fixed: 336877207
    Change-Id: I366e66c2e8bef31156406844322b909db6712c7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5485762
    Auto-Submit: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292542}

--- chrome/test/data/webui/compose/compose_app_test.ts
+++ chrome/test/data/webui/compose/compose_app_test.ts
@@ -787,6 +787,10 @@ suite('ComposeApp', () => {
   });
 
   test('PartialResponseIsShown', async () => {
+    loadTimeData.overrideValues({
+      enableOnDeviceDogfoodFooter: true,
+    });
+
     // Make streaming work instantly.
     app.$.resultText.enableInstantStreamingForTesting();
 
