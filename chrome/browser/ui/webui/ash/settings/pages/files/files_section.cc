3ccee6423c966f8ffe6afe031a372f169a9b87ca
Aida Zolic <aidazolic@chromium.org>
2024-04-25 18:03:29 +0000

    [skyvault] Disable MyFiles row in storage
    Disables the MyFiles row in the storage management settings page, if
    local files are disabled by policy.
    
    See go/skyvault-dd for more context.
    
    Example with SkyVault enabled:
    http://screen/p2yhNqjiQX2yaXi
    
    Fixed: b/325005312
    Change-Id: I31c451d00037bbdf27513dadea4fccca4a4b2f18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5400797
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292564}

--- chrome/browser/ui/webui/ash/settings/pages/files/files_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/files/files_section.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_handler.h"
 #include "chrome/browser/ui/webui/ash/smb_shares/smb_shares_localized_strings_provider.h"
 #include "chrome/browser/ui/webui/webui_util.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/user_manager/user.h"
@@ -298,6 +299,11 @@ void FilesSection::AddLoadTimeData(content::WebUIDataSource* html_source) {
   html_source->AddBoolean(
       "enableDriveFsBulkPinning",
       drive::util::IsDriveFsBulkPinningAvailable(profile()));
+
+  html_source->AddBoolean(
+      "enableSkyVault",
+      base::FeatureList::IsEnabled(::features::kSkyVault) &&
+          base::FeatureList::IsEnabled(::features::kSkyVaultV2));
 }
 
 void FilesSection::AddHandlers(content::WebUI* web_ui) {
