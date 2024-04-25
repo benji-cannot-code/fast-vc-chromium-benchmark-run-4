32e9c0852adbd3b871e0ffa91dc7d75a27b9e6f8
Joshua Pawlicki <waffles@chromium.org>
2024-04-25 20:40:37 +0000

    Cleanup: Remove puffin patches feature.
    Assume the feature is enabled everywhere.
    
    This is in response to a request on chromium-dev to clean up unused
    features; I didn't file a bug for it (a bug seems like busywork).
    
    Change-Id: I13ab7c293e3011308a5f51c1ac3a3835f5f4bb83
    Fixed: b/337071324
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490491
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292675}

--- components/update_client/features.cc
+++ components/update_client/features.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 namespace update_client::features {
-BASE_FEATURE(kPuffinPatches, "PuffinPatches", base::FEATURE_ENABLED_BY_DEFAULT);
-
 #if BUILDFLAG(IS_MAC)
 BASE_FEATURE(kDynamicCrxDownloaderPriority,
              "DynamicCrxDownloaderPriority",
--- components/update_client/features.h
+++ components/update_client/features.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace update_client::features {
-BASE_DECLARE_FEATURE(kPuffinPatches);
-
 #if BUILDFLAG(IS_MAC)
 BASE_DECLARE_FEATURE(kDynamicCrxDownloaderPriority);
 // The minimum size (in bytes) for which background downloads should be
