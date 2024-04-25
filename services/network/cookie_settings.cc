556d998f6f32d5554943e18ade171b66933d09d5
Shuran Huang <shuuran@chromium.org>
2024-04-25 13:59:20 +0000

    Consider source of the content settings for cookie access in network
    Bug: 335639259, 40538766
    Change-Id: Id1a13792e119caf47f53b4ed2b43a7dec8680a96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468522
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292419}

--- services/network/cookie_settings.cc
+++ services/network/cookie_settings.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/containers/contains.h"
+#include "base/containers/fixed_flat_map.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
@@ -115,6 +116,26 @@ bool IsOriginOpaqueHttpOrHttps(const url::Origin* top_frame_origin) {
   return url.SchemeIsHTTPOrHTTPS();
 }
 
+// This map should stay in sync with the `kProviderNamesSourceMap` in
+// components/content_settings/core/browser/host_content_settings_map.cc.
+// TODO(https://crbug.com/40538766): remove this mapping once we use enum
+// instead of string for storing setting source.
+constexpr auto kProviderNamesSourceMap =
+    base::MakeFixedFlatMap<base::StringPiece, content_settings::SettingSource>({
+        {"webui_allowlist", content_settings::SETTING_SOURCE_ALLOWLIST},
+        {"policy", content_settings::SETTING_SOURCE_POLICY},
+        {"supervised_user", content_settings::SETTING_SOURCE_SUPERVISED},
+        {"extension", content_settings::SETTING_SOURCE_EXTENSION},
+        {"installed_webapp_provider",
+         content_settings::SETTING_SOURCE_INSTALLED_WEBAPP},
+        {"notification_android", content_settings::SETTING_SOURCE_USER},
+        {"one_time", content_settings::SETTING_SOURCE_USER},
+        {"preference", content_settings::SETTING_SOURCE_USER},
+        {"default", content_settings::SETTING_SOURCE_USER},
+        {"tests", content_settings::SETTING_SOURCE_USER},
+        {"tests_other", content_settings::SETTING_SOURCE_USER},
+    });
+
 }  // namespace
 
 // static
@@ -192,7 +213,7 @@ void CookieSettings::set_content_settings(
             ContentSettingsPattern::Wildcard(),
             ContentSettingsPattern::Wildcard(),
             base::Value(CONTENT_SETTING_ALLOW),
-            /*source=*/std::string(),
+            /*source=*/"default",
             /*incognito=*/false);
       }
     }
@@ -499,6 +520,9 @@ ContentSetting CookieSettings::GetContentSetting(
         if (result) {
           if (info) {
             info->SetAttributes(*result);
+            if (index.source().has_value() && !index.source()->empty()) {
+              info->source = kProviderNamesSourceMap.at(index.source().value());
+            }
           }
           return content_settings::ValueToContentSetting(result->second.value);
         }
@@ -510,6 +534,9 @@ ContentSetting CookieSettings::GetContentSetting(
       if (result) {
         if (info) {
           info->SetAttributes(*result);
+          if (!result->source.empty()) {
+            info->source = kProviderNamesSourceMap.at(result->source);
+          }
         }
         return result->GetContentSetting();
       }
