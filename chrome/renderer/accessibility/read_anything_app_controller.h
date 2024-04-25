26af3a5a50b24baf9356f4daf615aa91ec939a85
Kristi Saney <kristislee@google.com>
2024-04-25 18:46:06 +0000

    [Read Aloud] Hide auto voice switching behind a feature flag
    With feature freeze coming up, this feature is not ready so we should
    hide it behind a flag.
    
    Bug: 325965520
    Change-Id: I8c188e8780f055b2556f94383defb2a902ae23e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5472188
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292594}

--- chrome/renderer/accessibility/read_anything_app_controller.h
+++ chrome/renderer/accessibility/read_anything_app_controller.h
@@ -178,6 +178,7 @@ class ReadAnythingAppController
   bool IsGoogleDocs() const;
   bool IsWebUIToolbarEnabled() const;
   bool IsReadAloudEnabled() const;
+  bool IsAutoVoiceSwitchingEnabled() const;
   void OnStandardLineSpacing();
   void OnLooseLineSpacing();
   void OnVeryLooseLineSpacing();
