6d7eecb2af11a809ce125e7d8945331bda07a68f
Fredrik Söderquist <fs@opera.com>
2024-04-25 14:17:11 +0000

    Handle missing optional step (B) argument in CSS round() function
    The following part of the spec[1] was not handled properly:
    
      If the type of A matches <number>, then B may be omitted, and defaults
      to 1; omitting B is otherwise invalid.
    
    Canonicalize the parameter list by adding B=1 when the above is
    fulfilled. Update the serialization code to prefer the shortest form by
    dropping B if it can be omitted per above.
    
    [1] https://drafts.csswg.org/css-values-4/#round-func
    
    Bug: 336681966
    Change-Id: I582c11e352b3b26f91c840fc97ac16b6787fdcc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5487527
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292428}

--- third_party/blink/renderer/core/css/css_math_operator.h
+++ third_party/blink/renderer/core/css/css_math_operator.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 class String;
+class StringView;
 }  // namespace WTF
 
 namespace blink {
@@ -40,6 +41,7 @@ enum class CSSMathOperator {
 
 CSSMathOperator ParseCSSArithmeticOperator(const CSSParserToken& token);
 WTF::String ToString(CSSMathOperator);
+WTF::StringView ToRoundingStrategyString(CSSMathOperator);
 
 bool IsComparison(CSSMathOperator);
 
