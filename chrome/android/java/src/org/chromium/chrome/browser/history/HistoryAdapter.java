aa1c54793ce8d3d094963df2b3b3967bdbd99dc7
Zach Katz <katzz@google.com>
2024-04-25 00:29:31 +0000

    [ASH] Remove extra divider line in CCT history info header
    Bug: 332570741
    Change-Id: Id506520a307f091970949bb3eb4752af01011e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484423
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Zach Katz <katzz@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292205}

--- chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/history/HistoryAdapter.java
@@ -195,6 +195,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         }
         startLoadingItems();
         updateClearBrowsingDataButtonVisibility();
+        updatePrivacyDisclaimerBottomSpace();
     }
 
     /**
@@ -365,6 +366,7 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
 
         updateClearBrowsingDataButtonVisibility();
         setPrivacyDisclaimer();
+        updatePrivacyDisclaimerBottomSpace();
     }
 
     ViewGroup getClearBrowsingDataButtonContainer(ViewGroup parent) {
@@ -453,6 +455,11 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         mPrivacyDisclaimerTextView.setText(text);
     }
 
+    private void updatePrivacyDisclaimerBottomSpace() {
+        boolean hideBottomSpace = mClearBrowsingDataButtonVisible || mManager.launchedForApp();
+        mPrivacyDisclaimerBottomSpace.setVisibility(hideBottomSpace ? View.GONE : View.VISIBLE);
+    }
+
     private CharSequence getPrivacyDisclaimerClickableSpanString(
             Context context, @StringRes int resId) {
         var s = context.getResources().getString(resId);
@@ -521,7 +528,6 @@ public class HistoryAdapter extends DateDividedAdapter implements BrowsingHistor
         boolean shouldShowButton = mManager.getShouldShowClearData();
         if (mClearBrowsingDataButtonVisible == shouldShowButton) return;
         mClearBrowsingDataButtonVisible = shouldShowButton;
-        mPrivacyDisclaimerBottomSpace.setVisibility(shouldShowButton ? View.GONE : View.VISIBLE);
 
         if (mAreHeadersInitialized) setHeaders();
     }
