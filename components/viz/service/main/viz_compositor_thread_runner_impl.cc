16aaefa341c8f0f656fa58250ce17f195224cb54
Vladimir Levin <vmpstr@chromium.org>
2024-04-25 15:14:42 +0000

    Viz: Avoid recreating lost contexts in the OnContextLost function.
    This patch does a posttask to recreate the shared context. This allows
    the rest of the MarkContextLost function to run without UAF.
    
    Here's the UAF:
    - When we're invoking OnContextLost we hold on the ref to the shared
      context (correctly) since we don't want that to be deleted while
      the callbacks are running: https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_context_state.cc;l=875;drc=59c007a7ad2f034baeffab754f3f5bf109b69cda
    
    - However, when we create a new context, for the VK path we also init
      a new gr context:
    https://source.chromium.org/chromium/chromium/src/+/main:components/viz/common/gpu/vulkan_in_process_context_provider.cc;l=181;drc=32b32b9c707d1737552b4f033b77064a551b7146
    
      This is initialized in
    https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_context_state.cc;l=496-500;drc=59c007a7ad2f034baeffab754f3f5bf109b69cda
    
    - Now, because our vk context provider hasn't changed and because shared context state is storing gr_context_ as a raw_ptr, this means the gr_context_ on the _old_ shared context that was lost is now dangling.
    
    - We then access it here as part of context lost function for a UAF:
    https://source.chromium.org/chromium/chromium/src/+/main:gpu/command_buffer/service/shared_context_state.cc;l=1011;drc=fa67bc861debe561f482e5023096ced07cf33f45
    
    R=khushalsagar@chromium.org
    
    Bug: 336655184, 336589468
    Change-Id: I4868d1ef7bea31b1d53cfa44109491d1dd684afa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486168
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292448}

--- components/viz/service/main/viz_compositor_thread_runner_impl.cc
+++ components/viz/service/main/viz_compositor_thread_runner_impl.cc
@@ -151,13 +151,11 @@ void VizCompositorThreadRunnerImpl::OnContextLost() {
   DCHECK(shared_context_state_);
   shared_context_state_->RemoveContextLostObserver(this);
 
-  CreateSharedImageInterfaceOnGpu();
-  task_runner_->PostTask(
+  base::SingleThreadTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
-      base::BindOnce(&VizCompositorThreadRunnerImpl::
-                         SetFrameSinkImplSharedImageInterfaceOnCompositor,
-                     base::Unretained(this),
-                     base::Unretained(shared_image_interface_.get())));
+      base::BindOnce(
+          &VizCompositorThreadRunnerImpl::CreateSharedImageInterfaceOnGpu,
+          weak_factory_.GetWeakPtr(), true));
 }
 
 void VizCompositorThreadRunnerImpl::
@@ -168,7 +166,8 @@ void VizCompositorThreadRunnerImpl::
   }
 }
 
-void VizCompositorThreadRunnerImpl::CreateSharedImageInterfaceOnGpu() {
+void VizCompositorThreadRunnerImpl::CreateSharedImageInterfaceOnGpu(
+    bool post_update_to_compositor) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(gpu_sequence_checker_);
 
   shared_context_state_ =
@@ -197,13 +196,22 @@ void VizCompositorThreadRunnerImpl::CreateSharedImageInterfaceOnGpu() {
 
     shared_context_state_->AddContextLostObserver(this);
   }
+
+  if (post_update_to_compositor) {
+    task_runner_->PostTask(
+        FROM_HERE,
+        base::BindOnce(&VizCompositorThreadRunnerImpl::
+                           SetFrameSinkImplSharedImageInterfaceOnCompositor,
+                       base::Unretained(this),
+                       base::Unretained(shared_image_interface_.get())));
+  }
 }
 
 void VizCompositorThreadRunnerImpl::CreateFrameSinkManager(
     mojom::FrameSinkManagerParamsPtr params,
     GpuServiceImpl* gpu_service) {
   gpu_service_ = gpu_service;
-  CreateSharedImageInterfaceOnGpu();
+  CreateSharedImageInterfaceOnGpu(false);
   // All of the unretained objects are owned on the GPU thread and destroyed
   // after VizCompositorThread has been shutdown.
   task_runner_->PostTask(
