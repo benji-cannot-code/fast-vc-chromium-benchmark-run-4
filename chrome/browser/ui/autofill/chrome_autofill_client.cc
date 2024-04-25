cc051c243f51214b9480673bbfc7326d5b79510c
Jan Keitel <jkeitel@google.com>
2024-04-25 11:29:35 +0000

    Hide suggestion controller if tab does not have focus on Show.
    Early returns in KA/popup controller code need to be accompanied by
    a Hide() to ensure that the controller is destroyed.
    
    Bug: 336740337
    Change-Id: Ib25b9456fffcdc40e939731b74368d10de88e903
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479634
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292375}

--- chrome/browser/ui/autofill/chrome_autofill_client.cc
+++ chrome/browser/ui/autofill/chrome_autofill_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/command_line.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
@@ -88,6 +89,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/payments/bubble_show_options.h"
 #include "components/autofill/core/browser/ui/payments/card_unmask_authentication_selection_dialog_controller_impl.h"
 #include "components/autofill/core/browser/ui/payments/card_unmask_otp_input_dialog_controller_impl.h"
+#include "components/autofill/core/browser/ui/popup_hiding_reasons.h"
 #include "components/autofill/core/browser/ui/popup_item_ids.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
@@ -211,7 +213,11 @@ ChromeAutofillClient::~ChromeAutofillClient() {
   // requires that the WebContents instance still be valid and it is not at
   // this point (in particular, the WebContentsImpl destructor has already
   // finished running and we are now in the base class destructor).
-  DCHECK(!suggestion_controller_);
+  if (suggestion_controller_) {
+    base::debug::DumpWithoutCrashing();
+    // Hide the controller to avoid a memory leak.
+    suggestion_controller_->Hide(PopupHidingReason::kTabGone);
+  }
 }
 
 version_info::Channel ChromeAutofillClient::GetChannel() const {
