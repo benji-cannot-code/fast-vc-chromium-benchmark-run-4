16ef85e7038ff4b5602d529bc190788ce084253c
anthie@google.com <anthie@google.com>
2024-04-25 10:17:30 +0000

    Expose chrome flags for the Skip Parent Approval for extensions feature
    In addition, extend the expiration for the existing, related,
    enable-family-link-extensions-permissions flag.
    
    Bug: b/O318069335, b/335232628
    Change-Id: I9a956404abe3c5c8725b7fdc059587b0dcb41f73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5471729
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292357}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -164,6 +164,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/soda/soda_features.h"
 #include "components/spellcheck/common/spellcheck_features.h"
 #include "components/spellcheck/spellcheck_buildflags.h"
+#include "components/supervised_user/core/common/features.h"
+#include "components/supervised_user/core/common/supervised_user_constants.h"
 #include "components/sync/base/command_line_switches.h"
 #include "components/sync/base/features.h"
 #include "components/tracing/common/tracing_switches.h"
@@ -337,7 +339,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 #include "chrome/browser/enterprise/profile_management/profile_management_features.h"
-#include "components/supervised_user/core/common/features.h"
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -7904,6 +7905,26 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          supervised_user::
              kEnableExtensionsPermissionsForSupervisedUsersOnDesktop)},
+
+    {"enable-updated-supervised-user-extension-approval-strings",
+     flag_descriptions::kUpdatedSupervisedUserExtensionApprovalStringsName,
+     flag_descriptions::
+         kUpdatedSupervisedUserExtensionApprovalStringsDescription,
+     kOsLinux | kOsMac | kOsWin,
+     FEATURE_VALUE_TYPE(
+         supervised_user::kUpdatedSupervisedUserExtensionApprovalStrings)},
+#endif
+
+#if BUILDFLAG(ENABLE_EXTENSIONS)
+    {"enable-skip-parent-approval-to-install-extensions",
+     flag_descriptions::
+         kEnableSupervisedUserSkipParentApprovalToInstallExtensionsName,
+     flag_descriptions::
+         EnableSupervisedUserSkipParentApprovalToInstallExtensionsDescription,
+     kOsLinux | kOsMac | kOsWin | kOsCrOS,
+     FEATURE_VALUE_TYPE(
+         supervised_user::
+             kEnableSupervisedUserSkipParentApprovalToInstallExtensions)},
 #endif
 
     {"notification-scheduler", flag_descriptions::kNotificationSchedulerName,
