3a8f98327e6de48cbe1ee48265c3d4b0bec890a1
Takuto Ikuta <tikuta@chromium.org>
2024-04-25 14:14:35 +0000

    base: add IWYU pragma to bind.h
    This is to prevent including bind_internal.h when we apply IWYU.
    
    ref: https://github.com/include-what-you-use/include-what-you-use/blob/master/docs/IWYUPragmas.md#iwyu-pragma-export
    
    Bug: 40248422
    Change-Id: Ic2f7fb1a580688dda4a19873db98f017e63be946
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5470131
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292427}

--- base/functional/bind.h
+++ base/functional/bind.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/compiler_specific.h"
-#include "base/functional/bind_internal.h"
+#include "base/functional/bind_internal.h"  // IWYU pragma: export
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
 
