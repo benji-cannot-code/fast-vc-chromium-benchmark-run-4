9dcc11aed29849952c8cb519bae925131a5f7d8b
Saifuddin Hitawala <hitawala@chromium.org>
2024-04-25 20:04:05 +0000

    [media] Remove legacy mailbox codepath from VideoUtil
    Last few use case of legacy mailboxes have been removed. Accordingly,
    ReadbackTexturePlaneToMemorySyncSkImage is never called and can be
    removed.
    
    Bug: 40057997
    Change-Id: I36152e57c60d7ea04039cc6ab4c5fd3bf2c1136a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473365
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292654}

--- media/base/video_util.h
+++ media/base/video_util.h
@@ -19,8 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 
-class GrDirectContext;
-
 namespace base {
 class TimeDelta;
 }
@@ -161,7 +159,6 @@ MEDIA_EXPORT scoped_refptr<VideoFrame> ConvertToMemoryMappedFrame(
 MEDIA_EXPORT scoped_refptr<VideoFrame> ReadbackTextureBackedFrameToMemorySync(
     VideoFrame& txt_frame,
     gpu::raster::RasterInterface* ri,
-    GrDirectContext* gr_context,
     const gpu::Capabilities& caps,
     VideoFramePool* pool = nullptr);
 
@@ -174,7 +171,6 @@ MEDIA_EXPORT bool ReadbackTexturePlaneToMemorySync(
     uint8_t* dest_pixels,
     size_t dest_stride,
     gpu::raster::RasterInterface* ri,
-    GrDirectContext* gr_context,
     const gpu::Capabilities& caps);
 
 // Converts a frame with I420A format into I420 by dropping alpha channel.
