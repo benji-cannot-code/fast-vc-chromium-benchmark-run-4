725c1bb22b30f8c9e65ec679cb44e32841a5657b
Takashi Toyoshima <toyoshim@chromium.org>
2024-04-25 11:50:08 +0000

    LinkPreview: Add initiator side usage metric
    This CL introduces a new metric that records feature usage
    per page visit.
    
    It is enabled only on non-Android as PreviewManager is not
    implemented on Android.
    
    This patch also improves page load metrics observer test
    infra by adding a reliable way to wait for observer's
    completion.
    
    Change-Id: I91ab91b089d099e55791667401ab55826f339f9c
    Bug: 335383638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449005
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292378}

--- chrome/browser/page_load_metrics/observers/preview_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/preview_page_load_metrics_observer.cc
@@ -6,9 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/preview_page_load_metrics_observer.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "build/build_config.h"
 #include "components/page_load_metrics/browser/page_load_metrics_observer_delegate.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "content/public/browser/navigation_handle.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/preloading/preview/preview_manager.h"
+#endif
 
 PreviewPageLoadMetricsObserver::ObservePolicy
 PreviewPageLoadMetricsObserver::OnStart(
@@ -166,6 +171,21 @@ void PreviewPageLoadMetricsObserver::RecordMetrics() {
           total_foreground_duration_);
       break;
   }
+
+#if !BUILDFLAG(IS_ANDROID)
+  // Note: PreviewManager is not implemented on Android yet.
+  // PreviewManager is created when it is used. So, `usage` should be
+  // `kNotUsed` if it doesn't exist.
+  PreviewManager::Usage usage = PreviewManager::Usage::kNotUsed;
+  PreviewManager* manager =
+      PreviewManager::FromWebContents(GetDelegate().GetWebContents());
+  if (manager) {
+    usage = manager->usage();
+  }
+  // This metric indicates if the current page initiates the LinkPreview feature
+  // and if the previewed page is promoted or not.
+  base::UmaHistogramEnumeration("PageLoad.Clients.LinkPreview.Usage", usage);
+#endif
 }
 
 void PreviewPageLoadMetricsObserver::CheckPageTransitionType(
