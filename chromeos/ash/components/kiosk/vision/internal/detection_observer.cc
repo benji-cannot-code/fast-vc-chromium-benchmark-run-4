4721e6d84df32c1b5672ac187ac811c7986bab12
Edman Anjos <edmanp@google.com>
2024-04-25 08:02:34 +0000

    kiosk: Implement vision DetectionObserver
    This introduces mojo methods in CrosCameraService used to emit relevant
    events to KioskVision.
    
    KioskVision does not yet respond to events received. A future CL will
    introduce a processor for events. Meanwhile this CL implements an
    initial FakeProcessor that we use in tests.
    
    Bug: b:334038534
    Test: testing/xvfb.py tools/autotest.py -C out_/Default chromeos/ash/components/kiosk/vision/kiosk_vision_unittest.cc
    Change-Id: Id796cd64580807029994efc7714792e14e9dc98a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5447704
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Sean Li <seannli@google.com>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292312}

---
+++ chromeos/ash/components/kiosk/vision/internal/detection_observer.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/components/kiosk/vision/internal/detection_observer.h"
+
+#include <utility>
+
+#include "chromeos/ash/components/kiosk/vision/internal/detection_processor.h"
+#include "media/capture/video/chromeos/mojom/cros_camera_service.mojom-forward.h"
+
+namespace ash::kiosk_vision {
+
+DetectionObserver::DetectionObserver(DetectionProcessors processors)
+    : processors_(std::move(processors)) {
+  // TODO(b/333698067) CHECK processors_ is not empty.
+}
+
+DetectionObserver::~DetectionObserver() = default;
+
+void DetectionObserver::OnDetection(
+    cros::mojom::KioskVisionDetectionPtr detection) {
+  for (auto& processor : processors_) {
+    processor->OnDetection(*detection);
+  }
+}
+
+void DetectionObserver::OnError(cros::mojom::KioskVisionError error) {
+  for (auto& processor : processors_) {
+    processor->OnError(error);
+  }
+}
+
+}  // namespace ash::kiosk_vision
--- media/capture/video/chromeos/camera_hal_dispatcher_impl_unittest.cc
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/chromeos/camera_hal_dispatcher_impl.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/functional/bind.h"
@@ -85,6 +86,12 @@ class MockCrosCameraService : public cros::mojom::CrosCameraService {
   }
 
  private:
+  // `cros::mojom::CrosCameraService` implementation of non relevant methods.
+  void StartKioskVisionDetection(
+      const std::string& dlc_path,
+      mojo::PendingRemote<cros::mojom::KioskVisionObserver> observer) override {
+  }
+
   mojo::Receiver<cros::mojom::CrosCameraService> receiver_{this};
 };
 
