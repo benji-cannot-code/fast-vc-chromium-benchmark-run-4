9573d995e67cdadf549ffba3cf21a2d21aa7520e
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-04-25 00:21:34 +0000

    [views-ax] Remove SetAccessibleName call from PrivacyScreenToastView
    In preparation for the migration and removal of View::SetAccessibleName,
    we're cleaning up a few files that were using it in a non-conventional
    way.
    
    PrivacyScreenToastView was using SetAccessibleName to cache the
    accessible name in the `accessible_name_` member of the View class, so
    it could later be retrieved by the GetAccessibleName method when
    requested by the WidgetDelegate of the RootView.
    
    While this was technically working, it was not the intended use of
    SetAccessibleName, and it was causing test failures in CL:5445832 as
    we are trying to migrate away from the `accessible_name_` member
    on View.
    
    This CL sets the accessible name directly on a new member of the
    PrivacyScreenToastView class and removes the SetAccessibleName call.
    
    In the near future, we will get rid of that new member and instead
    notify the RootView directly when the accessible name changes, as part
    of the ViewsAX project.
    
    Bug: 325137417
    Change-Id: Ia021a36c490802f2f43fd53be1f0a27d3b8e5250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484294
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1292199}

--- ash/system/privacy_screen/privacy_screen_toast_controller.cc
+++ ash/system/privacy_screen/privacy_screen_toast_controller.cc
@@ -103,7 +103,7 @@ void PrivacyScreenToastController::OnMouseExitedView() {
 std::u16string PrivacyScreenToastController::GetAccessibleNameForBubble() {
   if (!toast_view_)
     return std::u16string();
-  return toast_view_->GetAccessibleName();
+  return toast_view_->accessible_name();
 }
 
 void PrivacyScreenToastController::HideBubble(
