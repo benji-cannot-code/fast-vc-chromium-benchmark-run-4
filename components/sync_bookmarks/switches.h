8ef92503256186a77bfbb43b59c1150ad3152415
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-04-25 06:00:15 +0000

    Remove kSyncOmitLargeBookmarkFaviconUrl flag
    This was a kill switch for a change rolled out in M102
    (https://crrev.com/c/3535786).
    
    Fixed: 336840139
    Change-Id: I82f59e1997a974471698e5eee42a8fe46c278e06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5483869
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292284}

--- components/sync_bookmarks/switches.h
+++ components/sync_bookmarks/switches.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-BASE_DECLARE_FEATURE(kSyncOmitLargeBookmarkFaviconUrl);
-
 // TODO(crbug.com/1232951): remove the feature toggle once most of bookmarks
 // have been reuploaded.
 BASE_DECLARE_FEATURE(kSyncReuploadBookmarks);
