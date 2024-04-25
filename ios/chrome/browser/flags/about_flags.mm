f70577ba73505b0d76e99c6fac808408edce81b5
Louis Romero <lpromero@google.com>
2024-04-25 10:29:20 +0000

    Add flag for Tab Group Sync on iOS
    This CL adds an experimental flag and a helper to determine if tab
    groups should sync with other devices.
    
    Fixed: 336530286
    Change-Id: If45f37a700fb621ce4193e065e4bbb786c9c7fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476929
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292361}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1597,6 +1597,9 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kOmniboxActionsInSuggestName,
      flag_descriptions::kOmniboxActionsInSuggestDescription, flags_ui::kOsIos,
      FEATURE_VALUE_TYPE(kOmniboxActionsInSuggest)},
+    {"tab-group-sync", flag_descriptions::kTabGroupSync,
+     flag_descriptions::kTabGroupSync, flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kTabGroupSync)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
