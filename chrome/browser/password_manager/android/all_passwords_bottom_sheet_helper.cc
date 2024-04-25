4cabbf374d484dfa82c067d95ddb9f64b46f5de1
Egor Pasko <pasko@chromium.org>
2024-04-25 13:18:12 +0000

    Revert "[UPM] Always set use_login_database_as_backend to false for android"
    This reverts commit 6183fd7d4b080a7a70ecaf123408d86101ebfdec.
    
    Reason for revert: crbug.com/336996928 (chrome_public_test_apk failing on android-pie-arm64-rel)
    
    Original change's description:
    > [UPM] Always set use_login_database_as_backend to false for android
    >
    > IMPORTANT: Using the login database on Android will be deprecated in
    > about 6 months. This CL makes using the GMS Core for password storing
    > the default approach.
    >
    > chrome_public_apk can still be built with the login database by setting
    > the argument use_login_database_as_backend=true in the args.gn.
    > The default value of use_login_database_as_backend is set to false on
    > Android in this CL.
    >
    > The increase of binary size is taking place because the build flag value is changed, which causes our feature code to be compiled for Chromium builds now. Chrome builds already contain the code, so for those, this change is a no-op.
    >
    > Bug: 331746629
    > Binary-Size: Size increase is unavoidable (see above)
    > Change-Id: Ibf3fe2d6c63bae521496d722ceebe7d0b4ac41d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402473
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1292307}
    
    Bug: 331746629
    Change-Id: Ic1fd0fdc524f74bd5740a95546fe73de6a160394
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5489239
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Anna Tsvirchkova <atsvirchkova@google.com>
    Owners-Override: Egor Pasko <pasko@chromium.org>
    Auto-Submit: Egor Pasko <pasko@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1292406}

--- chrome/browser/password_manager/android/all_passwords_bottom_sheet_helper.cc
+++ chrome/browser/password_manager/android/all_passwords_bottom_sheet_helper.cc
@@ -14,10 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 AllPasswordsBottomSheetHelper::AllPasswordsBottomSheetHelper(
     password_manager::PasswordStoreInterface* profile_store,
     password_manager::PasswordStoreInterface* account_store) {
-  if (profile_store) {
-    profile_store->GetAllLoginsWithAffiliationAndBrandingInformation(
-        weak_ptr_factory_.GetWeakPtr());
-  }
+  DCHECK(profile_store);
+  profile_store->GetAllLoginsWithAffiliationAndBrandingInformation(
+      weak_ptr_factory_.GetWeakPtr());
   if (account_store) {
     account_store->GetAllLoginsWithAffiliationAndBrandingInformation(
         weak_ptr_factory_.GetWeakPtr());
