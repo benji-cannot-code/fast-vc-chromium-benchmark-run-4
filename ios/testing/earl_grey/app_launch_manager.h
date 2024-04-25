fe02b6c49b884d2273028c41645f9578526a0b9d
Benjamin Williams <bwwilliams@google.com>
2024-04-25 16:27:56 +0000

    [iOS] Add EG Test for Docking Promo.
    This change introduces an EG Test for the Docking Promo, to verify it's
    displayed after an inactivity window.
    
    This change comprises:
    
    1. Created EG test: The test simulates inactivity, launches the app, and
    verifies the Docking Promo's presence.
    
    2. Modified default trigger: The default display trigger for the Docking
    Promo has been changed from 'kAfterFre' to 'kAppLaunch' to align with
    the EG test scenario.
    
    3. Exposed -backgroundApplication: The AppLaunchManager class now
    publicly exposes the -backgroundApplication method, simplifying app
    launch simulation within tests.
    
    4. Updated IncognitoReauthTestCase: The test case has been refactored to
    use the new -backgroundApplication helper.
    
    Change-Id: Ifbec95d99993bfe0c3da1353fb8123c244361431
    Bug: 335849952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5443697
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1292499}

--- ios/testing/earl_grey/app_launch_manager.h
+++ ios/testing/earl_grey/app_launch_manager.h
@@ -67,6 +67,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // no-op.
 - (void)backgroundAndForegroundApp;
 
+// Moves app to background and returns YES if the application successfully
+// transitioned to the background; NO if the backgrounding attempt failed.
+- (BOOL)backgroundApplication [[nodiscard]];
+
 // Adds an observer for AppLaunchManager.
 - (void)addObserver:(id<AppLaunchManagerObserver>)observer;
 
