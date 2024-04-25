39bb35982b9d25738c95a470dda05988da960c9c
Henrique Nakashima <hnakashima@chromium.org>
2024-04-25 17:40:49 +0000

    [Android] ViewElements are expected to be enabled by default
    They can also be expected to be displayed but disabled.
    
    Introduce ViewElement.Options and ExistsCondition.Options to configure
    this expectation. Move custom ViewElement ids into Options to
    keep the API clean.
    
    Bug: 328275348,336846333
    Change-Id: I1082084c51038b17dc6a0d4791dce3729643fc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5485087
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292545}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ViewConditions.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ViewConditions.java
@@ -33,8 +33,8 @@ import java.util.regex.Pattern;
 public class ViewConditions {
     /** Fulfilled when a single matching View exists and is displayed. */
     public static class DisplayedCondition extends ExistsCondition {
-        public DisplayedCondition(Matcher<View> matcher) {
-            super(allOf(matcher, isDisplayed()));
+        public DisplayedCondition(Matcher<View> matcher, ExistsCondition.Options options) {
+            super(allOf(matcher, isDisplayed()), options);
         }
     }
 
@@ -47,9 +47,10 @@ public class ViewConditions {
         private final DisplayedCondition mDisplayedCondition;
         private final Condition mGate;
 
-        public GatedDisplayedCondition(Matcher<View> matcher, Condition gate) {
+        public GatedDisplayedCondition(
+                Matcher<View> matcher, Condition gate, ExistsCondition.Options options) {
             super();
-            mDisplayedCondition = new DisplayedCondition(matcher);
+            mDisplayedCondition = new DisplayedCondition(matcher, options);
             mGate = gate;
         }
 
@@ -76,11 +77,13 @@ public class ViewConditions {
     /** Fulfilled when a single matching View exists. */
     public static class ExistsCondition extends InstrumentationThreadCondition {
         private final Matcher<View> mMatcher;
+        private final Options mOptions;
         private View mViewMatched;
 
-        public ExistsCondition(Matcher<View> matcher) {
+        public ExistsCondition(Matcher<View> matcher, Options options) {
             super();
-            this.mMatcher = matcher;
+            mMatcher = matcher;
+            mOptions = options;
         }
 
         @Override
@@ -116,6 +119,15 @@ public class ViewConditions {
                                 mViewMatched = view;
                             }
                         });
+                if (mOptions.mExpectEnabled) {
+                    if (!mViewMatched.isEnabled()) {
+                        return notFulfilled("View displayed but disabled");
+                    }
+                } else { // Expected a displayed but disabled View.
+                    if (mViewMatched.isEnabled()) {
+                        return notFulfilled("View displayed but enabled");
+                    }
+                }
                 return fulfilled(message[0]);
             } catch (NoMatchingViewException
                     | NoMatchingRootException
@@ -128,6 +140,32 @@ public class ViewConditions {
                             e);
                 }
                 return notFulfilled(e.getClass().getSimpleName());
+            }
+        }
+
+        /**
+         * @return an Options builder to customize the ViewCondition.
+         */
+        public static Options.Builder newOptions() {
+            return new Options().new Builder();
+        }
+
+        /** Extra options for declaring ExistsCondition. */
+        public static class Options {
+            boolean mExpectEnabled = true;
+
+            private Options() {}
+
+            public class Builder {
+                public Options build() {
+                    return Options.this;
+                }
+
+                /** Whether the view is expected to be enabled or disabled. */
+                public Builder withExpectEnabled(boolean state) {
+                    mExpectEnabled = state;
+                    return this;
+                }
             }
         }
     }
