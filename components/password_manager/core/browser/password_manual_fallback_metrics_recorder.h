7a5d0f9d8b38c91acc698f20a5d3d7ddf1e2093d
Timofey Chudakov <tchudakov@google.com>
2024-04-26 17:06:43 +0000

    [Mullhet] Implement the metric for tracking password MF latency.
    This CL creates a new PasswordManualFallbackMetricsRecorder. This
    class will encapsulate all manual fallback related metrics recording
    logic. This CL also implements the following metric to track the
    delay between the user triggering the manual fallback from the
    Chrome context menu and seeing the suggestion.
    
    This CL corresponds to the "Metrics" section in the design doc:
    go/chrome-mullhet-suggestion-generation.
    
    Next step: record the metric in the PasswordManualFallbackFlow.
    
    MF = manual fallback.
    
    Bug: 337218328
    Change-Id: I17e7fa90d4cf44d8e1dd0899631dba52e468a79e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491247
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293107}

---
+++ components/password_manager/core/browser/password_manual_fallback_metrics_recorder.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_MANUAL_FALLBACK_METRICS_RECORDER_H_
+#define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_MANUAL_FALLBACK_METRICS_RECORDER_H_
+
+#include "base/time/time.h"
+
+namespace password_manager {
+
+// Encapsulates logic for logging manual fallback related metrics.
+class PasswordManualFallbackMetricsRecorder {
+ public:
+  PasswordManualFallbackMetricsRecorder();
+  PasswordManualFallbackMetricsRecorder(
+      const PasswordManualFallbackMetricsRecorder&);
+  PasswordManualFallbackMetricsRecorder& operator=(
+      const PasswordManualFallbackMetricsRecorder&);
+  ~PasswordManualFallbackMetricsRecorder();
+
+  // Records "PasswordManager.ManualFallback.ShowSuggestions.Latency" metric.
+  void RecordDataFetchingLatency() const;
+
+ private:
+  base::Time start_ = base::Time::Now();
+};
+
+}  // namespace password_manager
+
+#endif  // COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_PASSWORD_MANUAL_FALLBACK_METRICS_RECORDER_H_
