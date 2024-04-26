1a9869d8d1989ed19b4cf1b0cf5709a8d00e588f
rbpotter <rbpotter@chromium.org>
2024-04-26 02:49:00 +0000

    cr_components: Remove unnecessary use of Polymer elements
    Widely shared code should not use paper- or iron- elements where
    native and/or Lit-based alternatives exist.
    
    Bug: 40943652
    Change-Id: I73961d82d84b86c6a77a00147fec472efffcd88f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5485912
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292840}

--- ui/webui/resources/cr_components/managed_dialog/managed_dialog.ts
+++ ui/webui/resources/cr_components/managed_dialog/managed_dialog.ts
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import '//resources/cr_elements/cr_button/cr_button.js';
 import '//resources/cr_elements/cr_dialog/cr_dialog.js';
-import '//resources/cr_elements/icons.html.js';
-import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/cr_elements/cr_icon/cr_icon.js';
+import '//resources/cr_elements/icons_lit.html.js';
 
 import type {CrDialogElement} from '//resources/cr_elements/cr_dialog/cr_dialog.js';
 import {I18nMixinLit} from '//resources/cr_elements/i18n_mixin_lit.js';
