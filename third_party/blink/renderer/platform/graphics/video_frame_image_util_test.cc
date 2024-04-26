195610f273a6def0a65859ca1d33f102b79535ff
Colin Blundell <blundell@chromium.org>
2024-04-26 14:12:16 +0000

    [Blink] Remove legacy mailbox handling from //third_party/blink/graphics
    Bug: 40057997
    Change-Id: Ifd7e9b6685cc5556565d00839ff1ae1cd792c94f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494065
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293027}

--- third_party/blink/renderer/platform/graphics/video_frame_image_util_test.cc
+++ third_party/blink/renderer/platform/graphics/video_frame_image_util_test.cc
@@ -138,7 +138,6 @@ TEST(VideoFrameImageUtilTest, WillCreateAcceleratedImagesFromVideoFrame) {
                         media::VideoFrame::STORAGE_OPAQUE,
                         media::PIXEL_FORMAT_XRGB, base::TimeDelta());
     EXPECT_EQ(shared_image_frame->NumTextures(), 1u);
-    EXPECT_TRUE(shared_image_frame->mailbox_holder(0).mailbox.IsSharedImage());
 #if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_MAC)
     EXPECT_FALSE(
         WillCreateAcceleratedImagesFromVideoFrame(shared_image_frame.get()));
@@ -158,7 +157,6 @@ TEST(VideoFrameImageUtilTest, CreateImageFromVideoFrameZeroCopy) {
                       media::VideoFrame::STORAGE_OPAQUE,
                       media::PIXEL_FORMAT_XRGB, base::TimeDelta());
   EXPECT_EQ(shared_image_frame->NumTextures(), 1u);
-  EXPECT_TRUE(shared_image_frame->mailbox_holder(0).mailbox.IsSharedImage());
 
   auto image = CreateImageFromVideoFrame(shared_image_frame);
   ASSERT_TRUE(image->IsTextureBacked());
