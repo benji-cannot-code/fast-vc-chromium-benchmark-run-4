4cd1ab64d99b4b1600fa845015db15e45c6349da
Josh Hilke <jrhilke@google.com>
2024-04-26 05:16:17 +0000

    Add Chrome crash observer to metric reporting manager.
    Add a fatal chrome crash observer to the metric reporting manager which
    sends fatal chrome crashes to a new destination: CHROME_CRASH_EVENTS.
    
    DanglingUntriaged-notes: just updating unit test
    Change-Id: Ieee2effa58edf2e5049b535aea6c5e981cd6dcad
    Bug: b:332614580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478414
    Commit-Queue: Josh Hilke <jrhilke@google.com>
    Reviewed-by: Alberto Juarez <albertojuarez@google.com>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292866}

--- chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
+++ chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_manager.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_psr_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/cros_healthd_sampler_handlers/cros_healthd_sampler_handler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/device_activity/device_activity_sampler.h"
+#include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/chrome_fatal_crash_events_observer.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/fatal_crash/fatal_crash_events_observer.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/kiosk_heartbeat/kiosk_heartbeat_telemetry_sampler.h"
 #include "chrome/browser/ash/policy/reporting/metrics_reporting/metric_reporting_prefs.h"
@@ -99,6 +100,9 @@ BASE_FEATURE(kEnableAppEventsObserver,
              base::FEATURE_ENABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableFatalCrashEventsObserver,
              "EnableFatalCrashEventsObserver",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kEnableChromeFatalCrashEventsObserver,
+             "EnableChromeFatalCrashEventsObserver",
              base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kEnableRuntimeCountersTelemetry,
              "EnableRuntimeCountersTelemetry",
@@ -261,6 +265,10 @@ MetricReportingManager::MetricReportingManager(
   crash_event_report_queue_ = delegate_->CreateMetricReportQueue(
       EventType::kDevice, Destination::CRASH_EVENTS, Priority::FAST_BATCH,
       /*rate_limiter=*/nullptr, std::move(source_info));
+  chrome_crash_event_report_queue_ = delegate_->CreateMetricReportQueue(
+      EventType::kDevice, Destination::CHROME_CRASH_EVENTS,
+      Priority::FAST_BATCH,
+      /*rate_limiter=*/nullptr, std::move(source_info));
   DelayedInit();
 
   if (managed_session_service) {
@@ -287,6 +295,7 @@ void MetricReportingManager::Shutdown() {
   user_telemetry_report_queue_.reset();
   event_report_queue_.reset();
   crash_event_report_queue_.reset();
+  chrome_crash_event_report_queue_.reset();
   user_event_report_queue_.reset();
   app_event_report_queue_.reset();
   website_event_report_queue_.reset();
@@ -701,6 +710,14 @@ void MetricReportingManager::InitFatalCrashCollectors() {
     event_observer_managers_.emplace_back(delegate_->CreateEventObserverManager(
         FatalCrashEventsObserver::Create(), crash_event_report_queue_.get(),
         &reporting_settings_, ash::kReportDeviceCrashReportInfo,
+        metrics::kReportDeviceCrashReportInfoDefaultValue,
+        /*collector_pool=*/this));
+  }
+  if (base::FeatureList::IsEnabled(kEnableChromeFatalCrashEventsObserver)) {
+    event_observer_managers_.emplace_back(delegate_->CreateEventObserverManager(
+        FatalCrashEventsObserver::Create(),
+        chrome_crash_event_report_queue_.get(), &reporting_settings_,
+        ash::kReportDeviceCrashReportInfo,
         metrics::kReportDeviceCrashReportInfoDefaultValue,
         /*collector_pool=*/this));
   }
