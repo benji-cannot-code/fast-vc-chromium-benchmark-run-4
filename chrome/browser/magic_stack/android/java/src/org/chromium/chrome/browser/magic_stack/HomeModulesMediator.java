e850f4402f96a0c028b850ecdc1e3f6fd283605d
Xi Han <hanxi@google.com>
2024-04-26 22:14:48 +0000

    [MagicStack] Clean up magic stack's data when no module to show.
    In this CL, we fix an issue the magic stack's |mIsShown| is still be
    true if:
    1)  Expect some modules to build but no one replied before time out;
    2)  There isn't any module can be built.
    After:
    https://drive.google.com/file/d/1z4vxCJKoGXv7w2ylwS4k2vK2VvN4ZaHM/view?usp=drive_link
    
    Bug: 337321749
    Change-Id: I5f31d69b9d9e23d3243675119c101588808c7e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492162
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293288}

--- chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesMediator.java
+++ chrome/browser/magic_stack/android/java/src/org/chromium/chrome/browser/magic_stack/HomeModulesMediator.java
@@ -206,6 +206,8 @@ public class HomeModulesMediator {
             mHandler.postDelayed(this::onModuleFetchTimeOut, MODULE_FETCHING_TIMEOUT_MS);
         } else {
             mIsFetchingModules = false;
+            // If there isn't any module to build, hide the magic stack now to clean up data.
+            hide();
         }
     }
 
@@ -377,6 +379,12 @@ public class HomeModulesMediator {
                 append(cachedResponse);
             }
             mModuleResultsWaitingIndex++;
+        }
+
+        if (mModel.size() == 0) {
+            // It is possible that there isn't any module has data to show, hide the magic stack
+            // now to clean up data.
+            hide();
         }
     }
 
@@ -728,5 +736,9 @@ public class HomeModulesMediator {
 
     boolean getIsFetchingModulesForTesting() {
         return mIsFetchingModules;
+    }
+
+    boolean getIsShownForTesting() {
+        return mIsShown;
     }
 }
