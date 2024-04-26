3381a54555d8c2378bfa52f692232af9eabe790c
Paul Adedeji <pauladedeji@google.com>
2024-04-26 22:16:24 +0000

    Integrate ProductInfo with table content.
    Fetches product info for urls, then aggregates them with their specs.
    This CL only utilizes `imageUrl`s from a product's info at the moment,
    but other parts of the info will be used in the future.
    
    screenshot/7q6eXjMpMgwf4nm
    
    Change-Id: I807ed87fcd6da4413296ede40278dcfdf07e0654
    Bug: 332322830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466449
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293291}

--- chrome/test/data/webui/commerce/product_specifications/table_test.ts
+++ chrome/test/data/webui/commerce/product_specifications/table_test.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://compare/table.js';
 
 import type {TableElement} from 'chrome://compare/table.js';
+import type {CrAutoImgElement} from 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
 
@@ -21,14 +22,52 @@ suite('ProductSpecificationsTableTest', () => {
   test('column count correct', async () => {
     // Arrange / Act.
     tableElement.columns = [
-      {selectedItem: {title: 'title', url: '', imageUrl: ''}},
-      {selectedItem: {title: 'title2', url: '', imageUrl: ''}},
+      {
+        selectedItem:
+            {title: 'title', url: 'https://example.com/1', imageUrl: ''},
+      },
+      {
+        selectedItem:
+            {title: 'title2', url: 'https://example.com/2', imageUrl: ''},
+      },
     ];
     await waitAfterNextRender(tableElement);
 
     // Assert.
     const columns = tableElement.shadowRoot!.querySelectorAll('.col');
     assertEquals(2, columns.length);
+  });
+
+  test('images are displayed', async () => {
+    // Arrange / Act.
+    tableElement.columns = [
+      {
+        selectedItem: {
+          title: 'title',
+          url: 'https://example.com/1',
+          imageUrl: 'https://foo.com/image',
+        },
+      },
+      {
+        selectedItem: {
+          title: 'title2',
+          url: 'https://example.com/2',
+          imageUrl: 'https://bar.com/image',
+        },
+      },
+    ];
+    await waitAfterNextRender(tableElement);
+
+    // Assert.
+    const columnImages =
+        tableElement.shadowRoot!.querySelectorAll<CrAutoImgElement>('.col img');
+    assertEquals(2, columnImages.length);
+    assertEquals(
+        tableElement.columns[0]!.selectedItem.imageUrl,
+        columnImages[0]!.autoSrc);
+    assertEquals(
+        tableElement.columns[1]!.selectedItem.imageUrl,
+        columnImages[1]!.autoSrc);
   });
 
   test('product rows show the correct data', async () => {
