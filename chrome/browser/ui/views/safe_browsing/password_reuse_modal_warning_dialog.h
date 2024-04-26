f06cfe4c9f27761f9766a76f4481a517b9aa3f1a
weidongliu <weidongliu@chromium.org>
2024-04-26 16:43:43 +0000

    views: use CalculatePreferredSize(SizeBounds) in /chrome/browser/ui/views/safe_browsing.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    This CL was uploaded by git cl split.
    
    Bug: 40232718
    Change-Id: I7c6ba5d7a65bd11e6a2bc57155628569f56377f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490888
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293091}

--- chrome/browser/ui/views/safe_browsing/password_reuse_modal_warning_dialog.h
+++ chrome/browser/ui/views/safe_browsing/password_reuse_modal_warning_dialog.h
@@ -48,7 +48,8 @@ class PasswordReuseModalWarningDialog
       views::Label* message_body_label);
 
   // views::DialogDelegateView:
-  gfx::Size CalculatePreferredSize() const override;
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override;
   std::u16string GetWindowTitle() const override;
   bool ShouldShowCloseButton() const override;
   ui::ImageModel GetWindowIcon() override;
