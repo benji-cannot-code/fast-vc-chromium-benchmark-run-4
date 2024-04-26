63fb75f420d1dd426e47e7c974ea5301ff86aff3
Mason Freed <masonf@chromium.org>
2024-04-26 14:20:51 +0000

    Make setting defaultValue from a trusted input event validate
    This adds a large-ish test of the validation behavior of the
    textarea element, which as far as I can tell is under-specified,
    or at least I could not find this behavior in the spec. I looked
    mainly here:
    
    https://html.spec.whatwg.org/multipage/form-control-infrastructure.html#validity-states
    
    In particular, there are some interesting differences in the
    validity of initially-empty vs initially-non-empty textareas.
    
    This CL, though, also aligns the behavior of Chromium with both
    WebKit and Gecko in the specific case where the `input` event
    handler sets textarea.defaultValue. In that case, the textarea
    will be validated correctly. Previous to this CL, Chromium would
    not validate the textarea as a result of the change.
    
    Fixed: 333940413
    Change-Id: Iec38fb510296d34419b7ed3df8ab396d2d737d69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463568
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293030}

--- third_party/blink/renderer/core/html/forms/html_text_area_element.cc
+++ third_party/blink/renderer/core/html/forms/html_text_area_element.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/platform_locale.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
@@ -170,7 +171,9 @@ int HTMLTextAreaElement::scrollHeight() {
 
 void HTMLTextAreaElement::ChildrenChanged(const ChildrenChange& change) {
   HTMLElement::ChildrenChanged(change);
-  SetLastChangeWasNotUserEdit();
+  if (!RuntimeEnabledFeatures::TextAreaChildrenChangedStillValidatesEnabled()) {
+    SetLastChangeWasNotUserEdit();
+  }
   if (is_dirty_)
     SetInnerEditorValue(Value());
   else
