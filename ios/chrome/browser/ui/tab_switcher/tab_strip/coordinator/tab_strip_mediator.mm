47c1204498120d8b5509d402d94b0ed8df782ced
Quentin Pubert <qpubert@google.com>
2024-04-26 10:02:59 +0000

    [iOS][TabStrip] Ensure active WebState remains always visible
    This CL ensures the current active WebState always remains visible by
    expanding the group it was moved to (if any), not just when there is a
    new active WebState, but also if the current active WebState was moved
    or changed status.
    
    Demo:
    https://drive.google.com/file/d/1qF27PzzGksyFn6bMp4gZdzU3MtiVsewN/view
    
    Bug: 336514726
    Change-Id: I6d7850903bed4fe2e50fa3343d146b2645ee65ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491243
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1292945}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_mediator.mm
@@ -259,6 +259,9 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
     return;
   }
 
+  bool activeWebStateDidChangeStatus = false;
+  bool activeWebStateDidMove = false;
+
   switch (change.type()) {
     case WebStateListChange::Type::kStatusOnly: {
       // The activation is handled after this switch statement.
@@ -270,6 +273,9 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
               toWebStateListIndex:statusOnlyChange.index()
                          oldGroup:statusOnlyChange.old_group()
                          newGroup:statusOnlyChange.new_group()];
+        if (statusOnlyChange.index() == webStateList->active_index()) {
+          activeWebStateDidChangeStatus = true;
+        }
       }
       break;
     }
@@ -359,6 +365,9 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
             toWebStateListIndex:moveChange.moved_to_index()
                        oldGroup:moveChange.old_group()
                        newGroup:moveChange.new_group()];
+      if (moveChange.moved_to_index() == webStateList->active_index()) {
+        activeWebStateDidMove = true;
+      }
       break;
     }
     case WebStateListChange::Type::kReplace: {
@@ -474,7 +483,11 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
     }
   }
 
-  if (status.active_web_state_change()) {
+  // If there is a new active WebState, or the current active WebState moved or
+  // changed status, ensure it is still selected and visible i.e. ensure that if
+  // it is in a group, then this group is not collapsed.
+  if (status.active_web_state_change() || activeWebStateDidMove ||
+      activeWebStateDidChangeStatus) {
     const int activeIndex = webStateList->active_index();
     // If the selected index changes as a result of the last webstate being
     // detached, the active index will be -1.
@@ -485,8 +498,7 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
     TabSwitcherItem* item = [[WebStateTabSwitcherItem alloc]
         initWithWebState:status.new_active_web_state];
     [self.consumer selectItem:item];
-    // If the new active WebState is in a group, ensure that group is not
-    // collapsed.
+    // If the active WebState is in a group, ensure that group is not collapsed.
     const TabGroup* groupOfActiveWebState =
         webStateList->GetGroupOfWebStateAt(activeIndex);
     if (groupOfActiveWebState &&
