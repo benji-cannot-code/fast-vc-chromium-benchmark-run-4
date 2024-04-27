ea625943af80c5075859e5e21b0070c07524dc4c
Jonathan Lee <jonathanjlee@google.com>
2024-04-27 02:06:14 +0000

    [blinkpy] Streamline stderr args in `Executive.run_command(...)`
    Let the caller specify the stderr handle instead of deriving it from
    `return_stderr` and `ignore_stderr`, a tristate represented as a pair of
    flags where it's invalid for both to be true simultaneously.
    
    No functional change. This is preparatory refactoring for logging the
    stderr of an unsuccessful `git cl try`.
    
    Bug: 336964853
    Change-Id: Icb68ab0eae1e4a961263e296171762367bce4249
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494696
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293348}

--- third_party/blink/tools/blinkpy/common/system/executive_mock.py
+++ third_party/blink/tools/blinkpy/common/system/executive_mock.py
@@ -142,8 +142,7 @@ class MockExecutive(object):
             timeout_seconds=None,
             error_handler=None,
             return_exit_code=False,
-            return_stderr=True,
-            ignore_stderr=False,
+            stderr=STDOUT,
             decode_output=True,
             debug_logging=True):
         self._append_call(args, cwd=cwd, input=input, env=env)
@@ -181,7 +180,7 @@ class MockExecutive(object):
             error_handler(script_error)
 
         output = self._output
-        if return_stderr:
+        if stderr == self.STDOUT:
             output += self._stderr
         if decode_output:
             output = six.ensure_text(output)
