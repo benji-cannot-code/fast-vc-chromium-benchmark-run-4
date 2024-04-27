ca9b43fdec28b5a0a14c08c6a37ebcde8bafae93
Yongshun Liu <yongshun@chromium.org>
2024-04-27 00:48:49 +0000

    focus_mode: Add `YoutubeMusicController`
    This adds `YoutubeMusicController` for managing API communications for
    ash via an API delegate. The delegate will manage its API clients to
    make the requests. Both the delegate and client will be added in
    following CLs.
    
    Bug: b/331643640
    Test: N/A
    Change-Id: Ie81d01e104fbb7bd6eb66e5fc685a223bbe0c0f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448486
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293334}

---
+++ ash/system/focus_mode/youtube_music_controller.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_FOCUS_MODE_YOUTUBE_MUSIC_CONTROLLER_H_
+#define ASH_SYSTEM_FOCUS_MODE_YOUTUBE_MUSIC_CONTROLLER_H_
+
+#include "ash/ash_export.h"
+#include "ash/public/cpp/session/session_observer.h"
+
+class AccountId;
+
+namespace ash {
+
+// Provides access to the Youtube Music API for the active account through
+// `YoutubeMusicDelegate`.
+class ASH_EXPORT YoutubeMusicController : public SessionObserver {
+ public:
+  YoutubeMusicController();
+  YoutubeMusicController(const YoutubeMusicController&) = delete;
+  YoutubeMusicController& operator=(const YoutubeMusicController&) = delete;
+  ~YoutubeMusicController() override;
+
+  // SessionObserver:
+  void OnActiveUserSessionChanged(const AccountId& account_id) override;
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_FOCUS_MODE_YOUTUBE_MUSIC_CONTROLLER_H_
