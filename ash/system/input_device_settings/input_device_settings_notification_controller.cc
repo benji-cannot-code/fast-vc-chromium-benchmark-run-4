d19887d659ba1b0cbf1871889fac1409150b0373
Michael Checo <michaelcheco@google.com>
2024-04-27 01:52:46 +0000

    [WE] Add show notification methods for new device types
    Bug: b/329686601
    Change-Id: I8552edd64fc3f9d28b977ad3183367f21907a840
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479744
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293346}

--- ash/system/input_device_settings/input_device_settings_notification_controller.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/contains.h"
 #include "base/containers/fixed_flat_map.h"
+#include "base/notimplemented.h"
 #include "base/notreached.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -706,6 +707,25 @@ void InputDeviceSettingsNotificationController::
       kNotificationKeyboardIcon,
       message_center::SystemNotificationWarningLevel::NORMAL);
   message_center_->AddNotification(std::move(notification));
+}
+
+void InputDeviceSettingsNotificationController::
+    NotifyKeyboardFirstTimeConnected(const mojom::Keyboard& keyboard) {
+  // TODO(b/329686601): Implement this function.
+  NOTIMPLEMENTED();
+}
+
+void InputDeviceSettingsNotificationController::
+    NotifyTouchpadFirstTimeConnected(const mojom::Touchpad& touchpad) {
+  // TODO(b/329686601): Implement this function.
+  NOTIMPLEMENTED();
+}
+
+void InputDeviceSettingsNotificationController::
+    NotifyPointingStickFirstTimeConnected(
+        const mojom::PointingStick& pointing_stick) {
+  // TODO(b/329686601): Implement this function.
+  NOTIMPLEMENTED();
 }
 
 void InputDeviceSettingsNotificationController::NotifyMouseIsCustomizable(
