4bf0e63f96b11637cc5ce4af1484239cae7625f9
David Black <dmblack@google.com>
2024-04-27 03:14:16 +0000

    Wire up ENABLE_MERGE_REQUEST build flag to container app preinstall.
    When ENABLE_MERGE_REQUEST build flag is enabled:
    * container app preinstall feature flag is enabled by default
    * container app preinstall feature key is bypassed
    
    Bug: b:335454778
    Change-Id: I96615c8dcdb1f13d1404cc7ec9c760f8fd104124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484287
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293356}

--- chrome/browser/ui/ash/container_app/container_app_interactive_uitest.cc
+++ chrome/browser/ui/ash/container_app/container_app_interactive_uitest.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "chrome/test/interaction/interactive_browser_test.h"
+#include "chromeos/components/libsegmentation/buildflags.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "chromeos/constants/chromeos_switches.h"
 #include "components/app_constants/constants.h"
@@ -857,9 +858,12 @@ IN_PROC_BROWSER_TEST_P(ContainerAppInteractiveUiTest, UninstallFromShelf) {
 enum class IneligibilityReason {
   kMinValue = 0,
   kFeatureFlagDisabled = kMinValue,
+#if !BUILDFLAG(ENABLE_MERGE_REQUEST)
+  // NOTE: Key is bypassed when `ENABLE_MERGE_REQUEST` is enabled.
   kFeatureKeyEmpty,
   kFeatureKeyParamIncorrect,
   kFeatureKeySwitchIncorrect,
+#endif  // !BUILDFLAG(ENABLE_MERGE_REQUEST)
   kFeatureManagementFlagDisabled,
   kUserManaged,
   kUserTypeChild,
@@ -874,9 +878,12 @@ enum class IneligibilityReason {
 inline std::ostream& operator<<(std::ostream& os, IneligibilityReason reason) {
   switch (reason) {
     INELIGIBILITY_REASON_CASE(kFeatureFlagDisabled);
+#if !BUILDFLAG(ENABLE_MERGE_REQUEST)
+    // NOTE: Key is bypassed when `ENABLE_MERGE_REQUEST` is enabled.
     INELIGIBILITY_REASON_CASE(kFeatureKeyEmpty);
     INELIGIBILITY_REASON_CASE(kFeatureKeyParamIncorrect);
     INELIGIBILITY_REASON_CASE(kFeatureKeySwitchIncorrect);
+#endif  // !BUILDFLAG(ENABLE_MERGE_REQUEST)
     INELIGIBILITY_REASON_CASE(kFeatureManagementFlagDisabled);
     INELIGIBILITY_REASON_CASE(kUserManaged);
     INELIGIBILITY_REASON_CASE(kUserTypeChild);
@@ -976,13 +983,23 @@ class ContainerAppInteractiveUiIneligibilityTest
   // Returns whether the feature key param is incorrect given test
   // parameterization.
   bool IsFeatureKeyParamIncorrect() const {
+#if !BUILDFLAG(ENABLE_MERGE_REQUEST)
     return GetParam() == IneligibilityReason::kFeatureKeyParamIncorrect;
+#else   // !BUILDFLAG(ENABLE_MERGE_REQUEST)
+    // NOTE: Key is bypassed when `ENABLE_MERGE_REQUEST` is enabled.
+    return true;
+#endif  // BUILDFLAG(ENABLE_MERGE_REQUEST)
   }
 
   // Returns whether the feature key switch is incorrect given test
   // parameterization.
   bool IsFeatureKeySwitchIncorrect() const {
+#if !BUILDFLAG(ENABLE_MERGE_REQUEST)
     return GetParam() == IneligibilityReason::kFeatureKeySwitchIncorrect;
+#else   // !BUILDFLAG(ENABLE_MERGE_REQUEST)
+    // NOTE: Key is bypassed when `ENABLE_MERGE_REQUEST` is enabled.
+    return true;
+#endif  // BUILDFLAG(ENABLE_MERGE_REQUEST)
   }
 
   // Returns whether the feature management flag is disabled given test
@@ -994,11 +1011,16 @@ class ContainerAppInteractiveUiIneligibilityTest
   // Returns whether the feature key should be ignored given test
   // parameterization.
   bool ShouldIgnoreFeatureKey() const {
+#if !BUILDFLAG(ENABLE_MERGE_REQUEST)
     return !std::set<IneligibilityReason>(
                 {IneligibilityReason::kFeatureKeyEmpty,
                  IneligibilityReason::kFeatureKeyParamIncorrect,
                  IneligibilityReason::kFeatureKeySwitchIncorrect})
                 .contains(GetParam());
+#else   // !BUILDFLAG(ENABLE_MERGE_REQUEST)
+    // NOTE: Key is bypassed when `ENABLE_MERGE_REQUEST` is enabled.
+    return false;
+#endif  // BUILDFLAG(ENABLE_MERGE_REQUEST)
   }
 
   // Used to enable/disable the container app preinstallation based on test
