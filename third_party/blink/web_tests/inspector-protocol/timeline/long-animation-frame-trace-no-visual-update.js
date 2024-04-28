a79cb631676811d59b4c6fa68b98b637c76348a7
Annie Sullivan <sullivan@chromium.org>
2024-04-28 13:51:58 +0000

    LoAF: Update trace events to add more details and specific timings.
    1) Only add trace events when EITHER: The LoAF entry measures until
       next paint (has a renterStart) and can be any duration, or, the
       LoAF entry has fallen back to only measure task time (does not have
       renderStart) but is still a >50ms long task.
    2) Add nested async trace events for firstUIEventTime,
       renderStartTime, styleAndLayoutStartTime, and each script and its
       execution.
    
    Bug: 333906584
    Change-Id: I1f7c3509b949cf64adf7bd5eb782a5c0a4f42a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491920
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Annie Sullivan <sullivan@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293471}

--- third_party/blink/web_tests/inspector-protocol/timeline/long-animation-frame-trace-no-visual-update.js
+++ third_party/blink/web_tests/inspector-protocol/timeline/long-animation-frame-trace-no-visual-update.js
@@ -1,6 +1,17 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 (async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
   // Test traces
+  function checkScriptTraceEvent(events, name, id, duration) {
+    const scriptEvents = events.filter(e => e.name == name && e.id == id);
+    if (scriptEvents.length < 2) {
+      testRunner.log(`No ${name} event in trace`);
+    }
+    else if ((scriptEvents[1].ts - scriptEvents[0].ts) / 1000 <= duration) {
+      testRunner.log(`${name} trace event duration <= ${duration}`);
+    } else {
+      testRunner.log(`${name} event duration > ${duration}`);
+    }
+  }
   var {session} = await testRunner.startHTML(
       `
       <head></head>
@@ -31,18 +42,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       e => e.name == 'AnimationFrame' &&
           e.args.data?.duration > 200 &&
           e.args.data?.numScripts == 1);
-  const short_animation_frame_events = events.filter(
-      e => e.name == 'AnimationFrame' && e.args.data?.numScripts == 0);
   testRunner.log(
       loaf_tracing_event ? 'Found matching LoAF event' :
-                           'No matching LoAF event')
+                           'No matching LoAF event');
   if (loaf_tracing_event) {
     const {data} = loaf_tracing_event.args;
     testRunner.log(`duration-blockingDuration${
         data.duration - data.blockingDuration >= 50 ? '>=50' : '<50'}`);
     testRunner.log(`numScripts=${data.numScripts}`);
   }
+  checkScriptTraceEvent(
+      events, 'AnimationFrame::Script', loaf_tracing_event.id, 50);
+  const short_animation_frame_events = events.filter(
+      e => e.name == 'AnimationFrame' && e.args.data?.numScripts == 0);
   testRunner.log(`Found matching short LoaF events ${
       short_animation_frame_events.length >= 4 ? '>=4' : '<4'}`);
+  const short_frame_ids = short_animation_frame_events.map(({ id }) => id);
+  const short_animation_frame_render_events = events.filter(
+      e => e.name == 'AnimationFrame::Render' && short_frame_ids.includes(e.id));
+  testRunner.log(`Found matching short AnimationFrame::Render events ${
+      short_animation_frame_render_events.length >= 4 ? '>=4' : '<4'}`);
   testRunner.completeTest();
 })
