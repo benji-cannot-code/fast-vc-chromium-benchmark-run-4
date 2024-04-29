0d045394482f7fe6d0bcfda2b604884a352fd692
Sam Davis Omekara <samomekarajr@microsoft.com>
2024-04-29 20:19:14 +0000

    Pull Mac system highlight color for kColorCssSystemHighlight
    This CL addresses the problem where CSS Highlight color appeared as an
    unintended dark blue color on Mac. The issue was caused as a result of
    moving the retrieval of system colors in Blink to the color pipeline.
    Previously with the color pipeline, the Mac-specific system highlight
    color was not being fetched for the `kColorCssSystemHighlight` color
    id. This resulted in a fallback to the default dark blue.
    
    This CL corrects this by ensuring the `kColorCssSystemHighlight` color
    id is fetched by from Mac OS. This ensures that the CSS Highlight and
    selection color are consistent as stated in the spec.
    
    Bug: 336152476
    Change-Id: Ia3e61732b157fc60e5591e27c572e9941a803b36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491001
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1293924}

--- ui/color/css_system_color_mixer.cc
+++ ui/color/css_system_color_mixer.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-#if !BUILDFLAG(IS_WIN)
+#if !BUILDFLAG(IS_WIN) && !BUILDFLAG(IS_MAC)
 void MapNativeColorsToCssSystemColors(ColorMixer& mixer, ColorProviderKey key) {
 }
 #endif
