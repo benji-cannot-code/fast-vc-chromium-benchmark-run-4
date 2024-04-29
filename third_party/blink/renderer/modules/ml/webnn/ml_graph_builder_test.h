b765443e4f38e4f77ab557d61ba46c0e0e0fd0fd
mei1127 <shanxing.mei@intel.com>
2024-04-29 05:32:22 +0000

    webnn: Migrate Pad validation tests to WPTs
    This CL adds WPT tests for pad and removes the unit tests
    `MLGraphBuilderTest.PadTest`, `MLGraphTestMojo.PadTest` and
    `MLGraphTest.PadTest`.
    
    Bug: 327337526, 328026885
    Change-Id: Ic24215dea7c4b995719df168000e107064bef3a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476427
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Shanxing Mei <shanxing.mei@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1293535}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gru_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_lstm_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_operand_data_type.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_pad_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_reduce_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_split_options.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
@@ -61,13 +60,6 @@ MLOperand* BuildElementWiseBinary(
     webnn::mojom::blink::ElementWiseBinary::Kind kind,
     const MLOperand* a,
     const MLOperand* b);
-
-MLOperand* BuildPad(V8TestingScope& scope,
-                    MLGraphBuilder* builder,
-                    const MLOperand* input,
-                    const Vector<uint32_t>& beginningPadding,
-                    const Vector<uint32_t>& endingPadding,
-                    const MLPadOptions* options = MLPadOptions::Create());
 
 MLOperand* BuildGemm(V8TestingScope& scope,
                      MLGraphBuilder* builder,
