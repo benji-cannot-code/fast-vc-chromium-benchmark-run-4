7adea150f78e5643593ea210f1992f6453295ee9
Kramer Ge <fangzhoug@chromium.org>
2024-04-29 17:59:09 +0000

    Set kAnimationsDisabledKey according to DesktopWindowTreeHost
    On Init(), DesktopWindowTreeHostWin disables animation on TYPE_WINDOW
    that has standard frame, while DesktopWindowTreeHostPlatform disables
    animation on TYPE_WINDOW altogether.
    
    Difference of this logic explained by crrev.com/c/570670:
      "in X11, the animations are always handled by the window manager,
      whether there is a custom frame or not."
    
    Move setting kAnimationsDisabledKey during runtime to respective class
    and append the exclusion logic to prevent unintentional enablement of
    window animation.
    
    Bug: 336785232
    Change-Id: I3829813709a03fab015a34557a2c6cc97eb37295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5497616
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293811}

--- ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -1149,7 +1149,6 @@ void DesktopNativeWidgetAura::SetVisibilityChangedAnimationsEnabled(
     bool value) {
   if (desktop_window_tree_host_)
     desktop_window_tree_host_->SetVisibilityChangedAnimationsEnabled(value);
-  content_window()->SetProperty(aura::client::kAnimationsDisabledKey, !value);
 }
 
 void DesktopNativeWidgetAura::SetVisibilityAnimationDuration(
