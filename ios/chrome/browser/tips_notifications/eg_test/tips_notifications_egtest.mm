d881690ddcf1258f2f0b7ef477ec1aeb74c9737b
Scott Yoder <scottyoder@google.com>
2024-04-29 18:28:28 +0000

    [ios] Fix failing Tips Notifications EG test
    This clears the new kIosDefaultBrowserPromoLastAction pref, which
    ensures that the Default Browser item appears in the SetUpList.
    
    Change-Id: Ic9674e32bb5a03bed614110a4f2a26203e807350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5497915
    Reviewed-by: Hira Mahmood <hiramahmood@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293833}

--- ios/chrome/browser/tips_notifications/eg_test/tips_notifications_egtest.mm
+++ ios/chrome/browser/tips_notifications/eg_test/tips_notifications_egtest.mm
@@ -44,7 +44,7 @@ void MaybeTapAllowNotifications() {
   XCUIApplication* springboardApplication = [[XCUIApplication alloc]
       initWithBundleIdentifier:@"com.apple.springboard"];
   auto button = springboardApplication.buttons[@"Allow"];
-  if ([button waitForExistenceWithTimeout:1.5]) {
+  if ([button waitForExistenceWithTimeout:1]) {
     [button tap];
     [ChromeEarlGreyUI waitForAppToIdle];
   }
@@ -105,6 +105,8 @@ void MaybeDismissNotification() {
   [ChromeEarlGrey clearDefaultBrowserPromoData];
   [ChromeEarlGrey resetDataForLocalStatePref:
                       prefs::kIosCredentialProviderPromoLastActionTaken];
+  [ChromeEarlGrey
+      resetDataForLocalStatePref:prefs::kIosDefaultBrowserPromoLastAction];
   [NewTabPageAppInterface resetSetUpListPrefs];
   [ChromeEarlGrey
       resetDataForLocalStatePref:prefs::kAppLevelPushNotificationPermissions];
@@ -155,7 +157,6 @@ void MaybeDismissNotification() {
 #pragma mark - Tests
 
 // Tests the SetUpList long press menu item to toggle Tips Notifications.
-// TODO:(crbug.com/334134064) Fix and reenable test.
 - (void)testToggleTipsNotificationsMenuItem {
   [ChromeEarlGrey
       resetDataForLocalStatePref:prefs::kAppLevelPushNotificationPermissions];
