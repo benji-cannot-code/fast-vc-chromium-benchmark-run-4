96281afb5a42cc417aee1b117c4d1d75a9ed24e9
Calder Kitagawa <ckitagawa@chromium.org>
2024-04-29 17:03:59 +0000

    [Hub] Disable browser controls while visible
    On LFF there is a seemingly few pixel "overlap" between the android
    toolbar and the Hub's toolbar (at the bottom). For some reason in this
    region click events on the Hub can "pass through" to the Toolbar. (This
    is not an issue on phones). I suspect this is related to the prior
    ability for the toolbar to reparent atop the Tab Switcher (although I
    cannot find where this happens). As an interim solution hide the toolbar
    Android view while the Hub is fully visible to prevent touch inputs from
    passing through to the toolbar.
    
    Fixed: 337616153
    Change-Id: I8ddcda80145811774f7e48784960a54dc35d443b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5493335
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293763}

--- chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutUnitTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/hub/HubLayoutUnitTest.java
@@ -665,6 +665,7 @@ public class HubLayoutUnitTest {
             @HubLayoutAnimationType int expectedAnimationType) {
         assertFalse(mHubLayout.isRunningAnimations());
         assertFalse(mHubLayout.onUpdateAnimation(FAKE_TIME, false));
+        assertFalse(mHubLayout.forceHideBrowserControlsAndroidView());
 
         startShowing(fromLayout, animate);
 
@@ -684,6 +685,7 @@ public class HubLayoutUnitTest {
         assertFalse(mHubLayout.isRunningAnimations());
         assertFalse(mHubLayout.onUpdateAnimation(FAKE_TIME, false));
         verify(mHubLayout).doneShowing();
+        assertTrue(mHubLayout.forceHideBrowserControlsAndroidView());
         assertEquals(1, mActionTester.getActionCount("MobileToolbarShowStackView"));
         verify(mTab).hide(eq(TabHidingType.TAB_SWITCHER_SHOWN));
         if (fromLayout == LayoutType.START_SURFACE) {
@@ -705,6 +707,7 @@ public class HubLayoutUnitTest {
             assertFalse(mHubLayout.isRunningAnimations());
             assertFalse(mHubLayout.onUpdateAnimation(FAKE_TIME, false));
             startHiding(nextLayout, nextTabId);
+            assertFalse(mHubLayout.forceHideBrowserControlsAndroidView());
         }
 
         assertEquals(expectedAnimationType, mHubLayout.getCurrentAnimationType());
@@ -720,6 +723,7 @@ public class HubLayoutUnitTest {
         verify(mHubController, times(1)).onHubLayoutDoneHiding();
         assertEquals(0, mFrameLayout.getChildCount());
         verify(mHubLayout).doneHiding();
+        assertFalse(mHubLayout.forceHideBrowserControlsAndroidView());
         assertEquals(1, mActionTester.getActionCount("MobileExitStackView"));
 
         if (nextLayout == LayoutType.START_SURFACE) {
