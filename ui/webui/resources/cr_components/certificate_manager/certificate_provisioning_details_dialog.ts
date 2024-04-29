d18c09909dc1199863e097331eaa054b2a493a5d
rbpotter <rbpotter@chromium.org>
2024-04-29 21:40:58 +0000

    WebUI: Remove iron-collapse dependencies from cr_components
    Code served from chrome://resources is considered widely shared and
    should not use iron- or paper- dependencies if native and/or Lit-based
    solutions exist.
    
    Bug: 40943652
    Change-Id: I49a9853f52fdc021ae243e4c3e9dad34c00e5d67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494629
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293990}

--- ui/webui/resources/cr_components/certificate_manager/certificate_provisioning_details_dialog.ts
+++ ui/webui/resources/cr_components/certificate_manager/certificate_provisioning_details_dialog.ts
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 import '//resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import '//resources/cr_elements/cr_dialog/cr_dialog.js';
+import '//resources/cr_elements/cr_collapse/cr_collapse.js';
 import '//resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 
 import type {CrDialogElement} from '//resources/cr_elements/cr_dialog/cr_dialog.js';
