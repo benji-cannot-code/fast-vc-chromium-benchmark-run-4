b5ee3f1640c39775d34a5238aca3457b123c0551
weidongliu <weidongliu@chromium.org>
2024-04-29 07:10:16 +0000

    views: use CalculatePreferredSize(SizeBounds) in /chrome/browser/ui/views/desktop_capture.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    This CL was uploaded by git cl split.
    
    Bug: 40232718
    Change-Id: I18b136c2794dbae5f54bfa9bd59b31cc54122abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490887
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293547}

--- chrome/browser/ui/views/desktop_capture/desktop_media_list_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_list_view.cc
@@ -84,7 +84,8 @@ void DesktopMediaListView::OnSelectionChanged() {
   controller_->OnSourceSelectionChanged();
 }
 
-gfx::Size DesktopMediaListView::CalculatePreferredSize() const {
+gfx::Size DesktopMediaListView::CalculatePreferredSize(
+    const views::SizeBounds& /*available_size*/) const {
   const int total_rows =
       (static_cast<int>(children().size()) + active_style_->columns - 1) /
       active_style_->columns;
