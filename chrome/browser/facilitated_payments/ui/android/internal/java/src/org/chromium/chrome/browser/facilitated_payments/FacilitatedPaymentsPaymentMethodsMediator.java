6b09d7ab334094c86d2c981a5b9c311435967ce9
Longsheng Yin <longsheng@google.com>
2024-04-29 21:33:31 +0000

    [PIX] Add PIX bottom sheet description text.
    Mock: http://shortn/_74k9mlP9ja
    Implementation: https://screenshot.googleplex.com/893tBsthAkqj827
    
    Bug: b:300334696
    Change-Id: I9784b9099e59b0a1cba6c30006856083456c0a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492508
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Longsheng Yin <longsheng@google.com>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293981}

--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsMediator.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsMediator.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.facilitated_payments;
 
+import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.DESCRIPTION_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.IMAGE_DRAWABLE_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.TITLE_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.SHEET_ITEMS;
@@ -38,8 +39,9 @@ class FacilitatedPaymentsPaymentMethodsMediator {
         return new ListItem(
                 FacilitatedPaymentsPaymentMethodsProperties.ItemType.HEADER,
                 new PropertyModel.Builder(HeaderProperties.ALL_KEYS)
-                        .with(TITLE_ID, R.string.pix_payment_methods_bottom_sheet_title)
+                        .with(DESCRIPTION_ID, R.string.pix_payment_methods_bottom_sheet_description)
                         .with(IMAGE_DRAWABLE_ID, R.drawable.pix_gpay_logo)
+                        .with(TITLE_ID, R.string.pix_payment_methods_bottom_sheet_title)
                         .build());
     }
 }
--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsViewBinder.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsViewBinder.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.facilitated_payments;
 
+import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.DESCRIPTION_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.IMAGE_DRAWABLE_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.HeaderProperties.TITLE_ID;
 import static org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsProperties.SHEET_ITEMS;
@@ -73,6 +74,10 @@ class FacilitatedPaymentsPaymentMethodsViewBinder {
         if (propertyKey == TITLE_ID) {
             TextView sheetTitleText = view.findViewById(R.id.sheet_title);
             sheetTitleText.setText(view.getContext().getResources().getString(model.get(TITLE_ID)));
+        } else if (propertyKey == DESCRIPTION_ID) {
+            TextView sheetDescriptionText = view.findViewById(R.id.description_text);
+            sheetDescriptionText.setText(
+                    view.getContext().getResources().getString(model.get(DESCRIPTION_ID)));
         } else if (propertyKey == IMAGE_DRAWABLE_ID) {
             ImageView sheetHeaderImage = view.findViewById(R.id.branding_icon);
             sheetHeaderImage.setImageDrawable(
