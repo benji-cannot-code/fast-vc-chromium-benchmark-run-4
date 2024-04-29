61b17e82bb147b73fe4d3b6138b53ad57087fe3f
Olivier Li Shing Tat-Dupuis <olivierli@chromium.org>
2024-04-29 16:43:28 +0000

    Cleanup unused variable in HttpCacheTransaction
    Bug: 331123686
    Change-Id: I495615d75a668900e4c1fdcfe4b85b1e8378785b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494606
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293745}

--- net/http/http_cache_transaction.cc
+++ net/http/http_cache_transaction.cc
@@ -1198,7 +1198,6 @@ int HttpCache::Transaction::DoOpenOrCreateEntry() {
     // Record this as CantConditionalize, but otherwise proceed as we would
     // below --- as we've already dropped the old entry.
     couldnt_conditionalize_request_ = true;
-    validation_cause_ = VALIDATION_CAUSE_ZERO_FRESHNESS;
     UpdateCacheEntryStatus(CacheEntryStatus::ENTRY_CANT_CONDITIONALIZE);
   }
 
@@ -2941,7 +2940,6 @@ ValidationType HttpCache::Transaction::RequiresValidation() {
       !response_.vary_data.MatchesRequest(*request_,
                                           *response_.headers.get())) {
     vary_mismatch_ = true;
-    validation_cause_ = VALIDATION_CAUSE_VARY_MISMATCH;
     return VALIDATION_SYNCHRONOUS;
   }
 
@@ -2980,18 +2978,7 @@ ValidationType HttpCache::Transaction::RequiresValidation() {
   }
 
   if (validate_flag) {
-    validation_cause_ = VALIDATION_CAUSE_VALIDATE_FLAG;
     return VALIDATION_SYNCHRONOUS;
-  }
-
-  if (validation_required_by_headers != VALIDATION_NONE) {
-    HttpResponseHeaders::FreshnessLifetimes lifetimes =
-        response_.headers->GetFreshnessLifetimes(response_.response_time);
-    if (lifetimes.freshness == base::TimeDelta()) {
-      validation_cause_ = VALIDATION_CAUSE_ZERO_FRESHNESS;
-    } else {
-      validation_cause_ = VALIDATION_CAUSE_STALE;
-    }
   }
 
   if (validation_required_by_headers == VALIDATION_ASYNCHRONOUS) {
