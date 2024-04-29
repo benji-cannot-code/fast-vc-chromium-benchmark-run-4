ab2e523d25897b3c8e435b0470d32692c789d259
Yves Arrouye <drcrash@google.com>
2024-04-29 18:36:59 +0000

    Rename the setup method for Flex devices with FRE enabled
    The previous name was vague. "Command line switch set to always?" Sure,
    but which one? What for? All is clear(er) now.
    
    Bug: b:314539401
    Test: unit_tests --gtest_filter=*AutoEnrollmentTypeCheckerTest*
    Change-Id: Ie332160f377692767a1fc5e0f64a0272fd4e5a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5496386
    Commit-Queue: Yves Arrouye <drcrash@chromium.org>
    Auto-Submit: Yves Arrouye <drcrash@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293838}

--- chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker_unittest.cc
+++ chrome/browser/ash/policy/enrollment/auto_enrollment_type_checker_unittest.cc
@@ -64,7 +64,7 @@ class AutoEnrollmentTypeCheckerTest : public testing::Test {
         ash::system::kFirmwareTypeValueNonchrome);
   }
 
-  void SetUpFlexDeviceWithCommandLineSwitchToAlways() {
+  void SetUpFlexDeviceWithFREOnFlexEnabled() {
     flex_test_helper_.SetUpFlexDevice();
     flex_test_helper_.EnableFREOnFlex();
   }
@@ -359,7 +359,7 @@ TEST_F(AutoEnrollmentTypeCheckerTest,
 
 TEST_F(AutoEnrollmentTypeCheckerTest,
        FRERequiredOnFlexEnabledByCommandLineSwitch) {
-  SetUpFlexDeviceWithCommandLineSwitchToAlways();
+  SetUpFlexDeviceWithFREOnFlexEnabled();
 
   EXPECT_TRUE(AutoEnrollmentTypeChecker::IsFREEnabled());
   EXPECT_EQ(AutoEnrollmentTypeChecker::GetFRERequirementAccordingToVPD(
@@ -369,7 +369,7 @@ TEST_F(AutoEnrollmentTypeCheckerTest,
 
 TEST_F(AutoEnrollmentTypeCheckerTest,
        FRERequiredOnFlexOverridenByFREEnabledCommandLineSwitchSetToNever) {
-  SetUpFlexDeviceWithCommandLineSwitchToAlways();
+  SetUpFlexDeviceWithFREOnFlexEnabled();
   command_line_.GetProcessCommandLine()->AppendSwitchASCII(
       ash::switches::kEnterpriseEnableForcedReEnrollment,
       AutoEnrollmentTypeChecker::kForcedReEnrollmentNever);
@@ -689,7 +689,7 @@ class AutoEnrollmentTypeCheckerUnifiedStateDeterminationTestP
     } else if (device_os_ == DeviceOs::FlexWithoutFRE) {
       flex_test_helper_.SetUpFlexDevice();
     } else if (device_os_ == DeviceOs::FlexWithFRE) {
-      SetUpFlexDeviceWithCommandLineSwitchToAlways();
+      SetUpFlexDeviceWithFREOnFlexEnabled();
     }
     AutoEnrollmentTypeChecker::SetUnifiedStateDeterminationKillSwitchForTesting(
         kill_switch_enabled_);
