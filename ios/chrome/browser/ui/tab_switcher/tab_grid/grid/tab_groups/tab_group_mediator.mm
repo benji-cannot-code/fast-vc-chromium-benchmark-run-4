17a23a8e7d1c038c9ab476198aa49f24475a20ac
Ewann Pelle <ewannpv@chromium.org>
2024-04-29 12:04:35 +0000

    [iOS][TG] Disable Drag/Drop Group in Group
    Screenshot:
    https://drive.google.com/file/d/1mUMzCgv3DoUDZ-aGK0J7SlUe3le5g50R/
    
    Fixed: 337191714
    Change-Id: Id9e2f239613d3acf783d8996e327ac3c288144ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490427
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293617}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mediator.mm
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [_groupConsumer setGroupTitle:tabGroup->GetTitle()];
     [_groupConsumer setGroupColor:tabGroup->GetColor()];
 
+    [self switchToMode:TabGridModeGroup];
     [self populateConsumerItems];
   }
   return self;
