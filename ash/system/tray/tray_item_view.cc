bca00b709dce6cc6372f65395b442325d1e4ead9
weidongliu <weidongliu@chromium.org>
2024-04-29 17:16:44 +0000

    views: ImageViewBase use CalculatePreferredSize(SizeBounds) override.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: Id8e92804e8925f4fb9c44574c97631ed789091d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492837
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293771}

--- ash/system/tray/tray_item_view.cc
+++ ash/system/tray/tray_item_view.cc
@@ -238,7 +238,7 @@ gfx::Size TrayItemView::CalculatePreferredSize(
     size = gfx::Size(kUnifiedTrayIconSize, kUnifiedTrayIconSize);
     // Some TrayItemViews have slightly larger icons (e.g. Ethernet with VPN
     // badge).
-    size.SetToMax(image_view_->CalculatePreferredSize());
+    size.SetToMax(image_view_->CalculatePreferredSize({}));
   }
 
   if (!animation_.get() || !animation_->is_animating() ||
