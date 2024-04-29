67d9772a433a47a8142bc59a7ac64f1c967882cd
Jack Yammine <jyammine@google.com>
2024-04-29 12:35:49 +0000

    Reland "Remove kForYouFre feature flag check from profile_customization_util.cc"
    This is a reland of commit 307508b3208fd7ceb4507b5679e021b73c74afc9
    
    AppControllerOpenShortcutBrowserTest.OpenShortcutOnStartup was removed
    
    Original change's description:
    > Remove kForYouFre feature flag check from profile_customization_util.cc
    >
    > The feature is now fully launched. This is part of the cleanup process.
    >
    > Fixed: b:336721302
    > Change-Id: I2cc93653000be95b35bf9b37ee34ea9c4f68d762
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482108
    > Auto-Submit: Jack Yammine <jyammine@google.com>
    > Commit-Queue: Jack Yammine <jyammine@google.com>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1292906}
    
    Change-Id: Iae432d895d117f41c46e71cc257f0697a1778cc3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490294
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293626}

--- chrome/browser/app_controller_mac_browsertest.mm
+++ chrome/browser/app_controller_mac_browsertest.mm
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/ui_features.h"
-#include "chrome/browser/ui/webui/welcome/helpers.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_paths.h"
@@ -604,9 +603,6 @@ class AppControllerFirstRunBrowserTest : public AppControllerBrowserTest {
     InProcessBrowserTest::SetUpDefaultCommandLine(command_line);
     command_line->RemoveSwitch(switches::kNoFirstRun);
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_{kForYouFre};
 };
 
 IN_PROC_BROWSER_TEST_F(AppControllerFirstRunBrowserTest,
@@ -639,21 +635,7 @@ IN_PROC_BROWSER_TEST_F(AppControllerFirstRunBrowserTest,
 
 class AppControllerOpenShortcutBrowserTest : public InProcessBrowserTest {
  protected:
-  AppControllerOpenShortcutBrowserTest()
-      : AppControllerOpenShortcutBrowserTest(/*enable_fre=*/false) {}
-
-  AppControllerOpenShortcutBrowserTest(bool enable_fre) {
-    std::vector<base::test::FeatureRef> enabled_features = {
-        welcome::kForceEnabled};
-    std::vector<base::test::FeatureRef> disabled_features = {};
-    if (enable_fre) {
-      enabled_features.push_back(kForYouFre);
-    } else {
-      disabled_features.push_back(kForYouFre);
-    }
-
-    scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
-  }
+  AppControllerOpenShortcutBrowserTest() = default;
 
   void SetUpInProcessBrowserTestFixture() override {
     // In order to mimic opening shortcut during browser startup, we need to
@@ -692,31 +674,10 @@ class AppControllerOpenShortcutBrowserTest : public InProcessBrowserTest {
     // append about:blank as default url.
     command_line->AppendArg(chrome::kChromeUINewTabURL);
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(AppControllerOpenShortcutBrowserTest,
                        OpenShortcutOnStartup) {
-  // The two tabs expected are the Welcome page and the desired URL.
-  EXPECT_EQ(2, browser()->tab_strip_model()->count());
-  EXPECT_EQ(g_open_shortcut_url, browser()
-                                     ->tab_strip_model()
-                                     ->GetActiveWebContents()
-                                     ->GetLastCommittedURL());
-}
-
-class AppControllerOpenShortcutWithFreBrowserTest
-    : public AppControllerOpenShortcutBrowserTest {
- protected:
-  AppControllerOpenShortcutWithFreBrowserTest()
-      : AppControllerOpenShortcutBrowserTest(/*enable_fre=*/true) {}
-};
-
-IN_PROC_BROWSER_TEST_F(AppControllerOpenShortcutWithFreBrowserTest,
-                       OpenShortcutOnStartup) {
-  // The Welcome page is not expected.
   EXPECT_EQ(1, browser()->tab_strip_model()->count());
 
   EXPECT_EQ(g_open_shortcut_url,
