6cc7c11bec098e18362ce4609eb80bec7194c601
JohnLai <johnlai@google.com>
2024-04-29 16:51:57 +0000

    floss: Add LE discoverable mode
    Support a new advertising parameter: LeDiscoverableMode.
    The default mode of a started advertisement is general discoverable
    mode.
    
    BUG=b:331693399
    TEST=out/Default/device_unittests --gtest_filter="BluetoothFlossTest.*"
    
    Change-Id: Ibaf34bfe12c2e2b7b51c3cad7d59108f271beeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5465014
    Reviewed-by: Ying Hsu <yinghsu@chromium.org>
    Reviewed-by: Michael Sun <michaelfsun@google.com>
    Commit-Queue: Michael Sun <michaelfsun@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293753}

--- device/bluetooth/floss/floss_advertiser_client.cc
+++ device/bluetooth/floss/floss_advertiser_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace floss {
 namespace {
+constexpr char kDiscoverable[] = "discoverable";
 constexpr char kConnectable[] = "connectable";
 constexpr char kScannable[] = "scannable";
 constexpr char kIsLegacy[] = "is_legacy";
@@ -37,11 +38,34 @@ void FlossDBusClient::WriteDBusParam(dbus::MessageWriter* writer,
 }
 
 template <>
+void FlossDBusClient::WriteDBusParam(
+    dbus::MessageWriter* writer,
+    const AdvertisingSetParametersOld& params) {
+  dbus::MessageWriter array(nullptr);
+
+  writer->OpenArray("{sv}", &array);
+
+  WriteDictEntry(&array, kConnectable, params.connectable);
+  WriteDictEntry(&array, kScannable, params.scannable);
+  WriteDictEntry(&array, kIsLegacy, params.is_legacy);
+  WriteDictEntry(&array, kIsAnonymous, params.is_anonymous);
+  WriteDictEntry(&array, kIncludeTxPower, params.include_tx_power);
+  WriteDictEntry(&array, kPrimaryPhy, params.primary_phy);
+  WriteDictEntry(&array, kSecondaryPhy, params.secondary_phy);
+  WriteDictEntry(&array, kInterval, params.interval);
+  WriteDictEntry(&array, kTxPowerLevel, params.tx_power_level);
+  WriteDictEntry(&array, kOwnAddressType, params.own_address_type);
+  writer->CloseContainer(&array);
+}
+
+template <>
 void FlossDBusClient::WriteDBusParam(dbus::MessageWriter* writer,
                                      const AdvertisingSetParameters& params) {
   dbus::MessageWriter array(nullptr);
 
   writer->OpenArray("{sv}", &array);
+
+  WriteDictEntry(&array, kDiscoverable, params.discoverable);
   WriteDictEntry(&array, kConnectable, params.connectable);
   WriteDictEntry(&array, kScannable, params.scannable);
   WriteDictEntry(&array, kIsLegacy, params.is_legacy);
@@ -98,6 +122,12 @@ bool FlossDBusClient::ReadDBusParam(dbus::MessageReader* reader,
 template <>
 const DBusTypeInfo& GetDBusTypeInfo(const OwnAddressType*) {
   static DBusTypeInfo info{"i", "OwnAddressType"};
+  return info;
+}
+
+template <>
+const DBusTypeInfo& GetDBusTypeInfo(const AdvertisingSetParametersOld*) {
+  static DBusTypeInfo info{"a{sv}", "AdvertisingSetParametersOld"};
   return info;
 }
 
@@ -244,14 +274,32 @@ void FlossAdvertiserClient::StartAdvertisingSet(
     const int32_t max_ext_adv_events,
     StartSuccessCallback success_callback,
     ErrorCallback error_callback) {
-  CallAdvertisingMethod(
-      base::BindOnce(
-          &FlossAdvertiserClient::CompleteStartAdvertisingSetCallback,
-          weak_ptr_factory_.GetWeakPtr(), std::move(success_callback),
-          std::move(error_callback)),
-      advertiser::kStartAdvertisingSet, params, adv_data, scan_rsp,
-      periodic_params, periodic_data, duration, max_ext_adv_events,
-      callback_id_);
+  if (version_ >= base::Version("0.5")) {
+    CallAdvertisingMethod(
+        base::BindOnce(
+            &FlossAdvertiserClient::CompleteStartAdvertisingSetCallback,
+            weak_ptr_factory_.GetWeakPtr(), std::move(success_callback),
+            std::move(error_callback)),
+        advertiser::kStartAdvertisingSet, params, adv_data, scan_rsp,
+        periodic_params, periodic_data, duration, max_ext_adv_events,
+        callback_id_);
+  } else {
+    AdvertisingSetParametersOld params_old = {
+        params.connectable,      params.scannable,        params.is_legacy,
+        params.is_anonymous,     params.include_tx_power, params.primary_phy,
+        params.secondary_phy,    params.interval,         params.tx_power_level,
+        params.own_address_type,
+    };
+
+    CallAdvertisingMethod(
+        base::BindOnce(
+            &FlossAdvertiserClient::CompleteStartAdvertisingSetCallback,
+            weak_ptr_factory_.GetWeakPtr(), std::move(success_callback),
+            std::move(error_callback)),
+        advertiser::kStartAdvertisingSet, params_old, adv_data, scan_rsp,
+        periodic_params, periodic_data, duration, max_ext_adv_events,
+        callback_id_);
+  }
 }
 
 void FlossAdvertiserClient::StopAdvertisingSet(
