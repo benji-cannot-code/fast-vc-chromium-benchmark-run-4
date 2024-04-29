260977140dfdccaeadc707ac897080c7ee718d6a
Austin Eng <enga@chromium.org>
2024-04-29 20:41:38 +0000

    Reland "webgpu: use real proc table in swap buffer provider test"
    This is a reland of commit 9064b996534ccc7a1f81b2dd1abeb3e4d6758fa6
    
    UBSAN error determined false positive; suppressed in
    https://dawn-review.googlesource.com/c/dawn/+/185964
    
    Original change's description:
    > webgpu: use real proc table in swap buffer provider test
    >
    > Blink will soon use the WebGPU C++ API linked directly to the wire
    > client functions. Thus, we can no longer rely on a custom-built
    > proc table of empty functions.
    >
    > Bug: chromium:40195122
    > Change-Id: Iaa352c13cc100fbcb6deacfea50c16a8884f112a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479009
    > Commit-Queue: Austin Eng <enga@chromium.org>
    > Reviewed-by: Stephen White <senorblanco@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    > Auto-Submit: Austin Eng <enga@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1292766}
    
    Bug: chromium:40195122
    Change-Id: I8e011897321be9ccff00ce757c2498cf0637b300
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5493381
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293944}

---
+++ third_party/blink/renderer/platform/graphics/gpu/webgpu_native_test_support.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_GPU_WEBGPU_NATIVE_TEST_SUPPORT_H_
+#define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_GPU_WEBGPU_NATIVE_TEST_SUPPORT_H_
+
+#include <dawn/dawn_proc_table.h>
+#include <webgpu/webgpu.h>
+
+namespace blink {
+
+const DawnProcTable& GetDawnNativeProcs();
+WGPUInstance MakeNativeWGPUInstance();
+
+}  // namespace blink
+
+#endif  // THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_GPU_WEBGPU_NATIVE_TEST_SUPPORT_H_
