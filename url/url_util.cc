77c97a4dc47a4cd4fc18461d9d92f55b9c74d908
Jan Keitel <jkeitel@google.com>
2024-04-29 09:01:50 +0000

    Use std::string_view in ParseNonSpecialURL.
    Bug: 325408566
    Change-Id: I0d267c2bab1233844cf23ac88a369f781a94bb3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476328
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293572}

--- url/url_util.cc
+++ url/url_util.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/url_constants.h"
 #include "url/url_features.h"
 #include "url/url_file.h"
+#include "url/url_parse_internal.h"
 #include "url/url_util_internal.h"
 
 namespace url {
@@ -266,7 +267,6 @@ bool DoCanonicalize(const CHAR* spec,
   // before storing it in our object.
   bool success;
   SchemeType scheme_type = SCHEME_WITH_HOST_PORT_AND_USER_INFORMATION;
-  Parsed parsed_input;
   if (DoCompareSchemeComponent(spec, scheme, url::kFileScheme)) {
     // File URLs are special.
     success = CanonicalizeFileURL(
@@ -298,10 +298,11 @@ bool DoCanonicalize(const CHAR* spec,
   } else {
     // Non-special scheme URLs like data: and javascript:.
     if (url::IsUsingStandardCompliantNonSpecialSchemeURLParsing()) {
-      ParseNonSpecialURLInternal(spec, spec_len, trim_path_end, &parsed_input);
-      success =
-          CanonicalizeNonSpecialURL(spec, spec_len, parsed_input,
-                                    charset_converter, *output, *output_parsed);
+      success = CanonicalizeNonSpecialURL(
+          spec, spec_len,
+          ParseNonSpecialURLInternal(std::basic_string_view(spec, spec_len),
+                                     trim_path_end),
+          charset_converter, *output, *output_parsed);
     } else {
       success = CanonicalizePathURL(
           spec, spec_len,
