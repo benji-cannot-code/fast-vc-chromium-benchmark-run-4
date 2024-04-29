2915f0f35cb5381bf50d513e3b818884e2f2e488
L. David Baron <dbaron@chromium.org>
2024-04-29 20:54:34 +0000

    Remove padding in structure of blink::Node.
    This reorders member variables of blink::Node to remove padding on
    64-bit.  It shouldn't change any sizes on 32-bit.
    
    On 64-bit machines, this changes sizeof(blink::Node) from 64 to 56
    bytes, sizeof(blink::ContainerNode) from 72 to 64 bytes,
    sizeof(blink::Element) from 104 to 96 bytes, and sizeof(blink::Text)
    from 80 to 72 bytes.  However, it changes blink::Node and
    blink::ContainerNode away from ending with 4 bytes of padding that can
    be used (subject to alignment requirements) by the first member of a
    subclass.  blink::Element and blink::Text end with 4 bytes of padding
    both before and after the change.
    
    This appears to give approximately an 0.18% improvement on Speedometer3
    on M1 (statistically significant with 256 runs).
    
    Change-Id: Iea4109391c5fbbfdba393f70bdcc0a7ed4107e4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491923
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293955}

--- third_party/blink/renderer/core/dom/node.h
+++ third_party/blink/renderer/core/dom/node.h
@@ -1181,6 +1181,9 @@ class CORE_EXPORT Node : public EventTarget {
 
   ShadowRoot* GetSlotAssignmentRoot() const;
 
+  // EventTarget ends with a single 32-bit member, so put one 32-bit member
+  // first to avoid padding on 64-bit.
+  uint32_t node_flags_;
   // Both parent and tree_scope are hot accessed members. Keep them uncompressed
   // for performance reasons.
   subtle::UncompressedMember<Node> parent_or_shadow_host_node_;
@@ -1191,7 +1194,6 @@ class CORE_EXPORT Node : public EventTarget {
   Member<Node> next_;
   Member<LayoutObject> layout_object_;
   Member<NodeRareData> data_;
-  uint32_t node_flags_;
 };
 
 inline void Node::SetParentOrShadowHostNode(ContainerNode* parent) {
