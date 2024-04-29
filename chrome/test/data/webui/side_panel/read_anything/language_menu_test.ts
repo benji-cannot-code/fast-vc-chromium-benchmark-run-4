2e38e9c88442b3809041bb50b261533bb39d275a
Lauren Winston <lwinston@google.com>
2024-04-29 22:03:39 +0000

    [Read Aloud] Show "no results found" in language menu search.
    https://screenshot.googleplex.com/9g77zqPGc5NFVdc.png
    
    Fixed: 337560191
    Change-Id: I870fbb2eb8b31d926b6c149aa10cfe55af85f8f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499220
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294005}

--- chrome/test/data/webui/side_panel/read_anything/language_menu_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/language_menu_test.ts
@@ -59,6 +59,11 @@ suite('LanguageMenuElement', () => {
         '.search-field')!;
   };
 
+  const getNoResultsFoundMessage = () => {
+    return languageMenu.$.languageMenu.querySelector<HTMLElement>(
+        '#noResultsMessage');
+  };
+
   setup(() => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     languageMenu = document.createElement('language-menu');
@@ -101,6 +106,7 @@ suite('LanguageMenuElement', () => {
         assertLanguageLineWithTextAndSwitch(
             getLanguageLineItems()[1]!, 'en-UK');
         assertEquals(getLanguageSearchField().value, '');
+        assertEquals(getNoResultsFoundMessage()!.hidden, true);
       });
     });
 
@@ -128,6 +134,7 @@ suite('LanguageMenuElement', () => {
           await getLanguageSearchField().updateComplete;
           assertTrue(isPositionedOnPage(languageMenu));
           assertEquals(getLanguageLineItems().length, 0);
+          assertEquals(getNoResultsFoundMessage()!.hidden, false);
         });
 
         test('it displays matching language with a match', async () => {
@@ -136,6 +143,7 @@ suite('LanguageMenuElement', () => {
           assertEquals(getLanguageLineItems().length, 1);
           assertLanguageLineWithTextAndSwitch(
               getLanguageLineItems()[0]!, 'English (United States)');
+          assertEquals(getNoResultsFoundMessage()!.hidden, true);
         });
       });
     });
