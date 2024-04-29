3f2f7195ff862efc304a2a351c438518524912f0
Cheick Cisse <cheickcisse@google.com>
2024-04-29 18:01:01 +0000

    [PriceInsights][iOS] Add unittests to PriceInsightsCell
    This CL:
    - Checks the order of the views within the UICollectionViewCell
    - Checks that each component renders the expected data.
    
    Bug: b/335649544
    Change-Id: Ib2288148ea1697a61be601c6a45b410dde50dea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464067
    Reviewed-by: Gayane Petrosyan <gayane@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1293813}

--- ios/chrome/browser/price_insights/ui/price_insights_cell.mm
+++ ios/chrome/browser/price_insights/ui/price_insights_cell.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/sys_string_conversions.h"
 #import "components/strings/grit/components_strings.h"
+#import "ios/chrome/browser/price_insights/ui/price_insights_constants.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/price_notifications/cells/price_notifications_track_button.h"
@@ -74,6 +75,7 @@ const float kCornerRadius = 24;
     _contentStackView.alignment = UIStackViewAlignmentFill;
     _contentStackView.clipsToBounds = YES;
     _contentStackView.layer.cornerRadius = kCornerRadius;
+    [_contentStackView setAccessibilityIdentifier:kContentStackViewIdentifier];
 
     [self.contentView addSubview:_contentStackView];
     AddSameConstraintsWithInsets(
@@ -125,7 +127,8 @@ const float kCornerRadius = 24;
   }
 
   // Configure Buying options.
-  if (self.item.buyingOptionsURL.is_valid()) {
+  if ([self hasPriceHistory] && [self hasPriceRange] &&
+      self.item.buyingOptionsURL.is_valid()) {
     [self configureBuyingOptions];
     [_contentStackView addArrangedSubview:_buyingOptionsStackView];
   }
@@ -160,12 +163,15 @@ const float kCornerRadius = 24;
 // for price tracking or price range when price history is also available.
 - (void)configurePriceTrackingAndRange {
   UILabel* priceTrackingTitle = [self createLabel];
+  [priceTrackingTitle setAccessibilityIdentifier:kPriceTrackingTitleIdentifier];
   priceTrackingTitle.font =
       CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightSemibold);
   priceTrackingTitle.textColor = [UIColor colorNamed:kTextPrimaryColor];
   priceTrackingTitle.text = self.item.title;
 
   UILabel* priceTrackingSubtitle = [self createLabel];
+  [priceTrackingSubtitle
+      setAccessibilityIdentifier:kPriceTrackingSubtitleIdentifier];
   priceTrackingSubtitle.font =
       CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightRegular);
   priceTrackingSubtitle.textColor = [UIColor colorNamed:kTextSecondaryColor];
@@ -202,10 +208,13 @@ const float kCornerRadius = 24;
   verticalStack.spacing = kPriceTrackingVerticalStackViewSpacing;
 
   _priceTrackingStackView = [[UIStackView alloc] init];
+  [_priceTrackingStackView
+      setAccessibilityIdentifier:kPriceTrackingStackViewIdentifier];
   [_priceTrackingStackView addArrangedSubview:verticalStack];
 
   if (self.item.canPriceTrack) {
     _trackButton = [[PriceNotificationsTrackButton alloc] init];
+    [_trackButton setAccessibilityIdentifier:kPriceTrackingButtonIdentifier];
     [_trackButton addTarget:self
                      action:@selector(trackButtonToggled)
            forControlEvents:UIControlEventTouchUpInside];
@@ -228,12 +237,14 @@ const float kCornerRadius = 24;
 // Method that creates a view for the buying options module.
 - (void)configureBuyingOptions {
   UILabel* title = [self createLabel];
+  [title setAccessibilityIdentifier:kBuyingOptionsTitleIdentifier];
   title.font =
       CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightSemibold);
   title.text = l10n_util::GetNSString(IDS_PRICE_INSIGHTS_BUYING_OPTIONS_TITLE);
   title.textColor = [UIColor colorNamed:kTextPrimaryColor];
 
   UILabel* subtitle = [self createLabel];
+  [subtitle setAccessibilityIdentifier:kBuyingOptionsSubtitleIdentifier];
   subtitle.font =
       CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightRegular);
   subtitle.text =
@@ -253,6 +264,8 @@ const float kCornerRadius = 24;
 
   _buyingOptionsStackView = [[UIStackView alloc]
       initWithArrangedSubviews:@[ verticalStack, iconView ]];
+  [_buyingOptionsStackView
+      setAccessibilityIdentifier:kBuyingOptionsStackViewIdentifier];
   _buyingOptionsStackView.axis = UILayoutConstraintAxisHorizontal;
   _buyingOptionsStackView.spacing = kHorizontalStackViewSpacing;
   _buyingOptionsStackView.distribution = UIStackViewDistributionFill;
@@ -281,6 +294,7 @@ const float kCornerRadius = 24;
   verticalStack.alignment = UIStackViewAlignmentLeading;
 
   UILabel* title = [self createLabel];
+  [title setAccessibilityIdentifier:kPriceHistoryTitleIdentifier];
   title.font =
       CreateDynamicFont(UIFontTextStyleSubheadline, UIFontWeightSemibold);
   title.text = titleText;
@@ -289,6 +303,8 @@ const float kCornerRadius = 24;
 
   if (primarySubtitleText.length) {
     UILabel* primarySubtitle = [self createLabel];
+    [primarySubtitle
+        setAccessibilityIdentifier:kPriceHistoryPrimarySubtitleIdentifier];
     primarySubtitle.font =
         CreateDynamicFont(UIFontTextStyleFootnote, UIFontWeightRegular);
     primarySubtitle.text = primarySubtitleText;
@@ -299,6 +315,8 @@ const float kCornerRadius = 24;
     // secondarySubtitle are present.
     if (secondarySubtitleText.length) {
       UILabel* secondarySubtitle = [self createLabel];
+      [secondarySubtitle
+          setAccessibilityIdentifier:kPriceHistorySecondarySubtitleIdentifier];
       secondarySubtitle.font =
           CreateDynamicFont(UIFontTextStyleFootnote, UIFontWeightRegular);
       secondarySubtitle.text = secondarySubtitleText;
@@ -310,6 +328,8 @@ const float kCornerRadius = 24;
   _priceHistoryStackView = [[UIStackView alloc] initWithArrangedSubviews:@[
     verticalStack
   ]];
+  [_priceHistoryStackView
+      setAccessibilityIdentifier:kPriceHistoryStackViewIdentifier];
   _priceHistoryStackView.axis = UILayoutConstraintAxisVertical;
   _priceHistoryStackView.spacing = kPriceHistoryContentSpacing;
   _priceHistoryStackView.distribution = UIStackViewDistributionFill;
