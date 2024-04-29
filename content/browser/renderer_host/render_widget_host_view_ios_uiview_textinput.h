02c6478e4e7009a9be514dcb84d37b1828e505c9
Justin <justincohen@google.com>
2024-04-29 19:18:59 +0000

    ios: Refactor out objective-c classes from RenderWidgetHostViewIOS file.
    Change-Id: I628bd11dfe67ce013e04d5c470b580a9cedea30b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490385
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293882}

---
+++ content/browser/renderer_host/render_widget_host_view_ios_uiview_textinput.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_IOS_UIVIEW_TEXTINPUT_H_
+#define CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_IOS_UIVIEW_TEXTINPUT_H_
+
+#import <UIKit/UIKit.h>
+
+#include "base/memory/weak_ptr.h"
+#include "content/browser/renderer_host/render_widget_host_impl.h"
+#include "content/browser/renderer_host/render_widget_host_view_ios.h"
+#include "content/browser/renderer_host/render_widget_host_view_ios_uiview_textinput.h"
+#include "third_party/blink/public/mojom/input/input_handler.mojom-forward.h"
+#include "ui/gfx/geometry/vector2d_f.h"
+
+@interface RenderWidgetUIViewTextInput : UIView <UITextInput> {
+  base::WeakPtr<content::RenderWidgetHostViewIOS> _view;
+}
+
+- (instancetype)initWithWidget:
+    (base::WeakPtr<content::RenderWidgetHostViewIOS>)view;
+- (void)onUpdateTextInputState:(const ui::mojom::TextInputState&)state
+                    withBounds:(CGRect)bounds;
+- (void)showKeyboard:(bool)has_text withBounds:(CGRect)bounds;
+- (void)hideKeyboard;
+
+@end
+
+#endif  // CONTENT_BROWSER_RENDERER_HOST_RENDER_WIDGET_HOST_VIEW_IOS_UIVIEW_TEXTINPUT_H_
