c39571087c0b5e2c003221220fc428f5e425dba0
Bo Majewski <majewski@chromium.org>
2024-04-29 02:01:01 +0000

    [Files]: Adds a SQL implementation of a posting list.
    A posting_list_table and its indexes implement an index and inverted
    inidex (posting list). The table just holds associations between pairs
    of (augmented) term IDs and URL ID. The indexes allow us to quickly
    fetch all URL IDs for the given term or all term IDs for the given URL.
    Adds basic tests.
    
    Bug: b:327534824
    Change-Id: I6980c59280402f3f23027a5fad5061b66f4be0fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5480769
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293514}

--- chrome/browser/ash/file_manager/indexing/file_info_table.h
+++ chrome/browser/ash/file_manager/indexing/file_info_table.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ash/file_manager/indexing/file_info.h"
-#include "chrome/browser/ash/file_manager/indexing/url_table.h"
 #include "sql/database.h"
 
 namespace file_manager {
