8b66c16f073011987cda3c8d5d16d46751d24abd
weidongliu <weidongliu@chromium.org>
2024-04-29 09:02:10 +0000

    views: use CalculatePreferredSize(SizeBounds) in //components.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: Ia25de2abf0d02a38c66bb6ce5ab164153214a44e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482766
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293573}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -1556,7 +1556,8 @@ void ShellSurfaceBase::OnWidgetClosing(views::Widget* widget) {
 ////////////////////////////////////////////////////////////////////////////////
 // views::Views overrides:
 
-gfx::Size ShellSurfaceBase::CalculatePreferredSize() const {
+gfx::Size ShellSurfaceBase::CalculatePreferredSize(
+    const views::SizeBounds& available_size) const {
   if (!geometry_.IsEmpty())
     return geometry_.size();
 
@@ -2435,7 +2436,7 @@ void ShellSurfaceBase::CommitWidget() {
     // `views::Widget::InitParams.bounds`
     if (window_state && window_state->IsMaximizedOrFullscreenOrPinned() &&
         (!initial_bounds_ || initial_bounds_->IsEmpty())) {
-      gfx::Size current_content_size = CalculatePreferredSize();
+      gfx::Size current_content_size = CalculatePreferredSize({});
       gfx::Rect restore_bounds = display::Screen::GetScreen()
                                      ->GetDisplayNearestWindow(window)
                                      .work_area();
