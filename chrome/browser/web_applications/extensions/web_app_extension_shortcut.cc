6d5110e3749c4628883014387d52dececf3e9f3e
Solomon Kinard <solomonkinard@chromium.org>
2024-04-29 14:28:19 +0000

    Extensions: CodeHealth: Give enums some class
    Enum should be declared with class:
    https://g3doc.corp.google.com/devtools/library_club/g3doc/totw/86.md
    
    Enums should be named like constants with a leading lowercase character:
    https://google.github.io/styleguide/cppguide.html#Enumerator_Names
    
    Tricium recommends emplace_back:
    crrev.com/c/5492183/2/extensions/browser/image_loader.cc#257
    
    Bug: None
    Change-Id: I6de7958a7d251e3d744e05bb910f62c5e89d2805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492183
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293670}

--- chrome/browser/web_applications/extensions/web_app_extension_shortcut.cc
+++ chrome/browser/web_applications/extensions/web_app_extension_shortcut.cc
@@ -168,8 +168,8 @@ void GetShortcutInfoForApp(const extensions::Extension* extension,
 
   for (int size : GetDesiredIconSizesForShortcut()) {
     extensions::ExtensionResource resource =
-        extensions::IconsInfo::GetIconResource(extension, size,
-                                               ExtensionIconSet::MATCH_EXACTLY);
+        extensions::IconsInfo::GetIconResource(
+            extension, size, ExtensionIconSet::Match::kExactly);
     if (!resource.empty()) {
       info_list.emplace_back(
           resource, extensions::ImageLoader::ImageRepresentation::ALWAYS_RESIZE,
@@ -186,11 +186,11 @@ void GetShortcutInfoForApp(const extensions::Extension* extension,
     // so look for a larger icon first.
     // TODO(crbug.com/329953472): Use a predefined threshold.
     extensions::ExtensionResource resource =
-        extensions::IconsInfo::GetIconResource(extension, size,
-                                               ExtensionIconSet::MATCH_BIGGER);
+        extensions::IconsInfo::GetIconResource(
+            extension, size, ExtensionIconSet::Match::kBigger);
     if (resource.empty()) {
       resource = extensions::IconsInfo::GetIconResource(
-          extension, size, ExtensionIconSet::MATCH_SMALLER);
+          extension, size, ExtensionIconSet::Match::kSmaller);
     }
     info_list.emplace_back(
         resource, extensions::ImageLoader::ImageRepresentation::ALWAYS_RESIZE,
