a01e3946abfc1fe7bee6f816a2236a1e5e5862c9
Vladimir Levin <vmpstr@chromium.org>
2024-04-29 15:18:15 +0000

    Reland: VT: Use BlitRequests in the gpu path for capturing textures
    Original description
    --------------------
    For now, we still wait for the CopyOutput ack before using the textures
    but at least it's exercising the right path. Since software BlitRequests
    path is not yet available, we would need to maintain two paths.
    
    This is sufficient evidence that we can use it in ipif paint holding
    though
    
    Added fix for the reland
    ------------------------
    Propagate is_software bit from the compositor to indicate that surface
    saved frame should be using software resources too.
    
    R=khushalsagar@chromium.org, vasilyt@chromium.org
    
    Change-Id: I3d406a2bf549e6333d86e68c4f0af1bac3997cf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492141
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293696}

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2403,6 +2403,8 @@ viz::CompositorFrameMetadata LayerTreeHostImpl::MakeCompositorFrameMetadata() {
     screenshot_destination_ = base::UnguessableToken();
   }
 
+  metadata.is_software = !layer_tree_frame_sink_->context_provider();
+
   return metadata;
 }
 
