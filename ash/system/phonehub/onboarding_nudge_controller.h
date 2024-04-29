6c33b4fce49b3c25661dd6ac9ccc007a32d39729
Kevin Radtke <kradtke@chromium.org>
2024-04-29 23:02:43 +0000

    nudge: Speculative fix for anchored nudge dtor crash
    Use a weak ptr instead of `base::Unretained` for the `kPhoneHub` nudge
    to match how other nudges send their callbacks and prevent possible
    nullptr dereference caused by the nudge controller not existing when the
    custom nudge dismiss callback is ran.
    
    Bug: b:336563579
    Change-Id: I6865381e6f839d555de2d797e5099c10643b8db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498861
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294037}

--- ash/system/phonehub/onboarding_nudge_controller.h
+++ ash/system/phonehub/onboarding_nudge_controller.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chromeos/ash/components/phonehub/feature_status_provider.h"
 
 class PrefRegistrySimple;
@@ -105,6 +106,8 @@ class ASH_EXPORT OnboardingNudgeController
   base::RepeatingClosure stop_animation_callback_;
   base::RepeatingClosure start_animation_callback_;
   raw_ptr<base::Clock> clock_;
+
+  base::WeakPtrFactory<OnboardingNudgeController> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
