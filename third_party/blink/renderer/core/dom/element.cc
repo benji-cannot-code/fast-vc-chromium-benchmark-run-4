9cdebedb3fd6ef5d2476b604eed175e950d3ab77
Noam Rosenthal <nrosenthal@chromium.org>
2024-04-29 19:32:38 +0000

    Keep element as fullscreen during atomic move
    - We keep the kContainsFullScreenElement flag on the element, and
      don't remove the element from top-layer
    - We still clear the kContainsFullScreenElement flag from the old
      node's ancestor chain
    - When re-inserting, we set the kContainsFullScreenElement on the
      new ancestor chain
    
    Bug: 40150299
    Change-Id: I0ffd6cb6ed12009646169419e96e06e69c9ab389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466094
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293887}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -3038,6 +3038,13 @@ Node::InsertionNotificationRequest Element::InsertedInto(
     SetIsInCanvasSubtree(true);
   }
 
+  if (GetDocument().StatePreservingAtomicMoveInProgress() &&
+      Fullscreen::IsFullscreenElement(*this)) {
+    // We don't actually need to cross frame boundaries, but we do need to mark
+    // all our ancestors as containing a full screen element.
+    SetContainsFullScreenElementOnAncestorsCrossingFrameBoundaries(true);
+  }
+
   return kInsertionDone;
 }
 
@@ -3100,7 +3107,7 @@ void Element::RemovedFrom(ContainerNode& insertion_point) {
 
   document.GetRootScrollerController().ElementRemoved(*this);
 
-  if (IsInTopLayer()) {
+  if (IsInTopLayer() && !document.StatePreservingAtomicMoveInProgress()) {
     Fullscreen::ElementRemoved(*this);
     document.RemoveFromTopLayerImmediately(this);
   }
