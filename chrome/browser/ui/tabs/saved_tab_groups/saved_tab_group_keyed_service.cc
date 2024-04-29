7e589db79a6e3315acd340266d601330a6b2b625
David Pennington <dpenning@chromium.org>
2024-04-29 18:13:04 +0000

    [SavedTabGroupV2] Add a footer to the TabGroupEditorBubbleView
    Adds a footer with a learn more link
    
    https://drive.google.com/file/d/1IugX4gaVdbybYJxAoo_uRh4qu7gWuhuC/view?usp=drive_link&resourcekey=0-CIjcf5U5VP5Yo9XoENwzXA
    
    Change-Id: Ic8cf4c332b7a31e49ac7e088f20b36baa51a144f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449449
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293824}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/url_and_id.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/model_type_store_service_factory.h"
+#include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils_desktop.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -38,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/report_unrecoverable_error.h"
 #include "components/sync/model/client_tag_based_model_type_processor.h"
 #include "components/sync/model/model_type_store_service.h"
+#include "components/sync/service/sync_service.h"
+#include "components/sync/service/sync_user_settings.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
 #include "content/public/browser/web_contents.h"
@@ -71,6 +74,19 @@ SavedTabGroupKeyedService::SavedTabGroupKeyedService(Profile* profile)
 
 SavedTabGroupKeyedService::~SavedTabGroupKeyedService() {
   model_.RemoveObserver(this);
+}
+
+// Whether the sync setting is on for saved tab groups.
+bool SavedTabGroupKeyedService::AreSavedTabGroupsSynced() {
+  const syncer::SyncService* const sync_service =
+      SyncServiceFactory::GetForProfile(profile_);
+
+  if (!sync_service->IsSyncFeatureEnabled()) {
+    return false;
+  }
+
+  return sync_service->GetUserSettings()->GetSelectedTypes().Has(
+      syncer::UserSelectableType::kSavedTabGroups);
 }
 
 syncer::OnceModelTypeStoreFactory SavedTabGroupKeyedService::GetStoreFactory() {
--- chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
+++ chrome/browser/ui/views/tabs/tab_group_editor_bubble_view.cc
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.h"
@@ -79,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/separator.h"
+#include "ui/views/controls/styled_label.h"
 #include "ui/views/interaction/element_tracker_views.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/flex_layout.h"
@@ -91,6 +94,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 constexpr base::TimeDelta kTemporaryBookmarkBarDuration = base::Seconds(15);
+constexpr int kDialogWidth = 240;
+constexpr const char kLearnMoreURL[] =
+    "https://support.google.com/chrome/answer/165139";
 
 std::unique_ptr<views::LabelButton> CreateMenuItem(
     int button_id,
@@ -370,6 +376,7 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
         ui::ImageModel::FromVectorIcon(features::IsChromeRefresh2023()
                                            ? kTrashCanRefreshIcon
                                            : kTrashCanIcon))));
+    footer_ = AddChildView(std::make_unique<Footer>(browser_));
   }
 
   // The move menu item must be added to the menu by this point.
@@ -384,9 +391,14 @@ TabGroupEditorBubbleView::TabGroupEditorBubbleView(
   const int vertical_spacing = control_insets.top();
   const int horizontal_spacing = control_insets.left();
 
+  gfx::Insets interior_margins = gfx::Insets::VH(vertical_spacing, 0);
+  if (footer_) {
+    interior_margins.set_bottom(0);
+  }
+
   SetLayoutManager(std::make_unique<views::FlexLayout>())
       ->SetOrientation(views::LayoutOrientation::kVertical)
-      .SetInteriorMargin(gfx::Insets::VH(vertical_spacing, 0));
+      .SetInteriorMargin(interior_margins);
 
   title_field_->SetProperty(
       views::kMarginsKey,
@@ -694,4 +706,73 @@ void TabGroupEditorBubbleView::TitleField::ShowContextMenu(
 }
 
 BEGIN_METADATA(TabGroupEditorBubbleView, TitleField)
+END_METADATA
+
+TabGroupEditorBubbleView::Footer::Footer(const Browser* browser) {
+  views::FlexLayout* flex_layout =
+      views::View::SetLayoutManager(std::make_unique<views::FlexLayout>());
+  flex_layout->SetOrientation(views::LayoutOrientation::kVertical)
+      .SetMainAxisAlignment(views::LayoutAlignment::kStart)
+      .SetCollapseMargins(true);
+
+  SetBackground(
+      views::CreateThemedSolidBackground(ui::kColorBubbleFooterBackground));
+
+  tab_groups::SavedTabGroupKeyedService* const saved_tab_group_service =
+      tab_groups::SavedTabGroupServiceFactory::GetForProfile(
+          browser->profile());
+  CHECK(saved_tab_group_service);
+
+  // Get the keyed service and check if saved.
+  views::StyledLabel* footer_label =
+      AddChildView(std::make_unique<views::StyledLabel>());
+  footer_label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+
+  std::vector<std::u16string> footer_text_substr;
+
+  // Strings for the footer are different if the user has sync enabled.
+  footer_text_substr.push_back(l10n_util::GetStringUTF16(
+      saved_tab_group_service->AreSavedTabGroupsSynced()
+          ? IDS_TAB_GROUP_EDITOR_BUBBLE_FOOTER_SYNC_ENABLED
+          : IDS_TAB_GROUP_EDITOR_BUBBLE_FOOTER_SYNC_DISABLED));
+
+  // Learn more link for the footer.
+  footer_text_substr.push_back(l10n_util::GetStringUTF16(IDS_LEARN_MORE));
+
+  std::vector<size_t> offsets;
+  std::u16string styled_text =
+      base::ReplaceStringPlaceholders(u"$1 $2", footer_text_substr, &offsets);
+  footer_label->SetText(styled_text);
+
+  gfx::Range details_range(offsets[1], styled_text.length());
+
+  views::StyledLabel::RangeStyleInfo link_style =
+      views::StyledLabel::RangeStyleInfo::CreateForLink(base::BindRepeating(
+          &TabGroupEditorBubbleView::Footer::OpenLearnMorePage, browser));
+
+  footer_label->AddStyleRange(details_range, link_style);
+
+  ChromeLayoutProvider* layout_provider = ChromeLayoutProvider::Get();
+  const int horizontal_spacing = layout_provider->GetDistanceMetric(
+      views::DISTANCE_RELATED_CONTROL_HORIZONTAL);
+  const int vertical_spacing = layout_provider->GetDistanceMetric(
+      views::DISTANCE_RELATED_CONTROL_VERTICAL);
+  const gfx::Insets control_insets =
+      ui::TouchUiController::Get()->touch_ui()
+          ? gfx::Insets::VH(5 * vertical_spacing / 4, horizontal_spacing)
+          : gfx::Insets::VH(vertical_spacing, horizontal_spacing);
+  footer_label->SizeToFit(kDialogWidth - control_insets.right() -
+                          control_insets.left());
+  SetSize({kDialogWidth, height()});
+  SetBorder(views::CreateEmptyBorder(control_insets));
+}
+
+// static
+void TabGroupEditorBubbleView::Footer::OpenLearnMorePage(
+    const Browser* browser) {
+  browser->tab_strip_model()->delegate()->AddTabAt(GURL(kLearnMoreURL), -1,
+                                                   true);
+}
+
+BEGIN_METADATA(TabGroupEditorBubbleView, Footer)
 END_METADATA
