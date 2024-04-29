273e9c0991c675c571d0eb5ee8bdeeab2790010d
Leszek Swirski <leszeks@google.com>
2024-04-29 08:25:41 +0000

    [android] Re-adds ability to include both v8 snapshot types
    A reland of https://crrev.com/c/3209682, which allows selecting at
    runtime whether V8 uses the blink-generated context snapshot or not.
    This reland is both a rebase and an update, and as such has some
    mechanical differences with the original patch -- in particular, it
    switches to using BUILDFLAGS over `#if defined`.
    
    We're relanding this because we want to retry the experiment with
    various new differences (e.g. trying out enabling for just high-end
    Android, collecting metrics from partners rather than aggregated whole
    web performance, looking at metrics outside of CWV, experimenting with
    the impact on the trade-off space of alternative compression schemes
    for the snapshot).
    
    Original description:
    
    > And adds feature as to which one is used at runtime. Specific
    > parts:
    > . adds gn arg: include_both_v8_snapshots. At this time this is
    >   only supported on android. I will likely make it work on chromeos
    >   next.
    > . Adds feature kUseContextSnapshot. This feature is available if
    >   include_both_v8_snapshots is set.
    > . Adds switch kUseContextSnapshotSwitch. This is passed from browser
    >   to renderer if kUseContextSnapshot is enabled. A separate switch is
    >   used as at the time the v8 snapshot is loaded features have not been
    >   loaded.
    > And this updates a bunch of build targets accordingly.
    
    Bug: 40200623, 40539769
    Change-Id: I4e82581809321ec143b3c1484af4267279aa1e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5482109
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Auto-Submit: Leszek Swirski <leszeks@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293558}

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "content/common/content_export.h"
 #include "media/media_buildflags.h"
+#include "tools/v8_context_snapshot/buildflags.h"
 
 namespace switches {
 
@@ -217,6 +218,9 @@ CONTENT_EXPORT extern const char kUseFakeUIForDigitalIdentity[];
 CONTENT_EXPORT extern const char kUseFakeUIForFedCM[];
 CONTENT_EXPORT extern const char kUseFakeUIForMediaStream[];
 CONTENT_EXPORT extern const char kVideoImageTextureTarget[];
+#if BUILDFLAG(IS_ANDROID) && BUILDFLAG(INCLUDE_BOTH_V8_SNAPSHOTS)
+CONTENT_EXPORT extern const char kUseContextSnapshotSwitch[];
+#endif
 CONTENT_EXPORT extern const char kUseMobileUserAgent[];
 CONTENT_EXPORT extern const char kUseMockCertVerifierForTesting[];
 extern const char kUtilityCmdPrefix[];
