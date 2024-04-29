4767605457b124af019edf4f54ff778cd73b0c32
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-04-29 18:49:34 +0000

    [views-ax] Avoid calling GetAccessibleName in GetAccessibleNodeData
    With CL:5445832, GetAccessibleName will now directly return the
    accessible name in the AXNodeData cache instead of returning the name
    in the cached accessible_name_ attribute on the View.
    In the near future, GetAccessibleNodeData will be used to initialize
    that cache. This would lead us to a circular dependency.
    
    To fix this, we now retrieve the accessible name in GetAccessibleNodeData
    in a different manner (either from a local cache or recompute it).
    
    Bug: 325137417
    Change-Id: Ic5e269c7be7efe5a3b93936bb783278c7eed295d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486710
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1293846}

--- components/media_message_center/media_notification_view_impl.cc
+++ components/media_message_center/media_notification_view_impl.cc
@@ -341,8 +341,8 @@ void MediaNotificationViewImpl::GetAccessibleNodeData(
       l10n_util::GetStringUTF8(
           IDS_MEDIA_MESSAGE_CENTER_MEDIA_NOTIFICATION_ACCESSIBLE_NAME));
 
-  if (!GetAccessibleName().empty()) {
-    node_data->SetNameChecked(GetAccessibleName());
+  if (!accessible_name_.empty()) {
+    node_data->SetNameChecked(accessible_name_);
   }
 }
 
@@ -392,7 +392,8 @@ void MediaNotificationViewImpl::UpdateWithMediaMetadata(
   title_label_->SetText(metadata.title);
   artist_label_->SetText(metadata.artist);
 
-  SetAccessibleName(GetAccessibleNameFromMetadata(metadata));
+  accessible_name_ = GetAccessibleNameFromMetadata(metadata);
+  SetAccessibleName(accessible_name_);
 
   // The title label should only be a11y-focusable when there is text to be
   // read.
