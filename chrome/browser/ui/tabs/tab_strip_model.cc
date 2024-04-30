940a67aee4340c9830419f678e07f5fb7cf09e54
Andreas Nazlidis <andreas221b@gmail.com>
2024-04-30 17:00:43 +0000

    [ContextMenu] Enable Mute Site for audible
    At present the Mute Site item is shown in context menu for
    all sites like new tab, even though there is no necessity.
    This CL allows the Mute Site item to be enabled only for
    for tabs that contain audible web pages.
    
    TEST=Ran all unit tests
    
    Bug: 40836356
    
    Fixed: 40836356
    Change-Id: I0d5ce5f5c7972718d5ca8f9de6d4a94995f47f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5468683
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294423}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -76,6 +76,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/tab_groups/tab_group_visual_data.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "components/webapps/common/web_app_id.h"
+#include "content/public/browser/media_session.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_observer.h"
@@ -1423,8 +1424,11 @@ bool TabStripModel::IsContextMenuCommandEnabled(
     case CommandToggleSiteMuted: {
       std::vector<int> indices = GetIndicesForCommand(context_index);
       for (int index : indices) {
-        if (!GetWebContentsAt(index)->GetLastCommittedURL().is_empty())
+        content::WebContents* web_contents = GetWebContentsAt(index);
+        if (!web_contents->GetLastCommittedURL().is_empty() &&
+            content::MediaSession::GetIfExists(web_contents)) {
           return true;
+        }
       }
       return false;
     }
