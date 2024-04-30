74eacf5b6c7e3f2e2792b644217e9b7102e62771
Fiona Macintosh <fmacintosh@google.com>
2024-04-30 20:15:30 +0000

    Logic to support thirdPartyCookiesAllowed=false extension API post-3PCD
    Fixed: b:329821825
    Change-Id: Ic94f07b8c1f065bae89e0993a4dbe62989c87815
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498762
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294547}

--- chrome/browser/extensions/api/preference/preference_api.cc
+++ chrome/browser/extensions/api/preference/preference_api.cc
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/pref_transformer_interface.h"
 #include "chrome/common/pref_names.h"
 #include "components/autofill/core/common/autofill_prefs.h"
+#include "components/content_settings/core/common/pref_names.h"
 #include "components/prefs/pref_service.h"
+#include "components/privacy_sandbox/tracking_protection_prefs.h"
 #include "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #include "extensions/browser/api/content_settings/content_settings_service.h"
 #include "extensions/browser/extension_pref_value_map.h"
@@ -706,6 +708,14 @@ ExtensionFunction::ResponseAction SetPreferenceFunction::Run() {
     prefs_helper->SetExtensionControlledPref(extension_id(),
                                              prefs::kSafeBrowsingEnhanced,
                                              scope, base::Value(false));
+  }
+
+  // TODO(https://b/333527273): Remove this logic &
+  // CookieControlsModeTransformer and replace with a transformer for this pref.
+  if (prefs::kCookieControlsMode == browser_pref && !value->GetBool()) {
+    prefs_helper->SetExtensionControlledPref(extension_id(),
+                                             prefs::kBlockAll3pcToggleEnabled,
+                                             scope, base::Value(true));
   }
 
   prefs_helper->SetExtensionControlledPref(extension_id(), browser_pref, scope,
