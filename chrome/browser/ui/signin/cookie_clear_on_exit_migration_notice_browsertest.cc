7670f95908a693de6c2f6b53f4e169929f7d3a29
David Roger <droger@chromium.org>
2024-04-30 16:28:00 +0000

    [Uno-D] Dialog to notify users of the "Clear on Exit" change
    When the user is signed in to desktop Chrome through the new UNO flow,
    their Google account cookies will be rebuilt when cleared. This impacts
    the behavior of the "Clear on Exit" setting, and existing users of this
    setting will be noified of the change through a new specific dialog.
    
    This CL adds the UI for the dialog. A future CL will trigger the dialog
    when exiting. It will be shown once per profile.
    
    Screenshot: https://storage.cloud.google.com/chromium-translation-screenshots/1502a95b9f2847fa32514ac60054e42807e89866
    
    Bug: b/331749448
    Change-Id: I4903556ae4f04ef591208d6204f88fe44734a309
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5489000
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294396}

---
+++ chrome/browser/ui/signin/cookie_clear_on_exit_migration_notice_browsertest.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/signin/cookie_clear_on_exit_migration_notice.h"
+
+#include "base/functional/callback_helpers.h"
+#include "chrome/browser/ui/signin/cookie_clear_on_exit_migration_notice.h"
+#include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "content/public/test/browser_test.h"
+
+class CookieClearOnExitMigrationNoticePixelTest : public DialogBrowserTest {
+ public:
+  void ShowUi(const std::string& name) override {
+    ShowCookieClearOnExitMigrationNotice(*browser(), base::DoNothing());
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(CookieClearOnExitMigrationNoticePixelTest,
+                       InvokeUi_default) {
+  ShowAndVerifyUi();
+}
