0f07d99d9cbd0c8500e6733729201266a8429cc6
Dibyajyoti Pal <dibyapal@google.com>
2024-04-30 20:54:13 +0000

    [Shortcuts] Implement badging for site icons
    This CL implements the logic from go/chrome-badging-for-shortcuts for
    icon badging of site icons based on Chrome resource icons. As per
    discussion, icon sizes above 128x128 has a badging ratio of 1/8, while
    other shorter icons have a ratio of 1/4.
    
    Manually tested by hooking up to create shortcut flow, and also
    includes automated testing with a newly introduced command line flag
    that allows updating the baseline that icons are compared with in
    the test.
    
    Change-Id: Ibfce2698a5d1ecde39e538dbf5bc0c3704c8e185
    Bug: 333727532, 333024272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466592
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294573}

--- chrome/browser/shortcuts/document_icon_fetcher_browsertest.cc
+++ chrome/browser/shortcuts/document_icon_fetcher_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/test_future.h"
 #include "base/types/expected.h"
 #include "chrome/browser/shortcuts/fetch_icons_from_document_task.h"
+#include "chrome/browser/shortcuts/image_test_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -23,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
-#include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/test/sk_gmock_support.h"
 
@@ -43,37 +43,6 @@ class DocumentIconFetcherTest : public InProcessBrowserTest {
 
   GURL GetPageWithDefaultFavicon() {
     return default_favicon_server_.GetURL("/index.html");
-  }
-
-  base::expected<SkBitmap, std::string> LoadImageFromTestFile(
-      const base::FilePath& relative_path_from_chrome_data) {
-    base::ScopedAllowBlockingForTesting allow_blocking;
-    // Load image data from test directory.
-    base::FilePath chrome_src_dir;
-    if (!base::PathService::Get(base::DIR_SRC_TEST_DATA_ROOT,
-                                &chrome_src_dir)) {
-      return base::unexpected("Could not find src directory.");
-    }
-
-    base::FilePath image_path =
-        chrome_src_dir.Append(FILE_PATH_LITERAL("chrome/test/data"))
-            .Append(relative_path_from_chrome_data);
-    if (!base::PathExists(image_path)) {
-      return base::unexpected(
-          base::StrCat({"Path does not exist: ", image_path.AsUTF8Unsafe()}));
-    }
-    std::string image_data;
-    if (!base::ReadFileToString(image_path, &image_data)) {
-      return base::unexpected("Could not read file.");
-    }
-
-    SkBitmap image;
-    if (!gfx::PNGCodec::Decode(
-            reinterpret_cast<const uint8_t*>(image_data.data()),
-            image_data.size(), &image)) {
-      return base::unexpected("Could not decode file.");
-    }
-    return image;
   }
 
  private:
---
+++ chrome/browser/shortcuts/icon_badging.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SHORTCUTS_ICON_BADGING_H_
+#define CHROME_BROWSER_SHORTCUTS_ICON_BADGING_H_
+
+#include <vector>
+
+namespace gfx {
+class ImageFamily;
+}  // namespace gfx
+
+class SkBitmap;
+
+namespace shortcuts {
+
+// Badges the Chrome logo, maintaining channel information in branded builds,
+// and the chromium logo on non branded builds to the site icon. This will
+// always return 9 images corresponding to commonly used shortcut sizes on
+// current operating systems, all of which are available in `ShortcutSizes`.
+gfx::ImageFamily ApplyProductLogoBadgeToIcons(std::vector<SkBitmap> icons);
+
+}  // namespace shortcuts
+
+#endif  // CHROME_BROWSER_SHORTCUTS_ICON_BADGING_H_
