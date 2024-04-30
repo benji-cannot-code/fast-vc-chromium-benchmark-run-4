530cdae7aaf070203d8f64a15940040cadd596c6
Lauren Winston <lwinston@google.com>
2024-04-30 23:23:28 +0000

    [Read Aloud] Filter Android Speech voices on ChromeOS Ash.
    These voices were causing problems and clutter in the voice menu.
    
    Additionally, these voices are supposedly remote voices, so they
    should already be filtered out, but they're showing up as local
    voices for some reason.
    
    Fixed: 338097397
    Change-Id: I7b920a1af431c27d34dae3ff2d3ab6eabc31aada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502839
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294666}

--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -712,6 +712,7 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
       .SetProperty("isGoogleDocs", &ReadAnythingAppController::IsGoogleDocs)
       .SetProperty("isReadAloudEnabled",
                    &ReadAnythingAppController::IsReadAloudEnabled)
+      .SetProperty("isChromeOsAsh", &ReadAnythingAppController::IsChromeOsAsh)
       .SetProperty("isAutoVoiceSwitchingEnabled",
                    &ReadAnythingAppController::IsAutoVoiceSwitchingEnabled)
       .SetProperty(
@@ -1164,6 +1165,14 @@ bool ReadAnythingAppController::IsWebUIToolbarEnabled() const {
 
 bool ReadAnythingAppController::IsReadAloudEnabled() const {
   return features::IsReadAnythingReadAloudEnabled();
+}
+
+bool ReadAnythingAppController::IsChromeOsAsh() const {
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  return true;
+#else
+  return false;
+#endif
 }
 
 bool ReadAnythingAppController::IsAutoVoiceSwitchingEnabled() const {
--- chrome/renderer/accessibility/read_anything_app_controller.h
+++ chrome/renderer/accessibility/read_anything_app_controller.h
@@ -178,6 +178,7 @@ class ReadAnythingAppController
   bool IsGoogleDocs() const;
   bool IsWebUIToolbarEnabled() const;
   bool IsReadAloudEnabled() const;
+  bool IsChromeOsAsh() const;
   bool IsAutoVoiceSwitchingEnabled() const;
   bool IsAutomaticWordHighlightingEnabled() const;
   void OnStandardLineSpacing();
