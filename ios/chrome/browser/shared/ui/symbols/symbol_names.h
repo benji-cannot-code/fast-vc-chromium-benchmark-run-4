8051b8873326624fefa73ef4dca77a3064622b14
Federica Germinario <fedegermi@google.com>
2024-04-30 08:54:57 +0000

    [ios] Add kFedexCarrierImage because it's still needed
    This code was removed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/5468189
    but it's still called from parcel_tracking_view.mm
    This CL adds again the code
    
    Change-Id: I21aecf2fc4f93d5fb9e156fff9e0e9d6ec324952
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5501335
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294213}

--- ios/chrome/browser/shared/ui/symbols/symbol_names.h
+++ ios/chrome/browser/shared/ui/symbols/symbol_names.h
@@ -33,6 +33,7 @@ extern NSString* const kGoogleSettingsPasswordsInOtherAppsBannerImage;
 extern NSString* const kLensKeyboardAccessoryImage;
 extern NSString* const kMulticolorChromeballSymbol;
 extern NSString* const kPageInsightsSymbol;
+extern NSString* const kFedexCarrierImage;
 extern NSString* const kUPSCarrierImage;
 extern NSString* const kUSPSCarrierImage;
 #else
