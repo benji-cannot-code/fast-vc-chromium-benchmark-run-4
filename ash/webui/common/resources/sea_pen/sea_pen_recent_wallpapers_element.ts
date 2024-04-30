42d579c2427657aa22d3f9bed7becb2d1e871299
Thuong Phan <thuongphan@chromium.org>
2024-04-30 16:44:04 +0000

    p13n: update RecentSeaPenImageInfo struct
    Replace the SeaPenUserVisibleQuery variable in RecentSeaPenImageInfo
    struct with SeaPenQuery. This allows us to get all the query information
    for a recent image instead of just the visible user prompt like before.
    The CL is to prepare to implement "Create more" feature to regenerate
    images using the same query.
    
    Bug: b/336633810
    Test: browser_tests --gtest_filter="*SeaPen*"
    Test: unit_tests --gtest_filter="*SeaPen*"
    Test: ash_unittest --gtest_filter="*SeaPen*"
    Change-Id: I7cccc2ca97d06f5a41a72f643bba8463839021bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479740
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294409}

--- ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_recent_wallpapers_element.ts
@@ -29,7 +29,7 @@ import {getSeaPenProvider} from './sea_pen_interface_provider.js';
 import {logRecentImageActionMenuItemClick, RecentImageActionMenuItem} from './sea_pen_metrics_logger.js';
 import {getTemplate} from './sea_pen_recent_wallpapers_element.html.js';
 import {WithSeaPenStore} from './sea_pen_store.js';
-import {isImageDataUrl, isNonEmptyArray, isPersonalizationApp, isSeaPenImageId} from './sea_pen_utils.js';
+import {getUserVisibleQuery, isImageDataUrl, isNonEmptyArray, isPersonalizationApp, isSeaPenImageId} from './sea_pen_utils.js';
 
 export class SeaPenRecentImageDeleteEvent extends CustomEvent<null> {
   static readonly EVENT_NAME = 'sea-pen-recent-image-delete';
@@ -189,13 +189,13 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
     }
 
     const data = recentImageData[recentImage];
-    if (!data || !data.imageInfo || !data.imageInfo.userVisibleQuery) {
+    if (!data || !data.imageInfo || !data.imageInfo.query) {
       return null;
     }
 
     const title = isPersonalizationApp() ? 'seaPenAboutDialogPrompt' :
                                            'vcBackgroundAboutDialogPrompt';
-    return this.i18n(title, data.imageInfo.userVisibleQuery.text);
+    return this.i18n(title, getUserVisibleQuery(data.imageInfo.query));
   }
 
   private getWallpaperInfoDateMessage_(
@@ -237,11 +237,11 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
     }
 
     const data = recentImageData[image];
-    if (!data || !data.imageInfo || !data.imageInfo.userVisibleQuery) {
+    if (!data || !data.imageInfo || !data.imageInfo.query) {
       return '';
     }
 
-    return data.imageInfo.userVisibleQuery.text;
+    return getUserVisibleQuery(data.imageInfo.query);
   }
 
   private getAriaIndex_(i: number): number {
@@ -376,7 +376,7 @@ export class SeaPenRecentWallpapersElement extends WithSeaPenStore {
 
     const data = recentImageData[recentImage];
     return !!data && !!data.imageInfo && !!data.imageInfo.creationTime &&
-        !!data.imageInfo.userVisibleQuery;
+        !!data.imageInfo.query;
   }
 
   private shouldShowWallpaperInfoDialog_(
