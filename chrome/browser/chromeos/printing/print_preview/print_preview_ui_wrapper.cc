ce159727eecdd1c677f29283f8459b33cc9cbc9e
Jimmy <jimmyxgong@chromium.org>
2024-04-30 02:30:31 +0000

    cros-preview: Add PrintPreviewUiWrapper skeleton
    PrintPreviewUiWrapper serves as a wrappper for cross
    process communication between the renderer and browser
    process for a given print preview UI. It must exist in
    //chrome to be able to communicate with the renderer.
    
    Bug: 334993067
    Change-Id: I6b0f8713d0d199813a7aae7c666ecfaec40a3310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5474740
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294112}

---
+++ chrome/browser/chromeos/printing/print_preview/print_preview_ui_wrapper.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/printing/print_preview/print_preview_ui_wrapper.h"
+
+#include "components/printing/common/print.mojom.h"
+
+namespace chromeos {
+
+// TODO(jimmyxgong): Implement stub.
+void PrintPreviewUiWrapper::DidPreviewPage(
+    printing::mojom::DidPreviewPageParamsPtr params,
+    int32_t request_id) {}
+
+}  // namespace chromeos
