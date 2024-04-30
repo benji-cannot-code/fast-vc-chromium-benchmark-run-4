c985fd377fd1c23940a3c8a14236fc532995130d
Mike West <mkwst@chromium.org>
2024-04-30 15:09:45 +0000

    Sketching `[InjectionMitigated]` for `getAllScreensMedia()`.
    This CL sketches an IDL attribute that prevents exposure of a given
    attribute/method/interface/etc in the absence of a strict CSP delivered
    as a header, along the lines of the [Securer Contexts explainer][1].
    
    As a proof of concept, the attribute is used to replace a subset of the
    runtime checks currently applied to `MediaDevices.getAllScreensMedia()`.
    This method should eventually be marked `[IsolatedContext]`, but will be
    enabled via alternative mechanisms (OT + admin policy) until IWAs are
    widely available. In the meantime, we can use this exploratory
    attribute to limit the method's exposure to those contexts in which it
    might be available.
    
    [1]: https://github.com/mikewest/securer-contexts
    
    Change-Id: I507722b13219defc8c7b8fd2ead230c1259cbac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458758
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294338}

---
+++ third_party/blink/web_tests/external/wpt/screen-capture/getallscreensmedia-exposure.tentative.https.window.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+async_test(t => {
+  const i = document.createElement('iframe');
+  i.src = "/common/blank.html";
+  i.onload = t.step_func_done(_ => {
+    assert_equals('undefined', typeof i.contentWindow.navigator.mediaDevices.getAllScreensMedia);
+  });
+  document.body.appendChild(i);
+}, "No CSP, no exposure.");
+
+async_test(t => {
+  const i = document.createElement('iframe');
+  i.src = "/content-security-policy/support/echo-policy.py?policy=" +
+          encodeURIComponent("script-src 'none'; object-src 'none'; base-uri 'none'; require-trusted-types-for 'script'");
+  i.onload = t.step_func_done(_ => {
+    assert_equals("function", typeof i.contentWindow.navigator.mediaDevices.getAllScreensMedia);
+  });
+  document.body.appendChild(i);
+}, "Strict CSP + TT, exposure.");
+
