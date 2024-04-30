723433b4bc4b00f5c4bbe4a55010b9f697bf5f46
Vishwas Uppoor <vishwasuppoor@google.com>
2024-04-30 02:24:41 +0000

    [PIX] Pipe PIX accounts to the show method
    Get the PIX accounts from PDM, and pipe it to the show method.
    This change only adds the PIX accounts to FPClient's show method. In
    future changes, it will be sent to the Java side via the controller and
    the bridge.
    
    Fixed: b/314826708
    Change-Id: I573ef4a7ba6f8fb306d00d6e006741bbd09ac33d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5493586
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294102}

--- components/facilitated_payments/core/browser/facilitated_payments_client.h
+++ components/facilitated_payments/core/browser/facilitated_payments_client.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 
+#include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
 #include "components/autofill/core/browser/payments/risk_data_loader.h"
 
 namespace autofill {
+class BankAccount;
 class PersonalDataManager;
 }  // namespace autofill
 
@@ -30,9 +32,11 @@ class FacilitatedPaymentsClient : public autofill::RiskDataLoader {
   // If the UI was shown, then returns true and later invokes the
   // `on_user_decision_callback` with the result of user's selection: a boolean
   // for acceptance or cancellation and the selected instrument ID in case of
-  // acceptance. If the UI was not shown, then returns false and does not invoke
-  // the callback.
+  // acceptance. `pix_account_suggestions` is the list of PIX accounts to be
+  // shown to the user for payment. If the UI was not shown, then returns false
+  // and does not invoke the callback.
   virtual bool ShowPixPaymentPrompt(
+      base::span<autofill::BankAccount> bank_account_suggestions,
       base::OnceCallback<void(bool, int64_t)> on_user_decision_callback);
 };
 
