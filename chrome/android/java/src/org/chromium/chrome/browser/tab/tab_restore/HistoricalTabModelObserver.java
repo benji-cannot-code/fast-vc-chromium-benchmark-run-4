68b94439033c338fada691481abc5ec0ec406495
Calder Kitagawa <ckitagawa@chromium.org>
2024-04-30 20:02:07 +0000

    [Tab Group Sync] Don't save hiding tab groups to recents
    Prevent any tab groups in the hiding state from being saved to Recent
    Tabs. These tab groups will appear in the Tab Group Pane instead.
    Groups that will be deleted from the Tab Group Pane will still appear
    here.
    
    Bug: 337029681
    Change-Id: Id690d02149a9ab65691c2c91ef4a11debbb64f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491611
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294537}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabModelObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/tab_restore/HistoricalTabModelObserver.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab.tab_restore;
 
+import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Token;
@@ -40,12 +41,12 @@ public class HistoricalTabModelObserver implements TabModelObserver {
         mTabGroupModelFilter = (TabGroupModelFilter) tabModelFilter;
         mHistoricalTabSaver = historicalTabSaver;
 
-        tabModelFilter.getTabModel().addObserver(this);
+        tabModelFilter.addObserver(this);
     }
 
     /** Removes observers. */
     public void destroy() {
-        mTabGroupModelFilter.getTabModel().removeObserver(this);
+        mTabGroupModelFilter.removeObserver(this);
     }
 
     @Override
@@ -68,6 +69,11 @@ public class HistoricalTabModelObserver implements TabModelObserver {
         List<HistoricalEntry> entries = new ArrayList<>();
 
         for (Tab tab : tabs) {
+            // Ignore tab groups that are being hidden. They will be accessible from the tab group
+            // pane instead.
+            @Nullable Token tabGroupId = tab.getTabGroupId();
+            if (mTabGroupModelFilter.isTabGroupHiding(tabGroupId)) continue;
+
             // {@link TabGroupModelFilter} removes tabs from its data model as soon as they are
             // pending closure so it cannot be directly relied upon for group structure. Instead
             // rely on the underlying root ID in the tab's persisted data which is used to restore
@@ -88,7 +94,7 @@ public class HistoricalTabModelObserver implements TabModelObserver {
             List<Tab> groupTabs = new ArrayList<Tab>();
             groupTabs.add(tab);
             HistoricalEntry historicalGroup =
-                    new HistoricalEntry(rootId, tab.getTabGroupId(), title, color, groupTabs);
+                    new HistoricalEntry(rootId, tabGroupId, title, color, groupTabs);
             entries.add(historicalGroup);
             idToGroup.put(rootId, historicalGroup);
         }
