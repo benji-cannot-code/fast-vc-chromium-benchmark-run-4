633164b2fce77a72e007205051655bc125c9e67b
Florian Leimgruber <fleimgruber@google.com>
2024-04-30 08:22:01 +0000

    Remove PDMObserver from FormDataImporter
    As part of go/split-pdm the PDMObserver is split into an address and a
    payments data manager observer. The FormDataImporter only cares about
    address changes (to remove potential multi-step import candidates).
    Thus, this CL replaces the FormDataImporter's PDMObserver with an
    AddressDataManager::Observer.
    
    Bug: b/322170538
    Change-Id: I2e1c1684faaaee2587ccb89c6b1ab9f7efb6e34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490445
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294199}

--- components/autofill/core/browser/form_data_importer_utils.h
+++ components/autofill/core/browser/form_data_importer_utils.h
@@ -14,12 +14,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/time/time.h"
+#include "components/autofill/core/browser/address_data_manager.h"
 #include "components/autofill/core/browser/autofill_profile_import_process.h"
 #include "components/autofill/core/browser/country_type.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_profile_comparator.h"
 #include "components/autofill/core/browser/form_structure.h"
-#include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/common/autofill_clock.h"
 #include "components/autofill/core/common/logging/log_buffer.h"
 #include "components/autofill/core/common/signatures.h"
@@ -136,11 +136,11 @@ class MultiStepImportMerger {
   // Stored profiles can be deleted/modified by the user/through sync, etc. This
   // potentially invalidates `multistep_candidates_`.
   // This function verifies all already imported `multistep_candidates_`
-  // against the corresponding profile stored in the `personal_data_manager`.
-  // Profiles that no longer exist in the personal data manager are removed from
+  // against the corresponding profile stored in the `address_data_manager`.
+  // Profiles that no longer exist in the address data manager are removed from
   // `multistep_candidates`. Similarly, profiles that were in modified in the
-  // personal data manager are updated in `multistep_candidates`.
-  void OnPersonalDataChanged(PersonalDataManager& personal_data_manager);
+  // address data manager are updated in `multistep_candidates`.
+  void OnAddressDataChanged(AddressDataManager& address_data_manager);
 
   void OnBrowsingHistoryCleared(const history::DeletionInfo& deletion_info);
 
