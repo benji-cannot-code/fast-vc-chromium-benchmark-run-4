3efbdf5e984b2e4920e78d447c5dfb2020ad72a3
Clark DuVall <cduvall@chromium.org>
2024-04-30 15:54:45 +0000

    Add histogram to track how long AwContents constructor takes
    Bug: 332706093
    Change-Id: I0c4b6c3ac4ffed7dec5cad449662bc59e690ba91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499315
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294367}

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -206,6 +206,9 @@ public class AwContents implements SmartClipProvider {
     private static final Pattern sDataURLWithSelectorPattern =
             Pattern.compile("^[^#]*(#[A-Za-z][A-Za-z0-9\\-_:.]*)$");
 
+    private static final String CONSTRUCTOR_HISTOGRAM_NAME =
+            "Android.WebView.AwContentsConstructorTime";
+
     private static class ForceAuxiliaryBitmapRendering {
         private static final boolean sResult = lazyCheck();
 
@@ -1271,6 +1274,7 @@ public class AwContents implements SmartClipProvider {
             AwSettings settings,
             DependencyFactory dependencyFactory) {
         assert browserContext != null;
+        long startTime = SystemClock.uptimeMillis();
         sLastId += 1;
         mId = sLastId;
         if (!browserContext.isDefaultAwBrowserContext()) {
@@ -1389,6 +1393,11 @@ public class AwContents implements SmartClipProvider {
             }
 
             onContainerViewChanged();
+        }
+        long delta = SystemClock.uptimeMillis() - startTime;
+        RecordHistogram.recordTimesHistogram(CONSTRUCTOR_HISTOGRAM_NAME, delta);
+        if (mId == 1) {
+            RecordHistogram.recordTimesHistogram(CONSTRUCTOR_HISTOGRAM_NAME + ".First", delta);
         }
     }
 
