e01b24f2cf6ddc8e49b767c3cf32998c329c8c67
Stephen McGruer <smcgruer@chromium.org>
2024-04-30 18:06:04 +0000

    [SPC] Add experimental ability to show network/issuer icons (2/N)
    This CL is the second in a chain of CLs to add the ability, behind a
    flag, to include card network and issuer icons in the SPC transaction
    dialog. This is intended purely as a UX experiment and so the API
    changes are not 'fully' designed - for example, we use very
    card-specific terminology (Network, Issuer) which we do not generally do
    for SPC as it is payment method agnostic.
    
    This second CL hooks up the data from the model to the UI. The presented
    data is not yet signed into the ClientDataJSON, which it would need to
    be for any non-experimental launch of this feature.
    
    Before: https://screenshot.googleplex.com/9ZcvVbPNrZteeYo.png
    After (with flag): https://screenshot.googleplex.com/BwicVFABHFm4DcG.png
    
    Bug: 333945861
    Change-Id: Ic46e63a86d84161531c80553028575619fc79c76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492140
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294471}

--- components/payments/core/sizes.h
+++ components/payments/core/sizes.h
@@ -8,16 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace payments {
 
-// The default width of the instrument icon for SPC. Based on the size of the
-// default instrument icon.
-constexpr int kSecurePaymentConfirmationInstrumentIconDefaultWidthPx = 20;
+// The default width of icons for the SPC Transaction UX. Used for any fallback
+// icons that may be shown if the caller-supplied icon isn't available.
+inline constexpr int kSecurePaymentConfirmationIconDefaultWidthPx = 20;
 
-// The maximum width of the instrument icon for SPC. Used as the preferred size
-// of downloaded instrument icons.
-constexpr int kSecurePaymentConfirmationInstrumentIconMaximumWidthPx = 32;
+// The maximum width of icons in the SPC Transaction UX. Used as the preferred
+// size of downloaded icons.
+inline constexpr int kSecurePaymentConfirmationIconMaximumWidthPx = 32;
 
-// The default height of the instrument icon for SPC.
-constexpr int kSecurePaymentConfirmationInstrumentIconHeightPx = 20;
+// The default height of icons for the SPC Transaction UX.
+inline constexpr int kSecurePaymentConfirmationIconHeightPx = 20;
 
 }  // namespace payments
 
