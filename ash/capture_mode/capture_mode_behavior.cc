f311031154e2c441d58289ac60f738444078011e
Ahmed Fakhry <afakhry@chromium.org>
2024-04-30 15:26:48 +0000

    capture_mode: Remove audio mixing feature and chrome flags
    The Audio Mixing feature launched in M-119, and has been enabled
    by default since then. This CL cleans up the feature and chrome
    flags.
    
    Fixed: b/337961107
    Test: Manually, existing tests.
    Change-Id: I762c9b09e137270bfa600e0413679850f3c37890
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499171
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294352}

--- ash/capture_mode/capture_mode_behavior.cc
+++ ash/capture_mode/capture_mode_behavior.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_util.h"
 #include "ash/capture_mode/game_capture_bar_view.h"
 #include "ash/capture_mode/normal_capture_bar_view.h"
-#include "ash/constants/ash_features.h"
 #include "ash/projector/projector_controller_impl.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shelf/shelf_layout_manager.h"
@@ -125,11 +124,12 @@ class ProjectorBehavior : public CaptureModeBehavior {
     switch (mode) {
       case AudioRecordingMode::kOff:
       case AudioRecordingMode::kSystem:
+        // Projector does not support turning off audio recording nor recording
+        // the system audio separately without the microphone.
         return false;
       case AudioRecordingMode::kMicrophone:
-        return true;
       case AudioRecordingMode::kSystemAndMicrophone:
-        return features::IsCaptureModeAudioMixingEnabled();
+        return true;
     }
   }
   bool ShouldCreateRecordingOverlayController() const override { return true; }
@@ -178,13 +178,11 @@ class GameDashboardBehavior : public CaptureModeBehavior,
                               public aura::WindowObserver {
  public:
   GameDashboardBehavior()
-      : CaptureModeBehavior({CaptureModeType::kVideo,
-                             CaptureModeSource::kWindow, RecordingType::kWebM,
-                             features::IsCaptureModeAudioMixingEnabled()
-                                 ? AudioRecordingMode::kSystemAndMicrophone
-                                 : AudioRecordingMode::kMicrophone,
-                             /*demo_tools_enabled=*/false},
-                            BehaviorType::kGameDashboard) {}
+      : CaptureModeBehavior(
+            {CaptureModeType::kVideo, CaptureModeSource::kWindow,
+             RecordingType::kWebM, AudioRecordingMode::kSystemAndMicrophone,
+             /*demo_tools_enabled=*/false},
+            BehaviorType::kGameDashboard) {}
 
   GameDashboardBehavior(const GameDashboardBehavior&) = delete;
   GameDashboardBehavior operator=(const GameDashboardBehavior&) = delete;
@@ -348,10 +346,9 @@ bool CaptureModeBehavior::SupportsAudioRecordingMode(
   switch (mode) {
     case AudioRecordingMode::kOff:
     case AudioRecordingMode::kMicrophone:
-      return true;
     case AudioRecordingMode::kSystem:
     case AudioRecordingMode::kSystemAndMicrophone:
-      return features::IsCaptureModeAudioMixingEnabled();
+      return true;
   }
 }
 
