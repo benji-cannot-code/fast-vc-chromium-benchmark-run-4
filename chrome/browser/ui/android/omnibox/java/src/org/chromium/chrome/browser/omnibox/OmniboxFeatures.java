ac5fc7b69acbd48ad8fe3581691207161bd61e90
Tomasz Wiszkowski <ender@google.com>
2024-04-30 20:20:19 +0000

    Migrate duplicate cached Feature/Param combinations to common code.
    Change-Id: I47a74f9f215eb7e61c71edbc695009c55f927a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499676
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294549}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxFeatures.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/OmniboxFeatures.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.omnibox;
 
 import android.content.Context;
-import android.util.ArraySet;
 
 import org.chromium.base.BaseSwitches;
 import org.chromium.base.CommandLine;
@@ -16,7 +15,6 @@ import org.chromium.base.cached_flags.BooleanCachedFieldTrialParameter;
 import org.chromium.base.cached_flags.CachedFieldTrialParameter;
 import org.chromium.base.cached_flags.CachedFlag;
 import org.chromium.base.cached_flags.CachedFlagUtils;
-import org.chromium.build.BuildConfig;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.util.ConversionUtils;
 import org.chromium.components.omnibox.OmniboxFeatureList;
@@ -24,7 +22,6 @@ import org.chromium.components.omnibox.OmniboxFeatureMap;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /** This is the place where we define these: List of Omnibox features and parameters. */
 public class OmniboxFeatures {
@@ -43,7 +40,6 @@ public class OmniboxFeatures {
     // Each flag created via newFlag() will be automatically added to this list.
     private static final List<CachedFlag> sCachedFlags = new ArrayList<>();
     private static final List<CachedFieldTrialParameter> sCachedParams = new ArrayList<>();
-    private static final Set<String> sCachedParameterNames = new ArraySet<>();
 
     /// Holds the information whether logic should focus on preserving memory on this device.
     private static Boolean sIsLowMemoryDevice;
@@ -88,22 +84,8 @@ public class OmniboxFeatures {
         var param =
                 new BooleanCachedFieldTrialParameter(
                         OmniboxFeatureMap.getInstance(), featureName, variationName, defaultValue);
-        declareCachedFieldTrialParameter(param);
-        return param;
-    }
-
-    /** Add CachedFieldTrialParameter to a list of params to be preserved. */
-    private static void declareCachedFieldTrialParameter(CachedFieldTrialParameter param) {
-        if (BuildConfig.ENABLE_ASSERTS) {
-            String combinedName = param.getFeatureName() + ":" + param.getParameterName();
-            assert !sCachedParameterNames.contains(combinedName)
-                    : String.format(
-                            "Feature '%s' has a duplicate parameter '%s'",
-                            param.getFeatureName(), param.getParameterName());
-            sCachedParameterNames.add(combinedName);
-        }
-
         sCachedParams.add(param);
+        return param;
     }
 
     /**
