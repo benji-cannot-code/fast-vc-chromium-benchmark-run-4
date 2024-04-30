9be6e7dda8dfd147357e4890e9112b2bf702d095
Gauthier Ambard <gambard@chromium.org>
2024-04-30 10:19:47 +0000

    [iOS] Add a TabGroup snapshot request UUID
    If a new request is started, prevent the previous from completing.
    
    Fixed: 337864491
    Change-Id: I1f75f8cd1ed3526d3fca171319f7ec1b972b5b92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494369
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294232}

--- ios/chrome/browser/ui/tab_switcher/tab_group_item.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_group_item.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSMutableDictionary<NSNumber*, GroupTabInfo*>* _tabGroupInfos;
   base::WeakPtr<const TabGroup> _tabGroup;
   const void* _tabGroupIdentifier;
+  NSUUID* _requestUUID;
 }
 
 - (instancetype)initWithTabGroup:(const TabGroup*)tabGroup
@@ -89,6 +90,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Reset the array to ensure to not display old snapshots and or favicons.
   [_tabGroupInfos removeAllObjects];
+  NSUUID* UUID = [NSUUID UUID];
+  _requestUUID = UUID;
   NSUInteger numberOfRequests = MIN(7, _tabGroup->range().count());
   int firstIndex = _tabGroup->range().range_begin();
   for (NSUInteger requestIndex = 0; requestIndex < numberOfRequests;
@@ -99,11 +102,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     __weak TabGroupItem* weakSelf = self;
     [TabGroupUtils fetchTabGroupInfoFromWebState:webState
                                       completion:^(GroupTabInfo* info) {
-                                        [weakSelf addInfo:info
-                                            forRequestNumber:requestIndex];
                                         [weakSelf
-                                            notifyCompletion:completion
-                                            numberOfRequests:numberOfRequests];
+                                            groupTabInfoFetched:info
+                                               forRequestNumber:requestIndex
+                                                numberOfRequest:numberOfRequests
+                                                     completion:completion
+                                                    requestUUID:UUID];
                                       }];
   }
 }
@@ -116,30 +120,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - Private helpers
 
-// Adds the given info to the GroupTabInfo array.
-- (void)addInfo:(GroupTabInfo*)info forRequestNumber:(NSUInteger)requestNumber {
-  _tabGroupInfos[@(requestNumber)] = info;
-}
-
-// Saves the snapshot and favicon couple in the same GroupTabInfo. Call the
-// completion if there is no new snapshot or favicon to save.
-- (void)notifyCompletion:(GroupTabInfosFetchingCompletionBlock)completion
-        numberOfRequests:(NSUInteger)numberOfRequests {
-  if (!_tabGroup) {
-    completion(self, @[]);
+// Called when `info` for the `requestNumber` out of `numberOfRequests` is
+// fetched for the `requestUUID`.
+- (void)groupTabInfoFetched:(GroupTabInfo*)info
+           forRequestNumber:(NSUInteger)requestNumber
+            numberOfRequest:(NSUInteger)numberOfRequests
+                 completion:(GroupTabInfosFetchingCompletionBlock)completion
+                requestUUID:(NSUUID*)requestUUId {
+  if (![requestUUId isEqual:_requestUUID] || !_tabGroup) {
     return;
   }
-  if (_tabGroupInfos.count == numberOfRequests) {
-    auto comparator = ^NSComparisonResult(NSNumber* obj1, NSNumber* obj2) {
-      return [obj1 compare:obj2];
-    };
-    NSMutableArray* infos = [NSMutableArray array];
-    for (NSNumber* key in
-         [[_tabGroupInfos allKeys] sortedArrayUsingComparator:comparator]) {
-      [infos addObject:_tabGroupInfos[key]];
-    }
-    completion(self, infos);
+
+  _tabGroupInfos[@(requestNumber)] = info;
+
+  if (_tabGroupInfos.count != numberOfRequests) {
+    return;
   }
+
+  auto comparator = ^NSComparisonResult(NSNumber* obj1, NSNumber* obj2) {
+    return [obj1 compare:obj2];
+  };
+  NSMutableArray* infos = [NSMutableArray array];
+  for (NSNumber* key in
+       [[_tabGroupInfos allKeys] sortedArrayUsingComparator:comparator]) {
+    [infos addObject:_tabGroupInfos[key]];
+  }
+  completion(self, infos);
 }
 
 @end
