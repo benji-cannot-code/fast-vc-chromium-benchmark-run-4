a9afa0801ebd64cad0bf9bff8bdf341d16d9c1cf
weidongliu <weidongliu@chromium.org>
2024-04-30 14:08:38 +0000

    views: use CalculatePreferredSize(SizeBounds) in /chrome/browser/ui/views/location_bar/cookie_controls.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    This CL was uploaded by git cl split.
    
    Bug: 40232718
    Change-Id: I392e4601ffaf81e337bd9ff3dd499cda37f0493d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492831
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294310}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.h
@@ -52,7 +52,8 @@ class CookieControlsBubbleViewImpl : public CookieControlsBubbleView,
       base::RepeatingClosureList::CallbackType callback) override;
 
  protected:
-  gfx::Size CalculatePreferredSize() const override;
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override;
 
  private:
   // LocationBarBubbleDelegateView:
