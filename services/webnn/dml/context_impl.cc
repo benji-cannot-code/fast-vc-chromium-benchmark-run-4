5f05a5aa8a8c1caaa50c2fc43d9f2f24f518d4e4
Pooja Patel <patelpooja@microsoft.com>
2024-04-30 18:15:51 +0000

    Convert d3d-related DLOGs under services/webnn to LOGs
    We want general d3d-related errors to show up for users in
    about://gpu - let's convert some of these dlogs into logs.
    
    Bug: 334988065
    Change-Id: I17f1bc19329096dd22f199eae96eb0e184e3281c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499567
    Commit-Queue: Pooja Patel <patelpooja@microsoft.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1294474}

--- services/webnn/dml/context_impl.cc
+++ services/webnn/dml/context_impl.cc
@@ -67,8 +67,8 @@ std::unique_ptr<WebNNBufferImpl> ContextImpl::CreateBufferImpl(
       CreateDefaultBuffer(adapter_->d3d12_device(), aligned_buffer_byte_size,
                           L"WebNN_Default_Buffer_External", buffer);
   if (FAILED(hr)) {
-    DLOG(ERROR) << "Failed to create the default buffer: "
-                << logging::SystemErrorCodeToString(hr);
+    LOG(ERROR) << "[WebNN] Failed to create the default buffer: "
+               << logging::SystemErrorCodeToString(hr);
     return nullptr;
   }
 
@@ -95,8 +95,8 @@ void ContextImpl::ReadBuffer(const WebNNBufferImpl& src_buffer,
   hr = CreateReadbackBuffer(adapter_->d3d12_device(), src_buffer_size,
                             L"WebNN_Readback_Buffer", download_buffer);
   if (FAILED(hr)) {
-    DLOG(ERROR) << "Failed to create the download buffer: "
-                << logging::SystemErrorCodeToString(hr);
+    LOG(ERROR) << "[WebNN] Failed to create the download buffer: "
+               << logging::SystemErrorCodeToString(hr);
     std::move(callback).Run(ToError<mojom::ReadBufferResult>(
         mojom::Error::Code::kUnknownError, "Failed to read buffer."));
     return;
@@ -154,8 +154,8 @@ void ContextImpl::OnReadbackComplete(
   void* mapped_download_data = nullptr;
   hr = download_buffer->Map(0, nullptr, &mapped_download_data);
   if (FAILED(hr)) {
-    DLOG(ERROR) << "Failed to map the download buffer: "
-                << logging::SystemErrorCodeToString(hr);
+    LOG(ERROR) << "[WebNN] Failed to map the download buffer: "
+               << logging::SystemErrorCodeToString(hr);
     std::move(callback).Run(ToError<mojom::ReadBufferResult>(
         mojom::Error::Code::kUnknownError, "Failed to read buffer."));
     return;
@@ -182,8 +182,8 @@ void ContextImpl::WriteBuffer(const WebNNBufferImpl& dst_buffer,
                                   L"WebNN_Upload_Buffer", upload_buffer);
   if (FAILED(hr)) {
     // TODO(crbug.com/41492165): generate error using context.
-    DLOG(ERROR) << "Failed to create the upload buffer: "
-                << logging::SystemErrorCodeToString(hr);
+    LOG(ERROR) << "[WebNN] Failed to create the upload buffer: "
+               << logging::SystemErrorCodeToString(hr);
     return;
   }
 
@@ -193,8 +193,8 @@ void ContextImpl::WriteBuffer(const WebNNBufferImpl& dst_buffer,
   void* mapped_upload_data = nullptr;
   hr = upload_buffer->Map(0, nullptr, &mapped_upload_data);
   if (FAILED(hr)) {
-    DLOG(ERROR) << "Failed to map the upload buffer: "
-                << logging::SystemErrorCodeToString(hr);
+    LOG(ERROR) << "[WebNN] Failed to map the upload buffer: "
+               << logging::SystemErrorCodeToString(hr);
     return;
   }
 
--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -191,7 +191,7 @@ void WebNNContextProviderImpl::CreateWebNNContext(
     std::move(callback).Run(ToError<mojom::CreateContextResult>(
         mojom::Error::Code::kNotSupportedError,
         "The cpu device is not supported."));
-    DLOG(ERROR) << "WebNN Service is not supported on CPU on Windows.";
+    LOG(ERROR) << "[WebNN] Service is not supported on CPU on Windows.";
     return;
   }
 
@@ -201,7 +201,7 @@ void WebNNContextProviderImpl::CreateWebNNContext(
     std::move(callback).Run(ToError<mojom::CreateContextResult>(
         mojom::Error::Code::kNotSupportedError,
         "WebNN is not compatible with GPU."));
-    DLOG(ERROR) << "WebNN is not compatible with GPU.";
+    LOG(ERROR) << "[WebNN] is not compatible with GPU.";
     return;
   }
 
@@ -235,7 +235,7 @@ void WebNNContextProviderImpl::CreateWebNNContext(
     std::move(callback).Run(mojom::CreateContextResult::NewError(
         dml::CreateError(mojom::Error::Code::kUnknownError,
                          "Failed to create a WebNN context.")));
-    DLOG(ERROR) << "Failed to open the command recorder.";
+    LOG(ERROR) << "[WebNN] Failed to open the command recorder.";
     return;
   }
 
@@ -262,14 +262,14 @@ void WebNNContextProviderImpl::CreateWebNNContext(
     std::move(callback).Run(ToError<mojom::CreateContextResult>(
         mojom::Error::Code::kNotSupportedError,
         "WebNN Service is not supported on this platform."));
-    DLOG(ERROR) << "WebNN Service is not supported on this platform.";
+    LOG(ERROR) << "[WebNN] Service is not supported on this platform.";
   }
 #else
   // TODO(crbug.com/40206287): Supporting WebNN Service on the platform.
   std::move(callback).Run(ToError<mojom::CreateContextResult>(
       mojom::Error::Code::kNotSupportedError,
       "WebNN Service is not supported on this platform."));
-  DLOG(ERROR) << "WebNN Service is not supported on this platform.";
+  LOG(ERROR) << "[WebNN] Service is not supported on this platform.";
 #endif
 }
 
