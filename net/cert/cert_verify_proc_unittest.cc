e8b0dbf37377c68f81da040508b696ae8f33b797
Emily Stark <estark@google.com>
2024-04-30 15:55:00 +0000

    Change CERT_STATUS_NONUNIQUE_NAME back to a warning on cronet
    Cronet consumers have a test setup that uses a known root that issues
    certs for intranet names. It'll take some time to invetsigate how to
    fix these tests, so in the meantime, this CL changes this error back
    to a warning for Cronet specifically.
    
    Bug: 41437527, b/337196170
    Change-Id: Idd721362ab44b2db7042c84bcfa0e97710c97239
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498975
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294368}

--- net/cert/cert_verify_proc_unittest.cc
+++ net/cert/cert_verify_proc_unittest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "crypto/sha2.h"
+#include "net/base/cronet_buildflags.h"
 #include "net/base/net_errors.h"
 #include "net/cert/asn1_util.h"
 #include "net/cert/cert_net_fetcher.h"
@@ -1486,7 +1487,13 @@ TEST(CertVerifyProcTest, IntranetHostsRejected) {
                               /*ocsp_response=*/std::string(),
                               /*sct_list=*/std::string(), 0, &verify_result,
                               NetLogWithSource());
+  // Intranet certificates from known roots are accepted without error in Cronet
+  // to avoid breaking consumer tests. See b/337196170 (Google-internal).
+#if BUILDFLAG(CRONET_BUILD)
+  EXPECT_THAT(error, IsOk());
+#else
   EXPECT_THAT(error, IsError(ERR_CERT_NON_UNIQUE_NAME));
+#endif  // BUILDFLAG(CRONET_BUILD)
   EXPECT_TRUE(verify_result.cert_status & CERT_STATUS_NON_UNIQUE_NAME);
 
   // However, if the CA is not well known, these should not be flagged:
