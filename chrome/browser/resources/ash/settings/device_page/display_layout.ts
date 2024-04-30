bb0ad39580deeb55764b0ebf7c942a5634285083
rbpotter <rbpotter@chromium.org>
2024-04-30 20:42:17 +0000

    WebUI: Remove paper-styles/shadow.js from the repo
    This was still being imported in 5 locations in settings, PDF viewer,
    OS Settings, and the ChromeOS emulator page, but none of these pages
    actually used any of the CSS variables defined in shadow.js. Remove the
    imports since they were not doing anything, and remove the file from
    the repo and build.
    
    Bug: 40943652
    Change-Id: I3afe2dca9e95e038859a7a2d11f9d4d3dcfdec05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5500541
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294562}

--- chrome/browser/resources/ash/settings/device_page/display_layout.ts
+++ chrome/browser/resources/ash/settings/device_page/display_layout.ts
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * more displays and allows them to be arranged.
  */
 
-import 'chrome://resources/polymer/v3_0/paper-styles/shadow.js';
 import '../settings_shared.css.js';
 
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
