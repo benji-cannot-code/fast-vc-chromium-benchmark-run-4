81d67d3cc985efaf8c9a43c8138568451d7fbed9
weidongliu <weidongliu@chromium.org>
2024-04-30 04:32:25 +0000

    views: BaseScrollBarThumb use CalculatePreferredSize(SizeBounds) override.
    In order to ensure implementation consistency and avoid oversights, we
    should unify CalculatePreferredSize() and GetHeightForWidth(). Replace
    them with the CalculatePreferredSize(SizeBounds) overload.
    
    Bug: 40232718
    Change-Id: I6ee93de16937a8e867ab505b2dba74c8586838f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491353
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294158}

--- ui/views/controls/scrollbar/scroll_bar_views.cc
+++ ui/views/controls/scrollbar/scroll_bar_views.cc
@@ -36,7 +36,9 @@ class ScrollBarThumb : public BaseScrollBarThumb {
   explicit ScrollBarThumb(ScrollBar* scroll_bar);
   ~ScrollBarThumb() override;
 
-  gfx::Size CalculatePreferredSize() const override;
+  // BaseScrollBarThumb:
+  gfx::Size CalculatePreferredSize(
+      const SizeBounds& available_size) const override;
 
  protected:
   void OnPaint(gfx::Canvas* canvas) override;
@@ -55,7 +57,8 @@ ScrollBarThumb::ScrollBarThumb(ScrollBar* scroll_bar)
 
 ScrollBarThumb::~ScrollBarThumb() = default;
 
-gfx::Size ScrollBarThumb::CalculatePreferredSize() const {
+gfx::Size ScrollBarThumb::CalculatePreferredSize(
+    const SizeBounds& /*available_size*/) const {
   if (!GetWidget())
     return gfx::Size();
   return GetNativeTheme()->GetPartSize(
