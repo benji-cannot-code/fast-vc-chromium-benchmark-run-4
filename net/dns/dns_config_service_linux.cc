b4d565b9b641632231f0c3eb68f9d2ece0aa7d89
Tsuyoshi Horo <horo@chromium.org>
2024-04-30 23:21:13 +0000

    Cleanup DNS related UMAs
    OBSOLETE_HISTOGRAM[Net.DNS.DnsConfig.Resolv.FileChange]=Not monitored recently.
    OBSOLETE_HISTOGRAM[Net.DNS.DnsConfig.Resolv.Read]=Not monitored recently.
    OBSOLETE_HISTOGRAM[Net.DNS.DnsConfig.Resolv.Valid]=Not monitored recently.
    
    Net.DNS.DnsConfig.Resolv.Compatible and Net.DNS.DnsConfig.Windows.*
    metrics are still useful.
    
    Fixed: 330166584, 333390644, 333389848
    Change-Id: If445d82f290b5b155e79f6428f6b3d66ce707814
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5501156
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294665}

--- net/dns/dns_config_service_linux.cc
+++ net/dns/dns_config_service_linux.cc
@@ -371,7 +371,6 @@ class DnsConfigServiceLinux::Watcher : public DnsConfigService::Watcher {
 
  private:
   void OnResolvFilePathWatcherChange(const base::FilePath& path, bool error) {
-    base::UmaHistogramBoolean("Net.DNS.DnsConfig.Resolv.FileChange", true);
     OnConfigChanged(!error);
   }
 
@@ -452,12 +451,8 @@ class DnsConfigServiceLinux::ConfigReader : public SerialWorker {
         }
       }
 
-      base::UmaHistogramBoolean("Net.DNS.DnsConfig.Resolv.Read",
-                                dns_config_.has_value());
       if (!dns_config_.has_value())
         return;
-      base::UmaHistogramBoolean("Net.DNS.DnsConfig.Resolv.Valid",
-                                dns_config_->IsValid());
       base::UmaHistogramBoolean("Net.DNS.DnsConfig.Resolv.Compatible",
                                 !dns_config_->unhandled_options);
 
