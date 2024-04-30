2110a6d3962e6538dd2516c44728f47fa062f760
Sunggook Chue <sunggch@microsoft.com>
2024-04-30 17:58:33 +0000

    Increase test time for back-forward-cache-with-open-webrtc-connection.https.window.js
    There is high number of test timeout back-forward-cache-with-open-webrtc-connection.https.window.js
    so it adds timeout=long for increased test time.
    
    Bug: 337094902
    Change-Id: I28f27eecde19be67b9b6a33cdcb9fff947550349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494542
    Reviewed-by: Yuzu Saijo <yuzus@chromium.org>
    Commit-Queue: Sunggook Chue <sunggch@microsoft.com>
    Reviewed-by: Rahul Singh <rahsin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1294462}

--- third_party/blink/web_tests/external/wpt/webrtc/back-forward-cache-with-open-webrtc-connection.https.window.js
+++ third_party/blink/web_tests/external/wpt/webrtc/back-forward-cache-with-open-webrtc-connection.https.window.js
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/html/browsers/browsing-the-web/back-forward-cache/resources/rc-helper.js
 // META: script=/html/browsers/browsing-the-web/remote-context-helper/resources/remote-context-helper.js
 // META: script=resources/webrtc-test-helpers.sub.js
+// META: timeout=long
 
 'use strict';
 
