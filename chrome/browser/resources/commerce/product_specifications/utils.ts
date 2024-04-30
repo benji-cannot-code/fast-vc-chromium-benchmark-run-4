26877401c3a428445dec4577221440ec7f085d3d
Emily Shack <emshack@chromium.org>
2024-04-30 19:32:30 +0000

    Refactor product selection menu into new component
    This will (hopefully) make the files a bit more skimmable, and will
    better scale when introducing a new type of button that can trigger the
    menu
    
    Bug: b:331956620
    Change-Id: Ib91ff63337a7b3f996ec173fda675f45411870b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499497
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294514}

---
+++ chrome/browser/resources/commerce/product_specifications/utils.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {assert} from 'chrome://resources/js/assert.js';
+
+export interface UrlListEntry {
+  title: string;
+  url: string;
+  imageUrl: string;
+}
+
+export function getAbbreviatedUrl(urlString: string) {
+  const url = new URL(urlString);
+  // Chrome URLs should all have been filtered out.
+  assert(url.protocol !== 'chrome:');
+  return url.hostname;
+}
