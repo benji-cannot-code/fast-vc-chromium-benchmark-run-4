e3754d2625ae11105ff3d21433d837ef62efd3ba
Arnaud Mandy <arnaud.mandy@intel.com>
2024-04-30 16:43:44 +0000

    compute pressure: Rename supportedSources to knownSources
    Based on specification change [1], supportedSources attribute
    is renamed to knownSources.
    
    [1]: https://github.com/w3c/compute-pressure/pull/268
    
    Bug: 337837870
    Change-Id: Ic80d0c806531ed99f8bc2c368f89e55427b0202c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491092
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294408}

--- third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
+++ third_party/blink/renderer/modules/compute_pressure/pressure_observer.cc
@@ -44,7 +44,7 @@ PressureObserver* PressureObserver::Create(V8PressureUpdateCallback* callback) {
 }
 
 // static
-Vector<V8PressureSource> PressureObserver::supportedSources() {
+Vector<V8PressureSource> PressureObserver::knownSources() {
   return Vector<V8PressureSource>(
       {V8PressureSource(V8PressureSource::Enum::kCpu)});
 }
@@ -130,7 +130,7 @@ void PressureObserver::disconnect() {
   }
 
   // Reject all pending promises.
-  for (const auto& source : supportedSources()) {
+  for (const auto& source : knownSources()) {
     RejectPendingResolvers(source.AsEnum(), DOMExceptionCode::kAbortError,
                            "Called disconnect method.");
   }
@@ -245,7 +245,7 @@ void PressureObserver::OnBindingFailed(V8PressureSource::Enum source,
 }
 
 void PressureObserver::OnConnectionError() {
-  for (const auto& source : supportedSources()) {
+  for (const auto& source : knownSources()) {
     RejectPendingResolvers(source.AsEnum(),
                            DOMExceptionCode::kNotSupportedError,
                            "Connection error.");
--- third_party/blink/web_tests/external/wpt/compute-pressure/compute_pressure_known_sources.https.any.js
+++ third_party/blink/web_tests/external/wpt/compute-pressure/compute_pressure_known_sources.https.any.js
@@ -5,18 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 test(() => {
   // Compute Pressure should support at least "cpu"
-  const sources = PressureObserver.supportedSources;
+  const sources = PressureObserver.knownSources;
   assert_in_array('cpu', sources);
 }, 'PressureObserver should support at least "cpu"');
 
 test(() => {
   // Compute Pressure should be frozen array
-  const sources = PressureObserver.supportedSources;
-  assert_equals(sources, PressureObserver.supportedSources);
+  const sources = PressureObserver.knownSources;
+  assert_equals(sources, PressureObserver.knownSources);
 }, 'PressureObserver must return always the same array');
 
 test(() => {
   // Compute Pressure should be frozen array
-  let sources = PressureObserver.supportedSources;
-  assert_equals(Object.isFrozen(), true);
+  let sources = PressureObserver.knownSources;
+  assert_equals(Object.isFrozen(sources), true);
 }, 'PressureObserver must return a frozen array');
