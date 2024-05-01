c532da24a63f137b3197f104011db2d5fe042d98
Srinivas Sista <srinivassista@chromium.org>
2024-05-01 23:59:21 +0000

    Revert "Moving zoom from WebViewImpl to WebFrameWidgetImpl"
    This reverts commit 3d6aca656f25ee76fa641edd9882aa87bdcb1e7e.
    
    Reason for revert: Top crashon canary 
    
    Original change's description:
    > Moving zoom from WebViewImpl to WebFrameWidgetImpl
    >
    > As we allow CSS zoom to affect iframes, zoom can no longer be a property
    > of a web view, as a web view can contain several frames, each with a
    > different zoom. This change removes the zoom_level_ variable and
    > the functions associated with it from WebViewImpl,and moves it to the
    > frame level.
    >
    > Bug: 329482480
    >
    > Change-Id: I1136e3a40f0c07587b584055ac4348fc9c9b2b2d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5402103
    > Reviewed-by: Stefan Zager <szager@chromium.org>
    > Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    > Auto-Submit: Yotam Hacohen <yotha@chromium.org>
    > Commit-Queue: Yotam Hacohen <yotha@chromium.org>
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1293975}
    
    Bug: 329482480
    Change-Id: I06e0d5c59c0ba8e9c6fc2eba63d6f879a5fdd3d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506825
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Auto-Submit: Srinivas Sista <srinivassista@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295189}

--- third_party/blink/renderer/core/frame/web_remote_frame_impl.h
+++ third_party/blink/renderer/core/frame/web_remote_frame_impl.h
@@ -24,7 +24,7 @@ struct FrameVisualProperties;
 class RemoteFrame;
 class RemoteFrameClientImpl;
 enum class WebFrameLoadType;
-class WebFrameWidgetImpl;
+class WebFrameWidget;
 class WebView;
 class WindowAgentFactory;
 
@@ -127,7 +127,7 @@ class CORE_EXPORT WebRemoteFrameImpl final
   friend class RemoteFrameClientImpl;
 
   void SetCoreFrame(RemoteFrame*);
-  void InitializeFrameVisualProperties(WebFrameWidgetImpl* ancestor_widget,
+  void InitializeFrameVisualProperties(WebFrameWidget* ancestor_widget,
                                        WebView* web_view);
 
   // Inherited from WebFrame, but intentionally hidden: it never makes sense
