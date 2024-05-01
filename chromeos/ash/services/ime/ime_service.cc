28650aae8ebfde815350e1ffcdb661ce9de25c3a
Bao-Duy Tran <tranbaoduy@chromium.org>
2024-05-01 06:56:36 +0000

    Disable & wire kImeDownloaderExperiment flag to IME shared lib.
    This Cr feature flag was added @ https://crrev.com/c/5028574,
    ON-by-default and intended to just serve as a killswitch.
    
    It was then referenced in CrOS 1P IME shared lib (internal
    repo) @ http://cl/588960858, http://cl/589694093, and
    http://cl/590354281, but in fact the wiring between the two
    sides has always been missing.
    
    Thus the flag has been seen as "always OFF" by the IME
    shared lib (where the feature impl resides). The feature
    (intended to be ON) has always been OFF by accident. This
    is somewhat confusing and misleading.
    
    This CL fixes the situation by adding the missing
    wiring, and also turns the feature to OFF-by-default to
    retain the (albeit unintended) status quo. It can be
    flipped to ON-by-default separately later when appropriate.
    
    Bug: b:310745255
    Change-Id: Ic45702a3d41938e3a2cd64aaea5966faddcdff93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5500466
    Commit-Queue: Bao-Duy Tran <tranbaoduy@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294798}

--- chromeos/ash/services/ime/ime_service.cc
+++ chromeos/ash/services/ime/ime_service.cc
@@ -164,6 +164,7 @@ bool ImeService::IsFeatureEnabled(const char* feature_name) {
       &features::kFirstPartyVietnameseInput,
       &ash::standalone_browser::features::kLacrosOnly,
       &features::kSystemJapanesePhysicalTyping,
+      &features::kImeDownloaderExperiment,
       &features::kImeDownloaderUpdate,
       &features::kImeKoreanOnlyModeSwitchOnRightAlt,
       &features::kImeUsEnglishExperimentalModel,
