8e7abe12e7fe4dc2f40fa2a1c9d7df2290e868a1
Fritz Koenig <frkoenig@chromium.org>
2024-05-01 23:21:27 +0000

    media/gpu/v4l2/test: Enable I010 -> PNG conversion
    Because the frames are already in I010 for MD5 comparison
    reasons, only a I010 -> RGB conversion is necessary.
    
    Bug: b:337927172
    Change-Id: I65bfc372ca7fad3b7122dbc551f0f3fd0768afd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502120
    Reviewed-by: Steve Cho <stevecho@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295167}

--- media/gpu/v4l2/test/av1_decoder.h
+++ media/gpu/v4l2/test/av1_decoder.h
@@ -46,7 +46,8 @@ class Av1Decoder : public VideoDecoder {
                                        std::vector<uint8_t>& y_plane,
                                        std::vector<uint8_t>& u_plane,
                                        std::vector<uint8_t>& v_plane,
-                                       gfx::Size& size) override;
+                                       gfx::Size& size,
+                                       BitDepth& bit_depth) override;
 
  private:
   enum class ParsingResult {
