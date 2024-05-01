ef97c231d7371d13aa18d967388cdbfa0da965bd
Sebsg <sebsg@chromium.org>
2024-05-01 03:06:21 +0000

    [CPE] Improve OnLoginsChanged to trigger file re-writes less often.
    Add a check in UPDATE case to only trigger a rewrite on password change.
    Password usage used to trigger this (due to use count and use date being
    modified).
    
    Also only trigger InjectAffiliationAndBrandingInformation if there is a
    new credential added, since this code path triggers a file re-write.
    
    Bug: 337272549
    
    Change-Id: If8f99b39d74d9f0d3d51c84992afc999f84e404a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5467436
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294748}

--- ios/chrome/browser/credential_provider/model/credential_provider_util.h
+++ ios/chrome/browser/credential_provider/model/credential_provider_util.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class ChromeBrowserState;
 class FaviconLoader;
 
+extern const char kSyncStoreHistogramName[];
+
 // Returns the equivalent of a unique record identifier. Built from the unique
 // columns in the logins database.
 NSString* RecordIdentifierForPasswordForm(
--- ios/chrome/browser/credential_provider/model/credential_provider_util.mm
+++ ios/chrome/browser/credential_provider/model/credential_provider_util.mm
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::SysUTF16ToNSString;
 using base::UTF8ToUTF16;
 
+extern const char kSyncStoreHistogramName[] =
+    "IOS.CredentialExtension.SyncedStore";
+
 namespace {
 
 // Max number of stored favicons.
