bd36487f3a694e29742402fba6f3b8f33e9f4c2b
Tomasz Wiszkowski <ender@google.com>
2024-05-01 22:08:01 +0000

    Control the list of Search in CCT allowed packages with Finch.
    Please see https://crrev.com/i/7246267 for more context.
    
    Change-Id: Ifce9ecd5e485e32b0dd6286b796921d74140cc33
    Bug: 338255681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506854
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295121}

--- chrome/browser/android/browserservices/intents/java/src/org/chromium/chrome/browser/browserservices/intents/BrowserServicesIntentDataProvider.java
+++ chrome/browser/android/browserservices/intents/java/src/org/chromium/chrome/browser/browserservices/intents/BrowserServicesIntentDataProvider.java
@@ -601,4 +601,9 @@ public abstract class BrowserServicesIntentDataProvider {
     public int getSideSheetPosition() {
         return ACTIVITY_SIDE_SHEET_POSITION_END;
     }
+
+    /** Return whether calling package should be allowed to present an interactive Omnibox. */
+    public boolean isInteractiveOmniboxAllowed() {
+        return false;
+    }
 }
