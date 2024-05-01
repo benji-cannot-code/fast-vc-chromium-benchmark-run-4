fd90ac3418c6767aa2fc66d73cd6060d17072463
Michele Fan <michelefan@chromium.org>
2024-05-01 21:36:38 +0000

    snap-group: Fix cursor update issue on divider handler view
    Demo: http://b/335300903#comment2
    Fixed: b/335300903
    Test: Added new unit test + manually
    Change-Id: I8b16b27a380103ede9cc5071dcf2bad96aa06762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503953
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295098}

--- ash/wm/splitview/split_view_divider_view.h
+++ ash/wm/splitview/split_view_divider_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class DividerHandlerView;
 class IconButton;
 class SplitViewDivider;
 
@@ -93,7 +94,7 @@ class SplitViewDividerView : public views::View,
 
   raw_ptr<SplitViewDivider> divider_;
 
-  raw_ptr<views::View> handler_view_ = nullptr;
+  raw_ptr<DividerHandlerView> handler_view_ = nullptr;
   raw_ptr<IconButton> feedback_button_ = nullptr;
 
   base::WeakPtrFactory<SplitViewDividerView> weak_ptr_factory_{this};
