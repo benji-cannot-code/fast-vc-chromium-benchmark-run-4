a0c9706dbd7330d65c52b8ee970170b2a995d017
Austin Sullivan <asully@chromium.org>
2024-05-01 21:05:14 +0000

    webnn: Plumb through numThreads to the //services TFLite backend
    This is a temporary solution to close the perf gap between the TFLite
    backend and the XNNPACK backend, which will allow us to delete the
    XNNPACK backend
    
    Long-term discussions of how to specify this behavior are happening on
    https://github.com/webmachinelearning/webnn/issues/436
    
    Bug: 338162119
    Change-Id: I42199744f4a8f3e685cc550dcd013183be65aeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506116
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295088}

--- services/webnn/webnn_context_provider_impl_unittest.cc
+++ services/webnn/webnn_context_provider_impl_unittest.cc
@@ -91,7 +91,8 @@ TEST_F(WebNNContextProviderImplTest, CPUNotSupported) {
   provider_remote->CreateWebNNContext(
       mojom::CreateContextOptions::New(
           mojom::CreateContextOptions::Device::kCpu,
-          mojom::CreateContextOptions::PowerPreference::kDefault),
+          mojom::CreateContextOptions::PowerPreference::kDefault,
+          /*thread_count_hint=*/0),
       future.GetCallback());
   mojom::CreateContextResultPtr result = future.Take();
   ASSERT_TRUE(result->is_error());
@@ -112,7 +113,8 @@ TEST_F(WebNNContextProviderImplTest, GPUNotSupported) {
   provider_remote->CreateWebNNContext(
       mojom::CreateContextOptions::New(
           mojom::CreateContextOptions::Device::kGpu,
-          mojom::CreateContextOptions::PowerPreference::kDefault),
+          mojom::CreateContextOptions::PowerPreference::kDefault,
+          /*thread_count_hint=*/0),
       future.GetCallback());
   mojom::CreateContextResultPtr result = future.Take();
   ASSERT_TRUE(result->is_error());
--- services/webnn/webnn_graph_impl_backend_test.cc
+++ services/webnn/webnn_graph_impl_backend_test.cc
@@ -80,7 +80,8 @@ void BuildAndCompute(
   base::test::TestFuture<mojom::CreateContextResultPtr> create_context_future;
   webnn_provider_remote->CreateWebNNContext(
       mojom::CreateContextOptions::New(
-          device, mojom::CreateContextOptions::PowerPreference::kDefault),
+          device, mojom::CreateContextOptions::PowerPreference::kDefault,
+          /*thread_count_hint=*/0),
       create_context_future.GetCallback());
   mojom::CreateContextResultPtr create_context_result =
       create_context_future.Take();
@@ -6169,7 +6170,8 @@ TEST_F(WebNNGraphImplBackendTest, BuildOneGraphToComputeMultipleTimes) {
   webnn_provider_remote->CreateWebNNContext(
       mojom::CreateContextOptions::New(
           mojom::CreateContextOptions::Device::kGpu,
-          mojom::CreateContextOptions::PowerPreference::kDefault),
+          mojom::CreateContextOptions::PowerPreference::kDefault,
+          /*thread_count_hint=*/0),
       create_context_future.GetCallback());
   mojom::CreateContextResultPtr create_context_result =
       create_context_future.Take();
