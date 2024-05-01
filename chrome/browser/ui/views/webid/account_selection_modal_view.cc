a39b1a3b61a5de56146daaa24ae9971f91916da9
Md Hasibul Hasan <hasibulhasan873@gmail.com>
2024-05-01 19:17:13 +0000

    [FedCM] Refactor AccountSelectionViewBase Methods
    Refactoring some AccountSelectionViewBase methods into a util file.
    Refactoring was suggested in the following CL.
    https://chromium-review.googlesource.com/c/chromium/src/+/5440117/comment/9b9a4114_828f43a7/
    
    Bug: 333581015
    Change-Id: Ic53fb41dbd82c36d70d06c087fd8e8fd35725b1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5455231
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295000}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/controls/hover_button.h"
 #include "chrome/browser/ui/views/webid/account_selection_view_base.h"
 #include "chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.h"
+#include "chrome/browser/ui/views/webid/webid_utils.h"
 #include "chrome/grit/browser_resources.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
@@ -85,8 +86,9 @@ AccountSelectionModalView::AccountSelectionModalView(
       kBetweenChildSpacing));
   SetButtons(ui::DIALOG_BUTTON_NONE);
 
-  title_ = GetTitle(top_frame_for_display, /*iframe_for_display=*/std::nullopt,
-                    idp_title, rp_context);
+  title_ = webid::GetTitle(top_frame_for_display,
+                           /*iframe_for_display=*/std::nullopt, idp_title,
+                           rp_context);
 }
 
 AccountSelectionModalView::~AccountSelectionModalView() = default;
