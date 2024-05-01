951af495cd17f7e447bf7384ffae3670fe360b51
Sam Maier <smaier@chromium.org>
2024-05-01 19:38:59 +0000

    jni_zero: fix a new java/native mismatch breaking multiplexing
    Bug: 40612925
    Change-Id: I1dcf6afece9a3092ec204f38c85b932c353b3165
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506193
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295020}

--- chrome/test/payments/android/payment_request_test_bridge.cc
+++ chrome/test/payments/android/payment_request_test_bridge.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/no_destructor.h"
-#include "chrome/test/test_support_jni_headers/PaymentRequestTestBridge_jni.h"
+#include "chrome/test/payment_test_support_jni_headers/PaymentRequestTestBridge_jni.h"
 #include "content/public/browser/web_contents.h"
 
 namespace payments {
