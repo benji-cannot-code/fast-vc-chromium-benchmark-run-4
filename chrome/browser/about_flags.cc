b21a6abf87e4205845ecf90009b371083b307649
Akihiro Ota <akihiroota@chromium.org>
2024-05-01 22:01:03 +0000

    Accessibility: Add a feature flag for the STS keyboard shortcut
    Bug: b/286006143
    Change-Id: I89374489cd231a5e7a19152cc5445aa247269ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506534
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295117}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -7842,6 +7842,11 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kAccessibilityMouseKeysDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(features::kAccessibilityMouseKeys)},
 
+    {"enable-accessibility-select-to-speak-shortcut",
+     flag_descriptions::kAccessibilitySelectToSpeakShortcutName,
+     flag_descriptions::kAccessibilitySelectToSpeakShortcutDescription, kOsCrOS,
+     FEATURE_VALUE_TYPE(features::kAccessibilitySelectToSpeakShortcut)},
+
     {"enable-pip-double-tap-to-resize",
      flag_descriptions::kPipDoubleTapToResizeName,
      flag_descriptions::kPipDoubleTapToResizeDescription, kOsCrOS,
