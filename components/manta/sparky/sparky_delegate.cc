a93308474a395ce17b125be888bd0b8f192b09e9
Lauren Commeignes <laurencom@chromium.org>
2024-05-01 01:55:16 +0000

    Add in experimental sparky delegate.
    Bug: 336703051
    Change-Id: I0465c7738f5fc18be8a3ef6bd808bb5037790db4
    Test: Tests will be created once the Sparky Provider is created.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5500158
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294721}

---
+++ components/manta/sparky/sparky_delegate.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/manta/sparky/sparky_delegate.h"
+
+#include <memory>
+#include <optional>
+
+#include "base/values.h"
+
+namespace manta {
+
+SettingsData::SettingsData(const std::string& pref_name,
+                           const PrefType& pref_type,
+                           std::optional<base::Value> value)
+    : pref_name(pref_name), value(std::move(value)) {}
+
+SettingsData::~SettingsData() = default;
+
+void SettingsData::UpdateValue(std::optional<base::Value> new_value) {
+  value = std::move(new_value);
+}
+
+SparkyDelegate::SparkyDelegate() = default;
+SparkyDelegate::~SparkyDelegate() = default;
+
+}  // namespace manta
