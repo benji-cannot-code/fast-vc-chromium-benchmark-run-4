971348a1399834f6a26b90aa9e1d0042672d2425
Srinivas Sista <srinivassista@chromium.org>
2024-05-01 22:14:31 +0000

    Revert "[A11y] Move <option> bounds caching to AXObjectCache"
    This reverts commit 75ab0703e945b6c46b6c57d35112db2690255e40.
    
    Reason for revert: Top crasher bug on canary 
    
    Original change's description:
    > [A11y] Move <option> bounds caching to AXObjectCache
    >
    > Also add stricter tests and assertions.
    >
    > Moving to the AXObjectCache helps prepare for CL:5467030 which removes,
    > AXMenuList* classes.
    >
    > Bug: none
    > Change-Id: Ifd6e06654ef00be4b54698e76e5593ad07667695
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5497817
    > Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    > Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    > Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    > Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1294532}
    
    Bug: none
    Change-Id: Ice308a46c214f8ac37372ed76842de37dd06ded1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506576
    Reviewed-by: Krishna Govind <govind@chromium.org>
    Owners-Override: Srinivas Sista <srinivassista@chromium.org>
    Commit-Queue: Srinivas Sista <srinivassista@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295127}

--- third_party/blink/renderer/modules/accessibility/ax_menu_list.h
+++ third_party/blink/renderer/modules/accessibility/ax_menu_list.h
@@ -57,6 +57,14 @@ class AXMenuList final : public AXNodeObject {
 
   AXObject* GetOrCreateMockPopupChild();
 
+  const WTF::Vector<gfx::Rect>& GetOptionsBounds() const {
+    return options_bounds_;
+  }
+
+  void SetOptionsBounds(const WTF::Vector<gfx::Rect>& options_bounds) {
+    options_bounds_ = options_bounds;
+  }
+
  private:
   friend class AXMenuListOption;
 
@@ -66,6 +74,8 @@ class AXMenuList final : public AXNodeObject {
   void AddChildren() override;
 
   bool IsCollapsed() const;
+
+  WTF::Vector<gfx::Rect> options_bounds_;
 
   Member<AXObject> popup_;
 };
