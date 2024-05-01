156650a4fa085b26ba1058e84d272f98d80aacce
Avi Drissman <avi@chromium.org>
2024-05-01 17:24:00 +0000

    Don't use SCK API in situations where it's known to not work
    The new SCK API is known to not work if there is more than one copy of
    Chromium running at a time.
    
    Bug: 335449467
    Bug: 333443445
    Change-Id: I33a187c7a53a6b1d7432b9a2611757c7f08fd244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503231
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294935}

---
+++ ui/snapshot/snapshot_mac.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_SNAPSHOT_SNAPSHOT_MAC_H_
+#define UI_SNAPSHOT_SNAPSHOT_MAC_H_
+
+#include "ui/snapshot/snapshot_export.h"
+
+namespace ui {
+
+enum class SnapshotAPI {
+  kUnspecified,
+  kOldAPI,  // CGWindowListCreateImage()
+  kNewAPI   // ScreenCaptureKit (requires macOS 14.4 or later)
+};
+
+// Forces the usage of a specific snapshotting API for testing purposes. Do not
+// force `kNewAPI` on a macOS release before 14.4. Use `kUnspecified` to reset a
+// forced value.
+SNAPSHOT_EXPORT void ForceAPIUsageForTesting(SnapshotAPI api);
+
+}  // namespace ui
+
+#endif  // UI_SNAPSHOT_SNAPSHOT_MAC_H_
