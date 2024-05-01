8b103d5df65f9bff584634e604f72a4b5698a717
Mike Dougherty <michaeldo@chromium.org>
2024-05-01 12:52:19 +0000

    Move ValueResultFromWKResult to ios/web/public
    This function can be outside of ios/web to convert JS responses into
    base::Value types.
    
    
    Bug: None
    Change-Id: I2c30c1beed7b90b15e88e4753f9c694f28471c79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502912
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294835}

--- ios/web/js_messaging/web_frame_impl.mm
+++ ios/web/js_messaging/web_frame_impl.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/web/js_messaging/java_script_content_world.h"
 #import "ios/web/js_messaging/java_script_feature_manager.h"
 #import "ios/web/js_messaging/web_view_js_utils.h"
+#import "ios/web/public/js_messaging/web_view_js_utils.h"
 #import "ios/web/public/thread/web_task_traits.h"
 #import "ios/web/public/thread/web_thread.h"
 #import "url/gurl.h"
--- ios/web/js_messaging/web_view_js_utils.h
+++ ios/web/js_messaging/web_view_js_utils.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_WEB_JS_MESSAGING_WEB_VIEW_JS_UTILS_H_
 
 #import <Foundation/Foundation.h>
-#include <memory>
 
 @class WKContentWorld;
 @class WKFrameInfo;
@@ -16,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 class Value;
 }  // namespace base
+
+#import "ios/web/public/js_messaging/web_view_js_utils.h"
 
 namespace web {
 
@@ -32,9 +33,6 @@ enum JSEvaluationErrorCode {
   // JS evaluation is not allowed for security reasons.
   JS_EVALUATION_ERROR_CODE_REJECTED = -1001,
 };
-
-// Converts result of WKWebView script evaluation to base::Value.
-std::unique_ptr<base::Value> ValueResultFromWKResult(id result);
 
 // Converts base::Value to an equivalent Foundation object.
 id NSObjectFromValueResult(const base::Value* value_result);
