aa4940356cb24ad8ed435053efbdf234e97db0f9
rbpotter <rbpotter@chromium.org>
2024-05-01 21:01:39 +0000

    Power Bookmarks: Fire iron-resize when height of child items changes
    Bug: 337043247
    Change-Id: I993cb3dde9c00a62b27cc49f7a10e1bdc33f9efb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499234
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295083}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.ts
@@ -99,6 +99,11 @@ export class PowerBookmarkRowElement extends PolymerElement {
         type: String,
         value: '',
       },
+      listItemSize_: {
+        type: String,
+        computed: 'computeListItemSize_(compact)',
+        observer: 'onListItemSizeChanged_',
+      },
     };
   }
 
@@ -118,6 +123,8 @@ export class PowerBookmarkRowElement extends PolymerElement {
   trailingIconAriaLabel: string;
   trailingIconTooltip: string;
   imageUrls: string[];
+
+  private listItemSize_: CrUrlListItemSize;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -159,8 +166,16 @@ export class PowerBookmarkRowElement extends PolymerElement {
     }
   }
 
-  private getItemSize_() {
+  private computeListItemSize_(): CrUrlListItemSize {
     return this.compact ? CrUrlListItemSize.COMPACT : CrUrlListItemSize.LARGE;
+  }
+
+  private async onListItemSizeChanged_() {
+    await this.$.crUrlListItem.updateComplete;
+    if (this.parentNode &&
+        (this.parentNode as HTMLElement).tagName === 'IRON-LIST') {
+      this.parentNode.dispatchEvent(new CustomEvent('iron-resize'));
+    }
   }
 
   private isBookmarksBar_(): boolean {
