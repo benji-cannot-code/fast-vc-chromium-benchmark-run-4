cb084fb1415a46ad913e3786df7e820f2f187c31
Joe Mason <joenotcharles@google.com>
2024-05-01 01:35:23 +0000

    [PM] Use WorkerNode::GetOrigin() in WebMemoryAggregator
    WorkerNode now tracks the origin of workers without needing their URL.
    This closes the last TODO in WebMemoryAggregator that needed
    PlzDedicatedWorker.
    
    Bug: 40165281
    Change-Id: Id010b403a1f69ebec2282064bb4bd73f0fe82bc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473567
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1294707}

--- components/performance_manager/v8_memory/v8_memory_test_helpers.h
+++ components/performance_manager/v8_memory/v8_memory_test_helpers.h
@@ -32,6 +32,10 @@ namespace content {
 class RenderFrameHost;
 }
 
+namespace url {
+class Origin;
+}
+
 namespace performance_manager {
 
 class FrameNodeImpl;
@@ -307,7 +311,7 @@ class WebMemoryTestHarness : public GraphTestHarness {
   }
 
   WorkerNodeImpl* AddWorkerNode(WorkerNode::WorkerType worker_type,
-                                std::string url,
+                                std::string script_url,
                                 Bytes bytes,
                                 FrameNodeImpl* parent);
 
@@ -315,7 +319,7 @@ class WebMemoryTestHarness : public GraphTestHarness {
                                            FrameNodeImpl* parent);
 
   WorkerNodeImpl* AddWorkerNode(WorkerNode::WorkerType worker_type,
-                                std::string url,
+                                std::string script_url,
                                 Bytes bytes,
                                 WorkerNodeImpl* parent);
 
@@ -337,8 +341,9 @@ class WebMemoryTestHarness : public GraphTestHarness {
                                   std::optional<std::string> src_attribute,
                                   Bytes canvas_bytes = std::nullopt);
   WorkerNodeImpl* AddWorkerNodeImpl(WorkerNode::WorkerType worker_type,
-                                    std::string url,
-                                    Bytes bytes);
+                                    const url::Origin& origin,
+                                    std::string script_url = std::string(),
+                                    Bytes bytes = std::nullopt);
   int GetNextUniqueId();
   TestNodeWrapper<ProcessNodeImpl> process_;
   TestNodeWrapper<ProcessNodeImpl> other_process_;
