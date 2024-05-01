eaa553e5836f82653d1b7edc5a07c4690c21f238
siashah <siashah@chromium.org>
2024-05-01 20:50:32 +0000

    Add skeleton for facilitated payments metrics
    This CL only contains the metrics definition and helper methods to log
    the metrics for IsAvailable and GetClientToken. This is to parellelize
    the metrics task while the other parts of the pay flow are being worked
    upon.
    
    Metrics doc: go/pix-in-chrome-metrics-dd
    
    Follow-up changes:
    * Add more metrics and helper methods.
    * Call helper methods from corresponding code paths.
    
    Bug: b/335853873
    Change-Id: Ifa4b34485246cf70e2ae3f82e19c89405093351c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491775
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295077}

---
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_FACILITATED_PAYMENTS_CORE_METRICS_FACILITATED_PAYMENTS_METRICS_H_
+#define COMPONENTS_FACILITATED_PAYMENTS_CORE_METRICS_FACILITATED_PAYMENTS_METRICS_H_
+
+namespace base {
+class TimeDelta;
+}
+
+namespace payments::facilitated {
+
+// Log the result of whether the facilitated payments is available or not.
+void LogIsApiAvailableResult(bool result, base::TimeDelta duration);
+
+// Log the result of the GetClientToken call made to api client.
+void LogGetClientTokenResult(bool result, base::TimeDelta duration);
+
+}  // namespace payments::facilitated
+
+#endif  // COMPONENTS_FACILITATED_PAYMENTS_CORE_METRICS_FACILITATED_PAYMENTS_METRICS_H_
