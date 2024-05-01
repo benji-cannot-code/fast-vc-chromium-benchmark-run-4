9a491036a7b8a50db47f1137598464fb5aeeaf2f
Olivier Robin <olivierrobin@google.com>
2024-05-01 21:03:50 +0000

    [iOS] Stop updating consumer during download while in background
    When Chrome is in background, UI update may lead to watchdog kill.
    Suspend UI update when Chrome is in background.
    
    Bug: 338192475
    Change-Id: I5597cf8509a56c95a27b965962e1f0298805707c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505635
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295086}

--- ios/chrome/browser/ui/download/download_manager_coordinator.mm
+++ ios/chrome/browser/ui/download/download_manager_coordinator.mm
@@ -152,6 +152,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _mediator.SetDriveService(nullptr);
   _mediator.SetPrefService(nullptr);
   _mediator.SetIdentityManager(nullptr);
+  _mediator.Disconnect();
 
   if (_viewController) {
     [self.presenter dismissAnimated:self.animatesPresentation];
