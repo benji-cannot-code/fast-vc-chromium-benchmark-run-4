90dfc0b93ebb4304532d89806f2a9ed944bf3b39
Ian Wells <iwells@chromium.org>
2024-05-01 16:58:56 +0000

    Revert "Split app_service/webapk into its own BUILD.gn file."
    This reverts commit 306478860b1f3f23e10b6c4ad54436863bbce0c4.
    
    Reason for revert: https://g-issues.chromium.org/issues/338220073
    
    Original change's description:
    > Split app_service/webapk into its own BUILD.gn file.
    >
    > This creates a new BUILD.gn file for app_service/webapk which is
    > unidirectionally depended on by the App Service. As part of this,
    > webapk_manager.{h,cc} is moved to the app_service directory as it
    > depends on App Service, avoiding a dependency cycle.
    >
    > Bug: b/336172688
    > Change-Id: I5f16ab2bcbf1c9ab91465fab53e13c20c5b6e228
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5495189
    > Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    > Commit-Queue: Dominick Ng <dominickn@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1294740}
    
    Bug: b/336172688
    Change-Id: Iece71b427bd1c57d086c12207ba26b8d75a5c29a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5504734
    Reviewed-by: Haiyang Pan <hypan@google.com>
    Auto-Submit: Ian Wells <iwells@chromium.org>
    Commit-Queue: Haiyang Pan <hypan@google.com>
    Owners-Override: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1294922}

--- chrome/browser/apps/app_service/publishers/arc_apps.cc
+++ chrome/browser/apps/app_service/publishers/arc_apps.cc
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/promise_apps/promise_app.h"
 #include "chrome/browser/apps/app_service/promise_apps/promise_app_registry_cache.h"
 #include "chrome/browser/apps/app_service/publishers/arc_apps_factory.h"
-#include "chrome/browser/apps/app_service/webapk_manager.h"
+#include "chrome/browser/apps/app_service/webapk/webapk_manager.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_icon.h"
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ash/apps/apk_web_app_service.h"
