f2764479d2745c18b4ff541d7f5f181bb7a8fa38
Mohamed Heikal <mheikal@chromium.org>
2024-05-02 22:06:51 +0000

    Add getName to ConditionalState
    ConditionChecker should output the name of the state whose conditions we
    are trying to fulfil in its debugging/error messages. Codify a getName
    abstract function for ConditionalState that then passes it on to
    ConditionChecker.
    
    Change-Id: I4e0b3ab726981d76b8f8bfaaaf6213cd088ee659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512703
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295784}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ConditionalState.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ConditionalState.java
@@ -119,6 +119,11 @@ public abstract class ConditionalState {
     protected void onStopMonitoringTransitionFrom() {}
 
     /**
+     * @return the name of the State for use in debugging/error messages.
+     */
+    public abstract String getName();
+
+    /**
      * @return the lifecycle {@link Phase} this ConditionalState is in.
      */
     public @Phase int getPhase() {
@@ -148,7 +153,7 @@ public abstract class ConditionalState {
             }
         }
 
-        ConditionChecker.check(enterConditions);
+        ConditionChecker.check(getName(), enterConditions);
     }
 
     /**
--- base/test/android/javatests/src/org/chromium/base/test/transit/Facility.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Facility.java
@@ -31,6 +31,7 @@ public abstract class Facility<HostStationT extends Station> extends Conditional
     protected final HostStationT mHostStation;
     private final int mId;
     private static int sLastFacilityId = 1000;
+    private String mName;
 
     /**
      * Constructor.
@@ -49,12 +50,19 @@ public abstract class Facility<HostStationT extends Station> extends Conditional
         mId = ++sLastFacilityId;
         mHostStation = hostStation;
         mHostStation.registerFacility(this);
+        mName =
+                String.format(
+                        "<S%d|F%d: %s>", mHostStation.getId(), mId, getClass().getSimpleName());
+    }
+
+    @Override
+    public String getName() {
+        return mName;
     }
 
     @Override
     public String toString() {
-        return String.format(
-                "<S%d|F%d: %s>", mHostStation.getId(), mId, getClass().getSimpleName());
+        return mName;
     }
 
     /**
