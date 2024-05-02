9fdcd88255f591ef0cda8dd11767039d10099523
Sky Malice <skym@chromium.org>
2024-05-02 23:24:15 +0000

    Move account lookup from action confirm dialog to manager.
    Bug: 338448328
    Change-Id: I0b3a53c5a8925a6bbad45bf4c1c1aca441a9817c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512651
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295840}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ActionConfirmationDialogUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ActionConfirmationDialogUnitTest.java
@@ -8,13 +8,15 @@ package org.chromium.chrome.browser.tasks.tab_management;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import androidx.annotation.StringRes;
+import androidx.core.util.Function;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.Before;
@@ -29,11 +31,7 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.signin.services.IdentityServicesProvider;
 import org.chromium.chrome.browser.tasks.tab_management.ActionConfirmationDialog.ConfirmationDialogResult;
-import org.chromium.components.signin.base.CoreAccountInfo;
-import org.chromium.components.signin.identitymanager.ConsentLevel;
-import org.chromium.components.signin.identitymanager.IdentityManager;
 import org.chromium.ui.base.TestActivity;
 import org.chromium.ui.modaldialog.DialogDismissalCause;
 import org.chromium.ui.modaldialog.ModalDialogManager;
@@ -45,8 +43,6 @@ import org.chromium.ui.modelutil.PropertyModel;
 /** Unit tests for {@link ActionConfirmationDialog}. */
 @RunWith(BaseRobolectricTestRunner.class)
 public class ActionConfirmationDialogUnitTest {
-    private static final String TEST_EMAIL = "test@gmail.com";
-
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
     @Rule
@@ -56,9 +52,6 @@ public class ActionConfirmationDialogUnitTest {
     @Mock private Profile mProfile;
     @Mock private ModalDialogManager mModalDialogManager;
     @Mock private ConfirmationDialogResult mConfirmationDialogResult;
-    @Mock private IdentityServicesProvider mIdentityServicesProvider;
-    @Mock private IdentityManager mIdentityManager;
-    @Mock private CoreAccountInfo mCoreAccountInfo;
 
     @Captor private ArgumentCaptor<PropertyModel> mPropertyModelArgumentCaptor;
 
@@ -67,16 +60,19 @@ public class ActionConfirmationDialogUnitTest {
     @Before
     public void setUp() {
         mActivityScenarioRule.getScenario().onActivity(this::onActivity);
-        IdentityServicesProvider.setInstanceForTests(mIdentityServicesProvider);
-        when(mIdentityServicesProvider.getIdentityManager(mProfile)).thenReturn(mIdentityManager);
-        when(mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SIGNIN))
-                .thenReturn(mCoreAccountInfo);
-        when(mCoreAccountInfo.getEmail()).thenReturn(TEST_EMAIL);
     }
 
     private void onActivity(TestActivity activity) {
         mActivity = activity;
         mActivity.setTheme(R.style.Theme_BrowserUI_DayNight);
+    }
+
+    private Function<Resources, String> noSyncResolver(@StringRes int stringRes) {
+        return (resources) -> resources.getString(stringRes);
+    }
+
+    private Function<Resources, String> syncResolver(@StringRes int stringRes, String account) {
+        return (resources) -> resources.getString(stringRes, account);
     }
 
     @Test
@@ -85,7 +81,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_no_sync_description,
+                noSyncResolver(R.string.delete_tab_group_no_sync_description),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
@@ -108,7 +104,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_description,
+                syncResolver(R.string.delete_tab_group_description, "test@gmail.com"),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
@@ -129,7 +125,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_no_sync_description,
+                noSyncResolver(R.string.delete_tab_group_no_sync_description),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
@@ -149,7 +145,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_no_sync_description,
+                noSyncResolver(R.string.delete_tab_group_no_sync_description),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
@@ -169,7 +165,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_no_sync_description,
+                noSyncResolver(R.string.delete_tab_group_no_sync_description),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
@@ -193,7 +189,7 @@ public class ActionConfirmationDialogUnitTest {
                 new ActionConfirmationDialog(mProfile, mActivity, mModalDialogManager);
         dialog.show(
                 R.string.close_from_group_dialog_title,
-                R.string.delete_tab_group_no_sync_description,
+                noSyncResolver(R.string.delete_tab_group_no_sync_description),
                 R.string.delete_tab_group_action,
                 mConfirmationDialogResult);
 
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ActionConfirmationManagerUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ActionConfirmationManagerUnitTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -17,6 +18,7 @@ import static org.mockito.Mockito.when;
 import android.app.Activity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.TextView;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
@@ -43,6 +45,7 @@ import org.chromium.components.prefs.PrefService;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.identitymanager.ConsentLevel;
 import org.chromium.components.signin.identitymanager.IdentityManager;
+import org.chromium.components.sync.ModelType;
 import org.chromium.components.sync.SyncService;
 import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.components.user_prefs.UserPrefsJni;
@@ -54,10 +57,13 @@ import org.chromium.ui.modaldialog.ModalDialogProperties.Controller;
 import org.chromium.ui.modelutil.PropertyModel;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 /** Unit tests for {@link ActionConfirmationManager}. */
 @RunWith(BaseRobolectricTestRunner.class)
 public class ActionConfirmationManagerUnitTest {
+    private static final String TEST_EMAIL = "test@gmail.com";
+
     private static final int TAB1_ID = 1;
     private static final int TAB2_ID = 2;
     private static final int TAB3_ID = 3;
@@ -246,6 +252,63 @@ public class ActionConfirmationManagerUnitTest {
                 mPropertyModelArgumentCaptor.getValue(),
                 DialogDismissalCause.POSITIVE_BUTTON_CLICKED);
         verify(mOnResult).onResult(ConfirmationResult.CONFIRMATION_POSITIVE);
+    }
+
+    @Test
+    public void testProcessRemoveTabAttempt_NoSignIn() {
+        when(mIdentityManager.getPrimaryAccountInfo(ConsentLevel.SIGNIN)).thenReturn(null);
+
+        ActionConfirmationManager actionConfirmationManager =
+                new ActionConfirmationManager(
+                        mProfile, mActivity, mTabGroupModelFilter, mModalDialogManager);
+        actionConfirmationManager.processRemoveTabAttempt(Arrays.asList(TAB1_ID), mOnResult);
+        verify(mModalDialogManager).showDialog(mPropertyModelArgumentCaptor.capture(), anyInt());
+
+        View customView =
+                mPropertyModelArgumentCaptor.getValue().get(ModalDialogProperties.CUSTOM_VIEW);
+        TextView descriptionTextView = customView.findViewById(R.id.description_text_view);
+        assertEquals(
+                "This will permanently delete the group from your device",
+                descriptionTextView.getText());
+    }
+
+    @Test
+    public void testProcessRemoveTabAttempt_SignInButNoSync() {
+        when(mCoreAccountInfo.getEmail()).thenReturn(TEST_EMAIL);
+        when(mSyncService.getActiveDataTypes()).thenReturn(Collections.emptySet());
+
+        ActionConfirmationManager actionConfirmationManager =
+                new ActionConfirmationManager(
+                        mProfile, mActivity, mTabGroupModelFilter, mModalDialogManager);
+        actionConfirmationManager.processRemoveTabAttempt(Arrays.asList(TAB1_ID), mOnResult);
+        verify(mModalDialogManager).showDialog(mPropertyModelArgumentCaptor.capture(), anyInt());
+
+        View customView =
+                mPropertyModelArgumentCaptor.getValue().get(ModalDialogProperties.CUSTOM_VIEW);
+        TextView descriptionTextView = customView.findViewById(R.id.description_text_view);
+        assertEquals(
+                "This will permanently delete the group from your device",
+                descriptionTextView.getText());
+    }
+
+    @Test
+    public void testProcessRemoveTabAttempt_SignInAndSync() {
+        when(mCoreAccountInfo.getEmail()).thenReturn(TEST_EMAIL);
+        when(mSyncService.getActiveDataTypes())
+                .thenReturn(Collections.singleton(ModelType.SAVED_TAB_GROUP));
+
+        ActionConfirmationManager actionConfirmationManager =
+                new ActionConfirmationManager(
+                        mProfile, mActivity, mTabGroupModelFilter, mModalDialogManager);
+        actionConfirmationManager.processRemoveTabAttempt(Arrays.asList(TAB1_ID), mOnResult);
+        verify(mModalDialogManager).showDialog(mPropertyModelArgumentCaptor.capture(), anyInt());
+
+        View customView =
+                mPropertyModelArgumentCaptor.getValue().get(ModalDialogProperties.CUSTOM_VIEW);
+        TextView descriptionTextView = customView.findViewById(R.id.description_text_view);
+        assertEquals(
+                "This will delete the group from all devices signed into test@gmail.com",
+                descriptionTextView.getText());
     }
 
     @Test
