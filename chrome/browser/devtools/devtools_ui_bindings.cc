1b1630ada768e3b582c051dc56681c93f8b2f9b2
Wolfgang Beyer <wolfi@chromium.org>
2024-05-02 07:20:45 +0000

    [DevTools] Add command line flag for disabling DevTools self-XSS warnings
    This adds the command line flag `--unsafely-disable-devtools-self-xss-warnings`, which when set is passed on to DevTools, which will then
    not show any self-XSS warnings when pasting code.
    
    DevTools CL: https://crrev.com/c/5499700
    
    Bug: chromium:41491762
    Change-Id: I6b46c079b2a46513294a830717f384380d1e0bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499304
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295300}

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -421,6 +421,10 @@ std::string SanitizeFrontendQueryParam(
     return value;
   }
 
+  if (key == "disableSelfXssWarnings" && value == "true") {
+    return value;
+  }
+
   return std::string();
 }
 
