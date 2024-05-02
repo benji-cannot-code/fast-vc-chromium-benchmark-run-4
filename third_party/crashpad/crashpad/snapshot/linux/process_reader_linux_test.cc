ec69dcab2c603b9f96e9b2bfda116535a979f1f6
Morten Stenshorne <mstensho@chromium.org>
2024-05-02 11:56:33 +0000

    Disable flaky ProcessReaderLinux.SelfBasic
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-ubsan-vptr/29796/overview
    
    Bug: 331803336
    Change-Id: Ic5e8a2ef4a4a9f01b4f17b1c326b0493a5308dba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5509514
    Reviewed-by: Nicola Tommasi <tommasin@chromium.org>
    Owners-Override: Morten Stenshorne <mstensho@google.com>
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Nicola Tommasi <tommasin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295387}

--- third_party/crashpad/crashpad/snapshot/linux/process_reader_linux_test.cc
+++ third_party/crashpad/crashpad/snapshot/linux/process_reader_linux_test.cc
@@ -73,7 +73,8 @@ pid_t gettid() {
   return syscall(SYS_gettid);
 }
 
-TEST(ProcessReaderLinux, SelfBasic) {
+// TODO(crbug.com/331803336): Flaky.
+TEST(ProcessReaderLinux, DISABLED_SelfBasic) {
   FakePtraceConnection connection;
   connection.Initialize(getpid());
 
