dcda8ecc63ed2b7be27416836e503b6314847809
danakj <danakj@chromium.org>
2024-05-02 17:21:37 +0000

    Spanify base::Crc32
    Use std::array which is bounds checked instead of a native array.
    
    R=thestig@chromium.org
    
    Bug: 40284755
    Change-Id: I936e8c2d443b31217e4c3bc231bbda6bbacbc38b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508080
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295563}

--- base/metrics/crc32.h
+++ base/metrics/crc32.h
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
+
 #include "base/base_export.h"
 #include "base/containers/span.h"
 
 namespace base {
 
-BASE_EXPORT extern const uint32_t kCrcTable[256];
+BASE_EXPORT extern const std::array<uint32_t, 256> kCrcTable;
 
 // This provides a simple, fast CRC-32 calculation that can be used for checking
 // the integrity of data.  It is not a "secure" calculation!  |sum| can start
