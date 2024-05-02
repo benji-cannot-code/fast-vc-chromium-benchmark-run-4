acb8ebb86a7dfe4dc6e224a835cd29225be23884
Mikhail Khokhlov <khokhlov@google.com>
2024-05-02 21:05:17 +0000

    Remove use_perfetto_client_library build flag in /cc/metrics
    This flag was introduced to provide a smooth transition from legacy
    tracing macros to Perfetto tracing SDK. Now the transition is complete,
    and the flag is unconditionally set to true on all platforms. So we can
    remove the flag and the code that is disabled by it.
    
    This CL was uploaded by git cl split.
    
    R=kylechar@chromium.org
    
    Bug: 42050015, 336718643
    Change-Id: I6ee790f87001e90a269fbda436ee163196a7ecfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505595
    Auto-Submit: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295749}

--- cc/metrics/compositor_frame_reporting_controller_unittest.cc
+++ cc/metrics/compositor_frame_reporting_controller_unittest.cc
@@ -335,9 +335,7 @@ class CompositorFrameReportingControllerTest : public testing::Test {
   viz::FrameTokenGenerator current_token_;
   DroppedFrameCounter dropped_counter_;
   TotalFrameCounter total_frame_counter_;
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   ::base::test::TracingEnvironment tracing_environment_;
-#endif  // BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 };
 
 TEST_F(CompositorFrameReportingControllerTest, ActiveReporterCounts) {
@@ -2157,10 +2155,8 @@ TEST_F(CompositorFrameReportingControllerTest, MainFrameBeforeCommit) {
 // BF3->BMF3->PF2->SF(3+1)->PF(3+1)
 TEST_F(CompositorFrameReportingControllerTest,
        ScrollJankMetricsPresentationOrderAbortedMain) {
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("input");
-#endif
 
   std::unique_ptr<EventMetrics> metrics_1 = CreateScrollUpdateEventMetrics(
       ui::ScrollInputType::kWheel, /*is_inertial=*/false,
@@ -2244,7 +2240,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   reporting_controller_.DidPresentCompositorFrame(*current_token_,
                                                   details_3);  // PF(3+1)
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   absl::Status status = ttp.StopAndParseTrace();
   ASSERT_TRUE(status.ok()) << status.message();
   std::string query =
@@ -2259,7 +2254,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   EXPECT_THAT(result.value(),
               ::testing::ElementsAre(std::vector<std::string>{"cnt"},
                                      std::vector<std::string>{"0"}));
-#endif
 }
 
 // This test verifies a main thread scroll scenario where a frame with
@@ -2277,10 +2271,8 @@ TEST_F(CompositorFrameReportingControllerTest,
        ScrollJankMetricsPresentationOrderDroppedPartialOnMainScroll) {
   base::HistogramTester histogram_tester;
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("input");
-#endif
 
   std::unique_ptr<EventMetrics> metrics_1 = CreateScrollUpdateEventMetrics(
       ui::ScrollInputType::kWheel, /*is_inertial=*/false,
@@ -2350,7 +2342,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   reporting_controller_.DidPresentCompositorFrame(*current_token_,
                                                   details_3);  // PF(3+1)
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   absl::Status status = ttp.StopAndParseTrace();
   ASSERT_TRUE(status.ok()) << status.message();
   std::string query =
@@ -2366,7 +2357,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   EXPECT_THAT(result.value(),
               ::testing::ElementsAre(std::vector<std::string>{"cnt"},
                                      std::vector<std::string>{"0"}));
-#endif
 
   histogram_tester.ExpectTotalCount("Event.ScrollJank.MissedVsyncs.PerFrame",
                                     1);
@@ -2384,10 +2374,8 @@ TEST_F(CompositorFrameReportingControllerTest,
        ScrollJankMetricsPresentationOrderDroppedPartialOnImplScroll) {
   base::HistogramTester histogram_tester;
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("input");
-#endif
 
   std::unique_ptr<EventMetrics> metrics_1 = CreateScrollUpdateEventMetrics(
       ui::ScrollInputType::kWheel, /*is_inertial=*/false,
@@ -2438,7 +2426,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   reporting_controller_.DidPresentCompositorFrame(*current_token_,
                                                   details_2);  // PF(2+1)
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   absl::Status status = ttp.StopAndParseTrace();
   ASSERT_TRUE(status.ok()) << status.message();
   std::string query =
@@ -2453,7 +2440,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   EXPECT_THAT(result.value(),
               ::testing::ElementsAre(std::vector<std::string>{"cnt"},
                                      std::vector<std::string>{"0"}));
-#endif
 
   histogram_tester.ExpectTotalCount("Event.ScrollJank.MissedVsyncs.PerFrame",
                                     1);
@@ -2475,10 +2461,8 @@ TEST_F(CompositorFrameReportingControllerTest,
        ScrollJankMetricsPresentationOrderReceivedDroppedEventsAreNotPassed) {
   base::HistogramTester histogram_tester;
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("input");
-#endif
 
   std::unique_ptr<EventMetrics> metrics_1 = CreateScrollUpdateEventMetrics(
       ui::ScrollInputType::kWheel, /*is_inertial=*/false,
@@ -2576,7 +2560,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   reporting_controller_.DidPresentCompositorFrame(*current_token_,
                                                   details_4);  // PF(4+2)
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
   absl::Status status = ttp.StopAndParseTrace();
   ASSERT_TRUE(status.ok()) << status.message();
   std::string query =
@@ -2589,7 +2572,6 @@ TEST_F(CompositorFrameReportingControllerTest,
   EXPECT_THAT(result.value(),
               ::testing::ElementsAre(std::vector<std::string>{"cnt"},
                                      std::vector<std::string>{"0"}));
-#endif
 
   // Expect reporters R3impl, R2main to report data to scroll jank tracker.
   // R3impl - The events it received from previously dropped frames.
@@ -2598,7 +2580,6 @@ TEST_F(CompositorFrameReportingControllerTest,
                                     2);
 }
 
-#if BUILDFLAG(USE_PERFETTO_CLIENT_LIBRARY)
 TEST_F(CompositorFrameReportingControllerTest, EmitsEventLatencyId) {
   base::test::TestTraceProcessor ttp;
   ttp.StartTrace("input");
@@ -2792,7 +2773,6 @@ TEST_F(CompositorFrameReportingControllerTest, VsyncIntervalArg) {
                                   std::vector<std::string>{"32", "1"}));
 }
 
-#endif
 
 }  // namespace
 }  // namespace cc
