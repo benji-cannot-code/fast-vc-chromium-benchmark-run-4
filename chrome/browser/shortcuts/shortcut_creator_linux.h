7efe4fb918967650e65a94f28a7a00c055591a7c
Daniel Murphy <dmurph@chromium.org>
2024-05-02 22:37:54 +0000

    [Shortcuts] Hook up linux shortcut creator to common API.
    This hooks up the linux shortcut creator implementation to the common
    API that the task will use.
    
    Bug: 331614430,333024272
    Change-Id: Iff1be34ab39e1e9d14184a755a16021b32582144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502995
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295803}

--- chrome/browser/shortcuts/shortcut_creator_linux.h
+++ chrome/browser/shortcuts/shortcut_creator_linux.h
@@ -23,9 +23,6 @@ class Image;
 namespace shortcuts {
 class LinuxXdgWrapper;
 
-// TODO(crbug.com/333024272): Hook up CreateShortcutOnUserDesktop to
-// CreateShortcutOnLinuxDesktop on linux.
-
 // Creates a shortcut on the user's desktop and installs it into the system's
 // desktop menu. The icon is written into the user's profile directory. This
 // method makes blocking calls and must be called on an appropriate task runner.
@@ -43,7 +40,7 @@ class LinuxXdgWrapper;
 ShortcutCreatorResult CreateShortcutOnLinuxDesktop(
     const std::string& shortcut_name,
     const GURL& shortcut_url,
-    gfx::Image icon,
+    const gfx::Image& icon,
     const base::FilePath& profile_path,
     LinuxXdgWrapper& xdg_wrapper);
 
