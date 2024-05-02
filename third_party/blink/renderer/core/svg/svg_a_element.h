4634240f62df1f467bf8a5f1a109df96c29477c4
Luke Warlow <lwarlow@igalia.com>
2024-05-02 22:58:47 +0000

    Implement interesttarget support for SVG `<a>` element
    Adds interesttarget and interestaction attributes to SVG namespace.
    Overrides necessary functions inside of svg_a_element.
    
    See: https://github.com/openui/open-ui/issues/1032
    Bug: 326681249
    Change-Id: I2b50d5db474e767b3135dc4fa6ce69f69d26420f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5432193
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Luke <lwarlow@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1295825}

--- third_party/blink/renderer/core/svg/svg_a_element.h
+++ third_party/blink/renderer/core/svg/svg_a_element.h
@@ -38,6 +38,9 @@ class CORE_EXPORT SVGAElement final : public SVGGraphicsElement,
 
   explicit SVGAElement(Document&);
 
+  Element* interestTargetElement() override;
+  AtomicString interestAction() const override;
+
   void Trace(Visitor*) const override;
 
  private:
