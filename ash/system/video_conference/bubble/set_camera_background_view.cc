b1dfd38746b1c9c236641f52844121d6ec3c2724
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-05-02 16:42:25 +0000

    [views-ax] Deprecate SetAccessibleDescription & replace by cache setter
    This CLs officially deprecates the View::SetAccessibleDescription in
    favor of the ViewAccessibility::SetDescription, which sets the value
    directly in the accessibility cache.
    
    It also replaces all calls to SetAccessibleDescription to the setter
    on the ViewAccessibility member. We can't remove
    SetAccessibleDescription for now because it's still being used in
    Microsoft Edge, but we'll remove it as soon as we removed the callers
    from there.
    
    This CL doesn't introduce a change in behavior. It's part of the
    ViewsAX project. [1]
    
    [1] https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ic88f6662fff558f2366b4ea3e44b9d0f5defe5ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506865
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1295539}

--- ash/system/video_conference/bubble/set_camera_background_view.cc
+++ ash/system/video_conference/bubble/set_camera_background_view.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/codec/jpeg_codec.h"
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/image/image_skia_rep.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/animated_image_view.h"
 #include "ui/views/controls/button/image_button.h"
@@ -181,7 +182,7 @@ class RecentlyUsedImageButton : public views::ImageButton {
   void SetAccessibilityLabelFromRecentSeaPenImageInfo(
       personalization_app::mojom::RecentSeaPenImageInfoPtr info) {
     SetAccessibleRole(ax::mojom::Role::kListItem);
-    SetAccessibleDescription(l10n_util::GetStringUTF16(
+    GetViewAccessibility().SetDescription(l10n_util::GetStringUTF16(
         IDS_ASH_VIDEO_CONFERENCE_BUBBLE_BACKGROUND_BLUR_IMAGE_LIST_ITEM_DESCRIPTION));
 
     std::u16string query;
