a57384f5fd77e8c82a1c8533e778efe95dae1951
Tomasz Wiszkowski <ender@google.com>
2024-05-02 19:14:46 +0000

    Suppress Search in CCT on Incognito CCTs.
    While not technically supported right now, our MVP coverage excludes
    Incognito tabs.
    
    Change-Id: I8e0109170abe223ef73c44b4ecdbb2386d0a70cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510089
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295662}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabIntentDataProvider.java
@@ -1492,6 +1492,7 @@ public class CustomTabIntentDataProvider extends BrowserServicesIntentDataProvid
     @Override
     public boolean isInteractiveOmniboxAllowed() {
         if (!ChromeFeatureList.sSearchInCCT.isEnabled()) return false;
+        if (isIncognito()) return false;
 
         return isPackageNameInList(
                 getClientPackageName(), OMNIBOX_ALLOWED_PACKAGE_NAMES.getValue());
