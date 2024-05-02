e83c12f033e859b70f2eb4341706903f6e2f8cab
kylechar <kylechar@chromium.org>
2024-05-02 21:00:59 +0000

    Reland "Split shared state out of DawnContextProvider"
    This is a reland of commit c16abb5d1f3a4f400827af98064c4767752b8147. It
    fixes access to DawnContextProvider when implementation is not compiled.
    
    Original change's description:
    > Split shared state out of DawnContextProvider
    >
    > There needs to be a DawnContextProvider per thread for DrDC to work for
    > Graphite/Dawn/Vulkan. With Dawn/Vulkan the wgpu::Instance+Adapter+Device
    > can be shared between the graphite contexts. This splits DawnSharedState
    > out of DawnContextProvider to accomplish this shared ownership.
    >
    > This change should be a no-op since DawnContextProvider holds
    > the only ref to DawnSharedState. A follow up will change that when DrDC
    > is enabled so that two DawnContextProviders reference the same
    > DawnSharedState.
    >
    > Bug: 40945609
    > Change-Id: I7b630276d7b329c77d30644dae59e641fbf3d787
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5497715
    > Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1294930}
    
    Bug: 40945609
    Change-Id: I46cbdfaa96b21b3d0577fd229cee39a4de7ae867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5511422
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295743}

--- gpu/command_buffer/service/dawn_context_provider.h
+++ gpu/command_buffer/service/dawn_context_provider.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/memory/raw_ptr.h"
-#include "base/synchronization/lock.h"
-#include "base/thread_annotations.h"
-#include "base/trace_event/memory_dump_provider.h"
 #include "build/build_config.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "gpu/command_buffer/service/dawn_caching_interface.h"
@@ -35,14 +32,11 @@ class Context;
 }  // namespace skgpu::graphite
 
 namespace gpu {
-namespace webgpu {
-class DawnInstance;
-}  // namespace webgpu
 
-class GPU_GLES2_EXPORT DawnContextProvider
-    : public base::trace_event::MemoryDumpProvider {
+class DawnSharedState;
+
+class GPU_GLES2_EXPORT DawnContextProvider {
  public:
-  using CacheBlobCallback = webgpu::DawnCachingInterface::CacheBlobCallback;
   static std::unique_ptr<DawnContextProvider> Create(
       const GpuPreferences& gpu_preferences = GpuPreferences(),
       const GpuDriverBugWorkarounds& gpu_driver_workarounds =
@@ -59,15 +53,12 @@ class GPU_GLES2_EXPORT DawnContextProvider
 
   DawnContextProvider(const DawnContextProvider&) = delete;
   DawnContextProvider& operator=(const DawnContextProvider&) = delete;
+  ~DawnContextProvider();
 
-  ~DawnContextProvider() override;
-
-  wgpu::Device GetDevice() const { return device_; }
-  wgpu::BackendType backend_type() const { return backend_type_; }
-  bool is_vulkan_swiftshader_adapter() const {
-    return is_vulkan_swiftshader_adapter_;
-  }
-  wgpu::Adapter GetAdapter() const { return adapter_; }
+  wgpu::Device GetDevice() const;
+  wgpu::BackendType backend_type() const;
+  bool is_vulkan_swiftshader_adapter() const;
+  wgpu::Adapter GetAdapter() const;
   wgpu::Instance GetInstance() const;
 
   void SetCachingInterface(
@@ -88,45 +79,12 @@ class GPU_GLES2_EXPORT DawnContextProvider
 
   std::optional<error::ContextLostReason> GetResetStatus() const;
 
-  void OnError(WGPUErrorType error_type, const char* message);
-
  private:
-  // Cache functions for Dawn device to use.
-  static size_t LoadCachedData(const void* key,
-                               size_t key_size,
-                               void* value,
-                               size_t value_size,
-                               void* userdata);
-  static void StoreCachedData(const void* key,
-                              size_t key_size,
-                              const void* value,
-                              size_t value_size,
-                              void* userdata);
+  explicit DawnContextProvider(
+      scoped_refptr<DawnSharedState> dawn_shared_state);
 
-  explicit DawnContextProvider();
-
-  bool Initialize(wgpu::BackendType backend_type,
-                  bool force_fallback_adapter,
-                  const GpuPreferences& gpu_preferences,
-                  const GpuDriverBugWorkarounds& gpu_driver_workarounds);
-
-  bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& args,
-                    base::trace_event::ProcessMemoryDump* pmd) override;
-
-  std::unique_ptr<webgpu::DawnCachingInterface> caching_interface_;
-  std::unique_ptr<dawn::platform::Platform> platform_;
-  std::unique_ptr<webgpu::DawnInstance> instance_;
-  wgpu::Adapter adapter_;
-  wgpu::Device device_;
-  wgpu::BackendType backend_type_;
-  bool is_vulkan_swiftshader_adapter_ = false;
+  scoped_refptr<DawnSharedState> dawn_shared_state_;
   std::unique_ptr<skgpu::graphite::Context> graphite_context_;
-
-  bool registered_memory_dump_provider_ = false;
-
-  mutable base::Lock context_lost_lock_;
-  std::optional<error::ContextLostReason> context_lost_reason_
-      GUARDED_BY(context_lost_lock_);
 };
 
 }  // namespace gpu
