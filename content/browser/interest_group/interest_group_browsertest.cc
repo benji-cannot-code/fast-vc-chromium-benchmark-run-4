fb1fcacc7783c4a8fbfeec253780a4af1828b6b8
Qingxin Wu <qingxinwu@google.com>
2024-05-02 16:35:39 +0000

    Add kFledgeDeprecatedRenderURLReplacements back to browser test.
    Added in https://crrev.com/c/5504852, but removed in a rebase in
    https://crrev.com/c/5490151 by mistake.
    
    Change-Id: I940bafea862b1b41513bbcf4564e36d8982658b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5507217
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Caleb Raitto <caraitto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295530}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -723,7 +723,8 @@ class InterestGroupBrowserTest : public ContentBrowserTest {
          {blink::features::kFledgeCustomMaxAuctionAdComponents,
           {{"FledgeAdComponentLimit", "40"}}},
          {blink::features::kFledgeRealTimeReporting, {}},
-         {blink::features::kFledgeReportingTimeout, {}}},
+         {blink::features::kFledgeReportingTimeout, {}},
+         {blink::features::kFledgeDeprecatedRenderURLReplacements, {}}},
         /*disabled_features=*/
         {blink::features::kFencedFrames,
          blink::features::kFledgeEnforceKAnonymity,
