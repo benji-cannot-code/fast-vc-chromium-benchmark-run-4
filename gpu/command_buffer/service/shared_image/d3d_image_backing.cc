b7d63dd997a8611b3e090e0457201a256164d00b
Michael Tang <tangm@microsoft.com>
2024-05-02 22:05:51 +0000

    gpu: win: Avoid null write fence in D3DImageBacking
    In crrev.com/c/5456006, D3DImageBacking was made to support multiple
    write fences, however this change could result in a null write fence in
    the case of a non-shared texture (e.g. with --disable-skia-graphite).
    
    Bug: 335003893
    Change-Id: If98d1442587a661e515eabc46897d388d0a4afd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510835
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1295783}

--- gpu/command_buffer/service/shared_image/d3d_image_backing.cc
+++ gpu/command_buffer/service/shared_image/d3d_image_backing.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <d3d11_3.h>
 
+#include <functional>
+
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/ptr_util.h"
@@ -846,14 +848,14 @@ void D3DImageBacking::EndAccessD3D11(
   // Do not create a fence for the texture's original device if we're only using
   // the texture on one device or using a keyed mutex. The fence is lazily
   // created on the first access from another device in GetPendingWaitFences().
-  scoped_refptr<gfx::D3DSharedFence> signaled_fence;
+  D3DSharedFenceSet signaled_fence;
   if (use_fence_synchronization()) {
     auto& d3d11_signal_fence = d3d11_signaled_fence_map_[d3d11_device];
     if (!d3d11_signal_fence) {
       d3d11_signal_fence = gfx::D3DSharedFence::CreateForD3D11(d3d11_device);
     }
     if (d3d11_signal_fence && d3d11_signal_fence->IncrementAndSignalD3D11()) {
-      signaled_fence = d3d11_signal_fence;
+      signaled_fence.insert(d3d11_signal_fence);
     } else {
       LOG(ERROR) << "Failed to signal D3D11 device fence on EndAccess";
     }
@@ -863,7 +865,7 @@ void D3DImageBacking::EndAccessD3D11(
     dxgi_shared_handle_state_->ReleaseKeyedMutex(d3d11_device);
   }
 
-  EndAccessCommon({signaled_fence});
+  EndAccessCommon(signaled_fence);
 }
 
 bool D3DImageBacking::ValidateBeginAccess(bool write_access) const {
@@ -893,6 +895,7 @@ void D3DImageBacking::BeginAccessCommon(bool write_access) {
 
 void D3DImageBacking::EndAccessCommon(
     const D3DSharedFenceSet& signaled_fences) {
+  DCHECK(base::ranges::all_of(signaled_fences, std::identity()));
   if (in_write_access_) {
     DCHECK(write_fences_.empty());
     DCHECK(read_fences_.empty());
