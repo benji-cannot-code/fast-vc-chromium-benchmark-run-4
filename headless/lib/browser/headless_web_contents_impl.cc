0f7badc62176074106188d26f9e84ae25a0f4142
Weizhong Xia <weizhong@google.com>
2024-05-02 15:27:39 +0000

    Support BFCache in headless shell
    Majority of BFCache related tests are now passing on the try run
    below:
    
    https://chromium-layout-test-archives.storage.googleapis.com/results.html?json=chromium/try/linux-wpt-chromium-rel/1234/headless_shell_wpt_tests%20%28with%20patch%2C%20experimental%29/full_results_jsonp.js
    
    Bug: 41483142
    Change-Id: If98e41ac08f01b62abe3b8c185dc46418d06de8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503379
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295483}

--- headless/lib/browser/headless_web_contents_impl.cc
+++ headless/lib/browser/headless_web_contents_impl.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "headless/lib/browser/headless_browser_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
 #include "headless/lib/browser/protocol/headless_handler.h"
+#include "headless/public/switches.h"
 #include "printing/buildflags/buildflags.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
 #include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
@@ -202,6 +203,11 @@ class HeadlessWebContentsImpl::Delegate : public content::WebContentsDelegate {
     DirectoryEnumerator::Start(path, std::move(listener));
   }
 
+  bool IsBackForwardCacheSupported() override {
+    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+    return command_line->HasSwitch(switches::kEnableBackForwardCache);
+  }
+
   void RequestPointerLock(content::WebContents* web_contents,
                           bool user_gesture,
                           bool last_unlocked_by_target) override {
@@ -348,10 +354,10 @@ HeadlessWebContentsImpl::HeadlessWebContentsImpl(
       browser_context->options()->font_render_hinting();
 
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kForceWebRtcIPHandlingPolicy)) {
+  if (command_line->HasSwitch(::switches::kForceWebRtcIPHandlingPolicy)) {
     web_contents_->GetMutableRendererPrefs()->webrtc_ip_handling_policy =
         command_line->GetSwitchValueASCII(
-            switches::kForceWebRtcIPHandlingPolicy);
+            ::switches::kForceWebRtcIPHandlingPolicy);
   }
 
   web_contents_->SetDelegate(web_contents_delegate_.get());
--- headless/lib/switches.cc
+++ headless/lib/switches.cc
@@ -49,6 +49,9 @@ const char kDisableLazyLoading[] = "disable-lazy-loading";
 // UserDatadir.
 const char kDiskCacheDir[] = "disk-cache-dir";
 
+// Enable Back Forward Cache support
+const char kEnableBackForwardCache[] = "enable-bfcache";
+
 // Whether or not begin frames should be issued over DevToolsProtocol
 // (experimental).
 const char kEnableBeginFrameControl[] = "enable-begin-frame-control";
