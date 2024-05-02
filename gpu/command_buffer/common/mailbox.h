17e380aab9b320ac0594e93c9d7f9560260387ef
Colin Blundell <blundell@chromium.org>
2024-05-02 08:52:19 +0000

    [//gpu] Make Mailbox::IsSharedImage() internal to TransferableResource
    The only remaining use case for this function is for differentiating
    when a software TransferableResource is holding a software SharedImage
    versus a legacy SharedBitmap. This CL makes Mailbox::IsSharedImage()
    private with friending by TransferableResource and introduces a function
    on TransferableResource that clients needing to do this differentiation
    now use.
    
    Bug: 40057997, 337538024
    Change-Id: I399cc9b905cbe4a0594c70775a9ddd2bfc3e8a7c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498216
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295321}

--- gpu/command_buffer/common/mailbox.h
+++ gpu/command_buffer/common/mailbox.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace viz {
 class SharedBitmap;
+struct TransferableResource;
 }
 
 namespace gpu {
@@ -54,9 +55,6 @@ struct COMPONENT_EXPORT(GPU_MAILBOX) Mailbox {
   void SetZero();
   void SetName(const int8_t* name);
 
-  // Indicates whether this mailbox is used with the SharedImage system.
-  bool IsSharedImage() const;
-
   // Generate a unique unguessable mailbox name for use with the SharedImage
   // system.
   static Mailbox GenerateForSharedImage();
@@ -77,8 +75,10 @@ struct COMPONENT_EXPORT(GPU_MAILBOX) Mailbox {
   // A temporary solution until when kSharedBitmapToSharedImage is enabled by
   // default and the legacy ShareBitmap path is removed.
   static Mailbox GenerateLegacySharedBitmapMailbox();
+  bool IsSharedImage() const;
 
   friend class viz::SharedBitmap;
+  friend struct viz::TransferableResource;
 };
 
 }  // namespace gpu
