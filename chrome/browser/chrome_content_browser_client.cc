0b10cc56548c548360472344cd903566a1630650
Hailey Wang <haileywang@google.com>
2024-05-02 22:05:44 +0000

    [Data Sharing] Add navigation throttle for data sharing
    Add a NavigationThrottle class for data sharing URL interception.
    
    Bug: b/336873603
    Change-Id: I50c164b94f892cc84f9f0c80034a76f80df22410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484301
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295782}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -64,6 +64,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/data_saver/data_saver.h"
+#include "chrome/browser/data_sharing/data_sharing_navigation_throttle.h"
 #include "chrome/browser/defaults.h"
 #include "chrome/browser/device_api/device_service_impl.h"
 #include "chrome/browser/device_api/managed_configuration_service.h"
@@ -5376,6 +5377,11 @@ ChromeContentBrowserClient::CreateThrottlesForNavigation(
 
   MaybeAddThrottle(MaybeCreateVisitedLinkNavigationThrottleFor(handle),
                    &throttles);
+
+  MaybeAddThrottle(
+      data_sharing::DataSharingNavigationThrottle::MaybeCreateThrottleFor(
+          handle),
+      &throttles);
 
   return throttles;
 }
