684388a6b279c6d130af547d9044ade158a85a9c
Norge Vizcay <vizcay@google.com>
2024-05-02 12:28:08 +0000

    Introduce class outline for plus address affiliation matcher.
    The CL introduces the initial setup of a plus address affiliation
    matcher. The class will supply the PlusAddressService with a
    list of plus profiles that are relevant matches to the originally
    requested plus profile, based on the affiliation of their
    facets.
    
    Bug: b/338368194
    Change-Id: I0a3c8178db99a67c25ea230d92876b4c5b45aef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5501281
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1295395}

---
+++ components/plus_addresses/affiliations/plus_address_affiliation_match_helper.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/plus_addresses/affiliations/plus_address_affiliation_match_helper.h"
+
+#include "components/affiliations/core/browser/affiliation_service.h"
+#include "components/plus_addresses/plus_address_service.h"
+#include "components/plus_addresses/plus_address_types.h"
+
+namespace plus_addresses {
+
+PlusAddressAffiliationMatchHelper::PlusAddressAffiliationMatchHelper(
+    PlusAddressService* plus_address_service,
+    affiliations::AffiliationService* affiliation_service)
+    : plus_address_service_(*plus_address_service),
+      affiliation_service_(*affiliation_service) {}
+
+PlusAddressAffiliationMatchHelper::~PlusAddressAffiliationMatchHelper() =
+    default;
+
+void PlusAddressAffiliationMatchHelper::GetAffiliatedPlusProfiles(
+    const PlusProfile& plus_profile,
+    AffiliatedPlusProfilesCallback result_callback) {
+  // TODO(b/324553908): Complete.
+}
+
+}  // namespace plus_addresses
