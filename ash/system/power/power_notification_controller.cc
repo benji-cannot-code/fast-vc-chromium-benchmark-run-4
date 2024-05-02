38d77bc0d81276bdd019bfec33071d7cbee24958
Longbo Wei <longbowei@google.com>
2024-05-02 22:34:33 +0000

    power-notification: Record time_to_empty when critical notification shows.
    * Record the remaining battery time when the status changes to
      critical and the corresponding notification is displayed. This metric
      is recorded only once per transition to critical state.
    
    Bug=b:338254873
    Test=ash_unittests -f PowerNotificationControllerTest
    
    Change-Id: Idcbde9796627cff53d8f8e296c5a2bc1c85d7aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505923
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295801}

--- ash/system/power/power_notification_controller.cc
+++ ash/system/power/power_notification_controller.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/number_formatting.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/chromeos/devicetype_utils.h"
@@ -91,6 +92,19 @@ void LogBatteryForNoCharger(
   VLOG(1) << "Showing " << GetNotificationStateString(state)
           << " notification. No charger connected."
           << " Remaining time: " << remaining_minutes << " minutes.";
+}
+
+// Record remaining battery time in second when notification is shown for
+// critical state.
+void RecordTimeToEmptyForCriticalState(base::TimeDelta remaining_time) {
+  // Use the custom counts function instead of custom times so we can record in
+  // seconds instead of milliseconds. The max bucket is 10 minutes.
+  base::UmaHistogramCustomCounts(
+      "Ash.PowerNotification.TimeToEmptyForCritialState",
+      remaining_time.InSeconds(),
+      /*min=*/1,
+      /*exclusive_max=*/base::Minutes(10).InSeconds(),
+      /*buckets=*/100);
 }
 
 }  // namespace
@@ -395,6 +409,7 @@ bool PowerNotificationController::UpdateNotificationStateForRemainingTime() {
       if (remaining_minutes <= kCriticalMinutes) {
         notification_state_ = NOTIFICATION_CRITICAL;
         LogBatteryForNoCharger(notification_state_, remaining_minutes);
+        RecordTimeToEmptyForCriticalState(remaining_time.value());
         return true;
       }
       if (remaining_minutes <= kLowPowerMinutes) {
@@ -410,6 +425,7 @@ bool PowerNotificationController::UpdateNotificationStateForRemainingTime() {
       if (remaining_minutes <= kCriticalMinutes) {
         notification_state_ = NOTIFICATION_CRITICAL;
         LogBatteryForNoCharger(notification_state_, remaining_minutes);
+        RecordTimeToEmptyForCriticalState(remaining_time.value());
         return true;
       }
       return false;
