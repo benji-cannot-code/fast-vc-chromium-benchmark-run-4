a9995e8bcdb73bdb1f42847a4db75fc241c289c0
Lei Zhang <thestig@chromium.org>
2024-05-02 01:29:21 +0000

    Remove unnecessary renderer_preferences_util.h includes
    Since chrome/browser/renderer_preferences_util.h only contains a single
    function, it is easy to find files that include this header but do not
    call that function.
    
    Change-Id: I4a775ea659e21b04e118f248cf9ceca866489753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502632
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295224}

--- chrome/browser/background/background_contents.cc
+++ chrome/browser/background/background_contents.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/background/background_contents_service.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/renderer_preferences_util.h"
 #include "chrome/browser/task_manager/web_contents_tags.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/navigation_handle.h"
