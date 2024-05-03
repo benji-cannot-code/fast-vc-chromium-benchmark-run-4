c598e43b036b259046cb35130ab37bc29b9c9303
Fergal Daly <fergal@chromium.org>
2024-05-03 00:59:56 +0000

    Remove SetHeaderPolicy, there are no callers.
    Also remove allowlists_set_by_manifest_ which has no readers anymore.
    
    Change-Id: I344c95283af15a5fd6a3d148da704e46213ba64f
    Bug: 338183241
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5476587
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295883}

--- third_party/blink/public/common/permissions_policy/permissions_policy.h
+++ third_party/blink/public/common/permissions_policy/permissions_policy.h
@@ -233,10 +233,6 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
   std::optional<std::string> GetEndpointForFeature(
       mojom::PermissionsPolicyFeature feature) const;
 
-  // Sets the declared policy from the parsed Permissions-Policy HTTP header.
-  // Unrecognized features will be ignored.
-  void SetHeaderPolicy(const ParsedPermissionsPolicy& parsed_header);
-
   // Further restricts the policy for Isolated Apps that have a
   // Permissions-Policy HTTP header, |parsed_header|, that might be more
   // restrictive than its permissions policy declared in the Web App Manifest.
@@ -354,10 +350,6 @@ class BLINK_COMMON_EXPORT PermissionsPolicy {
 
   // Set this to true if `allowlists_` have already been checked.
   mutable bool disallow_updates_ = false;
-
-  // Set this to true if `allowlists_` was populated by web app manifest
-  // declared permissions policy.
-  bool allowlists_set_by_manifest_{false};
 
   // Records whether or not each feature was enabled for this frame by its
   // parent frame.
