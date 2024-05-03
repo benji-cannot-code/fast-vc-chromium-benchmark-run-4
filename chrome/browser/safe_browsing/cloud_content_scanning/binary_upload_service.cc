e57cb27faeade3bcb33c01f7784ebaa3d87182dd
Alexander-Joseph Silva <ajsilva@google.com>
2024-05-03 19:06:43 +0000

    Populate blocking proto field in print/paste code
    Bug: b/326058805
    Change-Id: I729ecc4de7836e69cf8084394c9e28d4b361d505
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5459332
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Alexander-Joseph Silva <ajsilva@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296257}

--- chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
@@ -245,6 +245,10 @@ void BinaryUploadService::Request::set_require_metadata_verdict(
       require_metadata_verdict);
 }
 
+void BinaryUploadService::Request::set_blocking(bool blocking) {
+  content_analysis_request_.set_blocking(blocking);
+}
+
 std::string BinaryUploadService::Request::SetRandomRequestToken() {
   DCHECK(request_token().empty());
   content_analysis_request_.set_request_token(
@@ -301,8 +305,9 @@ uint64_t BinaryUploadService::Request::user_action_requests_count() const {
 }
 
 GURL BinaryUploadService::Request::tab_url() const {
-  if (!content_analysis_request_.has_request_data())
+  if (!content_analysis_request_.has_request_data()) {
     return GURL();
+  }
   return GURL(content_analysis_request_.request_data().tab_url());
 }
 
@@ -319,9 +324,14 @@ BinaryUploadService::Request::reason() const {
   return content_analysis_request_.reason();
 }
 
+bool BinaryUploadService::Request::blocking() const {
+  return content_analysis_request_.blocking();
+}
+
 void BinaryUploadService::Request::StartRequest() {
-  if (!request_start_callback_.is_null())
+  if (!request_start_callback_.is_null()) {
     std::move(request_start_callback_).Run(*this);
+  }
 }
 
 void BinaryUploadService::Request::FinishRequest(
@@ -368,8 +378,9 @@ GURL BinaryUploadService::Request::GetUrlWithParams() const {
                                     connector);
   }
 
-  for (const std::string& tag : content_analysis_request_.tags())
+  for (const std::string& tag : content_analysis_request_.tags()) {
     url = net::AppendQueryParameter(url, enterprise::kUrlParamTag, tag);
+  }
 
   return url;
 }
