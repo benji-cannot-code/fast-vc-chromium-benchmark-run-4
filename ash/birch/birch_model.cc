928b63d530b9747cf76aee12019a306d8dafde61
Sammie Quon <sammiequon@chromium.org>
2024-05-03 19:28:36 +0000

    forest: Fix tests with forest enabled (1/N)
    This fixes birch and hot seat tests.
    
    Bug: b:338414459
    Change-Id: I13aae7857423f4b561360b45191f8a03f1e04307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512327
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296277}

--- ash/birch/birch_model.cc
+++ ash/birch/birch_model.cc
@@ -462,6 +462,10 @@ void BirchModel::MarkDataNotFresh() {
 
 void BirchModel::InitPrefChangeRegistrars() {
   PrefService* prefs = GetPrefService();
+  if (!prefs) {
+    return;
+  }
+
   calendar_pref_registrar_.Init(prefs);
   calendar_pref_registrar_.Add(
       prefs::kBirchUseCalendar,
@@ -525,6 +529,10 @@ void BirchModel::OnReleaseNotesPrefChanged() {
 
 void BirchModel::RecordProviderHiddenHistograms() {
   PrefService* prefs = GetPrefService();
+  if (!prefs) {
+    return;
+  }
+
   base::UmaHistogramBoolean("Ash.Birch.ProviderHidden.Calendar",
                             !prefs->GetBoolean(prefs::kBirchUseCalendar));
   base::UmaHistogramBoolean("Ash.Birch.ProviderHidden.FileSuggest",
