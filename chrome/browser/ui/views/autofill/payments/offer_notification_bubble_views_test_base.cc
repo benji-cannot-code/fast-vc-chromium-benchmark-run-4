122479704c37d2e6867792674a8ee02c064fc2f2
Andrew Grieve <agrieve@chromium.org>
2024-05-03 19:13:05 +0000

    Revert "Remove PDMObserver from AutofillOfferManager"
    This reverts commit dbc3b1eb7fd3f8b27e296fc32abfb5b0d6d4fede.
    
    Reason for revert: Likely cause of failing browsertest:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-x86-rel/21772
    
    Original change's description:
    > Remove PDMObserver from AutofillOfferManager
    >
    > As part of go/split-pdm the PDMObserver is split into an address and a
    > payments data manager observer. The offer manager only cares about
    > payment changes (to update the set of eligible merchant domains when
    > the set of offers changes). Thus, this CL replaces the offer manager's
    > PDMObserver with an PaymentsDataManager::Observer.
    >
    > Bug: b/322170538
    > Change-Id: I73d1093936934a562deb478325e28765f721d248
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5496758
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1295393}
    
    Bug: b/322170538, 338615163
    Change-Id: Ie2c06527d131a5f8db8bcfd41fd29fe0c948dab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513027
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296264}

--- chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
+++ chrome/browser/ui/views/autofill/payments/offer_notification_bubble_views_test_base.cc
@@ -112,7 +112,7 @@ OfferNotificationBubbleViewsTestBase::CreateCardLinkedOfferDataWithDomains(
   card->set_instrument_id(kCreditCardInstrumentId);
   personal_data_->payments_data_manager().AddServerCreditCardForTest(
       std::move(card));
-  test_api(personal_data_->payments_data_manager()).NotifyObservers();
+  personal_data_->NotifyPersonalDataObserver();
   int64_t offer_id = 4444;
   base::Time expiry = AutofillClock::Now() + base::Days(2);
   std::vector<GURL> merchant_origins;
@@ -198,7 +198,7 @@ void OfferNotificationBubbleViewsTestBase::SetUpCardLinkedOfferDataWithDomains(
   // CreateCardLinkedOfferDataWithDomains(~) will add the necessary card.
   test_api(personal_data_->payments_data_manager())
       .AddOfferData(CreateCardLinkedOfferDataWithDomains(domains));
-  test_api(personal_data_->payments_data_manager()).NotifyObservers();
+  personal_data_->NotifyPersonalDataObserver();
 }
 
 void OfferNotificationBubbleViewsTestBase::
@@ -207,7 +207,7 @@ void OfferNotificationBubbleViewsTestBase::
   personal_data_->payments_data_manager().ClearAllServerDataForTesting();
   test_api(personal_data_->payments_data_manager())
       .AddOfferData(CreateFreeListingCouponDataWithDomains(domains));
-  test_api(personal_data_->payments_data_manager()).NotifyObservers();
+  personal_data_->NotifyPersonalDataObserver();
 }
 
 void OfferNotificationBubbleViewsTestBase::
@@ -215,7 +215,7 @@ void OfferNotificationBubbleViewsTestBase::
   personal_data_->payments_data_manager().ClearAllServerDataForTesting();
   test_api(personal_data_->payments_data_manager())
       .AddOfferData(CreateGPayPromoCodeOfferDataWithDomains(domains));
-  test_api(personal_data_->payments_data_manager()).NotifyObservers();
+  personal_data_->NotifyPersonalDataObserver();
 }
 
 void OfferNotificationBubbleViewsTestBase::
