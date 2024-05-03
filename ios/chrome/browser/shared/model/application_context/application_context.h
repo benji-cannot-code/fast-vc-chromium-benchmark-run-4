5bc02346014abe7f758546d75c07896674914d48
Jérôme Lebel <jlebel@chromium.org>
2024-05-03 19:48:45 +0000

    [iOS] Replace GetSSOService by GetSingleSignOnService
    This patch replaces:
    ApplicationContext::GetSSOService()
    by:
    ApplicationContext::GetSingleSignOnService()
    and in all subclasses.
    
    There is no behavious changes.
    
    * http://crrev.com/c/5509322 Rename the method
    * http://crrev.com/i/7249097 Change calls
    =>http://crrev.com/c/5509704 Cleanup
    
    Change-Id: Ic86f9a9d30b283acb5983eb0cb2ebce7bf458c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5509704
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296292}

--- ios/chrome/browser/shared/model/application_context/application_context.h
+++ ios/chrome/browser/shared/model/application_context/application_context.h
@@ -167,8 +167,6 @@ class ApplicationContext {
 
   // Returns the SingleSignOnService instance used by this application.
   virtual id<SingleSignOnService> GetSingleSignOnService() = 0;
-  // Deprecated, replaced with `GetSingleSignOnService()`.
-  virtual id<SingleSignOnService> GetSSOService();
 
   // Returns the SystemIdentityManager instance used by this application.
   virtual SystemIdentityManager* GetSystemIdentityManager() = 0;
