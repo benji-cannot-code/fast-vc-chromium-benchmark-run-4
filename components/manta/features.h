6e88b76d8d8ebdbebd9f021e125c4959b1cf4e88
Xinglong Luan <alanlxl@google.com>
2024-05-03 05:16:27 +0000

    [mahi] add a flag that controls mahi endpoint
    Bug: b:338474005
    Test: autoninja -C out_${SDK_BOARD}/Release chrome
    Change-Id: I0839ca825b88f9de1d333974d5e88e1aefefde6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514383
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Auto-Submit: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295943}

--- components/manta/features.h
+++ components/manta/features.h
@@ -25,6 +25,8 @@ COMPONENT_EXPORT(MANTA) bool IsOrcaUseProdServerEnabled();
 
 COMPONENT_EXPORT(MANTA) bool IsSeaPenUseProdServerEnabled();
 
+COMPONENT_EXPORT(MANTA) bool IsMahiUseProdServerEnabled();
+
 }  // namespace manta::features
 
 #endif  // COMPONENTS_MANTA_FEATURES_H_
