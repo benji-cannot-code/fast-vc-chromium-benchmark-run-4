96e5f3286b52fc38a446641683df0221e914183c
Guillem Perez <guiperez@google.com>
2024-05-03 23:06:54 +0000

    [iOS] Separate content and regular notification registration calls.
    This CL addresses a bug where the browser state was being accessed
    before the browser is loaded. To prevent this, a new check has been
    added to see if the browser is active and the content notifications
    registration call has been guarded behind a flag.
    
    Bug: 338607993
    Change-Id: I4a178ab924346351a0895d67aeec1c35544ba5f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5516017
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Guillem Perez <guiperez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296424}

--- ios/chrome/browser/push_notification/model/push_notification_delegate.mm
+++ ios/chrome/browser/push_notification/model/push_notification_delegate.mm
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 
@@ -198,7 +199,7 @@ GaiaIdToPushNotificationPreferenceMapFromCache(
   config.singleSignOnService =
       GetApplicationContext()->GetSingleSignOnService();
 
-  if (browserState) {
+  if (IsContentPushNotificationsEnabled() && browserState) {
     AuthenticationService* authService =
         AuthenticationServiceFactory::GetForBrowserState(browserState);
     if (authService &&
