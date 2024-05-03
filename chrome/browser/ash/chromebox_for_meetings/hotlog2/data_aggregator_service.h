6c0ddc1756d34cffde772a3aa5d56df16f46c261
Niko Tsirakis <ntsirakis@google.com>
2024-05-03 20:31:15 +0000

    [chromebox_for_meetings] Add CfmDeviceInfoService to Hotlog2
    This service is used to get a provision-agnostic ID for the
    running CfM. This ID will be used to differentiate uploaded
    logs from different machines.
    
    Bug: b:338427630
    Change-Id: I026537ba7d975a2f2074c18e7fea85a5f4648766
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512182
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Niko Tsirakis <ntsirakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296318}

--- chrome/browser/ash/chromebox_for_meetings/hotlog2/data_aggregator_service.h
+++ chrome/browser/ash/chromebox_for_meetings/hotlog2/data_aggregator_service.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/chromebox_for_meetings/cfm_observer.h"
 #include "chromeos/ash/services/chromebox_for_meetings/public/mojom/meet_devices_data_aggregator.mojom-shared.h"
 #include "chromeos/ash/services/chromebox_for_meetings/public/mojom/meet_devices_data_aggregator.mojom.h"
+#include "chromeos/ash/services/chromebox_for_meetings/public/mojom/meet_devices_info.mojom.h"
 #include "chromeos/ash/services/chromebox_for_meetings/public/mojom/meet_devices_logger.mojom.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
@@ -77,6 +78,12 @@ class DataAggregatorService : public CfmObserver,
   void OnRequestBindUploadService(const std::string& interface_name,
                                   size_t num_tries,
                                   bool success);
+  void InitializeDeviceInfoEndpoint(size_t num_tries);
+  void OnRequestBindDeviceInfoService(const std::string& interface_name,
+                                      size_t num_tries,
+                                      bool success);
+  void RequestDeviceId();
+  void StoreDeviceId(chromeos::cfm::mojom::PolicyInfoPtr policy_info);
   void StartFetchTimer();
   void FetchFromAllSourcesAndEnqueue();
   void EnqueueData(const std::string& source_name,
@@ -95,6 +102,12 @@ class DataAggregatorService : public CfmObserver,
 
   // Remote endpoint for CfmLoggerService
   mojo::Remote<chromeos::cfm::mojom::MeetDevicesLogger> uploader_remote_;
+
+  // Remote endpoint for CfmDeviceInfoService
+  mojo::Remote<chromeos::cfm::mojom::MeetDevicesInfo> device_info_remote_;
+
+  // Unique device ID for the CfM that is permanent across provisioning.
+  std::string device_id_;
 
   // Must be the last class member.
   base::WeakPtrFactory<DataAggregatorService> weak_ptr_factory_{this};
