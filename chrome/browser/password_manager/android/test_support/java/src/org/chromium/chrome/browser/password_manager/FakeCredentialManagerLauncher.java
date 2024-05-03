464c373b560ddbc49082ef73ffb49a1e59303777
Andrew Grieve <agrieve@chromium.org>
2024-05-03 17:43:36 +0000

    Revert "[Fixit] Test launching credential manager from settings"
    This reverts commit 4161083a85d03bb94c975d3e8e96f9de65bba6e7.
    
    Reason for revert: New test failing on CI
    E.g.:
    https://ci.chromium.org/ui/p/chromium/builders/ci/android-pie-arm64-rel/23078/overview
    
    Original change's description:
    > [Fixit] Test launching credential manager from settings
    >
    > This test verifies that the credential manager fakes are called when
    > password manager is accessed from Chrome settings.
    > This is the first integration test that verifies such a use case.
    >
    > Bug: b/329432495
    > Change-Id: Ib87f2b2366a3fd51dd9beb4e3afd1c8cc8f1bb4e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5509633
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Reviewed-by: Peter Conn <peconn@chromium.org>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1296058}
    
    Bug: b/329432495
    Change-Id: Ide650e6769ab6b4ceee7f4741b4a6c9f78d0748b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513025
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1296193}

--- chrome/browser/password_manager/android/test_support/java/src/org/chromium/chrome/browser/password_manager/FakeCredentialManagerLauncher.java
+++ chrome/browser/password_manager/android/test_support/java/src/org/chromium/chrome/browser/password_manager/FakeCredentialManagerLauncher.java
@@ -14,8 +14,6 @@ import org.chromium.base.Callback;
 public class FakeCredentialManagerLauncher implements CredentialManagerLauncher {
     private PendingIntent mPendingIntent;
     private Exception mException;
-    private Callback<PendingIntent> mSuccessCallback;
-    private Callback<Exception> mFailureCallback;
 
     public void setIntent(PendingIntent pendingIntent) {
         mPendingIntent = pendingIntent;
@@ -25,14 +23,6 @@ public class FakeCredentialManagerLauncher implements CredentialManagerLauncher 
         mException = exception;
     }
 
-    public void setSuccessCallback(Callback<PendingIntent> successCallback) {
-        mSuccessCallback = successCallback;
-    }
-
-    public void setFailureCallback(Callback<Exception> failureCallback) {
-        mFailureCallback = failureCallback;
-    }
-
     @Override
     public void getAccountCredentialManagerIntent(
             @ManagePasswordsReferrer int referrer,
@@ -40,17 +30,9 @@ public class FakeCredentialManagerLauncher implements CredentialManagerLauncher 
             Callback<PendingIntent> successCallback,
             Callback<Exception> failureCallback) {
         if (accountName == null) {
-            if (mFailureCallback != null) {
-                mFailureCallback.onResult(
-                        new CredentialManagerBackendException(
-                                "Called without an account",
-                                CredentialManagerError.NO_ACCOUNT_NAME));
-            } else {
-                failureCallback.onResult(
-                        new CredentialManagerBackendException(
-                                "Called without an account",
-                                CredentialManagerError.NO_ACCOUNT_NAME));
-            }
+            failureCallback.onResult(
+                    new CredentialManagerBackendException(
+                            "Called without an account", CredentialManagerError.NO_ACCOUNT_NAME));
             return;
         }
         getCredentialManagerLaunchIntent(successCallback, failureCallback);
@@ -67,18 +49,10 @@ public class FakeCredentialManagerLauncher implements CredentialManagerLauncher 
     private void getCredentialManagerLaunchIntent(
             Callback<PendingIntent> successCallback, Callback<Exception> failureCallback) {
         if (mException != null) {
-            if (mFailureCallback != null) {
-                mFailureCallback.onResult(mException);
-            } else {
-                failureCallback.onResult(mException);
-            }
+            failureCallback.onResult(mException);
             return;
         }
-        if (mSuccessCallback != null) {
-            mSuccessCallback.onResult(mPendingIntent);
-        } else {
-            successCallback.onResult(mPendingIntent);
-        }
+        successCallback.onResult(mPendingIntent);
     }
 
     @Override
