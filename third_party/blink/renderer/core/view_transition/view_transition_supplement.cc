ee3f485a17e79e221f3c8f3e0e5742734e1a9501
Khushal Sagar <khushalsagar@chromium.org>
2024-05-03 20:54:07 +0000

    view-transition: Enable concurrent transitions for same-document.
    Currently we only allow 1 transition within a LocalFrame tree. If
    multiple LocalFrames rendering to the same compositor attempt to run a
    ViewTransition, the ancestor transition suppresses the child transition.
    
    This change removes this restriction now that the compositing stack
    supports this pattern.
    
    Bug: 330917526
    Change-Id: Ib7973799b46a3d9dd223d8fdc6e3b5bb99e22f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5499058
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: David Bokan <bokan@chromium.org>
    Reviewed-by: David Bokan <bokan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296341}

--- third_party/blink/renderer/core/view_transition/view_transition_supplement.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_supplement.cc
@@ -177,7 +177,8 @@ DOMViewTransition* ViewTransitionSupplement::StartTransition(
 
   // If there is a transition in a parent frame, give that precedence over a
   // transition in a child frame.
-  if (HasActiveTransitionInAncestorFrame(document.GetFrame())) {
+  if (!RuntimeEnabledFeatures::ConcurrentViewTransitionsEnabled() &&
+      HasActiveTransitionInAncestorFrame(document.GetFrame())) {
     auto skipped_transition = transition_;
     skipped_transition->SkipTransition();
 
@@ -187,7 +188,9 @@ DOMViewTransition* ViewTransitionSupplement::StartTransition(
 
   // Skip transitions in all frames associated with this widget. We can only
   // have one transition per widget/CC.
-  SkipTransitionInAllLocalFrames(document.GetFrame());
+  if (!RuntimeEnabledFeatures::ConcurrentViewTransitionsEnabled()) {
+    SkipTransitionInAllLocalFrames(document.GetFrame());
+  }
   DCHECK(transition_);
 
   return transition_->GetScriptDelegate();
