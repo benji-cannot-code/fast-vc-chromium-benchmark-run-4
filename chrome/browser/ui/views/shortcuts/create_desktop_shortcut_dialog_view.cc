6900d024514a06724a2c506b7c67a4d07eeffcec
Dibyajyoti Pal <dibyapal@google.com>
2024-05-03 22:31:31 +0000

    [Shortcuts] Don't show new Create shortcut dialog if one is visible
    This CL uses the WebModalDialogContentsManager to prevent the create
    shortcut dialog from showing up if any other web modal dialog is
    visible, thus preventing duplicate stacks of the create shortcut
    dialog from being shown.
    
    This has been manually tested by hooking up the Create Shortcut flow
    and also via a browser test that has been added.
    
    Also includes comments added to enhance readability of the code in
    this area.
    
    Bug: 333731400, 333024272
    Change-Id: I9d0be5ecc148944b4ce6e97686e157e5a57f88e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506956
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296402}

--- chrome/browser/ui/views/shortcuts/create_desktop_shortcut_dialog_view.cc
+++ chrome/browser/ui/views/shortcuts/create_desktop_shortcut_dialog_view.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/prefs/pref_service.h"
+#include "components/web_modal/web_contents_modal_dialog_manager.h"
+#include "content/public/browser/document_user_data.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/dialog_model.h"
@@ -33,7 +35,17 @@ void ShowCreateShortcutDialog(
     CreateShortcutDialogCallback dialog_action_and_text_callback) {
   Browser* browser = chrome::FindBrowserWithTab(web_contents);
   if (!browser) {
-    std::move(dialog_action_and_text_callback).Run(false, title);
+    std::move(dialog_action_and_text_callback)
+        .Run(/*is_accepted=*/false, title);
+    return;
+  }
+
+  // Do not show the dialog if it is already being shown.
+  const web_modal::WebContentsModalDialogManager* manager =
+      web_modal::WebContentsModalDialogManager::FromWebContents(web_contents);
+  if (!manager || manager->IsDialogActive()) {
+    std::move(dialog_action_and_text_callback)
+        .Run(/*is_accepted=*/false, title);
     return;
   }
 
