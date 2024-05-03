c420753b92ccf62c4c51cd634f28aa264eb44dd2
Jérôme Lebel <jlebel@chromium.org>
2024-05-03 16:32:58 +0000

    [iOS] Rename singleSignOnService property in configuration classes
    This patch has no behaviour changes.
    
    `ssoService` is renamed to `singleSignOnService`.
    
    This impacts:
    DiscoverFeedConguration
    MailtoHandleConfiguration
    PhotoServiceConfiguration
    PushNotificationConfiguration
    TrustedVaultClientConfiguration
    LensConfiguration
    
    * http://crrev.com/c/5509612 Update property name
    * http://crrev.com/i/7249956 Update calls
    =>http://crrev.com/c/5510862 Cleanup
    
    Change-Id: Id50fc51d6f0be7ee933e02ef29d9bd4f981d8d51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510862
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296145}

--- ios/chrome/browser/mailto_handler/model/mailto_handler_configuration.h
+++ ios/chrome/browser/mailto_handler/model/mailto_handler_configuration.h
@@ -24,8 +24,6 @@ class PrefService;
 
 // SingleSignOnService used by MailtoHandlerService.
 @property(nonatomic, strong) id<SingleSignOnService> singleSignOnService;
-// Deprecated, please use `singleSignOnService`.
-@property(nonatomic, strong, readonly) id<SingleSignOnService> ssoService;
 
 @end
 
--- ios/chrome/browser/signin/model/trusted_vault_configuration.h
+++ ios/chrome/browser/signin/model/trusted_vault_configuration.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // SingleSignOnService used by TrustedVaultClientBackend.
 @property(nonatomic, strong) id<SingleSignOnService> singleSignOnService;
-// Deprecated, need to use `singleSignOnService`.
-@property(nonatomic, strong, readonly) id<SingleSignOnService> ssoService;
 
 @end
 
