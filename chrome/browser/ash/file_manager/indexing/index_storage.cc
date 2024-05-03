a301ca1a5477c1d66334f80b7109376697dddd56
Bo Majewski <majewski@chromium.org>
2024-05-03 01:52:49 +0000

    [Files]: Unifies RAM and SQL approach. (Step 1 of 3)
    Creates an file index implementation (FileIndexImpl) that gathers
    common code used between RAM (in-memory) and SQL (on disk)
    implementations of the inverted index. Creates an IndexStorage
    interface that defines methods needed by FileIndexImpl to maintain
    an inverted index. Moves inmemory_file_index
    code that was implementing index storage to ram_storage.
    
    Bug: b:327534824
    Change-Id: If57a9f3f357959db0b681e909801d7b2a6877414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5492562
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295897}

---
+++ chrome/browser/ash/file_manager/indexing/index_storage.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/file_manager/indexing/index_storage.h"
+
+namespace file_manager {
+
+IndexStorage::IndexStorage() {}
+
+IndexStorage::~IndexStorage() {}
+
+bool IndexStorage::Init() {
+  return true;
+}
+
+bool IndexStorage::Close() {
+  return true;
+}
+
+}  // namespace file_manager
