67d3a3eea58a686ef4805893c6b268a1c3606f3f
Chris Lu <thegreenfrog@chromium.org>
2024-05-03 19:59:13 +0000

    [ios] Remove double Magic Stack Segmentation rank fetch
    ContentSuggestionsMediator's consumer is used to configure MVTs, but
    -fetchLatestMagicStackRanking only needs to be called in
    -configureConsumer if IsIOSMagicStackCollectionViewEnabled is NO since
    -setMagicStackConsumer: already does so.
    
    Bug: 338456998
    Change-Id: I002fb1f2c2c4b6aa74b21ae8dd3c3ed3b0ca8393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512626
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296299}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_mediator.mm
@@ -110,7 +110,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (!self.consumer) {
     return;
   }
-  [self.magicStackRankingModel fetchLatestMagicStackRanking];
+  if (!IsIOSMagicStackCollectionViewEnabled()) {
+    [self.magicStackRankingModel fetchLatestMagicStackRanking];
+  }
   if (!ShouldPutMostVisitedSitesInMagicStack() &&
       self.mostVisitedTilesMediator.mostVisitedConfig) {
     [self.consumer setMostVisitedTilesConfig:self.mostVisitedTilesMediator
