1dd55b72c5c1c232f14a9262252dce33fbf2f02b
anthie@google.com <anthie@google.com>
2024-05-03 15:12:25 +0000

    Enable SkipParentApprovalToInstallExtensions fieldtrial on ChromeOS
    Bug: b/324900757
    Change-Id: Ia91011ac24cc6596a645c380890e9208b8751d2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515010
    Auto-Submit: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296095}

--- chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
@@ -349,6 +349,7 @@ class SupervisedUserExtensionWebstorePrivateApiTest
             }) {
 
     std::vector<base::test::FeatureRef> enabled_features;
+    std::vector<base::test::FeatureRef> disabled_features;
 #if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
     enabled_features.push_back(
         supervised_user::
@@ -359,8 +360,12 @@ class SupervisedUserExtensionWebstorePrivateApiTest
       enabled_features.push_back(
           supervised_user::
               kEnableSupervisedUserSkipParentApprovalToInstallExtensions);
+    } else {
+      disabled_features.push_back(
+          supervised_user::
+              kEnableSupervisedUserSkipParentApprovalToInstallExtensions);
     }
-    feature_list_.InitWithFeatures(enabled_features, /*disabled_features=*/{});
+    feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
   ~SupervisedUserExtensionWebstorePrivateApiTest() override {
