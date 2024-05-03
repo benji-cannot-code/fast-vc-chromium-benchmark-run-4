2d96030dc4233917a7b358be3722a24c1b4dcc0d
Fabio Tirelo <ftirelo@chromium.org>
2024-05-03 17:19:37 +0000

    [Site search policy] Some consistency touches with starter pack
    This mostly solves a few inconsistencies between starter pack
    suggestions and featured site search.
    
    Fixed: 309458788
    Bug: 309458788
    Change-Id: Iad404601f4d4dbd0cb8d25e191fcb74c451f88b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505203
    Reviewed-by: Angela Yoeurng <yoangela@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296181}

--- components/omnibox/browser/autocomplete_controller_unittest.cc
+++ components/omnibox/browser/autocomplete_controller_unittest.cc
@@ -84,6 +84,17 @@ class AutocompleteControllerTest : public testing::Test {
     return match;
   }
 
+  AutocompleteMatch CreateFeaturedEnterpriseSearch(std::u16string keyword) {
+    AutocompleteMatch match;
+    match.type = AutocompleteMatchType::Type::FEATURED_ENTERPRISE_SEARCH;
+    match.contents = keyword;
+    match.keyword = keyword;
+    match.associated_keyword = std::make_unique<AutocompleteMatch>(
+        nullptr, 1000, false, AutocompleteMatchType::SEARCH_WHAT_YOU_TYPED);
+    match.associated_keyword->keyword = keyword;
+    return match;
+  }
+
   AutocompleteMatch CreateSearchMatch(std::string name,
                                       bool allowed_to_be_default_match,
                                       int traditional_relevance) {
@@ -341,6 +352,7 @@ TEST_F(AutocompleteControllerTest, FilterMatchesForInstantKeywordWithBareAt) {
       CreateStarterPackMatch(u"@bookmarks"),
       CreateStarterPackMatch(u"@history"),
       CreateStarterPackMatch(u"@tabs"),
+      CreateFeaturedEnterpriseSearch(u"@work"),
   });
 
   AutocompleteInput input(u"@", 1u, metrics::OmniboxEventProto::OTHER,
@@ -348,13 +360,15 @@ TEST_F(AutocompleteControllerTest, FilterMatchesForInstantKeywordWithBareAt) {
   controller_.MaybeCleanSuggestionsForKeywordMode(
       input, &controller_.internal_result_);
 
-  EXPECT_EQ(controller_.internal_result_.size(), 4u);
-  EXPECT_TRUE(
-      std::all_of(controller_.internal_result_.begin(),
-                  controller_.internal_result_.end(), [](const auto& match) {
-                    return match.type == AutocompleteMatchType::STARTER_PACK ||
-                           match.contents == u"@";
-                  }));
+  EXPECT_EQ(controller_.internal_result_.size(), 5u);
+  EXPECT_TRUE(std::all_of(
+      controller_.internal_result_.begin(), controller_.internal_result_.end(),
+      [](const auto& match) {
+        return match.type == AutocompleteMatchType::STARTER_PACK ||
+               match.type ==
+                   AutocompleteMatchType::FEATURED_ENTERPRISE_SEARCH ||
+               match.contents == u"@";
+      }));
 }
 #endif
 
