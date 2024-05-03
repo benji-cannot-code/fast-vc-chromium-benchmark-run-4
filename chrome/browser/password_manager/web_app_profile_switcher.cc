28a55a0299fe42b93214c4663ca0f2ae5435f562
Glen Robertson <glenrob@chromium.org>
2024-05-03 11:45:03 +0000

    WebAppInstallInfo: Remove usage of default constructor in Password Mgr
    The default WebAppInstallInfo constructor is deprecated and being
    removed to allow enforcement of invariants like required fields. This
    migrates password_manager code to use the preferred constructor. No
    functional changes.
    
    POC for this change: dmurph@ if glenrob@ is unavailable.
    
    Bug: b/280862254
    Change-Id: I520947216e4432229020ef4c96f48dff61e17c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514404
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296016}

--- chrome/browser/password_manager/web_app_profile_switcher.cc
+++ chrome/browser/password_manager/web_app_profile_switcher.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/password_manager/web_app_profile_switcher.h"
 
+#include <memory>
 #include <optional>
 
 #include "base/command_line.h"
@@ -31,18 +32,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-web_app::WebAppInstallInfo MakeInstallInfoFromApp(
-    const web_app::WebApp* web_app) {
-  web_app::WebAppInstallInfo install_info;
-  install_info.title = base::UTF8ToUTF16(web_app->untranslated_name());
-  install_info.description =
-      base::UTF8ToUTF16(web_app->untranslated_description());
-  install_info.start_url = web_app->start_url();
-  install_info.manifest_id = web_app->manifest_id();
-  install_info.manifest_url = web_app->manifest_url();
-  install_info.scope = web_app->scope();
-  install_info.manifest_icons = web_app->manifest_icons();
-  install_info.display_mode = web_app->display_mode();
+std::unique_ptr<web_app::WebAppInstallInfo> MakeInstallInfoFromApp(
+    const web_app::WebApp& web_app) {
+  auto install_info = std::make_unique<web_app::WebAppInstallInfo>(
+      web_app.manifest_id(), web_app.start_url());
+  install_info->title = base::UTF8ToUTF16(web_app.untranslated_name());
+  install_info->description =
+      base::UTF8ToUTF16(web_app.untranslated_description());
+  install_info->manifest_url = web_app.manifest_url();
+  install_info->scope = web_app.scope();
+  install_info->manifest_icons = web_app.manifest_icons();
+  install_info->display_mode = web_app.display_mode();
   return install_info;
 }
 
@@ -139,8 +139,7 @@ void WebAppProfileSwitcher::InstallAndLaunchWebApp(
   const web_app::WebApp* web_app =
       active_profile_provider->registrar_unsafe().GetAppById(app_id_);
   DCHECK(web_app);
-  auto install_info = std::make_unique<web_app::WebAppInstallInfo>(
-      MakeInstallInfoFromApp(web_app));
+  auto install_info = MakeInstallInfoFromApp(*web_app);
   install_info->icon_bitmaps = std::move(icon_bitmaps);
 
   web_app::WebAppInstallParams install_params;
