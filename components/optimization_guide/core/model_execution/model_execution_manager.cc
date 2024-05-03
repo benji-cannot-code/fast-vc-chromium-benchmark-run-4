66660eae0dbfb294dc4a14407e75eacc4e383a72
Sophie Chang <sophiechang@chromium.org>
2024-05-03 16:29:37 +0000

    Add a method for whether an on-device session for a feature can be created
    Bug: 335750081
    Change-Id: I6338064871f5c9c6a95b72365d307f1b6756cc1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512428
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296143}

--- components/optimization_guide/core/model_execution/model_execution_manager.cc
+++ components/optimization_guide/core/model_execution/model_execution_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/model_quality/model_quality_log_entry.h"
 #include "components/optimization_guide/core/model_util.h"
 #include "components/optimization_guide/core/optimization_guide_constants.h"
+#include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_logger.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
 #include "components/optimization_guide/core/optimization_guide_model_provider.h"
@@ -239,6 +240,15 @@ void ModelExecutionManager::ExecuteModel(
       base::BindOnce(&ModelExecutionManager::OnModelExecuteResponse,
                      weak_ptr_factory_.GetWeakPtr(), feature,
                      std::move(log_ai_data_request), std::move(callback)));
+}
+
+bool ModelExecutionManager::CanCreateOnDeviceSession(
+    ModelBasedCapabilityKey feature) {
+  if (!on_device_model_service_controller_) {
+    return false;
+  }
+  return on_device_model_service_controller_->CanCreateSession(feature) ==
+         OnDeviceModelEligibilityReason::kSuccess;
 }
 
 std::unique_ptr<OptimizationGuideModelExecutor::Session>
