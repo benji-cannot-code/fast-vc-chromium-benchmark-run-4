0f6f8e8cf6cd9398d5df3366c46a8ff3043e8f61
Adam Langley <agl@chromium.org>
2024-05-03 20:37:19 +0000

    webauthn: GPM should be at the top of the list.
    Bug: 40274370
    Change-Id: I3c3aa8d37c39740f7e2e08240a48fdd68a06ea64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512931
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296324}

--- chrome/browser/webauthn/authenticator_request_dialog_model.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_model.cc
@@ -2149,6 +2149,16 @@ void AuthenticatorRequestDialogController::PopulateMechanisms() {
     }
   }
 
+  if (base::FeatureList::IsEnabled(device::kWebAuthnEnclaveAuthenticator) &&
+      enclave_enabled_ && !is_get_assertion) {
+    const std::u16string name =
+        l10n_util::GetStringUTF16(IDS_WEBAUTHN_SOURCE_GOOGLE_PASSWORD_MANAGER);
+    model_->mechanisms.emplace_back(
+        Mechanism::Enclave(), name, name, kIcloudKeychainIcon,
+        base::BindRepeating(&AuthenticatorRequestDialogController::StartEnclave,
+                            base::Unretained(this)));
+  }
+
   if (transport_availability_.has_icloud_keychain && allow_icloud_keychain_ &&
       // The mechanism for iCloud Keychain only appears for create(), or if
       // Chrome doesn't have permission to enumerate credentials and thus the
@@ -2163,16 +2173,6 @@ void AuthenticatorRequestDialogController::PopulateMechanisms() {
         base::BindRepeating(
             &AuthenticatorRequestDialogController::StartICloudKeychain,
             base::Unretained(this)));
-  }
-
-  if (base::FeatureList::IsEnabled(device::kWebAuthnEnclaveAuthenticator) &&
-      enclave_enabled_ && !is_get_assertion) {
-    const std::u16string name =
-        l10n_util::GetStringUTF16(IDS_WEBAUTHN_SOURCE_GOOGLE_PASSWORD_MANAGER);
-    model_->mechanisms.emplace_back(
-        Mechanism::Enclave(), name, name, kIcloudKeychainIcon,
-        base::BindRepeating(&AuthenticatorRequestDialogController::StartEnclave,
-                            base::Unretained(this)));
   }
 
   std::optional<std::pair<int, AuthenticatorTransport>> windows_button_label;
