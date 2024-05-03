ea0251c80b4ee59d49161eb59909b75a1b4b11f5
Jing Wang <jiwan@chromium.org>
2024-05-03 03:23:28 +0000

    Reland "picker: add metrics for picker start session event"
    This reverts commit a44812c4e96868b4542a1da861eafd82b378fa53.
    
    Reason for revert: fixed the test failure in debug mode.
    
    In debug mode, is_component_build is set to true, and because
    structured_metrics_client is in a static_library target and picker is
    in a component target (ash), structured_metrics_client was duplicated
    when linked to ash and ash_unittest.
    
    filed b/337961280 to the metrics team to properly fix the dependency
    issue, for now as a work around, we created a static library target
    only for session_metrics class.
    
    Original change's description:
    > Revert "picker: add metrics for picker start session event"
    >
    > This reverts commit 38779eb72240033519ac50acf8ac0c7976254450.
    >
    > Reason for revert:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/36291/overview
    >
    > Original change's description:
    > > picker: add metrics for picker start session event
    > >
    > > Bug: b:322927522
    > > Change-Id: Idcdedaf53c19c0dd9c11161119a120bd0d04c487
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5464640
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Reviewed-by: Jong Ahn <jongahn@chromium.org>
    > > Reviewed-by: Darren Shen <shend@chromium.org>
    > > Commit-Queue: Jing Wang <jiwan@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1291618}
    >
    > Bug: b:322927522
    > Change-Id: Ib14cd23e75beb5fb57aa144fd3b24da58b0e8dd0
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481573
    > Owners-Override: Jihad Hanna <jihadghanna@google.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1291827}
    
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-dbg
    Bug: b:322927522
    Change-Id: Ic620ff5031a8585538b31d74e04d2dae7a42bb6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5493185
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295922}

--- ash/picker/metrics/picker_session_metrics.h
+++ ash/picker/metrics/picker_session_metrics.h
@@ -8,12 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
-#include "ash/ash_export.h"
+namespace ui {
+class TextInputClient;
+}  // namespace ui
 
 namespace ash {
 
 // Records metrics for a session of using Picker.
-class ASH_EXPORT PickerSessionMetrics {
+class PickerSessionMetrics {
  public:
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
@@ -31,6 +33,9 @@ class ASH_EXPORT PickerSessionMetrics {
   ~PickerSessionMetrics();
 
   void RecordOutcome(SessionOutcome outcome);
+
+  // Records CrOS event metrics when a picker session starts.
+  void OnStartSession(ui::TextInputClient* client);
 
  private:
   // Whether the outcome of this session has been recorded.
