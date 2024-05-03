917f378bb1040a2c924022925771e67ac4589484
Leonard Grey <lgrey@chromium.org>
2024-05-03 17:07:52 +0000

    Mac: deflake ShareMenuBrowsertests
    ui::GrabWindowSnapshot going async means that we're checking before the
    share actually happens.
    
    Bug: 41490939
    Change-Id: Ibb9e10abcb6aaf0ed9c51e9d821d4a291c4a7f68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512153
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Auto-Submit: Leonard Grey <lgrey@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296171}

--- chrome/browser/ui/cocoa/share_menu_controller.mm
+++ chrome/browser/ui/cocoa/share_menu_controller.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/mac_util.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/run_loop.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/global_keyboard_shortcuts_mac.h"
@@ -225,11 +226,14 @@ bool CanShare() {
     _activity.title = title;
     [_activity becomeCurrent];
   }
-
+  base::RunLoop run_loop;
+  auto done = run_loop.QuitClosure();
   [self saveTransitionDataFromBrowser:browser
                          whenComplete:base::BindOnce(^{
                            [service performWithItems:@[ url ]];
+                           std::move(done).Run();
                          })];
+  run_loop.Run();
 }
 
 // Opens the "Sharing" subpane of the "Extensions" macOS preference pane.
