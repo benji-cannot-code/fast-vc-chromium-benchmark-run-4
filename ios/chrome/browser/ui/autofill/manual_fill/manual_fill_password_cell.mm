babf50d79fdf81e5875a5f6e0f98bd0bd25c2ffe
Noémie St-Onge <noemies@google.com>
2024-05-03 21:04:47 +0000

    [iOSKAUpgrade] Create the "Autofill Form" button and add it to pwd cells
    This CL creates the new "Autofill Form" button and displays it in the
    manual fill password cells. This button will be added to payment and
    address cells in separate CLs. Taps on the button will also be handled
    in other CLs.
    
    Before/After comparison when the KA Upgrade feature is disabled (there shouldn't be any changes):
    https://screenshot.googleplex.com/DDU6VRjyNHN4j62.png
    
    Before/After comparison when the KA Upgrade feature is enabled:
    https://screenshot.googleplex.com/7KByofXtkPxMJG7.png
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 326407097, 326412705
    Change-Id: Ic5aec1dc3bd79a67322edab3c6b28806a9105aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502955
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296343}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_password_cell.mm
@@ -152,6 +152,9 @@ static const CGFloat kOffsetForConnectedCell = 16;
 // Layout guide for the cell's content.
 @property(nonatomic, strong) UILayoutGuide* layoutGuide;
 
+// Button to autofill the current form with the credential's data.
+@property(nonatomic, strong) UIButton* autofillFormButton;
+
 @end
 
 @implementation ManualFillPasswordCell
@@ -263,6 +266,12 @@ static const CGFloat kOffsetForConnectedCell = 16;
     self.grayLine.hidden = YES;
   }
 
+  if (IsKeyboardAccessoryUpgradeEnabled()) {
+    AddViewToVerticalLeadViews(self.autofillFormButton,
+                               ManualFillCellView::ElementType::kOther,
+                               verticalLeadViews);
+  }
+
   // Set and activate constraints.
   self.dynamicConstraints = [[NSMutableArray alloc] init];
   CGFloat offset = isConnectedToPreviousCell ? -kOffsetForConnectedCell : 0;
@@ -335,6 +344,13 @@ static const CGFloat kOffsetForConnectedCell = 16;
       staticConstraints, @[ self.passwordButton ], self.layoutGuide,
       kChipsHorizontalMargin,
       AppendConstraintsHorizontalEqualOrSmallerThanGuide);
+
+  if (IsKeyboardAccessoryUpgradeEnabled()) {
+    self.autofillFormButton = CreateAutofillFormButton();
+    [self.contentView addSubview:self.autofillFormButton];
+    AppendHorizontalConstraintsForViews(
+        staticConstraints, @[ self.autofillFormButton ], self.layoutGuide);
+  }
 
   [NSLayoutConstraint activateConstraints:staticConstraints];
 }
