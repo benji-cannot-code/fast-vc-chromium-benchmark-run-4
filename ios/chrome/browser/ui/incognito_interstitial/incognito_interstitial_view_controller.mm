7224918ca9515e9b3c6e43b062642fbd80e8fed5
Mariam Ali <alimariam@google.com>
2024-05-03 12:53:58 +0000

    [Code Health][iOS] Remove revamped label from variables in incognito NTP
    Bug: 40838267
    Change-Id: Ic387cc44ae9e750e11547b4daea3cd29fe201b6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490797
    Auto-Submit: Mariam Ali <alimariam@google.com>
    Commit-Queue: David Jean <djean@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Reviewed-by: David Jean <djean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296029}

--- ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_view_controller.mm
+++ ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_view_controller.mm
@@ -103,14 +103,12 @@ const CGFloat kTitleLabelLineHeightMultiple = 1.3;
   self.modalInPresentation = YES;
 
   // Creating the Incognito view (same one as NTP).
-  UIScrollView* incognitoView = NULL;
-  IncognitoView* revampedIncognitoView =
+  IncognitoView* incognitoView =
       [[IncognitoView alloc] initWithFrame:CGRectZero
              showTopIncognitoImageAndTitle:NO
                  stackViewHorizontalMargin:0
                          stackViewMaxWidth:CGFLOAT_MAX];
-  revampedIncognitoView.URLLoaderDelegate = self.URLLoaderDelegate;
-  incognitoView = revampedIncognitoView;
+  incognitoView.URLLoaderDelegate = self.URLLoaderDelegate;
   incognitoView.translatesAutoresizingMaskIntoConstraints = NO;
   incognitoView.bounces = NO;
 
@@ -161,7 +159,6 @@ const CGFloat kTitleLabelLineHeightMultiple = 1.3;
   [self.navigationBar pushNavigationItem:navigationRootItem animated:false];
   [self updateNavigationBarAppearance];
 
-  incognitoView.translatesAutoresizingMaskIntoConstraints = NO;
   [NSLayoutConstraint activateConstraints:@[
     [stackView.leadingAnchor
         constraintEqualToAnchor:self.specificContentView.leadingAnchor],
