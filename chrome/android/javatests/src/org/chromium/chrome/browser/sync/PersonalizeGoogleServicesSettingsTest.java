798aaf055ee0c34f1c1298507e3a62926042c3c1
Nicola Tommasi <tommasin@chromium.org>
2024-05-03 07:44:51 +0000

    [Gardener][Clank] Disable flaky test
    Disable PersonalizeGoogleServicesSettingsTest#testUserNotSignedIn.
    
    Bug: 334206890
    Change-Id: I7da8e54fac3bb7a5260b5fb54af390df03c64591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508601
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Owners-Override: Nicola Tommasi <tommasin@chromium.org>
    Auto-Submit: Nicola Tommasi <tommasin@chromium.org>
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1295973}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/PersonalizeGoogleServicesSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/PersonalizeGoogleServicesSettingsTest.java
@@ -28,6 +28,7 @@ import org.mockito.junit.MockitoRule;
 import org.chromium.base.test.util.ApplicationTestUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AppHooks;
@@ -122,6 +123,7 @@ public class PersonalizeGoogleServicesSettingsTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "Flaky test: b/334206890")
     @Feature({"PersonalizedGoogleServices"})
     public void testUserNotSignedIn() {
         mSettingsActivityTestRule.startSettingsActivity();
