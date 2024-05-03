408f9625df686eff48e9644d4b9156ac5c9f8a9c
Andy Phan <andyphan@chromium.org>
2024-05-03 17:24:35 +0000

    [PDF Ink Signatures] Fix annotation button showing when feature disabled
    The annotation button is showing, even when the kPdfInk2 feature flag is
    disabled. This is because pdf_extension_util::AddAdditionalData() has an
    enable_annotations argument that is usually true. This was usually
    ignored for Windows, macOS, and Linux, but https://crrev.com/1291410
    started using it, regardless of the the feature flag value. Fix this by
    only using it when the flag is enabled.
    
    Fixed: 338332430
    Change-Id: Iefbd04b4493322bcd2f4b09c6f5e1db6f5d7d8c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510829
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296183}

---
+++ chrome/test/data/pdf/ink2_disabled_test.ts
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+
+const viewer = document.body.querySelector('pdf-viewer')!;
+
+chrome.test.runTests([
+  // Test that the annotate button isn't shown when annotation mode is disabled.
+  function testAnnotationsDisabled() {
+    chrome.test.assertFalse(loadTimeData.getBoolean('pdfAnnotationsEnabled'));
+    chrome.test.assertFalse(loadTimeData.getBoolean('pdfInk2Enabled'));
+
+    const viewerToolbar = viewer.$.toolbar;
+    chrome.test.assertFalse(viewerToolbar.pdfAnnotationsEnabled);
+
+    const annotateButton = viewerToolbar.shadowRoot!.querySelector('#annotate');
+    chrome.test.assertTrue(annotateButton === null);
+    chrome.test.succeed();
+  },
+]);
