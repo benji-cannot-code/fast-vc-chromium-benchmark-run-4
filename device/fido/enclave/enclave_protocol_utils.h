60997f2323348e05655d19d02915ed42c5dd6d52
Ken Buchanan <kenrb@chromium.org>
2024-05-03 16:39:38 +0000

    [WebAuthn] Delay the creation of UV keys on Windows
    Windows Hello performs a user verification when a UV key is created,
    which isn't ideal at the time of device registration with the enclave
    authenticator service.
    
    This change creates a special EnclaveManager state for Windows systems
    that support UV, in which UV key creation is deferred. The creation
    happens when the user attempts a WebAuthn operation with the enclave.
    At that point they will have have to authenticate to Windows Hello,
    allowing the key to be created, and the enclave will accept the new
    UV public key for future use.
    
    Bug: 40274370
    Change-Id: Ia1c55e10e9f1f61d95516da7c46278822bce8059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503677
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296151}

--- device/fido/enclave/enclave_protocol_utils.h
+++ device/fido/enclave/enclave_protocol_utils.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
-#include "base/values.h"
 #include "device/fido/authenticator_get_assertion_response.h"
 #include "device/fido/authenticator_make_credential_response.h"
 #include "device/fido/ctap_get_assertion_request.h"
@@ -38,6 +37,8 @@ class JSONRequest;
 namespace enclave {
 
 // Parses a decrypted assertion command response from the enclave.
+// If there are multiple request responses in the array, it assumes the last
+// one is for the GetAssertion.
 // Returns one of: A successful response, an error code received from the
 //                 enclave, or a string describing an unhandled failure.
 absl::variant<AuthenticatorGetAssertionResponse, int, std::string>
@@ -46,6 +47,8 @@ absl::variant<AuthenticatorGetAssertionResponse, int, std::string>
                                   base::span<const uint8_t> credential_id);
 
 // Parses a decrypted registration command response from the enclave.
+// If there are multiple request responses in the array, it assumes the last
+// one is for the MakeCredential.
 // Returns one of: A pair containing the response and the new passkey entity,
 //                 an error code received from the enclave, or a string
 //                 describing an unhandled failure.
@@ -78,6 +81,12 @@ cbor::Value COMPONENT_EXPORT(DEVICE_FIDO) BuildMakeCredentialCommand(
     std::unique_ptr<ClaimedPIN> claimed_pin,
     std::optional<std::vector<uint8_t>> wrapped_secret,
     std::optional<std::vector<uint8_t>> secret);
+
+// Returns a CBOR value with the provided AddUVKey command to the enclave.
+// It must precede a credential registration or assertion request in the
+// command array.
+cbor::Value COMPONENT_EXPORT(DEVICE_FIDO)
+    BuildAddUVKeyCommand(base::span<const uint8_t> uv_public_key);
 
 // Builds a CBOR serialization of the command to be sent to the enclave
 // service which can then be encrypted and sent over HTTPS.
