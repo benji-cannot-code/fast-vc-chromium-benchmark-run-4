2fae2488a3b0a241feb578de8d341f0ff53c4e31
Dmitry Vykochko <vykochko@google.com>
2024-05-06 09:52:34 +0000

    [mullhet] Highlight search query in suggestion main_text.
    Bug: 325246516
    Change-Id: Ie6a331ddcff1c10c5e0ff0d0c824ce6befae8d4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490279
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296741}

--- chrome/browser/ui/autofill/mock_autofill_popup_controller.cc
+++ chrome/browser/ui/autofill/mock_autofill_popup_controller.cc
@@ -5,9 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/mock_autofill_popup_controller.h"
 
+#include "testing/gmock/include/gmock/gmock.h"
+
 namespace autofill {
 
-MockAutofillPopupController::MockAutofillPopupController() = default;
+MockAutofillPopupController::MockAutofillPopupController() {
+  ON_CALL(*this, GetSuggestionFilterMatches)
+      .WillByDefault(::testing::ReturnRef(filter_matches_));
+}
 
 MockAutofillPopupController::~MockAutofillPopupController() = default;
 
