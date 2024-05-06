fb68806fb177bcfef84ed72610872376dff28939
Keren Zhu <kerenzhu@chromium.org>
2024-05-06 19:48:38 +0000

    Accept mouse events in inactive window for Top Chrome WebUIs
    Top Chrome WebUIs are expected to behave like views UIs, where currently
    a big difference lies in handling mouse events in inactive windows.
    While views UIs allow hover effects and clicks to pass through when the
    window is inactive, Top Chrome WebUIs did not. This patch allows Top
    Chrome WebUIs to accept mouse events when the host window is inactive.
    
    Specifically, mouse move and click event are handled with subtle
    differences:
    * mouse move: accepts the event if the application is active (the
      content window could be inactive).
    * mouse click: always accepts the event.
    
    Note that, the mouse move behavior aligns with views UI, but this is not
    the behavior of other native macOS apps (e.g Notes, Safari), where move
    events are also always handled in the non-content area.
    
    A new method, acceptsMouseEventsOption, is added to
    RenderWidgetHostViewMacDelegate. It returns an AcceptMouseEventsOption
    enum of three possible options:
    * kAcceptMouseEventsInActiveWindow
    * kAcceptMouseEventsInActiveApp
    * kAcceptMouseEventsAlways
    
    For render widgets that host Top Chrome WebUIs, the browser now returns
    kAcceptMouseEventsInActiveApp, allowing interaction with inactive
    windows within an active app.
    
    This change applies to both trusted and untrusted Top Chrome WebUIs.
    
    Bug: 40791625, 338127521
    Change-Id: Ie9d31afa202e7ee5215f3be5746c3c84aa7577da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5516009
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297056}

--- chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
+++ chrome/browser/renderer_host/chrome_render_widget_host_view_mac_delegate.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/webui/top_chrome/webui_url_utils.h"
 #include "chrome/common/url_constants.h"
 #include "components/prefs/pref_service.h"
 #include "components/spellcheck/browser/pref_names.h"
@@ -408,6 +409,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (view.window.firstResponder == view) {
     [self makeAnyDialogKey];
   }
+}
+
+- (AcceptMouseEventsOption)acceptsMouseEventsOption {
+  content::WebContents* webContents = self.webContents;
+  if (!webContents) {
+    return kAcceptMouseEventsInActiveWindow;
+  }
+
+  // For Top Chrome WebUIs, allows inactive windows to accept
+  // mouse events as long as the application is active. This
+  // mimics the behavior of views UI.
+  if (IsTopChromeWebUIURL(webContents->GetVisibleURL()) ||
+      IsTopChromeUntrustedWebUIURL(webContents->GetVisibleURL())) {
+    return kAcceptMouseEventsInActiveApp;
+  }
+
+  return kAcceptMouseEventsInActiveWindow;
 }
 
 @end
