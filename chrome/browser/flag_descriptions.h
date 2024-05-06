e7a9e2e21d2de8edfde9bb7d24a583be2aeeaf65
Awad Osman <awado@google.com>
2024-05-06 18:20:41 +0000

    Remove kTailoredSecurityUpdatedMessages feature flag
    Removes the flag and all references to it in the code base. Removes
    conditional code paths as well.
    
    Bug: 40284366
    Change-Id: I287aa7035aa4c67ccc9fb82fd3db4e5b45064ee2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513863
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296982}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -790,9 +790,6 @@ extern const char kEnableSearchEngineChoiceDescription[];
 extern const char kEnableSuspiciousSiteDetectionRTLookupsName[];
 extern const char kEnableSuspiciousSiteDetectionRTLookupsDescription[];
 
-extern const char kEnableTailoredSecurityUpdatedMessagesName[];
-extern const char kEnableTailoredSecurityUpdatedMessagesDescription[];
-
 extern const char kEnableFencedFramesName[];
 extern const char kEnableFencedFramesDescription[];
 
