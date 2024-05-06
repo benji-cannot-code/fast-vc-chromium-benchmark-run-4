97f364b0167ff0ece2997f69774d6717a8409fb3
Theo Cristea <theocristea@google.com>
2024-05-06 15:52:52 +0000

    [Gardening] Disable failing WebappDisplayModeTest on tablet bots
    Bug: 339000434
    Change-Id: I3a4262deaafccaf2bb6a2b1634832b63427dc233
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518612
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296877}

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappDisplayModeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappDisplayModeTest.java
@@ -21,6 +21,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.blink.mojom.DisplayMode;
@@ -33,6 +34,7 @@ import org.chromium.chrome.test.util.browser.webapps.WebappTestPage;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_public.browser.test.util.TouchCommon;
+import org.chromium.ui.test.util.UiDisableIf;
 import org.chromium.ui.test.util.UiRestriction;
 
 import java.util.concurrent.TimeoutException;
@@ -60,6 +62,7 @@ public class WebappDisplayModeTest {
 
     @Test
     @SmallTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/339000434
     @Feature({"Webapps"})
     public void testFullScreen() {
         WebappActivity activity = startActivity(DisplayMode.FULLSCREEN, "");
@@ -70,6 +73,7 @@ public class WebappDisplayModeTest {
 
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/339000434
     @Feature({"Webapps"})
     public void testFullScreenInFullscreen() {
         WebappActivity activity = startActivity(DisplayMode.FULLSCREEN, "fullscreen_on_click");
