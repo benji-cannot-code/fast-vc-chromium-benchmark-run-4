615322e5203323f693e3f877a85e8efadead118a
Dibyajyoti Pal <dibyapal@google.com>
2024-05-06 15:59:27 +0000

    [Shortcuts] Support Chrome for testing branded builds for icon badging
    This CL enables support for Chrome for testing branded builds for
    go/shortcuts-not-apps, ensuring go/chrome-badging-for-shortcuts is
    maintained.
    
    If google_chrome_for_testing = true is specified in the args, then the
    normal chromium icons are used, and if google_chrome_for_testing_branded
    is true, it uses the chrome test icons available in src-internal.
    
    Tested manually and by using automated testing. The current test
    expectations have been updated to also account for this use case.
    This should be the last fix for crbug/338392162.
    
    Bug: 338392162, 333024272
    Change-Id: I4affb5eb2e32dc064ac1bef6ee5de842eecc7b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5516501
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296883}

--- chrome/browser/shortcuts/icon_badging_unittest.cc
+++ chrome/browser/shortcuts/icon_badging_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/to_string.h"
 #include "base/test/gmock_expected_support.h"
 #include "build/branding_buildflags.h"
+#include "build/buildflag.h"
 #include "chrome/browser/shortcuts/image_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -49,6 +50,9 @@ base::FilePath GetCompileTimeTestFolders() {
   base::FilePath compile_time_folder;
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
   compile_time_folder = base::FilePath(FILE_PATH_LITERAL("chrome_branded"));
+#elif BUILDFLAG(GOOGLE_CHROME_FOR_TESTING_BRANDING)
+  compile_time_folder =
+      base::FilePath(FILE_PATH_LITERAL("chrome_for_testing_branded"));
 #else
   compile_time_folder = base::FilePath(FILE_PATH_LITERAL("chromium"));
 #endif
