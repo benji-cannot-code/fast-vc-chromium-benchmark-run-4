720c045b7c2cd017ff39cf8ee06ea1127a016421
Andrea Gomez <andreaxg@google.com>
2024-05-06 23:45:20 +0000

    Reland "Delay readability check until page load is done"
    Use the UI default thread
    
    This is a reland of commit c8c46c1c6a917c8febacc96c8bf41e92951388b9
    
    Original change's description:
    > Delay readability check until page load is done
    >
    > 1) Removing the server readability check from isReadable so the MTB and
    > app menu won't trigger the readability.
    >
    > 2) added readability check call 3 seconds after
    > didFirstVisuallyNonEmptyPaint
    >
    > 3) removed other readability checks that happen early on tab selection
    >
    > Bug: 335272896
    > Change-Id: I2527b83ba497acc8d739c1f92ef7d9a6a1dfb336
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5484321
    > Commit-Queue: Andrea Gomez <andreaxg@google.com>
    > Reviewed-by: Basia Zimirska <basiaz@google.com>
    > Auto-Submit: Andrea Gomez <andreaxg@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1296915}
    
    Bug: 335272896
    Change-Id: Ie16afc76cde2dafe554c8a0f1e3373d30c3f7564
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519114
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Basia Zimirska <basiaz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297192}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudToolbarButtonController.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudToolbarButtonController.java
@@ -84,7 +84,6 @@ public class ReadAloudToolbarButtonController extends BaseButtonDataProvider {
 
     @Override
     protected boolean shouldShowButton(Tab tab) {
-
         if (!super.shouldShowButton(tab) || tab == null || mControllerSupplier.get() == null) {
             return false;
         }
