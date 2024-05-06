fd2ca9b0d78f26d20941da3fc9dde14777b45e1f
Theo Cristea <theocristea@google.com>
2024-05-06 14:00:23 +0000

    [Gardening] Disable failing PageInfoViewDarkModeTest on tablet bots
    Bug: 338978357
    Change-Id: I74b05431c14beec39abae8942c99cb27c7f4a465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518749
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296824}

--- chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewDarkModeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/page_info/PageInfoViewDarkModeTest.java
@@ -24,6 +24,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
@@ -41,6 +42,7 @@ import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.net.test.EmbeddedTestServerRule;
 import org.chromium.ui.test.util.RenderTestRule;
+import org.chromium.ui.test.util.UiDisableIf;
 
 import java.io.IOException;
 
@@ -142,6 +144,7 @@ public class PageInfoViewDarkModeTest {
     /** Tests PageInfo on internal page. */
     @Test
     @MediumTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338978357
     @Feature({"RenderTest"})
     public void testChromePage() throws IOException {
         loadUrlAndOpenPageInfo("chrome://version/");
