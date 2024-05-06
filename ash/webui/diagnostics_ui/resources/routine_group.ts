122c62a05f36d66b3c46c07d6b9fb1c8da59fdda
Michael Checo <michaelcheco@google.com>
2024-05-06 17:50:43 +0000

    diagnostics: Convert routine_group_test to TS
    Bug: b/245336251
    Change-Id: I05957698bc0066b2fdb825f2fb0490ea0f7e7bf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5458892
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296951}

--- ash/webui/diagnostics_ui/resources/routine_group.ts
+++ ash/webui/diagnostics_ui/resources/routine_group.ts
@@ -26,7 +26,7 @@ function getRoutine(routineProp: RoutineProperties): RoutineType {
  */
 export class RoutineGroup {
   routineProperties: RoutineProperties[];
-  private nonBlockingRoutines: Set<RoutineType>;
+  nonBlockingRoutines: Set<RoutineType>;
   routines: RoutineType[];
   groupName: string;
   progress: ExecutionProgress;
