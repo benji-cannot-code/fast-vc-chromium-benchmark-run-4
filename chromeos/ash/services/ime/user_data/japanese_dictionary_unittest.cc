907098fc1ba1780a0c2f36ab1aaf92e8d9b8d2e2
jhtin <jhtin@chromium.org>
2024-05-06 06:00:32 +0000

    JP SystemPK: Move fetch_japanese_dictionary.proto > japanese_dictionary.
    JapaneseDictionary proto definitions are used in other calls that will
    be created in the future for editing entries and dictionaries.
    
    Hence we should rename it to make it clear it is isn't just used for
    just one fetch_japanese_dictionary command.
    
    Bug: b/336226897
    Change-Id: I36b777a235c8ce2ebb9cbafc95abba8352fb6b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514868
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296706}

--- chromeos/ash/services/ime/user_data/japanese_dictionary_unittest.cc
+++ chromeos/ash/services/ime/user_data/japanese_dictionary_unittest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/services/ime/user_data/japanese_dictionary.h"
 
-#include "chromeos/ash/services/ime/public/cpp/shared_lib/proto/fetch_japanese_dictionary.pb.h"
+#include "chromeos/ash/services/ime/public/cpp/shared_lib/proto/japanese_dictionary.pb.h"
 #include "chromeos/ash/services/ime/public/mojom/user_data_japanese_dictionary.mojom.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
