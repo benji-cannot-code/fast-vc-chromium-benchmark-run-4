1e35ff25ef79a62af640679e89a230463491a27d
Lei Zhang <thestig@chromium.org>
2024-05-06 20:57:10 +0000

    Split chrome_browser_main_extra_parts.h into its own target
    In chrome/browser/BUILD.gn, move this file out of the "browser" target
    and into its own "main_extra_parts" target. Then update build rules to
    account for this new target. This will prevent circular dependencies, as
    several targets implement their own extra parts.
    
    Also remove unneeded chrome_browser_main_extra_parts.h includes to avoid
    having to add more unnecessary dependencies.
    
    Change-Id: I2eda05b59fd1efaf4ca7c6a0a626ebd284bbba83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5509094
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297101}

--- chrome/browser/ui/views/crostini/crostini_dialogue_browser_test_util.cc
+++ chrome/browser/ui/views/crostini/crostini_dialogue_browser_test_util.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/fake_crostini_features.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_browser_main.h"
-#include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_features.h"
 #include "components/component_updater/ash/fake_component_manager_ash.h"
