afa4b05b81c14e6991aab8ae062263ce271c18cd
Yulun Wu <yulunwu@chromium.org>
2024-05-06 19:19:26 +0000

    Update desk button to update background based on shelf alignment.
    Bug: 331803288
    Change-Id: I611535ccbeab11d87333bb3951115433ace4dc35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512723
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297036}

--- ash/wm/desks/desk_button/desk_button.cc
+++ ash/wm/desks/desk_button/desk_button.cc
@@ -63,6 +63,11 @@ DeskButton::DeskButton()
 
 DeskButton::~DeskButton() {}
 
+void DeskButton::SetZeroState(bool zero_state) {
+  zero_state_ = zero_state;
+  UpdateBackground();
+}
+
 gfx::Size DeskButton::CalculatePreferredSize(
     const views::SizeBounds& available_size) const {
   if (zero_state_) {
@@ -192,8 +197,7 @@ void DeskButton::Init(DeskButtonContainer* desk_button_container) {
   SetPaintToLayer();
   layer()->SetFillsBoundsOpaquely(false);
   SetFlipCanvasOnPaintForRTLUI(false);
-  SetBackground(views::CreateThemedRoundedRectBackground(
-      cros_tokens::kCrosSysSystemOnBase1, kDeskButtonCornerRadius));
+  UpdateBackground();
 
   SetInstallFocusRingOnFocus(true);
   SetFocusBehavior(views::View::FocusBehavior::ALWAYS);
@@ -238,10 +242,7 @@ void DeskButton::SetActivation(bool is_activated) {
 
   is_activated_ = is_activated;
 
-  SetBackground(views::CreateThemedRoundedRectBackground(
-      is_activated_ ? cros_tokens::kCrosSysSystemPrimaryContainer
-                    : cros_tokens::kCrosSysSystemOnBase1,
-      kDeskButtonCornerRadius));
+  UpdateBackground();
   desk_name_label_->SetEnabledColorId(
       is_activated_ ? cros_tokens::kCrosSysSystemOnPrimaryContainer
                     : cros_tokens::kCrosSysOnSurface);
@@ -373,6 +374,14 @@ void DeskButton::UpdateShelfAutoHideDisabler(
   } else {
     disabler.emplace(desk_button_container_->shelf());
   }
+}
+
+void DeskButton::UpdateBackground() {
+  SetBackground(views::CreateThemedRoundedRectBackground(
+      is_activated_ ? cros_tokens::kCrosSysSystemPrimaryContainer
+                    : (zero_state_ ? cros_tokens::kCrosSysSystemOnBase
+                                   : cros_tokens::kCrosSysSystemOnBase1),
+      kDeskButtonCornerRadius));
 }
 
 BEGIN_METADATA(DeskButton)
