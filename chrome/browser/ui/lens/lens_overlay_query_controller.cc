49b1a4e7ebef54bb7d379060d3fbce63659b4a92
Jason Hu <hujasonx@google.com>
2024-05-06 17:36:56 +0000

    [lensoverlay] Add Lens OAuth2 scope to unconsented allowlist
    Fixes a crash if sync is disabled:
    "Check failed: GetUnconsentedOAuth2Scopes().count(scope)"
    
    Bug: 338627086
    Change-Id: Ida0ed1a998992345f845a4b033ae27713a8f579b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5516347
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296935}

--- chrome/browser/ui/lens/lens_overlay_query_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_query_controller.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/variations/variations_ids_provider.h"
 #include "components/version_info/channel.h"
 #include "google_apis/common/api_error_codes.h"
+#include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "google_apis/google_api_keys.h"
 #include "net/base/url_util.h"
@@ -49,7 +50,6 @@ constexpr char kHttpMethod[] = "POST";
 constexpr char kContentType[] = "application/x-protobuf";
 constexpr char kDeveloperKey[] = "X-Developer-Key";
 constexpr char kSessionIdQueryParameterKey[] = "gsessionid";
-constexpr char kLensOAuthScope[] = "https://www.googleapis.com/auth/lens";
 constexpr char kOAuthConsumerName[] = "LensOverlayQueryController";
 constexpr base::TimeDelta kServerRequestTimeout = base::Minutes(1);
 
@@ -534,7 +534,7 @@ void LensOverlayQueryController::CreateAndFetchEndpointFetcher(
                                  std::move(fetcher_created_callback),
                                  std::move(fetched_response_callback)));
     signin::ScopeSet oauth_scopes;
-    oauth_scopes.insert(kLensOAuthScope);
+    oauth_scopes.insert(GaiaConstants::kLensOAuth2Scope);
 
     // If an access token fetcher is already in flight, it is intentionally
     // replaced by this newer one.
