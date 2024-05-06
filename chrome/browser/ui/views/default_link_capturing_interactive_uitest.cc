95574597f3fb632c3a0105351ba78c72f7cbfdac
Tom Van Goethem <tov@chromium.org>
2024-05-06 12:47:37 +0000

    [Gardener] Disable failing WebAppLinkCapturingIPHPromoTest tests
    Several WebAppLinkCapturingIPHPromoTest tests have been consistently
    failing on Linux MSan builders.
    https://ci.chromium.org/p/chromium/builders/ci/Linux%20MSan%20Tests/b8748697448185454545
    is the first failing build.
    
    Bug: 338969664
    Change-Id: Id748d5a9d641f415e88e017101b0c19765769ada
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518192
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Auto-Submit: Tom Van Goethem <tov@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Owners-Override: Tom Van Goethem <tov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296793}

--- chrome/browser/ui/views/default_link_capturing_interactive_uitest.cc
+++ chrome/browser/ui/views/default_link_capturing_interactive_uitest.cc
@@ -200,6 +200,24 @@ IN_PROC_BROWSER_TEST_F(IntentPickerInteractiveUiTest,
 }
 #endif  // BUILDFLAG(IS_MAC)
 
+// TODO(b/338969664): The following tests are failing on Linux MSan, so
+// disabling for now. These should be re-enabled.
+#if BUILDFLAG(IS_LINUX) && defined(MEMORY_SANITIZER)
+#define MAYBE_AcceptingBubbleMeasuresUserAccept \
+  DISABLED_AcceptingBubbleMeasuresUserAccept
+#define MAYBE_BubbleDismissMeasuresUserDismiss \
+  DISABLED_BubbleDismissMeasuresUserDismiss
+#define MAYBE_ClosingAppWindowMeasuresDismiss \
+  DISABLED_ClosingAppWindowMeasuresDismiss
+#define MAYBE_IPHShownOnLinkClick DISABLED_IPHShownOnLinkClick
+#else
+#define MAYBE_AcceptingBubbleMeasuresUserAccept \
+  AcceptingBubbleMeasuresUserAccept
+#define MAYBE_BubbleDismissMeasuresUserDismiss BubbleDismissMeasuresUserDismiss
+#define MAYBE_ClosingAppWindowMeasuresDismiss ClosingAppWindowMeasuresDismiss
+#define MAYBE_IPHShownOnLinkClick IPHShownOnLinkClick
+#endif
+
 // Test to verify that launching an app from the intent picker chip shows the
 // IPH bubble if the feature flag is enabled.
 class WebAppLinkCapturingIPHPromoTest
@@ -316,7 +334,7 @@ IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
 }
 
 IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
-                       IPHShownOnLinkClick) {
+                       MAYBE_IPHShownOnLinkClick) {
   const webapps::AppId app_id = InstallApp();
   SetUpSiteForLinkCapturingIphBubble(app_id);
 
@@ -333,7 +351,7 @@ IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
 }
 
 IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
-                       ClosingAppWindowMeasuresDismiss) {
+                       MAYBE_ClosingAppWindowMeasuresDismiss) {
   const webapps::AppId app_id = InstallApp();
   base::UserActionTester user_action_tester;
   SetUpSiteForLinkCapturingIphBubble(app_id);
@@ -349,7 +367,7 @@ IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
 }
 
 IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
-                       AcceptingBubbleMeasuresUserAccept) {
+                       MAYBE_AcceptingBubbleMeasuresUserAccept) {
   const webapps::AppId app_id = InstallApp();
   base::UserActionTester user_action_tester;
   SetUpSiteForLinkCapturingIphBubble(app_id);
@@ -363,7 +381,7 @@ IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
 }
 
 IN_PROC_BROWSER_TEST_P(WebAppLinkCapturingIPHPromoTest,
-                       BubbleDismissMeasuresUserDismiss) {
+                       MAYBE_BubbleDismissMeasuresUserDismiss) {
   const webapps::AppId app_id = InstallApp();
   base::UserActionTester user_action_tester;
   SetUpSiteForLinkCapturingIphBubble(app_id);
