710ec171b3eccde6a6b1f3bdde87777c1aae67bd
Steven Holte <holte@google.com>
2024-05-06 22:46:41 +0000

    Always use explicit raw_output check.
    * Always behave as though a Raw Output check is configured, removing
      the use of the scores provided directly in OnResponse/OnComplete.
    
    Bug: 336378632
    
    Change-Id: Ic8277383d93e26e56755b3e88056fdf0d43175ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5516289
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297168}

--- components/optimization_guide/core/model_execution/safety_config.cc
+++ components/optimization_guide/core/model_execution/safety_config.cc
@@ -5,13 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/optimization_guide/core/model_execution/safety_config.h"
 
+#include <cstddef>
 #include <optional>
 #include <string>
 
 #include "base/containers/contains.h"
+#include "base/no_destructor.h"
 #include "components/optimization_guide/core/model_execution/substitution.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/optimization_guide/proto/string_value.pb.h"
+#include "components/optimization_guide/proto/substitution.pb.h"
 #include "components/optimization_guide/proto/text_safety_model_metadata.pb.h"
 #include "services/on_device_model/public/mojom/on_device_model.mojom.h"
 
@@ -43,6 +46,31 @@ bool HasUnsafeScores(
   return false;
 }
 
+using CheckTemplate =
+    google::protobuf::RepeatedPtrField<proto::SubstitutedString>;
+
+CheckTemplate MakeDefaultRawOutputCheckTemplate() {
+  CheckTemplate input_template;
+  auto* sub = input_template.Add();
+  sub->set_string_template("%s");
+  sub->add_substitutions()
+      ->add_candidates()
+      ->mutable_proto_field()
+      ->add_proto_descriptors()
+      ->set_tag_number(1);
+  return input_template;
+}
+
+const CheckTemplate& GetRawOutputCheckTemplate(
+    const proto::FeatureTextSafetyConfiguration& config) {
+  if (!config.has_raw_output_check()) {
+    const static base::NoDestructor<CheckTemplate> default_template(
+        MakeDefaultRawOutputCheckTemplate());
+    return *default_template;
+  }
+  return config.raw_output_check().input_template();
+}
+
 }  // namespace
 
 SafetyConfig::SafetyConfig() = default;
@@ -53,13 +81,11 @@ SafetyConfig::SafetyConfig(SafetyConfig&&) = default;
 SafetyConfig::~SafetyConfig() = default;
 SafetyConfig& SafetyConfig::operator=(SafetyConfig&&) = default;
 
-bool SafetyConfig::IsMissingSafetyInfo(bool has_safety_info) const {
-  return proto_ && !has_safety_info;
-}
-
-std::optional<uint32_t> SafetyConfig::TokenInterval() const {
+uint32_t SafetyConfig::TokenInterval() const {
   if (!proto_) {
-    return std::nullopt;
+    // In the absence of a config, HasRawOutputCheck() should be false, so it
+    // will trivially pass. Returning 1 here admits every partial output.
+    return 1;
   }
   return features::GetOnDeviceModelTextSafetyTokenInterval();
 }
@@ -137,15 +163,14 @@ bool SafetyConfig::IsRequestUnsafe(
 }
 
 bool SafetyConfig::HasRawOutputCheck() const {
-  return proto_ && proto_->has_raw_output_check();
+  return proto_.has_value();
 }
 
 std::optional<SubstitutionResult> SafetyConfig::GetRawOutputCheckInput(
     const std::string& raw_output) const {
   proto::StringValue message;
   message.set_value(raw_output);
-  return CreateSubstitutions(message,
-                             proto_->raw_output_check().input_template());
+  return CreateSubstitutions(message, GetRawOutputCheckTemplate(*proto_));
 }
 
 }  // namespace optimization_guide
