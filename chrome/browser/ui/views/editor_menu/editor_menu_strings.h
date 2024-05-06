a36f14d0edb0821efcf12a47a04a0b82b5995e34
Chuong Ho <hdchuong@google.com>
2024-05-06 00:56:54 +0000

    editor: update the freeform input field placeholder to a shorter version
    To prevent the placeholder from overflowing the freeform text input
    field, this CL updates its text content to a shorter version.
    
    
    Bug: 337729187
    Change-Id: Ib6385f9f4ec5acc3fe2af4dee33b697db9dfa09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513904
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Auto-Submit: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296671}

--- chrome/browser/ui/views/editor_menu/editor_menu_strings.h
+++ chrome/browser/ui/views/editor_menu/editor_menu_strings.h
@@ -22,9 +22,7 @@ std::u16string GetEditorMenuWriteCardTitle();
 
 std::u16string GetEditorMenuRewriteCardTitle();
 
-std::u16string GetEditorMenuWriteCardFreeformHolder();
-
-std::u16string GetEditorMenuRewriteCardFreeformHolder();
+std::u16string GetEditorMenuFreeformPromptInputFieldPlaceholder();
 
 std::u16string GetEditorMenuSettingsTooltip();
 
