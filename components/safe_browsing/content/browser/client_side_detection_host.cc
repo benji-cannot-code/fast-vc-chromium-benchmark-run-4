2bd7a6ad2d7e655c5d41b1764c2252ae44b05fd9
Seung Jae Lim <andysjlim@chromium.org>
2024-05-06 15:56:53 +0000

    Send sample ping at a rare chance on CSD allowlist match
    Bug: 338200326
    Change-Id: Id8b06bc604ad9c630142f3acf4bd5db3d6b9aff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506976
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296879}

--- components/safe_browsing/content/browser/client_side_detection_host.cc
+++ components/safe_browsing/content/browser/client_side_detection_host.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/rand_util.h"
 #include "base/task/sequenced_task_runner_helpers.h"
 #include "base/task/thread_pool.h"
 #include "base/time/default_tick_clock.h"
@@ -73,6 +74,7 @@ namespace {
 const char kCsdDebugFeatureDirectoryFlag[] = "csd-debug-feature-directory";
 const char kSkipCSDAllowlistOnPreclassification[] =
     "safe-browsing-skip-csd-allowlist";
+const float kProbabilityForSendingSampleRequest = 0.01;
 
 void WriteFeaturesToDisk(const ClientPhishingRequest& features,
                          const base::FilePath& base_path) {
@@ -352,7 +354,7 @@ class ClientSideDetectionHost::ShouldClassifyUrlRequest
                             PreClassificationCheckResult phishing_reason,
                             bool match_allowlist) {
     DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-    if (match_allowlist) {
+    if (match_allowlist && !CanSendSamplePing()) {
       phishing_reason =
           PreClassificationCheckResult::NO_CLASSIFY_MATCH_CSD_ALLOWLIST;
     }
@@ -417,6 +419,13 @@ class ClientSideDetectionHost::ShouldClassifyUrlRequest
       // returns false.
       start_phishing_classification_cb_.Reset();
     }
+  }
+
+  bool CanSendSamplePing() {
+    return host_ && host_->delegate_->GetPrefs() &&
+           IsEnhancedProtectionEnabled(*host_->delegate_->GetPrefs()) &&
+           base::RandDouble() <= kProbabilityForSendingSampleRequest &&
+           base::FeatureList::IsEnabled(kClientSideDetectionSamplePing);
   }
 
   const GURL url_;
