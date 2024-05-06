5892b98f3371308785775f12af7163191938a049
Vidhan Jain <vidhanj@google.com>
2024-05-06 12:16:28 +0000

    [iOS] Simplify header/footer lengths for the bottom sheet
    This CL does the following:
    1. Move the footer from being an TextItem to being a footer of a section.
    2. Fix the header and footer height for the sections in the view.
    
    Screenshot: https://screenshot.googleplex.com/JaMnTEvMJvUbkcc.png
    Mock: https://screenshot.googleplex.com/ArigLV9HsuJvWTx.png
    
    Bug: 1482269
    Change-Id: I19c326b0eabb33352a65cc143f25dc956c95544d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510462
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296775}

--- ios/chrome/browser/ui/autofill/autofill_profile_edit_table_view_controller.mm
+++ ios/chrome/browser/ui/autofill/autofill_profile_edit_table_view_controller.mm
@@ -283,12 +283,7 @@ const CGFloat kLineSpacingBetweenErrorAndFooter = 12.0f;
   NSInteger sectionIdentifier =
       [_controller.tableViewModel sectionIdentifierForSectionIndex:section];
 
-  AutofillProfileDetailsSectionIdentifier firstFieldSection =
-      _dynamicallyLoadInputFieldsEnabled
-          ? AutofillProfileDetailsSectionIdentifierName
-          : AutofillProfileDetailsSectionIdentifierFields;
-
-  return sectionIdentifier == firstFieldSection ||
+  return sectionIdentifier == AutofillProfileDetailsSectionIdentifierFooter ||
          sectionIdentifier ==
              AutofillProfileDetailsSectionIdentifierErrorFooter;
 }
@@ -297,12 +292,12 @@ const CGFloat kLineSpacingBetweenErrorAndFooter = 12.0f;
   NSInteger sectionIdentifier =
       [_controller.tableViewModel sectionIdentifierForSectionIndex:section];
 
-  AutofillProfileDetailsSectionIdentifier lastFieldSection =
-      _dynamicallyLoadInputFieldsEnabled
-          ? AutofillProfileDetailsSectionIdentifierPhoneEmail
-          : AutofillProfileDetailsSectionIdentifierFields;
+  if (_dynamicallyLoadInputFieldsEnabled) {
+    return sectionIdentifier ==
+           AutofillProfileDetailsSectionIdentifierPhoneEmail;
+  }
 
-  return (sectionIdentifier == lastFieldSection) ||
+  return (sectionIdentifier == AutofillProfileDetailsSectionIdentifierFields) ||
          (!_settingsView &&
           sectionIdentifier == AutofillProfileDetailsSectionIdentifierFooter);
 }
@@ -322,17 +317,27 @@ const CGFloat kLineSpacingBetweenErrorAndFooter = 12.0f;
 - (void)loadMessageAndButtonForModalIfSaveOrUpdate:(BOOL)update {
   CHECK(!_settingsView);
   TableViewModel* model = _controller.tableViewModel;
+
+  if (self.accountProfile || self.migrationPrompt) {
+    CHECK([_userEmail length] > 0);
+    if (_dynamicallyLoadInputFieldsEnabled) {
+      [model addSectionWithIdentifier:
+                 AutofillProfileDetailsSectionIdentifierFooter];
+      [model setFooter:[self footerItem]
+          forSectionWithIdentifier:
+              AutofillProfileDetailsSectionIdentifierFooter];
+    } else {
+      [model addItem:[self footerItemForModalViewIfSaveOrUpdate:update]
+          toSectionWithIdentifier:
+              AutofillProfileDetailsSectionIdentifierFields];
+    }
+  }
+
   AutofillProfileDetailsSectionIdentifier sectionIdentifier =
       AutofillProfileDetailsSectionIdentifierFields;
   if (_dynamicallyLoadInputFieldsEnabled) {
     sectionIdentifier = AutofillProfileDetailsSectionIdentifierButton;
     [model addSectionWithIdentifier:sectionIdentifier];
-  }
-
-  if (self.accountProfile || self.migrationPrompt) {
-    DCHECK([_userEmail length] > 0);
-    [model addItem:[self footerItemForModalViewIfSaveOrUpdate:update]
-        toSectionWithIdentifier:sectionIdentifier];
   }
 
   [model addItem:[self saveButtonIfSaveOrUpdate:update]
@@ -427,7 +432,6 @@ const CGFloat kLineSpacingBetweenErrorAndFooter = 12.0f;
 
 // Creates and returns the `TableViewLinkHeaderFooterItem` footer item.
 - (TableViewLinkHeaderFooterItem*)footerItem {
-  CHECK(_settingsView);
   TableViewLinkHeaderFooterItem* item = [[TableViewLinkHeaderFooterItem alloc]
       initWithType:AutofillProfileDetailsItemTypeFooter];
   item.text = [self footerMessage];
