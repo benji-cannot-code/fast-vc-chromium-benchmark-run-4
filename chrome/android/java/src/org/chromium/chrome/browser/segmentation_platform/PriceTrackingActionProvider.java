6ccfd90d6882959cf5fb45197351973b9b6c01a3
Salvador Guerrero <salg@google.com>
2024-05-06 17:46:16 +0000

    [Fixit] Remove contextual page action price tracking flag
    This CL removes the "ContextualPageActionPriceTracking" flag. It was
    used to launch the price tracking feature which is now enabled by
    default.
    
    It also fixes a bookmark test that broke when the flag was enabled
    by not loading bookmark data on non-http(s) pages. (e.g. about:blank).
    
    Bug: 1385901,1406059
    Change-Id: Ic7ef68ac726395d99f6b4267c49ff606c88d0196
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5125916
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296944}

--- chrome/android/java/src/org/chromium/chrome/browser/segmentation_platform/PriceTrackingActionProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/segmentation_platform/PriceTrackingActionProvider.java
@@ -12,6 +12,7 @@ import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.commerce.core.ShoppingService;
+import org.chromium.components.embedder_support.util.UrlUtilities;
 
 /** Provides price tracking signal for showing contextual page action for a given tab. */
 public class PriceTrackingActionProvider implements ContextualPageActionController.ActionProvider {
@@ -31,6 +32,13 @@ public class PriceTrackingActionProvider implements ContextualPageActionControll
 
     @Override
     public void getAction(Tab tab, SignalAccumulator signalAccumulator) {
+
+        if (tab == null || tab.getUrl() == null || !UrlUtilities.isHttpOrHttps(tab.getUrl())) {
+            signalAccumulator.setHasPriceTracking(false);
+            signalAccumulator.notifySignalAvailable();
+            return;
+        }
+
         final BookmarkModel bookmarkModel = mBookmarkModelSupplier.get();
         bookmarkModel.finishLoadingBookmarkModel(
                 () -> {
