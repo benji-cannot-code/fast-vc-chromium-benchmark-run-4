d7a802bbe12cd01a0d8ac52e33307076fb7a8527
Colin Blundell <blundell@chromium.org>
2024-05-06 07:57:25 +0000

    [//components/viz] Read TransferableResource fields via accessors
    As a step toward eliminating TransferableResource::mailbox_holder, this
    CL changes all accesses in this module to read its fields via the
    accessors that now exist for this purpose.
    
    Bug: 337538024
    Change-Id: I56a7f09bef6c9784bb1f0f521e1064db75108314
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513444
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296723}

--- components/viz/service/display/display_resource_provider.cc
+++ components/viz/service/display/display_resource_provider.cc
@@ -106,8 +106,7 @@ bool DisplayResourceProvider::OnMemoryDump(
       pmd->CreateSharedMemoryOwnershipEdge(
           dump->guid(), resource.shared_bitmap_tracing_guid, kImportance);
     } else {
-      auto guid = GetSharedImageGUIDForTracing(
-          resource.transferable.mailbox_holder.mailbox);
+      auto guid = GetSharedImageGUIDForTracing(resource.transferable.mailbox());
       pmd->CreateSharedGlobalAllocatorDump(guid);
       pmd->AddOwnershipEdge(dump->guid(), guid, kImportance);
     }
@@ -236,7 +235,7 @@ void DisplayResourceProvider::ReceiveFromChild(
     }
 
     if (transferable_resource.is_software != IsSoftware() ||
-        transferable_resource.mailbox_holder.mailbox.IsZero()) {
+        transferable_resource.is_empty()) {
       TRACE_EVENT0(
           "viz", "DisplayResourceProvider::ReceiveFromChild dropping invalid");
       std::vector<ReturnedResource> returned;
@@ -283,7 +282,7 @@ gpu::Mailbox DisplayResourceProvider::GetMailbox(ResourceId resource_id) const {
   const ChildResource* resource = TryGetResource(resource_id);
   if (!resource)
     return gpu::Mailbox();
-  return resource->transferable.mailbox_holder.mailbox;
+  return resource->transferable.mailbox();
 }
 
 const std::unordered_map<ResourceId, ResourceId, ResourceIdHasher>&
@@ -560,7 +559,7 @@ DisplayResourceProvider::ChildResource::ChildResource(
     const TransferableResource& transferable)
     : child_id(child_id), transferable(transferable) {
   if (is_gpu_resource_type())
-    UpdateSyncToken(transferable.mailbox_holder.sync_token);
+    UpdateSyncToken(transferable.sync_token());
 }
 
 DisplayResourceProvider::ChildResource::ChildResource(ChildResource&& other) =
