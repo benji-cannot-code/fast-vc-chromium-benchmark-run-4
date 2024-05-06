09e7719d5c59d263fc7a6586e1d23ed7ffff6a11
stluong <stluong@chromium.org>
2024-05-06 17:52:43 +0000

    Introduce the performance notification button
    This CL introduces the performance notification toolbar button.
    Currently, the button is permanently shown on the toolbar button and
    doesn't do anything when it is clicked. Logic to trigger the toolbar
    button to show/hide and show a dialog when clicked will be added in
    follow up CLs.
    
    Picture of Button:
    https://screenshot.googleplex.com/8mKkodVgQhXe3vv.png
    
    Bug: 338072465
    Change-Id: I118afd10a84a31b4959d9a42042d4d46a650b38a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5503223
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1296954}

---
+++ chrome/browser/ui/performance_controls/performance_intervention_button_controller_delegate.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_PERFORMANCE_CONTROLS_PERFORMANCE_INTERVENTION_BUTTON_CONTROLLER_DELEGATE_H_
+#define CHROME_BROWSER_UI_PERFORMANCE_CONTROLS_PERFORMANCE_INTERVENTION_BUTTON_CONTROLLER_DELEGATE_H_
+
+// This delegate class is to control the performance intervention toolbar
+// button.
+class PerformanceInterventionButtonControllerDelegate {
+ public:
+  virtual ~PerformanceInterventionButtonControllerDelegate() = default;
+
+  // Show the performance intervention toolbar button.
+  virtual void Show() = 0;
+
+  // Hides the performance intervention toolbar button.
+  virtual void Hide() = 0;
+};
+
+#endif  // CHROME_BROWSER_UI_PERFORMANCE_CONTROLS_PERFORMANCE_INTERVENTION_BUTTON_CONTROLLER_DELEGATE_H_
