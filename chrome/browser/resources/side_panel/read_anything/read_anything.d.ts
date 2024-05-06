90396462f8de9b863b365fedc5c73734d7f5e621
Lauren Winston <lwinston@google.com>
2024-05-06 20:13:09 +0000

    Rename LogUmaHistogramLongTimes to LogUmaHistogramTimes.
    This was a misnaming and we'll need to log long times for
    upcoming logging, but I wanted to make the rename changes on
    their own to avoid something getting mixed up in a CL that
    does a rename + introduces a new method with the same name
    as the old name.
    
    Bug: 300329617
    Change-Id: I284e60d36c9b14552d5683abf73ef81e2ba750b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5518845
    Auto-Submit: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297077}

--- chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
@@ -335,7 +335,7 @@ declare namespace chrome {
     // updateVoicePackStatusFromInstallResponse()
     function sendInstallVoicePackRequest(language: string): void;
 
-    // Log UmaHistogramLong
+    // Log UmaHistogram
     function logMetric(time: number, metricName: string): void;
 
     // Log speech errors.
