f35a22464305675d9ff03a6d8a9ecb65f2742959
Dmitry Vykochko <vykochko@google.com>
2024-05-06 12:21:20 +0000

    [mullhet] Make footer suggestions non-filterable.
    This CL makes footer suggestions non-dependent on the search query.
    View footer suggestion utils were moved to be used in the controller.
    
    Bug: 325246516
    Change-Id: If451b37ced6b5006604f40e244628fef5697a64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513324
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296779}

--- chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_factory_utils.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_set.h"
 #include "chrome/browser/ui/autofill/autofill_popup_controller.h"
+#include "chrome/browser/ui/autofill/autofill_suggestion_controller_utils.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_base_view.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_cell_utils.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_row_content_view.h"
