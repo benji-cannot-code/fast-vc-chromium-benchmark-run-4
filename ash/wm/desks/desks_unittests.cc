9d3f78515676175699b33612cc1db51753b842f1
Sammie Quon <sammiequon@chromium.org>
2024-05-06 22:52:17 +0000

    forest: Fix tests with forest enabled (4/N)
    Fixes desks and wallpaper tests. For desks, the size of the expanded
    desks bar in overview is larger. For wallpaper, there is a new solid
    color, wallpaper under layer that should be considered in tests.
    
    I opted not to add a new parameter since this is only temporary, and I
    didn't want to add more tests. Especially with the desks tests, adding
    a new boolean parameter would mean 8 more variations per test.
    
    Test: none
    Bug: b:338414459
    Change-Id: I818fd875c7d31942ab4d01e1813c8d2ede9bfb83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519278
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297172}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "ash/test/ash_test_helper.h"
 #include "ash/test/test_widget_builder.h"
+#include "ash/utility/forest_util.h"
 #include "ash/wm/desks/default_desk_button.h"
 #include "ash/wm/desks/desk.h"
 #include "ash/wm/desks/desk_action_button.h"
@@ -9623,6 +9624,9 @@ struct DeskBarTestBasicCase {
 TEST_P(DeskBarTest, Basic) {
   UpdateDisplay("800x600");
 
+  const int expected_expanded_overview_height =
+      IsForestFeatureEnabled() ? 114 : 98;
+
   const DeskBarTestBasicCase tests[] = {
       {.test_name = "single desk + bottom shelf + saved desks",
        .desks = {0},
@@ -9667,8 +9671,10 @@ TEST_P(DeskBarTest, Basic) {
        .has_saved_desks = true,
        .desk_button_bar_widget_bounds = {221, 446, 358, 98},
        .desk_button_bar_view_bounds = {0, 0, 358, 98},
-       .overview_bar_widget_bounds = {0, 0, 800, 98},
-       .overview_bar_view_bounds = {0, 0, 800, 98}},
+       .overview_bar_widget_bounds = {0, 0, 800,
+                                      expected_expanded_overview_height},
+       .overview_bar_view_bounds = {0, 0, 800,
+                                    expected_expanded_overview_height}},
   };
 
   auto* desks_controller = DesksController::Get();
@@ -9752,10 +9758,14 @@ TEST_P(DeskBarTest, BasicSecondaryDisplay) {
   auto* desk_bar_widget = desk_bar_view->GetWidget();
   ASSERT_TRUE(desk_bar_widget);
 
+  const int expected_expanded_overview_height =
+      IsForestFeatureEnabled() ? 114 : 98;
+
   if (bar_type_ == DeskBarViewBase::Type::kOverview) {
     EXPECT_THAT(desk_bar_widget->GetWindowBoundsInScreen(),
-                gfx::Rect(800, 0, 800, 98));
-    EXPECT_THAT(desk_bar_view->bounds(), gfx::Rect(0, 0, 800, 98));
+                gfx::Rect(800, 0, 800, expected_expanded_overview_height));
+    EXPECT_THAT(desk_bar_view->bounds(),
+                gfx::Rect(0, 0, 800, expected_expanded_overview_height));
     EXPECT_FALSE(desk_bar_view->IsZeroState());
   } else {
     EXPECT_THAT(desk_bar_widget->GetWindowBoundsInScreen(),
