622ed7678d18e9ff33ec723c8b85dbdc35d01c19
Theo Cristea <theocristea@google.com>
2024-05-06 13:42:10 +0000

    [Gardening] Disable failing multi windows tests on tablet bots
    Bug: 338976206
    Change-Id: I16aed276556971ac40f87d896bbc7e0d45fe6ece
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513461
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1296815}

--- chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtilsTest.java
@@ -46,6 +46,7 @@ import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
+import org.chromium.ui.test.util.UiDisableIf;
 
 import java.util.concurrent.TimeoutException;
 
@@ -76,6 +77,7 @@ public class MultiWindowUtilsTest {
     /** Tests that ChromeTabbedActivity2 is used for intents when EXTRA_WINDOW_ID is set to 2. */
     @Test
     @SmallTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338976206
     @Feature("MultiWindow")
     public void testTabbedActivityForIntentWithExtraWindowId() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
@@ -96,6 +98,7 @@ public class MultiWindowUtilsTest {
      */
     @Test
     @SmallTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338976206
     @Feature("MultiWindow")
     public void testTabbedActivityForIntentLastResumedActivity() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
@@ -156,6 +159,7 @@ public class MultiWindowUtilsTest {
      */
     @Test
     @SmallTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338976206
     @Feature("MultiWindow")
     public void testTabbedActivityForIntentOnlyActivity2IsRunning() {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
@@ -212,6 +216,7 @@ public class MultiWindowUtilsTest {
      */
     @Test
     @SmallTest
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338976206
     @Feature("MultiWindow")
     public void testAreMultipleChromeInstancesRunningSecondInstanceKilledFirst()
             throws TimeoutException {
@@ -308,6 +313,7 @@ public class MultiWindowUtilsTest {
     @DisableIf.Build(
             sdk_is_less_than = Build.VERSION_CODES.O,
             message = "https://crbug.com/1077249")
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // https://crbug.com/338976206
     public void testAreMultipleChromeInstancesRunningFirstInstanceKilledFirst()
             throws TimeoutException {
         ChromeTabbedActivity activity1 = mActivityTestRule.getActivity();
