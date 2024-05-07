1fb73fcd406435c799e2faa011b4a0356db1cc60
Michele Fan <michelefan@chromium.org>
2024-05-07 05:54:35 +0000

    snap-group: Fix crash when accessing library button with Snap Group
    Demo: http://b/335301800#comment5
    Fixed: b/335301800
    Test: Added regression test (crash w/o the changes) + manually
    Change-Id: I43bfdca4c6cd4e192d5efd646d09c54f323c9a1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512032
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297306}

--- ash/wm/overview/overview_group_item.cc
+++ ash/wm/overview/overview_group_item.cc
@@ -74,6 +74,22 @@ OverviewGroupItem::OverviewGroupItem(const Windows& windows,
 
 OverviewGroupItem::~OverviewGroupItem() = default;
 
+void OverviewGroupItem::HideForSavedDeskLibrary(bool animate) {
+  for (const auto& item : overview_items_) {
+    item->HideForSavedDeskLibrary(animate);
+  }
+
+  OverviewItemBase::HideForSavedDeskLibrary(animate);
+}
+
+void OverviewGroupItem::RevertHideForSavedDeskLibrary(bool animate) {
+  for (const auto& item : overview_items_) {
+    item->RevertHideForSavedDeskLibrary(animate);
+  }
+
+  OverviewItemBase::RevertHideForSavedDeskLibrary(animate);
+}
+
 aura::Window* OverviewGroupItem::GetWindow() {
   // TODO(michelefan): `GetWindow()` will be replaced by `GetWindows()` in a
   // follow-up cl.
@@ -331,10 +347,6 @@ void OverviewGroupItem::OnStartingAnimationComplete() {
     item->OnStartingAnimationComplete();
   }
 }
-
-void OverviewGroupItem::HideForSavedDeskLibrary(bool animate) {}
-
-void OverviewGroupItem::RevertHideForSavedDeskLibrary(bool animate) {}
 
 void OverviewGroupItem::CloseWindows() {
   for (const auto& overview_item : overview_items_) {
