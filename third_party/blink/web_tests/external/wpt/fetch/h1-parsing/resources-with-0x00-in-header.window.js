d04393080f852968785c9b6079366e2010c033c5
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-05-07 09:51:12 +0000

    Import wpt@b9ab25d42550f950598505d9f7cf148cbba7a3a8
    https://github.com/web-platform-tests/wpt/compare/570f87231...b9ab25d42
    
    Using wpt-import in Chromium bc3f20a8eaa683731ea7d2df246018e53d837197.
    With Chromium commits locally applied on WPT:
      885f4460a1 "Exclude transparent text with decorations from being [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bingler@chromium.org:
      external/wpt/cookies
    japhet@chromium.org:
      external/wpt/fetch
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I01edce21976bc9a2b74f766d9f396340fd126e3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5517296
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1297383}

--- third_party/blink/web_tests/external/wpt/fetch/h1-parsing/resources-with-0x00-in-header.window.js
+++ third_party/blink/web_tests/external/wpt/fetch/h1-parsing/resources-with-0x00-in-header.window.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/common/get-host-info.sub.js
+
 async_test(t => {
   const script = document.createElement("script");
   t.add_cleanup(() => script.remove());
@@ -30,3 +32,7 @@ async_test(t => {
   img.onload = t.unreached_func();
   document.body.append(img);
 }, "Expect network error for image with 0x00 in a header");
+
+promise_test(async t => {
+  return promise_rejects_js(t, TypeError, fetch(get_host_info().HTTP_REMOTE_ORIGIN + "/fetch/h1-parsing/resources/blue-with-0x00-in-a-header.asis", {mode:"no-cors"}));
+}, "Expect network error for fetch with 0x00 in a header");
