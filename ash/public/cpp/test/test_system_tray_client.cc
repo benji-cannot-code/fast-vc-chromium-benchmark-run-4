a6edc93ff5be93c4f5876e776b14f24d63a4be78
Michael Checo <michaelcheco@google.com>
2024-05-07 02:40:31 +0000

    [WE] Show first-time connection notification for pointing sticks
    * The strings used in the notification are temporary and will be
      updated in a follow-up CL.
    
    Bug: b/329686601
    Change-Id: I379324f7510b8d53c80493532bd063778da10d4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513707
    Reviewed-by: Muhammad Hasan Khan <mhasank@chromium.org>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297268}

--- ash/public/cpp/test/test_system_tray_client.cc
+++ ash/public/cpp/test/test_system_tray_client.cc
@@ -215,4 +215,8 @@ void TestSystemTrayClient::ShowKeyboardSettings() {
   ++show_keyboard_settings_count_;
 }
 
+void TestSystemTrayClient::ShowPointingStickSettings() {
+  ++show_pointing_stick_settings_count_;
+}
+
 }  // namespace ash
