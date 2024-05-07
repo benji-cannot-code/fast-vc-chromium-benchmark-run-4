8a40ca975f2126947747be949a3863e4a16f28f0
Xi Han <hanxi@google.com>
2024-05-07 14:30:32 +0000

    [TabResumption] Log availability of salient image.
    In this CL. we log a histogram "MagicStack.Clank.TabResumption.IsSalientImageAvailable".
    
    Bug: 338642271
    Change-Id: I4c713f6014200ab4905532562bf20d2f7d4a97fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512509
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297471}

---
+++ chrome/browser/tab_resumption/junit/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleMetricsUtilsUnitTest.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tab_resumption;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.HistogramWatcher;
+
+/** Unit tests for {@link TabResumptionModuleMetricsUtils}. */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+public class TabResumptionModuleMetricsUtilsUnitTest {
+    @Test
+    @SmallTest
+    public void testRecordSalientImageAvailability() {
+        String histogramName = "MagicStack.Clank.TabResumption.IsSalientImageAvailable";
+
+        var histogramWatcher =
+                HistogramWatcher.newBuilder().expectBooleanRecord(histogramName, true).build();
+        TabResumptionModuleMetricsUtils.recordSalientImageAvailability(true);
+        histogramWatcher.assertExpected();
+
+        histogramWatcher =
+                HistogramWatcher.newBuilder().expectBooleanRecord(histogramName, false).build();
+        TabResumptionModuleMetricsUtils.recordSalientImageAvailability(false);
+        histogramWatcher.assertExpected();
+    }
+}
