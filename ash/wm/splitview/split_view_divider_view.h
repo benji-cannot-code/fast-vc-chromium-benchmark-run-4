78e4fca52519c8dfc5308b3fb63ea979210ba206
Michele Fan <michelefan@chromium.org>
2024-05-07 17:34:51 +0000

    snap-group: Make divider thinner on hover
    This cl makes visual changes for the divider on hover/drag so that it's
    less prominenet.
    
    This cl also improves the test coverage to test the divider default and
    enlarged visuals.
    
    This cl also `inline` the constants in `split_view_constants.h` to
    improve performance.
    
    Demo: http://b/339079972#comment#2
    Fixed: b/339079972
    Test: Modified existing tests to improve the coverage
    Change-Id: I380cbdc88b74fd18c2aa0b7bd6892fb3b0d89c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519631
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297607}

--- ash/wm/splitview/split_view_divider_view.h
+++ ash/wm/splitview/split_view_divider_view.h
@@ -6,9 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_SPLITVIEW_SPLIT_VIEW_DIVIDER_VIEW_H_
 #define ASH_WM_SPLITVIEW_SPLIT_VIEW_DIVIDER_VIEW_H_
 
+#include "ash/ash_export.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/views/view.h"
 #include "ui/views/view_targeter_delegate.h"
+
+namespace gfx {
+class Rect;
+}  // namespace gfx
 
 namespace ash {
 
@@ -58,7 +63,10 @@ class SplitViewDividerView : public views::View,
   bool DoesIntersectRect(const views::View* target,
                          const gfx::Rect& rect) const override;
 
-  IconButton* feedback_button_for_testing() const { return feedback_button_; }
+  ASH_EXPORT gfx::Rect GetHandlerViewBoundsInScreenForTesting() const;
+
+  DividerHandlerView* handler_view_for_testing() { return handler_view_; }
+  IconButton* feedback_button_for_testing() { return feedback_button_; }
 
  private:
   friend class SplitViewDivider;
