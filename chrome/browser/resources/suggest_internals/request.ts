e646080ed99d1fdb67dfa35f9dafe9a33218b51d
rbpotter <rbpotter@chromium.org>
2024-05-07 23:45:59 +0000

    WebUI: Use cr-collapse in various debug pages
    Polymer-based iron-collapse is being removed.
    
    Bug: 40943652
    Change-Id: Id9ea730ca3f622f7948ff4bb1cdb59bc9ca13931
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523673
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297854}

--- chrome/browser/resources/suggest_internals/request.ts
+++ chrome/browser/resources/suggest_internals/request.ts
@@ -4,12 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './icons.html.js';
+import '//resources/cr_elements/cr_collapse/cr_collapse.js';
 import '//resources/cr_elements/cr_expand_button/cr_expand_button.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
+import '//resources/cr_elements/cr_icon/cr_icon.js';
 import '//resources/cr_elements/cr_shared_style.css.js';
 import '//resources/cr_elements/cr_shared_vars.css.js';
-import '//resources/polymer/v3_0/iron-collapse/iron-collapse.js';
-import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
+import '//resources/cr_elements/icons_lit.html.js';
 
 import {sanitizeInnerHtml} from '//resources/js/parse_html_subset.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
