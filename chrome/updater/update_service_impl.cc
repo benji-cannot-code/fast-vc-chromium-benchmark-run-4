3807ca361b2fa5a86e9a6265fe0088af79c1f800
Joshua Pawlicki <waffles@chromium.org>
2024-05-07 20:24:23 +0000

    Updater: Allow self-uninstall if EULA is not accepted.
    This allows the updater to sense that all relevant apps have been
    uninstalled, and if so, to uninstall itself, without pinging the update
    server.
    
    Fixed: b/40195459
    Change-Id: Ie70933950dda77ce5245283fb9d863a934180d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522634
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297738}

--- chrome/updater/update_service_impl.cc
+++ chrome/updater/update_service_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/configurator.h"
 #include "chrome/updater/constants.h"
 #include "chrome/updater/persisted_data.h"
+#include "chrome/updater/remove_uninstalled_apps_task.h"
 #include "chrome/updater/update_service.h"
 #include "chrome/updater/update_service_impl_impl.h"
 #include "chrome/updater/util/util.h"
@@ -62,7 +63,8 @@ void UpdateServiceImpl::RunPeriodicTasks(base::OnceClosure callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!IsEulaAccepted() || IsOemMode()) {
     VLOG(1) << __func__ << " rejected (EULA required or OEM mode).";
-    std::move(callback).Run();
+    base::MakeRefCounted<RemoveUninstalledAppsTask>(config_, scope_)
+        ->Run(std::move(callback));
     return;
   }
   delegate_->RunPeriodicTasks(std::move(callback));
