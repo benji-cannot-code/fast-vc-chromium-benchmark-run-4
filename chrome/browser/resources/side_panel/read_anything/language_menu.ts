982e43bb17601db62a01321c079c4bd04c93b487
Yu Zhong <yuzhong@google.com>
2024-05-07 05:50:01 +0000

    [Read Aloud] Language menu styling polish
    Clean up colors in styling, works in chrome dark mode now.
    
    Bug: 332638153
    Fixed: 332638153
    Change-Id: I92187e265dc95edce2bd8ea7371e28cf71fbdc2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514701
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Yu Zhong <yuzhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297298}

--- chrome/browser/resources/side_panel/read_anything/language_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/language_menu.ts
@@ -3,7 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_hidden_style.css.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import '//resources/cr_elements/cr_icons.css.js';
 import '//resources/cr_elements/icons.html.js';
