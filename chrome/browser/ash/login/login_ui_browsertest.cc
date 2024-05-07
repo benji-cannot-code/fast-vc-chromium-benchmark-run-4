039a1a8c98c28dadfe71d79ae8164f1e02e72346
Denis Kuznetsov <antrim@chromium.org>
2024-05-07 13:36:21 +0000

    Fix some browser tests to avoid using StubAuthenticator
    Bug: b:325279785
    Change-Id: I0bf50fd782622659938aba23f6ec21d5b6b3b732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512612
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297445}

--- chrome/browser/ash/login/login_ui_browsertest.cc
+++ chrome/browser/ash/login/login_ui_browsertest.cc
@@ -195,7 +195,8 @@ class DisplayPasswordButtonTest : public LoginManagerTest {
     login_manager_mixin_.SkipPostLoginScreens();
 
     auto context = LoginManagerMixin::CreateDefaultUserContext(test_user);
-    login_manager_mixin_.LoginAndWaitForActiveSession(context);
+    login_manager_mixin_.LoginAsNewRegularUser(context);
+    login_manager_mixin_.WaitForActiveSession();
 
     ScreenLockerTester screen_locker_tester;
     screen_locker_tester.Lock();
@@ -337,7 +338,8 @@ class UserManagementDisclosureTest : public LoginManagerTest {
     login_manager_mixin_.SkipPostLoginScreens();
 
     auto context = LoginManagerMixin::CreateDefaultUserContext(test_user);
-    login_manager_mixin_.LoginAndWaitForActiveSession(context);
+    login_manager_mixin_.LoginAsNewRegularUser(context);
+    login_manager_mixin_.WaitForActiveSession();
 
     ScreenLockerTester screen_locker_tester;
     screen_locker_tester.Lock();
