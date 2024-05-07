96d5dd42fd666c28e641339fee268a3cae01e0d4
Hira Mahmood <hiramahmood@google.com>
2024-05-07 18:30:45 +0000

    Reland "[iOS] Limit parcel tracking feature to US only"
    This is a reland of commit 55e77469b8f42b9d28cf352dca470cb6e7e2af84
    which was reverted due to breaking a test.
    
    Will be submitted after internal CL that fixes the broken test:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/7260263
    
    Original change's description:
    > [iOS] Limit parcel tracking feature to US only
    >
    > This CL limits parcel tracking to US only by checking for the user's
    > permanent country.
    >
    > Will be submitted after https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/7229856
    >
    > Bug: None
    > Change-Id: I205bd1cc9a5c748f10225638aaf88fdaa2ea13a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5462903
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Reviewed-by: Sergio Collazos <sczs@chromium.org>
    > Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1293730}
    
    Bug: None
    Change-Id: I1c7a430366542e7ff32ac59bcb2281604ada2d1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519010
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297665}

--- ios/chrome/browser/parcel_tracking/parcel_tracking_util.mm
+++ ios/chrome/browser/parcel_tracking/parcel_tracking_util.mm
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/commerce/core/commerce_types.h"
 #import "components/commerce/core/shopping_service.h"
 #import "components/signin/public/base/consent_level.h"
+#import "components/variations/service/variations_service.h"
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
 #import "ios/chrome/browser/parcel_tracking/metrics.h"
 #import "ios/chrome/browser/parcel_tracking/parcel_tracking_prefs.h"
@@ -29,7 +30,11 @@ const CGFloat parcelLimit = 5;
 }  // namespace
 
 bool IsIOSParcelTrackingEnabled() {
-  return GetApplicationContext()->GetLocalState()->GetBoolean(
+  variations::VariationsService* variations_service =
+      GetApplicationContext()->GetVariationsService();
+  return variations_service &&
+         variations_service->GetStoredPermanentCountry() == "us" &&
+         GetApplicationContext()->GetLocalState()->GetBoolean(
              prefs::kIosParcelTrackingPolicyEnabled);
 }
 
--- ios/chrome/browser/parcel_tracking/parcel_tracking_util_unittest.mm
+++ ios/chrome/browser/parcel_tracking/parcel_tracking_util_unittest.mm
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "components/commerce/core/mock_shopping_service.h"
+#import "components/variations/service/variations_service.h"
+#import "components/variations/service/variations_service_client.h"
+#import "components/variations/synthetic_trial_registry.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
@@ -17,7 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity_manager.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
+#import "ios/chrome/test/ios_chrome_scoped_testing_variations_service.h"
+#import "ios/chrome/test/testing_application_context.h"
 #import "ios/web/public/test/web_task_environment.h"
+#import "services/network/test/test_network_connection_tracker.h"
 #import "testing/platform_test.h"
 
 class ParcelTrackingUtilTest : public PlatformTest {
@@ -76,6 +82,8 @@ class ParcelTrackingUtilTest : public PlatformTest {
 TEST_F(ParcelTrackingUtilTest, UserIsEligibleForPrompt) {
   SignIn();
   SetPromptDisplayedStatus(false);
+  IOSChromeScopedTestingVariationsService scoped_variations_service;
+  scoped_variations_service.Get()->OverrideStoredPermanentCountry("us");
   EXPECT_TRUE(IsUserEligibleParcelTrackingOptInPrompt(
       browser_state_->GetPrefs(), shopping_service_.get()));
 }
@@ -86,6 +94,8 @@ TEST_F(ParcelTrackingUtilTest, NotSignedIn) {
   SignOut();
   SetPromptDisplayedStatus(false);
   shopping_service_->SetIsParcelTrackingEligible(false);
+  IOSChromeScopedTestingVariationsService scoped_variations_service;
+  scoped_variations_service.Get()->OverrideStoredPermanentCountry("us");
   EXPECT_FALSE(IsUserEligibleParcelTrackingOptInPrompt(
       browser_state_->GetPrefs(), shopping_service_.get()));
 }
@@ -93,6 +103,17 @@ TEST_F(ParcelTrackingUtilTest, NotSignedIn) {
 // Tests that IsUserEligibleParcelTrackingOptInPrompt returns false when the
 // user has seen the prompt.
 TEST_F(ParcelTrackingUtilTest, UserHasSeenPrompt) {
+  SignIn();
+  SetPromptDisplayedStatus(true);
+  IOSChromeScopedTestingVariationsService scoped_variations_service;
+  scoped_variations_service.Get()->OverrideStoredPermanentCountry("us");
+  EXPECT_FALSE(IsUserEligibleParcelTrackingOptInPrompt(
+      browser_state_->GetPrefs(), shopping_service_.get()));
+}
+
+// Tests that IsUserEligibleParcelTrackingOptInPrompt returns false when the
+// permanent country is not set to US.
+TEST_F(ParcelTrackingUtilTest, CountryNotUS) {
   SignIn();
   SetPromptDisplayedStatus(true);
   EXPECT_FALSE(IsUserEligibleParcelTrackingOptInPrompt(
