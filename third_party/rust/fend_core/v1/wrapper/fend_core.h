1a70bf4311ffe4c76603a7b1da80c28973c8964a
Jing Wang <jiwan@chromium.org>
2024-05-07 00:42:03 +0000

    Reland "picker: display at most 2dp for math results"
    This reverts commit 04e683da32061cd8f4ce4bada5088b9f95e8ee15.
    
    Reason for revert: fixed the msan failure with workaround
    
    Now we only add "in 2dp" (together with @noapprox) when there are
    valid results returned for the raw query. In this way, we won't
    trigger msan issue in non-math-related tests.
    
    Original change's description:
    > Revert "picker: display at most 2dp for math results"
    >
    > This reverts commit 3e9499d6167099d958601243d1fb10e7792883f8.
    >
    > Reason for revert: Crashes on MSAN builds: See https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/42903/overview for the first crash.
    >
    >
    > Original change's description:
    > > picker: display at most 2dp for math results
    > >
    > > Bug: b:337112140
    > > Change-Id: I763e99bb6a8a05b42e17e4c0cda221b29b937a1c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505069
    > > Reviewed-by: Darren Shen <shend@chromium.org>
    > > Commit-Queue: Jing Wang <jiwan@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1294772}
    >
    > Bug: b:337112140
    > Change-Id: I42729cbf0c27ccb582fa02cc26781ed3ba859abe
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5504830
    > Commit-Queue: Ian Clelland <iclelland@chromium.org>
    > Owners-Override: Ian Clelland <iclelland@chromium.org>
    > Auto-Submit: Ian Clelland <iclelland@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1294840}
    
    Bug: b:337112140
    Change-Id: Ie5ee91a1b8ee916510cada2eb295399d348fb273
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508095
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297223}

--- third_party/rust/fend_core/v1/wrapper/fend_core.h
+++ third_party/rust/fend_core/v1/wrapper/fend_core.h
@@ -12,9 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace fend_core {
 
 // Try evaluating the query string with fend library, within `timeout_in_ms` ms.
-// Returns the result string if the evaluation succeeded, or std::nullopt if failed.
-// If `timeout_in_ms` = 0, there is no timeout.
-std::optional<std::string> evaluate(std::string_view query, unsigned int timeout_in_ms = 100);
+// Returns the result string if the evaluation succeeded, or std::nullopt if
+// failed. If `timeout_in_ms` = 0, there is no timeout.
+std::optional<std::string> evaluate(std::string_view query,
+                                    unsigned int timeout_in_ms = 100);
 
 }  // namespace fend_core
 
--- third_party/rust/fend_core/v1/wrapper/fend_core_unittest.cc
+++ third_party/rust/fend_core/v1/wrapper/fend_core_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
-#include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace fend_core {
 namespace {
@@ -32,7 +32,7 @@ TEST(MAYBE_FendCoreTest, SimpleMath) {
 
 TEST(MAYBE_FendCoreTest, NoApproxString) {
   std::optional<std::string> result = evaluate("1/3", /*timeout_in_ms=*/0);
-  EXPECT_THAT(result, Optional(Eq("0.3333333333")));
+  EXPECT_THAT(result, Optional(Eq("0.33")));
 }
 
 TEST(MAYBE_FendCoreTest, FiltersTrivialResult) {
@@ -51,8 +51,9 @@ TEST(MAYBE_FendCoreTest, FiltersLambdaResults) {
 }
 
 TEST(MAYBE_FendCoreTest, UnitConversion) {
-  std::optional<std::string> result = evaluate("2 miles in meters", /*timeout_in_ms=*/0);
-  EXPECT_THAT(result, Optional(Eq("3218.688 meters")));
+  std::optional<std::string> result =
+      evaluate("2 miles in meters", /*timeout_in_ms=*/0);
+  EXPECT_THAT(result, Optional(Eq("3218.68 meters")));
 }
 
 // This test passes MSan as it does not allocate on the Rust side. However, we
@@ -63,7 +64,8 @@ TEST(MAYBE_FendCoreTest, HandlesInvalidInput) {
 }
 
 TEST(MAYBE_FendCoreTest, CanTimeout) {
-  std::optional<std::string> result = evaluate("10**100000", /*timeout_in_ms=*/500);
+  std::optional<std::string> result =
+      evaluate("10**100000", /*timeout_in_ms=*/500);
   EXPECT_EQ(result, std::nullopt);
 }
 
