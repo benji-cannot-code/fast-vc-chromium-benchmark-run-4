9dff30869f694ce64f3a668d8bf90246bb8be474
Bo Majewski <majewski@chromium.org>
2024-05-07 01:28:46 +0000

    [Files]: Changes GetAugmentedTermId to take a term.
    Changes the signature of GetAugmentedTermId to just take const Term&.
    Adds const to GetUrlId(). Adds a way to get the original text
    (std::u16stirng) from Term. Adjusts tests to these changes. Adds a new
    test to exercise GetAugmentedTermIdsForUrl.
    
    This makes the API more resilient, as it is all too easy to use term IDs
    where augmented term IDs should be used. This fixes errors in tests,
    where this mixup was noticed.
    
    Bug: b:327534824
    Change-Id: If9fa6f551048095ceb77799fdb443731fe96a7a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513848
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297244}

--- chrome/browser/ash/file_manager/indexing/term.cc
+++ chrome/browser/ash/file_manager/indexing/term.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace file_manager {
 
 Term::Term(const std::string& field, const std::u16string& text)
-    : field_(field), text_(base::UTF16ToUTF8((text))) {}
+    : field_(field), text_(text), text_bytes_(base::UTF16ToUTF8(text)) {}
 
 Term::~Term() = default;
 
