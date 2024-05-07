b69bedd973cd76e57059b47cb1bd1b4120a78a40
Hailey Wang <haileywang@google.com>
2024-05-07 22:20:13 +0000

    [Data Sharing] Add data sharing service functions for url interception
    Add data sharing service functions for when an url is intercepted by
    the navigation throttle.
    
    Bug: b/336873603
    Change-Id: I8cf472374338a895f8a0896ebc5fd86c9eda0bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5486107
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297801}

--- chrome/browser/data_sharing/data_sharing_navigation_throttle.h
+++ chrome/browser/data_sharing/data_sharing_navigation_throttle.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DATA_SHARING_DATA_SHARING_NAVIGATION_THROTTLE_H_
 #define CHROME_BROWSER_DATA_SHARING_DATA_SHARING_NAVIGATION_THROTTLE_H_
 
+#include "components/data_sharing/public/data_sharing_service.h"
 #include "content/public/browser/navigation_throttle.h"
 
 namespace content {
