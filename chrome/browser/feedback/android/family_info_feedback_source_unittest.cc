f16f780c15893de686eb688259948006066a8396
Nohemi Fernandez <fernandex@chromium.org>
2024-05-07 10:41:33 +0000

    Ensure consistency of family info feedback FAMILY_MEMBER_ROLE tag across platforms.
    Bug: N/A
    Change-Id: I2fb831592203fd06e3ec592149260b83daef1597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506639
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297394}

--- chrome/browser/feedback/android/family_info_feedback_source_unittest.cc
+++ chrome/browser/feedback/android/family_info_feedback_source_unittest.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/browser/supervised_user_service.h"
 #include "components/supervised_user/core/browser/supervised_user_url_filter.h"
 #include "components/supervised_user/core/browser/supervised_user_utils.h"
+#include "components/supervised_user/core/common/supervised_user_constants.h"
 #include "content/public/test/browser_task_environment.h"
 #include "google_apis/gaia/google_service_auth_error.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -40,7 +41,6 @@ namespace chrome::android {
 namespace {
 
 const char kTestEmail[] = "test@gmail.com";
-const char kFeedbackTagFamilyMemberRole[] = "Family_Member_Role";
 const char kFeedbackTagParentalControlSitesChild[] =
     "Parental_Control_Sites_Child";
 
@@ -218,7 +218,7 @@ class FamilyInfoFeedbackSourceTest
             base::android::JavaParamRef<jobject>(env_,
                                                  j_feedback_source_.obj()),
             base::android::ConvertUTF8ToJavaString(
-                env_, kFeedbackTagFamilyMemberRole));
+                env_, supervised_user::kFamilyMemberRoleFeedbackTag));
     return base::android::ConvertJavaStringToUTF8(env_, j_value);
   }
 
