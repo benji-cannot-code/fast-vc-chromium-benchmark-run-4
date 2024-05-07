357806afa84d344bbdb849604113b8f6b4eae336
Brittany Hartmire <bhartmire@google.com>
2024-05-07 22:01:42 +0000

    [QuickStart]: Increase advertisement metric duration range
    UmaHistogramTimes sets default max duration of 10s, which is not
    sufficient for these metrics.
    
    Test: existing tests pass
    Change-Id: I1165729d3dfeb40951f635083e0075bce83a4d95
    Bug: b/339236651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522349
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297793}

--- chromeos/ash/components/quick_start/quick_start_metrics.cc
+++ chromeos/ash/components/quick_start/quick_start_metrics.cc
@@ -503,8 +503,8 @@ void QuickStartMetrics::RecordFastPairAdvertisementEnded(
 
   if (succeeded) {
     CHECK(!error_code.has_value());
-    base::UmaHistogramTimes(kFastPairAdvertisementEndedDurationHistogramName,
-                            duration);
+    base::UmaHistogramMediumTimes(
+        kFastPairAdvertisementEndedDurationHistogramName, duration);
   } else {
     CHECK(error_code.has_value());
     base::UmaHistogramEnumeration(
@@ -549,7 +549,7 @@ void QuickStartMetrics::RecordNearbyConnectionsAdvertisementEnded(
 
   if (succeeded) {
     CHECK(!error_code.has_value());
-    base::UmaHistogramTimes(
+    base::UmaHistogramMediumTimes(
         kNearbyConnectionsAdvertisementEndedDurationHistogramName, duration);
   } else {
     CHECK(error_code.has_value());
