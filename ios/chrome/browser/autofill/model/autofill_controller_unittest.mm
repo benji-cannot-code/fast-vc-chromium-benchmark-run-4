f9caba820a0412daf5c19f646992cc039400d925
Jihad Hanna <jihadghanna@google.com>
2024-05-07 14:31:47 +0000

    Remove duplicate histogram
    This histogram is removed in favor of the newly added
    Autofill.SuggestionsCount.Address
    
    Bug: 324029575
    Change-Id: If15558bb0db710471451568bdb09b442001542de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5517638
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297473}

--- ios/chrome/browser/autofill/model/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/model/autofill_controller_unittest.mm
@@ -533,7 +533,7 @@ TEST_F(AutofillControllerTest, ProfileSuggestions) {
   ResetWaitForSuggestionRetrieval();
   web::test::ExecuteJavaScript(@"document.forms[0].name.focus()", web_state());
   WaitForSuggestionRetrieval(/*wait_for_trigger=*/YES);
-  ExpectMetric("Autofill.AddressSuggestionsCount", 1);
+  ExpectMetric("Autofill.SuggestionsCount.Address", 1);
   EXPECT_EQ(1U, [suggestion_controller() suggestions].count);
   FormSuggestion* suggestion = [suggestion_controller() suggestions][0];
   EXPECT_NSEQ(@"Homer Simpson", suggestion.value);
@@ -554,7 +554,7 @@ TEST_F(AutofillControllerTest, ProfileSuggestionsTwoAnonymousForms) {
   ResetWaitForSuggestionRetrieval();
   web::test::ExecuteJavaScript(@"document.forms[0].name.focus()", web_state());
   WaitForSuggestionRetrieval(/*wait_for_trigger=*/YES);
-  ExpectMetric("Autofill.AddressSuggestionsCount", 1);
+  ExpectMetric("Autofill.SuggestionsCount.Address", 1);
   EXPECT_EQ(1U, [suggestion_controller() suggestions].count);
   FormSuggestion* suggestion = [suggestion_controller() suggestions][0];
   EXPECT_NSEQ(@"Homer Simpson", suggestion.value);
@@ -574,7 +574,7 @@ TEST_F(AutofillControllerTest, ProfileSuggestionsFromSelectField) {
   ResetWaitForSuggestionRetrieval();
   web::test::ExecuteJavaScript(@"document.forms[0].state.focus()", web_state());
   WaitForSuggestionRetrieval(/*wait_for_trigger=*/YES);
-  ExpectMetric("Autofill.AddressSuggestionsCount", 1);
+  ExpectMetric("Autofill.SuggestionsCount.Address", 1);
   EXPECT_EQ(1U, [suggestion_controller() suggestions].count);
   FormSuggestion* suggestion = [suggestion_controller() suggestions][0];
   EXPECT_NSEQ(@"IL", suggestion.value);
@@ -622,7 +622,7 @@ TEST_F(AutofillControllerTest, MultipleProfileSuggestions) {
   ResetWaitForSuggestionRetrieval();
   web::test::ExecuteJavaScript(@"document.forms[0].name.focus()", web_state());
   WaitForSuggestionRetrieval(/*wait_for_trigger=*/YES);
-  ExpectMetric("Autofill.AddressSuggestionsCount", 2);
+  ExpectMetric("Autofill.SuggestionsCount.Address", 2);
   EXPECT_EQ(2U, [suggestion_controller() suggestions].count);
 }
 
