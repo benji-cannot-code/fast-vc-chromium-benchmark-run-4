d7d55761a1465467df06422884bff072dbffc173
Jose Dapena Paz <jdapena@igalia.com>
2024-05-07 08:48:24 +0000

    IWYU: missing include for std::vector usage in poison_metadata_recorder.h
    Bug: 41455655
    Change-Id: I86804cfff5753a7e1bfb52f93439f229cd8c7095
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515231
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1297364}

--- components/gwp_asan/client/lightweight_detector/poison_metadata_recorder.h
+++ components/gwp_asan/client/lightweight_detector/poison_metadata_recorder.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <atomic>
 #include <memory>
+#include <vector>
 
 #include "base/export_template.h"
 #include "base/gtest_prod_util.h"
