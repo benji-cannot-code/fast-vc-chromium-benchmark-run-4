531e3b462d8795cae9c413bb586097e0a87310f4
Louis Romero <lpromero@google.com>
2024-05-07 22:02:42 +0000

    Don't add invalid index when creating a group
    This CL avoids adding the invalid index if a WebState identifier doesn't
    match any current WebStateList index.
    
    Fixed: 336766886
    Change-Id: I8655611d56fdf1a1b25f8346b8d8592b5c773924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522295
    Commit-Queue: Sergio Collazos <sczs@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297794}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/create_tab_group_mediator.mm
@@ -144,9 +144,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       int index = GetWebStateIndex(_webStateList, WebStateSearchCriteria{
                                                       .identifier = identifier,
                                                   });
-      tabIndexes.insert(index);
+      if (index != WebStateList::kInvalidIndex) {
+        tabIndexes.insert(index);
+      }
     }
-    _webStateList->CreateGroup(tabIndexes, visualData);
+    if (!tabIndexes.empty()) {
+      _webStateList->CreateGroup(tabIndexes, visualData);
+    }
   }
   completion();
 }
