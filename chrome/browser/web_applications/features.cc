540e031f498e1bf00b1087bf8a57c1a2199dab17
Glen Robertson <glenrob@chromium.org>
2024-05-07 02:08:58 +0000

    WebApps: Clean up {SyncOnly,}SeparateUserDisplayModeForCrOS flags
    This feature launched in M122 and hasn't seen any issues. Cleaning up
    the flags to simplify the code.
    
    POC for this change: dmurph@ if glenrob@ is unavailable.
    
    Bug: b/338505202
    Fixed: b/338505202
    Change-Id: I4822d8ba91e8b371869c2963f4f46a2173202ccc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514226
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Glen Robertson <glenrob@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297258}

--- chrome/browser/web_applications/features.cc
+++ chrome/browser/web_applications/features.cc
@@ -7,14 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
-BASE_FEATURE(kSeparateUserDisplayModeForCrOS,
-             "SeparateUserDisplayModeForCrOS",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSyncOnlySeparateUserDisplayModeForCrOS,
-             "SyncOnlySeparateUserDisplayModeForCrOS",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 #if BUILDFLAG(IS_CHROMEOS)
 BASE_FEATURE(kUserDisplayModeSyncBrowserMitigation,
              "UserDisplayModeSyncBrowserMitigation",
