0ce7a4a86aec2824b465d6521ab717326ef5e3d1
Daniel Murphy <dmurph@chromium.org>
2024-05-07 02:22:15 +0000

    [PWA] Add flag to enable scope extensions and link capturing together.
    This patch adds a flag to enable link capturing with scope extensions,
    and updates some existing tests that were CrOS only to now run on WML
    as well.
    
    Bug: 338640785,b:294079334
    Change-Id: Ib89c99e3f8fa7bf7bfab8d9e9fe656950a5c49b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513279
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297265}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4675,6 +4675,9 @@ extern const char kExplicitBrowserSigninUIOnDesktopDescription[];
 
 extern const char kDesktopPWAsUserLinkCapturingName[];
 extern const char kDesktopPWAsUserLinkCapturingDescription[];
+
+extern const char kDesktopPWAsUserLinkCapturingScopeExtensionsName[];
+extern const char kDesktopPWAsUserLinkCapturingScopeExtensionsDescription[];
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(ENABLE_HLS_DEMUXER)
--- chrome/common/chrome_features.h
+++ chrome/common/chrome_features.h
@@ -178,6 +178,9 @@ BASE_DECLARE_FEATURE(kDesktopPWAsLinkCapturing);
 COMPONENT_EXPORT(CHROME_FEATURES)
 extern const base::FeatureParam<bool> kLinksCapturedByDefault;
 
+COMPONENT_EXPORT(CHROME_FEATURES)
+BASE_DECLARE_FEATURE(kDesktopPWAsLinkCapturingWithScopeExtensions);
+
 // Default amount of days after which the global link capturing IPH guardrails
 // are cleared from storage.
 inline constexpr int kTotalDaysToStoreLinkCapturingIPHGuardrails = 30;
