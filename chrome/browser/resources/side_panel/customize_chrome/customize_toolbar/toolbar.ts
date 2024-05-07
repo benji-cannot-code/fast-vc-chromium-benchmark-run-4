0f65562eb462a17a31f7dfa997ba20d09c0fc57d
dpapad <dpapad@chromium.org>
2024-05-07 19:57:51 +0000

    SidePanel: Migrate c/b/r/side_panel/customize_chrome to Lit, part 9.
    In this part migrating customize_toolbar/toolbar.ts
    
    Bug: 40943652
    Change-Id: If8b548ba8d749f102d19252b340370da5936bac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523505
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297724}

--- chrome/browser/resources/side_panel/customize_chrome/customize_toolbar/toolbar.ts
+++ chrome/browser/resources/side_panel/customize_chrome/customize_toolbar/toolbar.ts
@@ -4,16 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
-import 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_shared_style.css.js';
 
-import type {SpHeadingElement} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
 import type {CrToggleElement} from '//resources/cr_elements/cr_toggle/cr_toggle.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import type {SpHeadingElement} from 'chrome://customize-chrome-side-panel.top-chrome/shared/sp_heading.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import type {CustomizeToolbarHandlerInterface} from '../customize_toolbar.mojom-webui.js';
 
 import {CustomizeToolbarApiProxy} from './customize_toolbar_api_proxy.js';
-import {getTemplate} from './toolbar.html.js';
+import {getCss} from './toolbar.css.js';
+import {getHtml} from './toolbar.html.js';
 
 export interface ToolbarElement {
   $: {
@@ -23,17 +23,17 @@ export interface ToolbarElement {
   };
 }
 
-export class ToolbarElement extends PolymerElement {
+export class ToolbarElement extends CrLitElement {
   static get is() {
     return 'customize-chrome-toolbar';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
-    return {};
+  override render() {
+    return getHtml.bind(this)();
   }
 
   private handler_: CustomizeToolbarHandlerInterface;
@@ -60,7 +60,7 @@ export class ToolbarElement extends PolymerElement {
     const callbackRouter =
         CustomizeToolbarApiProxy.getInstance().callbackRouter;
     this.listenerIds_.push(callbackRouter.setActionPinned.addListener(
-        this.setActionPinned.bind(this)));
+        this.setActionPinned_.bind(this)));
   }
 
   override disconnectedCallback() {
@@ -68,21 +68,22 @@ export class ToolbarElement extends PolymerElement {
     const callbackRouter =
         CustomizeToolbarApiProxy.getInstance().callbackRouter;
     this.listenerIds_.forEach(id => callbackRouter.removeListener(id));
+    this.listenerIds_ = [];
   }
 
   focusOnBackButton() {
     this.$.heading.getBackButton().focus();
   }
 
-  private onBackClick_() {
-    this.dispatchEvent(new Event('back-click'));
+  protected onBackClick_() {
+    this.fire('back-click');
   }
 
-  private onActionToggle_(event: CustomEvent) {
+  protected onActionToggle_(event: CustomEvent<boolean>) {
     this.handler_.pinAction(this.actionId_, event.detail);
   }
 
-  private setActionPinned(actionId: number, pinned: boolean) {
+  private setActionPinned_(actionId: number, pinned: boolean) {
     if (actionId !== this.actionId_) {
       return;
     }
