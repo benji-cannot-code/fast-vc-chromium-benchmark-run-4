ca7bb566e88678ae74681a0054c97ffced7efc82
Peter McNeeley <petermcneeley@chromium.org>
2024-05-07 14:34:29 +0000

    Readonly begin access for dawn ozone representation
    Bug: b/332832083
    Change-Id: Icc61663f12585cf74fedea210fe8566bfc5f7eb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505204
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297475}

--- gpu/command_buffer/service/shared_image/dawn_ozone_image_representation.h
+++ gpu/command_buffer/service/shared_image/dawn_ozone_image_representation.h
@@ -53,6 +53,7 @@ class DawnOzoneImageRepresentation : public DawnImageRepresentation {
   std::vector<wgpu::TextureFormat> view_formats_;
   scoped_refptr<gfx::NativePixmap> pixmap_;
   wgpu::Texture texture_;
+  bool is_readonly_ = false;
 };
 
 }  // namespace gpu
