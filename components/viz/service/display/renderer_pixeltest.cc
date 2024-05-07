f42dd9c511648798fe16b1c68188e2ff3e9534e7
Saifuddin Hitawala <hitawala@chromium.org>
2024-05-07 20:10:52 +0000

    Multiplanar: Skip viz unittest if WritePixelYUV is not enabled
    Bug: 338600935, 338650865
    Change-Id: Id4a1a70c2f5069356fca369f37a74db1cc7f4a2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521781
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297733}

--- components/viz/service/display/renderer_pixeltest.cc
+++ components/viz/service/display/renderer_pixeltest.cc
@@ -2180,6 +2180,9 @@ TEST_P(IntersectingQuadPixelTest, RenderPassQuads) {
 }
 
 TEST_P(IntersectingMultiplanarVideoQuadPixelTest, YUVVideoQuads) {
+  if (!media::IsWritePixelsYUVEnabled()) {
+    GTEST_SKIP() << "Skip this test if UseWritePixelsYUV is disabled";
+  }
   this->SetupQuadStateTransformsAndRenderPass();
   gfx::Rect inner_rect(
       ((this->quad_rect_.x() + (this->quad_rect_.width() / 4)) & ~0xF),
