2174b6bcc8f9b4d6a3911288d2c32235d94ae9f8
Alexander Hendrich <hendrich@chromium.org>
2024-05-07 16:09:09 +0000

    Add debug logging for b/333460350
    Temporarily add some logging for b/333460350. Plan is to only leave
    these logs in for a day in order to better debug the flaky test
    failures.
    
    Bug: b:333460350
    Change-Id: I5a3823edc550857add2900c9276868cfd836ae9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512707
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Alexander Hendrich <hendrich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297533}

--- components/policy/core/browser/policy_pref_mapping_test.cc
+++ components/policy/core/browser/policy_pref_mapping_test.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
+#include "build/buildflag.h"
 #include "build/chromeos_buildflags.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/schema.h"
@@ -719,6 +720,11 @@ void VerifyPolicyToPrefMappings(const base::FilePath& test_case_dir,
           continue;
         }
 
+#if BUILDFLAG(IS_WIN)
+        // TODO(b/333460350) Remove logs here added temporarily for debugging.
+        LOG(INFO) << policy_name << " # " << idx << " # " << i;
+#endif
+
         for (const auto& pref_case : pref_mapping->prefs()) {
           SCOPED_TRACE(::testing::Message() << "Pref: " << pref_case->pref());
           PrefService* prefs =
@@ -784,6 +790,11 @@ void VerifyPolicyToPrefMappings(const base::FilePath& test_case_dir,
       }
     }
   }
+
+#if BUILDFLAG(IS_WIN)
+  // TODO(b/333460350) Remove logs here added temporarily for debugging.
+  LOG(INFO) << "Done";
+#endif
 }
 
 }  // namespace policy
