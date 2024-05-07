4eda5a0c8408c0ff57e1d5348b825692cd68aec8
Tina Wang <tinazwang@google.com>
2024-05-07 06:24:26 +0000

    [Notif] Add params for notification feature flag for experiments
    Adding params for three registration-only groups.
    
    Bug: b/338254318
    Change-Id: Ia372e03755b94b0267e9c5db8a5462e2b1c79b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506736
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297313}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -253,6 +253,15 @@ const FeatureEntry::FeatureParam kContentPushNotificationsEnabledProvisional[] =
 const FeatureEntry::FeatureParam
     kContentPushNotificationsEnabledProvisionalBypass[] = {
         {kContentPushNotificationsExperimentType, "4"}};
+const FeatureEntry::FeatureParam
+    kContentPushNotificationsPromoRegistrationOnly[] = {
+        {kContentPushNotificationsExperimentType, "5"}};
+const FeatureEntry::FeatureParam
+    kContentPushNotificationsProvisionalRegistrationOnly[] = {
+        {kContentPushNotificationsExperimentType, "6"}};
+const FeatureEntry::FeatureParam
+    kContentPushNotificationsSetUpListRegistrationOnly[] = {
+        {kContentPushNotificationsExperimentType, "7"}};
 
 const FeatureEntry::FeatureVariation kContentPushNotificationsVariations[] = {
     {"Promo", kContentPushNotificationsEnabledPromo,
@@ -263,7 +272,15 @@ const FeatureEntry::FeatureVariation kContentPushNotificationsVariations[] = {
      std::size(kContentPushNotificationsEnabledProvisional), nullptr},
     {"Provisional Ignore Conditions",
      kContentPushNotificationsEnabledProvisionalBypass,
-     std::size(kContentPushNotificationsEnabledProvisionalBypass), nullptr}};
+     std::size(kContentPushNotificationsEnabledProvisionalBypass), nullptr},
+    {"Promo Registeration Only", kContentPushNotificationsPromoRegistrationOnly,
+     std::size(kContentPushNotificationsPromoRegistrationOnly), nullptr},
+    {"Provisional Notification Registeration Only",
+     kContentPushNotificationsProvisionalRegistrationOnly,
+     std::size(kContentPushNotificationsProvisionalRegistrationOnly), nullptr},
+    {"Set up list Registeration Only",
+     kContentPushNotificationsSetUpListRegistrationOnly,
+     std::size(kContentPushNotificationsSetUpListRegistrationOnly), nullptr}};
 
 const FeatureEntry::FeatureParam kFeedHeaderSettingDisabledStickyHeader[] = {
     {kDisableStickyHeaderForFollowingFeed, "true"}};
