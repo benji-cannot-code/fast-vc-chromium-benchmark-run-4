31de9fe059dc5d55199221458308bcf4f0f8e012
Jong Ahn <jongahn@google.com>
2024-05-07 00:43:47 +0000

    [Structured Metrics]: Remove old nearby share feature flag.
    It has been some time since this has launched so removing feature flag.
    
    Bug: 336801244
    Change-Id: I754b523d7d29b4147e621e2a5fd4f2d578d66665
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512595
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Jong Ahn <jongahn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297226}

--- components/metrics/structured/structured_metrics_features.cc
+++ components/metrics/structured/structured_metrics_features.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metrics::structured {
 
-BASE_FEATURE(kNearbyShareMetrics,
-             "NearbyShareMetrics",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kEnabledStructuredMetricsService,
              "EnableStructuredMetricsService",
              base::FEATURE_ENABLED_BY_DEFAULT);
