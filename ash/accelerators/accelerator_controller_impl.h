a59e3d337a24dca018abe086c78780d843c53f18
YuhanYang <yyhyyh@google.com>
2024-05-07 07:20:04 +0000

    Shortcut: Add prefs to display caps lock blocked nudge
    - Add prefs to record nudge shown count and last shown time for the
    capsLock blocked by the function key. If the nudge is shown more than
    three times or within the last 24 hours, won't show the nudge anymore.
    
    Bug: b/216049298
    Test: ash_unittests
    Change-Id: I3c619790325065de1fe8ca7a8498265887935290
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513682
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297335}

--- ash/accelerators/accelerator_controller_impl.h
+++ ash/accelerators/accelerator_controller_impl.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/ui/accessibility_confirmation_dialog.h"
 #include "ash/ash_export.h"
 #include "ash/public/cpp/accelerators.h"
+#include "ash/system/input_device_settings/input_device_settings_notification_controller.h"
 #include "base/files/file_path.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
@@ -294,6 +295,9 @@ class ASH_EXPORT AcceleratorControllerImpl
   // Please refer to the comment on |DebugInterfaceAsh| for the lifetime of this
   // pointer.
   raw_ptr<DebugDelegate> debug_delegate_ = nullptr;
+
+  std::unique_ptr<InputDeviceSettingsNotificationController>
+      notification_controller_;
 };
 
 }  // namespace ash
