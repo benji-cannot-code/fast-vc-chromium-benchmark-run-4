562c21fabcfb90eeffac929789e618e590fbf8f7
Avi Drissman <avi@chromium.org>
2024-05-08 21:27:08 +0000

    Revert "Make LoginManagerMixin::LoginAsNew*User use FUDAC by default"
    This reverts commit 43261a2b7d5a3f5b5ce8d95f8d072a782ff09118.
    
    Reason for revert:
    
    Starting with https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/41422/overview the OnboardingTest.OnboardingUserActivityRegularUser test has been failing. Reverting due to suspicions that this might be the CL causing it.
    
    Also reverting https://chromium-review.googlesource.com/c/chromium/src/+/5522161 as it’s not clear which is the cause.
    
    Original change's description:
    > Make LoginManagerMixin::LoginAsNew*User use FUDAC by default
    >
    > This is a reland of crrev.com/c/5459252. Test that caused a revert
    > was incorrectly waiting for profile loading instead of session start,
    > and in branded build one of OOBE screens was not automatically skipped.
    >
    >
    > This change ensures that all invariants are correctly satisfied
    > for subsequent onboarding process (e.g. that there is a correctly
    > established authsession).
    >
    > Also:
    >  * updated PasswordSelectionScreen to support repeated MaybeSkip;
    >  * rewrote RecoveryEligibilityScreen test to use actual
    >    login/policies;
    >  * skip SyncConsentTest*.AbortedTest if local passwords are enabled,
    >    as with local passwords feature aborting at this stage would
    >    trigger user cleanup;
    >
    > Bug: b:325279785
    > Change-Id: I249e337f4858e3362f5f0335c1a6c7e7213933c8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5525633
    > Reviewed-by: Istvan Nagy <iscsi@google.com>
    > Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1298126}
    
    Bug: b:325279785
    Change-Id: I3ed65b6e8af0cf867f9629b490a7907a635b6515
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526040
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1298362}

--- chrome/browser/ash/login/screens/sync_consent_browsertest.cc
+++ chrome/browser/ash/login/screens/sync_consent_browsertest.cc
@@ -371,11 +371,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentTest, SkippedSyncDisabledByPolicy) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentTest, PRE_AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    // With local passwords feature aborting at this stage would
-    // trigger user cleanup and remove all user information.
-    GTEST_SKIP();
-  }
   LoginAndShowSyncConsentScreenWithCapability();
   WaitForScreenShown();
   test::OobeJS().CreateVisibilityWaiter(true, {kSyncConsent})->Wait();
@@ -383,12 +378,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentTest, PRE_AbortedSetup) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentTest, AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    // With local passwords feature aborting at this stage would
-    // trigger user cleanup and remove all user information,
-    // so this test makes no sense.
-    GTEST_SKIP();
-  }
   EXPECT_EQ(session_manager::SessionState::LOGIN_PRIMARY,
             session_manager::SessionManager::Get()->session_state());
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
@@ -788,9 +777,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, Decline) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, PRE_AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   LoginAndShowSyncConsentScreenWithCapability();
   WaitForScreenShown();
   test::OobeJS().CreateVisibilityWaiter(true, {kSyncConsent})->Wait();
@@ -798,9 +784,6 @@ IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, PRE_AbortedSetup) {
 }
 
 IN_PROC_BROWSER_TEST_F(SyncConsentMinorModeTest, AbortedSetup) {
-  if (ash::features::AreLocalPasswordsEnabledForConsumers()) {
-    GTEST_SKIP();
-  }
   EXPECT_EQ(session_manager::SessionState::LOGIN_PRIMARY,
             session_manager::SessionManager::Get()->session_state());
   Profile* profile = ProfileManager::GetPrimaryUserProfile();
