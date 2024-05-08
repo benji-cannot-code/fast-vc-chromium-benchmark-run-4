0c940974707b3a0433169617c3a02043dcb9579c
Jihad Hanna <jihadghanna@google.com>
2024-05-08 16:09:41 +0000

    Replace SuggestionSuggestion by Suggestion
    This is a small cleanup following the renaming of PopupItemId to
    SuggestionType
    
    Bug: 40100455
    Change-Id: I080a9caabd538c4df14ec2a2e4d5fc1747eab0e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526654
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298150}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -335,8 +335,7 @@ void LogTimeDelayForSingleFieldFormFill(
       delay);
 }
 
-FillDataType GetEventTypeFromSingleFieldSuggestionSuggestionType(
-    SuggestionType type) {
+FillDataType GetEventTypeFromSingleFieldSuggestionType(SuggestionType type) {
   switch (type) {
     case SuggestionType::kAutocompleteEntry:
       return FillDataType::kSingleFieldFormFillerAutocomplete;
@@ -1670,8 +1669,7 @@ void BrowserAutofillManager::OnSingleFieldSuggestionSelected(
           GetFillingProductFromSuggestionType(type))) {
     autofill_trigger_field->AppendLogEventIfNotRepeated(
         TriggerFillFieldLogEvent{
-            .data_type =
-                GetEventTypeFromSingleFieldSuggestionSuggestionType(type),
+            .data_type = GetEventTypeFromSingleFieldSuggestionType(type),
             .associated_country_code = "",
             .timestamp = AutofillClock::Now()});
   }
