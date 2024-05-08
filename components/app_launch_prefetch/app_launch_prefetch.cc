076f96c0a54b90c01d8eda50810c485050a53782
Sean Maher <spvw@chromium.org>
2024-05-08 17:24:23 +0000

    app_launch_prefetch: Cleanup BUILD and debug includes
    Bug: 40946432, 325307453
    Change-Id: I6e64375af79b886355236a03eece8721bcf23432
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490539
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Sean Maher <spvw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298197}

--- components/app_launch_prefetch/app_launch_prefetch.cc
+++ components/app_launch_prefetch/app_launch_prefetch.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/win/windows_version.h"
 
-#include "base/debug/stack_trace.h"
-
 namespace {
 
 BASE_FEATURE(kExpandedPrefetchRange,
