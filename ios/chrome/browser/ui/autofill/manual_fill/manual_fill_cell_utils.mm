7490c6349a5d2a0a1fe49bf1db3da5e74d1fff09
Noémie St-Onge <noemies@google.com>
2024-05-08 18:25:57 +0000

    [iOSKAUpgrade] Set min height of "Autofill Form" button to 44px
    This CL makes sure that the "Autofill Form" button always has a height
    of at least 44px. The previous height was of 40px with the default font
    size setting.
    
    Before/After comparison:
    https://screenshot.googleplex.com/3dV2SmDn6YBV95v.png
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 339173622
    Change-Id: Iddd508c0831dce9fc6392810d2814c9176306d20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521859
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298242}

--- ios/chrome/browser/ui/autofill/manual_fill/manual_fill_cell_utils.mm
+++ ios/chrome/browser/ui/autofill/manual_fill/manual_fill_cell_utils.mm
@@ -22,7 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Vertical insets of the "Autofill Form" button.
-constexpr CGFloat kAutofillFormButtonVerticalInsets = 9;
+constexpr CGFloat kAutofillFormButtonVerticalInsets = 11;
+
+// Minimum height of the "Autofill Form" button.
+constexpr CGFloat kAutofillFormButtonMinHeight = 44;
 
 // Bottom margin for the cell content. Used when the Keyboard Accessory Upgrade
 // feature is disabled.
@@ -479,6 +482,10 @@ UIButton* CreateAutofillFormButton() {
       NSDirectionalEdgeInsetsMake(kAutofillFormButtonVerticalInsets, 0,
                                   kAutofillFormButtonVerticalInsets, 0);
   button.configuration = buttonConfiguration;
+
+  [button.heightAnchor
+      constraintGreaterThanOrEqualToConstant:kAutofillFormButtonMinHeight]
+      .active = YES;
 
   SetConfigurationTitle(
       button, l10n_util::GetNSString(
