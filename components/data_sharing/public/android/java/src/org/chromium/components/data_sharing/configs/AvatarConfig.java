22a608ac235f264fdd927ad0639ca2e2c6d90605
ssid <ssid@chromium.org>
2024-05-08 16:36:59 +0000

    Move DataSharingUIDelegate to components.
    We want the native UI delegate to own the java version and
    ideally have all platforms use the same delegate interface.
    Moving to components makes it easy to fetch from the service
    and easier to unittest.
    
    Bug: b/336873603
    Change-Id: I1bceac4ef441f95240476400cf9bd4155dcc248d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519625
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298172}

--- components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/AvatarConfig.java
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/AvatarConfig.java
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.data_sharing.configs;
+package org.chromium.components.data_sharing.configs;
 
-/** Config class for GroupMemberListView UI. */
-public class GroupMemberConfig {
+/** Config class for Avatars UI. */
+public class AvatarConfig {
 
     /** Constructor */
-    public GroupMemberConfig() {}
+    public AvatarConfig() {}
 }
