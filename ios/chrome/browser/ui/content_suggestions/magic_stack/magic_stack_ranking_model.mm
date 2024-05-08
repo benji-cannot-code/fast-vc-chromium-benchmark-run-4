77b0d6ce0e8d071f5f53d6b2befe138ccf0919fe
Chris Lu <thegreenfrog@chromium.org>
2024-05-08 14:58:02 +0000

    [ios] Show Parcel Tracking Immediately if Ready upon Setup
    Sometimes (Opening NTP), fetching tracked parcels finishes before
    -configureConsumer: is called. Thus, the parcel config needs to be sent
    to the consumer here.
    
    Change-Id: Ifec0db513fa9294dff75bd0b195d18fa8a82681d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523034
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298095}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
@@ -143,6 +143,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             RunningSafetyCheckState::kDefault) {
       [self.consumer showSafetyCheck:_safetyCheckMediator.safetyCheckState];
     }
+    if (IsIOSParcelTrackingEnabled() &&
+        !IsParcelTrackingDisabled(GetApplicationContext()->GetLocalState()) &&
+        _parcelTrackingMediator.parcelTrackingItemToShow) {
+      [self.consumer showParcelTrackingItem:_parcelTrackingMediator
+                                                .parcelTrackingItemToShow];
+    }
     [self.consumer setShortcutTilesConfig:_shortcutsMediator.shortcutsConfig];
   }
 }
