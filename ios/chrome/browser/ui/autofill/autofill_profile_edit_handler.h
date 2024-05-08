b10a9accda388ab67fc06f9496c40516eecf20d3
Vidhan Jain <vidhanj@google.com>
2024-05-08 13:54:33 +0000

    [iOS] Create save/update button as a footer item
    Currently, the button item gets added to a section. As a result, the
    section is shown with boundary which is not expected. Therefore, this
    CL adds a button as a header footer item for this use case. For Edit
    Address view, this button is added as a footer.
    
    Mock: https://screenshot.googleplex.com/ArigLV9HsuJvWTx.png
    
    Before this CL,
    Screenshot: https://screenshot.googleplex.com/JaMnTEvMJvUbkcc.png
    
    After this CL,
    Screenshots:
    1. Save in account: https://screenshot.googleplex.com/AKQa4QbjHbrdBZT.png
    2. Save in account with error: https://screenshot.googleplex.com/6sLL7wyi4JeF6Rf.png
    3. Save Locally: https://screenshot.googleplex.com/7xbLs9mWmRdgsJ6.png
    
    Screencast: http://screencast/cast/NjIxODE4NjE5MzEwODk5MnxjZmRmNzRjNy0yNQ
    
    Bug: 1482269
    Change-Id: Iffad1259823359f700a0d7dac9c0c897a3b4817a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513310
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298073}

--- ios/chrome/browser/ui/autofill/autofill_profile_edit_handler.h
+++ ios/chrome/browser/ui/autofill/autofill_profile_edit_handler.h
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
        forRowAtIndexPath:(NSIndexPath*)indexPath
         withTextDelegate:(id<UITextFieldDelegate>)delegate;
 
+// Called for setting footer properties in `section`.
+- (void)configureView:(UIView*)view forFooterInSection:(NSInteger)section;
+
 // Returns header sections view controller  whose height should be 0.
 - (BOOL)heightForHeaderShouldBeZeroInSection:(NSInteger)section;
 
