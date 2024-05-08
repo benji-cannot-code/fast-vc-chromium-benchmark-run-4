6170636ff5b019880bff06727e93e50b63ca7faa
Zack Han <zackhan@chromium.org>
2024-05-08 19:56:23 +0000

    [SBER Deprecation] Create feature flag and a new pref value.
    In this CL, we create a new feature flag to guard the Extended Reporting
    (SBER) deprecation. We also add a new safe browsing pref value to record
    users' extended reporting state before the deprecation. On every run of
    Chrome, we will check the SBER deprecation flag and decide if we need to
    store the SBER state.
    
    Bug: 336547987
    Change-Id: I4102ed8f0740a373e4d1284d9c7fae5e4a85ab80
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5478189
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298309}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -98,6 +98,10 @@ BASE_FEATURE(kEncryptedArchivesMetadata,
              "SafeBrowsingEncryptedArchivesMetadata",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kExtendedReportingRemovePrefDependency,
+             "ExtendedReportingRemovePrefDependency",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kExtensionTelemetryConfiguration,
              "SafeBrowsingExtensionTelemetryConfiguration",
              base::FEATURE_DISABLED_BY_DEFAULT);
