8a9df93a3934b32ca3b3ef90ec84ed57b0b8dc6e
Ernesto Izquierdo Clua <eic@google.com>
2024-05-08 19:55:06 +0000

    [ios] Cleanup AutofillAgent form filling success metrics recording
    Removes outdated TODO and moves metrics recording to a separate method.
    
    Change-Id: Idaf1b38491569f60d72c96f272be0796163760f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5525938
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298306}

--- components/autofill/ios/browser/autofill_agent.mm
+++ components/autofill/ios/browser/autofill_agent.mm
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/memory/raw_ptr.h"
 #import "base/memory/weak_ptr.h"
 #import "base/metrics/field_trial.h"
-#import "base/metrics/histogram_macros.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
@@ -953,13 +953,14 @@ constexpr CGFloat kSuggestionIconWidth = 32;
                      inFrame:(web::WebFrame*)frame
         fieldToFormLookupMap:(const FieldToFormLookupMap&)fieldToFormLookupMap {
   std::map<uint32_t, std::u16string> fillingResults;
-  if (!autofill::ExtractFillingResults(resultsAsJsonStr, &fillingResults)) {
-    return;
+  if (autofill::ExtractFillingResults(resultsAsJsonStr, &fillingResults)) {
+    [self updateFieldManagerWithFillingResults:fillingResults inFrame:frame];
+    [self notifyAboutFormFillingResults:fillingResults
+                                inFrame:frame
+                   fieldToFormLookupMap:fieldToFormLookupMap];
   }
-  [self updateFieldManagerWithFillingResults:fillingResults inFrame:frame];
-  [self notifyAboutFormFillingResults:fillingResults
-                              inFrame:frame
-                 fieldToFormLookupMap:fieldToFormLookupMap];
+
+  [self recordFormFillingSuccessMetrics:!fillingResults.empty()];
 }
 
 // Called when did clear fields.
@@ -985,14 +986,6 @@ constexpr CGFloat kSuggestionIconWidth = 32;
                                      inFrame:frame
                                    withValue:fillData.second];
   }
-
-  // TODO(crbug.com/40150011): Remove once the experiment is over.
-  UMA_HISTOGRAM_BOOLEAN("Autofill.FormFillSuccessIOS", !fillingResults.empty());
-
-  ukm::SourceId source_id = ukm::GetSourceIdForWebStateDocument(_webState);
-  ukm::builders::Autofill_FormFillSuccessIOS(source_id)
-      .SetFormFillSuccess(!fillingResults.empty())
-      .Record(ukm::UkmRecorder::Get());
 }
 
 - (void)updateFieldManagerForSpecificField:(FieldRendererId)fieldRendererID
@@ -1338,6 +1331,18 @@ constexpr CGFloat kSuggestionIconWidth = 32;
       /*track_user_edited_fields=*/true);
 
   [self scanFormsInWebState:webState inFrame:frame];
+}
+
+// Records if the renderer was able to fill the Autofill-provided values in a
+// form or formless fields.
+- (void)recordFormFillingSuccessMetrics:(BOOL)success {
+  base::UmaHistogramBoolean(/*name=*/"Autofill.FormFillSuccessIOS",
+                            /*sample=*/success);
+
+  ukm::SourceId source_id = ukm::GetSourceIdForWebStateDocument(_webState);
+  ukm::builders::Autofill_FormFillSuccessIOS(source_id)
+      .SetFormFillSuccess(success)
+      .Record(ukm::UkmRecorder::Get());
 }
 
 @end
