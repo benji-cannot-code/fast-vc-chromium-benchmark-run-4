7c70f033afcf7b5000b8378a26ab2e14ebc8f1e9
Florian Jacky <fjacky@google.com>
2024-05-08 20:57:47 +0000

    Enable one time permission support for PEPC
    This CL supports one time permissions in PEPC without requiring an active 1TP flag. This means, that we essentially fully enable one time permissions, but don't show its UI if the flag isn't active.
    
    For the previously-denied view, always only show the one time option (This CL does not include aligning the strings depending on the one time permission configuration of the client).
    
    
    Bug: 339198010
    Change-Id: I15b8676c696faecca428ddb5d5cf4ed62488ea2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522598
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298346}

--- components/permissions/permission_util.cc
+++ components/permissions/permission_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/content_features.h"
 #include "third_party/blink/public/common/permissions/permission_utils.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy_feature.mojom.h"
@@ -243,8 +244,7 @@ bool PermissionUtil::CanPermissionBeAllowedOnce(ContentSettingsType type) {
     case ContentSettingsType::MEDIASTREAM_MIC:
     case ContentSettingsType::MEDIASTREAM_CAMERA:
     case ContentSettingsType::SMART_CARD_DATA:
-      return base::FeatureList::IsEnabled(
-          permissions::features::kOneTimePermission);
+      return true;
     default:
       return false;
   }
