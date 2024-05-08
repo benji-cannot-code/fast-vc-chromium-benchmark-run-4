8651d99050e577922455b01f59adadd08cf823f8
David Bertoni <dbertoni@chromium.org>
2024-05-08 14:52:02 +0000

    [Code Health] Remove some uses of base::SupportsWeakPtr. (ios)
    This CL replaces uses of base::SupportsWeakPtr with a
    base::WeakPtrFactory data member.
    
    Bug: 40485134
    Change-Id: I77e8eaa0d0d7dda5178862533e73cdc27edaf399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522859
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298090}

--- ios/chrome/browser/sessions/legacy_session_restoration_service_unittest.mm
+++ ios/chrome/browser/sessions/legacy_session_restoration_service_unittest.mm
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/files/scoped_temp_dir.h"
 #import "base/functional/bind.h"
 #import "base/memory/raw_ptr.h"
+#import "base/memory/weak_ptr.h"
 #import "base/run_loop.h"
 #import "base/scoped_multi_source_observation.h"
 #import "base/strings/stringprintf.h"
@@ -81,7 +82,7 @@ struct Wrapper {
  private:
   using Callback = base::OnceCallback<Ret(Args...)>;
 
-  struct Flag : base::SupportsWeakPtr<Flag> {
+  struct Flag final {
     explicit Flag(Wrapper* owner, Callback callback)
         : owner_(owner), callback_(std::move(callback)) {}
 
@@ -96,12 +97,13 @@ struct Wrapper {
 
     raw_ptr<Wrapper<Ret, Args...>> owner_;
     Callback callback_;
+    base::WeakPtrFactory<Flag> weak_ptr_factory_{this};
   };
 
  public:
   Wrapper(Callback callback) {
     auto flag = std::make_unique<Flag>(this, std::move(callback));
-    flag_ = flag->AsWeakPtr();
+    flag_ = flag->weak_ptr_factory_.GetWeakPtr();
 
     callback_ = base::BindOnce(&Flag::Run, std::move(flag));
   }
