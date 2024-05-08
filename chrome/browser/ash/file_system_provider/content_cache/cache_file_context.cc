ab03a0a189a9503a7290a09f391e28d2c852ea42
Ben Reich <benreich@chromium.org>
2024-05-08 04:48:59 +0000

    [FspCache] Convert CacheFileContext to class instead of struct
    To enable eviction and locking of files, some derived fields need to be
    introduced on the `CacheFileContext`. This breaks the purpose of a
    struct (just a container of data see http://go/totw/207) so let's
    convert it to a class to enable easy adding of these fields.
    
    This essentially converts the struct to mutators/accessors and updates
    all the call sites to use these new methods.
    
    Bug: b/339320580
    Test: unit_tests --gtest_filter=FileSystemProviderContentCacheImplTest*
    Change-Id: I925c69e61302be477f2bc46946dd2db6096a2122
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524316
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297946}

--- chrome/browser/ash/file_system_provider/content_cache/cache_file_context.cc
+++ chrome/browser/ash/file_system_provider/content_cache/cache_file_context.cc
@@ -8,10 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::file_system_provider {
 
 CacheFileContext::CacheFileContext(const std::string& version_tag)
-    : version_tag(version_tag) {}
+    : version_tag_(version_tag) {}
 
 CacheFileContext::CacheFileContext(int64_t bytes_on_disk, int64_t id)
-    : bytes_on_disk(bytes_on_disk), id(id) {}
+    : bytes_on_disk_(bytes_on_disk), id_(id) {}
 
 CacheFileContext::CacheFileContext(CacheFileContext&&) = default;
 CacheFileContext& CacheFileContext::operator=(CacheFileContext&&) = default;
