58be29af9d84658352265a122318d228f1d800e4
Sammie Quon <sammiequon@chromium.org>
2024-05-08 22:46:32 +0000

    wm: Hide drag window in mini view
    Test: manual
    Change-Id: I467b79d9c21739bd6fd37c8feafaad8862087b22
    Fixed: b:338466952
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524150
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298397}

--- ash/wm/drag_window_controller.cc
+++ ash/wm/drag_window_controller.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/drag_window_controller.h"
 
 #include "ash/public/cpp/shell_window_ids.h"
+#include "ash/public/cpp/window_properties.h"
 #include "ash/shell.h"
 #include "ash/wm/window_mirror_view.h"
 #include "base/memory/raw_ptr.h"
@@ -115,8 +116,9 @@ class DragWindowController::DragWindowDetails {
     params.name = "DragWindow";
     params.activatable = views::Widget::InitParams::Activatable::kNo;
     params.accept_events = false;
-    const int parent_id = original_window->parent()->GetId();
-    params.parent = root_window_->GetChildById(parent_id);
+    params.init_properties_container.SetProperty(kHideInDeskMiniViewKey, true);
+    params.parent =
+        root_window_->GetChildById(original_window->parent()->GetId());
 
     widget_ = std::make_unique<views::Widget>();
     widget_->set_focus_on_creation(false);
