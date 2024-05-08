7033d07b3f8fc8e2dc9d7263f82fc60180de1abc
Takuto Ikuta <tikuta@chromium.org>
2024-05-08 03:58:51 +0000

    base/trace_event: add 'IWYU pragma: export' for some macros
    Without this annotation, include of "base/trace_event/trace_event.h"
    is replaced with headers in perfetto/tracing by include
    cleaner if the file uses macros from them.
    
    But I think we want to use those macros via trace_event.h in chromium.
    
    ref: https://github.com/include-what-you-use/include-what-you-use/blob/master/docs/IWYUPragmas.md#iwyu-pragma-export
    
    Bug: 336474469
    Change-Id: Ib9e61ff38b50ae9e15ed7aa9ce318c2a9937b349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5508562
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297936}

--- base/trace_event/trace_event.h
+++ base/trace_event/trace_event.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/time/time_override.h"
 #include "base/trace_event/builtin_categories.h"
-#include "base/trace_event/common/trace_event_common.h"
+#include "base/trace_event/common/trace_event_common.h"  // IWYU pragma: export
 #include "base/trace_event/trace_arguments.h"
 #include "base/trace_event/trace_category.h"
 #include "base/trace_event/trace_log.h"
