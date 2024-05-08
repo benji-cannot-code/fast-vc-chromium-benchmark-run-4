9281390aa0ff6cfc1baba56f33e7a2e0fad6213d
Wenbo Jie <wenbojie@chromium.org>
2024-05-08 00:07:42 +0000

    Reland "[FilesF2] Always run Files app with the new directory tree"
    This is a reland of commit e287242a593d5129248399893681cf3fb61de01e,
    disabled the flaky test on MSan.
    
    Note to Gardener: if this change triggers some flakiness, please do not
    revert the whole change, instead please just disable the flaky ones.
    
    Original change's description:
    > [FilesF2] Always run Files app with the new directory tree
    >
    > * Always use the new tree regardless of the feature flag.
    > * Remove NewDirectoryTree() variant for all browser tests since
    >   all tests now are running with the new tree by default.
    > * Remove new tree check in all browser tests.
    >
    > Bug: b:285977941
    > Test: browser_tests --gtest_filter="*FilesAppBrowserTest.Test*"
    > Change-Id: I7c056c0e07911c8e92c264df2b6397beb20137ea
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5505032
    > Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    > Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1295204}
    
    Bug: b:285977941
    Change-Id: I02d92fb186638efc371c1c140769ab9d400ed9d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514086
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Wenbo Jie <wenbojie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297862}

--- ui/file_manager/integration_tests/file_manager/trash.ts
+++ ui/file_manager/integration_tests/file_manager/trash.ts
@@ -1292,9 +1292,7 @@ export async function trashTogglingHiddenFilesNavigatesAwayFromTrash() {
 
   // Expect current directory goes back to MyFiles.
   await remoteCall.waitUntilCurrentDirectoryIsChanged(appId, '/My files');
-  if (directoryTree.isNewTree) {
-    await directoryTree.waitForItemLostByLabel('.Trash');
-  }
+  await directoryTree.waitForItemLostByLabel('.Trash');
 
   // Show hidden files again.
   await remoteCall.showHiddenFiles(appId);
@@ -1307,7 +1305,5 @@ export async function trashTogglingHiddenFilesNavigatesAwayFromTrash() {
 
   // Expect current directory goes back to MyFiles.
   await remoteCall.waitUntilCurrentDirectoryIsChanged(appId, '/My files');
-  if (directoryTree.isNewTree) {
-    await directoryTree.waitForItemLostByLabel('.Trash');
-  }
+  await directoryTree.waitForItemLostByLabel('.Trash');
 }
