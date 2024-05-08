0a4b03b414fb846cf418a5b626f6b1b15d6f09ed
Helmut Januschka <helmut@januschka.com>
2024-05-08 20:49:01 +0000

    StringPiece follow up - remove "using std::string_view"
    Bug: 40506050
    Change-Id: I917d4bf43a1959a6ab5264e4fd29d5ca1885609c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522834
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Cr-Commit-Position: refs/heads/main@{#1298340}

--- components/value_store/leveldb_value_store.cc
+++ components/value_store/leveldb_value_store.cc
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/leveldatabase/src/include/leveldb/iterator.h"
 #include "third_party/leveldatabase/src/include/leveldb/write_batch.h"
 
-using std::string_view;
-
 namespace {
 
 const char kInvalidJson[] = "Invalid JSON";
