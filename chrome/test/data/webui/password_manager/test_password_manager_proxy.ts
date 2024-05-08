f2c81ed73cd6685c34786de68f95b7ce8a53bc44
Markus Heintz <markusheintz@google.com>
2024-05-08 20:04:02 +0000

    Adds IsConnectedToCloudAuthenticator method to the passwordsPrivate API.
    This methods enables the Password Manager settings UI to check whether the chrome client is connected to the Cloud Authenticator and if the disconnect UI should be displayed (offered).
    
    Bug: 338959659
    Change-Id: I8430f24a9982f17bc7e4c954b6239af610d74b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522536
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Markus Heintz <markusheintz@google.com>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298316}

--- chrome/test/data/webui/password_manager/test_password_manager_proxy.ts
+++ chrome/test/data/webui/password_manager/test_password_manager_proxy.ts
@@ -28,6 +28,7 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
     isPasswordManagerPinAvailable: boolean,
     changePasswordManagerPinSuccesful: boolean|null,
     disconnectCloudAuthenticatorSuccessful: boolean|null,
+    isConnectedToCloudAuthenticator: boolean,
   };
 
   listeners: {
@@ -73,6 +74,7 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
       'getUrlCollection',
       'importPasswords',
       'isAccountStoreDefault',
+      'isConnectedToCloudAuthenticator',
       'isOptedInForAccountStorage',
       'isPasswordManagerPinAvailable',
       'movePasswordsToAccount',
@@ -109,6 +111,7 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
       isPasswordManagerPinAvailable: false,
       changePasswordManagerPinSuccesful: null,
       disconnectCloudAuthenticatorSuccessful: null,
+      isConnectedToCloudAuthenticator: false,
     };
 
     // Holds listeners so they can be called when needed.
@@ -406,5 +409,10 @@ export class TestPasswordManagerProxy extends TestBrowserProxy implements
       return Promise.resolve(this.data.disconnectCloudAuthenticatorSuccessful);
     }
     return Promise.reject(new Error());
+  }
+
+  isConnectedToCloudAuthenticator(): Promise<boolean> {
+    this.methodCalled('isConnectedToCloudAuthenticator');
+    return Promise.resolve(this.data.isConnectedToCloudAuthenticator);
   }
 }
