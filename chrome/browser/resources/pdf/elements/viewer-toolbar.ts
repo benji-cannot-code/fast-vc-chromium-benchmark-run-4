95395786ea20d600d673c34885edcea6e1f6784f
Andy Phan <andyphan@chromium.org>
2024-05-08 05:58:33 +0000

    [PDF Ink Signatures] Display annotation bar
    Display the annotation bar when annotation mode is enabled, and hide it
    when annotation mode is disabled.
    
    Bug: 335521184
    Change-Id: I4a720a3cea2ad83c23b4620a2305009e47f7de6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494566
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297963}

--- chrome/browser/resources/pdf/elements/viewer-toolbar.ts
+++ chrome/browser/resources/pdf/elements/viewer-toolbar.ts
@@ -13,10 +13,11 @@ import './viewer-download-controls.js';
 import './viewer-page-selector.js';
 import './pdf-shared.css.js';
 import './shared-vars.css.js';
-// <if expr="enable_ink">
+// <if expr="enable_ink or enable_pdf_ink2">
 import './viewer-annotations-bar.js';
+// </if>
+// <if expr="enable_ink">
 import './viewer-annotations-mode-dialog.js';
-
 // </if>
 
 import type {CrActionMenuElement} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
@@ -145,7 +146,9 @@ export class ViewerToolbarElement extends PolymerElement {
         type: Boolean,
         value: false,
       },
+      // </if>
 
+      // <if expr="enable_ink or enable_pdf_ink2">
       showAnnotationsBar_: {
         type: Boolean,
         computed: 'computeShowAnnotationsBar_(' +
@@ -181,11 +184,12 @@ export class ViewerToolbarElement extends PolymerElement {
   // <if expr="enable_ink or enable_pdf_ink2">
   annotationAvailable: boolean;
   annotationMode: boolean;
+
+  private showAnnotationsBar_: boolean;
   // </if>
 
   // <if expr="enable_ink">
   private showAnnotationsModeDialog_: boolean;
-  private showAnnotationsBar_: boolean;
   // </if>
 
   // <if expr="enable_pdf_ink2">
@@ -243,7 +247,7 @@ export class ViewerToolbarElement extends PolymerElement {
     this.getZoomInput_().value = `${this.viewportZoomPercent_}%`;
   }
 
-  // <if expr="enable_ink">
+  // <if expr="enable_ink or enable_pdf_ink2">
   private computeShowAnnotationsBar_(): boolean {
     return this.pdfAnnotationsEnabled && !this.loading_ && this.annotationMode;
   }
