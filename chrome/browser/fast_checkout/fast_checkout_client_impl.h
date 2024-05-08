7971e72fe5e0795361cb2a117a0389f67fc4bf1b
Florian Leimgruber <fleimgruber@google.com>
2024-05-08 07:27:53 +0000

    Reland "Remove PDMObserver from AutofillOfferManager"
    This is a reland of commit dbc3b1eb7fd3f8b27e296fc32abfb5b0d6d4fede
    
    OfferNotificationControllerAndroidBrowserTest::SetUpOfferDataWithDomains
    still notified the PDM observer instead of the PayDM observer.
    (Arguably, that shouldn't happen manually to begin with..)
    
    Original change's description:
    > Remove PDMObserver from AutofillOfferManager
    >
    > As part of go/split-pdm the PDMObserver is split into an address and a
    > payments data manager observer. The offer manager only cares about
    > payment changes (to update the set of eligible merchant domains when
    > the set of offers changes). Thus, this CL replaces the offer manager's
    > PDMObserver with an PaymentsDataManager::Observer.
    >
    > Bug: b/322170538
    > Change-Id: I73d1093936934a562deb478325e28765f721d248
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5496758
    > Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1295393}
    
    Bug: b/322170538
    Change-Id: Ifa3f3d5101397c9459d8f937a37315a116c365d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522596
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1297984}

--- chrome/browser/fast_checkout/fast_checkout_client_impl.h
+++ chrome/browser/fast_checkout/fast_checkout_client_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/payments/full_card_request.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/browser/personal_data_manager_observer.h"
 #include "components/autofill/core/browser/ui/fast_checkout_client.h"
 #include "components/autofill/core/browser/ui/fast_checkout_enums.h"
 #include "content/public/browser/web_contents.h"
