8a1268e3281541a72a99236c04d36169da95786d
Austin Eng <enga@google.com>
2024-05-08 17:04:28 +0000

    chrome ml: allow Windows ARM GPU adapters
    Bug: 339235133
    Change-Id: Ife8f7b0a1690c17aa9b10b6664081f7daf5a9415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527213
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298188}

--- services/on_device_model/ml/chrome_ml.cc
+++ services/on_device_model/ml/chrome_ml.cc
@@ -212,7 +212,8 @@ bool ChromeML::IsGpuBlocked() const {
                     .blocklist_string = kGpuBlockList.Get(),
                     .ignores =
                         WebGPUBlocklistReason::IndirectComputeRootConstants |
-                        WebGPUBlocklistReason::Consteval22ndBit,
+                        WebGPUBlocklistReason::Consteval22ndBit |
+                        WebGPUBlocklistReason::WindowsARM,
                 });
             if (query_data->blocklisted) {
               wgpu::AdapterProperties properties;
