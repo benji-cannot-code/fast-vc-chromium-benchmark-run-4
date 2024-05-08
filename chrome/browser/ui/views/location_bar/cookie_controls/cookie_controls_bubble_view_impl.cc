b7593afbb544db3decfcdcc24e8533e07a1975cc
jiawei wang <hellojw513@gmail.com>
2024-05-08 13:15:26 +0000

    Bubble: remove all SizeToContents() in /chrome/browser/ui/views/location_bar
    Remove all SizeToContents() and add InvalidateLayout() when necessary.
    This changes is used to make the bubbles using autosize mode via
    autosize parameter.(autosize mode: When the preferred size of bubble
    changes,its widget automatically resizes itself to meet the content's
    size)
    
    The ultimate goal of this bug is to remove all SizeToContents() to
    reduce developer confusion about when SizeToContents() needs to be
    called.
    
    This CL was uploaded by git cl split.
    
    R=khalidpeer@chromium.org, kmg@google.com
    
    Bug: 41493925
    Change-Id: I27ee75a332f4e0cd09720361197de8b86a1cd373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514339
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298062}

--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.cc
@@ -29,7 +29,7 @@ CookieControlsBubbleViewImpl::CookieControlsBubbleViewImpl(
     views::View* anchor_view,
     content::WebContents* web_contents,
     OnCloseBubbleCallback callback)
-    : LocationBarBubbleDelegateView(anchor_view, web_contents),
+    : LocationBarBubbleDelegateView(anchor_view, web_contents,true),
       callback_(std::move(callback)) {
   SetShowCloseButton(true);
   SetButtons(ui::DIALOG_BUTTON_NONE);
@@ -65,7 +65,6 @@ void CookieControlsBubbleViewImpl::InitReloadingView(
 
 void CookieControlsBubbleViewImpl::UpdateTitle(const std::u16string& title) {
   SetTitle(title);
-  SizeToContents();
 }
 
 void CookieControlsBubbleViewImpl::UpdateSubtitle(
@@ -86,7 +85,7 @@ void CookieControlsBubbleViewImpl::SwitchToReloadingView() {
       base::UserMetricsAction("CookieControls.Bubble.ReloadingShown"));
   GetReloadingView()->SetVisible(true);
   GetContentView()->SetVisible(false);
-  SizeToContents();
+  InvalidateLayout();
 }
 
 CookieControlsContentView* CookieControlsBubbleViewImpl::GetContentView() {
@@ -120,11 +119,6 @@ gfx::Size CookieControlsBubbleViewImpl::CalculatePreferredSize(
                      views::DistanceMetric::DISTANCE_BUBBLE_PREFERRED_WIDTH),
                  kMaxBubbleWidth);
   return gfx::Size(width, size.height());
-}
-
-void CookieControlsBubbleViewImpl::ChildPreferredSizeChanged(
-    views::View* child) {
-  SizeToContents();
 }
 
 void CookieControlsBubbleViewImpl::CloseBubble() {
--- chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.h
+++ chrome/browser/ui/views/location_bar/cookie_controls/cookie_controls_bubble_view_impl.h
@@ -61,9 +61,6 @@ class CookieControlsBubbleViewImpl : public CookieControlsBubbleView,
   void CloseBubble() override;
   bool OnCloseRequested(views::Widget::ClosedReason close_reason) override;
 
-  // views::View:
-  void ChildPreferredSizeChanged(views::View* child) override;
-
   raw_ptr<View> reloading_view_ = nullptr;
   raw_ptr<CookieControlsContentView> content_view_ = nullptr;
 
