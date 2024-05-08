c64ed783386c2c6b00fda7a65a556bf966cc347c
Kazuki Takise <takise@google.com>
2024-05-08 04:20:31 +0000

    Don't adjust bounds for visibility for tucked PiP
    Tucked windows are located outside of the area that's allowed by
    `AdjustBoundsToEnsureMinimumWindowVisibility()`, but exo calls
    this function whenever the bounds of a client controlled window
    get updated, which could incorrectly push tucked PiP toward inside
    the display.
    
    BUG=b:337113950
    TEST=Manual. PiP gets tucked at the proper position.
    
    Change-Id: I03f4dffd8c5686005a0d0040a08cafc437c15d71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5500336
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Auto-Submit: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297940}

--- ash/wm/window_positioning_utils.cc
+++ ash/wm/window_positioning_utils.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/root_window_controller.h"
 #include "ash/screen_util.h"
 #include "ash/shell.h"
+#include "ash/wm/pip/pip_controller.h"
 #include "ash/wm/system_modal_container_layout_manager.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_restore/window_restore_controller.h"
@@ -79,6 +80,12 @@ void AdjustBoundsSmallerThan(const gfx::Size& max_size, gfx::Rect* bounds) {
 void AdjustBoundsToEnsureMinimumWindowVisibility(const gfx::Rect& visible_area,
                                                  bool client_controlled,
                                                  gfx::Rect* bounds) {
+  if (Shell::Get()->pip_controller()->is_tucked()) {
+    // PiP is allowed to be positioned beyond the threshold while it's tucked.
+    // TODO(http://b/337113950): Check if this is also needed for float windows.
+    return;
+  }
+
   int min_width =
       std::max(kMinimumOnScreenArea,
                static_cast<int>(bounds->width() * kMinimumPercentOnScreenArea));
