c762f89d8308359506be7607bae86e4a94a1bc2b
dpapad <dpapad@chromium.org>
2024-05-08 23:09:18 +0000

    SidePanel: Migrate c/b/r/side_panel/customize_chrome to Lit, part 10.
    In this part migrating app.ts, which is the last file that directly
    depends on Polymer. Follow-up CLs will remove/replace any remaining
    transitive dependencies to Polymer (like iron-pages), as well as
    perform a few post-migration cleanups.
    
    Also, had to remove `on-click="onChromeWebStoreLinkClick_"` from being
    part of a localized string, as the equivalent Lit binding
    `@click="${this.onChromeWebStoreLinkClick_}"` is not allowed in
    $i18nRaw{} replacements and results in a failed CHECK(). Instead moved
    the listener to the parent DOM element.
    
    Bug: 40943652
    Change-Id: Iae02335bf2c93e431adac7bf7226f082db5eadb8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521689
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298411}

--- chrome/test/data/webui/side_panel/customize_chrome/app_test.ts
+++ chrome/test/data/webui/side_panel/customize_chrome/app_test.ts
@@ -15,7 +15,7 @@ import {assertEquals, assertGE, assertTrue} from 'chrome://webui-test/chai_asser
 import type {MetricsTracker} from 'chrome://webui-test/metrics_test_support.js';
 import {fakeMetricsPrivate} from 'chrome://webui-test/metrics_test_support.js';
 import type {TestMock} from 'chrome://webui-test/test_mock.js';
-import {eventToPromise} from 'chrome://webui-test/test_util.js';
+import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {installMock} from './test_support.js';
 
@@ -91,6 +91,7 @@ suite('AppTest', () => {
     // Send event for edit theme being clicked.
     customizeChromeApp.$.appearanceElement.dispatchEvent(
         new Event('edit-theme-click'));
+    await microtasksFinished();
     // Current page should now be categories.
     assertTrue(customizeChromeApp.$.categoriesPage.classList.contains(
         'iron-selected'));
@@ -100,6 +101,7 @@ suite('AppTest', () => {
     customizeChromeApp.$.categoriesPage.dispatchEvent(
         new CustomEvent<BackgroundCollection>(
             'collection-select', {detail: testCollection}));
+    await microtasksFinished();
     // Current page should now be themes.
     assertTrue(
         customizeChromeApp.$.themesPage.classList.contains('iron-selected'));
@@ -107,6 +109,7 @@ suite('AppTest', () => {
 
     // Send event for back click.
     customizeChromeApp.$.themesPage.dispatchEvent(new Event('back-click'));
+    await microtasksFinished();
     // Current page should now be categories.
     assertTrue(customizeChromeApp.$.categoriesPage.classList.contains(
         'iron-selected'));
@@ -115,6 +118,7 @@ suite('AppTest', () => {
     // Send event for upload image.
     customizeChromeApp.$.categoriesPage.dispatchEvent(
         new Event('local-image-upload'));
+    await microtasksFinished();
     // Current page should now be overview.
     assertTrue(
         customizeChromeApp.$.overviewPage.classList.contains('iron-selected'));
@@ -123,10 +127,12 @@ suite('AppTest', () => {
     // Set page back to categories.
     customizeChromeApp.$.appearanceElement.dispatchEvent(
         new Event('edit-theme-click'));
+    await microtasksFinished();
     assertEquals(customizeChromeApp, document.activeElement);
 
     // Send event for back click.
     customizeChromeApp.$.categoriesPage.dispatchEvent(new Event('back-click'));
+    await microtasksFinished();
     // Current page should now be overview.
     assertTrue(
         customizeChromeApp.$.overviewPage.classList.contains('iron-selected'));
