7e854f7a4e5a0fa601adc8a388ad4ec075ba0ded
Peter McNeeley <petermcneeley@chromium.org>
2024-05-08 16:29:21 +0000

    Enable NonBatchedCopySharedImage by default
    See:
    https://chromium-review.googlesource.com/c/chromium/src/+/5518478
    
    Bug: 41181609
    Change-Id: I29edca34f16ff5e1814e160dcf6a40f2d40de339
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526496
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298167}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -199,6 +199,6 @@ BASE_FEATURE(kMetricsBackfillAdjustmentHoldback,
 
 BASE_FEATURE(kNonBatchedCopySharedImage,
              "NonBatchedCopySharedImage",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
