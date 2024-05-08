7ad8dcb06a3380a018e3d3af10f3fb1aec296df0
Xiang Xiao <xiangxiao@google.com>
2024-05-08 16:49:34 +0000

    Enable the reading mode docs integration flag.
    Bug: 324143642
    Change-Id: Icb4b0ef03b00d36de061c1ca1d5d62dc4039f348
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523475
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Xiang Xiao <xiangxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298180}

--- chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator_unittest.cc
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator_unittest.cc
@@ -43,7 +43,8 @@ class ReadAnythingCoordinatorTest : public TestWithBrowserView {
  public:
   void SetUp() override {
     base::test::ScopedFeatureList features;
-    scoped_feature_list_.InitWithFeatures({features::kReadAnything}, {});
+    scoped_feature_list_.InitWithFeatures(
+        {features::kReadAnything, features::kReadAnythingDocsIntegration}, {});
     TestWithBrowserView::SetUp();
 
     side_panel_coordinator_ =
@@ -153,11 +154,8 @@ TEST_F(ReadAnythingCoordinatorTest,
 }
 
 #if !BUILDFLAG(IS_CHROMEOS_LACROS)
-TEST_F(
-    ReadAnythingCoordinatorTest,
-    // TODO(crbug.com/324143642): Re-enable this test when the docs integration
-    // flag is enabled.
-    DISABLED_SidePanelShowAndHide_NonLacros_CallEmbeddedA11yExtensionLoader) {
+TEST_F(ReadAnythingCoordinatorTest,
+       SidePanelShowAndHide_NonLacros_CallEmbeddedA11yExtensionLoader) {
   SidePanelEntry* entry = side_panel_registry_->GetEntryForKey(
       SidePanelEntry::Key(SidePanelEntry::Id::kReadAnything));
   EXPECT_FALSE(EmbeddedA11yExtensionLoader::GetInstance()->IsExtensionInstalled(
@@ -177,9 +175,7 @@ TEST_F(
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
 TEST_F(
     ReadAnythingCoordinatorTest,
-    // TODO(crbug.com/324143642): Re-enable this test when the docs integration
-    // flag is enabled.
-    DISABLED_SidePanelShowAndHide_Lacros_EmbeddedA11yManagerLacrosUpdateReadingModeState) {
+    SidePanelShowAndHide_Lacros_EmbeddedA11yManagerLacrosUpdateReadingModeState) {
   SidePanelEntry* entry = side_panel_registry_->GetEntryForKey(
       SidePanelEntry::Key(SidePanelEntry::Id::kReadAnything));
   EXPECT_FALSE(
