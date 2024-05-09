920896511bc354253b6663556ed0fd48cdcbaa95
ahmedmoussa <ahmedmoussa@google.com>
2024-05-09 20:19:24 +0000

    [live-preview] Reset ContextFactory pointer in previews on closing
    Reset ContextFactory raw pointer in CameraVideoFrameHandler (CVFH) on
    requesting to close the subscription. This is needed because there is a
    chance where CVFH instance outlives ContextFactory. The issue appears to
    be only repro on MAC in a local build.
    
    Bug: b:339520001
    Change-Id: I117641ad93119d15a6909767d093cf3588b2fd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528133
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298835}

--- components/capture_mode/camera_video_frame_handler.cc
+++ components/capture_mode/camera_video_frame_handler.cc
@@ -751,6 +751,13 @@ void CameraVideoFrameHandler::Close(base::OnceClosure close_complete_callback) {
   // `delegate_` might be freed any time after this point, so nullify it to
   // reflect that.
   delegate_ = nullptr;
+
+  // Since the destruction of `this` is postponed till `close_complete_callback`
+  // is invoked, there is a chance where `this` might outlive
+  // `context_factory_`. This is why we need to reset it.
+  buffer_map_.clear();
+  context_factory_ = nullptr;
+
   camera_video_stream_subsciption_remote_->Close(
       std::move(close_complete_callback));
 }
@@ -760,6 +767,10 @@ void CameraVideoFrameHandler::OnCaptureConfigurationChanged() {}
 void CameraVideoFrameHandler::OnNewBuffer(
     int buffer_id,
     media::mojom::VideoBufferHandlePtr buffer_handle) {
+  if (!active_) {
+    return;
+  }
+
   const auto pair = buffer_map_.emplace(
       buffer_id,
       BufferHandleHolder::Create(
@@ -808,7 +819,7 @@ void CameraVideoFrameHandler::OnFrameReadyInBuffer(
 }
 
 void CameraVideoFrameHandler::OnBufferRetired(int buffer_id) {
-  DCHECK(buffer_map_.contains(buffer_id));
+  DCHECK(buffer_map_.contains(buffer_id) || !active_);
   buffer_map_.erase(buffer_id);
 }
 
