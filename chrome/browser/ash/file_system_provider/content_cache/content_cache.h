64b58efbf7e1942d49d9664f0be0fc90168a9487
cassycc <cassycc@google.com>
2024-05-09 06:11:03 +0000

    FspCache: Remove watcher upon cache eviction
    Evictions occur when:
    - The CloudFileSystem manually requests an eviction.
    - The content cache evicts items to make more space.
    
    The CloudFileSystem would have no idea when an item was evicted in the
    latter case. This is an issue since the CloudFileSystem adds watchers
    whenever an item is added to the cache and it needs to remove them upon
    eviction.
    
    Add a method ContentCache::SetOnItemEvictedCallback() to allow the
    CloudFileSystem to set a repeating callback for the ContentCache to call
    whenever an item is evicted.
    
    When this callback is called with the file path evicted, remove the
    watcher for this file path.
    
    Test: --gtest_filter="FileSystemProviderContentCacheImplTest
    Bug: b/328679535
    Change-Id: I41d574f89a797ef6ba3d47ca20087342de136fca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514531
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298531}

--- chrome/browser/ash/file_system_provider/content_cache/content_cache.h
+++ chrome/browser/ash/file_system_provider/content_cache/content_cache.h
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::file_system_provider {
 
 using FileErrorCallback = base::OnceCallback<void(base::File::Error)>;
+using OnItemEvictedCallback =
+    base::RepeatingCallback<void(const base::FilePath&)>;
 
 // Alias to explain the inner int indicates `bytes_read`.
 using FileErrorOrBytesRead = base::FileErrorOr<int>;
@@ -85,6 +87,11 @@ class ContentCache {
   // is inaccessible from this point onwards despite it remaining on disk and
   // the database. It will be removed when `RemoveItems()` is called.
   virtual void Evict(const base::FilePath& file_path) = 0;
+
+  // Call this on_item_evicted_callback with the item's FSP path when it is
+  // evicted.
+  virtual void SetOnItemEvictedCallback(
+      OnItemEvictedCallback on_item_evicted_callback) = 0;
 
   // Remove items which have their `evicted` bool set to true. If an
   // removal is already in progress, the callback will be queued to be called
