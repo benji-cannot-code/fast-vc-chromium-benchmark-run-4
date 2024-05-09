81d752f7166025a2fa480838645fabb39b4efac8
Henrique Nakashima <hnakashima@chromium.org>
2024-05-09 19:37:52 +0000

    [Android] Turn WebContentsElementInState into a Supplier<WebContents>
    It now gets the WebContents from a loaded Tab instead of from the
    ChromeTabbedActivity, which makes it match the right Tab instead of any
    that is on the screen..
    
    Bug: 338433319
    Change-Id: Ifbdbc87095157d4a8ed3c46035e9d87f22d86781
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522696
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298803}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/testhtmls/PopupOnClickPageStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/testhtmls/PopupOnClickPageStation.java
@@ -41,7 +41,8 @@ public class PopupOnClickPageStation extends WebPageStation {
         super.declareElements(elements);
 
         mLinkToPopup =
-                elements.declareElementInState(new HtmlElementInState(LINK_TO_POPUP, mWebContents));
+                elements.declareElementInState(
+                        new HtmlElementInState(LINK_TO_POPUP, mWebContentsSupplier));
     }
 
     /** Opens the same page as a pop-up (in Android, this means in a new tab). */
