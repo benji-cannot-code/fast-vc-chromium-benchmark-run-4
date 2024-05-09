d741f43264ee714f99c7b06c84ccd182e28c6c91
cassycc <cassycc@google.com>
2024-05-09 07:14:23 +0000

    FspCache: Make the version tag a read-only property
    A file is cached at a particular version and the only way to update this
    is to delete the file from cache and make a new entry for a new version.
    Make the version tag a read-only property to make sure it can't be
    changed.
    
    Bug: b/339320580
    Change-Id: I74e287d27a20f14e622ab135ce8dca48ad2f64d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526696
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298542}

--- chrome/browser/ash/file_system_provider/content_cache/content_cache_impl_unittest.cc
+++ chrome/browser/ash/file_system_provider/content_cache/content_cache_impl_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/file_system_provider/content_cache/content_cache_impl.h"
 
+#include <memory>
+
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -110,16 +112,16 @@ class FileSystemProviderContentCacheImplTest : public testing::Test {
     return inserted_id;
   }
 
-  ContextDatabase::Item GetItemById(int64_t item_id) {
-    ContextDatabase::Item item;
-    TestFuture<bool> db_get_item_future;
+  std::unique_ptr<std::optional<ContextDatabase::Item>> GetItemById(
+      int64_t item_id) {
+    TestFuture<std::unique_ptr<std::optional<ContextDatabase::Item>>>
+        db_get_item_future;
     db_task_runner_->PostTaskAndReplyWithResult(
         FROM_HERE, base::BindLambdaForTesting([&]() {
-          return context_db_->GetItemById(item_id, item);
+          return context_db_->GetItemById(item_id);
         }),
         db_get_item_future.GetCallback());
-    EXPECT_TRUE(db_get_item_future.Get());
-    return item;
+    return db_get_item_future.Take();
   }
 
   void AddItemToDbAndDisk(const base::FilePath& fsp_path,
@@ -574,8 +576,11 @@ TEST_F(FileSystemProviderContentCacheImplTest,
                                               /*offset=*/0, kDefaultChunkSize,
                                               base::DoNothing()));
 
-  ContextDatabase::Item item = GetItemById(inserted_id);
-  EXPECT_FALSE(item.item_exists);
+  // Expect `std::nullopt` is returned as the item is not found.
+  std::unique_ptr<std::optional<ContextDatabase::Item>> item =
+      GetItemById(inserted_id);
+  EXPECT_TRUE(item);
+  EXPECT_FALSE(item->has_value());
 }
 
 TEST_F(FileSystemProviderContentCacheImplTest, RemoveItemsRemovesEvictedFiles) {
--- chrome/browser/ash/file_system_provider/content_cache/context_database.cc
+++ chrome/browser/ash/file_system_provider/content_cache/context_database.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/file_system_provider/content_cache/context_database.h"
 
+#include <memory>
+#include <optional>
 #include <sstream>
 
 #include "base/sequence_checker.h"
@@ -65,8 +67,6 @@ constexpr int ContextDatabase::kCurrentVersionNumber = 1;
 
 // The oldest version that is still compatible with `kCurrentVersionNumber`.
 constexpr int ContextDatabase::kCompatibleVersionNumber = 1;
-
-ContextDatabase::Item::Item() = default;
 
 ContextDatabase::Item::Item(int64_t id,
                             const std::string& fsp_path,
@@ -149,18 +149,19 @@ bool ContextDatabase::AddItem(const base::FilePath& fsp_path,
   return true;
 }
 
-bool ContextDatabase::GetItemById(int64_t item_id, Item& item) {
+std::unique_ptr<std::optional<ContextDatabase::Item>>
+ContextDatabase::GetItemById(int64_t item_id) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (item_id < 0) {
-    return false;
+    return nullptr;
   }
 
   std::unique_ptr<sql::Statement> statement = std::make_unique<sql::Statement>(
       db_.GetCachedStatement(SQL_FROM_HERE, kSelectItemByIdSql));
   if (!statement) {
     LOG(ERROR) << "Couldn't create SQL statement";
-    return false;
+    return nullptr;
   }
 
   statement->BindInt64(0, item_id);
@@ -168,19 +169,18 @@ bool ContextDatabase::GetItemById(int64_t item_id, Item& item) {
     // In the event the `Step()` failed, this could simply mean there is no item
     // for the `item_id`. `Succeeded` will return true in this case.
     if (statement->Succeeded()) {
-      item.item_exists = false;
-      return true;
+      return std::make_unique<std::optional<Item>>(std::nullopt);
     }
     LOG(ERROR) << "Couldn't execute statement";
-    return false;
+    return nullptr;
   }
 
-  item.fsp_path = base::FilePath(statement->ColumnString(0));
-  item.version_tag = statement->ColumnString(1);
-  item.accessed_time =
-      base::Time::FromMillisecondsSinceUnixEpoch(statement->ColumnInt64(2));
-
-  return true;
+  return std::make_unique<std::optional<ContextDatabase::Item>>(Item(
+      item_id,
+      /*fsp_path=*/statement->ColumnString(0),
+      /*version_tag=*/statement->ColumnString(1),
+      /*accessed_time=*/
+      base::Time::FromMillisecondsSinceUnixEpoch(statement->ColumnInt64(2))));
 }
 
 bool ContextDatabase::UpdateAccessedTime(int64_t item_id,
