01d943a29845fc722a456f97dac95ce887a37ad1
Sophie Chang <sophiechang@chromium.org>
2024-05-09 18:25:04 +0000

    Make GenerateEmbeddings a public method so we can just call it straight in the benchmark
    Bug: 339103147
    Change-Id: I312e52f70f1890dcb50783130f0067d7f6a619a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529695
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Jun Zou <junzou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298746}

--- services/passage_embeddings/passage_embedder.h
+++ services/passage_embeddings/passage_embedder.h
@@ -33,6 +33,10 @@ class PassageEmbedder : public mojom::PassageEmbedder {
   // to be called before the model can be executed.
   void SetEmbeddingsModelInputWindowSize(uint32_t size);
 
+  // mojom::PassageEmbedder:
+  void GenerateEmbeddings(const std::vector<std::string>& inputs,
+                          GenerateEmbeddingsCallback callback) override;
+
  private:
   // Loads the text embeddings tflite model from the bytes in the given file.
   // Return true if successful.
@@ -47,10 +51,6 @@ class PassageEmbedder : public mojom::PassageEmbedder {
 
   // Executes the model to generate text embeddings result for the input.
   std::optional<OutputType> Execute(InputType input);
-
-  // mojom::PassageEmbedder:
-  void GenerateEmbeddings(const std::vector<std::string>& inputs,
-                          GenerateEmbeddingsCallback callback) override;
 
   mojo::Receiver<mojom::PassageEmbedder> receiver_;
 
