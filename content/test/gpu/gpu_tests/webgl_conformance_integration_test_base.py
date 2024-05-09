d81bf6f04d0b9ee7797a28458f8d4b6ff4779627
Brian Sheedy <bsheedy@chromium.org>
2024-05-09 11:21:53 +0000

    Scale context lost timeouts
    Scales the timeouts use in the context lost tests in order to account
    for additional slowness when running with ASan enabled.
    
    This involves moving the existing ASan code from WebGL/WebGPU tests into
    the base GPU integration test class.
    
    Bug: 338574390
    Change-Id: I42ccd54351270af92fc8a23d30d6bcb593c8d961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528154
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298583}

--- content/test/gpu/gpu_tests/webgl_conformance_integration_test_base.py
+++ content/test/gpu/gpu_tests/webgl_conformance_integration_test_base.py
@@ -75,7 +75,6 @@ class WebGLConformanceIntegrationTestBase(
     gpu_integration_test.GpuIntegrationTest):
 
   _webgl_version: Optional[int] = None
-  is_asan = False
   _crash_count = 0
   _gl_backend = ''
   _angle_backend = ''
@@ -398,7 +397,7 @@ class WebGLConformanceIntegrationTestBase(
     # Parallel jobs increase load and can slow down test execution, so scale
     # based on the number of jobs. Target 2x increase with 4 jobs.
     multiplier = 1 + (self.child.jobs - 1) / 3.0
-    if self.is_asan:
+    if self._is_asan:
       multiplier *= ASAN_MULTIPLIER
     if self._finder_options.browser_type == 'web-engine-shell':
       multiplier *= WEBENGINE_MULTIPLIER
@@ -460,7 +459,7 @@ class WebGLConformanceIntegrationTestBase(
 
   def _GetTestTimeout(self) -> int:
     timeout = 300
-    if self.is_asan:
+    if self._is_asan:
       # Asan runs much slower and needs a longer timeout
       timeout *= 2
     return timeout
@@ -638,13 +637,6 @@ class WebGLConformanceIntegrationTestBase(
   def GetPlatformTags(cls, browser: ct.Browser) -> List[str]:
     assert cls._webgl_version is not None
     tags = super().GetPlatformTags(browser)
-
-    system_info = browser.GetSystemInfo()
-    gpu_info = None
-    if system_info:
-      gpu_info = system_info.gpu
-      cls.is_asan = gpu_info.aux_attributes.get('is_asan', False)
-
     return tags
 
   @classmethod
