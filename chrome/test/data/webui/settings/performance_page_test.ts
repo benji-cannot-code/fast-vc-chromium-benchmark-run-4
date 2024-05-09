b4e8e50265601dce6e74655f611ed785eb37a2e6
rbpotter <rbpotter@chromium.org>
2024-05-09 23:18:02 +0000

    Settings: Speculatively re-enable test
    Flaky failure could not be reproduced locally with a large (20) number
    of attempts, so fix is speculative.
    
    Adding an await on the expected 'show-tooltip' event, that triggers
    actually updating the cr-tooltip, before waiting for microtasksFinished
    to allow rendering to complete.
    
    Bug: 337336425
    Change-Id: I5bd8399367a82b3d7805bb7b996d5350846c2539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528766
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298964}

--- chrome/test/data/webui/settings/performance_page_test.ts
+++ chrome/test/data/webui/settings/performance_page_test.ts
@@ -320,8 +320,7 @@ suite('TabDiscardExceptionList', function() {
     assertTrue(exceptionList.$.noSitesAdded.hidden);
   });
 
-  // TODO(crbug.com/337336425): Flaky on Windows and Linux.
-  test.skip('testManagedExceptionList', async () => {
+  test('testManagedExceptionList', async () => {
     const userRules = 3;
     const managedRules = 3;
     setupExceptionListEntries(
@@ -338,7 +337,9 @@ suite('TabDiscardExceptionList', function() {
     const tooltip = exceptionList.$.tooltip.$.tooltip;
     assertTrue(!!tooltip);
     assertTrue(tooltip.hidden);
+    const onShowTooltip = eventToPromise('show-tooltip', exceptionList);
     indicator.dispatchEvent(new Event('focus'));
+    await onShowTooltip;
     await microtasksFinished();
     assertEquals(
         CrPolicyStrings.controlledSettingPolicy,
