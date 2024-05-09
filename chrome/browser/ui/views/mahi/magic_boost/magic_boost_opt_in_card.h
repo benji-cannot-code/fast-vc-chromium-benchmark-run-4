dc4248176311549b3699daea1366470d253bedfb
Kevin Radtke <kradtke@chromium.org>
2024-05-09 01:09:03 +0000

    mahi: Opt-in card view
    Implement UI for the new mahi opt-in card.
    
    screenshot: https://screenshot.googleplex.com/4PHDM6udgeXBG3D
    
    Bug: b:337868994
    Change-Id: I07402f60005abb9ab331ca87575cb89fbaaa4f8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528156
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298464}

---
+++ chrome/browser/ui/views/mahi/magic_boost/magic_boost_opt_in_card.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_MAHI_MAGIC_BOOST_MAGIC_BOOST_OPT_IN_CARD_H_
+#define CHROME_BROWSER_UI_VIEWS_MAHI_MAGIC_BOOST_MAGIC_BOOST_OPT_IN_CARD_H_
+
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/layout/flex_layout_view.h"
+
+namespace chromeos {
+
+// The Magic Boost opt-in card view.
+class MagicBoostOptInCard : public views::FlexLayoutView {
+  METADATA_HEADER(MagicBoostOptInCard, views::FlexLayoutView)
+
+ public:
+  MagicBoostOptInCard();
+  MagicBoostOptInCard(const MagicBoostOptInCard&) = delete;
+  MagicBoostOptInCard& operator=(const MagicBoostOptInCard&) = delete;
+  ~MagicBoostOptInCard() override;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_UI_VIEWS_MAHI_MAGIC_BOOST_MAGIC_BOOST_OPT_IN_CARD_H_
