cea8710e66b8483f6360f80216d508636652df2a
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-05-09 17:48:53 +0000

    Revert "webnn: Enforce input data type constraints for gather indices"
    This reverts commit ef29e11bfb1f1f4eae0935fb58f0e4129620837b.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5728899092709376
    
    Sample build with failed test: https://ci.chromium.org/b/8748452022241283393
    Affected test(s):
    [ninja://services:services_unittests/WebNNGraphImplBackendTest.BuildAndComputeSingleOperatorGather](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fservices:services_unittests%2FWebNNGraphImplBackendTest.BuildAndComputeSingleOperatorGather?q=VHash%3A3db3852ce13cc70a)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5728899092709376&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5520313&type=BUG
    
    Original change's description:
    > webnn: Enforce input data type constraints for gather indices
    >
    > As specified in https://github.com/webmachinelearning/webnn/pull/646
    >
    > Bug: 328567884
    > Change-Id: I33eba7e1def430b1cb94e3e7a4868e82c5bbd9a3
    > Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel,mac14.arm64-blink-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5520313
    > Reviewed-by: Austin Sullivan <asully@chromium.org>
    > Commit-Queue: Lisha Guo <lisha.guo@intel.com>
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1298443}
    >
    
    Bug: 328567884
    Change-Id: Iab874e915f5603861929cdf07ba4c09698b76503
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528819
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298718}

--- services/webnn/coreml/graph_builder.cc
+++ services/webnn/coreml/graph_builder.cc
@@ -1585,11 +1585,6 @@ base::expected<void, mojom::ErrorPtr> GraphBuilder::AddOperationForGather(
     return NewNotSupportedError("Unsupported input datatype.");
   }
 
-  CHECK(indices_operand_info.mil_data_type ==
-            CoreML::Specification::MILSpec::DataType::UINT32 ||
-        indices_operand_info.mil_data_type ==
-            CoreML::Specification::MILSpec::DataType::INT64);
-
   // TODO: crbug.com/338640913 - figure out what data type should be allowed for
   // WebNN.
   static constexpr auto kSupportedGatherIndicesTypes =
@@ -1601,7 +1596,6 @@ base::expected<void, mojom::ErrorPtr> GraphBuilder::AddOperationForGather(
           indices_operand_info.mil_data_type)) {
     return NewNotSupportedError("Unsupported indices datatype.");
   }
-
   static constexpr char kParamIndices[] = "indices";
   static constexpr char kParamValidateIndices[] = "validate_indices";
 
