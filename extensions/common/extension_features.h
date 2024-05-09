fe8ae557b712a77a360412785c7bcc025f3ef990
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-05-09 18:56:50 +0000

    [Extensions] Add fieldtrial params for mv2 exception list
    Chrome has announced that MV2 extensions will gradually no longer be
    supported in the browser. The first step of this is going to be a user-
    visible warning, with future stages removing slowly support. None of
    these changes will take effect on any Chrome channel before June, 2024.
    See more information at
    https://developer.chrome.com/blog/resuming-the-transition-to-mv3.
    
    This CL introduces a finch feature of an MV2 exception list, which
    will allow server-side configuration of any single extension that is
    identified as critically needing a temporary exception from the MV2
    deprecation experiments. This shouldn't be needed, but is in place as
    a safeguard.
    
    Bug: 337191307
    Change-Id: Ib2a447a827599ab722a3bdb8036588bc48761926
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528158
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298773}

--- extensions/common/extension_features.h
+++ extensions/common/extension_features.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EXTENSIONS_COMMON_EXTENSION_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace extensions_features {
 
@@ -92,6 +93,11 @@ BASE_DECLARE_FEATURE(kExtensionDynamicURLRedirection);
 // Controls displaying a warning that affected MV2 extensions may no longer be
 // supported.
 BASE_DECLARE_FEATURE(kExtensionManifestV2DeprecationWarning);
+
+// Allows server-side configuration of a temporary exception list.
+BASE_DECLARE_FEATURE(kExtensionManifestV2ExceptionList);
+extern const base::FeatureParam<std::string>
+    kExtensionManifestV2ExceptionListParam;
 
 // Side panel API availability.
 BASE_DECLARE_FEATURE(kExtensionSidePanelIntegration);
