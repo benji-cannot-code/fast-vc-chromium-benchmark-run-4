83d5b08f6186171a99563eff609a89ae11f87122
Peilin Wang <peilinwang@google.com>
2024-05-09 18:19:55 +0000

    Browser controls in viz 1: flags
    First cl of many. Future cl's will include metrics and java+viz work for token implementation and to move quads based on offset tags.
    
    Bug: 325471990
    Change-Id: I4f8ad718e893391529e77aa45d0d873f6783c5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521915
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Peilin Wang <peilinwang@google.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298742}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/subresource_filter/core/browser/subresource_filter_features.h"
 #include "components/supervised_user/core/common/features.h"
 #include "components/sync/base/features.h"
+#include "components/viz/common/features.h"
 #include "components/webapps/browser/features.h"
 #include "content/public/common/content_features.h"
 #include "device/fido/features.h"
@@ -113,6 +114,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &download::features::kDownloadsMigrateToJobsAPI,
     &base::features::kCollectAndroidFrameTimelineMetrics,
     &download::features::kDownloadNotificationServiceUnifiedAPI,
+    &features::kAndroidBrowserControlsInViz,
     &features::kGenericSensorExtraClasses,
     &features::kBackForwardCache,
     &features::kBoardingPassDetector,
