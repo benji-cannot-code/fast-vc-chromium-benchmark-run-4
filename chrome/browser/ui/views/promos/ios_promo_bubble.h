e7cf5e5467e5724080db4ceb106122066de85a3e
aliaaborhamy <aliaaborhamy@google.com>
2024-05-09 18:44:28 +0000

    [Desktop to iOS] Add generic promo delegate and update promo utils.
    This CL includes the update of promo_utils and the generic
    promo delegate that is serving the shared promo UI view
    https://crrev.com/c/5490402.
    
    Aiming to phase out the legacy promo but building out a
    parameterized version of all the utils and views, which
    is why there are duplicates for now, but they will be
    cleaned up (crbug.com/339262105).
    
    
    Bug: 335866652
    Change-Id: I6b2af63d8e70a13e3d8148d358e0f30f0847574b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519309
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298762}

--- chrome/browser/ui/views/promos/ios_promo_bubble.h
+++ chrome/browser/ui/views/promos/ios_promo_bubble.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_PROMOS_IOS_PROMO_BUBBLE_H_
 #define CHROME_BROWSER_UI_VIEWS_PROMOS_IOS_PROMO_BUBBLE_H_
 
+// #include "chrome/browser/promos/promos_types.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
-#include "ios_promo_constants.h"
-#include "ios_promo_types.h"
+#include "chrome/browser/ui/views/promos/ios_promo_constants.h"
 
 namespace views {
 class PageActionIconView;
@@ -16,6 +16,8 @@ class View;
 }  // namespace views
 
 class Browser;
+
+enum class IOSPromoType;
 
 // A view for the iOS bubble promo which displays a QR code.
 class IOSPromoBubble {
--- chrome/browser/ui/views/promos/ios_promo_bubble_browsertest.cc
+++ chrome/browser/ui/views/promos/ios_promo_bubble_browsertest.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/promos/promos_pref_names.h"
+#include "chrome/browser/promos/promos_types.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/toolbar_button_provider.h"
-#include "chrome/browser/ui/views/promos/ios_promo_types.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "content/public/test/browser_test.h"
@@ -35,7 +35,7 @@ class IOSPromoBubbleTest : public DialogBrowserTest {
         button_provider->GetAnchorView(PageActionIconType::kManagePasswords),
         button_provider->GetPageActionIconView(
             PageActionIconType::kManagePasswords),
-        browser(), IOSPromoType::kIOSPasswordPromo);
+        browser(), IOSPromoType::kPassword);
   }
 };
 
