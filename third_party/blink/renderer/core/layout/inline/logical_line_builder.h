656dd67b415fe9ab2dd57934ab761d29a60018dc
Kent Tamura <tkent@chromium.org>
2024-05-09 11:15:52 +0000

    RubyLB: Move RebuildBoxStates() to LogicalLineBuilder
    * Pass InlineBreakToken::StartItemIndex() instead of InlineBreakToken*.
    
    * Remove the InlineLayoutStateStack* argument. The data member of
      LogicalLineBuilder is used.
    
    * Make HandleOpenTag() private, and remove the InlineLayoutStateStack*
      argument.
    
    This is a preparation to rebuild ruby-text states.
    This CL has no behavior changes.
    
    Bug: 324111880
    Change-Id: I230bb816d689aab6f364ad4e01736c7366dc3b6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528636
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298581}

--- third_party/blink/renderer/core/layout/inline/logical_line_builder.h
+++ third_party/blink/renderer/core/layout/inline/logical_line_builder.h
@@ -30,6 +30,8 @@ class LogicalLineBuilder {
                      InlineLayoutStateStack* state_stack,
                      InlineChildLayoutContext* context);
 
+  void RebuildBoxStates(const LineInfo& line_info, wtf_size_t end_item_index);
+
   // `main_line_helper` can be nullptr if the line is for ruby annotations.
   void CreateLine(LineInfo* line_info,
                   LogicalLineItems* line_box,
@@ -47,17 +49,15 @@ class LogicalLineBuilder {
     return has_relative_positioned_items_;
   }
 
-  InlineBoxState* HandleOpenTag(const InlineItem&,
-                                const InlineItemResult&,
-                                LogicalLineItems*,
-                                InlineLayoutStateStack*) const;
-
  private:
   InlineBoxState* HandleItemResults(const LineInfo& line_info,
                                     InlineItemResults& line_items,
                                     LogicalLineItems* line_box,
                                     InlineLayoutAlgorithm* main_line_helper,
                                     InlineBoxState* box);
+  InlineBoxState* HandleOpenTag(const InlineItem&,
+                                const InlineItemResult&,
+                                LogicalLineItems*);
   InlineBoxState* HandleCloseTag(const InlineItem&,
                                  const InlineItemResult&,
                                  LogicalLineItems* line_box,
