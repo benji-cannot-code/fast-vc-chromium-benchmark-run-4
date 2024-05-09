b77e000ae7594aa193551190470ccdb21075527f
Daniel Cheng <dcheng@chromium.org>
2024-05-09 22:20:34 +0000

    Use absl::Cleanup to ensure memory is zeroed //components/os_crypt
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: Ia6bfc6c6987cb79344043aa110fdb0f944babe3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530310
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298922}

--- components/os_crypt/async/common/encryptor.cc
+++ components/os_crypt/async/common/encryptor.cc
@@ -18,14 +18,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/aead.h"
 #include "crypto/random.h"
 #include "mojo/public/cpp/bindings/default_construct_tag.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
 
 #include <dpapi.h>
 
-#include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
 #include "components/os_crypt/async/common/encryptor_features.h"
 #endif
 
@@ -110,9 +109,9 @@ std::vector<uint8_t> Encryptor::Key::Encrypt(
 #if BUILDFLAG(IS_WIN)
       // Copy. This makes it thread safe. Must outlive aead.
       std::vector<uint8_t> decrypted_key(key_);
-      base::ScopedClosureRunner zero_memory(
-          base::BindOnce(base::IgnoreResult(&::SecureZeroMemory),
-                         std::data(decrypted_key), std::size(decrypted_key)));
+      absl::Cleanup zero_memory = [&decrypted_key] {
+        ::SecureZeroMemory(decrypted_key.data(), decrypted_key.size());
+      };
 
       if (encrypted_) {
         ::CryptUnprotectMemory(std::data(decrypted_key),
@@ -155,9 +154,9 @@ std::optional<std::vector<uint8_t>> Encryptor::Key::Decrypt(
 #if BUILDFLAG(IS_WIN)
       // Copy. This makes it thread safe. Must outlive aead.
       std::vector<uint8_t> decrypted_key(key_);
-      base::ScopedClosureRunner zero_memory(
-          base::BindOnce(base::IgnoreResult(&::SecureZeroMemory),
-                         std::data(decrypted_key), std::size(decrypted_key)));
+      absl::Cleanup zero_memory = [&decrypted_key] {
+        ::SecureZeroMemory(decrypted_key.data(), decrypted_key.size());
+      };
       if (encrypted_) {
         ::CryptUnprotectMemory(std::data(decrypted_key),
                                std::size(decrypted_key),
