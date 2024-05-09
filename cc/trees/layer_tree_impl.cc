80e6ac872d2a951ec0af328087493c9114c15876
Victor Miura <vmiura@chromium.org>
2024-05-09 19:07:53 +0000

    [cc-slimming] Propagate property tree invalidations without pushing layers
    Reverts https://chromium-review.googlesource.com/c/chromium/src/+/4081632
    and replaces it with a lighter weight invalidation of draw properties
    when property changes are detected on layers.
    
    Bug: 338427404, 40246766
    Change-Id: I2f4f96a66b7c327978efe5ccdffd42c6b38fe032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528760
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Victor Miura <vmiura@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298784}

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -661,6 +661,8 @@ void LayerTreeImpl::PullPropertiesFrom(
   unsafe_state.mutator_host->PushPropertiesTo(mutator_host(),
                                               unsafe_state.property_trees);
 
+  // Make sure that property tree based changes are moved to layers
+  // and draw properties are invalidated.
   MoveChangeTrackingToLayers();
 
   lifecycle().AdvanceTo(LayerTreeLifecycle::kNotSyncing);
@@ -893,6 +895,10 @@ void LayerTreeImpl::PushPropertiesTo(LayerTreeImpl* target_tree) {
 
   for (auto& request : TakeViewTransitionRequests())
     target_tree->AddViewTransitionRequest(std::move(request));
+
+  // Make sure that property tree based changes are moved to layers
+  // and draw properties are invalidated.
+  target_tree->MoveChangeTrackingToLayers();
 }
 
 void LayerTreeImpl::HandleTickmarksVisibilityChange() {
