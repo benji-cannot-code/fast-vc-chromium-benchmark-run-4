7f0ff95213876d95f073519c83b81d3ea8cd2761
Ilya Sherman <isherman@chromium.org>
2024-05-09 23:49:57 +0000

    Show the Feedback UI for dogfood clients
    By default, enterprise policy disables both model quality logs *and* the
    Feedback UI. This is appropriate for most enterprise users; but for
    dogfood users, it is more appropriate to enable the Feedback UI (while
    leaving the model quality logs disabled).
    
    R=curranmax@chromium.org
    
    Bug: b/335328673
    Change-Id: Idce50091736068aefa06a107ede98ffdf122477b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519598
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Auto-Submit: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298985}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/models.pb.h"
 #include "components/prefs/pref_service.h"
 #include "components/user_prefs/user_prefs.h"
+#include "components/variations/service/variations_service.h"
 #include "components/variations/synthetic_trials.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/browser_thread.h"
@@ -714,14 +715,23 @@ bool OptimizationGuideKeyedService::ShouldFeatureBeCurrentlyEnabledForUser(
       ->ShouldFeatureBeCurrentlyEnabledForUser(feature);
 }
 
-bool OptimizationGuideKeyedService::ShouldFeatureBeCurrentlyAllowedForLogging(
+bool OptimizationGuideKeyedService::ShouldFeatureBeCurrentlyAllowedForFeedback(
     optimization_guide::UserVisibleFeatureKey feature) const {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!model_execution_features_controller_) {
-    return false;
+
+  // If logging is enabled, feedback is always also enabled.
+  if (model_execution_features_controller_ &&
+      model_execution_features_controller_
+          ->ShouldFeatureBeCurrentlyAllowedForLogging(feature)) {
+    return true;
   }
-  return model_execution_features_controller_
-      ->ShouldFeatureBeCurrentlyAllowedForLogging(feature);
+
+  // Otherwise, feedback is disabled, with one exception: On dogfood clients,
+  // feedback is always enabled (as long as the feature is enabled).
+  auto* variations_service = g_browser_process->variations_service();
+  bool is_dogfood_client =
+      !!variations_service && variations_service->IsLikelyDogfoodClient();
+  return is_dogfood_client && ShouldFeatureBeCurrentlyEnabledForUser(feature);
 }
 
 bool OptimizationGuideKeyedService::ShouldShowExperimentalAIPromo() const {
--- chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
@@ -587,7 +587,7 @@ void TabSearchPageHandler::TriggerFeedback(int32_t session_id) {
   OptimizationGuideKeyedService* opt_guide_keyed_service =
       OptimizationGuideKeyedServiceFactory::GetForProfile(browser->profile());
   if (!opt_guide_keyed_service ||
-      !opt_guide_keyed_service->ShouldFeatureBeCurrentlyAllowedForLogging(
+      !opt_guide_keyed_service->ShouldFeatureBeCurrentlyAllowedForFeedback(
           optimization_guide::UserVisibleFeatureKey::kTabOrganization)) {
     return;
   }
