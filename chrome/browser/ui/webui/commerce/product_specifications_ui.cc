58920abb47a8d8db55a55663bf7e2b8a2698baaf
Paul Adedeji <pauladedeji@google.com>
2024-05-09 18:51:27 +0000

    Fix issue where product images don't show.
    *Demos of problem and solution in bug description/comments.
    
    Problem: Images only show when the NTP is opened first, because the
             `product_specifications` backend doesn't properly instantiate
             the `chrome://image` data source (and the NTP does).
    Solution: Instantiate the `chrome://image` data source in
              `product_specifications` backend.
    
    Change-Id: Ie62de1250bbd2506d372e81a6412c7920b5a357b
    Bug: 339646755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529761
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1298770}

--- chrome/browser/ui/webui/commerce/product_specifications_ui.cc
+++ chrome/browser/ui/webui/commerce/product_specifications_ui.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/feature_engagement/tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/sanitized_image_source.h"
 #include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/browser/ui/webui/webui_util.h"
 #include "chrome/common/webui_url_constants.h"
@@ -38,8 +39,11 @@ ProductSpecificationsUI::ProductSpecificationsUI(content::WebUI* web_ui)
           kProductSpecifications, kProductSpecificationsRegionLaunched)) {
     return;
   }
-  // Add ThemeSource to serve chrome logo.
+  // Add ThemeSource to serve the chrome logo.
   content::URLDataSource::Add(profile, std::make_unique<ThemeSource>(profile));
+  // Add SanitizedImageSource to embed images in WebUI.
+  content::URLDataSource::Add(profile,
+                              std::make_unique<SanitizedImageSource>(profile));
 
   // Set up the chrome://compare source.
   content::WebUIDataSource* source = content::WebUIDataSource::CreateAndAdd(
