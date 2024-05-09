b838f3b203eccabdfce3779972471a3163451c31
Ana Salazar <anasalazar@chromium.org>
2024-05-09 17:17:53 +0000

    Set up AppsCollections counterfactual experimental arm
    AppsCollections should be enabled for ~15% of new users.  However, in
    order to have a correct sample size for experiments, we need to set up a
    'counterfactual arm'. This way, the population of users who will report
    to have AppsCollections enabled will be separated into the Enabled arm
    and the Counterfactual arm, and that way a guarateed population is set
    up for the experiment analysis.
    
    This CL also modifies the relevant metrics for the experiment to
    consider the correct experiment arm.
    
    OBSOLETE_HISTOGRAM[Apps.AppList.AppsCollections.AppLaunchesByCategory]=Replaced by Apps.AppListBubble.{AppListPage}.AppLaunchesByCategory
    
    Bug: None
    Change-Id: Ifbce84f61dc14113d205c543c198752f910437d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524333
    Commit-Queue: Ana Salazar <anasalazar@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Yulun Wu <yulunwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298700}

--- ash/public/cpp/app_list/app_list_features.h
+++ ash/public/cpp/app_list/app_list_features.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/time/time.h"
 
 namespace app_list_features {
@@ -43,6 +44,11 @@ ASH_PUBLIC_EXPORT BASE_DECLARE_FEATURE(kDragAndDropRefactor);
 // different categories filled with predefined apps.
 ASH_PUBLIC_EXPORT BASE_DECLARE_FEATURE(kAppsCollections);
 
+// Whether the Apps Collections is enabled counterfactually as part of an
+// experiment arm.
+const base::FeatureParam<bool> kAppsCollectionsEnabledCounterfactually{
+    &kAppsCollections, "is-counterfactual", false};
+
 // Forces the user elegibility of the AppsCollections feature. When this is
 // enabled, the regular checks for the account are ignored and AppsCollection
 // show by default.
@@ -59,6 +65,7 @@ ASH_PUBLIC_EXPORT base::TimeDelta DynamicSearchUpdateAnimationDuration();
 ASH_PUBLIC_EXPORT bool IsLauncherPlayStoreSearchEnabled();
 ASH_PUBLIC_EXPORT bool IsDragAndDropRefactorEnabled();
 ASH_PUBLIC_EXPORT bool IsAppsCollectionsEnabled();
+ASH_PUBLIC_EXPORT bool IsAppsCollectionsEnabledCounterfactually();
 ASH_PUBLIC_EXPORT bool IsForceShowAppsCollectionsEnabled();
 
 }  // namespace app_list_features
