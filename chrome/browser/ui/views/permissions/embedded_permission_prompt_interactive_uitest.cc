78c5c3f22e4b6f65aba886fe07a297e1a0216827
Yi Gu <yigu@chromium.org>
2024-05-09 16:22:26 +0000

    [Gardener] Disable TestPepcHistograms on all platforms
    The test is flaky across platforms:
    https://ci.chromium.org/ui/test/chrome/ninja%3A%2F%2Fchrome%2Ftest%3Ainteractive_ui_tests%2FEmbeddedPermissionPromptInteractiveTest.TestPepcHistograms
    
    Bug: 40275129
    Change-Id: I4185d9259d1aaf21fb6fe9f69d45b8aaa7548bdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526281
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Owners-Override: Yi Gu <yigu@chromium.org>
    Auto-Submit: Yi Gu <yigu@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1298665}

--- chrome/browser/ui/views/permissions/embedded_permission_prompt_interactive_uitest.cc
+++ chrome/browser/ui/views/permissions/embedded_permission_prompt_interactive_uitest.cc
@@ -313,7 +313,7 @@ class EmbeddedPermissionPromptInteractiveTest : public InteractiveBrowserTest {
 #define MAYBE_TestPartialPermissionsLabels TestPartialPermissionsLabels
 #define MAYBE_TestPermissionElementDialogPositioning \
   TestPermissionElementDialogPositioning
-#define MAYBE_TestPepcHistograms TestPepcHistograms
+#define MAYBE_TestPepcHistograms DISABLED_TestPepcHistograms
 #endif
 IN_PROC_BROWSER_TEST_F(EmbeddedPermissionPromptInteractiveTest,
                        MAYBE_BasicFlowMicrophone) {
