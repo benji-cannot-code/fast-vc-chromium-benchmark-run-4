2fb1dfe66ac1e56c2e60ddc5b919cbcbab11dd1d
mei1127 <shanxing.mei@intel.com>
2024-05-09 05:51:52 +0000

    webnn: Migrate Reduce validation tests to WPTs
    This CL adds WPT tests for reduce and removes the unit tests
    `MLGraphBuilderTest.ReduceTest`, `MLGraphTestMojo.ReduceTest` and
    `MLGraphTest.ReduceTest`.
    
    Bug: 327337526, 328026885
    Change-Id: I26717e0ffdbd2b5cd18f70fb9cefba5d9e719a69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5496312
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Shanxing Mei <shanxing.mei@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1298528}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_test.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gru_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_lstm_options.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_operand_data_type.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_reduce_options.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph.h"
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_builder_utils.h"
@@ -65,13 +64,6 @@ MLOperand* BuildGemm(V8TestingScope& scope,
                      const MLOperand* a,
                      const MLOperand* b,
                      const MLGemmOptions* options = MLGemmOptions::Create());
-
-MLOperand* BuildReduce(
-    V8TestingScope& scope,
-    MLGraphBuilder* builder,
-    webnn::mojom::blink::Reduce::Kind kind,
-    const MLOperand* input,
-    const MLReduceOptions* options = MLReduceOptions::Create());
 
 }  // namespace blink
 
