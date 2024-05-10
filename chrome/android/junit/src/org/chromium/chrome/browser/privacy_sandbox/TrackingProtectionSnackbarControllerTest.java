6cae38f76b85906079acaa80e0c94af8bd671a49
Matt Dembski <dembski@google.com>
2024-05-10 00:37:21 +0000

    [PWA][Heuristic] CookiesControlBridge used to handle TrackingProtection snackbar for PWA.
    Snackbar (and the UserBypass) should be displayed to the users in the situation of problems / issues. For now we use a simple heuristic to determine whether there are problems with a specific site. We check refresh hits in 30 seconds time window. If that's greater than 2, we treat this situation as a possible breakage and displays the UserBypass control (in the case of PWA - that's snackbar + page info).
    
    This heuristic is used across different surfaces: Desktop, BrowserApp, CCT. We are adding it to the PWA now.
    
    Bug: 327660595
    Change-Id: I8cefe967f1df29785a58ee2dca6590673c8519d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523749
    Commit-Queue: Matt Dembski <dembski@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299004}

--- chrome/android/junit/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionSnackbarControllerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionSnackbarControllerTest.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -22,9 +23,11 @@ import org.robolectric.annotation.Config;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
+import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ActivityType;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
+import org.chromium.components.content_settings.CookieControlsBridgeJni;
 
 /** Test for Tracking Protection Snackbar controller for WebApk. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -35,9 +38,15 @@ public class TrackingProtectionSnackbarControllerTest {
 
     @Mock private SnackbarManager mSnackbarManagerMock;
 
+    @Rule public JniMocker mocker = new JniMocker();
+
+    private FakeCookieControlsBridge mFakeCookieControlsBridge;
+
     @Before
     public void setUp() {
         MockitoAnnotations.openMocks(this);
+        mFakeCookieControlsBridge = new FakeCookieControlsBridge();
+        mocker.mock(CookieControlsBridgeJni.TEST_HOOKS, mFakeCookieControlsBridge);
     }
 
     @Test
@@ -46,9 +55,10 @@ public class TrackingProtectionSnackbarControllerTest {
         doNothing().when(mSnackbarManagerMock).showSnackbar(any());
         doReturn(mSnackbarManagerMock).when(mSnackbarManagerSupplierMock).get();
         TrackingProtectionSnackbarController controller =
-                new TrackingProtectionSnackbarController(null, mSnackbarManagerSupplierMock);
+                new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.WEB_APK);
 
-        controller.showSnackbar(ActivityType.WEB_APK);
+        controller.showSnackbar();
         verify(mSnackbarManagerMock, times(1)).showSnackbar(any());
     }
 
@@ -57,14 +67,26 @@ public class TrackingProtectionSnackbarControllerTest {
     public void testShowSnackbarForNonWebApk() {
         doNothing().when(mSnackbarManagerMock).showSnackbar(any());
         doReturn(mSnackbarManagerMock).when(mSnackbarManagerSupplierMock).get();
-        TrackingProtectionSnackbarController controller =
-                new TrackingProtectionSnackbarController(null, mSnackbarManagerSupplierMock);
 
-        controller.showSnackbar(ActivityType.TRUSTED_WEB_ACTIVITY);
-        controller.showSnackbar(ActivityType.CUSTOM_TAB);
-        controller.showSnackbar(ActivityType.PRE_FIRST_TAB);
-        controller.showSnackbar(ActivityType.TABBED);
-        controller.showSnackbar(ActivityType.WEBAPP);
+        new TrackingProtectionSnackbarController(
+                        null,
+                        mSnackbarManagerSupplierMock,
+                        null,
+                        null,
+                        ActivityType.TRUSTED_WEB_ACTIVITY)
+                .showSnackbar();
+        new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.CUSTOM_TAB)
+                .showSnackbar();
+        new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.PRE_FIRST_TAB)
+                .showSnackbar();
+        new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.TABBED)
+                .showSnackbar();
+        new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.WEBAPP)
+                .showSnackbar();
 
         verifyNoInteractions(mSnackbarManagerMock);
         verifyNoInteractions(mSnackbarManagerSupplierMock);
@@ -76,9 +98,10 @@ public class TrackingProtectionSnackbarControllerTest {
         doNothing().when(mSnackbarManagerMock).showSnackbar(any());
         doReturn(mSnackbarManagerMock).when(mSnackbarManagerSupplierMock).get();
         TrackingProtectionSnackbarController controller =
-                new TrackingProtectionSnackbarController(null, mSnackbarManagerSupplierMock);
+                new TrackingProtectionSnackbarController(
+                        null, mSnackbarManagerSupplierMock, null, null, ActivityType.WEB_APK);
 
-        controller.showSnackbar(ActivityType.WEB_APK);
+        controller.showSnackbar();
 
         verifyNoInteractions(mSnackbarManagerMock);
         verifyNoInteractions(mSnackbarManagerSupplierMock);
