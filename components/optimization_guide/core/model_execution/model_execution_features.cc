62cbf33c1b7e0cef592ff4b84c626f65bc400971
Sophie Chang <sophiechang@chromium.org>
2024-05-10 23:32:33 +0000

    Onboard new enum for prompt API
    Bug: 339702934
    Change-Id: I3615bead0de8252e33c88ae7bb433cad71a35fd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529351
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299594}

--- components/optimization_guide/core/model_execution/model_execution_features.cc
+++ components/optimization_guide/core/model_execution/model_execution_features.cc
@@ -54,6 +54,10 @@ BASE_FEATURE(kOnDeviceModelTestFeature,
              "OnDeviceModelTestFeature",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kOnDeviceModelPromptApiFeature,
+             "OnDeviceModelPromptApiFeature",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsGraduatedFeature(UserVisibleFeatureKey feature) {
   bool is_graduated = false;
   switch (feature) {
@@ -111,6 +115,8 @@ bool IsOnDeviceModelEnabled(ModelBasedCapabilityKey feature) {
     case ModelBasedCapabilityKey::kWallpaperSearch:
     case ModelBasedCapabilityKey::kTextSafety:
       return false;
+    case ModelBasedCapabilityKey::kPromptApi:
+      return true;
   }
 }
 
@@ -121,6 +127,9 @@ bool IsOnDeviceModelAdaptationEnabled(ModelBasedCapabilityKey feature) {
     case ModelBasedCapabilityKey::kTest:
       return base::GetFieldTrialParamByFeatureAsBool(
           kOnDeviceModelTestFeature, "enable_adaptation", false);
+    case ModelBasedCapabilityKey::kPromptApi:
+      return base::GetFieldTrialParamByFeatureAsBool(
+          kOnDeviceModelPromptApiFeature, "enable_adaptation", false);
     case ModelBasedCapabilityKey::kTabOrganization:
     case ModelBasedCapabilityKey::kWallpaperSearch:
     case ModelBasedCapabilityKey::kTextSafety:
