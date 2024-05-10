829c9b6d9c68138a43554f66467cf85a9629862b
Bo Majewski <majewski@chromium.org>
2024-05-10 06:37:16 +0000

    [Files]: Code cleanup.
    Changes the terminology:
    
      text of Term becomes token
      text_bytes of Term becomes token_bytes
      term_id generated from term's text, now token, becomes token_id
      augmented_term_id generated from Term becomes just term_id
    
    Correct the code to follow this pattern. This does not yet move the
    term_table.* and augmented_term_table to new file locations.
    
    Bug: b:327534824
    Change-Id: I9ff4732c2cf55bc8baf6057a292522349e7ade3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528708
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299091}

--- chrome/browser/ash/file_manager/indexing/term.cc
+++ chrome/browser/ash/file_manager/indexing/term.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_manager {
 
-Term::Term(const std::string& field, const std::u16string& text)
-    : field_(field), text_(text), text_bytes_(base::UTF16ToUTF8(text)) {}
+Term::Term(const std::string& field, const std::u16string& token)
+    : field_(field), token_(token), token_bytes_(base::UTF16ToUTF8(token)) {}
 
 Term::~Term() = default;
 
