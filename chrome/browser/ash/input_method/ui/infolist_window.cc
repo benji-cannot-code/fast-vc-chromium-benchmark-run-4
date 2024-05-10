0396306c4c4642997c36d896f8e8e89cc8d5924f
jiawei wang <hellojw513@gmail.com>
2024-05-10 08:42:31 +0000

    Bubble: remove all SizeToContents() in /chrome/browser/ash/input_method/ui
    Remove all SizeToContents() and add InvalidateLayout() when necessary.
    This changes is used to make the bubbles using autosize mode via
    autosize parameter.(autosize mode: When the preferred size of bubble
    changes,its widget automatically resizes itself to meet the content's
    size)
    
    The ultimate goal of this bug is to remove all SizeToContents() to
    reduce developer confusion about when SizeToContents() needs to be
    called.
    
    This CL was uploaded by git cl split.
    
    R=shend@chromium.org
    
    Bug: 41493925
    Change-Id: Ifd77468db68bfdf9a4e21876f787551869130dd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515236
    Reviewed-by: Weidong Liu <weidongliu@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299125}

--- chrome/browser/ash/input_method/ui/infolist_window.cc
+++ chrome/browser/ash/input_method/ui/infolist_window.cc
@@ -187,7 +187,9 @@ void InfolistEntryView::UpdateBackground() {
 InfolistWindow::InfolistWindow(views::View* candidate_window,
                                const std::vector<ui::InfolistEntry>& entries)
     : views::BubbleDialogDelegateView(candidate_window,
-                                      views::BubbleBorder::NONE),
+                                      views::BubbleBorder::NONE,
+                                      views::BubbleBorder::DIALOG_SHADOW,
+                                      true),
       title_font_list_(gfx::Font(kJapaneseFontName, kFontSizeDelta + 15)),
       description_font_list_(
           gfx::Font(kJapaneseFontName, kFontSizeDelta + 11)) {
@@ -234,7 +236,6 @@ void InfolistWindow::InitWidget() {
 
   // BubbleFrameView will be initialized through CreateBubble.
   GetBubbleFrameView()->SetBubbleBorder(std::make_unique<InfolistBorder>());
-  SizeToContents();
 }
 
 void InfolistWindow::Relayout(const std::vector<ui::InfolistEntry>& entries) {
@@ -258,7 +259,6 @@ void InfolistWindow::Relayout(const std::vector<ui::InfolistEntry>& entries) {
   }
 
   DeprecatedLayoutImmediately();
-  SizeToContents();
 }
 
 void InfolistWindow::ShowWithDelay() {
