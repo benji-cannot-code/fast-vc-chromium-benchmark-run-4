c404bddb91eb348a71069a666f01dd50bf69eb54
Johannes Kron <kron@chromium.org>
2024-05-10 11:11:41 +0000

    [AVFoundation] Reenable camera capture stall check log on macOS 14
    Reenable the log but keep the stop/start sequence disabled. This way
    it will be possible to see in the logs if there are camera issues where
    a stall was detected and perhaps could have been fixed by restarting
    the camera.
    
    Bug: 335210401
    Change-Id: I971b9bd22325891b8dd20bdb23b45200af30a7f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522578
    Reviewed-by: Ilya Nikolaevskiy <ilnik@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299172}

--- media/capture/video/apple/video_capture_device_avfoundation.mm
+++ media/capture/video/apple/video_capture_device_avfoundation.mm
@@ -48,8 +48,8 @@ BASE_FEATURE(kAVFoundationCaptureForwardSampleTimestamps,
              "AVFoundationCaptureForwardSampleTimestamps",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kAVFoundationCaptureSonomaStallCheck,
-             "AVFoundationCaptureSonomaStallCheck",
+BASE_FEATURE(kAVFoundationCaptureSonomaRestartStalledCamera,
+             "AVFoundationCaptureSonomaRestartStalledCamera",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 namespace {
@@ -86,12 +86,13 @@ std::optional<base::TimeTicks> GetCMSampleBufferTimestamp(
   return std::nullopt;
 }
 
-bool ShouldRunStallCheck() {
+bool ShouldRestartStalledCamera() {
   // The stall check should not be needed on macOS 14 due to a redesign of the
   // camera capture in macOS 14. It also interferes with the Presenter's Overlay
   // feature that was introduced in macOS 14. See https://crbug.com/335210401.
   if (@available(macOS 14.0, *)) {
-    return base::FeatureList::IsEnabled(kAVFoundationCaptureSonomaStallCheck);
+    return base::FeatureList::IsEnabled(
+        kAVFoundationCaptureSonomaRestartStalledCamera);
   }
   return true;
 }
@@ -581,9 +582,7 @@ AVCaptureDeviceFormat* FindBestCaptureFormat(
     _capturedFirstFrame = false;
     _capturedFrameSinceLastStallCheck = NO;
   }
-  if (ShouldRunStallCheck()) {
-    [self doStallCheck:0];
-  }
+  [self doStallCheck:0];
   return YES;
 }
 
@@ -1032,9 +1031,15 @@ AVCaptureDeviceFormat* FindBestCaptureFormat(
             nextFailedCheckCount),
         kStallCheckInterval);
   } else {
-    [self logMessage:"Capture appears to have stalled, restarting."];
-    [self stopCapture];
-    [self startCapture];
+    if (ShouldRestartStalledCamera()) {
+      [self logMessage:"Capture appears to have stalled, restarting."];
+      [self stopCapture];
+      [self startCapture];
+    } else {
+      [self logMessage:
+                "Capture appears to have stalled, restarting may have helped "
+                "but is disabled. See https://issues.chromium.org/335210401."];
+    }
   }
 }
 
