76ecfa55b6d9be041bc8037584372bf5d74f2831
Katie Dektar <katie@chromium.org>
2024-05-10 14:49:28 +0000

    [a11y] Add feature flag for shake to locate
    There is no feature yet, this just adds the flag.
    
    Bug: b/324271234
    Test: N/A
    Change-Id: I458b4005b781f454ceb1a03821c35ddcc50d58a0
    AX-Relnotes: N/A
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529051
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299226}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -664,6 +664,9 @@ extern const char kAccessibilityOverscrollSettingFeatureDescription[];
 extern const char kAccessibilityServiceName[];
 extern const char kAccessibilityServiceDescription[];
 
+extern const char kAccessibilityShakeToLocateName[];
+extern const char kAccessibilityShakeToLocateDescription[];
+
 extern const char kExperimentalAccessibilityColorEnhancementSettingsName[];
 extern const char
     kExperimentalAccessibilityColorEnhancementSettingsDescription[];
