731d4a31f7818c6f463371ee258621204a4ada27
Andy Paicu <andypaicu@chromium.org>
2024-05-10 19:54:12 +0000

    [PEPC] Enable tabindex by default for permission elements
    Similarly to button controls, the permission should have a default
    tabindex value of "0".
    
    Removed "tabindex" from browser test html as it was no longer necessary.
    This also serves to verify the behavior as expected.
    
    Also did a tiny bit of cleanup by putting all override functions
    together in html_permission_element.h, and removing inline
    implementation.
    
    Fixed: 337828697
    Change-Id: Id6f8bfdf5b99d600ceb7e8cb03b07f4902d06f2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5501039
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299455}

--- third_party/blink/renderer/core/html/html_permission_element.h
+++ third_party/blink/renderer/core/html/html_permission_element.h
@@ -52,6 +52,8 @@ class CORE_EXPORT HTMLPermissionElement final
   void DetachLayoutTree(bool performing_reattach) override;
   void Focus(const FocusParams& params) override;
   bool SupportsFocus(UpdateBehavior) const override;
+  int DefaultTabIndex() const override;
+  CascadeFilter GetCascadeFilter() const override;
 
   bool granted() const { return permissions_granted_; }
 
@@ -62,11 +64,6 @@ class CORE_EXPORT HTMLPermissionElement final
 
   const Member<HTMLSpanElement>& permission_text_span_for_testing() const {
     return permission_text_span_;
-  }
-
-  CascadeFilter GetCascadeFilter() const override {
-    // Reject all properties for which 'kValidForPermissionElement' is false.
-    return CascadeFilter(CSSProperty::kValidForPermissionElement, false);
   }
 
   bool IsFullyVisibleForTesting() const { return is_fully_visible_; }
