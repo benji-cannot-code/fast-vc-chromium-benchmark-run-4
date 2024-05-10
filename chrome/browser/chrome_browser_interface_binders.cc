f74455cc6b7767a343a596ad6d0abf961cdda52c
Gloria Fang <gloriafang@google.com>
2024-05-10 20:16:08 +0000

    Add shoppingservice api to history.
    This makes shoppingServiceApi proxy available to product_specification_lists.ts, so that we can get the product specifications list in the history panel.
    
    Change-Id: I9188008cbac9de3ec7f6bb1b9289bdbaa904d4f5
    Bug: b/338234553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5491504
    Commit-Queue: Gloria Fang <gloriafang@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299475}

--- chrome/browser/chrome_browser_interface_binders.cc
+++ chrome/browser/chrome_browser_interface_binders.cc
@@ -1353,7 +1353,7 @@ void PopulateChromeWebUIFrameBinders(
   RegisterWebUIControllerInterfaceBinder<
       shopping_service::mojom::ShoppingServiceHandlerFactory,
       BookmarksSidePanelUI, commerce::ProductSpecificationsUI,
-      ShoppingInsightsSidePanelUI>(map);
+      ShoppingInsightsSidePanelUI, HistoryUI>(map);
 
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kPerformanceControlsSidePanel)) {
--- chrome/browser/ui/webui/history/history_ui.h
+++ chrome/browser/ui/webui/history/history_ui.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/base/resource/resource_scale_factor.h"
 #include "ui/webui/mojo_web_ui_controller.h"
+#include "ui/webui/resources/cr_components/commerce/shopping_service.mojom.h"
 #include "ui/webui/resources/cr_components/history_clusters/history_clusters.mojom-forward.h"
 #include "ui/webui/resources/cr_components/history_embeddings/history_embeddings.mojom.h"
 
@@ -27,6 +28,10 @@ class HistoryClustersHandler;
 }
 
 class HistoryEmbeddingsHandler;
+
+namespace commerce {
+class ShoppingServiceHandler;
+}  // namespace commerce
 
 namespace page_image_service {
 class ImageServiceHandler;
@@ -43,7 +48,9 @@ class HistoryUIConfig : public content::WebUIConfig {
       const GURL& url) override;
 };
 
-class HistoryUI : public ui::MojoWebUIController {
+class HistoryUI
+    : public ui::MojoWebUIController,
+      public shopping_service::mojom::ShoppingServiceHandlerFactory {
  public:
   explicit HistoryUI(content::WebUI* web_ui);
   HistoryUI(const HistoryUI&) = delete;
@@ -63,6 +70,10 @@ class HistoryUI : public ui::MojoWebUIController {
       mojo::PendingReceiver<page_image_service::mojom::PageImageServiceHandler>
           pending_page_handler);
 
+  void BindInterface(
+      mojo::PendingReceiver<
+          shopping_service::mojom::ShoppingServiceHandlerFactory> receiver);
+
   // For testing only.
   history_clusters::HistoryClustersHandler*
   GetHistoryClustersHandlerForTesting() {
@@ -70,12 +81,19 @@ class HistoryUI : public ui::MojoWebUIController {
   }
 
  private:
+  void CreateShoppingServiceHandler(
+      mojo::PendingRemote<shopping_service::mojom::Page> page,
+      mojo::PendingReceiver<shopping_service::mojom::ShoppingServiceHandler>
+          receiver) override;
   std::unique_ptr<HistoryEmbeddingsHandler> history_embeddings_handler_;
   std::unique_ptr<history_clusters::HistoryClustersHandler>
       history_clusters_handler_;
   std::unique_ptr<page_image_service::ImageServiceHandler>
       image_service_handler_;
   PrefChangeRegistrar pref_change_registrar_;
+  std::unique_ptr<commerce::ShoppingServiceHandler> shopping_service_handler_;
+  mojo::Receiver<shopping_service::mojom::ShoppingServiceHandlerFactory>
+      shopping_service_factory_receiver_{this};
 
   void UpdateDataSource();
 
