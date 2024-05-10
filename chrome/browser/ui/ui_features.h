f60b795016f4b90a097ec56acadcde6ffd5c3d96
David Pennington <dpenning@chromium.org>
2024-05-10 19:46:59 +0000

    [TabStrip] Remove old ChromeOS experiment in TabStrip.
    There was an experiment on ChromeOS at some point to use a frame button
    for tabsearch. This experiment does not seem to have ever been launched,
    and with CR23 we are moving to a more prominent tab search feature that
    has other functionality attached to it that are not compatible with
    prior implementation's frame buttons.
    
    Change-Id: I6b8aa28b2882dd3700f3e3e3c0abc9332bf89449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512600
    Auto-Submit: David Pennington <dpenning@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299446}

--- chrome/browser/ui/ui_features.h
+++ chrome/browser/ui/ui_features.h
@@ -300,10 +300,6 @@ BASE_DECLARE_FEATURE(kWebUITabStrip);
 // tap gesture on the WebUI Tab Strip.
 BASE_DECLARE_FEATURE(kWebUITabStripContextMenuAfterTap);
 
-#if BUILDFLAG(IS_CHROMEOS)
-BASE_DECLARE_FEATURE(kChromeOSTabSearchCaptionButton);
-#endif
-
 // Cocoa to views migration.
 #if BUILDFLAG(IS_MAC)
 BASE_DECLARE_FEATURE(kLocationPermissionsExperiment);
