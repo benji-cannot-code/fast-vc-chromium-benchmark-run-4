a4c9647568e4254f33d246794c12fba106874c2c
Chris Lu <thegreenfrog@chromium.org>
2024-05-10 23:46:11 +0000

    [ios] Add IOS.MagicStack.Start/NTP.SegmentationRankingFetchTime
    These metrics will measure the fetch time for the Magic Stack
    Segmentation ranking to validate hypothesis that Start fetches take
    much longer.
    
    Change-Id: I3aa3a9ae83e958768ce831426bc95d86b74e0896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530559
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299600}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model.h"
 
+#import "base/metrics/histogram_macros.h"
 #import "components/segmentation_platform/public/constants.h"
 #import "components/segmentation_platform/public/features.h"
 #import "components/segmentation_platform/public/segmentation_platform_service.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/cells/shortcuts_mediator.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_consumer.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_recorder.h"
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model_delegate.h"
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/most_visited_tiles_config.h"
@@ -66,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ParcelTrackingMediator* _parcelTrackingMediator;
   ShortcutsMediator* _shortcutsMediator;
   SafetyCheckMagicStackMediator* _safetyCheckMediator;
+  base::TimeTicks ranking_fetch_start_time_;
 }
 
 - (instancetype)initWithSegmentationService:
@@ -366,6 +369,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   } else {
     options.on_demand_execution = true;
   }
+  ranking_fetch_start_time_ = base::TimeTicks::Now();
   _segmentationService->GetClassificationResult(
       segmentation_platform::kIosModuleRankerKey, options, inputContext,
       base::BindOnce(
@@ -379,6 +383,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (const segmentation_platform::ClassificationResult&)result {
   if (result.status != segmentation_platform::PredictionStatus::kSucceeded) {
     return;
+  }
+
+  if ([self.homeStartDataSource isStartSurface]) {
+    LOCAL_HISTOGRAM_TIMES(kMagicStackStartSegmentationRankingFetchTimeHistogram,
+                          base::TimeTicks::Now() - ranking_fetch_start_time_);
+  } else {
+    LOCAL_HISTOGRAM_TIMES(kMagicStackNTPSegmentationRankingFetchTimeHistogram,
+                          base::TimeTicks::Now() - ranking_fetch_start_time_);
   }
 
   NSMutableArray* magicStackOrder = [NSMutableArray array];
--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model_unittest.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model_unittest.mm
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/cells/shortcuts_mediator.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_consumer.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_metrics_recorder.h"
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_ranking_model_delegate.h"
 #import "ios/chrome/browser/ui/content_suggestions/parcel_tracking/parcel_tracking_item.h"
@@ -349,6 +350,8 @@ TEST_F(MagicStackRankingModelTest, TestMagicStackOrderSegmentationServiceCall) {
         return _magicStackRankingModel.hasReceivedMagicStackResponse;
       }));
   EXPECT_OCMOCK_VERIFY(consumer_);
+  histogram_tester_->ExpectTotalCount(
+      kMagicStackNTPSegmentationRankingFetchTimeHistogram, 1);
 }
 
 // Tests that the -setMagicStackOrder: consumer call is executed with the
