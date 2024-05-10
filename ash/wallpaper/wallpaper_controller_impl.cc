66efba312a866e10ab0df31014baa6abfe84b64b
Jason Thai <jasontt@chromium.org>
2024-05-10 16:46:43 +0000

    wallpaper: remove ClearPrefChangeObserverForTesting
    The test still passes without it.
    
    BUG=none
    TEST=ash_unittests --gtest_filter="WallpaperControllerTest*"
    
    Cq-Include-Trybots: luci.chrome.try:linux-chromeos-chrome
    Change-Id: I268458a558a393292dfbc5c7e48b20d86b6677b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530523
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299301}

--- ash/wallpaper/wallpaper_controller_impl.cc
+++ ash/wallpaper/wallpaper_controller_impl.cc
@@ -1761,10 +1761,6 @@ void WallpaperControllerImpl::ReloadWallpaperForTesting(bool clear_cache) {
   ReloadWallpaper(clear_cache);
 }
 
-void WallpaperControllerImpl::ClearPrefChangeObserverForTesting() {
-  pref_change_registrar_.reset();
-}
-
 void WallpaperControllerImpl::OverrideDriveFsDelegateForTesting(
     std::unique_ptr<WallpaperDriveFsDelegate> drivefs_delegate) {
   CHECK_IS_TEST();
