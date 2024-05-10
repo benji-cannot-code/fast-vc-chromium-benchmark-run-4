fea8e7fc8df0aa73185d5996c3879921fe328eff
Arthur Wang <wuwang@google.com>
2024-05-10 21:58:33 +0000

    Update Crashpad to 1174aa4fc3196f478b5b4b77fc970d1cb85cb7cf
    9032284170d6 Pull latest Fuchsia toolchain
    1174aa4fc319 Add build/build_config.h in capture_memory.cc
    
    Change-Id: I5623f08cb79ade2e17291dc1765b0a5c43333496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531073
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299548}

--- third_party/crashpad/crashpad/snapshot/capture_memory.cc
+++ third_party/crashpad/crashpad/snapshot/capture_memory.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/heap_array.h"
 #include "base/logging.h"
+#include "build/build_config.h"
 #include "snapshot/memory_snapshot.h"
 
 namespace crashpad {
