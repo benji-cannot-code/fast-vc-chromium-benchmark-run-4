b651de2b835ac396de6dff4ba08eb77a73d59d4b
Daniel Cheng <dcheng@chromium.org>
2024-05-10 15:45:02 +0000

    Use absl::Cleanup to ensure temp file cleanup in safe_browsing::V4Store
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: Id485fc5d647ad99793e1f479c3cc412cb5578008
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529608
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299266}

--- components/safe_browsing/core/browser/db/v4_store.cc
+++ components/safe_browsing/core/browser/db/v4_store.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/safe_browsing/core/common/proto/webui.pb.h"
 #include "crypto/secure_hash.h"
 #include "crypto/sha2.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "third_party/protobuf/src/google/protobuf/io/zero_copy_stream_impl_lite.h"
 
 using base::TimeTicks;
@@ -956,16 +957,13 @@ StoreWriteResult V4Store::WriteToDisk(V4StoreFileFormat* file_format) {
   // Attempt writing to a temporary file first and at the end, swap the files.
   const base::FilePath new_filename = TemporaryFileForFilename(store_path_);
 
-  base::ScopedClosureRunner cleanup_on_error(base::BindOnce(
-      [](const base::FilePath& new_filename, const base::FilePath& store_path,
-         V4StoreFileFormat* file_format) {
-        base::DeleteFile(new_filename);
-        for (const auto& hash_file : file_format->hash_files()) {
-          base::DeleteFile(
-              MmapHashPrefixMap::GetPath(store_path, hash_file.extension()));
-        }
-      },
-      new_filename, store_path_, base::Unretained(file_format)));
+  absl::Cleanup cleanup_on_error = [&new_filename, this, file_format] {
+    base::DeleteFile(new_filename);
+    for (const auto& hash_file : file_format->hash_files()) {
+      base::DeleteFile(
+          MmapHashPrefixMap::GetPath(store_path_, hash_file.extension()));
+    }
+  };
 
   int64_t written = 0;
   // `write_session` must remain alive until `file_format` is committed to disk.
@@ -995,8 +993,8 @@ StoreWriteResult V4Store::WriteToDisk(V4StoreFileFormat* file_format) {
   for (const auto& hash_file : file_format->hash_files())
     file_size_ += hash_file.file_size();
 
-  // No cleanup needed, reset the closure.
-  std::ignore = cleanup_on_error.Release();
+  // No cleanup needed, cancel the cleanup.
+  std::move(cleanup_on_error).Cancel();
   CleanupExtraFiles(store_path_, *file_format);
 
   return WRITE_SUCCESS;
