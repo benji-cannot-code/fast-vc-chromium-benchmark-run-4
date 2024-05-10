c7bcf03442fa3cfdb048fa35e083e7994dbcc941
Andrew Paseltiner <apaseltiner@chromium.org>
2024-05-10 16:35:59 +0000

    Avoid crashing when setAttributionReportingTracking is enabled twice
    Previously this method crashed due to a DCHECK in
    base::ScopedObservation::Observe when an observation was already in
    effect.
    
    Bug: 339141101
    Change-Id: Ie9e202c121fd7dff885c60981cd759a3206f8959
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527382
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299294}

--- content/browser/devtools/protocol/storage_handler.cc
+++ content/browser/devtools/protocol/storage_handler.cc
@@ -2223,7 +2223,11 @@ Response StorageHandler::SetAttributionReportingTracking(bool enable) {
     if (!manager) {
       return Response::ServerError("Attribution Reporting is disabled.");
     }
-    attribution_observation_.Observe(manager);
+    // Prevent `DCHECK` crashes in `base::ScopedObservation::Observe()` when we
+    // are already observing.
+    if (!attribution_observation_.IsObserving()) {
+      attribution_observation_.Observe(manager);
+    }
   } else {
     attribution_observation_.Reset();
   }
