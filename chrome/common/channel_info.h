2a1cf283e12064d600d6e44cb9d2d4353abf8725
Eriko Kurimoto <elkurin@chromium.org>
2024-05-10 06:54:17 +0000

    version_info: Remove GetDesktopName from Lacros
    Since it's not used on Lacros.
    
    Bug: 40118868
    Change-Id: If21817e841a889984b988d763d65940c51f39117
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529422
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299105}

--- chrome/common/channel_info.h
+++ chrome/common/channel_info.h
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
+#if BUILDFLAG(IS_LINUX)
 namespace base {
 class Environment;
 }
+#endif  // BUILDFLAG(IS_LINUX)
 
 namespace version_info {
 enum class Channel;
@@ -101,15 +103,11 @@ std::string GetChannelSuffixForDataDir();
 
 #if BUILDFLAG(IS_LINUX)
 std::string GetChannelSuffixForExtraFlagsEnvVarName();
-#endif
 
-// TODO(crbug.com/40118868): Revisit the macro expression once build flag switch
-// of lacros-chrome is complete.
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
 // Returns the channel-specific filename of the desktop shortcut used to launch
 // the browser.
 std::string GetDesktopName(base::Environment* env);
-#endif
+#endif  // BUILDFLAG(IS_LINUX)
 
 }  // namespace chrome
 
