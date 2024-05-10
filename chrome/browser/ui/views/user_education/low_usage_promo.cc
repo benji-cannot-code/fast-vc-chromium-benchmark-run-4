2f7e80253f13bc3b53fbf84bccfc37d54333a8db
Joshua Pawlicki <waffles@chromium.org>
2024-05-10 13:20:55 +0000

    Revert "Add finch param to focus low usage promo"
    This reverts commit 6040f787f25984763aaa6a8e6947a554771e7866.
    
    Reason for revert: widespread interactive_ui_tests failures that are crashing in feature_promo_specification.cc - example: https://luci-milo.appspot.com/ui/p/chrome/builders/ci/win64-rel-ready/21760/overview
    
    Original change's description:
    > Add finch param to focus low usage promo
    >
    > Default is not to focus, as they show at startup.
    >
    > Change-Id: Id2863e0cfc14619cd7f70d1d659f832d371eed2c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528413
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1299137}
    
    Change-Id: I867145546dde80b8c7e51cae475ef099115917f8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531822
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299196}

--- chrome/browser/ui/views/user_education/low_usage_promo.cc
+++ chrome/browser/ui/views/user_education/low_usage_promo.cc
@@ -48,7 +48,6 @@ namespace {
 
 const base::Feature& kLowUsagePromo =
     feature_engagement::kIPHDesktopReEngagementFeature;
-constexpr char kLowUsagePromoFocusFieldTrialParam[] = "focus_on_show";
 
 constexpr char kIncludeAiPromosFieldTrialParam[] = "include_ai";
 
@@ -212,7 +211,7 @@ FeaturePromoSpecification CreateLowUsagePromoSpecification(Profile* profile) {
   // Note: numbers correspond to indices in the spec; they are preserved here to
   // ensure that if promos need to be changed or re-ordered we can retain the
   // correspondence.
-  auto spec = FeaturePromoSpecification::CreateForRotatingPromo(
+  return FeaturePromoSpecification::CreateForRotatingPromo(
       kLowUsagePromo,
 
       // 1.
@@ -324,10 +323,4 @@ FeaturePromoSpecification CreateLowUsagePromoSpecification(Profile* profile) {
       CreateNavigatePromo(IDS_REENGAGEMENT_PROMO_ENERGY_MEMORY_SAVER,
                           IDS_REENGAGEMENT_PROMO_SETTINGS_ACTION,
                           chrome::GetSettingsUrl(chrome::kPerformanceSubPage)));
-
-  // Allow focus based on field trial param; default is no focus.
-  spec.OverrideFocusOnShow(base::GetFieldTrialParamByFeatureAsBool(
-      kLowUsagePromo, kLowUsagePromoFocusFieldTrialParam, false));
-
-  return spec;
 }
