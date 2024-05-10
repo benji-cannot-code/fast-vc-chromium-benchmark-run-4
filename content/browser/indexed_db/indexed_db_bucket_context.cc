462f25a09551a253f353f731b07f324709162a84
Evan Stade <estade@chromium.org>
2024-05-10 19:54:02 +0000

    IDB: fix indexeddb-internals for sharded backing stores, take 2.
    The mojo callback for the response to the idb-internals page was
    executing on a bucket thread instead of the main IDB thread.
    
    Fix this by using SequenceBound the way it's intended to be used
    instead of passing a callback to the bucket context.
    
    Bug: 337994998
    Change-Id: I08757523b116e8c987853f9b04745b8efcf2f272
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528756
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299454}

--- content/browser/indexed_db/indexed_db_bucket_context.cc
+++ content/browser/indexed_db/indexed_db_bucket_context.cc
@@ -990,9 +990,8 @@ void IndexedDBBucketContext::DeleteDatabase(
   }
 }
 
-void IndexedDBBucketContext::FillInMetadata(
-    storage::mojom::IdbBucketMetadataPtr info,
-    base::OnceCallback<void(storage::mojom::IdbBucketMetadataPtr)> result) {
+storage::mojom::IdbBucketMetadataPtr IndexedDBBucketContext::FillInMetadata(
+    storage::mojom::IdbBucketMetadataPtr info) {
   // TODO(jsbell): Sort by name?
   std::vector<storage::mojom::IdbDatabaseMetadataPtr> database_list;
 
@@ -1074,7 +1073,7 @@ void IndexedDBBucketContext::FillInMetadata(
     database_list.push_back(std::move(db_info));
   }
   info->databases = std::move(database_list);
-  std::move(result).Run(std::move(info));
+  return info;
 }
 
 IndexedDBBucketContext* IndexedDBBucketContext::GetReferenceForTesting() {
