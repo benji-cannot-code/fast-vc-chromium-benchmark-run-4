def99370a18f3e3faa73107198fee7d1deb429e5
Ella Ge <eirage@chromium.org>
2024-05-10 18:00:15 +0000

    [WebAPKRestore] Guard by fetching restorable apps by backend flag
    Otherwise the sync service and sync data won't be available.
    
    Bug: 41496289
    Change-Id: I88827889c679b68d290b6fa7a0ab33a5b1b9a455
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526030
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Commit-Queue: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299371}

--- chrome/browser/android/webapk/webapk_sync_service.cc
+++ chrome/browser/android/webapk/webapk_sync_service.cc
@@ -182,7 +182,8 @@ static void JNI_WebApkSyncService_FetchRestorableApps(
     JNIEnv* env,
     Profile* profile,
     const JavaParamRef<jobject>& java_callback) {
-  if (profile == nullptr) {
+  if (profile == nullptr ||
+      !base::FeatureList::IsEnabled(syncer::kWebApkBackupAndRestoreBackend)) {
     return;
   }
 
