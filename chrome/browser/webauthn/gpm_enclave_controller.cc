d354cb82a721d0a207e822cfff2a6f436a6141ea
Adam Langley <agl@chromium.org>
2024-05-10 01:28:26 +0000

    webauthn: always update UI when a GPM credential is selected.
    Without this, a user can select a GPM credential and then click again
    while the enclave transaction is running, which seriously confuses the
    code.
    
    Bug: 40274370
    Change-Id: Ief4c74afff5f1b456fbad269cf12cc1d20bc790a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530100
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299032}

--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -816,6 +816,7 @@ void GPMEnclaveController::OnGPMPasskeySelected(
 
   switch (account_state_) {
     case AccountState::kReady:
+      model_->SetStep(Step::kGPMConnecting);
       StartTransaction();
       break;
 
