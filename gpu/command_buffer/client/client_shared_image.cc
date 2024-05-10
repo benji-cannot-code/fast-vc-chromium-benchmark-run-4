2ff47bac5f3f448c0ec5df748f205929dceabf2c
vikas soni <vikassoni@chromium.org>
2024-05-10 19:16:42 +0000

    Add ClientSharedImage::OnMemoryDump().
    Adda new method ClientSharedImage::OnMemoryDump() which uses mailbox id
    as ownership edge. This will be used in general as a replacement for
    GpuMemoryBuffer::OnMemoryDump() with MappableSI.
    
    Bug: 40264376
    Change-Id: I8ee6067c671022611018c62a5c3c142c8bf24c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531955
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299431}

--- gpu/command_buffer/client/client_shared_image.cc
+++ gpu/command_buffer/client/client_shared_image.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_is_test.h"
 #include "base/containers/contains.h"
+#include "base/trace_event/process_memory_dump.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/common/gpu_memory_buffer_support.h"
 #include "gpu/command_buffer/common/shared_image_capabilities.h"
@@ -394,6 +395,15 @@ scoped_refptr<ClientSharedImage> ClientSharedImage::ImportUnowned(
       exported_shared_image.mailbox_, exported_shared_image.metadata_,
       exported_shared_image.creation_sync_token_,
       exported_shared_image.texture_target_));
+}
+
+void ClientSharedImage::OnMemoryDump(
+    base::trace_event::ProcessMemoryDump* pmd,
+    const base::trace_event::MemoryAllocatorDumpGuid& buffer_dump_guid,
+    int importance) {
+  auto tracing_guid = GetGUIDForTracing();
+  pmd->CreateSharedGlobalAllocatorDump(tracing_guid);
+  pmd->AddOwnershipEdge(buffer_dump_guid, tracing_guid, importance);
 }
 
 // static
