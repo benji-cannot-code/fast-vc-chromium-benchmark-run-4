bbe4397f08b04f1ba53875106f68697b7189d07b
Tony Herre <toprice@chromium.org>
2024-05-10 13:05:38 +0000

    Add UMA breaking down webrtc logging start errors
    Includes renaming "WebRtcTextLogging.Start" to "...Started" and
    re-enabling, as the former has been expired for ages.
    
    Bug: 313592212
    Change-Id: Ib059f48336e6af8cb48f808c46528fe7e84faf6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521414
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Johannes Kron <kron@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299177}

--- chrome/browser/media/webrtc/webrtc_text_log_handler.cc
+++ chrome/browser/media/webrtc/webrtc_text_log_handler.cc
@@ -131,6 +131,21 @@ std::string IPAddressToSensitiveString(const net::IPAddress& address) {
 #endif
 }
 
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class StartError {
+  kRendererClosing = 0,
+  kLogAlreadyOpen = 1,
+  kApplyForStartFailed = 2,
+  kCancelled = 3,
+  kRendererClosingInStartDone = 4,
+  kMaxValue = kRendererClosingInStartDone,
+};
+
+void RecordStartError(StartError error) {
+  base::UmaHistogramEnumeration("WebRtcTextLogging.StartError", error);
+}
+
 }  // namespace
 
 WebRtcTextLogHandler::WebRtcTextLogHandler(int render_process_id)
@@ -192,16 +207,19 @@ void WebRtcTextLogHandler::SetMetaData(
 bool WebRtcTextLogHandler::StartLogging(GenericDoneCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!callback.is_null());
+  base::UmaHistogramBoolean("WebRtcTextLogging.StartCalled", true);
 
   if (channel_is_closing_) {
     FireGenericDoneCallback(std::move(callback), false,
                             "The renderer is closing.");
+    RecordStartError(StartError::kRendererClosing);
     return false;
   }
 
   if (logging_state_ != CLOSED) {
     FireGenericDoneCallback(std::move(callback), false,
                             "A log is already open.");
+    RecordStartError(StartError::kLogAlreadyOpen);
     return false;
   }
 
@@ -210,6 +228,7 @@ bool WebRtcTextLogHandler::StartLogging(GenericDoneCallback callback) {
     FireGenericDoneCallback(std::move(callback), false,
                             "Cannot start, maybe the maximum number of "
                             "simultaneuos logs has been reached.");
+    RecordStartError(StartError::kApplyForStartFailed);
     return false;
   }
 
@@ -234,12 +253,13 @@ void WebRtcTextLogHandler::StartDone(GenericDoneCallback callback) {
   if (channel_is_closing_) {
     FireGenericDoneCallback(std::move(callback), false,
                             "Failed to start log. Renderer is closing.");
+    RecordStartError(StartError::kRendererClosingInStartDone);
     return;
   }
 
   DCHECK_EQ(STARTING, logging_state_);
 
-  base::UmaHistogramSparse("WebRtcTextLogging.Start", web_app_id_);
+  base::UmaHistogramSparse("WebRtcTextLogging.Started", web_app_id_);
 
   logging_started_time_ = base::Time::Now();
   logging_state_ = STARTED;
@@ -441,6 +461,7 @@ void WebRtcTextLogHandler::OnGetNetworkInterfaceListFinish(
 
   if (logging_state_ != STARTING || channel_is_closing_) {
     FireGenericDoneCallback(std::move(callback), false, "Logging cancelled.");
+    RecordStartError(StartError::kCancelled);
     return;
   }
 
