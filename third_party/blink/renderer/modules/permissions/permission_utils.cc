9a8119d10f6911aa1bdbb6143150990893a84af1
Takumi Fujimoto <takumif@chromium.org>
2024-05-10 13:10:36 +0000

    Expose keyboard-lock and pointer-lock in the Permissions API
    Allow querying for the permission grant state for the two APIs.
    
    Bug: 333988290
    Change-Id: I543f08729ac32c3b3d305b0d7cf6c46863f71166
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5481227
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299186}

--- third_party/blink/renderer/modules/permissions/permission_utils.cc
+++ third_party/blink/renderer/modules/permissions/permission_utils.cc
@@ -131,9 +131,11 @@ String PermissionNameToString(PermissionName name) {
       return "captured-surface-control";
     case PermissionName::SPEAKER_SELECTION:
       return "speaker-selection";
+    case PermissionName::KEYBOARD_LOCK:
+      return "keyboard-lock";
+    case PermissionName::POINTER_LOCK:
+      return "pointer-lock";
   }
-  NOTREACHED();
-  return "unknown";
 }
 
 PermissionDescriptorPtr CreatePermissionDescriptor(PermissionName name) {
@@ -379,6 +381,12 @@ PermissionDescriptorPtr ParsePermissionDescriptor(
       return nullptr;
     }
     return CreatePermissionDescriptor(PermissionName::SPEAKER_SELECTION);
+  }
+  if (name == V8PermissionName::Enum::kKeyboardLock) {
+    return CreatePermissionDescriptor(PermissionName::KEYBOARD_LOCK);
+  }
+  if (name == V8PermissionName::Enum::kPointerLock) {
+    return CreatePermissionDescriptor(PermissionName::POINTER_LOCK);
   }
   return nullptr;
 }
