57560c4b33fd19d3c020b6e937fb4c17ce05f4f2
Kent Tamura <tkent@chromium.org>
2024-05-10 06:51:25 +0000

    RubyLB: RebuildBoxStates() supports ranges starting at <rt>
    InlineLayoutStateStack for an annotation line needs an open tag
    information starting at an <rt>. So RebuildBoxStates() should take the
    `start_item_index` argument.
    
    This CL has no behavior changes. `start_item_index` is always 0 for now.
    
    Bug: 324111880
    Change-Id: I46396683b2b9b278e87bc70f87b1f0359a1ae757
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528897
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299102}

--- third_party/blink/renderer/core/layout/inline/inline_items_data.cc
+++ third_party/blink/renderer/core/layout/inline/inline_items_data.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-void InlineItemsData::GetOpenTagItems(wtf_size_t size,
+void InlineItemsData::GetOpenTagItems(wtf_size_t start_index,
+                                      wtf_size_t size,
                                       OpenTagItems* open_items) const {
   DCHECK_LE(size, items.size());
-  for (const InlineItem& item : base::make_span(items.data(), size)) {
+  for (const InlineItem& item :
+       base::make_span(items).subspan(start_index, size)) {
     if (item.Type() == InlineItem::kOpenTag) {
       open_items->push_back(&item);
     } else if (item.Type() == InlineItem::kCloseTag) {
