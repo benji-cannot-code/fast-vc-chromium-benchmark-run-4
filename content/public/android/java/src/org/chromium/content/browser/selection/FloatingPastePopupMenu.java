9191456155c6909280bde93d12ca4aac4162f512
Zakarie Yaris <yaris@google.com>
2024-05-10 09:48:56 +0000

    Update the FloatingPastePopupMenu delegate [1/x]
    first in a series of changes to merge the FloatingPastePopupMenu functionality into SelectionPopupControllerImpl. Also see CL: https://chromium-review.googlesource.com/c/chromium/src/+/5344269
    
    Bug: 320643289
    Change-Id: Ice66d33a03c785949f4365d1a11292a39909351e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522216
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Zakarie Yaris <yaris@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299152}

--- content/public/android/java/src/org/chromium/content/browser/selection/FloatingPastePopupMenu.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/FloatingPastePopupMenu.java
@@ -26,9 +26,9 @@ import java.util.SortedSet;
 
 /** Paste popup implementation based on floating ActionModes. */
 // TODO(crbug.com/40925113): Merge this class with SelectionPopupControllerImpl and remove.
-public class FloatingPastePopupMenu implements PastePopupMenu {
+public class FloatingPastePopupMenu {
     private final View mParent;
-    private final PastePopupMenuDelegate mDelegate;
+    private final SelectionPopupControllerImpl mDelegate;
     private final Context mContext;
 
     private ActionMode mActionMode;
@@ -39,7 +39,7 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
     public FloatingPastePopupMenu(
             Context context,
             View parent,
-            PastePopupMenuDelegate delegate,
+            SelectionPopupControllerImpl delegate,
             @Nullable SelectionActionMenuDelegate selectionActionMenuDelegate) {
         mParent = parent;
         mDelegate = delegate;
@@ -48,7 +48,6 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
         mCustomMenuItemClickListeners = new HashMap<>();
     }
 
-    @Override
     public void show(Rect selectionRect) {
         mSelectionRect = selectionRect;
         if (mActionMode != null) {
@@ -59,7 +58,6 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
         ensureActionMode();
     }
 
-    @Override
     public void hide() {
         if (mActionMode != null) {
             mActionMode.finish();
@@ -71,7 +69,7 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
         if (mActionMode != null) return;
 
         ActionMode actionMode =
-                mParent.startActionMode(new ActionModeCallback(), ActionMode.TYPE_FLOATING);
+                mParent.startActionMode(new PasteActionModeCallback(), ActionMode.TYPE_FLOATING);
         if (actionMode != null) {
             // crbug.com/651706
             LGEmailActionModeWorkaroundImpl.runIfNecessary(mContext, actionMode);
@@ -81,7 +79,7 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
         }
     }
 
-    private class ActionModeCallback extends ActionMode.Callback2 {
+    private class PasteActionModeCallback extends ActionMode.Callback2 {
         @Override
         public boolean onCreateActionMode(ActionMode mode, Menu menu) {
             createPasteMenu(mode, menu);
@@ -155,9 +153,11 @@ public class FloatingPastePopupMenu implements PastePopupMenu {
                 int id = item.getItemId();
                 if (id == R.id.select_action_menu_paste) {
                     mDelegate.paste();
+                    mDelegate.dismissTextHandles();
                     mode.finish();
                 } else if (id == R.id.select_action_menu_paste_as_plain_text) {
                     mDelegate.pasteAsPlainText();
+                    mDelegate.dismissTextHandles();
                     mode.finish();
                 } else if (id == R.id.select_action_menu_select_all) {
                     mDelegate.selectAll();
