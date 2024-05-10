6f8ea8698bfbe9c089cd705d8f0ff1fd04e186fd
dljames <dljames@chromium.org>
2024-05-10 19:50:10 +0000

    [SavedTabGroups] Remove sync icon when TabGroupsSaveV2 flag enabled
    Change-Id: I0423e7a2e73cbda1a77cd46f6a78f3ca875ced8c
    Bug: 323972032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528096
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299451}

--- chrome/browser/ui/views/tabs/tab_group_header.cc
+++ chrome/browser/ui/views/tabs/tab_group_header.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_strip_layout.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_types.h"
 #include "chrome/grit/generated_resources.h"
+#include "components/saved_tab_groups/features.h"
 #include "components/tab_groups/tab_group_color.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
@@ -579,6 +580,10 @@ int TabGroupHeader::GetCollapsedHeaderWidth() const {
 }
 
 bool TabGroupHeader::ShouldShowSyncIcon() const {
+  if (tab_groups::IsTabGroupsSaveV2Enabled()) {
+    return false;
+  }
+
   return saved_tab_group_service_ && saved_tab_group_service_->model() &&
          saved_tab_group_service_->model()->Contains(group().value());
 }
