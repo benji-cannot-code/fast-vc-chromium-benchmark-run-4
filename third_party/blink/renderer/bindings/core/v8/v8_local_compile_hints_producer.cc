6cf3ae6c4f5d426829fea0dbe1c2f5b11462fcb7
Marja Hölttä <marja@google.com>
2024-05-10 09:58:47 +0000

    [compile hints] Use the more memory-efficient compile hints API
    See https://chromium-review.googlesource.com/c/v8/v8/+/5522223
    
    Bug: 40286622
    Change-Id: I16f8920957b642f39d96dc9de26d132e23f988f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526662
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299153}

--- third_party/blink/renderer/bindings/core/v8/v8_local_compile_hints_producer.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_local_compile_hints_producer.cc
@@ -36,11 +36,13 @@ void V8LocalCompileHintsProducer::RecordScript(
     return;
   }
   v8::Isolate* isolate = execution_context->GetIsolate();
-  v8_scripts_.emplace_back(v8::Global<v8::Script>(isolate, script));
+  compile_hints_collectors_.emplace_back(isolate,
+                                         script->GetCompileHintsCollector());
   cache_handlers_.emplace_back(cache_handler);
 }
 
 void V8LocalCompileHintsProducer::GenerateData(bool final_data) {
+  DCHECK_EQ(cache_handlers_.size(), compile_hints_collectors_.size());
   if (cache_handlers_.empty()) {
     return;
   }
@@ -53,7 +55,6 @@ void V8LocalCompileHintsProducer::GenerateData(bool final_data) {
       ExecutionContext::GetCodeCacheHostFromContext(execution_context);
   v8::HandleScope handle_scope(isolate);
 
-  DCHECK_EQ(cache_handlers_.size(), v8_scripts_.size());
   for (wtf_size_t i = 0; i < cache_handlers_.size(); ++i) {
     CachedMetadataHandler* cache_handler = cache_handlers_.at(i);
 
@@ -74,8 +75,10 @@ void V8LocalCompileHintsProducer::GenerateData(bool final_data) {
             ? V8CodeCache::SetMetadataType::kLocalCompileHintsAtInteractive
             : V8CodeCache::SetMetadataType::kLocalCompileHintsAtFMP);
 
-    v8::Local<v8::Script> script = v8_scripts_[i].Get(isolate);
-    std::vector<int> compile_hints = script->GetProducedCompileHints();
+    v8::Local<v8::CompileHintsCollector> compile_hints_collector =
+        compile_hints_collectors_.at(i).Get(isolate);
+    std::vector<int> compile_hints =
+        compile_hints_collector->GetCompileHints(isolate);
 
     uint64_t timestamp = V8CodeCache::GetTimestamp();
     std::unique_ptr<v8::ScriptCompiler::CachedData> data(
@@ -89,7 +92,7 @@ void V8LocalCompileHintsProducer::GenerateData(bool final_data) {
   }
   if (final_data) {
     cache_handlers_.clear();
-    v8_scripts_.clear();
+    compile_hints_collectors_.clear();
     base::UmaHistogramEnumeration(kLocalCompileHintsGeneratedHistogram,
                                   LocalCompileHintsGenerated::kFinal);
 
@@ -135,6 +138,7 @@ V8LocalCompileHintsProducer::CreateCompileHintsCachedDataForScript(
 void V8LocalCompileHintsProducer::Trace(Visitor* visitor) const {
   visitor->Trace(cache_handlers_);
   visitor->Trace(frame_);
+  visitor->Trace(compile_hints_collectors_);
 }
 
 }  // namespace blink::v8_compile_hints
