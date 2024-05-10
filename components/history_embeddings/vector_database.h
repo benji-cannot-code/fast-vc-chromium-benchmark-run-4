6f3607c6af635076654e4346e89345fe7e598066
Orin Jaworski <orinj@google.com>
2024-05-10 16:56:47 +0000

    Create ChromeHistoryEmbeddingsService; support sending quality log data
    This CL implements basic quality logging in the HistoryEmbeddingsService
    with an uploader in Chrome. The base service class will default to
    not log, and a feature parameter must be enabled in order for data
    to be sent.
    
    There's a new unit test to confirm the logging code doesn't break, but
    it will need to be separately hooked to both the chrome://history
    front end and the omnibox.
    
    Bug: 339237806
    Change-Id: I9dd3d2ab084db9533adfd03ccacf6bb07bb6e7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529693
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299315}

--- components/history_embeddings/vector_database.h
+++ components/history_embeddings/vector_database.h
@@ -35,6 +35,7 @@ struct UrlPassages {
 class Embedding {
  public:
   explicit Embedding(std::vector<float> data);
+  Embedding();
   ~Embedding();
   Embedding(const Embedding&);
   Embedding& operator=(const Embedding&);
@@ -84,6 +85,18 @@ struct UrlEmbeddings {
 };
 
 struct ScoredUrl {
+  ScoredUrl(history::URLID url_id,
+            history::VisitID visit_id,
+            base::Time visit_time,
+            float score,
+            size_t index,
+            Embedding passage_embedding);
+  ~ScoredUrl();
+  ScoredUrl(ScoredUrl&&);
+  ScoredUrl& operator=(ScoredUrl&&);
+  ScoredUrl(const ScoredUrl&);
+  ScoredUrl& operator=(ScoredUrl&);
+
   // Basic data about the found URL/visit.
   history::URLID url_id;
   history::VisitID visit_id;
@@ -95,6 +108,9 @@ struct ScoredUrl {
   // Index of the embedding, which also corresponds to the index of the source
   // passage used to compute the embedding.
   size_t index;
+
+  // The embedding for the source passage. Preserved during search.
+  Embedding passage_embedding;
 
   // Source passage; may not be populated during search, but kept in this
   // struct for convenience when passing finished results to service callers.
