6828d1e1d5a734223e5cc7eadb4870936a66ad6b
Olivier Li Shing Tat-Dupuis <olivierli@chromium.org>
2024-05-10 18:15:42 +0000

    Reject HttpCache requests that are suspected to be no-store
    Bug: 331123686
    
    Change-Id: I1a8021fb680adf3710e854b17a5dd64045400f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5403088
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299382}

--- net/http/http_cache.cc
+++ net/http/http_cache.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/functional/callback_helpers.h"
+#include "base/hash/sha1.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
@@ -67,6 +68,14 @@ bool g_init_cache = false;
 // True if split cache is enabled by default. Must be set before any HTTP cache
 // has been initialized.
 bool g_enable_split_cache = false;
+
+base::SHA1Digest GetHashForKey(const std::string& key) {
+  base::SHA1Digest sha_hash;
+  base::SHA1HashBytes(reinterpret_cast<const unsigned char*>(key.data()),
+                      key.size(), sha_hash.data());
+  return sha_hash;
+}
+
 }  // namespace
 
 const char HttpCache::kDoubleKeyPrefix[] = "_dk_";
@@ -396,7 +405,9 @@ HttpCache::HttpCache(std::unique_ptr<HttpTransactionFactory> network_layer,
       backend_factory_(std::move(backend_factory)),
 
       network_layer_(std::move(network_layer)),
-      clock_(base::DefaultClock::GetInstance()) {
+      clock_(base::DefaultClock::GetInstance()),
+      keys_marked_no_store_(
+          features::kAvoidEntryCreationForNoStoreCacheSize.Get()) {
   g_init_cache = true;
   HttpNetworkSession* session = network_layer_->GetSession();
   // Session may be NULL in unittests.
@@ -1352,6 +1363,15 @@ bool HttpCache::RemovePendingTransactionFromPendingOp(
     }
   }
   return false;
+}
+
+void HttpCache::MarkKeyNoStore(const std::string& key) {
+  keys_marked_no_store_.Put(GetHashForKey(key));
+}
+
+bool HttpCache::DidKeyLeadToNoStoreResponse(const std::string& key) {
+  return keys_marked_no_store_.Get(GetHashForKey(key)) !=
+         keys_marked_no_store_.end();
 }
 
 void HttpCache::OnProcessQueuedTransactions(scoped_refptr<ActiveEntry> entry) {
