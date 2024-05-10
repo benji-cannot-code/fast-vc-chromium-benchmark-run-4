17bb10f31caeb7a6c0bb3b70a22306957f410a64
Chuong Ho <hdchuong@google.com>
2024-05-10 17:35:12 +0000

    editor: limit l10n strings to en, ja, de and fr
    We only allow en, ja, de, fr strings to be shown in copyeditor UI if users have the required flags enabled.
    
    
    Change-Id: If7cd3b51b8be76bdaadf073aaf261bb618ac8947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528619
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Auto-Submit: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299349}

--- components/manta/manta_service.cc
+++ components/manta/manta_service.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/contains.h"
+#include "base/containers/fixed_flat_set.h"
 #include "base/memory/scoped_refptr.h"
 #include "build/chromeos_buildflags.h"
 #include "components/account_id/account_id.h"
@@ -37,6 +39,19 @@ FeatureSupportStatus ConvertToMantaFeatureSupportStatus(signin::Tribool value) {
       return FeatureSupportStatus::kUnsupported;
   }
 }
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+
+constexpr auto kAllowedLanguagesForAddingLocaleToRequest =
+    base::MakeFixedFlatSet<std::string_view>({"de", "en", "en-GB", "fr", "ja"});
+
+bool ShouldIncludeLocaleInRequest(std::string_view locale) {
+  return chromeos::features::IsOrcaUseL10nStringsEnabled() ||
+         (chromeos::features::IsOrcaInternationalizeEnabled() &&
+          base::Contains(kAllowedLanguagesForAddingLocaleToRequest, locale));
+}
+
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 }  // namespace
 
@@ -90,8 +105,7 @@ std::unique_ptr<OrcaProvider> MantaService::CreateOrcaProvider() {
       shared_url_loader_factory_, identity_manager_, is_demo_mode_,
       chrome_version_,
       /*locale=*/
-      chromeos::features::IsOrcaUseL10nStringsEnabled() ? locale_
-                                                        : std::string());
+      ShouldIncludeLocaleInRequest(locale_) ? locale_ : std::string());
 }
 
 std::unique_ptr<SnapperProvider> MantaService::CreateSnapperProvider() {
