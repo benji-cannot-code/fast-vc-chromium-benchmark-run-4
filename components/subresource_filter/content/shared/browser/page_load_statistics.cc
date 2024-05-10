d2b9fad2126f96a4b721a375f40689ebf975f1d1
Zainab Rizvi <rizvis@google.com>
2024-05-10 21:38:12 +0000

    [FPF] Move PageLoadStatistics to /content/shared
    This allows them to be reused by the Fingerprinting Protection
    throttle manager.
    
    This is a pure  refactor.
    
    Bug: 40280666
    Change-Id: I671171a2d301fc112f9336f76f122dcf269ef472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527772
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299534}

--- components/subresource_filter/content/shared/browser/page_load_statistics.cc
+++ components/subresource_filter/content/shared/browser/page_load_statistics.cc
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/subresource_filter/content/browser/page_load_statistics.h"
+#include "components/subresource_filter/content/shared/browser/page_load_statistics.h"
+
 
 #include <string_view>
 
