614c401b6e10caf608f9a2b8f3df3dd811ac8251
Lei Zhang <thestig@chromium.org>
2024-05-11 01:00:44 +0000

    [PDF Ink Signatures] Add basic input handling code
    Add basic and HandleInputEvent() code to InkModule. HandleInputEvent()
    includes the basic parts needed to handle the relevant input events,
    though the coordinate system and paint invalidation still needs further
    work. Additionally, this adds 2 helper functions for creating brushes.
    They have fixed values for now.
    
    Bug: 335524380
    Change-Id: Ia3418f9787e81baf26d18d62a23a8e87ed9ba706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530093
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299643}

--- pdf/ink_module.h
+++ pdf/ink_module.h
@@ -6,12 +6,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_INK_MODULE_H_
 #define PDF_INK_MODULE_H_
 
+#include <memory>
+#include <optional>
+#include <vector>
+
+#include "base/time/time.h"
 #include "base/values.h"
 #include "pdf/buildflags.h"
+#include "pdf/ink/ink_stroke_input.h"
 
 static_assert(BUILDFLAG(ENABLE_PDF_INK2), "ENABLE_PDF_INK2 not set to true");
 
+namespace blink {
+class WebInputEvent;
+class WebMouseEvent;
+}  // namespace blink
+
 namespace chrome_pdf {
+
+class InkStroke;
 
 class InkModule {
  public:
@@ -22,13 +35,25 @@ class InkModule {
 
   bool enabled() const { return enabled_; }
 
+  // Returns whether the event was handled or not.
+  bool HandleInputEvent(const blink::WebInputEvent& event);
+
   // Returns whether the message was handled or not.
   bool OnMessage(const base::Value::Dict& message);
 
  private:
+  bool OnMouseDown(const blink::WebMouseEvent& event);
+  bool OnMouseUp(const blink::WebMouseEvent& event);
+  bool OnMouseMove(const blink::WebMouseEvent& event);
+
   void HandleSetAnnotationModeMessage(const base::Value::Dict& message);
 
   bool enabled_ = false;
+
+  // Set when InkModule is in the middle of drawing a stroke.
+  std::optional<base::Time> ink_start_time_;
+  std::vector<InkStrokeInput> ink_inputs_;
+  std::vector<std::unique_ptr<InkStroke>> ink_strokes_;
 };
 
 }  // namespace chrome_pdf
