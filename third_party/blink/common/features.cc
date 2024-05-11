9de4ec9f5b42ed2cbb02aebd9e3f024b861f7931
Clark DuVall <cduvall@chromium.org>
2024-05-11 00:28:55 +0000

    Avoid running DocumentLoader::StartLoadingResponse during initial commit
    DocumentLoader::StartLoadingResponse body can take multiple ms and post
    a bunch of tasks when initializing the renderer with the initial empty
    document. This is not necessary, since the document doesn't have
    anything to load. This change adds a feature to avoid doing this. If
    the results look promising, I may look into other renderer init
    improvements.
    
    Bug: 332706093
    Change-Id: I503f4851ebe722e50a513886512b323e856f481a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502925
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299623}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -2297,6 +2297,11 @@ BASE_FEATURE(kStopInBackground,
 #endif
 );
 
+// Reduces the work done during renderer initialization.
+BASE_FEATURE(kStreamlineRendererInit,
+             "StreamlineRendererInit",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kStylusPointerAdjustment,
              "StylusPointerAdjustment",
              base::FEATURE_ENABLED_BY_DEFAULT);
