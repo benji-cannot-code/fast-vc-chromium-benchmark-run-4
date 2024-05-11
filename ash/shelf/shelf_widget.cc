ff0ee253cdda2ec3bb92e22793d48c9ef3e78dde
Xiaodan Zhu <zxdan@chromium.org>
2024-05-11 00:03:24 +0000

    Reland "oak: show shelf background when exit Overview"
    This is a reland of commit fdcaed5c78fa886fa067cdd523577147ee192686
    
    Original change's description:
    > oak: show shelf background when exit Overview
    >
    > The shelf background should be shown when start to exit Overview,
    > but currently it shows after fully exiting the Overview. The problem
    > is because we forgot to observe OverviewController such that the
    > background does not update when exiting the Overview.
    >
    > Bug: b:339683597
    > Change-Id: Iccd7a339d9e2c8a8b24c2777e16d122f89b279cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530225
    > Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    > Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1298992}
    
    Bug: b:339683597
    Change-Id: I0fbde9d562d210b3cdda17ba6e5f71d0f8930994
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529343
    Auto-Submit: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299611}

--- ash/shelf/shelf_widget.cc
+++ ash/shelf/shelf_widget.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/style_util.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/utility/forest_util.h"
+#include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/work_area_insets.h"
 #include "base/command_line.h"
@@ -705,6 +706,8 @@ void ShelfWidget::Initialize(aura::Window* shelf_container) {
   background_animator_.AddObserver(delegate_view_);
   shelf_->AddObserver(this);
 
+  Shell::Get()->overview_controller()->AddObserver(this);
+
   // Sets initial session state to make sure the UI is properly shown.
   OnSessionStateChanged(Shell::Get()->session_controller()->GetSessionState());
   delegate_view_->SetEnableArrowKeyTraversal(true);
@@ -732,6 +735,10 @@ void ShelfWidget::Shutdown() {
   // Don't need to observe focus/activation during shutdown.
   Shell::Get()->focus_cycler()->RemoveWidget(this);
   SetFocusCycler(nullptr);
+
+  if (auto* overview_controller = Shell::Get()->overview_controller()) {
+    overview_controller->RemoveObserver(this);
+  }
 }
 
 void ShelfWidget::RegisterHotseatWidget(HotseatWidget* hotseat_widget) {
