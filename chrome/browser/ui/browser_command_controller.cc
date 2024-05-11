c155f3e00d8bdec9bca95c0fd3580a84d559f7ea
Dibyajyoti Pal <dibyapal@google.com>
2024-05-11 00:00:32 +0000

    [Shortcuts] Introduce chrome::CreateDesktopShortcutForCurrentWebContents
    This CL does the following:
    1. Creates a chrome::CreateDesktopShortcutForCurrentWebContents() that
    is triggered from browser_command_controller.cc to start the create
    shortcut flow.
    2. Moves the implementation based logic inside its definition closer
    to shortcuts dialog code, to keep the public interface more free.
    3. Makes it simpler to test the entire E2E flow by creating a public
    interface that takes in a browser and the final callback as an input.
    
    This is based on feedback from the last CL in the chain:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/5503499/comment/f00ac9bb_34441e8c/
    
    Bug: 339656893, 333024272
    Change-Id: I7e0508d9e045f4dd6d479b7b72b46157ebf95b35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530336
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299610}

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -137,10 +137,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/public/ozone_platform.h"
 #endif
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
-#include "chrome/browser/shortcuts/create_shortcut_for_current_web_contents_task.h"
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
-
 using WebExposedIsolationLevel = content::WebExposedIsolationLevel;
 
 namespace chrome {
@@ -774,15 +770,7 @@ bool BrowserCommandController::ExecuteCommandWithDisposition(
       base::RecordAction(base::UserMetricsAction("CreateShortcut"));
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
       if (base::FeatureList::IsEnabled(features::kShortcutsNotApps)) {
-        content::WebContents* const web_contents =
-            browser_->tab_strip_model()->GetActiveWebContents();
-
-        if (web_contents) {
-          shortcuts::CreateShortcutForCurrentWebContentsTask::CreateAndStart(
-              *web_contents,
-              base::BindOnce(&chrome::ShowCreateShortcutDialog, web_contents),
-              base::DoNothing());
-        }
+        chrome::CreateDesktopShortcutForActiveWebContents(browser_);
       } else {
         web_app::CreateWebAppFromCurrentWebContents(
             browser_, web_app::WebAppInstallFlow::kCreateShortcut);
