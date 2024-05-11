8182bb5e5076d8cbae67b906b3310fd92e734fd1
Juan Mojica <juanmojica@google.com>
2024-05-11 00:13:55 +0000

    [lensoverlay] Support region selection on click.
    Change-Id: I08c40cedd1e63d026076c7021311491f217eb357
    Bug: b:339492493
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531851
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299616}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -96,6 +96,11 @@ constexpr base::FeatureParam<bool>
     kUseSearchContextForTextOnlyLensOverlayRequests{
         &kLensOverlay, "use-search-context-for-text-only-requests", false};
 
+constexpr base::FeatureParam<int> kLensOverlayTapRegionHeight{
+    &kLensOverlay, "tap-region-height", 300};
+constexpr base::FeatureParam<int> kLensOverlayTapRegionWidth{
+    &kLensOverlay, "tap-region-width", 300};
+
 constexpr base::FeatureParam<std::string> kHomepageURLForLens{
     &kLensStandalone, "lens-homepage-url", "https://lens.google.com/v3/"};
 
@@ -301,6 +306,14 @@ bool IsLensOverlayGoogleDseRequired() {
 
 std::string GetLensOverlayResultsSearchLoadingURL() {
   return kResultsSearchLoadingUrl.Get();
+}
+
+int GetLensOverlayTapRegionHeight() {
+  return kLensOverlayTapRegionHeight.Get();
+}
+
+int GetLensOverlayTapRegionWidth() {
+  return kLensOverlayTapRegionWidth.Get();
 }
 
 }  // namespace lens::features
