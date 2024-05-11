807b349c93d0246e6bcd38bae37d160ebb7318a3
Michael Checo <michaelcheco@google.com>
2024-05-11 01:07:34 +0000

    diagnostics: Convert system_page_test to TS
    Bug: b/245336251
    Test: browser_tests
    Change-Id: Idd31a02ab262abe1fd6ca68f75523b40020d3338
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5520000
    Reviewed-by: Camden Bickel <cambickel@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299646}

--- ash/webui/diagnostics_ui/resources/system_page.ts
+++ ash/webui/diagnostics_ui/resources/system_page.ts
@@ -43,8 +43,8 @@ export interface SystemPageElement {
 const SystemPageElementBase = I18nMixin(PolymerElement);
 
 export class SystemPageElement extends SystemPageElementBase {
-  static get is(): string {
-    return 'system-page';
+  static get is(): 'system-page' {
+    return 'system-page' as const;
   }
 
   static get template(): HTMLTemplateElement {
@@ -167,6 +167,12 @@ export class SystemPageElement extends SystemPageElementBase {
       // to avoid duplicate code in all navigatable pages.
       this.browserProxy.recordNavigation('system');
     }
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    [SystemPageElement.is]: SystemPageElement;
   }
 }
 
