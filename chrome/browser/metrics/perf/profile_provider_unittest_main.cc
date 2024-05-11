ce201867d529c6cf2f9c700c4c24ba44abc513ca
Daniel Cheng <dcheng@chromium.org>
2024-05-11 08:52:47 +0000

    Use absl::Cleanup for test debug logging in //chrome/browser/metrics
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: I425ac827b098d2b148b2c6eae1820fb6c0f00e8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530870
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Gabriel Marin <gmx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299708}

--- chrome/browser/metrics/perf/profile_provider_unittest_main.cc
+++ chrome/browser/metrics/perf/profile_provider_unittest_main.cc
@@ -3,13 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/metrics/perf/profile_provider_chromeos.h"
-
 #include <tuple>
 
 #include "base/command_line.h"
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
 #include "base/metrics/field_trial.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/statistics_recorder.h"
@@ -24,10 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/metrics/perf/collection_params.h"
 #include "chrome/browser/metrics/perf/metric_provider.h"
 #include "chrome/browser/metrics/perf/perf_events_collector.h"
+#include "chrome/browser/metrics/perf/profile_provider_chromeos.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "third_party/metrics_proto/sampled_profile.pb.h"
 #include "ui/aura/env.h"
 
@@ -195,9 +194,8 @@ class ProfileProviderRealCollectionTest : public testing::Test {
   }
 
   void AssertProfileData(SampledProfile::TriggerEvent trigger_event) {
-    // Sets up a ScopedClosureRunner for logging extra information on assertion
-    // failure.
-    base::ScopedClosureRunner scoped_log_error(base::BindOnce([]() {
+    // Log extra information on assertion failure.
+    absl::Cleanup scoped_log_error = [] {
       // Collection failed: log the failure in the UMA histogram.
       auto* histogram =
           base::StatisticsRecorder::FindHistogram("ChromeOS.CWP.CollectPerf");
@@ -210,7 +208,7 @@ class ProfileProviderRealCollectionTest : public testing::Test {
       std::string histogram_ascii;
       histogram->WriteAscii(&histogram_ascii);
       LOG(ERROR) << "Profile collection result: " << histogram_ascii;
-    }));
+    };
 
     std::vector<SampledProfile> stored_profiles;
     ASSERT_TRUE(profile_provider_->GetSampledProfiles(&stored_profiles));
@@ -221,7 +219,7 @@ class ProfileProviderRealCollectionTest : public testing::Test {
     ASSERT_TRUE(profile.has_perf_data());
 
     // Collection succeeded: don't output the error log.
-    std::ignore = scoped_log_error.Release();
+    std::move(scoped_log_error).Cancel();
   }
 
  protected:
