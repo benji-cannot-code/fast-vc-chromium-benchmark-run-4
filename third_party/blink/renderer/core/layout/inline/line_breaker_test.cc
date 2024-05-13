e40afd5b56828ac824a9dcff88f29f9d66abc0b0
Kent Tamura <tkent@chromium.org>
2024-05-13 03:27:51 +0000

    RubyLB: ::first-line style should not be applied to all rubies
    Sub-LineBreakers always referred to InlineItems for ::first-line
    incorrectly because the constructor for them don't have an
    InlineBreakToken.
    
    This CL fixes it by passing the parent LineBreaker through
    SetInputRange(), then PrepareNextLine() updates items_data_.
    
    Bug: 324111880
    Change-Id: I1c19b727335fb1b636f731b58155789b60480c09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528710
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299875}

--- third_party/blink/renderer/core/layout/inline/line_breaker_test.cc
+++ third_party/blink/renderer/core/layout/inline/line_breaker_test.cc
@@ -1141,7 +1141,8 @@ TEST_F(LineBreakerTest, SetInputRange) {
                            LineLayoutOpportunity(LayoutUnit::Max()),
                            leading_floats, nullptr, nullptr, &exclusion_space);
   // <span> to just after </span>.
-  line_breaker.SetInputRange({1, 6}, 4, LineBreaker::WhitespaceState::kLeading);
+  line_breaker.SetInputRange({1, 6}, 4, LineBreaker::WhitespaceState::kLeading,
+                             nullptr);
   LineInfo line_info;
   line_breaker.NextLine(&line_info);
   // The result should contain only <span>...</span>.
