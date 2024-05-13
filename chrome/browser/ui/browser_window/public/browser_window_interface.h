ae644e9d0f173cd7c1fd3ea72d136d300c3cc0a2
Erik Chen <erikchen@chromium.org>
2024-05-13 21:04:50 +0000

    Modularize LensPermissionBubbleController.
    This CL moves LensPermissionBubbleController into a BUILD.gn target with
    no circular dependencies. This CL has no intended behavior change. One
    side effect is the modularization of browser_element_identifiers.
    
    Change-Id: Ida12604ab161339d73b4fb8df7b75cb164e4db4a
    Bug: 339497734
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5534267
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300290}

--- chrome/browser/ui/browser_window/public/browser_window_interface.h
+++ chrome/browser/ui/browser_window/public/browser_window_interface.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BROWSER_WINDOW_PUBLIC_BROWSER_WINDOW_INTERFACE_H_
 #define CHROME_BROWSER_UI_BROWSER_WINDOW_PUBLIC_BROWSER_WINDOW_INTERFACE_H_
 
+#include "ui/base/window_open_disposition.h"
+
 // This is the public interface for a browser window. Most features in
 // //chrome/browser depend on this interface, and thus to prevent circular
 // dependencies this interface should not depend on anything else in //chrome.
@@ -17,12 +19,17 @@ namespace views {
 class WebView;
 }  // namespace views
 
+class GURL;
+
 class BrowserWindowInterface {
  public:
   // The contents of the active tab is rendered in a views::WebView. When the
   // active tab switches, the contents of the views::WebView is modified, but
   // the instance itself remains the same.
   virtual views::WebView* GetWebView() = 0;
+
+  // Opens a URL, with the given disposition.
+  virtual void OpenURL(const GURL& gurl, WindowOpenDisposition disposition) = 0;
 };
 
 #endif  // CHROME_BROWSER_UI_BROWSER_WINDOW_PUBLIC_BROWSER_WINDOW_INTERFACE_H_
--- chrome/browser/ui/lens/lens_permission_bubble_controller.h
+++ chrome/browser/ui/lens/lens_permission_bubble_controller.h
@@ -25,7 +25,7 @@ namespace views {
 class Widget;
 }
 
-class Browser;
+class BrowserWindowInterface;
 class PrefService;
 
 namespace lens {
@@ -54,16 +54,14 @@ class LensPermissionBubbleController {
   };
   // LINT.ThenChange(//tools/metrics/histograms/metadata/others/enums.xml:LensPermissionBubbleUserAction)
 
-  LensPermissionBubbleController(Browser* browser, PrefService* pref_service);
+  LensPermissionBubbleController(
+      BrowserWindowInterface* browser_window_interface,
+      PrefService* pref_service);
   LensPermissionBubbleController(const LensPermissionBubbleController&) =
       delete;
   LensPermissionBubbleController& operator=(
       const LensPermissionBubbleController&) = delete;
   ~LensPermissionBubbleController();
-
-  static std::unique_ptr<LensPermissionBubbleController> CreateInstance(
-      Browser* browser,
-      PrefService* pref_service);
 
   // Shows a tab-modal dialog. `callback` is called when the permission is
   // granted, whether by user directly accepting this dialog or indirectly via
@@ -84,7 +82,7 @@ class LensPermissionBubbleController {
   void OnPermissionPreferenceUpdated(RequestPermissionCallback callback);
 
   // The associated browser.
-  raw_ptr<Browser> browser_ = nullptr;
+  raw_ptr<BrowserWindowInterface> browser_window_interface_ = nullptr;
   // The pref service associated with the current profile.
   raw_ptr<PrefService> pref_service_ = nullptr;
   // Registrar for pref change notifications.
