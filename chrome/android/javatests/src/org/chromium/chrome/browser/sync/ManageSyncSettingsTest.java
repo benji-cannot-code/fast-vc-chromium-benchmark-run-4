af1493ecee6febf4eac9225f3a698987b7441842
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-05-13 14:45:55 +0000

    Revert "[android] Expose UPM backend outdated error in getIdentityError()"
    This reverts commit 38fc1f86fb42cc64961b5258ed2f258bea9c735e.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5653047822778368
    
    Sample build with failed test: https://ci.chromium.org/b/8748060519193963665
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.sync.AccountManagementFragmentTest#testActionForAuthError](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.sync.AccountManagementFragmentTest%23testActionForAuthError?q=VHash%3A1f05a35c44be6f93)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.sync.AccountManagementFragmentTest#testIdentityErrorCardDynamicallyHidden](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.sync.AccountManagementFragmentTest%23testIdentityErrorCardDynamicallyHidden?q=VHash%3A1f05a35c44be6f93)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.sync.AccountManagementFragmentTest#testIdentityErrorCardDynamicallyShownOnError](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.sync.AccountManagementFragmentTest%23testIdentityErrorCardDynamicallyShownOnError?q=VHash%3A1f05a35c44be6f93)
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.sync.AccountManagementFragmentTest#testIdentityErrorCardNotShownIfNoError](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.sync.AccountManagementFragmentTest%23testIdentityErrorCardNotShownIfNoError?q=VHash%3A1f05a35c44be6f93)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5653047822778368&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5515148&type=BUG
    
    Original change's description:
    > [android] Expose UPM backend outdated error in getIdentityError()
    >
    > The method will now expose the SyncError.UPM_BACKEND_OUTDATED if the
    > following are satisfied:
    > - the kEnablePasswordsAccountStorageForNonSyncingUsers flag is enabled
    > - the PASSWORDS data type is enabled
    > - GmsCore is outdated  (as defined by
    > PasswordManagerUtilBridge.isGmsCoreUpdateRequired()).
    >
    > If the flag is disabled, there should be no behavior change:
    > getIdentityError() should only be invoked for signed-in non-syncing
    > users and PASSWORDS is disabled in that case.
    >
    > Bug: 330438265
    > Change-Id: I6d2e589c1b6c37cd1c3e6ff6b435a29254ebfa12
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515148
    > Commit-Queue: Ankush Singh <ankushkush@google.com>
    > Reviewed-by: Victor Vianna <victorvianna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1299954}
    >
    
    Bug: 330438265
    Change-Id: Ib012794abb1373afa1e0ee887e19c1760c80644c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535054
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300017}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/ManageSyncSettingsTest.java
@@ -1208,7 +1208,6 @@ public class ManageSyncSettingsTest {
         verifyUrlKeyedAnonymizedDataCollectionNotSet();
     }
 
-    // TODO(crbug.com/330438265): Extend this test for the identity error card.
     @Test
     @LargeTest
     @Feature({"Sync"})
@@ -1237,7 +1236,6 @@ public class ManageSyncSettingsTest {
         Intents.release();
     }
 
-    // TODO(crbug.com/330438265): Extend this test for the identity error card.
     @Test
     @SmallTest
     @Feature({"Sync"})
