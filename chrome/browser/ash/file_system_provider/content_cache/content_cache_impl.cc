94aafb76223c98c3cebe0131ef2071f9813a8520
Ben Reich <benreich@chromium.org>
2024-05-13 00:15:19 +0000

    [FspCache] Rename LocalFile to LocalFD
    LocalFile is a wrapper around a `base::File` which is essentially an FD.
    To avoid confusion where multiple `LocalFile`s could reference the same
    file, update the naming to FD instead.
    
    Bug: b/339320580
    Test: unit_tests --gtest_filter=FileSystemProvider*
    Change-Id: Icdd20799e2f5ab2eab95660b8a2948ff2295ae65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531350
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1299848}

--- chrome/browser/ash/file_system_provider/content_cache/content_cache_impl.cc
+++ chrome/browser/ash/file_system_provider/content_cache/content_cache_impl.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/file_system_provider/content_cache/content_cache.h"
 #include "chrome/browser/ash/file_system_provider/content_cache/content_lru_cache.h"
 #include "chrome/browser/ash/file_system_provider/content_cache/context_database.h"
-#include "chrome/browser/ash/file_system_provider/content_cache/local_file.h"
+#include "chrome/browser/ash/file_system_provider/content_cache/local_fd.h"
 #include "chrome/browser/ash/file_system_provider/provided_file_system_interface.h"
 #include "net/base/io_buffer.h"
 
@@ -317,8 +317,8 @@ void ContentCacheImpl::ReadBytes(
           << length << "', bytes_on_disk = '" << ctx.bytes_on_disk()
           << "'} is available";
 
-  LocalFile& local_file =
-      GetOrCreateLocalFile(file.request_id, GetPathOnDiskFromId(ctx.id()));
+  LocalFD& local_file =
+      GetOrCreateLocalFD(file.request_id, GetPathOnDiskFromId(ctx.id()));
   local_file.ReadBytes(buffer, offset, length,
                        base::BindOnce(&ContentCacheImpl::OnBytesRead,
                                       weak_ptr_factory_.GetWeakPtr(),
@@ -418,8 +418,8 @@ void ContentCacheImpl::WriteBytes(const OpenedCloudFile& file,
   } else {
     // The ID has already been created and is known on disk, bypass generating
     // the ID and simply start writing to the file.
-    LocalFile& local_file =
-        GetOrCreateLocalFile(file.request_id, GetPathOnDiskFromId(ctx.id()));
+    LocalFD& local_file =
+        GetOrCreateLocalFD(file.request_id, GetPathOnDiskFromId(ctx.id()));
     local_file.WriteBytes(buffer, offset, length,
                           std::move(on_bytes_written_callback));
   }
@@ -454,8 +454,8 @@ void ContentCacheImpl::OnFileIdGenerated(
   DCHECK(inserted_id);
   DCHECK_GT(*inserted_id, 0);
   it->second.set_id(*inserted_id);
-  LocalFile& local_file =
-      GetOrCreateLocalFile(file.request_id, GetPathOnDiskFromId(*inserted_id));
+  LocalFD& local_file =
+      GetOrCreateLocalFD(file.request_id, GetPathOnDiskFromId(*inserted_id));
   local_file.WriteBytes(buffer, offset, length,
                         std::move(on_bytes_written_callback));
 }
@@ -599,8 +599,8 @@ std::vector<base::FilePath> ContentCacheImpl::GetCachedFilePaths() {
   return cached_file_paths;
 }
 
-LocalFile& ContentCacheImpl::GetOrCreateLocalFile(int request_id,
-                                                  const base::FilePath& path) {
+LocalFD& ContentCacheImpl::GetOrCreateLocalFD(int request_id,
+                                              const base::FilePath& path) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto [cached_file, inserted] =
       local_files_.try_emplace(request_id, path, io_task_runner_);
