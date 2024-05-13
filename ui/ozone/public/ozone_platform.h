a610d30d8682df5ea885e9e2b379129507ce71d7
Nick Diego Yamane <nickdiego@igalia.com>
2024-05-13 16:14:41 +0000

    fixup! wayland: disable eye dropper blink feature till it's properly supported
    Get rid of the leftovers from crrev.com/1269993
    
    R=sky
    
    Bug: None
    Change-Id: If8bbfc1804fc3c515885ad7996f03b79ebfcbfe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510985
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Auto-Submit: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1300061}

--- ui/ozone/public/ozone_platform.h
+++ ui/ozone/public/ozone_platform.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/containers/flat_set.h"
-#include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/task/single_thread_task_runner.h"
@@ -396,7 +395,6 @@ class COMPONENT_EXPORT(OZONE) OzonePlatform {
   bool initialized_ui_ = false;
   bool initialized_gpu_ = false;
   bool prearly_initialized_ = false;
-  bool pre_feature_list_initialized_ = false;
 
   // This value is checked on multiple threads. Declaring it volatile makes
   // modifications to |single_process_| visible by other threads. Mutex is not
