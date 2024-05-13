4a52911ebd3dab2c11c7910e72afb0cae1c0d0c2
Ernesto Izquierdo Clua <eic@google.com>
2024-05-13 14:51:28 +0000

    [xhr] Add metric tracking form submission sources on iOS
    Fixed: 329667640
    Change-Id: I62879f3b3993249e896e28befa61bc25662e5f5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522956
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300019}

--- ios/chrome/browser/autofill/model/autofill_controller_unittest.mm
+++ ios/chrome/browser/autofill/model/autofill_controller_unittest.mm
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/common/autofill_clock.h"
 #import "components/autofill/core/common/autofill_features.h"
 #import "components/autofill/core/common/field_data_manager.h"
+#import "components/autofill/core/common/mojom/autofill_types.mojom-shared.h"
 #import "components/autofill/ios/browser/autofill_agent.h"
 #import "components/autofill/ios/browser/autofill_driver_ios.h"
 #import "components/autofill/ios/browser/autofill_driver_ios_factory.h"
@@ -316,11 +317,10 @@ class AutofillControllerTest : public PlatformTest {
   std::unique_ptr<TestChromeBrowserState> browser_state_;
   std::unique_ptr<web::WebState> web_state_;
   bool processed_a_task_ = false;
-
- private:
   // Histogram tester for these tests.
   std::unique_ptr<base::HistogramTester> histogram_tester_;
 
+ private:
   std::unique_ptr<autofill::AutofillClient> autofill_client_;
 
   AutofillAgent* autofill_agent_;
@@ -859,6 +859,11 @@ TEST_F(AutofillControllerTest, CreditCardImport) {
             credit_card.GetInfo(AutofillType(CREDIT_CARD_EXP_MONTH), "en-US"));
   EXPECT_EQ(u"2999", credit_card.GetInfo(
                          AutofillType(CREDIT_CARD_EXP_4_DIGIT_YEAR), "en-US"));
+
+  histogram_tester_->ExpectUniqueSample(
+      /*name=*/kAutofillSubmissionDetectionSourceHistogram,
+      /*sample=*/mojom::SubmissionSource::FORM_SUBMISSION,
+      /*expected_count=*/1);
 }
 
 // Checks that an HTML page containing a credit card-type form which is
@@ -925,6 +930,11 @@ TEST_F(AutofillControllerTest, CreditCardImportAfterFormRemoval) {
             credit_card.GetInfo(AutofillType(CREDIT_CARD_EXP_MONTH), "en-US"));
   EXPECT_EQ(u"2999", credit_card.GetInfo(
                          AutofillType(CREDIT_CARD_EXP_4_DIGIT_YEAR), "en-US"));
+
+  histogram_tester_->ExpectUniqueSample(
+      /*name=*/kAutofillSubmissionDetectionSourceHistogram,
+      /*sample=*/mojom::SubmissionSource::XHR_SUCCEEDED,
+      /*expected_count=*/1);
 }
 
 // Checks that an HTML page containing a credit card-type form which is
@@ -1071,6 +1081,11 @@ TEST_F(AutofillControllerTest, ProfileImportAfterFormlessFormRemoval) {
             profile.GetInfo(AutofillType(ADDRESS_HOME_CITY), "en-US"));
   EXPECT_EQ(u"IL", profile.GetInfo(AutofillType(ADDRESS_HOME_STATE), "en-US"));
   EXPECT_EQ(u"55123", profile.GetInfo(AutofillType(ADDRESS_HOME_ZIP), "en-US"));
+
+  histogram_tester_->ExpectUniqueSample(
+      /*name=*/kAutofillSubmissionDetectionSourceHistogram,
+      /*sample=*/mojom::SubmissionSource::XHR_SUCCEEDED,
+      /*expected_count=*/1);
 }
 
 }  // namespace
