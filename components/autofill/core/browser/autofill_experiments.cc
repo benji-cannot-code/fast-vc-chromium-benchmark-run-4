29be35c0658687b221ab17592bb7ef37ae633dea
Qihui Zhao <qihuizhao@google.com>
2024-05-13 18:14:33 +0000

    Rename *CreditCardSupported to *PaymentMethodSupported
    This CL rename IsTouchToFillCreditCardSupported to
    IsTouchToFillPaymentMethodSupported in autofill_experiment as this
    method will be used to check both credit card and IBAN on whether
    should be supported on Clank platform.
    
    Bug: 309163724
    Change-Id: I1875caa1fcdc1c473cce496ef4bbdc141a97d36f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530003
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300140}

--- components/autofill/core/browser/autofill_experiments.cc
+++ components/autofill/core/browser/autofill_experiments.cc
@@ -264,7 +264,7 @@ bool IsDeviceAuthAvailable(
   return false;
 #endif
 }
-bool IsTouchToFillCreditCardSupported() {
+bool IsTouchToFillPaymentMethodSupported() {
 #if BUILDFLAG(IS_ANDROID)
   // Touch To Fill is only supported on Android.
   return true;
