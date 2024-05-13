ed12030fd69a2aae69c95f6087d64151ec5b315c
Qihui Zhao <qihuizhao@google.com>
2024-05-13 17:19:01 +0000

    Rename variable in TTF metrics to credit card specific
    This CL renames variables in TouchToFillPaymentMethod to credit card
    specific as IBAN related metrics will be introduced.
    
    Bug: b:332193789
    Change-Id: I815e651bd3d513d22cd530a1ca9086b35d22d0c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531855
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300115}

--- chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
+++ chrome/browser/touch_to_fill/autofill/android/internal/java/src/org/chromium/chrome/browser/touch_to_fill/payments/TouchToFillPaymentMethodMediator.java
@@ -76,11 +76,11 @@ class TouchToFillPaymentMethodMediator {
     }
 
     @VisibleForTesting
-    static final String TOUCH_TO_FILL_OUTCOME_HISTOGRAM =
+    static final String TOUCH_TO_FILL_CREDIT_CARD_OUTCOME_HISTOGRAM =
             "Autofill.TouchToFill.CreditCard.Outcome2";
 
     @VisibleForTesting
-    static final String TOUCH_TO_FILL_INDEX_SELECTED =
+    static final String TOUCH_TO_FILL_CREDIT_CARD_INDEX_SELECTED =
             "Autofill.TouchToFill.CreditCard.SelectedIndex";
 
     @VisibleForTesting
@@ -191,7 +191,7 @@ class TouchToFillPaymentMethodMediator {
         mDelegate.onDismissed(dismissedByUser);
         if (dismissedByUser) {
             RecordHistogram.recordEnumeratedHistogram(
-                    TOUCH_TO_FILL_OUTCOME_HISTOGRAM,
+                    TOUCH_TO_FILL_CREDIT_CARD_OUTCOME_HISTOGRAM,
                     TouchToFillCreditCardOutcome.DISMISS,
                     TouchToFillCreditCardOutcome.MAX_VALUE + 1);
         }
@@ -214,7 +214,8 @@ class TouchToFillPaymentMethodMediator {
                 card.getIsVirtual()
                         ? TouchToFillCreditCardOutcome.VIRTUAL_CARD
                         : TouchToFillCreditCardOutcome.CREDIT_CARD);
-        RecordHistogram.recordCount100Histogram(TOUCH_TO_FILL_INDEX_SELECTED, mCards.indexOf(card));
+        RecordHistogram.recordCount100Histogram(
+                TOUCH_TO_FILL_CREDIT_CARD_INDEX_SELECTED, mCards.indexOf(card));
     }
 
     public void onSelectedIban(Iban iban) {
@@ -284,10 +285,10 @@ class TouchToFillPaymentMethodMediator {
                                 TouchToFillPaymentMethodProperties.IbanProperties
                                         .NON_TRANSFORMING_IBAN_KEYS)
                         .with(
-                            TouchToFillPaymentMethodProperties.IbanProperties.IBAN_VALUE,
+                                TouchToFillPaymentMethodProperties.IbanProperties.IBAN_VALUE,
                                 iban.getLabel())
                         .with(
-                            TouchToFillPaymentMethodProperties.IbanProperties.IBAN_NICKNAME,
+                                TouchToFillPaymentMethodProperties.IbanProperties.IBAN_NICKNAME,
                                 iban.getNickname())
                         .with(ON_IBAN_CLICK_ACTION, () -> this.onSelectedIban(iban));
         return ibanModelBuilder.build();
@@ -337,7 +338,7 @@ class TouchToFillPaymentMethodMediator {
     private static void recordTouchToFillOutcomeHistogram(
             @TouchToFillCreditCardOutcome int outcome) {
         RecordHistogram.recordEnumeratedHistogram(
-                TOUCH_TO_FILL_OUTCOME_HISTOGRAM,
+                TOUCH_TO_FILL_CREDIT_CARD_OUTCOME_HISTOGRAM,
                 outcome,
                 TouchToFillCreditCardOutcome.MAX_VALUE + 1);
     }
