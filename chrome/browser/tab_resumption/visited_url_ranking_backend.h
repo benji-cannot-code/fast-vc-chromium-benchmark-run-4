cc4213cc7d39b188d99c7b509ce87773dc32918b
Samuel Huang <huangs@chromium.org>
2024-05-13 15:44:57 +0000

    [Android TRM] Move V2 results usage from Provider-level to Backend-level.
    Originally we envisioned Tab Resumption Module's to V2 results to be
    a Provider that uses JNI to access components/visted_url_ranking. The
    JNI was named TabResumptionBridge, and the Provider named
    SmartTabResumptionModuleDatProvider.
    
    It turns out that we still need to manage data freshness. Consequently,
    visted_url_ranking is placed at the same level as ForeignSessionHelper
    (agnostically called "sync derived"). How this is treated in code:
    * The common interface is SuggestionBackend, so far adapted only to
      ForeignSessionHelper.
    * Backend is used by SyncDerivedSuggestionEntrySource singleton.
    * Source is used by SyncDerivedTabResumptionDataProvider.
    
    This CL adapts the V2 results interface to use the same thing:
    * Remove SmartTabResumptionDataProvider.
    * Rename TabResumptionBridge / tab_resumption_bridge to
      VisitedUrlRankingBackend / visited_url_ranking_backend.
      * Stub SuggestionBackend overrides for now.
    * Builder: Use V2 usage flag to select Backend instead of Provider.
    
    Bug: 337858147
    Change-Id: Ib9c9aed3dde818174cfbf659fa47df7ef330875a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5534405
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300046}

---
+++ chrome/browser/tab_resumption/visited_url_ranking_backend.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_TAB_RESUMPTION_VISITED_URL_RANKING_BACKEND_H_
+#define CHROME_BROWSER_TAB_RESUMPTION_VISITED_URL_RANKING_BACKEND_H_
+
+#include <jni.h>
+
+#include "base/memory/raw_ptr.h"
+#include "chrome/browser/profiles/profile.h"
+
+namespace tab_resumption::jni {
+
+// Provides the fetch and rank services of the Tab resumption backend to
+// Java.
+class VisitedUrlRankingBackend {
+ public:
+  explicit VisitedUrlRankingBackend(Profile* profile);
+
+  VisitedUrlRankingBackend(const VisitedUrlRankingBackend&) = delete;
+  VisitedUrlRankingBackend& operator=(const VisitedUrlRankingBackend&) = delete;
+
+  ~VisitedUrlRankingBackend();
+
+  void Destroy(JNIEnv* env);
+
+ private:
+  raw_ptr<Profile> profile_;
+};
+
+}  // namespace tab_resumption::jni
+
+#endif  // CHROME_BROWSER_TAB_RESUMPTION_VISITED_URL_RANKING_BACKEND_H_
