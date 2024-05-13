584c81c2ab70d8fa8cf013a7998f2f716aa0b83f
Dave Tapuska <dtapuska@chromium.org>
2024-05-13 20:12:37 +0000

    [devtools] MainThreadDebugger remove some variables
    The instance variable was only used in DCHECKs and is sufficiently
    covered by the V8PerIsolateData::SetThreadDebugger DCHECK.
    
    Removing the instance_ variable allows us to also remove a static
    lock object.
    
    Change-Id: I2f4a313c09c40601f831cf065eda4fad906426ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535809
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300251}

--- third_party/blink/renderer/core/inspector/main_thread_debugger.cc
+++ third_party/blink/renderer/core/inspector/main_thread_debugger.cc
@@ -80,11 +80,6 @@ namespace blink {
 
 namespace {
 
-base::Lock& CreationLock() {
-  DEFINE_THREAD_SAFE_STATIC_LOCAL(base::Lock, lock, ());
-  return lock;
-}
-
 LocalFrame* ToFrame(ExecutionContext* context) {
   if (!context)
     return nullptr;
@@ -96,20 +91,11 @@ LocalFrame* ToFrame(ExecutionContext* context) {
 }
 }
 
-MainThreadDebugger* MainThreadDebugger::instance_ = nullptr;
-
 MainThreadDebugger::MainThreadDebugger(v8::Isolate* isolate)
     : ThreadDebuggerCommonImpl(isolate), paused_(false) {
-  base::AutoLock locker(CreationLock());
-  DCHECK(!instance_);
-  instance_ = this;
 }
 
-MainThreadDebugger::~MainThreadDebugger() {
-  base::AutoLock locker(CreationLock());
-  DCHECK_EQ(instance_, this);
-  instance_ = nullptr;
-}
+MainThreadDebugger::~MainThreadDebugger() = default;
 
 void MainThreadDebugger::ReportConsoleMessage(
     ExecutionContext* context,
