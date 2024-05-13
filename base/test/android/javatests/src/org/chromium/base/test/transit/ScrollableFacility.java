f6c25b7eaf4e44d28691c29d1a2d2cf665e1e601
Henrique Nakashima <hnakashima@chromium.org>
2024-05-13 19:48:53 +0000

    [Android] Perform pre-checks of Conditions
    - Start timers before the Trigger.
    - Check at least one Condition is not fulfilled already.
    - Get supplied values prior to onStartMonitoring() if a value is
      already available.
    
    Bug: 339907208,339498286,338636219
    Change-Id: I0c8452557026025e50340990cc02c9c48aaeb555
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530506
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300227}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ScrollableFacility.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ScrollableFacility.java
@@ -316,7 +316,13 @@ public abstract class ScrollableFacility<HostStationT extends Station>
 
             ItemOnScreenFacility<HostStationT, SelectReturnT> focusedItem =
                     new ItemOnScreenFacility<>(mHostStation, this);
-            return Facility.enterSync(focusedItem, this::maybeScrollTo);
+
+            try {
+                onView(mOnScreenViewMatcher).check(matches(isCompletelyDisplayed()));
+                return Facility.enterSync(focusedItem, /* trigger= */ null);
+            } catch (AssertionError | NoMatchingViewException e) {
+                return Facility.enterSync(focusedItem, this::triggerScrollTo);
+            }
         }
 
         protected void setSelectHandler(Callable<SelectReturnT> selectHandler) {
@@ -336,18 +342,14 @@ public abstract class ScrollableFacility<HostStationT extends Station>
             return mSelectHandler;
         }
 
-        private void maybeScrollTo() {
+        private void triggerScrollTo() {
             try {
-                onView(mOnScreenViewMatcher).check(matches(isCompletelyDisplayed()));
-            } catch (AssertionError | NoMatchingViewException e) {
-                try {
-                    onData(mOffScreenDataMatcher).perform(ViewActions.scrollTo());
-                } catch (PerformException performException) {
-                    throw TravelException.newTravelException(
-                            String.format(
-                                    "Could not scroll using data matcher %s", mOnScreenViewMatcher),
-                            performException);
-                }
+                onData(mOffScreenDataMatcher).perform(ViewActions.scrollTo());
+            } catch (PerformException performException) {
+                throw TravelException.newTravelException(
+                        String.format(
+                                "Could not scroll using data matcher %s", mOnScreenViewMatcher),
+                        performException);
             }
         }
     }
--- base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
@@ -23,7 +23,7 @@ public abstract class Transition {
     }
 
     protected final TransitionOptions mOptions;
-    @Nullable private final Trigger mTrigger;
+    @Nullable protected final Trigger mTrigger;
 
     Transition(TransitionOptions options, @Nullable Trigger trigger) {
         mOptions = options;
@@ -92,6 +92,7 @@ public abstract class Transition {
         @Nullable List<Condition> mTransitionConditions;
         long mTimeoutMs;
         int mTries = 1;
+        boolean mPossiblyAlreadyFulfilled;
 
         private TransitionOptions() {}
 
@@ -127,6 +128,12 @@ public abstract class Transition {
              */
             public Builder withRetry() {
                 mTries = 2;
+                return this;
+            }
+
+            /** The Transition's Conditions might already be all fulfilled before the Trigger. */
+            public Builder withPossiblyAlreadyFulfilled() {
+                mPossiblyAlreadyFulfilled = true;
                 return this;
             }
         }
