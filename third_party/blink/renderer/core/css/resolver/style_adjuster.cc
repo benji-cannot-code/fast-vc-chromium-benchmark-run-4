28bed451099a7f44e5bbcff252aaeed11ce1ab05
Khushal Sagar <khushalsagar@chromium.org>
2024-05-13 15:21:07 +0000

    blink: Optimize check for vt participants.
    R=vmpstr@chromium.org
    
    Change-Id: Id66d14ee73f9bbb18f912694897deb843147b512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530186
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300031}

--- third_party/blink/renderer/core/css/resolver/style_adjuster.cc
+++ third_party/blink/renderer/core/css/resolver/style_adjuster.cc
@@ -1189,9 +1189,10 @@ void StyleAdjuster::AdjustComputedStyle(StyleResolverState& state,
     element->AdjustStyle(base::PassKey<StyleAdjuster>(), builder);
   }
 
-  if (element &&
+  // We need to use styled element here to ensure coverage for pseudo-elements.
+  if (state.GetStyledElement() &&
       ViewTransitionUtils::IsViewTransitionElementExcludingRootFromSupplement(
-          *element)) {
+          *state.GetStyledElement())) {
     builder.SetElementIsViewTransitionParticipant();
   }
 
