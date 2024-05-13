10d275eb88dd0fba170123646fb44085f776c2e9
Greg Thompson <grt@chromium.org>
2024-05-13 14:32:26 +0000

    [accessibility] Optimize and clean up ui::AXUniqueId
    - Make the class final and remove the virtual dtor so that instances are
      only a 32-bit int.
    - To prevent accidental instantiation, delete the default ctor and force
      callers to explicitly invoke a static factory function.
    - Make instances movable so that they can be put into containers and
      otherwise passed around as needed.
    - Migrate from leaky LazyInstance to NoDestructor.
    
    Bug: None
    AX-Relnotes: n/a.
    Change-Id: Ibd9b9751e0edc6f602a1dd85beddf330c8210ed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5533929
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300011}

--- ui/accessibility/platform/ax_platform_node.cc
+++ ui/accessibility/platform/ax_platform_node.cc
@@ -62,7 +62,7 @@ void AXPlatformNode::Destroy() {
 int32_t AXPlatformNode::GetUniqueId() const {
   DCHECK(GetDelegate()) << "|GetUniqueId| must be called after |Init|.";
   return GetDelegate() ? GetDelegate()->GetUniqueId().Get()
-                       : kInvalidAXUniqueId;
+                       : AXUniqueId::kInvalidId;
 }
 
 std::string AXPlatformNode::ToString() {
