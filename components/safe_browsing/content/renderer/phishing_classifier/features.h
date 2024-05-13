69f68de339d49806267ef744d03e521db3583b84
Nwokedi Idika <nwokedi@chromium.org>
2024-05-13 21:02:30 +0000

    Conditionally use RESIZE_BEST
    In the event we are constructing an image embedding, use to the
    best image quality.
    
    Bug: b/340202274
    Change-Id: Ib570bf2952caae7e6dba30b488fdf3a2295e1326
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5534414
    Reviewed-by: Seung Jae Lim <andysjlim@chromium.org>
    Commit-Queue: Nwokedi Idika <nwokedi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300286}

--- components/safe_browsing/content/renderer/phishing_classifier/features.h
+++ components/safe_browsing/content/renderer/phishing_classifier/features.h
@@ -74,6 +74,8 @@ class FeatureMap {
   std::unordered_map<std::string, double> features_;
 };
 
+BASE_DECLARE_FEATURE(kConditionalImageResize);
+
 BASE_DECLARE_FEATURE(kClientSideDetectionRetryLimit);
 
 extern const base::FeatureParam<int> kClientSideDetectionRetryLimitTime;
