26452f885cea1ade03e0d13b58549ba98f4f1337
punithbnayak <punithbnayak@chromium.org>
2024-05-13 18:21:19 +0000

    [Code Health] MiraclePtr rewrites - Remove RAW_PTR_EXCLUSION
    Doc: https://docs.google.com/document/d/1VHUGnV18H_VAK2BmLQLnB8oOrR25ydvuE-8uviY2G5k/edit#heading=h.3e4d6y61tgs
    
    Bug: None
    Change-Id: I08d527d95d28322e2f978f7877d8d77216b827dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5487956
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Punith Nayak <punithbnayak@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300151}

--- ash/system/holding_space/holding_space_view_delegate.h
+++ ash/system/holding_space/holding_space_view_delegate.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/scoped_observation.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/display/display_observer.h"
@@ -211,10 +210,8 @@ class ASH_EXPORT HoldingSpaceViewDelegate
   // is used when determining the range for selection performed via shift-click.
   raw_ptr<HoldingSpaceItemView, DanglingUntriaged> selected_range_start_ =
       nullptr;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter
-  // for: #addr-of
-  RAW_PTR_EXCLUSION HoldingSpaceItemView* selected_range_end_ = nullptr;
 
+  raw_ptr<HoldingSpaceItemView> selected_range_end_ = nullptr;
   // Dictates how UI should represent holding space item views' selected states
   // to the user based on device state and `selection_size_`.
   SelectionUi selection_ui_;
