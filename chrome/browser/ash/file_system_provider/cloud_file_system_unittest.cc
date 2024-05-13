70c6b9e0cd96cef3659f93051c9a2f97429ad68b
Ben Reich <benreich@chromium.org>
2024-05-13 00:09:44 +0000

    [FspCache] Make buffer refcounted before using in the content cache
    The `ProvidedFileSystemInterface` currently accepts a rawptr for the
    `net::IOBuffer` type. For the content cache, let's ensure the buffer is
    a refcounted type by using `base::WrapRefCounted`. This fixes a
    compilation error where a static_assert is thrown [1] which expects any
    parameters to a bound callback to be of type `scoped_refptr` if they
    can be.
    
    [1] http://shortn/_zWvyGApJEq
    
    Bug: b/339320580
    Change-Id: Ifc73d35acb9aee1517ba9499a32d810ca7ddb62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531349
    Reviewed-by: Cassy Chun-Crogan <cassycc@google.com>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1299846}

--- chrome/browser/ash/file_system_provider/cloud_file_system_unittest.cc
+++ chrome/browser/ash/file_system_provider/cloud_file_system_unittest.cc
@@ -75,7 +75,7 @@ class MockContentCache : public ContentCache {
   MOCK_METHOD(void,
               ReadBytes,
               (const OpenedCloudFile& file,
-               net::IOBuffer* buffer,
+               scoped_refptr<net::IOBuffer> buffer,
                int64_t offset,
                int length,
                ProvidedFileSystemInterface::ReadChunkReceivedCallback callback),
@@ -83,7 +83,7 @@ class MockContentCache : public ContentCache {
   MOCK_METHOD(void,
               WriteBytes,
               (const OpenedCloudFile& file,
-               net::IOBuffer* buffer,
+               scoped_refptr<net::IOBuffer> buffer,
                int64_t offset,
                int length,
                FileErrorCallback callback),
@@ -257,7 +257,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest, ContiguousReadsWriteToCache) {
 
   // Set the first read bytes to return `base::File::FILE_ERROR_NOT_FOUND`, this
   // indicates that the data is not cached in the content cache.
-  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer.get(), /*offset=*/0,
+  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer, /*offset=*/0,
                                              /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(/*bytes_read=*/-1, /*has_more=*/false,
                                    base::File::FILE_ERROR_NOT_FOUND));
@@ -265,7 +265,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest, ContiguousReadsWriteToCache) {
   // Set the first write bytes to return successfully, this indicates the post
   // FSP stream to disk succeeded.
   EXPECT_CALL(*mock_content_cache,
-              WriteBytes(_, buffer.get(), /*offset=*/0,
+              WriteBytes(_, buffer, /*offset=*/0,
                          /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(base::File::FILE_OK));
 
@@ -281,12 +281,12 @@ TEST_F(FileSystemProviderCloudFileSystemTest, ContiguousReadsWriteToCache) {
                    Field(&Watcher::recursive, IsFalse()))))));
 
   // Read the next chunk.
-  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer.get(), /*offset=*/1,
+  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer, /*offset=*/1,
                                              /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(/*bytes_read=*/-1, /*has_more=*/false,
                                    base::File::FILE_ERROR_NOT_FOUND));
   EXPECT_CALL(*mock_content_cache,
-              WriteBytes(_, buffer.get(), /*offset=*/1,
+              WriteBytes(_, buffer, /*offset=*/1,
                          /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(base::File::FILE_OK));
   ReadFileSuccessfully(*cloud_file_system, file_handle, buffer, /*offset=*/1,
@@ -316,7 +316,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest,
 
   // Set the first read bytes to return `base::File::FILE_OK`, this indicates
   // that the data is fresh and available in the cache.
-  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer.get(), /*offset=*/0,
+  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer, /*offset=*/0,
                                              /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(/*bytes_read=*/1, /*has_more=*/false,
                                    base::File::FILE_OK));
@@ -342,7 +342,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest,
 
   // Make the inner callback to return `base::File::FILE_ERROR_FAILED` to
   // indicate that the actual underlying read of the cached file failed.
-  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer.get(), /*offset=*/0,
+  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer, /*offset=*/0,
                                              /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(/*bytes_read=*/-1, /*has_more=*/false,
                                    base::File::FILE_ERROR_FAILED));
@@ -368,7 +368,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest,
 
   // Set the first read bytes to return `base::File::FILE_ERROR_NOT_FOUND`, this
   // indicates that the data is not cached in the content cache.
-  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer.get(), /*offset=*/0,
+  EXPECT_CALL(*mock_content_cache, ReadBytes(_, buffer, /*offset=*/0,
                                              /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(/*bytes_read=*/-1, /*has_more=*/false,
                                    base::File::FILE_ERROR_NOT_FOUND));
@@ -378,7 +378,7 @@ TEST_F(FileSystemProviderCloudFileSystemTest,
   // should succeed back to the caller and follow up requests will defer
   // straight to the FSP.
   EXPECT_CALL(*mock_content_cache,
-              WriteBytes(_, buffer.get(), /*offset=*/0,
+              WriteBytes(_, buffer, /*offset=*/0,
                          /*length=*/1, IsNotNullCallback()))
       .WillOnce(RunOnceCallback<4>(base::File::FILE_ERROR_FAILED));
 
--- chrome/browser/ash/file_system_provider/content_cache/content_cache.h
+++ chrome/browser/ash/file_system_provider/content_cache/content_cache.h
@@ -49,7 +49,7 @@ class ContentCache {
   // if the bytes don't exist.
   virtual void ReadBytes(
       const OpenedCloudFile& file,
-      net::IOBuffer* buffer,
+      scoped_refptr<net::IOBuffer> buffer,
       int64_t offset,
       int length,
       ProvidedFileSystemInterface::ReadChunkReceivedCallback callback) = 0;
@@ -62,7 +62,7 @@ class ContentCache {
   //   - No other writer must be writing to the file at the moment
   // If any conditions are not satisfied, return false.
   virtual void WriteBytes(const OpenedCloudFile& file,
-                          net::IOBuffer* buffer,
+                          scoped_refptr<net::IOBuffer> buffer,
                           int64_t offset,
                           int length,
                           FileErrorCallback callback) = 0;
