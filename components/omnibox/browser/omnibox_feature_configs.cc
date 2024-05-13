c45885887df3b018e7b47e58bf65460128d44b5c
manukh <manukh@chromium.org>
2024-05-13 16:55:51 +0000

    [omnibox] Reorder `ReportNumZPSInSession` alphabetically
    No behavior change.
    
    Change-Id: I5e6e735de32430f254ab2bb0fe5e7d9d337b960e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532903
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300096}

--- components/omnibox/browser/omnibox_feature_configs.cc
+++ components/omnibox/browser/omnibox_feature_configs.cc
@@ -110,6 +110,13 @@ RealboxContextualAndTrendingSuggestions::
 }
 
 // static
+BASE_FEATURE(ReportNumZPSInSession::kReportNumZPSInSession,
+             "ReportNumZPSInSession",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+ReportNumZPSInSession::ReportNumZPSInSession()
+    : enabled(base::FeatureList::IsEnabled(kReportNumZPSInSession)) {}
+
+// static
 BASE_FEATURE(ShortcutBoosting::kShortcutBoost,
              "OmniboxShortcutBoost",
              base::FEATURE_ENABLED_BY_DEFAULT);
@@ -156,12 +163,5 @@ VitalizeAutocompletedKeywords::VitalizeAutocompletedKeywords() {
                                   "VitalizeAutocompletedKeywordsScore", 450)
               .Get();
 }
-
-// static
-BASE_FEATURE(ReportNumZPSInSession::kReportNumZPSInSession,
-             "ReportNumZPSInSession",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-ReportNumZPSInSession::ReportNumZPSInSession()
-    : enabled(base::FeatureList::IsEnabled(kReportNumZPSInSession)) {}
 
 }  // namespace omnibox_feature_configs
