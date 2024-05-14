04a6d9cccb4fbc071dd30c53fd4b0a50c575eae9
Khalid Peer <khalidpeer@chromium.org>
2024-05-14 23:53:12 +0000

    [omnibox] Replace deprecated `assert` with `with` in JS imports.
    The chrome://omnibox UI is currently broken due to an error caused by
    usage of the deprecated `assert` keyword in the JS imports. This CL
    fixes the issue by making use of the recommended `with` keyword as an
    alternative.
    
    In order to play nice with the current TS toolchain used when building
    Chromium, this CL also disables JS minification across all builders for
    this particular WebUI (since the `terser` minifier hasn't been updated
    yet to support the new `with` keyword).
    
    Bug: 340297713, 340278433, 339686362
    Change-Id: Ifdef7bf33724154d202d9b355e0fb5f2dfbf813b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529040
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300984}

--- chrome/browser/resources/omnibox/omnibox_input.ts
+++ chrome/browser/resources/omnibox/omnibox_input.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {OmniboxElement} from './omnibox_element.js';
 // @ts-ignore:next-line
-import sheet from './omnibox_input.css' assert {type : 'css'};
+import sheet from './omnibox_input.css' with {type : 'css'};
 
 export interface QueryInputs {
   inputText: string;
