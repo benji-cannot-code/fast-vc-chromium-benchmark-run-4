4e948298c86adc723e11811b93a3eff8815233b5
Thomas Quintanilla <tquintanilla@google.com>
2024-05-14 18:15:04 +0000

    Records number of source regs per reporting origin per navigation
    Will give us greater context on how reporting origins currently register
    navigation sources.
    
    Bug: 339659108
    Change-Id: I6a4f2462d896aec311aea2017be91c851666fa07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528761
    Auto-Submit: Thomas Quintanilla <tquintanilla@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Anthony Garant <anthonygarant@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300762}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.h
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.h
@@ -260,6 +260,11 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl final
   void MaybeStartNavigation(int64_t navigation_id);
   void MaybeDoneWithNavigation(int64_t navigation_id, bool due_to_timeout);
 
+  void AddNavigationSourceRegistrationToBatchMap(
+      int64_t navigation_id,
+      const attribution_reporting::SuitableOrigin& reporting_origin);
+  void ClearRegistrationsForNavigationBatch(int64_t navigation_id);
+
   void MaybeBindDeferredReceivers(int64_t navigation_id, bool due_to_timeout);
   void ClearRegistrationsDeferUntilNavigation(int64_t navigation_id);
 
@@ -383,6 +388,13 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl final
   // Guardrail to ensure that a navigation which can receive registrations is
   // always eventually considered done.
   SequentialTimeoutsTimer navigation_registrations_timer_;
+
+  // Stores the reporting origin of each registration tied to navigation keyed
+  // by the navigation id.
+  base::flat_map<
+      int64_t,
+      base::flat_map<attribution_reporting::SuitableOrigin, int /*count*/>>
+      registrations_per_navigation_;
 
   data_decoder::DataDecoder data_decoder_;
 
