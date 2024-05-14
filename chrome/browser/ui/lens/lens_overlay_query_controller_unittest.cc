9a2921a0bda8c6e3f14532dbdbda8ea503e13b09
Jason Hu <hujasonx@google.com>
2024-05-14 16:51:10 +0000

    [lensoverlay] Add time zone and country code to request
    Bug: 337957278
    Change-Id: Ied61bae3999266a66d47136a178d71e18b8a0ed3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513648
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300683}

--- chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
+++ chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_status_code.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/icu/source/common/unicode/locid.h"
+#include "third_party/icu/source/common/unicode/unistr.h"
+#include "third_party/icu/source/i18n/unicode/timezone.h"
 #include "third_party/lens_server_proto/lens_overlay_server.pb.h"
 #include "third_party/lens_server_proto/lens_overlay_visual_search_interaction_data.pb.h"
 #include "url/gurl.h"
@@ -43,8 +46,8 @@ constexpr char kTestServerSessionId[] = "server_session_id";
 // The fake api key to use for fetching requests.
 constexpr char kTestApiKey[] = "test_api_key";
 
-// The language to use.
-constexpr char kLanguage[] = "en-US";
+// The locale to use.
+constexpr char kLocale[] = "en-US";
 
 // The fake page information.
 constexpr char kTestPageUrl[] = "https://www.google.com";
@@ -62,6 +65,12 @@ constexpr char kVisualSearchInteractionDataQueryParameterKey[] = "vsint";
 // The encoded search context for the test page and title.
 constexpr char kTestEncodedSearchContext[] =
     "ChdodHRwczovL3d3dy5nb29nbGUuY29tLxIKUGFnZSBUaXRsZQ";
+
+// The region.
+constexpr char kRegion[] = "US";
+
+// The time zone.
+constexpr char kTimeZone[] = "America/Los_Angeles";
 
 class FakeEndpointFetcher : public EndpointFetcher {
  public:
@@ -180,7 +189,12 @@ class LensOverlayQueryControllerTest : public testing::Test {
   void SetUp() override {
     TestingProfile::Builder profile_builder;
     profile_ = profile_builder.Build();
-    g_browser_process->SetApplicationLocale(kLanguage);
+    g_browser_process->SetApplicationLocale(kLocale);
+    icu::TimeZone::adoptDefault(
+        icu::TimeZone::createTimeZone(icu::UnicodeString(kTimeZone)));
+    UErrorCode error_code = U_ZERO_ERROR;
+    icu::Locale::setDefault(icu::Locale(kLocale), error_code);
+    ASSERT_TRUE(U_SUCCESS(error_code));
   }
 };
 
@@ -214,6 +228,21 @@ TEST_F(LensOverlayQueryControllerTest, FetchInitialQuery_ReturnsResponse) {
                 .image_metadata()
                 .height(),
             100);
+  ASSERT_EQ(query_controller.sent_objects_request_.request_context()
+                .client_context()
+                .locale_context()
+                .language(),
+            kLocale);
+  ASSERT_EQ(query_controller.sent_objects_request_.request_context()
+                .client_context()
+                .locale_context()
+                .region(),
+            kRegion);
+  ASSERT_EQ(query_controller.sent_objects_request_.request_context()
+                .client_context()
+                .locale_context()
+                .time_zone(),
+            kTimeZone);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -268,11 +297,6 @@ TEST_F(LensOverlayQueryControllerTest,
                 .image_metadata()
                 .height(),
             100);
-  ASSERT_EQ(query_controller.sent_objects_request_.request_context()
-                .client_context()
-                .locale_context()
-                .language(),
-            kLanguage);
   ASSERT_TRUE(url_response_future.Get().has_url());
   ASSERT_EQ(GetSelectionTypeFromUrl(url_response_future.Get().url()),
             lens::REGION_SEARCH);
