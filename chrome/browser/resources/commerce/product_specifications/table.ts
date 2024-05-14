471299df85b0d28a9cbda786631cbcd6680998b5
Paul Adedeji <pauladedeji@google.com>
2024-05-14 17:21:29 +0000

    Display button when images are hovered over.
    *Also adds some styling for images and their containers.
    
    screen/9CyffqXVNXBrsyo
    
    Change-Id: Ia9aa88f795167b6af6be1e99dcd49b1dce7c0fb0
    Bug: 339470597
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5534114
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300713}

--- chrome/browser/resources/commerce/product_specifications/table.ts
+++ chrome/browser/resources/commerce/product_specifications/table.ts
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import './product_selector.js';
+import 'chrome://resources/cr_elements/cr_icons.css.js';
+import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 
 import type {DomRepeatEvent} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -388,6 +388,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorProductSpecificationsDivider) \
   E_CPONLY(kColorProductSpecificationsPageBackground) \
   E_CPONLY(kColorProductSpecificationsPrimaryTitle) \
+  E_CPONLY(kColorProductSpecificationsIcon) \
   E_CPONLY(kColorProductSpecificationsSecondaryTitle) \
   E_CPONLY(kColorProductSpecificationsSummaryBackground) \
   /* Profile Menu colors. */ \
