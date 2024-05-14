2a08c372b8637156f24bc3882d0305370929d3b0
Nico Weber <thakis@chromium.org>
2024-05-14 21:31:15 +0000

    services/webnn: Remove unused fstream include
    Unused after https://chromium-review.googlesource.com/c/chromium/src/+/5448033
    
    Bug: none
    Change-Id: I9c99e0d8db3b0a8796c7247777d096dc7f5ee988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5533959
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300902}

--- services/webnn/coreml/graph_builder.cc
+++ services/webnn/coreml/graph_builder.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 #include <cstdint>
-#include <fstream>
 #include <memory>
 #include <optional>
 #include <string>
