fb7d75eb53a50240455740ba32cb4c5f744aee0e
Juan Mojica <juanmojica@google.com>
2024-05-14 17:48:49 +0000

    [lensoverlay] Add custom lens overlay side panel web view.
    This allows us to turn on the context menu and handle new tab clicks
    within the side panel web contents.
    
    Change-Id: Ibdb42c71b796f3117c3417c4bec5c7c6c398f9a5
    Bug: b:337941049, b:339100617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536221
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300738}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3831,6 +3831,10 @@ bool RenderViewContextMenu::IsQRCodeGeneratorEnabled() const {
 }
 
 bool RenderViewContextMenu::IsRegionSearchEnabled() const {
+  if (!GetBrowser()) {
+    return false;
+  }
+
   if (LensOverlayController::IsEnabled(GetProfile())) {
     return GetBrowser()->is_type_normal();
   }
