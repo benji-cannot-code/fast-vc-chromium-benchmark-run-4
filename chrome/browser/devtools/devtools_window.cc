065eda303125cf10d5f4c67ac6f349760a5dc9a5
Nikolay Vitkov <nvitkov@chromium.org>
2024-05-14 17:46:07 +0000

    [DevTools] Add feature flag for Freestyler
    Bug: b/339230378
    Change-Id: Iaecece9aba24e2924a62547579f71116c43bfa38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535569
    Auto-Submit: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300736}

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -1342,6 +1342,10 @@ GURL DevToolsWindow::GetDevToolsURL(Profile* profile,
       if (blocked_reason.blocked_by_rollout) {
         url += "&ci_blockedByRollout=true";
       }
+      if (base::FeatureList::IsEnabled(
+              ::features::kDevToolsFreestylerDogfood)) {
+        url += "&freestyler_dogfood=true";
+      }
       break;
     case kFrontendWorker:
       url = kWorkerFrontendURL + remote_base;
