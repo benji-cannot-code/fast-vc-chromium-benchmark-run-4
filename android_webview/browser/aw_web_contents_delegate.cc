84ba5d485cbd453bdd9c52c236740df86570106b
kenoss <kenoss@chromium.org>
2024-05-14 14:06:57 +0000

    WebView: Relax initiator check for prerender activation
    Prerender activation checks that navigation parameters of prerender and
    activation match to prevent unintended activations. For example, this
    prevents renderer-initiated prerender (e.g. by speculationrules) and
    browser-initiated activation (e.g. navigation from omnibox).
    
    In Android WebView, we can relax this check as WebView is intended to
    host embedder-trusted contents, and embedder can enforce it with
    callbacks like `shouldInterceptRequest`. See also
    https://crrev.com/c/5501018 for API to allow preloading for embedders.
    This CL relaxes the check.
    
    Note that we use `ui::PAGE_TRANSITION_FROM_API` to distinguish a
    navigation request is triggered by `loadUrl` or not. The use of this bit
    is embedder dependent. So, this CL also adds
    `WebContentsDelegate.ShouldAllowInitiatorMismatchOfPrerender2`.
    
    We'll continue to discuss to remove initiator check (with appropriate
    replacement) for all platforms. See discussion [1] and bug [2].
    
    [1] https://docs.google.com/document/d/1rSiMrHKVSvfri1u23uQ86DMEYg45qAL-7ERRLEfMyjE
    [2] https://crbug.com/340416082
    
    Fixed: b:331591550
    Bug: b:340416082
    Change-Id: I8ca125a79256b3c4704f1bc64580cdcc42d1d681
    Unresolved-Comment-Reason: Relaxing on WebView with feature flag is approved, but we are still discussing appropriate change for all platforms. See https://docs.google.com/document/d/1rSiMrHKVSvfri1u23uQ86DMEYg45qAL-7ERRLEfMyjE
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5424690
    Commit-Queue: Ken Okada <kenoss@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300600}

--- android_webview/browser/aw_web_contents_delegate.cc
+++ android_webview/browser/aw_web_contents_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_array.h"
 #include "base/android/jni_string.h"
 #include "base/android/scoped_java_ref.h"
+#include "base/check.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -356,6 +357,23 @@ AwWebContentsDelegate::ShouldOverrideUserAgentForPrerender2() {
   // For WebView, always use the user agent override, which is set every time
   // the user agent in AwSettings is modified.
   return content::NavigationController::UA_OVERRIDE_TRUE;
+}
+
+bool AwWebContentsDelegate::ShouldAllowPartialParamMismatchOfPrerender2(
+    content::NavigationHandle& navigation_handle) {
+  // We relax initiator checks on WebView first, but continue to discuss.
+  //
+  // TODO(https://crbug.com/340416082): Relax initiator check for all platforms.
+
+  // `ui::PAGE_TRANSITION_FROM_API` bit distinguishes that the activation
+  // navigation is triggered by `WebView.loadUrl()`.
+  bool ret =
+      navigation_handle.GetPageTransition() & ui::PAGE_TRANSITION_FROM_API;
+  if (ret) {
+    CHECK(!navigation_handle.GetInitiatorFrameToken().has_value());
+    CHECK(!navigation_handle.GetInitiatorOrigin().has_value());
+  }
+  return ret;
 }
 
 scoped_refptr<content::FileSelectListener>
