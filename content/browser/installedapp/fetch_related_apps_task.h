f3e4ae06ec28c5a65a92b7a95c789f95df7a68d9
Marcos Eizayaga <maeizayaga@microsoft.com>
2024-05-14 23:14:23 +0000

    Refactor getInstalledRelatedApp to allow for matching PWAs and UWPs
    This change implements PWA matching and refactors the getInstalledRelatedApps
    implementation for Desktop to allow for PWA matching.
    
    More information in this public doc
    https://docs.google.com/document/d/1lpciOY8dhpsCPABAM_5g3yLmyjTxuJspgyEbm0QYFrM
    
    Validation for UWPs
    - Install LinkedIn App for Windows in windows store
    - Navigate to https://linkedin.com
    - Open console and run
      - `await Promise.all([navigator.getInstalledRelatedApps(), navigator.getInstalledRelatedApps()])`
      - Expected: An array of two arrays containing the LinkedIn windows platform related app
    
    Validation for PWAs
    - Navigate to https://battle-thinkable-aristosuchus.glitch.me/pwa
    - Install as a PWA
    - Open console and run
      - `await Promise.all([navigator.getInstalledRelatedApps(), navigator.getInstalledRelatedApps()])`
      - An array of two arrays containing the installed PWA (self)
    
    Bug: 332827981
    Change-Id: I0263e828df541bfc95f1678dea910e191c9095f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5449587
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marcos Eizayaga <maeizayaga@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1300962}

---
+++ content/browser/installedapp/fetch_related_apps_task.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_INSTALLEDAPP_FETCH_RELATED_APPS_TASK_H_
+#define CONTENT_BROWSER_INSTALLEDAPP_FETCH_RELATED_APPS_TASK_H_
+
+#include <vector>
+
+#include "base/functional/callback_forward.h"
+#include "third_party/blink/public/mojom/installedapp/related_application.mojom-forward.h"
+
+class GURL;
+
+namespace content {
+
+using FetchRelatedAppsTaskResult =
+    std::vector<blink::mojom::RelatedApplicationPtr>;
+using FetchRelatedAppsTaskCallback =
+    base::OnceCallback<void(FetchRelatedAppsTaskResult)>;
+
+// This task is used by InstalledAppProviderImpl to fetch related apps
+// from a specific platform.
+class FetchRelatedAppsTask {
+ public:
+  virtual ~FetchRelatedAppsTask() = default;
+  virtual void Start(
+      const GURL& frame_url,
+      std::vector<blink::mojom::RelatedApplicationPtr> related_applications,
+      FetchRelatedAppsTaskCallback done_callback) = 0;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_INSTALLEDAPP_FETCH_RELATED_APPS_TASK_H_
--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -93,6 +93,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/tts_environment_android.h"
 #else
 #include "services/video_effects/public/mojom/video_effects_processor.mojom-forward.h"
+#include "third_party/blink/public/mojom/installedapp/related_application.mojom.h"
 #endif
 
 using AttributionReportType =
@@ -1741,5 +1742,16 @@ void ContentBrowserClient::BindModelManager(
     mojo::PendingReceiver<blink::mojom::ModelManager> receiver) {
   MockModelManager::Create(rfh, std::move(receiver));
 }
+
+#if !BUILDFLAG(IS_ANDROID)
+void ContentBrowserClient::QueryInstalledWebAppsByManifestId(
+    const GURL& frame_url,
+    const GURL& manifest_id,
+    content::BrowserContext* browser_context,
+    base::OnceCallback<void(std::optional<blink::mojom::RelatedApplication>)>
+        callback) {
+  std::move(callback).Run(std::nullopt);
+}
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 }  // namespace content
