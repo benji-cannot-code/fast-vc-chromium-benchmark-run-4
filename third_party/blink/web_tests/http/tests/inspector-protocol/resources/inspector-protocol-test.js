84b631c16ec5da3d1a5cc6b0b768ac319d6e5670
Alex Rudenko <alexrudenko@chromium.org>
2024-05-14 08:47:32 +0000

    Stabilize executionContextUniqueId in tests
    The new attribute is added in https://chromium-review.googlesource.com/c/v8/v8/+/5518197
    
    Bug: none
    Change-Id: I1485cc2c54bdb4d6bbbb9a5b108ff79f282ec13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535301
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300501}

--- third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/resources/inspector-protocol-test.js
@@ -40,6 +40,7 @@ var TestRunner = class {
       'documentURL',
       'styleSheetId',
       'executionContextId',
+      'executionContextUniqueId',
       'openerId',
       'targetId',
       'browserContextId',
