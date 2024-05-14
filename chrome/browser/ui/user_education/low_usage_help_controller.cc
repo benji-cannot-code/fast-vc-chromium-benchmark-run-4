5a0308065870dd7802c38b9617d9d9103717120a
Dana Fried <dfried@chromium.org>
2024-05-14 16:07:07 +0000

    Low Usage Promo: record when the promo isn't shown due to no browser
    There are some corner cases where a new session starts e.g. before a
    browser window becomes active.
    
    In these cases we retry showing the promo a second later. However it
    would be nice to have some telemetry telling us when the first (or
    second!) attempt to show the promo fails, outside of the normal reasons
    a promo might not show.
    
    Bug: 340142181
    Change-Id: Ia3d66ddb8eb8d09edef766f4bd1d277a955363f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536031
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300659}

--- chrome/browser/ui/user_education/low_usage_help_controller.cc
+++ chrome/browser/ui/user_education/low_usage_help_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/user_education/low_usage_help_controller.h"
 
+#include "base/metrics/histogram_macros.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
 #include "chrome/browser/profiles/profile.h"
@@ -70,6 +71,13 @@ void LowUsageHelpController::MaybeShowPromo() {
   auto* const browser = chrome::FindBrowserWithProfile(profile_);
   if (!browser) {
     // This can happen if windows are still loading up; that's fine.
+
+    // Record the try count.
+    const int try_count = retrying_ ? 2 : 1;
+    UMA_HISTOGRAM_EXACT_LINEAR(
+        "UserEducation.MessageNotShown.DesktopReEngagement.NoBrowser",
+        try_count, 3);
+
     // Try again with a small delay. If this was already a retry, then just
     // don't show the promo.
     if (!retrying_) {
@@ -86,7 +94,5 @@ void LowUsageHelpController::MaybeShowPromo() {
   const bool result = browser->window()->MaybeShowStartupFeaturePromo(
       feature_engagement::kIPHDesktopReEngagementFeature);
 
-  // TODO(dfried): maybe write some additional telemetry here (though just
-  // checking the show result histograms should be fairly informative).
   (void)result;
 }
