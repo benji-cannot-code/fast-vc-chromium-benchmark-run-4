6e81212d65f6710838bf41916a15c2a7419412a7
Tzarial Kuznia <zork@google.com>
2024-05-14 18:34:08 +0000

    [a11y] Allow selection of mouse button with mouse keys
    This allows you to cycle through the current mouse button when using
    Mouse keys.
    
    BUG=b:259372916
    TEST=ash_unittests --gtest_filter=MouseKeysTest.*
    
    Change-Id: Ib5dd372093ecad6a668dd68159f5dce3e18851ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527801
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Tzarial Kuznia <zork@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300774}

--- ash/accessibility/mouse_keys/mouse_keys_controller.h
+++ ash/accessibility/mouse_keys/mouse_keys_controller.h
@@ -67,7 +67,17 @@ class ASH_EXPORT MouseKeysController : public ui::EventHandler {
     kKeyDown,
     kKeyDownRight,
     kKeyClick,
+    kKeySelectLeftButton,
+    kKeySelectRightButton,
+    kKeySelectBothButtons,
+    kKeySelectNextButton,
     kKeyCount,
+  };
+
+  enum MouseButton {
+    kLeft,
+    kRight,
+    kBoth,
   };
 
  private:
@@ -84,6 +94,7 @@ class ASH_EXPORT MouseKeysController : public ui::EventHandler {
                                    MouseKey output);
   void PressKey(MouseKey key);
   void ReleaseKey(MouseKey key);
+  void SelectNextButton();
   void RefreshVelocity();
   void UpdateState();
 
@@ -94,6 +105,7 @@ class ASH_EXPORT MouseKeysController : public ui::EventHandler {
   double max_speed_;
   gfx::Vector2d move_direction_;
   double speed_ = 0;
+  MouseButton current_mouse_button_ = kLeft;
 
   bool pressed_keys_[kKeyCount];
   gfx::Point last_mouse_position_dips_ = gfx::Point(-1, -1);
