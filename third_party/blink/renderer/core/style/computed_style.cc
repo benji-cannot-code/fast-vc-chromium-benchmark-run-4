5a2222d6e746f113413e994c37915a87aed3670f
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-05-14 22:45:51 +0000

    [field-diff] Remove ScrollAnchorDisablingPropertyChanged
    This shifts ScrollAnchorDisablingPropertyChanged to the field-diff
    logic.
    
    There should be no behaviour change.
    
    Bug: 339300436
    Change-Id: I106c924b98a7c13dbbdfca0d788116a85fb51e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512683
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300947}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -911,7 +911,7 @@ StyleDifference ComputedStyle::VisualInvalidationDiff(
 
   // The following condition needs to be at last, because it may depend on
   // conditions in diff computed above.
-  if (ScrollAnchorDisablingPropertyChanged(other, diff)) {
+  if ((field_diff & kScrollAnchor) || diff.TransformChanged()) {
     diff.SetScrollAnchorDisablingPropertyChanged();
   }
 
@@ -924,20 +924,6 @@ StyleDifference ComputedStyle::VisualInvalidationDiff(
   // transition properly.
 
   return diff;
-}
-
-bool ComputedStyle::ScrollAnchorDisablingPropertyChanged(
-    const ComputedStyle& other,
-    const StyleDifference& diff) const {
-  if (ComputedStyleBase::ScrollAnchorDisablingPropertyChanged(*this, other)) {
-    return true;
-  }
-
-  if (diff.TransformChanged()) {
-    return true;
-  }
-
-  return false;
 }
 
 bool ComputedStyle::DiffNeedsFullLayoutAndPaintInvalidation(
