d1bc91f0954a178bfc0b6309ca73b0c867b9524d
Benoit Lize <lizeb@chromium.org>
2024-05-14 09:46:02 +0000

    [content] Propagate visibility updates to inner WebContents
    Prior to this commit, inner WebContents are not notified when their
    embedding page becomes (in)visible, whereas the rendering side is
    notified. This leads to bugs, since for instance anything that relies on
    the page visibility API (web visible) doesn't get updates, but also
    other parts of blink.
    
    The clearest example (see linked bug) is that PDF page thumbnails
    disappear when switching between tabs. The root cause is that their
    TextureLayerImpls are cleared from the active tree during compositor
    frame eviction, but the main side doesn't know that it should update it,
    since it's not aware that visibility has changed.
    
    This CL addresses it, with a kill switch added in case of issues.
    
    Bug: 336843455
    Change-Id: I69c10a899e0fd5a2a2ae2ff6f6116e92c5aa2d79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494484
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300523}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 #include <vector>
 
+#include "base/feature_list.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -2668,6 +2669,32 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, DefaultFocusForNonEmbeddedPDF) {
       "is_plugin_focused;";
 
   ASSERT_EQ(true, content::EvalJs(extension_host, script));
+}
+
+IN_PROC_BROWSER_TEST_P(PDFExtensionTest, PdfVisibility) {
+  GURL url = embedded_test_server()->GetURL("/pdf/pdf_embed.html");
+  EXPECT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
+  WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  EXPECT_EQ(content::Visibility::VISIBLE, web_contents->GetVisibility());
+
+  web_contents->WasHidden();
+  EXPECT_EQ(content::Visibility::HIDDEN, web_contents->GetVisibility());
+
+  auto inner_contents = web_contents->GetInnerWebContents();
+  if (base::FeatureList::IsEnabled(chrome_pdf::features::kPdfOopif)) {
+    // No inner WebContents, nothing to do.
+    EXPECT_EQ(0u, inner_contents.size());
+    return;
+  }
+
+  ASSERT_EQ(1u, inner_contents.size());
+  WebContents* inner = inner_contents[0];
+  EXPECT_EQ(content::Visibility::HIDDEN, inner->GetVisibility());
+
+  web_contents->WasShown();
+  EXPECT_EQ(content::Visibility::VISIBLE, web_contents->GetVisibility());
+  EXPECT_EQ(content::Visibility::VISIBLE, inner->GetVisibility());
 }
 
 // A helper for waiting for the first request for |url_to_intercept|.
