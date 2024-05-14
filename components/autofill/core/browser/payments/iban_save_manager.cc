9777eba5983cc3f2391c3268092da09cd3285c2f
Qihui Zhao <qihuizhao@google.com>
2024-05-14 17:15:48 +0000

    Add "validation_regex" on GetIbanUploadDetailsResponse
    This CL adds "GetIbanUploadDetailsRequest" in preflight response
    GetIbanUploadDetailsResponse, so that it can be used to validate
    the given IBAN value on upload save.
    
    Bug: b:335730479
    Change-Id: Id28964f644809dc2734a5289425cb19f87d5bb76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5519581
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300707}

--- components/autofill/core/browser/payments/iban_save_manager.cc
+++ components/autofill/core/browser/payments/iban_save_manager.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_util.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/strike_databases/payments/iban_save_strike_database.h"
+#include "components/autofill/core/common/autofill_regexes.h"
 #include "components/sync/service/sync_user_settings.h"
 
 namespace autofill {
@@ -296,12 +297,17 @@ void IbanSaveManager::OnDidGetUploadDetails(
     bool show_save_prompt,
     Iban import_candidate,
     AutofillClient::PaymentsRpcResult result,
+    const std::u16string& validation_regex,
     const std::u16string& context_token,
     std::unique_ptr<base::Value::Dict> legal_message) {
   if (observer_for_testing_) {
     observer_for_testing_->OnReceivedGetUploadDetailsResponse();
   }
-  if (result == AutofillClient::PaymentsRpcResult::kSuccess) {
+
+  // Upload should only be offered when result is `kSuccess` and the IBAN passes
+  // regex validation.
+  if (result == AutofillClient::PaymentsRpcResult::kSuccess &&
+      MatchesRegex(import_candidate.value(), *CompileRegex(validation_regex))) {
     // Upload should only be offered when legal messages are parsed
     // successfully.
     LegalMessageLines parsed_legal_message_lines;
