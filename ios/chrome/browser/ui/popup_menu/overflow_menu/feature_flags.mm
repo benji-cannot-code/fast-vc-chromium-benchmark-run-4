0bf3d975943691c9970a856063d06bb797ae4934
Robbie Gibson <rkgibson@google.com>
2024-05-14 17:40:51 +0000

    [iOS] Enable Overflow Menu Customization to 100%
    Enabling the flag revealed a small bug in the current implementation.
    If there are 2 new items, one with an feature-defined new badge and one
    without, the first item should be sorted before the second one, which
    was not being done. The behavioral difference here is pretty minor.
    
    Bug: 294393632
    Change-Id: I408ed4b6be58d7d291b3c9df8238bdf797c30bfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5525309
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300731}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/feature_flags.mm
@@ -13,7 +13,7 @@ BASE_FEATURE(kNewOverflowMenu,
 
 BASE_FEATURE(kOverflowMenuCustomization,
              "OverflowMenuCustomization",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsNewOverflowMenuEnabled() {
   return base::FeatureList::IsEnabled(kNewOverflowMenu);
