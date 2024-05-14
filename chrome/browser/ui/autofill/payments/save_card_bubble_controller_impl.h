6a64667b5aa6159c0345437b13bf274a7e07ce45
Elly <ellyjones@chromium.org>
2024-05-14 08:08:33 +0000

    autofill: use a scoper to reset SaveCardBubble testing focus bit
    This replaces a manual call to set the global state, which also had no
    way to unset it.
    
    Bug: None
    Change-Id: I3550353f744db79756fc1b8d80a9878a43142ad2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536189
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300465}

--- chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h
+++ chrome/browser/ui/autofill/payments/save_card_bubble_controller_impl.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/auto_reset.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_controller_base.h"
 #include "chrome/browser/ui/autofill/payments/save_card_bubble_controller.h"
@@ -129,7 +130,7 @@ class SaveCardBubbleControllerImpl
   PaymentBubbleType GetPaymentBubbleType() const override;
   int GetSaveSuccessAnimationStringId() const override;
 
-  static void IgnoreWindowActivationForTesting();
+  static base::AutoReset<bool> IgnoreWindowActivationForTesting();
 
  protected:
   explicit SaveCardBubbleControllerImpl(content::WebContents* web_contents);
--- chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
+++ chrome/browser/ui/views/autofill/payments/save_card_bubble_views_browsertest.cc
@@ -235,8 +235,6 @@ class SaveCardBubbleViewsFullFormBrowserTest
     geolocation_overrider_ =
         std::make_unique<device::ScopedGeolocationOverrider>(
             kFakeGeolocationLatitude, kFakeGeolocationLongitude);
-
-    SaveCardBubbleControllerImpl::IgnoreWindowActivationForTesting();
   }
 
   void TearDownOnMainThread() override {
@@ -769,6 +767,9 @@ class SaveCardBubbleViewsFullFormBrowserTest
   scoped_refptr<network::SharedURLLoaderFactory> test_shared_loader_factory_;
   TestAutofillManagerInjector<TestAutofillManager> autofill_manager_injector_;
   std::unique_ptr<device::ScopedGeolocationOverrider> geolocation_overrider_;
+
+  base::AutoReset<bool> ignore_window_activation_for_testing_{
+      SaveCardBubbleControllerImpl::IgnoreWindowActivationForTesting()};
 };
 
 // Tests the local save bubble. Ensures that clicking the [No thanks] button
