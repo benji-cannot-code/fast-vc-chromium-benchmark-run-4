9af11dd708868d162e099f354d2d74c13f46a039
Ari Chivukula <arichiv@chromium.org>
2024-05-14 21:52:34 +0000

    Remove DisableThirdPartySessionStoragePartitioningAfterGeneralPartitioning
    This is expiring after M126. Let's clear it out ASAP given how much
    mess it created.
    
    Bug: 339811214, 1407150
    Change-Id: I321c58ee728b18e2a47edbd046e26511cc5fdab5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530589
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300916}

--- content/browser/renderer_host/frame_tree.cc
+++ content/browser/renderer_host/frame_tree.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/frame/frame_owner_element_type.h"
 #include "third_party/blink/public/common/frame/frame_policy.h"
 #include "third_party/blink/public/common/loader/loader_constants.h"
-#include "third_party/blink/public/common/storage_key/storage_key.h"
 #include "third_party/blink/public/mojom/frame/frame_owner_properties.mojom.h"
 
 namespace content {
@@ -1031,30 +1030,6 @@ void FrameTree::FocusOuterFrameTrees() {
     }
     frame_tree_to_focus = &outer_node->frame_tree();
   }
-}
-
-void FrameTree::RegisterOriginForUnpartitionedSessionStorageAccess(
-    const url::Origin& origin) {
-  if (origin.opaque()) {
-    return;
-  }
-  unpartitioned_session_storage_origins_.insert(origin);
-}
-
-void FrameTree::UnregisterOriginForUnpartitionedSessionStorageAccess(
-    const url::Origin& origin) {
-  unpartitioned_session_storage_origins_.erase(origin);
-}
-
-const blink::StorageKey FrameTree::GetSessionStorageKey(
-    const blink::StorageKey& storage_key) {
-  if (unpartitioned_session_storage_origins_.find(storage_key.origin()) !=
-      unpartitioned_session_storage_origins_.end()) {
-    // If the storage key matches a participating origin we need to return the
-    // first-party version for use in binding session storage.
-    return blink::StorageKey::CreateFirstParty(storage_key.origin());
-  }
-  return storage_key;
 }
 
 }  // namespace content
