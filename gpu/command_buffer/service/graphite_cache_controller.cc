cf8e433eb5985a509f5b1a1cab4171e901da2d74
Sunny Sachanandani <sunnyps@chromium.org>
2024-05-14 17:12:05 +0000

    graphite: Cleanup GraphiteImageProvider periodically
    GraphiteImageProvider can hold on to a lot of resources from SkPicture
    paint shaders or image filters. Under Ganesh, these used to be inside
    Skia's resource cache and were cleaned up via the usual mechanisms like
    performDeferredCleanup and freeGpuResources. With Graphite, these are
    explicitly managed by GraphiteImageProvider in Chromium, so we mimic the
    cleanup logic that we use for regular Skia resources.
    
    Bug: 340350815
    Change-Id: I72c71f1857f44d4f192dca3f075387e6c343a47f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529843
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Auto-Submit: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300704}

--- gpu/command_buffer/service/graphite_cache_controller.cc
+++ gpu/command_buffer/service/graphite_cache_controller.cc
@@ -8,20 +8,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "gpu/command_buffer/service/graphite_image_provider.h"
 #include "third_party/skia/include/gpu/graphite/Context.h"
 #include "third_party/skia/include/gpu/graphite/Recorder.h"
 
 namespace gpu::raster {
 namespace {
-constexpr base::TimeDelta kCleanupDelay = base::Seconds(5);
+// Any resources not used in the last 5 seconds should be purged.
+constexpr base::TimeDelta kResourceNotUsedSinceDelay = base::Seconds(5);
+
+// All unused resources should be purged after an idle time delay of 5 seconds.
+constexpr base::TimeDelta kPerformCleanupDelay = base::Seconds(5);
 }
 
 GraphiteCacheController::GraphiteCacheController(
     skgpu::graphite::Recorder* recorder,
     skgpu::graphite::Context* context)
     : recorder_(recorder), context_(context) {
+  CHECK(recorder_);
   timer_ = std::make_unique<base::RetainingOneShotTimer>(
-      FROM_HERE, kCleanupDelay,
+      FROM_HERE, kPerformCleanupDelay,
       base::BindRepeating(&GraphiteCacheController::PerformCleanup,
                           weak_ptr_factory_.GetWeakPtr()));
   DETACH_FROM_SEQUENCE(sequence_checker_);
@@ -33,14 +39,15 @@ GraphiteCacheController::~GraphiteCacheController() {
 
 void GraphiteCacheController::ScheduleCleanup() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  // Cleanup resources which are not used in 5 seconds.
-  constexpr std::chrono::seconds kNotUseTime{5};
   if (context_) {
-    context_->performDeferredCleanup(kNotUseTime);
+    context_->performDeferredCleanup(
+        std::chrono::seconds(kResourceNotUsedSinceDelay.InSeconds()));
   }
-  if (recorder_) {
-    recorder_->performDeferredCleanup(kNotUseTime);
-  }
+  auto* image_provider =
+      static_cast<GraphiteImageProvider*>(recorder_->clientImageProvider());
+  image_provider->PurgeImagesNotUsedSince(kResourceNotUsedSinceDelay);
+  recorder_->performDeferredCleanup(
+      std::chrono::seconds(kResourceNotUsedSinceDelay.InSeconds()));
   // Reset the timer, so PerformCleanup() will be called until ScheduleCleanup()
   // is not called for 5 seconds.
   timer_->Reset();
@@ -52,9 +59,10 @@ void GraphiteCacheController::PerformCleanup() {
   if (context_) {
     context_->freeGpuResources();
   }
-  if (recorder_) {
-    recorder_->freeGpuResources();
-  }
+  auto* image_provider =
+      static_cast<GraphiteImageProvider*>(recorder_->clientImageProvider());
+  image_provider->ClearImageCache();
+  recorder_->freeGpuResources();
 }
 
 }  // namespace gpu::raster
