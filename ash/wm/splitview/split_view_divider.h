4665591f537c913b350630d97bde78c8409fa940
Sophie Wen <sophiewen@google.com>
2024-05-14 17:49:58 +0000

    snap-groups: Fix resize on multi-display
    This fixes the cursor resize issue, which was happening because
    `divider_position` should be relative to the parent/root, not screen.
    
    Demo: https://b.corp.google.com/issues/340307930#comment2
    
    Test: added, verified it fails w/o fix in patchset 1
    Bug: b/340307930
    Change-Id: Ide43acbdee13db6d5ad8c7fad99ee0ba33b759ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536111
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300740}

--- ash/wm/splitview/split_view_divider.h
+++ ash/wm/splitview/split_view_divider.h
@@ -51,6 +51,8 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   ~SplitViewDivider() override;
 
   // static
+  // Returns the divider bounds in screen where `divider_position` is in the
+  // divider's root window's bounds.
   static gfx::Rect GetDividerBoundsInScreen(
       const gfx::Rect& work_area_bounds_in_screen,
       bool landscape,
@@ -80,8 +82,8 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   // Updates the divider's target visibility.
   void SetVisible(bool visible);
 
-  // Sets the divider's position, ensuring it meets the minimum window size
-  // requirement.
+  // Sets the divider's position in root window bounds, ensuring it meets the
+  // minimum window size requirement.
   void SetDividerPosition(int divider_position);
 
   // Updates divider position while resizing, keeping it within allowed range.
@@ -196,7 +198,8 @@ class ASH_EXPORT SplitViewDivider : public aura::WindowObserver,
   const raw_ptr<LayoutDividerController> controller_;
 
   // The distance between the origin of `divider_widget_` and the origin
-  // of the current display's work area in screen coordinates.
+  // of the current display's work area in screen coordinates, which essentially
+  // makes it relative to the divider widget's root window.
   //     |<---     divider_position_    --->|
   //     ---------------------------------------------------------------
   //     |                                  | |                        |
