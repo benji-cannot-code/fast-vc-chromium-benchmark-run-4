21c6096b63116594f639992d8c48c668d58c6416
Nathan Memmott <memmott@chromium.org>
2024-05-14 18:35:35 +0000

    FilePathWatcher: Process Change Events from ReadDirectoryChangesW
    Instead of just reporting each batch of events as a change event,
    actually go through the batch and report each change event.
    
    This is in preparation to support WatchWithChangeInfo on Windows.
    
    Bug: 321980447
    Change-Id: Ic6c8d41c06e32bd6a9dace5c9e119c53ba8ee965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5479669
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300776}

--- base/files/file_path_watcher.h
+++ base/files/file_path_watcher.h
@@ -158,6 +158,13 @@ class BASE_EXPORT FilePathWatcher {
     // allow to shut down properly while the object is still alive.
     virtual void Cancel() = 0;
 
+#if BUILDFLAG(IS_WIN)
+    // Gets the Lock associated with the base::FilePathWatcher implementation's
+    // Watch thread. Tests can use this to block that thread and cause a buffer
+    // overflow.
+    virtual Lock& GetWatchThreadLockForTest() = 0;
+#endif
+
    protected:
     friend class FilePathWatcher;
 
@@ -213,6 +220,13 @@ class BASE_EXPORT FilePathWatcher {
   bool WatchWithChangeInfo(const FilePath& path,
                            const WatchOptions& options,
                            const CallbackWithChangeInfo& callback);
+
+#if BUILDFLAG(IS_WIN)
+  // Gets the Lock associated with the base::FilePathWatcher implementation's
+  // Watch thread. Tests can use this to block that thread and cause a buffer
+  // overflow.
+  Lock& GetWatchThreadLockForTest();
+#endif
 
  private:
   explicit FilePathWatcher(std::unique_ptr<PlatformDelegate> delegate);
