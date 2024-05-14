d6a76ddf77ecf6db0a6e077d57d36194a7562de2
Anastasia Helfinstein <anastasi@google.com>
2024-05-14 17:16:25 +0000

    [ChromeVox Tutorial] Convert TutorialLesson to class-based Polymer
    TypeScript cannot understand the function-based Polymer syntax, so
    before the tutorial files can be migrated to TypeScript they must be
    converted to class-based Polymer.
    
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: Iba2c87aada83a0b8ef4fb4480a08d8cba6c3d15d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498856
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300708}

--- chrome/browser/resources/chromeos/accessibility/common/tutorial/tutorial_lesson.js
+++ chrome/browser/resources/chromeos/accessibility/common/tutorial/tutorial_lesson.js
@@ -14,46 +14,60 @@ import 'chrome://resources/ash/common/cr_elements/md_select.css.js';
 import 'chrome://resources/ash/common/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/ash/common/cr_elements/cr_shared_vars.css.js';
 
-import {html, Polymer} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {html, mixinBehaviors, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {InteractionMedium} from './constants.js';
-import {Localization} from './localization.js';
+import {Localization, LocalizationInterface} from './localization.js';
 
-export const TutorialLesson = Polymer({
-  is: 'tutorial-lesson',
+/**
+ * @constructor
+ * @extends {PolymerElement}
+ * @implements {LocalizationInterface}
+ */
+const TutorialLessonBase = mixinBehaviors([Localization], PolymerElement);
 
-  _template: html`{__html_template__}`,
+/** @polymer */
+export class TutorialLesson extends TutorialLessonBase {
+  static get is() {
+    return 'tutorial-lesson';
+  }
 
-  behaviors: [Localization],
+  static get template() {
+    return html`{__html_template__}`;
+  }
 
-  properties: {
-    lessonId: {type: Number},
+  static get properties() {
+    return {
+      lessonId: {type: Number},
 
-    title: {type: String},
+      title: {type: String},
 
-    content: {type: Array},
+      content: {type: Array},
 
-    medium: {type: String},
+      medium: {type: String},
 
-    curriculums: {type: Array},
+      curriculums: {type: Array},
 
-    practiceTitle: {type: String},
+      practiceTitle: {type: String},
 
-    practiceInstructions: {type: String},
+      practiceInstructions: {type: String},
 
-    practiceFile: {type: String},
+      practiceFile: {type: String},
 
-    actions: {type: Array},
+      actions: {type: Array},
 
-    autoInteractive: {type: Boolean, value: false},
+      autoInteractive: {type: Boolean, value: false},
 
-    // Observed properties.
-    /** @type {number} */
-    activeLessonId: {type: Number, observer: 'setVisibility'},
-  },
+      // Observed properties.
+      /** @type {number} */
+      activeLessonId: {type: Number, observer: 'setVisibility'},
+    };
+  }
 
   /** @override */
   ready() {
+    super.ready();
+
     this.$.contentTemplate.addEventListener('dom-change', evt => {
       this.dispatchEvent(new CustomEvent('lessonready', {composed: true}));
     });
@@ -73,7 +87,7 @@ export const TutorialLesson = Polymer({
         evt.stopPropagation();
       }, true);
     }
-  },
+  }
 
   /**
    * Updates this lessons visibility whenever the active lesson of the tutorial
@@ -86,7 +100,7 @@ export const TutorialLesson = Polymer({
     } else {
       this.hide();
     }
-  },
+  }
 
   /** @private */
   show() {
@@ -98,10 +112,10 @@ export const TutorialLesson = Polymer({
       // pressed. To ensure users hear instructions for these lessons, place
       // focus on the first piece of text content.
       // Shorthand for Polymer.dom(this.root).querySelector(...).
-      focus = this.$$('p');
+      focus = this.shadowRoot.querySelector('p');
     } else {
       // Otherwise, we can place focus on the lesson title.
-      focus = this.$$('h1');
+      focus = this.shadowRoot.querySelector('h1');
     }
     if (!focus) {
       throw new Error('A lesson must have an element to focus.');
@@ -111,16 +125,14 @@ export const TutorialLesson = Polymer({
       // Call show() again if we weren't able to focus the target element.
       setTimeout(() => this.show(), 500);
     }
-  },
+  }
 
   /** @private */
   hide() {
     this.$.container.hidden = true;
-  },
-
+  }
 
   // Methods for managing the practice area.
-
 
   /**
    * Asynchronously populates practice area.
@@ -140,31 +152,31 @@ export const TutorialLesson = Polymer({
       console.error('Failed to open practice file: ' + path);
       console.error(err);
     }
-  },
+  }
 
   /** @private */
   startPractice() {
     this.notifyStartPractice();
     this.$.practice.showModal();
     this.$.practiceInstructions.focus();
-  },
+  }
 
   /** @private */
   endPractice() {
     this.$.practice.close();
     this.notifyEndPractice();
     this.$.startPractice.focus();
-  },
+  }
 
   /** @private */
   notifyStartPractice() {
     this.dispatchEvent(new CustomEvent('startpractice', {composed: true}));
-  },
+  }
 
   /** @private */
   notifyEndPractice() {
     this.dispatchEvent(new CustomEvent('endpractice', {composed: true}));
-  },
+  }
 
   // Miscellaneous methods.
 
@@ -179,7 +191,7 @@ export const TutorialLesson = Polymer({
     }
 
     return false;
-  },
+  }
 
   /**
    * @return {boolean}
@@ -191,17 +203,17 @@ export const TutorialLesson = Polymer({
     }
 
     return false;
-  },
+  }
 
   /** @return {Element} */
   get contentDiv() {
     return this.$.content;
-  },
+  }
 
   /** @return {string} */
   getTitleText() {
     return this.$.title.textContent;
-  },
+  }
 
   /** @private */
   localizePracticeAreaContent() {
@@ -211,7 +223,7 @@ export const TutorialLesson = Polymer({
       const msgId = element.getAttribute('msgid');
       element.textContent = this.getMsg(msgId);
     }
-  },
+  }
 
   /**
    * @private
@@ -225,5 +237,6 @@ export const TutorialLesson = Polymer({
     // Automatically return the description for touch, since the only supported
     // interaction mediums are touch and keyboard.
     return this.getMsg('tutorial_touch_lesson_title_description');
-  },
-});
+  }
+}
+customElements.define(TutorialLesson.is, TutorialLesson);
