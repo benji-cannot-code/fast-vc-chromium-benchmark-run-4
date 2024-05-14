35ba7bf6c7bfb721dddb973d9a19796a510289d3
Aaron Leventhal <aleventhal@google.com>
2024-05-14 19:16:43 +0000

    [A11y] Simplify AX tree for input type=file
    - Do not include extra inner button in a11y tree. It's strange and
    potentially problematic to have nested buttons, even though the
    inner button is invisible.
    - Allows Blink to uniformly prune aria-hidden UA shadow content. This
    helps simplify ComputeIsIgnoredButIncludedInTree().
    
    Bug: none
    Change-Id: I44b3f283d704f3c89c3035eb3997ca1d2a14310a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535855
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300799}

--- chrome/browser/ash/startup_settings_cache_browsertest.cc
+++ chrome/browser/ash/startup_settings_cache_browsertest.cc
@@ -47,7 +47,7 @@ IN_PROC_BROWSER_TEST_F(StartupSettingsCacheTest, RendererLocale) {
   // The localized button label in the renderer is in Spanish.
   WaitForAccessibilityTreeToContainNodeWithName(
       browser()->tab_strip_model()->GetActiveWebContents(),
-      "Seleccionar archivo");
+      "Seleccionar archivo: Ning\u00FAn archivo seleccionado");
 }
 
 }  // namespace
