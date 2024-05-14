ab599edcf821132f7b0bcbd741432a6ee8bb874c
Tom Burgin <bur@google.com>
2024-05-14 19:17:54 +0000

    immersive fullscreen: tab drag prevent fullscreen window restore
    When dragging the last tab[s] of an immersive fullscreen window
    the window will exit fullscreen. Immersive fullscreen on macOS hosts
    the tab strip in a separate widget from the browser. When exiting
    fullscreen the tab strip is returned the the browsers window. This
    widget change mid drag causing issues for the tab drag controller.
    
    Until a proper fix can be implemented this CL disables fullscreen window
    restoration during a tab drag while in immersive fullscreen.
    
    Bug: 339074805
    Change-Id: I26e9cc59e3aec5834c252450abb367a705e9add1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538489
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Tom Burgin <bur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1300801}

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/containers/adapters.h"
 #include "base/containers/contains.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/i18n/rtl.h"
@@ -57,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/web_app_tabbed_utils.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "content/public/browser/web_contents.h"
@@ -608,6 +610,17 @@ bool TabDragController::CanRemoveTabDuringDrag(
   return !IsDraggingTab(contents) || is_mutating_;
 }
 
+bool TabDragController::CanRestoreFullscreenWindowDuringDrag() const {
+#if BUILDFLAG(IS_MAC)
+  // On macOS in immersive fullscreen mode restoring the window moves the tab
+  // strip between widgets breaking a number of assumptions during the drag.
+  // Disable window restoration during a drag while in immersive fullscreen.
+  return !base::FeatureList::IsEnabled(features::kImmersiveFullscreen);
+#else
+  return true;
+#endif
+}
+
 void TabDragController::Drag(const gfx::Point& point_in_screen) {
   TRACE_EVENT1("views", "TabDragController::Drag", "point_in_screen",
                point_in_screen.ToString());
@@ -674,7 +687,8 @@ void TabDragController::Drag(const gfx::Point& point_in_screen) {
       views::Widget* widget = GetAttachedBrowserWidget();
       gfx::Rect new_bounds;
       gfx::Vector2d drag_offset;
-      if (was_source_maximized_ || was_source_fullscreen_) {
+      if (was_source_maximized_ ||
+          (was_source_fullscreen_ && CanRestoreFullscreenWindowDuringDrag())) {
         did_restore_window_ = true;
         // When all tabs in a maximized browser are dragged the browser gets
         // restored during the drag and maximized back when the drag ends.
