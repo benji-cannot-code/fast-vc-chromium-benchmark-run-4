09a920ca7fe80600a03c813e329071265b10970c
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-05-14 20:19:51 +0000

    [field-diff] UpdatePropertySpecificDifferencesTextDecorationOrColor
    This shifts UpdatePropertySpecificDifferencesTextDecorationOrColor
    to the field-diff logic.
    
    The one behaviour change in this patch is making TextEmphasisFill
    trigger a full paint/layout invalidation.
    
    This was/is hiding a near impossible bug to trigger. E.g. TextEmphasisFill triggers the use of different glyphs to be used
    for text-emphasis. Almost always this will be the same size.
    
    However it is *possible* for a font to have different sized glyphs for
    this, hence trigger a full layout/paint invalidation.
    
    There should be no other behaviour change.
    
    Bug: 339300436
    Change-Id: I37bd548b3773061a1b739bd68f878c1a734ae4a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510986
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300844}

--- third_party/blink/renderer/core/style/computed_style.cc
+++ third_party/blink/renderer/core/style/computed_style.cc
@@ -1255,10 +1255,7 @@ void ComputedStyle::UpdatePropertySpecificDifferences(
   if (field_diff & kTextDecoration) {
     diff.SetTextDecorationOrColorChanged();
   }
-
-  if (!diff.NeedsNormalPaintInvalidation() &&
-      ComputedStyleBase::UpdatePropertySpecificDifferencesTextDecorationOrColor(
-          *this, other)) {
+  if (field_diff & kColor) {
     diff.SetTextDecorationOrColorChanged();
   }
 
