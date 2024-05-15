45279822407273fa4484c0f552c05e0c1605c511
Arthur Milchior <arthurmilchior@chromium.org>
2024-05-15 13:42:28 +0000

    [iOS][Bookmarks] Repair testCachePositionIsResetWhenNodeIsDeleted
    The test was flaky because, when of a race conflict.
    When a displayed folder is deleted, the view jump to its parent.
    But if the navigation controller is dismissed, the view is created and never stopped.
    
    In this case, the view was created after the dismiss started and before the completion block.
    
    By recording that the navigation controller will be dismissed and
    avoiding creating new views, the issue is avoided.
    
    As far as I can tell, there is no need for similar protections in
    other places where the views could be created, because other places
    require user interaction.
    
    Fixed: 339676044
    Change-Id: I65ed2da48a8850495e966b47c1374feadfe86cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538270
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301255}

--- ios/chrome/browser/ui/bookmarks/home/bookmarks_home_view_controller.mm
+++ ios/chrome/browser/ui/bookmarks/home/bookmarks_home_view_controller.mm
@@ -234,6 +234,9 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
   raw_ptr<const bookmarks::BookmarkNode> _externalBookmark;
   // Whether the view controller was requested to shutdown.
   BOOL _isShutDown;
+  // Whether the navigation controller is being dismissed.
+  // In which case, do not open anything on top of it.
+  BOOL _isBeingDismissed;
 }
 
 @synthesize editingFolderCell = _editingFolderCell;
@@ -382,12 +385,17 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
   return stack;
 }
 
+- (void)willDismiss {
+  _isBeingDismissed = YES;
+}
+
 - (void)willDismissBySwipeDown {
   if (self.searchController.active) {
     // Dismiss the keyboard if trying to dismiss the VC so the keyboard doesn't
     // linger until the VC dismissal has completed.
     [self.searchController.searchBar endEditing:YES];
   }
+  [self willDismiss];
 }
 
 #pragma mark - UIViewController
@@ -1055,6 +1063,11 @@ std::vector<GURL> GetUrlsToOpen(const std::vector<const BookmarkNode*>& nodes) {
 
 - (void)jumpToFolder:(const bookmarks::BookmarkNode*)folder {
   // Clear bookmark path cache.
+  if (_isBeingDismissed) {
+    // The navigation controller is being dismissed.
+    // Do not open more views.
+    return;
+  }
   int64_t unusedFolderId;
   BookmarkModelType modelType;
   int unusedIndexPathRow;
