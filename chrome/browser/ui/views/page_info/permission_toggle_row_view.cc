05e41cfdaeb962506835fde55a1c4c7bb47fd68e
Elias Klim <elklm@chromium.org>
2024-05-15 15:20:12 +0000

    Revert "[LHS indicator] Update PageInfo subtitle on permission reset"
    This reverts commit 6536d599fe7a190e7029ba62a55e4503f68da6a8.
    
    Reason for revert: Potential crash https://g-issues.chromium.org/issues/340814282
    
    Original change's description:
    > [LHS indicator] Update PageInfo subtitle on permission reset
    >
    > Bug: 340539963
    > Change-Id: I9e72c75b89db1ec22669fb0b022fb5a7ffd7cd58
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541795
    > Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    > Commit-Queue: Elias Klim <elklm@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1301264}
    
    Bug: 340539963
    Change-Id: Idc4f5e813ca53b274e85de0ffc1daae29d2bfbb8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538285
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Elias Klim <elklm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301309}

--- chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
+++ chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/image_button_factory.h"
 #include "ui/views/controls/button/toggle_button.h"
 #include "ui/views/controls/highlight_path_generator.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/style/typography.h"
 #include "ui/views/view_class_properties.h"
@@ -90,11 +91,11 @@ PermissionToggleRowView::PermissionToggleRowView(
   int settings_text_id = 0, settings_link_id = 0;
   if (delegate->ShouldShowSettingsLinkForPermission(
           permission.type, &settings_text_id, &settings_link_id)) {
-    permission_blocked_on_system_level_ = true;
+    permission_disabled_on_system_level_ = true;
     std::u16string settings_text_for_link =
         l10n_util::GetStringUTF16(settings_link_id);
     size_t offset;
-    blocked_on_system_level_label_ =
+    views::StyledLabel* label =
         row_view_->AddSecondaryStyledLabel(l10n_util::GetStringFUTF16(
             settings_text_id, settings_text_for_link, &offset));
     base::RepeatingClosure clicked = base::BindRepeating(
@@ -102,7 +103,7 @@ PermissionToggleRowView::PermissionToggleRowView(
           row->delegate_->SettingsLinkClicked(type);
         },
         base::Unretained(this), permission.type);
-    blocked_on_system_level_label_->AddStyleRange(
+    label->AddStyleRange(
         gfx::Range(offset, offset + settings_text_for_link.length()),
         views::StyledLabel::RangeStyleInfo::CreateForLink(clicked));
 
@@ -232,7 +233,7 @@ void PermissionToggleRowView::InitForUserSource(
     views::InstallCircleHighlightPathGenerator(subpage_button.get());
     subpage_button->SetMinimumImageSize({icon_size, icon_size});
     subpage_button->SetFlipCanvasOnPaintForRTLUI(false);
-    if (permission_blocked_on_system_level_) {
+    if (permission_disabled_on_system_level_) {
       subpage_button->SetEnabled(false);
     }
     row_view_->AddControl(std::move(subpage_button));
@@ -272,23 +273,13 @@ void PermissionToggleRowView::InitForManagedSource(
 }
 
 void PermissionToggleRowView::UpdateUiOnPermissionChanged() {
-  if (blocked_on_system_level_label_) {
-    if (permission_.setting == CONTENT_SETTING_DEFAULT) {
-      permission_blocked_on_system_level_ = false;
-      blocked_on_system_level_label_->SetVisible(false);
-    } else {
-      permission_blocked_on_system_level_ = true;
-      blocked_on_system_level_label_->SetVisible(true);
-    }
-  }
-
   // Change the permission icon to reflect the selected setting.
   row_view_->SetIcon(PageInfoViewFactory::GetPermissionIcon(
-      permission_, permission_blocked_on_system_level_));
+      permission_, permission_disabled_on_system_level_));
 
   // Update toggle state if it is used.
   if (toggle_button_) {
-    toggle_button_->SetEnabled(!permission_blocked_on_system_level_);
+    toggle_button_->SetEnabled(!permission_disabled_on_system_level_);
     toggle_button_->AnimateIsOn(PageInfoUI::IsToggleOn(permission_));
   }
 
