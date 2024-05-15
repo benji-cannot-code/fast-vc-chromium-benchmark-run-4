2264d9e797a0d12d5aaa17a43b1062872d64c135
Tsuyoshi Horo <horo@chromium.org>
2024-05-15 04:02:27 +0000

    Roll src/net/third_party/quiche/src/ d3bc5ffc9..64cdc52a2 (13 commits)
    https://quiche.googlesource.com/quiche.git/+log/d3bc5ffc929b..64cdc52a285d
    
    $ git log d3bc5ffc9..64cdc52a2 --date=short --no-merges --format='%ad %ae %s'
    2024-05-14 quiche-dev Remove unnecessary break statement in WebTransportWriteBlockedList::IsStreamBlocked()
    2024-05-14 birenroy Fixes accounting for DataFrameSources in NgHttp2Adapter.
    2024-05-14 quiche-dev Resolve the following 4 technical debt issues: (readability-avoid-const-params-in-decls)
    2024-05-14 vasilvv Implement QuicWriteBlockedList version that supports WebTransport priority scheme.
    2024-05-14 wub Set `gfe2_restart_flag_quic_opport_bundle_qpack_decoder_data5` to false on chromium.
    2024-05-14 birenroy Associates all test cases that involve a `DataFrameSource` with a test fixture.
    2024-05-14 quiche-dev Enabling rolled out flags.
    2024-05-13 rch Deprecate --gfe2_reloadable_flag_quic_enable_http3_metadata_decoding
    2024-05-13 birenroy Migrates the remaining tests from TestDataFrameSource -> VisitorDataSource.
    2024-05-13 birenroy Migrates oghttp2_adapter_test.cc from TestDataFrameSource to VisitorDataSource.
    2024-05-13 birenroy Migrates nghttp2_adapter_test.cc from TestDataFrameSource to VisitorDataSource.
    2024-05-13 birenroy Adds VisitorDataSource, which invokes visitor methods to set up and send DATA frames.
    2024-05-13 wub No public description
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Change-Id: I20f5ad9c28e8d678b42e1ffc7ad14d650481ea60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538830
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301082}

--- net/quic/quic_http_utils.cc
+++ net/quic/quic_http_utils.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/metrics/histogram_macros.h"
+#include "base/strings/string_number_conversions.h"
 #include "net/spdy/spdy_log_util.h"
 
 namespace net {
@@ -42,22 +43,15 @@ base::Value::Dict QuicRequestNetLogParams(quic::QuicStreamId stream_id,
     case quic::QuicPriorityType::kWebTransport: {
       auto web_transport_priority = priority.web_transport();
       dict.Set("quic_priority_type", "web_transport");
-      const char* stream_type = "invalid";
-      switch (web_transport_priority.stream_type) {
-        case quic::WebTransportStreamPriority::StreamType::kData:
-          stream_type = "data";
-          break;
-        case quic::WebTransportStreamPriority::StreamType::kHttp:
-          stream_type = "http";
-          break;
-        case quic::WebTransportStreamPriority::StreamType::kStatic:
-          stream_type = "static";
-          break;
-      }
-      dict.Set("web_transport_stream_type", stream_type);
-      // send_order is an int64_t, but base::Value doesn't support that type.
+      dict.Set("web_transport_session_id",
+               static_cast<int>(web_transport_priority.session_id));
+
+      // `send_group_number` is an uint64_t, `send_order` is an int64_t. But
+      // base::Value doesn't support these types.
       // Case to a double instead. As this is just for diagnostics, some loss of
       // precision is acceptable.
+      dict.Set("web_transport_send_group_number",
+               static_cast<double>(web_transport_priority.send_group_number));
       dict.Set("web_transport_send_order",
                static_cast<double>(web_transport_priority.send_order));
       break;
