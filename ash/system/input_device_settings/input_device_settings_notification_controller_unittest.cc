31a182c6b56a1eb0fff0c8cec77d0df2bd847c14
Michael Checo <michaelcheco@google.com>
2024-05-15 20:19:41 +0000

    [WE] Use new prefs for mouse/graphics tablet notifications
    Bug: b/329686601
    Change-Id: Iafdd46a470982fb18b13859261d989c7cc9596cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535059
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301532}

--- ash/system/input_device_settings/input_device_settings_notification_controller_unittest.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/ash_test_base.h"
 #include "base/containers/contains.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/values.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -796,6 +797,28 @@ TEST_F(InputDeviceSettingsNotificationControllerTest,
   EXPECT_EQ(expected_notification_count, message_center()->NotificationCount());
   EXPECT_TRUE(message_center()->FindVisibleNotificationById(
       "welcome_experience_keyboards_2"));
+}
+
+TEST_F(InputDeviceSettingsNotificationControllerTest,
+       CorrectNotificationPrefUsed) {
+  mojom::MousePtr mojom_mouse = mojom::Mouse::New();
+  mojom_mouse->device_key = "0001:0001";
+  mojom_mouse->id = 1;
+  mojom_mouse->settings = mojom::MouseSettings::New();
+  controller()->NotifyMouseIsCustomizable(*mojom_mouse);
+  EXPECT_TRUE(message_center()->FindVisibleNotificationById(
+      "peripheral_customization_mouse_1"));
+  EXPECT_FALSE(message_center()->FindVisibleNotificationById(
+      "welcome_experience_mouse_1"));
+  base::test::ScopedFeatureList feature_list;
+  feature_list.InitAndEnableFeature(features::kWelcomeExperience);
+  mojom_mouse->id = 2;
+  mojom_mouse->device_key = "0001:0002";
+  controller()->NotifyMouseIsCustomizable(*mojom_mouse);
+  EXPECT_TRUE(message_center()->FindVisibleNotificationById(
+      "welcome_experience_mouse_2"));
+  EXPECT_FALSE(message_center()->FindVisibleNotificationById(
+      "peripheral_customization_mouse_2"));
 }
 
 TEST_F(InputDeviceSettingsNotificationControllerTest,
