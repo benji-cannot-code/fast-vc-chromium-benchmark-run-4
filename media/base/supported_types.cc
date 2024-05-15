a1894d4df5543e6ff9fc242612b827cea091815f
Austin Osagie <austinosagie@google.com>
2024-05-15 21:20:31 +0000

    Remove unused include from supported_types.cc
    Bug: b/338100595
    Change-Id: I0a8b70bdbacc1fa86a3592f41a3e4ca0b32e3089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527575
    Commit-Queue: Austin Osagie <austinosagie@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301578}

--- media/base/supported_types.cc
+++ media/base/supported_types.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/media_client.h"
 #include "media/base/media_switches.h"
 #include "media/media_buildflags.h"
-#include "ui/display/display_switches.h"
 #include "ui/gfx/hdr_metadata.h"
 
 #if BUILDFLAG(ENABLE_LIBVPX)
