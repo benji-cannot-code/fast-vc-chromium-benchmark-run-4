f7887e4eca90134adf952e9f55f82b94aa58ab32
Ryan Sultanem <rsult@google.com>
2024-05-15 17:07:55 +0000

    [Uno-D][Metrics] Create SigninMetricsService for metrics recording
    Implemented as a KeyedService that depends on the IdentityManager
    and contains a pref service to write/read prefs. Listens to the
    IdentityManager sign in events to record metrics.
    
    Considered implementations:
    1) Keyed Service (manages it's own scope).
    2) Member of the IdentityManager, most events are directly emitted from
    the IdentityManager itself.
    3) Some existing service that already listens to the IdentityManager
    such as (AccountInvestigator or GAIAInfoUpdateService).
    
    Deciding to go with option (1) because of the following:
    - The metrics that will be recorded are not very trivial and will need
    read/write from prefs while listening to events from the IdentityManager
    so mixing up with existing (potentially already non trivial) code is
    not great.
    - Solution (2) could be good, however it would require to either trigger
    specific calls to the structure next to the observers, so we could just
    use the observer itself. Also it would be better not to mix between
    the logic and the metrics.
    - Solution (2) could also be implemented as a member + observer of
    IdentityManager, in that case the IdentityManager would only be a
    holder for the structure, for which it was cleaner to have a
    dedicated KeyedService with a proper dependency. Also it would be
    simpler for direct access if needed.
    
    Bug: b:333072229
    Change-Id: I10835065a842f32227618ec2429c00e69d9db347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542075
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301388}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -170,6 +170,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/signin/primary_account_policy_manager_factory.h"
+#include "chrome/browser/signin/signin_metrics_service_factory.h"
 #include "chrome/browser/signin/signin_profile_attributes_updater_factory.h"
 #include "chrome/browser/ssl/https_first_mode_settings_tracker.h"
 #include "chrome/browser/ssl/sct_reporting_service_factory.h"
@@ -1185,6 +1186,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if BUILDFLAG(ENABLE_DICE_SUPPORT) || BUILDFLAG(IS_CHROMEOS_LACROS)
   SigninManagerFactory::GetInstance();
 #endif
+  SigninMetricsServiceFactory::GetInstance();
   SigninProfileAttributesUpdaterFactory::GetInstance();
   if (site_engagement::SiteEngagementService::IsEnabled()) {
     site_engagement::SiteEngagementServiceFactory::GetInstance();
