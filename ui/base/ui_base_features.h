e188404f392998744dc42aec600a9b88eae8c1ba
Dirk Pranke <dpranke@google.com>
2024-05-15 02:18:36 +0000

    Remove misleading "Keep sorted!" comment in ui_base_features.h.
    We weren't actually keeping the file sorted, and we decided
    removing the comment was an acceptable alternative to actually
    sorting the file and introducing a bunch of diffs.
    
    Change-Id: I8621d8fbc2972629f2728dba805aeaa3eda33cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540418
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301051}

--- ui/base/ui_base_features.h
+++ ui/base/ui_base_features.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace features {
 
-// Keep sorted!
-
 COMPONENT_EXPORT(UI_BASE_FEATURES)
 BASE_DECLARE_FEATURE(kExperimentalFlingAnimation);
 COMPONENT_EXPORT(UI_BASE_FEATURES) BASE_DECLARE_FEATURE(kFocusFollowsCursor);
