2997bb9f410e2834f2993307d21bb7c9dff88ce2
Anastasia Helfinstein <anastasi@google.com>
2024-05-15 15:43:02 +0000

    [Switch Access] Move FocusRingManager to explicit initialization
    AX-Relnotes: n/a.
    Bug: b/261227533
    Change-Id: I7772348381f8d37364fbb83bcaf9ff1c76f080ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5208350
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301329}

--- chrome/browser/resources/chromeos/accessibility/switch_access/test_support.js
+++ chrome/browser/resources/chromeos/accessibility/switch_access/test_support.js
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   module = await import('./switch_access.js');
   globalThis.SwitchAccess = module.SwitchAccess;
+  await SwitchAccess.ready();
 
   const focusRingState = {
     'primary': {'role': '', 'name': ''},
