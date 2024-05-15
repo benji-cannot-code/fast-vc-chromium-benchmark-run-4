1e9a2d5ecdb418cdac0ef2ded03243f8f2ba3de9
Nick Diego Yamane <nickdiego@igalia.com>
2024-05-15 16:36:17 +0000

    wayland: factor HandleDragEnd out of Reset in data drag controller
    The nested message loop used for outgoing drag sessions should be tied
    to the dnd_drop_performed event rather than the whole underlying drag
    flow, which includes async wl_data_source events, such as, dnd_finished
    and cancelled. Further details can be found at analysis [1].
    
    Another effect of this change is that it works as a mitigation to
    prevent browser process from hanging, ie: stopping to respond to input
    events caused by an never ending drag-and-drop loop when dealing with
    misbehaving/buggy or malicious Wayland compositors. A real case of this
    was found in the wild with some versions of Kwin 6, as described in the
    linked crbugs.
    
    [1] https://notes.nickdiego.dev/chromium/wayland-events-during-drag
    
    Bug: 329479345, 336449364, 325741108
    Test: WaylandDataDragControllerTest.OutgoingSessionWithoutDndFinished in ozone_unittests
    Change-Id: Ib4757223ce9e490c2a65999da77bf1257f7fc3ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539621
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Max Ihlenfeldt <max@igalia.com>
    Reviewed-by: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1301363}

--- ui/ozone/platform/wayland/host/wayland_data_source.h
+++ ui/ozone/platform/wayland/host/wayland_data_source.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/notreached.h"
+#include "base/time/time.h"
 #include "ui/ozone/platform/wayland/common/wayland_object.h"
 
 struct wl_data_source;
@@ -65,6 +66,10 @@ class DataSource {
     virtual void OnDataSourceSend(DataSource<T>* source,
                                   const std::string& mime_type,
                                   std::string* contents) = 0;
+    // Optional callback intended to be implemented only by dnd-capable delegate
+    // implementations.
+    virtual void OnDataSourceDropPerformed(DataSource<T>* source,
+                                           base::TimeTicks timestamp) {}
 
    protected:
     virtual ~Delegate() = default;
@@ -87,6 +92,7 @@ class DataSource {
 
  private:
   void HandleFinishEvent(bool completed);
+  void HandleDropEvent();
   void HandleSendEvent(const std::string& mime_type, int32_t fd);
 
   // {T}_listener callbacks:
