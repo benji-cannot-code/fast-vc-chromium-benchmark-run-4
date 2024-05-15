9251216fd8b80713d03b246117466df211471f44
Hiroki Nakagawa <nhiroki@chromium.org>
2024-05-15 00:12:28 +0000

    NoStatePrefetch: Remove StartPrefetchingFromOmnibox()
    This is a follow-up for https://crrev.com/c/5490608. NoStatePrefetch
    trigger for Omnibox is no longer used in production.
    
    Several tests still use the Omnibox trigger, but actually they just want
    to run NoStatePrefetch. This CL replaces them with another trigger
    (NoStatePrefetchManager::AddSameOriginSpeculation()) that will not be
    removed soon.
    
    Bug: 335028973
    Change-Id: I5eee814fd1ab439c7833909ca2b09fd23f2cf454
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529420
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1300995}

--- components/no_state_prefetch/browser/no_state_prefetch_contents.cc
+++ components/no_state_prefetch/browser/no_state_prefetch_contents.cc
@@ -177,7 +177,6 @@ NoStatePrefetchContents::NoStatePrefetchContents(
       process_pid_(base::kNullProcessId),
       origin_(origin) {
   switch (origin) {
-    case ORIGIN_OMNIBOX:
     case ORIGIN_NAVIGATION_PREDICTOR:
       DCHECK(!initiator_origin_.has_value());
       break;
@@ -304,10 +303,7 @@ void NoStatePrefetchContents::StartPrerendering(
   load_url_params.referrer = referrer_;
   load_url_params.initiator_origin = initiator_origin_;
   load_url_params.transition_type = ui::PAGE_TRANSITION_LINK;
-  if (origin_ == ORIGIN_OMNIBOX) {
-    load_url_params.transition_type = ui::PageTransitionFromInt(
-        ui::PAGE_TRANSITION_TYPED | ui::PAGE_TRANSITION_FROM_ADDRESS_BAR);
-  } else if (origin_ == ORIGIN_NAVIGATION_PREDICTOR) {
+  if (origin_ == ORIGIN_NAVIGATION_PREDICTOR) {
     load_url_params.transition_type =
         ui::PageTransitionFromInt(ui::PAGE_TRANSITION_GENERATED);
   }
