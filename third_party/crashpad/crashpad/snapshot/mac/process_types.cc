c554a71379ef5e7b053d3ea6064b91b34c4db609
Arthur Wang <wuwang@google.com>
2024-05-15 20:42:06 +0000

    Update Crashpad to aef83033716bf46eae7bb371d38faf4482acdd05
    4f99326c2ed8 Replace std::unique_ptr with base::HeapArray in
                 process_memory.cc
    aef83033716b Fix format issue
    
    Change-Id: I17f1379a5cbd6f1256d53bc4a10b4b4ff10d0c7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542329
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Arthur Wang <wuwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301555}

--- third_party/crashpad/crashpad/snapshot/mac/process_types.cc
+++ third_party/crashpad/crashpad/snapshot/mac/process_types.cc
@@ -20,8 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <uuid/uuid.h>
 
 #include <iterator>
-#include <memory>
 
+#include "base/containers/heap_array.h"
 #include "snapshot/mac/process_types/internal.h"
 #include "util/process/process_memory_mac.h"
 
@@ -246,8 +246,9 @@ inline void Assign<UInt64Array4, UInt32Array4>(UInt64Array4* destination,
                                           mach_vm_address_t address,           \
                                           size_t count,                        \
                                           struct_name* generic) {              \
-    std::unique_ptr<T[]> specific(new T[count]);                               \
-    if (!T::ReadArrayInto(process_reader, address, count, &specific[0])) {     \
+    auto specific = base::HeapArray<T>::Uninit(count);                         \
+    if (!T::ReadArrayInto(                                                     \
+            process_reader, address, specific.size(), specific.data())) {      \
       return false;                                                            \
     }                                                                          \
     for (size_t index = 0; index < count; ++index) {                           \
