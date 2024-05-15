83ffa7fb76ed40d2ff75751af33f9d08dd2e23d5
Cranel W <cranelw@chromium.org>
2024-05-15 05:39:02 +0000

    cras_audio_client: support style transfer
    Changes:
    * support SetStyleTransferEnabled
    * support GetStyleTransferSupported
    
    Bug: b:327530996
    Test: unittest
    Change-Id: I5b3079026c6ef399659407a4e26494e5370a5e54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529193
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Cranel W <cranelw@chromium.org>
    Reviewed-by: Ching Yun Chang <whalechang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301111}

--- chromeos/ash/components/dbus/audio/fake_cras_audio_client.h
+++ chromeos/ash/components/dbus/audio/fake_cras_audio_client.h
@@ -34,6 +34,8 @@ class COMPONENT_EXPORT(DBUS_AUDIO) FakeCrasAudioClient
 
   void SetNoiseCancellationSupported(bool noise_cancellation_supported);
   uint32_t GetNoiseCancellationEnabledCount();
+  void SetStyleTransferSupported(bool style_transfer_supported);
+  bool GetStyleTransferEnabled();
   void SetHfpMicSrSupported(bool hfp_mic_sr_supported);
   uint32_t GetHfpMicSrEnabled();
 
@@ -68,6 +70,9 @@ class COMPONENT_EXPORT(DBUS_AUDIO) FakeCrasAudioClient
   void SetInputMute(bool mute_on) override;
   void SetNoiseCancellationEnabled(bool noise_cancellation_on) override;
   void GetNoiseCancellationSupported(
+      chromeos::DBusMethodCallback<bool> callback) override;
+  void SetStyleTransferEnabled(bool style_transfer_on) override;
+  void GetStyleTransferSupported(
       chromeos::DBusMethodCallback<bool> callback) override;
   void SetActiveOutputNode(uint64_t node_id) override;
   void SetActiveInputNode(uint64_t node_id) override;
@@ -172,6 +177,8 @@ class COMPONENT_EXPORT(DBUS_AUDIO) FakeCrasAudioClient
     return noise_cancellation_enabled_;
   }
 
+  bool style_transfer_enabled() const { return style_transfer_enabled_; }
+
   bool speak_on_mute_detection_enabled() const {
     return speak_on_mute_detection_enabled_;
   }
@@ -195,10 +202,12 @@ class COMPONENT_EXPORT(DBUS_AUDIO) FakeCrasAudioClient
   bool send_volume_change_events_synchronous_ = true;
   bool enable_gain_change_events_ = true;
   bool noise_cancellation_supported_ = false;
+  bool style_transfer_supported_ = false;
   uint32_t battery_level_ = 0;
   uint32_t noise_cancellation_enabled_counter_ = 0;
   int32_t number_non_chrome_output_streams_ = 0;
   bool noise_cancellation_enabled_ = false;
+  bool style_transfer_enabled_ = false;
   bool speak_on_mute_detection_enabled_ = false;
   bool force_respect_ui_gains_enabled_ = false;
   bool hfp_mic_sr_enabled_ = false;
