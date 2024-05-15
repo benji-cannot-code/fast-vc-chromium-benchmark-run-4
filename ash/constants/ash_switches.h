2b5f97a40ba23343709d23a949fb70ff40802015
Bohdan Tyshchenko <bohdanty@google.com>
2024-05-15 04:36:19 +0000

    [OOBE] - Introduce switch to skip new user check
    Add "--oobe-skip-new-user-check-for-testing" switch that disables new
    user check during the recommend apps screen. It will be used to
    simplify manual testing process.
    
    Bug: b/339789465
    Change-Id: Idbb21fcdb23c86920a9fe7a1de1d8d827705834f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537930
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301097}

--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -299,6 +299,8 @@ extern const char kOobePrintFrontendLoadTimings[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeScreenshotDirectory[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kOobeShowAccessibilityButtonOnMarketingOptInForTesting[];
+COMPONENT_EXPORT(ASH_CONSTANTS)
+extern const char kOobeSkipNewUserCheckForTesting[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeSkipPostLogin[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeSkipToLogin[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kOobeTimerInterval[];
@@ -407,6 +409,9 @@ COMPONENT_EXPORT(ASH_CONSTANTS) bool IsSigninFrameClientCertsEnabled();
 // testing.
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool ShouldTetherHostScansIgnoreWiredConnections();
+
+// Returns true if we should skip new user check on the recommend apps screen.
+COMPONENT_EXPORT(ASH_CONSTANTS) bool ShouldSkipNewUserCheckForTesting();
 
 // Returns true if we should skip all other OOBE pages after user login.
 COMPONENT_EXPORT(ASH_CONSTANTS) bool ShouldSkipOobePostLogin();
--- chrome/browser/ash/login/screens/personalized_recommend_apps_screen.cc
+++ chrome/browser/ash/login/screens/personalized_recommend_apps_screen.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/login/screens/personalized_recommend_apps_screen.h"
 
+#include "ash/constants/ash_switches.h"
 #include "chrome/browser/ash/arc/arc_util.h"
 #include "chrome/browser/ash/login/login_pref_names.h"
 #include "chrome/browser/ash/login/wizard_controller.h"
@@ -69,9 +70,16 @@ bool PersonalizedRecommendAppsScreen::MaybeSkip(WizardContext& context) {
     return true;
   }
 
-  // Only show screen for new ChromeOS user.
+  // This screen is currently shown only to the new-to-ChromeOS users. To
+  // understand which user is actually a new one we use a synced preference
+  // `kOobeMarketingOptInScreenFinished` which is set only once for each user
+  // in the end of the OOBE flow.
+  // This logic might be replaced in the future by checking a list of potential
+  // synced apps for the user, but at this moment this preference check is
+  // sufficient for our needs.
   PrefService* prefs = ProfileManager::GetActiveUserProfile()->GetPrefs();
-  if (prefs->GetBoolean(prefs::kOobeMarketingOptInScreenFinished)) {
+  if (prefs->GetBoolean(prefs::kOobeMarketingOptInScreenFinished) &&
+      !ash::switches::ShouldSkipNewUserCheckForTesting()) {
     return true;
   }
 
