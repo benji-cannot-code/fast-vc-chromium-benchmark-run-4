72d1a9fc695751d0fd2378be7403dcae5f905582
Jan Keitel <jkeitel@google.com>
2024-05-15 14:06:36 +0000

    Expose PWM form parsing to Autofill.
    Currently, FormDataParser is used inside //components/password_manager
    and //components/android_autofill. This CL makes its results available
    to //components/autofill via dependency injection through
    AutofillClient. The injection is necessary because //components/autofill
    should not depend on targets in //components/password_manager.
    
    The motivation for making password form classifications available inside
    Autofill is that Autofill suggestions are shown on password forms if
    Password Manager does not have suggestions of its own. However, we want
    to avoid offering plus address creation on all PW-related forms apart
    from signup forms and therefore need access to the Password Manager
    heuristics.
    
    Follow-up CLs will add iOS support and make use of the predictions
    inside BrowserAutofillManager.
    
    Bug: 323387899
    Change-Id: Ib4c3e79169752a373f684743d3acecd72fc2bca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537916
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301271}

---
+++ components/autofill/content/browser/autofill_test_utils.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CONTENT_BROWSER_AUTOFILL_TEST_UTILS_H_
+#define COMPONENTS_AUTOFILL_CONTENT_BROWSER_AUTOFILL_TEST_UTILS_H_
+
+#include <vector>
+
+namespace content {
+class RenderFrameHost;
+}  // namespace content
+
+namespace autofill {
+
+struct FormData;
+class FormFieldData;
+
+namespace test {
+
+// Creates a `FormData` whose `host_frame` and the host frames of its fields
+// correspond to `rfh`.
+FormData CreateFormDataForRenderFrameHost(content::RenderFrameHost& rfh,
+                                          std::vector<FormFieldData> fields);
+
+}  // namespace test
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CONTENT_BROWSER_AUTOFILL_TEST_UTILS_H_
