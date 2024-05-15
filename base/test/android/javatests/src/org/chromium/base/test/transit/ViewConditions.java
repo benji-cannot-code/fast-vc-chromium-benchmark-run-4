7e1acc177fa59d0c971d899bb3a1ca2501760552
Henrique Nakashima <hnakashima@chromium.org>
2024-05-15 23:03:43 +0000

    [Android] Check is any activities exist before onView()
    The onView().perform() failure due to no Activities is slow, takes
    400-500ms. Check if there are Activities before checking ViewConditions.
    
    Bug: 340893303
    Change-Id: Ia114d8c6530e7c43f02011ef927ef3b2e6cd9c4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542562
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301640}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ViewConditions.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ViewConditions.java
@@ -27,6 +27,7 @@ import androidx.test.platform.app.InstrumentationRegistry;
 import org.hamcrest.Matcher;
 import org.hamcrest.StringDescription;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.test.util.RawFailureHandler;
 
 import java.util.ArrayList;
@@ -100,6 +101,10 @@ public class ViewConditions {
 
         @Override
         protected ConditionStatus checkWithSuppliers() {
+            if (!ApplicationStatus.hasVisibleActivities()) {
+                return awaiting("No visible activities");
+            }
+
             ViewInteraction viewInteraction =
                     onView(mMatcher).withFailureHandler(RawFailureHandler.getInstance());
             String[] message = new String[1];
@@ -194,6 +199,10 @@ public class ViewConditions {
 
         @Override
         protected ConditionStatus checkWithSuppliers() {
+            if (!ApplicationStatus.hasVisibleActivities()) {
+                return fulfilled("No visible activities");
+            }
+
             try {
                 onView(mMatcher)
                         .withFailureHandler(RawFailureHandler.getInstance())
