0bc0f537595eb5e0559e8d4e86d1b96b8f8844b2
Danila Kuzmin <dkuzmin@google.com>
2024-05-15 20:06:21 +0000

    [oobe] Determine orientation using media queries in screens
    Instead of explicitly setting the orientation from the browser side
    we can determine the orientation using a media query. That simplifies
    the code and reduces additional dependencies for other WebUIs that
    reuse OOBE components and styles.
    
    Bug: b:336507936
    Change-Id: Ie88c652a3c3d3677cec009783dbb526b955783d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473625
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301521}

--- chrome/browser/resources/chromeos/login/screens/osauth/gaia_password_changed.ts
+++ chrome/browser/resources/chromeos/login/screens/osauth/gaia_password_changed.ts
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '//resources/ash/common/cr_elements/cros_color_overrides.css.js';
 import '//resources/polymer/v3_0/iron-icon/iron-icon.js';
-import '//resources/polymer/v3_0/iron-media-query/iron-media-query.js';
 import '../../components/oobe_icons.html.js';
 import '../../components/buttons/oobe_next_button.js';
 import '../../components/common_styles/oobe_common_styles.css.js';
