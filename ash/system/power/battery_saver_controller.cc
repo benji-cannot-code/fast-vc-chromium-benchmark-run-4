57dd9615fafaf75d9ec3af230aa895ded2e301f8
Charles William Dick <cwd@google.com>
2024-05-15 00:50:07 +0000

    CrOS Battery Saver metrics use TimeTicks
    Enabled duration metrics used base::Time, which includes time asleep.
    This CL switches to base::TimeTicks, which uses CLOCK_MONOTONIC, which
    does not include time alseep.
    
    Bug: b:339747852
    Test: ash_unittests, check chrome://histograms
    Change-Id: Ic39d9272fbe898ada7cc983737addfb1aadfd1d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529419
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Charles Dick <cwd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301015}

--- ash/system/power/battery_saver_controller.cc
+++ ash/system/power/battery_saver_controller.cc
@@ -238,7 +238,7 @@ void BatterySaverController::SetState(bool active, UpdateReason reason) {
   if (active && !enable_record_) {
     // An enable_record_ means that we were already active, so skip metrics if
     // it exists.
-    enable_record_ = EnableRecord{base::Time::Now(), reason};
+    enable_record_ = EnableRecord{base::TimeTicks::Now(), reason};
     base::UmaHistogramPercentage("Ash.BatterySaver.BatteryPercent.Enabled",
                                  static_cast<int>(battery_percent));
     if (time_to_empty) {
@@ -274,7 +274,7 @@ void BatterySaverController::SetState(bool active, UpdateReason reason) {
                                     *time_to_empty, base::Hours(0),
                                     base::Hours(10), 100);
     }
-    auto duration = base::Time::Now() - enable_record_->time;
+    auto duration = base::TimeTicks::Now() - enable_record_->time;
     base::UmaHistogramCustomTimes("Ash.BatterySaver.Duration", duration,
                                   base::Hours(0), base::Hours(10), 100);
     // Duration by enabled reason metrics
