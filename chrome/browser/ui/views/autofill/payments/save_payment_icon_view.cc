86449161908024db429347997daf093c3ff3d7d5
Jan Keitel <jkeitel@google.com>
2024-05-15 15:22:41 +0000

    Avoid incorrect downcast in SavePaymentIconView.
    SavePaymentIconView performs a downcast from AutofillBubbleBase to
    the different classes extending it. To check whether it is "safe" to
    perform the downcast, the class queries
    SavePaymentIconController::GetPaymentBubbleType. As a result, it is
    crucial that GetPaymentBubbleType always returns the type of bubble
    that the controller is currently shown.
    
    The recent addition of the
    SaevCardAndVirtualCardEnrollConfirmationBubbleViews violates this
    assumption.
    
    This CL fixes that by adding a new value to PaymentBubbleType and
    returning that when this new view is showing.
    
    Bug: 339305065
    Change-Id: If564926710a8d6523d884528cbcb864f1eb0b3dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541897
    Reviewed-by: Darwin Yang <darwinyang@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301310}

--- chrome/browser/ui/views/autofill/payments/save_payment_icon_view.cc
+++ chrome/browser/ui/views/autofill/payments/save_payment_icon_view.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/autofill/payments/save_payment_icon_view.h"
 
+#include "base/notreached.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/ui/autofill/payments/save_payment_icon_controller.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/autofill/payments/manage_saved_iban_bubble_view.h"
+#include "chrome/browser/ui/views/autofill/payments/save_card_and_virtual_card_enroll_confirmation_bubble_views.h"
 #include "chrome/browser/ui/views/autofill/payments/save_card_bubble_views.h"
 #include "chrome/browser/ui/views/autofill/payments/save_iban_bubble_view.h"
 #include "chrome/grit/generated_resources.h"
@@ -68,7 +70,13 @@ views::BubbleDialogDelegate* SavePaymentIconView::GetBubble() const {
     case SavePaymentIconController::PaymentBubbleType::kManageSavedIban:
       return static_cast<autofill::ManageSavedIbanBubbleView*>(
           controller->GetPaymentBubbleView());
+    case SavePaymentIconController::PaymentBubbleType::
+        kCreditCardSaveConfirmation:
+      return static_cast<
+          autofill::SaveCardAndVirtualCardEnrollConfirmationBubbleViews*>(
+          controller->GetPaymentBubbleView());
   }
+  NOTREACHED_NORETURN();
 }
 
 void SavePaymentIconView::UpdateImpl() {
