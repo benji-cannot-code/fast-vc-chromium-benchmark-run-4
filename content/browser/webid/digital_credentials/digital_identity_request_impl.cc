4f345d571cf057969a90fac11f7ff7c366eda38d
Peter Kotwicz <pkotwicz@chromium.org>
2024-05-15 19:19:48 +0000

    [RWI] Require user activation for real world identity API
    BUG=338261531
    
    Change-Id: If5967b8db446f717129384c53e0b741920d72b4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532484
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301496}

--- content/browser/webid/digital_credentials/digital_identity_request_impl.cc
+++ content/browser/webid/digital_credentials/digital_identity_request_impl.cc
@@ -179,6 +179,11 @@ void DigitalIdentityRequestImpl::Request(
 
   callback_ = std::move(callback);
 
+  if (!render_frame_host().HasTransientUserActivation()) {
+    CompleteRequest("", RequestStatusForMetrics::kErrorOther);
+    return;
+  }
+
   std::optional<std::string> request_json_string =
       digital_credential_provider->request;
   std::string request_to_send =
