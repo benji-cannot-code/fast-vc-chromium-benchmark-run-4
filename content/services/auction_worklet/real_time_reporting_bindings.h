8df88118e1d51e6c01e170b4575a15ffe73db032
Jose Dapena Paz <jdapena@igalia.com>
2024-05-15 13:59:56 +0000

    libstdc++: real time reporting bindings vector requires full declaration
    libstdc++ implementation of std::vector requires the full declaration
    of the type and not a forward declaration. This breaks compilation of
    RealTimeReportingBindings, as it has an std::vector of
    RealTimeReportingContributionPtr.
    
    Bug: 41455655
    Change-Id: I9ee70fbb58af36f4cfc43a5d4b1e99799f4c760c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541055
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1301266}

--- content/services/auction_worklet/real_time_reporting_bindings.h
+++ content/services/auction_worklet/real_time_reporting_bindings.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/services/auction_worklet/auction_v8_helper.h"
 #include "content/services/auction_worklet/context_recycler.h"
-#include "content/services/auction_worklet/public/mojom/real_time_reporting.mojom-forward.h"
+#include "content/services/auction_worklet/public/mojom/real_time_reporting.mojom.h"
 #include "v8/include/v8-forward.h"
 
 namespace auction_worklet {
