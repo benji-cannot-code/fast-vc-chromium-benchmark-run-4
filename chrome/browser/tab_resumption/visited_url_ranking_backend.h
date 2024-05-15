1524bbbe92ecfa13c7deca16842d822fb862bd51
Samuel Huang <huangs@chromium.org>
2024-05-15 19:15:00 +0000

    [Android TRM] Integrate with Fetch-and-Rank Service
    This CL integrates the Tab Resumption Module integration with Fetch-and-
    Rank Service AKA V2 results AKA components/visited_url_ranking. The
    feature is currently hidden behind flag #enable-tab-resumption-module
    set to "Enabled V2". Details:
    * ChromeBrowserMainExtraPartsProfiles::
      EnsureBrowserContextKeyedServiceFactoriesBuilt(): Add initialization
      call for VisitedURLRankingServiceFactory(), for Android only.
    * VisitedUrlRankingBackend:
      * Implement SuggestionBackend interfaces.
      * Implement C++ callbacks to notify update / refresh, make new
        SuggestionEntry instances, and to pass async results.
      * Inject Java object to C++ counterpart. This is needed to support
        non-static async callback onRefresh() -> triggerUpdate()
    * visited_url_ranking_backend.cc:
      * Add FetchAndRankFlow as a ref-counted, stateful workhorse to
       interface with Fetch-and-Rank Service.
        * Need to comment out call to Ranker since the feature is
          unimplemented and unstubbed.
      * VisitedUrlRankingBackend(): Implement main interfaces.
    
    hidden behind flag. We conducted manual tests for the feature.
    
    Bug: 337858147
    Low-Coverage-Reason: This CL is mostly glue code, and the feature is
    Change-Id: I42ac53395906dda9455a99656ef0e0ef82d0b6f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5507435
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301492}

--- chrome/browser/tab_resumption/visited_url_ranking_backend.h
+++ chrome/browser/tab_resumption/visited_url_ranking_backend.h
@@ -8,16 +8,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile.h"
+#include "components/visited_url_ranking/public/fetch_result.h"
+#include "components/visited_url_ranking/public/visited_url_ranking_service.h"
 
 namespace tab_resumption::jni {
 
-// Provides the fetch and rank services of the Tab resumption backend to
-// Java.
+// Provides the fetch and rank services of the Tab resumption backend to Java.
 class VisitedUrlRankingBackend {
  public:
-  explicit VisitedUrlRankingBackend(Profile* profile);
+  VisitedUrlRankingBackend(const jni_zero::JavaRef<jobject>& jobj,
+                           Profile* profile);
 
   VisitedUrlRankingBackend(const VisitedUrlRankingBackend&) = delete;
   VisitedUrlRankingBackend& operator=(const VisitedUrlRankingBackend&) = delete;
@@ -26,8 +30,29 @@ class VisitedUrlRankingBackend {
 
   void Destroy(JNIEnv* env);
 
+  // Trigger sync update, debounced.
+  void TriggerUpdate(JNIEnv* env);
+
+  // Computes ranked suggestions using synced data available on-device. Writes
+  // result to |suggestion| and calls |callback| on completion.
+  void GetRankedSuggestions(JNIEnv* env,
+                            jlong before_time_ms,
+                            const jni_zero::JavaParamRef<jobject>& suggestions,
+                            const jni_zero::JavaParamRef<jobject>& callback);
+
  private:
-  raw_ptr<Profile> profile_;
+  // Called when sync completes, to trigger refresh in Java.
+  void OnRefresh();
+
+ private:
+  jni_zero::ScopedJavaGlobalRef<jobject> jobj_;
+
+  raw_ptr<Profile> profile_;  // weak
+
+  base::CallbackListSubscription foreign_session_updated_subscription_;
+
+  // NOTE: Weak pointers must be invalidated before all other member variables.
+  base::WeakPtrFactory<VisitedUrlRankingBackend> weak_ptr_factory_{this};
 };
 
 }  // namespace tab_resumption::jni
