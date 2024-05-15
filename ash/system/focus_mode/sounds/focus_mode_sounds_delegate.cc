730726c05ae0101a5e8da79fd267c2615f03a836
Sean Kau <skau@chromium.org>
2024-05-15 19:05:40 +0000

    Introduce an interface for the focus mode sound backends
    Define stubs for the two backends and use them in the
    FocusModeSoundsController.
    
    Bug: b:331837970
    Change-Id: I863b7c219821965f13052b3c7664962d9c896746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532479
    Reviewed-by: Hongyu Long <hongyulong@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301484}

---
+++ ash/system/focus_mode/sounds/focus_mode_sounds_delegate.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/focus_mode/sounds/focus_mode_sounds_delegate.h"
+
+namespace ash {
+
+FocusModeSoundsDelegate::Playlist::Playlist(const std::string& id,
+                                            const std::string& title,
+                                            const GURL& thumbnail_url)
+    : id(id), title(title), thumbnail_url(thumbnail_url) {}
+FocusModeSoundsDelegate::Playlist::Playlist(const Playlist&) = default;
+FocusModeSoundsDelegate::Playlist& FocusModeSoundsDelegate::Playlist::operator=(
+    const Playlist&) = default;
+FocusModeSoundsDelegate::Playlist::~Playlist() = default;
+
+FocusModeSoundsDelegate::Track::Track(const std::string& title,
+                                      const std::string& artist,
+                                      const std::string& source,
+                                      const GURL& thumbnail_url,
+                                      const GURL& source_url)
+    : title(title),
+      artist(artist),
+      source(source),
+      thumbnail_url(thumbnail_url),
+      source_url(source_url) {}
+FocusModeSoundsDelegate::Track::Track(const Track&) = default;
+FocusModeSoundsDelegate::Track& FocusModeSoundsDelegate::Track::operator=(
+    const Track&) = default;
+FocusModeSoundsDelegate::Track::~Track() = default;
+
+}  // namespace ash
