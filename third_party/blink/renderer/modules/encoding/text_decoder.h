bf5dc00f63071e09920c592255017aac8d44b641
Jose Dapena Paz <jdapena@igalia.com>
2024-05-15 17:35:08 +0000

    IWYU: missing include for usage of std::optional in text_decoder.h
    Bug: 41455655
    Change-Id: I8902831e823760d455ca274bfe45b44be9e947f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541275
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301397}

--- third_party/blink/renderer/modules/encoding/text_decoder.h
+++ third_party/blink/renderer/modules/encoding/text_decoder.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ENCODING_TEXT_DECODER_H_
 
 #include <memory>
+#include <optional>
 
 #include "third_party/blink/renderer/bindings/core/v8/v8_typedefs.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_text_decode_options.h"
