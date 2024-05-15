32f01e126230ff31e2ba542409bd0d2b79f6aaf5
Allen Bauer <kylixrd@chromium.org>
2024-05-15 21:07:40 +0000

    Harden FocusManagerEventHandler against Widget destruction prior to its own destruction.
    When the Widget ownership is CLIENT_OWNS_WIDGET, it is possible for the NativeWidget's destruction to occur after the Widget's destruction has already completed. This had left a dangling raw-pointer in FocusManagerEventHandler. Changing to base::WeakPtr solves this issue.
    
    Change-Id: I006397345a8346dae8d69527d5b53e15ab47f17c
    Bug: 40232479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541703
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Auto-Submit: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301571}

--- ui/views/widget/focus_manager_event_handler.h
+++ ui/views/widget/focus_manager_event_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "ui/events/event_handler.h"
 
 namespace aura {
@@ -35,7 +36,7 @@ class FocusManagerEventHandler : public ui::EventHandler {
   std::string_view GetLogContext() const override;
 
  private:
-  raw_ptr<Widget> widget_;
+  base::WeakPtr<Widget> widget_;
 
   // |window_| is the event target that is associated with this class.
   raw_ptr<aura::Window> window_;
