ca17599829c2e5e5f16de0d4fa41bf96e3afca06
Seung Jae Lim <andysjlim@chromium.org>
2024-05-15 19:18:13 +0000

    Add report type to ClientPhishingRequest for CSD allowlist match.
    This will also override local model result when the sample ping is
    requested.
    
    Bug: 338200326
    Change-Id: I0f68cf77e95ce7ab5df3bc9befc4793dccee10f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529586
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301494}

--- chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
+++ chrome/browser/safe_browsing/client_side_detection_host_browsertest.cc
@@ -390,7 +390,8 @@ IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostPrerenderBrowserTest,
 
   // Bypass the pre-classification checks.
   csd_host->OnPhishingPreClassificationDone(
-      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true);
+      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true,
+      /*is_sample_ping=*/false);
 
   // A prerendered navigation committing should not cancel classification.
   // We simulate the commit of a prerendered navigation to avoid races
@@ -449,7 +450,8 @@ IN_PROC_BROWSER_TEST_F(ClientSideDetectionHostPrerenderBrowserTest,
 
   // Bypass the pre-classification checks.
   csd_host->OnPhishingPreClassificationDone(
-      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true);
+      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true,
+      /*is_sample_ping=*/false);
 
   run_loop.Run();
 
@@ -501,7 +503,8 @@ IN_PROC_BROWSER_TEST_F(
 
   // Bypass the pre-classification checks.
   csd_host->OnPhishingPreClassificationDone(
-      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true);
+      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true,
+      /*is_sample_ping=*/false);
 
   run_loop.Run();
 
@@ -573,7 +576,8 @@ IN_PROC_BROWSER_TEST_F(
 
   // Bypass the pre-classification checks.
   csd_host->OnPhishingPreClassificationDone(
-      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true);
+      ClientSideDetectionType::TRIGGER_MODELS, /*should_classify=*/true,
+      /*is_sample_ping=*/false);
 
   run_loop.Run();
 
@@ -743,7 +747,7 @@ IN_PROC_BROWSER_TEST_F(
   // "NO_CLASSIFY_PRIVATE_IP".
   csd_host->OnPhishingPreClassificationDone(
       ClientSideDetectionType::KEYBOARD_LOCK_REQUESTED,
-      /*should_classify=*/true);
+      /*should_classify=*/true, /*is_sample_ping=*/false);
 
   run_loop.Run();
 
@@ -815,7 +819,7 @@ IN_PROC_BROWSER_TEST_F(
   // "NO_CLASSIFY_PRIVATE_IP".
   csd_host->OnPhishingPreClassificationDone(
       ClientSideDetectionType::POINTER_LOCK_REQUESTED,
-      /*should_classify=*/true);
+      /*should_classify=*/true, /*is_sample_ping=*/false);
 
   run_loop.Run();
 
