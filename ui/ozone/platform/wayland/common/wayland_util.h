535eec2a8b9a10bb0182b3178f0253bbdaba038d
Thomas Lukaszewicz <tluk@chromium.org>
2024-05-15 19:58:21 +0000

    [ozone] Handle input release events during wayland window drags
    See problem 1 in go/ozone-dnd-async-init for details.
    
    When Chrome requests a drag session (for window or system dnd) it
    requests a drag session via the wl_data_device::start_drag request.
    
    Synchronous with this request Chrome will set up the supporting
    classes and structures for the drag session and proceeds under the
    assumption the drag has started.
    
    However after the start_drag request there may be existing events
    already dispatched by the server that are incongruent with a drag
    state (pointer released for e.g.).
    
    Processing such an event whilst in a dragging state does not make
    sense and can lead to crashes. We do not want to no-op event
    processing since Wayland is a stateful protocol and pointer / touch
    devices can start to drift out of sync with the server.
    
    This CL updates WaylandWindowDragController to cancel the drag if
    input release events are received before the server has acknowledged
    the drag, no-oping these events otherwise. This aligns the window
    drag controller with the data drag controller.
    
    Bug: 330274075
    Change-Id: I7ac8472ec1ca3de719a9a1d50d0af20a18cf4174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513723
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301519}

--- ui/ozone/platform/wayland/common/wayland_util.h
+++ ui/ozone/platform/wayland/common/wayland_util.h
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/files/scoped_file.h"
 #include "base/functional/callback.h"
+#include "base/functional/callback_forward.h"
 #include "base/time/time.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/events/platform_event.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_f.h"
 #include "ui/gfx/geometry/transform.h"
@@ -127,6 +129,15 @@ base::TimeTicks EventMillisecondsToTimeTicks(uint32_t milliseconds);
 // A scale less than 1 can cause borders to not be rendered properly. So this
 // ensures the scale is at least 1.
 float ClampScale(float scale);
+
+// Common event dispatch handler for wayland drag sessions. Returns true if the
+// platform event was handled and event propagation should stop.
+// `start_drag_ack_received` should be true if the server has acknowledged the
+// client's start_drag request. `cancel_drag_cb` may be run if the drag session
+// needs to be cancelled by the client.
+bool MaybeHandlePlatformEventForDrag(const ui::PlatformEvent& event,
+                                     bool start_drag_ack_received,
+                                     base::OnceClosure cancel_drag_cb);
 
 }  // namespace wl
 
