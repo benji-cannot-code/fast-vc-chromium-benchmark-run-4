9a8d1969554975e38a1b7c54c97ccf9ef74191f0
François Doray <fdoray@chromium.org>
2024-05-15 21:57:28 +0000

    [ipc] Add feature to report possible skipped message in ChannelAssociatedGroupController.
    Enabling the MojoChannelAssociatedSendUsesRunOrPostTask feature
    resulted in crash reports indicating that messages were not received
    in the expected order (could be skipped or reordered). This CL adds
    a feature to crash when sending a message fails but more messages could
    potentially be sent, in order to better understand these crash reports.
    
    Bug: 1503967
    Change-Id: I89d49f3b36269359f01af5861154619665706991
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5521936
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301609}

--- ipc/ipc_mojo_bootstrap.cc
+++ ipc/ipc_mojo_bootstrap.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/typed_macros.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/urgent_message_observer.h"
+#include "mojo/public/c/system/types.h"
 #include "mojo/public/cpp/bindings/associated_group.h"
 #include "mojo/public/cpp/bindings/associated_group_controller.h"
 #include "mojo/public/cpp/bindings/connector.h"
@@ -52,6 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pipe_control_message_handler.h"
 #include "mojo/public/cpp/bindings/pipe_control_message_handler_delegate.h"
 #include "mojo/public/cpp/bindings/pipe_control_message_proxy.h"
+#include "mojo/public/cpp/bindings/scoped_message_error_crash_key.h"
 #include "mojo/public/cpp/bindings/sequence_local_sync_event_watcher.h"
 #include "mojo/public/cpp/bindings/tracing_helpers.h"
 #include "third_party/abseil-cpp/absl/base/attributes.h"
@@ -66,6 +68,10 @@ ABSL_CONST_INIT thread_local bool off_sequence_binding_allowed = false;
 
 BASE_FEATURE(kMojoChannelAssociatedSendUsesRunOrPostTask,
              "MojoChannelAssociatedSendUsesRunOrPostTask",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kMojoChannelAssociatedCrashesOnSendError,
+             "MojoChannelAssociatedCrashesOnSendError",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 // Used to track some internal Channel state in pursuit of message leaks.
@@ -919,7 +925,24 @@ class ChannelAssociatedGroupController
       }
       return true;
     }
-    return connector_->Accept(message);
+    MojoResult result = connector_->AcceptAndGetResult(message);
+
+    // TODO(crbug.com/40944462): Remove this code when the cause of skipped
+    // messages with MojoChannelAssociatedSendUsesRunOrPostTask is understood,
+    // or no later than November 2024.
+    if (result != MOJO_RESULT_OK && !connector_->encountered_error() &&
+        base::FeatureList::IsEnabled(
+            kMojoChannelAssociatedCrashesOnSendError)) {
+      // Crash when sending a message fails and `connector_` can send more
+      // messages, as that breaks the assumption that messages are received in
+      // the order they were sent. Note: `connector_` cannot send more messages
+      // when `encountered_error()` is true.
+      mojo::debug::ScopedMessageErrorCrashKey crash_key(
+          base::StringPrintf("SendMessage failed with error %d", result));
+      CHECK(false);
+    }
+
+    return result == MOJO_RESULT_OK;
   }
 
   void SendMessageOnSequenceViaTask(mojo::Message message) {
--- mojo/public/cpp/bindings/lib/connector.cc
+++ mojo/public/cpp/bindings/lib/connector.cc
@@ -315,17 +315,22 @@ bool Connector::PrefersSerializedMessages() {
 }
 
 bool Connector::Accept(Message* message) {
+  MojoResult result = AcceptAndGetResult(message);
+  return result == MOJO_RESULT_OK;
+}
+
+MojoResult Connector::AcceptAndGetResult(Message* message) {
   if (!lock_ && task_runner_)
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   if (TS_UNCHECKED_READ(error_)) {
-    return false;
+    return MOJO_RESULT_UNKNOWN;
   }
 
   internal::MayAutoLock locker(&lock_);
 
   if (!message_pipe_.is_valid() || drop_writes_)
-    return true;
+    return MOJO_RESULT_OK;
 
 #if defined(ENABLE_IPC_FUZZER)
   if (message_dumper_ && message->is_serialized()) {
@@ -360,6 +365,7 @@ bool Connector::Accept(Message* message) {
       // from the caller since we'd like them to continue consuming any backlog
       // of incoming messages before regarding the message pipe as closed.
       drop_writes_ = true;
+      rv = MOJO_RESULT_OK;
       break;
     case MOJO_RESULT_BUSY:
       // We'd get a "busy" result if one of the message's handles is:
@@ -373,13 +379,13 @@ bool Connector::Accept(Message* message) {
       // crbug.com/389666, etc. are resolved, this will make tests fail quickly
       // rather than hanging.)
       CHECK(false) << "Race condition or other bug detected";
-      return false;
+      break;
     default:
       // This particular write was rejected, presumably because of bad input.
       // The pipe is not necessarily in a bad state.
-      return false;
+      break;
   }
-  return true;
+  return rv;
 }
 
 void Connector::AllowWokenUpBySyncWatchOnSameThread() {
