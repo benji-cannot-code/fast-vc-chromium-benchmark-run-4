830b52a33f0925635771c5b82bea0014581194db
Christoph Schwering <schwering@google.com>
2024-05-15 15:41:41 +0000

    [Autofill] Remove unused member, fix comments/naming in AED
    This CL does two unrelated cleanup tasks:
    
    AutofillExternalDelegate::element_bounds_ is unused. The CL removes it.
    
    AutofillExternalDelegate::shortcut_test_suggestion_index_ is not a
    Boolean. The CL fixes the comment.
    
    AutofillExternalDelegate::ScopedAutofillPopupShortcutForTesting is
    not specific to the popup. The CL renames it.
    
    Bug: 40100455
    Change-Id: I68b05dfc02f19261cad5692be620f3e9348923ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541898
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301326}

--- components/autofill/core/browser/autofill_external_delegate.h
+++ components/autofill/core/browser/autofill_external_delegate.h
@@ -38,7 +38,7 @@ enum class CreditCardFetchResult;
 class AutofillExternalDelegate : public AutofillSuggestionDelegate,
                                  public AddressDataManager::Observer {
  public:
-  class ScopedAutofillPopupShortcutForTesting;
+  class ScopedSuggestionSelectionShortcut;
 
   // Creates an AutofillExternalDelegate for the specified
   // BrowserAutofillManager and AutofillDriver.
@@ -246,9 +246,9 @@ class AutofillExternalDelegate : public AutofillSuggestionDelegate,
   // delete address profile dialog is closed.
   AutofillSuggestionTriggerSource GetReopenTriggerSource() const;
 
-  // If true, OnSuggestionsReturned() passes one of the suggestions directly to
-  // DidAcceptSuggestion(). See ScopedAutofillPopupShortcutForTesting for
-  // details.
+  // If non-negative, OnSuggestionsReturned() passes one of the suggestions
+  // directly to DidAcceptSuggestion(). See ScopedSuggestionSelectionShortcut
+  // for details.
   static int shortcut_test_suggestion_index_;
 
   const raw_ref<BrowserAutofillManager> manager_;
@@ -256,8 +256,6 @@ class AutofillExternalDelegate : public AutofillSuggestionDelegate,
   // The current form and field selected by Autofill.
   FormData query_form_;
   FormFieldData query_field_;
-  // The bounds of the form field that the user is interacting with.
-  gfx::RectF element_bounds_;
   // The method how suggestions were triggered on the current form.
   AutofillSuggestionTriggerSource trigger_source_;
 
@@ -297,20 +295,20 @@ class AutofillExternalDelegate : public AutofillSuggestionDelegate,
 // narrower scope around, for example, AutofillDriver::AskForValuesToFill(),
 // but beware of potential asynchronicity (e.g., due to asynchronous parsing or
 // asynchronous fetching of suggestions).
-class AutofillExternalDelegate::ScopedAutofillPopupShortcutForTesting {
+class AutofillExternalDelegate::ScopedSuggestionSelectionShortcut {
  public:
-  explicit ScopedAutofillPopupShortcutForTesting(int index = 0) {
+  explicit ScopedSuggestionSelectionShortcut(int index = 0) {
     DCHECK(index >= 0);
     DCHECK(shortcut_test_suggestion_index_ < 0);
     shortcut_test_suggestion_index_ = index;
   }
 
-  ScopedAutofillPopupShortcutForTesting(
-      const ScopedAutofillPopupShortcutForTesting&) = delete;
-  ScopedAutofillPopupShortcutForTesting& operator=(
-      const ScopedAutofillPopupShortcutForTesting&) = delete;
+  ScopedSuggestionSelectionShortcut(const ScopedSuggestionSelectionShortcut&) =
+      delete;
+  ScopedSuggestionSelectionShortcut& operator=(
+      const ScopedSuggestionSelectionShortcut&) = delete;
 
-  ~ScopedAutofillPopupShortcutForTesting() {
+  ~ScopedSuggestionSelectionShortcut() {
     DCHECK(shortcut_test_suggestion_index_ >= 0);
     shortcut_test_suggestion_index_ = -1;
   }
