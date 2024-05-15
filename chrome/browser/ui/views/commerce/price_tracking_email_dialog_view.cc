37736fa15dfeb42681fbce5160c0d5037068ec41
dljames <dljames@chromium.org>
2024-05-15 23:40:16 +0000

    [Commerce] Use ChromeRefresh2023 logic by default
    Files in chrome/browser/ui/views/commerce/ now defaults to using
    the ChromeRefresh2023 logic.
    
    #chrome-refresh-2023-cleanup
    
    Change-Id: Ia2716c613dcd39223cfc4739ef7541d8dfbddb8c
    Bug: 335848375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542739
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301660}

--- chrome/browser/ui/views/commerce/price_tracking_email_dialog_view.cc
+++ chrome/browser/ui/views/commerce/price_tracking_email_dialog_view.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/referrer.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/views/controls/styled_label.h"
 #include "ui/views/layout/flex_layout_view.h"
@@ -85,9 +84,7 @@ PriceTrackingEmailDialogView::PriceTrackingEmailDialogView(
                  l10n_util::GetStringUTF16(IDS_PRICE_TRACKING_YES_IM_IN));
   SetButtonLabel(ui::DIALOG_BUTTON_CANCEL,
                  l10n_util::GetStringUTF16(IDS_PRICE_TRACKING_NOT_NOW));
-  SetButtonStyle(ui::DIALOG_BUTTON_CANCEL, features::IsChromeRefresh2023()
-                                               ? ui::ButtonStyle::kTonal
-                                               : ui::ButtonStyle::kDefault);
+  SetButtonStyle(ui::DIALOG_BUTTON_CANCEL, ui::ButtonStyle::kTonal);
   SetAcceptCallback(base::BindOnce(&PriceTrackingEmailDialogView::OnAccepted,
                                    weak_factory_.GetWeakPtr()));
   SetCancelCallback(base::BindOnce(&PriceTrackingEmailDialogView::OnCanceled,
