5b21855124ca6e15c6ca834c1449ba2d35f3ba66
Bo Majewski <majewski@chromium.org>
2024-05-15 00:36:18 +0000

    [Files]: Moves files to reflect current names.
    Moves term_table to token_table, as it defines the TokenTable class.
    Move augmented_term_table to term_table, as it defines TermTable class.
    Adjust the include statements and BUILD file.
    
    Bug: b:327534824
    Change-Id: I98cdee38b35141232954049d07c680c1d654cc67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526560
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301004}

--- chrome/browser/ash/file_manager/indexing/token_table_unittest.cc
+++ chrome/browser/ash/file_manager/indexing/token_table_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/file_manager/indexing/term_table.h"
+#include "chrome/browser/ash/file_manager/indexing/token_table.h"
 
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
