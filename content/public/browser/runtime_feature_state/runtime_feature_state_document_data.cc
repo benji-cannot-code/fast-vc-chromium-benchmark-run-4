42c9d388ed3a7ca075393de3e91e1eba90833cf2
Ari Chivukula <arichiv@chromium.org>
2024-05-15 23:42:17 +0000

    [BREFs] Read 1P origin when fetching read-only context
    There's no reason to risk the caller using the wrong 1p origin when
    checking 3p tokens. Let's just read the origin from the render frame
    host directly when the data is fetched. This should reduce the chance of
    unexpected errors being introduced in future.
    
    Bug: 40254534
    Change-Id: Ie37fa1f32b7915eed90c87ad704598a133878c87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540369
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301662}

--- content/public/browser/runtime_feature_state/runtime_feature_state_document_data.cc
+++ content/public/browser/runtime_feature_state/runtime_feature_state_document_data.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/runtime_feature_state/runtime_feature_state_document_data.h"
 
+#include "content/public/browser/render_frame_host.h"
+
 namespace content {
 
 DOCUMENT_USER_DATA_KEY_IMPL(RuntimeFeatureStateDocumentData);
@@ -14,7 +16,9 @@ RuntimeFeatureStateDocumentData::~RuntimeFeatureStateDocumentData() = default;
 RuntimeFeatureStateDocumentData::RuntimeFeatureStateDocumentData(
     RenderFrameHost* rfh,
     const blink::RuntimeFeatureStateReadContext& read_context)
-    : DocumentUserData(rfh),
-      runtime_feature_state_read_context_(read_context) {}
+    : DocumentUserData(rfh), runtime_feature_state_read_context_(read_context) {
+  runtime_feature_state_read_context_.set_origin_on_navigation(
+      origin(), base::PassKey<RuntimeFeatureStateDocumentData>());
+}
 
 }  // namespace content
