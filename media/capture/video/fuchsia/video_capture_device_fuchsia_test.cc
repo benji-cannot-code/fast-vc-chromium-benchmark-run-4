a5544b3d59f58fa9e13b6727c8aed30a58237077
Dustin Green <dustingreen@google.com>
2024-05-15 23:32:18 +0000

    [fuchsia][sysmem2] route sysmem2.Allocator
    Route fuchsia.sysmem2.Allocator everywhere fuchsia.sysmem.Allocator
    is routed.
    
    BUG: b/306258175
    Change-Id: Ie01e4688edbb0a1fa2b3168c229b2f41b2edfd47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528805
    Commit-Queue: Dustin Green <dustingreen@google.com>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301654}

--- media/capture/video/fuchsia/video_capture_device_fuchsia_test.cc
+++ media/capture/video/fuchsia/video_capture_device_fuchsia_test.cc
@@ -204,6 +204,7 @@ class VideoCaptureDeviceFuchsiaTest : public testing::Test {
  public:
   VideoCaptureDeviceFuchsiaTest() {
     test_context_.AddService("fuchsia.sysmem.Allocator");
+    test_context_.AddService("fuchsia.sysmem2.Allocator");
   }
 
   ~VideoCaptureDeviceFuchsiaTest() override {
--- sandbox/policy/fuchsia/sandbox_policy_fuchsia.cc
+++ sandbox/policy/fuchsia/sandbox_policy_fuchsia.cc
@@ -103,6 +103,9 @@ constexpr SandboxConfig kGpuConfig = {
         fuchsia::media::ProfileProvider::Name_,
         fuchsia::mediacodec::CodecFactory::Name_,
         fuchsia::sysmem::Allocator::Name_,
+
+        // Switch from string to fuchsia::sysmem2::Allocator::Name_ when SDK 19.
+        "fuchsia.sysmem2.Allocator",
         "fuchsia.vulkan.loader.Loader",
         fuchsia::tracing::provider::Registry::Name_,
         fuchsia::ui::composition::Allocator::Name_,
@@ -129,6 +132,9 @@ constexpr SandboxConfig kRendererConfig = {
         fuchsia::media::ProfileProvider::Name_,
         fuchsia::memorypressure::Provider::Name_,
         fuchsia::sysmem::Allocator::Name_,
+
+        // Switch from string to fuchsia::sysmem2::Allocator::Name_ when SDK 19.
+        "fuchsia.sysmem2.Allocator",
         fuchsia::ui::composition::Allocator::Name_,
     }),
     0,
@@ -138,6 +144,9 @@ constexpr SandboxConfig kVideoCaptureConfig = {
     base::make_span((const char* const[]){
         fuchsia::camera3::DeviceWatcher::Name_,
         fuchsia::sysmem::Allocator::Name_,
+
+        // Switch from string to fuchsia::sysmem2::Allocator::Name_ when SDK 19.
+        "fuchsia.sysmem2.Allocator",
     }),
     0,
 };
