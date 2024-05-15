d52a9947a7f9305936c90ebe1f07616d9e3e6fc6
Daniel Cheng <dcheng@chromium.org>
2024-05-15 10:47:01 +0000

    Use absl::Cleanup to record metrics in SupervisedUserURLFilter
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: I29760585b4b01f3213ba8eabf0d09b3938a106d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536273
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301199}

--- components/supervised_user/core/browser/supervised_user_url_filter.cc
+++ components/supervised_user/core/browser/supervised_user_url_filter.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/url_matcher/url_util.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "url/gurl.h"
 #include "url/url_constants.h"
 
@@ -522,9 +523,8 @@ SupervisedUserURLFilter::GetManualFilteringBehaviorForURL(const GURL& url) {
   std::optional<FilteringSubdomainConflictType> conflict_type = std::nullopt;
 
   // Records the conflict metrics when the current scope exits.
-  base::ScopedClosureRunner histogram_recorder(base::BindOnce(
-      [](const FilteringBehavior& result,
-         const std::optional<FilteringSubdomainConflictType>& conflict_type) {
+  absl::Cleanup histogram_recorder =
+      [&result, &conflict_type] {
         if (result != FilteringBehavior::kInvalid) {
           // Record the potential conflict and its type.
           bool conflict = conflict_type.has_value();
@@ -536,8 +536,7 @@ SupervisedUserURLFilter::GetManualFilteringBehaviorForURL(const GURL& url) {
                 conflict_type.value());
           }
         }
-      },
-      std::cref(result), std::cref(conflict_type)));
+      };
 
   // Check manual overrides for the exact URL.
   auto url_it = url_map_.find(url_matcher::util::Normalize(url));
