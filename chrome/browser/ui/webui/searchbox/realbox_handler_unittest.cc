109f36e432a5197a7e0415be31b1b093bd191727
Justin Donnelly <jdonnelly@chromium.org>
2024-05-15 19:53:04 +0000

    [lensoverlay] Synchronize searchbox state with overlay.
    When the thumbnail or text associated with an overlay selection is removed or modified in the searchbox, clears the selection in the overlay.
    
    Additionally:
    - Returns the correct page classification from `LensOverlayController::GetPageClassification()`, based on whether there is any current image selection or not. This produces the correct type of suggestions after clearing the thumbnail.
    - Clears matches from the searchbox when the thumbnail is removed since they are associated with the thumbnail and no longer valid.
    
    Bug: b:337297776
    Change-Id: I424aa951b250caa67311cc1ee11c859e880781d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5533600
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301515}

--- chrome/browser/ui/webui/searchbox/realbox_handler_unittest.cc
+++ chrome/browser/ui/webui/searchbox/realbox_handler_unittest.cc
@@ -101,6 +101,7 @@ class MockLensSearchboxClient : public LensSearchboxClient {
               GetLensResponse,
               (),
               (override, const));
+  MOCK_METHOD(void, OnTextModified, (), (override));
   MOCK_METHOD(void, OnThumbnailRemoved, (), (override));
   MOCK_METHOD(void,
               OnSuggestionAccepted,
--- ui/webui/resources/cr_components/searchbox/realbox.ts
+++ ui/webui/resources/cr_components/searchbox/realbox.ts
@@ -598,6 +598,7 @@ export class RealboxElement extends RealboxElementBase {
           // Remove thumbnail, focus input, and notify browser.
           this.thumbnailUrl_ = '';
           this.$.input.focus();
+          this.clearAutocompleteMatches_();
           this.pageHandler_.onThumbnailRemoved();
           e.preventDefault();
         } else if (e.key === 'Tab') {
@@ -762,6 +763,7 @@ export class RealboxElement extends RealboxElementBase {
     /* Remove thumbnail, focus input, and notify browser. */
     this.thumbnailUrl_ = '';
     this.$.input.focus();
+    this.clearAutocompleteMatches_();
     this.pageHandler_.onThumbnailRemoved();
   }
 
