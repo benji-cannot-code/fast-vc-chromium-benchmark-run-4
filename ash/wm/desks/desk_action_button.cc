61372d208ee6d554e1f42cc5ca0089ba253c460a
Sammie Quon <sammiequon@chromium.org>
2024-05-15 20:12:44 +0000

    desks: Use lambdas to improve readability
    Use early returns inside a lambda for large operations. It increases
    the code but improves readability and scalibity. Also adds some setup
    work for new focus on desk bar elements.
    
    Test: manual
    Fixed: b:326124631
    Bug: b:325335020
    Change-Id: Iceb6f8e959fcf5a8deae76bfbd892418a6113c3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540728
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301527}

--- ash/wm/desks/desk_action_button.cc
+++ ash/wm/desks/desk_action_button.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/desks/desk_action_button.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/style/color_provider.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -47,7 +48,8 @@ DeskActionButton::DeskActionButton(const std::u16string& tooltip,
       std::make_unique<views::CircleHighlightPathGenerator>(
           -gfx::Insets(focus_ring->GetHaloThickness() / 2)));
   if (desk_action_view_->mini_view()->owner_bar()->type() ==
-      DeskBarViewBase::Type::kOverview) {
+          DeskBarViewBase::Type::kOverview &&
+      !features::IsOverviewNewFocusEnabled()) {
     focus_ring->SetHasFocusPredicate(
         base::BindRepeating([](const views::View* view) {
           const auto* v = views::AsViewClass<DeskActionButton>(view);
--- ash/wm/desks/desk_button_base.cc
+++ ash/wm/desks/desk_button_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/desks/desk_button_base.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/wm/desks/desk_bar_view_base.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "ash/wm/wm_constants.h"
@@ -54,7 +55,8 @@ DeskButtonBase::DeskButtonBase(const std::u16string& text,
   views::FocusRing* focus_ring = views::FocusRing::Get(this);
   focus_ring->SetOutsetFocusRingDisabled(true);
   focus_ring->SetColorId(ui::kColorAshFocusRing);
-  if (bar_view_->type() == DeskBarViewBase::Type::kOverview) {
+  if (bar_view_->type() == DeskBarViewBase::Type::kOverview &&
+      !features::IsOverviewNewFocusEnabled()) {
     focus_ring->SetHasFocusPredicate(
         base::BindRepeating([](const views::View* view) {
           const auto* v = views::AsViewClass<DeskButtonBase>(view);
