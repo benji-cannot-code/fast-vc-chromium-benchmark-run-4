1fcbb860908b872eb2f9d351a44b4c5b897deeaf
Gabriel Oliveira <gabolvr@google.com>
2024-05-15 14:34:00 +0000

    [Uno-D] Create IPH for remembering explicit browser signin preference
    Screenshot:
    https://screenshot.googleplex.com/7SF8KPbrs7Ciant.png
    
    Preview: https://screencast.googleplex.com/cast/NTk3Mjg2ODM1NjYzNjY3Mnw0NGNjMjliYi01MQ
    
    PRD: https://docs.google.com/document/d/1Wq6j0fLSBuHGqHQXo9pKQaghm7RB_2D1LSRrg8W3GqE/edit?resourcekey=0-radQT_JsSI8E30Jt-ld6xw&tab=t.0#heading=h.bq1lynm1th5r
    
    Bug: 340805499, b/319395873
    Change-Id: I068ba3055d68eeae5c0990039edbf4e752d0ba52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5510843
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Gabriel Oliveira <gabolvr@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301287}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -163,6 +163,7 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHDiscardRingFeature,
     &kIPHDownloadEsbPromoFeature,
     &kIPHExperimentalAIPromoFeature,
+    &kIPHExplicitBrowserSigninPreferenceRememberedFeature,
 #if BUILDFLAG(ENABLE_EXTENSIONS)
     &kIPHExtensionsMenuFeature,
     &kIPHExtensionsRequestAccessButtonFeature,
