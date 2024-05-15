4f314dfbc3f75c077e6b8dc42868b262c319e674
Eriko Kurimoto <elkurin@chromium.org>
2024-05-15 00:36:54 +0000

    gardening: Fix SmartDimComponentIntegrationTest.SmartDim
    This CL fixes SmartDimComponentIntegrationTest.SmartDim failure due to
    base::File::~File() call from FirmwareUpdateManager::GetFileDescriptor
    on main thread by moving base::File call inside the task to post.
    
    It also corrects the invalid sequence access in RefreshRemoteAllowed.
    RefreshRemoteAllowed is called on the thread pool, so should bind the
    value referring the singleton at the original thread beforehand.
    
    Bug: b:340146987
    Change-Id: Ic9c804b7ce649edae2d04503088649aaa09f040d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535076
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301005}

--- chromeos/ash/components/fwupd/firmware_update_manager.h
+++ chromeos/ash/components/fwupd/firmware_update_manager.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/files/file.h"
 #include "base/files/file_path.h"
-#include "base/files/scoped_file.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -67,7 +66,7 @@ enum class MethodResult {
   // DEPRECATED: kInstallFailed = 1,
   kFailedToCreateUpdateDirectory = 2,
   // DEPRECATED: kInvalidDestinationFile = 3,
-  kInvalidFileDescriptor = 4,
+  kInvalidFile = 4,
   kFailedToDownloadToFile = 5,
   kFailedToCreatePatchFile = 6,
   kEmptyPatchFile = 7,
@@ -218,11 +217,11 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_FWUPD) FirmwareUpdateManager
                     const base::FilePath& filepath,
                     MethodCallback callback);
 
-  // Callback handler after fetching the file descriptor.
-  void OnGetFileDescriptor(const std::string& device_id,
-                           FirmwareInstallOptions options,
-                           MethodCallback callback,
-                           base::ScopedFD file_descriptor);
+  // Callback handler after fetching the file.
+  void OnGetFile(const std::string& device_id,
+                 FirmwareInstallOptions options,
+                 MethodCallback callback,
+                 base::File file);
 
   // Query the fwupd DBus client to install an update for a certain device.
   void InstallUpdate(const std::string& device_id,
