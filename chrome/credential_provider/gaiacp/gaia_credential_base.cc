fd0132b629aaee210d02fb818fa8f827b5ca9118
Daniel Cheng <dcheng@chromium.org>
2024-05-15 17:36:15 +0000

    Use absl::Cleanup to zero buffers in //chrome/credential_provider
    absl::Cleanup is a helper to run arbitrary code when the absl::Cleanup
    object goes out of scope, which is useful for executing cleanup code or
    ensuring something always runs. The current //base equivalent is
    base::ScopedClosureRunner, which executes a base::OnceClosure when the
    runner object goes out of scope.
    
    Compared to base::ScopedClosureRunner, there are several benefits to
    using absl::Cleanup:
    - works with capturing lambdas, which are often much more concise than
      base::BindOnce()
    - requires no heap allocations
    - less impact on binary size since absl::Cleanup instantiates fewer
      templates
    
    This CL is part of a project-wide cleanup to migrate to absl::Cleanup
    where appropriate. The general criteria for migrating usages of
    base::ScopedClosureRunner:
    - The cleanup scoper must not escape block scope, e.g. it is not
      returned from the function, passed to another function, or bound into
      a callback.
    - The cleanup scoper's type does not need to be named, e.g. the scoper
      construction can use CTAD:
        absl::Cleanup run_at_exit = [] { RestoreSettings(original); };
      Note: having to write absl::Cleanup<decltype(lambda)> as a type is
      often a sign that absl::Cleanup is not a good fit for how the code is
      currently structured.
    - The cleanup scoper is not simply running a base::OnceClosure.
    
    Bug: 339492604
    Change-Id: I35f143dff21b6525aed40be32cb5c4c3bf176da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5530307
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Tien Mai <tienmai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301399}

--- chrome/credential_provider/gaiacp/gaia_credential_base.cc
+++ chrome/credential_provider/gaiacp/gaia_credential_base.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
-#include "base/functional/callback_helpers.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/path_service.h"
@@ -69,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_switches.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "third_party/abseil-cpp/absl/cleanup/cleanup.h"
 #include "third_party/re2/src/re2/re2.h"
 
 namespace credential_provider {
@@ -629,15 +629,15 @@ HRESULT WaitForLoginUIAndGetResult(
   DCHECK(status_text);
 
   // Buffer used to accumulate output from UI.
-  const int kBufferSize = 4096;
-  std::vector<char> output_buffer(kBufferSize, '\0');
-  base::ScopedClosureRunner zero_buffer_on_exit(
-      base::BindOnce(base::IgnoreResult(&SecurelyClearBuffer),
-                     &output_buffer[0], kBufferSize));
+  constexpr int kBufferSize = 4096;
+  std::array<char, kBufferSize> output_buffer = {};
+  absl::Cleanup zero_buffer_on_exit = [&output_buffer] {
+    SecurelyClearBuffer(output_buffer.data(), output_buffer.size());
+  };
 
   HRESULT hr = WaitForProcess(uiprocinfo->procinfo.process_handle(),
                               uiprocinfo->parent_handles, exit_code,
-                              &output_buffer[0], kBufferSize);
+                              output_buffer.data(), output_buffer.size());
   // output_buffer contains sensitive information like the password. Don't log
   // it.
   LOGFN(VERBOSE) << "exit_code=" << *exit_code;
@@ -654,7 +654,7 @@ HRESULT WaitForLoginUIAndGetResult(
     return E_FAIL;
   }
 
-  *json_result = std::string(&output_buffer[0]);
+  *json_result = std::string(output_buffer.data());
   return S_OK;
 }
 
