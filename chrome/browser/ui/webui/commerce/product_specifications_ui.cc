6c88e179d8d9e9ed9c3e0defb74ebfecec322ff3
Emily Shack <emshack@chromium.org>
2024-05-15 20:38:36 +0000

    Add additional product specification menu items
    Bug: 340336762
    Change-Id: Id43a4f8d43bcd72241df77f9257dc524a56f8a8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536225
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301554}

--- chrome/browser/ui/webui/commerce/product_specifications_ui.cc
+++ chrome/browser/ui/webui/commerce/product_specifications_ui.cc
@@ -58,6 +58,8 @@ ProductSpecificationsUI::ProductSpecificationsUI(content::WebUI* web_ui)
       IDR_COMMERCE_PRODUCT_SPECIFICATIONS_PRODUCT_SPECIFICATIONS_HTML);
 
   static constexpr webui::LocalizedString kLocalizedStrings[] = {
+      {"addToNewGroup", IDS_PRODUCT_SPECIFICATIONS_ADD_TO_NEW_GROUP},
+      {"delete", IDS_PRODUCT_SPECIFICATIONS_DELETE},
       {"emptyMenu", IDS_PRODUCT_SPECIFICATIONS_EMPTY_SELECTION_MENU},
       {"emptyProductSelector",
        IDS_PRODUCT_SPECIFICATIONS_EMPTY_PRODUCT_SELECTOR},
@@ -68,6 +70,7 @@ ProductSpecificationsUI::ProductSpecificationsUI(content::WebUI* web_ui)
       {"recentlyViewedTabs",
        IDS_PRODUCT_SPECIFICATIONS_RECENTLY_VIEWED_TABS_SECTION},
       {"renameGroup", IDS_PRODUCT_SPECIFICATIONS_RENAME_GROUP},
+      {"seeAll", IDS_PRODUCT_SPECIFICATIONS_SEE_ALL},
   };
   source->AddLocalizedStrings(kLocalizedStrings);
 
