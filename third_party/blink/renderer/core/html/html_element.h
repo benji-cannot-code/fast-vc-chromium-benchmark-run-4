1e294eb638bfa10bdff58a52feb8a3950510e19a
L. David Baron <dbaron@chromium.org>
2024-05-16 23:51:10 +0000

    Remove DirAutoNoInheritance feature flag, which shipped in M123.
    Bug: 576815
    Change-Id: I6ca4a79c992c369f58b755eae0794da452f457da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545027
    Auto-Submit: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302322}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -163,7 +163,10 @@ class CORE_EXPORT HTMLElement : public Element {
 
   static bool IsValidDirAttribute(const AtomicString& value);
   static bool ElementAffectsDirectionality(const Node* node);
-  static bool ElementInheritsDirectionality(const Node* node);
+  static bool ElementInheritsDirectionality(const Node* node) {
+    return !HTMLElement::ElementAffectsDirectionality(node);
+  }
+
   static const TextControlElement*
   ElementIfAutoDirectionalityFormAssociatedOrNull(const Element* element);
   static TextControlElement* ElementIfAutoDirectionalityFormAssociatedOrNull(
