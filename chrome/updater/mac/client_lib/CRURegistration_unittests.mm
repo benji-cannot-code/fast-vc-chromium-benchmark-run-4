7308d013b265ad2d81c1d0ae2fbbe101a5314dd6
Adam Norberg <norberg@google.com>
2024-05-16 19:07:41 +0000

    Stub for Mac client library to provide access to updater features.
    This library will avoid depending on other parts of Chromium; it's
    intended for static linking into non-Chromium binaries that want to
    package and use the Chromium updater. This CL implements almost none of
    its features, defining only the bare object.
    
    I underestimated the complexity of implementing this and I'm breaking
    down a larger incomplete CL into smaller parts to start landing them
    so they will be more manageable to review (and to fix).
    
    Change-Id: I756bb5f47b041dc4f43aad0764c869198533ea4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502600
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Adam Norberg <norberg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1302137}

---
+++ chrome/updater/mac/client_lib/CRURegistration_unittests.mm
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/updater/mac/client_lib/CRURegistration.h"
+
+#import <Foundation/Foundation.h>
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
+
+TEST(CRURegistrationTest, SmokeTest) {
+  CRURegistration* registration = [[CRURegistration alloc]
+      initWithAppId:
+          @"org.chromium.ChromiumUpdater.CRURegistrationTest.SmokeTest"];
+  ASSERT_TRUE(registration);
+}
+
+}  // namespace
