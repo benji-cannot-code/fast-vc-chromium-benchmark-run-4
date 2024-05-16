fb10140a852e0f00eeb6f79c5641d0e7d970e957
Brahim Chikhaoui <bchikhaoui@google.com>
2024-05-16 10:22:30 +0000

    [OOBE] - Add the oobe app discovery service
    adding the oobe app discovery service to fetch personalized recommended
    apps and categories from almanac endpoint.
    
    Bug: b:340240263
    Change-Id: Idc9da36aff0404860944d572e13d68f2752086bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537991
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301851}

--- chrome/browser/ash/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/browser_context_keyed_service_factories.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/extensions/login_screen_extensions_content_script_manager_factory.h"
 #include "chrome/browser/ash/login/extensions/login_screen_extensions_lifetime_manager_factory.h"
 #include "chrome/browser/ash/login/lock/online_reauth/lock_screen_reauth_manager_factory.h"
+#include "chrome/browser/ash/login/oobe_apps_service/oobe_apps_discovery_service_factory.h"
 #include "chrome/browser/ash/login/osauth/profile_prefs_auth_policy_connector_factory.h"
 #include "chrome/browser/ash/login/quick_unlock/quick_unlock_factory.h"
 #include "chrome/browser/ash/login/saml/password_sync_token_verifier_factory.h"
@@ -221,6 +222,7 @@ void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   OAuth2LoginManagerFactory::GetInstance();
   on_device_controls::AppControlsServiceFactory::GetInstance();
   OfflineSigninLimiterFactory::GetInstance();
+  OobeAppsDiscoveryServiceFactory::GetInstance();
   OwnerSettingsServiceAshFactory::GetInstance();
   PasswordSyncTokenVerifierFactory::GetInstance();
   personalization_app::PersonalizationAppManagerFactory::GetInstance();
