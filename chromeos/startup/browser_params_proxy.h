06c716e83b501962f22dd8b99f6f5fae547ca5a2
Andre Le <leandre@chromium.org>
2024-05-16 17:09:43 +0000

    MagicBoost: Add feature flag for Magic Boost
    Add a feature flag for Magic Boost in ash, and also pass the flag value
    to use in lacros.
    
    Fixed: b:340915722
    Change-Id: I09b16acc89032b8b75b25214b1dd510aa4d588b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539450
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302036}

--- chromeos/startup/browser_params_proxy.h
+++ chromeos/startup/browser_params_proxy.h
@@ -177,6 +177,8 @@ class COMPONENT_EXPORT(CHROMEOS_STARTUP) BrowserParamsProxy {
 
   bool IsCrosMallEnabled() const;
 
+  bool IsMagicBoostEnabled() const;
+
   bool IsMahiEnabled() const;
 
   bool IsContainerAppPreinstallEnabled() const;
