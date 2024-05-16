65eac2c7a22271b4a7a9d3d89d93987e07941d23
James Cook <jamescook@chromium.org>
2024-05-16 18:27:28 +0000

    birch: Add caching for weather icons
    Usually the weather does not change much throughout the day, so cache
    the downloaded "weather condition" icon. Add this capability via a
    generic "BirchIconCache" as we'll probably want to cache the icons for
    drive items and the like.
    
    Bug: b:334940393
    Test: added to ash_unittests
    Change-Id: I2000d28a2ca93837f938aab5190dd25d5eb8a062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542630
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302100}

--- ash/birch/birch_model.cc
+++ ash/birch/birch_model.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/birch/birch_data_provider.h"
+#include "ash/birch/birch_icon_cache.h"
 #include "ash/birch/birch_item.h"
 #include "ash/birch/birch_item_remover.h"
 #include "ash/birch/birch_ranker.h"
@@ -59,7 +60,8 @@ BirchModel::BirchModel()
       recent_tab_data_(prefs::kBirchUseRecentTabs, "Tab"),
       self_share_data_(prefs::kBirchUseSelfShare, "SelfShare"),
       release_notes_data_(prefs::kBirchUseReleaseNotes, "ReleaseNotes"),
-      weather_data_(prefs::kBirchUseWeather, "Weather") {
+      weather_data_(prefs::kBirchUseWeather, "Weather"),
+      icon_cache_(std::make_unique<BirchIconCache>()) {
   if (features::IsBirchWeatherEnabled()) {
     weather_provider_ = std::make_unique<BirchWeatherProvider>(this);
   }
--- ash/birch/birch_model.h
+++ ash/birch/birch_model.h
@@ -25,6 +25,7 @@ class PrefRegistrySimple;
 namespace ash {
 
 class BirchDataProvider;
+class BirchIconCache;
 class BirchItemRemover;
 
 // Birch model, which is used to aggregate and store relevant information from
@@ -106,6 +107,7 @@ class ASH_EXPORT BirchModel : public SessionObserver,
   void SetClientAndInit(BirchClient* client);
 
   BirchClient* birch_client() { return birch_client_; }
+  BirchIconCache* icon_cache() { return icon_cache_.get(); }
 
   const std::vector<BirchCalendarItem>& GetCalendarItemsForTest() const {
     return calendar_data_.items;
@@ -225,6 +227,8 @@ class ASH_EXPORT BirchModel : public SessionObserver,
   DataTypeInfo<BirchWeatherItem> weather_data_;
 
   raw_ptr<BirchClient> birch_client_ = nullptr;
+
+  std::unique_ptr<BirchIconCache> icon_cache_;
 
   std::unique_ptr<BirchDataProvider> weather_provider_;
 
