5d2f12b9f9385acc0756149c7c90de12e7bfc7b0
Jeroen Dhollander <jeroendh@google.com>
2024-05-16 22:16:23 +0000

    Fix MetricsService crash
    When the user log store is unset the metrics log is cleared as well,
    so the service should stop receiving recorded actions.
    
    Bug: b/328919325
    Change-Id: I49ba68e8af5666144aa75dd48f68e0d36e6c4e9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535572
    Auto-Submit: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302259}

--- components/metrics/metrics_service.cc
+++ components/metrics/metrics_service.cc
@@ -140,6 +140,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_samples.h"
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/metrics/statistics_recorder.h"
+#include "base/metrics/user_metrics.h"
 #include "base/process/process_handle.h"
 #include "base/rand_util.h"
 #include "base/task/sequenced_task_runner.h"
@@ -698,8 +699,9 @@ void MetricsService::SetUserLogStore(
 }
 
 void MetricsService::UnsetUserLogStore() {
-  if (!log_store()->has_alternate_ongoing_log_store())
+  if (!log_store()->has_alternate_ongoing_log_store()) {
     return;
+  }
 
   if (state_ >= SENDING_LOGS) {
     PushPendingLogsToPersistentStorage(
@@ -724,9 +726,10 @@ void MetricsService::UnsetUserLogStore() {
       /*required_flags=*/base::Histogram::kUmaTargetedHistogramFlag,
       &histogram_snapshot_manager);
 
-  // Discard the current log and don't store it.
+  // Discard the current log, don't store it and stop recording.
   CHECK(current_log_);
   current_log_.reset();
+  DisableRecording();
 
   log_store()->UnsetAlternateOngoingLogStore();
   RecordUserLogStoreState(kUnsetPreSendLogsState);
@@ -892,6 +895,7 @@ void MetricsService::InitializeMetricsState() {
 
 void MetricsService::OnUserAction(const std::string& action,
                                   base::TimeTicks action_time) {
+  CHECK(current_log_);
   current_log_->RecordUserAction(action, action_time);
   HandleIdleSinceLastTransmission(false);
 }
