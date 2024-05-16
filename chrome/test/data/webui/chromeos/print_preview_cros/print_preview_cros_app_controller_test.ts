2bec468bfa7e56ce5bbd74888dba78292edd8b7c
Gavin Williams <gavinwill@chromium.org>
2024-05-16 18:30:57 +0000

    cros-preview: Create capabilities manager
    - Create the Capabilities manager responsible for fetching destination
      capabilities from Ash.
    - The manager will listen for changes to the active destination then
      fetch and cache its capabilities.
    - The manager send out events alerting consumers when caps are loading
      or ready.
    - The manager exposes an API for getting cached capabilities.
    
    Test: browser_test --gtest_filter=*PrintPreviewCros*
    Bug: b:323421684
    Change-Id: I8905e4a75c0236b36f2d2713ba5cc9e52857bc65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535748
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302104}

--- chrome/test/data/webui/chromeos/print_preview_cros/print_preview_cros_app_controller_test.ts
+++ chrome/test/data/webui/chromeos/print_preview_cros/print_preview_cros_app_controller_test.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-print/js/print_preview_cros_app_controller.js';
 
+import {CAPABILITIES_MANAGER_SESSION_INITIALIZED, CapabilitiesManager} from 'chrome://os-print/js/data/capabilities_manager.js';
 import {DESTINATION_MANAGER_SESSION_INITIALIZED, DestinationManager} from 'chrome://os-print/js/data/destination_manager.js';
 import {PREVIEW_TICKET_MANAGER_SESSION_INITIALIZED, PreviewTicketManager} from 'chrome://os-print/js/data/preview_ticket_manager.js';
 import {PRINT_TICKET_MANAGER_SESSION_INITIALIZED, PrintTicketManager} from 'chrome://os-print/js/data/print_ticket_manager.js';
@@ -26,6 +27,7 @@ suite('PrintPreviewCrosAppController', () => {
     mockTimer = new MockTimer();
     mockTimer.install();
 
+    CapabilitiesManager.resetInstanceForTesting();
     DestinationManager.resetInstanceForTesting();
     PrintTicketManager.resetInstanceForTesting();
     PreviewTicketManager.resetInstanceForTesting();
@@ -37,6 +39,7 @@ suite('PrintPreviewCrosAppController', () => {
 
   teardown(() => {
     printPreviewPageHandler.reset();
+    CapabilitiesManager.resetInstanceForTesting();
     DestinationManager.resetInstanceForTesting();
     PreviewTicketManager.resetInstanceForTesting();
     PrintTicketManager.resetInstanceForTesting();
@@ -144,6 +147,31 @@ suite('PrintPreviewCrosAppController', () => {
         assertTrue(
             previewTicketManager.isSessionInitialized(),
             'After initializeSession PreviewTicketManager instance should be ' +
+                'initialized');
+      });
+
+  // Verify capabilities manager is initialized after start session resolves.
+  test(
+      'on resolve of startSession calls CapabilitiesManager.initializeSession',
+      async () => {
+        printPreviewPageHandler.setTestDelay(testDelay);
+
+        const controller = new PrintPreviewCrosAppController();
+        assertTrue(!!controller, 'Unable to create controller');
+        const capabilitiesManager = CapabilitiesManager.getInstance();
+        assertFalse(
+            capabilitiesManager.isSessionInitialized(),
+            'Before initializeSession CapabilitiesManager instance should ' +
+                'not be initialized');
+
+        // Move timer forward to resolve startSession.
+        mockTimer.tick(testDelay);
+        await eventToPromise(
+            CAPABILITIES_MANAGER_SESSION_INITIALIZED, capabilitiesManager);
+
+        assertTrue(
+            capabilitiesManager.isSessionInitialized(),
+            'After initializeSession CapabilitiesManager instance should be ' +
                 'initialized');
       });
 });
