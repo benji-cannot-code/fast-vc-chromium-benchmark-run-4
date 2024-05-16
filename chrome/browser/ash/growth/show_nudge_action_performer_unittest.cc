c65e7860435041c664b1321b7469e9eec3855683
Tao Wu <wutao@chromium.org>
2024-05-16 21:30:13 +0000

    growth: Add mock ui performer observer
    Refactor tests.
    
    Bug: 331504244
    Change-Id: I70fcfd89b3074ef5f3d68f4982b1c0c37f81ba3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542699
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302233}

--- chrome/browser/ash/growth/show_nudge_action_performer_unittest.cc
+++ chrome/browser/ash/growth/show_nudge_action_performer_unittest.cc
@@ -13,9 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observation.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/ash/growth/metrics.h"
-#include "chrome/browser/ash/growth/ui_action_performer.h"
+#include "chrome/browser/ash/growth/mock_ui_performer_observer.h"
 #include "content/public/test/browser_task_environment.h"
-#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -26,16 +25,6 @@ constexpr char kNudgePayloadTemplate[] = R"(
       "%s": "text"
     }
 )";
-
-class MockUiPerformerObserver : public UiActionPerformer::Observer {
- public:
-  // UiActionPerformer::Observer:
-  MOCK_METHOD(void, OnReadyToLogImpression, (int), (override));
-
-  MOCK_METHOD(void, OnDismissed, (int), (override));
-
-  MOCK_METHOD(void, OnButtonPressed, (int, CampaignButtonId, bool), (override));
-};
 
 }  // namespace
 
