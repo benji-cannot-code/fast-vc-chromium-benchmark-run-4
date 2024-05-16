e828c947d104bd654e8bd6d2966625ed9643e0b8
Jack Yammine <jyammine@google.com>
2024-05-16 14:25:58 +0000

    [waffle]Stop resetting the choice location pref to kOther
    The `kDefaultSearchProviderChoiceLocationPrefName` pref was being reset
    to kOther after every navigation due to a bug. We fixed the issue and
    now store the pref value in the `kDefaultSearchProviderDataPrefName`
    dictionary with the value of `kChoiceLocation`.
    
    Bug: b:340800687, 341042778
    Change-Id: I8a40cb3afebc0fbfec7adc3ff8e002c9646d90f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537942
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301934}

--- components/search_engines/default_search_manager.cc
+++ components/search_engines/default_search_manager.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <optional>
 #include <utility>
 
 #include "base/check.h"
+#include "base/check_is_test.h"
 #include "base/compiler_specific.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
@@ -21,10 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "base/values.h"
 #include "build/chromeos_buildflags.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_service.h"
 #include "components/prefs/pref_value_map.h"
+#include "components/search_engines/choice_made_location.h"
 #include "components/search_engines/search_engines_pref_names.h"
 #include "components/search_engines/template_url_data.h"
 #include "components/search_engines/template_url_data_util.h"
@@ -97,6 +101,8 @@ const char DefaultSearchManager::kPrefetchLikelyNavigations[] =
 const char DefaultSearchManager::kIsActive[] = "is_active";
 const char DefaultSearchManager::kStarterPackId[] = "starter_pack_id";
 const char DefaultSearchManager::kEnforcedByPolicy[] = "enforced_by_policy";
+
+const char DefaultSearchManager::kChoiceLocation[] = "choice_location";
 
 DefaultSearchManager::DefaultSearchManager(
     PrefService* pref_service,
@@ -222,17 +228,28 @@ DefaultSearchManager::GetDefaultSearchEngineSource() const {
 search_engines::ChoiceMadeLocation
 DefaultSearchManager::GetChoiceMadeLocationForUserSelectedDefaultSearchEngine()
     const {
-  if (GetDefaultSearchEngineSource() != Source::FROM_USER) {
+  if (!pref_service_) {
+    CHECK_IS_TEST();
     return search_engines::ChoiceMadeLocation::kOther;
   }
-  int choice_made_location =
-      pref_service_->GetInteger(kDefaultSearchProviderChoiceLocationPrefName);
-  if (choice_made_location < 0 ||
-      choice_made_location >
+
+  const base::Value::Dict& template_url_dictionary =
+      pref_service_->GetDict(kDefaultSearchProviderDataPrefName);
+  std::optional<int> choice_made_location =
+      template_url_dictionary.FindInt(kChoiceLocation);
+
+  if (GetDefaultSearchEngineSource() != Source::FROM_USER ||
+      !choice_made_location.has_value()) {
+    return search_engines::ChoiceMadeLocation::kOther;
+  }
+
+  if (choice_made_location.value() < 0 ||
+      choice_made_location.value() >
           static_cast<int>(search_engines::ChoiceMadeLocation::kMaxValue)) {
     return search_engines::ChoiceMadeLocation::kOther;
   }
-  return static_cast<search_engines::ChoiceMadeLocation>(choice_made_location);
+  return static_cast<search_engines::ChoiceMadeLocation>(
+      choice_made_location.value());
 }
 
 const TemplateURLData* DefaultSearchManager::GetFallbackSearchEngine() const {
@@ -249,11 +266,11 @@ void DefaultSearchManager::SetUserSelectedDefaultSearchEngine(
     NotifyObserver();
     return;
   }
-
+  base::Value::Dict template_url_dictionary = TemplateURLDataToDictionary(data);
+  template_url_dictionary.Set(kChoiceLocation,
+                              static_cast<int>(choice_location));
   pref_service_->SetDict(kDefaultSearchProviderDataPrefName,
-                         TemplateURLDataToDictionary(data));
-  pref_service_->SetInteger(kDefaultSearchProviderChoiceLocationPrefName,
-                            static_cast<int>(choice_location));
+                         std::move(template_url_dictionary));
 #if BUILDFLAG(IS_ANDROID)
   // Commit the pref immediately so it isn't lost if the app is killed.
   pref_service_->CommitPendingWrite();
@@ -263,7 +280,6 @@ void DefaultSearchManager::SetUserSelectedDefaultSearchEngine(
 void DefaultSearchManager::ClearUserSelectedDefaultSearchEngine() {
   if (pref_service_) {
     pref_service_->ClearPref(kDefaultSearchProviderDataPrefName);
-    pref_service_->ClearPref(kDefaultSearchProviderChoiceLocationPrefName);
   } else {
     prefs_default_search_.reset();
     NotifyObserver();
