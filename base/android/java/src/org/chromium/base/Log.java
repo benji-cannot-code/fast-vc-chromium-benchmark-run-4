7978faae289a8e123f0f83e52fdc21042bd2eb07
Andrew Grieve <agrieve@chromium.org>
2024-05-16 14:21:29 +0000

    Reland "Android: Remove Log.getCallOrigin()"
    This reverts commit 310e96098d355f05b4d9498b97d71b8f4416b79b.
    
    Reason for reland: Removing @CheckDiscard & not removing proguard rules
    
    Original change's description:
    > Revert "Android: Remove Log.getCallOrigin()"
    >
    > This reverts commit 9030bcd0fd5fda965700102633b7406d0836591b.
    >
    > Reason for revert: Broke cronet bot:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/android-cronet-x64-rel/18478/overview
    >
    > Original change's description:
    > > Android: Remove Log.getCallOrigin()
    > >
    > > It's not that useful and causes issues for cronet
    > >
    > > Bug: 340192797
    > > Change-Id: I53ec88bf393d58106bb8724036255b42de8b9434
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539012
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Etienne Dechamps <edechamps@google.com>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1301256}
    >
    > Bug: 340192797
    > Change-Id: I209ee0e14316755c26fb14d66936bcd62c5e12cc
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540654
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1301283}
    
    Bug: 340192797
    Change-Id: I6f9d0e2e4b6ae864912a80f43697fbf13956f924
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539906
    Reviewed-by: Etienne Dechamps <edechamps@google.com>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Etienne Dechamps <edechamps@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301933}

--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.base;
 
 import org.chromium.build.annotations.AlwaysInline;
-import org.chromium.build.annotations.CheckDiscard;
 
 import java.util.Locale;
 
@@ -66,22 +65,13 @@ public class Log {
         return "cr_" + tag;
     }
 
-    /**
-     * Returns a formatted log message, using the supplied format and arguments.
-     * The message will be prepended with the filename and line number of the call.
-     */
-    private static String formatLogWithStack(
-            String messageTemplate, Throwable tr, Object... params) {
-        return "[" + getCallOrigin() + "] " + formatLog(messageTemplate, tr, params);
-    }
-
     private static boolean isDebug() {
         // Proguard sets value to false in release builds.
         return true;
     }
 
     /**
-     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}, but always
+     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}.
      * In release: Always returns false (via proguard rule).
      */
     public static boolean isLoggable(String tag, int level) {
@@ -95,19 +85,18 @@ public class Log {
     /**
      * Sends a {@link android.util.Log#VERBOSE} log message.
      *
-     * @param tag Used to identify the source of a log message. Might be modified in the output
-     *            (see {@link #normalizeTag(String)})
+     * @param tag Used to identify the source of a log message. Might be modified in the output (see
+     *     {@link #normalizeTag(String)})
      * @param messageTemplate The message you would like logged. It is to be specified as a format
-     *                        string.
+     *     string.
      * @param args Arguments referenced by the format specifiers in the format string. If the last
-     *             one is a {@link Throwable}, its trace will be printed.
+     *     one is a {@link Throwable}, its trace will be printed.
      */
-    @CheckDiscard("crbug.com/1231625")
     public static void v(String tag, String messageTemplate, Object... args) {
         if (!isDebug()) return;
 
         Throwable tr = getThrowableToLog(args);
-        String message = formatLogWithStack(messageTemplate, tr, args);
+        String message = formatLog(messageTemplate, tr, args);
         tag = normalizeTag(tag);
         if (tr != null) {
             android.util.Log.v(tag, message, tr);
@@ -119,19 +108,18 @@ public class Log {
     /**
      * Sends a {@link android.util.Log#DEBUG} log message.
      *
-     * @param tag Used to identify the source of a log message. Might be modified in the output
-     *            (see {@link #normalizeTag(String)})
+     * @param tag Used to identify the source of a log message. Might be modified in the output (see
+     *     {@link #normalizeTag(String)})
      * @param messageTemplate The message you would like logged. It is to be specified as a format
-     *                        string.
+     *     string.
      * @param args Arguments referenced by the format specifiers in the format string. If the last
-     *             one is a {@link Throwable}, its trace will be printed.
+     *     one is a {@link Throwable}, its trace will be printed.
      */
-    @CheckDiscard("crbug.com/1231625")
     public static void d(String tag, String messageTemplate, Object... args) {
         if (!isDebug()) return;
 
         Throwable tr = getThrowableToLog(args);
-        String message = formatLogWithStack(messageTemplate, tr, args);
+        String message = formatLog(messageTemplate, tr, args);
         tag = normalizeTag(tag);
         if (tr != null) {
             android.util.Log.d(tag, message, tr);
@@ -887,29 +875,5 @@ public class Log {
 
         if (!(lastArg instanceof Throwable)) return null;
         return (Throwable) lastArg;
-    }
-
-    /** Returns a string form of the origin of the log call, to be used as secondary tag.*/
-    @CheckDiscard("crbug.com/1231625")
-    private static String getCallOrigin() {
-        StackTraceElement[] st = Thread.currentThread().getStackTrace();
-
-        // The call stack should look like:
-        //   n [a variable number of calls depending on the vm used]
-        //  +0 getCallOrigin()
-        //  +1 formatLogWithStack()
-        //  +2 privateLogFunction: verbose or debug
-        //  +3 caller
-
-        int callerStackIndex;
-        String logClassName = Log.class.getName();
-        for (callerStackIndex = 0; callerStackIndex < st.length; callerStackIndex++) {
-            if (st[callerStackIndex].getClassName().equals(logClassName)) {
-                callerStackIndex += 3;
-                break;
-            }
-        }
-
-        return st[callerStackIndex].getFileName() + ":" + st[callerStackIndex].getLineNumber();
     }
 }
