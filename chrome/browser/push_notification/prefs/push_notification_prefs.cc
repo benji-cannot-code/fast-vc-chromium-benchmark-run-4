e5da9cf5571f65b303a8246b0b9d81472cdfca2d
Alex Kingsborough <akingsb@google.com>
2024-05-16 06:13:17 +0000

    [Chime] Add retries to PNS
    When PNS fails to register itself, retry with exponential
    backoff. However, if the failure is due to internet connection issues,
    only retry when the connection has been restored.
    
    Test: Added unittests and manually tested on DUT
    Change-Id: I17a8aba82459b759b40611a07ec6db3ea2c2a332
    Bug: b/323949082
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529690
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301775}

--- chrome/browser/push_notification/prefs/push_notification_prefs.cc
+++ chrome/browser/push_notification/prefs/push_notification_prefs.cc
@@ -10,8 +10,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace push_notification {
 
+namespace prefs {
+
+const char kPushNotificationRegistrationAttemptBackoffSchedulerPrefName[] =
+    "push_notification.scheduler.registration_attempt_backoff";
+
+}  // namespace prefs
+
 // TODO(b/306399332): Add pref for `representative_target_id` to be used when
 // registering with Chime.
-void RegisterPushNotificationPrefs(PrefRegistrySimple* registry) {}
+void RegisterPushNotificationPrefs(PrefRegistrySimple* registry) {
+  registry->RegisterDictionaryPref(
+      prefs::kPushNotificationRegistrationAttemptBackoffSchedulerPrefName);
+}
 
 }  // namespace push_notification
