f86f255eb8cd69f1d5e4f0e060ae81ebfd2f5e96
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-05-16 17:44:34 +0000

    Move using declaration out of base_rendering_context_2d.h
    We should never add convenience using declarations in header files. We
    should only do so if we intend on adding that symbol to the public API
    of the header.
    See: https://google.github.io/styleguide/cppguide.html#Aliases
    
    The `using cc::UsePaintCache` in base_rendering_context_2d.h was adding
    `UsePaintCache` as a public symbol of the `blink` namespace, which then
    tricked include-what-you-use and clang-include-cleaner into recommending
    that header as a provider of the `UsePaintCache` symbol.
    
    Change-Id: I9801822e879f08b93dcf60f262f59d1cdf96384d
    Bug: 40241751
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540371
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Reviewed-by: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302074}

--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc
@@ -93,6 +93,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+using ::cc::UsePaintCache;
+
 BASE_FEATURE(kDisableCanvasOverdrawOptimization,
              "DisableCanvasOverdrawOptimization",
              base::FEATURE_DISABLED_BY_DEFAULT);
--- third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d_test.cc
@@ -69,6 +69,7 @@ using ::cc::SaveLayerOp;
 using ::cc::SaveOp;
 using ::cc::SetMatrixOp;
 using ::cc::TranslateOp;
+using ::cc::UsePaintCache;
 using ::testing::IsEmpty;
 using ::testing::Not;
 using ::testing::Pointee;
