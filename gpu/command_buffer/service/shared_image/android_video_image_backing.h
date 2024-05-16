ccc1caa699f6053d996112db63de0b9b835a7670
Colin Blundell <blundell@chromium.org>
2024-05-16 07:13:53 +0000

    [gpu] Give DawnContextProvider to AndroidVideoImageBacking::GetYcbcrInfo
    AndroidVideoImageBacking::GetYcbcrInfo() currently takes in a
    VulkanContextProvider and returns null info if the VulkanContextProvider
    is null. Callsites supply a valid provider only if Skia is using Vulkan.
    
    For Graphite, we will need to populate the YCbCr info via Dawn. This CL
    adds plumbing of the DawnContextProvider into GetYcbcrInfo() if Skia is
    using Dawn. A followup CL will use the DawnContextProvider in
    GetYcbcrInfo() to populate the YCbCr info.
    
    Bug: 41488897
    Change-Id: I59b07a209ed3c6c036bd8a305c13178563fdbb4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538179
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301790}

--- gpu/command_buffer/service/shared_image/android_video_image_backing.h
+++ gpu/command_buffer/service/shared_image/android_video_image_backing.h
@@ -18,6 +18,7 @@ class VulkanContextProvider;
 }  // namespace viz
 
 namespace gpu {
+class DawnContextProvider;
 struct Mailbox;
 struct VulkanYCbCrInfo;
 class AbstractTextureAndroid;
@@ -40,11 +41,12 @@ class GPU_GLES2_EXPORT AndroidVideoImageBacking : public AndroidImageBacking {
       scoped_refptr<SharedContextState> context_state,
       scoped_refptr<RefCountedLock> drdc_lock);
 
-  // Returns ycbcr information. This is only valid in vulkan context and
-  // nullopt for other context.
+  // Returns ycbcr information. This is only valid in vulkan/dawn contexts and
+  // nullopt for other contexts.
   static std::optional<VulkanYCbCrInfo> GetYcbcrInfo(
       TextureOwner* texture_owner,
-      viz::VulkanContextProvider* vulkan_context_provider);
+      viz::VulkanContextProvider* vulkan_context_provider,
+      DawnContextProvider* dawn_context_provider);
 
   ~AndroidVideoImageBacking() override;
 
