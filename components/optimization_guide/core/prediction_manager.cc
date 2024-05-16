aafb687253e9088919eb107563706a736466e22a
rajendrant <rajendrant@chromium.org>
2024-05-16 19:51:20 +0000

    Allow base model spec in model metadata
    Change-Id: If95f900c7ccac18e59b6d4d82d8b59a8f802fca3
    Bug: b/310740288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544564
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302163}

--- components/optimization_guide/core/prediction_manager.cc
+++ components/optimization_guide/core/prediction_manager.cc
@@ -149,7 +149,11 @@ bool IsModelMetadataTypeOnServerAllowlist(const proto::Any& model_metadata) {
          model_metadata.type_url() ==
              "type.googleapis.com/"
              "google.internal.chrome.optimizationguide.v1."
-             "HistoryClustersModuleRankingModelMetadata";
+             "HistoryClustersModuleRankingModelMetadata" ||
+         model_metadata.type_url() ==
+             "type.googleapis.com/"
+             "google.internal.chrome.optimizationguide.v1."
+             "OnDeviceBaseModelMetadata";
 }
 
 void RecordModelAvailableAtRegistration(
@@ -200,8 +204,9 @@ PredictionManager::PredictionManager(
 }
 
 PredictionManager::~PredictionManager() {
-  if (prediction_model_download_manager_)
+  if (prediction_model_download_manager_) {
     prediction_model_download_manager_->RemoveObserver(this);
+  }
 }
 
 void PredictionManager::Initialize(
@@ -353,8 +358,9 @@ void PredictionManager::FetchModels() {
       static_cast<int>(
           *base_model_info.supported_model_engine_versions().begin()));
 
-  if (switches::IsModelOverridePresent())
+  if (switches::IsModelOverridePresent()) {
     return;
+  }
 
   if (!ShouldFetchModels(off_the_record_,
                          component_updates_enabled_provider_.Run())) {
@@ -419,8 +425,9 @@ void PredictionManager::FetchModels() {
 
     auto model_it =
         optimization_target_model_info_map_.find(registration_info.first);
-    if (model_it != optimization_target_model_info_map_.end())
+    if (model_it != optimization_target_model_info_map_.end()) {
       model_info.set_version(model_it->second.get()->GetVersion());
+    }
 
     models_info.push_back(model_info);
     if (optimization_guide_logger_->ShouldEnableDebugLogs()) {
@@ -624,8 +631,9 @@ void PredictionManager::UpdatePredictionModels(
 void PredictionManager::OnModelReady(const base::FilePath& base_model_dir,
                                      const proto::PredictionModel& model) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (switches::IsModelOverridePresent())
+  if (switches::IsModelOverridePresent()) {
     return;
+  }
 
   DCHECK(model.model_info().has_version() &&
          model.model_info().has_optimization_target());
@@ -813,8 +821,9 @@ void PredictionManager::OnLoadPredictionModel(
   }
   bool success = ProcessAndStoreLoadedModel(*model);
   DCHECK_EQ(optimization_target, model->model_info().optimization_target());
-  if (record_availability_metrics)
+  if (record_availability_metrics) {
     RecordModelAvailableAtRegistration(optimization_target, success);
+  }
   OnProcessLoadedModel(*model, success);
 }
 
@@ -849,12 +858,15 @@ void PredictionManager::RemoveModelFromStore(
 bool PredictionManager::ProcessAndStoreLoadedModel(
     const proto::PredictionModel& model) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (!model.model_info().has_optimization_target())
+  if (!model.model_info().has_optimization_target()) {
     return false;
-  if (!model.model_info().has_version())
+  }
+  if (!model.model_info().has_version()) {
     return false;
-  if (!model.has_model())
+  }
+  if (!model.has_model()) {
     return false;
+  }
   if (!model_registration_info_map_.contains(
           model.model_info().optimization_target())) {
     return false;
@@ -892,8 +904,9 @@ bool PredictionManager::ShouldUpdateStoredModelForTarget(
 
   auto model_meta_it =
       optimization_target_model_info_map_.find(optimization_target);
-  if (model_meta_it != optimization_target_model_info_map_.end())
+  if (model_meta_it != optimization_target_model_info_map_.end()) {
     return model_meta_it->second->GetVersion() != new_version;
+  }
 
   return true;
 }
