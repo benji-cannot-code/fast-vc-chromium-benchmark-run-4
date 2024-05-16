0299fdabfb7707c4949c2dba716cb259564dbefe
Kartar Singh <kartarsingh@google.com>
2024-05-16 10:26:48 +0000

    Send VIEW_SCROLLED accessbility events every 100ms during scrolls.
    Current logic is on every GSU consumed ack we try to send VIEW_SCROLLED
    unless it already had been sent in last 100ms. This change makes the
    event sending logic independent of GSU acks instead now the events will
    be sent every 100ms, except when the view base class had already sent
    the event.
    
    This change helps us move closer to removing scroll update ack
    consumers. Currently there is only one user of
    onScrollUpdateGestureConsumed() i.e. in webview. We are planning to get
    rid of it as part of InputVizard
    project(https://docs.google.com/document/d/1mcydbkgFCO_TT9NuFE962L8PLJWT2XOfXUAPO88VuKE).
    Webview itself doesn't get affected by the project but this will allow
    chrome and webview to have same gesture listener interface, instead of
    one having the onScrollUpdateGestureConsumed, while the other one does
    not.
    
    Bug: b/328601354
    Change-Id: Ibe6ac5e24c11ab887b7b307415ba05e08dcca8bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5514149
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301853}

--- android_webview/browser/aw_feature_map.cc
+++ android_webview/browser/aw_feature_map.cc
@@ -48,6 +48,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewReduceUAAndroidVersionDeviceModel,
     &features::kWebViewEnableCrash,
     &features::kWebViewPreloadClasses,
+    &features::kWebViewDoNotSendAccessibilityEventsOnGSU,
 };
 
 // static
--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -895,7 +895,10 @@ public class AwContents implements SmartClipProvider {
 
         @Override
         public void onScrollUpdateGestureConsumed() {
-            mScrollAccessibilityHelper.postViewScrolledAccessibilityEventCallback();
+            if (!AwFeatureMap.isEnabled(
+                    AwFeatures.WEBVIEW_DO_NOT_SEND_ACCESSIBILITY_EVENTS_ON_GSU)) {
+                mScrollAccessibilityHelper.postViewScrolledAccessibilityEventCallback();
+            }
             if (AwFeatureMap.isEnabled(AwFeatures.WEBVIEW_INVOKE_ZOOM_PICKER_ON_GSU)) {
                 mZoomControls.invokeZoomPicker();
             }
@@ -914,6 +917,10 @@ public class AwContents implements SmartClipProvider {
                 mAwFrameMetricsListener.onWebContentsScrollStateUpdate(
                         /* isScrolling= */ true, mId);
             }
+            if (AwFeatureMap.isEnabled(
+                    AwFeatures.WEBVIEW_DO_NOT_SEND_ACCESSIBILITY_EVENTS_ON_GSU)) {
+                mScrollAccessibilityHelper.setIsInAScroll(true);
+            }
         }
 
         @Override
@@ -927,6 +934,10 @@ public class AwContents implements SmartClipProvider {
             if (mAwFrameMetricsListener != null) {
                 mAwFrameMetricsListener.onWebContentsScrollStateUpdate(
                         /* isScrolling= */ false, mId);
+            }
+            if (AwFeatureMap.isEnabled(
+                    AwFeatures.WEBVIEW_DO_NOT_SEND_ACCESSIBILITY_EVENTS_ON_GSU)) {
+                mScrollAccessibilityHelper.setIsInAScroll(false);
             }
         }
 
