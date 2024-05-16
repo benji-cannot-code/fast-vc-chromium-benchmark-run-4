233cc63332e85de470ea6f0dd149c2eb253fa4c4
Brian Sheedy <bsheedy@chromium.org>
2024-05-16 20:46:05 +0000

    Support platform-specific trace tests
    Updates the trace tests test generation code and associated unittest
    to support platform-specific tests without needing to add Skip
    expectations. This was previously not possible because the test
    expectation unittest would end up failing when:
    
    * A test expectation existed for a platform-specific test
    * The unittest was run on a platform other than that specific platform
    
    This is because the unittest would determine that the expectation was
    for a non-existent test.
    
    In order to achieve this, the following refactors were necessary:
    
    * Created a trace_test equivalent of pixel_test_pages
    * Moved the WebGPU cache trace test definitions to the new file
    * Updated the unittest to use this new file and take into account the
      different possible prefixes
    
    Bug: 337737554
    Change-Id: I005220569642d3c8aa166fee8ec2b106af680c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531080
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302205}

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -571,7 +571,8 @@ class PixelTestPages():
     ]
 
   @staticmethod
-  def WebGPUPages(base_name):
+  def WebGPUPages(base_name) -> List[PixelTestPage]:
+
     class Mode(Enum):
       WEBGPU_DEFAULT = 0
       WEBGPU_SWIFTSHADER = 1
@@ -676,7 +677,7 @@ class PixelTestPages():
             webgpu_pages_helper(base_name, mode=Mode.VULKAN_SWIFTSHADER))
 
   @staticmethod
-  def WebGPUCanvasCapturePages(base_name):
+  def WebGPUCanvasCapturePages(base_name) -> List[PixelTestPage]:
     webgpu_args = cba.ENABLE_WEBGPU_FOR_TESTING + [
         cba.ENABLE_EXPERIMENTAL_WEB_PLATFORM_FEATURES
     ]
@@ -1580,7 +1581,7 @@ class PixelTestPages():
 
   # This should only be used with the cast_streaming suite.
   @staticmethod
-  def CastStreamingReceiverPages(base_name):
+  def CastStreamingReceiverPages(base_name) -> List[PixelTestPage]:
     return [
         PixelTestPage('receiver.html',
                       base_name + '_VP8_1Frame',
