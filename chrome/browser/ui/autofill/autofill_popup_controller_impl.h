04595c5290ad01028951b4038d6d4ba27977ecc3
Taylor Bergquist <tbergquist@chromium.org>
2024-05-16 00:35:11 +0000

    Revert "[mullhet] Add "No suggestion found" message to autofill popup."
    This reverts commit d6ab32ca156418eb861c4bdf89a4b3c2bf29a3f6.
    
    Reason for revert: Causes flakiness in PopupViewViewsBrowsertest.ScrollingInNonRootPopupNonStickyFooter and PopupViewViewsBrowsertest.ScrollingInRootPopupStickyFooter
    
    Original change's description:
    > [mullhet] Add "No suggestion found" message to autofill popup.
    >
    > Bug: 325246516
    > Change-Id: I44e6fcc63c97dc474597e4ca31eff8e755e34bc6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515156
    > Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    > Reviewed-by: Jan Keitel <jkeitel@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1298141}
    
    Bug: 325246516
    Change-Id: I31b0b3b151b02268bee316e9e3e492981d47279e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542991
    Auto-Submit: Taylor Bergquist <tbergquist@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Owners-Override: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301674}

--- chrome/browser/ui/autofill/autofill_popup_controller_impl.h
+++ chrome/browser/ui/autofill/autofill_popup_controller_impl.h
@@ -109,7 +109,6 @@ class AutofillPopupControllerImpl
   void SetFilter(std::optional<SuggestionFilter> filter) override;
   bool HandleKeyPressEvent(
       const content::NativeWebKeyboardEvent& event) override;
-  bool HasFilteredOutSuggestions() const override;
   base::WeakPtr<AutofillPopupController> GetWeakPtr() override;
   void SetViewForTesting(base::WeakPtr<AutofillPopupView> view) override;
 
