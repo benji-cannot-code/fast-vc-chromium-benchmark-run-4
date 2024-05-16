221a192d2a5d7b31833bc00be483c3bbc6d9fa8f
Dibyajyoti Pal <dibyapal@google.com>
2024-05-16 19:10:24 +0000

    [Shortcuts] Make Windows shortcut creation callback asynchronous
    This CL makes the final callback on windows shortcut creation async.
    This ensures alignment with the Chrome browser design principles [1]
    since the callback is run asynchronously for all other implementations of the same behavior in this area.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:docs/chrome_browser_design_principles.md
    
    Bug: 333024272
    Change-Id: Iee21631871613d91a3f7f1f0774acd228751eb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545200
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302139}

--- chrome/browser/shortcuts/shortcut_creator_win_unittest.cc
+++ chrome/browser/shortcuts/shortcut_creator_win_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_path_override.h"
+#include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "base/test/test_shortcut_win.h"
 #include "base/win/scoped_com_initializer.h"
@@ -69,6 +70,7 @@ class ShortcutCreatorWinTest : public testing::Test {
   base::ScopedTempDir default_profile_path_;
   base::ScopedPathOverride desktop_override_{base::DIR_USER_DESKTOP};
   base::win::ScopedCOMInitializer com_initializer_;
+  base::test::SingleThreadTaskEnvironment task_environment_;
 };
 
 void ShortcutCreatorWinTest::VerifyShortcut() const {
