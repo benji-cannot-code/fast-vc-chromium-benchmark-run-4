65c3244e27c7bc76bffec6049a4cd2f910f32022
Norge Vizcay <vizcay@google.com>
2024-05-16 14:43:05 +0000

    Split building SuggestionsContext from Autofill suggestions generation.
    The CL extract most of the logic that generates SuggestionsContext out
    of `GetAvailableSuggestions`, which in the final state of this refactoring process will only be in charge of generating Autofill address and credit card suggestions.
    
    This CL attempts to restructure the code without affecting any functionality.
    
    Bug: b/340494671
    Change-Id: I3d4574ce32a4664e6efa6543484c9a662ef4eb18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537966
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1301943}

--- components/autofill/core/browser/suggestions_context.cc
+++ components/autofill/core/browser/suggestions_context.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 SuggestionsContext::SuggestionsContext() = default;
-
+SuggestionsContext::SuggestionsContext(const SuggestionsContext&) = default;
+SuggestionsContext& SuggestionsContext::operator=(const SuggestionsContext&) =
+    default;
 SuggestionsContext::~SuggestionsContext() = default;
 
 }  // namespace autofill
