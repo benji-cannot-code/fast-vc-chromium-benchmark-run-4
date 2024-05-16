ef68642e7ffb57c401195b3906ef06f970065f21
rbpotter <rbpotter@chromium.org>
2024-05-16 17:27:04 +0000

    Management UI: Migrate to cr-icon, fix transitive dependency
    Management UI was using iron-icon but did not actually import
    iron-icon.js to define this element, which resulted in breakages when
    the element(s) it was relying on for the transitive dependency were
    updated. Since iron-icon is now discouraged, update the UI to use
    cr-icon instead, and import cr_icon.js so that the UI no longer relies
    on transitive dependencies to get icon element definitions.
    
    Fixed: 340893677
    Change-Id: Idbfcae0c1ff2bfcf8d8bf07f0a2226826d645e36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542727
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302052}

--- chrome/browser/resources/management/management_ui.ts
+++ chrome/browser/resources/management/management_ui.ts
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
 import 'chrome://resources/cr_elements/cr_icons.css.js';
 import 'chrome://resources/cr_elements/cr_page_host_style.css.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar.js';
 import 'chrome://resources/cr_elements/cr_hidden_style.css.js';
-import 'chrome://resources/cr_elements/icons.html.js';
+import 'chrome://resources/cr_elements/icons_lit.html.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import './icons.html.js';
 import './strings.m.js';
