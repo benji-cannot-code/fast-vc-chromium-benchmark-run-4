760b6e2ba0462ea51ca4e3b8d5db0aa04079c4fd
Evan Stade <estade@chromium.org>
2024-05-16 20:49:55 +0000

    Quota: return error for GetBucketSpaceRemaining when manager is null.
    The `false` was being implicitly converted to an int64_t which is not
    treated as an error condition.
    
    The manager is null during shutdown.
    
    Bug: none
    Change-Id: Ia483091e3aa4400866e0442bdcd3f37e4f52f948
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545025
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302209}

--- storage/browser/quota/quota_manager_proxy.cc
+++ storage/browser/quota/quota_manager_proxy.cc
@@ -577,8 +577,9 @@ void QuotaManagerProxy::GetBucketSpaceRemaining(
 
   auto respond =
       base::BindPostTask(std::move(callback_task_runner), std::move(callback));
+
   if (!quota_manager_impl_) {
-    std::move(respond).Run(false);
+    std::move(respond).Run(base::unexpected(QuotaError::kUnknownError));
     return;
   }
 
