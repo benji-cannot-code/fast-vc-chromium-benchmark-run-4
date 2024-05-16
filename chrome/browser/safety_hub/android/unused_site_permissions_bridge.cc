203a0bdbfd513fb8c6449b70f12052d1c4607bb3
Rubin Deliallisi <rubindl@chromium.org>
2024-05-16 11:54:06 +0000

    [SHoA] Remaining permissions bridge methods
    - Add methods for regranting revoked permissions.
    - Add methods to undo regranting revoked permissions.
    - Add methods for acknowledging the revoked permissions list.
    - Add methods to undo acknowledging the revoked permissions list.
    
    Bug: 324562205
    Change-Id: I776cc3cdfef7f0479e16f82241277b203936868d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541901
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301880}

--- chrome/browser/safety_hub/android/unused_site_permissions_bridge.cc
+++ chrome/browser/safety_hub/android/unused_site_permissions_bridge.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_constraints.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 namespace jni_zero {
 
@@ -83,8 +85,73 @@ std::vector<PermissionsData> GetRevokedPermissions(Profile* profile) {
                                       service_result.end());
 }
 
+void RegrantPermissions(Profile* profile, std::string& primary_pattern) {
+  UnusedSitePermissionsService* service =
+      UnusedSitePermissionsServiceFactory::GetForProfile(profile);
+  CHECK(service);
+
+  url::Origin origin = url::Origin::Create(GURL(primary_pattern));
+  service->RegrantPermissionsForOrigin(origin);
+}
+
+void UndoRegrantPermissions(Profile* profile,
+                            PermissionsData& permissions_data) {
+  UnusedSitePermissionsService* service =
+      UnusedSitePermissionsServiceFactory::GetForProfile(profile);
+  CHECK(service);
+
+  service->UndoRegrantPermissionsForOrigin(permissions_data);
+}
+
+void ClearRevokedPermissionsReviewList(Profile* profile) {
+  UnusedSitePermissionsService* service =
+      UnusedSitePermissionsServiceFactory::GetForProfile(profile);
+  CHECK(service);
+
+  service->ClearRevokedPermissionsList();
+}
+
+void RestoreRevokedPermissionsReviewList(
+    Profile* profile,
+    std::vector<PermissionsData>& permissions_data_list) {
+  UnusedSitePermissionsService* service =
+      UnusedSitePermissionsServiceFactory::GetForProfile(profile);
+  CHECK(service);
+
+  for (const auto& permissions_data : permissions_data_list) {
+    service->StorePermissionInRevokedPermissionSetting(permissions_data);
+  }
+}
+
 static std::vector<PermissionsData>
 JNI_UnusedSitePermissionsBridge_GetRevokedPermissions(JNIEnv* env,
                                                       Profile* profile) {
   return GetRevokedPermissions(profile);
+}
+
+static void JNI_UnusedSitePermissionsBridge_RegrantPermissions(
+    JNIEnv* env,
+    Profile* profile,
+    std::string& primary_pattern) {
+  RegrantPermissions(profile, primary_pattern);
+}
+
+static void JNI_UnusedSitePermissionsBridge_UndoRegrantPermissions(
+    JNIEnv* env,
+    Profile* profile,
+    PermissionsData& permissions_data) {
+  UndoRegrantPermissions(profile, permissions_data);
+}
+
+static void JNI_UnusedSitePermissionsBridge_ClearRevokedPermissionsReviewList(
+    JNIEnv* env,
+    Profile* profile) {
+  ClearRevokedPermissionsReviewList(profile);
+}
+
+static void JNI_UnusedSitePermissionsBridge_RestoreRevokedPermissionsReviewList(
+    JNIEnv* env,
+    Profile* profile,
+    std::vector<PermissionsData>& permissions_data_list) {
+  RestoreRevokedPermissionsReviewList(profile, permissions_data_list);
 }
