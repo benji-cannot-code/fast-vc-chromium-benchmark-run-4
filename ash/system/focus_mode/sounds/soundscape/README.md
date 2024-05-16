049f7015eb9694c6631e61bc41c4c8fba58d868c
Sean Kau <skau@chromium.org>
2024-05-16 18:38:09 +0000

    Parse the Focus Mode Sound config JSON
    Converts a string into domain objects as long as all the expected fields
    are present.
    
    Bug: 331837970
    Change-Id: I68fcedce3ec06e8ef753c57c0d8d096c125e66d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512326
    Commit-Queue: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302112}

---
+++ ash/system/focus_mode/sounds/soundscape/README.md
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Focus Sounds
+
+Soundscapes is the internal name for Focus Sounds. Soundscapes are the
+playlists that are only used by Focus Mode and independent of the other
+sound options in Focus Mode.
+
+This folder contains code relevant to downloading and parsing the data for
+this backend.
---
+++ ash/system/focus_mode/sounds/soundscape/test/test_data.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_FOCUS_MODE_SOUNDS_SOUNDSCAPE_TEST_TEST_DATA_H_
+#define ASH_SYSTEM_FOCUS_MODE_SOUNDS_SOUNDSCAPE_TEST_TEST_DATA_H_
+
+namespace ash {
+
+extern const char kTestConfig[];
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_FOCUS_MODE_SOUNDS_SOUNDSCAPE_TEST_TEST_DATA_H_
