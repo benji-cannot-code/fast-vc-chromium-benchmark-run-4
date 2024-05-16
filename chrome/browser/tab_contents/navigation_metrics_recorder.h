60c00bfc7b747dff621b0213aac3d7c536d746bf
Yaron Friedman <yfriedman@google.com>
2024-05-16 20:11:41 +0000

    Remove synthetic fieldtrials for SiteIsolation and Origin-Agent-Cluster.
    These were used for the original experiments on Android and aren't required anymore.
    
    Bug: 341054011
    Change-Id: I1b20ca226bec64335f5384b9f76a7db208015f87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544879
    Commit-Queue: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302181}

--- chrome/browser/tab_contents/navigation_metrics_recorder.h
+++ chrome/browser/tab_contents/navigation_metrics_recorder.h
@@ -37,12 +37,6 @@ class NavigationMetricsRecorder
       delete;
 
   ~NavigationMetricsRecorder() override;
-
-  // Turns on a synthetic field trial that is set on any navigation to a site
-  // that requires a dedicated process.  Normally this is only used on Android,
-  // but tests on other platforms can also turn this behavior on.
-  void EnableSiteIsolationSyntheticTrialForTesting();
-
  private:
   explicit NavigationMetricsRecorder(content::WebContents* web_contents);
   friend class content::WebContentsUserData<NavigationMetricsRecorder>;
@@ -55,7 +49,6 @@ class NavigationMetricsRecorder
 
   raw_ptr<site_engagement::SiteEngagementService> site_engagement_service_;
   scoped_refptr<content_settings::CookieSettings> cookie_settings_;
-  bool is_synthetic_isolation_trial_enabled_;
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
