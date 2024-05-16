c390e968764c778100716ac9dd6ed0daaffc2e50
Sun Yueru <yrsun@chromium.org>
2024-05-16 14:10:18 +0000

    Remove some unused DEPS in components/ukm
    Change-Id: I8c60185526c60fd88e0e228445a91676d950d6de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538691
    Auto-Submit: Sun Yueru <yrsun@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301928}

--- components/ukm/content/source_url_recorder_browsertest.cc
+++ components/ukm/content/source_url_recorder_browsertest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/browser/shell_download_manager_delegate.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source.h"
-#include "third_party/blink/public/common/features.h"
 
 class SourceUrlRecorderWebContentsObserverBrowserTest
     : public content::ContentBrowserTest {
