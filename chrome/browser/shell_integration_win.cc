045608724b2953fdcc645d7d10cde46b5856f58b
Junhao Huang (from Dev Box) <junhaohuang@microsoft.com>
2024-05-16 01:57:23 +0000

    win: Improve HTTP scheme default program query
    This change updates the implementation of GetHttpSchemeUserChoiceProgId
    to use the IApplicationAssociationRegistration interface, improving the
    reliability and accuracy of retrieving the user's default program for
    handling HTTP schemes on Windows.
    
    The new implementation uses the SHCreateAssociationRegistration and
    QueryCurrentDefault API calls, which are the recommended methods for
    querying default applications on Windows. These methods provide a more
    robust and system-supported way of determining default associations.
    
    Bug: 336681968
    Change-Id: Ic9442130132c0c48c8e4ed9f5d82a249cdfabd3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5487056
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Commit-Queue: Junhao Huang <junhaohuang@microsoft.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301710}

--- chrome/browser/shell_integration_win.cc
+++ chrome/browser/shell_integration_win.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "base/win/registry.h"
+#include "base/win/scoped_co_mem.h"
 #include "base/win/scoped_propvariant.h"
 #include "base/win/shlwapi.h"
 #include "base/win/shortcut.h"
@@ -631,12 +632,19 @@ void MigrateChromeAndChromeProxyShortcuts(
 }
 
 std::wstring GetHttpSchemeUserChoiceProgId() {
-  std::wstring prog_id;
-  base::win::RegKey key(HKEY_CURRENT_USER, ShellUtil::kRegVistaUrlPrefs,
-                        KEY_QUERY_VALUE);
-  if (key.Valid())
-    key.ReadValue(L"ProgId", &prog_id);
-  return prog_id;
+  Microsoft::WRL::ComPtr<IApplicationAssociationRegistration> registration;
+  HRESULT hr = ::SHCreateAssociationRegistration(IID_PPV_ARGS(&registration));
+  if (FAILED(hr)) {
+    return std::wstring();
+  }
+
+  base::win::ScopedCoMem<wchar_t> prog_id;
+  hr = registration->QueryCurrentDefault(L"http", AT_URLPROTOCOL, AL_EFFECTIVE,
+                                         &prog_id);
+  if (FAILED(hr)) {
+    return std::wstring();
+  }
+  return prog_id.get();
 }
 
 }  // namespace
