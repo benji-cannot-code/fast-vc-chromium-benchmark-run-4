b301dea344f8af0925b5b7e7cde79a38b726e88d
Adrian Taylor <adetaylor@chromium.org>
2024-05-16 16:02:27 +0000

    in_process_fuzzers: single process mode libfuzzer
    This is the base class for various large complex fuzzers based on the
    browser_test framework which involves the real (ish) renderer and
    browser processes.
    
    However, we ideally want to run them in a single process, so that
    fuzzing engines can gather coverage on all the code explored (and,
    perhaps more importantly, because firing up an additional renderer
    process for each test case is very slow).
    
    This hasn't been possible with libfuzzer until now because the renderer
    code involves unloading at least one shared object, the vulkan library
    used by Swiftshader. Unloading shared objects doesn't work with
    libfuzzer; the coverage bitmaps for the shared object gets freed, but
    code continues to write where they used to be.
    
    This CL introduces a shim which prevents us from running dlclose() in
    libfuzzer builds.
    
    Cq-Include-Trybots: luci.chromium.try:linux-centipede-asan-rel
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel
    Cq-Include-Trybots: luci.chromium.try:win-libfuzzer-asan-rel
    Bug: 40051117
    Change-Id: Ia61845342e9f0369574ac17fe79e53796e80127a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542427
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Jonathan Metzman <metzman@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1301988}

---
+++ build/sanitizers/dlcloseshim.c
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// In due course we may need to replicate more of the complexity from
+//  base/allocator/partition_allocator/src/partition_alloc/
+//  shim/allocator_shim_internals.h
+// but as we're targeting just libfuzzer Linux builds, perhaps we don't need
+// it.
+
+__attribute__((visibility("default"), noinline)) void __wrap_dlclose(void *handle) {
+  // Do nothing. We don't want to call the real dlclose on libfuzzer builds.
+}
