7bbee912ac4fc18705cea41874ec6c4c7ae1acde
Wes Okuhara <wesokuhara@google.com>
2024-05-16 22:50:11 +0000

    CrOS Settings: Pref object is immutable data in V2 control components
    The `pref` property in components using PrefControlMixinInternal should
    not be modified directly. The pref object's value should only be
    updated by the `user-action-setting-pref-change` event which avoids
    Polymer's native two-way databinding. Once the event is handled and the
    pref is updated in the prefs state, the control components will receive
    the updated pref object via downward data flow.
    
    Bug: b:340670731, b:333453826
    Test: browser_tests --gtest_filter="*Settings*V2*"
    Change-Id: I6b3af1a956d55e4714dee7cc465889ee8dc82a44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540278
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1302281}

--- chrome/test/data/webui/chromeos/settings/controls/v2/pref_control_mixin_internal_test.ts
+++ chrome/test/data/webui/chromeos/settings/controls/v2/pref_control_mixin_internal_test.ts
@@ -20,8 +20,8 @@ suite('PrefControlMixinInternal', () => {
 
   const fakePrefObject = {
     key: 'foo.bar.baz',
-    type: chrome.settingsPrivate.PrefType.BOOLEAN,
-    value: false,
+    type: chrome.settingsPrivate.PrefType.STRING,
+    value: 'initialValue',
   };
 
   setup(async () => {
@@ -121,11 +121,11 @@ suite('PrefControlMixinInternal', () => {
 
       assertThrows(() => {
         testElement.validatePref();
-      }, 'TEST-ELEMENT#exampleID error: Invalid pref type BOOLEAN.');
+      }, 'TEST-ELEMENT#exampleID error: Invalid pref type STRING.');
     });
 
     test('pref type is validated', () => {
-      testElement.validPrefTypes = [chrome.settingsPrivate.PrefType.BOOLEAN];
+      testElement.validPrefTypes = [chrome.settingsPrivate.PrefType.STRING];
       testElement.pref = {...fakePrefObject};
 
       testElement.validatePref();
@@ -150,13 +150,6 @@ suite('PrefControlMixinInternal', () => {
       }, 'updatePrefValueFromUserAction() requires pref to be defined.');
     });
 
-    test('Updates the local pref value', () => {
-      testElement.pref = {...fakePrefObject};
-      const expectedValue = 'newValue9001';
-      testElement.updatePrefValueFromUserAction(expectedValue);
-      assertEquals(expectedValue, testElement.pref.value);
-    });
-
     test('Dispatches a "user-action-setting-pref-change" event', async () => {
       testElement.pref = {...fakePrefObject};
 
@@ -168,6 +161,17 @@ suite('PrefControlMixinInternal', () => {
       const event = await eventPromise;
       assertEquals(fakePrefObject.key, event.detail.prefKey);
       assertEquals(expectedValue, event.detail.value);
+    });
+
+    test('Does not update the pref object value directly', async () => {
+      testElement.pref = {...fakePrefObject};
+      const initialValue = testElement.pref.value;
+
+      const eventPromise =
+          eventToPromise('user-action-setting-pref-change', window);
+      testElement.updatePrefValueFromUserAction('newValue9001');
+      await eventPromise;
+      assertEquals(initialValue, testElement.pref.value);
     });
   });
 });
--- chrome/test/data/webui/chromeos/settings/controls/v2/settings_dropdown_v2_test.ts
+++ chrome/test/data/webui/chromeos/settings/controls/v2/settings_dropdown_v2_test.ts
@@ -12,7 +12,7 @@ import {eventToPromise, isVisible} from 'chrome://webui-test/test_util.js';
 
 import {clearBody} from '../../utils.js';
 
-suite('<settings-dropdown-v2>', () => {
+suite(SettingsDropdownV2Element.is, () => {
   let dropdownElement: SettingsDropdownV2Element;
   let internalSelectElement: HTMLSelectElement;
 
@@ -240,15 +240,6 @@ suite('<settings-dropdown-v2>', () => {
       assertTrue(isVisible(policyIndicator));
     });
 
-    test('Selecting an option updates local pref value', () => {
-      for (const testOption of testOptions) {
-        const value = testOption.value;
-        simulateSelectAction(value);
-        assertOptionSelected(value);
-        assertEquals(value, dropdownElement.pref!.value);
-      }
-    });
-
     test('Selecting an option dispatches pref change event', async () => {
       for (const testOption of testOptions) {
         const prefChangeEventPromise =
@@ -262,6 +253,23 @@ suite('<settings-dropdown-v2>', () => {
         assertEquals(value, event.detail.value);
       }
     });
+
+    test(
+        'Selecting an option does not update the pref value directly',
+        async () => {
+          const initialPrefValue = dropdownElement.pref!.value;
+
+          const prefChangeEventPromise =
+              eventToPromise('user-action-setting-pref-change', window);
+          const value = testOptions[0]!.value;
+          simulateSelectAction(value);
+          assertOptionSelected(value);
+          await prefChangeEventPromise;
+
+          // Local pref object should be treated as immutable data and should
+          // not be updated directly.
+          assertEquals(initialPrefValue, dropdownElement.pref!.value);
+        });
 
     test('Selecting an option dispatches change event', async () => {
       for (const testOption of testOptions) {
