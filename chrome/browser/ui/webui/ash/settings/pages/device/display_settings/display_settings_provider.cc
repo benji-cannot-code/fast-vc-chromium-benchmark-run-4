e0a02f9f8f118b195100f67adfd469d95bb06c78
Cam Bickel <cambickel@google.com>
2024-05-17 22:38:36 +0000

    brightness: Only send brightness slider metrics after short delay (2s)
    The implementation is similar to this CL: https://crrev.com/c/4266370
    
    Test: ash_unittests --gtest_filter=*BrightnessControllerChromeosTest*
    Test: Tested on my DUT
    Bug: b/328287584
    Change-Id: Icf35b90f5b37b0c7299a6d994e76d2c7dbaff027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545840
    Reviewed-by: Wenyu Zhang <zhangwenyu@google.com>
    Commit-Queue: Camden Bickel <cambickel@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302895}

--- chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.cc
+++ chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/strings/strcat.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/ui/webui/ash/settings/pages/device/display_settings/display_settings_provider.mojom.h"
 #include "chromeos/dbus/power_manager/backlight.pb.h"
 #include "ui/display/manager/display_manager.h"
@@ -36,6 +38,11 @@ constexpr int kUserOverrideDisplaySettingsTimeDeltaBucketCount = 100;
 // The time threshold whether user override display settings metrics would be
 // fired or not.
 constexpr int kUserOverrideDisplaySettingsTimeThresholdInMinute = 60;
+
+// The interval of the timer that records the brightness slider adjusted event.
+// Multiple changes to the brightness percentage will not be recorded until
+// after this interval elapses.
+constexpr base::TimeDelta kMetricsDelayTimerInterval = base::Seconds(2);
 
 // Get UMA histogram name that records the time elapsed between users changing
 // the display settings and the display is connected.
@@ -59,7 +66,12 @@ const std::string GetUserOverrideDefaultSettingsHistogramName(
 
 }  // namespace
 
-DisplaySettingsProvider::DisplaySettingsProvider() {
+DisplaySettingsProvider::DisplaySettingsProvider()
+    : brightness_slider_metric_delay_timer_(
+          FROM_HERE,
+          kMetricsDelayTimerInterval,
+          this,
+          &DisplaySettingsProvider::RecordBrightnessSliderAdjusted) {
   if (Shell::HasInstance()) {
     shell_observation_.Observe(ash::Shell::Get());
   }
@@ -361,10 +373,16 @@ void DisplaySettingsProvider::SetInternalDisplayScreenBrightness(
       percent, /*gradual=*/true, /*source=*/
       BrightnessControlDelegate::BrightnessChangeSource::kSettingsApp);
 
+  last_set_brightness_percent_ = percent;
+  // Start or reset timer for recording to metrics.
+  brightness_slider_metric_delay_timer_.Reset();
+}
+
+void DisplaySettingsProvider::RecordBrightnessSliderAdjusted() {
   // Record the brightness change event.
   std::string histogram_name(base::StrCat(
       {kDisplaySettingsHistogramName, ".Internal.BrightnessSliderAdjusted"}));
-  base::UmaHistogramPercentage(histogram_name, percent);
+  base::UmaHistogramPercentage(histogram_name, last_set_brightness_percent_);
 }
 
 void DisplaySettingsProvider::SetInternalDisplayAmbientLightSensorEnabled(
