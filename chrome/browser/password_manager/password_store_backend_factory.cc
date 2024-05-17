a9751b662f16a30575ea84404aabbe839a0402e3
Theo Cristea <theocristea@google.com>
2024-05-17 13:49:38 +0000

    Reland "Add prefs to monitor whether a user has autofillable credentials stored"
    This is a reland of commit e4356e4960bd0f872e6650d6968d2f770e6b2c5f
    
    The dangling pointer is caused by the `prefs` argument of the callbacks.
    It can be indeed dangling, but it is never dereferenced when dangling.
    
    Added an intermediate callback to solve this issue. The intermediate
    callback only has a weak pointer to the backend as an argument and
    calls the final callback (the one which actually sets the prefs) only
    if the weak pointer to the backend is still valid.
    
    This makes sure that no callback is called with a danlging `prefs`
    argument.
    
    Original change's description:
    > Add prefs to monitor whether a user has autofillable credentials stored
    >
    > The added prefs are:
    > `kAutofillableCredentialsProfileStoreLoginDatabase`,
    > `kAutofillableCredentialsAccountStoreLoginDatabase`.
    > Their value is true if the user has any autofillable password entries
    > saved. These two prefs exist only on Desktop.
    >
    > The use of these prefs is to know whether the code should allow the user
    > to trigger password suggestions from the context menu (if the user
    > doesn't have any autofillable credentials, then they shouldn't be
    > allowed to trigger suggestions). The context menu logic will be added in
    > a separate CL.
    >
    > Bug: 321678141
    > Change-Id: Ie0c0da9bf42c768069ed544f0638c02c71b779aa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522541
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Theo Cristea <theocristea@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1300534}
    
    Bug: 321678141
    Change-Id: I7a30d7a34698e71763878145be69ad566fe3f46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538178
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302535}

--- chrome/browser/password_manager/password_store_backend_factory.cc
+++ chrome/browser/password_manager/password_store_backend_factory.cc
@@ -148,12 +148,18 @@ std::unique_ptr<PasswordStoreBackend> CreateProfilePasswordStoreBackend(
 #endif  // !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
 
   auto is_profile_db_empty_cb =
-#if BUILDFLAG(IS_ANDROID)
       base::BindPostTaskToCurrentDefault(base::BindRepeating(
-          &password_manager::SetEmptyStorePref, prefs, backend->AsWeakPtr(),
-          password_manager::prefs::kEmptyProfileStoreLoginDatabase));
+          &password_manager::IntermediateCallbackForSettingPrefs,
+          backend->AsWeakPtr(),
+#if BUILDFLAG(IS_ANDROID)
+          base::BindRepeating(
+              &password_manager::SetEmptyStorePref, prefs,
+              password_manager::prefs::kEmptyProfileStoreLoginDatabase)));
 #else
-      base::NullCallback();
+          base::BindRepeating(
+              &password_manager::SetAutofillableCredentialsStorePref, prefs,
+              password_manager::prefs::
+                  kAutofillableCredentialsProfileStoreLoginDatabase)));
 #endif
   login_db_ptr->SetIsEmptyCb(std::move(is_profile_db_empty_cb));
 
@@ -169,6 +175,9 @@ std::unique_ptr<PasswordStoreBackend> CreateAccountPasswordStoreBackend(
       password_manager::CreateLoginDatabaseForAccountStorage(
           login_db_directory));
   std::unique_ptr<PasswordStoreBackend> backend;
+#if !BUILDFLAG(IS_ANDROID)
+  password_manager::LoginDatabase* login_db_ptr = login_db.get();
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 #if BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
   backend = std::make_unique<PasswordStoreBuiltInBackend>(
@@ -194,5 +203,18 @@ std::unique_ptr<PasswordStoreBackend> CreateAccountPasswordStoreBackend(
           prefs, /*password_affiliation_adapter=*/nullptr,
           password_manager::kAccountStore));
 #endif
+
+#if !BUILDFLAG(IS_ANDROID)
+  auto is_account_db_empty_cb =
+      base::BindPostTaskToCurrentDefault(base::BindRepeating(
+          &password_manager::IntermediateCallbackForSettingPrefs,
+          backend->AsWeakPtr(),
+          base::BindRepeating(
+              &password_manager::SetAutofillableCredentialsStorePref, prefs,
+              password_manager::prefs::
+                  kAutofillableCredentialsAccountStoreLoginDatabase)));
+  login_db_ptr->SetIsEmptyCb(std::move(is_account_db_empty_cb));
+#endif  // !BUILDFLAG(IS_ANDROID)
+
   return backend;
 }
