dbb17fcedaec276d9e4af5ba5901febb4db9b766
Lijin Shen <lazzzis@google.com>
2024-05-17 01:01:16 +0000

    [Garden] Disable StatusBarColorControllerTest.testStatusBarColorNtp
    Bug: 341157444
    Change-Id: Ia1835d2609d09b115788565c91f535a71076936a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546679
    Reviewed-by: Charles Hager <clhager@google.com>
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Owners-Override: Lijin Shen <lazzzis@google.com>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1302352}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ui/system/StatusBarColorControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ui/system/StatusBarColorControllerTest.java
@@ -35,6 +35,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -209,6 +210,7 @@ public class StatusBarColorControllerTest {
     @Feature({"StatusBar"})
     @DisableFeatures({ChromeFeatureList.SURFACE_POLISH})
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE}) // Status bar is always black on tablets
+    @DisabledTest(message = "https://issues.chromium.org/issues/341157444")
     public void testStatusBarColorNtp() throws Exception {
         ChromeTabbedActivity activity = sActivityTestRule.getActivity();
         final int expectedStandardColor = ChromeColors.getDefaultThemeColor(activity, false);
