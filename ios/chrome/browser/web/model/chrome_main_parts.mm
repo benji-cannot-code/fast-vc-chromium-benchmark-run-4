1eef90a98b2ea1eb40597092e6796b1c987373e1
Francois Doray <fdoray@chromium.org>
2024-05-17 19:40:01 +0000

    Initialize //base features on iOS.
    //base has optimizations gated by global variables which are
    initialized in base::features::Init() based on the base::FeatureList.
    
    This CL adds a call to base::features::Init() on iOS, immediately
    after base::FeatureList creation. This will allow assessing the impact
    of *future* optimizations on iOS via field experiments (note: since
    there is currently no field experiment targeting iOS which affects the
    features read by base::features::Init(), landing this CL should be
    a no-op).
    
    Change-Id: If4678e5202ebd8e0482714b1f09c835c55e4602b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5427214
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302800}

--- ios/chrome/browser/web/model/chrome_main_parts.mm
+++ ios/chrome/browser/web/model/chrome_main_parts.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/allocator/partition_alloc_support.h"
 #import "base/check_op.h"
 #import "base/feature_list.h"
+#import "base/features.h"
 #import "base/files/file_path.h"
 #import "base/ios/ios_util.h"
 #import "base/memory/ptr_util.h"
@@ -235,6 +236,11 @@ void IOSChromeMainParts::PreCreateThreads() {
   // initialization which happens in BrowserProcess:PreCreateThreads. Metrics
   // initialization is handled in PreMainMessageLoopRun since it posts tasks.
   SetUpFieldTrials(command_line_variation_ids);
+
+  // Initialize //base features that depend on the `FeatureList`. Don't force
+  // emitting profiler metadata since the profiler doesn't run on iOS.
+  base::features::Init(
+      base::features::EmitThreadControllerProfilerMetadata::kFeatureDependent);
 
   // Set metrics upload for stack/heap profiles.
   IOSThreadProfiler::SetBrowserProcessReceiverCallback(base::BindRepeating(
