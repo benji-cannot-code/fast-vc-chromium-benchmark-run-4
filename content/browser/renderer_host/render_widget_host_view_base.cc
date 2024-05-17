c549e6d50cb225f9d34dcff9df0dd532fcb6acd2
Frank Liberato <liberato@chromium.org>
2024-05-17 00:26:53 +0000

    [pip] Show capture media picker in pip if it's focused
    If `getDisplayMedia()` is called on a pip window's opener, but
    the pip window currently has the focus, then show the UI in the
    pip window instead.  It's common to open the stream in the opener
    so that its lifetime isn't tied to the pip window, but it can be
    confusing if the UI shows up in the opener while the user is
    interacting with the pip window.
    
    Bug: 328673345, 40618765
    Change-Id: I436d9d04d29b5998d59227e0e64776f99f4c213f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536096
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Frank Liberato <liberato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302339}

--- content/browser/renderer_host/render_widget_host_view_base.cc
+++ content/browser/renderer_host/render_widget_host_view_base.cc
@@ -1196,4 +1196,8 @@ void RenderWidgetHostViewBase::SetViewTransitionResources(
   view_transition_resources_ = std::move(resources);
 }
 
+bool RenderWidgetHostViewBase::IsTestRenderWidgetHostView() const {
+  return false;
+}
+
 }  // namespace content
