e49286f8dc046bf04e5673634b9710329da430dd
Ken Buchanan <kenrb@chromium.org>
2024-05-17 16:15:40 +0000

    [WebAuthn] Add browser test for declining GPM passkey bootstrapping
    This adds a test for the change in https://crrev.com/1299568.
    
    Change-Id: If7f307fd0bd2b9eabb3713b53008f28fec67c669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544819
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302630}

--- chrome/browser/webauthn/test_util.h
+++ chrome/browser/webauthn/test_util.h
@@ -6,11 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEBAUTHN_TEST_UTIL_H_
 #define CHROME_BROWSER_WEBAUTHN_TEST_UTIL_H_
 
+#include <memory>
 #include <utility>
 
 #include "base/files/file_path.h"
 #include "base/process/process.h"
+#include "base/time/time.h"
 #include "device/fido/enclave/constants.h"
+
+namespace device::cablev2 {
+struct Pairing;
+}
 
 // Starts a test enclave service instance. Returns the Process instance and
 // the port number on which the enclave is listening for connections.
@@ -19,5 +25,10 @@ std::pair<base::Process, uint16_t> StartWebAuthnEnclave(base::FilePath cwd);
 
 device::enclave::ScopedEnclaveOverride TestWebAuthnEnclaveIdentity(
     uint16_t port);
+
+std::unique_ptr<device::cablev2::Pairing> TestPhone(const char* name,
+                                                    uint8_t public_key,
+                                                    base::Time last_updated,
+                                                    int channel_priority);
 
 #endif  // CHROME_BROWSER_WEBAUTHN_TEST_UTIL_H_
