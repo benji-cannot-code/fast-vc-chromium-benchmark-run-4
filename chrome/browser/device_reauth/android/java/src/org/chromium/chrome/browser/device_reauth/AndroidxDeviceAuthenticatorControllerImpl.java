627c2d214f944b6e0705f4c426a87ea8fc20a8b3
Anna Tsvirchkova <atsvirchkova@google.com>
2024-05-17 14:00:39 +0000

    [Reauth] Introduce the feature flag for biometric reauth refactoring
    This CL is a preparation for changing the lib from
    android.hardware.biometrics to androidx.biometric. The CL introduces the feature flag for this refactoring and a new controller which is going to implement the functionality using the new lib.
    
    Android platform feature (biometric) which can't be accessed in tests.
    
    Bug: 340473460
    Low-Coverage-Reason: The code introduced, wraps the communication with
    Change-Id: Id29a68c756af781a91e5b77641d5616a02a2a13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538224
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302541}

---
+++ chrome/browser/device_reauth/android/java/src/org/chromium/chrome/browser/device_reauth/AndroidxDeviceAuthenticatorControllerImpl.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.device_reauth;
+
+class AndroidxDeviceAuthenticatorControllerImpl implements DeviceAuthenticatorController {
+
+    @Override
+    public @BiometricsAvailability int canAuthenticateWithBiometric() {
+        // TODO(crbug.com/340473460): Implement.
+        return BiometricsAvailability.NOT_ENROLLED;
+    }
+
+    @Override
+    public boolean canAuthenticateWithBiometricOrScreenLock() {
+        // TODO(crbug.com/340473460): Implement.
+        return false;
+    }
+
+    @Override
+    public void authenticate() {
+        // TODO(crbug.com/340473460): Implement.
+    }
+
+    @Override
+    public void cancel() {
+        // TODO(crbug.com/340473460): Implement.
+    }
+}
