f54a67446bced8f64a164c8725665308721d6c5e
Sophie Chang <sophiechang@chromium.org>
2024-05-17 22:42:32 +0000

    Move pref for whether on-device feature has been used to eligibility check
    This also removes a redundant feature check since the class isnt created when the feature is turned off.
    
    Moving the pref update here since this shows an interest in using the on-device APIs which might be used as an eligibility check before a feature actually uses it
    
    Change-Id: I00727e88b847abe261cfe152f5402998743dec2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547948
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1302898}

--- components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
+++ components/optimization_guide/core/model_execution/on_device_model_service_controller.cc
@@ -75,9 +75,8 @@ void OnDeviceModelServiceController::Init() {
 
 OnDeviceModelEligibilityReason OnDeviceModelServiceController::CanCreateSession(
     ModelBasedCapabilityKey feature) {
-  if (!base::FeatureList::IsEnabled(
-          features::kOptimizationGuideOnDeviceModel)) {
-    return OnDeviceModelEligibilityReason::kFeatureNotEnabled;
+  if (on_device_component_state_manager_) {
+    on_device_component_state_manager_->OnDeviceEligibleFeatureUsed();
   }
 
   if (!model_metadata_) {
@@ -129,10 +128,6 @@ OnDeviceModelServiceController::CreateSession(
     base::WeakPtr<ModelQualityLogsUploaderService>
         model_quality_uploader_service,
     const std::optional<SessionConfigParams>& config_params) {
-  if (on_device_component_state_manager_) {
-    on_device_component_state_manager_->OnDeviceEligibleFeatureUsed();
-  }
-
   OnDeviceModelEligibilityReason reason = CanCreateSession(feature);
   CHECK_NE(reason, OnDeviceModelEligibilityReason::kUnknown);
   base::UmaHistogramEnumeration(
