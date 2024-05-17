7c0b7a44ebe0535446deaadeb0e1f75177d6bf06
Alex Ilin <alexilin@chromium.org>
2024-05-17 13:14:55 +0000

    [DBSC] Bug fix for refresh_url in the assertion token
    Update BoundSessionRefreshCookieFetcherImpl to pass a correct refresh
    URL to GenerateBindingKeyAssertion().
    
    Bug: b/325440849
    Change-Id: Ibdb2bfe10c79c85f09fd84c6e72baa23481b04f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545443
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1302524}

--- chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl.cc
+++ chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl.cc
@@ -152,9 +152,7 @@ void BoundSessionRefreshCookieFetcherImpl::StartRefreshRequest(
         })");
 
   auto request = std::make_unique<network::ResourceRequest>();
-  request->url = !refresh_url_.is_empty()
-                     ? refresh_url_
-                     : GaiaUrls::GetInstance()->rotate_bound_cookies_url();
+  request->url = GetRefreshUrl();
   request->method = "GET";
 
   if (sec_session_challenge_response) {
@@ -348,7 +346,7 @@ void BoundSessionRefreshCookieFetcherImpl::RefreshWithChallenge(
               "BoundSessionRefreshCookieFetcherImpl::RefreshWithChallenge",
               perfetto::Flow::FromPointer(this));
   session_binding_helper_->GenerateBindingKeyAssertion(
-      challenge, GaiaUrls::GetInstance()->rotate_bound_cookies_url(),
+      challenge, GetRefreshUrl(),
       base::BindOnce(
           &BoundSessionRefreshCookieFetcherImpl::OnGenerateBindingKeyAssertion,
           weak_ptr_factory_.GetWeakPtr(), base::ElapsedTimer()));
@@ -408,4 +406,10 @@ void BoundSessionRefreshCookieFetcherImpl::OnCookiesAccessed(
 void BoundSessionRefreshCookieFetcherImpl::Clone(
     mojo::PendingReceiver<network::mojom::CookieAccessObserver> observer) {
   cookie_observers_.Add(this, std::move(observer));
+}
+
+const GURL& BoundSessionRefreshCookieFetcherImpl::GetRefreshUrl() {
+  return !refresh_url_.is_empty()
+             ? refresh_url_
+             : GaiaUrls::GetInstance()->rotate_bound_cookies_url();
 }
--- chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl_unittest.cc
+++ chrome/browser/signin/bound_session_credentials/bound_session_refresh_cookie_fetcher_impl_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/protobuf_matchers.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
+#include "base/test/values_test_util.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/signin/bound_session_credentials/bound_session_params_util.h"
@@ -62,6 +63,24 @@ constexpr char kSessionId[] = "session_id";
 constexpr char kChallenge[] = "aGVsbG8_d29ybGQ";
 constexpr net::Error kConnectionNetError = net::ERR_UNEXPECTED;
 
+MATCHER_P3(JwtHasExpectedFields, session_id, challenge, destination_url, "") {
+  std::string_view jwt = arg;
+  std::optional<base::Value::Dict> payload_dict =
+      signin::ExtractPayloadFromJwt(jwt);
+  if (!payload_dict) {
+    *result_listener << "Couldn't parse payload from JWT: " << jwt;
+    return false;
+  }
+
+  *result_listener << " with payload " << payload_dict->DebugString();
+  return testing::ExplainMatchResult(
+      base::test::DictionaryHasValues(base::Value::Dict()
+                                          .Set("sub", session_id)
+                                          .Set("jti", challenge)
+                                          .Set("aud", destination_url.spec())),
+      *payload_dict, result_listener);
+}
+
 UnexportableKeyId GenerateNewKey(
     UnexportableKeyService& unexportable_key_service) {
   base::test::TestFuture<ServiceErrorOr<UnexportableKeyId>> generate_future;
@@ -72,28 +91,6 @@ UnexportableKeyId GenerateNewKey(
   ServiceErrorOr<UnexportableKeyId> key_id = generate_future.Get();
   CHECK(key_id.has_value());
   return *key_id;
-}
-
-std::string GetChallengeFromJwt(std::string_view jwt) {
-  std::vector<std::string_view> parts = base::SplitStringPiece(
-      jwt, ".", base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
-
-  if (parts.size() != 3) {
-    return std::string();
-  }
-
-  std::string payload;
-  if (!base::Base64UrlDecode(
-          parts[1], base::Base64UrlDecodePolicy::DISALLOW_PADDING, &payload)) {
-    return std::string();
-  }
-  std::optional<base::Value::Dict> payload_dict =
-      base::JSONReader::ReadDict(payload);
-  if (!payload_dict) {
-    return std::string();
-  }
-  std::string* challenge = payload_dict->FindString("jti");
-  return challenge ? *challenge : std::string();
 }
 
 std::string CreateChallengeHeaderValue(const std::string& challenge) {
@@ -250,7 +247,8 @@ TEST_F(BoundSessionRefreshCookieFetcherImplTest, SuccessExpectedCookieSet) {
                         /*expect_assertion_was_generated_count=*/0);
 }
 
-TEST_F(BoundSessionRefreshCookieFetcherImplTest, SuccessNonEmptyRefreshUrl) {
+TEST_F(BoundSessionRefreshCookieFetcherImplTest,
+       SuccessWithChallengeNonEmptyRefreshUrl) {
   const GURL refresh_url("https://security.google.com/CheckBinding");
   fetcher_ = std::make_unique<BoundSessionRefreshCookieFetcherImpl>(
       test_url_loader_factory_.GetSafeWeakWrapper(), *session_binding_helper_,
@@ -261,11 +259,24 @@ TEST_F(BoundSessionRefreshCookieFetcherImplTest, SuccessNonEmptyRefreshUrl) {
   RefreshTestFuture future;
   fetcher_->Start(future.GetCallback());
 
-  EXPECT_EQ(test_url_loader_factory_.total_requests(), 1u);
+  SimulateChallengeRequired(CreateChallengeHeaderValue(kChallenge));
+  task_environment_.RunUntilIdle();
+  EXPECT_FALSE(future.IsReady());
+  EXPECT_EQ(test_url_loader_factory_.NumPending(), 1);
   network::TestURLLoaderFactory::PendingRequest* pending_request =
       test_url_loader_factory_.GetPendingRequest(0);
   EXPECT_EQ(pending_request->request.url, refresh_url);
+  auto headers = pending_request->request.headers;
+  std::string assertion;
+  EXPECT_TRUE(headers.GetHeader("Sec-Session-Google-Response", &assertion));
 
+  EXPECT_TRUE(signin::VerifyJwtSignature(
+      assertion, *unexportable_key_service_.GetAlgorithm(binding_key_id_),
+      *unexportable_key_service_.GetSubjectPublicKeyInfo(binding_key_id_)));
+  EXPECT_THAT(assertion,
+              JwtHasExpectedFields(kSessionId, kChallenge, refresh_url));
+
+  // Set required cookies and complete the request.
   SimulateOnCookiesAccessed(network::mojom::CookieAccessDetails::Type::kChange);
   test_url_loader_factory_.SimulateResponseForPendingRequest(
       pending_request->request.url.spec(), "");
@@ -273,7 +284,7 @@ TEST_F(BoundSessionRefreshCookieFetcherImplTest, SuccessNonEmptyRefreshUrl) {
   EXPECT_TRUE(future.IsReady());
   EXPECT_EQ(future.Get(), Result::kSuccess);
   VerifyMetricsRecorded(Result::kSuccess,
-                        /*expect_assertion_was_generated_count=*/0);
+                        /*expect_assertion_was_generated_count=*/1);
 }
 
 TEST_F(BoundSessionRefreshCookieFetcherImplTest,
@@ -453,7 +464,10 @@ TEST_F(BoundSessionRefreshCookieFetcherImplTest, ChallengeRequired) {
   EXPECT_TRUE(signin::VerifyJwtSignature(
       assertion, *unexportable_key_service_.GetAlgorithm(binding_key_id_),
       *unexportable_key_service_.GetSubjectPublicKeyInfo(binding_key_id_)));
-  EXPECT_EQ(GetChallengeFromJwt(assertion), kChallenge);
+  EXPECT_THAT(assertion,
+              JwtHasExpectedFields(
+                  kSessionId, kChallenge,
+                  GURL("https://accounts.google.com/RotateBoundCookies")));
 
   // Set required cookies and complete the request.
   SimulateOnCookiesAccessed(network::mojom::CookieAccessDetails::Type::kChange);
