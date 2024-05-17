eef3d6c63b2c50b0eec98f2e2f81d20972055e25
Tomasz Wiszkowski <ender@google.com>
2024-05-17 16:48:14 +0000

    Retire Adaptive Suggestions Java implementation
    With Native implementation deployed, this change simply cleans up the
    Java code :).
    
    Change-Id: I3eba143806e707471b3f0c01a502fbe7a20d652e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540420
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302655}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediator.java
@@ -1150,8 +1150,6 @@ class AutocompleteMediator
         // addresses cases where hardware keyboard is attached to a device, or where user explicitly
         // called the keyboard back after we hid it.
         if (mDelegate.isKeyboardActive()) {
-            mDropdownViewInfoListBuilder.setDropdownHeightWithKeyboardActive(newHeight);
-
             if (!mNativeInitialized || mAutocomplete == null) return;
 
             int suggestionHeight =
