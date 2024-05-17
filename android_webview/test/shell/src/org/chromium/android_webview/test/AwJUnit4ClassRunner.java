07ffe736acdfe54626cc9e40734db296658fa128
Zhaoyang Li <zhaoyangli@chromium.org>
2024-05-17 18:43:51 +0000

    Add a flag to enable only running WV multiprocess mode tests
    This will be useful when we want to execute only multiprocess mode
    tests on some builders.
    
    Bug: b/340651691
    Change-Id: I548d4e6dc688bc037e4f9300a50ed1530bf85b5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538986
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Zhaoyang Li <zhaoyangli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302738}

--- android_webview/test/shell/src/org/chromium/android_webview/test/AwJUnit4ClassRunner.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/AwJUnit4ClassRunner.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.android_webview.test;
 
 import androidx.annotation.CallSuper;
+import androidx.test.InstrumentationRegistry;
 
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
@@ -32,6 +33,9 @@ import java.util.List;
 public class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
     // This should match the definition in Android test runner scripts: bit.ly/3ynoREM
     private static final String MULTIPROCESS_TEST_NAME_SUFFIX = "__multiprocess_mode";
+
+    private static final String ONLY_RUN_MULTI_PROCESS_MODE_FLAG =
+            "AwJUnit4ClassRunner.MultiProcessOnly";
 
     private final TestHook mWebViewMultiProcessHook =
             (targetContext, testMethod) -> {
@@ -78,11 +82,18 @@ public class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
 
     @Override
     protected List<FrameworkMethod> getChildren() {
+        boolean runSingleProcess =
+                !"true"
+                        .equals(
+                                InstrumentationRegistry.getArguments()
+                                        .getString(ONLY_RUN_MULTI_PROCESS_MODE_FLAG));
         List<FrameworkMethod> result = new ArrayList<>();
         for (FrameworkMethod method : computeTestMethods()) {
             switch (processModeForMethod(method)) {
                 case SINGLE_PROCESS:
-                    result.add(method);
+                    if (runSingleProcess) {
+                        result.add(method);
+                    }
                     break;
                 case MULTI_PROCESS:
                     result.add(new WebViewMultiProcessFrameworkMethod(method));
@@ -90,7 +101,9 @@ public class AwJUnit4ClassRunner extends BaseJUnit4ClassRunner {
                 case SINGLE_AND_MULTI_PROCESS:
                 default:
                     result.add(new WebViewMultiProcessFrameworkMethod(method));
-                    result.add(method);
+                    if (runSingleProcess) {
+                        result.add(method);
+                    }
                     break;
             }
         }
