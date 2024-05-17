aad61295ccab966c69f71d2a77c1c64a4af5bd9f
Phillis Tang <phillis@chromium.org>
2024-05-17 22:38:20 +0000

    webnn: check operand_id overflow
    CoreML generates internal operand ids for emulation. This checks if
    the number exceeds the UINT64_MAX before incrementing the
    `internal_operand_id_`. In reality this should never happen but this
    fixes fuzzer failure.
    
    Also fix the initialization of `interal_operand_id_` to not assume the
    `id_to_operand_map` is ordered, as that assumption might break when
    we do emulations before reaching backends.
    
    Bug: 340919088
    Change-Id: Ie3b7b9eaf1a096ce3822c3f58e742a8d70c0dbfc
    Cq-Include-Trybots: luci.chromium.try:mac14-blink-rel,mac14.arm64-blink-rel,linux-blink-rel,gpu-fyi-try-win11-qualcomm-rel-64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545009
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302894}

--- services/webnn/coreml/graph_builder.h
+++ services/webnn/coreml/graph_builder.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/stack_allocated.h"
+#include "base/numerics/checked_math.h"
 #include "base/types/expected.h"
 #include "services/webnn/public/mojom/webnn_error.mojom-forward.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom.h"
@@ -368,7 +369,8 @@ class GraphBuilder {
   SetupMlPackageDirStructure();
 
   std::string GetCoreMLNameFromOperand(uint64_t operand_id);
-  [[nodiscard]] uint64_t GenerateInternalOperandInfo(
+  [[nodiscard]] base::expected<uint64_t, mojom::ErrorPtr>
+  GenerateInternalOperandInfo(
       CoreML::Specification::MILSpec::DataType mil_data_type,
       base::span<const uint32_t> dimensions);
 
@@ -379,7 +381,7 @@ class GraphBuilder {
 
   // Used to generate unique names for internal operands generated for WebNN
   // operations that need to be decomposed into multiple CoreML operations.
-  uint64_t internal_operand_id_;
+  base::CheckedNumeric<uint64_t> internal_operand_id_;
 
   CoreML::Specification::Model ml_model_;
   raw_ptr<CoreML::Specification::MILSpec::Program> program_;
