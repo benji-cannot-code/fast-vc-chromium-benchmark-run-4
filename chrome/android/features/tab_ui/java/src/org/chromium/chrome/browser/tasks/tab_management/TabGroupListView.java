b1a9c1667207d3e4248019cdee3dc8d1195a7f32
Sky Malice <skym@chromium.org>
2024-05-17 16:05:59 +0000

    Reset TabGroupRowView favicons on bind.
    Re-enable default RecyclerView animations to mask favicon gap on
    rebind.
    
    Bug: 341141519
    Change-Id: I8fe4e6bef70d0e5992494bb806b52d256ca7641a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545439
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302623}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListView.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupListView.java
@@ -38,7 +38,6 @@ public class TabGroupListView extends FrameLayout {
         Context context = getContext();
         mRecyclerView = findViewById(R.id.tab_group_list_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-        mRecyclerView.setItemAnimator(null);
 
         mEmptyStateContainer = findViewById(R.id.empty_state_container);
 
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupRowViewUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGroupRowViewUnitTest.java
@@ -219,4 +219,16 @@ public class TabGroupRowViewUnitTest {
         assertNull(imageView.getDrawable());
         assertEquals("", textView.getText());
     }
+
+    @Test
+    @SmallTest
+    public void testResetOnBind() {
+        remakeWithProperty(ASYNC_FAVICON_TOP_LEFT, (callback) -> callback.onResult(mDrawable));
+        ImageView imageView =
+                mTabGroupStartIconParent.getChildAt(0).findViewById(R.id.favicon_image);
+        assertEquals(mDrawable, imageView.getDrawable());
+
+        mTabGroupRowView.resetOnBind();
+        assertNull(imageView.getDrawable());
+    }
 }
