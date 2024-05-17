b29128b2318d3e89118e88749e3bd16f374f279a
Dale Curtis <dalecurtis@chromium.org>
2024-05-17 17:00:15 +0000

    Revert "[lensoverlay] Add cursor tooltip and update the initial toast string"
    This reverts commit 7dbdb60cbe8bd06c2eba3c66de367cbef78b44b0.
    
    Reason for revert: msan failures: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/47761/overview
    LensOverlayControllerCUJTest.EscapeKeyClose
    LensOverlayControllerCUJTest.OpenAndClose 
    
    Original change's description:
    > [lensoverlay] Add cursor tooltip and update the initial toast string
    >
    > Also, fixes a small issue where the mouse icon would appear in the top
    > left corner before moving to the mouse upon mouse move.
    >
    > Demo: http://screencast/cast/NTQ1MTQ0NzIyMjE0MDkyOHwwYjY0NDFkMC1iNg
    >
    > Bug: 337943205, 340285304
    > Change-Id: I5d612438b4a68a251706693ab608b154bdbccddf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5543279
    > Reviewed-by: Bryan Nguyen <nguyenbryan@google.com>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Commit-Queue: Jason Hu <hujasonx@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1302347}
    
    Bug: 337943205, 340285304
    Change-Id: I28410d2998da1d1e96a0eaae8fd2199fb8fa174f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548111
    Owners-Override: Dale Curtis <dalecurtis@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1302663}

--- chrome/browser/resources/lens/overlay/object_layer.ts
+++ chrome/browser/resources/lens/overlay/object_layer.ts
@@ -11,7 +11,6 @@ import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.m
 import type {DomRepeat} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxyImpl} from './browser_proxy.js';
-import {type CursorTooltipData, CursorTooltipType} from './cursor_tooltip.js';
 import {CenterRotatedBox_CoordinateType} from './geometry.mojom-webui.js';
 import type {CenterRotatedBox} from './geometry.mojom-webui.js';
 import type {LensPageCallbackRouter} from './lens.mojom-webui.js';
@@ -184,12 +183,6 @@ export class ObjectLayerElement extends PolymerElement {
       composed: true,
       detail: {cursor: CursorType.POINTER},
     }));
-    this.dispatchEvent(
-        new CustomEvent<CursorTooltipData>('set-cursor-tooltip', {
-          bubbles: true,
-          composed: true,
-          detail: {tooltipType: CursorTooltipType.CLICK_SEARCH},
-        }));
   }
 
   private onSegmentationUnhovered() {
@@ -200,12 +193,6 @@ export class ObjectLayerElement extends PolymerElement {
       composed: true,
       detail: {cursor: CursorType.DEFAULT},
     }));
-    this.dispatchEvent(
-        new CustomEvent<CursorTooltipData>('set-cursor-tooltip', {
-          bubbles: true,
-          composed: true,
-          detail: {tooltipType: CursorTooltipType.REGION_SEARCH},
-        }));
   }
 
   private handlePointerEnter(event: PointerEvent) {
