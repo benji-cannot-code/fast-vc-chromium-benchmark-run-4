aad169e92235c1b8dcf84955e41e15148a08bf71
Calder Kitagawa <ckitagawa@chromium.org>
2024-05-17 21:45:29 +0000

    Re-enable TabGridIncognitoReauthPromoTest
    Now that Hub is launched I think these tests work properly looking at
    test-reviver so re-enable them.
    
    Fixed: 41483016
    Change-Id: I1861c408ca49320b439f3a17d1a3d16fb15dd04b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544872
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302870}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIncognitoReauthPromoTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridIncognitoReauthPromoTest.java
@@ -32,7 +32,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.Restriction;
@@ -83,7 +82,6 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1510419")
     public void testIncognitoReauthPromoShown() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
@@ -97,7 +95,6 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1510419")
     public void testSnackBarShown_WhenClickingReviewActionProvider() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
@@ -128,7 +125,6 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1510419")
     public void testIncognitoPromoNotShownInRegularMode_WhenTogglingFromIncognito() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
@@ -147,7 +143,6 @@ public class TabGridIncognitoReauthPromoTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "https://crbug.com/1510419")
     public void testIncognitoReauthPromo_NoThanks_HidesTheCard() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
 
