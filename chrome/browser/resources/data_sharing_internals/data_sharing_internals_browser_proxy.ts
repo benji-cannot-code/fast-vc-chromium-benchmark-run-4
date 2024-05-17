4cbe8f77145057a7e18e9c5844b1b630ef469262
Min Qin <qinmin@chromium.org>
2024-05-17 01:12:33 +0000

    Display group information on data sharing internal page
    Bug: 328265318
    Change-Id: Icc8623d8f846fc3f5b64ea10debac1645e180ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5532275
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302355}

---
+++ chrome/browser/resources/data_sharing_internals/data_sharing_internals_browser_proxy.ts
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote} from './data_sharing_internals.mojom-webui.js';
+
+export class DataSharingInternalsBrowserProxy {
+  private handler: PageHandlerRemote;
+  private callbackRouter: PageCallbackRouter;
+
+  constructor() {
+    this.callbackRouter = new PageCallbackRouter();
+    this.handler = new PageHandlerRemote();
+    const factory = PageHandlerFactory.getRemote();
+    factory.createPageHandler(
+        this.callbackRouter.$.bindNewPipeAndPassRemote(),
+        this.handler.$.bindNewPipeAndPassReceiver());
+  }
+
+  getAllGroups() {
+    return this.handler.getAllGroups();
+  }
+
+  static getInstance(): DataSharingInternalsBrowserProxy {
+    return instance || (instance = new DataSharingInternalsBrowserProxy());
+  }
+
+  getCallbackRouter(): PageCallbackRouter {
+    return this.callbackRouter;
+  }
+}
+
+let instance: DataSharingInternalsBrowserProxy|null = null;
