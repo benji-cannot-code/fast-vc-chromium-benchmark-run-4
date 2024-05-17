79ebb1df66f666aef5f69551a1c2fa73fa6ad1f0
Joe Antonetti <joeantonetti@google.com>
2024-05-17 15:09:51 +0000

    [Tether] Add Control for Notifications Toggle
    This change adds a notification control for Instant Hotspot.
    
    https://screencast.googleplex.com/cast/NDUyNjUzNjE0ODM4NTc5MnxjOGMyN2E3Yi0wZg
    
    Screenshot for Translation: https://screenshot.googleplex.com/4hsvh9G3CBbeYrR
    
    TEST=Added Tests
    Bug=b/2F290071512
    
    Change-Id: I0e810577891abf3c3fc5b34c58825fd0e6a7cc31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538949
    Commit-Queue: Joe Antonetti <joeantonetti@google.com>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302588}

--- chrome/browser/extensions/api/settings_private/prefs_util.cc
+++ chrome/browser/extensions/api/settings_private/prefs_util.cc
@@ -88,6 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/settings_private/chromeos_resolve_time_zone_by_geolocation_on_off.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
+#include "chromeos/ash/components/tether/pref_names.h"
 #include "chromeos/ash/services/assistant/public/cpp/assistant_prefs.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
 #include "components/account_manager_core/pref_names.h"
@@ -448,6 +449,10 @@ const PrefsUtil::TypedPrefMap& PrefsUtil::GetAllowlistedKeys() {
       settings_api::PrefType::kString;
   (*s_allowlist)[::prefs::kNearbySharingDataUsageName] =
       settings_api::PrefType::kNumber;
+
+  // Instant Hotspot
+  (*s_allowlist)[::ash::tether::prefs::kNotificationsEnabled] =
+      settings_api::PrefType::kBoolean;
 #endif
 
   // Search page.
