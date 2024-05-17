28e3377c6a969d8d6812d6111f9a6a604c2cc88c
danakj <danakj@chromium.org>
2024-05-17 00:29:49 +0000

    Roll getrandom: 0.2.14 => 0.2.15 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * getrandom: 0.2.14 => 0.2.15
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * getrandom@0.2.15: does-not-implement-crypto, safe-to-run
    
    Bug: None
    Change-Id: Ib443d8f1102fbb810c84f64349fb8caaa143d6c4
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5533216
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302340}

--- third_party/rust/chromium_crates_io/vendor/getrandom-0.2.15/src/getentropy.rs
+++ third_party/rust/chromium_crates_io/vendor/getrandom-0.2.15/src/getentropy.rs
@@ -1,16 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! Implementation for macOS
+//! Implementation using getentropy(2)
+//!
+//! Available since:
+//!   - macOS 10.12
+//!   - OpenBSD 5.6
+//!   - Emscripten 2.0.5
+//!   - vita newlib since Dec 2021
+//!
+//! For these targets, we use getentropy(2) because getrandom(2) doesn't exist.
 use crate::{util_libc::last_os_error, Error};
 use core::mem::MaybeUninit;
 
-extern "C" {
-    // Supported as of macOS 10.12+.
-    fn getentropy(buf: *mut u8, size: libc::size_t) -> libc::c_int;
-}
-
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
     for chunk in dest.chunks_mut(256) {
-        let ret = unsafe { getentropy(chunk.as_mut_ptr() as *mut u8, chunk.len()) };
+        let ret = unsafe { libc::getentropy(chunk.as_mut_ptr() as *mut libc::c_void, chunk.len()) };
         if ret != 0 {
             return Err(last_os_error());
         }
--- third_party/rust/chromium_crates_io/vendor/getrandom-0.2.15/src/netbsd.rs
+++ third_party/rust/chromium_crates_io/vendor/getrandom-0.2.15/src/netbsd.rs
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! Implementation for FreeBSD and NetBSD
+//! Implementation for NetBSD
 use crate::{
     util_libc::{sys_fill_exact, Weak},
     Error,
@@ -29,7 +29,7 @@ fn kern_arnd(buf: &mut [MaybeUninit<u8>]) -> libc::ssize_t {
 type GetRandomFn = unsafe extern "C" fn(*mut u8, libc::size_t, libc::c_uint) -> libc::ssize_t;
 
 pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
-    // getrandom(2) was introduced in FreeBSD 12.0 and NetBSD 10.0
+    // getrandom(2) was introduced in NetBSD 10.0
     static GETRANDOM: Weak = unsafe { Weak::new("getrandom\0") };
     if let Some(fptr) = GETRANDOM.ptr() {
         let func: GetRandomFn = unsafe { core::mem::transmute(fptr) };
@@ -38,7 +38,7 @@ pub fn getrandom_inner(dest: &mut [MaybeUninit<u8>]) -> Result<(), Error> {
         });
     }
 
-    // Both FreeBSD and NetBSD will only return up to 256 bytes at a time, and
+    // NetBSD will only return up to 256 bytes at a time, and
     // older NetBSD kernels will fail on longer buffers.
     for chunk in dest.chunks_mut(256) {
         sys_fill_exact(chunk, kern_arnd)?
