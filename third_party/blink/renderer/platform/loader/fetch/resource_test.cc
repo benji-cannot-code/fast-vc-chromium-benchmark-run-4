b1ba605cd32a75f128b3192494e1be3e2af49c69
Kouhei Ueno <kouhei@chromium.org>
2024-05-17 00:48:09 +0000

    rename MemoryCache::ResourceForURL{,ForTesting}
    As discussed, for the reason added as the comment, the method shouldn't
    be called out side test.
    
    Rename it so the intent is clear.
    
    Bug: 341136042
    Change-Id: I7a0b4337533c2c03ebd1ff28dd4d60776504c674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546698
    Auto-Submit: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302346}

--- third_party/blink/renderer/platform/loader/fetch/resource_test.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_test.cc
@@ -133,7 +133,7 @@ TEST_F(ResourceTest, RevalidationFailed) {
   EXPECT_FALSE(resource->HasSuccessfulRevalidation());
   EXPECT_EQ(200, resource->GetResponse().HttpStatusCode());
   EXPECT_FALSE(resource->ResourceBuffer());
-  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURL(url));
+  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURLForTesting(url));
 
   resource->AppendData(kData);
 
@@ -175,7 +175,7 @@ TEST_F(ResourceTest, RevalidationSucceeded) {
   EXPECT_TRUE(resource->HasSuccessfulRevalidation());
   EXPECT_EQ(200, resource->GetResponse().HttpStatusCode());
   EXPECT_EQ(4u, resource->ResourceBuffer()->size());
-  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURL(url));
+  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURLForTesting(url));
 
   MemoryCache::Get()->Remove(resource);
 
@@ -209,7 +209,7 @@ TEST_F(ResourceTest, RevalidationSucceededForResourceWithoutBody) {
   EXPECT_TRUE(resource->HasSuccessfulRevalidation());
   EXPECT_EQ(200, resource->GetResponse().HttpStatusCode());
   EXPECT_FALSE(resource->ResourceBuffer());
-  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURL(url));
+  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURLForTesting(url));
   MemoryCache::Get()->Remove(resource);
 
   resource->RemoveClient(client);
@@ -369,7 +369,7 @@ TEST_F(ResourceTest, RedirectDuringRevalidation) {
   EXPECT_EQ(redirect_target_url, resource->LastResourceRequest().Url());
   EXPECT_EQ(200, resource->GetResponse().HttpStatusCode());
   EXPECT_EQ(3u, resource->ResourceBuffer()->size());
-  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURL(url));
+  EXPECT_EQ(resource, MemoryCache::Get()->ResourceForURLForTesting(url));
 
   EXPECT_TRUE(client->NotifyFinishedCalled());
 
