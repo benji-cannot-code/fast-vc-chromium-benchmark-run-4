c2eff487ae40b2d679b0c51284ee9acde20f75af
Patrick Monette <pmonette@chromium.org>
2024-05-17 20:21:34 +0000

    Remove RunPMOnMainThread
    Change-Id: If9d7a6025b42b6f67dceafa232357519d20b3695
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544706
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302821}

--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -16,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_manager::features {
 
-BASE_FEATURE(kRunOnMainThread,
-             "RunPerformanceManagerOnMainThread",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kRunOnMainThreadSync,
              "RunPerformanceManagerOnMainThreadSync",
              base::FEATURE_DISABLED_BY_DEFAULT);
