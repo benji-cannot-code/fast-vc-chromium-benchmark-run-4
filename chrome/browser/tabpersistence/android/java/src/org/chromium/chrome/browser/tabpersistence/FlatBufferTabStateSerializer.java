e83d47270046fdd8455e7bda9e7858199cdf5fe0
David Maunder <davidjm@google.com>
2024-05-17 15:05:21 +0000

    Use asReadyOnlyBuffer() when copying bytes in TabState FlatBuffer
    In crrev.com/c/5546438 the WebContentsState bytes were copied
    before writing to the file. I just realized we should use
    asReadOnlyBuffer() instead of buffer() to ensure the
    ByteBuffer's position, limit etc. cannot be modified for
    the duration of the copy. This is consistent with legacy TabState
    which uses asReadOnlyBuffer().
    
    Bug: b:5546438
    Change-Id: I44dc44abef511fbb249108f99051a6545915f7c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546345
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302586}

--- chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
+++ chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
@@ -98,7 +98,7 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
                 || webContentsState.buffer().limit() == 0) {
             return new byte[] {};
         }
-        ByteBuffer buffer = webContentsState.buffer();
+        ByteBuffer buffer = webContentsState.buffer().asReadOnlyBuffer();
         byte[] contentsStateBytes = new byte[buffer.limit()];
         buffer.rewind();
         buffer.get(contentsStateBytes);
