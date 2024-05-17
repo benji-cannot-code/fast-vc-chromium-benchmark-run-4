0760264abb786f76324751071f8ecc69eab21902
danakj <danakj@chromium.org>
2024-05-17 13:51:38 +0000

    Roll read-fonts: 0.19.0 => 0.19.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * read-fonts: 0.19.0 => 0.19.1
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * read-fonts@0.19.1: does-not-implement-crypto, safe-to-deploy,
      ub-risk-2
    
    Bug: None
    Change-Id: I0624c8b73c26e4eac9a5c9498f84441dd58d5890
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535157
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302538}

--- third_party/rust/chromium_crates_io/vendor/read-fonts-0.19.0/src/tables/hmtx.rs
+++
@@ -1,4 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! The [hmtx (Horizontal Metrics)](https://docs.microsoft.com/en-us/typography/opentype/spec/hmtx) table
-
-include!("../../generated/generated_hmtx.rs");
--- third_party/rust/chromium_crates_io/vendor/read-fonts-0.19.0/src/tables/vmtx.rs
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-//! The [vmtx (Vertical Metrics)](https://docs.microsoft.com/en-us/typography/opentype/spec/vmtx) table
-
-pub use super::hmtx::LongMetric;
-
-include!("../../generated/generated_vmtx.rs");
