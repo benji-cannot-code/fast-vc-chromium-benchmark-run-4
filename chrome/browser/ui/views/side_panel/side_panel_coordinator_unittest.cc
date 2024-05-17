d67faf384c1b99d8731b1e9c781b2d7673653ea7
Eriko Kurimoto <elkurin@chromium.org>
2024-05-17 05:25:11 +0000

    Revert "Set ReadAnythingLocalSidePanel enabled by default."
    This reverts commit 6b25479c2cdebc468076aa3471483cf8c3d7a45f.
    
    Reason for revert: All/LensSidePanelCoordinatorTest.* are failing on
    linux-chromeos-chrome. First failure:
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/41611/overview
    
    Original change's description:
    > Set ReadAnythingLocalSidePanel enabled by default.
    >
    > The read anything local side panel feature is launched in M126.
    >
    > Bug: 40909106
    > Change-Id: I44390af95651c48924452b3aa2ebda8362f83aae
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536098
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Commit-Queue: Xiang Xiao <xiangxiao@google.com>
    > Reviewed-by: Abigail Klein <abigailbklein@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1302298}
    
    Bug: 40909106
    Change-Id: If86af7dbfbb8b8efd323372d990610ba1c792cf7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546703
    Auto-Submit: Eriko Kurimoto <elkurin@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1302412}

--- chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
+++ chrome/browser/ui/views/side_panel/side_panel_coordinator_unittest.cc
@@ -208,8 +208,6 @@ class SidePanelCoordinatorTest : public TestWithBrowserView {
         SidePanelRegistry::Get(browser_view()->GetActiveWebContents());
     registry->Deregister(
         SidePanelEntry::Key(SidePanelEntry::Id::kSearchCompanion));
-    registry->Deregister(
-        SidePanelEntry::Key(SidePanelEntry::Id::kReadAnything));
   }
 
  protected:
