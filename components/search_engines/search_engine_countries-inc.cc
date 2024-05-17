6e6dbc27b6ad5ef9fcb492048459f351ccb55a19
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-05-17 12:44:32 +0000

    🧇 Add extra country codes for some missing territories
    Bug: 341247415, b/341047036
    Change-Id: I6ab230224299111b2a950717558c790ff2917b42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545401
    Auto-Submit: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302513}

--- components/search_engines/search_engine_countries-inc.cc
+++ components/search_engines/search_engine_countries-inc.cc
@@ -1261,6 +1261,10 @@ const std::vector<EngineAndTier> GetPrepopulationSetFromCountryID(
 
     // Countries using the "Spain" engine set.
     UNHANDLED_COUNTRY(A, D)  // Andorra
+    UNHANDLED_COUNTRY(E, A)  // Ceuta & Melilla (not in ISO 3166-1 but included
+                             // in some Chrome country code lists)
+    UNHANDLED_COUNTRY(I, C)  // Canary Islands (not in ISO 3166-1 but included
+                             // in some Chrome country code lists)
     END_UNHANDLED_COUNTRIES(E, S)
 
     // Countries using the "Finland" engine set.
@@ -1270,6 +1274,7 @@ const std::vector<EngineAndTier> GetPrepopulationSetFromCountryID(
     // Countries using the "France" engine set.
     UNHANDLED_COUNTRY(B, F)  // Burkina Faso
     UNHANDLED_COUNTRY(B, J)  // Benin
+    UNHANDLED_COUNTRY(B, L)  // St. Barthélemy
     UNHANDLED_COUNTRY(C, D)  // Congo - Kinshasa
     UNHANDLED_COUNTRY(C, F)  // Central African Republic
     UNHANDLED_COUNTRY(C, G)  // Congo - Brazzaville
@@ -1285,6 +1290,7 @@ const std::vector<EngineAndTier> GetPrepopulationSetFromCountryID(
     UNHANDLED_COUNTRY(I, P)  // Clipperton Island ('IP' is an WinXP-ism; ISO
                              //                    includes it with France)
 #endif
+    UNHANDLED_COUNTRY(M, F)  // Saint Martin
     UNHANDLED_COUNTRY(M, L)  // Mali
     UNHANDLED_COUNTRY(M, Q)  // Martinique
     UNHANDLED_COUNTRY(N, C)  // New Caledonia
