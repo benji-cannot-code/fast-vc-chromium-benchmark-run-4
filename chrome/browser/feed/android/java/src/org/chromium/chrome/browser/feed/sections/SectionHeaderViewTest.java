f537bd5fed36f84da0384b207b89e96d52eb528f
Xinyi Ji <xinyiji@google.com>
2024-05-17 17:47:44 +0000

    [SurfacePolish][CleanUp] Clean up surface polish code in test (part 2).
    Clean up tests using ChromeFeatureList.SURFACE_POLISH.
    
    Bug: 331667743
    Change-Id: I69e3e6c53140d123ed38ae353e4c51434af6728d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545921
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302703}

--- chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderViewTest.java
+++ chrome/browser/feed/android/java/src/org/chromium/chrome/browser/feed/sections/SectionHeaderViewTest.java
@@ -32,7 +32,6 @@ import org.chromium.base.FeatureList;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Features;
 import org.chromium.base.test.util.Features.DisableFeatures;
-import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.feed.componentinterfaces.SurfaceCoordinator.StreamTabId;
@@ -94,7 +93,6 @@ public final class SectionHeaderViewTest {
 
     @Test
     @SmallTest
-    @EnableFeatures({ChromeFeatureList.SURFACE_POLISH})
     public void mainContentTopMarginTest() {
         mSectionHeaderView.onFinishInflate();
 
