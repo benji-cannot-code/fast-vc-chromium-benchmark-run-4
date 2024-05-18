e15180d6166a8a54af9348b35658809b69c7630b
Tao Wu <wutao@chromium.org>
2024-05-18 00:54:27 +0000

    growth: Add event trigger
    Bug: 340905230
    Test: add new tests
    Change-Id: Ieacefb2c038251104e2a9166188d8e6d5b5e6c1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542656
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302946}

--- chrome/browser/ash/growth/campaigns_manager_session.cc
+++ chrome/browser/ash/growth/campaigns_manager_session.cc
@@ -82,7 +82,9 @@ void MaybeTriggerCampaignsWhenAppOpened() {
 
   auto* campaigns_manager = growth::CampaignsManager::Get();
   CHECK(campaigns_manager);
-  campaigns_manager->SetTrigger(growth::TriggeringType::kAppOpened);
+
+  growth::Trigger trigger(growth::TriggerType::kAppOpened);
+  campaigns_manager->SetTrigger(std::move(trigger));
 
   MaybeTriggerSlot(growth::Slot::kNudge);
   MaybeTriggerSlot(growth::Slot::kNotification);
@@ -95,7 +97,9 @@ void MaybeTriggerCampaignsWhenCampaignsLoaded() {
 
   auto* campaigns_manager = growth::CampaignsManager::Get();
   CHECK(campaigns_manager);
-  campaigns_manager->SetTrigger(growth::TriggeringType::kCampaignsLoaded);
+
+  growth::Trigger trigger(growth::TriggerType::kCampaignsLoaded);
+  campaigns_manager->SetTrigger(std::move(trigger));
 
   MaybeTriggerSlot(growth::Slot::kNudge);
   MaybeTriggerSlot(growth::Slot::kNotification);
