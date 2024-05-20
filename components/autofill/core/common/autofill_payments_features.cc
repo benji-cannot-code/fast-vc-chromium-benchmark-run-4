82533c1b8f343bad909df5a7e70947d76d5e8201
Christoph Schwering <schwering@google.com>
2024-05-20 18:54:44 +0000

    [Autofill] Move autofill*features.{h,cc} to the same build target
    This CL moves autofill_payments_features.{cc,h} into the same
    build target as autofill_features.{cc,h}.
    
    For historic reasons (crbug.com/40179179#comment2), this build
    target is a component. Since components are preferred in general and
    supposedly especially for features, this CL leaves it a component
    (and therefore annotates the symbols from autofill_payments_features.h
    with COMPONENT_EXPORT()).
    
    Bug: 40100455
    Change-Id: I5d2fe967ed861e03eb543b9d10f093b1f6936dd6
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545512
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303409}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::features {
 
-// Features
-
 // When enabled, Android N+ devices will be supported for FIDO authentication.
 BASE_FEATURE(kAutofillEnableAndroidNKeyForFidoAuthentication,
              "AutofillEnableAndroidNKeyForFidoAuthentication",
