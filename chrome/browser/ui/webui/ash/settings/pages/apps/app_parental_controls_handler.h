ac6d4abc9a67fc36d4057fb2642cfcd6b84c7bce
Courtney Wong <courtneywong@chromium.org>
2024-05-20 18:02:34 +0000

    app-controls: Refresh app list on readiness updates
    Listen for readiness changes to apps and notify the client of the change
    to update the app parental controls UI.
    
    Bug: b:337027957
    Change-Id: I5ad600d28a1c3d126704bd3d2d9c95ca0624cf79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545010
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303382}

--- chrome/browser/ui/webui/ash/settings/pages/apps/app_parental_controls_handler.h
+++ chrome/browser/ui/webui/ash/settings/pages/apps/app_parental_controls_handler.h
@@ -43,6 +43,9 @@ class AppParentalControlsHandler
   // app_parental_controls::mojom::AppParentalControlsHandler:
   void GetApps(GetAppsCallback callback) override;
   void UpdateApp(const std::string& id, bool is_blocked) override;
+  void AddObserver(mojo::PendingRemote<
+                   app_parental_controls::mojom::AppParentalControlsObserver>
+                       observer) override;
 
   void BindInterface(
       mojo::PendingReceiver<
@@ -50,10 +53,15 @@ class AppParentalControlsHandler
 
  private:
   // apps::AppRegistryCache::Observer:
+  void OnAppUpdate(const apps::AppUpdate& update) override;
   void OnAppRegistryCacheWillBeDestroyed(
       apps::AppRegistryCache* cache) override;
 
   std::vector<app_parental_controls::mojom::AppPtr> GetAppList();
+  void NotifyAppChanged(app_parental_controls::mojom::AppPtr app);
+
+  mojo::RemoteSet<app_parental_controls::mojom::AppParentalControlsObserver>
+      observer_list_;
 
   raw_ptr<apps::AppServiceProxy> app_service_proxy_ = nullptr;
 
