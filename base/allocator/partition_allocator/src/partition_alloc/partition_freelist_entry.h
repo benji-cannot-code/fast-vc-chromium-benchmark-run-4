324d6201bd04364e9ea8f33a1be24af5f5c3e309
Kalvin Lee <kdlee@chromium.org>
2024-05-20 06:11:19 +0000

    PA: Make `PartitionFreelistDispatcher` constexpr
    To satisfy Chromium style, exit-time destructor constraints, and not run
    afoul of Windows CRT re-entrancy, we remove the virtual destructor of
    `PartitionFreelistDispatcher` and make them `constexpr`.
    
    Fixed: 336007395
    Change-Id: I1ed350ac8fe9889bd6bff2078d3065ec73b13783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541212
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303219}

--- base/allocator/partition_allocator/src/partition_alloc/partition_freelist_entry.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_freelist_entry.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "partition_alloc/partition_alloc_base/bits.h"
 #include "partition_alloc/partition_alloc_base/compiler_specific.h"
 #include "partition_alloc/partition_alloc_base/component_export.h"
-#include "partition_alloc/partition_alloc_base/no_destructor.h"
 #include "partition_alloc/partition_alloc_buildflags.h"
 #include "partition_alloc/partition_alloc_constants.h"
 
@@ -105,8 +104,6 @@ struct PartitionFreelistDispatcher {
       PartitionFreelistEntry* entry) const = 0;
   PA_ALWAYS_INLINE virtual constexpr bool IsEncodedNextPtrZero(
       PartitionFreelistEntry* entry) const = 0;
-
-  virtual ~PartitionFreelistDispatcher() = default;
 #else
   static const PartitionFreelistDispatcher* Create(
       PartitionFreelistEncoding encoding) {
@@ -192,7 +189,7 @@ struct PartitionFreelistDispatcher {
 
 #if PA_BUILDFLAG(USE_FREELIST_DISPATCHER)
 template <PartitionFreelistEncoding encoding>
-struct PartitionFreelistDispatcherImpl : PartitionFreelistDispatcher {
+struct PartitionFreelistDispatcherImpl final : PartitionFreelistDispatcher {
   using Entry =
       std::conditional_t<encoding ==
                              PartitionFreelistEncoding::kEncodedFreeList,
@@ -296,24 +293,32 @@ struct PartitionFreelistDispatcherImpl : PartitionFreelistDispatcher {
   }
 };
 
+// Both dispatchers are constexpr
+// 1. to avoid "declaration requires an exit-time destructor" error
+//    e.g. on android-cronet-mainline-clang-arm64-dbg.
+// 2. to not create re-entrancy issues with Windows CRT
+//    (crbug.com/336007395).
+inline static constexpr PartitionFreelistDispatcherImpl<
+    PartitionFreelistEncoding::kEncodedFreeList>
+    kEncodedImplDispatcher{};
+inline static constexpr PartitionFreelistDispatcherImpl<
+    PartitionFreelistEncoding::kPoolOffsetFreeList>
+    kPoolOffsetImplDispatcher{};
+
 PA_ALWAYS_INLINE const PartitionFreelistDispatcher*
 PartitionFreelistDispatcher::Create(PartitionFreelistEncoding encoding) {
   switch (encoding) {
     case PartitionFreelistEncoding::kEncodedFreeList: {
-      static base::NoDestructor<PartitionFreelistDispatcherImpl<
-          PartitionFreelistEncoding::kEncodedFreeList>>
-          encoded_impl;
-      return encoded_impl.get();
+      return &kEncodedImplDispatcher;
     }
     case PartitionFreelistEncoding::kPoolOffsetFreeList: {
-      static base::NoDestructor<PartitionFreelistDispatcherImpl<
-          PartitionFreelistEncoding::kPoolOffsetFreeList>>
-          pool_offset_impl;
-      return pool_offset_impl.get();
+      return &kPoolOffsetImplDispatcher;
     }
   }
 }
+
 #endif  // PA_BUILDFLAG(USE_FREELIST_DISPATCHER)
+
 }  // namespace partition_alloc::internal
 
 #endif  // PARTITION_ALLOC_PARTITION_FREELIST_ENTRY_H_
