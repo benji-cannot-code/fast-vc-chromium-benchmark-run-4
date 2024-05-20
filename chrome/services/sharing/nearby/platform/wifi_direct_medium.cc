c82c0fa5f63f8e1a0b11ed27eb2b30866f429c3c
Jack Shira <jackshira@google.com>
2024-05-20 23:10:58 +0000

    [Nearby Share] Implement StopWifiDirect
    Implements StopWifiDirect for the WifiDirectMedium.
    
    Bug: b/341388643
    Tests: unittested
    Change-Id: I0f581c873e5a8942313f7a9d9ad4bb44448bde60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548985
    Commit-Queue: Jack Shira <jackshira@google.com>
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303510}

--- chrome/services/sharing/nearby/platform/wifi_direct_medium.cc
+++ chrome/services/sharing/nearby/platform/wifi_direct_medium.cc
@@ -46,7 +46,10 @@ bool WifiDirectMedium::StartWifiDirect(WifiDirectCredentials* credentials) {
 }
 
 bool WifiDirectMedium::StopWifiDirect() {
-  NOTIMPLEMENTED();
+  if (connection_) {
+    connection_.reset();
+    return true;
+  }
   return false;
 }
 
