6d97f74cef7c505ffea4aaa4f544c1fdefae81f4
Dominik Röttsches <drott@chromium.org>
2024-05-20 10:50:51 +0000

    Disable SearchEngineChoiceDialogBrowserTest in CrOS
    Disable
    SearchEngineChoiceDialogBrowserTest.DialogDoesNotShowAgainAfterSettingPref
    on CrOS, no culprit could be identified.
    
    Bug: b/341643240
    Change-Id: Ib29bf53215d8595dbc3faf6f123b49c20ebc62f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5549745
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Auto-Submit: Dominik Röttsches <drott@chromium.org>
    Owners-Override: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303261}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_browsertest.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
+#include "build/buildflag.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
 #include "chrome/browser/profiles/keep_alive/profile_keep_alive_types.h"
@@ -504,8 +505,16 @@ IN_PROC_BROWSER_TEST_F(SearchEngineChoiceDialogBrowserTest,
                             EntryPoint::kDialog);
 }
 
+// TODO(b/341643240): Fix test.
+#if BUILDFLAG(IS_CHROMEOS)
+#define MAYBE_DialogDoesNotShowAgainAfterSettingPref \
+  DISABLED_DialogDoesNotShowAgainAfterSettingPref
+#else
+#define MAYBE_DialogDoesNotShowAgainAfterSettingPref \
+  DialogDoesNotShowAgainAfterSettingPref
+#endif
 IN_PROC_BROWSER_TEST_F(SearchEngineChoiceDialogBrowserTest,
-                       DialogDoesNotShowAgainAfterSettingPref) {
+                       MAYBE_DialogDoesNotShowAgainAfterSettingPref) {
   auto* service = static_cast<MockSearchEngineChoiceDialogService*>(
       SearchEngineChoiceDialogServiceFactory::GetForProfile(
           browser()->profile()));
