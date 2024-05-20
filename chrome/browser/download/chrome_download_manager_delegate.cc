211e316f7ac2ec6e2a88fb74ba19dca4a672cc9a
Steven Luong <stluong@chromium.org>
2024-05-20 20:18:11 +0000

    Revert "use in memory download for pdf in incognito mode"
    This reverts commit d000a247954a55ea973935dff404bd0e06a44daf.
    
    Reason for revert: Tree closure
    
    Original change's description:
    > use in memory download for pdf in incognito mode
    >
    > Bug: 338466611
    > Change-Id: Ie97c24d633d58cc0126f4ca0db66bac24283299b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539855
    > Commit-Queue: Min Qin <qinmin@chromium.org>
    > Reviewed-by: Shu Yang <shuyng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1303414}
    
    Bug: 338466611
    Change-Id: I4de33e511ee2f20160b7693fdc3c32421d984713
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551616
    Auto-Submit: Steven Luong <stluong@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Steven Luong <stluong@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303446}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -100,7 +100,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/build_info.h"
 #include "base/android/content_uri_utils.h"
 #include "base/android/path_utils.h"
-#include "base/process/process_handle.h"
 #include "chrome/browser/android/tab_android.h"
 #include "chrome/browser/download/android/chrome_duplicate_download_infobar_delegate.h"
 #include "chrome/browser/download/android/download_controller.h"
@@ -636,14 +635,9 @@ bool ChromeDownloadManagerDelegate::DetermineDownloadTarget(
           profile_->GetPrefs()->GetString(prefs::kDefaultCharset),
           download->GetSuggestedFilename(), download->GetMimeType(),
           l10n_util::GetStringUTF8(IDS_DEFAULT_DOWNLOAD_FILENAME));
-      if (profile_->IsOffTheRecord()) {
-        download_path = download->GetDownloadFile()->FullPath();
-      } else {
-        base::FilePath cache_dir;
-        base::android::GetCacheDirectory(&cache_dir);
-        download_path =
-            cache_dir.Append(kPdfDirName).Append(generated_filename);
-      }
+      base::FilePath cache_dir;
+      base::android::GetCacheDirectory(&cache_dir);
+      download_path = cache_dir.Append(kPdfDirName).Append(generated_filename);
       action = DownloadPathReservationTracker::UNIQUIFY;
     } else {
       action = DownloadPathReservationTracker::OVERWRITE;
