8d71cbd21f58ef0ab09b1aba0f1fccff9c62b341
Evan Stade <estade@chromium.org>
2024-05-20 22:02:12 +0000

    IndexedDB: add more debugging code for locking failures.
    It appears Chromium is still attempting to lock the same LevelDB LOCK
    file more than once as there are still errors after b41453229aa80dede3a.
    This change adds some crash keys to help point us in the right
    direction.
    
    The holder of the original lock is added by way of a stack trace.
    It's expected the location of the original lock request is the same as
    the location of the second/conflicting lock request, so this alone may
    not be helpful.
    
    Some additional information is added in IndexedDBBucketContext:
    * the database path, which could tell us if this is only happening to
      non-default buckets, such as third-party buckets.
    * the number of attempts, in case the first failed attempt somehow
      doesn't release the lock before the second attempt
    * the number of times this bucket context has opened a backing store,
      which should help diagnose if this is a problem of two different
      bucket contexts trying to open the db vs. one bucket context opening
      it multiple times.
    * whether this error condition occurs when backing stores are sharded,
      since that affects IndexedDBBucketContext lifetime somewhat.
    
    Bug: 340398745
    Change-Id: I56713a3d82400bcdfe160e747ab1001c1962ba5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551420
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303481}

--- components/services/storage/public/cpp/filesystem/filesystem_impl.cc
+++ components/services/storage/public/cpp/filesystem/filesystem_impl.cc
@@ -5,12 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/services/storage/public/cpp/filesystem/filesystem_impl.h"
 
-#include <set>
+#include <map>
 #include <vector>
 
 #include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/debug/dump_without_crashing.h"
+#include "base/debug/stack_trace.h"
 #include "base/files/file.h"
 #include "base/files/file_enumerator.h"
 #include "base/files/file_util.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "base/types/expected_macros.h"
 #include "build/build_config.h"
+#include "components/crash/core/common/crash_key.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -42,7 +44,20 @@ class LockTable {
   bool AddLock(const base::FilePath& path) {
     DCHECK(path.IsAbsolute());
     base::AutoLock lock(lock_);
-    auto result = lock_paths_.insert(path.NormalizePathSeparators());
+    auto result = lock_paths_.insert(std::make_pair(
+        path.NormalizePathSeparators(), base::debug::StackTrace()));
+
+    // TODO(crbug.com/340398745): resolve this mystery and remove this
+    // block, or even replace it with a CHECK.
+    if (!result.second) {
+      static crash_reporter::CrashKeyString<1024> trace_key(
+          "crbug/340398745/existing_lock_stack");
+      crash_reporter::SetCrashKeyStringToStackTrace(&trace_key,
+                                                    result.first->second);
+      base::debug::DumpWithoutCrashing();
+      trace_key.Clear();
+    }
+
     return result.second;
   }
 
@@ -55,7 +70,8 @@ class LockTable {
 
  private:
   base::Lock lock_;
-  std::set<base::FilePath> lock_paths_ GUARDED_BY(lock_);
+  std::map<base::FilePath, base::debug::StackTrace> lock_paths_
+      GUARDED_BY(lock_);
 };
 
 // Get the global singleton instance of LockTable. This returned object is
@@ -270,9 +286,6 @@ base::FileErrorOr<base::File> FilesystemImpl::LockFileLocal(
     return base::unexpected(file.error_details());
 
   if (!GetLockTable().AddLock(path)) {
-    // TODO(crbug.com/340398745): resolve this mystery and remove this line, or
-    // even replace it with a CHECK.
-    base::debug::DumpWithoutCrashing();
     return base::unexpected(base::File::FILE_ERROR_IN_USE);
   }
 
