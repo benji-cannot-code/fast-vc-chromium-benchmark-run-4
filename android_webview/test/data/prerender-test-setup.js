de46417859b67377ddaeb04220e1283231da0a02
Hiroki Nakagawa <nhiroki@chromium.org>
2024-05-20 02:05:15 +0000

    WebView Prerender: Add tests for No-Vary-Search with multiple params
    This CL adds a more complex test case for No-Vary-Search header on
    WebView. This confirms the case where all the header directives are
    specified (i.e., `key-order`, `params`, and `except`).
    
    The test depends on mock http header mechanism that can be specified per
    test html file. prerender.html is already used for the other
    No-Vary-Search header test that specifies only `params`, so this CL adds
    a new test file and a mock http header file for the complex case. To
    share the test helpers among the test files, this CL also adds
    `prerender-test-setup.js`.
    
    Adding a new script file confuses some existing tests that strictly
    check resource loading on `shouldInterceptRequest` etc. This CL tweaks
    them to pass.
    
    Bug: 339588213
    Change-Id: I7d0bfc44d3247bd1a1557962453cab4246938ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541297
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303181}

---
+++ android_webview/test/data/prerender-test-setup.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Objects starting with the "aw" prefix are injected by AwPrerenderTest.
+
+function createIframe(url) {
+  const iframe = document.createElement('iframe');
+  iframe.setAttribute('src', url);
+  document.body.appendChild(iframe);
+}
+
+function wasActivated() {
+  return self.performance?.getEntriesByType?.('navigation')[0]
+             ?.activationStart > 0;
+}
+
+const wasPrerendered = document.prerendering || wasActivated();
+
+if (wasActivated()) {
+  awActivationFuture.activated();
+} else {
+  document.onprerenderingchange = () => awActivationFuture.activated();
+}
+
+// Notify the primary page that this prerendered page started.
+window.localStorage.setItem('pageStarted', location.href);
