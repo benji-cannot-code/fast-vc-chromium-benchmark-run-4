ee3111750acfb02cc15a9fc168c04d97539c1dce
Andrew Paseltiner <apaseltiner@chromium.org>
2024-05-20 16:21:22 +0000

    Remove unused android_time.h
    Change-Id: Ic5fbd260e88e1cae429c27446844c634007928db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551152
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303320}

--- components/history/core/browser/android/android_cache_database.cc
+++ components/history/core/browser/android/android_cache_database.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "components/history/core/browser/android/android_time.h"
 #include "sql/database.h"
 #include "sql/statement.h"
 
