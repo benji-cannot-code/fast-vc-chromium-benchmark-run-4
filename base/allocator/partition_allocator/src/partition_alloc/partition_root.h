7c0f48ba535e4ec0d8fb00cbb940278725690676
Kalvin Lee <kdlee@chromium.org>
2024-05-20 10:09:16 +0000

    PA: Add 4S feature plumbing
    Add the `base::Feature` governing the small single-slot span experiment
    and plumb it through to PartitionAlloc.
    
    Bug: 333443437
    Change-Id: I09cc7fdae595d02284a0c508f04efe98755a7848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547265
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303254}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.h
@@ -188,6 +188,7 @@ struct PartitionOptions {
 #endif
 
   EnableToggle use_pool_offset_freelists = kDisabled;
+  EnableToggle use_small_single_slot_spans = kDisabled;
 };
 
 constexpr PartitionOptions::PartitionOptions() = default;
