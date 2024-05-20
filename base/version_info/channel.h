f54ab359482dccc0d53ea3f45a76757fe057aa02
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-05-20 21:14:46 +0000

    Revert "Make NOTREACHED() without parameters [[noreturn]]"
    This reverts commit fdeac78ccd92e2cc578d8cfa04219599520138b4.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8747389795956447889
    
    Sample failed build: https://ci.chromium.org/b/8747389795956447889
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5548908&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8747389795956447889&type=BUG
    
    Original change's description:
    > Make NOTREACHED() without parameters [[noreturn]]
    >
    > Previous callers have been migrated to NOTREACHED_IN_MIGRATION(). This
    > also renames the few NOTREACHED_NORETURN() callers in base/ to
    > NOTREACHED() to verify that it's still working (no missing-return).
    >
    > Let's see how many times this needs to be reverted and relanded. :)
    >
    > Bug: 40580068
    > Change-Id: Ib8155e461ea9d0853c9508e28fc45389114da48c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548908
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Owners-Override: Lei Zhang <thestig@chromium.org>
    > Auto-Submit: Peter Boström <pbos@chromium.org>
    > Commit-Queue: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1303439}
    >
    
    Bug: 40580068
    Change-Id: Id1604628ad9bea340ab15c75be216a60f58d0252
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551621
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1303467}

--- base/version_info/channel.h
+++ base/version_info/channel.h
@@ -40,7 +40,7 @@ constexpr std::string_view GetChannelString(Channel channel) {
     case Channel::UNKNOWN:
       return "unknown";
   }
-  NOTREACHED();
+  NOTREACHED_NORETURN();
 }
 
 }  // namespace version_info
