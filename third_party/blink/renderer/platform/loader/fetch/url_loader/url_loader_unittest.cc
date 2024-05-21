b60d0359d2b50fc4cc5005151aea19d67819740c
Tsuyoshi Horo <horo@chromium.org>
2024-05-21 12:54:47 +0000

    Spanify DidReceiveData methods
    This CL spanify the DidReceiveData method of URLLoaderTestDelegate,  URLLoaderClient, ResourceLoader and some test classes.
    
    Bug: 40284755
    Change-Id: Ia101a9c25a22fb90e5acd5ec1aa24cb7b9c087a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548298
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303701}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader_unittest.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/url_loader_unittest.cc
@@ -254,7 +254,7 @@ class TestURLLoaderClient : public URLLoaderClient {
     }
   }
 
-  void DidReceiveData(const char* data, size_t dataLength) override {
+  void DidReceiveData(base::span<const char> data) override {
     NOTREACHED_IN_MIGRATION();
   }
 
--- third_party/blink/renderer/platform/testing/url_loader_test_delegate.h
+++ third_party/blink/renderer/platform/testing/url_loader_test_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_TESTING_URL_LOADER_TEST_DELEGATE_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_TESTING_URL_LOADER_TEST_DELEGATE_H_
 
+#include "base/containers/span.h"
 #include "base/time/time.h"
 #include "third_party/blink/public/platform/web_common.h"
 
@@ -28,8 +29,7 @@ class URLLoaderTestDelegate {
   virtual void DidReceiveResponse(URLLoaderClient* original_client,
                                   const WebURLResponse&);
   virtual void DidReceiveData(URLLoaderClient* original_client,
-                              const char* data,
-                              size_t data_length);
+                              base::span<const char> data);
   virtual void DidFail(URLLoaderClient* original_client,
                        const WebURLError&,
                        int64_t total_encoded_data_length,
