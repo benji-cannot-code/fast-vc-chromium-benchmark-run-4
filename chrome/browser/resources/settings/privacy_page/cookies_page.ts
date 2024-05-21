137e7746015b6fce99dfb9f08185f229b88bd443
Fiona Macintosh <fmacintosh@google.com>
2024-05-21 16:10:26 +0000

    [TP] Add Tracking Protection exceptions list to settings page
    This just adds the component, content based on user state (i.e. pre-3PC
    with ACT vs. TP) will be sent as a followup
    
    Bug: b:334970346
    Change-Id: I181443847df3192953b5200e9133833e10b49d31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5463519
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Kevin Graney <kmg@google.com>
    Commit-Queue: Fiona Macintosh <fmacintosh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303812}

--- chrome/browser/resources/settings/privacy_page/cookies_page.ts
+++ chrome/browser/resources/settings/privacy_page/cookies_page.ts
@@ -95,6 +95,11 @@ export class SettingsCookiesPageElement extends SettingsCookiesPageElementBase {
         value: ContentSettingsTypes.COOKIES,
       },
 
+      trackingProtectionContentSettingType_: {
+        type: String,
+        value: ContentSettingsTypes.TRACKING_PROTECTION,
+      },
+
       exceptionListsReadOnly_: {
         type: Boolean,
         value: false,
@@ -115,6 +120,12 @@ export class SettingsCookiesPageElement extends SettingsCookiesPageElementBase {
       enableFirstPartySetsUI_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('firstPartySetsUIEnabled'),
+      },
+
+      enableTrackingProtectionRolloutUx_: {
+        type: Boolean,
+        value: () =>
+            loadTimeData.getBoolean('enableTrackingProtectionRolloutUx'),
       },
 
       is3pcdRedesignEnabled_: {
@@ -150,10 +161,12 @@ export class SettingsCookiesPageElement extends SettingsCookiesPageElementBase {
 
   searchTerm: string;
   private cookiesContentSettingType_: ContentSettingsTypes;
+  private trackingProtectionContentSettingType_: ContentSettingsTypes;
   private exceptionListsReadOnly_: boolean;
   private blockAllPref_: chrome.settingsPrivate.PrefObject;
   focusConfig: FocusConfig;
   private enableFirstPartySetsUI_: boolean;
+  private enableTrackingProtectionRolloutUx_: boolean;
   private is3pcdRedesignEnabled_: boolean;
   private isIpProtectionAvailable_: boolean;
   private isFingerprintingProtectionAvailable_: boolean;
