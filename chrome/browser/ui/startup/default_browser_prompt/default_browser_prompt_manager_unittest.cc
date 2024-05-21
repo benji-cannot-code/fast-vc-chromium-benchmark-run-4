f544ab03336d82a78bcffbdf0c9e44a38f1a75e7
Alison Gale <agale@chromium.org>
2024-05-21 17:38:53 +0000

    Fix default prompt crash when browser is closed
    Chrome can still be running in the background after the browser is
    closed. In that case, don't write the prefs because they will be
    handled during the next startup (similar to what happens when the timer
    expires and Chrome is completely off).
    
    Bug: 341855213
    Change-Id: I40962867ce1c10b04a330460a048bab5af5df2ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553315
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303876}

--- chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager_unittest.cc
+++ chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
+#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/startup/default_browser_prompt/default_browser_prompt_prefs.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/common/pref_names.h"
@@ -386,4 +387,23 @@ TEST_F(DefaultBrowserPromptManagerTest, StopShowingIfFirstShownTimeTooOld) {
   EXPECT_EQ(base::Time::Now(),
             local_state()->GetTime(prefs::kDefaultBrowserLastDeclinedTime));
   EXPECT_EQ(2, local_state()->GetInteger(prefs::kDefaultBrowserDeclinedCount));
+}
+
+// This is a regression test for a crash that occurred when the default prompt
+// timer expired after a browser was closed.
+TEST_F(DefaultBrowserPromptManagerTest, DoesNotWritePrefWhenBrowserIsClosed) {
+  EnableDefaultBrowserPromptRefreshFeatureWithParams(
+      {{features::kShowDefaultBrowserAppMenuChip.name, "true"},
+       {features::kDefaultBrowserAppMenuDuration.name, "1d"}});
+  chrome::startup::default_prompt::MaybeResetAppMenuPromptPrefs(profile());
+  manager()->MaybeShowPrompt();
+  EXPECT_TRUE(manager()->get_show_app_menu_prompt());
+  EXPECT_EQ(0, local_state()->GetInteger(prefs::kDefaultBrowserDeclinedCount));
+
+  // Close browser then trigger timer
+  BrowserList::RemoveBrowser(BrowserList::GetInstance()->GetLastActive());
+  task_environment()->FastForwardBy(base::Days(1));
+
+  EXPECT_FALSE(manager()->get_show_app_menu_prompt());
+  EXPECT_EQ(0, local_state()->GetInteger(prefs::kDefaultBrowserDeclinedCount));
 }
