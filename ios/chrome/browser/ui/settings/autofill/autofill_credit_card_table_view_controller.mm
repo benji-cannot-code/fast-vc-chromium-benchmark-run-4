38c4252dbba1f6652fb351e41a0610a8b26a251e
Noémie St-Onge <noemies@google.com>
2024-05-21 16:15:00 +0000

    [iOS] Fix VoiceOver shift when switching toggle in payment settings
    When switching the "Save and Fill Payment Methods" toggle in payment
    settings, the VoiceOver focus would temporarily move to the below table
    view cell, before going back to its original position.
    
    When toggling the switch, the "AutofillPaymentMethodsEnabled" pref is
    also updated, ultimately triggering a data reload of the table view. It
    seems that VoiceOver wasn't able to properly process the simultaneous UI
    updates of updating the toggle state and reloading the table view,
    causing it to move its focus when it shouldn't have.
    
    This CL resolves the issue by slightly delaying updating the pref when
    VoiceOver is running. The delay is small enough to not be noticeable.
    
    Fixed: 326923292
    Change-Id: I9d22cfe20883cc52ff8530a076b24d5602d7021c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544692
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1303818}

--- ios/chrome/browser/ui/settings/autofill/autofill_credit_card_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/autofill/autofill_credit_card_table_view_controller.mm
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+constexpr base::TimeDelta kUpdatePrefDelay = base::Seconds(0.3);
+
 enum SectionIdentifier : NSInteger {
   SectionIdentifierAutofillCardSwitch = kSectionIdentifierEnumZero,
   SectionIdentifierMandatoryReauthSwitch,
@@ -508,8 +510,20 @@ using autofill::autofill_metrics::MandatoryReauthOptInOrOutSource;
   [self setSwitchItemOn:[switchView isOn]
                itemType:ItemTypeAutofillCardSwitch
       sectionIdentifier:SectionIdentifierAutofillCardSwitch];
-  [self setAutofillCreditCardEnabled:[switchView isOn]];
-  self.addButtonInToolbar.enabled = [self isAutofillCreditCardEnabled];
+
+  // Delay updating the pref when VoiceOver is running to prevent a temporary
+  // focus shift due to simultaneous UI updates, see crbug.com/326923292.
+  if (UIAccessibilityIsVoiceOverRunning()) {
+    __weak __typeof(self) weakSelf = self;
+    base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+        FROM_HERE, base::BindOnce(^{
+          [weakSelf
+              updateAutofillCreditCardPrefAndToolbarForState:[switchView isOn]];
+        }),
+        kUpdatePrefDelay);
+  } else {
+    [self updateAutofillCreditCardPrefAndToolbarForState:[switchView isOn]];
+  }
 }
 
 - (void)mandatoryReauthSwitchChanged:(UISwitch*)switchView {
@@ -855,6 +869,13 @@ using autofill::autofill_metrics::MandatoryReauthOptInOrOutSource;
   LogMandatoryReauthOptInOrOutUpdateEvent(
       MandatoryReauthOptInOrOutSource::kSettingsPage,
       /*opt_in=*/!mandatoryReauthEnabled, flow_event);
+}
+
+// Updates the Autofill Credit Card pref and the view controller's toolbar
+// according to the provided `enabled` state.
+- (void)updateAutofillCreditCardPrefAndToolbarForState:(BOOL)enabled {
+  [self setAutofillCreditCardEnabled:enabled];
+  self.addButtonInToolbar.enabled = [self isAutofillCreditCardEnabled];
 }
 
 #pragma mark - AutofillAddCreditCardCoordinatorDelegate
