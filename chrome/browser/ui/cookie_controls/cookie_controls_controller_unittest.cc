60d1ac68bd048a32efd946e716e6fc30b3ed8a3f
Matt Dembski <dembski@google.com>
2024-05-21 20:14:26 +0000

    Fix crash in CookieControlsController::OnEntryPointAnimated()
    The crash happens when the WebContents is not prepared before the animation is called. The fix is to check if the WebContents is valid before accessing it.
    
    Bug:341576798
    
    Change-Id: I3ae02be88d915b6ec1e3381419a749ee70c09ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5550353
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Auto-Submit: Matt Dembski <dembski@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304004}

--- chrome/browser/ui/cookie_controls/cookie_controls_controller_unittest.cc
+++ chrome/browser/ui/cookie_controls/cookie_controls_controller_unittest.cc
@@ -51,18 +51,13 @@ constexpr char kCookieControlsActivatedSiteDataAccessHistogram[] =
 class MockCookieControlsObserver
     : public content_settings::CookieControlsObserver {
  public:
-  MOCK_METHOD(void,
-              OnStatusChanged,
+  MOCK_METHOD(void, OnStatusChanged,
               (/*controls_visible*/ bool,
-               /*protections_on*/ bool,
-               CookieControlsEnforcement,
-               CookieBlocking3pcdStatus,
-               base::Time));
-  MOCK_METHOD(void,
-              OnCookieControlsIconStatusChanged,
+               /*protections_on*/ bool, CookieControlsEnforcement,
+               CookieBlocking3pcdStatus, base::Time));
+  MOCK_METHOD(void, OnCookieControlsIconStatusChanged,
               (/*icon_visible*/ bool,
-               /*protections_on*/ bool,
-               CookieBlocking3pcdStatus,
+               /*protections_on*/ bool, CookieBlocking3pcdStatus,
                /*should_highlight*/ bool));
   MOCK_METHOD(void, OnFinishedPageReloadWithChangedSettings, ());
 };
@@ -133,10 +128,8 @@ class CookieControlsUserBypassTest : public ChromeRenderViewHostTestHarness,
   }
 
   void ValidateCookieControlsActivatedUKM(
-      bool fed_cm_initiated,
-      bool storage_access_api_requested,
-      int page_refresh_count,
-      bool repeated_activation,
+      bool fed_cm_initiated, bool storage_access_api_requested,
+      int page_refresh_count, bool repeated_activation,
       blink::mojom::EngagementLevel site_engagement_level,
       ThirdPartySiteDataAccessType site_data_access_type) {
     auto entries = ukm_recorder_->GetEntriesByName(
@@ -1333,6 +1326,21 @@ TEST_P(CookieControlsUserBypassTest, SandboxedTopLevelFrame) {
                            CookieBlocking3pcdStatus::kNotIn3pcd,
                            /*should_highlight=*/false));
   cookie_controls()->Update(web_contents());
+  testing::Mock::VerifyAndClearExpectations(mock());
+}
+
+TEST_P(CookieControlsUserBypassTest,
+       FrequentPageReloadsWithoutUpdateBeingCalled) {
+  NavigateAndCommit(GURL("https://example.com"));
+  // Call the entry point animated function without setting up the observer.
+  cookie_controls()->OnEntryPointAnimated();
+  auto* hcsm = HostContentSettingsMapFactory::GetForProfile(profile());
+
+  // A setting is not recorded.
+  base::Value stored_value =
+      hcsm->GetWebsiteSetting(GURL("https://example.com"), GURL(),
+                              ContentSettingsType::COOKIE_CONTROLS_METADATA);
+  EXPECT_TRUE(stored_value.is_none());
   testing::Mock::VerifyAndClearExpectations(mock());
 }
 
