016e6eacd1a5f21da03a363cb572dd797793c6d3
Kenichi Ishibashi <bashi@chromium.org>
2024-05-21 02:03:36 +0000

    Revert "chromebox_for_meetings: Add Hotline to CrOSAPI"
    This reverts commit b9643b0bdf47279520eb6d81edf568fa344bf81e.
    
    Reason for revert: Broke the build: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-lacros-dbg/33988/overview
    Original change's description:
    > chromebox_for_meetings: Add Hotline to CrOSAPI
    >
    > Add CfmServiceContext to CrOSAPI to allow access in LaCrOS.
    >
    > Bug: b:340146720
    > Test: autoninja -C out/Default/ chromeos:chromeos_unittests && ./out/Default/chromeos_unittests --dbus-stub --single-process-tests --gtest_filter=Cfm*
    > Test: autoninja -C out/Default/ chrome/test:unit_tests && xvfb-run ./out/Default/unit_tests --single-process-tests --gtest_filter=Cfm*
    > Change-Id: I3bdb40a70a6163f1d7695117ba3c8c03c2be7e22
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538699
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    > Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1303539}
    
    Bug: b:340146720
    Change-Id: I32f3254c245054e08b4bd25f4ac4ae8f983a477b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5550739
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Owners-Override: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303562}

--- chromeos/lacros/lacros_service.h
+++ chromeos/lacros/lacros_service.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/crosapi/mojom/video_capture.mojom.h"
 #include "chromeos/crosapi/mojom/volume_manager.mojom.h"
 #include "chromeos/lacros/lacros_service_never_blocking_state.h"
-#include "chromeos/services/chromebox_for_meetings/public/mojom/cfm_service_manager.mojom.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
 #include "components/policy/core/common/policy_namespace.h"
 #include "mojo/public/cpp/bindings/generic_pending_receiver.h"
@@ -204,11 +203,6 @@ class COMPONENT_EXPORT(CHROMEOS_LACROS) LacrosService {
 
   // This may be called on any thread.
   void BindBrowserCdmFactory(mojo::GenericPendingReceiver receiver);
-
-  // This may be called on any thread.
-  void BindCfmServiceContext(
-      mojo::PendingReceiver<chromeos::cfm::mojom::CfmServiceContext>
-          pending_receiver);
 
   // This may be called on any thread.
   void BindGeolocationService(
