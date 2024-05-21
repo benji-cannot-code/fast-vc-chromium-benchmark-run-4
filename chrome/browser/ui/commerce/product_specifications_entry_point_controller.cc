9ac3a8cff7f0c3d6d782ed4a7a9f1f143685b187
Hao Liu <haoliuk@chromium.org>
2024-05-21 20:25:24 +0000

    Revert "Hide product specification button when current window is not eligible"
    This reverts commit 5ef74130ceeb6dc17c769debe11c0ee9accabe34.
    
    Reason for revert: Failing tests. https://ci.chromium.org/ui/p/chromium/builders/ci/Mac13%20Tests/13605/overview
    
    Original change's description:
    > Hide product specification button when current window is not eligible
    >
    > While the tab strip entry point for product specifications is still
    > showing, it might become invalid if the window is no longer eligible.
    > This could be triggered by:
    > * Tab belongs to the current cluster being removed via closure, tab
    >   movement, etc.
    > * Tab belongs to the current cluster is navigated and no longer
    >   belongs to the current cluster.
    >
    > This CL hides the entry point when the window is no longer eligible.
    >
    > Bug: b/325661685
    > Change-Id: I20365e5d09cb6b0a01790c5b549775edaf55698e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5550594
    > Reviewed-by: Matthew Jones <mdjones@chromium.org>
    > Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    > Reviewed-by: Emily Shack <emshack@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1303868}
    
    Bug: b/325661685
    Change-Id: I62328add542662c989b117601ac90b329417dba0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553915
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Hao Liu <haoliuk@chromium.org>
    Auto-Submit: Hao Liu <haoliuk@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304015}

--- chrome/browser/ui/commerce/product_specifications_entry_point_controller.cc
+++ chrome/browser/ui/commerce/product_specifications_entry_point_controller.cc
@@ -17,20 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Number of URLs of the same cluster that a window needs to contain in order
-// for the entry point to stay valid.
-constexpr int kEligibleWindowUrlCountForValidation = 2;
-// Number of URLs of the same cluster that a window needs to contain in order
-// for the entry point to trigger for navigation.
-constexpr int kEligibleWindowUrlCountForNavigationTriggering = 3;
+constexpr int kEligibleWindowUrlCountForNavigation = 3;
 
-bool CheckWindowContainsEntryPointURLs(
+bool IsNavigationEligibleForEntryPoint(
     TabStripModel* tab_strip_model,
-    commerce::EntryPointInfo entry_point_info,
-    size_t threshold) {
+    commerce::EntryPointInfo entry_point_info) {
   std::set<GURL> similar_urls =
       entry_point_info.similar_candidate_products_urls;
-  if (similar_urls.size() < threshold) {
+  if (similar_urls.size() < kEligibleWindowUrlCountForNavigation) {
     return false;
   }
   std::set<GURL> eligible_urls_in_current_window;
@@ -38,26 +32,14 @@ bool CheckWindowContainsEntryPointURLs(
     GURL tab_url = tab_strip_model->GetWebContentsAt(i)->GetLastCommittedURL();
     if (similar_urls.find(tab_url) != similar_urls.end()) {
       eligible_urls_in_current_window.insert(tab_url);
-      if (eligible_urls_in_current_window.size() >= threshold) {
+      if (eligible_urls_in_current_window.size() >=
+          kEligibleWindowUrlCountForNavigation) {
         return true;
       }
     }
   }
-  return eligible_urls_in_current_window.size() >= threshold;
-}
-
-bool IsWindowValidForEntryPoint(TabStripModel* tab_strip_model,
-                                commerce::EntryPointInfo entry_point_info) {
-  return CheckWindowContainsEntryPointURLs(
-      tab_strip_model, entry_point_info, kEligibleWindowUrlCountForValidation);
-}
-
-bool IsNavigationEligibleForEntryPoint(
-    TabStripModel* tab_strip_model,
-    commerce::EntryPointInfo entry_point_info) {
-  return CheckWindowContainsEntryPointURLs(
-      tab_strip_model, entry_point_info,
-      kEligibleWindowUrlCountForNavigationTriggering);
+  return eligible_urls_in_current_window.size() >=
+         kEligibleWindowUrlCountForNavigation;
 }
 
 }  // namespace
@@ -85,9 +67,6 @@ void ProductSpecificationsEntryPointController::OnTabStripModelChanged(
     TabStripModel* tab_strip_model,
     const TabStripModelChange& change,
     const TabStripSelectionChange& selection) {
-  if (change.type() == TabStripModelChange::Type::kRemoved) {
-    MaybeHideEntryPoint();
-  }
   // Filter out non-tab-selection events.
   if (change.type() != TabStripModelChange::Type::kSelectionOnly ||
       !selection.active_tab_changed() || !selection.old_contents ||
@@ -110,10 +89,8 @@ void ProductSpecificationsEntryPointController::TabChangedAt(
     content::WebContents* contents,
     int index,
     TabChangeType change_type) {
-  if (change_type == TabChangeType::kAll &&
-      contents->GetLastCommittedURL() != last_committed_url_) {
+  if (change_type == TabChangeType::kAll) {
     last_committed_url_ = contents->GetLastCommittedURL();
-    MaybeHideEntryPoint();
   }
 }
 
@@ -171,17 +148,6 @@ void ProductSpecificationsEntryPointController::OnClusterFinishedForNavigation(
   current_entry_point_info_ = entry_point_info;
   for (auto& observer : observers_) {
     observer.ShowEntryPointWithTitle(entry_point_info->title);
-  }
-}
-
-void ProductSpecificationsEntryPointController::MaybeHideEntryPoint() {
-  if (!current_entry_point_info_.has_value() ||
-      IsWindowValidForEntryPoint(browser_->tab_strip_model(),
-                                 current_entry_point_info_.value())) {
-    return;
-  }
-  for (auto& observer : observers_) {
-    observer.HideEntryPoint();
   }
 }
 }  // namespace commerce
--- chrome/browser/ui/commerce/product_specifications_entry_point_controller.h
+++ chrome/browser/ui/commerce/product_specifications_entry_point_controller.h
@@ -30,9 +30,6 @@ class ProductSpecificationsEntryPointController
    public:
     // Called when entry points should show with `title`.
     virtual void ShowEntryPointWithTitle(const std::string title) {}
-
-    // Called when entry points should hide.
-    virtual void HideEntryPoint() {}
   };
 
   explicit ProductSpecificationsEntryPointController(Browser* browser);
@@ -73,8 +70,6 @@ class ProductSpecificationsEntryPointController
   }
 
  private:
-  void MaybeHideEntryPoint();
-
   // Info of the entry point that is currently showing, when available.
   std::optional<EntryPointInfo> current_entry_point_info_;
   raw_ptr<Browser, DanglingUntriaged> browser_;
