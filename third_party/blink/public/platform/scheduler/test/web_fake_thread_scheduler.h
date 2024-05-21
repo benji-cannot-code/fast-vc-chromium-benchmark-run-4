6a1a1836e905463f0b878021e1ab38de8b16c08e
Min Chen <minch@chromium.org>
2024-05-21 02:59:21 +0000

    Revert "Remove WebThreadScheduler::CompositorTaskRunner"
    This reverts commit fd7a732bd8024d9182b1d237f1cc45319d0d6757.
    
    Reason for revert: Cause some tests failure on chromeos uprev
    see b/341794182 for more details.
    
    Original change's description:
    > Remove WebThreadScheduler::CompositorTaskRunner
    >
    > We had a whole task queue associated for the only caller of falling
    > back to software mode. Just use the default IPC task runner for this.
    >
    > Change-Id: I29bc6ca00ab66e591c10f233204ebbc26639ba9d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545793
    > Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Scott Haseley <shaseley@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1302846}
    
    Change-Id: I6d63108be8e7ecaba431535286e46eb634604d2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552319
    Reviewed-by: David Munro <davidmunro@google.com>
    Commit-Queue: David Munro <davidmunro@google.com>
    Owners-Override: David Munro <davidmunro@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1303570}

--- third_party/blink/public/platform/scheduler/test/web_fake_thread_scheduler.h
+++ third_party/blink/public/platform/scheduler/test/web_fake_thread_scheduler.h
@@ -22,6 +22,7 @@ class WebFakeThreadScheduler : public WebThreadScheduler {
 
   // RendererScheduler implementation.
   std::unique_ptr<MainThread> CreateMainThread() override;
+  scoped_refptr<base::SingleThreadTaskRunner> CompositorTaskRunner() override;
   std::unique_ptr<WebAgentGroupScheduler> CreateWebAgentGroupScheduler()
       override;
   void SetRendererHidden(bool hidden) override;
