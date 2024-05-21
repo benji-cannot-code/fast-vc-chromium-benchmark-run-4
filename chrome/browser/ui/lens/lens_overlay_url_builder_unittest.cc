9b90e5d77a36c3fb935605f61c313b77cb16f873
Jason Hu <hujasonx@google.com>
2024-05-21 23:13:45 +0000

    [lensoverlay] Add Lens footprints url param
    Bug: 341339414
    Change-Id: I0c5244ba52f3b403ba2466f105057ccd25cda28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555864
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304118}

--- chrome/browser/ui/lens/lens_overlay_url_builder_unittest.cc
+++ chrome/browser/ui/lens/lens_overlay_url_builder_unittest.cc
@@ -243,7 +243,7 @@ TEST_F(LensOverlayUrlBuilderTest, BuildLensSearchURLEmptyClusterInfo) {
 
   std::string expected_url = base::StringPrintf(
       "%s?cs=0&source=chrome.cr.menu&gsc=1&hl=%s&q=%s&lns_mode=mu&"
-      "gsessionid=&udm=24&vsrid=%s",
+      "lns_fp=1&gsessionid=&udm=24&vsrid=%s",
       kResultsSearchBaseUrl, kLanguage, escaped_text_query.c_str(),
       EncodeRequestId(request_id.get()).c_str());
 
@@ -274,7 +274,7 @@ TEST_F(LensOverlayUrlBuilderTest, BuildLensSearchURLWithSessionId) {
 
   std::string expected_url = base::StringPrintf(
       "%s?cs=0&source=chrome.cr.menu&gsc=1&hl=%s&q=%s&lns_mode=mu&"
-      "gsessionid=%s&udm=24&vsrid=%s",
+      "lns_fp=1&gsessionid=%s&udm=24&vsrid=%s",
       kResultsSearchBaseUrl, kLanguage, escaped_text_query.c_str(),
       search_session_id.c_str(), EncodeRequestId(request_id.get()).c_str());
 
@@ -309,7 +309,7 @@ TEST_F(LensOverlayUrlBuilderTest, BuildLensSearchURLWithNoTextQuery) {
 
   std::string expected_url = base::StringPrintf(
       "%s?cs=0&source=chrome.cr.menu&gsc=1&hl=%s&q=&lns_mode=un&"
-      "gsessionid=%s&udm=26&vsrid=%s",
+      "lns_fp=1&gsessionid=%s&udm=26&vsrid=%s",
       kResultsSearchBaseUrl, kLanguage, search_session_id.c_str(),
       encoded_request_id.c_str());
 
@@ -345,7 +345,7 @@ TEST_F(LensOverlayUrlBuilderTest, BuildLensSearchURLWithAdditionalParams) {
 
   std::string expected_url = base::StringPrintf(
       "%s?cs=0&source=chrome.cr.menu&param=value&gsc=1&hl=%s&q=&lns_"
-      "mode=un&gsessionid=%s&udm=26&"
+      "mode=un&lns_fp=1&gsessionid=%s&udm=26&"
       "vsrid=%s",
       kResultsSearchBaseUrl, kLanguage, search_session_id.c_str(),
       encoded_request_id.c_str());
