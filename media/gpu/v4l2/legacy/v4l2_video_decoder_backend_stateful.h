3ccd00a79172f52e41d43c28cc302511976a138f
Miguel Casas <mcasas@chromium.org>
2024-05-21 15:52:21 +0000

    media/gpu/v4l2: move V4L2StatefulVideoDecoderBackend to legacy/
    Since it's only used ATM for ARCVM on Trogdor; this is to avoid
    confusion by developers reading //media/gpu/v4l2.
    
    No functional changes. Done via
    > git mv media/gpu/v4l2/v4l2_video_decoder_backend_stateful.* media/gpu/v4l2/legacy/
    > tools/git/mass-rename.py
    
    Bug: 333469457
    Change-Id: Id4b3bb23c20bb4b1bebbfd54925a7df8cdeb8a90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546268
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303800}

--- media/gpu/v4l2/legacy/v4l2_video_decoder_backend_stateful.h
+++ media/gpu/v4l2/legacy/v4l2_video_decoder_backend_stateful.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
-#define MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
+#ifndef MEDIA_GPU_V4L2_LEGACY_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
+#define MEDIA_GPU_V4L2_LEGACY_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
 
 #include <memory>
 #include <optional>
@@ -179,4 +179,4 @@ class V4L2StatefulVideoDecoderBackend : public V4L2VideoDecoderBackend {
 
 }  // namespace media
 
-#endif  // MEDIA_GPU_V4L2_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
+#endif  // MEDIA_GPU_V4L2_LEGACY_V4L2_VIDEO_DECODER_BACKEND_STATEFUL_H_
