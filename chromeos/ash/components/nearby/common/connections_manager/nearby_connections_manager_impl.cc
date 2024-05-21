f2b1882faea04820751c7a06c90fbd85f50d87ae
Crisrael Lucero <crisrael@google.com>
2024-05-21 16:51:20 +0000

    [Nearby Presence] Add V3 metric for Connection Success rate
    Add a histogram to track the success rate for connecting to a remote
    device upon receiving the `OnConnectionResultV3()` callback.
    
    Test: modified unit tests
    Fixed: b/321750086, b/321749982
    Change-Id: I4405fdd9573f43b813c62ba90b5680fef542cb07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5512929
    Reviewed-by: Jack Shira <jackshira@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303840}

--- chromeos/ash/components/nearby/common/connections_manager/nearby_connections_manager_impl.cc
+++ chromeos/ash/components/nearby/common/connections_manager/nearby_connections_manager_impl.cc
@@ -1012,15 +1012,14 @@ void NearbyConnectionsManagerImpl::OnConnectionInitiatedV3(
 void NearbyConnectionsManagerImpl::OnConnectionResultV3(
     const std::string& endpoint_id,
     Status status) {
-  CD_LOG(INFO, Feature::NEARBY_INFRA)
-      << __func__ << ": OnConnectionResult result=" << status;
+  CD_LOG(INFO, Feature::NEARBY_INFRA) << __func__ << ": result=" << status;
 
   auto it = pending_outgoing_connections_.find(endpoint_id);
   if (it == pending_outgoing_connections_.end()) {
-    connection_listener_v3s_.ReportBadMessage(
-        base::StringPrintf("OnConnectionResult() received endpoint_id=%s which "
-                           "does not exist in connections V3",
-                           endpoint_id.c_str()));
+    connection_listener_v3s_.ReportBadMessage(base::StringPrintf(
+        "OnConnectionResultV3() received endpoint_id=%s which "
+        "does not exist in connections V3",
+        endpoint_id.c_str()));
     return;
   }
 
@@ -1035,6 +1034,8 @@ void NearbyConnectionsManagerImpl::OnConnectionResultV3(
     std::move(it->second).Run(/*nearby_connection=*/nullptr);
   }
 
+  base::UmaHistogramEnumeration("Nearby.Connections.V3.Connection.Result",
+                                status);
   pending_outgoing_connections_.erase(it);
   connect_timeout_timers_v3_.erase(endpoint_id);
 }
