50e52b1b49cd2f88bb73f346019748bd534a8776
Menghan Yang <myuu@google.com>
2024-05-21 15:22:19 +0000

    [UNOp2][Android] Update sign-in bottom sheet layout
    A new design needs to be applied to the account picker when UNO is
    enabled.
    
    Following https://crrev.com/c/5513326, this CL duplicates the remaining
    layout files for the sign-in bottom sheet, and applies the new design
    to the corresponding files.
    
    It also adds a Space at the bottom of the collapsed bottom sheet,
    to adapt the bottom margin when the dismiss button is gone.
    
    See https://crbug.com/335383200#comment2 for screenshots.
    
    Bug: 335383200
    Change-Id: I7ace9e5ad02fbc725c838e78cc426384ed917a73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542074
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303773}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetView.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetView.java
@@ -14,6 +14,7 @@ import android.view.View.OnClickListener;
 import android.view.ViewGroup.MarginLayoutParams;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageView;
+import android.widget.Space;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -87,6 +88,7 @@ class AccountPickerBottomSheetView implements BottomSheetContent {
     private final RecyclerView mAccountListView;
     private final View mSelectedAccountView;
     private final ButtonCompat mDismissButton;
+    private final Space mDismissButtonGoneMarginSpace;
 
     /**
      * @param activity The activity that hosts this view. Used for inflating views.
@@ -121,6 +123,10 @@ class AccountPickerBottomSheetView implements BottomSheetContent {
                 mViewFlipper
                         .getChildAt(ViewState.COLLAPSED_ACCOUNT_LIST)
                         .findViewById(R.id.account_picker_dismiss_button);
+        mDismissButtonGoneMarginSpace =
+                mViewFlipper
+                        .getChildAt(ViewState.COLLAPSED_ACCOUNT_LIST)
+                        .findViewById(R.id.account_picker_dismiss_button_gone_margin_space);
 
         setUpContinueButton(
                 mViewFlipper.getChildAt(ViewState.NO_ACCOUNTS),
@@ -224,10 +230,10 @@ class AccountPickerBottomSheetView implements BottomSheetContent {
         }
 
         if (cancelButton == 0) {
-            mDismissButton.setVisibility(View.GONE);
+            showDismissButton(false);
         } else {
             mDismissButton.setText(cancelButton);
-            mDismissButton.setVisibility(View.VISIBLE);
+            showDismissButton(true);
         }
     }
 
@@ -340,6 +346,16 @@ class AccountPickerBottomSheetView implements BottomSheetContent {
         }
     }
 
+    private void showDismissButton(boolean shouldShow) {
+        if (shouldShow) {
+            mDismissButton.setVisibility(View.VISIBLE);
+            mDismissButtonGoneMarginSpace.setVisibility(View.GONE);
+        } else {
+            mDismissButton.setVisibility(View.GONE);
+            mDismissButtonGoneMarginSpace.setVisibility(View.VISIBLE);
+        }
+    }
+
     // TODO(b/40944124): Move the layout configurations to the xml file after UNO is launched.
     private void revampSelectedAccountView() {
         Context context = mSelectedAccountView.getContext();
@@ -349,7 +365,7 @@ class AccountPickerBottomSheetView implements BottomSheetContent {
         int padding = ViewUtils.dpToPx(context, 16);
         mSelectedAccountView.setPadding(padding, padding, padding, padding);
         int horizontalMargin = ViewUtils.dpToPx(context, 24);
-        int bottomMargin = ViewUtils.dpToPx(context, 16);
+        int bottomMargin = ViewUtils.dpToPx(context, 12);
         MarginLayoutParams params = (MarginLayoutParams) mSelectedAccountView.getLayoutParams();
         params.setMargins(
                 /* left= */ horizontalMargin,
