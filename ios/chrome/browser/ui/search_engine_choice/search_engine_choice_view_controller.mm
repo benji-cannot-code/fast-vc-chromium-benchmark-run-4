3d41df1a5cab31b58c5b7e39083a4514cbae0881
Jérôme Lebel <jlebel@chromium.org>
2024-05-21 16:33:33 +0000

    [iOS][Waffle] Enable search engine dialog EGTests
    Re-enable testSearchEngineChoiceScreenSelectThenScroll and
    testSearchEngineChoiceScreenScrollThenSelect.
    
    To avoid having flakiness with the more button,
    switches::kEeaListCountryOverride needs to be used.
    
    Bug: 329210226
    Fixed: b/329579023
    Change-Id: I40326a62539a00ccb21c4ed7d06c69d527627015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513327
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303829}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.mm
@@ -74,9 +74,6 @@ SnippetSearchEngineButton* CreateSnippetSearchEngineButtonWithElement(
   button.snippetText = element.snippetDescription;
   button.translatesAutoresizingMaskIntoConstraints = NO;
   button.searchEngineKeyword = element.keyword;
-  button.accessibilityIdentifier =
-      [NSString stringWithFormat:@"%@%@", kSnippetSearchEngineIdentifierPrefix,
-                                 element.name];
   return button;
 }
 
@@ -138,7 +135,6 @@ UIButton* CreateSetAsDefaultButton() {
   SetConfigurationTitle(
       button, l10n_util::GetNSString(IDS_SEARCH_ENGINE_CHOICE_BUTTON_TITLE));
   button.translatesAutoresizingMaskIntoConstraints = NO;
-  button.accessibilityIdentifier = kSetAsDefaultSearchEngineIdentifier;
   // Add semantic group, so the user can skip all the search engine stack view,
   // and jump to the SetAsDefault button, using VoiceOver.
   button.accessibilityContainerType = UIAccessibilityContainerTypeSemanticGroup;
@@ -360,6 +356,8 @@ UIButton* CreateMorePillButton() {
   _floatingSetAsDefaultButton = CreateSetAsDefaultButton();
   _floatingSetAsDefaultButton.translatesAutoresizingMaskIntoConstraints = NO;
   [_floatingSetAsDefaultButtonContainer addSubview:_floatingSetAsDefaultButton];
+  _floatingSetAsDefaultButton.accessibilityIdentifier =
+      kSetAsDefaultSearchEngineIdentifier;
   _floatingSetAsDefaultButtonContainer.accessibilityContainerType =
       UIAccessibilityContainerTypeSemanticGroup;
   [_floatingSetAsDefaultButton addTarget:self
@@ -372,6 +370,7 @@ UIButton* CreateMorePillButton() {
   _morePillButton = CreateMorePillButton();
   _morePillButton.translatesAutoresizingMaskIntoConstraints = NO;
   [view addSubview:_morePillButton];
+  _morePillButton.accessibilityIdentifier = kSearchEngineMoreButtonIdentifier;
   [_morePillButton addTarget:self
                       action:@selector(moreButtonAction)
             forControlEvents:UIControlEventTouchUpInside];
