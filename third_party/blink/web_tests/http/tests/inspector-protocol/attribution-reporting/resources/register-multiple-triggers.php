6baf366fca4860c0f1cca0af53c406df99cb874a
Anthony Garant <anthonygarant@chromium.org>
2024-05-21 16:13:22 +0000

    Revert "Log audit issues upon receiving duplicate web ARA registration headers"
    This reverts commit 86f842e68a04dce46c23355944bfd70764fef824.
    
    Reason for revert: Non-coalescing ARA headers will be reverted. As such, we must revert this change first. Some WPT covering the status quo behavior will be re-added in a follow up CL.
    
    Original change's description:
    > Log audit issues upon receiving duplicate web ARA registration headers
    >
    > The issue description will be updated in this CL:
    > https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5544984
    >
    > Change-Id: Ie0de73c368bcaff61b98054ea32dd8ffb514d334
    > Bug: 40242261
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544682
    > Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    > Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1302546}
    
    Bug: 40242261
    Change-Id: Ic9466c3b5a185bfdf048a0c01b07eef9f3f18d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552872
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303815}

--- third_party/blink/web_tests/http/tests/inspector-protocol/attribution-reporting/resources/register-multiple-triggers.php
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<?php
-header('Attribution-Reporting-Register-Trigger: {}');
-header('Attribution-Reporting-Register-Trigger: {}', /*replace=*/false);
-?>
