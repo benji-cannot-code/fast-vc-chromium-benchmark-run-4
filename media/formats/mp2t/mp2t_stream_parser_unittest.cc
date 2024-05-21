7e2bd1f2975dea18e9d3bc6b9ab2488e30cfb744
Feras Aldahlawi <feras@chromium.org>
2024-05-21 19:56:20 +0000

    [media] Convert std::unique_ptr<T[]> to HeapArray<T> in media/formats/*
    See
    https://docs.google.com/document/d/1YsPR8GoN8VTP1ABKCISaQkuBif1Cn80cTxTjsM8QT4s
    for an explanation on why this is favorable.
    
    Bug: b/40285824, b/326459033, b/326459152, b/326459328
    Change-Id: Id51b510ba45d4de4420e7675e19db1b8e7732d8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548554
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Feras Aldahlawi <feras@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303989}

--- media/formats/mp2t/mp2t_stream_parser_unittest.cc
+++ media/formats/mp2t/mp2t_stream_parser_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/formats/mp2t/mp2t_stream_parser.h"
 
+#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -12,9 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include <openssl/aes.h>
-#include <openssl/evp.h>
-
+#include "base/containers/heap_array.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
@@ -89,11 +89,10 @@ std::string DecryptSampleAES(const std::string& key,
                               reinterpret_cast<const uint8_t*>(iv.data()), 0),
             1);
   EVP_CIPHER_CTX_set_padding(ctx.get(), 0);
-  const size_t output_size = input_size;
-  std::unique_ptr<char[]> output(new char[output_size]);
+  auto output = base::HeapArray<char>::Uninit(input_size);
   uint8_t* in_ptr = const_cast<uint8_t*>(input);
-  uint8_t* out_ptr = reinterpret_cast<uint8_t*>(output.get());
-  size_t bytes_remaining = output_size;
+  uint8_t* out_ptr = reinterpret_cast<uint8_t*>(output.data());
+  size_t bytes_remaining = output.size();
 
   while (bytes_remaining) {
     int unused;
@@ -116,7 +115,7 @@ std::string DecryptSampleAES(const std::string& key,
     }
   }
 
-  result.assign(output.get(), output_size);
+  result.assign(output.begin(), output.end());
   return result;
 }
 
--- media/formats/webm/cluster_builder.h
+++ media/formats/webm/cluster_builder.h
@@ -10,25 +10,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/containers/heap_array.h"
+
 namespace media {
 
 class Cluster {
  public:
   Cluster() = delete;
 
-  Cluster(std::unique_ptr<uint8_t[]> data, int size);
+  // The size of the `bytes_used` might be less size of `data`.
+  Cluster(base::HeapArray<uint8_t> data, int bytes_used);
 
   Cluster(const Cluster&) = delete;
   Cluster& operator=(const Cluster&) = delete;
 
   ~Cluster();
 
-  const uint8_t* data() const { return data_.get(); }
-  int size() const { return size_; }
+  // TODO(frs): This should be changed to return a span.
+  const uint8_t* data() const { return data_.data(); }
+  int bytes_used() const { return bytes_used_; }
 
  private:
-  std::unique_ptr<uint8_t[]> data_;
-  int size_;
+  base::HeapArray<uint8_t> data_;
+  const int bytes_used_;
 };
 
 class ClusterBuilder {
@@ -73,7 +77,7 @@ class ClusterBuilder {
                              const uint8_t* data,
                              int size);
   void Reset();
-  void ExtendBuffer(int bytes_needed);
+  void ExtendBuffer(size_t bytes_needed);
   void UpdateUInt64(int offset, int64_t value);
   void WriteBlock(uint8_t* buf,
                   int track_num,
@@ -82,9 +86,8 @@ class ClusterBuilder {
                   const uint8_t* data,
                   int size);
 
-  std::unique_ptr<uint8_t[]> buffer_;
-  int buffer_size_;
-  int bytes_used_;
+  base::HeapArray<uint8_t> buffer_;
+  size_t bytes_used_;
   int64_t cluster_timecode_;
 };
 
