db4ec8bb1cedc035ce0a741423de91c95da107d3
Victor-Gabriel Savu <vsavu@google.com>
2024-05-21 14:03:35 +0000

    lacros: DeviceAutoUpdateDisabled must prevent lacros updates
    The existing DeviceAutoUpdateDisabled policy controls updates on
    ChromeOS, it must also prevent updates of stateful lacros.
    
    The default behavior is unchanged and this CL will only have an impact
    when stateful lacros is rolled out for managed users.
    
    Bug: b:330507074
    Test: Check for component update with DeviceAutoUpdateDisabled true and false and see the log output
    Change-Id: I74f8e1b434e4e8e0c38bf8ac7eb7e60b75a5c7cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490433
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1303729}

--- chrome/browser/component_updater/cros_component_installer_chromeos.cc
+++ chrome/browser/component_updater/cros_component_installer_chromeos.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/dbus/dbus_thread_manager.h"
 #include "chromeos/ash/components/dbus/image_loader/image_loader_client.h"
+#include "chromeos/ash/components/settings/cros_settings.h"
+#include "chromeos/ash/components/settings/cros_settings_names.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/component_updater/component_updater_paths.h"
 #include "components/component_updater/component_updater_switches.h"
@@ -128,6 +130,8 @@ std::vector<ComponentConfig> GetInstalled() {
   return configs;
 }
 
+const bool kDefaultLacrosAllowUpdates = true;
+
 }  // namespace
 
 CrOSComponentInstallerPolicy::CrOSComponentInstallerPolicy(
@@ -151,6 +155,10 @@ bool CrOSComponentInstallerPolicy::SupportsGroupPolicyEnabledComponentUpdates()
 }
 
 bool CrOSComponentInstallerPolicy::RequiresNetworkEncryption() const {
+  return true;
+}
+
+bool CrOSComponentInstallerPolicy::AllowUpdates() const {
   return true;
 }
 
@@ -282,6 +290,26 @@ LacrosInstallerPolicy::GetInstallerAttributes() const {
 // Lacros is supposed to be updated even if component updates are turned off.
 bool LacrosInstallerPolicy::SupportsGroupPolicyEnabledComponentUpdates() const {
   return false;
+}
+
+bool LacrosInstallerPolicy::AllowUpdates() const {
+  bool allow_updates = kDefaultLacrosAllowUpdates;
+
+  ash::CrosSettings* settings = ash::CrosSettings::Get();
+  if (!settings) {
+    return allow_updates;
+  }
+
+  const base::Value* os_updates_disabled =
+      settings->GetPref(ash::kUpdateDisabled);
+  if (os_updates_disabled == nullptr || !os_updates_disabled->is_bool()) {
+    return allow_updates;
+  }
+
+  // We disable Lacros updates when ChromeOS system updates are disabled.
+  allow_updates = !os_updates_disabled->GetBool();
+
+  return allow_updates;
 }
 
 // static
--- chrome/browser/component_updater/cros_component_installer_chromeos.h
+++ chrome/browser/component_updater/cros_component_installer_chromeos.h
@@ -86,6 +86,7 @@ class CrOSComponentInstallerPolicy : public ComponentInstallerPolicy {
   base::FilePath GetRelativeInstallDir() const override;
   void GetHash(std::vector<uint8_t>* hash) const override;
   std::string GetName() const override;
+  bool AllowUpdates() const override;
 
  protected:
   const raw_ptr<CrOSComponentInstaller, DanglingUntriaged>
@@ -138,6 +139,7 @@ class LacrosInstallerPolicy : public CrOSComponentInstallerPolicy {
                       base::Value::Dict manifest) override;
   update_client::InstallerAttributes GetInstallerAttributes() const override;
   bool SupportsGroupPolicyEnabledComponentUpdates() const override;
+  bool AllowUpdates() const override;
 
   static void SetAshVersionForTest(const char* version);
 };
