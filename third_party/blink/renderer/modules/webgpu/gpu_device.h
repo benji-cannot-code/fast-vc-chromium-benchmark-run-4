50068977a64bc6b391610efce4fbb667ff84abfc
Austin Eng <enga@chromium.org>
2024-05-21 06:02:25 +0000

    Use C++ callbacks for requestAdapter/requestDevice/popErrorScope
    Bug: 40195122
    Change-Id: I99f8d93a86e23cf2b2bc1bec76a76bc449770c42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546695
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Loko Kung <lokokung@google.com>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303615}

--- third_party/blink/renderer/modules/webgpu/gpu_device.h
+++ third_party/blink/renderer/modules/webgpu/gpu_device.h
@@ -185,7 +185,8 @@ class GPUDevice final : public EventTarget,
 
   void OnPopErrorScopeCallback(
       ScriptPromiseResolver<IDLNullable<GPUError>>* resolver,
-      WGPUErrorType type,
+      wgpu::PopErrorScopeStatus status,
+      wgpu::ErrorType type,
       const char* message);
 
   void OnCreateRenderPipelineAsyncCallback(
