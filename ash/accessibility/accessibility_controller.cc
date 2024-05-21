eb1250fb7fe4f2f2379ed145b70d89847e0a1510
Tzarial Kuznia <zork@google.com>
2024-05-21 14:18:46 +0000

    [a11y] Add use primary keys toggle to mouse keys.
    This allows you to switch between using the primary keyboard and the num
    pad.
    
    BUG=b:259372916
    TEST=Enable feature flag.  Go to the mouse keys settings page.  Check
    that the 'Use primary keyboard' option works.
    
    Change-Id: Ief46f5cc6f7ba53e5b3328a6ee4651beedeac052
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5531103
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Commit-Queue: Tzarial Kuznia <zork@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303736}

--- ash/accessibility/accessibility_controller.cc
+++ ash/accessibility/accessibility_controller.cc
@@ -263,6 +263,7 @@ constexpr const char* const kCopiedOnSigninAccessibilityPrefs[]{
     prefs::kAccessibilityMouseKeysEnabled,
     prefs::kAccessibilityMouseKeysAcceleration,
     prefs::kAccessibilityMouseKeysMaxSpeed,
+    prefs::kAccessibilityMouseKeysUsePrimaryKeys,
     prefs::kAccessibilityMouseKeysDominantHand,
     prefs::kAccessibilityScreenMagnifierEnabled,
     prefs::kAccessibilityScreenMagnifierFocusFollowingEnabled,
@@ -1264,6 +1265,8 @@ void AccessibilityController::RegisterProfilePrefs(
                                MouseKeysController::kDefaultAcceleration);
   registry->RegisterDoublePref(prefs::kAccessibilityMouseKeysMaxSpeed,
                                MouseKeysController::kDefaultMaxSpeed);
+  registry->RegisterBooleanPref(prefs::kAccessibilityMouseKeysUsePrimaryKeys,
+                                true);
   registry->RegisterIntegerPref(
       prefs::kAccessibilityMouseKeysDominantHand,
       static_cast<int>(MouseKeysDominantHand::kRightHandDominant));
@@ -2323,6 +2326,11 @@ void AccessibilityController::ObservePrefs(PrefService* prefs) {
             &AccessibilityController::UpdateMouseKeysMaxSpeedFromPref,
             base::Unretained(this)));
     pref_change_registrar_->Add(
+        prefs::kAccessibilityMouseKeysUsePrimaryKeys,
+        base::BindRepeating(
+            &AccessibilityController::UpdateMouseKeysUsePrimaryKeysFromPref,
+            base::Unretained(this)));
+    pref_change_registrar_->Add(
         prefs::kAccessibilityMouseKeysDominantHand,
         base::BindRepeating(
             &AccessibilityController::UpdateMouseKeysDominantHandFromPref,
@@ -2423,6 +2431,7 @@ void AccessibilityController::ObservePrefs(PrefService* prefs) {
   if (::features::IsAccessibilityMouseKeysEnabled()) {
     UpdateMouseKeysAccelerationFromPref();
     UpdateMouseKeysMaxSpeedFromPref();
+    UpdateMouseKeysUsePrimaryKeysFromPref();
     UpdateMouseKeysDominantHandFromPref();
   }
   UpdateFloatingMenuPositionFromPref();
@@ -2517,6 +2526,13 @@ void AccessibilityController::UpdateMouseKeysMaxSpeedFromPref() {
   double max_speed =
       active_user_prefs_->GetDouble(prefs::kAccessibilityMouseKeysMaxSpeed);
   Shell::Get()->mouse_keys_controller()->SetMaxSpeed(max_speed);
+}
+
+void AccessibilityController::UpdateMouseKeysUsePrimaryKeysFromPref() {
+  DCHECK(active_user_prefs_);
+  bool value = active_user_prefs_->GetBoolean(
+      prefs::kAccessibilityMouseKeysUsePrimaryKeys);
+  Shell::Get()->mouse_keys_controller()->set_use_primary_keys(value);
 }
 
 void AccessibilityController::UpdateMouseKeysDominantHandFromPref() {
