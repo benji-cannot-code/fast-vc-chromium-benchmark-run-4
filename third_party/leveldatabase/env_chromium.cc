3bf9ce2a4f16d7767803976411b22c5150c7e52f
Evan Stade <estade@chromium.org>
2024-05-21 22:59:34 +0000

    IDB: remove debug code for LOCK file failures.
    This reverts commits 4490965817518382bc7af3b808b69e84e29a26d5 and
    8b120bdc24358d2d717cd58ee565f31b47cea80 given that a plausible
    mechanism for double-locking has been found.
    
    We'll continue to investigate with some new histograms.
    
    Bug: 340398745
    Change-Id: If040f4290c0c1e37dcc7609779b3f200045e0ffd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555929
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304107}

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -889,6 +889,7 @@ Status ChromiumEnv::RenameFile(const std::string& src, const std::string& dst) {
 
 Status ChromiumEnv::LockFile(const std::string& fname, FileLock** lock) {
   *lock = nullptr;
+  Status result;
   const base::FilePath path = base::FilePath::FromUTF8Unsafe(fname);
   Retrier retrier;
   FileErrorOr<std::unique_ptr<storage::FilesystemProxy::FileLock>> lock_result;
@@ -901,7 +902,7 @@ Status ChromiumEnv::LockFile(const std::string& fname, FileLock** lock) {
   }
 
   *lock = new ChromiumFileLock(std::move(lock_result.value()), fname);
-  return Status::OK();
+  return result;
 }
 
 Status ChromiumEnv::UnlockFile(FileLock* lock) {
