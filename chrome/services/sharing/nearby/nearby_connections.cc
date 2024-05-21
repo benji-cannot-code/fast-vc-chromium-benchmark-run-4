422cb14ff5d548d7ab077b247754a93ad2a94f42
Alex Kingsborough <akingsb@google.com>
2024-05-21 23:30:02 +0000

    Remove NS_LOG macro
    NS_LOG has successfully been replaced with CD_LOG so the associated
    files are no longer needed. This commit cleans up the old NS_LOG files
    and any associated includes.
    
    Test: CQ passing
    Change-Id: I4df117252ea0a22daa4f8ad6cee680715ed7de50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5554528
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304127}

--- chrome/services/sharing/nearby/nearby_connections.cc
+++ chrome/services/sharing/nearby/nearby_connections.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/time/time.h"
-#include "chrome/browser/nearby_sharing/logging/logging.h"
 #include "chrome/services/sharing/nearby/nearby_connections_conversions.h"
 #include "chrome/services/sharing/nearby/platform/input_file.h"
 #include "chromeos/ash/components/nearby/presence/conversions/nearby_presence_conversions.h"
