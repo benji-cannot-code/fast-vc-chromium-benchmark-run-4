0fb5a6b67128e58180c041d9544d5ec2eefb2f7c
Andrew Grieve <agrieve@chromium.org>
2024-05-21 15:31:46 +0000

    Android: Prevent new Robolectric shadows via an allowlist
    Bug: b/341267427
    Change-Id: I7e02f3ef4ef69930579727735acdb40b0581622c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545930
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303784}

--- testing/android/junit/java/src/org/chromium/testing/local/TestListComputer.java
+++ testing/android/junit/java/src/org/chromium/testing/local/TestListComputer.java
@@ -35,8 +35,13 @@ import java.util.TreeSet;
 
 class TestListComputer extends Computer {
     private final List<Description> mDescriptions = new ArrayList<>();
+    private final Allowlist mShadowsAllowlist;
 
-    private static String computeConfig(
+    TestListComputer(Allowlist shadowsAllowlist) {
+        mShadowsAllowlist = shadowsAllowlist;
+    }
+
+    private String computeConfig(
             Description description,
             Set<String> instrumentedPackages,
             Set<String> instrumentedClasses) {
@@ -65,6 +70,9 @@ class TestListComputer extends Computer {
                 if (className.isEmpty()) {
                     className = annotation.value().getName();
                 }
+                if (!mShadowsAllowlist.allow(className)) {
+                    throwShadowException(clazz.getName(), className);
+                }
                 instrumentedClasses.add(className);
             }
         }
@@ -80,6 +88,21 @@ class TestListComputer extends Computer {
             }
         }
         return looperMode + sdkSuffix;
+    }
+
+    private void throwShadowException(String shadowClass, String shadowingClass) {
+        String msg =
+                """
+
+            Found non-allowlisted Robolectric shadow: %s (shadowing %s).
+            Please limit usage of shadows to non-application code by adding explicit test stubbing \
+            logic via set*ForTesting() methods.
+
+            See: https://chromium.googlesource.com/chromium/src/+/main/styleguide/java/java.md#testing
+            Used allowlist: %s
+            """
+                        .formatted(shadowClass, shadowingClass, mShadowsAllowlist.getFilename());
+        throw new RuntimeException(msg);
     }
 
     private class TestListRunner extends Runner implements Filterable {
@@ -148,6 +171,12 @@ class TestListComputer extends Computer {
     }
 
     public void writeJson(File outputFile) throws FileNotFoundException, JSONException {
+        try (PrintStream stream = new PrintStream(new FileOutputStream(outputFile))) {
+            stream.print(createJson());
+        }
+    }
+
+    JSONObject createJson() throws JSONException {
         var instrumentedPackages = new TreeSet<String>();
         var instrumentedClasses = new TreeSet<String>();
         JSONObject root = new JSONObject();
@@ -172,9 +201,6 @@ class TestListComputer extends Computer {
         for (String s : instrumentedClasses) {
             arr.put(s);
         }
-
-        try (PrintStream stream = new PrintStream(new FileOutputStream(outputFile))) {
-            stream.print(root);
-        }
+        return root;
     }
 }
