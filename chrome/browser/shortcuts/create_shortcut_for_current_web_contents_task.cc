f57eeb2ca14df2150800e36c60e0c1937619a4b2
Dibyajyoti Pal <dibyapal@google.com>
2024-05-21 16:13:49 +0000

    [Shortcuts] DocumentIconFetcher runs a single task to fetch icons
    This CL is essentially a no-op, and has been confirmed with manual
    testing and by running the existing browser tests (thanks for those!).
    
    The DocumentIconFetcher was initially built with the assumption that it can trigger multiple icon downloading tasks. Now that the entire system
    has been built, there can be only a single document icon download call
    triggered for the current document. As a result, it does not need
    the multiple task architecture.
    
    This CL cleans up the old code, and makes the DocumentIconFetcher perform the icon fetching itself. The old contract of the task
    being self destructed and any pending callbacks run asynchronously during destruction are still maintained.
    
    Bug: 333024272
    Change-Id: Iba5bd8573a84bfdc2b1dbc6d18ec72182fe835b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546540
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1303816}

--- chrome/browser/shortcuts/create_shortcut_for_current_web_contents_task.cc
+++ chrome/browser/shortcuts/create_shortcut_for_current_web_contents_task.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/bind_post_task.h"
 #include "chrome/browser/platform_util.h"  // nogncheck (crbug.com/335727004)
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/shortcuts/document_icon_fetcher.h"
+#include "chrome/browser/shortcuts/document_icon_fetcher_task.h"
 #include "chrome/browser/shortcuts/icon_badging.h"
 #include "chrome/browser/shortcuts/shortcut_creator.h"
 #include "chrome/common/chrome_features.h"
@@ -102,15 +102,18 @@ void CreateShortcutForCurrentWebContentsTask::FetchIcons(
   callback_ = std::move(callback);
   Observe(&web_contents);
 
-  DocumentIconFetcher::FetchIcons(
+  icon_fetcher_task_ = std::make_unique<DocumentIconFetcherTask>(
       web_contents, base::BindOnce(&CreateShortcutForCurrentWebContentsTask::
                                        OnIconsFetchedStartBadgingAndShowDialog,
                                    weak_ptr_factory_.GetWeakPtr()));
+  icon_fetcher_task_->StartIconFetching();
 }
 
 void CreateShortcutForCurrentWebContentsTask::
     OnIconsFetchedStartBadgingAndShowDialog(
         FetchIconsFromDocumentResult result) {
+  CHECK(icon_fetcher_task_);
+  icon_fetcher_task_.reset();
   if (!result.has_value()) {
     OnMetadataFetchCompleteSelfDestruct(
         base::unexpected(ShortcutCreationTaskResult::kIconFetchingFailed));
