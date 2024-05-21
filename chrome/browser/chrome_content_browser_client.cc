783d2e7b4f43b2ab1fce535d248db705b1d708de
Joey Arhar <jarhar@chromium.org>
2024-05-21 14:53:49 +0000

    Add enterprise policy for :--foo deprecation
    I am changing the syntax of the custom state pseudo class from :--foo to
    :state(foo). In order to do that, I am deprecating and eventually
    removing support for :--foo. We have already been emitting a console
    error when :--foo is used for several releases, but in order to actually
    disable it by default we need an enterprise policy in case anyone is
    relying on it.
    
    After having this enterprise policy available, I will disable the
    CSSCustomStateDeprecatedSyntax flag by default, and then after enough
    milestones, I will remove the code for it and retire/deprecate this
    enterprise policy.
    
    This deprecation for the old syntax:
    https://chromestatus.com/feature/5140610730426368
    https://groups.google.com/a/chromium.org/g/blink-dev/c/JvpHoUfhJYE/m/uRtWiqoHAQAJ
    
    The new syntax which has already been enabled by default:
    https://chromestatus.com/feature/5586433790443520
    https://groups.google.com/a/chromium.org/g/blink-dev/c/GDmNaxAuCY4/m/pwH7c4p6AAAJ
    
    Bug: 1514397
    Change-Id: I41785603403087a0a7e2e0d4e38ad2be6b012dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5538599
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1303748}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1610,6 +1610,10 @@ void ChromeContentBrowserClient::RegisterProfilePrefs(
                                 false);
 
   registry->RegisterBooleanPref(
+      policy::policy_prefs::kCSSCustomStateDeprecatedSyntaxEnabled,
+      /*default_value=*/false);
+
+  registry->RegisterBooleanPref(
       policy::policy_prefs::kBeforeunloadEventCancelByPreventDefaultEnabled,
       true);
 
@@ -2684,6 +2688,11 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
               policy::policy_prefs::kKeyboardFocusableScrollersEnabled)) {
         command_line->AppendSwitch(
             blink::switches::kKeyboardFocusableScrollersOptOut);
+      }
+      if (prefs->GetBoolean(
+              policy::policy_prefs::kCSSCustomStateDeprecatedSyntaxEnabled)) {
+        command_line->AppendSwitch(
+            blink::switches::kCSSCustomStateDeprecatedSyntaxEnabled);
       }
 
       if (prefs->GetBoolean(policy::policy_prefs::
