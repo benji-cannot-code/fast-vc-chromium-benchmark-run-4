562e839ad81e3030f3f19facd9210b4b512f7931
Lei Zhang <thestig@chromium.org>
2024-05-22 18:58:23 +0000

    [PDF Ink Signatures] Add InkIntersectsPointWithShape() and stub
    Add a utility function that checks if a point intersects a shape. This
    will be used to determine if the eraser tool is making contact with a
    stroke. The stub implementation simply returns false.
    
    Bug: 335521183
    Change-Id: Ia955acf94d58f2e55f697faa42f4f7fe9b666db2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556415
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304591}

---
+++ pdf/ink/ink_intersects.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_INK_INK_INTERSECTS_H_
+#define PDF_INK_INK_INTERSECTS_H_
+
+namespace chrome_pdf {
+
+class InkModeledShape;
+struct InkAffineTransform;
+
+bool InkIntersectsPointWithShape(float point_x,
+                                 float point_y,
+                                 const InkModeledShape& shape,
+                                 const InkAffineTransform& transform);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_INK_INK_INTERSECTS_H_
