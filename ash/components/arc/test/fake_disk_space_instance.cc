0aa6de143c47fa812622d6fa26bd0f36589f75df
Momoko Hattori <momohatt@google.com>
2024-05-22 07:45:52 +0000

    arc: Add GetApplicationsSize method to ArcDiskSpaceBridge
    To merge ArcStorageManager into ArcDiskSpaceBridge, this CL copies
    ArcStorageManager's only functionality, GetApplicationsSize(), to
    ArcDiskSpaceBridge. This CL only adds the mojo interface and the wrapper
    of the new method, and the actual implementation will be done by
    http://ag/27354382.
    
    BUG=b:340478972
    TEST=See CL:5541515.
    TEST=ash_components_unittests --gtest_filter="ArcDiskSpaceBridgeTest.*"
    
    Change-Id: I44d52083db5ddf7c911a29a94105c1263d58b0cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541157
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Momoko Hattori <momohatt@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Satoshi Niwa <niwa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304277}

---
+++ ash/components/arc/test/fake_disk_space_instance.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/components/arc/test/fake_disk_space_instance.h"
+
+namespace arc {
+
+FakeDiskSpaceInstance::FakeDiskSpaceInstance() = default;
+FakeDiskSpaceInstance::~FakeDiskSpaceInstance() = default;
+
+void FakeDiskSpaceInstance::Init(
+    ::mojo::PendingRemote<mojom::DiskSpaceHost> host_remote,
+    InitCallback callback) {
+  host_remote_.reset();
+  host_remote_.Bind(std::move(host_remote));
+  std::move(callback).Run();
+}
+
+void FakeDiskSpaceInstance::GetApplicationsSize(
+    GetApplicationsSizeCallback callback) {
+  ++num_get_applications_size_called_;
+  mojom::ApplicationsSizePtr size = mojom::ApplicationsSize::New();
+  size->total_code_bytes = 42;
+  size->total_data_bytes = 43;
+  size->total_cache_bytes = 44;
+  std::move(callback).Run(/*succeeded=*/true, std::move(size));
+}
+
+}  // namespace arc
