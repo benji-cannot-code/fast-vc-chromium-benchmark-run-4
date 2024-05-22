8e23e6c7e405aa26c192131deecd164ded7c07df
Yiren Wang <yrw@chromium.org>
2024-05-22 02:14:44 +0000

    Fix source title and device name for MediaItemUIUpdatedView
    Currently a cast media item always has a source title that combines the
    source and cast device name, such as "YouTube · Living room TV". The
    updated UI requires the source and device name to be shown separately,
    so we need to modify the source title and use another variable to record
    the device name.
    
    New UI screenshot: https://screenshot.googleplex.com/AtpKTnYr3Map6y5
    
    Bug: 335309106
    Change-Id: I75be6df0b163314e7e8a6a8cca83b104f79ac6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548984
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304205}

--- chrome/browser/ui/global_media_controls/cast_media_notification_item.cc
+++ chrome/browser/ui/global_media_controls/cast_media_notification_item.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/storage_partition.h"
+#include "media/base/media_switches.h"
 #include "net/base/load_flags.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
 #include "net/url_request/referrer_policy.h"
@@ -129,6 +130,13 @@ media_session::mojom::MediaSessionInfo::SessionState ToSessionState(
 }
 
 std::u16string GetSourceTitle(const media_router::MediaRoute& route) {
+#if !BUILDFLAG(IS_CHROMEOS)
+  // Never include the media sink name for updated media UI on non-CrOS.
+  if (base::FeatureList::IsEnabled(media::kGlobalMediaControlsUpdatedUI)) {
+    return base::UTF8ToUTF16(route.description());
+  }
+#endif
+
   if (route.media_sink_name().empty())
     return base::UTF8ToUTF16(route.description());
 
@@ -161,6 +169,7 @@ CastMediaNotificationItem::CastMediaNotificationItem(
                               base::Unretained(this))),
       session_info_(CreateSessionInfo()) {
   metadata_.source_title = GetSourceTitle(route);
+  device_name_ = route.media_sink_name();
 }
 
 CastMediaNotificationItem::~CastMediaNotificationItem() {
@@ -252,7 +261,9 @@ void CastMediaNotificationItem::OnMediaStatusUpdated(
 
 void CastMediaNotificationItem::OnRouteUpdated(
     const media_router::MediaRoute& route) {
-  DCHECK_EQ(route.media_route_id(), media_route_id_);
+  CHECK_EQ(route.media_route_id(), media_route_id_);
+  device_name_ = route.media_sink_name();
+
   bool updated = false;
   const std::u16string new_source_title = GetSourceTitle(route);
   if (metadata_.source_title != new_source_title) {
--- chrome/browser/ui/global_media_controls/cast_media_notification_item_unittest.cc
+++ chrome/browser/ui/global_media_controls/cast_media_notification_item_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/media_router/common/media_route.h"
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/test/browser_task_environment.h"
+#include "media/base/media_switches.h"
 #include "net/url_request/referrer_policy.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -89,6 +90,9 @@ class MockSessionController : public CastMediaSessionController {
 class CastMediaNotificationItemTest : public testing::Test {
  public:
   void SetUp() override {
+#if !BUILDFLAG(IS_CHROMEOS)
+    feature_list_.InitAndEnableFeature(media::kGlobalMediaControlsUpdatedUI);
+#endif
     auto session_controller =
         std::make_unique<testing::NiceMock<MockSessionController>>(
             mojo::Remote<media_router::mojom::MediaController>());
@@ -119,12 +123,21 @@ class CastMediaNotificationItemTest : public testing::Test {
         .WillOnce([&](const base::flat_set<MediaSessionAction>& actions) {
           EXPECT_EQ(0u, actions.size());
         });
+
+#if BUILDFLAG(IS_CHROMEOS)
     EXPECT_CALL(view_, UpdateWithMediaMetadata(_))
         .WillOnce([&](const media_session::MediaMetadata& metadata) {
           const std::string separator = " \xC2\xB7 ";
           EXPECT_EQ(base::UTF8ToUTF16(kRouteDesc + separator + kSinkName),
                     metadata.source_title);
         });
+#else
+    EXPECT_CALL(view_, UpdateWithMediaMetadata(_))
+        .WillOnce([&](const media_session::MediaMetadata& metadata) {
+          EXPECT_EQ(kRouteDesc, base::UTF16ToUTF8(metadata.source_title));
+        });
+#endif
+
     item_->SetView(&view_);
     testing::Mock::VerifyAndClearExpectations(&view_);
   }
@@ -148,6 +161,7 @@ class CastMediaNotificationItemTest : public testing::Test {
   testing::NiceMock<media_message_center::test::MockMediaNotificationView>
       view_;
   std::unique_ptr<CastMediaNotificationItem> item_;
+  base::test::ScopedFeatureList feature_list_;
 };
 
 TEST_F(CastMediaNotificationItemTest, UpdateSessionInfo) {
@@ -386,4 +400,14 @@ TEST_F(CastMediaNotificationItemTest, StopCasting) {
   EXPECT_CALL(*mock_router, TerminateRoute(item_->route_id()));
   EXPECT_CALL(item_manager_, FocusDialog());
   item_->StopCasting();
+}
+
+TEST_F(CastMediaNotificationItemTest, UpdateMediaSinkName) {
+  EXPECT_EQ(kSinkName, item_->device_name());
+  media_router::MediaRoute route(
+      kRouteId, media_router::MediaSource("source_id"), "sink_id", kRouteDesc,
+      /*is_local=*/true);
+  route.set_media_sink_name("New Sink");
+  item_->OnRouteUpdated(route);
+  EXPECT_EQ(route.media_sink_name(), item_->device_name());
 }
