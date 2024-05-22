0943187db481d7e349550f59082341cf31df47d2
Fiona Macintosh <fmacintosh@google.com>
2024-05-22 09:57:15 +0000

    [TP] Add metrics for Tracking Protection settings page exceptions
    Bug: b:334970346
    Change-Id: I3e9e4173212138d25f274e0b5c015fc60001a28f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527346
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304324}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -1672,6 +1672,11 @@ void SiteSettingsHandler::HandleResetCategoryPermissionForPattern(
     base::RecordAction(base::UserMetricsAction(
         "ThirdPartyCookies.SettingsSiteException.Removed"));
   }
+
+  if (content_type == ContentSettingsType::TRACKING_PROTECTION) {
+    base::RecordAction(base::UserMetricsAction(
+        "Settings.TrackingProtection.SiteExceptionRemoved"));
+  }
 }
 
 void SiteSettingsHandler::HandleSetCategoryPermissionForPattern(
@@ -1750,6 +1755,11 @@ void SiteSettingsHandler::HandleSetCategoryPermissionForPattern(
       !secondary_pattern.MatchesAllHosts()) {
     base::RecordAction(base::UserMetricsAction(
         "ThirdPartyCookies.SettingsSiteException.Added"));
+  }
+
+  if (content_type == ContentSettingsType::TRACKING_PROTECTION) {
+    base::RecordAction(base::UserMetricsAction(
+        "Settings.TrackingProtection.SiteExceptionAdded"));
   }
 }
 
