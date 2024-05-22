b8fd4801b7f3c90d83b01b7f376b927f315720e1
Thiabaud Engelbrecht <thiabaud@google.com>
2024-05-22 14:15:30 +0000

    [pre_freeze] Rename |SetDidRegisterTask| to |RegisterPrivateMemoryFootprintMetric|.
    The previous name is unclear, so rename it to better reflect what it
    does.
    
    Bug: 339114386
    Change-Id: I64b87f4e28d7571c9c7f464efdd27f9e6b819209
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553157
    Reviewed-by: Egor Pasko <pasko@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304410}

--- base/android/pre_freeze_background_memory_trimmer.cc
+++ base/android/pre_freeze_background_memory_trimmer.cc
@@ -179,7 +179,7 @@ void PreFreezeBackgroundMemoryTrimmer::PostDelayedBackgroundTaskInternal(
     base::TimeDelta delay) {
   DCHECK(SupportsModernTrim());
 
-  SetDidRegisterTaskInternal();
+  RegisterPrivateMemoryFootprintMetric();
 
   if (!base::FeatureList::IsEnabled(kOnPreFreezeMemoryTrim)) {
     task_runner->PostDelayedTask(
@@ -296,11 +296,12 @@ void PreFreezeBackgroundMemoryTrimmer::UnregisterBackgroundTaskInternal(
 }
 
 // static
-void PreFreezeBackgroundMemoryTrimmer::SetDidRegisterTask() {
-  Instance().SetDidRegisterTaskInternal();
+void PreFreezeBackgroundMemoryTrimmer::RegisterPrivateMemoryFootprintMetric() {
+  Instance().RegisterPrivateMemoryFootprintMetricInternal();
 }
 
-void PreFreezeBackgroundMemoryTrimmer::SetDidRegisterTaskInternal() {
+void PreFreezeBackgroundMemoryTrimmer::
+    RegisterPrivateMemoryFootprintMetricInternal() {
   base::AutoLock locker(lock_);
   if (!did_register_task_) {
     did_register_task_ = true;
