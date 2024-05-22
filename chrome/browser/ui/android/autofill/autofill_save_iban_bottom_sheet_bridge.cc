fa0c27f3ae0b78fd4da44001c1bf374caa54331d
Verina Armanyous <averina@google.com>
2024-05-22 17:37:03 +0000

    [IBAN][Clank] Add logic to trigger save IBAN bottom sheet 1/X
    Create GetOrCreatAutofillSaveIban() that will launch save IBAN
    bottom sheet on the c++ side. This is a no-op since WindowAndroid
    and TabModel (used for displaying the UI) are currently not used
    on the Java side. The AutofillSaveIbanBottomSheetBridge
    will be updated in crrev.com/c/5482012 to use WindowAndroid
    and TabModel in triggering the bottom sheet UI.
    
    Design doc: go/autofill-iban-dd-clank
    This feature is enabled with the following flag: AUTOFILL_ENABLE_LOCAL_IBAN.
    
    Bug: b/309163103
    Change-Id: If5f48b128c36ff84c449523825c434d23fc2d0be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522299
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304545}

--- chrome/browser/ui/android/autofill/autofill_save_iban_bottom_sheet_bridge.cc
+++ chrome/browser/ui/android/autofill/autofill_save_iban_bottom_sheet_bridge.cc
@@ -10,14 +10,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_string.h"
 #include "chrome/android/chrome_jni_headers/AutofillSaveIbanBottomSheetBridge_jni.h"
 #include "chrome/browser/ui/android/autofill/autofill_save_iban_delegate.h"
+#include "chrome/browser/ui/android/tab_model/tab_model.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/android/window_android.h"
 
 namespace autofill {
 
-AutofillSaveIbanBottomSheetBridge::AutofillSaveIbanBottomSheetBridge()
-    : java_autofill_save_iban_bottom_sheet_bridge_(
-          Java_AutofillSaveIbanBottomSheetBridge_Constructor(
-              base::android::AttachCurrentThread(),
-              reinterpret_cast<jlong>(this))) {}
+AutofillSaveIbanBottomSheetBridge::AutofillSaveIbanBottomSheetBridge(
+    ui::WindowAndroid* window_android,
+    TabModel* tab_model) {
+  CHECK(window_android);
+  CHECK(tab_model);
+  java_autofill_save_iban_bottom_sheet_bridge_ =
+      Java_AutofillSaveIbanBottomSheetBridge_Constructor(
+          base::android::AttachCurrentThread(), reinterpret_cast<jlong>(this),
+          window_android->GetJavaObject(), tab_model->GetJavaObject());
+}
 
 AutofillSaveIbanBottomSheetBridge::~AutofillSaveIbanBottomSheetBridge() {
   if (java_autofill_save_iban_bottom_sheet_bridge_) {
