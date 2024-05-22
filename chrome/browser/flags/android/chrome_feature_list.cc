15ce9dd6e1e5c09eba35ea074110a7c0e18890f2
Tomasz Wiszkowski <ender@google.com>
2024-05-22 19:00:34 +0000

    Clean up launched ClearOmniboxFocusAfterNavigation experiment.
    Change-Id: I40cfce315b83d3b6765fb4038dfce1839681b620
    Fixed: 277805322
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556483
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304592}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -181,7 +181,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kBrowserControlsEarlyResize,
     &kCacheActivityTaskID,
     &kCastDeviceFilter,
-    &kClearOmniboxFocusAfterNavigation,
     &kCCTBeforeUnload,
     &kCCTClientDataHeader,
     &kCCTExtendTrustedCdnPublisher,
@@ -492,10 +491,6 @@ BASE_FEATURE(kCacheActivityTaskID,
 BASE_FEATURE(kCastDeviceFilter,
              "CastDeviceFilter",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kClearOmniboxFocusAfterNavigation,
-             "ClearOmniboxFocusAfterNavigation",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTBeforeUnload,
              "CCTBeforeUnload",
