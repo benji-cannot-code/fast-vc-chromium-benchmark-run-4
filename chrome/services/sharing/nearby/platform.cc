113bae69b691c86d00c826d8186ff7ce09710871
Juliet Levesque <julietlevesque@google.com>
2024-05-22 01:48:09 +0000

    Reland "[Nearby Presence] Flag BleV2Medium creation in platform.cc"
    This is a reland of commit 7885f9bf5626c4e57e9f6eeec19a89bac33c2178
    since the crash fix to handle the nullptr is landing in crrev/c/5556026
    
    Original change's description:
    > [Nearby Presence] Flag BleV2Medium creation in platform.cc
    >
    > Adds a flag to return the BleV2Medium class in platform.cc, which is
    > required in order to prevent BleV2Medium from being returned while
    > we are still in development. Other classes do not need this check
    > because they rely on flags on the other side of the mojo pipe which
    > determine whether or not the passed in Mojo remotes are bound or not,
    > however, since BleV2Medium uses the same BluetoothAdapter remote, we
    > cannot depend on this as a way to flag it's creation, and need to
    > additionally check the flag.
    >
    > Tested: Manually verified on DUT
    > Bug: b/333834234
    > Change-Id: Ic8c4511d397417d59875882154065f51cc80ed55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551113
    > Reviewed-by: Jack Shira <jackshira@google.com>
    > Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1303493}
    
    Bug: b/333834234
    Change-Id: Id50eef1e796b05591af7f2aaaa7276275d5a36ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553023
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304200}

--- chrome/services/sharing/nearby/platform.cc
+++ chrome/services/sharing/nearby/platform.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/nearby/public/mojom/firewall_hole.mojom.h"
 #include "chromeos/ash/services/nearby/public/mojom/tcp_socket_factory.mojom.h"
 #include "chromeos/services/network_config/public/mojom/cros_network_config.mojom.h"
+#include "components/cross_device/nearby/nearby_features.h"
 #include "device/bluetooth/public/mojom/adapter.mojom.h"
 #include "mojo/public/cpp/bindings/shared_remote.h"
 #include "third_party/nearby/src/internal/platform/implementation/atomic_boolean.h"
@@ -279,7 +280,8 @@ std::unique_ptr<ble_v2::BleMedium> ImplementationPlatform::CreateBleV2Medium(
   // created by ImplementationPlatform::CreateBluetoothAdapter(). Instead,
   // directly use the cached bluetooth::mojom::Adapter.
   if (nearby_shared_remotes &&
-      nearby_shared_remotes->bluetooth_adapter.is_bound()) {
+      nearby_shared_remotes->bluetooth_adapter.is_bound() &&
+      features::IsNearbyBleV2Enabled()) {
     return std::make_unique<chrome::BleV2Medium>(
         nearby_shared_remotes->bluetooth_adapter);
   }
