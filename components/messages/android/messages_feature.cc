cbf6f1d0047fe19df025a5f7ef436a70d590c4a5
Sam Maier <smaier@chromium.org>
2024-05-22 18:29:55 +0000

    jni_zero: Fixing up last targets before multiplexing enabled
    These targets had deps onto a big generate_jni, which corresponded to 2
    different native targets. I've just split the generate_jni into 2,
    corresponding to the native targets which include the generated headers.
    
    Bug: 40612925
    Change-Id: I6a8da681127d6668e8b465932f765fe4c58f626b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5558365
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304573}

--- components/messages/android/messages_feature.cc
+++ components/messages/android/messages_feature.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/no_destructor.h"
-#include "components/messages/android/jni_headers/MessageFeatureMap_jni.h"
+#include "components/messages/android/feature_flags_jni_headers/MessageFeatureMap_jni.h"
 
 namespace messages {
 
--- content/app/android/content_child_process_service_delegate.cc
+++ content/app/android/content_child_process_service_delegate.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/child/child_thread_impl.h"
 #include "content/common/android/surface_wrapper.h"
 #include "content/common/shared_file_util.h"
-#include "content/public/android/content_main_dex_jni/ContentChildProcessServiceDelegate_jni.h"
+#include "content/public/android/content_app_jni/ContentChildProcessServiceDelegate_jni.h"
 #include "content/public/common/content_descriptors.h"
 #include "content/public/common/content_switches.h"
 #include "gpu/command_buffer/service/texture_owner.h"
