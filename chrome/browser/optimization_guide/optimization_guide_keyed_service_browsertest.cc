fd5650b290de69b87cc9561812314e1e00e0ac90
Scott Haseley <shaseley@google.com>
2024-05-22 23:27:14 +0000

    Revert "[Cleanup] Simplify the ModelQualityLogEntry destructor"
    This reverts commit c8e5a9e8c14c7d47efdf1c52ee7fa818b01dcb10.
    
    Reason for revert: Compile failures, example:
    https://ci.chromium.org/ui/p/chromium/builders/ci/lacros-amd64-generic-rel-non-skylab/10112/overview
    
    Original change's description:
    > [Cleanup] Simplify the ModelQualityLogEntry destructor
    >
    > * Remove an unnecessary histogram
    > * Move SetSystemProfileProto call into ModelQualityLogsUploaderService,
    >   which is responsible for setting all other similar proto fields.
    > * Remove an unused method from the ModelQualityLogsUploaderService API.
    >
    > OBSOLETE_HISTOGRAMS=Removed in M127, no longer needed.
    >
    > R=curranmax@chromium.org
    >
    > Bug: 341806620
    > Change-Id: I98dd2b0df6d93e4d87cf7ac0dfd1d54f00d376b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551627
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Robert Ogden <robertogden@chromium.org>
    > Auto-Submit: Ilya Sherman <isherman@chromium.org>
    > Reviewed-by: Max Curran <curranmax@chromium.org>
    > Commit-Queue: Robert Ogden <robertogden@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1304745}
    
    Bug: 341806620
    Change-Id: I585cc1f26045eba1e01e374cfb0168a5f0e092fc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560377
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Scott Haseley <shaseley@google.com>
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304760}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
@@ -67,9 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
-#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_network_connection_tracker.h"
-#include "services/network/test/test_url_loader_factory.h"
 
 namespace optimization_guide {
 
@@ -1448,25 +1446,17 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
   ASSERT_FALSE(
       g_browser_process->GetMetricsServicesManager()->IsMetricsConsentGiven());
 
-  // Intercept network requests.
-  network::TestURLLoaderFactory url_loader_factory;
-  service()
-      ->GetChromeModelQualityLogsUploaderService()
-      ->SetUrlLoaderFactoryForTesting(
-          base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
-              &url_loader_factory));
-
   // Create a new ModelQualityLogEntry for compose.
   std::unique_ptr<ModelQualityLogEntry> log_entry =
       GetModelQualityLogEntryForCompose();
 
   // Destruct the log entry, this should trigger uploading the logs.
   log_entry.reset();
+  base::RunLoop().RunUntilIdle();
 
   // Upload should be stopped on destruction as there is no metrics consent.
-  base::RunLoop().RunUntilIdle();
-  content::RunAllTasksUntilIdle();
-  EXPECT_EQ(0, url_loader_factory.NumPending());
+  histogram_tester()->ExpectUniqueSample(
+      "OptimizationGuide.ModelQualityLogEntry.UploadedOnDestruction", false, 1);
 }
 
 IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
@@ -1786,25 +1776,17 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
   EXPECT_TRUE(ogks->GetModelQualityLogsUploaderService()->CanUploadLogs(
       UserVisibleFeatureKey::kCompose));
 
-  // Intercept network requests.
-  network::TestURLLoaderFactory url_loader_factory;
-  service()
-      ->GetChromeModelQualityLogsUploaderService()
-      ->SetUrlLoaderFactoryForTesting(
-          base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
-              &url_loader_factory));
-
   // Create a new ModelQualityLogEntry for compose.
   std::unique_ptr<ModelQualityLogEntry> log_entry =
       GetModelQualityLogEntryForCompose();
 
   // Destruct the log entry, this should upload the logs.
   log_entry.reset();
+  base::RunLoop().RunUntilIdle();
 
   // Logs should be uploaded on destruction.
-  base::RunLoop().RunUntilIdle();
-  content::RunAllTasksUntilIdle();
-  EXPECT_EQ(1, url_loader_factory.NumPending());
+  histogram_tester()->ExpectUniqueSample(
+      "OptimizationGuide.ModelQualityLogEntry.UploadedOnDestruction", true, 1);
 }
 
 }  // namespace optimization_guide
--- components/optimization_guide/core/model_quality/model_quality_logs_uploader_service.h
+++ components/optimization_guide/core/model_quality/model_quality_logs_uploader_service.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/types/optional_ref.h"
 #include "components/optimization_guide/core/model_execution/feature_keys.h"
+#include "components/optimization_guide/core/model_quality/model_quality_logs_uploader.h"
 #include "components/optimization_guide/proto/model_quality_service.pb.h"
 #include "url/gurl.h"
 
@@ -26,8 +27,6 @@ class SharedURLLoaderFactory;
 }  // namespace network
 
 namespace optimization_guide {
-
-class ModelQualityLogEntry;
 
 class ModelQualityLogsUploaderService {
  public:
@@ -49,15 +48,13 @@ class ModelQualityLogsUploaderService {
   // Sets system profile proto corresponding to the logging_metadata.
   virtual void SetSystemProfileProto(proto::LoggingMetadata* logging_metadata);
 
+  void UploadModelQualityLogs(std::unique_ptr<ModelQualityLogEntry> log_entry);
+
   // Returns the WeakPtr for uploading logs during model qualtiy logs
   // destruction.
   base::WeakPtr<ModelQualityLogsUploaderService> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
-
-  // Test-only setter. Pairs well with TestUrlLoaderFactory.
-  void SetUrlLoaderFactoryForTesting(
-      scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory);
 
  private:
   friend class ModelQualityLogsUploaderServiceTest;
