173bb024a3dff10ccd2c25bf542b1b3cef9466af
Jose Dapena Paz <jdapena@igalia.com>
2024-05-22 08:28:16 +0000

    IWYU: usage of std::optional requires include in card_unmask_challenge_option.h
    Bug: 41455655
    Change-Id: I3b0e74b83fed9ae13431b217017dde05fee50d14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556726
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1304295}

--- components/autofill/core/browser/payments/card_unmask_challenge_option.h
+++ components/autofill/core/browser/payments/card_unmask_challenge_option.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_CARD_UNMASK_CHALLENGE_OPTION_H_
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_CARD_UNMASK_CHALLENGE_OPTION_H_
 
+#include <optional>
 #include <string>
 
 #include "base/types/strong_alias.h"
