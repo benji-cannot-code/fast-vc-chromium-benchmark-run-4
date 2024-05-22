96b26e4bcf93ab862b91a036bb91d25b9e6944fe
Jérôme Lebel <jlebel@chromium.org>
2024-05-22 16:08:55 +0000

    [iOS] Cleanup unused API in ManageSyncSettingsCoordinator
    Remove in:
    -[ManageSyncSettingsCoordinator initWithBaseNavigationController:
                                                             browser:
                                                        accountState:]
    -[ManageSyncSettingsCoordinator presentViewController:]
    
    Those APIs are not used anymore after:
    crrev.com/c/5554270
    crrev.com/c/5554370
    
    Fixed: 341217588
    Change-Id: I7b384d8b525f7bfcad815be22b8ec8d3601c1cfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553972
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304472}

--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.h
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.h
@@ -38,13 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Delegate.
 @property(nonatomic, weak) id<ManageSyncSettingsCoordinatorDelegate> delegate;
 
-// Initializes ManageSyncSettingsCoordinator to present its controller modally.
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser
-                              accountState:
-                                  (SyncSettingsAccountState)accountState
-    NS_DESIGNATED_INITIALIZER;
-
 // Initializes ManageSyncSettingsCoordinator to view its controller by pushing
 // it on top of the navigation stack.
 - (instancetype)initWithBaseNavigationController:
--- ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_sync_settings_coordinator.mm
@@ -125,17 +125,6 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 
 @synthesize baseNavigationController = _baseNavigationController;
 
-- (instancetype)initWithBaseViewController:(UIViewController*)viewController
-                                   browser:(Browser*)browser
-                              accountState:
-                                  (SyncSettingsAccountState)accountState {
-  if (self = [super initWithBaseViewController:viewController
-                                       browser:browser]) {
-    _accountState = accountState;
-  }
-  return self;
-}
-
 - (instancetype)initWithBaseNavigationController:
                     (UINavigationController*)navigationController
                                          browser:(Browser*)browser
@@ -217,12 +206,9 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 
   self.mediator.consumer = viewController;
 
-  if (_baseNavigationController) {
-    [self.baseNavigationController pushViewController:viewController
-                                             animated:YES];
-  } else {
-    [self presentViewController:viewController];
-  }
+  CHECK(_baseNavigationController, base::NotFatalUntil::M129);
+  [self.baseNavigationController pushViewController:viewController
+                                           animated:YES];
   _syncObserver = std::make_unique<SyncObserverBridge>(self, self.syncService);
 }
 
@@ -265,18 +251,6 @@ using DismissViewCallback = SystemIdentityManager::DismissViewCallback;
 }
 
 #pragma mark - Private
-
-- (void)presentViewController:(UIViewController*)controller {
-  SettingsNavigationController* navigationController =
-      [[SettingsNavigationController alloc]
-          initWithRootViewController:controller
-                             browser:self.browser
-                            delegate:self];
-  _navigationControllerInModalView = navigationController;
-  [self.baseViewController presentViewController:navigationController
-                                        animated:YES
-                                      completion:nil];
-}
 
 - (void)stopBulkUpload {
   [_bulkUploadCoordinator stop];
