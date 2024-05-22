104bf2be15678d3a066fd2636307222ec34d2ee7
Jose Dapena Paz <jdapena@igalia.com>
2024-05-22 08:41:25 +0000

    GCC: avoid ambiguous type iterating observer in WrapWithPrefixPrefStore
    Bug: 40565911
    Change-Id: Ie6c84dcf3f207db35614461ef4b1b99df769883d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552858
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1304298}

--- components/prefs/wrap_with_prefix_pref_store.cc
+++ components/prefs/wrap_with_prefix_pref_store.cc
@@ -138,7 +138,7 @@ void WrapWithPrefixPrefStore::OnPrefValueChanged(const std::string& key) {
     return;
   }
   std::string original_key(RemoveDottedPrefix(key));
-  for (Observer& observer : observers_) {
+  for (PrefStore::Observer& observer : observers_) {
     observer.OnPrefValueChanged(original_key);
   }
 }
@@ -149,7 +149,7 @@ void WrapWithPrefixPrefStore::OnInitializationCompleted(bool succeeded) {
       read_error_delegate_.has_value() && read_error_delegate_.value()) {
     read_error_delegate_.value()->OnError(read_error);
   }
-  for (Observer& observer : observers_) {
+  for (PrefStore::Observer& observer : observers_) {
     observer.OnInitializationCompleted(succeeded);
   }
 }
