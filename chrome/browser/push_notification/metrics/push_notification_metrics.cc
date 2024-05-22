c1d8be716ac2d4b28fa04dbfe3bbaa8629a3a2ad
Alex Kingsborough <akingsb@google.com>
2024-05-22 16:27:04 +0000

    [Chime] Add metric for response time on API call successes
    Test: added unittests
    Change-Id: I889b45d631f5e03aebcc9e342211f657f7ef3c3c
    Bug: b/340936637
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5543031
    Commit-Queue: Alex Kingsborough <akingsb@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304490}

--- chrome/browser/push_notification/metrics/push_notification_metrics.cc
+++ chrome/browser/push_notification/metrics/push_notification_metrics.cc
@@ -12,6 +12,9 @@ namespace {
 const char kTotalTokenRetrievalTime[] =
     "PushNotification.ChromeOS.GCM.Token.RetrievalTime";
 
+const char kTotalSuccessfulRegistrationResponseTime[] =
+    "PushNotification.ChromeOS.MultiLoginUpdateApi.ResponseTime.Success";
+
 }  // namespace
 
 namespace push_notification::metrics {
@@ -19,6 +22,12 @@ namespace push_notification::metrics {
 void RecordPushNotificationServiceTimeToRetrieveToken(
     base::TimeDelta total_retrieval_time) {
   base::UmaHistogramTimes(kTotalTokenRetrievalTime, total_retrieval_time);
+}
+
+void RecordPushNotificationServiceTimeToReceiveRegistrationSuccessResponse(
+    base::TimeDelta registration_response_time) {
+  base::UmaHistogramTimes(kTotalSuccessfulRegistrationResponseTime,
+                          registration_response_time);
 }
 
 }  // namespace push_notification::metrics
