3a6f9bca7d8e51cd2f8138e04a941c807247b11d
Hung Nguyen <hungmn@google.com>
2024-05-22 22:57:33 +0000

    Rename arc feature guest zram to guest swap
    To prepare for the new virtual swap param to be added to this feature.
    
    BUG=b:329310285
    TEST=ash_components_unittests && unit_tests
    TestWorkingSetTrimmerChromeOS
    
    Change-Id: I0793ba4bdc1b50e6b2ac9eb081c7cbf6d4cde02f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548760
    Commit-Queue: Hung Nguyen <hungmn@google.com>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Auto-Submit: Hung Nguyen <hungmn@google.com>
    Reviewed-by: Alexandre Marciano Gimenez <raging@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304746}

--- chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos_unittest.cc
+++ chrome/browser/performance_manager/mechanisms/working_set_trimmer_chromeos_unittest.cc
@@ -216,7 +216,7 @@ TEST_F(TestWorkingSetTrimmerChromeOS,
   base::test::ScopedFeatureList feature_list;
   base::FieldTrialParams params;
   params["guest_reclaim_enabled"] = "true";
-  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestZram, params);
+  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestSwap, params);
   memory_instance()->set_reclaim_all_result(2, 1);
   // Making arc session trim result to be false to be sure it's not being used.
   FakeArcSessionHolder session_holder(arc_session_runner());
@@ -244,7 +244,7 @@ TEST_F(TestWorkingSetTrimmerChromeOS,
   base::test::ScopedFeatureList feature_list;
   base::FieldTrialParams params;
   params["guest_reclaim_enabled"] = "true";
-  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestZram, params);
+  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestSwap, params);
   memory_instance()->set_reclaim_all_result(0, 0);
 
   std::optional<bool> result;
@@ -270,7 +270,7 @@ TEST_F(TestWorkingSetTrimmerChromeOS,
   base::FieldTrialParams params;
   params["guest_reclaim_enabled"] = "true";
   params["guest_reclaim_only_anonymous"] = "true";
-  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestZram, params);
+  feature_list.InitAndEnableFeatureWithParameters(arc::kGuestSwap, params);
   memory_instance()->set_reclaim_all_result(0, 0);
   memory_instance()->set_reclaim_anon_result(2, 0);
 
@@ -294,7 +294,7 @@ TEST_F(TestWorkingSetTrimmerChromeOS,
   FakeArcSessionHolder session_holder(arc_session_runner());
   session_holder.session()->set_trim_result(true, "");
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndDisableFeature(arc::kGuestZram);
+  feature_list.InitAndDisableFeature(arc::kGuestSwap);
   // If memory_instance is used then the trim operation should fail.
   memory_instance()->set_reclaim_all_result(0, 0);
 
@@ -313,7 +313,7 @@ TEST_F(
   FakeArcSessionHolder session_holder(arc_session_runner());
   session_holder.session()->set_trim_result(true, "");
   base::test::ScopedFeatureList feature_list;
-  feature_list.InitAndEnableFeature(arc::kGuestZram);
+  feature_list.InitAndEnableFeature(arc::kGuestSwap);
   // If memory_instance is used then the trim operation should fail.
   memory_instance()->set_reclaim_all_result(0, 0);
 
