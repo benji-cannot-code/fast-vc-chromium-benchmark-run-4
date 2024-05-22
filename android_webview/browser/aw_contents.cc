6b1d17bd22aa21b56933e225ee129f59c5f876a0
Jiacheng Guo <gjc@google.com>
2024-05-22 01:24:11 +0000

    Prerender: Cancel preredering when adding and removing DocumentStartJavascript
    The addDocumentStartJavascript injects a script to be called in any
    frame whose origin matches allowedOriginRules when the document begins
    to load. The CL cancels all the prerendered pages so that the injected
    scripts can get executed and the removed script no longer gets executed.
    
    Bug: 341600966
    Change-Id: I744a0dc0cc5d74e803ca54d61dfd9c8625f4e6f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547287
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304188}

--- android_webview/browser/aw_contents.cc
+++ android_webview/browser/aw_contents.cc
@@ -1319,6 +1319,7 @@ jint AwContents::AddDocumentStartJavaScript(
   AppendJavaStringArrayToStringVector(env, allowed_origin_rules,
                                       &native_allowed_origin_rule_strings);
   web_contents()->GetController().GetBackForwardCache().Flush();
+  web_contents()->CancelAllPrerendering();
   auto result = GetJsCommunicationHost()->AddDocumentStartJavaScript(
       base::android::ConvertJavaStringToUTF16(env, script),
       native_allowed_origin_rule_strings);
@@ -1332,6 +1333,7 @@ jint AwContents::AddDocumentStartJavaScript(
 }
 
 void AwContents::RemoveDocumentStartJavaScript(JNIEnv* env, jint script_id) {
+  web_contents()->CancelAllPrerendering();
   GetJsCommunicationHost()->RemoveDocumentStartJavaScript(script_id);
 }
 
