430d05d6808b4d5502d8da7b5198327cd1ec2eef
Ryan Sultanem <rsult@google.com>
2024-05-22 14:41:45 +0000

    [Uno-D] Fix issue with signin pending start time resetting
    Unit test
    `SigninMetricsServiceTest.ReceivingMultipleErrorsDoesNotResetPref` did
    not catch the issue because the last error is cached, and the same
    error was sent twice, making the second error not to notify.
    
    Error caching is reset on startup.
    This fix also makes sure that new errors do not trigger a reset
    of the pref time.
    
    For the test, make sure that each error triggers two different errors
    to avoid caching problems.
    
    Bug: 333072181
    Change-Id: Id88e42cc51c186eef81255e12fb3c5ecee147dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557843
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304428}

--- components/signin/core/browser/signin_metrics_service_unittests.cc
+++ components/signin/core/browser/signin_metrics_service_unittests.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/signin/public/identity_manager/identity_test_utils.h"
 #include "google_apis/gaia/core_account_id.h"
+#include "google_apis/gaia/google_service_auth_error.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -116,10 +117,22 @@ class SigninMetricsServiceTest : public ::testing::Test {
     // Calling
     // `IdentityTestEnvironment::SetInvalidRefreshTokenForPrimaryAccount()` will
     // not fire the notification event in unit tests. Directly fire it here.
+    GoogleServiceAuthError error1 =
+        GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
+            GoogleServiceAuthError::InvalidGaiaCredentialsReason::
+                CREDENTIALS_REJECTED_BY_SERVER);
     identity_test_environment_.UpdatePersistentErrorOfRefreshTokenForAccount(
-        account_id, GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
-                        GoogleServiceAuthError::InvalidGaiaCredentialsReason::
-                            CREDENTIALS_REJECTED_BY_CLIENT));
+        account_id, error1);
+
+    // Trigger two different errors to make sure the effect of the error is well
+    // propagated and not dismissed due to caching the last error.
+    GoogleServiceAuthError error2 =
+        GoogleServiceAuthError::FromInvalidGaiaCredentialsReason(
+            GoogleServiceAuthError::InvalidGaiaCredentialsReason::
+                CREDENTIALS_REJECTED_BY_CLIENT);
+    ASSERT_NE(error1, error2);
+    identity_test_environment_.UpdatePersistentErrorOfRefreshTokenForAccount(
+        account_id, error2);
   }
 
   base::test::TaskEnvironment task_environment_;
