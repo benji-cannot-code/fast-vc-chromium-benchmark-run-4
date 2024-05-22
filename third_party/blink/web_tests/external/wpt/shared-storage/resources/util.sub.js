093d1062dbec654fe89a0ce6ac9985f600410e40
Yao Xiao <yaoxia@chromium.org>
2024-05-22 19:19:02 +0000

    [shared storage] Fix some web platform tests
    Issue: Some WPTs finish too early and does not actually wait for the
    async operations. We encountered test failures due to a mojom
    connection CHECK on `CodeCacheHost`, which reveals this underlying
    problem.
    
    This patch fixes those cases. Also, for
    shared-storage-writable-img-idl-attribute-included-bytes.tentative.https.sub.html,
    there are some additional problems:
    - The "+" is a special URL character. Thus, we apply
      encodeURIComponent on the key/value.
    - Passing emoji character directly doesn't seem to work. It's
      being translated to a different/unexpected sequence. In this CL,
      we replace the emoji with explicit UTF-16 code units created from
      String.fromCharCode(). Filed a bug (https://crbug.com/341973427)
      for further investigation.
    
    
    
    Bug: 341893986
    Change-Id: I208e4af410a3a1092a0da88decbe43687fe8b718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556543
    Reviewed-by: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304604}

--- third_party/blink/web_tests/external/wpt/shared-storage/resources/util.sub.js
+++ third_party/blink/web_tests/external/wpt/shared-storage/resources/util.sub.js
@@ -1,8 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function loadSharedStorageImage(data) {
   let {key, value, hasSharedStorageWritableAttribute, isSameOrigin} = data;
+  const encodedKey = encodeURIComponent(key);
+  const encodedValue = encodeURIComponent(value);
   const sameOriginSrc = `/shared-storage/resources/` +
-      `shared-storage-writable-pixel.png?key=${key}&value=${value}`;
+      `shared-storage-writable-pixel.png?key=${encodedKey}&value=${encodedValue}`;
   const crossOriginSrc =
       'https://{{domains[www]}}:{{ports[https][0]}}' + sameOriginSrc;
 
