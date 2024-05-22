00bef492e3d2a2c275c55a2fda839f6a05b9729b
Mohamed Heikal <mheikal@chromium.org>
2024-05-22 20:03:24 +0000

    Add factory methods to ThreadCondition classes
    Make it easier to create simple Conditions from predicate functions.
    
    Change-Id: Ic75646a690302518793458574c6c642133760580
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555261
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304624}

--- base/test/android/javatests/src/org/chromium/base/test/transit/InstrumentationThreadCondition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/InstrumentationThreadCondition.java
@@ -5,9 +5,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.transit;
 
+import java.util.concurrent.Callable;
+
 /** A {@link Condition} that is checked in the instrumentation thread. */
 public abstract class InstrumentationThreadCondition extends Condition {
     public InstrumentationThreadCondition() {
         super(/* isRunOnUiThread= */ false);
+    }
+
+    /**
+     * Create a simple {@link InstrumentationThreadCondition} that does not need any parameters or
+     * to wait on suppliers.
+     */
+    public static InstrumentationThreadCondition from(
+            String description, Callable<ConditionStatus> check) {
+        return new InstrumentationThreadCondition() {
+            @Override
+            public String buildDescription() {
+                return description;
+            }
+
+            @Override
+            public ConditionStatus checkWithSuppliers() {
+                try {
+                    return check.call();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 }
--- base/test/android/javatests/src/org/chromium/base/test/transit/UiThreadCondition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/UiThreadCondition.java
@@ -5,9 +5,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.transit;
 
+import java.util.concurrent.Callable;
+
 /** A {@link Condition} that is checked in the UI thread. */
 public abstract class UiThreadCondition extends Condition {
     public UiThreadCondition() {
         super(/* isRunOnUiThread= */ true);
+    }
+
+    /**
+     * Create a simple {@link UiThreadCondition} that does not need any parameters or to wait on
+     * suppliers.
+     */
+    public static UiThreadCondition from(String description, Callable<ConditionStatus> check) {
+        return new UiThreadCondition() {
+            @Override
+            public String buildDescription() {
+                return description;
+            }
+
+            @Override
+            public ConditionStatus checkWithSuppliers() {
+                try {
+                    return check.call();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
     }
 }
