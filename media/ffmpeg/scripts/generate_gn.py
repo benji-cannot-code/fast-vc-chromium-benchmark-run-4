564f408961128c379aaad505018936ccf333b480
Dale Curtis <dalecurtis@chromium.org>
2024-05-22 00:25:57 +0000

    Remove ChromeOS support from ffmpeg scripts. Fix syntax issues.
    This has been disabled by default since M120 and all LTS issues
    have been resolved as WontFix.
    
    This also fixes a few syntax issues from when the scripts were
    ported over to src.git and adds a help message to ensure folks
    know they should use --auto-merge
    
    R=liberato
    
    Bug: 341986302, 40074555
    Change-Id: I1e89591070e8301c0699cc213512db437af82f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556250
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304161}

--- media/ffmpeg/scripts/generate_gn.py
+++ media/ffmpeg/scripts/generate_gn.py
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python3
 #
-# Copyright (c) 2012 The Chromium Authors.
+# Copyright 2012 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Creates a GN include file for building FFmpeg from source.
@@ -82,7 +82,7 @@ _Attrs = ('ARCHITECTURE', 'TARGET', 'PLATFORM')
 Attr = collections.namedtuple('Attr', _Attrs)(*_Attrs)
 SUPPORT_MATRIX = {
     Attr.ARCHITECTURE: set(['ia32', 'x64', 'arm', 'arm64', 'arm-neon']),
-    Attr.TARGET: set(['Chromium', 'Chrome', 'ChromeOS']),
+    Attr.TARGET: set(['Chromium', 'Chrome']),
     Attr.PLATFORM: set(['android', 'linux', 'win', 'mac'])
 }
 
@@ -373,7 +373,6 @@ class SourceSet(object):
 
         # Write out all C sources.
         c_sources = list(filter(IsCFile, sources))
-        print(c_sources)
         if c_sources:
             stanza += indent(GN_C_SOURCES_BEGIN)
             for name in c_sources:
@@ -493,8 +492,6 @@ def GetAttributeValuesRange(attribute, condition):
     # fragile to changes in our supported platforms. Fortunately, these platforms
     # don't change often. Refactor if we run into trouble.
     platform = condition.PLATFORM
-    if attribute == Attr.TARGET and platform != '*' and platform != 'linux':
-        values.difference_update(['ChromeOS'])
     if attribute == Attr.ARCHITECTURE and platform == 'win':
         values.intersection_update(['ia32', 'x64', 'arm64'])
     if attribute == Attr.ARCHITECTURE and platform == 'mac':
@@ -990,7 +987,8 @@ def main():
                 if not os.path.exists(build_dir):
                     continue
 
-                print(f'Processing build directory: {name} {options.build_dir}')
+                print(
+                    f'Processing build directory: {name} {options.build_dir}')
                 print(build_dir)
                 object_files = GetObjectFiles(build_dir)
                 if not object_files:
@@ -1046,6 +1044,7 @@ def main():
         WriteGn(fd, sets)
 
     subprocess.run(['gn', 'format', gn_file_name])
+
 
 if __name__ == '__main__':
     main()
