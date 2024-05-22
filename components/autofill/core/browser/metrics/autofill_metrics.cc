ddb03a6baae5036de3c4ecbef23d841a5b84836e
Bruno Braga <brunobraga@google.com>
2024-05-22 15:29:16 +0000

    Popup hiding reason is logged per filling product
    This will allow us to have fine grained overview of how feature affects
    the popup hiding, specially because autocomplete and passwords dominate
    this metrics.
    
    This is currently an issue because there is a regression on filling
    acceptance when mullet is on. The fact that this metric is lumped
    together makes it harder to correlate the regression with some popup
    hiding reason.
    
    Bug: b/341916065
    Change-Id: I7fbd25f25be51125ce38cc90407fe371d5865632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5554557
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1304451}

--- components/autofill/core/browser/metrics/autofill_metrics.cc
+++ components/autofill/core/browser/metrics/autofill_metrics.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/field_type_utils.h"
 #include "components/autofill/core/browser/field_types.h"
+#include "components/autofill/core/browser/filling_product.h"
 #include "components/autofill/core/browser/form_structure.h"
 #include "components/autofill/core/browser/form_types.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics_utils.h"
@@ -1594,8 +1595,14 @@ void AutofillMetrics::LogNumberOfAddressesSuppressedForDisuse(
 
 // static
 void AutofillMetrics::LogAutofillSuggestionHidingReason(
+    FillingProduct filling_product,
     SuggestionHidingReason reason) {
   base::UmaHistogramEnumeration("Autofill.PopupHidingReason", reason);
+  base::UmaHistogramEnumeration(base::StrCat({
+                                    "Autofill.PopupHidingReason.",
+                                    FillingProductToString(filling_product),
+                                }),
+                                reason);
 }
 
 void AutofillMetrics::LogSectioningMetrics(
