4cc88197e96a2f8e1a2393d9cd9ea27a4eacff15
Ben Reich <benreich@chromium.org>
2024-05-22 17:57:06 +0000

    [drivefs] Log out the mount and unmount status
    A few bugs have been raised about DriveFS returning empty results and it
    has been identified that potentially DriveFS is remounting or not
    available at the time. We currently can derive the mount/unmounted
    status across a number of logs (drivefs, cros-disks) but the single log
    that is present across all mediums (i.e. crash reports, listnr) is the
    chrome log. So let's ensure this contains the mount and unmount logs as
    well.
    
    Bug: b/337144085, b/342044261, b/339171231
    Change-Id: I8ff1c3b0172f050c5144bf40c2bf831129322d5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553637
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Reviewed-by: Austin Tankiang <austinct@chromium.org>
    Auto-Submit: Ben Reich <benreich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304560}

--- chromeos/ash/components/drivefs/drivefs_host.cc
+++ chromeos/ash/components/drivefs/drivefs_host.cc
@@ -315,6 +315,8 @@ DriveFsHost::~DriveFsHost() {
 
 bool DriveFsHost::Mount() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // TODO(b/336831215): Remove these logs once bug has been fixed.
+  LOG(ERROR) << "DriveFs mounted";
   const AccountId& account_id = delegate_->GetAccountId();
   if (mount_state_ || !account_id.HasAccountIdKey() ||
       account_id.GetUserEmail().empty()) {
@@ -325,6 +327,8 @@ bool DriveFsHost::Mount() {
 }
 
 void DriveFsHost::Unmount() {
+  // TODO(b/336831215): Remove these logs once bug has been fixed.
+  LOG(ERROR) << "DriveFs unmounted";
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   mount_state_.reset();
 }
