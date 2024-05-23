bd429c9840474fb4b913310c52219f8ef0bbd4d3
Danny Wang <wangdanny@google.com>
2024-05-23 23:31:55 +0000

    modifier-split: Add error message if accelerator contains right alt
    Screenshot: http://screen/eUnQFR7JmnGxCmJ
    
    Bug: b/216049298, b/340668387
    Test: ash_webui_unittests
    Change-Id: I980262884559941c63675cd33a7444f6c579dcca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539850
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305410}

--- ash/webui/shortcut_customization_ui/resources/js/accelerator_view.ts
+++ ash/webui/shortcut_customization_ui/resources/js/accelerator_view.ts
@@ -463,6 +463,12 @@ export class AcceleratorViewElement extends AcceleratorViewElementBase {
         this.makeA11yAnnouncement(this.statusMessage);
         return;
       }
+      case AcceleratorConfigResult.kBlockRightAlt: {
+        this.statusMessage = this.i18n('blockRightAltKey');
+        this.hasError = true;
+        this.makeA11yAnnouncement(this.statusMessage);
+        return;
+      }
       case AcceleratorConfigResult.kSuccess: {
         this.fireEditCompletedActionEvent(this.editAction);
         getShortcutProvider().recordAddOrEditSubactions(
