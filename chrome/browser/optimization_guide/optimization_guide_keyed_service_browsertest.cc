6ea24b29aa9758e02ce15e6222cfb610857bca45
Ilya Sherman <isherman@chromium.org>
2024-05-23 08:07:38 +0000

    Reland "[Cleanup] Simplify the ModelQualityLogEntry destructor"
    This reverts commit e7df283f3b5098716a60c14f28115505ad19afff.
    
    Reason for revert: The conflicting CL was also reverted; this one is safe to reland
    
    Original change's description:
    > Revert "[Cleanup] Simplify the ModelQualityLogEntry destructor"
    >
    > This reverts commit c8e5a9e8c14c7d47efdf1c52ee7fa818b01dcb10.
    >
    > Reason for revert: Compile failures, example:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/lacros-amd64-generic-rel-non-skylab/10112/overview
    >
    > Original change's description:
    > > [Cleanup] Simplify the ModelQualityLogEntry destructor
    > >
    > > * Remove an unnecessary histogram
    > > * Move SetSystemProfileProto call into ModelQualityLogsUploaderService,
    > >   which is responsible for setting all other similar proto fields.
    > > * Remove an unused method from the ModelQualityLogsUploaderService API.
    > >
    > > OBSOLETE_HISTOGRAMS=Removed in M127, no longer needed.
    > >
    > > R=curranmax@chromium.org
    > >
    > > Bug: 341806620
    > > Change-Id: I98dd2b0df6d93e4d87cf7ac0dfd1d54f00d376b0
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5551627
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Robert Ogden <robertogden@chromium.org>
    > > Auto-Submit: Ilya Sherman <isherman@chromium.org>
    > > Reviewed-by: Max Curran <curranmax@chromium.org>
    > > Commit-Queue: Robert Ogden <robertogden@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1304745}
    >
    > Bug: 341806620
    > Change-Id: I585cc1f26045eba1e01e374cfb0168a5f0e092fc
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560377
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Scott Haseley <shaseley@google.com>
    > Commit-Queue: Scott Haseley <shaseley@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1304760}
    
    Bug: 341806620
    Change-Id: I49c18de903ac2bc30c66d6e63447d6280ef31b86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560302
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304909}

--- chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
+++ chrome/browser/optimization_guide/optimization_guide_keyed_service_browsertest.cc
@@ -67,7 +67,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source.h"
 #include "services/network/public/cpp/network_connection_tracker.h"
+#include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_network_connection_tracker.h"
+#include "services/network/test/test_url_loader_factory.h"
 
 namespace optimization_guide {
 
@@ -1446,17 +1448,25 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
   ASSERT_FALSE(
       g_browser_process->GetMetricsServicesManager()->IsMetricsConsentGiven());
 
+  // Intercept network requests.
+  network::TestURLLoaderFactory url_loader_factory;
+  service()
+      ->GetChromeModelQualityLogsUploaderService()
+      ->SetUrlLoaderFactoryForTesting(
+          base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+              &url_loader_factory));
+
   // Create a new ModelQualityLogEntry for compose.
   std::unique_ptr<ModelQualityLogEntry> log_entry =
       GetModelQualityLogEntryForCompose();
 
   // Destruct the log entry, this should trigger uploading the logs.
   log_entry.reset();
-  base::RunLoop().RunUntilIdle();
 
   // Upload should be stopped on destruction as there is no metrics consent.
-  histogram_tester()->ExpectUniqueSample(
-      "OptimizationGuide.ModelQualityLogEntry.UploadedOnDestruction", false, 1);
+  base::RunLoop().RunUntilIdle();
+  content::RunAllTasksUntilIdle();
+  EXPECT_EQ(0, url_loader_factory.NumPending());
 }
 
 IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
@@ -1776,17 +1786,25 @@ IN_PROC_BROWSER_TEST_F(OptimizationGuideKeyedServiceBrowserTest,
   EXPECT_TRUE(ogks->GetChromeModelQualityLogsUploaderService()->CanUploadLogs(
       UserVisibleFeatureKey::kCompose));
 
+  // Intercept network requests.
+  network::TestURLLoaderFactory url_loader_factory;
+  service()
+      ->GetChromeModelQualityLogsUploaderService()
+      ->SetUrlLoaderFactoryForTesting(
+          base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
+              &url_loader_factory));
+
   // Create a new ModelQualityLogEntry for compose.
   std::unique_ptr<ModelQualityLogEntry> log_entry =
       GetModelQualityLogEntryForCompose();
 
   // Destruct the log entry, this should upload the logs.
   log_entry.reset();
-  base::RunLoop().RunUntilIdle();
 
   // Logs should be uploaded on destruction.
-  histogram_tester()->ExpectUniqueSample(
-      "OptimizationGuide.ModelQualityLogEntry.UploadedOnDestruction", true, 1);
+  base::RunLoop().RunUntilIdle();
+  content::RunAllTasksUntilIdle();
+  EXPECT_EQ(1, url_loader_factory.NumPending());
 }
 
 }  // namespace optimization_guide
