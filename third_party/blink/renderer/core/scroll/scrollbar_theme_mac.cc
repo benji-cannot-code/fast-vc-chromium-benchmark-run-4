599b8d9ae9e87104002842851d7defc3b951f7f3
Gastón Rodríguez <gastonr@microsoft.com>
2024-05-23 17:29:15 +0000

    Mark ScrollbarTheme functions as const.
    The ScrollbarTheme class has a lot of functions that can be declared
    as const without any changes to their body. This CL redefines all such
    functions as const at no cost.
    
    Bug: 340848926
    Change-Id: Ia707572364183fab5717bfff2cc616f736780692
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560343
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1305187}

--- third_party/blink/renderer/core/scroll/scrollbar_theme_mac.cc
+++ third_party/blink/renderer/core/scroll/scrollbar_theme_mac.cc
@@ -113,7 +113,7 @@ void ScrollbarThemeMac::PaintTickmarks(GraphicsContext& context,
 }
 
 bool ScrollbarThemeMac::ShouldCenterOnThumb(const Scrollbar& scrollbar,
-                                            const WebMouseEvent& event) {
+                                            const WebMouseEvent& event) const {
   bool alt_key_pressed = event.GetModifiers() & WebInputEvent::kAltKey;
   return (event.button == WebPointerProperties::Button::kLeft) &&
          (s_jump_on_track_click != alt_key_pressed);
@@ -121,17 +121,17 @@ bool ScrollbarThemeMac::ShouldCenterOnThumb(const Scrollbar& scrollbar,
 
 ScrollbarThemeMac::~ScrollbarThemeMac() {}
 
-base::TimeDelta ScrollbarThemeMac::InitialAutoscrollTimerDelay() {
+base::TimeDelta ScrollbarThemeMac::InitialAutoscrollTimerDelay() const {
   return base::Seconds(s_initial_button_delay);
 }
 
-base::TimeDelta ScrollbarThemeMac::AutoscrollTimerDelay() {
+base::TimeDelta ScrollbarThemeMac::AutoscrollTimerDelay() const {
   return base::Seconds(s_autoscroll_button_delay);
 }
 
 bool ScrollbarThemeMac::ShouldDragDocumentInsteadOfThumb(
     const Scrollbar&,
-    const WebMouseEvent& event) {
+    const WebMouseEvent& event) const {
   return (event.GetModifiers() & WebInputEvent::Modifiers::kAltKey) != 0;
 }
 
@@ -336,7 +336,7 @@ bool ScrollbarThemeMac::UsesOverlayScrollbars() const {
   return PreferOverlayScrollerStyle();
 }
 
-bool ScrollbarThemeMac::HasThumb(const Scrollbar& scrollbar) {
+bool ScrollbarThemeMac::HasThumb(const Scrollbar& scrollbar) const {
   const auto& painter_values = GetScrollbarPainterValues(scrollbar);
   int min_length_for_thumb =
       painter_values.knob_min_length + painter_values.track_overlap_end_inset +
@@ -348,19 +348,20 @@ bool ScrollbarThemeMac::HasThumb(const Scrollbar& scrollbar) {
               : scrollbar.Height()) >= min_length_for_thumb;
 }
 
-gfx::Rect ScrollbarThemeMac::BackButtonRect(const Scrollbar& scrollbar) {
+gfx::Rect ScrollbarThemeMac::BackButtonRect(const Scrollbar& scrollbar) const {
   return gfx::Rect();
 }
 
-gfx::Rect ScrollbarThemeMac::ForwardButtonRect(const Scrollbar& scrollbar) {
+gfx::Rect ScrollbarThemeMac::ForwardButtonRect(
+    const Scrollbar& scrollbar) const {
   return gfx::Rect();
 }
 
-gfx::Rect ScrollbarThemeMac::TrackRect(const Scrollbar& scrollbar) {
+gfx::Rect ScrollbarThemeMac::TrackRect(const Scrollbar& scrollbar) const {
   return scrollbar.FrameRect();
 }
 
-int ScrollbarThemeMac::MinimumThumbLength(const Scrollbar& scrollbar) {
+int ScrollbarThemeMac::MinimumThumbLength(const Scrollbar& scrollbar) const {
   const auto& painter_values = GetScrollbarPainterValues(scrollbar);
   return painter_values.knob_min_length;
 }
