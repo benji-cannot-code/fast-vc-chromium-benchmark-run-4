126eded519ee0f4a31273f16abe04d1bd40f24a0
Hongyu Long <hongyulong@chromium.org>
2024-05-23 23:11:24 +0000

    focus-mode: Add SessionDuration and EndingMomentBubbleAction
    This Cl will add Ash.FocusMode.SessionDuration and
    Ash.FocusMode.EndingMomentBubbleAction histograms.
    
    Bug: b/340617806
    Test: unittests
    Change-Id: I3938dccc05ca4f0cfaef29a17770a5e9b9dab4f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560779
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305394}

--- ash/system/focus_mode/focus_mode_controller.cc
+++ ash/system/focus_mode/focus_mode_controller.cc
@@ -217,6 +217,9 @@ void FocusModeController::ExtendSessionDuration() {
 
   std::string message;
   if (was_in_ending_moment) {
+    focus_mode_metrics_recorder_->RecordHistogramOnEndingMoment(
+        focus_mode_histogram_names::EndingMomentBubbleClosedReason::kExtended);
+
     message = l10n_util::GetStringUTF8(
         IDS_ASH_STATUS_TRAY_FOCUS_MODE_EXTEND_TEN_MINUTES_BUTTON_ALERT);
   } else {
@@ -251,6 +254,15 @@ void FocusModeController::ExtendSessionDuration() {
 void FocusModeController::ResetFocusSession() {
   if (focus_mode_metrics_recorder_) {
     focus_mode_metrics_recorder_->RecordHistogramsOnEnd();
+    if (!in_focus_session()) {
+      focus_mode_metrics_recorder_->RecordHistogramOnEndingMoment(
+          current_session()->persistent_ending()
+              ? focus_mode_histogram_names::EndingMomentBubbleClosedReason::
+                    kOpended
+              : focus_mode_histogram_names::EndingMomentBubbleClosedReason::
+                    kIgnored);
+    }
+
     focus_mode_metrics_recorder_.reset();
   }
 
--- ash/system/focus_mode/focus_mode_metrics_recorder.cc
+++ ash/system/focus_mode/focus_mode_metrics_recorder.cc
@@ -122,6 +122,15 @@ void RecordPercentCompletedHistogram(double progress,
       /*percent=*/(progress * 100));
 }
 
+void RecordSessionDurationHistogram(const int time_elapsed) {
+  base::UmaHistogramCustomCounts(
+      /*name=*/focus_mode_histogram_names::kSessionDurationHistogramName,
+      /*sample=*/time_elapsed,
+      /*min=*/0,
+      /*exclusive_max=*/focus_mode_util::kMaximumDuration.InMinutes(),
+      /*buckets=*/50);
+}
+
 }  // namespace
 
 FocusModeMetricsRecorder::FocusModeMetricsRecorder(
@@ -174,6 +183,14 @@ void FocusModeMetricsRecorder::RecordHistogramsOnEnd() {
       session_snapshot.session_duration.InMinutes());
   RecordPercentCompletedHistogram(
       session_snapshot.progress, session_snapshot.session_duration.InMinutes());
+  RecordSessionDurationHistogram(session_snapshot.time_elapsed.InMinutes());
+}
+
+void FocusModeMetricsRecorder::RecordHistogramOnEndingMoment(
+    focus_mode_histogram_names::EndingMomentBubbleClosedReason reason) {
+  base::UmaHistogramEnumeration(
+      /*name=*/focus_mode_histogram_names::kEndingMomentBubbleActionHistogram,
+      /*sample=*/reason);
 }
 
 }  // namespace ash
