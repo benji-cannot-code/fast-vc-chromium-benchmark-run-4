0d6b1e995f5d9b4cd210222db758339b3c31e6fd
Hayato Ito <hayato@chromium.org>
2024-05-23 06:48:24 +0000

    [URL] Preserve Blink's CSP behavior for non-special URLs
    This CL is similar to https://crrev.com/c/5528336, but for Blink's
    CSP implementation to ensure the behavior remains consistent
    regardless of non-special URL support.
    
    The following tests now pass:
    
    - CSPSourceTest.MatchingAsSelf
    - CSPSourceTest.SchemeIsEmpty
    
    Bug: 40063064
    Change-Id: I80d7e64a627ffc9dbb7616027467ea1c6616b983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561804
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304889}

--- third_party/blink/renderer/core/frame/csp/csp_source_test.cc
+++ third_party/blink/renderer/core/frame/csp/csp_source_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/csp/csp_source.h"
 
+#include "base/test/scoped_feature_list.h"
 #include "services/network/public/mojom/content_security_policy.mojom-blink.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
+#include "url/url_features.h"
 
 namespace blink {
 
@@ -149,7 +151,25 @@ TEST(CSPSourceTest, InsecureHostSchemeMatchesSecureScheme) {
                                 KURL(base, "https://not-example.com:8000/")));
 }
 
-TEST(CSPSourceTest, SchemeIsEmpty) {
+class CSPSourceParamTest : public ::testing::TestWithParam<bool> {
+ public:
+  CSPSourceParamTest()
+      : use_standard_compliant_non_special_scheme_url_parsing_(GetParam()) {
+    scoped_feature_list_.InitWithFeatureState(
+        url::kStandardCompliantNonSpecialSchemeURLParsing,
+        use_standard_compliant_non_special_scheme_url_parsing_);
+  }
+
+ protected:
+  bool use_standard_compliant_non_special_scheme_url_parsing_;
+
+ private:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+
+INSTANTIATE_TEST_SUITE_P(All, CSPSourceParamTest, ::testing::Bool());
+
+TEST_P(CSPSourceParamTest, SchemeIsEmpty) {
   KURL base;
 
   // Self scheme is http.
@@ -325,7 +345,7 @@ TEST(CSPSourceTest, HostMatches) {
   }
 }
 
-TEST(CSPSourceTest, MatchingAsSelf) {
+TEST_P(CSPSourceParamTest, MatchingAsSelf) {
   // Testing Step 4 of
   // https://w3c.github.io/webappsec-csp/#match-url-to-source-expression
   struct Source {
