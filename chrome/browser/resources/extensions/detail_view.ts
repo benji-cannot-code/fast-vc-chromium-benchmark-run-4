c85d71f5c7c847fa14f0f5e62dcfd436541f7a35
EmiliaPaz <emiliapaz@chromium.org>
2024-05-23 06:47:05 +0000

    [Extensions] Add warning to extension's detail page for mv2 deprecation
    Add a message warning in the extension's detail page in
    chrome://extensions when the feature is enabled and the extension is
    affected by the mv2 deprecation.
    
    This CL only adds the UI. Next one will add on click functionality for
    the button.
    
    Screenshot: https://drive.google.com/file/d/14mIV0zWSZL4P34kB5LLecpiGCEd6VpSn/view?usp=sharing
    
    Bug: 337191307
    Change-Id: I40bfa2bff352d82ee446dfa394984695b0f67335
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5559864
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304888}

--- chrome/browser/resources/extensions/detail_view.ts
+++ chrome/browser/resources/extensions/detail_view.ts
@@ -110,6 +110,13 @@ export class ExtensionsDetailViewElement extends
         observer: 'onShowSafetyCheckChanged_',
       },
 
+      /** Whether the mv2 deprecation message warning is shown. */
+      showMv2DeprecationMessage_: {
+        type: Boolean,
+        computed: 'computeShowMv2DeprecationMessage_(' +
+            'data.isAffectedByMV2Deprecation)',
+      },
+
       /** Whether the extensions blocklist text is shown. */
       showBlocklistText_: {
         type: Boolean,
@@ -139,6 +146,7 @@ export class ExtensionsDetailViewElement extends
   showActivityLog: boolean;
   fromActivityLog: boolean;
   private showSafetyCheck_: boolean;
+  private showMv2DeprecationMessage_: boolean;
   private showBlocklistText_: boolean;
   private size_: string;
   private sortedViews_: chrome.developerPrivate.ExtensionView[];
@@ -453,6 +461,14 @@ export class ExtensionsDetailViewElement extends
     return !!(
         this.data.safetyCheckText && this.data.safetyCheckText.detailString &&
         this.data.acknowledgeSafetyCheckWarning !== true);
+  }
+
+  /**
+   * Returns whether the mv2 deprecation message warning should be displayed.
+   */
+  private computeShowMv2DeprecationMessage_(): boolean {
+    return loadTimeData.getBoolean('MV2DeprecationPanelEnabled') &&
+        this.data.isAffectedByMV2Deprecation;
   }
 
   private onShowSafetyCheckChanged_() {
