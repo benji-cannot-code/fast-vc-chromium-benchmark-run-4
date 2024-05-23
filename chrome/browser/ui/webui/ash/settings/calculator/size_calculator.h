014b5c027882089b376e420e75a5373695780d2c
François Degros <fdegros@chromium.org>
2024-05-23 06:40:20 +0000

    Simplify SizeCalculator and subclasses
    Remove some duplicated logic.
    Improved logs.
    
    TEST=unit_tests --gtest_filter=StorageHandlerTest*
    
    Bug: b:338158838
    Change-Id: I4980c24f6b5037c4a4a1606c3c73bfdc6c117b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553813
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304883}

--- chrome/browser/ui/webui/ash/settings/calculator/size_calculator.h
+++ chrome/browser/ui/webui/ash/settings/calculator/size_calculator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <array>
 #include <bitset>
 #include <memory>
+#include <ostream>
 #include <vector>
 
 #include "ash/components/arc/mojom/storage_manager.mojom.h"
@@ -58,7 +59,7 @@ class SizeCalculator {
   static constexpr int kCalculationTypeCount =
       static_cast<int>(CalculationType::kLastCalculationItem) + 1;
 
-  explicit SizeCalculator(const CalculationType& calculation_type);
+  explicit SizeCalculator(CalculationType calculation_type);
   virtual ~SizeCalculator();
 
   // Starts the size calculation of a given storage item.
@@ -75,16 +76,19 @@ class SizeCalculator {
   virtual void PerformCalculation() = 0;
 
   // Notify the StorageHandler about the calculated storage item size
-  void NotifySizeCalculated(int64_t total_bytes);
+  void NotifySizeCalculated(int64_t size);
 
   // Item id.
   const CalculationType calculation_type_;
 
   // Flag indicating that fetch operations for storage size are ongoing.
   bool calculating_ = false;
+
   // Observers being notified about storage items size changes.
-  base::ObserverList<SizeCalculator::Observer> observers_;
+  base::ObserverList<Observer> observers_;
 };
+
+std::ostream& operator<<(std::ostream& out, SizeCalculator::CalculationType t);
 
 // Class handling the calculation of the total disk space on the system.
 class TotalDiskSpaceCalculator : public SizeCalculator {
@@ -107,8 +111,6 @@ class TotalDiskSpaceCalculator : public SizeCalculator {
   void OnGetRootDeviceSize(std::optional<int64_t> reply);
 
   void GetTotalDiskSpace();
-
-  void OnGetTotalDiskSpace(int64_t* total_bytes);
 
   raw_ptr<Profile> profile_;
   base::WeakPtrFactory<TotalDiskSpaceCalculator> weak_ptr_factory_{this};
@@ -136,8 +138,6 @@ class FreeDiskSpaceCalculator : public SizeCalculator {
 
   void GetFreeDiskSpace();
 
-  void OnGetFreeDiskSpace(int64_t* available_bytes);
-
   raw_ptr<Profile> profile_;
   base::WeakPtrFactory<FreeDiskSpaceCalculator> weak_ptr_factory_{this};
 };
@@ -156,8 +156,6 @@ class DriveOfflineSizeCalculator : public SizeCalculator {
   friend class DriveOfflineSizeTestAPI;
 
   void PerformCalculation() override;
-
-  void OnGetOfflineItemsSize(int64_t offline_bytes);
 
   raw_ptr<Profile> profile_;
   base::WeakPtrFactory<DriveOfflineSizeCalculator> weak_ptr_factory_{this};
@@ -179,9 +177,6 @@ class MyFilesSizeCalculator : public SizeCalculator {
 
   // SizeCalculator:
   void PerformCalculation() override;
-
-  // Updates the size of MyFiles and Play files.
-  void OnGetMyFilesSize(int64_t total_bytes);
 
   raw_ptr<Profile> profile_;
   base::WeakPtrFactory<MyFilesSizeCalculator> weak_ptr_factory_{this};
@@ -327,9 +322,6 @@ class CrostiniSizeCalculator : public SizeCalculator {
   // Callback to update the size of Crostini VMs.
   void OnGetCrostiniSize(
       std::optional<vm_tools::concierge::ListVmDisksResponse>);
-
-  // Helper function to simplify updating the reported size of Crostini.
-  void UpdateSize(int64_t total_bytes);
 
   raw_ptr<Profile> profile_;
   base::WeakPtrFactory<CrostiniSizeCalculator> weak_ptr_factory_{this};
--- chrome/browser/ui/webui/ash/settings/pages/storage/device_storage_util.cc
+++ chrome/browser/ui/webui/ash/settings/pages/storage/device_storage_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/settings/pages/storage/device_storage_util.h"
 
+#include <bit>
+
 namespace ash::settings {
 
 int64_t RoundByteSize(int64_t bytes) {
@@ -16,20 +18,7 @@ int64_t RoundByteSize(int64_t bytes) {
     return 0;
   }
 
-  // Subtract one to the original number of bytes.
-  bytes--;
-  // Set all the lower bits to 1.
-  bytes |= bytes >> 1;
-  bytes |= bytes >> 2;
-  bytes |= bytes >> 4;
-  bytes |= bytes >> 8;
-  bytes |= bytes >> 16;
-  bytes |= bytes >> 32;
-  // Add one. The one bit beyond the highest set bit is set to 1. All the lower
-  // bits are set to 0.
-  bytes++;
-
-  return bytes;
+  return std::bit_ceil(static_cast<uint64_t>(bytes));
 }
 
 }  // namespace ash::settings
