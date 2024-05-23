0eaad73ed63a85d6128af72dde1d028c2debf115
Jood Hajeer <jood@google.com>
2024-05-23 09:05:50 +0000

    [iOS] Show identity disc on the tab switcher page
    The new account switching menu will have an entry point in the tab switcher page, it will be viewed using an identity disc, see mocks:
    https://www.figma.com/design/UDyNaZrkg6tTmDgbVMWgcR/Multi-account-on-mobile?node-id=2582-117172&t=Jlo5tZ4SthpXGln7-4
    
    This CL adds PersonCropCircleSymbol in the tab switcher page. And later
    CLs will 1) add a target to view the account menu, and 2) update the
    identity image with the signed in account avatar.
    The change is controller by feature kIdentityDiscAccountSwitch.
    
    Screenshot in crbug.com/336719423#comment7 and #comment8
    
    Bug: 336719423
    Change-Id: I27c7122bceefc6aeccf3d0ecc133fd8d1650f301
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557823
    Commit-Queue: Jood Hajeer <jood@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304933}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_top_toolbar.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_top_toolbar.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
+#import "ios/chrome/browser/ui/ntp/new_tab_page_feature.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_page_control.h"
@@ -49,6 +50,7 @@ const CGFloat kSymbolSearchImagePointSize = 22;
   UIBarButtonItem* _selectAllButton;
   UIBarButtonItem* _selectedTabsItem;
   UIBarButtonItem* _searchButton;
+  UIBarButtonItem* _identityDiscItem;
   UIBarButtonItem* _doneButton;
   UIBarButtonItem* _closeAllOrUndoButton;
   UIBarButtonItem* _editButton;
@@ -302,7 +304,9 @@ const CGFloat kSymbolSearchImagePointSize = 22;
         _selectionModeFixedSpace, trailingButton
       ]];
     } else {
-      trailingButton = _spaceItem;
+      trailingButton = base::FeatureList::IsEnabled(kIdentityDiscAccountSwitch)
+                           ? _identityDiscItem
+                           : _spaceItem;
       [self setItems:@[
         _leadingButton, _spaceItem, centralItem, _spaceItem, trailingButton
       ]];
@@ -344,6 +348,12 @@ const CGFloat kSymbolSearchImagePointSize = 22;
 
   if (_mode != TabGridModeNormal) {
     [items addObject:_selectionModeFixedSpace];
+  }
+
+  if (base::FeatureList::IsEnabled(kIdentityDiscAccountSwitch)) {
+    // In Landscape mode, the upper right corner will have the identity disc
+    // before the "Done" button.
+    [items addObject:_identityDiscItem];
   }
 
   [items addObject:trailingButton];
@@ -457,6 +467,15 @@ const CGFloat kSymbolSearchImagePointSize = 22;
   [_searchBarView addSubview:_searchBar];
   [_searchBarView sizeToFit];
   _searchBarItem = [[UIBarButtonItem alloc] initWithCustomView:_searchBarView];
+
+  UIImage* identityImage = DefaultSymbolTemplateWithPointSize(
+      kPersonCropCircleSymbol, kIdentityImageDimension);
+  // TODO(crbug.com/336719423): Add action to view the account switching menu.
+  _identityDiscItem =
+      [[UIBarButtonItem alloc] initWithImage:identityImage
+                                       style:UIBarButtonItemStylePlain
+                                      target:self
+                                      action:nil];
 
   _iconButtonAdditionalSpaceItem = [[UIBarButtonItem alloc]
       initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace
