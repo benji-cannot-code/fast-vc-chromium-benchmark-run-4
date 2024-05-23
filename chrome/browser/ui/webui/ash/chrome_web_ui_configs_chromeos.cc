8054aafd2dca2f3970f2a0050e3e42ed55437fc2
Edman Anjos <edmanp@google.com>
2024-05-23 13:13:38 +0000

    kiosk: Introduce chrome://kiosk-vision-internals
    chrome://kiosk-vision-internals will eventually be used to develop and
    test the Kiosk Vision feature. This CL simply introduces the page and
    makes it accessible. Following CLs will allow it to communicate with the
    rest of the feature and display debugging information.
    
    Fixed: b:338995158
    Change-Id: Ib5c955ac6e9dbec4188e2a6b5da1b113d94ac304
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5554500
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305026}

--- chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
@@ -112,6 +112,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/vm/vm_ui.h"
 #include "chrome/browser/ui/webui/nearby_internals/nearby_internals_ui.h"
 #include "chrome/browser/ui/webui/nearby_share/nearby_share_dialog_ui.h"
+#include "chrome/browser/ui/webui/webui_util.h"
+#include "chromeos/ash/components/kiosk/vision/webui/ui_controller.h"
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "ash/webui/conch/conch_ui.h"
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
@@ -255,6 +257,8 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<EmojiUIConfig>());
   map.AddWebUIConfig(
       std::make_unique<extended_updates::ExtendedUpdatesUIConfig>());
+  map.AddWebUIConfig(std::make_unique<ash::kiosk_vision::UIConfig>(
+      base::BindRepeating(webui::SetupWebUIDataSource)));
   map.AddWebUIConfig(
       MakeComponentConfigWithDelegate<FilesInternalsUIConfig, FilesInternalsUI,
                                       ChromeFilesInternalsUIDelegate>());
---
+++ chromeos/ash/components/kiosk/vision/webui/app.ts
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './strings.m.js';
+
+import { loadTimeData } from '//resources/js/load_time_data.js';
+import { PolymerElement } from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import { getTemplate } from './app.html.js';
+
+export class KioskVisionInternalsAppElement extends PolymerElement {
+  static get is() {
+    return 'kiosk-vision-internals-app';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+
+  static get properties() {
+    return {
+      message_: {
+        type: String,
+        value: () => loadTimeData.getString('message'),
+      },
+    };
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'kiosk-vision-internals-app': KioskVisionInternalsAppElement;
+  }
+}
+
+customElements.define(
+  KioskVisionInternalsAppElement.is, KioskVisionInternalsAppElement);
+
+
