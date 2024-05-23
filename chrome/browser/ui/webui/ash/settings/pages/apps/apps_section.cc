347ad6704c5b044a4d848997cb7d98fdcfe0fff3
Amber Haynes <amberhaynes@chromium.org>
2024-05-23 18:30:06 +0000

    app-controls: Add "Forgot PIN?" help center link
    + Added an link to the "Forgot PIN?" element in the PIN verification dialog.
    + On click, the help center link is opened in a new tab.
    
    Demo: http://screencast/cast/NTk2NDk0NjY5MjExMjM4NHwyZjQ2ZjYwYi1kMw
    
    Bug: b:339219535
    Change-Id: Ie4b2b8ce5ec90b6355ca90c3e12b0806be48a73b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564163
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Amber Haynes <amberhaynes@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305239}

--- chrome/browser/ui/webui/ash/settings/pages/apps/apps_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/apps/apps_section.cc
@@ -440,6 +440,9 @@ void AddAppParentalControlsStrings(content::WebUIDataSource* html_source) {
   };
 
   html_source->AddLocalizedStrings(kLocalizedStrings);
+
+  html_source->AddString("appParentalControlsLearnMoreUrl",
+                         chrome::kAppParentalControlsLearnMoreUrl);
 }
 
 bool ShowPluginVm(const Profile* profile, const PrefService& pref_service) {
