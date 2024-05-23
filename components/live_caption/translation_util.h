5f8db9b83b3df563ec4fc238d52b4262f247259e
Rob Schonberger <robsc@chromium.org>
2024-05-23 23:34:37 +0000

    soda: Refactor to extract translation utility functions to file.
    This pulls out a number of anonymous functions into a common
    translation_util library, which allows them to be tested and also used
    from different classes.
    
    Future updates will include using these from a system live caption
    location.
    
    AX-Relnotes: No user visible update.
    
    Bug: b/338322372
    Change-Id: I66afe1f695d134972a5e190445876edf60938b19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553174
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Rob Schonberger <robsc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305414}

---
+++ components/live_caption/translation_util.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef COMPONENTS_LIVE_CAPTION_TRANSLATION_UTIL_H_
+#define COMPONENTS_LIVE_CAPTION_TRANSLATION_UTIL_H_
+#include <string>
+#include <vector>
+namespace captions {
+
+// Split the transcription into sentences. Spaces are included in the preceding
+// sentence.
+std::vector<std::string> SplitSentences(const std::string& text,
+                                        const std::string& locale);
+bool ContainsTrailingSpace(const std::string& str);
+
+std::string RemoveTrailingSpace(const std::string& str);
+
+std::string RemovePunctuationToLower(std::string str);
+std::string GetTranslationCacheKey(const std::string& source_language,
+                                   const std::string& target_language,
+                                   const std::string& transcription);
+bool IsIdeographicLocale(const std::string& locale);
+}  // namespace captions
+
+#endif  // COMPONENTS_LIVE_CAPTION_TRANSLATION_UTIL_H_
