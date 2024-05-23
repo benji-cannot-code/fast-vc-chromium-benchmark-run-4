7f8db5bdc15000a565229daeb703bc75fc297baf
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-05-23 14:30:47 +0000

    Fix RealtimeReportingClient when kOidcAuthProfileManagement is enabled
    This CL does the following changes to `RealtimeReportingClient`:
    - It changes the profile cloud policy manager getter used to also work
      for an OIDC auth profile. This is a fix similar to what was done in
      crrev.com/c/5381562
    - It adds extra logic to check the `kProfileUserEmail` pref when it is
      unavailable through an `IdentityManager`, as is the case for OIDC auth
      profiles.
    
    Bug: b/338079484
    Change-Id: Ic3435c9f162232764440617288872ac18b6acc97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560107
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305061}

--- chrome/browser/enterprise/connectors/reporting/realtime_reporting_client_unittest.cc
+++ chrome/browser/enterprise/connectors/reporting/realtime_reporting_client_unittest.cc
@@ -52,12 +52,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+#include "chrome/browser/enterprise/profile_management/profile_management_features.h"
+#include "chrome/browser/enterprise/signin/enterprise_signin_prefs.h"
 #include "components/device_signals/core/browser/signals_types.h"
 #endif
 
 using testing::_;
 
 namespace enterprise_connectors {
+
+namespace {
 
 std::unique_ptr<KeyedService> BuildRealtimeReportingClient(
     content::BrowserContext* context) {
@@ -174,6 +178,27 @@ class RealtimeReportingClientIsRealtimeReportingEnabledTest
 #endif
 };
 
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+class RealtimeReportingClientOidcTest : public RealtimeReportingClientTestBase {
+ public:
+  RealtimeReportingClientOidcTest() {
+    scoped_feature_list_.InitAndEnableFeature(
+        profile_management::features::kOidcAuthProfileManagement);
+  }
+
+  void SetUp() override {
+    RealtimeReportingClientTestBase::SetUp();
+    profile_->GetPrefs()->SetString(enterprise_signin::prefs::kProfileUserEmail,
+                                    "oidc@user.email");
+  }
+
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_;
+};
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
+
+}  // namespace
+
 TEST_P(RealtimeReportingClientIsRealtimeReportingEnabledTest,
        ShouldInitRealtimeReportingClient) {
   EXPECT_EQ(should_init(),
@@ -284,6 +309,14 @@ TEST_F(RealtimeReportingClientTestBase,
   AddCrowdstrikeSignalsToEvent(event, response);
   EXPECT_EQ(event.Find("securityAgents"), nullptr);
 }
+
+TEST_F(RealtimeReportingClientOidcTest, Username) {
+  RealtimeReportingClient client(profile_);
+  ASSERT_EQ(client.GetProfileUserName(), "oidc@user.email");
+}
+
+// TODO(b/342232001): Add more tests for the `RealtimeReportingClientOidcTest`
+// fixture to cover key use cases.
 
 #endif
 }  // namespace enterprise_connectors
