c541d4d97743b5c91cd26b4bb56baa9206ebf5f6
Henrique Nakashima <hnakashima@chromium.org>
2024-05-23 19:08:35 +0000

    [Android] Assert Condition#buildDescription() returns non-null
    Fix Condition which was printed as "null".
    
    Bug: None
    Change-Id: Id725c607c4b4e31a94397bb759d09fcb6aac76d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544654
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305265}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Condition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Condition.java
@@ -80,6 +80,8 @@ public abstract class Condition {
      */
     protected void rebuildDescription() {
         mDescription = buildDescription();
+        assert mDescription != null
+                : this.getClass().getCanonicalName() + "#buildDescription() should not return null";
     }
 
     /**
--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/TabModelSelectorCondition.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/TabModelSelectorCondition.java
@@ -35,7 +35,7 @@ public class TabModelSelectorCondition extends UiThreadCondition
 
     @Override
     public String buildDescription() {
-        return null;
+        return "TabModelSelector is available";
     }
 
     @Override
