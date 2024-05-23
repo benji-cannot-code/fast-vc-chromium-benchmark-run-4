9c939e059427913fa0e7f24600dfb95f3457aaf6
Christian Xu <christianxu@google.com>
2024-05-23 22:56:42 +0000

    [iOS] Add user action for accepting the default suggestion
    Add user action to record when the user accepts the default omnibox
    suggestion by pressing on enter / go.
    
    Bug: 328894739
    Change-Id: I00443e1b7adb9aab644a50b0a1401404942c5f8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563123
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305382}

--- ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_view_ios.mm
@@ -549,6 +549,8 @@ void OmniboxViewIOS::OnAccept() {
                      NTPTabHelper->ShouldShowStartSurface());
   }
 
+  base::RecordAction(UserMetricsAction("IOS.Omnibox.AcceptDefaultSuggestion"));
+
   if (model()) {
     model()->OpenSelection();
   }
