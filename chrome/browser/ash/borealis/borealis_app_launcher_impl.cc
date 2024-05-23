d4d5cf7fbeff000821d37afbe26507ca4fa27973
Phillip Pearson <philpearson@google.com>
2024-05-23 18:50:37 +0000

    borealis: Attribute launches to a referring source
    Bug: b:309029917
    Change-Id: I9449535b9350ed98a3582848002af403ca22bf72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5473905
    Commit-Queue: Phillip Pearson <philpearson@google.com>
    Reviewed-by: Daniel Ng <danielng@google.com>
    Reviewed-by: Femi Adegunloye <mrfemi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305256}

--- chrome/browser/ash/borealis/borealis_app_launcher_impl.cc
+++ chrome/browser/ash/borealis/borealis_app_launcher_impl.cc
@@ -46,6 +46,7 @@ void BorealisAppLauncherImpl::Launch(std::string app_id,
            .IsRunning()) {
     borealis::ShowBorealisSplashScreenView(profile_);
   }
+  RecordBorealisLaunchSourceHistogram(source);
   BorealisService::GetForProfile(profile_)->ContextManager().StartBorealis(
       base::BindOnce(
           [](Profile* profile, std::string app_id,
--- chrome/browser/ash/guest_os/guest_os_registry_service.cc
+++ chrome/browser/ash/guest_os/guest_os_registry_service.cc
@@ -78,7 +78,7 @@ void Launch(vm_tools::apps::VmType vm_type,
 
     case VmType::BOREALIS:
       borealis::BorealisService::GetForProfile(profile)->AppLauncher().Launch(
-          app_id, {url.spec()}, borealis::BorealisLaunchSource::kUnknown,
+          app_id, {url.spec()}, borealis::BorealisLaunchSource::kAppUrlHandler,
           base::DoNothing());
       break;
 
