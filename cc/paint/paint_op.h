0702a825d734d16c134887e94dd514a648bb8d3a
Scott Violet <sky@chromium.org>
2024-05-23 13:43:02 +0000

    cc: factor CorePaintFlags/PaintFlags better
    And add support for the lite-ops calling IsValid() as it's
    needed for the blend mode.
    
    Bug: 340122178,341173819
    Change-Id: I3231a86539b5f22301ac1254d1583572a8e6d31f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5548819
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305038}

--- cc/paint/paint_op.h
+++ cc/paint/paint_op.h
@@ -637,7 +637,7 @@ class CC_PAINT_EXPORT DrawLineLiteOp final : public PaintOp {
   static void Raster(const DrawLineLiteOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return true; }
+  bool IsValid() const { return core_paint_flags.IsValid(); }
   bool EqualsForTesting(const DrawLineLiteOp& other) const;
   HAS_SERIALIZATION_FUNCTIONS();
 
@@ -670,7 +670,7 @@ class CC_PAINT_EXPORT DrawArcLiteOp final : public PaintOp {
   static void Raster(const DrawArcLiteOp* op,
                      SkCanvas* canvas,
                      const PlaybackParams& params);
-  bool IsValid() const { return true; }
+  bool IsValid() const { return core_paint_flags.IsValid(); }
   bool EqualsForTesting(const DrawArcLiteOp& other) const;
   HAS_SERIALIZATION_FUNCTIONS();
 
