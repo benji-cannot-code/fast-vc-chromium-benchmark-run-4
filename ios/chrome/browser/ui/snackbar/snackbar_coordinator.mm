d5e2180764a725cb17ecf57622d0a9c79e41d7ea
Scott Yoder <scottyoder@google.com>
2024-05-23 20:22:35 +0000

    [ios] Enable SnackbarUseLegacyDismissalBehavior by default
    Bug: 332491188
    Change-Id: I875f55c72d897cf0824a1d6524c07957c6de31f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564264
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305305}

--- ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
+++ ios/chrome/browser/ui/snackbar/snackbar_coordinator.mm
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 BASE_FEATURE(kSnackbarUseLegacyDismissalBehavior,
              "SnackbarUseLegacyDismissalBehavior",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Allow access to `usesLegacyDismissalBehavior` since the autoroller to update
 // the header is broken.
