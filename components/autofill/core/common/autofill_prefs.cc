6c9724dcdf78e5b6cd2ddc3206021785908cec3b
Florian Leimgruber <fleimgruber@google.com>
2024-05-23 08:59:43 +0000

    Run deduplication routine twice per milestone
    AutofillSilentlyRemoveQuasiDuplicates modifies how the deduplication
    routine behaves. Since the deduplication routine is only run once per
    milestone, this would force us to stay on every channel for at least one
    milestone during the rollout to get meaningful data.
    
    This CL thus relaxes the "once per milestone" condition and allows
    triggering the deduplication routine a second time for users enrolled in
    the experiment. This is done by adding a new pref, which tracks whether
    the deduplication logic ran a second time.
    
    This will prevent us from using the existing deduplication metrics (e.g.
    number of profiles removed) for the rollout, since the baseline is
    different. A follow-up will thus implement feature specific metrics.
    
    Bug: b/325450676
    Change-Id: I959b4c6400146754df3bad70a9b549d79d673dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5541803
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304929}

--- components/autofill/core/common/autofill_prefs.cc
+++ components/autofill/core/common/autofill_prefs.cc
@@ -61,6 +61,8 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
   // Non-synced prefs. Used for per-device choices, e.g., signin promo.
   registry->RegisterBooleanPref(prefs::kAutofillCreditCardFidoAuthEnabled,
                                 false);
+  registry->RegisterBooleanPref(
+      prefs::kAutofillRanQuasiDuplicateExtraDeduplication, false);
 #if BUILDFLAG(IS_ANDROID)
   registry->RegisterBooleanPref(
       prefs::kAutofillCreditCardFidoAuthOfferCheckboxState, true);
