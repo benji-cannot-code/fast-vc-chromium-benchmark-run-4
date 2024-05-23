f468b4a741d095fb08103bb6fdc6ba90f92a5b68
Lauren Winston <lwinston@google.com>
2024-05-23 23:03:43 +0000

    Remove some todos.
    Bug: none
    Change-Id: I3de276f4badda17478283febd5e7dc78c5725ead
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5565333
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305388}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -918,8 +918,6 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
         // Only call sendInstallVoicePackRequest() if it's not already
         // downloading
         if (this.getVoicePackStatus_(lang) !== VoicePackStatus.INSTALLING) {
-          // TODO(b/326130935): Hide the message when installation completes or
-          // show an error message if something fails.
           this.setVoicePackStatus_(lang, VoicePackStatus.INSTALLING);
           chrome.readingMode.sendInstallVoicePackRequest(lang);
         }
@@ -1173,7 +1171,6 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
     }
 
     const defaultUtteranceSettings = this.defaultUtteranceSettings();
-    // TODO(crbug.com/40927698): Finalize the default voice preview text.
     const utterance = new SpeechSynthesisUtterance(
         loadTimeData.getString('readingModeVoicePreviewText'));
     const voice = event.detail.previewVoice;
@@ -1183,7 +1180,6 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
     utterance.pitch = defaultUtteranceSettings.pitch;
     utterance.rate = defaultUtteranceSettings.rate;
 
-    // TODO(crbug.com/40927698): Add tests for pause button
     utterance.onstart = event => {
       this.previewVoicePlaying = event.utterance.voice;
     };
@@ -1837,7 +1833,6 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
       // TODO(crbug.com/40927698): Ensure the rate is valid for the current
       // speech engine.
       rate: this.rate,
-      // TODO(crbug.com/40927698): Ensure the correct default values are used.
       volume: 1,
       pitch: 1,
     };
