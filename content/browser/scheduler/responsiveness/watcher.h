3baed193772f8df1e0ebd01ec6060088c7380d8f
François Doray <fdoray@chromium.org>
2024-05-23 20:06:26 +0000

    Remove heap allocation in content::Watcher::DidRunTaskOn(UI|IO)Thread.
    Under the ReduceCpuUtilization2 feature, the OnceCallback in
    content::Watcher::DidRunTaskOn(UI|IO)Thread is replaced with a
    FunctionRef to avoid heap allocations.
    
    Motivation: Remove one of the top heap allocations identified in
    a local benchmark used by the team.
    (go/whats-heavy-in-chrome-olivierli-may-2024#slide=id.g2dc35d2ec03_0_184)
    
    Bug: 40287434
    Change-Id: I73cf3ee8075c029e90b39d8a6fc0ea4c8c253954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544962
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305297}

--- content/browser/scheduler/responsiveness/watcher.h
+++ content/browser/scheduler/responsiveness/watcher.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/scheduler/responsiveness/metric_source.h"
 #include "content/common/content_export.h"
+#include "third_party/abseil-cpp/absl/types/variant.h"
 
 namespace content {
 namespace responsiveness {
@@ -97,9 +98,16 @@ class CONTENT_EXPORT Watcher : public base::RefCounted<Watcher>,
                    bool was_blocked_or_low_priority,
                    std::vector<Metadata>* currently_running_metadata);
 
+  // TODO(crbug.com/40287434): After the "ReduceCpuUtilization2" feature is
+  // cleaned up (~January 2025), remove the absl::variant in favor of a
+  // base::FunctionRef.
+  using TaskOrEventFinishedSignature = void(base::TimeTicks,
+                                            base::TimeTicks,
+                                            base::TimeTicks);
+  using TaskOrEventFinishedCallback =
+      absl::variant<base::OnceCallback<TaskOrEventFinishedSignature>,
+                    base::FunctionRef<TaskOrEventFinishedSignature>>;
   // |callback| will either be synchronously invoked, or else never invoked.
-  using TaskOrEventFinishedCallback = base::OnceCallback<
-      void(base::TimeTicks, base::TimeTicks, base::TimeTicks)>;
   void DidRunTask(const base::PendingTask* task,
                   std::vector<Metadata>* currently_running_metadata,
                   int* mismatched_task_identifiers,
