80da258ac8a790d2f014f4abc8dc78ef85ad756e
James Cook <jamescook@chromium.org>
2024-05-23 23:47:19 +0000

    birch: Fix ranking for BirchMostVisitedItem
    According to the priorities and triggers sheet, the most visited item
    is supposed to be shown only in the morning. Also, it's supposed to be
    shown once initially and then for 2 minutes (in overview) after the
    first show. Then it should not show any more.
    
    Bug: b:340346995
    Test: ash_unittests --gtest_filter=*BirchModel*
    Change-Id: Id0e8bf11652fed30cb623205662aee3c37d79823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560315
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305430}

--- ash/birch/birch_model.cc
+++ ash/birch/birch_model.cc
@@ -367,8 +367,11 @@ std::vector<std::unique_ptr<BirchItem>> BirchModel::GetAllItems() {
   for (auto& tab : recent_tab_data_.items) {
     all_items.push_back(std::make_unique<BirchTabItem>(tab));
   }
-  for (auto& item : url_to_most_visited_item) {
-    all_items.push_back(std::make_unique<BirchMostVisitedItem>(item.second));
+  if (ShouldShowMostVisited()) {
+    for (auto& item : url_to_most_visited_item) {
+      all_items.push_back(std::make_unique<BirchMostVisitedItem>(item.second));
+    }
+    most_visited_last_shown_ = GetNow();
   }
   for (auto& event : url_to_self_share_item) {
     all_items.push_back(std::make_unique<BirchSelfShareItem>(event.second));
@@ -657,6 +660,14 @@ void BirchModel::RecordProviderHiddenHistograms() {
 
 bool BirchModel::IsItemRemoverInitialized() {
   return item_remover_ && item_remover_->Initialized();
+}
+
+bool BirchModel::ShouldShowMostVisited() {
+  if (most_visited_last_shown_.is_null()) {
+    return true;  // Never been shown.
+  }
+  // Re-show for up to 2 minutes.
+  return GetNow() - most_visited_last_shown_ < base::Minutes(2);
 }
 
 }  // namespace ash
