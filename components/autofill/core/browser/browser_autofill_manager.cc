501838685ae416518296f8bd64a3c0675cfd59f9
Norge Vizcay <vizcay@google.com>
2024-05-23 16:07:49 +0000

    Move plus address manual trigger out of GetAvailableSuggestions.
    The CL moves generating manual fallback suggestions for plus addresses
    out of the GetAvailableSuggestions function, which has now been left
    with the sole purpose of generating address and credit card suggestions.
    
    Bug: b/340494671
    Change-Id: I5aaae554d59f718e5c44cc70bbdb2d2d22960fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557767
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305128}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -1370,6 +1370,19 @@ void BrowserAutofillManager::GenerateSuggestionsAndMaybeShowUI(
     return;
   }
 
+  if (IsPlusAddressesManuallyTriggered(trigger_source)) {
+    // The list of suggestions is never empty. It contains either existing plus
+    // addresses or the option to create a new one.
+    suggestions = client().GetPlusAddressDelegate()->GetSuggestions(
+        client().GetLastCommittedPrimaryMainFrameOrigin(),
+        client().IsOffTheRecord(),
+        client().ClassifyAsPasswordForm(*this, form.global_id(),
+                                        field.global_id()),
+        field.value(), trigger_source);
+    std::move(callback).Run(/*show_suggestions=*/true, std::move(suggestions));
+    return;
+  }
+
   // Check if other suggestion sources should be queried. Other suggestions may
   // include Compose or single field form suggestions. Manual fallbacks can't
   // trigger different suggestion types.
@@ -2772,12 +2785,7 @@ std::vector<Suggestion> BrowserAutofillManager::GetAvailableSuggestions(
     AutofillSuggestionTriggerSource trigger_source,
     SuggestionsContext& context) {
   if (IsPlusAddressesManuallyTriggered(trigger_source)) {
-    return client().GetPlusAddressDelegate()->GetSuggestions(
-        client().GetLastCommittedPrimaryMainFrameOrigin(),
-        client().IsOffTheRecord(),
-        client().ClassifyAsPasswordForm(*this, form.global_id(),
-                                        field.global_id()),
-        field.value(), trigger_source);
+    return {};
   }
 
   if (context.should_show_mixed_content_warning) {
