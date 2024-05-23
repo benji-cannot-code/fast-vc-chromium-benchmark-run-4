98da41e8994c7b5f4ddd351c953f551da9fb654e
Andrew Grieve <agrieve@chromium.org>
2024-05-23 19:23:42 +0000

    Android: Fix VERBOSE and DEBUG logs not appearing in debug builds
    This was broken by https://chromium-review.googlesource.com/c/chromium/src/+/5539533
    
    This also makes chromium's Log.isLoggable() method return true for
    debug builds, whereas the system version returns false for VERBOSE
    and DEBUG levels (despite them showing in logcat)
    
    Bug: None
    Change-Id: I0cc5a13d29a0aa1ef6a4431bb54129c67290d9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5559823
    Commit-Queue: Peter Wen <wnwen@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Wen <wnwen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305274}

--- base/android/java/src/org/chromium/base/Log.java
+++ base/android/java/src/org/chromium/base/Log.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base;
 
+import org.chromium.build.BuildConfig;
 import org.chromium.build.annotations.AlwaysInline;
 
 import java.util.Locale;
@@ -66,11 +67,15 @@ public class Log {
     }
 
     /**
-     * In debug: Forwards to {@link android.util.Log#isLoggable(String, int)}.
-     * In release: Always returns false (via proguard rule).
+     * When BuildConfig.ENABLE_DEBUG_LOGS=true, returns true. Otherwise, forwards to {@link
+     * android.util.Log#isLoggable(String, int)} (which returns false for levels < INFO, unless
+     * configured otherwise by R8's -maximumremovedandroidloglevel).
+     *
+     * <p>https://stackoverflow.com/questions/7948204/does-log-isloggable-returns-wrong-values
      */
+    @AlwaysInline
     public static boolean isLoggable(String tag, int level) {
-        return android.util.Log.isLoggable(tag, level);
+        return BuildConfig.ENABLE_DEBUG_LOGS || android.util.Log.isLoggable(tag, level);
     }
 
     /**
