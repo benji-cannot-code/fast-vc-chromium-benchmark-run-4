0c8ff308916118f3ff8563e678740bde3cc4bf02
Norge Vizcay <vizcay@google.com>
2024-05-23 07:26:20 +0000

    Move logging suggestions count into OnGenerateSuggestionsComplete.
    Logging the address and credit card suggestions count happens regardless
    of the surface that ends up showing suggestions during
    `GenerateSuggestionsAndMaybeShowUI`. This CL moves the logic into
    `OnGenerateSuggestionsComplete`
    
    Bug: b/340494671
    Change-Id: I127ded258664d02e4e25bf6fa662891900577011
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557745
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1304898}

--- components/autofill/core/browser/browser_autofill_manager.cc
+++ components/autofill/core/browser/browser_autofill_manager.cc
@@ -1287,10 +1287,10 @@ void BrowserAutofillManager::OnAskForValuesToFillImpl(
       BuildSuggestionsContext(form, field, trigger_source);
 
   GenerateSuggestionsAndMaybeShowUI(
-      form, field, trigger_source, std::move(context),
+      form, field, trigger_source, context,
       base::BindOnce(&BrowserAutofillManager::OnGenerateSuggestionsComplete,
                      weak_ptr_factory_.GetWeakPtr(), form, field,
-                     trigger_source));
+                     trigger_source, context));
 }
 
 void BrowserAutofillManager::GenerateSuggestionsAndMaybeShowUI(
@@ -1337,8 +1337,6 @@ void BrowserAutofillManager::GenerateSuggestionsAndMaybeShowUI(
   if (context.is_autofill_available && ShouldSuppressSuggestions()) {
     return;
   }
-
-  LogSuggestionsCount(context, suggestions);
 
   const bool form_element_was_clicked =
       trigger_source ==
@@ -1491,8 +1489,10 @@ void BrowserAutofillManager::OnGenerateSuggestionsComplete(
     const FormData& form,
     const FormFieldData& field,
     AutofillSuggestionTriggerSource trigger_source,
+    const SuggestionsContext& context,
     bool show_suggestions,
     std::vector<Suggestion> suggestions) {
+  LogSuggestionsCount(context, suggestions);
   // When focusing on a field, log whether there is a suggestion for the user
   // and whether the suggestion is shown.
   FormStructure* form_structure = nullptr;
