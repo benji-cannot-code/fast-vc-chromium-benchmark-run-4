36a0f4566f2e9e0732ca8bff3cd02ee778d9ceb0
Khushal Sagar <khushalsagar@chromium.org>
2024-05-23 23:43:57 +0000

    view-transition: Add paint holding for in-process iframes
    This change adds paint holding for in-process subframe navigations when
    there is a ViewTransition by doing the following:
    
    1. Assign the effect node which groups all content rendered by the
    iframe a ViewTransition resource ID. This ensures that the content
    doesn't paint into its target. It also ensures the content is cached
    into a texture in Viz.
    
    2. Create a ViewTransitionContentLayer to draw the above render pass/
    texture *after* painting the contents of the iframe. This ordering is
    important to ensure the PaintLayer for the iframe contents (which uses
    the above effect node) is painted before the new foreign layer.
    
    3. The resource ID for the subframe contents is included in the
    ViewTransitionState for cross-document navigations.
    
    Change-Id: I805847e8a032c2f19d094b270ef57046578a4e69
    Bug: 330920521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545686
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305425}

--- third_party/blink/renderer/core/paint/embedded_content_painter.cc
+++ third_party/blink/renderer/core/paint/embedded_content_painter.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "cc/layers/view_transition_content_layer.h"
 #include "third_party/blink/renderer/core/frame/embedded_content_view.h"
+#include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_offset.h"
 #include "third_party/blink/renderer/core/layout/layout_embedded_content.h"
 #include "third_party/blink/renderer/core/paint/box_painter.h"
@@ -16,10 +18,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/replaced_painter.h"
 #include "third_party/blink/renderer/core/paint/scrollable_area_painter.h"
+#include "third_party/blink/renderer/core/view_transition/view_transition.h"
 #include "third_party/blink/renderer/platform/graphics/paint/display_item_cache_skipper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
+#include "third_party/blink/renderer/platform/graphics/paint/foreign_layer_display_item.h"
 
 namespace blink {
+
+namespace {
+scoped_refptr<cc::ViewTransitionContentLayer> GetSubframeSnapshotLayer(
+    const EmbeddedContentView& embedded_content_view,
+    PaintPhase phase) {
+  if (phase != PaintPhase::kForeground) {
+    return nullptr;
+  }
+
+  auto* local_frame_view = DynamicTo<LocalFrameView>(embedded_content_view);
+  if (!local_frame_view) {
+    return nullptr;
+  }
+
+  auto* transition = ViewTransitionUtils::GetTransition(
+      *local_frame_view->GetFrame().GetDocument());
+  if (!transition) {
+    return nullptr;
+  }
+
+  return transition->GetSubframeSnapshotLayer();
+}
+
+}  // namespace
 
 void EmbeddedContentPainter::PaintReplaced(const PaintInfo& paint_info,
                                            const PhysicalOffset& paint_offset) {
@@ -43,6 +71,28 @@ void EmbeddedContentPainter::PaintReplaced(const PaintInfo& paint_info,
   adjusted_cull_rect.Move(-view_paint_offset);
   embedded_content_view->Paint(paint_info.context, paint_info.GetPaintFlags(),
                                adjusted_cull_rect, view_paint_offset);
+
+  // During a ViewTransition in a LocalFrame sub-frame, we need to keep painting
+  // the old Document's last frame until the new Document is ready to start
+  // rendering.
+  //
+  // Note: The iframe is throttled for the duration the new state is not ready
+  // to display. This is true for both same-document transitions (the update
+  // callback is running) and cross-document transitions (the new Document is
+  // render-blocked).
+  //
+  // When the iframe is throttled, the embedded content view will not paint
+  // anything but we still paint this foreign layer to keep displaying the old
+  // content.
+  if (auto layer =
+          GetSubframeSnapshotLayer(*embedded_content_view, paint_info.phase)) {
+    GraphicsContext& context = paint_info.context;
+    layer->SetBounds(embedded_content_view->FrameRect().size());
+    layer->SetIsDrawable(true);
+    RecordForeignLayer(context, layout_embedded_content_,
+                       DisplayItem::kForeignLayerViewTransitionContent,
+                       std::move(layer), paint_location);
+  }
 }
 
 }  // namespace blink
