4757400c9370bba933ac1e94a9d90261d8139dda
Sergey Poromov <poromov@chromium.org>
2024-05-23 12:43:13 +0000

    [skyvault] Remove trailing separators from the policy paths.
    Some places (e.g.ScreenCapture) do direct compare on the path and it
    doesn't work if there if the path ends with "/", so removing it.
    
    Change-Id: I63cf307f34921480efbbba6caa860b1adaf63d58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5554558
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305020}

--- chrome/browser/ash/policy/skyvault/file_location_utils.cc
+++ chrome/browser/ash/policy/skyvault/file_location_utils.cc
@@ -79,9 +79,12 @@ base::FilePath ResolvePath(const std::string& path_str) {
       return base::FilePath();
     }
     std::string result_str = path_str;
-    const base::FilePath resolved = base::FilePath(result_str.replace(
-        google_drive_position, strlen(kGoogleDrivePolicyVariableName),
-        drive_path.Append("root").AsUTF8Unsafe()));
+    const base::FilePath resolved =
+        base::FilePath(
+            result_str.replace(google_drive_position,
+                               strlen(kGoogleDrivePolicyVariableName),
+                               drive_path.Append("root").AsUTF8Unsafe()))
+            .StripTrailingSeparators();
     return resolved;
   }
 
@@ -94,9 +97,11 @@ base::FilePath ResolvePath(const std::string& path_str) {
     }
 
     std::string result_str = path_str;
-    const base::FilePath resolved = base::FilePath(result_str.replace(
-        one_drive_position, strlen(kOneDrivePolicyVariableName),
-        one_drive_path.AsUTF8Unsafe()));
+    const base::FilePath resolved =
+        base::FilePath(result_str.replace(one_drive_position,
+                                          strlen(kOneDrivePolicyVariableName),
+                                          one_drive_path.AsUTF8Unsafe()))
+            .StripTrailingSeparators();
     return resolved;
   }
 
