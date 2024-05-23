b142f10156509c157b5c9dfc16fa97267a5a574e
Joseph Park <josephjoopark@chromium.org>
2024-05-23 05:14:39 +0000

    Clean pinned toolbar button tooltip text
    Bug: 337938827
    Change-Id: Ib4e55a67849e406d649bae4b1d8ecbca31af1219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553497
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304867}

--- chrome/browser/ui/browser_actions.cc
+++ chrome/browser/ui/browser_actions.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/base/ui_base_features.h"
+#include "ui/gfx/text_utils.h"
 #include "ui/gfx/vector_icon_types.h"
 
 namespace {
@@ -52,10 +53,13 @@ actions::ActionItem::ActionItemBuilder ChromeMenuAction(
     int title_id,
     int tooltip_id,
     const gfx::VectorIcon& icon) {
+  auto clean_text = [](int str_id) {
+    return gfx::RemoveAccelerator(l10n_util::GetStringUTF16(str_id));
+  };
   return actions::ActionItem::Builder(callback)
       .SetActionId(action_id)
-      .SetText(l10n_util::GetStringUTF16(title_id))
-      .SetTooltipText(l10n_util::GetStringUTF16(tooltip_id))
+      .SetText(clean_text(title_id))
+      .SetTooltipText(clean_text(tooltip_id))
       .SetImage(ui::ImageModel::FromVectorIcon(icon, ui::kColorIcon))
       .SetProperty(actions::kActionItemPinnableKey, true);
 }
