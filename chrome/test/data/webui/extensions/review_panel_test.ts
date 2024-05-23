a9b1c45a03f8f539e40a02d70c17e4b0b6d992a2
EmiliaPaz <emiliapaz@chromium.org>
2024-05-23 01:03:47 +0000

    [Extensions] Move review panel visibility computation to item list
    Currently, extensions-review-panel
     - is inside a wrapper div that adds a bunch of margins and space
     - is visible iff the feature is enabled
     - it's contents are visible iff there are any unsafe extensions, or the
       completion info needs to be shown.
    
    This means that the wrapper div will always add the extra space even
    when extensions-review-panel is not visible.
    
    To fix this we:
     1) Move the div inside the condition of whether we are showing
        extensions-review-panel
     2) Move the "show review panel" computation from review panel to
        item list because:
         a. the dom-if better belongs to the parent (item list) than
            wrapping all the children (review panel) content
         b. mv2 deprecation panel, also in item list, will need to know
            whether the review panel is visible. Thus, we should have
            panels visibility computation in item list (mv2 deprecation
            panel visibility is computed there).
    
    Updated review panel test to reflect the changes (review panel now
    only receives the unsafe extensions) and added new ones to test
    the review panel visibility computations in item list test.
    
    Functionality and UI should not be changed.
    
    Bug: 338528606, 340606782
    Change-Id: I25c2eb8eecf2f1749ead8814b82804fedeb9aa44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552073
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1304799}

--- chrome/test/data/webui/extensions/review_panel_test.ts
+++ chrome/test/data/webui/extensions/review_panel_test.ts
@@ -36,8 +36,6 @@ suite('ExtensionsReviewPanel', function() {
         id: 'a'.repeat(32),
         safetyCheckText: {panelString: 'This extension contains malware.'},
       }),
-      createExtensionInfo({name: 'Bravo', id: 'b'.repeat(32)}),
-      createExtensionInfo({name: 'Charlie', id: 'c'.repeat(29)}),
     ];
     element.extensions = extensionItems;
     document.body.appendChild(element);
@@ -110,6 +108,18 @@ suite('ExtensionsReviewPanel', function() {
             ?.querySelector('.extension-representation')
             ?.textContent,
         'Alpha');
+  });
+
+  test('CompletionStateShouldBeShownIfNoExtensions', async function() {
+    const completionTextContainer =
+        element.shadowRoot!.querySelector('.completion-container');
+    assertTrue(!!completionTextContainer);
+    assertFalse(isVisible(completionTextContainer));
+
+    element.set('extensions', []);
+    await flushTasks();
+
+    assertTrue(isVisible(completionTextContainer));
   });
 
   test('CompletionStateShouldBeShownAfterDeletingItems', async function() {
@@ -186,17 +196,9 @@ suite('ExtensionsReviewPanel', function() {
         element.shadowRoot!.querySelector('.completion-container');
     class MockKeepItemDelegate extends MockItemDelegate {
       override setItemSafetyCheckWarningAcknowledged(): void {
-        const extensionItems = [
-          createExtensionInfo({
-            name: 'Alpha',
-            id: 'a'.repeat(32),
-            safetyCheckText: {panelString: 'This extension contains malware.'},
-            acknowledgeSafetyCheckWarning: true,
-          }),
-          createExtensionInfo({name: 'Bravo', id: 'b'.repeat(32)}),
-          createExtensionInfo({name: 'Charlie', id: 'c'.repeat(29)}),
-        ];
-        element.extensions = extensionItems;
+        // Update extensions to be an empty list since the only previous
+        // extension was marked as acknowledged.
+        element.set('extensions', []);
       }
     }
     element.delegate = new MockKeepItemDelegate();
@@ -216,7 +218,6 @@ suite('ExtensionsReviewPanel', function() {
 
     // Click the Keep the Extension button.
     actionMenu.querySelector('button')!.click();
-    await flushTasks();
     await browserProxy.whenCalled('extensionKeptAction');
 
     // The extension row should be removed and the completion state should be
