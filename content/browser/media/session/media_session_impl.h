8e07d6451ade455d415ab7080a3b3980aab3cd95
Tommy Steimel <steimel@chromium.org>
2024-05-24 13:57:53 +0000

    MediaSession: Use a MediaSessionImpl WeakPtr in MediaSessionServiceImpl
    Currently, every time MediaSessionServiceImpl wants to talk to its
    associated MediaSessionImpl, it recalculates it from its
    RenderFrameHostId. This can lead to issues where a
    MediaSessionServiceImpl of a disconnected RenderFrameHost can no longer
    access the MediaSessionImpl to tell it that it is being deleted,
    leaving MediaSessionImpl with a dangling raw_ptr.
    
    Bug: 338929744
    Change-Id: I8f404c1a39510a24643c1f973a32bf6c0bbde123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564794
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305678}

--- content/browser/media/session/media_session_impl.h
+++ content/browser/media/session/media_session_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/id_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "content/browser/media/session/audio_focus_delegate.h"
@@ -350,6 +351,9 @@ class MediaSessionImpl : public MediaSession,
   // Returns the Audio Focus request ID associated with this media session.
   const base::UnguessableToken& GetRequestId() const;
 
+  // Returns a WeakPtr to `this`.
+  base::WeakPtr<MediaSessionImpl> GetWeakPtr();
+
   CONTENT_EXPORT bool HasImageCacheForTest(const GURL& image_url) const;
 
   // Make sure that all observers have received any pending callbacks from us,
@@ -658,6 +662,8 @@ class MediaSessionImpl : public MediaSession,
   // changes often enough to be considered live. See
   // `MaybeGuardDurationUpdate()` for details on duration changes.
   bool is_considered_live_ = false;
+
+  base::WeakPtrFactory<MediaSessionImpl> weak_factory_{this};
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
