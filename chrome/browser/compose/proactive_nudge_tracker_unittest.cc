01839238f5baf4aedad8e65180f18e26498f8d01
Dan Harrington <harringtond@chromium.org>
2024-05-24 19:14:18 +0000

    Add metrics for compose
    * Update UKM Compose.TextElementUsage to include:
      * EditingTime
      * FieldSignature
      * FormSignature
    * Add new UMA: Compose.TextElementUsage.LongField.EditingTime
    * Add new UMA: Compose.ProactiveNudge.DerivedEngagement
    
    See http://shortn/_VdVoGsLegl for UKM approval
    
    Bug: b:341940723
    Change-Id: I765b8c30c06ac8fb728057059aeac6c23e3a8000
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555661
    Commit-Queue: Dan H <harringtond@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305848}

--- chrome/browser/compose/proactive_nudge_tracker_unittest.cc
+++ chrome/browser/compose/proactive_nudge_tracker_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/test/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "components/autofill/core/common/autofill_test_utils.h"
@@ -628,6 +629,7 @@ TEST_F(ProactiveNudgeTrackerDerivedEngagementTest, MinimalUse) {
 }
 
 TEST_F(ProactiveNudgeTrackerDerivedEngagementTest, SuggestionGenerated) {
+  base::HistogramTester histograms;
   TestFuture<segmentation_platform::TrainingLabels>& training_labels =
       TriggerNudgeForField(0, CreateTestFormFieldData());
   compose::ComposeSessionEvents events;
@@ -643,6 +645,9 @@ TEST_F(ProactiveNudgeTrackerDerivedEngagementTest, SuggestionGenerated) {
           "Compose.ProactiveNudge.DerivedEngagement",
           static_cast<base::HistogramBase::Sample>(
               ProactiveNudgeDerivedEngagement::kGeneratedComposeSuggestion)));
+  histograms.ExpectUniqueSample(
+      "Compose.ProactiveNudge.DerivedEngagement",
+      ProactiveNudgeDerivedEngagement::kGeneratedComposeSuggestion, 1);
 }
 
 TEST_F(ProactiveNudgeTrackerDerivedEngagementTest, AcceptedSuggestion) {
--- components/compose/core/browser/compose_features.cc
+++ components/compose/core/browser/compose_features.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/compose/core/browser/compose_features.h"
 
+#include "base/feature_list.h"
+
 namespace compose::features {
 
 // Note: Compose is enabled by default because it requires settings UI enabling.
@@ -78,4 +80,7 @@ BASE_FEATURE(kEnableNudgeForUnspecifiedHint,
              "ComposeEnableNudgeForUnspecifiedHint",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kEnableAdditionalTextMetrics,
+             "EnableAdditionalTextMetrics",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 }  // namespace compose::features
