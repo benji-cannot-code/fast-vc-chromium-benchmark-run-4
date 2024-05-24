784dc36e588be71dbb72e7a6020e19f2f8e87c02
mingmingtasd <mingming1.xu@intel.com>
2024-05-24 01:25:07 +0000

    WebNN: Remove the unnecessary base::ScopedClosureRunner
    To fix a nit from CL 5441190[1], remove the unnecessary
    base::ScopedClosureRunner to directly call `ML::RemovePendingResolver`
    in `MLContext::OnCreateWebNNContext`.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5441190
    
    Bug: 333263679
    Change-Id: I22846e2421f8516a91ddaa8dee8aba58e7100085
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561149
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1305464}

--- third_party/blink/renderer/modules/ml/ml_context.cc
+++ third_party/blink/renderer/modules/ml/ml_context.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/ml/ml_context.h"
 
-#include "base/functional/callback_helpers.h"
 #include "base/notreached.h"
 #include "components/ml/webnn/features.mojom-blink.h"
 #include "third_party/blink/public/platform/task_type.h"
@@ -190,11 +189,7 @@ void MLContext::OnCreateWebNNContext(
     ScopedMLTrace scoped_trace,
     ScriptPromiseResolver<MLContext>* resolver,
     webnn::mojom::blink::CreateContextResultPtr result) {
-  base::ScopedClosureRunner runner(WTF::BindOnce(
-      [](MLContext* context, ScriptPromiseResolver<MLContext>* resolver) {
-        context->ml_->RemovePendingResolver(resolver);
-      },
-      WrapPersistent(this), WrapPersistent(resolver)));
+  ml_->RemovePendingResolver(resolver);
   ScriptState* script_state = resolver->GetScriptState();
   if (!script_state) {
     return;
