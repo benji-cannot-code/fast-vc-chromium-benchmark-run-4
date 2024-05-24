dcac44e308706c666e50fe3e32988a25377132e5
Lei Zhang <thestig@chromium.org>
2024-05-24 19:17:58 +0000

    [PDF Ink Signatures] Replace PdfInkBrush fields used only for testing
    Remove PdfInkBrush's type and params fields, as they are only used for
    testing. Since these fields are passed down further into the Ink library
    update the Ink library interface with ForTesting methods to get these
    values instead. Then update unit tests to call the new methods, which
    demonstrates that the brush opacity is always set to 1.
    
    Bug: 335524382, 342414726
    Change-Id: I586b36aec5ecbdaaad1f9ad404d1cec46f1a9bf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563510
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1305853}

--- pdf/ink/stub/ink_brush_family_stub.cc
+++ pdf/ink/stub/ink_brush_family_stub.cc
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "pdf/ink/ink_brush_family.h"
+#include "pdf/ink/stub/ink_brush_family_stub.h"
 
 #include <memory>
+#include <utility>
 
-#include "base/memory/ptr_util.h"
 #include "pdf/ink/ink_brush_paint.h"
 #include "pdf/ink/ink_brush_tip.h"
 
@@ -18,8 +18,16 @@ std::unique_ptr<InkBrushFamily> InkBrushFamily::Create(
     InkBrushTip tip,
     InkBrushPaint paint,
     std::string_view uri_string) {
-  // Protected ctor.
-  return base::WrapUnique(new InkBrushFamily());
+  return std::make_unique<InkBrushFamilyStub>(std::move(tip));
+}
+
+InkBrushFamilyStub::InkBrushFamilyStub(InkBrushTip tip)
+    : opacity_(tip.opacity_multiplier) {}
+
+InkBrushFamilyStub::~InkBrushFamilyStub() = default;
+
+float InkBrushFamilyStub::GetOpacityForTesting() const {
+  return opacity_;
 }
 
 }  // namespace chrome_pdf
