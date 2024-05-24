7dbc91c5bc412d312dcb0e848e9e0592faad6b0c
Thuong Phan <thuongphan@chromium.org>
2024-05-24 16:50:23 +0000

    p13n: update the correct prompt when creating more free text image
    When a user selects "Create more" option of a recent image which was
    generated using free text query, the Sea Pen result page should show the
    correct prompt.
    Demo:http://shortn/_bWZCG6do4p
    
    Bug: b/342003936
    Test: browser_tests --gtest_filter="*PersonalizationAppSeaPenBrowserTest*"
    Change-Id: Iedebe837fc5caa374738bf65d2fdf5162e77759d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564380
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Commit-Queue: Thuong Phan <thuongphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305768}

--- ash/webui/common/resources/sea_pen/sea_pen_input_query_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_input_query_element.ts
@@ -45,6 +45,12 @@ export class SeaPenInputQueryElement extends WithSeaPenStore {
     return {
       textValue_: String,
 
+      seaPenQuery_: {
+        type: Object,
+        value: null,
+        observer: 'onSeaPenQueryChanged_',
+      },
+
       thumbnails_: {
         type: Object,
         observer: 'updateSearchButton_',
@@ -74,6 +80,7 @@ export class SeaPenInputQueryElement extends WithSeaPenStore {
   }
 
   private textValue_: string;
+  private seaPenQuery_: SeaPenQuery|null;
   private thumbnails_: SeaPenThumbnail[]|null;
   private thumbnailsLoading_: boolean;
   private searchButtonText_: string|null;
@@ -86,9 +93,18 @@ export class SeaPenInputQueryElement extends WithSeaPenStore {
         'thumbnails_', state => state.thumbnails);
     this.watch<SeaPenInputQueryElement['thumbnailsLoading_']>(
         'thumbnailsLoading_', state => state.loading.thumbnails);
+    this.watch<SeaPenInputQueryElement['seaPenQuery_']>(
+        'seaPenQuery_', state => state.currentSeaPenQuery);
     this.updateFromStore();
 
     this.$.queryInput.focusInput();
+  }
+
+  private onSeaPenQueryChanged_(seaPenQuery: SeaPenQuery|null) {
+    if (!seaPenQuery || !seaPenQuery.textQuery) {
+      return;
+    }
+    this.textValue_ = seaPenQuery.textQuery;
   }
 
   private onClickInputQuerySearchButton_(event: Event) {
