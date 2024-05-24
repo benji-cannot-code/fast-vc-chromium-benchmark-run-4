ab25a0c3696696784b4611c952dab79e50bdd33c
Eitan Goldberger <eitang@google.com>
2024-05-24 17:24:51 +0000

    [Read aloud] Remove VoicePackStatus.REMOVED_BY_USER status.
    Per new discussion in team chat, we will re-install languages that the
    user manually uninstalled via the TTS engine, so we don't need this
    state.
    
    Also, per my debug logs, voices weren't marked as REMOVED_BY_USER
    anyways when I manually removed them.
    
    Change-Id: Ic53e9b0007022ae578dc33b57ca06d7c947d4195
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570386
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305796}

--- chrome/browser/resources/side_panel/read_anything/language_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/language_menu.ts
@@ -263,7 +263,6 @@ export class LanguageMenuElement extends LanguageMenuElementBase {
       case VoicePackStatus.NONE:
       case VoicePackStatus.EXISTS:
       case VoicePackStatus.INSTALLED:
-      case VoicePackStatus.REMOVED_BY_USER:
       default:
         return '';
     }
