89a472f5ddbdf636cf391855be32caf7e449da7e
Dustin J. Mitchell <djmitche@chromium.org>
2024-05-24 18:33:01 +0000

    Roll either: 1.11.0 => 1.12.0 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * either: 1.11.0 => 1.12.0
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * either@1.12.0: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I992f5b61e79ec3a3e16710edddb5ae510e7e7fab
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555783
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305831}

--- third_party/rust/chromium_crates_io/vendor/either-1.12.0/src/iterator.rs
+++ third_party/rust/chromium_crates_io/vendor/either-1.12.0/src/iterator.rs
@@ -142,10 +142,9 @@ where
         for_both!(*self, ref mut inner => inner.next_back())
     }
 
-    // TODO(MSRV): This was stabilized in Rust 1.37
-    // fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-    //     for_both!(*self, ref mut inner => inner.nth_back(n))
-    // }
+    fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
+        for_both!(*self, ref mut inner => inner.nth_back(n))
+    }
 
     fn rfold<Acc, G>(self, init: Acc, f: G) -> Acc
     where
@@ -280,10 +279,9 @@ where
         Some(map_either!(self.inner, ref mut inner => inner.next_back()?))
     }
 
-    // TODO(MSRV): This was stabilized in Rust 1.37
-    // fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
-    //     Some(map_either!(self.inner, ref mut inner => inner.nth_back(n)?))
-    // }
+    fn nth_back(&mut self, n: usize) -> Option<Self::Item> {
+        Some(map_either!(self.inner, ref mut inner => inner.nth_back(n)?))
+    }
 
     fn rfold<Acc, G>(self, init: Acc, f: G) -> Acc
     where
