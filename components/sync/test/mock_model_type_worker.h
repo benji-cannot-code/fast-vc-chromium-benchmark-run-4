db71c59699a81139393bfb10ae34d074785d0a44
Mikel Astiz <mastiz@chromium.org>
2024-05-24 15:40:22 +0000

    [sync] Fix DCHECK failure if model reports error during activation
    One API contract between processor and datatype-specific bridge is that,
    once a bridge returns an error (e.g. database error), the processor
    should no longer interact with the bridge (it entered an error state
    that isn't recoverable runtime).
    
    There are several DCHECKs (DUMP_WILL_BE_CHECK) that verify this
    invariant.
    
    One of them is known to fail and is tackled in this patch: if the bridge
    reports and error *while* the datatype is being activated (which is
    asynchronous), the error may be ignored and the activation complete.
    
    The fix consists of invalidating weak pointers in this case, which is
    a one-liner in ClientTagBasedModelTypeProcessor::ReportErrorImpl().
    
    The remaining changes in this patch are test-only changes, required
    to reproduce the desired state, and meanwhile modernize/improve
    MockModelTypeWorker.
    
    Change-Id: Ia68efc4121f41d8f6e65ab97ccf0d0d2434d0c02
    Fixed: 339689058
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568233
    Reviewed-by: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305734}

--- components/sync/test/mock_model_type_worker.h
+++ components/sync/test/mock_model_type_worker.h
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/base/model_type.h"
 #include "components/sync/engine/commit_and_get_updates_types.h"
 #include "components/sync/engine/commit_queue.h"
+#include "components/sync/engine/data_type_activation_response.h"
 #include "components/sync/engine/model_type_processor.h"
 #include "components/sync/protocol/model_type_state.pb.h"
 
@@ -38,8 +39,8 @@ namespace syncer {
 // plausible UpdateResponseData and CommitResponseData messages.
 class MockModelTypeWorker : public CommitQueue {
  public:
-  MockModelTypeWorker(const sync_pb::ModelTypeState& model_type_state,
-                      ModelTypeProcessor* processor);
+  static std::unique_ptr<MockModelTypeWorker> CreateWorkerAndConnectSync(
+      std::unique_ptr<DataTypeActivationResponse> context);
 
   MockModelTypeWorker(const MockModelTypeWorker&) = delete;
   MockModelTypeWorker& operator=(const MockModelTypeWorker&) = delete;
@@ -48,10 +49,6 @@ class MockModelTypeWorker : public CommitQueue {
 
   // Callback when local changes are received from the processor.
   void LocalChangesReceived(CommitRequestDataList&& commit_request);
-
-  // Returns a CommitQueue that forwards all calls to `this`, useful for APIs
-  // that need to take ownership of a CommitQueue.
-  std::unique_ptr<CommitQueue> MakeForwardingCommitQueue();
 
   // Implementation for CommitQueue.
   void NudgeForCommit() override;
@@ -78,6 +75,10 @@ class MockModelTypeWorker : public CommitQueue {
   // Updates the model type state to be used in all future updates from server.
   void UpdateModelTypeState(const sync_pb::ModelTypeState& model_type_state);
 
+  const sync_pb::ModelTypeState model_type_state() const {
+    return model_type_state_;
+  }
+
   // Trigger an update from the server. See GenerateUpdateData for parameter
   // descriptions. |version_offset| defaults to 1 and |ekn| defaults to the
   // current encryption key name the worker has.
@@ -91,7 +92,9 @@ class MockModelTypeWorker : public CommitQueue {
                         const sync_pb::EntitySpecifics& specifics,
                         int64_t version_offset,
                         const std::string& ekn);
-  void UpdateFromServer(UpdateResponseDataList updates);
+  void UpdateFromServer(UpdateResponseDataList updates,
+                        std::optional<sync_pb::GarbageCollectionDirective>
+                            gc_directive = std::nullopt);
 
   // Returns an UpdateResponseData representing an update received from
   // the server. Updates server state accordingly.
@@ -152,18 +155,14 @@ class MockModelTypeWorker : public CommitQueue {
   void UpdateWithEncryptionKey(const std::string& ekn,
                                UpdateResponseDataList update);
 
-  void UpdateWithGarbageCollection(
-      UpdateResponseDataList updates,
-      const sync_pb::GarbageCollectionDirective& gcd);
-
-  void UpdateWithGarbageCollection(
-      const sync_pb::GarbageCollectionDirective& gcd);
-
   // Overrides the default behavior to immediately get local changes from the
   // processor as soon as NudgeForCommit() is invoked.
   void DisableGetLocalChangesUponNudge();
 
  private:
+  explicit MockModelTypeWorker(
+      std::unique_ptr<DataTypeActivationResponse> context);
+
   // Generate an ID string.
   static std::string GenerateId(const ClientTagHash& tag_hash);
 
@@ -184,7 +183,7 @@ class MockModelTypeWorker : public CommitQueue {
   sync_pb::ModelTypeState model_type_state_;
 
   // A pointer to the processor for this mock worker.
-  const raw_ptr<ModelTypeProcessor, AcrossTasksDanglingUntriaged> processor_;
+  const std::unique_ptr<ModelTypeProcessor> processor_;
 
   // A record of past commits requests.
   base::circular_deque<CommitRequestDataList> pending_commits_;
