9e8be804a90d12f15409cf33de024e33e9f883c3
Maria Kazinova <kazinova@google.com>
2024-05-24 09:42:23 +0000

    [Passwords] UMA for save prompt acceptance for users without passwords
    Add a metric that logs save prompt dismissal reason for users who
    currently don't have passwords saved. The metric is needed to
    measure adoption of password saving among new users.
    
    For now the metric is only recorded on Desktop. Follow-up CLs will add
    recording on Android and iOS.
    
    Bug: b/342332945
    Change-Id: I9833cd129961ba45e6f4f56e948bde9ae4323a40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563206
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305589}

--- ios/chrome/browser/passwords/model/ios_chrome_save_password_infobar_delegate.mm
+++ ios/chrome/browser/passwords/model/ios_chrome_save_password_infobar_delegate.mm
@@ -123,7 +123,8 @@ void RecordDismissalMetrics(
         infobar_response);
   } else {
     password_manager::metrics_util::LogSaveUIDismissalReason(
-        infobar_response, account_storage_user_state);
+        infobar_response, account_storage_user_state,
+        /*log_adoption_metric=*/false);
   }
 }
 
