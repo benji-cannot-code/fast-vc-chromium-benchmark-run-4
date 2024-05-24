4e23f6ce3f29ea46c12cb766688b17f465626f53
Jerry Liu <pzliu@google.com>
2024-05-24 21:11:28 +0000

    p13n: remove non-jelly code in p13n common components
    Clean up non-Jelly code paths for p13n common elements in PUB.
    
    BUG=b:282050032
    TEST=browser_tests --gtest_filter="*Personalization*"
    
    Change-Id: I854dc18627b7798b013bf8319ee6d591d8f3c954
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563616
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305928}

--- ash/webui/personalization_app/resources/js/personalization_main_element.ts
+++ ash/webui/personalization_app/resources/js/personalization_main_element.ts
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './ambient/ambient_preview_large_element.js';
 
 import {getShouldShowTimeOfDayBanner} from './ambient/ambient_controller.js';
-import {isAmbientModeAllowed, isPersonalizationJellyEnabled, isRgbKeyboardSupported} from './load_time_booleans.js';
+import {isRgbKeyboardSupported} from './load_time_booleans.js';
 import {getTemplate} from './personalization_main_element.html.js';
 import {WithPersonalizationStore} from './personalization_store.js';
 
@@ -27,12 +27,6 @@ export class PersonalizationMainElement extends WithPersonalizationStore {
   static get properties() {
     return {
       path: String,
-      shouldShowAmbientPreview_: {
-        type: Boolean,
-        value() {
-          return isAmbientModeAllowed() || isPersonalizationJellyEnabled();
-        },
-      },
       isRgbKeyboardSupported_: {
         type: Boolean,
         value() {
--- ash/webui/personalization_app/resources/js/utils.ts
+++ ash/webui/personalization_app/resources/js/utils.ts
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 
 import {AmbientModeAlbum, BacklightColor, BLUE_COLOR, GooglePhotosAlbum, GREEN_COLOR, INDIGO_COLOR, PURPLE_COLOR, RED_COLOR, WHITE_COLOR, YELLOW_COLOR} from './../personalization_app.mojom-webui.js';
-import {isPersonalizationJellyEnabled} from './load_time_booleans.js';
 
 export interface ColorInfo {
   hexVal: string;
@@ -135,15 +134,6 @@ export function getPresetColors(): Record<string, ColorInfo> {
 export function isRecentHighlightsAlbum(album: AmbientModeAlbum|
                                         GooglePhotosAlbum): boolean {
   return album.id === 'RecentHighlights';
-}
-
-/**
- * Returns the icon string for the checkmark.
- */
-export function getCheckmarkIcon(): string {
-  return isPersonalizationJellyEnabled() ?
-      'personalization-shared:circle-checkmark' :
-      'personalization:checkmark';
 }
 
 /**
