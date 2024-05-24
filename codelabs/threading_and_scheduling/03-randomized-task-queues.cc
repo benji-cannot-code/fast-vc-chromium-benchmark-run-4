3ddc16ad333b3dba83807b862d779e6620a14749
Claudio DeSouza <cdesouza@chromium.org>
2024-05-24 16:43:06 +0000

    Replace StringPiece with std::string_view in //codelabs
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //codelabs
    
    Bug: 40506050
    Change-Id: I0fb8cce6c16529ad971989a5af1f19b5e1129a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555569
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305765}

--- codelabs/threading_and_scheduling/03-randomized-task-queues.cc
+++ codelabs/threading_and_scheduling/03-randomized-task-queues.cc
@@ -3,11 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string_view>
+
 #include "base/logging.h"
 #include "base/message_loop/message_pump.h"
 #include "base/rand_util.h"
 #include "base/run_loop.h"
-#include "base/strings/string_piece.h"
 #include "base/task/sequence_manager/sequence_manager.h"
 #include "base/task/sequence_manager/task_queue.h"
 #include "base/task/single_thread_task_runner.h"
@@ -16,7 +17,7 @@ enum class TaskType : unsigned char {
   kSource1 = 1,
 };
 
-void Task(base::StringPiece tq, int task_number) {
+void Task(std::string_view tq, int task_number) {
   if (tq == "A") {
     LOG(INFO) << "TaskQueue(" << tq << "): " << task_number;
   } else {
