3dc19d8d6b509ecc2ec226e170adc7c77ba897e4
Nigel Tao <nigeltao@chromium.org>
2024-05-24 03:46:09 +0000

    fusebox: remove keep_extension argument
    The ExternalFileURLToFuseboxMonikerFileURL argument was introduced (when
    refactoring) for risk minimization, so that some existing code paths had
    unchanged behavior. The behavior change should be harmless, though. This
    commit removes the argument, simplifying the API.
    
    Bug: b/255901609
    Test: CQ
    Change-Id: Ic343c1c16028fe2152ddc97538c28fb994155bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552852
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305496}

--- chrome/browser/ash/fileapi/external_file_url_util.cc
+++ chrome/browser/ash/fileapi/external_file_url_util.cc
@@ -84,8 +84,7 @@ GURL ExternalFileURLToFuseboxMonikerFileURL(
     content::BrowserContext* browser_context,
     const GURL& url,
     bool read_only,
-    base::TimeDelta lifetime,
-    bool keep_extension) {
+    base::TimeDelta lifetime) {
   const base::FilePath virtual_path = ExternalFileURLToVirtualPath(url);
 
   const storage::FileSystemURL fs_url =
@@ -121,10 +120,8 @@ GURL ExternalFileURLToFuseboxMonikerFileURL(
       lifetime);
 
   base::FilePath moniker_path(fusebox::MonikerMap::GetFilename(moniker));
-  if (keep_extension) {
-    moniker_path = moniker_path.AddExtension(virtual_path.Extension());
-  }
-  return net::FilePathToFileURL(moniker_path);
+  return net::FilePathToFileURL(
+      moniker_path.AddExtension(virtual_path.Extension()));
 }
 
 }  // namespace ash
