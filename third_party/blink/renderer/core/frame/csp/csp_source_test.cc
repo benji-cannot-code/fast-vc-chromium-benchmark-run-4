4cf4fc2998098dd8e0002773ca9ea7be28fe3388
Hayato Ito <hayato@chromium.org>
2024-05-24 02:06:25 +0000

    Revert "[URL] Preserve Blink's CSP behavior for non-special URLs"
    This reverts commit 5de3a8d8986b27ec4278e4e6c949f909c8b2ae9b.
    
    Reason for revert: Possible culprit on https://crbug.com/342443922 (P0 issue).
    
    
    Original change's description:
    > [URL] Preserve Blink's CSP behavior for non-special URLs
    >
    > This CL is similar to https://crrev.com/c/5528336, but for Blink's
    > CSP implementation to ensure the behavior remains consistent
    > regardless of non-special URL support.
    >
    > The following tests now pass:
    >
    > - CSPSourceTest.MatchingAsSelf
    > - CSPSourceTest.SchemeIsEmpty
    >
    > Bug: 40063064
    > Change-Id: I80d7e64a627ffc9dbb7616027467ea1c6616b983
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561804
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Commit-Queue: Hayato Ito <hayato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1304889}
    
    Bug: 40063064
    Change-Id: I08672b26fd6f963db855024a07892b06339ef87e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564286
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305479}

--- third_party/blink/renderer/core/frame/csp/csp_source_test.cc
+++ third_party/blink/renderer/core/frame/csp/csp_source_test.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/csp/csp_source.h"
 
-#include "base/test/scoped_feature_list.h"
 #include "services/network/public/mojom/content_security_policy.mojom-blink.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -13,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
-#include "url/url_features.h"
 
 namespace blink {
 
@@ -151,25 +149,7 @@ TEST(CSPSourceTest, InsecureHostSchemeMatchesSecureScheme) {
                                 KURL(base, "https://not-example.com:8000/")));
 }
 
-class CSPSourceParamTest : public ::testing::TestWithParam<bool> {
- public:
-  CSPSourceParamTest()
-      : use_standard_compliant_non_special_scheme_url_parsing_(GetParam()) {
-    scoped_feature_list_.InitWithFeatureState(
-        url::kStandardCompliantNonSpecialSchemeURLParsing,
-        use_standard_compliant_non_special_scheme_url_parsing_);
-  }
-
- protected:
-  bool use_standard_compliant_non_special_scheme_url_parsing_;
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
-
-INSTANTIATE_TEST_SUITE_P(All, CSPSourceParamTest, ::testing::Bool());
-
-TEST_P(CSPSourceParamTest, SchemeIsEmpty) {
+TEST(CSPSourceTest, SchemeIsEmpty) {
   KURL base;
 
   // Self scheme is http.
@@ -345,7 +325,7 @@ TEST(CSPSourceTest, HostMatches) {
   }
 }
 
-TEST_P(CSPSourceParamTest, MatchingAsSelf) {
+TEST(CSPSourceTest, MatchingAsSelf) {
   // Testing Step 4 of
   // https://w3c.github.io/webappsec-csp/#match-url-to-source-expression
   struct Source {
