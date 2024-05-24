b06f351b2f551bb2323c972629ce43e18af94722
Christoph Schwering <schwering@google.com>
2024-05-24 21:42:40 +0000

    [Autofill] Default-initialize PopupOpenArgs::anchor_type
    This CL fixes use of uninitialized memory of PopupOpenArgs::anchor_type
    in ChromeAutofillClient::ShowAutofillSuggestionsImpl().
    
    The reason is that ChromeAutofillClientTest's
    AutofillManualFallbackIPH_HideOnShowAutofillSuggestions passes a
    default-constructed PopupOpenArgs that doesn't initialize its
    PopupOpenArgs::anchor_type.
    
    Bug: 342632514, b/340817507
    Change-Id: I3cbf7b7f7f6a5d6d5c9dddc0efaebed03fb540ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569256
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305941}

--- components/autofill/core/browser/autofill_client.h
+++ components/autofill/core/browser/autofill_client.h
@@ -301,7 +301,7 @@ class AutofillClient {
     AutofillSuggestionTriggerSource trigger_source =
         AutofillSuggestionTriggerSource::kUnspecified;
     int32_t form_control_ax_id = 0;
-    PopupAnchorType anchor_type;
+    PopupAnchorType anchor_type = PopupAnchorType::kField;
   };
 
   // Describes the position of the Autofill popup on the screen.
