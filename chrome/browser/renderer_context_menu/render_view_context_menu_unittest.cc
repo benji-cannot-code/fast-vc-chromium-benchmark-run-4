bdd44b655f112495c6d231a7eedbd01daf20157e
Bruno Braga <brunobraga@google.com>
2024-05-24 12:49:50 +0000

    [autofill] Delete enabled by default flag
    Bug: 1447387
    Change-Id: I91190552bdc5119360012a53b1f6f4570c38412d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560927
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1305652}

--- chrome/browser/renderer_context_menu/render_view_context_menu_unittest.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu_unittest.cc
@@ -1183,8 +1183,6 @@ class RenderViewContextMenuHideAutofillSuggestionsTest
   }
 
  private:
-  base::test::ScopedFeatureList feature_list_{
-      autofill::features::kAutofillPopupDoesNotOverlapWithContextMenu};
   autofill::TestAutofillClientInjector<autofill::TestContentAutofillClient>
       autofill_client_injector_;
 };
