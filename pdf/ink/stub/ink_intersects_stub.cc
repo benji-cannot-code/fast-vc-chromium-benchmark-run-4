a46f08a4148006b66052d816273b0886d73e1052
Lei Zhang <thestig@chromium.org>
2024-05-24 21:46:44 +0000

    [PDF Ink Signatures] Change InkIntersectsPointWithShape() to take a rect
    Change the InkIntersectsPointWithShape() utility function to take a rect
    and rename it to InkIntersectsRectWithShape(). A point by itself
    requires very precise targeting to get an intersection. The eraser tool
    needs to have less precision for it to be used comfortably. Switching to
    doing the hit test with a rect allows the caller to adjust that
    precision.
    
    Bug: 335521183
    Change-Id: I6570ec53fdf6a8019b123b19ed9a7d953b1aa282
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563514
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305945}

--- pdf/ink/stub/ink_intersects_stub.cc
+++ pdf/ink/stub/ink_intersects_stub.cc
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome_pdf {
 
-bool InkIntersectsPointWithShape(float point_x,
-                                 float point_y,
-                                 const InkModeledShape& shape,
-                                 const InkAffineTransform& transform) {
+bool InkIntersectsRectWithShape(float rect_x,
+                                float rect_y,
+                                float rect_width,
+                                float rect_height,
+                                const InkModeledShape& shape,
+                                const InkAffineTransform& transform) {
   return false;
 }
 
