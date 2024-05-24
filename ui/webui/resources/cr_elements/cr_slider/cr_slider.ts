6862f7551662d6c5a92073f6315036ffbee8d094
dpapad <dpapad@chromium.org>
2024-05-24 21:40:52 +0000

    WebUI: Initialize cr-slider value to zero on startup.
    This CL partially reverts http://crrev.com/c/1682308 which was
    intentionally hiding a cr-slider until an initial value was assigned by
    client code. The flashing issue that it was fixing no longer happens,
    most likely due to the migration of cr-slider to Lit and the more
    predictable rendering cycles.
    
    This is in preparation of turning on stricter TS compiler flags for
    cr_elements/.
    
    Bug: 40943652
    Change-Id: Idb2f73ca3405b4d99c1032b95679f55c968d1495
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563418
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305940}

--- ui/webui/resources/cr_elements/cr_slider/cr_slider.ts
+++ ui/webui/resources/cr_elements/cr_slider/cr_slider.ts
@@ -179,7 +179,7 @@ export class CrSliderElement extends CrSliderElementBase {
   noKeybindings: boolean = false;
   snaps: boolean = false;
   ticks: SliderTick[]|number[] = [];
-  value: number;
+  value: number = 0;
 
   protected disabled_: boolean = false;
   protected label_: string = '';
@@ -233,10 +233,6 @@ export class CrSliderElement extends CrSliderElementBase {
 
   override updated(changedProperties: PropertyValues<this>) {
     super.updated(changedProperties);
-
-    if (changedProperties.has('value')) {
-      this.$.container.hidden = false;
-    }
 
     if ((changedProperties as Map<PropertyKey, unknown>).has('disabled_')) {
       this.setAttribute('tabindex', this.disabled_ ? '-1' : '0');
