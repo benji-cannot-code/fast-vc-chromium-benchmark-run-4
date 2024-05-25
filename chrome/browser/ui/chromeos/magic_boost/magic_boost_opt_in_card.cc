26065f58379d25254ae0d48a7f215ca0c6fa63aa
Kevin Radtke <kradtke@google.com>
2024-05-25 00:59:06 +0000

    mahi: Update opt-in strings
    Use updated strings and icons for the opt-in card.
    
    orca:    https://screenshot.googleplex.com/9CyuWrp96TG8jjr
    no orca: https://screenshot.googleplex.com/BVLxAQxsmSynMrZ
    
    Bug: b:341337400
    Change-Id: I70869dff876da1a4274e547b1deb92fe86ae1930
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552134
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306022}

--- chrome/browser/ui/chromeos/magic_boost/magic_boost_opt_in_card.cc
+++ chrome/browser/ui/chromeos/magic_boost/magic_boost_opt_in_card.cc
@@ -7,11 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "build/branding_buildflags.h"
 #include "chrome/browser/chromeos/mahi/mahi_prefs_controller.h"
 #include "chrome/browser/ui/chromeos/magic_boost/magic_boost_constants.h"
 #include "chrome/browser/ui/chromeos/magic_boost/magic_boost_controller.h"
 #include "chrome/browser/ui/views/editor_menu/utils/utils.h"
-#include "components/vector_icons/vector_icons.h"
+#include "chromeos/ui/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/compositor/layer.h"
@@ -28,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/unique_widget_ptr.h"
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "chromeos/ash/resources/internal/strings/grit/ash_internal_strings.h"
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 namespace chromeos {
 
@@ -58,22 +63,25 @@ constexpr int kBetweenImageAndTextSpacing = 16;
 constexpr int kBetweenContentsAndButtonsSpacing = 16;
 constexpr int kBetweenLabelsSpacing = 4;
 
-// Placeholder values
-// TODO(b/339528642): Resolve with real strings.
-const std::u16string title_text = u"Title text";
-const std::u16string body_text =
+// Placeholder strings
+#if !BUILDFLAG(GOOGLE_CHROME_BRANDING)
+const std::u16string kPlaceholderTitleText = u"Title text";
+const std::u16string kPlaceholderBodyText =
     u"Body text that is multi-line which means it can span from one line to up "
     u"to three lines for this case";
-const std::u16string secondary_button_text = u"No thanks";
-const std::u16string primary_button_text = u"Try it";
+#endif  // !BUILDFLAG(GOOGLE_CHROME_BRANDING)
+
+// Content strings
+const std::u16string kSecondaryButtonText = u"No thanks";
+const std::u16string kPrimaryButtonText = u"Try it";
 
 // Font lists
-const gfx::FontList body_text_font_list =
+const gfx::FontList kBodyTextFontList =
     gfx::FontList({"Google Sans", "Roboto"},
                   gfx::Font::NORMAL,
                   /*font_size=*/12,
                   gfx::Font::Weight::NORMAL);
-const gfx::FontList title_text_font_list =
+const gfx::FontList kTitleTextFontList =
     gfx::FontList({"Google Sans", "Roboto"},
                   gfx::Font::NORMAL,
                   /*font_size=*/14,
@@ -83,7 +91,7 @@ const gfx::FontList title_text_font_list =
 
 // MagicBoostOptInCard --------------------------------------------------------
 
-MagicBoostOptInCard::MagicBoostOptInCard() {
+MagicBoostOptInCard::MagicBoostOptInCard(const bool include_orca) {
   SetLayoutManager(std::make_unique<views::FlexLayout>())
       ->SetOrientation(views::LayoutOrientation::kVertical)
       .SetInteriorMargin(kInteriorMargin)
@@ -121,7 +129,7 @@ MagicBoostOptInCard::MagicBoostOptInCard() {
               views::Builder<views::ImageView>()
                   .SetPreferredSize(gfx::Size(kImageViewSize, kImageViewSize))
                   .SetImage(ui::ImageModel::FromVectorIcon(
-                      vector_icons::kUsbIcon, ui::kColorSysOnPrimaryContainer,
+                      kMahiSparkIcon, ui::kColorSysOnPrimaryContainer,
                       kImageViewIconSize))
                   .SetBackground(views::CreateThemedSolidBackground(
                       ui::kColorSysPrimaryContainer))
@@ -152,24 +160,37 @@ MagicBoostOptInCard::MagicBoostOptInCard() {
               views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToZero,
                                        views::MaximumFlexSizeRule::kUnbounded,
                                        /*adjust_height_for_width=*/true))
-          .AddChildren(views::Builder<views::Label>()
-                           .SetText(title_text)
-                           .SetHorizontalAlignment(gfx::ALIGN_LEFT)
-                           .SetEnabledColorId(ui::kColorSysOnSurface)
-                           .SetAutoColorReadabilityEnabled(false)
-                           .SetSubpixelRenderingEnabled(false)
-                           .SetFontList(title_text_font_list)
-                           .SetMultiLine(true)
-                           .SetMaxLines(kTitleLabelMaxLines),
-                       views::Builder<views::Label>()
-                           .SetText(body_text)
-                           .SetHorizontalAlignment(gfx::ALIGN_LEFT)
-                           .SetEnabledColorId(ui::kColorSysOnSurface)
-                           .SetAutoColorReadabilityEnabled(false)
-                           .SetSubpixelRenderingEnabled(false)
-                           .SetFontList(body_text_font_list)
-                           .SetMultiLine(true)
-                           .SetMaxLines(kBodyLabelMaxLines))
+          .AddChildren(
+              views::Builder<views::Label>()
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+                  .SetText(l10n_util::GetStringUTF16(
+                      include_orca ? IDS_MAGIC_BOOST_OPT_IN_CARD_TITLE
+                                   : IDS_MAGIC_BOOST_OPT_IN_CARD_NO_ORCA_TITLE))
+#else
+                  .SetText(kPlaceholderTitleText)
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+                  .SetHorizontalAlignment(gfx::ALIGN_LEFT)
+                  .SetEnabledColorId(ui::kColorSysOnSurface)
+                  .SetAutoColorReadabilityEnabled(false)
+                  .SetSubpixelRenderingEnabled(false)
+                  .SetFontList(kTitleTextFontList)
+                  .SetMultiLine(true)
+                  .SetMaxLines(kTitleLabelMaxLines),
+              views::Builder<views::Label>()
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+                  .SetText(l10n_util::GetStringUTF16(
+                      include_orca ? IDS_MAGIC_BOOST_OPT_IN_CARD_BODY
+                                   : IDS_MAGIC_BOOST_OPT_IN_CARD_NO_ORCA_BODY))
+#else
+                  .SetText(kPlaceholderBodyText)
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+                  .SetHorizontalAlignment(gfx::ALIGN_LEFT)
+                  .SetEnabledColorId(ui::kColorSysOnSurface)
+                  .SetAutoColorReadabilityEnabled(false)
+                  .SetSubpixelRenderingEnabled(false)
+                  .SetFontList(kBodyTextFontList)
+                  .SetMultiLine(true)
+                  .SetMaxLines(kBodyLabelMaxLines))
           .Build());
 
   // Create buttons container that holds two buttons.
@@ -184,16 +205,16 @@ MagicBoostOptInCard::MagicBoostOptInCard() {
           .AddChildren(views::Builder<views::MdTextButton>()
                            .CopyAddressTo(&secondary_button_)
                            .SetID(magic_boost::ViewId::OptInCardSecondaryButton)
-                           .SetText(secondary_button_text)
-                           .SetAccessibleName(secondary_button_text)
+                           .SetText(kSecondaryButtonText)
+                           .SetAccessibleName(kSecondaryButtonText)
                            .SetStyle(ui::ButtonStyle::kText)
                            .SetCallback(base::BindRepeating(
                                &MagicBoostOptInCard::OnSecondaryButtonPressed,
                                weak_ptr_factory_.GetWeakPtr())),
                        views::Builder<views::MdTextButton>()
                            .SetID(magic_boost::ViewId::OptInCardPrimaryButton)
-                           .SetText(primary_button_text)
-                           .SetAccessibleName(primary_button_text)
+                           .SetText(kPrimaryButtonText)
+                           .SetAccessibleName(kPrimaryButtonText)
                            .SetStyle(ui::ButtonStyle::kProminent)
                            .SetCallback(base::BindRepeating(
                                &MagicBoostOptInCard::OnPrimaryButtonPressed,
@@ -205,7 +226,8 @@ MagicBoostOptInCard::~MagicBoostOptInCard() = default;
 
 // static
 views::UniqueWidgetPtr MagicBoostOptInCard::CreateWidget(
-    const gfx::Rect& anchor_view_bounds) {
+    const gfx::Rect& anchor_view_bounds,
+    const bool include_orca) {
   views::Widget::InitParams params(
       views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET,
       views::Widget::InitParams::TYPE_POPUP);
@@ -218,8 +240,8 @@ views::UniqueWidgetPtr MagicBoostOptInCard::CreateWidget(
 
   views::UniqueWidgetPtr widget =
       std::make_unique<views::Widget>(std::move(params));
-  MagicBoostOptInCard* magic_boost_opt_in_card =
-      widget->SetContentsView(std::make_unique<MagicBoostOptInCard>());
+  MagicBoostOptInCard* magic_boost_opt_in_card = widget->SetContentsView(
+      std::make_unique<MagicBoostOptInCard>(include_orca));
   magic_boost_opt_in_card->UpdateWidgetBounds(anchor_view_bounds);
 
   return widget;
