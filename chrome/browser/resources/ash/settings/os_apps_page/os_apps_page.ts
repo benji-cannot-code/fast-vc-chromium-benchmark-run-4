441dfc295cf1cd87e2377676400597c78d367664
Sean Kau <skau@chromium.org>
2024-05-25 03:32:28 +0000

    Reland "Redirect to the APPS page for direct navigation"
    This is a reland of commit 11ea6b176c4e46873f837edef1144c73c084ede7
    
    Test was failing due to introduction of the app-setup-dialog. Submit
    action for that dialog has been modified to update isPinVerified_
    
    Original change's description:
    > Redirect to the APPS page for direct navigation
    >
    > If the parental controls page is opened directly, redirect to the apps
    > page.  The Apps page sets is-verified before opening parental controls
    > if the PIN was accepted.
    >
    > Bug: b:333946259
    > Change-Id: I8968972e7d41e3cb1949cfe6327c5df48f8dea86
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5529844
    > Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Aga Wronska <agawronska@chromium.org>
    > Commit-Queue: Sean Kau <skau@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1304792}
    
    Bug: b:333946259
    Change-Id: I4fa2a9c3e676c9d2b21a501f7955e54e50a38d95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563773
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306056}

--- chrome/browser/resources/ash/settings/os_apps_page/os_apps_page.ts
+++ chrome/browser/resources/ash/settings/os_apps_page/os_apps_page.ts
@@ -215,6 +215,11 @@ export class OsSettingsAppsPageElement extends OsSettingsAppsPageElementBase {
         value: false,
       },
 
+      isPinVerified_: {
+        type: Boolean,
+        value: false,
+      },
+
       /**
        * Used by DeepLinkingMixin to focus this page's deep links.
        */
@@ -270,6 +275,7 @@ export class OsSettingsAppsPageElement extends OsSettingsAppsPageElementBase {
   private appsWithNotifications_: AppWithNotifications[];
   private isArcVmManageUsbAvailable_: boolean;
   private isDndEnabled_: boolean;
+  private isPinVerified_: boolean;
   private readonly isPlayStoreAvailable_: boolean;
   private isPluginVmAvailable_: boolean;
   private isRevampWayfindingEnabled_: boolean;
@@ -466,6 +472,7 @@ export class OsSettingsAppsPageElement extends OsSettingsAppsPageElementBase {
   }
 
   private navigateToParentalControls_(): void {
+    this.isPinVerified_ = true;
     Router.getInstance().navigateTo(routes.APP_PARENTAL_CONTROLS);
   }
 }
