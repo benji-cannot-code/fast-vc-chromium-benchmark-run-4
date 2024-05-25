8238ea0849c6cdab86886035e71eb20a015ac92f
xiangdong kong <xiangdongkong@google.com>
2024-05-25 01:23:08 +0000

    printing: Implement dispatching print requests to lacros from ash
    - Marked the struct RefCountedMemory as stable so that it can be used
      in extension_printer.mojom.
    - Added ash browser tests that requires lacros to test the extension
      printer crosapi in a round-trip (ash->lacros->ash).
    
    Bug: b:40273973
    Test: browser_tests --lacros-chrome-path=out_linux_ash/Release/lacros_clang_x64/test_lacros_chrome --test-launcher-summary-output=/tmp/default/output.json --gtest_filter=ExtensionPrinterHandlerAdapterAshBrowserTest.*
    Change-Id: I68574f51347d2d58f5c65f1d11727523e339d5c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539377
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306030}

--- chrome/browser/ui/webui/print_preview/extension_printer_service_provider_lacros.cc
+++ chrome/browser/ui/webui/print_preview/extension_printer_service_provider_lacros.cc
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/print_preview/extension_printer_service_provider_lacros.h"
 
 #include <memory>
+#include <string>
+#include <unordered_map>
 
 #include "base/functional/bind.h"
+#include "base/no_destructor.h"
 #include "base/unguessable_token.h"
+#include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/webui/print_preview/extension_printer_service_provider_factory_lacros.h"
@@ -17,6 +21,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/lacros/lacros_service.h"
 
 namespace printing {
+
+using crosapi::mojom::StartPrintStatus;
+
+StartPrintStatus ToStartPrintStatus(const base::Value& status) {
+  static base::NoDestructor<std::unordered_map<std::string, StartPrintStatus>>
+      string_to_status_map(
+          {{"UNKNOWN", StartPrintStatus::kUnknown},
+           {"OK", StartPrintStatus::KOk},
+           {"FAILED", StartPrintStatus::kFailed},
+           {"INVALID_TICKET", StartPrintStatus::kInvalidTicket},
+           {"INVALID_DATA", StartPrintStatus::kInvalidData}});
+
+  const auto it = string_to_status_map->find(status.GetString());
+  return it != string_to_status_map->end() ? it->second
+                                           : StartPrintStatus::kUnknown;
+}
 
 ExtensionPrinterServiceProviderLacros::ExtensionPrinterServiceProviderLacros(
     content::BrowserContext* browser_context)
@@ -101,6 +121,22 @@ void ExtensionPrinterServiceProviderLacros::DispatchStartGetCapability(
       << " destination_id=" << destination_id;
   extension_printer_handler_->StartGetCapability(destination_id,
                                                  std::move(callback));
+}
+
+void ExtensionPrinterServiceProviderLacros::DispatchStartPrint(
+    const std::u16string& job_title,
+    base::Value::Dict settings,
+    scoped_refptr<::base::RefCountedMemory> print_data,
+    DispatchStartPrintCallback callback) {
+  VLOG(1) << "ExtensionPrinterServiceProviderLacros::DispatchStartPrint():"
+          << " job_title=" << job_title;
+  extension_printer_handler_->StartPrint(
+      job_title, std::move(settings), print_data,
+      base::BindOnce(
+          [](DispatchStartPrintCallback callback, const base::Value& status) {
+            std::move(callback).Run(ToStartPrintStatus(status));
+          },
+          std::move(callback)));
 }
 
 }  // namespace printing
