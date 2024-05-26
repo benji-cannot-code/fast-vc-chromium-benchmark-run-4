48700e29f19f8c3f85ee294daef56428e7e42adf
Claudio DeSouza <cdesouza@chromium.org>
2024-05-26 12:27:50 +0000

    Replace StringPiece with std::string_view in //components/crash
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //components/crash
    
    Bug: 40506050
    Change-Id: I723ac7226db0f127f80c261810fa40490c6d3f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555570
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306173}

--- components/crash/core/app/crashpad_mac.mm
+++ components/crash/core/app/crashpad_mac.mm
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/files/file_path.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/sys_string_conversions.h"
 #include "build/branding_buildflags.h"
 #include "components/crash/core/app/crash_reporter_client.h"
