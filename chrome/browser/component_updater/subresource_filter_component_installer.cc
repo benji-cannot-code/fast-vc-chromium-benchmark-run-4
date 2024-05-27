710b981f604ed0472fda955397ee06c867f428a2
Claudio DeSouza <cdesouza@chromium.org>
2024-05-27 23:34:18 +0000

    Replace StringPiece with std::string_view in //c/b/component_updater
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //chrome/browser/component_updater
    
    Bug: 40506050
    Change-Id: I58d140eba83bbe5fac1f5a225192110f7fce44d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555562
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306539}

--- chrome/browser/component_updater/subresource_filter_component_installer.cc
+++ chrome/browser/component_updater/subresource_filter_component_installer.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/ref_counted.h"
 #include "base/path_service.h"
-#include "base/strings/string_piece.h"
 #include "base/values.h"
 #include "base/version.h"
 #include "chrome/browser/browser_process.h"
