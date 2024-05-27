f690bdf86b891a504047c5f384176dd48afe4cb3
Jack Yammine <jyammine@google.com>
2024-05-27 09:13:03 +0000

    [waffle]Explicitly support 'NO_REPROMPT' as reprompt param
    We previously handled everything that wasn't a valid JSON as an
    `invalid dictionary`.
    
    We add the `NO_REPROMPT` param to be able to identify this special
    value compared to potentially other sources of errors.
    
    Fixed: b:341217272
    Change-Id: Ida2aafa7e02abd9e4a34822fa13b9e5902f29d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569614
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306327}

--- components/search_engines/search_engine_choice/search_engine_choice_utils.h
+++ components/search_engines/search_engine_choice/search_engine_choice_utils.h
@@ -23,6 +23,11 @@ struct TemplateURLData;
 
 namespace search_engines {
 
+// The string that's passed to
+// `switches::kSearchEngineChoiceTriggerRepromptParams` so that we don't
+// reprompt users with the choice screen.
+inline const char kNoRepromptString[] = "NO_REPROMPT";
+
 extern const char kSearchEngineChoiceScreenProfileInitConditionsHistogram[];
 extern const char kSearchEngineChoiceScreenNavigationConditionsHistogram[];
 extern const char kSearchEngineChoiceScreenEventsHistogram[];
@@ -149,8 +154,10 @@ enum class RepromptResult {
   kChromeTooOld = 4,
   // The choice was made recently enough.
   kRecentChoice = 5,
+  // Do not reprompt the users.
+  kNoReprompt = 6,
 
-  kMaxValue = kRecentChoice,
+  kMaxValue = kNoReprompt,
 };
 
 struct ChoiceScreenDisplayState {
