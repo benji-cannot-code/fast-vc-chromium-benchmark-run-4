2b7435b8b61a6de585271402e408aee10e6cb327
Kam Kwankajornkiet <kamchonlathorn@google.com>
2024-05-27 08:28:20 +0000

    CCA: Check if test bridge is connected from Tast
    Add a function in TestBridge to be called from Tast side to indicate
    that the camera app is launched from Tast. The value can be used to
    avoid unnecessary operations in test, such as showing user education
    dialog.
    
    Change in Tast side: CL:5568705
    
    Bug: b:340399683
    Test: tast run $brya camera.CCAUICLI camera.CCAUIIntent.vm
    Change-Id: Iaec0c59c8de9eac14af308f8f2633c831a646283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568538
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Reviewed-by: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306312}

--- ash/webui/camera_app_ui/resources/js/init.ts
+++ ash/webui/camera_app_ui/resources/js/init.ts
@@ -21,6 +21,7 @@ declare global {
     // TODO(crbug.com/980846): Refactor to use a better way rather than window
     // properties to pass data to other modules.
     appWindow: Comlink.Remote<AppWindow>|null;
+    isInTestSession: boolean;
     windowCreationTime: number;
   }
 }
@@ -57,6 +58,7 @@ document.addEventListener('DOMContentLoaded', async () => {
   const appWindow = await testBridge.bindWindow(window.location.href);
   window.appWindow = appWindow;
   window.windowCreationTime = performance.now();
+  window.isInTestSession = await testBridge.isInTestSession();
   if (appWindow !== null) {
     await appWindow.waitUntilReadyOnTastSide();
   }
