1118437728dd56dafa61e329fe1b3db214fe9300
Fiona Macintosh <fmacintosh@google.com>
2024-05-27 15:22:14 +0000

    IsOnlyPartitionedStorageAccessAllowed: feature-gate TP check+use TPS API
    Bug: b:340279021
    Change-Id: I472ed9f7569e759d90259e02d3554992ebde635f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570375
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Theodore Olsauskas-Warren <sauski@google.com>
    Reviewed-by: Theodore Olsauskas-Warren <sauski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306437}

--- chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
+++ chrome/browser/ui/views/site_data/page_specific_site_data_dialog.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/site_data/page_specific_site_data_dialog.h"
+
 #include <memory>
 #include <string>
 
@@ -14,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/history/history_service_factory.h"
+#include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -34,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/content_settings/core/common/content_settings_utils.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "components/omnibox/browser/favicon_cache.h"
+#include "components/privacy_sandbox/privacy_sandbox_features.h"
+#include "components/privacy_sandbox/tracking_protection_settings.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browsing_data_filter_builder.h"
 #include "content/public/browser/browsing_data_remover.h"
@@ -138,6 +142,8 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
         HistoryServiceFactory::GetForProfile(
             profile, ServiceAccessType::EXPLICIT_ACCESS));
     cookie_settings_ = CookieSettingsFactory::GetForProfile(profile);
+    tracking_protection_settings_ =
+        TrackingProtectionSettingsFactory::GetForProfile(profile);
     host_content_settings_map_ =
         HostContentSettingsMapFactory::GetForProfile(profile);
 
@@ -336,15 +342,18 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
             current_url, GURL(), ContentSettingsType::COOKIES);
 
     // Check for either a COOKIES or TRACKING_PROTECTION site exception.
-    bool has_site_level_exception = false;
-    for (const auto type : {ContentSettingsType::COOKIES,
-                            ContentSettingsType::TRACKING_PROTECTION}) {
-      content_settings::SettingInfo info;
-      host_content_settings_map_->GetContentSetting(site_origin.GetURL(),
-                                                    current_url, type, &info);
+    content_settings::SettingInfo info;
+    host_content_settings_map_->GetContentSetting(
+        site_origin.GetURL(), current_url, ContentSettingsType::COOKIES, &info);
+    bool has_site_level_exception =
+        info.primary_pattern != ContentSettingsPattern::Wildcard() ||
+        info.secondary_pattern != ContentSettingsPattern::Wildcard();
+
+    if (base::FeatureList::IsEnabled(
+            privacy_sandbox::kTrackingProtectionContentSettingFor3pcb)) {
       has_site_level_exception |=
-          info.primary_pattern != ContentSettingsPattern::Wildcard() ||
-          info.secondary_pattern != ContentSettingsPattern::Wildcard();
+          tracking_protection_settings_->GetTrackingProtectionSetting(
+              current_url) == CONTENT_SETTING_ALLOW;
     }
 
     // Partitioned access is displayed when all of these conditions are met:
@@ -394,6 +403,8 @@ class PageSpecificSiteDataDialogModelDelegate : public ui::DialogModelDelegate {
       blocked_browsing_data_model_for_testing_ = nullptr;
   std::unique_ptr<FaviconCache> favicon_cache_;
   scoped_refptr<content_settings::CookieSettings> cookie_settings_;
+  raw_ptr<privacy_sandbox::TrackingProtectionSettings>
+      tracking_protection_settings_;
   raw_ptr<HostContentSettingsMap> host_content_settings_map_;
 
   // Whether user has done any changes to the site data, deleted site data for a
