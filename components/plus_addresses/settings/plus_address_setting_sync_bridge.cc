67651c0579f99a9167ea00da979597ecabcd05dc
Florian Leimgruber <fleimgruber@google.com>
2024-05-27 14:13:41 +0000

    PlusAddressSettingSyncBridge: Load metadata
    Adds logic initialising the ModelTypeStore, loading existing metadata
    and calling ModelReadyToSync().
    
    Bug: b/342089839
    Change-Id: I343cdb2811faf4e3055e798bf737cb326876909f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557886
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306412}

--- components/plus_addresses/settings/plus_address_setting_sync_bridge.cc
+++ components/plus_addresses/settings/plus_address_setting_sync_bridge.cc
@@ -8,16 +8,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 
+#include "base/functional/bind.h"
 #include "base/notreached.h"
+#include "base/sequence_checker.h"
+#include "components/sync/base/model_type.h"
 #include "components/sync/model/client_tag_based_model_type_processor.h"
 #include "components/sync/model/model_type_store.h"
 
 namespace plus_addresses {
 
+// Macro to simplify reporting errors raised by ModelTypeStore operations.
+#define RETURN_IF_ERROR(error)               \
+  if (error) {                               \
+    change_processor()->ReportError(*error); \
+    return;                                  \
+  }
+
 PlusAddressSettingSyncBridge::PlusAddressSettingSyncBridge(
     std::unique_ptr<syncer::ModelTypeChangeProcessor> change_processor,
     syncer::OnceModelTypeStoreFactory store_factory)
-    : ModelTypeSyncBridge(std::move(change_processor)) {}
+    : ModelTypeSyncBridge(std::move(change_processor)) {
+  std::move(store_factory)
+      .Run(syncer::PLUS_ADDRESS_SETTING,
+           base::BindOnce(&PlusAddressSettingSyncBridge::OnStoreCreated,
+                          weak_factory_.GetWeakPtr()));
+}
+
+PlusAddressSettingSyncBridge::~PlusAddressSettingSyncBridge() = default;
 
 std::unique_ptr<syncer::MetadataChangeList>
 PlusAddressSettingSyncBridge::CreateMetadataChangeList() {
@@ -61,6 +78,25 @@ std::string PlusAddressSettingSyncBridge::GetStorageKey(
     const syncer::EntityData& entity_data) {
   NOTIMPLEMENTED();
   return "";
+}
+
+void PlusAddressSettingSyncBridge::OnStoreCreated(
+    const std::optional<syncer::ModelError>& error,
+    std::unique_ptr<syncer::ModelTypeStore> store) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  RETURN_IF_ERROR(error);
+  store_ = std::move(store);
+  store_->ReadAllMetadata(
+      base::BindOnce(&PlusAddressSettingSyncBridge::StartSyncingWithMetadata,
+                     weak_factory_.GetWeakPtr()));
+}
+
+void PlusAddressSettingSyncBridge::StartSyncingWithMetadata(
+    const std::optional<syncer::ModelError>& error,
+    std::unique_ptr<syncer::MetadataBatch> metadata_batch) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  RETURN_IF_ERROR(error);
+  change_processor()->ModelReadyToSync(std::move(metadata_batch));
 }
 
 }  // namespace plus_addresses
