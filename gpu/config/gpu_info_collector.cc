cbea55299c62c73f3d649897bdec7cee0d5a3ba0
Claudio DeSouza <cdesouza@chromium.org>
2024-05-27 19:40:06 +0000

    Replace StringPiece with std::string_view in //gpu
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //gpu
    
    Bug: 40506050
    Change-Id: Iae761a663c8857de1c178ead82b221c90f94936c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555577
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306513}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/path_service.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/trace_event/trace_event.h"
--- gpu/vulkan/tests/basic_vulkan_test.cc
+++ gpu/vulkan/tests/basic_vulkan_test.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/vulkan/tests/basic_vulkan_test.h"
 
 #include "base/command_line.h"
-#include "base/strings/string_piece.h"
 #include "build/build_config.h"
 #include "gpu/vulkan/init/vulkan_factory.h"
 #include "gpu/vulkan/tests/native_window.h"
