ab45f940cd595ab72edca0ff88fc536661e1f9f1
Koji Ishii <kojii@chromium.org>
2024-05-27 13:06:06 +0000

    [text-box-trim] Support when the last line is empty
    Normally, empty lines can exist only in an empty inline
    formatting context. But there are some rare cases where empty
    lines can exist in non-empty inline formatting context. When
    such empty lines are at the end of the inline formatting
    context, `RelayoutForTextBoxTrimEnd()` needs to let
    `InlineLayoutAlgorithm` know whether the line is the last
    non-empty line or not.
    
    This patch adds `ShouldForceTextBoxTrimEnd()` to communicate
    that information from `BlockLayoutAlgorithm` to
    `InlineLayoutAlgorithm`.
    
    Bug: 40254880
    Change-Id: Ia440325c502db0dcc3c8f248cc3c7ec3588f7fd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568835
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306386}

--- third_party/blink/renderer/core/layout/constraint_space.h
+++ third_party/blink/renderer/core/layout/constraint_space.h
@@ -798,6 +798,9 @@ class CORE_EXPORT ConstraintSpace final {
   bool ShouldTextBoxTrimEnd() const {
     return HasRareData() && rare_data_->should_text_box_trim_end;
   }
+  bool ShouldForceTextBoxTrimEnd() const {
+    return HasRareData() && rare_data_->should_force_text_box_trim_end;
+  }
 
   // Return how percentage-based margins and padding should be resolved.
   DecorationPercentageResolutionType GetDecorationPercentageResolutionType()
@@ -969,6 +972,7 @@ class CORE_EXPORT ConstraintSpace final {
           is_inside_repeatable_content(other.is_inside_repeatable_content),
           should_text_box_trim_start(other.should_text_box_trim_start),
           should_text_box_trim_end(other.should_text_box_trim_end),
+          should_force_text_box_trim_end(other.should_force_text_box_trim_end),
           decoration_percentage_resolution_type(
               other.decoration_percentage_resolution_type) {
       switch (GetDataUnionType()) {
@@ -1054,6 +1058,8 @@ class CORE_EXPORT ConstraintSpace final {
           is_inside_repeatable_content != other.is_inside_repeatable_content ||
           should_text_box_trim_start != other.should_text_box_trim_start ||
           should_text_box_trim_end != other.should_text_box_trim_end ||
+          should_force_text_box_trim_end !=
+              other.should_force_text_box_trim_end ||
           decoration_percentage_resolution_type !=
               other.decoration_percentage_resolution_type) {
         return false;
@@ -1095,6 +1101,7 @@ class CORE_EXPORT ConstraintSpace final {
           propagate_child_break_values || is_at_fragmentainer_start ||
           should_repeat || is_inside_repeatable_content ||
           should_text_box_trim_start || should_text_box_trim_end ||
+          should_force_text_box_trim_end ||
           decoration_percentage_resolution_type) {
         return false;
       }
@@ -1352,6 +1359,7 @@ class CORE_EXPORT ConstraintSpace final {
     unsigned is_inside_repeatable_content : 1 = false;
     unsigned should_text_box_trim_start : 1 = false;
     unsigned should_text_box_trim_end : 1 = false;
+    unsigned should_force_text_box_trim_end : 1 = false;
     unsigned decoration_percentage_resolution_type : 1 = static_cast<unsigned>(
         DecorationPercentageResolutionType::kContainingBlockInlineSize);
 
--- third_party/blink/renderer/core/layout/inline/inline_break_token.h
+++ third_party/blink/renderer/core/layout/inline/inline_break_token.h
@@ -85,6 +85,7 @@ class CORE_EXPORT InlineBreakToken final : public BreakToken {
   const InlineItemTextIndex& Start() const { return start_; }
   wtf_size_t StartItemIndex() const { return start_.item_index; }
   wtf_size_t StartTextOffset() const { return start_.text_offset; }
+  static bool IsStartEqual(const InlineBreakToken*, const InlineBreakToken*);
 
   bool UseFirstLineStyle() const {
     return flags_ & kUseFirstLineStyle;
@@ -143,6 +144,14 @@ class CORE_EXPORT InlineBreakToken final : public BreakToken {
   // This is an array of one item if |kHasRareData|, or zero.
   RareData rare_data_[];
 };
+
+inline bool InlineBreakToken::IsStartEqual(const InlineBreakToken* lhs,
+                                           const InlineBreakToken* rhs) {
+  if (!lhs) {
+    return !rhs;
+  }
+  return rhs && lhs->Start() == rhs->Start();
+}
 
 template <>
 struct DowncastTraits<InlineBreakToken> {
