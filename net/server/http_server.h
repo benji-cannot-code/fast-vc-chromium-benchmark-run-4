16fa30857b9a3f530f756a50a69d72e2d8c9c6b7
Matt Menke <mmenke@chromium.org>
2024-05-27 06:15:57 +0000

    Fix DanglingUntriaged pointer in net::WebSocket.
    The class has a raw pointer to the HttpServer that created it.
    The HttpServer owns HttpConnections, and HttpConnections own WebSockets,
    so this looks safe, but on close, the HttpServer posts a task to destroy
    the affected HttpConnection. If the HttpServer happens to be destroyed
    before that task runs, the WebSocket has a dangling pointer.
    
    This CL fixes that by making the HttpServer own HttpConnections whose
    destruction is pending (in a vector separate from live connections), so
    if the HttpServer is destroyed before the deletion task runs, the
    WebSockets are still destroyed first.
    
    Bug: 40212619
    Change-Id: Idc5122d6e33db0d61392391980700ad8a083e5ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570147
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306278}

--- net/server/http_server.h
+++ net/server/http_server.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 #include <string_view>
+#include <vector>
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -129,12 +130,20 @@ class HttpServer {
   // Whether or not Close() has been called during delegate callback processing.
   bool HasClosedConnection(HttpConnection* connection);
 
+  void DestroyClosedConnections();
+
   const std::unique_ptr<ServerSocket> server_socket_;
   std::unique_ptr<StreamSocket> accepted_socket_;
   const raw_ptr<HttpServer::Delegate> delegate_;
 
   int last_id_ = 0;
   std::map<int, std::unique_ptr<HttpConnection>> id_to_connection_;
+
+  // Vector of connections whose destruction is pending. Connections may have
+  // WebSockets with raw pointers to `this`, so should not out live this, but
+  // also cannot safely be destroyed synchronously, so on connection close, add
+  // a Connection here, and post a task to destroy them.
+  std::vector<std::unique_ptr<HttpConnection>> closed_connections_;
 
   base::WeakPtrFactory<HttpServer> weak_ptr_factory_{this};
 };
