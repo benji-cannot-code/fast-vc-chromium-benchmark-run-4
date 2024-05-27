1e37dda11c67247dd57bed714140faa7f9310bdb
Claudio DeSouza <cdesouza@chromium.org>
2024-05-27 23:18:39 +0000

    Replace StringPiece with std::string_view in //c/b/browser_switcher
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //chrome/browser/browser_switcher.
    
    Bug: 40506050
    Change-Id: I103ab9b35370d45d598c7dc0ceacc69f7f1213c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555561
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306536}

--- chrome/browser/browser_switcher/browser_switcher_service_win.cc
+++ chrome/browser/browser_switcher/browser_switcher_service_win.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/path_service.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/syslog_logging.h"
