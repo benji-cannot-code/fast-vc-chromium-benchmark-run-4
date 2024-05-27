20fc37a3706b225162b0a0e2aeb65d66c5ecb308
Glen Robertson <glenrob@chromium.org>
2024-05-27 07:04:54 +0000

    WebAppInstallInfo: remove default constructor and all remaining usage
    The default WebAppInstallInfo constructor is being removed to allow
    enforcement of invariants like required fields. This migrates all
    remaining code to use the preferred constructor.
    
    POC for this change: dmurph@ if glenrob@ is unavailable.
    
    Bug: b/280862254
    Change-Id: I65410cd0dba00990364aa218067fcdf370e63782
    Fixed: b/280862254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5552860
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Auto-Submit: Glen Robertson <glenrob@chromium.org>
    Commit-Queue: Patti Lor <patricialor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306288}

--- chrome/browser/lacros/web_app_provider_bridge_lacros.cc
+++ chrome/browser/lacros/web_app_provider_bridge_lacros.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_command_manager.h"
 #include "chrome/browser/web_applications/web_app_command_scheduler.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
+#include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_install_finalizer.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
@@ -151,9 +152,15 @@ void WebAppProviderBridgeLacros::WebAppInstalledInArcImpl(
     Profile* profile) {
   DCHECK(profile);
   auto* provider = web_app::WebAppProvider::GetForWebApps(profile);
-  auto install_info = std::make_unique<web_app::WebAppInstallInfo>();
+  GURL start_url = arc_install_info->start_url;
+  // TODO(b:340994232): ARC-installed web apps should pass through a manifest ID
+  // and use it here instead of assuming it is not set and generating it from
+  // the start URL.
+  webapps::ManifestId manifest_id =
+      web_app::GenerateManifestIdFromStartUrlOnly(start_url);
+  auto install_info =
+      std::make_unique<web_app::WebAppInstallInfo>(manifest_id, start_url);
   install_info->title = arc_install_info->title;
-  install_info->start_url = arc_install_info->start_url;
   install_info->display_mode = blink::mojom::DisplayMode::kStandalone;
   install_info->user_display_mode =
       web_app::mojom::UserDisplayMode::kStandalone;
--- chrome/browser/web_applications/web_app_install_info.cc
+++ chrome/browser/web_applications/web_app_install_info.cc
@@ -355,8 +355,6 @@ WebAppInstallInfo::CreateWithStartUrlForTesting(const GURL& start_url) {
   return info;
 }
 
-WebAppInstallInfo::WebAppInstallInfo() = default;
-
 WebAppInstallInfo::WebAppInstallInfo(const webapps::ManifestId& manifest_id)
     : manifest_id(manifest_id) {
   CHECK(manifest_id.is_valid());
