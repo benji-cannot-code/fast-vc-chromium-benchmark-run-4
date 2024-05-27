67bb2c105622751462a743e0238cdd83a872d825
Takuto Ikuta <tikuta@chromium.org>
2024-05-27 14:52:53 +0000

    tools/clang: remove goma references
    goma is not maintained anymore.
    
    Bug: 41489832
    Change-Id: I9e76317674ae7e63c0fbb935eb16af95c99ba36b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568922
    Reviewed-by: Hans Wennborg <hans@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306425}

--- tools/clang/scripts/remote_ld.py
+++ tools/clang/scripts/remote_ld.py
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Linker wrapper that performs distributed ThinLTO on Goma or Reclient.
+# Linker wrapper that performs distributed ThinLTO on Reclient.
 #
 # Usage: Pass the original link command as parameters to this script.
 # E.g. original: clang++ -o foo foo.o
-# Becomes: goma-ld clang++ -o foo foo.o
+# Becomes: remote_ld clang++ -o foo foo.o
 
 import os
 import re
