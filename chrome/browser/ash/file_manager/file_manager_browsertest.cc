01a12289d4be848cc8fb03ed5b5492bdf9418a0b
Aida Zolic <aidazolic@chromium.org>
2024-05-27 06:50:10 +0000

    [skyvault] Display error when no volumes
    Adds an error message to be displayed when there are no volumes, which
    can for example happen if SkyVault is misconfigured: local files are
    disabled but the cloud location isn't properly set (DownloadDirectory).
    
    Demo: http://screen/8sJPhPxuX8gaT9i
    
    R=lucmult@chromium.org
    
    Fixed: b/328031885
    Change-Id: Ibb9f855049f4cc5a3cd8b91ca2a84eafcbb9969d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5537890
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306285}

--- chrome/browser/ash/file_manager/file_manager_browsertest.cc
+++ chrome/browser/ash/file_manager/file_manager_browsertest.cc
@@ -395,6 +395,9 @@ WRAPPED_INSTANTIATE_TEST_SUITE_P(
             .EnableSkyVault(),
         TestCase("fileDisplayOneDrivePlaceholder")
             .DontMountVolumes()
+            .EnableSkyVault(),
+        TestCase("fileDisplayFileSystemDisabled")
+            .DontMountVolumes()
             .EnableSkyVault()));
 
 WRAPPED_INSTANTIATE_TEST_SUITE_P(
