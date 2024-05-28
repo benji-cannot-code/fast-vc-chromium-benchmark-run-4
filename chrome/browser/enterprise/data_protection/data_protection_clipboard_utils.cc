c2fc6a9c61a4a952bb96c609dcc2ff1ed32c5be7
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-05-28 15:43:07 +0000

    Clean up kEnableSourceInPasteReports flag
    This experiment is no longer necessary, the new behavior it gated is
    simple enough not to require a regular Finch rollout.
    
    Bug: b:325454406
    Change-Id: Ia60ec2db634b46faaeff5716f3f76d033813f858
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570979
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306785}

--- chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.cc
+++ chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.cc
@@ -189,10 +189,6 @@ bool SkipDataControlOrContentAnalysisChecks(
 std::string GetClipboardSourceString(
     const content::ClipboardEndpoint& source,
     const content::ClipboardEndpoint& destination) {
-  if (!base::FeatureList::IsEnabled(kEnableSourceInPasteReports)) {
-    return "";
-  }
-
   if (!source.browser_context()) {
     return "CLIPBOARD";
   }
@@ -505,10 +501,6 @@ void IsCopyRestrictedByDialog(
 }
 
 }  // namespace
-
-BASE_FEATURE(kEnableSourceInPasteReports,
-             "EnableSourceInPasteReports",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 
 void PasteIfAllowedByPolicy(
     const content::ClipboardEndpoint& source,
--- chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
+++ chrome/browser/enterprise/data_protection/data_protection_clipboard_utils.h
@@ -13,11 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_data_protection {
 
-// Controls attaching the source of a clipboard interaction to scanning requests
-// and to reported events.
-// TODO(b/325454406): Clean up this experiment when needed.
-BASE_DECLARE_FEATURE(kEnableSourceInPasteReports);
-
 // This function checks if a paste is allowed to proceed according to the
 // following policies:
 // - DataLeakPreventionRulesList
