1896865c2aa9acd095456825370f0a0d46d1bedb
Dave Tapuska <dtapuska@chromium.org>
2024-05-28 18:58:06 +0000

    [ios] Turn off gesture testing by default
    Clicks would not work so turn off gesture recognition in iOS and force
    blink to start text selection.
    
    Change-Id: I83c5cabad3d12347dc3e8ed188e7a4719f8f38cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577835
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306916}

--- content/browser/renderer_host/render_widget_host_view_ios_uiview.mm
+++ content/browser/renderer_host/render_widget_host_view_ios_uiview.mm
@@ -538,7 +538,7 @@ static void* kObservingContext = &kObservingContext;
 - (BOOL)textInteractionGesture:(BEGestureType)gestureType
             shouldBeginAtPoint:(CGPoint)point {
   // Check if point is really selectable here.
-  return YES;
+  return NO;
 }
 
 - (void)selectWordForReplacement {
