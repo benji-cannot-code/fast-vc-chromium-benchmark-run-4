8605dc940d33975e265c319e61ec0ce052d21ccd
Tzarial Kuznia <zork@google.com>
2024-05-28 19:11:37 +0000

    [a11y Remove prefs::kAccessibilityScreenMagnifierCenterFocus
    Change-Id: Idf252c5ad1412b9dbe093b67d66d7d761626d9a9
    Fixed: 331856387
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540495
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Dominic Battre <battre@chromium.org>
    Commit-Queue: Tzarial Kuznia <zork@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306931}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1086,6 +1086,10 @@ inline constexpr char kAccessibilityMouseKeysShortcutToPauseEnabled[] =
 // text fields.
 inline constexpr char kAccessibilityMouseKeysDisableInTextFields[] =
     "settings.a11y.mouse_keys.disable_in_text_fields";
+// A boolean pref which determines whether screen magnifier should center
+// the text input focus.
+inline constexpr char kAccessibilityScreenMagnifierCenterFocus[] =
+    "settings.a11y.screen_magnifier_center_focus";
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 // Deprecated 05/2024.
@@ -1576,6 +1580,7 @@ void RegisterProfilePrefsForMigration(
                                 true);
   registry->RegisterBooleanPref(kAccessibilityMouseKeysDisableInTextFields,
                                 true);
+  registry->RegisterBooleanPref(kAccessibilityScreenMagnifierCenterFocus, true);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   // Deprecated 05/2024.
@@ -2950,6 +2955,7 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   // Added 05/2024.
   profile_prefs->ClearPref(kAccessibilityMouseKeysShortcutToPauseEnabled);
   profile_prefs->ClearPref(kAccessibilityMouseKeysDisableInTextFields);
+  profile_prefs->ClearPref(kAccessibilityScreenMagnifierCenterFocus);
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
   // Added 05/2024.
