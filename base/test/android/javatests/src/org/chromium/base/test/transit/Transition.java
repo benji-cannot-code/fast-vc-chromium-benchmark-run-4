d77cb4be44dba91e76214ec16eecc709fadeadb1
Henrique Nakashima <hnakashima@chromium.org>
2024-05-28 20:07:58 +0000

    Make any Throwable into a TravelException in Transition
    Bug: 325517421
    Change-Id: I6503e0f1f6a3d4cc281bbecab3a4e4ba4ebd441b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577714
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306980}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Transition.java
@@ -9,7 +9,6 @@ import androidx.annotation.Nullable;
 
 import org.chromium.base.Log;
 import org.chromium.base.test.transit.ConditionWaiter.ConditionWait;
-import org.chromium.base.test.util.CriteriaNotSatisfiedException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -72,7 +71,7 @@ public abstract class Transition {
         mWaits = createWaits();
         try {
             ConditionWaiter.preCheck(mWaits, mOptions, mTrigger);
-        } catch (CriteriaNotSatisfiedException e) {
+        } catch (Throwable e) {
             throw newTransitionException(e);
         }
         for (ConditionWait wait : mWaits) {
@@ -113,9 +112,9 @@ public abstract class Transition {
             try {
                 mTrigger.triggerTransition();
                 Log.i(TAG, "%s: finished running trigger", toDebugString());
-            } catch (Exception e) {
+            } catch (Throwable e) {
                 throw TravelException.newTravelException(
-                        "Exception thrown by Transition trigger for " + toDebugString(), e);
+                        String.format("%s: trigger threw ", toDebugString()), e);
             }
         } else {
             Log.i(TAG, "%s is triggerless", toDebugString());
@@ -128,7 +127,7 @@ public abstract class Transition {
         // for flakiness due to tight timeouts.
         try {
             ConditionWaiter.waitFor(mWaits, mOptions);
-        } catch (AssertionError e) {
+        } catch (Throwable e) {
             throw newTransitionException(e);
         }
     }
