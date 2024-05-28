84be810a5d57f5a2df165d68af6ad08565d0c053
Rouslan Solomakhin <rouslan@chromium.org>
2024-05-28 16:51:45 +0000

    [CC Number Validation Target] Create ":autofill_regex" target (6/9)
    This patch moves the following files into the new build target
    ":autofill_regex":
    
      - autofill_regexes.h
      - autofill_regexes.cc
      - autofill_regex_constants.h
    
    The patch updates the build dependencies to point to ":autofill_regex"
    and cleans up the #include statements in the autofill_regexes.cc file.
    
    Design doc: https://bit.ly/cc-number-validation-target-dd
    
    Bug: b:281812289
    Change-Id: I7eb0e8287edf7e3b14a50272dfac9d83104926eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544699
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306828}

--- components/autofill/core/common/autofill_regexes.cc
+++ components/autofill/core/common/autofill_regexes.cc
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/autofill/core/common/autofill_regexes.h"
 
-#include <map>
-#include <memory>
-#include <string>
-#include <string_view>
-#include <utility>
+#include <tuple>
 
 #include "base/check.h"
 #include "base/i18n/unicodestring.h"
