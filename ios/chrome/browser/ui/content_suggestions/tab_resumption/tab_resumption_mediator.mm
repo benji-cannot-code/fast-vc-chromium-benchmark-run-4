5b33f79a5b4156589aa7419b4f07428dabbb207c
Chris Lu <thegreenfrog@chromium.org>
2024-05-28 20:36:50 +0000

    [ios, MagicStack] Move itemConfig nil to after delegate call
    The delegate uses the itemConfig to figure out which index to remove,
    so nilling must happen after the call.
    
    Fixed: 343251033
    Change-Id: I85f9d298614371d8b207d2dcd921d8ab0f61dbd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578374
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1307006}

--- ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_mediator.mm
@@ -293,8 +293,8 @@ NSString* kStartSurfaceSceneEnterIntoBackgroundTime =
 
 - (void)mostRecentTabWasRemoved:(web::WebState*)webState {
   if (self.itemConfig && self.itemConfig.itemType == kMostRecentTab) {
-    self.itemConfig = nil;
     [self.delegate removeTabResumptionModule];
+    self.itemConfig = nil;
   }
 }
 
