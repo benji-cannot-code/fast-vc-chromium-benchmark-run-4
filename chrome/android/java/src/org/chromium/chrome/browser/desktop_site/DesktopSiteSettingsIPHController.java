bf711f46a82e00c48f650b80cb39c48f4f66b95e
Shu Yang <shuyng@google.com>
2024-05-28 18:29:26 +0000

    Do not show Desktop Site Settings IPH on native pages
    e.g. https://screenshot.googleplex.com/5BRhTodR6eL2jf4.png
    
    Change-Id: Ia70912d2a14511f1b79f9c540f7c01f994aa14a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5572868
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1306897}

--- chrome/android/java/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIPHController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/desktop_site/DesktopSiteSettingsIPHController.java
@@ -117,6 +117,7 @@ public class DesktopSiteSettingsIPHController {
 
     @VisibleForTesting
     void showGenericIPH(@NonNull Tab tab, Profile profile) {
+        if (tab.isNativePage()) return;
         Tracker tracker = TrackerFactory.getTrackerForProfile(profile);
         String featureName = FeatureConstants.REQUEST_DESKTOP_SITE_EXCEPTIONS_GENERIC_FEATURE;
         if (perSiteIPHPreChecksFailed(tab, tracker, featureName)) return;
@@ -167,6 +168,7 @@ public class DesktopSiteSettingsIPHController {
     @VisibleForTesting
     boolean showWindowSettingIPH(@NonNull Tab tab, Profile profile) {
         if (mMessageDispatcher == null) return false;
+        if (tab.isNativePage()) return false;
 
         // Return early when the IPH triggering criteria is not satisfied.
         Tracker tracker = TrackerFactory.getTrackerForProfile(profile);
