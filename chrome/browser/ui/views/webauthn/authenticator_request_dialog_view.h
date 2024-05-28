98c7b355faaa32ae45c0c1be24132de566d7500b
Adam Langley <agl@chromium.org>
2024-05-28 21:43:37 +0000

    webauthn: drop dialog close callback
    See referenced bug for why we want to do this. The remaining question is
    whether this will have negative effects.
    
    We need to consider cases where the dialog will be closed on us because,
    now, the `Cancel` method won't be called. Firstly, calling `Cancel`
    wasn't correct because, for several sheets, that doesn't cancel the
    request. So the current code isn't good in any case.
    
    But the dialog may be closed on us because the tab is closed, but that
    already destroys the AuthenticatorImpl and, thus, all the other objects
    that are part of a request. I think that covers the cases?
    
    Fixed: 342469257
    Change-Id: Ia7af0978916a615dd4c879f649ca55a698cd39dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5567116
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307057}

--- chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.h
+++ chrome/browser/ui/views/webauthn/authenticator_request_dialog_view.h
@@ -105,8 +105,6 @@ class AuthenticatorRequestDialogView
   void ForgotGPMPinPressed();
   void GPMPinOptionChosen(bool is_arbitrary);
 
-  void OnDialogClosing();
-
   raw_ptr<AuthenticatorRequestDialogModel> model_;
 
   raw_ptr<AuthenticatorRequestSheetView, DanglingUntriaged> sheet_ = nullptr;
