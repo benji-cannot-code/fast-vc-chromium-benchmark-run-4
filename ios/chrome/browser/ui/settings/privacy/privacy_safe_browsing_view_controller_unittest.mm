97417d54d0f01c400ee74f094e6f6332331b6fe3
Joemer Ramos <joemerramos@chromium.org>
2024-05-28 15:36:33 +0000

    [iOS] Adjust Safe Browsing Test To Account For HPRT
    Originally, testing didn't account for a string change in standard
    protection. This CL refactors code to address for when HPRT (Hash Prefix
    Real Time) is enabled and chrome branding is enabled.
    
    Fixed: 339424308
    Change-Id: I330898adf5bb526296df4bbef9cec8998b1efa29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5554145
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1306774}

--- ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_view_controller_unittest.mm
@@ -5,8 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_view_controller.h"
 
+#import "build/branding_buildflags.h"
 #import "components/prefs/testing_pref_service.h"
 #import "components/safe_browsing/core/common/features.h"
+#import "components/safe_browsing/core/common/hashprefix_realtime/hash_realtime_utils.h"
 #import "components/safe_browsing/core/common/safe_browsing_prefs.h"
 #import "components/sync_preferences/testing_pref_service_syncable.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
@@ -14,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_controller_test.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
 #import "ios/chrome/browser/ui/settings/privacy/privacy_safe_browsing_mediator.h"
+#import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -53,11 +56,6 @@ class PrivacySafeBrowsingViewControllerTest
 };
 
 TEST_F(PrivacySafeBrowsingViewControllerTest, TestModel) {
-  if (@available(iOS 16, *)) {
-    // TODO:(crbug.com/339424308): Failing on ios 16.7.1 and 16.7.2.
-    return;
-  }
-
   CreateController();
   CheckController();
 
@@ -70,12 +68,25 @@ TEST_F(PrivacySafeBrowsingViewControllerTest, TestModel) {
       l10n_util::GetNSString(
           IDS_IOS_PRIVACY_SAFE_BROWSING_ENHANCED_PROTECTION_FRIENDLIER_SUMMARY),
       0, 0);
+
+  NSInteger standard_protection_summary = 0;
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if (safe_browsing::hash_realtime_utils::
+          IsHashRealTimeLookupEligibleInSession()) {
+    standard_protection_summary =
+        IDS_IOS_PRIVACY_SAFE_BROWSING_STANDARD_PROTECTION_FRIENDLIER_SUMMARY_PROXY;
+  }
+#endif
+
+  if (!standard_protection_summary) {
+    standard_protection_summary =
+        IDS_IOS_PRIVACY_SAFE_BROWSING_STANDARD_PROTECTION_FRIENDLIER_SUMMARY;
+  }
+
   CheckTextCellTextAndDetailText(
       l10n_util::GetNSString(
           IDS_IOS_PRIVACY_SAFE_BROWSING_STANDARD_PROTECTION_TITLE),
-      l10n_util::GetNSString(
-          IDS_IOS_PRIVACY_SAFE_BROWSING_STANDARD_PROTECTION_FRIENDLIER_SUMMARY),
-      0, 1);
+      l10n_util::GetNSString(standard_protection_summary), 0, 1);
   CheckTextCellTextAndDetailText(
       l10n_util::GetNSString(IDS_IOS_PRIVACY_SAFE_BROWSING_NO_PROTECTION_TITLE),
       l10n_util::GetNSString(
