fb57437250393d6d2717b5a96c1b3882b54cdd8c
John Delaney <johnidel@chromium.org>
2024-05-28 18:54:57 +0000

    Refactor ClearData() between AttributionResolver and storage
    Replaces the pure pass-through methods with two primitives on storage
    for clearing all data, and clearing data with a filter.
    
    Change-Id: I1cbad555aea6ed8ce0a8b2d6961443d3300e87a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577257
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306914}

--- content/browser/attribution_reporting/attribution_storage_sql.cc
+++ content/browser/attribution_reporting/attribution_storage_sql.cc
@@ -1899,7 +1899,7 @@ std::optional<base::Time> AttributionStorageSql::AdjustOfflineReportTimes() {
   return GetNextReportTime(base::Time::Min());
 }
 
-void AttributionStorageSql::ClearData(
+void AttributionStorageSql::ClearDataWithFilter(
     base::Time delete_begin,
     base::Time delete_end,
     StoragePartition::StorageKeyMatcherFunction filter,
@@ -1908,17 +1908,6 @@ void AttributionStorageSql::ClearData(
   if (!LazyInit(DbCreationPolicy::kIgnoreIfAbsent)) {
     return;
   }
-
-  SCOPED_UMA_HISTOGRAM_TIMER("Conversions.ClearDataTime");
-  if (filter.is_null() && (delete_begin.is_null() || delete_begin.is_min()) &&
-      delete_end.is_max()) {
-    ClearAllDataAllTime(delete_rate_limit_data);
-    return;
-  }
-
-  // Measure the time it takes to perform a clear with a filter separately from
-  // the above histogram.
-  SCOPED_UMA_HISTOGRAM_TIMER("Conversions.Storage.ClearDataWithFilterDuration");
 
   // Delete the data in a transaction to avoid cases where the source part
   // of a report is deleted without deleting the associated report, or
@@ -1988,6 +1977,11 @@ void AttributionStorageSql::ClearData(
 }
 
 void AttributionStorageSql::ClearAllDataAllTime(bool delete_rate_limit_data) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (!LazyInit(DbCreationPolicy::kIgnoreIfAbsent)) {
+    return;
+  }
+
   sql::Transaction transaction(&db_);
   if (!transaction.Begin()) {
     return;
@@ -3320,14 +3314,6 @@ AttributionStorageSql::GetAllDataKeys() {
   return keys;
 }
 
-void AttributionStorageSql::DeleteByDataKey(
-    const AttributionDataModel::DataKey& key) {
-  ClearData(base::Time::Min(), base::Time::Max(),
-            base::BindRepeating(
-                std::equal_to<blink::StorageKey>(),
-                blink::StorageKey::CreateFirstParty(key.reporting_origin())),
-            /*delete_rate_limit_data=*/true);
-}
 
 void AttributionStorageSql::SetDelegate(AttributionResolverDelegate* delegate) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
