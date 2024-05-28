ca4de3fb4e964664c964b668c88c97f1ce642dd6
Alexis Hetu <sugoi@chromium.org>
2024-05-28 16:11:07 +0000

    [iOS] Use new GPay symbol
    Replace GPay icon with new GPay symbol where possible.
    
    The new symbol was added here:
    https://chrome-internal-review.googlesource.com/c/chrome/ios_internal/+/7325347
    
    Bug: 40947290
    Change-Id: I60e3e8221452b6595007803fb21fb0bf5645251e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570507
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1306800}

--- ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/ui/autofill/bottom_sheet/virtual_card_enrollment_bottom_sheet_view_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/core/browser/payments/payments_service_url.h"
 #import "components/grit/components_scaled_resources.h"
 #import "ios/chrome/browser/net/model/crurl.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_detail_icon_item.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -173,11 +174,9 @@ CGFloat const kCreditCardCellHeight = 64;
 // Returns the google pay badge image corresponding to the current
 // UIUserInterfaceStyle (light/dark mode).
 - (UIImage*)googlePayBadgeImage {
-  // IDR_AUTOFILL_GOOGLE_PAY_DARK only exists in official builds.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark
-             ? NativeImage(IDR_AUTOFILL_GOOGLE_PAY_DARK)
-             : NativeImage(IDR_AUTOFILL_GOOGLE_PAY);
+  return MakeSymbolMulticolor(CustomSymbolWithPointSize(
+      kGooglePaySymbol, kCreditCardCellHeight - 2 * kLogoPadding));
 #else
   return NativeImage(IDR_AUTOFILL_GOOGLE_PAY);
 #endif
--- ios/chrome/browser/ui/autofill/cells/target_account_item.mm
+++ ios/chrome/browser/ui/autofill/cells/target_account_item.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/apple/foundation_util.h"
 #import "build/branding_buildflags.h"
 #import "components/grit/components_scaled_resources.h"
+#import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/table_view/cells/table_view_cell.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -73,13 +74,11 @@ const CGFloat kAccountCellSpacing = 7;
   UIImageView* googlePayBadge = [[UIImageView alloc] init];
   googlePayBadge.translatesAutoresizingMaskIntoConstraints = NO;
   googlePayBadge.contentMode = UIViewContentModeScaleAspectFit;
-  googlePayBadge.image = NativeImage(IDR_AUTOFILL_GOOGLE_PAY);
-// IDR_AUTOFILL_GOOGLE_PAY_DARK only exists in official builds.
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  if (UITraitCollection.currentTraitCollection.userInterfaceStyle ==
-      UIUserInterfaceStyleDark) {
-    googlePayBadge.image = NativeImage(IDR_AUTOFILL_GOOGLE_PAY_DARK);
-  }
+  googlePayBadge.image = MakeSymbolMulticolor(
+      CustomSymbolWithPointSize(kGooglePaySymbol, kAccountCellBadgeSize));
+#else
+  googlePayBadge.image = NativeImage(IDR_AUTOFILL_GOOGLE_PAY);
 #endif
   [self.contentView addSubview:googlePayBadge];
 
