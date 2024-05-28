bc506302055abd65e12510be61945e3cf2e57683
Ken Buchanan <kenrb@chromium.org>
2024-05-28 23:41:56 +0000

    [WebAuthn] Unregister device when deferred UV key submission fails
    This identifies when an attempt to submit a UV key after registration
    fails, and causes the device to unregister itself in that situation.
    
    Bug: 40274370
    Change-Id: Ie1d4ff03d652ff345ad8ba22fad49e80a7fbf7ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570160
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307126}

--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -1151,6 +1151,9 @@ void GPMEnclaveController::StartEnclaveTransaction(
       request->user_verified = true;
       request->uv_key_creation_callback =
           enclave_manager_->UserVerifyingKeyCreationCallback();
+      request->unregister_callback =
+          base::BindOnce(&EnclaveManager::Unenroll,
+                         enclave_manager_->GetWeakPtr(), base::DoNothing());
       break;
     case EnclaveUserVerificationMethod::kUnsatisfiable:
       NOTREACHED_NORETURN();
--- device/fido/enclave/enclave_protocol_utils_unittest.cc
+++ device/fido/enclave/enclave_protocol_utils_unittest.cc
@@ -419,7 +419,8 @@ TEST_F(EnclaveProtocolUtilsTest, ParseGetAssertionResponse_Failures) {
     std::vector<uint8_t> cred_id = {0, 1, 2};
     auto parse_result =
         ParseGetAssertionResponse(std::move(response_cbor), cred_id);
-    EXPECT_TRUE(absl::holds_alternative<std::string>(parse_result))
+    EXPECT_TRUE(absl::holds_alternative<ErrorResponse>(parse_result) &&
+                absl::get<ErrorResponse>(parse_result).error_string.has_value())
         << "Failed GetAssertion response parsing for: " << test_case.name;
   }
 }
@@ -484,7 +485,8 @@ TEST_F(EnclaveProtocolUtilsTest, ParseMakeCredentialResponse_StringFailures) {
     auto parse_result = ParseMakeCredentialResponse(
         std::move(response_cbor), ctap_request, kWrappedSecretVersion,
         /*user_verified=*/false);
-    EXPECT_TRUE(absl::holds_alternative<std::string>(parse_result))
+    EXPECT_TRUE(absl::holds_alternative<ErrorResponse>(parse_result) &&
+                absl::get<ErrorResponse>(parse_result).error_string.has_value())
         << "Failed MakeCredential response parsing for: " << test_case.name;
   }
 }
@@ -497,8 +499,9 @@ TEST_F(EnclaveProtocolUtilsTest, ParseGetAssertionResponse_IntegerFailure) {
   auto parse_result =
       ParseGetAssertionResponse(std::move(response_cbor), cred_id);
 
-  EXPECT_TRUE(absl::holds_alternative<int>(parse_result));
-  EXPECT_EQ(absl::get<int>(parse_result), 2);
+  EXPECT_TRUE(absl::holds_alternative<ErrorResponse>(parse_result));
+  EXPECT_TRUE(absl::get<ErrorResponse>(parse_result).error_code.has_value());
+  EXPECT_EQ(*absl::get<ErrorResponse>(parse_result).error_code, 2);
 }
 
 TEST_F(EnclaveProtocolUtilsTest, ParseMakeCredentialResponse_IntegerFailure) {
@@ -515,8 +518,9 @@ TEST_F(EnclaveProtocolUtilsTest, ParseMakeCredentialResponse_IntegerFailure) {
       std::move(response_cbor), ctap_request, kWrappedSecretVersion,
       /*user_verified=*/false);
 
-  EXPECT_TRUE(absl::holds_alternative<int>(parse_result));
-  EXPECT_EQ(absl::get<int>(parse_result), 2);
+  EXPECT_TRUE(absl::holds_alternative<ErrorResponse>(parse_result));
+  EXPECT_TRUE(absl::get<ErrorResponse>(parse_result).error_code.has_value());
+  EXPECT_EQ(*absl::get<ErrorResponse>(parse_result).error_code, 2);
 }
 
 }  // namespace enclave
