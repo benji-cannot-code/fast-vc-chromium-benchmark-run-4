ed4191eee4411afa8cfa6316a9892a814d922c8f
James Cook <jamescook@chromium.org>
2024-05-28 20:24:30 +0000

    birch: Add browser test coverage for calendar chip
    Verify the chip appears and that clicking on it loads the appropriate
    URL.
    
    Bug: b:334939776
    Test: browser_tests --gtest_filter=BirchBrowserTest.*
    Change-Id: I88afdc54daf556444fde8f08d83f8e354a5f403b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564228
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306995}

--- chrome/browser/ui/ash/birch/birch_keyed_service.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service.cc
@@ -55,6 +55,9 @@ void BirchKeyedService::OnShellDestroying() {
 }
 
 BirchDataProvider* BirchKeyedService::GetCalendarProvider() {
+  if (calendar_provider_for_test_) {
+    return calendar_provider_for_test_;
+  }
   return calendar_provider_.get();
 }
 
