a8dc2cbb00a9e7cdab36f7bbd4ee2ac8c685dce4
S Ganesh <ganesh@chromium.org>
2024-05-28 21:49:16 +0000

    Revert "Add unittests for AuthPanel"
    This reverts commit e87d394e8636dc53ad2ea563eb2effecccdb894d.
    
    Reason for revert: Step "ash_unittests (retry shards) on Ubuntu-22.04" failing on builder "chromium/ci/Linux Chromium OS ASan LSan Tests (1)"
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/58283/overview
    
    Original change's description:
    > Add unittests for AuthPanel
    >
    > Bug: b:271248452
    > Change-Id: I5ae86f5a6c0db162b21af16702b4dac635677c6e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5526147
    > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Reviewed-by: Renato Silva <rrsilva@google.com>
    > Commit-Queue: Elie Maamari <emaamari@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1306892}
    
    Bug: b:271248452
    Change-Id: Ice7007946101583175a3f2b0a5e0f6228abf447e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578705
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Owners-Override: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307059}

--- ash/in_session_auth/in_session_auth_dialog_controller_impl.cc
+++ ash/in_session_auth/in_session_auth_dialog_controller_impl.cc
@@ -182,7 +182,7 @@ void InSessionAuthDialogControllerImpl::OnUserAuthAttemptConfirmed(
       base::BindRepeating(
           &InSessionAuthDialogControllerImpl::OnAuthPanelPreferredSizeChanged,
           weak_factory_.GetWeakPtr()),
-      connector, AuthHub::Get());
+      connector);
 
   out_consumer = contents_view->GetAuthPanel();
   dialog_ = CreateAuthDialogWidget(std::move(contents_view));
