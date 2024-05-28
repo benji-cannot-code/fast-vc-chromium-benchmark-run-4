7cacbbbf827c97d11721f35c2660ba529330d5e9
Howard Yang <hcyang@google.com>
2024-05-28 03:47:04 +0000

    Show WebAuthn dialog for lacros passkey auth
    Make the InSessionAuth::RequestLegacyWebauthn handler summon the
    WebAuthn dialog when receiving lacros's request.
    
    Bug: b:342097802
    Test: CQ
    Test: deploy onto DUT, go to webauthn.io and verify that GPM passkey
    auth works.
    
    Change-Id: Ie97d11e2b3a1482e25a6ad7b05360b2e7621eee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5558224
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Howard Yang <hcyang@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306586}

--- ash/in_session_auth/in_session_auth_dialog_controller_impl.cc
+++ ash/in_session_auth/in_session_auth_dialog_controller_impl.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/in_session_auth/in_session_auth_dialog_contents_view.h"
 #include "ash/public/cpp/in_session_auth_dialog_controller.h"
 #include "ash/public/cpp/in_session_auth_token_provider.h"
+#include "ash/public/cpp/webauthn_dialog_controller.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
 #include "base/check.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/osauth/public/auth_factor_status_consumer.h"
 #include "chromeos/ash/components/osauth/public/auth_hub.h"
 #include "chromeos/ash/components/osauth/public/common_types.h"
+#include "chromeos/components/webauthn/webauthn_request_registrar.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget_delegate.h"
@@ -141,6 +143,22 @@ void InSessionAuthDialogControllerImpl::ShowAuthDialog(
          account_id))
         ->Show();
   }
+}
+
+void InSessionAuthDialogControllerImpl::ShowLegacyWebAuthnDialog(
+    const std::string& rp_id,
+    const std::string& window_id,
+    WebAuthNDialogController::FinishCallback on_auth_complete) {
+  aura::Window* source_window =
+      chromeos::webauthn::WebAuthnRequestRegistrar::Get()
+          ->GetWindowForRequestId(window_id);
+  if (!source_window) {
+    LOG(ERROR) << "Can't find the window for the given window id.";
+    std::move(on_auth_complete).Run(false);
+    return;
+  }
+  WebAuthNDialogController::Get()->ShowAuthenticationDialog(
+      source_window, rp_id, std::move(on_auth_complete));
 }
 
 void InSessionAuthDialogControllerImpl::SetTokenProvider(
