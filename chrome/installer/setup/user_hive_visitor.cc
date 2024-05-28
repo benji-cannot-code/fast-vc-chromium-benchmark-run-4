a354a6605fb1477164f207d7d9d931dce7bccc3c
Claudio DeSouza <cdesouza@chromium.org>
2024-05-28 02:05:33 +0000

    Replace StringPiece with std::string_view in //chrome/installer
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //chrome/installer
    
    Bug: 40506050
    Change-Id: I0875caacf482d8414be2d49252a955d276c02ae4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555566
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306569}

--- chrome/installer/setup/user_hive_visitor.cc
+++ chrome/installer/setup/user_hive_visitor.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/logging.h"
 #include "base/rand_util.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "chrome/installer/util/scoped_token_privilege.h"
