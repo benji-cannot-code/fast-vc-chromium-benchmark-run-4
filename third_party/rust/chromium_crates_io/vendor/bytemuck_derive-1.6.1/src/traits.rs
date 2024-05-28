3d6b6fd7471cbca52c8b9d6b11e16487b9dcb254
Lukasz Anforowicz <lukasza@chromium.org>
2024-05-28 17:44:54 +0000

    Roll bytemuck_derive: 1.6.0 => 1.6.1 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * bytemuck_derive: 1.6.0 => 1.6.1
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * bytemuck_derive@1.6.1: crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: If2853199b6946838745a27b85abb764489e6a3d2
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5575820
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306863}

--- third_party/rust/chromium_crates_io/vendor/bytemuck_derive-1.6.1/src/traits.rs
+++ third_party/rust/chromium_crates_io/vendor/bytemuck_derive-1.6.1/src/traits.rs
@@ -318,8 +318,10 @@ impl Derivable for TransparentWrapper {
       Ok(quote!(
         const _: () = {
           #[repr(transparent)]
+          #[allow(clippy::multiple_bound_locations)]
           struct AssertWrappedIsWrapped #impl_generics((u8, ::core::marker::PhantomData<#wrapped_field_ty>), #(#nonwrapped_field_tys),*) #where_clause;
           fn assert_zeroable<Z: #crate_name::Zeroable>() {}
+          #[allow(clippy::multiple_bound_locations)]
           fn check #impl_generics () #where_clause {
             #(
               assert_zeroable::<#nonwrapped_field_tys>();
@@ -486,6 +488,7 @@ fn generate_checked_bit_pattern_struct(
 
   Ok((
     quote! {
+        #[doc = #GENERATED_TYPE_DOCUMENTATION]
         #repr
         #[derive(Clone, Copy, #crate_name::AnyBitPattern)]
         #derive_dbg
@@ -653,6 +656,7 @@ fn generate_checked_bit_pattern_enum_with_fields(
 
       Ok((
         quote! {
+          #[doc = #GENERATED_TYPE_DOCUMENTATION]
           #[derive(::core::clone::Clone, ::core::marker::Copy, #crate_name::AnyBitPattern)]
           #derive_dbg
           #bits_repr
@@ -704,6 +708,7 @@ fn generate_checked_bit_pattern_enum_with_fields(
 
       Ok((
         quote! {
+          #[doc = #GENERATED_TYPE_DOCUMENTATION]
           #[derive(::core::clone::Clone, ::core::marker::Copy, #crate_name::CheckedBitPattern)]
           #[repr(C)]
           #vis struct #bits_ty(#(#fields),*);
@@ -776,6 +781,7 @@ fn generate_checked_bit_pattern_enum_with_fields(
 
       Ok((
         quote! {
+          #[doc = #GENERATED_TYPE_DOCUMENTATION]
           #[derive(::core::clone::Clone, ::core::marker::Copy, #crate_name::AnyBitPattern)]
           #bits_repr
           #[allow(non_snake_case)]
@@ -1264,3 +1270,6 @@ pub fn bytemuck_crate_name(input: &DeriveInput) -> TokenStream {
 
   return crate_name;
 }
+
+const GENERATED_TYPE_DOCUMENTATION: &str =
+  " `bytemuck`-generated type for internal purposes only.";
