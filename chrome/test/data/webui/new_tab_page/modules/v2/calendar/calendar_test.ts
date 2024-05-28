b3498b0e5e8fe14722473946908ded36e5cb320a
Riley Tatum <rtatum@google.com>
2024-05-28 17:48:48 +0000

    [NTP][Enterprise] Show start time next to event titles
    Adds start time to fake data, offsets it since JS uses UNIX epoch
    for timestamps, and formats the time into a string matching designs.
    
    I also used locale based formatting for the time, since a lot of the
    world doesn't use am/pm timestamps like en-us. This way JS will
    determine if it should be there based on the user's set locale.
    
    screenshot: http://screenshot/98VqqquRQ4G3LBU
    
    Bug: 342477200
    Change-Id: Ib9178a49002a9add93ba2910d54aef60a457ce8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5567311
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306868}

--- chrome/test/data/webui/new_tab_page/modules/v2/calendar/calendar_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/calendar/calendar_test.ts
@@ -4,12 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {CalendarElement} from 'chrome://new-tab-page/lazy_load.js';
-import {assertEquals} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {waitAfterNextRender} from 'chrome://webui-test/polymer_test_util.js';
+import {isVisible} from 'chrome://webui-test/test_util.js';
 
 import {createEvents} from './test_support.js';
 
-suite('NewTabPageModulesGoogleCalendarModuleTest', () => {
+suite('NewTabPageModulesCalendarTest', () => {
   let element: CalendarElement;
 
   setup(async () => {
@@ -24,10 +25,11 @@ suite('NewTabPageModulesGoogleCalendarModuleTest', () => {
     await waitAfterNextRender(element);
 
     // Assert.
-    const eventElements = element.shadowRoot!.querySelectorAll('p');
+    const eventElements =
+        element.shadowRoot!.querySelectorAll('ntp-calendar-event');
     assertEquals(eventElements.length, numEvents);
-    for (let i = 0; i < numEvents; i++) {
-      assertEquals(eventElements[i]!.innerHTML, `Test Event ${i}`);
-    }
+    eventElements.forEach((element) => {
+      assertTrue(isVisible(element));
+    });
   });
 });
