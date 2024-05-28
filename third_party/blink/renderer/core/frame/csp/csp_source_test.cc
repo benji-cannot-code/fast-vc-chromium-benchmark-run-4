4b7fd52040783e89e4f9971dd9438d0e84569c7d
Hayato Ito <hayato@chromium.org>
2024-05-28 01:25:48 +0000

    Reland "[URL] Preserve Blink's CSP behavior for non-special URLs"
    This is a reland of commit 5de3a8d8986b27ec4278e4e6c949f909c8b2ae9b
    
    The difference between the reverted CL and this CL is
    https://chromium-review.googlesource.com/c/chromium/src/+/5569881/1..8.
    
    The problem was that the url.Host() object doesn't outlive
    HostForCSP() function.
    
    Original change's description:
    > [URL] Preserve Blink's CSP behavior for non-special URLs
    >
    > This CL is similar to https://crrev.com/c/5528336, but for Blink's
    > CSP implementation to ensure the behavior remains consistent
    > regardless of non-special URL support.
    >
    > The following tests now pass:
    >
    > - CSPSourceTest.MatchingAsSelf
    > - CSPSourceTest.SchemeIsEmpty
    >
    > Bug: 40063064
    > Change-Id: I80d7e64a627ffc9dbb7616027467ea1c6616b983
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561804
    > Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    > Commit-Queue: Hayato Ito <hayato@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1304889}
    
    Bug: 40063064
    Change-Id: Ic6323eac8a609ae35d96deb562def502eeb103fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569881
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306562}

--- third_party/blink/renderer/core/frame/csp/csp_source_test.cc
+++ third_party/blink/renderer/core/frame/csp/csp_source_test.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/frame/csp/csp_source.h"
 
+#include "base/test/with_feature_override.h"
 #include "services/network/public/mojom/content_security_policy.mojom-blink.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/core/dom/document.h"
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/loader/fetch/resource_request.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/weborigin/security_origin.h"
+#include "url/url_features.h"
 
 namespace blink {
 
@@ -149,7 +151,17 @@ TEST(CSPSourceTest, InsecureHostSchemeMatchesSecureScheme) {
                                 KURL(base, "https://not-example.com:8000/")));
 }
 
-TEST(CSPSourceTest, SchemeIsEmpty) {
+class CSPSourceParamTest : public base::test::WithFeatureOverride,
+                           public ::testing::Test {
+ public:
+  CSPSourceParamTest()
+      : WithFeatureOverride(url::kStandardCompliantNonSpecialSchemeURLParsing) {
+  }
+};
+
+INSTANTIATE_FEATURE_OVERRIDE_TEST_SUITE(CSPSourceParamTest);
+
+TEST_P(CSPSourceParamTest, SchemeIsEmpty) {
   KURL base;
 
   // Self scheme is http.
@@ -325,7 +337,7 @@ TEST(CSPSourceTest, HostMatches) {
   }
 }
 
-TEST(CSPSourceTest, MatchingAsSelf) {
+TEST_P(CSPSourceParamTest, MatchingAsSelf) {
   // Testing Step 4 of
   // https://w3c.github.io/webappsec-csp/#match-url-to-source-expression
   struct Source {
