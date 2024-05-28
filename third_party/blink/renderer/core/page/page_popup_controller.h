0eab5e9b1c47090d0b6f75d8bbcf4e4ba845b318
lauren n. liberda <lauren@selfisekai.rocks>
2024-05-28 17:54:26 +0000

    iwyu: missing <optional> in page_popup_controller.h
    Bug: 41455655
    Change-Id: I6483b9ec9d3c4acc08fb2bce8cac409c32a06dc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569043
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1306871}

--- third_party/blink/renderer/core/page/page_popup_controller.h
+++ third_party/blink/renderer/core/page/page_popup_controller.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_PAGE_POPUP_CONTROLLER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAGE_PAGE_POPUP_CONTROLLER_H_
 
+#include <optional>
+
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
