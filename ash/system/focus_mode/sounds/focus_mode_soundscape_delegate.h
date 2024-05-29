77c508cae7369a6a3764734d2c3050b0f9bf421d
Sean Kau <skau@chromium.org>
2024-05-29 19:07:16 +0000

    Implement GetTrack for FocusModeSoundcapeDelegate
    Delegate will return a random track from the playlist identified by
    playlist_id and will always play the last track last (to prevent an
    immediate repeat).
    
    Bug: b:342686170
    Change-Id: I8ba6a64a9d0d851425a2445af21ceb76eb2984ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570632
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307617}

--- ash/system/focus_mode/sounds/focus_mode_soundscape_delegate.h
+++ ash/system/focus_mode/sounds/focus_mode_soundscape_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/system/focus_mode/sounds/focus_mode_sounds_delegate.h"
+#include "ash/system/focus_mode/sounds/soundscape/playlist_tracker.h"
 #include "ash/system/focus_mode/sounds/soundscape/soundscape_types.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
@@ -21,7 +22,11 @@ class SoundscapesDownloader;
 
 class ASH_EXPORT FocusModeSoundscapeDelegate : public FocusModeSoundsDelegate {
  public:
-  FocusModeSoundscapeDelegate(const std::string& locale);
+  static std::unique_ptr<FocusModeSoundscapeDelegate> Create(
+      const std::string& locale);
+
+  FocusModeSoundscapeDelegate(
+      std::unique_ptr<SoundscapesDownloader> downloader);
   ~FocusModeSoundscapeDelegate() override;
 
   // FocusModeSoundsDelegate:
@@ -36,6 +41,9 @@ class ASH_EXPORT FocusModeSoundscapeDelegate : public FocusModeSoundsDelegate {
       std::optional<SoundscapeConfiguration> configuration);
 
   base::Time last_update_;
+
+  std::optional<PlaylistTracker> playlist_tracker_;
+
   std::optional<SoundscapeConfiguration> cached_configuration_;
   std::unique_ptr<SoundscapesDownloader> downloader_;
 
