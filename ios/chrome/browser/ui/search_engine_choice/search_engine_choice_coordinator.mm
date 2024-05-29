dbb4a79c8656775e82b59acb780500cbe07d0544
Jérôme Lebel <jlebel@chromium.org>
2024-05-29 14:27:44 +0000

    [iOS][Waffle] Skips the search engine if needed
    When creating the FRE, the list of screen is set. If the enterprise
    policies are loaded after this list is set, the search engine screen
    will be present while it should not.
    
    This patch adds code to skip the search engine screen just before
    being presented.
    
    This change modifies also the regular search engine dialog, but this
    should not change any behaviour, since we check if the search engine
    dialog should appear, just before it appears.
    
    EGTests is added in crrev.com/c/5577200.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL See crrev.com/c/5577200
    Bug: b/328040518
    Change-Id: I2980207bd488228e50020f17f93ee4b5baaf30fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578357
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307404}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_coordinator.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_coordinator.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/search_engines/search_engines_switches.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
+#import "ios/chrome/browser/search_engine_choice/model/search_engine_choice_util.h"
 #import "ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.h"
 #import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
@@ -83,6 +84,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Make sure we use the original browser state (non-incognito).
   ChromeBrowserState* originalBrowserState =
       self.browser->GetBrowserState()->GetOriginalChromeBrowserState();
+  const search_engines::ChoicePromo choicePromo =
+      _firstRun ? search_engines::ChoicePromo::kFre
+                : search_engines::ChoicePromo::kDialog;
+  if (!ShouldDisplaySearchEngineChoiceScreen(
+          *originalBrowserState, choicePromo,
+          /*app_started_via_external_intent=*/false)) {
+    // If the search engine enterprise pocliy has been loaded, just before to
+    // open the Search Engine Choice dialog, it should be skipped.
+    [self dismissChoiceScreen];
+    return;
+  }
   _viewController =
       [[SearchEngineChoiceViewController alloc] initWithFirstRunMode:_firstRun];
   _viewController.actionDelegate = self;
