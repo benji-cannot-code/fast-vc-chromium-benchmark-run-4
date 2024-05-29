2d49658177fb06c2f1c8dd75d3cd51f5375442a4
lauren n. liberda <lauren@selfisekai.rocks>
2024-05-29 18:12:37 +0000

    iwyu/base: missing stdint.h include in named_trigger
    Bug: 40565911
    Change-Id: I631e7752d7af7bc2234825f098ba21a0e0047b9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490719
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307571}

--- base/trace_event/named_trigger.h
+++ base/trace_event/named_trigger.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_TRACE_EVENT_NAMED_TRIGGER_H_
 #define BASE_TRACE_EVENT_NAMED_TRIGGER_H_
 
+#include <stdint.h>
+
 #include <optional>
 #include <string>
 
