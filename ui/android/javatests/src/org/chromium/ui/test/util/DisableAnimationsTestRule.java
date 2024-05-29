ccab79ea7d4642f963122f3ce4a34f6c963f6e70
Henrique Nakashima <hnakashima@chromium.org>
2024-05-29 18:08:06 +0000

    [Android] Close ParcelFileDescriptor in DisableAnimationsTestRule
    Fixes errors showing in logcat on Android S+:
    05-28 18:07:03.201 16455 16466 D StrictMode: StrictMode policy violation: android.os.strictmode.LeakedClosableViolation: A resource was acquired at attached stack trace but never released. See java.io.Closeable for information on avoiding resource leaks.
    
    Bug: None
    Change-Id: I545092ccc345131cb3881fcb10522489a6140e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578260
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307563}

--- ui/android/javatests/src/org/chromium/ui/test/util/DisableAnimationsTestRule.java
+++ ui/android/javatests/src/org/chromium/ui/test/util/DisableAnimationsTestRule.java
@@ -7,6 +7,7 @@ package org.chromium.ui.test.util;
 
 import android.os.Build;
 import android.os.IBinder;
+import android.os.ParcelFileDescriptor;
 import android.provider.Settings;
 
 import androidx.test.InstrumentationRegistry;
@@ -120,9 +121,12 @@ public class DisableAnimationsTestRule implements TestRule {
                             "settings put global transition_animation_scale " + scaleFactor,
                             "settings put global window_animation_scale " + scaleFactor);
             for (String command : commandToRuns) {
-                InstrumentationRegistry.getInstrumentation()
-                        .getUiAutomation()
-                        .executeShellCommand(command);
+                try (ParcelFileDescriptor parcelFileDescriptor =
+                        InstrumentationRegistry.getInstrumentation()
+                                .getUiAutomation()
+                                .executeShellCommand(command)) {
+                    parcelFileDescriptor.checkError();
+                }
             }
         } else {
             // Set animation scales through reflection in R-.
