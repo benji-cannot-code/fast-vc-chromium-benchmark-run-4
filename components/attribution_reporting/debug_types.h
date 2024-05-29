80092f0c8d287b7e02a738ea305521cfb6d70562
Andrew Paseltiner <apaseltiner@chromium.org>
2024-05-29 20:34:17 +0000

    Move DebugDataType enum from //content C++ to //components Mojo
    To enable it to be used in //components/attribution_reporting and its
    associated Mojo interfaces for the forthcoming aggregatable debug report
    feature.
    
    This CL contains no user-visible changes, though it does rename two enum
    variants to match their corresponding serializations.
    
    Change-Id: I62d14e551c74d4e4c8ba0e3b77cd470605559111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582353
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307674}

---
+++ components/attribution_reporting/debug_types.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_ATTRIBUTION_REPORTING_DEBUG_TYPES_H_
+#define COMPONENTS_ATTRIBUTION_REPORTING_DEBUG_TYPES_H_
+
+#include <string_view>
+
+#include "base/component_export.h"
+#include "components/attribution_reporting/debug_types.mojom-forward.h"
+
+namespace attribution_reporting {
+
+COMPONENT_EXPORT(ATTRIBUTION_REPORTING)
+std::string_view SerializeDebugDataType(mojom::DebugDataType);
+
+}  // namespace attribution_reporting
+
+#endif  // COMPONENTS_ATTRIBUTION_REPORTING_DEBUG_TYPES_H_
