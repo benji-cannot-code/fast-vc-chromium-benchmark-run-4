316d8ef91eb7b4ba20d445e0319425d7dcd81a49
Vidhan Jain <vidhanj@google.com>
2024-05-29 12:06:13 +0000

    Cleanup feature AutofillEnableSelectList
    The feature has been enabled for a while now.
    
    Bug: 40261659
    Change-Id: I6dc0019185d3d75f0f7417fb2298e9993ce07fdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5563688
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307343}

--- components/autofill/content/renderer/form_autofill_util_browsertest.cc
+++ components/autofill/content/renderer/form_autofill_util_browsertest.cc
@@ -199,8 +199,7 @@ class FormAutofillUtilsTest : public content::RenderViewTest {
   FormAutofillUtilsTest() {
     scoped_feature_list_.InitWithFeatures(
         /*enabled_features=*/
-        {features::kAutofillReplaceCachedWebElementsByRendererIds,
-         features::kAutofillEnableSelectList},
+        {features::kAutofillReplaceCachedWebElementsByRendererIds},
         /*disabled_features=*/{});
   }
   ~FormAutofillUtilsTest() override = default;
@@ -1514,30 +1513,7 @@ INSTANTIATE_TEST_SUITE_P(
       return cases;
     }()));
 
-// FormAutofillUtilsTest subclass for testing with and without
-// features::kAutofillEnableSelectList feature enabled.
-class SelectListAutofillParamTest : public FormAutofillUtilsTest,
-                                    public testing::WithParamInterface<bool> {
- public:
-  SelectListAutofillParamTest() {
-    scoped_feature_list_.InitWithFeatureState(
-        features::kAutofillEnableSelectList, IsAutofillingSelectListEnabled());
-  }
-  ~SelectListAutofillParamTest() override = default;
-
-  bool IsAutofillingSelectListEnabled() const { return GetParam(); }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
-};
-
-INSTANTIATE_TEST_SUITE_P(FormAutofillUtilsTest,
-                         SelectListAutofillParamTest,
-                         ::testing::Bool());
-
-// Test that ExtractFormData() ignores <selectlist> if
-// features::kAutofillEnableSelectList is disabled.
-TEST_P(SelectListAutofillParamTest, WebFormElementToFormData) {
+TEST_F(FormAutofillUtilsTest, WebFormElementToFormData) {
   LoadHTML(R"(
     <form id='form'>
       <input id='input'>
@@ -1553,8 +1529,7 @@ TEST_P(SelectListAutofillParamTest, WebFormElementToFormData) {
   auto form_element = GetFormElementById(doc, "form");
   FormData form_data = *ExtractFormData(doc, form_element, field_data_manager(),
                                         /*extract_options=*/{});
-  EXPECT_EQ(form_data.fields.size(),
-            IsAutofillingSelectListEnabled() ? 2u : 1u);
+  EXPECT_EQ(form_data.fields.size(), 2u);
 
   {
     WebElement element = GetElementById(doc, "input");
@@ -1564,13 +1539,11 @@ TEST_P(SelectListAutofillParamTest, WebFormElementToFormData) {
                                       form_data.fields[0]));
   }
 
-  if (IsAutofillingSelectListEnabled()) {
-    WebElement element = GetElementById(doc, "selectlist");
-    ASSERT_FALSE(element.IsNull());
-    ASSERT_TRUE(element.IsFormControlElement());
-    EXPECT_TRUE(HaveSameFormControlId(element.To<WebFormControlElement>(),
-                                      form_data.fields[1]));
-  }
+  WebElement element = GetElementById(doc, "selectlist");
+  ASSERT_FALSE(element.IsNull());
+  ASSERT_TRUE(element.IsFormControlElement());
+  EXPECT_TRUE(HaveSameFormControlId(element.To<WebFormControlElement>(),
+                                    form_data.fields[1]));
 }
 
 // Tests that if the number of iframes exceeds kMaxExtractableChildFrames,
