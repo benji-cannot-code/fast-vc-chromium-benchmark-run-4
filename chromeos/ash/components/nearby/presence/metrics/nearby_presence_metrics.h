55a99c8790b315bbe7073c22d8ea3312df94fc65
Crisrael Lucero <crisrael@google.com>
2024-05-29 20:33:01 +0000

    [Nearby Presence] Record scan request results
    This CL records the result of a `StartScan()` request and updates
    comments on syncing enums. Additionally, modified a unit test that was
    not capturing a failed `GetProcessReference()`.
    
    Test: unit tests, verified on chrome://histograms with prototype
    Fixed: b/321750506
    Change-Id: Ie88cda4386d7428a6540827c08fb75e1263abd18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5570628
    Reviewed-by: Jack Shira <jackshira@google.com>
    Reviewed-by: Juliet Lévesque <julietlevesque@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307672}

--- chromeos/ash/components/nearby/presence/metrics/nearby_presence_metrics.h
+++ chromeos/ash/components/nearby/presence/metrics/nearby_presence_metrics.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time/time.h"
 #include "chromeos/ash/components/nearby/common/client/nearby_http_result.h"
+#include "chromeos/ash/components/nearby/presence/enums/nearby_presence_enums.h"
 #include "chromeos/ash/services/nearby/public/cpp/nearby_process_manager.h"
 
 namespace ash::nearby::presence::metrics {
@@ -48,6 +49,7 @@ void RecordFirstTimeServerRegistrationFailureReason(
     ash::nearby::NearbyHttpResult failure_reason);
 void RecordFirstTimeServerRegistrationTotalAttemptsNeededCount(
     int attempt_count);
+void RecordScanRequestResult(enums::StatusCode result);
 void RecordDeviceFoundLatency(base::TimeDelta device_found_latency);
 void RecordNearbyProcessShutdownReason(
     ash::nearby::NearbyProcessManager::NearbyProcessShutdownReason
--- chromeos/ash/components/nearby/presence/nearby_presence_service_impl.cc
+++ chromeos/ash/components/nearby/presence/nearby_presence_service_impl.cc
@@ -83,6 +83,8 @@ void NearbyPresenceServiceImpl::StartScan(
     std::move(on_start_scan_callback)
         .Run(/*scan_session=*/nullptr,
              /*status=*/enums::StatusCode::kFailedToStartProcess);
+
+    metrics::RecordScanRequestResult(enums::StatusCode::kFailedToStartProcess);
     return;
   }
 
@@ -235,6 +237,8 @@ void NearbyPresenceServiceImpl::OnScanStarted(
   }
   std::move(on_start_scan_callback)
       .Run(std::move(scan_session), enums::ConvertToPresenceStatus(status));
+
+  metrics::RecordScanRequestResult(enums::ConvertToPresenceStatus(status));
 }
 
 void NearbyPresenceServiceImpl::OnScanSessionDisconnect(
