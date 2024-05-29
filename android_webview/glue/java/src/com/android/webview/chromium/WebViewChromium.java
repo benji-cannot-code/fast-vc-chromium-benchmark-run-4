65918260b60b488b067ec817fec6727bac11ae73
Jiacheng Guo <gjc@google.com>
2024-05-29 23:53:13 +0000

    Prerender: Cancel prerendering when setting WebView clients
    The CL exports the `cancelAllPrerendering` function to Java and cancels
    the prerendering if the user resets the WebViewClient and the
    WebChromeClient. By cancelling the prerendering, the newly set client
    can receive the callbacks sent to the previous client.
    
    Bug: 342978960
    Change-Id: I0854c8b62e6e1a6b1f2e47692bbc69a3236d16f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573229
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307782}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromium.java
@@ -2183,6 +2183,7 @@ class WebViewChromium
     public void setWebViewClient(WebViewClient client) {
         try (TraceEvent event = TraceEvent.scoped("WebView.APICall.Framework.SET_WEBVIEW_CLIENT")) {
             recordWebViewApiCall(ApiCall.SET_WEBVIEW_CLIENT);
+            mAwContents.cancelAllPrerendering();
             mSharedWebViewChromium.setWebViewClient(client);
             mContentsClientAdapter.setWebViewClient(mSharedWebViewChromium.getWebViewClient());
             if (client != null) {
@@ -2259,6 +2260,7 @@ class WebViewChromium
         try (TraceEvent event =
                 TraceEvent.scoped("WebView.APICall.Framework.SET_WEBCHROME_CLIENT")) {
             recordWebViewApiCall(ApiCall.SET_WEBCHROME_CLIENT);
+            mAwContents.cancelAllPrerendering();
             mWebSettings.getAwSettings().setFullscreenSupported(doesSupportFullscreen(client));
             mSharedWebViewChromium.setWebChromeClient(client);
             mContentsClientAdapter.setWebChromeClient(mSharedWebViewChromium.getWebChromeClient());
