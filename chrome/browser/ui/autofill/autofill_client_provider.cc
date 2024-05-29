50fdf3141227f96d141ffdd855032999af2ddd8c
Friedrich Horschig <fhorschig@chromium.org>
2024-05-29 16:15:12 +0000

    [Android][3PPWM] Add policy to lock 3P PWM setting
    This enterprise policy allows administrators to prevent managed clients
    from enabling 3P password managers. It only affects the upcoming Android
    Autofill APIs and not the currently launched implementation that uses
    a11y APIs.
    
    It's enabled by default. User can then change their Clank setting to use
    a 3P password manager.
    If disabled, that setting will be unavailable. On restart, Chrome will
    reset the setting and ensure the built-in provider is used for each tab.
    The restart is also required when toggling the setting independently of
    the policy since already created tabs can't switch the password manager.
    
    Bug: 308427163
    Change-Id: I963627fca921126023205c1667c30e897cc0f0ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545622
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Victor Gabriel Savu <vsavu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307476}

--- chrome/browser/ui/autofill/autofill_client_provider.cc
+++ chrome/browser/ui/autofill/autofill_client_provider.cc
@@ -28,6 +28,9 @@ bool UsesVirtualViewStructureForAutofill(const PrefService* prefs) {
           features::kAutofillVirtualViewStructureAndroid)) {
     return false;
   }
+  if (!prefs->GetBoolean(prefs::kAutofillThirdPartyPasswordManagersAllowed)) {
+    return false;
+  }
   if (!prefs->GetBoolean(prefs::kAutofillUsingVirtualViewStructure)) {
     return false;
   }
