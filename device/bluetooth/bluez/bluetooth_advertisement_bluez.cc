c78412e480bf82d27dabd8d5a2173da7c3ef3cd6
Yun-Hao Chung <howardchung@google.com>
2024-05-29 00:41:45 +0000

    bluetooth: Only use ext adv when adapter support
    This prevents the advertisement client register extended advertisements
    when the adapter doesn't support it.
    
    verify that quick share advertise success on rammus/shyvana and
    brya/taniks.
    
    Bug: b:341007722
    Test: Enable #nearby-ble-v2 and disable #nearby-bluetooth-classic-adv,
    Change-Id: Ia3c0550bcf18fce36dba28febf6d5fd553111739
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547250
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Commit-Queue: Yun-hao Chung <howardchung@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307147}

--- device/bluetooth/bluez/bluetooth_advertisement_bluez.cc
+++ device/bluetooth/bluez/bluetooth_advertisement_bluez.cc
@@ -86,7 +86,7 @@ BluetoothAdvertisementBlueZ::BluetoothAdvertisementBlueZ(
   DCHECK(bluez::BluezDBusManager::Get());
   provider_ = bluez::BluetoothLEAdvertisementServiceProvider::Create(
       bluez::BluezDBusManager::Get()->GetSystemBus(), advertisement_object_path,
-      this,
+      this, adapter->IsExtendedAdvertisementsAvailable(),
       static_cast<
           bluez::BluetoothLEAdvertisementServiceProvider::AdvertisementType>(
           data->type()),
