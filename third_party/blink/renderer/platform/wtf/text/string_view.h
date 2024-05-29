30dce266f6d5bed1b87e63708f95b7f41736fa9b
Kevin McNee <mcnee@chromium.org>
2024-05-29 15:25:23 +0000

    Prevent construction of StringViews with a temporary String the view will outlive
    We use the lifetimebound attribute on the StringView constructor that
    takes a String in order to have the compiler guard against this.
    
    We add a no-compile test to verify that several incorrect usages cause
    compile failures.
    
    Bug: 342551581
    Change-Id: I4cb32dbd214d0d21d7fa3e569ccc928aa6d5a621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568381
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307444}

--- third_party/blink/renderer/platform/wtf/text/string_view.h
+++ third_party/blink/renderer/platform/wtf/text/string_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/dcheck_is_on.h"
 #include "base/numerics/safe_conversions.h"
+#include "third_party/abseil-cpp/absl/base/attributes.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/get_ptr.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
@@ -116,14 +117,22 @@ class WTF_EXPORT StringView {
   StringView(StringImpl&, unsigned offset, unsigned length);
 
   // From a String, implemented in wtf_string.h
-  inline StringView(const String&, unsigned offset, unsigned length);
-  inline StringView(const String&, unsigned offset);
-  inline StringView(const String&);
+  inline StringView(const String& string ABSL_ATTRIBUTE_LIFETIME_BOUND,
+                    unsigned offset,
+                    unsigned length);
+  inline StringView(const String& string ABSL_ATTRIBUTE_LIFETIME_BOUND,
+                    unsigned offset);
+  // NOLINTNEXTLINE(google-explicit-constructor)
+  inline StringView(const String& string ABSL_ATTRIBUTE_LIFETIME_BOUND);
 
   // From an AtomicString, implemented in atomic_string.h
-  inline StringView(const AtomicString&, unsigned offset, unsigned length);
-  inline StringView(const AtomicString&, unsigned offset);
-  inline StringView(const AtomicString&);
+  inline StringView(const AtomicString& string ABSL_ATTRIBUTE_LIFETIME_BOUND,
+                    unsigned offset,
+                    unsigned length);
+  inline StringView(const AtomicString& string ABSL_ATTRIBUTE_LIFETIME_BOUND,
+                    unsigned offset);
+  // NOLINTNEXTLINE(google-explicit-constructor)
+  inline StringView(const AtomicString& string ABSL_ATTRIBUTE_LIFETIME_BOUND);
 
   // From a literal string or LChar buffer:
   StringView(const LChar* chars, unsigned length)
--- third_party/blink/renderer/platform/wtf/text/wtf_string.h
+++ third_party/blink/renderer/platform/wtf/text/wtf_string.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "build/build_config.h"
+#include "third_party/abseil-cpp/absl/base/attributes.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/integer_to_string_conversion.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_impl.h"
@@ -716,13 +717,17 @@ class WTF_EXPORT NewlineThenWhitespaceStringsTable {
 // double-quotes, and escapes characters other than ASCII printables.
 WTF_EXPORT std::ostream& operator<<(std::ostream&, const String&);
 
-inline StringView::StringView(const String& string,
+inline StringView::StringView(const String& string
+                                  ABSL_ATTRIBUTE_LIFETIME_BOUND,
                               unsigned offset,
                               unsigned length)
     : StringView(string.Impl(), offset, length) {}
-inline StringView::StringView(const String& string, unsigned offset)
+inline StringView::StringView(const String& string
+                                  ABSL_ATTRIBUTE_LIFETIME_BOUND,
+                              unsigned offset)
     : StringView(string.Impl(), offset) {}
-inline StringView::StringView(const String& string)
+inline StringView::StringView(
+    const String& string ABSL_ATTRIBUTE_LIFETIME_BOUND)
     : StringView(string.Impl()) {}
 
 }  // namespace WTF
