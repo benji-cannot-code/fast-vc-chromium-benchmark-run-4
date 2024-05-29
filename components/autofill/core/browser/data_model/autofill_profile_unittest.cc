3bc1b3a480deac489d6b1eda4787234b535122bb
Wolfgang Billenstein <bwolfgang@google.com>
2024-05-29 13:31:34 +0000

    [Autofill] Add UMA Autofill.Quality.DaysUntilFirstUsage.Profile
    Adds a new UMA metric that records the number of days from saving an
    Autofill profil until using for the first time before it got purged.
    
    Change-Id: I2adc74ac75f8866c31c4d9a5d4bad200b7c08e89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5575478
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307375}

--- components/autofill/core/browser/data_model/autofill_profile_unittest.cc
+++ components/autofill/core/browser/data_model/autofill_profile_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/memory/raw_ptr.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "base/uuid.h"
@@ -1678,6 +1679,25 @@ TEST(AutofillProfileTest, GetStorableTypeOf) {
   // Test that stored types are returned as-is.
   EXPECT_EQ(profile.GetStorableTypeOf(ADDRESS_HOME_STATE), ADDRESS_HOME_STATE);
   EXPECT_EQ(profile.GetStorableTypeOf(COMPANY_NAME), COMPANY_NAME);
+}
+
+// Tests that `AutofillProfile::RecordUseAndLog()` logs days until first usage.
+TEST(AutofillProfileTest, EmitsDaysUntilFirstUsageProfile) {
+  TestAutofillClock clock;
+  const size_t expect_number_of_days = 237;
+  AutofillProfile profile(i18n_model_definition::kLegacyHierarchyCountryCode);
+  clock.Advance(base::Days(expect_number_of_days));
+
+  base::HistogramTester histogram_tester;
+  profile.RecordAndLogUse();
+  histogram_tester.ExpectUniqueSample(
+      "Autofill.Quality.DaysUntilFirstUsage.Profile", expect_number_of_days, 1);
+
+  profile.RecordAndLogUse();
+  EXPECT_EQ(histogram_tester
+                .GetAllSamples("Autofill.Quality.DaysUntilFirstUsage.Profile")
+                .size(),
+            1UL);
 }
 
 enum Expectation { GREATER, LESS };
