110745aeb432d9c09c1df27bfb82d9b4983c5971
Ivana Drašković <tivi@google.com>
2024-05-29 12:00:07 +0000

    [gbb-cct] Add user action log for Save and Share button
    Bug: 341878998
    
    Change-Id: I747ae0a6d535b4e6473b7161cfca94166cd94949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569229
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Edmund Wright <edmundw@google.com>
    Commit-Queue: Edmund Wright <edmundw@google.com>
    Auto-Submit: Ivana Drašković <tivi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307341}

--- chrome/browser/ui/android/google_bottom_bar/java/src/org/chromium/chrome/browser/ui/google_bottom_bar/GoogleBottomBarActionsHandler.java
+++ chrome/browser/ui/android/google_bottom_bar/java/src/org/chromium/chrome/browser/ui/google_bottom_bar/GoogleBottomBarActionsHandler.java
@@ -14,6 +14,7 @@ import android.view.View;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Log;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.page_insights.PageInsightsCoordinator;
 import org.chromium.chrome.browser.share.ShareDelegate;
@@ -92,6 +93,8 @@ class GoogleBottomBarActionsHandler {
     }
 
     private void onShareButtonClick(ButtonConfig buttonConfig) {
+        // TODO(b/342576463) Remove after GBB experiment
+        RecordUserAction.record("CustomTabsCustomActionButtonClick");
         PendingIntent pendingIntent = buttonConfig.getPendingIntent();
         if (pendingIntent != null) {
             sendPendingIntentWithUrl(pendingIntent);
@@ -119,6 +122,8 @@ class GoogleBottomBarActionsHandler {
     }
 
     private void onSaveButtonClick(ButtonConfig buttonConfig, View view) {
+        // TODO(b/342576463) Remove after GBB experiment
+        RecordUserAction.record("CustomTabsCustomActionButtonClick");
         PendingIntent pendingIntent = buttonConfig.getPendingIntent();
         if (pendingIntent != null) {
             sendPendingIntentWithUrl(pendingIntent);
