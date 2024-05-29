da96528d1d0e44ffd7f180c7eb4e529e942da7f8
JunHo Seo <junho0924.seo@lge.com>
2024-05-29 09:22:45 +0000

    Snav: Convert ramaining snav legacy tests to testharness.js
    And finally this CL removes legacy helper js file
    (spatial-navigation-utils.js).
    
    Bug: 41365516
    Change-Id: I7959e191480a6caeb081c689c48c26fe56cb28d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578059
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: JunHo Seo <junho0924.seo@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1307296}

--- third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
+++ third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
@@ -28,6 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       case 'Backward':
         eventSender.keyDown('Tab', ['shiftKey']);
         break;
+      case 'Space':
+        eventSender.keyDown(' ');
+        break;
     }
   }
 
