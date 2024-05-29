6ddf6416dc0802f150f8cb0dde0421e7cd106c90
Aaron Leventhal <aleventhal@google.com>
2024-05-29 22:18:23 +0000

    [A11y] Simplify ComputeIsIgnoredButIncludedInTree
    Add a comment as to what the legitimate reasons are for doing this
    and remove the SVG group rule which is not.
    
    The overall purpose is so that we know when to use this extra
    "ignored but included" bit and when not too -- it's extra complexity
    and easily confuses developers.
    
    Bug: none
    Change-Id: I2e59f85c96483f2e107664d4a4d4137bfe69f562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582731
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307728}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -2996,7 +2996,8 @@ bool AXObject::IsLineBreakingObject() const {
   // virtual accessibility nodes, for example.
   //
   // We assume that most images on the Web are inline.
-  return !IsImage() && ui::IsStructure(RoleValue());
+  return !IsImage() && ui::IsStructure(RoleValue()) &&
+         !IsA<SVGElement>(GetNode());
 }
 
 bool AXObject::IsLinked() const {
@@ -4021,10 +4022,6 @@ bool AXObject::ComputeIsIgnoredButIncludedInTree() {
   if (element->HasTagName(html_names::kRtTag)) {
     return true;
   }
-
-  // Preserve SVG grouping elements.
-  if (IsA<SVGGElement>(element))
-    return true;
 
   // Keep table-related elements in the tree, because it's too easy for them
   // to in and out of being ignored based on their ancestry, as their role
