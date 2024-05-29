5b9f24b3f7f36e6ea23906c641a9a42de7f7d244
Teresa Mao <temao@chromium.org>
2024-05-29 23:18:06 +0000

    Feedback UI: Migrate to Lit, part 1.
    This CL migrates:
    - app.ts
    - autofill_metadata_app.ts
    - system_info_app.ts
    
    Bug: 40943652
    Change-Id: I62a8b3b391845d55a69bfbd9df309372593757b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578119
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307763}

--- chrome/test/data/webui/feedback/feedback_test.ts
+++ chrome/test/data/webui/feedback/feedback_test.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://feedback/app.js';
 
-import type {FeedbackAppElement} from 'chrome://feedback/app.js';
+import type {AppElement} from 'chrome://feedback/app.js';
 import {FeedbackBrowserProxyImpl} from 'chrome://feedback/js/feedback_browser_proxy.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {OpenWindowProxyImpl} from 'chrome://resources/js/open_window_proxy.js';
@@ -18,7 +18,7 @@ import {TestFeedbackBrowserProxy} from './test_feedback_browser_proxy.js';
 const USER_EMAIL = 'dummy_user_email';
 
 suite('FeedbackTest', function() {
-  let app: FeedbackAppElement;
+  let app: AppElement;
   let browserProxy: TestFeedbackBrowserProxy;
   let openWindowProxy: TestOpenWindowProxy;
 
@@ -30,12 +30,12 @@ suite('FeedbackTest', function() {
     browserProxy.setUserEmail(USER_EMAIL);
     FeedbackBrowserProxyImpl.setInstance(browserProxy);
 
-    app = await createFeedbackAppElement();
+    app = await createAppElement();
   });
 
-  async function createFeedbackAppElement(): Promise<FeedbackAppElement> {
+  async function createAppElement(): Promise<AppElement> {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
-    const app: FeedbackAppElement = document.createElement('feedback-app');
+    const app: AppElement = document.createElement('feedback-app');
     document.body.appendChild(app);
     await eventToPromise('ready-for-testing', app);
     return app;
@@ -88,7 +88,7 @@ suite('FeedbackTest', function() {
   test('ScreenshotFailed', async function() {
     browserProxy.setUserMedia(Promise.reject(
         {name: 'error', message: 'error', constraintName: 'error'}));
-    app = await createFeedbackAppElement();
+    app = await createAppElement();
 
     const checkbox =
         app.getRequiredElement<HTMLInputElement>('#screenshot-checkbox');
@@ -161,7 +161,7 @@ suite('FeedbackTest', function() {
 
 suite('AIFeedbackTest', function() {
   const LOG_ID: string = 'TEST_LOG_ID';
-  let app: FeedbackAppElement;
+  let app: AppElement;
   let browserProxy: TestFeedbackBrowserProxy;
   let openWindowProxy: TestOpenWindowProxy;
 
@@ -235,7 +235,7 @@ suite('AIFeedbackTest', function() {
 });
 
 suite('SeaPenFeedbackTest', function() {
-  let app: FeedbackAppElement;
+  let app: AppElement;
   let browserProxy: TestFeedbackBrowserProxy;
   let openWindowProxy: TestOpenWindowProxy;
 
