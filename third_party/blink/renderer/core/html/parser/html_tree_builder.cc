7501ab2f57d1344f708658e28e37525d35017802
Joey Arhar <jarhar@chromium.org>
2024-05-29 18:05:49 +0000

    Remove SelectHr flag
    This feature was enabled by default in M119, so it has been enough time
    that we can remove the flag now.
    
    Bug: 40064328
    Change-Id: Ie818da4492633482b36e3b81005f42e74e25a70b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5556030
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307559}

--- third_party/blink/renderer/core/html/parser/html_tree_builder.cc
+++ third_party/blink/renderer/core/html/parser/html_tree_builder.cc
@@ -1534,9 +1534,6 @@ void HTMLTreeBuilder::ProcessStartTag(AtomicHTMLToken* token) {
           tree_.InsertHTMLElement(token);
           return;
         case HTMLTag::kHr:
-          if (!RuntimeEnabledFeatures::SelectHrEnabled()) {
-            break;
-          }
           if (tree_.CurrentStackItem()->MatchesHTMLTag(HTMLTag::kOption)) {
             AtomicHTMLToken end_option(HTMLToken::kEndTag, HTMLTag::kOption);
             ProcessEndTag(&end_option);
