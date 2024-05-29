bfeadd30256663048d1d5415ec360415b0609f6f
Hailey Wang <haileywang@google.com>
2024-05-29 17:27:04 +0000

    [Gardener] Disable failing device_dialog tests
    Bug: b/343347280
    Change-Id: I47feab623c71bf66b3b77c62ed763391b29b68d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582169
    Owners-Override: Hailey Wang <haileywang@google.com>
    Commit-Queue: Hailey Wang <haileywang@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307528}

--- chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/UsbChooserDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/device_dialog/UsbChooserDialogTest.java
@@ -29,6 +29,7 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.profiles.ProfileManager;
@@ -200,6 +201,7 @@ public class UsbChooserDialogTest {
 
     @Test
     @SmallTest
+    @DisabledTest(message = "b/343347280")
     public void testChooserBlockedByModalDialogManager() {
         ModalDialogManager mockModalDialogManager = mock(ModalDialogManager.class);
         when(mockModalDialogManager.isSuspended(ModalDialogManager.ModalDialogType.APP))
