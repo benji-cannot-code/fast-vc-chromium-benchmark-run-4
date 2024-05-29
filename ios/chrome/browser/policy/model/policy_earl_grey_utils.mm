eb790e2d609807d2406cea0fb6608fe853e01faf
Jérôme Lebel <jlebel@chromium.org>
2024-05-29 18:27:28 +0000

    [iOS] Add APIs to add a policy
    Add the following APIs to be able to add a policy without removing
    the existing ones:
    void MergePolicy(bool enabled, const std::string& policy_key);
    void MergePolicy(int value, const std::string& policy_key);
    void MErgePolicyWithStringValue(const std::string& value,
                                    const std::string& policy_key);
    
    Low-Coverage-Reason: OTHER Those changes are related to EGTest code
    Change-Id: I259e62841558c96807717b55738292fc43e1ac63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577415
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1307582}

--- ios/chrome/browser/policy/model/policy_earl_grey_utils.mm
+++ ios/chrome/browser/policy/model/policy_earl_grey_utils.mm
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/policy/model/policy_app_interface.h"
 
 namespace {
+
 // Returns a JSON-encoded string representing the given `base::Value`. If
 // `value` is nullptr, returns a string representing a `base::Value` of type
 // NONE.
@@ -19,6 +20,20 @@ std::string SerializeValue(const base::Value value) {
   serializer.Serialize(std::move(value));
   return serialized_value;
 }
+
+// Merges the json policy corresponding to `policy_key` to the existing
+// policies with its value sets to `json_value`.
+void MergePolicy(const std::string& json_value, const std::string& policy_key) {
+  [PolicyAppInterface mergePolicyValue:base::SysUTF8ToNSString(json_value)
+                                forKey:base::SysUTF8ToNSString(policy_key)];
+}
+
+// Merges the value policy corresponding to `policy_key` to the existing
+// policies with its value sets to `value`.
+void MergePolicy(base::Value value, const std::string& policy_key) {
+  MergePolicy(SerializeValue(std::move(value)), policy_key);
+}
+
 }  // namespace
 
 namespace policy_test_utils {
@@ -34,13 +49,26 @@ void SetPolicy(bool enabled, const std::string& policy_key) {
   SetPolicy(base::Value(enabled), policy_key);
 }
 
+void MergePolicy(bool enabled, const std::string& policy_key) {
+  ::MergePolicy(base::Value(enabled), policy_key);
+}
+
 void SetPolicy(int value, const std::string& policy_key) {
   SetPolicy(base::Value(value), policy_key);
+}
+
+void MergePolicy(int value, const std::string& policy_key) {
+  ::MergePolicy(base::Value(value), policy_key);
 }
 
 void SetPolicyWithStringValue(const std::string& value,
                               const std::string& policy_key) {
   SetPolicy(base::Value(value), policy_key);
+}
+
+void MergePolicyWithStringValue(const std::string& value,
+                                const std::string& policy_key) {
+  ::MergePolicy(base::Value(value), policy_key);
 }
 
 void SetPolicy(const std::string& json_value, const std::string& policy_key) {
