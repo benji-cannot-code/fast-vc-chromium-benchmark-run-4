6a3b6b24e42355106eca5ff9cde28f42bf8144af
Siye Liu <siliu@microsoft.com>
2024-05-30 18:54:16 +0000

    Add a new options argument to `document.caretPositionFromPoint` API.
    Per https://github.com/w3c/csswg-drafts/issues/10345, we should change
    `shadowRoots` parameter from a rest parameter to a dictionary which
    contains an array of shadow roots.
    
    Bug: 41487363
    Change-Id: I0686d1eae75797ae07cad215a71a24ab54c7398d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553027
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1308173}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_aria_notification_options.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_caret_position_from_point_options.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_element_creation_options.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_element_registration_options.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_observable_array_css_style_sheet.h"
@@ -1756,7 +1757,7 @@ Range* Document::caretRangeFromPoint(int x, int y) {
 CaretPosition* Document::caretPositionFromPoint(
     float x,
     float y,
-    const HeapVector<Member<ShadowRoot>>& shadow_roots) {
+    const CaretPositionFromPointOptions* options) {
   if (!GetLayoutView()) {
     return nullptr;
   }
@@ -1773,7 +1774,8 @@ CaretPosition* Document::caretPositionFromPoint(
   }
   bool adjust_position = false;
   while (anchor_node->IsInShadowTree() &&
-         !shadow_roots.Contains(anchor_node->GetTreeScope())) {
+         !(options->hasShadowRoots() &&
+           options->shadowRoots().Contains(anchor_node->GetTreeScope()))) {
     anchor_node = anchor_node->OwnerShadowHost();
     adjust_position = true;
   }
