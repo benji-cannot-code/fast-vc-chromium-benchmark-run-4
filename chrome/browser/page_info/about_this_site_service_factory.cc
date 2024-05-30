767715061a82e08f21fdcf768cfd19f113928f8f
Christian Dullweber <dullweber@chromium.org>
2024-05-30 15:37:39 +0000

    ATP: Handle null OptimizationGuide
    OptimizationGuide can be null so AboutThisPageFactory should consider
    that case.
    
    Bug: 343681864
    Change-Id: I14edcc5061cfec1279a04d0aed3bb483bf6d651e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581437
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308019}

--- chrome/browser/page_info/about_this_site_service_factory.cc
+++ chrome/browser/page_info/about_this_site_service_factory.cc
@@ -54,9 +54,14 @@ AboutThisSiteServiceFactory::BuildServiceInstanceForBrowserContext(
 
   Profile* profile = Profile::FromBrowserContext(browser_context);
 
+  auto* optimization_guide =
+      OptimizationGuideKeyedServiceFactory::GetForProfile(profile);
+  if (!optimization_guide) {
+    return nullptr;
+  }
+
   return std::make_unique<page_info::AboutThisSiteService>(
-      OptimizationGuideKeyedServiceFactory::GetForProfile(profile),
-      profile->IsOffTheRecord(), profile->GetPrefs(),
+      optimization_guide, profile->IsOffTheRecord(), profile->GetPrefs(),
       TemplateURLServiceFactory::GetForProfile(profile));
 }
 
