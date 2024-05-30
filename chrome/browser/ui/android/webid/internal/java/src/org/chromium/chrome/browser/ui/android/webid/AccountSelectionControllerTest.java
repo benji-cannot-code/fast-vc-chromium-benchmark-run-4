cf188503a063ab34da955eeee5077f940b3c9dfa
Christian Biesinger <cbiesinger@chromium.org>
2024-05-30 16:00:15 +0000

    [FedCM] Make the Android UI use the downloaded bitmap
    After https://crrev.com/c/5564909 and https://crrev.com/c/5574105 we have
    the decoded profile picture in the IdentityRequestAccount; this CL changes
    Android to use that decoded picture instead of redownloading it.
    
    Bug: 340895241
    Change-Id: I856a35fe3818f0610345213f078ee538082004cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582729
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1308033}

--- chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionControllerTest.java
+++ chrome/browser/ui/android/webid/internal/java/src/org/chromium/chrome/browser/ui/android/webid/AccountSelectionControllerTest.java
@@ -12,7 +12,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
-import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.never;
@@ -22,7 +21,6 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import static org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.AccountProperties.ACCOUNT;
-import static org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.AccountProperties.AVATAR;
 import static org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.HeaderProperties.IDP_BRAND_ICON;
 import static org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.HeaderProperties.IDP_FOR_DISPLAY;
 import static org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.HeaderProperties.IFRAME_FOR_DISPLAY;
@@ -37,7 +35,6 @@ import androidx.annotation.Px;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -51,7 +48,6 @@ import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.AccountProperties;
-import org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.AccountProperties.Avatar;
 import org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.ContinueButtonProperties;
 import org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.DataSharingConsentProperties;
 import org.chromium.chrome.browser.ui.android.webid.AccountSelectionProperties.ErrorProperties;
@@ -107,9 +103,17 @@ public class AccountSelectionControllerTest {
                     "Ana Doe",
                     "Ana",
                     TEST_PROFILE_PIC,
+                    /* pictureBitmap= */ null,
                     /* isSignIn= */ true);
     private static final Account BOB =
-            new Account("Bob", "", "Bob", "", TEST_PROFILE_PIC, /* isSignIn= */ true);
+            new Account(
+                    "Bob",
+                    "",
+                    "Bob",
+                    "",
+                    TEST_PROFILE_PIC,
+                    /* pictureBitmap= */ null,
+                    /* isSignIn= */ true);
     private static final Account CARL =
             new Account(
                     "Carl",
@@ -117,6 +121,7 @@ public class AccountSelectionControllerTest {
                     "Carl Test",
                     ":)",
                     TEST_PROFILE_PIC,
+                    /* pictureBitmap= */ null,
                     /* isSignIn= */ true);
     private static final Account NEW_USER =
             new Account(
@@ -125,6 +130,7 @@ public class AccountSelectionControllerTest {
                     "Sam E. Goto",
                     "Sam",
                     TEST_PROFILE_PIC,
+                    /* pictureBitmap= */ null,
                     /* isSignIn= */ false);
     private static final String[] RP_CONTEXTS =
             new String[] {"signin", "signup", "use", "continue"};
@@ -281,10 +287,8 @@ public class AccountSelectionControllerTest {
         PropertyModel headerModel = mModel.get(ItemProperties.HEADER);
         assertNull(headerModel.get(IDP_BRAND_ICON));
 
-        // The only downloaded icon should not be an IDP brand icon.
-        verify(mMockImageFetcher, times(1))
-                .fetchImage(
-                        argThat(imageFetcherParamsHaveUrl(TEST_PROFILE_PIC)), any(Callback.class));
+        // There should be no downloads.
+        verify(mMockImageFetcher, times(0)).fetchImage(any(), any());
     }
 
     @Test
@@ -302,70 +306,6 @@ public class AccountSelectionControllerTest {
 
         PropertyModel headerModel = mModel.get(ItemProperties.HEADER);
         assertEquals(HeaderType.SIGN_IN, headerModel.get(TYPE));
-    }
-
-    @Test
-    public void testShowAccountsSetsAccountListAndRequestsAvatar() {
-        mMediator.showAccounts(
-                TEST_ETLD_PLUS_ONE,
-                TEST_ETLD_PLUS_ONE_1,
-                TEST_ETLD_PLUS_ONE_2,
-                Arrays.asList(ANA, BOB),
-                IDP_METADATA,
-                CLIENT_ID_METADATA,
-                /* isAutoReauthn= */ false,
-                /* rpContext= */ "signin",
-                /* requestPermission= */ true);
-        assertEquals("Incorrect item sheet count", 2, mSheetAccountItems.size());
-        assertNull(mSheetAccountItems.get(0).model.get(AVATAR));
-        assertNull(mSheetAccountItems.get(1).model.get(AVATAR));
-
-        // Both accounts have the same profile pic URL
-        ImageFetcher.Params expected_params =
-                ImageFetcher.Params.create(
-                        TEST_PROFILE_PIC.getSpec(),
-                        ImageFetcher.WEB_ID_ACCOUNT_SELECTION_UMA_CLIENT_NAME,
-                        DESIRED_AVATAR_SIZE,
-                        DESIRED_AVATAR_SIZE);
-
-        verify(mMockImageFetcher, times(2)).fetchImage(eq(expected_params), any());
-    }
-
-    @Test
-    public void testFetchAvatarUpdatesModel() {
-        mMediator.showAccounts(
-                TEST_ETLD_PLUS_ONE,
-                TEST_ETLD_PLUS_ONE_1,
-                TEST_ETLD_PLUS_ONE_2,
-                Collections.singletonList(CARL),
-                IDP_METADATA,
-                CLIENT_ID_METADATA,
-                /* isAutoReauthn= */ false,
-                /* rpContext= */ "signin",
-                /* requestPermission= */ true);
-        assertEquals("Incorrect item sheet count", 1, mSheetAccountItems.size());
-        assertEquals("Incorrect account", CARL, mSheetAccountItems.get(0).model.get(ACCOUNT));
-        assertNull(mSheetAccountItems.get(0).model.get(AVATAR));
-
-        ImageFetcher.Params expected_params =
-                ImageFetcher.Params.create(
-                        TEST_PROFILE_PIC.getSpec(),
-                        ImageFetcher.WEB_ID_ACCOUNT_SELECTION_UMA_CLIENT_NAME,
-                        DESIRED_AVATAR_SIZE,
-                        DESIRED_AVATAR_SIZE);
-
-        final ArgumentCaptor<Callback<Bitmap>> callback = ArgumentCaptor.forClass(Callback.class);
-        verify(mMockImageFetcher).fetchImage(eq(expected_params), callback.capture());
-
-        Bitmap bitmap =
-                Bitmap.createBitmap(
-                        DESIRED_AVATAR_SIZE, DESIRED_AVATAR_SIZE, Bitmap.Config.ARGB_8888);
-        callback.getValue().onResult(bitmap);
-
-        Avatar avatarData = mSheetAccountItems.get(0).model.get(AVATAR);
-        assertEquals("incorrect avatar bitmap", bitmap, avatarData.mAvatar);
-        assertEquals("incorrect avatar name", CARL.getName(), avatarData.mName);
-        assertEquals("incorrect avatar size", DESIRED_AVATAR_SIZE, avatarData.mAvatarSize);
     }
 
     @Test
--- chrome/browser/ui/android/webid/java/src/org/chromium/chrome/browser/ui/android/webid/data/Account.java
+++ chrome/browser/ui/android/webid/java/src/org/chromium/chrome/browser/ui/android/webid/data/Account.java
@@ -5,13 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.android.webid.data;
 
+import android.graphics.Bitmap;
+
 import org.jni_zero.CalledByNative;
 
 import org.chromium.url.GURL;
 
 /**
- * This class holds the data used to represent a selectable account in the
- * Account Selection sheet.
+ * This class holds the data used to represent a selectable account in the Account Selection sheet.
  */
 public class Account {
     private final String mId;
@@ -19,13 +20,16 @@ public class Account {
     private final String mName;
     private final String mGivenName;
     private final GURL mPictureUrl;
+    private final Bitmap mPictureBitmap;
     private final boolean mIsSignIn;
 
     /**
      * @param id The account ID.
      * @param email Email shown to the user.
+     * @param name Full name.
      * @param givenName Given name.
-     * @param picture picture URL of the avatar shown to the user.
+     * @param pictureUrl picture URL of the avatar shown to the user.
+     * @param pictureBitmap the Bitmap for the picture in pictureUrl.
      * @param isSignIn whether this account is a sign in or a sign up.
      */
     @CalledByNative
@@ -35,12 +39,14 @@ public class Account {
             String name,
             String givenName,
             GURL pictureUrl,
+            Bitmap pictureBitmap,
             boolean isSignIn) {
         mId = id;
         mEmail = email;
         mName = name;
         mGivenName = givenName;
         mPictureUrl = pictureUrl;
+        mPictureBitmap = pictureBitmap;
         mIsSignIn = isSignIn;
     }
 
@@ -58,6 +64,10 @@ public class Account {
 
     public GURL getPictureUrl() {
         return mPictureUrl;
+    }
+
+    public Bitmap getPictureBitmap() {
+        return mPictureBitmap;
     }
 
     public boolean isSignIn() {
