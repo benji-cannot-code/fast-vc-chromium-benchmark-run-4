f83b535ac7c7cb9ab6f37b489a1ced3ee88ce048
Ningxin Hu <ningxin.hu@intel.com>
2024-05-30 00:25:10 +0000

    WebNN: First query GENERIC_ML adapters for NPU device type
    When creating a context for NPU device type, DirectML implementation
    should first query adapters supporting the generic machine learning
    property. If there are no generic machine learning adapters available,
    the implementation could query the core compute adapters. The core
    compute adapters may be GPU that should be filtered out.
    
    To use the `DXCORE_ADAPTER_ATTRIBUTE_D3D12_GENERIC_ML` declaration, this
    CL uses the latest `dxcore.h` header from third_party DirectX headers
    repo. This CL also creates a `dxguids` target in DirectX headers repo
    that defines the GUID for `DXCORE_ADAPTER_ATTRIBUTE_D3D12_GENERIC_ML`
    rather than linking to `dxguid.lib` of Windows SDK which may not include
    that GUID definition.
    
    Bug: 341947682
    Change-Id: I9f0b42708fdb8e4eb0e0c69b5d638fbd52bffa41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557241
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1307804}

--- services/webnn/dml/platform_functions.h
+++ services/webnn/dml/platform_functions.h
@@ -13,9 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_native_library.h"
 #include "third_party/microsoft_dxheaders/include/directml.h"
 #include "third_party/microsoft_dxheaders/src/include/directx/d3d12.h"
-
-// Windows SDK headers should be included after DirectX headers.
-#include <dxcore.h>
+#include "third_party/microsoft_dxheaders/src/include/directx/dxcore.h"
 
 namespace webnn::dml {
 
