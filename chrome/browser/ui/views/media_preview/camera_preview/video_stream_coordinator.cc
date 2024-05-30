c10e7ef8aa1d7faeb9e1cf4066217b48518452d8
ahmedmoussa <ahmedmoussa@google.com>
2024-05-30 22:09:48 +0000

    [media-preview] Draw preview badge / icon on top of video frames
    Bug: b:342492508
    Change-Id: Id1b17058ff3a7859fded98d0d38344765b79132a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5571758
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Bryant Chandler <bryantchandler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308307}

--- chrome/browser/ui/views/media_preview/camera_preview/video_stream_coordinator.cc
+++ chrome/browser/ui/views/media_preview/camera_preview/video_stream_coordinator.cc
@@ -9,19 +9,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "chrome/browser/ui/views/media_preview/camera_preview/preview_badge.h"
 #include "chrome/browser/ui/views/media_preview/camera_preview/video_format_comparison.h"
 #include "chrome/browser/ui/views/media_preview/camera_preview/video_stream_view.h"
 #include "chrome/browser/ui/views/media_preview/media_preview_metrics.h"
 #include "content/public/browser/context_factory.h"
 #include "media/capture/video_capture_types.h"
+#include "ui/views/layout/box_layout_view.h"
+#include "ui/views/layout/fill_layout.h"
 
 VideoStreamCoordinator::VideoStreamCoordinator(
     views::View& parent_view,
     media_preview_metrics::Context metrics_context)
     : metrics_context_(metrics_context) {
+  auto* container = parent_view.AddChildView(std::make_unique<views::View>());
+  container->SetLayoutManager(std::make_unique<views::FillLayout>());
+
   video_stream_view_ =
-      parent_view.AddChildView(std::make_unique<VideoStreamView>());
+      container->AddChildView(std::make_unique<VideoStreamView>());
   scoped_observation_.Observe(video_stream_view_);
+
+  auto* badge_holder =
+      container->AddChildView(std::make_unique<views::BoxLayoutView>());
+  badge_holder->SetOrientation(views::BoxLayout::Orientation::kHorizontal);
+  badge_holder->SetMainAxisAlignment(views::BoxLayout::MainAxisAlignment::kEnd);
+  badge_holder->SetCrossAxisAlignment(
+      views::BoxLayout::CrossAxisAlignment::kStart);
+
+  preview_badge_view_ =
+      badge_holder->AddChildView(preview_badge::CreatePreviewBadge());
+  preview_badge_view_->SetVisible(false);
 }
 
 VideoStreamCoordinator::~VideoStreamCoordinator() {
@@ -66,6 +83,7 @@ void VideoStreamCoordinator::OnCameraVideoFrame(
 
   if (video_stream_view_) {
     video_stream_view_->ScheduleFramePaint(std::move(frame));
+    preview_badge_view_->SetVisible(!has_permission_);
   }
 
   if (!video_stream_start_time_) {
@@ -81,6 +99,7 @@ void VideoStreamCoordinator::OnFatalErrorOrDisconnection() {
   video_stream_start_time_.reset();
   if (video_stream_view_) {
     video_stream_view_->ClearFrame();
+    preview_badge_view_->SetVisible(false);
   }
 }
 
@@ -104,6 +123,7 @@ void VideoStreamCoordinator::StopInternal(
     // This order is needed as to clear all frame references before resetting
     // the buffers which happens within Close().
     video_stream_view_->ClearFrame();
+    preview_badge_view_->SetVisible(false);
   }
 
   if (video_frame_handler_) {
