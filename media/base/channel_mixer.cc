04a72ad50414f051101b9c370d8d1b96c330715d
Evan Liu <evliu@google.com>
2024-05-30 20:41:37 +0000

    Remove ChannelMixer constructor
    This CL removes the ChannelMixer constructor that takes in ChannelLayouts without the corresponding channel count.
    
    Bug: 336222612
    Change-Id: I2986d4ac48b97ff22282fbf7bd28e03405b6fbed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494544
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Erik Jensen <rkjnsn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308252}

--- media/base/channel_mixer.cc
+++ media/base/channel_mixer.cc
@@ -17,13 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 ChannelMixer::ChannelMixer(ChannelLayout input_layout,
-                           ChannelLayout output_layout)
-    : ChannelMixer(input_layout,
-                   ChannelLayoutToChannelCount(input_layout),
-                   output_layout,
-                   ChannelLayoutToChannelCount(output_layout)) {}
-
-ChannelMixer::ChannelMixer(ChannelLayout input_layout,
                            int input_channels,
                            ChannelLayout output_layout,
                            int output_channels) {
