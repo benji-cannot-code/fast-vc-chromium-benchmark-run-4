d9711a5a1da6310f0c883fb84e4c17972d301151
Xi Han <hanxi@google.com>
2024-05-30 14:31:30 +0000

    [TabResumption] Implement TabModel data fetcher for Android.
    In this CL:
    1. Implement the TabModel data fetcher of Android, and use it when
       feature parameter "fetch_local_tabs_backend" is enabled.
    2. Fix the default value of kTabResumptionModule_enable_v2_ml in
       about_flags.
    
    Bug: 343209609, 337858147
    Change-Id: I09c010bcd6c161b9faaeb76d97e2aad2c9ad483f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577298
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307988}

--- chrome/browser/visited_url_ranking/visited_url_ranking_service_factory.cc
+++ chrome/browser/visited_url_ranking/visited_url_ranking_service_factory.cc
@@ -40,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX) || \
     BUILDFLAG(IS_CHROMEOS)
 #include "chrome/browser/visited_url_ranking/desktop_tab_model_url_visit_data_fetcher.h"
+#elif BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/visited_url_ranking/android_tab_model_url_visit_data_fetcher.h"
 #endif
 
 namespace visited_url_ranking {
@@ -100,6 +102,11 @@ VisitedURLRankingServiceFactory::BuildServiceInstanceForBrowserContext(
   data_fetchers.emplace(
       Fetcher::kTabModel,
       std::make_unique<visited_url_ranking::DesktopTabModelURLVisitDataFetcher>(
+          profile));
+#elif BUILDFLAG(IS_ANDROID)
+  data_fetchers.emplace(
+      Fetcher::kTabModel,
+      std::make_unique<visited_url_ranking::AndroidTabModelURLVisitDataFetcher>(
           profile));
 #endif
 
