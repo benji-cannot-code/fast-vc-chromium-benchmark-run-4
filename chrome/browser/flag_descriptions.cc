ec8a02b3ddd09ce28ed4acd84a82fd9d46598ff1
Koji Ishii <kojii@chromium.org>
2024-05-30 22:28:49 +0000

    [text-box-trim] Add a switch to `about://flags`
    This patch adds a switch for the `text-box-trim` property to
    the `about://flags` page.
    
    Bug: 40254880
    Change-Id: Iae8fa1d457ad2992b8d730ed8212fda385e726b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584537
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308324}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -179,6 +179,10 @@ const char kCssGamutMappingName[] = "CSS Gamut Mapping";
 const char kCssGamutMappingDescription[] =
     "Enable experimental CSS gamut mapping implementation.";
 
+const char kCssTextBoxTrimName[] = "CSS text-box-trim";
+const char kCssTextBoxTrimDescription[] =
+    "Enable experimental CSS text-box-trim property.";
+
 const char kCustomizeChromeSidePanelExtensionsCardName[] =
     "Customize Chrome Side Panel Extension Card";
 const char kCustomizeChromeSidePanelExtensionsCardDescription[] =
