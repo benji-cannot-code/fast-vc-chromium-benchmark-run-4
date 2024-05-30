382ab7ae3faa3f6b2950e03a0f54b7a318b7bc46
Andy Phan <andyphan@chromium.org>
2024-05-30 01:01:49 +0000

    [PDF Ink Signatures] Set highlighter brush opacity
    Set the brush opacity to a lower value when the highlighter brush is
    selected.
    
    Bug: 335524382
    Change-Id: Ia361cc8b00278b95bf726568e17e9c9ad903c50b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5565244
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1307815}

--- pdf/ink_module_unittest.cc
+++ pdf/ink_module_unittest.cc
@@ -133,7 +133,6 @@ TEST_F(InkModuleTest, HandleSetAnnotationBrushMessagePen) {
   const InkBrush& ink_brush = brush->GetInkBrush();
   EXPECT_EQ(SkColorSetRGB(10, 255, 50), ink_brush.GetColorForTesting());
   EXPECT_EQ(8.0f, ink_brush.GetSizeForTesting());
-  // TODO(crbug.com/335524382): Update with real value.
   EXPECT_EQ(1.0f, ink_brush.GetOpacityForTesting());
 }
 
@@ -155,8 +154,7 @@ TEST_F(InkModuleTest, HandleSetAnnotationBrushMessageHighlighter) {
   const InkBrush& ink_brush = brush->GetInkBrush();
   EXPECT_EQ(SkColorSetRGB(240, 133, 0), ink_brush.GetColorForTesting());
   EXPECT_EQ(4.5f, ink_brush.GetSizeForTesting());
-  // TODO(crbug.com/335524382): Update with real value.
-  EXPECT_EQ(1.0f, ink_brush.GetOpacityForTesting());
+  EXPECT_EQ(0.4f, ink_brush.GetOpacityForTesting());
 }
 
 // Verify that brushes with zero color values can be set as the annotation
@@ -177,7 +175,6 @@ TEST_F(InkModuleTest, HandleSetAnnotationBrushMessageColorZero) {
   const InkBrush& ink_brush = brush->GetInkBrush();
   EXPECT_EQ(SkColorSetRGB(0, 0, 0), ink_brush.GetColorForTesting());
   EXPECT_EQ(4.5f, ink_brush.GetSizeForTesting());
-  // TODO(crbug.com/335524382): Update with real value.
   EXPECT_EQ(1.0f, ink_brush.GetOpacityForTesting());
 }
 
@@ -200,8 +197,7 @@ TEST_F(InkModuleTest, HandleSetAnnotationBrushMessageSizeZeroTranslation) {
   const InkBrush& ink_brush = brush->GetInkBrush();
   EXPECT_EQ(SkColorSetRGB(255, 255, 255), ink_brush.GetColorForTesting());
   EXPECT_EQ(1.0f, ink_brush.GetSizeForTesting());
-  // TODO(crbug.com/335524382): Update with real value.
-  EXPECT_EQ(1.0f, ink_brush.GetOpacityForTesting());
+  EXPECT_EQ(0.4f, ink_brush.GetOpacityForTesting());
 }
 
 // Verify that the size of the brush is properly translated. The PDF extension's
@@ -222,8 +218,7 @@ TEST_F(InkModuleTest, HandleSetAnnotationBrushMessageSizeOneTranslation) {
   const InkBrush& ink_brush = brush->GetInkBrush();
   EXPECT_EQ(SkColorSetRGB(255, 255, 255), ink_brush.GetColorForTesting());
   EXPECT_EQ(8.0f, ink_brush.GetSizeForTesting());
-  // TODO(crbug.com/335524382): Update with real value.
-  EXPECT_EQ(1.0f, ink_brush.GetOpacityForTesting());
+  EXPECT_EQ(0.4f, ink_brush.GetOpacityForTesting());
 }
 
 TEST_F(InkModuleTest, HandleSetAnnotationModeMessage) {
