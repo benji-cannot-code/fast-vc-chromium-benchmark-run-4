dd9f6690b5246980880850d1183b426a5d3d3f0d
Calder Kitagawa <ckitagawa@chromium.org>
2024-05-30 23:52:02 +0000

    [Hub] Set a default value for overview color supplier pre-Hub
    Set a default color value for the OverviewColorSupplier when Hub isn't
    created so that we don't early return in ToolbarColorObserverManager#notifyToolbarColorChanged().
    
    Demo:
    https://drive.google.com/file/d/1dyBhPRcbkPXUqTxe58ds6QIzz4JZkKz7/view?usp=sharing
    
    Bug: 343659073, 337673264
    Change-Id: I1bce95755bef6865d7c2dc78c20f726b0b352401
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582375
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308366}

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/ToolbarTest.java
@@ -53,6 +53,7 @@ import org.chromium.chrome.browser.layouts.LayoutTestUtils;
 import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tabbed_mode.TabbedRootUiCoordinator;
+import org.chromium.chrome.browser.tasks.tab_management.TabUiThemeUtil;
 import org.chromium.chrome.browser.toolbar.top.TabStripTransitionCoordinator;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
@@ -282,6 +283,13 @@ public class ToolbarTest {
                                         .getContainerViewForTesting()
                                         .getHeight(),
                                 Matchers.equalTo(toolbarLayoutHeight)));
+        CriteriaHelper.pollUiThread(
+                () ->
+                        Criteria.checkThat(
+                                activity.getToolbarManager()
+                                        .getStatusBarColorController()
+                                        .getStatusBarColorWithoutStatusIndicator(),
+                                Matchers.equalTo(activity.getToolbarManager().getPrimaryColor())));
 
         TabStripTransitionCoordinator.setMinScreenWidthForTesting(1);
         TestThreadUtils.runOnUiThreadBlocking(
@@ -296,6 +304,15 @@ public class ToolbarTest {
                                         .getContainerViewForTesting()
                                         .getHeight(),
                                 Matchers.equalTo(toolbarLayoutHeight + tabStripHeightResource)));
+        CriteriaHelper.pollUiThread(
+                () ->
+                        Criteria.checkThat(
+                                activity.getToolbarManager()
+                                        .getStatusBarColorController()
+                                        .getStatusBarColorWithoutStatusIndicator(),
+                                Matchers.equalTo(
+                                        TabUiThemeUtil.getTabStripBackgroundColor(
+                                                activity, /* isIncognito= */ false))));
     }
 
     private void checkTabStripHeightOnUiThread(int tabStripHeight) {
