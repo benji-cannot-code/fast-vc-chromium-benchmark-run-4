7e8a4496ef08714370f78e5e71cf205412e5c6b9
Minoru Chikamune <chikamune@chromium.org>
2024-05-30 00:22:03 +0000

    Add histogram to track EstableGpuChannelSync
    This CL does not intend to start working on this area, but I'm curious
    about the latency of the first call of Gpu::EstablishGpuChannelSync()
    since I observed the first call is very slow.
    
    Bug: 40208065
    Change-Id: I929faaf4fe81e95500c15532ba8cc787d6a11140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568923
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307803}

--- services/viz/public/cpp/gpu/gpu.cc
+++ services/viz/public/cpp/gpu/gpu.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread_checker.h"
+#include "base/timer/elapsed_timer.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -352,6 +354,7 @@ scoped_refptr<gpu::GpuChannelHost> Gpu::EstablishGpuChannelSync() {
   if (channel)
     return channel;
 
+  base::ElapsedTimer timer;
   SCOPED_UMA_HISTOGRAM_TIMER("GPU.EstablishGpuChannelSyncTime");
   base::WaitableEvent event(base::WaitableEvent::ResetPolicy::MANUAL,
                             base::WaitableEvent::InitialState::SIGNALED);
@@ -362,6 +365,12 @@ scoped_refptr<gpu::GpuChannelHost> Gpu::EstablishGpuChannelSync() {
   // from calls to EstablishGpuChannel() before we return from here.
   pending_request_->FinishOnMain();
 
+  static bool first_run_in_process = true;
+  if (first_run_in_process) {
+    first_run_in_process = false;
+    base::UmaHistogramTimes("GPU.EstablishGpuChannelSyncTime.FirstRun",
+                            timer.Elapsed());
+  }
   return gpu_channel_;
 }
 
