cb8caa00c33e5450dab394bebf95e3b3968623ee
Hidehiko Abe <hidehiko@chromium.org>
2024-05-30 17:00:16 +0000

    Move CheckProfileForSanity to UserManagerDelegateImpl.
    To decouple the dependency from ChromeUserManagerImpl more.
    
    TEST=Tryjob
    
    Bug: 278643115
    Change-Id: I4816f1429ed22ea805b2475007ff2a8033ebabf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584420
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308077}

--- components/user_manager/fake_user_manager_delegate.cc
+++ components/user_manager/fake_user_manager_delegate.cc
@@ -28,4 +28,8 @@ bool FakeUserManagerDelegate::IsUserSessionRestoreInProgress() {
   return false;
 }
 
+void FakeUserManagerDelegate::CheckProfileOnLogin(const User& user) {
+  // Do nothing.
+}
+
 }  // namespace user_manager
