4d3df2385c960f3bdf8f6ad2a0c5e6b0b0243ca1
Daniel Rubery <drubery@chromium.org>
2024-05-30 22:32:16 +0000

    Add user gesture argument to IdentifyReferrerChain
    This function is meant to be shared between desktop and Android, which
    vary slightly in their user gesture requirements. So this CL adds
    another argument to the helper function.
    
    Bug: 324293877
    Change-Id: I5a5a12f5eae84a20fd58ec6ee2259388f640327f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5583197
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308329}

--- chrome/browser/safe_browsing/download_protection/download_request_maker.cc
+++ chrome/browser/safe_browsing/download_protection/download_request_maker.cc
@@ -135,7 +135,8 @@ DownloadRequestMaker::CreateFromFileSystemAccess(
     resource.set_referrer(ShortURLForReporting(item.frame_url));
 
   std::unique_ptr<ReferrerChainData> referrer_chain_data =
-      IdentifyReferrerChain(item);
+      IdentifyReferrerChain(item,
+                            service->GetDownloadAttributionUserGestureLimit());
 
   return std::make_unique<DownloadRequestMaker>(
       binary_feature_extractor, item.browser_context,
