7af91aa66967b18dea20e9d8a8c19d94365352bf
David Bertoni <dbertoni@chromium.org>
2024-05-30 18:50:21 +0000

    [Code Health] Remove a use of base::SupportsWeakPtr. (net/dns)
    Bug: 40485134
    Change-Id: Idf8d8c1a1ffae68d20b593db046d536feaa8ed78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581226
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308167}

--- net/dns/mock_host_resolver.h
+++ net/dns/mock_host_resolver.h
@@ -88,9 +88,7 @@ int ParseAddressList(std::string_view host_list,
 // rule, otherwise DCHECKs will fire.
 
 // Base class shared by MockHostResolver and MockCachingHostResolver.
-class MockHostResolverBase
-    : public HostResolver,
-      public base::SupportsWeakPtr<MockHostResolverBase> {
+class MockHostResolverBase : public HostResolver {
  private:
   class RequestImpl;
   class ProbeRequestImpl;
@@ -458,6 +456,8 @@ class MockHostResolverBase
   scoped_refptr<State> state_;
 
   THREAD_CHECKER(thread_checker_);
+
+  base::WeakPtrFactory<MockHostResolverBase> weak_ptr_factory_{this};
 };
 
 class MockHostResolver : public MockHostResolverBase {
