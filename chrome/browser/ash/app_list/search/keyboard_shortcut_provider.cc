6e5217b194ef4485ac6587d75a9d7958f0f763d8
CJ Huang <chenjih@google.com>
2024-05-30 15:12:13 +0000

    launcher: defer LSS access - key shortcut [4/4]
    It is the effort to defer the launcher access to LSS to post user login.
    After all related providers refactored, we should expect no LSS access
    from launcher during the user login, which help to improve the user
    experience.
    
    This CL refactors keyboard shortcut provider. The search handler is
    shared between launcher search and key shortcut app search.
    No logical change other than deferring the initialization.
    Have verified on DUT both launcher search and keyshortcut app search
    works as before.
    
    Have also verified that after this change, LSS is only launched after
    `OnUserSessionStartUpTaskCompleted` is fired, which is post user login.
    
    Bug: b/259443606
    Test: Existing + manually tested on DUT.
    Change-Id: I42d2d2355745a8abe9b4f026ba362646d343aa37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582792
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308011}

--- chrome/browser/ash/app_list/search/keyboard_shortcut_provider.cc
+++ chrome/browser/ash/app_list/search/keyboard_shortcut_provider.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/components/string_matching/tokenized_string.h"
 #include "chromeos/ash/components/string_matching/tokenized_string_match.h"
+#include "components/session_manager/core/session_manager.h"
+#include "components/session_manager/core/session_manager_observer.h"
 #include "content/public/browser/storage_partition.h"
 
 namespace app_list {
@@ -57,19 +59,43 @@ void RemoveDisabledShortcuts(
 
 KeyboardShortcutProvider::KeyboardShortcutProvider(Profile* profile)
     : SearchProvider(SearchCategory::kHelp), profile_(profile) {
-  DCHECK(profile_);
+  CHECK(profile_);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  auto* shortcuts_app_manager_factory =
-      ash::shortcut_ui::ShortcutsAppManagerFactory::GetForBrowserContext(
-          profile_);
-  // The factory is null in unit tests.
-  if (shortcuts_app_manager_factory) {
-    search_handler_ = shortcuts_app_manager_factory->search_handler();
+  auto* session_manager = session_manager::SessionManager::Get();
+  if (session_manager->IsUserSessionStartUpTaskCompleted()) {
+    // If user session start up task has completed, the initialization can
+    // start.
+    Initialize();
+  } else {
+    // Wait for the user session start up task completion to prioritize
+    // resources for them.
+    session_manager_observation_.Observe(session_manager);
   }
 }
 
 KeyboardShortcutProvider::~KeyboardShortcutProvider() = default;
+
+void KeyboardShortcutProvider::Initialize(
+    ash::shortcut_ui::SearchHandler* fake_search_handler) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  // Initialization is happening, so we no longer need to wait for user session
+  // start up task completion.
+  session_manager_observation_.Reset();
+
+  // Use fake search handler if provided in tests, or get it from
+  // `shortcuts_app_manager`.
+  if (fake_search_handler) {
+    search_handler_ = fake_search_handler;
+    return;
+  }
+
+  auto* shortcuts_app_manager =
+      ash::shortcut_ui::ShortcutsAppManagerFactory::GetForBrowserContext(
+          profile_);
+  CHECK(shortcuts_app_manager);
+  search_handler_ = shortcuts_app_manager->search_handler();
+}
 
 void KeyboardShortcutProvider::Start(const std::u16string& query) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -100,6 +126,10 @@ void KeyboardShortcutProvider::StopQuery() {
 
 ash::AppListSearchResultType KeyboardShortcutProvider::ResultType() const {
   return ash::AppListSearchResultType::kKeyboardShortcut;
+}
+
+void KeyboardShortcutProvider::OnUserSessionStartUpTaskCompleted() {
+  Initialize();
 }
 
 void KeyboardShortcutProvider::OnShortcutsSearchComplete(
