dd8d3060ce1292d6dc046bb85526cae028c066db
Yichen <yichenz@chromium.org>
2024-05-30 20:10:55 +0000

    ui: Remove ColorProvider::HasMixers()
    Remove ColorProvider::HasMixers() since color map is lazily
    created thus no need to check if color provider is initialized
    correctly.
    
    BUG=340815321
    
    Change-Id: I7ade0788b2557b688c535b3e5ee59ac7459c2ce0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577523
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308234}

--- ui/color/color_provider.cc
+++ ui/color/color_provider.cc
@@ -89,8 +89,6 @@ class ColorProvider::ColorProviderInternal {
     return (it == mixers_.cend()) ? nullptr : &(*it);
   }
 
-  bool HasMixers() const { return !mixers_.empty(); }
-
   void SetColorForTesting(ColorId id, SkColor color) { color_map_[id] = color; }
 
   void GenerateColorMapForTesting() {
@@ -138,10 +136,6 @@ ColorMixer& ColorProvider::AddPostprocessingMixer() {
 
 SkColor ColorProvider::GetColor(ColorId id) const {
   return internal_->GetColor(id);
-}
-
-bool ColorProvider::HasMixers() const {
-  return internal_->HasMixers();
 }
 
 void ColorProvider::SetColorForTesting(ColorId id, SkColor color) {
