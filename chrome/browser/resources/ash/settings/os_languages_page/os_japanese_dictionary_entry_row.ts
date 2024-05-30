124a5bfb7ce598d7a537cc46d963d0dccc951014
jhtin <jhtin@chromium.org>
2024-05-30 01:34:11 +0000

    JP SystemPK: JPDict entry rows can edit the entry it contains.
    Created a UserDataServiceProvider which will let us use the mojom remote
    in different components without creating a new receiver/remote every
    single time so that entry-rows can access mojom.
    
    Bug: b:340393256
    Change-Id: I5be2f69f4bfaebb26281363837e12885fbfe9ecf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546840
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307824}

--- chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @fileoverview A row that represents an editable Japanese dictionary entry.
  */
 
+import {CrInputElement} from 'chrome://resources/ash/common/cr_elements/cr_input/cr_input.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {JapaneseDictionaryEntry} from '../mojom-webui/user_data_japanese_dictionary.mojom-webui.js';
 
 import {getTemplate} from './os_japanese_dictionary_entry_row.html.js';
+import {UserDataServiceProvider} from './user_data_service_provider.js';
 
 class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
   static get is() {
@@ -24,15 +26,48 @@ class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
 
   static get properties() {
     return {
+      // TODO(b/340393256): Handle deserialization.
+      dictId: {
+        type: Number,
+      },
+      index: {
+        type: Number,
+      },
       entry: {
         type: Object,
       },
     };
   }
 
+  // The ID of the Japanese User Dictionary that the entry is part of.
+  dictId: bigint;
+
+  // Index of the entry within the dictionary.
+  index: number;
+
   // The JapaneseDictionary entry that represents a key value pair and
   // attributes.
   entry: JapaneseDictionaryEntry;
+
+  private saveReading_(e: Event): void {
+    this.entry.key = (e.target as CrInputElement).value;
+    this.saveEntryToDictionary_();
+  }
+
+  private saveWord_(e: Event): void {
+    this.entry.value = (e.target as CrInputElement).value;
+    this.saveEntryToDictionary_();
+  }
+
+  private saveComment_(e: Event): void {
+    this.entry.comment = (e.target as CrInputElement).value;
+    this.saveEntryToDictionary_();
+  }
+
+  private async saveEntryToDictionary_(): Promise<void> {
+    UserDataServiceProvider.getRemote().editJapaneseDictionaryEntry(
+        this.dictId, this.index, this.entry);
+  }
 }
 
 customElements.define(
---
+++ chrome/browser/resources/ash/settings/os_languages_page/user_data_service_provider.ts
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Singleton that provides a remote for the UserDataService.
+ */
+import {InputMethodUserDataService, InputMethodUserDataServiceInterface} from '../mojom-webui/input_method_user_data.mojom-webui.js';
+
+export class UserDataServiceProvider {
+  private static instance_: InputMethodUserDataServiceInterface|null = null;
+
+  static getRemote(): InputMethodUserDataServiceInterface {
+    if (UserDataServiceProvider.instance_ === null) {
+      UserDataServiceProvider.instance_ =
+          InputMethodUserDataService.getRemote();
+    }
+    return UserDataServiceProvider.instance_;
+  }
+}
