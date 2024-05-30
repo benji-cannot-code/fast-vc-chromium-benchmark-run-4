06c4adcd130b68c990ca2f1d2d03317c468daf3e
Claudio DeSouza <cdesouza@chromium.org>
2024-05-30 12:42:16 +0000

    Replace StringPiece with std::string_view in //c/b/subresource_filter
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //chrome/browser/subresource_filter
    
    Bug: 40506050
    Change-Id: I711a2f38c725d8a46721d7827d22b4e8a9591b8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555564
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307951}

--- chrome/browser/subresource_filter/subresource_filter_dns_alias_browsertest.cc
+++ chrome/browser/subresource_filter/subresource_filter_dns_alias_browsertest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/strings/pattern.h"
 #include "base/strings/strcat.h"
-#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/safe_browsing/test_safe_browsing_database_helper.h"
