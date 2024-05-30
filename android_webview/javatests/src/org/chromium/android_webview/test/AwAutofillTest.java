d4c2eeb4457af5fd619accefb3ac23d5fba28e7e
Sky Malice <skym@chromium.org>
2024-05-30 22:16:16 +0000

    Revert "Add AndroidAutofillDirectFormSubmission fieldtrial entry"
    This reverts commit d0104962fd60e34bf71b1b34656e12b81b204daa.
    
    Reason for revert: AwAutofillTest failures. In blamelist ofr https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20WebView%20P%20(dbg)/15783/overview
    
    Original change's description:
    > Add AndroidAutofillDirectFormSubmission fieldtrial entry
    >
    > Bug: 40944109
    > Change-Id: Ic245b67b1054f6366030b8c600c4ff0b86b38c14
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5434056
    > Reviewed-by: Dominic Battre <battre@chromium.org>
    > Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1308109}
    
    Bug: 40944109
    Change-Id: I27cc5b651302aa83af7b12e4c390bbf94fb76085
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586456
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Sky Malice <skym@chromium.org>
    Auto-Submit: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308313}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
@@ -339,7 +339,10 @@ public class AwAutofillTest extends AwParameterizedTest {
                     mTest.waitForCallbackAndVerifyTypes(
                             mCnt,
                             new Integer[] {
-                                AUTOFILL_VALUE_CHANGED, AUTOFILL_COMMIT, AUTOFILL_CANCEL
+                                AUTOFILL_VIEW_EXITED,
+                                AUTOFILL_VALUE_CHANGED,
+                                AUTOFILL_COMMIT,
+                                AUTOFILL_CANCEL
                             });
         }
 
@@ -1322,7 +1325,11 @@ public class AwAutofillTest extends AwParameterizedTest {
         waitForCallbackAndVerifyTypes(
                 cnt,
                 new Integer[] {
-                    AUTOFILL_VALUE_CHANGED, AUTOFILL_VALUE_CHANGED, AUTOFILL_COMMIT, AUTOFILL_CANCEL
+                    AUTOFILL_VIEW_EXITED,
+                    AUTOFILL_VALUE_CHANGED,
+                    AUTOFILL_VALUE_CHANGED,
+                    AUTOFILL_COMMIT,
+                    AUTOFILL_CANCEL
                 });
         assertEquals(SubmissionSource.PROBABLY_FORM_SUBMITTED, mSubmissionSource);
     }
