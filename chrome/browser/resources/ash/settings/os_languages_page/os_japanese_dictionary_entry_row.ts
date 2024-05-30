dbe60567614b057aec6aeec3c3e14b8b773e4ad9
jhtin <jhtin@chromium.org>
2024-05-30 01:34:21 +0000

    JP SystemPK: Dictionary editor can now add an entry.
    Recording:
    http://screencast/cast/NTg4OTM2ODY4OTQ3NTU4NHxiMTc5ZGEyZC01Mg
    
    Dictionary editor needs to have the functionality to add a "local"
    entry that then gets synced onto disk when it becomes a "valid" entry
    (i.e.  not empty). This cl adds the ability to create a new entry
    locally without syncing it yet and then handle an "add" api call when
    the entry is filled in.
    
    In order to do this without keeping track of individual sync states in
    parent components, we pass the "synced" number of entries down the
    components tree until the entry-row finally calculates whether it is a
    locally added entry or not.
    
    The "add" button also only allows you to add one entry at a time, so
    it disappears when there is already a work in progress locally-added
    entry, and then reappears on sync.
    
    Note: The only way to sync right now is to click a button on the main
    japanese dictionary page. In the future this will be fired via an
    event that is propagated from the entry-rows.
    
    Bug: b:340393256
    Change-Id: I09a234b4e5d6d436ff2785d9b186d4ed137295de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546842
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: jhtin <jhtin@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1307825}

--- chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
+++ chrome/browser/resources/ash/settings/os_languages_page/os_japanese_dictionary_entry_row.ts
@@ -36,8 +36,14 @@ class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
       entry: {
         type: Object,
       },
+      locallyAdded: {
+        type: Boolean,
+      },
     };
   }
+
+  // Whether the entry needs to be added to the storage.
+  locallyAdded = false;
 
   // The ID of the Japanese User Dictionary that the entry is part of.
   dictId: bigint;
@@ -65,8 +71,26 @@ class OsJapaneseDictionaryEntryRowElement extends PolymerElement {
   }
 
   private async saveEntryToDictionary_(): Promise<void> {
-    UserDataServiceProvider.getRemote().editJapaneseDictionaryEntry(
-        this.dictId, this.index, this.entry);
+    if (this.entry.key === '' || this.entry.value === '') {
+      return;
+    }
+    if (this.locallyAdded) {
+      // Entry does not exist inside the storage, hence we need to use the "add"
+      // function to add this entry.
+      // TODO(b/340393256): Handle possible race condition when two add..Entry
+      // requests are in flight at the same time.
+      const resp =
+          (await UserDataServiceProvider.getRemote().addJapaneseDictionaryEntry(
+               this.dictId, this.entry))
+              .status;
+      // If successful, then the entry is no longer "locally added", since it
+      // also exists inside the storage. Future edits need to be done via the
+      // "edit" api call.
+      this.locallyAdded = !resp.success;
+    } else {
+      UserDataServiceProvider.getRemote().editJapaneseDictionaryEntry(
+          this.dictId, this.index, this.entry);
+    }
   }
 }
 
