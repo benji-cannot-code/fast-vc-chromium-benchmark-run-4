2df716e7dfb20746004e0f32f0e6552e0ec9f316
Mohamed Heikal <mheikal@chromium.org>
2024-05-30 21:40:33 +0000

    Revert "Improve error message for batched tests cascading failures"
    This reverts commit 5ab8a0525e0438708387d191a77cb995174de87c.
    
    Reason for revert: Flakey tests can possibly shadow real failures in a batch.
    
    Original change's description:
    > Improve error message for batched tests cascading failures
    >
    > When a batched test case fails, it can leave chrome in an unknown state
    > for the next test case. This can cause the next test to fail
    > unpredictably with weird error message that do not make it clear what
    > happened. This cl changes behaviour so that all remaining cases in a
    > batch are skipped with a message explaining that another test in the
    > batch failed.
    >
    > Bug: 330927824
    > Change-Id: I8e02420c1a458f544bcdcf5009d4078e45975dd3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577917
    > Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    > Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1308023}
    
    Bug: 330927824
    Change-Id: If25f3ab3b38259aa480726a29d86e2c9d0fe1bc3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586140
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1308292}

--- base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
+++ base/test/android/javatests/src/org/chromium/base/test/BaseJUnit4ClassRunner.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test;
 
-import static org.junit.Assume.assumeTrue;
-
 import android.app.Application;
 import android.app.job.JobScheduler;
 import android.content.Context;
@@ -102,7 +100,6 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
 
     protected final RestrictionSkipCheck mRestrictionSkipCheck = new RestrictionSkipCheck();
     private long mTestStartTimeMs;
-    private String mFailedBatchTestName;
 
     /**
      * Create a BaseJUnit4ClassRunner to run {@code klass} and initialize values.
@@ -293,17 +290,9 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
         BaseJUnit4ClassRunner.getApplication().getSystemService(JobScheduler.class).cancelAll();
     }
 
-    private static boolean isUnitTest(FrameworkMethod testMethod) {
-        Batch annotation = testMethod.getDeclaringClass().getAnnotation(Batch.class);
-        return annotation != null && annotation.value().equals(Batch.UNIT_TESTS);
-    }
-
-    private static boolean isBatchedTest(FrameworkMethod testMethod) {
-        return testMethod.getDeclaringClass().getAnnotation(Batch.class) != null;
-    }
-
     private static void blockUnitTestsFromStartingBrowser(FrameworkMethod testMethod) {
-        if (isUnitTest(testMethod)) {
+        Batch annotation = testMethod.getDeclaringClass().getAnnotation(Batch.class);
+        if (annotation != null && annotation.value().equals(Batch.UNIT_TESTS)) {
             if (testMethod.getAnnotation(RequiresRestart.class) != null) return;
             LibraryLoader.setBrowserProcessStartupBlockedForTesting();
         }
@@ -348,7 +337,6 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
                 onBeforeTestMethod(method);
                 Throwable exception = null;
                 try {
-                    checkBatchedTestFailure(method);
                     // Runs @Rules, then @Befores, then test method.
                     innerStatement.evaluate();
                     performExtraAssertions(method);
@@ -367,25 +355,10 @@ public class BaseJUnit4ClassRunner extends AndroidJUnit4ClassRunner {
                     }
                 }
                 if (exception != null) {
-                    markBatchTestFailed(method);
                     throw exception;
                 }
             }
         };
-    }
-
-    private void markBatchTestFailed(FrameworkMethod method) {
-        if (mFailedBatchTestName == null && isBatchedTest(method) && !isUnitTest(method)) {
-            mFailedBatchTestName = method.getName();
-        }
-    }
-
-    private void checkBatchedTestFailure(FrameworkMethod method) {
-        assumeTrue(
-                "A previous batched test ("
-                        + mFailedBatchTestName
-                        + ") failed leaving chrome in an unknown state. Skipping test.",
-                mFailedBatchTestName == null);
     }
 
     private void onBeforeTestMethod(FrameworkMethod method) {
