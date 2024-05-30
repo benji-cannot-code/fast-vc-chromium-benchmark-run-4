b165a59360ef861581a1093ac0336ac209786c4d
Erik Chen <erikchen@chromium.org>
2024-05-30 21:06:41 +0000

    Modularize chrome/browser/search/background
    This CL has no intended behavior change.
    
    Change-Id: Ibd05d4eeee25494a183c3b6fcbe9fbee41e8bde3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578751
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308270}

--- chrome/browser/search/background/ntp_background_service.cc
+++ chrome/browser/search/background/ntp_background_service.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/strcat.h"
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/search/background/ntp_background.pb.h"
 #include "chrome/browser/search/background/ntp_backgrounds.h"
 #include "components/search/ntp_features.h"
