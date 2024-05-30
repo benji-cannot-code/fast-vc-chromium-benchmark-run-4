265f6561643a004eddce02b5c50450c1602ad9e5
Tina Wang <tinazwang@google.com>
2024-05-30 20:51:25 +0000

    [Notif] Remove provisional bypass condition param
    Remove provisional bypass condition param because checking it will make
    starts_active = false not work for experiments. Add a separate flag for
    the testing purpose. This flag will start working from M127.
    
    Bug: b/322348322
    Change-Id: I69ce3188264fd520991f5f8451ae160f84eb5f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582254
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308261}

--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -366,6 +366,13 @@ BASE_DECLARE_FEATURE(kContentPushNotifications);
 // Feature flag to enable Content Notification experiments.
 BASE_DECLARE_FEATURE(kContentNotificationExperiment);
 
+// Feature flag to enable Content Notification Provisional without any
+// conditions.
+BASE_DECLARE_FEATURE(kContentNotificationProvisionalIgnoreConditions);
+
+// True if Content Notification Provisional is enabled without any conditions.
+bool IsContentNotificationProvisionalIgnoreConditions();
+
 // Feature flag to enable the Large Fakebox design changes.
 BASE_DECLARE_FEATURE(kIOSLargeFakebox);
 
@@ -505,11 +512,6 @@ bool IsContentPushNotificationsSetUpListEnabled();
 
 // YES when the Content Provisional Push Notifications are enabled.
 bool IsContentPushNotificationsProvisionalEnabled();
-
-// TODO(b/322348322): Remove provisional notifications bypass conditions testing
-// flag param. YES when the Content Provisional Push Notifications are enabled
-// and the time based conditions should be ignored.
-bool IsContentPushNotificationsProvisionalBypass();
 
 // YES when the Content Push Notifications Promo is registered with no UI
 // change.
--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -385,6 +385,15 @@ bool IsContentNotificationExperimentEnabled() {
   return base::FeatureList::IsEnabled(kContentNotificationExperiment);
 }
 
+BASE_FEATURE(kContentNotificationProvisionalIgnoreConditions,
+             "ContentNotificationProvisionalIgnoreConditions",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool IsContentNotificationProvisionalIgnoreConditions() {
+  return base::FeatureList::IsEnabled(
+      kContentNotificationProvisionalIgnoreConditions);
+}
+
 BASE_FEATURE(kIOSLargeFakebox,
              "IOSLargeFakebox",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -645,16 +654,7 @@ bool IsContentPushNotificationsSetUpListEnabled() {
 
 bool IsContentPushNotificationsProvisionalEnabled() {
   return (ContentNotificationsExperimentTypeEnabled() ==
-              NotificationsExperimentTypeProvisional ||
-          ContentNotificationsExperimentTypeEnabled() ==
-              NotificationsExperimentTypeProvisionalBypass);
-}
-
-// TODO(b/322348322): Remove provisional notifications bypass conditions testing
-// flag param.
-bool IsContentPushNotificationsProvisionalBypass() {
-  return (ContentNotificationsExperimentTypeEnabled() ==
-          NotificationsExperimentTypeProvisionalBypass);
+          NotificationsExperimentTypeProvisional);
 }
 
 bool IsContentPushNotificationsPromoRegistrationOnly() {
