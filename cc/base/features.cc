c8ce2203d35c78e4444629b35fd86771dfa9885a
Jonathan Ross <jonross@chromium.org>
2024-05-30 20:00:55 +0000

    Remove SlidingWindowForDroppedFrameCounter
    We added this as a way to parameterize sliding windows for dropped
    frames. However we never experimented with it, and it is not required
    for current UKMs nor benchmarking.
    
    This removes the feature and the parameterization in
    DroppedFrameCounter.
    
    Bug: 334977830
    Change-Id: Ia966c65728ef0e8a7cc62dcbbc8210b69e415d85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582419
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308223}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -79,10 +79,6 @@ BASE_FEATURE(kNoPreserveLastMutation,
              "NoPreserveLastMutation",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kSlidingWindowForDroppedFrameCounter,
-             "SlidingWindowForDroppedFrameCounter",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kNormalPriorityImageDecoding,
              "NormalPriorityImageDecoding",
              base::FEATURE_ENABLED_BY_DEFAULT);
