ca768283c0ba44ce9f80aef48b600d7c521d7e96
Marijn Kruisselbrink <mek@chromium.org>
2024-05-31 22:28:08 +0000

    [Shortcuts] Remove kShortcutCreatedEvent and change tests.
    This changes the ShortcutTracker code to make sure to not consider a
    shortcut "created" (or "shown" in kombucha language) until the file is
    not empty. This then eliminates the need for a separate "wait for
    created" verb, as now waiting for the shortcut to be "shown" is enough.
    
    Also adds a CheckShortcut helper verb wrapping around CheckElement to
    make it slightly easier to verify shortcut properties as well as makes
    sure that both that verb and LaunchShortcut internally use InAnyContext,
    avoiding the need to write that in tests.
    
    Finally adds missing [[nodiscard]] annotations to all the shortcut
    specific test verbs.
    
    Bug: 343247628
    Change-Id: I6b620aae47b1f412d947fedc20bdc6a0840380aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586295
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308935}

--- chrome/browser/ui/views/shortcuts/shortcut_integration_browsertest.cc
+++ chrome/browser/ui/views/shortcuts/shortcut_integration_browsertest.cc
@@ -32,11 +32,9 @@ IN_PROC_BROWSER_TEST_F(ShortcutIntegrationBrowserTest, CreateAndLaunch) {
       InstrumentTab(kInitialTabId),
       NavigateWebContents(kInitialTabId, kPageWithIconsUrl),
       InstrumentNextShortcut(kNewShortcutId),
-      ShowAndAcceptCreateShortcutDialog(), WaitForShortcutCreated(),
-      InAnyContext(CheckElement(kNewShortcutId, &GetShortcutPath,
-                                IsShortcutForUrl(kPageWithIconsUrl))),
-      InstrumentNextTab(kNewTabId),
-      InAnyContext(LaunchShortcut(kNewShortcutId)),
+      ShowAndAcceptCreateShortcutDialog(),
+      CheckShortcut(kNewShortcutId, IsShortcutForUrl(kPageWithIconsUrl)),
+      InstrumentNextTab(kNewTabId), LaunchShortcut(kNewShortcutId),
       WaitForWebContentsNavigation(kNewTabId, kPageWithIconsUrl));
 }
 
@@ -48,9 +46,7 @@ IN_PROC_BROWSER_TEST_F(ShortcutIntegrationBrowserTest, CustomTitle) {
       NavigateWebContents(kInitialTabId, kPageWithIconsUrl),
       InstrumentNextShortcut(kNewShortcutId),
       ShowCreateShortcutDialogSetTitleAndAccept(u"Hello World!"),
-      WaitForShortcutCreated(),
-      InAnyContext(CheckElement(kNewShortcutId, &GetShortcutPath,
-                                IsShortcutWithTitle(u"Hello World!"))));
+      CheckShortcut(kNewShortcutId, IsShortcutWithTitle(u"Hello World!")));
 }
 
 }  // namespace shortcuts
