da26d3bb6ff046a72cd6d14c9a5745bd8ba819a1
Dale Curtis <dalecurtis@chromium.org>
2024-05-31 19:04:51 +0000

    Revert "Roll src/third_party/dav1d/libdav1d/ 006ca01d3..805d9e5a8 (78 commits)"
    This reverts commit f621135366ccaad1026f5e377670b94d8a868f5f.
    
    Reason for revert: crbug.com/343965945
    
    Bug: 343965945
    
    Original change's description:
    > Roll src/third_party/dav1d/libdav1d/ 006ca01d3..805d9e5a8 (78 commits)
    >
    > https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/006ca01d387a..805d9e5a8ffc
    >
    > $ git log 006ca01d3..805d9e5a8 --date=short --no-merges --format='%ad %ae %s'
    > 2024-05-24 jb Update NEWS for 1.4.2
    > 2024-05-12 siekyleb ARM64: Minor improvement to symbol decode
    > 2024-05-20 gramner tests: Verify dav1d command line in dav1d_argon.bash
    > 2024-05-19 martin arm64: msac: Explicitly use the ldur instruction
    > 2024-04-06 code CI: Update Android image
    > 2024-04-24 siekyleb ARM64: Various optimizations for symbol decode
    > 2024-05-01 Arpad.Panyik AArch64: Optimize prep_neon function
    > 2024-05-01 Arpad.Panyik AArch64: Optimize jump table calculation of prep_neon
    > 2024-05-01 Arpad.Panyik AArch64: Optimize BTI landing pads of prep_neon
    > 2024-05-13 gramner x86: Update x86inc.asm
    > (...)
    > 2024-02-25 jb Update to 1.4.1
    > 2024-03-08 code Update THANKS.md
    > 2024-03-08 martin arm32: Fix right shifts in the 16bpc iwht implementation
    > 2024-02-25 unlord arm32/msac: Trim C functions, saves 1024 bytes
    > 2024-02-25 unlord arm64/msac: Trim C functions, saves 1392 bytes
    > 2024-02-25 unlord arm: Use -fno-align-functions when building
    > 2024-02-25 unlord arm32/itx: Trim dav1d_inv_wht4_1d_c, saves 68 bytes
    > 2024-02-25 unlord arm64/itx: Trim dav1d_inv_wht4_1d_c, saves 92 bytes
    > 2024-02-24 unlord arm32/itx16: Add 4x4 12bpc NEON wht_wht transform
    > 2024-02-24 unlord arm64/itx16: Add 4x4 12bpc NEON wht_wht transform
    >
    > Created with:
    >   roll-dep src/third_party/dav1d/libdav1d
    >
    > Change-Id: I967de35a36901c3f65ad25aaf74207739e828bc5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582333
    > Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    > Commit-Queue: Wan-Teh Chang <wtc@google.com>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1308043}
    
    Change-Id: I645be9c0d09019e1a94a7ad4c3d66db01b17261f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589786
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1308809}

--- third_party/dav1d/config/linux/arm64/config.h
+++ third_party/dav1d/config/linux/arm64/config.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // #define CONFIG_LOG 1 -- Logging is controlled by Chromium
 
-#define CONFIG_MACOS_KPERF 0
-
 #define ENDIANNESS_BIG 0
 
 #define HAVE_ASM 1
--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.4.2-0-g805d9e5"
+#define DAV1D_VERSION "1.4.0-72-g006ca01"
