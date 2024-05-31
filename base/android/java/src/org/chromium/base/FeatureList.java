82763a09742aa110c43d4e67c742eaee0776e06f
Andrew Grieve <agrieve@chromium.org>
2024-05-31 03:20:21 +0000

    Reset to prior values rather than null for FeatureList.setTestValues()
    E.g. MessageContainerTest.java calls this method from @BeforeClass and
    from within testCustomA11yActions()
    
    Bug: None
    Change-Id: I8425cca80a19e71668a95984458af3f3eb2fb562
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581940
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308440}

--- base/android/java/src/org/chromium/base/FeatureList.java
+++ base/android/java/src/org/chromium/base/FeatureList.java
@@ -138,8 +138,9 @@ public class FeatureList {
      */
     @VisibleForTesting
     public static void setTestValues(TestValues testFeatures) {
+        TestValues prevValues = sTestFeatures;
         sTestFeatures = testFeatures;
-        ResettersForTesting.register(() -> sTestFeatures = null);
+        ResettersForTesting.register(() -> sTestFeatures = prevValues);
     }
 
     /**
