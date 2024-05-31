bb5902d9867ec4b384d977876c94ea2a695fd9f8
Thanh Nguyen <thanhdng@chromium.org>
2024-05-31 05:20:11 +0000

    [Mahi] Use cache in mahi manager
    Uses cache in mahi manager to store summaries and extracted content.
    
    Bug: 338140794
    Change-Id: I4d3ae5296ec312b84c27a8556302c1a9013a5ec8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5583511
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308466}

---
+++ chrome/browser/ash/mahi/fake_mahi_browser_delegate_ash.h
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_MAHI_FAKE_MAHI_BROWSER_DELEGATE_ASH_H_
+#define CHROME_BROWSER_ASH_MAHI_FAKE_MAHI_BROWSER_DELEGATE_ASH_H_
+
+#include "chrome/browser/ash/mahi/mahi_browser_delegate_ash.h"
+namespace ash {
+
+class FakeMahiBrowserDelegateAsh : public MahiBrowserDelegateAsh {
+ public:
+  FakeMahiBrowserDelegateAsh();
+  FakeMahiBrowserDelegateAsh(const FakeMahiBrowserDelegateAsh&) = delete;
+  FakeMahiBrowserDelegateAsh& operator=(const FakeMahiBrowserDelegateAsh&) =
+      delete;
+  ~FakeMahiBrowserDelegateAsh() override;
+
+  void GetContentFromClient(
+      const base::UnguessableToken& client_id,
+      const base::UnguessableToken& page_id,
+      crosapi::mojom::MahiBrowserClient::GetContentCallback callback) override;
+};
+
+}  // namespace ash
+
+#endif  // CHROME_BROWSER_ASH_MAHI_FAKE_MAHI_BROWSER_DELEGATE_ASH_H_
--- chrome/browser/ash/mahi/mahi_manager_impl.h
+++ chrome/browser/ash/mahi/mahi_manager_impl.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/mahi/mahi_ui_controller.h"
 #include "base/memory/raw_ptr.h"
 #include "base/unguessable_token.h"
+#include "chrome/browser/ash/mahi/mahi_browser_delegate_ash.h"
+#include "chrome/browser/ash/mahi/mahi_cache_manager.h"
 #include "chromeos/components/mahi/public/cpp/mahi_manager.h"
 #include "components/manta/mahi_provider.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -70,22 +72,28 @@ class MahiManagerImpl : public chromeos::MahiManager, public SessionObserver {
   void MaybeInitializeAndDiscardPendingRequests();
 
   void OnGetPageContentForSummary(
+      crosapi::mojom::MahiPageInfoPtr request_page_info,
       MahiSummaryCallback callback,
       crosapi::mojom::MahiPageContentPtr mahi_content_ptr);
 
   void OnGetPageContentForQA(
+      crosapi::mojom::MahiPageInfoPtr request_page_info,
       const std::u16string& question,
       MahiAnswerQuestionCallback callback,
       crosapi::mojom::MahiPageContentPtr mahi_content_ptr);
 
-  void OnMahiProviderSummaryResponse(MahiSummaryCallback summary_callback,
-                                     base::Value::Dict dict,
-                                     manta::MantaStatus status);
+  void OnMahiProviderSummaryResponse(
+      crosapi::mojom::MahiPageInfoPtr request_page_info,
+      MahiSummaryCallback summary_callback,
+      base::Value::Dict dict,
+      manta::MantaStatus status);
 
-  void OnMahiProviderQAResponse(const std::u16string& question,
-                                MahiAnswerQuestionCallback callback,
-                                base::Value::Dict dict,
-                                manta::MantaStatus status);
+  void OnMahiProviderQAResponse(
+      crosapi::mojom::MahiPageInfoPtr request_page_info,
+      const std::u16string& question,
+      MahiAnswerQuestionCallback callback,
+      base::Value::Dict dict,
+      manta::MantaStatus status);
 
   std::unique_ptr<PrefChangeRegistrar> pref_change_registrar_;
   base::ScopedObservation<SessionController, SessionObserver>
@@ -97,7 +105,9 @@ class MahiManagerImpl : public chromeos::MahiManager, public SessionObserver {
   crosapi::mojom::MahiPageContentPtr current_panel_content_ =
       crosapi::mojom::MahiPageContent::New();
 
-  GURL current_panel_url_;
+  // Stores metadata of the current content in the panel.
+  crosapi::mojom::MahiPageInfoPtr current_panel_info_ =
+      crosapi::mojom::MahiPageInfo::New();
 
   // Pair of question and their corresponding answer for the current panel
   // content
@@ -105,11 +115,15 @@ class MahiManagerImpl : public chromeos::MahiManager, public SessionObserver {
 
   std::unique_ptr<manta::MahiProvider> mahi_provider_;
 
+  raw_ptr<ash::MahiBrowserDelegateAsh> mahi_browser_delegate_ash_ = nullptr;
+
   // Keeps track of the latest result and code, used for feedback.
   std::u16string latest_summary_;
   chromeos::MahiResponseStatus latest_response_status_;
 
   MahiUiController ui_controller_;
+
+  std::unique_ptr<MahiCacheManager> cache_manager_;
 
   // If true, tries to get content from MediaAppContentManager instead.
   bool media_app_pdf_focused_ = false;
