c89547d90155b01c83a08dd4bc1e7435a63d0cc6
dpapad <dpapad@chromium.org>
2024-05-31 18:36:15 +0000

    TabSearch: Migrate to Lit, part 4.
    In this part migrating:
     - tab_organization_in_progress.ts
     - tab_organization_not_started.ts
     - tab_organization_not_started_image.ts
     - tab_organization_results_actions.ts
    
    Bug: 40943652
    Change-Id: I82c6a070e9a387089040061850eaee46198c3089
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586677
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308784}

--- chrome/browser/resources/tab_search/tab_organization_in_progress.ts
+++ chrome/browser/resources/tab_search/tab_organization_in_progress.ts
@@ -3,14 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './strings.m.js';
-import './tab_organization_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_loading_gradient/cr_loading_gradient.js';
 
-import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './tab_organization_in_progress.html.js';
+import {getCss} from './tab_organization_in_progress.css.js';
+import {getHtml} from './tab_organization_in_progress.html.js';
 
 export interface TabOrganizationInProgressElement {
   $: {
@@ -19,17 +17,17 @@ export interface TabOrganizationInProgressElement {
 }
 
 // Loading state for the tab organization UI.
-export class TabOrganizationInProgressElement extends PolymerElement {
+export class TabOrganizationInProgressElement extends CrLitElement {
   static get is() {
     return 'tab-organization-in-progress';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  private getTitle_(): string {
-    return loadTimeData.getString('inProgressTitle');
+  override render() {
+    return getHtml.bind(this)();
   }
 }
 
