7ec01e7960e3adc88ab59cf7cda78fabba7bad39
Teresa Mao <temao@chromium.org>
2024-05-31 08:01:51 +0000

    Feedback UI: Migrate to Lit, part 2.
    This CL migrates the following .html files to .html.ts.
    - app.html
    - autofill_metadata_app.html
    - system_info_app.html
    
    Bug: 40943652
    Change-Id: Ieb0f528f6aec3237cb7414e0fd31bad1ebd3a97b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586377
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308500}

--- chrome/browser/resources/feedback/app.html.ts
+++ chrome/browser/resources/feedback/app.html.ts
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {AppElement} from './app.js';
+
+export function getHtml(this: AppElement) {
+  return html`<!--_html_template_start_-->
 <div id="content-pane" class="content">
   <p id="free-form-text">$i18n{freeFormText}</p>
   <textarea id="description-text" aria-labelledby="free-form-text"
@@ -100,3 +110,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </button>
   </div>
 </div>
+<!--_html_template_end_-->`;
+}
---
+++ chrome/browser/resources/feedback/system_info_app.html.ts
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {SystemInfoAppElement} from './system_info_app.js';
+
+export function getHtml(this: SystemInfoAppElement) {
+  return html`<!--_html_template_start_-->
+<div id="header">
+  <h1 id="title">$i18n{sysinfoPageTitle}</h1>
+</div>
+<key-value-pair-viewer ?loading="${this.loading_}" .entries="${this.entries_}">
+</key-value-pair-viewer>
+<!--_html_template_end_-->`;
+}
