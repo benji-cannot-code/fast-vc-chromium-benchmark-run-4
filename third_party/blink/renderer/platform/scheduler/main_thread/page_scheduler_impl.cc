702aa4fe5d91ee1cd70bfc7747905ffb7469d445
Thiabaud Engelbrecht <thiabaud@google.com>
2024-05-31 20:08:57 +0000

    [pre_freeze] Purge memory immediately when a page is frozen
    This CL is a follow-up to
    https://chromium-review.googlesource.com/c/chromium/src/+/5526994. In
    this CL, we also purge immediately when a page is frozen via PreFreeze,
    instead of waiting one second as normal. This should not affect any
    behavior, since this is all hidden behind a feature flag.
    
    Bug: 334144248, 339426791
    Change-Id: If82aec5e76989e13b330067afb128329bde0a4e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5539026
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308866}

--- third_party/blink/renderer/platform/scheduler/main_thread/page_scheduler_impl.cc
+++ third_party/blink/renderer/platform/scheduler/main_thread/page_scheduler_impl.cc
@@ -247,8 +247,10 @@ void PageSchedulerImpl::SetPageFrozen(bool frozen) {
   SetPageFrozenImpl(frozen, policy_updater);
 }
 
-void PageSchedulerImpl::SetPageFrozenImpl(bool frozen,
-                                          PolicyUpdater& policy_updater) {
+void PageSchedulerImpl::SetPageFrozenImpl(
+    bool frozen,
+    PolicyUpdater& policy_updater,
+    base::MemoryReductionTaskContext called_from) {
   // Only pages owned by web views can be frozen.
   DCHECK(!frozen || IsOrdinary());
 
@@ -262,7 +264,7 @@ void PageSchedulerImpl::SetPageFrozenImpl(bool frozen,
   }
   policy_updater.UpdatePagePolicy(this);
   if (frozen) {
-    main_thread_scheduler_->OnPageFrozen();
+    main_thread_scheduler_->OnPageFrozen(called_from);
     if (audio_state_ == AudioState::kRecentlyAudible) {
       // A recently audible page is being frozen before the audio silent timer
       // fired, which can happen if freezing from outside the scheduler (e.g.
@@ -859,7 +861,7 @@ void PageSchedulerImpl::UpdateFrozenState(
   }
 
   if (freeze_time > now) {
-    SetPageFrozenImpl(/* frozen=*/false, policy_updater);
+    SetPageFrozenImpl(/* frozen=*/false, policy_updater, called_from);
     if (!freeze_time.is_max()) {
       update_frozen_state_timer_.SetTaskRunner(
           main_thread_scheduler_->ControlTaskRunner());
@@ -874,7 +876,7 @@ void PageSchedulerImpl::UpdateFrozenState(
               base::Unretained(this)));
     }
   } else {
-    SetPageFrozenImpl(/* frozen=*/true, policy_updater);
+    SetPageFrozenImpl(/* frozen=*/true, policy_updater, called_from);
   }
 }
 
