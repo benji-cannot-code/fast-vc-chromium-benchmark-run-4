6ecf547103decf2a459aba82e68943760dcbe362
Jack Shira <jackshira@google.com>
2024-05-31 18:40:44 +0000

    [Nearby Share] Add WiDi socket outline
    Adds a new WifiDirectSocket implementation scaffold.
    
    Bug: b/343865045
    Tests: N/A
    Change-Id: Icb89ec1d27d16aef0e1b62bc7d844d062c0a99e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5587067
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308789}

---
+++ chrome/services/sharing/nearby/platform/wifi_direct_socket.cc
@@ -0,0 +1,53 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/services/sharing/nearby/platform/wifi_direct_socket.h"
+
+namespace nearby::chrome {
+
+SocketInputStream::SocketInputStream() = default;
+
+ExceptionOr<ByteArray> SocketInputStream::Read(std::int64_t size) {
+  NOTIMPLEMENTED();
+  return {Exception::kSuccess};
+}
+
+Exception SocketInputStream::Close() {
+  return {Exception::kSuccess};
+}
+
+SocketOutputStream::SocketOutputStream() = default;
+
+Exception SocketOutputStream::Write(const ByteArray& data) {
+  NOTIMPLEMENTED();
+  return {Exception::kSuccess};
+}
+
+Exception SocketOutputStream::Flush() {
+  return {Exception::kSuccess};
+}
+
+Exception SocketOutputStream::Close() {
+  return {Exception::kSuccess};
+}
+
+WifiDirectSocket::WifiDirectSocket() = default;
+
+WifiDirectSocket::~WifiDirectSocket() {}
+
+InputStream& WifiDirectSocket::GetInputStream() {
+  return input_stream_;
+}
+
+OutputStream& WifiDirectSocket::GetOutputStream() {
+  return output_stream_;
+}
+
+Exception WifiDirectSocket::Close() {
+  NOTIMPLEMENTED();
+  return {Exception::kSuccess};
+}
+
+}  // namespace nearby::chrome
