30ffa7c1111d39ce5db7292a95940fb71273190d
Qihui Zhao <qihuizhao@google.com>
2024-05-31 12:35:52 +0000

    Add a static method GetCountryCode() in IBAN class
    Bug: b:341071600
    Change-Id: Iaddb25c12675d508121f104994693e60d836c4ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582495
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308582}

--- components/autofill/core/browser/data_model/iban_unittest.cc
+++ components/autofill/core/browser/data_model/iban_unittest.cc
@@ -330,6 +330,7 @@ TEST(IbanTest, GetCountryCode) {
   Iban iban;
   iban.set_value(u"ch5604835012345678009");
 
+  EXPECT_EQ(iban.GetCountryCode(iban.value()), "CH");
   EXPECT_EQ(iban.GetCountryCode(), "CH");
 }
 
