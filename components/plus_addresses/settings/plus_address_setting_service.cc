5141fcb6e2483da0c1d8c17bee1c93fe79c0f375
Florian Leimgruber <fleimgruber@google.com>
2024-05-31 10:36:53 +0000

    Add public API to PlusAddressSettingService
    Adds getters for settings of various types, similar to PrefService's
    API.
    
    Bug: b/342089839
    Change-Id: Id69a4d4c9d324675a39d5ab32cdc9a1891de3c4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578080
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308547}

--- components/plus_addresses/settings/plus_address_setting_service.cc
+++ components/plus_addresses/settings/plus_address_setting_service.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/forwarding_model_type_controller_delegate.h"
 #include "components/sync/model/model_type_controller_delegate.h"
 #include "components/sync/model/model_type_store.h"
+#include "components/sync/protocol/plus_address_setting_specifics.pb.h"
 
 namespace plus_addresses {
 
@@ -32,11 +33,34 @@ PlusAddressSettingService::PlusAddressSettingService(
 
 PlusAddressSettingService::~PlusAddressSettingService() = default;
 
+bool PlusAddressSettingService::GetIsPlusAddressesEnabled() const {
+  // TODO(b/342089839): Finalize setting name.
+  return GetBoolean("plus_address.is_enabled");
+}
+
+bool PlusAddressSettingService::GetHasAcceptedNotice() const {
+  // TODO(b/342089839): Finalize setting name.
+  return GetBoolean("plus_address.has_accepted_notice");
+}
+
+bool PlusAddressSettingService::GetIsOptedInToDogfood() const {
+  // TODO(b/342089839): Finalize setting name.
+  return GetBoolean("plus_address.is_opted_in_to_dogfood");
+}
+
 std::unique_ptr<syncer::ModelTypeControllerDelegate>
 PlusAddressSettingService::GetSyncControllerDelegate() {
   CHECK(base::FeatureList::IsEnabled(syncer::kSyncPlusAddressSetting));
   return std::make_unique<syncer::ForwardingModelTypeControllerDelegate>(
       sync_bridge_->change_processor()->GetControllerDelegate().get());
+}
+
+bool PlusAddressSettingService::GetBoolean(std::string_view name) const {
+  if (auto setting = sync_bridge_->GetSetting(name)) {
+    CHECK(setting->has_bool_value());
+    return setting->bool_value();
+  }
+  return false;
 }
 
 }  // namespace plus_addresses
