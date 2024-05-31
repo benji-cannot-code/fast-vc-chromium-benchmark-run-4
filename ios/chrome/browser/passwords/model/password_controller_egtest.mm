52abba2bf1af6f2a035d9b573e5cfe025eb98e99
Vincent Boisselle <vincb@google.com>
2024-05-31 14:50:45 +0000

    [ios] Explicitly enable the pwd bottom sheet when egtesting sign-up uff
    This shouldn't be needed as the feature is now enabled by default, but
    somehow the test fails when no fieldtrial config...
    
    Change-Id: I58750afd4534f6457bed3c0b765111f3c176a073
    Fixed: 343361399
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588645
    Reviewed-by: Ernesto Izquierdo Clua <eic@google.com>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308627}

--- ios/chrome/browser/passwords/model/password_controller_egtest.mm
+++ ios/chrome/browser/passwords/model/password_controller_egtest.mm
@@ -203,6 +203,8 @@ void LoginOnUff() {
                    (DISABLED_testUpdateTypedCredentialInUff)]) {
     config.features_enabled.push_back(
         password_manager::features::kIosDetectUsernameInUff);
+    config.features_enabled.push_back(
+        password_manager::features::kIOSPasswordBottomSheet);
   }
   return config;
 }
