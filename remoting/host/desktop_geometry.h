ca933bf9c5454b4ec675f17587080bdcdd692e3a
Jose Dapena Paz <jdapena@igalia.com>
2024-05-31 16:44:26 +0000

    IWYU: missing include for usage of std::optional in desktop_geometry.h
    Bug: 41455655
    Change-Id: Ida85893e7c72c8e8df697bdab129cbefc2b01c88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5587500
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308711}

--- remoting/host/desktop_geometry.h
+++ remoting/host/desktop_geometry.h
@@ -6,11 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef REMOTING_HOST_DESKTOP_GEOMETRY_H_
 #define REMOTING_HOST_DESKTOP_GEOMETRY_H_
 
+#include <optional>
+#include <vector>
+
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/vector2d.h"
-
-#include <vector>
 
 namespace remoting {
 using DesktopScreenId = intptr_t;
