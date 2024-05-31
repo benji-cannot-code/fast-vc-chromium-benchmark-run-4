7b9d9d9e686e1209a4d50231a341485f5a36f6a1
Tsuyoshi Horo <horo@chromium.org>
2024-05-31 09:42:10 +0000

    Implement background WebFont decoding
    This CL moves WebFont resource decoding off the main thread when
    BackgroundResourceFetch feature is enabled.
    
    This CL implements MaybeCreateBackgroundResponseProcessorFactory()
    method in FontResource. This method is called before starting the
    resource request, and it returns a BackgroundFontProcessorFactory which
    is used to create a BackgroundFontProcessor on a background thread. The
    BackgroundFontProcessor receives the response on the background thread,
    and decodes font data on a dedicated task runner. The result is posted
    back to the main thread via PostTaskToMainThread().
    
    Note: On Windows, the font decoding task runner is GC-enabled because
    MakeTypefaceDefaultFontMgr() method in web_font_typeface_factory.cc
    is getting a SkFontMgr from FontCache in FontGlobalContext which is a
    GC’ed class.
    
    Bug: 40244488
    
    Change-Id: I96986391f85f4ed1cb39d453de01da587acd7308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5394501
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308530}

--- third_party/blink/renderer/platform/fonts/web_font_decoder.h
+++ third_party/blink/renderer/platform/fonts/web_font_decoder.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_WEB_FONT_DECODER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_FONTS_WEB_FONT_DECODER_H_
 
+#include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/skia/include/core/SkRefCnt.h"
@@ -40,7 +41,7 @@ class SkTypeface;
 
 namespace blink {
 
-class WebFontDecoder final {
+class PLATFORM_EXPORT WebFontDecoder final {
   STACK_ALLOCATED();
 
  public:
