cce555170a55d1a04468313f00ce6d2d5b63b2bf
Longbo Wei <longbowei@google.com>
2024-05-31 16:10:23 +0000

    kbd-backlight: Restore keyboard ALS status during reboot
    This change follows the same pattern and is in sync with the logic of
    restoring display brightness behavior. With this change, the keyboard
    ambient light sensor status is restored during login.
    
    Restoring keyboard brightness during login will be handled in the next
    CL
    
    Bug: b/326009607
    Test: ash_unittests -f KeyboardBrightnessControllerTest
    Change-Id: I765b31112b876a3ab744d04eb09a9c2330345be5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569990
    Commit-Queue: Longbo Wei <longbowei@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308682}

--- ash/system/keyboard_brightness/keyboard_brightness_controller.h
+++ ash/system/keyboard_brightness/keyboard_brightness_controller.h
@@ -9,17 +9,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "ash/ash_export.h"
+#include "ash/login/ui/login_data_dispatcher.h"
+#include "ash/public/cpp/session/session_observer.h"
 #include "ash/system/keyboard_brightness_control_delegate.h"
 #include "base/memory/weak_ptr.h"
+#include "chromeos/dbus/power/power_manager_client.h"
+
+class AccountId;
+class PrefService;
 
 namespace ash {
+
+class SessionControllerImpl;
 
 // A class which controls keyboard brightness when Alt+F6, Alt+F7 or a
 // multimedia key for keyboard brightness is pressed.
 class ASH_EXPORT KeyboardBrightnessController
-    : public KeyboardBrightnessControlDelegate {
+    : public KeyboardBrightnessControlDelegate,
+      public SessionObserver,
+      public LoginDataDispatcher::Observer,
+      public chromeos::PowerManagerClient::Observer {
  public:
-  KeyboardBrightnessController();
+  explicit KeyboardBrightnessController(
+      PrefService* local_state,
+      SessionControllerImpl* session_controller);
 
   // Disallow copy and move.
   KeyboardBrightnessController(const KeyboardBrightnessController&) = delete;
@@ -27,6 +40,16 @@ class ASH_EXPORT KeyboardBrightnessController
       delete;
 
   ~KeyboardBrightnessController() override;
+
+  // SessionObserver:
+  void OnActiveUserSessionChanged(const AccountId& account_id) override;
+
+  // PowerManagerClient::Observer:
+  void KeyboardAmbientLightSensorEnabledChanged(
+      const power_manager::AmbientLightSensorChange& change) override;
+
+  // LoginDataDispatcher::Observer:
+  void OnFocusPod(const AccountId& account_id) override;
 
  private:
   // Overridden from KeyboardBrightnessControlDelegate:
@@ -40,7 +63,17 @@ class ASH_EXPORT KeyboardBrightnessController
   void HandleGetKeyboardAmbientLightSensorEnabled(
       base::OnceCallback<void(std::optional<bool>)> callback) override;
 
+  // Restore keyboard brightness settings during reboot.
+  void RestoreKeyboardBrightnessSettings(const AccountId& account_id);
+
   void OnReceiveHasKeyboardBacklight(std::optional<bool> has_backlight);
+
+  // The current AccountId, used to set and retrieve prefs. Expected to be
+  // nullopt on the login screen, but will be set on login.
+  std::optional<AccountId> active_account_id_;
+
+  raw_ptr<PrefService> local_state_;                   // unowned.
+  raw_ptr<SessionControllerImpl> session_controller_;  // unowned.
 
   base::WeakPtrFactory<KeyboardBrightnessController> weak_ptr_factory_{this};
 };
