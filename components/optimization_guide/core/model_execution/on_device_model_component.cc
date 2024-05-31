eb598dbbb5a2d7c9a418c652074d7a3df281c1ca
rajendrant <rajendrant@chromium.org>
2024-05-31 05:10:56 +0000

    Stop component updater registration when model is overriden
    Change-Id: I8e7334333058c0d20959bf300009796be5ca32f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577119
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308465}

--- components/optimization_guide/core/model_execution/on_device_model_component.cc
+++ components/optimization_guide/core/model_execution/on_device_model_component.cc
@@ -203,6 +203,9 @@ void OnDeviceModelComponentStateManager::InstallerRegistered() {
 }
 
 void OnDeviceModelComponentStateManager::BeginUpdateRegistration() {
+  if (switches::GetOnDeviceModelExecutionOverride()) {
+    return;
+  }
   delegate_->GetFreeDiskSpace(
       delegate_->GetInstallDirectory(),
       base::BindOnce(
