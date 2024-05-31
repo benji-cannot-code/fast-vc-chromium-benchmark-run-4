8b4fd1f0d5ada9fffa55b3aa30b9f8e64939c751
Nina Satragno <nsatragno@chromium.org>
2024-05-31 16:01:56 +0000

    [webauthn] Fix triggering enclave autofill loading
    When tapping a GPM passkey from autofill, don't show the modal loading
    screen. Instead, rely on the autofill loading indicator.
    
    Fixed: 343480031
    Change-Id: Ib948820866df3ff33272bc2ecba8b799f5e71345
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581652
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308676}

--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -854,7 +854,10 @@ void GPMEnclaveController::OnGPMPasskeySelected(
 
   switch (account_state_) {
     case AccountState::kReady:
-      model_->SetStep(Step::kGPMConnecting);
+      if (model_->step() != Step::kConditionalMediation) {
+        // The autofill UI shows its own loading indicator.
+        model_->SetStep(Step::kGPMConnecting);
+      }
       StartTransaction();
       break;
 
@@ -876,7 +879,11 @@ void GPMEnclaveController::OnGPMPasskeySelected(
 
     case AccountState::kLoading:
     case AccountState::kChecking:
-      model_->SetStep(Step::kGPMConnecting);
+      if (model_->step() != Step::kConditionalMediation &&
+          model_->step() != Step::kNotStarted) {
+        // The autofill UI shows its own loading indicator.
+        model_->SetStep(Step::kGPMConnecting);
+      }
       waiting_for_account_state_ =
           base::BindOnce(&GPMEnclaveController::OnGPMPasskeySelected,
                          weak_ptr_factory_.GetWeakPtr(), *selected_cred_id_);
