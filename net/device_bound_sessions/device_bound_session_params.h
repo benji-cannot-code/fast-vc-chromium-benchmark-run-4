83e822543a203f879a510a234c127d00ce404b63
Kristian Monsen <kristianm@google.com>
2024-05-31 06:24:43 +0000

    Send registration requests
    Implementing sending registration requests when starting a new session.
    
    Bug: 41495201
    Change-Id: I7b944eba2ca1d6d9dc6a4fc8368135a70e32d65b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528093
    Reviewed-by: Lily Chen <chlily@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Kristian Monsen <kristianm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308480}

---
+++ net/device_bound_sessions/device_bound_session_params.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_DEVICE_BOUND_SESSIONS_DEVICE_BOUND_SESSION_PARAMS_H_
+#define NET_DEVICE_BOUND_SESSIONS_DEVICE_BOUND_SESSION_PARAMS_H_
+
+namespace net {
+
+struct DeviceBoundSessionParams {};
+
+}  // namespace net
+
+#endif  // NET_DEVICE_BOUND_SESSIONS_DEVICE_BOUND_SESSION_PARAMS_H_
--- net/device_bound_sessions/device_bound_session_service.h
+++ net/device_bound_sessions/device_bound_session_service.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "net/base/net_export.h"
+#include "net/device_bound_sessions/device_bound_session_params.h"
 #include "net/device_bound_sessions/device_bound_session_registration_fetcher_param.h"
 
 namespace net {
@@ -20,6 +21,8 @@ class URLRequestContext;
 // Full information can be found at https://github.com/WICG/dbsc
 class NET_EXPORT DeviceBoundSessionService {
  public:
+  // Returns nullptr if unexportable key provider is not supported by the
+  // platform or the device.
   static std::unique_ptr<DeviceBoundSessionService> Create(
       const URLRequestContext* request_context);
 
@@ -29,6 +32,13 @@ class NET_EXPORT DeviceBoundSessionService {
 
   virtual ~DeviceBoundSessionService() = default;
 
+  // Called to register a new session after getting a Sec-Session-Registration
+  // header.
+  // Registration parameters to be used for creating the registration
+  // request.
+  // Isolation info to be used for registration request, this should be the
+  // same as was used for the response with the Sec-Session-Registration
+  // header.
   virtual void RegisterBoundSession(
       DeviceBoundSessionRegistrationFetcherParam registration_params,
       const IsolationInfo& isolation_info) = 0;
