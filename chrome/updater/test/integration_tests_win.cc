56e952b07c1dac7a44df2e7c505edf4c5e5385b9
Xiaoling Bao <xiaolingbao@chromium.org>
2024-05-31 22:31:58 +0000

    Add integration test for IPolicyStatus interface.
    This is to mitigate low coverage issue with policy query.
    
    Bug: 341356216
    Change-Id: Ic6859e352609eb03894be5e5e056f3056e6dcc94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589953
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1308938}

--- chrome/updater/test/integration_tests_win.cc
+++ chrome/updater/test/integration_tests_win.cc
@@ -1536,8 +1536,6 @@ void ExpectLegacyAppCommandWebSucceeds(UpdaterScope scope,
   DeleteAppClientKey(scope, appid);
 }
 
-namespace {
-
 void ExpectPolicyStatusValues(
     Microsoft::WRL::ComPtr<IPolicyStatusValue> policy_status_value,
     const std::wstring& expected_source,
@@ -1555,8 +1553,6 @@ void ExpectPolicyStatusValues(
   EXPECT_HRESULT_SUCCEEDED(policy_status_value->get_hasConflict(&has_conflict));
   EXPECT_EQ(has_conflict, expected_has_conflict);
 }
-
-}  // namespace
 
 void ExpectLegacyPolicyStatusSucceeds(UpdaterScope scope) {
   Microsoft::WRL::ComPtr<IUnknown> policy_status_server;
