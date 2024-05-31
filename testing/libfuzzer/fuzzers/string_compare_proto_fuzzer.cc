0f36823595ea12573bf2c5964767fb63e16074c6
Paul Semel <paulsemel@chromium.org>
2024-05-31 11:42:18 +0000

    fuzzing: introduce string_compare_proto_fuzzer
    This fuzzer aims at testing whether our protobuf mutation works
    correctly and that we manage to find the string "fish" quickly.
    
    Change-Id: I357d7ba142910b2d62df6b6dd31f65addf2d0fe0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584843
    Auto-Submit: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308562}

---
+++ testing/libfuzzer/fuzzers/string_compare_proto_fuzzer.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "testing/libfuzzer/proto/string_compare.pb.h"
+#include "third_party/libprotobuf-mutator/src/src/libfuzzer/libfuzzer_macro.h"
+
+DEFINE_BINARY_PROTO_FUZZER(const string_compare::StringCompare& proto) {
+  if (proto.value() == "fish") {
+    std::cout << "Found fish\n";
+    exit(1);
+  }
+}
