82a953392116a87393110b2d128ec36afed0347e
Joe Mason <joenotcharles@google.com>
2024-05-31 21:14:18 +0000

    Remove HeapProfilerIncludeZero feature
    The feature has been default-enabled since M125.
    
    R=anthonyvd
    
    Bug: 40840943
    Change-Id: Iab048dbff483d36d82c2910a6aab593e900f4e53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581134
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308896}

--- components/heap_profiling/in_process/heap_profiler_controller.cc
+++ components/heap_profiling/in_process/heap_profiler_controller.cc
@@ -50,10 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace heap_profiling {
 
-BASE_FEATURE(kHeapProfilerIncludeZero,
-             "HeapProfilerIncludeZero",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kHeapProfilerCentralControl,
              "HeapProfilerCentralControl",
              base::FEATURE_DISABLED_BY_DEFAULT);
@@ -457,10 +453,6 @@ void HeapProfilerController::RetrieveAndSendSnapshot(
   // Also summarize over all process types.
   base::UmaHistogramCounts100000("HeapProfiling.InProcess.SamplesPerSnapshot",
                                  samples.size());
-  if (!base::FeatureList::IsEnabled(kHeapProfilerIncludeZero) &&
-      samples.empty()) {
-    return;
-  }
 
   base::ModuleCache module_cache;
   metrics::CallStackProfileParams params(
