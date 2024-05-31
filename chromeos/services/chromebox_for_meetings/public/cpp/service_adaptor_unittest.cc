974004663f1e6cbe58f206d24cb334ef6bfbc363
Kyle Williams <kdgwill@google.com>
2024-05-31 02:36:05 +0000

    Reland "chromebox_for_meetings: ServiceAdaptor For Lacros/Ash"
    This is a reland of commit e6fe47860229280198f961f8ae7bd1e169cff2ec
    Dependencies missing for LaCrOS impl added in crrev.com/c/5561171
    
    Original change's description:
    > chromebox_for_meetings: ServiceAdaptor For Lacros/Ash
    >
    > Move ServiceAdaptor to a neutral location for use in both lacros and ash.
    >
    > Bug: b/341504827
    > Test: autoninja -C out/Default/ chromeos:chromeos_unittests && ./out/Default/chromeos_unittests --dbus-stub --single-process-tests --gtest_filter=Cfm*
    > Test: autoninja -C out/Default/ chrome/test:unit_tests && xvfb-run ./out/Default/unit_tests --single-process-tests --gtest_filter=Cfm*
    > Change-Id: Ia14b60786c0138f078750b569f52736b8e6efdce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5546033
    > Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    > Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    > Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1303544}
    
    Bug: b/341504827
    Change-Id: I57e9ffadce630a06f46c71c9753e0f57d5522283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553213
    Reviewed-by: Jenny Zhang <jennyz@chromium.org>
    Commit-Queue: Kyle Williams <kdgwill@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308426}

--- chromeos/services/chromebox_for_meetings/public/cpp/service_adaptor_unittest.cc
+++ chromeos/services/chromebox_for_meetings/public/cpp/service_adaptor_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/chromebox_for_meetings/service_adaptor.h"
+#include "chromeos/services/chromebox_for_meetings/public/cpp//service_adaptor.h"
 
 #include <optional>
 #include <utility>
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/mock_callback.h"
 #include "base/test/task_environment.h"
-#include "chromeos/ash/components/dbus/chromebox_for_meetings/cfm_hotline_client.h"
 #include "chromeos/services/chromebox_for_meetings/public/cpp/fake_service_connection.h"
 #include "chromeos/services/chromebox_for_meetings/public/cpp/fake_service_context.h"
 #include "chromeos/services/chromebox_for_meetings/public/cpp/service_connection.h"
@@ -25,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace ash::cfm {
+namespace chromeos::cfm {
 namespace {
 
 using ::testing::_;
@@ -66,12 +65,9 @@ class CfmServiceAdaptorTest : public testing::Test {
   CfmServiceAdaptorTest& operator=(const CfmServiceAdaptorTest&) = delete;
 
   void SetUp() override {
-    CfmHotlineClient::InitializeFake();
     chromeos::cfm::ServiceConnection::UseFakeServiceConnectionForTesting(
         &fake_service_connection);
   }
-
-  void TearDown() override { CfmHotlineClient::Shutdown(); }
 
   void SetCallback(
       chromeos::cfm::FakeServiceConnectionImpl::FakeBootstrapCallback
@@ -131,4 +127,4 @@ TEST_F(CfmServiceAdaptorTest, BindServiceAdaptor) {
 }
 
 }  // namespace
-}  // namespace ash::cfm
+}  // namespace chromeos::cfm
