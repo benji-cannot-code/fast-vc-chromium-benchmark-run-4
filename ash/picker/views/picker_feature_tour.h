d9dc83c9d0ac8715c665140df6667f17959f1f0e
Darren Shen <shend@chromium.org>
2024-05-31 07:05:16 +0000

    Add feature tour to Picker
    This is not used at the moment. Future CLs will add the necessary
    trigger logic to show the feature tour.
    
    Use placeholder strings for the heading and description for now.
    
    Bug: b/343599950
    Change-Id: Id6d67129670ef4b3185efde50ab4d83b22cf4c56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584566
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308488}

---
+++ ash/picker/views/picker_feature_tour.h
@@ -0,0 +1,37 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_PICKER_VIEWS_PICKER_FEATURE_TOUR_H_
+#define ASH_PICKER_VIEWS_PICKER_FEATURE_TOUR_H_
+
+#include "ash/ash_export.h"
+#include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/widget/unique_widget_ptr.h"
+
+namespace views {
+class Widget;
+}
+
+namespace ash {
+
+class ASH_EXPORT PickerFeatureTour {
+ public:
+  PickerFeatureTour();
+  PickerFeatureTour(const PickerFeatureTour&) = delete;
+  PickerFeatureTour& operator=(const PickerFeatureTour&) = delete;
+  ~PickerFeatureTour();
+
+  // Shows the feature tour dialog.
+  void Show();
+
+  views::Widget* widget_for_testing();
+
+ private:
+  views::UniqueWidgetPtr widget_;
+};
+
+}  // namespace ash
+
+#endif  // ASH_PICKER_VIEWS_PICKER_FEATURE_TOUR_H_
---
+++ ash/picker/views/picker_feature_tour_unittest.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/picker/views/picker_feature_tour.h"
+
+#include "ash/test/ash_test_base.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ash {
+namespace {
+
+using PickerFeatureTourTest = AshTestBase;
+
+TEST_F(PickerFeatureTourTest, ShowShowsDialog) {
+  PickerFeatureTour feature_tour;
+
+  feature_tour.Show();
+
+  EXPECT_NE(feature_tour.widget_for_testing(), nullptr);
+}
+
+}  // namespace
+}  // namespace ash
