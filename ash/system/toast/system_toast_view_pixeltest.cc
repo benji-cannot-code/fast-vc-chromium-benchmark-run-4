b229953d2d34d2b4ba34c230a1e97c1137ce8980
Michael Ludwig <michaelludwig@google.com>
2024-05-31 18:51:42 +0000

    [skia] Remove SK_DONT_PAD_LAYER_IMAGES staging flag
    Bug: b/323886180
    Change-Id: Ic7566fc03a37252fc3f030de44b029149c4d80ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582374
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Robert Phillips <robertphillips@google.com>
    Cr-Commit-Position: refs/heads/main@{#1308796}

--- ash/system/toast/system_toast_view_pixeltest.cc
+++ ash/system/toast/system_toast_view_pixeltest.cc
@@ -79,7 +79,7 @@ TEST_F(SystemToastViewPixelTest, WithLeadingIcon) {
       /*dismiss_callback=*/base::DoNothing(), /*leading_icon=*/kTestIcon));
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "screenshot", /*revision_number=*/4, GetContentsView()));
+      "screenshot", /*revision_number=*/5, GetContentsView()));
 }
 
 TEST_F(SystemToastViewPixelTest, WithButton) {
@@ -87,7 +87,7 @@ TEST_F(SystemToastViewPixelTest, WithButton) {
       /*text=*/kTestText, /*dismiss_text=*/kTestButtonText));
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
-      "screenshot", /*revision_number=*/5, GetContentsView()));
+      "screenshot", /*revision_number=*/6, GetContentsView()));
 }
 
 TEST_F(SystemToastViewPixelTest, WithLeadingIconAndButton) {
