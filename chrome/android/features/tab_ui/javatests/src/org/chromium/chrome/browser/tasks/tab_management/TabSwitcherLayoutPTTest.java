5ef0b77aa71e337bed56e95df41367cf87b0c9eb
Calder Kitagawa <ckitagawa@chromium.org>
2024-05-31 20:06:39 +0000

    [Cleanup] Remove unused mModalDialogManager
    Change-Id: Ie24bbd0d14b2d36c5cc4b5730befc4ac50ed159e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588929
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308862}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutPTTest.java
@@ -38,7 +38,6 @@ import org.chromium.chrome.test.transit.PageStation;
 import org.chromium.chrome.test.transit.TabThumbnailsCapturedFacility;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.net.test.EmbeddedTestServer;
-import org.chromium.ui.modaldialog.ModalDialogManager;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -71,7 +70,6 @@ public class TabSwitcherLayoutPTTest {
 
     private static EmbeddedTestServer sTestServer;
 
-    private ModalDialogManager mModalDialogManager;
     private PageStation mStartPage;
 
     @Before
