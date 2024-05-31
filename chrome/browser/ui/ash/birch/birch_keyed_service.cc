aaf7f267c1f5bacef4bfc142486db75b0b30db90
James Cook <jamescook@chromium.org>
2024-05-31 18:21:16 +0000

    birch: Connect BirchLastActiveItem to history and favicon services
    Birch shows a suggestion chip for the last opened tab. The URL comes
    from the history service and the favicon comes from the favicon
    service.
    
    Bug: b:340347081
    Test: unit_tests --gtest_filter=BirchLastActive*
    Change-Id: I55298ec10fe0544eba08a39892e1a622232613b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586136
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308770}

--- chrome/browser/ui/ash/birch/birch_keyed_service.cc
+++ chrome/browser/ui/ash/birch/birch_keyed_service.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/ash/birch/birch_calendar_provider.h"
 #include "chrome/browser/ui/ash/birch/birch_file_suggest_provider.h"
+#include "chrome/browser/ui/ash/birch/birch_last_active_provider.h"
 #include "chrome/browser/ui/ash/birch/birch_most_visited_provider.h"
 #include "chrome/browser/ui/ash/birch/birch_recent_tabs_provider.h"
 #include "chrome/browser/ui/ash/birch/birch_release_notes_provider.h"
@@ -37,6 +38,7 @@ BirchKeyedService::BirchKeyedService(Profile* profile)
       file_suggest_provider_(
           std::make_unique<BirchFileSuggestProvider>(profile)),
       recent_tabs_provider_(std::make_unique<BirchRecentTabsProvider>(profile)),
+      last_active_provider_(std::make_unique<BirchLastActiveProvider>(profile)),
       most_visited_provider_(
           std::make_unique<BirchMostVisitedProvider>(profile)),
       release_notes_provider_(
@@ -80,8 +82,7 @@ BirchDataProvider* BirchKeyedService::GetRecentTabsProvider() {
 }
 
 BirchDataProvider* BirchKeyedService::GetLastActiveProvider() {
-  // TODO(jamescook): Add a real provider.
-  return nullptr;
+  return last_active_provider_.get();
 }
 
 BirchDataProvider* BirchKeyedService::GetMostVisitedProvider() {
