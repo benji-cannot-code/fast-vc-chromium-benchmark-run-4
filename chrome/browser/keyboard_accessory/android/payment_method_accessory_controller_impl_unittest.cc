ce48254d84edb9101ef6348844c003cb331021b4
Rouslan Solomakhin <rouslan@chromium.org>
2024-05-31 15:08:10 +0000

    [CC Number Validation Target] Update keyboard accessory (7c/9)
    This patch updates //chrome/browser/keyboard_accessory/android to use
    the credit card network identifiers (e.g., kVisaCard) from
    credit_card_network_identifiers.h instead of relying on credit_card.h.
    
    Design doc: https://bit.ly/cc-number-validation-target-dd
    
    Bug: b:281812289
    Change-Id: I68bce3fb40437cdd7e77b9e3050028d7ed6e8389
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5544760
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308641}

--- chrome/browser/keyboard_accessory/android/payment_method_accessory_controller_impl_unittest.cc
+++ chrome/browser/keyboard_accessory/android/payment_method_accessory_controller_impl_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/test_personal_data_manager.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/autofill/core/common/credit_card_network_identifiers.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/unique_ids.h"
 #include "components/strings/grit/components_strings.h"
