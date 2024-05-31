4111d73af52b9ede11dce71c96fffab032e17aec
Daniel Rubery <drubery@chromium.org>
2024-05-31 21:10:18 +0000

    Stop observing completed items in DangerousDownloadDialogBridge
    DangerousDownloadDialogBridge maintains a vector `download_items_` of
    items currently showing dialogs and being observed. It uses this to
    avoid duplicate dialogs and duplicate observations, but it removes items
    from `download_items_` without stopping the observation, which allows
    for duplicate observations.
    
    This CL fixes the issue by correctly tracking showing dialogs and
    observations in `download_items_`.
    
    Fixed: 343966405
    Change-Id: I2331a694bbc48aef2b95ac7bf1f4e9d2ff6d7cdb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5587975
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308890}

--- chrome/browser/download/android/dangerous_download_dialog_bridge.cc
+++ chrome/browser/download/android/dangerous_download_dialog_bridge.cc
@@ -68,8 +68,10 @@ void DangerousDownloadDialogBridge::Show(download::DownloadItem* download_item,
 void DangerousDownloadDialogBridge::OnDownloadDestroyed(
     download::DownloadItem* download_item) {
   auto iter = base::ranges::find(download_items_, download_item);
-  if (iter != download_items_.end())
+  if (iter != download_items_.end()) {
+    (*iter)->RemoveObserver(this);
     download_items_.erase(iter);
+  }
 }
 
 void DangerousDownloadDialogBridge::Accepted(
@@ -77,8 +79,10 @@ void DangerousDownloadDialogBridge::Accepted(
     const JavaParamRef<jstring>& jdownload_guid) {
   download::DownloadItem* download = DownloadDialogUtils::FindAndRemoveDownload(
       &download_items_, ConvertJavaStringToUTF8(env, jdownload_guid));
-  if (download)
+  if (download) {
+    download->RemoveObserver(this);
     download->ValidateDangerousDownload();
+  }
 }
 
 void DangerousDownloadDialogBridge::Cancelled(
@@ -86,6 +90,8 @@ void DangerousDownloadDialogBridge::Cancelled(
     const JavaParamRef<jstring>& jdownload_guid) {
   download::DownloadItem* download = DownloadDialogUtils::FindAndRemoveDownload(
       &download_items_, ConvertJavaStringToUTF8(env, jdownload_guid));
-  if (download)
+  if (download) {
+    download->RemoveObserver(this);
     download->Remove();
+  }
 }
