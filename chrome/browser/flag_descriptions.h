60c7ae3744184f594ace3c847f6263ce08f89920
John Palmer <jopalmer@chromium.org>
2024-05-31 00:48:47 +0000

    Add flag for lens/gallery
    Bug: b:323965375
    Change-Id: I43733bfb674f2d71a670defb6c3da5d391685bc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5579076
    Reviewed-by: Patti Lor <patricialor@chromium.org>
    Commit-Queue: John Palmer <jopalmer@chromium.org>
    Auto-Submit: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1308388}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1066,6 +1066,9 @@ extern const char kForgotPasswordFormSupportDescription[];
 extern const char kFullscreenPopupWindowsName[];
 extern const char kFullscreenPopupWindowsDescription[];
 
+extern const char kGalleryAppLensName[];
+extern const char kGalleryAppLensDescription[];
+
 extern const char kGalleryAppPdfEditNotificationName[];
 extern const char kGalleryAppPdfEditNotificationDescription[];
 
