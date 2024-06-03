591d9e34de133616f2a4fe9e5bd7b0c9f664de4c
Erik Chen <erikchen@chromium.org>
2024-06-03 22:02:02 +0000

    Move lens overlay c++ files into the singular lens overlay folder.
    This CL is a refactor with no intended behavior change.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    
    Change-Id: I9fe2776f21ab954836967ae37e848d5241682070
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5591083
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309626}

--- chrome/browser/ui/lens/lens_overlay_side_panel_web_view.h
+++ chrome/browser/ui/lens/lens_overlay_side_panel_web_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
-#define CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
+#ifndef CHROME_BROWSER_UI_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
+#define CHROME_BROWSER_UI_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
 
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
@@ -64,4 +64,4 @@ class LensOverlaySidePanelWebView
   base::WeakPtrFactory<LensOverlaySidePanelWebView> weak_factory_{this};
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_SIDE_PANEL_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
+#endif  // CHROME_BROWSER_UI_LENS_LENS_OVERLAY_SIDE_PANEL_WEB_VIEW_H_
