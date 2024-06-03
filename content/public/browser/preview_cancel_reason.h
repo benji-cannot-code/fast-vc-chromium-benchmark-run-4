63212ed1cac497e175f9030ab8dce7dfb4897fa1
Bruno Pitrus <brunopitrus@hotmail.com>
2024-06-03 23:42:31 +0000

    IWYU: add missing include for std::string
    Bug: 41455655
    Change-Id: I425fc0acc6d61a43673cdae1810a0dfc7c127b56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588305
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309688}

--- content/public/browser/preview_cancel_reason.h
+++ content/public/browser/preview_cancel_reason.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_PREVIEW_CANCEL_REASON_H_
 #define CONTENT_PUBLIC_BROWSER_PREVIEW_CANCEL_REASON_H_
 
+#include <string>
+
 #include "content/common/content_export.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
