48b73a33e720ac93397d28c04dc4b5b9b167ba55
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-06-03 18:56:54 +0000

    Reland "[views-ax] Add RootView's ViewAccessibility as a WidgetObserver"
    This is a reland of commit 066f6305c3af9bd534597844c7321614e2dc30e5
    
    There were some tests where views were being reparented to a non-
    closed widget and we were not updating the value of the cached
    `is_widget_closed_`.
    
    The updated fix makes sure we account for this by updating if needed
    when reparenting.
    
    We also add an override for OnWidgetDestroyed which we missed last time.
    
    Original change's description:
    > [views-ax] Add RootView's ViewAccessibility as a WidgetObserver
    >
    > This CL makes the RootView's ViewAccessibility object a
    > Widget Observer.
    >
    > This is because we want the accessible data of a view to "react"
    > to when the widget closes. There are multiple code paths that lead
    > to this happening so instead of adding function calls there to
    > manually update the data, we add the RootView's ViewAccessibility
    > as a WidgetObserver so we take advantage of the already existing
    > system to notify observers of a closing widget.
    >
    > Previously, we were setting data for when a widget was closed
    > at "query" time in GetAccessibleNodeData, but we're moving away from
    > this pull system to a push system for the ViewsAX project and these
    > changes align with the project.
    >
    > Additionally, this CL adds a macro that is added to some setters
    > in ViewAccessibility so that we no-op when the widget is closed,
    > since otherwise the closed widget data may be overwritten by callers
    > when the widget has already closed. This would be wrong behavior but
    > also could lead to crashes.
    >
    > Lastly, this CL adds a check in ViewAXPlatformNodeDelegate's
    > GetData so that we exit early (while setting the correct closed
    > widget state data) if the widget does not exist.
    >
    > This CL is part of the ViewsAX project:
    > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    >
    > Bug: 325137417
    > Change-Id: I5b84281b4e58502065e3905178c0134cf6928037
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5536123
    > Reviewed-by: Dirk Pranke <dpranke@google.com>
    > Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1302831}
    
    Bug: 325137417
    Change-Id: I7987a8b886a4f9e9ac5f7669325f18bce4a60482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553088
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1309490}

--- ui/views/accessibility/view_accessibility.h
+++ ui/views/accessibility/view_accessibility.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/accessibility/ax_virtual_view.h"
 #include "ui/views/accessibility/view_accessibility_utils.h"
 #include "ui/views/views_export.h"
+#include "ui/views/widget/widget_observer.h"
 
 namespace ui {
 
@@ -44,7 +45,7 @@ class Widget;
 //
 // In most cases, subclasses of |ViewAccessibility| own the |AXPlatformNode|
 // that implements the native accessibility APIs on a specific platform.
-class VIEWS_EXPORT ViewAccessibility {
+class VIEWS_EXPORT ViewAccessibility : public WidgetObserver {
  public:
   using AccessibilityEventsCallback =
       base::RepeatingCallback<void(const ui::AXPlatformNodeDelegate*,
@@ -55,7 +56,7 @@ class VIEWS_EXPORT ViewAccessibility {
 
   ViewAccessibility(const ViewAccessibility&) = delete;
   ViewAccessibility& operator=(const ViewAccessibility&) = delete;
-  virtual ~ViewAccessibility();
+  ~ViewAccessibility() override;
 
   // Modifies |node_data| to reflect the current accessible state of the
   // associated View, taking any custom overrides into account
@@ -396,6 +397,12 @@ class VIEWS_EXPORT ViewAccessibility {
   const AccessibilityEventsCallback& accessibility_events_callback() const;
   void set_accessibility_events_callback(AccessibilityEventsCallback callback);
 
+  // WidgetObserver overrides.
+  void OnWidgetClosing(Widget* widget) override;
+  void OnWidgetDestroyed(Widget* widget) override;
+
+  void OnWidgetUpdated(Widget* widget, Widget* old_widget);
+
  protected:
   explicit ViewAccessibility(View* view);
 
@@ -406,6 +413,8 @@ class VIEWS_EXPORT ViewAccessibility {
 
  private:
   FRIEND_TEST_ALL_PREFIXES(ViewTest, PauseAccessibilityEvents);
+  FRIEND_TEST_ALL_PREFIXES(ViewTest,
+                           WidgetObserverViewWidgetClosedViewReparented);
 
   // Initializes the `data_` object with the values returned from
   // `View::GetAccessibleNodeData` called on the owning view.
@@ -427,6 +436,10 @@ class VIEWS_EXPORT ViewAccessibility {
   // `should_be_ignored_`, or if it has been pruned (`pruned_`), or if its role
   // is 'kNone'.
   void UpdateIgnoredState();
+
+  void SetWidgetClosedRecursive(Widget* widget, bool value);
+
+  void SetDataForClosedWidget(ui::AXNodeData* data) const;
 
   // Weak. Owns this.
   const raw_ptr<View> view_;
@@ -488,6 +501,10 @@ class VIEWS_EXPORT ViewAccessibility {
   bool pause_accessibility_events_ = false;
 
   bool ignore_missing_widget_for_testing_ = false;
+
+  bool is_widget_closed_ = false;
+
+  base::ScopedObservation<Widget, WidgetObserver> observation_{this};
 };
 
 class IgnoreMissingWidgetForTestingScopedSetter {
