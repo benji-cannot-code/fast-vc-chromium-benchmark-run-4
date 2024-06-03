edba60bbf247b6773186e45426f7d72341372fff
Takashi Sakamoto <tasak@google.com>
2024-06-03 07:06:04 +0000

    [clang-plugin] Remove FindBadConstructs code from raw_ptr plugin.
    Also update FindBadConstructs not to invoke FindBadRawPtrPatterns
    if no options are specified. Because to switch the check, we will
    do in the following way:
    (1) update //build/compiler/clang/BUILD.gn to make raw_ptr plugin
    do FindBadRawPtrPatterns instead of FindBadConstructs plugin.
    (2) remove FindBadRawPtrPatterns from FindBadConstructs plugin.
    (3) wait for the next clang roll.
    So between (2) and (3), FindBadConstructs plugin should not execute
    FindBadRawPtrPatterns, but the code executes matcherAST independent
    of plugin options. Without this CL, matcherAST will run twice
    (one with several matchers and the other without any matchers, but
    executing matchAST spends some time.)
    
    Change-Id: I5f566596fcb2752b436762236b796a93dac5f7f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577718
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309196}

--- tools/clang/raw_ptr_plugin/FindBadRawPtrPatterns.h
+++ tools/clang/raw_ptr_plugin/FindBadRawPtrPatterns.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace raw_ptr_plugin {
 
-void FindBadRawPtrPatterns(Options options,
+void FindBadRawPtrPatterns(const Options& options,
                            clang::ASTContext& ast_context,
                            clang::CompilerInstance& compiler);
 
