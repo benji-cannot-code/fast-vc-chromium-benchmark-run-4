c7fef0a99ad938d9c2e90c7596df932a94a94c69
Wen-Chien Wang <wcwang@chromium.org>
2024-06-03 00:37:13 +0000

    [gtm] Fix the overscroll behavior in non-scrollable scroll views
    The correct behavior of overscroll is that, when scrolling down from
    the bottom of Tasks, the expand states for both Tasks and Classroom
    should toggle and shows the Classroom. This is the same for Classroom
    but with opposite direction. However, the current issue is that this
    doesn't work when the scroll view is not scrollable. This cl fixes
    the bug by considering this case.
    
    Bug: b/338917100
    Change-Id: I2a30edb977a264dc27d41ec0e4526c6f59140d94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590564
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309137}

--- ash/glanceables/common/glanceables_contents_scroll_view.h
+++ ash/glanceables/common/glanceables_contents_scroll_view.h
@@ -25,6 +25,7 @@ class GlanceablesContentsScrollView : public views::ScrollView {
   void SetOnOverscrollCallback(const base::RepeatingClosure& callback);
 
   // views::ScrollView:
+  void OnGestureEvent(ui::GestureEvent* event) override;
   void ChildPreferredSizeChanged(views::View* view) override;
 
  private:
