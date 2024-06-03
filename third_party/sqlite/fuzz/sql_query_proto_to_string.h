78954592cc09a27d74e46e53228a86c7319b5bf1
Dan McArdle <dmcardle@chromium.org>
2024-06-03 19:34:09 +0000

    sql: IWYU and GN fixes for sql_recovery_lpm_fuzzer
    Change-Id: Ifce3d5eb25d356e15c78534788e9d1858cabf787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593269
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309525}

--- third_party/sqlite/fuzz/sql_query_proto_to_string.h
+++ third_party/sqlite/fuzz/sql_query_proto_to_string.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "third_party/sqlite/fuzz/sql_queries.pb.h"
+#include "third_party/sqlite/fuzz/sql_queries.pb.h"  // IWYU pragma: export
 
 namespace sql_fuzzer {
 
