b24a6a759d37079d96396a4ca877506e0c4ca0ff
Zijie He <zijiehe@google.com>
2024-06-03 21:31:21 +0000

    [PWA] Implement PWA.openCurrentPageInApp
    Include-Ci-Only-Tests: true
    Bug: 331214986
    Change-Id: I54098439237f87f867a4d669331f5b4a35b521bc
    Fixed: 339453521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5579223
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309593}

--- chrome/browser/web_applications/test/fake_web_app_ui_manager.h
+++ chrome/browser/web_applications/test/fake_web_app_ui_manager.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/app_launch_util.h"
 #include "components/webapps/common/web_app_id.h"
 
+class Browser;
+
 namespace base {
 class FilePath;
 }  // namespace base
@@ -64,9 +66,9 @@ class FakeWebAppUiManager : public WebAppUiManager {
       const std::optional<webapps::AppId>& new_app_id) const override {}
   bool CanReparentAppTabToWindow(const webapps::AppId& app_id,
                                  bool shortcut_created) const override;
-  void ReparentAppTabToWindow(content::WebContents* contents,
-                              const webapps::AppId& app_id,
-                              bool shortcut_created) override;
+  Browser* ReparentAppTabToWindow(content::WebContents* contents,
+                                  const webapps::AppId& app_id,
+                                  bool shortcut_created) override;
   void ShowWebAppFileLaunchDialog(
       const std::vector<base::FilePath>& file_paths,
       const webapps::AppId& app_id,
