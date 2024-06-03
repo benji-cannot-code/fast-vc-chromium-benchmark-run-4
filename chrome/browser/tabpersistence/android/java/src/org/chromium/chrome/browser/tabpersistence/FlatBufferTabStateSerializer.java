1191fc8df4a9018dceb68ba6b7652b8693a83467
David Maunder <davidjm@google.com>
2024-06-03 21:35:57 +0000

    Handle IllegalArgumentException in FlatBuffer deserialization
    We're seeing a small number of IllegalArgumentExceptions in
    FlatBuffer deserialization. I suspect these are due to an I/O
    error. Note, not 100% of legacy TabState restores are successful -
    there are fallback paths such as reloading the URL from the
    Tab metadata file. In this case, we fail to restore the
    WebContentsState, the TabState isn't usable (and we should
    fallback to reloading from the Tab metadata file). This is
    because TabImpl assumes a frozen Tab has a non-null WebContentsState
    (see line 1843 in TabImpl - see go/frozen-webcontentsstate-null).
    
    Bug: b:344629383
    Change-Id: I4ae7d1790cbd91c2c64d5a81622997d5d2eab58b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594769
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309595}

--- chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
+++ chrome/browser/tabpersistence/android/java/src/org/chromium/chrome/browser/tabpersistence/FlatBufferTabStateSerializer.java
@@ -57,7 +57,10 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
         /** FlatBuffer deserialization failed because of an index out of bounds exception. */
         int FAILURE_INDEX_OUT_OF_BOUNDS_EXCEPTION = 2;
 
-        int NUM_ENTRIES = 3;
+        /** FlatBuffer deserialization failed because of an illegal argument exception. */
+        int FAILURE_ILLEGAL_ARGUMENT_EXCEPTION = 3;
+
+        int NUM_ENTRIES = 4;
     }
 
     @Override
@@ -139,6 +142,11 @@ public class FlatBufferTabStateSerializer implements TabStateSerializer {
             RecordHistogram.recordEnumeratedHistogram(
                     "Tabs.TabState.FlatBufferDeserializeResult",
                     TabStateFlatBufferDeserializeResult.FAILURE_INDEX_OUT_OF_BOUNDS_EXCEPTION,
+                    TabStateFlatBufferDeserializeResult.NUM_ENTRIES);
+        } catch (IllegalArgumentException e) {
+            RecordHistogram.recordEnumeratedHistogram(
+                    "Tabs.TabState.FlatBufferDeserializeResult",
+                    TabStateFlatBufferDeserializeResult.FAILURE_ILLEGAL_ARGUMENT_EXCEPTION,
                     TabStateFlatBufferDeserializeResult.NUM_ENTRIES);
         } catch (Exception e) {
             RecordHistogram.recordEnumeratedHistogram(
