de5e8a78d8f58713128836f078b4c5e8f6e3a2da
Shintaro Kawamura <kawasin@google.com>
2024-06-03 04:24:49 +0000

    Reland kCrOSLockMainProgramText
    kCrOSLockMainProgramText was introduced by
    https://crrev.com/c/1885298 but it was cancelled because it has
    regression on memory capacity with Lacros.
    
    Since we have PGO now, just mlock-ing the first 32MB of chrome text has
    benefit to preventing i/o wait on code.
    
    This feature is available on ash only.
    
    MlockMapping() falls mlock2 back to mlock if mlock2 is not supported.
    This is needed as a short term workaround because linux-chromeos-rel
    builder does not support mlock2 (https://crbug.com/343573200).
    
    Bug: b/342533062
    
    Change-Id: I6936c20c8b2e4263c25f29b5b35c0504a5b2299e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5573228
    Reviewed-by: Brian Geffon <bgeffon@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Shin Kawamura <kawasin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309177}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -156,6 +156,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/dbus/ash_dbus_helper.h"
 #include "chrome/browser/ash/dbus_schedqos_state_handler.h"
 #include "chrome/browser/ash/startup_settings_cache.h"
+#include "chromeos/ash/components/memory/memory.h"
 #include "chromeos/ash/components/memory/mglru.h"
 #include "content/public/common/content_features.h"
 #include "ui/lottie/resource.h"  // nogncheck
@@ -1122,6 +1123,8 @@ void ChromeMainDelegate::CommonEarlyInitialization(InvokedIn invoked_in) {
   if (is_browser_process) {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
     ash::InitializeMGLRU();
+
+    ash::LockMainProgramText();
 #endif
   }
 
