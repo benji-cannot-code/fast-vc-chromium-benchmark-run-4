56ecf2613a187df365c00ede12890521153beee9
Lei Zhang <thestig@chromium.org>
2024-06-03 20:04:59 +0000

    [PDF Ink Signatures] Send "finishInkStroke" message to the frontend
    In InkModule, whenever a stroke finishes drawing, call
    InkModule::Client::InkStrokeFinished() to let the client know this
    action happened. The real client then relays the message to the
    frontend.
    
    Bug: 335521182
    Change-Id: Idf4fbc7208e727c6d41eebda0a9c022ea0281194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590494
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309545}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -1976,6 +1976,13 @@ SkBitmap PdfViewWebPlugin::GetImageForOcr(int32_t page_index,
   return engine_->GetImageForOcr(page_index, page_object_index);
 }
 
+#if BUILDFLAG(ENABLE_PDF_INK2)
+void PdfViewWebPlugin::InkStrokeFinished() {
+  base::Value::Dict message;
+  message.Set("type", "finishInkStroke");
+  client_->PostMessage(std::move(message));
+}
+
 int PdfViewWebPlugin::VisiblePageIndexFromPoint(const gfx::PointF& point) {
   gfx::Point rounded_point = gfx::ToRoundedPoint(point);
   for (int i = 0; i < engine_->GetNumberOfPages(); ++i) {
@@ -1990,6 +1997,7 @@ int PdfViewWebPlugin::VisiblePageIndexFromPoint(const gfx::PointF& point) {
   }
   return -1;
 }
+#endif  // BUILDFLAG(ENABLE_PDF_INK2)
 
 void PdfViewWebPlugin::HandleAccessibilityAction(
     const AccessibilityActionData& action_data) {
