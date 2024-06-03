c381bfa6055bf3e3abf314ef39071f7bc15ee1b7
Claudio DeSouza <cdesouza@chromium.org>
2024-06-03 19:29:58 +0000

    Replace StringPiece with std::string_view in //chrome
    For a while base::StringPiece has been merely an alias to string_view.
    This CL removes all the uses of base::StringPiece and its variants from
    //chrome
    
    Bug: 40506050
    Change-Id: Ib2dde557a9022c6c1d019594c9f7e322f1ece33c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555580
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Claudio DeSouza <cdesouza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309519}

--- chrome/renderer/net/net_error_page_controller.cc
+++ chrome/renderer/net/net_error_page_controller.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/net/net_error_page_controller.h"
 
-#include "base/strings/string_piece.h"
 #include "content/public/renderer/render_frame.h"
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
