b73d518b125ed9cc0f60ed4a00c9b9c0a2f24c15
Nate Chapin <japhet@chromium.org>
2024-06-03 18:50:41 +0000

    Move most of ObservableArray to platform/bindings/
    ObservableArrayImplHelper doesn't need anything in core/, though it
    still makes sense to have a separate, un-templated
    ObservableArrayBase.
    
    ObservableArrayExoticObjectImpl only uses core/ for
    ToV8Traits<ScriptWrappable> on a non-null object, so use
    ScriptWrappable::ToV8() instead and collapse it into
    ObservableArrayExoticObject.
    
    Change-Id: Ie45a824be4d732b003a98690a8d42fd7203d1623
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588020
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309484}

--- third_party/blink/renderer/bindings/core/v8/observable_array_exotic_object_handler.h
+++ third_party/blink/renderer/bindings/core/v8/observable_array_exotic_object_handler.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/bindings/core/v8/idl_types.h"
 #include "third_party/blink/renderer/bindings/core/v8/native_value_traits_impl.h"
-#include "third_party/blink/renderer/bindings/core/v8/observable_array.h"
 #include "third_party/blink/renderer/bindings/core/v8/to_v8_traits.h"
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
+#include "third_party/blink/renderer/platform/bindings/observable_array.h"
 #include "third_party/blink/renderer/platform/bindings/script_state.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/bindings/v8_binding.h"
@@ -467,8 +467,8 @@ class ObservableArrayExoticObjectHandler {
   static BackingListWrappable& ToWrappableOrDie(v8::Isolate* isolate,
                                                 v8::Local<v8::Array> target) {
     bindings::ObservableArrayBase* base =
-        bindings::ObservableArrayExoticObjectImpl::
-            ProxyTargetToObservableArrayBaseOrDie(isolate, target);
+        ObservableArrayExoticObject::ProxyTargetToObservableArrayBaseOrDie(
+            isolate, target);
     return *static_cast<BackingListWrappable*>(base);
   }
 
