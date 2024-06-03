7a554f1a4efc36d58d902fde820eb2a309226115
Vinny Persky <vinnypersky@google.com>
2024-06-03 23:49:17 +0000

    CL to fix FIDO enrollment after CVC auth
    This CL ensures, in the case of FIDO registration after CVC auth, that
    the form is filled immediately if the returned FIDO request options are
    invalid, and FIDO enrollment for that specific card is not attempted.
    
    Due to the current code, and the way bugs that users have had have
    presented, it seems likely that FIDO request options returned for card-
    specific enrollments can (rarely) be invalid.
    
    Bug: 40207638
    Change-Id: If0166ea8a5e1c550ee0eecfa3634874c5ed41638
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5513281
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309693}

--- components/autofill/core/browser/payments/credit_card_fido_authenticator.h
+++ components/autofill/core/browser/payments/credit_card_fido_authenticator.h
@@ -151,6 +151,10 @@ class CreditCardFidoAuthenticator
   // Returns the current flow.
   Flow current_flow() { return current_flow_; }
 
+  // Returns true if `request_options` contains a challenge and has a non-empty
+  // list of keys that each have a Credential ID.
+  bool IsValidRequestOptions(const base::Value::Dict& request_options);
+
  private:
   friend class BrowserAutofillManagerTest;
   friend class CreditCardAccessManagerTest;
@@ -230,10 +234,6 @@ class CreditCardFidoAuthenticator
   base::Value::Dict ParseAttestationResponse(
       blink::mojom::MakeCredentialAuthenticatorResponsePtr
           attestation_response);
-
-  // Returns true if |request_options| contains a challenge and has a non-empty
-  // list of keys that each have a Credential ID.
-  bool IsValidRequestOptions(const base::Value::Dict& request_options);
 
   // Returns true if |request_options| contains a challenge.
   bool IsValidCreationOptions(const base::Value::Dict& creation_options);
