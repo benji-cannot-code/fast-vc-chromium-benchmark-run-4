12e82dc1557ac942f1515ddb2ccfcee8962fdf8c
Wolfgang Beyer <wolfi@chromium.org>
2024-06-03 08:37:15 +0000

    [DevTools] Add `getHostConfig` UI binding for sending status of `base::Features` to DevTools
    DevTools needs to know the state of various `base::Features` in order
    to know which features to show/hide from users. So far this has been
    done via query params being appended to the DevTools URL. This CL adds
    a more scalable alternative by adding a `getHostConfig` method to
    DevTools UI bindings. For now, its response only contains info
    relevant to the console insights feature, but adding more info can
    easily be done whenever this info is needed by DevTools.
    
    Corresponding DevTools CL: https://crrev.com/c/5538381
    
    Follow-up CL adding a test (requires DevTools CL to land first):
    https://crrev.com/c/5578016
    
    Bug: chromium:328226367
    Change-Id: I9f3108bf8c50905771127904dc6d179eb580f147
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5344413
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309217}

--- chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
+++ chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
@@ -441,6 +441,8 @@ DevToolsEmbedderMessageDispatcher::CreateForDevToolsFrontend(
                      &Delegate::ClearPreferences, delegate);
   d->RegisterHandlerWithCallback("getSyncInformation",
                                  &Delegate::GetSyncInformation, delegate);
+  d->RegisterHandlerWithCallback("getHostConfig", &Delegate::GetHostConfig,
+                                 delegate);
   d->RegisterHandlerWithCallback("reattach",
                                  &Delegate::Reattach, delegate);
   d->RegisterHandler("readyForTest",
