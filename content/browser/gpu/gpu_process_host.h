b7323fb89b23d61fa4f59447211dc5b5342fdb4e
Colin Blundell <blundell@chromium.org>
2024-06-03 13:33:14 +0000

    [//content] DCHECK that GpuProcessHost methods are on UI thread
    Followup to [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5587517
    
    Bug: 40602071
    Change-Id: I4e4bb731537b0e93f9b11f0f3c269f99e9483ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5591693
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309293}

--- content/browser/gpu/gpu_process_host.h
+++ content/browser/gpu/gpu_process_host.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/memory/memory_pressure_listener.h"
 #include "base/memory/weak_ptr.h"
-#include "base/sequence_checker.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "components/viz/host/gpu_host_impl.h"
@@ -285,8 +284,6 @@ class GpuProcessHost : public BrowserChildProcessHostDelegate,
 #endif
 
   std::unique_ptr<viz::GpuHostImpl> gpu_host_;
-
-  SEQUENCE_CHECKER(sequence_checker_);
 
   base::WeakPtrFactory<GpuProcessHost> weak_ptr_factory_{this};
 };
