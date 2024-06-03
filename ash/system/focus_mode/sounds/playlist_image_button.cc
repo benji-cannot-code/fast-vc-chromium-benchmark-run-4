798f63e4d471032614f70b7d2fb2fd9a48b7ab84
Sean Kau <skau@chromium.org>
2024-06-03 21:49:24 +0000

    Adjust size of curvy cutout for Focus Mode playlists
    Now rounder and slightly larger.
    
    Bug: b:343976541
    Change-Id: I8acf0e5cde976215126fa087986d9e946c8ad748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589500
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309609}

--- ash/system/focus_mode/sounds/playlist_image_button.cc
+++ ash/system/focus_mode/sounds/playlist_image_button.cc
@@ -17,8 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-constexpr auto kCutoutSize = gfx::SizeF(24.f, 24.f);
-constexpr int kCutoutInnerCornerRadius = 8;
+constexpr auto kCutoutSize = gfx::SizeF(28.f, 28.f);
+constexpr int kCutoutInnerCornerRadius = 16;
+constexpr int kCutoutOuterCornerRadius = 10;
 constexpr int kSinglePlaylistViewWidth = 72;
 constexpr int kIconSize = 20;
 constexpr int kMediaActionIconSpacing = 6;
@@ -116,6 +117,7 @@ void PlaylistImageButton::SetIsSelected(bool is_selected) {
     builder
         .AddCutout(RoundedRectCutoutPathBuilder::Corner::kUpperLeft,
                    kCutoutSize)
+        .CutoutOuterCornerRadius(kCutoutOuterCornerRadius)
         .CutoutInnerCornerRadius(kCutoutInnerCornerRadius);
   }
   image_view_->SetClipPath(builder.Build());
