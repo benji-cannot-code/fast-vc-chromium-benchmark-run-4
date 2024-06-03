9a7dfa7874623f54055dccbbf45c7dd0fd3a4ad5
Noémie St-Onge <noemies@google.com>
2024-06-03 14:35:56 +0000

    [iOSKAUpgrade] Move AutofillCreditCardUtil to i/c/b/u/autofill/
    This CL moves the autofill_credit_card_util.h/mm files from
    i/c/b/u/s/autofill/ to i/c/b/u/autofill so that it can be used in
    i/c/b/u/autofill/ files. This change is needed for a follow-up CL where
    a AutofillCreditCardUtil method will be called from the
    FormInputAccessoryCoordinator (located at i/c/b/u/a/manual_fill/). See
    [1] for ref.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5525431/4/ios/chrome/browser/ui/autofill/form_input_accessory/form_input_accessory_coordinator.mm
    
    Bug: 326413453
    Change-Id: If4827670a8d24e27d0820881399d78ee6d850720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589167
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309303}

--- ios/chrome/browser/ui/autofill/autofill_credit_card_util.mm
+++ ios/chrome/browser/ui/autofill/autofill_credit_card_util.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/autofill/autofill_credit_card_util.h"
+#import "ios/chrome/browser/ui/autofill/autofill_credit_card_util.h"
 
 #import "base/strings/sys_string_conversions.h"
 #import "components/autofill/core/browser/autofill_type.h"
