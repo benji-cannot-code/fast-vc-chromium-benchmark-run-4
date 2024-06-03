e6bc78b507870339a9e07c4b1a84b9763477ece6
Mark Yacoub <markyacoub@google.com>
2024-06-03 18:32:22 +0000

    ozone: Use atomic calls for setting connector props.
    Rename SetProperty to SetConnectorProperty to make clear that it's
    targeting connector props.
    
    Setting properties was done using the legacy uapi which we don't want to
    use often.
    Similar to setting CRTC properties, use an atomic commit to set
    connector properties.
    This doesn't introduce any functionality changes as the legacy uapi uses
    atomic under the hood for atomic drivers.
    The atomic commit is blocking to match the same behavior of the legacy
    call.
    
    This change is to pave the way of serializing atomic calls.
    
    BUG=b:342617770
    TEST=unittests using this call + chrome logs with props being set
    
    Change-Id: Iaffd83a6d9f959929b4a1d15b24ca579e298761a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577274
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Mark Yacoub <markyacoub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309469}

--- ui/ozone/platform/drm/gpu/drm_display.cc
+++ ui/ozone/platform/drm/gpu/drm_display.cc
@@ -67,9 +67,10 @@ bool DrmDisplay::PrivacyScreenProperty::SetPrivacyScreenProperty(bool enabled) {
 
   const display::PrivacyScreenState state_to_set =
       enabled ? display::kEnabled : display::kDisabled;
-  if (!drm_->SetProperty(connector_->connector_id, property->prop_id,
-                         GetDrmValueForInternalType(state_to_set, *property,
-                                                    kPrivacyScreenStates))) {
+  if (!drm_->SetConnectorPropertyValue(
+          connector_->connector_id, property->prop_id,
+          GetDrmValueForInternalType(state_to_set, *property,
+                                     kPrivacyScreenStates))) {
     LOG(ERROR) << (enabled ? "Enabling" : "Disabling") << " property '"
                << property->name << "' failed!";
     return false;
@@ -200,8 +201,8 @@ bool DrmDisplay::SetHdcpKeyProp(const std::string& key) {
       drm_->GetProperty(connector_.get(), kContentProtectionKey));
   DCHECK(hdcp_key_property);
 
-  return drm_->SetProperty(connector_->connector_id, hdcp_key_property->prop_id,
-                           key_blob->id());
+  return drm_->SetConnectorPropertyValue(
+      connector_->connector_id, hdcp_key_property->prop_id, key_blob->id());
 }
 
 // When reading DRM state always check that it's still valid. Any sort of events
@@ -286,13 +287,15 @@ bool DrmDisplay::SetHDCPState(
         return false;
       }
       VLOG(3) << "HDCP Content Type not supported, default to Type 0";
-    } else if (!drm_->SetProperty(connector_->connector_id,
-                                  content_type_property->prop_id,
-                                  GetDrmValueForInternalType(
-                                      protection_method, *content_type_property,
-                                      kHdcpContentTypeStates))) {
-      // Failed setting HDCP Content Type.
-      return false;
+    } else {
+      if (!drm_->SetConnectorPropertyValue(
+              connector_->connector_id, content_type_property->prop_id,
+              GetDrmValueForInternalType(protection_method,
+                                         *content_type_property,
+                                         kHdcpContentTypeStates))) {
+        // Failed setting HDCP Content Type.
+        return false;
+      }
     }
   }
 
@@ -303,7 +306,7 @@ bool DrmDisplay::SetHDCPState(
     return false;
   }
 
-  return drm_->SetProperty(
+  return drm_->SetConnectorPropertyValue(
       connector_->connector_id, hdcp_property->prop_id,
       GetDrmValueForInternalType(state, *hdcp_property,
                                  kContentProtectionStates));
@@ -365,8 +368,8 @@ bool DrmDisplay::ClearHdrOutputMetadata() {
     return false;
   }
 
-  if (!drm_->SetProperty(connector_->connector_id,
-                         hdr_output_metadata_property->prop_id, 0)) {
+  if (!drm_->SetConnectorPropertyValue(
+          connector_->connector_id, hdr_output_metadata_property->prop_id, 0)) {
     PLOG(INFO) << "Cannot set '" << kHdrOutputMetadata
                << "' property on connector " << connector_->connector_id;
     return false;
@@ -437,9 +440,9 @@ bool DrmDisplay::SetHdrOutputMetadata(const gfx::ColorSpace color_space) {
   }
 
   if (!hdr_output_metadata_property->prop_id ||
-      !drm_->SetProperty(connector_->connector_id,
-                         hdr_output_metadata_property->prop_id,
-                         hdr_output_metadata_property_blob->id())) {
+      !drm_->SetConnectorPropertyValue(
+          connector_->connector_id, hdr_output_metadata_property->prop_id,
+          hdr_output_metadata_property_blob->id())) {
     PLOG(INFO) << "Cannot set '" << kHdrOutputMetadata << "' property.";
     return false;
   }
@@ -455,7 +458,7 @@ bool DrmDisplay::SetColorspaceProperty(const gfx::ColorSpace color_space) {
     return false;
   }
   if (!color_space_property->prop_id ||
-      !drm_->SetProperty(
+      !drm_->SetConnectorPropertyValue(
           connector_->connector_id, color_space_property->prop_id,
           GetEnumValueForName(*drm_, color_space_property->prop_id,
                               GetNameForColorspace(color_space)))) {
