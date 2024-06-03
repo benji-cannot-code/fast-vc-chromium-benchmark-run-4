cdf9492d00cfeb8536ceb7ec56d50617abdecc8c
Lauren Winston <lwinston@google.com>
2024-06-03 23:31:05 +0000

    Update string const references.
    Based on feedback on crrev.com/c/5584291, but since that CL
    has some questions, going ahead and breaking off into
    its own to ensure we're not accidentally copying strings.
    
    Bug: none
    Change-Id: I1dc621a22ee097b67d732ae5ebd5e883dc3705d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595112
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309674}

--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -1740,17 +1740,20 @@ int ReadAnythingAppController::GetNextWordHighlightLength(int index) {
   return model_.GetNextWordHighlightLength(index);
 }
 
-void ReadAnythingAppController::LogUmaHistogramTimes(int64_t time,
-                                                     std::string metric) {
+void ReadAnythingAppController::LogUmaHistogramTimes(
+    int64_t time,
+    const std::string& metric) {
   base::UmaHistogramTimes(metric, base::Milliseconds(time));
 }
 
-void ReadAnythingAppController::LogUmaHistogramLongTimes(int64_t time,
-                                                         std::string metric) {
+void ReadAnythingAppController::LogUmaHistogramLongTimes(
+    int64_t time,
+    const std::string& metric) {
   base::UmaHistogramLongTimes(metric, base::Milliseconds(time));
 }
 
-void ReadAnythingAppController::IncrementMetricCount(std::string metric) {
+void ReadAnythingAppController::IncrementMetricCount(
+    const std::string& metric) {
   model_.IncrementMetric(metric);
 }
 
@@ -1758,7 +1761,8 @@ void ReadAnythingAppController::LogSpeechEventCounts() {
   model_.LogSpeechEventCounts();
 }
 
-void ReadAnythingAppController::LogSpeechErrorEvent(std::string error_code) {
+void ReadAnythingAppController::LogSpeechErrorEvent(
+    const std::string& error_code) {
   std::optional<ReadAnythingSpeechError> error;
   if (error_code == "text-too-long") {
     error = ReadAnythingSpeechError::kTextTooLong;
