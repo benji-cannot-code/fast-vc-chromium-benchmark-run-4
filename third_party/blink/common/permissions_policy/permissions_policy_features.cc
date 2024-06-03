4ace66366cb048900ea77d4f90383204386bfead
Chase Phillips <cmp@chromium.org>
2024-06-03 18:57:08 +0000

    permissions policy: Update comment to refer to the correct file input
    Change-Id: Icc9d67aa36f73125e47c4fe41a419bd2a90822a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585761
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309491}

--- third_party/blink/common/permissions_policy/permissions_policy_features.cc
+++ third_party/blink/common/permissions_policy/permissions_policy_features.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 // This file contains static code that is combined with templated code of
-// permissions_policy_features.cc.tmpl.
+// permissions_policy_features_generated.cc.tmpl.
 
 namespace blink {
 
