ab0717a0857b69da75925862bf026bcb69f93eeb
Cassy Chun-Crogan <cassycc@google.com>
2024-06-03 03:57:46 +0000

    Revert "FspCache: Evict file when GetMetadata and OpenFile observe new vTag"
    This reverts commit 4eaa3c9d692f3ede4095891c8b56e1629cca3130.
    
    Reason for revert: Child of CL:5581587 which is getting reverted.
    
    Original change's description:
    > FspCache: Evict file when GetMetadata and OpenFile observe new vTag
    >
    > Also undo the changes added in CL:5556842 where the check for a new
    > version tag occurs in ReadBytes (from ReadFile) as this version tag
    > comes from the OpenFile result and the check now occurs as part of
    > OpenFile.
    >
    > Test: unit_tests --gtest_filter=FileSystemProviderCloudFileSystemTest
    > Bug: b/339114823
    > Change-Id: Ie3f953bddebd1ebab5d6e2235c3abab4c32f5993
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584556
    > Reviewed-by: Ben Reich <benreich@chromium.org>
    > Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1309134}
    
    Bug: b/339114823
    Change-Id: Ief1010811cc50b07f7703e0533861ab3517baef2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588768
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309172}

--- chrome/browser/ash/file_system_provider/fake_provided_file_system.h
+++ chrome/browser/ash/file_system_provider/fake_provided_file_system.h
@@ -43,7 +43,6 @@ class OperationRequestManager;
 // default.
 extern const base::FilePath::CharType kFakeFilePath[];
 extern const char kFakeFileText[];
-extern const char kFakeFileVersionTag[];
 
 // Represents a file or a directory on a fake file system.
 struct FakeEntry {
@@ -181,8 +180,6 @@ class FakeProvidedFileSystem : public ProvidedFileSystemInterface {
   void Configure(storage::AsyncFileUtil::StatusCallback callback) override;
   base::WeakPtr<ProvidedFileSystemInterface> GetWeakPtr() override;
   std::unique_ptr<ScopedUserInteraction> StartUserInteraction() override;
-
-  base::WeakPtr<FakeProvidedFileSystem> GetFakeWeakPtr();
 
  private:
   using Entries = std::map<base::FilePath, std::unique_ptr<FakeEntry>>;
