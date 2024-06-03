d2797ebda2e508e17385d0eac6db17a3b3eab9c9
Nina Satragno <nsatragno@chromium.org>
2024-06-03 15:48:37 +0000

    [webauthn] Handle trusted vault service timeout gracefully
    Handle a user tapping on a passkey before & after the vault service
    connection times out gracefully by showing an error instead of crashing.
    
    Fixed: 343669719
    Change-Id: I7611100e124a47e78bed13559bc05f643adfd4e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585480
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309347}

--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -513,7 +513,7 @@ void GPMEnclaveController::DownloadAccountState() {
 
   account_state_timeout_ = std::make_unique<base::OneShotTimer>();
   account_state_timeout_->Start(
-      FROM_HERE, base::Milliseconds(1000),
+      FROM_HERE, kDownloadAccountStateTimeout,
       base::BindOnce(&GPMEnclaveController::OnAccountStateTimeOut,
                      weak_ptr_factory_.GetWeakPtr()));
 
@@ -546,6 +546,7 @@ void GPMEnclaveController::OnAccountStateTimeOut() {
     SetAccountStateReady();
     SetActive(true);
   } else {
+    model_->OnLoadingEnclaveTimeout();
     account_state_ = AccountState::kNone;
     SetActive(false);
   }
@@ -895,7 +896,8 @@ void GPMEnclaveController::OnGPMPasskeySelected(
                            "account state is kNone";
         model_->ContactPriorityPhone();
       } else {
-        NOTREACHED_NORETURN();
+        // This can happen if a passkey is selected after the enclave times out.
+        model_->SetStep(Step::kGPMError);
       }
       break;
 
--- chrome/browser/webauthn/gpm_enclave_controller.h
+++ chrome/browser/webauthn/gpm_enclave_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
@@ -46,6 +47,8 @@ class Profile;
 class GPMEnclaveController : AuthenticatorRequestDialogModel::Observer,
                              EnclaveManager::Observer {
  public:
+  static constexpr base::TimeDelta kDownloadAccountStateTimeout =
+      base::Seconds(1);
   struct DownloadedAccountState;
   enum class EnclaveUserVerificationMethod;
 
