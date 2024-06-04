a89fe419c534d84d0f5d72e91641b0ea1fc43743
Mark Schillaci <mschillaci@google.com>
2024-06-04 18:24:44 +0000

    Add new tests to highlight tabIndex issue on Android
    AX-Relnotes: N/A
    Bug: 342172183
    Change-Id: Ic97ee27195d21594ddda2279750ab8352f06779b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593372
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310041}

--- content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/accessibility/WebContentsAccessibilityTreeTest.java
@@ -616,6 +616,12 @@ public class WebContentsAccessibilityTreeTest {
 
     @Test
     @SmallTest
+    public void test_ariaLabelWithTabIndex() {
+        performAriaTest("aria-label-with-tabindex.html");
+    }
+
+    @Test
+    @SmallTest
     public void test_ariaLabelAugmentInnerText() {
         performAriaTest("aria-label-augment-inner-text.html");
     }
