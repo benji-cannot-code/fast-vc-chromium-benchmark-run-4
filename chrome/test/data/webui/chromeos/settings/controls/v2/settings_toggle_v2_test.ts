d9e6d976d3c60ebc1a37e38de5c873d139e968cb
Wes Okuhara <wesokuhara@google.com>
2024-06-04 20:02:15 +0000

    CrOS Settings: Settings control element change events are not composed
    Settings V2 control components dispatch a "change" event when modified
    via user action. This change event should be handled by the parent
    element whose shadow DOM contains the control component, and should not
    pass that shadow DOM boundary.
    
    Bug: b:340649888
    Test: browser_tests --gtest_filter="OSSettings*Settings*V2"
    Change-Id: I6b25c03054717ed7ba844f9a7f3340488c23b2cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593240
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310148}

--- chrome/test/data/webui/chromeos/settings/controls/v2/settings_toggle_v2_test.ts
+++ chrome/test/data/webui/chromeos/settings/controls/v2/settings_toggle_v2_test.ts
@@ -90,6 +90,8 @@ suite(SettingsToggleV2Element.is, () => {
 
         const event = await checkedChangeEventPromise;
         assertEquals(toggleElement.checked, event.detail);
+        // Event should not pass the shadow DOM boundary.
+        assertFalse(event.composed);
       });
 
   test('the internal cr-toggle control value changes on click', () => {
