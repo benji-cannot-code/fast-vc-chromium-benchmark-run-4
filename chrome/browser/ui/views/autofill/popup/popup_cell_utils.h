0a5556c01f0270f5ddaf259cbf8daf451325b742
Adem Derinel <derinel@google.com>
2024-06-04 08:06:45 +0000

    [Autofill] Fix styling of disabled icons in the autofill popup
    Unfortunately when the disabled style is applied on the whole ImageView,
    MacOS grays out the whole bounding rectangle (screen/4ibAzybF5CKDnQy).
    
    This CL moves the graying out to the ImageModel before creating the
    ImageView.
    
    The CL also IWYUfies the files it touches.
    
    Impl: screen/98j8n9oKQG65bxw
    
    Fixed: 344470541
    Change-Id: I2b3822165a77cb07097738894dddedaaf496a271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588315
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309804}

--- chrome/browser/ui/views/autofill/popup/popup_cell_utils.h
+++ chrome/browser/ui/views/autofill/popup/popup_cell_utils.h
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_POPUP_POPUP_CELL_UTILS_H_
 
 #include <memory>
-#include <optional>
+#include <string>
+#include <vector>
 
-#include "components/autofill/core/browser/filling_product.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
 #include "components/autofill/core/browser/ui/suggestion_type.h"
+#include "ui/base/models/image_model.h"
 #include "ui/gfx/vector_icon_types.h"
-#include "ui/views/style/typography.h"
 
 namespace views {
 class View;
@@ -105,9 +105,8 @@ void AddSuggestionContentToView(
     std::vector<std::unique_ptr<views::View>> subtext_views,
     PopupRowContentView& content_view);
 
-std::unique_ptr<views::ImageView> ImageViewFromVectorIcon(
-    const gfx::VectorIcon& vector_icon,
-    int icon_size);
+ui::ImageModel ImageModelFromVectorIcon(const gfx::VectorIcon& vector_icon,
+                                        int icon_size);
 
 // Appplies a grayed-out disabled style to views conveying that it is
 // deactivated and non-acceptable.
