5f41e0653515c6640fca848c6f9cc7a50adbeb76
Zaina Al-Mashni <zalmashni@google.com>
2024-06-04 07:20:13 +0000

    [SHoA] Add notification review service API calls
    Enable Add methods to:
    - Fetch the sorted notification permissions for review.
    - Ignore notifications  from origin
    - Undo ignore notifications from origin
    - Allow/Block/Reset notification permission for origin
    
    Bug: 324562205
    Change-Id: Ia6852cd30f1d1a269e47cca677978693d66a9788
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5587869
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1309789}

--- chrome/browser/safety_hub/android/notification_permission_review_bridge.h
+++ chrome/browser/safety_hub/android/notification_permission_review_bridge.h
@@ -8,8 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include <vector>
+
 #include "base/android/scoped_java_ref.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/safety_hub/notification_permission_review_service.h"
+
+class Profile;
 
 // JNI helper methods to enable unit testing.
 NotificationPermissions FromJavaNotificationPermissions(
@@ -19,6 +24,24 @@ NotificationPermissions FromJavaNotificationPermissions(
 base::android::ScopedJavaLocalRef<jobject> ToJavaNotificationPermissions(
     JNIEnv* env,
     const NotificationPermissions& obj);
+
+std::vector<NotificationPermissions> GetNotificationPermissions(
+    Profile* profile);
+
+void IgnoreOriginForNotificationPermissionReview(Profile* profile,
+                                                 const std::string& origin);
+
+void UndoIgnoreOriginForNotificationPermissionReview(Profile* profile,
+                                                     const std::string& origin);
+
+void BlockNotificationPermissionForOrigin(Profile* profile,
+                                          const std::string& origin);
+
+void AllowNotificationPermissionForOrigin(Profile* profile,
+                                          const std::string& origin);
+
+void ResetNotificationPermissionForOrigin(Profile* profile,
+                                          const std::string& origin);
 
 namespace jni_zero {
 
