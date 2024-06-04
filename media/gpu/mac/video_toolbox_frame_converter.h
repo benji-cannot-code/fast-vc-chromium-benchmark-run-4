eeb6b0dca47ec6320b363b1f8eb6f66f0d056ef8
Mingjing Zhang <mjzhang@chromium.org>
2024-06-04 20:20:52 +0000

    Refactor VideoToolboxFrameConverter with ClientSharedImage
    This CL refactors VideoToolboxFrameConverter so that this class now
    creates a ClientSharedImage (instead of a MailboxHolder array)
    to pass into VideoFrame::WrapNativeTexture(). The code using the
    shared image destruction callback is also refactored, since the
    destruction is already handled by ClientSharedImage.
    
    Bug: 1494911
    Change-Id: I2196c982c6e4b8515d91d54f097b6b538908de77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593816
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310162}

--- media/gpu/mac/video_toolbox_frame_converter.h
+++ media/gpu/mac/video_toolbox_frame_converter.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/time/time.h"
+#include "gpu/command_buffer/client/client_shared_image.h"
 #include "gpu/command_buffer/service/sequence_id.h"
 #include "gpu/ipc/service/command_buffer_stub.h"
 #include "media/base/video_frame.h"
@@ -68,7 +69,7 @@ class VideoToolboxFrameConverter
   void DestroyStub();
 
   void OnVideoFrameReleased(
-      base::OnceCallback<void(const gpu::SyncToken&)> destroy_shared_image_cb,
+      scoped_refptr<gpu::ClientSharedImage> client_shared_image,
       base::apple::ScopedCFTypeRef<CVImageBufferRef> image,
       const gpu::SyncToken& sync_token);
 
