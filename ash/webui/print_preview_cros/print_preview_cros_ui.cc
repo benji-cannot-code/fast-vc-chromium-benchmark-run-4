64a5e11f25103f4a7e28a464a077bd9e8f383b33
Gavin Williams <gavinwill@chromium.org>
2024-06-04 21:54:26 +0000

    cros-preview: Create DestinationProvider mojo interface
    This CL is creating the DestinationProvider mojo interface and can be
    broken down into 3 parts:
    
    - The mojo files `destination_provider.mojom` and
      `printer_capabilities.mojom` are created with the enums and structs
      needed to support the DestinationProvider::FetchCapabilities
      interface method. The additional methods will be added in follow up
      CLs.
    
    - The backend `destination_provider.*` files implement the
      DestinationProvider interface. This simple implementation constructs
      a fixed capabilities for the PDF printer. In a follow up CL, this
      logic will be moved to a PDF printer specific handler.
    
    - The provider is created and bound to the UI via the WebUI config.
    
    Test: ash_webui_unittests --gtest_filter=*DestinationProviderTest*
    Bug: b:323421684
    Change-Id: I211db7664ce479cfdaa256052df1046073cdf13a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564192
    Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310250}

--- ash/webui/print_preview_cros/print_preview_cros_ui.cc
+++ ash/webui/print_preview_cros/print_preview_cros_ui.cc
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/webui/print_preview_cros/print_preview_cros_ui.h"
 
+#include <memory>
+#include <utility>
+
 #include "ash/constants/ash_features.h"
 #include "ash/webui/common/trusted_types_util.h"
 #include "ash/webui/grit/ash_print_preview_cros_app_resources.h"
 #include "ash/webui/grit/ash_print_preview_cros_app_resources_map.h"
+#include "ash/webui/print_preview_cros/backend/destination_provider.h"
+#include "ash/webui/print_preview_cros/mojom/destination_provider.mojom.h"
 #include "ash/webui/print_preview_cros/url_constants.h"
 #include "base/containers/span.h"
 #include "content/public/browser/web_contents.h"
@@ -73,6 +78,12 @@ void PrintPreviewCrosUI::BindInterface(
     mojo::PendingReceiver<color_change_listener::mojom::PageHandler> receiver) {
   color_provider_handler_ = std::make_unique<ui::ColorChangeHandler>(
       web_ui()->GetWebContents(), std::move(receiver));
+}
+
+void PrintPreviewCrosUI::BindInterface(
+    mojo::PendingReceiver<mojom::DestinationProvider> receiver) {
+  destination_provider_ = std::make_unique<DestinationProvider>();
+  destination_provider_->BindInterface(std::move(receiver));
 }
 
 WEB_UI_CONTROLLER_TYPE_IMPL(PrintPreviewCrosUI)
--- ash/webui/print_preview_cros/print_preview_cros_ui.h
+++ ash/webui/print_preview_cros/print_preview_cros_ui.h
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WEBUI_PRINT_PREVIEW_CROS_PRINT_PREVIEW_CROS_UI_H_
 #define ASH_WEBUI_PRINT_PREVIEW_CROS_PRINT_PREVIEW_CROS_UI_H_
 
+#include <memory>
+
 #include "ash/webui/common/chrome_os_webui_config.h"
+#include "ash/webui/print_preview_cros/mojom/destination_provider.mojom.h"
 #include "ash/webui/print_preview_cros/url_constants.h"
 #include "content/public/common/url_constants.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 #include "ui/webui/resources/cr_components/color_change_listener/color_change_listener.mojom.h"
 
@@ -23,6 +27,7 @@ class ColorChangeHandler;
 
 namespace ash::printing::print_preview {
 
+class DestinationProvider;
 class PrintPreviewCrosUI;
 
 // The WebUI configuration for chrome://os-print.
@@ -49,7 +54,11 @@ class PrintPreviewCrosUI : public ui::MojoWebDialogUI {
       mojo::PendingReceiver<color_change_listener::mojom::PageHandler>
           receiver);
 
+  void BindInterface(
+      mojo::PendingReceiver<mojom::DestinationProvider> receiver);
+
  private:
+  std::unique_ptr<DestinationProvider> destination_provider_;
   // The color change handler notifies the WebUI when the color provider
   // changes.
   std::unique_ptr<ui::ColorChangeHandler> color_provider_handler_;
