74309f5769fd7f429bb08fad662195e677318c75
Alexander Tekle <alexandertekle@google.com>
2024-06-04 20:09:04 +0000

    [Metadata] Enable flags by default
    This CL will enable the flags by default for the metadata card art and name experiment.
    
    Bug: 1493422
    Change-Id: Icaa0f4ce2ab256cf76502a6e7ade9975e6d3dbec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5271769
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310156}

--- components/autofill/core/common/autofill_payments_features.cc
+++ components/autofill/core/common/autofill_payments_features.cc
@@ -18,11 +18,7 @@ BASE_FEATURE(kAutofillEnableAndroidNKeyForFidoAuthentication,
 // Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardArtImage,
              "AutofillEnableCardArtImage",
-#if BUILDFLAG(IS_IOS)
              base::FEATURE_ENABLED_BY_DEFAULT);
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
 
 // When enabled, server will return card art images of the exact required
 // dimension.
@@ -52,11 +48,7 @@ BASE_FEATURE(kAutofillEnableCardBenefitsSync,
 // Payments Autofill UI.
 BASE_FEATURE(kAutofillEnableCardProductName,
              "AutofillEnableCardProductName",
-#if BUILDFLAG(IS_IOS)
              base::FEATURE_ENABLED_BY_DEFAULT);
-#else
-             base::FEATURE_DISABLED_BY_DEFAULT);
-#endif
 
 // When enabled, we will store CVC for both local and server credit cards. This
 // will also allow the users to autofill their CVCs on checkout pages.
@@ -195,7 +187,11 @@ BASE_FEATURE(kAutofillEnableVerveCardSupport,
 // when the virtual card is presented to users.
 BASE_FEATURE(kAutofillEnableVirtualCardMetadata,
              "AutofillEnableVirtualCardMetadata",
+#if BUILDFLAG(IS_IOS)
              base::FEATURE_DISABLED_BY_DEFAULT);
+#else
+             base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
 // When enabled, Autofill will attempt to find standalone CVC fields for VCN
 // card on file when parsing forms.
