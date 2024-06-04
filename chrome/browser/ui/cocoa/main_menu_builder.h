b0696a3c843ff496a0b923e852ac74ca56803490
Avi Drissman <avi@chromium.org>
2024-06-04 22:46:26 +0000

    Improve Mac menu handling
    Prevent macOS from fiddling with Chromium's File menu and Quit menu
    item, and implement "Close All" directly.
    
    Fixed: 342866357
    Fixed: 338051651
    Change-Id: I10508a7d2637789c44353ce592b91554465bdb79
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597418
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310273}

--- chrome/browser/ui/cocoa/main_menu_builder.h
+++ chrome/browser/ui/cocoa/main_menu_builder.h
@@ -37,9 +37,7 @@ NSMenuItem* BuildFileMenuForTesting(bool is_pwa);
 
 namespace internal {
 
-// Helper class that builds NSMenuItems from data. Instances of this class
-// should not outlive an autorelease pool scope as it does not retain any
-// Objective-C members.
+// Helper class that builds NSMenuItems from data.
 //
 // This builder follows a fluent-interface pattern where the setters are
 // not prefixed with the typical "set_" and they return a reference to this
@@ -55,7 +53,8 @@ class MenuItemBuilder {
 
   ~MenuItemBuilder();
 
-  // Converts the item to a separator. Only tag() is also applicable.
+  // Converts the item to a separator. Only tag() and hidden() are also
+  // applicable.
   MenuItemBuilder& is_separator() {
     DCHECK_EQ(string_id_, 0);
     is_separator_ = true;
@@ -101,7 +100,7 @@ class MenuItemBuilder {
   // the one specified here is used instead.
   MenuItemBuilder& key_equivalent(NSString* key_equivalent,
                                   NSEventModifierFlags flags) {
-    DCHECK((flags & NSEventModifierFlagShift) == 0)
+    CHECK((flags & NSEventModifierFlagShift) == 0)
         << "The shift modifier flag should be directly applied to the key "
            "equivalent.";
     key_equivalent_ = key_equivalent;
@@ -159,8 +158,6 @@ class MenuItemBuilder {
   bool is_hidden_ = false;
 
   bool is_section_header_ = false;
-
-  // Copy and assign allowed.
 };
 
 }  // namespace internal
