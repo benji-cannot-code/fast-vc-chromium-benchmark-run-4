f53392f0f9faf1389f9970f4e12ab83f14d13459
Jack Shira <jackshira@google.com>
2024-06-04 01:22:35 +0000

    [Nearby Share] Implement Close for WiDi socket
    Implements the Close method for WifiDirectSocket.
    
    Bug: b/343994370
    Tests: unittested
    Change-Id: I93d5de44434731fff18bbe316f346cd6d5ede4b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589098
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309731}

--- chrome/services/sharing/nearby/platform/wifi_direct_socket.cc
+++ chrome/services/sharing/nearby/platform/wifi_direct_socket.cc
@@ -5,6 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/services/sharing/nearby/platform/wifi_direct_socket.h"
 
+#include <utility>
+
+#include "base/check.h"
+#include "base/threading/thread_restrictions.h"
+#include "net/socket/stream_socket.h"
+
 namespace nearby::chrome {
 
 SocketInputStream::SocketInputStream() = default;
@@ -33,9 +39,14 @@ Exception SocketOutputStream::Close() {
   return {Exception::kSuccess};
 }
 
-WifiDirectSocket::WifiDirectSocket() = default;
+WifiDirectSocket::WifiDirectSocket(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    std::unique_ptr<net::StreamSocket> stream_socket)
+    : task_runner_(task_runner), stream_socket_(std::move(stream_socket)) {}
 
-WifiDirectSocket::~WifiDirectSocket() {}
+WifiDirectSocket::~WifiDirectSocket() {
+  Close();
+}
 
 InputStream& WifiDirectSocket::GetInputStream() {
   return input_stream_;
@@ -46,8 +57,34 @@ OutputStream& WifiDirectSocket::GetOutputStream() {
 }
 
 Exception WifiDirectSocket::Close() {
-  NOTIMPLEMENTED();
+  if (!stream_socket_) {
+    return {Exception::kFailed};
+  }
+
+  // Directly call `CloseSocket` if the current sequence is on the appropriate
+  // task runner.
+  if (task_runner_->RunsTasksInCurrentSequence()) {
+    CloseSocket(nullptr);
+    return {Exception::kSuccess};
+  }
+
+  // Cleanup the socket on the IO thread.
+  base::WaitableEvent waitable_event;
+  task_runner_->PostTask(
+      FROM_HERE, base::BindOnce(&WifiDirectSocket::CloseSocket,
+                                base::Unretained(this), &waitable_event));
+  base::ScopedAllowBaseSyncPrimitives allow;
+  waitable_event.Wait();
+
   return {Exception::kSuccess};
+}
+
+void WifiDirectSocket::CloseSocket(base::WaitableEvent* close_waitable_event) {
+  CHECK(task_runner_->RunsTasksInCurrentSequence());
+  stream_socket_.reset();
+  if (close_waitable_event) {
+    close_waitable_event->Signal();
+  }
 }
 
 }  // namespace nearby::chrome
