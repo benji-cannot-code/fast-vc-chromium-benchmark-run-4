817e482562690918630792ca099cec77bae85282
Henrique Nakashima <hnakashima@chromium.org>
2024-06-04 21:27:25 +0000

    Unbatch tests that break with batching on CQ [3]
    Third round of unbatching tests that will break on CQ when I fix
    batching with sharding.
    
    These are likely broken locally too.
    
    This is necessary to land
    https://chromium-review.googlesource.com/c/chromium/src/+/5588887
    without breaking CQ.
    
    Bug: 343771968
    Change-Id: I2838c3619db15a4da74d88ce0947a55be306ebb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598320
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310203}

--- components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetBufferedOutputStreamTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/urlconnection/CronetBufferedOutputStreamTest.java
@@ -19,6 +19,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.net.CronetEngine;
 import org.chromium.net.CronetTestRule;
 import org.chromium.net.CronetTestRule.CronetImplementation;
@@ -151,6 +152,7 @@ public class CronetBufferedOutputStreamTest {
 
     @Test
     @SmallTest
+    @RequiresRestart("crbug.com/344966615")
     public void testPostWithContentLengthWriteOneByte() throws Exception {
         URL url = new URL(NativeTestServer.getEchoBodyURL());
         mConnection = (HttpURLConnection) mCronetEngine.openConnection(url);
