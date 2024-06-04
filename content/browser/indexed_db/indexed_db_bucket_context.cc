c70604be2a426880b36509cc476429023dab6771
Brad Triebwasser <btriebw@chromium.org>
2024-06-04 19:36:55 +0000

    Add metadata snapshot recording for indexeddb-internals.
    Adds the ability to start and stop a recording indexeddb bucket
    metadata. Adds an interface for scrubbing through the snapshots of data.
    
    Bug: 336930738
    Change-Id: I07fd4400ab24d5b918ce827fa5f34ce672499bbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5523539
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310112}

--- content/browser/indexed_db/indexed_db_bucket_context.cc
+++ content/browser/indexed_db/indexed_db_bucket_context.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <inttypes.h>
 #include <stddef.h>
 
+#include <algorithm>
 #include <atomic>
 #include <compare>
 #include <list>
@@ -44,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
+#include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/memory_allocator_dump.h"
@@ -620,6 +622,25 @@ void IndexedDBBucketContext::ForceClose(bool doom) {
   RunTasks();
 }
 
+void IndexedDBBucketContext::StartMetadataRecording() {
+  CHECK(!metadata_recording_enabled_);
+  metadata_recording_start_time_ = base::Time::Now();
+  metadata_recording_enabled_ = true;
+  RecordInternalsSnapshot();  // Capture the initial snapshot.
+}
+
+void IndexedDBBucketContext::StopMetadataRecording(
+    base::OnceCallback<void(std::vector<storage::mojom::IdbBucketMetadataPtr>)>
+        callback) {
+  metadata_recording_enabled_ = false;
+
+  std::vector<storage::mojom::IdbBucketMetadataPtr> result;
+  std::move(metadata_recording_buffer_.begin(),
+            metadata_recording_buffer_.end(), std::back_inserter(result));
+  metadata_recording_buffer_.clear();
+  std::move(callback).Run(std::move(result));
+}
+
 int64_t IndexedDBBucketContext::GetInMemorySize() {
   return backing_store_ ? backing_store_->GetInMemorySize() : 0;
 }
@@ -1017,6 +1038,12 @@ storage::mojom::IdbBucketMetadataPtr IndexedDBBucketContext::FillInMetadata(
   }
   info->databases = std::move(database_list);
   return info;
+}
+
+void IndexedDBBucketContext::NotifyOfIdbInternalsRelevantChange() {
+  if (metadata_recording_enabled_) {
+    RecordInternalsSnapshot();
+  }
 }
 
 IndexedDBBucketContext* IndexedDBBucketContext::GetReferenceForTesting() {
@@ -1687,6 +1714,16 @@ void IndexedDBBucketContext::OnReceiverDisconnected() {
       delegate().on_ready_for_destruction) {
     std::move(delegate().on_ready_for_destruction).Run();
   }
+}
+
+void IndexedDBBucketContext::RecordInternalsSnapshot() {
+  storage::mojom::IdbBucketMetadataPtr metadata =
+      storage::mojom::IdbBucketMetadata::New();
+  metadata->bucket_locator = bucket_locator();
+  metadata = FillInMetadata(std::move(metadata));
+  metadata->delta_recording_start_ms =
+      (base::Time::Now() - metadata_recording_start_time_).InMilliseconds();
+  metadata_recording_buffer_.push_back(std::move(metadata));
 }
 
 IndexedDBBucketContext::ReceiverContext::ReceiverContext(
