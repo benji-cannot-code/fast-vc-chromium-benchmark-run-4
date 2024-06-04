4d6c3b5b9f507ae1cfa0b25040ab614440efadc4
Mason Freed <masonf@chromium.org>
2024-06-04 22:09:52 +0000

    Add deprecation message for getInnerHTML
    The [1] CL added a console warning, but didn't actually add the
    deprecation warning itself. This CL adds that, plus a test.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5585794
    
    Bug: 41492947
    Change-Id: Id33c02575680870551fa0f7dd7389ba48a98ae15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597703
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310259}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/getinnerhtml-issue.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  // TODO(crbug.com/41492947) This test can be deleted once getInnerHTML is removed.
+  const { session, dp } = await testRunner.startBlank(
+  'Verifies that calling getInnerHTML() triggers a deprecation issue.');
+
+  await dp.Audits.enable();
+  const promise = dp.Audits.onceIssueAdded();
+  session.evaluate('document.body.getInnerHTML()');
+
+  const result = await promise;
+  testRunner.log(result.params, 'Inspector issue: ');
+  testRunner.completeTest();
+})
