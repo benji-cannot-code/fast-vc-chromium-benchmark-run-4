d6d32a71a76d6a9a0eb772686686868c67fb4f33
Sergey Poromov <poromov@chromium.org>
2024-06-04 15:28:26 +0000

    [skyvault] Add a note on chrome://management page.
    In case the admin configured all downloads or screen capture to be
    automatically uploaded to cloud storage (Google Drive or OneDrive),
    we need to show a corresponding message to chrome://management page.
    
    Fixed: b/342561417
    Test: New tests added.
    Change-Id: I67f17a220dfabb74a366584af6807a1a60cfba07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569747
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309971}

--- chrome/browser/ash/policy/skyvault/policy_utils.h
+++ chrome/browser/ash/policy/skyvault/policy_utils.h
@@ -6,11 +6,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_POLICY_SKYVAULT_POLICY_UTILS_H_
 #define CHROME_BROWSER_ASH_POLICY_SKYVAULT_POLICY_UTILS_H_
 
+class Profile;
+
 namespace policy::local_user_files {
+
+extern const char kGoogleDrivePolicyVariableName[],
+    kOneDrivePolicyVariableName[];
+
+// Enum describing where the admin configured the files to be saved.
+enum class FileSaveDestination {
+  kNotSpecified = 0,
+  kDownloads = 1,
+  kGoogleDrive = 2,
+  kOneDrive = 3,
+  kMaxValue = kOneDrive,
+};
 
 // Returns whether local user files are enabled on the device by the flag and
 // policy.
 bool LocalUserFilesAllowed();
+
+// Get the destination where downloads are saved.
+FileSaveDestination GetDownloadsDestination(Profile* profile);
+
+// Get the destination where screen captures are saved.
+FileSaveDestination GetScreenCaptureDestination(Profile* profile);
 
 }  // namespace policy::local_user_files
 
