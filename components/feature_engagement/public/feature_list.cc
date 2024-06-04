ae7940815005fedab7d9ed8aed6596aeda33d0c3
aliaaborhamy <aliaaborhamy@google.com>
2024-06-04 23:06:03 +0000

    [Desktop to iOS] Add feature engagement for iOS payment promo.
    This change is to allow other IPH's to explicitly block the
    iOS payment promo bubble on desktop if needed.
    
    Bug: 341994522
    Change-Id: I6dd280cbe0cce442a198289ee5480f722c37c139
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590828
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Aliaa Alborhamy <aliaaborhamy@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310287}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -269,6 +269,7 @@ const base::Feature* const kAllFeatures[] = {
 #if !BUILDFLAG(IS_ANDROID) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
     &kIPHiOSPasswordPromoDesktopFeature,
     &kIPHiOSAddressPromoDesktopFeature,
+    &kIPHiOSPaymentPromoDesktopFeature
 #endif  // !BUILDFLAG(IS_ANDROID) && BUILDFLAG(GOOGLE_CHROME_BRANDING)
 };
 }  // namespace
