de744bbb725e652c9028a8767eca54cbf5f53a9e
Federica Germinario <fedegermi@google.com>
2024-06-04 19:10:48 +0000

    [ios][mim] Refactor kIosSyncSegmentsNewTabPageDisplayCount
    This Cl moves key kIosSyncSegmentsNewTabPageDisplayCount under the
    correct PrefService (with this change it will become profile specific
    instead of being application specific).
    
    Fixed: 343020227
    Change-Id: I02ffd3c41007274406c262bb970519849938f47f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5591700
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310079}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -344,6 +344,22 @@ void MigrateTimePrefFromLocalStatePrefsToProfilePrefs(
   }
 }
 
+// Helper function migrating the `int` preference from LocalState prefs to
+// BrowserState prefs.
+void MigrateIntegerPrefFromLocalStatePrefsToProfilePrefs(
+    std::string_view pref_name,
+    PrefService* profile_pref_service) {
+  PrefService* local_pref_service = GetApplicationContext()->GetLocalState();
+
+  const PrefService::Preference* legacy_pref =
+      local_pref_service->FindPreference(pref_name.data());
+  if (legacy_pref && !legacy_pref->IsDefaultValue()) {
+    profile_pref_service->SetInteger(pref_name.data(),
+                             local_pref_service->GetInteger(pref_name.data()));
+    local_pref_service->ClearPref(pref_name.data());
+  }
+}
+
 }  // namespace
 
 void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
@@ -472,9 +488,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
   // Preference related to the tab pickup feature.
   registry->RegisterBooleanPref(prefs::kTabPickupEnabled, true);
-
-  registry->RegisterIntegerPref(prefs::kIosSyncSegmentsNewTabPageDisplayCount,
-                                0);
 
   // Pref used to store the number of impressions of the Most Visited Sites
   // since a freshness signal of the Most Visited Sites.
@@ -813,6 +826,9 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   // Deprecated 05/2024.
   registry->RegisterBooleanPref(kAutologinEnabled, true);
   registry->RegisterListPref(kReverseAutologinRejectedEmailList);
+
+  registry->RegisterIntegerPref(prefs::kIosSyncSegmentsNewTabPageDisplayCount,
+                                0);
 }
 
 // This method should be periodically pruned of year+ old migrations.
@@ -1033,6 +1049,10 @@ void MigrateObsoleteBrowserStatePrefs(const base::FilePath& state_path,
   // Added 05/2024.
   prefs->ClearPref(kAutologinEnabled);
   prefs->ClearPref(kReverseAutologinRejectedEmailList);
+
+  // Added 06/2024.
+  MigrateIntegerPrefFromLocalStatePrefsToProfilePrefs(
+      prefs::kIosSyncSegmentsNewTabPageDisplayCount, prefs);
 }
 
 void MigrateObsoleteUserDefault() {
