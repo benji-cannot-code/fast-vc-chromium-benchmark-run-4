e82698f9102a278fe408301174a48379403be915
Timofey Chudakov <tchudakov@google.com>
2024-06-04 08:55:09 +0000

    [Android] Use monospaced font for the suggested plus address.
    Screenshot: https://screenshot.googleplex.com/9bbxMxxTWq5TcSe.
    
    Bug: 342126606
    Change-Id: I45736a2fde597a9e5372ceba75baa74eb2dbaeec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593871
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309825}

--- chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationBottomSheetContent.java
+++ chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationBottomSheetContent.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.ui.plus_addresses;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
 import android.text.style.TextAppearanceSpan;
@@ -89,6 +90,9 @@ public class PlusAddressCreationBottomSheetContent implements BottomSheetContent
 
         TextView proposedPlusAddressView = mContentView.findViewById(R.id.proposed_plus_address);
         proposedPlusAddressView.setText(proposedPlusAddressPlaceholder);
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.PLUS_ADDRESS_UI_REDESIGN)) {
+            proposedPlusAddressView.setTypeface(Typeface.MONOSPACE);
+        }
 
         NoUnderlineClickableSpan errorReportLink =
                 new NoUnderlineClickableSpan(
