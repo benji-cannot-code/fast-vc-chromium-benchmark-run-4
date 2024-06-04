f27dcebb02b50b4b8b466af1d2e0c2ebbcecd3fb
Nan Lin <linnan@chromium.org>
2024-06-04 20:49:19 +0000

    Roll wpt javascript
    This rolls wpt javascript to latest commit at
    https://github.com/web-platform-tests/wpt.
    
    Change-Id: Ib7646851622db48e1c72b2db3217284355cf565d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598439
    Auto-Submit: Nan Lin <linnan@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310190}

--- third_party/blink/web_tests/resources/testharness.js
+++ third_party/blink/web_tests/resources/testharness.js
@@ -2175,7 +2175,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             func = funcOrConstructor;
             description = descriptionOrFunc;
             assert(maybeDescription === undefined,
-                   "Too many args passed to no-constructor version of promise_rejects_dom, or accidentally explicitly passed undefined");
+                   "Too many args passed to no-constructor version of assert_throws_dom, or accidentally explicitly passed undefined");
         }
         assert_throws_dom_impl(type, func, description, "assert_throws_dom", constructor)
     }
