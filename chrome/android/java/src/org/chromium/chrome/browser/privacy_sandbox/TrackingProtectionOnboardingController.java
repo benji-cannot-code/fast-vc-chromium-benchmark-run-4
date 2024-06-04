bc458b510b431ecc25b6bb2361e0a1096f834c7c
Matt Dembski <dembski@google.com>
2024-06-04 21:45:30 +0000

    [TrackingProtection][Onboarding][3PCD100%] Basic onboarding controller
    Introduces the Tracking Protection onboarding experience in a very basic state for testing purposes so that once the business logic is implemented in (b/341975190), Clank UI is ready to integrate.
    
    Bug: 341968245
    Change-Id: Ic91a87d34f060cb4cee451b29124a38f6def9f55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584667
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Matt Dembski <dembski@google.com>
    Auto-Submit: Matt Dembski <dembski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310235}

--- chrome/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionOnboardingController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionOnboardingController.java
@@ -39,6 +39,7 @@ public class TrackingProtectionOnboardingController {
 
     private ActivityTabTabObserver mActivityTabTabObserver;
     private TrackingProtectionModeBOnboardingView mTrackingProtectionModeBOnboardingView;
+    private TrackingProtectionOnboardingView mTrackingProtectionOnboardingView;
 
     /**
      * Creates a TrackingProtectionNoticeController.
@@ -101,6 +102,19 @@ public class TrackingProtectionOnboardingController {
     public void maybeOnboard(Tab tab, TrackingProtectionOnboardingType onboardingType) {
         if (onboardingType == TrackingProtectionOnboardingType.MODE_B) {
             maybeOnboardModeB(tab);
+        } else if (onboardingType == TrackingProtectionOnboardingType.TP_FULL_LAUNCH) {
+            maybeOnboardFullLaunch();
+        }
+    }
+
+    // TODO(b/341968245): Add proper logic once b/341975190 is finished.
+    private void maybeOnboardFullLaunch() {
+        try {
+            TrackingProtectionOnboardingView onboardingView = getTrackingProtectionOnboardingView();
+            onboardingView.showNotice(
+                    (b) -> {}, (i) -> {}, () -> PrimaryActionClickBehavior.DISMISS_IMMEDIATELY);
+        } finally {
+            destroy();
         }
     }
 
@@ -153,6 +167,20 @@ public class TrackingProtectionOnboardingController {
                             mTrackingProtectionBridge);
         }
         return mTrackingProtectionModeBOnboardingView;
+    }
+
+    void setTrackingProtectionOnboardingView(
+            TrackingProtectionOnboardingView trackingProtectionOnboardingView) {
+        mTrackingProtectionOnboardingView = trackingProtectionOnboardingView;
+    }
+
+    TrackingProtectionOnboardingView getTrackingProtectionOnboardingView() {
+        if (mTrackingProtectionOnboardingView == null) {
+            mTrackingProtectionOnboardingView =
+                    new TrackingProtectionOnboardingView(
+                            mContext, mMessageDispatcher, mSettingsLauncher);
+        }
+        return mTrackingProtectionOnboardingView;
     }
 
     void setTrackingProtectionModeBOnboardingView(
--- chrome/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionOnboardingType.java
+++ chrome/android/java/src/org/chromium/chrome/browser/privacy_sandbox/TrackingProtectionOnboardingType.java
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.privacy_sandbox;
 
 /** Enum representing different onboarding types for tracking protection. */
-enum TrackingProtectionOnboardingType {
+public enum TrackingProtectionOnboardingType {
     /** Mode B onboarding type. */
     MODE_B,
 
