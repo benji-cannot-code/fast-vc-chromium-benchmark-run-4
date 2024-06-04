36dae79b930e834cc9bca3d3e7decf784639fb85
Aishwarya Rajesh <aishwaryarj@google.com>
2024-06-04 20:32:22 +0000

    [Gardener] Disable QuickDeleteAnimationGradientDrawableRenderTest#testWipeAnimation
    Bug: 344953217
    Change-Id: I351b69a52e8e0816dde468320766a72b98a38732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597883
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Owners-Override: Aishwarya Rajesh <aishwaryarj@google.com>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310173}

--- chrome/browser/quick_delete/android/javatests/src/org/chromium/chrome/browser/quick_delete/QuickDeleteAnimationGradientDrawableRenderTest.java
+++ chrome/browser/quick_delete/android/javatests/src/org/chromium/chrome/browser/quick_delete/QuickDeleteAnimationGradientDrawableRenderTest.java
@@ -25,6 +25,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.BaseActivityTestRule;
 import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Batch;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.night_mode.ChromeNightModeTestUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
@@ -80,6 +81,7 @@ public class QuickDeleteAnimationGradientDrawableRenderTest {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "crbug.com/344953217")
     public void testWipeAnimation() throws Exception {
         View testView = setUpTestView();
         QuickDeleteAnimationGradientDrawable drawable =
