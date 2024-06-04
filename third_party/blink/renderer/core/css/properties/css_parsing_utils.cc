cc3bca2274034553dc4415afce97914d87b4192c
Steinar H. Gunderson <sesse@chromium.org>
2024-06-04 12:04:48 +0000

    Fix !important parsing on contain-type.
    Fixed: 343249146
    Change-Id: I4e2aad294e609ffd91c25be2e0084843e9671f60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5596350
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309867}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -8341,7 +8341,7 @@ CSSValue* ConsumeContainerType(CSSParserTokenStream& stream) {
         continue;
       }
     }
-    return nullptr;
+    break;
   } while (!stream.AtEnd());
 
   CSSValueList* list = CSSValueList::CreateSpaceSeparated();
@@ -8350,6 +8350,9 @@ CSSValue* ConsumeContainerType(CSSParserTokenStream& stream) {
   }
   if (scroll_state_value) {
     list->Append(*scroll_state_value);
+  }
+  if (list->length() == 0) {
+    return nullptr;
   }
   return list;
 }
