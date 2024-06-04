f8c0dc825f7fc6929aeab297cfda3ec992a53ade
Collin Baker <collinbaker@chromium.org>
2024-06-04 22:05:29 +0000

    Remove outdated Mojo Rust code
    The bindings code will be replaced using the code generation framework
    in //mojo/public/tools/bindings. The support code for de/serialization
    will be rewritten, and the checked-in mojom bindings (from what tool
    they came is a mystery) are deleted.
    
    The "run loop" code is also removed, since any Chromium implementation
    will integrate with Chromium's task infrastructure.
    
    Bug: 40206847
    Change-Id: I0fdac8e0ed581c6ba25898ff63c05fb5a9043923
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Change-Id: I0fdac8e0ed581c6ba25898ff63c05fb5a9043923
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594111
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310256}

--- mojo/public/rust/lib.rs
+++ mojo/public/rust/lib.rs
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chromium::import! {
-    pub "//mojo/public/rust:mojo_bindings" as bindings;
     pub "//mojo/public/rust:mojo_system" as system;
 }
 
