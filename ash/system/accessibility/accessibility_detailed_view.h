10f8fe924ca1127f8c21063905fb86356703cefc
Katie Dektar <katie@chromium.org>
2024-06-04 21:29:30 +0000

    [a11y] Add reduced animations to kiosk mode quick settings
    Adds reduced animations setting to kiosk mode quick settings
    (which is accessible from the floating accessibility menu
    in kiosk mode). Setting is not added in login or lock screen.
    
    Screenshots:
    http://screenshot/3tJSrn2SKby2RQ8
    http://screenshot/BViXoTQ5oXcWJkW
    
    Bug: b/344570149
    Test: new, manual
    Change-Id: I0500d1ffb4a16f884915fa98d58fd7452db85941
    AX-Relnotes: N/A (behind flag)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593906
    Commit-Queue: David Yeung <dayeung@chromium.org>
    Auto-Submit: Katie Dektar <katie@chromium.org>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310206}

--- ash/system/accessibility/accessibility_detailed_view.h
+++ ash/system/accessibility/accessibility_detailed_view.h
@@ -95,6 +95,7 @@ class ASH_EXPORT AccessibilityDetailedView
   HoverHighlightView* AddHighlightMouseCursorView(views::View* container);
   HoverHighlightView* AddHighlightKeyboardFocusView(views::View* container);
   HoverHighlightView* AddStickyKeysView(views::View* container);
+  HoverHighlightView* AddReducedAnimationsView(views::View* container);
 
   // Adds a HoverHighlightView to the scroll list and returns it.
   HoverHighlightView* AddScrollListFeatureItem(views::View* container,
@@ -144,6 +145,7 @@ class ASH_EXPORT AccessibilityDetailedView
   raw_ptr<HoverHighlightView> highlight_mouse_cursor_view_ = nullptr;
   raw_ptr<HoverHighlightView> highlight_keyboard_focus_view_ = nullptr;
   raw_ptr<HoverHighlightView> sticky_keys_view_ = nullptr;
+  raw_ptr<HoverHighlightView> reduced_animations_view_ = nullptr;
 
   // Views that appear in the top section listing enabled items. Created if the
   // feature is enabled, otherwise nullptr. Owned by views hierarchy.
@@ -165,6 +167,7 @@ class ASH_EXPORT AccessibilityDetailedView
   raw_ptr<HoverHighlightView> highlight_mouse_cursor_top_view_ = nullptr;
   raw_ptr<HoverHighlightView> highlight_keyboard_focus_top_view_ = nullptr;
   raw_ptr<HoverHighlightView> sticky_keys_top_view_ = nullptr;
+  raw_ptr<HoverHighlightView> reduced_animations_top_view_ = nullptr;
 
   raw_ptr<views::Button> help_view_ = nullptr;
   raw_ptr<views::Button> settings_view_ = nullptr;
