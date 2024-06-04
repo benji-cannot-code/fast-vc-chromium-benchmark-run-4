7e93bfafca454bf40c04708576bd02be7c016d75
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-06-04 17:22:32 +0000

    Import wpt@0a17913e941b243f74180cb95473ae261aa86db8
    https://github.com/web-platform-tests/wpt/compare/93263b60a...0a17913e9
    
    Using wpt-import in Chromium 9485f7e4b73a28c626ccf04f15608abca76e9012.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    hbos@chromium.org, hta@chromium.org, guidou@chromium.org, phancke@microsoft.com:
      external/wpt/webrtc
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ia305b61ce79005b7ee7831540cb9bd08b88a3642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593296
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1310019}

--- third_party/blink/web_tests/external/wpt/webcodecs/temporal-svc-encoding.https.any.js
+++ third_party/blink/web_tests/external/wpt/webcodecs/temporal-svc-encoding.https.any.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
 // META: global=window,dedicatedworker
 // META: script=/webcodecs/video-encoder-utils.js
 // META: variant=?av1
