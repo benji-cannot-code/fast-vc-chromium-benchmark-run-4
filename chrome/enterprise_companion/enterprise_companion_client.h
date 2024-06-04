12ac40e395f3f5477dc0f217812fac67abd1991a
Noah Rose Ledesma <noahrose@google.com>
2024-06-04 15:24:39 +0000

    Create Mojo interface for CEC
    Define a mojom interface and IPC stub. The stub creates a
    NamedMojoIpcServer which clients can connect to via a name exported via
    a thin client library.
    
    Bug: 342180612
    Change-Id: Ic00d66023dc557e01be96ede314a537c20411144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577474
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309968}

---
+++ chrome/enterprise_companion/enterprise_companion_client.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_ENTERPRISE_COMPANION_ENTERPRISE_COMPANION_CLIENT_H_
+#define CHROME_ENTERPRISE_COMPANION_ENTERPRISE_COMPANION_CLIENT_H_
+
+#include "mojo/public/cpp/platform/named_platform_channel.h"
+
+// Utilities useful to clients of Chrome Enterprise Companion.
+
+namespace enterprise_companion {
+
+// Returns the server name for establishing IPC via NamedMojoIpcServer.
+mojo::NamedPlatformChannel::ServerName GetServerName();
+
+}  // namespace enterprise_companion
+
+#endif  // CHROME_ENTERPRISE_COMPANION_ENTERPRISE_COMPANION_CLIENT_H_
