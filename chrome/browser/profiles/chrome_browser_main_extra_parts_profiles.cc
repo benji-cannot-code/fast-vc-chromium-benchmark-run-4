772d0978d9efd12e205ad5b0969233eb60a3178f
Florian Leimgruber <fleimgruber@google.com>
2024-06-04 12:31:37 +0000

    Register PlusAddressSettingService
    Apparently, for each keyed service GetInstance() should get called in
    EnsureBrowserContextKeyedServiceFactoriesBuilt(). This CL adds the
    missing call to PlusAddressSettingService::GetInstance().
    
    The equivalent missing iOS call was added by crrev.com/c/5575381.
    
    Bug: b/342089839
    Change-Id: I57aba91280f20311c934aa45534f122bf557b1c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588406
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1309879}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -125,6 +125,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/persisted_state_db/session_proto_db_factory.h"
 #include "chrome/browser/plugins/plugin_prefs_factory.h"
 #include "chrome/browser/plus_addresses/plus_address_service_factory.h"
+#include "chrome/browser/plus_addresses/plus_address_setting_service_factory.h"
 #include "chrome/browser/policy/cloud/user_cloud_policy_invalidator_factory.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/predictors/loading_predictor_factory.h"
@@ -1040,6 +1041,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   PluginPrefsFactory::GetInstance();
 #endif
   PlusAddressServiceFactory::GetInstance();
+  PlusAddressSettingServiceFactory::GetInstance();
 #if BUILDFLAG(IS_CHROMEOS)
   policy::DlpDownloadObserverFactory::GetInstance();
   policy::DlpRulesManagerFactory::GetInstance();
