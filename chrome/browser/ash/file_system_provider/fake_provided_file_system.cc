3ac1fac1c27f99cf2f1ebada6ba9a588a75f6033
Cassy Chun-Crogan <cassycc@google.com>
2024-06-04 02:20:24 +0000

    Reland "FspCache: Evict file when GetMetadata and OpenFile observe new vTag"
    This reverts commit 0ebb558d97cb2f994dfa0a4528358960342b9ebb.
    
    Reason for revert: This was reverted because it was landed on top of a
    CL that needed to be reverted. This CL has since been relanded at
    CL:5591193.
    
    Original change's description:
    > Revert "FspCache: Evict file when GetMetadata and OpenFile observe new vTag"
    >
    > This reverts commit 4eaa3c9d692f3ede4095891c8b56e1629cca3130.
    >
    > Reason for revert: Child of CL:5581587 which is getting reverted.
    >
    > Original change's description:
    > > FspCache: Evict file when GetMetadata and OpenFile observe new vTag
    > >
    > > Also undo the changes added in CL:5556842 where the check for a new
    > > version tag occurs in ReadBytes (from ReadFile) as this version tag
    > > comes from the OpenFile result and the check now occurs as part of
    > > OpenFile.
    > >
    > > Test: unit_tests --gtest_filter=FileSystemProviderCloudFileSystemTest
    > > Bug: b/339114823
    > > Change-Id: Ie3f953bddebd1ebab5d6e2235c3abab4c32f5993
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584556
    > > Reviewed-by: Ben Reich <benreich@chromium.org>
    > > Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1309134}
    >
    > Bug: b/339114823
    > Change-Id: Ief1010811cc50b07f7703e0533861ab3517baef2
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588768
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1309172}
    
    Bug: b/339114823
    Change-Id: I0f01aaec6860d95905e50a8beb3e89a4ec447492
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595270
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: Cassy Chun-Crogan <cassycc@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1309744}

--- chrome/browser/ash/file_system_provider/fake_provided_file_system.cc
+++ chrome/browser/ash/file_system_provider/fake_provided_file_system.cc
@@ -196,6 +196,12 @@ AbortCallback FakeProvidedFileSystem::GetMetadata(
     metadata->thumbnail =
         std::make_unique<std::string>(*entry_it->second->metadata->thumbnail);
   }
+  // Make a copy of the `CloudFileInfo` to pass to the callback.
+  if (fields & ProvidedFileSystemInterface::METADATA_FIELD_CLOUD_FILE_INFO &&
+      entry_it->second->metadata->cloud_file_info.get()) {
+    metadata->cloud_file_info = std::make_unique<CloudFileInfo>(
+        entry_it->second->metadata->cloud_file_info->version_tag);
+  }
 
   return PostAbortableTask(base::BindOnce(
       std::move(callback), std::move(metadata), base::File::FILE_OK));
@@ -494,6 +500,10 @@ AbortCallback FakeProvidedFileSystem::WriteFile(
     if (!entry->write_buffer) {
       // Only update metadata if we are writing contents directly.
       *entry->metadata->size = offset + length;
+      // Update the version when the contents change.
+      if (entry->metadata->cloud_file_info.get()) {
+        entry->metadata->cloud_file_info->version_tag += "1";
+      }
     }
     write_buffer.resize(*entry->metadata->size);
   }
@@ -663,6 +673,10 @@ FakeProvidedFileSystem::GetWeakPtr() {
 std::unique_ptr<ScopedUserInteraction>
 FakeProvidedFileSystem::StartUserInteraction() {
   return nullptr;
+}
+
+base::WeakPtr<FakeProvidedFileSystem> FakeProvidedFileSystem::GetFakeWeakPtr() {
+  return weak_ptr_factory_.GetWeakPtr();
 }
 
 AbortCallback FakeProvidedFileSystem::PostAbortableTask(
