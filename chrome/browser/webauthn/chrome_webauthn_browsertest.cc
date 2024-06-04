ba967653c6e71db9072f1f05bc3e14b7655befa9
Adam Langley <agl@chromium.org>
2024-06-04 18:47:32 +0000

    Reapply "Add WebAuthenticationEnclaveAuthenticator to fieldtrial_testing_config.json"
    This reverts commit 5ca2989f1e4b11f6b8504b0c0b98d724a08d52d6.
    
    I missed that the test that failed on Windows was a browser test, and
    thus the CQ wouldn't notice whether it failed on macOS because we don't
    run browser tests on macOS. On the plus side, since it fails on macOS I
    can fix it. On the negative side, this was another delay caused by the
    CQ not bothering to run tests macOS.
    
    Bug: 40274370
    Change-Id: I9647d806573fcb9e759d19ebc943a1403bd96124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594497
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310058}

--- chrome/browser/webauthn/chrome_webauthn_browsertest.cc
+++ chrome/browser/webauthn/chrome_webauthn_browsertest.cc
@@ -372,6 +372,8 @@ class WebAuthnGpmPasskeyTest : public WebAuthnBrowserTest {
       return transport_availability_info_;
     }
 
+    void WaitForUI() { run_loop_.Run(); }
+
     // ChromeAuthenticatorRequestDelegate::TestObserver:
     void Created(ChromeAuthenticatorRequestDelegate* delegate) override {}
 
@@ -392,8 +394,7 @@ class WebAuthnGpmPasskeyTest : public WebAuthnBrowserTest {
     }
 
     void UIShown(ChromeAuthenticatorRequestDelegate* delegate) override {
-      delegate->dialog_controller()->OnAccountPreselected(
-          device::fido_parsing_utils::Materialize(kCredentialID));
+      run_loop_.Quit();
     }
 
     void CableV2ExtensionSeen(
@@ -404,6 +405,7 @@ class WebAuthnGpmPasskeyTest : public WebAuthnBrowserTest {
         override {}
 
    private:
+    base::RunLoop run_loop_;
     std::optional<device::FidoRequestHandlerBase::TransportAvailabilityInfo>
         transport_availability_info_;
   };
@@ -415,6 +417,7 @@ class WebAuthnGpmPasskeyTest : public WebAuthnBrowserTest {
 
   void SetUpOnMainThread() override {
     WebAuthnBrowserTest::SetUpOnMainThread();
+    observer_ = std::make_unique<Observer>();
     ChromeAuthenticatorRequestDelegate::SetGlobalObserverForTesting(
         observer_.get());
   }
@@ -425,7 +428,7 @@ class WebAuthnGpmPasskeyTest : public WebAuthnBrowserTest {
   }
 
  protected:
-  std::unique_ptr<Observer> observer_ = std::make_unique<Observer>();
+  std::unique_ptr<Observer> observer_;
   base::test::ScopedFeatureList scoped_feature_list_;
 };
 
@@ -465,10 +468,11 @@ IN_PROC_BROWSER_TEST_F(WebAuthnGpmPasskeyTest, FilterGPMPasskeys) {
           std::move(virtual_device_factory));
 
   // Request an assertion with a credential ID matching only the first passkey.
-  EXPECT_EQ(
-      "webauthn: OK",
-      content::EvalJs(browser()->tab_strip_model()->GetActiveWebContents(),
-                      kGetAssertionCredID1234));
+  content::ExecuteScriptAsync(
+      browser()->tab_strip_model()->GetActiveWebContents(),
+      kGetAssertionCredID1234);
+
+  observer_->WaitForUI();
 
   // Only the first passkey should be in the recognized credentials list.
   device::DiscoverableCredentialMetadata expected(
