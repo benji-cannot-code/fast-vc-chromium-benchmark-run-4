cad96a4f7792a72018a8fb44ca9f9082456666d2
Daseul Lee <dslee@chromium.org>
2024-06-04 21:46:28 +0000

    [FSA] Rename created/deleted change types to appeared/disappeared
    This renaming is motivated by the case in which a move occurred into
    or out of the watched scope should be considered as created/deleted,
    and that naming appeared/disappeared would describe better for events
    with respect to the given scope. Note that this API is not shipped
    yet, and is still under development.
    
    Bug: 341135779
    Change-Id: If7d38a8a96cfba81be7d7647f063cbade510dd51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5553084
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310236}

--- content/browser/file_system_access/file_system_access_observer_browsertest.cc
+++ content/browser/file_system_access/file_system_access_observer_browsertest.cc
@@ -659,13 +659,14 @@ IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
   //
   // TODO(crbug.com/340584120): Consider reporting a consistent change
   // type when writing to a file via a WritableFileStream. On the local file
-  // system, changes are naively considered "created" events because the swap
+  // system, changes are naively considered "appeared" events because the swap
   // file is moved over the target file. Meanwhile, the BucketFS intentionally
   // reports the move as a modification if the move overwrote an existing file.
   const std::string expected_change_type =
       SupportsChangeInfo()
-          ? (GetTestFileSystemType() == TestFileSystemType::kBucket ? "modified"
-                                                                    : "created")
+          ? (GetTestFileSystemType() == TestFileSystemType::kBucket
+                 ? "modified"
+                 : "appeared")
           : "unknown";
   EXPECT_THAT(*records.GetList().front().GetDict().FindString("type"),
               testing::StrEq(expected_change_type));
@@ -846,7 +847,7 @@ IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
-                       ObserveDirectoryReportsCreatedOnMoveIntoScope) {
+                       ObserveDirectoryReportsAppearedOnMoveIntoScope) {
   base::FilePath dir_path = CreateDirectoryToBePicked();
 
   const std::string script =
@@ -871,8 +872,9 @@ IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
   auto& record_dict = records.GetList().front().GetDict();
   const std::string expected_change_type =
       SupportsChangeInfo()
-          ? (GetTestFileSystemType() == TestFileSystemType::kBucket ? "moved"
-                                                                    : "created")
+          ? (GetTestFileSystemType() == TestFileSystemType::kBucket
+                 ? "moved"
+                 : "appeared")
           : "unknown";
   EXPECT_THAT(*record_dict.FindString("type"),
               testing::StrEq(expected_change_type));
@@ -887,7 +889,7 @@ IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
-                       ObserveDirectoryReportsDeletedOnMoveOutsideScope) {
+                       ObserveDirectoryReportsDisappearedOnMoveOutsideScope) {
   base::FilePath dir_path = CreateDirectoryToBePicked();
 
   const std::string script =
@@ -912,10 +914,10 @@ IN_PROC_BROWSER_TEST_P(FileSystemAccessObserverBrowserTest,
   auto& record_dict = records.GetList().front().GetDict();
   // TODO(crbug.com/40105284): Consider reporting a consistent change
   // type when moving a file out of the watched scope. On the BucketFS,
-  // changes are considered "deleted" events while on local file system, it is
-  // reported as "moved".
+  // changes are considered "disappeared" events while on local file system, it
+  // is reported as "moved".
   const std::string expected_change_type =
-      SupportsChangeInfo() ? "deleted" : "unknown";
+      SupportsChangeInfo() ? "disappeared" : "unknown";
   EXPECT_THAT(*record_dict.FindString("type"),
               testing::StrEq(expected_change_type));
   if (SupportsReportingModifiedPath()) {
