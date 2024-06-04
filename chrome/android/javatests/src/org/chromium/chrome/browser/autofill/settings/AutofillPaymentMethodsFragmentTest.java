ff650633361798f8483bce6eb57803221a994823
Qihui Zhao <qihuizhao@google.com>
2024-06-04 13:25:34 +0000

    Update display value for IBAN
    This CL updates GetIdentifierStringForAutofillDisplay() in IBAN class
    so that the displayed value for IBAN is formatted by:
    first2 + formatted dots + last4.
    
    Bug: b:343261998
    Change-Id: I0358e3db3b7bc5c85e6e5c5aa25e87fce5aeca9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5581098
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1309905}

--- chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragmentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/autofill/settings/AutofillPaymentMethodsFragmentTest.java
@@ -1169,7 +1169,7 @@ public class AutofillPaymentMethodsFragmentTest {
         SettingsActivity activity = mSettingsActivityTestRule.startSettingsActivity();
         Preference ibanPreference = getFirstPaymentMethodPreference(activity);
 
-        assertThat(ibanPreference.getTitle().toString()).contains("BE71");
+        assertThat(ibanPreference.getTitle().toString()).contains("BE");
         assertThat(ibanPreference.getSummary().toString()).isEqualTo("My IBAN");
     }
 
@@ -1182,7 +1182,7 @@ public class AutofillPaymentMethodsFragmentTest {
         SettingsActivity activity = mSettingsActivityTestRule.startSettingsActivity();
         Preference ibanPreference = getFirstPaymentMethodPreference(activity);
 
-        assertThat(ibanPreference.getTitle().toString()).contains("RU02");
+        assertThat(ibanPreference.getTitle().toString()).contains("RU");
         assertThat(ibanPreference.getSummary().toString()).contains("");
     }
 
