61fd36abf02a5636833052d0efb76d04b890a7e5
Xinghui Lu <xinghuilu@chromium.org>
2024-06-05 16:13:29 +0000

    [SubresourceCleanup] Remove is_subresource in SBErrorDisplayOptions
    After subresource warnings are disabled, is_subresource is always set
    to false. It is redundant.
    
    TriggerManager set this field to true at the moment, but it is never
    used by TriggerManager.
    
    Bug: 324108312
    Change-Id: Ia731334f3124b5a66ec9ed4021fdde2b507c116c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5596109
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310663}

--- components/security_interstitials/core/base_safe_browsing_error_ui.h
+++ components/security_interstitials/core/base_safe_browsing_error_ui.h
@@ -43,7 +43,6 @@ class BaseSafeBrowsingErrorUI {
 
   struct SBErrorDisplayOptions {
     SBErrorDisplayOptions(bool is_main_frame_load_pending,
-                          bool is_subresource,
                           bool is_extended_reporting_opt_in_allowed,
                           bool is_off_the_record,
                           bool is_extended_reporting_enabled,
@@ -61,9 +60,6 @@ class BaseSafeBrowsingErrorUI {
     // Indicates if this SB interstitial is shown when the main frame load is
     // pending.
     bool is_main_frame_load_pending;
-
-    // Indicates if this SB interstitial is triggered by subresource.
-    bool is_subresource;
 
     // Indicates if user is allowed to opt-in extended reporting preference.
     bool is_extended_reporting_opt_in_allowed;
@@ -122,8 +118,6 @@ class BaseSafeBrowsingErrorUI {
   bool is_main_frame_load_pending() const {
     return display_options_.is_main_frame_load_pending;
   }
-
-  bool is_subresource() const { return display_options_.is_subresource; }
 
   bool is_extended_reporting_opt_in_allowed() const {
     return display_options_.is_extended_reporting_opt_in_allowed;
