e5832fc237c093f02c6d9e63c0917e995c276c28
Alex N. Jose <alexnj@chromium.org>
2024-06-05 12:44:44 +0000

    Deflake long-animation-frame-trace-no-visual-update test
    The test was flaking due to script execution time going above 5ms on
    very slow devices. This CL changes the filter for counting short
    AnimationFrames to <200ms.
    
    Bug: none
    Change-Id: I5bb50874fc79833334b25fe6673bbe4213868528
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599348
    Reviewed-by: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310557}

--- third_party/blink/web_tests/inspector-protocol/timeline/long-animation-frame-trace-no-visual-update.js
+++ third_party/blink/web_tests/inspector-protocol/timeline/long-animation-frame-trace-no-visual-update.js
@@ -54,9 +54,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   checkScriptTraceEvent(
       events, 'AnimationFrame::Script::Execute', loaf_tracing_event.id, 50);
+
+  // We want to assert that there are exactly four short AnimationFrame events with
+  // no scripts counted against. However, on some really slow devices (eg. cpu=1 tests),
+  // this tends to flake with short frames' script execution time going above the 5ms
+  // threshold. In order to deflake, we use a < 200 threshold here to count short frames.
   const short_animation_frame_events = events.filter(
       e => e.name == 'AnimationFrame' &&
-          e.args.animation_frame_timing_info?.num_scripts == 0);
+          (e.args.animation_frame_timing_info?.duration_ms || 0) < 200);
   testRunner.log(`Found matching short LoaF events ${
       short_animation_frame_events.length >= 4 ? '>=4' : '<4'}`);
   const short_frame_ids = short_animation_frame_events.map(({ id }) => id);
