f4f4b981a101c3b8de7a405fefa7df2adcfe8ff5
Orin Jaworski <orinj@google.com>
2024-06-05 23:17:28 +0000

    Split kSearchMinimumWordCount to separate params for query vs. passages
    Per team discussion, finer grained control is needed, so these
    parameters can be set independently:
    - kSearchQueryMinimumWordCount
    - kSearchPassageMinimumWordCount
    
    Bug: 342666969
    Change-Id: Id7edb46022825b24dc524bb5960e6ddfaf680efe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586378
    Reviewed-by: Jun Zou <junzou@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310967}

--- components/history_embeddings/history_embeddings_features.cc
+++ components/history_embeddings/history_embeddings_features.cc
@@ -24,9 +24,14 @@ const base::FeatureParam<int> kPassageExtractionMaxWordsPerAggregatePassage(
     "PassageExtractionMaxWordsPerAggregatePassage",
     200);
 
-const base::FeatureParam<int> kSearchMinimumWordCount(&kHistoryEmbeddings,
-                                                      "SearchMinimumWordCount",
-                                                      3);
+const base::FeatureParam<int> kSearchQueryMinimumWordCount(
+    &kHistoryEmbeddings,
+    "SearchQueryMinimumWordCount",
+    3);
+const base::FeatureParam<int> kSearchPassageMinimumWordCount(
+    &kHistoryEmbeddings,
+    "SearchPassageMinimumWordCount",
+    5);
 
 const base::FeatureParam<int> kSearchResultItemCount(&kHistoryEmbeddings,
                                                      "SearchResultItemCount",
