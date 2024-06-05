339e5f7b1b1671608927b9a3679d139bb77ae167
Erik Chen <erikchen@chromium.org>
2024-06-05 06:07:51 +0000

    Modularize //chrome/browser/search_engines.
    This CL has no intended behavior change.
    
    This CL makes two small additional changes:
    * Modularize chrome/browser/android/locale.
    * Move a call to ProfileManager::GetActiveUserProfile() to its caller
      LocaleTemplateUrlLoader. This is still not optimal, but is better than
      the status quo.
    
    Change-Id: Idfca4ffdfff3ca60b9f1555ab47c98b4f14c35a4
    Bug: 343754417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586050
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310434}

--- chrome/browser/android/locale/locale_manager.cc
+++ chrome/browser/android/locale/locale_manager.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/android/locale/locale_manager.h"
 
+#include <jni.h>
+
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
 #include "chrome/browser/locale/jni_headers/LocaleManager_jni.h"
--- chrome/browser/locale/java/src/org/chromium/chrome/browser/locale/LocaleTemplateUrlLoader.java
+++ chrome/browser/locale/java/src/org/chromium/chrome/browser/locale/LocaleTemplateUrlLoader.java
@@ -5,11 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.locale;
 
+import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.profiles.ProfileManager;
+
 /**
- * A loader class for changes of template url in a given special locale. This is a JNI bridge and
- * it owns the native object. Make sure to call destroy() after this object is not used anymore.
+ * A loader class for changes of template url in a given special locale. This is a JNI bridge and it
+ * owns the native object. Make sure to call destroy() after this object is not used anymore.
  */
 public class LocaleTemplateUrlLoader {
     private final String mLocaleId;
@@ -23,7 +27,10 @@ public class LocaleTemplateUrlLoader {
     public LocaleTemplateUrlLoader(String localeId) {
         assert localeId.length() == 2;
         mLocaleId = localeId;
-        mNativeLocaleTemplateUrlLoader = LocaleTemplateUrlLoaderJni.get().init(localeId);
+        // TODO(b/344633755): Remove getLastUsedRegularProfile().
+        mNativeLocaleTemplateUrlLoader =
+                LocaleTemplateUrlLoaderJni.get()
+                        .init(localeId, ProfileManager.getLastUsedRegularProfile());
     }
 
     /** This *must* be called after the {@link LocaleTemplateUrlLoader} is not used anymore. */
@@ -73,7 +80,7 @@ public class LocaleTemplateUrlLoader {
 
     @NativeMethods
     interface Natives {
-        long init(String localeId);
+        long init(String localeId, @JniType("Profile*") Profile profile);
 
         void destroy(long nativeLocaleTemplateUrlLoader);
 
