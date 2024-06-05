51ccb7e9cf07d1fa39221f38dfef16a5a0bd5157
Olivier Robin <olivierrobin@google.com>
2024-06-05 14:05:18 +0000

    Integrate TR2 with Chrome iOS
    - Create the factory for the new service
    - integrate with the TabResumptionMediator
    - Create flags
    
    Low-Coverage-Reason: HARD_TO_TEST: about_flags.mm cannot be tested
    Fixed: 342389603, 342389884, 342392587
    Change-Id: If55937c7f4fef2da10e284225f068f659a693be6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5497198
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310593}

--- ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -144,6 +144,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/trusted_vault/model/ios_trusted_vault_service_factory.h"
 #import "ios/chrome/browser/unified_consent/model/unified_consent_service_factory.h"
 #import "ios/chrome/browser/unit_conversion/unit_conversion_service_factory.h"
+#import "ios/chrome/browser/visited_url_ranking/model/visited_url_ranking_service_factory.h"
 #import "ios/chrome/browser/voice/ui_bundled/text_to_speech_playback_controller_factory.h"
 #import "ios/chrome/browser/web/model/java_script_console/java_script_console_feature_factory.h"
 #import "ios/chrome/browser/web/model/session_state/web_session_state_cache_factory.h"
@@ -306,6 +307,7 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   UnitConversionServiceFactory::GetInstance();
   UrlLanguageHistogramFactory::GetInstance();
   VerdictCacheManagerFactory::GetInstance();
+  VisitedURLRankingServiceFactory::GetInstance();
   WebSessionStateCacheFactory::GetInstance();
   // Keep the above list alphabetized! Don't just add new entries at the end.
 
--- ios/chrome/browser/shared/public/features/features.h
+++ ios/chrome/browser/shared/public/features/features.h
@@ -539,6 +539,9 @@ BASE_DECLARE_FEATURE(kTabResumption);
 // Feature that enables enhancements for Tab Resumption.
 BASE_DECLARE_FEATURE(kTabResumption1_5);
 
+// Feature that enables tab resumption 2.0.
+BASE_DECLARE_FEATURE(kTabResumption2);
+
 // A parameter to indicate whether the Most Visited Tiles should be in the Magic
 // Stack.
 extern const char kMagicStackMostVisitedModuleParam[];
@@ -577,6 +580,10 @@ CGFloat HomeModuleMinimumPadding();
 
 // Whether the tab resumption feature is enabled.
 bool IsTabResumptionEnabled();
+
+// Whether the tab resumption feature is enabled in 2.0 version. Implies
+// `IsTabResumptionEnabled`.
+bool IsTabResumption2_0Enabled();
 
 // Whether the tab resumption feature is enabled for most recent tab only.
 bool IsTabResumptionEnabledForMostRecentTabOnly();
