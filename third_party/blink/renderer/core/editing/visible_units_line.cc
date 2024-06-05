0a6bfe060d3c199a4af6013896d60f30bd06172b
zhoupeng <zhoupeng.1996@bytedance.com>
2024-06-05 03:23:21 +0000

    InSameLine should be capable of accurately handling situations that involve the inline-block node
    Consider this test case:
    <div>first line</div>
    this<span style="display: inline-block">i|s</span>broken
    <div>last line</div>
    
    Move the caret to the previous line by pressing the up button,
    but the caret position remains unchanged.
    
    `InSameLine` will determine whether the two positions are in the
    same inline formatting context. However, the span node is an
    inline-block node, so it is in a different inline formatting context
    than #text"this".
    As a user, the span node is located on the same line as #text"this".
    When the anchor node of `position` is an inline-block node,
    `InSameLine` should determine whether the two positions are in the
    same inline contents.
    
    Bug: 41497469
    Change-Id: I196fecf2fb41f7126a339744f9be88e790dff164
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582749
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Peng Zhou <zhoupeng.1996@bytedance.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310397}

--- third_party/blink/renderer/core/editing/visible_units_line.cc
+++ third_party/blink/renderer/core/editing/visible_units_line.cc
@@ -34,10 +34,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/editing/editing_utilities.h"
 #include "third_party/blink/renderer/core/editing/inline_box_position.h"
 #include "third_party/blink/renderer/core/editing/ng_flat_tree_shorthands.h"
+#include "third_party/blink/renderer/core/editing/text_offset_mapping.h"
 #include "third_party/blink/renderer/core/editing/visible_position.h"
 #include "third_party/blink/renderer/core/layout/inline/inline_caret_position.h"
 #include "third_party/blink/renderer/core/layout/inline/line_utils.h"
 #include "third_party/blink/renderer/core/layout/inline/offset_mapping.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
 namespace blink {
 
@@ -229,6 +231,13 @@ PositionWithAffinityTemplate<Strategy> StartOfLineAlgorithm(
       vis_pos, c.GetPosition());
 }
 
+bool IsInlineBlock(const LayoutBlockFlow* block_flow) {
+  if (!block_flow) {
+    return false;
+  }
+  return block_flow->StyleRef().Display() == EDisplay::kInlineBlock;
+}
+
 }  // namespace
 
 PositionWithAffinity StartOfLine(const PositionWithAffinity& current_position) {
@@ -386,11 +395,24 @@ static bool InSameLineAlgorithm(
   const LayoutBlockFlow* block2 =
       NGInlineFormattingContextOf(position2.GetPosition());
   if (block1 || block2) {
-    if (block1 != block2) {
-      return false;
-    }
-    if (!InSameNGLineBox(position1, position2)) {
-      return false;
+    if (RuntimeEnabledFeatures::InlineBlockInSameLineEnabled() &&
+        (IsInlineBlock(block1) || IsInlineBlock(block2))) {
+      const TextOffsetMapping::InlineContents inline_contents1 =
+          TextOffsetMapping::FindForwardInlineContents(
+              ToPositionInFlatTree(position1.GetPosition()));
+      const TextOffsetMapping::InlineContents inline_contents2 =
+          TextOffsetMapping::FindForwardInlineContents(
+              ToPositionInFlatTree(position2.GetPosition()));
+      if (inline_contents1 != inline_contents2) {
+        return false;
+      }
+    } else {
+      if (block1 != block2) {
+        return false;
+      }
+      if (!InSameNGLineBox(position1, position2)) {
+        return false;
+      }
     }
     // See (ParameterizedVisibleUnitsLineTest.InSameLineWithMixedEditability
     return RootEditableElementOf(position1.GetPosition()) ==
