e0b4a3490be89ee030461d8b71ba854b67e41db3
Ramin Halavati <rhalavati@chromium.org>
2024-06-05 05:12:39 +0000

    Remove PDF OCR context menu.
    PDF OCR will be always enabled and the context menu is no more needed.
    
    AX-Relnotes: PDF OCR context menu is removed.
    Bug: 341856144
    Change-Id: I52772ccd6eda6913ac3b816f84d6092be6f5a2cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593793
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310419}

--- chrome/browser/guest_view/mime_handler_view/chrome_mime_handler_view_guest_delegate.cc
+++ chrome/browser/guest_view/mime_handler_view/chrome_mime_handler_view_guest_delegate.cc
@@ -51,7 +51,7 @@ void ChromeMimeHandlerViewGuestDelegate::RecordLoadMetric(
                           ? PDFLoadStatus::kLoadedFullPagePdfWithPdfium
                           : PDFLoadStatus::kLoadedEmbeddedPdfWithPdfium);
 
-  accessibility::RecordPDFOpenedWithA11yFeatureWithPdfOcr(browser_context);
+  accessibility::RecordPDFOpenedWithA11yFeatureWithPdfOcr();
 }
 
 }  // namespace extensions
--- chrome/browser/ui/actions/chrome_action_id.h
+++ chrome/browser/ui/actions/chrome_action_id.h
@@ -468,10 +468,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   /* Screen AI layout extraction. */ \
   E(kActionContentContextRunLayoutExtraction, \
     IDC_CONTENT_CONTEXT_RUN_LAYOUT_EXTRACTION) \
-  /* PDF OCR */ \
-  E(kActionContentContextPdfOcr, IDC_CONTENT_CONTEXT_PDF_OCR) \
-  E(kActionContentContextPdfOcrAlways, IDC_CONTENT_CONTEXT_PDF_OCR_ALWAYS) \
-  E(kActionContentContextPdfOcrOnce, IDC_CONTENT_CONTEXT_PDF_OCR_ONCE) \
   /* Tab Search */ \
   E(kActionTabSearch, IDC_TAB_SEARCH) \
   E(kActionTabSearchClose, IDC_TAB_SEARCH_CLOSE) \
