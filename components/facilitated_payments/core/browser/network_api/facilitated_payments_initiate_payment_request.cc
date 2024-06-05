7d5b020ac1959d411d84b760af56ab398841e001
siashah <siashah@chromium.org>
2024-06-05 17:22:10 +0000

    Encode the client token as base64
    The client token is not UTF8 compliant so converting to string fails.
    Also, the server seems to work fine with the base64 encoded client
    token so we probably needed to this anyway.
    
    Bug: b/344026364
    Change-Id: I60a458811df457a646a5977ff53ecd18855441e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597572
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310711}

--- components/facilitated_payments/core/browser/network_api/facilitated_payments_initiate_payment_request.cc
+++ components/facilitated_payments/core/browser/network_api/facilitated_payments_initiate_payment_request.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/base64.h"
 #include "base/json/json_writer.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
@@ -58,9 +59,9 @@ std::string FacilitatedPaymentsInitiatePaymentRequest::GetRequestContent() {
   risk_data.Set("value", request_details_->risk_data_);
   request_dict.Set("risk_data_encoded", std::move(risk_data));
 
-  request_dict.Set("client_token", base::Value(std::string(
-                                       request_details_->client_token_.begin(),
-                                       request_details_->client_token_.end())));
+  request_dict.Set(
+      "client_token",
+      base::Value(base::Base64Encode(request_details_->client_token_)));
 
   base::Value::Dict context;
   context.Set("language_code", app_locale_);
--- components/facilitated_payments/core/browser/network_api/facilitated_payments_initiate_payment_request_unittest.cc
+++ components/facilitated_payments/core/browser/network_api/facilitated_payments_initiate_payment_request_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/base64.h"
 #include "base/functional/callback_helpers.h"
 #include "base/json/json_reader.h"
 #include "base/values.h"
@@ -23,6 +24,8 @@ TEST_F(FacilitatedPaymentsInitiatePaymentRequestTest,
   auto request_details_full =
       std::make_unique<FacilitatedPaymentsInitiatePaymentRequestDetails>();
   request_details_full->risk_data_ = "seems pretty risky";
+  // The client token will be base64 encoded as "dG9rZW4=" in the request
+  // content.
   request_details_full->client_token_ =
       std::vector<uint8_t>{'t', 'o', 'k', 'e', 'n'};
   request_details_full->billing_customer_number_ = 11;
@@ -42,7 +45,8 @@ TEST_F(FacilitatedPaymentsInitiatePaymentRequestTest,
   EXPECT_EQ(
       request->GetRequestContent(),
       "{\"chrome_user_context\":{\"full_sync_enabled\":true},\"client_token\":"
-      "\"token\",\"context\":{\"billable_service\":70154,\"customer_context\":{"
+      "\"dG9rZW4=\",\"context\":{\"billable_service\":70154,\"customer_"
+      "context\":{"
       "\"external_customer_id\":\"11\"},\"language_code\":\"US\"},\"merchant_"
       "info\":{\"merchant_checkout_page_url\":\"https://foo.com/"
       "bar\"},\"payment_details\":{\"payment_rail\":\"PIX\",\"qr_code\":\"a "
@@ -59,6 +63,8 @@ TEST_F(FacilitatedPaymentsInitiatePaymentRequestTest,
   // `billing_customer_number_` and `merchant_payment_page_url_` optional fields
   // are not set.
   request_details_without_optional_data->risk_data_ = "seems pretty risky";
+  // The client token will be base64 encoded as "dG9rZW4=" in the request
+  // content.
   request_details_without_optional_data->client_token_ =
       std::vector<uint8_t>{'t', 'o', 'k', 'e', 'n'};
   request_details_without_optional_data->instrument_id_ = 13;
@@ -79,7 +85,7 @@ TEST_F(FacilitatedPaymentsInitiatePaymentRequestTest,
   // be absent.
   EXPECT_EQ(request->GetRequestContent(),
             "{\"chrome_user_context\":{\"full_sync_enabled\":true},\"client_"
-            "token\":\"token\",\"context\":{\"billable_service\":70154,"
+            "token\":\"dG9rZW4=\",\"context\":{\"billable_service\":70154,"
             "\"language_code\":\"US\"},\"payment_details\":{\"payment_rail\":"
             "\"PIX\",\"qr_code\":\"a valid "
             "code\"},\"risk_data_encoded\":{\"encoding_type\":\"BASE_64\","
