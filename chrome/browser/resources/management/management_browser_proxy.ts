0199744ba148623bff7fd0dd620b518b7260480a
Georg Neis <neis@chromium.org>
2024-06-05 04:35:15 +0000

    Revert "[skyvault] Add a note on chrome://management page."
    This reverts commit e886322051ec5d41b9d002ed90670a6fba2d58f3.
    
    Reason for revert: b/344933567
    
    Original change's description:
    > [skyvault] Add a note on chrome://management page.
    >
    > In case the admin configured all downloads or screen capture to be
    > automatically uploaded to cloud storage (Google Drive or OneDrive),
    > we need to show a corresponding message to chrome://management page.
    >
    > Fixed: b/342561417
    > Test: New tests added.
    > Change-Id: I67f17a220dfabb74a366584af6807a1a60cfba07
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5569747
    > Commit-Queue: Sergey Poromov <poromov@chromium.org>
    > Reviewed-by: Martin Šrámek <msramek@chromium.org>
    > Reviewed-by: Aida Zolic <aidazolic@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1309971}
    
    Bug: b:344933567, b:342561417
    Change-Id: I60ee0d81482af46df61ce654626f0dac17fddb30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599382
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Owners-Override: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310410}

--- chrome/browser/resources/management/management_browser_proxy.ts
+++ chrome/browser/resources/management/management_browser_proxy.ts
@@ -107,12 +107,6 @@ export interface ManagementBrowserProxy {
   getLocalTrustRootsInfo(): Promise<boolean>;
 
   /**
-   * @return Whether uploading of downloads or screenshots to cloud storages is
-   *     configured.
-   */
-  getFilesUploadToCloudInfo(): Promise<string>;
-
-  /**
    * @return List of items to display in device reporting section.
    */
   getDeviceReportingInfo(): Promise<DeviceReportingResponse[]>;
@@ -154,10 +148,6 @@ export class ManagementBrowserProxyImpl implements ManagementBrowserProxy {
   // <if expr="is_chromeos">
   getLocalTrustRootsInfo() {
     return sendWithPromise('getLocalTrustRootsInfo');
-  }
-
-  getFilesUploadToCloudInfo() {
-    return sendWithPromise('getFilesUploadToCloudInfo');
   }
 
   getDeviceReportingInfo() {
