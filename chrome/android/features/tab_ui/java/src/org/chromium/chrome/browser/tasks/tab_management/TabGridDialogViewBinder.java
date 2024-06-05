556e5b0a623769fe763f454689dda22e322a0cc9
Calder Kitagawa <ckitagawa@chromium.org>
2024-06-05 22:19:44 +0000

    [Tab Switcher] Fix flicker in list mode when expanding/shrinking group
    This is technically a longstanding bug. In grid mode,
    TabGridDialogView#setupDialogAnimation is called with a provided source
    view that is copied for the animation. This allows the shrink and
    expand animation to work correctly.
    
    In list mode this behavior is skipped because the view type in list
    mode is incompatible with the expected view. Instead we just show a
    colors rectangle going between the list entry and the dialog. However,
    the color of that entry was, by default, the Window color which is
    white in light mode and black in dark mode.
    
    On incognito it remains white in light mode and looks awful.
    
    This CL explicitly sets a color for the animated version of the card's
    background so that it looks less jarring. Longer term we should
    probably revisit how this animation is created, but that will take
    way longer than just fixing this small bug.
    
    Demo
    https://drive.google.com/file/d/1XQtGTdr8fNtnHFIEb9MVdm4JDq6lrPxa/view?usp=sharing
    
    Fixed: 345091924
    Change-Id: I4e7e205574607fcf92acd6b08e91e72119012dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600445
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1310936}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogViewBinder.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tasks.tab_management;
 
 import static org.chromium.chrome.browser.tasks.tab_management.TabGridDialogProperties.ADD_CLICK_LISTENER;
+import static org.chromium.chrome.browser.tasks.tab_management.TabGridDialogProperties.ANIMATION_BACKGROUND_COLOR;
 import static org.chromium.chrome.browser.tasks.tab_management.TabGridDialogProperties.ANIMATION_SOURCE_VIEW;
 import static org.chromium.chrome.browser.tasks.tab_management.TabGridDialogProperties.BINDING_TOKEN;
 import static org.chromium.chrome.browser.tasks.tab_management.TabGridDialogProperties.BROWSER_CONTROLS_STATE_PROVIDER;
@@ -249,6 +250,13 @@ class TabGridDialogViewBinder {
                 // Fit the scrim to the TabGridDialog again after the bottom sheet visibility
                 // changes.
                 viewHolder.dialogView.refreshScrim();
+            }
+        } else if (ANIMATION_BACKGROUND_COLOR == propertyKey) {
+            // Only set in LIST mode not GRID mode. Will always be set in LIST mode. Mode is not
+            // mutable without restarting the app.
+            if (model.get(ANIMATION_BACKGROUND_COLOR) != null) {
+                viewHolder.dialogView.updateAnimationBackgroundColor(
+                        model.get(ANIMATION_BACKGROUND_COLOR));
             }
         }
     }
