e10c3e32096d9d870eff430a84d0eee97270b26b
Tsuyoshi Horo <horo@chromium.org>
2024-06-05 03:18:33 +0000

    Remove PlaceholderImage
    The placeholder image logic was introduced by [1]. But this is not used
    anymore after [2].
    [1]: https://crrev.com/2423683002
    [2]: https://crrev.com/c/2125570
    
    Bug: 40686947
    Change-Id: Ie2121fdd4b1363c70354b499ec076be883710649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595509
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310395}

--- third_party/blink/renderer/core/paint/image_painter.cc
+++ third_party/blink/renderer/core/paint/image_painter.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/paint/display_item_cache_skipper.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/path.h"
-#include "third_party/blink/renderer/platform/graphics/placeholder_image.h"
 #include "third_party/blink/renderer/platform/graphics/scoped_image_rendering_settings.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
--- third_party/blink/renderer/core/style/style_fetched_image.cc
+++ third_party/blink/renderer/core/style/style_fetched_image.cc
@@ -38,7 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/svg/graphics/svg_image.h"
 #include "third_party/blink/renderer/core/svg/graphics/svg_image_for_container.h"
 #include "third_party/blink/renderer/platform/graphics/bitmap_image.h"
-#include "third_party/blink/renderer/platform/graphics/placeholder_image.h"
 
 namespace blink {
 
@@ -254,11 +253,6 @@ scoped_refptr<Image> StyleFetchedImage::GetImage(
     const ComputedStyle& style,
     const gfx::SizeF& target_size) const {
   Image* image = image_->GetImage();
-  if (image->IsPlaceholderImage()) {
-    static_cast<PlaceholderImage*>(image)->SetIconAndTextScaleFactor(
-        style.EffectiveZoom());
-  }
-
   auto* svg_image = DynamicTo<SVGImage>(image);
   if (!svg_image) {
     return image;
