d5668ac99637861c36bf75fb6cd9ed7677d16dc0
Sean Kau <skau@chromium.org>
2024-06-05 17:25:43 +0000

    Add a policy for Focus Mode Sounds
    Policy currently allows for 3 states: Everything (YTM + Focus Sounds)
    enabled, just Focus Sounds (no YTM), or Disabled (Sounds completely
    disabled). Updates to the UI to follow.
    
    Bug: b:328121041
    Change-Id: Ia673b7ab6a7d94a68eff5dc2d6582c2c545d95b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547974
    Auto-Submit: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310713}

--- ash/system/focus_mode/focus_mode_controller.cc
+++ ash/system/focus_mode/focus_mode_controller.cc
@@ -185,6 +185,10 @@ void FocusModeController::RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterDictionaryPref(
       prefs::kFocusModeSoundSection,
       user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+
+  // Pref only set via policy.
+  registry->RegisterStringPref(prefs::kFocusModeSoundsEnabled,
+                               focus_mode_util::kFocusModeSoundsEnabled);
 }
 
 void FocusModeController::AddObserver(Observer* observer) {
