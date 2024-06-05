f5ed551e25199e1e3f602d839285d289adac9bcb
Michele Fan <michelefan@chromium.org>
2024-06-05 06:54:13 +0000

    snap-group: Fix desk removal after moving Snap Group to all desks crash
    Demo: http://b/344982754#comment3
    Fixed: b/344982754
    Test: Added regression test (fail with same stack track) + manually
    Change-Id: I63c4ce8ffb02f25fbe3066cbcfab98596c095f37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597927
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310448}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_grid.h"
 #include "ash/wm/overview/overview_item.h"
 #include "ash/wm/overview/scoped_overview_hide_windows.h"
+#include "ash/wm/snap_group/snap_group_controller.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_utils.h"
 #include "ash/wm/switchable_windows.h"
@@ -991,6 +992,14 @@ void DesksController::AddVisibleOnAllDesksWindow(aura::Window* window) {
   // added.
   if (!visible_on_all_desks_windows_.emplace(window).second)
     return;
+
+  if (SnapGroupController* snap_group_controller = SnapGroupController::Get()) {
+    if (SnapGroup* snap_group =
+            snap_group_controller->GetSnapGroupForGivenWindow(window)) {
+      snap_group_controller->RemoveSnapGroup(
+          snap_group, SnapGroupExitPoint::kVisibleOnAllDesks);
+    }
+  }
 
   // A window is made visible on all desks by always keeping it on the active
   // desk. If `window` isn't already on the active desk, then we need to move it
--- ash/wm/snap_group/snap_group_controller.cc
+++ ash/wm/snap_group/snap_group_controller.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
+#include "ash/wm/desks/desks_util.h"
 #include "ash/wm/mru_window_tracker.h"
 #include "ash/wm/overview/overview_controller.h"
 #include "ash/wm/overview/overview_utils.h"
@@ -90,6 +91,13 @@ SnapGroup* SnapGroupController::AddSnapGroup(
 
   // We only allow snap group to be created if the windows fit the work area.
   if (!CanWindowsFitInWorkArea(window1, window2)) {
+    return nullptr;
+  }
+
+  // Disallow forming a Snap Group if either of the windows is configured to be
+  // "visible on all workspaces".
+  if (desks_util::IsWindowVisibleOnAllWorkspaces(window1) ||
+      desks_util::IsWindowVisibleOnAllWorkspaces(window2)) {
     return nullptr;
   }
 
