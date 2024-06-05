a437189db5176e1d30287155e2119d58806d9969
Zhe Li <zheliooo@google.com>
2024-06-05 23:24:58 +0000

    [TabGroupCollapse] Fix bottom indicator sometimes expanded when group is collapsed
    Bug: 345254942
    Change-Id: Ic4a1cf03cbfab7b28715587e819db0373947d69f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599008
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1310971}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -2761,7 +2761,7 @@ public class StripLayoutHelper implements StripLayoutTabDelegate, StripLayoutGro
      */
     private float calculateBottomIndicatorWidth(
             StripLayoutGroupTitle groupTitle, int numOfTabsInGroup) {
-        if (groupTitle == null || numOfTabsInGroup == 0) {
+        if (groupTitle == null || groupTitle.isCollapsed() || numOfTabsInGroup == 0) {
             return 0.f;
         }
         float tabWidth = mCachedTabWidth - mTabOverlapWidth;
