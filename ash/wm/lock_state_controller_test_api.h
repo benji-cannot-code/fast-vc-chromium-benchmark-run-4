e219686b1af0e2dc3d016038a5f44a367e28f8b8
Eriko Kurimoto <elkurin@chromium.org>
2024-06-05 02:02:04 +0000

    ash: Fix LockStateController dangling ptr
    Bug: 343842938
    Change-Id: I8ebbe8716e7e7b62f22ca7ac28e839461ae39b48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588030
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310366}

--- ash/wm/lock_state_controller_test_api.h
+++ ash/wm/lock_state_controller_test_api.h
@@ -54,7 +54,7 @@ class LockStateControllerTestApi {
   }
 
  private:
-  raw_ptr<LockStateController, DanglingUntriaged> controller_;  // not owned
+  raw_ptr<LockStateController> controller_;  // not owned
 };
 
 }  // namespace ash
