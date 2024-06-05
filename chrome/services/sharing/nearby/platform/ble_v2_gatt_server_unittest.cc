8eb122fcfa647d515bdcbc674391c806dd8ec9ec
Juliet Levesque <julietlevesque@google.com>
2024-06-05 19:18:15 +0000

    [Nearby Presence] Add metric for creating GATT characteristics result
    Adds a metric to capture the success/failure of creating a GATT
    characteristic.
    
    Tested: unit tests
    Bug: b/338108513, b/311430390
    Change-Id: Iaf28b6aef0af1a85ac70b86a06e0a0462a4a81f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502931
    Commit-Queue: Juliet Lévesque <julietlevesque@google.com>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310808}

--- chrome/services/sharing/nearby/platform/ble_v2_gatt_server_unittest.cc
+++ chrome/services/sharing/nearby/platform/ble_v2_gatt_server_unittest.cc
@@ -158,6 +158,9 @@ TEST_F(BleV2GattServerTest,
     histogram_tester_.ExpectBucketCount(
         "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
         /*bucket: success=*/1, 1);
+    histogram_tester_.ExpectBucketCount(
+        "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
+        /*bucket: success=*/1, 1);
   }
 
   // Second time, expect no call to browser process since it already
@@ -172,6 +175,9 @@ TEST_F(BleV2GattServerTest,
     histogram_tester_.ExpectBucketCount(
         "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
         /*bucket: success=*/1, 1);
+    histogram_tester_.ExpectBucketCount(
+        "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
+        /*bucket: success=*/1, 2);
   }
 
   EXPECT_EQ(2, fake_gatt_service_ptr->GetNumCharacteristicUuids());
@@ -188,6 +194,9 @@ TEST_F(BleV2GattServerTest, CreateCharacteristic_Success) {
   histogram_tester_.ExpectBucketCount(
       "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
       /*bucket: success=*/1, 1);
+  histogram_tester_.ExpectBucketCount(
+      "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
+      /*bucket: success=*/1, 1);
 }
 
 TEST_F(BleV2GattServerTest, CreateCharacteristic_Failure) {
@@ -201,6 +210,9 @@ TEST_F(BleV2GattServerTest, CreateCharacteristic_Failure) {
   histogram_tester_.ExpectBucketCount(
       "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
       /*bucket: success=*/1, 1);
+  histogram_tester_.ExpectBucketCount(
+      "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
+      /*bucket: success=*/0, 1);
 }
 
 TEST_F(BleV2GattServerTest,
@@ -221,6 +233,9 @@ TEST_F(BleV2GattServerTest,
     histogram_tester_.ExpectBucketCount(
         "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
         /*bucket: success=*/1, 1);
+    histogram_tester_.ExpectBucketCount(
+        "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
+        /*bucket: success=*/1, 1);
   }
 
   // Second time, expect no call to browser process since it already
@@ -232,6 +247,9 @@ TEST_F(BleV2GattServerTest,
     EXPECT_EQ(1, fake_gatt_service_ptr->GetNumCharacteristicUuids());
     histogram_tester_.ExpectBucketCount(
         "Nearby.Connections.BleV2.GattServer.CreateLocalGattService.Result",
+        /*bucket: success=*/1, 1);
+    histogram_tester_.ExpectBucketCount(
+        "Nearby.Connections.BleV2.GattServer.CreateCharacteristic.Result",
         /*bucket: success=*/1, 1);
   }
 }
