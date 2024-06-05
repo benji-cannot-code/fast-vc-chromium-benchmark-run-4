ac3a91194ac8e6210685af2b225c09baf5d30205
Jimmy <jimmyxgong@chromium.org>
2024-06-05 18:00:44 +0000

    cros-preview: Start preview generation in PrintPreviewUiWrapper
    Implement the initial step to generating a print preview.
    A followup will consume the generated preview and composite
    it into a pdf.
    
    Bug: 334993067
    Test: unit_tests, local
    Change-Id: I5b7598377db0816a42a6b79da23840020e20e65a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5547018
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Commit-Queue: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310747}

--- chrome/browser/chromeos/printing/print_preview/print_view_manager_cros.h
+++ chrome/browser/chromeos/printing/print_preview/print_view_manager_cros.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_PRINTING_PRINT_PREVIEW_PRINT_VIEW_MANAGER_CROS_H_
 
 #include "base/unguessable_token.h"
+#include "base/values.h"
+#include "chrome/browser/chromeos/printing/print_preview/print_preview_ui_wrapper.h"
 #include "chrome/browser/chromeos/printing/print_preview/print_view_manager_cros_base.h"
 #include "components/printing/common/print.mojom-forward.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -59,6 +61,8 @@ class PrintViewManagerCros
   bool PrintPreviewNow(content::RenderFrameHost* rfh, bool has_selection);
   void PrintPreviewDone();
 
+  // Start the print preview generation.
+  void HandleGeneratePrintPreview(const base::Value::Dict& settings);
   // Inform the PrintRenderFrame that the dialog has been removed and clears out
   // the render frame host associated with this instance.
   void HandlePrintPreviewRemoved();
@@ -68,6 +72,7 @@ class PrintViewManagerCros
   }
 
  private:
+  friend class PrintViewManagerCrosTest;
   friend class chromeos::printing::PrintPreviewWebContentsManagerBrowserTest;
   friend class content::WebContentsUserData<PrintViewManagerCros>;
 
@@ -76,8 +81,14 @@ class PrintViewManagerCros
   void set_render_frame_host_for_testing(content::RenderFrameHost* rfh) {
     render_frame_host_ = rfh;
   }
+
   // The current RFH that is print previewing.
   raw_ptr<content::RenderFrameHost> render_frame_host_ = nullptr;
+
+  // Own the instance of the UI wrapper, this holds wrapper for a print preview
+  // UI endpoint for renderer communication.
+  std::unique_ptr<PrintPreviewUiWrapper> ui_wrapper_;
+
   // Unique ID of the webcontent tied to this instance. This token is created
   // by this class and is passed to clients interested in identifying the
   // webcontents.
