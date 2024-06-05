c4a74e1b42a7190aaae0c31fb5677e1b20e70289
Daniel Rubery <drubery@chromium.org>
2024-06-05 20:33:05 +0000

    Automatically generate enums in SafetyNetApiHandler
    This CL automatically generates SafeBrowsingResult and VerifyAppsResult
    to limit the possibility of changing one but not the other.
    
    Bug: 337940905
    Change-Id: I098e9b74522d8da53d2a6911417fdb8a7fef1384
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593903
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310856}

--- chrome/android/javatests/src/org/chromium/chrome/browser/MockSafetyNetApiHandler.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/MockSafetyNetApiHandler.java
@@ -11,7 +11,9 @@ import org.json.JSONObject;
 
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
+import org.chromium.components.safe_browsing.SafeBrowsingResult;
 import org.chromium.components.safe_browsing.SafetyNetApiHandler;
+import org.chromium.components.safe_browsing.VerifyAppsResult;
 
 import java.util.HashMap;
 import java.util.Map;
--- components/safe_browsing/android/safe_browsing_api_handler_util.h
+++ components/safe_browsing/android/safe_browsing_api_handler_util.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace safe_browsing {
 
 // These match what SafetyNetApiHandler.java uses for |resultStatus|
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.safe_browsing
+// GENERATED_JAVA_CLASS_NAME_OVERRIDE: SafeBrowsingResult
 enum class SafetyNetRemoteCallResultStatus {
   INTERNAL_ERROR = -1,
   SUCCESS = 0,
@@ -126,13 +128,13 @@ enum class UmaRemoteCallResult {
   MAX_VALUE
 };
 
-// LINT.IfChange
+// LINT.IfChange(VerifyAppsEnabledResult)
 // The result of either SafetyNet.isVerifyAppsEnabled or
 // SafetyNet.enableVerifyApps. These values are persisted to
 // logs. Entries should not be renumbered and numeric values should
-// never be reused. Please update
-// //tools/metrics/histograms/metadata/sb_client/enums.xml:SafeBrowsingVerifyAppsEnabledResult
-// when changing this enum.
+// never be reused.
+// GENERATED_JAVA_ENUM_PACKAGE: org.chromium.components.safe_browsing
+// GENERATED_JAVA_CLASS_NAME_OVERRIDE: VerifyAppsResult
 enum class VerifyAppsEnabledResult {
   SUCCESS_ENABLED = 0,
   SUCCESS_NOT_ENABLED = 1,
@@ -140,7 +142,7 @@ enum class VerifyAppsEnabledResult {
   FAILED = 3,
   kMaxValue = FAILED,
 };
-// LINT.ThenChange(/components/safe_browsing/android/java/src/org/chromium/components/safe_browsing/SafetyNetApiHandler.java)
+// LINT.ThenChange(//tools/metrics/histograms/metadata/sb_client/enums.xml:SafeBrowsingVerifyAppsEnabledResult)
 
 // This parses the JSON from the GMSCore API and then:
 //   1) Picks the most severe threat type
