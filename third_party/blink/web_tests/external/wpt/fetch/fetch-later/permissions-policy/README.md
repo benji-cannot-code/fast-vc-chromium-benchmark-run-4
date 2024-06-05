4c31baca4719742b5b0b408645393143adaa145f
Ming-Ying Chung <mych@chromium.org>
2024-06-05 05:41:35 +0000

    Reland "[fetch-later] Define new permissions policy `deferred-fetch`"
    This is a reland of commit 193729386996ac0bd40dba0aa98b7f85ff7b94d1
    
    The failed test from https://crbug.com/339120680 is removed by https://crrev.com/c/5524593/1..2 as the same behavior is already covered by `/fetch/fetch-later/permissions-policy/deferred-fetch-default-permissions-policy.tentative.https.window.js` more carefully.
    
    Original change's description:
    > [fetch-later] Define new permissions policy `deferred-fetch`
    >
    > 1-Pager: https://docs.google.com/document/d/1P70kdENIByy3qWabN5rUPmBVkkANNSOM_jZynLnqINY/edit
    >
    > This CL follows the [guide] to define a new permissions policy
    > `deferred-fetch`, which is used to gate the new `fetchLater()` API.
    > Relevant WPT are added in this CL, and the subsequent CL will further
    > use this policy to adjust the request quota.
    >
    > In this CL, `deferred-fetch` is not added to chrome://settings/content
    > page. And no permission prompt for it.
    >
    > - webappsec request: https://github.com/w3c/webappsec-permissions-policy/issues/544
    > - "deferred-fetch" usage in fetchLater draft spec: https://whatpr.org/fetch/1647.html#request-a-deferred-fetch
    > - "deferred-fetch" policy discussion: https://github.com/WICG/pending-beacon/issues/87#issuecomment-1985358609
    > - FetchLater Explainer: https://github.com/WICG/pending-beacon/blob/main/docs/fetch-later-api.md
    >
    >
    > [guide]: https://chromium.googlesource.com/chromium/src/+/main/components/permissions/add_new_permission.md
    >
    > Bug: b:40276121,339120680
    > Change-Id: I2db4dd7484610fefb50c463552155b220f13ac5d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5367481
    > Reviewed-by: Adam Rice <ricea@chromium.org>
    > Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    > Reviewed-by: Ian Clelland <iclelland@chromium.org>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1297320}
    
    Bug: b:40276121,339120680
    Change-Id: I41e2f0980f5d72eb6c0687a82d96ad4a59f59936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524593
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310429}

---
+++ third_party/blink/web_tests/external/wpt/fetch/fetch-later/permissions-policy/README.md
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Permissions Policy: "deferred-fetch" Tests
+
+This folder contains tests to cover the permissions policy "deferred-fetch",
+which is used to gate the `fetchLater()` API.
+
+The tests follow the patterns from
+permissions-policy/README.md to cover all use cases of permissions policy for a
+new feature.
---
+++ third_party/blink/web_tests/external/wpt/fetch/fetch-later/permissions-policy/deferred-fetch-supported-by-permissions-policy.tentative.window.js
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=The feature list should advertise deferred-fetch
+'use strict';
+
+// https://w3c.github.io/webappsec-permissions-policy/#dom-permissions-policy-features
+// https://wicg.github.io/local-fonts/#permissions-policy
+test(() => {
+  assert_in_array('deferred-fetch', document.featurePolicy.features());
+}, 'document.featurePolicy.features should advertise deferred-fetch.');
