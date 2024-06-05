1a433cdd58d3ba38a9b5856e94a0aa3cfa6ffe6c
Ayu Ishii <ayui@chromium.org>
2024-06-05 20:14:03 +0000

    SyncFileSystem: Add metrics for invalidations
    This change adds metrics for SyncFileSystem invalidations.
    SyncFileSystem is used by ChromeApps users. Metrics
    specifically try to measure how many invalidations are sent
    from Drive that require update to the local device.
    
    Change-Id: Id73987c438a75a70b6787fd30fbe59526311e88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5583716
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310840}

--- components/drive/drive_notification_manager.cc
+++ components/drive/drive_notification_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/observer_list.h"
 #include "base/rand_util.h"
@@ -202,7 +203,7 @@ void DriveNotificationManager::RestartPollingTimer() {
   polling_timer_.Start(
       FROM_HERE, base::Seconds(interval_secs + jitter),
       base::BindOnce(&DriveNotificationManager::NotifyObserversToUpdate,
-                     weak_ptr_factory_.GetWeakPtr(), NOTIFICATION_POLLING,
+                     weak_ptr_factory_.GetWeakPtr(), kNotificationPolling,
                      std::map<std::string, int64_t>()));
 }
 
@@ -224,7 +225,10 @@ void DriveNotificationManager::NotifyObserversToUpdate(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DVLOG(1) << "Notifying observers: " << NotificationSourceToString(source);
 
-  if (source == NOTIFICATION_XMPP) {
+  base::UmaHistogramEnumeration("Storage.SyncFileSystem.NotificationSource",
+                                source);
+
+  if (source == kNotificationXMPP) {
     auto my_drive_invalidation = invalidations.find("");
     if (my_drive_invalidation != invalidations.end()) {
       // The invalidation version for My Drive is smaller than what's expected
@@ -283,7 +287,7 @@ void DriveNotificationManager::OnBatchTimerExpired() {
   std::map<std::string, int64_t> change_ids_to_update;
   invalidated_change_ids_.swap(change_ids_to_update);
   if (!change_ids_to_update.empty()) {
-    NotifyObserversToUpdate(NOTIFICATION_XMPP, std::move(change_ids_to_update));
+    NotifyObserversToUpdate(kNotificationXMPP, std::move(change_ids_to_update));
   }
 }
 
@@ -291,9 +295,9 @@ void DriveNotificationManager::OnBatchTimerExpired() {
 std::string DriveNotificationManager::NotificationSourceToString(
     NotificationSource source) {
   switch (source) {
-    case NOTIFICATION_XMPP:
+    case kNotificationXMPP:
       return "NOTIFICATION_XMPP";
-    case NOTIFICATION_POLLING:
+    case kNotificationPolling:
       return "NOTIFICATION_POLLING";
   }
 
