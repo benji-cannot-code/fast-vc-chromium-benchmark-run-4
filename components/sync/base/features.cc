94c1fe3099150ebb253a2c53cf68524ea226dccf
Marc Treib <treib@chromium.org>
2024-06-05 10:35:29 +0000

    Enable SyncSchedulerUseWallClockTimer by default
    Bug: 40939309
    Change-Id: I391883b6d085dad830ac6bb978e51391804481b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5331888
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310524}

--- components/sync/base/features.cc
+++ components/sync/base/features.cc
@@ -211,7 +211,7 @@ BASE_FEATURE(kSyncIncreaseNudgeDelayForSingleClient,
 
 BASE_FEATURE(kSyncSchedulerUseWallClockTimer,
              "SyncSchedulerUseWallClockTimer",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kTrustedVaultAutoUpgradeSyntheticFieldTrial,
              "TrustedVaultAutoUpgradeSyntheticFieldTrial",
