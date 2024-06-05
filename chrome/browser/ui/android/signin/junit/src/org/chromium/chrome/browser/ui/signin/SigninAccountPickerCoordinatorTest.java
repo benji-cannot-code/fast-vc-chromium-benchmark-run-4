10feaa11f5d0a2f16a194c5f71e370378944f53d
Menghan Yang <myuu@google.com>
2024-06-05 14:03:35 +0000

    [UNOp2][Android] Update history sync screen enter animation
    This CL updates the new sign-in flow activity animation style, to
    allow the history sync to appear 100ms after the bottom sheet is
    dismissed, and with a slide-up animation.
    
    See https://crbug.com/338532467#comment8 for the recordings.
    
    Bug: 338532467, 338541746
    Change-Id: I2aeb88ce28edcf480557764e6568647fa5def4e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594113
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1310591}

--- chrome/browser/ui/android/signin/junit/src/org/chromium/chrome/browser/ui/signin/SigninAccountPickerCoordinatorTest.java
+++ chrome/browser/ui/android/signin/junit/src/org/chromium/chrome/browser/ui/signin/SigninAccountPickerCoordinatorTest.java
@@ -28,8 +28,10 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
+import org.robolectric.annotation.Config;
 
 import org.chromium.base.Promise;
+import org.chromium.base.task.test.ShadowPostTask;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -56,6 +58,7 @@ import java.lang.ref.WeakReference;
 /** Unit tests for {@link SigninAccountPickerCoordinator}. */
 @Batch(Batch.UNIT_TESTS)
 @RunWith(BaseRobolectricTestRunner.class)
+@Config(shadows = {ShadowPostTask.class})
 @EnableFeatures({ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS})
 public class SigninAccountPickerCoordinatorTest {
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
@@ -90,6 +93,7 @@ public class SigninAccountPickerCoordinatorTest {
                             mContainerView = new FrameLayout(mActivity);
                             mActivity.setContentView(mContainerView);
                         });
+        ShadowPostTask.setTestImpl((taskTraits, task, delay) -> task.run());
         mJniMocker.mock(SigninMetricsUtilsJni.TEST_HOOKS, mSigninMetricsUtilsNativeMock);
         AccountManagerFacadeProvider.setInstanceForTests(mAccountManagerFacadeMock);
         when(mAccountManagerFacadeMock.getCoreAccountInfos()).thenReturn(new Promise<>());
