297049ed3f75d1cacc1e608d31c6cae56c3aed65
danakj <danakj@chromium.org>
2024-06-05 19:13:48 +0000

    Remove base::SHA1HashBytes(), consolidate into base::SHA1Hash()
    Now that there's no ambiguity we don't need the span suffix, just
    as we don't have one in base::HexEncode().
    
    Convert GPU Command Buffer code from using `char*` with a known constant
    length to be `span<uint8_t, kHashLength>` for compile-time
    bounds-safety.
    
    Convert other uses of SHA1HashBytes() to SHA1Hash() and the use of
    spans.
    
    Add an overload of base::as_byte_span and base::as_writable_byte_span
    that takes a compile-time size for the output span, and verifies at
    runtime that it is correct. This allows the caller to skip calling the
    span ctor or make_span<N> overload in order to move from a variable
    sized container to a fixed-size byte span.
    
    R=davidben@chromium.org, sunnyps@chromium.org
    
    Bug: 40284755
    Change-Id: I0e6b923303731e846655ee25ec59d78eccc62d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597223
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310804}

--- testing/libfuzzer/fuzzers/sha1_fuzzer.cc
+++ testing/libfuzzer/fuzzers/sha1_fuzzer.cc
@@ -9,8 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/hash/sha1.h"
 
 // Entry point for LibFuzzer.
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  unsigned char sha1[base::kSHA1Length] = {};
-  base::SHA1HashBytes(data, size, sha1);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data_ptr, size_t size) {
+  // SAFETY: libfuzzer gives a valid pointer and size pair.
+  auto data = UNSAFE_BUFFERS(base::span(data_ptr, size));
+  base::SHA1Digest sha1 = base::SHA1Hash(data);
+  (void)sha1;
   return 0;
 }
