e14c7b5100f9fc0c2967e5bca5aaa8a784c8785d
Tsuyoshi Horo <horo@chromium.org>
2024-06-05 07:15:55 +0000

    Introduce StaticDataNavigationBodyLoader::CreateWithData()
    This CL introduces a new static method StaticDataNavigationBodyLoader::
    CreateWithData(). Currently there are several places where
    StaticDataNavigationBodyLoader is created, and then Write() and Finish()
    is called. With this new method, we can simplify the construction of a
    StaticDataNavigationBodyLoader.
    
    This new method can also reduce the memory copy. Currently the
    Write(const SharedBuffer& data) method is causing a memory copy of the
    body data. In the new method, `data_` can be assigned without the memory
    copy.
    
    Bug: 342859647
    Change-Id: I33e7f40095ff037bd50f77b4f61b7ccf07a05add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585763
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310453}

--- third_party/blink/renderer/core/loader/document_loader_test.cc
+++ third_party/blink/renderer/core/loader/document_loader_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/auto_reset.h"
+#include "base/containers/span.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/unguessable_token.h"
@@ -94,7 +95,9 @@ class BodyLoaderTestDelegate : public URLLoaderTestDelegate {
     return true;
   }
 
-  void Write(const char* data) { body_loader_raw_->Write(data, strlen(data)); }
+  void Write(const char* data) {
+    body_loader_raw_->Write(base::make_span(data, strlen(data)));
+  }
 
   void Finish() { body_loader_raw_->Finish(); }
 
@@ -307,7 +310,7 @@ TEST_P(DocumentLoaderTest, MultiChunkWithReentrancy) {
 
     void DispatchOneByte() {
       char c = data_.TakeFirst();
-      body_loader_->Write(&c, 1);
+      body_loader_->Write(base::make_span(&c, static_cast<size_t>(1)));
     }
 
     bool ServedReentrantly() const { return served_reentrantly_; }
