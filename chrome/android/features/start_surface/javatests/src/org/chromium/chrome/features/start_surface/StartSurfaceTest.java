75f377df83c60b07a114554769acdc56ee306a62
Xi Han <hanxi@google.com>
2024-06-05 19:54:42 +0000

    [MagicStack] Enable Magic Stack and Price change module by default.
    In this CL:
    1. we clean up three feature flags: MAGIC_STACK_ANDROID,
       PRICE_CHANGE_MODULE and SegmentationPlatformAndroidHomeModuleRanker.
    2. Clean up tests for showing single tab cards, and replaced with
       showing the magic stack.
    
    Bug: 41485537
    Change-Id: I1059a2799ba239e543b1202edc3da7d6fedbcee7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593391
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310831}

--- chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
+++ chrome/android/features/start_surface/javatests/src/org/chromium/chrome/features/start_surface/StartSurfaceTest.java
@@ -85,7 +85,6 @@ import org.chromium.chrome.browser.layouts.LayoutStateProvider;
 import org.chromium.chrome.browser.layouts.LayoutTestUtils;
 import org.chromium.chrome.browser.layouts.LayoutType;
 import org.chromium.chrome.browser.profiles.ProfileManager;
-import org.chromium.chrome.browser.single_tab.SingleTabSwitcherMediator;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tasks.ReturnToChromeUtil;
@@ -212,7 +211,7 @@ public class StartSurfaceTest {
         onViewWaiting(withId(R.id.primary_tasks_surface_view));
         onViewWaiting(withId(R.id.search_box_text));
         onViewWaiting(withId(R.id.mv_tiles_container)).check(matches(isDisplayed()));
-        onViewWaiting(withId(R.id.tab_switcher_module_container)).check(matches(isDisplayed()));
+        onViewWaiting(withId(R.id.single_tab_view)).check(matches(isDisplayed()));
         onView(withId(R.id.tasks_surface_body)).check(matches(isDisplayed()));
 
         // TODO(crbug.com/40128588): fix toolbar to make incognito switch part of the view.
@@ -249,7 +248,6 @@ public class StartSurfaceTest {
         onViewWaiting(withId(R.id.primary_tasks_surface_view));
         onViewWaiting(withId(R.id.search_box_text));
         onView(withId(R.id.mv_tiles_container)).check(matches(isDisplayed()));
-        onView(withId(R.id.tab_switcher_module_container)).check(matches(isDisplayed()));
         onView(withId(R.id.single_tab_view)).check(matches(isDisplayed()));
         onView(withId(R.id.tasks_surface_body)).check(matches(isDisplayed()));
 
@@ -457,12 +455,6 @@ public class StartSurfaceTest {
 
         // Histograms should be only recorded when StartSurface is shown immediately after
         // launch.
-        Assert.assertEquals(
-                expectedRecordCount,
-                RecordHistogram.getHistogramTotalCountForTesting(
-                        StartSurfaceConfiguration.getHistogramName(
-                                SingleTabSwitcherMediator.SINGLE_TAB_TITLE_AVAILABLE_TIME_UMA)));
-
         Assert.assertEquals(
                 expectedRecordCount,
                 RecordHistogram.getHistogramTotalCountForTesting(
@@ -995,7 +987,7 @@ public class StartSurfaceTest {
         onViewWaiting(withId(R.id.tab_switcher_toolbar));
         onViewWaiting(withId(R.id.search_box_text)).check(matches(isDisplayed()));
         onViewWaiting(withId(R.id.mv_tiles_container)).check(matches(isDisplayed()));
-        onViewWaiting(withId(R.id.tab_switcher_module_container)).check(matches(isDisplayed()));
+        onViewWaiting(withId(R.id.single_tab_view)).check(matches(isDisplayed()));
         onViewWaiting(withId(R.id.tasks_surface_body));
 
         View startSurfaceView =
