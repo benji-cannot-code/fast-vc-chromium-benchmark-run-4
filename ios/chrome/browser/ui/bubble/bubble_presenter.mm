8a72467baf6c1db6022e33128b9db969d788ebeb
ginnnnnnny <ginnyhuang@chromium.org>
2024-06-05 22:34:46 +0000

    [iOS][ffr] Actually dismiss toolbar swipe IPH!
    Before the CL, the dismissal for toolbar swipe has never been reported
    to feature engagement tracker. Fixing.
    
    Fixed: 345285087
    Change-Id: I059a49b4fbba70ed87a3ddf0ff8936cea0294ed4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597274
    Reviewed-by: Huiting Yu <huitingyu@google.com>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1310944}

--- ios/chrome/browser/ui/bubble/bubble_presenter.mm
+++ ios/chrome/browser/ui/bubble/bubble_presenter.mm
@@ -718,6 +718,7 @@ BOOL CanGestureInProductHelpViewFitInGuide(GestureInProductHelpView* view,
       topConstraintForBottomEdgeSwipe;
   toolbarSwipeGestureIPH.topConstraintForTopEdgeSwipe =
       topConstraintForTopEdgeSwipe;
+  toolbarSwipeGestureIPH.delegate = self;
   [self.rootViewController.view addSubview:toolbarSwipeGestureIPH];
   AddSameConstraints(toolbarSwipeGestureIPH, guide);
 
