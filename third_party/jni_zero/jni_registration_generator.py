5dc0ba83c8cee72e276cc1cad6d6e4ce078c5ad0
Sam Maier <smaier@chromium.org>
2024-06-05 18:11:51 +0000

    jni_zero: Add concept of priority files to put first in multiplexing
    This is a fix the cause of b/343678433 when we turned on multiplexing.
    Webview's java side of multiplexing was based off the secondary library,
    while the primary library had an entirely different switch-case
    assignment. This would cause java to call into incorrect functions in
    native.
    
    Example:
    Webview Secondary Library and Webview Java
     funcA: 0
     funcZ: 1
    
    Chrome Primary Library and Chrome Java
     funcA: 0
     funcB: 1
     funcZ: 2
    
    So, if webview called funcZ, it would end up in funcB when using the
    primary library. The fix here just puts all of webview's numbers as 1..N
    so that any Chrome-only functions come afterwards - in the example,
    funcB would go to 2 and funcZ would match webview with 1.
    
    Bug: 343746437
    Change-Id: I61a4d0f6534efacfa1a9661535b84745a7c296a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598222
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310757}

--- third_party/jni_zero/jni_registration_generator.py
+++ third_party/jni_zero/jni_registration_generator.py
@@ -83,7 +83,7 @@ def _Flatten(jni_objs_by_path, paths):
   return itertools.chain(*(jni_objs_by_path[p] for p in paths))
 
 
-def _Generate(options, native_sources, java_sources):
+def _Generate(options, native_sources, java_sources, priority_java_sources):
   """Generates files required to perform JNI registration.
 
   Generates a srcjar containing a single class, GEN_JNI, that contains all
@@ -98,6 +98,8 @@ def _Generate(options, native_sources, java_sources):
         dependency tree. The source of truth.
     java_sources: A list of .jni.pickle or .java file paths. Used to assert
         against native_sources.
+    priority_java_sources: A list of .jni.pickle or .java file paths. Used to
+        put these listed java files first in multiplexing.
   """
   native_sources_set = set(native_sources)
   java_sources_set = set(java_sources)
@@ -110,8 +112,13 @@ def _Generate(options, native_sources, java_sources):
   for jni_obj in _Flatten(jni_objs_by_path,
                           native_sources_set & java_sources_set):
     dicts.append(DictionaryGenerator(jni_obj, options).Generate())
-  # Sort to make output deterministic.
-  dicts.sort(key=lambda d: d['FULL_CLASS_NAME'])
+
+  priority_java_sources = set(
+      priority_java_sources) if priority_java_sources else {}
+  # Sort to make output deterministic, and to put priority_java_sources at the
+  # top.
+  dicts.sort(key=lambda d: (d['FILE_PATH'] not in priority_java_sources, d[
+      'FULL_CLASS_NAME']))
 
   stubs = _GenerateStubsAndAssert(options, jni_objs_by_path, native_sources_set,
                                   java_sources_set)
@@ -917,8 +924,14 @@ def main(parser, args):
     parser.error('--require-mocks requires --enable-proxy-mocks.')
   if not args.header_path and args.manual_jni_registration:
     parser.error('--manual-jni-registration requires --header-path.')
+  if not args.header_path and args.enable_jni_multiplexing:
+    parser.error('--enable-jni-multiplexing requires --header-path.')
   if args.remove_uncalled_methods and not args.native_sources_file:
     parser.error('--remove-uncalled-methods requires --native-sources-file.')
+  if args.priority_java_sources_file and not args.enable_jni_multiplexing:
+    parser.error('--priority-java-sources is only for multiplexing.')
+  if args.enable_jni_multiplexing and not args.use_proxy_hash:
+    parser.error('--enable-jni-multiplexing requires --use_proxy_hash.')
 
   java_sources = _ParseSourceList(args.java_sources_file)
   if args.native_sources_file:
@@ -931,8 +944,12 @@ def main(parser, args):
       # Just treating it like we have perfect alignment between native and java
       # when only looking at java.
       native_sources = java_sources
+  if args.priority_java_sources_file:
+    priority_java_sources = _ParseSourceList(args.priority_java_sources_file)
+  else:
+    priority_java_sources = None
 
-  _Generate(args, native_sources, java_sources=java_sources)
+  _Generate(args, native_sources, java_sources, priority_java_sources)
 
   if args.depfile:
     # GN does not declare a dep on the sources files to avoid circular
