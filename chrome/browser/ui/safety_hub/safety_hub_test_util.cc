3b9ed14ba53b6d8641d0f9205cd976fc3bd7d823
EmiliaPaz <emiliapaz@chromium.org>
2024-06-05 16:54:42 +0000

    [Extensions Cleanup] Retrieve extensions directly from set
    Retrieve the extension directly from the respective set rather than
    generating a copy of them.
    
    Bug: n/a
    Change-Id: I6833de271cc6db6b16e06f78f7596578e187eb0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578753
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tom Van Goethem <tov@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310691}

--- chrome/browser/ui/safety_hub/safety_hub_test_util.cc
+++ chrome/browser/ui/safety_hub/safety_hub_test_util.cc
@@ -207,10 +207,8 @@ void CleanAllMockExtensions(Profile* profile) {
                   profile);
 
   // Check that all extensions were successfully uninstalled.
-  const extensions::ExtensionSet extensions =
-      extensions::ExtensionRegistry::Get(profile)
-          ->GenerateInstalledExtensionsSet(
-              extensions::ExtensionRegistry::ENABLED);
+  const extensions::ExtensionSet& extensions =
+      extensions::ExtensionRegistry::Get(profile)->enabled_extensions();
   EXPECT_TRUE(extensions.empty());
 }
 
