712a2a85a361207e53da17673e067efe778d45d9
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-06-05 06:42:37 +0000

    [views-ax] Remove a11y setters from the AccessiblePaneView ctor
    This change removes all usage of accessibility setters from
    the AccessiblePaneView constructor and instead sets the attributes
    directly in the GetAccessibleNodeData override. It doesn't introduce a
    change in behavior, since the attributes exposed to platform
    accessibility APIs is the union of the ones set through the setters and
    the one returned by GetAccessibleNodeData.
    
    This change is needed because, with the ViewsAX project, we aim to
    maintain a cache of all accessibility attributes. To ensure this cache
    is only active when needed, it must be initialized independently of the
    view's instantiation.
    
    GetAccessibleNodeData will soon only be used to initialize the
    accessibility cache, and will be renamed to reflect its new
    responsibilities.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I8c1a5e441795bdc0a3c50534ced4c0ab8abb98e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589381
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1310445}

--- ui/views/accessible_pane_view.cc
+++ ui/views/accessible_pane_view.cc
@@ -52,7 +52,6 @@ class AccessiblePaneViewFocusSearch : public FocusSearch {
 AccessiblePaneView::AccessiblePaneView()
     : last_focused_view_tracker_(std::make_unique<ViewTracker>()) {
   focus_search_ = std::make_unique<AccessiblePaneViewFocusSearch>(this);
-  GetViewAccessibility().SetProperties(ax::mojom::Role::kPane);
 }
 
 AccessiblePaneView::~AccessiblePaneView() {
@@ -222,6 +221,11 @@ void AccessiblePaneView::SetVisible(bool flag) {
 
 void AccessiblePaneView::RequestFocus() {
   SetPaneFocusAndFocusDefault();
+}
+
+void AccessiblePaneView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
+  View::GetAccessibleNodeData(node_data);
+  node_data->role = ax::mojom::Role::kPane;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
