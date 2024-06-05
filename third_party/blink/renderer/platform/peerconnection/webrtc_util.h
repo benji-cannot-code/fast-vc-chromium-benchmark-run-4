c40643f6d0bd6ca9bc09b10f48e90c97c6cd6877
Hirokazu Honda <hiroh@chromium.org>
2024-06-05 07:29:26 +0000

    peerconnection: Move WebRTCFormatToCodecProfile() to webrtc_util.h
    This CL moves WebRTCFormatToCodecProfile() to webrtc_util.h so it can
    be used in other files.
    
    Bug: b:320555128
    Test: Build
    Change-Id: Idf0ab8fa730d5ccc65527f81326a826241def64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561083
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Erik Språng <sprang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310457}

--- third_party/blink/renderer/platform/peerconnection/webrtc_util.h
+++ third_party/blink/renderer/platform/peerconnection/webrtc_util.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_WEBRTC_UTIL_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_PEERCONNECTION_WEBRTC_UTIL_H_
 
+#include <optional>
+
 #include "base/time/time.h"
+#include "media/base/video_codecs.h"
 #include "third_party/blink/renderer/platform/network/parsed_content_type.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -22,6 +25,9 @@ ConvertToSdpVideoFormatParameters(
     const ParsedContentHeaderFieldParameters& parameters);
 
 base::TimeTicks PLATFORM_EXPORT ConvertToBaseTimeTicks(webrtc::Timestamp time);
+
+std::optional<media::VideoCodecProfile> PLATFORM_EXPORT
+WebRTCFormatToCodecProfile(const webrtc::SdpVideoFormat& sdp);
 
 }  // namespace blink
 
