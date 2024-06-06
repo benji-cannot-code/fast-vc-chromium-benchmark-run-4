4a9f6710232104e84ef9e18fa9704aa0a0616920
Dana Fried <dfried@chromium.org>
2024-06-06 23:27:57 +0000

    [GM2 Cleanup] content_setting_*
    #chrome-refresh-2023-cleanup
    
    Change-Id: I72a868652817130b1ba911dec9b4ef84b9422f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606354
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311616}

--- chrome/browser/ui/views/content_setting_site_row_view.cc
+++ chrome/browser/ui/views/content_setting_site_row_view.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
-#include "ui/base/ui_base_features.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/toggle_button.h"
 #include "ui/views/controls/image_view.h"
@@ -65,9 +64,7 @@ ContentSettingSiteRowView::ContentSettingSiteRowView(
       views::kFlexBehaviorKey,
       views::FlexSpecification(views::MinimumFlexSizeRule::kScaleToZero,
                                views::MaximumFlexSizeRule::kUnbounded));
-  if (features::IsChromeRefresh2023()) {
-    title_label->SetTextStyle(views::style::STYLE_BODY_3_MEDIUM);
-  }
+  title_label->SetTextStyle(views::style::STYLE_BODY_3_MEDIUM);
 
   toggle_button_ = AddChildView(std::make_unique<views::ToggleButton>(
       base::BindRepeating(&ContentSettingSiteRowView::OnToggleButtonPressed,
