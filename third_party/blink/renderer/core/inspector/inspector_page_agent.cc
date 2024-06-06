967f4f2a2e16eca7afdc214e4f7e408a94e052b4
Tsuyoshi Horo <horo@chromium.org>
2024-06-06 15:03:37 +0000

    Use SegmentedBuffer in InspectorNetworkAgent
    InspectorNetworkAgent's InspectorFileReaderLoaderClient is using a
    SharedBuffer for appending data while reading data from a
    BlobDataHandle. SharedBuffer is a refcounted SegmentedBuffer. But we
    don't need to share the data in the InspectorFileReaderLoaderClient.
    So to make the ownership clear, this CL changes
    InspectorFileReaderLoaderClient to use a SegmentedBuffer to append the
    data while reading data from a BlobDataHandle.
    This CL also changes SharedBufferContent() method to accept
    a pointer of a SegmentedBuffer instead of a SharedBuffer.
    
    Bug: 342859647
    Change-Id: I059709be452e390dc9ebd28dc058a09b3c9c6fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595330
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311259}

--- third_party/blink/renderer/core/inspector/inspector_page_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_page_agent.cc
@@ -329,8 +329,8 @@ KURL InspectorPageAgent::UrlWithoutFragment(const KURL& url) {
 }
 
 // static
-bool InspectorPageAgent::SharedBufferContent(
-    scoped_refptr<const SharedBuffer> buffer,
+bool InspectorPageAgent::SegmentedBufferContent(
+    const SegmentedBuffer* buffer,
     const String& mime_type,
     const String& text_encoding_name,
     String* result,
@@ -343,7 +343,7 @@ bool InspectorPageAgent::SharedBufferContent(
       CreateResourceTextDecoder(mime_type, text_encoding_name);
   WTF::TextEncoding encoding(text_encoding_name);
 
-  const SegmentedBuffer::DeprecatedFlatData flat_buffer(buffer.get());
+  const SegmentedBuffer::DeprecatedFlatData flat_buffer(buffer);
   if (decoder) {
     text_content = decoder->Decode(flat_buffer.Data(), flat_buffer.size());
     text_content = text_content + decoder->Flush();
@@ -405,8 +405,8 @@ bool InspectorPageAgent::CachedResourceContent(const Resource* cached_resource,
       if (text_encoding_name.empty() &&
           cached_resource->GetType() != blink::ResourceType::kRaw)
         text_encoding_name = "WinLatin1";
-      return InspectorPageAgent::SharedBufferContent(
-          cached_resource->ResourceBuffer(),
+      return InspectorPageAgent::SegmentedBufferContent(
+          cached_resource->ResourceBuffer().get(),
           cached_resource->GetResponse().MimeType(), text_encoding_name, result,
           base64_encoded);
   }
