7e590ed21692e494d666478b4b104b8dc99c604d
Rouslan Solomakhin <rouslan@chromium.org>
2024-06-06 15:43:12 +0000

    [Gardening] Disable flaky pixel test on Windows.
    This patch disables the following test that has been frequently flaking
    on "Win10 Tests x64" since 2024-05-09:
    "FirstRunIntroPixelTest.InvokeUi_default/LightTheme".
    
    Owners-Override: True
    Bug: 40261456
    Change-Id: Ib04bc056952bc0c810b79e75f949d55651d6eb14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599025
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311289}

--- chrome/browser/ui/views/profiles/first_run_intro_browsertest.cc
+++ chrome/browser/ui/views/profiles/first_run_intro_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_helpers.h"
 #include "base/strings/strcat.h"
+#include "build/build_config.h"
 #include "chrome/browser/enterprise/browser_management/management_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/test/test_browser_ui.h"
@@ -46,7 +47,12 @@ const FirstRunTestParam kTestParams[] = {
     {.pixel_test_param = {.test_suffix = "DarkThemeFixedSize",
                           .use_dark_theme = true},
      .use_fixed_size = true},
+#if !BUILDFLAG(IS_WIN)
+    // TODO(https://crbug.com/40261456): The following test has been frequently
+    // flaking on "Win10 Tests x64" since 2024-05-09:
+    // FirstRunIntroPixelTest.InvokeUi_default/LightTheme
     {.pixel_test_param = {.test_suffix = "LightTheme"}},
+#endif
     {.pixel_test_param = {.test_suffix = "LongerStringsFixedSize"},
      .use_fixed_size = true,
      .use_longer_strings = true},
