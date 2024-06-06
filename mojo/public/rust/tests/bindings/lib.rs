40c23a6279359c0460e01804ea161b3155687180
Collin Baker <collinbaker@chromium.org>
2024-06-06 22:50:53 +0000

    Parse message header
    Add code to begin interpreting a mojo message. Runs a select set of
    mojo validation tests to verify message header validation works.
    
    Bug: 40206847
    Change-Id: I9f5b41a13929ab9e9544314b6b9fb62c99d38799
    
    Cq-Include-Trybots: luci.chromium.try:android-rust-arm32-rel,android-rust-arm64-dbg,android-rust-arm64-rel,linux-rust-x64-dbg,linux-rust-x64-rel,mac-rust-x64-dbg,win-rust-x64-dbg,win-rust-x64-rel
    Change-Id: I9f5b41a13929ab9e9544314b6b9fb62c99d38799
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600008
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311605}

--- mojo/public/rust/tests/bindings/lib.rs
+++ mojo/public/rust/tests/bindings/lib.rs
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! Test Rust code generated for mojom test definitions, and associated code for
 //! bindings support.
 
+mod validation;
+
 use rust_gtest_interop::prelude::*;
 
 chromium::import! {
