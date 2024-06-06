68d09be4d2c54cf20117be031c4912de39bcc9b1
Sirisha Kavuluru <skavuluru@google.com>
2024-06-06 18:34:28 +0000

    [DW] Set ActivityOptions in PendingIntent to allow tab tearing.
    Tested tab tearing works as expected on Android V on Pixel Tablet.
    
    Bug: 344986692
    Change-Id: I86be3f5bc5ea1e91c4884880afef2945472d7b1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598188
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1311432}

--- chrome/android/java/src/org/chromium/chrome/browser/dragdrop/ChromeDragAndDropBrowserDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dragdrop/ChromeDragAndDropBrowserDelegate.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.dragdrop;
 
 import android.app.Activity;
+import android.app.ActivityOptions;
 import android.app.PendingIntent;
 import android.content.ClipData;
 import android.content.ClipData.Item;
@@ -20,6 +21,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -139,10 +141,12 @@ public class ChromeDragAndDropBrowserDelegate implements DragAndDropBrowserDeleg
                 DragAndDropLauncherActivity.getTabIntent(
                         context, tab.getId(), MultiWindowUtils.INVALID_INSTANCE_ID);
         if (intent != null) {
+            ActivityOptions opts = ActivityOptions.makeBasic();
+            ApiCompatibilityUtils.setCreatorActivityOptionsBackgroundActivityStartMode(opts);
             PendingIntent pendingIntent =
-                    PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-            ClipData.Item item =
-                    ClipDataItemBuilder.buildClipDataItemWithPendingIntent(pendingIntent);
+                    PendingIntent.getActivity(
+                            context, 0, intent, PendingIntent.FLAG_IMMUTABLE, opts.toBundle());
+            Item item = ClipDataItemBuilder.buildClipDataItemWithPendingIntent(pendingIntent);
             return item == null ? null : new ClipData(null, mSupportedMimeTypes, item);
         }
         return null;
