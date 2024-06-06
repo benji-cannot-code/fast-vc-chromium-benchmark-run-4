35415c7cca5871b1940c0b68765bc01b198fe54e
Jood Hajeer <jood@google.com>
2024-06-06 11:01:15 +0000

    [iOS] Clean up EnableBatchUploadFromBookmarksManager
    This feature was enabled by default in M126 (crrev.com/c/5510444).
    
    Bug: 339399031
    Change-Id: I7f72a3dbfd5cbe9e1c2c04cc8fd8a4d34af2fdd3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600754
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311168}

--- ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_coordinator.mm
@@ -644,14 +644,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.shouldShowSignInPromo = NO;
     _signinPromoViewMediator.signinPromoAction = signinPromoAction;
   }
-  const std::string lastSignedInGaiaId =
-      _prefService->GetString(prefs::kGoogleServicesLastSyncingGaiaId);
-  // Show the promo if the last syncing user signed out and chose to clear
-  // data, or if the feature kEnableBatchUploadFromBookmarksManager is
-  // enabled.
-  self.shouldShowSignInPromo =
-      lastSignedInGaiaId.empty() ||
-      base::FeatureList::IsEnabled(kEnableBatchUploadFromBookmarksManager);
+  self.shouldShowSignInPromo = YES;
 }
 
 // Updates the visibility of the sign-in promo.
