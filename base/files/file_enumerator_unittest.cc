e5f498d273ddc46cda58b72b66b812de93390b3e
danakj <danakj@chromium.org>
2024-06-06 20:10:37 +0000

    Use spans in base file utils
    Remove ReadFile() overload from a pointer and size, and convert any
    callers to use the span overload.
    
    Turn on unsafe buffers warnings in file_util_unittest.cc and the
    file_util implementation files and fix them all.
    
    Bug: 40284755
    Change-Id: I33642151cbe32b983a4c67f7cce9c1c7f79ce512
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604926
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Owners-Override: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311497}

--- base/files/file_enumerator_unittest.cc
+++ base/files/file_enumerator_unittest.cc
@@ -98,7 +98,7 @@ circular_deque<FilePath> RunEnumerator(
 }
 
 bool CreateDummyFile(const FilePath& path) {
-  return WriteFile(path, "42", sizeof("42")) == sizeof("42");
+  return WriteFile(path, byte_span_from_cstring("42"));
 }
 
 bool GetFileInfo(const FilePath& file_path, File::Info& info) {
