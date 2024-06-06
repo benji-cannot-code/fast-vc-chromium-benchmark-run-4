2c6e002b41a52e5201e8921659d0d4eee8f92b9f
dpapad <dpapad@chromium.org>
2024-06-06 13:51:29 +0000

    Migrate chrome://management to Lit, part 3 (last).
    In this part checking in management_ui.html.ts instead of
    auto-generating it at build time.
    
    Bug: 40943652
    Change-Id: I5b7011722fbecfd988228fd5611e0f079d895158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598236
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Fabio Tirelo <ftirelo@chromium.org>
    Reviewed-by: Fabio Tirelo <ftirelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311220}

--- chrome/browser/resources/management/management_ui.html.ts
+++ chrome/browser/resources/management/management_ui.html.ts
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ManagementUiElement} from './management_ui.js';
+
+export function getHtml(this: ManagementUiElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <cr-toolbar page-name="$i18n{toolbarTitle}" role="banner" autofocus
     @search-changed="${this.onSearchChanged_}" clear-label="$i18n{clearSearch}"
     search-prompt="$i18n{searchPrompt}">
@@ -227,3 +238,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </div>
   </div>
 </main>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
