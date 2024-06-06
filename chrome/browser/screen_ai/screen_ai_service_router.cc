86261fa07867c9869cad3e6c01d04304092b255c
Ramin Halavati <rhalavati@chromium.org>
2024-06-06 15:37:33 +0000

    Remove unused headers from c/b/screen_ai/*
    A few unused headers are removed, no functional change.
    
    Change-Id: Id6601ebbba2357dce72c00a45632d74c9d971e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600622
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311285}

--- chrome/browser/screen_ai/screen_ai_service_router.cc
+++ chrome/browser/screen_ai/screen_ai_service_router.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/service_process_host.h"
 #include "content/public/browser/service_process_host_passkeys.h"
 #include "mojo/public/mojom/base/file_path.mojom.h"
