ee111383beba00529b99af2d51017d81395b242c
Alison Gale <agale@chromium.org>
2024-06-06 18:46:27 +0000

    Deflake bookmarks side panel test and move to interactive test
    Change-Id: If29b581f54bf394b3e1bb90a72d08150373cee89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604027
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311443}

--- chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_interactive_test.cc
+++ chrome/test/data/webui/side_panel/bookmarks/sp_bookmarks_interactive_test.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_notes/user_notes_features.h"
 #include "content/public/test/browser_test.h"
 
-class SidePanelBookmarksTest : public WebUIMochaBrowserTest {
+class SidePanelBookmarksTest : public WebUIMochaFocusTest {
  protected:
   SidePanelBookmarksTest() {
     set_test_loader_host(chrome::kChromeUIBookmarksSidePanelHost);
