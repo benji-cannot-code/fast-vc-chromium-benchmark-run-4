a5896cacbdba6ad6e49694f6503f107ae5cdd3d4
Igor Sheludko <ishell@chromium.org>
2024-06-06 08:51:41 +0000

    [v8][api] Fix chrome.loadTimes() and chrome.csi() objects setup
    ... which was broken during recent refactoring
    https://crrev.com/c/5476356.
    
    Fixed: 345274914
    Bug: 336325111
    Change-Id: I8f5d88bb5d36df2df67afd218ce33e5bd2f01b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598774
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311127}

--- chrome/renderer/loadtimes_extension_bindings.cc
+++ chrome/renderer/loadtimes_extension_bindings.cc
@@ -210,7 +210,8 @@ class LoadTimesExtensionWrapper : public v8::Extension {
           .ToLocalChecked();
     };
 
-    // Defines a property on |load_times| object with given name and value.
+    // Defines a property on |load_times| object with given name and value,
+    // returns true on success, false otherwise.
     auto define_prop = [=](std::string_view name, v8::Local<v8::Value> value) {
       v8::Local<v8::String> name_str =
           v8::String::NewFromUtf8(isolate, name.data(),
@@ -218,10 +219,10 @@ class LoadTimesExtensionWrapper : public v8::Extension {
                                   name.length())
               .ToLocalChecked();
 
-      return !load_times
-                  ->SetNativeDataProperty(ctx, name_str, LoadtimesGetter,
-                                          EmptySetter, value)
-                  .FromMaybe(false);
+      return load_times
+          ->SetNativeDataProperty(ctx, name_str, LoadtimesGetter, EmptySetter,
+                                  value)
+          .FromMaybe(false);
     };
 
     if (!define_prop("requestTime", v8_num(request_time))) {
@@ -310,7 +311,8 @@ class LoadTimesExtensionWrapper : public v8::Extension {
     // Helper lambda for creating v8::Number.
     auto v8_num = [=](double value) { return v8::Number::New(isolate, value); };
 
-    // Defines a property on |csi| object with given name and value.
+    // Defines a property on |csi| object with given name and value,
+    // returns true on success, false otherwise.
     auto define_prop = [=](std::string_view name, v8::Local<v8::Value> value) {
       v8::Local<v8::String> name_str =
           v8::String::NewFromUtf8(isolate, name.data(),
@@ -318,9 +320,9 @@ class LoadTimesExtensionWrapper : public v8::Extension {
                                   name.length())
               .ToLocalChecked();
 
-      return !csi->SetNativeDataProperty(ctx, name_str, CSIGetter, EmptySetter,
-                                         value)
-                  .FromMaybe(false);
+      return csi
+          ->SetNativeDataProperty(ctx, name_str, CSIGetter, EmptySetter, value)
+          .FromMaybe(false);
     };
 
     if (!define_prop("startE", v8_num(start.InMillisecondsSinceUnixEpoch()))) {
