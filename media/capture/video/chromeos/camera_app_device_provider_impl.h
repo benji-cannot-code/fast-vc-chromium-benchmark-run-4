22eb1750cb4c5e29ef190de59244e0c5fea40f55
seannli <seannli@google.com>
2024-06-06 12:34:46 +0000

    Reland "camera: Remove multi-stream logic in CCA"
    This is a reland of commit f578e4b23238b9365ab5eb9a63d8d99412f632fb
    
    Relanding with solving the issue of not updating the device list and
    notifying listeners when a USB camera is removed.
    
    Original change's description:
    > camera: Remove multi-stream logic in CCA
    >
    > There is no obvious advantage when using multi-stream recording, so we
    > are going to remove multi-stream logic to make VCD easier to maintain.
    > This CL removes the multi-stream logic in CCA.
    >
    > Bug: b:275304766
    > Test: tast run DUT camera.CCAUIStress.* camera.CCAUIRecordVideo*
    > Change-Id: Id55e6c9c6118e4f80b102ce570ad8e924a0cd5bd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5152990
    > Commit-Queue: Sean Li <seannli@google.com>
    > Reviewed-by: Wei Lee <wtlee@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Dorah Kim <dorahkim@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1265086}
    
    Bug: b:275304766
    Test: tast run DUT camera.CCAUIStress.* camera.CCAUIRecordVideo*
    Test: Test: tast run DUT camera.CCAUIExternal
    Change-Id: I1968b113fe37051952b4c27a2ad14525fc8dd87e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5349179
    Reviewed-by: Dorah Kim <dorahkim@google.com>
    Commit-Queue: Sean Li <seannli@google.com>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311189}

--- media/capture/video/chromeos/camera_app_device_provider_impl.h
+++ media/capture/video/chromeos/camera_app_device_provider_impl.h
@@ -41,10 +41,6 @@ class CAPTURE_EXPORT CameraAppDeviceProviderImpl
   void GetCameraAppDevice(const std::string& source_id,
                           GetCameraAppDeviceCallback callback) override;
   void IsSupported(IsSupportedCallback callback) override;
-  void SetVirtualDeviceEnabled(
-      const std::string& device_id,
-      bool enabled,
-      SetVirtualDeviceEnabledCallback callback) override;
   void IsDeviceInUse(const std::string& source_id,
                      IsDeviceInUseCallback callback) override;
 
@@ -53,10 +49,6 @@ class CAPTURE_EXPORT CameraAppDeviceProviderImpl
       GetCameraAppDeviceCallback callback,
       const std::optional<std::string>& device_id);
 
-  void SetVirtualDeviceEnabledWithDeviceId(
-      bool enable,
-      SetVirtualDeviceEnabledCallback callback,
-      const std::optional<std::string>& device_id);
   void IsDeviceInUseWithDeviceId(IsDeviceInUseCallback callback,
                                  const std::optional<std::string>& device_id);
 
