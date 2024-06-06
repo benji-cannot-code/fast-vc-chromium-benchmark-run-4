3352a944a230dbf7c4822ae57cc24abd64d1abb6
Pi-Hsun Shih <pihsun@chromium.org>
2024-06-06 05:34:36 +0000

    recorder: Migrate WebUI controller from internal repo
    Migrate the WebUI controller of the recorder app from internal repo to
    public. Changes from internal:
    
    * Various variables are renamed from conch to recorder_app.
    * Remove the soda integration, which needs more work and would be done
      in a follow up CL.
    * Remove some unused header includes.
    
    Also add minimal .html / .js file so it compiles. The real frontend will
    be migrated in a future CL.
    
    Bug: b:343340409
    Test: compile
    Change-Id: Iea755e084168eac8e92c2b5da48b65efbef7abe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595516
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311082}

---
+++ ash/webui/recorder_app_ui/url_constants.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WEBUI_RECORDER_APP_UI_URL_CONSTANTS_H_
+#define ASH_WEBUI_RECORDER_APP_UI_URL_CONSTANTS_H_
+
+namespace ash {
+
+extern const char kChromeUIRecorderAppHost[];
+extern const char kChromeUIRecorderAppURL[];
+
+}  // namespace ash
+
+#endif  // ASH_WEBUI_RECORDER_APP_UI_URL_CONSTANTS_H_
