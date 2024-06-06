934a2c070c71ddfcb8a887ddba7365c3216a6bf0
Jamie Walch <jamiewalch@chromium.org>
2024-06-06 23:41:58 +0000

    Support touch events for IT2Me on supported platforms.
    Based on discussions, there seems to be no good reason why this is limited to remote access mode. If I'm reading it correctly, I think this was inadvertently removed in https://codereview.chromium.org/2456563002.
    
    Change-Id: Ie2c89c64cf092f2e2c2395c91d1b54194054646d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606327
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Jamie Walch <jamiewalch@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311625}

--- remoting/host/me2me_desktop_environment.cc
+++ remoting/host/me2me_desktop_environment.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/desktop_resizer.h"
 #include "remoting/host/host_window.h"
 #include "remoting/host/host_window_proxy.h"
-#include "remoting/host/input_injector.h"
 #include "remoting/host/input_monitor/local_input_monitor.h"
 #include "remoting/host/remote_open_url/remote_open_url_util.h"
 #include "remoting/host/resizing_host_observer.h"
@@ -93,11 +92,6 @@ std::string Me2MeDesktopEnvironment::GetCapabilities() const {
     capabilities += " ";
   }
   capabilities += protocol::kRateLimitResizeRequests;
-
-  if (InputInjector::SupportsTouchEvents()) {
-    capabilities += " ";
-    capabilities += protocol::kTouchEventsCapability;
-  }
 
   if (desktop_environment_options().enable_file_transfer()) {
     capabilities += " ";
