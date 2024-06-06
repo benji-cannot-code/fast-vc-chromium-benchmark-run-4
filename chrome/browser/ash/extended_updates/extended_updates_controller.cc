dd21ca78eedad138fee9f4115f208438bb98d5db
Artyom Chen <artyomchen@google.com>
2024-06-06 10:48:23 +0000

    extended_updates: Fix Guest Mode crash
    Check OwnerSettingsService for nullptr when deciding whether to show
    extended updates notifications. OwnerSettingsService can be
    uninitialized for off the record profiles.
    
    Test: autoninja -C out/Default unit_tests && out/Default/unit_tests
    Test: manually patched a DUT and tested that guest mode does not crash
    Bug: b/344652238
    Change-Id: I95e4b36e36f9259a78e07ae7149910077e52ca02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600764
    Reviewed-by: Miriam Polzer <mpolzer@google.com>
    Commit-Queue: Artyom Chen <artyomchen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311163}

--- chrome/browser/ash/extended_updates/extended_updates_controller.cc
+++ chrome/browser/ash/extended_updates/extended_updates_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "base/logging.h"
 #include "base/time/default_clock.h"
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
@@ -181,10 +182,17 @@ void ExtendedUpdatesController::OnEolInfo(
     return;
   }
 
-  // This function is called upon login, so owner settings may not have finished
-  // loading yet. Defer decision to show notification until then.
   auto* owner_settings =
       OwnerSettingsServiceAshFactory::GetForBrowserContext(context);
+  if (!owner_settings) {
+    // In some sessions OwnerSettingsService may be completely uninitialized,
+    // for example Guest Mode.
+    LOG(WARNING) << "OwnerSettingsService is uninitialized for the profile."
+                    " Will not notify about extended updates";
+    return;
+  }
+  // This function is called upon login, so owner settings may not have finished
+  // loading yet. Defer decision to show notification until then.
   owner_settings->IsOwnerAsync(base::IgnoreArgs<bool>(
       base::BindOnce(&ExtendedUpdatesController::OnOwnershipDetermined,
                      weak_factory_.GetWeakPtr(), context->GetWeakPtr())));
