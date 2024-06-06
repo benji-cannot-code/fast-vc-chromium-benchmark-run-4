0c15b7e4e69cb1ec3d3cebf23c15a6c4b60722af
Juan Mojica <juanmojica@google.com>
2024-06-06 19:07:49 +0000

    [lensoverlay] Fix side panel crash when opening new tab.
    We need to post this as a task to prevent the side panel web contents
    from being destroyed before the callbacks are executed.
    
    Change-Id: I39c46f1aaf815fc003644e573eb2e22a5b64c80a
    Bug: b:345465864
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605091
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311459}

--- chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h
+++ chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents_observer.h"
 
 class Browser;
@@ -85,6 +86,9 @@ class LensOverlaySidePanelCoordinator : public SidePanelEntryObserver,
       content::NavigationHandle* navigation_handle) override;
   void DOMContentLoaded(content::RenderFrameHost* render_frame_host) override;
 
+  // Opens the provided url params in the main browser as a new tab.
+  void OpenURLInBrowser(const content::OpenURLParams& params);
+
   // Registers the entry in the side panel if it doesn't already exist.
   void RegisterEntry();
 
@@ -107,6 +111,7 @@ class LensOverlaySidePanelCoordinator : public SidePanelEntryObserver,
 
   base::WeakPtr<content::WebContents> tab_web_contents_;
   raw_ptr<LensOverlaySidePanelWebView> side_panel_web_view_;
+  base::WeakPtrFactory<LensOverlaySidePanelCoordinator> weak_ptr_factory_{this};
 };
 
 }  // namespace lens
