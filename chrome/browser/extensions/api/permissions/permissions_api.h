27688b4d420c3fd44978ac4fe221218c2009be49
EmiliaPaz <emiliapaz@chromium.org>
2024-06-06 16:08:29 +0000

    [Extensions c2s] Add permissions.removeSiteAccessRequests
    Allow the extension to remove site access requests by introducing
    permissions.removeSiteAccessRequest. Request will only be removed if
    it's existent.
    
    API is only available for MV3 extensions under a feature flag.
    
    Bug: 331680187
    Change-Id: Ieeeda42f6e72ac36b75ea8d91994bf59298d82cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598001
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311310}

--- chrome/browser/extensions/api/permissions/permissions_api.h
+++ chrome/browser/extensions/api/permissions/permissions_api.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "chrome/browser/extensions/extension_install_prompt.h"
+#include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_function.h"
 #include "extensions/common/permissions/permission_set.h"
 
@@ -120,6 +121,19 @@ class PermissionsAddSiteAccessRequestFunction : public ExtensionFunction {
 
  protected:
   ~PermissionsAddSiteAccessRequestFunction() override = default;
+
+  // ExtensionFunction:
+  ResponseAction Run() override;
+};
+
+// chrome.permissions.removeSiteAccessRequest
+class PermissionsRemoveSiteAccessRequestFunction : public ExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("permissions.removeSiteAccessRequest",
+                             PERMISSIONS_REMOVESITEACCESSREQUEST)
+
+ protected:
+  ~PermissionsRemoveSiteAccessRequestFunction() override = default;
 
   // ExtensionFunction:
   ResponseAction Run() override;
