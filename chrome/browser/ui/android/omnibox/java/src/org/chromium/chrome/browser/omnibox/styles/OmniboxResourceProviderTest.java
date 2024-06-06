ce8e41518add59f924f36d70c9cc5d1021cde728
Gang Wu <gangwu@chromium.org>
2024-06-06 05:37:27 +0000

    Update additional text color
    According to UX(https://screenshot.googleplex.com/oJXL2HfWxsWC6w3), we use the secondary text color for now.
    
    Bug: b/342222022
    
    Change-Id: Ic2574ef52c4eb48519620ad236fc3ba3479d4e6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585667
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1311086}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/styles/OmniboxResourceProviderTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/styles/OmniboxResourceProviderTest.java
@@ -458,4 +458,14 @@ public class OmniboxResourceProviderTest {
                 originalContext,
                 OmniboxResourceProvider.maybeReplaceContextForSmallTabletWindow(originalContext));
     }
+
+    @Test
+    public void getAdditionalTextColor() {
+        final int defaultTextColorSecondary =
+                MaterialColors.getColor(mActivity, R.attr.colorOnSurfaceVariant, TAG);
+        assertEquals(
+                "Wrong additional text color.",
+                defaultTextColorSecondary,
+                OmniboxResourceProvider.getAdditionalTextColor(mActivity));
+    }
 }
