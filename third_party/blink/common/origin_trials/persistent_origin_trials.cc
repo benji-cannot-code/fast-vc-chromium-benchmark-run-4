19d0a2b770ef01abc09c3002a52bfc991bf29877
Joshua Hood <jdh@chromium.org>
2024-06-06 21:11:36 +0000

    Add RuntimeEnabledFeatures entry for origin trial for 3PCD
    This does not affect Blink behavior, but is needed to support integration with the Origin Trials framework.
    
    Bug: 332707126
    Change-Id: Idf3715d47705f6fbbec3051d358d6221a5a183ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599005
    Auto-Submit: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311559}

--- third_party/blink/common/origin_trials/persistent_origin_trials.cc
+++ third_party/blink/common/origin_trials/persistent_origin_trials.cc
@@ -28,6 +28,7 @@ bool IsTrialPersistentToNextResponse(std::string_view trial_name) {
       "WebViewXRequestedWithDeprecation",
       "Tpcd",
       "TopLevelTpcd",
+      "LimitThirdPartyCookies",
   };
   return base::Contains(kPersistentTrials, trial_name);
 }
