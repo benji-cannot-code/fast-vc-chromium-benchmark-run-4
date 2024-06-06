ba3136fd6b22d9a53a60f8c1d2a0ea0f8154deff
Kelvin Jiang <kelvinjiang@chromium.org>
2024-06-06 20:13:57 +0000

    [Extensions] Add experiment config for UseItemSnippetsAPI
    This adds testing config for the feature implemented in
    crrev.com/c/5246130 for Windows, Mac and Linux
    
    Migrating tests for ChromeOS so they pass with the new feature flag
    will be done in a follow up
    
    Bug: 325314721
    Change-Id: I2faec2bcb4273f506fed6d134fa63d4d075b67fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582839
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311502}

--- chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
+++ chrome/browser/ash/login/app_mode/test/kiosk_enterprise_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ash/app_mode/fake_cws.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_launch_error.h"
@@ -38,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test_utils.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/app_window_registry.h"
+#include "extensions/common/extension_features.h"
 #include "extensions/common/mojom/manifest.mojom-shared.h"
 #include "google_apis/gaia/fake_gaia.h"
 #include "google_apis/gaia/gaia_constants.h"
@@ -82,7 +84,12 @@ class KioskEnterpriseTest : public KioskBaseTest {
   KioskEnterpriseTest& operator=(const KioskEnterpriseTest&) = delete;
 
  protected:
-  KioskEnterpriseTest() = default;
+  KioskEnterpriseTest() {
+    // TODO(crbug.com/325314721): Remove this feature override once test-only
+    // handling for the item snippets API is complete.
+    scoped_feature_list_.InitAndDisableFeature(
+        extensions_features::kUseItemSnippetsAPI);
+  }
 
   // KioskBaseTest:
   void SetUpOnMainThread() override {
@@ -142,6 +149,7 @@ class KioskEnterpriseTest : public KioskBaseTest {
  private:
   DeviceStateMixin device_state_{
       &mixin_host_, DeviceStateMixin::State::OOBE_COMPLETED_CLOUD_ENROLLED};
+  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(KioskEnterpriseTest, EnterpriseKioskApp) {
