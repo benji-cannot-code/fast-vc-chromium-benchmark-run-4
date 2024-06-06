f056b4ea77d693fed8e01821df3caec5732aec04
Tomasz Wiszkowski <ender@google.com>
2024-06-06 23:00:18 +0000

    Force instantiate RevenueStats during late init stage.
    This change ensures RevenueStats instance is created and appropriate
    tracking ids get applied during post-native initialization.
    
    Normally this class is instantiated much earlier by various other
    facilities, but it is theoretically possible that some flows would not
    include this step.
    
    This change ensures that RevenueStats is initialized during the Chrome
    start up process.
    
    Change-Id: Id1e7cfea24dc304128ecb5ccfda150450bd7d1b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606364
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311610}

--- chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/ProcessInitializationHandler.java
@@ -91,6 +91,7 @@ import org.chromium.chrome.browser.profiles.ProfileKeyedMap.ProfileSelection;
 import org.chromium.chrome.browser.profiles.ProfileManager;
 import org.chromium.chrome.browser.profiles.ProfileManagerUtils;
 import org.chromium.chrome.browser.quickactionsearchwidget.QuickActionSearchWidgetProvider;
+import org.chromium.chrome.browser.rlz.RevenueStats;
 import org.chromium.chrome.browser.searchwidget.SearchWidgetProvider;
 import org.chromium.chrome.browser.signin.SigninCheckerProvider;
 import org.chromium.chrome.browser.tab.state.PersistedTabData;
@@ -664,6 +665,8 @@ public class ProcessInitializationHandler {
 
         // Record the saved restore state in a histogram
         tasks.add(ChromeBackupAgentImpl::recordRestoreHistogram);
+
+        tasks.add(RevenueStats::getInstance);
 
         tasks.add(
                 () -> {
