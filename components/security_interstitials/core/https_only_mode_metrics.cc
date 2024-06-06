79e80d61b22f3df1294c25083ddfba9a62565acd
Chris Thompson <cthomp@chromium.org>
2024-06-06 00:01:41 +0000

    Exempt non-unique hostnames from HTTPS-First Mode in Incognito
    For the default-enabled HFM-in-Incognito, skip upgrading non-unique
    hostnames as they are likely to fail (as they can't get publicly
    trusted certificates), to reduce the incidence of HTTP warnings.
    
    This adds a new flag to the HttpInterstitialState to track when
    HFM-in-Incognito applies (instead of setting the main HFM flag),
    simplifies some of the HTTPS-Upgrades browser tests now that
    HTTPS-Upgrades is fully launched, and cleans up logic for checking
    exemptions.
    
    Bug: 40937027
    Change-Id: I550043d4d357138bdc93ebc3a6c1e6f9b4a444f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597803
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310990}

--- components/security_interstitials/core/https_only_mode_metrics.cc
+++ components/security_interstitials/core/https_only_mode_metrics.cc
@@ -28,6 +28,9 @@ InterstitialReason GetInterstitialReason(
   if (interstitial_state.enabled_by_pref) {
     return InterstitialReason::kPref;
   }
+  if (interstitial_state.enabled_by_incognito) {
+    return InterstitialReason::kIncognito;
+  }
   return InterstitialReason::kUnknown;
 }
 
