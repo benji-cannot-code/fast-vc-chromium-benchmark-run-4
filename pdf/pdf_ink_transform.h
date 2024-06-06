6986c77c9bb80df749be854abeb07dfb74e43ad5
Alan Screen <awscreen@chromium.org>
2024-06-06 21:19:13 +0000

    [PDF Ink Signatures] Converter for input positions format
    Event positions collected during an ink stroke are provided as screen
    coordinates, at an offset from the top left corner of an available
    marking area.  To understand the real position within the page being
    stroked, the position needs to take page orientation, zoom level, and
    offsets into account.
    
    Introduce a utility helper to convert the ink event input positions into
    a canonical format, representing the position as CSS pixels within a
    page in its original orientation.  The returned position will represent
    the location in the page at 100% zoom for its natural orientation.
    
    Bug: 335517471
    Change-Id: I4f8717d410b015f8da8f675a29eac8fbcb6b3715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597798
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311562}

---
+++ pdf/pdf_ink_transform.h
@@ -0,0 +1,45 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_PDF_INK_TRANSFORM_H_
+#define PDF_PDF_INK_TRANSFORM_H_
+
+#include "pdf/buildflags.h"
+#include "pdf/page_orientation.h"
+#include "ui/gfx/geometry/point_f.h"
+
+static_assert(BUILDFLAG(ENABLE_PDF_INK2), "ENABLE_PDF_INK2 not set to true");
+
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
+namespace chrome_pdf {
+
+// Converts a screen-based event input position into a page-based CSS pixels
+// position.  This canonical format is relative to the upper-left corner of a
+// page for its original orientation at a scale factor of 100%.
+// - `event_position`:
+//     The input position, in screen-based coordinates.  Must already have had
+//     any offset from a viewport origin to the page origin applied to it.
+// - `orientation`:
+//     Current orientation of the page.
+// - `page_content_rect`:
+//     Scaled and rotated CSS coordinates of the page content area.  The amount
+//     of scale and rotation match that of `orientation` and `scale_factor`.
+//     The area's origin has the same offset from a viewport origin as
+//     `event_position`.  Must not be empty.
+// - `scale_factor`:
+//     The current zoom factor, with 1.0 representing identity.  Must be greater
+//     than zero.  This is used to ensure the resulting point is relative to a
+//     scale factor of 100%.
+gfx::PointF EventPositionToCanonicalPosition(const gfx::PointF& event_position,
+                                             PageOrientation orientation,
+                                             const gfx::Rect& page_content_rect,
+                                             float scale_factor);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_PDF_INK_TRANSFORM_H_
