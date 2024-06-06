13179565ce18ca4f3211190080197eca52a8088d
Chuan Chen <chuanchen@google.com>
2024-06-06 00:08:23 +0000

    Update echo private ash to use VPD activation date instead of OOBE file date.
    I've verified that the date returned to the ECHO extension survives powerwashing.
    
    Bug: 344620198
    Change-Id: I3ce1cf74f9645deb61aa8aba9afbdb0b2b57492a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599379
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Chuan Chen <chuanchen@google.com>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1310993}

--- chrome/browser/ash/crosapi/echo_private_ash.cc
+++ chrome/browser/ash/crosapi/echo_private_ash.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/common/url_constants.h"
+#include "chromeos/ash/components/report/utils/time_utils.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "chromeos/ash/components/system/statistics_provider.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
@@ -29,19 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/window_open_disposition.h"
 
 namespace crosapi {
-
-namespace {
-
-// Gets the Oobe timestamp on a sequence that allows file-access.
-std::string GetOobeTimestampBackground() {
-  base::Time creation_time = ash::StartupUtils::GetTimeOfOobeFlagFileCreation();
-  return !creation_time.is_null()
-             ? base::UnlocalizedTimeFormatWithPattern(creation_time, "y-M-d",
-                                                      icu::TimeZone::getGMT())
-             : std::string();
-}
-
-}  // namespace
 
 EchoPrivateAsh::EchoPrivateAsh() = default;
 EchoPrivateAsh::~EchoPrivateAsh() = default;
@@ -84,9 +72,15 @@ void EchoPrivateAsh::CheckRedeemOffersAllowed(const std::string& window_id,
 }
 
 void EchoPrivateAsh::GetOobeTimestamp(GetOobeTimestampCallback callback) {
-  base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&GetOobeTimestampBackground), std::move(callback));
+  std::string result;
+  if (const std::optional<base::Time> activateDate =
+          ash::report::utils::GetFirstActiveWeek()) {
+    result = base::UnlocalizedTimeFormatWithPattern(
+        activateDate.value(), "y-M-d", icu::TimeZone::getGMT());
+  }
+
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), result));
 }
 
 void EchoPrivateAsh::GetRegistrationCode(mojom::RegistrationCodeType type,
--- chrome/browser/chromeos/extensions/echo_private/echo_private_apitest.cc
+++ chrome/browser/chromeos/extensions/echo_private/echo_private_apitest.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
+#include "chromeos/ash/components/system/fake_statistics_provider.h"
+#include "chromeos/ash/components/system/statistics_provider.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/api_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -42,6 +44,16 @@ class ExtensionEchoPrivateApiTest : public extensions::ExtensionApiTest {
   ~ExtensionEchoPrivateApiTest() override {}
 
   void SetUp() override {
+    statistics_provider_.SetVpdStatus(
+        ash::system::StatisticsProvider::VpdStatus::kValid);
+    statistics_provider_.SetMachineStatistic(ash::system::kOffersCouponCodeKey,
+                                             "COUPON_CODE");
+    statistics_provider_.SetMachineStatistic(ash::system::kOffersGroupCodeKey,
+                                             "GROUP_CODE");
+    statistics_provider_.SetMachineStatistic(ash::system::kActivateDateKey,
+                                             "2024-13");
+    ash::system::StatisticsProvider::SetTestProvider(&statistics_provider_);
+
     ash::EchoDialogView::AddShowCallbackForTesting(base::BindOnce(
         &ExtensionEchoPrivateApiTest::OnDialogShown, base::Unretained(this)));
     extensions::ExtensionApiTest::SetUp();
@@ -130,6 +142,7 @@ class ExtensionEchoPrivateApiTest : public extensions::ExtensionApiTest {
   int expected_dialog_buttons_;
   DialogTestAction dialog_action_;
   ash::ScopedTestingCrosSettings scoped_testing_cros_settings_;
+  ash::system::FakeStatisticsProvider statistics_provider_;
 
  private:
   int dialog_invocation_count_;
