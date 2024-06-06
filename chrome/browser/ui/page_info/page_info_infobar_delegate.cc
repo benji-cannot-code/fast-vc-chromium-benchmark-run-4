b7486faa32f9b718f8f8c69aad76ca1664fa087b
Dana Fried <dfried@chromium.org>
2024-06-06 23:47:45 +0000

    [GM2 Cleanup] page_info_infobar_delegate.cc
    #chrome-refresh-2023-cleanup
    
    Change-Id: I8d87e1b9970a799864cc9806b2b669950570a4f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605395
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311629}

--- chrome/browser/ui/page_info/page_info_infobar_delegate.cc
+++ chrome/browser/ui/page_info/page_info_infobar_delegate.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/ui_base_features.h"
 
 // static
 void PageInfoInfoBarDelegate::Create(
@@ -24,7 +23,7 @@ void PageInfoInfoBarDelegate::Create(
       std::unique_ptr<ConfirmInfoBarDelegate>(new PageInfoInfoBarDelegate())));
 }
 
-PageInfoInfoBarDelegate::PageInfoInfoBarDelegate() : ConfirmInfoBarDelegate() {}
+PageInfoInfoBarDelegate::PageInfoInfoBarDelegate() = default;
 
 PageInfoInfoBarDelegate::~PageInfoInfoBarDelegate() {}
 
@@ -34,9 +33,7 @@ PageInfoInfoBarDelegate::GetIdentifier() const {
 }
 
 const gfx::VectorIcon& PageInfoInfoBarDelegate::GetVectorIcon() const {
-  return features::IsChromeRefresh2023()
-             ? vector_icons::kSettingsChromeRefreshIcon
-             : vector_icons::kSettingsIcon;
+  return vector_icons::kSettingsChromeRefreshIcon;
 }
 
 std::u16string PageInfoInfoBarDelegate::GetMessageText() const {
