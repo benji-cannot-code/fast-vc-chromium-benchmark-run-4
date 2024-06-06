48c21dd67757043398f7f9e4cb998c240cff12ee
Lingqi Chi <lingqi@chromium.org>
2024-06-06 08:54:54 +0000

    Prefetch: guard the prefetch param
    See the issue description for details
    
    Bug: 345275145
    Change-Id: I94dbdce72579b545d315353b1cd1de36387faf24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600919
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311128}

--- chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_prefetch_request.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/containers/fixed_flat_set.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/metrics/histogram_functions.h"
@@ -50,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include "base/check_is_test.h"
 #include "chrome/browser/android/omnibox/geolocation_header.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
@@ -116,7 +118,40 @@ const char* SearchPrefetchStatusToString(SearchPrefetchStatus status) {
   }
 }
 
+#if BUILDFLAG(IS_ANDROID)
+// TODO(crbug.com/345275145): remove this block.
+
+bool is_test = false;
+
+bool CheckPrefetchParameterExistence(const GURL& url, int throttle_position) {
+  std::string_view query_piece = url.query_piece();
+  url::Component query(0, url.query_piece().length());
+  url::Component key, value;
+  while (url::ExtractQueryKeyValue(query_piece, &query, &key, &value)) {
+    if (query_piece.substr(key.begin, key.len) == "pf" && !value.is_empty()) {
+      return true;
+    }
+  }
+  // It is quite common that a test does not specify the parameter.
+  if (!is_test) {
+    SCOPED_CRASH_KEY_NUMBER("Bug_345275145", "throttle", throttle_position);
+    base::debug::DumpWithoutCrashing();
+    return false;
+  }
+  // Let tests continue fetching.
+  return true;
+}
+#endif  // BUILDFLAG(IS_ANDROID)
+
 }  // namespace
+
+#if BUILDFLAG(IS_ANDROID)
+// static
+void SearchPrefetchRequest::SetIsTest() {
+  CHECK_IS_TEST();
+  is_test = true;
+}
+#endif
 
 SearchPrefetchRequest::SearchPrefetchRequest(
     const GURL& canonical_search_url,
@@ -272,6 +307,13 @@ bool SearchPrefetchRequest::StartPrefetchRequest(Profile* profile) {
     TRACE_EVENT0(
         "loading",
         "SearchPrefetchRequest::StartPrefetchRequest.ExecuteThrottles");
+#if BUILDFLAG(IS_ANDROID)
+    if (!CheckPrefetchParameterExistence(resource_request->url, -1)) {
+      return false;
+    }
+    size_t throttle_id = 0;
+#endif
+
     for (auto& throttle : throttles) {
       CheckForCancelledOrPausedDelegate cancel_or_pause_delegate;
       throttle->set_delegate(&cancel_or_pause_delegate);
@@ -282,6 +324,12 @@ bool SearchPrefetchRequest::StartPrefetchRequest(Profile* profile) {
             "SearchPrefetchRequest::StartPrefetchRequest.WillStartRequest");
         throttle->WillStartRequest(resource_request.get(), &should_defer);
       }
+#if BUILDFLAG(IS_ANDROID)
+      if (!CheckPrefetchParameterExistence(resource_request->url,
+                                           throttle_id++)) {
+        return false;
+      }
+#endif
 
       // Make sure throttles are deleted before |cancel_or_pause_delegate| in
       // case they call into the delegate in the destructor.
