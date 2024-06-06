8bee347b0ea29da1692f7d50124eab470bc51b0b
Thomas Nguyen <tungnh@chromium.org>
2024-06-06 15:15:27 +0000

    [PEPC]: Fire device status changed event when request finishes
    Before requesting the embedded permission, we will cache the device
    status. Once the request is complete, we will trigger a status change
    event.
    
    Fixed: 343010261
    
    Change-Id: I6f938577e3344a91634d09c36073b3d5472f714b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598758
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311271}

--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -737,7 +737,6 @@ void HTMLPermissionElement::OnEmbeddedPermissionsDecided(
       return;
     case EmbeddedPermissionControlResult::kGranted:
       permissions_granted_ = true;
-      UpdateAppearance();
       DispatchEvent(*Event::Create(event_type_names::kResolve));
       return;
     case EmbeddedPermissionControlResult::kDenied:
