d0acd3a5130a18f68b58ef1367ef0e6c531fb410
Bohdan Tyshchenko <bohdanty@google.com>
2024-06-06 13:34:13 +0000

    [OOBE] - Clear Use-Case selection screen UI on every exit
    Reset data in the Device Use-Case screen on every transition to free up
    WebViews.
    
    To handle previous screen's state checked icons we add a new function to
    change checked state when we rendered all items.
    
    Previously, if you select some use-cases and then press skip and go back
    you would end-up having wrong UI as non-selected icons will still have
    an old UI state.
    
    Bug: b/342594677
    Change-Id: Ia89be82b8fd8010b1152fe05ee6c42f12aea823b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598725
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311212}

--- chrome/browser/resources/chromeos/login/screens/common/categories_selection.ts
+++ chrome/browser/resources/chromeos/login/screens/common/categories_selection.ts
@@ -39,7 +39,7 @@ export const CategoriesScreenElementBase =
           MultiStepBehaviorInterface,
     };
 
-enum CaegoriesStep {
+enum CategoriesStep {
   LOADING = 'loading',
   OVERVIEW = 'overview',
 }
@@ -78,12 +78,12 @@ export class CategoriesScreenElement extends CategoriesScreenElementBase {
   private numberOfSelectedCategories: number;
 
   override get UI_STEPS() {
-    return CaegoriesStep;
+    return CategoriesStep;
   }
 
   // eslint-disable-next-line @typescript-eslint/naming-convention
   override defaultUIStep() {
-    return CaegoriesStep.LOADING;
+    return CategoriesStep.LOADING;
   }
 
   override ready(): void {
@@ -103,6 +103,16 @@ export class CategoriesScreenElement extends CategoriesScreenElementBase {
     return OobeUiState.ONBOARDING;
   }
 
+  override onBeforeShow(): void {
+    this.setUIStep(CategoriesStep.LOADING);
+  }
+
+
+  onBeforeHide(): void {
+    this.shadowRoot!.querySelector<OobeCategoriesList>(
+                        '#categoriesList')!.reset();
+  }
+
   setCategoriesData(categoriesData: CategoriesScreenData): void {
     assert('categories' in categoriesData);
     this.shadowRoot!.querySelector<OobeCategoriesList>('#categoriesList')!
@@ -110,7 +120,7 @@ export class CategoriesScreenElement extends CategoriesScreenElementBase {
   }
 
   setOverviewStep(): void {
-    this.setUIStep(CaegoriesStep.OVERVIEW);
+    this.setUIStep(CategoriesStep.OVERVIEW);
     const categoriesList =
         this.shadowRoot?.querySelector<HTMLElement>('#categoriesList');
     if (categoriesList instanceof HTMLElement) {
