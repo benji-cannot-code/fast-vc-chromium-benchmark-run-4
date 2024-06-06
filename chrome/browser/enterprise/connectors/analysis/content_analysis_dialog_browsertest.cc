040ba3ca40fdaa75296ccec572883f5896bc257c
dileep maurya <dileepmaurya@microsoft.com>
2024-06-06 05:08:03 +0000

    [views-ax] Migrate GetAccessibleRole to ViewAccessibility getter in chrome/browser/ui/* folder
    This CL migrates callers from using View::GetAccessibleRole to the
    new ViewAccessibility::GetCachedRole for all the files
    in chrome/browser/ui/*. Also, this CL does not introduce 
    a change in behavior.
    You can find the design doc for the ViewsAX project here:
    
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit
    
    Bug: 325137417
    Change-Id: I8931b21199597670d926f8a7adffa2a165909f76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590894
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Dileep Maurya <dileepmaurya@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311076}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_dialog_browsertest.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_dialog_browsertest.cc
@@ -1202,7 +1202,8 @@ IN_PROC_BROWSER_TEST_F(ContentAnalysisDialogPlainTests,
   ui::AXNodeData textarea_data;
   textarea->GetViewAccessibility().GetAccessibleNodeData(&textarea_data);
   EXPECT_EQ(textarea_data.role, ax::mojom::Role::kTextField);
-  EXPECT_EQ(textarea->GetAccessibleRole(), ax::mojom::Role::kTextField);
+  EXPECT_EQ(textarea->GetViewAccessibility().GetCachedRole(),
+            ax::mojom::Role::kTextField);
   EXPECT_EQ(
       textarea_data.GetString16Attribute(ax::mojom::StringAttribute::kName),
       label->GetViewAccessibility().GetCachedName());
