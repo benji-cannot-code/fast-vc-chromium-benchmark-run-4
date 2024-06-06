d2f6042554e3f9ad28736e74dafa7ef3baee9335
Peter Boström <pbos@chromium.org>
2024-06-06 10:58:42 +0000

    Upgrade NOTREACHED to CHECK in FidoHidMessage
    This invariant failure should be unreachable (filtered before it reaches
    this point).
    
    Bug: 40580068
    Change-Id: I589aaf97619b04e24f2a6b7f23d852eb7406b066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600525
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311166}

--- device/fido/hid/fido_hid_message.cc
+++ device/fido/hid/fido_hid_message.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <numeric>
 #include <utility>
 
+#include "base/check_op.h"
 #include "base/memory/ptr_util.h"
 #include "base/numerics/safe_conversions.h"
 #include "device/fido/fido_parsing_utils.h"
@@ -24,11 +25,9 @@ std::optional<FidoHidMessage> FidoHidMessage::Create(
   if (data.size() > kHidMaxMessageSize)
     return std::nullopt;
 
-  if (max_report_size <= kHidInitPacketHeaderSize ||
-      max_report_size > kHidMaxPacketSize) {
-    NOTREACHED_IN_MIGRATION();
-    return std::nullopt;
-  }
+  // Unsupported devices should have been dropped in fido_hid_discovery.cc.
+  CHECK_GT(max_report_size, kHidInitPacketHeaderSize);
+  CHECK_LE(max_report_size, kHidMaxPacketSize);
 
   switch (type) {
     case FidoHidDeviceCommand::kPing:
