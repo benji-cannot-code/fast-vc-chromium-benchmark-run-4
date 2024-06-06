3627f3c7b524e7797a787e1814a488a9f15acac8
Henrique Nakashima <hnakashima@chromium.org>
2024-06-06 21:01:43 +0000

    [Android] Workaround cut off color picker - HubNewTabGroupDialogFacility
    HubNewTabGroupDialogFacility required the color picker to be >90%
    displayed but in android_30_google_apis_x86.textpb the color picker
    looks slightly cut off. Make the check more lenient, we still check that
    the colored circles are shown.
    
    Bug: 345489175
    Change-Id: I68eb26542c50c8d0a10b8da0694687a1d14ee13c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605184
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311549}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ViewElement.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ViewElement.java
@@ -55,6 +55,14 @@ public class ViewElement {
     }
 
     /**
+     * Alias for {@link #sharedViewElement(Matcher, Options)} as the default way to declare
+     * ViewElements.
+     */
+    public static ViewElement viewElement(Matcher<View> viewMatcher, Options options) {
+        return sharedViewElement(viewMatcher, options);
+    }
+
+    /**
      * Version of {@link #sharedViewElement(Matcher, Options)} using default Options.
      *
      * <p>This is a good default method to the declare ViewElements; when in doubt, use this.
@@ -187,6 +195,7 @@ public class ViewElement {
         static final Options DEFAULT = new Options();
         protected boolean mExpectEnabled = true;
         protected String mElementId;
+        protected Integer mDisplayedPercentageRequired = MIN_DISPLAYED_PERCENT;
 
         protected Options() {}
 
@@ -212,6 +221,16 @@ public class ViewElement {
              */
             public Builder expectDisabled() {
                 mExpectEnabled = false;
+                return this;
+            }
+
+            /**
+             * Changes the minimum percentage of the View that needs be displayed to fulfill the
+             * enter Condition. Default is >=90% visible, which matches the minimum requirement for
+             * ViewInteractions like click().
+             */
+            public Builder displayingAtLeast(int percentage) {
+                mDisplayedPercentageRequired = percentage;
                 return this;
             }
         }
