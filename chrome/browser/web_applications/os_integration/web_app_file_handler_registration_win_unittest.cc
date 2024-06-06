802d460e6861b5b4deecab1b5dc7e32e634744d1
Dibyajyoti Pal <dibyapal@google.com>
2024-06-06 15:40:02 +0000

    [PWA] Remove FakeWebAppFileHandlerManager
    This CL removes the FakeWebAppFileHandlerManager, and changes all
    usages of it to use the real WebAppFileHandlerManager. The unit-tests
    have been moved to the same test file as FileHandlingSubManager to
    use existing Synchronization workflow to test the E2E logic, thus
    reducing the tight coupling in the previous implementation.
    
    This helps make the removal of the actual WebAppFileHandlerManager
    a lot easier, and hence has been split into a separate CL. This
    should technically be a no-op.
    
    UPDATE: After 3-4 days of repititive debugging and running swarming
    tests, the flakiness on Mac is still not easily reproducible. Since
    these tests have merely been moved with little change in logic,
    the `Validate-Test-Flakiness` flag is being used to bypass that check.
    The flakiness will be being investigated separately by crbug.com/345367397.
    
    Bug: 267330807, 1401125
    Validate-Test-Flakiness: skip
    Change-Id: Iaed6336a3b4d94eee1202cdb46bac8f1c5d38aaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585526
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311287}

--- chrome/browser/web_applications/os_integration/web_app_file_handler_registration_win_unittest.cc
+++ chrome/browser/web_applications/os_integration/web_app_file_handler_registration_win_unittest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/web_applications/chrome_pwa_launcher/chrome_pwa_launcher_util.h"
 #include "chrome/browser/web_applications/os_integration/web_app_handler_registration_utils_win.h"
-#include "chrome/browser/web_applications/test/fake_web_app_file_handler_manager.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/installer/util/shell_util.h"
--- chrome/browser/web_applications/test/fake_os_integration_manager.cc
+++ chrome/browser/web_applications/test/fake_os_integration_manager.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/os_integration/web_app_file_handler_manager.h"
 #include "chrome/browser/web_applications/os_integration/web_app_protocol_handler_manager.h"
-#include "chrome/browser/web_applications/test/fake_web_app_file_handler_manager.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
 
 namespace web_app {
@@ -29,16 +28,11 @@ FakeOsIntegrationManager::FakeOsIntegrationManager(
           std::make_unique<OsIntegrationManager::ScopedSuppressForTesting>()) {
   if (!has_file_handler_manager()) {
     set_file_handler_manager(
-        std::make_unique<FakeWebAppFileHandlerManager>(profile));
+        std::make_unique<WebAppFileHandlerManager>(profile));
   }
 }
 
 FakeOsIntegrationManager::~FakeOsIntegrationManager() = default;
-
-void FakeOsIntegrationManager::SetFileHandlerManager(
-    std::unique_ptr<WebAppFileHandlerManager> file_handler_manager) {
-  set_file_handler_manager(std::move(file_handler_manager));
-}
 
 FakeOsIntegrationManager*
 FakeOsIntegrationManager::AsTestOsIntegrationManager() {
