2e22e356e83511bbc7b44214df8952c126668444
James Cook <jamescook@chromium.org>
2024-06-06 15:40:30 +0000

    birch: Add --birch-is-morning command line switch
    This simulates morning for birch chip ranking purposes. It makes it
    easier to view chips that only show in the mornings, like last active
    tab and most visited tab.
    
    Bug: none
    Change-Id: I7e91cb084b178a1156a523f539532c55f42dadac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604563
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311288}

--- ash/birch/birch_ranker.cc
+++ ash/birch/birch_ranker.cc
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "ash/birch/birch_item.h"
+#include "ash/constants/ash_switches.h"
 #include "base/check.h"
+#include "base/command_line.h"
 #include "base/time/time.h"
 
 namespace {
@@ -255,6 +257,10 @@ float BirchRanker::GetReleaseNotesItemRanking(
 }
 
 bool BirchRanker::IsMorning() const {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kBirchIsMorning)) {
+    return true;
+  }
   base::Time last_midnight = now_.LocalMidnight();
   base::Time five_am_today = last_midnight + base::Hours(5);
   base::Time noon_today = last_midnight + base::Hours(12);
