281684b713e6c224b72653e8277b1967dba2f91e
Istvan Nagy <iscsi@google.com>
2024-06-06 15:44:46 +0000

    Add accessible name to the auth textfield.
    Bug: b:286527724
    Change-Id: Ifa13727d4f5cf3779fc9e723fb6988d212c16c16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598791
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311291}

--- ash/auth/views/auth_textfield.cc
+++ ash/auth/views/auth_textfield.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ash/auth/views/auth_textfield_timer.h"
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/system_textfield.h"
 #include "ash/style/system_textfield_controller.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/observer_list_types.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/font_list.h"
@@ -67,6 +69,16 @@ AuthTextfield::AuthTextfield(AuthType auth_type)
   // Set the text colors.
   SetTextColorId(cros_tokens::kCrosSysOnSurface);
   SetPlaceholderTextColorId(cros_tokens::kCrosSysDisabled);
+
+  // Set Accessible name
+  if (auth_type_ == AuthType::kPassword) {
+    SetAccessibleName(l10n_util::GetStringUTF16(
+        IDS_ASH_AUTH_TEXTFIELD_PASSWORD_ACCESSIBLE_NAME));
+  } else {
+    CHECK_EQ(auth_type_, AuthType::kPin);
+    SetAccessibleName(
+        l10n_util::GetStringUTF16(IDS_ASH_AUTH_TEXTFIELD_PIN_ACCESSIBLE_NAME));
+  }
 }
 
 AuthTextfield::~AuthTextfield() = default;
