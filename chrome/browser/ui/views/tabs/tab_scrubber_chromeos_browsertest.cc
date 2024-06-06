0d318ccabcc68c78858e9d600e84fc4a150b16b1
Shibalik Mohapatra <shibalik@chromium.org>
2024-06-06 22:22:00 +0000

    [TabStripModelRefactor] Add method for moving multiple tabs to simplify MoveSelectedTo and MoveGroupTo without breaking group contiguity.
    Change-Id: I1c5884e6c0346b066b251a1e0a351fe5c2d70470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588382
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311593}

--- chrome/browser/ui/views/tabs/tab_scrubber_chromeos_browsertest.cc
+++ chrome/browser/ui/views/tabs/tab_scrubber_chromeos_browsertest.cc
@@ -490,7 +490,7 @@ IN_PROC_BROWSER_TEST_F(TabScrubberChromeOSTest, MoveHighlighted) {
   EXPECT_TRUE(TabScrubberChromeOS::GetInstance()->IsActivationPending());
   browser()->tab_strip_model()->ToggleSelectionAt(0);
   browser()->tab_strip_model()->ToggleSelectionAt(1);
-  browser()->tab_strip_model()->MoveSelectedTabsTo(1);
+  browser()->tab_strip_model()->MoveSelectedTabsTo(1, std::nullopt);
   EXPECT_EQ(1, TabScrubberChromeOS::GetInstance()->highlighted_tab());
 }
 
@@ -503,7 +503,7 @@ IN_PROC_BROWSER_TEST_F(TabScrubberChromeOSTest, MoveBefore) {
   EXPECT_TRUE(TabScrubberChromeOS::GetInstance()->IsActivationPending());
   browser()->tab_strip_model()->ToggleSelectionAt(0);
   browser()->tab_strip_model()->ToggleSelectionAt(2);
-  browser()->tab_strip_model()->MoveSelectedTabsTo(2);
+  browser()->tab_strip_model()->MoveSelectedTabsTo(2, std::nullopt);
   EXPECT_EQ(0, TabScrubberChromeOS::GetInstance()->highlighted_tab());
 }
 
@@ -514,7 +514,7 @@ IN_PROC_BROWSER_TEST_F(TabScrubberChromeOSTest, MoveAfter) {
 
   SendScrubEvent(browser(), 1);
   EXPECT_TRUE(TabScrubberChromeOS::GetInstance()->IsActivationPending());
-  browser()->tab_strip_model()->MoveSelectedTabsTo(0);
+  browser()->tab_strip_model()->MoveSelectedTabsTo(0, std::nullopt);
   EXPECT_EQ(2, TabScrubberChromeOS::GetInstance()->highlighted_tab());
 }
 
@@ -575,7 +575,7 @@ IN_PROC_BROWSER_TEST_F(TabScrubberChromeOSTest, RTLMoveBefore) {
   EXPECT_TRUE(TabScrubberChromeOS::GetInstance()->IsActivationPending());
   browser()->tab_strip_model()->ToggleSelectionAt(0);
   browser()->tab_strip_model()->ToggleSelectionAt(2);
-  browser()->tab_strip_model()->MoveSelectedTabsTo(2);
+  browser()->tab_strip_model()->MoveSelectedTabsTo(2, std::nullopt);
   EXPECT_EQ(0, TabScrubberChromeOS::GetInstance()->highlighted_tab());
 }
 
