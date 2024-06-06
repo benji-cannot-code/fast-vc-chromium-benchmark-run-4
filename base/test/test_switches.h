86695a12d08fd54c0c0eabd6e87181f41b42b7a1
Adrian Taylor <adetaylor@chromium.org>
2024-06-06 06:18:23 +0000

    Initialize FuzzTest for all Chromium tests.
    FUZZ_TEST is a new type of test which is half way between a unit test
    and a fuzzer (the same code can run in two modes).
    
    Eventually, we hope to allow their free inclusion in existing Chromium
    unit test targets (though this is still not encouraged by Chromium's
    fuzzing documentation as there are a number of things we need to resolve
    first).
    
    This extra call in base::TestSuite sets up FuzzTest ready for this.
    
    base/test:test_support is included in all sorts of test targets, not
    just test suites. And, of the test suites where it ends up being
    included, some of them will have fuzztests and some of them won't. This
    CL is heavily structured such that the new code impacts only test suites
    which have fuzztests.
    
    Specifically,
    
    * base/test:test_support ends up being included in various pre-existing
      fuzzers. This is a problem because those fuzzers might
      have implementations of core fuzzing functions such as
      LLVMFuzzerCustomMutator, which would conflict with the fuzztest
      implementation. We therefore take pains not to include
      src/fuzztest/internal/compatibility_mode.cc except if the test suite
      really does have fuzztests.
    * base/test:test_support is sometimes linked from targets that include
      protobuf_full (notably some GPU test targets). fuzztest depends on
      protobuf_lite. On component builds this causes a conflict - a build
      failure on Windows and ODR violations on other platforms. We therefore
      need to avoid including fuzztests' dependencies into
      base/test:test_support unless there really are fuzztests in the test
      suite.
    
    Fortunately, testing/test.gni already knows whether there are fuzztests
    in a given test suite, as it needs to generate wrapper executables for
    each fuzztest.
    
    We therefore use dependency injection, where base/test:test_support calls
    a MaybeInitFuzztest function. In test suites containing real fuzztests,
    a function pointer will have been populated with a real function that
    can initialize fuzztests.
    
    A couple of other details:
    * Fuzztest takes the liberty of looking at its command line arguments
      "later". Chromium turns out to modify its command line, and thus the
      fuzztest command line would become invalid. We store a copy to avoid
      this problem.
    * When test suites are running in fuzzing mode, we avoid going into
      multiple process test mode.
    
    Cq-Include-Trybots: luci.chromium.try:linux-libfuzzer-asan-rel,win-libfuzzer-asan-rel,linux-centipede-asan-rel
    Bug: 40286211
    Change-Id: I9456ffd6b273c0e32fd88abd8547bf7f9cad96a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4981917
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311096}

--- base/test/test_switches.h
+++ base/test/test_switches.h
@@ -43,6 +43,9 @@ extern const char kTestTinyTimeout[];
 extern const char kUiTestActionMaxTimeout[];
 extern const char kUiTestActionTimeout[];
 extern const char kWithDeathTestStackTraces[];
+extern const char kFuzz[];
+extern const char kFuzzFor[];
+extern const char kListFuzzTests[];
 
 #if BUILDFLAG(IS_IOS)
 extern const char kEnableRunIOSUnittestsWithXCTest[];
