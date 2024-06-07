cf83de60c6a4d2387f98ad3944d6b368d26132a4
Takumi Fujimoto <takumif@chromium.org>
2024-06-07 22:01:57 +0000

    Use the system scrim color for exclusive access permission prompt
    The system scrim color is currently black with 0.8 opacity.
    
    Bug: 345721017
    Change-Id: Ie98cc88b84781b03bb2a85140a9ce8399acdd0f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607803
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312280}

--- chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.cc
+++ chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.cc
@@ -51,7 +51,7 @@ void ExclusiveAccessPermissionPrompt::ShowPrompt() {
   content_scrim_widget_ =
       EmbeddedPermissionPromptContentScrimView::CreateScrimWidget(
           weak_factory_.GetWeakPtr(),
-          SkColorSetA(SK_ColorBLACK, SK_AlphaOPAQUE * 0.9f));
+          web_contents()->GetColorProvider().GetColor(ui::kColorSysStateScrim));
   prompt_view->UpdateAnchor(content_scrim_widget_.get());
   prompt_view->Show();
 }
