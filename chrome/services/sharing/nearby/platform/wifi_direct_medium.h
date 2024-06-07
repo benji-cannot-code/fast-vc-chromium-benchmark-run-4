db87f408ae88ed2212f6087a1a6bc64f4b26c210
Jack Shira <jackshira@google.com>
2024-06-07 04:19:27 +0000

    [Nearby Share] Implement Connect for WiDi
    Implements both ConnectWifiDirect and ConnectToGroup for
    WifiDirectMedium.
    
    Tests: unittested, manually verfied sending to P8
    Bug: b/345308631
    Change-Id: I15daa9b7ef9a2c4eef1a6e006571ebb9cf01b200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5602153
    Reviewed-by: Brando Socarras <brandosocarras@google.com>
    Commit-Queue: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311746}

--- chrome/services/sharing/nearby/platform/wifi_direct_medium.h
+++ chrome/services/sharing/nearby/platform/wifi_direct_medium.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/services/wifi_direct/public/mojom/wifi_direct_manager.mojom.h"
 #include "mojo/public/cpp/bindings/shared_remote.h"
 #include "net/socket/socket_descriptor.h"
+#include "net/socket/tcp_client_socket.h"
 #include "net/socket/tcp_server_socket.h"
 #include "third_party/nearby/src/internal/platform/implementation/wifi_direct.h"
 #include "third_party/nearby/src/internal/platform/wifi_credential.h"
@@ -63,6 +64,14 @@ class WifiDirectMedium : public api::WifiDirectMedium {
       base::WaitableEvent* waitable_event,
       ash::wifi_direct::mojom::WifiDirectConnectionPropertiesPtr properties);
 
+  void ConnectGroup(WifiDirectCredentials* credentials,
+                    base::WaitableEvent* waitable_event);
+  void OnGroupConnected(
+      base::WaitableEvent* waitable_event,
+      ash::wifi_direct::mojom::WifiDirectOperationResult result,
+      mojo::PendingRemote<ash::wifi_direct::mojom::WifiDirectConnection>
+          connection);
+
   void AssociateSocket(bool* did_associate,
                        base::WaitableEvent* waitable_event,
                        mojo::PlatformHandle socket_descriptor);
@@ -73,6 +82,17 @@ class WifiDirectMedium : public api::WifiDirectMedium {
                                net::SocketDescriptor socket_descriptor,
                                std::unique_ptr<net::TCPServerSocket>* socket,
                                base::WaitableEvent* waitable_event);
+  void CreateAndConnectSocket(const std::string_view& ip_address,
+                              int port,
+                              int fd,
+                              std::unique_ptr<net::TCPClientSocket>* socket,
+                              base::WaitableEvent* waitable_event);
+  void OnSocketConnected(std::unique_ptr<net::TCPClientSocket>* socket,
+                         base::WaitableEvent* waitable_event,
+                         std::unique_ptr<net::TCPSocket> tcp_socket,
+                         net::IPEndPoint ip_endpoint,
+                         int result);
+
   void OpenFirewallHole(
       ash::nearby::TcpServerSocketPort port,
       mojo::PendingRemote<::sharing::mojom::FirewallHole>* output,
