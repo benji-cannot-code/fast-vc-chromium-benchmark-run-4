7f3275dcd85681d504cd4bf349854e299c3be538
Darren Shen <shend@chromium.org>
2024-06-07 04:51:17 +0000

    Add preview support for Drive Files.
    Preview thumbnails for Drive files need to be obtained from DriveFS,
    not the ThumbnailLoader.
    
    Bug: b/345377359
    Change-Id: I78af69ea63d96e1c0b4281d030b36d4dcde56022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606882
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311749}

--- chrome/browser/ui/ash/picker/picker_thumbnail_loader.h
+++ chrome/browser/ui/ash/picker/picker_thumbnail_loader.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file.h"
 #include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/ash/thumbnail_loader.h"
 
 class SkBitmap;
@@ -35,7 +36,15 @@ class PickerThumbnailLoader {
             LoadCallback callback);
 
  private:
+  void DecodeDriveThumbnail(LoadCallback callback,
+                            const gfx::Size& size,
+                            const std::optional<std::vector<uint8_t>>& bytes);
+  void OnDriveThumbnailDecoded(LoadCallback callback, const SkBitmap& image);
+
+  raw_ptr<Profile> profile_;
   ash::ThumbnailLoader thumbnail_loader_;
+
+  base::WeakPtrFactory<PickerThumbnailLoader> weak_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_ASH_PICKER_PICKER_THUMBNAIL_LOADER_H_
