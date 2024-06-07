ea795dea3ff083348b1fa395f21926e16c4451e2
Olivier ROBIN <olivierrobin@google.com>
2024-06-07 11:29:39 +0000

    Reconcile TR15 and TR2
    Fix the reporting after TR15 and TR2 are merged
    
    Bug: 342390229
    Change-Id: I14e7694baa2a810e466a2cf7556a77bf80aa3661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598787
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311882}

--- ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_item.mm
+++ ios/chrome/browser/ui/content_suggestions/tab_resumption/tab_resumption_item.mm
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _syncedTime = item.syncedTime;
   _faviconImage = item.faviconImage;
   _salientImage = item.salientImage;
+  _URLKey = item.URLKey;
+  _requestID = item.requestID;
 }
 
 @end
