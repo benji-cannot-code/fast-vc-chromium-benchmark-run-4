573b07d844d37897b4c80cdab3b87581d353ec1d
Joe Downing <joedow@google.com>
2024-06-07 23:34:13 +0000

    Enable crash uploading in the Daemon process
    This CL pulls in the crash file directory watcher and uploader
    classes used in the linux crash_uploader_binary into the Windows
    Daemon process. I'm adding it to the Daemon rather than running
    a separate binary as the directory requires SYSTEM access so I
    would need to run the child process as SYSTEM (which the Daemon
    already is) and I'd need to add the new binary to the WXS and
    signing files. Given that the plan is to move to Crashpad after
    M127 is released, I'd prefer to keep this code in the Daemon.
    
    The other interesting change in this CL is that I had to move
    the thread pool init code from 'Initialize' to the c'tor in
    the DaemonProcess class. This meant using a different
    TaskEnvironment in the tests as previously the threadpool was
    not initialized.
    
    Bug: crbug.com/341368569
    Change-Id: I7873e9bb1c5061616e29d83e50eb7f658c484d42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608451
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312326}

--- remoting/host/daemon_process.cc
+++ remoting/host/daemon_process.cc
@@ -183,6 +183,11 @@ DaemonProcess::DaemonProcess(
   // TODO(sammc): On OSX, mojo::core::SetMachPortProvider() should be called
   // with a base::PortProvider implementation. Add it here when this code is
   // used on OSX.
+
+  // Tests may use their own thread pool so create one if needed.
+  if (!base::ThreadPoolInstance::Get()) {
+    base::ThreadPoolInstance::CreateAndStartWithDefaultParams("Daemon");
+  }
 }
 
 void DaemonProcess::CreateDesktopSession(int terminal_id,
@@ -267,8 +272,6 @@ void DaemonProcess::Initialize() {
   config_watcher_->Watch(this);
   host_event_logger_ =
       HostEventLogger::Create(status_monitor_, kApplicationName);
-
-  base::ThreadPoolInstance::CreateAndStartWithDefaultParams("Daemon");
 
   StartChromotingHostServices();
 
--- remoting/host/daemon_process_win.cc
+++ remoting/host/daemon_process_win.cc
@@ -19,9 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/process/process.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/task/task_traits.h"
+#include "base/task/thread_pool.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "base/win/registry.h"
@@ -33,13 +36,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/base/auto_thread.h"
 #include "remoting/base/auto_thread_task_runner.h"
 #include "remoting/base/breakpad.h"
+#include "remoting/base/breakpad_utils.h"
 #include "remoting/base/logging.h"
 #include "remoting/base/scoped_sc_handle_win.h"
+#include "remoting/base/url_request_context_getter.h"
 #include "remoting/host/base/host_exit_codes.h"
 #include "remoting/host/base/screen_resolution.h"
 #include "remoting/host/base/switches.h"
 #include "remoting/host/branding.h"
 #include "remoting/host/chromoting_host_services_server.h"
+#include "remoting/host/crash/crash_directory_watcher.h"
+#include "remoting/host/crash/crash_file_uploader.h"
 #include "remoting/host/desktop_session_win.h"
 #include "remoting/host/host_config.h"
 #include "remoting/host/host_main.h"
@@ -55,6 +62,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/security_descriptor.h"
 #include "remoting/host/win/unprivileged_process_delegate.h"
 #include "remoting/host/win/worker_process_launcher.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+#include "services/network/transitional_url_loader_factory_owner.h"
 
 using base::win::ScopedHandle;
 
@@ -124,7 +133,7 @@ class DaemonProcessWin : public DaemonProcess {
 
   // If the user has consented to crash reporting, this method will start a
   // BreakpadServer instance to handle crashes from the network process.
-  void ConfigureOopCrashServer();
+  void ConfigureCrashReporting();
 
  protected:
   // DaemonProcess implementation.
@@ -169,6 +178,13 @@ class DaemonProcessWin : public DaemonProcess {
   std::unique_ptr<EtwTraceConsumer> etw_trace_consumer_;
 
   std::unique_ptr<ChromotingHostServicesServer> ipc_server_;
+
+  // Classes needed to upload crash reports.
+  scoped_refptr<net::URLRequestContextGetter> url_request_context_getter_;
+  std::unique_ptr<network::TransitionalURLLoaderFactoryOwner>
+      url_loader_factory_owner_;
+  std::unique_ptr<CrashFileUploader> crash_file_uploader_;
+  std::unique_ptr<CrashDirectoryWatcher> crash_directory_watcher_;
 
   mojo::AssociatedRemote<mojom::DesktopSessionConnectionEvents>
       desktop_session_connection_events_;
@@ -341,8 +357,8 @@ std::unique_ptr<DaemonProcess> DaemonProcess::Create(
   // Configure host logging first so we can capture subsequent events.
   daemon_process->ConfigureHostLogging();
 
-  // Initial OOP crash server before the network process is launched.
-  daemon_process->ConfigureOopCrashServer();
+  // Initialize crash reporting before the network process is launched.
+  daemon_process->ConfigureCrashReporting();
 
   // Finishes configuring the Daemon process and launches the network process.
   daemon_process->Initialize();
@@ -513,10 +529,26 @@ void DaemonProcessWin::BindChromotingHostServices(
                                                      peer_pid);
 }
 
-void DaemonProcessWin::ConfigureOopCrashServer() {
+void DaemonProcessWin::ConfigureCrashReporting() {
   if (IsUsageStatsAllowed()) {
     InitializeOopCrashServer();
-    // TODO: joedow - Initialize minidump file watcher and uploader.
+
+    url_request_context_getter_ = base::MakeRefCounted<URLRequestContextGetter>(
+        base::ThreadPool::CreateSingleThreadTaskRunner(
+            {base::MayBlock(), base::TaskPriority::BEST_EFFORT}));
+    url_loader_factory_owner_ =
+        std::make_unique<network::TransitionalURLLoaderFactoryOwner>(
+            url_request_context_getter_);
+    crash_file_uploader_ = std::make_unique<CrashFileUploader>(
+        url_loader_factory_owner_->GetURLLoaderFactory());
+    crash_directory_watcher_ = std::make_unique<CrashDirectoryWatcher>();
+    // base::Unretained is sound as this instance controls the lifetime of both
+    // |crash_directory_watcher_| and |crash_file_uploader_| and the Upload
+    // callback runs on this sequence.
+    crash_directory_watcher_->Watch(
+        GetMinidumpDirectoryPath(),
+        base::BindRepeating(&CrashFileUploader::Upload,
+                            base::Unretained(crash_file_uploader_.get())));
   }
 }
 
