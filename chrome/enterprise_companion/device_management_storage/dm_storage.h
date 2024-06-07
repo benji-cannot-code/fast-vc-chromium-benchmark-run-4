4e6974b477882ea36d117adac97d8717f4639ee8
Noah Rose Ledesma <noahrose@google.com>
2024-06-07 19:20:13 +0000

    Create a function to read from DM storage
    O4's implementation of DMStorage has an opinionated interface which
    provides a method to read and deserialize O4 policies. CEC's
    implementation should provide a means to read policies of any type.
    Deserializing the policy value into the appropriate proto type can
    become the responsibility of the caller.
    
    Bug: 344878880
    Change-Id: I157996280813344d120bfbc45c90c5a983a08a9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605070
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312173}

--- chrome/enterprise_companion/device_management_storage/dm_storage.h
+++ chrome/enterprise_companion/device_management_storage/dm_storage.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "base/containers/flat_map.h"
@@ -15,10 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/sequence_checker.h"
 #include "build/build_config.h"
-
-namespace wireless_android_enterprise_devicemanagement {
-class OmahaSettingsClientProto;
-}
+#include "components/policy/proto/device_management_backend.pb.h"
 
 namespace device_management_storage {
 
@@ -189,11 +187,10 @@ class DMStorage : public base::RefCountedThreadSafe<DMStorage> {
   // information loaded from file |policy_cache_root_|\CachedPolicyInfo.
   std::unique_ptr<CachedPolicyInfo> GetCachedPolicyInfo() const;
 
-  // Returns the Omaha policy settings loaded from PolicyFetchResponse file in
-  // |policy_cache_root_|\{Base64Encoded{kGoogleUpdatePolicyType}} directory.
-  std::unique_ptr<
-      ::wireless_android_enterprise_devicemanagement::OmahaSettingsClientProto>
-  GetOmahaPolicySettings() const;
+  // Returns the policy data loaded from the PolicyFetchResponse file in the
+  // |policy_cache_root_|\{Base64Encoded{|policy_type|}} directory.
+  std::optional<enterprise_management::PolicyData> ReadPolicyData(
+      const std::string& policy_type);
 
   // Returns the folder that caches the downloaded policies.
   base::FilePath policy_cache_folder() const { return policy_cache_root_; }
