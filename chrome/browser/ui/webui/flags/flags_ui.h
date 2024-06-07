f915b450c15686d4821363886e649160e4a67eb0
dpapad <dpapad@chromium.org>
2024-06-07 01:34:33 +0000

    Reland "WebUI: Migrate FlagsUI to WebUIConfig."
    Reland notes: Addressed the reason original CL was reverted in precursor
    CLs, so this is safe to do now.
    
    Fixed: 344643692
    Change-Id: I559d23d9a20e0fb851ce8c38e90c32c7c02aadd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598279
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311688}

--- chrome/browser/ui/webui/flags/flags_ui.h
+++ chrome/browser/ui/webui/flags/flags_ui.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/webui_config.h"
 #include "content/public/common/url_constants.h"
 #include "ui/base/resource/resource_scale_factor.h"
 
@@ -23,6 +24,13 @@ class WebUIDataSource;
 }
 
 class FlagsUI;
+
+class FlagsUIConfig : public content::DefaultWebUIConfig<FlagsUI> {
+ public:
+  FlagsUIConfig()
+      : DefaultWebUIConfig(content::kChromeUIScheme,
+                           chrome::kChromeUIFlagsHost) {}
+};
 
 class FlagsUI : public content::WebUIController {
  public:
