50a796ce3b5ef0c663914414bdb76242d50199bf
Yao Xiao <yaoxia@chromium.org>
2024-06-07 00:37:29 +0000

    [shared storage] Allow createWorklet() in opaque origin contexts
    PR: https://github.com/WICG/shared-storage/pull/156
    
    Bug: 345274915
    Change-Id: I748df668635d77a2d158bd86222aa2fdecfed3eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564969
    Reviewed-by: Cammie Smith Barnes <cammie@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311659}

--- third_party/blink/renderer/modules/shared_storage/shared_storage_worklet.cc
+++ third_party/blink/renderer/modules/shared_storage/shared_storage_worklet.cc
@@ -117,11 +117,15 @@ void SharedStorageWorklet::AddModuleHelper(ScriptState* script_state,
     return;
   }
 
-  if (execution_context->GetSecurityOrigin()->IsOpaque()) {
-    resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
-        script_state->GetIsolate(), DOMExceptionCode::kInvalidAccessError,
-        kOpaqueOriginCheckErrorMessage));
-    return;
+  // In an opaque origin context, addModule() is not allowed, but
+  // createWorklet() is allowed.
+  if (!cross_origin_script_allowed_) {
+    if (execution_context->GetSecurityOrigin()->IsOpaque()) {
+      resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
+          script_state->GetIsolate(), DOMExceptionCode::kInvalidAccessError,
+          kOpaqueOriginCheckErrorMessage));
+      return;
+    }
   }
 
   KURL script_source_url = execution_context->CompleteURL(module_url);
@@ -310,9 +314,10 @@ ScriptPromise<V8SharedStorageResponse> SharedStorageWorklet::selectURL(
     return promise;
   }
 
-  // The opaque origin should have been checked in addModule() or
-  // createWorklet() already.
-  CHECK(!execution_context->GetSecurityOrigin()->IsOpaque());
+  if (!cross_origin_script_allowed_) {
+    // The opaque origin should have been checked in addModule() already.
+    CHECK(!execution_context->GetSecurityOrigin()->IsOpaque());
+  }
 
   if (!IsValidSharedStorageURLsArrayLength(urls.size())) {
     resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
@@ -559,9 +564,10 @@ ScriptPromise<IDLAny> SharedStorageWorklet::run(
     return promise;
   }
 
-  // The opaque origin should have been checked in addModule() or
-  // createWorklet() already.
-  CHECK(!execution_context->GetSecurityOrigin()->IsOpaque());
+  if (!cross_origin_script_allowed_) {
+    // The opaque origin should have been checked in addModule() already.
+    CHECK(!execution_context->GetSecurityOrigin()->IsOpaque());
+  }
 
   if (!keep_alive_after_operation_) {
     resolver->Reject(V8ThrowDOMException::CreateOrEmpty(
