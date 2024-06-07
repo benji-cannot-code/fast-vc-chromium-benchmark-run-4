3b1d1cf650b5bddbb7f13059026301d3de5a340c
Ramin Halavati <rhalavati@chromium.org>
2024-06-07 08:48:52 +0000

    Deprecate kAccessibilityPdfOcrAlwaysActive pref.
    PDF OCR is now always enabled and pref is no longer needed.
    
    Bug: 341856144
    Change-Id: I0ce5865ec5bd7255002578139327bb69aed833cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604489
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311820}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1121,6 +1121,12 @@ inline constexpr char kAccessibilityReadAnythingOmniboxIconLabelShownCount[] =
     "settings.a11y.read_anything.omnibox_icon_label_shown_count";
 #endif
 
+// Deprecated 06/2024.
+#if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
+inline constexpr char kAccessibilityPdfOcrAlwaysActive[] =
+    "settings.a11y.pdf_ocr_always_active";
+#endif
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1624,6 +1630,11 @@ void RegisterProfilePrefsForMigration(
 #if !BUILDFLAG(IS_ANDROID)
   registry->RegisterIntegerPref(
       kAccessibilityReadAnythingOmniboxIconLabelShownCount, 0);
+#endif
+
+// Deprecated 06/2024.
+#if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
+  registry->RegisterBooleanPref(kAccessibilityPdfOcrAlwaysActive, true);
 #endif
 }
 
@@ -2347,7 +2358,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   registry->RegisterBooleanPref(
       prefs::kAccessibilityMainNodeAnnotationsEnabled, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
-  registry->RegisterBooleanPref(prefs::kAccessibilityPdfOcrAlwaysActive, true);
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -3012,6 +3022,11 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
 #if !BUILDFLAG(IS_ANDROID)
   profile_prefs->ClearPref(
       kAccessibilityReadAnythingOmniboxIconLabelShownCount);
+#endif
+
+// Added 06/2024.
+#if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
+  profile_prefs->ClearPref(kAccessibilityPdfOcrAlwaysActive);
 #endif
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
