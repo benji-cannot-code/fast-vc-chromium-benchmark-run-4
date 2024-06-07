2dd4a79ab2fa56d88f4b53c0a1234a26bc68cfb4
Guoxing Zhao <charleszhao@chromium.org>
2024-06-07 02:06:40 +0000

    [VC] disable vc background for enterprise.
    Set the Image button to be visable-but-disabled state and let the enterprise to enable it.
    
    TEST='test on dut'
    
    Change-Id: Ie78a1060f5a1f13565084339bfb337075a3909c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5584567
    Reviewed-by: CJ Huang <chenjih@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Charles Zhao <charleszhao@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311703}

--- ash/session/test_session_controller_client.cc
+++ ash/session/test_session_controller_client.cc
@@ -329,7 +329,7 @@ base::FilePath TestSessionControllerClient::GetProfilePath(
   return base::FilePath("/profile/path").Append(account_id.GetUserEmail());
 }
 
-bool TestSessionControllerClient::IsEligibleForSeaPen(
+std::tuple<bool, bool> TestSessionControllerClient::IsEligibleForSeaPen(
     const AccountId& account_id) {
   return is_eligible_for_background_replace_;
 }
--- ash/session/test_session_controller_client.h
+++ ash/session/test_session_controller_client.h
@@ -148,7 +148,8 @@ class TestSessionControllerClient : public SessionControllerClient {
   PrefService* GetSigninScreenPrefService() override;
   PrefService* GetUserPrefService(const AccountId& account_id) override;
   base::FilePath GetProfilePath(const AccountId& account_id) override;
-  bool IsEligibleForSeaPen(const AccountId& account_id) override;
+  std::tuple<bool, bool> IsEligibleForSeaPen(
+      const AccountId& account_id) override;
   std::optional<int> GetExistingUsersCount() const override;
 
   // By default `LockScreen()` only changes the session state but no UI views
@@ -159,7 +160,7 @@ class TestSessionControllerClient : public SessionControllerClient {
   }
 
   void set_is_eligible_for_background_replace(
-      bool is_eligible_for_background_replace) {
+      const std::tuple<bool, bool>& is_eligible_for_background_replace) {
     is_eligible_for_background_replace_ = is_eligible_for_background_replace;
   }
 
@@ -186,7 +187,7 @@ class TestSessionControllerClient : public SessionControllerClient {
 
   bool is_enterprise_managed_ = false;
 
-  bool is_eligible_for_background_replace_ = true;
+  std::tuple<bool, bool> is_eligible_for_background_replace_ = {true, true};
 
   int existing_users_count_ = 0;
 
