433a92543d275d98ba06836491be68cc42846951
Istvan Nagy <iscsi@google.com>
2024-06-07 16:34:00 +0000

    Add unittest to the auth textfield timer class
    Bug: b:344519626
    Change-Id: If0f8e1366740571786084f59b4d66e37cf3d8341
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598730
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312048}

--- ash/auth/views/auth_textfield_unittest.cc
+++ ash/auth/views/auth_textfield_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "ash/auth/views/test_support/mock_auth_textfield_observer.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/check.h"
@@ -28,19 +29,6 @@ namespace {
 
 constexpr std::u16string kPassword = u"password";
 constexpr std::u16string kPIN = u"123456";
-
-class MockObserver : public AuthTextfield::Observer {
- public:
-  MockObserver() {}
-  ~MockObserver() override = default;
-
-  MOCK_METHOD(void, OnTextfieldBlur, (), (override));
-  MOCK_METHOD(void, OnTextfieldFocus, (), (override));
-  MOCK_METHOD(void, OnContentsChanged, (const std::u16string&), (override));
-  MOCK_METHOD(void, OnTextVisibleChanged, (bool), (override));
-  MOCK_METHOD(void, OnSubmit, (), (override));
-  MOCK_METHOD(void, OnEscape, (), (override));
-};
 
 }  // namespace
 
@@ -67,7 +55,7 @@ class PasswordTextfieldUnitTest : public AshTestBase {
 
     auth_textfield_ = widget_->SetContentsView(
         std::make_unique<AuthTextfield>(AuthTextfield::AuthType::kPassword));
-    mock_observer_ = std::make_unique<MockObserver>();
+    mock_observer_ = std::make_unique<MockAuthTextfieldObserver>();
     auth_textfield_->AddObserver(mock_observer_.get());
   }
 
@@ -80,7 +68,7 @@ class PasswordTextfieldUnitTest : public AshTestBase {
   }
 
   std::unique_ptr<views::Widget> widget_;
-  std::unique_ptr<MockObserver> mock_observer_;
+  std::unique_ptr<MockAuthTextfieldObserver> mock_observer_;
   raw_ptr<AuthTextfield> auth_textfield_;
 };
 
@@ -153,7 +141,7 @@ class PinTextfieldUnitTest : public AshTestBase {
     widget_ = CreateFramelessTestWidget();
     widget_->SetFullscreen(true);
 
-    mock_observer_ = std::make_unique<MockObserver>();
+    mock_observer_ = std::make_unique<MockAuthTextfieldObserver>();
     auth_textfield_ = widget_->SetContentsView(
         std::make_unique<AuthTextfield>(AuthTextfield::AuthType::kPin));
     auth_textfield_->SetText(kPIN);
@@ -170,7 +158,7 @@ class PinTextfieldUnitTest : public AshTestBase {
 
   std::unique_ptr<views::Widget> widget_;
   raw_ptr<AuthTextfield> auth_textfield_;
-  std::unique_ptr<MockObserver> mock_observer_;
+  std::unique_ptr<MockAuthTextfieldObserver> mock_observer_;
 };
 
 // Testing PIN textfield OnContentsChanged Observer.
