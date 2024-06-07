0a196b072f244b1b3b1b382733d1e7b0197d9c4f
Dana Fried <dfried@chromium.org>
2024-06-07 15:58:46 +0000

    [GM2 Cleanup] request_type.cc
    #chrome-refresh-2023-cleanup
    
    Bug: 335903961
    Change-Id: I4a091b9182985dd3f91fd38eff0341883c709aa1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606506
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Ravjit Uppal <ravjit@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312010}

--- components/permissions/request_type.cc
+++ components/permissions/request_type.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/permissions/features.h"
 #include "components/permissions/permission_request.h"
 #include "components/permissions/permissions_client.h"
-#include "ui/base/ui_base_features.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "components/resources/android/theme_resources.h"
@@ -142,35 +141,26 @@ const gfx::VectorIcon& GetIconIdDesktop(RequestType type) {
 }
 
 const gfx::VectorIcon& GetBlockedIconIdDesktop(RequestType type) {
-  const bool cr23 = ::features::IsChromeRefresh2023();
   switch (type) {
     case RequestType::kGeolocation:
-      return cr23 ? vector_icons::kLocationOffChromeRefreshIcon
-                  : vector_icons::kLocationOffIcon;
+      return vector_icons::kLocationOffChromeRefreshIcon;
     case RequestType::kNotifications:
-      return cr23 ? vector_icons::kNotificationsOffChromeRefreshIcon
-                  : vector_icons::kNotificationsOffIcon;
+      return vector_icons::kNotificationsOffChromeRefreshIcon;
     case RequestType::kArSession:
     case RequestType::kVrSession:
-      return cr23 ? vector_icons::kVrHeadsetOffChromeRefreshIcon
-                  : vector_icons::kVrHeadsetOffIcon;
+      return vector_icons::kVrHeadsetOffChromeRefreshIcon;
     case RequestType::kCameraStream:
-      return cr23 ? vector_icons::kVideocamOffChromeRefreshIcon
-                  : vector_icons::kVideocamOffIcon;
+      return vector_icons::kVideocamOffChromeRefreshIcon;
     case RequestType::kCapturedSurfaceControl:
       return vector_icons::kTouchpadMouseOffIcon;
     case RequestType::kClipboard:
-      return cr23 ? vector_icons::kContentPasteOffChromeRefreshIcon
-                  : vector_icons::kContentPasteOffIcon;
+      return vector_icons::kContentPasteOffChromeRefreshIcon;
     case RequestType::kIdleDetection:
-      return cr23 ? vector_icons::kDevicesOffChromeRefreshIcon
-                  : vector_icons::kDevicesOffIcon;
+      return vector_icons::kDevicesOffChromeRefreshIcon;
     case RequestType::kMicStream:
-      return cr23 ? vector_icons::kMicOffChromeRefreshIcon
-                  : vector_icons::kMicOffIcon;
+      return vector_icons::kMicOffChromeRefreshIcon;
     case RequestType::kMidiSysex:
-      return cr23 ? vector_icons::kMidiOffChromeRefreshIcon
-                  : vector_icons::kMidiOffIcon;
+      return vector_icons::kMidiOffChromeRefreshIcon;
     case RequestType::kStorageAccess:
       return vector_icons::kStorageAccessOffIcon;
     case RequestType::kIdentityProvider:
