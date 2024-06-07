d52b2b1b18d05136bc98a7a3a3cd993a6a1f1e86
Fredrik Söderquist <fs@opera.com>
2024-06-07 10:26:22 +0000

    Remove ToPixelSnappedSize(const DeprecatedLayoutSize&, ...)
    It can trivially be replaced by PhysicalRect::PixelSnappedSize(), which
    as a bonus doesn't require any "type casting".
    
    Change-Id: I3e67a069b8348de454b7126490a1bcbd5026c789
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598768
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1311861}

--- third_party/blink/renderer/core/paint/box_reflection_utils.cc
+++ third_party/blink/renderer/core/paint/box_reflection_utils.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/paint/nine_piece_image_painter.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
-#include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/geometry/length_functions.h"
 #include "third_party/blink/renderer/platform/graphics/box_reflection.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
@@ -22,9 +21,9 @@ BoxReflection BoxReflectionForPaintLayer(const PaintLayer& layer,
 
   const LayoutBox* layout_box = layer.GetLayoutBox();
   // TODO(crbug.com/962299): Only correct if the paint offset is correct.
-  gfx::Size frame_size = ToPixelSnappedSize(
-      layout_box->Size().ToLayoutSize(),
-      layout_box->FirstFragment().PaintOffset().ToLayoutPoint());
+  gfx::Size frame_size = PhysicalRect(layout_box->FirstFragment().PaintOffset(),
+                                      layout_box->Size())
+                             .PixelSnappedSize();
   BoxReflection::ReflectionDirection direction =
       BoxReflection::kVerticalReflection;
   float offset = 0;
--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/scroll/smooth_scroll_sequencer.h"
 #include "third_party/blink/renderer/core/view_transition/view_transition.h"
 #include "third_party/blink/renderer/core/view_transition/view_transition_utils.h"
-#include "third_party/blink/renderer/platform/geometry/layout_size.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "ui/base/ui_base_features.h"
@@ -3094,9 +3093,9 @@ gfx::Size PaintLayerScrollableArea::PixelSnappedBorderBoxSize() const {
   // geometry. For now we ensure correct pixel snapping of overflow controls by
   // calling PositionOverflowControls() again when paint offset is updated.
   // TODO(crbug.com/962299): Only correct if the paint offset is correct.
-  return ToPixelSnappedSize(
-      GetLayoutBox()->Size().ToLayoutSize(),
-      GetLayoutBox()->FirstFragment().PaintOffset().ToLayoutPoint());
+  return PhysicalRect(GetLayoutBox()->FirstFragment().PaintOffset(),
+                      GetLayoutBox()->Size())
+      .PixelSnappedSize();
 }
 
 void PaintLayerScrollableArea::DropCompositorScrollDeltaNextCommit() {
