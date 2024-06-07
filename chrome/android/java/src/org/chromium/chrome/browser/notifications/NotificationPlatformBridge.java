5f59e3e5927a332826fc1c5a07681f272da8009f
Balazs Engedy <engedy@chromium.org>
2024-06-07 20:17:42 +0000

    Fix regression with backing up notification images.
    In crrev.com/c/5405074, in the user journey where the user presses the
    "Unsubscribe" button on a notification, the order of backing up
    notification image resources, and replacing the notification with the
    `provisionally unsubsribed` notification has been accidentally reversed.
    This led to image resources not being restored correctly if the user
    chooses to "Undo" the unsubscribe action.
    
    This CL fixes this and adds a regression test.
    
    Bug: 345640570
    Change-Id: I58fc4eb1e1e8d0b066ea142290ecf443ae414b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607790
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312205}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
@@ -1277,9 +1277,11 @@ public class NotificationPlatformBridge {
 
         // TODO(crbug.com/41494401): Verify if we can/need to use the correct profile here.
         NotificationSuspender suspender =
-                new NotificationSuspender(ProfileManager.getLastUsedRegularProfile());
+                new NotificationSuspender(
+                        ProfileManager.getLastUsedRegularProfile(),
+                        ContextUtils.getApplicationContext(),
+                        mNotificationManager);
         mOriginsWithProvisionallyRevokedPermissions.add(identifyingAttributes.origin);
-        displayProvisionallyUnsubscribedNotification(identifyingAttributes);
         suspender.storeNotificationResourcesFromOrigins(
                 Collections.singletonList(Uri.parse(identifyingAttributes.origin)),
                 (notificationIdsToCancel) -> {
@@ -1287,6 +1289,7 @@ public class NotificationPlatformBridge {
                             .recordSuspendedNotificationCountOnUnsubscribe(
                                     notificationIdsToCancel.size());
 
+                    displayProvisionallyUnsubscribedNotification(identifyingAttributes);
                     notificationIdsToCancel.remove(identifyingAttributes.notificationId);
                     suspender.cancelNotificationsWithIds(notificationIdsToCancel);
                 });
