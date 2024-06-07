890c2e86c85e12a1443ee2911a629ffab156625d
Stefan Zager <szager@chromium.org>
2024-06-07 17:30:48 +0000

    Transmit image decode request and response without delay.
    This patch adds the initial plumbing and flags for sending image
    decode requests without delay. It enables kicking off image decodes
    earlier in the pipeline so they can complete in parallel. A followup
    patch will address an issue of double-decodes.
    
    Bug: 330710844
    Change-Id: I8eb31270e09d1f81c7bdf924c682f6ec0f8aad3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5490502
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312099}

--- cc/trees/proxy.h
+++ cc/trees/proxy.h
@@ -91,6 +91,7 @@ class CC_EXPORT Proxy {
   // Must be called before deleting the proxy.
   virtual void Stop() = 0;
 
+  virtual void QueueImageDecode(int request_id, const PaintImage& image) = 0;
   virtual void SetMutator(std::unique_ptr<LayerTreeMutator> mutator) = 0;
 
   virtual void SetPaintWorkletLayerPainter(
