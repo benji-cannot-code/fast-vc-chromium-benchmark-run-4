bc3af9bb8c5e2232c924a0b9dcdd7a539429ccd2
Arthur Eubanks <aeubanks@google.com>
2024-06-07 22:43:33 +0000

    Fix unreachable code detected by future clang roll
    Bug: 343231820
    Change-Id: I020ce28c73b54c565d77b83dda6990fce14b92a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605690
    Auto-Submit: Arthur Eubanks <aeubanks@google.com>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312301}

--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils.cc
@@ -617,6 +617,8 @@ skgpu::graphite::TextureInfo GraphiteBackendTextureInfo(
 #if BUILDFLAG(SKIA_USE_METAL)
     return GraphiteMetalTextureInfo(format, plane_index, is_yuv_plane,
                                     mipmapped);
+#else
+  NOTREACHED_NORETURN();
 #endif
   } else {
     CHECK_EQ(gr_context_type, GrContextType::kGraphiteDawn);
@@ -625,9 +627,10 @@ skgpu::graphite::TextureInfo GraphiteBackendTextureInfo(
         format, readonly, is_yuv_plane, plane_index,
         /*array_slice=*/0, mipmapped, scanout_dcomp_surface,
         supports_multiplanar_rendering, supports_multiplanar_copy);
+#else
+  NOTREACHED_NORETURN();
 #endif
   }
-  NOTREACHED_NORETURN();
 }
 
 skgpu::graphite::TextureInfo GraphitePromiseTextureInfo(
@@ -640,6 +643,8 @@ skgpu::graphite::TextureInfo GraphitePromiseTextureInfo(
 #if BUILDFLAG(SKIA_USE_METAL)
     return GraphiteMetalTextureInfo(format, plane_index,
                                     /*is_yuv_plane=*/false, mipmapped);
+#else
+  NOTREACHED_NORETURN();
 #endif
   } else {
     CHECK_EQ(gr_context_type, GrContextType::kGraphiteDawn);
@@ -696,9 +701,10 @@ skgpu::graphite::TextureInfo GraphitePromiseTextureInfo(
 #endif
 
     return dawn_texture_info;
+#else
+  NOTREACHED_NORETURN();
 #endif
   }
-  NOTREACHED_NORETURN();
 }
 
 #if BUILDFLAG(SKIA_USE_DAWN)
