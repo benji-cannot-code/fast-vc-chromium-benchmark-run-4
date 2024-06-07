9601d61c773db26690eee7590a8df2ef523d2c7f
conniekxu <conniekxu@chromium.org>
2024-06-07 22:37:36 +0000

    CrOS Settings: Add storybook for settings-slider-row
    Screenshot: https://screenshot.googleplex.com/5EdUPXopt66Rc2b
    
    Screen record (slider value updates instantly):
    https://screencast.googleplex.com/cast/Njc0MjE3MjIyODM4NjgxNnw3YjI3MTVjMi1iNg
    
    Bug: b/343270827
    Change-Id: Ia9755b3362a5eed7a88d6cfd3cf22fe2e9e436a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598225
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312299}

--- chrome/browser/resources/ash/settings/internal/storybook/settings_slider_row_storybook.ts
+++ chrome/browser/resources/ash/settings/internal/storybook/settings_slider_row_storybook.ts
@@ -3,14 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import '../../controls/v2/settings_slider_row.js';
+
 import {SliderTick} from 'chrome://resources/ash/common/cr_elements/cr_slider/cr_slider.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {getTemplate} from './settings_slider_v2_storybook.html.js';
+import {getTemplate} from './settings_slider_row_storybook.html.js';
 
-export class SettingsSliderV2Storybook extends PolymerElement {
+export class SettingsSliderRowStorybook extends PolymerElement {
   static get is() {
-    return 'settings-slider-v2-storybook' as const;
+    return 'settings-slider-row-storybook' as const;
   }
 
   static get template() {
@@ -19,19 +21,9 @@ export class SettingsSliderV2Storybook extends PolymerElement {
 
   static get properties() {
     return {
-      sliderValueWithTicks_: {
-        type: Number,
-        value: 5,
-      },
-
       sliderValueWithScale_: {
         type: Number,
         value: 0.5,
-      },
-
-      hideLabel_: {
-        type: Boolean,
-        value: false,
       },
 
       virtualManagedPref_: {
@@ -43,11 +35,6 @@ export class SettingsSliderV2Storybook extends PolymerElement {
           enforcement: chrome.settingsPrivate.Enforcement.ENFORCED,
           controlledBy: chrome.settingsPrivate.ControlledBy.DEVICE_POLICY,
         },
-      },
-
-      disabled_: {
-        type: Boolean,
-        value: false,
       },
 
       ticks_: {
@@ -62,19 +49,30 @@ export class SettingsSliderV2Storybook extends PolymerElement {
           ];
         },
       },
+
+      hideLabel_: {
+        type: Boolean,
+        value: false,
+      },
+
+      updateValueInstantly_: {
+        type: Boolean,
+        value: false,
+      },
+
+      disabled_: {
+        type: Boolean,
+        value: false,
+      },
     };
   }
 
-  private sliderValueWithTicks_: number;
   private sliderValueWithScale_: number;
-  private hideLabel_: boolean;
   private virtualManagedPref_: chrome.settingsPrivate.PrefObject<number>;
   private ticks_: SliderTick[];
+  private hideLabel_: boolean;
+  private updateValueInstantly_: boolean;
   private disabled_: boolean;
-
-  private onTicksSliderChange_(event: CustomEvent<number>): void {
-    this.sliderValueWithTicks_ = event.detail;
-  }
 
   private onScaleSliderChange_(event: CustomEvent<number>): void {
     this.sliderValueWithScale_ = event.detail;
@@ -83,8 +81,9 @@ export class SettingsSliderV2Storybook extends PolymerElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    [SettingsSliderV2Storybook.is]: SettingsSliderV2Storybook;
+    [SettingsSliderRowStorybook.is]: SettingsSliderRowStorybook;
   }
 }
 
-customElements.define(SettingsSliderV2Storybook.is, SettingsSliderV2Storybook);
+customElements.define(
+    SettingsSliderRowStorybook.is, SettingsSliderRowStorybook);
