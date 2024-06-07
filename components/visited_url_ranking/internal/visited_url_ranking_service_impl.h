e57793603f6593dcafdb57834ea059d2f399863b
ssid <ssid@chromium.org>
2024-06-07 21:21:06 +0000

    [tab resume] Wait before recording seen results
    The URL can be activated after seen and if we recorded result as seen,
    then we would never record activated. To make this API easier to use,
    wait for a timeout before recording seen events, so if activated events
    show up, it is recorded.
    
    Change-Id: Ief7dfdee63b790602a03ef7226579ff752a51c52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598230
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312251}

--- components/visited_url_ranking/internal/visited_url_ranking_service_impl.h
+++ components/visited_url_ranking/internal/visited_url_ranking_service_impl.h
@@ -38,6 +38,9 @@ enum class Status;
 // The internal implementation of the VisitedURLRankingService.
 class VisitedURLRankingServiceImpl : public VisitedURLRankingService {
  public:
+  // Wait time before which we record kSeen events as feedback.
+  constexpr static int kSeenRecordDelaySec = 300;
+
   VisitedURLRankingServiceImpl(
       segmentation_platform::SegmentationPlatformService*
           segmentation_platform_service,
@@ -63,6 +66,12 @@ class VisitedURLRankingServiceImpl : public VisitedURLRankingService {
       segmentation_platform::TrainingRequestId visit_request_id) override;
 
  private:
+  // Trigger training data collection with the user action.
+  void TriggerTrainingData(
+      ScoredURLUserAction action,
+      const std::string& visit_id,
+      segmentation_platform::TrainingRequestId visit_request_id);
+
   // Callback invoked when the various fetcher instances have completed.
   void MergeVisitsAndCallback(
       GetURLVisitAggregatesCallback callback,
