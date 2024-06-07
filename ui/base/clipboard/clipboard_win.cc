031e20837815b2ccc800533d87a304e053dbce41
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-06-07 18:33:19 +0000

    Add NotifyClipboardDataChanged call to ClipboardWin
    This CL is required to have `CopyObserver` work in
    `ClipboardHostImpl`. This was the cause of b/343945726 as
    clipboard writes not notifying this observer resulted in
    DLP functionality not working.
    
    The `NotifyClipboardDataChanged()` call has to happen right
    after `CloseClipboard()` so that observers get notified immediately
    after the clipboard data has changed and a new seqno is
    available for it.
    
    Bug: b/343945726
    Change-Id: I945428e9ac79db60bdff744c6de432ef1d0efd38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588381
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312143}

--- ui/base/clipboard/clipboard_win.cc
+++ ui/base/clipboard/clipboard_win.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/base/clipboard/clipboard_constants.h"
 #include "ui/base/clipboard/clipboard_metrics.h"
+#include "ui/base/clipboard/clipboard_monitor.h"
 #include "ui/base/clipboard/clipboard_util.h"
 #include "ui/base/clipboard/clipboard_util_win.h"
 #include "ui/base/clipboard/custom_data_helper.h"
@@ -305,11 +306,17 @@ bool ClipboardWin::IsFormatAvailable(
 
 void ClipboardWin::Clear(ClipboardBuffer buffer) {
   DCHECK_EQ(buffer, ClipboardBuffer::kCopyPaste);
-  ScopedClipboard clipboard;
-  if (!clipboard.Acquire(GetClipboardWindow()))
-    return;
+  {
+    ScopedClipboard clipboard;
+    if (!clipboard.Acquire(GetClipboardWindow())) {
+      return;
+    }
 
-  ::EmptyClipboard();
+    ::EmptyClipboard();
+  }
+  // This call must happen after `clipboard`'s destructor so that observers are
+  // notified after the seqno has changed.
+  ClipboardMonitor::GetInstance()->NotifyClipboardDataChanged();
 }
 
 std::vector<std::u16string> ClipboardWin::GetStandardFormats(
@@ -691,32 +698,39 @@ void ClipboardWin::WritePortableAndPlatformRepresentations(
     std::vector<Clipboard::PlatformRepresentation> platform_representations,
     std::unique_ptr<DataTransferEndpoint> data_src,
     uint32_t privacy_types) {
-  ScopedClipboard clipboard;
-  if (!clipboard.Acquire(GetClipboardWindow()))
-    return;
-  ::EmptyClipboard();
-
-  DispatchPlatformRepresentations(std::move(platform_representations));
-  for (const auto& object : objects)
-    DispatchPortableRepresentation(object.second);
-
-  if (data_src && data_src->IsUrlType()) {
-    HGLOBAL glob = CreateGlobalData(data_src->GetURL()->spec());
-    WriteToClipboard(ClipboardFormatType::InternalSourceUrlType(), glob);
-  }
-  // Write privacy data if there is any.
-  // On Windows, there is no special format to conceal passwords, but
-  // don't save it in the history or cloud clipboard for privacy reasons.
-  if (privacy_types & Clipboard::PrivacyTypes::kNoDisplay) {
-    WriteConfidentialDataForPassword();
-  } else {
-    if (privacy_types & Clipboard::PrivacyTypes::kNoLocalClipboardHistory) {
-      WriteClipboardHistory();
+  {
+    ScopedClipboard clipboard;
+    if (!clipboard.Acquire(GetClipboardWindow())) {
+      return;
     }
-    if (privacy_types & Clipboard::PrivacyTypes::kNoCloudClipboard) {
-      WriteUploadCloudClipboard();
+    ::EmptyClipboard();
+
+    DispatchPlatformRepresentations(std::move(platform_representations));
+    for (const auto& object : objects) {
+      DispatchPortableRepresentation(object.second);
+    }
+
+    if (data_src && data_src->IsUrlType()) {
+      HGLOBAL glob = CreateGlobalData(data_src->GetURL()->spec());
+      WriteToClipboard(ClipboardFormatType::InternalSourceUrlType(), glob);
+    }
+    // Write privacy data if there is any.
+    // On Windows, there is no special format to conceal passwords, but
+    // don't save it in the history or cloud clipboard for privacy reasons.
+    if (privacy_types & Clipboard::PrivacyTypes::kNoDisplay) {
+      WriteConfidentialDataForPassword();
+    } else {
+      if (privacy_types & Clipboard::PrivacyTypes::kNoLocalClipboardHistory) {
+        WriteClipboardHistory();
+      }
+      if (privacy_types & Clipboard::PrivacyTypes::kNoCloudClipboard) {
+        WriteUploadCloudClipboard();
+      }
     }
   }
+  // This call must happen after `clipboard`'s destructor so that observers are
+  // notified after the seqno has changed.
+  ClipboardMonitor::GetInstance()->NotifyClipboardDataChanged();
 }
 
 void ClipboardWin::WriteText(std::string_view text) {
