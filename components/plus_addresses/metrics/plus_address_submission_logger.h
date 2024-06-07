b058aaec61def39a82d2d94429c7b87eea57ffd4
Jan Keitel <jkeitel@google.com>
2024-06-07 16:02:50 +0000

    Add more UMA metrics for plus address submission.
    This CL adds UMAs for some segments of the user population:
    - A segmentation by whether the account is managed.
    - Special UMAs for cases in which the renderer form has only a single
      field (and one further specialization in which the user profile
      is additionally non-managed).
    
    Bug: 343124027
    Change-Id: If093b24b5f95a7094ecf5b5044bcc510c0db325a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604587
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312015}

--- components/plus_addresses/metrics/plus_address_submission_logger.h
+++ components/plus_addresses/metrics/plus_address_submission_logger.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_plus_address_delegate.h"
 #include "components/autofill/core/common/unique_ids.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
+#include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace signin {
 class IdentityManager;
@@ -26,6 +27,9 @@ namespace plus_addresses::metrics {
 
 class PlusAddressSubmissionLogger final : autofill::AutofillManager::Observer {
  public:
+  // The prefix for all UMAs emitted by this class.
+  static constexpr char kUmaSubmissionPrefix[] = "PlusAddresses.Submission";
+
   // A callback used to verify whether a string corresponds to a valid plus
   // address.
   using PlusAddressVerifier = base::RepeatingCallback<bool(const std::string&)>;
@@ -64,12 +68,25 @@ class PlusAddressSubmissionLogger final : autofill::AutofillManager::Observer {
   // are deleted if the form containing the field is submitted or the Autofill
   // manager that belongs to the browser form containing the form field is reset
   // or destroyed.
-  using Record = ukm::builders::PlusAddresses_Submission;
+  struct Record final {
+    Record(ukm::SourceId source_id,
+           bool is_single_field_in_renderer_form,
+           bool is_first_time_user);
+    Record(Record&&);
+    Record& operator=(Record&&);
+    ~Record();
+
+    std::unique_ptr<ukm::builders::PlusAddresses_Submission> ukm_builder;
+    // This information for the following members is contained in the builder
+    // but cannot be read from there. This duplicates it to allow recording a
+    // UMA at submission.
+    bool is_single_field_in_renderer_form = false;
+    bool is_first_time_user = false;
+  };
   // TODO: crbug.com/343124027 - Consider just keeping one map keyed by
   // FieldGlobalId once frame tokens are set on iOS.
-  base::flat_map<
-      autofill::AutofillManager*,
-      base::flat_map<autofill::FieldGlobalId, std::unique_ptr<Record>>>
+  base::flat_map<autofill::AutofillManager*,
+                 base::flat_map<autofill::FieldGlobalId, Record>>
       records_;
 
   // Observations of all managers for which there is an entry in `records_`. The
