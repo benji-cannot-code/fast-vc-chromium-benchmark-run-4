38d5ccdc260f80c61ec55d0ef9442b991c6ab783
dljames <dljames@chromium.org>
2024-06-07 22:24:37 +0000

    [Capture] Remove References of ChromeRefresh2023
    #chrome-refresh-2023-cleanup
    
    Change-Id: I343033b9e88351ed3541f0a548137fbcb3d86fbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608977
    Commit-Queue: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312294}

--- chrome/browser/ui/views/desktop_capture/desktop_media_content_pane_view.cc
+++ chrome/browser/ui/views/desktop_capture/desktop_media_content_pane_view.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 DesktopMediaContentPaneView::DesktopMediaContentPaneView(
     std::unique_ptr<views::View> content_view,
     std::unique_ptr<ShareAudioView> share_audio_view) {
-  float bottom_radius = features::IsChromeRefresh2023() ? 8 : 4;
+  float bottom_radius = 8;
   SetBackground(views::CreateThemedRoundedRectBackground(ui::kColorSysSurface4,
                                                          /*top_radius=*/0,
                                                          bottom_radius));
