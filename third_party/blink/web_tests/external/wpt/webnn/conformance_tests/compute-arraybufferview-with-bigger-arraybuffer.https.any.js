e5cd0259b557fce90552a186a1076592d11e069f
Phillis Tang <phillis@chromium.org>
2024-06-07 20:28:54 +0000

    webnn: add npu virtualtestsuite for Mac and Windows
    Skip the tests for Windows on CQ until we have infra support. But still
    include Windows on VirtualTestSuite to make it easy for local runs.
    
    
    Change-Id: I56a910a0883694f875c82ab88c0cadd218f17c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5583198
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312213}

--- third_party/blink/web_tests/external/wpt/webnn/conformance_tests/compute-arraybufferview-with-bigger-arraybuffer.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/compute-arraybufferview-with-bigger-arraybuffer.https.any.js
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,dedicatedworker
 // META: variant=?cpu
 // META: variant=?gpu
+// META: variant=?npu
 // META: script=../resources/utils.js
 
 'use strict';
--- third_party/blink/web_tests/external/wpt/webnn/resources/utils.js
+++ third_party/blink/web_tests/external/wpt/webnn/resources/utils.js
@@ -20,7 +20,10 @@ const kContextOptionsForVariant = {
   },
   gpu: {
     deviceType: 'gpu',
-  }
+  },
+  npu: {
+    deviceType: 'npu',
+  },
 };
 
 // The maximum index to validate for the output's expected value.
