6c3ab8b215328bf2cae0e0f92326169fde30a7f0
Chad Duffin <chadduffin@chromium.org>
2024-06-07 17:53:38 +0000

    testing: Add API to close system web app
    This change adds an API to InteractiveAshTest that closes the system web
    app matching the provided type if it is open.
    
    Bug: b/338645411
    Change-Id: I01c314ac9f032bdbd582b47c7bfb9b081ddabd84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605958
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312123}

--- chrome/test/base/ash/interactive/interactive_ash_test.cc
+++ chrome/test/base/ash/interactive/interactive_ash_test.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/system_web_apps/system_web_app_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/test/base/chromeos/crosier/aura_window_title_observer.h"
@@ -95,6 +96,14 @@ ui::ElementContext InteractiveAshTest::LaunchSystemWebApp(
                   }));
   CHECK(browser);
   return browser->window()->GetElementContext();
+}
+
+void InteractiveAshTest::CloseSystemWebApp(ash::SystemWebAppType type) {
+  if (Profile* const profile = GetActiveUserProfile()) {
+    if (Browser* const browser = FindSystemWebAppBrowser(profile, type)) {
+      chrome::CloseWindow(browser);
+    }
+  }
 }
 
 ui::test::internal::InteractiveTestPrivate::MultiStep
