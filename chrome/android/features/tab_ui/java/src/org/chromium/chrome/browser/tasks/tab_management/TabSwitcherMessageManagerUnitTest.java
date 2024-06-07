9f920bfed75362797fe99a140fa91f5ed5175bc5
Brandon Wylie <wylieb@chromium.org>
2024-06-07 18:50:16 +0000

    [declutter] Add declutter dialog coordinator/view
    Bug: 340581912
    Change-Id: I0e7fd8e403e3645521a1866c7c9cbf5dc1295d53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605411
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1312156}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManagerUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherMessageManagerUnitTest.java
@@ -15,6 +15,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import android.app.Activity;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
@@ -33,12 +34,14 @@ import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.lifecycle.ActivityLifecycleDispatcher;
 import org.chromium.chrome.browser.multiwindow.MultiWindowModeStateDispatcher;
 import org.chromium.chrome.browser.price_tracking.PriceTrackingFeatures;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.tab.MockTab;
+import org.chromium.chrome.browser.tab_ui.TabContentManager;
 import org.chromium.chrome.browser.tabmodel.TabModel;
 import org.chromium.chrome.browser.tabmodel.TabModelFilter;
 import org.chromium.chrome.browser.tabmodel.TabModelObserver;
@@ -76,6 +79,9 @@ public class TabSwitcherMessageManagerUnitTest {
     @Mock private PriceWelcomeMessageReviewActionProvider mPriceWelcomeMessageReviewActionProvider;
     @Mock private PriceMessageService mPriceMessageService;
     @Mock private MessageUpdateObserver mMessageUpdateObserver;
+    @Mock private BrowserControlsStateProvider mBrowserControlsStateProvider;
+    @Mock private TabContentManager mTabContentManager;
+    @Mock private ViewGroup mRootView;
 
     @Captor private ArgumentCaptor<TabModelObserver> mTabModelObserverCaptor;
 
@@ -121,7 +127,11 @@ public class TabSwitcherMessageManagerUnitTest {
                         mCurrentTabModelFilterSupplier,
                         mMultiWindowModeStateDispatcher,
                         mSnackbarManager,
-                        mModalDialogManager);
+                        mModalDialogManager,
+                        mBrowserControlsStateProvider,
+                        mTabContentManager,
+                        TabListMode.GRID,
+                        mRootView);
         mMessageManager.registerMessages(mTabListCoordinator);
         mMessageManager.bind(
                 mTabListCoordinator, container, mPriceWelcomeMessageReviewActionProvider);
