4632416f683c83b54946519c62fa9bf5b4afbcce
Wes Okuhara <wesokuhara@google.com>
2024-06-07 20:01:22 +0000

    CrOS Settings: Create storybook for settings-dropdown-row
    Also updated the settings-dropdown-v2 storybook similarly.
    Screenshot: http://screen/f36mTDrKPimWRUV.png
    
    Bug: b:345010186
    Test: Manually test UI
    Change-Id: I8e5d8f8239bd229d293de7afa23767e3a0a0cbd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599418
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312193}

--- chrome/browser/resources/ash/settings/internal/storybook/settings_dropdown_row_storybook.ts
+++ chrome/browser/resources/ash/settings/internal/storybook/settings_dropdown_row_storybook.ts
@@ -3,18 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../../controls/v2/settings_dropdown_v2.js';
+import '../../controls/v2/settings_dropdown_row.js';
 import 'chrome://resources/ash/common/cr_elements/cr_checkbox/cr_checkbox.js';
 
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import type {DropdownOptionList} from '../../controls/v2/settings_dropdown_v2.js';
 
-import {getTemplate} from './settings_dropdown_v2_storybook.html.js';
+import {getTemplate} from './settings_dropdown_row_storybook.html.js';
 
-export class SettingsDropdownV2Storybook extends PolymerElement {
+export class SettingsDropdownRowStorybook extends PolymerElement {
   static get is() {
-    return 'settings-dropdown-v2-storybook' as const;
+    return 'settings-dropdown-row-storybook' as const;
   }
 
   static get template() {
@@ -70,9 +70,9 @@ export class SettingsDropdownV2Storybook extends PolymerElement {
 
 declare global {
   interface HTMLElementTagNameMap {
-    [SettingsDropdownV2Storybook.is]: SettingsDropdownV2Storybook;
+    [SettingsDropdownRowStorybook.is]: SettingsDropdownRowStorybook;
   }
 }
 
 customElements.define(
-    SettingsDropdownV2Storybook.is, SettingsDropdownV2Storybook);
+    SettingsDropdownRowStorybook.is, SettingsDropdownRowStorybook);
