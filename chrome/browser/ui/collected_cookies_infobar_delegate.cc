7d888a96f7b311a61c84a826e34cf687d9add472
Dana Fried <dfried@chromium.org>
2024-06-07 00:41:56 +0000

    [GM2 Cleanup] collected_cookies_infobar_delegate.cc
    #chrome-refresh-2023-cleanup
    
    Change-Id: I47c5362cb9035e492a34b3594b8a19f13e3f1a62
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605389
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311664}

--- chrome/browser/ui/collected_cookies_infobar_delegate.cc
+++ chrome/browser/ui/collected_cookies_infobar_delegate.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/ui_base_features.h"
 
 // static
 void CollectedCookiesInfoBarDelegate::Create(
@@ -24,8 +23,7 @@ void CollectedCookiesInfoBarDelegate::Create(
           new CollectedCookiesInfoBarDelegate())));
 }
 
-CollectedCookiesInfoBarDelegate::CollectedCookiesInfoBarDelegate()
-    : ConfirmInfoBarDelegate() {}
+CollectedCookiesInfoBarDelegate::CollectedCookiesInfoBarDelegate() = default;
 
 CollectedCookiesInfoBarDelegate::~CollectedCookiesInfoBarDelegate() {}
 
@@ -35,9 +33,7 @@ CollectedCookiesInfoBarDelegate::GetIdentifier() const {
 }
 
 const gfx::VectorIcon& CollectedCookiesInfoBarDelegate::GetVectorIcon() const {
-  return features::IsChromeRefresh2023()
-             ? vector_icons::kSettingsChromeRefreshIcon
-             : vector_icons::kSettingsIcon;
+  return vector_icons::kSettingsChromeRefreshIcon;
 }
 
 std::u16string CollectedCookiesInfoBarDelegate::GetMessageText() const {
