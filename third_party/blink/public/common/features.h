218e7909f25b700192023bbe162399bd735147dc
Nidhi Jaju <nidhijaju@chromium.org>
2024-06-07 02:00:56 +0000

    Optimize loading data URLs in general
    This CL adds some early returns and skips certain functions in
    BaseFetchContext::CanRequestInternal for data URLs, which is called
    when preparing the request in ResourceFetcher::RequestResource.
    
    Bug: 41496436
    Change-Id: I1198ab3206eddb08640ed97eb6a4afdf4621a2c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599801
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311702}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -1737,6 +1737,9 @@ BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kNoThrottlingVisibleAgent);
 // https://github.com/WICG/turtledove/blob/main/FLEDGE.md#23-scoring-bids
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kRenderSizeInScoreAdBrowserSignals);
 
+// Optimize loading data: URLs.
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kOptimizeLoadingDataUrls);
+
 }  // namespace features
 }  // namespace blink
 
