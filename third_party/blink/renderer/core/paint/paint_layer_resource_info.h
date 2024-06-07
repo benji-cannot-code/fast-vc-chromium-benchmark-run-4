84b5ea67cde59e07d249eedd29974dba82bbba33
Fredrik Söderquist <fs@opera.com>
2024-06-07 12:03:18 +0000

    Specify a viewport for resolving userSpaceOnUse filterUnits
    For non-SVG content, SVGViewportResolver won't be able to produce a
    viewport, but will return 0x0 which turn means that every part of the
    filter region that is percentages computes to zero. Instead allow
    setting up a viewport in FilterEffectBuilder and pass that along when
    resolving the filter region. For HTML elements use the border box size
    as the viewport (matches Gecko) and for <canvas> use the canvas
    dimensions.
    
    Guard behind the flag SvgFilterUserSpaceViewportForNonSvg.
    
    Fixed: 41189886
    Change-Id: Ib385340bdc4d30d6d17c447fb99d6db3bc66e8b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594220
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1311889}

--- third_party/blink/renderer/core/paint/paint_layer_resource_info.h
+++ third_party/blink/renderer/core/paint/paint_layer_resource_info.h
@@ -63,6 +63,12 @@ class PaintLayerResourceInfo final
   void SetFilterReferenceBox(const gfx::RectF& rect) {
     filter_reference_box_ = rect;
   }
+  const std::optional<gfx::SizeF>& FilterViewport() const {
+    return filter_viewport_;
+  }
+  void SetFilterViewport(std::optional<gfx::SizeF> viewport) {
+    filter_viewport_ = viewport;
+  }
 
   void ClearLayer() { layer_ = nullptr; }
 
@@ -74,6 +80,7 @@ class PaintLayerResourceInfo final
   // |ClearLayer| must be called before *layer_ becomes invalid.
   Member<PaintLayer> layer_;
   gfx::RectF filter_reference_box_;
+  std::optional<gfx::SizeF> filter_viewport_;
 };
 
 }  // namespace blink
