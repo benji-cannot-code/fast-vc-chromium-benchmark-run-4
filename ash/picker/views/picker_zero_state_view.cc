699a2289b74589fe4ea13ccdc340ee4f20889304
Takumi Fujimoto <takumif@chromium.org>
2024-06-07 06:25:07 +0000

    Revert "Prevent async zero state results from causing double pseudo focus."
    This reverts commit 3708f342b7ab43792f4d85100ae025d965f6b87b.
    
    Reason for revert:
    Suspected to be causing a compile failure:
    https://cr-buildbucket.appspot.com/build/8745811988292965569 
    
    Original change's description:
    > Prevent async zero state results from causing double pseudo focus.
    >
    > Currently, each Picker pseudo focus handler (e.g. PickerZeroStateView
    > and PickerEmojiBarView) separately tracks its own pseudo focused view,
    > without considering if other views are also handling pseudo focus.
    >
    > For PickerZeroStateView, there are some cases where an async result can
    > arrive and appear as pseudo focused even after the user has moved pseudo
    > focus off the Picker main container (e.g. press "up" to navigate to
    > emoji bar). This can make both the zero state and emoji bar appear
    > pseudo focused, even if key events are actually only being handled by
    > the emoji bar.
    >
    > Ideally, we should refactor pseudo focus so that it can be tracked and
    > checked "centrally" across Picker views. In the meantime though, prevent
    > async zero state results from appearing as pseudo focused if the zero
    > state page had explicitly already lost pseudo focus.
    >
    > Bug: b:340692819, b:345609546
    > Change-Id: If381f9cb7b3d194093784f3e251dfddb9129aa2a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607005
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Michelle Chen <michellegc@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1311768}
    
    Bug: b:340692819, b:345609546
    Change-Id: I8bab25b9887f2a349958e3bfce70a5027caf4714
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606585
    Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1311769}

--- ash/picker/views/picker_zero_state_view.cc
+++ ash/picker/views/picker_zero_state_view.cc
@@ -277,15 +277,6 @@ void PickerZeroStateView::ScrollPseudoFocusedViewToVisible() {
   }
 }
 
-void PickerZeroStateView::MovePseudoFocusToTopIfNeeded() {
-  // TODO: b/345609546 - It would be better to centrally track and check pseudo
-  // focus across all Picker views. As is, we can run into situations where
-  // multiple views all claim to have a `pseudo_focused_view_`.
-  if (pseudo_focused_view_ != nullptr) {
-    SetPseudoFocusedView(section_list_view_->GetTopItem());
-  }
-}
-
 void PickerZeroStateView::OnFetchRecentResults(
     std::vector<PickerSearchResult> results) {
   if (results.empty()) {
@@ -306,7 +297,7 @@ void PickerZeroStateView::OnFetchRecentResults(
       list_item_view->SetBadgeAction(delegate_->GetActionForResult(result));
     }
   }
-  MovePseudoFocusToTopIfNeeded();
+  SetPseudoFocusedView(section_list_view_->GetTopItem());
 }
 
 void PickerZeroStateView::OnFetchZeroStateEditorResults(
@@ -335,7 +326,7 @@ void PickerZeroStateView::OnFetchZeroStateEditorResults(
     section_view->AddListItem(std::move(item_view));
   }
   section_view->SetVisible(true);
-  MovePseudoFocusToTopIfNeeded();
+  SetPseudoFocusedView(section_list_view_->GetTopItem());
 }
 
 BEGIN_METADATA(PickerZeroStateView)
