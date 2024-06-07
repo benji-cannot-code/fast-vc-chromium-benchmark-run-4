a5423414c00cf4c9a363e245975c04638cb10c2e
Wolfgang Billenstein <bwolfgang@google.com>
2024-06-07 06:38:12 +0000

    [Autofill] Add UMA Autofill.ProfileImport.{New, Update}ProfileDecision2.{UserGroup}
    Adds UMA metrics that record the user's decision for adding a new
    profile and updating an existing profile. The new metrics replace
    Autofill.ProfileImport.{New, Update}ProfileDecision. The added benefits
    are variants that record the import decision for different user groups.
    
    OBSOLETE_HISTOGRAM[Autofill.ProfileImport.NewProfileDecision]=Replaced by Autofill.ProfileImport.NewProfileDecision2.
    OBSOLETE_HISTOGRAM[Autofill.ProfileImport.UpdateProfileDecision]=Replaced by Autofill.ProfileImport.UpdateProfileDecision2.
    
    Bug: 345453389
    Change-Id: I34e1bb4689d23af7d5950d50c5ce46015783ab19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5588627
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311775}

--- components/autofill/core/browser/metrics/profile_deduplication_metrics.h
+++ components/autofill/core/browser/metrics/profile_deduplication_metrics.h
@@ -13,6 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill::autofill_metrics {
 
+// Given the result of `CalculateMinimalIncompatibleTypeSets()`, returns the
+// minimum number of fields whose removal makes `import_candidate` a duplicate
+// of any entry in `existing_profiles`. Returns
+// `std::numeric_limits<int>::max()` in case `min_incompatible_sets` is empty.
+int GetDuplicationRank(base::span<const FieldTypeSet> min_incompatible_sets);
+
 // Logs various metrics around quasi duplicates (= profiles that are duplicates
 // except for a small number of types) for the `profiles` a user has stored at
 // browser startup.
