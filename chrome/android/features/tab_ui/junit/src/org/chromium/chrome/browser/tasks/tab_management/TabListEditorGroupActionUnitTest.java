ec02e7c3b0b3d879d6d1b7fe7240012b2c67cd24
Calder Kitagawa <ckitagawa@chromium.org>
2024-06-07 15:57:49 +0000

    [Tab Group Parity] Explicitly show dialog for groups of size 1
    We missed the case of using the bulk editor and grouping a single tab
    should show the dialog.
    
    This was overlooked in:
    https://chromium-review.googlesource.com/c/chromium/src/+/5604931
    
    Address this by adding the functionality and tests.
    
    Bug: 338660531
    Change-Id: I8f3fab74ed533491328d8b52c29d9e7045885901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608254
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312009}

--- chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorGroupActionUnitTest.java
+++ chrome/android/features/tab_ui/junit/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorGroupActionUnitTest.java
@@ -151,6 +151,7 @@ public class TabListEditorGroupActionUnitTest {
 
     @Test
     @SmallTest
+    @EnableFeatures(ChromeFeatureList.TAB_GROUP_PARITY_ANDROID)
     public void testSingleTabToGroup() {
         configure(false);
         List<Integer> tabIds = new ArrayList<>();
@@ -171,6 +172,7 @@ public class TabListEditorGroupActionUnitTest {
 
         Assert.assertTrue(mAction.perform());
         verify(mGroupFilter).createSingleTabGroup(tab, true);
+        verify(mTabGroupCreationDialogManager).showDialog(tab.getRootId(), mGroupFilter);
 
         tab.setTabGroupId(new Token(1L, 2L));
         when(mGroupFilter.isTabInTabGroup(tab)).thenReturn(true);
