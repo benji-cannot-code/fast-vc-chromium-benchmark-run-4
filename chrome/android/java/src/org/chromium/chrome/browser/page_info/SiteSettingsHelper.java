d76f15f8a268bb11a5338375e7587fd9a6b80df3
Shu Yang <shuyng@google.com>
2024-06-07 07:28:34 +0000

    Update page info for pdf pages
    Show a dedicated string for pdf native pages.
    
    Bug: 339511978
    Change-Id: I4dfbfc3ba44cdf870c0db6e66276dcc1bf98d7bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5585393
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Shu Yang <shuyng@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311794}

--- chrome/android/java/src/org/chromium/chrome/browser/page_info/SiteSettingsHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/page_info/SiteSettingsHelper.java
@@ -11,6 +11,8 @@ import android.os.Bundle;
 
 import org.chromium.chrome.browser.offlinepages.OfflinePageUtils;
 import org.chromium.chrome.browser.settings.SettingsLauncherImpl;
+import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabUtils;
 import org.chromium.components.browser_ui.settings.SettingsLauncher;
 import org.chromium.components.browser_ui.site_settings.ContentSettingsResources;
 import org.chromium.components.browser_ui.site_settings.SingleCategorySettings;
@@ -31,6 +33,8 @@ public class SiteSettingsHelper {
      * @param webContents The WebContents for which to check the site settings.
      */
     public static boolean isSiteSettingsAvailable(WebContents webContents) {
+        Tab tab = TabUtils.fromWebContents(webContents);
+        boolean isPdfPage = tab != null && tab.isNativePage() && tab.getNativePage().isPdf();
         boolean isOfflinePage = OfflinePageUtils.getOfflinePage(webContents) != null;
         // TODO(crbug.com/40663204): dedupe the
         // DomDistillerUrlUtils#getOriginalUrlFromDistillerUrl()
@@ -40,7 +44,7 @@ public class SiteSettingsHelper {
                         ? DomDistillerUrlUtils.getOriginalUrlFromDistillerUrl(
                                 webContents.getVisibleUrl())
                         : null;
-        return !isOfflinePage && url != null && UrlUtilities.isHttpOrHttps(url);
+        return !isPdfPage && !isOfflinePage && url != null && UrlUtilities.isHttpOrHttps(url);
     }
 
     /** Show the single category settings page for given category and type. */
