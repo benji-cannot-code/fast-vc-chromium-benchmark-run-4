f995dfc02c515ed7d09bacefec38c48a1afbe06e
Aaron Leventhal <aleventhal@google.com>
2024-06-07 22:02:44 +0000

    [A11y] Add debug info for missing parent during tree update
    Bug: 337378101
    Change-Id: Ifc8eff2654b8b88044363c1aa972025defbcf821
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605552
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312281}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -731,9 +731,7 @@ class MODULES_EXPORT AXObjectCacheImpl
 
     virtual ~TreeUpdateParams() = default;
     void Trace(Visitor* visitor) const { visitor->Trace(node); }
-#if DCHECK_IS_ON()
     std::string ToString();
-#endif
   };
 
  protected:
