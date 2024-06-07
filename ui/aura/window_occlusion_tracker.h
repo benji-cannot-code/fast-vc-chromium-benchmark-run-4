00cb2c76a253435c5ab88dc3567f1a45d9811b0e
Eric Sum <esum@google.com>
2024-06-07 21:03:07 +0000

    Allow WindowOcclusionTracker to be destroyed mid-animation.
    This applies to transient (i.e. secondary) WindowOcclusionTracker
    instances since the primary tracker usually outlives animations.
    There's a use-after-free crash if the tracker is destroyed
    mid-animation because it's not removing itself as a
    LayerAnimationObserver in its destructor.
    
    The fix for this then led to a number of other changes that were
    required to get all test cases to pass. So the CL became a broader
    effort to make the occlusion tracker more robust to any destruction
    order between the window, layer, animator, and occlusion tracker.
    
    Bug: b:294094124
    Change-Id: If00cbe64dd99d4512e3fe14342df8bc49b55587c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598394
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312242}

--- ui/aura/window_occlusion_tracker.h
+++ ui/aura/window_occlusion_tracker.h
@@ -19,7 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_observer.h"
 #include "ui/aura/window_tree_host_observer.h"
+#include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_observer.h"
+#include "ui/compositor/layer_animator.h"
+#include "ui/compositor/layer_observer.h"
 
 struct SkIRect;
 
@@ -48,6 +51,7 @@ class WindowOcclusionChangeBuilder;
 // Note that an occluded window may be drawn on the screen by window switching
 // features such as "Alt-Tab" or "Overview".
 class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
+                                           public ui::LayerObserver,
                                            public WindowObserver,
                                            public WindowTreeHostObserver {
  public:
@@ -352,6 +356,10 @@ class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
   void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override;
   void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override;
   void OnLayerAnimationScheduled(ui::LayerAnimationSequence* sequence) override;
+  bool RequiresNotificationWhenAnimatorDestroyed() const override;
+
+  // ui::LayerObserver:
+  void LayerDestroyed(ui::Layer* layer) override;
 
   // WindowObserver:
   void OnWindowHierarchyChanged(const HierarchyChangeParams& params) override;
@@ -381,6 +389,8 @@ class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
   void OnOcclusionStateChanged(WindowTreeHost* host,
                                Window::OcclusionState new_state,
                                const SkRegion& occluded_region) override;
+
+  void RemoveAnimationObservationForLayer(ui::Layer* layer);
 
   // Windows whose occlusion data is tracked.
   base::flat_map<Window*, OcclusionData> tracked_windows_;
@@ -420,6 +430,11 @@ class AURA_EXPORT WindowOcclusionTracker : public ui::LayerAnimationObserver,
       window_observations_{this};
   base::ScopedMultiSourceObservation<WindowTreeHost, WindowTreeHostObserver>
       window_tree_host_observations_{this};
+  base::ScopedMultiSourceObservation<ui::LayerAnimator,
+                                     ui::LayerAnimationObserver>
+      layer_animator_observations{this};
+  base::ScopedMultiSourceObservation<ui::Layer, ui::LayerObserver>
+      animated_layer_observations_{this};
 
   // Optional factory to create occlusion change builder.
   OcclusionChangeBuilderFactory occlusion_change_builder_factory_;
