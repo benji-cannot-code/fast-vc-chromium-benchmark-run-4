dc81dca9b5a576e897c46400adcc29cbdece2b0c
weidongliu <weidongliu@chromium.org>
2024-06-07 16:48:26 +0000

    views: delete some GetHeightForWidth overloads in //ui/views.
    Bug: 40232718
    Change-Id: I195e4b4edcb5fb4a223292d965a89a994fbd9c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5591189
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312059}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -368,10 +368,6 @@ gfx::Size LabelButton::GetMinimumSize() const {
   return size;
 }
 
-int LabelButton::GetHeightForWidth(int width) const {
-  return CalculatePreferredSize(SizeBounds(width, {})).height();
-}
-
 ProposedLayout LabelButton::CalculateProposedLayout(
     const SizeBounds& size_bounds) const {
   ProposedLayout layouts;
