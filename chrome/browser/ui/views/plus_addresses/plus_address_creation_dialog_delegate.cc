37a7d3d50fe5402255b4ba11fbf936d074e83fd2
Timofey Chudakov <tchudakov@google.com>
2024-06-07 08:05:50 +0000

    [Desktop] Use monospaced font for the proposed plus address.
    Before: https://screenshot.googleplex.com/3UKh2Z6qhdtGvsp
    After: https://screenshot.googleplex.com/7iK4Nr5fucqLq4A
    
    Bug: 342330801
    Change-Id: Ie057d625bbc78822e8a1e942d904fbd7ee64c5dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604544
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1311808}

--- chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.cc
+++ chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/plus_addresses/plus_address_creation_controller_desktop.h"
 #include "chrome/browser/ui/plus_addresses/plus_address_creation_view.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
+#include "chrome/browser/ui/views/chrome_typography.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/constrained_window/constrained_window_views.h"
 #include "components/plus_addresses/features.h"
@@ -284,6 +285,10 @@ PlusAddressCreationDialogDelegate::PlusAddressCreationDialogDelegate(
           .SetTextContext(views::style::CONTEXT_DIALOG_BODY_TEXT)
           .SetTextStyle(views::style::STYLE_PRIMARY)
           .Build());
+  if (base::FeatureList::IsEnabled(features::kPlusAddressUIRedesign)) {
+    plus_address_label_->SetTextContext(views::style::CONTEXT_LABEL);
+    plus_address_label_->SetTextStyle(STYLE_SECONDARY_MONOSPACED);
+  }
   plus_address_label_->SetProperty(views::kElementIdentifierKey,
                                    kPlusAddressSuggestedEmailElementId);
   plus_address_label_->SetSelectable(true);
