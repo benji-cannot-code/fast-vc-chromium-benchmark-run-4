10cf0e712f6981d2c3eda66c9ec12886f72504c8
François Degros <fdegros@chromium.org>
2024-06-07 03:22:27 +0000

    EmptyTrashIOTask::Cancel() marks the task as cancelled
    Even though the underlying deletion operations cannot be cancelled,
    marking the whole task as cancelled avoids some UI glitches.
    
    BUG=b:343105316, b:331535030
    TEST=unit_tests --gtest_filter=EmptyTrashIOTaskTest.*
    
    Change-Id: I397efa9be7f09e083d950ff1e3aa4092b399d37f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606944
    Commit-Queue: Ben Reich <benreich@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Reviewed-by: Ben Reich <benreich@chromium.org>
    Commit-Queue: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1311732}

--- chrome/browser/ash/file_manager/io_task.cc
+++ chrome/browser/ash/file_manager/io_task.cc
@@ -18,6 +18,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace file_manager::io_task {
 
+std::ostream& operator<<(std::ostream& out, const State state) {
+  switch (state) {
+#define PRINT(s)    \
+  case State::k##s: \
+    return out << #s;
+    PRINT(Queued)
+    PRINT(Scanning)
+    PRINT(InProgress)
+    PRINT(Paused)
+    PRINT(Success)
+    PRINT(Error)
+    PRINT(NeedPassword)
+    PRINT(Cancelled)
+#undef PRINT
+  }
+
+  return out << "State(" << static_cast<std::underlying_type_t<State>>(state)
+             << ")";
+}
+
 std::ostream& operator<<(std::ostream& out, OperationType op) {
   switch (op) {
 #define PRINT(s)            \
