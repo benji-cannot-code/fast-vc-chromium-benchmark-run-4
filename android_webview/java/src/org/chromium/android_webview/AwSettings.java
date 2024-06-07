d29e3c6517e0336e0269cfb77faed8a723ad696e
Jiacheng Guo <gjc@google.com>
2024-06-07 22:50:01 +0000

    Fix unintended WebView BFCache flush on new pages
    The CL fixes the unintended BFCache flush when a new page is
    created. When a new RFH is set, the `updateEverythingLocked` function will be called. We should not flush the BFCache in the function call.
    The testBFCacheWithMultiplePages has been updated to use the AwSettings to enable the BFCache since the BFCache will be only flushed when enabled in settings during `updateEverything`.
    
    Bug: 345633228
    Change-Id: I34e452d890df199e9c7e95b5a95cd8e092838b59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606548
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312305}

--- android_webview/java/src/org/chromium/android_webview/AwSettings.java
+++ android_webview/java/src/org/chromium/android_webview/AwSettings.java
@@ -485,7 +485,8 @@ public class AwSettings {
         assert mNativeAwSettings != 0;
         AwSettingsJni.get().updateEverythingLocked(mNativeAwSettings, AwSettings.this);
         onGestureZoomSupportChanged(supportsDoubleTapZoomLocked(), supportsMultiTouchZoomLocked());
-        setRequestedWithHeaderOriginAllowListLocked(mRequestedWithHeaderAllowedOriginRules);
+        setRequestedWithHeaderOriginAllowListLocked(
+                mRequestedWithHeaderAllowedOriginRules, /* flushBackForwardCache= */ false);
     }
 
     /** See {@link android.webkit.WebSettings#setBlockNetworkLoads}. */
@@ -1306,11 +1307,13 @@ public class AwSettings {
                 allowedOriginRules != null ? allowedOriginRules : Collections.emptySet();
         AwWebContentsMetricsRecorder.recordRequestedWithHeaderModeAPIUsage(allowedOriginRules);
         synchronized (mAwSettingsLock) {
-            setRequestedWithHeaderOriginAllowListLocked(allowedOriginRules);
+            setRequestedWithHeaderOriginAllowListLocked(
+                    allowedOriginRules, /* flushBackForwardCache= */ true);
         }
     }
 
-    private void setRequestedWithHeaderOriginAllowListLocked(final Set<String> allowedOriginRules) {
+    private void setRequestedWithHeaderOriginAllowListLocked(
+            final Set<String> allowedOriginRules, boolean flushBackForwardCache) {
         assert Thread.holdsLock(mAwSettingsLock);
         if (mNativeAwSettings == 0) {
             return;
@@ -1321,7 +1324,9 @@ public class AwSettings {
 
         mEventHandler.runOnUiThreadBlockingAndLocked(
                 () -> {
-                    flushBackForwardCache();
+                    if (flushBackForwardCache) {
+                        flushBackForwardCache();
+                    }
                     String[] rejected =
                             AwSettingsJni.get()
                                     .updateXRequestedWithAllowListOriginMatcher(
