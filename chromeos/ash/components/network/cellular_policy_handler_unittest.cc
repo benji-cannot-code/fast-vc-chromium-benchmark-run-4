479ca8ac853fd605897d5cd762044700f6587321
Jason Zhang <jiajunz@google.com>
2024-06-07 20:14:11 +0000

    [CrOS Cellular] Add back refresh profile before policy profile install
    Adds back the refresh profile list before trigger eSIM policy
    installation, removing this step could result in an incomplete list of
    eSIM profiles being shown in the UI.
    
    Bug: b/343745579
    Change-Id: I9cd4ca15ccf2f06170839e86d9c7f48cf1a7436d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5586253
    Reviewed-by: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312202}

--- chromeos/ash/components/network/cellular_policy_handler_unittest.cc
+++ chromeos/ash/components/network/cellular_policy_handler_unittest.cc
@@ -215,6 +215,8 @@ class CellularPolicyHandlerTest : public testing::Test {
   void InstallProfile(const base::Value::Dict& onc_config) {
     cellular_policy_handler()->InstallESim(onc_config);
     base::RunLoop().RunUntilIdle();
+
+    FastForwardRefreshDelay();
   }
 
   HermesProfileClient::Properties* FindProfileProperties(
@@ -466,6 +468,8 @@ TEST_F(CellularPolicyHandlerTest, InstallSuccess_SMDP) {
 
   CompleteShillServiceAutoConnect(*onc_config);
 
+  EXPECT_EQ(InhibitReason::kRefreshingProfileList,
+            cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kRequestingAvailableProfiles,
             cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kInstallingProfile,
@@ -527,6 +531,8 @@ TEST_F(CellularPolicyHandlerTest, InstallSuccess_SMDS) {
 
   CompleteShillServiceAutoConnect(*onc_config);
 
+  EXPECT_EQ(InhibitReason::kRefreshingProfileList,
+            cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kRequestingAvailableProfiles,
             cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kInstallingProfile,
@@ -563,6 +569,10 @@ TEST_F(CellularPolicyHandlerTest, InstallSuccess_DespiteHermesErrors) {
       chromeos::onc::ReadDictionaryFromJson(
           GenerateCellularPolicy(activation_code));
   ASSERT_TRUE(onc_config.has_value());
+
+  // Queue a success result for the call to refresh the profile list.
+  HermesEuiccClient::Get()->GetTestInterface()->QueueHermesErrorStatus(
+      HermesResponseStatus::kSuccess);
 
   // Queue a failure result for the SM-DS scan itself.
   HermesEuiccClient::Get()->GetTestInterface()->QueueHermesErrorStatus(
@@ -610,6 +620,8 @@ TEST_F(CellularPolicyHandlerTest, InstalledButFailedToEnable) {
   CellularInhibitorObserver cellular_inhibitor_observer;
   InstallProfile(*onc_config);
 
+  EXPECT_EQ(InhibitReason::kRefreshingProfileList,
+            cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kRequestingAvailableProfiles,
             cellular_inhibitor_observer.PopInhibitReason());
   EXPECT_EQ(InhibitReason::kInstallingProfile,
