31b79c03b76a4bdad470e1765b15a5043456c4fb
Clark DuVall <cduvall@chromium.org>
2024-06-08 00:13:43 +0000

    Add validation checking for on-device model
    This adds the ability to run simple sanity checks after downloading the
    on-device model. There are finch params to control a lot of the behavior
    to allow us to block sessions on the checks or just collect data.
    
    Bug: 345002352
    Change-Id: Ifbb71c416b946713b2b9fe13f2e420ef4d61a325
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598287
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1312340}

--- components/optimization_guide/core/model_execution/on_device_model_access_controller.h
+++ components/optimization_guide/core/model_execution/on_device_model_access_controller.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_ON_DEVICE_MODEL_ACCESS_CONTROLLER_H_
 
 #include "base/memory/raw_ref.h"
+#include "base/values.h"
+#include "components/optimization_guide/core/optimization_guide_enums.h"
 
 class PrefService;
 
@@ -38,7 +40,19 @@ class OnDeviceModelAccessController {
   // Called if the session times out.
   void OnSessionTimedOut();
 
+  bool ShouldValidateModel(std::string_view model_version);
+  void OnValidationFinished(OnDeviceModelValidationResult result);
+
  private:
+  struct ValidationState {
+    std::string component_version;
+    OnDeviceModelValidationResult result =
+        OnDeviceModelValidationResult::kUnknown;
+    int attempt_count = 0;
+  };
+  ValidationState GetValidationState() const;
+  void SetValidationState(const ValidationState& state);
+
   raw_ref<PrefService, LeakedDanglingUntriaged> pref_service_;
   bool is_gpu_blocked_ = false;
 };
