4cf6f7df2c2944847262877f68d910a393539f74
Balazs Engedy <engedy@chromium.org>
2024-06-08 09:07:23 +0000

    Add param to control broadcast/service PRE_UNSUBSCRIBE intent.
    This CL partially reverts crrev.com/c/5561063, with the result that the
    One Tap Unsubscribe feature will once again use broadcast intents by
    default for PRE_UNSUBSCRIBE actions.
    
    However, we also introduce a FeatureParam "use_service_intent" that
    can be used to use a service-type intent instead using field trials.
    
    Bug: 324827395
    Change-Id: I8449610b394adbf4a6320b74c3f3e895f016ed75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605773
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312421}

--- chrome/browser/browser_features.h
+++ chrome/browser/browser_features.h
@@ -104,6 +104,8 @@ BASE_DECLARE_FEATURE(kNoPreReadMainDll);
 
 #if BUILDFLAG(IS_ANDROID)
 BASE_DECLARE_FEATURE(kNotificationOneTapUnsubscribe);
+extern base::FeatureParam<bool>
+    kNotificationOneTapUnsubscribeUseServiceIntentParam;
 #endif
 
 BASE_DECLARE_FEATURE(kOmniboxTriggerForPrerender2);
