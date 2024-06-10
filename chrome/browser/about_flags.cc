58e538b65196afb76957cea6a6b66a19d5a83dfe
Katie Dektar <katie@chromium.org>
2024-06-10 14:23:08 +0000

    [A11y] Add flag for feature to magnify magnifier confirmation dialog
    Feature will be added separately.
    
    Bug: b/335728537
    AX-Relnotes: N/A
    Change-Id: If5ce573868a77e94a5d2f339531878ec49229b22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608612
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Katie Dektar <katie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312748}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -8026,6 +8026,12 @@ const FeatureEntry kFeatureEntries[] = {
      kOsCrOS,
      FEATURE_VALUE_TYPE(features::kAccessibilityCaretBlinkIntervalSetting)},
 
+    {"enable-accessibility-magnify-accelerator-dialog",
+     flag_descriptions::kAccessibilityMagnifyAcceleratorDialogName,
+     flag_descriptions::kAccessibilityMagnifyAcceleratorDialogDescription,
+     kOsCrOS,
+     FEATURE_VALUE_TYPE(features::kAccessibilityMagnifyAcceleratorDialog)},
+
     {"enable-accessibility-overscroll-setting",
      flag_descriptions::kAccessibilityOverscrollSettingFeatureName,
      flag_descriptions::kAccessibilityOverscrollSettingFeatureDescription,
--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1157,6 +1157,12 @@ const char kAccessibilityMagnifierFollowsStsDescription[] =
     "This option enables the fullscreen magnifier to follow Select to Speak's "
     "reading focus.";
 
+const char kAccessibilityMagnifyAcceleratorDialogName[] =
+    "Magnifier turns on with accelerator dialog";
+const char kAccessibilityMagnifyAcceleratorDialogDescription[] =
+    "This option enables the fullscreen magnifier to turn on as soon as the "
+    "accelerator dialog is shown.";
+
 const char kAccessibilityMouseKeysName[] = "Mouse Keys";
 const char kAccessibilityMouseKeysDescription[] =
     "This option enables you to control the mouse with the keyboard.";
