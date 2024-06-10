c2777e9746d45c3f21cfa87de25cb04428b6a94d
Patrick Monette <pmonette@chromium.org>
2024-06-10 03:36:10 +0000

    [PM] Use NodeSetView throughout performance_manager/
    This change removes all the Visit*Nodes() methods that were used to
    efficiently iterate over the public view of containers holding
    a private impl pointer.
    
    They are no longer needed because the Get*Nodes() are converted to
    return NodeSetView, which avoids the needless copies they used to
    do.
    
    Change-Id: I6dfb138ca78ff86ec0d7afab85253d7369cea661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600125
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312595}

--- components/performance_manager/public/graph/page_node.h
+++ components/performance_manager/public/graph/page_node.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/flat_set.h"
-#include "base/functional/function_ref.h"
 #include "base/observer_list_types.h"
 #include "components/performance_manager/public/graph/node.h"
+#include "components/performance_manager/public/graph/node_set_view.h"
 #include "components/performance_manager/public/mojom/coordination_unit.mojom.h"
 #include "components/performance_manager/public/mojom/lifecycle.mojom.h"
 #include "components/performance_manager/public/resource_attribution/page_context.h"
@@ -45,7 +45,10 @@ enum class PageType {
 // Extensions.
 class PageNode : public TypedNode<PageNode> {
  public:
-  using FrameNodeVisitor = base::FunctionRef<bool(const FrameNode*)>;
+  using NodeSet = base::flat_set<const Node*>;
+  template <class NodeViewPtr>
+  using NodeSetView = NodeSetView<NodeSet, NodeViewPtr>;
+
   using LifecycleState = mojom::LifecycleState;
   using Observer = PageNodeObserver;
   class ObserverDefaultImpl;
@@ -208,17 +211,9 @@ class PageNode : public TypedNode<PageNode> {
   // are no main frames at the moment, returns nullptr.
   virtual const FrameNode* GetMainFrameNode() const = 0;
 
-  // Visits the main frame nodes associated with this page. The iteration is
-  // halted if the visitor returns false. Returns true if every call to the
-  // visitor returned true, false otherwise.
-  virtual bool VisitMainFrameNodes(const FrameNodeVisitor& visitor) const = 0;
-
   // Returns all of the main frame nodes, both current and otherwise. If there
-  // are no main frames at the moment, returns the empty set. Note that this
-  // incurs a full container copy of all main frame nodes. Please use
-  // VisitMainFrameNodes when that makes sense.
-  virtual const base::flat_set<raw_ptr<const FrameNode, CtnExperimental>>
-  GetMainFrameNodes() const = 0;
+  // are no main frames at the moment, returns the empty set.
+  virtual NodeSetView<const FrameNode*> GetMainFrameNodes() const = 0;
 
   // Returns the URL the main frame last committed a navigation to, or the
   // initial URL of the page before navigation. The latter case is distinguished
--- components/performance_manager/public/graph/process_node.h
+++ components/performance_manager/public/graph/process_node.h
@@ -8,11 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/enum_set.h"
 #include "base/containers/flat_set.h"
-#include "base/functional/function_ref.h"
 #include "base/observer_list_types.h"
 #include "base/process/process.h"
 #include "base/task/task_traits.h"
 #include "components/performance_manager/public/graph/node.h"
+#include "components/performance_manager/public/graph/node_set_view.h"
 #include "components/performance_manager/public/render_process_host_id.h"
 #include "components/performance_manager/public/resource_attribution/process_context.h"
 #include "content/public/common/process_type.h"
@@ -44,8 +44,10 @@ class BrowserChildProcessHostProxy;
 // it.
 class ProcessNode : public TypedNode<ProcessNode> {
  public:
-  using FrameNodeVisitor = base::FunctionRef<bool(const FrameNode*)>;
-  using WorkerNodeVisitor = base::FunctionRef<bool(const WorkerNode*)>;
+  using NodeSet = base::flat_set<const Node*>;
+  template <class ReturnType>
+  using NodeSetView = NodeSetView<NodeSet, ReturnType>;
+
   using Observer = ProcessNodeObserver;
   class ObserverDefaultImpl;
 
@@ -109,23 +111,11 @@ class ProcessNode : public TypedNode<ProcessNode> {
   // metrics as specified in content::ChildProcessData during process creation.
   virtual const std::string& GetMetricsName() const = 0;
 
-  // Visits the frame nodes that are hosted in this process. The iteration is
-  // halted if the visitor returns false. Returns true if every call to the
-  // visitor returned true, false otherwise.
-  virtual bool VisitFrameNodes(const FrameNodeVisitor& visitor) const = 0;
+  // Returns the set of frame nodes that are hosted in this process.
+  virtual NodeSetView<const FrameNode*> GetFrameNodes() const = 0;
 
-  // Visits the worker nodes that are hosted in this process. The iteration is
-  // halted if the visitor returns false. Returns true if every call to the
-  // visitor returned true, false otherwise.
-  virtual bool VisitWorkerNodes(const WorkerNodeVisitor& visitor) const = 0;
-
-  // Returns the set of frame nodes that are hosted in this process. Note that
-  // calling this causes the set of nodes to be generated.
-  virtual base::flat_set<const FrameNode*> GetFrameNodes() const = 0;
-
-  // Returns the set of worker nodes that are hosted in this process. Note that
-  // calling this causes the set of nodes to be generated.
-  virtual base::flat_set<const WorkerNode*> GetWorkerNodes() const = 0;
+  // Returns the set of worker nodes that are hosted in this process.
+  virtual NodeSetView<const WorkerNode*> GetWorkerNodes() const = 0;
 
   // Returns true if the main thread task load is low (below some threshold
   // of usage). See ProcessNodeObserver::OnMainThreadTaskLoadIsLow.
