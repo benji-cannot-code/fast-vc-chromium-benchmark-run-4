1112dfa3ddbf8c0b367bd11e91ccade0af74de58
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2024-06-10 20:52:11 +0000

    [CrOS Bluetooth] Add device status notification logs
    This change adds Bluetooth logs to device status notifier to capture
    when a notification is being created for Bluetooth device state
    changes. This is to help debug issues where the notification is
    not being shown.
    
    Bug: b/345240570, b/339459005
    Test: N/A
    Change-Id: I3c64450a9333724328474827916ffc1a35d21f9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5617313
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Reviewed-by: Gordon Seto <gordonseto@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313036}

--- ash/system/bluetooth/bluetooth_device_status_ui_handler.cc
+++ ash/system/bluetooth/bluetooth_device_status_ui_handler.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "chromeos/ash/services/bluetooth_config/public/cpp/cros_bluetooth_config_util.h"
+#include "components/device_event_log/device_event_log.h"
 #include "device/bluetooth/chromeos/bluetooth_utils.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -43,6 +44,8 @@ BluetoothDeviceStatusUiHandler::~BluetoothDeviceStatusUiHandler() = default;
 
 void BluetoothDeviceStatusUiHandler::OnDevicePaired(
     PairedBluetoothDevicePropertiesPtr device) {
+  BLUETOOTH_LOG(EVENT) << "Notifying device was paired: "
+                       << device->device_properties->id;
   ash::ToastData toast_data(
       /*id=*/GetToastId(device.get()),
       ash::ToastCatalogName::kBluetoothDevicePaired,
@@ -57,6 +60,9 @@ void BluetoothDeviceStatusUiHandler::OnDevicePaired(
 
 void BluetoothDeviceStatusUiHandler::OnDeviceDisconnected(
     PairedBluetoothDevicePropertiesPtr device) {
+  BLUETOOTH_LOG(EVENT) << "Notifying device was disconnected: "
+                       << device->device_properties->id;
+
   ash::ToastData toast_data(
       /*id=*/GetToastId(device.get()),
       ash::ToastCatalogName::kBluetoothDeviceDisconnected,
@@ -71,6 +77,9 @@ void BluetoothDeviceStatusUiHandler::OnDeviceDisconnected(
 
 void BluetoothDeviceStatusUiHandler::OnDeviceConnected(
     PairedBluetoothDevicePropertiesPtr device) {
+  BLUETOOTH_LOG(EVENT) << "Notifying device was connected: "
+                       << device->device_properties->id;
+
   ash::ToastData toast_data(
       /*id=*/GetToastId(device.get()),
       ash::ToastCatalogName::kBluetoothDeviceConnected,
