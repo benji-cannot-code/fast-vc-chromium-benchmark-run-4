ce4a997e27b252828b14b28c62c9eb30a7c8f9a5
Lei Zhang <thestig@chromium.org>
2024-06-10 23:13:10 +0000

    Clean up crx_file deps in c/b/web_applications
    - Remove unnecessary crx_file includes and GN deps.
    - Remove a TODO that implies crx_file deps can be removed altogether,
      as it is still in use in GenerateAppIdFromManifestId().
    - Simplify some GN logic.
    
    Bug: 40775647
    Change-Id: Ibfe2a861f5844152a4cecadcc4f452ec5f260ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618031
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313116}

--- chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
+++ chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
@@ -65,7 +65,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #import "chrome/common/mac/app_mode_common.h"
 #include "chrome/grit/chrome_unscaled_resources.h"
-#include "components/crx_file/id_util.h"
 #include "components/variations/net/variations_command_line.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_task_traits.h"
--- chrome/browser/web_applications/policy/web_app_policy_manager_unittest.cc
+++ chrome/browser/web_applications/policy/web_app_policy_manager_unittest.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
-#include "components/crx_file/id_util.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/webapps/browser/install_result_code.h"
 #include "components/webapps/common/web_app_id.h"
