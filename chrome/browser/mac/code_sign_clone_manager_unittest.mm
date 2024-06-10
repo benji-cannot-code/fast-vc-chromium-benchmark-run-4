d00ae58c1c5999ed8715be276d42bc1e524e3773
Tom Burgin <bur@google.com>
2024-06-10 15:33:57 +0000

    code sign safe updates: clean up clone only if necessary
    Bug: 343924839
    Change-Id: I22ad193edeed042b3a29648e95f8a43368a5508a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5589169
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Tom Burgin <bur@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312797}

--- chrome/browser/mac/code_sign_clone_manager_unittest.mm
+++ chrome/browser/mac/code_sign_clone_manager_unittest.mm
@@ -108,6 +108,8 @@ TEST(CodeSignCloneManagerTest, Clone) {
   EXPECT_EQ(src_stat.st_ino, tmp_stat.st_ino);
   EXPECT_GT(src_stat.st_nlink, 1);
 
+  EXPECT_TRUE(clone_manager.get_needs_cleanup_for_testing());
+
   // The clone is typically cleaned up by a helper child process when
   // `clone_manager` goes out of scope. In the test environment that does not
   // happen so manually clean up.
@@ -141,6 +143,7 @@ TEST(CodeSignCloneManagerTest, InvalidDirhelperPath) {
 
   // Ensure there was a failure.
   EXPECT_TRUE(tmp_app_path.empty());
+  EXPECT_FALSE(clone_manager.get_needs_cleanup_for_testing());
 
   CodeSignCloneManager::ClearDirhelperPathForTesting();
 }
@@ -169,6 +172,7 @@ TEST(CodeSignCloneManagerTest, FailedHardLink) {
 
   // Ensure there was a failure.
   EXPECT_TRUE(tmp_app_path.empty());
+  EXPECT_FALSE(clone_manager.get_needs_cleanup_for_testing());
 }
 
 TEST(CodeSignCloneManagerTest, FailedClone) {
@@ -198,6 +202,7 @@ TEST(CodeSignCloneManagerTest, FailedClone) {
 
   // Ensure there was a failure.
   EXPECT_TRUE(tmp_app_path.empty());
+  EXPECT_FALSE(clone_manager.get_needs_cleanup_for_testing());
 }
 
 TEST(CodeSignCloneManagerTest, ChromeCodeSignCloneCleanupMain) {
