b8c479f9879a098faef360ccd3ea79b98dda21d3
Tomasz Wiszkowski <ender@google.com>
2024-06-10 22:12:12 +0000

    Add Lint rules to keep TemplateURL Parameter Validation in sync.
    Note: upstream validation not possible across repositories. This
    works here because it's not really enforced.
    
    Bug: 344025279
    Change-Id: Ic908befc605751786482e6064d8de2b5b0d1230e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600665
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313079}

--- components/search_engines/template_url_unittest.cc
+++ components/search_engines/template_url_unittest.cc
@@ -363,7 +363,7 @@ TEST_F(TemplateURLTest, ParsePlayStoreDefinitions) {
   //
   // Extracted from search_engine_chrome_metadata using:
   // sed -n 's/[^{]*\({[^}]*}\)[^{]*/"\1",\n/gp' | sort | uniq
-  // TODO(b/344023531): Add LINT rules linking to upstream tests.
+  // LINT.IfChange
   std::set<std::string> recognized_params{{
       "{google:RLZ}",
       "{google:assistedQueryStats}",
@@ -403,6 +403,7 @@ TEST_F(TemplateURLTest, ParsePlayStoreDefinitions) {
       "{searchTerms}",
       "{yandex:searchPath}",
   }};
+  // LINT.ThenChange(//googledata/experiments/play/features/gateway/http/setupandupdate/tests/chrome_template_url_parameters_validations.gcl)
 
   // Basic confirmation check; if this fails, it's possible the logic below
   // needs updating.
