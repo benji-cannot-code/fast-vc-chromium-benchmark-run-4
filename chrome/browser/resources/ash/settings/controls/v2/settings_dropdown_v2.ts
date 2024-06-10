dcddbaad4d5555d6ae3dcc240119a896083290c5
conniekxu <conniekxu@chromium.org>
2024-06-10 18:00:54 +0000

    CrOS Settings: Remove shared properties for settings control components
    Bug: b/345829675
    Change-Id: I64842b65484c8681ab218556f011b5d88116e678
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608377
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312907}

--- chrome/browser/resources/ash/settings/controls/v2/settings_dropdown_v2.ts
+++ chrome/browser/resources/ash/settings/controls/v2/settings_dropdown_v2.ts
@@ -32,7 +32,6 @@ import 'chrome://resources/ash/common/cr_elements/cros_color_overrides.css.js';
 import 'chrome://resources/ash/common/cr_elements/md_select.css.js';
 import 'chrome://resources/ash/common/cr_elements/policy/cr_policy_pref_indicator.js';
 
-import {PolymerElementProperties} from 'chrome://resources/polymer/v3_0/polymer/interfaces.js';
 import {microTask, PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {assertExists} from '../../assert_extras.js';
@@ -70,11 +69,7 @@ export class SettingsDropdownV2Element extends SettingsDropdownV2ElementBase {
     return getTemplate();
   }
 
-  /**
-   * Properties shared publicly with other elements that may encapsulate this
-   * element internally (e.g. settings-dropdown-row).
-   */
-  static get sharedProperties(): PolymerElementProperties {
+  static get properties() {
     return {
       /**
        * List of options for the dropdown.
@@ -97,12 +92,6 @@ export class SettingsDropdownV2Element extends SettingsDropdownV2ElementBase {
         type: String,
         value: undefined,
       },
-    };
-  }
-
-  static get properties() {
-    return {
-      ...this.sharedProperties,
 
       // A11y properties added since they are data-bound in HTML.
       ariaLabel: {
