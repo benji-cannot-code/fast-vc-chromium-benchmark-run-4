172d31ad72fa3e9d9b0a871840d06f49d71139ad
Lukasz Anforowicz <lukasza@chromium.org>
2024-06-10 20:37:36 +0000

    `span`-ification: `mojo::DataPipe[Consumer|Producer]Handle`: New API.
    This CL `span`-ifies the APIs in `mojo/public/cpp/system/data_pipe.h`:
    
    * `BeginReadData` and `BeginWriteData` return (via an out parameter)
      a `span` instead of returning a ptr+size pair.
    * `ReadData` and `WriteData` consume a `span` instead of consuming
      a ptr+size pair.
    
    This CL adds the new APIs + coverts callers under `//mojo`.
    
    For more details see the design note here:
    https://docs.google.com/document/d/1c4NKpXwpQ9MKK1SbJ4C6MvhXI8-KJZ4jq7N4VHTHJoI/edit?usp=sharing
    
    Bug: 40284755
    Change-Id: I70a7fab6e57eb3481305e82dd504de91042fa881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598233
    Auto-Submit: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313022}

--- mojo/public/cpp/system/data_pipe_producer.cc
+++ mojo/public/cpp/system/data_pipe_producer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/location.h"
@@ -109,10 +110,6 @@ class DataPipeProducer::SequenceState
 
   void TransferSomeBytes() {
     while (true) {
-      // Lock as much of the pipe as we can.
-      void* pipe_buffer;
-      size_t size = kDefaultMaxReadSize;
-
       DCHECK_LE(bytes_transferred_, data_source_->GetLength());
       const uint64_t max_data_size =
           data_source_->GetLength() - bytes_transferred_;
@@ -122,11 +119,14 @@ class DataPipeProducer::SequenceState
         return;
       }
 
-      if (static_cast<uint64_t>(size) > max_data_size)
-        size = static_cast<size_t>(max_data_size);
+      size_t size_hint = kDefaultMaxReadSize;
+      if (static_cast<uint64_t>(size_hint) > max_data_size) {
+        size_hint = static_cast<size_t>(max_data_size);
+      }
 
+      base::span<uint8_t> pipe_buffer;
       MojoResult mojo_result = producer_handle_->BeginWriteData(
-          &pipe_buffer, &size, MOJO_WRITE_DATA_FLAG_NONE);
+          size_hint, MOJO_WRITE_DATA_FLAG_NONE, pipe_buffer);
       if (mojo_result == MOJO_RESULT_SHOULD_WAIT)
         return;
       if (mojo_result != MOJO_RESULT_OK) {
@@ -134,10 +134,8 @@ class DataPipeProducer::SequenceState
         Finish(mojo_result);
         return;
       }
-      base::span<char> read_buffer(static_cast<char*>(pipe_buffer), size);
-
-      DataSource::ReadResult result =
-          data_source_->Read(bytes_transferred_, read_buffer);
+      DataSource::ReadResult result = data_source_->Read(
+          bytes_transferred_, base::as_writable_chars(pipe_buffer));
       producer_handle_->EndWriteData(result.bytes_read);
       // result.bytes_read == 0 is used to determine if the read operation did
       // not retrieve any bytes, which typically occurs when reaching the end of
