70783a474eda505a51765333d8d1f11a213dece2
Saifuddin Hitawala <hitawala@chromium.org>
2024-06-10 17:39:04 +0000

    [media] Remove MultiPlaneVideoCaptureSharedImages feature
    Multiplanar SI with hardware video decode is fully launched in M125
    and we can remove the legacy codepath for hardware. This change removes
    the MultiPlaneVideoCaptureSharedImages feature which is along the
    hardware decode path and cleans up per plane sampling for the new path.
    
    This can be done because MultiPlaneVideoCaptureSharedImages was used
    with legacy multiplanar for getting BufferPlanes and setting
    SharedImageFormatType. It is only enabled for Mac/Windows which do not
    use external sampler. Thus setting they type can be done with
    buildflags now and BufferPlanes are removed with legacy multiplanar
    path. Thus this change cleans up the feature.
    
    Also remove some dead includes.
    
    Bug: 332564976
    Change-Id: I717cd551a07dda843359dd73f60f304cfc9acbb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598448
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312894}

--- media/capture/video/video_capture_effects_processor.cc
+++ media/capture/video/video_capture_effects_processor.cc
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/shared_image_interface.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "gpu/command_buffer/common/sync_token.h"
-#include "gpu/ipc/common/gpu_memory_buffer_support.h"
-#include "media/base/media_switches.h"
 #include "media/base/video_types.h"
 #include "media/capture/video/video_capture_gpu_channel_host.h"
 #include "ui/gl/gl_bindings.h"
