db1e7f5bdfa1fe444fd1813270b90f5be4f7372e
Kevin McNee <mcnee@chromium.org>
2024-06-10 17:07:45 +0000

    Add test for focusing a WebView inside an AppView
    We add a test for the scenario described in the associated bug,
    where a Chrome App embeds an AppView which embeds a WebView, and we
    confirm that the embedders can focus the WebView content so that it can
    receive keyboard input without previous user input.
    
    Bug: b/336345688
    Change-Id: I3d0949274a0c0546b67352c3ffe7d4a9f0536d3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5609413
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312872}

--- chrome/test/data/extensions/platform_apps/app_view/shim/main.js
+++ chrome/test/data/extensions/platform_apps/app_view/shim/main.js
@@ -216,6 +216,19 @@ function testCloseWithPendingEmbedRequest(appToEmbed) {
   embedder.test.succeed();
 };
 
+function testFocusWebViewInAppView(appToEmbed) {
+  let appview = new AppView();
+  appview.style.border = 'solid';
+  document.body.appendChild(appview);
+  appview.connect(
+      appToEmbed, {'runWebViewInAppViewFocusTest': true}, (success) => {
+        embedder.test.assertTrue(success);
+        appview.focus();
+        // The test continues on the C++ side.
+        embedder.test.succeed();
+      });
+};
+
 embedder.test.testList = {
   'testAppViewWithUndefinedDataShouldSucceed':
       testAppViewWithUndefinedDataShouldSucceed,
@@ -226,6 +239,7 @@ embedder.test.testList = {
       testAppViewConnectFollowingPreviousConnect,
   'testAppViewEmbedSelfShouldFail': testAppViewEmbedSelfShouldFail,
   'testCloseWithPendingEmbedRequest': testCloseWithPendingEmbedRequest,
+  'testFocusWebViewInAppView': testFocusWebViewInAppView,
 };
 
 onload = function() {
---
+++ chrome/test/data/extensions/platform_apps/app_view/shim/skeleton/web_view_focus_test.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var dataUrl = `data:text/html,
+<body>
+  <p>Input:</p>
+  <input type="text" id="text_id">
+  <script>
+    var text_input = document.getElementById('text_id');
+    text_input.focus();
+
+    var textInputPromise = new Promise((resolve) => {
+      text_input.addEventListener('input', resolve);
+    });
+
+    async function waitForInput() {
+      await textInputPromise;
+    }
+  </script>
+</body>
+`;
+
+var webview = document.createElement('webview');
+webview.style.border = 'solid';
+webview.src = dataUrl;
+webview.addEventListener('loadstop', () => {
+  webview.focus();
+});
+document.body.appendChild(webview);
