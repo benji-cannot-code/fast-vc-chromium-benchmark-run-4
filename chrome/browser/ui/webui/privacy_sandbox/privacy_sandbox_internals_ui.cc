707287a785f28c78f946e159198be589b68dc415
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-06-10 14:47:30 +0000

    Add page handler and resources for Related Website Sets DevUI
    Add WebUI controller and reference to resources directory for
    related-website-sets.
    Add routing for chrome://privacy-sandbox-internals/related-website-sets
    
    Bug: 330877132
    Change-Id: I4d35273570a68d79ea5cf9d08a6c229039533b65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593094
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312764}

--- chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.cc
+++ chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.cc
@@ -3,6 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_ui.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+#include "base/feature_list.h"
+#include "components/privacy_sandbox/privacy_sandbox_features.h"
+#endif
+
 #include "base/json/json_writer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/privacy_sandbox/privacy_sandbox_internals_handler.h"
@@ -28,6 +34,13 @@ PrivacySandboxInternalsUI::PrivacySandboxInternalsUI(content::WebUI* web_ui)
       base::make_span(kPrivacySandboxInternalsResources,
                       kPrivacySandboxInternalsResourcesSize),
       IDR_PRIVACY_SANDBOX_INTERNALS_INDEX_HTML);
+
+#if !BUILDFLAG(IS_ANDROID)
+  if (base::FeatureList::IsEnabled(privacy_sandbox::kRelatedWebsiteSetsDevUI)) {
+    source->AddResourcePath("related-website-sets",
+                            IDR_RELATED_WEBSITE_SETS_RELATED_WEBSITE_SETS_HTML);
+  }
+#endif
 }
 
 PrivacySandboxInternalsUI::~PrivacySandboxInternalsUI() {}
