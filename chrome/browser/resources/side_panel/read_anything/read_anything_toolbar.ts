4a2ce0ef4171418985096242c7d3051e39f79490
Yu Zhong <yuzhong@google.com>
2024-06-10 16:05:24 +0000

    [Read Aloud] Fix toast contrast
    Show the language downloaded toast in front of language menu dialog
    when it's open so it does not get partially blocked
    by the dialog scrim.
    
    This is done by adding a cr-toast element inside the dialog and show that one when the dialog is open.
    
    https://screenshot.googleplex.com/6pxGi6Uv8ymYkjq
    
    Bug: 343966559
    Change-Id: I9dec7bc2a23553c28bce72ec14104c67cd9dcd99
    Fixed: 343966559
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607344
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312820}

--- chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything_toolbar.ts
@@ -151,6 +151,7 @@ export class ReadAnythingToolbarElement extends ReadAnythingToolbarElementBase {
       localeToDisplayName: Object,
       previewVoicePlaying: Object,
       areFontsLoaded_: Boolean,
+      lastDownloadedLang: String,
     };
   }
 
