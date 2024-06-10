78bf969f1ffa57959806290d1aa215738a8b58e2
Jérôme Lebel <jlebel@chromium.org>
2024-06-10 10:25:22 +0000

    [iOS] Use PrepopulatedEngine in search engine settings tests
    This patch replaces constant strings by PrepopulatedEngine values.
    This updates SearchEngineSettingsChoiceScreenTestCase and
    SearchEngineSettingsTestCase.
    
    Change-Id: I892c950693db3cd0b82f0014fb58231da10eef9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600883
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312671}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_earl_grey_ui_test_util.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_earl_grey_ui_test_util.mm
@@ -118,6 +118,13 @@ GREYElementInteraction* GetInteractionForSearchEngineSettingButton() {
                                kSearchEngineTableViewControllerId)];
 }
 
++ (GREYElementInteraction*)interactionForSettingsWithPrepopulatedSearchEngine:
+    (const TemplateURLPrepopulateData::PrepopulatedEngine&)prepopulatedEngine {
+  NSString* name =
+      [self searchEngineNameWithPrepopulatedEngine:prepopulatedEngine];
+  return [self interactionForSettingsSearchEngineWithName:name];
+}
+
 + (NSString*)searchEngineNameWithPrepopulatedEngine:
     (const TemplateURLPrepopulateData::PrepopulatedEngine&)prepopulatedEngine {
   return base::SysUTF16ToNSString(prepopulatedEngine.name);
