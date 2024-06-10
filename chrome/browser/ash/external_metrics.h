44daf8b7cd0aee6a2a82cb3365a570608e233196
Hirthanan Subenderan <hirthanan@google.com>
2024-06-10 18:45:10 +0000

    Process files in the early uma events directory.
    Process UMA events that were recorded externally before the
    stateful partition is mounted. The initial goal is to write to use
    crash reporter to write to this directory when passed the FLAGS_early,
    in order to collect early crash metrics.
    
    This can be expanded in the future to capture other metrics
    that occur before the stateful partition is mounted.
    
    See http://go/crash-reporter-dropping-early-uma-metrics
    
    BUG=330641099
    
    Change-Id: Ie13c4b3b990ff39e5749ad59ff6c43e6a3a315a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494567
    Reviewed-by: Ian Barkley-Yeung <iby@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hirthanan Subenderan <hirthanan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312948}

--- chrome/browser/ash/external_metrics.h
+++ chrome/browser/ash/external_metrics.h
@@ -32,6 +32,11 @@ class ExternalMetrics : public base::RefCountedThreadSafe<ExternalMetrics> {
   // metrics are read.
   static constexpr char kEventsDirectoryPath[] =
       "/var/lib/metrics/uma-events.d";
+  // This directory holds per-pid event files. These files contain metrics
+  // collected before the stateful partition is mounted. This is read
+  // by externally-reported metrics when the crash reporter is invoked with the
+  // FLAGS_early flag to capture early boot metrics.
+  static constexpr char kUmaEarlyMetricsDirectoryPath[] = "/run/early-metrics/";
 
   ExternalMetrics();
 
@@ -50,7 +55,8 @@ class ExternalMetrics : public base::RefCountedThreadSafe<ExternalMetrics> {
   // testing purpose.
   static scoped_refptr<ExternalMetrics> CreateForTesting(
       const std::string& filename,
-      const std::string& dir);
+      const std::string& uma_events_dir,
+      const std::string& uma_early_metrics_dir);
 
   // Synchronously executes one collection run for testing purposes. Does not
   // schedule any followup runs.
@@ -61,10 +67,13 @@ class ExternalMetrics : public base::RefCountedThreadSafe<ExternalMetrics> {
   friend class ExternalMetricsTest;
 
   FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest, CustomInterval);
+  FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest, ProcessSamplesSuccessfully);
   FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest, HandleMissingFile);
   FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest, CanReceiveHistogram);
   FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest,
                            CanReceiveHistogramFromPidFiles);
+  FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest,
+                           CanReceiveMetricsFromEarlyMetricsDir);
   FRIEND_TEST_ALL_PREFIXES(ExternalMetricsTest,
                            IncorrectHistogramsAreDiscarded);
 
@@ -119,6 +128,10 @@ class ExternalMetrics : public base::RefCountedThreadSafe<ExternalMetrics> {
   // Directory of per-pid event files used by libmetrics to send metrics to
   // Chrome.
   std::string uma_events_dir_;
+
+  // Directory of per-pid event files before stateful partition is mounted.
+  // Used by libmetrics to send metrics to Chrome.
+  std::string uma_early_metrics_dir_;
 
   // Interval between metrics being read from our sources.
   base::TimeDelta collection_interval_;
