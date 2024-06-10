8ae1d58e27118c473a6e73c129d277220f06e623
François Doray <fdoray@chromium.org>
2024-06-10 20:07:19 +0000

    Remove quick-intensive-throttling-after-loading flag.
    This CL removes the about:flags entry to control "quick intensive wake
    up throttling after loading". Note that the feature is still part of
    a combined performance experiment, so we're not removing the
    base::Feature yet.
    
    Fixed: 40266090
    Change-Id: Icb11621b3a7b3ea51ce9e94a371d190c9cd3b721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615232
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312999}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1774,9 +1774,6 @@ extern const char kQuickAppAccessTestUIDescription[];
 extern const char kQuickDimName[];
 extern const char kQuickDimDescription[];
 
-extern const char kQuickIntensiveWakeUpThrottlingAfterLoadingName[];
-extern const char kQuickIntensiveWakeUpThrottlingAfterLoadingDescription[];
-
 extern const char kQuickDeleteForAndroidName[];
 extern const char kQuickDeleteForAndroidDescription[];
 
