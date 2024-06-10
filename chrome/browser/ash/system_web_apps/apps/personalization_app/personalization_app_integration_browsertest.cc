67a06644eef90e4104b5fe86205f25c0f1ff6c68
Jerry Liu <pzliu@google.com>
2024-06-10 18:19:56 +0000

    p13n: move fullscreen preview impl to wallpaper controller client
    Move the implementation to wallpaper controller client so that it can
    be called from both wallpaper provider and sea pen provider. This
    change will allow us to enable fullscreen preview for SeaPen wallpaper.
    
    BUG=b:324247447
    TEST=unit_tests --gtest_filter="*Wallpaper*"
    
    Change-Id: I1da957c0890fd679a0c1cf269ef3ef03a04b1456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606209
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jerry Liu <pzliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312923}

--- chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_integration_browsertest.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_launch_params.h"
 #include "chrome/browser/ash/system_web_apps/test_support/system_web_app_integration_test.h"
 #include "chrome/browser/ui/browser_list.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "components/viz/common/frame_timing_details.h"
