327c4eb5b6e12296cf6259f92561a579bfacaa8e
Joshua Hood <jdh@chromium.org>
2024-06-10 17:26:09 +0000

    [3PCD] Add OT observer for origin trial for 3PCD
    This CL adds a OriginTrialsControllerDelegate::Observer to listen to
    status changes for "LimitThirdPartyCookies" origin trial and
    create/remove TOP_LEVEL_TPCD_ORIGIN_TRIAL content settings accordingly.
    
    Bug: 332707126
    Change-Id: I3380c297d57724acadb271110cc7b01686fa691d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5606981
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joshua Hood <jdh@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1312881}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -190,6 +190,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/tpcd/experiment/eligibility_service_factory.h"
 #include "chrome/browser/tpcd/heuristics/opener_heuristic_service_factory.h"
+#include "chrome/browser/tpcd/support/origin_trial_service_factory.h"
 #include "chrome/browser/tpcd/support/top_level_trial_service_factory.h"
 #include "chrome/browser/tpcd/support/tpcd_support_service_factory.h"
 #include "chrome/browser/translate/translate_model_service_factory.h"
@@ -1256,6 +1257,7 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
   TopSitesFactory::GetInstance();
   tpcd::experiment::EligibilityServiceFactory::GetInstance();
+  tpcd::trial::OriginTrialServiceFactory::GetInstance();
   tpcd::trial::TpcdTrialServiceFactory::GetInstance();
   tpcd::trial::TopLevelTrialServiceFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
