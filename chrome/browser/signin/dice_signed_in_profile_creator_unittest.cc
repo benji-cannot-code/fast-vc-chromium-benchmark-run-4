9913b94e13eb60155accb432483c2226af653489
Ryan Sultanem <rsult@google.com>
2024-06-10 14:49:07 +0000

    [Uno-D] Accepting signin interception sets ChromeSignin setting to always signin
    Bug: b:345737884
    Change-Id: Ib93005fda83e3de0b49fabc02ac13236f29c8a9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5613298
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1312766}

--- chrome/browser/signin/dice_signed_in_profile_creator_unittest.cc
+++ chrome/browser/signin/dice_signed_in_profile_creator_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/signin/dice_signed_in_profile_creator.h"
 
+#include <tuple>
+
 #include "base/barrier_closure.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -15,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/test_file_util.h"
 #include "chrome/browser/enterprise/profile_management/profile_management_features.h"
 #include "chrome/browser/profiles/profile_attributes_entry.h"
@@ -29,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "components/signin/public/base/signin_switches.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "content/public/browser/storage_partition.h"
 #include "content/public/test/browser_task_environment.h"
@@ -38,16 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char16_t kProfileTestName[] = u"profile_test_name";
-
-struct CookieTestParam {
-  bool enable_third_party_management_feature;
-  bool setup_cookies_to_move;
-};
-
-const CookieTestParam kTestCases[] = {{true, true},
-                                      {true, false},
-                                      {false, false},
-                                      {false, true}};
 
 void CreateCookies(
     Profile* profile,
@@ -116,16 +111,18 @@ class UnittestProfileManager : public FakeProfileManager {
 
 }  // namespace
 
+// Testing params:
+// - bool enable_third_party_management_feature
+// - bool setup_cookies_to_move
+// - bool explicit_browser_signin_enabled
 class DiceSignedInProfileCreatorTest
     : public testing::Test,
-      public testing::WithParamInterface<CookieTestParam>,
+      public testing::WithParamInterface<std::tuple<bool, bool, bool>>,
       public ProfileManagerObserver {
  public:
   DiceSignedInProfileCreatorTest()
       : local_state_(TestingBrowserProcess::GetGlobal()) {
-    scoped_feature_list_.InitWithFeatureState(
-        profile_management::features::kThirdPartyProfileManagement,
-        GetParam().enable_third_party_management_feature);
+    InitFeatures();
     auto profile_manager_unique = std::make_unique<UnittestProfileManager>(
         base::CreateUniqueTempDirectoryScopedToTest());
     profile_manager_ = profile_manager_unique.get();
@@ -138,7 +135,35 @@ class DiceSignedInProfileCreatorTest
     profile_manager()->AddObserver(this);
   }
 
+  void InitFeatures() {
+    std::vector<base::test::FeatureRef> enabled;
+    std::vector<base::test::FeatureRef> disabled;
+    if (enable_third_party_management_feature()) {
+      enabled.push_back(
+          profile_management::features::kThirdPartyProfileManagement);
+    } else {
+      disabled.push_back(
+          profile_management::features::kThirdPartyProfileManagement);
+    }
+
+    if (explicit_browser_signin_enabled()) {
+      enabled.push_back(switches::kExplicitBrowserSigninUIOnDesktop);
+    } else {
+      disabled.push_back(switches::kExplicitBrowserSigninUIOnDesktop);
+    }
+
+    scoped_feature_list_.InitWithFeatures(enabled, disabled);
+  }
+
   ~DiceSignedInProfileCreatorTest() override { DeleteProfiles(); }
+
+  bool enable_third_party_management_feature() {
+    return std::get<0>(GetParam());
+  }
+
+  bool setup_cookies_to_move() { return std::get<1>(GetParam()); }
+
+  bool explicit_browser_signin_enabled() { return std::get<2>(GetParam()); }
 
   UnittestProfileManager* profile_manager() { return profile_manager_; }
 
@@ -193,7 +218,7 @@ class DiceSignedInProfileCreatorTest
   }
 
   void SetupCookiesToMove() {
-    if (!GetParam().setup_cookies_to_move) {
+    if (!setup_cookies_to_move()) {
       return;
     }
     // Add some cookies
@@ -209,7 +234,7 @@ class DiceSignedInProfileCreatorTest
   }
 
   void VerifyCookiesMoved() {
-    if (!GetParam().setup_cookies_to_move) {
+    if (!setup_cookies_to_move()) {
       return;
     }
     GURL url("https://www.google.com/");
@@ -240,7 +265,7 @@ class DiceSignedInProfileCreatorTest
       loop.Run();
     }
 
-    if (!GetParam().enable_third_party_management_feature) {
+    if (!enable_third_party_management_feature()) {
       EXPECT_EQ(6u, cookies_source_profile.size());
       EXPECT_TRUE(cookies_new_profile.empty());
       return;
@@ -296,11 +321,15 @@ TEST_P(DiceSignedInProfileCreatorTest, CreateWithTokensLoaded) {
   EXPECT_EQ(signed_in_profile(), added_profile());
   EXPECT_FALSE(IdentityManagerFactory::GetForProfile(profile())
                    ->HasAccountWithRefreshToken(account_info.account_id));
-  EXPECT_EQ(1u, IdentityManagerFactory::GetForProfile(signed_in_profile())
-                    ->GetAccountsWithRefreshTokens()
-                    .size());
+  signin::IdentityManager* new_identity_manager =
+      IdentityManagerFactory::GetForProfile(signed_in_profile());
+  EXPECT_EQ(1u, new_identity_manager->GetAccountsWithRefreshTokens().size());
   EXPECT_TRUE(IdentityManagerFactory::GetForProfile(signed_in_profile())
                   ->HasAccountWithRefreshToken(account_info.account_id));
+  if (explicit_browser_signin_enabled()) {
+    EXPECT_TRUE(
+        new_identity_manager->HasPrimaryAccount(signin::ConsentLevel::kSignin));
+  }
 
   // Check profile type
   ASSERT_FALSE(signed_in_profile()->IsGuestSession());
@@ -407,4 +436,6 @@ TEST_P(DiceSignedInProfileCreatorTest, DeleteProfile) {
 
 INSTANTIATE_TEST_SUITE_P(All,
                          DiceSignedInProfileCreatorTest,
-                         ::testing::ValuesIn(kTestCases));
+                         testing::Combine(testing::Bool(),
+                                          testing::Bool(),
+                                          testing::Bool()));
