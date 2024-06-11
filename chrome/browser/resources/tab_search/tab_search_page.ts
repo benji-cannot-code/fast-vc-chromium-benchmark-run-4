8bc178805f18a8a1837bb9e993a69b74dc776bbf
rbpotter <rbpotter@chromium.org>
2024-06-11 16:58:16 +0000

    Tab Search: Avoid rendering extra items
    The introduction of the "tab organization" feature added an
    IntersectionObserver to tab search page to detect when the page is
    shown or hidden. This observer triggered a call to
    ensureAllDomItemsAvailable() on infinite-list, which renders all tab
    search items in the list. This is undesirable unless the user
    intentionally accesses later items in the list (e.g. by scrolling).
    Moreover, the observer was inconsistently also firing on initial page
    load, causing all tab search items to be immediately rendered even
    if the tab list tab was selected initially.
    
    Instead, add a new public method fillCurrentViewHeight() to
    infinite-list to render all items in the current view height. Call
    this from the observer instead. Add a boolean to the tab search page
    to only trigger this when the page actually goes from hidden to
    shown.
    
    Bug: 345599360
    Change-Id: I1375970de97c2d758d1d1516a12c286a0f154a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5609933
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313481}

--- chrome/browser/resources/tab_search/tab_search_page.ts
+++ chrome/browser/resources/tab_search/tab_search_page.ts
@@ -165,6 +165,7 @@ export class TabSearchPageElement extends TabSearchSearchFieldBase {
   private initiallySelectedTabIndex_: number = NO_SELECTION;
   private documentVisibilityChangedListener_: () => void;
   private elementVisibilityChangedListener_: IntersectionObserver;
+  private wasInactive_: boolean = loadTimeData.getInteger('tabIndex') !== 0;
 
   constructor() {
     super();
@@ -289,8 +290,10 @@ export class TabSearchPageElement extends TabSearchSearchFieldBase {
   }
 
   private onElementVisibilityChanged_(visible: boolean) {
-    if (visible) {
-      this.$.tabsList.ensureAllDomItemsAvailable();
+    if (visible && this.wasInactive_) {
+      this.$.tabsList.fillCurrentViewHeight();
+    } else if (!visible) {
+      this.wasInactive_ = true;
     }
   }
 
