fe61268a4ae42b5553e1aaf4a5a68db6862c1782
Trevor Perrier <perrier@google.com>
2024-06-11 22:20:06 +0000

    [Compose] Block proactive nudge on unsupported page language
    Previously the page language only blocked the proactive nudge if the
    language bypass flag was disabled.  This CL changes it so that the
    proactive nudge is never shown if the page language is unsupported.
    
    Since after this change the language bypass is only applicable to
    the context menu the flag name is changed from:
    
     - `kEnableComposeLanguageBypass` to
     - `kEnableComposeLanguageBypassForContextMenu`
    
    This CL also removes the TranslateLanguageProvider helper class. This
    class existed to make creating a MockTranslateManager in testing.
    However, in testing it is possible to create a real TranslateManager
    using a MockTranslateDriver, MockTranslateClient, and
    MockTranslateRanker.
    
    This CL moves to using a real TranslateManager
    in compose_enabling_unittests.
    
    Bug: b/345176060, b/345175551
    Change-Id: I2322dbbb018685f8b24e6581af25b386dab83bee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5616192
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313711}

--- chrome/browser/compose/compose_enabling.h
+++ chrome/browser/compose/compose_enabling.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/types/expected.h"
 #include "chrome/browser/compose/proto/compose_optimization_guide.pb.h"
-#include "chrome/browser/compose/translate_language_provider.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -42,7 +41,6 @@ class ComposeEnabling {
   using ScopedOverride = std::unique_ptr<base::ScopedClosureRunner>;
 
   explicit ComposeEnabling(
-      TranslateLanguageProvider* translate_language_provider,
       Profile* profile,
       signin::IdentityManager* identity_manager,
       OptimizationGuideKeyedService* opt_guide);
@@ -110,7 +108,6 @@ class ComposeEnabling {
       OptimizationGuideKeyedService* opt_guide,
       signin::IdentityManager* identity_manager);
 
-  raw_ptr<TranslateLanguageProvider> translate_language_provider_;
   raw_ptr<Profile> profile_;
   raw_ptr<OptimizationGuideKeyedService> opt_guide_;
   raw_ptr<signin::IdentityManager> identity_manager_;
