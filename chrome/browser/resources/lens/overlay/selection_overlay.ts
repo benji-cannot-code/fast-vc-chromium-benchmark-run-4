30b0bceac320a9bad5aa5bba988470ef7283a1fa
Juan Mojica <juanmojica@google.com>
2024-06-11 21:29:22 +0000

    [lensoverlay] Move copy text toast to parent of selection overlay.
    This prevents the backgroundScrim from intercepting clicks from the copy
    text toast dismissing the overlay.
    
    Demo: http://shortn/_aJCOUOfRe0
    
    Change-Id: I4a707023ac64c34b8bf463be63ff41c586881ef3
    Bug: b:339489310
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621482
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313673}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -13,7 +13,6 @@ import './strings.m.js';
 import '//resources/cr_elements/cr_button/cr_button.js';
 import '//resources/cr_elements/cr_toast/cr_toast.js';
 
-import type {CrToastElement} from '//resources/cr_elements/cr_toast/cr_toast.js';
 import {I18nMixin} from '//resources/cr_elements/i18n_mixin.js';
 import {assert} from '//resources/js/assert.js';
 import {EventTracker} from '//resources/js/event_tracker.js';
@@ -83,7 +82,6 @@ export interface DetectedTextContextMenuData {
 export interface SelectionOverlayElement {
   $: {
     backgroundImage: HTMLImageElement,
-    copyToast: CrToastElement,
     cursor: HTMLElement,
     detectedTextContextMenu: HTMLElement,
     initialFlashScrim: HTMLDivElement,
@@ -619,8 +617,7 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
         this.shadowRoot!.elementsFromPoint(event.clientX, event.clientY);
     // Do not intercept events that should go to the following elements.
     if (elementsAtPoint.includes(this.$.selectedTextContextMenu) ||
-        elementsAtPoint.includes(this.$.detectedTextContextMenu) ||
-        elementsAtPoint.includes(this.$.copyToast)) {
+        elementsAtPoint.includes(this.$.detectedTextContextMenu)) {
       return true;
     }
     // Ignore multi touch events and none left click events.
@@ -652,20 +649,10 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
   private async handleCopy() {
     navigator.clipboard.writeText(this.highlightedText);
     recordLensOverlayInteraction(UserAction.COPY_TEXT);
-    if (this.$.copyToast.open) {
-      // If toast already open, wait after hiding so that animation is
-      // smoother.
-      await this.$.copyToast.hide();
-      setTimeout(() => {
-        this.$.copyToast.show();
-      }, 100);
-    } else {
-      this.$.copyToast.show();
-    }
-  }
-
-  private onHideToastClick() {
-    this.$.copyToast.hide();
+    this.dispatchEvent(new CustomEvent('text-copied', {
+      bubbles: true,
+      composed: true,
+    }));
   }
 
   private handleSelectText() {
