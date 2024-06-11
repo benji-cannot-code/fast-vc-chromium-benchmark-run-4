52df5cecc51dc5fc4a757f8ba08cdb8d485c101d
Solomon Kinard <solomonkinard@chromium.org>
2024-06-11 23:01:49 +0000

    Revert "[PWA] Enable universal install on CrOS via field trial testing"
    This reverts commit 3f075793b28bae839af431d0fccfcde1c0a749ee.
    
    Reason for revert: crbug.com/346597051
    
    Tests failing every hour for the last four hours or so:
    * MLPromotionBrowserTest.MLPipelineNoCrashForExistingTracker
    * TelemetryExtensionApiGuardRealDelegateBrowserTest.UseCacheForMultipleApiAccess
    * WebAppBrowserTest.BrowserDisplayNotInstallable
    * WebAppBrowserTest.WebAppInternalsPage
    
    Original change's description:
    > [PWA] Enable universal install on CrOS via field trial testing
    >
    > Enable universal install on CrOS, test to verify behavior changes
    > before launching in production.
    >
    > Bug: 326611791
    > Change-Id: Ibe5595fd1f91523d310bd1b2d03697faa38cc6d1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545022
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1313540}
    
    Bug: 326611791
    Change-Id: I9238cfc60a812fbd0b941caca6f893b63dfd8b30
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623831
    Owners-Override: Solomon Kinard <solomonkinard@google.com>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1313729}

--- chrome/browser/ash/system_web_apps/apps/help_app/help_app_integration_browsertest.cc
+++ chrome/browser/ash/system_web_apps/apps/help_app/help_app_integration_browsertest.cc
@@ -52,7 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
-#include "chrome/common/chrome_features.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -797,12 +796,8 @@ IN_PROC_BROWSER_TEST_P(HelpAppIntegrationTest,
   content::TestNavigationObserver navigation_observer(test_url);
   navigation_observer.StartWatchingNewWebContents();
 
-  std::string dialog_name =
-      base::FeatureList::IsEnabled(::features::kWebAppUniversalInstall)
-          ? "WebAppSimpleInstallDialog"
-          : "PWAConfirmationBubbleView";
   views::NamedWidgetShownWaiter waiter(views::test::AnyWidgetTestPasskey(),
-                                       dialog_name);
+                                       "PWAConfirmationBubbleView");
 
   // Script that tells the Help App to call the
   // OpenUrlInBrowserAndTriggerInstallDialog Mojo function.
