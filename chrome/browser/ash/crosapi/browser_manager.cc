bee73f210d47dd86017e131fc6fd96f98a7a532e
Yuta Hijikata <ythjkt@google.com>
2024-06-11 06:14:02 +0000

    Clear standalone_browser_prefs once Lacros is disabled.
    These prefs are set by Lacros via crosapi. If Lacros is disabled, the
    prefs are not updatable from Ash thus they should be cleared upon
    Lacros disablement.
    
    Bug: 297826137
    Change-Id: I7706848d74374a9b583bdd9c5db30a3cc9e3b355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5562385
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313237}

--- chrome/browser/ash/crosapi/browser_manager.cc
+++ chrome/browser/ash/crosapi/browser_manager.cc
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
 #include "components/user_manager/user_type.h"
+#include "components/user_prefs/user_prefs.h"
 #include "components/version_info/version_info.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -900,7 +901,21 @@ void BrowserManager::ClearLacrosData() {
         }));
   }
 
-  // TODO(b/297826137): Remove 'standalone_browser_preferences.json'.
+  // Clear prefs set by Lacros and stored in
+  // 'standalone_browser_preferences.json' if Lacros is disabled.
+  const user_manager::User* user =
+      user_manager::UserManager::Get()->GetPrimaryUser();
+  if (!user) {
+    CHECK_IS_TEST();
+    return;
+  }
+  content::BrowserContext* context =
+      ash::BrowserContextHelper::Get()->GetBrowserContextByUser(user);
+  if (!context) {
+    CHECK_IS_TEST();
+    return;
+  }
+  user_prefs::UserPrefs::Get(context)->RemoveAllStandaloneBrowserPrefs();
 }
 
 void BrowserManager::OnBrowserServiceConnected(
