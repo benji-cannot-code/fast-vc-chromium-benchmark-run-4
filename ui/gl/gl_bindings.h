6383878e6e8205ab6cdccb485ad90a9b5f66a4db
Corentin Wallez <cwallez@chromium.org>
2024-06-11 12:33:31 +0000

    //ui/gl: Stop including platform GL headers other than EGL.
    Bug: 344606399
    Change-Id: Ice38ee6c4f6393ef13c6245095f904b7c8984c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594753
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313343}

--- ui/gl/gl_bindings.h
+++ ui/gl/gl_bindings.h
@@ -32,22 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/extension_set.h"
 #include "ui/gl/gl_export.h"
 
-// The standard OpenGL native extension headers are also included.
-#if BUILDFLAG(IS_WIN)
-#include <GL/wglext.h>
-#elif BUILDFLAG(IS_MAC)
-#include <OpenGL/OpenGL.h>
-#elif BUILDFLAG(IS_LINUX)
-using Display = struct _XDisplay;
-using Bool = int;
-using Status = int;
-using XID = unsigned long;
-using Colormap = XID;
-using Font = XID;
-using Pixmap = XID;
-using Window = XID;
-#endif
-
 // GLES2 defines not part of Desktop GL
 // Shader Precision-Specified Types
 #define GL_LOW_FLOAT                                     0x8DF0
