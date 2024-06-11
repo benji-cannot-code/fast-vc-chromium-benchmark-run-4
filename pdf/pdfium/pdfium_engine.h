3797931785e0a60fb25ce0d25ed0c6f5b4cb52ea
Lei Zhang <thestig@chromium.org>
2024-06-11 01:34:23 +0000

    Better expose PDFiumEngine::plugin_size() callers that fail DCHECK
    Use DUMP_WILL_BE_NOTREACHED() in PDFiumEngine::plugin_size() instead of
    a DCHECK(), so that the DCHECK() failures consistently generate
    non-fatal dumped in all builds. Temporarily un-inline plugin_size() to
    avoid putting more code into the header.
    
    Bug: 40193305
    Change-Id: I92459e6d36fe33d296404486dd4b93615113a719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618930
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313170}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/check.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ptr.h"
@@ -280,11 +279,7 @@ class PDFiumEngine : public PDFEngine,
   friend class PDFiumTestBase;
   friend class SelectionChangeInvalidator;
 
-  gfx::Size plugin_size() const {
-    // TODO(crbug.com/40193305): Just use .value() after fixing call sites.
-    DCHECK(plugin_size_.has_value());
-    return plugin_size_.value_or(gfx::Size());
-  }
+  gfx::Size plugin_size() const;
 
   // We finished getting the pdf file, so load it. This will complete
   // asynchronously (due to password fetching) and may be run multiple times.
