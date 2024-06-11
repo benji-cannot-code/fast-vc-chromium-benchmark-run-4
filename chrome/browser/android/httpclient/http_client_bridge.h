e9d483a20b20f95de87338828ffef39fcac80d38
Andrew Grieve <agrieve@chromium.org>
2024-06-11 19:33:09 +0000

    JNI Zero: Add support for @JniType("std::map<>")
    Serializes the Map to an Object[], and then populates a std::map (or
    similar).
    
    Bug: 326110639
    Change-Id: Iaa72e11ebac6c11a711845e99c98551969a7351f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615691
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313601}

--- chrome/browser/android/httpclient/http_client_bridge.h
+++ chrome/browser/android/httpclient/http_client_bridge.h
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
+#include <map>
 #include <vector>
 
 #include "base/android/scoped_java_ref.h"
 #include "base/memory/weak_ptr.h"
+#include "url/gurl.h"
 
 class Profile;
 
@@ -33,11 +35,10 @@ class HttpClientBridge {
 
   void SendNetworkRequest(
       JNIEnv* env,
-      const base::android::JavaParamRef<jobject>& j_gurl,
-      const base::android::JavaParamRef<jstring>& j_request_type,
-      const base::android::JavaParamRef<jbyteArray>& j_body,
-      const base::android::JavaParamRef<jobjectArray>& j_header_keys,
-      const base::android::JavaParamRef<jobjectArray>& j_header_values,
+      GURL& gurl,
+      std::string& request_type,
+      std::vector<uint8_t>& request_body,
+      std::map<std::string, std::string> headers,
       jint j_network_annotation_hashcode,
       const base::android::JavaParamRef<jobject>& j_callback);
 
@@ -46,8 +47,7 @@ class HttpClientBridge {
                 int32_t http_code,
                 int32_t net_error_code,
                 std::vector<uint8_t>&& response_bytes,
-                std::vector<std::string>&& response_header_keys,
-                std::vector<std::string>&& response_header_values);
+                std::map<std::string, std::string>&& response_headers);
 
   std::unique_ptr<HttpClient> http_client_;
   base::WeakPtrFactory<HttpClientBridge> weak_ptr_factory_{this};
