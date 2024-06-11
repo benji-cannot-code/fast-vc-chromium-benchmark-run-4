8102b273498f720e06aaea03a45c21dee6f5e7ad
anthie@google.com <anthie@google.com>
2024-06-11 17:01:46 +0000

    Record SU extensions counters daily metrics
    Record numbers of installed, enabled and disabled extensions
    daily for supervised users.
    
    Bug: b/321240396
    Change-Id: Idbb2c8b6cd5d3308af38db4532888b6bc55eefa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557490
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313488}

---
+++ chrome/browser/supervised_user/linux_mac_windows/supervised_user_extensions_metrics_delegate_impl.h
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_SUPERVISED_USER_LINUX_MAC_WINDOWS_SUPERVISED_USER_EXTENSIONS_METRICS_DELEGATE_IMPL_H_
+#define CHROME_BROWSER_SUPERVISED_USER_LINUX_MAC_WINDOWS_SUPERVISED_USER_EXTENSIONS_METRICS_DELEGATE_IMPL_H_
+
+#include "components/supervised_user/core/browser/supervised_user_metrics_service.h"
+#include "extensions/browser/extension_registry.h"
+
+class PrefService;
+
+class SupervisedUserExtensionsMetricsDelegateImpl
+    : public supervised_user::SupervisedUserMetricsService::
+          SupervisedUserMetricsServiceExtensionDelegate {
+ public:
+  SupervisedUserExtensionsMetricsDelegateImpl(
+      const extensions::ExtensionRegistry* extension_registry,
+      const PrefService& user_prefs);
+  ~SupervisedUserExtensionsMetricsDelegateImpl() override;
+  // SupervisedUserMetricsServiceExtensionDelegate implementation:
+  bool RecordExtensionsMetrics() override;
+
+ private:
+  const raw_ptr<const extensions::ExtensionRegistry> extension_registry_;
+  const raw_ref<const PrefService> user_prefs_;
+};
+
+#endif  // CHROME_BROWSER_SUPERVISED_USER_LINUX_MAC_WINDOWS_SUPERVISED_USER_EXTENSIONS_METRICS_DELEGATE_IMPL_H_
