cf9b4050de47d216d3afa4e0148e466dd8a2f3a5
Corentin Wallez <cwallez@chromium.org>
2024-06-11 11:00:39 +0000

    //gpu: Remove casts for Dawn's getProc injection.
    Bug: 343870490
    Change-Id: I3dea50807886afc9c6d84d1f8f04d62186779ac5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5612411
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313304}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -427,10 +427,7 @@ void ReportWebGPUSupportMetrics(dawn::native::Instance* instance) {
 
   dawn::native::opengl::RequestAdapterOptionsGetGLProc
       adapter_options_get_gl_proc = {};
-  // TODO(343870490): Remove the cast once the type is updated in Dawn.
-  adapter_options_get_gl_proc.getProc =
-      reinterpret_cast<decltype(adapter_options_get_gl_proc.getProc)>(
-          gl::GetGLProcAddress);
+  adapter_options_get_gl_proc.getProc = gl::GetGLProcAddress;
   adapter_options_get_gl_proc.display = EGL_NO_DISPLAY;
   adapter_options_get_gl_proc.nextInChain = adapter_options.nextInChain;
   adapter_options.nextInChain = &adapter_options_get_gl_proc;
@@ -919,10 +916,7 @@ void CollectDawnInfo(const gpu::GpuPreferences& gpu_preferences,
 #if BUILDFLAG(DAWN_ENABLE_BACKEND_OPENGLES)
   dawn::native::opengl::RequestAdapterOptionsGetGLProc
       adapter_options_get_gl_proc = {};
-  // TODO(343870490): Remove the cast once the type is updated in Dawn.
-  adapter_options_get_gl_proc.getProc =
-      reinterpret_cast<decltype(adapter_options_get_gl_proc.getProc)>(
-          gl::GetGLProcAddress);
+  adapter_options_get_gl_proc.getProc = gl::GetGLProcAddress;
   adapter_options_get_gl_proc.display = EGL_NO_DISPLAY;
   adapter_options_get_gl_proc.nextInChain = adapter_options.nextInChain;
   adapter_options.nextInChain = &adapter_options_get_gl_proc;
