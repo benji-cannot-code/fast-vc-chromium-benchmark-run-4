692d733b1ca32514835d346871618b76f6c5eca9
Michael Lippautz <mlippautz@chromium.org>
2024-06-11 15:59:31 +0000

    [gin] Remove assert requiring two embedder fields for ABs
    This assert made sure that V8 was compiled with two embedder fields
    for ABs to be able to dynamically use them as wrappers for
    DOMArrayBuffer without creating an intermediate JS API object.
    
    ABs now have a dedicated field for CppHeapPointers for this
    purpose.
    
    Bug: 346350847
    Change-Id: I2552f90fcdc59a3046a0beea34aafd17bbf6a859
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621930
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Auto-Submit: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Leszek Swirski <leszeks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313445}

--- gin/array_buffer.cc
+++ gin/array_buffer.cc
@@ -28,9 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gin {
 
-static_assert(V8_ARRAY_BUFFER_INTERNAL_FIELD_COUNT == 2,
-              "array buffers must have two internal fields");
-
 // ArrayBufferAllocator -------------------------------------------------------
 partition_alloc::PartitionRoot* ArrayBufferAllocator::partition_ = nullptr;
 
