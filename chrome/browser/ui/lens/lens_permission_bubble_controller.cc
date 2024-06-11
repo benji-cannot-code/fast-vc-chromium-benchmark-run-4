b5aeb57e02b6a42d5bed6e76b616459c701f3a0f
Nihar Majmudar <niharm@google.com>
2024-06-11 18:22:26 +0000

    [lensoverlay] Change permission bubble close button.
    Changes text in permission bubble from "Close" to "Cancel".
    
    Bug: 346425813
    Change-Id: I0c5cb2dd52e55269833e2280386ac3b1de24b56b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618371
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313558}

--- chrome/browser/ui/lens/lens_permission_bubble_controller.cc
+++ chrome/browser/ui/lens/lens_permission_bubble_controller.cc
@@ -128,7 +128,7 @@ LensPermissionBubbleController::CreateLensPermissionDialogModel() {
               weak_ptr_factory_.GetWeakPtr()),
           ui::DialogModel::Button::Params()
               .SetLabel(l10n_util::GetStringUTF16(
-                  IDS_LENS_PERMISSION_BUBBLE_DIALOG_CLOSE_BUTTON))
+                  IDS_LENS_PERMISSION_BUBBLE_DIALOG_CANCEL_BUTTON))
               .SetId(kLensPermissionDialogCancelButtonElementId)
               .SetStyle(ui::ButtonStyle::kTonal))
       .SetCloseActionCallback(base::BindOnce(
