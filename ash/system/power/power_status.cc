e53061fa18b144dd3452b8c2f5d1e2d13e67d314
Nikolai Artemiev <nartemiev@google.com>
2024-06-11 04:21:06 +0000

    ash: Show "Calculating battery %..." while waiting for battery data
    This commit fixes an issue where the status bar dialog box would show
    "Battery full", regardless of the actual battery level, until the UI
    received battery charge data from powerd.
    
    Normally powerd provides a battery reading before users can see the
    "Battery full" default state, however there are some some cases where it
    does not (particularly when the battery level is 0%).
    
    BUG=b:312817389,b:330614726
    
    Change-Id: I997eefc2751bfee54695e4c393cd2aa05b307254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5568570
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Commit-Queue: Nikolai Artemiev <nartemiev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313217}

--- ash/system/power/power_status.cc
+++ ash/system/power/power_status.cc
@@ -334,6 +334,11 @@ gfx::ImageSkia PowerStatus::GetBatteryImage(
 
 std::u16string PowerStatus::GetAccessibleNameString(
     bool full_description) const {
+  if (!proto_initialized_) {
+    return l10n_util::GetStringUTF16(
+        IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_CHARGE_LEVEL_ACCESSIBLE);
+  }
+
   if (IsBatteryFull()) {
     return l10n_util::GetStringUTF16(
         IDS_ASH_STATUS_TRAY_BATTERY_FULL_CHARGE_ACCESSIBLE);
@@ -396,7 +401,10 @@ std::pair<std::u16string, std::u16string> PowerStatus::GetStatusStrings()
     const {
   std::u16string percentage;
   std::u16string status;
-  if (IsBatteryFull()) {
+  if (!proto_initialized_) {
+    status = l10n_util::GetStringUTF16(
+        IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_CHARGE_LEVEL);
+  } else if (IsBatteryFull()) {
     status = l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_BATTERY_FULL);
   } else {
     percentage = base::FormatPercent(GetRoundedBatteryPercent());
