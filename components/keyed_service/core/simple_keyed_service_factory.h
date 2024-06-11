93089928b505b008e857a87274e1783a76caa2a4
Sylvain Defresne <sdefresne@chromium.org>
2024-06-11 17:28:55 +0000

    Remove KeyedServiceTemplatedFactory::IsOffTheRecord(...) method
    The method is defined, overridden but never called. Remove it as
    it is dead-code.
    
    Bug: none
    Change-Id: I7a1b49e1d6ac8faeff8314c1aa07301cef64707f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5596817
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313528}

--- components/keyed_service/core/simple_keyed_service_factory.h
+++ components/keyed_service/core/simple_keyed_service_factory.h
@@ -133,7 +133,6 @@ class KEYED_SERVICE_EXPORT SimpleKeyedServiceFactory
   // KeyedServiceFactory:
   std::unique_ptr<KeyedService> BuildServiceInstanceFor(
       void* context) const final;
-  bool IsOffTheRecord(void* context) const final;
 
   // KeyedServiceBaseFactory:
   void* GetContextToUse(void* context) const final;
