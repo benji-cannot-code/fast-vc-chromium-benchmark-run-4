e0254946df26333cef82998da7a6d6e5e3f16db4
Andrey Davydov <andreydav@google.com>
2024-06-11 17:41:50 +0000

    Add skeletons of FloatingSsoSyncBridge and FloatingSsoService
    FloatingSsoSyncBridge is the main part of Sync integration,
    FloatingSsoService is a KeyedService which will orchestrate the main
    feature logic. This cl adds their skeletons with lots of TODO comments
    referencing future work. Meaningful parts implemented here:
    - new feature flag to control the creation of FloatingSsoService
    - FloatingSsoService observes kFloatingSsoEnabled pref
    - two browser tests for points above
    - FloatingSsoServiceFactory overrides ServiceIsCreatedWithBrowserContext
    so that the service is created on profile creation
    - FloatingSsoServiceFactory specifies in its constructor that only
    regular profiles should have FloatingSsoService
    
    Design doc(internal): go/floating-sso-dd
    
    Bug: b:318391357, b:346354326, b:346354327
    Change-Id: I683bef6867d6485fec6c1316c67bd3ed299099a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577196
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313539}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -327,6 +327,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/certificate_provider/certificate_provider_service_factory.h"
 #include "chrome/browser/chromeos/cros_apps/cros_apps_key_event_handler_factory.h"
 #include "chrome/browser/chromeos/enterprise/cloud_storage/one_drive_pref_observer.h"
+#include "chrome/browser/chromeos/enterprise/floating_sso/floating_sso_service_factory.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_download_observer_factory.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager_factory.h"
 #include "chrome/browser/policy/networking/policy_cert_service_factory.h"
@@ -860,6 +861,19 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if !BUILDFLAG(IS_CHROMEOS_ASH) && !BUILDFLAG(IS_ANDROID)
   FirstRunServiceFactory::GetInstance();
 #endif
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
+  if (chromeos::features::IsFloatingSsoAllowed()) {
+    chromeos::floating_sso::FloatingSsoServiceFactory::GetInstance();
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // Floating SSO service must run in the main browser only, i.e. we shouldn't
+  // launch it in Ash when Lacros is enabled.
+  if (chromeos::features::IsFloatingSsoAllowed() &&
+      !crosapi::browser_util::IsLacrosEnabled()) {
+    chromeos::floating_sso::FloatingSsoServiceFactory::GetInstance();
+  }
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
   FontPrefChangeNotifierFactory::GetInstance();
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   GAIAInfoUpdateServiceFactory::GetInstance();
