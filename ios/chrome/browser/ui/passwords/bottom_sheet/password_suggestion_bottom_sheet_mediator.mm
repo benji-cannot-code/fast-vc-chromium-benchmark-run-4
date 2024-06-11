de5174da190fee13b546072e51a10466ff04d58f
Alexis Hetu <sugoi@chromium.org>
2024-06-11 17:58:30 +0000

    [iOS] Fix crash caused by variable going out of scope
    The 'params' variable is passed to the PasswordSuggestionBottomSheetMediator as a const reference, so we can't assume that it'll still exist by the time the completion block of the retrieveSuggestionsForForm method is called, so we store the members we need from it in local variables.
    
    Bug: 346453656
    Change-Id: I8f469b938769f74bb721830a7110ed77a67c906a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622850
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313547}

--- ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
+++ ios/chrome/browser/ui/passwords/bottom_sheet/password_suggestion_bottom_sheet_mediator.mm
@@ -186,6 +186,12 @@ int PrimaryActionStringIdFromSuggestion(FormSuggestion* suggestion) {
       self.suggestionsProvider = tabHelper->GetAccessoryViewProvider();
       DCHECK(self.suggestionsProvider);
 
+      // The 'params' argument may go out of scope before the completion block
+      // is called, so we need to store variables used in the completion block
+      // locally.
+      autofill::FormRendererId formId = params.form_renderer_id;
+      std::string frameId = params.frame_id;
+
       __weak __typeof(self) weakSelf = self;
       [self.suggestionsProvider
           retrieveSuggestionsForForm:params
@@ -194,9 +200,9 @@ int PrimaryActionStringIdFromSuggestion(FormSuggestion* suggestion) {
                 NSArray<FormSuggestion*>* suggestions,
                 id<FormInputSuggestionsProvider> formInputSuggestionsProvider) {
               weakSelf.suggestions = suggestions;
-              [weakSelf fetchCredentialsForForm:params.form_renderer_id
+              [weakSelf fetchCredentialsForForm:formId
                                        webState:activeWebState
-                                     webFrameId:params.frame_id];
+                                     webFrameId:frameId];
             }];
     }
 
