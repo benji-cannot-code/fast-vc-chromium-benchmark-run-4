3b419f4aed4969d13a36e736f387f1ccd4134d21
Lei Zhang <thestig@chromium.org>
2024-06-11 17:14:30 +0000

    Tidy content/public/browser/webui_config_map.cc
    - Use std::vector::reserve() in GetWebUIConfigList().
    - Use (or don't use) ternary operators when appropriate.
    - Fix various spacing issues and also auto-format the file.
    
    Change-Id: Idd5913913f18d3c9f39c586b0c17f17dd6bc81ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5620630
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313510}

--- content/public/browser/webui_config_map.cc
+++ content/public/browser/webui_config_map.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/webui_config_map.h"
 
+#include <utility>
+
 #include "base/containers/contains.h"
 #include "base/memory/raw_ref.h"
 #include "base/no_destructor.h"
@@ -31,10 +33,7 @@ class WebUIConfigMapWebUIControllerFactory : public WebUIControllerFactory {
   WebUI::TypeID GetWebUIType(BrowserContext* browser_context,
                              const GURL& url) override {
     auto* config = config_map_->GetConfig(browser_context, url);
-    if (!config)
-      return WebUI::kNoWebUI;
-
-    return reinterpret_cast<WebUI::TypeID>(config);
+    return config ? reinterpret_cast<WebUI::TypeID>(config) : WebUI::kNoWebUI;
   }
 
   bool UseWebUIForURL(BrowserContext* browser_context,
@@ -47,10 +46,7 @@ class WebUIConfigMapWebUIControllerFactory : public WebUIControllerFactory {
       const GURL& url) override {
     auto* browser_context = web_ui->GetWebContents()->GetBrowserContext();
     auto* config = config_map_->GetConfig(browser_context, url);
-    if (!config)
-      return nullptr;
-
-    return config->CreateWebUIController(web_ui, url);
+    return config ? config->CreateWebUIController(web_ui, url) : nullptr;
   }
 
  private:
@@ -60,6 +56,7 @@ class WebUIConfigMapWebUIControllerFactory : public WebUIControllerFactory {
 };
 
 }  // namespace
+
 // static
 WebUIConfigMap& WebUIConfigMap::GetInstance() {
   static base::NoDestructor<WebUIConfigMap> instance;
@@ -105,10 +102,11 @@ WebUIConfig* WebUIConfigMap::GetConfig(BrowserContext* browser_context,
   }
 
   auto origin_and_config = configs_map_.find(url::Origin::Create(url));
-  if (origin_and_config == configs_map_.end())
+  if (origin_and_config == configs_map_.end()) {
     return nullptr;
-  auto& config = origin_and_config->second;
+  }
 
+  auto& config = origin_and_config->second;
   if (!config->IsWebUIEnabled(browser_context) ||
       !config->ShouldHandleURL(url)) {
     return nullptr;
@@ -122,8 +120,9 @@ std::unique_ptr<WebUIConfig> WebUIConfigMap::RemoveConfig(const GURL& url) {
         url.scheme() == kChromeUIUntrustedScheme);
 
   auto it = configs_map_.find(url::Origin::Create(url));
-  if (it == configs_map_.end())
+  if (it == configs_map_.end()) {
     return nullptr;
+  }
 
   auto webui_config = std::move(it->second);
   configs_map_.erase(it);
@@ -133,13 +132,13 @@ std::unique_ptr<WebUIConfig> WebUIConfigMap::RemoveConfig(const GURL& url) {
 std::vector<WebUIConfigInfo> WebUIConfigMap::GetWebUIConfigList(
     BrowserContext* browser_context) {
   std::vector<WebUIConfigInfo> origins;
+  origins.reserve(configs_map_.size());
   for (auto& it : configs_map_) {
     auto& webui_config = it.second;
     origins.push_back({
         .origin = it.first,
-        .enabled = browser_context == nullptr
-                       ? false
-                       : webui_config->IsWebUIEnabled(browser_context),
+        .enabled =
+            browser_context && webui_config->IsWebUIEnabled(browser_context),
     });
   }
   return origins;
