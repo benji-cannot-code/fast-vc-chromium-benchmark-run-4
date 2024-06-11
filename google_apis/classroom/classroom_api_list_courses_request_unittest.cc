62ece943390f4f852eff5fccf4b17e8f4ef00012
Tali Gorokhovsky <taligoro@google.com>
2024-06-11 19:14:23 +0000

    Add teacherId query parameter to ListCoursesRequest.
    Bug: 343731449
    Change-Id: I795338b1b3208c79d40a95bbc5cd3670a56239c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5609432
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Tali Gorokhovsky <taligoro@google.com>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313580}

--- google_apis/classroom/classroom_api_list_courses_request_unittest.cc
+++ google_apis/classroom/classroom_api_list_courses_request_unittest.cc
@@ -47,9 +47,13 @@ class TestRequestHandler {
     response->set_code(net::HTTP_OK);
     response->set_content(R"(
         {
-          "courses": [
-            {"id": "course-1", "name": "Course Name 1", "courseState": "ACTIVE"}
-          ]
+        "courses": [
+          {
+          "id": "course-1",
+          "name": "Course Name 1",
+          "courseState": "ACTIVE"
+          }
+        ]
         })");
     response->set_content_type("application/json");
     return response;
@@ -123,7 +127,7 @@ TEST_F(ClassroomApiListCoursesRequestsTest, ListCoursesRequest) {
   base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
       future;
   auto request = std::make_unique<ListCoursesRequest>(
-      request_sender(), /*student_id=*/"test1@test.com",
+      request_sender(), /*student_id=*/"test1@test.com", /*teacher_id=*/"",
       /*page_token=*/"", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
@@ -150,7 +154,7 @@ TEST_F(ClassroomApiListCoursesRequestsTest,
   base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
       future;
   auto request = std::make_unique<ListCoursesRequest>(
-      request_sender(), /*student_id=*/"test1@test.com",
+      request_sender(), /*student_id=*/"test1@test.com", /*teacher_id=*/"",
       /*page_token=*/"qwerty", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
@@ -175,13 +179,38 @@ TEST_F(ClassroomApiListCoursesRequestsTest, ListCoursesRequestHandlesError) {
   base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
       future;
   auto request = std::make_unique<ListCoursesRequest>(
-      request_sender(), /*student_id=*/"test1@test.com",
+      request_sender(), /*student_id=*/"test1@test.com", /*teacher_id=*/"",
       /*page_token=*/"", future.GetCallback());
   request_sender()->StartRequestWithAuthRetry(std::move(request));
   ASSERT_TRUE(future.Wait());
 
   ASSERT_FALSE(future.Get().has_value());
   EXPECT_EQ(future.Get().error(), HTTP_INTERNAL_SERVER_ERROR);
+}
+
+TEST_F(ClassroomApiListCoursesRequestsTest, ListCoursesRequestTeacherId) {
+  EXPECT_CALL(
+      request_handler(),
+      HandleRequest(AllOf(
+          Field(&HttpRequest::method, Eq(HttpMethod::METHOD_GET)),
+          Field(&HttpRequest::relative_url,
+                Eq("/v1/courses"
+                   "?fields=courses(id%2Cname%2CcourseState)%2CnextPageToken"
+                   "&teacherId=teacher1%40test.com"
+                   "&courseStates=ACTIVE")))))
+      .WillOnce(Return(ByMove(TestRequestHandler::CreateSuccessfulResponse())));
+
+  base::test::TestFuture<base::expected<std::unique_ptr<Courses>, ApiErrorCode>>
+      future;
+  auto request = std::make_unique<ListCoursesRequest>(
+      request_sender(), /*student_id=*/"", /*teacher_id=*/"teacher1@test.com",
+      /*page_token=*/"", future.GetCallback());
+  request_sender()->StartRequestWithAuthRetry(std::move(request));
+  ASSERT_TRUE(future.Wait());
+
+  ASSERT_TRUE(future.Get().has_value());
+  ASSERT_TRUE(future.Get().value());
+  EXPECT_EQ(future.Get().value()->items().size(), 1u);
 }
 
 }  // namespace google_apis::classroom
