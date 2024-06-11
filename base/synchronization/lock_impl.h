fef779872b6d2730ba45f54dbd25687c6d9c8ed0
Jose Dapena Paz <jdapena@igalia.com>
2024-06-11 10:43:36 +0000

    IWYU: missing include for usage of std::exchange in lock_impl.h
    Bug: 41455655
    Change-Id: If324eb8eece3dc710e6bcc032cc0587f45dd876b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5616412
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313273}

--- base/synchronization/lock_impl.h
+++ base/synchronization/lock_impl.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_SYNCHRONIZATION_LOCK_IMPL_H_
 #define BASE_SYNCHRONIZATION_LOCK_IMPL_H_
 
+#include <utility>
+
 #include "base/base_export.h"
 #include "base/check.h"
 #include "base/dcheck_is_on.h"
