6c91919af44c2489efdf94395ff82a032f3aef5c
Keita Suzuki <suzukikeita@chromium.org>
2024-06-11 00:13:44 +0000

    [GWP] Add new trace trigger in OnStart()
    This CL adds new trace trigger |gws-navigation-start| into
    |GWSPageLoadMetricsObserver::OnStart()|.
    
    Change-Id: I70321f64fdbbb81b74ac87abddedc84f63556f77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5599221
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313145}

--- chrome/browser/page_load_metrics/observers/gws_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/gws_page_load_metrics_observer.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string_util.h"
 #include "base/time/time.h"
+#include "base/trace_event/named_trigger.h"
 #include "chrome/browser/browser_process.h"
 #include "components/page_load_metrics/browser/observers/core/largest_contentful_paint_handler.h"
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
@@ -47,6 +48,19 @@ const char kHistogramGWSParseStart[] =
 }  // namespace internal
 
 GWSPageLoadMetricsObserver::GWSPageLoadMetricsObserver() = default;
+
+page_load_metrics::PageLoadMetricsObserver::ObservePolicy
+GWSPageLoadMetricsObserver::OnStart(
+    content::NavigationHandle* navigation_handle,
+    const GURL& currently_committed_url,
+    bool started_in_foreground) {
+  if (page_load_metrics::IsGoogleSearchResultUrl(navigation_handle->GetURL())) {
+    // Emit a trigger to allow trace collection tied to gws navigations.
+    base::trace_event::EmitNamedTrigger("gws-navigation-start");
+  }
+
+  return CONTINUE_OBSERVING;
+}
 
 page_load_metrics::PageLoadMetricsObserver::ObservePolicy
 GWSPageLoadMetricsObserver::OnCommit(
