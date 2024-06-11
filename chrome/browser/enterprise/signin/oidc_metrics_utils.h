ebf880f5ba9724ef8a966f03b8e27ba4418e76c6
Zonghan Xu <xzonghan@google.com>
2024-06-11 21:59:45 +0000

    [OIDC Enrollment] Add testing for OIDC Guest/OTR mode change (from crrev.com/c/5604860)
    Add missing test coverage for changes from crrev.com/c/5604860, where we allow OIDC enrollment to skip OTR profiles to prevent crash.
    
    Also add a similar check for Guest session profiles.
    
    Bug: 335677156
    
    Change-Id: Ib985a01730ceef536cfcac675842d0eff46877f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618251
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313695}

--- chrome/browser/enterprise/signin/oidc_metrics_utils.h
+++ chrome/browser/enterprise/signin/oidc_metrics_utils.h
@@ -43,7 +43,8 @@ enum class OidcInterceptionResult {
   kInvalidUrlOrTokens = 2,
   kConsetDialogRejected = 3,
   kFailedToRegisterProfile = 4,
-  kMaxValue = kFailedToRegisterProfile,
+  kInvalidProfile = 5,
+  kMaxValue = kInvalidProfile,
 };
 
 // Outcomes of the OIDC profile enrolllment flow after profile registration.
