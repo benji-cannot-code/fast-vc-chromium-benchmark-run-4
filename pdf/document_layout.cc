04f71c34b0964aec8c5ddb1eecc4a6bb0a804611
Lei Zhang <thestig@chromium.org>
2024-06-11 17:07:23 +0000

    Rename remaining inset_sizes variables in //pdf
    Consistently name them "insets", since they are of type gfx::Insets.
    
    Change-Id: Ic6309bb9b555fe622760b102ab6bd2cad0259a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622770
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313497}

--- pdf/document_layout.cc
+++ pdf/document_layout.cc
@@ -32,9 +32,9 @@ int GetWidestPageWidth(const std::vector<gfx::Size>& page_sizes) {
   return widest_page_width;
 }
 
-gfx::Rect InsetRect(const gfx::Rect& rect, const gfx::Insets& inset_sizes) {
+gfx::Rect InsetRect(const gfx::Rect& rect, const gfx::Insets& insets) {
   gfx::Rect inset_rect(rect);
-  inset_rect.Inset(inset_sizes);
+  inset_rect.Inset(insets);
   return inset_rect;
 }
 
