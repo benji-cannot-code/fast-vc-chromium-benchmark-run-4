0b106298cb08e3eb46b488eaa7eb6b1d115c3c80
Joel Hockey <joelhockey@chromium.org>
2024-06-11 02:31:13 +0000

    Add UMAs for how many apps are installed and uninstalled in PWAM
    WebApp.Preinstalled.Install
    WebApp.Preinstalled.UninstallTotalCount
    WebApp.Preinstalled.UninstallSourceRemovedCount
    WebApp.Preinstalled.UninstallAppRemovedCount
    record the number of apps in the SynchronizeInstalledApps() callback
    install_results and uninstall_results fields.
    
    These UMAs allow us to check that no apps are being uinstalled when
    feature PreinstalledWebAppsCoreOnly is enabled. We expect to see:
    InstallCount go from roughly 15 (depends on board) to 6
    UninstallTotalCount have a 1-off value of roughly 9 then stay at 0
    UninstallSourceRemovedCount match UninstallTotalCount
    UninstallAppRemovedCount always be zero.  Any non-zero value
    indicates an error that apps did not migrate correctly, and have been
    inadvertently uninstalled.
    
    Bug: b/341824938
    Change-Id: I77d49f00787a5e1769f91902d91f157ce646fb93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595377
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Glen Robertson <glenrob@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313201}

--- chrome/browser/web_applications/migrations/migrate_preinstalls_to_aps_browsertest.cc
+++ chrome/browser/web_applications/migrations/migrate_preinstalls_to_aps_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/auto_reset.h"
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "build/build_config.h"
@@ -148,10 +149,22 @@ class MigratePreinstallsToApsToggleTest : public InProcessBrowserTest {
     return result;
   }
 
+  void ValidateHistograms(int install, int source_removed, int app_removed) {
+    histograms_.ExpectUniqueSample("WebApp.Preinstalled.InstallCount", install,
+                                   1);
+    histograms_.ExpectUniqueSample(
+        "WebApp.Preinstalled.UninstallSourceRemovedCount", source_removed, 1);
+    histograms_.ExpectUniqueSample(
+        "WebApp.Preinstalled.UninstallAppRemovedCount", app_removed, 1);
+    histograms_.ExpectUniqueSample("WebApp.Preinstalled.UninstallTotalCount",
+                                   source_removed + app_removed, 1);
+  }
+
   Profile* profile() { return browser()->profile(); }
 
  private:
   base::test::ScopedFeatureList feature_list_;
+  base::HistogramTester histograms_;
 };
 
 // All apps should be installed as kDefault.
@@ -162,6 +175,7 @@ IN_PROC_BROWSER_TEST_F(MigratePreinstallsToApsToggleTest, PRE_TurnOn) {
       testing::UnorderedElementsAre(
           kGmailAppId, kGoogleDocsAppId, kGoogleDriveAppId, kGoogleSheetsAppId,
           kGoogleSlidesAppId, kYoutubeAppId, kGoogleCalendarAppId));
+  ValidateHistograms(/*install=*/10, /*source_removed=*/0, /*app_removed=*/0);
 }
 
 // Non-core apps (calendar) should be migrated to kApsDefault.
@@ -174,6 +188,7 @@ IN_PROC_BROWSER_TEST_F(MigratePreinstallsToApsToggleTest, TurnOn) {
   EXPECT_THAT(GetAppIdsWithSources(
                   WebAppManagementTypes({WebAppManagement::Type::kApsDefault})),
               testing::ElementsAre(kGoogleCalendarAppId));
+  ValidateHistograms(/*install=*/6, /*source_removed=*/1, /*app_removed=*/0);
 }
 
 // Core apps will be preinstalled, we must simulate APS installing calendar.
@@ -194,6 +209,7 @@ IN_PROC_BROWSER_TEST_F(MigratePreinstallsToApsToggleTest, PRE_Rollback) {
   EXPECT_THAT(GetAppIdsWithSources(
                   WebAppManagementTypes({WebAppManagement::Type::kApsDefault})),
               testing::ElementsAre(kGoogleCalendarAppId));
+  ValidateHistograms(/*install=*/6, /*source_removed=*/0, /*app_removed=*/0);
 }
 
 // Core apps are kDefault, calendar is kDefault and kApsDefault.
@@ -207,6 +223,7 @@ IN_PROC_BROWSER_TEST_F(MigratePreinstallsToApsToggleTest, Rollback) {
                   WebAppManagementTypes({WebAppManagement::Type::kDefault,
                                          WebAppManagement::Type::kApsDefault})),
               testing::ElementsAre(kGoogleCalendarAppId));
+  ValidateHistograms(/*install=*/8, /*source_removed=*/0, /*app_removed=*/0);
 }
 
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
