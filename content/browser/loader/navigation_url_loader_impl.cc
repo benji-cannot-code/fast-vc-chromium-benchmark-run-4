ffb8d86c24276096484cdefde386f23228db13d8
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-06-11 14:11:25 +0000

    Disable CheckParsedHeadersEquals in Release mode.
    There is a known failing test affecting DCHECK_IS_ON users.
    It is not easily fixable at the moment, so this patches downgrades it to
    only debug builds.
    
    Bug: chromium:40864513
    Change-Id: I8c033c964a8dce4ce51d55b59b890fa9ee388349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615671
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313389}

--- content/browser/loader/navigation_url_loader_impl.cc
+++ content/browser/loader/navigation_url_loader_impl.cc
@@ -395,7 +395,16 @@ bool IsSameOriginRedirect(const std::vector<GURL>& url_chain) {
   return previous_origin.IsSameOriginWith(url_chain[url_chain.size() - 1]);
 }
 
-#if DCHECK_IS_ON()
+// TODO(https://crbug.com/346000235) there is a known failure with extensions
+// See test: ExtensionWebRequestApiTestWithContextType.HSTSUpgradeAfterRedirect
+// We still want to check this in debug mode to avoid further regressions, but
+// because it was affecting to many developers with DCHECK_IS_ON() this was
+// downgraded as a DEBUG only check.
+// After getting the approval to rewrite the CSP parser in Rust, we should be
+// able to remove "pre-parsing" of CSP in the network process and directly
+// parse them later in the browser process. This will make this check to become
+// unnecessary.
+#ifndef NDEBUG
 void CheckParsedHeadersEquals(const network::mojom::ParsedHeadersPtr& lhs,
                               const network::mojom::ParsedHeadersPtr& rhs,
                               const GURL& url) {
@@ -464,7 +473,7 @@ void CheckParsedHeadersEquals(const network::mojom::ParsedHeadersPtr& lhs,
          "field does not match. Please add a DCHECK before this one "
          "checking for the missing field.";
 }
-#endif
+#endif  // NDEBUG
 
 }  // namespace
 
@@ -1413,7 +1422,7 @@ void NavigationURLLoaderImpl::ParseHeaders(
   // - ServiceWorker
   // - WebUI
   if (head->parsed_headers) {
-#if DCHECK_IS_ON()
+#ifndef NDEBUG
     // In debug mode, force reparsing the headers and check that they match.
     auto check = [](base::OnceClosure continuation,
                     network::mojom::URLResponseHead* head, GURL url,
@@ -1424,9 +1433,9 @@ void NavigationURLLoaderImpl::ParseHeaders(
     GetNetworkService()->ParseHeaders(
         url, head->headers,
         base::BindOnce(check, std::move(continuation), head, url));
-#else
+#else   // NDEBUG
     std::move(continuation).Run();
-#endif
+#endif  // NDEBUG
     return;
   }
 
