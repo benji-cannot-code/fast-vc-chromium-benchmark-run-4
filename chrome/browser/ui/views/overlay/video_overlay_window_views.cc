71a4b396741fdecedf66ba82e73bd01a2b4a62aa
Tommy Steimel <steimel@chromium.org>
2024-06-11 14:44:21 +0000

    [video-pip] Avoid unwanted pausing in more cases
    In crrev.com/c/3232381, we added logic to video picture-in-picture's
    close button to only pause the video when the pause action is actually
    available. However, there are a couple of other ways to fire the
    close-and-pause logic, and those are not protected.
    
    This CL changes those other places to ensure that the pause action is
    available before pausing.
    
    Bug: 3232381
    Change-Id: Ie1b70336a11fc6bf7760c10678c70f123c7ceaac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582484
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313409}

--- chrome/browser/ui/views/overlay/video_overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views.cc
@@ -550,10 +550,7 @@ void VideoOverlayWindowViews::OnKeyEvent(ui::KeyEvent* event) {
 #if BUILDFLAG(IS_WIN)
   if (event->type() == ui::ET_KEY_PRESSED && event->IsAltDown() &&
       event->key_code() == ui::VKEY_F4) {
-    PictureInPictureWindowManager::GetInstance()
-        ->ExitPictureInPictureViaWindowUi(
-            PictureInPictureWindowManager::UiBehavior::
-                kCloseWindowAndPauseVideo);
+    CloseAndPauseIfAvailable();
     event->SetHandled();
   }
 #endif  // BUILDFLAG(IS_WIN)
@@ -807,20 +804,9 @@ void VideoOverlayWindowViews::SetUpViews() {
   auto video_view = std::make_unique<views::View>();
   auto controls_scrim_view = std::make_unique<ControlsBackgroundView>();
   auto controls_container_view = std::make_unique<views::View>();
-  auto close_controls_view =
-      std::make_unique<CloseImageButton>(base::BindRepeating(
-          [](VideoOverlayWindowViews* overlay) {
-            // Only pause the video if play/pause is available.
-            const bool should_pause_video = overlay->show_play_pause_button_;
-            PictureInPictureWindowManager::GetInstance()
-                ->ExitPictureInPictureViaWindowUi(
-                    should_pause_video
-                        ? PictureInPictureWindowManager::UiBehavior::
-                              kCloseWindowAndPauseVideo
-                        : PictureInPictureWindowManager::UiBehavior::
-                              kCloseWindowOnly);
-          },
-          base::Unretained(this)));
+  auto close_controls_view = std::make_unique<CloseImageButton>(
+      base::BindRepeating(&VideoOverlayWindowViews::CloseAndPauseIfAvailable,
+                          base::Unretained(this)));
   std::unique_ptr<OverlayWindowMinimizeButton> minimize_button;
   if (base::FeatureList::IsEnabled(
           media::kVideoPictureInPictureMinimizeButton)) {
@@ -1518,10 +1504,7 @@ void VideoOverlayWindowViews::OnGestureEvent(ui::GestureEvent* event) {
     controller_->SkipAd();
     event->SetHandled();
   } else if (GetCloseControlsBounds().Contains(event->location())) {
-    PictureInPictureWindowManager::GetInstance()
-        ->ExitPictureInPictureViaWindowUi(
-            PictureInPictureWindowManager::UiBehavior::
-                kCloseWindowAndPauseVideo);
+    CloseAndPauseIfAvailable();
     event->SetHandled();
   } else if (GetMinimizeControlsBounds().Contains(event->location())) {
     PictureInPictureWindowManager::GetInstance()
@@ -1618,6 +1601,15 @@ void VideoOverlayWindowViews::TogglePlayPause() {
   // the media player yet.
   bool is_active = controller_->TogglePlayPause();
   play_pause_controls_view_->SetPlaybackState(is_active ? kPlaying : kPaused);
+}
+
+void VideoOverlayWindowViews::CloseAndPauseIfAvailable() {
+  // Only pause the video if play/pause is available.
+  const bool should_pause_video = !!show_play_pause_button_;
+  PictureInPictureWindowManager::GetInstance()->ExitPictureInPictureViaWindowUi(
+      should_pause_video
+          ? PictureInPictureWindowManager::UiBehavior::kCloseWindowAndPauseVideo
+          : PictureInPictureWindowManager::UiBehavior::kCloseWindowOnly);
 }
 
 PlaybackImageButton*
