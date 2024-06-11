0c460b087bafbf0c2bfd01dacac59277e5c038f5
David Black <dmblack@google.com>
2024-06-11 19:18:03 +0000

    Remove remaining `HoldingSpaceWallpaperNudge` code.
    The experiment has been turned down and all associated code is being
    removed. Note that this CL removes associated histograms and prefs.
    
    OBSOLETE_HISTOGRAMS=Removes all histograms associated with feature.
    
    Bug: b:274659315
    Change-Id: Id8ceb80fa707f5df660d7ef20ace30f12908a308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5528222
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313587}

--- ash/user_education/user_education_controller.cc
+++ ash/user_education/user_education_controller.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "ash/session/session_controller_impl.h"
 #include "ash/shell.h"
-#include "ash/user_education/holding_space_wallpaper_nudge/holding_space_wallpaper_nudge_prefs.h"
 #include "ash/user_education/user_education_delegate.h"
 #include "ash/user_education/user_education_feature_controller.h"
 #include "ash/user_education/user_education_util.h"
@@ -52,7 +51,6 @@ UserEducationController* UserEducationController::Get() {
 // static
 void UserEducationController::RegisterProfilePrefs(
     PrefRegistrySimple* registry) {
-  holding_space_wallpaper_nudge_prefs::RegisterProfilePrefs(registry);
   welcome_tour_prefs::RegisterProfilePrefs(registry);
 }
 
