3036a05bfa452d3f44f9fb89287d532de685e116
Ilya Nikolaevskiy <ilnik@chromium.org>
2024-06-11 13:58:52 +0000

    Use WeakPtr in VideoCaptureDeviceFactoryLinux instead of Unretained(this)
    Fixed: 346044228
    Change-Id: Ia0e5ceb9115c8f23770dd3a2ffa5bef1d9a1c46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621333
    Commit-Queue: Ilya Nikolaevskiy <ilnik@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313383}

--- media/capture/video/linux/video_capture_device_factory_linux.h
+++ media/capture/video/linux/video_capture_device_factory_linux.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_CAPTURE_VIDEO_LINUX_VIDEO_CAPTURE_DEVICE_FACTORY_LINUX_H_
 #define MEDIA_CAPTURE_VIDEO_LINUX_VIDEO_CAPTURE_DEVICE_FACTORY_LINUX_H_
 
+#include "base/memory/weak_ptr.h"
+
 #if defined(WEBRTC_USE_PIPEWIRE)
 #include "media/capture/video/linux/video_capture_device_factory_webrtc.h"
 #endif  // defined(WEBRTC_USE_PIPEWIRE)
@@ -45,6 +47,9 @@ class CAPTURE_EXPORT VideoCaptureDeviceFactoryLinux
 #if defined(WEBRTC_USE_PIPEWIRE)
   std::unique_ptr<VideoCaptureDeviceFactoryWebRtc> webrtc_factory_;
 #endif  // defined(WEBRTC_USE_PIPEWIRE)
+
+  // Must be the last member.
+  base::WeakPtrFactory<VideoCaptureDeviceFactoryLinux> weak_ptr_factory_{this};
 };
 
 }  // namespace media
