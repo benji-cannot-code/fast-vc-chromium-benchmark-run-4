0272f83399075e1dbb0eaa1a3de1737b412bdf0f
Saifuddin Hitawala <hitawala@chromium.org>
2024-06-12 16:24:30 +0000

    [media] Remove SharedImageBufferHandleSet::texture_target
    Remove texture_target param from SharedImageBufferHandleSet as it should
    always use that from shared image.
    
    Bug: None
    Change-Id: I95059859a7d85485196d725ebfd1dbe7b649142a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608295
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314045}

--- third_party/blink/renderer/platform/video_capture/video_capture_impl.cc
+++ third_party/blink/renderer/platform/video_capture/video_capture_impl.cc
@@ -130,9 +130,6 @@ struct VideoCaptureImpl::BufferContext
   const gpu::SyncToken& shared_image_sync_token() const {
     return shared_image_sync_token_;
   }
-  uint32_t shared_image_texture_target() const {
-    return shared_image_texture_target_;
-  }
   media::GpuVideoAcceleratorFactories* gpu_factories() const {
     return gpu_factories_;
   }
@@ -219,7 +216,6 @@ struct VideoCaptureImpl::BufferContext
     shared_image_ = gpu::ClientSharedImage::ImportUnowned(
         shared_image_handles->shared_image);
     shared_image_sync_token_ = shared_image_handles->sync_token;
-    shared_image_texture_target_ = shared_image_handles->texture_target;
   }
 
   void InitializeFromGpuMemoryBufferHandle(
@@ -264,7 +260,6 @@ struct VideoCaptureImpl::BufferContext
   // Only valid for |buffer_type_ == SHARED_IMAGE_HANDLES|.
   scoped_refptr<gpu::ClientSharedImage> shared_image_;
   gpu::SyncToken shared_image_sync_token_;
-  uint32_t shared_image_texture_target_;
 
   // The following is for |buffer_type == GPU_MEMORY_BUFFER_HANDLE|.
 
@@ -379,7 +374,7 @@ VideoCaptureImpl::CreateVideoFrameInitData(
               video_frame_init_data.ready_buffer->info->pixel_format,
               buffer_context->shared_image(),
               buffer_context->shared_image_sync_token(),
-              buffer_context->shared_image_texture_target(),
+              buffer_context->shared_image()->GetTextureTarget(),
               media::VideoFrame::ReleaseMailboxCB(),
               gfx::Size(video_frame_init_data.ready_buffer->info->coded_size),
               gfx::Rect(video_frame_init_data.ready_buffer->info->visible_rect),
