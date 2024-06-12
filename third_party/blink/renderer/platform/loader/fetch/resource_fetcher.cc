3158051d53d3fb014b0a85f1c99ffa964581db98
Nidhi Jaju <nidhijaju@chromium.org>
2024-06-12 00:59:22 +0000

    Add histogram for time spent on requesting transparent placeholder resource
    This CL adds the following histogram:
    - Blink.Fetch.RequestResourceTime2.TransparentPlaceholderImage
    
    This also changes the behavior to check if all resource requests are
    transparent placeholder images, not only if the feature flag is
    enabled.
    
    Bug: 41496436
    Change-Id: I1d2fd90bff340ce852e51ae74342f4100eee4417
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5620067
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313755}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -979,8 +979,9 @@ Resource* ResourceFetcher::CreateResourceForStaticData(
 
   ResourceResponse response;
   scoped_refptr<SharedBuffer> data;
-  if (params.GetResourceRequest().GetKnownTransparentPlaceholderImageIndex() !=
-      kNotFound) {
+  if (blink::features::IsSimplifyLoadingTransparentPlaceholderImageEnabled() &&
+      (params.GetResourceRequest().GetKnownTransparentPlaceholderImageIndex() !=
+       kNotFound)) {
     // Skip the construction of `data`, since we won't use it.
 
     // We can defer the construction of `response`, but that would result in
@@ -1043,6 +1044,8 @@ Resource* ResourceFetcher::CreateResourceForStaticData(
 
       // We should only reach here on the transparent placeholder image
       // fast-path.
+      CHECK(blink::features::
+                IsSimplifyLoadingTransparentPlaceholderImageEnabled());
       CHECK_NE(params.GetResourceRequest()
                    .GetKnownTransparentPlaceholderImageIndex(),
                kNotFound);
@@ -1180,8 +1183,9 @@ std::optional<ResourceRequestBlockedReason> ResourceFetcher::PrepareRequest(
     const ResourceFactory& factory,
     WebScopedVirtualTimePauser& virtual_time_pauser) {
   ResourceRequest& resource_request = params.MutableResourceRequest();
-  if (resource_request.GetKnownTransparentPlaceholderImageIndex() !=
-      kNotFound) {
+  if (blink::features::IsSimplifyLoadingTransparentPlaceholderImageEnabled() &&
+      (resource_request.GetKnownTransparentPlaceholderImageIndex() !=
+       kNotFound)) {
     // Since we are not actually sending the request to the server,
     // we skip construction of the ResourceRequest for performance.
     // TODO(crbug.com/41496436): This breaks all observers which were notified
@@ -1303,6 +1307,12 @@ Resource* ResourceFetcher::RequestResource(FetchParameters& params,
     if (params.Url().ProtocolIsData()) {
       base::UmaHistogramMicrosecondsTimes(
           "Blink.Fetch.RequestResourceTime2.Data", elapsed);
+      if (params.GetResourceRequest()
+              .GetKnownTransparentPlaceholderImageIndex() != kNotFound) {
+        base::UmaHistogramMicrosecondsTimes(
+            "Blink.Fetch.RequestResourceTime2.TransparentPlaceholderImage",
+            elapsed);
+      }
     }
     if (params.IsSpeculativePreload() || params.IsLinkPreload()) {
       base::UmaHistogramMicrosecondsTimes(
