aa9064f28d80ab73ecb94cb78f7b1cf7be488401
David Pennington <dpenning@chromium.org>
2024-06-12 21:11:26 +0000

    [TabSearchPosition] add a feature flag for showing the setting.
    A feature flag is added that allows showing the setting in the settings
    page. If this flag is false, even if the pref is assigned, the value
    will not be read when placing the tab search button.
    
    CanShowTabSearchPositionSetting doesnt benefit from writing a unittest, which is why we have a low coverage reason.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 338622527
    Change-Id: I7d4df2aa8dd4f344a20c5aaf1d018f35f1ed4745
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622696
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314240}

---
+++ chrome/browser/ui/tabs/features.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/tabs/features.h"
+
+#include "base/feature_list.h"
+#include "chrome/browser/buildflags.h"
+
+namespace tabs {
+
+BASE_FEATURE(kTabSearchPositionSetting,
+             "TabSearchPositionSetting",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+bool CanShowTabSearchPositionSetting() {
+// Mac and other platforms will always have the tab search position in the
+// correct location, cros/linux/win git the user the option to change.
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN)
+  return base::FeatureList::IsEnabled(kTabSearchPositionSetting);
+#else
+  return false;
+#endif
+}
+
+}  // namespace tabs
