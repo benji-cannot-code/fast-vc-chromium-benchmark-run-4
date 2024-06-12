0f22b06af1209fc5b9eedca0e02bb7ed7b745022
Bo Liu <boliu@chromium.org>
2024-06-12 01:54:07 +0000

    Remove is_resourceless_software_draw_with_scroll_or_animation
    It is unused with a comment to either remove or investigate if its still
    useful. It was used to make webview's software draw scroll performance
    reasonable, but webview software draw is considered deprecreated.
    Clearly it's does not have impact if it's been unused for a long time.
    
    Change-Id: I6e85eaebd251d2b1d122f7d3d30d04856e47a09c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622156
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313760}

--- components/viz/service/display/software_renderer.h
+++ components/viz/service/display/software_renderer.h
@@ -41,10 +41,6 @@ class VIZ_SERVICE_EXPORT SoftwareRenderer : public DirectRenderer {
 
   void SwapBuffers(SwapFrameData swap_frame_data) override;
 
-  void SetDisablePictureQuadImageFiltering(bool disable) {
-    disable_picture_quad_image_filtering_ = disable;
-  }
-
  protected:
   bool CanPartialSwap() override;
   void UpdateRenderPassTextures(
@@ -113,8 +109,6 @@ class VIZ_SERVICE_EXPORT SoftwareRenderer : public DirectRenderer {
 
   // A map from RenderPass id to the bitmap used to draw the RenderPass from.
   base::flat_map<AggregatedRenderPassId, SkBitmap> render_pass_bitmaps_;
-
-  bool disable_picture_quad_image_filtering_ = false;
 
   bool is_scissor_enabled_ = false;
   gfx::Rect scissor_rect_;
