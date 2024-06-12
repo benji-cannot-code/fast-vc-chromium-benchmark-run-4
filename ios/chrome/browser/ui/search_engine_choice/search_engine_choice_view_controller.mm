f52ca81f5cd62fc1362846d5744cad7f0acf8dd1
Jérôme Lebel <jlebel@chromium.org>
2024-06-12 08:47:37 +0000

    [iOS] Move Chrome icon to ios_internal using symbol_names.h/mm
    This series of patches moves Chrome icon, used for the search engine
    choice dialog, to ios_internal.
    This dialog should not be used in Chromium, but it is used in
    EGTests.
    
    This patch removes the old icon and switch the implementation to use
    symbole name constants.
    
    * crrev.com/c/5599183 add Chromium icon and variables
    * crrev.com/i/7359996 add Chrome icon
    > crrev.com/c/5600599 remove old icon, switch implementation
    
    Bug: b/331562136
    Change-Id: Ic82e0b6dce0c4a92084aa646f3e8dc6f958aac22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600599
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1313865}

--- ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.mm
+++ ios/chrome/browser/ui/search_engine_choice/search_engine_choice_view_controller.mm
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Chrome logo with 40pt size.
-NSString* const kChromeIcon40pt = @"chrome_icon_40";
 // Space between the Chrome logo and the top of the screen.
 constexpr CGFloat kLogoTopMargin = 24.;
 // Logo dimensions.
@@ -260,7 +258,11 @@ CGFloat ConvertVerticalCoordonateWithMainViewReference(UIView* mainView,
   // Add logo image.
   // Need to use a regular png instead of custom symbol to have a better control
   // on the size and the margin of the logo.
-  UIImage* logoImage = [UIImage imageNamed:kChromeIcon40pt];
+#if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
+  UIImage* logoImage = [UIImage imageNamed:kChromeSearchEngineChoiceIcon];
+#else
+  UIImage* logoImage = [UIImage imageNamed:kChromiumSearchEngineChoiceIcon];
+#endif
   UIImageView* logoImageView = [[UIImageView alloc] initWithImage:logoImage];
   [scrollContentView addSubview:logoImageView];
   logoImageView.translatesAutoresizingMaskIntoConstraints = NO;
