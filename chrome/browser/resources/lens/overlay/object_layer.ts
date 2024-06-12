7575c18ffadc97e58c7b5b7375b607f396168679
Juan Mojica <juanmojica@google.com>
2024-06-12 04:30:13 +0000

    [lensoverlay] Shader should fade out after region focus.
    The cursor transition should only happen when the previous shimmer
    controller was NONE / steady state. The circles should fade out in place
    otherwise.
    
    Demo: http://shortn/_ohkjjn040P
    
    Change-Id: I505e8e6c0f840ae3e3c07ac7c33d7f2a05ea2f59
    Bug: b:345532379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622772
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313823}

--- chrome/browser/resources/lens/overlay/object_layer.ts
+++ chrome/browser/resources/lens/overlay/object_layer.ts
@@ -28,6 +28,8 @@ import {toPercent} from './values_converter.js';
 // The percent of the selection layer width and height the object needs to take
 // up to be considered full page.
 const FULLSCREEN_OBJECT_THRESHOLD_PERCENT = 0.95;
+// The transition duration for the fade out animation into the cursor state.
+const CURSOR_FADE_OUT_TRANSITION_DURATION = 150;
 
 // Returns true if the object has a valid bounding box and is renderable by the
 // ObjectLayer.
@@ -132,6 +134,8 @@ export class ObjectLayerElement extends PolymerElement {
   private preciseHighlight: boolean;
   // The overlay theme.
   private theme: OverlayTheme;
+  private fadeOutAnimations: Animation[] = [];
+  private fadeOutTimeoutIds: number[] = [];
 
   private readonly router: LensPageCallbackRouter =
       BrowserProxyImpl.getInstance().callbackRouter;
@@ -195,6 +199,7 @@ export class ObjectLayerElement extends PolymerElement {
   }
 
   private onSegmentationHovered(object: OverlayObject) {
+    this.clearAndCancelAnimation();
     this.drawObject(this.context, object);
     this.focusShimmer(object);
     this.dispatchEvent(new CustomEvent<CursorData>('set-cursor', {
@@ -212,7 +217,6 @@ export class ObjectLayerElement extends PolymerElement {
       bubbles: true,
       composed: true,
     }));
-
     // Only show the pointer if the object has a segmentation mask.
     const hasSegmentationMask = object.geometry!.segmentationPolygon.length > 0;
     if (hasSegmentationMask) {
@@ -221,7 +225,14 @@ export class ObjectLayerElement extends PolymerElement {
   }
 
   private onSegmentationUnhovered() {
-    this.clearCanvas(this.context);
+    this.fadeOutAnimations.push(
+        this.$.objectSelectionCanvas.animate({opacity: 0}, {
+          duration: CURSOR_FADE_OUT_TRANSITION_DURATION,
+          fill: 'none',
+        }));
+    this.fadeOutTimeoutIds.push(setTimeout(() => {
+      this.clearCanvas(this.context);
+    }, CURSOR_FADE_OUT_TRANSITION_DURATION));
     unfocusShimmer(this, ShimmerControlRequester.SEGMENTATION);
     this.dispatchEvent(new CustomEvent<CursorData>('set-cursor', {
       bubbles: true,
@@ -498,6 +509,21 @@ export class ObjectLayerElement extends PolymerElement {
       }
     }
     return null;
+  }
+
+  // Clears any animation state currently present on the canvas.
+  private clearAndCancelAnimation() {
+    // Clear and cancel any animations.
+    for (let i = 0; i < this.fadeOutAnimations.length; i++) {
+      this.fadeOutAnimations[i].cancel();
+    }
+    this.fadeOutAnimations = [];
+
+    this.clearCanvas(this.context);
+    for (let i = 0; i < this.fadeOutTimeoutIds.length; i++) {
+      clearTimeout(this.fadeOutTimeoutIds[i]);
+    }
+    this.fadeOutTimeoutIds = [];
   }
 
   // Testing method to get the objects on the page.
