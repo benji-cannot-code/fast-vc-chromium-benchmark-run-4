9f8b64c84d3636b837a6b250137ac80ecce675a2
Jiaming Cheng <jiamingc@chromium.org>
2024-06-12 07:06:53 +0000

    [Mahi] Implement `OnLinkClick` in the Magicboost disclaimer view
    Bug: b/339044721
    Change-Id: I22ed603898e3715620877af57a2f6e5e1ffa39ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622705
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313849}

--- ash/system/magic_boost/magic_boost_disclaimer_view.cc
+++ ash/system/magic_boost/magic_boost_disclaimer_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/public/cpp/new_window_delegate.h"
 #include "ash/public/cpp/resources/grit/ash_public_unscaled_resources.h"
 #include "ash/shell.h"
 #include "ash/style/typography.h"
@@ -79,7 +80,9 @@ constexpr char kTestURL[] = "https://www.google.com";
 
 // Opens the passed in `url` in a new tab.
 void OnLinkClick(const std::string& url) {
-  // TODO(b/339044721): open the url in a new tab.
+  NewWindowDelegate::GetPrimary()->OpenUrl(
+      GURL(url), NewWindowDelegate::OpenUrlFrom::kUserInteraction,
+      NewWindowDelegate::Disposition::kNewForegroundTab);
 }
 
 views::StyledLabel::RangeStyleInfo GetLinkTextStyle() {
