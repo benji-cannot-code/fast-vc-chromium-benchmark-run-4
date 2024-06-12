015d43781af980d5c0ab3e4a5273bc8cd26de371
Keren Zhu <kerenzhu@chromium.org>
2024-06-12 18:04:09 +0000

    views: ensure widget sublevels on activation
    On Windows, the stacking levels among widgets are reset after the
    browser regains activation using Alt+Tab, this causes widgets to be
    misplaced in terms of their z-order sublevels. This patch fixes the
    issue by re-ordering the entire widget tree when any widget in the tree
    becomes active.
    
    Bug: 345271842
    Change-Id: If24f73c0d62701b973b50ef05f4ad462b5998601
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622196
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314127}

--- chrome/browser/ui/views/passwords/password_bubble_interactive_uitest.cc
+++ chrome/browser/ui/views/passwords/password_bubble_interactive_uitest.cc
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textarea/textarea.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/focus/focus_manager.h"
+#include "ui/views/test/widget_test.h"
 
 using base::Bucket;
 using net::test_server::BasicHttpResponse;
@@ -479,12 +480,16 @@ IN_PROC_BROWSER_TEST_F(PasswordBubbleInteractiveUiTest, AutoSigninNoFocus) {
 IN_PROC_BROWSER_TEST_F(PasswordBubbleInteractiveUiTest, LeakPromptHidesBubble) {
   ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
   SetupPendingPassword();
-  EXPECT_TRUE(IsBubbleShowing());
+  ASSERT_NE(PasswordBubbleViewBase::manage_password_bubble(), nullptr);
+  views::Widget* password_bubble =
+      PasswordBubbleViewBase::manage_password_bubble()->GetWidget();
+  ASSERT_NE(password_bubble, nullptr);
+  views::test::WidgetVisibleWaiter(password_bubble).Wait();
 
   GetController()->OnCredentialLeak(
       password_manager::CredentialLeakFlags::kPasswordSaved,
       GURL("https://example.com"), std::u16string(u"Eve"));
-  EXPECT_FALSE(IsBubbleShowing());
+  views::test::WidgetDestroyedWaiter(password_bubble).Wait();
 }
 
 class PasswordBubbleInteractiveUiTestWithExplicitBrowserSigninParam
