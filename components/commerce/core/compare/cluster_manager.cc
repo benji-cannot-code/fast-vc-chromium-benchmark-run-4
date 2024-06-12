f82436ee855db8045041e4019745e05beabca124
Min Qin <qinmin@chromium.org>
2024-06-12 20:37:36 +0000

    Implement GetComparableProduct using backend server
    Bug: 346332211
    Change-Id: I1f816973b1379b5e70ea1a687002d84d32074c9a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615180
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314222}

--- components/commerce/core/compare/cluster_manager.cc
+++ components/commerce/core/compare/cluster_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/barrier_callback.h"
+#include "base/containers/contains.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/commerce/core/commerce_utils.h"
 #include "components/commerce/core/compare/candidate_product.h"
@@ -354,12 +355,18 @@ void ClusterManager::RemoveObserver(Observer* observer) {
   observers_.RemoveObserver(observer);
 }
 
-void ClusterManager::GetComparableUrls(const std::set<GURL>& product_urls,
-                                       GetComparableUrlsCallback callback) {
-  cluster_server_proxy_->GetComparableUrls(
-      product_urls,
-      base::BindOnce(&ClusterManager::OnGetComparableUrls,
-                     weak_ptr_factory_.GetWeakPtr(), std::move(callback)));
+void ClusterManager::GetComparableProducts(
+    const EntryPointInfo& entry_point_info,
+    GetEntryPointInfoCallback callback) {
+  std::vector<uint64_t> product_cluster_ids;
+  for (const auto& kv : entry_point_info.similar_candidate_products) {
+    product_cluster_ids.push_back(kv.second);
+  }
+  cluster_server_proxy_->GetComparableProducts(
+      std::move(product_cluster_ids),
+      base::BindOnce(&ClusterManager::OnGetComparableProducts,
+                     weak_ptr_factory_.GetWeakPtr(), entry_point_info,
+                     std::move(callback)));
 }
 
 std::set<GURL> ClusterManager::FindSimilarCandidateProducts(
@@ -465,27 +472,33 @@ void ClusterManager::OnProductInfoFetchedForSimilarUrls(
       title ? title.value() : "", std::move(map)));
 }
 
-void ClusterManager::OnGetComparableUrls(
-    GetComparableUrlsCallback callback,
-    bool success,
-    const std::set<GURL>& comparable_urls) {
-  if (!success) {
-    std::move(callback).Run(std::set<GURL>());
-    return;
-  }
-
-  std::set<GURL> open_urls;
+void ClusterManager::OnGetComparableProducts(
+    const EntryPointInfo& entry_point_info,
+    GetEntryPointInfoCallback callback,
+    const std::vector<uint64_t>& cluster_product_ids) {
+  std::map<GURL, uint64_t> product_cluster_id_map;
   const std::vector<UrlInfo> url_infos = get_open_url_infos_cb_.Run();
-  for (const auto& url : comparable_urls) {
-    for (const UrlInfo& url_info : url_infos) {
-      if (url == url_info.url) {
-        open_urls.emplace(url);
-        break;
+  for (const auto& kv : entry_point_info.similar_candidate_products) {
+    // If the product Id cannot be clustered, skip it.
+    if (!base::Contains(cluster_product_ids, kv.second)) {
+      continue;
+    }
+
+    // Only add URLs that are still open.
+    for (const auto& url_info : url_infos) {
+      if (kv.first == url_info.url) {
+        product_cluster_id_map.emplace(kv.first, kv.second);
       }
     }
   }
 
-  std::move(callback).Run(std::move(open_urls));
+  if (product_cluster_id_map.empty()) {
+    std::move(callback).Run(std::nullopt);
+    return;
+  }
+
+  std::move(callback).Run(EntryPointInfo(entry_point_info.title,
+                                         std::move(product_cluster_id_map)));
 }
 
 bool ClusterManager::IsSetEligibleForClustering(const base::Uuid& uuid,
--- components/commerce/core/compare/cluster_manager.h
+++ components/commerce/core/compare/cluster_manager.h
@@ -35,7 +35,6 @@ class ClusterManager : public ProductSpecificationsSet::Observer {
       base::RepeatingCallback<const std::vector<UrlInfo>()>;
   using GetEntryPointInfoCallback =
       base::OnceCallback<void(std::optional<EntryPointInfo>)>;
-  using GetComparableUrlsCallback = base::OnceCallback<void(std::set<GURL>)>;
 
   class Observer : public base::CheckedObserver {
    public:
@@ -96,9 +95,9 @@ class ClusterManager : public ProductSpecificationsSet::Observer {
   std::vector<GURL> FindSimilarCandidateProductsForProductGroup(
       const base::Uuid& uuid);
 
-  // Finds comparable URLs for a list of URLs.
-  void GetComparableUrls(const std::set<GURL>& product_urls,
-                         GetComparableUrlsCallback callback);
+  // Finds comparable products from an EntryPointInfo.
+  void GetComparableProducts(const EntryPointInfo& entry_point_info,
+                             GetEntryPointInfoCallback callback);
 
   // Registers an observer for cluster manager.
   void AddObserver(Observer* observer);
@@ -132,9 +131,10 @@ class ClusterManager : public ProductSpecificationsSet::Observer {
   // URLs doesn't include the `product_url`.
   std::set<GURL> FindSimilarCandidateProducts(const GURL& product_url);
 
-  void OnGetComparableUrls(GetComparableUrlsCallback callback,
-                           bool success,
-                           const std::set<GURL>& comparable_urls);
+  void OnGetComparableProducts(
+      const EntryPointInfo& entry_point_info,
+      GetEntryPointInfoCallback callback,
+      const std::vector<uint64_t>& cluster_product_ids);
 
   void OnProductInfoFetchedForSimilarUrls(
       std::optional<std::string> title,
