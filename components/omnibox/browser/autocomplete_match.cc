3ab48411eb5b40a92f7f4679d872fe208e8862d1
manukh <manukh@chromium.org>
2024-06-12 21:56:49 +0000

    [omnibox][history-embedding] Classification and queueing provider bugs
    Fixes 2 bugs with the history embeddings provider:
    
    1) When the input text was empty, the produced match classifications
       would be invalid; e.g. [(offset 0, style 2), (0, 0)]. This was
       because the empty input always prefixes the match text causing a
       length-0 classification to be added.
    
    2) The provider now clears matches when the async search response is
       received instead of requested. Otherwise, sending a 2nd search
       request before the 1st was returned would end up showing results for
       both responses. Not clearing on request also helps avoid flickering.
    
    Bug: 332360110
    Change-Id: Id7da7e27af71fecbcac105da31ec6e70c60a8dbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5627152
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314282}

--- components/omnibox/browser/autocomplete_match.cc
+++ components/omnibox/browser/autocomplete_match.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/omnibox/browser/autocomplete_match.h"
 
+#include <string>
 #include <string_view>
 #include <vector>
 
@@ -2161,15 +2162,24 @@ void AutocompleteMatch::ValidateClassifications(
     const std::u16string& text,
     const ACMatchClassifications& classifications,
     const std::string& provider_name) {
+#if DCHECK_IS_ON()
+  std::string debug_string = " text: " + base::UTF16ToUTF8(text) +
+                             ", provider: " + provider_name +
+                             ", classifications (offset, style): ";
+  for (const auto& classification : classifications) {
+    debug_string += " (" + base::NumberToString(classification.offset) + ", " +
+                    base::NumberToString(classification.style) + ")";
+  }
+
   if (text.empty()) {
-    DCHECK(classifications.empty());
+    DCHECK(classifications.empty()) << debug_string;
     return;
   }
 
   // The classifications should always cover the whole string.
-  DCHECK(!classifications.empty()) << "No classification for \"" << text << '"';
+  DCHECK(!classifications.empty()) << " No classification;" << debug_string;
   DCHECK_EQ(0U, classifications[0].offset)
-      << "Classification misses beginning for \"" << text << '"';
+      << " Classification misses beginning;" << debug_string;
   if (classifications.size() == 1)
     return;
 
@@ -2177,13 +2187,10 @@ void AutocompleteMatch::ValidateClassifications(
   size_t last_offset = classifications[0].offset;
   for (auto i(classifications.begin() + 1); i != classifications.end(); ++i) {
     DCHECK_GT(i->offset, last_offset)
-        << " Classification for \"" << text << "\" with offset of " << i->offset
-        << " is unsorted in relation to last offset of " << last_offset
-        << ". Provider: " << provider_name << ".";
+        << " Unsorted classification;" << debug_string;
     DCHECK_LT(i->offset, text.length())
-        << " Classification of [" << i->offset << "," << text.length()
-        << "] is out of bounds for \"" << text
-        << "\". Provider: " << provider_name << ".";
+        << " Out of bounds classification;" << debug_string;
     last_offset = i->offset;
   }
+#endif  // DCHECK_IS_ON()
 }
