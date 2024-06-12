f2dd431b006d2bf87d20915931f6be00e0bf9485
Siddhartha S <ssid@chromium.org>
2024-06-12 20:04:30 +0000

    [tab resume] Make the random default result disabled by default
    The existing resumption would use the default model and not
    for training, so return recent result for these cases.
    
    Change-Id: Ic0d56059c2960739a686a2ca95b66432b9086285
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5627660
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314204}

--- components/segmentation_platform/embedder/default_model/url_visit_resumption_ranker.cc
+++ components/segmentation_platform/embedder/default_model/url_visit_resumption_ranker.cc
@@ -52,7 +52,7 @@ URLVisitResumptionRanker::URLVisitResumptionRanker()
       use_random_score_(base::GetFieldTrialParamByFeatureAsBool(
           features::kSegmentationPlatformURLVisitResumptionRanker,
           "use_random_score",
-          true)) {}
+          false)) {}
 
 URLVisitResumptionRanker::~URLVisitResumptionRanker() = default;
 
