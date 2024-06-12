b3bfdd93592a261e095423b8135cf33780a702dc
Foromo Daniel Soromou <fosoromo@microsoft.com>
2024-06-12 22:39:00 +0000

    [File System Change Observer] Ignore changes to swap files
    Ignore changes that occur to the swap file.
    
    The current implementation relies on the file extension.
    
    Bug: 321980149, 40283778
    Change-Id: Ibe4ae01d4d3fb20c916b49022e49dd5a9e111834
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5540273
    Commit-Queue: Daniel Soromou <fosoromo@microsoft.com>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314309}

--- content/browser/file_system_access/file_system_access_watcher_manager.cc
+++ content/browser/file_system_access/file_system_access_watcher_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/check.h"
+#include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
@@ -141,10 +142,13 @@ void FileSystemAccessWatcherManager::OnRawChange(
     const FileSystemAccessChangeSource::ChangeInfo& change_info) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  // TODO(crbug.com/40283778): Ignore changes caused by API
-  // implementation details, such as writes to swap files.
-  //
   // TODO(crbug.com/40268906): Batch changes.
+
+  // Changes to swap files are implementation details and should be ignored.
+  if (changed_url.virtual_path().FinalExtension() ==
+      FILE_PATH_LITERAL(".crswap")) {
+    return;
+  }
 
   if (error) {
     // TODO(crbug.com/40105284): Instead of filtering an errored change,
