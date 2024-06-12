08b25eeba3579b8620d5c2363632699298f1b026
Benjamin Beaudry <benjamin.beaudry@microsoft.com>
2024-06-12 17:58:26 +0000

    [views-ax] Add IgnoreMissingWidgetForTestingScopedSetter back
    In CL:5553088, we moved the check for the missing widget to
    ViewAXPlatformNodeDelegate::GetData but for some reason didn't move the
    ignore_missing_widget_for_testing_ condition, used in tests that don't
    have a widget to bypass this check. This led to a test failure in
    CL:5551315, so adding it back now.
    
    The test in CL:5551315 was failing because it did not have a widget
    and was returning early instead of computing the accessible data.
    Since the test's purpose is to test accessibility properties, we must
    ignore the fact that it doesn't have a widget.
    
    Bug: 325137417
    Change-Id: I03ac822a1e85e85fe08907c14890f595e655c038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626309
    Auto-Submit: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1314124}

--- ui/views/accessibility/view_accessibility.h
+++ ui/views/accessibility/view_accessibility.h
@@ -411,6 +411,8 @@ class VIEWS_EXPORT ViewAccessibility : public WidgetObserver {
   // Used for testing. Called every time an accessibility event is fired.
   AccessibilityEventsCallback accessibility_events_callback_;
 
+  bool ignore_missing_widget_for_testing_ = false;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(ViewTest, PauseAccessibilityEvents);
   FRIEND_TEST_ALL_PREFIXES(ViewTest,
@@ -497,8 +499,6 @@ class VIEWS_EXPORT ViewAccessibility : public WidgetObserver {
   // Prevents accessibility events from being fired during initialization of
   // the owning View.
   bool pause_accessibility_events_ = false;
-
-  bool ignore_missing_widget_for_testing_ = false;
 
   bool is_widget_closed_ = false;
 
