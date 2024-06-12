d5ecfcb94c03c4ae35e164d01d7ac45835d4589b
Rubin Deliallisi <rubindl@chromium.org>
2024-06-12 10:49:16 +0000

    [SHoA] Add notifications preference with 3-dot menu support
    - Add a new preference for notifications that contains a 3-dot button,
    which expands a selection menu.
    - Integrate the new preference into the notifications fragment.
    - Populate the permissions fragment and implement the preference menu
    callbacks.
    
    Bug: 324562205
    Change-Id: I6cd87a0ec71bf2a24779297f6079c6743d8ba67d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5614750
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313913}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/NotificationPermissions.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/NotificationPermissions.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.safety_hub;
 
+import androidx.annotation.VisibleForTesting;
+
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JniType;
 
@@ -21,8 +23,9 @@ public class NotificationPermissions {
         mNotificationCount = notificationCount;
     }
 
+    @VisibleForTesting
     @CalledByNative
-    private static NotificationPermissions create(
+    static NotificationPermissions create(
             @JniType("std::string") String primaryPattern,
             @JniType("std::string") String secondaryPattern,
             @JniType("int32_t") int notificationCount) {
