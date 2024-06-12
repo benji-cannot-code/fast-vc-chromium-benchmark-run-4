811cd842714dfa61ef4995dbac1e16397d4f37a8
David Pennington <dpenning@chromium.org>
2024-06-12 21:39:33 +0000

    [TabSearchPosition] add a pref for changing the tab search position.
    A pref is added that provides a way for users to change the position of
    the tab search button. In a future CL this is used in the settings page.
    
    Bug: 338622527
    
    Change-Id: If395c6bd9a33528d68ea52947aa0c787cc2b0d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622970
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314263}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/organization/prefs.h"
 #include "chrome/browser/ui/tabs/pinned_tab_codec.h"
 #include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_pref_names.h"
+#include "chrome/browser/ui/tabs/tab_strip_prefs.h"
 #include "chrome/browser/ui/toolbar/chrome_labs/chrome_labs_prefs.h"
 #include "chrome/browser/ui/toolbar/chrome_location_bar_model_delegate.h"
 #include "chrome/browser/ui/toolbar/toolbar_pref_names.h"
@@ -2415,7 +2416,9 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
       webauthn::pref_names::kEnclaveFailedPINAttemptsCount, 0);
 
   side_panel_prefs::RegisterProfilePrefs(registry);
-#endif
+
+  tabs::RegisterProfilePrefs(registry);
+#endif  // !BUILDFLAG(IS_ANDROID)
 
   registry->RegisterBooleanPref(webauthn::pref_names::kAllowWithBrokenCerts,
                                 false);
