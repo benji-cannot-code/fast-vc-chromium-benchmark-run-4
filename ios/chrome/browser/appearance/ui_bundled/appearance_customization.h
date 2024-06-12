a1d44b9b997e05d3ae13a16683eb37375e14e712
Nan Lin <linnan@chromium.org>
2024-06-12 11:24:05 +0000

    [Code Health] Move appearance to ui_bundled
    Bug: 324232603
    Change-Id: I6aca79a353c70549375c80c9847c0b84ce0aa9b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5617655
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1313921}

--- ios/chrome/browser/appearance/ui_bundled/appearance_customization.h
+++ ios/chrome/browser/appearance/ui_bundled/appearance_customization.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_APPEARANCE_APPEARANCE_CUSTOMIZATION_H_
-#define IOS_CHROME_BROWSER_UI_APPEARANCE_APPEARANCE_CUSTOMIZATION_H_
+#ifndef IOS_CHROME_BROWSER_APPEARANCE_UI_BUNDLED_APPEARANCE_CUSTOMIZATION_H_
+#define IOS_CHROME_BROWSER_APPEARANCE_UI_BUNDLED_APPEARANCE_CUSTOMIZATION_H_
 
 #import <UIKit/UIKit.h>
 
@@ -14,4 +14,4 @@ void CustomizeUIAppearance();
 // Overrides some default UIWindow appearance values.
 void CustomizeUIWindowAppearance(UIWindow* window);
 
-#endif  // IOS_CHROME_BROWSER_UI_APPEARANCE_APPEARANCE_CUSTOMIZATION_H_
+#endif  // IOS_CHROME_BROWSER_APPEARANCE_UI_BUNDLED_APPEARANCE_CUSTOMIZATION_H_
