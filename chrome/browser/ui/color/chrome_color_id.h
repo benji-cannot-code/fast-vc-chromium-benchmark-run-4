bd7730d93f05643324e65d3f70f8081c85f00ef4
Thomas Lukaszewicz <tluk@chromium.org>
2024-06-12 19:23:39 +0000

    [omnibox] Fix IconLabelBubbleView colors
    Currently icon label bubble view can paint with either a
    transparent or opaque background.
    
    GetIconLabelBubbleSurroundingForegroundColor() and
    GetIconLabelBubbleBackgroundColor() are intended to get the
    foreground and background color from the icon's context when
    painting a transparent icon.
    
    In the case of opaque colors these are defined by the set of
    kColorPageInfo* set of color ids.
    
    Currently there is a bug for opaque page action icons due to the
    definition of the page info color ids mixing CWS and GM3 theme
    concepts.
    
    This CL ensures page info colors are correctly defined for both
    CWS and GM3 modalities. It also cleans up the foreground and
    background color accessors and handling for the
    IconLabelBubbleView for consistency.
    
    Cleaned up IconLabelBubbleViewTests to install the test inkdrop
    to avoid conflicts with the default-installed inkdrop.
    
    GM3
    Light Red - https://screenshot.googleplex.com/8GLJGHDEK6CiS3K.png
    Dark Red - https://screenshot.googleplex.com/9UgfxT2cckgb9dw.png
    Light Green - https://screenshot.googleplex.com/8AtesZ6Y7Wkm8Nv.png
    Dark Green - https://screenshot.googleplex.com/4zypiiEzFBrPcs4.png
    
    CWS
    https://screenshot.googleplex.com/5pSNofiuxsc4i5r.png
    https://screenshot.googleplex.com/Zead4paTCrRvi8P.png
    https://screenshot.googleplex.com/9BWKPwiarwNs78G.png
    https://screenshot.googleplex.com/5sHtUPb6wGK8Lnr.png
    https://screenshot.googleplex.com/9NW4RanVBzvAWr6.png
    https://screenshot.googleplex.com/6weYY5vzwqxJVec.png
    
    Bug: 345521958
    Change-Id: I866a31950234cf45b99f0f07dbb24f90296f8472
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604793
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1314171}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -352,10 +352,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorPageInfoBackgroundTonal) \
   E_CPONLY(kColorPageInfoChosenObjectDeleteButtonIcon) \
   E_CPONLY(kColorPageInfoChosenObjectDeleteButtonIconDisabled) \
+  E_CPONLY(kColorPageInfoForeground) \
+  E_CPONLY(kColorPageInfoForegroundTonal) \
   E_CPONLY(kColorPageInfoIconHover) \
   E_CPONLY(kColorPageInfoIconPressed) \
-  E_CPONLY(kColorPageInfoLensOverlayBackground) \
-  E_CPONLY(kColorPageInfoLensOverlayForeground) \
   E_CPONLY(kColorPageInfoPermissionBlockedOnSystemLevelDisabled) \
   E_CPONLY(kColorPageInfoPermissionForeground) \
   E_CPONLY(kColorPageInfoPermissionUsedIcon) \
