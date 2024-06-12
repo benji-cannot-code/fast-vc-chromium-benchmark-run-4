e7cab04d70bfc63ac4c7c2e3e534b40f6560bafd
Maks Orlovich <morlovich@chromium.org>
2024-06-12 02:42:14 +0000

    FLEDGE: Enable change of component ad limit to 40 by default
    ...in prep for stable 100% launch via experiment config.
    
    Bug: 324445653
    Change-Id: Ib36f0ab68fbafe7183cb4a86fc0f929e58c1e303
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448765
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313767}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1007,8 +1007,7 @@ BASE_FEATURE(kFledgeSplitTrustedSignalsFetchingURL,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 const base::FeatureParam<int> kFledgeCustomMaxAuctionAdComponentsValue{
-    &kFledgeCustomMaxAuctionAdComponents, "FledgeAdComponentLimit",
-    kMaxAdAuctionAdComponentsDefault};
+    &kFledgeCustomMaxAuctionAdComponents, "FledgeAdComponentLimit", 40};
 
 BASE_FEATURE(kFledgeNumberBidderWorkletGroupByOriginContextsToKeep,
              "FledgeBidderWorkletGroupByOriginContextsToKeep",
