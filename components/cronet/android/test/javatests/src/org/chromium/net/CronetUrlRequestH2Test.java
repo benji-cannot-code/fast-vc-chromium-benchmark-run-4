b504fe797b467961d530101b244734891eabc7d0
Mohannad Farrag <aymanm@google.com>
2024-06-12 11:41:20 +0000

    Unbatch Cronet tests
    The batching of those tests has only passed CQ because batching was
    broken on CQ. We should retry to batch those tests separately and
    individually after we have evaluated their eligibility for batching.
    
    Bug: 344966615
    Bug: 344966124
    Bug: 344665939
    Bug: 344675719
    Cq-Include-Trybots: luci.chromium.try:android-cronet-x64-dbg-12-tests,android-cronet-x64-dbg-13-tests,android-cronet-x64-dbg-14-tests,android-cronet-x86-dbg-10-tests,android-cronet-x86-dbg-11-tests,android-cronet-x86-dbg-lollipop-tests,android-cronet-x86-dbg-marshmallow-tests,android-cronet-x86-dbg-nougat-tests,android-cronet-x86-dbg-oreo-tests,android-cronet-x86-dbg-pie-tests
    Change-Id: Ice483e7b008b036031de5b8321e13699eff4011a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621346
    Commit-Queue: Chidera Olibie <colibie@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Chidera Olibie <colibie@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1313924}

--- components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestH2Test.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/CronetUrlRequestH2Test.java
@@ -20,6 +20,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.net.CronetTestRule.CronetImplementation;
 import org.chromium.net.CronetTestRule.IgnoreFor;
 
@@ -34,6 +35,7 @@ import org.chromium.net.CronetTestRule.IgnoreFor;
 @IgnoreFor(
         implementations = {CronetImplementation.FALLBACK},
         reason = "Fallback does not support H2")
+@DoNotBatch(reason = "crbug/1459563")
 public class CronetUrlRequestH2Test {
     private static final String TAG = CronetUrlRequestH2Test.class.getSimpleName();
 
