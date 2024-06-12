0b3e44e18a472de9e4bba8017cd457be2616e06d
Fredrik Söderquist <fs@opera.com>
2024-06-12 08:20:32 +0000

    Handle LayoutBoxModelObject's without layers in ApplyFiltersToRect()
    LayoutSVGText is an LayoutBoxModelObject but won't have any layer.
    
    Fixed: 41495708
    Change-Id: I627b92a3177315a6b16ab66fa536b107ab3b6091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621396
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1313858}

--- third_party/blink/renderer/core/layout/layout_box_model_object.cc
+++ third_party/blink/renderer/core/layout/layout_box_model_object.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/layout/layout_result.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
 #include "third_party/blink/renderer/core/layout/legacy_layout_tree_walking.h"
+#include "third_party/blink/renderer/core/layout/svg/svg_resources.h"
 #include "third_party/blink/renderer/core/layout/table/layout_table_section.h"
 #include "third_party/blink/renderer/core/page/scrolling/sticky_position_scrolling_constraints.h"
 #include "third_party/blink/renderer/core/paint/inline_paint_context.h"
@@ -489,11 +490,21 @@ PhysicalRect LayoutBoxModelObject::ApplyFiltersToRect(
     return rect;
   }
   gfx::RectF float_rect(rect);
-  gfx::RectF filter_reference_box = Layer()->FilterReferenceBox();
-  if (!filter_reference_box.size().IsZero()) {
-    float_rect.UnionEvenIfEmpty(filter_reference_box);
+  if (auto* layer = Layer()) {
+    const gfx::RectF filter_reference_box = layer->FilterReferenceBox();
+    if (!filter_reference_box.size().IsZero()) {
+      float_rect.UnionEvenIfEmpty(filter_reference_box);
+    }
+    float_rect = layer->MapRectForFilter(float_rect);
+  } else {
+    CHECK(IsSVGChild());
+    const gfx::RectF filter_reference_box =
+        SVGResources::ReferenceBoxForEffects(*this);
+    if (!filter_reference_box.size().IsZero()) {
+      float_rect.UnionEvenIfEmpty(filter_reference_box);
+    }
+    float_rect = StyleRef().Filter().MapRect(float_rect);
   }
-  float_rect = Layer()->MapRectForFilter(float_rect);
   return PhysicalRect::EnclosingRect(float_rect);
 }
 
