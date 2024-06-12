cb179bbfd26f0257060e3574c021e7ffd46d7f25
Saifuddin Hitawala <hitawala@chromium.org>
2024-06-12 02:41:52 +0000

    [capture] Use GetTextureTarget instead of 2D for VideoCaptureEffects
    Use ClientSharedImage::GetTextureTarget() instead of hardcoded
    GL_TEXTURE_2D for VideoCaptureEffectsProcessor.
    
    This will also allow follow-up removal of texture_target param
    from SharedImageBufferHandleSet as it will always use that from
    shared image.
    
    Bug: None
    Change-Id: Ife9672d0bf05be11dfd5f0d71db74bbd4dd9d925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608175
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313766}

--- media/capture/video/video_capture_effects_processor.cc
+++ media/capture/video/video_capture_effects_processor.cc
@@ -21,33 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 namespace {
-std::pair<gpu::ExportedSharedImage, gpu::SyncToken> CreateSharedImage(
-    const VideoCaptureDevice::Client::Buffer& buffer,
-    const mojom::VideoFrameInfo& frame_info) {
-  CHECK_EQ(frame_info.pixel_format, VideoPixelFormat::PIXEL_FORMAT_NV12);
-
-  auto& gpu_channel_host = VideoCaptureGpuChannelHost::GetInstance();
-
-  auto* sii = gpu_channel_host.SharedImageInterface();
-  CHECK(sii);
-
-  // Create a single shared image to back a multiplanar video frame.
-  gpu::SharedImageInfo info(
-      viz::MultiPlaneFormat::kNV12, frame_info.coded_size,
-      frame_info.color_space,
-      gpu::SHARED_IMAGE_USAGE_RASTER_WRITE |
-          gpu::SHARED_IMAGE_USAGE_RASTER_READ,
-      "VideoCaptureEffectsProcessorMultiPlanarSharedImage");
-  scoped_refptr<gpu::ClientSharedImage> shared_image = sii->CreateSharedImage(
-      std::move(info), buffer.handle_provider->GetGpuMemoryBufferHandle());
-  CHECK(shared_image);
-
-  gpu::ExportedSharedImage result = shared_image->Export();
-
-  auto sync_token = sii->GenVerifiedSyncToken();
-  return std::make_pair(result, sync_token);
-}
-
 mojom::VideoBufferHandlePtr CreateBufferHandle(
     const VideoCaptureDevice::Client::Buffer& buffer,
     const mojom::VideoFrameInfo& frame_info,
@@ -60,9 +33,28 @@ mojom::VideoBufferHandlePtr CreateBufferHandle(
       return mojom::VideoBufferHandle::NewUnsafeShmemRegion(
           buffer.handle_provider->DuplicateAsUnsafeRegion());
     case VideoCaptureBufferType::kGpuMemoryBuffer: {
-      auto [shared_image, sync_token] = CreateSharedImage(buffer, frame_info);
+      CHECK_EQ(frame_info.pixel_format, VideoPixelFormat::PIXEL_FORMAT_NV12);
+
+      auto* sii =
+          VideoCaptureGpuChannelHost::GetInstance().SharedImageInterface();
+      CHECK(sii);
+
+      // Create a single shared image to back a multiplanar video frame.
+      gpu::SharedImageInfo info(
+          viz::MultiPlaneFormat::kNV12, frame_info.coded_size,
+          frame_info.color_space,
+          gpu::SHARED_IMAGE_USAGE_RASTER_WRITE |
+              gpu::SHARED_IMAGE_USAGE_RASTER_READ,
+          "VideoCaptureEffectsProcessorMultiPlanarSharedImage");
+      scoped_refptr<gpu::ClientSharedImage> shared_image =
+          sii->CreateSharedImage(
+              std::move(info),
+              buffer.handle_provider->GetGpuMemoryBufferHandle());
+      CHECK(shared_image);
+
+      auto sync_token = sii->GenVerifiedSyncToken();
       auto shared_image_set = mojom::SharedImageBufferHandleSet::New(
-          shared_image, sync_token, GL_TEXTURE_2D);
+          shared_image->Export(), sync_token, shared_image->GetTextureTarget());
 
       return mojom::VideoBufferHandle::NewSharedImageHandles(
           std::move(shared_image_set));
