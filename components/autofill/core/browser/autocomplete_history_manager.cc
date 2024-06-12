dd1a93fb2f4e9d549c30ba0081b09e0c2d836b6e
Rouslan Solomakhin <rouslan@chromium.org>
2024-06-12 15:19:34 +0000

    [CC Number Validation Target] Remove validation include (7g/8)
    This patch removes the temporary credit_card_number_validation.h include
    from the validation.h header and updates the relevant files to include
    the credit_card_number_validation.h header directly. This include is
    being used only for the autofill::IsValidCreditCardNumber() function
    in this patch.
    
    Design doc: https://bit.ly/cc-number-validation-target-dd
    
    Bug: b:281812289
    Change-Id: Iaead894c783dd6fdc414cb3599733c51388af5c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622810
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1313994}

--- components/autofill/core/browser/autocomplete_history_manager.cc
+++ components/autofill/core/browser/autocomplete_history_manager.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_prefs.h"
 #include "components/autofill/core/common/autofill_regexes.h"
+#include "components/autofill/core/common/credit_card_number_validation.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/prefs/pref_service.h"
 #include "components/version_info/version_info.h"
--- components/autofill/core/browser/validation.h
+++ components/autofill/core/browser/validation.h
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 
-// TODO(b/281812289): Remove this include when all dependencies switch to
-// including this file directly instead of relying on validation.h.
-#include "components/autofill/core/common/credit_card_number_validation.h"
-
 namespace base {
 class Time;
 }  // namespace base
