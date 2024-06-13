974f1ffba0f742ad95587e44c3814b37027c7780
Andrew Grieve <agrieve@chromium.org>
2024-06-13 15:28:40 +0000

    Android: Remove no-op JUnitProcessor & InstrumentationProcessor
    Bug: 326415975
    Change-Id: I966c0f28dfc2e2e3c6516471d659a9fa005b6ca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626142
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314588}

--- base/test/android/javatests/src/org/chromium/base/test/util/Features.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/Features.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.util;
 
-import org.junit.rules.ExternalResource;
-
 import org.chromium.base.CommandLine;
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureParam;
@@ -47,12 +45,6 @@ public class Features {
     }
 
     private Features() {}
-
-    // TODO(b/326415975): Delete once no longer referenced.
-    public static class JUnitProcessor extends ExternalResource {}
-
-    // TODO(b/326415975): Delete once no longer referenced.
-    public static class InstrumentationProcessor extends ExternalResource {}
 
     static void resetCachedFlags() {
         // TODO(agrieve): Allow cached flags & field trials to be set in @BeforeClass.
