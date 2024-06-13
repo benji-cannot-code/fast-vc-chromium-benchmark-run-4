458e68b75716e3e6e387fac9dc9965ca2d8fa5a5
Nan Lin <linnan@chromium.org>
2024-06-13 22:24:38 +0000

    [Code Health] Remove RAW_PTR_EXCLUSTION in MenuSection
    Bug: 346693627
    Change-Id: I8bdc4ff151446ec0109b6575ed8fe650fcdc7612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630895
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314916}

--- chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu_browsertest.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_shelf_context_menu_browsertest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_item_delegate.h"
 #include "ash/public/cpp/shelf_model.h"
 #include "base/functional/callback_helpers.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/stack_allocated.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
@@ -51,10 +51,11 @@ class AppServiceShelfContextMenuBrowserTest : public InProcessBrowserTest {
   ~AppServiceShelfContextMenuBrowserTest() override = default;
 
   struct MenuSection {
+    STACK_ALLOCATED();
+
+   public:
     std::unique_ptr<ui::SimpleMenuModel> menu_model;
-    // This field is not a raw_ptr<> because it was filtered by the rewriter
-    // for: #addr-of, #union
-    RAW_PTR_EXCLUSION ui::MenuModel* sub_model = nullptr;
+    ui::MenuModel* sub_model = nullptr;
     size_t command_index = 0;
   };
 
