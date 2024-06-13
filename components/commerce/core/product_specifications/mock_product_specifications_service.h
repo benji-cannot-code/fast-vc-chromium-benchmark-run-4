4faddc7f6c4cdbd7b6e744cde674bd1d18018fff
Yue Zhang <yuezhanggg@chromium.org>
2024-06-13 22:38:49 +0000

    Use async ProductSpecificationsService API in ClusterManager
    Bug: b/341847257
    Change-Id: I4dab1ccd09172a04058e1497e208fe2fed5189ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623835
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314926}

--- components/commerce/core/product_specifications/mock_product_specifications_service.h
+++ components/commerce/core/product_specifications/mock_product_specifications_service.h
@@ -20,6 +20,10 @@ class MockProductSpecificationsService : public ProductSpecificationsService {
               GetAllProductSpecifications,
               (),
               (override));
+  MOCK_METHOD(void,
+              GetAllProductSpecifications,
+              (GetAllCallback callback),
+              (override));
   MOCK_METHOD(const std::optional<ProductSpecificationsSet>,
               GetSetByUuid,
               (const base::Uuid& uuid),
