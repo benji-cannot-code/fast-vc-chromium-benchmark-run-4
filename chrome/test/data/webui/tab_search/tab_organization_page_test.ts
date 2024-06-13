24483e85173b4d991372686802be87635fbcdada
Emily Shack <emshack@chromium.org>
2024-06-13 18:39:33 +0000

    Update string for when no group is found for tab
    Bug: 342228076
    Change-Id: I820caaa9a22c191271255bc3982e7cb1cf549e35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5627202
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314762}

--- chrome/test/data/webui/tab_search/tab_organization_page_test.ts
+++ chrome/test/data/webui/tab_search/tab_organization_page_test.ts
@@ -563,6 +563,12 @@ suite('TabOrganizationPageTest', () => {
   });
 
   test('Active tab missing from organization shows error', async () => {
+    const errorString = 'error';
+    const successString = 'success';
+    loadTimeData.overrideValues({
+      successMissingActiveTabTitle: errorString,
+      successTitle: successString,
+    });
     await tabOrganizationResultsSetup();
     tabOrganizationResults.session = createSession({
       activeTabId: 4,
@@ -582,13 +588,17 @@ suite('TabOrganizationPageTest', () => {
     });
     await microtasksFinished();
 
-    const errorElement =
-        tabOrganizationResults.shadowRoot!.querySelector('#error');
-    assertTrue(!!errorElement);
-    assertTrue(isVisible(errorElement));
+    const header = tabOrganizationResults.$.header;
+    assertEquals(errorString, header.textContent!.trim());
   });
 
   test('Active tab present in organization does not show error', async () => {
+    const errorString = 'error';
+    const successString = 'success';
+    loadTimeData.overrideValues({
+      successMissingActiveTabTitle: errorString,
+      successTitle: successString,
+    });
     await tabOrganizationResultsSetup();
     tabOrganizationResults.session = createSession({
       activeTabId: 2,
@@ -608,9 +618,7 @@ suite('TabOrganizationPageTest', () => {
     });
     await microtasksFinished();
 
-    const errorElement =
-        tabOrganizationResults.shadowRoot!.querySelector('#error');
-    assertTrue(!!errorElement);
-    assertFalse(isVisible(errorElement));
+    const header = tabOrganizationResults.$.header;
+    assertEquals(successString, header.textContent!.trim());
   });
 });
