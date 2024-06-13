7f6f00c5ef3252ba504fb7a55acb201c2ea38221
Ramin Halavati <rhalavati@chromium.org>
2024-06-13 05:09:24 +0000

    Update Accessibility.ScreenAI.Component.Available metric.
    The metric is updated to `Accessibility.ScreenAI.Component.Available2`
    which also records if network was available or not when component was
    not available.
    This helps with investigating if component unavailability is due to
    network issues, or other problems.
    
    OBSOLETE_HISTOGRAMS[Accessibility.ScreenAI.Component.Available]=Replaced by Accessibility.ScreenAI.Component.Available2
    
    AX-Relnotes: n/a
    Bug: 346439451
    Change-Id: I46bc81021bab644c117a304989849545be7654d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622014
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314441}

--- chrome/browser/screen_ai/screen_ai_service_router.cc
+++ chrome/browser/screen_ai/screen_ai_service_router.cc
@@ -21,9 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/task/thread_pool.h"
 #include "chrome/browser/screen_ai/screen_ai_install_state.h"
+#include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/service_process_host.h"
 #include "content/public/browser/service_process_host_passkeys.h"
 #include "mojo/public/mojom/base/file_path.mojom.h"
+#include "services/network/public/mojom/network_change_manager.mojom.h"
 #include "services/screen_ai/public/cpp/utilities.h"
 #include "ui/accessibility/accessibility_features.h"
 
@@ -32,6 +34,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace {
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+// If any value is added, please update `ComponentAvailability` in `enums.xml`.
+enum class ComponentAvailability {
+  kAvailable = 0,
+  kUnavailableWithNetwork = 1,
+  kUnavailableWithoutNetwork = 2,
+
+  kMaxValue = kUnavailableWithoutNetwork,
+};
 
 // Maximum time to wait for service initialization.
 // TODO(crbug.com/40947650): Update based on collected metrics.
@@ -110,9 +123,14 @@ std::unique_ptr<ComponentFiles> ComponentFiles::Load(
       files_list_file_name);
 }
 
-void RecordComponentAvailablity(bool available) {
-  base::UmaHistogramBoolean("Accessibility.ScreenAI.Component.Available",
-                            available);
+void RecordComponentAvailability(bool available) {
+  bool network = !content::GetNetworkConnectionTracker()->IsOffline();
+  base::UmaHistogramEnumeration(
+      "Accessibility.ScreenAI.Component.Available2",
+      available
+          ? ComponentAvailability::kAvailable
+          : (network ? ComponentAvailability::kUnavailableWithNetwork
+                     : ComponentAvailability::kUnavailableWithoutNetwork));
 }
 
 }  // namespace
@@ -151,7 +169,7 @@ void ScreenAIServiceRouter::GetServiceStateAsync(
   if (service_state) {
     // Either service is already initialized or disabled.
     std::move(callback).Run(*service_state);
-    RecordComponentAvailablity(true);
+    RecordComponentAvailability(true);
     return;
   }
 
@@ -196,7 +214,7 @@ void ScreenAIServiceRouter::StateChanged(ScreenAIInstallState::State state) {
       for (Service service : all_services) {
         CallPendingStatusRequests(service, false);
       }
-      RecordComponentAvailablity(false);
+      RecordComponentAvailability(false);
       break;
     }
 
@@ -205,7 +223,7 @@ void ScreenAIServiceRouter::StateChanged(ScreenAIInstallState::State state) {
       for (Service service : all_services) {
         InitializeServiceIfNeeded(service);
       }
-      RecordComponentAvailablity(true);
+      RecordComponentAvailability(true);
       break;
     }
   }
