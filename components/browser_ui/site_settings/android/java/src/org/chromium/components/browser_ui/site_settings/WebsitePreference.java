e7329412bc1b615f5e6eddc70ce5653f63f43ebc
Christian Dullweber <dullweber@chromium.org>
2024-06-13 13:32:47 +0000

    SiteSettings: Add site name to content description for delete buttons
    Form fields should have unique a11y labels. Add site name to delete
    buttons in site settings to make it more clear what data is going
    to be deleted.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Screenshot: https://crbug.com/327641034#comment7
    Bug: 327641034
    Change-Id: I0570ebe057e9deb271955e10fefe0aa0459d62d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5625656
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314548}

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreference.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/WebsitePreference.java
@@ -205,7 +205,11 @@ class WebsitePreference extends ChromeImageViewPreference {
             // Create and set the delete button for this preference.
             setImageView(
                     R.drawable.btn_close,
-                    R.string.webstorage_delete_data_dialog_title,
+                    getContext()
+                            .getResources()
+                            .getString(
+                                    R.string.webstorage_delete_data_content_description,
+                                    buildTitle()),
                     (OnClickListener)
                             view -> {
                                 SiteSettingsUtil.resetZoomLevel(
@@ -220,7 +224,11 @@ class WebsitePreference extends ChromeImageViewPreference {
         if (hasSubPage()) {
             setImageView(
                     R.drawable.ic_expand_more_horizontal_black_24dp,
-                    R.string.webstorage_delete_data_dialog_title,
+                    getContext()
+                            .getResources()
+                            .getString(
+                                    R.string.webstorage_delete_data_content_description,
+                                    buildTitle()),
                     (OnClickListener)
                             view -> {
                                 mStorageAccessSettingsPageListener
