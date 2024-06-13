136e1b732cf467c9b85da3e349aa9965676d7b2b
Nan Lin <linnan@chromium.org>
2024-06-13 16:16:39 +0000

    [Code Health] Remove RAW_PTR_EXCLUSION for DownloadMetadataManagerTestBase
    Bug: 346693888
    Change-Id: Ia492a984696bcb50744fd6c6b1adb3c323240c91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630155
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314639}

--- chrome/browser/safe_browsing/incident_reporting/download_metadata_manager_unittest.cc
+++ chrome/browser/safe_browsing/incident_reporting/download_metadata_manager_unittest.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/task/sequenced_task_runner.h"
 #include "chrome/test/base/testing_profile.h"
@@ -297,9 +297,7 @@ class DownloadMetadataManagerTestBase : public ::testing::Test {
 
   // The DownloadMetadataManager's content::DownloadManager::Observer. Captured
   // by download_manager_'s AddObserver action.
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION content::DownloadManager::Observer* dm_observer_ = nullptr;
+  raw_ptr<content::DownloadManager::Observer> dm_observer_ = nullptr;
 };
 
 // A parameterized test that exercises GetDownloadDetails. The parameters
