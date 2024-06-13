fb2a0f0ac50e26a524eb1a7c27a0e4f6a3b15283
Lukasz Anforowicz <lukasza@chromium.org>
2024-06-13 15:40:34 +0000

    `span`-ify `mojo/.../data_pipe.h`: Migrate `media`.
    `span`-ification: `mojo::DataPipe[Consumer|Producer]Handle`: Migrating
    callers.  This CL `span`-ifies the callers of the APIs in
    `mojo/public/cpp/system/data_pipe.h`:
    
    * `BeginReadData` and `BeginWriteData` return (via an out parameter)
      a `span` instead of returning a ptr+size pair.
    * `ReadData` and `WriteData` consume a `span` instead of consuming
      a ptr+size pair.
    
    For more details see the design note here:
    https://docs.google.com/document/d/1c4NKpXwpQ9MKK1SbJ4C6MvhXI8-KJZ4jq7N4VHTHJoI/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=sandersd@chromium.org
    
    Bug: 40284755
    Change-Id: I9fa226e9f42f95aa59bd99d0e6a7ec8de7c47b3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5619056
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314609}

--- media/mojo/common/mojo_data_pipe_read_write.cc
+++ media/mojo/common/mojo_data_pipe_read_write.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/compiler_specific.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
@@ -85,11 +86,19 @@ void MojoDataPipeReader::TryReadData(MojoResult result) {
   DCHECK_GT(current_buffer_size_, bytes_read_);
   size_t num_bytes = current_buffer_size_ - bytes_read_;
   if (current_buffer_) {
-    result = consumer_handle_->ReadData(current_buffer_ + bytes_read_,
-                                        &num_bytes, MOJO_READ_DATA_FLAG_NONE);
+    // SAFETY: Depending on the caller of Read to provide a valid ptr+size.
+    //
+    // TODO(lukasza): Consider removing the whole `MojoDataPipeReader` class:
+    // * It is only used from test code (from unit tests of
+    //   `MojoDataPipeWriter`)
+    // * It uses `UNSAFE_BUFFERS` + it is the only caller of `DiscardData`
+    base::span<uint8_t> buffer =
+        UNSAFE_BUFFERS(base::span(current_buffer_.get(), current_buffer_size_));
+    buffer = buffer.subspan(bytes_read_);
+    result =
+        consumer_handle_->ReadData(MOJO_READ_DATA_FLAG_NONE, buffer, num_bytes);
   } else {
-    result = consumer_handle_->ReadData(nullptr, &num_bytes,
-                                        MOJO_READ_DATA_FLAG_DISCARD);
+    result = consumer_handle_->DiscardData(num_bytes, num_bytes);
   }
 
   if (IsPipeReadWriteError(result)) {
@@ -163,7 +172,7 @@ void MojoDataPipeWriter::Write(const uint8_t* buffer,
                                DoneCB done_cb) {
   DVLOG(3) << __func__;
   // Write() can not be called when another writing request is in process.
-  DCHECK(!current_buffer_);
+  DCHECK(current_buffer_.empty());
   DCHECK(done_cb);
   if (!buffer_size) {
     std::move(done_cb).Run(true);
@@ -179,9 +188,9 @@ void MojoDataPipeWriter::Write(const uint8_t* buffer,
     return;
   }
 
-  current_buffer_ = buffer;
-  current_buffer_size_ = size_t{buffer_size};
-  bytes_written_ = 0;
+  // TODO(lukasza): Take `span` instead of `buffer` + `buffer_size`.
+  current_buffer_ =
+      UNSAFE_BUFFERS(base::span<const uint8_t>(buffer, size_t{buffer_size}));
   done_cb_ = std::move(done_cb);
   // Try writing data immediately to reduce latency.
   TryWriteData(MOJO_RESULT_OK);
@@ -193,19 +202,17 @@ void MojoDataPipeWriter::TryWriteData(MojoResult result) {
     return;
   }
 
-  DCHECK(current_buffer_);
-  DCHECK_GT(current_buffer_size_, bytes_written_);
-  size_t num_bytes = current_buffer_size_ - bytes_written_;
-
-  result = producer_handle_->WriteData(current_buffer_ + bytes_written_,
-                                       &num_bytes, MOJO_WRITE_DATA_FLAG_NONE);
+  DCHECK(!current_buffer_.empty());
+  size_t actually_written_bytes = 0;
+  result = producer_handle_->WriteData(
+      current_buffer_, MOJO_WRITE_DATA_FLAG_NONE, actually_written_bytes);
   if (IsPipeReadWriteError(result)) {
     OnPipeError(result);
   } else {
     if (result == MOJO_RESULT_OK) {
-      DCHECK_GT(num_bytes, 0u);
-      bytes_written_ += num_bytes;
-      if (bytes_written_ == current_buffer_size_) {
+      DCHECK_GT(actually_written_bytes, 0u);
+      current_buffer_ = current_buffer_.subspan(actually_written_bytes);
+      if (current_buffer_.empty()) {
         CompleteCurrentWrite();
         return;
       }
@@ -217,7 +224,7 @@ void MojoDataPipeWriter::TryWriteData(MojoResult result) {
 void MojoDataPipeWriter::CompleteCurrentWrite() {
   DVLOG(4) << __func__;
   DCHECK(done_cb_);
-  current_buffer_ = nullptr;
+  current_buffer_ = base::span<const uint8_t>();
   std::move(done_cb_).Run(true);
 }
 
@@ -227,13 +234,10 @@ void MojoDataPipeWriter::OnPipeError(MojoResult result) {
 
   producer_handle_.reset();
 
-  if (current_buffer_) {
+  if (!current_buffer_.empty()) {
     DVLOG(1) << __func__ << ": writing to data pipe failed. result=" << result
-             << ", buffer size=" << current_buffer_size_
-             << ", num_bytes(written)=" << bytes_written_;
-    current_buffer_ = nullptr;
-    current_buffer_size_ = 0;
-    bytes_written_ = 0;
+             << ", current_buffer_.size()=" << current_buffer_.size();
+    current_buffer_ = base::span<const uint8_t>();
     DCHECK(done_cb_);
     std::move(done_cb_).Run(false);
   }
