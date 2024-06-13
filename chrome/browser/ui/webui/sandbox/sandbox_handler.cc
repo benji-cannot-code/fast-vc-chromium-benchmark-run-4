6581069b99ab4c8ecc7c3f9ad271c23fdb57c5db
Fayez Razak <fayez.razak@microsoft.com>
2024-06-13 17:32:25 +0000

    Enables RestrictCoreSharing process mitigation policy to the renderer.
    Enabling RestrictCoreSharing mitigation on the renderer process
    restricts other processes to be scheduled on the same CPU core. This
    helps in restricting the side channel attacks like RIDL.
    
    This mitigation policy is supported from Windows 11 Build 25922 and
    above. This policy takes effect if there is no non-architectural
    core sharing and have enabled support for SMT isolation scheduling on
    the system.
    
    Currently the policy is applied on process creation. If the OS doesn't
    support the feature this will be a no-op on process creation.
    
    Bug: 325133343
    Change-Id: I190f4a5379216a510185b4a2e34b519f62b6bb68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607583
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Fayez Razak <fayez.razak@microsoft.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314698}

--- chrome/browser/ui/webui/sandbox/sandbox_handler.cc
+++ chrome/browser/ui/webui/sandbox/sandbox_handler.cc
@@ -92,6 +92,8 @@ base::Value::List FetchSandboxFeatures() {
       FeatureToValue(sandbox::policy::features::kWinSboxZeroAppShim));
   features.Append(
       FeatureToValue(sandbox::policy::features::kWinSboxNoFakeGdiInit));
+  features.Append(FeatureToValue(
+      sandbox::policy::features::kWinSboxRestrictCoreSharingOnRenderer));
   return features;
 }
 
