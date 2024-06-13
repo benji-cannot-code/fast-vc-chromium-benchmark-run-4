79090aebf3cc37a4c176b9c87e2ab4a393010473
Xiaochen Zhou <xiaochenzh@chromium.org>
2024-06-13 17:34:52 +0000

    Fenced frames: Disable "Save Link As..." after frame tree has network cutoff.
    Fenced frame can disable untrusted network access by calling
    `window.fence.disableUntrustedNetwork()`. Once called, the fenced frame
    and all its descendant iframes are not allowed make any network
    requests. This is done by marking the fenced frame nonce as revoked for
    network access. Requests that have such nonce will be cancelled.
    
    In this CL, we disable "Save Link As..." in the context menu after a
    fenced frame disables its network. Fenced frames are allowed to access
    cross-site information, for example, shared storage get, after its
    network is cutoff. It is possible to alter a link via javascript.
    For example, the cross-site information about a user can be appended
    to a link by using the "contextmenu" event. This way "Save Link As...",
    and other download methods, can be used as a channel to leak cross-site
    information. We must disable such channels.
    
    In the next few CLs, we plan to do the same for:
    "Save Image As..."
    "Save Audio As..."
    "Save Video As..."
    "Save Video Frame As..."
    
    Also, the other change we will make in the next few CLs is to append
    the IsolationInfo, which has the nonce, to the download requests from
    within the fenced frames. Requests that contain the nonce will be
    cancelled by `URLLoader::CancelRequestIfNonceMatchesAndUrlNotExempted`.
    This is needed to cancel download requests which are initiated just
    before a nonce's network is revoked.
    
    Bug: 1515599
    Change-Id: I78f82b0d2b704beef69391ac42fd019f7cce5e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564165
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Andrew Verge <averge@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314703}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3647,6 +3647,17 @@ bool RenderViewContextMenu::IsSaveAsItemAllowedByPolicy() const {
   return true;
 }
 
+bool RenderViewContextMenu::IsSaveAsItemAllowedByUntrustedNetworkStatus()
+    const {
+  if (!GetRenderFrameHost()) {
+    return true;
+  }
+
+  // Download requests are not allowed in a frame tree that has untrusted
+  // network access disabled.
+  return !GetRenderFrameHost()->IsUntrustedNetworkDisabled();
+}
+
 // Controller functions --------------------------------------------------------
 
 bool RenderViewContextMenu::IsReloadEnabled() const {
@@ -3711,6 +3722,10 @@ bool RenderViewContextMenu::IsTranslateEnabled() const {
 
 bool RenderViewContextMenu::IsSaveLinkAsEnabled() const {
   if (!IsSaveAsItemAllowedByPolicy()) {
+    return false;
+  }
+
+  if (!IsSaveAsItemAllowedByUntrustedNetworkStatus()) {
     return false;
   }
 
