970d2cb8bfb5970fa7d9819f5b57557cf50439e0
Alison Gale <agale@chromium.org>
2024-06-13 00:34:53 +0000

    Remove ChromeRefresh2023 feature flag from ui_features.cc
    This flag is no longer used in the codebase.
    
    Change-Id: I8fed7faf199a8a2a8441afcff38d9f82db8565ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626807
    Auto-Submit: Alison Gale <agale@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314366}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -105,8 +105,7 @@ BASE_FEATURE(kExtensionsMenuInAppMenu,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsExtensionMenuInRootAppMenu() {
-  return base::FeatureList::IsEnabled(kExtensionsMenuInAppMenu) ||
-         features::IsChromeRefresh2023();
+  return base::FeatureList::IsEnabled(kExtensionsMenuInAppMenu);
 }
 
 #if !defined(ANDROID)
@@ -344,8 +343,7 @@ BASE_FEATURE(kTabOrganization,
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsTabOrganization() {
-  return IsChromeRefresh2023() &&
-         base::FeatureList::IsEnabled(features::kTabOrganization);
+  return base::FeatureList::IsEnabled(features::kTabOrganization);
 }
 
 BASE_FEATURE(kMultiTabOrganization,
