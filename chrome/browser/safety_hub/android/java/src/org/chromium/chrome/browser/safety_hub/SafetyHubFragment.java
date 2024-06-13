873118a2cec4261ba77308e72f6091c1e9b2f2db
Zaina Al-Mashni <zalmashni@google.com>
2024-06-13 21:04:37 +0000

    [SHoA] Add the static components to the dashboard
    Add the rest of the static components to the Safety Hub main page, like
    the title, summary and the expandable safety tips section.
    The safety tips conetnt will be added in a follow up CL.
    
    Also break up SafetyHubFragment#onCreatePreferences as its already
    getting too long, and will get more complex as we expand on the UI.
    
    Screenshot: https://screenshot.googleplex.com/xdjDdTC566sAuA9
    Bug: 324562205
    Change-Id: I7b09456fc5bacf3f9c830562f8995c0d1a44ba7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621397
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1314861}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubFragment.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubFragment.java
@@ -38,8 +38,13 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
     public void onCreatePreferences(Bundle bundle, String s) {
         SettingsUtils.addPreferencesFromResource(this, R.xml.safety_hub_preferences);
         getActivity().setTitle(R.string.prefs_safety_check);
+        setUpAccountPasswordCheckModule();
+        setUpUpdateCheckModule();
+        setUpPermissionsRevocationModule();
+        setUpNotificationsReviewModule();
+    }
 
-        // Set up account-level password check preference.
+    private void setUpAccountPasswordCheckModule() {
         Preference passwordCheckPreference = findPreference(PREF_PASSWORDS);
         int compromisedPasswordsCount =
                 UserPrefs.get(getProfile()).getInteger(Pref.BREACHED_CREDENTIALS_COUNT);
@@ -62,8 +67,9 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
                 passwordCheckPropertyModel,
                 passwordCheckPreference,
                 SafetyHubModuleViewBinder::bindPasswordCheckProperties);
+    }
 
-        // Set up update check preference.
+    private void setUpUpdateCheckModule() {
         Preference updateCheckPreference = findPreference(PREF_UPDATE);
 
         PropertyModel updateCheckPropertyModel =
@@ -77,8 +83,9 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
                 updateCheckPropertyModel,
                 updateCheckPreference,
                 SafetyHubModuleViewBinder::bindUpdateCheckProperties);
+    }
 
-        // Set up permissions preference.
+    private void setUpPermissionsRevocationModule() {
         Preference permissionsPreference = findPreference(PREF_UNUSED_PERMISSIONS);
 
         mPermissionsModel =
@@ -98,8 +105,9 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
 
         mUnusedSitePermissionsBridge = UnusedSitePermissionsBridge.getForProfile(getProfile());
         mUnusedSitePermissionsBridge.addObserver(this);
+    }
 
-        // Set up notifications review preference.
+    private void setUpNotificationsReviewModule() {
         Preference notificationsPreference = findPreference(PREF_NOTIFICATIONS_REVIEW);
 
         mNotificationsModel =
