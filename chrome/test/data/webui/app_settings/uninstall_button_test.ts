39659950e93a7c2706d2d15b454ed6016013fff1
Teresa Mao <temao@chromium.org>
2024-06-13 19:55:45 +0000

    app-settings UI: Migrate to Lit, part 2.
    This CL migrates uninstall_button.ts.
    
    Bug: 40943652
    Change-Id: Id5a0f7dbfdcef53ae4bfb67f13711acd8a01aef6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623854
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314814}

--- chrome/test/data/webui/app_settings/uninstall_button_test.ts
+++ chrome/test/data/webui/app_settings/uninstall_button_test.ts
@@ -6,17 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /** @fileoverview Test suite for app-management-uninstall-button. */
 import 'chrome://app-settings/uninstall_button.js';
 
-import type {AppManagementUninstallButtonElement} from 'chrome://app-settings/uninstall_button.js';
+import type {UninstallButtonElement} from 'chrome://app-settings/uninstall_button.js';
 import type {App} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {InstallReason} from 'chrome://resources/cr_components/app_management/app_management.mojom-webui.js';
 import {BrowserProxy} from 'chrome://resources/cr_components/app_management/browser_proxy.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {createTestApp, TestAppManagementBrowserProxy} from './app_management_test_support.js';
 
 suite('AppManagementUninstallButtonTest', () => {
-  let uninstallButton: AppManagementUninstallButtonElement;
+  let uninstallButton: UninstallButtonElement;
   let testProxy: TestAppManagementBrowserProxy;
 
   setup(async function() {
@@ -29,7 +29,7 @@ suite('AppManagementUninstallButtonTest', () => {
     uninstallButton = document.createElement('app-management-uninstall-button');
     uninstallButton.app = app;
     document.body.appendChild(uninstallButton);
-    await flushTasks();
+    await microtasksFinished();
   }
 
   test('Click uninstall', async () => {
