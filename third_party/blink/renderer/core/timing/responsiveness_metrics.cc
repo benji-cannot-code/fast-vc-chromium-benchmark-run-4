505423088da47891e9d32382ca5035c28c955504
Aoyuan Zuo <zuoaoyuan@chromium.org>
2024-06-13 16:10:42 +0000

    Add Use Counters for Orphan Pointerup with Click
    Out of all the cases of pointerdown being optimized out, we would like
    to learn how often it is a discrete interaction which would have a click
    event followed.
    
    Bug: 40935137
    Change-Id: I1f13ac169f3b0c3190470620364b097598f2e3c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615431
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Commit-Queue: Aoyuan Zuo <zuoaoyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314635}

--- third_party/blink/renderer/core/timing/responsiveness_metrics.cc
+++ third_party/blink/renderer/core/timing/responsiveness_metrics.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "third_party/blink/renderer/core/timing/responsiveness_metrics.h"
+
 #include <memory>
 
 #include "base/metrics/histogram_functions.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/responsiveness_metrics/user_interaction_latency.h"
 #include "third_party/blink/renderer/core/dom/dom_high_res_time_stamp.h"
 #include "third_party/blink/renderer/core/event_type_names.h"
+#include "third_party/blink/renderer/core/events/pointer_event_factory.h"
 #include "third_party/blink/renderer/core/frame/frame.h"
 #include "third_party/blink/renderer/core/frame/local_dom_window.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
@@ -317,10 +319,14 @@ bool ResponsivenessMetrics::SetPointerIdAndRecordLatency(
     // Any existing pointerup in the map cannot fire a click.
     FlushPointerup();
 
+    is_last_pointerup_orphan_ = false;
+
     // Platforms like Android would create ever-increasing pointer_id for
     // interactions, whereas platforms like linux could reuse the same id for
     // different interactions. So resetting pointer_info here if it's flushed.
     if (!pointer_id_entry_map_.Contains(pointer_id)) {
+      is_last_pointerup_orphan_ = true;
+
       // Reset if pointer_info got flushed.
       pointer_info = nullptr;
 
@@ -358,6 +364,13 @@ bool ResponsivenessMetrics::SetPointerIdAndRecordLatency(
     pointer_flush_timer_.StartOneShot(kFlushTimerLength, FROM_HERE);
     last_pointer_id_ = pointer_id;
   } else if (event_type == event_type_names::kClick) {
+    if (is_last_pointerup_orphan_ &&
+        pointer_id != PointerEventFactory::kReservedNonPointerId) {
+      UseCounter::Count(window_performance_->GetExecutionContext(),
+                        WebFeature::kEventTimingOrphanPointerupWithClick);
+      is_last_pointerup_orphan_ = false;
+    }
+
     // We do not rely on the |pointer_id| for clicks because they may be
     // inaccurate. Instead, we rely on the last pointer id seen.
     pointer_info = nullptr;
