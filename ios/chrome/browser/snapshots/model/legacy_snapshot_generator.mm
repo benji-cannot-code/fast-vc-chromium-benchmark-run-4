d7be84bba6abf25d6968c2c8ce2a346d43919ca0
Asami Doi <asamidoi@chromium.org>
2024-06-13 08:15:37 +0000

    [ios] Mitigate DCHECK failures by replacing them with DumpWithoutCrashing()
    This CL mitigates DCHEK failures by replacing them with DumpWithoutCrashing() and adds crash keys to make it easier to debug.
    
    Bug: 345153432
    Change-Id: I8c646079e546d8f42c5afc484d00928543f2c46a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5620972
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314481}

--- ios/chrome/browser/snapshots/model/legacy_snapshot_generator.mm
+++ ios/chrome/browser/snapshots/model/legacy_snapshot_generator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/snapshots/model/legacy_snapshot_generator.h"
 
+#import "base/debug/crash_logging.h"
+#import "base/debug/dump_without_crashing.h"
 #import "base/functional/bind.h"
 #import "build/blink_buildflags.h"
 #import "ios/chrome/browser/snapshots/model/model_swift.h"
@@ -202,7 +204,16 @@ struct SnapshotInfo {
   if (!baseImage) {
     return nil;
   }
-  DCHECK(!CGRectIsEmpty(frameInBaseView));
+  if (CGRectIsEmpty(frameInBaseView)) {
+    // TODO(crbug.com/345153432): This should not be empty but we have observed
+    // `frameInBaseView` being empty.
+    SCOPED_CRASH_KEY_NUMBER("Snapshots", "frame_in_base_view_height",
+                            frameInBaseView.size.height);
+    SCOPED_CRASH_KEY_NUMBER("Snapshots", "frame_in_base_view_width",
+                            frameInBaseView.size.width);
+    base::debug::DumpWithoutCrashing();
+    return nil;
+  }
 
   // Scale `frameInBaseView` to handle an image with 2x scale.
   CGFloat scale = baseImage.scale;
@@ -315,7 +326,15 @@ struct SnapshotInfo {
   snapshotInfo.snapshotFrameInWindow =
       [snapshotInfo.baseView convertRect:snapshotInfo.snapshotFrameInBaseView
                                   toView:nil];
-  DCHECK(!CGRectIsEmpty(snapshotInfo.snapshotFrameInWindow));
+  if (CGRectIsEmpty(snapshotInfo.snapshotFrameInWindow)) {
+    // TODO(crbug.com/345153432): This should not be empty but we have observed
+    // `snapshotInfo.snapshotFrameInWindow` being empty.
+    SCOPED_CRASH_KEY_NUMBER("Snapshots", "frame_in_window_height",
+                            snapshotInfo.snapshotFrameInWindow.size.height);
+    SCOPED_CRASH_KEY_NUMBER("Snapshots", "frame_in_window_width",
+                            snapshotInfo.snapshotFrameInWindow.size.width);
+    base::debug::DumpWithoutCrashing();
+  }
   return snapshotInfo;
 }
 
