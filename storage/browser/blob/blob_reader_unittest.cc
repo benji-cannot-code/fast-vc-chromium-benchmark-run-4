09c4d87efa1f1b2923738cdaf3099ec6c074931d
Lukasz Anforowicz <lukasza@chromium.org>
2024-06-13 15:35:26 +0000

    `span`-ify `mojo/.../data_pipe.h`: Migrate `storage`.
    `span`-ification: `mojo::DataPipe[Consumer|Producer]Handle`: Migrating
    callers.  This CL `span`-ifies the callers of the APIs in
    `mojo/public/cpp/system/data_pipe.h`:
    
    * `BeginReadData` and `BeginWriteData` return (via an out parameter)
      a `span` instead of returning a ptr+size pair.
    * `ReadData` and `WriteData` consume a `span` instead of consuming
      a ptr+size pair.
    
    For more details see the design note here:
    https://docs.google.com/document/d/1c4NKpXwpQ9MKK1SbJ4C6MvhXI8-KJZ4jq7N4VHTHJoI/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=jsbell@chromium.org
    
    Bug: 40284755
    Change-Id: I2652d87271bebad81a2a80e5e12e6cb28b505fcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5619213
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314600}

--- storage/browser/blob/blob_reader_unittest.cc
+++ storage/browser/blob/blob_reader_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/heap_array.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -689,11 +690,11 @@ TEST_F(BlobReaderTest, ReadableDataHandleSingle) {
   size_t num_bytes = base::checked_cast<size_t>(bytes_read);
   std::vector<uint8_t> buffer(num_bytes);
   MojoReadDataFlags flags = MOJO_READ_DATA_FLAG_ALL_OR_NONE;
-  MojoResult read_result = consumer->ReadData(buffer.data(), &num_bytes, flags);
+  MojoResult read_result = consumer->ReadData(flags, buffer, num_bytes);
   ASSERT_EQ(MOJO_RESULT_OK, read_result);
   ASSERT_EQ(kData.size(), num_bytes);
 
-  EXPECT_EQ(0, memcmp(buffer.data(), kData.c_str(), kData.size()));
+  EXPECT_EQ(base::as_string_view(base::as_byte_span(buffer)), kData);
 }
 
 // This test is the same as ReadableDataHandleSingle, but adds the
@@ -736,12 +737,13 @@ TEST_F(BlobReaderTest, ReadableDataHandleSingleRange) {
   size_t num_bytes = base::checked_cast<size_t>(bytes_read);
   std::vector<uint8_t> buffer(num_bytes);
   MojoReadDataFlags flags = MOJO_READ_DATA_FLAG_ALL_OR_NONE;
-  MojoResult read_result = consumer->ReadData(buffer.data(), &num_bytes, flags);
+  MojoResult read_result = consumer->ReadData(flags, buffer, num_bytes);
   ASSERT_EQ(MOJO_RESULT_OK, read_result);
   ASSERT_EQ(range_length, num_bytes);
 
-  EXPECT_EQ(0,
-            memcmp(buffer.data(), kData.c_str() + range_start, range_length));
+  EXPECT_EQ(
+      base::as_string_view(base::as_byte_span(buffer)).substr(0, num_bytes),
+      kData.substr(range_start, range_length));
 }
 
 TEST_F(BlobReaderTest, ReadableDataHandleMultipleSlices) {
--- storage/browser/file_system/file_writer_delegate.cc
+++ storage/browser/file_system/file_writer_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/location.h"
@@ -149,9 +150,10 @@ void FileWriterDelegate::Read() {
   }
 
   DCHECK(data_pipe_);
-  size_t num_bytes = io_buffer_->size();
-  MojoResult result = data_pipe_->ReadData(io_buffer_->data(), &num_bytes,
-                                           MOJO_READ_DATA_FLAG_NONE);
+  size_t num_bytes = 0;
+  MojoResult result = data_pipe_->ReadData(
+      MOJO_READ_DATA_FLAG_NONE, base::as_writable_bytes(io_buffer_->span()),
+      num_bytes);
   if (result == MOJO_RESULT_SHOULD_WAIT) {
     data_pipe_watcher_.ArmOrNotify();
     return;
