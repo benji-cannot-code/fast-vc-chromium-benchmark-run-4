8cb588269c65a6cfdf4a0234d40de78c7f491f07
Wan-Teh Chang <wtc@google.com>
2024-06-13 19:01:38 +0000

    Roll src/third_party/libaom/source/libaom/ d41d6652b..76b95d70f (7 commits)
    https://aomedia.googlesource.com/aom.git/+log/d41d6652b66c..76b95d70f8e3
    
    $ git log d41d6652b..76b95d70f --date=short --no-merges --format='%ad %ae %s'
    2024-06-12 jzern README.md: add example Copyright comment block
    2024-06-12 jzern *.{asm,cmake,inc,pl,py,txt}: normalize copyright line
    2021-05-28 wtc Set obu_extension_flag correctly for frame OBUs
    2024-06-11 jzern *.{c,cc,h}: normalize AOMedia copyright
    2024-05-10 jzern av1_rtcd_defs.pl: rm av1_highbd_inv_txfm_add_NxM
    2024-06-11 jzern av1_inv_txfm_neon.c: add missing prototype
    2024-06-11 marpan rtc: Fix source_sad setting near boundary
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Bug: b:307414544
    Change-Id: I235beb98938322696aa4a19214896784296898ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630051
    Reviewed-by: James Zern <jzern@google.com>
    Commit-Queue: James Zern <jzern@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1314782}

--- third_party/libaom/source/config/config/aom_version.h
+++ third_party/libaom/source/config/config/aom_version.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (c) 2024, Alliance for Open Media. All rights reserved
+ * Copyright (c) 2024, Alliance for Open Media. All rights reserved.
  *
  * This source code is subject to the terms of the BSD 2 Clause License and
  * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 9
 #define VERSION_PATCH 1
-#define VERSION_EXTRA "217-gd41d6652b6"
+#define VERSION_EXTRA "224-g76b95d70f8"
 #define VERSION_PACKED \
   ((VERSION_MAJOR << 16) | (VERSION_MINOR << 8) | (VERSION_PATCH))
-#define VERSION_STRING_NOSP "3.9.1-217-gd41d6652b6"
-#define VERSION_STRING " 3.9.1-217-gd41d6652b6"
+#define VERSION_STRING_NOSP "3.9.1-224-g76b95d70f8"
+#define VERSION_STRING " 3.9.1-224-g76b95d70f8"
