73cd1ba5c6205ce48b7972ef2b9ea8b791e56a6d
Lukasz Anforowicz <lukasza@chromium.org>
2024-06-13 00:29:58 +0000

    `span`-ify `mojo/.../data_pipe.h`: Migrate `services/device`.
    `span`-ification: `mojo::DataPipe[Consumer|Producer]Handle`: Migrating
    callers.  This CL `span`-ifies the callers of the APIs in
    `mojo/public/cpp/system/data_pipe.h`:
    
    * `BeginReadData` and `BeginWriteData` return (via an out parameter)
      a `span` instead of returning a ptr+size pair.
    * `ReadData` and `WriteData` consume a `span` instead of consuming
      a ptr+size pair.
    
    For more details see the design note here:
    https://docs.google.com/document/d/1c4NKpXwpQ9MKK1SbJ4C6MvhXI8-KJZ4jq7N4VHTHJoI/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=blundell@chromium.org
    
    Bug: 40284755
    Change-Id: Ia1457dc83242b03269814b114645f679ff8055e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5619212
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314357}

--- services/device/serial/serial_port_impl.cc
+++ services/device/serial/serial_port_impl.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/task/single_thread_task_runner.h"
+#include "mojo/public/cpp/system/data_pipe.h"
 #include "services/device/serial/serial_io_handler.h"
 
 namespace device {
@@ -212,19 +213,16 @@ void SerialPortImpl::Close(bool flush, CloseCallback callback) {
 
 void SerialPortImpl::WriteToPort(MojoResult result,
                                  const mojo::HandleSignalsState& state) {
-  const void* buffer;
-  size_t num_bytes;
+  base::span<const uint8_t> buffer;
 
   if (result == MOJO_RESULT_OK) {
     DCHECK(in_stream_);
-    result = in_stream_->BeginReadData(&buffer, &num_bytes,
-                                       MOJO_WRITE_DATA_FLAG_NONE);
+    result = in_stream_->BeginReadData(MOJO_WRITE_DATA_FLAG_NONE, buffer);
   }
   if (result == MOJO_RESULT_OK) {
-    io_handler_->Write(
-        base::make_span(static_cast<const uint8_t*>(buffer), num_bytes),
-        base::BindOnce(&SerialPortImpl::OnWriteToPortCompleted,
-                       weak_factory_.GetWeakPtr()));
+    io_handler_->Write(buffer,
+                       base::BindOnce(&SerialPortImpl::OnWriteToPortCompleted,
+                                      weak_factory_.GetWeakPtr()));
     return;
   }
   if (result == MOJO_RESULT_SHOULD_WAIT) {
@@ -268,17 +266,16 @@ void SerialPortImpl::OnWriteToPortCompleted(uint32_t bytes_sent,
 void SerialPortImpl::ReadFromPortAndWriteOut(
     MojoResult result,
     const mojo::HandleSignalsState& state) {
-  void* buffer;
-  size_t num_bytes = mojo::DataPipeProducerHandle::kNoSizeHint;
+  base::span<uint8_t> buffer;
   if (result == MOJO_RESULT_OK) {
     DCHECK(out_stream_);
-    result = out_stream_->BeginWriteData(&buffer, &num_bytes,
-                                         MOJO_WRITE_DATA_FLAG_NONE);
+    result =
+        out_stream_->BeginWriteData(mojo::DataPipeProducerHandle::kNoSizeHint,
+                                    MOJO_WRITE_DATA_FLAG_NONE, buffer);
   }
   if (result == MOJO_RESULT_OK) {
-    io_handler_->Read(base::make_span(static_cast<uint8_t*>(buffer), num_bytes),
-                      base::BindOnce(&SerialPortImpl::WriteToOutStream,
-                                     weak_factory_.GetWeakPtr()));
+    io_handler_->Read(buffer, base::BindOnce(&SerialPortImpl::WriteToOutStream,
+                                             weak_factory_.GetWeakPtr()));
     return;
   }
   if (result == MOJO_RESULT_SHOULD_WAIT) {
