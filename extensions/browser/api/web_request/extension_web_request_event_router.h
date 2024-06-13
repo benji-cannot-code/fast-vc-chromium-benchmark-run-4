7a8a7c49756cb63da9b10b4536b67a7fa50863f5
David Bertoni <dbertoni@chromium.org>
2024-06-13 00:29:10 +0000

    [Extensions] Do some header files cleanup.
    Drive-by cleanup.
    
    Bug: 40593486
    Change-Id: Ia0dbe72d6680b8ca173e3c1796a8979796ff6be1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5610193
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314356}

--- extensions/browser/api/web_request/extension_web_request_event_router.h
+++ extensions/browser/api/web_request/extension_web_request_event_router.h
@@ -21,13 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/values.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "extensions/browser/api/declarative_net_request/action_tracker.h"
 #include "extensions/browser/api/declarative_webrequest/request_stage.h"
-#include "extensions/browser/api/declarative_webrequest/webrequest_rules_registry.h"
 #include "extensions/browser/api/web_request/web_request_api_helpers.h"
-#include "extensions/browser/api/web_request/web_request_permissions.h"
 #include "extensions/browser/extension_event_histogram_value.h"
-#include "extensions/browser/guest_view/guest_view_events.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/url_pattern_set.h"
 
@@ -49,7 +45,6 @@ class WebRequestRulesRegistry;
 class WebRequestEventDetails;
 struct WebRequestInfo;
 
-// This class defines common types for the two types of event routers.
 class WebRequestEventRouter : public KeyedService {
  public:
   explicit WebRequestEventRouter(content::BrowserContext* browser_context);
