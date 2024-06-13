0015a87c2c834c2a080bea02f72e661aaed1e15b
Joshua Pawlicki <waffles@chromium.org>
2024-06-13 15:20:10 +0000

    Reland "Updater: Periodically trigger the updater periodic tasks on Windows."
    Patch set 1 is the original CL. The main fix is in chrome/browser/BUILD.gn: moving the dep out of an !is_android section.
    
    This is a reland of commit 9b44dae398c2b0dc98085da8b98af3338b6c2111
    
    Original change's description:
    > Updater: Periodically trigger the updater periodic tasks on Windows.
    >
    > Bug: crbug.com/40239172
    > Change-Id: Iea06cc17fc07f6cd73e6ace963ea6bcf1f08ef57
    > Cq-Include-Trybots: luci.chrome.try:linux-chrome,mac-chrome,win-chrome,chromeos-eve-chrome
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605548
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1313669}
    
    Bug: crbug.com/40239172
    Change-Id: I4307462a21880fb75b7cfcbaea329ec01a3aa55a
    Cq-Include-Trybots: luci.chrome.try:linux-chrome,mac-chrome,win-chrome,chromeos-eve-chrome
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626152
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314584}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -207,6 +207,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/component_updater/registration.h"
 #endif  // BUILDFLAG(ENABLE_COMPONENT_UPDATER)
 
+#if BUILDFLAG(ENABLE_UPDATER)
+#include "chrome/browser/updater/scheduler.h"
+#endif
+
 #if BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/share/share_history.h"
@@ -881,6 +885,10 @@ void ChromeBrowserMainParts::PreCreateMainMessageLoop() {
 
   for (auto& chrome_extra_part : chrome_extra_parts_)
     chrome_extra_part->PreCreateMainMessageLoop();
+
+#if BUILDFLAG(ENABLE_UPDATER)
+  updater::SchedulePeriodicTasks();
+#endif  // BUILDFLAG(ENABLE_UPDATER)
 }
 
 void ChromeBrowserMainParts::PostCreateMainMessageLoop() {
