354228c663cc239cd93240471254b4d567369cf9
Stefan Zager <szager@chromium.org>
2024-06-13 22:46:22 +0000

    Zoom iframe contents according to CSS zoom on iframe element
    Zoom factor is set on a root LocalFrame by its corresponding
    WebFrameWidget, and previously the root frame would turn around and
    apply the same zoom factor to all of its descendant frames without
    regard for CSS zoom values on the <iframe> element in the embedding
    document. With this change, CSS zoom value on an <iframe> multiplies
    the frame-level zoom factor of the embedded document.
    
    One result of this change is that zoom factor is no longer propagated
    synchronously down the frame tree: a style update is required to
    propagatee zoom factor down the frame hierarchy.
    
    Bug: chromium:329482480
    Change-Id: I729d559264afe8edb4fef7bc3b18304b71581625
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564200
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314932}

--- third_party/blink/renderer/core/exported/web_view_impl.h
+++ third_party/blink/renderer/core/exported/web_view_impl.h
@@ -640,6 +640,7 @@ class CORE_EXPORT WebViewImpl final : public WebView,
 
   double ClampZoomLevel(double zoom_level) const;
   double ZoomLevelToZoomFactor(double zoom_level, bool for_main_frame) const;
+  double ZoomFactorToZoomLevel(double zoom_factor) const;
 
  private:
   FRIEND_TEST_ALL_PREFIXES(WebFrameTest, DivScrollIntoEditableTest);
