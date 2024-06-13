646f28ab3b9fef7adc10c0141ee94ef61be9d63b
Yu Zhong <yuzhong@google.com>
2024-06-13 22:19:05 +0000

    [Read Aloud] Refactor getLanguageDownloadedTitle_ to follow styling guideline
    Use class member instead of function parameters as the styleguide mandates.
    
    Bug: 343966559
    Change-Id: I60af9c928a6691cda078b42949ad3455a7007437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631001
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Yu Zhong <yuzhong@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1314911}

--- chrome/browser/resources/side_panel/read_anything/language_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/language_menu.ts
@@ -89,6 +89,7 @@ export class LanguageMenuElement extends LanguageMenuElementBase {
   selectedLang: string;
   localeToDisplayName: {[lang: string]: string};
   enabledLangs: string[];
+  lastDownloadedLang: string;
 
   availableVoices: SpeechSynthesisVoice[];
   private languageSearchValue_: string;
@@ -201,9 +202,9 @@ export class LanguageMenuElement extends LanguageMenuElementBase {
         lang;
   }
 
-  private getLanguageDownloadedTitle_(
-      localeToDisplayName: {[lang: string]: string}, lang: string) {
-    const langDisplayName = this.getDisplayName(localeToDisplayName, lang);
+  private getLanguageDownloadedTitle_() {
+    const langDisplayName =
+        this.getDisplayName(this.localeToDisplayName, this.lastDownloadedLang);
 
     return loadTimeData.getStringF(
         'readingModeVoiceDownloadedTitle', langDisplayName);
