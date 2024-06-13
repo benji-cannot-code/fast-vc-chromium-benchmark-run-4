cd861f68584549abcffb8166e61242216f9427aa
Ayu Ishii <ayui@chromium.org>
2024-06-13 20:10:08 +0000

    SyncFileSystem: Make polling interval configurable
    This change adds a FeatureParam to configure polling
    interval, for retrieving invalidations from Drive. This
    is added so it can be configured by finch if we can no
    longer rely on the invalidation service for subscribing
    to invalidations.
    
    Bug: 338254621
    Change-Id: Ia6cfac5fc60ffe119e7abd6b9787083988b7566f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623698
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1314821}

--- components/drive/drive_notification_manager.h
+++ components/drive/drive_notification_manager.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/default_tick_clock.h"
 #include "base/timer/timer.h"
 #include "components/drive/drive_notification_observer.h"
+#include "components/drive/features.h"
 #include "components/invalidation/public/invalidation_handler.h"
 #include "components/invalidation/public/invalidation_util.h"
 #include "components/keyed_service/core/keyed_service.h"
