4a86bc3826fa1145da3eadb5d81b8baa7c432de4
Paras Awasthi <awasthiparas6@gmail.com>
2024-06-14 20:36:44 +0000

    PowerBookmarks: Migrate power-bookmark-row to Lit part2
    In this part checking in the power_bookmark_row.html.ts file instead of auto-generating it.
    
    Bug: 346148109
    Change-Id: I6c48d1605ce0a0a1b1463db1d767d9e6d5aa194a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632385
    Commit-Queue: Paras Awasthi <awasthiparas6@gmail.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315431}

--- chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
+++ chrome/browser/resources/side_panel/bookmarks/power_bookmark_row.html.ts
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {PowerBookmarkRowElement} from './power_bookmark_row.ts';
+
+export function getHtml(this: PowerBookmarkRowElement) {
+  return html`
 <cr-url-list-item id="crUrlListItem"
     role="listitem"
     .size="${this.listItemSize}"
@@ -47,4 +57,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   ${this.isShoppingCollection ? html`
     <iron-icon slot="folder-icon" icon="bookmarks:shopping-collection">
         </iron-icon>` : ''}
-</cr-url-list-item>
+</cr-url-list-item>`;
+}
