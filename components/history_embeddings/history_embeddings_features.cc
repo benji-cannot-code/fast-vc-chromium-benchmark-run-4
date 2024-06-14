f89810e199a39498206f25473a78c44e4652fbc7
Orin Jaworski <orinj@google.com>
2024-06-14 22:33:10 +0000

    Use database as cache for passages and embeddings to bypass embedder
    This CL saves the embedder from having to recompute embeddings for
    passages that are already stored in the database when a visit is
    made to the same URL. Instead of computing the embedding with the
    model, the stored embedding is reused.
    
    Additionally, this CL adds histogram logging for cancellation of
    passage extraction, embedding, and storage. Various reason codes
    are logged so we can identify the step at which cancellation occurred.
    The tab helper should dominate, but later cancellation in the service
    might signal problems or opportunities to optimize.
    
    Also, this CL fixes b/347306992 using live loading tab count
    instead of a member value updated via notifications that could
    be missed when a loading tab is closed. (Random bug noticed during
    development.)
    
    Bug: 345819418
    Change-Id: Icaacf456717b9d88c0e14b3c8f3a1e31835440c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5629651
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1315485}

--- components/history_embeddings/history_embeddings_features.cc
+++ components/history_embeddings/history_embeddings_features.cc
@@ -93,4 +93,9 @@ const base::FeatureParam<bool> kRebuildEmbeddings(&kHistoryEmbeddings,
                                                   "RebuildEmbeddings",
                                                   true);
 
+const base::FeatureParam<bool> kUseDatabaseBeforeEmbedder(
+    &kHistoryEmbeddings,
+    "UseDatabaseBeforeEmbedder",
+    true);
+
 }  // namespace history_embeddings
