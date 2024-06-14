29a38b91bf4db68a062e52d55b6803e713837f2c
Joe Mason <joenotcharles@google.com>
2024-06-14 15:45:22 +0000

    [PM] Add GraphOwnedAndRegistered helper class
    Also converts DCHECK's in OwnedObjects and RegisteredObjects to
    CHECK's to make it easier to catch errors triggered by the new class.
    
    R=anthonyvd
    
    Bug: 346832106
    Change-Id: Idcf3e17fb7236284c536e894337c986593bc89fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626139
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315252}

--- components/performance_manager/graph/graph_registered_unittest.cc
+++ components/performance_manager/graph/graph_registered_unittest.cc
@@ -5,8 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/public/graph/graph_registered.h"
 
+#include <memory>
+#include <utility>
+
 #include "base/test/gtest_util.h"
 #include "components/performance_manager/performance_manager_registry_impl.h"
+#include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/performance_manager.h"
 #include "components/performance_manager/test_support/graph_test_harness.h"
 #include "components/performance_manager/test_support/performance_manager_test_harness.h"
@@ -29,6 +33,32 @@ class Bar : public GraphRegisteredImpl<Bar> {
   ~Bar() override = default;
 };
 
+// A GraphOwnedAndRegistered that doesn't override OnPassedToGraph and
+// OnTakenFromGraph.
+class OwnedFoo final : public GraphOwnedAndRegistered<OwnedFoo> {
+ public:
+  OwnedFoo() = default;
+  ~OwnedFoo() final = default;
+};
+
+// A GraphOwnedAndRegistered that overrides OnPassedToGraph and
+// OnTakenFromGraph.
+class OwnedBar final : public GraphOwnedAndRegistered<OwnedBar> {
+ public:
+  OwnedBar() = default;
+  ~OwnedBar() final = default;
+
+  void OnPassedToGraph(Graph* graph) final { on_passed_called_ = true; }
+  void OnTakenFromGraph(Graph* graph) final { on_taken_called_ = true; }
+
+  bool on_passed_called() const { return on_passed_called_; }
+  bool on_taken_called() const { return on_taken_called_; }
+
+ private:
+  bool on_passed_called_ = false;
+  bool on_taken_called_ = false;
+};
+
 TEST_F(GraphRegisteredTest, GraphRegistrationWorks) {
   // This ensures that the templated distinct TypeId generation works.
   ASSERT_NE(Foo::TypeId(), Bar::TypeId());
@@ -47,11 +77,11 @@ TEST_F(GraphRegisteredTest, GraphRegistrationWorks) {
   EXPECT_FALSE(graph()->GetRegisteredObjectAs<Bar>());
 
   // Inserting again fails.
-  EXPECT_DCHECK_DEATH(graph()->RegisterObject(&foo));
+  EXPECT_CHECK_DEATH(graph()->RegisterObject(&foo));
 
   // Unregistered the wrong object fails.
   Foo foo2;
-  EXPECT_DCHECK_DEATH(graph()->UnregisterObject(&foo2));
+  EXPECT_CHECK_DEATH(graph()->UnregisterObject(&foo2));
 
   // Unregistering works.
   graph()->UnregisterObject(&foo);
@@ -61,8 +91,8 @@ TEST_F(GraphRegisteredTest, GraphRegistrationWorks) {
   EXPECT_FALSE(graph()->GetRegisteredObjectAs<Bar>());
 
   // Unregistering again fails.
-  EXPECT_DCHECK_DEATH(graph()->UnregisterObject(&foo));
-  EXPECT_DCHECK_DEATH(graph()->UnregisterObject(&foo2));
+  EXPECT_CHECK_DEATH(graph()->UnregisterObject(&foo));
+  EXPECT_CHECK_DEATH(graph()->UnregisterObject(&foo2));
 
   // Registering multiple objects works.
   Bar bar;
@@ -90,9 +120,46 @@ TEST_F(GraphRegisteredTest, GraphRegistrationWorks) {
 
   // At this point if the graph is torn down it should explode because foo
   // hasn't been unregistered.
-  EXPECT_DCHECK_DEATH(TearDownAndDestroyGraph());
+  EXPECT_CHECK_DEATH(TearDownAndDestroyGraph());
 
   graph()->UnregisterObject(&foo);
+}
+
+TEST_F(GraphRegisteredTest, GraphOwnedAndRegistered) {
+  // Insertion works.
+  EXPECT_FALSE(graph()->GetRegisteredObjectAs<OwnedFoo>());
+  OwnedFoo* foo = graph()->PassToGraph(std::make_unique<OwnedFoo>());
+  EXPECT_EQ(foo, graph()->GetRegisteredObjectAs<OwnedFoo>());
+
+  EXPECT_FALSE(graph()->GetRegisteredObjectAs<OwnedBar>());
+  OwnedBar* bar = graph()->PassToGraph(std::make_unique<OwnedBar>());
+  EXPECT_EQ(bar, graph()->GetRegisteredObjectAs<OwnedBar>());
+  EXPECT_TRUE(bar->on_passed_called());
+
+  // Inserting again fails.
+  EXPECT_CHECK_DEATH(graph()->RegisterObject(foo));
+  EXPECT_CHECK_DEATH(graph()->PassToGraph(std::make_unique<OwnedFoo>()));
+
+  // Unregistering works.
+  std::unique_ptr<OwnedFoo> foo2 = graph()->TakeFromGraphAs<OwnedFoo>(foo);
+  EXPECT_EQ(foo, foo2.get());
+  EXPECT_EQ(nullptr, graph()->GetRegisteredObjectAs<OwnedFoo>());
+
+  std::unique_ptr<OwnedBar> bar2 = graph()->TakeFromGraphAs<OwnedBar>(bar);
+  EXPECT_EQ(bar, bar2.get());
+  EXPECT_EQ(nullptr, graph()->GetRegisteredObjectAs<OwnedBar>());
+  EXPECT_TRUE(bar->on_taken_called());
+
+  // Unregistering again fails.
+  EXPECT_CHECK_DEATH(graph()->UnregisterObject(foo));
+
+  // Passing back an object that was taken should re-register it.
+  graph()->PassToGraph(std::move(foo2));
+  EXPECT_EQ(foo, graph()->GetRegisteredObjectAs<OwnedFoo>());
+
+  // At this point the graph can be safely torn down because
+  // GraphOwnedAndRegistered objects will be deleted and unregistered.
+  TearDownAndDestroyGraph();
 }
 
 namespace {
