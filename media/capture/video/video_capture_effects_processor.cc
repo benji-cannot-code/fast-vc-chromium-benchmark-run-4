81dba2445453bfb8da311fb136b87f5b1163f33a
Saifuddin Hitawala <hitawala@chromium.org>
2024-06-14 15:35:32 +0000

    [media] Rename SharedImageHandles to SharedImageHandle
    Rename SharedImageHandles to SharedImageHandle as the
    SharedImageBufferHandleSet mojo now holds a single ExportedSharedImage.
    
    Bug: None
    Change-Id: Ia934cb60d8ce54503175569e83e5ef67797f960c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630277
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315248}

--- media/capture/video/video_capture_effects_processor.cc
+++ media/capture/video/video_capture_effects_processor.cc
@@ -56,7 +56,7 @@ mojom::VideoBufferHandlePtr CreateBufferHandle(
       auto shared_image_set = mojom::SharedImageBufferHandleSet::New(
           shared_image->Export(), sync_token);
 
-      return mojom::VideoBufferHandle::NewSharedImageHandles(
+      return mojom::VideoBufferHandle::NewSharedImageHandle(
           std::move(shared_image_set));
     }
     case VideoCaptureBufferType::kMailboxHolder:
