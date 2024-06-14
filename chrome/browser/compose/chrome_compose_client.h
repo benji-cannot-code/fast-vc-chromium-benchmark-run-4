6a6f2a9c75bcc4d9afacc1c9a80b6e6ee33b4d72
Christoph Schwering <schwering@google.com>
2024-06-14 15:34:24 +0000

    [Autofill][Compose] Fix DEPS issue
    There are the following includes:
    chrome_autofill_client.cc
     |
     +-- chrome_compose_client.h
          |
          +-- compose_enabling.h
          |    |
          +----+-- compose_optimization_guide.pb.h
    
    This is not reflected in chrome_autofill_client.cc's dependencies.
    
    This CL removes these includes from chrome_compose_client.h.
    
    Bug: 345656097
    Change-Id: I3a1b73a887b59f9a86d14fccbb3e42d4d05ae738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631174
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315246}

--- chrome/browser/compose/chrome_compose_client.h
+++ chrome/browser/compose/chrome_compose_client.h
@@ -13,10 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/gtest_prod_util.h"
 #include "base/token.h"
-#include "chrome/browser/compose/compose_enabling.h"
 #include "chrome/browser/compose/compose_session.h"
 #include "chrome/browser/compose/proactive_nudge_tracker.h"
-#include "chrome/browser/compose/proto/compose_optimization_guide.pb.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "chrome/common/compose/compose.mojom.h"
 #include "components/autofill/content/browser/scoped_autofill_managers_observation.h"
@@ -42,6 +40,8 @@ namespace content {
 class Page;
 class WebContents;
 }  // namespace content
+
+class ComposeEnabling;
 
 // An implementation of `ComposeClient` for Desktop and Android.
 class ChromeComposeClient
@@ -184,10 +184,6 @@ class ChromeComposeClient
   // compose::ProactiveNudgeTracker implementation.
   void ShowProactiveNudge(autofill::FormGlobalId form,
                           autofill::FieldGlobalId field) override;
-
-  // This API gets optimization guidance for a web site.  We use this
-  // to guide our decision to enable the feature and trigger the nudge.
-  compose::ComposeHintDecision GetOptimizationGuidanceForUrl(const GURL& url);
 
   ComposeEnabling& GetComposeEnabling();
 
--- chrome/browser/ui/views/compose/compose_interactive_uitest.cc
+++ chrome/browser/ui/views/compose/compose_interactive_uitest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/bind.h"
 #include "chrome/browser/optimization_guide/mock_optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
+#include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_test_util.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
