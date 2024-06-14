d356f5f59975c09b9d6e66e19a6acd630107f8a4
Elly <ellyjones@chromium.org>
2024-06-14 21:54:44 +0000

    views controls: fix unsafe buffer warnings
    This mostly involves a bunch of swaps from raw arrays to std::arrays.
    
    Bug: 344639839
    Change-Id: Ic3b11329bbb7ae6d3a69bfefa664450982a84ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631074
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315467}

--- ui/views/controls/button/image_button.cc
+++ ui/views/controls/button/image_button.cc
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(UNSAFE_BUFFERS_BUILD)
-// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
-// then remove this pragma.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ui/views/controls/button/image_button.h"
 
 #include <utility>
@@ -303,8 +297,9 @@ void ToggleImageButton::SetToggled(bool toggled) {
   if (toggled == toggled_)
     return;
 
-  for (int i = 0; i < STATE_COUNT; ++i)
+  for (size_t i = 0; i < STATE_COUNT; ++i) {
     std::swap(images_[i], alternate_images_[i]);
+  }
   toggled_ = toggled;
 
   OnPropertyChanged(&toggled_, kPropertyEffectsPaint);
--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(UNSAFE_BUFFERS_BUILD)
-// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
-// then remove this pragma.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ui/views/controls/button/label_button.h"
 
 #include <stddef.h>
@@ -671,9 +665,9 @@ void LabelButton::VisualStateChanged() {
 
 void LabelButton::ResetColorsFromNativeTheme() {
   // Since this is a LabelButton, use the label colors.
-  ui::ColorId color_ids[STATE_COUNT] = {
-      ui::kColorLabelForeground, ui::kColorLabelForeground,
-      ui::kColorLabelForeground, ui::kColorLabelForegroundDisabled};
+  constexpr std::array<ui::ColorId, STATE_COUNT> color_ids{
+      {ui::kColorLabelForeground, ui::kColorLabelForeground,
+       ui::kColorLabelForeground, ui::kColorLabelForegroundDisabled}};
 
   label_->SetBackground(nullptr);
   label_->SetAutoColorReadabilityEnabled(false);
