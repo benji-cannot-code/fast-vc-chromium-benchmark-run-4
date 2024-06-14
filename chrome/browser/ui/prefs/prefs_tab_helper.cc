f7e3eee4475bc35f6d0193013b97b231f52f09bf
Kent Tamura <tkent@chromium.org>
2024-06-14 06:39:07 +0000

    mac: Support comma-separated font lists
    This CL fixes an issue that chrome://settings/fonts didn't show a font
    name from a font list.
    
    Note: Page rendering behavior is not changed. Blink supports
    comma-separated font lists.
    
    Bug: 40535332
    Change-Id: I1c62a6987bff454aee55f9dfcb0a56c631079861
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5633154
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315054}

--- chrome/browser/ui/prefs/prefs_tab_helper.cc
+++ chrome/browser/ui/prefs/prefs_tab_helper.cc
@@ -64,6 +64,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
+#if BUILDFLAG(IS_MAC)
+#include "ui/gfx/font_list.h"
+#endif
+
 using blink::web_pref::WebPreferences;
 using content::WebContents;
 
@@ -111,11 +115,13 @@ ALL_FONT_SCRIPTS(WEBKIT_WEBPREFS_FONTS_STANDARD)
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
 // Resolves the comma-separated font list to the first available font in the
 // default value. crbug.com/41323186.
 BASE_FEATURE(kPrefsFontList, "PrefsFontList", base::FEATURE_ENABLED_BY_DEFAULT);
+#endif
 
+#if BUILDFLAG(IS_WIN)
 // On Windows with antialiasing we want to use an alternate fixed font like
 // Consolas, which looks much better than Courier New.
 bool ShouldUseAlternateDefaultFixedFont(const std::string& script) {
@@ -442,6 +448,15 @@ void PrefsTabHelper::RegisterProfilePrefs(
           base::FeatureList::IsEnabled(kPrefsFontList)) {
         value = FirstAvailableFont(value);
       }
+#elif BUILDFLAG(IS_MAC)
+      if (value.starts_with(',') &&
+          base::FeatureList::IsEnabled(kPrefsFontList)) {
+        value = gfx::FontList::FirstAvailableOrFirst(value);
+      }
+#else
+      DCHECK(!value.starts_with(','))
+          << "This platform doesn't support default font lists. "
+          << pref.pref_name << "=" << value;
 #endif
       registry->RegisterStringPref(pref.pref_name, value);
       fonts_with_defaults.insert(pref.pref_name);
