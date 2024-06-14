bf8694a5dad29b4a06055e8e9e4339c9591d2897
Jan Keitel <jkeitel@google.com>
2024-06-14 15:55:42 +0000

    Use plus address icons from plus_address component.
    https://crrev.com/c/5607669 added plus_address icons to
    //components/vector_icons and used it in Autofill suggestions and
    in the plus address creation dialog. This was a mistake - there was
    already an existing (internal) vector icon target inside
    //component/plus_addresses. This CL transitions to this target. A
    follow-up will remove the icons from vector_icons build target.
    
    Bug: 342125189
    Change-Id: I598e756420c69b1ddc5fa4ec5a8ea4b504821f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630507
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315257}

--- chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
+++ chrome/browser/ui/views/autofill/popup/popup_cell_utils.cc
@@ -42,9 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/layout_types.h"
 #include "ui/views/layout/table_layout.h"
 #include "ui/views/view_class_properties.h"
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#include "components/plus_addresses/resources/vector_icons.h"
-#endif
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -57,6 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/flex_layout.h"
 #include "ui/views/layout/table_layout_view.h"
 #include "ui/views/view.h"
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+#include "components/plus_addresses/resources/vector_icons.h"
+#endif
 
 namespace autofill::popup_cell_utils {
 
@@ -254,7 +255,7 @@ std::optional<ui::ImageModel> GetIconImageModelFromIcon(Suggestion::Icon icon) {
 #endif
     case Suggestion::Icon::kPlusAddressSmall:
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-      return ImageModelFromVectorIcon(vector_icons::kPlusAddressLogoSmallIcon,
+      return ImageModelFromVectorIcon(plus_addresses::kPlusAddressLogoSmallIcon,
                                       kIconSize);
 #else
       return ImageModelFromVectorIcon(vector_icons::kEmailIcon, kIconSize);
--- chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.cc
+++ chrome/browser/ui/views/plus_addresses/plus_address_creation_dialog_delegate.cc
@@ -54,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/style/typography.h"
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
+
 #if BUILDFLAG(GOOGLE_CHROME_BRANDING)
 #include "components/plus_addresses/resources/vector_icons.h"
 #endif
@@ -74,15 +75,14 @@ const int kPlusAddressRefreshColumnWidth = 48;
 const gfx::VectorIcon& kGoogleGLogoIcon = vector_icons::kGoogleGLogoIcon;
 const gfx::VectorIcon& kDarkGoogleGLogoIcon =
     vector_icons::kGoogleGLogoMonochromeIcon;
-const gfx::VectorIcon& kPlusAddressLogoIcon =
-    plus_addresses::kPlusAddressesLogoIcon;
-const gfx::VectorIcon& kPlusAddressLogoLargeIcon =
-    vector_icons::kPlusAddressLogoLargeIcon;
+const gfx::VectorIcon& kLogoIcon = plus_addresses::kPlusAddressesLogoIcon;
+const gfx::VectorIcon& kLogoLargeIcon =
+    plus_addresses::kPlusAddressLogoLargeIcon;
 #else
 const gfx::VectorIcon& kGoogleGLogoIcon = vector_icons::kProductIcon;
 const gfx::VectorIcon& kDarkGoogleGLogoIcon = vector_icons::kProductIcon;
-const gfx::VectorIcon& kPlusAddressLogoIcon = vector_icons::kProductIcon;
-const gfx::VectorIcon& kPlusAddressLogoLargeIcon = vector_icons::kProductIcon;
+const gfx::VectorIcon& kLogoIcon = vector_icons::kProductIcon;
+const gfx::VectorIcon& kLogoLargeIcon = vector_icons::kProductIcon;
 #endif
 
 int GetPlusAddressLabelVerticalMargin() {
@@ -148,11 +148,10 @@ PlusAddressCreationDialogDelegate::PlusAddressCreationDialogDelegate(
         views::kMarginsKey,
         gfx::Insets::VH(GetPlusAddressLabelVerticalMargin(), 0));
   } else {
-    logo_image =
-        views::Builder<views::ImageView>()
-            .SetImage(ui::ImageModel::FromVectorIcon(
-                kPlusAddressLogoIcon, ui::kColorIcon, kPlusAddressLogoWidth))
-            .Build();
+    logo_image = views::Builder<views::ImageView>()
+                     .SetImage(ui::ImageModel::FromVectorIcon(
+                         kLogoIcon, ui::kColorIcon, kPlusAddressLogoWidth))
+                     .Build();
   }
   primary_view->AddChildView(std::move(logo_image));
 
@@ -294,9 +293,8 @@ PlusAddressCreationDialogDelegate::PlusAddressCreationDialogDelegate(
   if (add_plus_address_icon) {
     plus_address_label_container_->AddChildView(
         views::Builder<views::ImageView>()
-            .SetImage(ui::ImageModel::FromVectorIcon(kPlusAddressLogoLargeIcon,
-                                                     ui::kColorIcon,
-                                                     kPlusAddressIconWidth))
+            .SetImage(ui::ImageModel::FromVectorIcon(
+                kLogoLargeIcon, ui::kColorIcon, kPlusAddressIconWidth))
             .Build());
   }
 
