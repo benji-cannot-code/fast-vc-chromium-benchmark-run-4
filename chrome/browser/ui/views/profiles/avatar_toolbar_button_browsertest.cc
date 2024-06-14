d2603d78656304e1d419ac18420f099aa6ee8bbc
Ryan Sultanem <rsult@google.com>
2024-06-14 12:45:47 +0000

    [Uno-D] Add temporary accessibility labels to the identity pill states
    Labels added on the identity pill states:
    - Signin pending state: Reads "Verify it's you" when collapsed.
    - Chrome Signin bubble showing: reads "Close" in addition to the pill
    content as a description.
    
    Final strings to be added in a follow up CL.
    
    Bug: 346602524, 346600155
    Change-Id: Ie129f901441bf09a5817dbd83d15ba8a118e406a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630976
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1315157}

--- chrome/browser/ui/views/profiles/avatar_toolbar_button_browsertest.cc
+++ chrome/browser/ui/views/profiles/avatar_toolbar_button_browsertest.cc
@@ -746,8 +746,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonBrowserTest,
   ExpectSyncPaused(avatar_button);
 
   std::u16string profile_switch_text(u"Profile Switch?");
-  base::ScopedClosureRunner hide_callback =
-      avatar_button->ShowExplicitText(profile_switch_text);
+  base::ScopedClosureRunner hide_callback = avatar_button->ShowExplicitText(
+      profile_switch_text, /*accessibility_label=*/std::nullopt);
   EXPECT_EQ(avatar_button->GetText(), profile_switch_text);
 
   // Clearing explicit text should go back to Sync Pause.
@@ -771,8 +771,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonBrowserTest,
 
   EnableSyncAndWait(u"test@gmail.com");
   std::u16string profile_switch_text(u"Profile Switch?");
-  base::ScopedClosureRunner hide_callback =
-      avatar_button->ShowExplicitText(profile_switch_text);
+  base::ScopedClosureRunner hide_callback = avatar_button->ShowExplicitText(
+      profile_switch_text, /*accessibility_label=*/std::nullopt);
   EXPECT_EQ(avatar_button->GetText(), profile_switch_text);
 
   SimulateSyncPaused();
@@ -790,7 +790,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonBrowserTest,
   ASSERT_EQ(avatar->GetText(), std::u16string());
 
   std::u16string new_text(u"Some New Text");
-  base::ScopedClosureRunner hide_callback = avatar->ShowExplicitText(new_text);
+  base::ScopedClosureRunner hide_callback =
+      avatar->ShowExplicitText(new_text, /*accessibility_label=*/std::nullopt);
 
   EXPECT_EQ(avatar->GetText(), new_text);
   hide_callback.RunAndReset();
@@ -807,8 +808,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonBrowserTest,
   // the caller.
   {
     std::u16string new_text(u"Some New Text");
-    base::ScopedClosureRunner hide_callback =
-        avatar->ShowExplicitText(new_text);
+    base::ScopedClosureRunner hide_callback = avatar->ShowExplicitText(
+        new_text, /*accessibility_label=*/std::nullopt);
     EXPECT_EQ(avatar->GetText(), new_text);
   }
 
@@ -821,14 +822,14 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonBrowserTest,
   ASSERT_EQ(avatar->GetText(), std::u16string());
 
   std::u16string initial_new_text(u"Some New Text");
-  base::ScopedClosureRunner initial_hide_callback =
-      avatar->ShowExplicitText(initial_new_text);
+  base::ScopedClosureRunner initial_hide_callback = avatar->ShowExplicitText(
+      initial_new_text, /*accessibility_label=*/std::nullopt);
 
   EXPECT_EQ(avatar->GetText(), initial_new_text);
 
   std::u16string override_new_text(u"Some New Override Text");
-  base::ScopedClosureRunner override_hide_callback =
-      avatar->ShowExplicitText(override_new_text);
+  base::ScopedClosureRunner override_hide_callback = avatar->ShowExplicitText(
+      override_new_text, /*accessibility_label=*/std::nullopt);
 
   EXPECT_EQ(avatar->GetText(), override_new_text);
 
@@ -1133,7 +1134,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonEnterpriseBadgingBrowserTest,
     chrome::enterprise_util::SetUserAcceptedAccountManagement(
         browser()->profile(), true);
     EXPECT_EQ(avatar_button->GetText(), work_label);
-    auto clear_closure = avatar_button->ShowExplicitText(u"Explicit text");
+    auto clear_closure = avatar_button->ShowExplicitText(
+        u"Explicit text", /*accessibility_label=*/std::nullopt);
     EXPECT_NE(avatar_button->GetText(), work_label);
     clear_closure.RunAndReset();
     EXPECT_EQ(avatar_button->GetText(), work_label);
@@ -1143,7 +1145,8 @@ IN_PROC_BROWSER_TEST_F(AvatarToolbarButtonEnterpriseBadgingBrowserTest,
     chrome::enterprise_util::SetUserAcceptedAccountManagement(
         browser()->profile(), false);
     EXPECT_NE(avatar_button->GetText(), work_label);
-    auto clear_closure = avatar_button->ShowExplicitText(u"Explicit text");
+    auto clear_closure = avatar_button->ShowExplicitText(
+        u"Explicit text", /*accessibility_label=*/std::nullopt);
     EXPECT_NE(avatar_button->GetText(), work_label);
     clear_closure.RunAndReset();
     EXPECT_NE(avatar_button->GetText(), work_label);
--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -74,6 +75,27 @@ std::u16string InterceptionTypeToIdentityPillText(
     case WebSigninInterceptor::SigninInterceptionType::kEnterprise:
       return l10n_util::GetStringUTF16(
           IDS_SIGNIN_DICE_WEB_INTERCEPT_AVATAR_BUTTON_SEPARATE_BROWSING_TEXT);
+    case WebSigninInterceptor::SigninInterceptionType::kEnterpriseForced:
+    case WebSigninInterceptor::SigninInterceptionType::
+        kEnterpriseAcceptManagement:
+    case WebSigninInterceptor::SigninInterceptionType::kEnterpriseOIDC:
+    case WebSigninInterceptor::SigninInterceptionType::kProfileSwitchForced:
+      // These intercept type do not show a bubble and should not need to change
+      // the identity pill text.
+      NOTREACHED_NORETURN();
+  }
+}
+
+std::optional<std::u16string> InteractionTypeToIdentityPillAccessibilityLabel(
+    WebSigninInterceptor::SigninInterceptionType interception_type) {
+  switch (interception_type) {
+    case WebSigninInterceptor::SigninInterceptionType::kChromeSignin:
+      // TODO(b/347011002): Final string to be added.
+      return l10n_util::GetStringUTF16(IDS_CLOSE);
+    case WebSigninInterceptor::SigninInterceptionType::kProfileSwitch:
+    case WebSigninInterceptor::SigninInterceptionType::kMultiUser:
+    case WebSigninInterceptor::SigninInterceptionType::kEnterprise:
+      return std::nullopt;
     case WebSigninInterceptor::SigninInterceptionType::kEnterpriseForced:
     case WebSigninInterceptor::SigninInterceptionType::
         kEnterpriseAcceptManagement:
@@ -414,8 +436,10 @@ void DiceWebSigninInterceptionBubbleView::ApplyAvatarButtonEffects() {
   if (ShouldUseFullDesign() || IsChromeSignin()) {
     // Adapt the identity pill, show the appropriate intercept text and
     // highlight the button as long as the text is shown.
-    hide_avatar_text_callback_ =
-        button->ShowExplicitText(InterceptionTypeToIdentityPillText(
+    hide_avatar_text_callback_ = button->ShowExplicitText(
+        InterceptionTypeToIdentityPillText(
+            bubble_parameters_.interception_type),
+        InteractionTypeToIdentityPillAccessibilityLabel(
             bubble_parameters_.interception_type));
   }
   // Avatar Button action behavior
