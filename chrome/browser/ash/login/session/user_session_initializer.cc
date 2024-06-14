f907d304fdb6eaf0559f7f8ea630cb4c36863391
Lauren Commeignes <laurencom@chromium.org>
2024-06-14 04:23:50 +0000

    Add in a new feature key for Sparky.
    Bug: b/345644229
    Change-Id: Ic60797842ba770b9590d28771b95bf4cb0158b15
    Test: Tested on DUT
    Low-Coverage-Reason: EXPERIMENTAL_CODE experimental feature
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605874
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315033}

--- chrome/browser/ash/login/session/user_session_initializer.cc
+++ chrome/browser/ash/login/session/user_session_initializer.cc
@@ -157,8 +157,7 @@ void UserSessionInitializer::OnUserProfileLoaded(const AccountId& account_id) {
     InitializePrimaryProfileServices(profile, user);
 
     FamilyUserMetricsServiceFactory::GetForBrowserContext(profile);
-    if (chromeos::features::IsMahiEnabled() &&
-        chromeos::features::IsSparkyEnabled()) {
+    if (chromeos::features::IsSparkyEnabled()) {
       ash::SparkyManagerServiceFactory::GetForProfile(profile);
     }
   }
