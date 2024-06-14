f7fe2eb473d7902479bc6db83d42caadb687ee9b
James Lee <ljjlee@google.com>
2024-06-14 13:43:14 +0000

    Don't require user credentials on ClassifyUrl calls
    Guarded by a new UncredentialedFilteringFallbackForSupervisedUsers
    feature flag.
    
    Bug: b/346264703
    Change-Id: I59a92af61e4bcdec1bec3787d1de252c19832cfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5625818
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315188}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -102,6 +102,10 @@ BASE_FEATURE(kHideGuestModeForSupervisedUsers,
              base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
+BASE_FEATURE(kUncredentialedFilteringFallbackForSupervisedUsers,
+             "UncredentialedFilteringFallbackForSupervisedUsers",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 #if BUILDFLAG(IS_ANDROID)
 BASE_FEATURE(kMigrateAccountManagementSettingsToCapabilities,
              "MigrateAccountManagementSettingsToCapabilities",
