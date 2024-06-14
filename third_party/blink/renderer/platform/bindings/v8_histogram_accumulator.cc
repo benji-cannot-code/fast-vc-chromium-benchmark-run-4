b830fae915867c36dfdda6c6f1c655e11f3acb54
Marja Hölttä <marja@chromium.org>
2024-06-14 09:32:34 +0000

    [cumulative compile metrics] Add a cumulative metric for V8.ExecuteMicroseconds
    This metric is only generated when slow histograms are enabled
    (Finch configs can enable slow histograms for a particular
    experiment).
    
    Execute histograms will necessarily be noisier than compile
    histograms, because web pages don't necessarily "quiet down".
    This CL measures the execute time at the "interactive" point,
    where compile histograms are also measured.
    
    Bug: 329408826
    Change-Id: I9e3c8d69cac33d4cb22c010a56bf707875e28391
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5633474
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315102}

--- third_party/blink/renderer/platform/bindings/v8_histogram_accumulator.cc
+++ third_party/blink/renderer/platform/bindings/v8_histogram_accumulator.cc
@@ -33,6 +33,9 @@ void* V8HistogramAccumulator::RegisterHistogram(base::HistogramBase* histogram,
              name == "V8.CompileDeserializeMicroSeconds.BackgroundThread") {
     histogram_and_sum = std::make_unique<HistogramAndSum>(
         histogram, &compile_background_sum_microseconds_);
+  } else if (name == "V8.ExecuteMicroSeconds") {
+    histogram_and_sum = std::make_unique<HistogramAndSum>(
+        histogram, &execute_sum_microseconds_);
   } else {
     histogram_and_sum = std::make_unique<HistogramAndSum>(histogram);
   }
@@ -58,6 +61,8 @@ void V8HistogramAccumulator::GenerateDataInteractive() {
       base::Microseconds(compile_foreground_sum_microseconds_.load()));
   compile_background_.interactive_histogram->AddTimeMicrosecondsGranularity(
       base::Microseconds(compile_background_sum_microseconds_.load()));
+  execute_.interactive_histogram->AddTimeMicrosecondsGranularity(
+      base::Microseconds(execute_sum_microseconds_.load()));
 }
 V8HistogramAccumulator::V8HistogramAccumulator() {
   // Create accumulating histograms.
@@ -71,6 +76,10 @@ V8HistogramAccumulator::V8HistogramAccumulator() {
   compile_background_.interactive_histogram = base::Histogram::FactoryGet(
       "V8.CompileBackgroundMicroSeconds.Cumulative.Interactive", min, max,
       buckets, base::Histogram::kUmaTargetedHistogramFlag);
+
+  execute_.interactive_histogram = base::Histogram::FactoryGet(
+      "V8.ExecuteMicroSeconds.Cumulative.Interactive", min, max, buckets,
+      base::Histogram::kUmaTargetedHistogramFlag);
 }
 
 }  // namespace blink
