0354ad5ea574229e557f25a99ad4bc756e0fd5a4
Shakti Sahu <shaktisahu@chromium.org>
2024-06-14 04:01:42 +0000

    [Tab Group Sync] Avoid writing the local ID update for desktop
    In https://chromium-review.googlesource.com/c/chromium/src/+/5583203/14/components/saved_tab_groups/saved_tab_group_sync_bridge.cc
    we started persisting the saved tab group entry to disk after a
    local ID change. However, on desktop it's unnecessary since local
    IDs are temporary for the session. Moreover, in crrev.com/c/5611692
    we fixed reentrancy by posting a callback but at TabGroupSyncService
    level which doesn't apply to desktop. In this CL, we are avoiding
    the write to storage on local ID change for desktop, which brings
    us to functionally equivalent to before recent changes.
    
    Bug: 346882796
    Change-Id: I4314b8cd9b17dbbcddd47dc50bf930935528992e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626468
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315027}

---
+++ components/saved_tab_groups/utils.cc
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/saved_tab_groups/utils.h"
+
+#include "build/build_config.h"
+#include "components/saved_tab_groups/types.h"
+
+namespace tab_groups {
+
+bool AreLocalIdsPersisted() {
+#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
+  return true;
+#else
+  return false;
+#endif
+}
+
+}  // namespace tab_groups
