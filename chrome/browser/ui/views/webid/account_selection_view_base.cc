bdb17469113125a81875ba0ec28b1af0da1e77b7
Tomasz Moniuszko <tmoniuszko@opera.com>
2024-06-14 15:57:04 +0000

    Add missing web_contents.h include
    IWYU fix related to web_contents_->GetColorProvider() call.
    Fixes compilation issue in some build configurations.
    
    Change-Id: I72e1d1917a241f68907856e7129be33afd4608c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5625865
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315259}

--- chrome/browser/ui/views/webid/account_selection_view_base.cc
+++ chrome/browser/ui/views/webid/account_selection_view_base.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/image_fetcher/core/image_decoder.h"
 #include "components/image_fetcher/core/image_fetcher_impl.h"
 #include "components/vector_icons/vector_icons.h"
+#include "content/public/browser/web_contents.h"
 #include "third_party/blink/public/mojom/webid/federated_auth_request.mojom.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
