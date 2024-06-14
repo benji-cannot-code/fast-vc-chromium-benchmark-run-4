458a5b0da9386eee76deafdffe341d688ffb874b
Austin Eng <enga@chromium.org>
2024-06-14 16:20:58 +0000

    Check wgpu::Status enum instead of implicit bool
    Bug: 42241199
    Change-Id: I57111edc3bf37e86c73974ae8310df52a95e1c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632576
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315270}

--- gpu/config/gpu_info_collector.cc
+++ gpu/config/gpu_info_collector.cc
@@ -329,7 +329,7 @@ void ReportWebGPUAdapterMetrics(dawn::native::Instance* instance) {
 
     WGPUSupportedLimits limits;
     limits.nextInChain = nullptr;
-    if (!adapter.GetLimits(&limits)) {
+    if (adapter.GetLimits(&limits) != wgpu::Status::Success) {
       continue;
     }
 
