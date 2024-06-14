47c73a79218ba8cda5efc19eb4b88c37a8d2b9e1
Aaron Leventhal <aleventhal@google.com>
2024-06-14 20:28:25 +0000

    [A11y] Update aria-owns child-to-owner maps before creating AXObjects
    Creating an object can lead to a cascade of other object creations
    via AddChildren(). However, adding children needs to know which
    objects are owned so that they are not added twice.
    
    Adding more checks in IsAriaOwned() helped debug the issue, because
    it caused a failing test related to chrome://bookmarks.
    
    Bug: 346158514
    Change-Id: I9039ef191372d48a9b22f5cbd9612a1e139c1532
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631583
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315424}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -7935,8 +7935,9 @@ String AXObject::ToString(bool verbose) const {
                            : CanSetFocusAttribute()) {
       string_builder = string_builder + " focusable";
     }
-    if (!IsDetached() && AXObjectCache().IsAriaOwned(this))
+    if (!IsDetached() && AXObjectCache().IsAriaOwned(this, /*checks*/ false)) {
       string_builder = string_builder + " isAriaOwned";
+    }
     if (IsIgnored()) {
       string_builder = string_builder + " isIgnored";
 #if defined(AX_FAIL_FAST_BUILD)
