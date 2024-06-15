ce600e40d3989916f5dbfd7c7f24716de9bc4e63
Sammie Quon <sammiequon@chromium.org>
2024-06-15 00:52:37 +0000

    overview: Rework overview focus (18/N)
    Prepare to enable flag by fixing tests part 1.
    
    Test: ash_unittests
    Bug: b:325335020
    Change-Id: I90cebbe7d40ddfa9510c738a6c673750f906f8b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622295
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315538}

--- ash/wm/snap_group/snap_group_unittest.cc
+++ ash/wm/snap_group/snap_group_unittest.cc
@@ -5737,7 +5737,8 @@ TEST_F(SnapGroupDesksTest, SaveDeskForSnapGroupWithAnotherSavedDesk) {
 
   // Explicitly disable `disable_app_id_check_for_saved_desks_` otherwise "Save
   // desk for later" button will be disabled.
-  overview_controller->set_disable_app_id_check_for_saved_desks_for_test(true);
+  base::AutoReset<bool> disable_app_id_check =
+      overview_controller->SetDisableAppIdCheckForTests();
 
   // Create `w0` and save `w0` in a saved desk by activing "Save desk for later"
   // button in Overview.
@@ -5800,8 +5801,6 @@ TEST_F(SnapGroupDesksTest, SaveDeskForSnapGroupWithAnotherSavedDesk) {
   event_generator->ClickLeftButton();
   EXPECT_FALSE(IsInOverviewSession());
   UnionBoundsEqualToWorkAreaBounds(w1.get(), w2.get(), snap_group_divider());
-
-  overview_controller->set_disable_app_id_check_for_saved_desks_for_test(false);
 }
 
 // Verify that Snap Group will be broken when setting a window that belongs to a
