ffd87d906d5b67adb8aadb81973f1e0ada476a1c
Colin Blundell <blundell@chromium.org>
2024-06-17 13:31:29 +0000

    [//gpu] Have AssociateMailbox() check WEBGPU_WRITE for writable usage
    This CL changes WebGPUDecoderImpl::AssociateMailbox() to require
    WEBGPU_WRITE usage on the mailbox being associated if the client has
    passed a writable WebGPU usage.
    
    Clients call AssociateMailbox() either directly or by creating a
    WebGPUMailboxTexture, which calls AssociateMailbox() in its constructor.
    The clients that can pass writable usages to AssociateMailbox() are
    the WebGPU swapbuffer [1] and canvas [2][3][4]. Both of these pass
    mailboxes referring to SharedImages that were deliberately created with
    WEBGPU_WRITE usage [5][6] (the canvas use cases use the SharedImage
    that was created by the CanvasResourceProvider, which will have been
    created via CanvasResourceProvider::CreateWebGPUImageProvider in these
    cases).
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc;l=222-235;drc=90cac1911508d3d682a67c97aa62483eb712f69a;bpv=1;bpt=1
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgpu/gpu_canvas_context.cc;l=782-786?q=gpu_canvas_context.cc&ss=chromium
    [3] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/canvas/canvas2d/base_rendering_context_2d.cc;l=3625-3684;drc=90cac1911508d3d682a67c97aa62483eb712f69a
    [4] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgpu/gpu_queue.cc;l=854-859;drc=90cac1911508d3d682a67c97aa62483eb712f69a
    [5] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/gpu/webgpu_swap_buffer_provider.cc;l=143-154?q=webgpu_swap&ss=chromium
    [6] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc;l=1081-1095?q=canvas_resource_provider.cc&ss=chromium
    
    Bug: 339171225
    Change-Id: I2c5c4657a01501f0436e241fb639f24791cec4f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5633344
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315790}

--- gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1979,6 +1979,13 @@ WebGPUDecoderImpl::AssociateMailboxDawn(
   }
 #endif
 
+  if ((usage & kAllowedWritableMailboxTextureUsages) &&
+      (!(shared_image->usage() & SHARED_IMAGE_USAGE_WEBGPU_WRITE))) {
+    LOG(ERROR) << "AssociateMailbox: Passing writable usages requires "
+                  "WebGPU write access to the SharedImage";
+    return nullptr;
+  }
+
   if ((internal_usage & kAllowedWritableMailboxTextureUsages) &&
       (!(shared_image->usage() & SHARED_IMAGE_USAGE_WEBGPU_WRITE))) {
     LOG(ERROR) << "AssociateMailbox: Passing writable internal usages requires "
@@ -2056,6 +2063,13 @@ WebGPUDecoderImpl::AssociateMailboxUsingSkiaFallback(
 
   if (!shared_image) {
     DLOG(ERROR) << "AssociateMailbox: Couldn't produce shared image";
+    return nullptr;
+  }
+
+  if ((usage & kAllowedWritableMailboxTextureUsages) &&
+      (!(shared_image->usage() & SHARED_IMAGE_USAGE_WEBGPU_WRITE))) {
+    LOG(ERROR) << "AssociateMailbox: Passing writable usages requires "
+                  "WebGPU write access to the SharedImage";
     return nullptr;
   }
 
