d122db4ad9cbdef2b9c4e42476e1bcecaf449124
Akihiro Ota <akihiroota@chromium.org>
2024-06-17 18:53:55 +0000

    FaceGaze: Add macro to toggle FaceGaze
    This change adds a macro that pauses/resumes FaceGaze mouse
    movement and gesture detection. When toggling, the MouseController
    can simply be stopped/started based on the new paused state. However,
    GestureHandler still needs to run because if FaceGaze is paused, it
    needs to detect the gesture to resume FaceGaze.
    
    Another detail included in this change is finishing in-progress macros
    when FaceGaze is paused. We run them to ensure that the system is
    in a clear state. For example, if a long left click is in-progress and
    FaceGaze is paused, we will send the mouse release event before
    pausing FaceGaze. This helps keep the EventGenerator (which stores if
    we're in the middle of a click or not) and other parts of the system
    in a good state.
    
    Bug: b/330766904
    Change-Id: I86eaf4c8208f0d6d0b150292098ed7dd165e9fda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5604850
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316000}

---
+++ chrome/browser/resources/chromeos/accessibility/common/action_fulfillment/macros/custom_callback_macro.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {Macro, RunMacroResult} from './macro.js';
+import {MacroName} from './macro_names.js';
+
+/** Class that implements a macro that runs the supplied callback. */
+export class CustomCallbackMacro extends Macro {
+  private callback_: () => void;
+
+  constructor(macroName: MacroName, callback: () => void) {
+    super(macroName);
+    this.callback_ = callback;
+  }
+
+  override run(): RunMacroResult {
+    this.callback_();
+    return this.createRunMacroResult_(/*isSuccess=*/ true);
+  }
+}
