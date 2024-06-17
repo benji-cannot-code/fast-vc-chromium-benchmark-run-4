6316c6abd986e51222f090a5fdd187b2ded9f14c
Caroline Rising <corising@chromium.org>
2024-06-17 21:12:22 +0000

    Update a11y announcement on button pin to/unpin from toolbar.
    Previously this announced "Side panel pinned/unpinned" this generalizes
    the announcement to "button pinned/unpinned"
    
    Bug: 323984400
    Change-Id: Ia8b3ed549d3424c98fce2c6c770d80d442861e4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5635872
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316080}

--- chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/user_metrics.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_actions.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_action_callback.h"
 #include "chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
@@ -215,10 +216,14 @@ void PinnedActionToolbarButton::UpdatePinnedStateForContextMenu() {
       base::StrCat({"Actions.PinnedToolbarButton.",
                     updated_pin_state ? "Pinned" : "Unpinned",
                     ".ByContextMenu.", metrics_name.value()}));
-  // TODO(corising): Update the text for these notifications once pinning
-  // expands past side panels.
-  GetViewAccessibility().AnnounceText(l10n_util::GetStringUTF16(
-      updated_pin_state ? IDS_SIDE_PANEL_PINNED : IDS_SIDE_PANEL_UNPINNED));
+  if (features::IsToolbarPinningEnabled()) {
+    GetViewAccessibility().AnnounceText(l10n_util::GetStringUTF16(
+        updated_pin_state ? IDS_TOOLBAR_BUTTON_PINNED
+                          : IDS_TOOLBAR_BUTTON_UNPINNED));
+  } else {
+    GetViewAccessibility().AnnounceText(l10n_util::GetStringUTF16(
+        updated_pin_state ? IDS_SIDE_PANEL_PINNED : IDS_SIDE_PANEL_UNPINNED));
+  }
   actions_model->UpdatePinnedState(action_id_, updated_pin_state);
 }
 
