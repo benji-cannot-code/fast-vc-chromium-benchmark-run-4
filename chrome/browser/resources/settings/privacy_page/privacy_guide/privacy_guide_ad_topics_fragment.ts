d9ddaaa1a2122a6f7d6ca2833d82ed3fb1f1d3c2
Jacob Stanley <jacobstanley@google.com>
2024-06-17 20:45:00 +0000

    Privacy Guide - Ad Topics Connecting Strings
    https://screenshot.googleplex.com/C8RTc4LcExZNH7N (Ad Topics Card)
    https://screenshot.googleplex.com/NMRvyoKtuqUPx9p (Completion Card)
    
    Bug: 331970504
    Change-Id: I00f634948531db58be8d3a80c5734f4f930230cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5582739
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1316057}

--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_ad_topics_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_ad_topics_fragment.ts
@@ -9,17 +9,40 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * card that contains the ad topics setting and its description.
  */
 
+import '../../controls/settings_toggle_button.js';
+import '../../icons.html.js';
+import './privacy_guide_description_item.js';
+
+import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import type {SettingsToggleButtonElement} from '../../controls/settings_toggle_button.js';
+import {PrivacySandboxBrowserProxyImpl} from '../../privacy_sandbox/privacy_sandbox_browser_proxy.js';
 
 import {getTemplate} from './privacy_guide_ad_topics_fragment.html.js';
 
-export class PrivacyGuideAdTopicsFragmentElement extends PolymerElement {
+const PrivacyGuideAdTopicsFragmentElementBase = PrefsMixin(PolymerElement);
+
+export class PrivacyGuideAdTopicsFragmentElement extends
+    PrivacyGuideAdTopicsFragmentElementBase {
   static get is() {
     return 'privacy-guide-ad-topics-fragment';
   }
 
   static get template() {
     return getTemplate();
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Preferences state.
+       */
+      prefs: {
+        type: Object,
+        notify: true,
+      },
+    };
   }
 
   override focus() {
@@ -29,6 +52,12 @@ export class PrivacyGuideAdTopicsFragmentElement extends PolymerElement {
     // to continue navigating the screen reader position downwards through the
     // newly visible content.
     this.shadowRoot!.querySelector<HTMLElement>('[focus-element]')!.focus();
+  }
+
+  private onToggleChange_(e: Event) {
+    const target = e.target as SettingsToggleButtonElement;
+    PrivacySandboxBrowserProxyImpl.getInstance().topicsToggleChanged(
+        target.checked);
   }
 }
 
--- chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
+++ chrome/browser/resources/settings/privacy_page/privacy_guide/privacy_guide_completion_fragment.ts
@@ -75,6 +75,17 @@ export class PrivacyGuideCompletionFragmentElement extends
         type: Boolean,
         value: false,
       },
+
+      privacySandboxRowSubLabel_: {
+        type: String,
+        value: () => {
+          return loadTimeData.getString(
+              loadTimeData.getBoolean(
+                  'isPrivacySandboxPrivacyGuideAdTopicsEnabled') ?
+                  'privacyGuideCompletionCardPrivacySandboxSubLabelAdTopics' :
+                  'privacyGuideCompletionCardPrivacySandboxSubLabel');
+        },
+      },
     };
   }
 
@@ -83,6 +94,7 @@ export class PrivacyGuideCompletionFragmentElement extends
   private shouldShowWaa_: boolean;
   private metricsBrowserProxy_: MetricsBrowserProxy =
       MetricsBrowserProxyImpl.getInstance();
+  private privacySandboxRowSubLabel_: string;
 
   override ready() {
     super.ready();
