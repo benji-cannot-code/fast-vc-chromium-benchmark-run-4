0934be370f59b3fbebda861505a71960ea8a776e
Brian Osman <brianosman@google.com>
2024-06-17 20:23:03 +0000

    PaintOpReader: Prevent ReadSimple(bool*)
    While debugging a "release-only" fuzzer bug, I wanted to understand why
    it wouldn't reproduce in debug builds. Ultimately, one of the booleans
    used to direct deserialization flow was read with ReadSimple. In
    optimized builds, the code assumes that zero and one are the only
    possible values, so we were getting divergent flow-control.
    
    Hopefully, this will make more fuzzer bugs behave consistently across
    build type. (It did in this case).
    
    Bug: 345562926
    Change-Id: I5e315274012c7cd342c94e0316ac77fdfbb1b802
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630823
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316049}

--- cc/paint/paint_op_reader.cc
+++ cc/paint/paint_op_reader.cc
@@ -118,6 +118,9 @@ bool PaintOpReader::ReadAndValidateOpHeader(uint8_t* type,
 template <typename T>
 void PaintOpReader::ReadSimple(T* val) {
   static_assert(std::is_trivially_copyable_v<T>);
+  // Serialized values other than 0 or 1 can cause different behavior in debug
+  // and release, making debugging harder. Please use Read(bool*).
+  static_assert(!std::is_same_v<T, bool>);
 
   AssertFieldAlignment();
   // Align everything to 4 bytes, as the writer does.
@@ -307,7 +310,7 @@ void PaintOpReader::Read(PaintFlags* flags) {
 
   if (enable_security_constraints_) {
     bool has_looper = false;
-    ReadSimple(&has_looper);
+    Read(&has_looper);
     if (has_looper) {
       SetInvalid(DeserializationError::kDrawLooperForbidden);
       return;
@@ -456,7 +459,7 @@ void PaintOpReader::Read(
     return;
 
   bool needs_mips;
-  ReadSimple(&needs_mips);
+  Read(&needs_mips);
   if (!valid_)
     return;
 
@@ -623,7 +626,7 @@ void PaintOpReader::Read(sk_sp<sktext::gpu::Slug>* slug) {
 
 void PaintOpReader::Read(sk_sp<DrawLooper>* looper) {
   bool has_looper = false;
-  ReadSimple(&has_looper);
+  Read(&has_looper);
   if (!has_looper) {
     *looper = nullptr;
     return;
@@ -658,7 +661,7 @@ void PaintOpReader::Read(sk_sp<DrawLooper>* looper) {
 
 void PaintOpReader::Read(sk_sp<PaintShader>* shader) {
   bool has_shader = false;
-  ReadSimple(&has_shader);
+  Read(&has_shader);
   if (!has_shader) {
     *shader = nullptr;
     return;
@@ -688,7 +691,7 @@ void PaintOpReader::Read(sk_sp<PaintShader>* shader) {
   if (!IsValidPaintShaderScalingBehavior(ref.scaling_behavior_))
     SetInvalid(DeserializationError::kInvalidPaintShaderScalingBehavior);
   bool has_local_matrix = false;
-  ReadSimple(&has_local_matrix);
+  Read(&has_local_matrix);
   if (has_local_matrix) {
     ref.local_matrix_.emplace();
     Read(&*ref.local_matrix_);
@@ -703,7 +706,7 @@ void PaintOpReader::Read(sk_sp<PaintShader>* shader) {
   Read(&ref.image_, PaintFlags::DynamicRangeLimitMixture(
                         PaintFlags::DynamicRangeLimit::kHigh));
   bool has_record = false;
-  ReadSimple(&has_record);
+  Read(&has_record);
   uint32_t shader_id = PaintShader::kInvalidRecordShaderId;
   size_t shader_size = 0;
   if (has_record) {
@@ -868,7 +871,7 @@ void PaintOpReader::Read(gpu::Mailbox* mailbox) {
 }
 
 void PaintOpReader::Read(SkHighContrastConfig* config) {
-  ReadSimple(&config->fGrayscale);
+  Read(&config->fGrayscale);
   ReadEnum<SkHighContrastConfig::InvertStyle,
            SkHighContrastConfig::InvertStyle::kLast>(&config->fInvertStyle);
   ReadSimple(&config->fContrast);
@@ -1326,7 +1329,7 @@ void PaintOpReader::ReadRecordPaintFilter(
     sk_sp<PaintFilter>* filter,
     const std::optional<PaintFilter::CropRect>& crop_rect) {
   bool has_filter = false;
-  ReadSimple(&has_filter);
+  Read(&has_filter);
   if (!has_filter) {
     *filter = nullptr;
     return;
