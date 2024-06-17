2ad526d3bb00251c8650aaa3257c56ce1b1b9a20
Andrew Grieve <agrieve@chromium.org>
2024-06-17 16:55:50 +0000

    Remove unused method in SimpleHttpClient
    Change-Id: I0fdda3994ee5ab1c9b9c7d7b31052fe4cc52615e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631577
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315931}

--- chrome/browser/android/httpclient/java/src/org/chromium/chrome/browser/android/httpclient/SimpleHttpClient.java
+++ chrome/browser/android/httpclient/java/src/org/chromium/chrome/browser/android/httpclient/SimpleHttpClient.java
@@ -21,7 +21,6 @@ import org.chromium.chrome.browser.profiles.ProfileKeyedMap;
 import org.chromium.net.NetworkTrafficAnnotationTag;
 import org.chromium.url.GURL;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
@@ -125,28 +124,6 @@ public class SimpleHttpClient implements Destroyable {
                                     annotation.getHashCode(),
                                     responseConsumer);
                 });
-    }
-
-    // TODO(agrieve): Delete once unused by //clank.
-    public static HttpResponse createHttpResponse(
-            int responseCode,
-            int netErrorCode,
-            byte[] body,
-            String[] headerKeys,
-            String[] headerValues) {
-        assert headerKeys.length == headerValues.length;
-
-        Map<String, String> responseHeaders = new HashMap<>();
-        for (int i = 0; i < headerKeys.length; i++) {
-            if (!responseHeaders.containsKey(headerKeys[i])) {
-                responseHeaders.put(headerKeys[i], headerValues[i]);
-            } else {
-                String headerValue = responseHeaders.get(headerKeys[i]);
-                headerValue += "\n" + headerValues[i];
-                responseHeaders.put(headerKeys[i], headerValue);
-            }
-        }
-        return createHttpResponse(responseCode, netErrorCode, body, responseHeaders);
     }
 
     /**
