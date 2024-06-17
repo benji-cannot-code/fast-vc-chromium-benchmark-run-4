a004eee892f348b310e9a876e1557771357292ac
Zhenyao Mo <zmo@chromium.org>
2024-06-17 14:35:56 +0000

    Remove gles2_conform_tests related code
    TEST=bots
    
    Bug: 347070204
    Change-Id: Idcd3a5b46cffaf08595b307e5c70a6587eba3f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634204
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315829}

--- gpu/command_buffer/client/gles2_implementation.cc
+++ gpu/command_buffer/client/gles2_implementation.cc
@@ -84,8 +84,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // If it was up to us we'd just always write to the destination but the OpenGL
 // spec defines the behavior of OpenGL functions, not us. :-(
-#if defined(__native_client__) || defined(GLES2_CONFORMANCE_TESTS) || \
-    BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
+#if defined(__native_client__) || BUILDFLAG(IS_MINIMAL_TOOLCHAIN)
 #define GPU_CLIENT_VALIDATE_DESTINATION_INITALIZATION_ASSERT(v)
 #define GPU_CLIENT_DCHECK(v)
 #elif defined(GPU_DCHECK)
