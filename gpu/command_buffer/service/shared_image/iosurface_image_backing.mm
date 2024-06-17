aee18aa7e7539cb5142447fd8227b458ec373d7d
Colin Blundell <blundell@chromium.org>
2024-06-17 15:37:31 +0000

    [//gpu] Have Mac Dawn SI rep check internal usages for type of access
    When using internal usages passed by the client, Dawn representations
    should take these internal usages into account when computing whether
    the Dawn access is readonly (for any purpose).
    
    This CL makes this fix for the IOSurface Dawn representation. I will
    audit the rest and make similar fixes as necessary.
    
    Bug: 339171225
    Change-Id: Idf6f3ec56ce0a78ce585285a6f5df64c23ff844e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631552
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315878}

--- gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
+++ gpu/command_buffer/service/shared_image/iosurface_image_backing.mm
@@ -773,16 +773,22 @@ class IOSurfaceImageBacking::DawnRepresentation final
   const wgpu::TextureFormat wgpu_format_;
   const std::vector<wgpu::TextureFormat> view_formats_;
 
-  // NOTE: `usage_` and `texture_` are valid only within the duration of a
-  // BeginAccess()/EndAccess() pair.
+  // NOTE: `usage_`, `internal_usage_`, and `texture_` are valid only within
+  // the duration of a BeginAccess()/EndAccess() pair.
   wgpu::TextureUsage usage_;
+  wgpu::TextureUsage internal_usage_;
   wgpu::Texture texture_;
 };
 
 wgpu::Texture IOSurfaceImageBacking::DawnRepresentation::BeginAccess(
     wgpu::TextureUsage wgpu_texture_usage,
     wgpu::TextureUsage internal_usage) {
-  const bool readonly = (wgpu_texture_usage & ~kReadOnlyUsage) == 0;
+  const bool readonly =
+      (wgpu_texture_usage & ~kReadOnlyUsage) == 0 &&
+      (!base::FeatureList::IsEnabled(
+           features::kDawnSIRepsUseClientProvidedInternalUsages) ||
+       (internal_usage & ~kReadOnlyUsage) == 0);
+
   IOSurfaceImageBacking* iosurface_backing =
       static_cast<IOSurfaceImageBacking*>(backing());
   if (!iosurface_backing->BeginAccess(readonly)) {
@@ -799,6 +805,7 @@ wgpu::Texture IOSurfaceImageBacking::DawnRepresentation::BeginAccess(
       /*device_to_exclude=*/device_);
 
   usage_ = wgpu_texture_usage;
+  internal_usage_ = internal_usage;
 
   texture_ = iosurface_backing->GetCachedWGPUTexture(device_, usage_);
   if (!texture_) {
@@ -890,7 +897,11 @@ void IOSurfaceImageBacking::DawnRepresentation::EndAccess() {
   // its state tracking.
   IOSurfaceImageBacking* iosurface_backing =
       static_cast<IOSurfaceImageBacking*>(backing());
-  const bool readonly = (usage_ & ~kReadOnlyUsage) == 0;
+  const bool readonly =
+      (usage_ & ~kReadOnlyUsage) == 0 &&
+      (!base::FeatureList::IsEnabled(
+           features::kDawnSIRepsUseClientProvidedInternalUsages) ||
+       (internal_usage_ & ~kReadOnlyUsage) == 0);
   iosurface_backing->EndAccess(readonly);
   int num_outstanding_accesses =
       iosurface_backing->TrackEndAccessToWGPUTexture(texture_);
@@ -901,7 +912,7 @@ void IOSurfaceImageBacking::DawnRepresentation::EndAccess() {
   // will happen when the last ongoing Dawn access finishes.
   if (num_outstanding_accesses > 0) {
     texture_ = nullptr;
-    usage_ = wgpu::TextureUsage::None;
+    usage_ = internal_usage_ = wgpu::TextureUsage::None;
     return;
   }
 
@@ -948,7 +959,7 @@ void IOSurfaceImageBacking::DawnRepresentation::EndAccess() {
   }
 
   texture_ = nullptr;
-  usage_ = wgpu::TextureUsage::None;
+  usage_ = internal_usage_ = wgpu::TextureUsage::None;
 }
 
 // Enabling this functionality reduces overhead in the compositor by lowering
