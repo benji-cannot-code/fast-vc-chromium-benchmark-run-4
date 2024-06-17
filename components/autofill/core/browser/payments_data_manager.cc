dd92a716d09d08ab949342840722f2ec1be97540
Jan Keitel <jkeitel@google.com>
2024-06-17 12:35:39 +0000

    Avoid unsafe buffer warnings in PaymentsDataManager.
    Bug: 347137620
    Change-Id: I21b68243ca5f8d8bc68bf46fbe4233c39cfc60e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631163
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315764}

--- components/autofill/core/browser/payments_data_manager.cc
+++ components/autofill/core/browser/payments_data_manager.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO: crbug.com/347137620 - Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/autofill/core/browser/payments_data_manager.h"
 
 #include <memory>
@@ -805,7 +800,7 @@ gfx::Image* PaymentsDataManager::GetCreditCardArtImageForUrl(
     return cached_image;
   }
 
-  FetchImagesForURLs(base::make_span(&card_art_url, 1u));
+  FetchImagesForURLs(base::span_from_ref(card_art_url));
   return nullptr;
 }
 
