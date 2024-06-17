7a339fdc8f7fc492a05f46457616326ca591fb86
Jan Keitel <jkeitel@google.com>
2024-06-17 12:35:54 +0000

    Fix unsafe buffers warnings in AutocompleteTable test.
    Bug: 347137620
    Change-Id: I28afd287a27f72fa1a45c28e10e145250a2245d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631164
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315765}

--- components/autofill/core/browser/webdata/autocomplete/autocomplete_table_unittest.cc
+++ components/autofill/core/browser/webdata/autocomplete/autocomplete_table_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO: crbug.com/347137620 - Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/autofill/core/browser/webdata/autocomplete/autocomplete_table.h"
 
 #include <string>
@@ -178,7 +173,7 @@ TEST_F(AutocompleteTableTest, Autocomplete) {
   changes.clear();
   EXPECT_TRUE(table_->RemoveFormElementsAddedBetween(begin, Time(), changes));
 
-  const AutocompleteChange kExpectedChanges[] = {
+  const auto kExpectedChanges = std::array{
       AutocompleteChange(AutocompleteChange::REMOVE,
                          AutocompleteKey(u"Name", u"Superman")),
       AutocompleteChange(AutocompleteChange::REMOVE,
@@ -188,7 +183,7 @@ TEST_F(AutocompleteTableTest, Autocomplete) {
       AutocompleteChange(AutocompleteChange::REMOVE,
                          AutocompleteKey(u"Favorite Color", u"Green")),
   };
-  EXPECT_EQ(std::size(kExpectedChanges), changes.size());
+  EXPECT_EQ(kExpectedChanges.size(), changes.size());
   for (size_t i = 0; i < std::size(kExpectedChanges); ++i) {
     EXPECT_EQ(kExpectedChanges[i], changes[i]);
   }
