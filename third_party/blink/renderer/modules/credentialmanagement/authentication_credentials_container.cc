5a01508c3d158700a65ef2257c73d64ac5abbf34
Nicolás Peña <npm@chromium.org>
2024-06-17 16:55:42 +0000

    [FedCM] Check for multiIdP being enabled when requesting 'any'
    The multi IDP flag is enabled by default on the browser to easily enable
    Origin Trial. This means that currently requesting 'any' may surface the
    multi IDP UI even if the multi IDP flag is not explicitly enabled. This
    CL fixes this by checking the multiIdP flag on the renderer before
    allowing registered IdPs to be used. This effectively means that the IdP
    registration API is gated by the multi IDP API from the RP side. It is
    not the case on the IDP side, e.g. registration can occur without having
    multi IDP enabled.
    
    Fixed: 340915582
    Change-Id: Ic3eb0f8ace82211f05711f940962f17f784b9939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5627337
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315930}

--- third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanagement/authentication_credentials_container.cc
@@ -1973,6 +1973,8 @@ void AuthenticationCredentialsContainer::GetForIdentity(
     }
 
     if (blink::RuntimeEnabledFeatures::FedCmIdPRegistrationEnabled() &&
+        blink::RuntimeEnabledFeatures::FedCmMultipleIdentityProvidersEnabled(
+            context) &&
         provider->hasConfigURL() && provider->configURL() == "any") {
       mojom::blink::IdentityProviderRequestOptionsPtr identity_provider =
           blink::mojom::blink::IdentityProviderRequestOptions::From(*provider);
