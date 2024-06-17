0fafe119f16f81620e97477e5b1826e5d5570bc6
kylechar <kylechar@chromium.org>
2024-06-17 14:07:59 +0000

    Don't skip GLTextureImageBackingTest on Mac
    Not needed anymore.
    
    Fixed: 40138142
    Change-Id: I1ac66e6ad9c2d22c107cbfaa05399f63a715c488
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630183
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315813}

--- gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory_unittest.cc
+++ gpu/command_buffer/service/shared_image/gl_texture_image_backing_factory_unittest.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/config/gpu_driver_bug_workarounds.h"
 #include "gpu/config/gpu_feature_info.h"
 #include "gpu/config/gpu_preferences.h"
-#include "gpu/config/gpu_test_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -434,14 +433,6 @@ TEST_P(GLTextureImageBackingFactoryWithFormatTest, IsSupported) {
 }
 
 TEST_P(GLTextureImageBackingFactoryWithFormatTest, Basic) {
-  // TODO(jonahr): Test fails on Mac with ANGLE/passthrough
-  // (crbug.com/1100975)
-  gpu::GPUTestBotConfig bot_config;
-  if (bot_config.LoadCurrentConfig(nullptr) &&
-      bot_config.Matches("mac passthrough")) {
-    GTEST_SKIP();
-  }
-
   viz::SharedImageFormat format = get_format();
   if (!IsFormatSupport(format)) {
     GTEST_SKIP();
