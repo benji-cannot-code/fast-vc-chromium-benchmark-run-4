c44e56af74414560bbabc9f401c0e990f5a4df73
Bartek Nowierski <bartekn@chromium.org>
2024-06-17 15:08:26 +0000

    [BRP] Rewrite MemoryReadStream::byte_buf_ to raw_span<>
    No apparent regression on Speedometer and MotionMark
    https://pinpoint-dot-chromeperf.appspot.com/job/10704149410000
    https://pinpoint-dot-chromeperf.appspot.com/job/1310da6e410000
    https://pinpoint-dot-chromeperf.appspot.com/job/122e278e410000
    https://pinpoint-dot-chromeperf.appspot.com/job/154c2575410000
    
    Change-Id: Ida12e3a4bbe84785ad0cec88ef9328e3df6de04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632737
    Commit-Queue: Scott Violet <sky@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1315857}

--- chrome/utility/safe_browsing/mac/read_stream.h
+++ chrome/utility/safe_browsing/mac/read_stream.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "base/files/file.h"
+#include "base/memory/raw_span.h"
 
 namespace safe_browsing {
 namespace dmg {
@@ -82,7 +83,7 @@ class MemoryReadStream : public ReadStream {
   base::span<const uint8_t> byte_buf() const { return byte_buf_; }
 
  protected:
-  base::span<const uint8_t> byte_buf_;
+  base::raw_span<const uint8_t> byte_buf_;
   off_t offset_;
 };
 
