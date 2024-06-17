6e82b5f0eb6f41f5796651d42b6e9863f211746c
Minoru Chikamune <chikamune@chromium.org>
2024-06-17 04:06:48 +0000

    Gardening: Disable AppContainerTest.CreateAndDeleteAppContainerProfile on ARM Windows
    Bug: 347547524
    Change-Id: I89db37a2b18f704efe70e9ff0ea735070e9d1b4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632549
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Minoru Chikamune <chikamune@google.com>
    Owners-Override: Minoru Chikamune <chikamune@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315638}

--- sandbox/win/src/app_container_unittest.cc
+++ sandbox/win/src/app_container_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/security_descriptor.h"
 #include "base/win/security_util.h"
 #include "base/win/sid.h"
+#include "build/build_config.h"
 #include "sandbox/features.h"
 #include "sandbox/win/src/app_container_base.h"
 #include "sandbox/win/src/security_capabilities.h"
@@ -228,7 +229,16 @@ TEST(AppContainerTest, SecurityCapabilities) {
       ValidSecurityCapabilities(&two_capabilities, package_sid, capabilities));
 }
 
-TEST(AppContainerTest, CreateAndDeleteAppContainerProfile) {
+// TODO(crbug/347547524): re-enable the tests once they are passing on
+// Windows ARM64.
+#if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
+#define MAYBE_CreateAndDeleteAppContainerProfile \
+  DISABLED_CreateAndDeleteAppContainerProfile
+#else
+#define MAYBE_CreateAndDeleteAppContainerProfile \
+  CreateAndDeleteAppContainerProfile
+#endif
+TEST(AppContainerTest, MAYBE_CreateAndDeleteAppContainerProfile) {
   if (!features::IsAppContainerSandboxSupported())
     return;
 
