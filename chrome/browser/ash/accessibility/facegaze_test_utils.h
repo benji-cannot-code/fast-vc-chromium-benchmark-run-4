461e45ec6db8c4a6c7ccdf7a8061bda7f63b3a0c
Akihiro Ota <akihiroota@chromium.org>
2024-06-17 20:56:45 +0000

    FaceGaze: Fix bug when calling metricsPrivate
    This change fixes a bug caused by calling the recordMediumTime() API.
    The API expects an integer value, but FaceGaze was passing a decimal
    value. This fixes the issue by rounding our performance value to
    an integer before calling the API. A test was also added to verify
    the bug no longer reproduces.
    
    Bug: N/A
    Change-Id: I7932d283e57c0c7d79ff4200e5f366f9e76c34fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631103
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316067}

--- chrome/browser/ash/accessibility/facegaze_test_utils.h
+++ chrome/browser/ash/accessibility/facegaze_test_utils.h
@@ -202,6 +202,8 @@ class FaceGazeTestUtils {
     MockFaceLandmarkerResult& WithGesture(const MediapipeGesture& gesture,
                                           int confidence);
 
+    MockFaceLandmarkerResult& WithLatency(int latency);
+
     const base::Value::Dict& forehead_location() const {
       return forehead_location_;
     }
@@ -209,7 +211,10 @@ class FaceGazeTestUtils {
       return recognized_gestures_;
     }
 
+    const std::optional<int>& latency() const { return latency_; }
+
    private:
+    std::optional<int> latency_;
     base::Value::Dict forehead_location_;
     base::Value::List recognized_gestures_;
   };
