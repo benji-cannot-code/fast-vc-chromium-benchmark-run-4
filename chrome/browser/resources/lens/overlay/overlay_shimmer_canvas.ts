7927954e939c661ad42b1c8e1792cd65cab221dd
Duncan Mercer <mercerd@google.com>
2024-06-17 16:13:35 +0000

    [lensoverlay] Add sparkles to the canvas approach
    Setting to default true because:
    1) I chatted with pdr@ and he thinks performances is looks good. Not taking very much CPU time.
    2) They are using the same SVG as the previous iteration which UX approved of.
    
    Demo: https://screencast.googleplex.com/cast/Njc0NDY4NTc5MDg4NzkzNnwwYmNkZDBhMS0zNw
    
    Bug: b/341141050
    Change-Id: Ib54eac1de2e71e54a10a8a29accd0eeab6087a4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634651
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1315905}

--- chrome/browser/resources/lens/overlay/overlay_shimmer_canvas.ts
+++ chrome/browser/resources/lens/overlay/overlay_shimmer_canvas.ts
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {assert, assertNotReached} from '//resources/js/assert.js';
 import {EventTracker} from '//resources/js/event_tracker.js';
+import {loadTimeData} from '//resources/js/load_time_data.js';
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BrowserProxyImpl} from './browser_proxy.js';
@@ -95,6 +96,11 @@ const REGION_SELECTION_STATE_CENTER_Y_AMPLITUDE_PERCENT = 40;
 // The time it takes in MS to transition from a different state to the region
 // selection state.
 const REGION_SELECTION_TRANSITION_DURATION = 750;
+
+// The opacity of the sparkles. The sparkles opacity also is dictated by the
+// circle pixel opacity below it. Meaning, the true opacity value is
+// SPARKLES_OPACITY * CIRCLE_OPACITY.
+const SPARKLES_OPACITY = 1;
 
 // Specifies the current animation state of the shader canvas.
 enum ShimmerState {
@@ -196,6 +202,7 @@ function createCircleGradient(
 export interface OverlayShimmerCanvasElement {
   $: {
     shaderCanvas: HTMLCanvasElement,
+    sparklesSvg: SVGImageElement,
   };
 }
 
@@ -279,6 +286,18 @@ export class OverlayShimmerCanvasElement extends PolymerElement {
   private regionWidth: number;
   private regionHeight: number;
 
+  // The sparkles pattern used for rendering the sparkling animation. Created
+  // when the sparkles PNG is loaded in. Null otherwise.
+  private sparklesPattern: CanvasPattern|null = null;
+  // A randomized value every 100ms to transform the sparkles to create
+  // movement.
+  private sparklesOffset: number = 0;
+  // The ID of the setInterval call that updates the sparklesOffset.
+  private sparklesIntervalId?: number;
+  // Whether the sparkles are enabled or not.
+  private enableSparkles: boolean =
+      loadTimeData.getBoolean('enableShimmerSparkles');
+
   // The current shimmer state.
   private shimmerState: ShimmerState = ShimmerState.NONE;
   // The start time of the current animation. When undefined, it will be set at
@@ -322,6 +341,27 @@ export class OverlayShimmerCanvasElement extends PolymerElement {
         id => assert(
             BrowserProxyImpl.getInstance().callbackRouter.removeListener(id)));
     this.listenerIds = [];
+
+    // Stop updating the sparkles if they are currently updating.
+    if (this.sparklesIntervalId) {
+      clearInterval(this.sparklesIntervalId);
+      this.sparklesIntervalId = undefined;
+    }
+  }
+
+  private onSparklesLoad() {
+    // If the flag to enable sparkles is off, ignore the SVG loading in which
+    // will cause skip initializing sparklesPattern so no sparkles appear.
+    if (!this.enableSparkles) {
+      return;
+    }
+
+    this.sparklesPattern =
+        this.context.createPattern(this.$.sparklesSvg, 'repeat');
+
+    this.sparklesIntervalId = setInterval(() => {
+      this.sparklesOffset = Math.round(Math.random() * 500);
+    }, 100);
   }
 
   /** Starts the invocation animation into the steady state. */
@@ -385,6 +425,7 @@ export class OverlayShimmerCanvasElement extends PolymerElement {
     this.resetCanvasPixelRatioIfNeeded();
 
     this.drawCircles(timeMs);
+    this.drawSparkles();
     requestAnimationFrame(this.stepAnimation.bind(this));
   }
 
@@ -578,6 +619,31 @@ export class OverlayShimmerCanvasElement extends PolymerElement {
     // If the last transition update resulted in a completed animation, clean up
     // an leftover animation state.
     this.finishAnimationIfNeeded(elapsed);
+  }
+
+  // Draws sparkles on the canvas using circles as an alpha mask.
+  private drawSparkles(): void {
+    if (!this.sparklesPattern) {
+      return;
+    }
+
+    // Update the sparkles position to use across the circles.
+    this.sparklesPattern!.setTransform(new DOMMatrixReadOnly().translate(
+        this.sparklesOffset, this.sparklesOffset));
+
+    this.context.save();
+
+    // Draw a path over the entire canvas.
+    this.context.beginPath();
+    this.context.rect(0, 0, this.canvasWidth, this.canvasHeight);
+    this.context.closePath();
+
+    this.context.globalCompositeOperation = 'source-atop';
+    this.context.globalAlpha = SPARKLES_OPACITY;
+    this.context.fillStyle = this.sparklesPattern;
+    this.context.fill();
+
+    this.context.restore();
   }
 
   // Checks the set canvas size and if it has changed, resets it on the canvas.
