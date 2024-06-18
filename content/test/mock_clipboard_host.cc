bdf372cfb61298cc4bf06530b5db4b4e6abde4fa
Anupam Snigdha <snianu@microsoft.com>
2024-06-18 16:40:36 +0000

    [Clipboard] Rename WriteCustomData to WriteDataTransferCustomData.
    `WriteCustomData` writes the data associated with the Chromium specific
    custom format which is being renamed to `DataTransferCustomType`, so
    rename it to `WriteDataTransferCustomData` to help clarify the
    purpose of the method.
    
    Bug: 333952122
    
    Change-Id: Ic758e46f9d383e3932834ed9f473e4fe74692a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634684
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1316511}

--- content/test/mock_clipboard_host.cc
+++ content/test/mock_clipboard_host.cc
@@ -160,7 +160,7 @@ void MockClipboardHost::WriteSmartPasteMarker() {
   write_smart_paste_ = true;
 }
 
-void MockClipboardHost::WriteCustomData(
+void MockClipboardHost::WriteDataTransferCustomData(
     const base::flat_map<std::u16string, std::u16string>& data) {
   if (needs_reset_)
     Reset();
