98453be75a239c8c2b2d26f1ddf3f5baeccfae7a
Siye Liu <siliu@microsoft.com>
2024-06-18 22:56:37 +0000

    Revert "Set inputscope as IS_PRIVATE for top level inprivate/incognito window"
    This reverts commit afe3d7e805149f1810511abda4fce0520a0234d5.
    
    Reason for revert: The change is causing issues in Google Slides in
    incognito mode: https://issues.chromium.org/issues/344961539.
    
    Original change's description:
    > Set inputscope as IS_PRIVATE for top level inprivate/incognito window
    >
    > If Chromium uses "SetInputScope" to set the IS_PRIVATE Input Scope for
    > the top level window for all InPrivate/Incognito browser windows, we
    > will use this information to honor the user's privacy, avoid using the data which is related/belongs to these private browser windows in Windows text input intelligence features.
    >
    >
    > Bug: 1510772
    > Change-Id: Ib35ecfc5e060690ea15be1e5c09cde0530f88e3e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5112656
    > Reviewed-by: Anupam Snigdha <snianu@microsoft.com>
    > Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    > Commit-Queue: Qianqian Lin <qilin@microsoft.com>
    > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1264112}
    
    Bug: 1510772
    Change-Id: Ie110dda9e6b5db6fb5da1d6a7433d88a4f8a0fe1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639507
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1316744}

--- ui/base/ime/win/input_method_win_tsf.cc
+++ ui/base/ime/win/input_method_win_tsf.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/virtual_keyboard_controller.h"
 #include "ui/base/ime/win/tsf_bridge.h"
 #include "ui/base/ime/win/tsf_event_router.h"
-#include "ui/base/ime/win/tsf_input_scope.h"
 
 namespace ui {
 
