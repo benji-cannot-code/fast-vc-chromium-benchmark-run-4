aae24d19a10f828c7e7dd8f70c4614250aa8d52b
Di Zhang <dizhangg@chromium.org>
2024-06-18 22:10:53 +0000

    Revert "Implement screenshot protection on Mac"
    This reverts commit 262f9bca8e2176aa6f5e94a6f8a0e2d0421fe043.
    
    Reason for revert: Test failing
    Builder: Mac13 Tests
    https://ci.chromium.org/p/chromium/builders/ci/Mac13%20Tests
    First failing build:
    https://ci.chromium.org/p/chromium/builders/ci/Mac13%20Tests/b8744759147963547009
    
    Original change's description:
    > Implement screenshot protection on Mac
    >
    > Follow-up to crrev.com/c/5578257
    >
    > This CL also moves data protection tests in browser_view_browsertest.cc
    > outside of the `!BUILDFLAG(IS_MAC)` section since the rationale for
    > exempting these tests from running on Mac doesn't apply to data
    > protection features.
    >
    > Bug: 322519161
    > Change-Id: I0500643959245099afc0e780d007f39c17b4a4fa
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639337
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Commit-Queue: Scott Violet <sky@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1316660}
    
    Bug: 322519161
    Change-Id: I9c317fed7592f3d7b5b263e74a95b5f08434dfe8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640704
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Di Zhang <dizhangg@google.com>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316720}

--- chrome/browser/ui/views/frame/browser_view_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_view_browsertest.cc
@@ -413,7 +413,6 @@ IN_PROC_BROWSER_TEST_F(BrowserViewTest, GetAccessibleTabModalDialogTree) {
   EXPECT_NE(ui::AXPlatformNodeTestHelper::FindChildByName(ax_node, "OK"),
             nullptr);
 }
-#endif  // !BUILDFLAG(IS_MAC)
 
 #if BUILDFLAG(ENTERPRISE_WATERMARK)
 
@@ -588,7 +587,8 @@ IN_PROC_BROWSER_TEST_F(BrowserViewDataProtectionTest,
                   ->has_text_for_testing());
 }
 
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+// TODO(crbug.com/322519161): Add test for Mac platform once implemented.
+#if BUILDFLAG(IS_WIN)
 
 IN_PROC_BROWSER_TEST_F(BrowserViewDataProtectionTest, DC_Screenshot) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"(
@@ -610,6 +610,8 @@ IN_PROC_BROWSER_TEST_F(BrowserViewDataProtectionTest, DC_Screenshot) {
   EXPECT_TRUE(widget->AreScreenshotsAllowed());
 }
 
-#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+#endif  // BUILDFLAG(IS_WIN)
 
 #endif  // BUILDFLAG(ENTERPRISE_WATERMARK)
+
+#endif  // !BUILDFLAG(IS_MAC)
