0a0faac5f5b2f6e336b73f00f843a690f422158c
thefrog <thefrog@chromium.org>
2024-06-18 18:32:05 +0000

    Remove WebSocketSBHandshakeThrottle non-extensions constructor + params
    Background: WebSocketSBHandshakeThrottle is no longer responsible for
    running Safe Browsing checks for websockets. It now only serves as a
    hook for extension telemetry.
    
    This CL removes the WebSocketSBHandshakeThrottle non-extensions
    constructor and non-extensions parameters from the extensions
    constructor. This also removes the lingering usage of the non-extensions
    constructor in WebSocketHandshakeThrottleProviderImpl.
    
    This CL is one in a series of CLs deprecating unused websocket Safe
    Browsing check code. Expected remaining changes are noted with in-code
    TODOs.
    
    Deprecated histograms:
     - SafeBrowsing.WebSocketCheck.Skipped
     - SafeBrowsing.WebSocketCheck.UrlScheme
    
    OBSOLETE_HISTOGRAM[SafeBrowsing.WebSocketCheck.Skipped]=No longer needed because all checks are skipped.
    OBSOLETE_HISTOGRAM[SafeBrowsing.WebSocketCheck.UrlScheme]=No longer needed because all checks are skipped.
    
    Bug: 40934395
    Change-Id: Ief19501f79d8fe8fef3944624d4372d58e9848bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5388697
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316593}

--- chrome/renderer/websocket_handshake_throttle_provider_impl.cc
+++ chrome/renderer/websocket_handshake_throttle_provider_impl.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebSocketHandshakeThrottleProviderImpl::WebSocketHandshakeThrottleProviderImpl(
     blink::ThreadSafeBrowserInterfaceBrokerProxy* broker) {
   DETACH_FROM_THREAD(thread_checker_);
-  broker->GetInterface(pending_safe_browsing_.InitWithNewPipeAndPassReceiver());
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   broker->GetInterface(
       pending_extension_web_request_reporter_.InitWithNewPipeAndPassReceiver());
@@ -31,9 +30,6 @@ WebSocketHandshakeThrottleProviderImpl::
 WebSocketHandshakeThrottleProviderImpl::WebSocketHandshakeThrottleProviderImpl(
     const WebSocketHandshakeThrottleProviderImpl& other) {
   DETACH_FROM_THREAD(thread_checker_);
-  DCHECK(other.safe_browsing_);
-  other.safe_browsing_->Clone(
-      pending_safe_browsing_.InitWithNewPipeAndPassReceiver());
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   DCHECK(other.extension_web_request_reporter_);
   other.extension_web_request_reporter_->Clone(
@@ -45,9 +41,6 @@ std::unique_ptr<blink::WebSocketHandshakeThrottleProvider>
 WebSocketHandshakeThrottleProviderImpl::Clone(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  if (pending_safe_browsing_) {
-    safe_browsing_.Bind(std::move(pending_safe_browsing_), task_runner);
-  }
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   if (pending_extension_web_request_reporter_) {
     extension_web_request_reporter_.Bind(
@@ -62,22 +55,15 @@ WebSocketHandshakeThrottleProviderImpl::CreateThrottle(
     base::optional_ref<const blink::LocalFrameToken> local_frame_token,
     scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
   DCHECK_CALLED_ON_VALID_THREAD(thread_checker_);
-  if (pending_safe_browsing_) {
-    safe_browsing_.Bind(std::move(pending_safe_browsing_),
-                        std::move(task_runner));
-  }
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   if (pending_extension_web_request_reporter_) {
     extension_web_request_reporter_.Bind(
         std::move(pending_extension_web_request_reporter_));
   }
-  auto throttle = std::make_unique<safe_browsing::WebSocketSBHandshakeThrottle>(
-      safe_browsing_.get(), local_frame_token,
+  return std::make_unique<safe_browsing::WebSocketSBHandshakeThrottle>(
       extension_web_request_reporter_.get());
 #else
-  auto throttle = std::make_unique<safe_browsing::WebSocketSBHandshakeThrottle>(
-      safe_browsing_.get(), local_frame_token);
+  return nullptr;
 #endif
-  return throttle;
 }
