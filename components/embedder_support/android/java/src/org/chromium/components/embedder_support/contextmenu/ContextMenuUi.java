078f5600dfe838e804464abf9254040cf13ba8eb
= <alibu@google.com>
2024-06-18 13:49:54 +0000

    Move ContextMenuUi to //components
    This interface is used for building a Context Menu in //chrome, moving
    it to //components will allow it to be used for a Hyperlink Context
    Menu in WebView as well.
    
    https://docs.google.com/document/d/1e3oMJvGEFPoGc42I_i0eYdWFVrZAuCy-jIvJbc9ctvA/edit?tab=t.0
    
    Bug: b/323344356
    Change-Id: If5f44404b08dfbe9051898bdc1f2771dbcacb1e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634021
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Ali Butt <alibu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316403}

--- components/embedder_support/android/java/src/org/chromium/components/embedder_support/contextmenu/ContextMenuUi.java
+++ components/embedder_support/android/java/src/org/chromium/components/embedder_support/contextmenu/ContextMenuUi.java
@@ -3,12 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.contextmenu;
+package org.chromium.components.embedder_support.contextmenu;
 
 import android.util.Pair;
 
 import org.chromium.base.Callback;
-import org.chromium.components.embedder_support.contextmenu.ContextMenuParams;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
@@ -21,7 +20,7 @@ import java.util.List;
  */
 public interface ContextMenuUi {
     /**
-     * Shows the Context Menu in Chrome.
+     * Shows the Context Menu.
      * @param window Used to inflate the context menu.
      * @param webContents The WebContents that this context menu belongs to.
      * @param params The current parameters for the the context menu.
