3165ab89903b7f1305777241a6a008dee628a649
Lauren Winston <lwinston@google.com>
2024-06-18 19:03:01 +0000

    [Read Anything] Remove accidental import adds.
    Bug: none
    Change-Id: Ic416227c21cf2d2a1a8c47365edca4a823497ef8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5638981
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Auto-Submit: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316609}

--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gin/dictionary.h"
 #include "gin/handle.h"
 #include "gin/object_template_builder.h"
-#include "read_anything_app_controller.h"
 #include "services/metrics/public/cpp/mojo_ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
--- chrome/renderer/accessibility/read_anything_app_controller_browsertest.cc
+++ chrome/renderer/accessibility/read_anything_app_controller_browsertest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/accessibility/ax_tree_distiller.h"
 #include "chrome/test/base/chrome_render_view_test.h"
 #include "content/public/renderer/render_frame.h"
-#include "read_anything_app_controller.h"
 #include "services/strings/grit/services_strings.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "ui/accessibility/accessibility_features.h"
