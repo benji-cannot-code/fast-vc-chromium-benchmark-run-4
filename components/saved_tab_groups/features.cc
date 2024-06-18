cd345e95b3774705234e4313e66063b40526f361
Min Qin <qinmin@chromium.org>
2024-06-18 06:42:49 +0000

    Defer media load on background synced tab
    For tab groups, tabs loaded in the background should not autoplay
    media. This CL prevents that from happening.
    Will clean up the has_played_before in DeferMediaLoad() in a separate
    CL to make merge easier.
    
    Bug: 346840824
    Change-Id: I1aadfb3a61dc6e13465edc0ad239b066f69c7595
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632397
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316272}

--- components/saved_tab_groups/features.cc
+++ components/saved_tab_groups/features.cc
@@ -74,6 +74,11 @@ BASE_FEATURE(kRestrictDownloadOnSyncedTabs,
              "RestrictDownloadOnSyncedTabs",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Feature flag to defer media load on background tab.
+BASE_FEATURE(kDeferMediaLoadInBackgroundTab,
+             "DeferMediaLoadInBackgroundTab",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsTabGroupsSaveV2Enabled() {
   return base::FeatureList::IsEnabled(kTabGroupsSaveV2);
 }
@@ -92,6 +97,10 @@ bool AlwaysAcceptServerDataInModel() {
 
 bool RestrictDownloadOnSyncedTabs() {
   return base::FeatureList::IsEnabled(kRestrictDownloadOnSyncedTabs);
+}
+
+bool DeferMediaLoadInBackgroundTab() {
+  return base::FeatureList::IsEnabled(kDeferMediaLoadInBackgroundTab);
 }
 
 }  // namespace tab_groups
