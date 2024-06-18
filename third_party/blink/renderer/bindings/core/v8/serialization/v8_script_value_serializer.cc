323da8e455094ce3c9a2485b86175e1d315e77ff
Jeremy Roman <jbroman@chromium.org>
2024-06-18 20:22:12 +0000

    V8ScriptValueSerializer: Take StringView instead of String.
    The condition in this comment has now arrived.
    
    Change-Id: Ifca9c45a2f58cd1bc6c8c0f8ed04442af22795d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639816
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316663}

--- third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_serializer.cc
+++ third_party/blink/renderer/bindings/core/v8/serialization/v8_script_value_serializer.cc
@@ -402,9 +402,7 @@ void V8ScriptValueSerializer::WriteUnguessableToken(
   WriteUint64(token.GetLowForSerialization());
 }
 
-void V8ScriptValueSerializer::WriteUTF8String(const String& string) {
-  // TODO(jbroman): Ideally this method would take a WTF::StringView, but the
-  // StringUTF8Adaptor trick doesn't yet work with StringView.
+void V8ScriptValueSerializer::WriteUTF8String(const StringView& string) {
   StringUTF8Adaptor utf8(string);
   WriteUint32(utf8.size());
   WriteRawBytes(utf8.data(), utf8.size());
@@ -821,8 +819,8 @@ bool V8ScriptValueSerializer::WriteDOMObject(ScriptWrappable* wrappable,
     WriteAndRequireInterfaceTag(kFencedFrameConfigTag);
 
     WriteUTF8String(
-        config
-            ->GetValueIgnoringVisibility<FencedFrameConfig::Attribute::kURL>());
+        config->GetValueIgnoringVisibility<FencedFrameConfig::Attribute::kURL>()
+            .GetString());
     WriteUint32(config->GetValueIgnoringVisibility<
                 FencedFrameConfig::Attribute::kWidth>());
     WriteUint32(config->GetValueIgnoringVisibility<
