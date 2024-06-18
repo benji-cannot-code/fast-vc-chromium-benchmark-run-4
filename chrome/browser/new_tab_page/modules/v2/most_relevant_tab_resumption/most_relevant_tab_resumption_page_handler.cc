6724f3c6ec670e900e01e0dea6c2130f98c898df
Marlon Facey <mfacey@chromium.org>
2024-06-18 20:07:55 +0000

    [Tab Resume 1.5] Integrate RecordAction API
    Add RecordAction API to click and dismiss handlers.
    
    Bug: 344891504
    Change-Id: I584ab9a88030f07c464c1a1abb3b1c6a1d9224a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630279
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316651}

--- chrome/browser/new_tab_page/modules/v2/most_relevant_tab_resumption/most_relevant_tab_resumption_page_handler.cc
+++ chrome/browser/new_tab_page/modules/v2/most_relevant_tab_resumption/most_relevant_tab_resumption_page_handler.cc
@@ -153,6 +153,8 @@ void MostRelevantTabResumptionPageHandler::GetTabs(GetTabsCallback callback) {
       auto tab_mojom = TabToMojom(
           CreateSampleURLVisitAggregateTab(GURL("https://www.google.com")));
       tab_mojom->url = GURL("https://www.google.com");
+      tab_mojom->url_key = "https://www.google.com";
+      tab_mojom->training_request_id = 0;
       tabs_mojom.push_back(std::move(tab_mojom));
     }
     std::move(callback).Run(std::move(tabs_mojom));
@@ -181,11 +183,17 @@ void MostRelevantTabResumptionPageHandler::DismissModule(
     const std::vector<history::mojom::TabPtr> tabs) {
   RemoveOldDismissedTabs();
   ScopedListPrefUpdate tab_list(profile_->GetPrefs(), kDismissedTabsPrefName);
+  auto* visited_url_ranking_service =
+      visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
+          profile_);
   for (const auto& tab : tabs) {
     tab_list->Append(base::Value(
         tab->url_key + ' ' +
         base::NumberToString(
             tab->timestamp->ToDeltaSinceWindowsEpoch().InMicroseconds())));
+    visited_url_ranking_service->RecordAction(
+        visited_url_ranking::ScoredURLUserAction::kDismissed, tab->url_key,
+        segmentation_platform::TrainingRequestId(tab->training_request_id));
   }
 }
 
@@ -197,16 +205,28 @@ void MostRelevantTabResumptionPageHandler::DismissTab(
       tab->url_key + ' ' +
       base::NumberToString(
           tab->timestamp->ToDeltaSinceWindowsEpoch().InMicroseconds())));
+  auto* visited_url_ranking_service =
+      visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
+          profile_);
+  visited_url_ranking_service->RecordAction(
+      visited_url_ranking::ScoredURLUserAction::kDismissed, tab->url_key,
+      segmentation_platform::TrainingRequestId(tab->training_request_id));
 }
 
 void MostRelevantTabResumptionPageHandler::RestoreModule(
     const std::vector<history::mojom::TabPtr> tabs) {
   ScopedListPrefUpdate tab_list(profile_->GetPrefs(), kDismissedTabsPrefName);
+  auto* visited_url_ranking_service =
+      visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
+          profile_);
   for (const auto& tab : tabs) {
     tab_list->EraseValue(base::Value(
         tab->url_key + ' ' +
         base::NumberToString(
             tab->timestamp->ToDeltaSinceWindowsEpoch().InMicroseconds())));
+    visited_url_ranking_service->RecordAction(
+        visited_url_ranking::ScoredURLUserAction::kSeen, tab->url_key,
+        segmentation_platform::TrainingRequestId(tab->training_request_id));
   }
 }
 
@@ -217,6 +237,12 @@ void MostRelevantTabResumptionPageHandler::RestoreTab(
       tab->url_key + ' ' +
       base::NumberToString(
           tab->timestamp->ToDeltaSinceWindowsEpoch().InMicroseconds())));
+  auto* visited_url_ranking_service =
+      visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
+          profile_);
+  visited_url_ranking_service->RecordAction(
+      visited_url_ranking::ScoredURLUserAction::kSeen, tab->url_key,
+      segmentation_platform::TrainingRequestId(tab->training_request_id));
 }
 
 void MostRelevantTabResumptionPageHandler::OnGotRankedURLVisitAggregates(
@@ -233,6 +259,8 @@ void MostRelevantTabResumptionPageHandler::OnGotRankedURLVisitAggregates(
       auto tab_mojom = TabToMojom(*tab);
       tab_mojom->url = **url_visit_aggregate.GetAssociatedURLs().begin();
       tab_mojom->url_key = url_visit_aggregate.url_key;
+      tab_mojom->training_request_id =
+          url_visit_aggregate.request_id.GetUnsafeValue();
       if (IsNewURL(tab_mojom)) {
         tabs_mojom.push_back(std::move(tab_mojom));
         base::UmaHistogramEnumeration(
@@ -248,6 +276,8 @@ void MostRelevantTabResumptionPageHandler::OnGotRankedURLVisitAggregates(
         history_tab_mojom->url =
             **url_visit_aggregate.GetAssociatedURLs().begin();
         history_tab_mojom->url_key = url_visit_aggregate.url_key;
+        history_tab_mojom->training_request_id =
+            url_visit_aggregate.request_id.GetUnsafeValue();
         if (IsNewURL(history_tab_mojom)) {
           tabs_mojom.push_back(std::move(history_tab_mojom));
           base::UmaHistogramEnumeration(
@@ -299,4 +329,15 @@ void MostRelevantTabResumptionPageHandler::RemoveOldDismissedTabs() {
       }
     }
   }
+}
+
+void MostRelevantTabResumptionPageHandler::RecordActivatedAction(
+    const std::string& url_key,
+    int64_t visit_request_id) {
+  auto* visited_url_ranking_service =
+      visited_url_ranking::VisitedURLRankingServiceFactory::GetForProfile(
+          profile_);
+  visited_url_ranking_service->RecordAction(
+      visited_url_ranking::ScoredURLUserAction::kActivated, url_key,
+      segmentation_platform::TrainingRequestId(visit_request_id));
 }
--- chrome/test/data/webui/new_tab_page/modules/v2/most_relevant_tab_resumption/module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/most_relevant_tab_resumption/module_test.ts
@@ -36,6 +36,7 @@ function createSampleTab(
         urlKey: '',
         title: 'Test Tab Title',
         timestamp: Date.now(),
+        trainingRequestId: 0,
         relativeTime: {microseconds: BigInt(0)},
         relativeTimeText: '0 seconds ago',
       },
@@ -177,6 +178,7 @@ suite('NewTabPageModulesMostRelevantTabResumptionModuleTest', () => {
       tabElement!.removeAttribute('href');
       tabElement!.click();
       assertEquals(1, metrics.count(`NewTabPage.TabResumption.ClickIndex`));
+      assertEquals(1, handler.getCallCount('recordActivatedAction'));
 
       await waitForUsageEvent;
     });
