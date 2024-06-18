0423aa25760b62e66d175c8f20c0bb14b19965f5
David Maunder <davidjm@google.com>
2024-06-18 22:21:16 +0000

    Add observer method for ProductSpecificationSet name update
    Bug: 348007274
    Change-Id: I16f97c56f9f8b9cce748ecd839208fbf13cb408d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5632574
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316726}

--- components/commerce/core/product_specifications/product_specifications_service_unittest.cc
+++ components/commerce/core/product_specifications/product_specifications_service_unittest.cc
@@ -130,6 +130,10 @@ MATCHER_P2(HasProductSpecsNameUrl, name, urls, "") {
   return arg.name() == name && arg.urls() == urls;
 }
 
+MATCHER_P(HasProductSpecsName, name, "") {
+  return arg == name;
+}
+
 }  // namespace
 
 namespace commerce {
@@ -146,6 +150,11 @@ class MockProductSpecificationsSetObserver
               OnProductSpecificationsSetUpdate,
               (const ProductSpecificationsSet& before,
                const ProductSpecificationsSet& after),
+              (override));
+
+  MOCK_METHOD(void,
+              OnProductSpecificationsSetNameUpdate,
+              (const std::string& before, const std::string& after),
               (override));
 
   MOCK_METHOD(void,
@@ -369,13 +378,17 @@ TEST_F(ProductSpecificationsServiceTest, TestSetName) {
 
   const base::Uuid uuid_to_modify = specifications[0].uuid();
 
+  const std::string new_name = "updated name";
   EXPECT_CALL(*observer(),
               OnProductSpecificationsSetUpdate(
                   HasAllProductSpecs(kProductComparisonSpecifics[0]),
                   IsSetWithUuid(uuid_to_modify)))
       .Times(1);
-
-  const std::string new_name = "updated name";
+  EXPECT_CALL(*observer(),
+              OnProductSpecificationsSetNameUpdate(
+                  HasProductSpecsName(kProductComparisonSpecifics[0].name()),
+                  HasProductSpecsName(new_name)))
+      .Times(1);
 
   service()->SetName(uuid_to_modify, new_name);
 
@@ -402,6 +415,10 @@ TEST_F(ProductSpecificationsServiceTest, TestSetNameAndUrls_BadId) {
 
   EXPECT_CALL(*observer(),
               OnProductSpecificationsSetUpdate(testing::_, testing::_))
+      .Times(0);
+
+  EXPECT_CALL(*observer(),
+              OnProductSpecificationsSetNameUpdate(testing::_, testing::_))
       .Times(0);
 
   const std::vector<GURL> new_urls = {GURL("http://example.com/updated")};
