67535d12a511df36939b128d8d596ecc69a33520
Samar <samarchehade@google.com>
2024-06-18 18:14:17 +0000

    [Signin][Android] Correctly handle the case when account fetching fails.
    We also fix the test suite so that it doesn't carry over the list of
    CoreAccountInfos from previous tests. This way the tests fail with the
    previous implementation but pass now.
    
    Fixed: 345960089
    Change-Id: I46051093266b1ba544462974463c03b47a689217
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630393
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1316581}

--- components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
@@ -366,7 +366,7 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
                         // We shouldn't wait indefinitely for the account fetching to succeed, at it
                         // might block certain features. Fall back to an empty list to allow the
                         // user to proceed.
-                        allAccounts = mAllAccounts.get() == null ? mAllAccounts.get() : List.of();
+                        allAccounts = mAllAccounts.get() == null ? List.of() : mAllAccounts.get();
                     }
                 }
                 if (mNumberOfRetries != 0) {
@@ -433,5 +433,11 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
             return capabilityName.substring(ACCOUNT_CAPABILITY_NAME_PREFIX.length());
         }
         return capabilityName;
+    }
+
+    public void resetAccountsForTesting() {
+        mCoreAccountInfosPromise = new Promise<>();
+        mAllAccounts.set(null);
+        updateAccounts();
     }
 }
--- components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
+++ components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
@@ -117,7 +117,7 @@ public class AccountManagerFacadeImplTest {
     private ShadowPostTaskImpl mPostTaskRunner;
 
     private FakeAccountManagerDelegate mDelegate;
-    private AccountManagerFacade mFacade;
+    private AccountManagerFacadeImpl mFacade;
 
     @Mock private AccountManagerDelegate mDelegateMock;
 
@@ -138,6 +138,7 @@ public class AccountManagerFacadeImplTest {
         ThreadUtils.setThreadAssertsDisabledForTesting(true);
         mDelegate = spy(new FakeAccountManagerDelegate());
         mFacade = new AccountManagerFacadeImpl(mDelegate);
+        mFacade.resetAccountsForTesting();
 
         mFacadeWithSystemDelegate =
                 new AccountManagerFacadeImpl(new SystemAccountManagerDelegate());
@@ -396,6 +397,7 @@ public class AccountManagerFacadeImplTest {
 
     @Test
     public void testGetCoreAccountInfosWithAccountPatternsChange() throws Exception {
+        mDelegate.callOnCoreAccountInfoChanged();
         Assert.assertEquals(List.of(), mFacade.getCoreAccountInfos().getResult());
 
         CoreAccountInfo accountInfo1 = addTestAccount("test1@gmail.com");
