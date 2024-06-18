5804b798f4701835a5866e3265b4f203c564a4cd
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-06-18 20:28:04 +0000

    Add GmsCoreVersionRestriction for 24w15 and use it in one test
    The goal is for the version to be above this constant.
    https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/features/password_features.h;l=164;drc=44f4a4979b2fa3ea13be2f3d5558eeda603e74b1
    
    Note: I bumped most bots to 24w22 in crrev.com/c/5633475.
    
    Bug: 324370397
    Change-Id: Iae9979baaeaf7e98cab48eab2893773691ba13ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630720
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316669}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browsing_data/BrowsingDataTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browsing_data/BrowsingDataTest.java
@@ -22,6 +22,7 @@ import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.Features.EnableFeatures;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.password_manager.PasswordManagerTestHelper;
@@ -35,6 +36,7 @@ import org.chromium.chrome.test.util.browser.signin.SigninTestRule;
 import org.chromium.content_public.browser.test.util.JavaScriptUtils;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.net.test.EmbeddedTestServer;
+import org.chromium.ui.test.util.GmsCoreVersionRestriction;
 import org.chromium.url.GURL;
 
 import java.util.Arrays;
@@ -45,7 +47,6 @@ import java.util.concurrent.TimeoutException;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @CommandLineFlags.Add({
     ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
-    ChromeSwitches.SKIP_LOCAL_UPM_GMS_CORE_VERSION_CHECK_FOR_TESTING,
 })
 @Batch(Batch.PER_CLASS)
 public class BrowsingDataTest {
@@ -199,6 +200,7 @@ public class BrowsingDataTest {
     @EnableFeatures({
         ChromeFeatureList.UNIFIED_PASSWORD_MANAGER_LOCAL_PASSWORDS_ANDROID_NO_MIGRATION
     })
+    @Restriction({GmsCoreVersionRestriction.RESTRICTION_TYPE_VERSION_GE_24W15})
     public void testLocalAndAccountPasswordsDeleted() throws Exception {
         // Set up a syncing user with one password in each store.
         mSigninTestRule.addTestAccountThenSigninAndEnableSync();
