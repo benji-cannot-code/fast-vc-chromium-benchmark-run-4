7aa97cabdc1fae35334afcc0c8fc81bbaea54d46
Darren Shen <shend@chromium.org>
2024-06-18 04:14:15 +0000

    Move preset editor list item logic to AddResult.
    Bug: 343092747
    Change-Id: I52c6ff3b4c587053af2ac70c7a0f87ac90e46098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5635951
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316245}

--- ash/picker/views/picker_section_view.cc
+++ ash/picker/views/picker_section_view.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/typography.h"
 #include "base/functional/overloaded.h"
 #include "base/notreached.h"
+#include "chromeos/components/editor_menu/public/cpp/icon.h"
 #include "chromeos/ui/base/file_icon_util.h"
 #include "chromeos/ui/vector_icons/vector_icons.h"
 #include "components/vector_icons/vector_icons.h"
@@ -268,9 +269,19 @@ PickerItemView* PickerSectionView::AddResult(
           [&](const PickerSearchResult::EditorData& data) -> PickerItemView* {
             auto item_view = std::make_unique<PickerListItemView>(
                 std::move(select_result_callback));
-            const PickerCategory category = GetCategoryForEditorData(data);
-            item_view->SetPrimaryText(GetLabelForPickerCategory(category));
-            item_view->SetLeadingIcon(GetIconForPickerCategory(category));
+            if (data.category.has_value()) {
+              // Preset write or rewrite.
+              item_view->SetPrimaryText(data.display_name);
+              item_view->SetLeadingIcon(ui::ImageModel::FromVectorIcon(
+                  chromeos::editor_menu::GetIconForPresetQueryCategory(
+                      *data.category),
+                  cros_tokens::kCrosSysOnSurface));
+            } else {
+              // Freeform write or rewrite.
+              const PickerCategory category = GetCategoryForEditorData(data);
+              item_view->SetPrimaryText(GetLabelForPickerCategory(category));
+              item_view->SetLeadingIcon(GetIconForPickerCategory(category));
+            }
             return AddListItem(std::move(item_view));
           },
       },
