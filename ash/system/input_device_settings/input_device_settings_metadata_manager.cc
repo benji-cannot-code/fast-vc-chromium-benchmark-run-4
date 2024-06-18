49a87812522cbaaa87f4ab589bd38303cb2a394a
Michael Checo <michaelcheco@google.com>
2024-06-18 03:25:12 +0000

    [WE] Create unique keys for tracking image requests
    - This allows image requests for the Settings UI and first-time
      connection notifications to be tracked separately.
    
    Bug: b/329686601
    Change-Id: Ife8e52c4bdce7b7c51a8bdfc89dd95ec69848aa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598041
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316236}

--- ash/system/input_device_settings/input_device_settings_metadata_manager.cc
+++ ash/system/input_device_settings/input_device_settings_metadata_manager.cc
@@ -13,6 +13,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+namespace {
+
+std::string GenerateImageRequestKey(const std::string& key,
+                                    DeviceImageDestination destination) {
+  return key + "_" + base::NumberToString(static_cast<int>(destination));
+}
+
+}  // namespace
+
 InputDeviceSettingsMetadataManager::InputDeviceSettingsMetadataManager(
     std::unique_ptr<DeviceImageDownloader> image_downloader,
     std::unique_ptr<DeviceImageStorage> image_storage)
@@ -55,7 +64,8 @@ void InputDeviceSettingsMetadataManager::OnDeviceImageFetched(
     device_image_storage_->PersistDeviceImage(device_key,
                                               device_image.data_url());
   }
-  auto it = device_callback_map_.find(device_key);
+  auto it = device_callback_map_.find(
+      GenerateImageRequestKey(device_key, destination));
 
   if (it == device_callback_map_.end()) {
     return;
@@ -83,7 +93,8 @@ void InputDeviceSettingsMetadataManager::GetDeviceImagePreferringCache(
     std::move(callback).Run(DeviceImage(device_key, device_image.value()));
     return;
   }
-  device_callback_map_[device_key].push_back(std::move(callback));
+  device_callback_map_[GenerateImageRequestKey(device_key, destination)]
+      .push_back(std::move(callback));
   image_downloader_->DownloadImage(
       device_key, account_id, destination,
       base::BindOnce(&InputDeviceSettingsMetadataManager::OnDeviceImageFetched,
