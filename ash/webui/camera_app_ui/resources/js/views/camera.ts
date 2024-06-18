76010fd2e7a140bb959a36e288ce884b90720a35
Dorah <dorahkim@google.com>
2024-06-18 04:05:41 +0000

    CCA: change perf metrics naming
    This CL changes photo/video/time-lapse/portrait-mode post processing
    metrics to include saving in their names, since the metrics include
    file saving as well.
    
    BUG=b:331904752
    Test=Manually.
    
    Change-Id: I6e22b50e1c97b1e75e27619e20a371aaade77c00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5524597
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Dorah Kim <dorahkim@google.com>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316242}

--- ash/webui/camera_app_ui/resources/js/views/camera.ts
+++ ash/webui/camera_app_ui/resources/js/views/camera.ts
@@ -511,7 +511,7 @@ export class Camera extends View implements CameraViewUI {
 
   async onPhotoCaptureDone(pendingPhotoResult: Promise<PhotoResult>):
       Promise<void> {
-    state.set(PerfEvent.PHOTO_CAPTURE_POST_PROCESSING, true);
+    state.set(PerfEvent.PHOTO_CAPTURE_POST_PROCESSING_SAVING, true);
 
     pendingPhotoResult = this.cropIfUsingSquareResolution(pendingPhotoResult);
 
@@ -538,11 +538,12 @@ export class Camera extends View implements CameraViewUI {
         throw e;
       }
       state.set(
-          PerfEvent.PHOTO_CAPTURE_POST_PROCESSING, false,
+          PerfEvent.PHOTO_CAPTURE_POST_PROCESSING_SAVING, false,
           {resolution, facing: this.getFacing()});
     } catch (e) {
       state.set(
-          PerfEvent.PHOTO_CAPTURE_POST_PROCESSING, false, {hasError: true});
+          PerfEvent.PHOTO_CAPTURE_POST_PROCESSING_SAVING, false,
+          {hasError: true});
       throw e;
     }
     ChromeHelper.getInstance().maybeTriggerSurvey();
@@ -551,7 +552,7 @@ export class Camera extends View implements CameraViewUI {
   async onPortraitCaptureDone(
       pendingReference: Promise<PhotoResult>,
       pendingPortrait: Promise<PhotoResult>): Promise<void> {
-    state.set(PerfEvent.PORTRAIT_MODE_CAPTURE_POST_PROCESSING, true);
+    state.set(PerfEvent.PORTRAIT_MODE_CAPTURE_POST_PROCESSING_SAVING, true);
 
     let filenamer: Filenamer;
 
@@ -616,7 +617,7 @@ export class Camera extends View implements CameraViewUI {
     }
     const hasError = error !== null;
     state.set(
-        PerfEvent.PORTRAIT_MODE_CAPTURE_POST_PROCESSING, false,
+        PerfEvent.PORTRAIT_MODE_CAPTURE_POST_PROCESSING_SAVING, false,
         {hasError, facing: this.getFacing()});
     if (hasError) {
       throw error;
@@ -813,7 +814,7 @@ export class Camera extends View implements CameraViewUI {
     if (autoStopped) {
       this.showLowStorageDialog(LowStorageDialogType.AUTO_STOP);
     }
-    state.set(PerfEvent.VIDEO_CAPTURE_POST_PROCESSING, true);
+    state.set(PerfEvent.VIDEO_CAPTURE_POST_PROCESSING_SAVING, true);
     try {
       metrics.sendCaptureEvent({
         recordType: metrics.RecordType.NORMAL_VIDEO,
@@ -829,11 +830,12 @@ export class Camera extends View implements CameraViewUI {
       const file = assertExists(await videoSaver.endWrite());
       await this.resultSaver.saveVideo(file);
       state.set(
-          PerfEvent.VIDEO_CAPTURE_POST_PROCESSING, false,
+          PerfEvent.VIDEO_CAPTURE_POST_PROCESSING_SAVING, false,
           {resolution, facing: this.getFacing()});
     } catch (e) {
       state.set(
-          PerfEvent.VIDEO_CAPTURE_POST_PROCESSING, false, {hasError: true});
+          PerfEvent.VIDEO_CAPTURE_POST_PROCESSING_SAVING, false,
+          {hasError: true});
       throw e;
     }
     ChromeHelper.getInstance().maybeTriggerSurvey();
@@ -846,7 +848,7 @@ export class Camera extends View implements CameraViewUI {
       this.showLowStorageDialog(LowStorageDialogType.AUTO_STOP);
     }
     nav.open(ViewName.FLASH, I18nString.MSG_PROCESSING_VIDEO);
-    state.set(PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING, true);
+    state.set(PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING_SAVING, true);
     try {
       metrics.sendCaptureEvent({
         recordType: metrics.RecordType.TIME_LAPSE,
@@ -863,11 +865,11 @@ export class Camera extends View implements CameraViewUI {
       const file = assertExists(await timeLapseSaver.endWrite());
       await this.resultSaver.saveVideo(file);
       state.set(
-          PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING, false,
+          PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING_SAVING, false,
           {resolution, facing: this.getFacing()});
     } catch (e) {
       state.set(
-          PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING, false,
+          PerfEvent.TIME_LAPSE_CAPTURE_POST_PROCESSING_SAVING, false,
           {hasError: true});
       throw e;
     } finally {
