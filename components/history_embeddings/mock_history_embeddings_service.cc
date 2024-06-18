5fe74c34f029ffa80a22491e6a25e3d6e6699ae8
manukh <manukh@chromium.org>
2024-06-18 19:21:51 +0000

    [omnibox][history-embeddings] Test coverage for embeddings provider
    Bug: 332360110
    Change-Id: Ia20ab93258c431b4833367be524b000b7ae5dfbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5638228
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316627}

---
+++ components/history_embeddings/mock_history_embeddings_service.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/history_embeddings/mock_history_embeddings_service.h"
+
+namespace history_embeddings {
+
+MockHistoryEmbeddingsService::MockHistoryEmbeddingsService()
+    : HistoryEmbeddingsService(nullptr, nullptr, nullptr, nullptr, nullptr) {}
+
+MockHistoryEmbeddingsService::~MockHistoryEmbeddingsService() = default;
+
+}  // namespace history_embeddings
