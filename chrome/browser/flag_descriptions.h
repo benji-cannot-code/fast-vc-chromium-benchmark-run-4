9800596c72ae9335db96029aafdd2baf2247db4d
siashah <siashah@chromium.org>
2024-06-18 13:41:44 +0000

    Add flag for Pix code detection on copy event
    Bug: b/345277115
    Change-Id: I9598ddfac5ac40d24694ade78de949d88edbd721
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634384
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316397}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2531,6 +2531,9 @@ extern const char kEnableEncryptedAV1Description[];
 extern const char kEnablePixDetectionName[];
 extern const char kEnablePixDetectionDescription[];
 
+extern const char kEnablePixDetectionOnCopyEventName[];
+extern const char kEnablePixDetectionOnCopyEventDescription[];
+
 extern const char kEnablePixDetectionOnDomContentLoadedName[];
 extern const char kEnablePixDetectionOnDomContentLoadedDescription[];
 
