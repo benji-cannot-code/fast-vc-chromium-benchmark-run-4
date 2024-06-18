c79c744db8f5dd1a048a2fec577efd1c3ff32733
Dave Tapuska <dtapuska@chromium.org>
2024-06-18 19:11:18 +0000

    [Code Health] Remove a use of base::SupportsWeakPtr. (ml)
    Bug: 40485134
    Change-Id: If24950c568e88d5d0880ed413a3a75df06704aa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639237
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316618}

--- services/on_device_model/ml/on_device_model_executor.cc
+++ services/on_device_model/ml/on_device_model_executor.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/numerics/safe_conversions.h"
@@ -109,7 +110,7 @@ uint32_t GetTopK(std::optional<uint32_t> top_k) {
 }
 
 // Handles sending and canceling responses.
-class Responder : public base::SupportsWeakPtr<Responder> {
+class Responder final {
  public:
   explicit Responder(
       mojo::PendingRemote<on_device_model::mojom::StreamingResponder> responder,
@@ -126,7 +127,7 @@ class Responder : public base::SupportsWeakPtr<Responder> {
   ChromeMLCancelFn* GetCancelFn() { return &cancel_; }
 
   ChromeMLExecutionOutputFn CreateOutputFn() {
-    return [weak_ptr = AsWeakPtr(),
+    return [weak_ptr = weak_ptr_factory_.GetWeakPtr(),
             task_runner = base::SequencedTaskRunner::GetCurrentDefault()](
                const ChromeMLExecutionOutput* output) {
       std::optional<std::string> text;
@@ -218,11 +219,12 @@ class Responder : public base::SupportsWeakPtr<Responder> {
   const scoped_refptr<LanguageDetector> language_detector_;
   ChromeMLCancelFn cancel_;
   base::OnceClosure on_complete_;
+  base::WeakPtrFactory<Responder> weak_ptr_factory_{this};
 };
 
 // Handles calling the ContextClient on completion and canceling the context
 // request.
-class ContextHolder : public base::SupportsWeakPtr<ContextHolder> {
+class ContextHolder final {
  public:
   explicit ContextHolder(
       mojo::PendingRemote<on_device_model::mojom::ContextClient> client,
@@ -249,6 +251,10 @@ class ContextHolder : public base::SupportsWeakPtr<ContextHolder> {
 
   ChromeMLContextSavedFn CreateContextSavedFn() {
     return CreateWeakCallbackFn(&ContextHolder::OnComplete, this);
+  }
+
+  base::WeakPtr<ContextHolder> AsWeakPtr() {
+    return weak_ptr_factory_.GetWeakPtr();
   }
 
  private:
@@ -281,6 +287,7 @@ class ContextHolder : public base::SupportsWeakPtr<ContextHolder> {
   base::OnceCallback<void(ContextHolder*)> on_disconnect_;
   ChromeMLCancelFn cancel_;
   base::OnceClosure on_complete_;
+  base::WeakPtrFactory<ContextHolder> weak_ptr_factory_{this};
 };
 
 class SessionImpl : public on_device_model::OnDeviceModel::Session {
