8526cf2affddb056ceea3fad16ff4a1e49adc968
Sky Malice <skym@chromium.org>
2024-06-18 22:37:01 +0000

    Update Hub FAB text to look better with high contrast.
    Bug: 348027655
    Change-Id: I9941eed0e7539a77d7f81d871fae4db503bc151b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640409
    Commit-Queue: Sky Malice <skym@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316736}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostViewRenderTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubPaneHostViewRenderTest.java
@@ -44,7 +44,7 @@ public class HubPaneHostViewRenderTest {
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_MOBILE_HUB)
-                    .setRevision(3)
+                    .setRevision(4)
                     .build();
 
     private Activity mActivity;
