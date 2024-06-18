e18e184c0d9050f13030b6e0c5ac2dd9fedd2c58
Calder Kitagawa <ckitagawa@chromium.org>
2024-06-18 21:41:39 +0000

    [Tab Switcher] Fix scrollToTab offset being wrong with declutter active
    With a message card for declutter present the scroll offset here will
    be wrong. Fix this by accounting for the offset.
    
    Bug: 40946413
    Fixed: 348028400
    Change-Id: If609a0d186779e2a92486c83219f96079a64ef4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639819
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316703}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneMediatorUnitTest.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneMediatorUnitTest.java
@@ -10,6 +10,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -59,6 +60,7 @@ import org.chromium.chrome.browser.tasks.tab_management.TabGridDialogMediator.Di
 import org.chromium.chrome.browser.tasks.tab_management.TabListCoordinator.TabListMode;
 import org.chromium.chrome.browser.tasks.tab_management.TabListEditorCoordinator.TabListEditorController;
 import org.chromium.chrome.browser.tasks.tab_management.TabListMediator.TabActionListener;
+import org.chromium.chrome.browser.tasks.tab_management.TabSwitcherPaneMediator.TabIndexLookup;
 import org.chromium.chrome.features.start_surface.StartSurfaceUserData;
 import org.chromium.chrome.test.util.browser.tabmodel.MockTabModel;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler.BackPressResult;
@@ -85,6 +87,7 @@ public class TabSwitcherPaneMediatorUnitTest {
     @Mock private View mCustomView;
     @Mock private Runnable mCustomViewBackPressRunnable;
     @Mock private Callback<Integer> mOnTabClickedCallback;
+    @Mock private TabIndexLookup mTabIndexLookup;
 
     @Captor private ArgumentCaptor<TabModelObserver> mTabModelObserverCaptor;
 
@@ -114,6 +117,7 @@ public class TabSwitcherPaneMediatorUnitTest {
     @Before
     public void setUp() {
         when(mProfile.isOffTheRecord()).thenReturn(false);
+        when(mTabIndexLookup.getNthTabIndexInModel(anyInt())).thenAnswer(i -> i.getArguments()[0]);
         mTabModel = new MockTabModel(mProfile, null);
         mTabModel.addTab(
                 new MockTab(UNGROUPED_TAB_ID, mProfile, TabLaunchType.FROM_START_SURFACE),
@@ -167,7 +171,8 @@ public class TabSwitcherPaneMediatorUnitTest {
                         mOnTabSwitcherShownRunnable,
                         mIsVisibleSupplier,
                         mIsAnimatingSupplier,
-                        mOnTabClickedCallback);
+                        mOnTabClickedCallback,
+                        mTabIndexLookup);
 
         assertTrue(mTabModelFilterSupplier.hasObservers());
         assertTrue(mIsVisibleSupplier.hasObservers());
@@ -257,7 +262,8 @@ public class TabSwitcherPaneMediatorUnitTest {
                         mOnTabSwitcherShownRunnable,
                         mIsVisibleSupplier,
                         mIsAnimatingSupplier,
-                        mOnTabClickedCallback);
+                        mOnTabClickedCallback,
+                        mTabIndexLookup);
         ShadowLooper.runUiThreadTasks();
 
         mIsVisibleSupplier.set(true);
@@ -410,6 +416,18 @@ public class TabSwitcherPaneMediatorUnitTest {
 
         mMediator.scrollToTabById(GROUPED_TAB_2_ID);
         assertEquals(1, mModel.get(INITIAL_SCROLL_INDEX).intValue());
+
+        int overrideIndex = 7;
+        when(mTabIndexLookup.getNthTabIndexInModel(anyInt())).thenReturn(overrideIndex);
+
+        mMediator.setInitialScrollIndexOffset();
+        assertEquals(overrideIndex, mModel.get(INITIAL_SCROLL_INDEX).intValue());
+
+        mMediator.scrollToTab(index);
+        assertEquals(index, mModel.get(INITIAL_SCROLL_INDEX).intValue());
+
+        mMediator.scrollToTabById(GROUPED_TAB_2_ID);
+        assertEquals(overrideIndex, mModel.get(INITIAL_SCROLL_INDEX).intValue());
     }
 
     @Test
