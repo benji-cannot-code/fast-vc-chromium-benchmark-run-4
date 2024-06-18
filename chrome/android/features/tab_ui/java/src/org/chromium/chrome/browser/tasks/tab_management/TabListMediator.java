0ba233b7754fad23912a0241f6335f8e6e8df299
Brandon Wylie <wylieb@chromium.org>
2024-06-18 21:04:22 +0000

    Use indexOfNthTabCard when calling into updateTab
    Bug: 347970497
    Change-Id: I96a89df7676bd1df401dadec9ff8625b955d9200
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640534
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1316685}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListMediator.java
@@ -1140,7 +1140,12 @@ class TabListMediator {
                                 final int currentSelectedTabId =
                                         TabModelUtils.getCurrentTabId(groupFilter.getTabModel());
                                 boolean isSelected = currentSelectedTabId == groupTab.getId();
-                                updateTab(groupIndex, groupTab, isSelected, true, false);
+                                updateTab(
+                                        mModel.indexOfNthTabCard(groupIndex),
+                                        groupTab,
+                                        isSelected,
+                                        true,
+                                        false);
 
                                 return;
                             }
