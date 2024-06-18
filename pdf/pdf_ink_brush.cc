bcd480ee89b2444ea9b4933f44206b3f6dd086ee
Alan Screen <awscreen@chromium.org>
2024-06-18 19:03:39 +0000

    [PDF Ink Signatures] Add brush invalidation area method
    Points collected while doing stroking with a brush will not be visible
    to the user unless the viewport is properly notified to repaint the
    affected areas.
    
    Introduce a PdfInkBrush method to help determine the minimal area to be
    invalidated based on the size of the brush.
    
    Bug: 335517471
    Change-Id: Ie0b787d6fc8b5a028d1e39b42749cdf02e173738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634395
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1316610}

--- pdf/pdf_ink_brush.cc
+++ pdf/pdf_ink_brush.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/ink/ink_brush_family.h"
 #include "pdf/ink/ink_brush_paint.h"
 #include "pdf/ink/ink_brush_tip.h"
+#include "ui/gfx/geometry/point_f.h"
+#include "ui/gfx/geometry/rect_conversions.h"
 
 namespace chrome_pdf {
 
@@ -65,6 +67,17 @@ std::unique_ptr<InkBrush> CreateInkBrush(PdfInkBrush::Type type,
                           /*epsilon=*/0.1f);
 }
 
+// Determine the area to invalidate centered around a point where a brush is
+// applied.
+gfx::Rect GetPointInvalidateArea(float brush_diameter,
+                                 const gfx::PointF& center) {
+  // Choose a rectangle that surrounds the point for the brush radius.
+  float brush_radius = brush_diameter / 2;
+  return gfx::ToEnclosingRect(gfx::RectF(center.x() - brush_radius,
+                                         center.y() - brush_radius,
+                                         brush_diameter, brush_diameter));
+}
+
 }  // namespace
 
 // static
@@ -88,6 +101,19 @@ PdfInkBrush::~PdfInkBrush() = default;
 
 const InkBrush& PdfInkBrush::GetInkBrush() const {
   return *ink_brush_;
+}
+
+// Determine the area to invalidate encompassing a line between two
+// consecutive points where a brush is applied.
+gfx::Rect PdfInkBrush::GetInvalidateArea(const gfx::PointF& center1,
+                                         const gfx::PointF& center2) const {
+  // For a line connecting `center1` to `center2`, the invalidate
+  // region is the union between the areas affected by them both.
+  float brush_diameter = ink_brush_->GetSize();
+  gfx::Rect area1 = GetPointInvalidateArea(brush_diameter, center1);
+  gfx::Rect area2 = GetPointInvalidateArea(brush_diameter, center2);
+  area2.Union(area1);
+  return area2;
 }
 
 }  // namespace chrome_pdf
