99f9ae076ce1491db1b2397da19455201262ad2a
Pi-Hsun Shih <pihsun@chromium.org>
2024-06-18 15:10:59 +0000

    recorder: Add soda integration
    Add soda integration by adding a chrome_recorder_app_ui_delegate that
    can be called to install SODA, and listen to the soda installation
    progress in the WebUI controller.
    
    Bug: b:343340409
    Test: compile
    Test: recorder app works with CL:5599246
    Change-Id: Ib739bef2b9a32e600b7314a08c1616252b60828d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5596133
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316454}

---
+++ ash/webui/recorder_app_ui/recorder_app_ui_delegate.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_WEBUI_RECORDER_APP_UI_RECORDER_APP_UI_DELEGATE_H_
+#define ASH_WEBUI_RECORDER_APP_UI_RECORDER_APP_UI_DELEGATE_H_
+
+#include "components/soda/constants.h"
+
+namespace ash {
+// A delegate which exposes browser functionality from //chrome to the recorder
+// app ui page handler.
+class RecorderAppUIDelegate {
+ public:
+  virtual void InstallSoda(speech::LanguageCode languageCode) = 0;
+
+  virtual ~RecorderAppUIDelegate() = default;
+};
+
+}  // namespace ash
+
+#endif  // ASH_WEBUI_RECORDER_APP_UI_RECORDER_APP_UI_DELEGATE_H_
