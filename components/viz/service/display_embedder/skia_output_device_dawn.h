224daddee9bea36ced71fce3ef55f97aeb3db2d1
kylechar <kylechar@chromium.org>
2024-06-18 16:44:13 +0000

    Have SkiaOutputDeviceDawn creation fail
    SkiaOutputDeviceDawn currently crashes due to CHECK failure if
    initialization fails. Rather than crash the GPU process, add
    SkiaOutputDeviceDawn::Create() which will return null on initialization
    failure instead.
    
    This fixes GPU process restart on Android with Graphite/Dawn/Vulkan.
    After the GPU process crashes the next call to
    vkCreateAndroidSurfaceKHR() will fail. This failure is presented to the
    browser as ContextResult::kSurfaceFailure which causes the browser to
    recreate the Java surface. The next try to initialize
    SkiaOutputDeviceDawn is successful. This matches the behavior with
    SkiaOutputDeviceVulkan and SkiaOutputDeviceGL although it's still not
    ideal.
    
    Bug: 347047834
    Change-Id: Ie0873b083ca74af35720ca0e41eb01fc690bdf98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5634232
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316514}

--- components/viz/service/display_embedder/skia_output_device_dawn.h
+++ components/viz/service/display_embedder/skia_output_device_dawn.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "base/types/pass_key.h"
 #include "build/build_config.h"
 #include "components/viz/service/display_embedder/skia_output_device.h"
 #include "third_party/dawn/include/dawn/native/DawnNative.h"
@@ -34,12 +35,21 @@ namespace viz {
 
 class SkiaOutputDeviceDawn : public SkiaOutputDevice {
  public:
-  SkiaOutputDeviceDawn(
+  using PassKey = base::PassKey<SkiaOutputDeviceDawn>;
+
+  static std::unique_ptr<SkiaOutputDeviceDawn> Create(
       scoped_refptr<gpu::SharedContextState> context_state,
       gfx::SurfaceOrigin origin,
       gpu::SurfaceHandle surface_handle,
       gpu::MemoryTracker* memory_tracker,
       DidSwapBufferCompleteCallback did_swap_buffer_complete_callback);
+
+  SkiaOutputDeviceDawn(
+      scoped_refptr<gpu::SharedContextState> context_state,
+      gfx::SurfaceOrigin origin,
+      gpu::MemoryTracker* memory_tracker,
+      DidSwapBufferCompleteCallback did_swap_buffer_complete_callback,
+      base::PassKey<SkiaOutputDeviceDawn>);
 
   SkiaOutputDeviceDawn(const SkiaOutputDeviceDawn&) = delete;
   SkiaOutputDeviceDawn& operator=(const SkiaOutputDeviceDawn&) = delete;
@@ -62,6 +72,8 @@ class SkiaOutputDeviceDawn : public SkiaOutputDevice {
   void EndPaint() override;
 
  private:
+  bool Initialize(gpu::SurfaceHandle surface_handle);
+
   scoped_refptr<gpu::SharedContextState> context_state_;
   wgpu::Surface surface_;
   wgpu::SwapChain swap_chain_;
