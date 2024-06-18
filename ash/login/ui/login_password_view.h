19eedfe779ee313d47432783290c6943602773c6
Istvan Nagy <iscsi@google.com>
2024-06-18 08:22:06 +0000

    Remove the unused and unnecessary caps lock status variable.
    Bug: b:344520320
    Change-Id: I08e7eb5b7c503714d77a1d19622fe5968593c87c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5621159
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1316305}

--- ash/login/ui/login_password_view.h
+++ ash/login/ui/login_password_view.h
@@ -74,10 +74,6 @@ class ASH_EXPORT LoginPasswordView : public views::View,
     views::View* submit_button() const;
     views::ToggleImageButton* display_password_button() const;
 
-    bool is_capslock_highlight_for_testing() {
-      return view_->is_capslock_higlight_;
-    }
-
    private:
     raw_ptr<LoginPasswordView> view_;
   };
@@ -213,8 +209,6 @@ class ASH_EXPORT LoginPasswordView : public views::View,
   raw_ptr<ArrowButtonView> submit_button_ = nullptr;
   raw_ptr<DisplayPasswordButton> display_password_button_ = nullptr;
   raw_ptr<views::ImageView> capslock_icon_ = nullptr;
-
-  bool is_capslock_higlight_ = false;
 
   base::WeakPtrFactory<LoginPasswordView> weak_ptr_factory_{this};
 };
