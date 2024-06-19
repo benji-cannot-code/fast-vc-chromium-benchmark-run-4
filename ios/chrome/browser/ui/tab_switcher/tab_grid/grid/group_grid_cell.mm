7e0dbf7e4e9380e4d115dab5a106fc529da1a350
Louis Romero <lpromero@google.com>
2024-06-19 11:44:51 +0000

    Move tab_groups from tab_grid/grid to tab_grid/
    `tab_groups` is not just about a grid, so moving it up one level.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE Moving files
    Bug: 329626033
    Change-Id: I477fa055ee9901ce2c0001ccec5e29209eed0bd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639716
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316960}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/group_grid_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/group_grid_cell.mm
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_constants.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_snapshots_view.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_snapshots_view.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/grit/ios_strings.h"
--- ios/chrome/browser/ui/tab_switcher/tab_strip/test/tab_strip_group_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/test/tab_strip_group_egtest.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <XCTest/XCTest.h>
 
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_groups_constants.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_strip/ui/swift_constants_for_objective_c.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
