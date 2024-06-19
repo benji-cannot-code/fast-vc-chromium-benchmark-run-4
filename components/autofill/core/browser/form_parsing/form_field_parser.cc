1a57a8757fbe1cc93d89ea403e137fb0b3b21498
Florian Leimgruber <fleimgruber@google.com>
2024-06-19 14:04:11 +0000

    Pattern provider cleanup: Remove unused parameters
    Since pattern provider is the default, it is no longer necessary to pass
    in a regex + match types to parsing functions. A pattern name suffices.
    This CL removes the unused parameters and updates all callers.
    
    Removing now unused regexes from autofill_regex_constants is deferred to
    a follow-up.
    
    Bug: 40280853
    Change-Id: Ib49feb9efcc66296012218d566101c7bb4d1b5c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5636091
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317009}

--- components/autofill/core/browser/form_parsing/form_field_parser.cc
+++ components/autofill/core/browser/form_parsing/form_field_parser.cc
@@ -420,12 +420,10 @@ bool FormFieldParser::FieldMatchesMatchPatternRef(
 // static
 bool FormFieldParser::ParseField(ParsingContext& context,
                                  AutofillScanner* scanner,
-                                 std::u16string_view pattern,
                                  base::span<const MatchPatternRef> patterns,
                                  raw_ptr<AutofillField>* match,
                                  const char* regex_name) {
-  return ParseFieldSpecifics(context, scanner, pattern, kDefaultMatchParams,
-                             patterns, match, regex_name);
+  return ParseFieldSpecifics(context, scanner, patterns, match, regex_name);
 }
 
 // static
@@ -481,8 +479,6 @@ bool FormFieldParser::ParseFieldSpecificsWithNewPatterns(
 bool FormFieldParser::ParseFieldSpecifics(
     ParsingContext& context,
     AutofillScanner* scanner,
-    std::u16string_view pattern,
-    const MatchParams& match_type,
     base::span<const MatchPatternRef> patterns,
     raw_ptr<AutofillField>* match,
     const char* regex_name,
