b6e8fbb4b6e7c1473c9b8d39f49c0437849bc9d3
Louis Romero <lpromero@google.com>
2024-06-19 10:01:04 +0000

    Move Tab Group grid view controller
    This CL moves the BaseGridViewController sbclass used for Tab Group
    viewing outside of `grid/tab_groups`, which will soon move to the
    `tab_grid` folder.
    
    Bug: 329626033
    Change-Id: I233858ec12980081f8488e6e6aab4cade09c5102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640054
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316929}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.mm
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_grid_view_controller.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.h"
 
 #import "base/apple/foundation_util.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_view_delegate.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_header.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_header.h"
 
 @implementation TabGroupGridViewController {
   // Registered header.
--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_view_controller.mm
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/keyboard/UIKeyCommand+Chrome.h"
 #import "ios/chrome/browser/ui/menu/action_factory.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_constants.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_grid_view_controller.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_group_mutator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_groups_commands.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_groups/tab_groups_constants.h"
