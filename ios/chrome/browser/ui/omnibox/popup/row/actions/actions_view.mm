d4555903d627acd5033e370be61b1bc1b499f39e
Ameur Hosni <ameurhosni@google.com>
2024-06-19 14:32:38 +0000

    [IOS][Omnibox] Update the actions button color
    This CL adjusts the actions buttons color following UX suggestion:
    http://shortn/_qMHWomqxIr
    
    Change-Id: Ic90d88f8ed2068f3b5a498bba2b7235a5fc56a72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639953
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317021}

--- ios/chrome/browser/ui/omnibox/popup/row/actions/actions_view.mm
+++ ios/chrome/browser/ui/omnibox/popup/row/actions/actions_view.mm
@@ -214,7 +214,7 @@ const CGFloat kLeadingPadding = 61;
     UIBackgroundConfiguration* backgroundConfig =
         [UIBackgroundConfiguration clearConfiguration];
     backgroundConfig.backgroundColor =
-        [UIColor colorNamed:kFaviconBackgroundColor];
+        [UIColor colorNamed:kTertiaryBackgroundColor];
     configuration.background = backgroundConfig;
   }
 
