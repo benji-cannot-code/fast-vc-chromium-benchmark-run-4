a396aefd491dd223e1c84ca63bdf08d2c0b42b2d
Morten Stenshorne <mstensho@chromium.org>
2024-06-19 11:46:08 +0000

    Page shrinking should also shrink the margins.
    If a page is larger than what fits on paper, it will be scaled down to
    fit. This also includes margins, which means that the size available to
    page margin boxes is affected.
    
    Also add a test for a page size smaller than paper, to make sure that
    page margins don't use the extra space inserted for page border box
    centering, but instead honors @page margin size.
    
    Bug: 40341678
    Change-Id: Iea85c2a8ba2ad85e62f11af10adf77dcf51a38ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5642513
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316961}

--- third_party/blink/renderer/core/layout/geometry/box_strut.cc
+++ third_party/blink/renderer/core/layout/geometry/box_strut.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/geometry/box_strut.h"
 
+#include "third_party/blink/renderer/core/layout/geometry/logical_rect.h"
 #include "third_party/blink/renderer/core/layout/geometry/physical_rect.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
@@ -28,6 +29,20 @@ BoxStrut::BoxStrut(const LineBoxStrut& line_relative, bool is_flipped_lines) {
     *this = {line_relative.inline_start, line_relative.inline_end,
              line_relative.line_under, line_relative.line_over};
   }
+}
+
+BoxStrut::BoxStrut(const LogicalSize& outer_size, const LogicalRect& inner_rect)
+    : inline_start(inner_rect.offset.inline_offset),
+      inline_end(outer_size.inline_size - inner_rect.InlineEndOffset()),
+      block_start(inner_rect.offset.block_offset),
+      block_end(outer_size.block_size - inner_rect.BlockEndOffset()) {}
+
+BoxStrut& BoxStrut::Intersect(const BoxStrut& other) {
+  inline_start = std::min(inline_start, other.inline_start);
+  inline_end = std::min(inline_end, other.inline_end);
+  block_start = std::min(block_start, other.block_start);
+  block_end = std::min(block_end, other.block_end);
+  return *this;
 }
 
 LineBoxStrut::LineBoxStrut(const BoxStrut& flow_relative,
