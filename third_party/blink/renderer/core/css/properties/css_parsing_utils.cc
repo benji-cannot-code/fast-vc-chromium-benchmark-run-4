eeb381f92273042af5007305bedca168f85bcd37
Anders Hartvoll Ruud <andruud@chromium.org>
2024-06-19 09:04:10 +0000

    Treat '!' as EOF in in ConsumeGridTemplateRowsAndAreasAndColumns
    Our property parser should generally stop at first unknown token
    to allow the caller to handle any trailing !important.
    CL:5509594 (and/or related CLs) did however introduce a few exceptions
    to this for practical purposes. These exceptions need to handle
    the sudden appearance of '!' following an otherwise valid value,
    but we forgot do that in one of the branches in ConsumeGridTemplate-
    RowsAndAreasAndColumns.
    
    Fixed: 347193631
    Change-Id: Ia49740296139b4e1be1f1ee1f22c2fb9616433af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630448
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1316918}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -6997,7 +6997,8 @@ bool ConsumeGridTemplateRowsAndAreasAndColumns(
     }
     template_columns = ConsumeGridTrackList(
         stream, context, TrackListType::kGridTemplateNoRepeat);
-    if (!template_columns || !stream.AtEnd()) {
+    if (!template_columns ||
+        !(stream.AtEnd() || stream.Peek().Delimiter() == '!')) {
       return false;
     }
   } else {
