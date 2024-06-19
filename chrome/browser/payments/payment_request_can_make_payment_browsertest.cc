616e217dc025806e9509284bd8e2207cc8979ece
Stephen McGruer <smcgruer@chromium.org>
2024-06-19 19:02:41 +0000

    [PaymentRequest] Add tests for kCanMakePaymentEnabled histograms
    These histograms were added in a previous CL,
    https://chromium-review.googlesource.com/c/chromium/src/+/5573805, but
    tests were not added at this time. Add some basic tests for the
    non-trivial histograms that are measured there.
    
    Bug: 343011427
    Change-Id: I62e5ba79e8514d92a0c5cccbca1f95c39667830c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643230
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317124}

--- chrome/browser/payments/payment_request_can_make_payment_browsertest.cc
+++ chrome/browser/payments/payment_request_can_make_payment_browsertest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/payments/payment_request_platform_browsertest_base.h"
 #include "content/public/test/browser_test.h"
@@ -51,13 +52,21 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
 // PaymentRequest.hasEnrolledInstrument() should return true."
 IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
                        AppRespondsTrueToCanMakePaymentEvent) {
+  base::HistogramTester histogram_tester;
   std::string method;
   InstallPaymentApp("a.com", "/can_make_payment_true_responder.js", &method);
 
   NavigateTo("b.com", "/payment_request_can_make_payment_query_test.html");
 
   ExpectCanMakePayment(true, method);
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.CanMakePayment.CallAllowedByPref", /*sample=*/1,
+      /*expected_bucket_count=*/1);
+
   ExpectHasEnrolledInstrument(true, method);
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.HasEnrolledInstrument.CallAllowedByPref", /*sample=*/1,
+      /*expected_bucket_count=*/1);
 }
 
 // A user has installed a payment app that responds "true" to the
@@ -105,10 +114,11 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
   ExpectHasEnrolledInstrument(false, method);
 }
 
-// Test the case where canMakePayment/hasEnrolledInstrument would return true,
-// but the user has disabled those APIs in settings.
+// Test the case where canMakePayment would return true, but the user has
+// disabled the API in settings.
 IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
                        CanMakePayment_SupportedButDisabled) {
+  base::HistogramTester histogram_tester;
   test_controller()->SetCanMakePaymentEnabledPref(false);
 
   std::string method;
@@ -117,7 +127,27 @@ IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
   NavigateTo("b.com", "/payment_request_can_make_payment_query_test.html");
 
   ExpectCanMakePayment(false, method);
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.CanMakePayment.CallAllowedByPref", /*sample=*/0,
+      /*expected_bucket_count=*/1);
+}
+
+// Test the case where hasEnrolledInstrument would return true, but the user has
+// disabled the API in settings.
+IN_PROC_BROWSER_TEST_F(PaymentRequestCanMakePaymentQueryTest,
+                       HasEnrolledInstrument_SupportedButDisabled) {
+  base::HistogramTester histogram_tester;
+  test_controller()->SetCanMakePaymentEnabledPref(false);
+
+  std::string method;
+  InstallPaymentApp("a.com", "/can_make_payment_true_responder.js", &method);
+
+  NavigateTo("b.com", "/payment_request_can_make_payment_query_test.html");
+
   ExpectHasEnrolledInstrument(false, method);
+  histogram_tester.ExpectUniqueSample(
+      "PaymentRequest.HasEnrolledInstrument.CallAllowedByPref", /*sample=*/0,
+      /*expected_bucket_count=*/1);
 }
 
 // Pages without a valid SSL certificate always get "false" from
