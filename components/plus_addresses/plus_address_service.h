69be0b9fde2e27fbfca84c96199bbdc8dd979573
Norge Vizcay <vizcay@google.com>
2024-06-20 13:32:12 +0000

    Add plus profiles cache.
    The CL adds a PlusAddressCache class that handles the in-memory copy of
    PlusProfiles. This clears the nuances of always modifying the two
    flat sets previously kept by the plus address service.
    
    Change-Id: Ie99c2b3f1ab841b87a60b375362c61dcc56322bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643473
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317329}

--- components/plus_addresses/plus_address_service.h
+++ components/plus_addresses/plus_address_service.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
-#include "base/containers/flat_set.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/observer_list.h"
 #include "base/observer_list_types.h"
@@ -20,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/plus_addresses/affiliations/plus_address_affiliation_match_helper.h"
 #include "components/plus_addresses/metrics/plus_address_submission_logger.h"
+#include "components/plus_addresses/plus_address_cache.h"
 #include "components/plus_addresses/plus_address_types.h"
 #include "components/plus_addresses/webdata/plus_address_webdata_service.h"
 #include "components/signin/public/identity_manager/account_info.h"
@@ -216,15 +216,8 @@ class PlusAddressService : public KeyedService,
       GetSuggestionsCallback callback,
       std::vector<PlusProfile> affiliated_profiles);
 
-  // The user's existing set of `PlusProfile`s, ordered by facet. Since only a
-  // single address per facet is supported, this can be used as the comparator.
-  base::flat_set<PlusProfile, PlusProfileFacetComparator> plus_profiles_
-      GUARDED_BY_CONTEXT(sequence_checker_);
-
-  // Used to drive the `IsPlusAddress` function, and derived from the values of
-  // `plus_profiles_`.
-  base::flat_set<std::string> plus_addresses_
-      GUARDED_BY_CONTEXT(sequence_checker_);
+  // Plus profiles in-memory cache.
+  PlusAddressCache plus_address_cache_ GUARDED_BY_CONTEXT(sequence_checker_);
 
   const raw_ref<signin::IdentityManager> identity_manager_;
 
