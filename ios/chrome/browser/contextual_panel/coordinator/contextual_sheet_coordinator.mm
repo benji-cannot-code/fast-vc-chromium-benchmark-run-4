9bbaef6a4c5e5b90cc3bba418c1ea1b8186d5fbd
Robbie Gibson <rkgibson@google.com>
2024-06-20 16:12:46 +0000

    [iOS] Add Contextual Panel animations for appearance and user drag
    This CL adds code to animate the appearance of the Contextual Panel
    and when the user lifts their finger.
    
    Fixed: 343728030, 343728212
    Change-Id: I9a852c97599f9516d467a27e9207fd24ab3f2b84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639821
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317415}

--- ios/chrome/browser/contextual_panel/coordinator/contextual_sheet_coordinator.mm
+++ ios/chrome/browser/contextual_panel/coordinator/contextual_sheet_coordinator.mm
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       initWithBaseViewController:_viewController
                          browser:self.browser];
   [_panelContentCoordinator start];
+
+  [_viewController animateAppearance];
 }
 
 - (void)stop {
--- ios/chrome/browser/contextual_panel/ui/contextual_sheet_view_controller.h
+++ ios/chrome/browser/contextual_panel/ui/contextual_sheet_view_controller.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Command handler.
 @property(nonatomic, weak) id<ContextualSheetCommands> contextualSheetHandler;
 
+// Animates the appearance of the sheet after the controller has been added to
+// its parent.
+- (void)animateAppearance;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_UI_CONTEXTUAL_SHEET_VIEW_CONTROLLER_H_
