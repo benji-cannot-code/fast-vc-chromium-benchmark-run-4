1de7c32dea5b8bda991f7f5d0934726208873eac
Tony Herre <toprice@chromium.org>
2024-06-20 13:38:20 +0000

    Implement RTCRtpTransport.readReceicedAcks()
    Add implementation for reading RTP packet feedback messages in batches,
    as part of the RtpTransport API being prototyped.
    
    Follows the shape of https://github.com/w3c/webrtc-rtptransport/pull/42
    Guarded by Blink Feature RTCRtpTransport. Also requires --force-fieldtrials=WebRTC-Bwe-InjectedCongestionController/Enabled
    
    Bug: 345101934
    Change-Id: I33ad3a4a2363e67f3edbd0949a3835a6cb6d430b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5607744
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Commit-Queue: Tony Herre <toprice@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317330}

--- third_party/blink/renderer/modules/peerconnection/adapters/web_rtc_cross_thread_copier.h
+++ third_party/blink/renderer/modules/peerconnection/adapters/web_rtc_cross_thread_copier.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 #include "third_party/webrtc/api/rtc_error.h"
 #include "third_party/webrtc/api/scoped_refptr.h"
+#include "third_party/webrtc/api/transport/network_types.h"
 
 namespace cricket {
 class Candidate;
@@ -151,6 +152,12 @@ struct CrossThreadCopier<rtc::scoped_refptr<webrtc::VideoTrackInterface>>
 template <>
 struct CrossThreadCopier<webrtc::DataBuffer>
     : public CrossThreadCopierPassThrough<webrtc::DataBuffer> {
+  STATIC_ONLY(CrossThreadCopier);
+};
+
+template <>
+struct CrossThreadCopier<webrtc::TransportPacketsFeedback>
+    : public CrossThreadCopierPassThrough<webrtc::TransportPacketsFeedback> {
   STATIC_ONLY(CrossThreadCopier);
 };
 
