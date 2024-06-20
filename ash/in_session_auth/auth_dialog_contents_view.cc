ead6514b6fdec6a2c2eb6c03f699f12a880ede07
Renato Silva <rrsilva@google.com>
2024-06-20 10:38:30 +0000

    [High5] Login/Lock screen remove unused method
    A long time ago, pressing enter on an empty password/pin would trigger
    a SmartUnlock attempt. The UI has changed over time and this behavior
    is no longer used. Remove the method and calls that were necessary.
    
    Bug: b/348326316
    Change-Id: I086fee9f8357339629187dc324a56a014c7fbb51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639962
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317278}

--- ash/in_session_auth/auth_dialog_contents_view.cc
+++ ash/in_session_auth/auth_dialog_contents_view.cc
@@ -519,7 +519,6 @@ void AuthDialogContentsView::AddPinTextInputView() {
   pin_text_input_view_->SetPaintToLayer();
   pin_text_input_view_->layer()->SetFillsBoundsOpaquely(false);
   pin_text_input_view_->SetDisplayPasswordButtonVisible(true);
-  pin_text_input_view_->SetEnabledOnEmptyPassword(false);
   pin_text_input_view_->SetFocusEnabledForTextfield(true);
 
   pin_text_input_view_->SetPlaceholderText(
@@ -533,7 +532,6 @@ void AuthDialogContentsView::AddPasswordView() {
   password_view_->SetPaintToLayer();
   password_view_->layer()->SetFillsBoundsOpaquely(false);
   password_view_->SetDisplayPasswordButtonVisible(true);
-  password_view_->SetEnabledOnEmptyPassword(false);
   password_view_->SetFocusEnabledForTextfield(true);
 
   password_view_->SetPlaceholderText(
