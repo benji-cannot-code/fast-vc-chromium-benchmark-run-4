c0b857958007d27a323196852f655707f8e45589
Menghan Yang <myuu@google.com>
2024-06-20 14:00:55 +0000

    [UNOp2][Android] Remove Parcelable from AccountPickerBottomSheetStrings
    AccountPickerBottomSheetStrings implemented Parcelable previously to
    ease its use as activities' Intent extra.
    
    However, ClassCastException happened when it was casted after
    retrieval from SigninAndHistoryOptInActivity's Intent. A speculative
    fix has been applied in https://crrev.com/c/5630955, but the fix
    verification would be tedious.
    
    Given the class is trivial (holding 3 integers only), this CL removes
    the Parcelable implementation from AccountPickerBottomSheetStrings and
    makes definitive the workaround of serializing its individual fields
    as a clean-up.
    
    Bug: 346709145
    Change-Id: Iffcd70126a27c47bc65ff2a55819e76541419bb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639711
    Auto-Submit: Menghan Yang <myuu@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317343}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetStrings.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetStrings.java
@@ -4,29 +4,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.ui.signin.account_picker;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import androidx.annotation.StringRes;
 
 /* Class containing string resource ids for the sign-in account picker bottom sheet. */
-public final class AccountPickerBottomSheetStrings implements Parcelable {
+public final class AccountPickerBottomSheetStrings {
     public final @StringRes int titleStringId;
     public final @StringRes int subtitleStringId;
     public final @StringRes int dismissButtonStringId;
-
-    public static final Parcelable.Creator<AccountPickerBottomSheetStrings> CREATOR =
-            new Parcelable.Creator<AccountPickerBottomSheetStrings>() {
-                @Override
-                public AccountPickerBottomSheetStrings createFromParcel(Parcel in) {
-                    return new AccountPickerBottomSheetStrings(in);
-                }
-
-                @Override
-                public AccountPickerBottomSheetStrings[] newArray(int size) {
-                    return new AccountPickerBottomSheetStrings[size];
-                }
-            };
 
     // Private constructor to enforce the use of the Builder.
     private AccountPickerBottomSheetStrings(
@@ -36,26 +20,6 @@ public final class AccountPickerBottomSheetStrings implements Parcelable {
         this.titleStringId = titleStringId;
         this.subtitleStringId = subtitleStringId;
         this.dismissButtonStringId = dismissButtonStringId;
-    }
-
-    private AccountPickerBottomSheetStrings(Parcel in) {
-        titleStringId = in.readInt();
-        subtitleStringId = in.readInt();
-        dismissButtonStringId = in.readInt();
-    }
-
-    /** Implements {@link Parcelable} */
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    /** Implements {@link Parcelable} */
-    @Override
-    public void writeToParcel(Parcel out, int flags) {
-        out.writeInt(titleStringId);
-        out.writeInt(subtitleStringId);
-        out.writeInt(dismissButtonStringId);
     }
 
     /**
