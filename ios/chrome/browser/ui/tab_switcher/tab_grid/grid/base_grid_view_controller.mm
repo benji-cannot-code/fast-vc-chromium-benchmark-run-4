84da2f544d347f0c319fcb60c5fa42ad3c0a9e2f
Aliona DANGLA <alionadangla@google.com>
2024-06-20 15:10:03 +0000

    [iOS] Reconfigure all the items when populate grid
    Tab group cell was not updated, doing that ensure that the cell reflect
    the tab group.
    
    Fixed: 348364683
    Change-Id: I32e27bd39069dbda6ecb8749fcc8ab2b4b7feaa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630406
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317380}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.mm
@@ -1100,15 +1100,13 @@ NSString* GroupGridCellAccessibilityIdentifier(NSUInteger index) {
     [snapshot appendItemsWithIdentifiers:@[ itemIdentifier ]];
   }
 
-  if (self.mode == TabGridModeSelection || self.mode == TabGridModeSearch) {
-    [self.diffableDataSource applySnapshotUsingReloadData:snapshot];
-  } else {
-    [self.diffableDataSource applySnapshot:snapshot
-                      animatingDifferences:YES
-                                completion:nil];
-  }
+  [snapshot reconfigureItemsWithIdentifiers:items];
+  [self.diffableDataSource applySnapshot:snapshot
+                    animatingDifferences:YES
+                              completion:nil];
 
   [self updateSelectedCollectionViewItemRingAndBringIntoView:NO];
+  [self updateVisibleCellIdentifiers];
 
   if ([self shouldShowEmptyState]) {
     [self animateEmptyStateIn];
