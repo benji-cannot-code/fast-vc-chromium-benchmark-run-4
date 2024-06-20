c008a37a5189de79049844d241a25302fb216e5d
Anupam Snigdha <snianu@microsoft.com>
2024-06-20 23:02:51 +0000

    [Clipboard] Write `image/svg+xml` content in UTF-8 format on Windows.
    Popular apps on Windows[1] use UTF-8 format to read/write SVG images.
    Even with a BOM char at the beginning, UTF-16 is not parsed correctly
    so the SVG image never gets pasted properly.
    
    Switch to UTF-8 on Windows while writing `image/svg+xml` format.
    This is similar to HTML format where we use UTF-8 on Windows.
    
    PSA: https://groups.google.com/a/chromium.org/g/blink-dev/c/bsjnNMEslYU/m/pjUxG4dhAAAJ?utm_medium=email
    
    [1] https://docs.google.com/document/d/1ULlihA0FOJOqcyD9MgzLZrAbk0uTQPJqDPuPJ2aiuS4/edit?usp=sharing
    
    Bug: 338250106
    
    Change-Id: I5fb189b235af3deae7d72628ed5904eca3304609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5506957
    Commit-Queue: Anupam Snigdha <snianu@microsoft.com>
    Reviewed-by: Christine Hollingsworth <christinesm@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1317665}

--- ui/base/clipboard/clipboard_win.cc
+++ ui/base/clipboard/clipboard_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
+#include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/lazy_instance.h"
@@ -47,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/clipboard/clipboard_util_win.h"
 #include "ui/base/clipboard/custom_data_helper.h"
 #include "ui/base/data_transfer_policy/data_transfer_endpoint.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/geometry/size.h"
@@ -501,9 +503,12 @@ void ClipboardWin::ReadSvg(ClipboardBuffer buffer,
 
   std::string data;
   ReadData(ClipboardFormatType::SvgType(), data_dst, &data);
-  result->assign(reinterpret_cast<const char16_t*>(data.data()),
-                 data.size() / sizeof(char16_t));
-
+  if (base::FeatureList::IsEnabled(features::kUseUtf8EncodingForSvgImage)) {
+    *result = base::UTF8ToUTF16(data);
+  } else {
+    result->assign(reinterpret_cast<const char16_t*>(data.data()),
+                   data.size() / sizeof(char16_t));
+  }
   TrimAfterNull(result);
 }
 
@@ -752,7 +757,12 @@ void ClipboardWin::WriteHTML(std::string_view markup,
 }
 
 void ClipboardWin::WriteSvg(std::string_view markup) {
-  HGLOBAL glob = CreateGlobalData(base::UTF8ToUTF16(markup));
+  HGLOBAL glob;
+  if (base::FeatureList::IsEnabled(features::kUseUtf8EncodingForSvgImage)) {
+    glob = CreateGlobalData(std::string(markup));
+  } else {
+    glob = CreateGlobalData(base::UTF8ToUTF16(markup));
+  }
 
   WriteToClipboard(ClipboardFormatType::SvgType(), glob);
 }
