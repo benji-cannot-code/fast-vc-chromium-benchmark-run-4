3e656623839cf65822ca1cd72ee8080e573b9de7
Marijn Kruisselbrink <mek@chromium.org>
2024-06-20 17:42:36 +0000

    [PWAs] Split WebAppAutoLoginUtil off into its own file.
    This is part 3/x of a series of CLs to clean up/organize the mac
    PWA OS integration code. Having many different classes all
    bunched together in web_app_shortcut_mac.mm makes it difficult to
    find code, so splitting up individual classes into their own files
    seems like a good idea.
    
    In follow up CLs more code will move to the os_integration/mac
    directory, ultimately making that its own build target as well
    (although for now files are kept in their original build files).
    
    Bug: 347167972
    Change-Id: I8d55dc4caaf4cdb7ecf1aa53b414a745633783fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631402
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317484}

--- chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
+++ chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/shortcuts/platform_util_mac.h"
 #include "chrome/browser/web_applications/os_integration/mac/icns_encoder.h"
+#include "chrome/browser/web_applications/os_integration/mac/web_app_auto_login_util.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_test_override.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
@@ -237,8 +238,6 @@ BASE_FEATURE(kWebAppMaskableIconsOnMac,
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 namespace {
-
-WebAppAutoLoginUtil* g_auto_login_util_for_testing = nullptr;
 
 // Result of creating app shortcut.
 // These values are persisted to logs. Entries should not be renumbered and
@@ -1163,47 +1162,6 @@ base::FilePath GetChromeAppsFolder() {
     return path;
 
   return path.Append(GetLocalizableAppShortcutsSubdirName());
-}
-
-// static
-WebAppAutoLoginUtil* WebAppAutoLoginUtil::GetInstance() {
-  if (g_auto_login_util_for_testing)
-    return g_auto_login_util_for_testing;
-
-  static base::NoDestructor<WebAppAutoLoginUtil> instance;
-  return instance.get();
-}
-
-// static
-void WebAppAutoLoginUtil::SetInstanceForTesting(
-    WebAppAutoLoginUtil* auto_login_util) {
-  g_auto_login_util_for_testing = auto_login_util;
-}
-
-void WebAppAutoLoginUtil::AddToLoginItems(const base::FilePath& app_bundle_path,
-                                          bool hide_on_startup) {
-  scoped_refptr<OsIntegrationTestOverride> os_override =
-      OsIntegrationTestOverride::Get();
-  if (os_override) {
-    CHECK_IS_TEST();
-    os_override->EnableOrDisablePathOnLogin(app_bundle_path,
-                                            /*enable_on_login=*/true);
-  } else {
-    base::mac::AddToLoginItems(app_bundle_path, hide_on_startup);
-  }
-}
-
-void WebAppAutoLoginUtil::RemoveFromLoginItems(
-    const base::FilePath& app_bundle_path) {
-  scoped_refptr<OsIntegrationTestOverride> os_override =
-      OsIntegrationTestOverride::Get();
-  if (os_override) {
-    CHECK_IS_TEST();
-    os_override->EnableOrDisablePathOnLogin(app_bundle_path,
-                                            /*enable_on_login=*/false);
-  } else {
-    base::mac::RemoveFromLoginItems(app_bundle_path);
-  }
 }
 
 WebAppShortcutCreator::WebAppShortcutCreator(const base::FilePath& app_data_dir,
--- chrome/browser/web_applications/os_integration/web_app_shortcut_mac_unittest.mm
+++ chrome/browser/web_applications/os_integration/web_app_shortcut_mac_unittest.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_path_override.h"
+#include "chrome/browser/web_applications/os_integration/mac/web_app_auto_login_util.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
