0552ea8181938f5e4c760cf715604c15d5bc7124
Greg Thompson <grt@chromium.org>
2024-06-20 21:52:27 +0000

    [mojo] Reduce overhead in SharedMemoryVersionController::GetSharedVersion
    This function was mapping/unmapping a read-only view of the memory owned
    by the controller on every call. Replace this with use of the existing
    writeable mapping.
    
    In addition:
    
    - Initialize class members in initializer lists and make them const.
    - CHECK validity of regions/mappings only in ctors, since they're const.
    - const-ify various methods.
    - SharedMemoryVersionClient no longer holds on to the
      ReadOnlySharedMemoryRegion received at construction, as it only needs
      the mapping created from it.
    - Replace use of cfoo headers with foo.h since we use uint64_t rather
      than std::uint64_t.
    
    Bug: 1500339
    Change-Id: I1d3b76ddd08f4a36d3de410f43a4e54749f1343f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645222
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317635}

--- mojo/public/cpp/base/shared_memory_version.h
+++ mojo/public/cpp/base/shared_memory_version.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MOJO_PUBLIC_CPP_BASE_SHARED_MEMORY_VERSION_H_
 #define MOJO_PUBLIC_CPP_BASE_SHARED_MEMORY_VERSION_H_
 
+#include <stdint.h>
+
 #include <atomic>
-#include <cstdint>
 
 #include "base/component_export.h"
-
 #include "base/memory/read_only_shared_memory_region.h"
 
 namespace mojo {
@@ -97,9 +97,9 @@ class COMPONENT_EXPORT(MOJO_BASE) SharedMemoryVersionController {
 
   // Get a shared memory region to be sent to a different process. It will be
   // used to instantiate a SharedMemoryVersionClient.
-  base::ReadOnlySharedMemoryRegion GetSharedMemoryRegion();
+  base::ReadOnlySharedMemoryRegion GetSharedMemoryRegion() const;
 
-  VersionType GetSharedVersion();
+  VersionType GetSharedVersion() const;
 
   // Increment shared version. This is not expected to cause a wrap of the value
   // during normal operation. This invariant is guaranteed with a CHECK.
@@ -111,7 +111,7 @@ class COMPONENT_EXPORT(MOJO_BASE) SharedMemoryVersionController {
   void SetVersion(VersionType version);
 
  private:
-  base::MappedReadOnlyRegion mapped_region_;
+  const base::MappedReadOnlyRegion mapped_region_;
 };
 
 // Used to keep track of a remote version number and compare it to a
@@ -128,16 +128,17 @@ class COMPONENT_EXPORT(MOJO_BASE) SharedMemoryVersionClient {
       delete;
 
   // These functions can be used to form statements such as:
-  // "Skip the IPC if `SharedVersionIsLessThan()` returns true. "
+  // "Skip the IPC if `SharedVersionIsLessThan()` returns true."
   // The functions err on the side of caution and return true if the comparison
   // is impossible since issuing an IPC should always be an option.
-  bool SharedVersionIsLessThan(VersionType version);
-  bool SharedVersionIsGreaterThan(VersionType version);
+  bool SharedVersionIsLessThan(VersionType version) const;
+  bool SharedVersionIsGreaterThan(VersionType version) const;
 
  private:
-  VersionType GetSharedVersion();
-  base::ReadOnlySharedMemoryRegion shared_region_;
-  base::ReadOnlySharedMemoryMapping read_only_mapping_;
+  // Returns the current value in shared memory.
+  VersionType GetSharedVersion() const;
+
+  const base::ReadOnlySharedMemoryMapping read_only_mapping_;
 };
 
 }  // namespace mojo
