624fc5a8c272a734153dd7d40d393fe47ec63cad
Shu Yang <shuyng@google.com>
2024-06-20 22:37:26 +0000

    Remove param forcedByUser from TabUtils#switchUserAgent
    forcedByUser in TabUtils#switchUserAgent is always false
    
    Bug: 40891239
    Change-Id: I96b999b323baef61d540f5841df93b360b100a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643887
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317647}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -2762,7 +2762,6 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
             TabUtils.switchUserAgent(
                     currentTab,
                     usingDesktopUserAgent,
-                    /* forcedByUser= */ false,
                     UseDesktopUserAgentCaller.ON_MENU_OR_KEYBOARD_ACTION);
             // TODO(crbug.com/40917998): Remove this IPH when the usage is low.
             RequestDesktopUtils.maybeShowUserEducationPromptForAppMenuSelection(
