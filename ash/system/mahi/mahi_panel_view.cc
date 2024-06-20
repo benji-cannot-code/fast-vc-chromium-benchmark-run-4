7694b649d9a149cffabbf600c7e311ec3954c837
Kevin Radtke <kradtke@google.com>
2024-06-20 23:34:48 +0000

    mahi: Add mahi panel strings to ash_strings
    Bug: b:331127382
    Change-Id: If4af67d2ebfb812de357dc64403a9531c0615e34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5641593
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Kevin Radtke <kradtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317687}

--- ash/system/mahi/mahi_panel_view.cc
+++ ash/system/mahi/mahi_panel_view.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
-#include "build/branding_buildflags.h"
 #include "chromeos/components/magic_boost/public/cpp/views/experiment_badge.h"
 #include "chromeos/components/mahi/public/cpp/mahi_manager.h"
 #include "components/vector_icons/vector_icons.h"
@@ -71,10 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 #include "url/gurl.h"
-
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-#include "chromeos/ash/resources/internal/strings/grit/ash_internal_strings.h"
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
 namespace ash {
 
@@ -145,23 +140,6 @@ void InstallTextfieldFocusRing(views::View* question_textfield,
       question_textfield,
       gfx::Insets::TLBR(0, focus_ring_left_inset, 0, focus_ring_right_inset),
       kAskQuestionContainerCornerRadius);
-}
-
-// TODO(b/331127382): Finalize Mahi panel strings.
-std::u16string GetMahiPanelTitle() {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return l10n_util::GetStringUTF16(IDS_MAHI_PANEL_TITLE);
-#else
-  return l10n_util::GetStringUTF16(IDS_ASH_MAHI_PANEL_TITLE);
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
-}
-
-std::u16string GetMahiPanelDisclaimer() {
-#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
-  return l10n_util::GetStringUTF16(IDS_MAHI_PANEL_DISCLAIMER);
-#else
-  return l10n_util::GetStringUTF16(IDS_ASH_MAHI_PANEL_DISCLAIMER);
-#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
 
 // FeedbackButton --------------------------------------------------------------
@@ -714,7 +692,8 @@ MahiPanelView::MahiPanelView(MahiUiController* ui_controller)
           .SetMainAxisAlignment(views::BoxLayout::MainAxisAlignment::kCenter)
           .SetBetweenChildSpacing(kFooterSpacing)
           .AddChildren(
-              views::Builder<views::Label>().SetText(GetMahiPanelDisclaimer()),
+              views::Builder<views::Label>().SetText(
+                  l10n_util::GetStringUTF16(IDS_ASH_MAHI_PANEL_DISCLAIMER)),
               views::Builder<views::Link>()
                   .SetText(l10n_util::GetStringUTF16(
                       IDS_ASH_MAHI_LEARN_MORE_LINK_LABEL_TEXT))
@@ -788,9 +767,7 @@ std::unique_ptr<views::View> MahiPanelView::CreateHeaderRow() {
               .SetID(mahi_constants::ViewId::kGoToSummaryOutlinesButton),
           // The Panel's title label
           views::Builder<views::Label>()
-              // TODO(b/319264190): Replace the string used here with the
-              // correct string ID.
-              .SetText(GetMahiPanelTitle())
+              .SetText(l10n_util::GetStringUTF16(IDS_ASH_MAHI_PANEL_TITLE))
               .SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_LEFT)
               .SetFontList(TypographyProvider::Get()->ResolveTypographyToken(
                   TypographyToken::kCrosTitle1))
@@ -806,9 +783,9 @@ std::unique_ptr<views::View> MahiPanelView::CreateHeaderRow() {
                   .SetVectorIcon(&kMediumOrLargeCloseButtonIcon)
                   // TODO(b/319264190): Replace the string used here with
                   // the correct string ID.
+                  .SetAccessibleName(u"Close button")
                   .Build())
               .SetID(mahi_constants::ViewId::kCloseButton)
-              .SetAccessibleName(u"Close button")
               .SetCallback(
                   base::BindRepeating(&MahiPanelView::OnCloseButtonPressed,
                                       weak_ptr_factory_.GetWeakPtr()))
