51e1cb49e0fec55f0681e8156b00e19700694f66
Marijn Kruisselbrink <mek@chromium.org>
2024-06-20 17:42:14 +0000

    [PWAs] Move IcnsEncoder to c/b/wa/os_integration/mac.
    This is part 2/x of a series of CLs to clean up/organize the mac
    PWA OS integration code. Since IcnsEncoder is mac-only functionality
    it makes sense to be moved to the "mac" subdirectory.
    
    In follow up CLs more code will move to the os_integration/mac
    directory, ultimately making that its own build target as well
    (although for now files are kept in their original build files).
    
    Bug: 347167972
    Change-Id: I78aa0b074df0f06648aff402f96ea7b23db7d2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631401
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317483}

--- chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
+++ chrome/browser/web_applications/os_integration/web_app_shortcut_mac.mm
@@ -55,7 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/mac/dock.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/shortcuts/platform_util_mac.h"
-#include "chrome/browser/web_applications/os_integration/icns_encoder.h"
+#include "chrome/browser/web_applications/os_integration/mac/icns_encoder.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_test_override.h"
 #include "chrome/browser/web_applications/os_integration/web_app_shortcut.h"
 #include "chrome/browser/web_applications/web_app_constants.h"
