af8c670e67d37ba2d60d9042b2fe9672af1994fd
Erin Stewart <erinstewart@google.com>
2024-06-20 16:34:38 +0000

    Revert "Extending GetCookiesString() and GetAllForUrl() with the SourceLocation"
    This reverts commit 9b9d8b0239b2e0102631c1dc482082b30de86762.
    
    Reason for revert: CaptureSourceLocation() reads the entire stack trace, which is very costly and was causing performance regressions
    
    Bug: 346617710, b/341594082
    
    Original change's description:
    > Extending GetCookiesString() and GetAllForUrl() with the SourceLocation
    >
    > The SourceLocation will be used to report the filename, line number, and
    > column number if a cookie access caused an error that came from script
    >
    > Bug: b/341594082
    > Change-Id: I5b2ede043ab61576a29bf57a63db553569afa0f0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590165
    > Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    > Auto-Submit: Erin Stewart <erinstewart@google.com>
    > Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    > Reviewed-by: Steven Bingler <bingler@chromium.org>
    > Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    > Commit-Queue: Erin Stewart <erinstewart@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1313854}
    
    Change-Id: I801a3f1af05108ba1ff8f610020cefca7d41a349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631530
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Erin Stewart <erinstewart@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317433}

--- services/network/restricted_cookie_manager.cc
+++ services/network/restricted_cookie_manager.cc
@@ -522,7 +522,6 @@ void RestrictedCookieManager::GetAllForUrl(
     mojom::CookieManagerGetOptionsPtr options,
     bool is_ad_tagged,
     bool force_disable_third_party_cookies,
-    mojom::SourceLocationPtr source_location,
     GetAllForUrlCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
@@ -549,8 +548,7 @@ void RestrictedCookieManager::GetAllForUrl(
           is_ad_tagged,
           GetCookieSettingOverrides(has_storage_access, is_ad_tagged,
                                     force_disable_third_party_cookies),
-          net_options, std::move(options), std::move(source_location),
-          std::move(callback)));
+          net_options, std::move(options), std::move(callback)));
 }
 
 void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
@@ -562,7 +560,6 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
     const net::CookieSettingOverrides& cookie_setting_overrides,
     const net::CookieOptions& net_options,
     mojom::CookieManagerGetOptionsPtr options,
-    mojom::SourceLocationPtr source_location,
     GetAllForUrlCallback callback,
     const net::CookieAccessResultList& cookie_list,
     const net::CookieAccessResultList& excluded_list) {
@@ -615,7 +612,7 @@ void RestrictedCookieManager::CookieListToGetAllForUrlCallback(
           isolated_top_frame_origin, site_for_cookies,
           std::move(on_cookies_accessed_result),
           /*devtools_request_id=*/std::nullopt, /*count=*/1, is_ad_tagged,
-          cookie_setting_overrides, std::move(source_location)));
+          cookie_setting_overrides, /*source_location=*/nullptr));
     }
   };
 
@@ -959,7 +956,6 @@ void RestrictedCookieManager::GetCookiesString(
     bool get_version_shared_memory,
     bool is_ad_tagged,
     bool force_disable_third_party_cookies,
-    mojom::SourceLocationPtr source_location,
     GetCookiesStringCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Checks done by GetAllForUrl
@@ -1006,7 +1002,7 @@ void RestrictedCookieManager::GetCookiesString(
   match_options->match_type = mojom::CookieMatchType::STARTS_WITH;
   GetAllForUrl(url, site_for_cookies, top_frame_origin, has_storage_access,
                std::move(match_options), is_ad_tagged,
-               force_disable_third_party_cookies, std::move(source_location),
+               force_disable_third_party_cookies,
                base::BindOnce([](const std::vector<net::CookieWithAccessResult>&
                                      cookies) {
                  return net::CanonicalCookie::BuildCookieLine(cookies);
--- services/network/restricted_cookie_manager.h
+++ services/network/restricted_cookie_manager.h
@@ -123,7 +123,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) RestrictedCookieManager
                     mojom::CookieManagerGetOptionsPtr options,
                     bool is_ad_tagged,
                     bool force_disable_third_party_cookies,
-                    mojom::SourceLocationPtr source_location,
                     GetAllForUrlCallback callback) override;
 
   void SetCanonicalCookie(const net::CanonicalCookie& cookie,
@@ -158,7 +157,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) RestrictedCookieManager
                         bool get_version_shared_memory,
                         bool is_ad_tagged,
                         bool force_disable_third_party_cookies,
-                        mojom::SourceLocationPtr source_location,
                         GetCookiesStringCallback callback) override;
   void CookiesEnabledFor(const GURL& url,
                          const net::SiteForCookies& site_for_cookies,
@@ -215,7 +213,6 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) RestrictedCookieManager
       const net::CookieSettingOverrides& cookie_setting_overrides,
       const net::CookieOptions& net_options,
       mojom::CookieManagerGetOptionsPtr options,
-      mojom::SourceLocationPtr source_location,
       GetAllForUrlCallback callback,
       const net::CookieAccessResultList& cookie_list,
       const net::CookieAccessResultList& excluded_cookies);
