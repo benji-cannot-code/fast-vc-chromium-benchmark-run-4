9f66c133ec60c359fb3013f1fdced93cca234e64
Zonghan Xu <xzonghan@google.com>
2024-06-20 17:55:49 +0000

    Correct ToggleExtensionReport
    Correct ChromeProfileRequestGenerator::ToggleExtensionReport to be enabling extensions instead of policies.
    
    Bug: 348366699
    Change-Id: I38b5dd23004799535a81d8d698116b03d4aff93a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645744
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zonghan Xu <xzonghan@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317496}

--- components/enterprise/browser/reporting/chrome_profile_request_generator.cc
+++ components/enterprise/browser/reporting/chrome_profile_request_generator.cc
@@ -41,7 +41,7 @@ void ChromeProfileRequestGenerator::Generate(ReportCallback callback) {
 }
 
 void ChromeProfileRequestGenerator::ToggleExtensionReport(bool enabled) {
-  profile_report_generator_.set_policies_enabled(enabled);
+  profile_report_generator_.set_extensions_enabled(enabled);
 }
 
 void ChromeProfileRequestGenerator::OnBrowserReportReady(
