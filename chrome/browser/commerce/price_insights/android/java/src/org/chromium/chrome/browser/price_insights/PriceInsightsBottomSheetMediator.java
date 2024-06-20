dcf4a9068605aba2b86e980670273a199d0c0658
Qi Bao <qib@google.com>
2024-06-20 19:21:03 +0000

    [Price insights clank] Create basic price insights bottom sheet MVC architecture
    Bug: 336825059
    Change-Id: Ibecef10d3f3b89b7235955ce6a0e1907f52ba87a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637854
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Commit-Queue: Qi Bao <qib@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317555}

---
+++ chrome/browser/commerce/price_insights/android/java/src/org/chromium/chrome/browser/price_insights/PriceInsightsBottomSheetMediator.java
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.price_insights;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import org.chromium.chrome.R;
+import org.chromium.components.commerce.core.ShoppingService;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** Mediator for price insights bottom sheet responsible for model update. */
+public class PriceInsightsBottomSheetMediator {
+    private final Context mContext;
+    private final ShoppingService mShoppingService;
+    private final PropertyModel mPropertyModel;
+
+    public PriceInsightsBottomSheetMediator(
+            @NonNull Context context,
+            @NonNull ShoppingService shoppingService,
+            @NonNull PropertyModel propertyModel) {
+        mContext = context;
+        mShoppingService = shoppingService;
+        mPropertyModel = propertyModel;
+    }
+
+    public void requestShowContent() {
+        mPropertyModel.set(
+                PriceInsightsBottomSheetProperties.PRICE_HISTORY_TITLE,
+                mContext.getResources().getString(R.string.price_history_title));
+    }
+}
