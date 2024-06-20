9f59567081be4c89759f60b0242265f5a661164d
Sammie Quon <sammiequon@chromium.org>
2024-06-20 23:51:03 +0000

    overview: Use view tracker to avoid dangling pointer
    This code is going away soon, but there seems to be an uptick in
    crashes lately.
    
    Test: Remove LeakedDanglingUtriaged from OverviewFocusCyclerOld::focused_view_ and run trybots
    Bug: b/336289329
    Change-Id: I90087017c14907f367beb884ecd92cf739baed21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643921
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317695}

--- ash/wm/overview/overview_focus_cycler_old.cc
+++ ash/wm/overview/overview_focus_cycler_old.cc
@@ -129,6 +129,7 @@ void OverviewFocusCyclerOld::MoveFocus(bool reverse) {
           ->MaybeToggleA11yHighlightOnUndoDeskRemovalToast()) {
     SetFocusVisibility(false);
     focused_view_ = nullptr;
+    focused_view_tracker_.SetView(nullptr);
     return;
   }
 
@@ -183,6 +184,7 @@ void OverviewFocusCyclerOld::OnViewDestroyingOrDisabling(
   deleted_index_ = view_index;
   focused_view_->SetFocused(false);
   focused_view_ = nullptr;
+  focused_view_tracker_.SetView(nullptr);
 }
 
 void OverviewFocusCyclerOld::SetFocusVisibility(bool visible) {
@@ -244,6 +246,7 @@ void OverviewFocusCyclerOld::ResetFocusedView() {
   deleted_index_.reset();
   focused_view_->SetFocused(false);
   focused_view_ = nullptr;
+  focused_view_tracker_.SetView(nullptr);
 }
 
 std::vector<OverviewFocusableView*> OverviewFocusCyclerOld::GetTraversableViews()
@@ -308,6 +311,9 @@ void OverviewFocusCyclerOld::UpdateFocus(
 
   OverviewFocusableView* previous_view = focused_view_;
   focused_view_ = view_to_be_focused;
+  focused_view_tracker_.SetView(view_to_be_focused->GetView());
+  focused_view_tracker_.SetIsDeletingCallback(base::BindOnce(
+      &OverviewFocusCyclerOld::OnFocusedViewDeleting, base::Unretained(this)));
 
   // Perform accessibility related tasks.
   if (!suppress_accessibility_event) {
@@ -327,6 +333,10 @@ void OverviewFocusCyclerOld::UpdateFocus(
     previous_view->SetFocused(false);
   }
   focused_view_->SetFocused(true);
+}
+
+void OverviewFocusCyclerOld::OnFocusedViewDeleting() {
+  focused_view_ = nullptr;
 }
 
 }  // namespace ash
--- ash/wm/overview/overview_focus_cycler_old.h
+++ ash/wm/overview/overview_focus_cycler_old.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/memory/raw_ptr.h"
+#include "ui/views/view_tracker.h"
 
 namespace ash {
 class OverviewFocusableView;
@@ -114,6 +115,8 @@ class ASH_EXPORT OverviewFocusCyclerOld {
   void UpdateFocus(OverviewFocusableView* view_to_be_focused,
                    bool suppress_accessibility_event = false);
 
+  void OnFocusedViewDeleting();
+
   // The overview session which owns this object. Guaranteed to be non-null for
   // the lifetime of `this`.
   const raw_ptr<OverviewSession> overview_session_;
@@ -125,6 +128,10 @@ class ASH_EXPORT OverviewFocusCyclerOld {
   // The current view that is being focused, if any.
   raw_ptr<OverviewFocusableView, LeakedDanglingUntriaged> focused_view_ =
       nullptr;
+  // Tracks the view associated with `focused_view_`. This is a strange pattern
+  // to have both an untriaged pointer and a view tracker, but it is temporary
+  // as this class is going away soon.
+  views::ViewTracker focused_view_tracker_;
 
   // Helps to update the current a11y override window. And accessibility
   // features will focus on the window that is being set. Once `this` goes out
