d25139e8e7abf235c323e6dfc30b9fc7392a1912
Ashutosh Singhal <macinashutosh@google.com>
2024-06-20 09:31:33 +0000

    Allow additional URLs to access device attributes API in web kiosk sessions
    Bug: b/343010457
    Change-Id: I83cb807be741511abe1bb0b63020b7a82cab0e2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5636437
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317262}

--- chrome/browser/device_api/device_service_impl.cc
+++ chrome/browser/device_api/device_service_impl.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/policy/web_app_policy_constants.h"
 #include "chrome/common/pref_names.h"
+#include "components/permissions/features.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_frame_host.h"
@@ -43,6 +44,13 @@ namespace {
 // Check whether the target origin is the same as the main application running
 // in the Kiosk session.
 bool IsEqualToKioskOrigin(const url::Origin& origin) {
+  if (base::FeatureList::IsEnabled(
+          permissions::features::kAllowMultipleOriginsForWebKioskPermissions)) {
+    if (chrome::IsWebKioskOriginAllowed(origin.GetURL())) {
+      return true;
+    }
+  }
+
 #if BUILDFLAG(IS_CHROMEOS_ASH)
   const AccountId& account_id =
       user_manager::UserManager::Get()->GetPrimaryUser()->GetAccountId();
