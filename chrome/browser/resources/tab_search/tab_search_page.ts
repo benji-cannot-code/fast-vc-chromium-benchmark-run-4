19ee13f9973835a98b4b8083a3d5bd53240c6ca9
dpapad <dpapad@chromium.org>
2024-06-20 21:37:15 +0000

    TabSearch: Use stricter Lit TS compiler flags.
    Removing the GN dependency to "//third_party/polymer/v3_0:library" and
    fixing any errors that show up as a result of the stricter Lit TS
    configuration.
    
    Bug: 40943652
    Change-Id: I759d2b3d9fa292f8721eb1894acefb26faf65edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640708
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1317618}

--- chrome/browser/resources/tab_search/tab_search_page.ts
+++ chrome/browser/resources/tab_search/tab_search_page.ts
@@ -127,10 +127,10 @@ export class TabSearchPageElement extends TabSearchSearchFieldBase {
   protected searchResultText_: string = '';
   protected activeSelectionId_?: string;
   protected shortcut_: string = loadTimeData.getString('shortcutText');
-  override autofocus: boolean;
+  override autofocus: boolean = false;
 
   private apiProxy_: TabSearchApiProxy = TabSearchApiProxyImpl.getInstance();
-  private metricsReporter_: MetricsReporter|null;
+  private metricsReporter_: MetricsReporter|null = null;
   private listenerIds_: number[] = [];
   private tabGroupsMap_: Map<string, TabGroup> = new Map();
   private recentlyClosedTabGroups_: TabGroupData[] = [];
@@ -141,7 +141,6 @@ export class TabSearchPageElement extends TabSearchSearchFieldBase {
   private openTabsTitleItem_: TitleItem;
   private recentlyClosedTitleItem_: TitleItem;
   private filteredOpenTabsCount_: number = 0;
-  private filteredMediaTabsCount_: number = 0;
   private initiallySelectedTabIndex_: number = NO_SELECTION;
   private documentVisibilityChangedListener_: () => void;
   private elementVisibilityChangedListener_: IntersectionObserver;
@@ -722,8 +721,6 @@ export class TabSearchPageElement extends TabSearchSearchFieldBase {
 
     this.filteredOpenTabsCount_ =
         filteredOpenTabs.length + filteredMediaTabs.length;
-
-    this.filteredMediaTabsCount_ = filteredMediaTabs.length;
 
     const recentlyClosedItems: Array<TabData|TabGroupData> =
         [...this.recentlyClosedTabs_, ...this.recentlyClosedTabGroups_];
--- chrome/test/data/webui/tab_search/tab_search_app_test.ts
+++ chrome/test/data/webui/tab_search/tab_search_app_test.ts
@@ -7,8 +7,7 @@ import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {TabSearchAppElement} from 'chrome://tab-search.top-chrome/tab_search.js';
 import {TabSearchApiProxyImpl} from 'chrome://tab-search.top-chrome/tab_search.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
-import {isVisible} from 'chrome://webui-test/test_util.js';
+import {isVisible, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestTabSearchApiProxy} from './test_tab_search_api_proxy.js';
 
@@ -28,7 +27,6 @@ suite('TabOrganizationPageTest', () => {
 
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
     document.body.appendChild(tabSearchApp);
-    await flushTasks();
   });
 
   test('Switching tabs calls setTabIndex', async () => {
@@ -57,7 +55,7 @@ suite('TabOrganizationPageTest', () => {
     assertEquals(0, crTabs.selected);
 
     testProxy.getCallbackRouterRemote().tabSearchTabIndexChanged(1);
-    await flushTasks();
+    await microtasksFinished();
 
     assertEquals(1, crTabs.selected);
   });
@@ -68,7 +66,7 @@ suite('TabOrganizationPageTest', () => {
     assertTrue(isVisible(crTabs));
 
     testProxy.getCallbackRouterRemote().tabOrganizationEnabledChanged(false);
-    await flushTasks();
+    await microtasksFinished();
 
     assertFalse(isVisible(crTabs));
   });
