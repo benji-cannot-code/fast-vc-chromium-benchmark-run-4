12055b8050c535efc268663d33a725df8e833a9b
Salma Elmahallawy <esalma@google.com>
2024-06-20 14:46:23 +0000

    [IOSSignout] Reset signout coordinator before destroying parent
    The signout action sheet coordinator should be stopped before the
    `ClearBrowsingDataViewController` is destroyed. Otherwise, the
    coordinator's dealloc method will be called before the coordinator is
    stopped (which currently fails at DCHECK if this happens.
    See bug details for more information on the rare case when this can
    happen.
    
    Bug: 346613519
    Change-Id: Ide64f26119d406b5f167151d2a0055220b47cf65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5642355
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317366}

--- ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_table_view_controller.mm
@@ -217,12 +217,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self.actionSheetCoordinator stop];
     self.actionSheetCoordinator = nil;
   }
+
   [self dismissAlertCoordinator];
+
   if (self.overlayCoordinator.started) {
     [self.overlayCoordinator stop];
     self.navigationController.interactivePopGestureRecognizer.delegate = nil;
     self.overlayCoordinator = nil;
   }
+
+  [_signoutCoordinator stop];
+  _signoutCoordinator = nil;
+
   _identityManagerObserverBridge.reset();
   [self.dataManager disconnect];
 }
