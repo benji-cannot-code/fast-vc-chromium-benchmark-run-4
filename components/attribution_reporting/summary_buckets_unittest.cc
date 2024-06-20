f8496f49553bc52425f3fe3491081c9dbb6b51aa
Andrew Paseltiner <apaseltiner@chromium.org>
2024-06-20 14:58:16 +0000

    Add JSON serialization for summary_window_operator
    This will be needed for proper display of trigger specs in the internals
    UI.
    
    Bug: 40287976
    Change-Id: I734064a8cd1dda3d5e99764326b19c915090e8fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644331
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317370}

--- components/attribution_reporting/summary_buckets_unittest.cc
+++ components/attribution_reporting/summary_buckets_unittest.cc
@@ -27,6 +27,7 @@ namespace {
 using ::attribution_reporting::mojom::SourceRegistrationError;
 using ::attribution_reporting::mojom::SummaryWindowOperator;
 using ::base::test::ErrorIs;
+using ::base::test::IsJson;
 using ::base::test::ValueIs;
 using ::testing::ElementsAre;
 using ::testing::Property;
@@ -74,6 +75,21 @@ TEST(SummaryWindowOperatorTest, Parse) {
     const base::Value::Dict dict = base::test::ParseJsonDict(test_case.json);
 
     EXPECT_THAT(ParseSummaryWindowOperator(dict), test_case.matches);
+  }
+}
+
+TEST(SummaryWindowOperatorTest, Serialize) {
+  {
+    base::Value::Dict out;
+    Serialize(SummaryWindowOperator::kCount, out);
+    EXPECT_THAT(out, IsJson(R"json({"summary_window_operator": "count"})json"));
+  }
+
+  {
+    base::Value::Dict out;
+    Serialize(SummaryWindowOperator::kValueSum, out);
+    EXPECT_THAT(out,
+                IsJson(R"json({"summary_window_operator": "value_sum"})json"));
   }
 }
 
@@ -170,7 +186,7 @@ TEST(SummaryBucketsTest, Serialize) {
   base::Value::Dict dict;
   SummaryBuckets({1, 3, 4294967295}).Serialize(dict);
 
-  EXPECT_THAT(dict, base::test::IsJson(R"json({
+  EXPECT_THAT(dict, IsJson(R"json({
     "summary_buckets": [1, 3, 4294967295]
   })json"));
 }
