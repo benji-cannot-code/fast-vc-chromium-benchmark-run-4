25ed7e09b85a77df5c0ae3228596479cd8c52c42
rgod <rgod@google.com>
2024-06-20 07:36:00 +0000

    [WebAuthn] Add some details to passkey saved confirmation bubble
    * Adds "Passkey saved" title ("Passkey saved and PIN created" will be
    added in a follow-up)
    * Adds row with passkey icon and label (for now the actual label is
    not passed)
    * Adds button for navigating to password manager (for now clicks are
    not handled as it's not yet decided what exactly they should open).
    
    Bug: b/345242100
    Change-Id: If4265eda56ca18e9f266b560908c71f3832a7b0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643111
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317232}

--- chrome/browser/ui/webauthn/passkey_saved_confirmation_controller_unittest.cc
+++ chrome/browser/ui/webauthn/passkey_saved_confirmation_controller_unittest.cc
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
@@ -67,7 +66,6 @@ TEST_F(PasskeySavedConfirmationControllerTest, DestroyImplicictly) {
 
 TEST_F(PasskeySavedConfirmationControllerTest, Content) {
   CreateController();
-  EXPECT_EQ(
-      controller()->GetTitle(),
-      l10n_util::GetStringUTF16(IDS_PASSWORD_MANAGER_CONFIRM_SAVED_TITLE));
+  EXPECT_EQ(controller()->GetTitle(),
+            l10n_util::GetStringUTF16(IDS_WEBAUTHN_GPM_PASSKEY_SAVED_TITLE));
 }
