3322651606039ae1c286ef1fffc7f9c8cc50e619
John Chen <johnchen@chromium.org>
2024-06-20 19:05:16 +0000

    [Perf] Expand crossbench runs
    Add crossbench-based Speedometer 3 to additional desktop platforms
    (one platform per OS type). Add JetStream 2 and MotionMark 1.3 to
    linux-perf-fyi.
    
    Bug: b/338629299, b/338630226
    Change-Id: Ie5fcc6729cfffc7ccd23c1c821117ef566299d83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639636
    Commit-Queue: John Chen <johnchen@chromium.org>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317541}

--- tools/perf/cross_device_test_config.py
+++ tools/perf/cross_device_test_config.py
@@ -147,12 +147,15 @@ TARGET_DEVICES = {
         'speedometer3-predictable': {
             'Speedometer3': 20,
         },
+        'speedometer3.crossbench': 20,
     },
     'linux-perf-fyi': {
         'speedometer2': 4,
         'speedometer2-nominorms': 4,
         'speedometer3': 4,
         'speedometer3.crossbench': 4,
+        'jetstream2.crossbench': 4,
+        'motionmark13.crossbench': 4,
     },
     'win-10_laptop_low_end-perf': {
         'jetstream2': {
@@ -192,6 +195,7 @@ TARGET_DEVICES = {
         'speedometer3': {
             'Speedometer3': 20,
         },
+        'speedometer3.crossbench': 20,
     },
     'linux-perf-calibration': {
         'jetstream2': {
@@ -235,6 +239,7 @@ TARGET_DEVICES = {
         'speedometer3-nominorms': {
             'Speedometer3': 20,
         },
+        'speedometer3.crossbench': 20,
     },
     'mac-m1_mini_2020-perf-pgo': {
         'jetstream2': {
