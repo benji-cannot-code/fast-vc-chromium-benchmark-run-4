75da2e2b8b91748d5dfed7ad490e3f8b034a4c30
Brandon Wylie <wylieb@chromium.org>
2024-06-20 18:16:35 +0000

    [declutter] Add TabActionState support to TabListEditor
    Bug: 347750500,342255180
    Change-Id: I0b03897dc4d37a59b68a4ca8b5100fb666121477
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637884
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317511}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorCoordinator.java
@@ -111,6 +111,9 @@ class TabListEditorCoordinator {
 
         /** Sets a custom {@link NavigationProvider} to handle "back" actions. */
         void setNavigationProvider(@NonNull NavigationProvider navigationProvider);
+
+        /** Sets the {@link TabActionState} for the TabListEditor. */
+        void setTabActionState(@TabActionState int tabActionState);
     }
 
     /** An interface for embedders to provide navigation. */
@@ -193,6 +196,12 @@ class TabListEditorCoordinator {
                 public ObservableSupplier<Boolean> getHandleBackPressChangedSupplier() {
                     return mTabListEditorMediator.getHandleBackPressChangedSupplier();
                 }
+
+                @Override
+                public void setTabActionState(@TabActionState int tabActionState) {
+                    mTabActionState = tabActionState;
+                    mTabListEditorMediator.setTabActionState(tabActionState);
+                }
             };
 
     private final Context mContext;
@@ -207,13 +216,13 @@ class TabListEditorCoordinator {
 
     private final @TabListMode int mTabListMode;
     private final boolean mDisplayGroups;
-    private final @TabActionState int mInitialTabActionState;
     private final ViewGroup mRootView;
     private final TabContentManager mTabContentManager;
 
     private MultiThumbnailCardProvider mMultiThumbnailCardProvider;
     private TabListCoordinator mTabListCoordinator;
     private PropertyModelChangeProcessor mTabListEditorLayoutChangeProcessor;
+    private @TabActionState int mTabActionState;
 
     public TabListEditorCoordinator(
             Context context,
@@ -235,7 +244,7 @@ class TabListEditorCoordinator {
             mClientTabListRecyclerViewPositionSetter = clientTabListRecyclerViewPositionSetter;
             mTabListMode = mode;
             mDisplayGroups = displayGroups;
-            mInitialTabActionState = initialTabActionState;
+            mTabActionState = initialTabActionState;
             mRootView = rootView;
             mTabContentManager = tabContentManager;
             assert mode == TabListCoordinator.TabListMode.GRID
@@ -259,7 +268,7 @@ class TabListEditorCoordinator {
                             displayGroups,
                             snackbarManager,
                             mTabListEditorLayout,
-                            initialTabActionState);
+                            mTabActionState);
             mTabListEditorMediator.setNavigationProvider(
                     new TabListEditorNavigationProvider(mContext, mTabListEditorController));
         }
@@ -373,7 +382,7 @@ class TabListEditorCoordinator {
                         mDisplayGroups,
                         null,
                         null,
-                        mInitialTabActionState,
+                        mTabActionState,
                         this::getSelectionDelegate,
                         null,
                         mTabListEditorLayout,
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEditorMediator.java
@@ -69,6 +69,7 @@ class TabListEditorMediator
     private SnackbarManager mSnackbarManager;
     private TabListEditorToolbar mTabListEditorToolbar;
     private TabListEditorCoordinator.NavigationProvider mNavigationProvider;
+    private @TabActionState int mTabActionState;
 
     private final View.OnClickListener mNavigationClickListener =
             new View.OnClickListener() {
@@ -94,6 +95,7 @@ class TabListEditorMediator
         mActionOnRelatedTabs = actionOnRelatedTabs;
         mSnackbarManager = snackbarManager;
         mTabListEditorLayout = tabListEditorLayout;
+        mTabActionState = initialTabActionState;
 
         mTabModelObserver =
                 new TabModelObserver() {
@@ -121,7 +123,7 @@ class TabListEditorMediator
                     public void willCloseTab(Tab tab, boolean didCloseAlone) {
                         // TODO(crbug.com/338103697): Query the TabList for the current tab action
                         // state.
-                        if (initialTabActionState != TabProperties.TabActionState.CLOSABLE) {
+                        if (mTabActionState != TabProperties.TabActionState.CLOSABLE) {
                             hide();
                         }
                     }
@@ -130,7 +132,7 @@ class TabListEditorMediator
                     // using a custom click handler when selecting tabs.
                     @Override
                     public void didSelectTab(Tab tab, @TabSelectionType int type, int lastId) {
-                        if (initialTabActionState == TabProperties.TabActionState.CLOSABLE
+                        if (mTabActionState == TabProperties.TabActionState.CLOSABLE
                                 && type == TabSelectionType.FROM_USER) {
                             hide();
                         }
@@ -335,6 +337,12 @@ class TabListEditorMediator
             @NonNull TabListEditorCoordinator.NavigationProvider navigationProvider) {
         assert navigationProvider != null;
         mNavigationProvider = navigationProvider;
+    }
+
+    @Override
+    public void setTabActionState(@TabActionState int tabActionState) {
+        mTabActionState = tabActionState;
+        mTabListCoordinator.setTabActionState(tabActionState);
     }
 
     @Override
