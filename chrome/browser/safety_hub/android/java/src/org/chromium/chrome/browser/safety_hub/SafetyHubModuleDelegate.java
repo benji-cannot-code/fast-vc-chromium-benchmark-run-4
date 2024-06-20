c2098de495f69f05be975815f5e8b0f5ee4b7546
Zaina Al-Mashni <zalmashni@google.com>
2024-06-20 19:27:40 +0000

    [SHoA] Add the click action for Update Check module
    Open the Chrome play store page when the user clicks on the Update
    check module.
    
    Low-Coverage-Reason: Tests are added but only run on a Chrome branded build.
    Bug: 324562205
    Change-Id: Iea9063271f627033c8476e490474057d98a312bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644824
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317561}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegate.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegate.java
@@ -35,6 +35,13 @@ public interface SafetyHubModuleDelegate {
     UpdateStatusProvider.UpdateStatus getUpdateStatus();
 
     /**
+     * Opens the Play Store page for the installed Chrome channel.
+     *
+     * @param context used to launch the play store intent.
+     */
+    void openGooglePlayStore(Context context);
+
+    /**
      * @return The current safe browsing state.
      */
     @SafeBrowsingState
