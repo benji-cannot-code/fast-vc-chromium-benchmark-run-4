322cad93206afe3445e6a7879ea707df3cd421d7
Ian Vollick <vollick@chromium.org>
2024-06-20 16:32:04 +0000

    Update CookieAccessDetails deduplication for new additions
    Also adds static_asserts to flag when things are added to
    CookieAccessDetails or SiteForCookies. Additionally, the
    code has been factored out into separate files and I have
    added unit tests.
    
    NO_IFTTT=Adding the initial checks.
    
    Bug: 344653945
    Change-Id: I94f8e5c783c543788d430367efec5848319ff8b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5595089
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317429}

--- services/network/restricted_cookie_manager.h
+++ services/network/restricted_cookie_manager.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_setting_override.h"
 #include "net/cookies/cookie_store.h"
 #include "net/first_party_sets/first_party_set_metadata.h"
+#include "services/network/counted_cookie_access_details_set.h"
 #include "services/network/public/mojom/cookie_access_observer.mojom.h"
 #include "services/network/public/mojom/restricted_cookie_manager.mojom.h"
 #include "services/network/public/mojom/source_location.mojom.h"
@@ -42,23 +43,6 @@ class SiteForCookies;
 }  // namespace net
 
 namespace network {
-
-using CountedCookieAccessDetailsPtr =
-    std::pair<mojom::CookieAccessDetailsPtr, std::unique_ptr<size_t>>;
-
-struct CookieAccessDetailsPtrComparer {
-  bool operator()(const CountedCookieAccessDetailsPtr& lhs,
-                  const CountedCookieAccessDetailsPtr& rhs) const;
-};
-
-using CookieAccessDetails =
-    std::set<CountedCookieAccessDetailsPtr, CookieAccessDetailsPtrComparer>;
-
-struct CookieWithAccessResultComparer {
-  bool operator()(
-      const net::CookieWithAccessResult& cookie_with_access_result1,
-      const net::CookieWithAccessResult& cookie_with_access_result2) const;
-};
 
 using CookieAccessDetailsList =
     std::vector<network::mojom::CookieAccessDetailsPtr>;
