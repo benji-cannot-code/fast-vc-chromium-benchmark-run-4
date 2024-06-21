30f2def6d493479ee15e9d1112a71122f862c8c7
Vishwas Uppoor <vishwasuppoor@google.com>
2024-06-21 17:49:30 +0000

    [PIX] Introduce sequence view interface
    This interface allows features to show multiple screens in the same
    bottom sheet (go/pay-autofill-view-transitions-in-payment-flows).
    
    Create a new view class with this interface for the FOP selector screen.
    
    Fixed: 348142385, 348194733
    Change-Id: I4c2012d0d285e67c13a1f95f4f87cc1e87216557
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5642990
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1318044}

---
+++ chrome/browser/facilitated_payments/ui/android/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsSequenceView.java
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.facilitated_payments;
+
+import android.view.View;
+import android.widget.FrameLayout;
+
+import org.chromium.ui.modelutil.PropertyModel;
+
+/**
+ * An interface that allows different facilitated payment views to be shown in the same bottom
+ * sheet.
+ */
+interface FacilitatedPaymentsSequenceView {
+    /**
+     * Inflates the view to be shown, and binds it to a model. Does not attach the view to the
+     * {@code viewContainer}.
+     *
+     * @param viewContainer The {@link FrameLayout} which will hold this view when it is shown.
+     */
+    void setupView(FrameLayout viewContainer);
+
+    /**
+     * @return The {@link View} to be shown.
+     */
+    View getView();
+
+    /**
+     * @return The {@link PropertyModel} that can be used to manipulate {@link View} contents.
+     */
+    PropertyModel getModel();
+
+    /**
+     * @return The vertical scroll offset of the {@link View}.
+     */
+    int getVerticalScrollOffset();
+}
