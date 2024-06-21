a65dea14821b0fd1569d869c3a3d1f9e6abc8bfe
Hiroki Sato <hirokisato@google.com>
2024-06-21 08:11:04 +0000

    arc-a11y: Add PaneTitleHandler to support pane title
    When a pane title attribute is set or changed on Android tree, it's
    expected for a screen reader to announce a new title.
    The attribute can be set on a random node, but it's expected behavior
    looks like a live region.
    Thus, as a solution to make the attribute announced by ChromeVox when
    necessary, this introduces PaneTitleHandler. PaneTitleHandler creates a
    *virtual* node, which doesn't exist in original Android tree, and
    populates it's name from the pane title, having a live region attribute.
    
    Bug: b/323424681
    Test: Follow steps in the bug
    Test: PaneTitleHandlerTest
    Change-Id: I5981c7fe4b7cb99c5202ec8822bc40cd8583dc3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5641922
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Commit-Queue: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317818}

--- services/accessibility/android/accessibility_window_info_data_wrapper.h
+++ services/accessibility/android/accessibility_window_info_data_wrapper.h
@@ -24,6 +24,8 @@ class AccessibilityWindowInfoDataWrapper : public AccessibilityInfoDataWrapper {
       AXTreeSourceAndroid* tree_source,
       mojom::AccessibilityWindowInfoData* window);
 
+  ~AccessibilityWindowInfoDataWrapper() override;
+
   AccessibilityWindowInfoDataWrapper(
       const AccessibilityWindowInfoDataWrapper&) = delete;
   AccessibilityWindowInfoDataWrapper& operator=(
@@ -50,6 +52,8 @@ class AccessibilityWindowInfoDataWrapper : public AccessibilityInfoDataWrapper {
           children) const override;
   int32_t GetWindowId() const override;
 
+  void AddVirtualChild(int32_t child_id);
+
  private:
   bool GetProperty(mojom::AccessibilityWindowBooleanProperty prop) const;
   bool GetProperty(mojom::AccessibilityWindowIntProperty prop,
@@ -62,6 +66,8 @@ class AccessibilityWindowInfoDataWrapper : public AccessibilityInfoDataWrapper {
 
   raw_ptr<mojom::AccessibilityWindowInfoData, DanglingUntriaged> window_ptr_ =
       nullptr;
+
+  std::vector<int32_t> virtual_child_ids_ = std::vector<int32_t>(0);
 };
 
 }  // namespace ax::android
