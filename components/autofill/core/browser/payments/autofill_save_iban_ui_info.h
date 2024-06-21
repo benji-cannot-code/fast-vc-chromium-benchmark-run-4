f897d4cac04866479cefe4c6c733390a9d252377
Verina Armanyous <averina@google.com>
2024-06-21 22:15:41 +0000

    [IBAN][Clank] Create AutofillSaveIbanUiInfo c++ struct 6/X
    This CL introduces AutofillSaveIbanUiInfo which will provide all
    essential IBAN information required to be transmitted and displayed
    on save IBAN bottom sheet. This is a no-op CL as the Java side will be
    added separately then they will be connected in a following CL.
    
    Design doc: go/iban-save-bottom-sheet-mvc-clank & go/autofill-iban-dd-clank
    
    Bug: 309163448
    Change-Id: I2e87817039687fcc70dac8523813049700194f22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623372
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318189}

---
+++ components/autofill/core/browser/payments/autofill_save_iban_ui_info.h
@@ -0,0 +1,39 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_AUTOFILL_SAVE_IBAN_UI_INFO_H_
+#define COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_AUTOFILL_SAVE_IBAN_UI_INFO_H_
+
+#include <string>
+
+namespace autofill {
+
+// Holds resources for save IBAN bottom sheet UI.
+struct AutofillSaveIbanUiInfo {
+  std::u16string iban_label;
+  std::u16string title_text;
+  std::u16string accept_text;
+  std::u16string cancel_text;
+
+  AutofillSaveIbanUiInfo();
+
+  // AutofillSaveIbanUiInfo is not copyable.
+  AutofillSaveIbanUiInfo(const AutofillSaveIbanUiInfo&) = delete;
+  AutofillSaveIbanUiInfo& operator=(const AutofillSaveIbanUiInfo&) = delete;
+
+  // AutofillSaveIbanUiInfo is moveable.
+  AutofillSaveIbanUiInfo(AutofillSaveIbanUiInfo&& other) noexcept;
+  AutofillSaveIbanUiInfo& operator=(AutofillSaveIbanUiInfo&& other);
+
+  ~AutofillSaveIbanUiInfo();
+
+  // Create the ui info for a local save prompt.
+  static AutofillSaveIbanUiInfo CreateForLocalSave(
+      const std::u16string& iban_label);
+};
+
+}  // namespace autofill
+
+#endif  // COMPONENTS_AUTOFILL_CORE_BROWSER_PAYMENTS_AUTOFILL_SAVE_IBAN_UI_INFO_H_
