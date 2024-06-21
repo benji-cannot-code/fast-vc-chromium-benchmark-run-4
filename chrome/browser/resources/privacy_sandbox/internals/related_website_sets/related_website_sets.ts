81a4d58010cb2a4bef36cfed3d1e5fdb77bcf1e8
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-06-21 11:01:24 +0000

    Develop Sidebar component for Related Website Sets Dev UI
    This setups up the sidebar component using webUI gallery and lit
    
    Light mode:
    https://screenshot.googleplex.com/6rRyVj4d9MykpDh.png
    https://screenshot.googleplex.com/5y98VgjHDAs8GPe.png
    
    Dark mode:
    https://screenshot.googleplex.com/6tJpftLZt2rQtTW.png
    https://screenshot.googleplex.com/9wfAegCxXDpZ8CU.png
    
    Bug: 330877132
    Change-Id: I9223a3886c9ee9c2097e5ff551cdd7c1fe7b8b81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631356
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317864}

--- chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
@@ -5,4 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './app.js';
 
+export {RelatedWebsiteSetsSidebarElement} from './sidebar.js';
 export {RelatedWebsiteSetsToolbarElement} from './toolbar.js';
