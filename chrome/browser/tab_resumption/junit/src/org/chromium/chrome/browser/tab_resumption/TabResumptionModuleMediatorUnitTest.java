43154bc42f3b89c5cf94bbe300f66b729580426d
Samuel Huang <huangs@chromium.org>
2024-06-21 20:38:03 +0000

    [Android TRM] Fix Local Tab suggestion lingering after refresh-on-closure.
    We already reload the TRM when the user closes the Tab corresponding to
    a Local Tab suggestion. However, it was found that these expired
    suggestions lingers after the refresh. Clicking on the the suggestion
    tile would close the NTP.
    
    For the failure observed, LocalTabTabResumptionDataProvider was used.
    The cause is that when Tab closure is being handled, the Tab still
    exists. Refreshing the TRM eagerly would still lead to this Tab being
    found and suggested.
    
    Three possible solutions are:
    1. In Providers (Java & C++), skip a Local Tab suggestion if the Tab is
       in the "closing" state. On C++ side we'd need to add the support to
       TabAndroid.
    2. In Mediator.isSuggestionValid(), make Local Tab invalid if it's in
       the "closing" state.
    3. In Mediator.Session, post |mReloadSessionCallback| execution as a
       task instead of calling it eagerly.
    
    This CL adopts (3) as the main solution, and (2) as extra guard. (1) is
    rejected because it leaks abstraction to Providers, and requires more
    changes.
    
    Bug: 347266932
    Change-Id: Ib144a7e36e992be5c0cd62c72f49297b8a7bc546
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630900
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318145}

--- chrome/browser/tab_resumption/junit/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleMediatorUnitTest.java
+++ chrome/browser/tab_resumption/junit/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleMediatorUnitTest.java
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab_resumption;
 
+import static android.os.Looper.getMainLooper;
+
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.robolectric.Shadows.shadowOf;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.filters.SmallTest;
@@ -27,6 +30,7 @@ import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
 import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tab.Tab;
@@ -44,6 +48,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeoutException;
 
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
@@ -92,6 +97,7 @@ public class TabResumptionModuleMediatorUnitTest extends TestSupportExtended {
     private TabResumptionModuleMediator mMediator;
 
     private int mReloadSessionCounter;
+    private CallbackHelper mReloadSessionCallbackHelper;
 
     @Before
     public void setUp() {
@@ -114,6 +120,7 @@ public class TabResumptionModuleMediatorUnitTest extends TestSupportExtended {
                         /* urlImageProvider= */ mUrlImageProvider,
                         /* reloadSessionCallback= */ () -> {
                             ++mReloadSessionCounter;
+                            mReloadSessionCallbackHelper.notifyCalled();
                         },
                         /* statusChangedCallback= */ () -> {},
                         /* seeMoreLinkClickCallback= */ () -> {},
@@ -481,6 +488,7 @@ public class TabResumptionModuleMediatorUnitTest extends TestSupportExtended {
                 Arrays.asList(
                         SuggestionEntry.createFromLocalTab(tab0),
                         SuggestionEntry.createFromLocalTab(tab1));
+        mReloadSessionCallbackHelper = new CallbackHelper();
 
         mMediator.loadModule();
         verify(mDataProvider, times(1)).fetchSuggestions(mFetchSuggestionCallbackCaptor.capture());
@@ -505,6 +513,12 @@ public class TabResumptionModuleMediatorUnitTest extends TestSupportExtended {
         mTabObserverMap.get(tab0.getId()).onClosingStateChanged(tab0, /* closing= */ false);
         Assert.assertEquals(0, mReloadSessionCounter);
         mTabObserverMap.get(tab0.getId()).onClosingStateChanged(tab0, /* closing= */ true);
+        try {
+            shadowOf(getMainLooper()).idle();
+            mReloadSessionCallbackHelper.waitForNext();
+        } catch (TimeoutException e) {
+            Assert.fail("Timed out waiting for reload callback.");
+        }
         Assert.assertEquals(1, mReloadSessionCounter);
 
         // Simulate ending session: Observation should be released.
