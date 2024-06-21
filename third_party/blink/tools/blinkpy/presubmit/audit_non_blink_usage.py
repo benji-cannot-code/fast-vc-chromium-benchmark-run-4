ca35a4078e166cb873fcf897970af1abf60dd0c4
Hao Liu <haoliuk@chromium.org>
2024-06-21 23:03:19 +0000

    Add base::TimeTicks to allowed identifer list in blink/public folder
    There has been presubmit error of usage of disallowed identifier
    base::TimeTicks when it is used inside the third_party/blink/public.
    
    This is to add it to allow list.
    
    Change-Id: I9887c58a0150e442b3f156c36e613a392901e0a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646410
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318206}

--- third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
+++ third_party/blink/tools/blinkpy/presubmit/audit_non_blink_usage.py
@@ -1114,6 +1114,9 @@ _CONFIG = [
             'base::OnceClosure',
             'base::RepeatingCallback',
             'base::RepeatingClosure',
+            'base::Time'
+            'base::TimeDelta',
+            'base::TimeTicks',
 
             'gfx::Point',
             'gfx::PointF',
