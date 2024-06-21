01960616dfe2d4cc4c10a97c5f7d0c352f20b92d
Matt Jones <mdjones@chromium.org>
2024-06-21 14:30:36 +0000

    Update the page title to match the product specifications set
    Bug: 347717391
    Change-Id: I4e2cffb9545e65af9ccc800b5d39a102f5b07cb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5635138
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317926}

--- chrome/browser/resources/commerce/product_specifications/app.ts
+++ chrome/browser/resources/commerce/product_specifications/app.ts
@@ -157,6 +157,7 @@ export class ProductSpecificationsElement extends PolymerElement {
       const {set} = await this.shoppingApi_.getProductSpecificationsSetByUuid(
           {value: idParam});
       if (set) {
+        document.title = set.name;
         this.setName_ = set.name;
         this.populateTable_(set.urls.map(url => (url.url)));
         return;
@@ -341,6 +342,7 @@ export class ProductSpecificationsElement extends PolymerElement {
     if (set.uuid.value !== this.id_?.value) {
       return;
     }
+    document.title = set.name;
     this.setName_ = set.name;
     this.populateTable_(set.urls.map(url => url.url));
   }
