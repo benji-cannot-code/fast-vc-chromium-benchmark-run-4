b964fdc80da84849bbd051dab176cec9bd7ae490
Ivana Žužić <izuzic@google.com>
2024-06-21 11:39:06 +0000

    Reland "Track password deletion from Clear browsing data"
    This is a reland of commit c050b1331f7f5d480bb8685f8318394d0a75f689
    
    The problem in the original CL was a failing test. It was failing
    because it was in the wrong test suit which didn't have the account
    store flag enabled.
    
    Original change's description:
    > Track password deletion from Clear browsing data
    >
    > If the password was manually deleted via Clear Browsing Data, the
    > corresponding bit should be set in the removal reason prefs.
    >
    > Bug: 342519805, 347154910
    > Change-Id: I542ee7e4a00a6e0d69e555cd70fd0e1148d772a7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5625946
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Commit-Queue: Ivana Žužić <izuzic@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1315086}
    
    Bug: 342519805, 347154910
    Change-Id: Ic2233507613f80e951b8acbd74f57bdeac55ee3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5638653
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317872}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate.cc
@@ -117,6 +117,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/open_from_clipboard/clipboard_recent_content.h"
 #include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/features/password_manager_features_util.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store/password_store_interface.h"
 #include "components/password_manager/core/browser/password_store/smart_bubble_stats_store.h"
 #include "components/payments/content/payment_manifest_web_data_service.h"
@@ -972,6 +973,12 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
     browsing_data::RemoveFederatedSiteSettingsData(delete_begin_, delete_end_,
                                                    website_settings_filter,
                                                    host_content_settings_map_);
+
+    // Record that a password removal action happened for the profile store.
+    AddPasswordRemovalReason(
+        profile_->GetPrefs(), password_manager::IsAccountStore(false),
+        password_manager::metrics_util::PasswordManagerCredentialRemovalReason::
+            kClearBrowsingData);
   }
 
   if (remove_mask & constants::DATA_TYPE_ACCOUNT_PASSWORDS) {
@@ -996,6 +1003,12 @@ void ChromeBrowsingDataRemoverDelegate::RemoveEmbedderData(
           CreateTaskCompletionClosure(TracingDataType::kAccountPasswords),
           std::move(sync_completion));
     }
+
+    // Record that a password removal action happened for the account store.
+    AddPasswordRemovalReason(
+        profile_->GetPrefs(), password_manager::IsAccountStore(true),
+        password_manager::metrics_util::PasswordManagerCredentialRemovalReason::
+            kClearBrowsingData);
   }
 
   CHECK(deferred_disable_passwords_auto_signin_cb_.is_null(),
