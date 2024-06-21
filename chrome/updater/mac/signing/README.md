1ee6e104e300cc5a1cfd91ec90f6fe0d90f7d536
Xiaoling Bao <xiaolingbao@google.com>
2024-06-21 17:16:08 +0000

    Skip signing the macOS updater pkg if no installer signing identity.
    This makes testing the signing flow easier on dev machine.
    
    Bug: 325538784
    Change-Id: I8c40982f93b292e95560e5f561019f19a8d61d19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646902
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318019}

--- chrome/updater/mac/signing/README.md
+++ chrome/updater/mac/signing/README.md
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Testing
-1. Create a self-signed certificate, e.g. with identity `myid`.
+1. Create a self-signed certificate, e.g. with identity `myid`, or use the
+ad-hoc signing identity `'-'`.
 2. `autoninja -C out/MyOutDir chrome/updater`
 3. ```
      out/MyOutDir/Updater\ Packaging/sign_updater.py --identity myid \
      --development --input out/MyOutDir --output MySignedOutputDir \
-     --no-notarize --development
+     --notarize=none
    ```
--- chrome/updater/mac/signing/pipeline.py
+++ chrome/updater/mac/signing/pipeline.py
@@ -108,7 +108,7 @@ def _package_and_sign_pkg(paths, config):
     """
     pkg_path = os.path.join(paths.output,
                             '{}.pkg'.format(config.packaging_basename))
-    commands.run_command([
+    args = [
         'pkgbuild',
         '--root',
         os.path.join(paths.work, config.app_dir),
@@ -118,11 +118,15 @@ def _package_and_sign_pkg(paths, config):
                      '%s.app' % config.app_product),
         '--scripts',
         os.path.join(paths.input, config.packaging_dir, 'signing', 'pkg'),
-        '--sign',
-        config.installer_identity,
         '--timestamp',
         pkg_path,
-    ])
+    ]
+    if config.installer_identity:
+        args.extends([
+            '--sign',
+            config.installer_identity,
+        ])
+    commands.run_command(args)
     return pkg_path
 
 
@@ -207,8 +211,9 @@ def sign_all(orig_paths,
         pkg_path = _package_and_sign_pkg(package_paths, config)
 
         # Notarize the packages, then staple.
-        uuid_to_path = {}
-        uuid_to_path[notarize.submit(pkg_path, config)] = pkg_path
-        uuid_to_path[notarize.submit(dmg_path, config)] = dmg_path
-        for uuid in notarize.wait_for_results(uuid_to_path.keys(), config):
-            notarize.staple(uuid_to_path[uuid])
+        if config.notarize.should_notarize():
+            uuid_to_path = {}
+            uuid_to_path[notarize.submit(pkg_path, config)] = pkg_path
+            uuid_to_path[notarize.submit(dmg_path, config)] = dmg_path
+            for uuid in notarize.wait_for_results(uuid_to_path.keys(), config):
+                notarize.staple(uuid_to_path[uuid])
