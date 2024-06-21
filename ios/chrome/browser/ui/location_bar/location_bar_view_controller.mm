4edf60ba6de5973526e0fcb405c35b69ca0e7349
Radu Nitescu <radunitescu@google.com>
2024-06-21 12:34:33 +0000

    [iOS] Remove pre iOS 16 code in omnibox related codebase
    Fixed: 347905458
    Change-Id: Ie8eac3e2ff48677c1cac4beb2206bc6800cb2b0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640038
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317888}

--- ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
+++ ios/chrome/browser/ui/location_bar/location_bar_view_controller.mm
@@ -694,16 +694,6 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
     [menuElements addObject:divider];
   }
 
-  // Reverse the array manually when preferredMenuElementOrder is not available.
-  if (IsBottomOmniboxAvailable()) {
-    if (!base::ios::IsRunningOnIOS16OrLater()) {
-      if (GetApplicationContext()->GetLocalState()->GetBoolean(
-              prefs::kBottomOmnibox)) {
-        menuElements =
-            [[[menuElements reverseObjectEnumerator] allObjects] mutableCopy];
-      }
-    }
-  }
   return [UIMenu menuWithTitle:@"" children:menuElements];
 }
 
@@ -732,10 +722,8 @@ const NSString* kScribbleOmniboxElementId = @"omnibox";
                    }];
 
   if (IsBottomOmniboxAvailable()) {
-    if (@available(iOS 16, *)) {
-      configuration.preferredMenuElementOrder =
-          UIContextMenuConfigurationElementOrderPriority;
-    }
+    configuration.preferredMenuElementOrder =
+        UIContextMenuConfigurationElementOrderPriority;
   }
   return configuration;
 }
