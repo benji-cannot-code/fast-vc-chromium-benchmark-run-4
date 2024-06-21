ae79b52cfe694f064822fa6b0f4dc96765ce08dd
Martin Kreichgauer <martinkr@google.com>
2024-06-21 17:59:09 +0000

    webauthn: Make PublicKeyCredential.toJSON() conform to the spec again
    There have been several changes since the original implementation:
    - Some IDL dictionary members are now `required`
    - `AuthenticatorAttestationResponseJSON` has additional fields
    - `PublicKeyCredentialJSON` is now a typedef for `object`
    
    This CL addresses all of these. This also makes the WPT pass again,
    which had already been updated upstream to match the spec behavior
    and was thus failing.
    
    Bug: 40250593,626703
    Change-Id: Iac37f5a364aa5ae3c3ccaa73f7b7e6be18a4e716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645230
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318053}

--- third_party/blink/renderer/modules/credentialmanagement/public_key_credential.h
+++ third_party/blink/renderer/modules/credentialmanagement/public_key_credential.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
+#include "v8/include/v8-local-handle.h"
+#include "v8/include/v8-value.h"
 
 namespace blink {
 
@@ -29,7 +31,6 @@ class PublicKeyCredentialCreationOptionsJSON;
 class PublicKeyCredentialRequestOptions;
 class PublicKeyCredentialRequestOptionsJSON;
 class ScriptState;
-class V8UnionAuthenticationResponseJSONOrRegistrationResponseJSON;
 
 class MODULES_EXPORT PublicKeyCredential : public Credential {
   DEFINE_WRAPPERTYPEINFO();
@@ -69,8 +70,7 @@ class MODULES_EXPORT PublicKeyCredential : public Credential {
       const PublicKeyCredentialRequestOptionsJSON*,
       ExceptionState&);
 
-  const V8UnionAuthenticationResponseJSONOrRegistrationResponseJSON* toJSON(
-      ScriptState*) const;
+  v8::Local<v8::Value> toJSON(ScriptState*) const;
 
   // Credential:
   void Trace(Visitor*) const override;
