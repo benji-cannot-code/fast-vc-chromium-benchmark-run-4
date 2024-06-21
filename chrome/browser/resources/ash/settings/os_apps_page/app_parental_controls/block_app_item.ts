acdbf74f2dc510fed437653a9b0d0bcef0f96a20
Courtney Wong <courtneywong@chromium.org>
2024-06-21 17:53:22 +0000

    app-controls: Toggle app blocked state when app row is clicked
    Tested: http://screencast/cast/Njc0NjYzMDQ3MTU0ODkyOHw4MGNkYTg1My04ZA
    Bug: b:343710136
    Change-Id: I25568fd57733b2a1016dd798654a60cc5ea2e22c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640754
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318049}

--- chrome/browser/resources/ash/settings/os_apps_page/app_parental_controls/block_app_item.ts
+++ chrome/browser/resources/ash/settings/os_apps_page/app_parental_controls/block_app_item.ts
@@ -36,8 +36,12 @@ export class BlockAppItemElement extends PolymerElement {
     this.mojoInterfaceProvider = getAppParentalControlsProvider();
   }
 
-  override connectedCallback(): void {
-    super.connectedCallback();
+  override ready(): void {
+    super.ready();
+
+    this.addEventListener('click', () => {
+      this.updateBlockedState_(!this.app.isBlocked);
+    });
   }
 
   private isAllowed_(app: App): boolean {
@@ -46,6 +50,10 @@ export class BlockAppItemElement extends PolymerElement {
 
   private onToggleChange_(e: CustomEvent<boolean>): void {
     const isBlocked = !e.detail;
+    this.updateBlockedState_(isBlocked);
+  }
+
+  private updateBlockedState_(isBlocked: boolean): void {
     this.mojoInterfaceProvider.updateApp(this.app.id, isBlocked);
   }
 
