5e282bb9b31bf319a49076270743dd7b86b52ae4
Colin Blundell <blundell@chromium.org>
2024-06-21 13:44:45 +0000

    [//chrome] Add flag for DawnSIRepsUseClientProvidedInternalUsages
    Bug: 339171225
    Change-Id: Iee779082cd6c30b3a8c827a3d02a93cfc6567cc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645865
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317912}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -564,6 +564,9 @@ extern const char kCooperativeSchedulingDescription[];
 extern const char kDarkenWebsitesCheckboxInThemesSettingName[];
 extern const char kDarkenWebsitesCheckboxInThemesSettingDescription[];
 
+extern const char kDawnSIRepsUseClientProvidedInternalUsagesName[];
+extern const char kDawnSIRepsUseClientProvidedInternalUsagesDescription[];
+
 extern const char kDebugPackedAppName[];
 extern const char kDebugPackedAppDescription[];
 
