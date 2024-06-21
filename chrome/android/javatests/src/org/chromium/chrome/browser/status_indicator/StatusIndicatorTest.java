ee4131ebd31c7a184f05a4a797afef629e51147d
Andrew Grieve <agrieve@chromium.org>
2024-06-21 20:26:53 +0000

    Android: Make StatusIndicatorTest not rely on disabled animations
    It was working fine with animations disabled, but was one of the few
    tests that stricting depends on it.
    
    This makes it work even with animations enabled.
    
    Bug: None
    Change-Id: I9cb89aa81f86856afb3aff5bdee59c05efb9651c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645728
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318140}

--- chrome/android/javatests/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/status_indicator/StatusIndicatorTest.java
@@ -15,7 +15,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.os.Build.VERSION_CODES;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -35,7 +34,6 @@ import org.junit.runner.RunWith;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
@@ -102,18 +100,11 @@ public class StatusIndicatorTest {
 
     @Test
     @MediumTest
-    @DisableIf.Build(
-            message = "https://crbug.com/1473240",
-            sdk_is_greater_than = VERSION_CODES.M,
-            sdk_is_less_than = VERSION_CODES.P)
     public void testShowAndHide() {
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
-
         TestThreadUtils.runOnUiThreadBlocking(
                 () ->
                         mStatusIndicatorCoordinator.show(
                                 "Status", null, Color.BLACK, Color.WHITE, Color.WHITE));
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
         // Wait until the status indicator finishes animating, or becomes fully visible.
         CriteriaHelper.pollUiThread(
@@ -121,13 +112,10 @@ public class StatusIndicatorTest {
                     Criteria.checkThat(
                             mBrowserControlsStateProvider.getTopControlsMinHeightOffset(),
                             Matchers.is(getStatusIndicator().getHeight()));
+                    Criteria.checkThat(
+                            getStatusIndicator().getVisibility(), Matchers.is(View.VISIBLE));
                 });
 
-        // Now, the Android view should be visible.
-        Assert.assertEquals(
-                "Wrong Android view visibility.",
-                View.VISIBLE,
-                getStatusIndicator().getVisibility());
         Assert.assertEquals(
                 "Wrong background color.",
                 Color.BLACK,
@@ -163,10 +151,10 @@ public class StatusIndicatorTest {
                     Criteria.checkThat(
                             mBrowserControlsStateProvider.getTopControlsMinHeightOffset(),
                             Matchers.is(0));
+                    Criteria.checkThat(
+                            getStatusIndicator().getVisibility(), Matchers.is(View.GONE));
                 });
 
-        Assert.assertEquals(
-                "Wrong Android view visibility.", View.GONE, getStatusIndicator().getVisibility());
         Assert.assertFalse(
                 "Composited view shouldn't be visible.",
                 mStatusIndicatorSceneLayer.isSceneOverlayTreeShowing());
@@ -175,13 +163,10 @@ public class StatusIndicatorTest {
     @Test
     @MediumTest
     public void testShowAfterHide() {
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
-
         TestThreadUtils.runOnUiThreadBlocking(
                 () ->
                         mStatusIndicatorCoordinator.show(
                                 "Status", null, Color.BLACK, Color.WHITE, Color.WHITE));
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
         // Wait until the status indicator finishes animating, or becomes fully visible.
         CriteriaHelper.pollUiThread(
@@ -189,16 +174,11 @@ public class StatusIndicatorTest {
                     Criteria.checkThat(
                             mBrowserControlsStateProvider.getTopControlsMinHeightOffset(),
                             Matchers.is(getStatusIndicator().getHeight()));
+                    Criteria.checkThat(
+                            getStatusIndicator().getVisibility(), Matchers.is(View.VISIBLE));
                 });
 
-        // Now, the Android view should be visible.
-        Assert.assertEquals(
-                "Wrong Android view visibility.",
-                View.VISIBLE,
-                getStatusIndicator().getVisibility());
-
         TestThreadUtils.runOnUiThreadBlocking(mStatusIndicatorCoordinator::hide);
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
         // Wait until the status indicator finishes animating, or becomes fully hidden.
         CriteriaHelper.pollUiThread(
@@ -206,10 +186,10 @@ public class StatusIndicatorTest {
                     Criteria.checkThat(
                             mBrowserControlsStateProvider.getTopControlsMinHeightOffset(),
                             Matchers.is(0));
+                    Criteria.checkThat(
+                            getStatusIndicator().getVisibility(), Matchers.is(View.GONE));
                 });
 
-        Assert.assertEquals(
-                "Wrong Android view visibility.", View.GONE, getStatusIndicator().getVisibility());
         Assert.assertFalse(
                 "Composited view shouldn't be visible.",
                 mStatusIndicatorSceneLayer.isSceneOverlayTreeShowing());
@@ -218,7 +198,6 @@ public class StatusIndicatorTest {
                 () ->
                         mStatusIndicatorCoordinator.show(
                                 "Status", null, Color.BLACK, Color.WHITE, Color.WHITE));
-        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
 
         // Wait until the status indicator finishes animating, or becomes fully visible.
         CriteriaHelper.pollUiThread(
@@ -226,13 +205,9 @@ public class StatusIndicatorTest {
                     Criteria.checkThat(
                             mBrowserControlsStateProvider.getTopControlsMinHeightOffset(),
                             Matchers.is(getStatusIndicator().getHeight()));
+                    Criteria.checkThat(
+                            getStatusIndicator().getVisibility(), Matchers.is(View.VISIBLE));
                 });
-
-        // Now, the Android view should be visible.
-        Assert.assertEquals(
-                "Wrong Android view visibility.",
-                View.VISIBLE,
-                getStatusIndicator().getVisibility());
     }
 
     @Test
