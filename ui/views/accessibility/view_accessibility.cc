8ddb3ed861a244e41421145f393bc30596d13692
Sejal Anand <sejalanand@microsoft.com>
2024-06-21 05:36:21 +0000

    [views_ax] Introducing setters for Heading Level.
    This CL introduces the ViewAccessibility setter for
    Heading Level, with this change heading levels will
    be defined for views.
    
    Bug: https://microsoft.visualstudio.com/Edge/_workitems/edit/37545928
    Change-Id: I6a6e6b4f0d64a1415ae84773cd5be3b8c8621c2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5633106
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Sejal Anand <sejalanand@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1317778}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -958,4 +958,9 @@ void ViewAccessibility::SetDataForClosedWidget(ui::AXNodeData* data) const {
   }
 }
 
+void ViewAccessibility::SetHierarchicalLevel(int hierarchical_level) {
+  data_.AddIntAttribute(ax::mojom::IntAttribute::kHierarchicalLevel,
+                        hierarchical_level);
+}
+
 }  // namespace views
