eb2f13e16beeff2a0a262bc9298059de96ad09b9
Andrew Rayskiy <greengrape@google.com>
2024-06-21 12:54:22 +0000

    [web_package] Introduce IntegrityBlockAttributes
    Bug: 346753548
    Change-Id: I94ce81b98a498c813e7f8dfcea65db6ea62c5eaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645246
    Reviewed-by: Giovanni Pezzino <giovax@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317893}

---
+++ components/web_package/signed_web_bundles/integrity_block_attributes.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/web_package/signed_web_bundles/integrity_block_attributes.h"
+
+namespace web_package {
+
+IntegrityBlockAttributes::IntegrityBlockAttributes(std::string web_bundle_id,
+                                                   std::vector<uint8_t> cbor)
+    : web_bundle_id_(std::move(web_bundle_id)), cbor_(std::move(cbor)) {}
+
+IntegrityBlockAttributes::~IntegrityBlockAttributes() = default;
+
+IntegrityBlockAttributes::IntegrityBlockAttributes(
+    mojo::DefaultConstruct::Tag) {}
+
+IntegrityBlockAttributes::IntegrityBlockAttributes(
+    const IntegrityBlockAttributes&) = default;
+IntegrityBlockAttributes& IntegrityBlockAttributes::operator=(
+    const IntegrityBlockAttributes&) = default;
+
+}  // namespace web_package
