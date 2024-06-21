751650dcd3c52d7a0d5b6c79074a1f13748640bc
Alexis Hetu <sugoi@chromium.org>
2024-06-21 21:03:31 +0000

    [iOS] New passkey deletion EG test
    This CL adds a new password manager test which verifies that
    passkey deletion works properly.
    
    Also refined the credential matching criteria to prevent matching
    the wrong passkey.
    
    Bug: 330355124
    Change-Id: I13fffcd17ae1bc57ed62e98224a17f92053df830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647526
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318157}

--- ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.mm
+++ ios/chrome/browser/ui/settings/password/password_details/password_details_mediator.mm
@@ -48,14 +48,18 @@ using password_manager::CredentialUIEntry;
 
 namespace {
 
-bool MatchesRealmUsernameAndPassword(CredentialDetails* credentialDetails,
-                                     const CredentialUIEntry& credential) {
+bool MatchesRealmUsernamePasswordAndCreationTime(
+    CredentialDetails* credentialDetails,
+    const CredentialUIEntry& credential) {
   return base::SysNSStringToUTF8(credentialDetails.signonRealm) ==
              credential.GetFirstSignonRealm() &&
          base::SysNSStringToUTF16(credentialDetails.username) ==
              credential.username &&
          base::SysNSStringToUTF16(credentialDetails.password) ==
-             credential.password;
+             credential.password &&
+         base::SysNSStringToUTF16(credentialDetails.userDisplayName) ==
+             credential.user_display_name &&
+         credentialDetails.creationTime == credential.creation_time;
 }
 
 // Whether displaying a credential as compromised is supported in the current
@@ -264,7 +268,8 @@ bool AreMatchingCredentials(const CredentialUIEntry& credential,
   // Map from CredentialDetails to CredentialUIEntry. Should support blocklists.
   auto it = base::ranges::find_if(
       _credentials, [credentialDetails](const CredentialUIEntry& credential) {
-        return MatchesRealmUsernameAndPassword(credentialDetails, credential);
+        return MatchesRealmUsernamePasswordAndCreationTime(credentialDetails,
+                                                           credential);
       });
   if (it == _credentials.end()) {
     // TODO(crbug.com/40862365): Convert into DCHECK.
@@ -292,7 +297,8 @@ bool AreMatchingCredentials(const CredentialUIEntry& credential,
   // Map from CredentialDetails to CredentialUIEntry.
   auto it = base::ranges::find_if(
       _credentials, [credentialDetails](const CredentialUIEntry& credential) {
-        return MatchesRealmUsernameAndPassword(credentialDetails, credential);
+        return MatchesRealmUsernamePasswordAndCreationTime(credentialDetails,
+                                                           credential);
       });
 
   if (it == _credentials.end()) {
@@ -310,7 +316,8 @@ bool AreMatchingCredentials(const CredentialUIEntry& credential,
     (CredentialDetails*)credentialDetails {
   auto localCredential = base::ranges::find_if(
       _credentials, [credentialDetails](const CredentialUIEntry& credential) {
-        return MatchesRealmUsernameAndPassword(credentialDetails, credential);
+        return MatchesRealmUsernamePasswordAndCreationTime(credentialDetails,
+                                                           credential);
       });
   std::optional<CredentialUIEntry> accountCredential =
       [self conflictingAccountPassword:credentialDetails];
@@ -335,7 +342,8 @@ bool AreMatchingCredentials(const CredentialUIEntry& credential,
   auto it = base::ranges::find_if(
       _credentials, [credentialDetails](
                         const password_manager::CredentialUIEntry& credential) {
-        return MatchesRealmUsernameAndPassword(credentialDetails, credential);
+        return MatchesRealmUsernamePasswordAndCreationTime(credentialDetails,
+                                                           credential);
       });
 
   if (it == _credentials.end()) {
