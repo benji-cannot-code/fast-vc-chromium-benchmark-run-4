2c0ac81fa3ab4bb3aa2a4ec34c2772152d1c4ccf
Andrii Natiahlyi <natiahlyi@google.com>
2024-06-21 13:18:30 +0000

    Remove obsolete passwords import TODOs
    This CL includes:
    - Updated PasswordManager.ImportDuration histogram description.
    - Start using `password_manager/core/common:constants` in the importer.
    
    Fixed: 40894187
    
    Change-Id: I85a83328a96686b3da5e32ef47464386884e3fb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643971
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1317902}

--- components/password_manager/core/browser/import/password_importer.cc
+++ components/password_manager/core/browser/import/password_importer.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/ui/credential_ui_entry.h"
 #include "components/password_manager/core/browser/ui/credential_utils.h"
 #include "components/password_manager/core/browser/ui/saved_passwords_presenter.h"
+#include "components/password_manager/core/common/password_manager_constants.h"
 #include "components/password_manager/services/csv_password/csv_password_parser_service.h"
 
 using password_manager::ImportEntry;
@@ -145,7 +146,8 @@ CSVPasswordToCredentialUIEntry(const CSVPassword& csv_password,
   auto with_status = [&](ImportEntry::Status status) {
     ImportEntry entry;
     entry.status = status;
-    // TODO(crbug.com/40894187): Use password_manager::GetShownOrigin.
+    // The raw URL is shown in the errors list in the UI to make it easier to
+    // match the listed entry with the one in the CSV file.
     auto url = csv_password.GetURL();
     entry.url = url.has_value() ? url.value().spec() : url.error();
     entry.username = csv_password.GetUsername();
@@ -231,7 +233,8 @@ std::vector<PasswordForm> GetMatchingPasswordForms(
 std::u16string ComputeNotesConcatenation(const std::u16string& local_note,
                                          const std::u16string& imported_note,
                                          NotesImportMetrics& metrics) {
-  CHECK_LE(imported_note.size(), PasswordImporter::MAX_NOTE_LENGTH);
+  CHECK_LE(imported_note.size(),
+           static_cast<unsigned>(constants::kMaxPasswordNoteLength));
 
   if (imported_note.empty()) {
     return local_note;
@@ -271,7 +274,7 @@ void MergeNotesOrReportError(const std::vector<PasswordForm>& local_forms,
   const std::u16string concatenation =
       ComputeNotesConcatenation(local_note, imported_note, metrics);
 
-  if (concatenation.size() > PasswordImporter::MAX_NOTE_LENGTH) {
+  if (concatenation.size() > constants::kMaxPasswordNoteLength) {
     // Notes concatenation size should not exceed 1000 characters.
     results.displayed_entries.push_back(CreateFailedImportEntry(
         imported_credential, ImportEntry::Status::LONG_CONCATENATED_NOTE));
@@ -331,10 +334,6 @@ void ReportImportResultsMetrics(const ImportResults& results,
                                   entry.status);
   }
 
-  // TODO(crbug.com/40894187): In case of conflicts, with `kPasswordsImportM2`
-  // enabled, `PasswordManager.ImportDuration` will be affected. Because, the
-  // flow is interrupted and the user needs to select the next action in the UI.
-  // Reporting or the histogram description needs to be updated accordingly.
   base::UmaHistogramLongTimes("PasswordManager.ImportDuration",
                               base::Time::Now() - start_time);
 
@@ -382,7 +381,8 @@ void ProcessParsedCredential(
   if (conflicting_credential.has_value()) {
     std::vector<PasswordForm> forms = GetMatchingPasswordForms(
         presenter, conflicting_credential.value(), to_store);
-    // TODO(crbug.com/40894187): Add conflicts check for notes.
+    // Password notes are not taken into account when conflicting passwords
+    // are overwritten. Only the local note is persisted.
     for (PasswordForm& form : forms) {
       form.password_value = imported_credential.password;
     }
@@ -509,7 +509,7 @@ void PasswordImporter::ConsumePasswords(
     std::move(results_callback).Run(std::move(results));
     return;
   }
-  if (seq->csv_passwords.size() > MAX_PASSWORDS_PER_IMPORT) {
+  if (seq->csv_passwords.size() > constants::kMaxPasswordsPerCSVFile) {
     results.status =
         password_manager::ImportResults::Status::NUM_PASSWORDS_EXCEEDED;
 
