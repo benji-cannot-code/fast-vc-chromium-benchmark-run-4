40eb5c2c8e39da101cafcf965cd79a99906c7271
Liviu Tinta <liviutinta@chromium.org>
2024-06-21 21:01:33 +0000

    Add X-Client-Data header to prefetch requests if needed
    Normal navigations add X-Client-Data request header for eligible URLs
    to inform server-side behavior when not in incognito mode.
    
    This CL adds the same functionality to prefetch requests.
    
    Bug: 347223102
    Change-Id: I448146b5bea99661d8bf5281a5d36d5c5a43bf8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5642198
    Commit-Queue: Liviu Tinta <liviutinta@chromium.org>
    Reviewed-by: Iman Saboori <isaboori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318156}

--- content/browser/preloading/prefetch/prefetch_service_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_service_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/elapsed_timer.h"
 #include "components/ukm/test_ukm_recorder.h"
+#include "components/variations/scoped_variations_ids_provider.h"
 #include "content/browser/preloading/prefetch/mock_prefetch_service_delegate.h"
 #include "content/browser/preloading/prefetch/prefetch_container.h"
 #include "content/browser/preloading/prefetch/prefetch_document_manager.h"
@@ -1094,6 +1095,9 @@ class PrefetchServiceTest : public RenderViewHostTestHarness {
   std::unique_ptr<base::ScopedMockElapsedTimersForTest> scoped_test_timer_;
 
   std::vector<PrefetchRequestHandler> request_handler_keep_alive_;
+
+  variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
+      variations::VariationsIdsProvider::Mode::kIgnoreSignedInState};
 };
 
 TEST_F(PrefetchServiceTest, SuccessCase) {
--- content/browser/preloading/prefetch/prefetch_url_loader_interceptor_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_url_loader_interceptor_unittest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "base/timer/elapsed_timer.h"
 #include "components/ukm/test_ukm_recorder.h"
+#include "components/variations/scoped_variations_ids_provider.h"
 #include "content/browser/loader/response_head_update_params.h"
 #include "content/browser/preloading/prefetch/prefetch_container.h"
 #include "content/browser/preloading/prefetch/prefetch_features.h"
@@ -531,6 +532,9 @@ class PrefetchURLLoaderInterceptorTestBase : public RenderViewHostTestHarness {
   std::unique_ptr<base::ScopedMockElapsedTimersForTest> scoped_test_timer_;
   // Disable sampling of UKM preloading logs.
   content::test::PreloadingConfigOverride preloading_config_override_;
+
+  variations::ScopedVariationsIdsProvider scoped_variations_ids_provider_{
+      variations::VariationsIdsProvider::Mode::kIgnoreSignedInState};
 };
 
 namespace {
