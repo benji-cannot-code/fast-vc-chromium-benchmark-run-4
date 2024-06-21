e2d79795df18c080d743387b471e3c8b57bedc3d
Dorah <dorahkim@google.com>
2024-06-21 10:46:00 +0000

    CCA: Attach Compute Pressure API to perfLogger
    This CL attaches Compute Pressure API to PerfLogger. In PerfLogger
    constructor, it starts a pressure observer and manages this using a map.
    
    BUG=b:340753645
    Test=Check performance log.
    
    Change-Id: Ieca11b5428047c2d166b1a93afeeaeb8dbbcca57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5561930
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1317859}

--- ash/webui/camera_app_ui/resources/js/app_window.ts
+++ ash/webui/camera_app_ui/resources/js/app_window.ts
@@ -107,6 +107,7 @@ export class AppWindow {
     this.perfs.push({
       event: event,
       duration: (performance.now() - this.launchedTime),
+      perfInfo: {},
     });
   }
 
--- ash/webui/camera_app_ui/resources/js/type.ts
+++ ash/webui/camera_app_ui/resources/js/type.ts
@@ -277,6 +277,13 @@ export enum PerfEvent {
   VIDEO_CAPTURE_POST_PROCESSING_SAVING = 'video-capture-post-processing-saving',
 }
 
+export enum Pressure {
+  NOMINAL,
+  FAIR,
+  SERIOUS,
+  CRITICAL,
+}
+
 export interface ImageBlob {
   blob: Blob;
   resolution: Resolution;
@@ -290,14 +297,14 @@ export interface PerfInformation {
   hasError?: boolean;
   resolution?: Resolution;
   facing?: Facing;
-  // Only for DOCUMENT_PDF_SAVING
-  pageCount?: number;
+  pageCount?: number;  // Only for DOCUMENT_PDF_SAVING
+  pressure?: Pressure;
 }
 
 export interface PerfEntry {
   event: PerfEvent;
   duration: number;
-  perfInfo?: PerfInformation;
+  perfInfo: PerfInformation;
 }
 
 export interface VideoTrackSettings {
