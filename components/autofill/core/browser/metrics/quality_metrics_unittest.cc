7d364efa7180960bca7f8d7843204995524ec9c3
Jihad Hanna <jihadghanna@google.com>
2024-06-21 17:17:14 +0000

    Rename RationalizationQuality metric
    The current metric name and description give no indication of what logic
    the following metric is evaluating and what the very generic bucket
    names signify.
    
    Bug: 40100455
    Change-Id: I844bbbee286a781c8b97e4feaa68bd5c99e630ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5600755
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318021}

--- components/autofill/core/browser/metrics/quality_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/quality_metrics_unittest.cc
@@ -170,7 +170,7 @@ TEST_F(QualityMetricsTest, QualityMetrics) {
 
 // Test that we log quality metrics appropriately with fields having
 // only_fill_when_focused and are supposed to log RATIONALIZATION_OK.
-TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationOk) {
+TEST_F(QualityMetricsTest, LoggedCorrectlyForRationalizationOk) {
   FormData form = CreateForm(
       {CreateTestFormField("Name", "name", "Elvis Aaron Presley",
                            FormControlType::kInputText),
@@ -216,7 +216,7 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationOk) {
   // Rationalization quality.
   {
     std::string rationalization_histogram =
-        "Autofill.RationalizationQuality.PhoneNumber";
+        "Autofill.Rationalization.OnlyFillWhenFocused.Quality";
     // RATIONALIZATION_OK is logged 3 times.
     histogram_tester.ExpectBucketCount(rationalization_histogram,
                                        AutofillMetrics::RATIONALIZATION_OK, 3);
@@ -225,7 +225,7 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationOk) {
 
 // Test that we log quality metrics appropriately with fields having
 // only_fill_when_focused and are supposed to log RATIONALIZATION_GOOD.
-TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationGood) {
+TEST_F(QualityMetricsTest, LoggedCorrectlyForRationalizationGood) {
   FormData form = CreateForm(
       {CreateTestFormField("Name", "name", "Elvis Aaron Presley",
                            FormControlType::kInputText),
@@ -255,7 +255,7 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationGood) {
   // Rationalization quality.
   {
     std::string rationalization_histogram =
-        "Autofill.RationalizationQuality.PhoneNumber";
+        "Autofill.Rationalization.OnlyFillWhenFocused.Quality";
     // RATIONALIZATION_GOOD is logged once.
     histogram_tester.ExpectBucketCount(
         rationalization_histogram, AutofillMetrics::RATIONALIZATION_GOOD, 1);
@@ -264,7 +264,7 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationGood) {
 
 // Test that we log quality metrics appropriately with fields having
 // only_fill_when_focused and are supposed to log RATIONALIZATION_BAD.
-TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationBad) {
+TEST_F(QualityMetricsTest, LoggedCorrectlyForRationalizationBad) {
   FormData form = CreateForm({
       CreateTestFormField("Name", "name", "Elvis Aaron Presley",
                           FormControlType::kInputText),
@@ -296,19 +296,15 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForRationalizationBad) {
   base::HistogramTester histogram_tester;
   SubmitForm(form);
 
-  // Rationalization quality.
-  {
-    std::string rationalization_histogram =
-        "Autofill.RationalizationQuality.PhoneNumber";
-    // RATIONALIZATION_BAD is logged once.
-    histogram_tester.ExpectBucketCount(rationalization_histogram,
-                                       AutofillMetrics::RATIONALIZATION_BAD, 1);
-  }
+  // RATIONALIZATION_BAD is logged once.
+  histogram_tester.ExpectBucketCount(
+      "Autofill.Rationalization.OnlyFillWhenFocused.Quality",
+      AutofillMetrics::RATIONALIZATION_BAD, 1);
 }
 
 // Test that we log quality metrics appropriately with fields having
 // only_fill_when_focused set to true.
-TEST_F(QualityMetricsTest, LoggedCorrecltyForOnlyFillWhenFocusedField) {
+TEST_F(QualityMetricsTest, LoggedCorrectlyForOnlyFillWhenFocusedField) {
   FormData form = CreateForm(
       {// TRUE_POSITIVE + no rationalization logging
        CreateTestFormField("Name", "name", "Elvis Aaron Presley",
@@ -362,7 +358,7 @@ TEST_F(QualityMetricsTest, LoggedCorrecltyForOnlyFillWhenFocusedField) {
 
   // Rationalization quality.
   EXPECT_THAT(histogram_tester.GetAllSamples(
-                  "Autofill.RationalizationQuality.PhoneNumber"),
+                  "Autofill.Rationalization.OnlyFillWhenFocused.Quality"),
               BucketsAre(Bucket(AutofillMetrics::RATIONALIZATION_GOOD, 1),
                          Bucket(AutofillMetrics::RATIONALIZATION_OK, 1),
                          Bucket(AutofillMetrics::RATIONALIZATION_BAD, 1)));
