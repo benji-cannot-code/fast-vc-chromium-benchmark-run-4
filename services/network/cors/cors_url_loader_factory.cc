479b93f6c7c6254c4da31ab7d18d671f384c3784
Yoichi Osato <yoichio@chromium.org>
2024-06-24 05:09:35 +0000

    Remove NetworkService.InitializedTime histograms.
    This CL removes the histograms no longer needed.
    
    OBSOLETE_HISTOGRAMS=Removed.
    
    Bug: 1439721, 347710725
    Change-Id: I8669b7dbbe08848adf49b1766c2c8087fd383dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644656
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318428}

--- services/network/cors/cors_url_loader_factory.cc
+++ services/network/cors/cors_url_loader_factory.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/crash_logging.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/trace_event/typed_macros.h"
 #include "base/types/optional_util.h"
 #include "mojo/public/cpp/bindings/message.h"
@@ -329,13 +328,6 @@ void CorsURLLoaderFactory::CreateLoaderAndStart(
     const net::MutableNetworkTrafficAnnotationTag& traffic_annotation) {
   TRACE_EVENT("loading", "CorsURLLoaderFactory::CreateLoaderAndStart",
               perfetto::Flow::FromPointer(this));
-#if BUILDFLAG(IS_ANDROID)
-  // Use pseudo flag to investigate histogram issue.
-  // See https://crbug.com/1439721.
-  const bool observed = true;
-  base::UmaHistogramBoolean("NetworkService.CorsURLLoaderFactoryStart",
-                            observed);
-#endif
 
   debug::ScopedResourceRequestCrashKeys request_crash_keys(resource_request);
   SCOPED_CRASH_KEY_NUMBER("net", "traffic_annotation_hash",
