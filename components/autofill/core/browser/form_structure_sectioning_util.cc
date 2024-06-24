2c8fc05f1a0078066509f2db44a18d76992f0ccc
Norge Vizcay <vizcay@google.com>
2024-06-24 12:04:32 +0000

    Fix unsafe buffer warnings in autofill sectioning util.
    Bug: 347137620
    Change-Id: I3b2a0050776fe47847e20110ffb1f1ac287fa794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650761
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318512}

--- components/autofill/core/browser/form_structure_sectioning_util.cc
+++ components/autofill/core/browser/form_structure_sectioning_util.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/341324165): Fix and remove.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/autofill/core/browser/form_structure_sectioning_util.h"
 
 #include <iterator>
@@ -209,7 +204,9 @@ void AssignSections(base::span<const std::unique_ptr<AutofillField>> fields) {
     begin = FindBeginOfNextSection(begin, fields.end());
     auto end = FindEndOfNextSection(begin, fields.end());
     DCHECK(begin != end || end == fields.end());
-    AssignFieldIdentifierSections({begin, end}, frame_token_ids);
+    // SAFETY: The iterators are from the same container.
+    AssignFieldIdentifierSections(UNSAFE_BUFFERS({begin, end}),
+                                  frame_token_ids);
     begin = end;
   }
 }
