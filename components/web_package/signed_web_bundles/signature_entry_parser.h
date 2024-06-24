83b2da91d47e77a6581e262f1582939422e969b2
Andrew Rayskiy <greengrape@google.com>
2024-06-24 18:47:57 +0000

    [web_package] Preliminary steps for Integrity Block v2
    * Support IntegrityBlockAttributes in WebBundleSigner
    * Move MagicBytes and VersionMagicBytes to constants.h
    * Restructure some bits in IntegrityBlockParser to allow incorporating
    v2 logic
    
    Bug: 346753548
    Change-Id: Ide6ec498474049e0389fe7d1b910a21bf9e502d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630623
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Giovanni Pezzino <giovax@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318701}

--- components/web_package/signed_web_bundles/signature_entry_parser.h
+++ components/web_package/signed_web_bundles/signature_entry_parser.h
@@ -10,11 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/types/expected.h"
 #include "components/web_package/mojom/web_bundle_parser.mojom.h"
+#include "components/web_package/signed_web_bundles/attribute_map_parser.h"
 #include "components/web_package/signed_web_bundles/types.h"
 
 namespace web_package {
-
-class AttributeMapParser;
 
 // This class is responsible for parsing a single signature entry from the
 // signature stack of the integrity block of a signed web bundle.
@@ -37,8 +36,7 @@ class SignatureStackEntryParser {
 
  private:
   void ReadSignatureStructure(const std::optional<BinaryData>& data);
-  void GetAttributesMap(
-      base::expected<std::pair<AttributesMap, uint64_t>, std::string> result);
+  void GetAttributesMap(AttributeMapParser::ParsingResult result);
   void ReadAttributesMapBytes(uint64_t num_bytes,
                               const std::optional<BinaryData>& data);
   void ReadSignatureHeader(const std::optional<BinaryData>& data);
