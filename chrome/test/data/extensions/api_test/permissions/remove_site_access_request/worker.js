89df9182ac3dcc9d51473e7649b6c4da363add84
EmiliaPaz <emiliapaz@chromium.org>
2024-06-24 20:00:26 +0000

    [Extensions c2s] Return error when removing a non-existent request
    Return an error when permissions.removeSiteAccessRequest did not
    remove the request, which happens when the request is non-existent.
    
    Bug: 330588494
    Change-Id: I865faf227c6157b6693673e78f6d9a5d0b6b5b3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643878
    Reviewed-by: Tim <tjudkins@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318751}

--- chrome/test/data/extensions/api_test/permissions/remove_site_access_request/worker.js
+++ chrome/test/data/extensions/api_test/permissions/remove_site_access_request/worker.js
@@ -3,6 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {openTab} from '/_test_resources/test_util/tabs_util.js';
+
+// Navigates to a url with `origin`.
+async function navigateTo(origin) {
+  const config = await chrome.test.getConfig();
+  const url = `http://${origin}:${config.testServer.port}/simple.html`;
+  const tab = await openTab(url);
+  return tab;
+}
+
 chrome.test.runTests([
   // Tests that an error is returned when request does not include neither
   // documentId or tabId.
@@ -44,6 +54,19 @@ chrome.test.runTests([
     await chrome.test.assertPromiseRejects(
         chrome.permissions.removeSiteAccessRequest(request),
         `Error: No document with ID '${documentId}'.`);
+
+    chrome.test.succeed();
+  },
+
+  // Tests that an error is returned when the request cannot be removed since
+  // the request doesn't exist.
+  async function nonExistentRequest() {
+    const tab = await navigateTo('example.com');
+    const request = {tabId: tab.id};
+    await chrome.test.assertPromiseRejects(
+        chrome.permissions.removeSiteAccessRequest(request),
+        `Error: Extension cannot remove a site access request that doesn't ` +
+            `exist.`);
 
     chrome.test.succeed();
   }
