48f2f46fd69e107ded5cb911488cc7d50fb44a85
Jing Wang <jiwan@chromium.org>
2024-06-24 04:01:16 +0000

    picker: add is_recent field in clipboard item
    This will be used for search results reranking.
    
    Also remove the recency filter in FetchResults api because no one is
    using it.
    
    Bug: b:325870201
    Change-Id: I3f072bfaa708a80f3bdcc2876dad4db13f8e0c90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647453
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318420}

--- ash/picker/picker_clipboard_provider.cc
+++ ash/picker/picker_clipboard_provider.cc
@@ -12,6 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 namespace {
+
+constexpr base::TimeDelta kRecencyThreshold = base::Seconds(60);
+
 std::optional<PickerSearchResult::ClipboardData::DisplayFormat>
 GetDisplayFormat(crosapi::mojom::ClipboardHistoryDisplayFormat format) {
   switch (format) {
@@ -49,27 +52,22 @@ PickerClipboardProvider::PickerClipboardProvider(base::Clock* clock)
 PickerClipboardProvider::~PickerClipboardProvider() = default;
 
 void PickerClipboardProvider::FetchResults(OnFetchResultsCallback callback,
-                                           const std::u16string& query,
-                                           base::TimeDelta recency) {
+                                           const std::u16string& query) {
   ash::ClipboardHistoryController* clipboard_history_controller =
       ash::ClipboardHistoryController::Get();
   if (clipboard_history_controller) {
     clipboard_history_controller->GetHistoryValues(base::BindOnce(
         &PickerClipboardProvider::OnFetchHistory,
-        weak_ptr_factory_.GetWeakPtr(), std::move(callback), query, recency));
+        weak_ptr_factory_.GetWeakPtr(), std::move(callback), query));
   }
 }
 
 void PickerClipboardProvider::OnFetchHistory(
     OnFetchResultsCallback callback,
     const std::u16string& query,
-    base::TimeDelta recency,
     std::vector<ClipboardHistoryItem> items) {
   std::vector<PickerSearchResult> results;
   for (const auto& item : items) {
-    if ((clock_->Now() - item.time_copied()) > recency) {
-      continue;
-    }
     if (!MatchQuery(item, query)) {
       continue;
     }
@@ -77,8 +75,8 @@ void PickerClipboardProvider::OnFetchHistory(
             display_format = GetDisplayFormat(item.display_format());
         display_format.has_value()) {
       results.push_back(PickerSearchResult::Clipboard(
-          item.id(), *display_format, item.display_text(),
-          item.display_image()));
+          item.id(), *display_format, item.display_text(), item.display_image(),
+          (clock_->Now() - item.time_copied()) < kRecencyThreshold));
     }
   }
   std::move(callback).Run(std::move(results));
--- ash/public/cpp/picker/picker_search_result.cc
+++ ash/public/cpp/picker/picker_search_result.cc
@@ -51,11 +51,13 @@ PickerSearchResult::ClipboardData::ClipboardData(
     base::UnguessableToken item_id,
     DisplayFormat display_format,
     std::u16string display_text,
-    std::optional<ui::ImageModel> display_image)
+    std::optional<ui::ImageModel> display_image,
+    bool is_recent)
     : item_id(item_id),
       display_format(display_format),
       display_text(std::move(display_text)),
-      display_image(std::move(display_image)) {}
+      display_image(std::move(display_image)),
+      is_recent(is_recent) {}
 
 PickerSearchResult::ClipboardData::ClipboardData(
     const PickerSearchResult::ClipboardData&) = default;
@@ -215,10 +217,11 @@ PickerSearchResult PickerSearchResult::Clipboard(
     base::UnguessableToken item_id,
     ClipboardData::DisplayFormat display_format,
     std::u16string display_text,
-    std::optional<ui::ImageModel> display_image) {
+    std::optional<ui::ImageModel> display_image,
+    bool is_recent) {
   return PickerSearchResult(ClipboardData(item_id, display_format,
                                           std::move(display_text),
-                                          std::move(display_image)));
+                                          std::move(display_image), is_recent));
 }
 
 PickerSearchResult PickerSearchResult::Gif(const GURL& preview_url,
