796d2e1a8b00852f68f4de1dbccd98f516963675
Evan Stade <estade@chromium.org>
2024-06-24 17:25:12 +0000

    Enable clang-tidy's include checker for componentized IDB code.
    Bug: none
    Change-Id: Ib862847affce34794e2c9832b122e2a579dc9fe2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5648542
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318649}

--- components/services/storage/indexed_db/scopes/leveldb_scopes.h
+++ components/services/storage/indexed_db/scopes/leveldb_scopes.h
@@ -7,11 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SERVICES_STORAGE_INDEXED_DB_SCOPES_LEVELDB_SCOPES_H_
 
 #include <stdint.h>
-#include <limits>
 #include <list>
 #include <memory>
 #include <string>
-#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -19,9 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/numerics/checked_math.h"
 #include "base/sequence_checker.h"
-#include "base/task/sequenced_task_runner.h"
 #include "components/services/storage/indexed_db/locks/partitioned_lock.h"
 #include "components/services/storage/indexed_db/locks/partitioned_lock_id.h"
 #include "components/services/storage/indexed_db/scopes/leveldb_scopes_coding.h"
