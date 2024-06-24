d501d0688856af4d36d477804da12b727d46c530
Alex Yang <aycyang@chromium.org>
2024-06-24 22:27:05 +0000

    [CodeHealth] Remove RAW_PTR_EXCLUSION from test IPC::Listeners
    Bug: 346694204, 346693740
    Change-Id: I299e619cdc43bb59d954f0c059f3688161ebe706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650558
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1318837}

--- ipc/ipc_channel_proxy_unittest.cc
+++ ipc/ipc_channel_proxy_unittest.cc
@@ -3,20 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
-#include "build/build_config.h"
-
 #include <stddef.h>
 #include <stdint.h>
 
 #include <memory>
 
+#include "base/memory/raw_ptr.h"
 #include "base/message_loop/message_pump_type.h"
 #include "base/pickle.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/thread.h"
+#include "build/build_config.h"
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_test_base.h"
 #include "ipc/message_filter.h"
@@ -50,7 +48,7 @@ namespace IPC {
 
 namespace {
 
-void CreateRunLoopAndRun(base::RunLoop** run_loop_ptr) {
+void CreateRunLoopAndRun(raw_ptr<base::RunLoop>* run_loop_ptr) {
   base::RunLoop run_loop;
   *run_loop_ptr = &run_loop;
   run_loop.Run();
@@ -87,8 +85,7 @@ class QuitListener : public IPC::Listener {
 
   bool bad_message_received_ = false;
   bool quit_message_received_ = false;
-  // RAW_PTR_EXCLUSION: #addr-of
-  RAW_PTR_EXCLUSION base::RunLoop* run_loop_ = nullptr;
+  raw_ptr<base::RunLoop> run_loop_ = nullptr;
 };
 
 class ChannelReflectorListener : public IPC::Listener {
@@ -130,8 +127,7 @@ class ChannelReflectorListener : public IPC::Listener {
     run_loop_->QuitWhenIdle();
   }
 
-  // RAW_PTR_EXCLUSION: #addr-of
-  RAW_PTR_EXCLUSION base::RunLoop* run_loop_ = nullptr;
+  raw_ptr<base::RunLoop> run_loop_ = nullptr;
 
  private:
   raw_ptr<IPC::Channel> channel_ = nullptr;
