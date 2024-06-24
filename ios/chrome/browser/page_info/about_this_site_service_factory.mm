fcc187db621d4465dc62a875a37d2912125d9507
Filipa Senra <fsenra@google.com>
2024-06-24 13:02:08 +0000

    [iOS][PageInfo] Add metric to record AboutThisPage language availability
    Bug: b:337008689
    Change-Id: I9698b2ca9aa32f4f387e54c39e648e85ad44d951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645102
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318528}

--- ios/chrome/browser/page_info/about_this_site_service_factory.mm
+++ ios/chrome/browser/page_info/about_this_site_service_factory.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/page_info/about_this_site_service_factory.h"
 
+#import "base/metrics/histogram_functions.h"
 #import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "components/page_info/core/about_this_site_service.h"
 #import "components/page_info/core/features.h"
@@ -41,8 +42,14 @@ AboutThisSiteServiceFactory::~AboutThisSiteServiceFactory() = default;
 std::unique_ptr<KeyedService>
 AboutThisSiteServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  if (!page_info::IsAboutThisSiteFeatureEnabled(
-          GetApplicationContext()->GetApplicationLocale())) {
+  const bool is_about_this_site_language_supported =
+      page_info::IsAboutThisSiteFeatureEnabled(
+          GetApplicationContext()->GetApplicationLocale());
+
+  base::UmaHistogramBoolean("Security.PageInfo.AboutThisSiteLanguageSupported",
+                            is_about_this_site_language_supported);
+
+  if (!is_about_this_site_language_supported) {
     return nullptr;
   }
 
