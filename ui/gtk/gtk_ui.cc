e280dd4bc05c4b263b67fae6fb6993c62d763b46
Orko Garai <orko@igalia.com>
2024-06-24 20:42:09 +0000

    Add switch to disable GTK IME
    GTK IME is not fully reliable on wayland due to several issues there.
    It is used by default in GTK UI for x11 always and for wayland in
    GTK4.
    
    Add a switch that gives users the ability to turn off GTK IME
    integration when GTK UI is used. This can be particularly useful for
    ozone/wayland until some of the issues are resolved or a more stable IME
    solution is made available, e.g. a newer text-input protocol is implemented.
    
    Bug: 41495892
    Change-Id: I1f35a06461cb61d7feb3adee851211bd4348fab7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631104
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1318773}

--- ui/gtk/gtk_ui.cc
+++ ui/gtk/gtk_ui.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ime/linux/linux_input_method_context.h"
 #include "ui/base/ime/text_input_flags.h"
 #include "ui/base/ozone_buildflags.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/color/color_id.h"
 #include "ui/color/color_provider.h"
 #include "ui/color/color_provider_key.h"
@@ -505,6 +506,10 @@ void GtkUi::SetWindowFrameAction(WindowFrameActionSource source,
 
 std::unique_ptr<ui::LinuxInputMethodContext> GtkUi::CreateInputMethodContext(
     ui::LinuxInputMethodContextDelegate* delegate) const {
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableGtkIme)) {
+    return nullptr;
+  }
   return GetPlatform()->CreateInputMethodContext(delegate);
 }
 
