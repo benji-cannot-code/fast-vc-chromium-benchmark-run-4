550593b370f885ae91060e1c73b0ea9e76453e7f
Abigail Klein <abigailbklein@google.com>
2024-06-24 23:21:58 +0000

    Add metrics for main node annotations feature.
    Bug: 327248734
    Change-Id: I2e0c3f1b38fe5b9da28d77e7b5623f2bc1cf6a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645384
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Abigail Klein <abigailbklein@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318861}

--- content/renderer/accessibility/annotations/ax_main_node_annotator.cc
+++ content/renderer/accessibility/annotations/ax_main_node_annotator.cc
@@ -7,12 +7,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/metrics/histogram_functions.h"
 #include "third_party/blink/public/common/browser_interface_broker_proxy.h"
 
 namespace content {
 
 using blink::WebAXObject;
 using blink::WebDocument;
+
+namespace {
+
+const char kHistogramsName[] =
+    "Accessibility.MainNodeAnnotations.AnnotationResult";
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class MainNodeAnnotationResult {
+  kSuccess = 0,
+  kInvalid = 1,
+  kDuplicate = 2,
+  kMaxValue = kDuplicate,
+};
+
+}  // namespace
 
 AXMainNodeAnnotator::AXMainNodeAnnotator(
     RenderAccessibilityImpl* const render_accessibility)
@@ -105,19 +122,27 @@ void AXMainNodeAnnotator::ProcessScreen2xResult(const WebDocument& document,
                                                 ui::AXNodeID main_node_id) {
   // If Screen2x returned an invalid main node id, do nothing.
   if (main_node_id == ui::kInvalidAXNodeID) {
+    base::UmaHistogramEnumeration(kHistogramsName,
+                                  MainNodeAnnotationResult::kInvalid);
     return;
   }
   // If the main node id was already set, do nothing.
   if (main_node_id_ != ui::kInvalidAXNodeID) {
+    base::UmaHistogramEnumeration(kHistogramsName,
+                                  MainNodeAnnotationResult::kDuplicate);
     return;
   }
   WebAXObject object = WebAXObject::FromWebDocumentByID(document, main_node_id);
   // If the tree has changed, do nothing.
   if (!object.IsIncludedInTree()) {
+    base::UmaHistogramEnumeration(kHistogramsName,
+                                  MainNodeAnnotationResult::kInvalid);
     return;
   }
   main_node_id_ = main_node_id;
   render_accessibility_->MarkWebAXObjectDirty(object);
+  base::UmaHistogramEnumeration(kHistogramsName,
+                                MainNodeAnnotationResult::kSuccess);
 }
 
 void AXMainNodeAnnotator::ComputeAuthorStatus(ui::AXTreeUpdate* update) {
