c3b3547700da0a160445274c82ecb92f214283d9
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-06-24 16:51:42 +0000

    Move ReleaseLockedImages to CanvasResourceProvider::FlushCanvas
    The main motivation for this change is to remove the call to
    ReleaseLockedImages from Canvas2DLayerBridge::FlushRecording, since we
    plan on deleting the whole Canvas2DLayerBridge class. The plan is for
    flushes to be done by calling CanvasResourceProvider::FlushCanvas,
    but since that function doesn't release locked images, we to add that
    logic.
    
    Currently, locked images aren't released consistently on all flushes
    because not all calls to FlushCanvas have a corresponding call to
    ReleaseLockedImages. This CL uniformises this by moving
    ReleaseLockedImages to a centralized location that applies to all
    flushes.
    
    Bug: 40280152
    Change-Id: Ice532733969b4895f05d44cf32e3a2226721ab8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637530
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318628}

--- third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
+++ third_party/blink/renderer/modules/canvas/offscreencanvas2d/offscreen_canvas_rendering_context_2d.cc
@@ -146,7 +146,6 @@ void OffscreenCanvasRenderingContext2D::FlushRecording(FlushReason reason) {
   }
 
   provider->FlushCanvas(reason);
-  provider->ReleaseLockedImages();
 }
 
 void OffscreenCanvasRenderingContext2D::FinalizeFrame(FlushReason reason) {
