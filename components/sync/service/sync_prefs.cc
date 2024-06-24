31a7a088941a87f60a221f51f43356891dcb4df9
Mikel Astiz <mastiz@chromium.org>
2024-06-24 18:02:11 +0000

    [sync] Remove iOS-specific codepaths in SyncPrefs
    The features are enabled by default on iOS, so there is no need to use
    ifdefs: the code is generally simpler to maintain if it is shared
    across platforms.
    
    The change is expected to be a no-op in practice.
    
    Theoretically, there could be clients with
    kReplaceSyncPromosWithSignInPromos set to disabled via variations, but
    this is considered near-impossible because it could only happen if the
    variations seed is more than 6 months old. These cases would presumably
    run into other more severe issues (independently of this patch) and,
    concerning the behavioral change here, it isn't particularly bad either
    (bookmark and reading-list sync would be turned off).
    
    Bug: 346918509
    Change-Id: I9a90d38e3cb93c230bf07a7ae7923d0fdfc25e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647886
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1318671}

--- components/sync/service/sync_prefs.cc
+++ components/sync/service/sync_prefs.cc
@@ -318,7 +318,6 @@ UserSelectableTypeSet SyncPrefs::GetSelectedTypesForAccount(
       } else if (type == UserSelectableType::kBookmarks ||
                  type == UserSelectableType::kReadingList) {
         type_enabled = true;
-#if !BUILDFLAG(IS_IOS)
         // Consider kBookmarks and kReadingList off by default until
         // `kReplaceSyncPromosWithSignInPromos` is enabled. For existing clients
         // at the time the feature transitions from disabled to enabled, the
@@ -327,7 +326,6 @@ UserSelectableTypeSet SyncPrefs::GetSelectedTypesForAccount(
         if (!base::FeatureList::IsEnabled(kReplaceSyncPromosWithSignInPromos)) {
           type_enabled = false;
         }
-#endif
       } else {
         // All other types are always enabled by default.
         type_enabled = true;
@@ -733,11 +731,7 @@ bool SyncPrefs::IsTypeSupportedInTransportMode(UserSelectableType type) {
   // Features to be enabled.
   switch (type) {
     case UserSelectableType::kBookmarks:
-#if BUILDFLAG(IS_IOS)
-      return true;
-#else
       return base::FeatureList::IsEnabled(kSyncEnableBookmarksInTransportMode);
-#endif
     case UserSelectableType::kReadingList:
       return syncer::IsReadingListAccountStorageEnabled();
     case UserSelectableType::kPreferences:
