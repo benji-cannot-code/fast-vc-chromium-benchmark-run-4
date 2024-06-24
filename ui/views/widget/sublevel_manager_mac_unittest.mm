09a22c46088bc6722d02302102c3e54b16bd66e9
Elly Fong-Jones <ellyjones@google.com>
2024-06-24 22:37:02 +0000

    views: remove Mac-specific unsafe buffer uses
    These are both straightforward conversions to std::array with associated
    fixups of index types (from int to size_t).
    
    Bug: 344639839
    Change-Id: I33fa6eed9fbe25d34003f44aaf296eb2df46994b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651179
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318839}

--- ui/views/widget/sublevel_manager_mac_unittest.mm
+++ ui/views/widget/sublevel_manager_mac_unittest.mm
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(UNSAFE_BUFFERS_BUILD)
-// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
-// then remove this pragma.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ui/views/widget/sublevel_manager.h"
 
 #include <AppKit/AppKit.h>
@@ -90,11 +84,11 @@ TEST_P(SublevelManagerMacTest, ExplicitUntrack) {
       CreateTestWidget(Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET);
   std::unique_ptr<Widget> root2 =
       CreateTestWidget(Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET);
-  std::unique_ptr<Widget> children[3];
+  std::array<std::unique_ptr<Widget>, 3> children;
 
   ShowWidget(root);
   ShowWidget(root2);
-  for (int i = 0; i < 3; i++) {
+  for (size_t i = 0; i < children.size(); i++) {
     children[i] = CreateChildWidget(
         root.get(), ui::ZOrderLevel::kNormal, i,
         std::get<Widget::InitParams::Activatable>(GetParam()));
