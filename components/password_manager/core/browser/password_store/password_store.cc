d2ee0d1ed44ade1ebb63574a23fdffcc540f8233
Viktor Semeniuk <vsemeniuk@google.com>
2024-06-24 14:58:16 +0000

    Obsolete old histogram
    OBSOLETE_HISTOGRAM[PasswordManager.PasswordStore.OnLoginsChanged]=
    Removed in M128. No longer relevant.
    
    Fixed: 325944785
    Change-Id: I01515dfc0f393aedcaae082e1e171dd9242021c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645049
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318571}

--- components/password_manager/core/browser/password_store/password_store.cc
+++ components/password_manager/core/browser/password_store/password_store.cc
@@ -441,9 +441,6 @@ void PasswordStore::NotifyLoginsChangedOnMainSequence(
   for (auto& observer : observers_) {
     observer.OnLoginsChanged(this, changes.value());
   }
-
-  base::UmaHistogramBoolean("PasswordManager.PasswordStore.OnLoginsChanged",
-                            true);
 }
 
 void PasswordStore::NotifyLoginsRetainedOnMainSequence(
