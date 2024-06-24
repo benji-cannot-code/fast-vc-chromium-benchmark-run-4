43b81250528e810aed96dabb5f8f3a6f181e678a
Ewann Pelle <ewannpv@chromium.org>
2024-06-24 15:49:00 +0000

    [iOS] Add a service to observe tab group local changes
    Create a class that is observing all the local change to propagate
    them to the sync service in the future.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL method logic not yet implemented
    Bug: 329640035
    Change-Id: I351f2d6c83acc5e56f946f92ed215a08f4383820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626318
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318590}

--- ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
+++ ios/chrome/browser/browser_state/model/browser_state_keyed_service_factories.mm
@@ -106,6 +106,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/safe_browsing/model/tailored_security/tailored_security_service_factory.h"
 #import "ios/chrome/browser/safe_browsing/model/verdict_cache_manager_factory.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_factory.h"
+#import "ios/chrome/browser/saved_tab_groups/model/tab_group_local_update_service_factory.h"
+#import "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
 #import "ios/chrome/browser/screen_time/model/screen_time_buildflags.h"
 #import "ios/chrome/browser/search_engines/model/search_engine_choice_service_factory.h"
 #import "ios/chrome/browser/search_engines/model/template_url_fetcher_factory.h"
@@ -328,6 +330,12 @@ void EnsureBrowserStateKeyedServiceFactoriesBuilt() {
   if (base::FeatureList::IsEnabled(
           send_tab_to_self::kSendTabToSelfIOSPushNotifications)) {
     IOSSharingMessageBridgeFactory::GetInstance();
+  }
+
+  if (IsTabGroupSyncEnabled()) {
+    // Ensure that the tab group sync services are created to observe updates.
+    tab_groups::TabGroupSyncServiceFactory::GetInstance();
+    tab_groups::TabGroupLocalUpdateServiceFactory::GetInstance();
   }
 
 #if BUILDFLAG(IOS_CREDENTIAL_PROVIDER_ENABLED)
