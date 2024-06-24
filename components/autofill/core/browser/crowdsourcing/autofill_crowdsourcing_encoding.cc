768670475f6f4d2fe836c2346837b726a1d4f4cf
Norge Vizcay <vizcay@google.com>
2024-06-24 06:28:54 +0000

    Fix unsafe buffer warnings in autofill crowdsourcing encoding.
    Bug: 347137620
    Change-Id: I331da82ef906c73573242758b47ce61a049ad3f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647887
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1318437}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/341324165): Fix and remove.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_encoding.h"
 
 #include <algorithm>
@@ -697,8 +692,9 @@ std::vector<AutofillUploadContents> EncodeUploadRequest(
                        return field->renderer_form_id() !=
                               (*subform_begin)->renderer_form_id();
                      });
-    EncodeFormFieldsForUpload(form, {subform_begin, subform_end},
-                              &uploads.back());
+    // SAFETY: The iterators are from the same container.
+    EncodeFormFieldsForUpload(
+        form, UNSAFE_BUFFERS({subform_begin, subform_end}), &uploads.back());
     subform_begin = subform_end;
   }
   return uploads;
