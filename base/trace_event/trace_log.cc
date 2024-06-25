b1e3a8434d8234cc1190c1760dbf1fdde172e223
Bartek Nowierski <bartekn@chromium.org>
2024-06-25 13:49:04 +0000

    [BRP] Remove TraceLog::OptionalAutoLock exception
    The embedding class can be marked STACK_ALLOCATED, and we generally don't rewrite these
    
    Bug: 348793153
    Change-Id: I2215c5e9113f25ad63116a80a569ff7a83579652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653890
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319114}

--- base/trace_event/trace_log.cc
+++ base/trace_event/trace_log.cc
@@ -28,13 +28,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/memory/stack_allocated.h"
 #include "base/no_destructor.h"
 #include "base/notreached.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/process/process.h"
 #include "base/process/process_metrics.h"
 #include "base/ranges/algorithm.h"
+#include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_tokenizer.h"
@@ -44,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool.h"
+#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_id_name_manager.h"
 #include "base/time/time.h"
@@ -54,12 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/process_memory_dump.h"
 #include "base/trace_event/trace_buffer.h"
 #include "base/trace_event/trace_event.h"
-#include "build/build_config.h"
-
-#include "base/numerics/safe_conversions.h"
-#include "base/run_loop.h"
-#include "base/task/thread_pool/thread_pool_instance.h"
 #include "base/tracing/perfetto_platform.h"
+#include "build/build_config.h"
 #include "third_party/perfetto/include/perfetto/ext/trace_processor/export_json.h"  // nogncheck
 #include "third_party/perfetto/include/perfetto/trace_processor/trace_processor_storage.h"  // nogncheck
 #include "third_party/perfetto/include/perfetto/tracing/console_interceptor.h"
@@ -430,6 +429,8 @@ class JsonStringOutputWriter
 // A helper class that allows the lock to be acquired in the middle of the scope
 // and unlocks at the end of scope if locked.
 class TraceLog::OptionalAutoLock {
+  STACK_ALLOCATED();
+
  public:
   explicit OptionalAutoLock(Lock* lock) : lock_(lock) {}
 
@@ -451,8 +452,7 @@ class TraceLog::OptionalAutoLock {
   }
 
  private:
-  // This field is not a raw_ptr<> because it is needed for lock annotations.
-  RAW_PTR_EXCLUSION Lock* lock_;
+  Lock* lock_;
   bool locked_ = false;
 };
 
