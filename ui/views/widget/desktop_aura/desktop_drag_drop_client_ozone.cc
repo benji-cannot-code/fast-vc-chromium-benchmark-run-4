33f2dc9e5f9adfabae2c124072d1f0165e15caa9
Elly <ellyjones@chromium.org>
2024-06-25 22:22:41 +0000

    ozone: fix unsafe buffer use in drag-drop client
    To do that, use SkBitmap's pixel data accessor rather than rolling our
    own.
    
    Bug: 344639839
    Change-Id: If4ad357a14b2a43783ba7795f4873938059aacbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651265
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Joseph Park <josephjoopark@chromium.org>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319455}

--- ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.cc
+++ ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.cc
@@ -3,12 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if defined(UNSAFE_BUFFERS_BUILD)
-// TODO(https://crbug.com/344639839): fix the unsafe buffer errors in this file,
-// then remove this pragma.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ui/views/widget/desktop_aura/desktop_drag_drop_client_ozone.h"
 
 #include <memory>
@@ -50,10 +44,8 @@ bool IsValidDragImage(const gfx::ImageSkia& image) {
   // transparent image.
   const SkBitmap* in_bitmap = image.bitmap();
   for (int y = 0; y < in_bitmap->height(); ++y) {
-    uint32_t* in_row = in_bitmap->getAddr32(0, y);
-
     for (int x = 0; x < in_bitmap->width(); ++x) {
-      if (SkColorGetA(in_row[x]) > kMinAlpha) {
+      if (SkColorGetA(in_bitmap->getColor(x, y)) > kMinAlpha) {
         return true;
       }
     }
