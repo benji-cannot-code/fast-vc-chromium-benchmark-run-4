754e0d8c4cfbbf3d7b630f8f9d14748a50f561de
Sana Akbani <sanaakbani@google.com>
2024-06-25 20:23:32 +0000

    Initialize ReadAloud UserActions to record metrics for seeking forward and back clicks
    Bug: b/316618089
    Change-Id: I8a67b5d675af481bb8a29c6efd65b2107fe0e594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646461
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Cr-Commit-Position: refs/heads/main@{#1319377}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerMediatorUnitTest.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/player/PlayerMediatorUnitTest.java
@@ -4,6 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.readaloud.player;
 
+import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
+
+import static org.hamcrest.Matchers.hasItems;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
@@ -27,6 +30,7 @@ import static org.chromium.chrome.modules.readaloud.PlaybackListener.State.UNKNO
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -44,6 +48,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.JniMocker;
+import org.chromium.base.test.util.UserActionTester;
 import org.chromium.chrome.browser.readaloud.ReadAloudMetrics;
 import org.chromium.chrome.browser.readaloud.ReadAloudPrefs;
 import org.chromium.chrome.browser.readaloud.ReadAloudPrefsJni;
@@ -80,6 +85,7 @@ public class PlayerMediatorUnitTest {
     private ObservableSupplierImpl<String> mSelectedVoiceIdSupplier;
     private ObservableSupplierImpl<Boolean> mHighlightingEnabledSupplier;
     @Captor private ArgumentCaptor<PlaybackListener> mPlaybackListenerCaptor;
+    public UserActionTester mUserActionTester;
 
     private PropertyModel mModel;
     private FakeClock mClock;
@@ -179,6 +185,12 @@ public class PlayerMediatorUnitTest {
         mMediator = new PlayerMediator(mPlayerCoordinator, mDelegate, mModel);
         mMediator.setClockForTesting(mClock);
         mOnSeekBarChangeListener = mMediator.getSeekBarChangeListener();
+        mUserActionTester = new UserActionTester();
+    }
+
+    @After
+    public void tearDown() {
+        mUserActionTester.tearDown();
     }
 
     @Test
@@ -410,6 +422,18 @@ public class PlayerMediatorUnitTest {
         mMediator.setPlayback(mPlayback);
         mMediator.onSeekBackClick();
         verify(mPlayback).seekRelative(-10 * 1_000_000_000L);
+    }
+
+    @Test
+    public void testBackClickActionRecords() {
+        mMediator.onSeekBackClick();
+        assertThat(mUserActionTester.getActions(), hasItems("ReadAloud.SeekBackward"));
+    }
+
+    @Test
+    public void testForwardClickActionRecords() {
+        mMediator.onSeekForwardClick();
+        assertThat(mUserActionTester.getActions(), hasItems("ReadAloud.SeekForward"));
     }
 
     @Test
