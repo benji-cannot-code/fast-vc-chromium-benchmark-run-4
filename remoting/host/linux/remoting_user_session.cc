b1bfe558ca4338fb21d6b6799f751c595fe14907
Alex Yang <aycyang@chromium.org>
2024-06-25 20:47:15 +0000

    [CodeHealth] Remove RAW_PTR_EXCLUSION from Linux PamHandle
    Bug: 346693503
    Change-Id: I1f8736919302c5a9c87e8854640d7978d4f2fd87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5649694
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Erik Jensen <rkjnsn@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319394}

--- remoting/host/linux/remoting_user_session.cc
+++ remoting/host/linux/remoting_user_session.cc
@@ -44,7 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "base/process/launch.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
@@ -185,8 +185,8 @@ class PamHandle {
   PamHandle(const char* service_name,
             const char* user,
             const struct pam_conv* pam_conversation) {
-    last_return_code_ =
-        pam_start(service_name, user, pam_conversation, &pam_handle_);
+    last_return_code_ = pam_start(service_name, user, pam_conversation,
+                                  &pam_handle_.AsEphemeralRawAddr());
     if (last_return_code_ != PAM_SUCCESS) {
       pam_handle_ = nullptr;
     }
@@ -284,9 +284,7 @@ class PamHandle {
   }
 
  private:
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION pam_handle_t* pam_handle_ = nullptr;
+  raw_ptr<pam_handle_t> pam_handle_ = nullptr;
   int last_return_code_ = PAM_SUCCESS;
 };
 
