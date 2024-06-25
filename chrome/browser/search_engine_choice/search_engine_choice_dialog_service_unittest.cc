1863e0270229a1d0b4462431e928e3ee8c28f47f
Jack Yammine <jyammine@google.com>
2024-06-25 12:57:11 +0000

    Reland "[waffle]Change the local_state to a ref in SearchengineChoiceService"
    This is a reland of commit 088c09b832e561a48857c29fa37d26625dd25051
    
    Original change's description:
    > [waffle]Change the local_state to a ref in SearchengineChoiceService
    >
    > We initially added it as a pointer because a lot of tests were
    > crashing when dereferencing it.
    >
    > We don't create the SearchEngineChoiceService anymore in all unit tests.
    > We initialize it correctly in the tests that needed.
    >
    
    The change was reverted because the
    SearchEngineChoiceDialogServiceFactory creates an instance of
    the SearchEngineChoiceService which is null in tests.
    
    We stop creating SearchEngineChoiceDialogService automatically
    in tests.
    
    > Fixed: b:342110115
    > Change-Id: I0af71c2ca9482ee6cbc5eac125562a9610871f18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5557846
    > Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    > Auto-Submit: Jack Yammine <jyammine@google.com>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Commit-Queue: Jack Yammine <jyammine@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1307298}
    
    Change-Id: Idfdd012357b5e369416ec728cfd2bca0a2096cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5577204
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319088}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_service_unittest.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_service_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_dialog_service_factory.h"
+#include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/dialog_test_browser_window.h"
@@ -95,9 +96,15 @@ class SearchEngineChoiceDialogServiceTest : public BrowserWithTestWindowTest {
 
   void SetUp() override {
     BrowserWithTestWindowTest::SetUp();
-    TemplateURLServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+    search_engines::SearchEngineChoiceServiceFactory::GetInstance()
+        ->SetTestingFactory(profile(),
+                            search_engines::SearchEngineChoiceServiceFactory::
+                                GetDefaultFactory());
+    TemplateURLServiceFactory::GetInstance()->SetTestingFactory(
         profile(),
         base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
+    SearchEngineChoiceDialogServiceFactory::GetInstance()->SetTestingFactory(
+        profile(), SearchEngineChoiceDialogServiceFactory::GetDefaultFactory());
 
     // The search engine choice feature is only enabled for countries in the
     // EEA region.
