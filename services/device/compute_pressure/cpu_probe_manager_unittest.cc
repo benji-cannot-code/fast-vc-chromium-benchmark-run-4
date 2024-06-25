9c04c466add02806d881f41f664340351a461e7b
Arnaud Mandy <arnaud.mandy@intel.com>
2024-06-25 11:55:32 +0000

    compute pressure: Update threshold based on feedback
    User feedback has indicated that the thresholds for fair, serious and critical states were too low. Even with a 60% cpu utilization, there was no visible effect from cpu pressure on the device.
    
    Based on the feedback and recommendations we increased the thresholds
    for pressure state decision.
    
    Bug: 348947995
    Change-Id: I963e6e8b6181279a8dd69ebae8ff4233326f8498
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645369
    Reviewed-by: Raphael Kubo Da Costa <raphael.kubo.da.costa@intel.com>
    Commit-Queue: Arnaud Mandy <arnaud.mandy@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1319070}

--- services/device/compute_pressure/cpu_probe_manager_unittest.cc
+++ services/device/compute_pressure/cpu_probe_manager_unittest.cc
@@ -127,7 +127,7 @@ TEST_F(CpuProbeManagerTest, EnsureStartedSkipsFirstSample) {
       // Value after first Update(), should be discarded.
       CpuSample{0.9},
       // Value after second Update(), should be reported.
-      CpuSample{0.4},
+      CpuSample{0.65},
   };
 
   base::RunLoop run_loop;
@@ -197,9 +197,9 @@ TEST_F(CpuProbeManagerTest, EnsureStartedCheckCalculateStateHysteresisUp) {
       // kNominal value after should be reported.
       CpuSample{0.3},
       // kFair value should be reported.
-      CpuSample{0.6},
+      CpuSample{0.7},
       // kSerious value should be reported.
-      CpuSample{0.9},
+      CpuSample{0.8},
       // kCritical value should be reported.
       CpuSample{1.0},
   };
@@ -231,9 +231,9 @@ TEST_F(CpuProbeManagerTest, EnsureStartedCheckCalculateStateHysteresisDown) {
       // kSerious value should be reported.
       CpuSample{0.85},
       // kFair value should be reported.
-      CpuSample{0.55},
+      CpuSample{0.70},
       // kNominal value should be reported.
-      CpuSample{0.25},
+      CpuSample{0.55},
   };
 
   base::RunLoop run_loop;
@@ -264,9 +264,9 @@ TEST_F(CpuProbeManagerTest,
       // kCritical value should be reported due to hysteresis.
       CpuSample{0.88},
       // kFair value should be reported.
-      CpuSample{0.58},
+      CpuSample{0.73},
       // kNominal value should be reported.
-      CpuSample{0.26},
+      CpuSample{0.56},
   };
 
   base::RunLoop run_loop;
@@ -295,9 +295,9 @@ TEST_F(CpuProbeManagerTest,
       // kCritical value after should be reported.
       CpuSample{0.95},
       // kFair value should be reported.
-      CpuSample{0.58},
+      CpuSample{0.73},
       // kFair value should be reported due to hysteresis.
-      CpuSample{0.28},
+      CpuSample{0.58},
   };
 
   base::RunLoop run_loop;
@@ -323,11 +323,11 @@ TEST_F(CpuProbeManagerTest,
       // Value after first Update(), should be discarded.
       CpuSample{1.0},
       // kNominal value after should be reported.
-      CpuSample{0.3},
+      CpuSample{0.6},
       // kFair value should be reported due to hysteresis.
-      CpuSample{0.32},
-      // kSerious value should be reported.
       CpuSample{0.62},
+      // kSerious value should be reported.
+      CpuSample{0.77},
       // kCritical value should be reported.
       CpuSample{0.91},
   };
