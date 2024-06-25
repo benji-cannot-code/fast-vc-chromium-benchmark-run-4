b93d767d1f6fc2b49768e5ed4d72c352d197f778
Ramin Halavati <rhalavati@chromium.org>
2024-06-25 15:32:32 +0000

    Remove stale ScreenAIAXTreeSerializer.
    The class is not used anymore, it was missed from previous removal CL:
    crrev.com/c/5629092
    
    AX-Relnotes: n/a
    Bug: 313384385
    Change-Id: Id8ae96242f7632b809d552ef72a98cad95b4ab3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654250
    Auto-Submit: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Kyungjun Lee <kyungjunlee@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1319165}

--- services/screen_ai/screen_ai_service_impl.cc
+++ services/screen_ai/screen_ai_service_impl.cc
@@ -24,8 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/screen_ai/proto/main_content_extractor_proto_convertor.h"
 #include "services/screen_ai/proto/visual_annotator_proto_convertor.h"
 #include "services/screen_ai/public/cpp/utilities.h"
-#include "services/screen_ai/screen_ai_ax_tree_serializer.h"
 #include "ui/accessibility/accessibility_features.h"
+#include "ui/accessibility/ax_node.h"
+#include "ui/accessibility/ax_tree.h"
 #include "ui/accessibility/ax_tree_id.h"
 #include "ui/gfx/geometry/rect_f.h"
 
