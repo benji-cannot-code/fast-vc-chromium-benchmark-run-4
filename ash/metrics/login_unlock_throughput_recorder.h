fdf27222e2b4f45cdc6f6fcee79e6f28d68db361
Jun Ishiguro <junis@google.com>
2024-06-25 06:38:47 +0000

    LoginUnlockThroughputRecorder: Consolidate session restore sources
    Currently, we check both of full session restore data and browser
    session restore data to get previous session's window ids. This CL
    changes LoginUnlockThroughputRecorder so that we only check full session
    restore data to collect window ids.
    
    Bug: b:328339021, b:343001594
    Change-Id: Ia17ddf296593f64cb7850e8315615ae93c8749e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5638192
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1318959}

--- ash/metrics/login_unlock_throughput_recorder.h
+++ ash/metrics/login_unlock_throughput_recorder.h
@@ -50,7 +50,6 @@ class ASH_EXPORT WindowRestoreTracker {
             base::OnceClosure on_all_window_shown,
             base::OnceClosure on_all_window_presented);
 
-  int NumberOfWindows() const;
   void AddWindow(int window_id, const std::string& app_id);
   void OnCreated(int window_id);
   void OnShown(int window_id, ui::Compositor* compositor);
@@ -150,13 +149,13 @@ class ASH_EXPORT LoginUnlockThroughputRecorder : public LoginState::Observer {
     return login_animation_throughput_reporter_.get();
   }
 
-  // This is called when the list of session windows gets completed. Note that
-  // this can be called multiple times when session restore is attempted
-  // multiple times, e.g. due to errors.
-  void BrowserSessionRestoreDataLoaded(std::vector<RestoreWindowID> window_ids);
-
-  // This is called when the list of full restore windows, e.g. Lacros windows.
-  void FullSessionRestoreDataLoaded(std::vector<RestoreWindowID> window_ids);
+  // This is called when the list of restore windows is ready. `window_ids`
+  // includes session window ids for browser windows that were created in the
+  // previous session. `restore_automatically` is true if session restore will
+  // start immediately after this call. More specifically, it will be true if
+  // the apps restore settings is set to "Always restore" in the OS settings.
+  void FullSessionRestoreDataLoaded(std::vector<RestoreWindowID> window_ids,
+                                    bool restore_automatically);
 
   // Records that ARC has finished booting.
   void ArcUiAvailableAfterLogin();
@@ -187,8 +186,6 @@ class ASH_EXPORT LoginUnlockThroughputRecorder : public LoginState::Observer {
 
   void OnPostLoginDeferredTaskTimerFired();
 
-  void MaybeRestoreDataLoaded();
-
   void OnAllWindowsCreated();
   void OnAllWindowsShown();
   void OnAllWindowsPresented();
@@ -203,12 +200,6 @@ class ASH_EXPORT LoginUnlockThroughputRecorder : public LoginState::Observer {
 
   bool user_logged_in_ = false;
 
-  // Session restore data comes from chrome::SessionRestore and ash::FullRestore
-  // independently.
-
-  // This flag is true after SessionRestore has finished loading its data.
-  bool browser_session_restore_data_loaded_ = false;
-
   // This flag is true after FullRestore has finished loading its data.
   bool full_session_restore_data_loaded_ = false;
 
@@ -218,7 +209,7 @@ class ASH_EXPORT LoginUnlockThroughputRecorder : public LoginState::Observer {
   // i.e. there is no pending icon on shelf after shelf is initialized.
   bool shelf_icons_loaded_ = false;
 
-  bool dcheck_shelf_animation_end_scheduled_ = false;
+  bool shelf_animation_end_scheduled_ = false;
 
   bool shelf_animation_finished_ = false;
 
