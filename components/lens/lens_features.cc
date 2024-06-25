7b09c6fb8e4b554a3ed692161756a42df61cb7d0
Juan Mojica <juanmojica@google.com>
2024-06-25 16:56:35 +0000

    Reland "[lensoverlay] Make blur intensity configurable via finch."
    This change was reverted at https://crrev.com/c/5644681 due to merge
    conflicts with the culprit CL at https://crrev.com/c/5650508. This CL
    should not have caused any failing tests.
    
    Original change: https://crrev.com/c/5651215
    
    Bug: b:345244262
    Change-Id: Ib2b41164cc978de3c1da6cac9eb3a49dffd4739d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653577
    Auto-Submit: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1319227}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -173,6 +173,9 @@ constexpr base::FeatureParam<int> kLensOverlaySignificantRegionMinArea{
 constexpr base::FeatureParam<int> kLensOverlayMaxSignificantRegions{
     &kLensOverlay, "max-significant-regions", 100};
 
+constexpr base::FeatureParam<int> kLensOverlayLivePageBlurRadiusPixels{
+    &kLensOverlay, "live-page-blur-radius-pixels", 200};
+
 constexpr base::FeatureParam<double>
     kLensOverlayPostSelectionComparisonThreshold{
         &kLensOverlay, "post-selection-comparison-threshold", 0.005};
@@ -499,6 +502,10 @@ int GetLensOverlayMaxSignificantRegions() {
 
 double GetLensOverlayPostSelectionComparisonThreshold() {
   return kLensOverlayPostSelectionComparisonThreshold.Get();
+}
+
+int GetLensOverlayLivePageBlurRadiusPixels() {
+  return kLensOverlayLivePageBlurRadiusPixels.Get();
 }
 
 }  // namespace lens::features
