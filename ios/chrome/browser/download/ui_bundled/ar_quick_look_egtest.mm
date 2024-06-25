d1c1c3e5d835f3e6546b4857bdffc7cef4412d3f
Joemer Ramos <joemerramos@chromium.org>
2024-06-25 15:02:39 +0000

    [iOS] Fix ARQuickLookEGTest Matcher Check Delay on iOS 18
    On iOS 18, this EG test has a slight delay between tapping a hyperlink
    and opening the augmented reality screen. It currently uncertain whether
    EG sync is delayed or if this is a time delay. This CL changes logic to
    wait and look for matcher element.
    
    Fixed: 347679910
    Change-Id: I8d5722a8f698bd97629fc1bd090832d247dfea24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654150
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319151}

--- ios/chrome/browser/download/ui_bundled/ar_quick_look_egtest.mm
+++ ios/chrome/browser/download/ui_bundled/ar_quick_look_egtest.mm
@@ -89,9 +89,8 @@ std::unique_ptr<net::test_server::HttpResponse> GetResponse(
   [ChromeEarlGrey tapWebStateElementWithID:@"good"];
 
   // Verify QLPreviewControllerView is presented.
-  [[EarlGrey
-      selectElementWithMatcher:grey_kindOfClassName(@"QLPreviewControllerView")]
-      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey waitForSufficientlyVisibleElementWithMatcher:
+                      grey_kindOfClassName(@"QLPreviewControllerView")];
 }
 
 - (void)testDownloadUnauthorized {
