2baad6af6ee8b7fbc12bb7004d71ba576a776fa6
Henrique Nakashima <hnakashima@chromium.org>
2024-06-25 21:16:57 +0000

    Revert "JNI Zero: Expose empty String, List, Map as constants"
    This reverts commit e4259e1fc57fdbb88111021a57dede285f4a15e5.
    
    Reason for revert: Broke local build
    
    Original change's description:
    > JNI Zero: Expose empty String, List, Map as constants
    >
    > AFAICT, ART allocates a new empty string object each time you call
    > env->NewString*() methods. Same with List and Map.
    >
    > To avoid allocations for empty things, use these singletons in
    > conversion functions.
    >
    > This adds a new JavaRef type: LeakedJavaGlobalRef, which is the same as
    > ScopedJavaGlobalRef, except without a destructor.
    >
    > Bug: None
    > Change-Id: Id87e7e7a8c7a8219e237d15a37ade582768383d0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645747
    > Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: Sam Maier <smaier@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1319347}
    
    Bug: None
    Change-Id: I101ea04150b33e2c8b9ac857466aa886365aea05
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5656590
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319417}

--- third_party/jni_zero/jni_zero.cc
+++ third_party/jni_zero/jni_zero.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/prctl.h>
 
 #include "third_party/jni_zero/jni_zero_internal.h"
-#include "third_party/jni_zero/jni_zero_jni/JniInit_jni.h"
 #include "third_party/jni_zero/logging.h"
 
 namespace jni_zero {
@@ -61,9 +60,6 @@ jclass GetSystemClassGlobalRef(JNIEnv* env, const char* class_name) {
 
 jclass g_object_class = nullptr;
 jclass g_string_class = nullptr;
-LeakedJavaGlobalRef<jstring> g_empty_string = nullptr;
-LeakedJavaGlobalRef<jobject> g_empty_list = nullptr;
-LeakedJavaGlobalRef<jobject> g_empty_map = nullptr;
 
 ScopedJavaLocalFrame::ScopedJavaLocalFrame(JNIEnv* env) : env_(env) {
   int failed = env_->PushLocalFrame(kDefaultLocalFrameCapacity);
@@ -228,15 +224,7 @@ void InitVM(JavaVM* vm) {
   JNIEnv* env = AttachCurrentThread();
   g_object_class = GetSystemClassGlobalRef(env, "java/lang/Object");
   g_string_class = GetSystemClassGlobalRef(env, "java/lang/String");
-  g_empty_string.Reset(
-      env, ScopedJavaLocalRef<jstring>(env, env->NewString(nullptr, 0)));
-  ScopedJavaLocalRef<jobjectArray> globals = Java_JniInit_init(env);
-  g_empty_list.Reset(env,
-                     ScopedJavaLocalRef<jobject>(
-                         env, env->GetObjectArrayElement(globals.obj(), 0)));
-  g_empty_map.Reset(env,
-                    ScopedJavaLocalRef<jobject>(
-                        env, env->GetObjectArrayElement(globals.obj(), 1)));
+  CheckException(env);
 }
 
 void DisableJvmForTesting() {
@@ -276,7 +264,6 @@ void CheckException(JNIEnv* env) {
   if (g_exception_handler_callback) {
     return g_exception_handler_callback(env);
   }
-  env->ExceptionDescribe();
   JNI_ZERO_FLOG("jni_zero crashing due to uncaught Java exception");
 }
 
