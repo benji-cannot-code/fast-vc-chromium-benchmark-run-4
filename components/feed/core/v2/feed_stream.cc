f4bcb7928122b0831cc2311fe6e04e6a4d0c0498
Jian Li <jianli@chromium.org>
2024-06-25 23:11:24 +0000

    Check language for web feed
    Bug: 349424259
    Change-Id: I89d4ddcb1440ba68c9798dd9b24b264cd6ada7c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653584
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319476}

--- components/feed/core/v2/feed_stream.cc
+++ components/feed/core/v2/feed_stream.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/offline_pages/task/closure_task.h"
 #include "components/prefs/pref_service.h"
 #include "components/signin/public/base/signin_pref_names.h"
+#include "ui/base/l10n/l10n_util.h"
 
 namespace feed {
 namespace {
@@ -632,7 +633,8 @@ bool FeedStream::IsFeedEnabledByDse() {
 }
 
 bool FeedStream::IsWebFeedEnabled() {
-  return feed::IsWebFeedEnabledForLocale(delegate_->GetCountry()) &&
+  return l10n_util::GetLanguage(delegate_->GetLanguageTag()) == "en" &&
+         feed::IsWebFeedEnabledForLocale(delegate_->GetCountry()) &&
          !delegate_->IsSupervisedAccount();
 }
 
