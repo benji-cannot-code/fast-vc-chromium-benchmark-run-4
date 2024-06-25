80ba599b7a716498cc85df9b31db13ec4ceee323
Benoit Lize <lizeb@chromium.org>
2024-06-25 17:39:22 +0000

    Revert "[BRP] Rewrite pointers in blink::CacheEntry to raw_ptr<>"
    This reverts commit 1f72661ad43e9a04fd0f0879b6d88fee5883fd5d.
    
    Reason for revert: Speculative fix for crbug/331983168
    
    Original change's description:
    > [BRP] Rewrite pointers in blink::CacheEntry to raw_ptr<>
    >
    > The manual exclusion reason seems to no longer be true as CQ passes. Update manual-field-to-ignore.txt appropriately.
    >
    > While at it, remove WTF::LruCache::MappedListNodeWithKey which no longer exists.
    >
    > No apparent regression on Speedometer and MotionMark:
    > https://pinpoint-dot-chromeperf.appspot.com/job/16185cff410000
    > https://pinpoint-dot-chromeperf.appspot.com/job/14ac2d1f410000
    > https://pinpoint-dot-chromeperf.appspot.com/job/1295305cc10000
    > https://pinpoint-dot-chromeperf.appspot.com/job/14861d17410000
    >
    > Bug: 348793153
    > Change-Id: I44bd4d2461bea050530af00199d614de876d492d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645173
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Auto-Submit: Bartek Nowierski <bartekn@chromium.org>
    > Commit-Queue: Jonathan Ross <jonross@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1318525}
    
    Bug: 348793153, 331983168
    Change-Id: I83a424921828b06d544afc8be9e0ab8d709c8795
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654311
    Owners-Override: Prudhvikumar Bommana <pbommana@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Prudhvikumar Bommana <pbommana@google.com>
    Cr-Commit-Position: refs/heads/main@{#1319265}

--- third_party/blink/renderer/platform/graphics/image_decoding_store.h
+++ third_party/blink/renderer/platform/graphics/image_decoding_store.h
@@ -87,7 +87,10 @@ class CacheEntry : public DoublyLinkedListNode<CacheEntry> {
   };
 
   CacheEntry(const ImageFrameGenerator* generator, int use_count)
-      : generator_(generator), use_count_(use_count) {}
+      : generator_(generator),
+        use_count_(use_count),
+        prev_(nullptr),
+        next_(nullptr) {}
   CacheEntry(const CacheEntry&) = delete;
   CacheEntry& operator=(const CacheEntry&) = delete;
 
@@ -111,8 +114,8 @@ class CacheEntry : public DoublyLinkedListNode<CacheEntry> {
   int use_count_;
 
  private:
-  raw_ptr<CacheEntry> prev_ = nullptr;
-  raw_ptr<CacheEntry> next_ = nullptr;
+  CacheEntry* prev_;
+  CacheEntry* next_;
 };
 
 class DecoderCacheEntry final : public CacheEntry {
