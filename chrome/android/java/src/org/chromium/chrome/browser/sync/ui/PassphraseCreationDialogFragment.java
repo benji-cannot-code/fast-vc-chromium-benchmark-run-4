5412bb21525e7f258bf88a3d6d1cad5813b0989e
Ankush Singh <ankushkush@google.com>
2024-06-25 14:30:37 +0000

    Use SyncSettingsUtils.openSyncDashboard for passphrase fragments
    There already exists a util in SyncSettingsUtils. The passphrase
    fragments (creation, type etc.) create the intent manually. This CL
    instead makes them use the util. Tested all the flows manually.
    
    Bug: 346491627
    Change-Id: I849706285072d39b5556b17eac92714ebda4551d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653752
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1319130}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/ui/PassphraseCreationDialogFragment.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/ui/PassphraseCreationDialogFragment.java
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.sync.ui;
 
 import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.method.LinkMovementMethod;
@@ -23,19 +20,16 @@ import android.widget.TextView.OnEditorActionListener;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.fragment.app.DialogFragment;
 
-import org.chromium.base.ContextUtils;
-import org.chromium.base.IntentUtils;
 import org.chromium.chrome.R;
-import org.chromium.chrome.browser.ChromeStringConstants;
 import org.chromium.chrome.browser.feedback.FragmentHelpAndFeedbackLauncher;
 import org.chromium.chrome.browser.feedback.HelpAndFeedbackLauncher;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.settings.ProfileDependentSetting;
 import org.chromium.chrome.browser.sync.SyncServiceFactory;
+import org.chromium.chrome.browser.sync.settings.SyncSettingsUtils;
 import org.chromium.ui.text.SpanApplier;
 import org.chromium.ui.text.SpanApplier.SpanInfo;
 
@@ -97,7 +91,6 @@ public class PassphraseCreationDialogFragment extends DialogFragment
     }
 
     private SpannableString getInstructionsText() {
-        final Context context = getActivity();
         boolean shouldReplaceSyncSettingsWithAccountSettings =
                 ChromeFeatureList.isEnabled(
                                 ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS)
@@ -113,16 +106,7 @@ public class PassphraseCreationDialogFragment extends DialogFragment
                         new ClickableSpan() {
                             @Override
                             public void onClick(View view) {
-                                // TODO(crbug.com/40944114): Move the following logic to open the
-                                // sync dashboard to a helper function.
-                                Uri syncDashboardUrl =
-                                        Uri.parse(ChromeStringConstants.SYNC_DASHBOARD_URL);
-                                Intent intent = new Intent(Intent.ACTION_VIEW, syncDashboardUrl);
-                                intent.setPackage(
-                                        ContextUtils.getApplicationContext().getPackageName());
-                                IntentUtils.safePutBinderExtra(
-                                        intent, CustomTabsIntent.EXTRA_SESSION, null);
-                                context.startActivity(intent);
+                                SyncSettingsUtils.openSyncDashboard(getActivity());
                             }
                         }));
     }
