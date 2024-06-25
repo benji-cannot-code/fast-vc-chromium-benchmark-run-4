146340d3ee78a2e476d1b45c6ef64e44802c3097
Dave Tapuska <dtapuska@chromium.org>
2024-06-25 16:24:34 +0000

    Remove some dead code
    The subresource preloader was removed in https://crrev.com/bcd62c80
    
    Delete dead code that isn't called.
    
    Change-Id: I70352dd1213de448dc302573712618e98d65bb0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654650
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319201}

--- content/public/renderer/render_frame_observer.h
+++ content/public/renderer/render_frame_observer.h
@@ -43,7 +43,6 @@ namespace blink {
 class WebDocumentLoader;
 class WebElement;
 class WebFormElement;
-class WebSecurityOrigin;
 class WebString;
 class WebURLRequest;
 class WebWorkerFetchContext;
@@ -374,11 +373,6 @@ class CONTENT_EXPORT RenderFrameObserver
   // return true. All other observers should return false (default).
   virtual bool SetUpSmoothnessReporting(
       base::ReadOnlySharedMemoryRegion& shared_memory);
-
-  // Notifies the observers of the origins for which subresource redirect
-  // optimizations can be preloaded.
-  virtual void PreloadSubresourceOptimizationsForOrigins(
-      const std::vector<blink::WebSecurityOrigin>& origins) {}
 
 #if BUILDFLAG(CONTENT_ENABLE_LEGACY_IPC)
   // IPC::Listener implementation.
