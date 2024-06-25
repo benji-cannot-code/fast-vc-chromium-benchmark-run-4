2d061a44b6ab65fba6d5922e94a502e02d89d6aa
Yongshun Liu <yongshun@chromium.org>
2024-06-25 16:38:06 +0000

    focus_mode: Parse `playbackReportingToken` from queue.prepare/queue.next requests
    This parses `playbackReportingToken` from queue.prepare/queue.next
    requests so that the player can report the playback when needed later.
    
    Bug: b/348698192
    Test: Manual
    Change-Id: I50f1b9662d6a6661f16b4dd5d75826aae385a2a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646176
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319213}

--- ash/system/focus_mode/sounds/youtube_music/youtube_music_types.h
+++ ash/system/focus_mode/sounds/youtube_music/youtube_music_types.h
@@ -93,6 +93,7 @@ struct ASH_EXPORT PlaybackContext {
                   const std::string& track_explicit_type,
                   const Image& track_image,
                   const GURL& stream_url,
+                  const std::string& playback_reporting_token,
                   const std::string& queue_name);
   PlaybackContext(const PlaybackContext& other);
   ~PlaybackContext();
@@ -108,6 +109,8 @@ struct ASH_EXPORT PlaybackContext {
   Image track_image;
 
   GURL stream_url;
+
+  std::string playback_reporting_token;
 
   std::string queue_name;
 };
