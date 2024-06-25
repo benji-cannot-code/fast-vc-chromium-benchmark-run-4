86bbd7417a2e83b32661f17b206436c76e776a79
rbpotter <rbpotter@chromium.org>
2024-06-25 20:53:41 +0000

    Tab search: Assert on parameter for switchToTab
    Followup addressing comment on
    https://chromium-review.googlesource.com/c/chromium/src/+/5626372
    
    Bug: 40943652
    Change-Id: Iae27ca45e296fb56d8e7d12ae16a2fcfab7abfa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644853
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319399}

--- chrome/test/data/webui/tab_search/tab_search_page_test.ts
+++ chrome/test/data/webui/tab_search/tab_search_page_test.ts
@@ -800,7 +800,9 @@ suite('TabSearchAppTest', () => {
     assertEquals(0, tabSearchPage.getSelectedIndex());
 
     keyDownOn(tabSearchPage.getSearchInput(), 0, [], 'Enter');
-    await testProxy.whenCalled('switchToTab');
+    // Assert switchToTab() was called appropriately for an unfiltered tab list.
+    const [tabInfo] = await testProxy.whenCalled('switchToTab');
+    assertEquals(1, tabInfo.tabId);
   });
 
   test('KeyPressOnItemTriggersActions', async () => {
@@ -812,6 +814,8 @@ suite('TabSearchAppTest', () => {
         tabSearchPage.$.tabsList.querySelector('tab-search-item')!;
 
     keyDownOn(tabSearchItem, 0, [], 'Enter');
-    await testProxy.whenCalled('switchToTab');
+    // Assert switchToTab() was called appropriately for an unfiltered tab list.
+    const [tabInfo] = await testProxy.whenCalled('switchToTab');
+    assertEquals(1, tabInfo.tabId);
   });
 });
