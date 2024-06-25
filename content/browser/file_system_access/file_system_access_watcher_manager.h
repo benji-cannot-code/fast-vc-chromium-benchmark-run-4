b2a1d22c15ae9bbfa1a3127c594d144b1da10143
Daseul Lee <dslee@chromium.org>
2024-06-25 20:21:14 +0000

    [FSA] Send "errored" callback and destroy observation on error
    When FileSystemAccessChangeSource reports an error, observation(s)
    watching for the registered scope should send an "errored" callback,
    and destroys itself as well as removing the corresponding change source
    (if it is the only observation set up for that source), since the
    errored observation is in a non-recoverable state.
    
    Bug: 341123799
    Change-Id: Iac5f5daace668a482e34a5c55c8cc9becf43d80a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645968
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319373}

--- content/browser/file_system_access/file_system_access_watcher_manager.h
+++ content/browser/file_system_access/file_system_access_watcher_manager.h
@@ -58,6 +58,8 @@ class CONTENT_EXPORT FileSystemAccessWatcherManager
       // Copyable and movable.
       Change(const Change&);
       Change(Change&&) noexcept;
+      Change& operator=(const Change&);
+      Change& operator=(Change&&) noexcept;
 
       storage::FileSystemURL url;
       FileSystemAccessChangeSource::ChangeInfo change_info;
@@ -67,8 +69,8 @@ class CONTENT_EXPORT FileSystemAccessWatcherManager
       }
     };
 
-    using OnChangesCallback =
-        base::RepeatingCallback<void(const std::list<Change>& changes)>;
+    using OnChangesCallback = base::RepeatingCallback<void(
+        const std::optional<std::list<Change>>& changes_or_error)>;
     Observation(FileSystemAccessWatcherManager* watcher_manager,
                 FileSystemAccessWatchScope scope,
                 base::PassKey<FileSystemAccessWatcherManager> pass_key);
@@ -81,7 +83,7 @@ class CONTENT_EXPORT FileSystemAccessWatcherManager
     const FileSystemAccessWatchScope& scope() const { return scope_; }
 
     void NotifyOfChanges(
-        const std::list<Change>& changes,
+        const std::optional<std::list<Change>>& changes_or_error,
         base::PassKey<FileSystemAccessWatcherManager> pass_key);
 
    private:
