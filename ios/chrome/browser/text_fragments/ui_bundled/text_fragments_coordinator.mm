6123f5e34a2c66e2bd7021f501a4479add0affbf
Tina Wang <tinazwang@google.com>
2024-06-25 21:37:28 +0000

    [Code Health][iOS] Move ios/c/b/ui/text_fragments to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324233207
    Change-Id: If587b2609949405e5d71578821aa35e6cae174c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651621
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319427}

--- ios/chrome/browser/text_fragments/ui_bundled/text_fragments_coordinator.mm
+++ ios/chrome/browser/text_fragments/ui_bundled/text_fragments_coordinator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/text_fragments/text_fragments_coordinator.h"
+#import "ios/chrome/browser/text_fragments/ui_bundled/text_fragments_coordinator.h"
 
 #import <memory>
 
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/open_new_tab_command.h"
 #import "ios/chrome/browser/shared/public/commands/share_highlight_command.h"
-#import "ios/chrome/browser/ui/text_fragments/text_fragments_mediator.h"
+#import "ios/chrome/browser/text_fragments/ui_bundled/text_fragments_mediator.h"
 #import "ios/chrome/browser/web_state_list/model/web_state_dependency_installer_bridge.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/text_fragments/text_fragments_manager.h"
