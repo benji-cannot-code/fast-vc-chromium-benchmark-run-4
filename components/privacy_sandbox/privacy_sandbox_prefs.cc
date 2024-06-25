b24fb7b17df2c2bd0a0d7b8208c20643c71b3887
Jacob Stanley <jacobstanley@google.com>
2024-06-25 15:44:13 +0000

    ActivityTypeStorage - Adding kOther and kPreFirstTab enum value
    Created new pref `privacy_sandbox.activity_type.record2` due to enum changing.
    
    Bug: 332767884
    Change-Id: If41799e6a888c119aee4197c70b9fe43926249cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5608199
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Ben Kelly <wanderview@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319173}

--- components/privacy_sandbox/privacy_sandbox_prefs.cc
+++ components/privacy_sandbox/privacy_sandbox_prefs.cc
@@ -63,6 +63,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
       prefs::kPrivacySandboxTopicsConsentTextAtLastUpdate, "");
 #if BUILDFLAG(IS_ANDROID)
   registry->RegisterListPref(prefs::kPrivacySandboxActivityTypeRecord);
+  registry->RegisterListPref(prefs::kPrivacySandboxActivityTypeRecord2);
 #endif
   // Register prefs for tracking protection.
   tracking_protection::RegisterProfilePrefs(registry);
