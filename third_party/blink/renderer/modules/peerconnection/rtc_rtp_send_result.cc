b7a24d2757fffe271415090aebebd8707cc88e23
Tony Herre <toprice@chromium.org>
2024-06-26 14:07:46 +0000

    Empty implementation of the RTCRtpSendStream interface from RtpTransport
    Initial empty impl of the interface types around RTCRtpSendStream,
    see https://github.com/w3c/webrtc-rtptransport/blob/main/api-outline.md
    
    Should allow for prototyping of the lowerlevel libwebrtc interface for
    actually sending RTP packets from JS.
    
    All guarded by the blink feature RTCRtpTransport.
    
    Bug: 345101934
    Change-Id: Idc96b5c016cc163ff51d3415d537c6500caa6a91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5642640
    Auto-Submit: Tony Herre <toprice@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319727}

---
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_send_result.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_send_result.h"
+
+namespace blink {
+
+RTCRtpSent* RTCRtpSendResult::sent() {
+  // TODO(crbug.com/345101934): Implement me.
+  return MakeGarbageCollected<RTCRtpSent>();
+}
+
+String RTCRtpSendResult::unsent() {
+  // TODO(crbug.com/345101934): Implement me.
+  return String();
+}
+
+}  // namespace blink
--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.h
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_rtc_set_parameter_options.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/mediastream/media_stream.h"
+#include "third_party/blink/renderer/modules/peerconnection/rtc_rtp_send_stream.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
@@ -102,6 +103,11 @@ class RTCRtpSender final : public ScriptWrappable,
   void set_streams(MediaStreamVector streams);
   void set_transceiver(RTCRtpTransceiver*);
   void set_transport(RTCDtlsTransport*);
+
+  ScriptPromise<HeapVector<Member<RTCRtpSendStream>>> replaceSendStreams() {
+    // TODO(crbug.com/345101934): Implement me.
+    return {};
+  }
 
   // ExecutionContextLifecycleObserver
   void ContextDestroyed() override;
