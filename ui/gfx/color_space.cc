705acbf07a08d4f1c8a7f28e9d5bcca4ffcad47b
Sida Zhu <zhusida@bytedance.com>
2024-06-26 20:45:27 +0000

    Video: Correct the bit depth return value for PIXEL_FROMAT_P{0|2|4}10
    These formats previously being named with PIXEL_FROMAT_P{0|2|4}16,
    while only P010 actually uses this format store data. Historically, because P016 always returns 16 bit depth, we have written some of the workarounds for this.
    
    The previous CL (https://crrev.com/c/5633141) just 
    completed the renaming, this CL intends to keep correct 
    the incorrect bit depth.
    
    Bug: 347597908
    Change-Id: I301f2ef39a9d3136231841b39b7d0a5854b99f53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5641797
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Sida Zhu <zhusida@bytedance.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320011}

--- ui/gfx/color_space.cc
+++ ui/gfx/color_space.cc
@@ -31,14 +31,6 @@ namespace gfx {
 
 namespace {
 
-// Videos that are from a 10 or 12 bit source, but are stored in a 16-bit
-// format (e.g, PIXEL_FORMAT_P016LE) will report having 16 bits per pixel.
-// Assume they have 10 bits per pixel.
-// https://crbug.com/1381100
-int BitDepthWithWorkaroundApplied(int bit_depth) {
-  return bit_depth == 16 ? 10 : bit_depth;
-}
-
 static bool FloatsEqualWithinTolerance(const float* a,
                                        const float* b,
                                        int n,
@@ -1025,7 +1017,6 @@ bool ColorSpace::GetPiecewiseHDRParams(float* sdr_joint,
 }
 
 SkM44 ColorSpace::GetTransferMatrix(int bit_depth) const {
-  bit_depth = BitDepthWithWorkaroundApplied(bit_depth);
   DCHECK_GE(bit_depth, 8);
   // If chroma samples are real numbers in the range of −0.5 to 0.5, an offset
   // of 0.5 is added to get real numbers in the range of 0 to 1. When
@@ -1112,7 +1103,6 @@ SkM44 ColorSpace::GetTransferMatrix(int bit_depth) const {
 }
 
 SkM44 ColorSpace::GetRangeAdjustMatrix(int bit_depth) const {
-  bit_depth = BitDepthWithWorkaroundApplied(bit_depth);
   DCHECK_GE(bit_depth, 8);
   switch (range_) {
     case RangeID::FULL:
@@ -1157,7 +1147,6 @@ SkM44 ColorSpace::GetRangeAdjustMatrix(int bit_depth) const {
 }
 
 bool ColorSpace::ToSkYUVColorSpace(int bit_depth, SkYUVColorSpace* out) const {
-  bit_depth = BitDepthWithWorkaroundApplied(bit_depth);
   switch (matrix_) {
     case MatrixID::BT709:
       *out = range_ == RangeID::FULL ? kRec709_Full_SkYUVColorSpace
@@ -1184,6 +1173,11 @@ bool ColorSpace::ToSkYUVColorSpace(int bit_depth, SkYUVColorSpace* out) const {
       if (bit_depth == 12) {
         *out = range_ == RangeID::FULL ? kBT2020_12bit_Full_SkYUVColorSpace
                                        : kBT2020_12bit_Limited_SkYUVColorSpace;
+        return true;
+      }
+      if (bit_depth == 16) {
+        *out = range_ == RangeID::FULL ? kBT2020_16bit_Full_SkYUVColorSpace
+                                       : kBT2020_16bit_Limited_SkYUVColorSpace;
         return true;
       }
       return false;
@@ -1222,6 +1216,11 @@ bool ColorSpace::ToSkYUVColorSpace(int bit_depth, SkYUVColorSpace* out) const {
       if (bit_depth == 12) {
         *out = range_ == RangeID::FULL ? kYCgCo_12bit_Full_SkYUVColorSpace
                                        : kYCgCo_12bit_Limited_SkYUVColorSpace;
+        return true;
+      }
+      if (bit_depth == 16) {
+        *out = range_ == RangeID::FULL ? kYCgCo_16bit_Full_SkYUVColorSpace
+                                       : kYCgCo_16bit_Limited_SkYUVColorSpace;
         return true;
       }
       return false;
