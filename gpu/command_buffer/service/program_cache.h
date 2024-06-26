317036385fc49274497a10f21e3a877f69cc6e69
Bartek Nowierski <bartekn@chromium.org>
2024-06-26 04:46:31 +0000

    [BRP] Un-rewrite ScopedCacheUse::cache_
    Turns out that rewriting it was unnecessary, as the embedding struct can be marked STACK_ALLOCATED, and we generally don't rewrite those. Since, STACK_ALLOCATED is viral, apply to ScopedContextOperation too.
    
    Change-Id: I9b00d171cd03082560517d626eca4893dbf643c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647252
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319567}

--- gpu/command_buffer/service/program_cache.h
+++ gpu/command_buffer/service/program_cache.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/hash/sha1.h"
 #include "base/memory/memory_pressure_listener.h"
-#include "base/memory/raw_ptr.h"
+#include "base/memory/stack_allocated.h"
 #include "base/synchronization/lock.h"
 #include "gpu/command_buffer/common/gl2_types.h"
 #include "gpu/gpu_gles2_export.h"
@@ -49,6 +49,8 @@ class GPU_GLES2_EXPORT ProgramCache {
   };
 
   class GPU_GLES2_EXPORT ScopedCacheUse {
+    STACK_ALLOCATED();
+
    public:
     ScopedCacheUse(ProgramCache* cache, CacheProgramCallback callback);
     // Disallow copy/assign as it is subtle and error prone (only one
@@ -62,7 +64,7 @@ class GPU_GLES2_EXPORT ProgramCache {
     ~ScopedCacheUse();
 
    private:
-    raw_ptr<ProgramCache> cache_;
+    ProgramCache* cache_;
   };
 
   explicit ProgramCache(size_t max_cache_size_bytes);
