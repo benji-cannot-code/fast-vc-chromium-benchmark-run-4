ce25a8659c21ca6e7fd534495544f74800e99ebc
Tina Wang <tinazwang@google.com>
2024-06-26 15:57:41 +0000

    [Code Health][iOS] Move ios/c/b/ui/start_surface to ui_bundled 1/3
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    This cl is the 1/3 cls for the file moving, because there is downstream
    reference to ios/c/b/ui/start_surface
    =>crrev/c/5652600
      crrev/i/7425316
      crrev/c/5655960
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324232531
    Change-Id: Ib3c0b95b36a54d9b0a04bf0822bdbffd3c37e478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5652600
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Tina Wang <tinazwang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319793}

---
+++ ios/chrome/browser/start_surface/ui_bundled/start_surface_features.mm
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/start_surface/ui_bundled/start_surface_features.h"
+
+#import "base/metrics/field_trial_params.h"
+
+namespace {
+
+// Default value for kReturnToStartSurfaceInactiveDurationInSeconds.
+constexpr base::TimeDelta kDefaultReturnToStartSurfaceInactiveDuration =
+    base::Hours(6);
+
+}  // anonymous namespace
+
+BASE_FEATURE(kStartSurface, "StartSurface", base::FEATURE_ENABLED_BY_DEFAULT);
+
+const char kReturnToStartSurfaceInactiveDurationInSeconds[] =
+    "ReturnToStartSurfaceInactiveDurationInSeconds";
+
+bool IsStartSurfaceEnabled() {
+  return base::FeatureList::IsEnabled(kStartSurface);
+}
+
+base::TimeDelta GetReturnToStartSurfaceDuration() {
+  return base::Seconds(base::GetFieldTrialParamByFeatureAsDouble(
+      kStartSurface, kReturnToStartSurfaceInactiveDurationInSeconds,
+      kDefaultReturnToStartSurfaceInactiveDuration.InSecondsF()));
+}
