7f7ae4de7902a8f4c1bfbd33f8aaabc653bcc18f
Kyungjun Lee <kyungjunlee@google.com>
2024-06-26 19:22:24 +0000

    Backlight OCR: record the most detected language in OCR data
    This CL adds a new metric to record the most detected language in OCR
    data when PDF pages are sent to the OCR model in Backlight. It also
    updates the FakeOpticalCharacterRecognizer class to set the language
    attribute in fake OCR data so that we can test this new metric and some
    of the existing tests accordingly.
    
    The metric recording logic is the same as that in PdfAccessibilityTree,
    so this CL moves it under the ScreenAI service code so that it can be
    shared between PdfAccessibilityTree and AXMediaAppUntrustedHandler.
    
    AX-Relnotes: n/a.
    Bug: 40904361
    Test: manually from chrome://histograms on DUT
    Test: out/cros/browser_tests --gtest_filter=*AXMediaAppUntrustedHandlerTest.*
    Change-Id: Ie104dfe5d1fdd4dd2641624caf5bf646b65917bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646450
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319945}

--- chrome/browser/accessibility/media_app/ax_media_app_untrusted_handler.cc
+++ chrome/browser/accessibility/media_app/ax_media_app_untrusted_handler.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_contents.h"
 #include "mojo/public/cpp/bindings/message.h"
+#include "services/screen_ai/public/cpp/metrics.h"
 #include "services/screen_ai/public/mojom/screen_ai_service.mojom.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/accessibility/ax_action_data.h"
@@ -959,6 +960,9 @@ void AXMediaAppUntrustedHandler::OnPageOcred(
   ui::AXTreeUpdate complete_tree_update = tree_update;
   if (!tree_update.nodes.empty()) {
     text_extracted_ = true;
+    screen_ai::RecordMostDetectedLanguageInOcrData(
+        "Accessibility.PdfOcr.MediaApp.MostDetectedLanguageInOcrData",
+        tree_update);
   } else {
     // We can't pass an empty update to `AXTree`s constructor, so we add an
     // empty root node instead.
