236a161789d4e6c69221a45297243967af74babb
Ilya Sherman <isherman@chromium.org>
2024-06-26 17:45:48 +0000

    [Cleanup] Call ModelQualityLogEntry::Upload() directly
    Routing through the OptimizationGuideKeyedService just adds an
    unnecessary layer of indirection. This wrapper will be removed in a
    follow-up. The only value add of the wrapper is to provide an API that
    can be mocked in tests, but it's clearer to provide a direct hook for
    tests.
    
    R=tluk@chromium.org
    
    Bug: 341806620
    Change-Id: I841e98bc464bd8b0d17130aa3a4a01f250150409
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5555169
    Auto-Submit: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Simon Pelchat <spelchat@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Ilya Sherman <isherman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319885}

---
+++ components/optimization_guide/core/model_quality/test_model_quality_logs_uploader_service.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/optimization_guide/core/model_quality/test_model_quality_logs_uploader_service.h"
+
+#include "base/memory/scoped_refptr.h"
+#include "services/network/public/cpp/shared_url_loader_factory.h"
+
+namespace optimization_guide {
+
+TestModelQualityLogsUploaderService::TestModelQualityLogsUploaderService(
+    PrefService* pref_service)
+    : ModelQualityLogsUploaderService(nullptr, pref_service) {}
+
+TestModelQualityLogsUploaderService::~TestModelQualityLogsUploaderService() =
+    default;
+
+bool TestModelQualityLogsUploaderService::CanUploadLogs(
+    UserVisibleFeatureKey feature) {
+  return true;
+}
+
+void TestModelQualityLogsUploaderService::UploadFinalizedLog(
+    std::unique_ptr<proto::LogAiDataRequest> log,
+    UserVisibleFeatureKey feature) {
+  uploaded_logs_.push_back(std::move(log));
+}
+
+}  // namespace optimization_guide
