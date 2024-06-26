4e5abafb97f51ad8d0ab725df0fc3b16764d3abb
Patrick Monette <pmonette@chromium.org>
2024-06-26 20:02:22 +0000

    [PM] Add NodeInlineData
    This is a replacement class for NodeAttachedDataOwnedByNodeType
    and NodeAttachedDataInternalOnNodeType that is easier to use.
    
    This change also migrates ProcessPriorityAggregator::Data to
    demonstrate its usage.
    
    Bug: 349425221
    Change-Id: If777749e806f83f65ebbe5851a384717e6ce594c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5631732
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319982}

--- components/performance_manager/graph/process_node_impl.cc
+++ components/performance_manager/graph/process_node_impl.cc
@@ -96,7 +96,6 @@ ProcessNodeImpl::~ProcessNodeImpl() {
   //                                  resolved.
   CHECK(worker_nodes_.empty());
   DCHECK(!frozen_frame_data_);
-  DCHECK(!process_priority_data_);
 }
 
 void ProcessNodeImpl::Bind(
@@ -438,8 +437,8 @@ void ProcessNodeImpl::OnBeforeLeavingGraph() {
 
 void ProcessNodeImpl::RemoveNodeAttachedData() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  DestroyNodeInlineDataStorage();
   frozen_frame_data_.Reset();
-  process_priority_data_.reset();
 }
 
 }  // namespace performance_manager
--- components/performance_manager/graph/process_node_impl.h
+++ components/performance_manager/graph/process_node_impl.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PERFORMANCE_MANAGER_GRAPH_PROCESS_NODE_IMPL_H_
 #define COMPONENTS_PERFORMANCE_MANAGER_GRAPH_PROCESS_NODE_IMPL_H_
 
-#include <memory>
 #include <optional>
 #include <string>
 
@@ -16,8 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/process_handle.h"
 #include "base/time/time.h"
 #include "base/types/pass_key.h"
+#include "components/performance_manager/decorators/process_priority_aggregator_data.h"
 #include "components/performance_manager/graph/node_attached_data.h"
 #include "components/performance_manager/graph/node_base.h"
+#include "components/performance_manager/graph/node_inline_data.h"
 #include "components/performance_manager/graph/properties.h"
 #include "components/performance_manager/public/browser_child_process_host_proxy.h"
 #include "components/performance_manager/public/graph/process_node.h"
@@ -52,7 +53,8 @@ struct BrowserProcessNodeTag {};
 class ProcessNodeImpl
     : public PublicNodeImpl<ProcessNodeImpl, ProcessNode>,
       public TypedNodeBase<ProcessNodeImpl, ProcessNode, ProcessNodeObserver>,
-      public mojom::ProcessCoordinationUnit {
+      public mojom::ProcessCoordinationUnit,
+      public SupportsNodeInlineData<ProcessPriorityAggregatorData> {
  public:
   using PassKey = base::PassKey<ProcessNodeImpl>;
 
@@ -163,7 +165,6 @@ class ProcessNodeImpl
  private:
   friend class FrozenFrameAggregatorAccess;
   friend class ProcessMetricsDecoratorAccess;
-  friend class ProcessPriorityAggregatorAccess;
 
   using AnyChildProcessHostProxy =
       absl::variant<RenderProcessHostProxy, BrowserChildProcessHostProxy>;
@@ -236,10 +237,6 @@ class ProcessNodeImpl
 
   // Inline storage for FrozenFrameAggregator user data.
   InternalNodeAttachedDataStorage<sizeof(uintptr_t) + 8> frozen_frame_data_
-      GUARDED_BY_CONTEXT(sequence_checker_);
-
-  // Inline storage for ProcessPriorityAggregator user data.
-  std::unique_ptr<NodeAttachedData> process_priority_data_
       GUARDED_BY_CONTEXT(sequence_checker_);
 
   base::WeakPtr<ProcessNodeImpl> weak_this_;
