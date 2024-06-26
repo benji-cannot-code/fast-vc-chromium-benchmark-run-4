e7649b9e369db49b75a570f22f62bf8825a81819
Jose Dapena Paz <jdapena@igalia.com>
2024-06-26 15:44:58 +0000

    IWYU: missing include for usage of std::variant in signin_utils.h
    Bug: 41455655
    Change-Id: I42bbe74ea847f66593b63198aad095a620d87d7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5656376
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319785}

--- chrome/browser/ui/webui/signin/signin_utils.h
+++ chrome/browser/ui/webui/signin/signin_utils.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SIGNIN_SIGNIN_UTILS_H_
 
 #include <string>
+#include <variant>
 
 #include "base/functional/callback.h"
 #include "base/time/time.h"
