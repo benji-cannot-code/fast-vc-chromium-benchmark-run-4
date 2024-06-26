e0557177fbe130b462511e35b709974bd25133c7
Marlon Facey <mfacey@chromium.org>
2024-06-26 20:05:39 +0000

    [Tab Resume 2.0] Update "See More" hover state and icon size
    Added row hover to "See More" button. Also updated icon/icon background
    size to be 24px/48px.
    
    
    Screenshot: http://screenshot/9ZSm5dvzz9e8FhH
    
    Bug: 348682138
    Change-Id: Iace700228461aec10efd5f28e97ea96ab0c32a57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661056
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319986}

--- chrome/browser/resources/new_tab_page/modules/v2/most_relevant_tab_resumption/module.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/most_relevant_tab_resumption/module.ts
@@ -211,7 +211,7 @@ private shouldShowDeviceIcon_:
   }
 
   private computeFaviconSize_(): number {
-    return 18;
+    return 24;
   }
 
   private computeShouldShowDeviceName_(tab: Tab): boolean {
