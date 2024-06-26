19a6f7b0c42354af24fb7f082a9c925214df3e72
Sahir Vellani <sahir.vellani@microsoft.com>
2024-06-26 02:51:18 +0000

    Deprecate expectedImprovement from DelegatedInkTrailPresenter
    This attribute was added so that we could potentially tell the developer
    about the improvement they could potentially expect when using this API.
    However, this is likely an avenue for finger printing, and we end up
    asking ourselves "why wouldn't they want it?" So it should probably just
    be removed. This is not expected to be a breaking change as
    expectedImprovement was not actually implemented.
    
    PR to update the spec: https://github.com/WICG/ink-enhancement/pull/34
    
    Change-Id: I8aed5e56f8f3ed174013d25848dc52e01927ae7a
    Bug: 347647410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5627855
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Sahir Vellani <sahir.vellani@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1319557}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/deprecation-issue-delegated-ink-expected-improvement.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const { session, dp } = await testRunner.startBlank(`Tests that deprecation issues are reported`);
+  await dp.Audits.enable();
+  const promise = dp.Audits.onceIssueAdded();
+  // Update this to test different deprecated code if WebFeature::kRangeExpand is removed.
+  session.evaluateAsync(`
+      navigator.ink.requestPresenter().then(e => { console.log(e.expectedImprovement); });
+  `);
+  const result = await promise;
+  testRunner.log(result.params, "Inspector issue: ");
+  testRunner.completeTest();
+})
