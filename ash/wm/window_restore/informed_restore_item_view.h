e5363e7379f3581c7cf06601f8e02ad880e6dbe1
Elijah Hewer <hewer@chromium.org>
2024-06-26 18:03:17 +0000

    Post Login: Rename remaining pine view classes
    Rename remaining Pine view classes to InformedRestore*
    view classes.
    
    Bug: b/328779923
    Test: CQ
    Change-Id: I8664b3c7ed8cfd6e7a0c003f3fdf4d4f0c4d218c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657211
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319898}

--- ash/wm/window_restore/informed_restore_item_view.h
+++ ash/wm/window_restore/informed_restore_item_view.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_WM_WINDOW_RESTORE_PINE_ITEM_VIEW_H_
-#define ASH_WM_WINDOW_RESTORE_PINE_ITEM_VIEW_H_
+#ifndef ASH_WM_WINDOW_RESTORE_INFORMED_RESTORE_ITEM_VIEW_H_
+#define ASH_WM_WINDOW_RESTORE_INFORMED_RESTORE_ITEM_VIEW_H_
 
 #include "ash/ash_export.h"
 #include "ash/wm/window_restore/informed_restore_contents_data.h"
@@ -24,8 +24,8 @@ namespace ash {
 // screenshot preview.
 //
 // TODO(hewer): Add ASCII art.
-class ASH_EXPORT PineItemView : public views::BoxLayoutView {
-  METADATA_HEADER(PineItemView, views::BoxLayoutView)
+class ASH_EXPORT InformedRestoreItemView : public views::BoxLayoutView {
+  METADATA_HEADER(InformedRestoreItemView, views::BoxLayoutView)
 
  public:
   using IndexedImagePair = std::pair</*index=*/int, gfx::ImageSkia>;
@@ -35,15 +35,15 @@ class ASH_EXPORT PineItemView : public views::BoxLayoutView {
   using IndexedImageCallback =
       base::OnceCallback<void(const IndexedImagePair&)>;
 
-  PineItemView(const InformedRestoreContentsData::AppInfo& app_info,
+  InformedRestoreItemView(const InformedRestoreContentsData::AppInfo& app_info,
                bool inside_screenshot);
-  PineItemView(const PineItemView&) = delete;
-  PineItemView& operator=(const PineItemView&) = delete;
-  ~PineItemView() override;
+  InformedRestoreItemView(const InformedRestoreItemView&) = delete;
+  InformedRestoreItemView& operator=(const InformedRestoreItemView&) = delete;
+  ~InformedRestoreItemView() override;
 
   const views::Label* title_label_view() const { return title_label_view_; }
 
-  base::WeakPtr<PineItemView> GetWeakPtr() {
+  base::WeakPtr<InformedRestoreItemView> GetWeakPtr() {
     return weak_ptr_factory_.GetWeakPtr();
   }
 
@@ -70,9 +70,9 @@ class ASH_EXPORT PineItemView : public views::BoxLayoutView {
 
   base::CancelableTaskTracker cancelable_favicon_task_tracker_;
 
-  base::WeakPtrFactory<PineItemView> weak_ptr_factory_{this};
+  base::WeakPtrFactory<InformedRestoreItemView> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
 
-#endif  // ASH_WM_WINDOW_RESTORE_PINE_ITEM_VIEW_H_
+#endif  // ASH_WM_WINDOW_RESTORE_INFORMED_RESTORE_ITEM_VIEW_H_
--- ash/wm/window_restore/informed_restore_unittest.cc
+++ ash/wm/window_restore/informed_restore_unittest.cc
@@ -27,15 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_restore/informed_restore_app_image_view.h"
 #include "ash/wm/window_restore/informed_restore_contents_data.h"
 #include "ash/wm/window_restore/informed_restore_contents_view.h"
+#include "ash/wm/window_restore/informed_restore_item_view.h"
+#include "ash/wm/window_restore/informed_restore_items_container_view.h"
+#include "ash/wm/window_restore/informed_restore_items_overflow_view.h"
+#include "ash/wm/window_restore/informed_restore_screenshot_icon_row_view.h"
 #include "ash/wm/window_restore/informed_restore_test_api.h"
 #include "ash/wm/window_restore/informed_restore_test_base.h"
 #include "ash/wm/window_restore/pine_constants.h"
 #include "ash/wm/window_restore/pine_context_menu_model.h"
 #include "ash/wm/window_restore/pine_controller.h"
-#include "ash/wm/window_restore/pine_item_view.h"
-#include "ash/wm/window_restore/pine_items_container_view.h"
-#include "ash/wm/window_restore/pine_items_overflow_view.h"
-#include "ash/wm/window_restore/pine_screenshot_icon_row_view.h"
 #include "ash/wm/window_restore/window_restore_metrics.h"
 #include "ash/wm/window_restore/window_restore_util.h"
 #include "base/command_line.h"
@@ -129,8 +129,8 @@ class InformedRestoreTest : public InformedRestoreTestBase {
     return GetViewCount(root, pine::kOverflowImageViewID);
   }
 
-  const PineScreenshotIconRowView* GetScreenshotIconRowView() const {
-    return static_cast<const PineScreenshotIconRowView*>(
+  const InformedRestoreScreenshotIconRowView* GetScreenshotIconRowView() const {
+    return static_cast<const InformedRestoreScreenshotIconRowView*>(
         GetContentsView()->GetViewByID(pine::kScreenshotIconRowViewID));
   }
 
@@ -342,7 +342,7 @@ TEST_F(InformedRestoreTest, ScreenshotIconRowMaxElements) {
   // time to show the pine dialog.
   EXPECT_FALSE(base::PathExists(file_path));
   // Screenshot icon row should be shown when there is a screenshot.
-  const PineScreenshotIconRowView* screenshot_icon_row_view =
+  const InformedRestoreScreenshotIconRowView* screenshot_icon_row_view =
       GetScreenshotIconRowView();
   EXPECT_TRUE(screenshot_icon_row_view);
   // The icon row should show all the elements and all of them should be shown
@@ -375,7 +375,7 @@ TEST_F(InformedRestoreTest, ScreenshotIconRowExceedMaxElements) {
       Shell::Get()->pine_controller()->contents_data();
   EXPECT_TRUE(contents_data && !contents_data->image.isNull());
   // Screenshot icon row should be shown when there is a screenshot.
-  const PineScreenshotIconRowView* screenshot_icon_row_view =
+  const InformedRestoreScreenshotIconRowView* screenshot_icon_row_view =
       GetScreenshotIconRowView();
   EXPECT_TRUE(screenshot_icon_row_view);
   // The icon row should still have at most 5 number of items, but only 4 of
@@ -673,14 +673,14 @@ TEST_F(InformedRestoreTest, ClickRestoreToExit) {
   EXPECT_FALSE(OverviewGridTestApi(overview_grid).pine_widget());
 }
 
-TEST_F(InformedRestoreTest, PineItemView) {
+TEST_F(InformedRestoreTest, InformedRestoreItemView) {
   InformedRestoreContentsData::AppInfo app_info(
       "TEST_ID", "TEST_TITLE", /*window_id=*/0,
       std::vector<GURL>{GURL(), GURL(), GURL(), GURL()}, 4u, 0);
 
   // Test when the tab count is within regular limits.
-  auto item_view =
-      std::make_unique<PineItemView>(app_info, /*inside_screenshot=*/false);
+  auto item_view = std::make_unique<InformedRestoreItemView>(
+      app_info, /*inside_screenshot=*/false);
   EXPECT_EQ(
       4u,
       item_view->GetViewByID(pine::kFaviconContainerViewID)->children().size());
@@ -688,8 +688,8 @@ TEST_F(InformedRestoreTest, PineItemView) {
 
   // Test the when the tab count has overflow.
   app_info.tab_count = 10u;
-  item_view =
-      std::make_unique<PineItemView>(app_info, /*inside_screenshot=*/false);
+  item_view = std::make_unique<InformedRestoreItemView>(
+      app_info, /*inside_screenshot=*/false);
   EXPECT_EQ(
       5u,
       item_view->GetViewByID(pine::kFaviconContainerViewID)->children().size());
@@ -813,7 +813,8 @@ TEST_F(InformedRestoreTest, LayoutLandscapeToPortrait) {
 
   // In portrait mode, the `InformedRestoreContentsView` should have three
   // children: the title and description container (header), the
-  // `PineItemsContainerView`, and the buttons container (footer).
+  // `InformedRestoreItemsContainerView`, and the buttons container
+  // (footer).
   contents_view = views::AsViewClass<InformedRestoreContentsView>(
       pine_widget->GetContentsView());
   ASSERT_TRUE(contents_view);
@@ -841,7 +842,8 @@ TEST_F(InformedRestoreTest, LayoutPortraitToLandscape) {
 
   // In portrait mode, the `InformedRestoreContentsView` should have three
   // children: the title and description container (header), the
-  // `PineItemsContainerView`, and the buttons container (footer).
+  // `InformedRestoreItemsContainerView`, and the buttons container
+  // (footer).
   auto* contents_view = views::AsViewClass<InformedRestoreContentsView>(
       pine_widget->GetContentsView());
   ASSERT_TRUE(contents_view);
@@ -943,8 +945,9 @@ TEST_F(PineAppIconTest, UpdateAfterSessionStarted) {
                                          gfx::Image(test_icon),
                                          /*max_deviation=*/0));
 
-  const PineItemView* item_view = views::AsViewClass<PineItemView>(
-      GetContentsView()->GetViewByID(pine::kItemViewID));
+  const InformedRestoreItemView* item_view =
+      views::AsViewClass<InformedRestoreItemView>(
+          GetContentsView()->GetViewByID(pine::kItemViewID));
   ASSERT_TRUE(item_view);
   ASSERT_TRUE(item_view->title_label_view());
   EXPECT_TRUE(item_view->title_label_view()->GetText().empty());
