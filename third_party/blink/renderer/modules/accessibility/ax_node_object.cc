4b45c9eb51da0c3c0d85ec117f079f146f06ea35
Morten Stenshorne <mstensho@chromium.org>
2024-06-26 17:31:41 +0000

    Clean up NG IsFoo() functions in LayoutObject / LayoutBox:
    Rename IsListItemIncludingNG() to IsListItem().
    Rename IsFlexItemIncludingNG() to IsFlexItem().
    Rename IsGridItemIncludingNG() to IsGridItem().
    
    Remove IsBoxListMarkerIncludingNG(), and use IsLayoutOutsideListMarker()
    instead.
    
    Bug: 40056555
    Change-Id: I2520173f2eae865f7808acde07954dee02b74c8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660436
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319873}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -1841,7 +1841,7 @@ ax::mojom::blink::Role AXNodeObject::RoleFromLayoutObjectOrNode() const {
     return ax::mojom::blink::Role::kListMarker;
   }
 
-  if (GetLayoutObject()->IsListItemIncludingNG()) {
+  if (GetLayoutObject()->IsListItem()) {
     return ax::mojom::blink::Role::kListItem;
   }
   if (GetLayoutObject()->IsBR()) {
@@ -7623,7 +7623,7 @@ AXObject* AXNodeObject::NextOnLine() const {
     return nullptr;
   }
 
-  if (layout_object->IsBoxListMarkerIncludingNG()) {
+  if (layout_object->IsLayoutOutsideListMarker()) {
     // A list marker should be followed by a list item on the same line.
     // Note that pseudo content is always included in the tree, so
     // NextSiblingIncludingIgnored() will succeed.
@@ -7746,7 +7746,7 @@ AXObject* AXNodeObject::PreviousOnLine() const {
         previous_sibling, false);
   }
 
-  if (layout_object->IsBoxListMarkerIncludingNG() ||
+  if (layout_object->IsLayoutOutsideListMarker() ||
       !layout_object->IsInLayoutNGInlineFormattingContext()) {
     return nullptr;
   }
