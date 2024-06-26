9328da907125e642543c1f863bf71f73d83dae32
Steinar H. Gunderson <sesse@chromium.org>
2024-06-26 11:21:16 +0000

    Implement use-counting for :nth-child(an + b of S).
    Fixed: 338008903
    Change-Id: I1d76d88aa30e604b0b055179049691a1afeedaf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5658251
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319674}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -2464,6 +2464,11 @@ static void RecordUsageAndDeprecationsOneSelector(
     case CSSSelector::kPseudoUserInvalid:
       feature = WebFeature::kCSSSelectorUserInvalid;
       break;
+    case CSSSelector::kPseudoNthChild:
+      if (selector->SelectorList()) {
+        feature = WebFeature::kCSSSelectorNthChildOfSelector;
+      }
+      break;
     default:
       break;
   }
