ae427a6e68ebc2dab81895a00341e43122426c59
Jose Dapena Paz <jdapena@igalia.com>
2024-06-26 15:42:15 +0000

    IWYU: missing include for usage of uint64_t in compare_utils.h
    Bug: 41455655
    Change-Id: I4901e84190a1439b1d4a3078ca448b656de0de67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657113
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319784}

--- components/commerce/core/compare/compare_utils.h
+++ components/commerce/core/compare/compare_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_COMMERCE_CORE_COMPARE_COMPARE_UTILS_H_
 #define COMPONENTS_COMMERCE_CORE_COMPARE_COMPARE_UTILS_H_
 
+#include <cstdint>
 #include <string>
 #include <vector>
 
