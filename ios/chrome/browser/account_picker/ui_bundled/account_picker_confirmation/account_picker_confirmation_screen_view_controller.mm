156b52b4d839fee369d68fcefed7cbf447c866be
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-06-26 15:26:22 +0000

    [iOS] Save to photos - account default choice
    The aim of this CL is to change the default account choice prompt, from
    "Ask which account to use every time" to "Save to this account every
    time".
    
    New behavior:
       -> Account picker vc: https://drive.google.com/file/d/1nz_puEXBMisigGG329Znkl6SYeo5EiQO/view?usp=sharing&resourcekey=0-u4QMUmMONZrziQca9XP-dg
       -> Download setting:
    https://drive.google.com/file/d/1hGXSoN-Nj4H75bXEvanv5L6G6zC6KQk-/view?usp=sharing&resourcekey=0-qEOLbfNWhikfddZ9DxuBvw
    
    Old behavior:
       -> Account picker vc: https://drive.google.com/file/d/1q1Z2raSoO8RXovQmD4_f6IvWEejdwvau/view?usp=sharing&resourcekey=0-dsxz2haTx9HMjm3gUraFHA
       -> Download setting: https://drive.google.com/file/d/1GLZJD0q6c3ogldrN9Ok5zINuFHB3ZFcY/view?usp=sharing&resourcekey=0-tT7ZNy5Y39OZQ4M72hY7dA
    
    
    
    Bug: 348371964
    Change-Id: I179526e8059186c45c50d32717649e4c4d3e7506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5649266
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319774}

--- ios/chrome/browser/account_picker/ui_bundled/account_picker_confirmation/account_picker_confirmation_screen_view_controller.mm
+++ ios/chrome/browser/account_picker/ui_bundled/account_picker_confirmation/account_picker_confirmation_screen_view_controller.mm
@@ -340,6 +340,9 @@ UILabel* CreateGooglePhotosTitleLabel() {
                             action:@selector(askEveryTimeSwitchAction:)
                   forControlEvents:UIControlEventValueChanged];
     _askEveryTimeSwitch.on = YES;
+    if (IsSaveToPhotosAccountPickerImprovementEnabled()) {
+      _askEveryTimeSwitch.on = NO;
+    }
     [_askEveryTimeSwitch
         setContentCompressionResistancePriority:UILayoutPriorityRequired
                                         forAxis:
@@ -437,9 +440,11 @@ UILabel* CreateGooglePhotosTitleLabel() {
 }
 
 - (void)askEveryTimeSwitchAction:(id)sender {
+  BOOL shouldAskEveryTime = !(_askEveryTimeSwitch.on ==
+                              IsSaveToPhotosAccountPickerImprovementEnabled());
   [_actionDelegate
       accountPickerConfirmationScreenViewController:self
-                                    setAskEveryTime:_askEveryTimeSwitch.on];
+                                    setAskEveryTime:shouldAskEveryTime];
 }
 
 - (void)primaryButtonAction:
