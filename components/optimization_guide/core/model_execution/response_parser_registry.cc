104d108a0336dd99b641a26f326fbd86f8d55dbe
Steven Holte <holte@google.com>
2024-06-26 19:49:11 +0000

    Register the JSON response parser type.
    Bug: 327062709
    
    Change-Id: I66671bdfd28495abfe04972e5e43137ccab434af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646351
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319973}

--- components/optimization_guide/core/model_execution/response_parser_registry.cc
+++ components/optimization_guide/core/model_execution/response_parser_registry.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/no_destructor.h"
+#include "components/optimization_guide/core/model_execution/json_response_parser.h"
 #include "components/optimization_guide/core/model_execution/simple_response_parser.h"
 
 namespace optimization_guide {
@@ -18,6 +19,8 @@ ResponseParserRegistry::ResponseParserRegistry() {
                      std::make_unique<SimpleResponseParserFactory>());
   factories_.emplace(proto::PARSER_KIND_SIMPLE,
                      std::make_unique<SimpleResponseParserFactory>());
+  factories_.emplace(proto::PARSER_KIND_JSON,
+                     std::make_unique<JsonResponseParserFactory>());
 }
 ResponseParserRegistry::~ResponseParserRegistry() = default;
 
