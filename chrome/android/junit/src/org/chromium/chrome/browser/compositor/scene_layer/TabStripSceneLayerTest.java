245732d7afe8f8586d31cc249ec8da2a5dffad40
Neil Coronado <nemco@google.com>
2024-06-26 22:41:35 +0000

    [TabStrip] Ensure we only attach the compositor buttons once
    Ensure we only attach the compositor buttons once.
    -Similarly, ensure that we only attach their backgrounds once.
    -This is to guarantee the initial z-ordering. e.g. this prevents the
     buttons from unexpectedly showing over top the scrim during the tab
     strip hiding transition.
    -Also includes general cleanup (removing unused params, unused method,
     creating a helper method, etc.).
    
    Bug: 348514158
    Change-Id: I40a86ab3ccc991e7ab1a18399c7c2e6016e2d6b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657491
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Neil Coronado <nemco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320071}

--- chrome/android/junit/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/compositor/scene_layer/TabStripSceneLayerTest.java
@@ -144,20 +144,17 @@ public class TabStripSceneLayerTest {
 
         // Verify JNI calls.
         verify(mTabStripSceneMock)
-                .updateModelSelectorButtonBackground(
+                .updateModelSelectorButton(
                         1L,
                         mTabStripSceneLayer,
                         mModelSelectorButton.getResourceId(),
                         ((TintedCompositorButton) mModelSelectorButton).getBackgroundResourceId(),
                         mModelSelectorButton.getDrawX(),
                         mModelSelectorButton.getDrawY(),
-                        mModelSelectorButton.getWidth() * mDpToPx,
-                        mModelSelectorButton.getHeight() * mDpToPx,
-                        false,
                         true,
+                        false,
                         ((TintedCompositorButton) mModelSelectorButton).getTint(),
                         ((TintedCompositorButton) mModelSelectorButton).getBackgroundTint(),
-                        false,
                         mModelSelectorButton.getOpacity(),
                         mResourceManager);
         verify(mTabStripSceneMock)
@@ -166,12 +163,12 @@ public class TabStripSceneLayerTest {
                         eq(mTabStripSceneLayer),
                         /* resourceId= */ anyInt(),
                         /* backgroundResourceId= */ anyInt(),
-                        /* isHovered= */ eq(mNewTabButton.isHovered()),
                         /* x= */ eq(mNewTabButton.getDrawX() * mDpToPx),
                         /* y= */ eq(mNewTabButton.getDrawY() * mDpToPx),
                         /* topPadding= */ eq(topPadding),
                         /* touchTargetOffset= */ anyFloat(),
                         /* visible= */ eq(mNewTabButton.isVisible()),
+                        /* isHovered= */ eq(mNewTabButton.isHovered()),
                         /* tint= */ anyInt(),
                         /* backgroundTint= */ anyInt(),
                         /* buttonAlpha= */ anyFloat(),
