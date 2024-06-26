d0847a1528675f22e2499949da4fe3467ca33f5b
Florian Jacky <fjacky@google.com>
2024-06-26 14:54:00 +0000

    Add additional context to Subpage buttons for permissions that have a subpage
    This mainly addresses an accessibility concern, however, using the same string as tooltip also improves the experience of other users.
    
    Bug: 339859794
    Change-Id: I629e24ac6a72e9ec530e93fc72c840aee55e3a17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654130
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Auto-Submit: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Florian Jacky <fjacky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1319755}

--- chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
+++ chrome/browser/ui/views/page_info/permission_toggle_row_view.cc
@@ -211,8 +211,8 @@ void PermissionToggleRowView::InitForUserSource(
             },
             base::Unretained(this)),
         vector_icons::kLaunchIcon);
-    subpage_button->SetTooltipText(l10n_util::GetStringUTF16(
-        IDS_PAGE_INFO_PERMISSIONS_SUBPAGE_BUTTON_TOOLTIP));
+    subpage_button->SetTooltipText(
+        PageInfoUI::PermissionSubpageButtonTooltipString(permission_.type));
     views::InstallCircleHighlightPathGenerator(subpage_button.get());
     subpage_button->SetMinimumImageSize({icon_size, icon_size});
     subpage_button->SetFlipCanvasOnPaintForRTLUI(false);
@@ -235,8 +235,8 @@ void PermissionToggleRowView::InitForUserSource(
             },
             base::Unretained(this)),
         vector_icons::kSubmenuArrowIcon);
-    subpage_button->SetTooltipText(l10n_util::GetStringUTF16(
-        IDS_PAGE_INFO_PERMISSIONS_SUBPAGE_BUTTON_TOOLTIP));
+    subpage_button->SetTooltipText(
+        PageInfoUI::PermissionSubpageButtonTooltipString(permission_.type));
     views::InstallCircleHighlightPathGenerator(subpage_button.get());
     subpage_button->SetMinimumImageSize({icon_size, icon_size});
     subpage_button->SetFlipCanvasOnPaintForRTLUI(false);
