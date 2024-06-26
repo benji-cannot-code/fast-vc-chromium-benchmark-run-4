301344ad0add812cf130aae3e5c55031a5acb199
Jonathan Ross <jonross@chromium.org>
2024-06-26 16:26:56 +0000

    Remove obsolete UMAs from CompositorTimingHistory
    There were a few histograms here still being calculated. However they
    have long been expired and removed from histograms.xml.
    
    This change removes them, and cleans up the classes around them. This
    leaves Scheduling.Renderer.DrawInterval2 intact.
    
    Bug: 338977417
    Change-Id: I3c9e2a1babc1aa6af760d61f61df33deddbd5043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5648774
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319818}

--- cc/metrics/compositor_timing_history.h
+++ cc/metrics/compositor_timing_history.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_METRICS_COMPOSITOR_TIMING_HISTORY_H_
 #define CC_METRICS_COMPOSITOR_TIMING_HISTORY_H_
 
-#include <memory>
-#include <vector>
-
 #include "base/memory/raw_ptr.h"
 #include "base/time/time.h"
 #include "cc/base/rolling_time_delta_history.h"
@@ -67,8 +64,6 @@ class CC_EXPORT CompositorTimingHistory {
   void SetRecordingEnabled(bool enabled);
 
   // Events to be timed.
-  void WillBeginImplFrame(const viz::BeginFrameArgs& args,
-                          base::TimeTicks now);
   void WillFinishImplFrame(bool needs_redraw);
   void BeginImplFrameNotExpectedSoon();
   void WillBeginMainFrame(const viz::BeginFrameArgs& args);
@@ -99,7 +94,6 @@ class CC_EXPORT CompositorTimingHistory {
 
   void SetCompositorDrawingContinuously(bool active);
 
-  static std::unique_ptr<UMAReporter> CreateUMAReporter(UMACategory category);
   virtual base::TimeTicks Now() const;
 
   bool using_synchronous_renderer_compositor_;
@@ -159,7 +153,7 @@ class CC_EXPORT CompositorTimingHistory {
 
   bool pending_tree_is_impl_side_;
 
-  std::unique_ptr<UMAReporter> uma_reporter_;
+  const UMACategory uma_category_;
 
   // Owned by LayerTreeHost and is destroyed when LayerTreeHost is destroyed.
   raw_ptr<RenderingStatsInstrumentation, DanglingUntriaged>
--- cc/metrics/compositor_timing_history_unittest.cc
+++ cc/metrics/compositor_timing_history_unittest.cc
@@ -5,7 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/metrics/compositor_timing_history.h"
 
-#include "base/logging.h"
+#include <memory>
+
 #include "base/memory/raw_ptr.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
