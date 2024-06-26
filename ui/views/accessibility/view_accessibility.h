48c21f939159b6189e61e3b3a762e930bb6b95eb
Evelynn Kaplan <evelynn.kaplan@microsoft.com>
2024-06-26 18:29:22 +0000

    [views-ax] Introduce setter for kMultiselectable
    This CL adds a new setter for `IsMultiselectable` to the
    `ViewAccessibility` class, which is a task that is part of the ViewsAX
    project. More about that project here:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    A follow-up CL will add callers of this new setter.
    
    Bug: 325137417
    Change-Id: If67d9f210a1446ee6148805dbc89ff2a6b028183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651664
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Evelynn Kaplan <evelynn.kaplan@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1319913}

--- ui/views/accessibility/view_accessibility.h
+++ ui/views/accessibility/view_accessibility.h
@@ -238,6 +238,8 @@ class VIEWS_EXPORT ViewAccessibility : public WidgetObserver {
 
   void SetIsSelected(bool selected);
 
+  void SetIsMultiselectable(bool multiselectable);
+
   void SetIsHovered(bool is_hovered);
   bool GetIsHovered() const;
 
