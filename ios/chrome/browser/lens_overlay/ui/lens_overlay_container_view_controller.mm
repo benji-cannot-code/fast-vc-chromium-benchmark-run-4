4f83cfb769cb3678ff902c35093bf90ebec9f158
Ameur Hosni <ameurhosni@google.com>
2024-06-26 15:08:59 +0000

    [IOS][LO] Adjust the close button top padding to match mocks
    Bug: 347236349
    Change-Id: If4499cd648f2272035c6bdc9afa48380cc74df9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5658196
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1319766}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // The close button top padding.
-const CGFloat closeButtonTopPadding = 54.0;
+const CGFloat closeButtonTopPadding = 10.0;
 // The close button trailing padding.
 const CGFloat closeButtonTrailingPadding = 16.0;
 
