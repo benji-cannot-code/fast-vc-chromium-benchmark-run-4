32ec06f5c67b4894f8e936a9ca3e55253b99ac14
Taylor Bergquist <tbergquist@chromium.org>
2024-06-26 21:08:27 +0000

    [Customize Toolbar] Add reset to default button.
    Bug: 323964533
    Change-Id: I3748efff5440910b8c9009ea8050112db7105f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637717
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320025}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.h
@@ -34,6 +34,7 @@ class CustomizeToolbarHandler
   void ListCategories(ListCategoriesCallback callback) override;
   void PinAction(side_panel::customize_chrome::mojom::ActionId action_id,
                  bool pin) override;
+  void ResetToDefault() override;
 
   // PinnedToolbarActionsModel::Observer:
   void OnActionAdded(const actions::ActionId& id) override;
