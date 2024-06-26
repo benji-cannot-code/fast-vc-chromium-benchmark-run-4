f01e420f6d12ef01b9a756e73fd88ee70b5f6cc1
Shu Yang <shuyng@google.com>
2024-06-26 20:31:42 +0000

    Clean up “Chrome will remember your choice” IPH
    This IPH requires usage of RDS tab-level settings within 180 days, which
    has been replaced by domain-level settings since Feb 2023.
    
    Bug: 40917998
    Change-Id: Ie61b7465f1692d0bc8211779475cc41023f08ee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5647282
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320002}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -70,7 +70,6 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHReadLaterAppMenuBookmarkThisPageFeature,
     &kIPHReadLaterAppMenuBookmarksFeature,
     &kIPHReadLaterBottomSheetFeature,
-    &kIPHRequestDesktopSiteAppMenuFeature,
     &kIPHRequestDesktopSiteDefaultOnFeature,
     &kIPHRequestDesktopSiteExceptionsGenericFeature,
     &kIPHRequestDesktopSiteWindowSettingFeature,
