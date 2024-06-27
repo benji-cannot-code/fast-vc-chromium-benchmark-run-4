d06c8c9f7fc43d2d21a01adef94d8f871864d5a3
Moe Ahmadi <mahmadi@chromium.org>
2024-06-27 19:55:01 +0000

    Revert "Use EnterpriseCompanionStatus for mojom interface"
    This reverts commit 87f29970bbf75f5b3469db8f4768cbb8dbc9b151.
    
    Reason for revert: breaking mac-arm64-archive-rel
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac-arm64-archive-rel/31191/overview
    
    Original change's description:
    > Use EnterpriseCompanionStatus for mojom interface
    >
    > IPC calls should return the canonical status type:
    > EnterpriseCompanionStatus.
    >
    > Changing the stable mojom in a backwards-incompatible way here is
    > permissible as the project has not shipped. Presubmits are disabled
    > here because there is otherwise no way to supress the
    > check_stable_mojom_compatibility presubmit.
    >
    > No-Presubmit: true
    > Bug: 342180612
    > Change-Id: Ic055eeb51d71d1f654738863a9dff544ff2f6fe3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5650706
    > Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Reviewed-by: S Ganesh <ganesh@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1320563}
    
    Bug: 342180612
    Change-Id: Ic731bdaf73b1b2a4ed42bda21bf3a554320b986e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664196
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Owners-Override: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320575}

--- chrome/enterprise_companion/enterprise_companion_service_stub_unittest.cc
+++ chrome/enterprise_companion/enterprise_companion_service_stub_unittest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/enterprise_companion/enterprise_companion_client.h"
 #include "chrome/enterprise_companion/enterprise_companion_service.h"
 #include "chrome/enterprise_companion/ipc_support.h"
-#include "chrome/enterprise_companion/mojom/enterprise_companion.mojom-forward.h"
 #include "chrome/enterprise_companion/mojom/enterprise_companion.mojom.h"
 #include "components/named_mojo_ipc_server/connection_info.h"
 #include "components/named_mojo_ipc_server/named_mojo_ipc_server_client_util.h"
@@ -142,7 +141,8 @@ TEST_F(EnterpriseCompanionServiceStubTest, ServiceReachable) {
   start_run_loop.Run(FROM_HERE);
 
   base::Process child_process = SpawnClient();
-  EXPECT_EQ(WaitForProcess(child_process, /*should_exit=*/true), 0);
+  EXPECT_EQ(WaitForProcess(child_process, /*should_exit=*/true),
+            static_cast<int>(mojom::EnterpriseCompanion::Result::kSuccess));
 }
 
 TEST_F(EnterpriseCompanionServiceStubTest, UntrustedCallerRejected) {
@@ -185,9 +185,11 @@ MULTIPROCESS_TEST_MAIN(EnterpriseCompanionClient) {
   base::RunLoop wait_for_response_run_loop;
   int32_t result_code = -1;
   remote->Shutdown(
-      base::BindLambdaForTesting([&result_code](mojom::StatusPtr status) {
-        result_code = static_cast<int>(status->code);
-      }).Then(wait_for_response_run_loop.QuitClosure()));
+      base::BindLambdaForTesting(
+          [&result_code](mojom::EnterpriseCompanion::Result result) {
+            result_code = static_cast<int>(result);
+          })
+          .Then(wait_for_response_run_loop.QuitClosure()));
   wait_for_response_run_loop.Run(FROM_HERE);
 
   return result_code;
