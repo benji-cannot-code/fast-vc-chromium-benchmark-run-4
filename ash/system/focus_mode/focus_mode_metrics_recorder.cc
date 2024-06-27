0b650e05e558d4c72053096576cb1969bacbbdfd
Hongyu Long <hongyulong@chromium.org>
2024-06-27 19:04:38 +0000

    focus-mode: Add Ash.FocusMode.PlaylistsDuringSession
    This CL will add a histogram to count the number of playlists played
    during a session.
    
    Bug: b/348033369
    Test: browser test
    Change-Id: I3e9b2f2e4923777c5601dadac2d99ed30196eb52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5655834
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320545}

--- ash/system/focus_mode/focus_mode_metrics_recorder.cc
+++ ash/system/focus_mode/focus_mode_metrics_recorder.cc
@@ -75,6 +75,12 @@ void RecordTasksCompletedHistogram(const int tasks_completed_count) {
       tasks_completed_count);
 }
 
+void RecordPlaylistsPlayedHistogram(const int playlists_played_count) {
+  base::UmaHistogramCounts100(
+      focus_mode_histogram_names::kCountPlaylistsPlayedDuringSession,
+      playlists_played_count);
+}
+
 void RecordDNDStateOnFocusEndHistogram(
     bool has_user_interactions_on_dnd_in_focus_session) {
   auto* message_center = message_center::MessageCenter::Get();
@@ -254,6 +260,8 @@ void FocusModeMetricsRecorder::SetHasSelectedSoundType(
     case focus_mode_util::SoundType::kNone:
       NOTREACHED_IN_MIGRATION();
   }
+
+  playlists_played_count_++;
 }
 
 void FocusModeMetricsRecorder::RecordHistogramsOnStart(
@@ -269,6 +277,7 @@ void FocusModeMetricsRecorder::RecordHistogramsOnStart(
 void FocusModeMetricsRecorder::RecordHistogramsOnEnd() {
   RecordTasksSelectedHistogram(tasks_selected_count_);
   RecordTasksCompletedHistogram(tasks_completed_count_);
+  RecordPlaylistsPlayedHistogram(playlists_played_count_);
   RecordDNDStateOnFocusEndHistogram(
       has_user_interactions_on_dnd_in_focus_session_);
 
