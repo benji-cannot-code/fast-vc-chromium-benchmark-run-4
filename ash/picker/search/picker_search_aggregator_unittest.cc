6cf99100dc9a654d0a63e4d3e1638a10b050fda6
mlcui <mlcui@google.com>
2024-06-27 07:55:16 +0000

    Add clipboard to search aggregator test
    This will be eventually split out into its own test due to the
    special-casing of suggestions.
    
    Bug: b:348067874
    Change-Id: I6a6a21001945d87e668aab0f08457353036e9d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657838
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320205}

--- ash/picker/search/picker_search_aggregator_unittest.cc
+++ ash/picker/search/picker_search_aggregator_unittest.cc
@@ -93,6 +93,10 @@ INSTANTIATE_TEST_SUITE_P(
             .section_type = PickerSectionType::kSuggestions,
         },
         TestCase{
+            .source = PickerSearchSource::kClipboard,
+            .section_type = PickerSectionType::kSuggestions,
+        },
+        TestCase{
             .source = PickerSearchSource::kEditorWrite,
             .section_type = PickerSectionType::kEditorWrite,
         },
