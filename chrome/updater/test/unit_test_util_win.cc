0f8fd772bc90413b61c12eada84dc4808e0615c6
S. Ganesh <ganesh@chromium.org>
2024-06-27 00:53:44 +0000

    updater: call the ADMX template tests from the updater tests
    Bug: b/40212354
    Change-Id: I12276dfb969985d5fce9f596d4bea769f445c738
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659432
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320118}

--- chrome/updater/test/unit_test_util_win.cc
+++ chrome/updater/test/unit_test_util_win.cc
@@ -14,13 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/path_service.h"
+#include "base/process/launch.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/test/test_timeouts.h"
 #include "base/win/atl.h"
 #include "base/win/registry.h"
-#include "chrome/updater/updater_scope.h"
 #include "chrome/updater/test/unit_test_util.h"
+#include "chrome/updater/updater_scope.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/win_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -170,6 +172,18 @@ test::EventHolder CreateEveryoneWaitableEventForTest() {
   return {base::WaitableEvent(base::win::ScopedHandle(
               ::CreateEvent(&sa, FALSE, FALSE, event_name.c_str()))),
           event_name};
+}
+
+int RunVPythonCommand(const base::CommandLine& command_line) {
+  base::CommandLine python_command = command_line;
+  python_command.PrependWrapper(FILE_PATH_LITERAL("vpython3.bat"));
+
+  int exit_code = -1;
+  base::Process process = base::LaunchProcess(python_command, {});
+  EXPECT_TRUE(process.IsValid());
+  EXPECT_TRUE(process.WaitForExitWithTimeout(TestTimeouts::action_timeout(),
+                                             &exit_code));
+  return exit_code;
 }
 
 }  // namespace updater::test
