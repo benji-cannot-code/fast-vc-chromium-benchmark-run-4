f406b9a200d2ddd4093153410282901bf21c3711
Alex Ilin <alexilin@chromium.org>
2024-06-27 08:40:30 +0000

    Reland "Fix HTML escape problem in SyncConfirmationUI"
    This reverts commit 1a6d09ee3a408c27ba9b213c2756c86e60831a78.
    
    Reason for revert: a fix for the memory issue is patched on top of the
    revert
    
    Original change's description:
    > Revert "Fix HTML escape problem in SyncConfirmationUI"
    >
    > This reverts commit e3540e5516c5557f941d22e878f5f07a7f363cd7.
    >
    > Reason for revert: Build failed: https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/48712/overview
    >
    > Original change's description:
    > > Fix HTML escape problem in SyncConfirmationUI
    > >
    > > When recording the user Sync consent, the sync confirmation UI verifies
    > > that the strings presented on WebUI match expected native strings. This
    > > is done by sending innerHTML of all concerned UI elements from WebUI to
    > > native when the user clicks the consent button.
    > >
    > > innerHTML has one problem -- it escapes some special characters like '<'
    > > , '&' or '"' in the text so that the text can be read as text and not as
    > > parts of HTML syntax.
    > >
    > > It is possible that the sync confirmation UI contains one of those
    > > escaped characters, especially in the sign-in intercept variant, where
    > > the UI displays the user account name.
    > >
    > > To avoid having to perform similar transformations on the native side to
    > > match innerHTML's contents, this CL simply replaces innerHTML with
    > > textContent, which returns an unescaped string.
    > >
    > > Fixed: 325749258
    > > Change-Id: Ieae606a775ca047bbd48ea6b9abda769f8ff97a7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644900
    > > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > > Commit-Queue: Alex Ilin <alexilin@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1319739}
    >
    > Change-Id: I54e5991b40a732fc1700cf7e8ee173991aebc5fa
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660617
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Victor Tan <victortan@chromium.org>
    > Commit-Queue: Victor Tan <victortan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1319853}
    
    Change-Id: Iea66f52962782168a9985096755102bc0a922ad6
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659821
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320212}

--- chrome/browser/ui/webui/signin/login_ui_test_utils.cc
+++ chrome/browser/ui/webui/signin/login_ui_test_utils.cc
@@ -193,7 +193,7 @@ bool ElementExistsByIdInSigninFrame(content::WebContents* web_contents,
   });
 }
 
-enum class SyncConfirmationDialogAction { kConfirm, kCancel };
+enum class SyncConfirmationDialogAction { kConfirm, kCancel, kSettings };
 
 enum class ReauthDialogAction { kConfirm, kCancel };
 
@@ -205,6 +205,8 @@ std::string GetButtonIdForSyncConfirmationDialogAction(
       return "confirmButton";
     case SyncConfirmationDialogAction::kCancel:
       return "notNowButton";
+    case SyncConfirmationDialogAction::kSettings:
+      return "settingsButton";
   }
 }
 
@@ -546,6 +548,12 @@ bool DismissSyncConfirmationDialog(Browser* browser,
 bool ConfirmSyncConfirmationDialog(Browser* browser, base::TimeDelta timeout) {
   return DismissSyncConfirmationDialog(browser, timeout,
                                        SyncConfirmationDialogAction::kConfirm);
+}
+
+bool GoToSettingsSyncConfirmationDialog(Browser* browser,
+                                        base::TimeDelta timeout) {
+  return DismissSyncConfirmationDialog(browser, timeout,
+                                       SyncConfirmationDialogAction::kSettings);
 }
 
 bool CancelSyncConfirmationDialog(Browser* browser, base::TimeDelta timeout) {
