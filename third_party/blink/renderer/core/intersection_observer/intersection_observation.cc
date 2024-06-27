44c6ba7a03518a3d6814619c2b2a485451574810
Xianzhu Wang <wangxianzhu@chromium.org>
2024-06-27 17:02:54 +0000

    Remove temporary Blink.IntersectionObservation.* metrics
    OBSOLETE_HISTOGRAMS=Temporary debugging metrics are removed.
    
    Fixed: 341719838
    Change-Id: Iec9f9227ebf3f7181348e60a0a79a0e13ceefbec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5649938
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320453}

--- third_party/blink/renderer/core/intersection_observer/intersection_observation.cc
+++ third_party/blink/renderer/core/intersection_observer/intersection_observation.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observation.h"
 
-#include "base/metrics/histogram_macros.h"
 #include "third_party/blink/renderer/core/dom/element_rare_data_vector.h"
 #include "third_party/blink/renderer/core/frame/local_frame.h"
 #include "third_party/blink/renderer/core/intersection_observer/element_intersection_observer_data.h"
@@ -74,33 +73,16 @@ int64_t IntersectionObservation::ComputeIntersection(
 #if CHECK_SKIPPED_UPDATE_ON_SCROLL()
   std::optional<IntersectionGeometry::CachedRects> cached_rects_backup;
 #endif
-  if (RuntimeEnabledFeatures::IntersectionOptimizationEnabled()) {
-    // These values are persisted to logs. Entries should not be renumbered and
-    // numeric values should never be reused.
-    enum UpdateType {
-      kNoUpdate = 0,
-      kScrollOnly = 1,
-      kCachedRectInvalid_Unused = 2,
-      kFullUpdate = 3,
-      kMaxValue = 3,
-    };
-    UpdateType update_type = kNoUpdate;
-    if (has_pending_update || !(compute_flags & kScrollAndVisibilityOnly)) {
-      update_type = kFullUpdate;
-    } else if (cached_rects_.min_scroll_delta_to_update.x() <= 0 ||
-               cached_rects_.min_scroll_delta_to_update.y() <= 0) {
-      update_type = kScrollOnly;
-    }
-    UMA_HISTOGRAM_ENUMERATION("Blink.IntersectionObservation.UpdateType",
-                              update_type);
-    if (update_type == kNoUpdate) {
+  if (RuntimeEnabledFeatures::IntersectionOptimizationEnabled() &&
+      !has_pending_update && (compute_flags & kScrollAndVisibilityOnly) &&
+      cached_rects_.min_scroll_delta_to_update.x() > 0 &&
+      cached_rects_.min_scroll_delta_to_update.y() > 0) {
 #if CHECK_SKIPPED_UPDATE_ON_SCROLL()
-      cached_rects_backup.emplace(cached_rects_);
+    cached_rects_backup.emplace(cached_rects_);
 #else
-      // This is equivalent to a full update.
-      return 1;
+    // This is equivalent to a full update.
+    return 1;
 #endif
-    }
   }
 
   unsigned geometry_flags = GetIntersectionGeometryFlags(compute_flags);
