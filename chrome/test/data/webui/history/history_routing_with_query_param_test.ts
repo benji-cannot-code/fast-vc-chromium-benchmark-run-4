b92f4742417e3c40e628ada3a3c125ab8a0b66be
manukh <manukh@chromium.org>
2024-06-27 00:06:01 +0000

    [reland][history-embeddings] Update thresholds
    Original CL: crrev.com/c/5656872 - patch 1
    
    Revert CL: crrev.com/c/5656872 due to failing
    `browser_tests_no_field_trial` tests.
    
    Tests fixed with patch 2
    
    --------------------------------------------------
    
    a) Update thresholds for the latest model, since users who've enabled
    embeddings via chrome://flags won't get the thresholds set in finch. In
    the future, we want these thresholds set in the model metadata so we can
    make sure all users have the right thresholds for the model they have.
    
    - SearchQueryMinimumWordCount   3  -> 1
    - SearchPassageMinimumWordCount 5 -> 2
    - ContentVisibilityThreshold    .5  -> 0
    - SearchScoreThreshold          .9  -> .7
    
    b) Make finch/command line `SearchScoreThreshold` override the
    `SearchScoreThreshold` in the model metadata. This allows devs to
    override it locally. It also allows finch to override it if we need to
    without having to deploy a new model. The plan is that finch won't set
    the param and users will fallback to the metadata threshold. If neither
    the feature param nor metadata thresholds are set, falls back to 0.9,
    the threshold used by the original model.
    
    c) Adds the passage to chrome://omnibox.
    
    Bug: 349394362, 347787553
    Change-Id: Ibd8f9ba42ef65cb4de6bacb626cfa289dad93eef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660733
    Commit-Queue: Jun Zou <junzou@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Jun Zou <junzou@chromium.org>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320099}

--- chrome/test/data/webui/history/history_routing_with_query_param_test.ts
+++ chrome/test/data/webui/history/history_routing_with_query_param_test.ts
@@ -6,10 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import 'chrome://history/history.js';
 
 import type {HistoryAppElement} from 'chrome://history/history.js';
-import {BrowserServiceImpl} from 'chrome://history/history.js';
+import {BrowserServiceImpl, HistoryEmbeddingsBrowserProxyImpl, HistoryEmbeddingsPageHandlerRemote} from 'chrome://history/history.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+import {TestMock} from 'chrome://webui-test/test_mock.js';
 
 import {TestBrowserService} from './test_browser_service.js';
 import {createHistoryInfo, navigateTo} from './test_util.js';
@@ -18,6 +19,8 @@ suite('routing-with-query-param', function() {
   let app: HistoryAppElement;
   let expectedQuery: string;
   let testService: TestBrowserService;
+  let embeddingsHandler: TestMock<HistoryEmbeddingsPageHandlerRemote>&
+      HistoryEmbeddingsPageHandlerRemote;
 
   setup(function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
@@ -32,6 +35,13 @@ suite('routing-with-query-param', function() {
       info: createHistoryInfo('query'),
       value: [],
     });
+
+    embeddingsHandler = TestMock.fromClass(HistoryEmbeddingsPageHandlerRemote);
+    HistoryEmbeddingsBrowserProxyImpl.setInstance(
+        new HistoryEmbeddingsBrowserProxyImpl(embeddingsHandler));
+    embeddingsHandler.setResultFor(
+        'search', Promise.resolve({result: {items: []}}));
+
     app = document.createElement('history-app');
     document.body.appendChild(app);
     expectedQuery = 'query';
--- components/history_embeddings/history_embeddings_features.cc
+++ components/history_embeddings/history_embeddings_features.cc
@@ -31,11 +31,11 @@ const base::FeatureParam<int> kPassageExtractionMaxWordsPerAggregatePassage(
 const base::FeatureParam<int> kSearchQueryMinimumWordCount(
     &kHistoryEmbeddings,
     "SearchQueryMinimumWordCount",
-    3);
+    1);
 const base::FeatureParam<int> kSearchPassageMinimumWordCount(
     &kHistoryEmbeddings,
     "SearchPassageMinimumWordCount",
-    5);
+    2);
 
 const base::FeatureParam<int> kSearchResultItemCount(&kHistoryEmbeddings,
                                                      "SearchResultItemCount",
@@ -48,11 +48,12 @@ const base::FeatureParam<bool> kAtKeywordAcceleration(&kHistoryEmbeddings,
 const base::FeatureParam<double> kContentVisibilityThreshold(
     &kHistoryEmbeddings,
     "ContentVisibilityThreshold",
-    0.5);
+    0);
 
+// See comment at `history_embeddings::GetScoreThreshold()`.
 const base::FeatureParam<double> kSearchScoreThreshold(&kHistoryEmbeddings,
                                                        "SearchScoreThreshold",
-                                                       0.9);
+                                                       -1);
 
 const base::FeatureParam<bool> kUseMlAnswerer(&kHistoryEmbeddings,
                                               "UseMlAnswerer",
