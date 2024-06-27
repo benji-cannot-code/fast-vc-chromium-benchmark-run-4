6d4a5729d8897fc0f55b1b2db34cea54f2715450
Karol Sygiet <sygiet@google.com>
2024-06-27 12:14:19 +0000

    Add feature flag for using IUserConsentVerifierInterop api on Windows
    This CL introduces a new feature flag that will be used to enable
    authenticating using Windows Hello, where the window to which the
    OS prompt should attach can be explicitly set.
    
    Bug: 349728186
    Change-Id: I73f2a5ad4bb8cb04a5278df66a8e6d244287dbe3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5662297
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320272}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3173,6 +3173,9 @@ extern const char kDevToolsTabTargetLiteralDescription[];
 
 #if BUILDFLAG(IS_WIN)
 
+extern const char kAuthenticateUsingInteropWindowsHelloApiName[];
+extern const char kAuthenticateUsingInteropWindowsHelloApiDescription[];
+
 extern const char kAuthenticateUsingNewWindowsHelloApiName[];
 extern const char kAuthenticateUsingNewWindowsHelloApiDescription[];
 
