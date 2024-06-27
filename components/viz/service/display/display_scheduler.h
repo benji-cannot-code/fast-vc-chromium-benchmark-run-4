9a6ffe7e975fedeabb45d7e5af8a0d702d6cf2ae
Francois Doray <fdoray@chromium.org>
2024-06-27 20:10:57 +0000

    [viz] Always run DisplayScheduler::OnBeginFrameDeadline on VizCompositorThread.
    The ExternalBeginFrameSourceWinUsesRunOrPostTask feature allows
    DisplayScheduler::OnBeginFrame to be invoked on the vsync thread, to
    skip a PostTask on the critical path of notifying clients of a
    BeginFrame(). While the call is on the vsync thread, it is guaranteed
    to be "in sequence" with the VizCompositorThread.
    
    Today, DisplayScheduler::OnBeginFrame may invoke
    DisplayScheduler::OnBeginFrameDeadline synchronously if a vsync occurs
    before the scheduled begin frame deadline callback is invoked. To
    avoid making a potentially expensive call to on the vsync thread, this
    CL uses PostTask to ensure that the call to
    DisplayScheduler::OnBeginFrameDeadline occurs on the
    VizCompositorThread. This also avoids the illegal call to
    SingleThreadTaskRunner::GetCurrentDefault from a callback run
    synchronously by RunOrPostTask:
    
      base::SingleThreadTaskRunner::GetCurrentDefault
      void viz::SkiaRenderer::EndPaint
      viz::SkiaRenderer::FinishDrawingRenderPass
      viz::DirectRenderer::DrawRenderPass
      viz::DirectRenderer::DrawRenderPassAndExecuteCopyRequests
      viz::DirectRenderer::DrawFrame
      viz::Display::DrawAndSwap
      viz::DisplayScheduler::DrawAndSwap
      viz::DisplayScheduler::OnBeginFrameDeadline
      viz::DisplayScheduler::OnBeginFrame
      viz::DisplayScheduler::BeginFrameObserver::OnBeginFrameDerivedImpl
      viz::BeginFrameObserverBase::OnBeginFrame
      viz::`anonymous namespace'::FilterAndIssueBeginFrame
      viz::ExternalBeginFrameSource::OnBeginFrame
      viz::ExternalBeginFrameSourceWin::OnVSyncOnSequence
    
    Fixed: 346493746
    Bug: 40944462
    Change-Id: I2d11212a2019f61b115d82aed58db77ea395f497
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630931
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320585}

--- components/viz/service/display/display_scheduler.h
+++ components/viz/service/display/display_scheduler.h
@@ -70,6 +70,7 @@ class VIZ_SERVICE_EXPORT DisplayScheduler
   class BeginFrameRequestObserverImpl;
 
   bool OnBeginFrame(const BeginFrameArgs& args);
+  void OnBeginFrameContinuation(const BeginFrameArgs& args);
   int MaxPendingSwaps() const;
 
   base::TimeTicks current_frame_display_time() const {
