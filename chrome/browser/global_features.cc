7bbf89d002e34874f5e7a4bc4f23f8aec91cf8e1
Mickey Burks <mickeyburks@chromium.org>
2024-06-27 16:38:42 +0000

    Add WhatsNewRegistry component
    Registry is defined in the user education components and available from
    GlobalFeatures.
    
    Design doc: go/whats-new-v2-dd
    
    Bug: 337888408
    Change-Id: I05ae6b5bdd2c0534e79bbd9d7405a3a69a6971a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644748
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320438}

--- chrome/browser/global_features.cc
+++ chrome/browser/global_features.cc
@@ -8,6 +8,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_is_test.h"
 #include "base/memory/ptr_util.h"
 #include "base/no_destructor.h"
+#include "build/build_config.h"
+
+#if !BUILDFLAG(IS_ANDROID)
+// This causes a gn error on Android builds, because gn does not understand
+// buildflags.
+#include "components/user_education/common/user_education_features.h"  // nogncheck
+#include "components/user_education/webui/whats_new_registry.h"  // nogncheck
+#endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace {
 
@@ -39,6 +47,12 @@ void GlobalFeatures::ReplaceGlobalFeaturesForTesting(
   f = std::move(factory);
 }
 
-void GlobalFeatures::Init() {}
+void GlobalFeatures::Init() {
+#if !BUILDFLAG(IS_ANDROID)
+  if (user_education::features::IsWhatsNewV2()) {
+    whats_new_registry_ = std::make_unique<whats_new::WhatsNewRegistry>();
+  }
+#endif  // !BUILDFLAG(IS_ANDROID)
+}
 
 GlobalFeatures::GlobalFeatures() = default;
