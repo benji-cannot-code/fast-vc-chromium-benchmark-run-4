11781930751f85399955c319d1f90121a85ea73f
Pi-Hsun Shih <pihsun@chromium.org>
2024-06-27 14:43:56 +0000

    recorder: Auto grant DISPLAY_MEDIA_SYSTEM_AUDIO
    Bug: b:337770054
    Test: manually
    Change-Id: I77d709722c088c267df10cb46a329b402fb7f4fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645270
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320363}

--- ash/webui/recorder_app_ui/resources/core/recording_session.ts
+++ ash/webui/recorder_app_ui/resources/core/recording_session.ts
@@ -45,24 +45,18 @@ export enum AudioSource {
   USER_MEDIA = 'USER_MEDIA',
 }
 
-async function getStreamFromAudioSource(source: AudioSource) {
+function getStreamFromAudioSource(source: AudioSource): Promise<MediaStream> {
   switch (source) {
     case AudioSource.USER_MEDIA:
       return navigator.mediaDevices.getUserMedia({audio: true});
     case AudioSource.DISPLAY_MEDIA: {
-      // The video stream is required for getDisplayMedia(), so we need to
-      // remove the video tracks manually.
       // TODO(shik): Handle the case that user cancelled the dialog, or stopped
       // sharing while recording.
-      const stream = await navigator.mediaDevices.getDisplayMedia({
+      return navigator.mediaDevices.getDisplayMedia({
+        video: false,
         audio: true,
         systemAudio: 'include',
       });
-      const videoTracks = stream.getVideoTracks();
-      for (const videoTrack of videoTracks) {
-        stream.removeTrack(videoTrack);
-      }
-      return stream;
     }
     default:
       assertExhaustive(source);
@@ -123,7 +117,8 @@ export class RecordingSession {
     this.mediaRecorder.start(TIME_SLICE_MS);
 
     this.audioProcessor.port.addEventListener(
-        'message', (ev: MessageEvent<Float32Array>) => {
+        'message',
+        (ev: MessageEvent<Float32Array>) => {
           const samples = ev.data;
           // Calculates the power of the slice. The value range is [0, 1].
           const power = Math.sqrt(
@@ -139,7 +134,8 @@ export class RecordingSession {
             d.push(scaledPower);
           });
           this.sodaSession.addAudio(samples);
-        });
+        },
+    );
     this.audioProcessor.port.start();
     this.sodaSessionUnsubscribe = this.sodaSession.subscribeEvent((ev) => {
       this.sodaEventTransformer.addEvent(ev);
@@ -178,8 +174,9 @@ export class RecordingSession {
     return new Blob(this.dataChunks, {type: AUDIO_MIME_TYPE});
   }
 
-  static async create(config: RecordingSessionConfig):
-      Promise<RecordingSession> {
+  static async create(
+      config: RecordingSessionConfig,
+      ): Promise<RecordingSession> {
     const sodaSession = await config.platformHandler.newSodaSession();
 
     const stream = await getStreamFromAudioSource(config.source);
