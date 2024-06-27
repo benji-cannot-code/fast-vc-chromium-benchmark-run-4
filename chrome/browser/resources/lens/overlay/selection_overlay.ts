2f68511f2ff7f5c920eac3d507cf97fc361301fe
Juan Mojica <juanmojica@google.com>
2024-06-27 23:50:02 +0000

    [lensoverlay] Fix shimmer post selection issues.
    This allows for fading out / fading in between segmentation hovers and
    region selections. The shader no longer moves from region to region.
    This also fixes the case where a text selection did not unfocus the
    cursor.
    
    Demo: http://shortn/_lpFZuTdjCN
    
    Change-Id: Idc4691f9a87232124afab10bff8f9769ed59f795
    Bug: b:348683439
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664305
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320715}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -159,6 +159,10 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
         type: Boolean,
         reflectToAttribute: true,
       },
+      shimmerFadeOutComplete: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
       darkenExtraScrim: {
         type: Boolean,
         reflectToAttribute: true,
@@ -199,6 +203,7 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
   private darkenExtraScrim: boolean = false;
   // Whether the shimmer is currently focused on a segmentation mask.
   private shimmerOnSegmentation: boolean = false;
+  private shimmerFadeOutComplete: boolean = true;
 
   private eventTracker_: EventTracker = new EventTracker();
   // Listener ids for events from the browser side.
@@ -233,6 +238,10 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
             this.removeDragListeners();
           }),
     ];
+    this.eventTracker_.add(
+        document, 'shimmer-fade-out-complete', (e: CustomEvent<boolean>) => {
+          this.shimmerFadeOutComplete = e.detail;
+        });
     this.eventTracker_.add(
         document, 'set-cursor', (e: CustomEvent<CursorData>) => {
           if (e.detail.cursor === CursorType.POINTER) {
@@ -691,6 +700,7 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
     this.$.textSelectionLayer.selectAndSendWords(
         this.detectedTextStartIndex, this.detectedTextEndIndex);
     this.$.postSelectionRenderer.clearSelection();
+    unfocusShimmer(this, ShimmerControlRequester.CURSOR);
   }
 
   private handleTranslate() {
