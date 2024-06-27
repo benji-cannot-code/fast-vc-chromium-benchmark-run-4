2ec3db91ce9b136b3757e1e5968a3df7862131ec
James Cook <jamescook@chromium.org>
2024-06-27 18:13:33 +0000

    birch: Query the FaviconService for favicon images in suggestion chips
    This fixes an issue where favicons would be blurry on high DPI displays
    (because our image download code doesn't properly handle ICO files with
    multiple images, among other potential issues). If the FaviconService
    doesn't have the icon, fall back to downloading off the network (it's
    better to have a blurry but recognizable favicon than a generic
    chrome icon).
    
    Bug: b:342028753
    Test: ash_unittests and unit_tests
    Change-Id: I2bc72c010d974a73d4c55ac249cccb78839f22b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661977
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320506}

--- ash/birch/birch_client.h
+++ ash/birch/birch_client.h
@@ -7,10 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_BIRCH_BIRCH_CLIENT_H_
 
 #include "ash/ash_export.h"
-#include "base/functional/callback_forward.h"
+#include "base/functional/callback.h"
+
+class GURL;
 
 namespace base {
 class FilePath;
+}
+
+namespace ui {
+class ImageModel;
 }
 
 namespace ash {
@@ -42,6 +48,13 @@ class ASH_EXPORT BirchClient {
   // Drive files the path looks like:
   // /media/fuse/drivefs-48de6bc248c2f6d8e809521347ef6190/root/Test doc.gdoc
   virtual void RemoveFileItemFromLauncher(const base::FilePath& path) = 0;
+
+  // Attempts to load the favicon at `url` out of the FaviconService cache.
+  // Invokes the callback either with a valid image (success) or an empty image
+  // (failure).
+  virtual void GetFaviconImage(
+      const GURL& url,
+      base::OnceCallback<void(const ui::ImageModel&)> callback) = 0;
 
   virtual ~BirchClient() = default;
 };
