c177dece4cdaffbd912c48a1068eca9ec48eb207
James Forshaw <forshaw@chromium.org>
2024-06-27 23:09:30 +0000

    [Windows] Add Creating AppContainer Profiles Without Firewall Setup.
    This CL adds some new methods to create AppContainer profiles without
    going via the main Win32 profile APIs. This allows the profiles to be
    created without registering them with the firewall service which might
    have issues on try bots and also in deployed use. Currently the new
    profile creation method is only used in tests, however a feature will
    be added later to move creation to the new methods.
    
    NOTE, if the new tests added by this CL start failing on the ARM64
    bots then it's safe to just revert this CL rather than disabling
    the tests.
    
    Bug: 40223285
    Change-Id: Ibf2025bda0979558a39ef2ddfdbcaf7231e098fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661192
    Commit-Queue: James Forshaw <forshaw@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320696}

--- sandbox/win/src/app_container_base.h
+++ sandbox/win/src/app_container_base.h
@@ -7,9 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SANDBOX_WIN_SRC_APP_CONTAINER_BASE_H_
 
 #include <memory>
+#include <optional>
+#include <string>
 #include <vector>
 
-#include <optional>
+#include "base/files/file_path.h"
 #include "base/win/access_token.h"
 #include "base/win/security_descriptor.h"
 #include "base/win/sid.h"
@@ -21,7 +23,9 @@ namespace sandbox {
 
 class AppContainerBase final : public AppContainer {
  public:
-  AppContainerBase(base::win::Sid& package_sid, AppContainerType type);
+  AppContainerBase(const wchar_t* package_name,
+                   base::win::Sid package_sid,
+                   AppContainerType type);
   AppContainerBase(const AppContainerBase&) = delete;
   AppContainerBase& operator=(const AppContainerBase&) = delete;
   ~AppContainerBase();
@@ -48,6 +52,9 @@ class AppContainerBase final : public AppContainer {
   // Get the package SID for this AC.
   const base::win::Sid& GetPackageSid() const;
 
+  // Get the package name for this AC.
+  const wchar_t* GetPackageName() const;
+
   // Creates a new AppContainer object. This will create a new profile
   // if it doesn't already exist. The profile must be deleted manually using
   // the Delete method if it's no longer required.
@@ -55,6 +62,15 @@ class AppContainerBase final : public AppContainer {
       const wchar_t* package_name,
       const wchar_t* display_name,
       const wchar_t* description);
+
+  // Creates a new AppContainer object. This will create a new profile
+  // if it doesn't already exist. The profile must be deleted manually using
+  // the `DeleteNoFirewall` method if it's no longer required. This differs from
+  // the `CreateProfile` method in that it doesn't register the profile with the
+  // system firewall which might remove contention and deadlock issues.
+  static std::unique_ptr<AppContainerBase> CreateProfileNoFirewall(
+      const wchar_t* package_name,
+      const wchar_t* display_name);
 
   // Opens a derived AppContainer object. No checks will be made on
   // whether the package exists or not.
@@ -64,9 +80,18 @@ class AppContainerBase final : public AppContainer {
   // token
   static std::unique_ptr<AppContainerBase> CreateLowbox(const wchar_t* sid);
 
+  // Checks if a profile with a given name exists.
+  static bool ProfileExists(const wchar_t* package_name);
+
   // Delete a profile based on name. Returns true if successful, or if the
   // package doesn't already exist.
   static bool Delete(const wchar_t* package_name);
+
+  // Delete a profile based on name. Returns true if successful, or if the
+  // package doesn't already exist. This differs from the `DeleteProfile`
+  // method in that it doesn't delete the profile with the system firewall
+  // which might remove contention and deadlock issues.
+  static bool DeleteNoFirewall(const wchar_t* package_name);
 
   // Build an impersontion token from an existing token.
   // `token` specify the base token to create the new token from. Must have
@@ -86,6 +111,7 @@ class AppContainerBase final : public AppContainer {
   bool AddCapability(const std::optional<base::win::Sid>& capability_sid,
                      bool impersonation_only);
 
+  std::wstring package_name_;
   base::win::Sid package_sid_;
   bool enable_low_privilege_app_container_;
   std::vector<base::win::Sid> capabilities_;
