d12cb85e0af99dfe36682692cabd000291cca9a2
Jinsuk Kim <jinsukkim@chromium.org>
2024-06-27 23:25:37 +0000

    [CCT] Add an option for external handler API
    Adds a new checkbox to enable external handler for the intent.
    This corresponds to the CCT API CustomTabsIntent.Builder
    
    Bug: 349550859
    Change-Id: I42fc4df2687bae9f1a52b3d131e2b154f8eed7d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661779
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320709}

--- chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
+++ chrome/browser/android/examples/custom_tabs_client/src/java/org/chromium/customtabsclient/MainActivity.java
@@ -111,6 +111,7 @@ public class MainActivity extends AppCompatActivity
     private static final String SHARED_PREF_SIDE_SHEET_ROUNDED_CORNER = "RoundedCorner";
     private static final String SHARED_PREF_CONTENT_SCROLL = "ContentScrollMayResizeTab";
     private static final String SHARED_PREF_SEARCH_IN_CCT = "SearchInCCT";
+    private static final String SHARED_PREF_SEND_TO_EXTERNAL_APP = "SendToExternalApp";
     private static final String SHARED_PREF_SHARE_IDENTITY = "ShareIdentity";
     private static final String SHARED_PREF_CONNECT_BUTTON = "ConnectButton";
     private static final String SHARED_PREF_DISCONNECT_BUTTON = "DisconnectButton";
@@ -164,6 +165,7 @@ public class MainActivity extends AppCompatActivity
     private CheckBox mSideSheetRoundedCornerCheckbox;
     private CheckBox mContentScrollCheckbox;
     private CheckBox mSearchInCCTCheckbox;
+    private CheckBox mSendToExternalAppCheckbox;
     private CheckBox mShareIdentityCheckbox;
     private TextView mPcctBreakpointLabel;
     private SeekBar mPcctBreakpointSlider;
@@ -672,6 +674,9 @@ public class MainActivity extends AppCompatActivity
         mShareIdentityCheckbox = findViewById(R.id.share_identity_checkbox);
         mShareIdentityCheckbox.setChecked(
                 mSharedPref.getInt(SHARED_PREF_SHARE_IDENTITY, UNCHECKED) == CHECKED);
+        mSendToExternalAppCheckbox = findViewById(R.id.send_to_external_app_checkbox);
+        mSendToExternalAppCheckbox.setChecked(
+                mSharedPref.getInt(SHARED_PREF_SEND_TO_EXTERNAL_APP, UNCHECKED) == CHECKED);
     }
 
     private void initializeCctSpinner() {
@@ -959,6 +964,9 @@ public class MainActivity extends AppCompatActivity
         }
 
         if (mShareIdentityCheckbox.isChecked()) builder.setShareIdentityEnabled(true);
+        if (mSendToExternalAppCheckbox.isChecked()) {
+            builder.setSendToExternalDefaultHandlerEnabled(true);
+        }
 
         CustomTabsIntent customTabsIntent;
 
