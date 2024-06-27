76c1fd009daa666cf923c80ae489ce5fa1c833eb
Jing Wang <jiwan@chromium.org>
2024-06-27 05:51:18 +0000

    picker: implement heuristic reranking
    Promote suggestion type if it contains recent copied items.
    Promote launcher results if it contains best match items.
    
    Bug: b:325870201
    Change-Id: Ia4e10ffcdda1a94d5f3d26ddb1f9f1c718525d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651821
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320183}

--- ash/picker/search/picker_search_aggregator.cc
+++ ash/picker/search/picker_search_aggregator.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/search/picker_search_source.h"
 #include "ash/picker/views/picker_view_delegate.h"
 #include "ash/public/cpp/picker/picker_search_result.h"
+#include "base/containers/flat_set.h"
+#include "base/functional/overloaded.h"
 #include "base/location.h"
 #include "base/memory/weak_ptr.h"
 #include "base/notreached.h"
@@ -46,6 +48,24 @@ PickerSectionType SectionTypeFromSearchSource(PickerSearchSource source) {
     case PickerSearchSource::kEditorRewrite:
       return PickerSectionType::kEditorRewrite;
   }
+}
+
+bool ShouldPromote(const PickerSearchResult& result) {
+  return std::visit(
+      base::Overloaded{[](const PickerSearchResult::ClipboardData& data) {
+                         return data.is_recent;
+                       },
+                       [](const PickerSearchResult::BrowsingHistoryData& data) {
+                         return data.best_match;
+                       },
+                       [](const PickerSearchResult::LocalFileData& data) {
+                         return data.best_match;
+                       },
+                       [](const PickerSearchResult::DriveFileData& data) {
+                         return data.best_match;
+                       },
+                       [](const auto& data) { return false; }},
+      result.data());
 }
 
 }  // namespace
@@ -109,6 +129,23 @@ bool PickerSearchAggregator::IsPostBurnIn() const {
 
 void PickerSearchAggregator::PublishBurnInResults() {
   std::vector<PickerSearchResultsSection> sections;
+  base::flat_set<PickerSectionType> published_types;
+
+  for (PickerSectionType type : {
+           PickerSectionType::kSuggestions,
+           PickerSectionType::kLinks,
+           PickerSectionType::kFiles,
+           PickerSectionType::kDriveFiles,
+       }) {
+    if (auto it = results_.find(type);
+        it != results_.end() &&
+        base::ranges::any_of(it->second.results, &ShouldPromote)) {
+      sections.emplace_back(type, std::move(it->second.results),
+                            it->second.has_more);
+      published_types.insert(type);
+    }
+  }
+
   for (PickerSectionType type : {
            PickerSectionType::kSuggestions,
            PickerSectionType::kCategories,
@@ -120,6 +157,9 @@ void PickerSearchAggregator::PublishBurnInResults() {
            PickerSectionType::kDriveFiles,
            PickerSectionType::kGifs,
        }) {
+    if (published_types.contains(type)) {
+      continue;
+    }
     if (auto it = results_.find(type);
         it != results_.end() && !it->second.results.empty()) {
       sections.emplace_back(type, std::move(it->second.results),
