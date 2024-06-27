2c169ac6d4b49546950f4622867b0365fd77397b
Hans Wennborg <hans@chromium.org>
2024-06-27 18:10:23 +0000

    Fix uses of std::{vector}<const T>
    It's not allowed by the standard, and libc++ is dropping support for it.
    
    Bug: 323708866, 349411322
    Change-Id: I41d28c2b360688ada85e9b1dfc4c6ff77daa52a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659919
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Auto-Submit: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320505}

--- chrome/browser/os_crypt/app_bound_encryption_provider_win.cc
+++ chrome/browser/os_crypt/app_bound_encryption_provider_win.cc
@@ -63,7 +63,7 @@ AppBoundEncryptionProviderWin::~AppBoundEncryptionProviderWin() = default;
 
 class AppBoundEncryptionProviderWin::COMWorker {
  public:
-  std::optional<const std::vector<const uint8_t>> EncryptKey(
+  std::optional<const std::vector<uint8_t>> EncryptKey(
       const std::vector<uint8_t>& decrypted_key) {
     std::string plaintext_string(decrypted_key.begin(), decrypted_key.end());
     std::string ciphertext;
@@ -89,11 +89,11 @@ class AppBoundEncryptionProviderWin::COMWorker {
       return std::nullopt;
     }
 
-    return std::vector<const uint8_t>(ciphertext.cbegin(), ciphertext.cend());
+    return std::vector<uint8_t>(ciphertext.cbegin(), ciphertext.cend());
   }
 
   std::optional<const std::vector<uint8_t>> DecryptKey(
-      const std::vector<const uint8_t>& encrypted_key) {
+      const std::vector<uint8_t>& encrypted_key) {
     DWORD last_error;
     std::string encrypted_key_string(encrypted_key.begin(),
                                      encrypted_key.end());
@@ -206,7 +206,7 @@ bool AppBoundEncryptionProviderWin::IsCompatibleWithOsCryptSync() {
   return false;
 }
 
-base::expected<std::vector<const uint8_t>,
+base::expected<std::vector<uint8_t>,
                AppBoundEncryptionProviderWin::KeyRetrievalStatus>
 AppBoundEncryptionProviderWin::RetrieveEncryptedKey() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -231,7 +231,7 @@ AppBoundEncryptionProviderWin::RetrieveEncryptedKey() {
   }
 
   // Trim off the key prefix.
-  return std::vector<const uint8_t>(
+  return std::vector<uint8_t>(
       encrypted_key_with_header->cbegin() + sizeof(kCryptAppBoundKeyPrefix),
       encrypted_key_with_header->cend());
 }
@@ -239,7 +239,7 @@ AppBoundEncryptionProviderWin::RetrieveEncryptedKey() {
 void AppBoundEncryptionProviderWin::StoreEncryptedKeyAndReply(
     const std::vector<uint8_t>& decrypted_key,
     KeyCallback callback,
-    const std::optional<std::vector<const uint8_t>>& encrypted_key) {
+    const std::optional<std::vector<uint8_t>>& encrypted_key) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!encrypted_key) {
     // Failure here causes the provider not to be registered.
