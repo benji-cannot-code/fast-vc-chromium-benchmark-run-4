18db51d77b86f4d611d4c9138de2ea6be2f451e5
mlcui <mlcui@google.com>
2024-06-27 03:35:07 +0000

    Use `available_categories_` to decide what CrOS searches to do
    Bug: b:326166751
    Change-Id: I6a6a2100ace138cbf023bc35be9214aa3f8b5bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657833
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320158}

--- ash/picker/search/picker_search_request.cc
+++ ash/picker/search/picker_search_request.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/picker/picker_search_result.h"
 #include "base/check.h"
 #include "base/check_deref.h"
+#include "base/containers/contains.h"
 #include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
@@ -81,13 +82,16 @@ PickerSearchRequest::PickerSearchRequest(
 
   std::vector<PickerSearchSource> cros_search_sources;
   cros_search_sources.reserve(3);
-  if (!category.has_value() || category == PickerCategory::kLinks) {
+  if ((!category.has_value() || category == PickerCategory::kLinks) &&
+      base::Contains(available_categories, PickerCategory::kLinks)) {
     cros_search_sources.push_back(PickerSearchSource::kOmnibox);
   }
-  if (!category.has_value() || category == PickerCategory::kLocalFiles) {
+  if ((!category.has_value() || category == PickerCategory::kLocalFiles) &&
+      base::Contains(available_categories, PickerCategory::kLocalFiles)) {
     cros_search_sources.push_back(PickerSearchSource::kLocalFile);
   }
-  if (!category.has_value() || category == PickerCategory::kDriveFiles) {
+  if ((!category.has_value() || category == PickerCategory::kDriveFiles) &&
+      base::Contains(available_categories, PickerCategory::kDriveFiles)) {
     cros_search_sources.push_back(PickerSearchSource::kDrive);
   }
 
