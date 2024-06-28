5584c74ddf4d30001f6a3feb07fa7cf53f724561
Min Qin <qinmin@chromium.org>
2024-06-28 18:30:16 +0000

    Clean up downloaded pdfs on browser startup
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 349945367
    Change-Id: I4d7f323c1fa3007fd2cffcf895213a1d0128798c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666116
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321089}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -120,6 +120,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/pdf/pdf_jni_headers/PdfUtils_jni.h"
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
+#include "components/download/public/common/download_task_runner.h"
 #include "components/infobars/content/content_infobar_manager.h"
 #include "content/public/common/content_features.h"
 #include "net/http/http_content_disposition.h"
@@ -337,6 +338,12 @@ void OnDownloadDialogClosed(
           ui::SelectedFileInfo(result.file_path));
       break;
   }
+}
+
+base::FilePath GetTempPdfDir() {
+  base::FilePath cache_dir;
+  base::android::GetCacheDirectory(&cache_dir);
+  return cache_dir.Append(kPdfDirName);
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
@@ -656,10 +663,7 @@ bool ChromeDownloadManagerDelegate::DetermineDownloadTarget(
             profile_->GetPrefs()->GetString(prefs::kDefaultCharset),
             download->GetSuggestedFilename(), download->GetMimeType(),
             l10n_util::GetStringUTF8(IDS_DEFAULT_DOWNLOAD_FILENAME));
-        base::FilePath cache_dir;
-        base::android::GetCacheDirectory(&cache_dir);
-        download_path =
-            cache_dir.Append(kPdfDirName).Append(generated_filename);
+        download_path = GetTempPdfDir().Append(generated_filename);
         action = DownloadPathReservationTracker::UNIQUIFY;
       }
     } else {
@@ -2078,7 +2082,12 @@ void ChromeDownloadManagerDelegate::ConnectToQuarantineService(
 }
 
 void ChromeDownloadManagerDelegate::OnManagerInitialized() {
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
+  if (ShouldOpenPdfInline()) {
+    download::GetDownloadTaskRunner()->PostTask(
+        FROM_HERE, base::BindOnce([]() { base::DeleteFile(GetTempPdfDir()); }));
+  }
+#else
   CancelAllEphemeralWarnings();
 #endif
 }
