ebe865d672cc52ea7d45495feaf3dba057717b87
Olivier ROBIN <olivierrobin@google.com>
2024-06-28 12:19:45 +0000

    [IOS RL] Remove srcset from distilled images
    srcset interferes with the downloaded image and if they are present,
    they will prevent the offline image to be displayed.
    
    Change-Id: Ic216533d78f033d80e6d85b6b431b7c55f92e1c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666878
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Quentin Pubert <qpubert@google.com>
    Cr-Commit-Position: refs/heads/main@{#1320894}

--- ios/chrome/browser/reading_list/model/url_downloader.mm
+++ ios/chrome/browser/reading_list/model/url_downloader.mm
@@ -56,6 +56,7 @@ const char kReplaceDownloadedImagesScript[] =
     "    var imgTags = document.getElementsByTagName(\"img\");"
     "    for(image of imgTags) {"
     "        image.src = imgData[image.src] || image.src;"
+    "        image.removeAttribute('srcset');"
     "    }"
     "}, false);"
     "</script>";
