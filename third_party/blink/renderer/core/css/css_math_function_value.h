a14d0555e790eee4e03c200fcb5ae752f07c3ed3
Daniil Sakhapov <sakhapov@chromium.org>
2024-06-28 10:59:24 +0000

    Implement ComputeValueInCanonicalUnits for CSSPrimitiveValue
    This function resolves units to canoinical value, similar to
    existing one, but now it will also resolve relative units with
    length resolver.
    This function will simplify computations for Media Queries,
    which can have value of many types.
    
    Bug: 40946458
    Change-Id: I3ac4831b1bd33449522af8471e3ec2588539db04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5658174
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320871}

--- third_party/blink/renderer/core/css/css_math_function_value.h
+++ third_party/blink/renderer/core/css/css_math_function_value.h
@@ -87,6 +87,7 @@ class CORE_EXPORT CSSMathFunctionValue : public CSSPrimitiveValue {
   int ComputeInteger(const CSSLengthResolver&) const;
   double ComputeNumber(const CSSLengthResolver&) const;
   double ComputePercentage(const CSSLengthResolver&) const;
+  double ComputeValueInCanonicalUnit(const CSSLengthResolver&) const;
 
   bool AccumulateLengthArray(CSSLengthArray& length_array,
                              double multiplier) const;
--- third_party/blink/renderer/core/css/css_numeric_literal_value.h
+++ third_party/blink/renderer/core/css/css_numeric_literal_value.h
@@ -73,6 +73,7 @@ class CORE_EXPORT CSSNumericLiteralValue : public CSSPrimitiveValue {
   double ComputeDegrees() const;
   double ComputeDotsPerPixel() const;
   double ComputeInCanonicalUnit() const;
+  double ComputeInCanonicalUnit(const CSSLengthResolver&) const;
 
   int ComputeInteger() const;
   double ComputeNumber() const;
