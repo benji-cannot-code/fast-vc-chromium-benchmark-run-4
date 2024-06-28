1364dcc94649159e39302d0f58b10dfec24a3c60
Sun Shin <sushin@nvidia.com>
2024-06-28 04:48:20 +0000

    Add RECV_ECN socket option in P2PSocket interface
    This patch adds interface changes to 'services/network' and
    'third_party/blink' to support ECN handling in WebRTC.
    
    ECN means Explicit Congestion Notification.
    See RFC9331 for details.
    
    Purpose it to allow webrtc to add new experimental socket options.
    Ex, https://webrtc-review.googlesource.com/c/src/+/332380
    
    Bug: webrtc:15368
    Change-Id: I219e2e39f14f4384f5e6b2e1a007f05efb8bb830
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5545427
    Reviewed-by: Crisrael Lucero <crisrael@google.com>
    Commit-Queue: Joone Hur <joone@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320795}

--- services/network/public/cpp/p2p_socket_type.h
+++ services/network/public/cpp/p2p_socket_type.h
@@ -19,9 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace network {
 
 enum P2PSocketOption {
-  P2P_SOCKET_OPT_RCVBUF,  // Receive buffer size.
-  P2P_SOCKET_OPT_SNDBUF,  // Send buffer size.
-  P2P_SOCKET_OPT_DSCP,    // DSCP code.
+  P2P_SOCKET_OPT_RCVBUF,    // Receive buffer size.
+  P2P_SOCKET_OPT_SNDBUF,    // Send buffer size.
+  P2P_SOCKET_OPT_DSCP,      // DSCP code.
+  P2P_SOCKET_OPT_RECV_ECN,  // Enable ECN receiving.
   P2P_SOCKET_OPT_MAX
 };
 
