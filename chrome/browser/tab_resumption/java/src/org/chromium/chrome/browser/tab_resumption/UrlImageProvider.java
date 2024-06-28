f274dddf3af245a5721782088c84977e93eb8be1
Xi Han <hanxi@google.com>
2024-06-28 21:33:50 +0000

    [TabResumption] Fix crash in UrlImageProvider.
    It seems the crash was caused the cached UrlImageProvider instance
    isn't set to null after it is destroyed. In this CL, we check if the
    instance has been destroyed before using it to stop the crash.
    
    Bug: 350021945
    Change-Id: I7d9c6dd1a8ccadec824a41a668003100b159fd52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663859
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321192}

--- chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/UrlImageProvider.java
+++ chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/UrlImageProvider.java
@@ -131,6 +131,11 @@ public class UrlImageProvider {
                 /* isSelected= */ false);
     }
 
+    /** Returns whether this UrlImageProvider instance has been destroyed. */
+    public boolean isDestroyed() {
+        return mLargeIconBridge == null;
+    }
+
     LargeIconBridge getLargeIconBridgeForTesting() {
         return mLargeIconBridge;
     }
--- chrome/browser/tab_resumption/junit/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleSuggestionsUnitTest.java
+++ chrome/browser/tab_resumption/junit/src/org/chromium/chrome/browser/tab_resumption/TabResumptionModuleSuggestionsUnitTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.tab_resumption;
 
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.mockito.ArgumentMatchers.anyInt;
@@ -318,5 +319,6 @@ public class TabResumptionModuleSuggestionsUnitTest extends TestSupport {
         urlImageProvider.destroy();
         assertNull(urlImageProvider.getImageServiceBridgeForTesting());
         assertNull(urlImageProvider.getLargeIconBridgeForTesting());
+        assertTrue(urlImageProvider.isDestroyed());
     }
 }
