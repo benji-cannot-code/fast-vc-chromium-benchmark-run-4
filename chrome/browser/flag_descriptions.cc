40bded35702e43a5ca7a78eecabf6d04ba818bcf
Brando Socarras <brandosocarras@google.com>
2024-06-28 21:14:46 +0000

    [QSv2] Create Quick Share v2 Flag
    Fixed: b/350029655
    Change-Id: I4f78335774afc8e49354023c3c9b9e4309c4a659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664652
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321183}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "flag_descriptions.h"
 #include "media/gpu/buildflags.h"
 #include "pdf/buildflags.h"
 
@@ -3064,6 +3065,12 @@ const char kQuickDeleteAndroidFollowupName[] =
 const char kQuickDeleteAndroidFollowupDescription[] =
     "Enables some follow up work on Clear Browsing Data for Quick Delete, "
     "including an improved post delete experience and animations.";
+
+const char kQuickShareV2Name[] = "Quick Share v2";
+const char kQuickShareV2Description[] =
+    "Enables Quick Share v2, which defaults Quick Share to 'Your Devices' "
+    "visibility, removes the 'Selected Contacts' visibility, removes the Quick "
+    "Share On/Off toggle.";
 
 const char kSendTabToSelfIOSPushNotificationsName[] =
     "Send tab to self iOS push notifications";
--- chrome/browser/nearby_sharing/common/nearby_share_features.cc
+++ chrome/browser/nearby_sharing/common/nearby_share_features.cc
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "chrome/browser/nearby_sharing/common/nearby_share_features.h"
+
 #include "base/feature_list.h"
+#include "nearby_share_features.h"
 
 namespace features {
 
@@ -38,8 +40,17 @@ BASE_FEATURE(kNearbySharingRestrictToContacts,
              "NearbySharingRestrictToContacts",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Enables Quick Share v2, which defaults Quick Share to 'Your Devices'
+// visibility, removes the 'Selected Contacts' visibility, removes the Quick
+// Share On/Off toggle.
+BASE_FEATURE(kQuickShareV2, "QuickShareV2", base::FEATURE_DISABLED_BY_DEFAULT);
+
 bool IsNameEnabled() {
   return base::FeatureList::IsEnabled(kIsNameEnabled);
+}
+
+bool IsQuickShareV2Enabled() {
+  return base::FeatureList::IsEnabled(kQuickShareV2);
 }
 
 bool IsRestrictToContactsEnabled() {
