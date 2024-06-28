bff1e2722e3caf073fc9bbeb6c50b50686c02e65
Bryan Nguyen <nguyenbryan@google.com>
2024-06-28 21:23:08 +0000

    [lensoverlay] Hide FedCM dialog when overlay is showing.
    Demo: https://screencast.googleplex.com/cast/NDc5NDgwNDA1ODcxODIwOHxjNmI5ODAzZi1hMQ
    
    Bug: b:348627269
    Change-Id: Ifc1d15e6128f5eb1b9c0b25a143a4dcd2221bd82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661576
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321189}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -1096,6 +1096,9 @@ void LensOverlayController::DidCaptureScreenshot(
 
   ShowOverlay();
 
+  for (Observer& observer : observers_) {
+    observer.OnLensOverlayDidShow();
+  }
   state_ = State::kStartingWebUI;
 }
 
@@ -1269,6 +1272,11 @@ void LensOverlayController::CloseUIPart2(
   fullscreen_observation_.Reset();
 
   multimodal_selection_type_ = lens::UNKNOWN_SELECTION_TYPE;
+
+  for (Observer& observer : observers_) {
+    observer.OnLensOverlayDidClose();
+  }
+
   state_ = State::kOff;
 
   base::UmaHistogramEnumeration("Lens.Overlay.Dismissed", dismissal_source);
