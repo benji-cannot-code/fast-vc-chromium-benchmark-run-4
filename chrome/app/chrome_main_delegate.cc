f9626890d37e38e4ec8887e9e2a8a1518a740198
Kramer Ge <fangzhoug@chromium.org>
2024-06-28 16:45:25 +0000

    ozone/wayland: Move ozone platform selection after CreateFeatureList
    In order to finch rollout a feature that turns ozone platform selection
    to auto, feature_list must be registered before-hand.
    
    Instead of initializing ozone-platform to override eye-dropper feature,
    query xdg_session_type, disable it if it's `wayland`.
    
    As a result eye-dropper would be disabled for wayland and xwayland.
    
    Bug: 40250220, 40800718
    Change-Id: Ie2544e88915cdc4f8de4f5f6fbc2cb9caf41a2db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663881
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321024}

--- chrome/app/chrome_main_delegate.cc
+++ chrome/app/chrome_main_delegate.cc
@@ -189,6 +189,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_LINUX)
 #include "base/nix/scoped_xdg_activation_token_injector.h"
+#include "ui/linux/display_server_utils.h"
 #endif
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID)
@@ -953,11 +954,13 @@ std::optional<int> ChromeMainDelegate::PostEarlyInitialization(
           ->chrome_feature_list_creator();
   chrome_feature_list_creator->CreateFeatureList();
 
-  // Initialize OzonePlatform, if not done yet.
 #if BUILDFLAG(IS_OZONE)
-  if (!ui::OzonePlatform::IsInitialized()) {
-    ui::OzonePlatform::PreEarlyInitialization();
-  }
+  // Initialize Ozone platform and add required feature flags as per platform's
+  // properties.
+#if BUILDFLAG(IS_LINUX)
+  ui::SetOzonePlatformForLinuxIfNeeded(*base::CommandLine::ForCurrentProcess());
+#endif
+  ui::OzonePlatform::PreEarlyInitialization();
 #endif  // BUILDFLAG(IS_OZONE)
 
   content::InitializeMojoCore();
