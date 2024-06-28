1a40c659acd42349991ab290963eb7532f51c51b
rajendrant <rajendrant@chromium.org>
2024-06-28 19:25:45 +0000

    Reland "Use database as cache for passages and embeddings to bypass embedder"
    This is a reland of commit 22c986f0ce1a5ef2efa704a2093a4d47eb18a63e
    
    The feature param to use database before embedder is turned off by default to avoid the https://crbug.com/347685218 crash. I will continue debug the crash.
    
    Original change's description:
    > Use database as cache for passages and embeddings to bypass embedder
    >
    > This CL saves the embedder from having to recompute embeddings for
    > passages that are already stored in the database when a visit is
    > made to the same URL. Instead of computing the embedding with the
    > model, the stored embedding is reused.
    >
    > Additionally, this CL adds histogram logging for cancellation of
    > passage extraction, embedding, and storage. Various reason codes
    > are logged so we can identify the step at which cancellation occurred.
    > The tab helper should dominate, but later cancellation in the service
    > might signal problems or opportunities to optimize.
    >
    > Also, this CL fixes b/347306992 using live loading tab count
    > instead of a member value updated via notifications that could
    > be missed when a loading tab is closed. (Random bug noticed during
    > development.)
    >
    > Bug: 345819418
    > Change-Id: Icaacf456717b9d88c0e14b3c8f3a1e31835440c6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5629651
    > Commit-Queue: Orin Jaworski <orinj@chromium.org>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Reviewed-by: Orin Jaworski <orinj@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1315485}
    
    Bug: 345819418
    Change-Id: I3e15c76133ef60add563224ebb2dcbb4c0ca5cdf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5656278
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321126}

--- chrome/browser/history_embeddings/history_embeddings_tab_helper.cc
+++ chrome/browser/history_embeddings/history_embeddings_tab_helper.cc
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 HistoryEmbeddingsTabHelper::HistoryEmbeddingsTabHelper(
     content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
-      content::WebContentsUserData<HistoryEmbeddingsTabHelper>(*web_contents),
-      loading_tab_count_(0) {
+      content::WebContentsUserData<HistoryEmbeddingsTabHelper>(*web_contents) {
   resource_coordinator::TabLoadTracker::Get()->AddObserver(this);
 }
 
@@ -79,23 +78,27 @@ void HistoryEmbeddingsTabHelper::DidFinishLoad(
   VLOG(3) << "Tabs Loading: "
           << resource_coordinator::TabLoadTracker::Get()->GetLoadingTabCount();
 
-  ScheduleExtraction(render_frame_host->GetWeakDocumentPtr());
+  if (!ScheduleExtraction(render_frame_host->GetWeakDocumentPtr())) {
+    history_embeddings::RecordExtractionCancelled(
+        history_embeddings::ExtractionCancelled::TAB_HELPER_DID_FINISH_LOAD);
+    VLOG(3) << "Extraction cancelled in DidFinishLoad";
+  }
 }
 
 void HistoryEmbeddingsTabHelper::OnLoadingStateChange(
     content::WebContents* web_contents,
     LoadingState old_loading_state,
     LoadingState new_loading_state) {
-  loading_tab_count_ =
+  size_t loading_tab_count =
       resource_coordinator::TabLoadTracker::Get()->GetLoadingTabCount();
   VLOG(3) << "Loading state changed for '" << web_contents->GetTitle()
-          << "' with " << loading_tab_count_ << " tabs now loading.";
+          << "' with " << loading_tab_count << " tabs now loading.";
 }
 
-void HistoryEmbeddingsTabHelper::ScheduleExtraction(
+bool HistoryEmbeddingsTabHelper::ScheduleExtraction(
     content::WeakDocumentPtr weak_render_frame_host) {
   if (!weak_render_frame_host.AsRenderFrameHostIfValid()) {
-    return;
+    return false;
   }
   // Schedule a new delayed task with a fresh weak pointer.
   base::SingleThreadTaskRunner::GetCurrentDefault()->PostDelayedTask(
@@ -103,19 +106,33 @@ void HistoryEmbeddingsTabHelper::ScheduleExtraction(
       base::BindOnce(&HistoryEmbeddingsTabHelper::ExtractPassages,
                      weak_ptr_factory_.GetWeakPtr(), weak_render_frame_host),
       base::Milliseconds(history_embeddings::kPassageExtractionDelay.Get()));
+  return true;
 }
 
 void HistoryEmbeddingsTabHelper::ExtractPassages(
     content::WeakDocumentPtr weak_render_frame_host) {
   if (!history_url_.has_value()) {
+    history_embeddings::RecordExtractionCancelled(
+        history_embeddings::ExtractionCancelled::
+            TAB_HELPER_EXTRACT_PASSAGES_URL);
+    VLOG(3) << "Extraction cancelled; no history_url_ value.";
     return;
   }
 
-  if (loading_tab_count_ > 0) {
+  size_t loading_tab_count =
+      resource_coordinator::TabLoadTracker::Get()->GetLoadingTabCount();
+  if (loading_tab_count > 0) {
     // Not ready yet. Try again after the delay.
-    ScheduleExtraction(weak_render_frame_host);
-    VLOG(3) << "Extraction rescheduled; " << loading_tab_count_
-            << " tabs still loading.";
+    if (!ScheduleExtraction(weak_render_frame_host)) {
+      history_embeddings::RecordExtractionCancelled(
+          history_embeddings::ExtractionCancelled::
+              TAB_HELPER_EXTRACT_PASSAGES_RESCHEDULE);
+      VLOG(3) << "Extraction cancelled; " << loading_tab_count
+              << " tabs still loading.";
+    } else {
+      VLOG(3) << "Extraction rescheduled; " << loading_tab_count
+              << " tabs still loading.";
+    }
     return;
   }
 
@@ -138,6 +155,9 @@ void HistoryEmbeddingsTabHelper::ExtractPassagesWithHistoryData(
   // visit being added to the DB, e.g. navigations to
   // "chrome://" URLs.
   if (!result.success || result.visits.empty()) {
+    history_embeddings::RecordExtractionCancelled(
+        history_embeddings::ExtractionCancelled::
+            TAB_HELPER_EXTRACT_PASSAGES_WITH_HISTORY_DATA_RESULTS);
     return;
   }
   const history::URLRow& url_row = result.row;
@@ -149,6 +169,9 @@ void HistoryEmbeddingsTabHelper::ExtractPassagesWithHistoryData(
   // navigation by comparing the visit_times.
   if (!history_visit_time_.has_value() ||
       latest_visit.visit_time != *history_visit_time_) {
+    history_embeddings::RecordExtractionCancelled(
+        history_embeddings::ExtractionCancelled::
+            TAB_HELPER_EXTRACT_PASSAGES_WITH_HISTORY_DATA_TIME);
     return;
   }
   // Make sure the latest visit (the first one in the array) is
@@ -159,12 +182,17 @@ void HistoryEmbeddingsTabHelper::ExtractPassagesWithHistoryData(
   // a problem, consider implementing a
   // GetMostRecent*Local*VisitsForURL().
   if (!latest_visit.originator_cache_guid.empty()) {
+    history_embeddings::RecordExtractionCancelled(
+        history_embeddings::ExtractionCancelled::
+            TAB_HELPER_EXTRACT_PASSAGES_WITH_HISTORY_DATA_GUID);
     return;
   }
 
   if (history_embeddings::HistoryEmbeddingsService* embeddings_service =
           GetHistoryEmbeddingsService()) {
-    embeddings_service->RetrievePassages(latest_visit, weak_render_frame_host);
+    embeddings_service->RetrievePassages(
+        latest_visit.url_id, latest_visit.visit_id, latest_visit.visit_time,
+        weak_render_frame_host);
   }
 
   // Clear the data. It isn't reused and will be set anew by later navigation.
