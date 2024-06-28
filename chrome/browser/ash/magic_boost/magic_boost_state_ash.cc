e796773d80be4a1d1f1a3056892847fffc27cc81
Ahmed Mehfooz <amehfooz@chromium.org>
2024-06-28 19:03:57 +0000

    Use MagicBoostState::Observer in mahi manager
    Bug: b/347771885
    Change-Id: Ica8d20bf7cd769d7f8ae13340da7b41a7686d9be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5649031
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321109}

--- chrome/browser/ash/magic_boost/magic_boost_state_ash.cc
+++ chrome/browser/ash/magic_boost/magic_boost_state_ash.cc
@@ -32,10 +32,9 @@ MagicBoostStateAsh::MagicBoostStateAsh() {
 
 MagicBoostStateAsh::~MagicBoostStateAsh() = default;
 
-void MagicBoostStateAsh::OnFirstSessionStarted() {
-  PrefService* prefs =
-      ash::Shell::Get()->session_controller()->GetPrimaryUserPrefService();
-  RegisterPrefChanges(prefs);
+void MagicBoostStateAsh::OnActiveUserPrefServiceChanged(
+    PrefService* pref_service) {
+  RegisterPrefChanges(pref_service);
 }
 
 int32_t MagicBoostStateAsh::AsyncIncrementHMRConsentWindowDismissCount() {
