392fa0aa3ed8f77d73a8c8a2c596e59bc2132597
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-06-28 23:29:01 +0000

    [Extensions] Re-evaluate MV2 extensions on policy updates
    Chrome has announced that MV2 extensions will gradually no longer be
    supported in the browser. See more information at
    https://developer.chrome.com/blog/resuming-the-transition-to-mv3.
    The second step of this, after warning users about MV2 extensions, will
    be to disable unsupported MV2 extensions. If users want to re-enable
    these extensions, they will be able to.
    
    This CL has Chrome monitor enterprise policy changes during a session
    and appropriately re-enable or disable any extensions that may need to
    be as a result of a policy change.
    
    Add unit tests for the same.
    
    Bug: 339061151
    Change-Id: I86acf27ed775122010d7c07eb89d546f0ad13bf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659473
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321251}

--- chrome/browser/extensions/manifest_v2_experiment_manager.cc
+++ chrome/browser/extensions/manifest_v2_experiment_manager.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/extensions/mv2_experiment_stage.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "extensions/browser/disable_reason.h"
@@ -19,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_registry_factory.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extensions_browser_client.h"
+#include "extensions/browser/pref_names.h"
 #include "extensions/browser/pref_types.h"
 #include "extensions/common/extension.h"
 #include "extensions/common/extension_features.h"
@@ -155,6 +157,16 @@ ManifestV2ExperimentManager::ManifestV2ExperimentManager(
       .Post(FROM_HERE,
             base::BindOnce(&ManifestV2ExperimentManager::OnExtensionSystemReady,
                            weak_factory_.GetWeakPtr()));
+
+  // Listen to management policy changes. `Unretained` is safe since the
+  // `pref_change_registrar` is owned by this class.
+  pref_change_registrar_.Init(
+      Profile::FromBrowserContext(browser_context_)->GetPrefs());
+  pref_change_registrar_.Add(
+      pref_names::kManifestV2Availability,
+      base::BindRepeating(
+          &ManifestV2ExperimentManager::OnManagementPolicyChanged,
+          base::Unretained(this)));
 }
 
 ManifestV2ExperimentManager::~ManifestV2ExperimentManager() = default;
@@ -358,6 +370,19 @@ void ManifestV2ExperimentManager::OnExtensionInstalled(
   }
 
   MaybeReEnableExtension(*extension);
+}
+
+void ManifestV2ExperimentManager::OnManagementPolicyChanged() {
+  // The management policy has changed. Go through all disabled extensions to
+  // check if any should be re-enabled, and go through all enabled extensions
+  // to see if any should be disabled (if the experiment is active).
+  CheckDisabledExtensions();
+
+  if (GetCurrentExperimentStage() != MV2ExperimentStage::kDisableWithReEnable) {
+    return;
+  }
+
+  DisableAffectedExtensions();
 }
 
 bool ManifestV2ExperimentManager::DidUserReEnableExtensionForTesting(
