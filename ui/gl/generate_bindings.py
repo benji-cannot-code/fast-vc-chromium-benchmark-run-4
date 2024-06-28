0ddc44abc6bb8be32a30114f3514bc9079c14e9f
Zhenyao Mo <zmo@chromium.org>
2024-06-28 22:29:33 +0000

    Remove ARB extensions support from GL bindings: Part XII.
    * GL_ARB_robustness
    
    This is part of
    https://chromium-review.googlesource.com/c/chromium/src/+/4355969
    
    The original CL caused many command buffer unit tests failing.
    
    I believe it is due to the test setups are wrong, but fixing all
    of them in one CL is too much.
    
    Splitting into multiple ones, so it's easier to read and review.
    
    TEST=bots
    
    Bug: 1426503
    Change-Id: Ie1a6c09b9992f467ba7f6110434d4bd59d42317e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5641276
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321218}

--- ui/gl/generate_bindings.py
+++ ui/gl/generate_bindings.py
@@ -902,8 +902,8 @@ GL_FUNCTIONS = [
       'GLint plane, GLenum pname, GLsizei bufSize, GLsizei* length, '
       'GLint* params', },
 { 'return_type': 'GLenum',
-  'names': ['glGetGraphicsResetStatusARB',
-            'glGetGraphicsResetStatusKHR',
+  'known_as': 'glGetGraphicsResetStatusARB',
+  'names': ['glGetGraphicsResetStatusKHR',
             'glGetGraphicsResetStatusEXT',
             'glGetGraphicsResetStatus'],
   'arguments': 'void', },
