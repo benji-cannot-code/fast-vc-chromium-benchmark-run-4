3a11b1b8fd8f3b96cd5d4dce002a68ef4b8bdb5e
Riley Tatum <rtatum@google.com>
2024-06-28 00:20:44 +0000

    [a11y] Add aria-label on ChromeOS Chrome Guest NTP Learn More links
    Bug: 323398609
    Change-Id: I426436cec88cc8fd7452298fa69c3712c60804d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660293
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ashley Prasad <ashleydp@google.com>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320730}

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -354,6 +354,10 @@ void NTPResourceCache::CreateNewTabGuestHTML() {
                           l10n_util::GetStringUTF16(IDS_LEARN_MORE));
     localized_strings.Set("enterpriseInfoHintLink",
                           chrome::kLearnMoreEnterpriseURL);
+    localized_strings.Set(
+        "enterpriseLearnMoreA11yLabel",
+        l10n_util::GetStringUTF16(
+            IDS_NEW_TAB_ENTERPRISE_GUEST_SESSION_LEARN_MORE_ACCESSIBILITY_TEXT));
     std::u16string enterprise_info;
     if (connector->IsCloudManaged()) {
       const std::string enterprise_domain_manager =
@@ -370,6 +374,7 @@ void NTPResourceCache::CreateNewTabGuestHTML() {
     localized_strings.Set("enterpriseInfoMessage", "");
     localized_strings.Set("enterpriseLearnMore", "");
     localized_strings.Set("enterpriseInfoHintLink", "");
+    localized_strings.Set("enterpriseLearnMoreA11yLabel", "");
   }
 #endif
 
