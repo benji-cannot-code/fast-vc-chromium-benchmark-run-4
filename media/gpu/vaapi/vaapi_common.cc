401b13331ac8b65dc38f133c8af26df6184f9f1f
Miguel Casas-Sanchez <mcasas@chromium.org>
2024-06-28 05:33:22 +0000

    media/gpu/vaapi: USe ScopedID<VASurfaceID> ISO VASurface
    VASurface is used for Vaapi*Picture classes (used for encoding and
    decoding) and also for VaapiDecodeSurfaceHandler's implementation.
    
    Decoding: ref-counted VASurfaces are unnecessary since Vaapi*Pictures
    are ref-counted themselves (that's needed for Duplicate() calls) and
    libva's VASurfaces don't need to be destroyed (they are owned by
    VaapiVideoDecoder::allocated_va_surfaces_); the only needed
    functionality is a release callback to VaapiVD::ReleaseVideoFrame.
    
    Encoding: same rationale but VaapiVEA does not keep ownership of any
    allocated libva VASurfaces; this CL sorts that out by moving the
    ScopedVASurface into the release callback, where it's bound until
    this is called.
    
    It's therefore simpler to just use ScopedID<VASurfaceID>, which holds
    an integer type and a release callback. This CL introduces that and
    calls it VASurfaceHandle.
    
    FTR this passes:
    - video.Play.(av1|vp9)_hw_*
    - video.ChromeStackDecoder.(vp9|av1)*
    - video.EncodeAccel.* video.WebCodecs.* webrtc.RTCPeerConnection.*
    - video.Seek.av1 video.Seek.vp9
    
    on rex.
    
    Bug: 339518553
    Change-Id: Id2523c7dc787122c6d6348f795c17ce1b9e4d2bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5590385
    Commit-Queue: Miguel Casas-Sanchez <mcasas@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320803}

--- media/gpu/vaapi/vaapi_common.cc
+++ media/gpu/vaapi/vaapi_common.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-VaapiH264Picture::VaapiH264Picture(scoped_refptr<VASurface> va_surface)
-    : va_surface_(va_surface) {}
+VaapiH264Picture::VaapiH264Picture(std::unique_ptr<VASurfaceHandle> va_surface)
+    : va_surface_(std::move(va_surface)) {}
 
 VaapiH264Picture::~VaapiH264Picture() = default;
 
@@ -19,8 +19,8 @@ VaapiH264Picture* VaapiH264Picture::AsVaapiH264Picture() {
 }
 
 #if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
-VaapiH265Picture::VaapiH265Picture(scoped_refptr<VASurface> va_surface)
-    : va_surface_(va_surface) {}
+VaapiH265Picture::VaapiH265Picture(std::unique_ptr<VASurfaceHandle> va_surface)
+    : va_surface_(std::move(va_surface)) {}
 
 VaapiH265Picture::~VaapiH265Picture() = default;
 
@@ -30,8 +30,8 @@ VaapiH265Picture* VaapiH265Picture::AsVaapiH265Picture() {
 
 #endif  // BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
 
-VaapiVP8Picture::VaapiVP8Picture(scoped_refptr<VASurface> va_surface)
-    : va_surface_(va_surface) {}
+VaapiVP8Picture::VaapiVP8Picture(std::unique_ptr<VASurfaceHandle> va_surface)
+    : va_surface_(std::move(va_surface)) {}
 
 VaapiVP8Picture::~VaapiVP8Picture() = default;
 
@@ -39,8 +39,8 @@ VaapiVP8Picture* VaapiVP8Picture::AsVaapiVP8Picture() {
   return this;
 }
 
-VaapiVP9Picture::VaapiVP9Picture(scoped_refptr<VASurface> va_surface)
-    : va_surface_(va_surface) {}
+VaapiVP9Picture::VaapiVP9Picture(std::unique_ptr<VASurfaceHandle> va_surface)
+    : va_surface_(std::move(va_surface)) {}
 
 VaapiVP9Picture::~VaapiVP9Picture() = default;
 
@@ -53,8 +53,8 @@ scoped_refptr<VP9Picture> VaapiVP9Picture::CreateDuplicate() {
 }
 
 VaapiAV1Picture::VaapiAV1Picture(
-    scoped_refptr<VASurface> display_va_surface,
-    scoped_refptr<VASurface> reconstruct_va_surface)
+    std::unique_ptr<VASurfaceHandle> display_va_surface,
+    std::unique_ptr<VASurfaceHandle> reconstruct_va_surface)
     : display_va_surface_(std::move(display_va_surface)),
       reconstruct_va_surface_(std::move(reconstruct_va_surface)) {}
 
--- media/gpu/vaapi/vaapi_common.h
+++ media/gpu/vaapi/vaapi_common.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "media/gpu/av1_picture.h"
 #include "media/gpu/h264_dpb.h"
-#include "media/gpu/vaapi/va_surface.h"
+#include "media/gpu/vaapi/vaapi_utils.h"
 #include "media/gpu/vp8_picture.h"
 #include "media/gpu/vp9_picture.h"
 #include "media/media_buildflags.h"
@@ -25,7 +25,7 @@ namespace media {
 
 class VaapiH264Picture : public H264Picture {
  public:
-  explicit VaapiH264Picture(scoped_refptr<VASurface> va_surface);
+  explicit VaapiH264Picture(std::unique_ptr<VASurfaceHandle> va_surface);
 
   VaapiH264Picture(const VaapiH264Picture&) = delete;
   VaapiH264Picture& operator=(const VaapiH264Picture&) = delete;
@@ -38,13 +38,13 @@ class VaapiH264Picture : public H264Picture {
   ~VaapiH264Picture() override;
 
  private:
-  scoped_refptr<VASurface> va_surface_;
+  std::unique_ptr<VASurfaceHandle> va_surface_;
 };
 
 #if BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
 class VaapiH265Picture : public H265Picture {
  public:
-  explicit VaapiH265Picture(scoped_refptr<VASurface> va_surface);
+  explicit VaapiH265Picture(std::unique_ptr<VASurfaceHandle> va_surface);
 
   VaapiH265Picture(const VaapiH265Picture&) = delete;
   VaapiH265Picture& operator=(const VaapiH265Picture&) = delete;
@@ -57,13 +57,13 @@ class VaapiH265Picture : public H265Picture {
   ~VaapiH265Picture() override;
 
  private:
-  scoped_refptr<VASurface> va_surface_;
+  std::unique_ptr<VASurfaceHandle> va_surface_;
 };
 #endif  // BUILDFLAG(ENABLE_HEVC_PARSER_AND_HW_DECODER)
 
 class VaapiVP8Picture : public VP8Picture {
  public:
-  explicit VaapiVP8Picture(scoped_refptr<VASurface> va_surface);
+  explicit VaapiVP8Picture(std::unique_ptr<VASurfaceHandle> va_surface);
 
   VaapiVP8Picture(const VaapiVP8Picture&) = delete;
   VaapiVP8Picture& operator=(const VaapiVP8Picture&) = delete;
@@ -76,12 +76,12 @@ class VaapiVP8Picture : public VP8Picture {
   ~VaapiVP8Picture() override;
 
  private:
-  scoped_refptr<VASurface> va_surface_;
+  std::unique_ptr<VASurfaceHandle> va_surface_;
 };
 
 class VaapiVP9Picture : public VP9Picture {
  public:
-  explicit VaapiVP9Picture(scoped_refptr<VASurface> va_surface);
+  explicit VaapiVP9Picture(std::unique_ptr<VASurfaceHandle> va_surface);
 
   VaapiVP9Picture(const VaapiVP9Picture&) = delete;
   VaapiVP9Picture& operator=(const VaapiVP9Picture&) = delete;
@@ -96,13 +96,13 @@ class VaapiVP9Picture : public VP9Picture {
  private:
   scoped_refptr<VP9Picture> CreateDuplicate() override;
 
-  scoped_refptr<VASurface> va_surface_;
+  std::unique_ptr<VASurfaceHandle> va_surface_;
 };
 
 class VaapiAV1Picture : public AV1Picture {
  public:
-  VaapiAV1Picture(scoped_refptr<VASurface> display_va_surface,
-                  scoped_refptr<VASurface> reconstruct_va_surface);
+  VaapiAV1Picture(std::unique_ptr<VASurfaceHandle> display_va_surface,
+                  std::unique_ptr<VASurfaceHandle> reconstruct_va_surface);
   VaapiAV1Picture(const VaapiAV1Picture&) = delete;
   VaapiAV1Picture& operator=(const VaapiAV1Picture&) = delete;
 
@@ -129,8 +129,8 @@ class VaapiAV1Picture : public AV1Picture {
   // reconstruct_va_surface() when calling ExecuteAndDestroyPendingBuffers()
   // (the driver expects the reconstructed surface as the target in the case
   // of film grain synthesis).
-  scoped_refptr<VASurface> display_va_surface_;
-  scoped_refptr<VASurface> reconstruct_va_surface_;
+  std::unique_ptr<VASurfaceHandle> display_va_surface_;
+  std::unique_ptr<VASurfaceHandle> reconstruct_va_surface_;
 };
 
 }  // namespace media
