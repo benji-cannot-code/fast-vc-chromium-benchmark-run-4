9f0e0f1981368fad6396f49a92a795b05c3b69f5
Ce Chen <cechen@google.com>
2024-06-28 17:20:57 +0000

    [omnibox] add two proto fields to set on device tail model params.
    This will allow us to tune probability thresholds and/or RNN steps for each different i18n models easier.
    
    Server side proto change: cl/646589542.
    
    Bug: 40241602
    Change-Id: I2d36902a31986b7f819ce17228ccf810d6d42365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661039
    Commit-Queue: Ce Chen <cechen@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321051}

--- components/omnibox/browser/on_device_tail_model_executor_unittest.cc
+++ components/omnibox/browser/on_device_tail_model_executor_unittest.cc
@@ -23,6 +23,8 @@ namespace {
 constexpr int kNumLayer = 1;
 constexpr int kStateSize = 512;
 constexpr int kEmbeddingDim = 64;
+constexpr int kMaxNumSteps = 20;
+constexpr float kProbabilityThreshold = 0.05;
 
 base::FilePath GetTestFilePath(const std::string& filename) {
   base::FilePath file_path;
@@ -107,6 +109,9 @@ class OnDeviceTailModelExecutorTest : public ::testing::Test {
     metadata.mutable_lstm_model_params()->set_state_size(kStateSize);
     metadata.mutable_lstm_model_params()->set_embedding_dimension(
         kEmbeddingDim);
+    metadata.mutable_lstm_model_params()->set_max_num_steps(kMaxNumSteps);
+    metadata.mutable_lstm_model_params()->set_probability_threshold(
+        kProbabilityThreshold);
 
     base::flat_set<base::FilePath> additional_files;
     additional_files.insert(GetTestFilePath("vocab_test.txt"));
@@ -365,7 +370,7 @@ TEST_F(OnDeviceTailModelExecutorTest, TestGenerateSuggestionsForPrefix) {
   std::vector<OnDeviceTailModelExecutor::Prediction> predictions;
 
   {
-    OnDeviceTailModelExecutor::ModelInput input("faceb", "", 5, 20, 0.05);
+    OnDeviceTailModelExecutor::ModelInput input("faceb", "", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     EXPECT_FALSE(predictions.empty());
     EXPECT_TRUE(base::StartsWith(predictions[0].suggestion, "facebook",
@@ -373,14 +378,13 @@ TEST_F(OnDeviceTailModelExecutorTest, TestGenerateSuggestionsForPrefix) {
   }
 
   {
-    OnDeviceTailModelExecutor::ModelInput input("", "snapchat", 5, 20, 0.05);
+    OnDeviceTailModelExecutor::ModelInput input("", "snapchat", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     EXPECT_TRUE(predictions.empty());
   }
 
   {
-    OnDeviceTailModelExecutor::ModelInput input("faceb", "snapchat", 5, 20,
-                                                0.05);
+    OnDeviceTailModelExecutor::ModelInput input("faceb", "snapchat", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     EXPECT_FALSE(predictions.empty());
     EXPECT_TRUE(base::StartsWith(predictions[0].suggestion, "facebook",
@@ -395,7 +399,7 @@ TEST_F(OnDeviceTailModelExecutorTest,
   {
     // Make sure the test model can predict "login" for prefix "logi".
     InitExecutor(base::flat_set<std::string>());
-    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5, 20, 0.05);
+    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     EXPECT_EQ(predictions.size(), 1U);
     EXPECT_TRUE(base::StartsWith(predictions[0].suggestion, "login",
@@ -406,7 +410,7 @@ TEST_F(OnDeviceTailModelExecutorTest,
     // The test badwords file contains hash for word "login", so |predictions|
     // should not contain results with word "login".
     InitExecutor(base::flat_set<std::string>({"badword_hashes_test.txt"}));
-    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5, 20, 0.05);
+    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     for (auto& prediction : predictions) {
       auto words =
@@ -420,7 +424,7 @@ TEST_F(OnDeviceTailModelExecutorTest,
     // The denylist contains substrings "ogi" & "abc", so |prediction| should
     // not contain these strings neither.
     InitExecutor(base::flat_set<std::string>({"denylist_test.txt"}));
-    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5, 20, 0.05);
+    OnDeviceTailModelExecutor::ModelInput input("logi", "", 5);
     predictions = executor_->GenerateSuggestionsForPrefix(input);
     for (auto& prediction : predictions) {
       EXPECT_FALSE(base::Contains(prediction.suggestion, "abc"));
