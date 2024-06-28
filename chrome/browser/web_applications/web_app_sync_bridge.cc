9e1ffc9980b066caa121a50ef1b2e8c77cdfc035
Marc Treib <treib@chromium.org>
2024-06-28 15:28:58 +0000

    ModelTypeSyncBridge impls: Migrate to synchronous Get[All]Data, part 6
    GetDataForCommit() and GetAllDataForDebugging() are being migrated from
    asynchronous (returning the result via a callback) to synchronous
    (returning the result directly).
    
    This CL does that for the following sync bridges:
    PlusAddressSyncBridge
    PlusAddressSettingSyncBridge
    WebAppSyncBridge
    WebApkSyncBridge
    SavedTabGroupSyncBridge
    SharedTabGroupDataSyncBridge
    ProductSpecificationsSyncBridge
    ReadingListSyncBridge
    
    Note: ModelTypeSyncBridge currently allows overriding either of the
    two functions (synchronous or asynchronous), with the async one
    calling into the sync one by default, so calling sites don't need
    to be modified in this patch.
    
    Bug: 347649753
    Change-Id: I28333c7c3b8b26c4851bae9d7eaa9255d580288f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664939
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320982}

--- chrome/browser/web_applications/web_app_sync_bridge.cc
+++ chrome/browser/web_applications/web_app_sync_bridge.cc
@@ -881,8 +881,8 @@ std::optional<syncer::ModelError> WebAppSyncBridge::ApplyIncrementalSyncChanges(
   return std::nullopt;
 }
 
-void WebAppSyncBridge::GetDataForCommit(StorageKeyList storage_keys,
-                                        DataCallback callback) {
+std::unique_ptr<syncer::DataBatch> WebAppSyncBridge::GetDataForCommit(
+    StorageKeyList storage_keys) {
   auto data_batch = std::make_unique<syncer::MutableDataBatch>();
 
   for (const webapps::AppId& app_id : storage_keys) {
@@ -891,10 +891,10 @@ void WebAppSyncBridge::GetDataForCommit(StorageKeyList storage_keys,
       data_batch->Put(app->app_id(), CreateSyncEntityData(*app));
   }
 
-  std::move(callback).Run(std::move(data_batch));
+  return data_batch;
 }
 
-void WebAppSyncBridge::GetAllDataForDebugging(DataCallback callback) {
+std::unique_ptr<syncer::DataBatch> WebAppSyncBridge::GetAllDataForDebugging() {
   auto data_batch = std::make_unique<syncer::MutableDataBatch>();
 
   for (const WebApp& app : registrar_->GetAppsIncludingStubs()) {
@@ -902,7 +902,7 @@ void WebAppSyncBridge::GetAllDataForDebugging(DataCallback callback) {
       data_batch->Put(app.app_id(), CreateSyncEntityData(app));
   }
 
-  std::move(callback).Run(std::move(data_batch));
+  return data_batch;
 }
 
 std::string WebAppSyncBridge::GetClientTag(
--- components/reading_list/core/reading_list_sync_bridge.cc
+++ components/reading_list/core/reading_list_sync_bridge.cc
@@ -272,8 +272,8 @@ ReadingListSyncBridge::ApplyIncrementalSyncChanges(
   return {};
 }
 
-void ReadingListSyncBridge::GetDataForCommit(StorageKeyList storage_keys,
-                                             DataCallback callback) {
+std::unique_ptr<syncer::DataBatch> ReadingListSyncBridge::GetDataForCommit(
+    StorageKeyList storage_keys) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto batch = std::make_unique<syncer::MutableDataBatch>();
   for (const std::string& url_string : storage_keys) {
@@ -284,10 +284,11 @@ void ReadingListSyncBridge::GetDataForCommit(StorageKeyList storage_keys,
     }
   }
 
-  std::move(callback).Run(std::move(batch));
+  return batch;
 }
 
-void ReadingListSyncBridge::GetAllDataForDebugging(DataCallback callback) {
+std::unique_ptr<syncer::DataBatch>
+ReadingListSyncBridge::GetAllDataForDebugging() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto batch = std::make_unique<syncer::MutableDataBatch>();
 
@@ -297,7 +298,7 @@ void ReadingListSyncBridge::GetAllDataForDebugging(DataCallback callback) {
     AddEntryToBatch(batch.get(), *entry);
   }
 
-  std::move(callback).Run(std::move(batch));
+  return batch;
 }
 
 void ReadingListSyncBridge::AddEntryToBatch(syncer::MutableDataBatch* batch,
