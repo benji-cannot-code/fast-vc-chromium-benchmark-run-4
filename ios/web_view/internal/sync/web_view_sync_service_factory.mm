c58b7b262950be4b87e553dd50fa829b89304b6f
Marc Treib <treib@chromium.org>
2024-06-28 10:56:15 +0000

    SyncServiceImpl: Retrieve IdentityManager via SyncClient
    ...instead of having it explicitly plumbed in via an extra field in the
    InitParams.
    This addresses an old TODO assigned to me.
    
    Bug: 349646665
    Change-Id: If9349e20e87cb40f6ec7cfd6d6e278c8ebd4a5ec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660421
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1320869}

--- ios/web_view/internal/sync/web_view_sync_service_factory.mm
+++ ios/web_view/internal/sync/web_view_sync_service_factory.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/time.h"
 #import "components/autofill/core/browser/personal_data_manager.h"
 #import "components/keyed_service/ios/browser_state_dependency_manager.h"
-#import "components/signin/public/identity_manager/identity_manager.h"
 #import "components/sync/base/model_type.h"
 #import "components/sync/base/sync_util.h"
 #import "components/sync/service/sync_service.h"
@@ -51,9 +50,10 @@ WebViewSyncServiceFactory::WebViewSyncServiceFactory()
     : BrowserStateKeyedServiceFactory(
           "SyncService",
           BrowserStateDependencyManager::GetInstance()) {
-  // The SyncService depends on various SyncableServices being around
+  // The SyncServiceImpl depends on various KeyedServices being around
   // when it is shut down.  Specify those dependencies here to build the proper
-  // destruction order.
+  // destruction order. Note that some of the dependencies are listed here but
+  // actually plumbed in WebViewSyncClient, which this factory constructs.
   DependsOn(WebViewDeviceInfoSyncServiceFactory::GetInstance());
   DependsOn(WebViewIdentityManagerFactory::GetInstance());
   DependsOn(WebViewWebDataServiceWrapperFactory::GetInstance());
@@ -72,12 +72,9 @@ WebViewSyncServiceFactory::BuildServiceInstanceFor(
   WebViewBrowserState* browser_state =
       WebViewBrowserState::FromBrowserState(context);
 
-  signin::IdentityManager* identity_manager =
-      WebViewIdentityManagerFactory::GetForBrowserState(browser_state);
   WebViewGCMProfileServiceFactory::GetForBrowserState(browser_state);
 
   syncer::SyncServiceImpl::InitParams init_params;
-  init_params.identity_manager = identity_manager;
   init_params.sync_client = WebViewSyncClient::Create(browser_state);
   init_params.url_loader_factory = browser_state->GetSharedURLLoaderFactory();
   init_params.network_connection_tracker =
