473352c1e7bba621011374171fd43f5fe9f00d4d
Takashi Toyoshima <toyoshim@chromium.org>
2024-07-01 14:22:21 +0000

    SafeSearch: Add some more metrics to understand DEFER effects
    This patch adds 3 more metrics that record throttle actions for the
    request, and duration time for asynchronous events that determine
    the performance effects.
    
    Change-Id: Ib3f9f82e8813f0786c1c0bf2bf4b2317c6de58d8
    Bug: 349964973
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661342
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321658}

--- components/policy/content/policy_blocklist_navigation_throttle.h
+++ components/policy/content/policy_blocklist_navigation_throttle.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_POLICY_CONTENT_POLICY_BLOCKLIST_NAVIGATION_THROTTLE_H_
 #define COMPONENTS_POLICY_CONTENT_POLICY_BLOCKLIST_NAVIGATION_THROTTLE_H_
 
+#include "base/time/time.h"
 #include "components/policy/content/safe_sites_navigation_throttle.h"
 #include "content/public/browser/navigation_throttle.h"
 
@@ -33,9 +34,25 @@ class PolicyBlocklistNavigationThrottle : public content::NavigationThrottle {
   // NavigationThrottle overrides.
   ThrottleCheckResult WillStartRequest() override;
   ThrottleCheckResult WillRedirectRequest() override;
+  ThrottleCheckResult WillProcessResponse() override;
   const char* GetNameForLogging() override;
 
  private:
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  //
+  // LINT.IfChange(RequestThrottleAction)
+  enum class RequestThrottleAction {
+    kNoRequest = 0,
+    kProceed = 1,
+    kBlock = 2,
+    kDefer = 3,
+    kProceedAfterDefer = 4,
+    kBlockAfterDefer = 5,
+    kMaxValue = kBlockAfterDefer,
+  };
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/navigation/enums.xml:PolicyBlocklistRequestThrottleAction)
+
   // Returns TRUE if this navigation is to view-source: and view-source is on
   // the URLBlocklist.
   bool IsBlockedViewSourceNavigation();
@@ -46,6 +63,17 @@ class PolicyBlocklistNavigationThrottle : public content::NavigationThrottle {
   ThrottleCheckResult CheckSafeSitesFilter(const GURL& url);
   void OnDeferredSafeSitesResult(bool is_safe,
                                  ThrottleCheckResult cancel_result);
+
+  void UpdateRequestThrottleAction(
+      content::NavigationThrottle::ThrottleAction action);
+
+  RequestThrottleAction request_throttle_action_ =
+      RequestThrottleAction::kNoRequest;
+
+  base::TimeTicks request_time_;
+  base::TimeTicks defer_time_;
+  base::TimeDelta defer_duration_;
+
   SafeSitesNavigationThrottle safe_sites_navigation_throttle_;
 
   raw_ptr<PolicyBlocklistService, DanglingUntriaged> blocklist_service_;
