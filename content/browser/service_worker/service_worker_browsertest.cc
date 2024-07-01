07a2a2c0ab40a49e7bf59da3ca2ee810d64ba168
Minoru Chikamune <chikamune@chromium.org>
2024-07-01 10:25:29 +0000

    Add investigation code for the ServiceWorkerWarmUp related flaky tests
    Test run history for Pointer:
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fcontent%2Ftest%3Acontent_browsertests%2FServiceWorkerWarmUpByPointerBrowserTest.PointeroverOrPointerdownWillWarmUpServiceWorker%2FAll.1?q=VHASH%3A3c1b07ee95842fb1
    
    Test run history for VisibleAnchor:
    https://ci.chromium.org/ui/test/chromium%3Aci/ninja%3A%2F%2Fcontent%2Ftest%3Acontent_browsertests%2FServiceWorkerWarmUpByVisibilityBrowserTest.VisibleAnchorWillWarmUpServiceWorker%2FAll.0?q=VHASH%3A42fa53de1517ea94
    
    Bug: 349700657
    Bug: 348660828
    Change-Id: I3094b83494906c9f60c920846167993bb4816086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670085
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321609}

--- content/browser/service_worker/service_worker_browsertest.cc
+++ content/browser/service_worker/service_worker_browsertest.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/statistics_recorder.h"
+#include "base/notreached.h"
 #include "base/run_loop.h"
 #include "base/scoped_observation.h"
 #include "base/strings/strcat.h"
@@ -4654,6 +4655,7 @@ class ServiceWorkerWarmUpBrowserTestBase : public ServiceWorkerBrowserTest {
       document.body.appendChild(a);
     )";
     EXPECT_TRUE(ExecJs(GetPrimaryMainFrame(), JsReplace(script, id, url)));
+    base::RunLoop().RunUntilIdle();
   }
 };
 
@@ -4723,6 +4725,10 @@ IN_PROC_BROWSER_TEST_P(ServiceWorkerWarmUpByVisibilityBrowserTest,
     // Wait until version1 or version2 is warmed up.
     while (!(version1->IsWarmedUp() || version2->IsWarmedUp())) {
       run_loop.RunUntilIdle();
+      EXPECT_NE(blink::EmbeddedWorkerStatus::kRunning,
+                version1->running_status());
+      EXPECT_NE(blink::EmbeddedWorkerStatus::kRunning,
+                version2->running_status());
     }
     // Make sure that only one version is warmed up.
     EXPECT_FALSE(version1->IsWarmedUp() && version2->IsWarmedUp());
@@ -4730,9 +4736,13 @@ IN_PROC_BROWSER_TEST_P(ServiceWorkerWarmUpByVisibilityBrowserTest,
     // Wait until version1 and version2 are warmed up.
     while (!(version1->IsWarmedUp() && version2->IsWarmedUp())) {
       run_loop.RunUntilIdle();
+      EXPECT_NE(blink::EmbeddedWorkerStatus::kRunning,
+                version1->running_status());
+      EXPECT_NE(blink::EmbeddedWorkerStatus::kRunning,
+                version2->running_status());
     }
   } else {
-    NOTREACHED_IN_MIGRATION();
+    NOTREACHED_NORETURN();
   }
 }
 
@@ -4874,6 +4884,13 @@ class ServiceWorkerWarmUpByPointerBrowserTest
                               blink::WebMouseEvent::Button::kLeft, point);
   }
 
+  void WaitForWarmedUp(const ServiceWorkerVersion& version) {
+    base::RunLoop run_loop;
+    while (!version.IsWarmedUp()) {
+      run_loop.RunUntilIdle();
+    }
+  }
+
  private:
   base::test::ScopedFeatureList feature_list_;
 };
@@ -4904,7 +4921,6 @@ IN_PROC_BROWSER_TEST_P(ServiceWorkerWarmUpByPointerBrowserTest,
   const GURL in_scope_url(
       embedded_test_server()->GetURL("/service_worker/empty.html"));
   const GURL out_scope_url(embedded_test_server()->GetURL("/empty.html"));
-  base::RunLoop run_loop;
 
   scoped_refptr<ServiceWorkerVersion> version =
       RegisterServiceWorker(in_scope_url, in_scope_url);
@@ -4914,8 +4930,6 @@ IN_PROC_BROWSER_TEST_P(ServiceWorkerWarmUpByPointerBrowserTest,
   EXPECT_EQ(blink::EmbeddedWorkerStatus::kStopped, version->running_status());
 
   AddAnchor("in_scope_url", in_scope_url);
-
-  run_loop.RunUntilIdle();
 
   // To ensure that the pointerover event is triggered, move the pointer away
   // from the anchor area.
@@ -4929,9 +4943,7 @@ IN_PROC_BROWSER_TEST_P(ServiceWorkerWarmUpByPointerBrowserTest,
     SimulateMouseDownWithElementIdAndWait("in_scope_url");
   }
 
-  while (!version->IsWarmedUp()) {
-    run_loop.RunUntilIdle();
-  }
+  WaitForWarmedUp(*version);
 }
 
 #endif  // !BUILDFLAG(IS_ANDROID)
