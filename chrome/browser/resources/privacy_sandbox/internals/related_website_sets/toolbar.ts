157546f9894b3bd80b65ab8757d25cf91d222934
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-07-01 20:53:49 +0000

    Remove toolbar event handlers from Toolbar element
    This removes event handlers from RWSToolbarElement. Events triggered by CrToolbar will be caught by app element instead.
    https://chromium-review.googlesource.com/c/chromium/src/+/5626595
    
    Bug: 348573599
    Change-Id: I04866bc70520554d57b1583e99a0ef2b46e448fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668624
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321842}

--- chrome/browser/resources/privacy_sandbox/internals/related_website_sets/toolbar.ts
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/toolbar.ts
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_checkbox/cr_checkbox.js';
-import '//resources/cr_elements/cr_input/cr_input.js';
 import '//resources/cr_elements/cr_toolbar/cr_toolbar.js';
 
 import type {CrToolbarElement} from '//resources/cr_elements/cr_toolbar/cr_toolbar.js';
@@ -52,19 +50,6 @@ export class RelatedWebsiteSetsToolbarElement extends CrLitElement {
   protected narrowThreshold_: number = 1096;
   protected pageName: string = '';
   protected searchPrompt_: string = 'Search site';
-
-  protected onNarrowChanged_(e: CustomEvent<{value: boolean}>) {
-    this.narrow = e.detail.value;
-    // Custom Event for parent component to listen for and update narrow status.
-    this.dispatchEvent(
-        new CustomEvent('narrowChanged', {detail: {data: this.narrow}}));
-    this.requestUpdate();
-  }
-
-  protected onMenuClick_() {
-    this.dispatchEvent(new CustomEvent('menuClicked'));
-    this.requestUpdate();
-  }
 }
 
 declare global {
