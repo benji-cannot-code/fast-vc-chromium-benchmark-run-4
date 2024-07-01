64867e932d9ba5e19e864e534d988aa410eed619
Michelle <michellegc@google.com>
2024-07-01 04:31:34 +0000

    Make the whole PickerMainContainerView a traversable item container.
    This is to support up / down key navigation from search field buttons
    (i.e. "back" and "clear"), which are part of the overall main container
    but not part of the main container's "active page".
    
    To make it faster to navigate between search results, up / down
    generally only visits search result views. However, other views such as
    the "back" and "clear" buttons are still tabbable. After tabbing to the
    "back" or "clear" button, pressing up / down will navigate between the
    buttons then back to the search results views (and then normal arrow key
    key navigation resumes).
    
    Bug: b:347103427
    Change-Id: I984d38eb98127c03f42dcb73013298a4b07583b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665257
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321515}

--- ash/picker/views/picker_main_container_view.h
+++ ash/picker/views/picker_main_container_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/picker/views/picker_contents_view.h"
+#include "ash/picker/views/picker_traversable_item_container.h"
 #include "base/memory/raw_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
@@ -24,7 +25,9 @@ class SystemShadow;
 
 // View for the main Picker container, which consists of the search field and
 // the main contents (e.g. search results page).
-class ASH_EXPORT PickerMainContainerView : public views::View {
+class ASH_EXPORT PickerMainContainerView
+    : public views::View,
+      public PickerTraversableItemContainer {
   METADATA_HEADER(PickerMainContainerView, views::View)
 
  public:
@@ -32,6 +35,15 @@ class ASH_EXPORT PickerMainContainerView : public views::View {
   PickerMainContainerView(const PickerMainContainerView&) = delete;
   PickerMainContainerView& operator=(const PickerMainContainerView&) = delete;
   ~PickerMainContainerView() override;
+
+  // PickerTraversableItemContainer:
+  views::View* GetTopItem() override;
+  views::View* GetBottomItem() override;
+  views::View* GetItemAbove(views::View* item) override;
+  views::View* GetItemBelow(views::View* item) override;
+  views::View* GetItemLeftOf(views::View* item) override;
+  views::View* GetItemRightOf(views::View* item) override;
+  bool ContainsItem(views::View* item) override;
 
   PickerSearchFieldView* AddSearchFieldView(
       std::unique_ptr<PickerSearchFieldView> search_field_view);
