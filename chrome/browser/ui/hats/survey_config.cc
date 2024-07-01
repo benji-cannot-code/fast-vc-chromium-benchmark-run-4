708465ce79db2c9dbe4679bd79129a50b1983647
Mickey Burks <mickeyburks@chromium.org>
2024-07-01 12:34:25 +0000

    Enable What's New HaTS by default
    This converts the feature enabled probability into a local state
    threshold value. The threshold is compared to the country's
    probability to determine if the survey should be attempted.
    
    Bug: 327398187
    Change-Id: Id05edbbd4534056213df96f063b6b016b5ad9269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5527042
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321635}

--- chrome/browser/ui/hats/survey_config.cc
+++ chrome/browser/ui/hats/survey_config.cc
@@ -454,7 +454,7 @@ std::vector<hats::SurveyConfig> GetAllSurveyConfigs() {
   // What's New survey.
   survey_configs.emplace_back(
       &features::kHappinessTrackingSurveysForDesktopWhatsNew,
-      kHatsSurveyTriggerWhatsNew);
+      kHatsSurveyTriggerWhatsNew, "SYLcvnoRH0ugnJ3q1cK0RAHYFycs");
 
   // Performance Controls surveys.
   survey_configs.emplace_back(
