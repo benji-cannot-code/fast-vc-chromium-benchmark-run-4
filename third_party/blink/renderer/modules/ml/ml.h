027c1fa5d59bdf961e22a3fe9b5644013d1a1c65
Austin Sullivan <asully@chromium.org>
2024-07-01 23:45:55 +0000

    Remove the Model Loader API - part 1/n
    Deletes the implementation of the Model Loader API in blink
    
    Bug: 350407897
    Change-Id: Ie72565c10f487cb50cdb9993d4ee5c3731a1c320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666054
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Jiewei Qian <qjw@chromium.org>
    Commit-Queue: Jiewei Qian <qjw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321913}

--- third_party/blink/renderer/modules/ml/ml.h
+++ third_party/blink/renderer/modules/ml/ml.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_H_
 
-#include "components/ml/mojom/ml_service.mojom-blink.h"
 #include "services/webnn/public/mojom/webnn_context_provider.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -24,8 +23,8 @@ namespace blink {
 class MLContextOptions;
 class ScriptState;
 
-// This class represents the "Machine Learning" object "navigator.ml" and will
-// be shared between the Model Loader API and WebNN API.
+// This class represents the "Machine Learning" object "navigator.ml" used by
+// the WebNN API. See https://www.w3.org/TR/webnn/#api-ml.
 class MODULES_EXPORT ML final : public ScriptWrappable,
                                 public ExecutionContextClient {
   DEFINE_WRAPPERTYPEINFO();
@@ -36,12 +35,6 @@ class MODULES_EXPORT ML final : public ScriptWrappable,
   ML(const ML&) = delete;
   ML& operator=(const ML&) = delete;
 
-  void CreateModelLoader(
-      ScriptState* script_state,
-      ml::model_loader::mojom::blink::CreateModelLoaderOptionsPtr options,
-      ml::model_loader::mojom::blink::MLService::CreateModelLoaderCallback
-          callback);
-
   void Trace(blink::Visitor*) const override;
 
   // IDL interface:
@@ -50,13 +43,6 @@ class MODULES_EXPORT ML final : public ScriptWrappable,
                                          ExceptionState& exception_state);
 
  private:
-  // Binds the ModelLoader Mojo connection to browser process if needed.
-  // Caller is responsible to ensure `script_state` has a valid
-  // `ExecutionContext`.
-  void EnsureModelLoaderServiceConnection(ScriptState* script_state);
-  HeapMojoRemote<ml::model_loader::mojom::blink::MLService>
-      model_loader_service_;
-
   // Reset the remote of `WebNNContextProvider` if the remote is cut off from
   // its receiver.
   void OnWebNNServiceConnectionError();
--- third_party/blink/renderer/modules/ml/ml_context.h
+++ third_party/blink/renderer/modules/ml/ml_context.h
@@ -6,7 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_CONTEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_ML_ML_CONTEXT_H_
 
+#include <optional>
+
+#include "base/containers/span.h"
+#include "mojo/public/cpp/bindings/pending_associated_receiver.h"
 #include "services/webnn/public/cpp/context_properties.h"
+#include "services/webnn/public/mojom/webnn_buffer.mojom-blink-forward.h"
 #include "services/webnn/public/mojom/webnn_context_provider.mojom-blink.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
