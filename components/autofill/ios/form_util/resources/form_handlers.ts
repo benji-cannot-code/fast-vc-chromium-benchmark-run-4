c294243c94cdbd37526f034511dfe2bf2818d97d
Christoph Schwering <schwering@google.com>
2024-07-01 23:22:16 +0000

    [Autofill] Fix references to {fill,form,autofill_form_features}.ts
    {fill,form,autofill_form_features}.js are now known as
    {fill,form,autofill_form_features}.ts.
    
    Bug: 40100455
    Change-Id: Ie283123d68583513815b1287686d67aa17b63c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667160
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: John Wu <jzw@chromium.org>
    Reviewed-by: John Wu <jzw@chromium.org>
    Auto-Submit: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321897}

--- components/autofill/ios/form_util/resources/form_handlers.ts
+++ components/autofill/ios/form_util/resources/form_handlers.ts
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Autofill keyboard accessory.
  */
 
-// Requires functions from fill.js, form.js, and autofill_form_features.js.
+// Requires functions from fill.ts, form.ts, and autofill_form_features.ts.
 
 import {processChildFrameMessage} from '//components/autofill/ios/form_util/resources/child_frame_registration_lib.js';
 import {gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
