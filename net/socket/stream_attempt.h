7da5509befbf90b6816e0381a86419158513d628
Kenichi Ishibashi <bashi@chromium.org>
2024-07-01 10:48:47 +0000

    Handle ERR_SSL_CLIENT_AUTH_CERT_NEEDED in TlsStreamAttempt
    Handles the error in the same way as SSLConnectJob.
    
    Bug: 346835898
    Change-Id: I14f64c70694da8a7757771b7ee1be3341b21cd41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669885
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321619}

--- net/socket/stream_attempt.h
+++ net/socket/stream_attempt.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/scoped_refptr.h"
 #include "net/base/completion_once_callback.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/load_states.h"
@@ -23,6 +24,7 @@ class ClientSocketFactory;
 class HttpNetworkSession;
 class SocketPerformanceWatcherFactory;
 class SSLClientContext;
+class SSLCertRequestInfo;
 class StreamSocket;
 class NetworkQualityEstimator;
 class NetLog;
@@ -68,6 +70,10 @@ class NET_EXPORT_PRIVATE StreamAttempt {
 
   // Returns the load state of this attempt.
   virtual LoadState GetLoadState() const = 0;
+
+  // If the attempt failed with ERR_SSL_CLIENT_AUTH_CERT_NEEDED, returns the
+  // SSLCertRequestInfo received. Otherwise, returns nullptr.
+  virtual scoped_refptr<SSLCertRequestInfo> GetCertRequestInfo();
 
   std::unique_ptr<StreamSocket> ReleaseStreamSocket();
 
--- net/socket/tls_stream_attempt.h
+++ net/socket/tls_stream_attempt.h
@@ -9,11 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/timer/timer.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/ip_endpoint.h"
 #include "net/base/net_export.h"
 #include "net/socket/stream_attempt.h"
+#include "net/ssl/ssl_cert_request_info.h"
 #include "net/ssl/ssl_config.h"
 
 namespace net {
@@ -57,7 +59,9 @@ class NET_EXPORT_PRIVATE TlsStreamAttempt final : public StreamAttempt {
 
   ~TlsStreamAttempt() override;
 
+  // StreamAttempt implementations:
   LoadState GetLoadState() const override;
+  scoped_refptr<SSLCertRequestInfo> GetCertRequestInfo() override;
 
   bool IsTlsHandshakeStarted() { return tls_handshake_started_; }
 
@@ -91,6 +95,7 @@ class NET_EXPORT_PRIVATE TlsStreamAttempt final : public StreamAttempt {
   bool tls_handshake_started_ = false;
   base::OneShotTimer tls_handshake_timeout_timer_;
   std::unique_ptr<SSLClientSocket> ssl_socket_;
+  scoped_refptr<SSLCertRequestInfo> ssl_cert_request_info_;
 };
 
 }  // namespace net
