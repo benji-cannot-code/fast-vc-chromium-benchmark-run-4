63fabb96c7e153804c922bfccb5956f93635583a
Anders Hartvoll Ruud <andruud@chromium.org>
2024-07-01 09:44:56 +0000

    [gardener] FaceGazeTest.ToggleFaceGazeGesturesShort is flaky
    Example: https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/42770
    
    Bug: 348603598
    Change-Id: Ia4ca524b834c6fbcbd46fe8568e1780ed339c266
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667558
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Owners-Override: Anders Hartvoll Ruud <andruud@google.com>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Auto-Submit: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321593}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test.js
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/facegaze_test.js
@@ -1077,7 +1077,8 @@ AX_TEST_F('FaceGazeTest', 'DISABLED_ClosesCameraStream', async function() {
   assertFalse(!!win);
 });
 
-AX_TEST_F('FaceGazeTest', 'ToggleFaceGazeGesturesShort', async function() {
+// TODO(crbug.com/348603598): Test is flaky.
+AX_TEST_F('FaceGazeTest', 'DISABLED_ToggleFaceGazeGesturesShort', async function() {
   const gestureToMacroName =
       new Map()
           .set(FacialGesture.JAW_OPEN, MacroName.TOGGLE_FACEGAZE)
