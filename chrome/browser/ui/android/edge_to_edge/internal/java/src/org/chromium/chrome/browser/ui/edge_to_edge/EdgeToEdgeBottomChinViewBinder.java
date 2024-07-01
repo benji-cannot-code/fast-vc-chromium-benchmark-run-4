5f5cd3d15ec192937931ad93264df823b71c0d9f
Charles Hager <clhager@google.com>
2024-07-01 19:46:23 +0000

    Add simple bottom chin MVC implementation
    go/e2e-bottom-chin-design-doc
    
    This is a simple first implementation for the bottom chin, which seeks to implement a new bottom browser controls component to act as a scrollable bottom navbar. This first pass sets up the file structure for the C++ and MVC for the component, as well as some rudimentary logic.
    
    Bug: 345383907
    Change-Id: I3a55ee309dc7007a7a461be126faebcd96a2fd10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5601926
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321804}

---
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeBottomChinViewBinder.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+package org.chromium.chrome.browser.ui.edge_to_edge;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel;
+
+class EdgeToEdgeBottomChinViewBinder {
+    static void bind(
+            PropertyModel model,
+            EdgeToEdgeBottomChinSceneLayer sceneLayer,
+            PropertyKey propertyKey) {
+        if (EdgeToEdgeBottomChinProperties.Y_OFFSET == propertyKey) {
+            sceneLayer.setYOffset(model.get(EdgeToEdgeBottomChinProperties.Y_OFFSET));
+        } else if (EdgeToEdgeBottomChinProperties.HEIGHT == propertyKey) {
+            sceneLayer.setHeight(model.get(EdgeToEdgeBottomChinProperties.HEIGHT));
+        } else if (EdgeToEdgeBottomChinProperties.IS_VISIBLE == propertyKey) {
+            sceneLayer.setIsVisible(model.get(EdgeToEdgeBottomChinProperties.IS_VISIBLE));
+        } else if (EdgeToEdgeBottomChinProperties.COLOR == propertyKey) {
+            sceneLayer.setColor(model.get(EdgeToEdgeBottomChinProperties.COLOR));
+        } else {
+            assert false : "Unhandled property detected in EdgeToEdgeBottomChinViewBinder!";
+        }
+    }
+
+    // TODO(crbug.com/345383907) Move #bind logic to the compositor MCP method
+    static void bindCompositorMCP(
+            PropertyModel model,
+            EdgeToEdgeBottomChinSceneLayer sceneLayer,
+            PropertyKey propertyKey) {
+        assert propertyKey == null;
+    }
+}
