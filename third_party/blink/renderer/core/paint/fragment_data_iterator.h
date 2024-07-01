ee91c6c05ccb9dbffc5640b5d6de7f0c7a1a4a1d
lauren n. liberda <lauren@selfisekai.rocks>
2024-07-01 18:20:47 +0000

    iwyu: include cstddef for nullptr_t in fragment_data_iterator.h
    Bug: 41455655
    Change-Id: I7ad70dc8b2b005d75baf0b3d0578e561ca4864fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5426194
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Morten Stenshorne <mstensho@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321762}

--- third_party/blink/renderer/core/paint/fragment_data_iterator.h
+++ third_party/blink/renderer/core/paint/fragment_data_iterator.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_FRAGMENT_DATA_ITERATOR_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_PAINT_FRAGMENT_DATA_ITERATOR_H_
 
+#include <cstddef>
 #include <optional>
 
 #include "third_party/blink/renderer/core/layout/inline/inline_cursor.h"
@@ -23,7 +24,7 @@ class FragmentDataIteratorBase {
 
  public:
   explicit FragmentDataIteratorBase(Head& head) : fragment_head_(head) {}
-  explicit FragmentDataIteratorBase(nullptr_t) {}
+  explicit FragmentDataIteratorBase(std::nullptr_t) {}
 
   Data* GetFragmentData() const {
     return !IsDone() ? &fragment_head_.at(idx_) : nullptr;
