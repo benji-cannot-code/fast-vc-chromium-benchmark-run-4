b4d8a0d0e390920c6de720d454d797cee020a9b1
Zekun Jiang <zekunjiang@google.com>
2024-07-01 20:33:44 +0000

    Add HISTORY_EMBEDDINGS optimization type.
    Bug: b/350035878
    Change-Id: Iff183855ff94c3916931d4ca66ddf586203b4bd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670591
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321830}

--- components/optimization_guide/core/hints_processing_util.cc
+++ components/optimization_guide/core/hints_processing_util.cc
@@ -120,6 +120,8 @@ std::string GetStringNameForOptimizationType(
       return "VcnMerchantOptOutDiscover";
     case proto::OptimizationType::VCN_MERCHANT_OPT_OUT_MASTERCARD:
       return "VcnMerchantOptOutMastercard";
+    case proto::OptimizationType::HISTORY_EMBEDDINGS:
+      return "HistoryEmbeddings";
   }
 
   // The returned string is used to record histograms for the optimization type.
