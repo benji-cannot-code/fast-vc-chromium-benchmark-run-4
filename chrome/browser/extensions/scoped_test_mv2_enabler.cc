abce0235f9ef45f65d4481f01ffe35ab9383b3c6
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-07-01 19:43:23 +0000

    [Extensions] Add a test utility to allow loading MV2 extensions
    Chrome has announced that MV2 extensions will gradually no longer be
    supported in the browser. See more information at
    https://developer.chrome.com/blog/resuming-the-transition-to-mv3.
    The second step of this, after warning users about MV2 extensions, will
    be to disable unsupported MV2 extensions. If users want to re-enable
    these extensions, they will be able to.
    
    This CL adds a test-only mechanism to allow MV2 extensions to always
    be loaded in Chrome's automated test suites. This ensures we can keep
    test coverage for legacy MV2 extensions until support is fully removed.
    
    Bug: 339061151
    Change-Id: I6ce0b6193ed7af202b654d2d526ca09e6070a747
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5646987
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321803}

---
+++ chrome/browser/extensions/scoped_test_mv2_enabler.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/scoped_test_mv2_enabler.h"
+
+#include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
+
+namespace extensions {
+
+ScopedTestMV2Enabler::ScopedTestMV2Enabler()
+    : enable_mv2_extensions_(
+          ManifestV2ExperimentManager::AllowMV2ExtensionsForTesting(
+              PassKey())) {}
+
+ScopedTestMV2Enabler::~ScopedTestMV2Enabler() = default;
+
+}  // namespace extensions
