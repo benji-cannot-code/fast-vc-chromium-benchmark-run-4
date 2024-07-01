bcb7dfbe0b9fdf6153ead9cf4b8243091e0d5ef2
dpapad <dpapad@chromium.org>
2024-07-01 18:54:13 +0000

    NewTabPage: Delete obsolete ntp-customize-dialog and related code.
    This is in preparation of deleting
    ui/webui/resources/cr_components/customize_themes.
    
    Bug: 40894552
    Change-Id: Ie55a6dc311e4479df27d5108e9cbf9ae48a43415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668722
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321772}

--- chrome/test/data/webui/new_tab_page/app_test.ts
+++ chrome/test/data/webui/new_tab_page/app_test.ts
@@ -79,11 +79,6 @@ suite('NewTabPageAppTest', () => {
   });
 
   suite('Misc', () => {
-    test('customize dialog closed on start', () => {
-      // Assert.
-      assertFalse(!!app.shadowRoot!.querySelector('ntp-customize-dialog'));
-    });
-
     test('logs height', async () => {
       // Assert.
       assertEquals(1, metrics.count('NewTabPage.Height'));
