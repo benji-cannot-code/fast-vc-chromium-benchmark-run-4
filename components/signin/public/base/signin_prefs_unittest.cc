700c90f0733a24d7630354bc4f9733c094337181
David Roger <droger@chromium.org>
2024-07-01 10:45:15 +0000

    [Uno-D] Add metric for removal of account-keyed prefs
    Records the number of accounts removed from account-keyed signin
    preferences.
    
    In addition to the overall number, additional histograms are added to
    split the data by signin state.
    
    Bug: b/346496565
    Change-Id: Ibcf7d1871717b12fd3ead6fb3d98258ff901838d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667404
    Commit-Queue: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321615}

--- components/signin/public/base/signin_prefs_unittest.cc
+++ components/signin/public/base/signin_prefs_unittest.cc
@@ -60,14 +60,16 @@ TEST_F(SigninPrefsTest, RemovingAccountPrefs) {
   ASSERT_TRUE(HasAccountPrefs(gaia_id3));
 
   // Should remove `gaia_id3`.
-  signin_prefs().RemoveAllAccountPrefsExcept({gaia_id1, gaia_id2});
+  EXPECT_EQ(signin_prefs().RemoveAllAccountPrefsExcept({gaia_id1, gaia_id2}),
+            1u);
   EXPECT_TRUE(HasAccountPrefs(gaia_id1));
   EXPECT_TRUE(HasAccountPrefs(gaia_id2));
   EXPECT_FALSE(HasAccountPrefs(gaia_id3));
 
   // Should remove `gaia_id2`. Adding a non existing pref should have no effect
   // (`gaia_id3`).
-  signin_prefs().RemoveAllAccountPrefsExcept({gaia_id1, gaia_id3});
+  EXPECT_EQ(signin_prefs().RemoveAllAccountPrefsExcept({gaia_id1, gaia_id3}),
+            1u);
   EXPECT_TRUE(HasAccountPrefs(gaia_id1));
   EXPECT_FALSE(HasAccountPrefs(gaia_id2));
   EXPECT_FALSE(HasAccountPrefs(gaia_id3));
@@ -87,7 +89,7 @@ TEST_F(SigninPrefsTest, RemovingAllAccountPrefs) {
   ASSERT_TRUE(HasAccountPrefs(gaia_id3));
 
   // Passing no accounts in the arguments should clear all prefs.
-  signin_prefs().RemoveAllAccountPrefsExcept({});
+  EXPECT_EQ(signin_prefs().RemoveAllAccountPrefsExcept({}), 3u);
   EXPECT_FALSE(HasAccountPrefs(gaia_id1));
   EXPECT_FALSE(HasAccountPrefs(gaia_id2));
   EXPECT_FALSE(HasAccountPrefs(gaia_id3));
