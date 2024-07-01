f118b1c33444bb8db56af7fdfa0b95e52b62def1
Paras Awasthi <awasthiparas6@gmail.com>
2024-07-01 19:38:36 +0000

    PowerBookmarks: Disabled Tree view feature flag as default case in power_bookmark_list_test file
    Bug: 346598546
    Change-Id: Ieed1f87ddb8b6d7c993dc29f0151fe0de43388ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666998
    Reviewed-by: Alison Gale <agale@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Paras Awasthi <awasthiparas6@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1321799}

--- chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_list_test.ts
+++ chrome/test/data/webui/side_panel/bookmarks/power_bookmarks_list_test.ts
@@ -192,6 +192,7 @@ suite('SidePanelPowerBookmarksListTest', () => {
       emptyBodyFolder: 'folder body',
       emptyTitleGuest: 'guest title',
       emptyBodyGuest: 'guest body',
+      bookmarksTreeViewEnabled: false,
     });
 
     await initializeUI();
