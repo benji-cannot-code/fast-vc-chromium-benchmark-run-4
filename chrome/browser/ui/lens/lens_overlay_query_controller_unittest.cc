f54f1d86059c1b769ecfce16d848c904c91aa89b
Jason Hu <hujasonx@google.com>
2024-07-01 21:09:36 +0000

    [lensoverlay] Fix gen204 crashing tests and enable flag
    Bug: 344615393
    Change-Id: I6664cb562c1dc41687cb287d2d2733f885dbf626
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670883
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Jason Hu <hujasonx@google.com>
    Cr-Commit-Position: refs/heads/main@{#1321849}

--- chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
+++ chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
@@ -117,6 +117,7 @@ class LensOverlayQueryControllerMock : public LensOverlayQueryController {
   lens::LensOverlayInteractionResponse fake_interaction_response_;
   lens::LensOverlayObjectsRequest sent_objects_request_;
   lens::LensOverlayInteractionRequest sent_interaction_request_;
+  int num_gen204_pings_sent_ = 0;
 
  protected:
   void CreateAndFetchEndpointFetcher(
@@ -149,6 +150,11 @@ class LensOverlayQueryControllerMock : public LensOverlayQueryController {
         FROM_HERE, base::BindOnce(std::move(fetcher_created_callback),
                                   std::move(endpoint_fetcher)));
     fetcher->PerformRequest(std::move(endpoint_fetcher_callback), kTestApiKey);
+  }
+
+  void SendLatencyGen204IfEnabled(int64_t latency_ms,
+                                  uint64_t gen204_identifier) override {
+    num_gen204_pings_sent_++;
   }
 };
 
@@ -254,6 +260,7 @@ TEST_F(LensOverlayQueryControllerTest, FetchInitialQuery_ReturnsResponse) {
                 .locale_context()
                 .time_zone(),
             kTimeZone);
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 0);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -355,6 +362,7 @@ TEST_F(LensOverlayQueryControllerTest,
       query_controller.sent_interaction_request_.interaction_request_metadata()
           .has_query_metadata());
   ASSERT_TRUE(has_start_time);
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 1);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -473,6 +481,7 @@ TEST_F(LensOverlayQueryControllerTest,
       query_controller.sent_interaction_request_.interaction_request_metadata()
           .has_query_metadata());
   ASSERT_TRUE(has_start_time);
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 1);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -578,6 +587,7 @@ TEST_F(LensOverlayQueryControllerTest,
           .query(),
       kTestQueryText);
   ASSERT_TRUE(has_start_time);
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 1);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -632,6 +642,7 @@ TEST_F(LensOverlayQueryControllerTest,
             lens::SELECT_TEXT_HIGHLIGHT);
   ASSERT_EQ(actual_encoded_search_context, kTestEncodedSearchContext);
   ASSERT_TRUE(has_start_time);
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 0);
 }
 
 TEST_F(LensOverlayQueryControllerTest,
@@ -685,6 +696,7 @@ TEST_F(LensOverlayQueryControllerTest,
   ASSERT_TRUE(full_image_response_future.IsReady());
   ASSERT_TRUE(url_response_future.IsReady());
   ASSERT_TRUE(interaction_data_response_future.IsReady());
+  ASSERT_EQ(query_controller.num_gen204_pings_sent_, 2);
 }
 
 }  // namespace lens
