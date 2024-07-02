41d06efce9a78b0e4dc369e60d671e218fef7ce3
Andres Ricardo Perez <andresrperez@chromium.org>
2024-07-02 15:29:28 +0000

    Add fonts to VariantGrid params and default Variant params
    For fonts to be loaded from the templates for single-file test grids,
    they must be visible from the grid as a whole, and they must be
    aggregated for cases with different fonts declared in different
    variants.
    
    This change adds the `fonts` param to grids using `_param_set`, creates
    it as an empty list when initializing a default variant.
    
    In a follow-up CL in the stack, a test is created that requires the
    loading of fonts and uses single-file variant grids.
    
    Bug: 341213359
    Change-Id: Id6d83d26ab4cfa9a98747d573926fc40a24ab565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5635554
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Andres Ricardo Perez <andresrperez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322188}

--- third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutilsunion.py
+++ third_party/blink/web_tests/external/wpt/html/canvas/tools/gentestutilsunion.py
@@ -361,6 +361,7 @@ class _Variant():
             'variant_name': '',
             'images': [],
             'svgimages': [],
+            'fonts': [],
         }
         params.update(test)
         return _Variant(params)
@@ -640,6 +641,7 @@ class _VariantGrid:
             'notes': self._unique_param('notes'),
             'images': self._param_set('images'),
             'svgimages': self._param_set('svgimages'),
+            'fonts': self._param_set('fonts'),
         }
         if self.template_type in (_TemplateType.REFERENCE,
                                   _TemplateType.HTML_REFERENCE):
