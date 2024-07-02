482e3954494dcb02ae29cdbd683b06f6f0766083
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-07-02 17:21:31 +0000

    Move analysis_settings* to components/enterprise/connectors/analysis/
    Bug: 318421275
    Change-Id: Ia0d29879a41e043847973bc55df323cc72618483
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665077
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322272}

--- chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
+++ chrome/browser/safe_browsing/cloud_content_scanning/binary_upload_service.cc
@@ -10,12 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/enterprise/connectors/analysis/analysis_settings.h"
 #include "chrome/browser/enterprise/connectors/common.h"
 #include "chrome/browser/policy/chrome_browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/safe_browsing/cloud_content_scanning/cloud_binary_upload_service_factory.h"
 #include "components/enterprise/common/strings.h"
+#include "components/enterprise/connectors/analysis/analysis_settings.h"
 #include "net/base/url_util.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 
