4b53989a3e61b45463bcff2404604a2128a53417
Jan Keitel <jkeitel@google.com>
2024-07-02 15:53:20 +0000

    Use learn more URL on plus address iOS bottomsheet.
    Bug: 348353662
    Change-Id: Id0eeea1e18b31f8199ff8bab0babf2cda272ff6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672185
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vidhan Jain <vidhanj@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322206}

--- ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_mediator.mm
+++ ios/chrome/browser/plus_addresses/coordinator/plus_address_bottom_sheet_mediator.mm
@@ -173,6 +173,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       return GURL(plus_addresses::features::kPlusAddressErrorReportUrl.Get());
     case PlusAddressURLType::kManagement:
       return GURL(plus_addresses::features::kPlusAddressManagementUrl.Get());
+    case PlusAddressURLType::kLearnMore:
+      return GURL(plus_addresses::features::kPlusAddressLearnMoreUrl.Get());
   }
 }
 @end
