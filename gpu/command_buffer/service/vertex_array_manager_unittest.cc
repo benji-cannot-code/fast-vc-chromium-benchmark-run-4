79620f229d246f59350b0b09a27431a092ebf829
Zhenyao Mo <zmo@chromium.org>
2024-07-02 15:32:57 +0000

    Remove ARB extensions support from GL bindings: Part VII.
    * GL_ARB_vertex_array_object
    
    This is part of
    https://chromium-review.googlesource.com/c/chromium/src/+/4355969
    
    The original CL caused many command buffer unit tests failing.
    
    I believe it is due to the test setups are wrong, but fixing all
    of them in one CL is too much.
    
    Splitting into multiple ones, so it's easier to read and review.
    
    TEST=bots
    
    Bug: 1426503
    Change-Id: I0ac81310ae23205eb3421c5e099b7387b13b5865
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637730
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322194}

--- gpu/command_buffer/service/vertex_array_manager_unittest.cc
+++ gpu/command_buffer/service/vertex_array_manager_unittest.cc
@@ -32,7 +32,7 @@ class VertexArrayManagerTest : public GpuServiceTest {
 
  protected:
   void SetUp() override {
-    GpuServiceTest::SetUpWithGLVersion("2.1", "GL_ARB_vertex_array_object");
+    GpuServiceTest::SetUpWithGLVersion("OpenGL ES 3.0", "");
     manager_ = std::make_unique<VertexArrayManager>();
   }
 
@@ -98,5 +98,3 @@ TEST_F(VertexArrayManagerTest, Destroy) {
 
 }  // namespace gles2
 }  // namespace gpu
-
-
