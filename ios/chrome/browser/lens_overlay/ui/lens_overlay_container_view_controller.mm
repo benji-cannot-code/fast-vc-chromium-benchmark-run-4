20112b6ca1a7bffd4cbc540ebd50d4feb789c120
Radu Nitescu <radunitescu@google.com>
2024-07-02 13:52:25 +0000

    [iOS][LO] Set accessibilityIdentifier after view loaded
    Setting the accessibilityIdentifier in init would cause the view to load
    before having a selectionViewController set. By moving this to
    viewDidLoad the selectionViewController is presented correctly
    
    Bug: None
    Change-Id: I5eeb6b2d22ded37268cc0cea4909d13df61e60ff
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665124
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322145}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
@@ -32,8 +32,6 @@ const CGFloat closeButtonTrailingPadding = 16.0;
 
   if (self) {
     _overlayCommandsHandler = handler;
-    self.view.accessibilityIdentifier =
-        kLenscontainerViewAccessibilityIdentifier;
   }
   return self;
 }
@@ -42,6 +40,7 @@ const CGFloat closeButtonTrailingPadding = 16.0;
   [super viewDidLoad];
 
   self.view.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
+  self.view.accessibilityIdentifier = kLenscontainerViewAccessibilityIdentifier;
 
   self.closeButton.translatesAutoresizingMaskIntoConstraints = NO;
 
