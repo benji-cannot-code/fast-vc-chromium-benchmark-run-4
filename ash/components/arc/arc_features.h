8149fdf49c8900ede72fb34069f0f166ab36a298
Yao Qijing <yaoqq@google.com>
2024-07-02 06:45:07 +0000

    Add friendlier error dialog flag and propagate to ARC
    This CL adds a flag to enable friendlier error dialog feature (replace
    disruptive error dialogs with Chrome notifications for some ANR and
    crash events), and propagates the flag to ARC.
    
    BUG=b:349503989
    Test=manual
    
    Change-Id: I1506c3a7ab8562c38c3e3b61cfbad0585dd04a60
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665373
    Commit-Queue: Qijing Yao <yaoqq@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322022}

--- ash/components/arc/arc_features.h
+++ ash/components/arc/arc_features.h
@@ -35,6 +35,7 @@ extern const base::FeatureParam<bool>
 BASE_DECLARE_FEATURE(kEnableArcNearbyShareFuseBox);
 BASE_DECLARE_FEATURE(kEnableArcS2Idle);
 BASE_DECLARE_FEATURE(kEnableArcVmDataMigration);
+BASE_DECLARE_FEATURE(kEnableFriendlierErrorDialog);
 BASE_DECLARE_FEATURE(kEnableLazyWebViewInit);
 BASE_DECLARE_FEATURE(kEnablePerVmCoreScheduling);
 BASE_DECLARE_FEATURE(kEnableReadOnlyPermissions);
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3367,6 +3367,9 @@ extern const char kArcExtendInputAnrTimeoutDescription[];
 extern const char kArcFilePickerExperimentName[];
 extern const char kArcFilePickerExperimentDescription[];
 
+extern const char kArcFriendlierErrorDialogName[];
+extern const char kArcFriendlierErrorDialogDescription[];
+
 extern const char kArcIdleManagerName[];
 extern const char kArcIdleManagerDescription[];
 
