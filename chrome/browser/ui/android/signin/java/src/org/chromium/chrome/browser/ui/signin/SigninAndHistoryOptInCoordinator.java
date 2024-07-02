793dec16935f7ca3c3c7bd2144272fd55d818a7a
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-07-02 09:05:49 +0000

    Revert "Update HistorySyncCoordinator to be created only once"
    This reverts commit f66d85d6f5cc8b355cc00108e58f7c8828d42ac7.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5702953349414912
    
    Sample build with failed test: https://ci.chromium.org/b/8743586120671829345
    Affected test(s):
    [ninja://chrome/android:chrome_public_test_apk/org.chromium.chrome.browser.ui.signin.history_sync.HistorySyncTest#testNegativeButtonNonMinorModeAccount](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Fandroid:chrome_public_test_apk%2Forg.chromium.chrome.browser.ui.signin.history_sync.HistorySyncTest%23testNegativeButtonNonMinorModeAccount?q=VHash%3A64c9b4839b185fb7)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5702953349414912&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5625787&type=BUG
    
    Original change's description:
    > Update HistorySyncCoordinator to be created only once
    >
    > * Refactors the Coordinator to be created only once and have a new View
    > be created on each configuration change.
    >
    > * This is done in so that the model can be persisted for the
    > SyncButtons.Shown metric.
    >
    > Bug: 346541884, 338552074
    > Change-Id: I71d7bb42385c04c1a90ba45edd56d57f90ff694f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5625787
    > Auto-Submit: Liza Bipin <mlbipin@google.com>
    > Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    > Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1321790}
    >
    
    Bug: 346541884, 338552074
    Change-Id: I8a3676d40ed9c0932031766619f1e5ed8cb1f332
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5671069
    Owners-Override: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322068}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninAndHistoryOptInCoordinator.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninAndHistoryOptInCoordinator.java
@@ -308,10 +308,10 @@ public class SigninAndHistoryOptInCoordinator
 
     public void switchHistorySyncLayout() {
         if (mHistorySyncCoordinator != null) {
+            mHistorySyncCoordinator.destroy();
             Profile profile = mProfileSupplier.get();
             assert profile != null;
-            mHistorySyncCoordinator.maybeRecreateView();
-            showDialogContentView();
+            showDialogContentView(profile);
         }
     }
 
@@ -452,12 +452,10 @@ public class SigninAndHistoryOptInCoordinator
                                 })
                         .build();
 
-        createHistorySyncCoordinator(profile);
-        showDialogContentView();
+        showDialogContentView(profile);
     }
 
-    private void createHistorySyncCoordinator(Profile profile) {
-        assert mHistorySyncCoordinator == null;
+    private void showDialogContentView(Profile profile) {
         mHistorySyncCoordinator =
                 new HistorySyncCoordinator(
                         mActivity,
@@ -468,10 +466,6 @@ public class SigninAndHistoryOptInCoordinator
                         mDidShowSigninStep && mIsHistorySyncDedicatedFlow,
                         null);
         assert mDialogModel != null;
-        mHistorySyncCoordinator.maybeRecreateView();
-    }
-
-    void showDialogContentView() {
         View view = mHistorySyncCoordinator.getView();
         view.setBackgroundColor(getHistorySyncBackgroundColor());
         mDialogModel.set(ModalDialogProperties.CUSTOM_VIEW, view);
--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/history_sync/HistorySyncTest.java
@@ -504,7 +504,7 @@ public class HistorySyncTest {
                                     null);
                     mActivityTestRule
                             .getActivity()
-                            .setContentView(mHistorySyncCoordinator.maybeRecreateView());
+                            .setContentView(mHistorySyncCoordinator.getView());
                 });
         ViewUtils.waitForVisibleView(allOf(withId(R.id.history_sync_title), isDisplayed()));
     }
