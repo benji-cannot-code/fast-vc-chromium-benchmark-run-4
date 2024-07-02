afef1070a9ce076975f1c4bd47a6584c5a03fba7
Christian Flach <cmfcmf@chromium.org>
2024-07-02 18:07:16 +0000

    iwa: Parse Update Channel metadata from Update Manifest
    Internal design doc: go/iwa-update-channels
    
    Fixed: 330157535
    Change-Id: If6f931999c1113682b358f58215af94703128bbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412797
    Commit-Queue: Christian Flach <cmfcmf@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322306}

--- chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
+++ chrome/browser/web_applications/isolated_web_apps/update_manifest/update_manifest_fuzzer.cc
@@ -22,6 +22,10 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   "versions": []
                 })"),
                 *base::JSONReader::Read(R"({
+                  "channels": {},
+                  "versions": []
+                })"),
+                *base::JSONReader::Read(R"({
                   "versions": [
                     {
                       "version": "1.0.0",
@@ -47,6 +51,24 @@ FUZZ_TEST(UpdateManifestFuzzTest, UpdateManifestCanSuccessfullyParseAnyString)
                   ]
                 })"),
                 *base::JSONReader::Read(R"({
+                  "versions": [
+                    {
+                      "version": "1.0.0",
+                      "url": "https://example.com/bundle.swbn",
+                      "channels": ["test", "stable", "test"]
+                    }
+                  ]
+                })"),
+                *base::JSONReader::Read(R"({
+                  "channels" {
+                    "test": {
+                      "name": "Test Title",
+                    },
+                    "stable": {
+                      "another property": 123,
+                    },
+                    "another channel": {}
+                  },
                   "versions": [
                     {
                       "version": "1.0.0",
