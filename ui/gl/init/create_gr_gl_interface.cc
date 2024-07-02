652de3f8bee7c716f19de468e14a98cd4cb8f824
Colin Blundell <blundell@chromium.org>
2024-07-02 15:35:55 +0000

    [//ui/gl] Add base::Feature to add delay when linking GL program
    Will be used for an ablation study to determine the impact of GL program
    linking delay on higher-level user metrics.
    
    Change-Id: I554d15c6d7788731e09a5a8ce33088da2e918856
    Bug: 40160681
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659910
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322196}

--- ui/gl/init/create_gr_gl_interface.cc
+++ ui/gl/init/create_gr_gl_interface.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/init/create_gr_gl_interface.h"
 
+#include "base/feature_list.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/no_destructor.h"
 #include "base/trace_event/trace_event.h"
@@ -28,6 +29,16 @@ namespace gl::init {
 // EGL_KHR_fence_sync extension. It's used to provide Skia ways of
 // synchronization on platforms that does not have GL fences but support EGL
 namespace {
+
+// If enabled, adds a delay to GL program link whose value is given by the
+// feature param. Used for an ablation study.
+BASE_FEATURE(kAddDelayToGLProgramLink,
+             "AddDelayToGLProgramLink",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+constexpr base::FeatureParam<int> kGLProgramLinkDelayMicroseconds{
+    &kAddDelayToGLProgramLink, /*name=*/"GLProgramLinkDelayMicroseconds",
+    /*default_value=*/1000};
+
 struct EGLFenceData {
   EGLSync sync;
   EGLDisplay display;
@@ -201,6 +212,11 @@ GrGLFunction<R GR_GL_FUNCTION_TYPE(GLuint, Args...)> bind_timed_link_function(
                                                    Args... args) -> R {
     gl::ScopedProgressReporter scoped_reporter(progress_reporter);
     SCOPED_UMA_HISTOGRAM_TIMER_MICROS("Gpu.GrLinkProgramUs");
+
+    if (base::FeatureList::IsEnabled(kAddDelayToGLProgramLink)) {
+      base::PlatformThread::Sleep(
+          base::Microseconds(kGLProgramLinkDelayMicroseconds.Get()));
+    }
 
     func(program, args...);
 
