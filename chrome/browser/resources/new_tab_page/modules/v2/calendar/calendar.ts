d8d7df2dcdb71bdf56f5aa2fe4874074b8808353
Riley Tatum <rtatum@google.com>
2024-07-02 00:45:39 +0000

    [NTP][Enterprise] Expand next calendar event
    Because events can now be up to 15 minutes past their end time, the
    first meeting isn't always the user's next meeting. This CL adds extra
    logic to selecting the expanded event to find the first event that
    is not already over.
    
    Also, updated styling for the first event because <style> is actually
    the first child, rather than the first event. Instead, select the first
    event (not the first row, otherwise, "See More" will also be selected
    by this).
    
    Screenshot: http://screenshot/7oQGUGJHtGRSB3Q
    
    Bug: 347053777
    Change-Id: I18c76abd02d5e83374591744a76b288668e35b50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665182
    Commit-Queue: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321941}

--- chrome/browser/resources/new_tab_page/modules/v2/calendar/calendar.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/calendar/calendar.ts
@@ -7,8 +7,10 @@ import './calendar_event.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import type {CalendarEvent} from '../../../google_calendar.mojom-webui.js';
+import {WindowProxy} from '../../../window_proxy.js';
 
 import {getTemplate} from './calendar.html.js';
+import {toJsTimestamp} from './common.js';
 
 export interface CalendarElement {
   $: {
@@ -34,7 +36,7 @@ export class CalendarElement extends PolymerElement {
       events: Object,
       expandedEventIndex_: {
         type: Number,
-        value: 0,
+        computed: 'computeExpandedEventIndex_(events)',
       },
     };
   }
@@ -43,6 +45,20 @@ export class CalendarElement extends PolymerElement {
   events: CalendarEvent[];
 
   private expandedEventIndex_: number;
+
+  private computeExpandedEventIndex_(): number {
+    const now = WindowProxy.getInstance().now();
+    for (let i = 0; i < this.events.length; i++) {
+      const endTimeMs = toJsTimestamp(this.events[i].endTime);
+
+      // If the current time is before the end of the meeting, it is the
+      // soonest meeting or current meeting, and should be expanded.
+      if (now < endTimeMs) {
+        return i;
+      }
+    }
+    return 0;
+  }
 
   private isExpanded_(index: number) {
     return index === this.expandedEventIndex_;
--- chrome/test/data/webui/new_tab_page/modules/v2/calendar/test_support.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/calendar/test_support.ts
@@ -4,6 +4,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {Attachment, CalendarEvent} from 'chrome://new-tab-page/google_calendar.mojom-webui.js';
+import type {Time} from 'chrome://resources/mojo/mojo/public/mojom/base/time.mojom-webui.js';
+
+// Microseconds between windows and unix epoch.
+const kWindowsToUnixEpochOffset: bigint = 11644473600000000n;
+
+export function toTime(time: Date): Time {
+  return {
+    internalValue: BigInt(time.valueOf()) * 1000n + kWindowsToUnixEpochOffset,
+  };
+}
 
 export function createAttachments(num: number): Attachment[] {
   const attachments: Attachment[] = [];
@@ -23,6 +33,7 @@ export function createEvent(
       {
         title: `Test Event ${index}`,
         startTime: {internalValue: 1230000000000n * BigInt(index)},
+        endTime: {internalValue: 1230000000000n * BigInt(index)},
         url: {url: `https://foo.com/${index}`},
         location: `Location ${index}`,
         attachments: createAttachments(3),
