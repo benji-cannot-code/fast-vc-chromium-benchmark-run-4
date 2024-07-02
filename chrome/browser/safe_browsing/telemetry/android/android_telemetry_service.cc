199cab96c857ba6f673d84880d333c8ef4a67b51
Daniel Rubery <drubery@chromium.org>
2024-07-02 19:48:33 +0000

    Indicate if Play Protect is enabled in APK telemetry reports
    For better understanding of end-to-end warning effectiveness, add a new
    boolean to APK telemetry reports with whether the user will be protected
    by Google Play Protect.
    
    This requires making AndroidTelemetryService::GetReport asynchronous,
    which is the bulk of this CL.
    
    Bug: 349939550
    Change-Id: I3afd1b61d5a192726848649429540a01a29e99af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640094
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322367}

--- chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
+++ chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.cc
@@ -25,6 +25,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/safe_browsing_navigation_observer_manager_factory.h"
 #include "components/keyed_service/core/service_access_type.h"
 #include "components/prefs/pref_service.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_bridge.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_util.h"
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/core/browser/db/database_manager.h"
 #include "components/safe_browsing/core/browser/ping_manager.h"
@@ -99,12 +101,8 @@ void AndroidTelemetryService::OnDownloadUpdated(download::DownloadItem* item) {
 
   if (item->GetState() == download::DownloadItem::COMPLETE) {
     // Download completed. Send report.
-    std::unique_ptr<ClientSafeBrowsingReportRequest> report = GetReport(item);
-    MaybeSendApkDownloadReport(
-        content::DownloadItemUtils::GetBrowserContext(item), std::move(report));
-    // No longer interested in this |DownloadItem| since the report has been
-    // sent so remove the observer.
-    item->RemoveObserver(this);
+    GetReport(item, base::BindOnce(&AndroidTelemetryService::OnGetReportDone,
+                                   weak_ptr_factory_.GetWeakPtr(), item));
   } else if (item->GetState() == download::DownloadItem::CANCELLED) {
     RecordApkDownloadTelemetryOutcome(
         ApkDownloadTelemetryOutcome::NOT_SENT_DOWNLOAD_CANCELLED);
@@ -114,8 +112,22 @@ void AndroidTelemetryService::OnDownloadUpdated(download::DownloadItem* item) {
   }
 }
 
+void AndroidTelemetryService::OnGetReportDone(
+    download::DownloadItem* item,
+    std::unique_ptr<ClientSafeBrowsingReportRequest> report) {
+  if (reports_in_progress_.contains(item)) {
+    reports_in_progress_.erase(item);
+    MaybeSendApkDownloadReport(
+        content::DownloadItemUtils::GetBrowserContext(item), std::move(report));
+    // No longer interested in this |DownloadItem| since the report has been
+    // sent so remove the observer.
+    item->RemoveObserver(this);
+  }
+}
+
 void AndroidTelemetryService::OnDownloadRemoved(download::DownloadItem* item) {
   referrer_chain_result_.erase(item);
+  reports_in_progress_.erase(item);
 }
 
 bool AndroidTelemetryService::CanSendPing(download::DownloadItem* item) {
@@ -207,10 +219,14 @@ void AndroidTelemetryService::FillReferrerChain(download::DownloadItem* item) {
                     std::move(data));
 }
 
-std::unique_ptr<ClientSafeBrowsingReportRequest>
-AndroidTelemetryService::GetReport(download::DownloadItem* item) {
+void AndroidTelemetryService::GetReport(
+    download::DownloadItem* item,
+    base::OnceCallback<void(std::unique_ptr<ClientSafeBrowsingReportRequest>)>
+        callback) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   DCHECK(item->IsDone());
+
+  reports_in_progress_.insert(item);
 
   std::unique_ptr<ClientSafeBrowsingReportRequest> report(
       new ClientSafeBrowsingReportRequest());
@@ -248,7 +264,32 @@ AndroidTelemetryService::GetReport(download::DownloadItem* item) {
   mutable_download_item_info->set_file_basename(
       item->GetTargetFilePath().BaseName().value());
 
-  return report;
+  if (base::FeatureList::IsEnabled(kGooglePlayProtectInApkTelemetry)) {
+    SafeBrowsingApiHandlerBridge::GetInstance().StartIsVerifyAppsEnabled(
+        base::BindOnce(&AndroidTelemetryService::IsVerifyAppsEnabled,
+                       weak_ptr_factory_.GetWeakPtr(), std::move(report),
+                       std::move(callback)));
+  } else {
+    IsVerifyAppsEnabled(std::move(report), std::move(callback),
+                        VerifyAppsEnabledResult::FAILED);
+  }
+}
+
+void AndroidTelemetryService::IsVerifyAppsEnabled(
+    std::unique_ptr<ClientSafeBrowsingReportRequest> report,
+    base::OnceCallback<void(std::unique_ptr<ClientSafeBrowsingReportRequest>)>
+        callback,
+    VerifyAppsEnabledResult result) {
+  base::UmaHistogramEnumeration(
+      "SBClientDownload.AndroidTelemetry.AppVerificationResult", result);
+
+  if (result == VerifyAppsEnabledResult::SUCCESS_ENABLED) {
+    report->mutable_client_properties()->set_app_verification_enabled(true);
+  } else if (result == VerifyAppsEnabledResult::SUCCESS_NOT_ENABLED) {
+    report->mutable_client_properties()->set_app_verification_enabled(false);
+  }
+
+  std::move(callback).Run(std::move(report));
 }
 
 void AndroidTelemetryService::MaybeSendApkDownloadReport(
--- chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.h
+++ chrome/browser/safe_browsing/telemetry/android/android_telemetry_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/telemetry/telemetry_service.h"
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/simple_download_manager_coordinator.h"
+#include "components/safe_browsing/android/safe_browsing_api_handler_util.h"
 #include "components/safe_browsing/content/browser/safe_browsing_navigation_observer_manager.h"
 #include "components/safe_browsing/core/common/proto/csd.pb.h"
 #include "content/public/browser/browser_context.h"
@@ -82,6 +83,9 @@ class AndroidTelemetryService
  private:
   friend class AndroidTelemetryServiceTest;
 
+  using OnGetReportDoneCallback = base::OnceCallback<void(
+      std::unique_ptr<ClientSafeBrowsingReportRequest>)>;
+
   enum class ApkDownloadTelemetryIncompleteReason {
     // |web_contents| was nullptr. This happens sometimes when downloads are
     // resumed but it's not clear exactly when.
@@ -113,9 +117,20 @@ class AndroidTelemetryService
   void FillReferrerChain(download::DownloadItem* item);
 
   // Sets the relevant fields in an instance of
-  // |ClientSafeBrowsingReportRequest| proto and returns that proto.
-  std::unique_ptr<ClientSafeBrowsingReportRequest> GetReport(
-      download::DownloadItem* item);
+  // |ClientSafeBrowsingReportRequest| proto and returns that proto
+  // asynchronously.
+  void GetReport(download::DownloadItem* item,
+                 OnGetReportDoneCallback callback);
+
+  // Callback when we know if app verification is enabled.
+  void IsVerifyAppsEnabled(
+      std::unique_ptr<ClientSafeBrowsingReportRequest> report,
+      OnGetReportDoneCallback callback,
+      VerifyAppsEnabledResult result);
+
+  // Callback when report generation is complete.
+  void OnGetReportDone(download::DownloadItem* item,
+                       std::unique_ptr<ClientSafeBrowsingReportRequest> report);
 
   // Sends |report| proto to the Safe Browsing backend. The report may not be
   // sent if the proto fails to serialize.
@@ -134,6 +149,9 @@ class AndroidTelemetryService
   // metrics only for downloads that send a ping, we persist the
   // outcome of referrer chain computation.
   std::map<download::DownloadItem*, ReferrerChainResult> referrer_chain_result_;
+
+  // The collection of `DownloadItem`s we're currently collecting reports for.
+  std::set<download::DownloadItem*> reports_in_progress_;
 
   base::WeakPtrFactory<AndroidTelemetryService> weak_ptr_factory_{this};
 };
