7e5c584c90dc655afc237123b8ed42f7fb84df0c
Tim Judkins <tjudkins@chromium.org>
2024-07-02 23:49:30 +0000

    [Extensions] Delete dashboardPrivate API
    This is only granted to the webstore hosted app and was used by the old
    Chrome Webstore site. Since this is not used by the new Webstore and
    all users on the most recent versions of Chrome should be directed to
    the new Webstore, we can remove this entirely now.
    
    Bug: 328494022
    Change-Id: I914a754b7643520e7d96b65c03f504487a67f496
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670656
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Tim <tjudkins@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322474}

--- chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
+++ chrome/browser/ui/toolbar/app_menu_model_interactive_uitest.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/common/chrome_features.h"
-#include "chrome/common/extensions/api/dashboard_private.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/interactive_test_utils.h"
