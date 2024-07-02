a76ce78fc1f856206b01b71ad0f8bad9bb69e619
Adam Rice <ricea@chromium.org>
2024-07-02 02:58:51 +0000

    Fix crash sending mail from chrome://net-export
    Clicking "send email" and immediately closing the site would crash on
    iOS due to a use of base::Unretained. Fix it.
    
    BUG=349653218
    
    Low-Coverage-Reason: TRIVIAL_CHANGE Minor bug fix to untested code.
    Change-Id: I52d6d1b9b6047a5bb81562aca6ce5281775e2132
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670141
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1321970}

--- ios/chrome/browser/webui/ui_bundled/net_export/net_export_ui.mm
+++ ios/chrome/browser/webui/ui_bundled/net_export/net_export_ui.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/functional/bind.h"
 #import "base/location.h"
 #import "base/memory/raw_ptr.h"
+#import "base/memory/weak_ptr.h"
 #import "base/scoped_observation.h"
 #import "base/strings/string_util.h"
 #import "base/values.h"
@@ -85,6 +86,8 @@ class NetExportMessageHandler
   base::ScopedObservation<net_log::NetExportFileWriter,
                           net_log::NetExportFileWriter::StateObserver>
       state_observation_manager_{this};
+
+  base::WeakPtrFactory<NetExportMessageHandler> weak_factory_{this};
 };
 
 NetExportMessageHandler::NetExportMessageHandler()
@@ -160,7 +163,7 @@ void NetExportMessageHandler::OnStopNetLog(const base::Value::List& list) {
 void NetExportMessageHandler::OnSendNetLog(const base::Value::List& list) {
   DCHECK_CURRENTLY_ON(web::WebThread::UI);
   file_writer_->GetFilePathToCompletedLog(base::BindOnce(
-      &NetExportMessageHandler::SendEmail, base::Unretained(this)));
+      &NetExportMessageHandler::SendEmail, weak_factory_.GetWeakPtr()));
 }
 
 void NetExportMessageHandler::OnNewState(const base::Value::Dict& state) {
