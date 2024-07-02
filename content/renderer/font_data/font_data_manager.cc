122ea9312d362a9541fc18fe2aa8e5cbccfdefbb
Kaylee Lubick <kjlubick@google.com>
2024-07-02 17:02:32 +0000

    Use gni file lists instead of src/ports files
    Requires http://review.skia.org/862177 and http://review.skia.org/862416
    to land/roll
    
    SkImageGenerator_skia.cpp and SkImageGenerator_none.cpp are currently empty files. Removing references to them here allows them to be
    removed from Skia.
    
    font_data_manager.cc uses a private API DWriteFontTypeface::MakeFromStream
    which possibly could be made public in a future CL, but for now is
    just being accepted as it was what had been previously working.
    
    Bug: b:256865569
    Change-Id: I777ba38a0822cb3c64b141c905ab7fac07861f7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598447
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322258}

--- content/renderer/font_data/font_data_manager.cc
+++ content/renderer/font_data/font_data_manager.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_features.h"
 #include "content/public/renderer/render_thread.h"
 #include "third_party/skia/include/ports/SkFontMgr_empty.h"
-#include "third_party/skia/src/ports/SkTypeface_win_dw.h"
+#include "third_party/skia/src/ports/SkTypeface_win_dw.h"  // nogncheck
 
 namespace font_data_service {
 
