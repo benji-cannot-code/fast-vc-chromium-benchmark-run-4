bf34dc95e4e8508daac476383e16e8468bab180a
Austin Sullivan <asully@chromium.org>
2024-07-02 18:50:52 +0000

    Remove the Model Loader API - part 2/n
    Deletes all flags related to the Model Loader API. The code these flags
    guarded was deleted in https://crrev.com/c/5666054
    
    Bug: 350407897
    Change-Id: I9361b000395ea076e4ff826286544ac9b32fe0cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669977
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Charlie Reis <creis@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322334}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1345,9 +1345,6 @@ extern const char kUsernameFirstFlowWithIntermediateValuesVotingDescription[];
 extern const char kCanvas2DLayersName[];
 extern const char kCanvas2DLayersDescription[];
 
-extern const char kEnableMachineLearningModelLoaderWebPlatformApiName[];
-extern const char kEnableMachineLearningModelLoaderWebPlatformApiDescription[];
-
 extern const char kWebMachineLearningNeuralNetworkName[];
 extern const char kWebMachineLearningNeuralNetworkDescription[];
 
