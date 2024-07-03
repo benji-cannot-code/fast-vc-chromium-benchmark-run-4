5dabc8fcffbc64b9e2ba5a6045dc41b1e75b3ff0
Benjamin Williams <bwwilliams@google.com>
2024-07-03 04:32:31 +0000

    [iOS] Conditionally create TableViewInfoButtonItem info button actions.
    This change modifies the creation of info button actions for
    TableViewInfoButtonItem. Instead of always creating actions, we now
    conditionally create them based on the visibility state of the info
    button. This prevents unnecessary actions from being created when the
    info button is hidden, improving performance and reducing potential
    issues, e.g. https://g-issues.chromium.org/issues/346978159.
    
    Bug: 346978159
    Change-Id: I5b5d3244276a67387ed03ef1840c24a959493f66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5671084
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322571}

--- ios/chrome/browser/shared/ui/table_view/cells/table_view_info_button_item.mm
+++ ios/chrome/browser/shared/ui/table_view/cells/table_view_info_button_item.mm
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (self.accessibilityHint) {
     cell.customizedAccessibilityHint = self.accessibilityHint;
   }
-  if (self.accessibilityDelegate) {
+  if (self.accessibilityDelegate && !self.infoButtonIsHidden) {
     cell.accessibilityCustomActions = [self createAccessibilityActions];
   }
   cell.isButtonSelectedForVoiceOver = self.accessibilityActivationPointOnButton;
