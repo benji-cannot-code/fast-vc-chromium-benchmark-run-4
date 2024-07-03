169b38ac9d25a9a927e77624eebf8de146ad1965
Adam Langley <agl@chromium.org>
2024-07-03 01:17:37 +0000

    webauthn: specify default triggering conditions for enclave on macOS.
    We won't have GPM passkey support on iOS by the time that we ship it for
    macOS, but many macOS users will use an iPhone and thus will be better
    served (for now) with iCloud Keychain.
    
    Thus this change attempts to figure out when it's reasonable to default
    to GPM on macOS, and when we should pick iCloud Keychain.
    
    GPM is a candidate for being the default create() destination when any
    of the following is true:
    
      * iCloud Drive is disabled. (Suggesting that the user isn't signed
        into iCloud.)
      * The user has denied Chrome iCloud Keychain enumeration permission in
        the past.
      * There's a non-Apple device syncing to the account.
      * There are GPM passkeys in the account.
    
    Bug: 40274370
    Change-Id: I8ab55b8272a4b4c9eb515b109b65bebcc13cd1ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5657014
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322512}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate_mac.h
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate_mac.h
@@ -6,9 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEBAUTHN_CHROME_AUTHENTICATOR_REQUEST_DELEGATE_MAC_H_
 #define CHROME_BROWSER_WEBAUTHN_CHROME_AUTHENTICATOR_REQUEST_DELEGATE_MAC_H_
 
+#include <memory>
+
 // IsICloudDriveEnabled returns true if iCloud Drive is available. This is used
 // as an approximation to "has iCloud Keychain enabled", which is what we would
 // like to know but cannot easily learn.
 bool IsICloudDriveEnabled();
+
+struct ScopedICloudDriveOverride {
+  virtual ~ScopedICloudDriveOverride() = 0;
+};
+
+// Override `IsICloudDriveEnabled` for testing purposes.
+std::unique_ptr<ScopedICloudDriveOverride> OverrideICloudDriveEnabled(
+    bool enabled);
 
 #endif  // CHROME_BROWSER_WEBAUTHN_CHROME_AUTHENTICATOR_REQUEST_DELEGATE_MAC_H_
--- device/fido/mac/icloud_keychain.h
+++ device/fido/mac/icloud_keychain.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DEVICE_FIDO_MAC_ICLOUD_KEYCHAIN_H_
 
 #include <memory>
+#include <optional>
+
 #include "base/component_export.h"
 
 namespace device {
@@ -34,6 +36,11 @@ COMPONENT_EXPORT(DEVICE_FIDO) bool IsSupported();
 // created synchronously after getting the `BrowserWindow` of a `WebContents`.)
 COMPONENT_EXPORT(DEVICE_FIDO)
 std::unique_ptr<FidoDiscoveryBase> NewDiscovery(uintptr_t ns_window);
+
+// Returns true if the user has granted passkey enumeration permission, false if
+// they have denied it, and `nullopt` if the user hasn't made a choice.
+COMPONENT_EXPORT(DEVICE_FIDO)
+std::optional<bool> HasPermission();
 
 }  // namespace fido::icloud_keychain
 }  // namespace device
