73cc509a08918d61fc5c0bd65d74f9c523953070
Shakti Sahu <shaktisahu@chromium.org>
2024-07-03 14:28:01 +0000

    [Tab Group Sync] Implement startup helper in native
    This CL adds startup helper implementation in native which
    does the following merge logic on startup
    1. Adds unsynced local groups to remote.
    2. Closes deleted groups from local tab model.
    3. Forces local groups to match their remote counterparts.
    4. Updates in-memory ID mappings of tabs.
    
    Bug: 350622883
    Change-Id: I5e3d0c5e372496f8201da2dca9443caef4fe8c6d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5671881
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322759}

--- components/saved_tab_groups/tab_group_sync_coordinator.h
+++ components/saved_tab_groups/tab_group_sync_coordinator.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/uuid.h"
 #include "components/saved_tab_groups/saved_tab_group.h"
+#include "components/saved_tab_groups/startup_helper.h"
 #include "components/saved_tab_groups/tab_group_sync_service.h"
 #include "components/saved_tab_groups/types.h"
 
@@ -54,6 +55,9 @@ class TabGroupSyncCoordinator final : public TabGroupSyncService::Observer {
 
   // The service which represents remote from the point of view of this class.
   const raw_ptr<TabGroupSyncService> service_;
+
+  // To help with the startup logic.
+  StartupHelper startup_helper_;
 };
 
 }  // namespace tab_groups
