c17ded4d2320b705846d5cb3528fe8df7a2f461d
dpapad <dpapad@chromium.org>
2024-07-03 04:39:21 +0000

    NewTabPage: Replace remaining <iron-icon> usages with <cr-icon>
    Bug: 40943652
    Change-Id: I785ad7d6d574c3036766134e86e2bbfc349c1e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5673759
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322574}

--- chrome/browser/resources/new_tab_page/modules/v2/module_header.ts
+++ chrome/browser/resources/new_tab_page/modules/v2/module_header.ts
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './icons.html.js';
 import 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
-import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
 
 import type {CrActionMenuElement} from 'chrome://resources/cr_elements/cr_action_menu/cr_action_menu.js';
 import {assert} from 'chrome://resources/js/assert.js';
