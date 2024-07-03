c9b5930843e423907d36185a91f1563595464328
Saifuddin Hitawala <hitawala@chromium.org>
2024-07-03 17:07:20 +0000

    [media] Return true for IsMultiPlaneFormatForSoftwareVideoEnabled
    UseMultiPlaneFormatForSoftwareVideo feature was last enabled in M126
    for Linux and ChromeOS and has gone to stable. This change updates
    the getter method to always return true and updates the
    GpuVideoAcceleratorFactoriesImpl.
    
    Bug: 332564976
    Change-Id: Ife09f0961d0c577994ac5b663e54f04deab16dcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676444
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322847}

--- media/base/media_switches.cc
+++ media/base/media_switches.cc
@@ -1848,11 +1848,7 @@ bool IsVideoCaptureAcceleratedJpegDecodingEnabled() {
 }
 
 bool IsMultiPlaneFormatForSoftwareVideoEnabled() {
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_APPLE)
   return true;
-#else
-  return base::FeatureList::IsEnabled(kUseMultiPlaneFormatForSoftwareVideo);
-#endif
 }
 
 bool IsWritePixelsYUVEnabled() {
