ef8db727040cbe272e8df757c4332dca068d1ce2
Scott Haseley <shaseley@chromium.org>
2024-07-03 21:37:27 +0000

    [blink scheduler] Minor refactoring in advance of new UseCase
    This CL refactors a few things to make the diffs simpler when the new
    UseCase and associated QueueTrait are added:
    
     - UseCase
       - Change kCount to kMaxValue and remove default case in switch
         statements
       - Add a helper for computing the RAILMode from UseCase to make
         UpdatePolicyLocked easier to read
       - Move UseCaseToString into use_case.cc as to be with related code.
       - Reorder the enum to group similar use cases (this is no longer
         associated with any UMAs)
     - MainThreadTaskQueue::QueueTraits
      - Initialize members in declaration and default the c'tor
      - Change operator== to be default (vs. member-wise ==)
    
    Bug: 350540984
    Change-Id: Ie66b5080c6bb537a48eeb538e164f331ab19b4e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663989
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323038}

--- third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
+++ third_party/blink/renderer/platform/scheduler/main_thread/main_thread_scheduler_impl.h
@@ -143,7 +143,6 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
     base::TimeDelta prioritize_compositing_after_delay_post_fcp;
   };
 
-  static const char* UseCaseToString(UseCase use_case);
   static const char* RAILModeToString(RAILMode rail_mode);
 
   explicit MainThreadSchedulerImpl(
@@ -546,6 +545,10 @@ class PLATFORM_EXPORT MainThreadSchedulerImpl
   UseCase ComputeCurrentUseCase(
       base::TimeTicks now,
       base::TimeDelta* expected_use_case_duration) const;
+
+  // Helper for computing the RAILMode based on the given UseCase and current
+  // scheduler state.
+  RAILMode ComputeCurrentRAILMode(UseCase) const;
 
   // An input event of some sort happened, the policy may need updating.
   void UpdateForInputEventOnCompositorThread(
