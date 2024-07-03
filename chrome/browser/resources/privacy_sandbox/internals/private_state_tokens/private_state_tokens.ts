b5119f67c6bb05de280596d6603212b8fd8e3f85
Aimen Moten <aimenmoten@google.com>
2024-07-03 19:07:06 +0000

    Sets up tests for PrivateStateTokensToolbarElement and PrivateStateTokensAppElement
    Bug: 348590926
    
    Change-Id: I94cce045d471b4da69ba9783448b28129fc22f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5661219
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Aimen Moten <aimenmoten@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322948}

--- chrome/browser/resources/privacy_sandbox/internals/private_state_tokens/private_state_tokens.ts
+++ chrome/browser/resources/privacy_sandbox/internals/private_state_tokens/private_state_tokens.ts
@@ -5,4 +5,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './app.js';
 
-export {PrivateStateTokensToolbarElement} from './toolbar.js';
+export type {PrivateStateTokensAppElement} from './app.js';
+export type {PrivateStateTokensToolbarElement} from './toolbar.js';
