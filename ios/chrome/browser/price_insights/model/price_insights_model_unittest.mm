40b2ca9912434435fc0be960bc2735846eb2d1f3
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-07-03 15:10:49 +0000

    [iOS cont. panel PI] Price Insights model uses FET for IPH impressions
    Price insights model now uses the FET for IPH impression management.
    
    Bug: b:347033997
    Change-Id: Ia3200fec529c8a62edc67f968b85d20b40eecdcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5639473
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322789}

--- ios/chrome/browser/price_insights/model/price_insights_model_unittest.mm
+++ ios/chrome/browser/price_insights/model/price_insights_model_unittest.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/commerce/core/commerce_feature_list.h"
 #import "components/commerce/core/commerce_types.h"
 #import "components/commerce/core/mock_shopping_service.h"
+#import "components/feature_engagement/public/feature_constants.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_item_configuration.h"
@@ -32,6 +33,8 @@ const char kTestUrl[] = "https://www.merchant.com/price_drop_product";
 const char kTestSecondUrl[] =
     "https://www.merchant.com/second_price_drop_product";
 std::string kTestTitle = "Product";
+std::string kPriceInsightsEntrypointUsedFETEvent =
+    "ios_contextual_panel_price_insights_entrypoint_used";
 
 }  // namespace
 
@@ -469,6 +472,10 @@ TEST_F(PriceInsightsModelTest, TestPriceBucketUnknownEmptyMessageLowRelevance) {
   EXPECT_EQ(ContextualPanelItemConfiguration::EntrypointImageType::SFSymbol,
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::low_relevance, config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is low, the entrypoint message is set to a
@@ -525,6 +532,10 @@ TEST_F(PriceInsightsModelTest, TestPriceBucketLowLowPriceMessageHighRelevance) {
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::high_relevance,
             config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is low, the entrypoint message is set to a
@@ -579,6 +590,10 @@ TEST_F(PriceInsightsModelTest, TestPriceBucketLowGoodDealMessageHighRelevance) {
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::high_relevance,
             config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is low, the entrypoint message is set to a
@@ -683,6 +698,10 @@ TEST_F(PriceInsightsModelTest,
   EXPECT_EQ(ContextualPanelItemConfiguration::EntrypointImageType::SFSymbol,
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::low_relevance, config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is high and the page is subscribed, the
@@ -732,6 +751,10 @@ TEST_F(PriceInsightsModelTest, TestPriceBucketHighSubscribedLowRelevance) {
   EXPECT_EQ(ContextualPanelItemConfiguration::EntrypointImageType::SFSymbol,
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::low_relevance, config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is high and the page can not be tracked, the
@@ -781,6 +804,10 @@ TEST_F(PriceInsightsModelTest,
   EXPECT_EQ(ContextualPanelItemConfiguration::EntrypointImageType::SFSymbol,
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::low_relevance, config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
 
 // Test that when the price bucket is high and the page is currently not
@@ -836,4 +863,8 @@ TEST_F(PriceInsightsModelTest, TestPriceBucketHighHighRelevance) {
             config->image_type);
   EXPECT_EQ(ContextualPanelItemConfiguration::high_relevance,
             config->relevance);
+  EXPECT_EQ(&feature_engagement::kIPHiOSContextualPanelPriceInsightsFeature,
+            config->iph_feature);
+  EXPECT_EQ(kPriceInsightsEntrypointUsedFETEvent,
+            config->iph_entrypoint_used_event_name);
 }
