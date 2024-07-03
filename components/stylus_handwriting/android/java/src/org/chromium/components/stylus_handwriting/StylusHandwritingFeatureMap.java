0c5f695b01a4bec26b885e84e954733eb47392c9
Doğancan Emek <dogancanemek@google.com>
2024-07-03 17:19:33 +0000

    Reducing Stylus TouchSlop
    Lowering the slop of handwriting initiation with stylus from 8dp to 2dp
    
    The dependency addition to Chrome and WebView is to make files from the native side depend on the newly created native code since before the change there was no native code depending on stylus_handwriting_feature_map.cc and stylus_handwriting_feature_map.h. Without the dependency additions, an "Excess Java files" error is seen while building.
    
    Bug: 317299999
    Change-Id: Ic5c8236989efa174ba6d5f1480195a850ef92647
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5412937
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Commit-Queue: Doğancan Emek <dogancanemek@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322854}

---
+++ components/stylus_handwriting/android/java/src/org/chromium/components/stylus_handwriting/StylusHandwritingFeatureMap.java
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.stylus_handwriting;
+
+import org.jni_zero.JNINamespace;
+import org.jni_zero.NativeMethods;
+
+import org.chromium.base.FeatureMap;
+
+/** Java accessor for state of Stylus Handwriting feature flags. */
+@JNINamespace("stylus_handwriting::android")
+public class StylusHandwritingFeatureMap extends FeatureMap {
+
+    public static final String USE_HANDWRITING_INITIATOR = "UseHandwritingInitiator";
+    private static final StylusHandwritingFeatureMap sInstance = new StylusHandwritingFeatureMap();
+
+    // Do not instantiate this class.
+    private StylusHandwritingFeatureMap() {
+        super();
+    }
+
+    /**
+     * @return the singleton StylusHandwritingFeatureMap.
+     */
+    public static StylusHandwritingFeatureMap getInstance() {
+        return sInstance;
+    }
+
+    /** Convenience method to call {@link #isEnabledInNative(String)} statically. */
+    public static boolean isEnabled(String featureName) {
+        return getInstance().isEnabledInNative(featureName);
+    }
+
+    @Override
+    protected long getNativeMap() {
+        return StylusHandwritingFeatureMapJni.get().getNativeMap();
+    }
+
+    @NativeMethods
+    public interface Natives {
+        long getNativeMap();
+    }
+}
