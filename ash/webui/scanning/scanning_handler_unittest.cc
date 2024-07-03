e687621b525a7f4261b71b3db55908aa4fa8baa4
Elly <ellyjones@chromium.org>
2024-07-03 23:46:20 +0000

    ui: remove some subclasses of SelectFilePolicy
    These subclasses have the same behavior as passing a null
    SelectFilePolicy, so they are not needed.
    
    Bug: 340178601
    Change-Id: I258dcb5aba777189940773f88862928aa5c1f46e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677107
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323101}

--- ash/webui/scanning/scanning_handler_unittest.cc
+++ ash/webui/scanning/scanning_handler_unittest.cc
@@ -39,14 +39,6 @@ constexpr char kTestFilePath[] = "/test/file/path";
 
 }  // namespace
 
-class TestSelectFilePolicy : public ui::SelectFilePolicy {
- public:
-  TestSelectFilePolicy& operator=(const TestSelectFilePolicy&) = delete;
-
-  bool CanOpenSelectFileDialog() override { return true; }
-  void SelectFileDenied() override {}
-};
-
 // A test ui::SelectFileDialog.
 class TestSelectFileDialog : public ui::SelectFileDialog {
  public:
@@ -123,7 +115,7 @@ class FakeScanningAppDelegate : public ScanningAppDelegate {
 
   std::unique_ptr<ui::SelectFilePolicy> CreateChromeSelectFilePolicy()
       override {
-    return std::make_unique<TestSelectFilePolicy>();
+    return nullptr;
   }
 
   std::string GetBaseNameFromPath(const base::FilePath& path) override {
