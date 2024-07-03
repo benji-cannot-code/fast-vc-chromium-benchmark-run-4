47c4548e8c5cb32aa8b0ee29788ba826e2f85898
Kenichi Ishibashi <bashi@chromium.org>
2024-07-03 05:19:21 +0000

    Add HttpStreamPool::Group and HttpTextBasedStreamHandle
    HttpStreamPool::Group maintains active and idle text-based HTTP streams
    that don't use proxies.
    
    An HttpTextBasedStreamHandle is a StreamSocketHandle that is
    associated with an HttpStreamPool::Group.
    
    This CL only implements an essential parts of these classes. See the
    design doc [1] for the overall plan.
    
    [1] https://docs.google.com/document/d/10zr0g6E7RNUK224DTMC89gvZrwJtCIH4Lam-ZW5q0Kw/edit?usp=sharing
    
    Bug: 346835898
    Change-Id: I3ec7839f43183ae1e04bab8623dfe90aea4560de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672141
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322582}

---
+++ net/http/http_text_based_stream_handle.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/http/http_text_based_stream_handle.h"
+
+#include <memory>
+
+#include "net/http/http_stream_pool_group.h"
+#include "net/socket/stream_socket.h"
+
+namespace net {
+
+HttpTextBasedStreamHandle::HttpTextBasedStreamHandle(
+    HttpStreamPool::Group* group,
+    std::unique_ptr<StreamSocket> socket,
+    int64_t generation)
+    : group_(group), generation_(generation) {
+  CHECK(group);
+  CHECK(socket);
+
+  // Always considered initialized.
+  SetSocket(std::move(socket));
+  set_is_initialized(true);
+}
+
+HttpTextBasedStreamHandle::~HttpTextBasedStreamHandle() {
+  Reset();
+}
+
+void HttpTextBasedStreamHandle::Reset() {
+  if (socket()) {
+    group_->ReleaseStreamSocket(PassSocket(), generation_);
+  }
+}
+
+}  // namespace net
