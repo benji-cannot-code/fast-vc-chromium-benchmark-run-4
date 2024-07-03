546756ecf7a22fc3d8106a870017ef1d7517ee0c
Bhuvana Betini <bbetini@google.com>
2024-07-03 00:11:52 +0000

    Implements Chrome Flags for Notification Width Increase
    Bug: 343768137
    Change-Id: I966f03345f338d6f95a7e214c902ff4219818736
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666819
    Commit-Queue: Bhuvana Betini <bbetini@google.com>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322486}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4164,6 +4164,9 @@ extern const char kMultiCalendarSupportDescription[];
 extern const char kMultiZoneRgbKeyboardName[];
 extern const char kMultiZoneRgbKeyboardDescription[];
 
+extern const char kNotificationWidthIncreaseName[];
+extern const char kNotificationWidthIncreaseDescription[];
+
 extern const char kEnableNearbyBleV2Name[];
 extern const char kEnableNearbyBleV2Description[];
 
