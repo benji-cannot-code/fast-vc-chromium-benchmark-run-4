ed12dfdc6d43b45a0fb96cfca52e05b0bc9d786b
Kalvin Lee <kdlee@chromium.org>
2024-07-03 06:56:45 +0000

    Revert "WebNN: Handle WebNN context lost error"
    This reverts commit a240af8bb2c11c08a6649388afdd4896818bf336.
    
    Reason for revert: `blink_wpt_tests` fail on "Win11 Tests x64"
    
    Original change's description:
    > WebNN: Handle WebNN context lost error
    >
    > This CL is to handle the WebNN context lost error and has implemented
    > for DirectML backend.
    >
    > 1. Defined `MLContextLostInfo` dictionary in the WebIDL to expose the
    > context lost information.
    > 2. Defined `Promise<MLContextLostInfo> lost` of `MLContext` interface
    > in the WebIDL, when the lost error is captured, the promise will be
    > resolved with the `MLContextLostInfo`.
    > 3. Defined `WebNNContextClient` mojom interface and it's method
    > `OnLost(string message)` to send the context lost message to
    > the renderer process when the `MLContext` object lost error in the
    > GPU process is captured. The renderer process will override this
    > method to handle the lost error.
    > 4. Implemented the context lost handling feature in DirectML backend.
    >
    > Bug: 40281640, 41492165
    > Change-Id: I16775ac8cdbd508e04025c22740d13f01e591b0d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5602134
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1322573}
    
    Bug: 40281640, 41492165
    Change-Id: Iae3d0047eb8c89e76d4d94044598f6f95bc020c9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669861
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322611}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -204,9 +204,6 @@ void WebNNContextProviderImpl::CreateWebNNContext(
   mojo::PendingRemote<mojom::WebNNContext> remote;
   auto receiver = remote.InitWithNewPipeAndPassReceiver();
 
-  mojo::PendingReceiver<mojom::WebNNContextClient> client_receiver;
-  auto client_remote = client_receiver.InitWithNewPipeAndPassRemote();
-
 #if BUILDFLAG(IS_WIN)
   if (ShouldCreateDmlContext(*options)) {
     DCHECK(gpu_feature_info_.IsInitialized());
@@ -253,9 +250,9 @@ void WebNNContextProviderImpl::CreateWebNNContext(
       return;
     }
 
-    context_impl = new dml::ContextImplDml(
-        std::move(adapter), std::move(receiver), std::move(client_remote), this,
-        std::move(command_recorder), gpu_feature_info_);
+    context_impl =
+        new dml::ContextImplDml(std::move(adapter), std::move(receiver), this,
+                                std::move(command_recorder), gpu_feature_info_);
   }
 #endif  // BUILDFLAG(IS_WIN)
 
@@ -263,8 +260,7 @@ void WebNNContextProviderImpl::CreateWebNNContext(
   // TODO: crbug.com/325612086 - Consider using supporting older Macs either
   // with TFLite or a more restrictive implementation on CoreML.
   if (__builtin_available(macOS 14, *)) {
-    context_impl = new coreml::ContextImplCoreml(std::move(receiver),
-                                                 std::move(client_remote), this,
+    context_impl = new coreml::ContextImplCoreml(std::move(receiver), this,
                                                  std::move(options));
   }
 #endif  // BUILDFLAG(IS_MAC)
@@ -273,11 +269,9 @@ void WebNNContextProviderImpl::CreateWebNNContext(
   if (!context_impl) {
 #if BUILDFLAG(IS_CHROMEOS)
     // TODO: crbug.com/41486052 - Create the TFLite context using `options`.
-    context_impl = new tflite::ContextImplCrOS(std::move(receiver),
-                                               std::move(client_remote), this);
+    context_impl = new tflite::ContextImplCrOS(std::move(receiver), this);
 #else
-    context_impl = new tflite::ContextImplTflite(std::move(receiver),
-                                                 std::move(client_remote), this,
+    context_impl = new tflite::ContextImplTflite(std::move(receiver), this,
                                                  std::move(options));
 #endif  // BUILDFLAG(IS_CHROMEOS)
   }
@@ -296,8 +290,7 @@ void WebNNContextProviderImpl::CreateWebNNContext(
   impls_.push_back(base::WrapUnique<WebNNContextImpl>(context_impl));
 
   auto success = mojom::CreateContextSuccess::New(
-      std::move(remote), std::move(client_receiver),
-      std::move(context_properties));
+      std::move(remote), std::move(context_properties));
   std::move(callback).Run(
       mojom::CreateContextResult::NewSuccess(std::move(success)));
 }
--- third_party/blink/renderer/modules/ml/ml_context.h
+++ third_party/blink/renderer/modules/ml/ml_context.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/public/mojom/webnn_context_provider.mojom-blink.h"
 #include "services/webnn/public/mojom/webnn_graph.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
-#include "third_party/blink/renderer/bindings/core/v8/script_promise_property.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_device_preference.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_device_type.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_model_format.h"
@@ -26,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/member.h"
 #include "third_party/blink/renderer/platform/heap/visitor.h"
-#include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
 
 namespace blink {
@@ -35,12 +33,9 @@ class ExecutionContext;
 class MLBuffer;
 class MLBufferDescriptor;
 class MLComputeResult;
-class MLContextLostInfo;
 class MLOpSupportLimits;
 
-class MODULES_EXPORT MLContext
-    : public ScriptWrappable,
-      public webnn::mojom::blink::WebNNContextClient {
+class MODULES_EXPORT MLContext : public ScriptWrappable {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
@@ -69,8 +64,6 @@ class MODULES_EXPORT MLContext
   void Trace(Visitor* visitor) const override;
 
   // IDL interface:
-  ScriptPromise<MLContextLostInfo> lost(ScriptState* script_state);
-
   ScriptPromise<MLComputeResult> compute(ScriptState* script_state,
                                          MLGraph* graph,
                                          const MLNamedArrayBufferViews& inputs,
@@ -135,14 +128,6 @@ class MODULES_EXPORT MLContext
   const MLOpSupportLimits* opSupportLimits(ScriptState* script_state);
 
  private:
-  using LostProperty = ScriptPromiseProperty<MLContextLostInfo, IDLUndefined>;
-
-  // Closes the `context_remote_` and `context_client_receiver_` pipes
-  // because the context has been lost.
-  void OnLost(const String& message) override;
-
-  void OnDisconnected();
-
   // Validate and write ArrayBuffer data to hardware accelerated OS
   // machine learning buffers in the WebNN Service.
   // `src_data` is the source span of the array buffer data.
@@ -163,13 +148,9 @@ class MODULES_EXPORT MLContext
   V8MLModelFormat model_format_;
   unsigned int num_threads_;
 
-  Member<LostProperty> lost_property_;
-
   // The `WebNNContext` is a initialized context that can be used by the
   // hardware accelerated OS machine learning API.
-  HeapMojoRemote<webnn::mojom::blink::WebNNContext> context_remote_;
-  HeapMojoReceiver<webnn::mojom::blink::WebNNContextClient, MLContext>
-      context_client_receiver_;
+  HeapMojoRemote<webnn::mojom::blink::WebNNContext> remote_context_;
   webnn::ContextProperties properties_;
 };
 
