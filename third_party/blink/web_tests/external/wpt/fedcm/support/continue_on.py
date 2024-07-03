ffb3034698df404e580985114a76a1b4ba695a9f
Christian Biesinger <cbiesinger@chromium.org>
2024-07-03 20:22:38 +0000

    [FedCM] Share more code between OnResolve and CompleteTokenRequest
    In particular, this makes us call SetRequiresUserMediation also for
    OnResolve.
    
    Bug: 349994201
    Change-Id: I0d0f4d374e9ab601eb603d714930074923789218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5673234
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322994}

--- third_party/blink/web_tests/external/wpt/fedcm/support/continue_on.py
+++ third_party/blink/web_tests/external/wpt/fedcm/support/continue_on.py
@@ -13,5 +13,7 @@ def main(request, response):
 
   account = request.POST.get(b"account_id").decode("utf-8")
   nonce = request.POST.get(b"nonce").decode("utf-8")
+  if nonce == "token":
+    return "{\"token\": \"account=%s\"}" % (account)
   return "{\"continue_on\": \"resolve.html?selected=%s&%s\"}" % (account, nonce)
 
