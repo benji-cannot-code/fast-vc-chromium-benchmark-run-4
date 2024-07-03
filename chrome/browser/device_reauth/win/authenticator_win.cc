b6d1238dfd2ca2c9b483546694bd3466386eff5d
Karol Sygiet <sygiet@google.com>
2024-07-03 13:47:49 +0000

    Cleanup windows authentication callbacks
    This CL removes posting functions handling authentication results.
    Since they were posted on UI thread, they can simply be called directly.
    
    Bug: 349728186
    Change-Id: I09248327c45349e9b0548904ff1d9ef928c4a173
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676104
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1322745}

--- chrome/browser/device_reauth/win/authenticator_win.cc
+++ chrome/browser/device_reauth/win/authenticator_win.cc
@@ -195,9 +195,9 @@ void AuthenticateWithLegacyApi(const std::u16string& message,
       std::move(result_callback));
 }
 
-void ReturnAuthenticationValue(base::OnceCallback<void(bool)> callback,
-                               UserConsentVerificationResult result,
-                               const std::u16string& message) {
+void OnAuthenticationReceived(base::OnceCallback<void(bool)> callback,
+                              const std::u16string& message,
+                              UserConsentVerificationResult result) {
   AuthenticationResultStatusWin authentication_result =
       ConvertUserConsentVerificationResult(result);
   RecordWindowsHelloAuthenticationResult(authentication_result);
@@ -230,14 +230,6 @@ void ReturnAuthenticationValue(base::OnceCallback<void(bool)> callback,
   }
 }
 
-void OnAuthenticationReceived(base::OnceCallback<void(bool)> callback,
-                              const std::u16string& message,
-                              UserConsentVerificationResult result) {
-  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, base::BindOnce(&ReturnAuthenticationValue, std::move(callback),
-                                result, message));
-}
-
 void OnAuthenticationAsyncOpFail(
     base::OnceCallback<void(bool)> callback,
     const std::u16string& message,
@@ -246,9 +238,7 @@ void OnAuthenticationAsyncOpFail(
       AuthenticationResultStatusWin::kAsyncOperationFailed);
   RecordAuthenticationAsyncOpFailureReson(hr);
 
-  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE,
-      base::BindOnce(&AuthenticateWithLegacyApi, message, std::move(callback)));
+  AuthenticateWithLegacyApi(message, std::move(callback));
 }
 
 // TODO(b/349728186): Cleanup after Win11 solution is launched.
