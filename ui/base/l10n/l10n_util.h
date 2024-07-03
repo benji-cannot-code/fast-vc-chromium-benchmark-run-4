1a25ede9fc6abceb4aec00f83623358f2eee221a
Tao Xiong <taox4@illinois.edu>
2024-07-03 19:02:02 +0000

    [Language] Require that Accept-Languages be on kAcceptLanguageList
    Currently, languages on the user's accept language list that are not on
    the list of possible accept languages will never be shown in the
    language settings, however, these stealth languages are sent as part of
    the Accept-Language header. This causes confusion.
    
    The reason languages displayed in settings must be on the possible
    accept language list is because we currently only get language names
    (i.e. "af" => "Afrikaans") for languages on that list.
    
    This CL changes `LanguagePrefs::SetUserSelectedLanguagesList` so that
    languages on the list must be on `kAcceptLanguageList`. Existing tests
    that broke this new assumption are updated.
    
    This CL also renames `l10n_util::IsLanguageAccepted` to the more
    descriptive `l10n_util::IsAcceptLanguageDisplayable`.
    
    
    Bug: 40175592, 40758624
    Change-Id: I472e7bcd344eac05d9f78218427581ba08a6327f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667975
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Tao Xiong <taox4@illinois.edu>
    Cr-Commit-Position: refs/heads/main@{#1322943}

--- ui/base/l10n/l10n_util.h
+++ ui/base/l10n/l10n_util.h
@@ -13,9 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <string>
+#include <string_view>
 #include <vector>
 
 #include "base/component_export.h"
+#include "base/containers/span.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_APPLE)
@@ -279,11 +281,21 @@ void GetAcceptLanguagesForLocale(const std::string& display_locale,
 COMPONENT_EXPORT(UI_BASE)
 void GetAcceptLanguages(std::vector<std::string>* locale_codes);
 
-// Returns true if |locale| is in a predefined AcceptLanguageList and
+// Returns true if |locale| is in a predefined |AcceptLanguageList|.
+COMPONENT_EXPORT(UI_BASE)
+bool IsPossibleAcceptLanguage(std::string_view locale);
+
+// Returns true if |locale| is in a predefined |AcceptLanguageList| and
 // a display name for the |locale| is available in the locale |display_locale|.
 COMPONENT_EXPORT(UI_BASE)
-bool IsLanguageAccepted(const std::string& display_locale,
-                        const std::string& locale);
+bool IsAcceptLanguageDisplayable(const std::string& display_locale,
+                                 const std::string& locale);
+
+// Filters the input vector of languages. Returns only those in the
+// |AcceptLanguageList|.
+COMPONENT_EXPORT(UI_BASE)
+std::vector<std::string> KeepAcceptedLanguages(
+    base::span<const std::string> languages);
 
 // Returns the preferred size of the contents view of a window based on
 // designer given constraints which might dependent on the language used.
