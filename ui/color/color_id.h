4c92d049ef373ea59b11d674af6d184d1e253abf
Benjamin Keen <bkeen@google.com>
2024-07-03 19:54:58 +0000

    Replace SysColorMixer GetResultingPaintColor usage with AlphaBlend
    This is a follow up to pkasting's suggestion. This change improves
    readability and performance.
    
    Bug: 349640594
    Change-Id: I2da6146972f03cf55aabbf7076043c3cb97b047d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665757
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1322980}

--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -154,6 +154,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorSysSurface3) \
   E_CPONLY(kColorSysSurface4) \
   E_CPONLY(kColorSysSurface5) \
+  E_CPONLY(kColorSysSurfaceNumberedForeground) \
   /* General. */ \
   E_CPONLY(kColorSysOnSurfaceSecondary) \
   E_CPONLY(kColorSysOnSurfaceSubtle) \
