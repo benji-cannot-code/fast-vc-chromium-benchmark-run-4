cb4f8cf50725cea6d3a017b3cb22603457acb003
Xi Han <hanxi@google.com>
2024-07-04 18:46:08 +0000

    [Start] Clean up ToolbarManager and ToolbarLayout|Phone.
    In this CL, the followings are cleaned up:
    1) ToolbarManager: anything related to Start surface;
    2) ToolbarPhone: both |mIsShowingStartSurfaceHomepage| and
       |mIsShowingStartSurfaceTabSwitcher| are always false. Remove both
       and usages.
    3) ToolbarPhone#isLocationBarShownInGeneralNtpOrStartSurface() is
       replaced by isLocationBarShownInGeneralNtp(), since "surface
       polish" has enabled by default.
    4) ToolbarLayout#onStartSurfaceStateChanged(): remove this orphan
       method.
    
    Bug: 344651414, 40221888, 40834987
    Change-Id: I089d6c8c5390477d305ea377bd2b6c0e97034ff0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675441
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1323439}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuCoordinator.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/TabSwitcherActionMenuCoordinator.java
@@ -76,9 +76,6 @@ public class TabSwitcherActionMenuCoordinator {
                     (ListMenuButton) view,
                     menu.buildMenuItems(),
                     (id) -> {
-                        // TODO(crbug.com/40834987): Refactor to allow subclasses to record
-                        // different
-                        // user actions and update StartSurfaceTabSwitcherActionMenuCoordinator.
                         recordUserActions(id);
                         onItemClicked.onResult(id);
                     });
