1e276074d2e985947e562d848067cc42101f4ca5
Karol Sygiet <sygiet@google.com>
2024-07-04 15:09:28 +0000

    Read the pref marking users that deleted undecryptable passwords
    This CL introduces an logic to read the pref responsible for marking
    kClearUndecryptablePasswords experiment users during BuiltInBackend
    init. This is needed to ensure that they reach the flag check to keep
    them active n the experiment.
    
    Bug: 40286735
    Change-Id: Ie9d4653019d4939eb5bdbc1fa3fa5cf67c9bbb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678209
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323358}

--- components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
+++ components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
@@ -30,6 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #endif  // !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
 
+#if !BUILDFLAG(IS_ANDROID)
+#include "components/password_manager/core/browser/features/password_features.h"
+#endif  // !BUILDFLAG(IS_ANDROID)
+
 namespace password_manager {
 
 namespace {
@@ -134,6 +138,15 @@ void PasswordStoreBuiltInBackend::InitBackend(
   affiliated_match_helper_ = affiliated_match_helper;
 
 #if !BUILDFLAG(IS_ANDROID)
+  // To ensure that groups of the kClearUndecryptablePasswords will stay
+  // balanced, after the cleanup is done an additional flag check is needed.
+  // Users won't reach the flag the normal way since the LoginDB is working
+  // correctly and thus flag is never reached.
+  // TODO(b/40286735): Remove after this feature is launched.
+  if (pref_service_->GetBoolean(prefs::kClearingUndecryptablePasswords)) {
+    base::FeatureList::IsEnabled(features::kClearUndecryptablePasswords);
+  }
+
   auto clearing_undecryptable_passwords_cb = base::BindPostTaskToCurrentDefault(
       base::BindRepeating(&PasswordStoreBuiltInBackend::
                               SetClearingUndecryptablePasswordsIsEnabledPref,
