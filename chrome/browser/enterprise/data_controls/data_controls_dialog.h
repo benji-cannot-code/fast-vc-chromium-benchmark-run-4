557fb6e46801a681cd128514fcc5b134733305aa
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-07-04 16:57:57 +0000

    Handle tab closing/navigating when a DataControlsDialog is showing
    The code added by this CL avoid crashes when a user ignores the Data
    Controls dialog and closes the tab or navigates to a new URL.
    
    Change-Id: I7b8288725fe416b693609f8cd239199b4794ab4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663718
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323404}

--- chrome/browser/enterprise/data_controls/data_controls_dialog.h
+++ chrome/browser/enterprise/data_controls/data_controls_dialog.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "components/enterprise/data_controls/rule.h"
+#include "content/public/browser/web_contents_observer.h"
 #include "ui/views/window/dialog_delegate.h"
 
 namespace content {
@@ -43,7 +44,8 @@ namespace data_controls {
 // * The "E" box represents the enterprise logo that looks like a building.
 // * The "Cancel"/"Proceed" choice is only available for warnings, blocked
 //   actions only have a "Close" button.
-class DataControlsDialog : public views::DialogDelegate {
+class DataControlsDialog : public views::DialogDelegate,
+                           public content::WebContentsObserver {
  public:
   // Represents the type of dialog, based on the action that triggered it and
   // severity of the triggered rule. This will change the strings in the dialog,
@@ -92,6 +94,10 @@ class DataControlsDialog : public views::DialogDelegate {
   bool ShouldShowCloseButton() const override;
   void OnWidgetInitialized() override;
 
+  // content::WebContentsObserver:
+  void WebContentsDestroyed() override;
+  void PrimaryPageChanged(content::Page& page) override;
+
   Type type() const;
 
  private:
@@ -110,7 +116,6 @@ class DataControlsDialog : public views::DialogDelegate {
   std::unique_ptr<views::Label> CreateMessage() const;
 
   Type type_;
-  raw_ptr<content::WebContents> web_contents_ = nullptr;
   raw_ptr<views::BoxLayoutView> contents_view_ = nullptr;
 
   // Called when the dialog closes, with `true` in the case of a bypassed
