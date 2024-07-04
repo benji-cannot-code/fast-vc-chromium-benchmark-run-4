2084b6355ed39d8e21a04579e6b6b61326ebddf9
Koji Ishii <kojii@chromium.org>
2024-07-04 22:19:40 +0000

    Reland "[text-decorations] Fix a crash for `::first-line`"
    This is a reland of commit c9d2271396f178e2c0747f2ef2ac727d8a02f9d1
    
    The original CL hits a `DCHECK` in the
    `linux-autofill-captured-sites-rel` bots. This CL changes to
    push 2 decoration boxes only when the conditions are known,
    and to fall back to 1 decoration box in other cases.
    
    This CL also puts the changes under a runtime flag.
    
    Original change's description:
    > [text-decorations] Fix a crash for `::first-line`
    >
    > This patch fixes a crash when `text-decoration` is applied to
    > `::first-line`.
    >
    > Fixed: 345562934
    > Change-Id: If26564ca3dcb913236771ef4a634928485b68eca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677308
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Auto-Submit: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Koji Ishii <kojii@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1323270}
    
    Change-Id: Ifb27df1bfd8e9d0a9dc37a1f18d27a6c9488cc29
    Fixed: 345562934
    Cq-Include-Trybots: luci.chrome.try:linux-autofill-captured-sites-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678276
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323484}

--- third_party/blink/renderer/core/paint/decorating_box.h
+++ third_party/blink/renderer/core/paint/decorating_box.h
@@ -27,7 +27,6 @@ class CORE_EXPORT DecoratingBox {
         style_(&style),
         decorations_(decorations ? decorations
                                  : &style.AppliedTextDecorations()) {
-    DCHECK_EQ(&style.AppliedTextDecorations(), decorations_);
   }
   DecoratingBox(const FragmentItem& item,
                 const ComputedStyle& style,
