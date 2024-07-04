5b747e1ed3e8991bc7c21ea6edbc08b060869f35
Daniel Cheng <dcheng@chromium.org>
2024-07-04 22:44:54 +0000

    Remove template_util.h include from //base/ranges/ranges.h
    As of https://crrev.com/1323365, ranges.h no longer needs this include.
    
    Change-Id: I2da708c0ea86ab89f919ee48dbfe8172cf3148e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678034
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323490}

--- base/ranges/ranges.h
+++ base/ranges/ranges.h
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <utility>
 
-// TODO(dcheng): Remove this in a followup.
-#include "base/template_util.h"
-
 namespace base::ranges {
 
 // Implementation of C++20's std::ranges::iterator_t.
