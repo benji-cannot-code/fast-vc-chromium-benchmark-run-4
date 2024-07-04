f3227ab069e29aa03fdabc0dc69a19a895e12c56
Gayane Petrosyan <gayane@google.com>
2024-07-04 18:17:53 +0000

    [iOS][WhatsNew] Add green color to background color choices
    Bug: crbug.com/351178117
    Change-Id: I4858a9bb786a68295e2b70f74478f6a09f41f037
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679142
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Auto-Submit: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323431}

--- ios/chrome/browser/ui/whats_new/data_source/whats_new_data_source.mm
+++ ios/chrome/browser/ui/whats_new/data_source/whats_new_data_source.mm
@@ -58,6 +58,8 @@ UIColor* GenerateColor(NSString* color) {
     return [UIColor colorNamed:kGrey800Color];
   } else if ([color isEqualToString:@"purple"]) {
     return [UIColor colorNamed:kPurple500Color];
+  } else if ([color isEqualToString:@"green"]) {
+    return [UIColor colorNamed:kGreen500Color];
   } else {
     return nil;
   }
