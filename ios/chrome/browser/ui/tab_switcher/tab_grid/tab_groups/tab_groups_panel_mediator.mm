60557df4a2632ce08a9a434b1fdaa26ee578c148
Louis Romero <lpromero@google.com>
2024-07-04 09:22:24 +0000

    Add empty state view to Tab Groups panel
    This CL adds an empty state view, similar to the other panels when they
    are empty.
    
    Screenshot: https://screenshot.googleplex.com/BnJfFygbvgJhFZt
    Video: https://drive.google.com/file/d/1WJvTtEhwvo7MdBAc3ICsDm-ePncSgEwY
    
    Fixed: 349788953
    Change-Id: Ie15983d36b6bece756899c286bce99c7cc01d8b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676884
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323248}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_panel_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_panel_mediator.mm
@@ -140,9 +140,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       item11, item12
     ];
   } else {
-    TabGroupsPanelItem* item = [[TabGroupsPanelItem alloc] init];
-    item.title = @"Vacation";
-    return @[ item ];
+    return @[];
   }
 }
 
