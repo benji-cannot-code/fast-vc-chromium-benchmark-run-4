6461875bb5057734825360347174171960b0e7eb
Zelin Liu <zelin@chromium.org>
2024-07-04 02:10:31 +0000

    controlledframe: Add WebHid permissions browser test
    Also adds a check for embedder's permissions policy.
    
    Bug: b/349853600
    Change-Id: I22696a640176755d163958049dae3debc11d815a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669650
    Reviewed-by: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Commit-Queue: Zelin Liu <zelin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323139}

--- chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.h
+++ chrome/browser/guest_view/web_view/chrome_web_view_permission_helper_delegate.h
@@ -89,6 +89,7 @@ class ChromeWebViewPermissionHelperDelegate
       const std::string& user_input);
 
   void OnHidPermissionResponse(base::OnceCallback<void(bool)> callback,
+                               const GURL& requesting_frame_url,
                                bool allow,
                                const std::string& user_input);
 
