7e8c8798dae773375dda38352208e3368bc6f491
Rune Lillesveen <futhark@chromium.org>
2024-07-04 19:25:39 +0000

    [@container] Update snapped state for snapchanging
    The snapped state would otherwise be stuck while the scroll operation
    happened. Need to keep the update on snapchanged since snapchanging is
    not covering that.
    
    TEST=wpt_internal/css/css-conditional/container-queries/
    
    Bug: 40279568
    Change-Id: I1fedb836a7b732319f0462c0d8a8f1b6f5fcfb7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676896
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323451}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -3240,6 +3240,9 @@ void PaintLayerScrollableArea::
   if (!container_data) {
     return;
   }
+
+  CreateAndSetSnappedQueryScrollSnapshotIfNeeded(new_target_ids);
+
   auto& rare_data = EnsureRareData();
   bool scrollsnapchanging =
       (rare_data.scrollsnapchanging_target_ids_
