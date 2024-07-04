08184b9ce4efa5063400e18b80670397799ea11b
Hiroki Nakagawa <nhiroki@chromium.org>
2024-07-04 01:10:04 +0000

    SearchPrefetch: Remove indirect SetLoaderDestructionCallbackForTesting() calls
    SetLoaderDestructionCallbackForTesting() in
    search_preload_unified_browsertest.cc calls
    SearchPrefetchService::SetLoaderDestructionCallbackForTesting(), but
    test cases can directly the function on SearchPrefetchService. This CL
    removes the indirect calls for code simplification.
    
    Bug: n/a
    Change-Id: I4aeb8c5b70ae82030b6cc6fc14cd1937f98a00d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675791
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323117}

--- chrome/browser/preloading/prefetch/search_prefetch/search_preload_unified_browsertest.cc
+++ chrome/browser/preloading/prefetch/search_prefetch/search_preload_unified_browsertest.cc
@@ -334,14 +334,6 @@ class SearchPreloadUnifiedBrowserTest : public PlatformBrowserTest,
     }
   }
 
-  void SetLoaderDestructionCallbackForTesting(
-      const GURL& canonical_search_url,
-      base::OnceClosure streaming_url_loader_destruction_callback) {
-    search_prefetch_service()->SetLoaderDestructionCallbackForTesting(
-        canonical_search_url,
-        std::move(streaming_url_loader_destruction_callback));
-  }
-
   // `WaitEvent::kLoadStopped` is the default value for a
   // TestNavigationObserver. Pass another event type to not wait until it
   // finishes loading.
@@ -1756,7 +1748,7 @@ IN_PROC_BROWSER_TEST_F(SearchPreloadUnifiedFallbackBrowserTest,
   EXPECT_TRUE(prefetch_status.has_value());
 
   base::RunLoop run_loop;
-  SetLoaderDestructionCallbackForTesting(
+  search_prefetch_service()->SetLoaderDestructionCallbackForTesting(
       GetCanonicalSearchURL(expected_prerender_url), run_loop.QuitClosure());
   ASSERT_TRUE(
       content::NavigateToURL(GetActiveWebContents(), expected_prerender_url));
@@ -1817,7 +1809,7 @@ IN_PROC_BROWSER_TEST_F(SearchPreloadUnifiedFallbackBrowserTest,
   content::test::PrerenderHostObserver prerender_observer(
       *GetActiveWebContents(), expected_prerender_url);
   base::RunLoop run_loop;
-  SetLoaderDestructionCallbackForTesting(
+  search_prefetch_service()->SetLoaderDestructionCallbackForTesting(
       GetCanonicalSearchURL(expected_prerender_url), run_loop.QuitClosure());
   NavigateToPrerenderedResult(expected_prerender_url);
   run_loop.Run();
@@ -2202,7 +2194,7 @@ IN_PROC_BROWSER_TEST_F(SearchPreloadUnifiedFallbackBrowserTest,
     content::test::PrerenderHostObserver prerender_observer(
         *GetActiveWebContents(), expected_prerender_url);
     base::RunLoop run_loop;
-    SetLoaderDestructionCallbackForTesting(
+    search_prefetch_service()->SetLoaderDestructionCallbackForTesting(
         GetCanonicalSearchURL(expected_prerender_url), run_loop.QuitClosure());
     NavigateToPrerenderedResult(expected_prerender_url);
     prerender_observer.WaitForActivation();
