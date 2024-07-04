24756359f226125e63393adf3615aa7da04602e9
Adithya Srinivasan <adithyas@chromium.org>
2024-07-04 15:16:14 +0000

    Portals: Remove is_inside_portal parameter in WebView
    Parameter has been unused since Portals was removed.
    
    Bug: 40287334
    Change-Id: Idc6ac40e88ee05b2b8fec3098c68100f279da353
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676924
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323363}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -486,7 +486,6 @@ WebView* WebView::Create(
     WebViewClient* client,
     bool is_hidden,
     blink::mojom::PrerenderParamPtr prerender_param,
-    bool is_inside_portal,
     std::optional<blink::FencedFrame::DeprecatedFencedFrameMode>
         fenced_frame_mode,
     bool compositing_enabled,
@@ -503,18 +502,17 @@ WebView* WebView::Create(
       client,
       is_hidden ? mojom::blink::PageVisibilityState::kHidden
                 : mojom::blink::PageVisibilityState::kVisible,
-      std::move(prerender_param), is_inside_portal, fenced_frame_mode,
-      compositing_enabled, widgets_never_composited, To<WebViewImpl>(opener),
-      std::move(page_handle), agent_group_scheduler,
-      session_storage_namespace_id, std::move(page_base_background_color),
-      browsing_context_group_info, color_provider_colors);
+      std::move(prerender_param), fenced_frame_mode, compositing_enabled,
+      widgets_never_composited, To<WebViewImpl>(opener), std::move(page_handle),
+      agent_group_scheduler, session_storage_namespace_id,
+      std::move(page_base_background_color), browsing_context_group_info,
+      color_provider_colors);
 }
 
 WebViewImpl* WebViewImpl::Create(
     WebViewClient* client,
     mojom::blink::PageVisibilityState visibility,
     blink::mojom::PrerenderParamPtr prerender_param,
-    bool is_inside_portal,
     std::optional<blink::FencedFrame::DeprecatedFencedFrameMode>
         fenced_frame_mode,
     bool compositing_enabled,
@@ -527,8 +525,8 @@ WebViewImpl* WebViewImpl::Create(
     const BrowsingContextGroupInfo& browsing_context_group_info,
     const ColorProviderColorMaps* color_provider_colors) {
   return new WebViewImpl(
-      client, visibility, std::move(prerender_param), is_inside_portal,
-      fenced_frame_mode, compositing_enabled, widgets_never_composited, opener,
+      client, visibility, std::move(prerender_param), fenced_frame_mode,
+      compositing_enabled, widgets_never_composited, opener,
       std::move(page_handle), agent_group_scheduler,
       session_storage_namespace_id, std::move(page_base_background_color),
       browsing_context_group_info, color_provider_colors);
@@ -585,7 +583,6 @@ WebViewImpl::WebViewImpl(
     WebViewClient* client,
     mojom::blink::PageVisibilityState visibility,
     blink::mojom::PrerenderParamPtr prerender_param,
-    bool is_inside_portal,
     std::optional<blink::FencedFrame::DeprecatedFencedFrameMode>
         fenced_frame_mode,
     bool does_composite,
