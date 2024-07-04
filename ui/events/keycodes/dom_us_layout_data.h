8581726ff79461aa4bb83547f6b5b2d5712eec23
Jan Keitel <jkeitel@google.com>
2024-07-04 08:04:10 +0000

    Use std::array in ui::PrintableCodeEntry  to avoid unsafe buffer error.
    Indexed access of a C-style array triggers unsafe buffer errors if
    those are turned on - presumably due to the underlying pointer
    arithmetic.
    
    Bug: 347137620
    Change-Id: Icf917b131c74211fb8337fd352117686678e8f1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672794
    Reviewed-by: Wez <wez@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323227}

--- ui/events/keycodes/dom_us_layout_data.h
+++ ui/events/keycodes/dom_us_layout_data.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_EVENTS_KEYCODES_DOM_US_LAYOUT_DATA_H_
 #define UI_EVENTS_KEYCODES_DOM_US_LAYOUT_DATA_H_
 
+#include <array>
+
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 
@@ -16,7 +18,7 @@ namespace ui {
 // interpretation when there is no other way to map a physical key.
 const struct PrintableCodeEntry {
   DomCode dom_code;
-  char16_t character[2];  // normal, shift
+  std::array<char16_t, 2> character;  // normal, shifted
 } kPrintableCodeMap[] = {
     {DomCode::US_A, {'a', 'A'}},
     {DomCode::US_B, {'b', 'B'}},
