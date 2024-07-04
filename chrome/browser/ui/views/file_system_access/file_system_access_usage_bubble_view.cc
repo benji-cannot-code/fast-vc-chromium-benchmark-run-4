5dd9fe054e1fe695cb2e1397e001810e866b8cc5
Daniel Cheng <dcheng@chromium.org>
2024-07-04 04:21:10 +0000

    [MSan] Fix use-after-dtor errors in FileSystemAccessUsageBubbleView
    The bubble view owns the `TableModel`, but also has a descendent
    `TableView`, which has a non-owning pointer to the `TableModel`. If the
    model is not explicitly cleared, this results in a use-after-dtor error,
    because:
    - `TableModel` is destroyed after `~FileSystemAccessUsageBubblewView()`
      completes.
    - Descendent views are not destroyed until the `View` base class of the
      bubble view is destroyed.
    - When the descendent `TableView` is destroyed, it unsets itself as an
      observer of the `TableModel`. However, at this point, the `TableModel`
      is already destroyed, and this is a use-after-dtor error.
    
    Bug: 40222690
    Change-Id: I6b3fdd71f60a5e57e65cb7df5f6f75c69dfcca99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676848
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323155}

--- chrome/browser/ui/views/file_system_access/file_system_access_usage_bubble_view.cc
+++ chrome/browser/ui/views/file_system_access/file_system_access_usage_bubble_view.cc
@@ -205,6 +205,11 @@ class CollapsibleListView : public views::View {
     table_view_parent_->SetVisible(false);
   }
 
+  void ClearModel() {
+    static_cast<views::TableView*>(table_view_parent_->contents())
+        ->SetModel(nullptr);
+  }
+
   // views::View
   void OnThemeChanged() override {
     views::View::OnThemeChanged();
@@ -368,7 +373,16 @@ FileSystemAccessUsageBubbleView::FileSystemAccessUsageBubbleView(
       views::DISTANCE_BUBBLE_PREFERRED_WIDTH));
 }
 
-FileSystemAccessUsageBubbleView::~FileSystemAccessUsageBubbleView() = default;
+FileSystemAccessUsageBubbleView::~FileSystemAccessUsageBubbleView() {
+  if (readable_collapsible_list_view_) {
+    static_cast<CollapsibleListView*>(readable_collapsible_list_view_)
+        ->ClearModel();
+  }
+  if (writable_collapsible_list_view_) {
+    static_cast<CollapsibleListView*>(writable_collapsible_list_view_)
+        ->ClearModel();
+  }
+}
 
 std::u16string FileSystemAccessUsageBubbleView::GetAccessibleWindowTitle()
     const {
@@ -429,7 +443,7 @@ void FileSystemAccessUsageBubbleView::Init() {
         label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
         AddChildView(std::move(label));
       }
-      AddChildView(
+      writable_collapsible_list_view_ = AddChildView(
           std::make_unique<CollapsibleListView>(&writable_paths_model_));
     }
 
@@ -443,7 +457,7 @@ void FileSystemAccessUsageBubbleView::Init() {
         label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
         AddChildView(std::move(label));
       }
-      AddChildView(
+      readable_collapsible_list_view_ = AddChildView(
           std::make_unique<CollapsibleListView>(&readable_paths_model_));
     }
   }
