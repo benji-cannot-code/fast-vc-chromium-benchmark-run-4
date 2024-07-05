c1c507570a6c8f48ed900e49cdb558cc574082ea
Ankush Singh <ankushkush@google.com>
2024-07-05 13:00:52 +0000

    Reset pointer to avoid dangling ptr in SyncEngineImplTest
    FakeSyncManager instance is owned by the SyncEngineImpl and is
    destroyed upon Shutdown(). This leads to a dangling `fake_sync_manager_`
    in SyncEngineImplTest.
    
    Bug: 350973717
    Change-Id: I0406c1f807d421f152a46234c0b601d31f8f7510
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679285
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Auto-Submit: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323661}

--- components/sync/service/glue/sync_engine_impl_unittest.cc
+++ components/sync/service/glue/sync_engine_impl_unittest.cc
@@ -87,7 +87,7 @@ class MockSyncEngineHost : public SyncEngineHost {
 
 class FakeSyncManagerFactory : public SyncManagerFactory {
  public:
-  explicit FakeSyncManagerFactory(
+  FakeSyncManagerFactory(
       raw_ptr<FakeSyncManager>* fake_manager,
       network::NetworkConnectionTracker* network_connection_tracker)
       : SyncManagerFactory(network_connection_tracker),
@@ -188,8 +188,6 @@ class SyncEngineImplTest : public testing::Test {
   }
 
   void TearDown() override {
-    fake_manager_ = nullptr;
-
     if (backend_) {
       ShutdownBackend(ShutdownReason::BROWSER_SHUTDOWN_AND_KEEP_DATA);
     }
@@ -228,6 +226,8 @@ class SyncEngineImplTest : public testing::Test {
   void ShutdownBackend(ShutdownReason reason) {
     DCHECK(backend_);
     backend_->StopSyncingForShutdown();
+    // Reset `fake_manager_` to avoid dangling pointer.
+    fake_manager_ = nullptr;
     backend_->Shutdown(reason);
     backend_.reset();
   }
@@ -525,6 +525,8 @@ TEST_F(SyncEngineImplTest, ModelTypeConnectorValidDuringShutdown) {
   backend_->StopSyncingForShutdown();
   // Verify that call to DisconnectDataType doesn't assert.
   backend_->DisconnectDataType(AUTOFILL);
+  // Reset `fake_manager_` to avoid dangling pointer.
+  fake_manager_ = nullptr;
   backend_->Shutdown(ShutdownReason::STOP_SYNC_AND_KEEP_DATA);
   backend_.reset();
 }
