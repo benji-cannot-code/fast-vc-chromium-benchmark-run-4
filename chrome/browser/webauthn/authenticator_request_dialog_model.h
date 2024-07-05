e91692ad4706497eed00555ffc5c81f1ff46403a
rgod <rgod@google.com>
2024-07-05 14:45:54 +0000

    [WebAuthn] Remove obsolete TODO
    This has been handled for a while.
    
    Change-Id: I60c4e9e2ca8727125aaf33c092642358fd5f622e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680282
    Auto-Submit: Rafał Godlewski <rgod@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323699}

--- chrome/browser/webauthn/authenticator_request_dialog_model.h
+++ chrome/browser/webauthn/authenticator_request_dialog_model.h
@@ -123,8 +123,6 @@ class Profile;
   /* (But not the GPM one.) */                                                \
   AUTHENTICATOR_REQUEST_EVENT_0(OnCreatePasskeyAccepted)                      \
   /* Called when the user accepts passkey creation dialog. */                 \
-  /* TODO(enclave): Add transition to authentication or bootstrapping  */     \
-  /* device. */                                                               \
   AUTHENTICATOR_REQUEST_EVENT_0(OnGPMCreatePasskey)                           \
   /* Called when the user accepts the warning dialog for creating a GPM */    \
   /* passkey in incognito mode.*/                                             \
