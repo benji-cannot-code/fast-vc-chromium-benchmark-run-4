b2aa79d05ae53f7eb3d1af2f421166a608ad3c7f
Daniel Cheng <dcheng@chromium.org>
2024-07-05 18:11:01 +0000

    Remove base/template_util.h
    This is essentially an empty file and is no longer needed.
    
    Bug: 40942274
    Change-Id: I2f6b86a7219283be543f51cb37b745756b7e18f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679621
    Owners-Override: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323763}

--- third_party/blink/renderer/platform/wtf/casting.h
+++ third_party/blink/renderer/platform/wtf/casting.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/notreached.h"
-#include "base/template_util.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
 
 namespace blink {
