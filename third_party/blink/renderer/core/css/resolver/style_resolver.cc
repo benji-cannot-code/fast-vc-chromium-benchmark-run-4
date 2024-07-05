f31a69c301dd3ac574e4db49afa29df193798ab5
Munira Tursunova <moonira@google.com>
2024-07-05 22:07:06 +0000

    Renamed HasAttrContent ComputedStyle flag
    Renamed 'HasAttrContent' ComputedStyle flag to 'HasAttrFunction' to
    reflect the expanded functionality of attr(), which is no longer limited
    to the 'content' property.
    
    Bug: 40320391
    Change-Id: I21783a7da33a7dc771eb2de1ad053b34d241e1a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676893
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323804}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -1602,7 +1602,7 @@ void StyleResolver::ApplyBaseStyleNoCache(
 
   if (RuntimeEnabledFeatures::CSSAdvancedAttrFunctionEnabled() &&
       state.HasAttrFunction()) {
-    state.StyleBuilder().SetHasAttrContent();
+    state.StyleBuilder().SetHasAttrFunction();
   }
 
   // Now we're done with all operations that may overwrite InsideLink,
