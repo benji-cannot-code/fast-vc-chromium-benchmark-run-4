b5bad9f09210fcaa3f1ed48ccb58c3787e0c372e
Christian Dullweber <dullweber@chromium.org>
2024-07-05 13:04:49 +0000

    Incognito: Fix test failures in Batch mode
    Reset Prefs between runs to avoid failures in batched execution.
    
    Bug: 344662664
    Change-Id: Idee92d53b68f9a140617c831f4ce96bd02cfb48d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676681
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323664}

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/IncognitoNewTabPageTest.java
@@ -25,6 +25,7 @@ import android.content.res.Resources;
 import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -32,6 +33,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.chromium.base.LocaleUtils;
+import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.chrome.browser.ProductConfig;
@@ -53,7 +55,7 @@ import java.util.Locale;
 
 /** Integration tests for IncognitoNewTabPage. */
 @RunWith(ChromeJUnit4ClassRunner.class)
-// TODO(crbug.com/344662664): Failing when batched, batch this again.
+@Batch(Batch.PER_CLASS)
 @DisableFeatures({ChromeFeatureList.TRACKING_PROTECTION_3PCD})
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class IncognitoNewTabPageTest {
@@ -90,6 +92,18 @@ public class IncognitoNewTabPageTest {
                     PrefService prefService =
                             UserPrefs.get(ProfileManager.getLastUsedRegularProfile());
                     prefService.setBoolean(Pref.TRACKING_PROTECTION3PCD_ENABLED, true);
+                });
+    }
+
+    @After
+    public void tearDown() {
+        TestThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    PrefService prefService =
+                            UserPrefs.get(ProfileManager.getLastUsedRegularProfile());
+
+                    prefService.clearPref(Pref.TRACKING_PROTECTION3PCD_ENABLED);
+                    prefService.clearPref(PrefNames.COOKIE_CONTROLS_MODE);
                 });
     }
 
