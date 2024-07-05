761eb27e22ed021cca6b4e6281321e766c58144c
Rune Lillesveen <futhark@chromium.org>
2024-07-05 11:46:44 +0000

    Remove BackdropInheritOriginating runtime flag
    Feature has been stable since M122.
    
    Bug: 40569411
    Change-Id: I595afcf343d6c4f3872e6413bcacd542e86796fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678951
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323647}

--- third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver_test.cc
@@ -2473,8 +2473,6 @@ TEST_F(StyleResolverTest, IsInertWithFrameAndFullscreen) {
 }
 
 TEST_F(StyleResolverTest, IsInertWithBackdrop) {
-  ScopedBackdropInheritOriginatingForTest backdrop_inherit(true);
-
   Document& document = GetDocument();
   NonThrowableExceptionState exception_state;
 
