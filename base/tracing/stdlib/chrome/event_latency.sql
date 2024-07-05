9a270dfa72d8e709bd219b33a5212e80d9d8a89e
Harkiran Bolaria <hbolaria@google.com>
2024-07-05 13:12:16 +0000

    Fix method name to allow copybara to autoroll stdlib to perfetto.
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests
    
    Change-Id: Ie7dd3880c231f2f1b76e2b006416bcb8339628a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675280
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Harkiran Bolaria <hbolaria@google.com>
    Cr-Commit-Position: refs/heads/main@{#1323669}

--- base/tracing/stdlib/chrome/event_latency.sql
+++ base/tracing/stdlib/chrome/event_latency.sql
@@ -3,21 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -- Use of this source code is governed by a BSD-style license that can be
 -- found in the LICENSE file.
 
--- Checks if slice has a descendant with provided name.
-CREATE PERFETTO FUNCTION _has_descendant_slice_with_name(
-  -- Id of the slice to check descendants of.
-  id INT,
-  -- Name of potential descendant slice.
-  descendant_name STRING
-)
--- Whether `descendant_name` is a name of an descendant slice.
-RETURNS BOOL AS
-SELECT EXISTS(
-  SELECT 1
-  FROM descendant_slice($id)
-  WHERE name = $descendant_name
-  LIMIT 1
-);
+INCLUDE PERFETTO MODULE deprecated.v42.common.slices;
 
 -- Finds the end timestamp for a given slice's descendant with a given name.
 -- If there are multiple descendants with a given name, the function will return
@@ -65,7 +51,7 @@ SELECT
   slice.ts,
   slice.dur,
   EXTRACT_arg(arg_set_id, 'event_latency.event_latency_id') AS scroll_update_id,
-  _has_descendant_slice_with_name(
+  has_descendant_slice_with_name(
     slice.id,
     'SubmitCompositorFrameToPresentationCompositorFrame')
   AS is_presented,
@@ -85,7 +71,7 @@ SELECT
   id,
   scroll_update_id,
   is_presented,
-  CASE WHEN _has_descendant_slice_with_name(
+  CASE WHEN has_descendant_slice_with_name(
       id,
       'SwapEndToPresentationCompositorFrame')
     THEN _descendant_slice_end(id, 'SwapEndToPresentationCompositorFrame')
@@ -99,7 +85,7 @@ WHERE (
   event_type GLOB '*GESTURE_SCROLL*'
   -- Pinches are only relevant if the frame was presented.
   OR (event_type GLOB '*GESTURE_PINCH_UPDATE'
-    AND _has_descendant_slice_with_name(
+    AND has_descendant_slice_with_name(
       id,
       'SubmitCompositorFrameToPresentationCompositorFrame')
   )
