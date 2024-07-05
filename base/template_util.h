0f022739e6b2368555305fb1244af39b3d8daf19
Daniel Cheng <dcheng@chromium.org>
2024-07-05 17:16:11 +0000

    Remove base::internal::is_iterator from template_util.h
    Bug: 40942274
    Change-Id: Iaf7762be6a66f58999ba4e6a87536d1d10f58746
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679620
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323750}

--- base/template_util.h
+++ base/template_util.h
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2011 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+//
+// TODO(dcheng): Remove this file.
 
 #ifndef BASE_TEMPLATE_UTIL_H_
 #define BASE_TEMPLATE_UTIL_H_
@@ -14,18 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 namespace internal {
-
-// Used to detect whether the given type is an iterator.  This is normally used
-// with std::enable_if to provide disambiguation for functions that take
-// templatzed iterators as input.
-template <typename T, typename = void>
-struct is_iterator : std::false_type {};
-
-template <typename T>
-struct is_iterator<
-    T,
-    std::void_t<typename std::iterator_traits<T>::iterator_category>>
-    : std::true_type {};
 
 }  // namespace internal
 
