475cea09a0630c2d522994b2e6e18bc3d9dae437
Theo Cristea <theocristea@google.com>
2024-07-05 13:13:55 +0000

    [mullhet]Move the password metrics recorder to `PasswordAutofillManager`
    This is a preparation CL for: crrev.com/c/5660566, which adds new
    metrics to the metrics recorder.
    
    Details about the implementation are in the "Metrics -> Implementation"
    section of the design doc:
    http://go/chrome-mullhet-dd#heading=h.tedzfsk8hkl4
    
    Bug: 321678141, 330304589
    Change-Id: I57da5c16e3731dcf2fad582d90b21f08f67e167c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672215
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323670}

--- components/password_manager/core/browser/password_autofill_manager.cc
+++ components/password_manager/core/browser/password_autofill_manager.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manual_fallback_flow.h"
+#include "components/password_manager/core/browser/password_manual_fallback_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_suggestion_flow.h"
 #include "components/password_manager/core/browser/password_suggestion_generator.h"
 #include "components/password_manager/core/browser/password_ui_utils.h"
@@ -161,10 +162,16 @@ PasswordAutofillManager::PasswordAutofillManager(
     : suggestion_generator_(password_manager_driver, password_client),
       password_manager_driver_(password_manager_driver),
       autofill_client_(autofill_client),
-      password_client_(password_client) {}
+      password_client_(password_client),
+      manual_fallback_metrics_recorder_(
+          std::make_unique<PasswordManualFallbackMetricsRecorder>()) {}
 
 PasswordAutofillManager::~PasswordAutofillManager() {
   CancelBiometricReauthIfOngoing();
+  // `manual_fallback_flow_` holds a raw pointer to
+  // `manual_fallback_metrics_recorder_`, so the flow should be reset first.
+  manual_fallback_flow_.reset();
+  manual_fallback_metrics_recorder_.reset();
 }
 
 absl::variant<autofill::AutofillDriver*, PasswordManagerDriver*>
@@ -423,6 +430,7 @@ void PasswordAutofillManager::OnShowPasswordSuggestions(
     if (!manual_fallback_flow_) {
       manual_fallback_flow_ = std::make_unique<PasswordManualFallbackFlow>(
           password_manager_driver_, autofill_client_, password_client_,
+          manual_fallback_metrics_recorder_.get(),
           password_client_->GetPasswordManager()->GetPasswordFormCache(),
           std::make_unique<SavedPasswordsPresenter>(
               password_client_->GetAffiliationService(),
@@ -473,7 +481,11 @@ void PasswordAutofillManager::DidNavigateMainFrame() {
   CancelBiometricReauthIfOngoing();
   favicon_tracker_.TryCancelAll();
   page_favicon_ = gfx::Image();
+  // `manual_fallback_flow_` holds a raw pointer to
+  // `manual_fallback_metrics_recorder_`, so the flow should be reset first.
   manual_fallback_flow_.reset();
+  manual_fallback_metrics_recorder_ =
+      std::make_unique<PasswordManualFallbackMetricsRecorder>();
 }
 
 bool PasswordAutofillManager::FillSuggestionForTest(
