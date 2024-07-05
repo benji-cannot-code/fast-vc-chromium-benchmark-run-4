04fb08722de5010990028063a00612acef393430
Ivana Žužić <izuzic@google.com>
2024-07-05 14:17:23 +0000

    [UPMClearMigratedData] Add the feature flag
    This CL adds a feature flag for deleting the login data file for users
    with passwords migrated to GMS Core.
    
    Bug: 350510016
    Change-Id: I31f25e78862c734ff3075e996acbf1b4a0e1b81e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678373
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1323689}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -4206,9 +4206,17 @@ const char kChimeAndroidSdkDescription[] =
     "Enable Chime SDK to receive push notification.";
 const char kChimeAndroidSdkName[] = "Use Chime SDK";
 
+const char kClearLoginDatabaseForAllMigratedUPMUsersDescription[] =
+    "This feature clears LoginDatabase for all users when UPM is properly "
+    "enabled and the credentials were migrated to GMS Core.";
+const char kClearLoginDatabaseForAllMigratedUPMUsersName[] =
+    "Clear LoginDatabase for all migrated UPM users";
+
 const char kClearLoginDatabaseForUPMUsersDescription[] =
-    "This feature clears LoginDatabase for syncing users when UPM is properly "
-    "enabled. Only works if UnifiedPasswordManagerSyncOnlyInGMSCore feature "
+    "This feature clears LoginDatabase for syncing users when UPM is "
+    "properly "
+    "enabled. Only works if UnifiedPasswordManagerSyncOnlyInGMSCore "
+    "feature "
     "flag is also enabled.";
 const char kClearLoginDatabaseForUPMUsersName[] =
     "Clear LoginDatabase for UPM users";
