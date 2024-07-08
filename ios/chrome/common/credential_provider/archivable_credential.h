65a9b4a77d3f0f3ded362d59932fa03358122dd0
Alexis Hetu <sugoi@chromium.org>
2024-07-08 19:59:12 +0000

    [iOS] Remove hex representation of passkey data
    The hex representation of data was very useful during the initial
    phases of development, but this is well understood enough now, so
    the representation can be simplified to just using NSData instead.
    
    Bug: 330355124
    Change-Id: I1d5ea81670f3716c5fb178e5d909239349798051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5683112
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324447}

--- ios/chrome/common/credential_provider/archivable_credential.h
+++ ios/chrome/common/credential_provider/archivable_credential.h
@@ -35,14 +35,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Initializer used for passkey credentials.
 - (instancetype)initWithFavicon:(NSString*)favicon
                recordIdentifier:(NSString*)recordIdentifier
-                         syncId:(NSString*)syncId
+                         syncId:(NSData*)syncId
                        username:(NSString*)username
                 userDisplayName:(NSString*)userDisplayName
-                         userId:(NSString*)userId
-                   credentialId:(NSString*)credentialId
+                         userId:(NSData*)userId
+                   credentialId:(NSData*)credentialId
                            rpId:(NSString*)rpId
-                     privateKey:(NSString*)privateKey
-                      encrypted:(NSString*)encrypted
+                     privateKey:(NSData*)privateKey
+                      encrypted:(NSData*)encrypted
                    creationTime:(int64_t)creationTime NS_DESIGNATED_INITIALIZER;
 
 - (instancetype)init NS_UNAVAILABLE;
