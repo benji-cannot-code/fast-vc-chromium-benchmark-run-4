4b0a17f61c7ab76bb8cd03d4490ff5a64b64eca6
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-08 15:28:31 +0000

    Remove SetMaxTransportState() from TestSyncService::StopAndClear()
    Test-only change. In [1] we removed one of the internal calls to
    SetMaxTransportState(), but not this one [2]. This CL finishes
    the job.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5665303/comment/e451fffc_90b4efe7/
    [2]
    https://source.chromium.org/chromium/chromium/src/+/main:components/sync/test/test_sync_service.cc;l=314;drc=1b28ae924353efe44adb967550fae814a109bffa
    
    Bug: None
    Change-Id: Ife087db8699d3271afe3321d99c804c4b51d787c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667411
    Reviewed-by: Marc Treib <treib@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324245}

--- components/sync/test/test_sync_service.cc
+++ components/sync/test/test_sync_service.cc
@@ -311,8 +311,6 @@ void TestSyncService::StopAndClear() {
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
   SetSignedInWithoutSyncFeature();
 #endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
-
-  SetMaxTransportState(TransportState::INITIALIZING);
 }
 
 void TestSyncService::OnDataTypeRequestsSyncStartup(ModelType type) {}
