17ab80b8dbf029b77da034ef8315a6f1a7e9122f
Sky Malice <skym@chromium.org>
2024-07-08 22:18:45 +0000

    Use CallbackController for long running delayed declutter task.
    Posting a delayed task to an essentially global TaskRunner causes a
    memory leak. Fixed by simply wrapping with a CallbackController, which
    both fixes the leak with weak references, and stops any use after
    destroy problems.
    
    Bug: 351871190
    Change-Id: I708a02c0a0962c2bcc333e3832d76952fca21bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685394
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324530}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
@@ -13,6 +13,7 @@ import org.chromium.base.ApplicationState;
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.ApplicationStatus.ApplicationStateListener;
 import org.chromium.base.Callback;
+import org.chromium.base.CallbackController;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
@@ -56,7 +57,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
         /**
          * Called when the archived {@link TabModel} is created.
          *
-         * @param TabModel The {@link TabModel} that was created.
+         * @param archivedTabModel The {@link TabModel} that was created.
          */
         public void onTabModelCreated(TabModel archivedTabModel);
     }
@@ -95,7 +96,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
     private boolean mRestoreTabsCalled;
     private boolean mDeclutterInitializationCalled;
     private boolean mRescueTabsCalled;
-    private boolean mArchiveAllButActiveCalled;
+    private CallbackController mCallbackController = new CallbackController();
 
     /**
      * Returns the ArchivedTabModelOrchestrator that corresponds to the given profile. Must be
@@ -112,6 +113,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
                             ProfileKeyedMap.ProfileSelection.REDIRECTED_TO_ORIGINAL);
             ApplicationStatus.registerApplicationStateListener(sApplicationStateListener);
         }
+
         return sProfileMap.getForProfile(
                 profile,
                 (originalProfile) ->
@@ -150,6 +152,11 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
 
     @Override
     public void destroy() {
+        if (mCallbackController != null) {
+            mCallbackController.destroy();
+            mCallbackController = null;
+        }
+
         if (mWindow != null) {
             mWindow.destroy();
             mWindow = null;
@@ -174,7 +181,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
     }
 
     /**
-     * Creates and initiailzes the class and fields, this must be called in the UI thread and can be
+     * Creates and initializes the class and fields, this must be called in the UI thread and can be
      * expensive therefore it should be called from DeferredStartupHandler. Although the lifecycle
      * methods inherited from {@link TabModelOrchestrator} are public, they aren't meant to be
      * called directly. - The {@link TabModelSelector} and the {@link TabPersistentStore} are
@@ -328,7 +335,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
     private void runDeclutterAndScheduleNext() {
         mTabArchiver.triggerScheduledDeclutter();
         mTaskRunner.postDelayedTask(
-                this::runDeclutterAndScheduleNext,
+                mCallbackController.makeCancelable(this::runDeclutterAndScheduleNext),
                 TimeUnit.HOURS.toMillis(mTabArchiveSettings.getDeclutterIntervalTimeDeltaHours()));
     }
 
