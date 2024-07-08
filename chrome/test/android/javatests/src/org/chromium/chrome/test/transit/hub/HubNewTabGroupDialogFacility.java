6042e68ccbc83a0916bd39de872ed0f2c03fa98e
Henrique Nakashima <hnakashima@chromium.org>
2024-07-08 19:09:28 +0000

    [Android] Implement SoftKeyboardElement
    Use it to fix flakiness in TabSwitcherListEditorPTTest.
    
    Bug: 345838217,345489175
    Change-Id: Id79ac06a99fdedc51b4237476aff2df4f24b8eeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651401
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324408}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/HubNewTabGroupDialogFacility.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/hub/HubNewTabGroupDialogFacility.java
@@ -35,6 +35,7 @@ import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.transit.HubTabSwitcherBaseStation;
 import org.chromium.chrome.test.transit.tab_groups.TabGroupUtil;
 import org.chromium.components.tab_groups.TabGroupColorId;
+import org.chromium.ui.test.transit.SoftKeyboardElement;
 
 import java.util.List;
 
@@ -108,6 +109,8 @@ public class HubNewTabGroupDialogFacility extends Facility<HubTabSwitcherBaseSta
         }
 
         elements.declareView(DONE_BUTTON);
+
+        elements.declareElementInState(new SoftKeyboardElement(mHostStation.getActivitySupplier()));
     }
 
     private Matcher<View> colorPickerIconMatcher(
---
+++ ui/android/javatests/src/org/chromium/ui/test/transit/SoftKeyboardElement.java
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.ui.test.transit;
+
+import android.app.Activity;
+
+import org.chromium.base.supplier.Supplier;
+import org.chromium.base.test.transit.Condition;
+import org.chromium.base.test.transit.ConditionWithResult;
+import org.chromium.base.test.transit.ElementInState;
+
+/** Represents the soft keyboard shown, expecting it to hide after exiting the ConditionalState. */
+public class SoftKeyboardElement extends ElementInState<Void> {
+
+    private final Supplier<? extends Activity> mActivitySupplier;
+
+    public SoftKeyboardElement(Supplier<? extends Activity> activitySupplier) {
+        super("SoftKeyboardElement");
+        mActivitySupplier = activitySupplier;
+    }
+
+    @Override
+    public ConditionWithResult<Void> createEnterCondition() {
+        return new SoftKeyboardCondition(mActivitySupplier, /* expectShowing= */ true);
+    }
+
+    @Override
+    public Condition createExitCondition() {
+        return new SoftKeyboardCondition(mActivitySupplier, /* expectShowing= */ false);
+    }
+}
