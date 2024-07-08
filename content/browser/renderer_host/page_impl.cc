d77bffa4287772a01714caf71de5ef5f7c81f2f7
Adithya Srinivasan <adithyas@chromium.org>
2024-07-08 21:57:16 +0000

    Portals: Clean up TODOs for crbug.com/40787700
    Bug: 40287334, 40787700
    Change-Id: I122e7f07a38dacb00d6f88036896d8f92169a171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676667
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324520}

--- content/browser/renderer_host/page_impl.cc
+++ content/browser/renderer_host/page_impl.cc
@@ -72,8 +72,6 @@ void PageImpl::GetManifest(GetManifestCallback callback) {
 }
 
 bool PageImpl::IsPrimary() const {
-  // TODO(crbug.com/40787700): Check for portals as well, once they are migrated
-  // to MPArch.
   if (main_document_->IsFencedFrameRoot())
     return false;
 
