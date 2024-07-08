d73f8d4ddf5eff41c405205cba8a50692768d2aa
Pi-Hsun Shih <pihsun@chromium.org>
2024-07-08 08:06:41 +0000

    recorder: Align onboarding flow with new design
    The main change is a new "maybe later" button to explicitly defers the
    selection, separate from the "cancel" button to disable transcription.
    
    Bug: b:344785475
    Test: manually
    Change-Id: If10033b2c247ad9cbe18eea2b65fbe8200d32ab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678103
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324093}

--- ash/webui/recorder_app_ui/resources/components/onboarding-dialog.ts
+++ ash/webui/recorder_app_ui/resources/components/onboarding-dialog.ts
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import '../components/cra/cra-button.js';
 import '../components/cra/cra-image.js';
-import '../components/cra/cra-pagination.js';
 
 import {
   css,
@@ -77,16 +76,15 @@ export class OnboardingDialog extends ReactiveLitElement {
       white-space: pre-wrap;
     }
 
-    #actions {
-      display: flex;
-      flex-flow: row;
-      justify-content: space-between;
-    }
-
     #buttons {
       display: flex;
       flex-flow: row;
       gap: 8px;
+      justify-content: right;
+
+      & > .left {
+        margin-right: auto;
+      }
     }
   `;
 
@@ -123,6 +121,10 @@ export class OnboardingDialog extends ReactiveLitElement {
     }
   }
 
+  private close() {
+    this.dispatchEvent(new Event('close'));
+  }
+
   private renderDialog(
     imageName: string,
     header: string,
@@ -142,11 +144,7 @@ export class OnboardingDialog extends ReactiveLitElement {
       <div id="content">
         <div id="header">${header}</div>
         <div id="description">${description}</div>
-        <div id="actions">
-          <cra-pagination dots="3" .selected=${this.step.value}>
-          </cra-pagination>
-          <div id="buttons">${buttons}</div>
-        </div>
+        <div id="buttons">${buttons}</div>
       </div>
     </div>`;
   }
@@ -179,8 +177,11 @@ export class OnboardingDialog extends ReactiveLitElement {
           this.platformHandler.installSoda();
           this.step.value = 2;
         };
-        const cancelTranscription = () => {
-          this.step.value = 2;
+        const disableTranscription = () => {
+          settings.mutate((s) => {
+            s.transcriptionEnabled = TranscriptionEnableState.DISABLED_FIRST;
+          });
+          this.close();
         };
         return this.renderDialog(
           'onboarding_transcription',
@@ -188,9 +189,15 @@ export class OnboardingDialog extends ReactiveLitElement {
           i18n.onboardingDialogTranscriptionDescription,
           html`
             <cra-button
+              .label=${i18n.onboardingDialogTranscriptionDeferButton}
+              class="left"
+              button-style="secondary"
+              @click=${this.close}
+            ></cra-button>
+            <cra-button
               .label=${i18n.onboardingDialogTranscriptionCancelButton}
               button-style="secondary"
-              @click=${cancelTranscription}
+              @click=${disableTranscription}
             ></cra-button>
             <cra-button
               .label=${i18n.onboardingDialogTranscriptionTurnOnButton}
@@ -201,22 +208,25 @@ export class OnboardingDialog extends ReactiveLitElement {
       }
       case 2: {
         // TODO: b/344785475 - Implement speaker ID enable/disable logic.
-        const nextStep = () => {
-          this.dispatchEvent(new Event('close'));
-        };
         return this.renderDialog(
           'onboarding_speaker_id',
           i18n.onboardingDialogSpeakerIdHeader,
           i18n.onboardingDialogSpeakerIdDescription,
           html`
             <cra-button
+              .label=${i18n.onboardingDialogSpeakerIdDeferButton}
+              class="left"
+              button-style="secondary"
+              @click=${this.close}
+            ></cra-button>
+            <cra-button
               .label=${i18n.onboardingDialogSpeakerIdDisallowButton}
               button-style="secondary"
-              @click=${nextStep}
+              @click=${this.close}
             ></cra-button>
             <cra-button
               .label=${i18n.onboardingDialogSpeakerIdAllowButton}
-              @click=${nextStep}
+              @click=${this.close}
             ></cra-button>
           `,
         );
