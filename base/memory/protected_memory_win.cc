37c3036ba6f628d62831a6954dc213b44bf1ac93
Jeffrey Gour <jegour@microsoft.com>
2024-07-08 19:51:52 +0000

    Make static initialization of base::ProtectedMemory objects required
    This change makes it so that static initialization of
    base::ProtectedMemory instances is always required, even if the type
    provides constant initialization. This achieved by removing the bool
    ConstructLazily template parameter from ProtectedMemory and related
    classes, and making the case where it's "true" the only case. Thus the
    lazy construction through base::ProtectedMemoryInitializer is required.
    
    This change is made in preparation of adding support for ProtectedMemory
    to non-windows platforms. On some platforms there is an issue with
    initializing the protected memory section to readonly at build/link
    time. To combat this, the required initialization of ProtectedMemory
    instances will be used to initialize the protected section to readonly
    at runtime.
    
    This change also updates previously added use cases of ProtectedMemory
    to add the now required initialization.
    
    Bug: 41495919
    Change-Id: Ice3d39caee28a3fc956d4d9ebca8c6f51d03ce1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668250
    Commit-Queue: Jeffrey Gour <jegour@microsoft.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324440}

--- base/memory/protected_memory_win.cc
+++ base/memory/protected_memory_win.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+#if BUILDFLAG(PROTECTED_MEMORY_ENABLED)
 namespace {
 
 bool SetMemory(void* start, void* end, DWORD prot) {
@@ -50,5 +51,6 @@ bool AutoWritableMemoryBase::SetMemoryReadWrite(void* start, void* end) {
 bool AutoWritableMemoryBase::SetMemoryReadOnly(void* start, void* end) {
   return SetMemory(start, end, PAGE_READONLY);
 }
+#endif  // BUILDFLAG(PROTECTED_MEMORY_ENABLED)
 
 }  // namespace base
--- content/public/test/render_view_test.cc
+++ content/public/test/render_view_test.cc
@@ -69,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_input_element.h"
 #include "third_party/blink/public/web/web_local_frame.h"
 #include "third_party/blink/public/web/web_script_source.h"
+#include "third_party/blink/public/web/web_v8_features.h"
 #include "third_party/blink/public/web/web_view.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_provider.h"
@@ -392,6 +393,7 @@ void RenderViewTest::SetUp() {
   blink::WebRuntimeFeatures::EnableTestOnlyFeatures(true);
   blink::WebRuntimeFeatures::EnableOverlayScrollbars(
       ui::IsOverlayScrollbarEnabled());
+  blink::WebV8Features::InitializeMojoJSAllowedProtectedMemory();
 
   test_io_thread_ =
       std::make_unique<base::TestIOThread>(base::TestIOThread::kAutoStart);
