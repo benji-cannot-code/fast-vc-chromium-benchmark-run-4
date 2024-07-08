ac86184a49f084b7be960a088d611b956ef5a88f
Duncan Mercer <mercerd@google.com>
2024-07-08 17:30:23 +0000

    [lensoverlay] Hide toolbar entrypoint on DSE change
    Only need to manually hide/show the toolbar entrypoint because it is
    our only persistent entrypoint. All other entrypoints visibility based
    on DSE is determined at render.
    
    Bug: 350920674
    Change-Id: Ic5a70b3ba038d829d4ed05e9a6a375ab63c9dc3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675851
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324334}

--- chrome/browser/ui/lens/lens_overlay_entry_point_controller.h
+++ chrome/browser/ui/lens/lens_overlay_entry_point_controller.h
@@ -10,6 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_observer.h"
+#include "components/search_engines/template_url_service.h"
+#include "components/search_engines/template_url_service_observer.h"
+#include "ui/actions/actions.h"
 
 namespace lens {
 
@@ -17,7 +20,8 @@ namespace lens {
 // state. This functionality needs to be separate from LensOverlayController,
 // since LensOverlayController exist per tab, while entry points are per browser
 // window.
-class LensOverlayEntryPointController : public FullscreenObserver {
+class LensOverlayEntryPointController : public FullscreenObserver,
+                                        public TemplateURLServiceObserver {
  public:
   explicit LensOverlayEntryPointController(Browser* browser);
   ~LensOverlayEntryPointController() override;
@@ -26,12 +30,25 @@ class LensOverlayEntryPointController : public FullscreenObserver {
   // FullscreenObserver:
   void OnFullscreenStateChanged() override;
 
-  // Updates the enable/disable state of entry points.
-  void UpdateEntryPointsState();
+  // TemplateURLServiceObserver:
+  void OnTemplateURLServiceChanged() override;
+  void OnTemplateURLServiceShuttingDown() override;
+
+  // Updates the enable/disable state of entry points. If hide_if_needed is
+  // true, instead of just disabling the entrypoint, we will also hide the
+  // entrypoint from the user.
+  void UpdateEntryPointsState(bool hide_if_needed);
+
+  // Returns the ActionItem corresponding to our pinnable toolbar entrypoint.
+  actions::ActionItem* GetToolbarEntrypoint();
 
   // Observer to check for browser window entering fullscreen.
   base::ScopedObservation<FullscreenController, FullscreenObserver>
       fullscreen_observation_{this};
+
+  // Observer to check for changes to the users DSE.
+  base::ScopedObservation<TemplateURLService, TemplateURLServiceObserver>
+      template_url_service_observation_{this};
 
   // Reference to the browser housing our entry points.
   raw_ptr<Browser> browser_;
