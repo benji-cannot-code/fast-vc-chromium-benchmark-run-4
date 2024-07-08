bf0bd38c98f1378bc27d66637faaffa1cef6274a
Peter Kvitek <kvitekp@chromium.org>
2024-07-08 19:36:48 +0000

    [headless] Add --version switch handling to headless_shell
    WPT framework needs to check headless_shell version so this CL
    adds --version switch handling similar to Chrome's.
    
    This also makes Chrome --version switch behavior consistent when
    running with --headless=new|old
    
    Bug: 327767951
    Change-Id: I20f733283190fcedb7775a4178f516bf976522b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5684183
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324427}

--- headless/app/headless_shell.cc
+++ headless/app/headless_shell.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/task/thread_pool.h"
+#include "base/version_info/version_info.h"
 #include "build/branding_buildflags.h"
 #include "build/build_config.h"
 #include "content/public/app/content_main.h"
@@ -266,6 +267,12 @@ int HeadlessShellMain(content::ContentMainParams params) {
   // TODO(fuchsia): Remove this when GPU accelerated compositing is ready.
   command_line.AppendSwitch(::switches::kDisableGpu);
 #endif
+
+  if (command_line.HasSwitch(switches::kVersion)) {
+    printf("%s %s\n", version_info::GetProductName().data(),
+           version_info::GetVersionNumber().data());
+    return EXIT_SUCCESS;
+  }
 
   if (command_line.GetArgs().size() > 1) {
     LOG(ERROR) << "Multiple targets are not supported.";
