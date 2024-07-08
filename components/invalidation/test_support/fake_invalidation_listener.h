6fd106fe20d14ba9316bd5c26d496fc34d2203c1
Artem Sumaneev <asumaneev@google.com>
2024-07-08 16:12:05 +0000

    [invalidation] Add fake InvalidationListener
    Bug: b:346508642
    Change-Id: I6b838d6258430dbb8bb99949f78e66cca9ddef1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5683344
    Reviewed-by: Artyom Chen <artyomchen@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324267}

---
+++ components/invalidation/test_support/fake_invalidation_listener.h
@@ -0,0 +1,46 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_INVALIDATION_TEST_SUPPORT_FAKE_INVALIDATION_LISTENER_H_
+#define COMPONENTS_INVALIDATION_TEST_SUPPORT_FAKE_INVALIDATION_LISTENER_H_
+
+#include "components/invalidation/invalidation_listener.h"
+
+namespace invalidation {
+
+class FakeInvalidationListener : public invalidation::InvalidationListener {
+ public:
+  FakeInvalidationListener() = default;
+  ~FakeInvalidationListener() override = default;
+
+  FakeInvalidationListener(const FakeInvalidationListener&) = delete;
+  FakeInvalidationListener& operator=(const FakeInvalidationListener&) = delete;
+
+  void Start() { Start(nullptr); }
+
+  void Shutdown() override;
+
+  void FireInvalidation(const invalidation::DirectInvalidation& invalidation);
+
+  bool HasObserver(const Observer* handler) const override;
+
+ private:
+  void AddObserver(Observer* handler) override;
+
+  void RemoveObserver(const Observer* handler) override;
+
+  void Start(invalidation::RegistrationTokenHandler*) override;
+
+  void SetRegistrationUploadStatus(
+      RegistrationTokenUploadStatus status) override {}
+
+  invalidation::InvalidationsExpected invalidations_state_ =
+      invalidation::InvalidationsExpected::kMaybe;
+  raw_ptr<Observer> observer_ = nullptr;
+};
+
+}  // namespace invalidation
+
+#endif  // COMPONENTS_INVALIDATION_TEST_SUPPORT_FAKE_INVALIDATION_LISTENER_H_
