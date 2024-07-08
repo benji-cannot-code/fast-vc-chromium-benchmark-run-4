07f4b2829a29ababca84184524348762838316bb
Yudhistira Erlandinata <yerlandinata@google.com>
2024-07-08 08:07:03 +0000

    HaTS: Implement separate cooldown for "prioritized" HaTS
    Idea of prioritized HaTS was already implemented previously,
    that is by simply ignoring the 60-days cooldown. However, if there are
    more than one prioritized HaTS, a global cooldown for these kind of HaTS
    is needed.
    
    This patch also renamed term "global cap opt out" to be "prioritized" for
    simplicity.
    
    More details: go/cros-camera:dd:prioritized-hats
    
    BUG=b:336908951
    TEST=Manual: Modify /etc/chrome_dev.conf to force a prioritized HaTS
    TEST=unit_tests --gtest_filter="*HatsNotificationControllerTest*"
    
    Change-Id: Ifcdc481ea12d3b52883fe1b3048ee3429742debf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5671767
    Reviewed-by: Eriko Kurimoto <elkurin@chromium.org>
    Commit-Queue: Yudhistira Erlandinata <yerlandinata@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324094}

--- chrome/browser/ash/hats/hats_notification_controller.h
+++ chrome/browser/ash/hats/hats_notification_controller.h
@@ -80,7 +80,7 @@ class HatsNotificationController : public message_center::NotificationDelegate,
       HatsNotificationControllerTest,
       Disconnected_RemoveNotification_Connected_AddNotification);
   FRIEND_TEST_ALL_PREFIXES(HatsNotificationControllerTest,
-                           DismissNotification_OptOutShouldUpdatePref);
+                           DismissNotification_PrioritizedShouldUpdatePref);
 
   ~HatsNotificationController() override;
 
