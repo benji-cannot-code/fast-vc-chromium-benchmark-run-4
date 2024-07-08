c6932a33c2fff3b899270255e01bdb3ddfdee525
Anna Tsvirchkova <atsvirchkova@google.com>
2024-07-08 15:29:40 +0000

    [Reauth] Fix Androidx lib replacement metric
    In onAuthenticationError BiometricPrompt.ERROR_USER_CANCELED should be
    used to report DeviceAuthUIResult.CANCELED_BY_USER, otherwise it causes
    a discrepancy in UMA metrics.
    
    Bug: 340473460
    Change-Id: If406a6e84ae7ea46ddbed7529ac40999be52156f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678376
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1324247}

--- chrome/browser/device_reauth/android/java/src/org/chromium/chrome/browser/device_reauth/AndroidxDeviceAuthenticatorControllerImpl.java
+++ chrome/browser/device_reauth/android/java/src/org/chromium/chrome/browser/device_reauth/AndroidxDeviceAuthenticatorControllerImpl.java
@@ -84,7 +84,7 @@ class AndroidxDeviceAuthenticatorControllerImpl implements DeviceAuthenticatorCo
                             @Override
                             public void onAuthenticationError(
                                     int errorCode, @NonNull CharSequence errString) {
-                                if (errorCode == BiometricPrompt.ERROR_CANCELED) {
+                                if (errorCode == BiometricPrompt.ERROR_USER_CANCELED) {
                                     onAuthenticationCompleted(DeviceAuthUIResult.CANCELED_BY_USER);
                                     return;
                                 }
