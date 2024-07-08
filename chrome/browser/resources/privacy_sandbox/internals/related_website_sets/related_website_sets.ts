1ab1db08b4f20975be51961dc8dcdc11a4cfb5a6
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-07-08 15:51:32 +0000

    Initialize App level testing for RWS Dev UI
    This setups up app test coverage, testing rendering visibility. Also sets export standards for typescript
    
    Bug: 330877132
    Change-Id: Id10255871bab34f3ab14f9d12413494170def01c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675707
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324254}

--- chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
@@ -5,5 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './app.js';
 
-export {RelatedWebsiteSetsSidebarElement} from './sidebar.js';
-export {RelatedWebsiteSetsToolbarElement} from './toolbar.js';
+export type {RelatedWebsiteSetsAppElement} from './app.js';
+export type {RelatedWebsiteSetsSidebarElement} from './sidebar.js';
+export type {RelatedWebsiteSetsToolbarElement} from './toolbar.js';
