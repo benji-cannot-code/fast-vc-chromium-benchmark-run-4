bc437242253db9885d7a0ce09ceb71ac7d8a4097
Henrique Nakashima <hnakashima@chromium.org>
2024-07-08 18:30:14 +0000

    [Android] Decouple declareElementFactory() from declaring the Condition
    declareElementFactory() should be usable with guarding Conditions
    from Elements or from superclasses.
    
    declareElementFactory() now does _not_ create a ConditionWait for
    the guarding Condition.
    
    Bug: 351396138
    Change-Id: I43ce939f87c0cac2168482cc83d8769b74e48e8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680353
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324380}

--- base/test/android/javatests/src/org/chromium/base/test/transit/TripUnitTest.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/TripUnitTest.java
@@ -43,6 +43,7 @@ public class TripUnitTest {
             elements.declareExitCondition(
                     InstrumentationThreadCondition.from(
                             "Exit Condition 1, always True", () -> Condition.fulfilled()));
+            elements.declareEnterCondition(mOuterCondition);
             elements.declareElementFactory(
                     mOuterCondition,
                     (nestedElements) -> {
@@ -58,6 +59,7 @@ public class TripUnitTest {
                                 InstrumentationThreadCondition.from(
                                         "Exit Condition 2, always True",
                                         () -> Condition.fulfilled()));
+                        nestedElements.declareEnterCondition(mInnerCondition);
                         nestedElements.declareElementFactory(
                                 mInnerCondition,
                                 (nestedNestedElements) -> {
@@ -163,7 +165,7 @@ public class TripUnitTest {
         // All elements from nested factories added to the destination elements.
         assertEquals(3, destinationStation.getElements().getElementsInState().size());
         assertEquals(2, destinationStation.getElements().getElementFactories().size());
-        assertEquals(3, destinationStation.getElements().getOtherEnterConditions().size());
+        assertEquals(5, destinationStation.getElements().getOtherEnterConditions().size());
         assertEquals(3, destinationStation.getElements().getOtherExitConditions().size());
 
         // Conditions started and stopped monitoring during transition.
