5d1a19ba8fa97ebc4f43069d1e11572b82ca4a77
mlcui <mlcui@google.com>
2024-07-08 04:32:36 +0000

    Split `StopSearch` out of `StartSearch` in view
    See https://crrev.com/c/5677580/comment/fd64f9d1_86137638/ for more
    background.
    
    Bug: b:350594721
    Change-Id: I6a6a2100a05cc83855c7507bcc2a83561029b7a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679333
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324028}

--- ash/picker/views/picker_view.cc
+++ ash/picker/views/picker_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/views/picker_view.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
@@ -405,31 +406,36 @@ gfx::Rect PickerView::GetTargetBounds(const gfx::Rect& anchor_bounds,
 }
 
 void PickerView::StartSearch(const std::u16string& query) {
-  delegate_->GetSessionMetrics().UpdateSearchQuery(query);
-  if (!query.empty()) {
-    SetActivePage(search_results_view_);
-    clear_results_timer_.Start(
-        FROM_HERE, kClearResultsTimeout,
-        base::BindOnce(&PickerView::OnClearResultsTimerFired,
-                       weak_ptr_factory_.GetWeakPtr()));
-    delegate_->StartEmojiSearch(query,
-                                base::BindOnce(&PickerView::PublishEmojiResults,
-                                               weak_ptr_factory_.GetWeakPtr()));
-    delegate_->StartSearch(
-        query, selected_category_,
-        base::BindRepeating(&PickerView::PublishSearchResults,
-                            weak_ptr_factory_.GetWeakPtr()));
-  } else {
-    if (selected_category_.has_value()) {
-      SetActivePage(category_results_view_);
-    } else {
-      SetActivePage(zero_state_view_);
-    }
-    delegate_->StopSearch();
-    clear_results_timer_.Stop();
-    search_results_view_->ClearSearchResults();
-    ResetEmojiBarToZeroState();
+  if (query.empty()) {
+    StopSearch();
+    return;
   }
+
+  delegate_->GetSessionMetrics().UpdateSearchQuery(query);
+  SetActivePage(search_results_view_);
+  clear_results_timer_.Start(
+      FROM_HERE, kClearResultsTimeout,
+      base::BindOnce(&PickerView::OnClearResultsTimerFired,
+                     weak_ptr_factory_.GetWeakPtr()));
+  delegate_->StartEmojiSearch(query,
+                              base::BindOnce(&PickerView::PublishEmojiResults,
+                                             weak_ptr_factory_.GetWeakPtr()));
+  delegate_->StartSearch(query, selected_category_,
+                         base::BindRepeating(&PickerView::PublishSearchResults,
+                                             weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PickerView::StopSearch() {
+  delegate_->GetSessionMetrics().UpdateSearchQuery(u"");
+  if (selected_category_.has_value()) {
+    SetActivePage(category_results_view_);
+  } else {
+    SetActivePage(zero_state_view_);
+  }
+  delegate_->StopSearch();
+  clear_results_timer_.Stop();
+  search_results_view_->ClearSearchResults();
+  ResetEmojiBarToZeroState();
 }
 
 void PickerView::PublishEmojiResults(std::vector<PickerSearchResult> results) {
