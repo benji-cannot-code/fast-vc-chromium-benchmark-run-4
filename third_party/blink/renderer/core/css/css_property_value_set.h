1f9abeebe9a3cf805a993bcf7bed20ecbf65b80b
Kent Tamura <tkent@chromium.org>
2024-07-08 23:11:19 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to core/css/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I0dd593b8c08fd135086c9c15636b09b37be1d676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5681171
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324559}

--- third_party/blink/renderer/core/css/css_property_value_set.h
+++ third_party/blink/renderer/core/css/css_property_value_set.h
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_PROPERTY_VALUE_SET_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_CSS_CSS_PROPERTY_VALUE_SET_H_
 
--- third_party/blink/renderer/core/css/robin_hood_map-inl.h
+++ third_party/blink/renderer/core/css/robin_hood_map-inl.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // Inline definitions for robin_hood_map.h.
 
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_CSS_ROBIN_HOOD_MAP_INL_H_
