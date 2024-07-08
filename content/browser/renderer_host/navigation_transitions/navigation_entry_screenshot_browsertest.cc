2cfa7d590d4138a7b781da9c7475e01a91d51c7d
Khushal Sagar <khushalsagar@chromium.org>
2024-07-08 18:20:43 +0000

    content: Add fixes for back forward transitions with same-RFH navs
    This change fixes the following issues for same-RFH navigations and back
    forward visual transitions:
    
    1. The navigation screenshot cache assumes we never persist a screenshot
    for the currently committed navigation entry. This assumption is broken
    for same-RFH navigations where the screenshot request is sent at the
    same time as the commit IPC is sent to the renderer. If the GPU responds
    with the screenshot before the renderer responds with DidCommit, the
    screenshot is persisted before the navigation has been committed.
    
    Fix this by restructuring the code such that we defer caching the
    screenshot until the NavigationRequest is being destroyed.
    
    2. The SurfaceLayer for the outgoing page is cloned during DidCommit
    only for cross-RFH navs. This is currently not done for same-RFH navs
    and it would be too late in the navigation lifecycle for these navs. This
    is because the SurfaceID of the RFH is updated when sending the commit
    message for these navs.
    
    Fix the above by cloning the SurfaceLayer in ReadyToCommit.
    Note: The current code misses this for same-document navs which should
    be fixed separately.
    
    The browser test still forces RenderDocument on all frames, this is
    removed in the follow up patch.
    
    R=liuwilliam@chromium.org, rakina@chromium.org
    
    Cq-Include-Trybots: luci.chromium.try:android-10-arm64-rel,android-11-x86-rel,android-12-x64-dbg,android-12l-x64-dbg,android-12-x64-rel,android-13-x64-rel,android-14-x64-rel,android-15-x64-fyi-rel,android-15-x64-rel,android-arm-compile-dbg,android-arm64-all-targets-dbg,android-arm64-rel,android-bfcache-rel,android-cronet-arm64-rel,android-fieldtrial-rel,android-oreo-x86-rel,android-official,android-mte-arm64-rel,android-oreo-arm64-dbg,android-pie-arm64-dbg,android-pie-x86-rel,android-perfetto-rel,android-x64-rel,android-x86-rel,android_blink_rel
    
    Bug: 41491612
    Change-Id: I3d12457ba49e6fd6cbc073ebc360986e13bd3d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680502
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324372}

--- content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_browsertest.cc
+++ content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_browsertest.cc
@@ -333,9 +333,6 @@ class NavigationEntryScreenshotBrowserTest
       host_getter_ = std::make_unique<HostGetterCrossOrigin>();
     }
 
-    InitAndEnableRenderDocumentFeature(&scoped_feature_list_render_document_,
-                                       RenderDocumentFeatureFullyEnabled()[0]);
-
     NavigationEntryScreenshotBrowserTestBase::SetUpCommandLine(command_line);
   }
 
@@ -381,7 +378,6 @@ class NavigationEntryScreenshotBrowserTest
 
  private:
   base::test::ScopedFeatureList scoped_feature_list_;
-  base::test::ScopedFeatureList scoped_feature_list_render_document_;
 
   std::unique_ptr<HostGetter> host_getter_;
 };
