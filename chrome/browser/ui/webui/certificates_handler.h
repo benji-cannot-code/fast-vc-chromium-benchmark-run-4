3e9a313651e7ebc3050155ab38474f7d59480cdb
Elly <ellyjones@chromium.org>
2024-07-08 18:15:13 +0000

    ui: break CertificatesHandler dep on SelectFileDialog params
    This class was using the params field to pass through a state enum
    indicating what kind of operation is in progress, for use by the
    SelectFileDialog::Listener callbacks. This change has
    CertificatesHandler store that state in a field instead and pass nullptr
    for params to SelectFile().
    
    Bug: 340178601
    Change-Id: Ic94428319ca962f166f92953eb95d917bb1f2754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675749
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324367}

--- chrome/browser/ui/webui/certificates_handler.h
+++ chrome/browser/ui/webui/certificates_handler.h
@@ -74,10 +74,8 @@ class CertificatesHandler : public content::WebUIMessageHandler,
   void CertificatesRefreshed() override;
 
   // SelectFileDialog::Listener implementation.
-  void FileSelected(const ui::SelectedFileInfo& file,
-                    int index,
-                    void* params) override;
-  void FileSelectionCanceled(void* params) override;
+  void FileSelected(const ui::SelectedFileInfo& file, int index) override;
+  void FileSelectionCanceled() override;
 
 #if BUILDFLAG(IS_CHROMEOS)
   // Register profile preferences.
@@ -85,6 +83,13 @@ class CertificatesHandler : public content::WebUIMessageHandler,
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
  private:
+  enum PendingOperation {
+    EXPORT_PERSONAL_FILE,
+    IMPORT_PERSONAL_FILE,
+    IMPORT_SERVER_FILE,
+    IMPORT_CA_FILE,
+  };
+
   // View certificate.
   void HandleViewCertificate(const base::Value::List& args);
 
@@ -253,6 +258,7 @@ class CertificatesHandler : public content::WebUIMessageHandler,
   std::string file_data_;
   net::ScopedCERTCertificateList selected_cert_list_;
   scoped_refptr<ui::SelectFileDialog> select_file_dialog_;
+  std::optional<PendingOperation> pending_operation_;
   crypto::ScopedPK11Slot slot_;
 
   // Used in reading and writing certificate files.
