1e81d5c692e079fb2bfd841d7276482180bd1f16
Sophie Wen <sophiewen@google.com>
2024-07-08 17:57:43 +0000

    snap-groups: Add test util APIs
    No functional change. This adds the convenience API
    `UnionBoundsEqualToWorkAreaBounds(SnapGroup* snap_group)`.
    
    Test: ash_unittests
    Bug: b/350112441
    Change-Id: Ib92d27b6b43f46fd3eedd6aa8d1b34a17e4e62b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677647
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324354}

--- ash/wm/splitview/split_view_test_util.cc
+++ ash/wm/splitview/split_view_test_util.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_session.h"
 #include "ash/wm/overview/overview_test_util.h"
 #include "ash/wm/overview/overview_utils.h"
+#include "ash/wm/snap_group/snap_group.h"
 #include "ash/wm/splitview/split_view_controller.h"
 #include "ash/wm/splitview/split_view_divider.h"
 #include "ash/wm/splitview/split_view_overview_session.h"
@@ -160,6 +161,17 @@ void UnionBoundsEqualToWorkAreaBounds(aura::Window* w1,
   EXPECT_EQ(
       display::Screen::GetScreen()->GetDisplayNearestWindow(w1).work_area(),
       union_bounds);
+}
+
+void UnionBoundsEqualToWorkAreaBounds(SnapGroup* snap_group) {
+  aura::Window* w1 = snap_group->window1();
+  aura::Window* w2 = snap_group->window2();
+  auto* divider = snap_group->snap_group_divider();
+  if (IsPhysicallyLeftOrTop(w1)) {
+    UnionBoundsEqualToWorkAreaBounds(w1, w2, divider);
+  } else {
+    UnionBoundsEqualToWorkAreaBounds(w2, w1, divider);
+  }
 }
 
 }  // namespace ash
