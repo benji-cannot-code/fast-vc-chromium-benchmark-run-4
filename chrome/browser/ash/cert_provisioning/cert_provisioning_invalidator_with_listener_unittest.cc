c0f785ac176151388e4455e894239a5aed35817c
Artem Sumaneev <asumaneev@google.com>
2024-07-08 12:28:45 +0000

    [cert] Make CertProvisioningInvalidationHandler use either invalidation service or listener
    CertProvisioningInvalidationHandler can receive invalidation from either
    InvalidationService or InvalidationListener, whichever is provided in
    the variant.
    
    CertProvisioningUserInvalidator keeps initializing the handler with
    InvalidationService until we are able to create InvalidationListener.
    
    CertProvisioningDeviceInvalidatorFactory accepts either
    InvalidationListener and creates the invalidator and the handler when
    needed, or AffiliatedInvalidationServiceProvider, and creates
    the handler and the invalidator when InvalidationService becomes
    available in OnInvalidationServiceSet (as before).
    
    Bug: b:344536527
    Test: TBD
    Change-Id: I795ed90b1a47685fe59840be49d9316d5a679f0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630496
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Reviewed-by: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324160}

---
+++ chrome/browser/ash/cert_provisioning/cert_provisioning_invalidator_with_listener_unittest.cc
@@ -0,0 +1,8 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ash/cert_provisioning/cert_provisioning_invalidator.h"
+
+// TODO(b/344536527): Implement unit tests.
