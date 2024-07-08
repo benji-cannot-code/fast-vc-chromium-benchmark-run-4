3a0ae11ac29d1fac7798869285aa8e2f473e3f22
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-08 14:27:08 +0000

    Stop recording passphrase type before profile initialization
    Details in https://crbug.com/338027160#comment25 and
    http://go/chrome-data-support-chat/w7oofPCzPyE/w7oofPCzPyE?cls=10
    
    OBSOLETE_HISTOGRAM[Sync.PassphraseType3]=Replaced by Sync.PassphraseType4
    
    Bug: 338027160
    Change-Id: I2b9d231ef9c063815853c48b0035e518a8a98138
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680957
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324211}

--- components/sync/service/passphrase_type_metrics_provider.h
+++ components/sync/service/passphrase_type_metrics_provider.h
@@ -47,8 +47,10 @@ class PassphraseTypeMetricsProvider : public metrics::MetricsProvider {
       base::RepeatingCallback<std::vector<const SyncService*>()>;
 
   // All SyncServices returned by |get_all_sync_services_callback| must be not
-  // null.
-  explicit PassphraseTypeMetricsProvider(
+  // null. If `use_cached_passphrase_type` is true, the metric will be computed
+  // based on the passphrase type that's cached in sync prefs.
+  PassphraseTypeMetricsProvider(
+      bool use_cached_passphrase_type,
       const GetAllSyncServicesCallback& get_all_sync_services_callback);
 
   PassphraseTypeMetricsProvider(const PassphraseTypeMetricsProvider& other) =
@@ -62,6 +64,7 @@ class PassphraseTypeMetricsProvider : public metrics::MetricsProvider {
   bool ProvideHistograms() override;
 
  private:
+  const bool use_cached_passphrase_type_;
   const GetAllSyncServicesCallback get_all_sync_services_callback_;
 };
 
