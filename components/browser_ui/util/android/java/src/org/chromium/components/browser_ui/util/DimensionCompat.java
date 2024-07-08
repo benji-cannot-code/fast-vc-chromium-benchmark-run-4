c84da3490a97225b8d372b2bd7b33d9ec43793cb
Sirisha Kavuluru <skavuluru@google.com>
2024-07-08 19:49:38 +0000

    Use navigation bar height to compute tab grid height
    Before: https://screenshot.googleplex.com/3zcuWPkEd7FS3Ly
    After: https://screenshot.googleplex.com/4VxfKrNN63Ya24y
    
    On tablets we still seeing a bit of white gap even with nav bar height since browser control height excludes status bar height on tablets [1]. Added a TODO to fix this.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java;l=1109-1131;drc=197cb5f6af77ae63d1676eeb61a806a7fef06e6d;bpv=0;bpt=1
    
    
    Bug: 41496737
    Change-Id: If9253349337c4a5a2fd02a3aaf35e88af2fdf091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5668045
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324438}

--- components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/DimensionCompat.java
+++ components/browser_ui/util/android/java/src/org/chromium/components/browser_ui/util/DimensionCompat.java
@@ -39,10 +39,14 @@ public abstract class DimensionCompat {
     public abstract void updatePosition();
 
     /**
-     * Returns window height. In multi-window mode, returns the window height of the current app
-     * excluding the navigation bar. In non-MW mode, it includes the navigation bar height as well.
+     * Returns window height including all system bar areas. In multi-window mode, returns the
+     * window height of the current app excluding the navigation bar. In non-MW mode, it includes
+     * the navigation bar height as well.
      */
     public abstract @Px int getWindowHeight();
+
+    /** Returns window width */
+    public abstract @Px int getWindowWidth();
 
     /** Returns the status bar height */
     public abstract @Px int getStatusbarHeight();
@@ -66,6 +70,12 @@ public abstract class DimensionCompat {
         @Px
         public int getWindowHeight() {
             return mActivity.getWindowManager().getCurrentWindowMetrics().getBounds().height();
+        }
+
+        @Override
+        @Px
+        public int getWindowWidth() {
+            return mActivity.getWindowManager().getCurrentWindowMetrics().getBounds().width();
         }
 
         @Override
@@ -107,7 +117,6 @@ public abstract class DimensionCompat {
             // On pre-R devices, We wait till the layout is complete and get the content
             // |android.R.id.content| view height. See |getAppUsableScreenHeightFromContent|.
             View contentFrame = mActivity.findViewById(android.R.id.content);
-
             // Maybe invoked before layout inflation? Simply return here - position update will be
             // attempted later again by |onPostInflationStartUp|.
             if (contentFrame == null) return;
@@ -131,26 +140,16 @@ public abstract class DimensionCompat {
                     });
         }
 
-        // TODO(jinsukkim): Explore the way to use androidx.window.WindowManager or
-        // androidx.window.java.WindowInfoRepoJavaAdapter once the androidx API get finalized and is
-        // available in Chromium to use #getCurrentWindowMetrics()/#currentWindowMetrics() to get
-        // the height of the display our Window currently in.
-        //
-        // The #getRealMetrics() method will give the physical size of the screen, which is
-        // generally fine when the app is not in multi-window mode and #getMetrics() will give the
-        // height excludes the decor views, so not suitable for our case. But in multi-window mode,
-        // we have no much choice, the closest way is to use #getMetrics() method, because we need
-        // to handle rotation.
         @Override
         @Px
         public int getWindowHeight() {
-            DisplayMetrics displayMetrics = new DisplayMetrics();
-            if (ApiCompatibilityUtils.isInMultiWindowMode(mActivity)) {
-                mActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-            } else {
-                mActivity.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
-            }
-            return displayMetrics.heightPixels;
+            return getDisplayMetrics().heightPixels;
+        }
+
+        @Override
+        @Px
+        public int getWindowWidth() {
+            return getDisplayMetrics().widthPixels;
         }
 
         @Override
@@ -183,6 +182,26 @@ public abstract class DimensionCompat {
                     - Math.max(
                             getAppUsableScreenHeightFromContent(),
                             getAppUsableScreenHeightFromDisplay());
+        }
+
+        // TODO(jinsukkim): Explore the way to use androidx.window.WindowManager or
+        // androidx.window.java.WindowInfoRepoJavaAdapter once the androidx API get finalized and is
+        // available in Chromium to use #getCurrentWindowMetrics()/#currentWindowMetrics() to get
+        // the height of the display our Window currently in.
+        //
+        // The #getRealMetrics() method will give the physical size of the screen, which is
+        // generally fine when the app is not in multi-window mode and #getMetrics() will give the
+        // height excludes the decor views, so not suitable for our case. But in multi-window mode,
+        // we have no much choice, the closest way is to use #getMetrics() method, because we need
+        // to handle rotation.
+        private DisplayMetrics getDisplayMetrics() {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            if (ApiCompatibilityUtils.isInMultiWindowMode(mActivity)) {
+                mActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            } else {
+                mActivity.getWindowManager().getDefaultDisplay().getRealMetrics(displayMetrics);
+            }
+            return displayMetrics;
         }
 
         private int getAppUsableScreenHeightFromContent() {
