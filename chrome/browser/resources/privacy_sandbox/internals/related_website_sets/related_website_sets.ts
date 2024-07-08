2d6703d2776fbace4cc1ef720c7746d7d3212ae4
Gabriel Tellez Ornelas <gtellezornelas@google.com>
2024-07-08 17:56:02 +0000

    Develop Card Component with expandable rows for Related Website Sets Dev UI
    This sets up the card component with expandable rows using webUI and lit
    
    Light Mode:
    collapsed: https://screenshot.googleplex.com/A44xPJLEtgkFgce.png
    expanded: https://screenshot.googleplex.com/5RZrDaT5535CB49.png
    
    Dark Mode:
    collapsed: https://screenshot.googleplex.com/7D8Xgap9TmwMFCL.png
    expanded: https://screenshot.googleplex.com/8n7WvQdmyBnMow7.png
    
    Bug: 330877132
    Change-Id: Ie975e994f52d9022a26f0598f4b473b64801e358
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5626595
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Commit-Queue: Gabriel Tellez Ornelas <gtellezornelas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324353}

--- chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
+++ chrome/browser/resources/privacy_sandbox/internals/related_website_sets/related_website_sets.ts
@@ -6,5 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './app.js';
 
 export type {RelatedWebsiteSetsAppElement} from './app.js';
+export {SiteType} from './related_website_set_list_item.js';
+export type {MemberSite, RelatedWebsiteSetListItemElement} from './related_website_set_list_item.js';
+export type {RelatedWebsiteSet, RelatedWebsiteSetsListContainerElement} from './related_website_sets_list_container.js';
 export type {RelatedWebsiteSetsSidebarElement} from './sidebar.js';
+export type {SiteFaviconElement} from './site_favicon.js';
 export type {RelatedWebsiteSetsToolbarElement} from './toolbar.js';
