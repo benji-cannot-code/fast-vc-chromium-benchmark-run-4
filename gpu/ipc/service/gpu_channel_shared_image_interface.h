c5c9567f74a903b9ed895566d941b7bbef9cabc8
Mingjing Zhang <mjzhang@chromium.org>
2024-07-08 20:54:16 +0000

    Use ClientSharedImage for D3D11 video
    As part of the ClientSharedImage refactoring, this CL allows
    DefaultTexture2DWrapper to hold a ClientSharedImage, and adds new code
    paths for creating, storing and utilizing the ClientSharedImage.
    
    This CL also adds a base::Feature that controls whether to use the
    new ClientSharedImage code logic. The feature is disabled in the
    current CL and will be eanbled in a follow-up CL.
    
    Bug: 1494911
    Change-Id: I6ced7da18b5bf62f6695f3d0aaf628d4a0fc5aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679226
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324481}

--- gpu/ipc/service/gpu_channel_shared_image_interface.h
+++ gpu/ipc/service/gpu_channel_shared_image_interface.h
@@ -18,6 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/ipc/service/shared_image_stub.h"
 #include "ui/gfx/gpu_memory_buffer.h"
 
+#if BUILDFLAG(IS_WIN)
+#include <d3d11.h>
+#include <wrl/client.h>
+
+#include "gpu/command_buffer/service/dxgi_shared_handle_manager.h"
+#include "gpu/command_buffer/service/shared_image/d3d_image_backing.h"
+#endif
+
 namespace base {
 class WaitableEvent;
 }
@@ -110,6 +118,15 @@ class GPU_IPC_SERVICE_EXPORT GpuChannelSharedImageInterface
       scoped_refptr<StreamTextureSharedImageInterface> image,
       scoped_refptr<RefCountedLock> drdc_lock);
 #endif
+
+#if BUILDFLAG(IS_WIN)
+  scoped_refptr<ClientSharedImage> CreateSharedImageForD3D11Video(
+      const SharedImageInfo& si_info,
+      Microsoft::WRL::ComPtr<ID3D11Texture2D> texture,
+      scoped_refptr<gpu::DXGISharedHandleState> dxgi_shared_handle_state,
+      size_t array_slice);
+#endif
+
   SequenceId sequence() { return sequence_; }
   scoped_refptr<gpu::SyncPointClientState> sync_point_client_state() {
     return sync_point_client_state_;
