1aa28e44f07b1221371729453e7398f4c69762fa
Andre Le <leandre@chromium.org>
2024-07-08 21:55:53 +0000

    MagicBoost: Add a capability to reset consent status in tester page
    Add a button to reset HMR consent status in
    chrome://status-area-internals. This is used as a helper to test the
    opt-in flow on a device.
    
    Bug: b:327783843
    Change-Id: Idd1d6da25db73a0c3aafd0b32d9c52ba04a0ed3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5683492
    Reviewed-by: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324519}

--- ash/webui/status_area_internals/resources/status_area_internals.ts
+++ ash/webui/status_area_internals/resources/status_area_internals.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/ash/common/cr_elements/cr_button/cr_button.js';
 import 'chrome://resources/ash/common/cr_elements/cr_toggle/cr_toggle.js';
 import './privacy_indicator_app_manager.js';
 
@@ -84,6 +85,12 @@ export class StatusAreaInternalsElement extends PolymerElement {
 
     const toggled = e.detail;
     pageHandler.setIsInUserChildSession(toggled);
+  }
+
+  onHmrConsentStatusReset(e: CustomEvent<boolean>) {
+    e.stopPropagation();
+
+    pageHandler.resetHmrConsentStatus();
   }
 }
 
--- ash/webui/status_area_internals/status_area_internals_handler.cc
+++ ash/webui/status_area_internals/status_area_internals_handler.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/video_conference/video_conference_tray_controller.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/components/magic_boost/public/cpp/magic_boost_state.h"
 #include "components/prefs/pref_service.h"
 
 namespace ash {
@@ -127,6 +128,11 @@ void StatusAreaInternalsHandler::SetIsInUserChildSession(
     bool in_child_session) {
   scoped_fake_model_->fake_model()->set_is_in_user_child_session(
       in_child_session);
+}
+
+void StatusAreaInternalsHandler::ResetHmrConsentStatus() {
+  chromeos::MagicBoostState::Get()->AsyncWriteConsentStatus(
+      chromeos::HMRConsentStatus::kUnset);
 }
 
 }  // namespace ash
