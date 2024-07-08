e157cc345a7286350b7beadfc8091d674cec7960
Avi Drissman <avi@chromium.org>
2024-07-08 17:40:43 +0000

    Use inline constexpr for constant strings in components/history_clusters
    This allows for better optimization and use of compile-time tools.
    
    Bug: 350944663
    Change-Id: I8a3e944de4587a7561dae6840ed6a8aff83ced14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677784
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324340}

--- components/history_clusters/history_clusters_internals/webui/url_constants.h
+++ components/history_clusters/history_clusters_internals/webui/url_constants.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace history_clusters_internals {
 
 // The host of the history clusters internals page URL.
-extern const char kChromeUIHistoryClustersInternalsHost[];
+inline constexpr char kChromeUIHistoryClustersInternalsHost[] =
+    "history-clusters-internals";
 
 }  // namespace history_clusters_internals
 
