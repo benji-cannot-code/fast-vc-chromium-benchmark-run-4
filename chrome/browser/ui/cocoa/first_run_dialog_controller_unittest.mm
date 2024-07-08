0ca88447d281dc31ccbe5d0f01accd959be5a713
Evan Stade <estade@chromium.org>
2024-07-08 18:16:07 +0000

    Remove layer of indirection in first run dialog
    Bug: 40463534
    Change-Id: I5b53cc4156a967992244155639c51c1ed68fe616
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677650
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324368}

--- chrome/browser/ui/cocoa/first_run_dialog_controller_unittest.mm
+++ chrome/browser/ui/cocoa/first_run_dialog_controller_unittest.mm
@@ -14,11 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using FirstRunDialogControllerTest = CocoaTest;
 
-FirstRunDialogViewController* MakeTestController(BOOL stats) {
-  return [[FirstRunDialogViewController alloc]
-      initWithStatsCheckboxInitiallyChecked:stats];
-}
-
 NSView* FindBrowserButton(NSView* view) {
   for (NSView* subview : [view subviews]) {
     if (![subview isKindOfClass:[NSButton class]]) {
@@ -33,19 +28,22 @@ NSView* FindBrowserButton(NSView* view) {
 }
 
 TEST(FirstRunDialogControllerTest, SetStatsDefault) {
-  FirstRunDialogViewController* controller = MakeTestController(YES);
+  FirstRunDialogViewController* controller =
+      [[FirstRunDialogViewController alloc] init];
   [controller view];  // Make sure view is actually loaded.
   EXPECT_TRUE([controller isStatsReportingEnabled]);
 }
 
 TEST(FirstRunDialogControllerTest, MakeDefaultBrowserDefault) {
-  FirstRunDialogViewController* controller = MakeTestController(YES);
+  FirstRunDialogViewController* controller =
+      [[FirstRunDialogViewController alloc] init];
   [controller view];
   EXPECT_TRUE([controller isMakeDefaultBrowserEnabled]);
 }
 
 TEST(FirstRunDialogControllerTest, ShowBrowser) {
-  FirstRunDialogViewController* controller = MakeTestController(YES);
+  FirstRunDialogViewController* controller =
+      [[FirstRunDialogViewController alloc] init];
   NSView* checkbox = FindBrowserButton([controller view]);
   EXPECT_FALSE(checkbox.hidden);
 }
@@ -54,12 +52,14 @@ TEST(FirstRunDialogControllerTest, LayoutWithLongStrings) {
   // It's necessary to call |view| on the controller before mangling the
   // strings, since otherwise the controller will lazily construct its view,
   // which might happen after the call to |set_mangle_localized_strings|.
-  FirstRunDialogViewController* defaultController = MakeTestController(YES);
+  FirstRunDialogViewController* defaultController =
+      [[FirstRunDialogViewController alloc] init];
   NSView* defaultView = [defaultController view];
 
   ui::ResourceBundle::GetSharedInstance().set_mangle_localized_strings_for_test(
       true);
-  FirstRunDialogViewController* longController = MakeTestController(YES);
+  FirstRunDialogViewController* longController =
+      [[FirstRunDialogViewController alloc] init];
   NSView* longView = [longController view];
 
   // Ensure that the mangled strings actually do change the height!
