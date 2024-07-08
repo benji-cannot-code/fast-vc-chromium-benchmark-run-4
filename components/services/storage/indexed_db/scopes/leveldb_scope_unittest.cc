60647e66f27dc9eec9d69fd7c7344898b6ca6ccc
Evan Stade <estade@chromium.org>
2024-07-08 16:49:26 +0000

    IDB: prospective fix for crash in IndexedDBTransaction::Abort()
    See bug for description. This also contains a cleanup of an unused
    member var.
    
    Bug: 350196532
    Change-Id: Ie3def2c80cb8a3a8a10b0885934435e3876d075d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677097
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324300}

--- components/services/storage/indexed_db/scopes/leveldb_scope_unittest.cc
+++ components/services/storage/indexed_db/scopes/leveldb_scope_unittest.cc
@@ -566,6 +566,9 @@ TEST_F(LevelDBScopeTest, BrokenDBForRevert) {
   std::move(break_db).Run(error);
   scope.reset();
 
+  // Wait until revert task reports failure.
+  task_env_.RunUntilIdle();
+
   EXPECT_FALSE(failure_status.ok());
   EXPECT_EQ(failure_status.ToString(), error.ToString());
 }
