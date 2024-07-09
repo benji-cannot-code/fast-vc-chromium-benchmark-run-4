925c810edeb9b7202c9b9f0d3fffd62472f90f67
Alex Gough <ajgo@chromium.org>
2024-07-09 05:39:02 +0000

    Turn iterator .end() DCHECK() into CHECK() for printing
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    
    For this CL instances were located with a weggli query
    of `DCHECK(_ != _.end());` and some filtering of paths.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: 351745839
    Change-Id: I93a2331bf04515c2b8db77fc8acf34a15d2d9a4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685184
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324677}

--- printing/metafile_skia.cc
+++ printing/metafile_skia.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
+#include "base/not_fatal_until.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/time/time.h"
 #include "base/unguessable_token.h"
@@ -456,7 +457,7 @@ void MetafileSkia::CustomDataToSkPictureCallback(SkCanvas* canvas,
     return;
 
   auto it = data_->subframe_pics.find(content_id);
-  DCHECK(it != data_->subframe_pics.end());
+  CHECK(it != data_->subframe_pics.end(), base::NotFatalUntil::M130);
 
   // Found the picture, draw it on canvas.
   sk_sp<SkPicture> pic = it->second;
