7b5b640959244ddc0141f11f54ed426283edc736
Rupert Wiser <bewise@chromium.org>
2024-07-09 09:08:17 +0000

    Revert "[Default Nav Transition] Remove top controls in NTP screenshot"
    This reverts commit 7938c61cf27787612c36688e9ebe841c2050f1a8.
    
    Reason for revert: Causing bots to fail on CI: https://crbug.com/351979006
    
    Original change's description:
    > [Default Nav Transition] Remove top controls in NTP screenshot
    >
    > In order to match the viewport of the webpage screenshot, this CL
    > excludes the area of top controls of NTP and maintain the
    > size of the screenshot. As a result, the bottom part of the screenshot
    > will be filled with the default background color of the NTP because
    > it is impossible to draw off-screen views in Android.
    >
    > Screenshot (with top controls): https://screenshot.googleplex.com/AJFcDYvXCbnvXgj.png
    > Screen record (w/o top controls): http://screencast/cast/NDY4NTA0OTc2ODE4MTc2MHxjODkyNWE0Zi1jOQ
    >
    > Bug: 331814846
    > Change-Id: Ieb8959ad2f202b15e055346f889a73355cdc4e06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560151
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > Commit-Queue: Lijin Shen <lazzzis@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1324606}
    
    Bug: 331814846
    Change-Id: Idee7cef1905d173ae74233fb5aa566e58f070691
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5687334
    Owners-Override: Rupert Wiser <bewise@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324742}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
@@ -46,12 +46,10 @@ import org.chromium.chrome.browser.ui.native_page.BasicSmoothTransitionDelegate;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
-import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.back_forward_transition.AnimationStage;
 import org.chromium.content_public.browser.test.util.TestThreadUtils;
-import org.chromium.content_public.browser.test.util.UiUtils;
 import org.chromium.content_public.common.ContentUrlConstants;
 import org.chromium.net.test.EmbeddedTestServer;
 import org.chromium.ui.base.PageTransition;
@@ -69,7 +67,6 @@ import java.util.concurrent.TimeoutException;
         message = "crbug.com/1276402 crbug.com/345352689")
 public class NavigationHandlerTest {
     private static final String RENDERED_PAGE = "/chrome/test/data/android/navigate/simple.html";
-    private static final String TEST_PAGE = "/chrome/test/data/android/test.html";
     private static final boolean LEFT_EDGE = true;
     private static final boolean RIGHT_EDGE = false;
 
@@ -84,10 +81,7 @@ public class NavigationHandlerTest {
 
     @Before
     public void setUp() throws InterruptedException {
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
-        mActivityTestRule.startMainActivityWithURL(mTestServer.getURL(TEST_PAGE));
+        mActivityTestRule.startMainActivityOnBlankPage();
         CompositorAnimationHandler.setTestingMode(true);
         mNavUtils = new GestureNavigationUtils(mActivityTestRule);
         mNavigationHandler = mNavUtils.getNavigationHandler();
@@ -128,12 +122,12 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.BACK_FORWARD_TRANSITIONS})
-    public void testSwipeBackToNTPWithTransition() throws InterruptedException {
-        UiUtils.settleDownUI(InstrumentationRegistry.getInstrumentation());
+    public void testSwipeBackToNTPWithTransition() {
         final Tab tab = mActivityTestRule.getActivity().getActivityTab();
         mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
-        UiUtils.settleDownUI(InstrumentationRegistry.getInstrumentation());
-        NewTabPageTestUtils.waitForNtpLoaded(mActivityTestRule.getActivity().getActivityTab());
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
 
         mNavUtils.swipeFromEdgeAndHold(true);
@@ -210,6 +204,9 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testReleaseGlowWithoutPrecedingPullIgnored() {
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -272,6 +269,9 @@ public class NavigationHandlerTest {
     public void testSwipeNavigateOnRenderedPage() {
         // TODO(crbug.com/40899221): Write a test variation running with
         //     ChromeFeatureList.BACK_FORWARD_TRANSITIONS enabled when the feature is completed.
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         mActivityTestRule.loadUrl(ContentUrlConstants.ABOUT_BLANK_DISPLAY_URL);
 
@@ -326,6 +326,9 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterDestroy() {
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(mNavigationHandler::destroy);
 
@@ -345,6 +348,9 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterTabDestroy() {
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(currentTab()::destroy);
 
@@ -358,6 +364,9 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterDestroyActivity_NativePage() {
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
         TestThreadUtils.runOnUiThreadBlocking(mActivityTestRule.getActivity()::finish);
 
