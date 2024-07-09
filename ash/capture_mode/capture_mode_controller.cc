d3e8d6f8909e1c7db9c0a8cfaf00f73a85ca17c5
Sergey Poromov <poromov@chromium.org>
2024-07-09 17:07:59 +0000

    [skyvault] Upload ScreenCaptures to OneDrive using CloudUpload flow.
    As the current solution with uploading ScreenCaptures using Fusebox
    is not stable, doesn't work for video recordings and is not able to
    show required UI, we migrate to more commonly used and supported
    CloudUpload flow - first file is saved locally to a temporary location
    and then is finalized by moving to the cloud.
    
    Bug: b/348177020
    Test: out/Default/ash_unittests --gtest_filter=*Capture* && out/Default/browser_tests --gtest_filter=*CaptureMode*
    Change-Id: Ia4a1da086e14aac7a8f6a20c03cc734976048aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5640062
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Aya Elsayed <ayaelattar@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324885}

--- ash/capture_mode/capture_mode_controller.cc
+++ ash/capture_mode/capture_mode_controller.cc
@@ -1572,7 +1572,7 @@ void CaptureModeController::OnImageCaptured(
   }
   blocking_task_runner_->PostTaskAndReplyWithResult(
       FROM_HERE,
-      base::BindOnce(&SaveFile, png_bytes, path,
+      base::BindOnce(&SaveFile, png_bytes, delegate_->RedirectFilePath(path),
                      GetFallbackFilePathFromFile(path)),
       base::BindOnce(&CaptureModeController::OnImageFileSaved,
                      weak_ptr_factory_.GetWeakPtr(), png_bytes, behavior));
@@ -1583,6 +1583,22 @@ void CaptureModeController::OnImageFileSaved(
     const CaptureModeBehavior* behavior,
     const base::FilePath& file_saved_path) {
   if (file_saved_path.empty()) {
+    OnImageFileFinalized(png_bytes, behavior, /*success=*/false,
+                         file_saved_path);
+    return;
+  }
+  delegate_->FinalizeSavedFile(
+      base::BindOnce(&CaptureModeController::OnImageFileFinalized,
+                     weak_ptr_factory_.GetWeakPtr(), png_bytes, behavior),
+      file_saved_path);
+}
+
+void CaptureModeController::OnImageFileFinalized(
+    scoped_refptr<base::RefCountedMemory> png_bytes,
+    const CaptureModeBehavior* behavior,
+    bool success,
+    const base::FilePath& file_saved_path) {
+  if (!success) {
     ShowFailureNotification();
     return;
   }
@@ -1606,10 +1622,10 @@ void CaptureModeController::OnImageFileSaved(
 }
 
 void CaptureModeController::OnVideoFileSaved(
-    const base::FilePath& saved_video_file_path,
     const gfx::ImageSkia& video_thumbnail,
+    const CaptureModeBehavior* behavior,
     bool success,
-    const CaptureModeBehavior* behavior) {
+    const base::FilePath& saved_video_file_path) {
   DCHECK(base::CurrentUIThread::IsSet());
 
   if (!success) {
@@ -2077,7 +2093,8 @@ void CaptureModeController::OnDlpRestrictionCheckedAtCountDownFinished(
   if (!GetCurrentCaptureFolder().is_default_downloads_folder) {
     blocking_task_runner_->PostTaskAndReplyWithResult(
         FROM_HERE,
-        base::BindOnce(&SelectFilePathForCapturedFile, current_path,
+        base::BindOnce(&SelectFilePathForCapturedFile,
+                       delegate_->RedirectFilePath(current_path),
                        GetFallbackFilePathFromFile(current_path)),
         base::BindOnce(&CaptureModeController::BeginVideoRecording,
                        weak_ptr_factory_.GetWeakPtr(), *capture_params));
@@ -2164,10 +2181,28 @@ void CaptureModeController::OnDlpRestrictionCheckedAtVideoEnd(
 
     DeleteFileAsync(blocking_task_runner_, video_file_path,
                     std::move(on_file_deleted_callback_for_test_));
+    OnVideoFileFinalized(/*should_delete_file=*/true, video_thumbnail);
   } else {
-    OnVideoFileSaved(video_file_path, video_thumbnail, success, behavior);
+    if (!success) {
+      OnVideoFileSaved(video_thumbnail, behavior, success, video_file_path);
+      OnVideoFileFinalized(/*should_delete_file=*/false, video_thumbnail);
+      return;
+    }
+    delegate_->FinalizeSavedFile(
+        base::BindOnce(&CaptureModeController::OnVideoFileSaved,
+                       weak_ptr_factory_.GetWeakPtr(), video_thumbnail,
+                       behavior)
+            .Then(base::BindOnce(&CaptureModeController::OnVideoFileFinalized,
+                                 weak_ptr_factory_.GetWeakPtr(),
+                                 /*should_delete_file=*/false,
+                                 video_thumbnail)),
+        video_file_path);
   }
+}
 
+void CaptureModeController::OnVideoFileFinalized(
+    bool should_delete_file,
+    const gfx::ImageSkia& video_thumbnail) {
   low_disk_space_threshold_reached_ = false;
   recording_start_time_ = base::TimeTicks();
 
--- chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.h
+++ chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/public/cpp/capture_mode/capture_mode_delegate.h"
 #include "base/files/file_path.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/functional/callback.h"
 #include "base/memory/weak_ptr.h"
 #include "chromeos/ash/components/drivefs/mojom/drivefs.mojom-forward.h"
@@ -83,6 +84,10 @@ class ChromeCaptureModeDelegate : public ash::CaptureModeDelegate {
       crosapi::mojom::VideoConferenceMediaUsageStatusPtr status) override;
   void NotifyDeviceUsedWhileDisabled(
       crosapi::mojom::VideoConferenceMediaDevice device) override;
+  void FinalizeSavedFile(
+      base::OnceCallback<void(bool, const base::FilePath&)> callback,
+      const base::FilePath& path) override;
+  base::FilePath RedirectFilePath(const base::FilePath& path) override;
 
  private:
   // Called back by the Drive integration service when the quota usage is
@@ -90,6 +95,9 @@ class ChromeCaptureModeDelegate : public ash::CaptureModeDelegate {
   void OnGetDriveQuotaUsage(ash::OnGotDriveFsFreeSpace callback,
                             drive::FileError error,
                             drivefs::mojom::QuotaUsagePtr usage);
+
+  // Called back once temporary directory for OneDrive is created.
+  void SetOdfsTempDir(base::ScopedTempDir temp_dir);
 
   // Used to temporarily disable capture mode in certain cases for which neither
   // a device policy, nor DLP will be triggered. For example, Some extension
@@ -105,6 +113,10 @@ class ChromeCaptureModeDelegate : public ash::CaptureModeDelegate {
 
   // True when a capture mode session is currently active.
   bool is_session_active_ = false;
+
+  // Temporary directory to which files will be redirected before being uploaded
+  // to OneDrive cloud. Created and destructed asynchronously.
+  base::ScopedTempDir odfs_temp_dir_;
 
   base::WeakPtrFactory<ChromeCaptureModeDelegate> weak_ptr_factory_{this};
 };
