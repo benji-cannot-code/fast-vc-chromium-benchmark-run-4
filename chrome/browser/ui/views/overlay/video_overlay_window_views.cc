a9646ef6f0cac016034abe81a2caed5e45af8858
Qianqian Lin <qilin@microsoft.com>
2024-07-09 19:56:30 +0000

    Set input scope as private or default
    Set private input scope on the top level incognito windows to
    avoid using the data in Windows text input intelligence features.
    Set non incognito windows with IS_DEFAULT so Windows knows they
    are non private windows with our changes in chromium but not
    private windows in earlier chromium without our changes. This
    is a reland for crrev.com/c/5112656, crrev.com/c/5324681 and
    crrev.com/c/5384598. We need to reland the change to avoid
    breaking privacy requirements in Windows Textinput
    Intelligence. The original changes caused a regression
    in Google Slides, which is caused by a timing issue related to
    a focus change triggered event. We moved the change to a place
    it will happen for a hwnd only once after it's created before
    any focus change.
    
    Bug: 1510772
    Change-Id: I98771d0a3332137273d68e1b3b58b565e4e2ca46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659464
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Qianqian Lin <qilin@microsoft.com>
    Reviewed-by: Sam Fortiner <samfort@microsoft.com>
    Reviewed-by: Yohei Yukawa <yukawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325071}

--- chrome/browser/ui/views/overlay/video_overlay_window_views.cc
+++ chrome/browser/ui/views/overlay/video_overlay_window_views.cc
@@ -63,8 +63,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/shell_integration_win.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/base/ime/text_input_client.h"
+#include "ui/base/ime/win/tsf_input_scope.h"
 #include "ui/base/win/shell.h"
 #endif
 
@@ -329,6 +332,19 @@ std::unique_ptr<VideoOverlayWindowViews> VideoOverlayWindowViews::Create(
           overlay_window->GetNativeWindow()->GetHost()->GetAcceleratedWidget());
     }
   }
+
+  InputScope input_scope = overlay_window->GetController()
+                                   ->GetWebContents()
+                                   ->GetRenderWidgetHostView()
+                                   ->GetTextInputClient()
+                                   ->ShouldDoLearning()
+                               ? IS_DEFAULT
+                               : IS_PRIVATE;
+
+  ui::tsf_inputscope::SetInputScope(
+      overlay_window->GetNativeWindow()->GetHost()->GetAcceleratedWidget(),
+      input_scope);
+
 #endif  // BUILDFLAG(IS_WIN)
 
   PictureInPictureOcclusionTracker* tracker =
--- ui/base/ime/win/tsf_input_scope.cc
+++ ui/base/ime/win/tsf_input_scope.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/containers/contains.h"
 #include "base/task/current_thread.h"
+#include "base/trace_event/trace_event.h"
 
 namespace ui::tsf_inputscope {
 namespace {
@@ -171,6 +172,35 @@ ITfInputScope* CreateInputScope(TextInputType text_input_type,
     input_scopes = GetInputScopes(text_input_type, text_input_mode);
   }
   return new TSFInputScope(input_scopes);
+}
+
+typedef HRESULT(WINAPI* SetInputScopeFunc)(HWND window_handle,
+                                           InputScope input_scope);
+
+SetInputScopeFunc g_set_input_scope = NULL;
+bool g_get_set_input_scope_done = false;
+
+void SetInputScope(HWND window_handle, InputScope input_scope) {
+  CHECK(base::CurrentUIThread::IsSet());
+  // Thread safety is not required because this function is under UI thread.
+  if (!g_get_set_input_scope_done) {
+    g_get_set_input_scope_done = true;
+
+    HMODULE module = NULL;
+    if (GetModuleHandleExW(GET_MODULE_HANDLE_EX_FLAG_PIN, L"msctf.dll",
+                           &module)) {
+      g_set_input_scope = reinterpret_cast<SetInputScopeFunc>(
+          GetProcAddress(module, "SetInputScope"));
+    }
+  }
+
+  if (g_set_input_scope) {
+    HRESULT hr = g_set_input_scope(window_handle, input_scope);
+    if (hr != S_OK) {
+      TRACE_EVENT2("ime", "SetInputScope", "input_scope", input_scope, "hr",
+                   hr);
+    }
+  }
 }
 
 }  // namespace ui::tsf_inputscope
