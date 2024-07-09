8c4e43b3d3a21affd1a4173ac1271c53ad1b2d39
Alex Gough <ajgo@chromium.org>
2024-07-09 20:22:00 +0000

    Turn iterator $var!=.end() DCHECK() into CHECK() for device
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
          'DCHECK($var != _.end());' \
       -p 'DCHECK(_.end() != $var);' \
       -p 'DCHECK_NE($var, _.end());' \
       -p 'DCHECK_NE(_.end(), $var);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=chengweih@chromium.org
    
    Bug: 351745839
    Change-Id: I2c13ce3b5a730244a2821252bb92f0013675ac0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689790
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Jack Hsieh <chengweih@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325093}

--- device/vr/android/arcore/ar_compositor_frame_sink.cc
+++ device/vr/android/arcore/ar_compositor_frame_sink.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/raw_ptr.h"
+#include "base/not_fatal_until.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/single_thread_task_runner.h"
 #include "components/viz/common/features.h"
@@ -254,7 +255,7 @@ void ArCompositorFrameSink::ReclaimResources(
       continue;
 
     auto it = id_to_frame_map_.find(resource.id);
-    DCHECK(it != id_to_frame_map_.end());
+    CHECK(it != id_to_frame_map_.end(), base::NotFatalUntil::M130);
     auto* rendering_frame = it->second;
 
     // While we now know that this resource is associated with this frame, we
