fc6845685d70b838088dc4b26ce6c69acaed0f23
Alex Gough <ajgo@chromium.org>
2024-07-09 21:45:56 +0000

    Turn iterator .end() DCHECK() into CHECK() for fuchsia_web
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    
    For this CL instances were located with a weggli query
    of `DCHECK(_ != _.end());` and some filtering of paths.
    
    This CL was uploaded by git cl split.
    
    R=sergeyu@chromium.org
    
    Bug: 351745839
    Change-Id: I0d0ba5c1e0ea8811ee76639dc057365bb45e23f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685395
    Reviewed-by: Zijie He <zijiehe@google.com>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325153}

--- fuchsia_web/runners/cast/cast_runner.cc
+++ fuchsia_web/runners/cast/cast_runner.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuchsia/web/cpp/fidl.h>
 #include <lib/fit/function.h>
+
 #include <memory>
 #include <string>
 #include <utility>
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/process_context.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
+#include "base/not_fatal_until.h"
 #include "base/process/process.h"
 #include "base/strings/strcat.h"
 #include "base/time/time.h"
@@ -456,7 +458,7 @@ WebContentRunner* CastRunner::CreateIsolatedRunner(
 
 void CastRunner::OnIsolatedContextEmpty(WebContentRunner* context) {
   auto it = isolated_contexts_.find(context);
-  DCHECK(it != isolated_contexts_.end());
+  CHECK(it != isolated_contexts_.end(), base::NotFatalUntil::M130);
   isolated_contexts_.erase(it);
 }
 
--- fuchsia_web/webengine/browser/context_impl.cc
+++ fuchsia_web/webengine/browser/context_impl.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <lib/fpromise/result.h>
 #include <lib/zx/channel.h>
 #include <lib/zx/handle.h>
+
 #include <memory>
 #include <utility>
 
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/fuchsia/koid.h"
 #include "base/fuchsia/mem_buffer_util.h"
 #include "base/functional/bind.h"
+#include "base/not_fatal_until.h"
 #include "base/strings/stringprintf.h"
 #include "base/trace_event/trace_event.h"
 #include "base/trace_event/typed_macros.h"
@@ -57,7 +59,7 @@ ContextImpl::~ContextImpl() {
 
 void ContextImpl::DestroyFrame(FrameImpl* frame) {
   auto iter = frames_.find(frame);
-  DCHECK(iter != frames_.end());
+  CHECK(iter != frames_.end(), base::NotFatalUntil::M130);
   frames_.erase(iter);
 }
 
