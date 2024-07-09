5a0983ed77d22a02dda3429a432317e01a371bed
Sophie Wen <sophiewen@google.com>
2024-07-09 20:10:34 +0000

    snap-groups: Move SnapGroupWorkspaceWindowResizerTest*
    No functional change. This moves SnapGroupWorkspaceWindowResizerTest*
    to SnapGroupPhantomBoundsTest* and updates them to use
    `EventGenerator::Drag()` instead of `CreateWindowResizer()`.
    
    Test: ash_unittests
    Bug: b:349892846
    Change-Id: Iec64f960f970516ccebec205e12781b9ab62587f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686152
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325078}

--- ash/utility/haptics_util_unittest.cc
+++ ash/utility/haptics_util_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/overview/overview_grid.h"
 #include "ash/wm/overview/overview_item.h"
 #include "ash/wm/overview/overview_session.h"
-#include "ash/wm/workspace/workspace_window_resizer.h"
+#include "ash/wm/workspace/workspace_window_resizer_test_api.h"
 #include "chromeos/ui/frame/multitask_menu/multitask_button.h"
 #include "chromeos/ui/frame/multitask_menu/multitask_menu_view_test_api.h"
 #include "chromeos/ui/frame/multitask_menu/split_button_view.h"
@@ -112,10 +112,11 @@ TEST_F(HapticsUtilTest, HapticFeedbackForNormalWindowSnap) {
     event_generator->set_current_screen_location(start);
     event_generator->PressLeftButton();
     event_generator->MoveMouseTo(test_case.first);
-    WorkspaceWindowResizer* workspace_resizer =
-        WorkspaceWindowResizer::GetInstanceForTest();
-    if (workspace_resizer->dwell_countdown_timer_.IsRunning())
-      workspace_resizer->dwell_countdown_timer_.FireNow();
+    auto& dwell_countdown_timer =
+        WorkspaceWindowResizerTestApi().GetDwellCountdownTimer();
+    if (dwell_countdown_timer.IsRunning()) {
+      dwell_countdown_timer.FireNow();
+    }
     EXPECT_EQ((int)i + 1, input_controller->GetSentHapticCount(
                               HapticTouchpadEffect::kSnap,
                               HapticTouchpadEffectStrength::kMedium));
---
+++ ash/wm/workspace/workspace_window_resizer_test_api.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/wm/workspace/workspace_window_resizer_test_api.h"
+
+#include "ash/wm/workspace/phantom_window_controller.h"
+#include "ash/wm/workspace/workspace_window_resizer.h"
+
+namespace ash {
+
+WorkspaceWindowResizerTestApi::WorkspaceWindowResizerTestApi() = default;
+
+WorkspaceWindowResizerTestApi::~WorkspaceWindowResizerTestApi() = default;
+
+PhantomWindowController*
+WorkspaceWindowResizerTestApi::GetSnapPhantomWindowController() {
+  auto* workspace_resizer = WorkspaceWindowResizer::GetInstanceForTest();
+  CHECK(workspace_resizer);
+  return workspace_resizer->snap_phantom_window_controller_.get();
+}
+
+base::OneShotTimer& WorkspaceWindowResizerTestApi::GetDwellCountdownTimer()
+    const {
+  auto* workspace_resizer = WorkspaceWindowResizer::GetInstanceForTest();
+  CHECK(workspace_resizer);
+  return workspace_resizer->dwell_countdown_timer_;
+}
+
+}  // namespace ash
