df83a7713a92b83321b87620ea0680e7c1eab715
Daniel Cheng <dcheng@chromium.org>
2024-07-09 18:23:05 +0000

    De-listify TabStripObserver for simplicity
    There are exactly two TabStripObserver implementations in the entire
    codebase, and they never observe the same TabStrip simultaneously.
    Migrate this to be a single observer interface instead, which:
    - removes the overhead of using ObserverList
    - removes the need to qualify AddObserver/RemoveObserver calls (since
      a TabStrip is also a views::View, which has its own AddObserver and
      RemoveObserver methods).
    
    Change-Id: Ied7fb64bb30c2209cebebee2285c65be0a05a0ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679613
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324996}

--- chrome/browser/ui/views/tabs/tab_strip.h
+++ chrome/browser/ui/views/tabs/tab_strip.h
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
-#include "base/observer_list.h"
-#include "base/scoped_observation.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
@@ -92,9 +90,8 @@ class TabStrip : public views::View,
   static int GetSizeNeededForViews(
       const std::vector<raw_ptr<TabSlotView, VectorExperimental>>& views);
 
-  // Add and remove observers to changes within this TabStrip.
-  void AddObserver(TabStripObserver* observer);
-  void RemoveObserver(TabStripObserver* observer);
+  // Sets the observer to be notified of changes within this TabStrip.
+  void SetTabStripObserver(TabStripObserver* observer);
 
   // Sets |background_offset_| and schedules a paint.
   void SetBackgroundOffset(int background_offset);
@@ -420,7 +417,7 @@ class TabStrip : public views::View,
 
   // -- Member Variables ------------------------------------------------------
 
-  base::ObserverList<TabStripObserver>::Unchecked observers_;
+  raw_ptr<TabStripObserver> observer_;
 
   std::unique_ptr<TabStripController> controller_;
 
