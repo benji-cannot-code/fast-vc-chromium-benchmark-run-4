04fd63497e12ce32f8fb09d7d4e2986898e23bc9
Duong Dac <ddac@chromium.org>
2024-07-09 17:26:44 +0000

    Supervised User verification interstitial
    This interstitial will be shown when supervised users who are signed
    out of the content area try to access Google-owned sites that require
    authentication.
    
    Bug: 346264927
    
    Change-Id: Ie4c949919f9300d9a38a8fd091820836e60459fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5659598
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1324918}

--- chrome/browser/ui/webui/interstitials/interstitial_ui.cc
+++ chrome/browser/ui/webui/interstitials/interstitial_ui.cc
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ssl/chrome_security_blocking_page_factory.h"
 #include "chrome/browser/ssl/https_only_mode_controller_client.h"
 #include "chrome/browser/ssl/insecure_form/insecure_form_controller_client.h"
+#include "chrome/browser/supervised_user/supervised_user_verification_controller_client.h"
+#include "chrome/browser/supervised_user/supervised_user_verification_page.h"
 #include "chrome/common/buildflags.h"
 #include "chrome/common/url_constants.h"
 #include "components/captive_portal/core/buildflags.h"
@@ -373,6 +375,19 @@ std::unique_ptr<EnterpriseWarnPage> CreateEnterpriseWarnPage(
                                                        kRequestUrl));
 }
 
+std::unique_ptr<SupervisedUserVerificationPage>
+CreateSupervisedUserVerificationPage(content::WebContents* web_contents) {
+  const GURL kRequestUrl("https://supervised-user-verification.example.net");
+  return std::make_unique<SupervisedUserVerificationPage>(
+      web_contents, kRequestUrl,
+      std::make_unique<SupervisedUserVerificationControllerClient>(
+          web_contents,
+          Profile::FromBrowserContext(web_contents->GetBrowserContext())
+              ->GetPrefs(),
+          g_browser_process->GetApplicationLocale(),
+          GURL(chrome::kChromeUINewTabURL), kRequestUrl));
+}
+
 std::unique_ptr<TestSafeBrowsingBlockingPageQuiet>
 CreateSafeBrowsingQuietBlockingPage(content::WebContents* web_contents) {
   safe_browsing::SBThreatType threat_type =
@@ -552,6 +567,8 @@ void InterstitialHTMLSource::StartDataRequest(
     interstitial_delegate = CreateInsecureFormPage(web_contents);
   } else if (path_without_query == "/https_only") {
     interstitial_delegate = CreateHttpsOnlyModePage(web_contents);
+  } else if (path_without_query == "/supervised-user-verify") {
+    interstitial_delegate = CreateSupervisedUserVerificationPage(web_contents);
   }
 
   if (path_without_query == "/quietsafebrowsing") {
@@ -559,7 +576,7 @@ void InterstitialHTMLSource::StartDataRequest(
         CreateSafeBrowsingQuietBlockingPage(web_contents);
     html = blocking_page->GetHTML();
     interstitial_delegate = std::move(blocking_page);
-  } else if (path_without_query == "/supervised_user") {
+  } else if (path_without_query == "/supervised-user-ask-parent") {
     html = GetSupervisedUserInterstitialHTML(path);
   } else if (interstitial_delegate.get()) {
     html = interstitial_delegate.get()->GetHTMLContents();
--- components/security_interstitials/core/browser/resources/interstitial_large.js
+++ components/security_interstitials/core/browser/resources/interstitial_large.js
@@ -78,6 +78,7 @@ function setupEvents() {
   const httpsOnly = interstitialType == 'HTTPS_ONLY';
   const enterpriseBlock = interstitialType === 'ENTERPRISE_BLOCK';
   const enterpriseWarn = interstitialType === 'ENTERPRISE_WARN';
+  const supervisedUserVerify = interstitialType === 'SUPERVISED_USER_VERIFY';
   const hidePrimaryButton = loadTimeData.getBoolean('hide_primary_button');
   const showRecurrentErrorParagraph =
       loadTimeData.getBoolean('show_recurrent_error_paragraph');
@@ -104,6 +105,8 @@ function setupEvents() {
     body.classList.add('enterprise-block');
   } else if (enterpriseWarn) {
     body.classList.add('enterprise-warn');
+  } else if (supervisedUserVerify) {
+    body.classList.add('supervised-user-verify');
   } else {
     body.classList.add('safe-browsing');
     // Override the default theme color.
@@ -217,7 +220,7 @@ function setupEvents() {
 
   const detailsButton = document.querySelector('#details-button');
   if (captivePortal || billing || lookalike || insecureForm || httpsOnly ||
-      enterpriseWarn || enterpriseBlock) {
+      enterpriseWarn || enterpriseBlock || supervisedUserVerify) {
     // Captive portal, billing, lookalike pages, insecure form, enterprise warn,
     // enterprise block, and HTTPS only mode interstitials don't
     // have details buttons.
