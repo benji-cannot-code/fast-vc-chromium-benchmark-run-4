3251cc80131110df4ef80e522ab21c9570e4a12b
Kent Tamura <tkent@chromium.org>
2024-07-09 21:57:46 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to renderer/modules/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I1b62c850cdf1420426b95b8ec41e88f3aa1d204b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686121
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325164}

--- third_party/blink/renderer/modules/background_fetch/background_fetch_registration.cc
+++ third_party/blink/renderer/modules/background_fetch/background_fetch_registration.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/modules/background_fetch/background_fetch_registration.h"
 
 #include <optional>
--- third_party/blink/renderer/modules/media/audio/audio_renderer_mixer_input_test.cc
+++ third_party/blink/renderer/modules/media/audio/audio_renderer_mixer_input_test.cc
@@ -3,6 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
+#include "third_party/blink/renderer/modules/media/audio/audio_renderer_mixer_input.h"
+
 #include <stddef.h>
 #include <memory>
 
@@ -16,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/modules/media/audio/audio_renderer_mixer.h"
-#include "third_party/blink/renderer/modules/media/audio/audio_renderer_mixer_input.h"
 #include "third_party/blink/renderer/modules/media/audio/audio_renderer_mixer_pool.h"
 
 using testing::_;
