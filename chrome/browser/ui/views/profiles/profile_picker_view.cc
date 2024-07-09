2c00b6796db82db0117991b35b872b5fc971493a
Orko Garai <orko@igalia.com>
2024-07-09 20:50:53 +0000

    chrome/linux: Fix profile picker icon
    Ensure app ID for wayland and WM_CLASS for x11 are set so that
    compositors can find the matching .desktop file and display a proper
    icon when the profile picker is shown initially.
    
    Bug: 41495747
    Change-Id: I2a7798b7c56aa56376e715968eed236e96a7b131
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690130
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1325121}

--- chrome/browser/ui/views/profiles/profile_picker_view.cc
+++ chrome/browser/ui/views/profiles/profile_picker_view.cc
@@ -85,6 +85,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/profiles/first_run_flow_controller_lacros.h"
 #endif
 
+#if BUILDFLAG(IS_LINUX)
+#include "chrome/browser/shell_integration_linux.h"
+#endif
+
 namespace {
 
 ProfilePickerView* g_profile_picker_view = nullptr;
@@ -111,6 +115,11 @@ class ProfilePickerWidget : public views::Widget {
     views::Widget::InitParams params(
         views::Widget::InitParams::NATIVE_WIDGET_OWNS_WIDGET);
     params.delegate = profile_picker_view_;
+#if BUILDFLAG(IS_LINUX)
+    params.wm_class_name = shell_integration_linux::GetProgramClassName();
+    params.wm_class_class = shell_integration_linux::GetProgramClassClass();
+    params.wayland_app_id = params.wm_class_class;
+#endif
     Init(std::move(params));
   }
   ~ProfilePickerWidget() override = default;
