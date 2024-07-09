e9e2bc26170df12302c9071a729e5f263e1ea620
Vladimir Levin <vmpstr@chromium.org>
2024-07-09 16:55:37 +0000

    Compose: Plumb AX snapshot to compose sessions
    This patch plumbs an a11y snapshot to the compose session. Note
    that this doesn't yet use the snapshot in any way, and it's behind
    a flag that's disabled by default.
    
    In subsequent patches, we will serialize the data and add it to the
    information sent to the compose server.
    
    R=carlosk@chromium.org
    
    Bug: 350946976
    Change-Id: I3fc102247c398f2c230b83aa4dc255408cdfb7b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5658972
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324864}

--- chrome/browser/compose/compose_session.cc
+++ chrome/browser/compose/compose_session.cc
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_user_data.h"
 #include "content/public/common/referrer.h"
 #include "mojo/public/cpp/bindings/callback_helpers.h"
+#include "ui/accessibility/ax_mode.h"
+#include "ui/accessibility/ax_tree_update.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
@@ -210,6 +212,8 @@ ComposeSession::ComposeSession(
       observer_(observer),
       collect_inner_text_(
           base::FeatureList::IsEnabled(compose::features::kComposeInnerText)),
+      collect_ax_snapshot_(
+          base::FeatureList::IsEnabled(compose::features::kComposeAXSnapshot)),
       inner_text_caller_(inner_text),
       ukm_source_id_(web_contents->GetPrimaryMainFrame()->GetPageUkmSourceId()),
       node_id_(node_id),
@@ -437,7 +441,7 @@ void ComposeSession::MakeRequest(
     return;
   }
 
-  if (!collect_inner_text_ || got_inner_text_) {
+  if (HasNecessaryPageContext()) {
     RequestWithSession(std::move(request), request_reason, is_input_edited);
   } else {
     // Prepare the compose call, which will be invoked when inner text
@@ -446,6 +450,11 @@ void ComposeSession::MakeRequest(
         &ComposeSession::RequestWithSession, weak_ptr_factory_.GetWeakPtr(),
         std::move(request), request_reason, is_input_edited);
   }
+}
+
+bool ComposeSession::HasNecessaryPageContext() const {
+  return (!collect_inner_text_ || got_inner_text_) &&
+         (!collect_ax_snapshot_ || got_ax_snapshot_);
 }
 
 void ComposeSession::RequestWithSession(
@@ -987,10 +996,10 @@ void ComposeSession::InitializeWithText(std::string_view selected_text) {
   initial_input_ = std::string(selected_text);
   session_events_.has_initial_text = !selected_text.empty();
 
-  MaybeRefreshInnerText(!initial_input_.empty());
+  MaybeRefreshPageContext(!initial_input_.empty());
 }
 
-void ComposeSession::MaybeRefreshInnerText(bool has_selection) {
+void ComposeSession::MaybeRefreshPageContext(bool has_selection) {
   // Update dialog state based on the current selection which can change while
   // the dialog is hidden.
   currently_has_selection_ = has_selection;
@@ -1010,6 +1019,7 @@ void ComposeSession::MaybeRefreshInnerText(bool has_selection) {
   ++session_events_.compose_prompt_view_count;
 
   RefreshInnerText();
+  RefreshAXSnapshot();
 
   // We should only autocompose once per session
   if (has_checked_autocompose_) {
@@ -1083,9 +1093,28 @@ void ComposeSession::UpdateInnerTextAndContinueComposeIfNecessary(
   }
   AddPageContentToSession(std::move(inner_text), node_offset,
                           std::move(trimmed_inner_text));
-  if (!continue_compose_.is_null()) {
+  TryContinueCompose();
+}
+
+void ComposeSession::TryContinueCompose() {
+  if (HasNecessaryPageContext() && !continue_compose_.is_null()) {
     std::move(continue_compose_).Run();
   }
+}
+
+void ComposeSession::UpdateAXSnapshotAndContinueComposeIfNecessary(
+    int request_id,
+    const ui::AXTreeUpdate& update) {
+  if (current_ax_snapshot_request_id_ != request_id) {
+    return;
+  }
+
+  got_ax_snapshot_ = true;
+
+  // TODO(crbug.com/350946976): Serialize and populate the necessary fields from
+  // `update` to `session_`.
+
+  TryContinueCompose();
 }
 
 void ComposeSession::RefreshInnerText() {
@@ -1098,13 +1127,30 @@ void ComposeSession::RefreshInnerText() {
 
   inner_text_caller_->GetInnerText(
       *web_contents_->GetPrimaryMainFrame(),
-      // This unsafeValue call is acceptable ehre because node_id is a
+      // This unsafeValue call is acceptable here because node_id is a
       // FieldRendererId which while being an U64 type is based one the int
       // DOMid which we are querying here.
       node_id_.renderer_id.GetUnsafeValue(),
       base::BindOnce(
           &ComposeSession::UpdateInnerTextAndContinueComposeIfNecessary,
           weak_ptr_factory_.GetWeakPtr(), current_inner_text_request_id_));
+}
+
+void ComposeSession::RefreshAXSnapshot() {
+  got_ax_snapshot_ = false;
+  if (!collect_ax_snapshot_) {
+    return;
+  }
+
+  ++current_ax_snapshot_request_id_;
+
+  web_contents_->RequestAXTreeSnapshot(
+      base::BindOnce(
+          &ComposeSession::UpdateAXSnapshotAndContinueComposeIfNecessary,
+          weak_ptr_factory_.GetWeakPtr(), current_ax_snapshot_request_id_),
+      ui::kAXModeWebContentsOnly,
+      compose::GetComposeConfig().max_ax_node_count_for_page_context,
+      /*timeout=*/{});
 }
 
 void ComposeSession::SetFirstRunCloseReason(
@@ -1130,7 +1176,7 @@ void ComposeSession::SetFirstRunCompleted() {
   fre_complete_ = true;
 
   // Start inner text capture which was skipped until FRE was complete.
-  MaybeRefreshInnerText(currently_has_selection_);
+  MaybeRefreshPageContext(currently_has_selection_);
 }
 
 void ComposeSession::SetMSBBCloseReason(
