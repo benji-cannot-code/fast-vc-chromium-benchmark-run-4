dc5f5006193b5a4d10884ed5b93fbb77d4f276ff
Xianzhu Wang <wangxianzhu@chromium.org>
2024-07-09 17:53:58 +0000

    [RasterInducingScroll] raster_inducing_scroll_offsets for PictureDrawQuad
    When creating a PictureDrawQuad, we need to pass
    raster_inducing_scroll_offsets along with display_item_list.
    During rendering, raster_inducing_scroll_offsets is passed to
    DisplayItemList::Raster().
    
    Also update ContentLayerClientImpl::AppendAdditionalInfoAsJSON()
    (for debugging) to pass raster_inducing_scroll_offsets to
    DisplayItemList::Raster().
    
    This CL may not have any real-world impact because we create
    PictureDrawQuads only in software resourceless mode which is only
    used by Android WebView which prefers composited scroll (thus no
    raster-inducing-scroll for now).
    
    Bug: 40517276
    Change-Id: I9885ff179b567a23e802cb7a9595cb6d3754fda0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686873
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324965}

--- third_party/blink/renderer/platform/graphics/compositing/content_layer_client_impl.cc
+++ third_party/blink/renderer/platform/graphics/compositing/content_layer_client_impl.cc
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/json/json_values.h"
 #include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 
+#if DCHECK_IS_ON()
+#include "cc/trees/layer_tree_host.h"
+#include "cc/trees/property_tree.h"
+#endif
+
 namespace blink {
 
 namespace {
@@ -77,7 +82,17 @@ void ContentLayerClientImpl::AppendAdditionalInfoAsJSON(
 #if DCHECK_IS_ON()
   if (flags & kLayerTreeIncludesPaintRecords) {
     LoggingCanvas canvas;
-    cc_display_item_list_->Raster(&canvas);
+    base::flat_map<cc::ElementId, gfx::PointF> raster_inducing_scroll_offsets;
+    for (auto& [scroll_element_id, _] :
+         cc_display_item_list_->raster_inducing_scrolls()) {
+      raster_inducing_scroll_offsets[scroll_element_id] =
+          layer.layer_tree_host()
+              ->property_trees()
+              ->scroll_tree()
+              .current_scroll_offset(scroll_element_id);
+    }
+    cc_display_item_list_->Raster(&canvas, /*image_provider=*/nullptr,
+                                  &raster_inducing_scroll_offsets);
     json.SetValue("paintRecord", canvas.Log());
   }
 #endif
