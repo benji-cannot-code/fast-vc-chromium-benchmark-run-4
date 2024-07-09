e0415271728004425887dd09612472c2fa712dc1
Alvin Lam <alvingo@google.com>
2024-07-09 18:04:20 +0000

    Reland "[3PCD Reminder] Initialize a reminder service for desktop."
    This is a reland of commit 6893414e2b68fda4f0a1e34673a0b7e4a465be48
    
    CL has been updated to add logic that will display the UB icon for the singular failing test. Root cause of the failure was due to the feature "TrackingProtection3pcd" not being enabled within a test with the field config disabled.
    
    The trybot to run the failing test suite will time out, instead I've ran the tests locally via UTR (`vpython3 tools/utr -B ci -b "Win10 Tests x64" -t browser_tests_no_field_trial compile-and-test -- --gtest_filter="*TrackingProtectionReminderDesktopUiController*"`) with the results being found https://luci-milo.appspot.com/ui/inv/u-alvingo-2024-07-08-19-21-00-11644859375b31a2/test-results
    
    Original change's description:
    > [3PCD Reminder] Initialize a reminder service for desktop.
    >
    > Bug: 347070414, 349121764
    > Change-Id: Ied7c3da07db5ca60198639a4288154629dd6e8c3
    > Fixed: 347070414
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5630585
    > Reviewed-by: Kevin Graney <kmg@google.com>
    > Reviewed-by: Ryan Sultanem <rsult@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Ryan Sultanem <rsult@google.com>
    > Auto-Submit: Alvin Lam <alvingo@google.com>
    > Reviewed-by: Abe Boujane <boujane@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1322651}
    
    Bug: 347070414, 349121764
    Change-Id: Id475de043c2d79774d61273aca07e8bd61af3433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675966
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Auto-Submit: Alvin Lam <alvingo@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324983}

--- components/privacy_sandbox/tracking_protection_reminder_service.cc
+++ components/privacy_sandbox/tracking_protection_reminder_service.cc
@@ -51,7 +51,7 @@ bool HasEnoughTimePassed(base::Time onboarded_timestamp) {
   return base::Time::Now() >= onboarded_timestamp + reminder_delay;
 }
 
-tracking_protection::TrackingProtectionReminderStatus GetReminderStatus(
+tracking_protection::TrackingProtectionReminderStatus GetReminderStatusInternal(
     PrefService* pref_service) {
   return static_cast<tracking_protection::TrackingProtectionReminderStatus>(
       pref_service->GetInteger(prefs::kTrackingProtectionReminderStatus));
@@ -60,7 +60,7 @@ tracking_protection::TrackingProtectionReminderStatus GetReminderStatus(
 void MaybeUpdateReminderStatus(PrefService* pref_service,
                                bool was_silently_onboarded = false) {
   // Do not overwrite the current reminder status if it's already set.
-  if (GetReminderStatus(pref_service) !=
+  if (GetReminderStatusInternal(pref_service) !=
       tracking_protection::TrackingProtectionReminderStatus::kUnset) {
     return;
   }
@@ -113,7 +113,7 @@ TrackingProtectionReminderService::~TrackingProtectionReminderService() =
     default;
 
 ReminderType TrackingProtectionReminderService::GetReminderType() {
-  if (GetReminderStatus(pref_service_) !=
+  if (GetReminderStatusInternal(pref_service_) !=
       tracking_protection::TrackingProtectionReminderStatus::kPendingReminder) {
     return ReminderType::kNone;
   }
@@ -135,6 +135,29 @@ ReminderType TrackingProtectionReminderService::GetReminderType() {
                                   : ReminderType::kActive;
 }
 
+void TrackingProtectionReminderService::OnReminderExperienced() {
+  if (GetReminderStatus() !=
+      tracking_protection::TrackingProtectionReminderStatus::kPendingReminder) {
+    return;
+  }
+  // TODO(crbug.com/349122141): Record the timestamp when this function is
+  // called to be passed as PSD with the HaTS survey.
+  SetReminderStatus(pref_service_,
+                    tracking_protection::TrackingProtectionReminderStatus::
+                        kExperiencedReminder);
+}
+
+bool TrackingProtectionReminderService::IsPendingReminder() {
+  return GetReminderStatus() ==
+         tracking_protection::TrackingProtectionReminderStatus::
+             kPendingReminder;
+}
+
+tracking_protection::TrackingProtectionReminderStatus
+TrackingProtectionReminderService::GetReminderStatus() {
+  return GetReminderStatusInternal(pref_service_);
+}
+
 void TrackingProtectionReminderService::Shutdown() {
   observers_.Clear();
   pref_change_registrar_.Reset();
@@ -151,7 +174,7 @@ void TrackingProtectionReminderService::RemoveObserver(Observer* observer) {
 void TrackingProtectionReminderService::OnReminderStatusChanged() {
   for (auto& observer : observers_) {
     observer.OnTrackingProtectionReminderStatusChanged(
-        GetReminderStatus(pref_service_));
+        GetReminderStatusInternal(pref_service_));
   }
 }
 
