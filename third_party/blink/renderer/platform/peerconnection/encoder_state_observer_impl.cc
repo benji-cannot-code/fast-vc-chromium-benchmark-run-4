36c50548fe5ae254eb375ab752f2665ef48e7315
Hirokazu Honda <hiroh@chromium.org>
2024-07-09 03:36:50 +0000

    EncoderStateObserverImpl: Add sequence checker
    EncoderStateObserverImpl instance is operated in the webrtc encoder
    sequence only except construction, which is executed in the webrtc
    worker sequence. This CL adds the sequence checker to validate it
    at run time.
    
    Bug: b:320555128
    Test: blink_platform_unittests
    Test: Google Meet call with DCHECK
    Change-Id: If20df27f21fca6bcc0a3200e09818065e4a3d437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679906
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Erik Språng <sprang@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324651}

--- third_party/blink/renderer/platform/peerconnection/encoder_state_observer_impl.cc
+++ third_party/blink/renderer/platform/peerconnection/encoder_state_observer_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/atomic_ref_count.h"
 #include "base/containers/contains.h"
+#include "base/sequence_checker.h"
 #include "third_party/webrtc/api/video/encoded_image.h"
 
 namespace blink {
@@ -124,21 +125,21 @@ class EncoderStateObserverImpl::EncoderState {
 EncoderStateObserverImpl::EncoderStateObserverImpl(
     media::VideoCodecProfile profile,
     const StatsCollector::StoreProcessingStatsCB& store_processing_stats_cb)
-    : StatsCollector(/*is_decode=*/false, profile, store_processing_stats_cb) {}
+    : StatsCollector(/*is_decode=*/false, profile, store_processing_stats_cb) {
+  DETACH_FROM_SEQUENCE(encoder_sequence_);
+}
 
 EncoderStateObserverImpl::~EncoderStateObserverImpl() {
-  std::vector<int> encoder_ids;
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   for (const auto& kv : encoder_state_by_id_) {
-    encoder_ids.push_back(kv.first);
-  }
-  for (const int id : encoder_ids) {
-    OnEncoderDestroyed(id);
+    OnEncoderDestroyed(kv.first);
   }
 }
 
 void EncoderStateObserverImpl::OnEncoderCreated(
     int encoder_id,
     const webrtc::VideoCodec& config) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   DCHECK(!base::Contains(encoder_state_by_id_, encoder_id));
 
   // Initially, assume all layers active.
@@ -155,6 +156,7 @@ void EncoderStateObserverImpl::OnEncoderCreated(
 }
 
 void EncoderStateObserverImpl::OnEncoderDestroyed(int encoder_id) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   EncoderState* encoder_state = GetEncoderState(encoder_id);
   if (!encoder_state) {
     return;
@@ -177,6 +179,7 @@ void EncoderStateObserverImpl::OnEncoderDestroyed(int encoder_id) {
 void EncoderStateObserverImpl::OnRatesUpdated(
     int encoder_id,
     const Vector<bool>& active_spatial_layers) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   EncoderState* encoder_state = GetEncoderState(encoder_id);
   if (!encoder_state) {
     return;
@@ -200,6 +203,7 @@ EncoderStateObserverImpl::GetEncoderState(int encoder_id,
 
 void EncoderStateObserverImpl::OnEncode(int encoder_id,
                                         uint32_t rtp_timestamp) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   EncoderState* encoder_state = GetEncoderState(encoder_id);
   if (!encoder_state) {
     return;
@@ -214,6 +218,7 @@ void EncoderStateObserverImpl::OnEncode(int encoder_id,
 
 void EncoderStateObserverImpl::OnEncodedImage(int encoder_id,
                                               const EncodeResult& result) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   if (!top_encoder_info_) {
     LOG(WARNING) << "Received encoded frame while no active encoder "
                     "configured, ignoring.";
@@ -279,6 +284,7 @@ void EncoderStateObserverImpl::UpdateStatsCollection(base::TimeTicks now) {
 
 std::optional<EncoderStateObserverImpl::TopLayerInfo>
 EncoderStateObserverImpl::FindHighestActiveEncoding() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(encoder_sequence_);
   std::optional<TopLayerInfo> top_info;
   for (const auto& kv : encoder_state_by_id_) {
     std::optional<TopLayerInfo> top_of_encoder = kv.second->TopLayer();
--- third_party/blink/renderer/platform/peerconnection/encoder_state_observer_impl.h
+++ third_party/blink/renderer/platform/peerconnection/encoder_state_observer_impl.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomic_ref_count.h"
 #include "base/containers/flat_map.h"
 #include "base/location.h"
+#include "base/sequence_checker.h"
 #include "third_party/blink/renderer/platform/peerconnection/encoder_state_observer.h"
 #include "third_party/blink/renderer/platform/peerconnection/stats_collector.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -22,6 +23,8 @@ namespace blink {
 // EncoderStateObserverImpl collects the encode stats for the top spatial layer
 // in SVC encoding, top stream in simulcast or the vanilla stream otherwise. It
 // doesn't collect stats if multiple encoders are running.
+// This is constructed in webrtc worker sequence. After construction, all the
+// operations including destructor must be done in the webrtc encoder sequence.
 class PLATFORM_EXPORT EncoderStateObserverImpl : public EncoderStateObserver,
                                                  public StatsCollector {
  public:
@@ -52,13 +55,21 @@ class PLATFORM_EXPORT EncoderStateObserverImpl : public EncoderStateObserver,
 
   EncoderState* GetEncoderState(
       int encoder_id,
-      base::Location location = base::Location::Current());
-  void UpdateStatsCollection(base::TimeTicks now);
+      base::Location location = base::Location::Current())
+      VALID_CONTEXT_REQUIRED(encoder_sequence_);
+  void UpdateStatsCollection(base::TimeTicks now)
+      VALID_CONTEXT_REQUIRED(encoder_sequence_);
 
-  base::flat_map<int, std::unique_ptr<EncoderState>> encoder_state_by_id_;
-  std::optional<TopLayerInfo> top_encoder_info_;
+  base::flat_map<int, std::unique_ptr<EncoderState>> encoder_state_by_id_
+      GUARDED_BY_CONTEXT(encoder_sequence_);
+  std::optional<TopLayerInfo> top_encoder_info_
+      GUARDED_BY_CONTEXT(encoder_sequence_);
 
-  base::TimeTicks last_update_stats_collection_time_;
+  base::TimeTicks last_update_stats_collection_time_
+      GUARDED_BY_CONTEXT(encoder_sequence_);
+
+  // WebRTC encoder sequence.
+  SEQUENCE_CHECKER(encoder_sequence_);
 };
 
 }  // namespace blink
