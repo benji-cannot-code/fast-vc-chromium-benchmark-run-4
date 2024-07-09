fdb0e22eb5417e4aeee279e467a4dc8ffe4ea4f9
Alexander Tekle <alexandertekle@google.com>
2024-07-09 18:51:15 +0000

    [Card on File] Fix fill-ins after kStandaloneCvc is refactored to new FieldTypeGroup
    Follow up for crrev.com/c/5499250. Because we moved it from kCreditCard group type, it no longer allows fill-ins without this fix.
    
    Bug: b/333085230
    Change-Id: Ic974953a94874b5794c0415b3b99fbcf9c26cf8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5623436
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1325022}

--- components/autofill/core/browser/autofill_external_delegate.cc
+++ components/autofill/core/browser/autofill_external_delegate.cc
@@ -1186,7 +1186,9 @@ bool AutofillExternalDelegate::IsPaymentsManualFallbackOnNonPaymentsField()
   if (trigger_source_ ==
       AutofillSuggestionTriggerSource::kManualFallbackPayments) {
     const AutofillField* field = GetQueriedAutofillField();
-    return !field || field->Type().group() != FieldTypeGroup::kCreditCard;
+    return !field || !FieldTypeGroupSet({FieldTypeGroup::kCreditCard,
+                                         FieldTypeGroup::kStandaloneCvcField})
+                          .contains(field->Type().group());
   }
   return false;
 }
