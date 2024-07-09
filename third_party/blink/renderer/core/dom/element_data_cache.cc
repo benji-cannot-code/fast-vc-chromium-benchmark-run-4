fc07d389927e826fb507220c76764b1364a2e550
Kent Tamura <tkent@chromium.org>
2024-07-09 01:58:47 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to core/dom/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: Ieb45b1d47658625cd6e50ae0ad2d0f9a6d8228c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5681536
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324623}

--- third_party/blink/renderer/core/dom/element_data_cache.cc
+++ third_party/blink/renderer/core/dom/element_data_cache.cc
@@ -25,6 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  */
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/core/dom/element_data_cache.h"
 
 #include "third_party/blink/renderer/core/dom/element_data.h"
--- third_party/blink/renderer/core/dom/space_split_string.cc
+++ third_party/blink/renderer/core/dom/space_split_string.cc
@@ -19,6 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02111-1307, USA.
  */
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/core/dom/space_split_string.h"
 
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
