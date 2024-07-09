d2f9e87fb6cf26bf319ae64807cf8cf888863d34
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-07-09 20:00:48 +0000

    [Apps] Fix dependency issue in app termination observation
    The AppTerminationObserver is a KeyedService that monitors for
    app termination. However, when it is notified that the app is
    terminating, it passes along a BrowserContext, which is then used
    to retrieve other BrowserContext services. This may be unsafe if
    the shutdown process for the BrowserContext has started, since
    the context may be marked as dead, which results in a failed
    assert.
    
    Fix this by nulling out the termination observation when the
    shutdown for KeyedServices begins for the associated BrowserContext.
    There was already no guarantee that app termination would be called
    for a BrowserContext if it had gone away, so this should, in theory,
    be fine.
    
    Bug: 352003806
    Change-Id: Iba87a99aa45eebe62e0d30cd6a6d907f44dd02d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5688864
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325072}

--- chrome/browser/apps/platform_apps/app_termination_observer.h
+++ chrome/browser/apps/platform_apps/app_termination_observer.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_APPS_PLATFORM_APPS_APP_TERMINATION_OBSERVER_H_
 #define CHROME_BROWSER_APPS_PLATFORM_APPS_APP_TERMINATION_OBSERVER_H_
 
+#include <optional>
+
 #include "base/callback_list.h"
 #include "base/memory/raw_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -29,12 +31,15 @@ class AppTerminationObserver : public KeyedService {
 
   static BrowserContextKeyedServiceFactory* GetFactoryInstance();
 
+  // KeyedService:
+  void Shutdown() override;
+
  private:
   void OnAppTerminating();
 
   raw_ptr<content::BrowserContext> browser_context_;
 
-  base::CallbackListSubscription subscription_;
+  std::optional<base::CallbackListSubscription> subscription_;
 };
 
 }  // namespace chrome_apps
