2e3081e464426c8a747f569ea67f56792eb0b3a2
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-09 16:54:28 +0000

    Remove unused import in name_table_ffi.rs
    This keeps showing up as a warning in builds.
    
    Change-Id: I5edbafd06f3537813020a3a87a355c4ddb4d7587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5688713
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324862}

--- content/browser/font_unique_name_lookup/name_table_ffi.rs
+++ content/browser/font_unique_name_lookup/name_table_ffi.rs
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-use read_fonts::{FileRef, FontRef, ReadError, TableProvider};
+use read_fonts::{FileRef, FontRef, ReadError};
 use skrifa::{string::StringId, MetadataProvider};
 // Only built for Android.
 use std::os::unix::ffi::OsStrExt;
