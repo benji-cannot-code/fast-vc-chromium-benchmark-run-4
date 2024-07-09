a3dc25c1761a04834712b9c362a5e6036fd26e25
Kent Tamura <tkent@chromium.org>
2024-07-09 01:59:01 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to core/layout/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I73d4e812f7b306fc4fc94e328b4ff2b0cdd454e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5681525
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324624}

--- third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
+++ third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/core/layout/grid/grid_layout_algorithm.h"
 
 #include "third_party/blink/renderer/core/layout/constraint_space_builder.h"
