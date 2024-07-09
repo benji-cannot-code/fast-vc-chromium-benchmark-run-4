3b4b44b3aa0ba90e060b77a5621356de70a62d73
Jason Jeremy Iman <jasongustaman@google.com>
2024-07-09 06:33:50 +0000

    Add policies to control DNS-over-HTTPS bypass feature
    This allows to set the policies DnsOverHttpsExcludedDomains and
    DnsOverHttpsIncludedDomains. The policies allow admins to configure
    domains to be excluded from using DNS-over-HTTPS.
    
    The feature can be useful for deployments where a public DNS-over-HTTPS
    DNS server is preferred by default, but internal plain-text DNS name
    server is needed to resolve internal domains. One such use case is with
    another policy DnsOverHttpsTemplatesWithIdentifiers.
    
    The feature is only supported on ChromeOS.
    
    Feature design docs for Googlers: go/cros-doh-bypass.
    
    Bug: b/351091814
    
    Change-Id: I11033c156016139289dd8e82b3149010c20456a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678305
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Andreea Costinas <acostinas@google.com>
    Commit-Queue: Jason Iman <jasongustaman@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1324697}

--- chrome/browser/ash/preferences.cc
+++ chrome/browser/ash/preferences.cc
@@ -177,6 +177,10 @@ void Preferences::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(::prefs::kLocalUserFilesAllowed, true);
   registry->RegisterStringPref(::prefs::kLocalUserFilesMigrationDestination,
                                "read_only");
+  registry->RegisterListPref(prefs::kDnsOverHttpsExcludedDomains,
+                             base::Value::List());
+  registry->RegisterListPref(prefs::kDnsOverHttpsIncludedDomains,
+                             base::Value::List());
 
   RegisterLocalStatePrefs(registry);
 }
