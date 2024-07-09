97410fc0fe8fbe3c0c36906b6a5b62f7f3cfaf61
Erica Lee <ericamlee@google.com>
2024-07-09 04:36:41 +0000

    p13n: escape freeform query
    Escape the text query before storing it in file data. Unescape it when
    extracting the data.
    
    https://screencast.googleplex.com/cast/NTA3NjQyNTA2NDMxNjkyOHwzYjQwZWE2MC02OQ
    
    If the text includes any html-like characteristics ("<div>"), an error
    is thrown and the About page will not be shown.
    
    BUG=b:347773658
    TEST=ash_unittests *SeaPenMetadata*
    
    Change-Id: I6045540e0b5284b3c994d9ecd9ffeaae82337a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670989
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Thuong Phan <thuongphan@chromium.org>
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324663}

--- ash/wallpaper/wallpaper_utils/sea_pen_metadata_utils.cc
+++ ash/wallpaper/wallpaper_utils/sea_pen_metadata_utils.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/time_formatting.h"
 #include "base/json/json_writer.h"
 #include "base/json/values_util.h"
+#include "base/strings/escape.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
@@ -76,8 +77,11 @@ SeaPenQueryDictToRecentImageInfo(
 
   auto* freeform_query = query_dict->FindString(kSeaPenFreeformQueryKey);
   if (freeform_query) {
+    std::string unescaped_text;
+    base::UnescapeBinaryURLComponentSafe(
+        *freeform_query, /* fail_on_path_separators= */ false, &unescaped_text);
     return personalization_app::mojom::RecentSeaPenImageInfo::New(
-        personalization_app::mojom::SeaPenQuery::NewTextQuery(*freeform_query),
+        personalization_app::mojom::SeaPenQuery::NewTextQuery(unescaped_text),
         GetCreationTimeInfo(*creation_time));
   }
 
@@ -164,7 +168,8 @@ base::Value::Dict SeaPenQueryToDict(
 
   switch (query->which()) {
     case personalization_app::mojom::SeaPenQuery::Tag::kTextQuery:
-      query_dict.Set(kSeaPenFreeformQueryKey, query->get_text_query());
+      query_dict.Set(kSeaPenFreeformQueryKey,
+                     base::EscapeAllExceptUnreserved(query->get_text_query()));
       break;
     case personalization_app::mojom::SeaPenQuery::Tag::kTemplateQuery:
       query_dict.Set(kSeaPenTemplateIdKey,
