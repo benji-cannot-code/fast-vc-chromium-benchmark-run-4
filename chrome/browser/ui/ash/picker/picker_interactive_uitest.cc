dc3566e5876ab5c4c501d4b5427c4b265e5e8b17
mlcui <mlcui@google.com>
2024-07-09 05:34:53 +0000

    Switch to search results view only when they arrive
    Bug: b:350594721
    Change-Id: I6a6a21005d4b917cdd378e6bb659924f0abfeec6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5674005
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1324675}

--- chrome/browser/ui/ash/picker/picker_interactive_uitest.cc
+++ chrome/browser/ui/ash/picker/picker_interactive_uitest.cc
@@ -230,8 +230,8 @@ IN_PROC_BROWSER_TEST_F(PickerInteractiveUiTest, SearchAndInsertDate) {
       ObserveState(kSearchFieldFocusedState, std::ref(picker_search_field)),
       WaitForState(kSearchFieldFocusedState, true),
       EnterText(ash::kPickerSearchFieldTextfieldElementId, u"today"),
-      WaitForShow(ash::kPickerSearchResultsListItemElementId),
       WaitForShow(ash::kPickerSearchResultsPageElementId),
+      WaitForShow(ash::kPickerSearchResultsListItemElementId),
       NameDescendantView(
           ash::kPickerSearchResultsPageElementId, kDateResultName,
           base::BindLambdaForTesting([kExpectedDate](const views::View* view) {
@@ -268,8 +268,8 @@ IN_PROC_BROWSER_TEST_F(PickerInteractiveUiTest, SearchAndInsertMath) {
       ObserveState(kSearchFieldFocusedState, std::ref(picker_search_field)),
       WaitForState(kSearchFieldFocusedState, true),
       EnterText(ash::kPickerSearchFieldTextfieldElementId, u"1 + 1"),
-      WaitForShow(ash::kPickerSearchResultsListItemElementId),
       WaitForShow(ash::kPickerSearchResultsPageElementId),
+      WaitForShow(ash::kPickerSearchResultsListItemElementId),
       NameDescendantView(
           ash::kPickerSearchResultsPageElementId, kMathResultName,
           base::BindLambdaForTesting(
@@ -415,8 +415,9 @@ IN_PROC_BROWSER_TEST_F(PickerSpokenFeedbackInteractiveUiTest,
       WaitForState(kSearchFieldFocusedState, true),
       // Enter a query that is guaranteed to have some results.
       EnterText(ash::kPickerSearchFieldTextfieldElementId, u"a"),
+      WaitForShow(ash::kPickerSearchResultsPageElementId),
       WaitForShow(ash::kPickerSearchResultsListItemElementId),
-      WaitForShow(ash::kPickerSearchResultsPageElementId), Do([&sm = sm_]() {
+      Do([&sm = sm_]() {
         SendKeyPress(ui::VKEY_DOWN);
         sm.ExpectSpeechPattern("*");
         // TODO: b/338142316 - Use correct role for result items.
