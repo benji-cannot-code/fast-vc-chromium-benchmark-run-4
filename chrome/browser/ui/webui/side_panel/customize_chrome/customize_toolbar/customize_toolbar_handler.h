027726fe1a3c01f9eed02caf284590f05976c45a
Taylor Bergquist <tbergquist@chromium.org>
2024-07-10 23:50:08 +0000

    [Customize Toolbar] Support Home/Forward buttons.
    The home and forward buttons must be handled separately because they
    don't have action items. Instead of using the PinnedToolbarActionsModel,
    the handler interacts with the existing profile prefs directly.
    
    Bug: 348480684
    Change-Id: I3caab916ec4bc576f89e9eb363d1844c53f3e6a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689759
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1325823}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.h
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar.mojom.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -47,6 +48,10 @@ class CustomizeToolbarHandler
 
  private:
   void OnActionPinnedChanged(actions::ActionId id, bool pinned);
+  void OnShowHomeButtonChanged();
+  void OnShowForwardButtonChanged();
+
+  PrefService* prefs() const;
 
   mojo::Remote<side_panel::customize_chrome::mojom::CustomizeToolbarClient>
       client_;
@@ -59,6 +64,8 @@ class CustomizeToolbarHandler
   base::ScopedObservation<PinnedToolbarActionsModel,
                           PinnedToolbarActionsModel::Observer>
       model_observation_{this};
+
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SIDE_PANEL_CUSTOMIZE_CHROME_CUSTOMIZE_TOOLBAR_CUSTOMIZE_TOOLBAR_HANDLER_H_
