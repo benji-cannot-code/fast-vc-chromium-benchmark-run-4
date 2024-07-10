10c63159f58cbaa8f20a51bbed894b12fe938897
Daniel Cheng <dcheng@chromium.org>
2024-07-10 02:28:10 +0000

    Revert "[MSan] Fix use-after-dtor error in TabStripScrollContainer"
    This reverts commit 290e945dc5e8830d88781259881b4cf776437b90.
    
    Reason for revert: Ironically, this fix causes MSan errors elsewhere:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/44505/overview
    
    Original change's description:
    > [MSan] Fix use-after-dtor error in TabStripScrollContainer
    >
    > TabStripScrollContainer registers itself as a ViewObserver of the
    > TabStrip but never unregisters itself. TabStrip is also an owned
    > descendent view of TabStripScrollContainer. This results in a
    > use-after-dtor when the TabStripScrollContainer is destroyed, since:
    >
    > - base classes are destroyed in reverse order
    > - so ViewObserver is destroyed before View
    > - destroying View destroys its descendents
    > - destroying the descendent TabSTrip notifies the ViewObserver, but the
    >   ViewObserver is already destroyed
    >
    > The fix is to use ScopedObservation to manage the observer lifetime; the
    > observer will be unregistered before the View base class runs.
    >
    > In theory, an alternate fix would be to reorder the base classes, but it
    > would be pointless. By definition, TabStripScrollContainer's destructor
    > has already completed by the time the base class View destructor runs—so
    > the observations would end up at the no-op ViewObserver base class
    > implementation rather than being routed to TabStripScrollContainer.
    >
    > Bug: 40222690
    > Change-Id: I776a224304eb0a38b180385eb64953c5342890c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679614
    > Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1325051}
    
    Bug: 40222690
    Change-Id: I54e5178bfab324fdbf2a1f82e363ea6b2deb50bd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690612
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325266}

--- chrome/browser/ui/views/tabs/tab_strip_scroll_container.h
+++ chrome/browser/ui/views/tabs/tab_strip_scroll_container.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/memory/raw_ptr.h"
-#include "base/scoped_observation.h"
 #include "chrome/browser/ui/views/tabs/overflow_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/view.h"
@@ -64,8 +63,6 @@ class TabStripScrollContainer : public views::View, views::ViewObserver {
   // enable or disable the scroll buttons based on the scroll position
   void MaybeUpdateScrollButtonState();
 
-  TabStrip* tab_strip() { return tab_strip_observation_.GetSource(); }
-
   // Subscription for scrolling of content view
   base::CallbackListSubscription on_contents_scrolled_subscription_;
 
@@ -80,8 +77,7 @@ class TabStripScrollContainer : public views::View, views::ViewObserver {
 
   // Actually scrolls |tab_strip_|.
   raw_ptr<views::ScrollView> scroll_view_;
-  base::ScopedObservation<TabStrip, views::ViewObserver> tab_strip_observation_{
-      this};
+  raw_ptr<TabStrip, DanglingUntriaged> tab_strip_;
 
   // The buttons that allow users to manually scroll |tab_strip_|.
   raw_ptr<views::ImageButton> leading_scroll_button_;
