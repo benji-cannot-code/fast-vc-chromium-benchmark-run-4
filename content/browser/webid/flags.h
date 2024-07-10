2b54ddd86a0b1b4161a6d25983060e36df183a4a
Christian Biesinger <cbiesinger@chromium.org>
2024-07-10 17:05:50 +0000

    [FedCM] Send SameSite=Lax cookies, in addition to None
    See discussion in https://github.com/fedidcg/FedCM/issues/587
    
    This is behind the FedCmSameSiteLax flag for now.
    
    Bug: 350944661
    Change-Id: I3e5fe99b98560f680217cd3326ff66d290a5290f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675433
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325570}

--- content/browser/webid/flags.h
+++ content/browser/webid/flags.h
@@ -56,6 +56,9 @@ bool IsFedCmUseOtherAccountEnabled(bool is_button_mode);
 // Whether the ButtonMode feature is enabled.
 bool IsFedCmButtonModeEnabled();
 
+// Whether sending of SameSite=Lax cookies is enabled.
+bool IsFedCmSameSiteLaxEnabled();
+
 }  // namespace content
 
 #endif  // CONTENT_BROWSER_WEBID_FLAGS_H_
