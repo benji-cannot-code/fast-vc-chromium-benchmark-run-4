ee9036670e6be30c10e2d69da0a444fe36dbf1e8
Joel Hockey <joelhockey@chromium.org>
2024-07-10 14:19:00 +0000

    Add test for GetContentUriFileSize()
    Followup from crrev.com/c/5679910
    
    The test for GetContentUriMimeType() is currently disabled due
    to flakiness.  Testing GetContentUriFileSize() requires the same
    structure to do InsertImageIntoMediaStore() which is what flaked.
    This tests both functions together, and leaves the test disabled for
    now.  Followup will enable the test, so at least if it flakes again,
    we still have the new test code.
    
    Bug: b/350860794
    Change-Id: Id3ee7c4d2fde90a0abaa287a354d84cb36609845
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690899
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325467}

--- base/android/content_uri_utils_unittest.cc
+++ base/android/content_uri_utils_unittest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/android/content_uri_utils.h"
+
+#include "base/files/file.h"
 #include "base/files/file_util.h"
 #include "base/path_service.h"
 #include "base/test/test_file_util.h"
@@ -13,7 +15,7 @@ namespace base {
 namespace android {
 
 // Disable test on Android due to flakiness: crbug.com/807080, crbug/1054637.
-TEST(ContentUriUtilsTest, DISABLED_ContentUriMimeTest) {
+TEST(ContentUriUtilsTest, DISABLED_Test) {
   // Get the test image path.
   FilePath data_dir;
   ASSERT_TRUE(PathService::Get(DIR_TEST_DATA, &data_dir));
@@ -27,12 +29,20 @@ TEST(ContentUriUtilsTest, DISABLED_ContentUriMimeTest) {
   EXPECT_TRUE(path.IsContentUri());
   EXPECT_TRUE(PathExists(path));
 
+  // Validate GetContentUriMimeType().
   std::string mime = GetContentUriMimeType(path);
   EXPECT_EQ(mime, std::string("image/png"));
+
+  // Validate GetContentUriFileSize().
+  File::Info info;
+  EXPECT_TRUE(GetFileInfo(path, &info));
+  EXPECT_GT(info.size, 0);
+  EXPECT_EQ(GetContentUriFileSize(path), info.size);
 
   FilePath invalid_path("content://foo.bar");
   mime = GetContentUriMimeType(invalid_path);
   EXPECT_TRUE(mime.empty());
+  EXPECT_EQ(GetContentUriFileSize(invalid_path), -1);
 }
 
 }  // namespace android
