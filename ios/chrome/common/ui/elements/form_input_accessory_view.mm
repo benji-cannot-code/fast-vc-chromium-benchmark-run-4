2b99d08acfd87c0bb96de32d433d737fdf78f65e
Christian Xu <christianxu@google.com>
2024-07-10 11:32:49 +0000

    [iOS][FormInputAccessory] Fix layout crash
    The bug indicates a crash when laying out the view.
    This CL update the layout only if the view already has a windows.
    
    Bug: 334988984
    Change-Id: Ic1b9a0aaa2067bbfe1d972a65e3c92d705e57a5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689290
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325404}

--- ios/chrome/common/ui/elements/form_input_accessory_view.mm
+++ ios/chrome/common/ui/elements/form_input_accessory_view.mm
@@ -160,7 +160,9 @@ NSString* const kFormInputAccessoryViewOmniboxTypingShieldAccessibilityID =
 
 - (void)setOmniboxTypingShieldHeight:(CGFloat)typingShieldHeight {
   _omniboxTypingShieldHeightConstraint.constant = typingShieldHeight;
-  [self layoutIfNeeded];
+  if (self.window) {
+    [self layoutIfNeeded];
+  }
 }
 
 #pragma mark - UIInputViewAudioFeedback
