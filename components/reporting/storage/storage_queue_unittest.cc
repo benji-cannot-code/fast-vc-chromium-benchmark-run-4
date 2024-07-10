6a27c59a74019c724d8c80a413cf5f94db31c20a
Leonid Baraz <lbaraz@chromium.org>
2024-07-10 00:11:34 +0000

    Prevent enqueuing UNDEFINED_DESTINATION events.
    Return error not only when destination is missing, but also when it is
    undefined.
    
    Bug: b:351679150
    Change-Id: I7796b993ae4abed9d3b8991f3f9f6098a9c75a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689284
    Commit-Queue: Leonid Baraz <lbaraz@chromium.org>
    Auto-Submit: Leonid Baraz <lbaraz@chromium.org>
    Reviewed-by: Alberto Juarez <albertojuarez@google.com>
    Commit-Queue: Alberto Juarez <albertojuarez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325220}

--- components/reporting/storage/storage_queue_unittest.cc
+++ components/reporting/storage/storage_queue_unittest.cc
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::testing::_;
+using ::testing::AllOf;
 using ::testing::AnyOf;
 using ::testing::AtMost;
 using ::testing::Between;
@@ -59,6 +60,7 @@ using ::testing::Eq;
 using ::testing::Gt;
 using ::testing::Invoke;
 using ::testing::Ne;
+using ::testing::Property;
 using ::testing::Return;
 using ::testing::Sequence;
 using ::testing::StrEq;
@@ -2590,6 +2592,35 @@ TEST_P(StorageQueueTest, WriteWithUnencryptedCopy) {
 
   // Flush manually.
   FlushOrDie();
+}
+
+TEST_P(StorageQueueTest, WriteWithNoDestination) {
+  static constexpr char kTestData[] = "test_data";
+
+  CreateTestStorageQueueOrDie(BuildStorageQueueOptionsOnlyManual());
+
+  Record record;
+  record.set_data(kTestData);
+  if (!dm_token_.empty()) {
+    record.set_dm_token(dm_token_);
+  }
+
+  // Attempt Write with no destination.
+  Status write_result = WriteRecord(std::move(record));
+  ASSERT_THAT(write_result,
+              AllOf(Property(&Status::code, Eq(error::FAILED_PRECONDITION)),
+                    Property(&Status::message,
+                             StrEq("Malformed record: missing destination"))))
+      << write_result;
+
+  // Attempt Write with undefined destination.
+  record.set_destination(UNDEFINED_DESTINATION);
+  write_result = WriteRecord(std::move(record));
+  ASSERT_THAT(write_result,
+              AllOf(Property(&Status::code, Eq(error::FAILED_PRECONDITION)),
+                    Property(&Status::message,
+                             StrEq("Malformed record: missing destination"))))
+      << write_result;
 }
 
 INSTANTIATE_TEST_SUITE_P(
