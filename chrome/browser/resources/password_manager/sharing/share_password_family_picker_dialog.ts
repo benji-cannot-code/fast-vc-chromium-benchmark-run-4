fdb24a5927ed0e300fe761797d86c4222bcfe2e9
Andrii Natiahlyi <natiahlyi@google.com>
2024-07-10 13:07:10 +0000

    [Password Sharing] Record family picker opened event
    Fixed: 346981553
    Change-Id: Iba7660d763b8fdbb15ca7364178e0457123e22f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691751
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Commit-Queue: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325436}

--- chrome/browser/resources/password_manager/sharing/share_password_family_picker_dialog.ts
+++ chrome/browser/resources/password_manager/sharing/share_password_family_picker_dialog.ts
@@ -77,6 +77,9 @@ export class SharePasswordFamilyPickerDialogElement extends UserUtilMixin
     override ready() {
       super.ready();
 
+      recordPasswordSharingInteraction(
+          PasswordSharingActions.FAMILY_PICKER_OPENED);
+
       // Pre-select the member if they are eligible for sharing and there are no
       // other members in the group.
       if (this.members.length === 1 && this.computeEligible_().length === 1) {
