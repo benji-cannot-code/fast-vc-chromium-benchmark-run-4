980ca23c0e0879e2f0d7c5a84e79cc0b1c9657a5
Allen Ballway <ballway@chromium.org>
2024-07-10 16:39:15 +0000

    Sandbox: Allow sched_setscheduler for Nvidia GPU sandbox
    UI/GPU process crashed with upstream mesa driver because
    "sched_setscheduler" syscall is not permitted in the GPU sandbox
    seccomp-bpf policy.  Allow this syscall for Nvidia GPU sandbox.
    
    BUG=b:348387705
    TEST=Nvidia platform boots to UI
    
    Change-Id: I3081418c91d95d502d8dceb6c2ca1da5d0b9feb2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654732
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: Allen Ballway <ballway@google.com>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325549}

--- sandbox/policy/linux/sandbox_seccomp_bpf_linux.cc
+++ sandbox/policy/linux/sandbox_seccomp_bpf_linux.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sandbox/policy/linux/bpf_cros_amd_gpu_policy_linux.h"
 #include "sandbox/policy/linux/bpf_cros_arm_gpu_policy_linux.h"
 #include "sandbox/policy/linux/bpf_cros_intel_gpu_policy_linux.h"
+#include "sandbox/policy/linux/bpf_cros_nvidia_gpu_policy_linux.h"
 #include "sandbox/policy/linux/bpf_gpu_policy_linux.h"
 #include "sandbox/policy/linux/bpf_network_policy_linux.h"
 #include "sandbox/policy/linux/bpf_ppapi_policy_linux.h"
@@ -133,6 +134,9 @@ std::unique_ptr<BPFBasePolicy> GetGpuProcessSandbox(
     }
     if (options.use_intel_specific_policies) {
       return std::make_unique<CrosIntelGpuProcessPolicy>();
+    }
+    if (options.use_nvidia_specific_policies) {
+      return std::make_unique<CrosNvidiaGpuProcessPolicy>();
     }
   }
   return std::make_unique<GpuProcessPolicy>();
