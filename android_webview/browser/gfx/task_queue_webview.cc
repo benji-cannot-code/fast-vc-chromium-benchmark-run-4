8eb8a929bd3307232b5fd024179d60eeb6a81f62
Colin Blundell <blundell@chromium.org>
2024-07-10 06:42:01 +0000

    [Viz] Have SkiaOutputSurfaceDependency vend SingleThreadTaskRunner
    This will be necessary to supply this TaskRunner when registering
    SkiaOutputSurfaceImpl as a MemoryDumpProvider. All implementations
    return a TaskRunner that is a SingleThreadTaskRunner.
    
    Bug: 330806170
    Change-Id: Ib36d738c543ee249c097ab5ea655f3f3c9dd3889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5688691
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325319}

--- android_webview/browser/gfx/task_queue_webview.cc
+++ android_webview/browser/gfx/task_queue_webview.cc
@@ -47,7 +47,7 @@ class TaskQueueViz : public TaskQueueWebView {
   void ScheduleTask(base::OnceClosure task, bool out_of_order) override;
   void ScheduleOrRetainTask(base::OnceClosure task) override;
   void ScheduleIdleTask(base::OnceClosure task) override;
-  scoped_refptr<base::TaskRunner> GetClientTaskRunner() override;
+  scoped_refptr<base::SingleThreadTaskRunner> GetClientTaskRunner() override;
   void InitializeVizThread(const scoped_refptr<base::SingleThreadTaskRunner>&
                                viz_task_runner) override;
   void ScheduleOnVizAndBlock(VizTask viz_task) override;
@@ -119,7 +119,8 @@ void TaskQueueViz::ScheduleIdleTask(base::OnceClosure task) {
   EmplaceTask(std::move(task));
 }
 
-scoped_refptr<base::TaskRunner> TaskQueueViz::GetClientTaskRunner() {
+scoped_refptr<base::SingleThreadTaskRunner>
+TaskQueueViz::GetClientTaskRunner() {
   DCHECK(viz_task_runner_);
   return viz_task_runner_;
 }
