383a7da1ed7f0da6a31add1ff825d8f02b58cca1
Elly <ellyjones@chromium.org>
2024-07-10 19:02:32 +0000

    stts: remove entry point metrics
    We don't use or look at these.
    
    Fixed: 344591560,344591800,344592212,344592294,344592334,344592452
    Change-Id: I4304f48e5c24cf88e5e237aded8a4d0885469bc1
    
    OBSOLETE_HISTOGRAMS=Remove all SendTabToSelf.{Token}.ClickResult histograms
    
    Change-Id: I4304f48e5c24cf88e5e237aded8a4d0885469bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685272
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325664}

--- chrome/browser/android/send_tab_to_self/metrics_recorder.cc
+++ chrome/browser/android/send_tab_to_self/metrics_recorder.cc
@@ -14,13 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace send_tab_to_self {
 
 // Static free function declared and called directly from Java.
-static void JNI_MetricsRecorder_RecordSendingEvent(JNIEnv* env,
-                                                   int sending_event) {
-  RecordSendingEvent(ShareEntryPoint::kShareSheet,
-                     static_cast<SendingEvent>(sending_event));
-}
-
-// Static free function declared and called directly from Java.
 static void JNI_MetricsRecorder_RecordNotificationShown(JNIEnv* env) {
   RecordNotificationShown();
 }
