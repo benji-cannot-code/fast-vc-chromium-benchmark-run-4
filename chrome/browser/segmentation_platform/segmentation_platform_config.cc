83f55caae93fbd7ee72fecb0e601c73ee06f9c1c
Xi Han <hanxi@google.com>
2024-07-10 15:02:26 +0000

    [Start] Clean up Start's segmentation model.
    In this CL, we remove:
    1) ChromeStartModelV2: segmentation model for start surface;
    2) Cached param: START_SURFACE_RETURN_TIME_USE_MODEL;
    3) SharedPreference key:
       Chrome.StartSurface.StartReturnTimeSegmentationResultMs
    
    Bug: 344651414
    Change-Id: I9344745706e1f787eb6a2289f90bcc2bc1f3ef91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5677380
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Hailey Wang <haileywang@google.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325479}

--- chrome/browser/segmentation_platform/segmentation_platform_config.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_config.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
-#include "chrome/browser/segmentation_platform/default_model/chrome_start_model_android_v2.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/commerce/core/shopping_service.h"
 #include "components/segmentation_platform/embedder/default_model/android_home_module_ranker.h"
@@ -155,7 +154,6 @@ std::vector<std::unique_ptr<Config>> GetSegmentationPlatformConfig(
     configs.emplace_back(GetConfigForContextualPageActions(context));
   }
 
-  configs.emplace_back(ChromeStartModelV2::GetConfig());
   configs.emplace_back(IntentionalUserModel::GetConfig());
   configs.emplace_back(PowerUserSegment::GetConfig());
   configs.emplace_back(FrequentFeatureUserModel::GetConfig());
