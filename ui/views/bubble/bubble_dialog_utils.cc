a915bd15e538351639f4663f662323d842a5884b
Peter Kotwicz <pkotwicz@chromium.org>
2024-07-10 21:00:26 +0000

    Notify user that bluetooth is not on during digital credential request
    This CL:
    - Adds dialog to notify user that bluetooth is not on during desktop
    digital credential request. Bluetooth detection logic is copied from
    webauthn.
    - Refactors QR-code dialog in order to have ability to have multi-step
    dialog. For instance, showing QR code after prompt telling user to turn
    on bluetooth
    - Pulls out BubbleDialogDelegate button configuration code from
    BubbleDialogModelHost so that it can be shared with other callers.
    
    BUG=341104762
    
    Change-Id: I2dd72e0bc4f1bfaddf68ffe6c9b7960cfb259d6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5618532
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325738}

---
+++ ui/views/bubble/bubble_dialog_utils.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/bubble/bubble_dialog_utils.h"
+
+#include "ui/base/models/dialog_model.h"
+#include "ui/views/bubble/bubble_dialog_delegate_view.h"
+#include "ui/views/controls/button/button.h"
+#include "ui/views/view_class_properties.h"
+
+namespace views {
+
+void ConfigureBubbleButtonForParams(
+    views::BubbleDialogDelegate& bubble_delegate,
+    views::Button* button_view,
+    ui::DialogButton dialog_button,
+    const ui::DialogModel::Button& model) {
+  bubble_delegate.SetButtonLabel(dialog_button, model.label());
+  bubble_delegate.SetButtonStyle(dialog_button, model.style());
+  bubble_delegate.SetButtonEnabled(dialog_button, model.is_enabled());
+
+  if (button_view) {
+    button_view->SetVisible(model.is_visible());
+    button_view->SetProperty(kElementIdentifierKey, model.id());
+  }
+}
+
+}  // namespace views
