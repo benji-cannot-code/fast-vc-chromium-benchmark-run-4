9c86940303f6755d4c69aa65cca01215d625f9a1
Anthony Garant <anthonygarant@chromium.org>
2024-07-10 19:09:48 +0000

    Add missing chromeOS include in record_handler_upload_unittest
    Change-Id: Id004531fd1574fd16984d8425f0b192683239706
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689975
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Reviewed-by: Leonid Baraz <lbaraz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325668}

--- chrome/browser/policy/messaging_layer/upload/record_handler_upload_unittest.cc
+++ chrome/browser/policy/messaging_layer/upload/record_handler_upload_unittest.cc
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "base/uuid.h"
+#endif  // BUILDFLAG(IS_CHROMEOS)
+
 using ::testing::_;
 using ::testing::AllOf;
 using ::testing::Between;
