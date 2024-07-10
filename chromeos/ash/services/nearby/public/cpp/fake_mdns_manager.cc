de89f829fb86f66ac4223b2590caf32a0f54b6bb
Daniel Classon <dclasson@google.com>
2024-07-10 00:03:53 +0000

    [Nearby Share] Wire MdnsManager mojom to Nearby utility process
    Pipes the MdnsManager remote to the Wifi LAN implementation within the
    Nearby utility process. Currently, only AddObserver has been piped
    and implemented end to end. Will eventually allow the Wifi LAN medium to
    start Mdns discovery and listen to service discovery events.
    
    Fixed: b/349650232
    Tested: Unit tests and manual testing on the prototype.
    Change-Id: I6f93845d4d1f5d0a2c68507caf25084020176b67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5660726
    Reviewed-by: Jack Shira <jackshira@google.com>
    Commit-Queue: Daniel Classon <dclasson@google.com>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325218}

---
+++ chromeos/ash/services/nearby/public/cpp/fake_mdns_manager.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/ash/services/nearby/public/cpp/fake_mdns_manager.h"
+
+namespace ash::nearby {
+
+FakeMdnsManager::FakeMdnsManager() = default;
+
+FakeMdnsManager::~FakeMdnsManager() = default;
+
+void FakeMdnsManager::StartDiscoverySession(
+    const std::string& service_type,
+    StartDiscoverySessionCallback callback) {
+  // NOT IMPLEMENTED
+}
+void FakeMdnsManager::StopDiscoverySession(
+    const std::string& service_type,
+    StopDiscoverySessionCallback callback) {
+  // NOT IMPLEMENTED
+}
+void FakeMdnsManager::AddObserver(
+    ::mojo::PendingRemote<::sharing::mojom::MdnsObserver> observer) {
+  // NOT IMPLEMENTED
+}
+
+}  // namespace ash::nearby
---
+++ chromeos/ash/services/nearby/public/cpp/fake_mdns_manager.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_SERVICES_NEARBY_PUBLIC_CPP_FAKE_MDNS_MANAGER_H_
+#define CHROMEOS_ASH_SERVICES_NEARBY_PUBLIC_CPP_FAKE_MDNS_MANAGER_H_
+
+#include "chromeos/ash/services/nearby/public/mojom/mdns.mojom.h"
+#include "mojo/public/cpp/bindings/pending_remote.h"
+
+namespace ash::nearby {
+
+// An implementation of MdnsManager for use in unit tests.
+class FakeMdnsManager : public ::sharing::mojom::MdnsManager {
+ public:
+  FakeMdnsManager();
+  ~FakeMdnsManager() override;
+
+  // ::sharing::mojom::MdnsManager
+  void StartDiscoverySession(const std::string& service_type,
+                             StartDiscoverySessionCallback callback) override;
+  void StopDiscoverySession(const std::string& service_type,
+                            StopDiscoverySessionCallback callback) override;
+  void AddObserver(
+      ::mojo::PendingRemote<::sharing::mojom::MdnsObserver> observer) override;
+};
+
+}  // namespace ash::nearby
+
+#endif  // CHROMEOS_ASH_SERVICES_NEARBY_PUBLIC_CPP_FAKE_MDNS_MANAGER_H_
