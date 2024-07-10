20d7cc665628c5fdef93672287830f02fd9d2264
Xianzhu Wang <wangxianzhu@chromium.org>
2024-07-10 20:17:53 +0000

    [RasterInducingScroll] status="test"
    To Sheriffs: If this CL causes any web test failures due to
    incomplete rebaselines, please let me know or suppress the failures
    temporarily and I'll fix them ASAP, instead of reverting.
    
    Code changes to make tests pass:
    - property_tree_manager.cc: Use MainThreadScrollingReason::
      kPreferNonCompositedScrolling for non-user-scrollable scrollers.
      One reason is that the scrollers don't have expanded cull rect so
      are not suitable for raster-inducing scroll. Another reason is to
      avoid timeout of virtual/threaded-prefer-compositing/fast/scroll-
      behavior/overflow-hidden-*.html. TODO(crbug.com/349864862).
    - Unit tests testing overlay scrollbars: Now the scrollbars are
      composited, and need an explicit SetHiddenIfOverlay(false) to make
      the scrollbars visible.
    
    Web test rebaselines:
    Most rebaselines are for antialiased subpixel changes, and changes
    about scrollbar compositing.
    
    Test configuration:
    - Remove virtual/raster-inducing-scroll
    - Add virtual/disable-raster-inducing-scroll
    - Some web tests test overlay scrollbar behavior in the main thread.
      They are skipped for RasterInducingScroll for now.
      TODO(crbug.com/349939555).
    
    Bug: 40517276
    Change-Id: I57763476e6667b123a5abc29a4047b1807746e03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643300
    Auto-Submit: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325710}

--- third_party/blink/renderer/core/paint/paint_layer_clipper_test.cc
+++ third_party/blink/renderer/core/paint/paint_layer_clipper_test.cc
@@ -545,6 +545,8 @@ TEST_F(PaintLayerClipperTest, ScrollbarClipBehaviorChild) {
 
   PaintLayer* parent_paint_layer = GetPaintLayerByElementId("parent");
   PaintLayer* child_paint_layer = GetPaintLayerByElementId("child");
+  parent_paint_layer->GetScrollableArea()->SetScrollbarsHiddenIfOverlay(false);
+  UpdateAllLifecyclePhasesForTest();
 
   ClipRectsContext context(
       parent_paint_layer,
@@ -576,8 +578,10 @@ TEST_F(PaintLayerClipperTest, ScrollbarClipBehaviorChildScrollBetween) {
 
   Element* parent = GetDocument().getElementById(AtomicString("parent"));
   PaintLayer* root_paint_layer = parent->GetLayoutObject()->View()->Layer();
-
   PaintLayer* child_paint_layer = GetPaintLayerByElementId("child");
+  parent->GetLayoutBox()->GetScrollableArea()->SetScrollbarsHiddenIfOverlay(
+      false);
+  UpdateAllLifecyclePhasesForTest();
 
   ClipRectsContext context(root_paint_layer,
                            &root_paint_layer->GetLayoutObject().FirstFragment(),
@@ -607,6 +611,8 @@ TEST_F(PaintLayerClipperTest, ScrollbarClipBehaviorParent) {
   )HTML");
 
   PaintLayer* parent_paint_layer = GetPaintLayerByElementId("parent");
+  parent_paint_layer->GetScrollableArea()->SetScrollbarsHiddenIfOverlay(false);
+  UpdateAllLifecyclePhasesForTest();
 
   ClipRectsContext context(
       parent_paint_layer,
