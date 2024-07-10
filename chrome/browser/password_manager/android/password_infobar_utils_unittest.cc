a344c72ad52cb4989c19cb802e89fed56bf8d8bb
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-10 10:41:13 +0000

    Test account with non-displayable email in PasswordInfobarUtilsTest
    This change adds a single new unit test.
    
    Change-Id: I7d1be93a59cc0cf266c242ab2dd23f021ceeaa82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691293
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325388}

--- chrome/browser/password_manager/android/password_infobar_utils_unittest.cc
+++ chrome/browser/password_manager/android/password_infobar_utils_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/test/task_environment.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
 #include "components/sync/base/user_selectable_type.h"
 #include "components/sync/test/test_sync_service.h"
@@ -16,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace password_manager {
 namespace {
 
-const char kTestEmail[] = "foo@gmail.com";
+const char kTestEmail[] = "john.doe@gmail.com";
+const char kTestFullName[] = "John Doe";
 
 class PasswordInfobarUtilsTest : public testing::Test {
  public:
@@ -30,6 +32,8 @@ class PasswordInfobarUtilsTest : public testing::Test {
     AccountInfo account_info =
         identity_test_environment_.MakePrimaryAccountAvailable(
             kTestEmail, signin::ConsentLevel::kSignin);
+    account_info.full_name = kTestFullName;
+    identity_test_environment_.UpdateAccountInfoForAccount(account_info);
     sync_service_.SetSignedIn(signin::ConsentLevel::kSignin, account_info);
   }
 
@@ -37,7 +41,23 @@ class PasswordInfobarUtilsTest : public testing::Test {
     AccountInfo account_info =
         identity_test_environment_.MakePrimaryAccountAvailable(
             kTestEmail, signin::ConsentLevel::kSync);
+    account_info.full_name = kTestFullName;
+    identity_test_environment_.UpdateAccountInfoForAccount(account_info);
     sync_service_.SetSignedIn(signin::ConsentLevel::kSync, account_info);
+  }
+
+  // Must only be called when there is a signed-in account.
+  void SetCanDisplaySignedInAccountEmail(bool can_display) {
+    CoreAccountInfo core_account_info =
+        identity_manager()->GetPrimaryAccountInfo(
+            signin::ConsentLevel::kSignin);
+    ASSERT_FALSE(core_account_info.IsEmpty())
+        << "Trying to update capability of signed-in account when there's none";
+    AccountInfo account_info =
+        identity_manager()->FindExtendedAccountInfo(core_account_info);
+    AccountCapabilitiesTestMutator(&account_info.capabilities)
+        .set_can_have_email_address_displayed(can_display);
+    identity_test_environment_.UpdateAccountInfoForAccount(account_info);
   }
 
   syncer::SyncService* sync_service() { return &sync_service_; }
@@ -104,6 +124,14 @@ TEST_F(PasswordInfobarUtilsTest, SyncingWithPasswordsDisabled) {
       std::nullopt);
   EXPECT_EQ(GetDisplayableAccountName(sync_service(), identity_manager()),
             std::string());
+}
+
+TEST_F(PasswordInfobarUtilsTest, NonDisplayableEmail) {
+  SignInWithSync();
+  SetCanDisplaySignedInAccountEmail(false);
+
+  EXPECT_EQ(GetDisplayableAccountName(sync_service(), identity_manager()),
+            kTestFullName);
 }
 
 }  // namespace
