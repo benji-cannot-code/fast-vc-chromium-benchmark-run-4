f2620dca9aa5628992e4fe7e3253e19392f37844
Kent Tamura <tkent@chromium.org>
2024-07-10 12:38:01 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to renderer/core/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: If81c731b6a1e4b7462ebcb2e1197f2607e3b8908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689271
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325420}

--- third_party/blink/renderer/core/frame/csp/conversion_util_fuzzer.cc
+++ third_party/blink/renderer/core/frame/csp/conversion_util_fuzzer.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "services/network/public/mojom/content_security_policy.mojom-blink.h"
 #include "third_party/blink/renderer/core/frame/csp/conversion_util.h"
 #include "third_party/blink/renderer/platform/heap/thread_state.h"
--- third_party/blink/renderer/core/html/track/vtt/vtt_scanner.cc
+++ third_party/blink/renderer/core/html/track/vtt/vtt_scanner.cc
@@ -28,6 +28,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/core/html/track/vtt/vtt_scanner.h"
 
 #include "third_party/blink/renderer/platform/wtf/text/string_to_number.h"
