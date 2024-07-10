7b741b1717d2ac75689d0bb66b61e348b1b58c53
Lauren Winston <lwinston@google.com>
2024-07-10 19:46:25 +0000

    [Read Aloud] Reset word boundary state during play from selection.
    Fixed: 352093326
    Change-Id: I0ebaf14995c23211ddbfdff6724248a3030f0794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690066
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325690}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -1414,6 +1414,7 @@ export class ReadAnythingElement extends ReadAnythingElementBase {
       if (hasSelection) {
         this.synth.cancel();
         chrome.readingMode.onRestartReadAloud();
+        this.resetToDefaultWordBoundaryState();
         playedFromSelection = this.playFromSelection();
       }
 
