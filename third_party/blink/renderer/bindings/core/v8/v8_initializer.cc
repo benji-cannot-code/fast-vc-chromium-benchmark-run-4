3fbc76d86d5ec9d6590d46dc312ae267f6d277a7
Kent Tamura <tkent@chromium.org>
2024-07-10 12:38:12 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to third_party/blink/
    https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#suppressions
    
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    Bug: 351564777
    Change-Id: I6d041c3e8dad77f6050a54d10b56e7ff9f704a28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689662
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325421}

--- third_party/blink/renderer/bindings/core/v8/v8_initializer.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_initializer.cc
@@ -24,6 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "third_party/blink/renderer/bindings/core/v8/v8_initializer.h"
 
 #include <algorithm>
