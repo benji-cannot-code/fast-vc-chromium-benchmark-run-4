88fa0e754d7911182f96d7b8b75167272f527ce0
Sylvain Defresne <sdefresne@chromium.org>
2024-07-10 09:59:50 +0000

    Fix dependency on KeyedServiceFactory internal methods
    The methods Associate(...) and Disassociate(...) should have been
    private. Instead of using them to implement a testing factory that
    return a specific object, use the fact that the testing factory is
    a base::OnceCallback<...> to pass a callback binding the object.
    
    Bug: 351120207
    Change-Id: I72d5c24ee953f7e908721586cbcfc0e59bf40798
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680384
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325377}

--- extensions/browser/extension_prefs_factory.cc
+++ extensions/browser/extension_prefs_factory.cc
@@ -34,8 +34,12 @@ ExtensionPrefsFactory* ExtensionPrefsFactory::GetInstance() {
 void ExtensionPrefsFactory::SetInstanceForTesting(
     content::BrowserContext* context,
     std::unique_ptr<ExtensionPrefs> prefs) {
-  Disassociate(context);
-  Associate(context, std::move(prefs));
+  SetTestingFactory(
+      context,
+      base::BindOnce([](std::unique_ptr<ExtensionPrefs> prefs,
+                        content::BrowserContext* context)
+                         -> std::unique_ptr<KeyedService> { return prefs; },
+                     std::move(prefs)));
 }
 
 ExtensionPrefsFactory::ExtensionPrefsFactory()
