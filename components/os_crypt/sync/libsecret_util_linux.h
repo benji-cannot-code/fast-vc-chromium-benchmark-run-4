dce13efc8ebd5356bab5a05884230d22c6e536cd
Jiacheng Guo <gjc@google.com>
2024-07-10 05:47:23 +0000

    Remove RAW_PTR_EXCLUSION for LibsecretLoader
    The CL removes the RAW_PTR_EXCLUSION for SearchHelper::error_ and
    updates the SearchHelper::results_ to use unique_ptr.
    
    Bug: 346693440
    Change-Id: I38aa2656a8c9454e01fc65563ca159364c48308b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686796
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325304}

--- components/os_crypt/sync/libsecret_util_linux.h
+++ components/os_crypt/sync/libsecret_util_linux.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <libsecret/secret.h>
 
 #include <list>
+#include <memory>
 #include <string>
 
 #include "base/component_export.h"
@@ -56,15 +57,23 @@ class LibsecretLoader {
 
     bool success() { return !error_; }
 
-    GList* results() { return results_; }
-    GError* error() { return error_; }
+    GList* results() { return results_.get(); }
+    GError* error() { return error_.get(); }
 
    private:
+    struct GErrorDeleter {
+      inline void operator()(GError* error) { g_error_free(error); }
+    };
+
+    struct GListDeleter {
+      inline void operator()(GList* list) {
+        g_list_free_full(list, &g_object_unref);
+      }
+    };
+
     // |results_| and |error_| are C-style objects owned by this instance.
-    raw_ptr<GList> results_ = nullptr;
-    // This field is not a raw_ptr<> because it was filtered by the rewriter
-    // for: #addr-of
-    RAW_PTR_EXCLUSION GError* error_ = nullptr;
+    std::unique_ptr<GList, GListDeleter> results_ = nullptr;
+    std::unique_ptr<GError, GErrorDeleter> error_ = nullptr;
   };
 
   LibsecretLoader() = delete;
