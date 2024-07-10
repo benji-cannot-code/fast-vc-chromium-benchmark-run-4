b9709604234cd3810ec53a684f9078b5cf6ef5de
Kristi Saney <kristislee@google.com>
2024-07-10 21:30:08 +0000

    [Read Aloud] Use AX tree lang if the model can't determine a language
    For some reason the yue wikipedia page was getting the unknown language
    code even though the html language is marked as yue. In this case we now
    fall back to the html language if there is one. If not, we still
    fallback to the browser language.
    
    Fixed: 352141808
    Change-Id: If9a3245ffa7a3f085324fbb0c691b6d4b1691f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690576
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325757}

--- chrome/renderer/accessibility/read_anything_app_model.cc
+++ chrome/renderer/accessibility/read_anything_app_model.cc
@@ -965,6 +965,7 @@ void ReadAnythingAppModel::ToggleImagesEnabled() {
 }
 
 void ReadAnythingAppModel::SetBaseLanguageCode(const std::string code) {
+  DCHECK(!code.empty());
   base_language_code_ = code;
   // Update whether each font is supported by the new language code.
   for (const auto& [font, font_info] : fonts::kFontInfos) {
