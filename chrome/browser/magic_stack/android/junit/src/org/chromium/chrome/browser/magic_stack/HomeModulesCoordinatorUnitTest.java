a7f5d36d03b74f90100184c476d2c6509083654f
Xi Han <hanxi@google.com>
2024-07-10 21:50:19 +0000

    [MagicStack] Add flag SegmentationPlatformAndroidHomeModuleRankerV2.
    In this CL, we add feature flag
    SegmentationPlatformAndroidHomeModuleRankerV2 to replace a previously
    used feature param "use_freshness_score".
    
    Bug: 337878177
    Change-Id: Icf377a49f2bc4ba1e34e08f7038d4f742c59d480
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689872
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325771}

--- chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
+++ chrome/browser/magic_stack/android/junit/src/org/chromium/chrome/browser/magic_stack/HomeModulesCoordinatorUnitTest.java
@@ -130,6 +130,8 @@ public class HomeModulesCoordinatorUnitTest {
         FeatureList.TestValues testValues = new FeatureList.TestValues();
         testValues.addFeatureFlagOverride(
                 ChromeFeatureList.SEGMENTATION_PLATFORM_ANDROID_HOME_MODULE_RANKER, true);
+        testValues.addFeatureFlagOverride(
+                ChromeFeatureList.SEGMENTATION_PLATFORM_ANDROID_HOME_MODULE_RANKER_V2, true);
         FeatureList.setTestValues(testValues);
     }
 
