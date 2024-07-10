3aa564c4f269573bfebf016a370bfb5f25f0f1d1
Erica Kovac <ekovac@google.com>
2024-07-10 15:49:21 +0000

    Add WPTs for 'allow-storage-access-by-user-activation' iframe sandbox
    Also moves existing test of whether the attribute value is supported into the newly created test file.
    
    
    R=cfredric@chromium.org
    
    Bug: 40278734
    Change-Id: I232df8dc1faa0dad0c5dddf342e64e4ba0dfbd2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685772
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Erica Kovac <ekovac@google.com>
    Cr-Commit-Position: refs/heads/main@{#1325508}

---
+++ third_party/blink/web_tests/external/wpt/storage-access-api/requestStorageAccess-sandboxed-iframe-allow-storage-access.sub.https.window.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=helpers.js
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+'use strict';
+
+test(() => {
+  let iframe = document.createElement('iframe');
+  assert_true(iframe.sandbox.supports('allow-storage-access-by-user-activation'), '`allow-storage-access-by-user-activation`' +
+    'sandbox attribute should be supported');
+}, "`allow-storage-access-by-user-activation` sandbox attribute is supported");
+
+(async function () {
+  const frameSourceUrl = 'https://{{hosts[alt][www]}}:{{ports[https][0]}}/storage-access-api/requestStorageAccess-sandboxed-iframe.sub.https.window.html';
+
+  let sandboxAttribute =
+    'allow-scripts allow-same-origin';
+  let testCase = 'sandboxed-iframe';
+
+  RunTestsInIFrame(
+    frameSourceUrl + `?testCase=${testCase}`,
+    sandboxAttribute);
+
+  sandboxAttribute += ' allow-storage-access-by-user-activation';
+  testCase = 'sandboxed-iframe-allow-storage-access-by-user-activation';
+
+  RunTestsInIFrame(
+    frameSourceUrl + `?testCase=${testCase}`,
+    sandboxAttribute);
+})();
