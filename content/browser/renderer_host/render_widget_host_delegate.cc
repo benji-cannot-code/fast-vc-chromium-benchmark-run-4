e8da46fd62fb824fab5433dd23e2d342c0df29f1
Adithya Srinivasan <adithyas@chromium.org>
2024-07-10 14:01:55 +0000

    Portals: Remove RenderWidgetHostViewDelegate::IsPortal
    Removes the method and its callsites (portals have been removed, so
    this is no longer implemented).
    
    Bug: 40287334
    Change-Id: I520b93c8049678d6334d8bd44558379c9bb1d1f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689481
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325456}

--- content/browser/renderer_host/render_widget_host_delegate.cc
+++ content/browser/renderer_host/render_widget_host_delegate.cc
@@ -163,10 +163,6 @@ bool RenderWidgetHostDelegate::IsShowingContextMenuOnPage() const {
   return false;
 }
 
-bool RenderWidgetHostDelegate::IsPortal() {
-  return false;
-}
-
 int RenderWidgetHostDelegate::GetVirtualKeyboardResizeHeight() {
   return 0;
 }
