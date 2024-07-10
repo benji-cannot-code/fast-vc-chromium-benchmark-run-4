e4ca2ef5f84e4b324197bf0271680136830a67b0
Felipe Andrade <fsandrade@chromium.org>
2024-07-10 16:52:55 +0000

    Revert "Change blink::WebKeyboardEvent to use std::array in is members"
    This reverts commit ddb1b5f2be65873e58f754f37d8d417c1343b356.
    
    Reason for revert: Suspect culprit of some of the crashes reported in:
    crbug.com/352208875, crbug.com/352281723 and crbug.com/352247428
    
    Original change's description:
    > Change blink::WebKeyboardEvent to use std::array in is members
    >
    > Change the `text` and `unmodified_text` members from C-arrays to
    > std::array, per guidance on:
    > https://chromium.googlesource.com/chromium/src/+/main/docs/unsafe_buffers.md#use-of-std_array
    >
    > Then update callers that no longer compile to either call data() or take
    > a span. Also fix a few lint errors.
    >
    > Low-Coverage-Reason: LARGE_SCALE_REFACTOR switching to std::array
    > Change-Id: Ic29e49ef8ae20702c6df561406e8fe34a66a7af7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685241
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1324966}
    
    Change-Id: Ib23f8d57d6106744d23e837b0a8a5cb72760aa69
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691655
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Felipe Andrade <fsandrade@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Owners-Override: Felipe Andrade <fsandrade@chromium.org>
    Auto-Submit: Felipe Andrade <fsandrade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325556}

--- third_party/blink/public/common/input/web_keyboard_event.h
+++ third_party/blink/public/common/input/web_keyboard_event.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_INPUT_WEB_KEYBOARD_EVENT_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_INPUT_WEB_KEYBOARD_EVENT_H_
 
-#include <array>
 #include <string>
 
 #include "third_party/blink/public/common/input/web_input_event.h"
@@ -62,8 +61,8 @@ class BLINK_COMMON_EXPORT WebKeyboardEvent : public WebInputEvent {
   // Windows guarantee one character per event.  The Mac does not, but in
   // reality that's all it ever gives.  We're generous, and cap it a bit
   // longer.
-  std::array<char16_t, kTextLengthCap> text = {};
-  std::array<char16_t, kTextLengthCap> unmodified_text = {};
+  char16_t text[kTextLengthCap] = {};
+  char16_t unmodified_text[kTextLengthCap] = {};
 
   WebKeyboardEvent(Type type, int modifiers, base::TimeTicks time_stamp)
       : WebInputEvent(type, modifiers, time_stamp) {}
