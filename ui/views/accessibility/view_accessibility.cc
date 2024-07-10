2b6894b36d83523dafbf48b27c333c065d1fef32
Vinay Singh <vinaysingh@microsoft.com>
2024-07-10 16:42:38 +0000

    [views-ax] Add a11y setter for kTextDirection attribute
    This CL introduces
    * ViewAccessibility::SetTextDirection();
    
    This setter will be used to dynamically update the accessibility cache
    when those the value of the attribute changes. Callers to this
    setter will be added in future CLs.
    This is currently updated in
    * https://source.chromium.org/chromium/chromium/src/+/main:ui/views/controls/textfield/textfield.cc
    
    Bug: 325137417
    Change-Id: I30bd7b60a56aa4f232bcfeefe199d30030cc1dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663928
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1325552}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -1034,6 +1034,15 @@ void ViewAccessibility::SetPopupForId(ui::AXPlatformNodeId popup_for_id) {
   data_.AddIntAttribute(ax::mojom::IntAttribute::kPopupForId, popup_for_id);
 }
 
+void ViewAccessibility::SetTextDirection(int text_direction) {
+  CHECK_GE(text_direction,
+           static_cast<int32_t>(ax::mojom::WritingDirection::kMinValue));
+  CHECK_LE(text_direction,
+           static_cast<int32_t>(ax::mojom::WritingDirection::kMaxValue));
+  data_.AddIntAttribute(ax::mojom::IntAttribute::kTextDirection,
+                        text_direction);
+}
+
 void ViewAccessibility::SetIsProtected(bool is_protected) {
   if (data_.HasState(ax::mojom::State::kProtected) == is_protected) {
     return;
