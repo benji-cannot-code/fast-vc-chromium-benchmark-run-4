5e9f50012eb9176f2c6be652cce914d8759ff6df
Fredrik Söderquist <fs@opera.com>
2024-07-11 14:45:02 +0000

    spanification: Path::Apply() and users
    Switch PathElement::points to a raw_span<gfx::PointF>. Update
    Path::Apply to use std::array<>s and construct span in the local
    buffers. Fix up the users of the API.
    
    Since all users of the API are updated, change the signature of the
    callback as well to use const PathElement&.
    
    Bug: 351564777
    Change-Id: I5e8d3d56cb0b1b1cf0395181d4323fccd3f83299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692211
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326140}

--- third_party/blink/renderer/platform/graphics/path.h
+++ third_party/blink/renderer/platform/graphics/path.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PATH_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_GRAPHICS_PATH_H_
 
-#include "base/memory/raw_ptr.h"
+#include "base/memory/raw_span.h"
 #include "third_party/blink/renderer/platform/geometry/float_rounded_rect.h"
 #include "third_party/blink/renderer/platform/graphics/graphics_types.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -66,7 +66,7 @@ enum PathElementType {
 // returns two tangent points and the endpoint.
 struct PathElement {
   PathElementType type;
-  raw_ptr<gfx::PointF, AllowPtrArithmetic> points;
+  base::raw_span<gfx::PointF> points;
 };
 
 // Result structure from Path::PointAndNormalAtLength() (and similar).
@@ -75,7 +75,7 @@ struct PointAndTangent {
   float tangent_in_degrees = 0;
 };
 
-typedef void (*PathApplierFunction)(void* info, const PathElement*);
+typedef void (*PathApplierFunction)(void* info, const PathElement&);
 
 class PLATFORM_EXPORT Path {
   USING_FAST_MALLOC(Path);
