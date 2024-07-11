8e04d8ffb7fb9652175e44a323cb9eab3e363285
Brett Wilson <brettw@chromium.org>
2024-07-11 18:03:00 +0000

    Improve hardware <Enter> handling in omnibox.
    On Android the handling of the enter key depended on the status of the
    hasAutocomplete() function which isn't correct, the enter key handling
    should always be forwarded to the enclosing control for acceptance.
    Without this, two enter presses were required in some cases.
    
    This only affects hardware keyboards. The normal on-screen keyboard triggers a slightly different code path which is why this was not flagged earlier.
    
    Removes an extra condition check for KeyEvent.ACTION_DOWN which was
    alreacy checked in an enclosing condition. This change is a cleanup with
    no behavior change.
    
    Elimiates a cursor flash when hitting enter by skipping an extra commit
    step on the input. Add a comment about why this is OK.
    
    Change-Id: I157a108dc62c804917ceac23aa1e2b91431e397e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690475
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326271}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/SpannableAutocompleteEditTextModelUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/SpannableAutocompleteEditTextModelUnitTest.java
@@ -144,7 +144,8 @@ public class SpannableAutocompleteEditTextModelUnitTest {
         mCurrentState.setAutocompleteText(Optional.of("google.com"));
 
         confirmAutocompletionAppliedWithKey(KeyEvent.KEYCODE_DPAD_RIGHT);
-        confirmAutocompletionApplied(KeyEvent.KEYCODE_ENTER);
+        // Enter is forwarded to the delegate for handling which is what "bypassed" checks.
+        confirmAutocompletionBypassed(KeyEvent.KEYCODE_ENTER);
         confirmAutocompletionApplied(KeyEvent.KEYCODE_TAB);
         confirmAutocompletionAppliedWithKey(KeyEvent.KEYCODE_DPAD_LEFT);
     }
@@ -155,7 +156,8 @@ public class SpannableAutocompleteEditTextModelUnitTest {
         mCurrentState.setAutocompleteText(Optional.of("google.com"));
 
         confirmAutocompletionAppliedWithKey(KeyEvent.KEYCODE_DPAD_LEFT);
-        confirmAutocompletionApplied(KeyEvent.KEYCODE_ENTER);
+        // Enter is forwarded to the delegate for handling which is what "bypassed" checks.
+        confirmAutocompletionBypassed(KeyEvent.KEYCODE_ENTER);
         confirmAutocompletionApplied(KeyEvent.KEYCODE_TAB);
         confirmAutocompletionAppliedWithKey(KeyEvent.KEYCODE_DPAD_RIGHT);
     }
