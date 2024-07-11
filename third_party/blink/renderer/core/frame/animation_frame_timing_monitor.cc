91f76bb9eda2fbcb3987c93582370e7885524d77
Joone Hur <joonehur@microsoft.com>
2024-07-11 08:52:26 +0000

    Remove unused parameter frame_time from BeginMainFrame method
    - Removed the unused parameter(base::TimeTicks frame_time) from
    the BeginMainFrame method in animation_frame_timing_monitor.cc an animation_frame_timing_monitor.h.
    - Updated the call to BeginMainFrame in web_frame_widget_impl.cc t reflect this change.
    
    Bug: none
    Change-Id: Idaca344d597baf39092c0f72eeb17eb69d7040cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679615
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Joone Hur <joone@chromium.org>
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Commit-Queue: Joone Hur <joone@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325990}

--- third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
+++ third_party/blink/renderer/core/frame/animation_frame_timing_monitor.cc
@@ -67,7 +67,6 @@ void AnimationFrameTimingMonitor::WillHandleInput(LocalFrame* frame) {
 }
 
 void AnimationFrameTimingMonitor::BeginMainFrame(
-    base::TimeTicks frame_time,
     LocalDOMWindow& local_root_window) {
   base::TimeTicks now = base::TimeTicks::Now();
   if (!current_frame_timing_info_) {
