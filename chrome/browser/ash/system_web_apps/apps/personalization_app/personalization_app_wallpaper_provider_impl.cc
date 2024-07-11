4f56779052cf3946fa9de3aef1723b92d21a8b85
Jeffrey Young <cowmoo@google.com>
2024-07-11 20:38:24 +0000

    p13n: eve default wallpaper not selectable
    * Encode local image thumbnails and device default wallpaper thumbnail
    as max dimension 384px jpg, previously 256px jpg for local images and
    1024px png for device default.
    * Change encoding quality from 90 to 100
    because 100 looks better if image has sharp edges, and the performance
    impact is negligible.
    * add console.error for future debugging if Personalization App fails
    to load device default wallpaper. This will generate a crash log entry
    for real production devices.
    
    Default wallpaper thumbnails were generated by resizing default
    wallpaper to max dimension 1024 and encoding as png. Eve's default
    wallpaper image is square, which resulted in a 1024x1024 image.
    Encoding that as png broke the maximum GURL size limit specified in
    url.mojom. The changes in the above paragraph produce thumbnail images
    less than 1/10 the url.mojom size limit.
    
    Bug: b:345846359
    Test: view chrome://personalization/wallpaper/local
    Change-Id: I26e2e95d51348b9222845469185368f1ad92bb42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5697866
    Commit-Queue: Jeffrey Young <cowmoo@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326364}

--- chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_wallpaper_provider_impl.cc
+++ chrome/browser/ash/system_web_apps/apps/personalization_app/personalization_app_wallpaper_provider_impl.cc
@@ -71,6 +71,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia_operations.h"
 #include "ui/gfx/image/image_util.h"
 #include "url/gurl.h"
+#include "url/mojom/url.mojom-forward.h"
 
 namespace ash::personalization_app {
 
@@ -80,8 +81,7 @@ using ash::WallpaperController;
 using ash::personalization_app::GetAccountId;
 using ash::personalization_app::GetUser;
 
-constexpr int kLocalImageThumbnailSizeDip = 256;
-constexpr int kCurrentWallpaperThumbnailSizeDip = 1024;
+constexpr int kLocalImageThumbnailSizeDip = 384;
 
 // Return the online wallpaper key. Use |info.unit_id| if available so we might
 // be able to fallback to the cached attribution.
@@ -92,12 +92,15 @@ const std::string GetOnlineWallpaperKey(ash::WallpaperInfo info) {
 
 GURL GetBitmapJpegDataUrl(const SkBitmap& bitmap) {
   std::vector<unsigned char> output;
-  if (!gfx::JPEGCodec::Encode(bitmap, /*quality=*/90, &output)) {
+  if (!gfx::JPEGCodec::Encode(bitmap, /*quality=*/100, &output)) {
     LOG(ERROR) << "Unable to encode bitmap";
     return GURL();
   }
-  return GetJpegDataUrl(
-      {reinterpret_cast<char*>(output.data()), output.size()});
+  GURL data_url =
+      GetJpegDataUrl({reinterpret_cast<char*>(output.data()), output.size()});
+  // @see `url.mojom` warning about dropping urls that are too long.
+  DCHECK_LT(data_url.spec().size(), url::mojom::kMaxURLChars);
+  return data_url;
 }
 
 }  // namespace
@@ -925,10 +928,9 @@ void PersonalizationAppWallpaperProviderImpl::OnGetDefaultImage(
     std::move(callback).Run(GURL());
     return;
   }
-  std::move(callback).Run(GURL(
-      webui::GetBitmapDataUrl(*WallpaperResizer::GetResizedImage(
-                                   image, kCurrentWallpaperThumbnailSizeDip)
-                                   .bitmap())));
+  gfx::ImageSkia resized =
+      WallpaperResizer::GetResizedImage(image, kLocalImageThumbnailSizeDip);
+  std::move(callback).Run(GetBitmapJpegDataUrl(*resized.bitmap()));
 }
 
 void PersonalizationAppWallpaperProviderImpl::OnGetLocalImages(
