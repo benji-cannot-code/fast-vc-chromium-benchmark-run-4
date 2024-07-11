8f27c06902643bf978204e4cc06c638bc89ced23
Karol Sygiet <sygiet@google.com>
2024-07-11 16:21:47 +0000

    Don't delete undecryptable passwords if disabled by policy
    This CL uses the value of the `DeletingUndecryptablePasswordsEnabled`
    policy while creating password store backend to determine if removing
    undecryptable passwords should be disabled.
    
    Bug: 40286735
    Change-Id: Icac68c0255acf6499be7c772f59dbf642435fa71
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692090
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326195}

--- components/password_manager/core/browser/password_store/login_database.h
+++ components/password_manager/core/browser/password_store/login_database.h
@@ -207,6 +207,9 @@ class LoginDatabase : public EncryptDecryptInterface {
   void SetClearingUndecryptablePasswordsCb(
       ClearingUndecryptablePasswordsCallback clearing_undecryptable_passwords);
 
+  void SetIsDeletingUndecryptableLoginsDisabledByPolicy(bool is_disabled) {
+    is_deleting_undecryptable_logins_disabled_by_policy_ = is_disabled;
+  }
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
   void SetIsUserDataDirPolicySet(bool is_set) {
     is_user_data_dir_policy_set_ = is_set;
@@ -394,6 +397,7 @@ class LoginDatabase : public EncryptDecryptInterface {
 
   std::optional<bool> were_undecryptable_logins_deleted_;
   bool is_user_data_dir_policy_set_ = false;
+  bool is_deleting_undecryptable_logins_disabled_by_policy_ = false;
 
   // These cached strings are used to build SQL statements.
   std::string add_statement_;
--- components/password_manager/core/browser/password_store/login_database_async_helper.h
+++ components/password_manager/core/browser/password_store/login_database_async_helper.h
@@ -98,6 +98,7 @@ class LoginDatabaseAsyncHelper : public PasswordStoreSync {
   // after a successful rollout.
   void SetClearingUndecryptablePasswordsCb(
       base::RepeatingCallback<void(bool)> clearing_undecryptable_passwords);
+  void SetIsDeletingUndecryptableLoginsDisabledByPolicy(bool is_disabled);
 
  private:
   // Implements PasswordStoreSync interface.
