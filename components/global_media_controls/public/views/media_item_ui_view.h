e143c561ba8d76a90161543c5f147f3af0f2ce8b
Yiren Wang <yrw@chromium.org>
2024-07-11 23:14:15 +0000

    Remove SlideOutController from MediaItemUIView
    SlideOutController is causing crash for QuickSettingsMediaView. Since it
    is no longer needed for CrOS updated media UI, and non-CrOS updated
    media UI will be launched soon without it either, we just remove it.
    
    Remove the GlobalMediaControlsUpdatedUI flag from MediaItemUIView too
    since non-CrOS updated media UI will not use MediaItemUIView any more.
    
    Bug: 340798896
    Change-Id: Ie7b8d9038ffb8900330211f47ef336f70e3cf883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690572
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Yiren Wang <yrw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326451}

--- components/global_media_controls/public/views/media_item_ui_view.h
+++ components/global_media_controls/public/views/media_item_ui_view.h
@@ -6,11 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_GLOBAL_MEDIA_CONTROLS_PUBLIC_VIEWS_MEDIA_ITEM_UI_VIEW_H_
 #define COMPONENTS_GLOBAL_MEDIA_CONTROLS_PUBLIC_VIEWS_MEDIA_ITEM_UI_VIEW_H_
 
-#include <string>
-
 #include "base/component_export.h"
-#include "base/memory/raw_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
 #include "components/global_media_controls/public/constants.h"
 #include "components/global_media_controls/public/media_item_ui.h"
@@ -20,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/media_message_center/media_notification_view_impl.h"
 #include "media/base/media_switches.h"
 #include "ui/base/metadata/metadata_header_macros.h"
-#include "ui/views/animation/slide_out_controller_delegate.h"
 #include "ui/views/focus/focus_manager.h"
 
 namespace media_message_center {
@@ -29,7 +24,6 @@ class MediaNotificationItem;
 
 namespace views {
 class ImageButton;
-class SlideOutController;
 }  // namespace views
 
 namespace global_media_controls {
@@ -44,7 +38,6 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaItemUIView
     : public views::Button,
       public media_message_center::MediaNotificationContainer,
       public global_media_controls::MediaItemUI,
-      public views::SlideOutControllerDelegate,
       public views::FocusChangeListener {
   METADATA_HEADER(MediaItemUIView, views::Button)
 
@@ -103,12 +96,6 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaItemUIView
   void OnShowCastingDevicesRequested() override;
   void OnListViewSizeChanged() override;
 
-  // views::SlideOutControllerDelegate:
-  ui::Layer* GetSlideOutLayer() override;
-  void OnSlideStarted() override {}
-  void OnSlideChanged(bool in_progress) override;
-  void OnSlideOut() override;
-
   // global_media_controls::MediaItemUI:
   void AddObserver(
       global_media_controls::MediaItemUIObserver* observer) override;
@@ -137,9 +124,6 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaItemUIView
     return device_selector_view_;
   }
   MediaItemUIFooter* footer_view_for_testing() { return footer_view_; }
-  views::SlideOutController* slide_out_controller_for_testing() {
-    return slide_out_controller_.get();
-  }
 
   bool is_playing_for_testing() { return is_playing_; }
   bool is_expanded_for_testing() { return is_expanded_; }
@@ -158,7 +142,6 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaItemUIView
   void OnSizeChanged();
 
   const std::string id_;
-  raw_ptr<views::View> swipeable_container_ = nullptr;
 
   std::u16string title_;
 
@@ -191,12 +174,7 @@ class COMPONENT_EXPORT(GLOBAL_MEDIA_CONTROLS) MediaItemUIView
 
   bool is_expanded_ = false;
 
-  bool is_sliding_ = false;
-
   base::ObserverList<global_media_controls::MediaItemUIObserver> observers_;
-
-  // Handles gesture events for swiping to dismiss notifications.
-  std::unique_ptr<views::SlideOutController> slide_out_controller_;
 
   // Sets to true when the notification theme is provided on Chrome OS.
   const bool has_notification_theme_;
