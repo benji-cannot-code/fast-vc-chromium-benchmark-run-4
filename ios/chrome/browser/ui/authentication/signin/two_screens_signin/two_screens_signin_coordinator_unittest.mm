393093e803a5ef3d880d59a15b2ed9324f9e38ab
Jérôme Lebel <jlebel@chromium.org>
2024-07-11 15:20:12 +0000

    [iOS] Make sure an fake identity is not added twice in unittests
    This patch updates TwoScreensSigninCoordinatorTest to make sure
    a fake identity is not added twice in FakeSystemIdentityManager.
    
    In a futur patch, adding an existing fake identity will trigger
    a DCHECK.
    
    Change-Id: If202236b897ad898ce9c9dc0cd0fb54a8e1bea2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5694827
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Reviewed-by: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326157}

--- ios/chrome/browser/ui/authentication/signin/two_screens_signin/two_screens_signin_coordinator_unittest.mm
+++ ios/chrome/browser/ui/authentication/signin/two_screens_signin/two_screens_signin_coordinator_unittest.mm
@@ -62,8 +62,8 @@ class TwoScreensSigninCoordinatorTest : public PlatformTest {
         FakeSystemIdentityManager::FromSystemIdentityManager(
             GetApplicationContext()->GetSystemIdentityManager());
     // Resets all preferences related to upgrade promo.
-    FakeSystemIdentity* fake_identity = [FakeSystemIdentity fakeIdentity1];
-    system_identity_manager->AddIdentity(fake_identity);
+    fake_identity_ = [FakeSystemIdentity fakeIdentity1];
+    system_identity_manager->AddIdentity(fake_identity_);
 
     coordinator_ = [[TwoScreensSigninCoordinator alloc]
         initWithBaseViewController:window_.rootViewController
@@ -108,15 +108,10 @@ class TwoScreensSigninCoordinatorTest : public PlatformTest {
 
   // Signs in a fake identity.
   void SigninFakeIdentity() {
-    FakeSystemIdentity* fake_identity = [FakeSystemIdentity fakeIdentity1];
-    FakeSystemIdentityManager* system_identity_manager =
-        FakeSystemIdentityManager::FromSystemIdentityManager(
-            GetApplicationContext()->GetSystemIdentityManager());
-    system_identity_manager->AddIdentity(fake_identity);
     AuthenticationService* auth_service = static_cast<AuthenticationService*>(
         AuthenticationServiceFactory::GetInstance()->GetForBrowserState(
             browser_state_.get()));
-    auth_service->SignIn(fake_identity,
+    auth_service->SignIn(fake_identity_,
                          signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN);
   }
 
@@ -135,6 +130,7 @@ class TwoScreensSigninCoordinatorTest : public PlatformTest {
   TwoScreensSigninCoordinator* coordinator_;
   base::UserActionTester user_actions_;
   UIWindow* window_;
+  FakeSystemIdentity* fake_identity_ = nil;
 };
 
 #pragma mark - Tests
