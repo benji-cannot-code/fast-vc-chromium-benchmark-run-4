ad7b32b2297b7b22974a9c1046664693465211d4
Lijin Shen <lazzzis@google.com>
2024-07-11 18:25:20 +0000

    Remove Activity.onBackPressed in TabbedRootUiCoordinator
    This one is evetually used by NavigationHandler in 3-button mode.
    A quick fix to avoid confusion and ambiguity.
    
    Bug: 344049600
    Change-Id: Iad4764e4c96d459d90425098718d56fab3c05ffe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685014
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326290}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -611,7 +611,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
                             @Override
                             public void onBackGesture() {
                                 // Back navigation gesture performs what the back button would do.
-                                mActivity.onBackPressed();
+                                mActivity.getOnBackPressedDispatcher().onBackPressed();
                             }
                         },
                         () -> mCompositorViewHolderSupplier.get());
