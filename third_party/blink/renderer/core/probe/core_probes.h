a659dae6c16536303fc77f7aa12b91e60ac7b9c0
Benedikt Meurer <bmeurer@chromium.org>
2024-07-11 12:50:41 +0000

    [inspector] Remove an unnecessary string copy for `console.timeStamp()`.
    When tracing and the Performance CDP domain are disabled, we don't need
    to eagerly copy the `v8_inspector::StringView` passed into the
    `consoleTimeStamp()` implementation into a WTF string, but we can defer
    that until after the fast-path checks. Likewise we don't need to eagerly
    determine the current `ExecutionContext` for the `v8::Isolate`, but we
    can defer that to until after the fast-path checks as well.
    
    We don't need to worry about the tracing case, since the C++ macro magic
    already takes care of defering the actual work until after the fast-path
    checks.
    
    This reduces the no-op overhead of the `consoleTimeStamp()` method in
    the `ThreadDebuggerCommonImpl` to basically two fast-path checks, one
    for tracing and one for the Performance agent probe.
    
    Bug: 350443026
    Change-Id: I3ab5852c1dace39d29503d9c8b6bd67d1823eac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690964
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326084}

--- third_party/blink/renderer/core/probe/core_probes.h
+++ third_party/blink/renderer/core/probe/core_probes.h
@@ -127,6 +127,10 @@ inline CoreProbeSink* ToCoreProbeSink(ExecutionContext* context) {
   return context ? context->GetProbeSink() : nullptr;
 }
 
+inline CoreProbeSink* ToCoreProbeSink(v8::Isolate* isolate) {
+  return isolate ? CurrentExecutionContext(isolate)->GetProbeSink() : nullptr;
+}
+
 inline CoreProbeSink* ToCoreProbeSink(ScriptState* script_state) {
   return ToCoreProbeSink(ToExecutionContext(script_state));
 }
