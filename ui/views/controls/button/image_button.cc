4e7cf5568525608035ddc178b272d7aed45d53cc
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-07-11 23:23:27 +0000

    [views-ax] Migrating Role in ui/Views to new system
    This CL migrates Views in the ui/Views path that set the role
    through GetAccessibleNodeData to use the new setters so that
    it is cached.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 352528963, 325137417
    Change-Id: I52f78d8a6e415dc663d4df59d72328f17965bb04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692865
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1326455}

--- ui/views/controls/button/image_button.cc
+++ ui/views/controls/button/image_button.cc
@@ -251,6 +251,16 @@ gfx::ImageSkia ImageButton::GetImageToPaint() {
   return !img.isNull() ? img : images_[STATE_NORMAL].Rasterize(color_provider);
 }
 
+void ToggleImageButton::UpdateAccessibleRoleIfNeeded() {
+  if ((toggled_ && !images_[ButtonState::STATE_NORMAL].IsEmpty()) ||
+      (!toggled_ && !alternate_images_[ButtonState::STATE_NORMAL].IsEmpty())) {
+    GetViewAccessibility().SetRole(ax::mojom::Role::kToggleButton);
+    return;
+  }
+
+  GetViewAccessibility().SetRole(ax::mojom::Role::kButton);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // ImageButton, private:
 
@@ -304,6 +314,7 @@ void ToggleImageButton::SetToggled(bool toggled) {
   toggled_ = toggled;
 
   OnPropertyChanged(&toggled_, kPropertyEffectsPaint);
+  UpdateAccessibleRoleIfNeeded();
   NotifyAccessibilityEvent(ax::mojom::Event::kCheckedStateChanged, true);
 }
 
@@ -323,6 +334,8 @@ void ToggleImageButton::SetToggledImageModel(
   } else {
     alternate_images_[image_state] = image_model;
   }
+
+  UpdateAccessibleRoleIfNeeded();
 }
 
 void ToggleImageButton::SetToggledBackground(std::unique_ptr<Background> b) {
@@ -371,6 +384,8 @@ void ToggleImageButton::SetImageModel(ButtonState image_state,
       SchedulePaint();
   }
   PreferredSizeChanged();
+
+  UpdateAccessibleRoleIfNeeded();
 }
 
 void ToggleImageButton::OnPaintBackground(gfx::Canvas* canvas) {
@@ -406,7 +421,6 @@ void ToggleImageButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   // accessible toggle button.
   if ((toggled_ && !images_[ButtonState::STATE_NORMAL].IsEmpty()) ||
       (!toggled_ && !alternate_images_[ButtonState::STATE_NORMAL].IsEmpty())) {
-    node_data->role = ax::mojom::Role::kToggleButton;
     node_data->SetCheckedState(toggled_ ? ax::mojom::CheckedState::kTrue
                                         : ax::mojom::CheckedState::kFalse);
   }
