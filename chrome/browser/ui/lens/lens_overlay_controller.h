e0f35aa3b0b89b95379e32cdd049819ea7dca324
Duncan Mercer <mercerd@google.com>
2024-07-11 17:25:28 +0000

    [lensoverlay] Close overlay on renderer close
    Bug: 341745843
    Change-Id: Ia4a3c874b76ba8a8e4dfc43ee7f2e59f10c71102
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692759
    Commit-Queue: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326246}

--- chrome/browser/ui/lens/lens_overlay_controller.h
+++ chrome/browser/ui/lens/lens_overlay_controller.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/autocomplete_match_type.h"
 #include "components/sessions/core/session_id.h"
 #include "components/viz/common/frame_timing_details.h"
+#include "content/public/browser/render_process_host_observer.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "mojo/public/cpp/bindings/associated_remote.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -88,7 +89,8 @@ class LensOverlayController : public LensSearchboxClient,
                               public SidePanelViewStateObserver,
                               public views::ViewObserver,
                               public views::WidgetObserver,
-                              public OmniboxTabHelper::Observer {
+                              public OmniboxTabHelper::Observer,
+                              public content::RenderProcessHostObserver {
  public:
   // Observer of LensOverlayController events.
   class Observer : public base::CheckedObserver {
@@ -659,6 +661,11 @@ class LensOverlayController : public LensSearchboxClient,
   void OnSidePanelDidOpen() override;
   void OnSidePanelCloseInterrupted() override;
   void OnSidePanelDidClose() override;
+
+  // content::RenderProcessHostObserver:
+  void RenderProcessExited(
+      content::RenderProcessHost* host,
+      const content::ChildProcessTerminationInfo& info) override;
 
   // Called when the associated tab enters the foreground.
   void TabForegrounded(tabs::TabInterface* tab);
