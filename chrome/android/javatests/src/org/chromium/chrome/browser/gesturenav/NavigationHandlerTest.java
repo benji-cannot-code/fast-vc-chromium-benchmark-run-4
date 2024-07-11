0f87e13bacddb38200cde9465a04ebcdf3c1b7a1
Lijin Shen <lazzzis@google.com>
2024-07-11 18:55:05 +0000

    Reland "[Default Nav Transition] Remove top controls in NTP screenshot"
    This reverts commit c2fd4d5853f233dc111d15b5f7ced9134b0641bd.
    
    The tests are using NavigationHandler which only
    works in 3-button nav mode and the test failed on OS
    which defaults to gesture nav mode.
    
    Original change's description:
    > Revert "[Default Nav Transition] Remove top controls in NTP screenshot"
    >
    > This reverts commit 7938c61cf27787612c36688e9ebe841c2050f1a8.
    >
    > Reason for revert: Causing bots to fail on CI: https://crbug.com/351979006
    >
    > Original change's description:
    > > [Default Nav Transition] Remove top controls in NTP screenshot
    > >
    > > In order to match the viewport of the webpage screenshot, this CL
    > > excludes the area of top controls of NTP and maintain the
    > > size of the screenshot. As a result, the bottom part of the screenshot
    > > will be filled with the default background color of the NTP because
    > > it is impossible to draw off-screen views in Android.
    > >
    > > Screenshot (with top controls): https://screenshot.googleplex.com/AJFcDYvXCbnvXgj.png
    > > Screen record (w/o top controls): http://screencast/cast/NDY4NTA0OTc2ODE4MTc2MHxjODkyNWE0Zi1jOQ
    > >
    > > Bug: 331814846
    > > Change-Id: Ieb8959ad2f202b15e055346f889a73355cdc4e06
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5560151
    > > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    > > Commit-Queue: Lijin Shen <lazzzis@google.com>
    > > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1324606}
    >
    > Bug: 331814846
    > Change-Id: Idee7cef1905d173ae74233fb5aa566e58f070691
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5687334
    > Owners-Override: Rupert Wiser <bewise@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rupert Wiser <bewise@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1324742}
    
    Bug: 331814846, 351979006
    Change-Id: Iac27e3d8cba61a2312d457bedcb61d16048ae5f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689652
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326304}

--- chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/gesturenav/NavigationHandlerTest.java
@@ -48,6 +48,7 @@ import org.chromium.chrome.browser.ui.native_page.BasicSmoothTransitionDelegate;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.util.ChromeTabUtils;
+import org.chromium.chrome.test.util.NewTabPageTestUtils;
 import org.chromium.components.embedder_support.util.UrlConstants;
 import org.chromium.content_public.browser.LoadUrlParams;
 import org.chromium.content_public.browser.back_forward_transition.AnimationStage;
@@ -71,6 +72,7 @@ import java.util.concurrent.TimeoutException;
 @Batch(Batch.PER_CLASS)
 public class NavigationHandlerTest {
     private static final String RENDERED_PAGE = "/chrome/test/data/android/navigate/simple.html";
+    private static final String TEST_PAGE = "/chrome/test/data/android/test.html";
     private static final boolean LEFT_EDGE = true;
     private static final boolean RIGHT_EDGE = false;
 
@@ -85,7 +87,10 @@ public class NavigationHandlerTest {
 
     @Before
     public void setUp() throws InterruptedException {
-        mActivityTestRule.startMainActivityOnBlankPage();
+        mTestServer =
+                EmbeddedTestServer.createAndStartServer(
+                        InstrumentationRegistry.getInstrumentation().getContext());
+        mActivityTestRule.startMainActivityWithURL(mTestServer.getURL(TEST_PAGE));
         CompositorAnimationHandler.setTestingMode(true);
         mNavUtils = new GestureNavigationUtils(mActivityTestRule);
         mNavigationHandler = mNavUtils.getNavigationHandler();
@@ -126,12 +131,12 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     @EnableFeatures({ChromeFeatureList.BACK_FORWARD_TRANSITIONS})
-    public void testSwipeBackToNTPWithTransition() {
+    public void testSwipeBackToNTPWithTransition() throws InterruptedException {
+        UiUtils.settleDownUI(InstrumentationRegistry.getInstrumentation());
         final Tab tab = mActivityTestRule.getActivity().getActivityTab();
         mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
+        UiUtils.settleDownUI(InstrumentationRegistry.getInstrumentation());
+        NewTabPageTestUtils.waitForNtpLoaded(mActivityTestRule.getActivity().getActivityTab());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
 
         mNavUtils.swipeFromEdgeAndHold(true);
@@ -173,9 +178,6 @@ public class NavigationHandlerTest {
         final Tab tab = mActivityTestRule.getActivity().getActivityTab();
         mActivityTestRule.loadUrl("chrome-native://bookmarks/folder/0");
         UiUtils.settleDownUI(InstrumentationRegistry.getInstrumentation());
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
 
         mNavUtils.swipeFromEdgeAndHold(true);
@@ -252,9 +254,6 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testReleaseGlowWithoutPrecedingPullIgnored() {
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -317,9 +316,6 @@ public class NavigationHandlerTest {
     public void testSwipeNavigateOnRenderedPage() {
         // TODO(crbug.com/40899221): Write a test variation running with
         //     ChromeFeatureList.BACK_FORWARD_TRANSITIONS enabled when the feature is completed.
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         mActivityTestRule.loadUrl(ContentUrlConstants.ABOUT_BLANK_DISPLAY_URL);
 
@@ -374,9 +370,6 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterDestroy() {
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(mNavigationHandler::destroy);
 
@@ -396,9 +389,6 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterTabDestroy() {
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(mTestServer.getURL(RENDERED_PAGE));
         TestThreadUtils.runOnUiThreadBlocking(currentTab()::destroy);
 
@@ -412,9 +402,6 @@ public class NavigationHandlerTest {
     @Test
     @SmallTest
     public void testSwipeAfterDestroyActivity_NativePage() {
-        mTestServer =
-                EmbeddedTestServer.createAndStartServer(
-                        InstrumentationRegistry.getInstrumentation().getContext());
         mActivityTestRule.loadUrl(UrlConstants.NTP_URL);
         TestThreadUtils.runOnUiThreadBlocking(mActivityTestRule.getActivity()::finish);
 
