fca8bc26caa8ce5f2d5f0ea4f816cda46ca6bea5
Jiaming Cheng <jiamingc@chromium.org>
2024-07-11 22:04:23 +0000

    [Mahi] Magic boost integration test
    Test the basic functions:
     showing the opt in card,
     clicking on the accept button on the card,
     clicking on the decline button on the card.
    
    Bug: b/349852159
    Change-Id: I79453c338e71dd21c131680f22292a8ee5ca2459
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691106
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Jiaming Cheng <jiamingc@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326412}

--- chrome/browser/ui/chromeos/magic_boost/magic_boost_opt_in_card.h
+++ chrome/browser/ui/chromeos/magic_boost/magic_boost_opt_in_card.h
@@ -47,6 +47,9 @@ class MagicBoostOptInCard : public chromeos::editor_menu::PreTargetHandlerView {
   // views::View:
   void RequestFocus() override;
 
+  // Returns the host widget's name.
+  static const char* GetWidgetNameForTest();
+
  private:
   // Button callbacks.
   void OnPrimaryButtonPressed();
