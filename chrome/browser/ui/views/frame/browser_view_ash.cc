a63c2c3c04b6b3e80c905fa5df2af2f29f5cff76
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-07-11 20:27:57 +0000

    Add a nullptr check in BrowserViewAsh
    During initialization step, the SadTabHelper can be null.
    
    Bug: b/352576309
    Change-Id: Ie5621ff17b279661c3c25383b9ae6d18582e593d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5697785
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326351}

--- chrome/browser/ui/views/frame/browser_view_ash.cc
+++ chrome/browser/ui/views/frame/browser_view_ash.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/views/frame/browser_view_ash.h"
+
 #include <algorithm>
 
 #include "base/check.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/frame/browser_view_ash.h"
 #include "chrome/browser/ui/views/sad_tab_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
@@ -109,7 +110,7 @@ void BrowserViewAsh::UpdateWindowRoundedCorners(int corner_radius) {
     // changes.
     if (auto* sad_tab_helper =
             SadTabHelper::FromWebContents(contents_webview->web_contents());
-        sad_tab_helper->sad_tab()) {
+        sad_tab_helper && sad_tab_helper->sad_tab()) {
       SadTabView* sad_tab_view =
           static_cast<SadTabView*>(sad_tab_helper->sad_tab());
       if (sad_tab_view->GetBackgroundRadii() != contents_webview_radii) {
