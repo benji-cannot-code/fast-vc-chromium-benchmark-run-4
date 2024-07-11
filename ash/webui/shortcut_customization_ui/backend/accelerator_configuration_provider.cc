0394e34b35e0ddbbfa8eb2255db35dc3f2af129f
Tzarial Kuznia <zork@google.com>
2024-07-11 06:21:28 +0000

    [a11y] Add Mouse Keys accelerator to Shortcut customization app.
    BUG=b:335456364
    TEST=ash_unittests --gtest_filter=MouseKeysTest.*
    
    Change-Id: I6eead29397ca15dfe34a784c94af8d728c9b4909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686299
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Emily Stark <estark@chromium.org>
    Auto-Submit: Tzarial Kuznia <zork@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325933}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
+#include "ui/accessibility/accessibility_features.h"
 #include "ui/base/ime/ash/input_method_manager.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/ui_base_features.h"
@@ -454,6 +455,8 @@ bool ShouldExcludeItem(const AcceleratorLayoutDetails& details) {
     case kTilingWindowResizeUp:
     case kTilingWindowResizeDown:
       return !features::IsTilingWindowResizeEnabled();
+    case kToggleMouseKeys:
+      return !::features::IsAccessibilityMouseKeysEnabled();
     case kToggleSnapGroupWindowsMinimizeAndRestore:
       return true;
   }
