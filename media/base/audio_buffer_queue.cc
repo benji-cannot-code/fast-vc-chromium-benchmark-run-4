efa6fbe44ab074db21918d683b7c772b1ff2de42
Dale Curtis <dalecurtis@chromium.org>
2024-07-11 04:28:34 +0000

    Fix a/v sync issues with repeated playback rate changes.
    WSOLA does not linearly consume frames based on playback rate,
    which breaks core assumptions about how AudioClock works. It usually
    works though since WSOLA consumes frames in a cyclical pattern.
    
    ... but that breaks down when the rate is changed repeatedly, since
    the pattern is no longer cyclical, delay builds infinitely. We could
    teach AudioClock about non-linear rates, but how is not obvious to me.
    
    Instead, since each FillBuffer() call can be considered to have a
    discrete rate (frames consumed / frames produced), we can give that value to AudioClock and everything lines up as expected. AudioClock
    already knows how to deal with changing linear rates.
    
    This results in each AudioClock::back_timestamp() and AudioBufferQueue::front_timestamp() drifting less than a millisecond;
    which is better than before even for static rates.
    
    Fixed: 40190553
    Change-Id: Ib283995f5afc8e290c85b1f7c1992bad413bc064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690689
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1325907}

--- media/base/audio_buffer_queue.cc
+++ media/base/audio_buffer_queue.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_op.h"
 #include "media/base/audio_bus.h"
+#include "media/base/audio_timestamp_helper.h"
 
 namespace media {
 
@@ -52,6 +53,16 @@ void AudioBufferQueue::SeekFrames(int frames) {
   CHECK_LE(frames, frames_);
   int taken = InternalRead(frames, true, 0, 0, NULL);
   DCHECK_EQ(taken, frames);
+}
+
+std::optional<base::TimeDelta> AudioBufferQueue::FrontTimestamp() const {
+  if (buffers_.empty()) {
+    return std::nullopt;
+  }
+
+  return buffers_.front()->timestamp() +
+         AudioTimestampHelper::FramesToTime(front_buffer_offset_,
+                                            buffers_.front()->sample_rate());
 }
 
 int AudioBufferQueue::InternalRead(int frames,
