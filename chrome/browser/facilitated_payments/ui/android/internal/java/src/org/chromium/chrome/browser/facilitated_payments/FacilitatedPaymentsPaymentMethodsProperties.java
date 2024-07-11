42415fa28795b55cbc817020b7e8bb19907b70f2
Vishwas Uppoor <vishwasuppoor@google.com>
2024-07-11 22:23:58 +0000

    [PIX] Add OK button to the error screen
    Add a button to the error screen to allow users to dismiss the screen.
    This is an additional dismissal mode, and does not replace the default
    dismissal modes.
    
    Screenshot(light mode): https://screenshot.googleplex.com/3YAzaGwdxMWpoei
    Screenshot(dark mode): https://screenshot.googleplex.com/SWXgnM2UsYCNpGp
    Demo: https://screencast.googleplex.com/cast/NjAzOTIxNzA3ODUzNDE0NHw0OTcyNGRmMS00OQ
    
    Bug: b/348141953
    Change-Id: I27dd5f1a73b4ff3180fb78a98f1dcfb925e98a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5695385
    Commit-Queue: Siddharth Shah <siashah@chromium.org>
    Auto-Submit: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326424}

--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsProperties.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsProperties.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.facilitated_payments;
 
 import android.graphics.Bitmap;
+import android.view.View.OnClickListener;
 
 import org.chromium.base.Callback;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
@@ -151,6 +152,21 @@ class FacilitatedPaymentsPaymentMethodsProperties {
         static final PropertyKey[] ALL_KEYS = {};
 
         private FooterProperties() {}
+    }
+
+    /**
+     * Properties defined here reflect the visible state of the error screen shown in a bottom
+     * sheet.
+     */
+    static class ErrorScreenProperties {
+        /** Primary button callback. */
+        static final WritableObjectPropertyKey<OnClickListener> PRIMARY_BUTTON_CALLBACK =
+                new WritableObjectPropertyKey<>("primary_button_callback");
+
+        /** All the properties of error screen. */
+        static final PropertyKey[] ALL_KEYS = {PRIMARY_BUTTON_CALLBACK};
+
+        private ErrorScreenProperties() {}
     }
 
     private FacilitatedPaymentsPaymentMethodsProperties() {}
