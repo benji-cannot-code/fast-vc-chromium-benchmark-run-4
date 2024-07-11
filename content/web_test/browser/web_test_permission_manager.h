8173a6c1a53a1c171d802304ab66db1289508d18
Igor Kraskevich <kraskevich@google.com>
2024-07-11 12:40:06 +0000

    [Android EMEA sheriff] Revert "[Permission] Remove SubscribeToPermissionStatusChange from PermissionManager"
    This reverts commit c129a1b9aefc806182cc00c74fe296e5cc7945fc.
    
    Reason for revert: breaks `NFCHostTest.GetNFCTwice` content unittest on https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests
    
    Original change's description:
    > [Permission] Remove SubscribeToPermissionStatusChange from PermissionManager
    >
    > This CL:
    > * creates a separate file for Subscription enum
    > * Removed duplicating `SubscribeToPermissionStatusChange` from PermissionManager
    > * Move related unittests into new test file `chrome/browser/permissions/permission_subscription_unittest.cc`
    >
    > Bug: 40238954
    > Change-Id: Ic1e106880a0ee2b12384d151e95de9ded783c72b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5498921
    > Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    > Reviewed-by: Peter Beverloo <peter@chromium.org>
    > Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    > Commit-Queue: Yifan Luo <lyf@chromium.org>
    > Reviewed-by: Sean Topping <seantopping@chromium.org>
    > Reviewed-by: Elias Klim <elklm@chromium.org>
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1325782}
    
    Bug: 40238954
    Change-Id: Ib1e0d21baa33075f5d0f93b127692824bd707378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690963
    Owners-Override: Igor Kraskevich <kraskevich@google.com>
    Auto-Submit: Igor Kraskevich <kraskevich@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326078}

--- content/web_test/browser/web_test_permission_manager.h
+++ content/web_test/browser/web_test_permission_manager.h
@@ -75,11 +75,17 @@ class WebTestPermissionManager
       blink::PermissionType permission,
       content::RenderFrameHost* render_frame_host,
       const url::Origin& overridden_origin) override;
-  void OnPermissionStatusChangeSubscriptionAdded(
-      content::PermissionController::SubscriptionId subscription_id) override;
-
+  SubscriptionId SubscribeToPermissionStatusChange(
+      blink::PermissionType permission,
+      RenderProcessHost* render_process_host,
+      RenderFrameHost* render_frame_host,
+      const GURL& requesting_origin,
+      bool should_include_device_status,
+      base::RepeatingCallback<void(blink::mojom::PermissionStatus)> callback)
+      override;
   void UnsubscribeFromPermissionStatusChange(
-      content::PermissionController::SubscriptionId subscription_id) override;
+      SubscriptionId subscription_id) override;
+
   void SetPermission(
       blink::PermissionType permission,
       blink::mojom::PermissionStatus status,
@@ -113,9 +119,6 @@ class WebTestPermissionManager
     bool operator==(const PermissionDescription& other) const;
     bool operator!=(const PermissionDescription& other) const;
 
-    bool operator==(PermissionStatusSubscription* other) const;
-    bool operator!=(PermissionStatusSubscription* other) const;
-
     // Hash operator for hash maps.
     struct Hash {
       size_t operator()(const PermissionDescription& description) const;
@@ -126,6 +129,9 @@ class WebTestPermissionManager
     GURL embedding_origin;
   };
 
+  struct Subscription;
+  using SubscriptionsMap =
+      base::IDMap<std::unique_ptr<Subscription>, SubscriptionId>;
   using PermissionsMap = std::unordered_map<PermissionDescription,
                                             blink::mojom::PermissionStatus,
                                             PermissionDescription::Hash>;
@@ -163,6 +169,10 @@ class WebTestPermissionManager
       {blink::PermissionType::TOP_LEVEL_STORAGE_ACCESS,
        blink::mojom::PermissionStatus::ASK},
   };
+
+  // List of subscribers currently listening to permission changes.
+  SubscriptionsMap subscriptions_;
+  SubscriptionId::Generator subscription_id_generator_;
 
   mojo::ReceiverSet<blink::test::mojom::PermissionAutomation> receivers_;
 };
