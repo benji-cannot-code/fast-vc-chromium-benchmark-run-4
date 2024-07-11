0a45809921d3c7a5009bf2e5aa6601ecb01415f3
Nan Lin <linnan@chromium.org>
2024-07-11 18:27:23 +0000

    Default enable feature AttributionSourceDestinationLimit
    Bug: 347657122
    Change-Id: I8d1bf66128334f1120f849cbe28a534bd064aee6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5615290
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326292}

--- components/attribution_reporting/source_registration_unittest.cc
+++ components/attribution_reporting/source_registration_unittest.cc
@@ -383,7 +383,8 @@ TEST(SourceRegistrationTest, ToJson) {
             "aggregatable_debug_reporting": {
               "budget": 0,
               "key_piece": "0x0"
-            }
+            },
+            "destination_limit_priority": "0"
           })json",
       },
       {
@@ -413,6 +414,7 @@ TEST(SourceRegistrationTest, ToJson) {
                               *AggregatableDebugReportingContribution::Create(
                                   /*key_piece=*/10, /*value=*/12)}},
                             /*aggregation_coordinator_origin=*/std::nullopt));
+                r.destination_limit_priority = 6;
               }),
           R"json({
             "aggregatable_report_window": 1,
@@ -444,7 +446,8 @@ TEST(SourceRegistrationTest, ToJson) {
                   "value": 12
                 }
               ]
-            }
+            },
+            "destination_limit_priority": "6"
           })json",
       },
   };
