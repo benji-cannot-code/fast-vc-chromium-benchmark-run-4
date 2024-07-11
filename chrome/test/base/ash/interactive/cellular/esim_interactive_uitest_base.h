018e09978989814b52aa799d6248f35013b46e1a
Jason Zhang <jiajunz@google.com>
2024-07-11 18:03:42 +0000

    test: Add create custom APN interactive ui test
    Added create default custom APN for APN revamp.
    
    Bug: b/340308659, b/338649197
    Change-Id: Ifd1d43512b2b948316523ef1a526faafa3e4d530
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690474
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326272}

---
+++ chrome/test/base/ash/interactive/cellular/esim_interactive_uitest_base.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_ASH_INTERACTIVE_CELLULAR_ESIM_INTERACTIVE_UITEST_BASE_H_
+#define CHROME_TEST_BASE_ASH_INTERACTIVE_CELLULAR_ESIM_INTERACTIVE_UITEST_BASE_H_
+
+#include "chrome/test/base/ash/interactive/cellular/esim_util.h"
+#include "chrome/test/base/ash/interactive/interactive_ash_test.h"
+
+namespace ash {
+
+// Base test of the eSIM interactive UI tests. `SetUpOnMainThread` will add
+// an EUICC with a carrier profile and connect to the eSIM service.
+class EsimInteractiveUiTestBase : public InteractiveAshTest {
+ protected:
+  // InteractiveAshTest:
+  void SetUpOnMainThread() override;
+
+  const EsimInfo& esim_info() { return esim_info_; }
+  const EuiccInfo& euicc_info() { return euicc_info_; }
+
+ private:
+  const EuiccInfo euicc_info_ = EuiccInfo{/*id=*/0};
+  const EsimInfo esim_info_ = EsimInfo{/*id=*/0};
+};
+
+}  // namespace ash
+
+#endif  // CHROME_TEST_BASE_ASH_INTERACTIVE_CELLULAR_ESIM_INTERACTIVE_UITEST_BASE_H_
