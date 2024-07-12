ee7dd7b007348386f97861ea76e6e090c5e28e2f
Ashish Kumar <ashishkum@microsoft.com>
2024-07-12 18:17:17 +0000

    [views-ax] Introducing setters for kDefaultActionVerb attribute
    Follow-up CLs make use of these.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I6266c20285ab3815dbab1d5ad79e555b188f4395
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5702061
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1326899}

--- ui/views/accessibility/view_accessibility.h
+++ ui/views/accessibility/view_accessibility.h
@@ -357,6 +357,10 @@ class VIEWS_EXPORT ViewAccessibility : public WidgetObserver {
   void SetValue(const std::u16string& value);
   void RemoveValue();
 
+  void SetDefaultActionVerb(
+      const ax::mojom::DefaultActionVerb default_action_verb);
+  void RemoveDefaultActionVerb();
+
   void SetHierarchicalLevel(int hierarchical_level);
 
   // Updates the focusable state of the `data_` object.
