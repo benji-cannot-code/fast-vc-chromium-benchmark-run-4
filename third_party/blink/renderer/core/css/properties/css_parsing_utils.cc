3795dcf843fa9371a5b716c3e09eedccfb49bef4
Steinar H. Gunderson <sesse@chromium.org>
2024-07-12 15:04:48 +0000

    Move registered custom property parsing to the streaming parser.
    It turned out the inspector style sheet was sending different values
    for the tokenized and untokenized values, so that had to be fixed
    as part of this change.
    
    Bug: 352467344
    Change-Id: I4d8b5b4ddb710d39ed5d5636b555d5e2b4469d38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5694985
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326775}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -1779,7 +1779,10 @@ template CSSPrimitiveValue* ConsumeTime(
     const CSSParserContext& context,
     CSSPrimitiveValue::ValueRange value_range);
 
-CSSPrimitiveValue* ConsumeResolution(CSSParserTokenRange& range,
+template <typename T>
+  requires std::is_same_v<T, CSSParserTokenStream> ||
+           std::is_same_v<T, CSSParserTokenRange>
+CSSPrimitiveValue* ConsumeResolution(T& range,
                                      const CSSParserContext& context) {
   if (const CSSParserToken& token = range.Peek();
       token.GetType() == kDimensionToken) {
@@ -1805,6 +1808,11 @@ CSSPrimitiveValue* ConsumeResolution(CSSParserTokenRange& range,
 
   return nullptr;
 }
+
+template CSSPrimitiveValue* ConsumeResolution(CSSParserTokenRange& range,
+                                              const CSSParserContext& context);
+template CSSPrimitiveValue* ConsumeResolution(CSSParserTokenStream& range,
+                                              const CSSParserContext& context);
 
 // https://drafts.csswg.org/css-values-4/#ratio-value
 //
