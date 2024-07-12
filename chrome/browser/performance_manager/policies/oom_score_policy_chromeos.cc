593590ebad87995db127c72b7d82bbca634be4bb
Fei Shao <fshao@chromium.org>
2024-07-12 15:52:02 +0000

    Update OOM score on focus change event as well
    Relying existing observer events to update OOM score doesn't cover
    particular heavy memory-demanding scenarios.
    
    We observed a case that high-resolution video playback with HW decoders
    requires large amount of RAM to run on MTK platforms.
    So, if users initiate multiple 4K videos in short time via Gallery App
    and all videos are visible (i.e. OnIsVisibleChanged() path will not be
    invoked), then only the first OnPageNodeAdded() will do the update,
    leading to outdated OOM scores for a stressed memory state.
    In this case, the tabs are less likely to be killed as they all have low
    OOM scores, so system can easily get stuck on the next memory allocation
    request, leading to indefinite hang.
    
    To address this, we can also monitor focus change events also - to start
    a new tab or tasks, the tab focus change is almost inevitable. This
    ensures the OOM scores will remain fresh and Chrome can effectively
    discard tabs when necessary.
    
    Bug: b:343810049, b:339439618
    
    Change-Id: I3d1be50955c38aa7377a2282a48ec5daed842e8d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686854
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Vovo Yang <vovoy@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326791}

--- chrome/browser/performance_manager/policies/oom_score_policy_chromeos.cc
+++ chrome/browser/performance_manager/policies/oom_score_policy_chromeos.cc
@@ -47,6 +47,10 @@ void OomScorePolicyChromeOS::OnBeforePageNodeRemoved(
   HandlePageNodeEventsThrottled();
 }
 
+void OomScorePolicyChromeOS::OnIsFocusedChanged(const PageNode* page_node) {
+  HandlePageNodeEventsThrottled();
+}
+
 void OomScorePolicyChromeOS::OnIsVisibleChanged(const PageNode* page_node) {
   HandlePageNodeEventsThrottled();
 }
