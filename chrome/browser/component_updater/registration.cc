6fa760a03d0259f6da266cf78ec23f86346ab9f7
Andrew Rayskiy <greengrape@google.com>
2024-07-12 18:42:13 +0000

    Create an installer policy for the IWA Key Distribution component
    The installation is guarded behind kIwaKeyDistributionComponent flag
    which is disabled by default.
    
    Bug: 346753548
    Change-Id: Idc3cc4b30c401c13e81ef15b012e40881bec2727
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679202
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326914}

--- chrome/browser/component_updater/registration.cc
+++ chrome/browser/component_updater/registration.cc
@@ -72,6 +72,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(IS_ANDROID)
 
 #if !BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/component_updater/iwa_key_distribution_component_installer.h"
 #include "chrome/browser/component_updater/zxcvbn_data_component_installer.h"
 #include "chrome/browser/resource_coordinator/tab_manager.h"
 #include "media/base/media_switches.h"
@@ -199,6 +200,7 @@ void RegisterComponentsForUpdate() {
 #endif
 
 #if !BUILDFLAG(IS_ANDROID)
+  RegisterIwaKeyDistributionComponent(cus);
   RegisterZxcvbnDataComponent(cus);
 #endif  // !BUILDFLAG(IS_ANDROID)
 
