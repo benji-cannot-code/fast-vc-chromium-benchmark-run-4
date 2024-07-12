bf5a48dc0b09bf71148eac7d052bda0266fa33b0
Niko Tsirakis <ntsirakis@google.com>
2024-07-12 16:33:47 +0000

    [chromebox_for_meetings] Batch LogPayloads together
    Prior to this commit, every data source would generate
    one TransportPayload for every new set of data. This means
    that a maximum of <# of data sources> transport payloads
    will be sent out every minute, which is problematic when
    scaling to thousands of machines.
    
    This commit batches source data together in a single
    TransportPayload until it meets one of two conditions:
    
    1. The payload grows to a pre-determined max size
    2. A pre-determined timeout is reached
    
    In testing, this reduced QPS by about 80%.
    
    Bug: b:340913913
    Change-Id: I28bbeb3448d69769a654a95a368a2f90b6bfbb69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598582
    Commit-Queue: Niko Tsirakis <ntsirakis@google.com>
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326817}

--- chrome/browser/ash/chromebox_for_meetings/artemis/data_aggregator_service.h
+++ chrome/browser/ash/chromebox_for_meetings/artemis/data_aggregator_service.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_CHROMEBOX_FOR_MEETINGS_ARTEMIS_DATA_AGGREGATOR_SERVICE_H_
 #define CHROME_BROWSER_ASH_CHROMEBOX_FOR_MEETINGS_ARTEMIS_DATA_AGGREGATOR_SERVICE_H_
 
+#include "base/time/time.h"
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/command_source.h"
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/log_source.h"
 #include "chromeos/ash/components/dbus/chromebox_for_meetings/cfm_observer.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/services/chromebox_for_meetings/public/proto/transport_payload.pb.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
+#include "net/base/backoff_entry.h"
 
 namespace ash::cfm {
 
@@ -89,14 +91,12 @@ class DataAggregatorService : public CfmObserver,
   void StoreDeviceId(chromeos::cfm::mojom::PolicyInfoPtr policy_info);
   void StartFetchTimer();
   void FetchFromAllSourcesAndEnqueue();
-  void EnqueueData(const std::string& source_name,
-                   const std::vector<std::string>& serialized_entries);
-  void WrapEntriesInTransportPayload(
+  void AppendEntriesToActivePayload(
       const std::string& source_name,
-      const std::vector<std::string>& serialized_entries,
-      proto::TransportPayload* transport_payload);
-  void HandleEnqueueResponse(const std::string& source_name,
-                             chromeos::cfm::mojom::LoggerStatusPtr status);
+      const std::vector<std::string>& serialized_entries);
+  bool IsPayloadReadyForUpload() const;
+  void EnqueueTransportPayload();
+  void HandleEnqueueResponse(chromeos::cfm::mojom::LoggerStatusPtr status);
 
   chromeos::cfm::ServiceAdaptor service_adaptor_;
   mojo::ReceiverSet<mojom::DataAggregator> receivers_;
@@ -114,8 +114,22 @@ class DataAggregatorService : public CfmObserver,
   // Remote endpoint for CfmDeviceInfoService
   mojo::Remote<chromeos::cfm::mojom::MeetDevicesInfo> device_info_remote_;
 
-  // Unique device ID for the CfM that is permanent across provisioning.
-  std::string device_id_;
+  // The current payload that is to be eventually Enqueue()'d to the
+  // CfmLogger. This will collect data until certain conditions are met
+  // (see IsPayloadReadyForUpload() method for details).
+  proto::TransportPayload active_transport_payload_;
+
+  // Used to track the time since we last pushed a payload to the wire.
+  // Will be used as a timeout of sorts for the next push.
+  base::TimeTicks last_upload_time_;
+
+  // Set to true between when we call Enqueue() and when we get a
+  // successful callback response.
+  bool enqueue_in_progress_ = false;
+
+  // A backoff retry timer that automatically adjusts itself if
+  // the initial enqueue fails, to avoid a DoS.
+  net::BackoffEntry enqueue_retry_backoff_;
 
   // Must be the last class member.
   base::WeakPtrFactory<DataAggregatorService> weak_ptr_factory_{this};
