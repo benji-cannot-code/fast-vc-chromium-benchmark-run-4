8758e3ecc4cde77206a78da17e3b260e92dfaad1
Divyansh Mangal <dmangal@microsoft.com>
2024-07-12 06:57:59 +0000

    [views-ax] Migrating kExpanded and kCollapsed attribute to new system for combobox view
    This CL migrates the kExpanded and kCollapsed state in
    ui\views\controls\combobox\combobox.cc whenever its value should
    change, rather than querying the value and computing it only when needed.
    
    This CL also contains unit tests for the view testing
    the accessibility states when the view is open and closed.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ibc51ad9a8d0ea3f7dbe705f0c8924103414b6b37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5653576
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1326584}

--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -173,6 +173,7 @@ Combobox::Combobox(ui::ComboboxModel* model) {
                                                 GetCornerRadius());
 
   GetViewAccessibility().SetProperties(ax::mojom::Role::kComboBoxSelect);
+  UpdateExpandedCollapsedAccessibleState();
 }
 
 Combobox::~Combobox() {
@@ -494,11 +495,6 @@ void Combobox::OnBlur() {
 
 void Combobox::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   View::GetAccessibleNodeData(node_data);
-  if (menu_runner_) {
-    node_data->AddState(ax::mojom::State::kExpanded);
-  } else {
-    node_data->AddState(ax::mojom::State::kCollapsed);
-  }
 
   if (GetEnabled()) {
     node_data->SetDefaultActionVerb(ax::mojom::DefaultActionVerb::kOpen);
@@ -532,6 +528,7 @@ void Combobox::OnComboboxModelChanged(ui::ComboboxModel* model) {
 
   if (IsMenuRunning()) {
     menu_runner_.reset();
+    UpdateExpandedCollapsedAccessibleState();
   }
 
   // If the selection is no longer valid (or the model is empty), restore the
@@ -696,7 +693,7 @@ void Combobox::ShowDropDownMenu(ui::MenuSourceType source_type) {
   }
   menu_runner_->RunMenuAt(GetWidget(), nullptr, bounds,
                           MenuAnchorPosition::kTopLeft, source_type);
-  NotifyAccessibilityEvent(ax::mojom::Event::kExpandedChanged, true);
+  UpdateExpandedCollapsedAccessibleState();
 }
 
 void Combobox::OnMenuClosed(Button::ButtonState original_button_state) {
@@ -708,7 +705,7 @@ void Combobox::OnMenuClosed(Button::ButtonState original_button_state) {
   menu_runner_.reset();
   arrow_button_->SetState(original_button_state);
   closed_time_ = base::TimeTicks::Now();
-  NotifyAccessibilityEvent(ax::mojom::Event::kExpandedChanged, true);
+  UpdateExpandedCollapsedAccessibleState();
 }
 
 void Combobox::MenuSelectionAt(size_t index) {
@@ -792,6 +789,14 @@ const gfx::FontList& Combobox::GetForegroundFontList() const {
     return TypographyProvider::Get().GetFont(kContext, *foreground_text_style_);
   }
   return GetFontList();
+}
+
+void Combobox::UpdateExpandedCollapsedAccessibleState() const {
+  if (menu_runner_) {
+    GetViewAccessibility().SetIsExpanded();
+  } else {
+    GetViewAccessibility().SetIsCollapsed();
+  }
 }
 
 BEGIN_METADATA(Combobox)
