d4a92a8bc41b84ba67ba48f6db23d6e1283f08c7
Kent Tamura <tkent@chromium.org>
2024-07-12 11:59:09 +0000

    spanification: Update fragment_items.{h,cc}
    Remove `#pragma allow_unsafe_buffers` from fragment_items.{h,cc},
    and fix errors.
    
    Bug: 351564777
    Change-Id: Idea02563792df18b6a0bdc3fa2892eeb9658891b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5700525
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326687}

--- third_party/blink/renderer/core/layout/inline/fragment_items.cc
+++ third_party/blink/renderer/core/layout/inline/fragment_items.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/layout/inline/fragment_items.h"
 
 #include "base/ranges/algorithm.h"
@@ -71,8 +66,8 @@ FragmentItems::FragmentItems(const FragmentItems& other)
 }
 
 bool FragmentItems::IsSubSpan(const Span& span) const {
-  return span.empty() ||
-         (span.data() >= ItemsData() && &span.back() < ItemsData() + Size());
+  return span.empty() || (span.data() >= ItemsData() && !items_.empty() &&
+                          &span.back() <= &items_.back());
 }
 
 void FragmentItems::FinalizeAfterLayout(
--- third_party/blink/renderer/core/layout/inline/fragment_items.h
+++ third_party/blink/renderer/core/layout/inline/fragment_items.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_INLINE_FRAGMENT_ITEMS_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_LAYOUT_INLINE_FRAGMENT_ITEMS_H_
 
@@ -35,7 +30,7 @@ class CORE_EXPORT FragmentItems final {
   wtf_size_t Size() const { return items_.size(); }
 
   using Span = base::span<const FragmentItem>;
-  Span Items() const { return base::make_span(ItemsData(), items_.size()); }
+  Span Items() const { return items_.MakeSpan(); }
   bool Equals(const Span& span) const {
     return ItemsData() == span.data() && Size() == span.size();
   }
