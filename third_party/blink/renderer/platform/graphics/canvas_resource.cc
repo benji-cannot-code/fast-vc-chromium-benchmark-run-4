7714efbd90830928ced589d4a3ed4af8a5c1dcfc
Colin Blundell <blundell@chromium.org>
2024-07-12 08:56:35 +0000

    [Blink] Make CanvasResource::TearDown() non-virtual
    Currently, CanvasResource::TearDown() is called only from subclasses'
    destructors via CanvasResource::OnDestruction(). Code relies on the
    invariant that TearDown() is not called in any other circumstances.
    
    To harden this invariant and make the code easier to reason about, I am
    going to be eliminating CanvasResource::TearDown() altogether and
    inlining the relevant code directly in subclasses' destructors. This CL
    takes a preparatory step by moving the TearDown() calls to be in
    subclasses' destructors rather than CanvasResource::OnDestroy(). This
    necessitates changing CanvasResource::OnDestroy() to return a bool
    indicating whether teardown of GPU context-associated state is
    possible.
    
    Bug: 352263194
    Change-Id: I33a18bc3d56d5a6ae4493d3988120948c7d2bbb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5701245
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326630}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -71,7 +71,7 @@ CanvasResource::~CanvasResource() {
 #endif
 }
 
-void CanvasResource::OnDestroy() {
+bool CanvasResource::OnDestroy() {
 #if DCHECK_IS_ON()
   did_call_on_destroy_ = true;
 #endif
@@ -83,13 +83,13 @@ void CanvasResource::OnDestroy() {
     // done and any resources tied to the context may be leaked. As such, this
     // case should arise only when the owning thread and its associated context
     // were torn down before this resource could be deleted.
-    return;
+    return false;
   }
 
   if (provider_) {
     provider_->OnDestroyResource();
   }
-  TearDown();
+  return true;
 }
 
 void CanvasResource::Release() {
@@ -292,7 +292,10 @@ CanvasResourceSharedBitmap::CanvasResourceSharedBitmap(
 }
 
 CanvasResourceSharedBitmap::~CanvasResourceSharedBitmap() {
-  OnDestroy();
+  if (!OnDestroy()) {
+    return;
+  }
+  TearDown();
 }
 
 bool CanvasResourceSharedBitmap::IsValid() const {
@@ -537,7 +540,10 @@ GrBackendTexture CanvasResourceSharedImage::CreateGrTexture() const {
 }
 
 CanvasResourceSharedImage::~CanvasResourceSharedImage() {
-  OnDestroy();
+  if (!OnDestroy()) {
+    return;
+  }
+  TearDown();
 }
 
 void CanvasResourceSharedImage::TearDown() {
@@ -823,7 +829,10 @@ scoped_refptr<ExternalCanvasResource> ExternalCanvasResource::Create(
 }
 
 ExternalCanvasResource::~ExternalCanvasResource() {
-  OnDestroy();
+  if (!OnDestroy()) {
+    return;
+  }
+  TearDown();
 }
 
 bool ExternalCanvasResource::IsValid() const {
@@ -953,7 +962,10 @@ scoped_refptr<CanvasResourceSwapChain> CanvasResourceSwapChain::Create(
 }
 
 CanvasResourceSwapChain::~CanvasResourceSwapChain() {
-  OnDestroy();
+  if (!OnDestroy()) {
+    return;
+  }
+  TearDown();
 }
 
 bool CanvasResourceSwapChain::IsValid() const {
