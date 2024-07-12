47ca038a9f97e713e0647f51a3779be611f711be
Daniel Murphy <dmurph@chromium.org>
2024-07-12 04:16:34 +0000

    [dPWA] Replacing IsLocallyInstalled with the correct InstallState
    The 'locally installed' term is confusing & encompasses two states:
    - Installed without OS integration (e.g. preinstalled apps)
    - Installed with OS integration
    
    This change is a mechanical non-functional change to removes all
    calls to `IsLocallyInstalled` with the correct `IsInstallState` or
    `GetInstallState` call.
    
    This is a 'git cl split' patch for directory:
    chrome/browser/ui/ash
    
    This CL was uploaded by git cl split.
    
    R=oshima@chromium.org
    
    Bug: 340952100,b:340951864
    Change-Id: I330b1f20f381795fe5d81eddcf744f4645c86927
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5690626
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326541}

--- chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.cc
+++ chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
+#include "chrome/browser/web_applications/proto/web_app_install_state.pb.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
@@ -127,7 +128,8 @@ class AppMatcher {
     DCHECK(profile);
     if (web_app::WebAppProvider* provider =
             web_app::WebAppProvider::GetForLocalAppsUnchecked(profile)) {
-      if (provider->registrar_unsafe().IsLocallyInstalled(app_id)) {
+      if (provider->registrar_unsafe().IsInstallState(
+              app_id, {web_app::proto::INSTALLED_WITH_OS_INTEGRATION})) {
         registrar_ = &provider->registrar_unsafe();
       }
     }
@@ -619,7 +621,8 @@ bool AppShortcutShelfItemController::IsWindowedWebApp() {
           web_app::WebAppProvider::GetForLocalAppsUnchecked(
               ChromeShelfController::instance()->profile())) {
     web_app::WebAppRegistrar& registrar = provider->registrar_unsafe();
-    if (registrar.IsLocallyInstalled(app_id())) {
+    if (registrar.IsInstallState(
+            app_id(), {web_app::proto::INSTALLED_WITH_OS_INTEGRATION})) {
       return registrar.GetAppUserDisplayMode(app_id()) !=
              web_app::mojom::UserDisplayMode::kBrowser;
     }
