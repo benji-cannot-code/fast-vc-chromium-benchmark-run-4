b8ee872e501cb2abd8f795e5cbcee40854adb1c4
Divyansh Mangal <dmangal@microsoft.com>
2024-07-12 09:55:39 +0000

    [views-ax] Migrating kExpanded and kCollapsed attribute to new system for suggestions section
    This CL migrates the kExpanded and kCollapsed state in
    ash\system\holding_space\suggestions_section.cc whenever its value 
    should change, rather than querying the value and computing it only when needed.
    
    This CL also contains unit tests for the view testing
    the accessibility states when the view is expanded and collapsed.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ide8065aa9e926b6926a2b53338981a4e403ce452
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654831
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1326653}

--- ash/system/holding_space/suggestions_section.cc
+++ ash/system/holding_space/suggestions_section.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/rrect_f.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/gfx/text_constants.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/image_view.h"
@@ -104,21 +105,11 @@ class Header : public views::Button {
   }
 
  private:
-  // views::Button:
-  void GetAccessibleNodeData(ui::AXNodeData* node_data) override {
-    views::Button::GetAccessibleNodeData(node_data);
-
-    // Add expanded/collapsed state to `node_data`.
-    auto* prefs = Shell::Get()->session_controller()->GetActivePrefService();
-    node_data->AddState(holding_space_prefs::IsSuggestionsExpanded(prefs)
-                            ? ax::mojom::State::kExpanded
-                            : ax::mojom::State::kCollapsed);
-  }
-
   void OnPressed() {
     auto* prefs = Shell::Get()->session_controller()->GetActivePrefService();
     bool expanded = holding_space_prefs::IsSuggestionsExpanded(prefs);
     holding_space_prefs::SetSuggestionsExpanded(prefs, !expanded);
+    UpdateExpandedCollapsedAccessibleState(!expanded);
 
     holding_space_metrics::RecordSuggestionsAction(
         expanded ? holding_space_metrics::SuggestionsAction::kCollapse
@@ -128,15 +119,20 @@ class Header : public views::Button {
   void UpdateState() {
     // Chevron.
     auto* prefs = Shell::Get()->session_controller()->GetActivePrefService();
+    bool expanded = holding_space_prefs::IsSuggestionsExpanded(prefs);
     chevron_->SetImage(ui::ImageModel::FromVectorIcon(
-        holding_space_prefs::IsSuggestionsExpanded(prefs)
-            ? kChevronUpSmallIcon
-            : kChevronDownSmallIcon,
+        expanded ? kChevronUpSmallIcon : kChevronDownSmallIcon,
         kColorAshIconColorSecondary, kHoldingSpaceSectionChevronIconSize));
 
-    // Accessibility.
-    NotifyAccessibilityEvent(ax::mojom::Event::kStateChanged,
-                             /*send_native_event=*/true);
+    UpdateExpandedCollapsedAccessibleState(expanded);
+  }
+
+  void UpdateExpandedCollapsedAccessibleState(bool expanded) const {
+    if (expanded) {
+      GetViewAccessibility().SetIsExpanded();
+    } else {
+      GetViewAccessibility().SetIsCollapsed();
+    }
   }
 
   // Owned by view hierarchy.
