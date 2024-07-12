baf1d627575bf78ba94c3cfbf25a12d2fdc78bd2
Asami Doi <asamidoi@chromium.org>
2024-07-12 08:21:28 +0000

    [ios] Replace defaultDataStore with dataStoreForIdentifier
    This CL sets a separated data store to WKWebViewConfiguration.
    The separated data store is created by `[WKWebsiteDataStore
    dataStoreForIdentifier:]` with a unique ID. The ID can be obtained
    via `GetWebKitStorageID()` in BrowserState.
    
    So we have 3 options for the data store:
    1. a non persistent data store for an incognito browser
    2. a separated data store for a newly created browser
    3. a default data store for an existing browser
    
    This CL also modifies CRWWKHTTPCookieStore to fix test failures.
    Some EGTests fails with the message
    "CookieManager::SetCanonicalCookieCallback was destroyed without first either being run or its corresponding binding being closed.".
    `HTTPCookieStore` instance becomes nil while tearing down an application
    because it's a weak pointer and a `completionHandler` in
    CRWWKHTTPCookieStore isn't called. Dispatch the callback if the weak
    pointer is nil.
    
    Bug: 342362970
    Change-Id: I06403891eee35950a489a45a204e74c379040079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5593877
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1326619}

--- ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 #import <WebKit/WebKit.h>
+
 #import <vector>
 
 #import "base/check.h"
@@ -76,11 +77,31 @@ void WKWebViewConfigurationProvider::ResetWithWebViewConfiguration(
     configuration_ = [configuration copy];
   }
 
-  if (browser_state_->IsOffTheRecord() && configuration == nil) {
-    // Set the data store only when configuration is nil because the data store
-    // in the configuration should be used.
-    [configuration_
-        setWebsiteDataStore:[WKWebsiteDataStore nonPersistentDataStore]];
+  // Set the data store only when configuration is nil because the data
+  // store in the configuration should be used.
+  if (configuration == nil) {
+    if (browser_state_->IsOffTheRecord()) {
+      // The data is stored in memory. A new non-persistent data store is
+      // created for each incognito browser state.
+      [configuration_
+          setWebsiteDataStore:[WKWebsiteDataStore nonPersistentDataStore]];
+    } else {
+      const std::string& storage_id = browser_state_->GetWebKitStorageID();
+      if (!storage_id.empty()) {
+        if (@available(iOS 17.0, *)) {
+          // Set the data store to configuration when the browser state is not
+          // incognito and the storage ID exists. `dataStoreForIdentifier:` is
+          // available after iOS 17. Otherwise, use the default data store.
+          [configuration_
+              setWebsiteDataStore:
+                  [WKWebsiteDataStore
+                      dataStoreForIdentifier:
+                          [[NSUUID alloc]
+                              initWithUUIDString:base::SysUTF8ToNSString(
+                                                     storage_id)]]];
+        }
+      }
+    }
   }
 
   // Explicitly set the default data store to the configuration. The data store
