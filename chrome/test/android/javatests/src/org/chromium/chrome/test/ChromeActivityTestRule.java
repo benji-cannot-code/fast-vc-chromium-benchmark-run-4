cabe6d37053fd794d0d7086036c98f2ba52b2a06
Andrew Grieve <agrieve@chromium.org>
2024-07-12 02:02:00 +0000

    Android: Remove custom thread exception handler from ChromeActivityTestRule
    It was doing two things:
    1) Adding a log message to aid with StrictMode failures
    2) Sending the test runner information about which test failed
    
    We no longer enable StrictMode in //chrome, so don't need 1).
    As for 2), this is certainly not the correct spot to add test-runner
    logic, but I'm pretty sure it is broken anyways (not setting the correct
    test class), and not necessary (our test runner will notice the
    failure without it).
    
    Bug: 326415975
    Change-Id: I5223fc1dceece850988618c1f45959a59d8a882a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5697685
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326513}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/ChromeActivityTestRule.java
@@ -12,10 +12,8 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 
 import androidx.test.core.app.ApplicationProvider;
-import androidx.test.internal.runner.listener.InstrumentationResultPrinter;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.hamcrest.Matchers;
@@ -98,8 +96,6 @@ public class ChromeActivityTestRule<T extends ChromeActivity> extends BaseActivi
     @Override
     protected void before() throws Throwable {
         super.before();
-        mDefaultUncaughtExceptionHandler = Thread.getDefaultUncaughtExceptionHandler();
-        Thread.setDefaultUncaughtExceptionHandler(new ChromeUncaughtExceptionHandler());
         ChromeApplicationTestUtils.setUp();
         // Instrumentation infrastructure and tests often access variables from the
         // instrumentation thread for asserts. See crbug.com/1173814 for more
@@ -122,7 +118,6 @@ public class ChromeActivityTestRule<T extends ChromeActivity> extends BaseActivi
 
     @Override
     protected void after() {
-        Thread.setDefaultUncaughtExceptionHandler(mDefaultUncaughtExceptionHandler);
         super.after();
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -555,23 +550,5 @@ public class ChromeActivityTestRule<T extends ChromeActivity> extends BaseActivi
         final Coordinates coord = Coordinates.createFor(getWebContents());
         CriteriaHelper.pollUiThread(
                 coord::frameInfoUpdated, "FrameInfo has not been updated in time.");
-    }
-
-    private class ChromeUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler {
-        @Override
-        public void uncaughtException(Thread t, Throwable e) {
-            String stackTrace = android.util.Log.getStackTraceString(e);
-            if (e.getClass().getName().endsWith("StrictModeViolation")) {
-                stackTrace += "\nSearch logcat for \"StrictMode policy violation\" for full stack.";
-            }
-            Bundle resultsBundle = new Bundle();
-            resultsBundle.putString(
-                    InstrumentationResultPrinter.REPORT_KEY_NAME_CLASS, getClass().getName());
-            resultsBundle.putString(
-                    InstrumentationResultPrinter.REPORT_KEY_NAME_TEST, mCurrentTestName);
-            resultsBundle.putString(InstrumentationResultPrinter.REPORT_KEY_STACK, stackTrace);
-            InstrumentationRegistry.getInstrumentation().sendStatus(-1, resultsBundle);
-            mDefaultUncaughtExceptionHandler.uncaughtException(t, e);
-        }
     }
 }
