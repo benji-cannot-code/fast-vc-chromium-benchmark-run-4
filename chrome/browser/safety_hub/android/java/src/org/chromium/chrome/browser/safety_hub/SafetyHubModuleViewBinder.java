1673850c7ceda887e02728f1498b4c4ac6590dd8
Zaina Al-Mashni <zalmashni@google.com>
2024-07-12 13:32:37 +0000

    [SHoA] Add an API to determine the module states
    Refactor out the logic to determine the state of each module into an API
    that can be re-used to determine the overall browser state ("all good"
    state if all ModuleOptions have a ModuleState >= INFO) and the order
    of the modules in the SH UI (WARNING states should be ordered first in
    the preference list).
    
    This adds the base logic for determining the module state, the "all good" indicator and ordering will be done in a followup.
    
    Bug: 324562205
    Change-Id: I7575fc93d8c9ab0d8d0d06056c4c6a2ca64847e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5692062
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326724}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleViewBinder.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleViewBinder.java
@@ -5,10 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.safety_hub;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
-
-import androidx.preference.Preference;
 
 import org.chromium.chrome.browser.omaha.UpdateStatusProvider;
 import org.chromium.chrome.browser.safe_browsing.SafeBrowsingState;
@@ -85,10 +84,10 @@ public class SafetyHubModuleViewBinder {
         @SafeBrowsingState
         int safeBrowsingState = model.get(SafetyHubModuleProperties.SAFE_BROWSING_STATE);
         boolean managed = model.get(SafetyHubModuleProperties.IS_CONTROLLED_BY_POLICY);
-
+        @SafetyHubModuleProperties.ModuleState
+        int state = getModuleState(model, SafetyHubModuleProperties.ModuleOption.SAFE_BROWSING);
         String title;
         String summary;
-        Drawable iconDrawable;
         String primaryButtonText = null;
         String secondaryButtonText = null;
         View.OnClickListener primaryButtonListener = null;
@@ -101,7 +100,6 @@ public class SafetyHubModuleViewBinder {
                         preference
                                 .getContext()
                                 .getString(R.string.safety_hub_safe_browsing_on_title);
-                iconDrawable = getCheckmarkIcon(preference);
 
                 if (managed) {
                     summary =
@@ -127,7 +125,6 @@ public class SafetyHubModuleViewBinder {
                         preference
                                 .getContext()
                                 .getString(R.string.safety_hub_safe_browsing_enhanced_title);
-                iconDrawable = getCheckmarkIcon(preference);
 
                 if (managed) {
                     summary =
@@ -156,14 +153,12 @@ public class SafetyHubModuleViewBinder {
                                 .getString(R.string.prefs_safe_browsing_no_protection_summary);
 
                 if (managed) {
-                    iconDrawable = getCheckmarkIcon(preference);
                     summary =
                             preference
                                     .getContext()
                                     .getString(
                                             R.string.safety_hub_safe_browsing_off_summary_managed);
                 } else {
-                    iconDrawable = getErrorIcon(preference);
                     summary =
                             preference
                                     .getContext()
@@ -176,7 +171,6 @@ public class SafetyHubModuleViewBinder {
                 }
         }
 
-        preference.setIcon(iconDrawable);
         preference.setTitle(title);
         preference.setSummary(summary);
         preference.setPrimaryButtonText(primaryButtonText);
@@ -184,14 +178,17 @@ public class SafetyHubModuleViewBinder {
         preference.setPrimaryButtonClickListener(primaryButtonListener);
         preference.setSecondaryButtonClickListener(secondaryButtonListener);
         preference.setExpanded(expanded);
+
+        preference.setIcon(getIconForModuleState(preference.getContext(), state, managed));
     }
 
     private static void updatePasswordCheckModule(
             SafetyHubExpandablePreference preference, PropertyModel model) {
         int compromisedPasswordsCount =
                 model.get(SafetyHubModuleProperties.COMPROMISED_PASSWORDS_COUNT);
+        @SafetyHubModuleProperties.ModuleState
+        int state = getModuleState(model, SafetyHubModuleProperties.ModuleOption.ACCOUNT_PASSWORDS);
         String title;
-        Drawable iconDrawable;
         String primaryButtonText = null;
         String secondaryButtonText = null;
         View.OnClickListener primaryButtonListener = null;
@@ -208,7 +205,6 @@ public class SafetyHubModuleViewBinder {
                                     compromisedPasswordsCount,
                                     compromisedPasswordsCount);
 
-            iconDrawable = getErrorIcon(preference);
             primaryButtonText =
                     preference
                             .getContext()
@@ -217,7 +213,6 @@ public class SafetyHubModuleViewBinder {
             expanded = true;
         } else {
             title = preference.getContext().getString(R.string.safety_check_passwords_safe);
-            iconDrawable = getCheckmarkIcon(preference);
             secondaryButtonText =
                     preference
                             .getContext()
@@ -226,22 +221,23 @@ public class SafetyHubModuleViewBinder {
                     model.get(SafetyHubModuleProperties.SAFE_STATE_BUTTON_LISTENER);
         }
         preference.setTitle(title);
-        preference.setIcon(iconDrawable);
         preference.setPrimaryButtonText(primaryButtonText);
         preference.setSecondaryButtonText(secondaryButtonText);
         preference.setPrimaryButtonClickListener(primaryButtonListener);
         preference.setSecondaryButtonClickListener(secondaryButtonListener);
         preference.setExpanded(expanded);
+
+        preference.setIcon(getIconForModuleState(preference.getContext(), state, false));
     }
 
     private static void updateUpdateCheckModule(
             SafetyHubExpandablePreference preference, PropertyModel model) {
         UpdateStatusProvider.UpdateStatus updateStatus =
                 model.get(SafetyHubModuleProperties.UPDATE_STATUS);
-
+        @SafetyHubModuleProperties.ModuleState
+        int state = getModuleState(model, SafetyHubModuleProperties.ModuleOption.UPDATE_CHECK);
         String title;
         String summary = null;
-        Drawable iconDrawable;
         String primaryButtonText = null;
         String secondaryButtonText = null;
         View.OnClickListener primaryButtonListener = null;
@@ -250,7 +246,6 @@ public class SafetyHubModuleViewBinder {
 
         if (updateStatus == null) {
             title = preference.getContext().getString(R.string.safety_check_updates_updated);
-            iconDrawable = getCheckmarkIcon(preference);
             secondaryButtonText =
                     preference.getContext().getString(R.string.safety_hub_go_to_google_play_button);
             secondaryButtonListener =
@@ -263,7 +258,6 @@ public class SafetyHubModuleViewBinder {
                                     .getContext()
                                     .getString(R.string.menu_update_unsupported_summary_default);
                     summary = updateStatus.latestUnsupportedVersion;
-                    iconDrawable = getErrorIcon(preference);
                     expanded = true;
                     break;
                 case UpdateStatusProvider.UpdateState.UPDATE_AVAILABLE:
@@ -271,7 +265,6 @@ public class SafetyHubModuleViewBinder {
                             preference
                                     .getContext()
                                     .getString(R.string.safety_check_updates_outdated);
-                    iconDrawable = getErrorIcon(preference);
                     primaryButtonText = preference.getContext().getString(R.string.menu_update);
                     primaryButtonListener =
                             model.get(SafetyHubModuleProperties.PRIMARY_BUTTON_LISTENER);
@@ -283,7 +276,6 @@ public class SafetyHubModuleViewBinder {
                                     .getContext()
                                     .getString(R.string.safety_check_updates_updated);
                     summary = updateStatus.latestVersion;
-                    iconDrawable = getCheckmarkIcon(preference);
                     secondaryButtonText =
                             preference
                                     .getContext()
@@ -293,7 +285,6 @@ public class SafetyHubModuleViewBinder {
             }
         }
 
-        preference.setIcon(iconDrawable);
         preference.setTitle(title);
         preference.setSummary(summary);
         preference.setPrimaryButtonText(primaryButtonText);
@@ -301,14 +292,18 @@ public class SafetyHubModuleViewBinder {
         preference.setPrimaryButtonClickListener(primaryButtonListener);
         preference.setSecondaryButtonClickListener(secondaryButtonListener);
         preference.setExpanded(expanded);
+
+        preference.setIcon(getIconForModuleState(preference.getContext(), state, false));
     }
 
     private static void updatePermissionsModule(
             SafetyHubExpandablePreference preference, PropertyModel model) {
         int sitesWithUnusedPermissionsCount =
                 model.get(SafetyHubModuleProperties.SITES_WITH_UNUSED_PERMISSIONS_COUNT);
+        @SafetyHubModuleProperties.ModuleState
+        int state =
+                getModuleState(model, SafetyHubModuleProperties.ModuleOption.UNUSED_PERMISSIONS);
         String title;
-        Drawable iconDrawable;
         String primaryButtonText = null;
         String secondaryButtonText;
         View.OnClickListener primaryButtonListener = null;
@@ -325,7 +320,6 @@ public class SafetyHubModuleViewBinder {
                                     sitesWithUnusedPermissionsCount,
                                     sitesWithUnusedPermissionsCount);
 
-            iconDrawable = getWarningIcon(preference);
             primaryButtonText = preference.getContext().getString(R.string.got_it);
             secondaryButtonText =
                     preference.getContext().getString(R.string.safety_hub_view_sites_button);
@@ -335,27 +329,30 @@ public class SafetyHubModuleViewBinder {
             expanded = true;
         } else {
             title = preference.getContext().getString(R.string.safety_hub_permissions_ok_title);
-            iconDrawable = getCheckmarkIcon(preference);
             secondaryButtonText =
                     preference.getContext().getString(R.string.safety_hub_go_to_settings_button);
             secondaryButtonListener =
                     model.get(SafetyHubModuleProperties.SAFE_STATE_BUTTON_LISTENER);
         }
+
         preference.setTitle(title);
-        preference.setIcon(iconDrawable);
         preference.setPrimaryButtonText(primaryButtonText);
         preference.setSecondaryButtonText(secondaryButtonText);
         preference.setPrimaryButtonClickListener(primaryButtonListener);
         preference.setSecondaryButtonClickListener(secondaryButtonListener);
         preference.setExpanded(expanded);
+
+        preference.setIcon(getIconForModuleState(preference.getContext(), state, false));
     }
 
     private static void updateNotificationsReviewModule(
             SafetyHubExpandablePreference preference, PropertyModel model) {
         int notificationPermissionsForReviewCount =
                 model.get(SafetyHubModuleProperties.NOTIFICATION_PERMISSIONS_FOR_REVIEW_COUNT);
+        @SafetyHubModuleProperties.ModuleState
+        int state =
+                getModuleState(model, SafetyHubModuleProperties.ModuleOption.NOTIFICATION_REVIEW);
         String title;
-        Drawable iconDrawable;
         String primaryButtonText = null;
         String secondaryButtonText;
         View.OnClickListener primaryButtonListener = null;
@@ -371,8 +368,6 @@ public class SafetyHubModuleViewBinder {
                                     R.plurals.safety_hub_notifications_review_warning_title,
                                     notificationPermissionsForReviewCount,
                                     notificationPermissionsForReviewCount);
-
-            iconDrawable = getWarningIcon(preference);
             primaryButtonText =
                     preference
                             .getContext()
@@ -388,7 +383,6 @@ public class SafetyHubModuleViewBinder {
                     preference
                             .getContext()
                             .getString(R.string.safety_hub_notifications_review_ok_title);
-            iconDrawable = getCheckmarkIcon(preference);
             secondaryButtonText =
                     preference
                             .getContext()
@@ -396,29 +390,84 @@ public class SafetyHubModuleViewBinder {
             secondaryButtonListener =
                     model.get(SafetyHubModuleProperties.SAFE_STATE_BUTTON_LISTENER);
         }
+
         preference.setTitle(title);
-        preference.setIcon(iconDrawable);
         preference.setPrimaryButtonText(primaryButtonText);
         preference.setSecondaryButtonText(secondaryButtonText);
         preference.setPrimaryButtonClickListener(primaryButtonListener);
         preference.setSecondaryButtonClickListener(secondaryButtonListener);
         preference.setExpanded(expanded);
+
+        preference.setIcon(getIconForModuleState(preference.getContext(), state, false));
     }
 
-    private static Drawable getErrorIcon(Preference preference) {
-        return SettingsUtils.getTintedIcon(
-                preference.getContext(), R.drawable.ic_error, R.color.default_red);
+    private static Drawable getIconForModuleState(
+            Context context, @SafetyHubModuleProperties.ModuleState int state, boolean managed) {
+        switch (state) {
+            case SafetyHubModuleProperties.ModuleState.SAFE:
+                return SettingsUtils.getTintedIcon(
+                        context, R.drawable.ic_checkmark_24dp, R.color.default_green);
+            case SafetyHubModuleProperties.ModuleState.INFO:
+                return SettingsUtils.getTintedIcon(
+                        context,
+                        R.drawable.btn_info,
+                        R.color.default_icon_color_secondary_tint_list);
+            case SafetyHubModuleProperties.ModuleState.WARNING:
+                return managed
+                        ? SettingsUtils.getTintedIcon(
+                                context,
+                                R.drawable.btn_info,
+                                R.color.default_icon_color_secondary_tint_list)
+                        : SettingsUtils.getTintedIcon(
+                                context, R.drawable.ic_error, R.color.default_red);
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 
-    private static Drawable getWarningIcon(Preference preference) {
-        return SettingsUtils.getTintedIcon(
-                preference.getContext(),
-                R.drawable.btn_info,
-                R.color.default_icon_color_secondary_tint_list);
-    }
-
-    private static Drawable getCheckmarkIcon(Preference preference) {
-        return SettingsUtils.getTintedIcon(
-                preference.getContext(), R.drawable.ic_checkmark_24dp, R.color.default_green);
+    private static @SafetyHubModuleProperties.ModuleState int getModuleState(
+            PropertyModel model, @SafetyHubModuleProperties.ModuleOption int option) {
+        switch (option) {
+            case SafetyHubModuleProperties.ModuleOption.ACCOUNT_PASSWORDS:
+                int compromisedPasswordsCount =
+                        model.get(SafetyHubModuleProperties.COMPROMISED_PASSWORDS_COUNT);
+                return compromisedPasswordsCount > 0
+                        ? SafetyHubModuleProperties.ModuleState.WARNING
+                        : SafetyHubModuleProperties.ModuleState.SAFE;
+            case SafetyHubModuleProperties.ModuleOption.UPDATE_CHECK:
+                UpdateStatusProvider.UpdateStatus updateStatus =
+                        model.get(SafetyHubModuleProperties.UPDATE_STATUS);
+                if (updateStatus != null
+                        && (updateStatus.updateState
+                                        == UpdateStatusProvider.UpdateState.UPDATE_AVAILABLE
+                                || updateStatus.updateState
+                                        == UpdateStatusProvider.UpdateState
+                                                .UNSUPPORTED_OS_VERSION)) {
+                    return SafetyHubModuleProperties.ModuleState.WARNING;
+                }
+                return SafetyHubModuleProperties.ModuleState.SAFE;
+            case SafetyHubModuleProperties.ModuleOption.UNUSED_PERMISSIONS:
+                int permissionsCount =
+                        model.get(SafetyHubModuleProperties.SITES_WITH_UNUSED_PERMISSIONS_COUNT);
+                return permissionsCount > 0
+                        ? SafetyHubModuleProperties.ModuleState.INFO
+                        : SafetyHubModuleProperties.ModuleState.SAFE;
+            case SafetyHubModuleProperties.ModuleOption.NOTIFICATION_REVIEW:
+                int notificationsCount =
+                        model.get(
+                                SafetyHubModuleProperties
+                                        .NOTIFICATION_PERMISSIONS_FOR_REVIEW_COUNT);
+                return notificationsCount > 0
+                        ? SafetyHubModuleProperties.ModuleState.INFO
+                        : SafetyHubModuleProperties.ModuleState.SAFE;
+            case SafetyHubModuleProperties.ModuleOption.SAFE_BROWSING:
+                @SafeBrowsingState
+                int safeBrowsingState = model.get(SafetyHubModuleProperties.SAFE_BROWSING_STATE);
+                return safeBrowsingState == SafeBrowsingState.NO_SAFE_BROWSING
+                        ? SafetyHubModuleProperties.ModuleState.WARNING
+                        : SafetyHubModuleProperties.ModuleState.SAFE;
+            default:
+                throw new IllegalArgumentException();
+        }
     }
 }
