cd0ac8bceade21d81fb0a51d2f6096e5dab27962
Darren Shen <shend@chromium.org>
2024-07-12 00:46:41 +0000

    Format URLs in results in a terser way in Picker.
    Picker shows a list of browsing history results. These results have the
    URL on a secondary label. The UX requirement is to shorten the URL by
    stripping out unnecessary info.
    
    Follow the same pattern in the Omnibox code by using
    url_formatter::FormatUrl.
    
    Fixed: b:350373388, b:345526919
    Change-Id: I70c1f6e0a53ddeba854df6a9ae9d149a0ca718fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5686274
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326485}

--- ash/picker/views/picker_list_item_view.cc
+++ ash/picker/views/picker_list_item_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/style_util.h"
 #include "ash/style/typography.h"
+#include "base/strings/string_util.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkScalar.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -295,6 +296,13 @@ ui::ImageModel PickerListItemView::GetPrimaryImageForTesting() const {
     return image->GetImageModel();
   }
   return ui::ImageModel();
+}
+
+std::u16string_view PickerListItemView::GetSecondaryTextForTesting() const {
+  if (secondary_label_ == nullptr) {
+    return base::EmptyString16();
+  }
+  return secondary_label_->GetText();
 }
 
 void PickerListItemView::UpdateIconWithPreview() {
