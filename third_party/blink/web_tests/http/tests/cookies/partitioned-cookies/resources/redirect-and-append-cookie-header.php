bacf66c55b0ba1667d00c82bba178eb0b76c9a30
selya <selya@chromium.org>
2024-07-12 14:52:51 +0000

    Add test for cookie headers when redirecting back from a cross-site subresource
    While addressing community feedback (in crbug.com/346610961) on the ancestor chain bit, the value of the ancestor chain bit when
    redirecting from a cross-site subresource to a same-site subresource
    was changed. This test is designed to capture that behavior and make
    it testable outside of a chromium browser test.
    
    Bug: 352295648
    Change-Id: Ia5cb5301b75a8ba772ab7cb7b688982c4636f479
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5695085
    Reviewed-by: Johann Hofmann <johannhof@chromium.org>
    Auto-Submit: Aaron Selya <selya@google.com>
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Aaron Selya <selya@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326770}

---
+++ third_party/blink/web_tests/http/tests/cookies/partitioned-cookies/resources/redirect-and-append-cookie-header.php
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  // Extract the cookie information.
+  $headers = getallheaders();
+  $cookieHeader = $headers['Cookie'];
+
+  // Append the cookieHeader to the destination url
+  // as a query parameter.
+  $url = $_GET['location'];
+  $urlWithQuery = $url . "?cookie=" . strval($cookieHeader);
+
+  // Perform the redirect.
+  header("Location: $urlWithQuery", TRUE, 302);
+?>
