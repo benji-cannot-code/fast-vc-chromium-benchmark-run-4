84561ec4c385cc452cad7ce72d77b4dda8ffe20f
Kent Tamura <tkent@chromium.org>
2024-07-12 15:44:48 +0000

    sideways: Fix step-up/step-down key bindings for <input type=number>
    Bug: 40501131
    Change-Id: I6029674fbedbe4e65efe741618ac7775a1648b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5699568
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326788}

--- third_party/blink/web_tests/fast/forms/resources/common-number-keyoperation.js
+++ third_party/blink/web_tests/fast/forms/resources/common-number-keyoperation.js
@@ -14,6 +14,11 @@ function testNumberKeyoperations(writingMode, initialValue) {
       stepUpKey = 'ArrowUp';
       moveToNextDigit = 'ArrowRight';
       moveToPrevDigit = 'ArrowLeft';
+    } else if (writingMode == 'sideways-lr') {
+      stepDownKey = 'ArrowRight';
+      stepUpKey = 'ArrowLeft';
+      moveToNextDigit = 'ArrowUp';
+      moveToPrevDigit = 'ArrowDown';
     } else {
       stepDownKey = 'ArrowLeft';
       stepUpKey = 'ArrowRight';
