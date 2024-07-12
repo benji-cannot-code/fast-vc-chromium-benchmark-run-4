06e096b94b5d5bfd36883b9fefd348c69f7bb8aa
Lauren Winston <lwinston@google.com>
2024-07-12 18:10:07 +0000

    [Read Aloud] Create voice list helper method to improve testability.
    An upcoming CL that filters out espeak voices was becoming more
    difficult than it needed to be to test, so this prepwork CL
    simplifies things significantly and will hopefully improve
    overall testability of the current voice list.
    
    Bug: 342417988
    Change-Id: I808c10d38f8ccc17bc8d5c79a1afef382c06e043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691102
    Reviewed-by: Eitan Goldberger <eitang@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326894}

--- chrome/test/data/webui/side_panel/read_anything/voice_language_util_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/voice_language_util_test.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 import 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 
-import {convertLangOrLocaleForVoicePackManager, convertLangOrLocaleToExactVoicePackLocale, convertLangToAnAvailableLangIfPresent, createInitialListOfEnabledLanguages, mojoVoicePackStatusToVoicePackStatusEnum, VoicePackServerStatusErrorCode, VoicePackServerStatusSuccessCode} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
+import {convertLangOrLocaleForVoicePackManager, convertLangOrLocaleToExactVoicePackLocale, convertLangToAnAvailableLangIfPresent, createInitialListOfEnabledLanguages, getFilteredVoiceList, mojoVoicePackStatusToVoicePackStatusEnum, VoicePackServerStatusErrorCode, VoicePackServerStatusSuccessCode} from 'chrome-untrusted://read-anything-side-panel.top-chrome/read_anything.js';
 import {assertDeepEquals, assertEquals} from 'chrome-untrusted://webui-test/chai_assert.js';
 
 
@@ -189,5 +189,56 @@ suite('voice and language utils', () => {
             /* storedLanguagesPref= */[],
             /* availableLangs= */[],
             /* langOfDefaultVoice= */ undefined));
+  });
+
+  test('getFilteredVoiceList filters remote voices', () => {
+    const voice1 = {
+      default: true,
+      name: 'Eitan',
+      lang: 'en-us',
+      localService: false,
+      voiceURI: '',
+    };
+    const voice2 = {
+      default: false,
+      name: 'Lauren',
+      lang: 'en-us',
+      localService: true,
+      voiceURI: '',
+    };
+    let possibleVoices: SpeechSynthesisVoice[] = [voice1];
+
+    // Remote voices not filtered out with just one voice.
+    assertDeepEquals([voice1], getFilteredVoiceList(possibleVoices));
+
+    possibleVoices = [voice1, voice2];
+    // Remote voices filtered out when a local voice exists
+    assertDeepEquals([voice2], getFilteredVoiceList(possibleVoices));
+  });
+
+  test('getFilteredVoiceList filters Android voices', () => {
+    const voice1 = {
+      default: false,
+      name: 'Xiang',
+      lang: 'en-us',
+      localService: true,
+      voiceURI: '',
+    };
+    const voice2 = {
+      default: true,
+      name: 'Kristi (Android)',
+      lang: 'en-us',
+      localService: true,
+      voiceURI: '',
+    };
+
+    // Android voices should be filtered out only on ChromeOS Ash.
+    const possibleVoices: SpeechSynthesisVoice[] = [voice1, voice2];
+
+    if (chrome.readingMode.isChromeOsAsh) {
+      assertDeepEquals([voice1], getFilteredVoiceList(possibleVoices));
+    } else {
+      assertDeepEquals([voice1, voice2], getFilteredVoiceList(possibleVoices));
+    }
   });
 });
