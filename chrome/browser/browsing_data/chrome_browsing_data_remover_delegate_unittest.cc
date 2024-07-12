3fc612b5f513ddc032b3b685e0a829f6d505fb63
Erin Stewart <erinstewart@google.com>
2024-07-12 15:17:37 +0000

    Putting the ReportingTargetType enum in the net namespace
    Change-Id: I91e351d24f39d7ec40818554fd5b52cb53af80b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5696708
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Erin Stewart <erinstewart@google.com>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326778}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -1037,7 +1037,7 @@ class MockReportingService : public net::ReportingService {
       const std::string& type,
       base::Value::Dict body,
       int depth,
-      ReportingTargetType target_type) override {
+      net::ReportingTargetType target_type) override {
     NOTREACHED_IN_MIGRATION();
   }
 
--- net/reporting/reporting_target_type.h
+++ net/reporting/reporting_target_type.h
@@ -6,9 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_REPORTING_REPORTING_TARGET_TYPE_H_
 #define NET_REPORTING_REPORTING_TARGET_TYPE_H_
 
+namespace net {
+
 // Used to distinguish web developer and enterprise entities so that enterprise
 // reports aren’t sent to web developer endpoints and web developer reports
 // aren’t sent to enterprise endpoints
 enum class ReportingTargetType { kDeveloper, kEnterprise };
+
+}  // namespace net
 
 #endif  // NET_REPORTING_REPORTING_TARGET_TYPE_H_
