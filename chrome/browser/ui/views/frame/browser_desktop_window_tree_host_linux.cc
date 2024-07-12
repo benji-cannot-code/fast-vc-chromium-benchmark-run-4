270342a2eccb6295b7087a1839fd08ed86c9ea56
Hidehiko Abe <hidehiko@chromium.org>
2024-07-12 15:36:29 +0000

    Implement async fullscreen operation behind a flag.
    This CL adds a way to asynchronously set window state fullscreen.
    The behavior is currently tested only on Lacros to start from
    smaller scope.
    Adding the feature flag also helps us to check the behavior
    in other platforms, and will be tested more.
    
    BUG=40276379
    TEST=Tryjob
    
    Change-Id: Ifdbc79daac7022d32ac1a0c43ea7e8c657b9078b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5633160
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326783}

--- chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
+++ chrome/browser/ui/views/frame/browser_desktop_window_tree_host_linux.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "third_party/skia/include/core/SkRRect.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom-shared.h"
+#include "ui/base/ui_base_features.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/rect_conversions.h"
 #include "ui/gfx/geometry/skia_conversions.h"
@@ -358,6 +359,13 @@ void BrowserDesktopWindowTreeHostLinux::OnWindowStateChanged(
     // If the browser view initiated this state change,
     // BrowserView::ProcessFullscreen will no-op, so this call is harmless.
     browser_view_->FullscreenStateChanging();
+
+    // In sync fullscreen window state mode, the FullscreenStateChanged()
+    // is called just after requesting to set fullscreen property
+    // to the platform, so not to call here to avoid duplicated invocation.
+    if (base::FeatureList::IsEnabled(features::kAsyncFullscreenWindowState)) {
+      browser_view_->FullscreenStateChanged();
+    }
   }
 
   UpdateFrameHints();
