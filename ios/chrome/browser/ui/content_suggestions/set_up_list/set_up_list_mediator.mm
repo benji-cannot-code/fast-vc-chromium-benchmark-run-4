a0239414ff4d7b84a4c2f624dcbca39527fd0cb5
Scott Yoder <scottyoder@google.com>
2024-07-12 17:11:31 +0000

    [ios] Fix issue recording IOS.SetUpList.ItemDisplayed
    With the MagicStack CollectionView refactor, this histogram was no
    longer being recorded because there is not a "showSetUpList" method
    in the mediator like there was previously. This adds it back in by
    recording it when the mediator loads the items.
    
    Fixed: 352718248
    Change-Id: Icb03b283f352ae6f7089bc8b5c26a67219bf3726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703056
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1326843}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_mediator.mm
@@ -246,7 +246,6 @@ bool DefaultBrowserPromoCompleted() {
 
 - (NSArray<SetUpListConfig*>*)setUpListConfigs {
   if (!_setUpListConfigs) {
-    NSArray<SetUpListItemViewData*>* items = [self setUpListItems];
     if ([self allItemsComplete]) {
       SetUpListConfig* config = [[SetUpListConfig alloc] init];
       config.setUpListConsumerSource = self;
@@ -254,9 +253,8 @@ bool DefaultBrowserPromoCompleted() {
       config.setUpListItems = @[ [self allSetItem] ];
       _setUpListConfigs = @[ config ];
     } else {
-      BOOL shouldShowCompactedSetUpListModule =
-          set_up_list_utils::ShouldShowCompactedSetUpListModule();
-      if (shouldShowCompactedSetUpListModule) {
+      NSArray<SetUpListItemViewData*>* items = [self setUpListItems];
+      if (set_up_list_utils::ShouldShowCompactedSetUpListModule()) {
         SetUpListConfig* config = [[SetUpListConfig alloc] init];
         config.shouldShowCompactModule = YES;
         config.shouldShowSeeMore = YES;
@@ -286,6 +284,14 @@ bool DefaultBrowserPromoCompleted() {
           [configs addObject:config];
         }
         _setUpListConfigs = configs;
+      }
+    }
+
+    // Record "ItemDisplayed" histogram for each item.
+    for (SetUpListConfig* config in _setUpListConfigs) {
+      for (SetUpListItemViewData* item in config.setUpListItems) {
+        [self.contentSuggestionsMetricsRecorder
+            recordSetUpListItemShown:item.type];
       }
     }
   }
