eedf03f8f54863384864f1dd59360422260f6874
Artem Sumaneev <asumaneev@google.com>
2024-07-12 09:31:31 +0000

    [remote_commands] Add tests for RemoteCommandsInvalidator with InvalidatonListener
    Implement tests for remote commands invalidations using new
    InvalidationListener stack.
    
    Bug: b:341376574
    Test: components_unittests --gtest_filter="RemoteCommandsInvalidatorWithInvalidationListenerTest*"
    Change-Id: Ia77f2799ff70d186f73d772acfd10d5119be7bd4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678983
    Reviewed-by: Igor <igorcov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326643}

--- components/policy/core/common/remote_commands/remote_commands_invalidator.cc
+++ components/policy/core/common/remote_commands/remote_commands_invalidator.cc
@@ -24,6 +24,7 @@ namespace policy {
 
 namespace {
 
+// TODO(b/351754537): Decide on final invalidation type name.
 constexpr char RemoteCommandsInvalidatorType[] = "remote_command";
 
 template <typename T, typename U>
