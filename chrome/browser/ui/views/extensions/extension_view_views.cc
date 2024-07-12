4d073262680ed0278b579174e10603e3b617eb91
Fred Shih <ffred@chromium.org>
2024-07-12 01:06:00 +0000

    [CodeHealth] Remove RAW_PTR_EXCLUSION for 'ExtensionViewViews::container_'
    This replaces the container_ pointer in ExtensionViewViews with a
    raw_ptr. The raw_ptr is preferable over a naked pointer because it is
    able to detect dangling pointers and prevent UAF.
    
    raw_ptr should be safe in this case, because OnPropertyChanged in
    ExtensionViewViews uses the field address of the container_ property.
    This read should not be affected by the use of raw_ptr.
    
    No perf test for this change because it is strictly browser only code.
    
    Fixed: 346693592
    Change-Id: I9467e427cdc72ececcc9cc5bf6c0c5aada0e8521
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691120
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326491}

--- chrome/browser/ui/views/extensions/extension_view_views.cc
+++ chrome/browser/ui/views/extensions/extension_view_views.cc
@@ -97,7 +97,7 @@ void ExtensionViewViews::SetContainer(
 }
 
 ExtensionViewViews::Container* ExtensionViewViews::GetContainer() const {
-  return container_;
+  return container_.get();
 }
 
 void ExtensionViewViews::AddObserver(Observer* observer) {
