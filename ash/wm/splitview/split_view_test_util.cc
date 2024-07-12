f248a07bcc7e37f7ad591a5b88dfc27896395ac6
Michele Fan <michelefan@chromium.org>
2024-07-12 20:39:25 +0000

    snap-group: Add pixel test for Snap Group divider
    This cl added pixel test for Snap Group divider.
    
    This cl also extracted needed Snap Group test utils to two new test
    util files.
    
    Demo: http://b/343756285#comment2
    Fixed: b/343756285
    Change-Id: I6ad7a0782a6e911b137bdf792043c3cc7d4249da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5698657
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326983}

--- ash/wm/splitview/split_view_test_util.cc
+++ ash/wm/splitview/split_view_test_util.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/splitview/split_view_divider.h"
 #include "ash/wm/splitview/split_view_overview_session.h"
 #include "ash/wm/splitview/split_view_utils.h"
-#include "ash/wm/test/test_non_client_frame_view_ash.h"
 #include "ash/wm/window_state.h"
 #include "ash/wm/wm_event.h"
 #include "testing/gtest/include/gtest/gtest.h"
