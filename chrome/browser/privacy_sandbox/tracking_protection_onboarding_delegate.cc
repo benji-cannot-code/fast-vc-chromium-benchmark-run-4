dd9713147fdff91fa7c5ccbe1d5bc7eb95622748
Aashna Sheth <aashnas@google.com>
2024-07-12 14:15:40 +0000

    [3PCD Eligibility] Create IsEnterpriseManaged
    This CL creates a new method to track user eligibility:
    IsEnterpriseManaged(). This method relies on functionality from
    chrome/browser/, so this CL also introduces a delegate.
    
    Bug: b:349787413
    
    Change-Id: Ic09c895ba4ed9e99bc7fec1c567890cb78ecf68a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670888
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1326748}

---
+++ chrome/browser/privacy_sandbox/tracking_protection_onboarding_delegate.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_sandbox/tracking_protection_onboarding_delegate.h"
+
+#include "chrome/browser/enterprise/util/managed_browser_utils.h"
+#include "components/privacy_sandbox/tracking_protection_onboarding.h"
+
+namespace privacy_sandbox {
+TrackingProtectionOnboardingDelegate::TrackingProtectionOnboardingDelegate(
+    Profile* profile)
+    : profile_(profile) {}
+
+TrackingProtectionOnboardingDelegate::~TrackingProtectionOnboardingDelegate() =
+    default;
+
+bool TrackingProtectionOnboardingDelegate::IsEnterpriseManaged() const {
+  return chrome::enterprise_util::IsBrowserManaged(profile_);
+}
+}  // namespace privacy_sandbox
