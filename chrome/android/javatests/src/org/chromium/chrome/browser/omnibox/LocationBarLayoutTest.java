c5918bb2e4ea2f1c9cc75c5df88b4501867641b1
Andrew Grieve <agrieve@chromium.org>
2024-07-12 20:23:44 +0000

    Revert "Revert (partial) "Android: TestThreadUtils -> ThreadUtils""
    This reverts commit a6703ae00b67210a60287ef29ba351a21be4e8be.
    
    Reason for revert: Test was just flaky. This also includes a
    speculative flakiness fix (couldn't repro the failure locally).
    
    Note test history:
    https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Fandroid%3Achrome_public_test_apk%2Forg.chromium.chrome.browser.omnibox.LocationBarLayoutTest%23testDeleteButton?q=V%3Abuilder%3Dandroid-12l-x64-dbg-tests+V%3Aos%3DUbuntu-22.04+V%3Atest_suite%3Dchrome_public_test_apk
    
    Original change's description:
    > Revert (partial) "Android: TestThreadUtils -> ThreadUtils"
    >
    > This reverts commit cb6e41f4b463d65ec40aac5ed1a39286cabf8fe7.
    >
    > Reason for revert: Broke test (see bug)
    >
    > Original change's description:
    > > Android: TestThreadUtils -> ThreadUtils
    > >
    > > The test version does not provide value over the non-test one.
    > >
    > > Bug: None
    > > Change-Id: Ib6ddcc0a0bb20b8cd546895d0455e7cb704ba1d6
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664377
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1326192}
    >
    > No-Try: true
    > Bug: 352763907
    > Change-Id: I2e8be78400f8b39108db4b53b64ad9a8006ccae8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703039
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Hailey Wang <haileywang@google.com>
    > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > Commit-Queue: Hailey Wang <haileywang@google.com>
    > Reviewed-by: Hailey Wang <haileywang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1326897}
    
    Bug: 352763907
    Change-Id: Ie26082bdbac273ebd79a323401fd102eb1ac5fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703042
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326972}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/LocationBarLayoutTest.java
@@ -37,6 +37,7 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.MathUtils;
+import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
@@ -54,7 +55,6 @@ import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
 import org.chromium.content_public.browser.test.util.ClickUtils;
-import org.chromium.content_public.browser.test.util.TestThreadUtils;
 import org.chromium.ui.permissions.AndroidPermissionDelegate;
 import org.chromium.ui.test.util.UiRestriction;
 
@@ -104,7 +104,7 @@ public class LocationBarLayoutTest {
 
     private String getUrlText(UrlBar urlBar) {
         try {
-            return TestThreadUtils.runOnUiThreadBlocking(() -> urlBar.getText().toString());
+            return ThreadUtils.runOnUiThreadBlocking(() -> urlBar.getText().toString());
         } catch (ExecutionException ex) {
             throw new RuntimeException(
                     "Failed to get the UrlBar's text! Exception below:\n" + ex.toString());
@@ -140,14 +140,14 @@ public class LocationBarLayoutTest {
 
     private void setUrlBarTextAndFocus(String text) {
         final UrlBar urlBar = getUrlBar();
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     urlBar.requestFocus();
                 });
         CriteriaHelper.pollUiThread(() -> urlBar.hasFocus());
 
         try {
-            TestThreadUtils.runOnUiThreadBlocking(
+            ThreadUtils.runOnUiThreadBlocking(
                     new Callable<Void>() {
                         @Override
                         public Void call() throws InterruptedException {
@@ -186,7 +186,10 @@ public class LocationBarLayoutTest {
     @SmallTest
     public void testDeleteButton() throws ExecutionException {
         setUrlBarTextAndFocus("testing");
-        Assert.assertEquals(getDeleteButton().getVisibility(), VISIBLE);
+        CriteriaHelper.pollUiThread(
+                () -> {
+                    Criteria.checkThat(getDeleteButton().getVisibility(), Matchers.is(VISIBLE));
+                });
         ClickUtils.clickButton(getDeleteButton());
         CriteriaHelper.pollUiThread(
                 () -> {
@@ -202,7 +205,7 @@ public class LocationBarLayoutTest {
 
         Assert.assertEquals(
                 0, RecordHistogram.getHistogramTotalCountForTesting("Android.OmniboxFocusReason"));
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     locationBarMediator.setUrlBarFocus(
                             true, SEARCH_TERMS_URL, OmniboxFocusReason.FAKE_BOX_LONG_PRESS);
@@ -213,7 +216,7 @@ public class LocationBarLayoutTest {
         Assert.assertEquals(
                 1, RecordHistogram.getHistogramTotalCountForTesting("Android.OmniboxFocusReason"));
 
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     locationBarMediator.setUrlBarFocus(
                             true, SEARCH_TERMS, OmniboxFocusReason.SEARCH_QUERY);
@@ -224,7 +227,7 @@ public class LocationBarLayoutTest {
         Assert.assertEquals(
                 1, RecordHistogram.getHistogramTotalCountForTesting("Android.OmniboxFocusReason"));
 
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     locationBarMediator.setUrlBarFocus(false, null, OmniboxFocusReason.UNFOCUS);
                 });
@@ -233,7 +236,7 @@ public class LocationBarLayoutTest {
         Assert.assertEquals(
                 1, RecordHistogram.getHistogramTotalCountForTesting("Android.OmniboxFocusReason"));
 
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     locationBarMediator.setUrlBarFocus(true, null, OmniboxFocusReason.OMNIBOX_TAP);
                 });
@@ -250,7 +253,7 @@ public class LocationBarLayoutTest {
         View statusIcon = getStatusIconView();
         View urlContainer = getUrlBar();
 
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     getUrlBar().requestFocus();
 
@@ -289,7 +292,7 @@ public class LocationBarLayoutTest {
     public void testEnforceMinimumUrlBarWidth() {
         setUrlBarTextAndFocus("");
 
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     View urlBar = getUrlBar();
                     LocationBarLayout locationBar = getLocationBar();
@@ -338,7 +341,7 @@ public class LocationBarLayoutTest {
     @MediumTest
     @Restriction({UiRestriction.RESTRICTION_TYPE_TABLET})
     public void testTabletUrlBarTranslation_revampEnabled() {
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     LocationBarLayout locationBar = getLocationBar();
                     View urlBar = getUrlBar();
@@ -385,7 +388,7 @@ public class LocationBarLayoutTest {
     @DisableFeatures(ChromeFeatureList.AVOID_RELAYOUT_DURING_FOCUS_ANIMATION)
     @Restriction({UiRestriction.RESTRICTION_TYPE_TABLET})
     public void testTabletUrlBarTranslation_revampEnabled_avoidRelayoutDisabled() {
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     LocationBarLayout locationBar = getLocationBar();
                     View urlBar = getUrlBar();
@@ -407,7 +410,7 @@ public class LocationBarLayoutTest {
     @MediumTest
     @Restriction({UiRestriction.RESTRICTION_TYPE_PHONE})
     public void testPhoneUrlBarAndStatusViewTranslation() {
-        TestThreadUtils.runOnUiThreadBlocking(
+        ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     Activity activity = mActivityTestRule.getActivity();
                     int statusIconAndUrlBarOffset =
