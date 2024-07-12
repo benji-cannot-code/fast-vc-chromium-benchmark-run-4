3526ddb6d85965f6adc618f72dcfc4cf02f21670
Ritika Gupta <ritikagup@google.com>
2024-07-12 11:05:11 +0000

    [Magic Stack] Making Segmentation API call ondemand.
    Enabling ondemand option for Segmentation API with the ability to cache result and use the cached result if ondemand result is unavailable for Clank Magic Stack. Ondemand is required to make sure that the model is able to use freshness inputs passed as input context.
    
    Bug: b/337878177
    Change-Id: I5a43d5e9ec7b259a71f707890c3a7ee21d41a3da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5665724
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326669}

--- components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
+++ components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/default_model/android_home_module_ranker.h"
 
 #include "components/segmentation_platform/embedder/default_model/default_model_test_base.h"
+#include "components/segmentation_platform/public/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace segmentation_platform {
@@ -30,8 +31,12 @@ TEST_F(AndroidHomeModuleRankerTest, ExecuteModelWithInputForAllModules) {
   ASSERT_TRUE(fetched_metadata_);
 
   EXPECT_FALSE(ExecuteWithInput(/*inputs=*/{}));
-
-  std::vector<float> input(27, 0);
+  size_t input_size =
+      base::FeatureList::IsEnabled(
+          features::kSegmentationPlatformAndroidHomeModuleRankerV2)
+          ? 27
+          : 24;
+  std::vector<float> input(input_size, 0);
   ExpectClassifierResults(
       input, {kPriceChange, kSingleTab, kTabResumptionForAndroidHome});
 }
