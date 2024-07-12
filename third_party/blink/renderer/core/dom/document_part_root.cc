4c9590de2370f5ccb17f5116b04978e31696699c
Daniel Vogelheim <vogelheim@chromium.org>
2024-07-12 17:55:18 +0000

    Remove unused header.
    The v8_union_document_documentfragment.h header does not appear to
    be used in this file.
    
    Change-Id: Ice842eee9650bdc8067425b12eadadea18635d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703696
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1326877}

--- third_party/blink/renderer/core/dom/document_part_root.cc
+++ third_party/blink/renderer/core/dom/document_part_root.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/dom/document_part_root.h"
 
-#include "third_party/blink/renderer/bindings/core/v8/v8_union_document_documentfragment.h"
 #include "third_party/blink/renderer/core/dom/container_node.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/document_fragment.h"
