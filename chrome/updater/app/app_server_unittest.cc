aee254c3ee07f5677f7de64edae3bd468b075241
S. Ganesh <ganesh@chromium.org>
2024-07-13 00:40:17 +0000

    updater: reduce installer error overlaps with Windows/other error codes
    To allow for the metainstaller process exit code to be meaningful, this
    CL moves all metainstaller/updater error codes above 0xFFFF (65535) for
    Windows only, which is the range of Windows error codes.
    
    With this CL:
    * Metainstaller error codes are in the 73000 range.
    * Error codes funnelled through `update_client` are in the 74000 range.
    * updater error codes are in the 75000 range.
    
    Low-Coverage-Reason: HARD_TO_TEST
    Fixed: b/40863154,b/352650305
    Change-Id: I26c232b4cf19d15e52a8de444a0b8a6a8fc2c753
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704279
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327087}

--- chrome/updater/app/app_server_unittest.cc
+++ chrome/updater/app/app_server_unittest.cc
@@ -173,7 +173,7 @@ TEST_F(AppServerTestCase, SelfPromoteFails) {
     EXPECT_CALL(*app, ActiveDuty).Times(0);
     EXPECT_CALL(*app, SwapInNewVersion).WillOnce(Return(false));
     EXPECT_CALL(*app, UninstallSelf).Times(0);
-    EXPECT_EQ(app->Run(), 2);
+    EXPECT_EQ(app->Run(), kErrorFailedToSwap);
   }
   scoped_refptr<GlobalPrefs> global_prefs =
       CreateGlobalPrefs(GetUpdaterScopeForTesting());
@@ -254,7 +254,7 @@ TEST_F(AppServerTestCase, StateDirtySwapFails) {
     EXPECT_CALL(*app, ActiveDuty).Times(0);
     EXPECT_CALL(*app, SwapInNewVersion).WillOnce(Return(false));
     EXPECT_CALL(*app, UninstallSelf).Times(0);
-    EXPECT_EQ(app->Run(), 2);
+    EXPECT_EQ(app->Run(), kErrorFailedToSwap);
   }
   scoped_refptr<GlobalPrefs> global_prefs =
       CreateGlobalPrefs(GetUpdaterScopeForTesting());
--- chrome/updater/win/installer/installer_main.cc
+++ chrome/updater/win/installer/installer_main.cc
@@ -10,13 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/at_exit.h"
-#include "base/base_paths.h"
-#include "base/files/file_path.h"
-#include "base/path_service.h"
-#include "base/strings/utf_string_conversions.h"
-#include "chrome/updater/updater_branding.h"
 #include "chrome/updater/win/installer/installer.h"
-#include "chrome/updater/win/ui/l10n_util.h"
 
 // http://blogs.msdn.com/oldnewthing/archive/2004/10/25/247180.aspx
 extern "C" IMAGE_DOS_HEADER __ImageBase;
@@ -26,23 +20,5 @@ int WINAPI wWinMain(HINSTANCE /* instance */,
                     LPWSTR command_line,
                     int /* command_show */) {
   base::AtExitManager exit_manager;
-
-  updater::ProcessExitResult result =
-      updater::WMain(reinterpret_cast<HMODULE>(&__ImageBase));
-
-  // If errors occur, display UI only for metainstaller errors, and also only
-  // when the metainstaller runs without command line arguments.
-  if (result.exit_code != updater::SUCCESS_EXIT_CODE &&
-      wcslen(command_line) == 0) {
-    const std::wstring metainstaller_error_string =
-        updater::GetLocalizedMetainstallerErrorString(result.exit_code,
-                                                      result.windows_error);
-    if (!metainstaller_error_string.empty()) {
-      base::FilePath exe_path;
-      base::PathService::Get(base::FILE_EXE, &exe_path);
-      ::MessageBoxEx(nullptr, metainstaller_error_string.c_str(),
-                     exe_path.BaseName().value().c_str(), 0, 0);
-    }
-  }
-  return result.exit_code;
+  return updater::WMain(reinterpret_cast<HMODULE>(&__ImageBase));
 }
