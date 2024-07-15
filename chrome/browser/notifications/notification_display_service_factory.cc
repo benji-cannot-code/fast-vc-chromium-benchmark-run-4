7279af3282ffbb61741622e8cde667e07a80f73a
Sylvain Defresne <sdefresne@chromium.org>
2024-07-15 13:54:07 +0000

    Fix dependencies of NotificationDisplayServiceFactory
    The NotificationDisplayServiceImpl implicitly access other
    KeyedService (see callstack in the linked bug) so it must
    declare those dependencies (otherwise destruction sequence
    may be broken).
    
    Additionally there is a circular dependency between the PlatformNotificationService and NotificationDisplayService
    services, so check at runtime that the factory does not
    return null.
    
        Due to the circular dependencies if all dependencies are
        declared, only one dependency is explicitly is declared.
    
    Bug: 342112724
    Fixed: 352329050
    Change-Id: Ifbbd300a4f7f9d08a07f7d9c75584e0b9c5b926f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691361
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327466}

--- chrome/browser/notifications/notification_display_service_factory.cc
+++ chrome/browser/notifications/notification_display_service_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/no_destructor.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/notifications/metrics/notification_metrics_logger_factory.h"
 #include "chrome/browser/notifications/notification_display_service_impl.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/buildflags.h"
@@ -42,7 +43,9 @@ NotificationDisplayServiceFactory::NotificationDisplayServiceFactory()
               // TODO(crbug.com/41488885): Check if this service is needed for
               // Ash Internals.
               .WithAshInternals(ProfileSelection::kOwnInstance)
-              .Build()) {}
+              .Build()) {
+  DependsOn(NotificationMetricsLoggerFactory::GetInstance());
+}
 
 std::unique_ptr<KeyedService>
 NotificationDisplayServiceFactory::BuildServiceInstanceForBrowserContext(
