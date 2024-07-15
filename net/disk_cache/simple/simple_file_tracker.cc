a358116e1fcfd3ab9792fa999221b56f084ba6f5
Alex Gough <ajgo@chromium.org>
2024-07-15 13:06:11 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for net/disk_cache
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=morlovich@chromium.org
    
    Bug: 351745839
    Change-Id: Ife53ad096b8b14143ebb4f373f4bf24f4a2689b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706649
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327447}

--- net/disk_cache/simple/simple_file_tracker.cc
+++ net/disk_cache/simple/simple_file_tracker.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file.h"
 #include "base/logging.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/not_fatal_until.h"
 #include "base/synchronization/lock.h"
 #include "net/disk_cache/disk_cache.h"
 #include "net/disk_cache/simple/simple_histogram_enums.h"
@@ -190,7 +191,7 @@ void SimpleFileTracker::Doom(const SimpleSynchronousEntry* owner,
                              EntryFileKey* key) {
   base::AutoLock hold_lock(lock_);
   auto iter = tracked_files_.find(key->entry_hash);
-  DCHECK(iter != tracked_files_.end());
+  CHECK(iter != tracked_files_.end(), base::NotFatalUntil::M130);
 
   uint64_t max_doom_gen = 0;
   for (const std::unique_ptr<TrackedFiles>& file_with_same_hash :
@@ -224,7 +225,7 @@ bool SimpleFileTracker::IsEmptyForTesting() {
 SimpleFileTracker::TrackedFiles* SimpleFileTracker::Find(
     const SimpleSynchronousEntry* owner) {
   auto candidates = tracked_files_.find(owner->entry_file_key().entry_hash);
-  DCHECK(candidates != tracked_files_.end());
+  CHECK(candidates != tracked_files_.end(), base::NotFatalUntil::M130);
   for (const auto& candidate : candidates->second) {
     if (candidate->owner == owner) {
       return candidate.get();
