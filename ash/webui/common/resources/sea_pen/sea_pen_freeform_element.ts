013e1bb305a28ba6486d09ffb00db2a2249faad5
Erica Lee <ericamlee@google.com>
2024-07-15 23:01:19 +0000

    p13n: add sample shuffle button
    Added the shuffle button for the samples.
    
    https://screencast.googleplex.com/cast/NTM0NzUyNjQxOTkzOTMyOHw1Y2ZjNmQxNi05Mg
    
    BUG=b:347324020
    TEST=browser_tests *SeaPenSamples*
    
    Change-Id: I57e3ceaa60def35f14bb3d20f68f7bd5bb8ba47f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685576
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jeffrey Young <cowmoo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327856}

--- ash/webui/common/resources/sea_pen/sea_pen_freeform_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_freeform_element.ts
@@ -9,13 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/ash/common/personalization/common.css.js';
 import 'chrome://resources/ash/common/personalization/cros_button_style.css.js';
+import 'chrome://resources/ash/common/personalization/personalization_shared_icons.html.js';
 import 'chrome://resources/ash/common/personalization/wallpaper.css.js';
 
 import {assertNotReached} from 'chrome://resources/js/assert.js';
 
+import {SEA_PEN_SAMPLES, SeaPenSamplePrompt} from './constants.js';
 import {SeaPenQuery} from './sea_pen.mojom-webui.js';
 import {getTemplate} from './sea_pen_freeform_element.html.js';
 import {WithSeaPenStore} from './sea_pen_store.js';
+import {isArrayEqual, shuffle} from './sea_pen_utils.js';
 
 /** Enumeration of supported tabs. */
 export enum FreeformTab {
@@ -43,9 +46,15 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
         type: Object,
         observer: 'onSeaPenQueryChanged_',
       },
+
+      samples: {
+        type: Array,
+        value: SEA_PEN_SAMPLES,
+      },
     };
   }
 
+  samples: SeaPenSamplePrompt[];
   private freeformTab_: FreeformTab;
   private seaPenQuery_: SeaPenQuery|null;
 
@@ -54,6 +63,7 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
     this.watch<SeaPenFreeformElement['seaPenQuery_']>(
         'seaPenQuery_', state => state.currentSeaPenQuery);
     this.updateFromStore();
+    this.onShuffleClicked_();
   }
 
   /** Invoked on tab selected. */
@@ -78,7 +88,7 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
         query?.textQuery ? FreeformTab.RESULTS : FreeformTab.SAMPLE_PROMPTS;
   }
 
-  private isTabStripEnabled_(query: SeaPenQuery) {
+  private isTabContainerEnabled_(query: SeaPenQuery) {
     return !!query?.textQuery;
   }
 
@@ -92,6 +102,18 @@ export class SeaPenFreeformElement extends WithSeaPenStore {
 
   private onRecentFreeformImageDelete_() {
     // TODO(b/347328001): add the function implementation.
+  }
+
+  private onShuffleClicked_(): void {
+    // Run shuffle (5 times at most) until the shuffled samples are
+    // different from current, which is highly likely to happen the first time.
+    for (let i = 0; i < 5; i++) {
+      const newSamples = shuffle(this.samples);
+      if (!isArrayEqual(newSamples, this.samples)) {
+        this.samples = newSamples;
+        break;
+      }
+    }
   }
 }
 
