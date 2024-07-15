1e4fd11bead8f51ea1d9ed334de9a90839b99f42
Alex Gough <ajgo@chromium.org>
2024-07-15 13:40:07 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for components/payments
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=smcgruer@chromium.org
    
    Bug: 351745839
    Change-Id: I4e677d788ac08491c28e9613ac31dc4ec863a791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706471
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327460}

--- components/payments/core/payment_manifest_downloader.cc
+++ components/payments/core/payment_manifest_downloader.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
+#include "base/not_fatal_until.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -189,7 +190,7 @@ void PaymentManifestDownloader::OnURLLoaderRedirect(
     const network::mojom::URLResponseHead& response_head,
     std::vector<std::string>* to_be_removed_headers) {
   auto download_it = downloads_.find(url_loader);
-  DCHECK(download_it != downloads_.end());
+  CHECK(download_it != downloads_.end(), base::NotFatalUntil::M130);
 
   std::unique_ptr<Download> download = std::move(download_it->second);
   downloads_.erase(download_it);
@@ -252,7 +253,7 @@ void PaymentManifestDownloader::OnURLLoaderCompleteInternal(
     scoped_refptr<net::HttpResponseHeaders> headers,
     int net_error) {
   auto download_it = downloads_.find(url_loader);
-  DCHECK(download_it != downloads_.end());
+  CHECK(download_it != downloads_.end(), base::NotFatalUntil::M130);
 
   std::unique_ptr<Download> download = std::move(download_it->second);
   downloads_.erase(download_it);
