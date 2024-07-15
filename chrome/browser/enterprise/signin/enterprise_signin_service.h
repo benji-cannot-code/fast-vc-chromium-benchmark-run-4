2ef133d8eb956bb7cb718d566b2862b1349150c4
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2024-07-15 19:22:41 +0000

    Reland "ProfileReauthPrompt: fix a race on startup"
    This is a reland of commit 3237df0c82f479a667ef60d1be18a428668a0606
    
    Original change's description:
    > ProfileReauthPrompt: fix a race on startup
    >
    > EnterpriseSigninService can detect the Sync-paused state early during
    > startup, before any browsers are initialized. Because there's no
    > browsers yet, EnterpriseSigninService doesn't open the reauth page (but
    > it should).
    >
    > When this happens, make EnterpriseSigninService start observing Browser
    > creation. When a Browser is ready, it'll open the reauth page there.
    >
    > Bug: b/347284300
    > Change-Id: I764748aab5327de0e481d9cfee953c39a5aafe54
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5698245
    > Reviewed-by: Yann Dago <ydago@chromium.org>
    > Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1326861}
    
    Bug: b/347284300
    Change-Id: Ic52916d604af7cebfb73e1ad67321119d3b8c5c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704077
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327710}

--- chrome/browser/enterprise/signin/enterprise_signin_service.h
+++ chrome/browser/enterprise/signin/enterprise_signin_service.h
@@ -8,8 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "build/build_config.h"
+
+#if !(BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN))
+#error EnterpriseSigninService should only be built on desktop platforms.
+#endif  // !(BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN))
+
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/sync/service/sync_service.h"
@@ -19,12 +27,16 @@ class Profile;
 
 namespace enterprise_signin {
 
-class EnterpriseSigninService : public KeyedService,
+class EnterpriseSigninService : public BrowserListObserver,
+                                public KeyedService,
                                 public syncer::SyncServiceObserver {
  public:
   explicit EnterpriseSigninService(Profile* profile);
 
   ~EnterpriseSigninService() override;
+
+  // BrowserListObserver:
+  void OnBrowserSetLastActive(Browser* browser) override;
 
   // syncer::SyncServiceObserver:
   void OnStateChanged(syncer::SyncService* sync) override;
@@ -36,8 +48,10 @@ class EnterpriseSigninService : public KeyedService,
   void OpenOrActivateGaiaReauthTab();
 
   raw_ptr<Profile> profile_;
+  base::ScopedObservation<BrowserList, BrowserListObserver>
+      browser_list_observation_{this};
   base::ScopedObservation<syncer::SyncService, syncer::SyncServiceObserver>
-      observation_{this};
+      sync_service_observation_{this};
 
   PrefChangeRegistrar pref_change_registrar_;
 
