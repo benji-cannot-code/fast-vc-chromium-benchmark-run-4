c946e4fbdc95f2df7a3ae30079e4462f9ae56316
Abe Boujane <boujane@google.com>
2024-07-15 20:52:40 +0000

    Added Full 3pcd feature + Pref
    Bug: 341975190
    Change-Id: Ic82e605c1d071d2291777eb81656feadeb19b694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709569
    Commit-Queue: Abe Boujane <boujane@google.com>
    Reviewed-by: Jacob Stanley <jacobstanley@google.com>
    Auto-Submit: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327772}

--- components/privacy_sandbox/tracking_protection_prefs.cc
+++ components/privacy_sandbox/tracking_protection_prefs.cc
@@ -47,6 +47,10 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterTimePref(prefs::kTrackingProtectionSilentOnboardedSince,
                              base::Time());
 
+  // Full 3PCD prefs.
+  registry->RegisterTimePref(prefs::kTrackingProtectionFull3PCDOnboarded,
+                             base::Time());
+
   // Tracking Protection Reminder Prefs
   registry->RegisterIntegerPref(
       prefs::kTrackingProtectionReminderStatus,
