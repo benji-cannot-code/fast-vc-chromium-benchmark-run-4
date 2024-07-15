db0799584aa0ee055ee6e8a96548a3376d5709c2
Ryan Sultanem <rsult@google.com>
2024-07-15 09:32:16 +0000

    [ProfileSelections][AshInternals] Enforce selection value for default factory instances
    Change-Id: Ib6406f8d857b25970442273f0489930f0979a579
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5178188
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327389}

--- chrome/browser/ash/kerberos/kerberos_credentials_manager_factory.cc
+++ chrome/browser/ash/kerberos/kerberos_credentials_manager_factory.cc
@@ -39,7 +39,13 @@ KerberosCredentialsManagerFactory::GetInstance() {
 
 KerberosCredentialsManagerFactory::KerberosCredentialsManagerFactory()
     : ProfileKeyedServiceFactory(
-          /*name=*/"KerberosCredentialsManager") {}
+          /*name=*/"KerberosCredentialsManager",
+          ProfileSelections::Builder()
+              .WithRegular(ProfileSelection::kOriginalOnly)
+              // TODO(crbug.com/41488885): Check if this service is needed for
+              // Ash Internals.
+              .WithAshInternals(ProfileSelection::kOriginalOnly)
+              .Build()) {}
 
 KerberosCredentialsManagerFactory::~KerberosCredentialsManagerFactory() =
     default;
