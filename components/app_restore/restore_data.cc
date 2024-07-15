73174c8b3ed4533e2768874995441bf36b4b522e
Alex Gough <ajgo@chromium.org>
2024-07-15 05:25:16 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for components/app_restore
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=xdai@chromium.org
    
    Bug: 351745839
    Change-Id: I8ffdf81a34607da583b77e1324f516743b84ce7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704168
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327330}

--- components/app_restore/restore_data.cc
+++ components/app_restore/restore_data.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "base/not_fatal_until.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "components/app_constants/constants.h"
@@ -277,7 +278,7 @@ int32_t RestoreData::FetchRestoreWindowId(const std::string& app_id) {
 
   // Move to the next window_id.
   auto data_it = it->second.find(window_id);
-  DCHECK(data_it != it->second.end());
+  CHECK(data_it != it->second.end(), base::NotFatalUntil::M130);
   ++data_it;
   if (data_it == it->second.end())
     chrome_app_id_to_current_window_id_.erase(app_id);
