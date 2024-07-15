60107ed26f9bc0de3cc268303dc0f72c7a24f1a0
Alex Gough <ajgo@chromium.org>
2024-07-15 17:49:23 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for components/search_engines
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=mpearson@chromium.org
    
    Bug: 351745839
    Change-Id: Idb15d9fa01e676ab28590c426fc9b9b34674aaa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706473
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327622}

--- components/search_engines/template_url_fetcher.cc
+++ components/search_engines/template_url_fetcher.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/not_fatal_until.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
@@ -280,6 +281,6 @@ void TemplateURLFetcher::ScheduleDownload(
 void TemplateURLFetcher::RequestCompleted(RequestDelegate* request) {
   auto i = base::ranges::find(requests_, request,
                               &std::unique_ptr<RequestDelegate>::get);
-  DCHECK(i != requests_.end());
+  CHECK(i != requests_.end(), base::NotFatalUntil::M130);
   requests_.erase(i);
 }
