594ea3ab3d8551e7acd800a8c96975f09f0d0bfd
Taiyo Mizuhashi <taiyo@chromium.org>
2024-07-15 21:16:33 +0000

    [CodeHealth] Remove RAW_PTR_EXCLUSION from media::RendererImplTest
    DanglingUntriaged-notes: Annotating pre-existing dangling pointers
    
    Fixed: 346693501, 346693830
    Change-Id: I3b5f98bfd19ea08ba69c63d59c282852deed63d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5695145
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327795}

--- media/renderers/renderer_impl_unittest.cc
+++ media/renderers/renderer_impl_unittest.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/test/gmock_callback_support.h"
@@ -361,12 +360,8 @@ class RendererImplTest : public ::testing::Test {
   std::unique_ptr<StrictMock<MockDemuxerStream>> audio_stream_;
   std::unique_ptr<StrictMock<MockDemuxerStream>> video_stream_;
   std::vector<DemuxerStream*> streams_;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION RendererClient* video_renderer_client_;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION RendererClient* audio_renderer_client_;
+  raw_ptr<RendererClient, DanglingUntriaged> video_renderer_client_;
+  raw_ptr<RendererClient, DanglingUntriaged> audio_renderer_client_;
   VideoDecoderConfig video_decoder_config_;
   PipelineStatus initialization_status_;
   bool is_encrypted_ = false;
