5ec02999b07aac2057d925f57ebdd232a8a5914d
Sanja Perisic <sanjaperisic@chromium.org>
2024-07-15 10:28:31 +0000

    Refactor annotator tests
    Extract common methods for verifying annotator overlay window stacking
    and size to //ash/annotator/annotator_test_util.h. Set up
    AnnotatorClient for tests in AnnotatorIntegrationHelper.
    
    Bug: 342104047
    Change-Id: Ifa3c23a834f3b257b099073f8cd28d30304a54b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5695627
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Sanja Perisic <sanjaperisic@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327405}

--- ash/capture_mode/capture_mode_test_util.cc
+++ ash/capture_mode/capture_mode_test_util.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/a11y_feature_type.h"
 #include "ash/accessibility/accessibility_controller.h"
 #include "ash/accessibility/autoclick/autoclick_controller.h"
-#include "ash/annotator/annotator_controller.h"
 #include "ash/capture_mode/capture_mode_bar_view.h"
 #include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/capture_mode/capture_mode_session.h"
@@ -417,10 +416,9 @@ ProjectorCaptureModeIntegrationHelper::ProjectorCaptureModeIntegrationHelper() =
     default;
 
 void ProjectorCaptureModeIntegrationHelper::SetUp() {
+  annotator_helper_.SetUp();
   auto* projector_controller = ProjectorController::Get();
   projector_controller->SetClient(&projector_client_);
-  auto* annotator_controller = Shell::Get()->annotator_controller();
-  annotator_controller->SetToolClient(&annotator_client_);
   ON_CALL(projector_client_, StopSpeechRecognition)
       .WillByDefault(testing::Invoke([]() {
         ProjectorController::Get()->OnSpeechRecognitionStopped(
