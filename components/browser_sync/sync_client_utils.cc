e8190188a559b5431bb29b776c521ad55eba6503
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-15 14:57:13 +0000

    [sync] Remove duplicated ModelType in GetLocalDataDescriptions() API
    No behavior change.
    The API had a ModelType both as key of the returned map and as a
    field in LocalDataDescription. That's not great because it's prone
    to invalid state, i.e. the 2 values differing. This CL removes
    LocalDataDescription::model_type.
    
    Change-Id: Ib009a317696b148a265f77ef27726914ba2753ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5707990
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327495}

--- components/browser_sync/sync_client_utils.cc
+++ components/browser_sync/sync_client_utils.cc
@@ -49,7 +49,6 @@ syncer::LocalDataDescription CreateLocalDataDescription(syncer::ModelType type,
                                                         ContainerT&& items,
                                                         F&& url_extractor) {
   syncer::LocalDataDescription desc;
-  desc.type = type;
   desc.item_count = items.size();
   // Using a set to get only the distinct domains. This also ensures an
   // alphabetical ordering of the domains.
--- components/sync/service/local_data_description.h
+++ components/sync/service/local_data_description.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer {
 
 struct LocalDataDescription {
-  ModelType type = syncer::UNSPECIFIED;
   // Actual count of local items.
   int item_count = 0;
   // Preferably contains up to 3 distinct domains corresponding to some of the
