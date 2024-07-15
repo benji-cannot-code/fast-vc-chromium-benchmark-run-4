7cc361ec736e1f395c5008fedf366436e4493103
Joey Arhar <jarhar@chromium.org>
2024-07-15 16:16:46 +0000

    Remove trailing content check in XMLDocumentParser
    This isn't needed anymore after the following libxml fix:
    https://gitlab.gnome.org/GNOME/libxml2/-/commit/8c5848bd
    
    More context here: https://gitlab.gnome.org/GNOME/libxml2/-/issues/767
    
    The old code is kept behind an #if to support people who build
    chrome without our included copy of libxml.
    
    Co-authored-by: Nick Wellnhofer <wellnhofer@aevum.de>
    Change-Id: I84116c5b3967ae16075f588d7016d34cf3790934
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703378
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327546}

--- third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
+++ third_party/blink/renderer/core/xml/parser/xml_document_parser.cc
@@ -1780,6 +1780,7 @@ bool XMLDocumentParser::AppendFragmentSource(const String& chunk) {
   xmlParseContent(Context());
   EndDocument();  // Close any open text nodes.
 
+#if LIBXML_VERSION < 21400
   // FIXME: If this code is actually needed, it should probably move to
   // finish()
   // XMLDocumentParserQt has a similar check (m_stream.error() ==
@@ -1795,6 +1796,7 @@ bool XMLDocumentParser::AppendFragmentSource(const String& chunk) {
            (bytes_processed >= 0 && !chunk_as_utf8.data()[bytes_processed]));
     return false;
   }
+#endif
 
   // No error if the chunk is well formed or it is not but we have no error.
   return Context()->wellFormed || !xmlCtxtGetLastError(Context());
