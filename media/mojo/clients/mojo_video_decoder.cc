bb298671b5e10e9373e0db2966b35c8f0d4dc730
Dan Sanders <sandersd@chromium.org>
2024-07-15 21:25:26 +0000

    [media] Invalidate weak pointers in MVD::Stop()
    There is no evidence that null reset callbacks are being set, and it
    doesn't look like double resets are the cause either. Another option
    would be Stop() calling the reset callback early, although this should
    only be possible if there is an earlier invalid decode ID.
    
    Bug: 335001233
    Change-Id: Ibd1f39569f99393e0584cc4c7be0ddf229683154
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709173
    Auto-Submit: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327802}

--- media/mojo/clients/mojo_video_decoder.cc
+++ media/mojo/clients/mojo_video_decoder.cc
@@ -327,10 +327,6 @@ void MojoVideoDecoder::Reset(base::OnceClosure reset_cb) {
     return;
   }
 
-  // TODO(crbug.com/335001233): rollback the change or replace it with a CHECK
-  // before closing the bug.
-  DUMP_WILL_BE_CHECK(reset_cb);
-
   reset_cb_ = std::move(reset_cb);
   remote_decoder_->Reset(
       base::BindOnce(&MojoVideoDecoder::OnResetDone, base::Unretained(this)));
@@ -493,6 +489,9 @@ void MojoVideoDecoder::Stop() {
 
   if (reset_cb_)
     std::move(reset_cb_).Run();
+
+  // Drop any outstanding callbacks.
+  weak_factory_.InvalidateWeakPtrs();
 }
 
 }  // namespace media
