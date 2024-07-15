481ac675367253f4c7673dbf9a87cc32cad81eb9
Henrique Nakashima <hnakashima@chromium.org>
2024-07-15 23:15:21 +0000

    [Android] Remove retry from clicking links
    PopupOnClickPageStation was flaky due to the DOM element click not
    happening.
    
    Bug: 331263719
    Change-Id: Ifd7294d449a9a1350b0758b54eb453ed150bb4a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704220
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327862}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/testhtmls/PopupOnClickPageStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/testhtmls/PopupOnClickPageStation.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.test.transit.testhtmls;
 
 import org.chromium.base.test.transit.Elements;
-import org.chromium.base.test.transit.Transition;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.transit.PageStation;
 import org.chromium.chrome.test.transit.PopupBlockedMessageFacility;
@@ -51,7 +50,7 @@ public class PopupOnClickPageStation extends WebPageStation {
                         .withIsOpeningTabs(1)
                         .withIsSelectingTabs(1)
                         .build();
-        return travelToSync(newPage, Transition.retryOption(), mLinkToPopup::click);
+        return travelToSync(newPage, mLinkToPopup::click);
     }
 
     /**
@@ -60,6 +59,6 @@ public class PopupOnClickPageStation extends WebPageStation {
      */
     public PopupBlockedMessageFacility clickLinkAndExpectPopupBlockedMessage() {
         PopupBlockedMessageFacility infoBar = new PopupBlockedMessageFacility(this, 1);
-        return enterFacilitySync(infoBar, Transition.retryOption(), mLinkToPopup::click);
+        return enterFacilitySync(infoBar, mLinkToPopup::click);
     }
 }
