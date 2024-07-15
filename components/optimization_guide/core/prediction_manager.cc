890135edd61f7e5c20193ac77952548689b3d756
Alex Gough <ajgo@chromium.org>
2024-07-15 16:48:52 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for components/optimization_guide
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=sophiechang@chromium.org
    
    Bug: 351745839
    Change-Id: Ie98e6d71ecfdbd8bcd99c41f838da58b97a7cd57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706572
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327565}

--- components/optimization_guide/core/prediction_manager.cc
+++ components/optimization_guide/core/prediction_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/metrics/histogram_macros_local.h"
+#include "base/not_fatal_until.h"
 #include "base/observer_list.h"
 #include "base/path_service.h"
 #include "base/sequence_checker.h"
@@ -311,7 +312,8 @@ void PredictionManager::RemoveObserverForOptimizationTargetModel(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto registration_info =
       model_registration_info_map_.find(optimization_target);
-  DCHECK(registration_info != model_registration_info_map_.end());
+  CHECK(registration_info != model_registration_info_map_.end(),
+        base::NotFatalUntil::M130);
 
   auto& observers = registration_info->second.model_observers;
   DCHECK(observers.HasObserver(observer));
