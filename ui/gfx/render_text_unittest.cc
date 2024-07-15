0bd381529675d7e09a8a9e3af4c81f78807c408b
Kurt Catti-Schmidt <kschmi@microsoft.com>
2024-07-15 18:19:19 +0000

    [Harfbuzz] Add TextRunList::MissingGlyphCount and use it in unit tests
    Split from
    https://chromium-review.googlesource.com/c/chromium/src/+/5634309 in
    order to simplify that change bit.
    
    No behavioral changes will happen as a result of this change, just some
    test cleanup and prep for the larger change.
    
    Bug: 40270295, 40267908
    Change-Id: I43b17061e4b8cf77409ec165c3ffde8cb353ebda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685167
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Reviewed-by: David Yeung <dayeung@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327661}

--- ui/gfx/render_text_unittest.cc
+++ ui/gfx/render_text_unittest.cc
@@ -7173,7 +7173,7 @@ TEST_F(RenderTextTest, HarfBuzz_UnicodeFallback) {
   render_text->SetText(u"\ud55c");
   const internal::TextRunList* run_list = GetHarfBuzzRunList();
   ASSERT_EQ(1U, run_list->size());
-  EXPECT_EQ(0U, run_list->runs()[0]->CountMissingGlyphs());
+  EXPECT_EQ(0U, run_list->MissingGlyphCount());
 }
 #endif  // !BUILDFLAG(IS_LINUX) && !BUILDFLAG(IS_CHROMEOS) &&
         // !BUILDFLAG(IS_ANDROID)
@@ -7192,7 +7192,7 @@ TEST_F(RenderTextTest, HarfBuzz_FallbackFontsSupportGlyphs) {
 
     const internal::TextRunList* run_list = GetHarfBuzzRunList();
     ASSERT_EQ(1U, run_list->size());
-    int missing_glyphs = run_list->runs()[0]->CountMissingGlyphs();
+    const int missing_glyphs = run_list->MissingGlyphCount();
     if (missing_glyphs != 0) {
       ADD_FAILURE() << "Text '" << text << "' is missing " << missing_glyphs
                     << " glyphs.";
@@ -7213,11 +7213,7 @@ TEST_F(RenderTextTest, HarfBuzz_MultiRunsSupportGlyphs) {
     RenderTextHarfBuzz* render_text = GetRenderText();
     render_text->SetText(text);
 
-    int missing_glyphs = 0;
-    const internal::TextRunList* run_list = GetHarfBuzzRunList();
-    for (const auto& run : run_list->runs())
-      missing_glyphs += run->CountMissingGlyphs();
-
+    const int missing_glyphs = GetHarfBuzzRunList()->MissingGlyphCount();
     if (missing_glyphs != 0) {
       ADD_FAILURE() << "Text '" << text << "' is missing " << missing_glyphs
                     << " glyphs.";
@@ -7245,12 +7241,7 @@ TEST_P(RenderTextTestWithFallbackFontCase, FallbackFont) {
   RenderTextHarfBuzz* render_text = GetRenderText();
   render_text->SetText(param.text);
 
-  int missing_glyphs = 0;
-  const internal::TextRunList* run_list = GetHarfBuzzRunList();
-  for (const auto& run : run_list->runs())
-    missing_glyphs += run->CountMissingGlyphs();
-
-  EXPECT_EQ(missing_glyphs, 0);
+  EXPECT_EQ(GetHarfBuzzRunList()->MissingGlyphCount(), 0U);
 }
 
 const FallbackFontCase kUnicodeDecomposeCases[] = {
@@ -7519,7 +7510,7 @@ TEST_F(RenderTextTest, ZeroWidthCharacters) {
     const internal::TextRunList* run_list = GetHarfBuzzRunList();
     EXPECT_EQ(0, run_list->width());
     ASSERT_EQ(run_list->runs().size(), 1U);
-    EXPECT_EQ(run_list->runs()[0]->CountMissingGlyphs(), 0U);
+    EXPECT_EQ(run_list->MissingGlyphCount(), 0U);
   }
 }
 
