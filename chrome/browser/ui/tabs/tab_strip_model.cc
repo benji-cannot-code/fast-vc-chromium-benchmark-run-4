32a2ffba550b807ef44ce730c13d949739028de7
dljames <dljames@chromium.org>
2024-07-15 19:22:51 +0000

    [TabStripModel] Add functions to notify when groups are added or removed
    Adds the following functions to TabStripModelObserver:
    OnTabGroupAdded
    OnTabGroupWillBeRemoved
    
    These functions are being added since there isn't a direct way to
    observe if a group was added or removed from the TabGroupModel. There
    is TabGroupStateChanged, but that fires multiple events when a group is
    added and can be a little much since each case must be considered.
    
    Change-Id: Ie63418136adca29db39879167974d9cf1cea7a6b
    Bug: NONE
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5697325
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1327711}

--- chrome/browser/ui/tabs/tab_strip_model.cc
+++ chrome/browser/ui/tabs/tab_strip_model.cc
@@ -678,6 +678,10 @@ void TabStripModel::CloseAllTabsInGroup(const tab_groups::TabGroupId& group) {
 
   delegate_->WillCloseGroup(group);
 
+  for (TabStripModelObserver& observer : observers_) {
+    observer.OnTabGroupWillBeRemoved(group);
+  }
+
   gfx::Range tabs_in_group = group_model_->GetTabGroup(group)->ListTabs();
   if (static_cast<int>(tabs_in_group.length()) == count())
     closing_all_ = true;
@@ -1091,6 +1095,10 @@ tab_groups::TabGroupId TabStripModel::AddToNewGroup(
   AddToNewGroupImpl(indices, group_id, visual_data);
   delegate_->GroupAdded(group_id);
 
+  for (TabStripModelObserver& observer : observers_) {
+    observer.OnTabGroupAdded(group_id);
+  }
+
   return group_id;
 }
 
@@ -1114,6 +1122,10 @@ tab_groups::TabGroupId TabStripModel::AddToNewGroup(
   // TODO(crbug.com/339858272) : Consolidate all default save logic to
   // TabStripModel::AddToNewGroupImpl.
   delegate_->GroupAdded(new_group);
+
+  for (TabStripModelObserver& observer : observers_) {
+    observer.OnTabGroupAdded(new_group);
+  }
 
   return new_group;
 }
