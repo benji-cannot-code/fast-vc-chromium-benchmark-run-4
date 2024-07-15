49ae297f5ed1426e9b4ac16805810efaa448c002
Alex Gough <ajgo@chromium.org>
2024-07-15 15:27:23 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for net/cookies
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=chromium-cookie-reviews@google.com
    
    Bug: 351745839
    Change-Id: I85fa88a8dd4fca91ca358faea64f3d8a685f8fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706541
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Commit-Queue: Steven Bingler <bingler@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327518}

--- net/cookies/cookie_monster_change_dispatcher.cc
+++ net/cookies/cookie_monster_change_dispatcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/functional/bind.h"
+#include "base/not_fatal_until.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/task_runner.h"
 #include "net/base/features.h"
@@ -252,12 +253,14 @@ void CookieMonsterChangeDispatcher::UnlinkSubscription(
 
   auto cookie_domain_map_iterator =
       cookie_domain_map_.find(subscription->domain_key());
-  DCHECK(cookie_domain_map_iterator != cookie_domain_map_.end());
+  CHECK(cookie_domain_map_iterator != cookie_domain_map_.end(),
+        base::NotFatalUntil::M130);
 
   CookieNameMap& cookie_name_map = cookie_domain_map_iterator->second;
   auto cookie_name_map_iterator =
       cookie_name_map.find(subscription->name_key());
-  DCHECK(cookie_name_map_iterator != cookie_name_map.end());
+  CHECK(cookie_name_map_iterator != cookie_name_map.end(),
+        base::NotFatalUntil::M130);
 
   SubscriptionList& subscription_list = cookie_name_map_iterator->second;
   subscription->RemoveFromList();
