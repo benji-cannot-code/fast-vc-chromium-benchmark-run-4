72e0ff596a7c7c002a9bb7ca437c2ac20712098b
Alex Gough <ajgo@chromium.org>
2024-07-15 09:04:08 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for components/content_settings
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=dullweber@chromium.org
    
    Bug: 351745839
    Change-Id: I5110b226e566568e375f8baa21e9a411c6fae685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706469
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327379}

--- components/content_settings/core/browser/content_settings_rule.cc
+++ components/content_settings/core/browser/content_settings_rule.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/not_fatal_until.h"
 
 namespace content_settings {
 
@@ -44,7 +45,7 @@ bool ConcatenationIterator::HasNext() const {
 
 std::unique_ptr<Rule> ConcatenationIterator::Next() {
   auto current_iterator = iterators_.begin();
-  DCHECK(current_iterator != iterators_.end());
+  CHECK(current_iterator != iterators_.end(), base::NotFatalUntil::M130);
   DCHECK((*current_iterator)->HasNext());
   std::unique_ptr<Rule> next_rule = (*current_iterator)->Next();
   if (!(*current_iterator)->HasNext()) {
