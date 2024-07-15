312a2a2faf276a22c4eb1e1799346d8bab11775e
Andrew Grieve <agrieve@chromium.org>
2024-07-15 15:42:53 +0000

    Android: Allow instrumentation thread access to ObservableSupplierImpl for all tests
    Rather than just those that use ChromeActivityTestRule.
    
    Bug: 40167293
    Change-Id: I2bcefe1727f925957730ac4d74e2909f6b9a533f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5699845
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327526}

--- chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/MinimizeAppAndCloseTabBackPressHandlerUnitTest.java
+++ chrome/browser/back_press/android/java/src/org/chromium/chrome/browser/back_press/MinimizeAppAndCloseTabBackPressHandlerUnitTest.java
@@ -66,7 +66,6 @@ public class MinimizeAppAndCloseTabBackPressHandlerUnitTest {
 
     @Before
     public void setUp() {
-        ObservableSupplierImpl.setIgnoreThreadChecksForTesting(true);
         createBackPressHandler();
     }
 
