724d0243183c569bb54354d37a03897c9ab79b5c
Alex Gough <ajgo@chromium.org>
2024-07-15 20:49:21 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for device/bluetooth
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=reillyg@chromium.org
    
    Bug: 351745839
    Change-Id: Ib7dd79b8b1e65209a7a1575ef60a6ebe2d339022
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706594
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327767}

--- device/bluetooth/bluetooth_adapter_win.cc
+++ device/bluetooth/bluetooth_adapter_win.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/memory/ptr_util.h"
+#include "base/not_fatal_until.h"
 #include "base/notreached.h"
 #include "base/stl_util.h"
 #include "base/task/sequenced_task_runner.h"
@@ -238,7 +239,7 @@ void BluetoothAdapterWin::DevicesPolled(
         observer.DeviceAdded(this, device_win_raw);
     } else if (base::Contains(changed_devices, device_state->address)) {
       auto iter = devices_.find(device_state->address);
-      DCHECK(iter != devices_.end());
+      CHECK(iter != devices_.end(), base::NotFatalUntil::M130);
       BluetoothDeviceWin* device_win =
           static_cast<BluetoothDeviceWin*>(iter->second.get());
       if (!device_win->IsEqual(*device_state)) {
