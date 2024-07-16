8f5a23ac640745d9e614456e8e70ecddabed37d5
Vincent Boisselle <vincb@google.com>
2024-07-16 19:50:51 +0000

    [ios] Autofill across frames: ack and retry registrations
    Also change the JS test fixture AddUserScript() injector to inject
    the script in all frames.
    
    Change-Id: I57d20b4157fb534d3c631768adf0e30fe75edc08
    Bug: 324931277
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675428
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328386}

--- ios/web/public/test/javascript_test.mm
+++ ios/web/public/test/javascript_test.mm
@@ -32,7 +32,7 @@ void JavascriptTest::AddUserScript(NSString* script_name) {
   WKUserScript* script = [[WKUserScript alloc]
         initWithSource:web::test::GetPageScript(script_name)
          injectionTime:WKUserScriptInjectionTimeAtDocumentStart
-      forMainFrameOnly:YES];
+      forMainFrameOnly:NO];
   [web_view_.configuration.userContentController addUserScript:script];
 }
 
