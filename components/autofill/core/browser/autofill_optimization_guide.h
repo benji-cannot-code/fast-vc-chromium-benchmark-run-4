ddccc1e76bba083a1967fb55c11cf3f1179ceed8
Dominic Battre <battre@chromium.org>
2024-07-16 19:49:52 +0000

    Register optimization guides for the Autofill Ablation Study
    Low-Coverage-Reason: TRIVIAL_CHANGE - Code is only used for testing and histograms
    Bug: 348362142
    Change-Id: I408ef4f2a30a8f45a435985f6c61e4c2dc66b98f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704064
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328385}

--- components/autofill/core/browser/autofill_optimization_guide.h
+++ components/autofill/core/browser/autofill_optimization_guide.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/autofill/core/browser/data_model/credit_card_benefit.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/optimization_guide/proto/hints.pb.h"
 
 class GURL;
 
@@ -77,6 +78,14 @@ class AutofillOptimizationGuide : public KeyedService {
   virtual bool ShouldBlockBenefitSuggestionLabelsForCardAndUrl(
       const CreditCard& card,
       const GURL& url) const;
+
+  // Returns whether `url` is eligible for ablation as per `type`.
+  // Type must be one of
+  // `optimization_guide::proto::AUTOFILL_ABLATION_SITES_LIST1`, ...,
+  // `optimization_guide::proto::AUTOFILL_ABLATION_SITES_LIST4`.
+  virtual bool IsEligibleForAblation(
+      const GURL& url,
+      optimization_guide::proto::OptimizationType type) const;
 
  private:
   // Raw pointer to a decider which is owned by the decider's factory.
