733275e1512863145acd79fb90dd26a6f9269c0a
Titouan Rigoudy <titouan@chromium.org>
2024-07-16 16:16:48 +0000

    [Fuzzing] Basic test for ASan crash symbolization.
    This only tests UaF crashes on ASan. MSan is currently broken, and UBSan
    does not detect the issue. More variants need to be added, but this
    demonstrates the basic approach.
    
    This test will not run on CI due to needing `use_fuzzing_engine`, which
    is only true on builders that run a custom recipe and do not run tests.
    Still, it allows local testing.
    
    Bug: 40948553, 350528348
    Change-Id: I2609e73848f63a5aef2a688048a75c6ce12cb6af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645864
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Commit-Queue: Titouan Rigoudy <titouan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328222}

---
+++ testing/libfuzzer/tests/stacktrace_test_uaf_fuzzer.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+
+#include "base/compiler_specific.h"
+
+// Tries to use a dangling pointer, triggers a UaF crash under ASAN.
+NOINLINE int TriggerUAF() {
+  auto ptr = std::make_unique<int>(0);
+  int* dangling = ptr.get();
+  ptr = nullptr;
+  return *dangling;
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  return TriggerUAF();
+}
