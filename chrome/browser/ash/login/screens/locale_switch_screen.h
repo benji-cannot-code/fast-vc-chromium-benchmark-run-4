d7eb580cd47fe0f593a152a0ad07d370b929d484
Danila Kuzmin <dkuzmin@google.com>
2024-07-16 13:56:30 +0000

    [oobe] Get user preferred locale from PeopleAPI
    Bug: b:330384267
    Change-Id: Ib4cde8c023a57f841abfffb23da2a0afad4917b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645663
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Side YILMAZ <sideyilmaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328132}

--- chrome/browser/ash/login/screens/locale_switch_screen.h
+++ chrome/browser/ash/login/screens/locale_switch_screen.h
@@ -6,16 +6,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_LOGIN_SCREENS_LOCALE_SWITCH_SCREEN_H_
 #define CHROME_BROWSER_ASH_LOGIN_SCREENS_LOCALE_SWITCH_SCREEN_H_
 
-#include "base/memory/raw_ptr.h"
-#include "chrome/browser/ash/login/screens/base_screen.h"
+#include <optional>
+#include <string>
 
+#include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/ash/base/locale_util.h"
+#include "chrome/browser/ash/login/screens/base_screen.h"
+#include "components/signin/public/identity_manager/access_token_info.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "google_apis/gaia/oauth2_api_call_flow.h"
+
+namespace signin {
+class PrimaryAccountAccessTokenFetcher;
+}  // namespace signin
 
 namespace ash {
 
@@ -67,16 +75,26 @@ class LocaleSwitchScreen : public BaseScreen,
   void HideImpl() override;
   bool MaybeSkip(WizardContext& context) override;
 
-  void SwitchLocale(std::string locale);
+  void SwitchLocale();
   void OnLanguageChangedCallback(
       const locale_util::LanguageSwitchResult& result);
   void OnLanguageChangedNotificationCallback(
       const locale_util::LanguageSwitchResult& result);
 
-  void ResetState();
+  void OnLocaleFetched(std::string locale);
+  void OnRequestFailure();
+  void AbandonPeopleAPICall();
   void OnTimeout();
 
+  void FetchPreferredUserLocaleAndSwitchAsync();
+  void OnAccessTokenRequestCompleted(GoogleServiceAuthError error,
+                                     signin::AccessTokenInfo access_token_info);
+
+  std::string locale_;
   base::WeakPtr<LocaleSwitchView> view_ = nullptr;
+  std::unique_ptr<signin::PrimaryAccountAccessTokenFetcher>
+      access_token_fetcher_;
+  std::unique_ptr<OAuth2ApiCallFlow> get_locale_people_api_call_;
 
   std::string gaia_id_;
   ScreenExitCallback exit_callback_;
@@ -91,6 +109,7 @@ class LocaleSwitchScreen : public BaseScreen,
   base::OneShotTimer timeout_waiter_;
 
   bool refresh_token_loaded_ = false;
+  bool account_capabilities_loaded_ = false;
 
   base::WeakPtrFactory<LocaleSwitchScreen> weak_factory_{this};
 };
