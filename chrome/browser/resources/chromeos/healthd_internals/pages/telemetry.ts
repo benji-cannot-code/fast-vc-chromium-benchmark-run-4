5514537fc7b3456b5efccd0b2d216212808f882b
Byron Lee <byronlee@google.com>
2024-07-16 03:59:46 +0000

    healthd_internals: Update telemetry page to display data
    Add a InfoCard component to display text data and update telemtry page
    with a block of power and thermal.
    
    Screenshot: https://screenshot.googleplex.com/3Z3H7WzZnwmq7eP
    
    Bug: b:350423216
    TEST: Check chrome:healthd-intermals/telemetry on DUT
    Change-Id: Ieafb8363edbf5346d207e892b050108d7f9cfeb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5679194
    Commit-Queue: Byron Lee <byronlee@chromium.org>
    Reviewed-by: Ethan Cheng <yycheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1327944}

--- chrome/browser/resources/chromeos/healthd_internals/pages/telemetry.ts
+++ chrome/browser/resources/chromeos/healthd_internals/pages/telemetry.ts
@@ -3,12 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/polymer/v3_0/iron-location/iron-location.js';
-import '//resources/polymer/v3_0/iron-pages/iron-pages.js';
+import '../info_card/power_thermal_card.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
+import {HealthdApiTelemetryResult} from '../externs.js';
+import type {HealthdInternalsPowerThermalCardElement} from '../info_card/power_thermal_card.js';
+
 import {getTemplate} from './telemetry.html.js';
+
+export interface HealthdInternalsTelemetryElement {
+  $: {
+    powerThermalCard: HealthdInternalsPowerThermalCardElement,
+  };
+}
 
 export class HealthdInternalsTelemetryElement extends PolymerElement {
   static get is() {
@@ -17,6 +25,10 @@ export class HealthdInternalsTelemetryElement extends PolymerElement {
 
   static get template() {
     return getTemplate();
+  }
+
+  updateTelemetryData(data: HealthdApiTelemetryResult) {
+    this.$.powerThermalCard.updateTelemetryData(data);
   }
 }
 
