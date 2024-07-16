e092fdafee638e29175fbf1af48520c31c076b52
Alex Turner <alexmt@chromium.org>
2024-07-16 16:21:20 +0000

    Extend NotFatalUntil for certain Subresource Filter CHECKs
    These CHECKs are (rarely) being hit, so let's extend the expiry by one
    milestone to avoid crashes as investigation continues.
    
    Bug: 336753737
    Change-Id: I524eb9362ce066b8afc946e00265c47ff8faf543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709390
    Commit-Queue: Alex Turner <alexmt@chromium.org>
    Auto-Submit: Alex Turner <alexmt@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328227}

--- components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
+++ components/subresource_filter/content/browser/content_subresource_filter_throttle_manager.cc
@@ -154,7 +154,7 @@ void ContentSubresourceFilterThrottleManager::ReadyToCommitInFrameNavigation(
              base::Contains(
                  ad_frames_,
                  frame_host->GetParentOrOuterDocument()->GetFrameTreeNodeId()),
-             base::NotFatalUntil::M129);
+             base::NotFatalUntil::M130);
     ad_evidence.set_is_complete();
     ad_evidence_for_navigation = ad_evidence;
 
@@ -304,7 +304,7 @@ void ContentSubresourceFilterThrottleManager::DidFinishInFrameNavigation(
     // (regardless of the URL).
     CHECK(!(navigation_handle->GetURL().IsAboutBlank() &&
             EnsureFrameAdEvidence(navigation_handle).IndicatesAdFrame()),
-          base::NotFatalUntil::M129);
+          base::NotFatalUntil::M130);
   } else {
     CHECK(navigation_handle->IsInMainFrame() ||
               EnsureFrameAdEvidence(navigation_handle).is_complete(),
@@ -556,7 +556,7 @@ void ContentSubresourceFilterThrottleManager::OnChildFrameNavigationEvaluated(
            base::Contains(ad_frames_,
                           navigation_handle->GetParentFrameOrOuterDocument()
                               ->GetFrameTreeNodeId()),
-           base::NotFatalUntil::M129);
+           base::NotFatalUntil::M130);
 
   ad_evidence.UpdateFilterListResult(
       InterpretLoadPolicyAsEvidence(load_policy));
