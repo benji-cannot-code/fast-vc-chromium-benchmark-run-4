3f08e8c94f61a23830d53cecdbf85f135145959b
Jeroen Dhollander <jeroendh@google.com>
2024-07-16 09:21:51 +0000

    Reland "Make InitializeKioskSystemSession private"
    This is a reland of commit c3ac621db30952ebeee32404d709b1296a337db9
    
    The issue was the MSan MinimumVersionKioskAutoLoginTest.AllowAutoLaunch
    relies on a launch restart, which we prevented. This change more closely
    maintains the previous behavior.
    
    Original change's description:
    > Make InitializeKioskSystemSession private
    >
    > This is invoked when the kiosk session has launched successfully
    > so there really should be no reason for this method to be accessible from
    > outside the kiosk code.
    >
    > Bug: 352288716
    > Test: testing/xvfb.py tools/autotest.py -C out/Release chrome/browser/ash/login/app_mode/kiosk_launch_controller_unittest.cc
    > Change-Id: Ic096a90814be60247680034e2149e5f6095b9f9d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5522293
    > Reviewed-by: Sergii Bykov <sbykov@google.com>
    > Commit-Queue: Edman Anjos <edman@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1326613}
    
    Bug: 352288716
    Change-Id: Idfb115897a389d9ead7c4422acae049f60be9183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704067
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328027}

--- chrome/browser/ash/app_mode/kiosk_test_helper.h
+++ chrome/browser/ash/app_mode/kiosk_test_helper.h
@@ -21,7 +21,6 @@ class KioskTestHelper {
 
   [[nodiscard]] static base::AutoReset<bool> SkipSplashScreenWait();
   [[nodiscard]] static base::AutoReset<bool> BlockAppLaunch();
-  [[nodiscard]] static base::AutoReset<bool> BlockSystemSessionCreation();
   [[nodiscard]] static base::AutoReset<bool> BlockExitOnFailure();
 };
 
