338270ca49fdec0f132cfb01bd2de6bd51b635d2
HAIFAN WANG <wanghaifan@google.com>
2024-07-16 22:13:46 +0000

    Correctly report kDemoModeApp to the metrics DemoMode.AppLaunchSource
    Before the Highlights app was converted to the Demo Mode SWA, it used
    [arc_apps_private_api.cc]ArcAppsPrivateLaunchAppFunction::Run() to
    launch ARC apps, and [chrome_management_api_delegate.cc]
    ChromeManagementAPIDelegate::LaunchAppFunctionDelegate() to launch
    Chrome apps. Thus, the metrics of kExtensionApi (now called
    kDemoModeApp) were reported there. This CL crrev/c/1445395 initially
    added the metrics in 2019. This metrics is not reported anymore so we
    can remove the old places.
    
    We now use the LaunchApp API to trigger launches of other installed
    web and Android apps from the Demo Mode App. Therefore, we should
    record the metrics over here in chrome_demo_mode_app_delegate.cc.
    This CL crrev/c/4632069 added the LaunchApp API.
    
    More comments are added to the browser test
    DemoModeAppIntegrationTest.LaunchWebAppFromComponentContent in
    demo_mode_app_integration_browsertest.cc.
    
    Rename the metrics enum "Highlights App" to "Demo Mode App" and the
    AppLaunchSource enum kExtensionApi to kDemoModeApp as a better naming.
    
    Bug: b/328822050
    Change-Id: Icb10661f22a8e7c8e4ec55c9e7e96f169f788fa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689482
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Commit-Queue: Haifan Wang <wanghaifan@google.com>
    Reviewed-by: Xiqi Ruan <xiqiruan@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328473}

--- chrome/browser/extensions/api/management/chrome_management_api_delegate.cc
+++ chrome/browser/extensions/api/management/chrome_management_api_delegate.cc
@@ -70,10 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/manifest/display_mode.mojom.h"
 #include "third_party/blink/public/mojom/window_features/window_features.mojom.h"
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ash/login/demo_mode/demo_session.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
-
 namespace {
 using InstallOrLaunchWebAppCallback =
     extensions::ManagementAPIDelegate::InstallOrLaunchWebAppCallback;
@@ -434,11 +430,6 @@ bool ChromeManagementAPIDelegate::LaunchAppFunctionDelegate(
       apps::AppLaunchParams(extension->id(), launch_container,
                             WindowOpenDisposition::NEW_FOREGROUND_TAB,
                             apps::LaunchSource::kFromManagementApi));
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  ash::DemoSession::RecordAppLaunchSourceIfInDemoMode(
-      ash::DemoSession::AppLaunchSource::kExtensionApi);
-#endif
 
   extensions::RecordAppLaunchType(extension_misc::APP_LAUNCH_EXTENSION_API,
                                   extension->GetType());
