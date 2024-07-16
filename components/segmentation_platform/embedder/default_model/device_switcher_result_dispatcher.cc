416741da4fac44dfc8339cdeb1041c2ec82e57ce
ssid <ssid@chromium.org>
2024-07-16 23:01:42 +0000

    Device swicther: add timeout for wait call
    The UI cannot wait forever for result, so add a timeout that can be used
    by the FRE
    
    Change-Id: I917e4d866258265c1ac1586b9d712e7dd3e5b783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709830
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328505}

--- components/segmentation_platform/embedder/default_model/device_switcher_result_dispatcher.cc
+++ components/segmentation_platform/embedder/default_model/device_switcher_result_dispatcher.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/default_model/device_switcher_result_dispatcher.h"
 
 #include "base/feature_list.h"
+#include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
 #include "components/prefs/pref_registry_simple.h"
 #include "components/prefs/scoped_user_pref_update.h"
@@ -56,6 +57,7 @@ DeviceSwitcherResultDispatcher::GetCachedClassificationResult() {
 }
 
 void DeviceSwitcherResultDispatcher::WaitForClassificationResult(
+    base::TimeDelta timeout,
     ClassificationResultCallback callback) {
   if (latest_result_) {
     std::move(callback).Run(std::move(*latest_result_));
@@ -65,6 +67,12 @@ void DeviceSwitcherResultDispatcher::WaitForClassificationResult(
   // This class does not support waiting for multiple requests.
   DCHECK(waiting_callback_.is_null());
   waiting_callback_ = std::move(callback);
+
+  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+      FROM_HERE,
+      base::BindOnce(&DeviceSwitcherResultDispatcher::OnWaitTimeout,
+                     weak_ptr_factory_.GetWeakPtr()),
+      timeout);
 }
 
 // static
@@ -107,6 +115,13 @@ void DeviceSwitcherResultDispatcher::OnGotResult(
 
   if (!waiting_callback_.is_null()) {
     std::move(waiting_callback_).Run(result);
+  }
+}
+
+void DeviceSwitcherResultDispatcher::OnWaitTimeout() {
+  if (!waiting_callback_.is_null()) {
+    std::move(waiting_callback_)
+        .Run(ClassificationResult(PredictionStatus::kNotReady));
   }
 }
 
