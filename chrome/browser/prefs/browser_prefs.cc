18eb20ed880c5129b1ffa0abce5abdd9a5635a87
Tim Sergeant <tsergeant@chromium.org>
2024-07-16 23:43:38 +0000

    Remove App Deduplication Service
    This code was never launched, and is unused. It can safely be removed.
    
    Bug: crbug.com/330210942, b:351216445
    Change-Id: I819cca09b4ae5cc5d2f7e430d4042c959256d375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706732
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328531}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -346,7 +346,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/arc_prefs.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/public/cpp/ash_prefs.h"
-#include "chrome/browser/apps/app_deduplication_service/app_deduplication_service.h"
 #include "chrome/browser/apps/app_discovery_service/almanac_fetcher.h"
 #include "chrome/browser/apps/app_preload_service/app_preload_service.h"
 #include "chrome/browser/apps/app_service/metrics/app_platform_metrics_service.h"
@@ -1017,6 +1016,12 @@ constexpr char kNtpModulesFreVisible[] = "NewTabPage.ModulesFreVisible";
 constexpr char kNtpModulesShownCount[] = "NewTabPage.ModulesShownCount";
 #endif
 
+// Deprecated 07/2024.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+constexpr char kAppDeduplicationServiceLastGetTimestamp[] =
+    "apps.app_deduplication_service.last_get_data_from_server_timestamp";
+#endif
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1384,6 +1389,12 @@ void RegisterProfilePrefsForMigration(
   registry->RegisterBooleanPref(kNtpModulesFreVisible, true);
   registry->RegisterIntegerPref(kNtpModulesShownCount, 0);
   registry->RegisterTimePref(kNtpModulesFirstShownTime, base::Time());
+#endif
+
+  // Deprecated 07/2024.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  registry->RegisterTimePref(kAppDeduplicationServiceLastGetTimestamp,
+                             base::Time());
 #endif
 }
 
@@ -1966,7 +1977,6 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry,
   apps::AlmanacFetcher::RegisterProfilePrefs(registry);
   apps::AppPlatformMetricsService::RegisterProfilePrefs(registry);
   apps::AppPreloadService::RegisterProfilePrefs(registry);
-  apps::deduplication::AppDeduplicationService::RegisterProfilePrefs(registry);
   apps::webapk_prefs::RegisterProfilePrefs(registry);
   arc::prefs::RegisterProfilePrefs(registry);
   ArcAppListPrefs::RegisterProfilePrefs(registry);
@@ -2662,6 +2672,12 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
   profile_prefs->ClearPref(kNtpModulesFreVisible);
   profile_prefs->ClearPref(kNtpModulesShownCount);
 #endif
+
+  // Added 07/2024.
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  profile_prefs->ClearPref(kAppDeduplicationServiceLastGetTimestamp);
+#endif
+
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_PROFILE_PREFS
 
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -305,7 +305,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
-#include "chrome/browser/apps/app_deduplication_service/app_deduplication_service_factory.h"
 #include "chrome/browser/apps/app_preload_service/app_preload_service_factory.h"
 #include "chrome/browser/apps/app_service/publishers/web_apps_crosapi_factory.h"
 #include "chrome/browser/apps/app_service/subscriber_crosapi_factory.h"
@@ -615,7 +614,6 @@ void ChromeBrowserMainExtraPartsProfiles::
   apps::AppServiceProxyFactory::GetInstance();
 #endif
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  apps::deduplication::AppDeduplicationServiceFactory::GetInstance();
   apps::StandaloneBrowserExtensionAppsFactoryForApp::GetInstance();
   apps::StandaloneBrowserExtensionAppsFactoryForExtension::GetInstance();
   apps::SubscriberCrosapiFactory::GetInstance();
