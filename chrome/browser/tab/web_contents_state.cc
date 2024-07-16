4fbaa65f71fc368c1c67f69d98e62c6f6593b0c5
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-16 17:24:01 +0000

    [Tab Group Sync] Fix rare crash when navigating background tabs
    When freezing and appending a navigation entry we would return a null
    WebContentsState from native in two cases leading to an NPE. This fixes
    that rare crash which only happens when TabGroupSync is enabled.
    
    Case 1 - The old WebContentsState could not be parse.
    Solution - clobber the previous navigation state and return a
    WebContentsState containing a single entry.
    
    Case 2 - Buffer allocation failed when amending the navigation entries.
    Solution - treat the tab as a pending lazy load tab instead.
    
    Histograms have been added for both cases to understand how often they
    occur. This should address the crash.
    
    Tests for these cases are very difficult to implement as they require
    forcing a consistently passing codepath to fail by corrupting a
    WebContentsState or failing to allocate memory. I manually tested the
    pathways for now by forcing the code to fail in custom builds and
    verifying we recovered.
    
    Fixed: 353187234
    Change-Id: Ib5ec48369b8eaa051c984df0b06f3404dcef4ec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713948
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328276}

--- chrome/browser/tab/web_contents_state.cc
+++ chrome/browser/tab/web_contents_state.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_bytebuffer.h"
 #include "base/android/jni_string.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/pickle.h"
 #include "chrome/browser/profiles/profile.h"
@@ -667,8 +668,16 @@ ScopedJavaLocalRef<jobject> WebContentsState::AppendPendingNavigation(
   bool success = WebContentsState::ExtractNavigationEntries(
       buffer, saved_state_version, &is_off_the_record, &current_entry_index,
       &navigations);
-  if (!success || jis_off_the_record != is_off_the_record) {
-    return ScopedJavaLocalRef<jobject>();
+
+  bool safeToAppend = success && jis_off_the_record == is_off_the_record;
+  base::UmaHistogramBoolean(
+      "Tabs.DeserializationResultForAppendPendingNavigation", safeToAppend);
+  if (!safeToAppend) {
+    LOG(WARNING) << "Failed to deserialize navigation entries, clobbering "
+                    "previous navigation state.";
+    return CreateSingleNavigationStateAsByteBuffer(
+        env, title, url, referrer_url, referrer_policy, jinitiator_origin,
+        jis_off_the_record);
   }
 
   std::vector<sessions::SerializedNavigationEntry> new_navigations;
