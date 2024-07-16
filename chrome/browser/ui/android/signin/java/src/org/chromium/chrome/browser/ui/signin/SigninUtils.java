a35219b9583ce0e5ad0553ef2e1f456060a167a6
Menghan Yang <myuu@google.com>
2024-07-16 11:14:28 +0000

    [UNOp2][Android] Use the new sign-in flow on Automotive
    This CL enables the bottom-sheet based new sign-in flow for Android
    Automotive. The upgrade promo will remain suppressed for Automotive.
    
    See https://crbug.com/352394844#comment3 for recording.
    
    Fixed: 352394844
    Change-Id: I3901bcf9b35c4e05e7211578e726516f6e924ce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5695645
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328063}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninUtils.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/SigninUtils.java
@@ -16,7 +16,6 @@ import android.text.TextUtils;
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
 
-import org.chromium.base.BuildInfo;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -313,7 +312,6 @@ public final class SigninUtils {
      */
     public static boolean shouldShowNewSigninFlow() {
         return ChromeFeatureList.isEnabled(
-                        ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS)
-                && !BuildInfo.getInstance().isAutomotive;
+                ChromeFeatureList.REPLACE_SYNC_PROMOS_WITH_SIGN_IN_PROMOS);
     }
 }
