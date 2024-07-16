14623ddb7ac8155baec57bb84f859fc421ee6460
Tsuyoshi Horo <horo@chromium.org>
2024-07-16 05:28:43 +0000

    Start ConsumeCodeCacheTask from the loading background thread.
    This CL introduces a new kBackgroundCodeCacheDecoderStart feature param.
    When this feature is enabled, the ConsumeCodeCacheTask is started from
    the loading background thread. So we can start the ConsumeCodeCacheTask
    even when the main thread is busy.
    
    This CL uses a new ScriptCompiler::StartConsumingCodeCacheOnBackground()
    method which was introduced by https://crrev.com/c/v8/v8/+/5685117.
    
    Bug: 40244488
    Change-Id: Ia3c26912a7a9bba54cde8a82c8c6ca3645f7b910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5373700
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327962}

--- third_party/blink/renderer/bindings/core/v8/script_decoder.h
+++ third_party/blink/renderer/bindings/core/v8/script_decoder.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/location.h"
 #include "base/task/sequenced_task_runner.h"
+#include "mojo/public/cpp/system/data_pipe.h"
+#include "mojo/public/cpp/system/data_pipe_drainer.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/bindings/parkable_string.h"
 #include "third_party/blink/renderer/platform/crypto.h"
@@ -90,6 +92,61 @@ class CORE_EXPORT ScriptDecoder {
   Digestor digestor_{kHashAlgorithmSha256};
   scoped_refptr<base::SequencedTaskRunner> client_task_runner_;
   scoped_refptr<base::SequencedTaskRunner> decoding_task_runner_;
+  StringBuilder builder_;
+
+  SegmentedBuffer raw_data_;
+};
+
+class DataPipeScriptDecoder;
+struct CORE_EXPORT DataPipeScriptDecoderDeleter {
+  void operator()(const DataPipeScriptDecoder* ptr);
+};
+
+using DataPipeScriptDecoderPtr =
+    std::unique_ptr<DataPipeScriptDecoder, DataPipeScriptDecoderDeleter>;
+
+// DataPipeScriptDecoder decodes and hashes the script source of a Mojo data
+// pipe on a worker thread. The OnDecodeFinishedCallback will receive the raw
+// data and the decoded data.
+// Currently this class is used only when BackgroundCodeCacheDecoderStart
+// feature is enabled.
+class CORE_EXPORT DataPipeScriptDecoder final
+    : public mojo::DataPipeDrainer::Client {
+ public:
+  using OnDecodeFinishedCallback =
+      CrossThreadOnceFunction<void(ScriptDecoder::Result)>;
+
+  // Creates a DataPipeScriptDecoder.
+  static DataPipeScriptDecoderPtr Create(
+      std::unique_ptr<TextResourceDecoder> decoder,
+      scoped_refptr<base::SequencedTaskRunner> client_task_runner,
+      OnDecodeFinishedCallback on_decode_finished_callback);
+
+  void Start(mojo::ScopedDataPipeConsumerHandle source);
+
+ private:
+  friend struct DataPipeScriptDecoderDeleter;
+
+  DataPipeScriptDecoder(
+      std::unique_ptr<TextResourceDecoder> decoder,
+      scoped_refptr<base::SequencedTaskRunner> client_task_runner,
+      OnDecodeFinishedCallback on_decode_finished_callback);
+  void Delete() const;
+
+  // implements mojo::DataPipeDrainer::Client
+  void OnDataAvailable(base::span<const uint8_t> data) override;
+  void OnDataComplete() override;
+
+  void AppendData(const String& data);
+
+  std::unique_ptr<TextResourceDecoder> decoder_;
+  scoped_refptr<base::SequencedTaskRunner> client_task_runner_;
+  OnDecodeFinishedCallback on_decode_finished_callback_;
+  scoped_refptr<base::SequencedTaskRunner> decoding_task_runner_;
+
+  std::unique_ptr<mojo::DataPipeDrainer> drainer_;
+  Digestor digestor_{kHashAlgorithmSha256};
+  DigestValue digest_value_;
   StringBuilder builder_;
 
   SegmentedBuffer raw_data_;
