d19213010e8840893b9d734778e39ce005fe10d7
Nicolás Peña <npm@chromium.org>
2024-07-16 22:26:49 +0000

    [FedCM] Fix a crash when closing a dialog in certain states
    Remove a default case and instead consider all cases explicitly in
    GetSheetType().
    
    Fixed: 353570003
    Change-Id: I92ec1a09746f7a8fb0264bb675817cb0d6561ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5714335
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328482}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
@@ -936,6 +936,8 @@ FedCmAccountSelectionView::SheetType FedCmAccountSelectionView::GetSheetType() {
     case State::SINGLE_ACCOUNT_PICKER:
     case State::MULTI_ACCOUNT_PICKER:
     case State::REQUEST_PERMISSION:
+    case State::SINGLE_RETURNING_ACCOUNT_PICKER:
+    case State::NEWLY_LOGGED_IN_ACCOUNT_PICKER:
       return SheetType::ACCOUNT_SELECTION;
 
     case State::VERIFYING:
@@ -949,9 +951,6 @@ FedCmAccountSelectionView::SheetType FedCmAccountSelectionView::GetSheetType() {
 
     case State::LOADING:
       return SheetType::LOADING;
-
-    default:
-      NOTREACHED_NORETURN();
   }
 }
 
@@ -1039,7 +1038,8 @@ bool FedCmAccountSelectionView::IsIdpSigninPopupOpen() {
   return popup_window_ && (state_ == State::LOADING ||
                            state_ == State::IDP_SIGNIN_STATUS_MISMATCH ||
                            state_ == State::SINGLE_ACCOUNT_PICKER ||
-                           state_ == State::MULTI_ACCOUNT_PICKER);
+                           state_ == State::MULTI_ACCOUNT_PICKER ||
+                           state_ == State::NEWLY_LOGGED_IN_ACCOUNT_PICKER);
 }
 
 void FedCmAccountSelectionView::PrimaryMainFrameWasResized(bool width_changed) {
