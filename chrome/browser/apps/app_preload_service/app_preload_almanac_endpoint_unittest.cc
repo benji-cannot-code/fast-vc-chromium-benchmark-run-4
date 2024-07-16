758557842c0db168829a404bd596d700b96e1503
Tim Sergeant <tsergeant@chromium.org>
2024-07-16 06:57:19 +0000

    APS: Remove ServerRoundTripTimeForFirstLogin metric
    This metric is about to expire, and has not been particularly useful
    over the past year. We can remove it.
    
    OBSOLETE_HISTOGRAMS=Removed due to lack of use
    
    Bug: crbug.com/353280411
    Change-Id: Ic12129134c98932b9877280b65d2c726a371b4b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5711209
    Reviewed-by: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1327990}

--- chrome/browser/apps/app_preload_service/app_preload_almanac_endpoint_unittest.cc
+++ chrome/browser/apps/app_preload_service/app_preload_almanac_endpoint_unittest.cc
@@ -31,13 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace {
-
-constexpr char kServerRoundTripHistogram[] =
-    "AppPreloadService.ServerRoundTripTimeForFirstLogin";
-
-}  // namespace
-
 namespace apps {
 
 class AppPreloadAlmanacEndpointTest : public testing::Test {
@@ -201,8 +194,6 @@ TEST_F(AppPreloadAlmanacEndpointTest, GetAppsForFirstLoginSuccessfulResponse) {
   EXPECT_EQ(shelf_pin_ordering.size(), 2u);
   EXPECT_EQ(shelf_pin_ordering[web_app1], 1u);
   EXPECT_EQ(shelf_pin_ordering[android_app1], 1u);
-
-  histograms_.ExpectTotalCount(kServerRoundTripHistogram, 1);
 }
 
 TEST_F(AppPreloadAlmanacEndpointTest, GetAppsForFirstLoginServerError) {
@@ -216,8 +207,6 @@ TEST_F(AppPreloadAlmanacEndpointTest, GetAppsForFirstLoginServerError) {
   app_preload_almanac_endpoint::GetAppsForFirstLogin(
       DeviceInfo(), *test_shared_loader_factory_, result.GetCallback());
   EXPECT_FALSE(std::get<0>(result.Get()).has_value());
-
-  histograms_.ExpectTotalCount(kServerRoundTripHistogram, 0);
 }
 
 TEST_F(AppPreloadAlmanacEndpointTest, GetAppsForFirstLoginNetworkError) {
@@ -232,8 +221,6 @@ TEST_F(AppPreloadAlmanacEndpointTest, GetAppsForFirstLoginNetworkError) {
   app_preload_almanac_endpoint::GetAppsForFirstLogin(
       DeviceInfo(), *test_shared_loader_factory_, result.GetCallback());
   EXPECT_FALSE(std::get<0>(result.Get()).has_value());
-
-  histograms_.ExpectTotalCount(kServerRoundTripHistogram, 0);
 }
 
 }  // namespace apps
