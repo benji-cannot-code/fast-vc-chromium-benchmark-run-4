68e73862b4496c86cec10aaa06fb1115585faa96
Haihan Chen <haihan@google.com>
2024-07-16 18:10:31 +0000

    Add experiment for enabling SB regionalized endpoints
    Have experiment to start ramping up new regionalized endpoint logic when ChromeDataRegionSetting policy is enabled. Related logic CL: https://crrev.com/c/5478767.
    
    Also changed feature name string to be more in line with the rest.
    
    Bug: b/353524410
    Change-Id: Ieea731e34edd4ecea8eed87a54637db58367ad2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713834
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328312}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -84,7 +84,7 @@ const base::FeatureParam<bool> kDelayedWarningsEnableMouseClicks{
     /*default_value=*/false};
 
 BASE_FEATURE(kDlpRegionalizedEndpoints,
-             "kDlpRegionalizedEndpoints",
+             "DlpRegionalizedEndpoints",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kDownloadReportWithoutUserDecision,
