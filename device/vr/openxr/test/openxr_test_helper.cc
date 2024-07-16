ba0086b07f7ef292bf1205c3ecda89515b54e734
Alex Gough <ajgo@chromium.org>
2024-07-16 20:28:26 +0000

    Turn iterator _!=_.end() DCHECK() into CHECK() for device/vr
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _.end());' \
          -p 'DCHECK(_.end() != _);' \
          -p 'DCHECK_NE(_, _.end());' \
          -p 'DCHECK_NE(_.end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=rafael.cintron@microsoft.com
    
    Bug: 351745839
    Change-Id: Ifba90f0e0d7ec695bf4d64e021f333fb57c69ac7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706266
    Commit-Queue: Rafael Cintron <rafael.cintron@microsoft.com>
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328411}

--- device/vr/openxr/test/openxr_test_helper.cc
+++ device/vr/openxr/test/openxr_test_helper.cc
@@ -475,7 +475,7 @@ device::OpenXrViewConfiguration& OpenXrTestHelper::GetViewConfigInfo(
 
   const auto& secondary_config = secondary_configs_supported_.find(view_config);
   // The view configuration type should have been validated by the caller.
-  DCHECK(secondary_config != secondary_configs_supported_.end());
+  CHECK(secondary_config != secondary_configs_supported_.end());
 
   return secondary_config->second;
 }
@@ -640,7 +640,7 @@ void OpenXrTestHelper::ReinitializeTextures() {
   // view configuration.
   const auto primary =
       primary_configs_supported_.find(view_configs_enabled_[0]);
-  DCHECK(primary != primary_configs_supported_.end());
+  CHECK(primary != primary_configs_supported_.end());
   AddDimensions(primary->second, total_width, total_height);
 
   // Add secondary views
@@ -650,7 +650,7 @@ void OpenXrTestHelper::ReinitializeTextures() {
            primary_configs_supported_.end());
     const auto secondary =
         secondary_configs_supported_.find(view_configs_enabled_[i]);
-    DCHECK(secondary != secondary_configs_supported_.end());
+    CHECK(secondary != secondary_configs_supported_.end());
     if (secondary->second.Active()) {
       AddDimensions(secondary->second, total_width, total_height);
     }
