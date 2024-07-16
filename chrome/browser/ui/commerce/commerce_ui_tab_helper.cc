36d4713abcfc84c6940a93d5ce6cd25db40d032b
Mei Liang <meiliang@chromium.org>
2024-07-16 19:00:39 +0000

    [Discounts] Add discounts page action controller
    The DiscountsPageActionController controls the visibility of the
    discount page action icon and the discount bubble view.
    
    This cl also use the DiscountsPageActionController in
    CommerceUiTabHelper behind a flag.
    
    Bug: 351935350
    Change-Id: Ifd5e1e798b98a1699a8cd4844be629f4b7415ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691257
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328353}

--- chrome/browser/ui/commerce/commerce_ui_tab_helper.cc
+++ chrome/browser/ui/commerce/commerce_ui_tab_helper.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/commerce/commerce_page_action_controller.h"
+#include "chrome/browser/ui/commerce/discounts_page_action_controller.h"
 #include "chrome/browser/ui/commerce/price_tracking_page_action_controller.h"
 #include "chrome/browser/ui/commerce/product_specifications_page_action_controller.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
@@ -103,36 +104,26 @@ CommerceUiTabHelper::CommerceUiTabHelper(
   auto* tracker = feature_engagement::TrackerFactory::GetForBrowserContext(
       web_contents()->GetBrowserContext());
 
-  base::RepeatingCallback<void()> update_callback = base::BindRepeating(
-      [](base::WeakPtr<CommerceUiTabHelper> helper) {
-        if (!helper) {
-          return;
-        }
-
-        helper->MaybeComputePageActionToExpand();
-        helper->UpdatePriceTrackingIconView();
-      },
-      weak_ptr_factory_.GetWeakPtr());
-
   price_tracking_controller_ =
       std::make_unique<PriceTrackingPageActionController>(
-          std::move(update_callback), shopping_service_, image_fetcher_,
-          tracker);
+          GetPageActionControllerNotificationCallback(base::BindRepeating(
+              &CommerceUiTabHelper::UpdatePriceTrackingIconView,
+              weak_ptr_factory_.GetWeakPtr())),
+          shopping_service_, image_fetcher_, tracker);
 
-  base::RepeatingCallback<void()> product_specifications_update_callback =
-      base::BindRepeating(
-          [](base::WeakPtr<CommerceUiTabHelper> helper) {
-            if (!helper) {
-              return;
-            }
-
-            helper->MaybeComputePageActionToExpand();
-            helper->UpdateProductSpecificationsIconView();
-          },
-          weak_ptr_factory_.GetWeakPtr());
   product_specifications_controller_ =
       std::make_unique<ProductSpecificationsPageActionController>(
-          std::move(product_specifications_update_callback), shopping_service_);
+          GetPageActionControllerNotificationCallback(base::BindRepeating(
+              &CommerceUiTabHelper::UpdateProductSpecificationsIconView,
+              weak_ptr_factory_.GetWeakPtr())),
+          shopping_service_);
+
+  discounts_page_action_controller_ =
+      std::make_unique<DiscountsPageActionController>(
+          GetPageActionControllerNotificationCallback(
+              base::BindRepeating(&CommerceUiTabHelper::UpdateDiscountsIconView,
+                                  weak_ptr_factory_.GetWeakPtr())),
+          shopping_service_);
 }
 
 CommerceUiTabHelper::~CommerceUiTabHelper() = default;
@@ -188,6 +179,9 @@ void CommerceUiTabHelper::DidFinishNavigation(
     return;
   }
 
+  discounts_page_action_controller_->ResetForNewNavigation(
+      web_contents()->GetLastCommittedURL());
+
   if (shopping_service_->IsPriceInsightsEligible()) {
     UpdatePriceInsightsIconView();
   }
@@ -206,7 +200,8 @@ void CommerceUiTabHelper::DidFinishNavigation(
                        weak_ptr_factory_.GetWeakPtr()));
   }
 
-  if (shopping_service_->IsDiscountEligibleToShowOnNavigation()) {
+  if (!base::FeatureList::IsEnabled(commerce::kDiscountsUiRefactor) &&
+      shopping_service_->IsDiscountEligibleToShowOnNavigation()) {
     shopping_service_->GetDiscountInfoForUrls(
         {web_contents()->GetLastCommittedURL()},
         base::BindOnce(&CommerceUiTabHelper::HandleDiscountsResponse,
@@ -252,6 +247,11 @@ void CommerceUiTabHelper::SetImageFetcherForTesting(
   image_fetcher_ = image_fetcher;
 }
 
+bool CommerceUiTabHelper::ShouldShowDiscountsIconView() {
+  return discounts_page_action_controller_->ShouldShowForNavigation().value_or(
+      false);
+}
+
 bool CommerceUiTabHelper::ShouldShowPriceTrackingIconView() {
   return price_tracking_controller_->ShouldShowForNavigation().value_or(false);
 }
@@ -270,6 +270,9 @@ void CommerceUiTabHelper::HandleProductInfoResponse(
     const GURL& url,
     const std::optional<const ProductInfo>& info) {
   if (url != web_contents()->GetLastCommittedURL() || !info.has_value()) {
+    // Price insights is depended on ProductInfo, it's impossible to get
+    // insights without it.
+    got_insights_response_for_page_ = true;
     MaybeComputePageActionToExpand();
     return;
   }
@@ -306,6 +309,7 @@ void CommerceUiTabHelper::HandlePriceInsightsInfoResponse(
   TriggerUpdateForIconView();
 }
 
+// TODO(b/351935350): Remove this.
 void CommerceUiTabHelper::HandleDiscountsResponse(const DiscountsMap& map) {
   bool response_has_discounts = false;
   if (!map.empty()) {
@@ -334,10 +338,18 @@ void CommerceUiTabHelper::MaybeComputePageActionToExpand() {
   }
 
   // Make sure we have responses from all the relevant features first.
-  if (shopping_service_->IsDiscountEligibleToShowOnNavigation() &&
-      !got_discounts_response_for_page_) {
-    return;
+  if (base::FeatureList::IsEnabled(commerce::kDiscountsUiRefactor)) {
+    if (!discounts_page_action_controller_->ShouldShowForNavigation()
+             .has_value()) {
+      return;
+    }
+  } else {
+    if (shopping_service_->IsDiscountEligibleToShowOnNavigation() &&
+        !got_discounts_response_for_page_) {
+      return;
+    }
   }
+
   if (shopping_service_->IsPriceInsightsEligible() &&
       !got_insights_response_for_page_) {
     return;
@@ -366,6 +378,7 @@ void CommerceUiTabHelper::MaybeComputePageActionToExpand() {
   }
 
   UpdateProductSpecificationsIconView();
+  UpdateDiscountsIconView();
   UpdatePriceTrackingIconView();
   UpdatePriceInsightsIconView();
 }
@@ -453,6 +466,10 @@ bool CommerceUiTabHelper::IsInRecommendedSet() {
   return product_specifications_controller_->IsInRecommendedSet();
 }
 
+const std::vector<DiscountInfo>& CommerceUiTabHelper::GetDiscounts() {
+  return discounts_page_action_controller_->GetDiscounts();
+}
+
 void CommerceUiTabHelper::UpdatePriceTrackingIconView() {
   UpdatePageActionIconView(web_contents(), PageActionIconType::kPriceTracking);
 }
@@ -527,6 +544,13 @@ CommerceUiTabHelper::GetPriceInsightsInfo() {
   return price_insights_info_;
 }
 
+void CommerceUiTabHelper::UpdateDiscountsIconView() {
+  if (!base::FeatureList::IsEnabled(commerce::kDiscountsUiRefactor)) {
+    return;
+  }
+  // TODO(b/351935350): Update the new discounts icon.
+}
+
 bool CommerceUiTabHelper::IsShowingDiscountsIcon() {
   auto* browser = chrome::FindBrowserWithTab(web_contents());
   if (!browser) {
@@ -563,12 +587,19 @@ void CommerceUiTabHelper::ComputePageActionToExpand() {
   // TODO(b:301440117): Splitting the triggering logic for each icon into
   //                    delegates would make this much easier to test.
 
-  // We don't have full control over the discounts icon, so if we detect
-  // that it is showing at all, block the others from expanding.
-  if (IsShowingDiscountsIcon()) {
-    MaybeRecordShoppingInformationUKM(
-        PageActionIconType::kPaymentsOfferNotification);
-    return;
+  if (base::FeatureList::IsEnabled(commerce::kDiscountsUiRefactor)) {
+    if (discounts_page_action_controller_->WantsExpandedUi()) {
+      // TODO(b/351935350): expand the new discounts icon.
+      return;
+    }
+  } else {
+    // We don't have full control over the discounts icon, so if we detect
+    // that it is showing at all, block the others from expanding.
+    if (IsShowingDiscountsIcon()) {
+      MaybeRecordShoppingInformationUKM(
+          PageActionIconType::kPaymentsOfferNotification);
+      return;
+    }
   }
 
   if (ShouldShowProductSpecificationsIconView()) {
@@ -648,6 +679,20 @@ void CommerceUiTabHelper::OnProductSpecificationsIconClicked() {
   product_specifications_controller_->OnIconClicked();
 }
 
+void CommerceUiTabHelper::OnDiscountsCouponCodeCopied() {
+  discounts_page_action_controller_->CouponCodeCopied();
+}
+
+bool CommerceUiTabHelper::IsDiscountsCouponCodeCopied() {
+  return discounts_page_action_controller_->IsCouponCodeCopied();
+}
+
+bool CommerceUiTabHelper::ShouldAutoShowDiscountsBubble(uint64_t discount_id,
+                                                        bool is_merchant_wide) {
+  return discounts_page_action_controller_->ShouldAutoShowBubble(
+      discount_id, is_merchant_wide);
+}
+
 void CommerceUiTabHelper::RecordIconMetrics(PageActionIconType page_action,
                                             bool from_icon_use) {
   if (icon_use_recorded_for_page_.contains(page_action)) {
@@ -704,6 +749,7 @@ void CommerceUiTabHelper::MaybeRecordShoppingInformationUKM(
 
   if (page_action_type.has_value()) {
     int64_t promoted_feature = 0;
+    // TODO(b/351935350): Update this to the new discounts icon type.
     if (page_action_type == PageActionIconType::kPaymentsOfferNotification) {
       promoted_feature =
           static_cast<int64_t>(ShoppingContextualFeature::kDiscounts);
@@ -729,6 +775,21 @@ void CommerceUiTabHelper::MaybeRecordShoppingInformationUKM(
 PriceTrackingPageActionController*
 CommerceUiTabHelper::GetPriceTrackingControllerForTesting() {
   return price_tracking_controller_.get();
+}
+
+void CommerceUiTabHelper::OnPageActionControllerNotification(
+    base::RepeatingClosure page_action_icon_update_callback) {
+  MaybeComputePageActionToExpand();
+  page_action_icon_update_callback.Run();
+}
+
+base::RepeatingClosure
+CommerceUiTabHelper::GetPageActionControllerNotificationCallback(
+    base::RepeatingClosure page_action_icon_update_callback) {
+  return base::BindRepeating(
+      &CommerceUiTabHelper::OnPageActionControllerNotification,
+      weak_ptr_factory_.GetWeakPtr(),
+      std::move(page_action_icon_update_callback));
 }
 
 void CommerceUiTabHelper::SetPriceTrackingControllerForTesting(
