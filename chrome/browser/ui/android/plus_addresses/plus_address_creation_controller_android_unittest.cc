15996b8341ee0443db0eead7c13777f012c4080d
Timofey Chudakov <tchudakov@google.com>
2024-07-16 13:41:24 +0000

    Remove unnecessary ScopedProfileSelectionsForFactoryTesting usages.
    Bug: n/a
    Change-Id: Ifb439d02349a87c38bde2e250244251a329f8111
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5712308
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328119}

--- chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
+++ chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
@@ -63,10 +63,7 @@ class PlusAddressCreationControllerAndroidEnabledTest
  public:
   PlusAddressCreationControllerAndroidEnabledTest()
       : ChromeRenderViewHostTestHarness(
-            base::test::TaskEnvironment::TimeSource::MOCK_TIME),
-        override_profile_selections_(
-            PlusAddressServiceFactory::GetInstance(),
-            PlusAddressServiceFactory::CreateProfileSelections()) {}
+            base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
 
   void SetUp() override {
     ChromeRenderViewHostTestHarness::SetUp();
@@ -111,8 +108,6 @@ class PlusAddressCreationControllerAndroidEnabledTest
   base::test::ScopedFeatureList features_{features::kPlusAddressesEnabled};
   // Ensures that the feature is known to be enabled, such that
   // `PlusAddressServiceFactory` doesn't bail early with a null return.
-  profiles::testing::ScopedProfileSelectionsForFactoryTesting
-      override_profile_selections_;
   signin::IdentityTestEnvironment identity_test_env_;
 };
 
