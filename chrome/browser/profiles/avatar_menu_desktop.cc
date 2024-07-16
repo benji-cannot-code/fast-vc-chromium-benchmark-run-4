16b9f36993aac955f8552f13ae0a4bce80ff561f
Amelie Schneider <amelies@google.com>
2024-07-16 12:39:41 +0000

    [Profiles][Windows] Change bagded profile avatar icon
    Behind the feature flag "OutlineSilhouetteIcon", change the profile
    avatar icon displayed next to the browser icon on Windows to be the
    new outline silhouette icon.
    
    The new icon is set once the profile is opened with the feature flag
    enabled (https://screencast.googleplex.com/cast/NjQ2MTE1Mzk2MTA1MDExMnw3OGEyMjdkMS05YQ).
    
    Screenshots:
    - Icon badge on shortcut: https://screenshot.googleplex.com/7gW2rQLCw7REnhP
    - Icon badge in taskbar & window switcher: https://screenshot.googleplex.com/9ZptW6Vo5bx8Cm5
    
    Bug: b:307739359
    Requested changes: https://docs.google.com/presentation/d/1fVZMbZ5zpkpAcaSn31gTaX5F-_mOZRjK_azWMDUBXqE/edit#slide=id.g29410333a18_0_31
    
    Change-Id: Iaa3bb4744d73205f86dff861c844b7ba570d4126
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5678767
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328088}

--- chrome/browser/profiles/avatar_menu_desktop.cc
+++ chrome/browser/profiles/avatar_menu_desktop.cc
@@ -39,6 +39,7 @@ AvatarMenu::ImageLoadStatus AvatarMenu::GetImageForMenuButton(
       status = ImageLoadStatus::LOADING;
   }
 
-  *image = entry->GetAvatarIcon(preferred_size, /*use_high_res_file=*/false);
+  *image = entry->GetAvatarIcon(preferred_size, /*use_high_res_file=*/false,
+                                /*icon_params=*/{.has_padding = false});
   return status;
 }
