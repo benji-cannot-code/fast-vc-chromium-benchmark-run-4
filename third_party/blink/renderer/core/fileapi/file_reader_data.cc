6e99740a487fc10d0c7b7b44e5d2b06f7b36b627
Fredrik Söderqvist <fs@opera.com>
2024-07-16 18:47:49 +0000

    spanification: core/fileapi/file_reader_data.cc
    Use ArrayBufferContents::ByteSpan().
    
    Bug: 351564777
    Change-Id: I253bfbe242641380bd2dd6ba2d862f786a696b8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713412
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1328339}

--- third_party/blink/renderer/core/fileapi/file_reader_data.cc
+++ third_party/blink/renderer/core/fileapi/file_reader_data.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/fileapi/file_reader_data.h"
 
 #include "third_party/blink/renderer/core/html/parser/text_resource_decoder.h"
@@ -41,10 +36,7 @@ String ToDataURL(ArrayBufferContents raw_data, const String& data_type) {
 
   if (raw_data.DataLength()) {
     Vector<char> out;
-    Base64Encode(
-        base::make_span(static_cast<const uint8_t*>(raw_data.Data()),
-                        base::checked_cast<unsigned>(raw_data.DataLength())),
-        out);
+    Base64Encode(raw_data.ByteSpan(), out);
     builder.Append(out.data(), out.size());
   }
 
