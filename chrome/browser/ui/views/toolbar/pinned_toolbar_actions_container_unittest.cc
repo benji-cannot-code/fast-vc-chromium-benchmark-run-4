95be8d143309dee180d3a1b47e7d6828fad175d2
Caroline Rising <corising@chromium.org>
2024-07-16 14:33:35 +0000

    Create a custom layout for the PinnedToolbarActionsContainer
    This is needed since the container has a divider that's visibility is
    dependant on there being any pinned buttons included in the layout to
    the left. Currently our layouts cannot support this.
    
    Bug: 348453824
    Change-Id: I5d670ab2275ec89f1a37d7f59318e3fed9aaa2e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5676468
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328156}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_unittest.cc
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/test_with_browser_view.h"
 #include "chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.h"
+#include "chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_layout.h"
 #include "chrome/browser/ui/views/toolbar/pinned_toolbar_button_status_indicator.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_view.h"
@@ -183,16 +184,18 @@ class PinnedToolbarActionsContainerTest : public TestWithBrowserView {
 
 TEST_F(PinnedToolbarActionsContainerTest, ContainerMargins) {
   // Verify margins are correctly set.
-  ASSERT_EQ(container()
-                ->GetTargetLayoutManager()
-                ->GetDefaultForTesting(views::kMarginsKey)
-                ->left(),
-            GetLayoutConstant(TOOLBAR_ICON_DEFAULT_MARGIN));
-  ASSERT_EQ(container()
-                ->GetTargetLayoutManager()
-                ->GetDefaultForTesting(views::kMarginsKey)
-                ->right(),
-            GetLayoutConstant(TOOLBAR_ICON_DEFAULT_MARGIN));
+  ASSERT_EQ(
+      static_cast<PinnedToolbarActionsContainerLayout*>(
+          container()->GetAnimatingLayoutManager()->target_layout_manager())
+          ->interior_margin()
+          .left(),
+      -GetLayoutConstant(TOOLBAR_ICON_DEFAULT_MARGIN));
+  ASSERT_EQ(
+      static_cast<PinnedToolbarActionsContainerLayout*>(
+          container()->GetAnimatingLayoutManager()->target_layout_manager())
+          ->interior_margin()
+          .right(),
+      -GetLayoutConstant(TOOLBAR_ICON_DEFAULT_MARGIN));
 }
 
 TEST_F(PinnedToolbarActionsContainerTest, PinningAndUnpinning) {
