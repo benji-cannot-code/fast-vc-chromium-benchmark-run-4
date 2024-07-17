e6ee33a341b556e637aebc84f048b8cc397a90f0
Louis Romero <lpromero@google.com>
2024-07-17 14:27:59 +0000

    Wait for TabGroupsSyncService initialization before loading groups
    In order to avoid many collection view reloads, don't push anything to
    the consumer before the TabGroupSyncService is initialized.
    
    Bug: 329625483
    Change-Id: Ie4fffec371bdb0e5f6e7197877ed0b418432bb66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713254
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328845}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_panel_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_panel_mediator.mm
@@ -64,7 +64,9 @@ NSArray<TabGroupsPanelItem*>* CreateItems(
   raw_ptr<tab_groups::TabGroupSyncService> _tabGroupSyncService;
   // The bridge between the service C++ observer and this Objective-C class.
   std::unique_ptr<TabGroupSyncServiceObserverBridge> _syncServiceObserver;
-  std::unique_ptr<ScopedTabGroupSyncObservation> _scopedTabGroupSyncObservation;
+  std::unique_ptr<ScopedTabGroupSyncObservation> _scopedSyncServiceObservation;
+  // Whether the service was fully initialized.
+  bool _serviceInitialized;
   // The regular WebStateList, to check if there are tabs to go back to when
   // pressing the Done button.
   base::WeakPtr<WebStateList> _regularWebStateList;
@@ -83,10 +85,10 @@ NSArray<TabGroupsPanelItem*>* CreateItems(
     _tabGroupSyncService = tabGroupSyncService;
     _syncServiceObserver =
         std::make_unique<TabGroupSyncServiceObserverBridge>(self);
-    _scopedTabGroupSyncObservation =
+    _scopedSyncServiceObservation =
         std::make_unique<ScopedTabGroupSyncObservation>(
             _syncServiceObserver.get());
-    _scopedTabGroupSyncObservation->Observe(_tabGroupSyncService);
+    _scopedSyncServiceObservation->Observe(_tabGroupSyncService);
     _regularWebStateList = regularWebStateList->AsWeakPtr();
     _isDisabled = disabled;
   }
@@ -102,7 +104,7 @@ NSArray<TabGroupsPanelItem*>* CreateItems(
 
 - (void)disconnect {
   _consumer = nil;
-  _scopedTabGroupSyncObservation.reset();
+  _scopedSyncServiceObservation.reset();
   _syncServiceObserver.reset();
   _tabGroupSyncService = nullptr;
   _regularWebStateList = nullptr;
@@ -172,6 +174,7 @@ NSArray<TabGroupsPanelItem*>* CreateItems(
 #pragma mark - TabGroupSyncServiceObserverDelegate
 
 - (void)tabGroupSyncServiceInitialized {
+  _serviceInitialized = true;
   [self populateItemsFromService];
 }
 
@@ -230,7 +233,9 @@ NSArray<TabGroupsPanelItem*>* CreateItems(
 
 // Reads the TabGroupSync
 - (void)populateItemsFromService {
-  [_consumer populateItems:CreateItems(_tabGroupSyncService->GetAllGroups())];
+  if (_serviceInitialized) {
+    [_consumer populateItems:CreateItems(_tabGroupSyncService->GetAllGroups())];
+  }
 }
 
 @end
