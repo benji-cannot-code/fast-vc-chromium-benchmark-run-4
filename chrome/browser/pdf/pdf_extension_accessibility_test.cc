2900f105db96286aa78bfd4f56475e664fc209e0
Yuki Shiino <yukishiino@chromium.org>
2024-07-17 05:10:35 +0000

    Revert "PDF OCR: Update one metric to use whether OCR is actually available"
    This reverts commit 77e4ed416ca83c308bb8d3a4d58e98e7ad487d8b.
    
    Reason for revert: Suspicious about causing a test failure:
    All/PdfOcrUmaTest.CheckOpenedWithScreenReader/OOPIF_Disabled
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-blink-web-tests-force-accessibility-rel/27402/overview
    
    Original change's description:
    > PDF OCR: Update one metric to use whether OCR is actually available
    >
    > This CL updates `RecordPDFOpenedWithA11yFeatureWithPdfOcr()` to record
    > the Accessibility.PDF.OpenedWith*.PdfOcr metrics with the information
    > about whether the OCR model is actually initialized.
    >
    > OBSOLETE_HISTOGRAMS=Patterned histogram Accessibility.PDF.OpenedWith{A11yFeature}.PdfOcr is replaced by Accessibility.PDF.OpenedWith{A11yFeature}.PdfOcr2
    >
    > AX-Relnotes: n/a.
    > Bug: 40912114
    > Test: out/Debug/browser_tests --gtest_filter=*PdfOcrUmaTest*
    > Change-Id: I0ee6bc381ab67bff6ba30db5973e6fbb92eb399f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689764
    > Reviewed-by: Mark Schillaci <mschillaci@google.com>
    > Reviewed-by: Abigail Klein <abigailbklein@google.com>
    > Reviewed-by: Kevin McNee <mcnee@chromium.org>
    > Reviewed-by: Andy Phan <andyphan@chromium.org>
    > Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    > Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1328539}
    
    Bug: 40912114
    Change-Id: Ib4705f18833f9711e74dbb6bf43c5d56df69ae02
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5715475
    Owners-Override: Yuki Shiino <yukishiino@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Yuki Shiino <yukishiino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328638}

--- chrome/browser/pdf/pdf_extension_accessibility_test.cc
+++ chrome/browser/pdf/pdf_extension_accessibility_test.cc
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/accessibility/accessibility_state_utils.h"
-#include "chrome/browser/accessibility/pdf_ocr_controller.h"
-#include "chrome/browser/accessibility/pdf_ocr_controller_factory.h"
 #include "chrome/browser/pdf/pdf_extension_test_base.h"
 #include "chrome/browser/pdf/pdf_extension_test_util.h"
 #include "chrome/browser/renderer_context_menu/render_view_context_menu_browsertest_util.h"
@@ -1200,13 +1198,6 @@ class PdfOcrUmaTest : public PDFExtensionAccessibilityTest,
   // PDFExtensionAccessibilityTest:
   bool UseOopif() const override { return GetParam(); }
 
-  void SetUpOnMainThread() override {
-    PDFExtensionAccessibilityTest::SetUpOnMainThread();
-
-    screen_ai::PdfOcrControllerFactory::GetForProfile(browser()->profile())
-        ->set_ocr_ready_for_testing();
-  }
-
  protected:
   std::vector<base::test::FeatureRef> GetEnabledFeatures() const override {
     std::vector<base::test::FeatureRef> enabled =
@@ -1246,7 +1237,7 @@ IN_PROC_BROWSER_TEST_P(PdfOcrUmaTest, CheckOpenedWithScreenReader) {
 
   base::HistogramTester histograms;
   histograms.ExpectUniqueSample(
-      "Accessibility.PDF.OpenedWithScreenReader.PdfOcr2", true,
+      "Accessibility.PDF.OpenedWithScreenReader.PdfOcr", true,
       /*expected_bucket_count=*/0);
 
   ASSERT_TRUE(LoadPdf(embedded_test_server()->GetURL("/pdf/test.pdf")));
@@ -1258,7 +1249,7 @@ IN_PROC_BROWSER_TEST_P(PdfOcrUmaTest, CheckOpenedWithScreenReader) {
 
   metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
   histograms.ExpectUniqueSample(
-      "Accessibility.PDF.OpenedWithScreenReader.PdfOcr2", true,
+      "Accessibility.PDF.OpenedWithScreenReader.PdfOcr", true,
       /*expected_bucket_count=*/1);
 }
 
@@ -1274,7 +1265,7 @@ IN_PROC_BROWSER_TEST_P(PdfOcrUmaTest, CheckOpenedWithSelectToSpeak) {
 
   base::HistogramTester histograms;
   histograms.ExpectUniqueSample(
-      "Accessibility.PDF.OpenedWithSelectToSpeak.PdfOcr2", true,
+      "Accessibility.PDF.OpenedWithSelectToSpeak.PdfOcr", true,
       /*expected_bucket_count=*/0);
 
   ASSERT_TRUE(LoadPdf(embedded_test_server()->GetURL("/pdf/test.pdf")));
@@ -1286,7 +1277,7 @@ IN_PROC_BROWSER_TEST_P(PdfOcrUmaTest, CheckOpenedWithSelectToSpeak) {
 
   metrics::SubprocessMetricsProvider::MergeHistogramDeltasForTesting();
   histograms.ExpectUniqueSample(
-      "Accessibility.PDF.OpenedWithSelectToSpeak.PdfOcr2", true,
+      "Accessibility.PDF.OpenedWithSelectToSpeak.PdfOcr", true,
       /*expected_bucket_count=*/1);
 }
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
