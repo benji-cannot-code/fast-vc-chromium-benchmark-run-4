0d95abd1ea3b90d5348d0d4ca3221e55d786b771
Christoph Schwering <schwering@google.com>
2024-07-17 12:39:47 +0000

    [Autofill] Align FormCache lifecycle with AutofillAgent
    Before this CL, AutofillAgent creates a new FormCache on each Reset().
    
    This CL aligns the lifecycle of FormCache with its owner AutofillAgent.
    On Reset(), the FormCache is now cleared (which is equivalent to
    creating a new one).
    
    As a drive-by the CL fixes that ExtractFormsUnthrottled() might
    drop a response callback, which is an error.
    
    Bug: 40100455
    Change-Id: I394afaaa489e58769a199b390e777a92d3bbf48e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713656
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1328797}

--- components/autofill/content/renderer/autofill_agent.h
+++ components/autofill/content/renderer/autofill_agent.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/content/common/mojom/autofill_agent.mojom.h"
 #include "components/autofill/content/common/mojom/autofill_driver.mojom.h"
 #include "components/autofill/content/renderer/form_autofill_util.h"
+#include "components/autofill/content/renderer/form_cache.h"
 #include "components/autofill/content/renderer/form_tracker.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/field_data_manager.h"
@@ -49,7 +50,6 @@ class WebFormElement;
 
 namespace autofill {
 
-class FormCache;
 class PasswordAutofillAgent;
 class PasswordGenerationAgent;
 
@@ -66,7 +66,6 @@ class PasswordGenerationAgent;
 // To handle this state, care must be taken to check for nullptrs:
 // - `unsafe_autofill_driver()`
 // - `unsafe_render_frame()`
-// - `form_cache_`
 //
 // This RenderFrame owns all forms and fields in the renderer-browser
 // communication:
@@ -471,9 +470,8 @@ class AutofillAgent : public content::RenderFrameObserver,
   // the direction to traverse in.
   blink::WebNode NextWebNode(const blink::WebNode& current_node, bool next);
 
-  // Contains the form of the document. Does not survive navigation and is
-  // reset when the AutofillAgent is pending deletion.
-  std::unique_ptr<FormCache> form_cache_;
+  // Contains the forms of the document.
+  FormCache form_cache_{this};
 
   std::unique_ptr<PasswordAutofillAgent> password_autofill_agent_;
   std::unique_ptr<PasswordGenerationAgent> password_generation_agent_;
