e8f7cf55da7ed8da5c07b9499c6314dc8d335318
Colin Blundell <blundell@chromium.org>
2024-07-17 14:15:56 +0000

    [Canvas] Eliminate MailboxSyncMode
    Change the remaining usage to pass in a bool indicating whether a
    verified sync token is needed, as this is all that this enum is used
    for.
    
    Bug: 352263194
    Change-Id: Ib1b2d226cc876e1b8e06d6f73a602a1b0dc14b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713037
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328840}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -166,7 +166,7 @@ static void ReleaseFrameResources(
 bool CanvasResource::PrepareTransferableResource(
     viz::TransferableResource* out_resource,
     CanvasResource::ReleaseCallback* out_callback,
-    MailboxSyncMode sync_mode) {
+    bool needs_verified_synctoken) {
   DCHECK(IsValid());
 
   DCHECK(out_callback);
@@ -183,7 +183,7 @@ bool CanvasResource::PrepareTransferableResource(
   if (SupportsAcceleratedCompositing()) {
     return UsesClientSharedImage()
                ? PrepareAcceleratedTransferableResourceFromClientSI(
-                     out_resource, sync_mode)
+                     out_resource, needs_verified_synctoken)
                : PrepareAcceleratedTransferableResourceWithoutClientSI(
                      out_resource);
   }
@@ -192,7 +192,7 @@ bool CanvasResource::PrepareTransferableResource(
 
 bool CanvasResource::PrepareAcceleratedTransferableResourceFromClientSI(
     viz::TransferableResource* out_resource,
-    MailboxSyncMode sync_mode) {
+    bool needs_verified_synctoken) {
   TRACE_EVENT0("blink",
                "CanvasResource::PrepareAcceleratedTransferableResource");
   // This method should only be called if this instance actually supports
@@ -211,9 +211,8 @@ bool CanvasResource::PrepareAcceleratedTransferableResourceFromClientSI(
 
   *out_resource = viz::TransferableResource::MakeGpu(
       client_shared_image->mailbox(), client_shared_image->GetTextureTarget(),
-      GetSyncToken(sync_mode == kVerifiedSyncToken), Size(),
-      GetSharedImageFormat(), IsOverlayCandidate(),
-      viz::TransferableResource::ResourceSource::kCanvas);
+      GetSyncToken(needs_verified_synctoken), Size(), GetSharedImageFormat(),
+      IsOverlayCandidate(), viz::TransferableResource::ResourceSource::kCanvas);
 
   out_resource->color_space = GetColorSpace();
   if (NeedsReadLockFences()) {
--- third_party/blink/renderer/platform/graphics/graphics_types.h
+++ third_party/blink/renderer/platform/graphics/graphics_types.h
@@ -159,12 +159,6 @@ enum class RasterModeHint {
   kPreferCPU,
 };
 
-enum MailboxSyncMode {
-  kVerifiedSyncToken,
-  kUnverifiedSyncToken,
-  kOrderingBarrier,
-};
-
 enum AntiAliasingMode { kNotAntiAliased, kAntiAliased };
 
 enum GradientSpreadMethod {
