a8d1d74639a12b7990b252dbf8199c7ccd39ee70
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-07-17 18:31:43 +0000

    Import wpt@59497e65c6e6d21296d19f8ea61874b1a4660a7e
    https://github.com/web-platform-tests/wpt/compare/0291480c4...59497e65c
    
    Using wpt-import in Chromium a075cdc1399decfde9406d8e651be4f482bc8d11.
    With Chromium commits locally applied on WPT:
      5841a8c6f2 "Better box decoration cloning around monolithic content."
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bokan@chromium.org:
      external/wpt/scroll-to-text-fragment
    
    NOAUTOREVERT=true
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I6281a6866ce950bc3637bb62f4d8310d5bea655a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717468
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: An Sung <ansung@google.com>
    Commit-Queue: An Sung <ansung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329010}

--- third_party/blink/web_tests/external/wpt/url/resources/a-element-origin.js
+++ third_party/blink/web_tests/external/wpt/url/resources/a-element-origin.js
@@ -1,5 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-promise_test(() => fetch("resources/urltestdata.json").then(res => res.json()).then(runURLTests), "Loading data…");
+promise_test(() => Promise.all([
+  fetch("resources/urltestdata.json").then(res => res.json()),
+  fetch("resources/urltestdata-javascript-only.json").then(res => res.json()),
+]).then((tests) => tests.flat()).then(runURLTests), "Loading data…");
 
 function setBase(base) {
   document.getElementById("base").href = base
