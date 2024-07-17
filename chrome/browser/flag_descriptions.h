ba01487c90b9aa8967d15be699347023d3354f15
Xi Han <hanxi@chromium.org>
2024-07-17 17:01:26 +0000

    Revert "[Passwords] Update experiment for showing suggestions on autofocus."
    This reverts commit faf13fcd89ce3245b79032a1453baf2b84b97daa.
    
    Reason for revert: b/353701623
    
    Original change's description:
    > [Passwords] Update experiment for showing suggestions on autofocus.
    >
    > The experiment was initially targeting only WebAuthn forms, but it
    > provided very little data, because there aren't many such forms in the wild. I want to restart the experiment, but to target all password
    > forms, as showing suggestions on autofocus makes sense for them too.
    >
    > Change-Id: I3315613448c09627853a441d11be1cc291eb7c55
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5712830
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    > Commit-Queue: Maria Kazinova <kazinova@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1328790}
    
    Change-Id: I14765caa358bbc38146a27760bd917bc9cc81811
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718728
    Reviewed-by: Xinyi Ji <xinyiji@chromium.org>
    Auto-Submit: Xi Han <hanxi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Owners-Override: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328953}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1664,8 +1664,9 @@ extern const char kPasswordGenerationStrongLabelExperimentDescription[];
 extern const char kPasswordManagerShadowDomSupportName[];
 extern const char kPasswordManagerShadowDomSupportDescription[];
 
-extern const char kPasswordManagerShowSuggestionsOnAutofocusName[];
-extern const char kPasswordManagerShowSuggestionsOnAutofocusDescription[];
+extern const char kPasswordManagerShowWebauthnSuggestionsOnAutofocusName[];
+extern const char
+    kPasswordManagerShowWebauthnSuggestionsOnAutofocusDescription[];
 
 extern const char kPasswordManualFallbackAvailableName[];
 extern const char kPasswordManualFallbackAvailableDescription[];
