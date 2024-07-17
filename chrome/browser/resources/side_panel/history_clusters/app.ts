a4db129d2e0342c3019ea3553bcc1f056a3ca932
Anthony Vallée-Dubois <anthonyvd@google.com>
2024-07-17 21:42:13 +0000

    Enable bundling for history clusters side panel
    Locally this was measured as a ~25% speedup to LCP in exchange for a
    ~55kB increase to resources.pak.
    
    Sizes of the uncompressed bundled/minified files:
    240K history_clusters/minified/app.rollup.js
    384K history_clusters/bundled/app.rollup.js
    
    Bug: 349397693
    Change-Id: I904e6cb7cb26dbdb7934f79f15de097c060c9bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5654058
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Commit-Queue: Anthony Vallée-Dubois <anthonyvd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329146}

--- chrome/browser/resources/side_panel/history_clusters/app.ts
+++ chrome/browser/resources/side_panel/history_clusters/app.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '../strings.m.js';
+import './strings.m.js';
 import 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
 import 'chrome://resources/cr_components/history_clusters/clusters.js';
 import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
