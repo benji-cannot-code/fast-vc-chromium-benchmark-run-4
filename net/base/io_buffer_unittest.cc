22accfcdbe825d98c88cd376b63303555e255161
kelsen liu <kelsenliu21@gmail.com>
2024-07-17 06:29:39 +0000

    part1: Add a method returns a span before the `offset()`
    Introduce the `GrowableIOBuffer::span_before_offset` method, which
    returns a span before the offset.
    
    Bug: 329476354
    Change-Id: I09497b2036b9cb313427adc33f313f8531088666
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704618
    Commit-Queue: kelsen liu <kelsenliu21@gmail.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328663}

---
+++ net/base/io_buffer_unittest.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/base/io_buffer.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace net {
+
+namespace {
+
+TEST(GrowableIOBufferTest, SpanBeforeOffset) {
+  scoped_refptr<net::GrowableIOBuffer> buffer =
+      base::MakeRefCounted<net::GrowableIOBuffer>();
+  buffer->SetCapacity(100);
+  EXPECT_EQ(0u, buffer->span_before_offset().size());
+
+  buffer->set_offset(10);
+  EXPECT_EQ(10u, buffer->span_before_offset().size());
+  EXPECT_EQ(buffer->everything().data(), buffer->span_before_offset().data());
+
+  buffer->set_offset(100);
+  EXPECT_EQ(100u, buffer->span_before_offset().size());
+  EXPECT_EQ(buffer->everything().data(), buffer->span_before_offset().data());
+}
+
+}  // anonymous namespace
+
+}  // namespace net
