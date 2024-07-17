5d0c0423f6229fafbc7f0b6e65220e803e1213d7
Christine Ying <chrstne@google.com>
2024-07-17 20:56:00 +0000

    [N&C] Adds re-action histograms, updates notice storage logic.
    New behavior:
    - Re-action emits a histogram and doesn't store prefs
    - If the notice entry DNE do not emit startup histograms
    - use schema constant and hide implementation within notice storage
    
    Change-Id: Id1d6301ca69d0ca6815c557b231a59ec27c121b6
    Bug: b:333408794
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704245
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329110}

--- chrome/browser/privacy_sandbox/privacy_sandbox_notice_service_unittest.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_notice_service_unittest.cc
@@ -54,7 +54,7 @@ TEST_F(PrivacySandboxNoticeServiceTest,
   auto notice_service = PrivacySandboxNoticeService(prefs());
   const auto result = notice_service.GetNoticeStorage()->ReadNoticeData(
       prefs(), GetTopicsNoticeName());
-  EXPECT_EQ(result->schema_version, 0);
+  EXPECT_EQ(result->schema_version, 1);
   EXPECT_EQ(result->notice_action_taken,
             NoticeActionTaken::kUnknownActionPreMigration);
   EXPECT_EQ(base::TimeToValue(result->notice_action_taken_time), "0");
@@ -68,9 +68,7 @@ TEST_F(PrivacySandboxNoticeServiceTest,
   auto notice_service = privacy_sandbox::PrivacySandboxNoticeService(prefs());
   const auto result = notice_service.GetNoticeStorage()->ReadNoticeData(
       prefs(), GetTopicsNoticeName());
-  EXPECT_EQ(result->schema_version, 0);
-  EXPECT_EQ(result->notice_action_taken, NoticeActionTaken::kNotSet);
-  EXPECT_EQ(base::TimeToValue(result->notice_action_taken_time), "0");
+  EXPECT_EQ(result, std::nullopt);
 }
 
 TEST_F(PrivacySandboxNoticeServiceTest,
@@ -84,7 +82,7 @@ TEST_F(PrivacySandboxNoticeServiceTest,
   auto notice_service = PrivacySandboxNoticeService(prefs());
   const auto result = notice_service.GetNoticeStorage()->ReadNoticeData(
       prefs(), GetTopicsNoticeName());
-  EXPECT_EQ(result->schema_version, 0);
+  EXPECT_EQ(result->schema_version, 1);
   EXPECT_EQ(result->notice_action_taken, NoticeActionTaken::kOptIn);
   EXPECT_EQ(result->notice_action_taken_time,
             base::Time::FromMillisecondsSinceUnixEpoch(100));
@@ -101,7 +99,7 @@ TEST_F(PrivacySandboxNoticeServiceTest,
   auto notice_service = PrivacySandboxNoticeService(prefs());
   const auto result = notice_service.GetNoticeStorage()->ReadNoticeData(
       prefs(), GetTopicsNoticeName());
-  EXPECT_EQ(result->schema_version, 0);
+  EXPECT_EQ(result->schema_version, 1);
   EXPECT_EQ(result->notice_action_taken, NoticeActionTaken::kOptOut);
   EXPECT_EQ(result->notice_action_taken_time,
             base::Time::FromMillisecondsSinceUnixEpoch(100));
