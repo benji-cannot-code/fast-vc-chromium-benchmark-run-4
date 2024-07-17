9c9eb455616f7ed184a1af8de32f887069c7ed09
Steven Holte <holte@google.com>
2024-07-17 18:28:19 +0000

    Remove support for InputOptions::safety_interval
    Bug: 352088659
    
    Change-Id: Iccdff8f20d4dd8abe6e36d7feecaacc0b532b631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703799
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329009}

--- ash/webui/recorder_app_ui/resources/platforms/swa/on_device_model.ts
+++ ash/webui/recorder_app_ui/resources/platforms/swa/on_device_model.ts
@@ -121,7 +121,7 @@ export class OnDeviceModel implements Model {
         maxTokens: null,
         tokenOffset: null,
         maxOutputTokens: null,
-        safetyInterval: null,
+        unusedSafetyInterval: null,
         topK: 1,
         temperature: 0,
       },
