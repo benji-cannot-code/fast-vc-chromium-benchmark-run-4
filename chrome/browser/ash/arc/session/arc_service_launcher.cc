cd10244e97eee628546f24032daa4aea3cb13900
Yao Qijing <yaoqq@google.com>
2024-07-17 09:03:16 +0000

    arc: Introduce ArcErrorNotificationBridge
    as ErrorNotificationHost to handle error data from ARC, which creates
    chrome notifications.
    
    BUG=b:351244156
    Test: Manual - checked mojo connection log
    
    Change-Id: I76f9fb4a8b7a41896b1c3a0ffb70cd5bdd7ed7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5680302
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Commit-Queue: Qijing Yao <yaoqq@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328708}

--- chrome/browser/ash/arc/session/arc_service_launcher.cc
+++ chrome/browser/ash/arc/session/arc_service_launcher.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/boot_phase_monitor/arc_boot_phase_monitor_bridge.h"
 #include "chrome/browser/ash/arc/enterprise/arc_enterprise_reporting_service.h"
 #include "chrome/browser/ash/arc/enterprise/cert_store/cert_store_service.h"
+#include "chrome/browser/ash/arc/error_notification/arc_error_notification_bridge.h"
 #include "chrome/browser/ash/arc/file_system_watcher/arc_file_system_watcher_service.h"
 #include "chrome/browser/ash/arc/fileapi/arc_documents_provider_root_map_factory.h"
 #include "chrome/browser/ash/arc/fileapi/arc_file_system_bridge.h"
@@ -258,6 +259,7 @@ void ArcServiceLauncher::OnPrimaryUserProfilePrepared(Profile* profile) {
   ArcDigitalGoodsBridge::GetForBrowserContext(profile);
   ArcDiskSpaceBridge::GetForBrowserContext(profile);
   ArcEnterpriseReportingService::GetForBrowserContext(profile);
+  ArcErrorNotificationBridge::GetForBrowserContext(profile);
   ArcFileSystemBridge::GetForBrowserContext(profile);
   ArcFileSystemMounter::GetForBrowserContext(profile);
   ArcFileSystemWatcherService::GetForBrowserContext(profile);
@@ -452,6 +454,7 @@ void ArcServiceLauncher::EnsureFactoriesBuilt() {
   ArcDiskSpaceBridge::EnsureFactoryBuilt();
   ArcDocumentsProviderRootMapFactory::GetInstance();
   ArcEnterpriseReportingService::EnsureFactoryBuilt();
+  ArcErrorNotificationBridge::EnsureFactoryBuilt();
   ArcFileSystemMounter::EnsureFactoryBuilt();
   ArcFileSystemOperationRunner::EnsureFactoryBuilt();
   ArcFileSystemWatcherService::EnsureFactoryBuilt();
