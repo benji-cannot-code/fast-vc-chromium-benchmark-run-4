84d51b849d381a26640d4b9932c94eebd7a8d85f
Jan Keitel <jkeitel@google.com>
2024-07-17 11:31:46 +0000

    Use profile-aware feature check in PlusAddressService.
    PlusAddressesEnabled will remain a Google Group controlled feature for
    the foreseeable future. To make sure it is only enabled on profiles
    enrolled in the experiment (as opposed to the entire installation),
    this CL starts using profile-aware feature checks.
    
    Bug: 348575889
    Change-Id: I7604c9fe7d432001bbeefbaa20c3311944a3c72c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703901
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328760}

--- chrome/browser/plus_addresses/plus_address_service_factory.cc
+++ chrome/browser/plus_addresses/plus_address_service_factory.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "chrome/browser/affiliations/affiliation_service_factory.h"
+#include "chrome/browser/metrics/variations/google_groups_updater_service_factory.h"
 #include "chrome/browser/plus_addresses/plus_address_setting_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_selections.h"
@@ -20,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/plus_address_http_client_impl.h"
 #include "components/plus_addresses/plus_address_service.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
+#include "components/variations/service/google_groups_updater_service.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 
 // static
@@ -59,6 +61,7 @@ PlusAddressServiceFactory::PlusAddressServiceFactory()
   DependsOn(WebDataServiceFactory::GetInstance());
   DependsOn(AffiliationServiceFactory::GetInstance());
   DependsOn(PlusAddressSettingServiceFactory::GetInstance());
+  DependsOn(GoogleGroupsUpdaterServiceFactory::GetInstance());
 }
 
 PlusAddressServiceFactory::~PlusAddressServiceFactory() = default;
@@ -87,7 +90,13 @@ PlusAddressServiceFactory::BuildServiceInstanceForBrowserContext(
               identity_manager, profile->GetURLLoaderFactory()),
           WebDataServiceFactory::GetPlusAddressWebDataForProfile(
               profile, ServiceAccessType::EXPLICIT_ACCESS),
-          affiliation_service);
+          affiliation_service,
+          /*feature_enabled_for_profile_check=*/
+          base::BindRepeating(
+              &GoogleGroupsUpdaterService::IsFeatureEnabledForProfile,
+              base::Unretained(
+                  GoogleGroupsUpdaterServiceFactory::GetForBrowserContext(
+                      context))));
 
   if (base::FeatureList::IsEnabled(
           plus_addresses::features::kPlusAddressAffiliations)) {
--- components/plus_addresses/fake_plus_address_service.cc
+++ components/plus_addresses/fake_plus_address_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/functional/callback.h"
 #include "components/plus_addresses/mock_plus_address_http_client.h"
 #include "components/plus_addresses/plus_address_types.h"
@@ -24,7 +25,9 @@ FakePlusAddressService::FakePlusAddressService(
           setting_service,
           std::make_unique<testing::NiceMock<MockPlusAddressHttpClient>>(),
           /*webdata_service=*/nullptr,
-          /*affiliation_service=*/&mock_affiliation_service_) {}
+          /*affiliation_service=*/&mock_affiliation_service_,
+          /*feature_enabled_for_profile_check=*/
+          base::BindRepeating(&base::FeatureList::IsEnabled)) {}
 
 FakePlusAddressService::~FakePlusAddressService() = default;
 
