fe9187994b2c7d019de6ae96706a2592aa3c41aa
Hao Liu <haoliuk@chromium.org>
2024-07-17 21:16:55 +0000

    Reland "Flush event timings at visibility change"
    This is a reland of commit e317b68be16b5774b22e4561c94aefbafd30793c
    The revert was due to failures of 2 newly added tests.
    They are disabled.
    
    Original change's description:
    > Flush event timings at visibility change
    >
    > This is to ensure that event timing data are emitted to performance
    > timeline and recorded in the UKM and trace when document unloading happens.
    >
    > We choose the page visibility change time point to report event
    > timings.
    >
    > Current implementation works for main frame.
    >
    > See https://docs.google.com/document/d/1aJcnDSgbKbY_SHDc5FHK4o_PcV8p5C9CS6cmD048DgQ/edit?usp=sharing
    > for details.
    >
    > Change-Id: Ied9673a1be54f4ef5e0fdaf97244f96b69aecb33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5502651
    > Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    > Reviewed-by: Michal Mocny <mmocny@chromium.org>
    > Reviewed-by: Aoyuan Zuo <zuoaoyuan@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Commit-Queue: Hao Liu <haoliuk@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1326363}
    
    Change-Id: I673ca5416c60ea355cf0af78d1217292964585cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704556
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Hao Liu <haoliuk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329130}

--- third_party/blink/renderer/core/timing/responsiveness_metrics.h
+++ third_party/blink/renderer/core/timing/responsiveness_metrics.h
@@ -148,6 +148,8 @@ class CORE_EXPORT ResponsivenessMetrics
   explicit ResponsivenessMetrics(WindowPerformance*);
   ~ResponsivenessMetrics();
 
+  void FlushAllEventsAtPageHidden();
+
   // Flush UKM timestamps of composition events for testing.
   void FlushAllEventsForTesting();
 
