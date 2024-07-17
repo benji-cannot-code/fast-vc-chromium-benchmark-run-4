99774f3f082a79f54079f491d8d05003cefb6bc2
Yao Qijing <yaoqq@google.com>
2024-07-17 09:03:28 +0000

    arc: Implement send error details for project friendly
    Creates chrome notification when error details are sent from Android,
    and pass Android the button click action by the user.
    
    BUG=b:332459217
    Test=manual
    
    Change-Id: I495109b2f168a4df6fd9b5286c3ab487686038cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5682682
    Reviewed-by: Shengsong Tan <sstan@chromium.org>
    Commit-Queue: Qijing Yao <yaoqq@google.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328709}

--- chrome/browser/ash/arc/error_notification/arc_error_notification_bridge.h
+++ chrome/browser/ash/arc/error_notification/arc_error_notification_bridge.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "mojo/public/cpp/bindings/remote.h"
 
+class Profile;
+
 namespace arc {
 
 class ArcBridgeService;
@@ -46,9 +48,14 @@ class ArcErrorNotificationBridge
       SendErrorDetailsCallback callback) override;
 
  private:
+  std::string GenerateNotificationId(mojom::ErrorType type,
+                                     const std::string& app_name);
+
   friend class ArcErrorNotificationBridgeFactory;
 
   raw_ptr<ArcBridgeService> arc_bridge_service_;
+
+  raw_ptr<Profile> profile_;
 
   base::WeakPtrFactory<ArcErrorNotificationBridge> weak_ptr_factory_{this};
 };
