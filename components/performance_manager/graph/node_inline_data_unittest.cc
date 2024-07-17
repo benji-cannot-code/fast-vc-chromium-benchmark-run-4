6cf9177ceeef6df6002c816f08c12a6cbdcbdc1e
Patrick Monette <pmonette@chromium.org>
2024-07-17 20:01:49 +0000

    NodeInlineData: Add static_asserts for IsSparseNodeInlineData
    Change-Id: I6a7cbb377bc6e93f90b63c9329779e1573d50bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718376
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329061}

--- components/performance_manager/graph/node_inline_data_unittest.cc
+++ components/performance_manager/graph/node_inline_data_unittest.cc
@@ -36,6 +36,10 @@ struct SparseDataWithExplicitCtor
   T value;
 };
 
+// Tests internal::IsSparseNodeInlineData.
+static_assert(!internal::IsSparseNodeInlineData<DataWithDefaultCtor<int>>);
+static_assert(internal::IsSparseNodeInlineData<SparseDataWithDefaultCtor<int>>);
+
 template <class... Args>
 class DummyTestSupportsNodeInlineData : public SupportsNodeInlineData<Args...> {
  public:
