588468ba1cff8d6728a0684ff6a9f32edfdd2657
Salvador Guerrero <salg@google.com>
2024-07-17 17:07:29 +0000

    [Adaptive Toolbar] Add new JNI method to get ranked buttons from model
    On tablets not all adaptive toolbar buttons are available (e.g. new tab), currently we don't show anything when one of those buttons are selected by the model, resulting on wasted space. This is partly
    because the JNI method that gets the model's results only returns the
    top button.
    
    This CL adds a method a list of all buttons sorted by ranking, so if
    the first option isn't available we can try the next ones.
    
    A follow-up CL will call this method from
    AdaptiveToolbarStatePredictor and remove the old method.
    
    added tests to the native side.
    
    Low-Coverage-Reason: HARD_TO_TEST Java code is mostly JNI boilerplate,
    Bug: 353311506
    Change-Id: I6d436a9733ddf0b848511347b41014a6014571e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5651219
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328961}

---
+++ chrome/browser/ui/android/toolbar/adaptive_toolbar_bridge.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_ANDROID_TOOLBAR_ADAPTIVE_TOOLBAR_BRIDGE_H_
+#define CHROME_BROWSER_UI_ANDROID_TOOLBAR_ADAPTIVE_TOOLBAR_BRIDGE_H_
+
+#include <string>
+
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/android/toolbar/adaptive_toolbar_enums.h"
+
+namespace adaptive_toolbar {
+
+void GetRankedSessionVariantButtons(
+    Profile* profile,
+    bool use_raw_results,
+    base::OnceCallback<void(bool, std::vector<int>)> callback);
+
+}  // namespace adaptive_toolbar
+
+#endif  // CHROME_BROWSER_UI_ANDROID_TOOLBAR_ADAPTIVE_TOOLBAR_BRIDGE_H_
