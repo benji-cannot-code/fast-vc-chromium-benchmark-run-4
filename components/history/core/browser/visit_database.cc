7472ced3d29175647776b5d3e65859d5f6f60324
Alex Gough <ajgo@chromium.org>
2024-07-17 23:44:02 +0000

    Turn iterator _!=_->end() DCHECK() into CHECK() for components/history
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130 unless in testonly code.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _->end());' \
          -p 'DCHECK(_->end() != _);' \
          -p 'DCHECK_NE(_, _->end());' \
          -p 'DCHECK_NE(_->end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=romanarora@chromium.org
    
    Bug: 351745839
    Change-Id: I476de0bb661b7b9589ae9edbffe0d06abc3e628c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719488
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329222}

--- components/history/core/browser/visit_database.cc
+++ components/history/core/browser/visit_database.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/logging.h"
+#include "base/not_fatal_until.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
 #include "components/google/core/common/google_util.h"
@@ -330,7 +331,7 @@ bool VisitDatabase::FillVisitVectorWithOptions(sql::Statement& statement,
         if (!ov.app_id && visit.app_id) {
           auto is_matched = [ov](VisitRow v) { return ov.url_id == v.url_id; };
           auto pos = std::find_if(visits->begin(), visits->end(), is_matched);
-          DCHECK(pos != visits->end());
+          CHECK(pos != visits->end(), base::NotFatalUntil::M130);
           *pos = visit;
           found_urls[visit.url_id] = visit;
         }
