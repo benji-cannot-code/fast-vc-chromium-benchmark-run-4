727bdff76eeaa76f6c30a01958e80f9360b0ae26
= <alibu@google.com>
2024-07-17 18:18:32 +0000

    Create hyperlink context menu in WebView
    The context menu is behind a feature flag, and is disabled by default.
    
    Design doc: https://docs.google.com/document/d/1e3oMJvGEFPoGc42I_i0eYdWFVrZAuCy-jIvJbc9ctvA/edit?tab=t.0#heading=h.17wg41voij6q
    
    Bug: b/323344356
    Change-Id: I87064d87d97990e3466a8f4354c398fa3891d287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703697
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Ali Butt <alibu@google.com>
    Commit-Queue: Ali Butt <alibu@google.com>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329000}

---
+++ android_webview/java/src/org/chromium/android_webview/contextmenu/AwContextMenuItemProperties.java
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.contextmenu;
+
+import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
+
+class AwContextMenuItemProperties {
+    public static final WritableObjectPropertyKey<CharSequence> TEXT =
+            new WritableObjectPropertyKey<>();
+    public static final WritableIntPropertyKey MENU_ID = new WritableIntPropertyKey();
+
+    public static final PropertyKey[] ALL_KEYS = {TEXT, MENU_ID};
+}
