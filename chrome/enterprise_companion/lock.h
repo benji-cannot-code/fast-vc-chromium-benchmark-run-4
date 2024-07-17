b48e668b5496951be4ec46dc53de2927d43583fc
Noah Rose Ledesma <noahrose@google.com>
2024-07-17 14:24:59 +0000

    Create AppInstall
    Create an App and configure a command-line switch to install CECA.
    
    Bug: 344878963
    Change-Id: I9cbeafcec03e77eb6a4cd2e0e8101a456460bb0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5714655
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328843}

--- chrome/enterprise_companion/lock.h
+++ chrome/enterprise_companion/lock.h
@@ -8,15 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/time/time.h"
 #include "components/named_system_lock/lock.h"
 
 namespace enterprise_companion {
 
 using ScopedLock = ::named_system_lock::ScopedLock;
 
-// Returns a ScopedLock, or nullptr if the lock could not be acquired. While the
-// ScopedLock exists, no other process on the machine may acquire that lock.
-std::unique_ptr<ScopedLock> CreateScopedLock();
+// Returns a ScopedLock, or nullptr if the lock could not be acquired before
+// `timeout`. While the ScopedLock exists, no other process on the machine may
+// acquire that lock.
+std::unique_ptr<ScopedLock> CreateScopedLock(
+    base::TimeDelta timeout = base::Seconds(0));
 
 }  // namespace enterprise_companion
 
