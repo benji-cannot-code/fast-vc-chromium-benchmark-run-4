17db2b858cb14987fce0de2199e17e3372b2f5cc
Gaurav Kumar <gauravkum@microsoft.com>
2024-07-17 05:35:34 +0000

    [view-ax] Introduce setters for kTextSelStart and kTextSelEnd attributes
    This CL introduces the ViewAccessibility setter for
    kTextSelStart and kTextSelEnd state.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I1cc244a8f47b10f6d5f9dd1e088cfb19d9ddf54e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667883
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1328644}

--- ui/views/accessibility/view_accessibility.cc
+++ ui/views/accessibility/view_accessibility.cc
@@ -1148,4 +1148,12 @@ void ViewAccessibility::RemoveExpandCollapseState() {
   SetState(ax::mojom::State::kCollapsed, false);
 }
 
+void ViewAccessibility::SetTextSelStart(int32_t text_sel_start) {
+  data_.AddIntAttribute(ax::mojom::IntAttribute::kTextSelStart, text_sel_start);
+}
+
+void ViewAccessibility::SetTextSelEnd(int32_t text_sel_end) {
+  data_.AddIntAttribute(ax::mojom::IntAttribute::kTextSelEnd, text_sel_end);
+}
+
 }  // namespace views
