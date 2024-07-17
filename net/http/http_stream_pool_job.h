b9ce41bb33092d424f84ac9ef2223ce97526517e
Kenichi Ishibashi <bashi@chromium.org>
2024-07-17 05:41:22 +0000

    Close an idle stream in HttpStreamPool::Job::MaybeAttemptConnection()
    We should close an idle stream when the per-pool limit is reached but
    the per-group limit is not reached. This behavior is similar to
    TransportClientSocketPool::RequestSocketInternal().
    
    Bug: 346835898
    Change-Id: I3fba2c08d1cd10e938065e9d029f1ad73fd4e6d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5700106
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328648}

--- net/http/http_stream_pool_job.h
+++ net/http/http_stream_pool_job.h
@@ -118,10 +118,6 @@ class HttpStreamPool::Job
   // Returns the current load state.
   LoadState GetLoadState() const;
 
-  // Returns true if we can't make any connection attempts due to per-pool or
-  // per-group limits.
-  bool ReachedMaxStreamLimit() const;
-
   void ResolveServiceEndpoint(RequestPriority initial_priority);
 
   void MaybeChangeServiceEndpointRequestPriority();
