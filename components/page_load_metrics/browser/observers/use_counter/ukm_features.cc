2fbc66dedd72010888ec236f7873986b29c5c5d2
Peter Kotwicz <pkotwicz@chromium.org>
2024-07-17 16:44:59 +0000

    Add UKM for user counter for digital credentials API
    Adding UKM should be OK because according to UMA the feature currently
    gets minimal usage.
    
    BUG=329133405
    
    Change-Id: I18cb3381718d0b6720ed4634b4242a81ac39be3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689977
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328935}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -402,6 +402,8 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::
               kSharedStorageAPI_CreateWorklet_CrossOriginScriptDefaultDataOrigin,
           WebFeature::kSharedStorageAPI_AddModule_CrossOriginScript,
+          WebFeature::kIdentityDigitalCredentials,
+          WebFeature::kIdentityDigitalCredentialsDeepLink,
       }));
   return *opt_in_features;
 }
