3b1096bdc1a9813def2fd5bb4dfe3a5510f7fd2d
Kaylee Lubick <kjlubick@google.com>
2024-07-17 15:11:36 +0000

    Use non-legacy Metal Graphite functions
    In the chain of CLs starting at http://review.skia.org/876660
    Skia deprecated the constructors for Graphite's TextureInfo
    and BackendTexture which directly took in Metal constructs and
    created some replacements. This updates Chromium to use those
    new APIs.
    
    This also removes some old #defines guarding legacy code which
    had been removed from Skia and were no-ops.
    
    Bug: b:294543706
    Change-Id: I7bdcf52d1a182cc47432f9e7d2bd578ac77232b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713460
    Commit-Queue: Kaylee Lubick <kjlubick@chromium.org>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Auto-Submit: Kaylee Lubick <kjlubick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328883}

--- gpu/command_buffer/service/shared_image/shared_image_format_service_utils_mac.mm
+++ gpu/command_buffer/service/shared_image/shared_image_format_service_utils_mac.mm
@@ -151,7 +151,7 @@ skgpu::graphite::TextureInfo GraphiteMetalTextureInfo(
 #endif
   mtl_texture_info.fMipmapped =
       mipmapped ? skgpu::Mipmapped::kYes : skgpu::Mipmapped::kNo;
-  return mtl_texture_info;
+  return skgpu::graphite::TextureInfos::MakeMetal(mtl_texture_info);
 }
 #endif
 
