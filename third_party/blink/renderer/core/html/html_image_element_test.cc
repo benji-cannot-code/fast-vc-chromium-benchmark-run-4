990082e598719754fee54ac89f69e36fa9f4db1a
Nidhi Jaju <nidhijaju@chromium.org>
2024-07-17 13:02:09 +0000

    Disable SimplifyLoadingTransparentPlaceholderImage feature in parameterized tests
    This CL explicitly disables the SimplifyLoadingTransparentPlaceholderImage
    feature when GetParam() is false in TransparentPlaceholderImageSimTest
    so we do not just assume that the feature is disabled by default.
    
    Bug: 41496436
    Change-Id: I69e8b944dcf6c175afe068c86b756e0a5f92a661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716211
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1328804}

--- third_party/blink/renderer/core/html/html_image_element_test.cc
+++ third_party/blink/renderer/core/html/html_image_element_test.cc
@@ -252,6 +252,9 @@ class TransparentPlaceholderImageSimTest
     if (GetParam()) {
       feature_list_.InitAndEnableFeature(
           features::kSimplifyLoadingTransparentPlaceholderImage);
+    } else {
+      feature_list_.InitAndDisableFeature(
+          features::kSimplifyLoadingTransparentPlaceholderImage);
     }
   }
 
