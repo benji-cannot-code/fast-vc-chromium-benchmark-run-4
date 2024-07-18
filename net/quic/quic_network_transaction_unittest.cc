a312f38fa9ec0abf003c3c968043d05328a1f6c9
Dustin J. Mitchell <djmitche@chromium.org>
2024-07-18 14:31:58 +0000

    Replace MakeAckAndConnectionClosePacket with equivalent frame calls
    Change-Id: I1df0b7ca521a733a756cf7a055198a40915f0f54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5636583
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329524}

--- net/quic/quic_network_transaction_unittest.cc
+++ net/quic/quic_network_transaction_unittest.cc
@@ -426,9 +426,10 @@ class QuicNetworkTransactionTest
       quic::QuicErrorCode quic_error,
       const std::string& quic_error_details,
       uint64_t frame_type) {
-    return client_maker_->MakeAckAndConnectionClosePacket(
-        num, largest_received, smallest_received, quic_error,
-        quic_error_details, frame_type);
+    return client_maker_->Packet(num)
+        .AddAckFrame(/*first_received=*/1, largest_received, smallest_received)
+        .AddConnectionCloseFrame(quic_error, quic_error_details, frame_type)
+        .Build();
   }
 
   std::unique_ptr<quic::QuicEncryptedPacket> ConstructServerRstPacket(
@@ -5943,8 +5944,12 @@ TEST_P(QuicNetworkTransactionTest, RetryAfterAsyncNoBufferSpace) {
   socket_data.AddRead(SYNCHRONOUS, ERR_IO_PENDING);  // No more data to read
   socket_data.AddWrite(
       SYNCHRONOUS,
-      client_maker_->MakeAckAndConnectionClosePacket(
-          packet_num++, 2, 1, quic::QUIC_CONNECTION_CANCELLED, "net error", 0));
+      client_maker_->Packet(packet_num++)
+          .AddAckFrame(/*first_received=*/1, /*largest_received=*/2,
+                       /*smallest_received=*/1)
+          .AddConnectionCloseFrame(quic::QUIC_CONNECTION_CANCELLED, "net error",
+                                   0)
+          .Build());
 
   socket_data.AddSocketDataToFactory(&socket_factory_);
 
@@ -5981,8 +5986,12 @@ TEST_P(QuicNetworkTransactionTest, RetryAfterSynchronousNoBufferSpace) {
   socket_data.AddRead(SYNCHRONOUS, ERR_IO_PENDING);  // No more data to read
   socket_data.AddWrite(
       SYNCHRONOUS,
-      client_maker_->MakeAckAndConnectionClosePacket(
-          packet_num++, 2, 1, quic::QUIC_CONNECTION_CANCELLED, "net error", 0));
+      client_maker_->Packet(packet_num++)
+          .AddAckFrame(/*first_received=*/1, /*largest_received=*/2,
+                       /*smallest_received=*/1)
+          .AddConnectionCloseFrame(quic::QUIC_CONNECTION_CANCELLED, "net error",
+                                   0)
+          .Build());
 
   socket_data.AddSocketDataToFactory(&socket_factory_);
 
