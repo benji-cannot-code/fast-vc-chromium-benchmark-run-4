66208e2d8cdc68fc371ca23ab248d390c6d6bdff
Theo Cristea <theocristea@google.com>
2024-07-18 14:09:07 +0000

    [Gardening] Disable flaky TabSwitcherLayoutTest tests
    Bug: 353946452
    Change-Id: Id134063212f9eaa0db7a30903cd888cc1f2cca0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721390
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329503}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherLayoutTest.java
@@ -522,6 +522,7 @@ public class TabSwitcherLayoutTest {
         ChromeFeatureList.TAB_GROUP_PARITY_ANDROID,
         ChromeFeatureList.TAB_GROUP_PANE_ANDROID
     })
+    @DisabledTest(message = "crbug.com/353946452")
     public void testTabGroupOverflowMenuInTabSwitcher_closeGroup() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         SnackbarManager snackbarManager = cta.getSnackbarManager();
@@ -703,6 +704,7 @@ public class TabSwitcherLayoutTest {
         ChromeFeatureList.TAB_GROUP_PARITY_ANDROID,
         ChromeFeatureList.TAB_GROUP_PANE_ANDROID,
     })
+    @DisabledTest(message = "crbug.com/353946452")
     public void testTabGroupOverflowMenuInTabSwitcher_ungroupDecline() {
         final ChromeTabbedActivity cta = mActivityTestRule.getActivity();
         createTabs(cta, false, 2);
