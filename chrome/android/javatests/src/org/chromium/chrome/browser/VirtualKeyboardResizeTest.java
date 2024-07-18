ee6c349f576a4f8f6b02008202861fa28b98bf94
Theo Cristea <theocristea@google.com>
2024-07-18 14:17:11 +0000

    [Gardning] Disable flaky VirtualKeyboardResizeTest
    Bug: 353947757
    Change-Id: I2de0fa9253e9e4c9a5234a93d11ec21d17fc04d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5720930
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329511}

--- chrome/android/javatests/src/org/chromium/chrome/browser/VirtualKeyboardResizeTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/VirtualKeyboardResizeTest.java
@@ -287,6 +287,7 @@ public class VirtualKeyboardResizeTest {
     @Test
     @MediumTest
     @CommandLineFlags.Add({"policy={\"VirtualKeyboardResizesLayoutByDefault\":true}"})
+    @DisabledTest(message = "crbug.com/353947757")
     public void testVirtualKeyboardDefaultResizeModeWithPolicy() throws Throwable {
         startMainActivityWithURL("/chrome/test/data/android/page_with_editable.html");
 
