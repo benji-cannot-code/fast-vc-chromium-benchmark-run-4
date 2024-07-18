6c3a52638bcd94a01b6bd9998f64a1a19f045cd0
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-18 18:37:31 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to sandbox/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 351564777
    Change-Id: I817a47d5ac73b416627fcf67490649ee7b7c43dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717754
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329709}

--- sandbox/linux/suid/common/suid_unsafe_environment_variables.h
+++ sandbox/linux/suid/common/suid_unsafe_environment_variables.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // This is a list of environment variables which the ELF loader unsets when
 // loading a SUID binary. Because they are unset rather than just ignored, they
 // aren't passed to child processes of SUID processes either.
--- sandbox/win/src/interception.cc
+++ sandbox/win/src/interception.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // For information about interceptions as a whole see
 // http://dev.chromium.org/developers/design-documents/sandbox .
 
