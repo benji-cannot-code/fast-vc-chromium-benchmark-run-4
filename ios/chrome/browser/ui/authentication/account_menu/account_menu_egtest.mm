7c953b8a8a4620a7d0ad48a214e4da6e2d1ba24d
Jood Hajeer <jood@google.com>
2024-07-18 15:00:55 +0000

    [iOS] Limit identity confirmation snackbar frequency
    One condition that limits the frequency of showing the identity
    confirmation snackbar is to Not show it at start-up for X (TBD) days
    after it was last prompted.
    
    The X days are still TBD in this CL I added a placeholder of 14 days.
    Will update later once the requirement is confirmed.
    
    For now, this CL introduces a pref to track the last Time the snackbar
    was prompted along with a condition that reads this value.
    
    Bug: 336720134
    Change-Id: Ic9ef839dee5d725daaae8f75024d87815a8388df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5712834
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329547}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_egtest.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_egtest.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/bookmarks/ui_bundled/bookmark_earl_grey.h"
+#import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/ui/authentication/account_menu/account_menu_constants.h"
 #import "ios/chrome/browser/ui/authentication/signin_earl_grey.h"
@@ -36,6 +37,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   config.features_enabled.push_back(kIdentityDiscAccountMenu);
 
   return config;
+}
+
+- (void)tearDown {
+  base::TimeDelta marginToAllowIdentityConfirmationSnackbar = base::Days(20);
+  [ChromeEarlGrey
+      setTimeValue:base::Time::FromDeltaSinceWindowsEpoch(
+                       marginToAllowIdentityConfirmationSnackbar)
+       forUserPref:prefs::kIdentityConfirmationSnackbarLastPromptTime];
+  [super tearDown];
 }
 
 - (void)testViewAccountMenu {
@@ -124,6 +134,43 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSString* snackbarMessage = l10n_util::GetNSStringF(
       IDS_IOS_ACCOUNT_MENU_SWITCH_CONFIRMATION_TITLE,
       base::SysNSStringToUTF16(fakeIdentity.userGivenName));
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(grey_text(snackbarMessage),
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_nil()];
+}
+
+// Verifies identity confirmation snackbar shows on startup with multiple
+// identities on device with frequency limitations.
+- (void)testFrequencyLimitation_IdentityConfirmationToast {
+  // Add multiple identities and sign in with one of them.
+  FakeSystemIdentity* fakeIdentity = [FakeSystemIdentity fakeIdentity1];
+  [SigninEarlGrey signinWithFakeIdentity:fakeIdentity];
+  FakeSystemIdentity* secondaryIdentity = [FakeSystemIdentity fakeIdentity2];
+  [SigninEarlGrey addFakeIdentity:secondaryIdentity];
+
+  // Background then foreground the app.
+  [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
+
+  // Confirm the snackbar shows.
+  NSString* snackbarMessage = l10n_util::GetNSStringF(
+      IDS_IOS_ACCOUNT_MENU_SWITCH_CONFIRMATION_TITLE,
+      base::SysNSStringToUTF16(fakeIdentity.userGivenName));
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(grey_text(snackbarMessage),
+                                          grey_sufficientlyVisible(), nil)]
+      assertWithMatcher:grey_sufficientlyVisible()];
+
+  // Dismiss the snackabr.
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(grey_text(snackbarMessage),
+                                          grey_sufficientlyVisible(), nil)]
+      performAction:grey_tap()];
+
+  // Background then foreground the app again.
+  [[AppLaunchManager sharedManager] backgroundAndForegroundApp];
+
+  // Confirm the snackbar does not show.
   [[EarlGrey
       selectElementWithMatcher:grey_allOf(grey_text(snackbarMessage),
                                           grey_sufficientlyVisible(), nil)]
