b306586408ea7310026d1b465114e92de682605b
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-18 17:25:19 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to skia/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 351564777
    Change-Id: I95a6e9c385a3c3f598359c86880fd6e454e605a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717734
    Reviewed-by: Brian Osman <brianosman@google.com>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Brian Osman <brianosman@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329660}

--- skia/ext/convolver_neon.cc
+++ skia/ext/convolver_neon.cc
@@ -1,4 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
 /*
  * Copyright 2016 Google Inc.
  *
--- skia/ext/image_operations_bench.cc
+++ skia/ext/image_operations_bench.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // This small program is used to measure the performance of the various
 // resize algorithms offered by the ImageOperations::Resize function.
 // It will generate an empty source bitmap, and rescale it to specified
