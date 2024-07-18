41364b27c7b7bcee2d09d0f6ea5e98dc47da8271
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2024-07-18 05:09:57 +0000

    arc: Enable ArcEnablePvclock by default
    As discussed in b/352211958, enable this by default with a reverse
    Finch experiment planned for stats monitoring.
    
    BUG=b:352212164
    TEST="virtio-pvclock is enabled for this vm" in /var/log/messages with feature enabled on Chrome side
    TEST=arc.Suspend.s10c2 with pvclock feature enabled
    TEST=arc.Suspend.s10c2 with pvclock feature disabled
    
    Change-Id: I3115bb89fdb1df93e768dddbdb6772990c7ec1a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716389
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329322}

--- ash/components/arc/arc_features.cc
+++ ash/components/arc/arc_features.cc
@@ -266,7 +266,7 @@ const base::FeatureParam<int> kVirtualSwapIntervalMs{
 // Controls whether to enable virtio-pvclock in ARCVM
 BASE_FEATURE(kArcVmPvclock,
              "ArcEnablePvclock",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Controls whether enable ignoring hover event ANR in input dispatcher.
 BASE_FEATURE(kIgnoreHoverEventAnr,
