debe689c314ef2512c81f5be3770efca16cbbe55
Arthur Milchior <arthurmilchior@chromium.org>
2024-07-18 12:17:13 +0000

    [iOS][Account menu] Compact width ipad’s sheet have correct height
    On compact width, the account menu, which is supposed to be a popover,
    becomes a sheet. The problem is that this sheet does not behave as
    the automatic sheet. The main differences are:
    * `popoverPresentationController.adaptiveSheetPresentationController`
    must be used to access the presentation controller,
    * `self.popoverPresentationController` is not nil but its methods are
    ignored. Instead, one must use
    `self.navigationController.popoverPresentationController`,
    * the detents should be set before the view being presented, otherwise
    it’s ignored.
    
    Result can be seen on
    https://drive.google.com/file/d/1dZMpQ1IjZdY0WRgLbABiaxrUG83CPwpz/view?usp=drive_link
    
    Fixed: 353703657
    Change-Id: I35ecf07a037d3b2ed214e0a9796d8f4e098299bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713615
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329452}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
@@ -114,6 +114,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _mediator.consumer = _viewController;
   _viewController.mutator = _mediator;
   _viewController.dataSource = _mediator;
+  [_viewController setUpBottomSheetPresentationController];
   [self.baseViewController presentViewController:_navigationController
                                         animated:YES
                                       completion:nil];
