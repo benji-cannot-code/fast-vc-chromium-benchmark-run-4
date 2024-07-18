5b8c1e9954a1e648cf8dfe7bf04dcc5c81b8193d
kelsen liu <kelsenliu21@gmail.com>
2024-07-18 03:02:59 +0000

    part4: Remove usages of `GrowableIOBuffer::StartOfBuffer`
    This CL removes usages of the `GrowableIOBuffer::StartOfBuffer` method
    and replaces unchecked pointers with `span`.
    
    Bug: 329476354
    Change-Id: I1e3b792d7c5aa1bc2e6a32eb90761033b4e79688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5704619
    Commit-Queue: kelsen liu <kelsenliu21@gmail.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329297}

--- components/media_router/common/providers/cast/channel/cast_framer_unittest.cc
+++ components/media_router/common/providers/cast/channel/cast_framer_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "base/containers/span.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/openscreen/src/cast/common/channel/proto/cast_channel.pb.h"
 
@@ -38,7 +39,7 @@ class CastFramerTest : public testing::Test {
   }
 
   void WriteToBuffer(const std::string& data) {
-    memcpy(buffer_->StartOfBuffer(), data.data(), data.size());
+    buffer_->everything().copy_prefix_from(base::as_byte_span(data));
   }
 
  protected:
