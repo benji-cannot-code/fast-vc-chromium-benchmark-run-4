736c138d6c00466507e50a4686d364b157456313
Jérôme Lebel <jlebel@chromium.org>
2024-07-18 19:18:21 +0000

    [iOS][TrustedVault] Remove WithCancelCallback suffix
    And this patch removes temporary `WithCancelCallback` suffix to
    * Reauthentication()
    * FixDegradedRecoverability()
    
    - crrev.com/c/5701265 Adds WithCancelCallback suffix
    - crrev.com/i/7488451 Removes in internal the suffix
    > crrev.com/c/5709050 Removes the suffix in the base class
    
    Bug: 343007092
    Change-Id: I90f1aeb028cc819946a80156bb5053f77dcf04cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709050
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329740}

--- ios/chrome/browser/signin/model/trusted_vault_client_backend.mm
+++ ios/chrome/browser/signin/model/trusted_vault_client_backend.mm
@@ -5,46 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/signin/model/trusted_vault_client_backend.h"
 
-#import "base/functional/callback.h"
-
 TrustedVaultClientBackend::TrustedVaultClientBackend() = default;
 
 TrustedVaultClientBackend::~TrustedVaultClientBackend() = default;
-
-TrustedVaultClientBackend::CancelDialogCallback
-TrustedVaultClientBackend::Reauthentication(
-    id<SystemIdentity> identity,
-    const std::string& security_domain_path,
-    UIViewController* presenting_view_controller,
-    CompletionBlock completion) {
-  return ReauthenticationWithCancelCallback(
-      identity, security_domain_path, presenting_view_controller, completion);
-}
-
-TrustedVaultClientBackend::CancelDialogCallback
-TrustedVaultClientBackend::ReauthenticationWithCancelCallback(
-    id<SystemIdentity> identity,
-    const std::string& security_domain_path,
-    UIViewController* presenting_view_controller,
-    CompletionBlock completion) {
-  NOTREACHED_NORETURN();
-}
-
-TrustedVaultClientBackend::CancelDialogCallback
-TrustedVaultClientBackend::FixDegradedRecoverability(
-    id<SystemIdentity> identity,
-    const std::string& security_domain_path,
-    UIViewController* presenting_view_controller,
-    CompletionBlock completion) {
-  return FixDegradedRecoverabilityWithCancelCallback(
-      identity, security_domain_path, presenting_view_controller, completion);
-}
-
-TrustedVaultClientBackend::CancelDialogCallback
-TrustedVaultClientBackend::FixDegradedRecoverabilityWithCancelCallback(
-    id<SystemIdentity> identity,
-    const std::string& security_domain_path,
-    UIViewController* presenting_view_controller,
-    CompletionBlock completion) {
-  NOTREACHED_NORETURN();
-}
