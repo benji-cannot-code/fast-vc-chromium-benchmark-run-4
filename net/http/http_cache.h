f7be4a828dfa53c4196259ddbb943b2189b5bb0d
kelsen liu <kelsenliu21@gmail.com>
2024-07-18 03:09:20 +0000

    part5: Remove usages of `GrowableIOBuffer::StartOfBuffer`
    This CL removes usages of the `GrowableIOBuffer::StartOfBuffer` method
    and replaces unchecked pointers with `span`. Additionally
    `HttpCache::ParseResponseInfo` now consumes a `span` instead of a
    ptr+size pair.
    
    Bug: 329476354
    Change-Id: Ib2446a3a52c16ad246fef787a3dfb143f3ff209e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5706188
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: kelsen liu <kelsenliu21@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1329299}

--- net/http/http_cache.h
+++ net/http/http_cache.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unordered_set>
 
 #include "base/containers/lru_cache.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/gtest_prod_util.h"
@@ -190,8 +191,7 @@ class NET_EXPORT HttpCache : public HttpTransactionFactory {
   disk_cache::Backend* GetCurrentBackend() const;
 
   // Given a header data blob, convert it to a response info object.
-  static bool ParseResponseInfo(const char* data,
-                                int len,
+  static bool ParseResponseInfo(base::span<const uint8_t> data,
                                 HttpResponseInfo* response_info,
                                 bool* response_truncated);
 
--- net/websockets/websocket_basic_stream.cc
+++ net/websockets/websocket_basic_stream.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/logging.h"
@@ -293,9 +294,10 @@ int WebSocketBasicStream::ReadEverything(
     DCHECK_GE(http_read_buffer_->offset(), 0);
     is_http_read_buffer_decoded_ = true;
     std::vector<std::unique_ptr<WebSocketFrameChunk>> frame_chunks;
-    if (!parser_.Decode(http_read_buffer_->StartOfBuffer(),
-                        http_read_buffer_->offset(), &frame_chunks))
+    auto data = base::as_chars(http_read_buffer_->span_before_offset());
+    if (!parser_.Decode(data.data(), data.size(), &frame_chunks)) {
       return WebSocketErrorToNetError(parser_.websocket_error());
+    }
     if (!frame_chunks.empty()) {
       int result = ConvertChunksToFrames(&frame_chunks, frames);
       if (result != ERR_IO_PENDING)
