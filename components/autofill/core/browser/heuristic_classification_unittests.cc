b8f2067d66d366b7753a42ee73200ac3609331a4
Norge Vizcay <vizcay@google.com>
2024-07-18 10:36:06 +0000

    Include AU model in heuristic tests.
    Bug: 41489892
    Change-Id: Id3e51c15e958095bb29c8cd2ee877d758af5f630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718233
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Norge Vizcay <vizcay@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329419}

--- components/autofill/core/browser/heuristic_classification_unittests.cc
+++ components/autofill/core/browser/heuristic_classification_unittests.cc
@@ -547,6 +547,7 @@ TEST_P(HeuristicClassificationTests, EndToEnd) {
   std::vector<base::test::FeatureRef> enabled_features = {
       // Support for new field types.
       features::kAutofillUseI18nAddressModel,
+      features::kAutofillUseAUAddressModel,
       features::kAutofillUseBRAddressModel,
       features::kAutofillUseCAAddressModel,
       features::kAutofillUseDEAddressModel,
