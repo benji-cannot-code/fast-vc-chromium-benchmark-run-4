10d4ea70869c73025669f52203ab8108ef19cf69
Sandor Major <sandormajor@google.com>
2024-07-18 16:20:48 +0000

    Make ContentSettingConstraints use the clock of HostContentSettingsMap
    Bug: b/338367663
    Change-Id: I5d491f256f96053963c309bb89160532d216433a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716855
    Auto-Submit: Sandor «Alex» Major <sandormajor@google.com>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329602}

--- chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
+++ chrome/browser/storage_access_api/storage_access_grant_permission_context.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
+#include "base/time/time.h"
 #include "chrome/browser/content_settings/cookie_settings_factory.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/dips/dips_service.h"
@@ -135,8 +136,9 @@ void RecordOutcomeSample(RequestOutcome outcome) {
 }
 
 content_settings::ContentSettingConstraints ComputeConstraints(
-    RequestOutcome outcome) {
-  content_settings::ContentSettingConstraints constraints;
+    RequestOutcome outcome,
+    base::Time now) {
+  content_settings::ContentSettingConstraints constraints(now);
   switch (outcome) {
     case RequestOutcome::kGrantedByFirstPartySet:
       constraints.set_lifetime(
@@ -636,7 +638,7 @@ void StorageAccessGrantPermissionContext::NotifyPermissionSetInternal(
 
   settings_map->SetContentSettingDefaultScope(
       requesting_origin, embedding_origin, ContentSettingsType::STORAGE_ACCESS,
-      content_setting, ComputeConstraints(outcome));
+      content_setting, ComputeConstraints(outcome, settings_map->Now()));
 
   ContentSettingsForOneType grants =
       settings_map->GetSettingsForOneType(ContentSettingsType::STORAGE_ACCESS);
