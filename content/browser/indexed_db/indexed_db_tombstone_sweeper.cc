156f5d74e5b43e7b8cca1d7c76b229a64cb2dc0a
Alex Gough <ajgo@chromium.org>
2024-07-18 16:52:34 +0000

    Turn iterator _!=_->end() DCHECK() into CHECK() for content/browser
    Accessing an invalid iterator can sometimes be a security
    issue and these checks are cheap, so upgrade to CHECKs.
    Generally these DCHECKS precede a use or erase of the
    checked iterator, which if the check is invalid (ie. the
    iterator == .end()) is UB.
    
    Added checks are NotFatalUntil::M130 unless in testonly code.
    
    `base/not_fatal_until.h` is added using tools/add_header.py,
    this may result in some main-file (foo.h for foo.cc) headers
    being re-sorted to be first as part `git cl format` of this CL.
    
    For this CL instances were located with a weggli query:
    
    ```
    weggli --verbose=1 --cpp \
             'DCHECK(_ != _->end());' \
          -p 'DCHECK(_->end() != _);' \
          -p 'DCHECK_NE(_, _->end());' \
          -p 'DCHECK_NE(_->end(), _);'
    ```
    
    which should avoid any potentially expensive calculations
    of the thing to be matched against .end().
    
    This CL was uploaded by git cl split.
    
    R=jsbell@chromium.org, pauljensen@chromium.org
    
    Bug: 351745839
    Change-Id: I05b1fb09489f17f838d855be80a0594af7688c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719014
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Auto-Submit: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329629}

--- content/browser/indexed_db/indexed_db_tombstone_sweeper.cc
+++ content/browser/indexed_db/indexed_db_tombstone_sweeper.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 
+#include "base/not_fatal_until.h"
 #include "base/rand_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/task/sequenced_task_runner.h"
@@ -41,7 +42,7 @@ WrappingIterator<T>::WrappingIterator(const T* container,
   DCHECK_LT(start_position, container_->size());
   inner_ = container_->begin();
   std::advance(inner_, start_position);
-  DCHECK(inner_ != container_->end());
+  CHECK(inner_ != container_->end(), base::NotFatalUntil::M130);
 }
 
 template <typename T>
