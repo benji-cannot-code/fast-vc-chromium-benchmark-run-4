c535908db51c4b02ad5479233067fb6ef32a4047
Steven Holte <holte@google.com>
2024-07-18 23:24:53 +0000

    Add test lib for making sessions.Execute arguments.
    Adds ResponseHolder for callback that captures results and some Request
    builder methods.
    
    Bug: 351908251
    
    Change-Id: I70fb99c521e60773437eec5e5fe3bc90e5ae3aeb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5722222
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1329887}

---
+++ components/optimization_guide/core/model_execution/test/request_builder.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TEST_REQUEST_BUILDER_H_
+#define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TEST_REQUEST_BUILDER_H_
+
+#include "components/optimization_guide/proto/features/compose.pb.h"
+
+namespace optimization_guide {
+
+// A ComposeRequest with page_metadata.page_url filled.
+proto::ComposeRequest PageUrlRequest(const std::string& input);
+
+// A ComposeRequest with generate_params.user_input filled.
+proto::ComposeRequest UserInputRequest(const std::string& input);
+
+// A ComposeRequest with rewrite_params.previous_response filled.
+proto::ComposeRequest RewriteRequest(const std::string& previous_response);
+
+}  // namespace optimization_guide
+
+#endif  // COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TEST_REQUEST_BUILDER_H_
