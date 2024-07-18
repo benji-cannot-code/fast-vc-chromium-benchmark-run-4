4e980650e2590267474b3ce0d2cc4e93c99cd508
Yuheng Huang <yuhengh@chromium.org>
2024-07-18 14:52:12 +0000

    Data Sharing: Initial integration
    Initial work for data sharing UI flow. The CL includes
    1. Plumbs identity manager to fetch auth token and supplies to the renderer through mojom.
    2. Add dev only UI to data-sharing page.
    3. Override some content security policies to make the SDK work.
    See [1] for more info.
    
    [1]https://docs.google.com/document/d/1cMLcV9mlSiW77eXVYHbgxZ4FW7_ZsZ59m5Bn6XgEuzA/edit?tab=t.0#bookmark=id.1hskmf1ve1ns
    
    Bug: 342638650
    Change-Id: I52cdc45ee3ab5b03dfa6ea7fdc8c33160bcb7327
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5710209
    Commit-Queue: Yuheng Huang <yuhengh@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329538}

--- chrome/browser/resources/data_sharing/browser_proxy_base.ts
+++ chrome/browser/resources/data_sharing/browser_proxy_base.ts
@@ -4,18 +4,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import type {PageHandlerInterface} from './data_sharing.mojom-webui.js';
-import {PageHandlerFactory, PageHandlerRemote} from './data_sharing.mojom-webui.js';
+import {PageCallbackRouter, PageHandlerFactory, PageHandlerRemote} from './data_sharing.mojom-webui.js';
 
 // Base class of BrowserProxy.
 // Implement common logic i.e. setting mojom pipe.
 export class BrowserProxyBase {
+  callbackRouter: PageCallbackRouter;
   handler: PageHandlerInterface;
 
   constructor() {
+    this.callbackRouter = new PageCallbackRouter();
+
     this.handler = new PageHandlerRemote();
 
     const factory = PageHandlerFactory.getRemote();
     factory.createPageHandler(
+        this.callbackRouter.$.bindNewPipeAndPassRemote(),
         (this.handler as PageHandlerRemote).$.bindNewPipeAndPassReceiver());
   }
 }
--- chrome/browser/resources/data_sharing/data_sharing_sdk.ts
+++ chrome/browser/resources/data_sharing/data_sharing_sdk.ts
@@ -4,3 +4,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // Dummy implementation of data_sharing_sdk.js for non-branded build.
+
+declare interface Token {
+  access_token: string;
+}
+declare global {
+  interface Window {
+    gapi: {auth: {getToken: () => Token}};
+    data_sharing_sdk: {
+      setOauthAccessToken: (accessToken: string) => void,
+      createGroup: (resourceId: string) => void,
+      runJoinFlow: (resourceId: string, tokenSecret: string) => void,
+      runInviteFlow: (resourceId: string) => void,
+      runManageFlow: (resourceId: string) => void,
+    };
+  }
+}
+export {};
+
+window.data_sharing_sdk = {
+  setOauthAccessToken: () => void{},
+  createGroup: () => void{},
+  runJoinFlow: () => void{},
+  runInviteFlow: () => void{},
+  runManageFlow: () => void{},
+};
