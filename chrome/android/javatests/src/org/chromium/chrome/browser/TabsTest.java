e9e0a8ee037c9eeaddb6b6b43178a5db073827d4
Theo Cristea <theocristea@google.com>
2024-07-18 11:27:22 +0000

    [Gardening] Disable flaky TabsTest#testHideKeyboard on tablet
    Bug: 353910783
    Change-Id: I86c102cdfe7108e8eba73ed83426f05323b85a39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717158
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Reviewed-by: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329437}

--- chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/TabsTest.java
@@ -41,6 +41,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Feature;
@@ -78,6 +79,7 @@ import org.chromium.content_public.browser.test.util.UiUtils;
 import org.chromium.content_public.common.ContentSwitches;
 import org.chromium.ui.modaldialog.ModalDialogProperties;
 import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.test.util.UiDisableIf;
 import org.chromium.ui.test.util.UiRestriction;
 
 import java.io.File;
@@ -294,6 +296,7 @@ public class TabsTest {
     @LargeTest
     @Restriction(UiRestriction.RESTRICTION_TYPE_TABLET)
     @Feature({"Android-TabSwitcher"})
+    @DisableIf.Device(type = {UiDisableIf.TABLET}) // crbug.com/353910783
     public void testHideKeyboard() throws Exception {
         // Open a new tab(The 1st tab) and click node.
         sActivityTestRule.loadUrlInNewTab(getUrl(TEST_FILE_PATH), false);
