6fc00baf6a32e68263ab952c18aaaf1c56e2fb30
Christoph Schwering <schwering@google.com>
2024-07-18 15:33:15 +0000

    [Autofill] Add metric AutofillAgent::DOMContentLoaded
    Bug: 353545328
    Change-Id: Icc2cbae844005db1e53db5764dda8e6156d98aaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718091
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329567}

--- components/autofill/content/renderer/autofill_agent_browsertest.cc
+++ components/autofill/content/renderer/autofill_agent_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/feature_list.h"
+#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
@@ -1544,6 +1545,17 @@ TEST_P(AutofillAgentTestClick, MAYBE_AskForValuesToFillOnClick) {
   Click("other");
   checkpoint.Call("right click on field");
   RightClick("f");
+}
+
+// Tests that DOMContentLoaded() emits a metric.
+TEST_F(AutofillAgentTest, DOMContentLoadedEmitsMetric) {
+  base::HistogramTester histogram_tester;
+  LoadHTML(R"(
+    <p>Hello world</p>
+  )");
+  EXPECT_THAT(histogram_tester.GetAllSamples(
+                  "Autofill.DOMContentLoadedInOutermostMainFrame"),
+              base::BucketsAre(base::Bucket(true, 1), base::Bucket(false, 0)));
 }
 
 }  // namespace
