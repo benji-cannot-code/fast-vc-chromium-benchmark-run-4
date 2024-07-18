54c759ddb33c90af8a61e9e8ffc150f60b3b340d
Greg Thompson <grt@chromium.org>
2024-07-18 07:54:19 +0000

    [windows] Delete the AppCompatFlags Layers value when it is empty
    The Layers value, when present, has a leading "~" character (for
    example, one might find a value of "~ WIN8RTM" when the Win 8
    compatibility mode is chosen). When the browser strips off the known
    compatibility modes, check to see if all that remains is "~". In this
    case, delete the value from the registry. This prevents extra work from
    being done on subsequent launches.
    
    Bug: 347710693
    Change-Id: Ifc7c7cf4e50c58fa97df4bf3c1767c35646bad3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713136
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329370}

--- chrome/browser/win/remove_app_compat_entries.cc
+++ chrome/browser/win/remove_app_compat_entries.cc
@@ -51,6 +51,13 @@ bool RemoveCompatLayers(std::wstring& layers) {
     return false;  // Nothing to remove.
   }
 
+  static constexpr std::wstring_view kMagicToken(L"~");
+  if (tokens.size() == 1 && tokens.front() == kMagicToken) {
+    // The input is nothing but the leading token.
+    layers.clear();
+    return true;
+  }
+
   // Remove all known compatibility mode layer names.
   static constexpr auto kCompatModeTokens =
       base::MakeFixedFlatSet<std::wstring_view>({
@@ -68,7 +75,7 @@ bool RemoveCompatLayers(std::wstring& layers) {
     return false;  // No changes made.
   }
   tokens.erase(new_end, tokens.end());
-  if (tokens.empty()) {
+  if (tokens.empty() || (tokens.size() == 1 && tokens.front() == kMagicToken)) {
     layers.clear();
   } else {
     layers = base::JoinString(tokens, L" ");
