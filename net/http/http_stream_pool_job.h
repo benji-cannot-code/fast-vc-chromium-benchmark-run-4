6d04edaab93f7567faea8a0626a07ef4f91a0993
Kenichi Ishibashi <bashi@chromium.org>
2024-07-18 05:04:43 +0000

    Handle client auth cert in HttpStreamPool
    When a connection attempt requests a client certificate, call
    HttpStreamRequest::Delegate::OnNeedsClientAuth() for all on-going
    requests, in a way similar to the way it handles certificate errors.
    
    If there are multiple attempts, another failure may happen
    (e.g. TCP handshake failed) while calling OnNeedsClientAuth() for all
    requests. In this case, we use the first fatal error (certificate
    errors, client auth is needed, no more endpoint to attempt).
    
    Bug: 346835898
    Change-Id: I1ee30e35ae052b2ccf12275bb0747480ceb8d40e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5715615
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329319}

--- net/http/http_stream_pool_job.h
+++ net/http/http_stream_pool_job.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "net/base/load_states.h"
@@ -26,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log_with_source.h"
 #include "net/socket/stream_attempt.h"
 #include "net/socket/tls_stream_attempt.h"
+#include "net/ssl/ssl_cert_request_info.h"
 #include "url/gurl.h"
 
 namespace net {
@@ -153,6 +155,9 @@ class HttpStreamPool::Job
   // Notifies a certificate error to all requests.
   void NotifyCertificateError(int rv, const SSLInfo& ssl_info);
 
+  // Notifies a client cert is needed to all requests.
+  void NotifyNeedsClientAuth(scoped_refptr<SSLCertRequestInfo> cert_info);
+
   // Creates a text based stream and notifies the highest priority request.
   void CreateTextBasedStreamAndNotify(
       std::unique_ptr<StreamSocket> stream_socket);
@@ -170,6 +175,9 @@ class HttpStreamPool::Job
 
   void OnInFlightAttemptComplete(InFlightAttempt* raw_attempt, int rv);
   void OnInFlightAttemptSlow(InFlightAttempt* raw_attempt);
+
+  void HandleAttemptFailure(std::unique_ptr<InFlightAttempt> in_flight_attempt,
+                            int rv);
 
   void MaybeComplete();
 
