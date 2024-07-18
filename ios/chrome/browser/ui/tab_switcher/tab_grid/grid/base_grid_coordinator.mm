74e1e6197b91a834c76c6c7577e70c0b3bd58ae1
Asami Doi <asamidoi@chromium.org>
2024-07-18 08:34:07 +0000

    [ios] Add TabGroupConfirmationCoordinator class
    This CL adds TabGroupConfirmationCoordinator class to handle
    ActionSheetCoordinator inside. It changes the title in the context
    menu depending on the action taken for a tab group.
    Currently, only "delete" action type is supported.
    
    No behavior/UI changes in this CL.
    
    Bug: 329631586
    Change-Id: I5ff5051a1463fc43e493541615e4ec0e4389e12f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5707868
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1329384}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_coordinator.mm
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/shared/coordinator/alert/action_sheet_coordinator.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
@@ -17,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_view_controller.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/grid_container_view_controller.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_group_confirmation_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/create_tab_group_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_coordinator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_view_controller.h"
@@ -34,8 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   TabGroupCoordinator* _tabGroupCoordinator;
   // Handles the creation of a new tab group.
   CreateTabGroupCoordinator* _tabGroupCreator;
-  // The action sheet coordinator, if one is currently being shown.
-  ActionSheetCoordinator* _actionSheetCoordinator;
+  // The coordinator to handle the confirmation dialog for the action taken for
+  // a tab group.
+  TabGroupConfirmationCoordinator* _tabGroupConfirmationCoordinator;
 }
 
 #pragma mark - Public
@@ -102,6 +103,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)stopChildCoordinators {
+  if (_tabGroupConfirmationCoordinator) {
+    [_tabGroupConfirmationCoordinator stop];
+    _tabGroupConfirmationCoordinator = nil;
+  }
   [self hideTabGroupCreationAnimated:NO];
   [self.tabGroupCoordinator stopChildCoordinators];
   [self.gridViewController dismissModals];
@@ -249,31 +254,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.mediator displayActiveTab];
 }
 
-- (void)showTabGroupDeleteConfirmationForGroup:(const TabGroup*)tabGroup
-                                    sourceView:(UIView*)sourceView {
-  _actionSheetCoordinator = [[ActionSheetCoordinator alloc]
+- (void)showTabGroupConfirmationForAction:(TabGroupActionType)actionType
+                                    group:(const TabGroup*)tabGroup
+                               sourceView:(UIView*)sourceView {
+  _tabGroupConfirmationCoordinator = [[TabGroupConfirmationCoordinator alloc]
       initWithBaseViewController:self.baseViewController
                          browser:self.browser
-                           title:nil
-                         message:nil
-                            rect:sourceView.bounds
-                            view:sourceView];
-
+                      actionType:actionType
+                      sourceView:sourceView];
   base::WeakPtr<const TabGroup> weakGroup = tabGroup->GetWeakPtr();
   __weak BaseGridCoordinator* weakSelf = self;
-  [_actionSheetCoordinator
-      addItemWithTitle:l10n_util::GetNSString(
-                           IDS_IOS_CONTENT_CONTEXT_DELETEGROUP)
-                action:^{
-                  [weakSelf closeTabsAndDeleteGroup:weakGroup];
-                }
-                 style:UIAlertActionStyleDestructive];
-  [_actionSheetCoordinator addItemWithTitle:l10n_util::GetNSString(IDS_CANCEL)
-                                     action:^{
-                                       [weakSelf dismissActionSheetCoordinator];
-                                     }
-                                      style:UIAlertActionStyleCancel];
-  [_actionSheetCoordinator start];
+  _tabGroupConfirmationCoordinator.action = ^{
+    [weakSelf closeTabsAndDeleteGroup:weakGroup];
+  };
+
+  [_tabGroupConfirmationCoordinator start];
 }
 
 #pragma mark - CreateOrEditTabGroupCoordinatorDelegate
@@ -334,14 +329,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (weakGroup) {
     [self.mediator closeTabGroup:weakGroup.get() andDeleteGroup:YES];
   }
-  [self dismissActionSheetCoordinator];
-}
-
-// Stops the action sheet coordinator currently showned and nullifies the
-// instance.
-- (void)dismissActionSheetCoordinator {
-  [_actionSheetCoordinator stop];
-  _actionSheetCoordinator = nil;
+  [_tabGroupConfirmationCoordinator stop];
+  _tabGroupConfirmationCoordinator = nil;
 }
 
 @end
