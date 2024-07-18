4b59d5c4659bf7cac2b5252e6730646a119da465
Gauthier Ambard <gambard@chromium.org>
2024-07-18 11:23:41 +0000

    [iOS] Update InactiveTabs button visibility when needed
    This CL makes sure to correctly update the inactive tabs button
    when necessary: when its visibility changes or when a data updates.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL refactor of existing feature
    Bug: 352722446
    Change-Id: I7489e08c27c4577716fdee00fb0eea15776d2960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713040
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329435}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
@@ -175,12 +175,7 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
   if (_inactiveTabsCount == 0) {
     return;
   }
-  [snapshot
-      insertSectionsWithIdentifiers:@[ kInactiveTabButtonSectionIdentifier ]
-        beforeSectionWithIdentifier:kGridOpenTabsSectionIdentifier];
-  GridItemIdentifier* item = [GridItemIdentifier inactiveTabsButtonIdentifier];
-  [snapshot appendItemsWithIdentifiers:@[ item ]
-             intoSectionWithIdentifier:kInactiveTabButtonSectionIdentifier];
+  [self addInactiveTabsButtonToSnapshot:snapshot];
 }
 
 #pragma mark - InactiveTabsInfoConsumer
@@ -193,7 +188,8 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
   _inactiveTabsCount = count;
 
   if (IsInactiveTabButtonRefactoringEnabled()) {
-    // TODO(crbug.com/352722446): reset the snapshot.
+    BOOL inactiveTabsButtonVisible = count != 0;
+    [self updateInactiveTabsButtonVisible:inactiveTabsButtonVisible];
   } else {
     // Update the layout.
     [self updateTabsSectionHeaderType];
@@ -218,7 +214,9 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
   _inactiveTabsDaysThreshold = daysThreshold;
 
   if (IsInactiveTabButtonRefactoringEnabled()) {
-    // TODO(crbug.com/352722446): Update the item if necessary.
+    BOOL inactiveTabsButtonVisible =
+        daysThreshold != kInactiveTabsDisabledByUser;
+    [self updateInactiveTabsButtonVisible:inactiveTabsButtonVisible];
   } else {
     // Update the header.
     if (oldDaysThreshold == kInactiveTabsDisabledByUser ||
@@ -249,6 +247,53 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
 }
 
 #pragma mark - Private
+
+// Updates the inactive tabs button based on its `visible` state (i.e.
+// reconfigure, show or remove).
+- (void)updateInactiveTabsButtonVisible:(BOOL)visible {
+  GridSnapshot* snapshot = [self.diffableDataSource snapshot];
+  if (visible) {
+    GridItemIdentifier* item =
+        [GridItemIdentifier inactiveTabsButtonIdentifier];
+
+    if ([snapshot indexOfItemIdentifier:item] != NSNotFound) {
+      [snapshot reconfigureItemsWithIdentifiers:@[ item ]];
+    } else {
+      [self addInactiveTabsButtonToSnapshot:snapshot];
+    }
+  } else {
+    BOOL isSectionInSnapshot =
+        [snapshot
+            indexOfSectionIdentifier:kInactiveTabButtonSectionIdentifier] !=
+        NSNotFound;
+
+    if (isSectionInSnapshot) {
+      [snapshot deleteSectionsWithIdentifiers:@[
+        kInactiveTabButtonSectionIdentifier
+      ]];
+    }
+  }
+  [self.diffableDataSource applySnapshot:snapshot animatingDifferences:YES];
+}
+
+// Adds the inactive tabs button to `snapshot` if it is not there yet.
+- (void)addInactiveTabsButtonToSnapshot:(GridSnapshot*)snapshot {
+  NSInteger sectionIndex =
+      [snapshot indexOfSectionIdentifier:kInactiveTabButtonSectionIdentifier];
+
+  if (sectionIndex == NSNotFound) {
+    [snapshot
+        insertSectionsWithIdentifiers:@[ kInactiveTabButtonSectionIdentifier ]
+          beforeSectionWithIdentifier:kGridOpenTabsSectionIdentifier];
+  }
+
+  GridItemIdentifier* item = [GridItemIdentifier inactiveTabsButtonIdentifier];
+
+  if ([snapshot indexOfItemIdentifier:item] == NSNotFound) {
+    [snapshot appendItemsWithIdentifiers:@[ item ]
+               intoSectionWithIdentifier:kInactiveTabButtonSectionIdentifier];
+  }
+}
 
 - (void)showInactiveTabsButtonHeader {
   CHECK(!IsInactiveTabButtonRefactoringEnabled());
