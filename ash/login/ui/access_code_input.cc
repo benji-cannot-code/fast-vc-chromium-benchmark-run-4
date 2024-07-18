d9a17c9c1663a272a4e1644c6d883f661e896b20
Gaurav Kumar <gauravkum@microsoft.com>
2024-07-18 06:44:29 +0000

    [views-ax] Migration of kProtected state in access_code_input
    This CL migrates the kProtected state in Views to be updated
    whenever its value should change, rather than querying the value
    and computing it only when needed.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I643f88887ae2683cdfda05d1c6a659a28c2ac87f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672320
    Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329346}

--- ash/login/ui/access_code_input.cc
+++ ash/login/ui/access_code_input.cc
@@ -285,6 +285,7 @@ FixedLengthCodeInput::FixedLengthCodeInput(int length,
     layout->SetFlexForView(field, 1);
   }
   text_value_for_a11y_ = std::u16string(length, ' ');
+  GetViewAccessibility().SetIsProtected(is_obscure_pin_);
 }
 
 FixedLengthCodeInput::~FixedLengthCodeInput() = default;
@@ -401,9 +402,7 @@ void FixedLengthCodeInput::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   const gfx::Range& range = GetSelectedRangeOfTextValueForA11y();
   node_data->AddIntAttribute(ax::mojom::IntAttribute::kTextSelStart,
                              range.start());
-  if (is_obscure_pin_) {
-    node_data->AddState(ax::mojom::State::kProtected);
-  }
+
   node_data->AddIntAttribute(ax::mojom::IntAttribute::kTextSelEnd, range.end());
 }
 
