ba54433be6502fea4f9ded8ca66459642e8dbd64
Egor Pasko <pasko@chromium.org>
2024-07-18 16:17:43 +0000

    android: Use top resumed position callback for InputHint
    Perform InputHintChecker.setView() in the
    Activity.onTopResumedActivityChanged() callback. It is called when
    activity gets or loses the top resumed position in the system.
    
    This should allow the Input Hint to work in multi-window mode.
    
    Since the InputHintChecker depends on native, the
    onTopResumedActivityChangedWithNative() is introduced, analogous to
    onResumeWithNative().
    
    Bug: 350531728
    Change-Id: Iecaadf1f43830bde26fb9bee053c348ccc838857
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713149
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Egor Pasko <pasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329597}

--- chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/init/NativeInitializationController.java
@@ -37,6 +37,7 @@ class NativeInitializationController {
     private boolean mHasDoneFirstDraw;
     private boolean mHasSignaledLibraryLoaded;
     private boolean mInitializationComplete;
+    private boolean mOnTopResumedPending;
 
     /**
      * This class encapsulates a call to onActivityResult that has to be deferred because the native
@@ -142,6 +143,11 @@ class NativeInitializationController {
             mOnResumePending = false;
             onResume();
         }
+
+        if (mOnTopResumedPending) {
+            mOnTopResumedPending = false;
+            onTopResumedActivityChanged(true);
+        }
     }
 
     /** Called when an activity gets an onStart call and is done with java only tasks. */
@@ -159,6 +165,21 @@ class NativeInitializationController {
             mActivityDelegate.onResumeWithNative();
         } else {
             mOnResumePending = true;
+        }
+    }
+
+    /**
+     * Called when activity gets or loses the top resumed position in the system.
+     *
+     * @param isTopResumedActivity {@code true} if it's the topmost resumed activity in the system,
+     *     {@code false} otherwise. A call with this as {@code true} will always be followed by
+     *     another one with {@code false}.
+     */
+    public void onTopResumedActivityChanged(boolean isTopResumedActivity) {
+        if (mInitializationComplete) {
+            mActivityDelegate.onTopResumedActivityChangedWithNative(isTopResumedActivity);
+        } else {
+            mOnTopResumedPending = isTopResumedActivity;
         }
     }
 
