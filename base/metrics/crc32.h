cb06d2eef042eb6e6a3e686865cca9f340dd6299
Tarcisio Fischer <tarcisio.fischer@arm.com>
2024-07-18 19:38:03 +0000

    Use Crc32 implementation from zlib
    zlib's CRC32 implementation is highly optimized.
    Switch to using it to reduce code duplication.
    
    Kept the old implementation on NaCl builds due to issues with zlib
    (crashing while loading the library on NaCl).
    
    New tests were added to ensure that such cases yields same results as
    the previous implementation. The new tests also pass in the old
    implementation.
    
    Change-Id: I03977e56a9220cd133de0fa187ae888ad4480e1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717691
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329749}

--- base/metrics/crc32.h
+++ base/metrics/crc32.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-BASE_EXPORT extern const std::array<uint32_t, 256> kCrcTable;
-
 // This provides a simple, fast CRC-32 calculation that can be used for checking
 // the integrity of data.  It is not a "secure" calculation!  |sum| can start
 // with any seed or be used to continue an operation began with previous data.
