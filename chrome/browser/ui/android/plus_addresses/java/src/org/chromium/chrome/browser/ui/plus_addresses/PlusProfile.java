040e899efeb722689038ebcf1916e930d4c7aaf3
Timofey Chudakov <tchudakov@google.com>
2024-07-18 09:07:58 +0000

    [Android] Add "Select plus address" action to KA.
    This CL corresponds to the "Select Shielded Email" section in
    the design doc: go/chrome-shielded-ids-clank-dd
    
    This CL implements only AllPlusAddressBottomSheetController and
    AllPlusAddressesBottomSheetView + basic functionality in the
    AllPlusAddressesBottomSheetBridge. No actual UI is added in this CL.
    
    Screenshot: https://screenshot.googleplex.com/3T49hDynMEBFDxw
    
    Bug: 327838324
    Change-Id: Id68358f27bc93691cdfe0b12090da0ab3dde373e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703300
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329397}

---
+++ chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusProfile.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.ui.plus_addresses;
+
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
+
+// Android plain old Java object class that is used to display the user's plus addresses in the all
+// plus addresses bottom sheet.
+@JNINamespace("plus_addresses")
+class PlusProfile {
+    // Plus address email address.
+    private String mPlusAddress;
+    // The domain where the plus address was created.
+    private String mOrigin;
+
+    @CalledByNative
+    public PlusProfile(
+            @JniType("std::string") String plusAddress, @JniType("std::string") String origin) {
+        mPlusAddress = plusAddress;
+        mOrigin = origin;
+    }
+
+    public String getPlusAddress() {
+        return mPlusAddress;
+    }
+
+    public String getOrigin() {
+        return mOrigin;
+    }
+}
