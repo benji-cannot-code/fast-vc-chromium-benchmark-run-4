2222bc976ff9c872171005640e19058db289195a
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-18 21:51:54 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to mojo/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 351564777
    Change-Id: I230585d1177479f83855ee6c57a3f1ded2e66839
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718251
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329842}

--- mojo/public/cpp/base/string16_mojom_traits.h
+++ mojo/public/cpp/base/string16_mojom_traits.h
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #ifndef MOJO_PUBLIC_CPP_BASE_STRING16_MOJOM_TRAITS_H_
 #define MOJO_PUBLIC_CPP_BASE_STRING16_MOJOM_TRAITS_H_
 
