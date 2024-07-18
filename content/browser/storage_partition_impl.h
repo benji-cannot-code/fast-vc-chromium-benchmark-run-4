5682a7ef4207b8e23f46ce506d7a3e146b493cbf
Hiroshige Hayashizaki <hiroshige@chromium.org>
2024-07-18 05:11:10 +0000

    Rename URLLoaderFactoryGetter to ReconnectableURLLoaderFactoryForIOThread
    Mechanical changes that don't change the behavior as a preparation
    for https://chromium-review.googlesource.com/c/chromium/src/+/5627156.
    
    Also
    - Makes CONTENT_EXPORT per-class.
    - Adds `using CreateCallback`, `final`, `default`.
    - Renames methods to align with `ReconnectableURLLoaderFactory`:
      `FlushNetworkInterfaceForTesting` -> `FlushOnIOThreadForTesting`,
      `FlushNetworkInterfaceOnIOThreadForTesting` -> `FlushForTesting`,
      `GetPendingNetworkFactory` -> `CloneForIOThread`.
    
    Bug: 346686150
    Change-Id: I1a3e0ee9b6bbf2b9b2fdf01712376ff826abb6ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5700712
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329323}

--- content/browser/storage_partition_impl.h
+++ content/browser/storage_partition_impl.h
@@ -392,7 +392,8 @@ class CONTENT_EXPORT StoragePartitionImpl
     return shared_storage_header_observer_.get();
   }
 
-  scoped_refptr<URLLoaderFactoryGetter> url_loader_factory_getter() {
+  scoped_refptr<ReconnectableURLLoaderFactoryForIOThread>
+  url_loader_factory_getter() {
     return url_loader_factory_getter_;
   }
 
@@ -720,7 +721,8 @@ class CONTENT_EXPORT StoragePartitionImpl
   bool initialized_ = false;
 
   mojo::Remote<storage::mojom::Partition> remote_partition_;
-  scoped_refptr<URLLoaderFactoryGetter> url_loader_factory_getter_;
+  scoped_refptr<ReconnectableURLLoaderFactoryForIOThread>
+      url_loader_factory_getter_;
   scoped_refptr<QuotaContext> quota_context_;
   scoped_refptr<storage::QuotaManager> quota_manager_;
   scoped_refptr<storage::FileSystemContext> filesystem_context_;
