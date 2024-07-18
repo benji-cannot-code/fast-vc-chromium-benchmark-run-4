81b461daf7d5c4a024db0d94e651141360c47572
Marlon Facey <mfacey@chromium.org>
2024-07-18 16:46:20 +0000

    [Tab Resume 2.0] Set up base classes for deduping
    Sets up base classes for url deduping including helper class and
    delegates to forward calls for document deduping and template url
    deduping.
    
    Design Doc:
    https://docs.google.com/document/d/1w7I9VzvY2orRdKNAydb60NaaEOdnMXtlc8XUsSH75SM/edit?pli=1
    
    Bug: 335200723
    Change-Id: Ice37c6eccc9f2d3ee2626e8382bfb4f9523ce732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5700085
    Commit-Queue: Marlon Facey <mfacey@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329625}

---
+++ components/url_deduplication/deduplication_strategy.cc
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/url_deduplication/deduplication_strategy.h"
+
+DeduplicationStrategy::DeduplicationStrategy() = default;
+
+DeduplicationStrategy::DeduplicationStrategy(
+    const DeduplicationStrategy& strategy) = default;
+
+DeduplicationStrategy::~DeduplicationStrategy() = default;
---
+++ components/url_deduplication/url_strip_handler.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_URL_DEDUPLICATION_URL_STRIP_HANDLER_H_
+#define COMPONENTS_URL_DEDUPLICATION_URL_STRIP_HANDLER_H_
+
+class GURL;
+
+// Class to handle various methods of stripping URLs.
+class URLStripHandler {
+ public:
+  // Strips params that are not useful for disambiguating urls.
+  GURL StripExtraParams(GURL);
+};
+
+#endif  // COMPONENTS_URL_DEDUPLICATION_URL_STRIP_HANDLER_H_
