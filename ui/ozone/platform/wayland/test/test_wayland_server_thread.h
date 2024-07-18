8a74c2e74e8c3894ae16c71c4045f6eaa4fdfe44
Orko Garai <orko@igalia.com>
2024-07-18 16:52:11 +0000

    [ozone/wayland] text-input-v3 Part 2: Initial connection, v3 test initialization
    Split from crrev.com/c/5630587.
    
    Summary of changes:
    - Add text-input-v3 global to WaylandConnection.
    - Support injecting TestZwpTextInputManagerV3 from
      TestWaylandServerThread
    - Initialize ZWPTextInputWrapperV3Test class for testing v3 wrapper.
    
    Design doc:
    https://docs.google.com/document/d/1GkOphcAQBMdW4iPiMOd9eKd70tlXWQaR7M3GJXGUDpQ/edit
    
    Bug: 40113488
    Change-Id: I0688bb3254995ee8b2a2ad4ca735bc44a5e5a92e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5709570
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329628}

--- ui/ozone/platform/wayland/test/test_wayland_server_thread.h
+++ ui/ozone/platform/wayland/test/test_wayland_server_thread.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
 #include "ui/display/types/display_constants.h"
+#include "ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h"
 #include "ui/ozone/platform/wayland/test/global_object.h"
 #include "ui/ozone/platform/wayland/test/mock_wayland_zcr_color_manager.h"
 #include "ui/ozone/platform/wayland/test/mock_wp_presentation.h"
@@ -61,6 +62,8 @@ enum class EnableAuraShellProtocol { kEnabled, kDisabled };
 struct ServerConfig {
   TestZcrTextInputExtensionV1::Version text_input_extension_version =
       TestZcrTextInputExtensionV1::Version::kV14;
+  ui::ZWPTextInputWrapperType text_input_wrapper_type =
+      ui::ZWPTextInputWrapperType::kV1;
   TestCompositor::Version compositor_version = TestCompositor::Version::kV4;
   PrimarySelectionProtocol primary_selection_protocol =
       PrimarySelectionProtocol::kNone;
@@ -160,6 +163,9 @@ class TestWaylandServerThread : public TestOutput::Delegate,
   TestZwpTextInputManagerV1* text_input_manager_v1() {
     return &zwp_text_input_manager_v1_;
   }
+  TestZwpTextInputManagerV3* text_input_manager_v3() {
+    return &zwp_text_input_manager_v3_;
+  }
   TestZwpLinuxExplicitSynchronizationV1*
   zwp_linux_explicit_synchronization_v1() {
     return &zwp_linux_explicit_synchronization_v1_;
@@ -247,6 +253,7 @@ class TestWaylandServerThread : public TestOutput::Delegate,
   TestZcrStylus zcr_stylus_;
   TestZcrTextInputExtensionV1 zcr_text_input_extension_v1_;
   TestZwpTextInputManagerV1 zwp_text_input_manager_v1_;
+  TestZwpTextInputManagerV3 zwp_text_input_manager_v3_;
   TestZwpLinuxExplicitSynchronizationV1 zwp_linux_explicit_synchronization_v1_;
   MockZwpLinuxDmabufV1 zwp_linux_dmabuf_v1_;
   MockWpPresentation wp_presentation_;
