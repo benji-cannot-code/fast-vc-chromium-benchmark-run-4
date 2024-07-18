d8d53886ace857acfd72071780f53faea9a0ee50
dpapad <dpapad@chromium.org>
2024-07-18 17:12:53 +0000

    NewTabPage: Migrate to Lit, part 8.
    In this part migrating modules/v2/file_suggestion/.
    
    As part of the migration
     - Removing the <cr-lazy-render> instance that was used to show the
       <ntp-info-dialog>, since cr-lazy-render should be used for things
       that should remain in the DOM after the 1st render, unlike dialogs
       which should be removed from the DOM once closed.
     - Exposed a show-on-attach property on ntp-info-dialog to make it
       easier to use it with Lit's conditional rendering.
     - Updated tests that exercise the various buttons on the header
       element's cr-action-menu to actually call click() on such elements,
       instead of manually firing events.
    
    Bug: 40943652
    Change-Id: I64037d0724d3c85f1ce554752fa11d540e54fa98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719477
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1329649}

--- chrome/browser/resources/new_tab_page/modules/info_dialog.ts
+++ chrome/browser/resources/new_tab_page/modules/info_dialog.ts
@@ -33,6 +33,14 @@ export class InfoDialogElement extends CrLitElement {
     return getHtml.bind(this)();
   }
 
+  static override get properties() {
+    return {
+      showOnAttach: {type: Boolean},
+    };
+  }
+
+  showOnAttach: boolean = false;
+
   showModal() {
     this.$.dialog.showModal();
   }
