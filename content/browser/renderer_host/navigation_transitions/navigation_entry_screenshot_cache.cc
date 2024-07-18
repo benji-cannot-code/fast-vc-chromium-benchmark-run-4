4c93657edfda01c5c3fbc6c290c13f2e8f5ef0a3
Baran Erfani <baranerf@google.com>
2024-07-18 20:30:04 +0000

    Added UMA Metrics For Cache Hit Or Miss Reasons
    The following metrics are added:
    - Cache hit
    - Cache miss - caused by subframe
    - Cache miss - not captured because of opting out of BFCache; related to crbug.com/40268228
    - Cache miss - others
    
    Bug: 40259040
    Change-Id: I46d464bfca128539b042ddcad09c579231fcbc88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689055
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Baran Erfani <baranerf@google.com>
    Reviewed-by: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329781}

--- content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_cache.cc
+++ content/browser/renderer_host/navigation_transitions/navigation_entry_screenshot_cache.cc
@@ -47,7 +47,7 @@ NavigationEntryScreenshotCache::NavigationEntryScreenshotCache(
 
 NavigationEntryScreenshotCache::~NavigationEntryScreenshotCache() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  Purge();
+  PurgeInternal(/*for_memory_pressure=*/false);
 }
 
 void NavigationEntryScreenshotCache::SetScreenshot(
@@ -130,6 +130,8 @@ void NavigationEntryScreenshotCache::SetScreenshotInternal(
       is_copied_from_embedder);
   entry->navigation_transition_data()
       .SetSameDocumentNavigationEntryScreenshotToken(std::nullopt);
+  entry->navigation_transition_data().set_cache_hit_or_miss_reason(
+      NavigationTransitionData::CacheHitOrMissReason::kCacheHit);
   cached_screenshots_.insert(entry->GetUniqueID());
   manager_->OnScreenshotCached(this, size);
 
@@ -152,6 +154,9 @@ NavigationEntryScreenshotCache::RemoveScreenshot(
   cached_screenshots_.erase(it);
   auto screenshot = RemoveScreenshotFromEntry(navigation_entry);
   manager_->OnScreenshotRemoved(this, screenshot->SizeInBytes());
+  static_cast<NavigationEntryImpl*>(navigation_entry)
+      ->navigation_transition_data()
+      .set_cache_hit_or_miss_reason(std::nullopt);
   return screenshot;
 }
 
@@ -227,11 +232,20 @@ void NavigationEntryScreenshotCache::EvictScreenshotsUntilUnderBudgetOrEmpty() {
       CHECK_LE(evicted_screenshot->SizeInBytes(),
                manager_->GetCurrentCacheSize());
       manager_->OnScreenshotRemoved(this, evicted_screenshot->SizeInBytes());
+
+      candidate_entry->navigation_transition_data()
+          .set_cache_hit_or_miss_reason(
+              NavigationTransitionData::CacheHitOrMissReason::
+                  kCacheMissEvicted);
     }
   }
 }
 
-void NavigationEntryScreenshotCache::Purge() {
+void NavigationEntryScreenshotCache::PurgeForMemoryPressure() {
+  PurgeInternal(/*for_memory_pressure=*/true);
+}
+
+void NavigationEntryScreenshotCache::PurgeInternal(bool for_memory_pressure) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   auto it = cached_screenshots_.begin();
   while (!IsEmpty()) {
@@ -241,6 +255,18 @@ void NavigationEntryScreenshotCache::Purge() {
     cached_screenshots_.erase(it);
     CHECK_LE(purged->SizeInBytes(), manager_->GetCurrentCacheSize());
     manager_->OnScreenshotRemoved(this, purged->SizeInBytes());
+
+    if (for_memory_pressure) {
+      evicted_entry->navigation_transition_data().set_cache_hit_or_miss_reason(
+          NavigationTransitionData::CacheHitOrMissReason::
+              kCacheMissPurgedMemoryPressure);
+    } else {
+      // Resetting the UMA enum since at this point `this` is getting destroyed
+      // by the destructor which invalidates the enum value.
+      evicted_entry->navigation_transition_data().set_cache_hit_or_miss_reason(
+          std::nullopt);
+    }
+
     it = cached_screenshots_.begin();
   }
 }
