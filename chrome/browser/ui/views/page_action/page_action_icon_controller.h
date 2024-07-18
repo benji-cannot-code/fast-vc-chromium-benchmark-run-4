23d8fbd9d095be9d6c4056cb261967402cd3a838
Thomas Lukaszewicz <tluk@chromium.org>
2024-07-18 04:01:58 +0000

    [lens] Update the lens entrypoint synchronously with pref changes
    Currently changing the "Always show lens shortcut" context menu
    option does not immediately affect the visibility of the entrypoint,
    its behavior will only change the next time the omnibox receives
    focus.
    
    This CL ensures the entrypoint updates immediately when the context
    menu option is selected.
    
    Bug: 353816632
    Change-Id: I91282baf6d3639a9ee69ed98d33cfcb23a937b78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717156
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1329307}

--- chrome/browser/ui/views/page_action/page_action_icon_controller.h
+++ chrome/browser/ui/views/page_action/page_action_icon_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/page_action/page_action_icon_type.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view.h"
 #include "chrome/browser/ui/views/page_action/page_action_icon_view_observer.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/zoom/zoom_event_manager.h"
 #include "components/zoom/zoom_event_manager_observer.h"
 #include "content/public/browser/navigation_handle.h"
@@ -109,6 +110,8 @@ class PageActionIconController : public PageActionIconViewObserver,
   raw_ptr<ZoomView> zoom_icon_ = nullptr;
 
   IconViews page_action_icon_views_;
+
+  PrefChangeRegistrar pref_change_registrar_;
 
   std::map<GURL, std::vector<raw_ptr<PageActionIconView, VectorExperimental>>>
       page_actions_excluded_from_logging_;
