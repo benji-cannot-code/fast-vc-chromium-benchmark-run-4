721eae8043900c0f148cbf01e551ff5f93c51742
Yuki Awano <yawano@google.com>
2024-07-18 12:29:52 +0000

    Disable ScalableIph for minor accounts
    Bug: b:353618236
    Fixed: b:353618236
    Test: browser_tests --gtest_filter=*ScalableIph*
    Change-Id: Id3aec615998228b2950dbe4d8720fa4e854a456b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716553
    Auto-Submit: Yuki Awano <yawano@google.com>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329461}

--- chrome/browser/scalable_iph/scalable_iph_factory.cc
+++ chrome/browser/scalable_iph/scalable_iph_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/scalable_iph/scalable_iph_factory.h"
 
+#include "base/check_is_test.h"
 #include "base/logging.h"
 #include "chromeos/ash/components/browser_context_helper/browser_context_helper.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
@@ -50,8 +51,21 @@ void ScalableIphFactory::InitializeServiceForProfile(Profile* profile) {
   // TODO(b/286604737): Disables ScalableIph services if multi-user sign-in is
   // used.
 
+  if (!on_building_service_instance_for_testing_callback_.is_null()) {
+    CHECK_IS_TEST();
+    on_building_service_instance_for_testing_callback_.Run(profile);
+  }
+
   // Create a `ScalableIph` service to start a timer for time tick event. Ignore
   // a return value. It can be nullptr if the browser context (i.e. profile) is
   // not eligible for `ScalableIph`.
   GetServiceForBrowserContext(profile, /*create=*/true);
+}
+
+void ScalableIphFactory::SetOnBuildingServiceInstanceForTestingCallback(
+    OnBuildingServiceInstanceForTestingCallback callback) {
+  CHECK_IS_TEST();
+  CHECK(on_building_service_instance_for_testing_callback_.is_null());
+  CHECK(!callback.is_null());
+  on_building_service_instance_for_testing_callback_ = std::move(callback);
 }
--- chrome/browser/scalable_iph/scalable_iph_factory.h
+++ chrome/browser/scalable_iph/scalable_iph_factory.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_SCALABLE_IPH_SCALABLE_IPH_FACTORY_H_
 
 #include "base/component_export.h"
+#include "base/functional/callback_forward.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
@@ -19,6 +20,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class COMPONENT_EXPORT(SCALABLE_IPH_FACTORY) ScalableIphFactory
     : public BrowserContextKeyedServiceFactory {
  public:
+  using OnBuildingServiceInstanceForTestingCallback =
+      base::RepeatingCallback<void(Profile* profile)>;
+
   static ScalableIphFactory* GetInstance();
 
   static scalable_iph::ScalableIph* GetForBrowserContext(
@@ -38,9 +42,21 @@ class COMPONENT_EXPORT(SCALABLE_IPH_FACTORY) ScalableIphFactory
   // happen. `GetForBrowserContext` does NOT instantiate a service.
   void InitializeServiceForProfile(Profile* profile);
 
+  // Allows test code to set up test environment before a service is built. This
+  // is called just before a service is built. For now, this is used to set
+  // can_use_manta_service capability. ScalableIph service is built immediately
+  // after user session starts. It's non-trival to do env set up at the correct
+  // timing without this type of helper method.
+  void SetOnBuildingServiceInstanceForTestingCallback(
+      OnBuildingServiceInstanceForTestingCallback callback);
+
  protected:
   ScalableIphFactory();
   ~ScalableIphFactory() override;
+
+ private:
+  OnBuildingServiceInstanceForTestingCallback
+      on_building_service_instance_for_testing_callback_;
 };
 
 #endif  // CHROME_BROWSER_SCALABLE_IPH_SCALABLE_IPH_FACTORY_H_
