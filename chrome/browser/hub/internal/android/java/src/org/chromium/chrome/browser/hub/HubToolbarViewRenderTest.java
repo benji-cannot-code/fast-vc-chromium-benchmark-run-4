6e3b299e524948f6a50500df449d4ea03c7cfc3a
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-18 20:50:07 +0000

    [Hub] Move pane switcher to align to bottom of toolbar
    See bug for before/after screenshots.
    
    Fixed: 353993723
    Change-Id: I05773203331f15410c4e23b14b1a997fee21671c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721318
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329798}

--- chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewRenderTest.java
+++ chrome/browser/hub/internal/android/java/src/org/chromium/chrome/browser/hub/HubToolbarViewRenderTest.java
@@ -42,7 +42,7 @@ public class HubToolbarViewRenderTest {
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
                     .setBugComponent(ChromeRenderTestRule.Component.UI_BROWSER_MOBILE_HUB)
-                    .setRevision(5)
+                    .setRevision(6)
                     .build();
 
     private Activity mActivity;
