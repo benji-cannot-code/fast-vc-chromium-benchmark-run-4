2be0661adb85771cef36e220229b340add39902a
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-07-18 14:27:34 +0000

    Create components/enterprise/data_controls/content/browser/ directory
    Follow-up to crrev.com/c/5695166
    
    This CL adds a "browser/" sub-directory to more explicitly contain
    browser processs-specific code.
    
    Bug: b/318421275
    Change-Id: I0936259667256f8f2c2740ba36497f0b3809c5c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717150
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329517}

--- components/enterprise/data_controls/content/browser/rules_service.h
+++ components/enterprise/data_controls/content/browser/rules_service.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_RULES_SERVICE_H_
-#define COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_RULES_SERVICE_H_
+#ifndef COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_RULES_SERVICE_H_
+#define COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_RULES_SERVICE_H_
 
 #include "components/enterprise/data_controls/core/rule.h"
 #include "components/enterprise/data_controls/core/verdict.h"
@@ -74,4 +74,4 @@ class RulesService : public KeyedService {
 
 }  // namespace data_controls
 
-#endif  // COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_RULES_SERVICE_H_
+#endif  // COMPONENTS_ENTERPRISE_DATA_CONTROLS_CONTENT_BROWSER_RULES_SERVICE_H_
