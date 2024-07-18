5a4be1be435e1df1a97d80a2962e1f0ed0c03c41
Juan Mojica <juanmojica@google.com>
2024-07-18 22:18:24 +0000

    [lensoverlay] Round object vertices by a configurable amount.
    Change-Id: If420c73733dcf79e8415d520fab515a749807c23
    Bug: b:343949232
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719251
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329852}

--- components/lens/lens_features.cc
+++ components/lens/lens_features.cc
@@ -198,6 +198,9 @@ constexpr base::FeatureParam<std::string> kLensOverlayGscQueryParamValue{
 const base::FeatureParam<bool> kLensOverlayEnableInFullscreen{
     &kLensOverlay, "enable-in-fullscreen", true};
 
+constexpr base::FeatureParam<int> kLensOverlaySegmentationMaskCornerRadius{
+    &kLensOverlay, "segmentation-mask-corner-radius", 12};
+
 constexpr base::FeatureParam<std::string> kHomepageURLForLens{
     &kLensStandalone, "lens-homepage-url", "https://lens.google.com/v3/"};
 
@@ -548,6 +551,10 @@ std::string GetLensOverlayGscQueryParamValue() {
 
 bool GetLensOverlayEnableInFullscreen() {
   return kLensOverlayEnableInFullscreen.Get();
+}
+
+int GetLensOverlaySegmentationMaskCornerRadius() {
+  return kLensOverlaySegmentationMaskCornerRadius.Get();
 }
 
 }  // namespace lens::features
