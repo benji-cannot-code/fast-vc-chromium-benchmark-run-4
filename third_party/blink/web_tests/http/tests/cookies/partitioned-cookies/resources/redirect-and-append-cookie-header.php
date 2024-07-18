e125e86bae43a6a73614ae2c30607a521d603a37
selya <selya@chromium.org>
2024-07-18 20:48:56 +0000

    Add WPT that covers same-site subresource redirects to x-site for CHIPS
    This test ensures that cookies are not made available in document.cookie and the http header, when a subresource is redirected to a cross-site.
    
    The existing test file ancestor-chain-subresource-redirect.https.html has been renamed to ancestor-chain-cross-site-subresource-to-same-site-redirect.https.html to differentiate it from this new test. The test in the file has also been refactored to replace the onLoad method with a promise that resolved when the iframe loads.
    
    
    
    Bug: 353963438
    Change-Id: I494a0ccf47301fd2b8d43ec284ebe735988bfdce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721470
    Reviewed-by: Steven Bingler <bingler@chromium.org>
    Reviewed-by: Aaron Selya <selya@google.com>
    Auto-Submit: Aaron Selya <selya@google.com>
    Commit-Queue: Aaron Selya <selya@google.com>
    Cr-Commit-Position: refs/heads/main@{#1329796}

--- third_party/blink/web_tests/http/tests/cookies/partitioned-cookies/resources/redirect-and-append-cookie-header.php
+++ third_party/blink/web_tests/http/tests/cookies/partitioned-cookies/resources/redirect-and-append-cookie-header.php
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
+  header("Access-Control-Allow-Origin: *");
+
   // Extract the cookie information.
   $headers = getallheaders();
   $cookieHeader = $headers['Cookie'];
