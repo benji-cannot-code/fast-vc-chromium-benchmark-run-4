02641c93955e701253285c1d473d1439613d8a19
Norman Bintang <normanbt@chromium.org>
2024-07-18 02:40:56 +0000

    vc-ui: Add title header in VC Tray
    Add a title header for VC Tray Bubble that will hold an icon, title label,
    mic indicator and a toggle for enabling/disabling sidetone.
    
    Bug: 349261564
    Change-Id: Ibb31e1770195e56adaec92a478b4ef0999ecc407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5644629
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Norman Bintang <normanbt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329283}

--- ash/system/video_conference/bubble/bubble_view.cc
+++ ash/system/video_conference/bubble/bubble_view.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
+#include "ash/constants/ash_features.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/video_conference/bubble/return_to_app_panel.h"
 #include "ash/system/video_conference/bubble/set_camera_background_view.h"
 #include "ash/system/video_conference/bubble/set_value_effects_view.h"
+#include "ash/system/video_conference/bubble/title_view.h"
 #include "ash/system/video_conference/bubble/toggle_effects_view.h"
 #include "ash/system/video_conference/effects/video_conference_tray_effects_manager.h"
 #include "ash/system/video_conference/video_conference_tray_controller.h"
@@ -123,6 +125,9 @@ BubbleView::BubbleView(const InitParams& init_params,
 BubbleView::~BubbleView() = default;
 
 void BubbleView::AddedToWidget() {
+  if (features::IsVcTrayTitleHeaderEnabled()) {
+    AddChildView(std::make_unique<TitleView>());
+  }
   // `ReturnToAppPanel` resides in the top-level layout and isn't part of the
   // scrollable area (that can't be added until the `BubbleView` officially has
   // a parent widget).
