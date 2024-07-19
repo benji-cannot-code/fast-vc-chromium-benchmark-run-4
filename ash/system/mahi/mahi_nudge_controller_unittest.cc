841eeefe2681516ce25f8ecda12424d36f0660f5
Michael Wojcicka <mwoj@google.com>
2024-07-19 20:37:37 +0000

    Revert "Quick Answers available from kPendingDisclaimer"
    This reverts commit a950cad1936c47436e0b0ad7aaeb75f828a34f35.
    
    Reason for revert: b/354252170 outlines that linux-chromeos-chrome builders are failing due to MahiUiBrowserTests, and the culprit CL is the previous one before this. Reverting to avoid merge conflict with the culprit revert.
    
    Original change's description:
    > Quick Answers available from kPendingDisclaimer
    >
    > - Quick Answers capability can be available from kPendingDisclaimer.
    >   Update the consent status mapping and add tests.
    > - Updates comments and enum value name to make it clear about the
    >   pending state.
    >
    > Bug: b:340628526
    > Test: chromeos_components_unittests --gtest_filter=*QuickAnswers*
    > Change-Id: I49b2ae60fc86cde78282f239ad813de47ee8c535
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719569
    > Commit-Queue: Andre Le <leandre@chromium.org>
    > Reviewed-by: Andre Le <leandre@chromium.org>
    > Auto-Submit: Yuki Awano <yawano@google.com>
    > Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1330349}
    
    Bug: b:354252170
    Change-Id: Ie7076a410a27d69944f9c991a48c79fb08b1df24
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726818
    Auto-Submit: Michael Wojcicka <mwoj@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Owners-Override: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330487}

--- ash/system/mahi/mahi_nudge_controller_unittest.cc
+++ ash/system/mahi/mahi_nudge_controller_unittest.cc
@@ -121,7 +121,7 @@ TEST_F(MahiNudgeControllerTest, NudgeDoesNotShow_WhenConsentStatusSet) {
   test_magic_boost_state_.AsyncWriteHMREnabled(false);
 
   test_magic_boost_state_.AsyncWriteConsentStatus(
-      chromeos::HMRConsentStatus::kPendingDisclaimer);
+      chromeos::HMRConsentStatus::kPending);
 
   EXPECT_FALSE(IsMahiNudgeShown());
   nudge_controller()->MaybeShowNudge();
