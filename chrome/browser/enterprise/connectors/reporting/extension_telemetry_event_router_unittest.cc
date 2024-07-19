0c1c96d9629cd867e5323a8e007b9c4c1ce1216e
Shanthanu Bhardwaj <xanth@google.com>
2024-07-19 20:06:47 +0000

    [ExtensionTelemetryEvent] Check Opt-in events instead of default event types
    This requires the event to be explicitly selected in the UI rather than being enabled for all users who selected "enable all events" in the admin UI
    
    Change-Id: Ia0aa33ef6ef0770713e4c727bc4f6b8acd615a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726312
    Commit-Queue: Shanthanu Bhardwaj <xanth@google.com>
    Reviewed-by: Richard Chen <richche@google.com>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1330458}

--- chrome/browser/enterprise/connectors/reporting/extension_telemetry_event_router_unittest.cc
+++ chrome/browser/enterprise/connectors/reporting/extension_telemetry_event_router_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
+#include "components/enterprise/connectors/reporting/constants.h"
 #include "components/enterprise/connectors/reporting/reporting_service_settings.h"
 #include "components/policy/core/common/cloud/mock_cloud_policy_client.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -105,9 +106,9 @@ class ExtensionTelemetryEventRouterTest : public testing::Test {
 
     test::SetOnSecurityEventReporting(
         profile_->GetPrefs(), /*enabled=*/true,
-        /*enabled_event_names=*/std::set<std::string>(),
+        /*enabled_event_names=*/{},
         /*enabled_opt_in_events=*/
-        std::map<std::string, std::vector<std::string>>());
+        {{enterprise_connectors::kExtensionTelemetryEvent, {"*"}}});
     // Set a mock cloud policy client in the router.
     client_ = std::make_unique<policy::MockCloudPolicyClient>();
     client_->SetDMToken("fake-token");
@@ -306,7 +307,11 @@ TEST_F(ExtensionTelemetryEventRouterTest, CheckIsPolicyEnabled) {
   EXPECT_FALSE(extension_telemetry_event_router_->IsPolicyEnabled());
 
   // Enable reporting.
-  test::SetOnSecurityEventReporting(profile_->GetPrefs(), /*enabled=*/true);
+  test::SetOnSecurityEventReporting(
+      profile_->GetPrefs(), /*enabled=*/true,
+      /*enabled_event_names=*/{},
+      /*enabled_opt_in_events=*/
+      {{enterprise_connectors::kExtensionTelemetryEvent, {"*"}}});
 
   EXPECT_TRUE(extension_telemetry_event_router_->IsPolicyEnabled());
 }
--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_browsertest.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_browsertest.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "components/enterprise/connectors/reporting/constants.h"
 #include "components/metrics/content/subprocess_metrics_provider.h"
 #include "components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing/core/common/features.h"
@@ -93,6 +94,13 @@ class ExtensionTelemetryServiceBrowserTest
     extensions::ExtensionApiTest::SetUpOnMainThread();
     host_resolver()->AddRule("*", "127.0.0.1");
 
+    // Enable enterprise policy. Verify that enterprise reporting is enabled.
+    enterprise_connectors::test::SetOnSecurityEventReporting(
+        /*prefs=*/prefs(),
+        /*enabled=*/true,
+        /*enabled_event_names=*/{},
+        /*enabled_opt_in_events=*/
+        {{enterprise_connectors::kExtensionTelemetryEvent, {"*"}}});
     // Helper to set up enterprise reporting and enable by default.
     event_report_validator_helper_ = std::make_unique<
         enterprise_connectors::test::EventReportValidatorHelper>(
