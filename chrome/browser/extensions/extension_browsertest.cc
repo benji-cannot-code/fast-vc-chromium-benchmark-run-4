5194159efd2be8b7e78fdfb232605f349a631749
Solomon Kinard <solomonkinard@chromium.org>
2024-07-19 20:03:24 +0000

    Extensions: CodeHealth: Add curly braces as per the latest style guide
    Bug: chromium:346952289
    Change-Id: Ia5c9a119817f8a81456356e86981379314b4b3ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726019
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330457}

--- chrome/browser/extensions/extension_browsertest.cc
+++ chrome/browser/extensions/extension_browsertest.cc
@@ -409,10 +409,11 @@ void ExtensionBrowserTest::OnShutdown(ExtensionRegistry* registry) {
 
 Profile* ExtensionBrowserTest::profile() {
   if (!profile_) {
-    if (browser())
+    if (browser()) {
       profile_ = browser()->profile();
-    else
+    } else {
       profile_ = ProfileManager::GetLastUsedProfile();
+    }
   }
   return profile_;
 }
@@ -510,8 +511,9 @@ const Extension* ExtensionBrowserTest::LoadExtension(
     const base::FilePath& path,
     const LoadOptions& options) {
   base::FilePath extension_path;
-  if (!ModifyExtensionIfNeeded(options, path, &extension_path))
+  if (!ModifyExtensionIfNeeded(options, path, &extension_path)) {
     return nullptr;
+  }
 
   if (options.load_as_component) {
     // TODO(crbug.com/40166157): Decide if other load options
@@ -543,8 +545,9 @@ const Extension* ExtensionBrowserTest::LoadExtension(
 
   scoped_refptr<const Extension> extension =
       loader.LoadExtension(extension_path);
-  if (extension)
+  if (extension) {
     last_loaded_extension_id_ = extension->id();
+  }
 
   if (options.wait_for_registration_stored &&
       BackgroundInfo::IsServiceWorkerBased(extension.get())) {
@@ -569,8 +572,9 @@ const Extension* ExtensionBrowserTest::LoadExtensionAsComponentWithManifest(
       extension_service()->component_loader()->Add(manifest, path);
   const Extension* extension =
       extension_registry()->enabled_extensions().GetByID(extension_id);
-  if (!extension)
+  if (!extension) {
     return nullptr;
+  }
   last_loaded_extension_id_ = extension->id();
   return extension;
 }
@@ -749,8 +753,9 @@ const Extension* ExtensionBrowserTest::InstallOrUpdateExtension(
     if (crx_path.Extension() != FILE_PATH_LITERAL(".crx")) {
       crx_path = PackExtension(path, ExtensionCreator::kNoRunFlags);
     }
-    if (crx_path.empty())
+    if (crx_path.empty()) {
       return nullptr;
+    }
 
     std::unique_ptr<ExtensionInstallPrompt> install_ui;
     if (prompt_auto_confirm) {
@@ -907,8 +912,9 @@ void ExtensionBrowserTest::OpenWindow(content::WebContents* contents,
               newtab->GetPrimaryMainFrame()->GetSiteInstance());
   }
 
-  if (newtab_result)
+  if (newtab_result) {
     *newtab_result = newtab;
+  }
 }
 
 bool ExtensionBrowserTest::NavigateInRenderer(content::WebContents* contents,
@@ -1004,8 +1010,9 @@ bool ExtensionBrowserTest::ModifyExtensionIfNeeded(
   }
 
   base::FilePath extension_root;
-  if (!CreateTempDirectoryCopy(temp_dir, input_path, &extension_root))
+  if (!CreateTempDirectoryCopy(temp_dir, input_path, &extension_root)) {
     return false;
+  }
 
   std::string error;
   std::optional<base::Value::Dict> manifest_dict =
