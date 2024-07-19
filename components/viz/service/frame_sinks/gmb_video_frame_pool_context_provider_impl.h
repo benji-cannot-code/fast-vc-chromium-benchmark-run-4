2a2885e31de0b9d4434479db0f2a8f216216be09
vikas soni <vikassoni@chromium.org>
2024-07-19 00:37:58 +0000

    [MappableSI] Add a new method RenderableGpuMemoryBufferVideoFramePool to create a MappableSI.
    Prepare RenderableGpuMemoryBufferVideoFramePool to work with MappableSI.
    Add a new method
    RenderableGpuMemoryBufferVideoFramePool::CreateSharedImage() which
    creates a Mappable shared image. This method will be used to create
    MappableSI instead of GpuMemoryBuffer in future CLs.
    
    Implement this method for all derived classes except tests. It will be
    implemented for tests later when MappableSI is actually enabled in
    RenderableGpuMemoryBufferVideoFramePool and is being tested.
    
    Also wire GpuMemoryBufferFactory to GmbVideoFramePoolContextProvider and
    its implementation to be able create a GMB handle directly.
    
    Bug: 40263579
    Change-Id: I51082e975aed4938b8f9e5ccafc95d91b23d26c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5667969
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329914}

--- components/viz/service/frame_sinks/gmb_video_frame_pool_context_provider_impl.h
+++ components/viz/service/frame_sinks/gmb_video_frame_pool_context_provider_impl.h
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/service/viz_service_export.h"
 #include "media/video/renderable_gpu_memory_buffer_video_frame_pool.h"
 
+namespace gpu {
+class GpuMemoryBufferFactory;
+}  // namespace gpu
+
 namespace viz {
 
 class GpuServiceImpl;
@@ -22,7 +26,8 @@ class VIZ_SERVICE_EXPORT GmbVideoFramePoolContextProviderImpl
  public:
   explicit GmbVideoFramePoolContextProviderImpl(
       GpuServiceImpl* gpu_service,
-      InProcessGpuMemoryBufferManager* gpu_memory_buffer_manager);
+      InProcessGpuMemoryBufferManager* gpu_memory_buffer_manager,
+      gpu::GpuMemoryBufferFactory* gpu_memory_buffer_factory);
 
   GmbVideoFramePoolContextProviderImpl(
       const GmbVideoFramePoolContextProviderImpl& other) = delete;
@@ -37,6 +42,7 @@ class VIZ_SERVICE_EXPORT GmbVideoFramePoolContextProviderImpl
  private:
   const raw_ptr<GpuServiceImpl> gpu_service_;
   const raw_ptr<InProcessGpuMemoryBufferManager> gpu_memory_buffer_manager_;
+  const raw_ptr<gpu::GpuMemoryBufferFactory> gpu_memory_buffer_factory_;
 };
 
 }  // namespace viz
