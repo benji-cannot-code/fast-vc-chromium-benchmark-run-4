90515dc1dcf131816f46dbc7ce47f3b9f0abf4f6
Helmut Januschka <helmut@januschka.com>
2024-07-19 20:44:32 +0000

    Remove string_piece.h
    Bug: 40506050
    Change-Id: Icff4561ee08806b40e80a60015d6c5fa7b47caa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5696946
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330492}

--- tools/clang/rewrite_raw_ptr_fields/tests/affected-expr-expected.cc
+++ tools/clang/rewrite_raw_ptr_fields/tests/affected-expr-expected.cc
@@ -445,11 +445,9 @@ void foo() {
 
 namespace implicit_constructors {
 
-// Based on //base/strings/string_piece.h:
 template <typename CharT>
 class BasicStringPiece;
 typedef BasicStringPiece<char> StringPiece;
-// Based on //base/strings/string_piece.h:
 template <typename CharT>
 class BasicStringPiece {
  public:
@@ -488,11 +486,9 @@ void foo() {
 
 namespace implicit_constructors_raw_ref_tests {
 
-// Based on //base/strings/string_piece.h:
 template <typename CharT>
 class BasicStringPiece;
 typedef BasicStringPiece<char> StringPiece;
-// Based on //base/strings/string_piece.h:
 template <typename CharT>
 class BasicStringPiece {
  public:
