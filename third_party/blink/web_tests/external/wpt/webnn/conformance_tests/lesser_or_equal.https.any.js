807f0f21d991daaa64de97e9c6d9fdd5392c4e20
Phillis Tang <phillis@chromium.org>
2024-07-19 01:06:09 +0000

    webnn: implement logicalNot on coreml
    Also fix WPT helpers to check opSupportLimits for output type, and
    cast to supported output type if current op's output type is not
    supported.
    
    Fixing WPTs causes it to take longer to run elementwise-logical tests
    that it needs to be splitted up to individual tests to avoid timeouts.
    
    Bug: 41481333
    Change-Id: I33882af4264bb2f5594224d495e511e2ad6ce3a1
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel,mac14.arm64-blink-rel,mac14-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719110
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329923}

---
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/lesser_or_equal.https.any.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=test WebNN API element-wise binary operations
+// META: global=window,dedicatedworker
+// META: variant=?cpu
+// META: variant=?gpu
+// META: variant=?npu
+// META: script=../resources/utils.js
+// META: timeout=long
+
+'use strict';
+
+// https://webmachinelearning.github.io/webnn/#dom-mlgraphbuilder-lesserorequal
+
+runWebNNConformanceTests('lesserOrEqual', buildOperationWithTwoInputs);
