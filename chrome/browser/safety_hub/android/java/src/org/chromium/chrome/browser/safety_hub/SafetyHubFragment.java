77864759329c5956e2c96a638365d0db9be798e7
Zaina Al-Mashni <zalmashni@google.com>
2024-07-19 14:58:09 +0000

    [SHoA] Update password count on sign-in/out
    Destroy/Re-create the PasswordStoreBridge on sign-out/sign-in and
    remove/add the observer accordingly. This allows us to get a correct
    count of the passwords in the account store and update the module with
    the up-to-date state when the user signs in through the Safety Hub page.
    
    Bug: 324562205
    Change-Id: I4eafd0fdedef6072ef0c3bde0db9a96c4be8de52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725932
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330240}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubFragment.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubFragment.java
@@ -106,7 +106,6 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
         mNotificationPermissionReviewBridge =
                 NotificationPermissionReviewBridge.getForProfile(getProfile());
         mSafetyHubFetchService = SafetyHubFetchServiceFactory.getForProfile(getProfile());
-        mPasswordStoreBridge = new PasswordStoreBridge(getProfile());
         mSigninManager = IdentityServicesProvider.get().getSigninManager(getProfile());
 
         setUpAccountPasswordCheckModule();
@@ -168,7 +167,10 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
                 SafetyHubModuleViewBinder::bindPasswordCheckProperties);
         mSafetyHubFetchService.addObserver(this);
         mSigninManager.addSignInStateObserver(this);
-        mPasswordStoreBridge.addObserver(this, true);
+        if (mDelegate.isSignedIn()) {
+            mPasswordStoreBridge = new PasswordStoreBridge(getProfile());
+            mPasswordStoreBridge.addObserver(this, true);
+        }
     }
 
     private void setUpUpdateCheckModule() {
@@ -392,7 +394,9 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
         mUnusedSitePermissionsBridge.removeObserver(this);
         mSafetyHubFetchService.removeObserver(this);
         mSigninManager.removeSignInStateObserver(this);
-        mPasswordStoreBridge.removeObserver(this);
+        if (mPasswordStoreBridge != null) {
+            mPasswordStoreBridge.removeObserver(this);
+        }
     }
 
     @Override
@@ -425,11 +429,19 @@ public class SafetyHubFragment extends SafetyHubBaseFragment
 
     @Override
     public void onSignedIn() {
+        if (mPasswordStoreBridge == null) {
+            mPasswordStoreBridge = new PasswordStoreBridge(getProfile());
+            mPasswordStoreBridge.addObserver(this, true);
+        }
         updatePasswordCheckPreference();
     }
 
     @Override
     public void onSignedOut() {
+        if (mPasswordStoreBridge != null) {
+            mPasswordStoreBridge.removeObserver(this);
+            mPasswordStoreBridge = null;
+        }
         updatePasswordCheckPreference();
     }
 
--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegateImpl.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubModuleDelegateImpl.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import org.chromium.base.supplier.Supplier;
 import org.chromium.build.BuildConfig;
@@ -74,7 +75,7 @@ public class SafetyHubModuleDelegateImpl implements SafetyHubModuleDelegate {
     }
 
     @Override
-    public UpdateStatusProvider.UpdateStatus getUpdateStatus() {
+    public @Nullable UpdateStatusProvider.UpdateStatus getUpdateStatus() {
         return SafetyHubFetchServiceFactory.getForProfile(mProfile).getUpdateStatus();
     }
 
@@ -104,10 +105,11 @@ public class SafetyHubModuleDelegateImpl implements SafetyHubModuleDelegate {
     }
 
     @Override
-    public int getAccountPasswordsCount(PasswordStoreBridge passwordStoreBridge) {
+    public int getAccountPasswordsCount(@Nullable PasswordStoreBridge passwordStoreBridge) {
         PasswordManagerHelper passwordManagerHelper = PasswordManagerHelper.getForProfile(mProfile);
         SyncService syncService = SyncServiceFactory.getForProfile(mProfile);
-        if (!PasswordManagerHelper.hasChosenToSyncPasswords(syncService)
+        if (passwordStoreBridge == null
+                || !PasswordManagerHelper.hasChosenToSyncPasswords(syncService)
                 || !passwordManagerHelper.canUseUpm()) return INVALID_PASSWORD_COUNT;
 
         if (usesSplitStoresAndUPMForLocal(UserPrefs.get(mProfile))) {
@@ -151,7 +153,7 @@ public class SafetyHubModuleDelegateImpl implements SafetyHubModuleDelegate {
     }
 
     @Override
-    public String getAccountEmail() {
+    public @Nullable String getAccountEmail() {
         SigninManager signinManager = IdentityServicesProvider.get().getSigninManager(mProfile);
         return CoreAccountInfo.getEmailFrom(
                 signinManager.getIdentityManager().getPrimaryAccountInfo(ConsentLevel.SIGNIN));
