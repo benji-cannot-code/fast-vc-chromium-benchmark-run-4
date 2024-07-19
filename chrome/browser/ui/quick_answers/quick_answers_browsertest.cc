fedadf2117e6d8423f00a7bbe5a19c8720a19c11
Andre Le <leandre@chromium.org>
2024-07-19 06:14:26 +0000

    MagicBoost: Parameterize QuickAnswers browser tests for the feature
    Parameterize the Quick Answers browsers tests to test the feature in
    both cases when Magic Boost is on or off.
    
    Bug: b:348016821
    Change-Id: Id461390d75cb9f87748dfca11ffa85d17acfb4e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719750
    Reviewed-by: Yuki Awano <yawano@google.com>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330059}

--- chrome/browser/ui/quick_answers/quick_answers_browsertest.cc
+++ chrome/browser/ui/quick_answers/quick_answers_browsertest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/quick_answers/ui/user_consent_view.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chrome/test/base/in_process_browser_test.h"
+#include "chromeos/components/magic_boost/public/cpp/magic_boost_state.h"
 #include "chromeos/components/quick_answers/public/cpp/controller/quick_answers_controller.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_prefs.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_state.h"
@@ -276,10 +277,18 @@ std::unique_ptr<QuickAnswersSession> CreateQuickAnswerUnitConversionResponse() {
 
 class QuickAnswersBrowserTest : public QuickAnswersBrowserTestBase {
  protected:
-  // This simulates a behavior where a user enables QuickAnswers from Settings.
   void SetQuickAnswersEnabled(bool enabled) {
-    chrome_test_utils::GetProfile(this)->GetPrefs()->SetBoolean(
-        prefs::kQuickAnswersEnabled, enabled);
+    if (IsMagicBoostEnabled()) {
+      // Approve HMRConsentStatus to bypass opt-in flow.
+      chromeos::MagicBoostState::Get()->AsyncWriteConsentStatus(
+          chromeos::HMRConsentStatus::kApproved);
+      chromeos::MagicBoostState::Get()->AsyncWriteHMREnabled(true);
+    } else {
+      // This simulates a behavior where a user enables QuickAnswers from
+      // Settings.
+      chrome_test_utils::GetProfile(this)->GetPrefs()->SetBoolean(
+          prefs::kQuickAnswersEnabled, enabled);
+    }
   }
 
   void SendTestImageNotification() {
@@ -350,7 +359,7 @@ class QuickAnswersBrowserTest : public QuickAnswersBrowserTestBase {
   base::TimeTicks fake_time_tick_;
 };
 
-IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(QuickAnswersBrowserTest,
                        QuickAnswersViewAboveNotification) {
   SetQuickAnswersEnabled(true);
 
@@ -382,8 +391,12 @@ IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
       quick_answers_view_widget->GetNativeView()));
 }
 
-IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(QuickAnswersBrowserTest,
                        UserConsentViewAboveNotification) {
+  if (IsMagicBoostEnabled()) {
+    GTEST_SKIP() << "This test only applies when Magic Boost is disabled.";
+  }
+
   // User consent view is stored within the `ReadWriteCardsUiController`'s
   // widget.
   views::NamedWidgetShownWaiter user_consent_view_widget_waiter(
@@ -425,7 +438,11 @@ IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
       user_consent_view_widget->GetNativeView()));
 }
 
-IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest, UserConsentViewImpressionCap) {
+IN_PROC_BROWSER_TEST_P(QuickAnswersBrowserTest, UserConsentViewImpressionCap) {
+  if (IsMagicBoostEnabled()) {
+    GTEST_SKIP() << "This test only applies when Magic Boost is disabled.";
+  }
+
   FakeControllerTimeTick();
 
   for (int i = 0; i < kConsentImpressionCap; ++i) {
@@ -456,7 +473,11 @@ IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest, UserConsentViewImpressionCap) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest, ClickAllowOnUserConsentView) {
+IN_PROC_BROWSER_TEST_P(QuickAnswersBrowserTest, ClickAllowOnUserConsentView) {
+  if (IsMagicBoostEnabled()) {
+    GTEST_SKIP() << "This test only applies when Magic Boost is disabled.";
+  }
+
   // User consent view is stored within the `ReadWriteCardsUiController`'s
   // widget.
   views::NamedWidgetShownWaiter user_consent_view_widget_waiter(
@@ -493,8 +514,12 @@ IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest, ClickAllowOnUserConsentView) {
             controller()->GetQuickAnswersVisibility());
 }
 
-IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(QuickAnswersBrowserTest,
                        ClickNoThanksOnUserConsentView) {
+  if (IsMagicBoostEnabled()) {
+    GTEST_SKIP() << "This test only applies when Magic Boost is disabled.";
+  }
+
   // User consent view is stored within the `ReadWriteCardsUiController`'s
   // widget.
   views::NamedWidgetShownWaiter user_consent_view_widget_waiter(
@@ -528,6 +553,11 @@ IN_PROC_BROWSER_TEST_F(QuickAnswersBrowserTest,
             controller()->GetQuickAnswersVisibility());
 }
 
+INSTANTIATE_TEST_SUITE_P(
+    /* no prefix */,
+    QuickAnswersBrowserTest,
+    ::testing::Bool());
+
 class RichAnswersBrowserTest : public QuickAnswersBrowserTest {
  protected:
   void SetUpOnMainThread() override {
@@ -560,7 +590,7 @@ class RichAnswersBrowserTest : public QuickAnswersBrowserTest {
       chromeos::features::kQuickAnswersRichCard};
 };
 
-IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(RichAnswersBrowserTest,
                        RichAnswersNotTriggeredOnInvalidResult) {
   views::Widget* quick_answers_view_widget = ShowQuickAnswersWidget();
 
@@ -578,7 +608,7 @@ IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
               QuickAnswersVisibility::kClosed);
 }
 
-IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(RichAnswersBrowserTest,
                        RichAnswersTriggeredAndDismissedOnValidResult) {
   views::Widget* quick_answers_view_widget = ShowQuickAnswersWidget();
 
@@ -608,7 +638,7 @@ IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
               QuickAnswersVisibility::kClosed);
 }
 
-IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(RichAnswersBrowserTest,
                        DefinitionResultCardContentsCorrectlyShown) {
   views::Widget* quick_answers_view_widget = ShowQuickAnswersWidget();
 
@@ -638,7 +668,7 @@ IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
   // TODO(b/326370198): Add checks for other card contents.
 }
 
-IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(RichAnswersBrowserTest,
                        TranslationResultCardContentsCorrectlyShown) {
   views::Widget* quick_answers_view_widget = ShowQuickAnswersWidget();
 
@@ -668,7 +698,7 @@ IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
   // TODO(b/326370198): Add checks for other card contents.
 }
 
-IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
+IN_PROC_BROWSER_TEST_P(RichAnswersBrowserTest,
                        UnitConversionResultCardContentsCorrectlyShown) {
   views::Widget* quick_answers_view_widget = ShowQuickAnswersWidget();
 
@@ -697,5 +727,10 @@ IN_PROC_BROWSER_TEST_F(RichAnswersBrowserTest,
 
   // TODO(b/326370198): Add checks for other card contents.
 }
+
+INSTANTIATE_TEST_SUITE_P(
+    /* no prefix */,
+    RichAnswersBrowserTest,
+    ::testing::Bool());
 
 }  // namespace quick_answers
--- chrome/browser/ui/quick_answers/quick_answers_browsertest_base.cc
+++ chrome/browser/ui/quick_answers/quick_answers_browsertest_base.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "chrome/test/base/chrome_test_utils.h"
 #include "chromeos/components/quick_answers/public/cpp/quick_answers_state.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
@@ -24,11 +25,19 @@ constexpr char kDataUrlTemplate[] =
 
 }  // namespace
 
-QuickAnswersBrowserTestBase::QuickAnswersBrowserTestBase() = default;
+QuickAnswersBrowserTestBase::QuickAnswersBrowserTestBase() {
+  scoped_feature_list_.InitWithFeatureState(chromeos::features::kMagicBoost,
+                                            IsMagicBoostEnabled());
+}
+
 QuickAnswersBrowserTestBase::~QuickAnswersBrowserTestBase() = default;
 
 void QuickAnswersBrowserTestBase::SetUpOnMainThread() {
   QuickAnswersState::Get()->SetEligibilityForTesting(true);
+}
+
+bool QuickAnswersBrowserTestBase::IsMagicBoostEnabled() const {
+  return GetParam();
 }
 
 // `ShowMenu` generates a web page with `params.selected_text` at a position of
