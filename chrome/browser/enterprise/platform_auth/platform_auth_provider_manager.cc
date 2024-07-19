adcd66f19e6ba51b3e9f9bde0c80b60a02362c5e
Yann Dago <ydago@chromium.org>
2024-07-19 15:04:51 +0000

    Add support for Enterprise Extensible SSO on Mac
    This CL adds support for Enterprise Extensible SSO on Mac.
    This feature is still experimental only.
    We extend the PlatformAuthManager's support to Mac by adding a new
    provider.
    The Mac implementation differs from the Windows one here since we
    do not need to get any origins nor individually checking if a url
    is supported because the OS does that for us.
    This feature will not be released before a proper design review, but
    this CL is to be able to test it.
    
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Bug: 340868357
    Change-Id: I16c82816de8bc363798d98dd7b13f4b9e3a29939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542759
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330249}

--- chrome/browser/enterprise/platform_auth/platform_auth_provider_manager.cc
+++ chrome/browser/enterprise/platform_auth/platform_auth_provider_manager.cc
@@ -37,6 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_WIN)
 #include "chrome/browser/enterprise/platform_auth/cloud_ap_provider_win.h"
+#elif BUILDFLAG(IS_MAC)
+#include "chrome/browser/enterprise/platform_auth/extensible_enterprise_sso_provider_mac.h"
 #endif
 
 namespace enterprise_auth {
@@ -46,6 +48,8 @@ namespace {
 std::unique_ptr<PlatformAuthProvider> MakeProvider() {
 #if BUILDFLAG(IS_WIN)
   return std::make_unique<CloudApProviderWin>();
+#elif BUILDFLAG(IS_MAC)
+  return std::make_unique<ExtensibleEnterpriseSSOProvider>();
 #else
   return nullptr;
 #endif
@@ -73,8 +77,13 @@ void PlatformAuthProviderManager::SetEnabled(bool enabled,
 
   enabled_ = enabled;
 
-  on_enable_complete_ = std::move(on_complete);
-  StartFetchOrigins();
+  if (supports_origin_filtering_) {
+    on_enable_complete_ = std::move(on_complete);
+    StartFetchOrigins();
+  } else if (on_complete) {
+    base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
+        FROM_HERE, std::move(on_complete));
+  }
 
   // TODO(crbug.com/40196687): Users may add/remove WebAccounts, which could
   // change the set of origins. Consider polling on a low-frequency timer and/or
@@ -89,7 +98,9 @@ bool PlatformAuthProviderManager::IsEnabled() const {
 
 bool PlatformAuthProviderManager::IsEnabledFor(const GURL& url) const {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  return base::Contains(origins_, url::Origin::Create(url));
+
+  return !supports_origin_filtering_ ||
+         base::Contains(origins_, url::Origin::Create(url));
 }
 
 void PlatformAuthProviderManager::GetData(const GURL& url,
@@ -115,12 +126,14 @@ PlatformAuthProviderManager::PlatformAuthProviderManager()
 
 PlatformAuthProviderManager::PlatformAuthProviderManager(
     std::unique_ptr<PlatformAuthProvider> provider)
-    : provider_(std::move(provider)) {}
+    : provider_(std::move(provider)),
+      supports_origin_filtering_(provider_->SupportsOriginFiltering()) {}
 
 PlatformAuthProviderManager::~PlatformAuthProviderManager() = default;
 
 void PlatformAuthProviderManager::StartFetchOrigins() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  CHECK(supports_origin_filtering_);
 
   if (enabled_ && provider_) {
     provider_->FetchOrigins(base::BindOnce(
@@ -133,6 +146,7 @@ void PlatformAuthProviderManager::StartFetchOrigins() {
 void PlatformAuthProviderManager::OnOrigins(
     std::unique_ptr<std::vector<url::Origin>> origins) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  CHECK(supports_origin_filtering_);
 
   base::flat_set<url::Origin> new_origins;
 
@@ -155,6 +169,7 @@ void PlatformAuthProviderManager::OnOrigins(
 std::unique_ptr<PlatformAuthProvider>
 PlatformAuthProviderManager::SetProviderForTesting(
     std::unique_ptr<PlatformAuthProvider> provider) {
+  supports_origin_filtering_ = provider->SupportsOriginFiltering();
   return std::exchange(provider_, std::move(provider));
 }
 
