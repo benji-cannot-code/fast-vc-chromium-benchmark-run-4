b10ec4be62f740f3a852e1cf288d4f5757787360
Erik Chen <erikchen@chromium.org>
2024-07-19 02:09:56 +0000

    Fix build graph for //chrome/browser.
    gn targets which allow circular includes with //chrome/browser:browser
    are (from a build graph perspective) part of the same logical unit. This
    means that all public dependencies must be shared. This was previously
    documented in comments in gn, but not enforced. This CL updates the
    comments and fixes all circular includes (except for those from
    is_chromeos_ash, which are numerous and will be tackled later).
    
    This CL creates a new target //chrome/browser/ui:ui_public_dependencies
    which captures all public dependencies of //chrome/browser/ui. This
    target is depended on by //chrome/browser:browser_public_dependencies.
    All circular includes with both //chrome/browser:browser and
    //chrome/browser/ui:ui are updated to publicly depend on
    //chrome/browser:browser_public_dependencies.
    
    Minor fixes include:
    * //chrome/browser/ui now specifies a missing dependency:
      //components/feed/core/proto:proto_ios_shared_v2.
    * chrome/browser/policy/messaging_layer/storage_selector
      was marked as a component despite circular dependencies on
      //chrome/browser, which is not a component. storage_selector was
      modified to stop being a component.
    
    Bug: 353628440
    Change-Id: Ibeaa1f503fcd289ec94da9e666907f4f42ce85bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719690
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1329963}

--- chrome/browser/policy/messaging_layer/storage_selector/storage_selector.h
+++ chrome/browser/policy/messaging_layer/storage_selector/storage_selector.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_POLICY_MESSAGING_LAYER_STORAGE_SELECTOR_STORAGE_SELECTOR_H_
 #define CHROME_BROWSER_POLICY_MESSAGING_LAYER_STORAGE_SELECTOR_STORAGE_SELECTOR_H_
 
-#include "base/component_export.h"
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
 #include "components/reporting/storage/storage_module_interface.h"
@@ -35,7 +34,7 @@ namespace reporting {
 // that case it always connects to Missive Daemon.
 // This class is never instantiated; it serves as a front for the client
 // configuration settings according to the build.
-class COMPONENT_EXPORT(STORAGE_SELECTOR) StorageSelector {
+class StorageSelector {
  public:
   static bool is_use_missive();
   static bool is_uploader_required();
