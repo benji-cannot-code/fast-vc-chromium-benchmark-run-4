8eec9d798816c41137fc0d1dca39f08c4068d827
Jiacheng Guo <gjc@google.com>
2024-07-19 06:28:11 +0000

    gardening: Disable failing test LensOverlayTest.CursorTooltip
    Bug: 354077610
    Change-Id: I8def54e71012d72594069ccbae468fc76d25a95b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5723951
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330061}

--- chrome/test/data/webui/lens/lens_webui_browsertest.cc
+++ chrome/test/data/webui/lens/lens_webui_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/test/run_until.h"
+#include "build/build_config.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/lens/lens_overlay_controller.h"
@@ -157,7 +158,12 @@ IN_PROC_BROWSER_TEST_F(LensOverlayTest, CubicBezier) {
   RunOverlayTest("lens/overlay/cubic_bezier_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(LensOverlayTest, CursorTooltip) {
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_CursorTooltip DISABLED_CursorTooltip
+#else
+#define MAYBE_CursorTooltip CursorTooltip
+#endif
+IN_PROC_BROWSER_TEST_F(LensOverlayTest, MAYBE_CursorTooltip) {
   RunOverlayTest("lens/overlay/cursor_tooltip_test.js", "mocha.run()");
 }
 
