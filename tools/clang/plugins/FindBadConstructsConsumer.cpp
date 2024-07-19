cbf0320e540605814b04af2e9f2d3867ec12a7cc
Takashi Sakamoto <tasak@google.com>
2024-07-19 03:02:54 +0000

    [clang-plugin] Remove raw_ptr code from FindBadConstructs plugin.
    Since raw_ptr code was moved into //tools/clang/raw_ptr_plugin,
    i.e. raw_ptr plugin, and //build/config/clang/BUILD.gn was modified
    to use raw_ptr plugin for raw_ptr checks, remove raw_ptr code from FindBadConsructs plugin.
    
    Bug: b/40945579
    
    Change-Id: I33bdcbe404dd74e2d3ec932157b5d1d69fa485d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5637255
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330000}

--- tools/clang/plugins/FindBadConstructsConsumer.cpp
+++ tools/clang/plugins/FindBadConstructsConsumer.cpp
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FindBadConstructsConsumer.h"
 
-#include "FindBadRawPtrPatterns.h"
 #include "Util.h"
 #include "clang/AST/Attr.h"
 #include "clang/Frontend/CompilerInstance.h"
@@ -269,13 +268,6 @@ void FindBadConstructsConsumer::Traverse(ASTContext& context) {
 
   if (ipc_visitor_) {
     ipc_visitor_->set_context(nullptr);
-  }
-
-  {
-    llvm::TimeTraceScope TimeScope(
-        "FindBadRawPtrPatterns in "
-        "FindBadConstructsConsumer::Traverse");
-    FindBadRawPtrPatterns(options_, context, instance());
   }
 }
 
