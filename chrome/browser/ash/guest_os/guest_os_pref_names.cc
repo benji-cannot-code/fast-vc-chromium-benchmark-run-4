024dcf88befa3ace01c0f317f8f3ca2706e2db3f
maciek swiech <drmasquatch@google.com>
2024-07-19 15:19:46 +0000

    Allow manual disabling of USB 'share with X' notifications.
    With more USB devices now being available for passthrough (see go/lofn),
    some users have reported complaints that they receive too many 'share
    with Linux/Android/etc' notifications upon laptop wake, connecting to
    docks, etc (really any time the kernel/udev enumerate USB devices).
    
    Currently the only way to disable these notifications is via policy,
    which 1.  is at far too broad granularity 2. only available to managed
    users anyhow.
    
    Therefore we add an option to disable these notifications from being
    displayed.  Note that this does not in any way prevent a user from
    passing these devices through to the guest of their choice, it simply
    prevents the notification from appearing. Since the default behavior is
    for notifications to exist, we default the relevant Chrome pref to true.
    
    Unfortunately there exists no top-level settings page for managing
    behaviors of various virtualized guests, coupled with
    guests-on-ChromeOS-classic being in a largely KTLO state, we have opted
    to share the notification state in one pref, mirrored across the
    settings pages of each guest.
    
    Screenshot: https://screenshot.googleplex.com/ABgBKsqZjNhGyo2
    
    Test: deploy to DUT, plug in notification-causing USB device with setting on and off
    Bug: 347877187
    Change-Id: Ie7afba211868be40891579f94adf490cde1b350e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5688497
    Commit-Queue: maciek swiech <drmasquatch@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Chris Bookholt <bookholt@google.com>
    Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330255}

--- chrome/browser/ash/guest_os/guest_os_pref_names.cc
+++ chrome/browser/ash/guest_os/guest_os_pref_names.cc
@@ -18,6 +18,7 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterDictionaryPref(
       kGuestOsTerminalSettings,
       user_prefs::PrefRegistrySyncable::SYNCABLE_OS_PREF);
+  registry->RegisterBooleanPref(kGuestOsUSBNotificationEnabled, true);
 }
 
 }  // namespace guest_os::prefs
--- chrome/browser/ash/usb/cros_usb_detector.cc
+++ chrome/browser/ash/usb/cros_usb_detector.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crostini/crostini_pref_names.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/guest_os/guest_id.h"
+#include "chrome/browser/ash/guest_os/guest_os_pref_names.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_features.h"
 #include "chrome/browser/ash/plugin_vm/plugin_vm_util.h"
 #include "chrome/browser/notifications/system_notification_helper.h"
@@ -524,7 +525,8 @@ void CrosUsbDetector::ConnectToDeviceManager() {
 
 bool CrosUsbDetector::ShouldShowNotification(const UsbDevice& device) {
   PrefService* prefs = profile()->GetPrefs();
-  if (!prefs->GetBoolean(ash::prefs::kUsbDetectorNotificationEnabled)) {
+  if (!prefs->GetBoolean(ash::prefs::kUsbDetectorNotificationEnabled) ||
+      !prefs->GetBoolean(guest_os::prefs::kGuestOsUSBNotificationEnabled)) {
     return false;
   }
 
