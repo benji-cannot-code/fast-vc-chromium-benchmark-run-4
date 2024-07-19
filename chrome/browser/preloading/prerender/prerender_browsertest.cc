ac5a45e9c200af4f926f8dfb9ebe411dd39625e5
HuanPo Lin <robertlin@chromium.org>
2024-07-19 03:17:12 +0000

    preload: Re-enable NewTabPagePrerenderNonHttps
    This CL re-enables
    PrerenderNewTabPageBrowserTest.NewTabPagePrerenderNonHttps, since the
    problem should be fixed by
    https://chromium-review.googlesource.com/c/chromium/src/+/5691239
    
    Bug: 345183784
    Change-Id: Ib1ba0f19cd1eb772f6e15c91beb18f0f5c5fdbcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717075
    Commit-Queue: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330004}

--- chrome/browser/preloading/prerender/prerender_browsertest.cc
+++ chrome/browser/preloading/prerender/prerender_browsertest.cc
@@ -868,7 +868,7 @@ IN_PROC_BROWSER_TEST_P(
 
 // Verify that NewTabPage prerender rejects non https url.
 IN_PROC_BROWSER_TEST_P(PrerenderNewTabPageBrowserTest,
-                       DISABLED_NewTabPagePrerenderNonHttps) {
+                       NewTabPagePrerenderNonHttps) {
   // Navigate to an initial page.
   ASSERT_TRUE(content::NavigateToURL(GetActiveWebContents(),
                                      GURL(chrome::kChromeUINewTabURL)));
