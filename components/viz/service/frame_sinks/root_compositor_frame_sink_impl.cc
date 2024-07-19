f542e764371fa9f5b32b034696c81ba7e5e2a375
Maggie Chen <magchen@chromium.org>
2024-07-19 21:03:23 +0000

    Fix Segv on unknown address in viz::ExternalBeginFrameSourceMac::SetMultipleHWRefreshRatesCallback
    This is a ASAN bug reported by compositor_frame_fuzzer.
    
    This fault was caused by a dereference of a high value address, and
    the fix is to track |created_external_begin_frame_source_mac| for the static_cast from ExternalBeginFrameSource*
    to ExternalBeginFrameSourceMac*.
    
    Bug: 353928344
    Change-Id: I16dabca8c6a4e7afac6024c41953147c55966947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726576
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Auto-Submit: Maggie Chen <magchen@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330505}

--- components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
+++ components/viz/service/frame_sinks/root_compositor_frame_sink_impl.cc
@@ -124,6 +124,9 @@ RootCompositorFrameSinkImpl::Create(
   std::unique_ptr<SyntheticBeginFrameSource> synthetic_begin_frame_source;
   ExternalBeginFrameSourceMojo* external_begin_frame_source_mojo = nullptr;
   bool hw_support_for_multiple_refresh_rates = false;
+#if BUILDFLAG(IS_MAC)
+  bool created_external_begin_frame_source_mac = false;
+#endif
 #if !BUILDFLAG(IS_APPLE)
   bool wants_vsync_updates = false;
 #endif
@@ -171,6 +174,7 @@ RootCompositorFrameSinkImpl::Create(
             std::make_unique<ExternalBeginFrameSourceMac>(
                 restart_id, params->renderer_settings.display_id,
                 output_surface.get());
+        created_external_begin_frame_source_mac = true;
       } else {
         auto time_source = std::make_unique<DelayBasedTimeSource>(
             base::SingleThreadTaskRunner::GetCurrentDefault().get());
@@ -257,7 +261,7 @@ RootCompositorFrameSinkImpl::Create(
             &RootCompositorFrameSinkImpl::SetDisplayVSyncParameters,
             base::Unretained(impl.get())));
 
-    if (features::IsCVDisplayLinkBeginFrameSourceEnabled()) {
+    if (created_external_begin_frame_source_mac) {
       static_cast<ExternalBeginFrameSourceMac*>(
           impl->external_begin_frame_source())
           ->SetMultipleHWRefreshRatesCallback(base::BindRepeating(
