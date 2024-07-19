069ae56306c7fa969d6f502fb116d18380a98514
Gauthier Ambard <gambard@chromium.org>
2024-07-19 11:56:06 +0000

    [iOS] Fix inactive tabs button visibility
    The button should be visible when both the count and the threshold are
    correct.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL refactor of existing feature
    Fixed: 352722446
    Change-Id: I8ef43fcab6585210521815b77ebbd3907f5c876f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724689
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330166}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
@@ -188,8 +188,7 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
   _inactiveTabsCount = count;
 
   if (IsInactiveTabButtonRefactoringEnabled()) {
-    BOOL inactiveTabsButtonVisible = count != 0;
-    [self updateInactiveTabsButtonVisible:inactiveTabsButtonVisible];
+    [self updateInactiveTabsButton];
   } else {
     // Update the layout.
     [self updateTabsSectionHeaderType];
@@ -214,9 +213,7 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
   _inactiveTabsDaysThreshold = daysThreshold;
 
   if (IsInactiveTabButtonRefactoringEnabled()) {
-    BOOL inactiveTabsButtonVisible =
-        daysThreshold != kInactiveTabsDisabledByUser;
-    [self updateInactiveTabsButtonVisible:inactiveTabsButtonVisible];
+    [self updateInactiveTabsButton];
   } else {
     // Update the header.
     if (oldDaysThreshold == kInactiveTabsDisabledByUser ||
@@ -248,9 +245,11 @@ constexpr base::TimeDelta kInactiveTabsHeaderAnimationDuration =
 
 #pragma mark - Private
 
-// Updates the inactive tabs button based on its `visible` state (i.e.
-// reconfigure, show or remove).
-- (void)updateInactiveTabsButtonVisible:(BOOL)visible {
+// Updates the inactive tabs button (reconfigure, show or remove) based on its
+// visible state.
+- (void)updateInactiveTabsButton {
+  BOOL visible = _inactiveTabsDaysThreshold != kInactiveTabsDisabledByUser &&
+                 _inactiveTabsCount != 0;
   GridSnapshot* snapshot = [self.diffableDataSource snapshot];
   if (visible) {
     GridItemIdentifier* item =
