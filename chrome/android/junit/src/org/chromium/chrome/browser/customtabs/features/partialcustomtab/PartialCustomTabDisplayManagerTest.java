1ea34bb57c81d9215fd854981d6f6ba80db5c11a
elabadysayed <elabadysayed@chromium.org>
2024-07-19 11:47:47 +0000

    [Gardner] Disable failing test in PartialCustomTabDisplayManagerTest
    Bug: 354141061
    Change-Id: Ib67096dd055a760af80a8b7937bf10be5b1c554a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725251
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Reviewed-by: Igor Kraskevich <kraskevich@google.com>
    Owners-Override: Sayed Elabady <elabadysayed@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330164}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabDisplayManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabDisplayManagerTest.java
@@ -298,6 +298,7 @@ public class PartialCustomTabDisplayManagerTest {
     }
 
     @Test
+    @DisabledTest(message = "b/354141061")
     public void transitionFromBottomSheetToSideSheetWhileSoftkeyboardIsOn() {
         mPCCTTestRule.configPortraitMode();
         PartialCustomTabDisplayManager displayManager = createPcctDisplayManager();
