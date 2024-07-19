02922a1eb5d9f74da104d14c43af32055bf83ea5
Hidehiko Abe <hidehiko@chromium.org>
2024-07-19 07:34:49 +0000

    Add //chrome/browser/ash/policy/reporting/**/BUILD.gn
    Bug: 335294371
    Test: Tryjob
    Change-Id: I89c913a3be34c498a2ad64dbcb22efd7230d6098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717400
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330075}

--- chrome/browser/ash/settings/device_settings_service.h
+++ chrome/browser/ash/settings/device_settings_service.h
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/common/cloud/cloud_policy_validator.h"
 #include "components/policy/proto/chrome_device_policy.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
-#include "crypto/scoped_nss_types.h"
 
 namespace ownership {
 class OwnerKeyUtil;
