74af55236fbb3a0d70f23fadf90627a284fd0875
Rubin Deliallisi <rubindl@chromium.org>
2024-07-19 19:49:59 +0000

    [SHoA] Integrate Safety Hub with the segmentation model
    Bug: 324562205
    Change-Id: I78994a4b606e77ca370b63e42efe5a351ab94015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5708069
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330447}

--- components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
+++ components/segmentation_platform/embedder/default_model/android_home_module_ranker_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/segmentation_platform/embedder/default_model/android_home_module_ranker.h"
 
 #include "components/segmentation_platform/embedder/default_model/default_model_test_base.h"
+#include "components/segmentation_platform/public/constants.h"
 #include "components/segmentation_platform/public/features.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -21,7 +22,7 @@ class AndroidHomeModuleRankerTest : public DefaultModelTestBase {
     DefaultModelTestBase::SetUp();
     bool isAndroidHomeModuleRankerV2Enabled = base::FeatureList::IsEnabled(
         features::kSegmentationPlatformAndroidHomeModuleRankerV2);
-    input_size = isAndroidHomeModuleRankerV2Enabled ? 9 : 6;
+    input_size = isAndroidHomeModuleRankerV2Enabled ? 12 : 8;
   }
 
   void TearDown() override { DefaultModelTestBase::TearDown(); }
@@ -40,8 +41,8 @@ TEST_F(AndroidHomeModuleRankerTest, ExecuteModelWithInputForAllModules) {
 
   EXPECT_FALSE(ExecuteWithInput(/*inputs=*/{}));
   std::vector<float> input(input_size, 0);
-  ExpectClassifierResults(
-      input, {kPriceChange, kSingleTab, kTabResumptionForAndroidHome});
+  ExpectClassifierResults(input, {kPriceChange, kSingleTab,
+                                  kTabResumptionForAndroidHome, kSafetyHub});
 }
 
 TEST_F(AndroidHomeModuleRankerTest,
@@ -54,8 +55,9 @@ TEST_F(AndroidHomeModuleRankerTest,
   input[0] = 1;
   input[2] = 1;
   input[4] = 1;
-  ExpectClassifierResults(
-      input, {kPriceChange, kSingleTab, kTabResumptionForAndroidHome});
+  input[6] = 1;
+  ExpectClassifierResults(input, {kSafetyHub, kPriceChange, kSingleTab,
+                                  kTabResumptionForAndroidHome});
 }
 
 TEST_F(AndroidHomeModuleRankerTest,
@@ -68,8 +70,9 @@ TEST_F(AndroidHomeModuleRankerTest,
   input[1] = 1;
   input[3] = 1;
   input[5] = 1;
-  ExpectClassifierResults(
-      input, {kSingleTab, kTabResumptionForAndroidHome, kPriceChange});
+  input[7] = 1;
+  ExpectClassifierResults(input, {kSingleTab, kTabResumptionForAndroidHome,
+                                  kPriceChange, kSafetyHub});
 }
 
 }  // namespace segmentation_platform
