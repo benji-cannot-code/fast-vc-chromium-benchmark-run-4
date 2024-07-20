f522392252a5ff26c5e154a6079638651fc00360
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-07-20 04:13:43 +0000

    [layout] Don't reuse lines if we've aligned content.
    We had this issue pre block-button layout, (for align-content).
    
    Basically when we reuse lines we re-add line-items using their
    previous offset, which is post alignment.
    
    This results in the alignment getting applied twice for reused lines.
    
    Skip the reuse lines optimization for content which has been aligned.
    
    Bug: 353898969
    Change-Id: Ic6dbe5222038d2ecb5e7df3e5e40db6f787d34fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725767
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330660}

--- third_party/blink/renderer/core/layout/layout_box_hot.cc
+++ third_party/blink/renderer/core/layout/layout_box_hot.cc
@@ -155,6 +155,11 @@ const LayoutResult* LayoutBox::CachedLayoutResult(
       return nullptr;
     }
 
+    // If we've shifted our children we can't rely on their position.
+    if (physical_fragment.HasMovedChildrenInBlockDirection()) {
+      return nullptr;
+    }
+
     cache_status = LayoutCacheStatus::kCanReuseLines;
   }
 
