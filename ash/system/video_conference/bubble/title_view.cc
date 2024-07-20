908020ef838a3764bcf31f25b1d077a7cbaed4ce
Norman Bintang <normanbt@chromium.org>
2024-07-20 04:37:24 +0000

    vc-ui: Create a mic indicator view
    Create a mic indicator view that shows how loud is the audio captured by
    the microphone. It shows 4 vertical lines that always make a wave, but
    the length of the lines depend on the ewma power that is reported by
    CRAS.
    
    Bug: 349261564
    Change-Id: I60daa38db94c9eafa109f89f7d23df0900a92687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691671
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Norman Bintang <normanbt@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330663}

--- ash/system/video_conference/bubble/title_view.cc
+++ ash/system/video_conference/bubble/title_view.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/video_conference/bubble/title_view.h"
 
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/style/color_provider.h"
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/typography.h"
+#include "ash/system/video_conference/bubble/mic_indicator.h"
 #include "ash/system/video_conference/video_conference_tray_controller.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -93,6 +95,10 @@ TitleView::TitleView() {
                        .SetVisible(VideoConferenceTrayController::Get()
                                        ->GetHasMicrophonePermissions())
                        .Build());
+
+  if (features::IsVcTrayMicIndicatorEnabled()) {
+    mic_test_column->AddChildView(std::make_unique<MicIndicator>());
+  }
 
   sidetone_button_ = mic_test_column->AddChildView(std::make_unique<IconButton>(
       base::BindRepeating(&TitleView::OnSidetoneButtonClicked,
