5f8ee8f476cb5b7ae60121492488a3c6abbf83ab
Michele Fan <michelefan@chromium.org>
2024-07-20 00:39:13 +0000

    snap-group: Only confine the bounds for dialog bubble or dialog transients
    Demo: http://b/347172567#14
    Fixed: b/347172567
    Test: Manually + updated existing
    Change-Id: Ia5c000cd61978302a062e88660ef58bdbddb392e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725529
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330613}

--- ash/wm/window_util.h
+++ ash/wm/window_util.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ui/base/window_state_type.h"
 #include "ui/aura/window.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
+#include "ui/views/window/dialog_delegate.h"
 #include "ui/wm/core/window_util.h"
 
 class PrefRegistrySimple;
@@ -208,6 +209,10 @@ ASH_EXPORT gfx::RectF GetTransformedBounds(aura::Window* transformed_window,
 // `transient_window`, if it's a bubble dialog.
 ASH_EXPORT views::BubbleDialogDelegate* AsBubbleDialogDelegate(
     aura::Window* transient_window);
+
+// Returns the `DialogDelegate` associated with the given `transient_window`, if
+// it's a dialog.
+views::DialogDelegate* AsDialogDelegate(aura::Window* transient_window);
 
 // If multi profile is on, check if |window| should be shown for the current
 // user.
