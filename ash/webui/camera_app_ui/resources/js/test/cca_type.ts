98cc9e518f8cffaac43a718c142b867403b16f96
Chu-Hsuan Yang <chuhsuan@chromium.org>
2024-07-22 03:55:59 +0000

    CCA: Add preview OCR option to Tast UI component
    Bug: b:350627756
    Test: Run with CL:5724771
    Change-Id: Ie1a76e0d61d8aedff09202e5ffa6fb7405e4533a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724735
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Commit-Queue: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330883}

--- ash/webui/camera_app_ui/resources/js/test/cca_type.ts
+++ ash/webui/camera_app_ui/resources/js/test/cca_type.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import {ExpertOption} from '../expert.js';
-import {StateUnion} from '../state.js';
+import {State, StateUnion} from '../state.js';
 import {ViewName} from '../type.js';
 
 export const SELECTOR_MAP = {
@@ -78,6 +78,7 @@ export const SELECTOR_MAP = {
   photoResolutionSettingButton: '#settings-photo-resolution',
   // TODO(kamchonlathorn): Remove this once its usage in Tast is removed.
   previewExposureTime: '#preview-exposure-time',
+  previewOcrOption: '#settings-preview-ocr',
   previewResolution: '#preview-resolution',
   previewVideo: '#preview-video',
   previewViewport: '#preview-viewport',
@@ -125,6 +126,10 @@ export const SETTING_OPTION_MAP = {
   showMetadataOption: {
     component: 'expertShowMetadataOption',
     state: ExpertOption.SHOW_METADATA,
+  },
+  previewOcrOption: {
+    component: 'previewOcrOption',
+    state: State.ENABLE_PREVIEW_OCR,
   },
 } satisfies Record<string, {component: UIComponent, state: StateUnion}>;
 export type SettingOption = keyof typeof SETTING_OPTION_MAP;
