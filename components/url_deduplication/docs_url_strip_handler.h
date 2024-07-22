f504199ff38ebae0fd04477e34e05a0628a3e33f
Marlon Facey <mfacey@chromium.org>
2024-07-22 18:14:09 +0000

    [Tab Resume 2.0] Set up doc url strip delegate for deduplication
    Add docs deduplication handler and unit test.
    
    Bug: 335200723
    Change-Id: I043b99c9c8c786ebe87f42fd1e5508db8035d204
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717152
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1331197}

---
+++ components/url_deduplication/docs_url_strip_handler.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_URL_DEDUPLICATION_DOCS_URL_STRIP_HANDLER_H_
+#define COMPONENTS_URL_DEDUPLICATION_DOCS_URL_STRIP_HANDLER_H_
+
+#include "components/url_deduplication/url_strip_handler.h"
+#include "url/gurl.h"
+
+class DocsURLStripHandler : public URLStripHandler {
+ public:
+  DocsURLStripHandler();
+
+  DocsURLStripHandler(const DocsURLStripHandler&) = delete;
+  DocsURLStripHandler& operator=(const DocsURLStripHandler&) = delete;
+
+  ~DocsURLStripHandler();
+  // URLStripHandler:
+  GURL StripExtraParams(GURL);
+};
+
+#endif  // COMPONENTS_URL_DEDUPLICATION_DOCS_URL_STRIP_HANDLER_H_
