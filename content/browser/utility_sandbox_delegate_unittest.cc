19264f8206ebef319282cac653259d09a2264c36
Alan Screen <awscreen@chromium.org>
2024-07-22 17:42:55 +0000

    Revert "[gardener] Limit UtilitySandboxDelegateWinTest configuration for ARM"
    This reverts commit dcd1d471664e4e4ce7f1f51065c4ffd9599eb4a4.
    
    Reason for revert: root issue in https://crbug.com/40223285 has been resolved
    
    Original change's description:
    > [gardener] Limit UtilitySandboxDelegateWinTest configuration for ARM
    >
    > The UtilitySandboxDelegateWinTest.IsAppContainerDisabled is flakily
    > failing for ARM when LPAC is used for the Print Compositor.  Disable
    > this part of the test until the flakes for this CPU type are figured
    > out.
    >
    > Bug: 353583970
    > Change-Id: I741cd4c967ebb85fad85f5296c5da8032172e046
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5719373
    > Commit-Queue: Alan Screen <awscreen@chromium.org>
    > Reviewed-by: Will Harris <wfh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1329150}
    
    Bug: 353583970, 40223285
    Change-Id: I11b84b6853758ee66f8bc026f0be82b32e7499e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731533
    Commit-Queue: Will Harris <wfh@chromium.org>
    Auto-Submit: Alan Screen <awscreen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331178}

--- content/browser/utility_sandbox_delegate_unittest.cc
+++ content/browser/utility_sandbox_delegate_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/win/windows_version.h"
-#include "build/build_config.h"
 #include "content/public/browser/child_process_host.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/common/content_client.h"
@@ -100,15 +99,8 @@ TEST_P(UtilitySandboxDelegateWinTest, IsAppContainerDisabled) {
 INSTANTIATE_TEST_SUITE_P(
     /* no prefix */,
     UtilitySandboxDelegateWinTest,
-// TODO(crbug.com/353583970):  Enable `kPrintCompositorLPAC` for ARM when
-// flakiness is resolved.
-#if defined(ARCH_CPU_ARM_FAMILY)
-    ::testing::Combine(/*AppContainerDisabled=*/::testing::Bool(),
-                       /*kPrintCompositorLPAC=*/::testing::Values(false)),
-#else
     ::testing::Combine(/*AppContainerDisabled=*/::testing::Bool(),
                        /*kPrintCompositorLPAC=*/::testing::Bool()),
-#endif
     [](const auto& info) {
       return base::StrCat(
           {std::get<0>(info.param) ? "ACDisabled" : "ACEnabled",
