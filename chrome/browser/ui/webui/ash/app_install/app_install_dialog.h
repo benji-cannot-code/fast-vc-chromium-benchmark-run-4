9fc5152f19cb9d448f70f3849a737a8f147c47b6
Alan Cutter <alancutter@chromium.org>
2024-07-22 07:37:34 +0000

    AIS: Add support for SVG app icons to the app install dialog
    AlmanacIconCache doesn't support rasterizing SVG icons which some
    apps in Almanac have.
    
    This CL adds AlmanacAppIconLoader which wraps AlmanacIconCache and
    adds SVG support as well as applying icon masking effects consistent
    with installed app icons.
    
    Screenshots: b/352203014#comment5
    
    Bug: b/352203014
    Change-Id: If3b52fa41893978af6d0d044d1ccb60af9d98bca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5691250
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330932}

--- chrome/browser/ui/webui/ash/app_install/app_install_dialog.h
+++ chrome/browser/ui/webui/ash/app_install/app_install_dialog.h
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_ASH_APP_INSTALL_APP_INSTALL_DIALOG_H_
 #define CHROME_BROWSER_UI_WEBUI_ASH_APP_INSTALL_APP_INSTALL_DIALOG_H_
 
+#include <optional>
+
 #include "chrome/browser/apps/almanac_api_client/almanac_icon_cache.h"
+#include "chrome/browser/apps/app_service/app_install/app_install_types.h"
 #include "chrome/browser/ui/webui/ash/app_install/app_install.mojom.h"
 #include "chrome/browser/ui/webui/ash/app_install/app_install_dialog_args.h"
 #include "chrome/browser/ui/webui/ash/app_install/app_install_ui.h"
@@ -14,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/services/app_service/public/cpp/icon_types.h"
 #include "components/services/app_service/public/cpp/package_id.h"
 #include "ui/views/native_window_tracker.h"
+
+namespace apps {
+class AlmanacAppIconLoader;
+}
 
 namespace ash::app_install {
 
@@ -36,9 +43,7 @@ class AppInstallDialog : public SystemWebDialogDelegate {
       std::string app_name,
       GURL app_url,
       std::string app_description,
-      GURL icon_url,
-      int icon_width,
-      bool is_icon_maskable,
+      std::optional<apps::AppInstallIcon> icon,
       std::vector<mojom::ScreenshotPtr> screenshots,
       base::OnceCallback<void(bool accepted)> dialog_accepted_callback);
 
@@ -68,11 +73,7 @@ class AppInstallDialog : public SystemWebDialogDelegate {
   AppInstallDialog();
   ~AppInstallDialog() override;
 
-  void OnIconDownloaded(int icon_width,
-                        bool is_icon_maskable,
-                        const gfx::Image& icon);
-
-  void OnLoadIcon(apps::IconValuePtr icon_value);
+  void OnAppIconLoaded(apps::IconValuePtr icon_value);
 
   void Show(gfx::NativeWindow parent, AppInstallDialogArgs dialog_args);
 
@@ -87,7 +88,7 @@ class AppInstallDialog : public SystemWebDialogDelegate {
   gfx::NativeWindow parent_;
   std::unique_ptr<views::NativeWindowTracker> parent_window_tracker_;
   AppInfoArgs app_info_args_;
-  std::unique_ptr<apps::AlmanacIconCache> icon_cache_;
+  std::unique_ptr<apps::AlmanacAppIconLoader> icon_loader_;
 
   base::WeakPtrFactory<AppInstallDialog> weak_factory_{this};
 };
--- chrome/browser/ui/webui/ash/app_install/app_install_dialog_browsertest.cc
+++ chrome/browser/ui/webui/ash/app_install/app_install_dialog_browsertest.cc
@@ -159,9 +159,7 @@ IN_PROC_BROWSER_TEST_F(AppInstallDialogBrowserTest, InstallApp) {
       /*app_name=*/"Test app",
       /*app_url=*/app_url,
       /*app_description=*/"",
-      /*icon_url=*/GURL(),
-      /*icon_width=*/0,
-      /*is_icon_maskable=*/false,
+      /*icon=*/std::nullopt,
       /*screenshots=*/{},
       /*dialog_accepted_callback=*/dialog_accepted_future.GetCallback());
   navigation_observer_dialog.Wait();
@@ -267,9 +265,7 @@ IN_PROC_BROWSER_TEST_F(AppInstallDialogBrowserTest, FailedInstall) {
       /*app_name=*/"Test app",
       /*app_url=*/GURL(kAppUrl),
       /*app_description=*/"",
-      /*icon_url=*/GURL(),
-      /*icon_width=*/0,
-      /*is_icon_maskable=*/false,
+      /*icon=*/std::nullopt,
       /*screenshots=*/{},
       base::BindOnce(
           [](base::WeakPtr<AppInstallDialog> dialog_handle,
