6edf870abb96bd55f710264a5740bd563d8d2804
mlcui <mlcui@google.com>
2024-07-22 04:08:12 +0000

    Remove `CHECK_DEREF`s on `main_label_` accessors
    `main_label_` should always be non-null as it was set in `CopyAddressTo`
    in the ctor.
    
    Bug: b:344717756
    Change-Id: I6a6a210041bd54902f92bbaab36ff504bb590cd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726636
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330884}

--- ash/picker/views/picker_preview_bubble.cc
+++ ash/picker/views/picker_preview_bubble.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/bubble/bubble_constants.h"
 #include "ash/bubble/bubble_utils.h"
 #include "ash/style/typography.h"
-#include "base/check_deref.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/geometry/insets.h"
@@ -128,11 +127,11 @@ void PickerPreviewBubbleView::SetPreviewImage(ui::ImageModel image) {
 }
 
 std::u16string_view PickerPreviewBubbleView::GetMainTextForTesting() {
-  return CHECK_DEREF(main_label_.get()).GetText();
+  return main_label_->GetText();
 }
 
 void PickerPreviewBubbleView::SetMainText(const std::u16string& text) {
-  CHECK_DEREF(main_label_.get()).SetText(text);
+  main_label_->SetText(text);
 }
 
 void PickerPreviewBubbleView::OnThemeChanged() {
