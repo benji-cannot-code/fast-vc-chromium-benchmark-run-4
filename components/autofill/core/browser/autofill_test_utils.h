7c57ff55a16da01dc5d69b4f8f5a04a13b9a5a96
Justin Wells <justinleewells@google.com>
2024-07-22 21:39:02 +0000

    Add methods to set and get payment instruments in PaymentsAutofillTable.
    These methods will be utilized by the AutofillWalletSyncBridge to ensure
    the payment instruments stay in sync.
    
    Change-Id: I63f6143fb76d82bf088d276fa63d77f8c9edb86f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5669656
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Justin Wells <justinleewells@google.com>
    Reviewed-by: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1331348}

--- components/autofill/core/browser/autofill_test_utils.h
+++ components/autofill/core/browser/autofill_test_utils.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/ui/suggestion.h"
 #include "components/autofill/core/browser/ui/suggestion_type.h"
 #include "components/autofill/core/common/autofill_test_utils.h"
+#include "components/sync/protocol/autofill_specifics.pb.h"
 
 class PrefService;
 
@@ -391,6 +392,14 @@ Suggestion CreateAutofillSuggestion(
 
 // Returns a bank account enabled for Pix with fake data.
 BankAccount CreatePixBankAccount(int64_t instrument_id);
+
+// Returns a payment instrument with a bank account filled with fake data.
+sync_pb::PaymentInstrument CreatePaymentInstrumentWithBankAccount(
+    int64_t instrument_id);
+
+// Returns a payment instrument with an IBAN filled with fake data.
+sync_pb::PaymentInstrument CreatePaymentInstrumentWithIban(
+    int64_t instrument_id);
 
 }  // namespace test
 }  // namespace autofill
