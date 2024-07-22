5feba89e12bd765d1bcb266a06541f3b43d40104
Min Qin <qinmin@chromium.org>
2024-07-22 20:35:14 +0000

    Allow insecure inline pdf download if it is displayed inline
    This CL added a flag to allow pdf download even the download is
    insecure. The CL exposes a isDownloadSafe flag to Java side so
    we can use it to display a warning icon in omnibox if the download
    is insecure.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 354714901
    Change-Id: I8241f78e03b6da1bfe7e35717a656586c3674d00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731312
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331295}

--- chrome/browser/download/android/download_controller.cc
+++ chrome/browser/download/android/download_controller.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/download/download_offline_content_provider.h"
 #include "chrome/browser/download/download_offline_content_provider_factory.h"
 #include "chrome/browser/download/download_stats.h"
+#include "chrome/browser/download/insecure_download_blocking.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/offline_pages/android/offline_page_bridge.h"
 #include "chrome/browser/permissions/permission_update_message_controller_android.h"
@@ -568,6 +569,7 @@ void DownloadController::OnDownloadComplete(download::DownloadItem* item) {
   // Multiple OnDownloadUpdated() notifications may be issued while the
   // download is in the COMPLETE state. Only handle one.
   item->RemoveObserver(this);
+  bool is_download_safe = true;
   // Call onDownloadCompleted
   TabAndroid* tab = nullptr;
   if (base::FeatureList::IsEnabled(features::kAndroidOpenPdfInline)) {
@@ -576,9 +578,17 @@ void DownloadController::OnDownloadComplete(download::DownloadItem* item) {
     if (web_contents) {
       tab = TabAndroid::FromWebContents(web_contents);
     }
+    download::DownloadItem::InsecureDownloadStatus status =
+        GetInsecureDownloadStatusForDownload(
+            Profile::FromBrowserContext(
+                content::DownloadItemUtils::GetBrowserContext(item)),
+            item->GetTargetFilePath(), item);
+    is_download_safe =
+        (status == download::DownloadItem::InsecureDownloadStatus::SAFE ||
+         status == download::DownloadItem::InsecureDownloadStatus::VALIDATED);
   }
   Java_DownloadController_onDownloadCompleted(
-      env, tab ? tab->GetJavaObject() : nullptr, j_item);
+      env, tab ? tab->GetJavaObject() : nullptr, j_item, is_download_safe);
 }
 
 void DownloadController::StartContextMenuDownload(
