b3ce3c3bcffa0bffbc1be8656b8c5118b690061a
Vidhan Jain <vidhanj@google.com>
2024-07-22 10:27:08 +0000

    [iOS] Add field trial config for AutofillDynamicallyLoadsFieldsForAddressInput
    Bug: 1482269
    Change-Id: Ie3a148586d78c43b6b412e3ff68c80c8eb5c0079
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725454
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330969}

--- ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.mm
+++ ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/browser/autofill_java_script_feature.h"
 #import "components/autofill/ios/browser/credit_card_save_manager_test_observer_bridge.h"
 #import "components/autofill/ios/browser/ios_test_event_waiter.h"
+#import "components/autofill/ios/common/features.h"
 #import "components/keyed_service/core/service_access_type.h"
 #import "components/password_manager/core/browser/password_manager_util.h"
 #import "components/password_manager/core/browser/password_store/password_store_consumer.h"
@@ -618,6 +619,11 @@ static std::unique_ptr<ScopedAutofillPaymentReauthModuleOverride>
 
 + (BOOL)isKeyboardAccessoryUpgradeEnabled {
   return IsKeyboardAccessoryUpgradeEnabled();
+}
+
++ (BOOL)isDynamicallyLoadFieldsOnInputEnabled {
+  return base::FeatureList::IsEnabled(
+      kAutofillDynamicallyLoadsFieldsForAddressInput);
 }
 
 #pragma mark - Private
