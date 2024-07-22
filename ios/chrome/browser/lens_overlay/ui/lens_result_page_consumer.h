6c4e28a106860daa20d8a4f1f9b25ca3bce82791
Christian Xu <christianxu@google.com>
2024-07-22 14:26:46 +0000

    [iOS][LO][LRP] Use webstate UnderPageBackgroundColor in result page
    Update the background color of the result page when the background color
    of the webstate changes.
    
    Bug: 347233074
    Change-Id: I93ba2f2260d8a780e844897e4f9c50a825ecc355
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL fake web state added in a separate CL
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5682302
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331039}

--- ios/chrome/browser/lens_overlay/ui/lens_result_page_consumer.h
+++ ios/chrome/browser/lens_overlay/ui/lens_result_page_consumer.h
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /// Sets `webView` in the consumer.
 - (void)setWebView:(UIView*)webView;
 
+/// Sets the background color of the consumer.
+- (void)setBackgroundColor:(UIColor*)backgroundColor;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_LENS_OVERLAY_UI_LENS_RESULT_PAGE_CONSUMER_H_
--- ios/chrome/browser/lens_overlay/ui/lens_result_page_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_result_page_view_controller.mm
@@ -146,6 +146,10 @@ const CGFloat kWebContainerTopPadding = 8;
   AddSameConstraints(_webView, self.webViewContainer);
 }
 
+- (void)setBackgroundColor:(UIColor*)backgroundColor {
+  self.view.backgroundColor = backgroundColor;
+}
+
 #pragma mark - Private
 
 /// Handles back button taps.
