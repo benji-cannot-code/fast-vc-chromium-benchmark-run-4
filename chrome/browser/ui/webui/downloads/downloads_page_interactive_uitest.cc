5973e52b4eb174f64c06560510c81bb5ea8c453a
Kerry Meng-Lin <kmenglin@google.com>
2024-07-22 23:22:17 +0000

    [DangerousDownloadInterstitial] Add fieldtrial testing config for the dangerous download interstitial.
    Bug: 349666436
    Change-Id: I92f65a96146d189387da36db19fc5f384a780227
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5722246
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Kerry Meng-Lin <kmenglin@google.com>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331420}

--- chrome/browser/ui/webui/downloads/downloads_page_interactive_uitest.cc
+++ chrome/browser/ui/webui/downloads/downloads_page_interactive_uitest.cc
@@ -38,7 +38,10 @@ using ::testing::Eq;
 class DownloadsPageInteractiveUitest
     : public InteractiveBrowserTestT<DownloadTestBase> {
  public:
-  DownloadsPageInteractiveUitest() = default;
+  DownloadsPageInteractiveUitest() {
+    feature_list_.InitAndEnableFeature(
+        safe_browsing::kDangerousDownloadInterstitial);
+  }
   ~DownloadsPageInteractiveUitest() override = default;
 
   void SetUp() override {
@@ -177,6 +180,9 @@ class DownloadsPageInteractiveUitest
                           {"/", DownloadTestBase::kDownloadTest1Path})));
     }));
   }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitest, DownloadItemsAppear) {
@@ -339,6 +345,26 @@ class DownloadsPageInteractiveUitestWithDangerType
   }
 };
 
+template <download::DownloadDangerType DangerType>
+class DownloadsPageInteractiveUitestWithDangerTypeForBypassDialog
+    : public DownloadsPageInteractiveUitestWithDangerType<DangerType> {
+ public:
+  DownloadsPageInteractiveUitestWithDangerTypeForBypassDialog() {
+    feature_list_.InitAndDisableFeature(
+        safe_browsing::kDangerousDownloadInterstitial);
+  }
+  ~DownloadsPageInteractiveUitestWithDangerTypeForBypassDialog() override =
+      default;
+
+  void SetUpOnMainThread() override {
+    DownloadsPageInteractiveUitestWithDangerType<
+        DangerType>::SetUpOnMainThread();
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
 // Uncommon downloads follow the "suspicious" pattern and show up with grey
 // icons and text. They can be validated from the page directly without a
 // confirmation dialog.
@@ -402,7 +428,13 @@ IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerous,
                   WaitForNoDownloads());
 }
 
-IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerous,
+// Dangerous downloads follow the "dangerous" pattern and show up with red
+// icons and text. They can be validated only from the confirmation dialog.
+using DownloadsPageInteractiveUitestDangerousFromBypassDialog =
+    DownloadsPageInteractiveUitestWithDangerTypeForBypassDialog<
+        download::DOWNLOAD_DANGER_TYPE_DANGEROUS_CONTENT>;
+
+IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerousFromBypassDialog,
                        ValidateDangerousFileFromPrompt) {
   RunTestSequence(
       OpenDownloadsPage(),                                           //
@@ -415,7 +447,7 @@ IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerous,
       WaitForTopmostItemDanger(false));
 }
 
-IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerous,
+IN_PROC_BROWSER_TEST_F(DownloadsPageInteractiveUitestDangerousFromBypassDialog,
                        CancelValidateDangerousFile) {
   RunTestSequence(OpenDownloadsPage(),                               //
                   DownloadDangerousTestFile(),                       //
