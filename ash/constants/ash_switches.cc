244c07b35ff3afa56d0f0410774819bb65ad8ef4
Lauren Commeignes <laurencom@chromium.org>
2024-07-22 01:08:56 +0000

    Add in server config to specify the server for Sparky.
    Adds in a new flag for the Sparky Server Url. This will be propagated
    through to the proto request for Sparky.
    
    Bug: b:352464311
    Test: Tested on DUT
    Low-Coverage-Reason: EXPERIMENTAL_CODE experimental feature
    Change-Id: I13a025abc8410120a6f23e720e5453d55d5e4609
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5699566
    Commit-Queue: Lauren Commeignes <laurencom@chromium.org>
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330856}

--- ash/constants/ash_switches.cc
+++ ash/constants/ash_switches.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_switches.h"
 
 #include <algorithm>
+#include <optional>
 #include <string>
 
 #include "base/auto_reset.h"
@@ -910,6 +911,9 @@ const char kMahiFeatureKey[] = "mahi-feature-key";
 // Supply secret key for the sparky feature.
 const char kSparkyFeatureKey[] = "sparky-feature-key";
 
+// Supply server url for the sparky feature.
+const char kSparkyServerUrl[] = "sparky-server-url";
+
 // Specifies the user that the browser data migration should happen for.
 const char kBrowserDataMigrationForUser[] = "browser-data-migration-for-user";
 
@@ -1475,6 +1479,19 @@ bool IsModifierSplitSecretKeyMatched() {
 
 base::AutoReset<bool> SetIgnoreModifierSplitSecretKeyForTest() {
   return {&g_ignore_modifier_split_secret_key, true};
+}
+
+std::optional<std::string> ObtainSparkyServerUrl() {
+  // Commandline looks like:
+  //  out/Default/chrome --user-data-dir=/tmp/tmp123
+  //  --sparky-server-url="INSERT KEY HERE"
+  //  --enable-features=Sparky
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(kSparkyServerUrl)) {
+    return std::make_optional(
+        base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+            kSparkyServerUrl));
+  }
+  return std::nullopt;
 }
 
 }  // namespace ash::switches
--- ash/constants/ash_switches.h
+++ ash/constants/ash_switches.h
@@ -278,6 +278,7 @@ COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDisallowLacros[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kDisableDisallowLacros[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kMahiFeatureKey[];
 COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kSparkyFeatureKey[];
+COMPONENT_EXPORT(ASH_CONSTANTS) extern const char kSparkyServerUrl[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
 extern const char kBrowserDataMigrationForUser[];
 COMPONENT_EXPORT(ASH_CONSTANTS)
@@ -525,6 +526,9 @@ bool IsSparkySecretKeyMatched();
 
 COMPONENT_EXPORT(ASH_CONSTANTS)
 base::AutoReset<bool> SetIgnoreSparkySecretKeyForTest();
+
+COMPONENT_EXPORT(ASH_CONSTANTS)
+std::optional<std::string> ObtainSparkyServerUrl();
 
 COMPONENT_EXPORT(ASH_CONSTANTS)
 bool IsModifierSplitSecretKeyMatched();
