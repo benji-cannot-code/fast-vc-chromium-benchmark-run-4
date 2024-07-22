442ccd8c770ca5f8bd8724431d86808c7fa5c6a4
Alex Ilin <alexilin@chromium.org>
2024-07-22 16:06:10 +0000

    Refactor result type of OAuth2MintTokenFlow
    "Return Value" > "Output Parameters"
    
    MintTokenResult structure used as the flow result will also make it
    easier to add new fields to the result.
    
    Bug: b/353199749
    Change-Id: I2192c528cfa2bba7059309edbc0d556d4ec72cda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703217
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331096}

--- chrome/browser/extensions/api/identity/identity_get_auth_token_function.cc
+++ chrome/browser/extensions/api/identity/identity_get_auth_token_function.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/manifest_handlers/oauth2_manifest_handler.h"
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/gaia_urls.h"
+#include "google_apis/gaia/oauth2_mint_token_flow.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "ui/base/idle/idle.h"
 
@@ -480,20 +481,18 @@ void IdentityGetAuthTokenFunction::StartMintToken(
 }
 
 void IdentityGetAuthTokenFunction::OnMintTokenSuccess(
-    const std::string& access_token,
-    const std::set<std::string>& granted_scopes,
-    int time_to_live) {
+    const OAuth2MintTokenFlow::MintTokenResult& result) {
   TRACE_EVENT_NESTABLE_ASYNC_INSTANT0("identity", "OnMintTokenSuccess", this);
 
   IdentityTokenCacheValue token = IdentityTokenCacheValue::CreateToken(
-      access_token, granted_scopes, base::Seconds(time_to_live));
+      result.access_token, result.granted_scopes, result.time_to_live);
   IdentityAPI::GetFactoryInstance()
       ->Get(GetProfile())
       ->token_cache()
       ->SetToken(token_key_, token);
 
   CompleteMintTokenFlow();
-  CompleteFunctionWithResult(access_token, granted_scopes);
+  CompleteFunctionWithResult(result.access_token, result.granted_scopes);
 }
 
 void IdentityGetAuthTokenFunction::OnMintTokenFailure(
--- google_apis/gaia/oauth2_mint_token_flow.h
+++ google_apis/gaia/oauth2_mint_token_flow.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "google_apis/gaia/oauth2_api_call_flow.h"
 #include "net/cookies/canonical_cookie.h"
@@ -141,13 +142,24 @@ class COMPONENT_EXPORT(GOOGLE_APIS) OAuth2MintTokenFlow
     Parameters& operator=(const Parameters&);
   };
 
+  // Result obtained from a successful OAuth2 flow.
+  struct COMPONENT_EXPORT(GOOGLE_APIS) MintTokenResult {
+    MintTokenResult();
+    ~MintTokenResult();
+
+    MintTokenResult(const MintTokenResult&) = delete;
+    MintTokenResult& operator=(const MintTokenResult&) = delete;
+    MintTokenResult(MintTokenResult&& other) noexcept;
+    MintTokenResult& operator=(MintTokenResult&& other) noexcept;
+
+    std::string access_token;
+    std::set<std::string> granted_scopes;
+    base::TimeDelta time_to_live;
+  };
+
   class COMPONENT_EXPORT(GOOGLE_APIS) Delegate {
    public:
-    // TODO(alexilin): change `time_to_live` parameter to be `base::TimeDelta`
-    // type.
-    virtual void OnMintTokenSuccess(const std::string& access_token,
-                                    const std::set<std::string>& granted_scopes,
-                                    int time_to_live) {}
+    virtual void OnMintTokenSuccess(const MintTokenResult& result) {}
     virtual void OnMintTokenFailure(const GoogleServiceAuthError& error) {}
     virtual void OnRemoteConsentSuccess(
         const RemoteConsentResolutionData& resolution_data) {}
@@ -210,9 +222,7 @@ class COMPONENT_EXPORT(GOOGLE_APIS) OAuth2MintTokenFlow
                            ParseRemoteConsentResponse_BadCookieList);
   FRIEND_TEST_ALL_PREFIXES(OAuth2MintTokenFlowTest, ParseMintTokenResponse);
 
-  void ReportSuccess(const std::string& access_token,
-                     const std::set<std::string>& granted_scopes,
-                     int time_to_live);
+  void ReportSuccess(const MintTokenResult& result);
   void ReportRemoteConsentSuccess(
       const RemoteConsentResolutionData& resolution_data);
   void ReportFailure(const GoogleServiceAuthError& error);
@@ -221,10 +231,8 @@ class COMPONENT_EXPORT(GOOGLE_APIS) OAuth2MintTokenFlow
       const base::Value::Dict& dict,
       RemoteConsentResolutionData* resolution_data);
 
-  static bool ParseMintTokenResponse(const base::Value::Dict& dict,
-                                     std::string* access_token,
-                                     std::set<std::string>* granted_scopes,
-                                     int* time_to_live);
+  static std::optional<MintTokenResult> ParseMintTokenResponse(
+      const base::Value::Dict& dict);
 
   raw_ptr<Delegate> delegate_;
   Parameters parameters_;
