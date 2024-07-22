1b2802d82cb6fd426ca8ba49b4c34f04eca688fb
Rakina Zata Amni <rakina@chromium.org>
2024-07-22 16:13:31 +0000

    Add render process init metrics for AbandonedPLMO
    This sets the UKM metric and adds a new histogram for init relative
    to navigation start. Because the render process init might happen
    at different points in time (before navigation, at navigation start,
    after final response), we don't include it in navigation milestones.
    Having the render process init time will be useful mostly to
    relation between it and when the commit/loading is process on the
    renderer side (mostly using UKM).
    
    Bug: 347706997
    Change-Id: I8daae96a00330244b26919c9b6eee6f973c57a95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721710
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331102}

--- components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/page_load_metrics/browser/page_load_metrics_util.h"
 #include "components/page_load_metrics/common/page_load_timing.h"
 #include "content/public/browser/navigation_handle.h"
+#include "content/public/browser/render_process_host.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -68,7 +69,7 @@ const char kAbandonReasonNewOtherNavigationRendererInitiated[] =
 const char kAbandonReasonFrameRemoved[] = "FrameRemoved";
 const char kAbandonReasonExplicitCancellation[] = "ExplicitCancellation";
 const char kAbandonReasonInternalCancellation[] = "InternalCancellation";
-const char kAbandonReasonRenderProcessGone[] = "RenderProcessGone";
+const char kAbandonReasonRendererProcessGone[] = "RendererProcessGone";
 const char kAbandonReasonNeverStarted[] = "NeverStarted";
 const char kAbandonReasonFailedSecurityCheck[] = "FailedSecurityCheck";
 const char kAbandonReasonOther[] = "Other";
@@ -97,6 +98,11 @@ const char kMilestoneParseStart[] = "ParseStart";
 const char kFirstContentfulPaint[] = "FirstContentfulPaint";
 const char kDOMContentLoaded[] = "DOMContentLoaded";
 
+const char kRendererProcessCreatedBeforeNavHistogramName[] =
+    "RendererProcessCreatedBeforeNav";
+const char kRendererProcessInitHistogramName[] =
+    "NavigationStartToRendererProcessInit";
+
 // TODO(https://crbug.com/347706997): Record more milestones related to loading
 // and process creation timing.
 
@@ -120,7 +126,7 @@ std::string AbandonedPageLoadMetricsObserver::AbandonReasonToString(
     case AbandonReason::kInternalCancellation:
       return internal::kAbandonReasonInternalCancellation;
     case AbandonReason::kRenderProcessGone:
-      return internal::kAbandonReasonRenderProcessGone;
+      return internal::kAbandonReasonRendererProcessGone;
     case AbandonReason::kNeverStarted:
       return internal::kAbandonReasonNeverStarted;
     case AbandonReason::kFailedSecurityCheck:
@@ -322,6 +328,12 @@ void AbandonedPageLoadMetricsObserver::LogAbandonHistograms(
         (first_backgrounded_timestamp_ - navigation_start_time)
             .InMilliseconds());
   }
+
+  if (!renderer_process_init_time_.is_null()) {
+    builder.SetRendererProcessInitTime(
+        (renderer_process_init_time_ - navigation_start_time).InMilliseconds());
+  }
+
   if (!first_hidden_timestamp_.is_null()) {
     builder.SetPreviousHiddenTime(
         (first_hidden_timestamp_ - navigation_start_time).InMilliseconds());
@@ -438,6 +450,32 @@ AbandonedPageLoadMetricsObserver::OnNavigationHandleTimingUpdated(
       navigation_handling_result = OnNavigationEvent(navigation_handle);
   if (navigation_handling_result != CONTINUE_OBSERVING) {
     return navigation_handling_result;
+  }
+
+  if (renderer_process_init_time_.is_null() &&
+      !latest_navigation_handle_timing_.navigation_commit_sent_time.is_null() &&
+      navigation_handle->GetRenderFrameHost()) {
+    renderer_process_init_time_ = navigation_handle->GetRenderFrameHost()
+                                      ->GetProcess()
+                                      ->GetLastInitTime();
+    bool renderer_process_created_before_navigation =
+        (renderer_process_init_time_ < GetDelegate().GetNavigationStart());
+    std::string base_suffix = GetHistogramSuffix(
+        NavigationMilestone::kCommitSent, renderer_process_init_time_);
+    for (std::string additional_suffix : GetAdditionalSuffixes()) {
+      std::string suffix = base_suffix + additional_suffix;
+      base::UmaHistogramBoolean(
+          GetHistogramPrefix() +
+              internal::kRendererProcessCreatedBeforeNavHistogramName + suffix,
+          renderer_process_created_before_navigation);
+      if (renderer_process_created_before_navigation) {
+        continue;
+      }
+      PAGE_LOAD_HISTOGRAM(
+          GetHistogramPrefix() + internal::kRendererProcessInitHistogramName +
+              suffix,
+          renderer_process_init_time_ - GetDelegate().GetNavigationStart());
+    }
   }
 
   if (navigation_handle->GetNetErrorCode() != net::OK) {
