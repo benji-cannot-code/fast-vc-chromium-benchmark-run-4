c882ee5238153762a1dfa88e956548d34ea16fb0
Erik Chen <erikchen@chromium.org>
2024-07-22 21:30:32 +0000

    Fix //chrome/browser build graph, part 2.
    The core requirement of allow_circular_includes_from is that all
    circularly dependent targets share all dependencies. In practice, the
    only relevant dependencies are public dependencies that generate header
    files.
    
    This CL extracts all generated mojoms, protos, buildflags, and a few
    custom targets from //chrome/browser:browser and //chrome/browser/ui:ui
    into a single new target: //chrome/browser:browser_generated_files. Note
    that generated files from outside of //chrome/browser are expected to
    already be correctly captured by browser_public_dependencies.
    
    This CL has no noticeable change in build time when testing locally.
    
    This CL modifies 2 mojo typemaps to have reduced traits_public_deps to
    avoid circular dependencies.
    
    Change-Id: I0c69b240db7da74988976b613db62ef60cfe9532
    Bug: 353628440
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5727139
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331340}

--- chrome/browser/ui/webui/tab_strip/tab_strip_mojom_traits.h
+++ chrome/browser/ui/webui/tab_strip/tab_strip_mojom_traits.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/fixed_flat_map.h"
 #include "chrome/browser/ui/tabs/tab_network_state.h"
-#include "chrome/browser/ui/tabs/tab_utils.h"
 #include "chrome/browser/ui/webui/tab_strip/tab_strip.mojom.h"
 
 namespace mojo {
