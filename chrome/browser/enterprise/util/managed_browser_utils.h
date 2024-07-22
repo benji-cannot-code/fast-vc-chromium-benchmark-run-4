c8308b0e307153e5f6d4cb2ad637e42b2335bbdf
Yann Dago <ydago@chromium.org>
2024-07-22 17:45:12 +0000

    Move management icon logic out of browser management service
    This CL also updates the management toolbar button to use the right
    management information.
    
    A side effect of this CL is breaking the management badge by not
    having the possibility to use the icon specified by policy. This
    is fine since the feature is not released and may not be released.
    
    The functionality there will be re-enabled in the future since it may
    require a refactor which would be out of scope here.
    
    Bug: 347245819, 351842161
    Fixed: 351842161
    Change-Id: I836c1b1623846892a2b219df2e0b618e03cdf4fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713462
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1331179}

--- chrome/browser/enterprise/util/managed_browser_utils.h
+++ chrome/browser/enterprise/util/managed_browser_utils.h
@@ -10,9 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/functional/bind.h"
 #include "build/build_config.h"
 #include "net/base/host_port_pair.h"
 #include "net/ssl/client_cert_identity.h"
+#include "ui/gfx/image/image.h"
+#include "url/gurl.h"
 
 struct AccountInfo;
 class GURL;
@@ -87,6 +90,13 @@ bool CanShowEnterpriseBadging(Profile* profile);
 // can still return `false` for consumer domains which are not hardcoded in
 // implementation.
 bool IsKnownConsumerDomain(const std::string& email_domain);
+
+// Returns an enterprise icon hosted at `url` for `profile` using `callback`.
+// An empty image is returned in case `url` is invalid or we fail to fetch the
+// image.
+void GetManagementIcon(const GURL& url,
+                       Profile* profile,
+                       base::OnceCallback<void(const gfx::Image&)> callback);
 
 }  // namespace enterprise_util
 }  // namespace chrome
--- chrome/browser/ui/views/profiles/management_toolbar_button.h
+++ chrome/browser/ui/views/profiles/management_toolbar_button.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_VIEWS_PROFILES_MANAGEMENT_TOOLBAR_BUTTON_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -24,6 +25,10 @@ class ManagementToolbarButton : public ToolbarButton {
   ManagementToolbarButton& operator=(const ManagementToolbarButton&) = delete;
   ~ManagementToolbarButton() override;
 
+  // Retrieves the latest management label and icon and stores them in
+  // `management_label_` and `management_icon_` respectively.
+  void UpdateManagementInfo();
+
   void UpdateText();
 
   // ToolbarButton:
@@ -37,14 +42,21 @@ class ManagementToolbarButton : public ToolbarButton {
 
   ui::ImageModel GetIcon() const;
 
+  void SetManagementLabel(const std::string& management_label);
+  void SetManagementIcon(const gfx::Image& management_icon);
+
   // Returns true if a text is set and is visible.
   bool IsLabelPresentAndVisible() const;
   // Updates the layout insets depending on whether it is a chip or a button.
   void UpdateLayoutInsets();
 
+  std::u16string management_label_;
+  gfx::Image management_icon_;
   const raw_ptr<Browser> browser_;
   const raw_ptr<Profile> profile_;
   PrefChangeRegistrar pref_change_registrar_;
+
+  base::WeakPtrFactory<ManagementToolbarButton> weak_ptr_factory_{this};
 };
 
 #endif  // CHROME_BROWSER_UI_VIEWS_PROFILES_MANAGEMENT_TOOLBAR_BUTTON_H_
