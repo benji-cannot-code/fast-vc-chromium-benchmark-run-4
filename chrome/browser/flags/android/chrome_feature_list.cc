6d724ee486875477c6a6dd4e2183749ec2594729
Xinyi Ji <xinyiji@google.com>
2024-07-22 22:12:12 +0000

    [LogoPolish] Enable feature LogoPolish by default.
    This CL enables feature LogoPolish with medium size by default.
    
    Bug: 354753809
    Change-Id: If47a83b26aaa75bf39382a5321d87505dc025ad4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730420
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331374}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -749,7 +749,7 @@ BASE_FEATURE(kIncognitoScreenshot,
              "IncognitoScreenshot",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kLogoPolish, "LogoPolish", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kLogoPolish, "LogoPolish", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kMagicStackAndroid,
              "MagicStackAndroid",
