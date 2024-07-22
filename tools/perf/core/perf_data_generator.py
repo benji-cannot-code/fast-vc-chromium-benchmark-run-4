308bb89862604be05fe6ca48544dd81edf729b07
Wenbin Zhang <wenbinzhang@google.com>
2024-07-22 22:11:17 +0000

    [chromeperf] Relax dimension values for win-10 laptops for labs to update os/gpu
    Relaxed the dimensions for lab's update.
    
    Win-10_laptop_low_end-perf will have only 24 devices with this change. It will be changed back to 48 when os/gpu are updated on the whole fleet.
    
    Bug: b/354044494
    Change-Id: Ic28416ca02edfdbd7618e9e67635ac2ceec5e3b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732275
    Reviewed-by: John Chen <johnchen@chromium.org>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331373}

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -806,9 +806,9 @@ BUILDERS = {
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
             'os':
-            'Windows-10-18363.476|Windows-10-18363.1621',
+            'Windows-10',
             'gpu':
-            '8086:1616-20.19.15.5070|8086:1616-10.0.18362.1',
+            '8086:1616',
             'synthetic_product_name':
             'HP Laptop 15-bs1xx [Type1ProductConfigId] (HP)'
         },
@@ -828,8 +828,8 @@ BUILDERS = {
         64,
         'dimension': {
             'pool': 'chrome.tests.perf',
-            'os': 'Windows-10-18363.476|Windows-10-18363.1621',
-            'gpu': '8086:1616-20.19.15.5070|8086:1616-10.0.18362.1',
+            'os': 'Windows-10',
+            'gpu': '8086:1616',
         },
     },
     'win-10-perf': {
@@ -889,8 +889,8 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-10-19043',
-            'gpu': '1002:1638-30.0.13033.1000',
+            'os': 'Windows-10',
+            'gpu': '1002:1638',
             'synthetic_product_name': 'OMEN by HP Laptop 16-c0xxx [ ] (HP)',
         },
     },
@@ -913,8 +913,8 @@ BUILDERS = {
             # that we can be informed if this
             # version ever changes or becomes inconsistent. It is important
             # that bots are homogeneous. See crbug.com/988045 for history.
-            'os': 'Windows-10-19043',
-            'gpu': '1002:1638-30.0.13033.1000',
+            'os': 'Windows-10',
+            'gpu': '1002:1638',
             'synthetic_product_name': 'OMEN by HP Laptop 16-c0xxx [ ] (HP)',
         },
     },
