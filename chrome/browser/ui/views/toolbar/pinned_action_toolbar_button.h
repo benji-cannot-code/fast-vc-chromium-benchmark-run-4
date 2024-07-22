e2f1ace3fbd804e1f0f3522bbc99f4a9199f4174
Lingqi Chi <lingqi@chromium.org>
2024-07-22 05:34:17 +0000

    Revert "Make actions in the "engaged" state higher priority for visibility during layout."
    This reverts commit f4dcf0bd56df33b0e7ee0e8a306e656d14bb86ad.
    
    Reason for revert: Linux MSAN failing
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/49282/overview
    
    Original change's description:
    > Make actions in the "engaged" state higher priority for visibility during layout.
    >
    > This cl turns the flex weight property using for
    > PinnedToolbarActionsContainerLayout into a priority enum. Views with
    > high priority are always visible in the layout, views with medium
    > priority (used for the "engaged" state) will be included in any
    > remaining space, and views with low priority will be then included in
    > any space left.
    >
    > Bug: 329301688
    > Change-Id: Ib52578680c3298b8858caf8e2cfff1cc47d1c96c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5721992
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Caroline Rising <corising@chromium.org>
    > Reviewed-by: David Pennington <dpenning@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1330634}
    
    Bug: 329301688
    Change-Id: I71d267a0f264696b958def835711692b2544b643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724736
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1330902}

--- chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.h
+++ chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.h
@@ -9,10 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 #include <string>
-#include <type_traits>
 
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container_layout.h"
 #include "chrome/browser/ui/views/toolbar/pinned_toolbar_button_status_indicator.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_button.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -119,8 +117,6 @@ class PinnedActionToolbarButtonActionViewInterface
   raw_ptr<PinnedActionToolbarButton> action_view_;
 };
 
-extern const ui::ClassProperty<
-    std::underlying_type_t<PinnedToolbarActionFlexPriority>>* const
-    kToolbarButtonFlexPriorityKey;
+extern const ui::ClassProperty<int>* const kToolbarButtonFlexWeightKey;
 
 #endif  // CHROME_BROWSER_UI_VIEWS_TOOLBAR_PINNED_ACTION_TOOLBAR_BUTTON_H_
