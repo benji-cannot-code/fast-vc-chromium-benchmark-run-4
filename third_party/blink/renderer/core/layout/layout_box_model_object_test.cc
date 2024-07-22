9ca52140a875c750717b1b664054420c9db2aa76
Koji Ishii <kojii@chromium.org>
2024-07-22 06:19:19 +0000

    [layout-unit] Use the three-way comparison (spaceship) operator
    This patch replaces existing comparison operator overloads
    with the three-way comparison (spaceship) operators, as
    allowed at [1].
    
    Some cases became ambiguous due to the semantics changes in
    C++20. Affected callers were disambiguated.
    
    Operators for `int` aren't included because it makes too many
    cases ambiguous. They will be investigated further.
    
    This patch has no behavior changes.
    
    [1] https://chromium.googlesource.com/chromium/src/+/HEAD/styleguide/c++/c++-features.md#Three_way-comparison-spaceship_operator-allowed
    
    Bug: 351092748, 40256175
    Change-Id: Ie9cc4e825145be4f415c2c5003a4f647db28f22a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726632
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1330918}

--- third_party/blink/renderer/core/layout/layout_box_model_object_test.cc
+++ third_party/blink/renderer/core/layout/layout_box_model_object_test.cc
@@ -135,7 +135,7 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionConstraints) {
   const auto* constraints = sticky->StickyConstraints();
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
-  ASSERT_EQ(0.f, constraints->top_inset);
+  ASSERT_EQ(0.f, constraints->top_inset->ToFloat());
 
   // The coordinates of the constraint rects should all be with respect to the
   // unscrolled scroller.
@@ -227,7 +227,7 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionInlineConstraints) {
   const auto* constraints = sticky->StickyConstraints();
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
-  EXPECT_EQ(10.f, constraints->top_inset);
+  EXPECT_EQ(10.f, constraints->top_inset->ToFloat());
 
   // The coordinates of the constraint rects should all be with respect to the
   // unscrolled scroller.
@@ -283,7 +283,7 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionVerticalRLInlineConstraints) {
   const auto* constraints = sticky->StickyConstraints();
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
-  EXPECT_EQ(10.f, constraints->top_inset);
+  EXPECT_EQ(10.f, constraints->top_inset->ToFloat());
 
   // The coordinates of the constraint rects should all be with respect to the
   // unscrolled scroller.
@@ -323,7 +323,7 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionTransforms) {
   const auto* constraints = sticky->StickyConstraints();
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
-  ASSERT_EQ(0.f, constraints->top_inset);
+  ASSERT_EQ(0.f, constraints->top_inset->ToFloat());
 
   // The coordinates of the constraint rects should all be with respect to the
   // unscrolled scroller.
@@ -360,7 +360,7 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionPercentageStyles) {
   const auto* constraints = sticky->StickyConstraints();
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
-  ASSERT_EQ(0.f, constraints->top_inset);
+  ASSERT_EQ(0.f, constraints->top_inset->ToFloat());
 
   if (RuntimeEnabledFeatures::LayoutIgnoreMarginsForStickyEnabled()) {
     ASSERT_EQ(
@@ -503,13 +503,16 @@ TEST_P(LayoutBoxModelObjectTest, StickyPositionConstraintInvalidation) {
   ASSERT_TRUE(constraints);
   EXPECT_TRUE(HasStickyLayer(scrollable_area, sticky));
 
-  EXPECT_EQ(25.f, constraints->scroll_container_relative_sticky_box_rect.X());
+  EXPECT_EQ(
+      25.f,
+      constraints->scroll_container_relative_sticky_box_rect.X().ToFloat());
   To<HTMLElement>(target->GetNode())->classList().Add(AtomicString("hide"));
   // After updating layout we should have the updated position.
   GetDocument().View()->UpdateLifecycleToLayoutClean(
       DocumentUpdateReason::kTest);
   EXPECT_EQ(50.f, sticky->StickyConstraints()
-                      ->scroll_container_relative_sticky_box_rect.X());
+                      ->scroll_container_relative_sticky_box_rect.X()
+                      .ToFloat());
 }
 
 TEST_P(LayoutBoxModelObjectTest, StickyPositionStatusChange) {
