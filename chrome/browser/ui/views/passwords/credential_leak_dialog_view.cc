dc2249ffd31fc0acff1e8f6aa2480832b6aa90a2
Fred Shih <ffred@chromium.org>
2024-07-22 19:10:06 +0000

    Refactor raw pointer usage in CredentialLeakDialogView
    We can use ExtractAsDangling() to avoid holding onto an unprotected raw
    pointer.
    
    Change-Id: Ib457e5c641f9379285bae6d94bd37f13c482b6b0
    Bug: 346693520
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5722843
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331233}

--- chrome/browser/ui/views/passwords/credential_leak_dialog_view.cc
+++ chrome/browser/ui/views/passwords/credential_leak_dialog_view.cc
@@ -65,9 +65,7 @@ CredentialLeakDialogView::CredentialLeakDialogView(
     //
     // Note that when this lambda gets bound it closes over &controller_, not
     // controller_ itself!
-    auto* raw_controller = controller->get();
-    *controller = nullptr;
-    (raw_controller->*(fn))();
+    (controller->ExtractAsDangling()->*(fn))();
   };
 
   SetAcceptCallback(
