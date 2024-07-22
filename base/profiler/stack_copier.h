d1c1667a4109a2636b8fc75592d58b6203a5ab24
Nico Weber <thakis@chromium.org>
2024-07-22 16:01:53 +0000

    base/profiler/stack_copier.h: IWYU for <vector>
    No behavior change. Needed for an upcoming libc++ roll.
    
    Bug: 354621206
    Change-Id: Ie2abe1a14abe81694c8158227e0b95151cc2d368
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730413
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331090}

--- base/profiler/stack_copier.h
+++ base/profiler/stack_copier.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include <vector>
+
 #include "base/base_export.h"
 #include "base/profiler/register_context.h"
 #include "base/time/time.h"
