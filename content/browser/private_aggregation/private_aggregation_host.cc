ca2f55173343728b13c838ea5b29b88525b11780
Dan McArdle <dmcardle@chromium.org>
2024-07-22 21:12:10 +0000

    PrivateAggregationHost: Add test for negative `remaining_timeout` case
    This is a behavior-preserving change.
    
    This CL adds a test that exercise the negative `remaining_timeout` edge
    case. As part of implementing the test, it adds a callback field to
    `PrivateAggregationHost`, which can only be set via a test-only mutator.
    
    Bug: 354124875
    Change-Id: Ie44359d960482ecffd648067fcd5af89683050f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725154
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Dan McArdle <dmcardle@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331328}

--- content/browser/private_aggregation/private_aggregation_host.cc
+++ content/browser/private_aggregation/private_aggregation_host.cc
@@ -462,7 +462,6 @@ void PrivateAggregationHost::OnReceiverDisconnected() {
     return;
   }
 
-  // The timeout hasn't been reached.
   CHECK(current_context.timeout_timer->IsRunning());
   pipes_with_timeout_count_--;
 
