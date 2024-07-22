f0b61a48646e9423db15964402dedeef7ccbd930
Michele Fan <michelefan@chromium.org>
2024-07-22 18:03:21 +0000

    snap-group: Add browser test for save-desk support with incognito
    Browser verification video: http://b/340305763#comment12
    
    Bug: b/340305763
    Test: Added browser test + manually
    Change-Id: If8f0ededcaa3e1443854b007d1b0dadc8c0d01bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5727258
    Commit-Queue: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331190}

--- ash/wm/snap_group/snap_group_unittest.cc
+++ ash/wm/snap_group/snap_group_unittest.cc
@@ -276,6 +276,9 @@ void DragWindowTo(ui::test::EventGenerator* event_generator,
   }
 }
 
+// -----------------------------------------------------------------------------
+// SnapGroupTestBase:
+
 class SnapGroupTestBase : public OverviewTestBase {
  public:
   template <typename... TaskEnvironmentTraits>
