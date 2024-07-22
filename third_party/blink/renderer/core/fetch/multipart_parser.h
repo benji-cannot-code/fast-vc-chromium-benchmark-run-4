47ee54cd61b018f36d482a8853212946fd8f3372
Fredrik Söderqvist <fs@opera.com>
2024-07-22 13:38:24 +0000

    spanification: Spanify MultipartParser::Matcher
    Replace the match-data pointer+size with a span. Update interface to
    talk spans.
    
    Bug: 351564777
    Change-Id: Id41910258218918d6f63944cc33b0ee3fdabdcbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725249
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1331026}

--- third_party/blink/renderer/core/fetch/multipart_parser.h
+++ third_party/blink/renderer/core/fetch/multipart_parser.h
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_MULTIPART_PARSER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FETCH_MULTIPART_PARSER_H_
 
@@ -66,26 +61,29 @@ class CORE_EXPORT MultipartParser final
 
    public:
     Matcher();
-    Matcher(const char* data, size_t num_matched_bytes, size_t);
+    Matcher(base::span<const char> match_data, size_t num_matched_bytes);
 
     bool Match(char value) {
-      DCHECK_LT(num_matched_bytes_, size_);
-      if (value != data_[num_matched_bytes_])
+      if (value != match_data_[num_matched_bytes_]) {
         return false;
+      }
       ++num_matched_bytes_;
       return true;
     }
-    bool Match(const char* first, const char* last);
-    bool IsMatchComplete() const { return num_matched_bytes_ == size_; }
+    bool Match(base::span<const char> data);
+    bool IsMatchComplete() const {
+      return num_matched_bytes_ == match_data_.size();
+    }
     size_t NumMatchedBytes() const { return num_matched_bytes_; }
     void SetNumMatchedBytes(size_t);
 
-    const char* Data() const { return data_; }
+    base::span<const char> MatchedData() const {
+      return match_data_.first(num_matched_bytes_);
+    }
 
    private:
-    const char* data_ = nullptr;
+    base::span<const char> match_data_;
     size_t num_matched_bytes_ = 0u;
-    size_t size_ = 0u;
   };
 
   Matcher CloseDelimiterSuffixMatcher() const;
