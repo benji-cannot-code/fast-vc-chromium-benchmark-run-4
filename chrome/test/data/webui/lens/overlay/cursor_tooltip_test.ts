7d0dcf04c200e0241484831950a6de48b08dde73
Duncan Mercer <mercerd@google.com>
2024-07-22 21:34:34 +0000

    [lensoverlay] Fix CursorTooltipTest
    Bug: 354711906, 354077610
    Change-Id: Ib3a343bbab59d3c68013dba67258656e03e027b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732160
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331343}

--- chrome/test/data/webui/lens/overlay/cursor_tooltip_test.ts
+++ chrome/test/data/webui/lens/overlay/cursor_tooltip_test.ts
@@ -55,7 +55,6 @@ suite('OverlayCursorTooltip', () => {
     // to prevent from causing issues in the tests. Also override localized
     // strings so tests don't fail if the strings change.
     loadTimeData.overrideValues({
-      'debugMode': true,
       'enableShimmer': false,
       'cursorTooltipTextHighlightMessage': 'Select text',
       'cursorTooltipClickMessage': 'Select object',
@@ -65,18 +64,11 @@ suite('OverlayCursorTooltip', () => {
 
     lensOverlayElement = document.createElement('lens-overlay-app');
     document.body.appendChild(lensOverlayElement);
-    waitBeforeNextRender(lensOverlayElement);
+    await waitBeforeNextRender(lensOverlayElement);
 
     selectionOverlayElement = lensOverlayElement.$.selectionOverlay;
     cursorTooltip = lensOverlayElement.$.cursorTooltip;
     tooltipEl = cursorTooltip.$.cursorTooltip;
-
-    // Since the size of the Selection Overlay is based on the screenshot which
-    // is not loaded in the test, we need to force the overlay to take up the
-    // viewport.
-    selectionOverlayElement.$.selectionOverlay.style.width = '100%';
-    selectionOverlayElement.$.selectionOverlay.style.height = '100%';
-    flushTasks();
 
     // Send a fake screenshot to unhide the selection overlay.
     const dataUriBytes = new TextEncoder().encode(SCREENSHOT_DATA_URI);
@@ -85,6 +77,13 @@ suite('OverlayCursorTooltip', () => {
         bytes: Array.from(dataUriBytes),
       } as BigBuffer,
     });
+
+    // Since the size of the Selection Overlay is based on the screenshot which
+    // is not loaded in the test, we need to force the overlay to take up the
+    // viewport.
+    selectionOverlayElement.$.selectionOverlay.style.width = '100%';
+    selectionOverlayElement.$.selectionOverlay.style.height = '100%';
+    await waitBeforeNextRender(lensOverlayElement);
 
     await addWords();
     await addObjects();
@@ -101,7 +100,7 @@ suite('OverlayCursorTooltip', () => {
     };
   }
 
-  function addWords() {
+  function addWords(): Promise<void> {
     const text = createText([
       createParagraph([
         createLine([
@@ -118,7 +117,7 @@ suite('OverlayCursorTooltip', () => {
     return flushTasks();
   }
 
-  function addObjects() {
+  function addObjects(): Promise<void> {
     const objects =
         [
           {x: 80, y: 20, width: 25, height: 10},
@@ -251,7 +250,8 @@ suite('OverlayCursorTooltip', () => {
 
     // Open the info menu.
     lensOverlayElement.$.moreOptionsButton.click();
-    assertFalse(isRendered(lensOverlayElement.$.moreOptionsMenu));
+    await flushTasks();
+    assertTrue(isVisible(lensOverlayElement.$.moreOptionsMenu));
 
     // Hover the background.
     await simulateUnhover(lensOverlayElement.$.moreOptionsButton);
