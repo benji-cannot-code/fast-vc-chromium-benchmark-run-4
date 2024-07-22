c493eba9db8c17ff3af232b8cb0f51dd669be25b
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-07-22 14:34:23 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to testing/*
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    Note that in patchset2, change to:
    /build/config/unsafe_buffers_paths.txt
    was reverted. Indeed, running too many (~3) CQ run touching this file is
    making the builder cache much slower. I will bundle every change to this
    file in a subsequent CL. I will limit myself to 1-2 CQ run per day.
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 40285824
    Change-Id: I08bd40253cf2d478190e566d66eab1f92f503448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718670
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Ben Pastene <bpastene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331042}

--- testing/android/native_test/native_test_launcher.cc
+++ testing/android/native_test/native_test_launcher.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 // This class sets up the environment for running the native tests inside an
 // android application. It outputs (to a fifo) markers identifying the
 // START/PASSED/CRASH of the test suite, FAILURE/SUCCESS of individual tests,
