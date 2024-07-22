0f1625d4fb4978838ba40d6684a111ea43d97cc9
John Stiles <johnstiles@google.com>
2024-07-22 18:06:24 +0000

    Detect modifications to the canvas during WebGPU access.
    We now steal away the canvas' resource provider when transferring
    the canvas to a WebGPU texture, and restore the resource provider
    when transferring back. If the canvas is drawn to in the interim,
    this is detected and now raises an exception.
    
    Change-Id: Idfe66a9ba98023ab383f176594600909b79bb4f8
    Bug: 343522739
    Bug: 351903350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5685133
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: John Stiles <johnstiles@google.com>
    Auto-Submit: John Stiles <johnstiles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331193}

---
+++ third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_transfer/transferBackFromGPUTexture-disallows-transfer-back-after-draw.https.worker.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=worker
+
+// ============================================================================
+
+importScripts("/resources/testharness.js");
+importScripts("./webgpu-helpers.js");
+
+// This test parallels
+// transferBackFromGPUTexture-disallows-transfer-back-after-draw.https.html.
+promise_test(() => {
+    return with_webgpu((adapter, adapterInfo, device) => {
+      return test_canvas_disallows_transfer_back_after_draw(
+          device,
+          new OffscreenCanvas(50, 50));
+    });
+  },
+  'transferBackFromGPUTexture() on a worker should not allow transferring ' +
+  'back after drawing to the canvas.'
+);
+
+done();
