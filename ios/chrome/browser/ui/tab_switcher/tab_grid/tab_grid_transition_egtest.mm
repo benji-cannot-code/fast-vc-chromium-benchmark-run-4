572324c47547b54320a2b3494ae4dc2e3a76d4c5
Justin Cohen <justincohen@chromium.org>
2024-07-22 16:07:29 +0000

    Revert "[ios] Fix test testLeaveSwitcherAfterEnteringAndExittingInactiveTabs"
    This reverts commit 84370d74ba652da29b093155e71d69b6ec66a1d9.
    
    Reason for revert: Failing here:
    https://ci.chromium.org/ui/p/chromium/builders/ci/ios18-sdk-simulator/772/test-results?q=ExactID%3Aninja%3A%2F%2Fios%2Fchrome%2Ftest%2Fearl_grey2%3Aios_chrome_ui_eg2tests_module%2FTabSwitcherTransitionTestCase%2FtestLeaveSwitcherAfterEnteringAndExittingInactiveTabs+VHash%3Af4dc1b850f2f9ace
    
    Original change's description:
    > [ios] Fix test testLeaveSwitcherAfterEnteringAndExittingInactiveTabs
    >
    > This CL adds a workaround to fix test in
    > TabSwitcherTransitionTestCase when run on iOS18 devices.
    >
    > Bug: 349816094
    > Change-Id: I97c98787fc53c2abab43a0dfd27334ff6cf2dbe4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713310
    > Auto-Submit: Federica Germinario <fedegermi@google.com>
    > Commit-Queue: Federica Germinario <fedegermi@google.com>
    > Reviewed-by: Louis Romero <lpromero@google.com>
    > Commit-Queue: Louis Romero <lpromero@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1328141}
    
    Bug: 349816094
    Change-Id: Ia4f1bb6fcf84a54ebe8319e272582545be458779
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724475
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331098}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_transition_egtest.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_transition_egtest.mm
@@ -711,17 +711,10 @@ void ExpectIdleHistogramBucketCount(const char* histogram,
   ExpectIdleHistogramCount(kUMATabSwitcherIdleIncognitoTabGridPageHistogram, 0);
 
   [ChromeEarlGrey showTabSwitcher];
-  if (@available(iOS 18, *)) {
-    XCUIApplication* app = [[XCUIApplication alloc] init];
-    if ([app.buttons[kInactiveTabsButtonAccessibilityIdentifier] exists]) {
-      [app.buttons[kInactiveTabsButtonAccessibilityIdentifier] tap];
-    }
-  } else {
-    [[EarlGrey
-        selectElementWithMatcher:
-            grey_accessibilityID(kInactiveTabsButtonAccessibilityIdentifier)]
-        performAction:grey_tap()];
-  }
+  [[EarlGrey
+      selectElementWithMatcher:grey_accessibilityID(
+                                   kInactiveTabsButtonAccessibilityIdentifier)]
+      performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:testing::NavigationBarBackButton()]
       performAction:grey_tap()];
   ShowTabViewController();
