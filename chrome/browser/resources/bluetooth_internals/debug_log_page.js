7e6eae59e7615c8cb92f8c0415f77aafdef61795
Archie Pusaka <apusaka@google.com>
2024-07-22 06:48:44 +0000

    bluetooth: Update string for btsnoop logging feature
    Adjust the strings according to suggestion from UI review.
    
    Bug: b/327298508
    Test: None
    Change-Id: Iacbda4ffcf6a81df6cc9532e0ef804608357d1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724649
    Commit-Queue: Archie Pusaka <apusaka@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>
    Cr-Commit-Position: refs/heads/main@{#1330924}

--- chrome/browser/resources/bluetooth_internals/debug_log_page.js
+++ chrome/browser/resources/bluetooth_internals/debug_log_page.js
@@ -69,7 +69,7 @@ export class DebugLogPage extends Page {
   setUpBtmonButton() {
     const elem = /** @type {!HTMLInputElement} */ ($('btmon-start-btn'));
     elem.addEventListener('click', this.onStartBtsnoopClick.bind(this));
-    this.setBtmonButtonText('Start snoop');
+    this.setBtmonButtonText('Start logging');
   }
 
   setBtmonButtonText(text) {
@@ -95,11 +95,11 @@ export class DebugLogPage extends Page {
     const {btsnoop: btsnoopInterface} =
         await this.bluetoothInternalsHandler_.startBtsnoop();
     if (btsnoopInterface != null) {
-      this.setBtmonStatusText('Btmon is running.');
-      this.setBtmonButtonText('Stop snoop');
+      this.setBtmonStatusText('Logging is ongoing.');
+      this.setBtmonButtonText('Stop logging');
       this.btsnoopInterface_ = btsnoopInterface;
     } else {
-      this.setBtmonStatusText('Fail to start btmon.');
+      this.setBtmonStatusText('Fail to start logging.');
       this.btsnoopInterface_ = null;
     }
   }
@@ -112,11 +112,11 @@ export class DebugLogPage extends Page {
     const {success} = await this.btsnoopInterface_.stop();
     if (success) {
       this.setBtmonStatusText(
-          'Btmon is stopped. Log is saved as capture.btsnoop in your Downloads directory.');
+          'Logging is stopped. Log is saved to Downloads as capture.btsnoop.');
     } else {
-      this.setBtmonStatusText('Fail to save snoop log.');
+      this.setBtmonStatusText('Fail to save debug log.');
     }
-    this.setBtmonButtonText('Start snoop');
+    this.setBtmonButtonText('Start logging');
     this.btsnoopInterface_ = null;
   }
 
