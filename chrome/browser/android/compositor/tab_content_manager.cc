2b6d2b2d17a4ab1a8971143fd2fcef6f08f84a89
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-22 23:06:02 +0000

    [Tab Thumbnails] Ignore possibly stuck captures when preparing tabs
    The fact we get stuck waiting on a pending capture seems to be one of
    the primary causes of the testToolbarMenuItem_GroupActionAndUndo being
    broken.
    
    I suspect that since this is a batched test a previous test in the suite
    did not wait for a thumbnail and its capture got stuck pending for a
    long time.
    
    Remove these in_flight_captures_ in native more eagerly.
    1) Remove the in_flight_captures_ for tabs that get closed.
    2) Before a capture iterate over the map and remove any elements where
       the tracker's weak pointer got invalidated.
    
    Bug: 353332369
    Change-Id: I508bb9ef13a45c21bc253da3dcafa365e2150c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731599
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331408}

--- chrome/browser/android/compositor/tab_content_manager.cc
+++ chrome/browser/android/compositor/tab_content_manager.cc
@@ -223,6 +223,7 @@ content::RenderWidgetHostView* TabContentManager::GetRwhvForTab(
 
 std::unique_ptr<thumbnail::ThumbnailCaptureTracker, base::OnTaskRunnerDeleter>
 TabContentManager::TrackCapture(thumbnail::TabId tab_id) {
+  CleanupTrackers();
   std::unique_ptr<thumbnail::ThumbnailCaptureTracker, base::OnTaskRunnerDeleter>
       tracker(new thumbnail::ThumbnailCaptureTracker(
                   base::BindOnce(&TabContentManager::OnTrackingFinished,
@@ -244,6 +245,11 @@ void TabContentManager::OnTrackingFinished(
   if (it->second.get() == tracker) {
     in_flight_captures_.erase(it);
   }
+}
+
+void TabContentManager::CleanupTrackers() {
+  base::EraseIf(in_flight_captures_,
+                [](const auto& pair) -> bool { return !pair.second; });
 }
 
 void TabContentManager::CaptureThumbnail(
@@ -331,6 +337,7 @@ void TabContentManager::NativeRemoveTabThumbnail(int tab_id) {
     readback_iter->second->SetToDropAfterReadback();
   }
   thumbnail_cache_->Remove(tab_id);
+  in_flight_captures_.erase(tab_id);
 }
 
 void TabContentManager::RemoveTabThumbnail(JNIEnv* env, jint tab_id) {
