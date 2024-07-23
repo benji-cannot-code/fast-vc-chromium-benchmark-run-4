ec3c8710bdc5e11b46109aa3cf6d679f071dac82
Nico Weber <thakis@chromium.org>
2024-07-23 01:07:55 +0000

    cros: IWYU fixes for upcoming libc++ roll
    Instead of adding <format> includes for std::format()-using files,
    move those to base::StringPrintf(), as advised by PRESUBMIT.
    
    No intended behavior change.
    
    Bug: 354621206
    Change-Id: I7234e55811fcbb0fc9db767ebf77f93a1d40873d
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730109
    Reviewed-by: Zequan Wu <zequanwu@google.com>
    Owners-Override: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Commit-Queue: Zequan Wu <zequanwu@google.com>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331466}

--- ash/public/cpp/login_screen_model.h
+++ ash/public/cpp/login_screen_model.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_PUBLIC_CPP_LOGIN_SCREEN_MODEL_H_
 
 #include <string>
+#include <vector>
 
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/time/time.h"
--- chrome/browser/ui/webui/ash/shortcut_customization/integration_tests/shortcut_customization_interactive_uitest.cc
+++ chrome/browser/ui/webui/ash/shortcut_customization/integration_tests/shortcut_customization_interactive_uitest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/settings/public/constants/routes.mojom-forward.h"
 #include "ash/webui/shortcut_customization_ui/url_constants.h"
 #include "base/json/json_writer.h"
+#include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/test/base/ash/interactive/interactive_ash_test.h"
@@ -224,8 +225,8 @@ class ShortcutCustomizationInteractiveUiTest : public InteractiveAshTest {
 
   auto AddKeyboard(bool is_external) {
     return Steps(
-        Log(std::format("Adding {0} keyboard",
-                        is_external ? "external" : "internal")),
+        Log(base::StringPrintf("Adding %s keyboard",
+                               is_external ? "external" : "internal")),
         Do([is_external, this]() {
           int id = kDeviceId1++;
           const auto id_string = base::NumberToString(id);
@@ -272,8 +273,8 @@ class ShortcutCustomizationInteractiveUiTest : public InteractiveAshTest {
   auto WaitForShortcutToContainNumAcceleartors(const DeepQuery& query,
                                                const int expected) {
     return Steps(
-        Log(std::format("Expecting shortcut to contain {0} accelerators",
-                        expected)),
+        Log(base::StringPrintf("Expecting shortcut to contain %d accelerators",
+                               expected)),
         CheckJsResultAt(webcontents_id_, query,
                         "e => e.querySelectorAll('accelerator-view').length",
                         expected));
@@ -319,13 +320,15 @@ class ShortcutCustomizationInteractiveUiTest : public InteractiveAshTest {
       int category_index,
       const std::vector<std::string>& expected_subcategories) {
     return Steps(
-        Log(std::format("Verifying that '{0}' is the active category when "
-                        "the Shortcut Customization app is first launched",
-                        category)),
+        Log(base::StringPrintf(
+            "Verifying that '%s' is the active category when "
+            "the Shortcut Customization app is first launched",
+            category.c_str())),
         WaitForElementTextContains(webcontents_id_, kActiveNavTabQuery,
                                    category),
-        Log(std::format("Verifying subcategories within the '{0}' category",
-                        category)),
+        Log(base::StringPrintf(
+            "Verifying subcategories within the '%s' category",
+            category.c_str())),
         CheckJsResult(
             webcontents_id_,
             base::StringPrintf(
