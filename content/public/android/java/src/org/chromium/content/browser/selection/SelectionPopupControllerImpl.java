142dee78c4f330f6ba302470595ab281015a8c96
Zakarie Yaris <yaris@google.com>
2024-07-23 11:44:12 +0000

    Add tests for correct menu item generation [8/8].
    These instrumentation tests ensure that
    correct menu items are added to the action
    mode depending on the state.
    
    Bug: 320643289
    Change-Id: I3d55cf57d31c14bc69c4afee7fc34b49ae360d4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5645224
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Commit-Queue: Zakarie Yaris <yaris@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331671}

--- content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
+++ content/public/android/java/src/org/chromium/content/browser/selection/SelectionPopupControllerImpl.java
@@ -650,7 +650,7 @@ public class SelectionPopupControllerImpl extends ActionModeCallbackHelper
         if (!isActionModeValid() && hasSelection()) clearSelection();
     }
 
-    void dismissTextHandles() {
+    private void dismissTextHandles() {
         if (mWebContents.getRenderWidgetHostView() != null) {
             mWebContents.getRenderWidgetHostView().dismissTextHandles();
         }
