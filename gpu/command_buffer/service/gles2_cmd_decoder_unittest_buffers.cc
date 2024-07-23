f4edd283476114c7680cf1e250ce0a6c1585cee4
Ari Chivukula <arichiv@chromium.org>
2024-07-23 19:19:11 +0000

    Replace std::unique_ptr<T[]> with HeapArray for: gpu/command_buffer/service/gles2_cmd_decoder_unittest_buffers.cc
    We need to replace occurrences of std::unique_ptr<T[]> with
    base::HeapArray<T>. Using std::unique_ptr<T[]> does not automatically
    preserve the size of the allocation. This forces a need for ad-hoc
    bounds checks, leading to bugs which attackers use to compromise our
    users.
    
    https://docs.google.com/document/d/1YsPR8GoN8VTP1ABKCISaQkuBif1Cn80cTxTjsM8QT4s/edit
    
    Fixed: 326458558
    Change-Id: I1e2fac2d8ff1a0715691cd994bf6347cb2c009d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734357
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331923}

--- gpu/command_buffer/service/gles2_cmd_decoder_unittest_buffers.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder_unittest_buffers.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
+#include "base/containers/heap_array.h"
 #include "gpu/command_buffer/service/gles2_cmd_decoder.h"
 #include "gpu/command_buffer/service/gles2_cmd_decoder_unittest.h"
 
@@ -706,11 +707,11 @@ TEST_P(GLES3DecoderTest, CopyBufferSubDataValidArgs) {
   // Set up the buffer so first half is kValue0 and second half is kValue1.
   DoBindBuffer(kTarget, client_buffer_id_, kServiceBufferId);
   DoBufferData(kTarget, kSize);
-  std::unique_ptr<char[]> data(new char[kHalfSize]);
-  memset(data.get(), kValue0, kHalfSize);
-  DoBufferSubData(kTarget, 0, kHalfSize, data.get());
-  memset(data.get(), kValue1, kHalfSize);
-  DoBufferSubData(kTarget, kHalfSize, kHalfSize, data.get());
+  auto data = base::HeapArray<char>::Uninit(kHalfSize);
+  memset(data.data(), kValue0, kHalfSize);
+  DoBufferSubData(kTarget, 0, kHalfSize, data.data());
+  memset(data.data(), kValue1, kHalfSize);
+  DoBufferSubData(kTarget, kHalfSize, kHalfSize, data.data());
   EXPECT_EQ(GL_NO_ERROR, GetGLError());
   Buffer* buffer = GetBuffer(client_buffer_id_);
   EXPECT_TRUE(buffer);
