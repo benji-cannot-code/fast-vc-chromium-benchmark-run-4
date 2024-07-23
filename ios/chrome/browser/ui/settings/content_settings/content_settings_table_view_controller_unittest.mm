734b0db6cbc89a4f998bf42307d883579eb8bfbb
Olivier ROBIN <olivierrobin@google.com>
2024-07-23 16:51:56 +0000

    Revert Measurement experiment flag
    The feature is breaking some websites.
    Launch was reverted in https://critique.corp.google.com/cl/654764088
    
    Change-Id: If7ebfc5960f4d32f81b69d89fc7c3532f1605072
    Bug: 354922853
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5729975
    Auto-Submit: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331801}

--- ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller_unittest.mm
+++ ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller_unittest.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/settings/content_settings/content_settings_table_view_controller.h"
 
 #import "base/apple/foundation_util.h"
+#import "base/feature_list.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
 #import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/ui/table_view/legacy_chrome_table_view_controller_test.h"
@@ -55,7 +56,11 @@ TEST_F(ContentSettingsTableViewControllerTest,
   } else {
     ASSERT_EQ(1, NumberOfSections());
   }
-  ASSERT_EQ(5, NumberOfItemsInSection(0));
+  if (base::FeatureList::IsEnabled(web::features::kEnableMeasurements)) {
+    ASSERT_EQ(5, NumberOfItemsInSection(0));
+  } else {
+    ASSERT_EQ(4, NumberOfItemsInSection(0));
+  }
   CheckDetailItemTextWithIds(IDS_IOS_BLOCK_POPUPS, IDS_IOS_SETTING_ON, 0, 0);
 }
 
