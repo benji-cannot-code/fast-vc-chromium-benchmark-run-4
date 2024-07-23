c3fa5b7df1609d2c3d493dd521477d9893f78469
Marc Treib <treib@chromium.org>
2024-07-23 17:08:42 +0000

    Signin: Metrics to track consumer vs managed (enterprise/EDU) accounts
    This CL introduces histograms that record, on profile startup, the
    number of accounts in the IdentityManager, and the type of each
    account, i.e. if it's a consumer or a managed account. Consumer
    accounts are further broken down into "gmail.com" vs "other well-known
    consumer domain" vs "other consumer domain", based on the
    functionality in AccountManagedStatusFinder.
    
    Bug: 353655763
    Change-Id: If65c235f9d47eec8416c9cb1d1cfbbed94b1c891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5718355
    Commit-Queue: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331819}

--- components/signin/core/browser/signin_metrics_service.h
+++ components/signin/core/browser/signin_metrics_service.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/scoped_observation.h"
 #include "components/keyed_service/core/keyed_service.h"
+#include "components/signin/core/browser/account_management_type_metrics_recorder.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 
@@ -76,6 +77,8 @@ class SigninMetricsService : public KeyedService,
 
   raw_ref<signin::IdentityManager> identity_manager_;
   const raw_ref<PrefService> pref_service_;
+
+  signin::AccountManagementTypeMetricsRecorder management_type_recorder_;
 
   base::ScopedObservation<signin::IdentityManager,
                           signin::IdentityManager::Observer>
