8426fd0d5e550f109bbd2eeb942db793884dc3b6
Jose Dapena Paz <jdapena@igalia.com>
2024-07-23 19:35:49 +0000

    IWYU: usage of SSLCertRequestInfo requires include at stream_attempt.cc
    Converting from nullptr to std::unique_ptr<> requires declaration of
    the type.
    
    Bug: 41455655
    Change-Id: Ida65d996cc3c579d5e7890b440bd642c97cea8b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5675543
    Reviewed-by: Eric Orth <ericorth@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1331937}

--- net/socket/stream_attempt.cc
+++ net/socket/stream_attempt.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/log/net_log.h"
 #include "net/log/net_log_with_source.h"
 #include "net/socket/stream_socket.h"
+#include "net/ssl/ssl_cert_request_info.h"
 
 namespace net {
 
