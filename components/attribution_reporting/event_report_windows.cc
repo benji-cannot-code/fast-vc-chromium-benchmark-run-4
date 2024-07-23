d27b23d8b16ab64a0fd327f68b009ed8aef4c713
Andrew Paseltiner <apaseltiner@chromium.org>
2024-07-23 20:20:30 +0000

    Add metric to track usage of legacy event_report_window field
    To help determine whether it can be deprecated and removed, as the
    event_report_windows (note the trailing S) field subsumes it.
    
    Change-Id: I6221bc0795fde179745b7a2cd031f6e574577e32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735014
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331969}

--- components/attribution_reporting/event_report_windows.cc
+++ components/attribution_reporting/event_report_windows.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/containers/flat_set.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/not_fatal_until.h"
 #include "base/ranges/algorithm.h"
 #include "base/time/time.h"
@@ -207,6 +208,10 @@ EventReportWindows::FromJSON(const base::Value::Dict& registration,
                              SourceType source_type) {
   const base::Value* singular_window = registration.Find(kEventReportWindow);
   const base::Value* multiple_windows = registration.Find(kEventReportWindows);
+
+  base::UmaHistogramBoolean("Conversions.LegacyEventReportWindow",
+                            !!singular_window);
+
   if (singular_window && multiple_windows) {
     return base::unexpected(
         SourceRegistrationError::kBothEventReportWindowFieldsFound);
