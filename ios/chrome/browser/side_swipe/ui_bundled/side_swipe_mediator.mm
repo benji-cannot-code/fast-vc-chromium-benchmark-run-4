6885eb03066f9c96e1895117170c9a720bfb6a41
ginnnnnnny <ginnyhuang@chromium.org>
2024-07-23 15:06:22 +0000

    [iOS][ffr] Enable animated side swipe for un-navigatable gestures
    There are unavoidable chances when the side swipe IPH appears on top of
    an page that somehow becomes unnavigatable when the user starts swiping.
    To make sure the IPH still responds to the user gesture without running
    into an error, we make the animated side swipe responds the way it does
    when the page itself is not navigateble.
    
    Animated screenshot: https://screenshot.googleplex.com/SCZ8HupGzi6chRF
    
    Fixed: 339829390
    Change-Id: Iae316f9c3fc1d3c81d68ad8cb016e8e5105e1774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730097
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331747}

--- ios/chrome/browser/side_swipe/ui_bundled/side_swipe_mediator.mm
+++ ios/chrome/browser/side_swipe/ui_bundled/side_swipe_mediator.mm
@@ -405,13 +405,6 @@ const CGFloat kIpadTabSwipeDistance = 100;
 // Animate page navigation.
 - (void)animatePageNavigationInDirection:
     (UISwipeGestureRecognizerDirection)direction {
-  if (![self canNavigate:IsSwipingBack(direction)]) {
-    // Back/forward state has changed when the user begins to swipe.
-    NOTREACHED(base::NotFatalUntil::M128)
-        << "Back/forward state has changed when the user begins to swipe.";
-    return;
-  }
-
   _inSwipe = YES;
   [_swipeDelegate updateAccessoryViewsForSideSwipeWithVisibility:NO];
 
@@ -424,10 +417,11 @@ const CGFloat kIpadTabSwipeDistance = 100;
                  CGRectGetWidth(navigatingBounds),
                  CGRectGetHeight(navigatingBounds) - headerHeight);
 
+  BOOL canNavigate = [self canNavigate:IsSwipingBack(direction)];
   _pageSideSwipeView = [[SideSwipeNavigationView alloc]
       initWithFrame:navigationFrame
       withDirection:direction
-        canNavigate:YES
+        canNavigate:canNavigate
               image:[UIImage imageNamed:@"side_swipe_navigation_back"]];
   [_pageSideSwipeView setTargetView:[_swipeDelegate sideSwipeContentView]];
 
@@ -438,7 +432,11 @@ const CGFloat kIpadTabSwipeDistance = 100;
   [_pageSideSwipeView
       animateHorizontalPanWithDirection:direction
                       completionHandler:^{
-                        [weakSelf handleOverThresholdCompletion:direction];
+                        if (canNavigate) {
+                          [weakSelf handleOverThresholdCompletion:direction];
+                        } else {
+                          [weakSelf handleUnderThresholdCompletion];
+                        }
                       }];
 }
 
