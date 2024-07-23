08fa92b11ed0fef5e5c689d4db8988902a0c21ba
Fiona Zhang <zfiona@google.com>
2024-07-23 18:56:28 +0000

    Pass initial enterprise endpoints to the ReportingService
    Pass ReportingEndpoints enterprise policy-configured endpoints to
    the ReportingService after the policy endpoint values are initially set.
    This allows enterprise endpoints to be eventually added to a pool of
    endpoints[1] where enterprise reports are allowed to be sent to. A
    future CL will add these endpoints to the ReportingCache.
    
    [1] https://www.w3.org/TR/reporting-1/#endpoint
    
    go/3pcd-enterprise-reporting (Googlers-only)
    
    Bug: 353957526
    Change-Id: If6a571bb9db60cac82405798b74cfdf8ea58a925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726023
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Fiona Zhang <zfiona@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331913}

--- net/reporting/reporting_service.h
+++ net/reporting/reporting_service.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
+#include "base/types/optional_ref.h"
 #include "base/unguessable_token.h"
 #include "net/base/net_export.h"
 #include "net/reporting/reporting_cache.h"
@@ -52,7 +53,8 @@ class NET_EXPORT ReportingService {
   static std::unique_ptr<ReportingService> Create(
       const ReportingPolicy& policy,
       URLRequestContext* request_context,
-      ReportingCache::PersistentReportingStore* store);
+      ReportingCache::PersistentReportingStore* store,
+      const base::flat_map<std::string, GURL>& enterprise_reporting_endpoints);
 
   // Creates a ReportingService for testing purposes using an
   // already-constructed ReportingContext. The ReportingService will take
--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -2733,6 +2733,10 @@ URLRequestContextOwner NetworkContext::MakeURLRequestContext(
     builder.set_persistent_reporting_and_nel_store(nullptr);
   }
 
+  if (params_->enterprise_reporting_endpoints.has_value()) {
+    builder.set_enterprise_reporting_endpoints(
+        params_->enterprise_reporting_endpoints.value());
+  }
 #endif  // BUILDFLAG(ENABLE_REPORTING)
 
   net::HttpNetworkSessionParams session_params;
