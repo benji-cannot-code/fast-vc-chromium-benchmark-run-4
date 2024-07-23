f6107d55a736eb228118407e08ac26e122a8701c
Ari Chivukula <arichiv@chromium.org>
2024-07-23 18:20:25 +0000

    Replace std::unique_ptr<T[]> with HeapArray for: chrome/browser/extensions/api/image_writer_private/test_utils.cc
    We need to replace occurrences of std::unique_ptr<T[]> with
    base::HeapArray<T>. Using std::unique_ptr<T[]> does not automatically
    preserve the size of the allocation. This forces a need for ad-hoc
    bounds checks, leading to bugs which attackers use to compromise our
    users.
    
    https://docs.google.com/document/d/1YsPR8GoN8VTP1ABKCISaQkuBif1Cn80cTxTjsM8QT4s/edit
    
    Fixed: 326457992, 326458512
    Change-Id: I2244230d6f5c1584b30887d7a16652a9cc199538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730154
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331882}

--- chrome/browser/extensions/api/image_writer_private/test_utils.cc
+++ chrome/browser/extensions/api/image_writer_private/test_utils.cc
@@ -6,8 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/image_writer_private/test_utils.h"
 
 #include <string.h>
+
 #include <utility>
 
+#include "base/containers/heap_array.h"
 #include "base/functional/bind.h"
 #include "base/location.h"
 #include "base/path_service.h"
@@ -284,22 +286,25 @@ const base::FilePath& ImageWriterTestUtils::GetDevicePath() {
 }
 
 bool ImageWriterTestUtils::ImageWrittenToDevice() {
-  std::unique_ptr<char[]> image_buffer(new char[kTestFileSize]);
-  std::unique_ptr<char[]> device_buffer(new char[kTestFileSize]);
+  auto image_buffer = base::HeapArray<char>::Uninit(kTestFileSize);
+  auto device_buffer = base::HeapArray<char>::Uninit(kTestFileSize);
 
-  int image_bytes_read =
-      ReadFile(test_image_path_, image_buffer.get(), kTestFileSize);
+  std::optional<uint64_t> image_bytes_read =
+      ReadFile(test_image_path_, image_buffer);
 
-  if (image_bytes_read < 0)
+  if (!image_bytes_read) {
     return false;
+  }
 
-  int device_bytes_read =
-      ReadFile(test_device_path_, device_buffer.get(), kTestFileSize);
+  std::optional<uint64_t> device_bytes_read =
+      ReadFile(test_device_path_, device_buffer);
 
-  if (image_bytes_read != device_bytes_read)
+  if (image_bytes_read != device_bytes_read) {
     return false;
+  }
 
-  return memcmp(image_buffer.get(), device_buffer.get(), image_bytes_read) == 0;
+  return memcmp(image_buffer.data(), device_buffer.data(), *image_bytes_read) ==
+         0;
 }
 
 bool ImageWriterTestUtils::FillFile(const base::FilePath& file,
