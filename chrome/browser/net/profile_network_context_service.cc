5f9f26cccd2999f67d9a6992da269c0c66911446
Fiona Zhang <zfiona@google.com>
2024-07-23 17:31:52 +0000

    Pass enterprise endpoints to the ReportingService on updates
    Pass ReportingEndpoints enterprise policy-configured endpoints to
    the ReportingService after the policy endpoint values are updated.
    This allows enterprise endpoints to be eventually added to a pool of
    endpoints[1] where enterprise reports are allowed to be sent to. A
    future CL will add these endpoints to the ReportingCache.
    
    [1] https://www.w3.org/TR/reporting-1/#endpoint
    
    go/3pcd-enterprise-reporting (Googlers-only)
    
    Bug: 353957526
    Change-Id: Id4d1272d70e5ee9f7ef6a030cc011313f4ed47c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703437
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Aaron Selya <selya@google.com>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Fiona Zhang <zfiona@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331836}

--- chrome/browser/net/profile_network_context_service.cc
+++ chrome/browser/net/profile_network_context_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
+#include "base/containers/flat_map.h"
 #include "base/feature_list.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
@@ -350,6 +351,17 @@ ProfileNetworkContextService::ProfileNetworkContextService(Profile* profile)
       base::BindRepeating(&ProfileNetworkContextService::
                               UpdateCorsNonWildcardRequestHeadersSupport,
                           base::Unretained(this)));
+
+#if BUILDFLAG(ENABLE_REPORTING)
+  if (base::FeatureList::IsEnabled(
+          net::features::kReportingApiEnableEnterpriseCookieIssues)) {
+    pref_change_registrar_.Add(
+        prefs::kReportingEndpoints,
+        base::BindRepeating(
+            &ProfileNetworkContextService::UpdateEnterpriseReportingEndpoints,
+            base::Unretained(this)));
+  }
+#endif  // BUILDFLAG(ENABLE_REPORTING)
 }
 
 ProfileNetworkContextService::~ProfileNetworkContextService() = default;
@@ -778,6 +790,38 @@ void ProfileNetworkContextService::
             ->SetCorsNonWildcardRequestHeadersSupport(value);
       });
 }
+
+#if BUILDFLAG(ENABLE_REPORTING)
+base::flat_map<std::string, GURL>
+ProfileNetworkContextService::GetEnterpriseReportingEndpoints() const {
+  using FlatMap = base::flat_map<std::string, GURL>;
+  // Create the underlying container first to allow sorting to
+  // be done in a single pass.
+  FlatMap::container_type pairs;
+  const base::Value::Dict& pref_dict =
+      profile_->GetPrefs()->GetDict(prefs::kReportingEndpoints);
+  pairs.reserve(pref_dict.size());
+  // The iterator for base::Value::Dict returns a temporary value when
+  // dereferenced, so a const reference is not used below.
+  for (const auto [endpoint_name, endpoint_url] : pref_dict) {
+    GURL endpoint(endpoint_url.GetString());
+    if (endpoint.is_valid() && endpoint.SchemeIsCryptographic()) {
+      pairs.emplace_back(endpoint_name, std::move(endpoint));
+    }
+  }
+  return FlatMap(std::move(pairs));
+}
+
+void ProfileNetworkContextService::UpdateEnterpriseReportingEndpoints() {
+  base::flat_map<std::string, GURL> endpoints =
+      GetEnterpriseReportingEndpoints();
+  profile_->ForEachLoadedStoragePartition(
+      [&](content::StoragePartition* storage_partition) {
+        storage_partition->GetNetworkContext()->SetEnterpriseReportingEndpoints(
+            endpoints);
+      });
+}
+#endif
 
 // static
 network::mojom::CookieManagerParamsPtr
