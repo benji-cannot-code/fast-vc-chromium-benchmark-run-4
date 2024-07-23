6be2f1a78c7bf5d6b5b13241111d82233e65160b
Michael Checo <michaelcheco@google.com>
2024-07-23 02:27:56 +0000

    [WE] Update device info css to match spec
    Spec: go/one-off-row-spec, go/settings-spec-device
    Screenshot: http://screen/5JsbwvXztGxB4pv
    
    - Device settings subpages now display either a device image provided
      by the OEM or a device icon consistent with the icons used on other
      settings pages.
    - Updates pointing stick subpage to use new header element
    
    Bug: b/329686601
    Change-Id: Ib70b2ba45314e320554426d10b5cc60d4e50bd56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5720149
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331503}

--- chrome/test/data/webui/chromeos/settings/device_page/per_device_pointing_stick_test.ts
+++ chrome/test/data/webui/chromeos/settings/device_page/per_device_pointing_stick_test.ts
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {fakePointingSticks, fakePointingSticks2, SettingsPerDevicePointingStickElement} from 'chrome://os-settings/os_settings.js';
+import {fakePointingSticks, fakePointingSticks2, PerDeviceSubsectionHeaderElement, SettingsPerDevicePointingStickElement} from 'chrome://os-settings/os_settings.js';
+import {strictQuery} from 'chrome://resources/ash/common/typescript_utils/strict_query.js';
 import {assert} from 'chrome://resources/js/assert.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
@@ -47,8 +48,11 @@ suite('<settings-per-device-pointing-stick>', function() {
             perDevicePointingStickPage.shadowRoot!.querySelectorAll(
                 'settings-per-device-pointing-stick-subsection');
         for (let i = 0; i < subsections.length; i++) {
+          const subsectionHeader = strictQuery(
+              'per-device-subsection-header', subsections[i]!.shadowRoot,
+              PerDeviceSubsectionHeaderElement);
           const name =
-              subsections[i]!.shadowRoot!.querySelector('h2')!.textContent;
+              subsectionHeader.shadowRoot!.querySelector('h2')!.textContent;
           if (fakePointingSticks[i]!.isExternal) {
             assertEquals(fakePointingSticks[i]!.name, name!.trim());
           } else {
