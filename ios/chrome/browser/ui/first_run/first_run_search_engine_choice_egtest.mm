82b2b2624551474b44c0df79225b956103d4c028
Joemer Ramos <joemerramos@chromium.org>
2024-07-23 23:53:49 +0000

    [iOS] Fix iOS 18 Failure in FirstRunSearchEngineChoiceTestCase
    An additional wait is needed to ensure that logic can find the Learn
    More button.
    
    Fixed: 354926312
    Change-Id: I495cf90e86d9bece1ed76ee8591e0bd59616324b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734298
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332031}

--- ios/chrome/browser/ui/first_run/first_run_search_engine_choice_egtest.mm
+++ ios/chrome/browser/ui/first_run/first_run_search_engine_choice_egtest.mm
@@ -226,10 +226,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    scrollViewIdentifier:kSearchEngineChoiceScrollViewIdentifier]
       performAction:grey_tap()];
   // Verify the Learn More view was presented.
-  [[EarlGrey selectElementWithMatcher:
-                 grey_accessibilityID(
-                     kSearchEngineChoiceLearnMoreAccessibilityIdentifier)]
-      assertWithMatcher:grey_notNil()];
+  [ChromeEarlGrey waitForUIElementToAppearWithMatcher:
+                      grey_accessibilityID(
+                          kSearchEngineChoiceLearnMoreAccessibilityIdentifier)];
   GREYAssertNil([MetricsAppInterface expectTotalCount:2
                                          forHistogram:eventHistogram],
                 @"Failed to record event histogram");
