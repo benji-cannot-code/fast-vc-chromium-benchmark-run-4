e886b17cc8c98516eed30b513430bb503e39d900
Benedikt Meurer <bmeurer@chromium.org>
2024-07-23 14:58:11 +0000

    [DevTools] Update test expectations and reenable tests.
    Bug: 41433391, 350443026
    Change-Id: I59336c17dfdd1196d26553a0d5f0f163179cb257
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5729943
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331743}

--- third_party/blink/web_tests/http/tests/inspector-protocol/cpu-profiler/console-profileEnd-parameterless-crash.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/cpu-profiler/console-profileEnd-parameterless-crash.js
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (function collectProfiles() {
       console.profile();
       console.profile('titled');
-      console.profileEnd();
+      console.profileEnd('titled');
       console.profileEnd();
     })();
   `);
