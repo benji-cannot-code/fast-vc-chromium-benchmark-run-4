529fb090f27b90e3afa891ff6dfca7ecc10d445c
Alex Gough <ajgo@chromium.org>
2024-07-23 03:17:47 +0000

    check result of find before dereferencing in preloading_decider
    Add checks between it = _.find() and it->_ operations in
    preloading decider.
    
    Bug: 351745839
    Change-Id: I962c4a65476493257036e8daff516f7fc2dc73d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732335
    Reviewed-by: Lingqi Chi <lingqi@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331521}

--- content/browser/preloading/preloading_decider.cc
+++ content/browser/preloading/preloading_decider.cc
@@ -488,6 +488,7 @@ bool PreloadingDecider::MaybePrefetch(
       std::move(matched_candidate_pair.value().second), enacting_predictor);
 
   auto it = on_standby_candidates_.find(key);
+  CHECK(it != on_standby_candidates_.end());
   std::vector<blink::mojom::SpeculationCandidatePtr> candidates_for_key =
       std::move(it->second);
   RemoveStandbyCandidate(key);
@@ -617,6 +618,7 @@ std::pair<bool, bool> PreloadingDecider::MaybePrerender(
       result.first && PredictionOccursInOtherWebContents(*candidate);
 
   auto it = on_standby_candidates_.find(key);
+  CHECK(it != on_standby_candidates_.end());
   std::vector<blink::mojom::SpeculationCandidatePtr> processed =
       std::move(it->second);
   RemoveStandbyCandidate(it->first);
