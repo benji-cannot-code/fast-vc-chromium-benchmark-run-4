dc32d4bd255905421bfb11b578fd71a4208d596e
Yu Zhong <yuzhong@google.com>
2024-07-23 01:00:41 +0000

    [Read Aloud] Tests clean up per webui team suggestion
    Reduce suite usage in Reading Mode tests for better readability and less redundant code, there are probably more places we can improve, I picked the most obvious cases before this CL gets too big.
    
    Fixed: 347342140
    Change-Id: I52ebf6b87bffb6bf99335d6706ddd941c9d79212
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726428
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Yu Zhong <yuzhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1331453}

--- chrome/test/data/webui/side_panel/read_anything/rate_selection_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/rate_selection_test.ts
@@ -41,15 +41,13 @@ suite('RateSelection', () => {
     });
   });
 
-  suite('by default', () => {
-    test('uses 1x', () => {
-      assertEquals('voice-rate:1', rateButton.ironIcon);
-      assertEquals(1, chrome.readingMode.speechRate);
-    });
+  test('by default', () => {
+    // Uses 1x
+    assertEquals('voice-rate:1', rateButton.ironIcon);
+    assertEquals(1, chrome.readingMode.speechRate);
 
-    test('menu is not open', () => {
-      assertFalse(toolbar.$.rateMenu.get().open);
-    });
+    // Menu is not open
+    assertFalse(toolbar.$.rateMenu.get().open);
   });
 
   test('menu button opens menu', () => {
@@ -68,11 +66,9 @@ suite('RateSelection', () => {
       options = getItemsInMenu(toolbar.$.rateMenu);
     });
 
-    test('has multiple options', () => {
+    test('displays options in increasing order with multiple options', () => {
       assertGT(options.length, 0);
-    });
 
-    test('displays options in increasing order', () => {
       let previousRate = -1;
       options.forEach((option) => {
         option.click();
@@ -94,7 +90,7 @@ suite('RateSelection', () => {
       assertEquals(rateValue, chrome.readingMode.speechRate);
       assertTrue(rateEmitted);
 
-      // updates icon on toolbar'
+      // updates icon on toolbar
       assertEquals('voice-rate:' + rateValue, rateButton.ironIcon);
 
       // closes menu
