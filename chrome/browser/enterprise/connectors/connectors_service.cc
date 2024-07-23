c2bf68010567499658fbc335706afb5affaabf68
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-07-23 13:15:15 +0000

    Create ConnectorsServiceBase
    This CL creates a new `ConnectorsServiceBase` class in components/ to
    start moving code to allow its reuse on mobile platforms. The following
    refactors are done to start supporting URLF on mobile:
    - `GetDMTokenForRealTimeUrlCheck()` and `GetAppliedRealTimeUrlCheck()`
      are move from `ConnectorsService` to `ConnectorsServiceBase`.
    - Some internal methods/structs of `ConnectorsService` are moved to
      support the above two URLF methods.
    - `GetPrefs()` methods are added to simplify accessing prefs from
      components.
    
    Bug: b/318842720
    Change-Id: I4d476de0c95cb4a60d0dad228f0465b51923cad6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730314
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331700}

--- chrome/browser/enterprise/connectors/connectors_service.cc
+++ chrome/browser/enterprise/connectors/connectors_service.cc
@@ -435,38 +435,6 @@ std::string ConnectorsService::GetManagementDomain() {
 #endif
 }
 
-std::optional<std::string> ConnectorsService::GetDMTokenForRealTimeUrlCheck()
-    const {
-  if (!ConnectorsEnabled())
-    return std::nullopt;
-
-  if (Profile::FromBrowserContext(context_)->GetPrefs()->GetInteger(
-          prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckMode) ==
-      safe_browsing::REAL_TIME_CHECK_DISABLED) {
-    return std::nullopt;
-  }
-
-  std::optional<DmToken> dm_token =
-      GetDmToken(prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckScope);
-
-  if (dm_token.has_value())
-    return dm_token.value().value;
-  return std::nullopt;
-}
-
-safe_browsing::EnterpriseRealTimeUrlCheckMode
-ConnectorsService::GetAppliedRealTimeUrlCheck() const {
-  if (!ConnectorsEnabled() ||
-      !GetDmToken(prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckScope)
-           .has_value()) {
-    return safe_browsing::REAL_TIME_CHECK_DISABLED;
-  }
-
-  return static_cast<safe_browsing::EnterpriseRealTimeUrlCheckMode>(
-      Profile::FromBrowserContext(context_)->GetPrefs()->GetInteger(
-          prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckMode));
-}
-
 std::string ConnectorsService::GetRealTimeUrlCheckIdentifier() const {
   auto dm_token =
       GetDmToken(prefs::kSafeBrowsingEnterpriseRealTimeUrlCheckScope);
@@ -495,14 +463,6 @@ void ConnectorsService::ObserveTelemetryReporting(
     base::RepeatingCallback<void()> callback) {
   connectors_manager_->SetTelemetryObserverCallback(callback);
 }
-
-ConnectorsService::DmToken::DmToken(const std::string& value,
-                                    policy::PolicyScope scope)
-    : value(value), scope(scope) {}
-ConnectorsService::DmToken::DmToken(DmToken&&) = default;
-ConnectorsService::DmToken& ConnectorsService::DmToken::operator=(DmToken&&) =
-    default;
-ConnectorsService::DmToken::~DmToken() = default;
 
 std::optional<ConnectorsService::DmToken> ConnectorsService::GetDmToken(
     const char* scope_pref) const {
@@ -563,9 +523,7 @@ policy::PolicyScope ConnectorsService::GetPolicyScope(
   // scope should always be POLICY_SCOPE_MACHINE.
   return policy::PolicyScope::POLICY_SCOPE_MACHINE;
 #else
-  return static_cast<policy::PolicyScope>(
-      Profile::FromBrowserContext(context_)->GetPrefs()->GetInteger(
-          scope_pref));
+  return static_cast<policy::PolicyScope>(GetPrefs()->GetInteger(scope_pref));
 #endif
 }
 
@@ -593,6 +551,14 @@ bool ConnectorsService::ConnectorsEnabled() const {
 #endif
 
   return !profile->IsOffTheRecord();
+}
+
+PrefService* ConnectorsService::GetPrefs() {
+  return Profile::FromBrowserContext(context_)->GetPrefs();
+}
+
+const PrefService* ConnectorsService::GetPrefs() const {
+  return Profile::FromBrowserContext(context_)->GetPrefs();
 }
 
 std::unique_ptr<ClientMetadata> ConnectorsService::BuildClientMetadata(
