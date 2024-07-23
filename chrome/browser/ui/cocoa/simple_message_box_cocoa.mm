d4b47dc4ce19a111100aacf3fe1c7733d4e2cb71
Keren Zhu <kerenzhu@chromium.org>
2024-07-23 20:28:22 +0000

    views: use native dialogs for message boxes if no parent window is given
    Message boxes are displayed as browser modal dialogs which require
    a parent window on non-CrOS, but sometimes a parent is not available,
    for example in ShowProfileErrorDialog(). When this happens, display
    native dialog (Message Box on Win and NSAlert on macOS) if possible.
    
    screenshot (old): https://screenshot.googleplex.com/9SCobCSuJbCuSsV
    screenshot (new): https://screenshot.googleplex.com/6JJ3EagdXPXwo4Q
    
    Note that the NSAlert default icon uses the application icon and will be
    the Chromium or Chrome icon in production.
    
    Add smoke test MessageBoxDialogUiTest that opens message boxes with or
    without a parent window. This test is for manual inspection only because
    the message box might not be possible to close programmatically.
    
    TEST=add tests to invoke the ui and manually verify that NSAlert is used
    
    Bug: 40228715, 40902461
    Low-Coverage-Reason: HARD_TO_TEST
    Change-Id: I83c994980d4e36fc8da05f73463571cbf25203b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732108
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1331975}

--- chrome/browser/ui/cocoa/simple_message_box_cocoa.mm
+++ chrome/browser/ui/cocoa/simple_message_box_cocoa.mm
@@ -19,7 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-MessageBoxResult ShowMessageBoxCocoa(const std::u16string& message,
+MessageBoxResult ShowMessageBoxCocoa(const std::u16string& title,
+                                     const std::u16string& message,
                                      MessageBoxType type,
                                      const std::u16string& checkbox_text) {
   startup_metric_utils::GetBrowser().SetNonBrowserUIDisplayed();
@@ -27,7 +28,12 @@ MessageBoxResult ShowMessageBoxCocoa(const std::u16string& message,
     return MESSAGE_BOX_RESULT_YES;
 
   NSAlert* alert = [[NSAlert alloc] init];
-  alert.messageText = base::SysUTF16ToNSString(message);
+  if (title.empty()) {
+    alert.messageText = base::SysUTF16ToNSString(message);
+  } else {
+    alert.messageText = base::SysUTF16ToNSString(title);
+    alert.informativeText = base::SysUTF16ToNSString(message);
+  }
   alert.alertStyle = NSAlertStyleWarning;
   if (type == MESSAGE_BOX_TYPE_QUESTION) {
     [alert addButtonWithTitle:l10n_util::GetNSString(
