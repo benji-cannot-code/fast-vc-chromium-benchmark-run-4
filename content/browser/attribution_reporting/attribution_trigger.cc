8e19d5360f71080b6bd81f0c9db5110e2f0f810c
John Delaney <johnidel@chromium.org>
2024-07-23 23:51:12 +0000

    [ara] Refactor MaybeCreateAndStoreReport() between resolver and storage
    Splits the logic for this function between the resolver and storage
    classes. Each individual sub-operation is still exposed as an API
    on storage, which can be looked at individually in the future.
    
    Notably this leaves MaybeStoreEventlevelReport() as a storage API.
    While this logic should move to the resolver eventually, this is left
    as-is for simplicity.
    
    Change-Id: If25808e4798caeb9f75313348d0bbfbadcb71dd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735033
    Commit-Queue: John Delaney <johnidel@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332025}

--- content/browser/attribution_reporting/attribution_trigger.cc
+++ content/browser/attribution_reporting/attribution_trigger.cc
@@ -7,6 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/containers/flat_map.h"
+#include "base/ranges/algorithm.h"
+#include "components/attribution_reporting/aggregatable_values.h"
 #include "components/attribution_reporting/suitable_origin.h"
 
 namespace content {
@@ -32,5 +35,14 @@ AttributionTrigger& AttributionTrigger::operator=(AttributionTrigger&&) =
     default;
 
 AttributionTrigger::~AttributionTrigger() = default;
+
+bool AttributionTrigger::HasAggregatableData() const {
+  return !registration_.aggregatable_trigger_data.empty() ||
+         base::ranges::any_of(
+             registration_.aggregatable_values,
+             [](const attribution_reporting::AggregatableValues& values) {
+               return !values.values().empty();
+             });
+}
 
 }  // namespace content
