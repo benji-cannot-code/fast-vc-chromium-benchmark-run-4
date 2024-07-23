8d2186d0d6b876600c66a563566cb725925903f9
Teresa Mao <temao@chromium.org>
2024-07-23 21:24:35 +0000

    profile-picker UI: Migrate to Lit, part 2.
    - Migrate profile_card.ts.
    - Create profile_picker_util.ts which contains methods for creating
    dummy values.
    - Migrate iron-icon to cr-icon.
    
    Bug: 40943652
    Change-Id: Ia004787c8451fbcfae98324efe33a720559f13d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730838
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1332005}

---
+++ chrome/browser/resources/signin/profile_picker/profile_picker_util.ts
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {ProfileState} from './manage_profiles_browser_proxy.js';
+
+export function createDummyProfileState(): ProfileState {
+  return {
+    profilePath: '',
+    localProfileName: '',
+    isSyncing: false,
+    needsSignin: false,
+    gaiaName: '',
+    userName: '',
+    avatarBadge: '',
+    avatarIcon: '',
+    // <if expr="chromeos_lacros">
+    isPrimaryLacrosProfile: false,
+    // </if>
+  };
+}
