c2670a774e0fd56468a9e0ab7a054bb24d89c841
Takuto Ikuta <tikuta@chromium.org>
2024-07-24 02:04:25 +0000

    content/public/renderer: fix missing deps
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/build/android-build-perf-ninja/1663/overview
    
    This is follow up CL of https://crrev.com/c/5718751.
    
    Bug: b/352472971
    Bug: 40253918
    Change-Id: I27e61cadd36bb300bbdf1278a5a929bbe9554910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730453
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332102}

---
+++ content/public/renderer/render_frame_media_playback_options.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/renderer/render_frame_media_playback_options.h"
+
+#include "build/android_buildflags.h"
+#include "build/build_config.h"
+
+namespace content {
+
+#if BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_DESKTOP_ANDROID)
+const bool kIsBackgroundMediaSuspendEnabled = true;
+#else
+const bool kIsBackgroundMediaSuspendEnabled = false;
+#endif
+
+}  // namespace content
