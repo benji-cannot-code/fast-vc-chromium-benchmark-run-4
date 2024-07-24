2fe52c593bef69a24a3eabb980b9c77ca7a94187
Jing Wang <jiwan@chromium.org>
2024-07-24 01:24:34 +0000

    picker: add api call to fetch real browsing history
    Bug: b:331114263
    Change-Id: I7244873baaad83f4c5aecc6a24ac626ad1935a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5716190
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332091}

--- chrome/browser/ui/ash/picker/picker_client_impl.h
+++ chrome/browser/ui/ash/picker/picker_client_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ash/app_list/search/ranking/ranker_manager.h"
 #include "chrome/browser/ash/login/session/user_session_manager.h"
+#include "chrome/browser/ui/ash/picker/picker_link_suggester.h"
 #include "ui/base/page_transition_types.h"
 #include "ui/base/window_open_disposition.h"
 #include "url/gurl.h"
@@ -88,6 +89,10 @@ class PickerClientImpl
     ranker_manager_ = std::move(ranker_manager);
   }
 
+  PickerLinkSuggester* get_link_suggester_for_test() {
+    return link_suggester_.get();
+  }
+
  private:
   // Implements `AppListControllerDelegate` with empty methods. Used only for
   // constructing search engine providers.
@@ -117,10 +122,6 @@ class PickerClientImpl
       CrosSearchResultsCallback callback,
       ash::AppListSearchResultType result_type,
       std::vector<std::unique_ptr<ChromeSearchResult>> results);
-  void OnZeroStateLinksSearchResultsUpdated(
-      SuggestedLinksCallback callback,
-      ash::AppListSearchResultType result_type,
-      std::vector<std::unique_ptr<ChromeSearchResult>> results);
   void SetProfileByUser(const user_manager::User* user);
   void SetProfile(Profile* profile);
 
@@ -146,11 +147,7 @@ class PickerClientImpl
   std::vector<app_list::CategoryMetadata> ranker_categories_;
 
   std::unique_ptr<PickerFileSuggester> file_suggester_;
-
-  // A dedicated cros search engine for zero state results for links.
-  // TODO: b/330938446 - Replace with proper zero-state logic.
-  std::unique_ptr<app_list::SearchEngine> zero_state_links_search_engine_;
-
+  std::unique_ptr<PickerLinkSuggester> link_suggester_;
   std::unique_ptr<PickerThumbnailLoader> thumbnail_loader_;
 
   base::ScopedObservation<user_manager::UserManager,
