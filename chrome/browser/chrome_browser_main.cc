ce122f5fb477e66d14c41b5dcd738baf67c0e6ca
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2024-07-24 10:05:21 +0000

    Gate CheckPakFileIntegrity execution behind a feature flag.
    In preparation for running Finch experiments to evaluate the performance
    impact and to disable it by default on ChromeOS (see next commit).
    
    BUG=b:342274735
    TEST=CQ
    
    Change-Id: I3ab7c558670980886f107477d71cfff1d9475174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5733794
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332227}

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1413,7 +1413,9 @@ void ChromeBrowserMainParts::PreBrowserStart() {
   // other services to start up before we start adjusting the oom priority.
   g_browser_process->GetTabManager()->Start();
 
-  CheckPakFileIntegrity();
+  if (base::FeatureList::IsEnabled(features::kReportPakFileIntegrity)) {
+    CheckPakFileIntegrity();
+  }
 #endif
 
   // The RulesetService will make the filtering rules available to renderers
