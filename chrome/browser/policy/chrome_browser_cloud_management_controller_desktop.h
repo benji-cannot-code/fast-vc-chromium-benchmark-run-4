a70e60c0b67fd5c339806dff1857339766630c6f
Jose Dapena Paz <jdapena@igalia.com>
2024-07-24 15:25:57 +0000

    IWYU: missing include for usage of std::variant in chrome_browser_cloud_management_controller_desktop.h
    Bug: 41455655
    Change-Id: I7ea5cf284ce010191537d1d16154b779f73a09c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735133
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1332340}

--- chrome/browser/policy/chrome_browser_cloud_management_controller_desktop.h
+++ chrome/browser/policy/chrome_browser_cloud_management_controller_desktop.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_POLICY_CHROME_BROWSER_CLOUD_MANAGEMENT_CONTROLLER_DESKTOP_H_
 #define CHROME_BROWSER_POLICY_CHROME_BROWSER_CLOUD_MANAGEMENT_CONTROLLER_DESKTOP_H_
 
+#include <variant>
+
 #include "base/task/single_thread_task_runner.h"
 #include "chrome/browser/policy/cbcm_invalidations_initializer.h"
 #include "components/enterprise/browser/controller/chrome_browser_cloud_management_controller.h"
