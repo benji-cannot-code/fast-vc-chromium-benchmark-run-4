c9aa66c76ca88460fcab1c70f175369b7f5cbf1a
Andrey Kosyakov <caseq@chromium.org>
2024-07-24 17:48:19 +0000

    Remove unused ScriptPromiseResolverBase::GetExceptionContext()
    Change-Id: If230dce82f324ab4325494bf870c44543c533cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5666603
    Auto-Submit: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332448}

--- third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h
+++ third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h
@@ -123,10 +123,6 @@ class CORE_EXPORT ScriptPromiseResolverBase
 
   ScriptState* GetScriptState() const { return script_state_.Get(); }
 
-  const ExceptionContext& GetExceptionContext() const {
-    return exception_context_;
-  }
-
   template <typename IDLResolvedType>
   ScriptPromiseResolver<IDLResolvedType>* DowncastTo() {
 #if DCHECK_IS_ON()
