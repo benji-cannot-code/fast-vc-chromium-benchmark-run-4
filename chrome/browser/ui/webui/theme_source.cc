5062d126fdcc3d4d736287eb156e0c0f664e2526
Sean Kau <skau@chromium.org>
2024-07-24 17:57:27 +0000

    Remove the kJelly feature flag in chrome/browser/ui/webui
    Feature is launched and can be removed.
    
    This CL was uploaded by git cl split.
    
    R=rbpotter@chromium.org
    
    Bug: None
    Change-Id: I42ec8558b0fd9bf003ab14d629bc29aa066143b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732375
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332455}

--- chrome/browser/ui/webui/theme_source.cc
+++ chrome/browser/ui/webui/theme_source.cc
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/grit/cros_styles_resources.h"  // nogncheck crbug.com/1113869
-#include "chromeos/constants/chromeos_features.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
@@ -407,12 +406,6 @@ std::string ThemeSource::GetAccessControlAllowOriginForOrigin(
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 void ThemeSource::SendTypographyCss(
     content::URLDataSource::GotDataCallback callback) {
-  if (!chromeos::features::IsJellyEnabled()) {
-    std::move(callback).Run(base::MakeRefCounted<base::RefCountedString>(
-        std::string("/* This file is intentionally blank */")));
-    return;
-  }
-
   const ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   std::move(callback).Run(rb.LoadDataResourceBytesForScale(
       IDR_CROS_STYLES_UI_CHROMEOS_STYLES_CROS_TYPOGRAPHY_CSS,
