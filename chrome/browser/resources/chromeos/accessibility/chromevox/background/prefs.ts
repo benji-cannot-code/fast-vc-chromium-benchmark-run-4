0c6c19e30a342c1c8d060ef846b38a8efe0041c5
Anastasia Helfinstein <anastasi@google.com>
2024-07-24 17:49:29 +0000

    [ChromeVox] Convert LogUrlWatcher to TypeScript
    AX-Relnotes: n/a.
    Bug: b/267329383
    Change-Id: I9651f37393786869d1a786eba5727f52839ff4e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5601443
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Jacob Francis <francisjp@google.com>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332450}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/prefs.ts
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/prefs.ts
@@ -129,7 +129,7 @@ export class ChromeVoxPrefs {
     } else if (key === 'enableEventStreamLogging') {
       EventStreamLogger.instance.updateAllFilters(value);
     }
-    this.enableOrDisableLogUrlWatcher_();
+    this.enableOrDisableLogUrlWatcher();
   }
 
   /**
@@ -168,7 +168,7 @@ export class ChromeVoxPrefs {
     ChromeVoxPrefs.darkScreen_ = newVal;
   }
 
-  private enableOrDisableLogUrlWatcher_(): void {
+  enableOrDisableLogUrlWatcher(): void {
     for (const pref of Object.values(LoggingPrefs)) {
       if (SettingsManager.getBoolean(pref)) {
         LogUrlWatcher.create();
