3661092205da2015a100205f39b5d0a1b5df20ed
Allen Bauer <kylixrd@chromium.org>
2024-07-24 13:29:23 +0000

    Use CLIENT_OWNS_WIDGET for the FindHost Widget.
    Change-Id: I229def4c2ac6e1cd59cb7c66ddc53aa1c995c1e9
    Bug: 338254375
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735159
    Reviewed-by: Scott Violet <sky@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332287}

--- chrome/browser/ui/views/find_bar_host.h
+++ chrome/browser/ui/views/find_bar_host.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_FIND_BAR_HOST_H_
 #define CHROME_BROWSER_UI_VIEWS_FIND_BAR_HOST_H_
 
+#include <memory>
+
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/find_bar/find_bar.h"
 #include "chrome/browser/ui/views/find_bar_view.h"
@@ -14,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/focus/external_focus_tracker.h"
 #include "ui/views/focus/focus_manager.h"
-#include "ui/views/widget/unique_widget_ptr.h"
 #include "ui/views/widget/widget_delegate.h"
 
 class BrowserView;
@@ -25,6 +26,9 @@ namespace find_in_page {
 class FindNotificationDetails;
 }
 
+namespace views {
+class Widget;
+}
 ////////////////////////////////////////////////////////////////////////////////
 //
 // The FindBarHost implements the container widget for the find-in-page
@@ -181,7 +185,7 @@ class FindBarHost : public FindBar,
 
   // Host is the Widget implementation that is created and maintained by the
   // find bar. It contains the find bar view.
-  views::UniqueWidgetPtr host_;
+  std::unique_ptr<views::Widget> host_;
 
   // A pointer back to the owning controller.
   raw_ptr<FindBarController> find_bar_controller_ = nullptr;
