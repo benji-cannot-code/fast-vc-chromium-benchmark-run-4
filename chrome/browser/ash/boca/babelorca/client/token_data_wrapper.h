d2215c374503e0848c3b66609a2ea2aad34d59c0
Ahmed Nasr <anasr@google.com>
2024-07-24 15:40:24 +0000

    [BabelOrca] Add OAuth token fetcher for tachyon.
    Also add an interface that can be used to fetch both oauth and
    tachyon tokens.
    
    Bug: b:353974384
    Change-Id: Id79f63490cb737e492d205eb199eb447ad975bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731540
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332349}

---
+++ chrome/browser/ash/boca/babelorca/client/token_data_wrapper.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ASH_BOCA_BABELORCA_CLIENT_TOKEN_DATA_WRAPPER_H_
+#define CHROME_BROWSER_ASH_BOCA_BABELORCA_CLIENT_TOKEN_DATA_WRAPPER_H_
+
+#include <string>
+#include <utility>
+
+#include "base/time/time.h"
+
+namespace babelorca {
+
+struct TokenDataWrapper {
+  std::string token;
+  base::Time expiration_time;
+
+  TokenDataWrapper() = default;
+  TokenDataWrapper(std::string token_param, base::Time expiration_time_param)
+      : token(std::move(token_param)), expiration_time(expiration_time_param) {}
+};
+
+}  // namespace babelorca
+
+#endif  // CHROME_BROWSER_ASH_BOCA_BABELORCA_CLIENT_TOKEN_DATA_WRAPPER_H_
