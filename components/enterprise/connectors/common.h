0be44f15f89f2b1c33ccc69ac8b2259035095890
Jose Dapena Paz <jdapena@igalia.com>
2024-07-24 16:10:59 +0000

    IWYU: missing include for usage of std::optional in enterprise connectors.
    Bug: 41455655
    Change-Id: I2c95a3a1e1f9db9d23d0b2a4562a83363d79e86f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735477
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Nancy Xiao <nancylanxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332370}

--- components/enterprise/connectors/common.h
+++ components/enterprise/connectors/common.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_ENTERPRISE_CONNECTORS_COMMON_H_
 
 #include <map>
+#include <optional>
 #include <set>
 #include <string>
 #include <utility>
