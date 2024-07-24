d4f0b3bc2a7cdcd3c32655b3a4685e4fae7d1efd
Kent Tamura <tkent@chromium.org>
2024-07-24 17:07:22 +0000

    css-anchor-position: Fix inset-area behavior in the sideways-lr wirting-mode
    This CL fixes WritingDirectionMode::IsFlippedY(), and improves test
    coverage.
    
    Bug: 40501131
    Change-Id: I1ef0181037eab3e8bd0ffbdbfac8b40b64b23116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736708
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332416}

--- third_party/blink/renderer/platform/text/writing_direction_mode.h
+++ third_party/blink/renderer/platform/text/writing_direction_mode.h
@@ -94,7 +94,7 @@ inline bool WritingDirectionMode::IsFlippedY() const {
     DCHECK(!IsFlippedBlocks());
     return false;
   }
-  return IsRtl();
+  return IsRtl() ^ (writing_mode_ == WritingMode::kSidewaysLr);
 }
 
 PLATFORM_EXPORT std::ostream& operator<<(std::ostream&,
