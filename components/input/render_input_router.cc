b49ef0a1569768ea99ef0627a121bc1e8397c382
Kartar Singh <kartarsingh@google.com>
2024-07-24 19:58:34 +0000

    Update trace events category in RenderInputRouter to 'input'
    Change-Id: Ice01afe204a0e078463ecab5dcbcc0e1a390bda0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735391
    Commit-Queue: Kartar Singh <kartarsingh@google.com>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332539}

--- components/input/render_input_router.cc
+++ components/input/render_input_router.cc
@@ -127,7 +127,7 @@ RenderInputRouter::RenderInputRouter(
       task_runner_(std::move(task_runner)) {}
 
 void RenderInputRouter::SetupInputRouter(float device_scale_factor) {
-  TRACE_EVENT("toplevel.flow", "RenderInputRouter::SetupInputRouter");
+  TRACE_EVENT("input", "RenderInputRouter::SetupInputRouter");
 
   input_router_ = std::make_unique<InputRouterImpl>(
       this, this, fling_scheduler_.get(),
@@ -146,7 +146,7 @@ void RenderInputRouter::BindRenderInputRouterInterfaces(
 }
 
 void RenderInputRouter::RendererWidgetCreated(bool for_frame_widget) {
-  TRACE_EVENT("toplevel.flow", "RenderInputRouter::RendererWidgetCreated");
+  TRACE_EVENT("input", "RenderInputRouter::RendererWidgetCreated");
 
   client_remote_->GetWidgetInputHandler(
       widget_input_handler_.BindNewPipeAndPassReceiver(task_runner_),
@@ -171,7 +171,7 @@ void RenderInputRouter::SetDeviceScaleFactor(float device_scale_factor) {
 }
 
 void RenderInputRouter::ProgressFlingIfNeeded(base::TimeTicks current_time) {
-  TRACE_EVENT("toplevel.flow", "RenderInputRouter::ProgressFlingIfNeeded");
+  TRACE_EVENT("input", "RenderInputRouter::ProgressFlingIfNeeded");
   fling_scheduler_->ProgressFlingOnBeginFrameIfneeded(current_time);
 }
 
@@ -180,7 +180,7 @@ void RenderInputRouter::StopFling() {
 }
 
 blink::mojom::WidgetInputHandler* RenderInputRouter::GetWidgetInputHandler() {
-  TRACE_EVENT("toplevel.flow", "RenderInputRouter::GetWidgetInputHandler");
+  TRACE_EVENT("input", "RenderInputRouter::GetWidgetInputHandler");
 
   if (widget_input_handler_) {
     return widget_input_handler_.get();
