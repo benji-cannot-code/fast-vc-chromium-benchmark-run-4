c963bce47caf3865c0512f9fd88680e09520aca2
James Cook <jamescook@chromium.org>
2024-07-24 04:18:24 +0000

    chromeos: Clean up AshIntegrationTest
    Remove some unused methods related to Lacros.
    
    This reveals that AshIntegrationTestTest is not running, but that will
    be fixed in a followup because I don't want to do cleanup and add a
    test to the waterfall in the same CL.
    
    Bug: none
    Change-Id: I5651a50cb526821ed359bd7c82c07abee6981256
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732772
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332140}

--- chrome/test/base/chromeos/crosier/ash_integration_test.h
+++ chrome/test/base/chromeos/crosier/ash_integration_test.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_TEST_BASE_CHROMEOS_CROSIER_ASH_INTEGRATION_TEST_H_
 #define CHROME_TEST_BASE_CHROMEOS_CROSIER_ASH_INTEGRATION_TEST_H_
 
-#include <memory>
-
 #include "base/files/scoped_temp_dir.h"
 #include "chrome/test/base/ash/interactive/interactive_ash_test.h"
 #include "chrome/test/base/chromeos/crosier/chromeos_integration_login_mixin.h"
@@ -19,10 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class CommandLine;
-}
-
-namespace net::test_server {
-class EmbeddedTestServer;
 }
 
 // Base class for tests of ash-chrome integration with the ChromeOS platform,
@@ -41,15 +35,6 @@ class AshIntegrationTest : public InteractiveAshTest {
   AshIntegrationTest& operator=(const AshIntegrationTest&) = delete;
   ~AshIntegrationTest() override;
 
-  // Sets up the command line and environment variables to support Lacros (by
-  // enabling the Wayland server in ash). Call this from SetUpCommandLine() if
-  // your test starts Lacros.
-  void SetUpCommandLineForLacros(base::CommandLine* command_line);
-
-  // Sets up the Lacros browser manager. Call this from
-  // SetUpInProcessBrowserTestFixture() if your test starts Lacros.
-  void SetUpLacrosBrowserManager();
-
   // Waits for Ash to be ready for Lacros, including starting the "Exo" Wayland
   // server. Call this method if your test starts Lacros, otherwise Exo may not
   // be ready and Lacros may not start.
@@ -57,7 +42,6 @@ class AshIntegrationTest : public InteractiveAshTest {
 
   // MixinBasedInProcessBrowserTest:
   void SetUpCommandLine(base::CommandLine* command_line) override;
-  void SetUpOnMainThread() override;
 
   ChromeOSIntegrationLoginMixin& login_mixin() { return login_mixin_; }
 
@@ -66,10 +50,6 @@ class AshIntegrationTest : public InteractiveAshTest {
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 
  private:
-  // Overrides the Gaia URL to point to a local test server that produces an
-  // error, which is expected behavior in test environments.
-  void OverrideGaiaUrlForLacros(base::CommandLine* command_line);
-
   // This test runs on linux-chromeos in interactive_ui_tests and on a DUT in
   // chromeos_integration_tests.
   ChromeOSIntegrationTestMixin chromeos_integration_test_mixin_{&mixin_host_};
@@ -81,8 +61,6 @@ class AshIntegrationTest : public InteractiveAshTest {
   // ARC is only supported on the branded build.
   ChromeOSIntegrationArcMixin arc_mixin_{&mixin_host_, login_mixin_};
 #endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
-
-  std::unique_ptr<net::test_server::EmbeddedTestServer> https_server_;
 };
 
 #endif  // CHROME_TEST_BASE_CHROMEOS_CROSIER_ASH_INTEGRATION_TEST_H_
