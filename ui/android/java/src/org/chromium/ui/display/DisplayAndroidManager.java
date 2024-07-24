79b625415ee49c4bb851119b53338cdfabf89343
Andrew Grieve <agrieve@chromium.org>
2024-07-24 19:44:50 +0000

    Android: Remove ApiHelperForR
    There is no longer a need for these wrapper classes.
    
    Bug: 40825179
    Change-Id: Ib75bbb7b4e51d8f13018db7809a489bb29b281e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734781
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332528}

--- ui/android/java/src/org/chromium/ui/display/DisplayAndroidManager.java
+++ ui/android/java/src/org/chromium/ui/display/DisplayAndroidManager.java
@@ -20,7 +20,6 @@ import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.compat.ApiHelperForR;
 
 /** DisplayAndroidManager is a class that informs its observers Display changes. */
 @JNINamespace("ui")
@@ -100,7 +99,7 @@ public class DisplayAndroidManager {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             Display display = null;
             try {
-                display = ApiHelperForR.getDisplay(context);
+                display = context.getDisplay();
             } catch (UnsupportedOperationException e) {
                 // Context is not associated with a display.
             }
