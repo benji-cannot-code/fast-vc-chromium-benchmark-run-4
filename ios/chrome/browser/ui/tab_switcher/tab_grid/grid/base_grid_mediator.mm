6256efdfb127c219e2ee2bf7fcbb48cb2e63414c
Asami Doi <asamidoi@chromium.org>
2024-07-24 07:10:38 +0000

    [ios] Do not show the confirmation when sync tab groups is disabled
    This CL partially reverts the changes in
    - https://chromium-review.googlesource.com/c/chromium/src/+/5683003
    - https://chromium-review.googlesource.com/c/chromium/src/+/5711908
    
    If the tab group sync is disabled, do not show the confirmation dialog
    and take an action directly without confirming it.
    
    Bug: 355128579
    Change-Id: I1a2459d1a170d0ca544d1fd0be4c445eec40afc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5729986
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332175}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_mediator.mm
@@ -1080,12 +1080,16 @@ Browser* GetBrowserForNonPinnedTabWithId(BrowserList* browser_list,
 }
 
 - (void)deleteTabGroup:(const TabGroup*)group sourceView:(UIView*)sourceView {
-  // TODO(crbug.com/329627336): Do not show the confirmation when
-  // IsTabGroupSyncEnabled() is disabled.
-  [self.tabGroupsHandler
-      showTabGroupConfirmationForAction:TabGroupActionType::kDeleteTabGroup
-                                  group:group
-                             sourceView:sourceView];
+  if (IsTabGroupSyncEnabled()) {
+    [self.tabGroupsHandler
+        showTabGroupConfirmationForAction:TabGroupActionType::kDeleteTabGroup
+                                    group:group
+                               sourceView:sourceView];
+    return;
+  }
+
+  DCHECK(!IsTabGroupSyncEnabled());
+  [self closeTabGroup:group andDeleteGroup:YES];
 }
 
 - (void)closeTabGroup:(const TabGroup*)group {
@@ -1093,12 +1097,16 @@ Browser* GetBrowserForNonPinnedTabWithId(BrowserList* browser_list,
 }
 
 - (void)ungroupTabGroup:(const TabGroup*)group sourceView:(UIView*)sourceView {
-  // TODO(crbug.com/329631586): Do not show the confirmation when
-  // IsTabGroupSyncEnabled() is disabled.
-  [self.tabGroupsHandler
-      showTabGroupConfirmationForAction:TabGroupActionType::kUngroupTabGroup
-                                  group:group
-                             sourceView:sourceView];
+  if (IsTabGroupSyncEnabled()) {
+    [self.tabGroupsHandler
+        showTabGroupConfirmationForAction:TabGroupActionType::kUngroupTabGroup
+                                    group:group
+                               sourceView:sourceView];
+    return;
+  }
+
+  DCHECK(!IsTabGroupSyncEnabled());
+  [self ungroupTabGroup:group];
 }
 
 - (void)closeAllItems {
