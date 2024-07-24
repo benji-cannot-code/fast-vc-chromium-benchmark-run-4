6691993f169c436ba3be891c1c598796aad1aba7
Yun-hao Chung <howardchung@google.com>
2024-07-24 04:25:02 +0000

    Floss: Wait for the Manager available before calling object manager
    With some extreme cases, if chrome starts earlier than Floss manager,
    chrome won't be able to register manager clients and the Bluetooth icon
    will be gone in the system tray.
    
    bluetooth is shown in the system tray.
    
    Bug: 314699012
    Test: stop ui; stop btmanagerd; start ui; start btmanagerd. Verify
    Change-Id: I57da1e5cced8dcc33a40c18448d379d1ab09c34d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724769
    Commit-Queue: Yun-hao Chung <howardchung@chromium.org>
    Reviewed-by: Hsin-chen Chuang <chharry@google.com>
    Reviewed-by: Katherine Lai <laikatherine@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332142}

--- device/bluetooth/floss/floss_dbus_manager.cc
+++ device/bluetooth/floss/floss_dbus_manager.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_pump_type.h"
 #include "base/threading/thread.h"
 #include "build/build_config.h"
+#include "components/device_event_log/device_event_log.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_manager.h"
@@ -74,19 +75,13 @@ FlossDBusManager::FlossDBusManager(dbus::Bus* bus, bool use_stubs) : bus_(bus) {
 
   CHECK(GetSystemBus()) << "Can't initialize real clients without DBus.";
 
-  dbus::MethodCall method_call(dbus::kObjectManagerInterface,
-                               dbus::kObjectManagerGetManagedObjects);
-
   VLOG(1) << "FlossDBusManager checking for object manager";
-  // Sets up callbacks checking for object manager support. Object manager is
-  // registered on the root object "/"
+
+  // Wait for the Floss Manager to be available
   GetSystemBus()
       ->GetObjectProxy(kManagerService, dbus::ObjectPath("/"))
-      ->CallMethodWithErrorCallback(
-          &method_call, kDBusTimeoutMs,
-          base::BindOnce(&FlossDBusManager::OnObjectManagerSupported,
-                         weak_ptr_factory_.GetWeakPtr()),
-          base::BindOnce(&FlossDBusManager::OnObjectManagerNotSupported,
+      ->WaitForServiceToBeAvailable(
+          base::BindOnce(&FlossDBusManager::OnManagerServiceAvailable,
                          weak_ptr_factory_.GetWeakPtr()));
 }
 
@@ -370,9 +365,34 @@ void FlossDBusManager::OnObjectManagerSupported(dbus::Response* response) {
   object_manager_->RegisterInterface(kSocketManagerInterface, this);
 }
 
+void FlossDBusManager::OnManagerServiceAvailable(bool is_available) {
+  if (!is_available) {
+    BLUETOOTH_LOG(ERROR) << "Floss Manager is not available.";
+    object_manager_support_known_ = true;
+    if (object_manager_support_known_callback_) {
+      std::move(object_manager_support_known_callback_).Run();
+    }
+    return;
+  }
+
+  dbus::MethodCall method_call(dbus::kObjectManagerInterface,
+                               dbus::kObjectManagerGetManagedObjects);
+
+  // Sets up callbacks checking for object manager support. Object manager is
+  // registered on the root object "/"
+  GetSystemBus()
+      ->GetObjectProxy(kManagerService, dbus::ObjectPath("/"))
+      ->CallMethodWithErrorCallback(
+          &method_call, kDBusTimeoutMs,
+          base::BindOnce(&FlossDBusManager::OnObjectManagerSupported,
+                         weak_ptr_factory_.GetWeakPtr()),
+          base::BindOnce(&FlossDBusManager::OnObjectManagerNotSupported,
+                         weak_ptr_factory_.GetWeakPtr()));
+}
+
 void FlossDBusManager::OnObjectManagerNotSupported(
     dbus::ErrorResponse* response) {
-  DVLOG(1) << "Floss Bluetooth not supported.";
+  BLUETOOTH_LOG(ERROR) << "Floss Bluetooth not supported.";
   object_manager_supported_ = false;
 
   // Don't initialize any clients since they need ObjectManager.
--- device/bluetooth/floss/floss_dbus_manager.h
+++ device/bluetooth/floss/floss_dbus_manager.h
@@ -242,6 +242,7 @@ class DEVICE_BLUETOOTH_EXPORT FlossDBusManager
   void OnObjectManagerSupported(dbus::Response* response);
   void OnObjectManagerNotSupported(dbus::ErrorResponse* response);
   void OnManagerClientInitComplete();
+  void OnManagerServiceAvailable(bool is_available);
 
   // Initializes the manager client
   void InitializeManagerClient();
