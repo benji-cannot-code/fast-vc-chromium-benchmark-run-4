64182c93bb999f44ac79c39047bcaa0779d39b97
Jingping Sun <jings@microsoft.com>
2024-07-24 16:03:35 +0000

    Remove TabModelSelector obsever when ChromeTabbedActivity destroyed
    ChromeTabbedActivity adds the observer to TabModelSelector when it creates the tab model. But it never calls removeObserver on TabModelSelector when it is destroyed.
    This CL will remove the obsever from TabModelSelector when ChromeTabbedActivity destroyed.
    
    Bug: 40949112
    Change-Id: I107b6dc4fabaf4774b0689b297c0bd2201129dd9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737030
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332363}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -3179,6 +3179,11 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
 
         if (mTabModelObserver != null) mTabModelObserver.destroy();
 
+        if (mTabModelSelectorObserver != null && mTabModelSelector != null) {
+            mTabModelSelector.removeObserver(mTabModelSelectorObserver);
+            mTabModelSelectorObserver = null;
+        }
+
         if (mUndoBarPopupController != null) {
             mUndoBarPopupController.destroy();
             mUndoBarPopupController = null;
