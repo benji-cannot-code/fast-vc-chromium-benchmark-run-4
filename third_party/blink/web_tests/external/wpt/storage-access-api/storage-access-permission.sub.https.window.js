137abc25114693ed4ffeff551f592de5fc0fab4b
W. James MacLean <wjmaclean@chromium.org>
2024-07-24 14:02:59 +0000

    Add extra synchronization to test.
    This CL adds an additional synchronization point in the test
    storage-access-permission.sub.https.window.js. This test installs a
    permission-change observer, then initiates a permission change. But the
    observer installation is itself asynchronous, and it seems possible the
    change the test is trying to observe could occur without the observer
    finishing its installation, causing the test to flake. This CL removes
    this possibility by forcing the test to wait for the observer
    installation to complete before initiating the change the observeer is
    intended to observe.
    
    This test has been observed flaking at
    https://github.com/web-platform-tests/wpt/runs/27636425026, so it is
    hoped this CL will eliminate that flake.
    
    Bug: 40259221
    Change-Id: I23e3aa10a3e1e635cc60b17e6ce8445012b6964c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734161
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332301}

--- third_party/blink/web_tests/external/wpt/storage-access-api/storage-access-permission.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/storage-access-api/storage-access-permission.sub.https.window.js
@@ -35,10 +35,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'prompt']);
       });
 
-      const observed = ObservePermissionChange(frame2);
-      await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'granted']);
+      // Install observer on frame, and wait for acknowledgement that it is
+      // installed.
+      assert_equals(await ObservePermissionChange(frame2),
+                    "permission_change_observer_installed");
 
-      const state = await observed;
+      const observed_event = new Promise(r => {
+        onmessage = e => r(e.data);
+      });
+      await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'granted']);
+      const state = await observed_event;
       assert_equals(state, "granted");
     }, 'Permissions grants are observable across same-origin iframes');
 
@@ -65,10 +71,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'prompt']);
       });
 
-      const observed = ObservePermissionChange(frame2);
-      await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'granted']);
+      // Install observer on frame, and wait for acknowledgement that it is
+      // installed.
+      assert_equals(await ObservePermissionChange(frame2),
+                    "permission_change_observer_installed");
 
-      const state = await observed;
+      const observed_event = new Promise(r => {
+        onmessage = e => r(e.data);
+      });
+      await SetPermissionInFrame(frame1, [{ name: 'storage-access' }, 'granted']);
+      const state = await observed_event;
       assert_equals(state, "granted");
     }, "Permissions grants are observable across same-site iframes");
 
