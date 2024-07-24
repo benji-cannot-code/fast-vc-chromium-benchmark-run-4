f2b94a1a0ac9f77e955576d70192392692bf1422
Hubert Chao <hchao@chromium.org>
2024-07-24 17:37:35 +0000

    Cert Management UI V2: Cert list styling change
    If the cert list is inside a subpage, don't surround the cert list with
    a border since it'll already be inside one.
    
    Example screenshot: https://screenshot.googleplex.com/6xb6YAPZnnzcb7e
    
    Bug: 40928765
    Change-Id: Ie8e46c2eca1c77ce0a2040e5bb2908002c60fb63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726294
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332429}

--- ui/webui/resources/cr_components/certificate_manager/certificate_list_v2.ts
+++ ui/webui/resources/cr_components/certificate_manager/certificate_list_v2.ts
@@ -56,6 +56,7 @@ export class CertificateListV2Element extends CertificateListV2ElementBase {
       // True if the export button should be hidden.
       // Export button may also be hidden if there are no certs in the list.
       hideExport: Boolean,
+      inSubpage: Boolean,
       expanded_: Boolean,
       certificates_: Array,
       hasCerts_: {
@@ -69,6 +70,7 @@ export class CertificateListV2Element extends CertificateListV2ElementBase {
   headerText: string;
   showImport: boolean = false;
   hideExport: boolean = false;
+  inSubpage: boolean = false;
   private expanded_: boolean = true;
   private certificates_: SummaryCertInfo[] = [];
   private hasCerts_: boolean;
@@ -80,6 +82,10 @@ export class CertificateListV2Element extends CertificateListV2ElementBase {
         .then((results: {certs: SummaryCertInfo[]}) => {
           this.certificates_ = results.certs;
         });
+
+    if (!this.inSubpage) {
+      this.$.certs.classList.add('card');
+    }
   }
 
   private onExportCertsClick_(e: Event) {
