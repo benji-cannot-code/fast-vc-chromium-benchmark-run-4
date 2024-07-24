67cc3d5fd47a099d36f9bccd2bf88385277c023a
Robbie Gibson <rkgibson@google.com>
2024-07-24 20:51:34 +0000

    [iOS] Add metric for Contextual Panel model response time
    This metric tracks how long it takes each info block's model to respond.
    
    Bug: 349839698
    Change-Id: I82c19f4d2632a42b7ca2891b027f8268c684fa47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736356
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332586}

--- ios/chrome/browser/contextual_panel/model/contextual_panel_tab_helper.mm
+++ ios/chrome/browser/contextual_panel/model/contextual_panel_tab_helper.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_tab_helper.h"
 
 #import "base/metrics/histogram_functions.h"
+#import "base/strings/stringprintf.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_item_configuration.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_item_type.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_model.h"
@@ -196,6 +197,8 @@ void ContextualPanelTabHelper::QueryModels() {
 
   responses_.clear();
 
+  request_start_time_ = base::Time::Now();
+
   // First, create all the response objects, to track completed responses
   // correctly if a response returns synchronously.
   for (const auto& [key, model] : models_) {
@@ -222,6 +225,12 @@ void ContextualPanelTabHelper::ModelCallbackReceived(
     DCHECK_EQ(item_type, configuration->item_type);
   }
   responses_[item_type] = ModelResponse(std::move(configuration));
+
+  std::string histogram_name =
+      base::StringPrintf("IOS.ContextualPanel.%s.ModelResponseTime",
+                         StringForItemType(item_type).c_str());
+  base::UmaHistogramTimes(histogram_name,
+                          base::Time::Now() - request_start_time_);
 
   // Check if all models have returned.
   for (const auto& [key, response] : responses_) {
