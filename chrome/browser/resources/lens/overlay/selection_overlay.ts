fc31f1f460189baef3715cf2c8614fa14e097abf
Bryan Nguyen <nguyenbryan@google.com>
2024-07-24 22:07:45 +0000

    [lensoverlay] Add translate option to detected text context menu.
    Demo: https://screencast.googleplex.com/cast/NjYzMjc5MDQxNzIxMTM5MnwwMjg2MTQ5Zi1mYQ
    
    Bug: b:354941145
    Change-Id: I558a595c34558edb7d6531300569cf6db63141dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737499
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332620}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -279,9 +279,20 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
         });
     this.eventTracker_.add(
         document, 'restore-selected-text-context-menu', () => {
-          // show-selected-text-context-menu must be triggered first so that
+          // show-selected-text-context-menu or
+          // update-selected-text-context-menu must be triggered first so that
           // instance variables are set.
           this.showSelectedTextContextMenu = true;
+        });
+    this.eventTracker_.add(
+        document, 'update-selected-text-context-menu',
+        (e: CustomEvent<SelectedTextContextMenuData>) => {
+          this.selectedTextContextMenuX = e.detail.left;
+          this.selectedTextContextMenuY = e.detail.bottom;
+          this.highlightedText = e.detail.text;
+          this.contentLanguage = e.detail.contentLanguage;
+          this.textSelectionStartIndex = e.detail.selectionStartIndex;
+          this.textSelectionEndIndex = e.detail.selectionEndIndex;
         });
     this.eventTracker_.add(
         document, 'show-detected-text-context-menu', (e: CustomEvent) => {
@@ -729,6 +740,13 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
     unfocusShimmer(this, ShimmerControlRequester.CURSOR);
   }
 
+  private handleTranslateDetectedText() {
+    this.$.textSelectionLayer.selectAndTranslateWords(
+        this.detectedTextStartIndex, this.detectedTextEndIndex);
+    this.$.postSelectionRenderer.clearSelection();
+    unfocusShimmer(this, ShimmerControlRequester.CURSOR);
+  }
+
   private handleTranslate() {
     BrowserProxyImpl.getInstance().handler.issueTranslateSelectionRequest(
         this.highlightedText, this.contentLanguage,
@@ -795,6 +813,10 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
 
   handleSelectTextForTesting() {
     this.handleSelectText();
+  }
+
+  handleTranslateDetectedTextForTesting() {
+    this.handleTranslateDetectedText();
   }
 
   handleTranslateForTesting() {
