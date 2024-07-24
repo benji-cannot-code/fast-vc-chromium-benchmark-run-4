5474340fb1aea3018e90b35db0cbde506956d7c7
Joshua Pawlicki <waffles@chromium.org>
2024-07-24 20:37:19 +0000

    Updater: Create 3pp CIPD packages for Windows arm64 updater.
    Bug: 40063600
    Change-Id: I9b87e2ff63e0a567cc875cc2af321f7728ee5589
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738957
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332576}

---
+++ third_party/updater/chromium_win_arm64/3pp/3pp.pb
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+create {
+  source {
+    script { name: "fetch.py" }
+    unpack_archive: true
+    no_archive_prune: true
+  }
+  build {
+  }
+}
+
+upload {
+  universal: true
+  pkg_prefix: "chromium/third_party/updater"
+}
---
+++ third_party/updater/chromium_win_arm64/3pp/install.sh
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash
+# Copyright 2022 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+set -e
+set -x
+set -o pipefail
+
+# An auto-created directory whose content will ultimately be uploaded to CIPD.
+# The commands below should output the built product to this directory.
+PREFIX="$1"
+
+mv UpdaterSetup_test.exe "$PREFIX"
