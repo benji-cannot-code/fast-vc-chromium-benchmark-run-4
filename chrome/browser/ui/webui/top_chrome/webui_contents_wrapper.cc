917877da301970cb75d71ae9632088b97613bcb0
Keren Zhu <kerenzhu@chromium.org>
2024-07-24 22:13:20 +0000

    preload-topchrome: resize host before showing preloaded WebUIs
    This patch resizes the top-chrome WebUI host when the host is set for
    WebUIContentsWrapper. This fixes the visual glitches caused by immediate
    size change after the host is shown.
    
    For preloaded WebUIs, the resize event ResizeDueToAutoResize originates
    asynchronously from viz surface when the WebUI is attached to a
    views::Widget. Effectively this means the resize would happen after the
    ShowUI() call.
    
    This patch sets the host size to the render frame size. When the auto-
    resize is enabled, the render frame will be in sync with the layout
    size.
    
    Any size update between SetHost() and ShowUI() should be captured by
    the existing WebUIContents::ResizeDueToAutoResize() and passed to the
    host.
    
    Bug: 40168622
    Change-Id: Iaa9b72e240e8ee18a8110b149193ff9c855e6146
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735037
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332629}

--- chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
+++ chrome/browser/ui/webui/top_chrome/webui_contents_wrapper.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/site_engagement/content/site_engagement_helper.h"
 #include "components/site_engagement/content/site_engagement_service.h"
 #include "content/public/browser/keyboard_event_processing_result.h"
+#include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "third_party/blink/public/common/associated_interfaces/associated_interface_provider.h"
 #include "ui/base/models/menu_model.h"
@@ -136,7 +137,6 @@ WebUIContentsWrapper::~WebUIContentsWrapper() {
 void WebUIContentsWrapper::ResizeDueToAutoResize(content::WebContents* source,
                                                   const gfx::Size& new_size) {
   DCHECK_EQ(web_contents(), source);
-  contents_requested_size_ = new_size;
   if (host_)
     host_->ResizeDueToAutoResize(source, new_size);
 }
@@ -241,8 +241,9 @@ void WebUIContentsWrapper::PrimaryMainFrameRenderProcessGone(
 }
 
 void WebUIContentsWrapper::ShowUI() {
-  if (host_)
+  if (host_) {
     host_->ShowUI();
+  }
 
   // The host should never proactively show the contents after the initial
   // show, in which case the contents could have already been preloaded.
@@ -278,8 +279,11 @@ void WebUIContentsWrapper::SetHost(
     return;
   }
 
-  if (webui_resizes_host_ && !contents_requested_size_.IsEmpty()) {
-    host_->ResizeDueToAutoResize(web_contents_.get(), contents_requested_size_);
+  // Resize the host to the frame size. If there are new updates to the frame
+  // size they will be capture by WebUIContentsWrapper::ResizeDueToAutoResize().
+  content::RenderFrameHost* rfh = web_contents_->GetPrimaryMainFrame();
+  if (webui_resizes_host_ && rfh && rfh->GetFrameSize().has_value()) {
+    host_->ResizeDueToAutoResize(web_contents_.get(), *rfh->GetFrameSize());
   }
 
   if (supports_draggable_regions_ && draggable_regions_.has_value()) {
--- content/test/test_web_contents.cc
+++ content/test/test_web_contents.cc
@@ -207,6 +207,10 @@ void TestWebContents::SetMainFrameMimeType(const std::string& mime_type) {
   GetPrimaryPage().SetContentsMimeType(mime_type);
 }
 
+void TestWebContents::SetMainFrameSize(const gfx::Size& frame_size) {
+  GetPrimaryMainFrame()->FrameSizeChanged(frame_size);
+}
+
 const std::string& TestWebContents::GetContentsMimeType() {
   return GetPrimaryPage().GetContentsMimeType();
 }
