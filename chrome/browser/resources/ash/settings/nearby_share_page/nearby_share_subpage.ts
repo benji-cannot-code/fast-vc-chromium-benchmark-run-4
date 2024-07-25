9f9cea278a3623a1c71d289d67d2a7d8059ebe15
Brando Socarras <brandosocarras@google.com>
2024-07-25 02:51:17 +0000

    [QSv2] QS Settings Page Mimics Mock
    When the QSv2 flag is enabled:
    - Visibility is changed via menu, not dialog.
    - Data Usage settings row is removed.
    
    Screenshot
    ----------
    https://screenshot.googleplex.com/5Vfh4Ww8J23rGAi.png
    
    Mock
    ----
    go/cros-quicksharev2-ux
    
            Visibility section does nothing.
    
    Tested: on DUT, no change in computer function. Toggling buttons in
    Bug: b/351865350
    Change-Id: If133c73ca033555dd343f81fe582bbf957706c5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731779
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Jack Shira <jackshira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332752}

--- chrome/browser/resources/ash/settings/nearby_share_page/nearby_share_subpage.ts
+++ chrome/browser/resources/ash/settings/nearby_share_page/nearby_share_subpage.ts
@@ -137,6 +137,21 @@ export class SettingsNearbyShareSubpageElement extends
         type: Boolean,
         value: () => loadTimeData.getBoolean('isQuickShareV2Enabled'),
       },
+
+      isDeviceVisible_: {
+        type: Boolean,
+        value: true,  // TODO
+      },
+
+      selectedVisibility_: {
+        type: String,
+        value: 'Your devices',  // TODO
+      },
+
+      isEveryoneModeOnlyForTenMinutes_: {
+        type: Boolean,
+        value: true,  // TODO
+      },
     };
   }
 
@@ -145,12 +160,15 @@ export class SettingsNearbyShareSubpageElement extends
   }
 
   isSettingsRetreived: boolean;
+  private isDeviceVisible_: boolean;
+  private isEveryoneModeOnlyForTenMinutes_: boolean;
   private inHighVisibility_: boolean;
   private isQuickShareV2Enabled_: boolean;
   private manageContactsUrl_: string;
   private profileLabel_: string;
   private profileName_: string;
   private receiveObserver_: ReceiveObserverReceiver|null;
+  private selectedVisibility_: string;
   private settings: NearbySettings;
   private shouldShowFastInititationNotificationToggle_: boolean;
   private showDataUsageDialog_: boolean;
@@ -462,6 +480,15 @@ export class SettingsNearbyShareSubpageElement extends
   private computeShouldShowFastInititationNotificationToggle_(
       isHardwareSupported: boolean): boolean {
     return isHardwareSupported;
+  }
+
+  private onSelectedVisibilityChange_(): void {
+    // TODO
+  }
+
+  private isEveryoneModeSelected_(): boolean {
+    // TODO
+    return this.selectedVisibility_ === 'Everyone';
   }
 }
 
