610f92b73fb83a724616b39582d729e4a3e26e35
Fergal Daly <fergal@chromium.org>
2024-07-25 00:02:28 +0000

    Pre-factor for splitting out a language_detection components.
    - fixes one typo
    - creates a test class to factor out some boilerplate from the tests
    - removes some unneeded deps
    - clean lint by concatenating namespace
    
    Bug: 354069716
    Change-Id: I1ea013b183a99ab95c1dd1a9a029559ea71eeb4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735492
    Auto-Submit: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332687}

--- components/translate/core/language_detection/language_detection_model.h
+++ components/translate/core/language_detection/language_detection_model.h
@@ -10,15 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file.h"
 
-namespace tflite {
-namespace task {
-namespace text {
-namespace nlclassifier {
+namespace tflite::task::text::nlclassifier {
 class NLClassifier;
-}
-}  // namespace text
-}  // namespace task
-}  // namespace tflite
+}  // namespace tflite::task::text::nlclassifier
 
 namespace translate {
 
