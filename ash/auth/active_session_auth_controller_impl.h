0ee56b555571a1d4bd793f34c478f183e31057e9
Elie Maamari <emaamari@google.com>
2024-07-25 13:19:51 +0000

    [High 5] Implement Active Session Auth Controller - Fixes
    Fixes follow-up for https://chromium-review.googlesource.com/c/chromium/src/+/5712829/5?usp=related-change
    
    Bug: b:352238958, b:348326316
    Change-Id: I094b260db02a69092a9b65b4a24a0257473f243f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724794
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332905}

--- ash/auth/active_session_auth_controller_impl.h
+++ ash/auth/active_session_auth_controller_impl.h
@@ -27,11 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 // ActiveSessionAuthControllerImpl is responsible for :
-// - Initialize the ActiveSessionAuthView and control this view
-// - Create and manage a widget to show the ActiveSessionAuthView
-// - Listening the ActiveSessionAuthView observers and call auth performer if
-// authentication is requested on the UI
-// - Call the callback with the authentication result
+// - Initialize the ActiveSessionAuthView and control this view.
+// - Create and manage a widget to show the ActiveSessionAuthView.
+// - Listening to the ActiveSessionAuthView observers and call auth performer if
+// authentication is requested on the UI.
+// - Call the callback with the authentication result.
 class ASH_EXPORT ActiveSessionAuthControllerImpl
     : public ActiveSessionAuthController,
       public ActiveSessionAuthView::Observer,
@@ -71,23 +71,16 @@ class ASH_EXPORT ActiveSessionAuthControllerImpl
 
  private:
   // Internal methods for authentication.
-  void OnAuthFactorsListed(
+  void OnAuthSessionStarted(
+      bool user_exists,
       std::unique_ptr<UserContext> user_context,
       std::optional<AuthenticationError> authentication_error);
-  void OnPasswordSessionStarted(
-      const std::u16string& password,
-      bool exists,
+  void OnAuthFactorsListed(
       std::unique_ptr<UserContext> user_context,
       std::optional<AuthenticationError> authentication_error);
   void OnAuthComplete(AuthInputType input_type,
                       std::unique_ptr<UserContext> user_context,
                       std::optional<AuthenticationError> authentication_error);
-  void OnPinSessionStarted(
-      const std::u16string& pin,
-      bool exists,
-      std::unique_ptr<UserContext> user_context,
-      std::optional<AuthenticationError> authentication_error);
-
   void NotifySuccess(const AuthProofToken& token, base::TimeDelta timeout);
 
   std::unique_ptr<views::Widget> widget_;
@@ -105,6 +98,8 @@ class ASH_EXPORT ActiveSessionAuthControllerImpl
 
   std::unique_ptr<AuthFactorEditor> auth_factor_editor_;
   std::unique_ptr<AuthPerformer> auth_performer_;
+
+  std::unique_ptr<UserContext> user_context_;
 
   base::WeakPtrFactory<ActiveSessionAuthControllerImpl> weak_ptr_factory_{this};
 };
--- ash/public/cpp/auth/active_session_auth_controller.h
+++ ash/public/cpp/auth/active_session_auth_controller.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_PUBLIC_CPP_AUTH_ACTIVE_SESSION_AUTH_CONTROLLER_H_
 #define ASH_PUBLIC_CPP_AUTH_ACTIVE_SESSION_AUTH_CONTROLLER_H_
 
-#include <memory>
-
 #include "ash/public/cpp/ash_public_export.h"
 #include "base/functional/callback_forward.h"
 #include "base/time/time.h"
