a55db134bad93266801d2b81d908dac01ce61c8b
Steven Holte <holte@google.com>
2024-07-25 03:58:08 +0000

    Simplify substitution Condition tests.
    Eliminate some extra complexity, and make some helpers.
    
    Change-Id: I87f930fc601139c83e2aa633af1270292e70f553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739393
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332767}

--- components/optimization_guide/core/model_execution/test/feature_config_builder.h
+++ components/optimization_guide/core/model_execution/test/feature_config_builder.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TEST_FEATURE_CONFIG_BUILDER_H_
 #define COMPONENTS_OPTIMIZATION_GUIDE_CORE_MODEL_EXECUTION_TEST_FEATURE_CONFIG_BUILDER_H_
 
+#include <initializer_list>
+#include <optional>
+#include <string>
+
 #include "components/optimization_guide/proto/descriptors.pb.h"
 #include "components/optimization_guide/proto/on_device_model_execution_config.pb.h"
 #include "components/optimization_guide/proto/redaction.pb.h"
@@ -20,6 +24,9 @@ proto::SafetyCategoryThreshold ForbidUnsafe();
 // Sets a threshold that will reject text without "reasonable" when used with
 // FakeOnDeviceModel::ClassifyTextSafety.
 proto::SafetyCategoryThreshold RequireReasonable();
+
+// Construct a ProtoField with the given tags.
+proto::ProtoField ProtoField(std::initializer_list<int32_t> tags);
 
 // Reference ComposeRequest::page_metadata.page_url
 proto::ProtoField PageUrlField();
@@ -76,6 +83,18 @@ inline proto::OnDeviceModelValidationConfig WillFailValidationConfig() {
   prompt->set_prompt("hello");
   prompt->set_expected_output("goodbye");
   return validation_config;
+}
+
+inline auto Int32Proto(int32_t value) {
+  proto::Value v;
+  v.set_int32_value(value);
+  return v;
+}
+
+inline auto Int64Proto(int64_t value) {
+  proto::Value v;
+  v.set_int64_value(value);
+  return v;
 }
 
 }  // namespace optimization_guide
