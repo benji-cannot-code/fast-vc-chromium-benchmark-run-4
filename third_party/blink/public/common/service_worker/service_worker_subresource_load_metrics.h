8da5a7d109913c2076637f633b16169d9479d5a6
Keita Suzuki <suzukikeita@chromium.org>
2024-07-25 01:35:04 +0000

    Reland: Add metrics of SW static routing API evaluation time for subresources
    This reland CL disables the flaky tests for Linux and Mac. The failure
    reasons are simliar to the original version of test
    `ChromeServiceWorkerTest.SubresourceCount`, so we will disable it for
    the time being.
    
    Original change's description:
    > Revert "Add metrics of SW static routing API evaluation time for subresources"
    >
    > This reverts commit e6c79c02e389e9f8c1b6bdf44fc33a867cf2a895.
    >
    > Reason for revert: Failing at Network Service Linux bot crbug.com/355104619
    >
    > Bug: 355104619
    >
    > Original change's description:
    > > Add metrics of SW static routing API evaluation time for subresources
    > >
    > > This CL adds metrics to measure the total time spent on router
    > > evaluation of ServiceWorker static routing API evaluation time for
    > > subresources.
    > >
    > > Change-Id: Ic33810c086999bc6ef741bdce830b6c7eeab84a5
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5717591
    > > Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    > > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > > Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1331824}
    >
    > Change-Id: Iae9e95e7637e44f7ca822871a181bc0811403014
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735637
    > Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    > Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    > Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    > Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1332155}
    
    Bug: 355104619
    Change-Id: I99c2cb24ce3039b8cb31632392d498fa0f64df36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735995
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332722}

--- third_party/blink/public/common/service_worker/service_worker_subresource_load_metrics.h
+++ third_party/blink/public/common/service_worker/service_worker_subresource_load_metrics.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_COMMON_SERVICE_WORKER_SERVICE_WORKER_SUBRESOURCE_LOAD_METRICS_H_
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_SERVICE_WORKER_SERVICE_WORKER_SUBRESOURCE_LOAD_METRICS_H_
 
+#include "base/time/time.h"
+
 namespace blink {
 
 // Metrics on subresource load handled by service workers.
@@ -135,6 +137,10 @@ struct ServiceWorkerSubresourceLoadMetrics {
   // Routing API, from navigation start till onload event.
   uint32_t matched_race_network_and_fetch_router_source_count = 0;
 
+  // Total router evaluation time of ServiceWorker Static Routing API
+  // for sub-resources.
+  base::TimeDelta total_router_evaluation_time_for_subresources;
+
   bool operator==(const ServiceWorkerSubresourceLoadMetrics& other) const {
     return image_handled == other.image_handled &&
            image_fallback == other.image_fallback &&
@@ -173,7 +179,9 @@ struct ServiceWorkerSubresourceLoadMetrics {
            matched_cache_router_source_count ==
                other.matched_cache_router_source_count &&
            matched_race_network_and_fetch_router_source_count ==
-               other.matched_race_network_and_fetch_router_source_count;
+               other.matched_race_network_and_fetch_router_source_count &&
+           total_router_evaluation_time_for_subresources ==
+               other.total_router_evaluation_time_for_subresources;
   }
 };
 
