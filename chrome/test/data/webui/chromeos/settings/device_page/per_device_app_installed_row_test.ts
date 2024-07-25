3e3df6219b559ac3d27b0dc7b13b5c38e8fe5310
Michael Checo <michaelcheco@google.com>
2024-07-25 16:32:44 +0000

    [WE] Add per-device-app-installed-row skeleton
    - 'per-device-app-installed-row' is responsible for displaying
       information for an installed app.
    
    Bug: b/329686601
    Change-Id: If87bf564b11bfb6baa2824e83ff101caef2de71c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735496
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333014}

---
+++ chrome/test/data/webui/chromeos/settings/device_page/per_device_app_installed_row_test.ts
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://os-settings/os_settings.js';
+
+import {PerDeviceAppInstalledRowElement} from 'chrome://os-settings/os_settings.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
+
+import {clearBody} from '../utils.js';
+
+suite(PerDeviceAppInstalledRowElement.is, () => {
+  let appRow: PerDeviceAppInstalledRowElement;
+
+  async function createAppRow() {
+    clearBody();
+    appRow = document.createElement(PerDeviceAppInstalledRowElement.is);
+    document.body.appendChild(appRow);
+    return flushTasks();
+  }
+
+  test('Initialize per-device-app-installed-row', async () => {
+    await createAppRow();
+    assertTrue(!!appRow.shadowRoot!.querySelector('.app-installed-row'));
+  });
+});
