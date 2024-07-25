1c0863c9b066694e4f50ce253b5ce0f5a34b118b
Xiyuan Xia <xiyuan@chromium.org>
2024-07-25 14:47:53 +0000

    ash: Move `OnAshRestart` from PreProfileInit to PostProfileInit
    `login_unlock_throughput_recorder()->OnAshRestart()` lets go of
    the deferred task runner. Moving it from `PreProfileInit` stage
    to `PostProfileInit` stage where ChromeSessionManager sets up
    the primary user session. The intentions are:
    - Profile loading is heavy (could take seconds on slow devices) so
      keeping the deferred tasks until it loads
    - New ash `SessionObserver::OnFirstSessionReady` happens as one
      deferred task. It should happen after `OnFirstSessionStarted`.
      `OnFirstSessionStarted` is currently after user profile loading.
    
    Bug: b:341172705
    Change-Id: I7395d84f11985a23c3587c0fc2247ad1f520347a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737468
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332935}

--- chrome/browser/ash/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/chrome_browser_main_parts_ash.cc
@@ -1113,7 +1113,6 @@ void ChromeBrowserMainPartsAsh::PreProfileInit() {
 
     session_manager::SessionManager::Get()->CreateSessionForRestart(
         account_id, user_id_hash);
-    ash::Shell::Get()->login_unlock_throughput_recorder()->OnAshRestart();
 
     // If restarting demo session, mark demo session as started before primary
     // profile starts initialization so browser context keyed services created
--- chrome/browser/ash/login/session/chrome_session_manager.cc
+++ chrome/browser/ash/login/session/chrome_session_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/components/arc/session/arc_vm_data_migration_status.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_switches.h"
+#include "ash/shell.h"
 #include "ash/webui/shimless_rma/shimless_rma.h"
 #include "base/check_deref.h"
 #include "base/command_line.h"
@@ -223,6 +224,11 @@ void StartUserSession(user_manager::UserManager* user_manager,
     // First user has been already marked as logged in and active in
     // PreProfileInit(). Restore sessions for other users in the background.
     user_session_mgr->RestoreActiveSessions();
+
+    // Chrome restart with existing user sessions and the last active user
+    // profile is loaded. Notify ash to signal post login work done at this
+    // stage for chrome restart.
+    ash::Shell::Get()->login_unlock_throughput_recorder()->OnAshRestart();
   }
 
   if (!is_running_test) {
