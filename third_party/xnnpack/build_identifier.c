2fb035c42eb94be31c5f87de8c327cf5a059b831
Catherine Liu <catliu@chromium.org>
2024-07-25 20:30:12 +0000

    Roll TFLite to Next Green Version
    Version Changes:
    XNNPACK: 8df2e765487685757ab5dabd12516cd0a24e3e25 to 562df460dfdcfdbf219dca691550ab7fbe24f2e5
    tflite: f9122e774969459927e9d956674c0aac06eb3b68 to 05691c2af966ef4d08a4efbbf7442357e68aad65
    
    Bug: 201542766
    Change-Id: If9fd8866808df4b3bc9e6db8ec174a2ca03dac59
    Cq-Include-Trybots: luci.chrome.try:optimization_guide-linux;luci.chrome.try:optimization_guide-mac-arm64;luci.chrome.try:optimization_guide-mac-x64;luci.chrome.try:optimization_guide-win32;luci.chrome.try:optimization_guide-win64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737473
    Auto-Submit: Catherine Liu <catliu@chromium.org>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Commit-Queue: Catherine Liu <catliu@chromium.org>
    Reviewed-by: Jonathan Freed <freedjm@google.com>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333177}

--- third_party/xnnpack/build_identifier.c
+++ third_party/xnnpack/build_identifier.c
@@ -9,6 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // The following inputs were used to generate this file.
 // - src/packing.c
+// - src/enums/allocation-type.c
+// - src/enums/datatype-strings.c
+// - src/enums/microkernel-type.c
+// - src/enums/node-type.c
+// - src/enums/operator-type.c
+// - src/log.c
+// - src/configs/hardware-config.c
+// - src/params.c
 // - src/tables/exp2-k-over-64.c
 // - src/tables/exp2-k-over-2048.c
 // - src/tables/exp2minus-k-over-4.c
@@ -18,15 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // - src/tables/exp2minus-k-over-64.c
 // - src/tables/exp2minus-k-over-2048.c
 // - src/tables/vlog.c
-// - src/enums/allocation-type.c
-// - src/enums/datatype-strings.c
-// - src/enums/microkernel-type.c
-// - src/enums/node-type.c
-// - src/enums/operator-type.c
-// - src/log.c
 // - src/amalgam/gen/scalar.c
 // - src/amalgam/gen/avxvnni.c
 // - src/amalgam/gen/avx256skx.c
+// - src/amalgam/gen/avx256vnni.c
+// - src/amalgam/gen/avx256vnnigfni.c
 // - src/amalgam/gen/avx512amx.c
 // - src/amalgam/gen/avx512fp16.c
 // - src/amalgam/gen/sse.c
@@ -49,10 +53,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 static const uint8_t xnn_build_identifier[] = {
-  223,   0,  73, 238, 216,  63, 151,  31,
-  246,  51,  63, 102,  29, 222, 188,  65,
-   34, 198,  57, 119, 152, 102, 194, 148,
-  208, 192,   4,  11, 236, 137,  30, 117
+   18, 134,  63, 187, 249, 246, 156,  79,
+  105,  28,  59,  69, 207, 116, 222,  37,
+   96, 184, 110, 245, 122, 236, 104,  54,
+  207, 223,  91, 224,  84, 102,  62, 112
 };
 
 size_t xnn_experimental_get_build_identifier_size() {
