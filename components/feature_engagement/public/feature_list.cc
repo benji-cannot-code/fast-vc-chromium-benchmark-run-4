0c8b81e5192aa4562cf213d980e2939734fbc7b2
Aashna Sheth <aashnas@google.com>
2024-07-25 17:54:18 +0000

    Clean up 3PCD IPH variants
    This CL removes the 'FullOnboarding' variant which was supposed to be
    used to onboard users to 3PCD. The remaining TrackingProtection variants
    are related to ModeB so are kept.
    
    Bug: b:355003040
    
    Change-Id: Ic64a9e7505ac3b8cae469e0863311ee8370e8735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734477
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333078}

--- components/feature_engagement/public/feature_list.cc
+++ components/feature_engagement/public/feature_list.cc
@@ -212,7 +212,6 @@ const base::Feature* const kAllFeatures[] = {
     &kIPHTabOrganizationSuccessFeature,
     &kIPHTabSearchFeature,
     &kIPHTrackingProtectionOnboardingFeature,
-    &kIPHTrackingProtectionFullOnboardingFeature,
     &kIPHTrackingProtectionReminderFeature,
     &kIPHWebUITabStripFeature,
     &kIPHDesktopPwaInstallFeature,
