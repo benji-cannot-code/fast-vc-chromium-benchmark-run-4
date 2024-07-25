68e56c8766c61e2c905551df5e8dd457c33dea19
Andrew Wolfers <aswolfers@chromium.org>
2024-07-25 21:58:53 +0000

    Add virtual mode creation to mode matching
    When attempting to configure a display, the DrmGpuDisplayManager must
    identify a drm mode which matches the DisplayMode provided by the config
    request. Prior to this change, if no matching mode could be found, the
    configuration would fail.
    
    This change allows displays to be configured with refresh rates that
    would not normally be accessible. Now when a mode cannot be matched for
    a given request, a virtual mode can be created and used in the resulting
    configuration.
    
    Virtual modes can be created on any VRR-capable display. First, the
    closest usable mode is determined (it must have the same size and a
    greater refresh rate). The virtual mode achieves the desired refresh
    rate by copying the timing parameters of the closest mode, except for
    the vtotal which is increased accordingly.
    
    Bug: b:321725211
    Change-Id: Ie4c9c3719cc0eb4ce7e0e1d4c3ff6c9fe2821a32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5635803
    Commit-Queue: Andrew Wolfers <aswolfers@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333235}

--- ui/display/types/display_mode.cc
+++ ui/display/types/display_mode.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ostream>
 
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/strings/stringprintf.h"
 
@@ -42,6 +43,17 @@ std::unique_ptr<DisplayMode> DisplayMode::CopyWithSize(
                                        vsync_rate_min_);
 }
 
+void DisplayMode::set_vsync_rate_min(
+    const std::optional<float>& vsync_rate_min) {
+  if (vsync_rate_min_ == vsync_rate_min) {
+    return;
+  }
+  if (vsync_rate_min_.has_value()) {
+    LOG(WARNING) << "set_vsync_rate_min() called when it is already populated.";
+  }
+  vsync_rate_min_ = vsync_rate_min;
+}
+
 bool DisplayMode::operator<(const DisplayMode& other) const {
   if (size_.GetArea() < other.size_.GetArea())
     return true;
@@ -69,6 +81,11 @@ std::string DisplayMode::ToString() const {
                             size_.ToString().c_str(),
                             is_interlaced_ ? "interlaced " : "", refresh_rate_,
                             vsync_rate_min_.value_or(0));
+}
+
+std::string DisplayMode::ToStringForTest() const {
+  return base::StringPrintf("[size=%s %srate=%f]", size_.ToString().c_str(),
+                            is_interlaced_ ? "interlaced " : "", refresh_rate_);
 }
 
 void PrintTo(const DisplayMode& mode, std::ostream* os) {
