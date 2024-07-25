3dd4446ee69c6dda1553890d2b6a21b87d56687e
Roman Arora <romanarora@chromium.org>
2024-07-25 19:14:15 +0000

    [visited-url-ranking] Transform latency histogram
    Adds a histogram to measure transform execution latency.
    
    Bug: 354271274
    Change-Id: Ide0404deac7d226365203bb7b8e4edb8326a7971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737659
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333143}

--- components/visited_url_ranking/internal/visited_url_ranking_service_impl.cc
+++ components/visited_url_ranking/internal/visited_url_ranking_service_impl.cc
@@ -337,7 +337,7 @@ void VisitedURLRankingServiceImpl::MergeVisitsAndCallback(
   TransformVisitsAndCallback(
       std::move(callback), options, std::move(transform_type_queue),
       URLVisitAggregatesTransformType::kUnspecified,
-      /*previous_aggregates_count=*/0,
+      /*previous_aggregates_count=*/0, base::Time::Now(),
       URLVisitAggregatesTransformer::Status::kSuccess,
       ComputeURLVisitAggregates(std::move(fetcher_results)));
 }
@@ -348,6 +348,7 @@ void VisitedURLRankingServiceImpl::TransformVisitsAndCallback(
     std::queue<URLVisitAggregatesTransformType> transform_type_queue,
     URLVisitAggregatesTransformType transform_type,
     size_t previous_aggregates_count,
+    base::Time start_time,
     URLVisitAggregatesTransformer::Status status,
     std::vector<URLVisitAggregate> aggregates) {
   if (transform_type != URLVisitAggregatesTransformType::kUnspecified) {
@@ -372,6 +373,11 @@ void VisitedURLRankingServiceImpl::TransformVisitsAndCallback(
         base::StringPrintf("VisitedURLRanking.TransformType.%s.InOutPercentage",
                            URLVisitAggregatesTransformTypeName(transform_type)),
         (aggregates.size() / previous_aggregates_count) * 100, 1, 100, 100);
+
+    base::UmaHistogramMediumTimes(
+        base::StringPrintf("VisitedURLRanking.TransformType.%s.Latency",
+                           URLVisitAggregatesTransformTypeName(transform_type)),
+        base::Time::Now() - start_time);
   }
 
   if (transform_type_queue.empty() || aggregates.empty()) {
@@ -400,7 +406,7 @@ void VisitedURLRankingServiceImpl::TransformVisitsAndCallback(
       base::BindOnce(&VisitedURLRankingServiceImpl::TransformVisitsAndCallback,
                      weak_ptr_factory_.GetWeakPtr(), std::move(callback),
                      options, std::move(transform_type_queue), transform_type,
-                     aggregates_count));
+                     aggregates_count, base::Time::Now()));
 }
 
 void VisitedURLRankingServiceImpl::GetNextResult(
