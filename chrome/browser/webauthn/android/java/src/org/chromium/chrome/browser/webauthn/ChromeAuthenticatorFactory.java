817094571697e7ea62745a3cd3cc701a783fa9a2
Adem Derinel <derinel@google.com>
2024-07-25 15:46:56 +0000

    [Android][WebAuthn] Introduce WebAuthnMode.CHROME_3PP_ENABLED
    Chrome is having native 3rd party password manager support (3pPWM). This
    effectively disables Chrome's own PWM and delegates the work to the
    platform.
    
    WebAuthn component should also adapt this mode. In this CL,
    - FIDO2 // CredMan parallel mode is disabled for Android 14+ because
        Chrome does not have TTF in 3pPWM mode.
    - BrowserCredManRequestDecorator is used for 3pPWM mode because branding
        for GPM is not required when the platform is responsible for
        autofill capabilities.
    
    Follow-up CLs will add tests and do the wiring for pre Android 14.
    
    Bug: 355347841
    Change-Id: Idc86378414a0cf5beae48352142fe0bb54fcdc8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741415
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1332985}

--- chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/ChromeAuthenticatorFactory.java
+++ chrome/browser/webauthn/android/java/src/org/chromium/chrome/browser/webauthn/ChromeAuthenticatorFactory.java
@@ -4,14 +4,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 package org.chromium.chrome.browser.webauthn;
 
+import org.chromium.chrome.browser.autofill.AndroidAutofillAvailabilityStatus;
+import org.chromium.chrome.browser.autofill.AutofillClientProviderUtils;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.components.user_prefs.UserPrefs;
 import org.chromium.components.webauthn.AuthenticatorFactory;
 import org.chromium.components.webauthn.WebauthnMode;
 import org.chromium.components.webauthn.WebauthnModeProvider;
 import org.chromium.content_public.browser.RenderFrameHost;
+import org.chromium.content_public.browser.WebContentsStatics;
+
+import java.util.Optional;
 
 public class ChromeAuthenticatorFactory extends AuthenticatorFactory {
     public ChromeAuthenticatorFactory(RenderFrameHost renderFrameHost) {
         super(renderFrameHost, new ChromeAuthenticatorConfirmationFactory());
-        WebauthnModeProvider.getInstance().setGlobalWebauthnMode(WebauthnMode.CHROME);
+        @AndroidAutofillAvailabilityStatus
+        int autofillStatus =
+                Optional.ofNullable(WebContentsStatics.fromRenderFrameHost(renderFrameHost))
+                        .map(Profile::fromWebContents)
+                        .map(UserPrefs::get)
+                        .map(AutofillClientProviderUtils::getAndroidAutofillFrameworkAvailability)
+                        .orElse(AndroidAutofillAvailabilityStatus.SETTING_TURNED_OFF);
+        WebauthnModeProvider.getInstance()
+                .setGlobalWebauthnMode(
+                        autofillStatus == AndroidAutofillAvailabilityStatus.AVAILABLE
+                                ? WebauthnMode.CHROME_3PP_ENABLED
+                                : WebauthnMode.CHROME);
     }
 }
