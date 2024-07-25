c69ad65234a7ab3a12f37726641819d28058872e
Taylor Bergquist <tbergquist@chromium.org>
2024-07-25 18:44:51 +0000

    [Customize Toolbar] Add copy link to side panel.
    Also reorder the actions to match the updated Figma action order.
    
    Bug: 354689887
    Change-Id: I0b0f662534fba2dcdd38f658f3119ef3cf9cb6b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5731216
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1333125}

--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler.cc
@@ -73,6 +73,8 @@ MojoActionForChromeAction(actions::ActionId action_id) {
       return side_panel::customize_chrome::mojom::ActionId::kDevTools;
     case kActionShowChromeLabs:
       return side_panel::customize_chrome::mojom::ActionId::kShowChromeLabs;
+    case kActionCopyUrl:
+      return side_panel::customize_chrome::mojom::ActionId::kCopyLink;
     default:
       return std::nullopt;
   }
@@ -125,6 +127,8 @@ std::optional<actions::ActionId> ChromeActionForMojoAction(
       return kActionDevTools;
     case side_panel::customize_chrome::mojom::ActionId::kShowChromeLabs:
       return kActionShowChromeLabs;
+    case side_panel::customize_chrome::mojom::ActionId::kCopyLink:
+      return kActionCopyUrl;
     default:
       return std::nullopt;
   }
@@ -179,8 +183,6 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
               .Rasterize(provider),
           scale_factor)));
 
-  actions.push_back(std::move(home_action));
-
   auto forward_action = side_panel::customize_chrome::mojom::Action::New(
       MojoActionForChromeAction(kActionForward).value(),
       base::UTF16ToUTF8(l10n_util::GetStringUTF16(IDS_ACCNAME_FORWARD)),
@@ -191,8 +193,6 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
               vector_icons::kForwardArrowChromeRefreshIcon, ui::kColorIcon)
               .Rasterize(provider),
           scale_factor)));
-
-  actions.push_back(std::move(forward_action));
 
   const auto add_action =
       [&actions, model, provider, scale_factor, scope_action](
@@ -213,18 +213,20 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
         actions.push_back(std::move(mojo_action));
       };
 
+  actions.push_back(std::move(home_action));
+  actions.push_back(std::move(forward_action));
   add_action(kActionNewIncognitoWindow,
              side_panel::customize_chrome::mojom::CategoryId::kNavigation);
 
   add_action(kActionShowPasswordsBubbleOrPage,
              side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
-  add_action(kActionSidePanelShowBookmarks,
-             side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
-  add_action(kActionSidePanelShowReadingList,
-             side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
   add_action(kActionShowPaymentsBubbleOrPage,
              side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
   add_action(kActionShowAddressesBubbleOrPage,
+             side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
+  add_action(kActionSidePanelShowBookmarks,
+             side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
+  add_action(kActionSidePanelShowReadingList,
              side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
   add_action(kActionSidePanelShowHistoryCluster,
              side_panel::customize_chrome::mojom::CategoryId::kYourChrome);
@@ -241,13 +243,15 @@ void CustomizeToolbarHandler::ListActions(ListActionsCallback callback) {
              side_panel::customize_chrome::mojom::CategoryId::kTools);
   add_action(kActionShowTranslate,
              side_panel::customize_chrome::mojom::CategoryId::kTools);
-  add_action(kActionSendTabToSelf,
-             side_panel::customize_chrome::mojom::CategoryId::kTools);
   add_action(kActionQrCodeGenerator,
              side_panel::customize_chrome::mojom::CategoryId::kTools);
   add_action(kActionRouteMedia,
              side_panel::customize_chrome::mojom::CategoryId::kTools);
   add_action(kActionSidePanelShowReadAnything,
+             side_panel::customize_chrome::mojom::CategoryId::kTools);
+  add_action(kActionCopyUrl,
+             side_panel::customize_chrome::mojom::CategoryId::kTools);
+  add_action(kActionSendTabToSelf,
              side_panel::customize_chrome::mojom::CategoryId::kTools);
   add_action(kActionTaskManager,
              side_panel::customize_chrome::mojom::CategoryId::kTools);
