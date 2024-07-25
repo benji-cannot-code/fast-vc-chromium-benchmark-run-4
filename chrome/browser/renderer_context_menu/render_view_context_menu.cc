efaecb657ac101742844a1d14d7c566dedc3a976
Emmanuel Arias Soto <eariassoto@google.com>
2024-07-25 17:50:06 +0000

    policy: Disable 'Save as' menu item for URLs blocked by URLBlocklist
    This change prevents managed users from saving content from pages
    blocked by administrators.
    
    Test: ./browser_tests
    
    Change-Id: I65e51a93ce00d3128bd23311b5158a4c1ec1b9b0
    Bug: b:275091656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741495
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Emmanuel Arias Soto <eariassoto@google.com>
    Reviewed-by: Sergiy Belozorov <sergiyb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333072}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -3679,11 +3679,20 @@ bool RenderViewContextMenu::IsSaveAsItemAllowedByPolicy() const {
   if (!local_state->GetBoolean(prefs::kAllowFileSelectionDialogs)) {
     return false;
   }
+
   // Check if all downloads are forbidden by policy.
   if (DownloadPrefs::FromBrowserContext(GetProfile())->download_restriction() ==
       DownloadPrefs::DownloadRestriction::ALL_FILES) {
     return false;
   }
+
+  PolicyBlocklistService* service =
+      PolicyBlocklistFactory::GetForBrowserContext(browser_context_);
+  if (service->GetURLBlocklistState(params_.link_url) ==
+      policy::URLBlocklist::URLBlocklistState::URL_IN_BLOCKLIST) {
+    return false;
+  }
+
   return true;
 }
 
@@ -3768,13 +3777,6 @@ bool RenderViewContextMenu::IsSaveLinkAsEnabled() const {
   }
 
   if (!IsSaveAsItemAllowedByUntrustedNetworkStatus()) {
-    return false;
-  }
-
-  PolicyBlocklistService* service =
-      PolicyBlocklistFactory::GetForBrowserContext(browser_context_);
-  if (service->GetURLBlocklistState(params_.link_url) ==
-      policy::URLBlocklist::URLBlocklistState::URL_IN_BLOCKLIST) {
     return false;
   }
 
