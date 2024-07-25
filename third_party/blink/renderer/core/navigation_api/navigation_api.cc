60a5dd2626469bca1fe6593a2fff325c32785280
Domenic Denicola <domenic@chromium.org>
2024-07-25 00:00:52 +0000

    Navigation API: always resolve URLs using UTF-8
    Follows https://github.com/whatwg/html/pull/9756.
    
    Fixed: 40283021
    Change-Id: Iac654e94044fb97c4618599fee8ecedcd0efdb4d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5722413
    Auto-Submit: Domenic Denicola <domenic@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332686}

--- third_party/blink/renderer/core/navigation_api/navigation_api.cc
+++ third_party/blink/renderer/core/navigation_api/navigation_api.cc
@@ -51,6 +51,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
+namespace {
+
+// Kill switch feature for fixing https://crbug.com/40283021.
+// TODO(domenic): clean this up after that fix has baked in stable for a few
+// milestones.
+BASE_FEATURE(kUtf8OnlyNavigationApiNavigations,
+             "Utf8OnlyNavigationApiNavigations",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+}  // namespace
+
 template <typename... DOMExceptionArgs>
 NavigationResult* EarlyErrorResult(ScriptState* script_state,
                                    DOMExceptionArgs&&... args) {
@@ -461,7 +471,10 @@ void NavigationApi::updateCurrentEntry(
 NavigationResult* NavigationApi::navigate(ScriptState* script_state,
                                           const String& url,
                                           NavigationNavigateOptions* options) {
-  KURL completed_url = window_->CompleteURL(url);
+  KURL completed_url =
+      base::FeatureList::IsEnabled(kUtf8OnlyNavigationApiNavigations)
+          ? KURL(window_->BaseURL(), url)
+          : window_->CompleteURL(url);
   if (!completed_url.IsValid()) {
     return EarlyErrorResult(script_state, DOMExceptionCode::kSyntaxError,
                             "Invalid URL '" + completed_url.GetString() + "'.");
