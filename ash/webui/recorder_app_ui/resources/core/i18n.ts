801235005b9ab1b4f383bfa5dd9ef40d6179338c
Pi-Hsun Shih <pihsun@chromium.org>
2024-07-25 06:34:34 +0000

    recorder: Add disabled state for summary
    Spec is updated to have a minimal UI when summary is disabled by user.
    
    Bug: b:344791047
    Test: manually
    Change-Id: Id9bfbddb0e36c9ade4a2cbf9254e884495d23984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736710
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332806}

--- ash/webui/recorder_app_ui/resources/core/i18n.ts
+++ ash/webui/recorder_app_ui/resources/core/i18n.ts
@@ -93,6 +93,7 @@ const noArgStrings = [
   'settingsOptionsTranscriptionLabel',
   'settingsSectionGeneralHeader',
   'settingsSectionTranscriptionSummaryHeader',
+  'summaryDisabledLabel',
   'summaryDownloadModelDescription',
   'summaryDownloadModelDisableButton',
   'summaryDownloadModelDownloadButton',
