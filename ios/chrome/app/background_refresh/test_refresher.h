02de5c454275a6623dc8ff21505df4da04247eb9
Mark Cogan <marq@google.com>
2024-07-25 16:57:38 +0000

    App refresh agent and logging refresh provider.
    This is a very preliminary implementation of app refresh support, sufficient for collecting further data in Canary. It's not suitable for any kind of production use yet.
    
    Change-Id: Id655b21f07f8b064afe9a962e8ac70c50f2bc909
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5494245
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333032}

---
+++ ios/chrome/app/background_refresh/test_refresher.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_BACKGROUND_REFRESH_TEST_REFRESHER_H_
+#define IOS_CHROME_APP_BACKGROUND_REFRESH_TEST_REFRESHER_H_
+
+#import <Foundation/Foundation.h>
+
+#import "ios/chrome/app/background_refresh/app_refresh_provider.h"
+
+@class AppState;
+
+// App refresher for collecting test data; not intended for production use.
+@interface TestRefresher : AppRefreshProvider
+
+// Don't use this as a precedent; an app state shouldn't be injected into a
+// refresh provider.
+- (instancetype)initWithAppState:(AppState*)appState;
+
+@end
+
+#endif  // IOS_CHROME_APP_BACKGROUND_REFRESH_TEST_REFRESHER_H_
