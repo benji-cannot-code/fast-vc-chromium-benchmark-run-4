655babd9c52510e375adc72ccf61e9cf5bb81612
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-25 09:28:58 +0000

    [sync] Revisit some static_assert() for GetNumModelTypes()
    1) Turn the ChromeBackupAgent one into an assert for the number of
    selectable types. The backed up prefs are per selectable type, not
    model type.
    
    2) Move the TypesRequiringUnsyncedDataCheckOnSignout() one from the
    unit test to the implementation, where it's more actionable. Also
    delete the trivial test, it doesn't bring much value.
    
    Bug: 355159574
    Change-Id: Iab080d97505bc11532a53b5c782a8d1fd123e35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739153
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1332844}

--- chrome/browser/android/chrome_backup_agent.cc
+++ chrome/browser/android/chrome_backup_agent.cc
@@ -19,9 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "chrome/android/chrome_jni_headers/ChromeBackupAgentImpl_jni.h"
 
-static_assert(53 == syncer::GetNumModelTypes(),
-              "If the new type has a corresponding pref, add it to "
-              "ChromeBackupAgentImpl.BACKUP_NATIVE_BOOL_PREFS");
+static_assert(14 == static_cast<int>(syncer::UserSelectableType::kLastType),
+              "When adding a new selectable type, add its pref to"
+              "ChromeBackupAgentImpl.BACKUP_NATIVE_SYNC_TYPE_BOOL_PREFS if the"
+              "type exists on Android");
 
 void JNI_ChromeBackupAgentImpl_CommitPendingPrefWrites(
     JNIEnv* env,
