71e070a182831f7e9e741e5ac0e9c4f391bef156
Jay Zhou <zhouzj@google.com>
2024-07-25 15:11:30 +0000

    Move common metric code to shared location
    There is some code in UKM that can be leveraged by other metrics. To prevent code duplication, we want to consolidate this code in a common location so all metrics can leverage it.
    
    Fixed: 40870309
    Change-Id: Iefd25896380679777030191809a4bb0483b1f669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5727639
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Jay Zhou <zhouzj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332958}

--- tools/metrics/common/presubmit_util.py
+++ tools/metrics/common/presubmit_util.py
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2015 The Chromium Authors
+# Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -111,3 +111,34 @@ def DoPresubmit(argv,
 
 def DoPresubmitMain(*args, **kwargs):
   sys.exit(DoPresubmit(*args, **kwargs))
+
+
+def CheckChange(xml_file, input_api, output_api):
+  """Checks that xml is pretty-printed and well-formatted."""
+  for f in input_api.AffectedTextFiles():
+    p = f.AbsoluteLocalPath()
+    if (input_api.basename(p) == xml_file
+        and input_api.os_path.dirname(p) == input_api.PresubmitLocalPath()):
+      cwd = input_api.os_path.dirname(p)
+
+      exit_code = input_api.subprocess.call(
+          [input_api.python3_executable, 'pretty_print.py', '--presubmit'],
+          cwd=cwd)
+      if exit_code != 0:
+        return [
+            output_api.PresubmitError(
+                '%s is not prettified; run `git cl format` to fix.' % xml_file),
+        ]
+
+      exit_code = input_api.subprocess.call(
+          [input_api.python3_executable, 'validate_format.py', '--presubmit'],
+          cwd=cwd)
+      if exit_code != 0:
+        return [
+            output_api.PresubmitError(
+                '%s does not pass format validation; run %s/validate_format.py '
+                'and fix the reported error(s) or warning(s).' %
+                (xml_file, input_api.PresubmitLocalPath())),
+        ]
+
+  return []
