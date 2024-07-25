197a08ff00a0a1467b1d03e7aa0f5499786f2977
Amelie Schneider <amelies@google.com>
2024-07-25 16:35:48 +0000

    [Profiles] Ensure icons are visible with web store themes
    With the feature flag "OutlineSilhouetteIcon" enabled, the avatar icon
    in the toolbar and app menu were sometimes not visible when the user
    chose a theme from the Chrome Web Store (see https://screenshot.googleplex.com/6GLU85UTuW9n2TC)
    
    This CL sets the colors of the icons in the toolbar and app menu so
    that in the case the user is using a theme from the web store, the icons
    will be visible against the background. If the user is using a
    standard theme, the stroke color is already chosen in a way where it is
    always visible against the background. Therefore, in case of standard
    themes, no changes are needed.
    
    Screenshots, using "Black & White" theme:
    - Light mode: https://screenshot.googleplex.com/56ZSK3qazDibd2r
    - Dark mode: https://screenshot.googleplex.com/8BJ7HVXY2zLrJch
    
    Bug: b:307739359
    
    Change-Id: Iaafc5dd92fcb21808b933114017b8314ddc00b7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703702
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333018}

--- chrome/browser/ui/toolbar/app_menu_model.cc
+++ chrome/browser/ui/toolbar/app_menu_model.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_util.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/browser/sync/sync_ui_util.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -498,6 +499,7 @@ ProfileSubMenuModel::ProfileSubMenuModel(
     if (BuildSyncSection()) {
       AddSeparator(ui::NORMAL_SEPARATOR);
     }
+
     ProfileAttributesEntry* profile_attributes =
         GetProfileAttributesFromProfile(profile);
     // If the profile is being deleted, profile_attributes may be null.
@@ -507,8 +509,10 @@ ProfileSubMenuModel::ProfileSubMenuModel(
           account_info.IsEmpty()
               ? profile_attributes->GetAvatarIcon(
                     avatar_icon_size, /*use_high_res_file=*/true,
-                    /*icon_params=*/
-                    {.has_padding = false, .has_background = false})
+                    GetPlaceholderAvatarIconParamsDependingOnTheme(
+                        ThemeServiceFactory::GetForProfile(profile),
+                        /*background_color_id=*/ui::kColorMenuBackground,
+                        *color_provider))
               : account_info.account_image;
       // The avatar image can be empty if the account image hasn't been
       // fetched yet, if there is no image, or in tests.
