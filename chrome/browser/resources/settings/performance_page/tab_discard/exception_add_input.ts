589107329c608852ac083b273562742d0fe43e26
rbpotter <rbpotter@chromium.org>
2024-07-25 23:05:01 +0000

    Settings: Remove some uses of iron-pages
    Specifically removing from clear browsing data dialog and the
    exception tabbed add dialog.
    
    For exception tabbed add dialog, manually calling notifyResize() for
    the iron-list child, since cr-page-selector does not use
    iron-resizable-behavior.
    
    Bug: 40943652
    Change-Id: Iafc3bd450d01839c304843cfa15e66de1f5f958f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741841
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333262}

--- chrome/browser/resources/settings/performance_page/tab_discard/exception_add_input.ts
+++ chrome/browser/resources/settings/performance_page/tab_discard/exception_add_input.ts
@@ -4,7 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
-import 'chrome://resources/polymer/v3_0/iron-pages/iron-pages.js';
 
 import type {PrefsMixinInterface} from '/shared/settings/prefs/prefs_mixin.js';
 import {PrefsMixin} from '/shared/settings/prefs/prefs_mixin.js';
