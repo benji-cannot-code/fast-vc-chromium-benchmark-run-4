955046e0828facec749991fb084c8b1f962c1752
Brahim Chikhaoui <bchikhaoui@google.com>
2024-07-25 09:13:02 +0000

    [OOBE][Perks] Implement userAction in PerksDiscoveryScreen
    Implement logic of user actions in perks screen
    
    Bug: b:347181097 , b:347181930
    Change-Id: If6c4a916e7c707d83b31115ee90d52372b8c68a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703699
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332839}

--- chrome/browser/ash/login/screens/perks_discovery_screen.cc
+++ chrome/browser/ash/login/screens/perks_discovery_screen.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
+constexpr const char kUserActionFinish[] = "finished";
 constexpr const char kUserActionLoaded[] = "loaded";
 
 std::vector<SinglePerkDiscoveryPayload> ParsePayload(
@@ -193,6 +194,13 @@ void PerksDiscoveryScreen::OnUserAction(const base::Value::List& args) {
     // transition if the loading step finishes too quickly.
     delay_overview_timer_.Start(FROM_HERE, delay_overview_step_, this,
                                 &PerksDiscoveryScreen::ShowOverviewStep);
+    return;
+  }
+
+  if (action_id == kUserActionFinish) {
+    CHECK_EQ(args.size(), 2u);
+    // TODO(b/347182375) forward the action to campaign manager
+    exit_callback_.Run(Result::kNext);
     return;
   }
 
