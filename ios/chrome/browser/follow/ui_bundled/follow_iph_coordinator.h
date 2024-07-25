1d68567abc560882bfe73bb5345397db2470231b
Maksim Moskvitin <mmoskvitin@google.com>
2024-07-25 09:36:21 +0000

    [Code Health][iOS] Move ios/c/b/ui/follow to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324232783
    Change-Id: I74615b92a819b70c3544c16303885d8b53fd1b77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735386
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1332846}

--- ios/chrome/browser/follow/ui_bundled/follow_iph_coordinator.h
+++ ios/chrome/browser/follow/ui_bundled/follow_iph_coordinator.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_FOLLOW_FOLLOW_IPH_COORDINATOR_H_
-#define IOS_CHROME_BROWSER_UI_FOLLOW_FOLLOW_IPH_COORDINATOR_H_
+#ifndef IOS_CHROME_BROWSER_FOLLOW_UI_BUNDLED_FOLLOW_IPH_COORDINATOR_H_
+#define IOS_CHROME_BROWSER_FOLLOW_UI_BUNDLED_FOLLOW_IPH_COORDINATOR_H_
 
 #import "ios/chrome/browser/follow/model/follow_iph_presenter.h"
 #import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
@@ -15,4 +15,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @interface FollowIPHCoordinator : ChromeCoordinator <FollowIPHPresenter>
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_FOLLOW_FOLLOW_IPH_COORDINATOR_H_
+#endif  // IOS_CHROME_BROWSER_FOLLOW_UI_BUNDLED_FOLLOW_IPH_COORDINATOR_H_
--- ios/chrome/browser/follow/ui_bundled/follow_iph_coordinator.mm
+++ ios/chrome/browser/follow/ui_bundled/follow_iph_coordinator.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/follow/follow_iph_coordinator.h"
+#import "ios/chrome/browser/follow/ui_bundled/follow_iph_coordinator.h"
 
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service_factory.h"
