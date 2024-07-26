7e47cffed4ce16e1b7a7b83199a430f10caf631d
Aishwarya Rajesh <aishwaryarj@google.com>
2024-07-26 19:14:36 +0000

    [TSLO] Update desktop windowing / tab tearing implementation to allow API backport on pre-V devices
    Low-Coverage-Reason: OTHER The updates in WindowInsetsUtils are for backward API compatibility and would otherwise WAI via direct OS API calls that are currently not testable due to lack of OS version support in Robolectric.
    Bug: 351028771
    Change-Id: I42c0db39d0ec55805c106e55c7e9ea21f1bdda7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740743
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333715}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -491,7 +491,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
             mDragDropTouchObserver = null;
         }
 
-        if (mAppHeaderCoordinator != null && VERSION.SDK_INT >= VERSION_CODES.VANILLA_ICE_CREAM) {
+        if (mAppHeaderCoordinator != null && VERSION.SDK_INT >= VERSION_CODES.R) {
             mAppHeaderCoordinator.destroy();
             mAppHeaderCoordinator = null;
         }
@@ -1179,7 +1179,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
         mToolbarManager.getTabStripHeightSupplier().addObserver(mOnTabStripHeightChangedCallback);
     }
 
-    @SuppressWarnings("NewApi") // Android V check is done via helper method.
+    @SuppressWarnings("NewApi") // OS version check is done via helper method.
     private void initAppHeaderCoordinator(Bundle savedInstanceState) {
         boolean isTablet = DeviceFormFactor.isNonMultiDisplayContextOnTablet(mActivity);
         if (!ToolbarFeatures.isTabStripWindowLayoutOptimizationEnabled(isTablet)) {
