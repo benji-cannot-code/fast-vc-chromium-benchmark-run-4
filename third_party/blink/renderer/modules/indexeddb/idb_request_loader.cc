85408102b2bac6dc54edec5dd48360e2645529e1
Evan Stade <estade@chromium.org>
2024-07-26 19:07:23 +0000

    Revert "IDB: add debug code to help trace down source of missing large value"
    This reverts commit e049586515e000258d6ad560b9ba35e0c4273d8b.
    
    Reason for revert: debug info no longer needed
    
    Original change's description:
    > IDB: add debug code to help trace down source of missing large value
    >
    > Bug: 342779913
    > Change-Id: I53b4708c5441847bfc024eeab9f03f6478a0b3a3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5622697
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Reviewed-by: Ayu Ishii <ayui@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1314084}
    
    Bug: 347104233
    Change-Id: Ic37d2c78cec2e4f2ef6f91bef455b4c0565f0c35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743916
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333712}

--- third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
+++ third_party/blink/renderer/modules/indexeddb/idb_request_loader.cc
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "base/debug/dump_without_crashing.h"
-#include "components/crash/core/common/crash_key.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -145,9 +143,6 @@ void IDBRequestLoader::DidFail(FileErrorCode) {
   DCHECK(file_reader_loading_);
   file_reader_loading_ = false;
 #endif  // DCHECK_IS_ON()
-
-  // TODO(https://crbug.com/3342779913): fix bug and remove this debug code.
-  base::debug::DumpWithoutCrashing();
 
   OnLoadComplete(/*error=*/true);
 }
