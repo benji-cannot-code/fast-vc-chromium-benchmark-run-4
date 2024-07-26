a9834ad0123b3b1c6bb3c2071552ebac23cf2b94
Maksim Moskvitin <mmoskvitin@google.com>
2024-07-26 08:09:06 +0000

    [Code Health][iOS] Move ios/c/b/ui/incognito_interstitial to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324232905
    Change-Id: I1d3d19d1220082d524fe9a5a7b3777f06d54ac42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735362
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333418}

--- ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_constants.mm
+++ ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_constants.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-#import "ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_constants.h"
+#import "ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_constants.h"
 
 NSString* const kIncognitoInterstitialAccessibilityIdentifier =
     @"incognitoInterstitialAccessibilityIdentifier";
--- ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_view_controller.h
+++ ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_view_controller.h
@@ -3,10 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_INCOGNITO_INTERSTITIAL_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
-#define IOS_CHROME_BROWSER_UI_INCOGNITO_INTERSTITIAL_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
+#ifndef IOS_CHROME_BROWSER_INCOGNITO_INTERSTITIAL_UI_BUNDLED_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
+#define IOS_CHROME_BROWSER_INCOGNITO_INTERSTITIAL_UI_BUNDLED_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
 
-#import "ios/chrome/browser/ui/incognito_interstitial/incognito_interstitial_view_controller_delegate.h"
+#import "ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_view_controller_delegate.h"
 #import "ios/chrome/common/ui/promo_style/promo_style_view_controller.h"
 
 @protocol NewTabPageURLLoaderDelegate;
@@ -26,4 +26,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-#endif  // IOS_CHROME_BROWSER_UI_INCOGNITO_INTERSTITIAL_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
+#endif  // IOS_CHROME_BROWSER_INCOGNITO_INTERSTITIAL_UI_BUNDLED_INCOGNITO_INTERSTITIAL_VIEW_CONTROLLER_H_
