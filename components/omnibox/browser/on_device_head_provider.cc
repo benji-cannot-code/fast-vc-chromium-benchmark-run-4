3bd1b4064886eb7a9c8404322d491e16d90f8494
Sergii Bykov <sbykov@google.com>
2024-07-26 18:28:44 +0000

    [Code Health] Remove expired omnibox OnDeviceHeadSuggest histograms
    Bug: 40878888
    Change-Id: I7617f9c7de4264c5ce32b7d29b1394efc59cf172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742340
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333686}

--- components/omnibox/browser/on_device_head_provider.cc
+++ components/omnibox/browser/on_device_head_provider.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/thread_pool.h"
-#include "base/time/time.h"
 #include "base/trace_event/trace_event.h"
 #include "components/omnibox/browser/autocomplete_provider_listener.h"
 #include "components/omnibox/browser/base_search_provider.h"
@@ -85,9 +84,6 @@ struct OnDeviceHeadProvider::OnDeviceHeadProviderParams {
 
   // Indicates whether this request failed or not.
   bool failed = false;
-
-  // The time when this request is created.
-  base::TimeTicks creation_time;
 
   OnDeviceHeadProviderParams(size_t request_id, const AutocompleteInput& input)
       : request_id(request_id), input(input) {}
@@ -209,7 +205,6 @@ OnDeviceHeadProvider::GetSuggestionsFromHeadModel(
     return params;
   }
 
-  params->creation_time = base::TimeTicks::Now();
   std::string sanitized_input = SanitizeInput(params->input.text());
 
   auto results = OnDeviceHeadModel::GetSuggestionsForPrefix(
@@ -315,8 +310,6 @@ void OnDeviceHeadProvider::AllSearchDone(
       client()->GetTemplateURLService();
 
   if (search::DefaultSearchProviderIsGoogle(template_url_service)) {
-    UMA_HISTOGRAM_CUSTOM_COUNTS("Omnibox.OnDeviceHeadSuggest.ResultCount",
-                                params->suggestions.size(), 1, 5, 6);
     matches_.clear();
 
     int head_relevance = params->input.type() == metrics::OmniboxInputType::URL
@@ -352,8 +345,6 @@ void OnDeviceHeadProvider::AllSearchDone(
         tail_relevance--;
       }
     }
-    UMA_HISTOGRAM_TIMES("Omnibox.OnDeviceHeadSuggest.AsyncQueryTime",
-                        base::TimeTicks::Now() - params->creation_time);
   }
 
   done_ = true;
