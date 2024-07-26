dba1293fbecc509fe2620201fe8802c56668b6d5
Nathan Memmott <memmott@chromium.org>
2024-07-26 00:27:54 +0000

    Change Observers: Add basic WPTs
    Adds some WPTs that just test the basic expectations of the API.
    
    Bug: 341136316
    Change-Id: I1d21afef7f920ecf58745f8b2a628918250c6b2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739854
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333296}

--- third_party/blink/web_tests/external/wpt/fs/FileSystemObserver.https.tentative.window.js
+++ third_party/blink/web_tests/external/wpt/fs/FileSystemObserver.https.tentative.window.js
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/service-workers/service-worker/resources/test-helpers.sub.js
 // META: script=resources/messaging-helpers.js
+// META: script=resources/sandboxed-fs-test-helpers.js
 // META: script=script-tests/FileSystemObserver.js
--- third_party/blink/web_tests/external/wpt/fs/script-tests/FileSystemObserver.js
+++ third_party/blink/web_tests/external/wpt/fs/script-tests/FileSystemObserver.js
@@ -7,13 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 promise_test(async t => {
   function dummyCallback(records, observer) {};
-  let success = true;
   try {
     const observer = new FileSystemObserver(dummyCallback);
   } catch (error) {
-    success = false;
+    assert_unreached();
   }
-  assert_true(success);
 }, 'Creating a FileSystemObserver from a window succeeds');
 
 promise_test(async t => {
@@ -56,3 +54,45 @@ promise_test(async t => {
 
   assert_false(response.createObserverSuccess);
 }, 'Creating a FileSystemObserver from a service worker fails');
+
+directory_test(async (t, root_dir) => {
+  function dummyCallback(records, observer) {};
+  const observer = new FileSystemObserver(dummyCallback);
+  try {
+    observer.unobserve(root_dir);
+  } catch (error) {
+    assert_unreached();
+  }
+}, 'Calling unobserve() without a corresponding observe() shouldn\t throw');
+
+directory_test(async (t, root_dir) => {
+  function dummyCallback(records, observer) {};
+  const observer = new FileSystemObserver(dummyCallback);
+  try {
+    observer.unobserve(root_dir);
+    observer.unobserve(root_dir);
+  } catch (error) {
+    assert_unreached();
+  }
+}, 'unobserve() is idempotent');
+
+promise_test(async t => {
+  function dummyCallback(records, observer) {};
+  const observer = new FileSystemObserver(dummyCallback);
+  try {
+    observer.disconnect();
+  } catch (error) {
+    assert_unreached();
+  }
+}, 'Calling disconnect() without observing shouldn\'t throw');
+
+promise_test(async t => {
+  function dummyCallback(records, observer) {};
+  const observer = new FileSystemObserver(dummyCallback);
+  try {
+    observer.disconnect();
+    observer.disconnect();
+  } catch (error) {
+    assert_unreached();
+  }
+}, 'disconnect() is idempotent');
