ff39531e10037a235c9b5d0059bf6a5c48148d81
Gauthier Ambard <gambard@chromium.org>
2024-07-26 11:12:33 +0000

    [iOS] Open tab groups from other windows from the third panel
    When the user is tapping on a tab group in the tab groups panel, if
    it is already opened on another window, the other window should be
    put to foreground and the group opened.
    
    Also moves the Tab Group Commands to the shared place.
    
    https://screencast.googleplex.com/cast/NTg2NTI2NzU0NzYwMjk0NHwyZDIyM2NjYS04Yw
    
    Bug: 329626537
    Change-Id: I5c58b901e0ef565b61184c39908d669ce8a1b83f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735101
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333470}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_coordinator.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/base_grid_coordinator.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_GRID_BASE_GRID_COORDINATOR_H_
 
 #import "ios/chrome/browser/shared/coordinator/chrome_coordinator/chrome_coordinator.h"
+#import "ios/chrome/browser/shared/public/commands/tab_groups_commands.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/create_or_edit_tab_group_coordinator_delegate.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_commands.h"
 
 class Browser;
 @protocol DisabledGridViewControllerDelegate;
--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_coordinator.mm
@@ -11,14 +11,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
+#import "ios/chrome/browser/shared/public/commands/tab_groups_commands.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/grid/tab_group_grid_view_controller.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_helper.h"
+#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_idle_status_handler.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_mediator.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_positioner.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_view_controller.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_groups_commands.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_context_menu/tab_context_menu_helper.h"
-#import "ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_idle_status_handler.h"
 #import "ios/web/public/web_state_id.h"
 
 namespace {
