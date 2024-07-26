1008b8e8cfb8e2e401a3ae134b26f00891ee863d
Mahmoud Rashad <mmrashad@google.com>
2024-07-26 11:07:46 +0000

    Add batch upload dialog, shows when user interact with batch upload card
    This is the batch upload dialog which allow the user to choose which
    data types to upload.
    
    All of the behavior changes in this patch are behind
    ChromeFeatureList.ENABLE_BATCH_UPLOAD_FROM_SETTINGS, which is disabled
    by default.
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL tests in a dedicated CL.
    Bug: 326040498
    Change-Id: I36b1fa4f1992ab40d876f7cd5f6bf6dc56265456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5729872
    Reviewed-by: Marc Treib <treib@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333469}

--- components/sync/android/java/src/org/chromium/components/sync/LocalDataDescription.java
+++ components/sync/android/java/src/org/chromium/components/sync/LocalDataDescription.java
@@ -45,6 +45,10 @@ public class LocalDataDescription {
         return mItemCount;
     }
 
+    public int domainCount() {
+        return mDomainCount;
+    }
+
     @NativeMethods
     interface Natives {
         @JniType("std::u16string")
