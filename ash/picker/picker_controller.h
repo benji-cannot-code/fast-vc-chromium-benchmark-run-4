10288547aa5fedadbb68dccbf529bc427e18b970
Darren Shen <shend@chromium.org>
2024-07-26 00:21:45 +0000

    Fix crash when suggestions return while Widget is closing
    PickerView has dependencies on objects in PickerController. These
    dependencies need to remain valid for the lifetime of PickerView.
    
    Previously, these dependencies were invalidated when the Widget is
    being destroyed. However, this happens before PickerView destruction,
    which means there's a brief moment where PickerView is still alive but
    the dependencies are no longer valid.
    
    Instead of invalidating on Widget destruction, invalidate it when
    PickerView is destroyed.
    
    Bug: b:355310155
    Change-Id: Iba8f59a9ff79f8978aa086642fad3a9b08abfb3f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740717
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333291}

--- ash/picker/picker_controller.h
+++ ash/picker/picker_controller.h
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "ui/base/emoji/emoji_panel_helper.h"
+#include "ui/views/view_observer.h"
 #include "ui/views/widget/unique_widget_ptr.h"
-#include "ui/views/widget/widget_observer.h"
 
 namespace ash {
 
@@ -42,7 +42,7 @@ class PickerSuggestionsController;
 
 // Controls a Picker widget.
 class ASH_EXPORT PickerController : public PickerViewDelegate,
-                                    public views::WidgetObserver,
+                                    public views::ViewObserver,
                                     public PickerAssetFetcherImplDelegate {
  public:
   PickerController();
@@ -110,8 +110,8 @@ class ASH_EXPORT PickerController : public PickerViewDelegate,
   std::vector<PickerSearchResult> GetSuggestedEmoji() override;
   bool IsGifsEnabled() override;
 
-  // views:WidgetObserver:
-  void OnWidgetDestroying(views::Widget* widget) override;
+  // views:ViewObserver:
+  void OnViewIsDeleting(views::View* view) override;
 
   // PickerAssetFetcherImplDelegate:
   void FetchFileThumbnail(const base::FilePath& path,
@@ -152,8 +152,8 @@ class ASH_EXPORT PickerController : public PickerViewDelegate,
   // Records metrics related to a session.
   std::unique_ptr<PickerSessionMetrics> session_metrics_;
 
-  base::ScopedObservation<views::Widget, views::WidgetObserver>
-      widget_observation_{this};
+  base::ScopedObservation<views::View, views::ViewObserver> view_observation_{
+      this};
 
   // Closes CapsLock state view after some time.
   base::OneShotTimer caps_lock_state_view_close_timer_;
