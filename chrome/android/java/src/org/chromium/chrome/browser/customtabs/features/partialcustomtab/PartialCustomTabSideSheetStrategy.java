adef1590738cd8a39649b7c6fa861436d0a687c2
Jinsuk Kim <jinsukkim@chromium.org>
2024-07-26 23:33:19 +0000

    [CCT] Remove expired histograms
    CustomTabs.SideSheetResizeType, CustomTabs.ResizeType2* are not in use
    and soon expiring. This CL removes them from histogram/code.
    
    OBSOLETE_HISTOGRAM[CustomTabs.SideSheetResizeType]=not used any more
    OBSOLETE_HISTOGRAM[CustomTabs.ResizeType2]=not used any more
    
    Bug: 354713196, 349764991
    Change-Id: Icdc210ed0465fb1ee68cf27255c3bf3a44f2877f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739950
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333830}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/features/partialcustomtab/PartialCustomTabSideSheetStrategy.java
@@ -37,7 +37,6 @@ import androidx.browser.customtabs.CustomTabsIntent;
 
 import org.chromium.base.MathUtils;
 import org.chromium.base.SysUtils;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider;
@@ -160,12 +159,6 @@ public class PartialCustomTabSideSheetStrategy extends PartialCustomTabBaseStrat
 
     @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
     boolean toggleMaximize(boolean animate) {
-        @ResizeType
-        int resizeType =
-                mIsMaximized ? ResizeType.MANUAL_MINIMIZATION : ResizeType.MANUAL_EXPANSION;
-        RecordHistogram.recordEnumeratedHistogram(
-                "CustomTabs.SideSheetResizeType", resizeType, ResizeType.COUNT);
-
         mIsMaximized = !mIsMaximized;
         if (mIsMaximized) {
             if (shouldDrawDividerLine()) resetCoordinatorLayoutInsets();
