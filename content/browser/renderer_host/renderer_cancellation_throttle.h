3b1a8c8cad6d687f4e4436378faf361347dc4548
Rakina Zata Amni <rakina@chromium.org>
2024-07-26 09:21:25 +0000

    Reland "Add WillCommitWithoutUrlLoader handling to RendererCancellationThrottle"
    This is a reland of commit a0507e59a8fb68341fa08f3682be1544df52207e
    
    PS2 fixes the failing CreationNotDeferredWithoutURLLoader test.
    
    Original change's description:
    > Add WillCommitWithoutUrlLoader handling to RendererCancellationThrottle
    >
    > Currently it only handles WillProcessResponse, so it didn't capture
    > no-URLLoader navigations like about:blank. This CL makes it so that
    > it captures both navigations with and without URLLoader.
    >
    > Bug: 352352911
    > Change-Id: I0a75a4f80c04d7ec540ed74c63a07098797293c9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741319
    > Reviewed-by: Mingyu Lei <leimy@chromium.org>
    > Commit-Queue: Mingyu Lei <leimy@chromium.org>
    > Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1333343}
    
    Bug: 352352911
    Change-Id: I9dc788aff3b8b025a930fdd73e42606efdbde500
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740921
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333443}

--- content/browser/renderer_host/renderer_cancellation_throttle.h
+++ content/browser/renderer_host/renderer_cancellation_throttle.h
@@ -61,8 +61,10 @@ class CONTENT_EXPORT RendererCancellationThrottle : public NavigationThrottle {
 
  private:
   NavigationThrottle::ThrottleCheckResult WillProcessResponse() override;
+  NavigationThrottle::ThrottleCheckResult WillCommitWithoutUrlLoader() override;
   const char* GetNameForLogging() override;
 
+  NavigationThrottle::ThrottleCheckResult WaitForRendererCancellationIfNeeded();
   void OnTimeout();
   void RestartTimeout();
 
