6d17d2877a25f408081adcc5fc3e2395d3ab221d
Alvin Ji <alvinji@chromium.org>
2024-07-26 16:49:53 +0000

    Introduce SystemPermissionSettings in content setting model
    This change replaces direct usage of GeolocationSystemPermissionManager
    with the more general SystemPermissionSettings in content setting image
    and bubble models when interacting with geolocation system permissions.
    Unit tests have been updated to use a new MockSystemPermissionSettings
    class, ensuring continued correctness.
    
    Bug: 353315032
    Change-Id: I87f205518eb68d927455d2dd7577e540d91030b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703555
    Reviewed-by: Elias Klim <elklm@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333619}

--- chrome/browser/permissions/system/system_permission_settings.cc
+++ chrome/browser/permissions/system/system_permission_settings.cc
@@ -19,6 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace system_permission_settings {
 
 namespace {
+
+PlatformHandle* instance_for_testing_ = nullptr;
+
 std::map<ContentSettingsType, bool>& GlobalTestingBlockOverrides() {
   static std::map<ContentSettingsType, bool> g_testing_block_overrides;
   return g_testing_block_overrides;
@@ -26,6 +29,9 @@ std::map<ContentSettingsType, bool>& GlobalTestingBlockOverrides() {
 
 PlatformHandle* GetPlatformHandle() {
 #if !BUILDFLAG(IS_ANDROID)
+  if (instance_for_testing_) {
+    return instance_for_testing_;
+  }
   return CHECK_DEREF(CHECK_DEREF(g_browser_process).GetFeatures())
       .system_permissions_platform_handle();
 #else
@@ -34,6 +40,11 @@ PlatformHandle* GetPlatformHandle() {
 }
 
 }  // namespace
+
+// static
+void SetInstanceForTesting(PlatformHandle* instance_for_testing) {
+  instance_for_testing_ = instance_for_testing;
+}
 
 // static
 bool CanPrompt(ContentSettingsType type) {
