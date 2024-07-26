3d930d330f1367dc776baae1dd1c0528589190df
Dave Tapuska <dtapuska@chromium.org>
2024-07-26 18:44:17 +0000

    Remove dead code - WebMemoryStatistics
    I do not see this code used anywhere so remove it.
    
    Change-Id: I685750aba2af9a742b468d31c1fad94233f7e2d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739730
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333697}

--- content/renderer/render_thread_impl.h
+++ content/renderer/render_thread_impl.h
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/scheduler/web_thread_scheduler.h"
 #include "third_party/blink/public/platform/url_loader_throttle_provider.h"
 #include "third_party/blink/public/platform/web_connection_type.h"
-#include "third_party/blink/public/web/web_memory_statistics.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace blink {
