53e7864b72057b28ca323f614b69c0c381c4d56e
Abe Boujane <boujane@google.com>
2024-07-26 16:05:10 +0000

    [3PCD] Remove 3pcd from TP onboarding 😔
    Change-Id: I84925892d6a6f2d8bd76b475d6229711872c6c63
    Bug: 355002762
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735684
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333581}

--- components/privacy_sandbox/tracking_protection_prefs.cc
+++ components/privacy_sandbox/tracking_protection_prefs.cc
@@ -47,10 +47,6 @@ void RegisterProfilePrefs(PrefRegistrySimple* registry) {
   registry->RegisterTimePref(prefs::kTrackingProtectionSilentOnboardedSince,
                              base::Time());
 
-  // Full 3PCD prefs.
-  registry->RegisterTimePref(prefs::kTrackingProtectionFull3PCDOnboarded,
-                             base::Time());
-
   // Tracking Protection Reminder Prefs
   registry->RegisterIntegerPref(
       prefs::kTrackingProtectionReminderStatus,
