2f32568efbdf6e2c16a50326f3f51d2067efdece
Takuto Ikuta <tikuta@chromium.org>
2024-07-26 02:17:21 +0000

    chrome/browser/ui/views/side_panel: fix missing deps
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/build/mac-build-perf-ninja/2392/overview
    
    Bug: 40253918
    Change-Id: Ie226241a80d5739dc839d375ae8afb850419759e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5724208
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333340}

--- chrome/browser/ui/views/side_panel/bookmarks/bookmarks_side_panel_coordinator.cc
+++ chrome/browser/ui/views/side_panel/bookmarks/bookmarks_side_panel_coordinator.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/ui_features.h"
-#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_registry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_web_ui_view.h"
--- chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_ui.h
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
-#include "chrome/browser/search/background/ntp_custom_background_service.h"
 #include "chrome/browser/search/background/ntp_custom_background_service_observer.h"
 #include "chrome/browser/themes/theme_service.h"
 #include "chrome/browser/themes/theme_service_observer.h"
@@ -79,6 +78,7 @@ class GURL;
 class MostRelevantTabResumptionPageHandler;
 class MostVisitedHandler;
 class NewTabPageHandler;
+class NtpCustomBackgroundService;
 class PrefRegistrySimple;
 class PrefService;
 class Profile;
