e08a3b0715a812069c6cad227a0fb106cb0e4436
Christoph Schwering <schwering@google.com>
2024-07-26 07:22:09 +0000

    [Autofill] Delete OnAutofillManager{Destroyed,Reset}()
    The previous CL crrev.com/c/5734833 had only migrated (most) observers.
    This CL removes the events
    AutofillManager::Observer::OnAutofillManager{Destroyed,Reset}()
    themselves.
    
    Bug: 354043640
    Change-Id: Ieb20766a3dfb22028a012bad542c2789377ec027
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741817
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333399}

--- components/autofill/core/browser/autofill_manager.h
+++ components/autofill/core/browser/autofill_manager.h
@@ -127,9 +127,6 @@ class AutofillManager
                                                LifecycleState previous,
                                                LifecycleState current) {}
 
-    virtual void OnAutofillManagerDestroyed(AutofillManager& manager) {}
-    virtual void OnAutofillManagerReset(AutofillManager& manager) {}
-
     virtual void OnBeforeLanguageDetermined(AutofillManager& manager) {}
     virtual void OnAfterLanguageDetermined(AutofillManager& manager) {}
 
