7eeaa86519a249c7462c7cb30b5ee6f604d34bfa
Timofey Chudakov <tchudakov@google.com>
2024-07-26 13:29:26 +0000

    [Mullhet] Split kGrouped MatchedFormType.
    Before this CL, PasswordManager.PotentialBestMatchFormType didn't
    differentiate between grouped Android and other credentials.
    This CL splits the MatchedFormType::kGrouped bucket into 2
    separate buckets. The existing bucket gets deprecated.
    
    Bug: 332673987
    Change-Id: I461b9e752c7f10cd3bcabcb62276a27719234889
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735375
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333510}

--- components/password_manager/core/browser/form_fetcher.h
+++ components/password_manager/core/browser/form_fetcher.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/span.h"
 #include "base/observer_list_types.h"
 #include "components/password_manager/core/browser/password_form.h"
+#include "components/password_manager/core/browser/password_form_metrics_recorder.h"
 #include "components/password_manager/core/browser/password_store/password_store_util.h"
 #include "components/signin/public/base/gaia_id_hash.h"
 
@@ -99,6 +100,15 @@ class FormFetcher {
   // Pointer to a preferred entry in the vector returned by GetBestMatches().
   virtual const PasswordForm* GetPreferredMatch() const = 0;
 
+  // If prefferred match exists, returns its form type. Please note, that
+  // `FormFetcher` ignored grouped credentials by default. However, if any
+  // grouped credentials are available, this function will return the form type
+  // of the potential grouped credential. `GetPreferredMatch` will still return
+  // `nullptr` in this case.
+  // Returns `std::nullopt` if no credentials were available.
+  virtual std::optional<PasswordFormMetricsRecorder::MatchedFormType>
+  GetPreferredOrPotentialMatchedFormType() const = 0;
+
   // Creates a copy of |*this| with contains the same credentials without the
   // need for calling Fetch().
   virtual std::unique_ptr<FormFetcher> Clone() = 0;
@@ -112,9 +122,6 @@ class FormFetcher {
   // account store.
   virtual std::optional<PasswordStoreBackendError> GetAccountStoreBackendError()
       const = 0;
-
-  // Returns if grouped credentials were fetched but were ignored.
-  virtual bool WereGroupedCredentialsAvailable() const = 0;
 };
 
 }  // namespace password_manager
--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -1166,8 +1166,7 @@ void PasswordFormManager::FillNow() {
   if (parsed_observed_form_->HasPasswordElement() &&
       !parsed_observed_form_->IsSingleUsername()) {
     metrics_recorder_->RecordPotentialPreferredMatch(
-        form_fetcher_->GetPreferredMatch(),
-        form_fetcher_->WereGroupedCredentialsAvailable());
+        form_fetcher_->GetPreferredOrPotentialMatchedFormType());
   }
 
   SendFillInformationToRenderer(
