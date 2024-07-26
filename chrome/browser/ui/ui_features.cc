45f07099294d2b647c67a2ea092170eb39770eef
Nico Weber <thakis@chromium.org>
2024-07-26 17:34:46 +0000

    Revert "Add feature flag to enable the Views Task Manager on Mac"
    This reverts commit de9c755c98a74d2accd0749f0adeac4952c25243.
    
    Reason for revert: There are currently no plans for using the
    views task manager on mac, see
    https://issues.chromium.org/issues/41385547#comment15
    No work has been done on this in the 3 years since this flag
    was added. If we do want to do it in the future, it's easy
    to add back this flag.
    
    Original change's description:
    > Add feature flag to enable the Views Task Manager on Mac
    >
    > Views Task Manager Disabled:
    > https://screenshot.googleplex.com/BXQxheMWK99rq7k.png
    >
    > Views Task Manager Enabled:
    > https://screenshot.googleplex.com/6cVvgVEwPUbSxRk.png
    >
    > Bug: 836957, 1208546
    > Change-Id: Ie685226a6991cb8f5889fb4ce0f91a2d579f4d7d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/2900560
    > Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    > Reviewed-by: Elly Fong-Jones <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#883641}
    
    Bug: 836957, 1208546
    Change-Id: I7e8ef1e609acea230ce00e0844574c106a197643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738431
    Auto-Submit: Nico Weber <thakis@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333653}

--- chrome/browser/ui/ui_features.cc
+++ chrome/browser/ui/ui_features.cc
@@ -443,10 +443,6 @@ BASE_FEATURE(kViewsFirstRunDialog,
              "ViewsFirstRunDialog",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kViewsTaskManager,
-             "ViewsTaskManager",
-             base::FEATURE_DISABLED_BY_DEFAULT);
-
 BASE_FEATURE(kViewsJSAppModalDialog,
              "ViewsJSAppModalDialog",
              base::FEATURE_DISABLED_BY_DEFAULT);
