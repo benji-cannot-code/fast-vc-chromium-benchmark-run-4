49c52a754767c6b2284b3e9d5764d9df5bdcf71a
thefrog <thefrog@chromium.org>
2024-07-26 17:44:16 +0000

    Add regression test to confirm OHTTPKeyService created if HPRT disabled
    crrev.com/c/5445492 refactored the OHTTP key service factory code so it
    still creates the service even when HPRT lookups are disabled. It
    removed test case DisabledForRegularProfiles_HashRealTimeLookupsDisabled
    because that case was no longer relevant with the refactor. This CL adds
    it back but flips the EXPECT condition as a regression test case for the
    original issue.
    
    Bug: 355577214, 333491722
    Change-Id: I4a370198b4b57300346abdaad3435f858b14239f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738907
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333660}

--- chrome/browser/safe_browsing/ohttp_key_service_factory_unittest.cc
+++ chrome/browser/safe_browsing/ohttp_key_service_factory_unittest.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/testing_profile_manager.h"
+#include "components/safe_browsing/core/common/features.h"
+#include "components/safe_browsing/core/common/hashprefix_realtime/hash_realtime_utils.h"
 #include "content/public/test/browser_task_environment.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -19,6 +21,9 @@ class OhttpKeyServiceFactoryTest : public testing::Test {
   OhttpKeyServiceFactoryTest() = default;
   ~OhttpKeyServiceFactoryTest() override = default;
   void SetUp() override {
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{kHashPrefixRealTimeLookups},
+        /*disabled_features=*/{});
     profile_manager_ = std::make_unique<TestingProfileManager>(
         TestingBrowserProcess::GetGlobal());
     ASSERT_TRUE(profile_manager_->SetUp());
@@ -38,9 +43,11 @@ class OhttpKeyServiceFactoryTest : public testing::Test {
  protected:
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfileManager> profile_manager_;
+  base::test::ScopedFeatureList feature_list_;
 
  private:
   scoped_refptr<safe_browsing::SafeBrowsingService> sb_service_;
+  hash_realtime_utils::GoogleChromeBrandingPretenderForTesting apply_branding_;
   OhttpKeyServiceAllowerForTesting allow_ohttp_key_service_;
 };
 
@@ -52,6 +59,20 @@ TEST_F(OhttpKeyServiceFactoryTest, DisabledForRegularProfiles) {
 #else
 TEST_F(OhttpKeyServiceFactoryTest, EnabledForRegularProfiles) {
   TestingProfile* profile = profile_manager_->CreateTestingProfile("profile");
+  EXPECT_NE(nullptr, OhttpKeyServiceFactory::GetForProfile(profile));
+}
+
+// Regression test requested in crbug.com/355577214.
+TEST_F(OhttpKeyServiceFactoryTest,
+       EnabledForRegularProfiles_HashRealTimeLookupsDisabled) {
+  feature_list_.Reset();
+  feature_list_.InitWithFeatures(
+      /*enabled_features=*/{},
+      /*disabled_features=*/{kHashPrefixRealTimeLookups});
+  TestingProfile* profile = profile_manager_->CreateTestingProfile("profile");
+  // The service should still be created even though HPRT lookups are disabled.
+  // Instead, the OHTTP key service disables itself if HPRT lookups are
+  // disabled.
   EXPECT_NE(nullptr, OhttpKeyServiceFactory::GetForProfile(profile));
 }
 
