749ba0cc87424efd7eac0caa433eb62381c211dc
Orko Garai <orko@igalia.com>
2024-07-26 14:59:43 +0000

    [ozone/wayland] text-input-v3 Part 6: Handle preedit
    Add preedit handling in text-input-v3 wrapper and gnome workaround for
    erroneous preedit cursor end in WaylandInputMethodContext.
    
    Latest wayland governance meeting notes:
    https://gitlab.freedesktop.org/wayland/wayland-protocols/-/wikis/meetings#2024-07-02---text-input-32-2
    
    Design doc:
    https://docs.google.com/document/d/1GkOphcAQBMdW4iPiMOd9eKd70tlXWQaR7M3GJXGUDpQ/edit
    
    Bug: 40113488
    Change-Id: Id661d348eb3a17bf5164d1b658fa2d81e99d835c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5711648
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1333554}

--- ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
+++ ui/ozone/platform/wayland/host/zwp_text_input_wrapper.h
@@ -32,9 +32,14 @@ class ZWPTextInputWrapperClient {
  public:
   struct SpanStyle {
     struct Style {
+      bool operator==(const Style& other) const = default;
+
       ImeTextSpan::Type type;
       ImeTextSpan::Thickness thickness;
     };
+
+    bool operator==(const SpanStyle& other) const = default;
+
     // Byte offset.
     uint32_t index;
     // Length in bytes.
