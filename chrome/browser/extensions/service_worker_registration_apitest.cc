03268585fd1a8de5d6b759922362c7582acf253d
Justin Lulejian <jlulejian@chromium.org>
2024-07-26 01:46:52 +0000

    [Extensions] Revive/extend some histograms for worker registration.
    Extensions.ServiceWorkerBackground.RegistrationWhenExpected:
      Revived because it appears
      Extensions.ServiceWorkerBackground.RegistrationMismatchMitigated has
      a higher fail rate than expected.
    
    Extensions.ServiceWorkerBackground.RegistrationTime:
      Revived because slow registrations might contribute to slow responses
      to messages sent to a worker.
    
    Extensions.ServiceWorkerBackground.RegistrationMismatchMitigated:
      Extend because it appears this is happening more often than we
      thought.
    
    Extensions.ServiceWorkerBackground.RegistrationMismatchLocation:
      Ditto to the above.
    
    Testing: all metrics except RegistrationTime have a test so I've added
    it to an existing test for verification.
    
    Bug: 346732739
    Change-Id: I52a782220f48d689a2c9d1cd9944fec412c6af88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742293
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333329}

--- chrome/browser/extensions/service_worker_registration_apitest.cc
+++ chrome/browser/extensions/service_worker_registration_apitest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_apitest.h"
@@ -157,6 +158,7 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerRegistrationApiTest,
   extension_dir.WriteManifest(kManifest);
   extension_dir.WriteFile(FILE_PATH_LITERAL("background.js"), kBackground);
 
+  base::HistogramTester histogram_tester;
   const Extension* extension = LoadExtension(
       extension_dir.UnpackedPath(), {.wait_for_registration_stored = true});
   ASSERT_TRUE(extension);
@@ -170,6 +172,15 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerRegistrationApiTest,
   EXPECT_EQ("0.1", stored_version.GetString());
   EXPECT_EQ(content::ServiceWorkerCapability::SERVICE_WORKER_NO_FETCH_HANDLER,
             GetServiceWorkerRegistrationState(*extension));
+
+  histogram_tester.ExpectTotalCount(
+      "Extensions.ServiceWorkerBackground.RegistrationTime",
+      /*expected_count=*/1);
+  // Confirm the time is a sane value (less than the maximum value).
+  histogram_tester.ExpectBucketCount(
+      "Extensions.ServiceWorkerBackground.RegistrationTime",
+      /*sample=*/base::Seconds(10).InMilliseconds(),
+      /*expected_count=*/0);
 }
 
 // Tests that updating an unpacked extension properly updates the extension's
