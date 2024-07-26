b184858e8338d58b266397e087f99f3b4732c0f5
Takumi Fujimoto <takumif@chromium.org>
2024-07-26 00:52:50 +0000

    Resolve the keyboard lock promise even without focus
    When keyboard lock is approved in a permission prompt, the prompt
    rather than the page has focus. This CL allows the keyboard lock
    promise to be resolved without focus, then later engage the lock when
    the page gains focus.
    
    Bug: 355329593
    Change-Id: I071733e114cb94507c43194fe839024beb27d9f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740693
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1333308}

--- chrome/browser/ui/exclusive_access/keyboard_lock_controller.h
+++ chrome/browser/ui/exclusive_access/keyboard_lock_controller.h
@@ -81,6 +81,10 @@ class KeyboardLockController : public ExclusiveAccessControllerBase {
   // Notifies the exclusive access tab that it must deactivate keyboard lock.
   void UnlockKeyboard();
 
+  // Notifies `web_contents` that it must deactivate keyboard lock.
+  void UnlockKeyboardForWebContents(
+      base::WeakPtr<content::WebContents> web_contents);
+
   // Called when the user has held down Escape.
   void HandleUserHeldEscapeDeprecated();
 
@@ -106,4 +110,4 @@ class KeyboardLockController : public ExclusiveAccessControllerBase {
   base::WeakPtrFactory<KeyboardLockController> weak_ptr_factory_{this};
 };
 
-#endif  //  CHROME_BROWSER_UI_EXCLUSIVE_ACCESS_KEYBOARD_LOCK_CONTROLLER_H_
+#endif  // CHROME_BROWSER_UI_EXCLUSIVE_ACCESS_KEYBOARD_LOCK_CONTROLLER_H_
