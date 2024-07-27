6b8b644f5cdb9d7759ad238b98aeaedfb0106106
S. Ganesh <ganesh@chromium.org>
2024-07-27 00:41:26 +0000

    updater: clone 'brand' values to registry.
    Some apps rely on the `brand` code being written to the registry in
    `ClientState`.
    
    Fixed: 355693627
    Change-Id: Ib4d168071115758f0c5f0986d24d6e312ad08873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744596
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333849}

--- chrome/updater/persisted_data.cc
+++ chrome/updater/persisted_data.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
 
-#include "base/strings/sys_string_conversions.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/win_constants.h"
 #endif
@@ -93,8 +92,8 @@ void PersistedData::SetProductVersion(const std::string& id,
   // creating the ClientState key, which is read to sense for application
   // uninstallation.
   SetRegistryKey(UpdaterScopeToHKeyRoot(scope_),
-                 GetAppClientStateKey(base::SysUTF8ToWide(id)), L"pv",
-                 base::SysUTF8ToWide(pv.GetString()));
+                 GetAppClientStateKey(base::UTF8ToWide(id)), kRegValuePV,
+                 base::UTF8ToWide(pv.GetString()));
 #endif
 }
 
@@ -166,6 +165,14 @@ std::string PersistedData::GetBrandCode(const std::string& id) const {
 void PersistedData::SetBrandCode(const std::string& id, const std::string& bc) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   SetString(id, kBC, bc);
+
+#if BUILDFLAG(IS_WIN)
+  // For backwards compatibility, we record the brand code in ClientState as
+  // well, since some applications read it from there.
+  SetRegistryKey(UpdaterScopeToHKeyRoot(scope_),
+                 GetAppClientStateKey(base::UTF8ToWide(id)), kRegValueBrandCode,
+                 base::UTF8ToWide(bc));
+#endif
 }
 
 base::FilePath PersistedData::GetBrandPath(const std::string& id) const {
@@ -210,8 +217,8 @@ void PersistedData::SetAP(const std::string& id, const std::string& ap) {
   // registry is correct. Clients should transition to requesting the
   // registration info for their application via RPC.
   SetRegistryKey(UpdaterScopeToHKeyRoot(scope_),
-                 GetAppClientStateKey(base::SysUTF8ToWide(id)), L"ap",
-                 base::SysUTF8ToWide(ap));
+                 GetAppClientStateKey(base::UTF8ToWide(id)), kRegValueAP,
+                 base::UTF8ToWide(ap));
 #endif
 }
 
@@ -280,8 +287,8 @@ void PersistedData::SetCohort(const std::string& id,
   // For backwards compatibility, we record the Cohort in ClientState as well.
   // (Some applications read it from there.)
   SetRegistryKey(UpdaterScopeToHKeyRoot(scope_),
-                 GetAppCohortKey(base::SysUTF8ToWide(id)), L"",
-                 base::SysUTF8ToWide(cohort));
+                 GetAppCohortKey(base::UTF8ToWide(id)), L"",
+                 base::UTF8ToWide(cohort));
 #endif
 }
 
@@ -299,8 +306,8 @@ void PersistedData::SetCohortName(const std::string& id,
   // For backwards compatibility, we record the Cohort in ClientState as well.
   // (Some applications read it from there.)
   SetRegistryKey(UpdaterScopeToHKeyRoot(scope_),
-                 GetAppCohortKey(base::SysUTF8ToWide(id)), kRegValueCohortName,
-                 base::SysUTF8ToWide(cohort_name));
+                 GetAppCohortKey(base::UTF8ToWide(id)), kRegValueCohortName,
+                 base::UTF8ToWide(cohort_name));
 #endif
 }
 
