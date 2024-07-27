16d4cc3b1d38618e9eeddc0542146daf9161556c
Sergey Poromov <poromov@chromium.org>
2024-07-27 13:13:57 +0000

    [skyvault] Show notification while uploading screencaptures to OneDrive.
    When ScreenCapture file should be uploaded to OneDrive as a part
    of "finalize" step, we need to show a notification with an upload
    progress bar and ability to cancel the upload.
    The notification is kept alive until the result callback for the
    uploader will be called.
    
    Bug: b/348177318
    Change-Id: I96e22586800fdd97d6d1c300c541ab26410bbcd0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741268
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333925}

--- chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.cc
+++ chrome/browser/ui/ash/capture_mode/chrome_capture_mode_delegate.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/dlp/dlp_content_manager_ash.h"
 #include "chrome/browser/ash/policy/skyvault/file_location_utils.h"
 #include "chrome/browser/ash/policy/skyvault/odfs_skyvault_uploader.h"
+#include "chrome/browser/ash/policy/skyvault/skyvault_capture_upload_notification.h"
 #include "chrome/browser/ash/video_conference/video_conference_manager_ash.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/download/download_prefs.h"
@@ -71,6 +72,8 @@ bool IsScreenCaptureDisabledByPolicy() {
 void CaptureFileFinalized(
     const base::FilePath& original_path,
     base::OnceCallback<void(bool, const base::FilePath&)> callback,
+    std::unique_ptr<policy::skyvault::SkyvaultCaptureUploadNotification>
+        upload_notification,
     bool success,
     storage::FileSystemURL file_url) {
   std::move(callback).Run(success, file_url.path());
@@ -385,12 +388,23 @@ void ChromeCaptureModeDelegate::FinalizeSavedFile(
   auto* profile = ProfileManager::GetActiveUserProfile();
   if (!odfs_temp_dir_.GetPath().empty() &&
       odfs_temp_dir_.GetPath().IsParent(path) && profile) {
-    // TODO(b/348177318): Show notification with progress during upload.
-    ash::cloud_upload::OdfsSkyvaultUploader::Upload(
+    // Passing the notification to the callback so that it's destructed once
+    // file upload finishes.
+    auto notification =
+        std::make_unique<policy::skyvault::SkyvaultCaptureUploadNotification>(
+            path);
+    auto notification_ptr = notification.get();
+    auto uploader = ash::cloud_upload::OdfsSkyvaultUploader::Upload(
         profile, path,
         ash::cloud_upload::OdfsSkyvaultUploader::FileType::kScreenCapture,
-        /*progress_callback=*/base::DoNothing(),
-        base::BindOnce(&CaptureFileFinalized, path, std::move(callback)));
+        base::BindRepeating(
+            &policy::skyvault::SkyvaultCaptureUploadNotification::
+                UpdateProgress,
+            notification->GetWeakPtr()),
+        base::BindOnce(&CaptureFileFinalized, path, std::move(callback),
+                       std::move(notification)));
+    notification_ptr->SetCancelClosure(base::BindOnce(
+        &ash::cloud_upload::OdfsSkyvaultUploader::Cancel, uploader));
     return;
   }
   std::move(callback).Run(/*success=*/true, path);
