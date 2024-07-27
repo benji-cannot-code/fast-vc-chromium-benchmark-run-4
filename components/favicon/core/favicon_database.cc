4b8766b98f3aaaae5f7abdcc68a3bfe7818bac22
Caleb Raitto <caraitto@chromium.org>
2024-07-27 05:31:55 +0000

    [Interest Group] Report DB upgrade and InitializeSchema() status to UMA
    Bug: 355010821
    Change-Id: Iab108f7f37856965e458c44542c0fddd15542bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736253
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333887}

--- components/favicon/core/favicon_database.cc
+++ components/favicon/core/favicon_database.cc
@@ -1018,9 +1018,9 @@ sql::InitStatus FaviconDatabase::InitImpl(const base::FilePath& db_name) {
 
   // Clear databases which are too old to process.
   DCHECK_LT(kDeprecatedVersionNumber, kCurrentVersionNumber);
-  if (!sql::MetaTable::RazeIfIncompatible(
+  if (sql::MetaTable::RazeIfIncompatible(
           &db_, /*lowest_supported_version=*/kDeprecatedVersionNumber + 1,
-          kCurrentVersionNumber)) {
+          kCurrentVersionNumber) == sql::RazeIfIncompatibleResult::kFailed) {
     return sql::INIT_FAILURE;
   }
 
