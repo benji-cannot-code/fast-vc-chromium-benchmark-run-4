22aae32d194a35fae5ed595b35476a5f7689cbc2
David Bertoni <dbertoni@chromium.org>
2024-07-27 00:45:21 +0000

    [Extensions] Create a BUILD.gn file for the contextMenus API.
    Bug: 40245282, 40593486
    Change-Id: I8541c9b8dfb906d4eb7b2be679c67582b2db8e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744029
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1333851}

--- chrome/browser/extensions/api/web_view/chrome_web_view_internal_api.cc
+++ chrome/browser/extensions/api/web_view/chrome_web_view_internal_api.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/strings/string_util.h"
-#include "chrome/browser/extensions/api/context_menus/context_menus_api.h"
 #include "chrome/browser/extensions/context_menu_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/chrome_web_view_internal.h"
