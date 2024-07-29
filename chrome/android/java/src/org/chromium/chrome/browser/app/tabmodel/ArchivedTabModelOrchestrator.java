f8c9a032a9860899da91cad3ae56790f3a9f2e1e
Brandon Wylie <wylieb@chromium.org>
2024-07-29 18:08:02 +0000

    [declutter] Manually call #saveState after archiving
    Archived tabs sometimes disappears on re-launch. Adding logging, and
    it seems that #saveState isn't always called when the application is
    torn down. This is likely caused by the tabs not being considered as
    "dirty", and there not being enough time during shutdown to actually
    save both of the tab models. Note: This is observed mostly when force
    quitting the app.
    
    Manually saving the tab model orchestrator when an archive pass happens.
    
    Bug: 355723422
    Change-Id: Ic1ba810fc1da31c53274facdd0b2efc736cb3001
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744533
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1334331}

--- chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestrator.java
@@ -342,6 +342,7 @@ public class ArchivedTabModelOrchestrator extends TabModelOrchestrator implement
         mTaskRunner.postDelayedTask(
                 mCallbackController.makeCancelable(this::runDeclutterAndScheduleNext),
                 TimeUnit.HOURS.toMillis(mTabArchiveSettings.getDeclutterIntervalTimeDeltaHours()));
+        saveState();
     }
 
     // Testing-specific methods
