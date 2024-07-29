5ede3c77e25389b78aae43180ac66789ffecee78
Adam Walls <avvall@google.com>
2024-07-29 21:56:15 +0000

    [WebView Shell] Make WebView Shell able to use net_java library
    In order to be able to import the net_java library into the WebView
    shell APK, a few minSDK errors need to be rectified so that the minSDK
    of 21 does not conflict with the minSDK of a couple of classes in the
    net_java library, which require minSDK of 23 & 24 or greater
    (Android M and N).
    See crrev/c/5736379 for the relevant findings.
    
    Bug: b:355497433
    Test: See linked bug for test details
    Change-Id: Ife742202d733e9037935cc721921c5a6cc5368d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741944
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Walls <avvall@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334480}

--- net/android/java/src/org/chromium/net/ProxyChangeListener.java
+++ net/android/java/src/org/chromium/net/ProxyChangeListener.java
@@ -175,6 +175,7 @@ public class ProxyChangeListener {
     // the Android SDK, so we have to use reflection to get at it and invoke
     // methods on it. If we fail, return an empty proxy config (meaning
     // use system properties).
+    @SuppressWarnings({"PrivateApi", "ObsoleteSdkInt"})
     private static ProxyConfig extractNewProxy(Intent intent) {
         Bundle extras = intent.getExtras();
         if (extras == null) {
@@ -294,6 +295,7 @@ public class ProxyChangeListener {
         return configFromConnectivityManager;
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     /* package */ void updateProxyConfigFromConnectivityManager(Intent intent) {
         runOnThread(() -> proxySettingsChanged(getProxyConfig(intent)));
     }
