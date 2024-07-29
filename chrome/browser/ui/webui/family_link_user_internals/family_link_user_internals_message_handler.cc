9fa525b971ed7dc55fe9a785778502552972ac00
Nohemi Fernandez <fernandex@chromium.org>
2024-07-29 17:11:04 +0000

    Remove reference to "Child" property in family link internals.
    Bug: 355385647
    Change-Id: I4caa596c910513de992a1a26c2119591a423da6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5746756
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Auto-Submit: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334300}

--- chrome/browser/ui/webui/family_link_user_internals/family_link_user_internals_message_handler.cc
+++ chrome/browser/ui/webui/family_link_user_internals/family_link_user_internals_message_handler.cc
@@ -194,7 +194,6 @@ void FamilyLinkUserInternalsMessageHandler::SendBasicInfo() {
 
   base::Value::List* section_profile = AddSection(&section_list, "Profile");
   AddSectionEntry(section_profile, "Account", profile->GetProfileUserName());
-  AddSectionEntry(section_profile, "Child", profile->IsChild());
 
   supervised_user::SupervisedUserURLFilter* filter =
       GetSupervisedUserService()->GetURLFilter();
