69c5888d4a4e5bacc2a81fe22a460e3c2a132560
Patrick Monette <pmonette@chromium.org>
2024-07-29 18:42:28 +0000

    [PM] Add a HadUserActivation property on frame nodes
    Change-Id: I5d6c392ab8620ebc152f862b78be9ced38f95fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742425
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334352}

--- components/performance_manager/graph/frame_node_impl.h
+++ components/performance_manager/graph/frame_node_impl.h
@@ -100,6 +100,7 @@ class FrameNodeImpl
   bool IsAdFrame() const override;
   bool IsHoldingWebLock() const override;
   bool IsHoldingIndexedDBLock() const override;
+  bool HadUserActivation() const override;
   bool HadFormInteraction() const override;
   bool HadUserEdits() const override;
   bool IsAudible() const override;
@@ -126,6 +127,7 @@ class FrameNodeImpl
 
   // Setters are not thread safe.
   void SetIsCurrent(bool is_current);
+  void SetHadUserActivation();
   void SetIsHoldingWebLock(bool is_holding_weblock);
   void SetIsHoldingIndexedDBLock(bool is_holding_indexeddb_lock);
   void SetIsAudible(bool is_audible);
@@ -295,6 +297,12 @@ class FrameNodeImpl
       LifecycleState,
       &FrameNodeObserver::OnFrameLifecycleStateChanged>
       lifecycle_state_{LifecycleState::kRunning};
+
+  // Indicates if the frame has been interacted with.
+  ObservedProperty::NotifiesOnlyOnChanges<
+      bool,
+      &FrameNodeObserver::OnHadUserActivationChanged>
+      had_user_activation_{false};
 
   ObservedProperty::
       NotifiesOnlyOnChanges<bool, &FrameNodeObserver::OnIsAdFrameChanged>
