19eabeb0c3933545d2352a4a636efbcd56bc55a9
Ashish Kumar <ashishkum@microsoft.com>
2024-07-29 06:59:29 +0000

    [views-ax] Migrates kDefaultActionVerb attribute to new system for textfield
    This CL migrates the kDefaultActionVerb value in
    ui/views/controls/textfield/textfield.cc whenever its value should
    change, rather than querying the value and computing it only when
    needed.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ie7adea6d3ed0319847d299809e7f039e86eda5df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5700868
    Commit-Queue: Ashish Kumar <ashishkum@microsoft.com>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1334104}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -269,6 +269,7 @@ Textfield::Textfield()
   // accessibility trees of all the platforms we support.
   GetViewAccessibility().SetIsLeaf(true);
   UpdateAccessibilityTextDirection();
+  UpdateAccessibleDefaultActionVerb();
 }
 
 Textfield::~Textfield() {
@@ -1051,9 +1052,6 @@ void Textfield::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   // Editable state indicates support of editable interface, and is always set
   // for a textfield, even if disabled or readonly.
   node_data->AddState(ax::mojom::State::kEditable);
-  if (GetEnabled()) {
-    node_data->SetDefaultActionVerb(ax::mojom::DefaultActionVerb::kActivate);
-  }
   if (text_input_type_ == ui::TEXT_INPUT_TYPE_PASSWORD) {
     node_data->SetValue(std::u16string(
         GetText().size(), gfx::RenderText::kPasswordReplacementChar));
@@ -3028,6 +3026,7 @@ void Textfield::OnEnabledChanged() {
   if (GetEnabled() && GetReadOnly()) {
     GetViewAccessibility().SetReadOnly(true);
   }
+  UpdateAccessibleDefaultActionVerb();
 }
 
 void Textfield::DropDraggedText(
@@ -3233,6 +3232,15 @@ void Textfield::StopSelectionDragging() {
   }
   selection_dragging_state_ = SelectionDraggingState::kNone;
   selection_drag_type_ = std::nullopt;
+}
+
+void Textfield::UpdateAccessibleDefaultActionVerb() {
+  if (GetEnabled()) {
+    GetViewAccessibility().SetDefaultActionVerb(
+        ax::mojom::DefaultActionVerb::kActivate);
+  } else {
+    GetViewAccessibility().RemoveDefaultActionVerb();
+  }
 }
 
 BEGIN_METADATA(Textfield)
