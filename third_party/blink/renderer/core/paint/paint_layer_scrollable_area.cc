a3baf54cc30f224974c049cd66a64f842c22034c
Xianzhu Wang <wangxianzhu@chromium.org>
2024-07-29 23:50:07 +0000

    Exclude kPopupNoThreadedInput from main-thread-scrolling-reasons on scroll nodes
    The reason is never used on scroll nodes. The actual usage is directly
    from WebPagePopupImpl [1]. The change is behind a killswitch
    ExcludePopupMainThreadScrollReason. When we remove the killswitch, we
    can rename main_thread_scrolling_reasons on scroll nodes to
    main_thread_repaint_reasons.
    
    This also help debug crbug.com/356075020 to see if the corrupted reasons
    come from blink.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/exported/web_page_popup_impl.cc;drc=8b829f444f9653642e03c0be30670c17f100a231;l=759
    
    Bug: 356075020
    Change-Id: I1e030d1154af8aa61739ff90335e3f9723f9a94b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5745837
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334537}

--- third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
+++ third_party/blink/renderer/core/paint/paint_layer_scrollable_area.cc
@@ -2591,6 +2591,13 @@ bool PaintLayerScrollableArea::ShouldScrollOnMainThread() const {
     return true;
   }
 
+  if (RuntimeEnabledFeatures::ExcludePopupMainThreadScrollingReasonEnabled() &&
+      !GetLayoutBox()->GetFrame()->Client()->GetWebFrame()) {
+    // If there's no WebFrame, then there's no WebFrameWidget, and we can't do
+    // threaded scrolling. This currently only happens in a WebPagePopup.
+    return true;
+  }
+
   if (const auto* paint_artifact_compositor =
           GetLayoutBox()->GetFrameView()->GetPaintArtifactCompositor()) {
     if (const auto* properties =
