cdb64f407b208fdaf0c90fd189017215f027a5fe
Nate Fischer <ntfschr@chromium.org>
2024-07-29 17:11:33 +0000

    AW: ensure URLLoaderThrottle is always used on UI thread
    This URLLoaderThrottle is a single-threaded class which is only safe to
    call on the UI thread. This is generally upheld by code which calls
    URLLoaderThrottle override methods, however there was one private method
    which was being called from Java. It's possible that the Java
    implementation code could call this on the Main Looper instead of the
    Chromium UI thread, so we can solve this by posting to the UI thread
    instead.
    
    This also introduces a utility class (BackgroundThreadExecutor) which
    I'm using for testing, however I intend to also use the same class in
    the production code in a future change.
    
    Fixed: 355541498
    Test: run_webview_instrumentation_test_apk -f AwSupervisedUserTest.*
    Change-Id: I8bc13fff95c337893cd4bebb060c8b9ccf3d70ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742930
    Reviewed-by: Aashna Jena <aashnajena@google.com>
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Aashna Jena <aashnajena@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334301}

---
+++ android_webview/java/src/org/chromium/android_webview/common/BackgroundThreadExecutor.java
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.common;
+
+import android.os.Handler;
+import android.os.HandlerThread;
+
+import java.util.concurrent.Executor;
+
+/** A helper class which creates a background thread to run tasks on. */
+public class BackgroundThreadExecutor implements Executor {
+    private final HandlerThread mHandlerThread;
+    private final Handler mHandler;
+
+    public BackgroundThreadExecutor(String threadName) {
+        mHandlerThread = new HandlerThread(threadName);
+        mHandlerThread.start();
+        mHandler = new Handler(mHandlerThread.getLooper());
+    }
+
+    @Override
+    public void execute(Runnable r) {
+        mHandler.post(r);
+    }
+}
