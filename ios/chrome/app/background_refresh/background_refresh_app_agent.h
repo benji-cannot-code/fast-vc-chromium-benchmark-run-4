ae2fc2eff7233d65ea5db881578429adb830635f
Federica Germinario <fedegermi@google.com>
2024-07-29 09:55:25 +0000

    [ios] Fix flag issue in BackgroundRefreshAppAgent
    Old implementation was not working because when
    BackgroundRefreshAppAgent is initialized:
    IsAppBackgroundRefreshEnabled()
    will return false even if the flag is enabled, to fix this issue
    background refresh request was moved at initStage:
    InitStageBrowserObjectsForBackgroundHandlers
    where is working as expected.
    
    Bug: none
    Change-Id: Ia4995347a31bc1b5183ebe7281cd0057fc56677d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741638
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334148}

--- ios/chrome/app/background_refresh/background_refresh_app_agent.h
+++ ios/chrome/app/background_refresh/background_refresh_app_agent.h
@@ -8,9 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
-@class AppRefreshProvider;
-
 #import "ios/chrome/app/application_delegate/observing_app_state_agent.h"
+
+@class AppRefreshProvider;
 
 // An app agent that manages background refresh tasks.
 // DEBUGGING/TESTING note: App refresh does not work on simulators; you must
--- ios/chrome/app/background_refresh/background_refresh_app_agent.mm
+++ ios/chrome/app/background_refresh/background_refresh_app_agent.mm
@@ -38,10 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)registerBackgroundRefreshTask {
-  if (!IsAppBackgroundRefreshEnabled()) {
-    return;
-  }
-
   auto handler = ^(BGTask* task) {
     [self systemTriggeredRefreshForTask:task];
   };
@@ -55,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)requestAppRefreshWithDelay:(NSTimeInterval)delay {
+  // Schedule requests only if flag is enabled.
   if (!IsAppBackgroundRefreshEnabled()) {
     return;
   }
