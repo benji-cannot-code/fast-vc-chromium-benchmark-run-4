68273c32d432fe728d9c47da8bee23a4ad75cf68
Sophia Tran <sophiatran@google.com>
2024-07-29 21:54:57 +0000

    Created method to detect when Reading Mode panel is fully scrolled.
    When the element holding the text in the Reading Mode side panel is
    fully scrolled, it calls an empty method in the app controller.
    This method will later be implemented to trigger an event where
    the main document automatically scrolls.
    
    Bug: 353590969
    Change-Id: I17a739b254f942ba0b9715c4e6e243aaed7d9ecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5715177
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Commit-Queue: Sophia Tran <sophiatran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334478}

--- chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
+++ chrome/browser/resources/side_panel/read_anything/read_anything.d.ts
@@ -258,6 +258,9 @@ declare namespace chrome {
     // SidePanelWebUIView::ShowUI
     function shouldShowUi(): boolean;
 
+    // Called when the Read Anything panel is scrolled all the way down.
+    function onScrolledToBottom(): void;
+
     ////////////////////////////////////////////////////////////////
     // Implemented in read_anything/app.ts and called by native c++.
     ////////////////////////////////////////////////////////////////
--- chrome/renderer/accessibility/read_anything_app_controller.cc
+++ chrome/renderer/accessibility/read_anything_app_controller.cc
@@ -942,7 +942,9 @@ gin::ObjectTemplateBuilder ReadAnythingAppController::GetObjectTemplateBuilder(
       .SetMethod("getNextWordHighlightLength",
                  &ReadAnythingAppController::GetNextWordHighlightLength)
       .SetMethod("getValidatedFontName",
-                 &ReadAnythingAppController::GetValidatedFontName);
+                 &ReadAnythingAppController::GetValidatedFontName)
+      .SetMethod("onScrolledToBottom",
+                 &ReadAnythingAppController::OnScrolledToBottom);
 }
 
 ui::AXNodeID ReadAnythingAppController::RootId() const {
@@ -1852,4 +1854,8 @@ int ReadAnythingAppController::GetNextWordHighlightLength(int index) {
 void ReadAnythingAppController::IncrementMetricCount(
     const std::string& metric) {
   read_aloud_model_.IncrementMetric(metric);
+}
+
+void ReadAnythingAppController::OnScrolledToBottom() {
+  // TODO (b/353590969): trigger an accessibility action to refresh/scroll more
 }
