2e16ed279ef2a63409d718c6c14858f1d9cec394
Mason Freed <masonf@chromium.org>
2024-07-29 21:38:49 +0000

    Add fieldtrial testing config for ElementGetInnerHTML disabled
    Approved to remove:
    
    https://groups.google.com/a/chromium.org/g/blink-dev/c/vMqW3pfItl0/m/MgmQ9Q3GCAAJ
    
    Bug: 41492947
    Change-Id: I52bbce32c5d0cf7815b3f8d6ef6af97d49a06373
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744034
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334468}

--- third_party/blink/web_tests/http/tests/inspector-protocol/issues/getinnerhtml-issue.js
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/getinnerhtml-issue.js
@@ -8,7 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   const promise = dp.Audits.onceIssueAdded();
   session.evaluate('document.body.getInnerHTML()');
 
-  const result = await promise;
-  testRunner.log(result.params, 'Inspector issue: ');
+  let noIssue = false;
+  const timeout = new Promise(resolve => setTimeout(() => {noIssue=true; resolve();},500));
+  const result = await Promise.any([promise, timeout]);
+  if (noIssue) {
+    testRunner.log('No issue');
+  } else {
+    testRunner.log(result.params, 'Inspector issue: ');
+  }
   testRunner.completeTest();
 })
