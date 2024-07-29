2d0b47735f8193cb8e69a5d397fc4d0fdfc5796e
Marijn Kruisselbrink <mek@chromium.org>
2024-07-29 23:33:14 +0000

    Enable link capturing reimpl in link capturing browser tests
    Make sure these tests are running with the correct base::Feature
    and parameters (which at the moment doesn't make a difference yet
    since nothing checks for the reimpl parameter yet).
    
    Bug: 351775835
    Change-Id: Id847509f554b162d0e8a73727d5eb25da8a47f2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744416
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334524}

--- chrome/browser/webapps/web_app_link_capturing_parameterized_browsertest.cc
+++ chrome/browser/webapps/web_app_link_capturing_parameterized_browsertest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <ostream>
+
 #include "base/base_paths.h"
 #include "base/files/file_util.h"
 #include "base/json/json_file_value_serializer.h"
@@ -11,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/threading/scoped_blocking_call.h"
 #include "base/types/expected.h"
 #include "chrome/browser/apps/app_service/app_registry_cache_waiter.h"
@@ -23,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/services/app_service/public/cpp/app_launch_util.h"
@@ -199,6 +203,13 @@ std::string_view ToParamString(OpenerMode opener) {
   }
 }
 
+template <typename ParamType, class Char, class Traits>
+std::basic_ostream<Char, Traits>& operator<<(
+    std::basic_ostream<Char, Traits>& os,
+    ParamType param) {
+  return os << ToParamString(param);
+}
+
 // The target to supply for the navigation:
 enum class NavigationTarget {
   // The target to supply for the navigation:
@@ -298,6 +309,10 @@ class WebAppLinkCapturingParameterizedBrowserTest
       public testing::WithParamInterface<LinkCaptureTestParam> {
  public:
   WebAppLinkCapturingParameterizedBrowserTest() {
+    std::map<std::string, std::string> parameters;
+    parameters["link_capturing_state"] = "reimpl_default_on";
+    scoped_feature_list_.InitAndEnableFeatureWithParameters(
+        features::kDesktopPWAsLinkCapturing, parameters);
     InitializeTestExpectations();
   }
 
@@ -658,6 +673,8 @@ class WebAppLinkCapturingParameterizedBrowserTest
     ASSERT_TRUE(test_expectations_) << "Unable to read test expectation file";
     ASSERT_TRUE(test_expectations_.value().is_dict());
   }
+
+  base::test::ScopedFeatureList scoped_feature_list_;
 
   // The path to the json file containing the test expectations.
   base::FilePath json_file_path_ =
