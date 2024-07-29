e3429644714abc9fcc1acddd3a9445100c816b8c
Curtis McMullan <curtismcmullan@chromium.org>
2024-07-29 03:11:33 +0000

    editor: use correct i18n flags for server call
    The client for the server is inspecting the incorrect flags for i18n.
    This CL fixes the issue by checking the correct flags.
    
    BUG=b:349679801
    
    Change-Id: I2affbd2fc14fa988f53c63a94f7dc6456a250951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737071
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334076}

--- chrome/browser/ash/input_method/editor_text_query_provider.cc
+++ chrome/browser/ash/input_method/editor_text_query_provider.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "ash/constants/ash_features.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/notreached.h"
 #include "base/strings/strcat.h"
@@ -25,6 +26,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::input_method {
 
 namespace {
+
+bool IsInternationalizeEnabled() {
+  return base::FeatureList::IsEnabled(features::kOrcaDanish) ||
+         base::FeatureList::IsEnabled(features::kOrcaDutch) ||
+         base::FeatureList::IsEnabled(features::kOrcaFinnish) ||
+         base::FeatureList::IsEnabled(features::kOrcaFrench) ||
+         base::FeatureList::IsEnabled(features::kOrcaGerman) ||
+         base::FeatureList::IsEnabled(features::kOrcaItalian) ||
+         base::FeatureList::IsEnabled(features::kOrcaJapanese) ||
+         base::FeatureList::IsEnabled(features::kOrcaNorwegian) ||
+         base::FeatureList::IsEnabled(features::kOrcaPortugese) ||
+         base::FeatureList::IsEnabled(features::kOrcaSpanish) ||
+         base::FeatureList::IsEnabled(features::kOrcaSwedish);
+}
 
 std::string GetConfigLabelFromFieldTrialConfig() {
   return base::GetFieldTrialParamValue("OrcaEnabled", "config_label");
@@ -56,7 +71,7 @@ std::map<std::string, std::string> CreateProviderRequest(
     provider_request["config_label"] = config_label;
   }
 
-  if (base::FeatureList::IsEnabled(chromeos::features::kOrcaInternationalize)) {
+  if (IsInternationalizeEnabled()) {
     provider_request["i18n"] = "true";
   }
 
