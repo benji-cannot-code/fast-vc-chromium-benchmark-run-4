7f49933b5509ea5a25097c6cfc7681c9d39f2760
Gabriel Viera <gviera@google.com>
2024-07-29 16:18:38 +0000

    [Webauthn] Add Passkey Deleted Bubble UI for Report API
    If a call to navigator.credentials.report() that includes
    unknownCredentialId successfully deletes a passkey from the Google
    Password Manager (GPM), a bubble will appear to notify the user that an
    invalid passkey was deleted and that they need to sign in to create
    a new one.
    
    https://screenshot.googleplex.com/VcaTs3d4mSFTdVf.png
    
    Bug: 347727501
    Change-Id: Ibafaa36d17b94bb53176fc1dfce9f9d290cbdab6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738785
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Gabriel Viera <gviera@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334269}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/passwords/passwords_client_ui_delegate.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
 #include "chrome/browser/webauthn/cablev2_devices.h"
 #include "chrome/browser/webauthn/enclave_manager.h"
@@ -615,12 +616,12 @@ ChromeWebAuthenticationDelegate::MaybeGetRequestProxy(
 }
 
 void ChromeWebAuthenticationDelegate::DeletePasskey(
-    content::BrowserContext* browser_context,
+    content::WebContents* web_contents,
     const std::vector<uint8_t>& passkey_credential_id,
     const std::string& relying_party_id) {
   webauthn::PasskeyModel* passkey_store =
       PasskeyModelFactory::GetInstance()->GetForProfile(
-          Profile::FromBrowserContext(browser_context));
+          Profile::FromBrowserContext(web_contents->GetBrowserContext()));
 
   CHECK(passkey_store);
   std::string credential_id(passkey_credential_id.begin(),
@@ -629,6 +630,11 @@ void ChromeWebAuthenticationDelegate::DeletePasskey(
       passkey_store->GetPasskeyByCredentialId(relying_party_id, credential_id);
   if (credential_specifics) {
     passkey_store->DeletePasskey(std::move(credential_id), FROM_HERE);
+    PasswordsClientUIDelegate* manage_passwords_ui_controller =
+        PasswordsClientUIDelegateFromWebContents(web_contents);
+    if (manage_passwords_ui_controller) {
+      manage_passwords_ui_controller->OnPasskeyDeleted();
+    }
   }
 }
 
