fdb03f1f70a14cb10bbb3539f5f5c0e1c8bd7e83
rbpotter <rbpotter@chromium.org>
2024-07-29 20:45:53 +0000

    Reland "PDF Viewer: Rename elements/ files to WebUI standard naming scheme"
    This is a reland of commit 1385d0f925357ecdc13ec17550904992cc654df2
    
    Original change's description:
    > PDF Viewer: Rename elements/ files to WebUI standard naming scheme
    >
    > WebUI element definition files generally follow the pattern of
    > file foo_bar.ts --> defines element FooBarElement with tag name foo-bar
    > or
    > file prefix/foo_bar.ts --> element PrefixFooBarElement with tag name
    > prefix-foo-bar
    >
    > PDF viewer differs from this pattern for its elements/ folder, using
    > dashes instead of underscores (i.e. foo-bar.ts --> FooBarElement). This
    > breaks some automated migration/infrastructure tooling and does not
    > seem necessary, so rename the PDF viewer elements files to use
    > underscores instead of dashes.
    >
    > This is in preparation for migrating the PDF Viewer code to Lit.
    >
    > Low-Coverage-Reason: LARGE_SCALE_REFACTOR file rename only
    > Bug: 40943652
    > Change-Id: I8201cb3799f2359a96ed415305c2692043c58ecb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742258
    > Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    > Reviewed-by: Andy Phan <andyphan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1333719}
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR file rename only
    Bug: 40943652
    Change-Id: Ida34ab19d1e943888c8fff9d3d72ad29c5f07da2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5746480
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334438}

--- chrome/browser/resources/pdf/toolbar_manager.ts
+++ chrome/browser/resources/pdf/toolbar_manager.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {isRTL} from 'chrome://resources/js/util.js';
 
-import type {ViewerZoomToolbarElement} from './elements/viewer-zoom-toolbar.js';
+import type {ViewerZoomToolbarElement} from './elements/viewer_zoom_toolbar.js';
 
 /**
  * Idle time in ms before the UI is hidden.
