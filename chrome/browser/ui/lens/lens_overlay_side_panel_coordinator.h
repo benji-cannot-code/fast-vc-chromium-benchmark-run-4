fffd53552f06bc52475234f2d2fda98406fcbca9
Erik Chen <erikchen@chromium.org>
2024-07-29 07:09:46 +0000

    Move SidePanelEntryObserver.
    This CL is a refactor with no intended behavior change.
    
    This CL moves SidePanelEntryObserver to
    //chrome/browser/ui/views/side_panel, and deletes the now empty
    //chrome/browser/ui/side_panel directory.
    
    Bug: 345499793
    Change-Id: Ie622f5ec4d135cc372255484ca24eec92cc364b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744578
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1334107}

--- chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h
+++ chrome/browser/ui/lens/lens_overlay_side_panel_coordinator.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/chrome_web_modal_dialog_manager_delegate.h"
-#include "chrome/browser/ui/side_panel/side_panel_entry_observer.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/page_navigator.h"
 #include "content/public/browser/web_contents_observer.h"
--- chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.h
+++ chrome/browser/ui/views/side_panel/read_anything/read_anything_coordinator.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/browser/ui/browser_user_data.h"
-#include "chrome/browser/ui/side_panel/side_panel_entry_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
+#include "chrome/browser/ui/views/side_panel/side_panel_entry_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 
 class Browser;
