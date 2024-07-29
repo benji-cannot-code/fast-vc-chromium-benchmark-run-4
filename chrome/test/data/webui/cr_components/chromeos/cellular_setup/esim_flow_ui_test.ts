112881261e7be83e3f1102dbd24470b1b9c6945c
Theo Johnson-Kanu <tjohnsonkanu@google.com>
2024-07-29 18:56:19 +0000

    [Cellular] Add cellular activation code test
    This change adds fake MediaStreamTrack and fake MediaStream,
    this test checks a scenario where two media streams cannot be opened
    at the same time, resulting in an error in the UI.
    
    Bug: b/351751646
    Test: Added a new test, ran CQ and deployed to DUT
    Change-Id: I38c6aa87f825c744e0c5d96a7de71c12b5196284
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5726379
    Reviewed-by: Michael Checo <michaelcheco@google.com>
    Commit-Queue: Theo Johnson-kanu <tjohnsonkanu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334365}

--- chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.ts
+++ chrome/test/data/webui/cr_components/chromeos/cellular_setup/esim_flow_ui_test.ts
@@ -141,7 +141,6 @@ suite(`CrComponentsEsimFlowUiTest`, function() {
     // requests to play the video due to the speed of execution. Avoid this by
     // mocking the play and pause actions.
     const playVideoFunction = () => {};
-    const stopStreamFunction = (_stream: MediaStream) => {};
     assertTrue(!!activationCodePage);
     assertTrue(!!profileLoadingPage);
     assertTrue(!!profileDiscoveryConsentPage);
@@ -151,7 +150,7 @@ suite(`CrComponentsEsimFlowUiTest`, function() {
 
     await activationCodePage.setFakesForTesting(
         FakeBarcodeDetector, FakeImageCapture, setIntervalFunction,
-        playVideoFunction, stopStreamFunction);
+        playVideoFunction);
   });
 
   function assertSelectedPage(pageName: EsimPageName, page: HTMLElement): void {
