e838561ee8535d9527b0f3d29f9a6f4e005fc8ca
Rune Lillesveen <futhark@chromium.org>
2024-07-29 09:29:55 +0000

    Disable flaky ArcRobotAccountAuthServiceTests
    Bug: 352951605, 354131115, 355199222
    Change-Id: I52fde56aca5d532bbd30ff1aeafa33db104cc019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739745
    Auto-Submit: Rune Lillesveen <futhark@chromium.org>
    Owners-Override: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334139}

--- chrome/browser/ash/arc/auth/arc_auth_service_browsertest.cc
+++ chrome/browser/ash/arc/auth/arc_auth_service_browsertest.cc
@@ -1150,7 +1150,9 @@ class ArcRobotAccountAuthServiceTest : public ArcAuthServiceTest {
 
 // Tests that when ARC requests account info for a demo session account,
 // Chrome supplies the info configured in SetAccountAndProfile() above.
-IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest, GetDemoAccount) {
+// TODO(crbug.com/355199222): Flaky test
+IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest,
+                       DISABLED_GetDemoAccount) {
   ash::DemoSession::SetDemoConfigForTesting(
       ash::DemoSession::DemoModeConfig::kOnline);
   ash::test::LockDemoDeviceInstallAttributes();
@@ -1175,8 +1177,9 @@ IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest, GetDemoAccount) {
   EXPECT_FALSE(auth_instance().account_info()->is_managed);
 }
 
+// TODO(crbug.com/354131115): Flaky test
 IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest,
-                       GetDemoAccountOnAuthTokenFetchFailure) {
+                       DISABLED_GetDemoAccountOnAuthTokenFetchFailure) {
   ash::DemoSession::SetDemoConfigForTesting(
       ash::DemoSession::DemoModeConfig::kOnline);
   ash::test::LockDemoDeviceInstallAttributes();
@@ -1232,8 +1235,9 @@ IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest,
   EXPECT_TRUE(auth_instance().account_info()->is_managed);
 }
 
+// TODO(crbug.com/352951605): Flaky test
 IN_PROC_BROWSER_TEST_P(ArcRobotAccountAuthServiceTest,
-                       RequestPublicAccountInfo) {
+                       DISABLED_RequestPublicAccountInfo) {
   SetAccountAndProfile(user_manager::UserType::kPublicAccount);
   profile()->GetProfilePolicyConnector()->OverrideIsManagedForTesting(true);
 
