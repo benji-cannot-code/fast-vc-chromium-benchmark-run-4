6e03bd8eab643dc7eb4b7de949c1e55a0757cbd3
Antonio Sartori <antoniosartori@chromium.org>
2024-07-29 12:45:43 +0000

    [CSP] Use null origin in frame-ancestors violation reports
    Frame-ancestors violation reports are sent in Chrome through the
    renderer of the embedding frame. However, the outgoing requests for
    these reports should not have the Origin header set to the header of
    the embedder.
    
    Bug: 354894364
    Change-Id: I8fd165aa12a5774afea359f79df7ee1e355e4412
    Cq-Do-Not-Cancel-Tryjobs: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743298
    Commit-Queue: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334189}

--- third_party/blink/renderer/core/loader/ping_loader.cc
+++ third_party/blink/renderer/core/loader/ping_loader.cc
@@ -147,7 +147,8 @@ void PingLoader::SendLinkAuditPing(LocalFrame* frame,
 
 void PingLoader::SendViolationReport(ExecutionContext* execution_context,
                                      const KURL& report_url,
-                                     scoped_refptr<EncodedFormData> report) {
+                                     scoped_refptr<EncodedFormData> report,
+                                     bool is_frame_ancestors_violation) {
   ResourceRequest request(report_url);
   request.SetHttpMethod(http_names::kPOST);
   request.SetHTTPContentType(AtomicString("application/csp-report"));
@@ -156,7 +157,16 @@ void PingLoader::SendViolationReport(ExecutionContext* execution_context,
   request.SetCredentialsMode(network::mojom::CredentialsMode::kSameOrigin);
   request.SetRequestContext(mojom::blink::RequestContextType::CSP_REPORT);
   request.SetRequestDestination(network::mojom::RequestDestination::kReport);
-  request.SetRequestorOrigin(execution_context->GetSecurityOrigin());
+
+  // For frame-ancestors violations, execution_context->GetSecurityOrigin() is
+  // the origin of the embedding frame, while violations should be sent by the
+  // (blocked) embedded frame.
+  if (is_frame_ancestors_violation) {
+    request.SetRequestorOrigin(SecurityOrigin::CreateUniqueOpaque());
+  } else {
+    request.SetRequestorOrigin(execution_context->GetSecurityOrigin());
+  }
+
   request.SetRedirectMode(network::mojom::RedirectMode::kError);
   FetchParameters params(
       std::move(request),
