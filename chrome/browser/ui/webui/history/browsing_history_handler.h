ec972b4d015455837e6762b42e200782ad6a85fc
Orin Jaworski <orinj@google.com>
2024-07-29 21:16:33 +0000

    Fix data race & CHECK on BrowsingHistoryHandler::remove_visits_callback_
    Fixed: 356050013
    Change-Id: I383f11d5618f49831db89035ebd60211703f863e
    Low-Coverage-Reason: TRIVIAL_CHANGE Fixing crash bug
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5745832
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334456}

--- chrome/browser/ui/webui/history/browsing_history_handler.h
+++ chrome/browser/ui/webui/history/browsing_history_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include <memory>
+#include <queue>
 #include <string>
 #include <utility>
 #include <vector>
@@ -104,7 +105,7 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
 
   base::OnceClosure query_history_continuation_;
 
-  std::string remove_visits_callback_;
+  std::queue<std::string> remove_visits_callbacks_;
 
   base::WeakPtrFactory<BrowsingHistoryHandler> weak_factory_{this};
 };
