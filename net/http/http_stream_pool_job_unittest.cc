4a04f0626c0a1b0fd049fbdc23e6b396085391ae
Kenichi Ishibashi <bashi@chromium.org>
2024-07-30 07:54:28 +0000

    Improve NetLog events in StreamAttempt classes
    To improve debuggability.
    
    Bug: 346835898
    Change-Id: Ibdd693f8d3c45bb5e4e86308b7a68abfd30cf8cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5746149
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334685}

--- net/http/http_stream_pool_job_unittest.cc
+++ net/http/http_stream_pool_job_unittest.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_stream_pool_group.h"
 #include "net/http/http_stream_pool_handle.h"
 #include "net/http/http_stream_pool_test_util.h"
+#include "net/log/test_net_log.h"
 #include "net/socket/socket_test_util.h"
 #include "net/socket/tcp_stream_attempt.h"
 #include "net/spdy/spdy_test_util_common.h"
@@ -483,6 +484,8 @@ class HttpStreamPoolJobTest : public TestWithTaskEnvironment {
 
  private:
   base::test::ScopedFeatureList feature_list_;
+  // For NetLog recording test coverage.
+  RecordingNetLogObserver net_log_observer_;
   SpdySessionDependencies session_deps_;
   std::unique_ptr<HttpNetworkSession> http_network_session_;
 };
--- net/socket/tcp_stream_attempt.cc
+++ net/socket/tcp_stream_attempt.cc
@@ -73,6 +73,12 @@ int TcpStreamAttempt::StartInternal() {
   return rv;
 }
 
+base::Value::Dict TcpStreamAttempt::GetNetLogStartParams() {
+  base::Value::Dict dict;
+  dict.Set("ip_endpoint", ip_endpoint().ToValue());
+  return dict;
+}
+
 void TcpStreamAttempt::HandleCompletion() {
   next_state_ = State::kNone;
   timeout_timer_.Stop();
