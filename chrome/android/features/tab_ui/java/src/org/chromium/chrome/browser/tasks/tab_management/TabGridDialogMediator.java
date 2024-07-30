5b36bf8375920ab3f03cc5234caa3982766aa74c
Calder Kitagawa <ckitagawa@chromium.org>
2024-07-30 13:50:16 +0000

    [Tab Groups] Refactor overflow menus
    Refactor the overflow menus in Tab Switcher to be more ergonomic for
    testing and to share the collaboration logic.
    
    Change-Id: Ib28fa9683219fff678feb2e33851f30fdcb01680
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5747372
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334795}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogMediator.java
@@ -160,6 +160,7 @@ public class TabGridDialogMediator
     private final Runnable mShowInviteFlowUIRunnable;
     private final ActionConfirmationManager mActionConfirmationManager;
 
+    private TabGridDialogMenuCoordinator mTabGridDialogMenuCoordinator;
     private TabGroupTitleEditor mTabGroupTitleEditor;
     private Supplier<TabListEditorController> mTabListEditorControllerSupplier;
     private boolean mTabListEditorSetup;
@@ -868,16 +869,19 @@ public class TabGridDialogMediator
             tabGroupSyncService = TabGroupSyncServiceFactory.getForProfile(profile);
             dataSharingService = DataSharingServiceFactory.getForProfile(profile);
         }
+        if (mTabGridDialogMenuCoordinator == null) {
+            mTabGridDialogMenuCoordinator =
+                    new TabGridDialogMenuCoordinator(
+                            mToolbarMenuCallback,
+                            () -> mCurrentTabModelFilterSupplier.get().getTabModel(),
+                            () -> mCurrentTabId,
+                            shouldShowDeleteGroup,
+                            identityManager,
+                            tabGroupSyncService,
+                            dataSharingService);
+        }
 
-        return TabGridDialogMenuCoordinator.getTabGridDialogMenuOnClickListener(
-                mToolbarMenuCallback,
-                () -> mCurrentTabId,
-                () -> mModel.get(TabGridDialogProperties.IS_INCOGNITO),
-                shouldShowDeleteGroup,
-                () -> mCurrentTabModelFilterSupplier.get().getTabModel(),
-                identityManager,
-                tabGroupSyncService,
-                dataSharingService);
+        return mTabGridDialogMenuCoordinator.getOnClickListener();
     }
 
     private View.OnClickListener getShareBarClickListener() {
