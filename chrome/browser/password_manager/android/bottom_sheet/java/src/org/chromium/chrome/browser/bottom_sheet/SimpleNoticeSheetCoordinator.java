83e59d73999a9b0bd7b5de701ec07b15905d688b
Ivana Žužić <izuzic@google.com>
2024-07-30 11:23:25 +0000

    [UPMAccessLossWarning] Add the coordinator
    This CL adds the coordinator for the simple notice sheet.
    
    Bug: 353283409
    Change-Id: Ibec5de3b6ad2b790d414f9d7760ce6bc31ab8e7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5708109
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1334751}

---
+++ chrome/browser/password_manager/android/bottom_sheet/java/src/org/chromium/chrome/browser/bottom_sheet/SimpleNoticeSheetCoordinator.java
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.bottom_sheet;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** The coordinator of the simple notice sheet. */
+public class SimpleNoticeSheetCoordinator {
+
+    private PropertyModel mModel;
+    private final SimpleNoticeSheetMediator mMediator = new SimpleNoticeSheetMediator();
+
+    public SimpleNoticeSheetCoordinator(BottomSheetController sheetController) {
+        mModel = SimpleNoticeSheetProperties.createDefaultModel(mMediator::onDismissed);
+        mMediator.initialize(mModel);
+    }
+
+    public void showSheet() {
+        mMediator.showSheet();
+    }
+
+    @VisibleForTesting
+    PropertyModel getModel() {
+        return mModel;
+    }
+}
