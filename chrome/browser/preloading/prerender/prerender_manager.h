9651cfae41c58108cff6287fd38c2d147c6fa0a7
Lingqi Chi <lingqi@chromium.org>
2024-07-30 09:05:27 +0000

    Prerender: delete the logic of recording FirstCorrectPrerenderHintReceivedToRealSearchNavigationStartedDuration
    This histogram was delete by https://crrev.com/c/5325918. And this
    CL deletes the recording logic.
    
    Bug: 41493548
    Change-Id: I09c71741c6cbbf5439060d8c5968a6aa8831d3ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5747899
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334708}

--- chrome/browser/preloading/prerender/prerender_manager.h
+++ chrome/browser/preloading/prerender/prerender_manager.h
@@ -59,8 +59,6 @@ class PrerenderManager : public content::WebContentsObserver,
   ~PrerenderManager() override;
 
   // content::WebContentsObserver
-  void DidStartNavigation(
-      content::NavigationHandle* navigation_handle) override;
   void DidFinishNavigation(
       content::NavigationHandle* navigation_handle) override;
 
