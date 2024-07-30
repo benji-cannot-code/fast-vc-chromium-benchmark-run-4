fd9fef0e118375b26eb38d588bec1f3bdd33ed64
Devon Loehr <dloehr@google.com>
2024-07-30 17:43:20 +0000

    Replace variable-length arrays with FixedArrays
    Variable-length arrays are not part of the C++ standard, and are
    dangerous to use because they allocate a potentially-unbounded
    amount of data on the stack. In addition, they trigger a clang warning
    (-Wvla-extension) which is currently suppressed in Chromium builds.
    
    This change replaces VLAs with base::FixedArray, which has similar
    performance but does not risk overflowing the stack (arrays which are
    too large will be allocated on the heap instead).
    
    Bug: 349656479
    AX-Relnotes: n/a.
    Change-Id: Ic5db65cff7eec3fc8159d530e774df3f6f26bd18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740824
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Katie Dektar <katie@chromium.org>
    Commit-Queue: Devon Loehr <dloehr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334962}

--- services/accessibility/features/mojo/mojo_handle.cc
+++ services/accessibility/features/mojo/mojo_handle.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/logging.h"
+#include "base/types/fixed_array.h"
 #include "gin/arguments.h"
 #include "gin/array_buffer.h"
 #include "gin/converter.h"
@@ -214,23 +215,19 @@ void MojoHandle::WriteMessage(gin::Arguments* arguments) {
     return;
   }
 
-  const void* bytes = nullptr;
-  size_t num_bytes = 0;
+  base::span<const uint8_t> bytes;
   if (args[0]->IsArrayBuffer()) {
     v8::Local<v8::ArrayBuffer> array = args[0].As<v8::ArrayBuffer>();
-    bytes = array->Data();
-    num_bytes = array->ByteLength();
+    bytes = base::make_span(static_cast<const uint8_t*>(array->Data()),
+                            array->ByteLength());
   } else {
     v8::Local<v8::ArrayBufferView> view = args[0].As<v8::ArrayBufferView>();
-    num_bytes = view->ByteLength();
-    void* bites[num_bytes];
-    view->CopyContents(bites, num_bytes);
-    bytes = bites;
+    base::FixedArray<uint8_t> bites(view->ByteLength());
+    view->CopyContents(bites.data(), bites.size());
+    bytes = base::span(bites);
   }
 
-  auto message = mojo::Message(
-      base::make_span(static_cast<const uint8_t*>(bytes), num_bytes),
-      base::make_span(scoped_handles));
+  auto message = mojo::Message(bytes, base::make_span(scoped_handles));
   DCHECK(!message.IsNull());
   MojoResult result = mojo::WriteMessageNew(
       mojo::MessagePipeHandle(handle_.get().value()), message.TakeMojoMessage(),
--- services/accessibility/features/text_decoder.cc
+++ services/accessibility/features/text_decoder.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/logging.h"
+#include "base/types/fixed_array.h"
 #include "gin/arguments.h"
 #include "gin/converter.h"
 #include "gin/data_object_builder.h"
@@ -48,26 +49,20 @@ void TextDecoder::Decode(gin::Arguments* arguments) {
   // Note: We do not support a TextDecoderOptions parameter.
   CHECK_GT(args.size(), 0u);
   CHECK(args[0]->IsArrayBuffer() || args[0]->IsArrayBufferView());
-  void* bytes = nullptr;
-  size_t num_bytes = 0;
+
+  v8::Local<v8::ArrayBufferView> view;
   if (args[0]->IsArrayBuffer()) {
     v8::Local<v8::ArrayBuffer> array = args[0].As<v8::ArrayBuffer>();
-    bytes = array->Data();
-    num_bytes = array->ByteLength();
+    view = v8::DataView::New(array, 0, array->ByteLength());
   } else {
-    v8::Local<v8::ArrayBufferView> view = args[0].As<v8::ArrayBufferView>();
-    num_bytes = view->ByteLength();
-    void* bites[num_bytes];
-    view->CopyContents(bites, num_bytes);
-    bytes = bites;
+    view = args[0].As<v8::ArrayBufferView>();
   }
-  char result[num_bytes + 1];
-  for (size_t i = 0; i < num_bytes; i++) {
-    result[i] = *(static_cast<char*>(bytes) + i);
-  }
-  result[num_bytes] = 0;
 
-  arguments->Return(std::string(result));
+  base::FixedArray<char> bytes(view->ByteLength() + 1);
+  view->CopyContents(bytes.data(), view->ByteLength());
+  bytes[view->ByteLength()] = 0;
+
+  arguments->Return(std::string(bytes.data()));
 }
 
 TextDecoder::TextDecoder(v8::Local<v8::Context> context)
