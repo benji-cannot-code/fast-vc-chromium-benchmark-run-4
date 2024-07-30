8dacf6add71f32ec9037a79e7193200b3adf6d9a
Anna Tsvirchkova <atsvirchkova@google.com>
2024-07-30 14:44:22 +0000

    [UPMAccessLossWarning] Show access loss warning
    This CL implements displaying the access loss warning of the appropriate
    type and adds all the necessary strings.
    
    code paths which are not executed. Those code paths don't need to be
    tested.
    
    Bug: 353285154
    Change-Id: I62c99e938ffaaad0b575b41c86209d3a334670db
    Validate-Test-Flakiness: skip
    Low-Coverage-Reason: The test contains multiple "assert false" for the
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741689
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334819}

---
+++ chrome/browser/password_manager/android/access_loss/java/src/org/chromium/chrome/browser/access_loss/PasswordAccessLossDialogSettingsCoordinatorTest.java
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.access_loss;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.annotation.Config;
+
+import org.chromium.base.ContextUtils;
+import org.chromium.base.test.BaseRobolectricTestRunner;
+import org.chromium.base.test.util.Batch;
+import org.chromium.ui.modaldialog.ModalDialogManager;
+import org.chromium.ui.modelutil.PropertyModel;
+import org.chromium.ui.test.util.modaldialog.FakeModalDialogManager;
+
+/** Tests for {@link PasswordAccessLossDialogSettingsCoordinator} */
+@RunWith(BaseRobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
+@Batch(Batch.PER_CLASS)
+public class PasswordAccessLossDialogSettingsCoordinatorTest {
+    private PasswordAccessLossDialogSettingsCoordinator mCoordinator =
+            new PasswordAccessLossDialogSettingsCoordinator();
+    private FakeModalDialogManager mModalDialogManager =
+            new FakeModalDialogManager(ModalDialogManager.ModalDialogType.APP);
+
+    @Test
+    public void showsAndHidesAccessLossDialog() {
+        mCoordinator.showPasswordAccessLossDialog(
+                ContextUtils.getApplicationContext(),
+                mModalDialogManager,
+                PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED);
+        PropertyModel mDialogModel = mModalDialogManager.getShownDialogModel();
+        Assert.assertNotNull(mDialogModel);
+
+        mModalDialogManager.clickNegativeButton();
+        Assert.assertNull(mModalDialogManager.getShownDialogModel());
+    }
+}
