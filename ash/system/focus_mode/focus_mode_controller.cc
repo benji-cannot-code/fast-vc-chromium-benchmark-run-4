1be39176a4c08aa4f8edf2815435f06cf1231de6
Hongyu Long <hongyulong@chromium.org>
2024-07-30 17:03:02 +0000

    focus-mode: Rotate congratulatory text and emoji
    This CL will the feature to rotate the text and emoji for ending moment.
    
    Bug: b/307388513
    Test: Manual
    Change-Id: I6cb28c2ea277dde303403bad779cc277ac46bda5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5720008
    Reviewed-by: Sean Kau <skau@chromium.org>
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Richard Chui <richui@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334932}

--- ash/system/focus_mode/focus_mode_controller.cc
+++ ash/system/focus_mode/focus_mode_controller.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/toast/anchored_nudge_manager_impl.h"
 #include "ash/system/unified/unified_system_tray.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/rand_util.h"
 #include "base/time/time.h"
 #include "components/pref_registry/pref_registry_syncable.h"
 #include "components/prefs/pref_registry_simple.h"
@@ -88,7 +89,9 @@ FocusModeTray* GetFocusModeTrayInActiveWindow() {
   return status_area_widget->focus_mode_tray();
 }
 
-void ShowEndingMomentNudge() {
+void ShowEndingMomentNudge(
+    const size_t congratulatory_index,
+    const std::optional<FocusModeSession>& current_session) {
   auto* tray = GetFocusModeTrayInActiveWindow();
   if (!tray) {
     return;
@@ -97,7 +100,7 @@ void ShowEndingMomentNudge() {
   // NOTE: we anchor to `tray->image_view()` in order to center the nudge
   // properly because there is extra spacing on the actual `FocusModeTray` view.
   const auto& title_and_emoji =
-      focus_mode_util::GetCongratulatoryTextAndEmoji();
+      focus_mode_util::GetCongratulatoryTextAndEmoji(congratulatory_index);
   AnchoredNudgeData nudge_data(focus_mode_util::kFocusModeEndingMomentNudgeId,
                                NudgeCatalogName::kFocusModeEndingMomentNudge,
                                title_and_emoji, tray->image_view());
@@ -109,7 +112,6 @@ void ShowEndingMomentNudge() {
       base::BindRepeating(&FocusModeTray::ShowBubble, base::Unretained(tray));
   AnchoredNudgeManager::Get()->Show(nudge_data);
 
-  auto current_session = FocusModeController::Get()->current_session();
   CHECK(current_session);
   const std::u16string duration_string =
       focus_mode_util::GetDurationString(current_session->session_duration(),
@@ -488,7 +490,7 @@ void FocusModeController::MaybeShowEndingMomentNudge() {
     return;
   }
 
-  ShowEndingMomentNudge();
+  ShowEndingMomentNudge(congratulatory_index_, current_session_);
 }
 
 void FocusModeController::TriggerEndingMomentImmediately() {
@@ -593,6 +595,8 @@ void FocusModeController::OnTimerTick() {
       return;
     case FocusModeSession::State::kEnding:
       timer_.Stop();
+      congratulatory_index_ = base::RandInt(
+          /*min=*/0, /*max=*/focus_mode_util::kCongratulatoryTitleNum - 1);
 
       if (media_widget_) {
         CloseMediaWidget();
