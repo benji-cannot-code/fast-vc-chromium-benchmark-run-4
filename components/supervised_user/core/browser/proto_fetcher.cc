a338e1a427b8e9e4300a34a3bf8fae11d71903ff
James Lee <ljjlee@google.com>
2024-07-30 13:20:53 +0000

    Use google_apis::AddDefaultAPIKeyToRequest() rather than manually setting key
    Bug: b/354922516, b/355544759
    Change-Id: I9751517085a81588201369aaf937caa041a198e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742337
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: James Lee <ljjlee@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1334784}

--- components/supervised_user/core/browser/proto_fetcher.cc
+++ components/supervised_user/core/browser/proto_fetcher.cc
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/supervised_user/core/browser/proto/kidsmanagement_messages.pb.h"
 #include "components/supervised_user/core/browser/proto/test.pb.h"
 #include "components/supervised_user/core/common/supervised_user_constants.h"
+#include "google_apis/common/api_key_request_util.h"
 #include "google_apis/gaia/google_service_auth_error.h"
-#include "google_apis/google_api_keys.h"
 #include "net/base/request_priority.h"
 #include "net/http/http_status_code.h"
 #include "proto_fetcher.h"
@@ -116,10 +116,7 @@ std::unique_ptr<network::SimpleURLLoader> InitializeSimpleUrlLoader(
         CreateAuthorizationHeader(access_token_info.value()));
   } else {
     CHECK(channel);
-    resource_request->headers.SetHeader(
-        "X-Goog-Api-Key", *channel == version_info::Channel::STABLE
-                              ? google_apis::GetAPIKey()
-                              : google_apis::GetNonStableAPIKey());
+    google_apis::AddDefaultAPIKeyToRequest(*resource_request, *channel);
   }
 
   std::unique_ptr<network::SimpleURLLoader> simple_url_loader =
