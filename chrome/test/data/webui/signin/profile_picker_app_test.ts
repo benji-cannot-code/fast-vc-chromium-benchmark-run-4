4fe9aa10dfb654b35d8f621b36148b42cec97297
Teresa Mao <temao@chromium.org>
2024-07-30 19:58:21 +0000

    profile-picker UI: Migrate to Lit, part 3.
    - Migrate profile_card_main_view.ts.
    - Migrate iron-icon to cr-icon.
    
    Bug: 40943652
    Change-Id: Ic7938cb1a906cac9e5173b23a4186f4c9c941cf3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736810
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335055}

--- chrome/test/data/webui/signin/profile_picker_app_test.ts
+++ chrome/test/data/webui/signin/profile_picker_app_test.ts
@@ -18,7 +18,7 @@ import {ensureLazyLoaded, ManageProfilesBrowserProxyImpl, navigateTo, Routes} fr
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {flushTasks, waitBeforeNextRender} from 'chrome://webui-test/polymer_test_util.js';
+import {waitBeforeNextRender} from 'chrome://webui-test/polymer_test_util.js';
 import {microtasksFinished, whenCheck} from 'chrome://webui-test/test_util.js';
 
 import {TestManageProfilesBrowserProxy} from './test_manage_profiles_browser_proxy.js';
@@ -81,7 +81,7 @@ suite('ProfilePickerAppTest', function() {
 
     webUIListenerCallback(
         'profiles-list-changed', [browserProxy.profileSample]);
-    flushTasks();
+    await microtasksFinished();
     assertEquals(profilesContainer.querySelectorAll('profile-card').length, 1);
     mainView.$.addProfile.click();
     await waitForProfileCreationLoad();
@@ -108,7 +108,7 @@ suite('ProfilePickerAppTest', function() {
       accountImageUrl: 'account-image-url',
     };
     webUIListenerCallback('available-accounts-changed', [availableAccount]);
-    flushTasks();
+    await microtasksFinished();
     choice.$.signInButton.click();
     // Start Lacros signin flow.
     await microtasksFinished();
@@ -131,7 +131,7 @@ suite('ProfilePickerAppTest', function() {
     assertTrue(!!choice);
     // No available account.
     webUIListenerCallback('available-accounts-changed', []);
-    flushTasks();
+    await microtasksFinished();
     choice.$.signInButton.click();
     return browserProxy.whenCalled('selectNewAccount');
   });
