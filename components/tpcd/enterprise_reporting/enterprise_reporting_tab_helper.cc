686eb903dbaa0f0508fdd0fa1d4711330338be5b
Erin Stewart <erinstewart@google.com>
2024-07-30 13:10:33 +0000

    Use mocking in EnterpriseReportingTabHelper unit tests
    The EnterpriseReportingTabHelper unit tests are not correctly implemented, as they do not test whether QueueEnterpriseReport was called. Unit tests now use mocking to resolve this.
    
    go/3pcd-enterprise-reporting (Googlers-only)
    
    Bug: 350966712
    Change-Id: Ibd44640eb32d5999480afd2f6b5d3913aa95a1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744037
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Aaron Selya <selya@google.com>
    Auto-Submit: Erin Stewart <erinstewart@google.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334780}

--- components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.cc
+++ components/tpcd/enterprise_reporting/enterprise_reporting_tab_helper.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tpcd::enterprise_reporting {
 
+namespace {
+
 constexpr char kEnterpriseErrorReportType[] =
     "enterprise-third-party-cookie-access-error";
 constexpr char kEnterpriseWarningReportType[] =
@@ -37,6 +39,8 @@ base::Value::Dict CreateReportBody(const ReportDetails& report_details) {
 
   return body;
 }
+
+}  // namespace
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(EnterpriseReportingTabHelper);
 
