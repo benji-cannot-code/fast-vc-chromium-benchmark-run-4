3f262c1673258765680616599f3ed1f287b8a109
Chris Fredrickson <cfredric@chromium.org>
2024-07-30 16:53:31 +0000

    Migrate to optional-based GetHeader overload
    HttpRequestHeaders::GetHeader has a new overload
    which returns an optional instead of using an out-
    parameter. I'm migrating callers and will eventually
    delete the old version of the method.
    
    This CL was uploaded by git cl split.
    
    R=pmeenan@chromium.org
    
    Bug: 355451174
    Change-Id: I86f3e2759f092909a946eaf27fcc7a08040300b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749232
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Patrick Meenan <pmeenan@chromium.org>
    Commit-Queue: Patrick Meenan <pmeenan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334920}

--- net/shared_dictionary/shared_dictionary_network_transaction_unittest.cc
+++ net/shared_dictionary/shared_dictionary_network_transaction_unittest.cc
@@ -146,10 +146,9 @@ static void BrotliTestTransactionHandler(const HttpRequestInfo* request,
                                          std::string* response_status,
                                          std::string* response_headers,
                                          std::string* response_data) {
-  std::string header_value;
-  EXPECT_TRUE(request->extra_headers.GetHeader(
-      shared_dictionary::kAvailableDictionaryHeaderName, &header_value));
-  EXPECT_EQ(kTestDictionarySha256Base64, header_value);
+  EXPECT_THAT(request->extra_headers.GetHeader(
+                  shared_dictionary::kAvailableDictionaryHeaderName),
+              testing::Optional(kTestDictionarySha256Base64));
   *response_data = kBrotliEncodedDataString;
 }
 
@@ -157,10 +156,9 @@ static void ZstdTestTransactionHandler(const HttpRequestInfo* request,
                                        std::string* response_status,
                                        std::string* response_headers,
                                        std::string* response_data) {
-  std::string header_value;
-  EXPECT_TRUE(request->extra_headers.GetHeader(
-      shared_dictionary::kAvailableDictionaryHeaderName, &header_value));
-  EXPECT_EQ(kTestDictionarySha256Base64, header_value);
+  EXPECT_THAT(request->extra_headers.GetHeader(
+                  shared_dictionary::kAvailableDictionaryHeaderName),
+              testing::Optional(kTestDictionarySha256Base64));
   *response_data = kZstdEncodedDataString;
 }
 
@@ -324,10 +322,8 @@ TEST_F(SharedDictionaryNetworkTransactionTest, DictionaryId) {
   scoped_mock_transaction_->handler = base::BindRepeating(
       [](const HttpRequestInfo* request, std::string* response_status,
          std::string* response_headers, std::string* response_data) {
-        std::string dictionary_id;
-        EXPECT_TRUE(
-            request->extra_headers.GetHeader("dictionary-id", &dictionary_id));
-        EXPECT_EQ("\"test-id\"", dictionary_id);
+        EXPECT_THAT(request->extra_headers.GetHeader("dictionary-id"),
+                    testing::Optional(std::string("\"test-id\"")));
         *response_data = kBrotliEncodedDataString;
       });
 
@@ -366,10 +362,9 @@ TEST_F(SharedDictionaryNetworkTransactionTest,
   scoped_mock_transaction_->handler = base::BindRepeating(
       [](const HttpRequestInfo* request, std::string* response_status,
          std::string* response_headers, std::string* response_data) {
-        std::string dictionary_id;
-        EXPECT_TRUE(
-            request->extra_headers.GetHeader("dictionary-id", &dictionary_id));
-        EXPECT_EQ("\"test\\\\dictionary\\\"id\"", dictionary_id);
+        EXPECT_THAT(
+            request->extra_headers.GetHeader("dictionary-id"),
+            testing::Optional(std::string("\"test\\\\dictionary\\\"id\"")));
         *response_data = kBrotliEncodedDataString;
       });
 
