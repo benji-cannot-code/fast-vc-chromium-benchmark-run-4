9bd7c4e3cd4416b1c84394cc117d82896cd1f100
Kenichi Ishibashi <bashi@chromium.org>
2024-07-30 06:26:25 +0000

    HttpStreamPool: Fix decrementing total connecting count when canceling
    When canceling in-flight connection attempts in an
    HttpStreamPool::Group, we should also decrement the total number of
    connecting count in HttpStreamPool. Otherwise, the total number of
    connecting count would be inconsistent with the sum of the in-flight
    connection attempts of all groups.
    
    Bug: 354087603
    Change-Id: If5c3f93f9cd3072e7db5fdea5492cad87e8bd8ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740046
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334656}

--- net/http/http_stream_pool_job_unittest.cc
+++ net/http/http_stream_pool_job_unittest.cc
@@ -1413,6 +1413,7 @@ TEST_F(HttpStreamPoolJobTest, SpdyOk) {
   ASSERT_EQ(group.GetJobForTesting()->InFlightAttemptCount(), 0u);
   ASSERT_EQ(group.IdleStreamSocketCount(), 0u);
   ASSERT_EQ(group.ActiveStreamSocketCount(), 1u);
+  ASSERT_EQ(pool().TotalConnectingStreamCount(), 0u);
 }
 
 TEST_F(HttpStreamPoolJobTest, SpdyCreateSessionFail) {
