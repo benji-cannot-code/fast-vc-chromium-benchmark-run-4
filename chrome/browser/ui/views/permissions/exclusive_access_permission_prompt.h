849960092ed643966f51e18494fad46afc185949
Kamila Hasanbega <hkamila@google.com>
2024-07-30 12:36:57 +0000

    PEPC BUGFIX: Check the parent view is not closed before scrim display
    Check that the native view (i.e. tab) is not closed before/as the
    scrim gets created otherwise:
    If any of the following
    |web_content| OR web_content->GetContentNativeView()| OR |views::Widget::GetTopLevelWidgetForNativeView|
    would return null, it would result in a null-dereference in l40 (top_level_widget->GetNativeView()).
    
    Fixed: 353609471
    Change-Id: I4d0c12b21f67028c583845e00ce601a505f01047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739245
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Reviewed-by: Andy Paicu <andypaicu@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334770}

--- chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.h
+++ chrome/browser/ui/views/permissions/exclusive_access_permission_prompt.h
@@ -51,7 +51,7 @@ class ExclusiveAccessPermissionPrompt
   ExclusiveAccessPermissionPromptView* GetViewForTesting();
 
  private:
-  void ShowPrompt();
+  bool ShowPrompt();
   void ClosePrompt();
 
   views::UniqueWidgetPtr content_scrim_widget_;
