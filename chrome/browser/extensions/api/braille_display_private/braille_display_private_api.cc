6b35650c7508990b974a77c1e853bc77efa355ca
David Bertoni <dbertoni@chromium.org>
2024-07-30 19:51:25 +0000

    [Extensions] Move a header file into a conditional section.
    This file is only needed on Ash and there's already a section for
    those files.
    
    Bug: 40593486
    Change-Id: Ib95a976da38643aa823d0ab7dca9fe7aa79c9a21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750581
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335050}

--- chrome/browser/extensions/api/braille_display_private/braille_display_private_api.cc
+++ chrome/browser/extensions/api/braille_display_private/braille_display_private_api.cc
@@ -13,11 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/extensions/api/braille_display_private/braille_controller.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/profiles/profile_manager.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/login/lock/screen_locker.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #endif
 
 namespace OnDisplayStateChanged =
