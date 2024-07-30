f0df4bb9c4783a40a1bd442fe23cc332ac6e838c
Hongchan Choi <hongchan@chromium.org>
2024-07-30 16:32:05 +0000

    Avoid accessing unconnected outputs in AudioWorkletHandler, AudioHandler
    This CL fixes the logic to zero out output buses regardless of the
    outgoing connection status. If an output bus is not connected (or
    disconnected), we should assume that the outgoing connection might
    be stale.
    
    This fix is verified locally by both the author and the reporter.
    
    Bug: 354847246
    Change-Id: If10c7bb816e50f7b88252aa9a981b53704724da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744142
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334898}

--- third_party/blink/renderer/modules/webaudio/audio_handler.cc
+++ third_party/blink/renderer/modules/webaudio/audio_handler.cc
@@ -399,7 +399,9 @@ bool AudioHandler::InputsAreSilent() {
 
 void AudioHandler::SilenceOutputs() {
   for (auto& output : outputs_) {
-    output->Bus()->Zero();
+    if (output->IsConnectedDuringRendering()) {
+      output->Bus()->Zero();
+    }
   }
 }
 
