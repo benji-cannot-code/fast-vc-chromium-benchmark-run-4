cf391c8db7f81f2903fbe51fc21928f0f2b264e1
vikas soni <vikassoni@chromium.org>
2024-07-30 17:36:38 +0000

    Remove GenGpuDiskCacheKeyPrefixInGpuService after launch.
    GenGpuDiskCacheKeyPrefixInGpuService has been fully enabled on M127
    and have been fully launched in M126 via finch.
    
    Removing the feature code in M128 since we no longer need to control it
    via finch.
    
    Bug: 40947330
    Change-Id: I8ab24ebb8f6e36e50662f50997a8915910967355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5734518
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1334953}

--- components/viz/service/gl/gpu_service_impl.cc
+++ components/viz/service/gl/gpu_service_impl.cc
@@ -1095,9 +1095,7 @@ void GpuServiceImpl::StoreBlobToDisk(const gpu::GpuDiskCacheHandle& handle,
                                      const std::string& key,
                                      const std::string& shader) {
   std::string prefix_key = key;
-  if (base::FeatureList::IsEnabled(
-          features::kGenGpuDiskCacheKeyPrefixInGpuService) &&
-      GetHandleType(handle) == gpu::GpuDiskCacheType::kGlShaders) {
+  if (GetHandleType(handle) == gpu::GpuDiskCacheType::kGlShaders) {
     std::string prefix = GetShaderPrefixKey();
     prefix_key = prefix + ":" + key;
   }
@@ -1118,9 +1116,7 @@ void GpuServiceImpl::LoadedBlob(const gpu::GpuDiskCacheHandle& handle,
   const bool clear_shader_cache = base::FeatureList::IsEnabled(
       features::kClearGrShaderDiskCacheOnInvalidPrefix);
 
-  if (base::FeatureList::IsEnabled(
-          features::kGenGpuDiskCacheKeyPrefixInGpuService) &&
-      GetHandleType(handle) == gpu::GpuDiskCacheType::kGlShaders) {
+  if (GetHandleType(handle) == gpu::GpuDiskCacheType::kGlShaders) {
     std::string prefix = GetShaderPrefixKey();
     bool prefix_ok = !key.compare(0, prefix.length(), prefix);
     UMA_HISTOGRAM_BOOLEAN("GPU.ShaderLoadPrefixOK", prefix_ok);
