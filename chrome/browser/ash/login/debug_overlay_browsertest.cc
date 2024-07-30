3c83b73ae31a274348a74e71e5993fb2193de0ff
Bohdan Tyshchenko <bohdanty@google.com>
2024-07-30 10:09:45 +0000

    [OOBE][Perks] - Extend debug_overlay_browsertest with new screen
    Bug: b/356136247
    Change-Id: I4eec16c46ed1b1af884241d59a057956b4d9483b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743605
    Reviewed-by: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1334727}

--- chrome/browser/ash/login/debug_overlay_browsertest.cc
+++ chrome/browser/ash/login/debug_overlay_browsertest.cc
@@ -22,7 +22,7 @@ constexpr char kDebugButton[] = "invokeDebuggerButton";
 constexpr char kDebugOverlay[] = "debuggerOverlay";
 constexpr char kScreensPanel[] = "DebuggerPanelScreens";
 
-constexpr int kCommonScreensCount = 52;
+constexpr int kCommonScreensCount = 53;
 constexpr int kOobeOnlyScreensCount = 10;
 constexpr int kLoginOnlyScreensCount = 6;
 
@@ -44,7 +44,8 @@ class DebugOverlayTest : public OobeBaseTest {
     feature_list_.InitWithFeatures(
         {features::kOobeChoobe, features::kOobeTouchpadScroll,
          features::kOobeDisplaySize, features::kOobeGaiaInfoScreen,
-         features::kOobeSoftwareUpdate, features::kOobePersonalizedOnboarding},
+         features::kOobeSoftwareUpdate, features::kOobePersonalizedOnboarding,
+         features::kOobePerksDiscovery},
         {});
   }
 
