7273bd6aa86be160dc0839b4d14c08973e7136a0
Michael Checo <michaelcheco@google.com>
2024-07-31 20:26:01 +0000

    [WE] Resize device image for notification
    Screenshots:
    - Before: http://screen/63bQEgxboZ6Gfdk
    - After: http://screen/37NvhLdRzw3Ww3E
    
    Bug: b/329686601
    Change-Id: I94e6c41e52d8c0de1a39eaaa7787f833fa062c98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737570
    Reviewed-by: David Padlipsky <dpad@google.com>
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335659}

--- ash/system/input_device_settings/input_device_settings_notification_controller.cc
+++ ash/system/input_device_settings/input_device_settings_notification_controller.cc
@@ -45,6 +45,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/events/ash/mojom/simulate_right_click_modifier.mojom-shared.h"
 #include "ui/events/ash/mojom/six_pack_shortcut_modifier.mojom-shared.h"
 #include "ui/events/keycodes/keyboard_codes_posix.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_operations.h"
+#include "ui/gfx/image/image_skia_rep_default.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notification_types.h"
@@ -53,6 +56,28 @@ namespace ash {
 
 namespace {
 
+// Needs to stay in sync with `kLargeImageMaxHeight` declared in
+// ui/message_center/views/notification_view_md.cc.
+const int kMaxNotificationHeight = 218;
+
+int CalculateScaledWidth(int width, int height) {
+  return (kMaxNotificationHeight * width) / height;
+}
+
+gfx::Image ResizeImage(gfx::ImageSkia image) {
+  const SkBitmap bitmap = *image.bitmap();
+  SkBitmap bitmap5x =
+      skia::ImageOperations::Resize(bitmap, skia::ImageOperations::RESIZE_BEST,
+                                    5 * bitmap.width(), 5 * bitmap.height());
+  gfx::ImageSkia image_skia = gfx::ImageSkia::CreateFromBitmap(bitmap5x, 5.0);
+  if (image_skia.height() > kMaxNotificationHeight) {
+    image_skia = gfx::ImageSkiaOperations::CreateResizedImage(
+        image_skia, skia::ImageOperations::RESIZE_BEST,
+        gfx::Size(CalculateScaledWidth(image_skia.width(), image_skia.height()),
+                  kMaxNotificationHeight));
+  }
+  return gfx::Image(image_skia);
+}
 // A nudge/tutorial will not be shown if it already been shown 3 times, or if 24
 // hours have not yet passed since it was last shown.
 constexpr int kNudgeMaxShownCount = 3;
@@ -599,7 +624,7 @@ void InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::NotifyMouseFirstTimeConnected(
     const mojom::Mouse& mouse,
-    const gfx::Image& device_image) {
+    const gfx::ImageSkia& device_image) {
   if (!IsActiveUserSession() || !mouse.is_external) {
     return;
   }
@@ -640,7 +665,7 @@ void InputDeviceSettingsNotificationController::NotifyMouseFirstTimeConnected(
 void InputDeviceSettingsNotificationController::
     NotifyGraphicsTabletFirstTimeConnected(
         const mojom::GraphicsTablet& graphics_tablet,
-        const gfx::Image& device_image) {
+        const gfx::ImageSkia& device_image) {
   if (!IsActiveUserSession()) {
     return;
   }
@@ -823,7 +848,7 @@ void InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::
     NotifyKeyboardFirstTimeConnected(const mojom::Keyboard& keyboard,
-                                     const gfx::Image& device_image) {
+                                     const gfx::ImageSkia& device_image) {
   if (!IsActiveUserSession() || !keyboard.is_external) {
     return;
   }
@@ -850,7 +875,7 @@ void InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::
     NotifyTouchpadFirstTimeConnected(const mojom::Touchpad& touchpad,
-                                     const gfx::Image& device_image) {
+                                     const gfx::ImageSkia& device_image) {
   if (!IsActiveUserSession() || !touchpad.is_external) {
     return;
   }
@@ -934,7 +959,7 @@ void InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::NotifyMouseIsCustomizable(
     const mojom::Mouse& mouse,
-    const gfx::Image& device_image) {
+    const gfx::ImageSkia& device_image) {
   const auto peripheral_name = base::UTF8ToUTF16(mouse.name);
   const auto notification_id = GetMouseNotificationID(mouse.id);
   const auto message =
@@ -944,8 +969,8 @@ void InputDeviceSettingsNotificationController::NotifyMouseIsCustomizable(
                 peripheral_name)
           : GetBatteryLevelMessage(*mouse.battery_info);
   message_center::RichNotificationData rich_notification_data;
-  if (!device_image.IsEmpty()) {
-    rich_notification_data.image = device_image;
+  if (!device_image.isNull()) {
+    rich_notification_data.image = ResizeImage(device_image);
   }
   rich_notification_data.buttons.emplace_back(l10n_util::GetStringUTF16(
       IDS_ASH_DEVICE_SETTINGS_NOTIFICATIONS_OPEN_SETTINGS_BUTTON));
@@ -967,7 +992,7 @@ void InputDeviceSettingsNotificationController::NotifyMouseIsCustomizable(
 
 void InputDeviceSettingsNotificationController::
     ShowKeyboardSettingsNotification(const mojom::Keyboard& keyboard,
-                                     const gfx::Image& device_image) {
+                                     const gfx::ImageSkia& device_image) {
   const auto peripheral_name = base::UTF8ToUTF16(keyboard.name);
   const auto notification_id = GetWelcomeExperienceNotificationId(
       kKeyboardNotificationPrefix, keyboard.id);
@@ -978,8 +1003,8 @@ void InputDeviceSettingsNotificationController::
                 peripheral_name)
           : GetBatteryLevelMessage(*keyboard.battery_info);
   message_center::RichNotificationData rich_notification_data;
-  if (!device_image.IsEmpty()) {
-    rich_notification_data.image = device_image;
+  if (!device_image.isNull()) {
+    rich_notification_data.image = ResizeImage(device_image);
   }
   rich_notification_data.buttons.emplace_back(l10n_util::GetStringUTF16(
       IDS_ASH_DEVICE_SETTINGS_NOTIFICATIONS_OPEN_SETTINGS_BUTTON));
@@ -1001,7 +1026,7 @@ void InputDeviceSettingsNotificationController::
 
 void InputDeviceSettingsNotificationController::
     ShowTouchpadSettingsNotification(const mojom::Touchpad& touchpad,
-                                     const gfx::Image& device_image) {
+                                     const gfx::ImageSkia& device_image) {
   const auto peripheral_name = base::UTF8ToUTF16(touchpad.name);
   const auto message =
       touchpad.battery_info.is_null()
@@ -1012,8 +1037,8 @@ void InputDeviceSettingsNotificationController::
   const auto notification_id = GetWelcomeExperienceNotificationId(
       kTouchpadNotificationPrefix, touchpad.id);
   message_center::RichNotificationData rich_notification_data;
-  if (!device_image.IsEmpty()) {
-    rich_notification_data.image = device_image;
+  if (!device_image.isNull()) {
+    rich_notification_data.image = ResizeImage(device_image);
   }
   rich_notification_data.buttons.emplace_back(l10n_util::GetStringUTF16(
       IDS_ASH_DEVICE_SETTINGS_NOTIFICATIONS_OPEN_SETTINGS_BUTTON));
@@ -1036,7 +1061,7 @@ void InputDeviceSettingsNotificationController::
 void InputDeviceSettingsNotificationController::
     NotifyGraphicsTabletIsCustomizable(
         const mojom::GraphicsTablet& graphics_tablet,
-        const gfx::Image& device_image) {
+        const gfx::ImageSkia& device_image) {
   const auto peripheral_name = base::UTF8ToUTF16(graphics_tablet.name);
   const auto message =
       graphics_tablet.battery_info.is_null()
@@ -1047,8 +1072,8 @@ void InputDeviceSettingsNotificationController::
   const auto notification_id =
       GetGraphicsTabletNotificationID(graphics_tablet.id);
   message_center::RichNotificationData rich_notification_data;
-  if (!device_image.IsEmpty()) {
-    rich_notification_data.image = device_image;
+  if (!device_image.isNull()) {
+    rich_notification_data.image = ResizeImage(device_image);
   }
   rich_notification_data.buttons.emplace_back(l10n_util::GetStringUTF16(
       IDS_ASH_DEVICE_SETTINGS_NOTIFICATIONS_OPEN_SETTINGS_BUTTON));
