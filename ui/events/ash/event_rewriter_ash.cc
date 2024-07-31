b357b13d734af4bfe6a01c9d73b709bbc51d6781
David Padlipsky <dpad@google.com>
2024-07-31 23:29:25 +0000

    Only show sixpack nudge once if multiple entries match
    Bug: b/356697164
    Change-Id: I2a6b415806994f56797788eddb02138edf41b754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754726
    Commit-Queue: Yuhan Yang <yyhyyh@google.com>
    Auto-Submit: David Padlipsky <dpad@google.com>
    Reviewed-by: Yuhan Yang <yyhyyh@google.com>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335764}

--- ui/events/ash/event_rewriter_ash.cc
+++ ui/events/ash/event_rewriter_ash.cc
@@ -1002,6 +1002,7 @@ void NotifySixPackRewriteBlockedByFnKey(
           map.result.key_code, (map.condition.flags & ui::EF_COMMAND_DOWN)
                                    ? mojom::SixPackShortcutModifier::kSearch
                                    : mojom::SixPackShortcutModifier::kAlt);
+      return;
     }
   }
 }
