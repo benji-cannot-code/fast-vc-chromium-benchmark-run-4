2b398cf6a020be3d436c9b5dade082af3817a313
Michael Tang <tangm@microsoft.com>
2024-07-31 18:34:25 +0000

    viz: Prevent bypass quad case for "direct scanout" RPDQ overlays
    On Windows, we support "direct scanout" RPDQ overlays in the partially
    delegated compositing case. These RPDQ overlays have backings that are
    scanout and can skip the extra copy associated with
    PrepareRenderPassOverlay. This change makes it so we avoid hitting the
    RPDQ bypass quad case for these overlays and always have an entry in
    render_pass_backings_. This decision is supported by the assumption that
    these RPDQ overlays represent planes that are intended to persist some
    time (e.g. web contents surface plane), so forcing it into a single
    allocation avoids potential thrashing on allocations.
    
    Bug: 40150597
    Change-Id: I58e2a5d13bdce88739cd25d869b7f7b99554b02e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750075
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1335581}

--- components/viz/service/display/direct_renderer.cc
+++ components/viz/service/display/direct_renderer.cc
@@ -135,7 +135,8 @@ gfx::Rect DirectRenderer::MoveFromDrawToWindowSpace(
 }
 
 const DrawQuad* DirectRenderer::CanPassBeDrawnDirectly(
-    const AggregatedRenderPass* pass) {
+    const AggregatedRenderPass* pass,
+    const RenderPassRequirements& requirements) {
   return nullptr;
 }
 
@@ -192,10 +193,14 @@ void DirectRenderer::DecideRenderPassAllocationsForFrame(
     // for performance.
 #endif
 
+    const RenderPassRequirements requirements =
+        CalculateRenderPassRequirements(pass.get());
+
     // If there's a copy request, we need an explicit renderpass backing so
     // only try to draw directly if there are no copy requests.
     if (!is_root && pass->copy_requests.empty()) {
-      if (const DrawQuad* quad = CanPassBeDrawnDirectly(pass.get())) {
+      if (const DrawQuad* quad =
+              CanPassBeDrawnDirectly(pass.get(), requirements)) {
         // If the render pass is drawn directly, it will not be drawn from as
         // a render pass so it's not added to the map.
         render_pass_bypass_quads_[pass->id] = quad;
@@ -203,8 +208,7 @@ void DirectRenderer::DecideRenderPassAllocationsForFrame(
       }
     }
 
-    render_passes_in_frame[pass->id] =
-        CalculateRenderPassRequirements(pass.get());
+    render_passes_in_frame[pass->id] = requirements;
   }
   UMA_HISTOGRAM_COUNTS_1000(
       "Compositing.Display.FlattenedRenderPassCount",
