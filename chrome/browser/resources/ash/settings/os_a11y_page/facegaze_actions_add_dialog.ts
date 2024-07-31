46003410b53081e40d8c533231ecd117193fd5af
Amanda Lin Dietz <aldietz@google.com>
2024-07-31 17:39:22 +0000

    [FaceGaze] Add functionality to open gesture threshold page when gesture button is clicked
    * When the gesture chip is clicked on the actions card page, open the
    gesture threshold page.
    * Add knob label to threshold slider.
    * Add test.
    
    Screenshot for knob label:
    https://screenshot.googleplex.com/4UcV9RumpcKEcEe
    
    Bug: 341770796, 355659370
    Change-Id: I8c4a29e1bb28cf6285241515ba3fbe1e4b856333
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742743
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335539}

--- chrome/browser/resources/ash/settings/os_a11y_page/facegaze_actions_add_dialog.ts
+++ chrome/browser/resources/ash/settings/os_a11y_page/facegaze_actions_add_dialog.ts
@@ -74,6 +74,11 @@ export class FaceGazeAddActionDialogElement extends
         observer: 'actionToAssignGestureChanged_',
       },
 
+      gestureToConfigure: {
+        type: Object,
+        observer: 'gestureToConfigureChanged_',
+      },
+
       leftClickGestures: {
         type: Array,
         value: () => [],
@@ -144,6 +149,11 @@ export class FaceGazeAddActionDialogElement extends
         type: Boolean,
         computed: 'shouldDisplayGesturePreviousButton_(initialPage)',
       },
+
+      displayThresholdPreviousButton_: {
+        type: Boolean,
+        computed: 'shouldDisplayThresholdPreviousButton_(initialPage)',
+      },
     };
   }
 
@@ -153,6 +163,7 @@ export class FaceGazeAddActionDialogElement extends
 
   actionToAssignGesture: MacroName|null = null;
   initialPage: AddDialogPage = AddDialogPage.SELECT_ACTION;
+  gestureToConfigure: FacialGesture|null = null;
   leftClickGestures: FacialGesture[] = [];
 
   // Internal state.
@@ -208,6 +219,12 @@ export class FaceGazeAddActionDialogElement extends
     return this.initialPage === AddDialogPage.SELECT_ACTION;
   }
 
+  private shouldDisplayThresholdPreviousButton_(): boolean {
+    // Only show the previous button on the threshold page if we are starting
+    // from an earlier dialog flow.
+    return this.initialPage !== AddDialogPage.GESTURE_THRESHOLD;
+  }
+
   // Dialog page navigation
   private shouldShowSelectAction_(): boolean {
     return this.currentPage_ === AddDialogPage.SELECT_ACTION;
@@ -231,6 +248,14 @@ export class FaceGazeAddActionDialogElement extends
     }
 
     this.selectedAction_ = newValue;
+  }
+
+  private gestureToConfigureChanged_(newValue: FacialGesture|null): void {
+    if (!newValue) {
+      return;
+    }
+
+    this.selectedGesture_ = newValue;
   }
 
   // If left-click action is assigned to a singular gesture then remove it
