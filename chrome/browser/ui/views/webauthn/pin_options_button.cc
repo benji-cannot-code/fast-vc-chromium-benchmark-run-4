2f4c31d78e283177707f952416176d650fed4dd8
rgod <rgod@google.com>
2024-07-31 17:19:19 +0000

    [a11y] Change pin options button items type to check
    This way the screen reader will announce the check icon as well.
    
    Fixed: b/356418218
    Change-Id: I7758edc1ea6475b3601c93f5342cc781a79f5461
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752662
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Rafał Godlewski <rgod@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335527}

--- chrome/browser/ui/views/webauthn/pin_options_button.cc
+++ chrome/browser/ui/views/webauthn/pin_options_button.cc
@@ -12,11 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/menu/menu_runner.h"
-#include "ui/views/vector_icons.h"
 
 namespace {
-
-constexpr int kCheckIconSize = 16;
 
 std::u16string GetCommandIdLabel(int command_id) {
   switch (command_id) {
@@ -40,6 +37,7 @@ PinOptionsButton::PinOptionsButton(const std::u16string& label,
           base::BindRepeating(&PinOptionsButton::ButtonPressed,
                               base::Unretained(this)),
           label),
+      checked_command_id_(checked_command_id),
       callback_(std::move(callback)),
       menu_model_(std::make_unique<ui::SimpleMenuModel>(this)) {
   GetViewAccessibility().SetName(label);
@@ -47,15 +45,7 @@ PinOptionsButton::PinOptionsButton(const std::u16string& label,
 
   for (int command_id = 0; command_id < CommandId::COMMAND_ID_COUNT;
        command_id++) {
-    const std::u16string command_label = GetCommandIdLabel(command_id);
-    if (command_id == checked_command_id) {
-      menu_model_->AddItemWithIcon(
-          command_id, command_label,
-          ui::ImageModel::FromVectorIcon(views::kMenuCheckIcon,
-                                         ui::kColorMenuIcon, kCheckIconSize));
-    } else {
-      menu_model_->AddItem(command_id, command_label);
-    }
+    menu_model_->AddCheckItem(command_id, GetCommandIdLabel(command_id));
   }
 }
 
@@ -82,6 +72,10 @@ void PinOptionsButton::ExecuteCommand(int command_id, int event_flags) {
       NOTREACHED_IN_MIGRATION();
       return;
   }
+}
+
+bool PinOptionsButton::IsCommandIdChecked(int command_id) const {
+  return checked_command_id_ == command_id;
 }
 
 BEGIN_METADATA(PinOptionsButton)
