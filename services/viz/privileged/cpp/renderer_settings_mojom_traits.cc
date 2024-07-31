d494d94ae08d02723381c4a5f47b868e2dcf70de
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-07-31 18:43:25 +0000

    Move renderer_settings_mojom_traits.* from viz/privileged/mojom/compositing/ to viz/privileged/cpp/
    Bug: b/356017406
    Change-Id: I0621a0dcff790243ad03d2805786993dc413dedb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744138
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335586}

--- services/viz/privileged/cpp/renderer_settings_mojom_traits.cc
+++ services/viz/privileged/cpp/renderer_settings_mojom_traits.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "services/viz/privileged/mojom/compositing/renderer_settings_mojom_traits.h"
+#include "services/viz/privileged/cpp/renderer_settings_mojom_traits.h"
 
 #include "build/build_config.h"
 
