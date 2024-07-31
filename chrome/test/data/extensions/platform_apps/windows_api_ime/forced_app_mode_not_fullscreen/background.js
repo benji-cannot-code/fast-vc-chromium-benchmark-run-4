7a528f417815cbf1317f012a0c0b557d03d8c8e2
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-07-31 21:09:56 +0000

    Update browser tests to account for rounded windows for ChromeOS
    In crrev.com/c/5731315, when determining the minimum window size, the
    window rounded corners are considered by app windows. To account for
    the new constraint, modify the test.
    
    Bug: b/352576309
    Change-Id: Ic53f62cecbd4f4ab5ff4265123d157db8eb6248a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754411
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335688}

--- chrome/test/data/extensions/platform_apps/windows_api_ime/forced_app_mode_not_fullscreen/background.js
+++ chrome/test/data/extensions/platform_apps/windows_api_ime/forced_app_mode_not_fullscreen/background.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var testBounds = {left: 0, top: 0, width: 10, height: 10};
+var testBounds = {left: 0, top: 0, width: 24, height: 24};
 
 chrome.test.runTests([
 
