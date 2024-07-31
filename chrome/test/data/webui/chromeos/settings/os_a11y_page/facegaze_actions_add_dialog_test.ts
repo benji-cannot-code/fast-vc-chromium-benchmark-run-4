85b3b3a5e47cec0e93e225b5610259177e27b887
Amanda Lin Dietz <aldietz@google.com>
2024-07-31 17:36:36 +0000

    [FaceGaze] Update logic for gesture dialog
    * Add logic to show the dialog for selecting a gesture when the
    'Assign a gesture' button is clicked for an action.
    * Update an existing action/null gesture row if the
    'Assign a gesture' button is clicked rather than pushing a new row.
    * Add logic to exclude gesture assigned to left click if only one
    gestures is assigned to left click
    * Add tests
    * Remove space/down/left/right/up keys to match spec
    
    Bug: 351025155, 353403651
    Change-Id: I5325d5d1ffcb76606c6803075ebb92eed2609b9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743834
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335538}

--- chrome/test/data/webui/chromeos/settings/os_a11y_page/facegaze_actions_add_dialog_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_a11y_page/facegaze_actions_add_dialog_test.ts
@@ -5,13 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://os-settings/lazy_load.js';
 
-import {FaceGazeAddActionDialogElement} from 'chrome://os-settings/lazy_load.js';
+import {AddDialogPage, FaceGazeAddActionDialogElement} from 'chrome://os-settings/lazy_load.js';
 import {CrButtonElement, CrSettingsPrefs, CrSliderElement, IronListElement, Router, routes, SettingsPrefsElement} from 'chrome://os-settings/os_settings.js';
 import {FacialGesture} from 'chrome://resources/ash/common/accessibility/facial_gestures.js';
 import {MacroName} from 'chrome://resources/ash/common/accessibility/macro_names.js';
 import {pressAndReleaseKeyOn} from 'chrome://resources/polymer/v3_0/iron-test-helpers/mock-interactions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
-import {assertFalse, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {assertEquals, assertFalse, assertNull, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
 
 import {clearBody} from '../utils.js';
@@ -29,6 +29,11 @@ suite('<facegaze-actions-add-dialog>', () => {
         document.createElement('facegaze-actions-add-dialog');
     faceGazeAddActionDialog.prefs = prefElement.prefs;
     document.body.appendChild(faceGazeAddActionDialog);
+
+    // Assume default open to SELECT_ACTION page.
+    assertEquals(
+        AddDialogPage.SELECT_ACTION,
+        faceGazeAddActionDialog.getCurrentPageForTest());
     flush();
   }
 
@@ -368,4 +373,14 @@ suite('<facegaze-actions-add-dialog>', () => {
 
         assertTrue(isThresholdValueSetInPref(55));
       });
+
+  test('action page switches pages when initialPage value is set', async () => {
+    await initPage();
+
+    faceGazeAddActionDialog.actionToAssignGesture = MacroName.MOUSE_CLICK_LEFT;
+    faceGazeAddActionDialog.initialPage = AddDialogPage.SELECT_GESTURE;
+    flush();
+
+    assertGesturesListNoSelection();
+  });
 });
