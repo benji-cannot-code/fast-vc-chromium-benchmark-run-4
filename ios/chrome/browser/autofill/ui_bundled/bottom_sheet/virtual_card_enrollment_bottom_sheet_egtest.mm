53c202b0aea31b41737ab2d82f4bed44dec47743
Slobodan Pejic <slobodan@chromium.org>
2024-07-31 20:27:10 +0000

    Make the logo on VCN enrollment an accessibility element. (Reland)
    Before this change the Google Payments logo was not announced by
    VoiceOver. Now, the image is an accessibility element with the
    accessibility label "Google Pay logo". VoiceOver announces "Google Pay
    logo. image GPay" for this logo.
    
    Reland of commit b9184f7839cb26467992ddae05f3436fac5de189: The EarlGrey
    assertion must be made in a test method that enables the required
    kAutofillEnableVcnEnrollLoadingAndConfirmation flag. Only changes to
    test code not production code were needed.
    
    Screencap:
    https://drive.google.com/file/d/1N02Aectx6m5gM7aECLNa-StkYzN4TTzy/view?usp=sharing&resourcekey=0-SlgOXN0sdebKgRZEORcg4w
    
    Bug: b:355495835, 339887700
    Change-Id: Ia2f88accf7a9e37950e9e2933fe7b4f10297b3ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753786
    Auto-Submit: Slobodan Pejic <slobodan@chromium.org>
    Reviewed-by: Kavita Soni <kavitasoni@chromium.org>
    Commit-Queue: Kavita Soni <kavitasoni@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335661}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/virtual_card_enrollment_bottom_sheet_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "build/branding_buildflags.h"
 #import "components/autofill/core/common/autofill_payments_features.h"
 #import "components/strings/grit/components_strings.h"
 #import "ios/chrome/browser/autofill/ui_bundled/authentication/authentication_egtest_util.h"
@@ -242,6 +243,14 @@ id<GREYMatcher> VirtualCardEnrollmentSkipButton() {
 
   // Avoid immediately failing due to missing access token.
   [AutofillAppInterface setAccessToken];
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  // Assert the logo has an accessibility label set.
+  [[EarlGrey selectElementWithMatcher:
+                 grey_accessibilityLabel(l10n_util::GetNSString(
+                     IDS_AUTOFILL_GOOGLE_PAY_LOGO_ACCESSIBLE_NAME))]
+      assertWithMatcher:grey_sufficientlyVisible()];
+#endif
 
   // Push the accept button on the virtual card enrollment bottom sheet.
   [[EarlGrey selectElementWithMatcher:VirtualCardEnrollmentAcceptButton()]
