41c66518cd56e3f9ea898b932b26e389c761fd78
Alex Rudenko <alexrudenko@chromium.org>
2024-07-31 11:08:26 +0000

    Create more dedicated tests for setCookie
    This CL adds new tests specifically for Network.setCookie
    in an effort to split up the huge test that is
    inspector-protocol/network/cookies-protocol-test.js
    
    Bug: 1325514
    Change-Id: I507eeacc925c4906ffc32cf9b331ff1e0192dd75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750783
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335370}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/network/cookies-set-cookie-update-existing.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const {dp} = await testRunner.startBlank(`Tests that cookies can be updated`);
+
+  const helper = (await testRunner.loadScript('resources/cookie-helper.js'))(dp);
+
+  await dp.Network.enable();
+
+  testRunner.log(await dp.Network.setCookie({
+    url: 'http://127.0.0.1',
+    name: 'foo',
+    value: 'bar1'
+  }), 'Result of Network.setCookie: ');
+
+  testRunner.log(await helper.getCookiesLog());
+
+  testRunner.log(await dp.Network.setCookie({
+    url: 'http://127.0.0.1',
+    name: 'foo',
+    value: 'second bar2'
+  }), 'Result of Network.setCookie to update existing one: ');
+
+  testRunner.log(await helper.getCookiesLog());
+
+  testRunner.completeTest();
+});
