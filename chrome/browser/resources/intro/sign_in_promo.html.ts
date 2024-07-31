779ac692db496d7519e7917af62a72b674b97e2d
dpapad <dpapad@chromium.org>
2024-07-31 18:28:05 +0000

    Intro: Migrate to Lit, part 2 (last).
    Check-in previously auto-generated *.html.ts files.
    
    Bug: 40943652
    Change-Id: Ib9fbc10279d4f6a64246dacab5848f23d2286964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751124
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335575}

--- chrome/browser/resources/intro/sign_in_promo.html.ts
+++ chrome/browser/resources/intro/sign_in_promo.html.ts
@@ -1,4 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+import type {SignInPromoElement} from './sign_in_promo.js';
+
+export function getHtml(this: SignInPromoElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <img class="tangible-sync-style-left-banner" alt="">
 <img class="tangible-sync-style-right-banner" alt="">
 
@@ -42,3 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </cr-button>
   </div>
 </div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
