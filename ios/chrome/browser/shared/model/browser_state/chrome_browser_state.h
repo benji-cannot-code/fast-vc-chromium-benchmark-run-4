f7b070857da26ee1b6d95a8631e2427684c0db07
Sylvain Defresne <sdefresne@chromium.org>
2024-07-31 10:14:50 +0000

    [ios] Add asynchronous initialisation support to ChromeBrowserState
    Add a new CreationMode value kAsynchronous which request that the
    ChromeBrowserState initialisation happens asynchronously. When
    used, the PrefService is loaded on a background thread and the
    KeyedService only attached when the PrefService has been loaded.
    
    This required splitting the initialisation in two with the part
    happening after the prefs has been loaded moved to OnPrefLoaded().
    Until this method is called, most of the code should not have
    access to the ChromeBrowserState object (since the prefs will be
    inconsistent and accessing KeyedService will fail).
    
    Bug: 335627080
    Change-Id: If85297b3c7cf2d83876c2fafed30e853f5b25307
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5748946
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335349}

--- ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h
+++ ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h
@@ -48,6 +48,7 @@ class ChromeBrowserState : public web::BrowserState {
  public:
   enum class CreationMode {
     kSynchronous,
+    kAsynchronous,
   };
 
   // Delegate notified of ChromeBrowserState creation events.
