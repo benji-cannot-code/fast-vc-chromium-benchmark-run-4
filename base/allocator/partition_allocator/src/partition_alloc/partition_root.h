d505ebad92fcca1b876f5a31edaea5bb72c4a362
Keishi Hattori <keishi@chromium.org>
2024-07-31 08:22:54 +0000

    [MTE] Use a random value to update a tag
    Using a random tag rather than incrementing will increase security.
    
    Change-Id: I4b8343e553b843df1c57b3f5c579b65ce8716741
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5366433
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335309}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.h
@@ -176,6 +176,7 @@ struct PartitionOptions {
 
   struct {
     EnableToggle enabled = kDisabled;
+    EnableToggle random_memory_tagging = kDisabled;
     TagViolationReportingMode reporting_mode =
         TagViolationReportingMode::kUndefined;
   } memory_tagging;
@@ -264,6 +265,7 @@ struct PA_ALIGNAS(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
     bool scheduler_loop_quarantine = false;
 #if PA_BUILDFLAG(HAS_MEMORY_TAGGING)
     bool memory_tagging_enabled_ = false;
+    bool use_random_memory_tagging_ = false;
     TagViolationReportingMode memory_tagging_reporting_mode_ =
         TagViolationReportingMode::kUndefined;
 #endif  // PA_BUILDFLAG(HAS_MEMORY_TAGGING)
@@ -595,6 +597,7 @@ struct PA_ALIGNAS(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
 #endif  // PA_BUILDFLAG(ENABLE_BACKUP_REF_PTR_SUPPORT)
 
   PA_ALWAYS_INLINE bool IsMemoryTaggingEnabled() const;
+  PA_ALWAYS_INLINE bool UseRandomMemoryTagging() const;
   PA_ALWAYS_INLINE TagViolationReportingMode
   memory_tagging_reporting_mode() const;
 
@@ -1417,6 +1420,14 @@ PA_ALWAYS_INLINE bool PartitionRoot::IsMemoryTaggingEnabled() const {
 #endif  // PA_BUILDFLAG(HAS_MEMORY_TAGGING)
 }
 
+PA_ALWAYS_INLINE bool PartitionRoot::UseRandomMemoryTagging() const {
+#if PA_BUILDFLAG(HAS_MEMORY_TAGGING)
+  return settings.use_random_memory_tagging_;
+#else
+  return false;
+#endif  // PA_BUILDFLAG(HAS_MEMORY_TAGGING)
+}
+
 PA_ALWAYS_INLINE TagViolationReportingMode
 PartitionRoot::memory_tagging_reporting_mode() const {
 #if PA_BUILDFLAG(HAS_MEMORY_TAGGING)
@@ -1498,7 +1509,15 @@ PA_ALWAYS_INLINE void PartitionRoot::FreeInline(void* object) {
       // Retag the `object` to provide MTE UaF mitigation. Doing so
       // invalidates the tag in the address of `object`, so it must
       // be refreshed.
-      object = internal::TagMemoryRangeIncrement(object, slot_size);
+      if (UseRandomMemoryTagging()) {
+        // Exclude the previous tag so that immediate use after free is detected
+        // 100% of the time.
+        uint8_t previous_tag = internal::ExtractTagFromPtr(object);
+        object = internal::TagMemoryRangeRandomly(object, slot_size,
+                                                  1 << previous_tag);
+      } else {
+        object = internal::TagMemoryRangeIncrement(object, slot_size);
+      }
     }
   }
 #else   // PA_BUILDFLAG(HAS_MEMORY_TAGGING)
