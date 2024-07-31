408a096c228508572a2df544039a05ea33ae5718
Justin Lulejian <jlulejian@chromium.org>
2024-07-31 17:40:40 +0000

    [ServiceWorker] Emit metric for extension script fetch network fails.
    New metric:
      Extensions.ServiceWorkerBackground.WorkerScriptFetchNetError
    
    Before this change blink::ServiceWorkerBackground::kErrorNetwork is a
    catch-all error returned for extension service worker script fetch
    failure. This is confusing because extension script fetches are meant
    to go to disk so a net error is unexpected.
    
    In order to determine what, of the many, net::Errors //extensions is
    encountering to get blink::ServiceWorkerBackground::kErrorNetwork --
    this emits the above metric.
    
    Once this is known it will point more specifically to what path in
    ExtensionURLLoader is bring encountered during the script fetch error.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This change contains only minor metrics changes that do not affect worker registration logic and are likely temporary until we gather the data.
    Bug: 346732739
    Change-Id: I8a078f2dc9720a809a892a8b89dcabb61c70611f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750936
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335541}

--- content/browser/service_worker/service_worker_register_job.cc
+++ content/browser/service_worker/service_worker_register_job.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
@@ -535,9 +536,17 @@ void ServiceWorkerRegisterJob::OnScriptFetchCompleted(
         version->script_cache_map()->main_script_status_message();
     if (message.empty())
       message = ServiceWorkerConsts::kServiceWorkerFetchScriptError;
-    Complete(version->DeduceStartWorkerFailureReason(
-                 blink::ServiceWorkerStatusCode::kErrorFailed),
-             message);
+    blink::ServiceWorkerStatusCode script_fetch_status_code =
+        version->DeduceStartWorkerFailureReason(
+            blink::ServiceWorkerStatusCode::kErrorFailed);
+    Complete(script_fetch_status_code, message);
+    if (script_fetch_status_code ==
+            blink::ServiceWorkerStatusCode::kErrorNetwork &&
+        version->scope().SchemeIs("chrome-extension")) {
+      base::UmaHistogramSparse(
+          "Extensions.ServiceWorkerBackground.WorkerScriptFetchNetError",
+          (int)version->GetMainScriptNetError());
+    }
     return;
   }
 
--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -2689,6 +2689,10 @@ ServiceWorkerVersion::DeduceStartWorkerFailureReason(
   return default_code;
 }
 
+net::Error ServiceWorkerVersion::GetMainScriptNetError() {
+  return net::Error(script_cache_map()->main_script_net_error());
+}
+
 void ServiceWorkerVersion::MarkIfStale() {
   if (!context_)
     return;
