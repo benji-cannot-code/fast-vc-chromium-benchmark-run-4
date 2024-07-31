ca62e90d9fdddf82ae2d0dcbe9b8d820c72f7533
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-31 22:34:31 +0000

    Add BuildInfo::set_gms_version_code_for_test() (part 3/3)
    This is part of a three-sided patch to allow overriding the GMS
    version code in tests.
    https://crrev.com/c/5752470 (1/3)
    https://crrev.com/i/7545924 (2/3)
    https://crrev.com/c/5752697 (3/3)
    
    This part turns the public final gmsVersionCode member into a private
    non-final mGmsVersionCode, and exposes
    set_gms_version_code_for_test() in C++ which can set it.
    As for the corresponding C++ member, base::android::BuildInfo docs [1]
    claim that it must be of type const char* and not std::string, so it
    can still be accessed in case of crash. So the new
    set_gms_version_code_for_test() takes a const std::string& and
    strdups the argument, just like the production code.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:base/android/build_info.h;l=183;drc=dfea6925f8d6ee75e3e45c0a45b3d6f7e974d562
    
    Bug: 346556567
    Change-Id: I7e6e91b9070383bbd4ca1e8ff3afcaf3b6230278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752697
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335731}

--- base/android/java/src/org/chromium/base/BuildInfo.java
+++ base/android/java/src/org/chromium/base/BuildInfo.java
@@ -21,6 +21,8 @@ import android.os.Process;
 import android.text.TextUtils;
 
 import org.jni_zero.CalledByNative;
+import org.jni_zero.CalledByNativeForTesting;
+import org.jni_zero.JniType;
 
 import org.chromium.build.BuildConfig;
 
@@ -78,9 +80,6 @@ public class BuildInfo {
     /** Result of PackageManager.getInstallerPackageName(). Never null, but may be "". */
     public final String installerPackageName;
 
-    /** The versionCode of Play Services (for crash reporting). */
-    public final String gmsVersionCode;
-
     /** Formatted ABI string (for crash reporting). */
     public final String abiString;
 
@@ -114,6 +113,9 @@ public class BuildInfo {
     @GuardedBy("mCertLock")
     private String mHostSigningCertSha256;
 
+    /** The versionCode of Play Services. Can be overridden in tests. */
+    private String mGmsVersionCode;
+
     private Object mCertLock = new Object();
 
     private static class Holder {
@@ -128,6 +130,11 @@ public class BuildInfo {
     @CalledByNative
     private static String lazyGetHostSigningCertSha256() {
         return BuildInfo.getInstance().getHostSigningCertSha256();
+    }
+
+    @CalledByNativeForTesting
+    private static void setGmsVersionCodeForTest(@JniType("std::string") String gmsVersionCode) {
+        getInstance().mGmsVersionCode = gmsVersionCode;
     }
 
     /** Returns a serialized string array of all properties of this class. */
@@ -149,7 +156,7 @@ public class BuildInfo {
             String.valueOf(versionCode),
             versionName,
             androidBuildFingerprint,
-            gmsVersionCode,
+            mGmsVersionCode,
             installerPackageName,
             abiString,
             customThemes,
@@ -214,7 +221,7 @@ public class BuildInfo {
 
     /** The versionCode of Play Services. */
     public String getGmsVersionCode() {
-        return gmsVersionCode;
+        return mGmsVersionCode;
     }
 
     private BuildInfo() {
@@ -311,7 +318,7 @@ public class BuildInfo {
         installerPackageName = nullToEmpty(pm.getInstallerPackageName(appInstalledPackageName));
 
         PackageInfo gmsPackageInfo = PackageUtils.getPackageInfo("com.google.android.gms", 0);
-        gmsVersionCode =
+        mGmsVersionCode =
                 gmsPackageInfo != null
                         ? String.valueOf(packageVersionCode(gmsPackageInfo))
                         : "gms versionCode not available.";
