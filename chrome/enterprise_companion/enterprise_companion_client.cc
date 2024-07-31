b4d16f4a4480af80c7f70cc180017947f5b8aec7
Noah Rose Ledesma <noahrose@google.com>
2024-07-31 18:42:17 +0000

    Add enable_usagestats argument to CECA dialer
    CECA picks up usage stats consent via a command line flag. Add an
    argument to ConnectAndLaunchServer to control whether this flag is set.
    O4 can use this to plumb its usage stats bit when initiating a
    connection.
    
    Bug: 344887294
    Change-Id: Id1d6c6cb413072dd0bf48f5965a58b083bc82ca8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753343
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335585}

--- chrome/enterprise_companion/enterprise_companion_client.cc
+++ chrome/enterprise_companion/enterprise_companion_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <utility>
 
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback.h"
 #include "base/logging.h"
@@ -30,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace enterprise_companion {
 
+const char kEnableUsageStatsSwitch[] = "enable-usage-stats";
+
 namespace {
 
 #if BUILDFLAG(IS_MAC)
@@ -41,12 +44,17 @@ constexpr char kServerName[] =
 constexpr wchar_t kServerName[] = PRODUCT_FULLNAME_STRING L"Service";
 #endif
 
-bool LaunchEnterpriseCompanionApp() {
+bool LaunchEnterpriseCompanionApp(bool enable_usagestats) {
   std::optional<base::FilePath> binary_path = FindExistingInstall();
   if (!binary_path) {
     return false;
   }
-  return base::LaunchProcess(base::CommandLine(*binary_path), {}).IsValid();
+
+  base::CommandLine command_line = base::CommandLine(*binary_path);
+  if (enable_usagestats) {
+    command_line.AppendSwitch(kEnableUsageStatsSwitch);
+  }
+  return base::LaunchProcess(command_line, {}).IsValid();
 }
 
 void OnEndpointReceived(
@@ -75,6 +83,7 @@ void ConnectWithRetries(
     const base::Clock* clock,
     int tries,
     base::Time deadline,
+    bool enable_usagestats,
     base::OnceCallback<void(mojo::PlatformChannelEndpoint)> callback) {
   if (clock->Now() > deadline) {
     VLOG(1) << "Failed to connect to EnterpriseCompanionService remote. "
@@ -83,7 +92,7 @@ void ConnectWithRetries(
     return;
   }
 
-  if (tries == 1 && !LaunchEnterpriseCompanionApp()) {
+  if (tries == 1 && !LaunchEnterpriseCompanionApp(enable_usagestats)) {
     VLOG(1) << "Failed to connect to EnterpriseCompanionService remote. "
                "The service could not be launched.";
     std::move(callback).Run({});
@@ -99,7 +108,7 @@ void ConnectWithRetries(
   base::ThreadPool::PostDelayedTask(
       FROM_HERE, {base::MayBlock()},
       base::BindOnce(&ConnectWithRetries, server_name, clock, tries + 1,
-                     deadline, std::move(callback)),
+                     deadline, enable_usagestats, std::move(callback)),
       base::Milliseconds(30 * tries));
 }
 
@@ -127,13 +136,14 @@ void ConnectToServer(
 void ConnectAndLaunchServer(
     const base::Clock* clock,
     base::TimeDelta timeout,
+    bool enable_usagestats,
     base::OnceCallback<void(std::unique_ptr<mojo::IsolatedConnection>,
                             mojo::Remote<mojom::EnterpriseCompanion>)> callback,
     const mojo::NamedPlatformChannel::ServerName& server_name) {
   base::ThreadPool::PostTask(
       FROM_HERE, {base::MayBlock()},
       base::BindOnce(&ConnectWithRetries, server_name, clock, /*tries=*/0,
-                     clock->Now() + timeout,
+                     clock->Now() + timeout, enable_usagestats,
                      base::BindPostTaskToCurrentDefault(base::BindOnce(
                          &OnEndpointReceived, std::move(callback)))));
 }
