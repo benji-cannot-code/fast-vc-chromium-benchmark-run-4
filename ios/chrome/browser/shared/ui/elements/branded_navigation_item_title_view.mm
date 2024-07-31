1931701708b79c29313f1e6f472326be84834cd8
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-07-31 10:10:17 +0000

    [iOS] Move the BrandedNavItemTitleView to be a shared UI element
    Bug: 355630358
    Change-Id: I41e58a848aeef33ef9b8cf9e7e1e6db7d94e7bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743636
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335348}

--- ios/chrome/browser/shared/ui/elements/branded_navigation_item_title_view.mm
+++ ios/chrome/browser/shared/ui/elements/branded_navigation_item_title_view.mm
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2022 The Chromium Authors
+// Copyright 2024 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/settings/password/branded_navigation_item_title_view.h"
+#import "ios/chrome/browser/shared/ui/elements/branded_navigation_item_title_view.h"
 
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/public/provider/chrome/browser/font/font_api.h"
--- ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_view_controller.mm
+++ ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_view_controller.mm
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/check.h"
 #import "base/metrics/histogram_macros.h"
+#import "ios/chrome/browser/shared/ui/elements/branded_navigation_item_title_view.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
-#import "ios/chrome/browser/ui/settings/password/branded_navigation_item_title_view.h"
 #import "ios/chrome/browser/ui/settings/password/create_password_manager_title_view.h"
 #import "ios/chrome/browser/ui/settings/password/reauthentication/reauthentication_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
