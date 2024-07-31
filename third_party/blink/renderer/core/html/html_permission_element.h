16937d765b537772ee41268142eb7bc488dca487
Takuto Ikuta <tikuta@chromium.org>
2024-07-31 07:26:33 +0000

    third_party/blink/renderer: remove unnecessary build deps
    Fixed: 354107461
    Change-Id: I2f2a069283e643d3f2491c418d762d0a6e49e501
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751863
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335288}

--- third_party/blink/renderer/core/html/html_permission_element.h
+++ third_party/blink/renderer/core/html/html_permission_element.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/intersection_observer/intersection_observer.h"
-#include "third_party/blink/renderer/core/page/page.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_vector.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_receiver_set.h"
@@ -29,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/vector.h"
 
 namespace blink {
+
+class Page;
 
 class CORE_EXPORT HTMLPermissionElement final
     : public HTMLElement,
