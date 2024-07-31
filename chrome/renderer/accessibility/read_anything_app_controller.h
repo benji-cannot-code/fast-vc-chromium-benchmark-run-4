5263449fcff792ed68d92b75c4b5145bba9d007b
Jacob Francis <francisjp@google.com>
2024-07-31 23:14:34 +0000

    Reland "[Read Anything] Update to use site isolation safe path"
    This is a reland of commit 43fc038118afa50faf2ff51d3ee587dfcfb5d6a9
    
    Original change's description:
    > [Read Anything] Update to use site isolation safe path
    >
    > - This CL updates the reading mode images code to use the new image
    >   downloader via ax node id.
    >
    > - A work around for hiding the canvases was also added since there was
    >   an issue with the automatic css in the html file.
    >
    > Bug: 328148648
    > Change-Id: I8192a1a38cd06392f343fc926a942ae0d594e221
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725760
    > Auto-Submit: Jacob Francis <francisjp@google.com>
    > Reviewed-by: David Tseng <dtseng@chromium.org>
    > Reviewed-by: Chris Bookholt <bookholt@chromium.org>
    > Commit-Queue: Jacob Francis <francisjp@google.com>
    > Reviewed-by: Chris Bookholt <bookholt@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1334374}
    
    Bug: 328148648
    Change-Id: Icf3c5b2dc20261b6a65302a11eaa7cf146253d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750065
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Auto-Submit: Jacob Francis <francisjp@google.com>
    Commit-Queue: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335750}

--- chrome/renderer/accessibility/read_anything_app_controller.h
+++ chrome/renderer/accessibility/read_anything_app_controller.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/accessibility/ax_node_id_forward.h"
 #include "ui/accessibility/ax_node_position.h"
@@ -114,6 +115,9 @@ class ReadAnythingAppController
                                ukm::SourceId ukm_source_id,
                                bool is_pdf) override;
   void OnAXTreeDestroyed(const ui::AXTreeID& tree_id) override;
+  void OnImageDataDownloaded(const ui::AXTreeID& tree_id,
+                             ui::AXNodeID node_id,
+                             const SkBitmap& image) override;
   void OnSettingsRestoredFromPrefs(
       read_anything::mojom::LineSpacing line_spacing,
       read_anything::mojom::LetterSpacing letter_spacing,
@@ -223,6 +227,7 @@ class ReadAnythingAppController
   std::vector<std::string> GetSupportedFonts();
   void RequestImageDataUrl(ui::AXNodeID node_id) const;
   std::string GetImageDataUrl(ui::AXNodeID node_id) const;
+  v8::Local<v8::Value> GetImageBitmap(ui::AXNodeID node_id);
   void OnSpeechPlayingStateChanged(bool is_speech_active);
   std::string GetValidatedFontName(const std::string& font) const;
   std::vector<std::string> GetAllFonts();
@@ -359,6 +364,8 @@ class ReadAnythingAppController
   mojo::Remote<read_anything::mojom::UntrustedPageHandler> page_handler_;
   mojo::Receiver<read_anything::mojom::UntrustedPage> receiver_{this};
 
+  std::map<ui::AXNodeID, SkBitmap> downloaded_images_;
+
   // Model that holds Read Aloud state for this controller.
   ReadAloudAppModel read_aloud_model_;
 
@@ -370,7 +377,6 @@ class ReadAnythingAppController
   ReadAnythingAppModel model_;
 
   // For metrics logging
-
   std::unique_ptr<ukm::MojoUkmRecorder> ukm_recorder_;
 
   // The time when the renderer constructor is first triggered.
