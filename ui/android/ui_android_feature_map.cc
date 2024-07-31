629b0a25d9a06230a1ecb3dbba5c09180c369a4e
Joel Hockey <joelhockey@chromium.org>
2024-07-31 06:55:05 +0000

    Use ACTION_OPEN_DOCUMENT and persist granted permissions
    This change adds support to use ACTION_OPEN_DOCUMENT rather than
    ACTION_GET_CONTENT when selecting a file.  It is plan to be run as
    a 50/50 experiment to test.
    
    ACTION_OPEN_DOCUMENT is more suitable than ACTION_GET_CONTENT since
    Android 4.4 to allow an app to have long-term, persistent access.
    
    In particular, DocumentsUI will provide write access when using
    ACTION_OPEN_DOCUMENT vs ACTION_GET_CONTENT.
    
    https://developer.android.com/guide/topics/providers/document-provider
    
    If read or write permissions were granted, we will persist them so
    that files can be used again in the next chrome session without
    needing to select the file again. This is useful for webapps that use
    the chrome FileSystem APIs.
    
    Low-Coverage-Reason: HARD_TO_TEST logging in exception handling not easily covered in tests
    Bug: 40101963, b/355066738
    Change-Id: Ib12e810f7bfb795433fec6e0d2b22a5d66a2c3b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5736615
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335271}

--- ui/android/ui_android_feature_map.cc
+++ ui/android/ui_android_feature_map.cc
@@ -24,6 +24,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &ui::kMirrorBackForwardGesturesInRTL,
     &ui::kReportAllAvailablePointerTypes,
     &ui::kRequireLeadingInTextViewWithLeading,
+    &ui::kSelectFileOpenDocument,
 };
 
 // static
