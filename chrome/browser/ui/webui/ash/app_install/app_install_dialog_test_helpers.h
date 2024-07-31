4e4383f37b3cddfd7d95ebd2ee89cee9d0b399f8
Tim Sergeant <tsergeant@chromium.org>
2024-07-31 07:09:24 +0000

    AIS: Extract App Install Dialog test helpers for use in other AIS tests
    This allows us to verify that the app install dialog opens in an error
    state from tests in the App Install Service directory.
    
    While I'm there, also updates the App Install Dialog tests to use the
    shared test server.
    
    Bug: b/334733649
    Change-Id: Ib953eab4685e20d095fe840f609f235d78753fb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741326
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335277}

---
+++ chrome/browser/ui/webui/ash/app_install/app_install_dialog_test_helpers.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_APP_INSTALL_APP_INSTALL_DIALOG_TEST_HELPERS_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_APP_INSTALL_APP_INSTALL_DIALOG_TEST_HELPERS_H_
+
+#include <optional>
+#include <string>
+
+namespace content {
+class WebContents;
+}
+
+namespace ash::app_install {
+
+// Returns the WebContents currently being displayed in an App Install Dialog,
+// or nullptr if there is no dialog open.
+content::WebContents* GetWebContentsFromDialog();
+
+std::string GetDialogTitle(content::WebContents* web_contents);
+
+std::optional<std::string> GetDialogActionButton(
+    content::WebContents* web_contents);
+
+[[nodiscard]] bool ClickDialogActionButton(content::WebContents* web_contents);
+
+}  // namespace ash::app_install
+
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_APP_INSTALL_APP_INSTALL_DIALOG_TEST_HELPERS_H_
