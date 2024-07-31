0d3a5bd7f1740069b653a6009648eb5db9f5361f
Darren Shen <shend@chromium.org>
2024-07-31 05:13:24 +0000

    Add test for feature tour accessibility.
    Bug: b:353730051
    Change-Id: Ib98b0db73b695f76a5c1e803b161114bfc054a3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741150
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335241}

--- chrome/browser/ui/ash/picker/picker_accessibility_browsertest.cc
+++ chrome/browser/ui/ash/picker/picker_accessibility_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/views/picker_emoji_bar_view.h"
 #include "ash/picker/views/picker_emoji_item_view.h"
 #include "ash/picker/views/picker_emoticon_item_view.h"
+#include "ash/picker/views/picker_feature_tour.h"
 #include "ash/picker/views/picker_item_with_submenu_view.h"
 #include "ash/picker/views/picker_key_event_handler.h"
 #include "ash/picker/views/picker_list_item_view.h"
@@ -26,8 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/icon_button.h"
 #include "ash/test/test_widget_builder.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/accessibility/speech_monitor.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/browsertest_util.h"
@@ -661,6 +665,24 @@ IN_PROC_BROWSER_TEST_F(PickerAccessibilityBrowserTest,
   });
   sm_.ExpectSpeechPattern("No results found.");
   sm_.ExpectSpeechPattern("Status");
+  sm_.Replay();
+}
+
+IN_PROC_BROWSER_TEST_F(PickerAccessibilityBrowserTest,
+                       ShowingFeatureTourAnnouncesContents) {
+  ash::PickerFeatureTour feature_tour;
+
+  sm_.Call([this, &feature_tour]() {
+    feature_tour.MaybeShowForFirstUse(browser()->profile()->GetPrefs(),
+                                      base::DoNothing());
+  });
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  sm_.ExpectSpeechPattern("*insert content*");
+#endif
+  sm_.ExpectSpeechPattern("Dialog");
+  sm_.ExpectSpeechPattern("Get started");
+  sm_.ExpectSpeechPattern("Button");
   sm_.Replay();
 }
 
