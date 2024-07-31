a25127fff67ed7060b72aee7c3f2a75b7d71eccc
Chris Fredrickson <cfredric@chromium.org>
2024-07-31 20:08:13 +0000

    Migrate to optional-based GetHeader overload
    HttpRequestHeaders::GetHeader has a new overload
    which returns an optional instead of using an out-
    parameter. I'm migrating callers and will eventually
    delete the old version of the method.
    
    This CL was uploaded by git cl split.
    
    R=csharrison@chromium.org
    
    Bug: 355451174
    Change-Id: I742bde6222de74a2e73e2390ccc8f7cf170289e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749706
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335648}

--- services/network/attribution/attribution_request_helper_unittest.cc
+++ services/network/attribution/attribution_request_helper_unittest.cc
@@ -207,9 +207,9 @@ TEST_F(AttributionRequestHelperTest, SetAttributionReportingHeaders) {
     resource_request.attribution_reporting_eligibility = test_case.eligibility;
     SetAttributionReportingHeaders(*request, resource_request);
 
-    std::string actual;
-    request->extra_request_headers().GetHeader(kAttributionReportingEligible,
-                                               &actual);
+    std::string actual = request->extra_request_headers()
+                             .GetHeader(kAttributionReportingEligible)
+                             .value_or(std::string());
 
     auto dict = net::structured_headers::ParseDictionary(actual);
     EXPECT_TRUE(dict.has_value());
@@ -258,9 +258,9 @@ TEST_F(AttributionCrossAppWebRequestHelperTest,
     resource_request.attribution_reporting_support = test_case.support;
     SetAttributionReportingHeaders(*request, resource_request);
 
-    std::string actual;
-    request->extra_request_headers().GetHeader(kAttributionReportingEligible,
-                                               &actual);
+    std::string actual = request->extra_request_headers()
+                             .GetHeader(kAttributionReportingEligible)
+                             .value_or(std::string());
 
     auto dict = net::structured_headers::ParseDictionary(actual);
     EXPECT_TRUE(dict.has_value());
