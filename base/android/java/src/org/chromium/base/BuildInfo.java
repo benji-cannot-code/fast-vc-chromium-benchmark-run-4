5f60827d02ac330d849c1d37e0bc05ac3b5ccfb4
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-07-31 21:20:35 +0000

    Add BuildInfo::set_gms_version_code_for_test() (part 1/3)
    This is part of a three-sided patch to allow overriding the GMS
    version code in tests.
    https://crrev.com/c/5752470 (1/3)
    https://crrev.com/i/7545924 (2/3)
    https://crrev.com/c/5752697 (3/3)
    
    This part exposes a getGmsVersionCode() method on top of the public
    gmsVersionCode member, and migrates uses of the member to the
    method.
    
    Bug: 346556567
    Change-Id: Ia6fc09d3fb98c6adabd6c63696270bc2198fde77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752470
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335695}

--- base/android/java/src/org/chromium/base/BuildInfo.java
+++ base/android/java/src/org/chromium/base/BuildInfo.java
@@ -212,6 +212,11 @@ public class BuildInfo {
         return Holder.INSTANCE;
     }
 
+    /** The versionCode of Play Services. */
+    public String getGmsVersionCode() {
+        return gmsVersionCode;
+    }
+
     private BuildInfo() {
         sInitialized = true;
         Context appContext = ContextUtils.getApplicationContext();
