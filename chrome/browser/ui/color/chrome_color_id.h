ff4f997925e0e2e60278c3c1c4cc777561cba586
Amelie Schneider <amelies@google.com>
2024-07-31 11:57:56 +0000

    [Profiles] Match avatar icon's stroke color to toolbar icons
    These changes are behind the feature flag `kOutlineSilhouetteIcon`.
    
    When using the dark mode, change the placeholder avatar icon's stroke
    color to match the color of the other toolbar icons. Also adapt the
    icon's background accordingly if the contrast would be too small.
    The icon's colors in light mode remain unchanged.
    
    Screenshots:
    - Default theme: https://screenshot.googleplex.com/4WF7KuToHTQeJnP
    - "Black & White" theme: https://screenshot.googleplex.com/JQdHRVbps9uPHht
    
    Bug: b:307739359
    
    Change-Id: Iea872aef4b069a820a9e87a2a1e18f911d998419
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741507
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Amelie Schneider <amelies@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335378}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -44,6 +44,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorAvatarButtonIncognitoHover) \
   E_CPONLY(kColorAvatarButtonNormalRipple) \
   E_CPONLY(kColorAvatarStrokeLight) \
+  E_CPONLY(kColorAvatarStroke) \
+  E_CPONLY(kColorAvatarFillForContrast) \
   /* Bookmark bar colors. */ \
   E_CPONLY(kColorBookmarkBarBackground) \
   E_CPONLY(kColorBookmarkBarForeground) \
