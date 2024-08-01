f2beb812f46a86a2813d327b774889da9f7115c8
Peter Kotwicz <pkotwicz@chromium.org>
2024-08-01 16:58:56 +0000

    Move Java digital_credentials code to dedicated directory
    BUG=None
    
    Change-Id: I79a7174c845b212389d893fba229de59dbe3b0e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753150
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336100}

--- chrome/browser/ui/android/digital_credentials/java/src/org/chromium/chrome/browser/ui/android/digital_credentials/DigitalIdentitySafetyInterstitialIntegrationTest.java
+++ chrome/browser/ui/android/digital_credentials/java/src/org/chromium/chrome/browser/ui/android/digital_credentials/DigitalIdentitySafetyInterstitialIntegrationTest.java
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.ui.android.webid;
+package org.chromium.chrome.browser.ui.android.digital_credentials;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
--- chrome/browser/ui/digital_credentials/digital_identity_safety_interstitial_bridge_android.cc
+++ chrome/browser/ui/digital_credentials/digital_identity_safety_interstitial_bridge_android.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "url/origin.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/browser/ui/android/webid/jni_headers/DigitalIdentitySafetyInterstitialBridge_jni.h"
+#include "chrome/browser/ui/android/digital_credentials/jni_headers/DigitalIdentitySafetyInterstitialBridge_jni.h"
 
 using base::android::AttachCurrentThread;
 using base::android::ScopedJavaLocalRef;
