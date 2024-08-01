849863aeddfd714f306ac3723434d666c9652df7
Pooja Patel <patelpooja@microsoft.com>
2024-08-01 01:56:16 +0000

    [WebNN] Add experimental feature flag
    So users can locally enable in-development WebNN features,
    this adds a new mojo flag as well as a runtime/blink feature flag,
    both named ExperimentalMachineLearningNeuralNetwork. An entry has been
    added to runtime_features.cc so that the
    webnn::mojom::features::kExperimentalWebMachineLearningNeuralNetwork
    flag is mapped to the new blink flag in kSetOnlyOverridden mode.
    A new entry to enable experimental features has been added to
    about://flags.
    
    The existing WebNN blink feature flag, MachineLearningNeuralNetwork,
    has been mapped to the existing
    webnn::mojom::features::kWebMachineLearningNeuralNetwork flag in
    kSetOnlyOverridden mode. For this blink feature flag, the status entry
    for it remains empty, though a origin_trial_feature_name entry should
    be added in the future when the original trial is approved.
    
    Change-Id: I381260f46d884836f486e9ffa1cc616c56f145fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742586
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Pooja Patel <patelpooja@microsoft.com>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335824}

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -390,7 +390,8 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
           {"LegacyWindowsDWriteFontFallback",
            raw_ref(features::kLegacyWindowsDWriteFontFallback)},
           {"MachineLearningNeuralNetwork",
-           raw_ref(webnn::mojom::features::kWebMachineLearningNeuralNetwork)},
+           raw_ref(webnn::mojom::features::kWebMachineLearningNeuralNetwork),
+           kSetOnlyIfOverridden},
           {"OriginIsolationHeader", raw_ref(features::kOriginIsolationHeader)},
           {"ReduceAcceptLanguage",
            raw_ref(network::features::kReduceAcceptLanguage)},
@@ -413,6 +414,10 @@ void SetRuntimeFeaturesFromChromiumFeatures() {
            raw_ref(features::kMediaStreamTrackTransfer)},
           {"PrivateNetworkAccessPermissionPrompt",
            raw_ref(network::features::kPrivateNetworkAccessPermissionPrompt),
+           kSetOnlyIfOverridden},
+          {"ExperimentalMachineLearningNeuralNetwork",
+           raw_ref(webnn::mojom::features::
+                       kExperimentalWebMachineLearningNeuralNetwork),
            kSetOnlyIfOverridden}};
   for (const auto& mapping : runtimeFeatureNameToChromiumFeatureMapping) {
     SetRuntimeFeatureFromChromiumFeature(
