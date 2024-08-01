b00d64a0d1e038f5bfe01be20613b1d2c5a72976
Jan Keitel <jkeitel@google.com>
2024-08-01 05:40:33 +0000

    Fix unsafe buffers warnings in AtomicFlagSet.
    Bug: 40284755
    Change-Id: Iafa48333889beb5ac1585cf336ec8652c66512a3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752602
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1335872}

--- base/task/sequence_manager/atomic_flag_set.h
+++ base/task/sequence_manager/atomic_flag_set.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_TASK_SEQUENCE_MANAGER_ATOMIC_FLAG_SET_H_
 #define BASE_TASK_SEQUENCE_MANAGER_ATOMIC_FLAG_SET_H_
 
+#include <array>
 #include <atomic>
 #include <memory>
 
@@ -14,9 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/task/sequence_manager/associated_thread_id.h"
 
-namespace base {
-namespace sequence_manager {
-namespace internal {
+namespace base::sequence_manager::internal {
 
 // This class maintains a set of AtomicFlags which can be activated or
 // deactivated at any time by any thread. When a flag is created a callback is
@@ -101,7 +100,7 @@ class BASE_EXPORT AtomicFlagSet {
 
     std::atomic<size_t> flags = {0};
     size_t allocated_flags = 0;
-    RepeatingClosure flag_callbacks[kNumFlags];
+    std::array<RepeatingClosure, kNumFlags> flag_callbacks;
     raw_ptr<Group> prev = nullptr;
     std::unique_ptr<Group> next;
     raw_ptr<Group> partially_free_list_prev = nullptr;
@@ -113,11 +112,11 @@ class BASE_EXPORT AtomicFlagSet {
 
     // Returns the index of the first unallocated flag. Must not be called when
     // all flags are set.
-    int FindFirstUnallocatedFlag() const;
+    size_t FindFirstUnallocatedFlag() const;
 
     // Computes the index of the |flag_callbacks| based on the number of leading
     // zero bits in |flag|.
-    static int IndexOfFirstFlagSet(size_t flag);
+    static size_t IndexOfFirstFlagSet(size_t flag);
   };
 
  private:
@@ -136,8 +135,6 @@ class BASE_EXPORT AtomicFlagSet {
   raw_ptr<Group> partially_free_list_head_ = nullptr;
 };
 
-}  // namespace internal
-}  // namespace sequence_manager
-}  // namespace base
+}  // namespace base::sequence_manager::internal
 
 #endif  // BASE_TASK_SEQUENCE_MANAGER_ATOMIC_FLAG_SET_H_
