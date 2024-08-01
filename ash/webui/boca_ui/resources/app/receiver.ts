c836e9c7c0856d0cd33290d40b2301d0f4bdf339
Benjamin Zielinski <bzielinski@google.com>
2024-08-01 23:06:03 +0000

    [boca] Fix color change updater
    This change makes sure the app loads the script which inits the color
    change updater.
    
    Bug: b/356927836
    Change-Id: I712cf76b60e1ce8d9747a4055b998bd76a6068ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758538
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336322}

--- ash/webui/boca_ui/resources/app/receiver.ts
+++ ash/webui/boca_ui/resources/app/receiver.ts
@@ -55,6 +55,4 @@ window.addEventListener('DOMContentLoaded', () => {
   // In that case, observe <body> until it is.
   const observer = new MutationObserver(mutationCallback);
   observer.observe(document.body, {childList: true});
-
-  ColorChangeUpdater.forDocument().start();
 });
