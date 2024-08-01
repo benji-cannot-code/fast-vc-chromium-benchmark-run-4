16dde5215f7046cc6b8099f0a7f9d28db0bcc0f3
Chris Fredrickson <cfredric@chromium.org>
2024-08-01 22:25:17 +0000

    Migrate to optional-based GetHeader overload
    HttpRequestHeaders::GetHeader has a new overload
    which returns an optional instead of using an out-
    parameter. I'm migrating callers and will eventually
    delete the old version of the method.
    
    This CL was uploaded by git cl split.
    
    R=dtrainor@chromium.org
    
    Bug: 355451174
    Change-Id: Ic7ccfc149717a7a09d9f05b22915b0821112c570
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749324
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336309}

--- components/download/internal/background_service/proto_conversions_unittest.cc
+++ components/download/internal/background_service/proto_conversions_unittest.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "components/download/internal/background_service/proto_conversions.h"
+
 #include <memory>
 #include <utility>
 
 #include "base/uuid.h"
 #include "components/download/internal/background_service/entry.h"
-#include "components/download/internal/background_service/proto_conversions.h"
 #include "components/download/internal/background_service/test/entry_utils.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -116,11 +117,8 @@ TEST_F(ProtoConversionsTest, RequestParamsWithHeadersConversion) {
   EXPECT_EQ(expected.require_safety_checks, actual.require_safety_checks);
   EXPECT_EQ(expected.credentials_mode, actual.credentials_mode);
 
-  std::string out;
-  actual.request_headers.GetHeader("key1", &out);
-  EXPECT_EQ("value1", out);
-  actual.request_headers.GetHeader("key2", &out);
-  EXPECT_EQ("value2", out);
+  EXPECT_EQ("value1", actual.request_headers.GetHeader("key1"));
+  EXPECT_EQ("value2", actual.request_headers.GetHeader("key2"));
   EXPECT_EQ(expected.request_headers.ToString(),
             actual.request_headers.ToString());
 }
