0027ff1bf21e61cf44c937af698d84540c107d8c
Eric Ekey <ericekey@google.com>
2024-08-01 16:57:46 +0000

    [iOS] Refactor FRE Default Browser Promo Implementation
    Expands utils headers for Segmented Default Browser promos and unit
    tests. Some refactoring for readability and ease-of-use.
    
    Fixed: 356622130
    Change-Id: Ia5f85bc620682952e05d3b751d072b18f11f94a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752550
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Eric Ekey <ericekey@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336098}

--- ios/chrome/browser/first_run/ui_bundled/default_browser/default_browser_screen_coordinator.mm
+++ ios/chrome/browser/first_run/ui_bundled/default_browser/default_browser_screen_coordinator.mm
@@ -79,6 +79,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _viewController = nil;
   _delegate = nil;
   _mediator.consumer = nil;
+  [_mediator disconnect];
   _mediator = nil;
 
   [super stop];
