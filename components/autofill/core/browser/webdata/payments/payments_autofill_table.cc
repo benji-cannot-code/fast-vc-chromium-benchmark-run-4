c0b036314902aba9d057cbefca7a03a96a207e0f
Justin Wells <justinleewells@google.com>
2024-08-01 14:08:02 +0000

    Add DropColumnIfExists to autofill_table_utils
    Change-Id: Iab64959f01bcd12453237bdc692217d955a96dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753982
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Justin Wells <justinleewells@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336023}

--- components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
+++ components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
@@ -2134,10 +2134,8 @@ bool PaymentsAutofillTable::
 
 bool PaymentsAutofillTable::
     MigrateToVersion131RemoveGenericPaymentInstrumentTypeColumn() {
-  return !DoesColumnExist(db_, kGenericPaymentInstrumentsTable,
-                          "payment_instrument_type") ||
-         DropColumn(db_, kGenericPaymentInstrumentsTable,
-                    "payment_instrument_type");
+  return DropColumnIfExists(db_, kGenericPaymentInstrumentsTable,
+                            "payment_instrument_type");
 }
 
 void PaymentsAutofillTable::AddMaskedCreditCards(
