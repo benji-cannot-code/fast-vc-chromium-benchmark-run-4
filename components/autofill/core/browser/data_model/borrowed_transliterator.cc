32d79551ff1f44b13dad3e9f18bf1d9d5f35096a
Norge Vizcay <vizcay@google.com>
2024-08-01 14:36:18 +0000

    Prepare BorrowedTransliterator ahead of German transliteration.
    The CL cleans up a little BorrowedTransliterator ahead of the
    introduction of German transliteration:
    - Adds TODO item.
    - Move comments to appropriate positions.
    - Use references instead of pointers for out parameters.
    
    Bug: 328968064
    Change-Id: I7996f4943df6a52d8b10d6c35b5e0d28b0a87e78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5755906
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336035}

--- components/autofill/core/browser/data_model/borrowed_transliterator.cc
+++ components/autofill/core/browser/data_model/borrowed_transliterator.cc
@@ -16,11 +16,11 @@ BorrowedTransliterator::BorrowedTransliterator() : auto_lock_(GetLock()) {}
 
 BorrowedTransliterator::~BorrowedTransliterator() = default;
 
-void BorrowedTransliterator::Transliterate(icu::UnicodeString* text) const {
+void BorrowedTransliterator::Transliterate(icu::UnicodeString& text) const {
   if (GetTransliterator() != nullptr) {
-    GetTransliterator()->transliterate(*text);
+    GetTransliterator()->transliterate(text);
   } else {
-    *text = text->toLower();
+    text = text.toLower();
   }
 }
 
@@ -67,7 +67,7 @@ icu::Transliterator* BorrowedTransliterator::GetTransliterator() {
 std::u16string RemoveDiacriticsAndConvertToLowerCase(
     std::u16string_view value) {
   icu::UnicodeString result = icu::UnicodeString(value.data(), value.length());
-  BorrowedTransliterator().Transliterate(&result);
+  BorrowedTransliterator().Transliterate(result);
   return base::i18n::UnicodeStringToString16(result);
 }
 
