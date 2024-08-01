6e03889b521b97545ad8d9d8953aff0b3db8b939
Rupert Ben Wiser <bewise@chromium.org>
2024-08-01 10:33:06 +0000

    webview: Cache auto granted SAA results
    The statements made by websites are unlikely to change in an app's
    life cycle and the additional network call is costly to make for statements
    so caching the results.
    
    I had to split up the SAA auto grant tests now that the result is cached
    to force our tests to use a new cache.
    
    This will impact the StorageAccessRelation2 metric I just introduced.
    Given that I added it super recently, I am happy to make add a note
    to the existing metric rather than introducing another replacement
    metric again.
    
    The cache is also applying to the existing SAA checks regardless of
    auto grants but that is fine from our perspective because the cache
    takes up very little space and other SAA calls will just resolve to
    rejected.
    
    Keeping this cache in the permission manager means our aw_contents
    code only has to care about the logic of resolving a request.
    
    Bug: 355206388
    Change-Id: Icb44ba1f0fd9a71b721dad52af2bcbf9d0f16ea4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5748576
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Rupert Wiser <bewise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335960}

--- android_webview/browser/aw_permission_manager.cc
+++ android_webview/browser/aw_permission_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/logging.h"
 #include "base/notreached.h"
+#include "base/sequence_checker.h"
 #include "base/task/thread_pool.h"
 #include "components/permissions/permission_manager.h"
 #include "components/permissions/permission_util.h"
@@ -249,10 +250,18 @@ class AwPermissionManager::PendingRequest {
   bool cancelled_;
 };
 
+// Regarding the saa_cache_ size: 99% of WebView site visits fall
+// into this count (based on Android.WebView.SitesVisitedWeekly) in a week so we
+// will cache those for revisits but not going above it to avoid using up too
+// much memory for any heavy case. This will cache in memory so every app reload
+// will require a new request. This will also be duplicated across profiles
+// which may be a useful property in the future so we aren't going to design
+// around that.
 AwPermissionManager::AwPermissionManager(
     const AwContextPermissionsDelegate& context_delegate)
     : context_delegate_(context_delegate),
-      result_cache_(new LastRequestResultCache) {}
+      result_cache_(new LastRequestResultCache),
+      saa_cache_(10) {}
 
 AwPermissionManager::~AwPermissionManager() {
   CancelPermissionRequests();
@@ -262,6 +271,8 @@ void AwPermissionManager::RequestPermissions(
     content::RenderFrameHost* render_frame_host,
     const content::PermissionRequestDescription& request_description,
     base::OnceCallback<void(const std::vector<PermissionStatus>&)> callback) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
   auto const& permissions = request_description.permissions;
   if (permissions.empty()) {
     std::move(callback).Run(std::vector<PermissionStatus>());
@@ -383,10 +394,23 @@ void AwPermissionManager::RequestPermissions(
             render_frame_host->GetOutermostMainFrame()
                 ->GetLastCommittedOrigin();
 
-        delegate->RequestStorageAccess(
-            outer_origin,
-            base::BindOnce(&OnRequestResponse, weak_ptr_factory_.GetWeakPtr(),
-                           request_id, permissions[i]));
+        auto cached_value = saa_cache_->Get(outer_origin.Serialize());
+        if (cached_value != saa_cache_->end()) {
+          auto is_granted = cached_value->second ? PermissionStatus::GRANTED
+                                                 : PermissionStatus::DENIED;
+          pending_request_raw->SetPermissionStatus(permissions[i], is_granted);
+          break;
+        }
+
+        auto on_saa_response =
+            base::BindOnce(&CacheAutoSAA, weak_ptr_factory_.GetWeakPtr(),
+                           outer_origin)
+                .Then(base::BindOnce(&OnRequestResponse,
+                                     weak_ptr_factory_.GetWeakPtr(), request_id,
+                                     permissions[i]));
+
+        delegate->RequestStorageAccess(outer_origin,
+                                       std::move(on_saa_response));
         break;
       }
       case PermissionType::MIDI:
@@ -431,6 +455,27 @@ void AwPermissionManager::RequestPermissions(
 }
 
 // static
+bool AwPermissionManager::CacheAutoSAA(
+    const base::WeakPtr<AwPermissionManager>& manager,
+    const url::Origin& origin,
+    bool allowed) {
+  // All delegate functions should be cancelled when the manager runs
+  // destructor. Therefore |manager| should be always valid here.
+  CHECK(manager);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(manager->sequence_checker_);
+
+  DVLOG(1) << "Caching auto granted SAA result " << allowed << " for origin "
+           << origin;
+
+  manager->saa_cache_->Put({
+      origin.Serialize(),
+      allowed,
+  });
+
+  return allowed;
+}
+
+// static
 void AwPermissionManager::OnRequestResponse(
     const base::WeakPtr<AwPermissionManager>& manager,
     int request_id,
@@ -438,7 +483,7 @@ void AwPermissionManager::OnRequestResponse(
     bool allowed) {
   // All delegate functions should be cancelled when the manager runs
   // destructor. Therefore |manager| should be always valid here.
-  DCHECK(manager);
+  CHECK(manager);
 
   PermissionStatus status =
       allowed ? PermissionStatus::GRANTED : PermissionStatus::DENIED;
