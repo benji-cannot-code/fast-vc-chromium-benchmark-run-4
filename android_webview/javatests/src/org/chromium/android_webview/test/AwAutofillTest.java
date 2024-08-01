bad0d50f25319902ee23f2b1a8395c75510528c9
Mateusz Zbikowski <zbikowski@google.com>
2024-08-01 15:33:10 +0000

    Fix typos in HTML in AwAutofillTest
    These make for incorrect HTML - this currently works as the parser is quite permissive, but it's not guaranteed to always be the case.
    
    Change-Id: I0acc82f64085e7dad53646a4de89b182a47722f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756361
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Mateusz Zbikowski <zbikowski@google.com>
    Auto-Submit: Mateusz Zbikowski <zbikowski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336056}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwAutofillTest.java
@@ -868,7 +868,7 @@ public class AwAutofillTest extends AwParameterizedTest {
                         <input type='text' id='text1' name='username'
                             placeholder='placeholder@placeholder.com'
                             autocomplete='username name'>
-                        <input type='password' id='passwordid' name='passwordname'
+                        <input type='password' id='passwordid' name='passwordname'>
                         <input type='submit'>
                     </form>""");
         int cnt = 0;
@@ -926,7 +926,7 @@ public class AwAutofillTest extends AwParameterizedTest {
                         <input type='text' id='text1' name='username'
                             placeholder='placeholder@placeholder.com'
                             autocomplete='username name'>
-                        <input type='password' id='passwordid' name='passwordname'
+                        <input type='password' id='passwordid' name='passwordname'>
                         <input type='submit'>
                     </form>""");
         int cnt = 0;
@@ -1246,7 +1246,7 @@ public class AwAutofillTest extends AwParameterizedTest {
                 """
                     <form action='a.html' name='formname' id='formid'>
                         <input type='password' id='passwordid'
-                            name='passwordname' <input type='submit'>
+                            name='passwordname'> <input type='submit'>
                     </form>""");
         DOMUtils.waitForNonZeroNodeBounds(mAwContents.getWebContents(), "passwordid");
         // Note that we currently depend on keyboard app's behavior.
@@ -3737,7 +3737,7 @@ public class AwAutofillTest extends AwParameterizedTest {
     }
 
     /**
-     * Loads an HTML snippet which will be used by the test to execute JS commands on. This snipped
+     * Loads an HTML snippet which will be used by the test to execute JS commands on. This snippet
      * is loaded on the test web server.
      *
      * @param htmlBody The body of the HTML snippet to be loaded.
