51bdb9c207c01516eb5574cc632e8dfb9dea8290
Ryan Sultanem <rsult@google.com>
2024-08-01 11:16:49 +0000

    [Uno-D] Fix IGA crash caused by reading unregistered pref in IdentityManager
    This is a quick fix to unblock IGA the soonest, as the read pref is
    only needed for Desktop as part of the Uno project.
    
    Other potential solutions to consider for later:
    - Move the SigninPrefs registration as part of the IdentityManager pref
    registration process (currently registered at the chrome browser level).
    - Move setting the pref out of the IdentityManager, in
    ChromeSigninClient maybe.
    - Make sure SigninPrefs are registered for IGA (does not seem like this
    needed though, since we only need those prefs for Desktop currently).
    
    Fixed: b:356162085
    Change-Id: Id45e3abc1fee90a8b7e6525feedfed20b79b1e09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752804
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335971}

--- components/signin/internal/identity_manager/primary_account_manager_unittest.cc
+++ components/signin/internal/identity_manager/primary_account_manager_unittest.cc
@@ -211,12 +211,14 @@ TEST_F(PrimaryAccountManagerTest, SignOut) {
   CoreAccountId main_account_id =
       AddToAccountTracker("account_id", "user@gmail.com");
   AccountInfo account_info = account_tracker()->GetAccountInfo(main_account_id);
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   {
     SigninPrefs signin_prefs(*prefs());
     std::optional<base::Time> last_signout_time =
         signin_prefs.GetChromeLastSignoutTime(account_info.gaia);
     EXPECT_FALSE(last_signout_time.has_value());
   }
+#endif
   manager_->SetPrimaryAccountInfo(account_info, ConsentLevel::kSync,
                                   AccessPoint::ACCESS_POINT_UNKNOWN);
   CheckSigninMetrics({.sign_in = AccessPoint::ACCESS_POINT_UNKNOWN,
@@ -233,6 +235,7 @@ TEST_F(PrimaryAccountManagerTest, SignOut) {
                       .sync_opt_in = AccessPoint::ACCESS_POINT_UNKNOWN,
                       .sign_out = signin_metrics::ProfileSignout::kTest,
                       .turn_off_sync = signin_metrics::ProfileSignout::kTest});
+#if BUILDFLAG(ENABLE_DICE_SUPPORT)
   {
     SigninPrefs signin_prefs(*prefs());
     std::optional<base::Time> last_signout_time =
@@ -240,6 +243,7 @@ TEST_F(PrimaryAccountManagerTest, SignOut) {
     ASSERT_TRUE(last_signout_time.has_value());
     EXPECT_LE(base::Time::Now() - last_signout_time.value(), base::Seconds(10));
   }
+#endif
 
   // Should not be persisted anymore
   ShutDownManager();
