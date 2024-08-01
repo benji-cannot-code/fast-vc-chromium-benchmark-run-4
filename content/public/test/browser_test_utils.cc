4e087fd6f244cafc426e51d8b594c44551ba4b2d
Jiacheng Guo <gjc@google.com>
2024-08-01 08:14:24 +0000

    Add parameter to delay the speculative RFH creation and filter out page restores
    The CL adds a parameter to delay the speculative RFH creation so as not
    to interfere with the loading.
    The CL also adds a parameter to filter out page restores such as BFCache
    restore and prerender activation. The same function
    `GetFrameHostForNavigation` will be immediately called in
    `OnResponseStarted`.
    
    Bug: 332435024
    Change-Id: Ic302ea2c3002f2e86f711fea2b0e65df1cf02f33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5737130
    Commit-Queue: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335920}

--- content/public/test/browser_test_utils.cc
+++ content/public/test/browser_test_utils.cc
@@ -587,7 +587,7 @@ ProxyHostObserver* GetProxyHostObserver() {
 
 bool IsRequestCompatibleWithSpeculativeRFH(NavigationRequest* request) {
   return request->state() <=
-             NavigationRequest::NavigationState::WILL_START_REQUEST &&
+             NavigationRequest::NavigationState::WILL_PROCESS_RESPONSE &&
          request->GetAssociatedRFHType() ==
              NavigationRequest::AssociatedRenderFrameHostType::NONE;
 }
