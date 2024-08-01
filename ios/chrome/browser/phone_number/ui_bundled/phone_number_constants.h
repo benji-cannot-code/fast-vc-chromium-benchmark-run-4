43622cba88d80da4cfd13b6ed5f89f5ec4add368
Maksim Moskvitin <mmoskvitin@google.com>
2024-08-01 16:56:35 +0000

    [Code Health][iOS] Move ios/c/b/ui/phone_number to ui_bundled
    To simplify the folder hierarchy on iOS and have a clearer layer
    separation, all the files in ios/c/b/ui/<foo>/ will be moved to
    ios/c/b/<foo>/ui_bundled/ to showcase that they are model files.
    
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR only moving code
    Bug: 40223315, 324232651
    Change-Id: Ib6eb3e5264256578b88c00ed6f09e7f70e89b23a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756778
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336096}

--- ios/chrome/browser/phone_number/ui_bundled/phone_number_constants.h
+++ ios/chrome/browser/phone_number/ui_bundled/phone_number_constants.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_PHONE_NUMBER_PHONE_NUMBER_CONSTANTS_H_
-#define IOS_CHROME_BROWSER_UI_PHONE_NUMBER_PHONE_NUMBER_CONSTANTS_H_
+#ifndef IOS_CHROME_BROWSER_PHONE_NUMBER_UI_BUNDLED_PHONE_NUMBER_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_PHONE_NUMBER_UI_BUNDLED_PHONE_NUMBER_CONSTANTS_H_
 
 #import <Foundation/Foundation.h>
 
@@ -12,4 +12,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 extern NSString* const kCountryCodePickerTableViewIdentifier;
 extern NSString* const kPhoneNumberActionsViewIdentifier;
 
-#endif  // IOS_CHROME_BROWSER_UI_PHONE_NUMBER_PHONE_NUMBER_CONSTANTS_H_
+#endif  // IOS_CHROME_BROWSER_PHONE_NUMBER_UI_BUNDLED_PHONE_NUMBER_CONSTANTS_H_
