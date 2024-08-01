a07d2e6149dc102b2df9ae8f7b3149eb51067a2d
Di Zhang <dizhangg@chromium.org>
2024-08-01 03:35:15 +0000

    Remove MeterShadowElement
    MeterShadowElement was added to disable feature
    FormControlsVerticalWritingModeDirectionSupport. Since this feature
    was shipped in stable in 124, we can remove MeterShadowElement [1].
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5750707/comment/63ee0c38_c0e97f1b/
    
    Change-Id: I4b83cd2673632ef78a1094a704131acc04a06f64
    Fixed: 40263746
    Fixed: 40246748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754707
    Auto-Submit: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1335856}

--- third_party/blink/renderer/core/html/html_meter_element.cc
+++ third_party/blink/renderer/core/html/html_meter_element.cc
@@ -25,9 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/frame/web_feature.h"
+#include "third_party/blink/renderer/core/html/html_div_element.h"
 #include "third_party/blink/renderer/core/html/html_slot_element.h"
 #include "third_party/blink/renderer/core/html/parser/html_parser_idioms.h"
-#include "third_party/blink/renderer/core/html/shadow/meter_shadow_element.h"
 #include "third_party/blink/renderer/core/html/shadow/shadow_element_names.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
@@ -195,20 +195,20 @@ void HTMLMeterElement::DidElementStateChange() {
 void HTMLMeterElement::DidAddUserAgentShadowRoot(ShadowRoot& root) {
   DCHECK(!value_);
 
-  auto* inner = MakeGarbageCollected<MeterShadowElement>(GetDocument());
+  auto* inner = MakeGarbageCollected<HTMLDivElement>(GetDocument());
   inner->SetShadowPseudoId(shadow_element_names::kPseudoMeterInnerElement);
   root.AppendChild(inner);
 
-  auto* bar = MakeGarbageCollected<MeterShadowElement>(GetDocument());
+  auto* bar = MakeGarbageCollected<HTMLDivElement>(GetDocument());
   bar->SetShadowPseudoId(AtomicString("-webkit-meter-bar"));
 
-  value_ = MakeGarbageCollected<MeterShadowElement>(GetDocument());
+  value_ = MakeGarbageCollected<HTMLDivElement>(GetDocument());
   UpdateValueAppearance(0);
   bar->AppendChild(value_);
 
   inner->AppendChild(bar);
 
-  auto* fallback = MakeGarbageCollected<MeterShadowElement>(GetDocument());
+  auto* fallback = MakeGarbageCollected<HTMLDivElement>(GetDocument());
   fallback->AppendChild(MakeGarbageCollected<HTMLSlotElement>(GetDocument()));
   fallback->SetShadowPseudoId(AtomicString("-internal-fallback"));
   root.AppendChild(fallback);
