1c855964c70ccd1731646ee6c88c9711577a037d
Daniel Rubery <drubery@chromium.org>
2024-08-01 20:14:15 +0000

    Spanify extension_telemetry
    This gets rid of unsafe buffer usages in extension_telemetry. All usages
    were arrays in test code, which are now replaced with std::arrays.
    
    Bug: 356368033
    Change-Id: I8e28fb0e356a8dfb83a57572edc1412f514c91b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757778
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336223}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_unittest.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/356368033): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service.h"
 
 #include "base/command_line.h"
@@ -68,13 +63,13 @@ namespace safe_browsing {
 
 namespace {
 
-constexpr const char* kExtensionId[] = {
-    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
-    "cccccccccccccccccccccccccccccccc", "dddddddddddddddddddddddddddddddd",
-    "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"};
-constexpr const char* kExtensionName[] = {
-    "Test Extension 0", "Test Extension 1", "Test Extension 2",
-    "Test Extension 3", "Test Extension 4"};
+constexpr auto kExtensionId = std::to_array(
+    {"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+     "cccccccccccccccccccccccccccccccc", "dddddddddddddddddddddddddddddddd",
+     "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"});
+constexpr auto kExtensionName =
+    std::to_array({"Test Extension 0", "Test Extension 1", "Test Extension 2",
+                   "Test Extension 3", "Test Extension 4"});
 constexpr const char kExtensionVersion[] = "1";
 constexpr const char kScriptCode[] = "document.write('Hello World')";
 constexpr const char kCookieName[] = "cookie-1";
--- chrome/browser/safe_browsing/extension_telemetry/tabs_execute_script_signal_processor_unittest.cc
+++ chrome/browser/safe_browsing/extension_telemetry/tabs_execute_script_signal_processor_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/356368033): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/browser/safe_browsing/extension_telemetry/tabs_execute_script_signal_processor.h"
 
 #include <array>
@@ -28,8 +23,8 @@ using TabsExecuteScriptInfo =
 using ScriptInfo =
     ExtensionTelemetryReportRequest_SignalInfo_TabsExecuteScriptInfo_ScriptInfo;
 
-constexpr const char* kExtensionId[] = {"aaaaaaaabbbbbbbbccccccccdddddddd",
-                                        "eeeeeeeeffffffffgggggggghhhhhhhh"};
+constexpr auto kExtensionId = std::to_array(
+    {"aaaaaaaabbbbbbbbccccccccdddddddd", "eeeeeeeeffffffffgggggggghhhhhhhh"});
 
 struct ScriptData {
   explicit ScriptData(const std::string& script_code)
