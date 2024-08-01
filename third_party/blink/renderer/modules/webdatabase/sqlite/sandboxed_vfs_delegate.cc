eb71f9047bbaaadfed066adaa7250a19b7914a9f
Avi Drissman <avi@chromium.org>
2024-08-01 21:06:14 +0000

    Eliminate IPC no longer needed on macOS
    In macOS 10.14 and earlier, ftruncate was not allowed from the
    sandbox. Starting with macOS 10.15, it has been.
    
    To work around this issue, an IPC was added to Chromium to do an
    ftruncate outside of the sandbox on behalf of a renderer unable to do
    so due to this sandbox issue.
    
    Chromium requires macOS 10.15+, so this IPC is no longer needed.
    Therefore, remove it.
    
    Bug: 40693199
    Bug: 40254578
    Change-Id: I3405a26cdf507a5a1c26c265dfadcb738a2bfecb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756685
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ken Rockot <rockot@google.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336256}

--- third_party/blink/renderer/modules/webdatabase/sqlite/sandboxed_vfs_delegate.cc
+++ third_party/blink/renderer/modules/webdatabase/sqlite/sandboxed_vfs_delegate.cc
@@ -85,11 +85,4 @@ SandboxedVfsDelegate::GetPathAccess(const base::FilePath& file_path) {
   return access;
 }
 
-bool SandboxedVfsDelegate::SetFileLength(const base::FilePath& file_path,
-                                         base::File& file,
-                                         size_t size) {
-  return WebDatabaseHost::GetInstance().SetFileSize(
-      StringFromFullPath(file_path), size);
-}
-
 }  // namespace blink
