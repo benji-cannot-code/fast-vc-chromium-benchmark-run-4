d78ed753942c631d6dddb8ea8f0bb5a9c272c98f
Keren Zhu <kerenzhu@chromium.org>
2024-08-01 18:56:01 +0000

    webui: migrate AboutUI to WebUIConfig
    Bug: 40222519
    Change-Id: I21ad26fb65af92a28164630f7e6eaf4146df18c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754207
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336162}

--- chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/ash/chrome_web_ui_configs_chromeos.cc
@@ -66,6 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/holding_space/holding_space_keyed_service.h"
 #include "chrome/browser/ui/ash/holding_space/holding_space_keyed_service_factory.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
+#include "chrome/browser/ui/webui/about/about_ui.h"
 #include "chrome/browser/ui/webui/ash/account_manager/account_manager_error_ui.h"
 #include "chrome/browser/ui/webui/ash/account_manager/account_migration_welcome_ui.h"
 #include "chrome/browser/ui/webui/ash/add_supervision/add_supervision_ui.h"
@@ -239,11 +240,13 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(std::make_unique<AssistantOptInUIConfig>());
   map.AddWebUIConfig(std::make_unique<AudioUIConfig>());
   map.AddWebUIConfig(std::make_unique<BluetoothPairingDialogUIConfig>());
+  map.AddWebUIConfig(std::make_unique<BorealisCreditsUI>());
   map.AddWebUIConfig(std::make_unique<BorealisInstallerUIConfig>());
   map.AddWebUIConfig(std::make_unique<cloud_upload::CloudUploadUIConfig>());
   map.AddWebUIConfig(std::make_unique<ColorInternalsUIConfig>());
   map.AddWebUIConfig(std::make_unique<ConfirmPasswordChangeUIConfig>());
   map.AddWebUIConfig(MakeConnectivityDiagnosticsUIConfig());
+  map.AddWebUIConfig(std::make_unique<CrostiniCreditsUI>());
   map.AddWebUIConfig(std::make_unique<CrostiniInstallerUIConfig>());
   map.AddWebUIConfig(std::make_unique<CrostiniUpgraderUIConfig>());
   map.AddWebUIConfig(std::make_unique<CryptohomeUIConfig>());
@@ -296,6 +299,7 @@ void RegisterAshChromeWebUIConfigs() {
   map.AddWebUIConfig(
       std::make_unique<office_fallback::OfficeFallbackUIConfig>());
   map.AddWebUIConfig(std::make_unique<OobeUIConfig>());
+  map.AddWebUIConfig(std::make_unique<OSCreditsUI>());
   map.AddWebUIConfig(
       MakeComponentConfigWithDelegate<OSFeedbackUIConfig, OSFeedbackUI,
                                       ChromeOsFeedbackDelegate>());
