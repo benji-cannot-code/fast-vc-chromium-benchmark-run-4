4780895318c3c2ff63aae2d8e5c2f8ef8b982ebb
Kevin Ellis <kevers@google.com>
2024-08-01 14:55:55 +0000

    [a11y] Infer (next|previous)OnlineID where possible
    An InlineTextBox that is the first child of its parent can infer the
    previous on line ID from the parent. Similarly, if the last child, it
    can infer the next on line ID from the parent. This inference reduces
    the amount of duplicate processing in (Next|Previous)Online calls.
    Profiling has shown that these calls are expensive.
    
    Next steps for resolving the issue:
    * Enable the feature for AX-tree dump tests.
    * Add a field trial config for the feature flag.
    * Either launch via a Finch roll out or enable with a Finch kill-switch.
    
    Further optimization work:
    * Address symmetry
      If A.nextOnline = B, then B.previousOnline = A.
      Caveat, some disambiguation required for AxNodeObject/InlineTextBox.
    * Optimize AxNodeObject::(Next|Previous)Online.
    
    
    Bug: 356614507
    
    Change-Id: I116736797c3c0ae12be272124639611e876dcab8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753283
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336042}

--- ui/accessibility/ax_computed_node_data.cc
+++ ui/accessibility/ax_computed_node_data.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/i18n/break_iterator.h"
 #include "base/logging.h"
+#include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
@@ -102,6 +103,37 @@ bool AXComputedNodeData::GetOrComputeIsDescendantOfPlatformLeaf() const {
 }
 
 bool AXComputedNodeData::HasOrCanComputeAttribute(
+    const ax::mojom::IntAttribute attribute) const {
+  if (owner_->data().HasIntAttribute(attribute)) {
+    return true;
+  }
+
+  if (!::features::IsAccessibilityPruneRedundantInlineConnectivityEnabled()) {
+    return false;
+  }
+
+  // Inline text boxes share the same next- or previous-on-line ID with the
+  // parent when traversing across the parent's boundary. Determination of the
+  // next- or previous-on-line IDs for this type of connectivity is expensive
+  // during the serialization process. Unnecessary to duplicate the effort.
+  if (owner_->data().role != ax::mojom::Role::kInlineTextBox) {
+    return false;
+  }
+
+  if (owner_ == owner_->GetParent()->GetFirstChild() &&
+      attribute == ax::mojom::IntAttribute::kPreviousOnLineId) {
+    return true;
+  }
+
+  if (owner_ == owner_->GetParent()->GetLastChild() &&
+      attribute == ax::mojom::IntAttribute::kNextOnLineId) {
+    return true;
+  }
+
+  return false;
+}
+
+bool AXComputedNodeData::HasOrCanComputeAttribute(
     const ax::mojom::StringAttribute attribute) const {
   if (owner_->data().HasStringAttribute(attribute))
     return true;
@@ -135,6 +167,27 @@ bool AXComputedNodeData::HasOrCanComputeAttribute(
       return true;
     default:
       return false;
+  }
+}
+
+std::optional<int> AXComputedNodeData::GetOrComputeAttribute(
+    const ax::mojom::IntAttribute attribute) const {
+  if (owner_->data().HasIntAttribute(attribute)) {
+    return owner_->data().GetIntAttribute(attribute);
+  }
+
+  if (!HasOrCanComputeAttribute(attribute)) {
+    return std::nullopt;
+  }
+
+  DCHECK(owner_->data().role == ax::mojom::Role::kInlineTextBox);
+  switch (attribute) {
+    case ax::mojom::IntAttribute::kPreviousOnLineId:
+    case ax::mojom::IntAttribute::kNextOnLineId:
+      return owner_->GetParent()->data().GetIntAttribute(attribute);
+
+    default:
+      NOTREACHED_NORETURN();
   }
 }
 
