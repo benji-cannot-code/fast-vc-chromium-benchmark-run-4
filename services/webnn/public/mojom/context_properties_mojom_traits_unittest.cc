0cb4c9352d9fdd28f258bd437f6e2806c45ad791
Shiyi Zou <shiyi.zou@intel.com>
2024-08-01 20:11:37 +0000

    webnn: add support limits for element-wise unary operators
    This CL adds data type support limits for element-wise unary operators,
    including `abs` `ceil` `cos` `erf` `exp` `floor` `identity` `log` `neg`
    `reciprocal` `sin` `sqrt` and `tan`.
    
    Bug: 345271830
    Change-Id: Ic6cce1fabf54589ea0da109c606e27f64dbb84aa
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5744790
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Shiyi Zou <shiyi.zou@intel.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336222}

--- services/webnn/public/mojom/context_properties_mojom_traits_unittest.cc
+++ services/webnn/public/mojom/context_properties_mojom_traits_unittest.cc
@@ -25,6 +25,19 @@ TEST(ContextPropertiesMojomTraitsTest, Basic) {
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
        webnn::SupportedDataTypes::All(),
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kInt32, webnn::OperandDataType::kInt64},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kInt8},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kFloat16},
+       {webnn::OperandDataType::kInt32, webnn::OperandDataType::kInt64},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       webnn::SupportedDataTypes::All(),
+       {webnn::OperandDataType::kUint64},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
+       {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32},
@@ -34,25 +47,10 @@ TEST(ContextPropertiesMojomTraitsTest, Basic) {
        {webnn::OperandDataType::kUint8},
        {webnn::OperandDataType::kFloat16, webnn::OperandDataType::kFloat32}});
 
-  webnn::ContextProperties output(webnn::InputOperandLayout::kNhwc, {{},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {},
-                                                                     {}});
+  webnn::ContextProperties output(
+      webnn::InputOperandLayout::kNhwc,
+      {{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {},
+       {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}});
 
   EXPECT_TRUE(
       mojo::test::SerializeAndDeserialize<webnn::mojom::ContextProperties>(
