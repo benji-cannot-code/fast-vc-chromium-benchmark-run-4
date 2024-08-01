45d78f0aa39177501bb5803c1dd4ccccbde15c7f
Adam Norberg <norberg@google.com>
2024-08-01 20:08:13 +0000

    Enclose ksadmin integration tests in the "non-component builds only" fence.
    This also moves the comment about not supporting component builds in
    general up to the first time the fence is introduced, to make it more
    obvious.
    
    I tried to write a comment introducing the last `#endif` too, warning
    future developers to add new integration tests _inside_ this region so
    they can avoid recreating my mistake, but I couldn't figure out how
    to word it and gave up. Definitely open to suggestions for that.
    
    BUG=356441850
    
    Change-Id: I98cc436c2f755b4ded197d5509e316ef10ff4429
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757325
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: Adam Norberg <norberg@google.com>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Adam Norberg <norberg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336219}

--- chrome/updater/test/integration_tests.cc
+++ chrome/updater/test/integration_tests.cc
@@ -100,6 +100,10 @@ namespace enterprise_management =
 using enterprise_management::ApplicationSettings;
 using enterprise_management::OmahaSettingsClientProto;
 
+// The project's position is that component builds are not portable outside of
+// the build directory. Therefore, installation of component builds is not
+// expected to work and these tests do not run on component builders.
+// See crbug.com/1112527.
 #if BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 void ExpectNoUpdateSequence(ScopedServer* test_server,
@@ -714,10 +718,6 @@ class IntegrationTest : public ::testing::Test {
 #define MAYBE_UpdateServiceStress UpdateServiceStress
 #endif
 
-// The project's position is that component builds are not portable outside of
-// the build directory. Therefore, installation of component builds is not
-// expected to work and these tests do not run on component builders.
-// See crbug.com/1112527.
 #if BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 // Tests the setup and teardown of the fixture.
@@ -3809,7 +3809,6 @@ TEST_F(IntegrationInstallerResultsTestNewInstalls, OnDemandCancel) {
 }
 
 #endif  // BUILDFLAG(IS_WIN)
-#endif  // BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 #if BUILDFLAG(IS_MAC)
 
@@ -4244,5 +4243,7 @@ TEST_F(IntegrationTestKSAdminFourApps, XCPathMismatchUser) {
 }
 
 #endif  // BUILDFLAG(IS_MAC)
+
+#endif  // BUILDFLAG(IS_WIN) || !defined(COMPONENT_BUILD)
 
 }  // namespace updater::test
