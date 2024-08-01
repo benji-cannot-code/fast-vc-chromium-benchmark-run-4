878cc0ebd84bd6b7d5381d22de5859512636d449
Andrew Bregger <andrewbregger@google.com>
2024-08-01 18:58:55 +0000

    Structured Metrics: Introduce Storage Manager
    Storage Manager is an improvement in how events are stored while
    on-device. The amount of memory and disk space can be capped such that
    it doesn't grow unboundeded.
    
    Doc: go/sm-storage-manager
    
    Bug: b/326259404
    Change-Id: I83af5ea9ba4c4ddb9721a8a69dbce31fdde06578
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5448452
    Commit-Queue: Andrew Bregger <andrewbregger@google.com>
    Reviewed-by: Jong Ahn <jongahn@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336166}

--- components/metrics/structured/lib/persistent_proto_internal.cc
+++ components/metrics/structured/lib/persistent_proto_internal.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/files/important_file_writer.h"
 #include "base/functional/bind.h"
-#include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
