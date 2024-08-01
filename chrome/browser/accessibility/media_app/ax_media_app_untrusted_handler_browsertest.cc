c56fa0bc2383b4e8cd0accbb43125aa89381d54c
Joshua Pawlicki <waffles@chromium.org>
2024-08-01 16:13:52 +0000

    Revert "MediaApp: Delete PDF a11y OCR flag."
    This reverts commit 9e4b1af6630c60b831073cb5d87db58ba7cf30e5.
    
    Reason for revert: Causes test failures, e.g. https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/43648/overview media_app_ocr_integration_browsertest.cc(124): Expected equality of these values:
      ax_factory->media_app_receivers().size()
        Which is: 0
      1u
        Which is: 1
    
    Original change's description:
    > MediaApp: Delete PDF a11y OCR flag.
    >
    > Bug: b/343859373
    > Change-Id: I4659d7a7b8ee9c07bdc37797a1ef7389bc3ca04a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5727699
    > Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    > Commit-Queue: Patti Lor <patricialor@chromium.org>
    > Reviewed-by: Zufeng Wang <zufeng@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1335882}
    
    Bug: b/343859373
    Change-Id: Ib3e681934576ff71735f0dce603cc4bb547f2302
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757241
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Joshua Pawlicki <waffles@chromium.org>
    Owners-Override: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1336077}

--- chrome/browser/accessibility/media_app/ax_media_app_untrusted_handler_browsertest.cc
+++ chrome/browser/accessibility/media_app/ax_media_app_untrusted_handler_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/media_app_ui/media_app_ui_untrusted.mojom.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
 #include "chrome/browser/accessibility/accessibility_state_utils.h"
 #include "chrome/browser/accessibility/media_app/ax_media_app.h"
 #include "chrome/browser/accessibility/media_app/ax_media_app_handler_factory.h"
@@ -107,7 +108,8 @@ constexpr std::string_view kLoadingMessage =
 
 class AXMediaAppUntrustedHandlerTest : public InProcessBrowserTest {
  public:
-  AXMediaAppUntrustedHandlerTest() {}
+  AXMediaAppUntrustedHandlerTest()
+      : feature_list_(ash::features::kMediaAppPdfA11yOcr) {}
   AXMediaAppUntrustedHandlerTest(
       const AXMediaAppUntrustedHandlerTest&) = delete;
   AXMediaAppUntrustedHandlerTest& operator=(
@@ -158,6 +160,9 @@ class AXMediaAppUntrustedHandlerTest : public InProcessBrowserTest {
 
   FakeAXMediaApp fake_media_app_;
   std::unique_ptr<TestAXMediaAppUntrustedHandler> handler_;
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 std::vector<PageMetadataPtr>
--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4204,6 +4204,9 @@ extern const char kMahiDescription[];
 extern const char kMahiFeatureKeyName[];
 extern const char kMahiFeatureKeyDescription[];
 
+extern const char kMediaAppPdfA11yOcrName[];
+extern const char kMediaAppPdfA11yOcrDescription[];
+
 extern const char kMediaAppPdfMahiName[];
 extern const char kMediaAppPdfMahiDescription[];
 
