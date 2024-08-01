0b4352a2b799f5cefd28e3e6800f7af8ccafe62c
Lei Zhang <thestig@chromium.org>
2024-08-01 01:35:54 +0000

    [PDF Ink Signatures] Add CalculatePageBoundaryIntersectPoint() helper
    Add a utility function that calculates where a line that starts inside a
    rect and ends outside a rect intersects the rect. This will be used to
    calculate where strokes enter/exit the page, given input events do not
    always perfectly land at page boundaries.
    
    Bug: 352578791
    Change-Id: If8e33d09a0fe29ffd8f5b578d3ab8c8d2455ec97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751342
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1335789}

---
+++ pdf/draw_utils/page_boundary_intersect.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_DRAW_UTILS_PAGE_BOUNDARY_INTERSECT_H_
+#define PDF_DRAW_UTILS_PAGE_BOUNDARY_INTERSECT_H_
+
+#include "ui/gfx/geometry/point_f.h"
+
+namespace gfx {
+class Rect;
+}  // namespace gfx
+
+namespace chrome_pdf {
+
+// Given:
+// - `page_rect` must be non-empty.
+// - `inside_point` must be inside of `page_rect`.
+// - `outside_point` must be outside of `page_rect`.
+//
+// A straight line from `inside_point` to `outside_point` must intersect the
+// boundary of `page_rect`. Return that intersection point.
+gfx::PointF CalculatePageBoundaryIntersectPoint(
+    const gfx::Rect& page_rect,
+    const gfx::PointF& inside_point,
+    const gfx::PointF& outside_point);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_DRAW_UTILS_PAGE_BOUNDARY_INTERSECT_H_
