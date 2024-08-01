dfa493e6f867572fe528f3d2fd58fd835c0b4046
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-01 19:50:23 +0000

    spanification: Remove assert on the clang rewritter.
    There is currently an error:
    > Assertion `fct_sig_nodes.find(l) != fct_sig_nodes.end()' failed
    
    This happens when a function is declared from third_party, but
    implemented in first party.
    
    This adds a regression test and skip the assert.
    
    Bug: 356666773
    Change-Id: If8988ccc47051653fb7d55b71e45002a50c82e5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756698
    Reviewed-by: Ali Hijazi <ahijazi@chromium.org>
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336207}

---
+++ tools/clang/spanify/tests/third_party/do_not_rewrite/header-original.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "header-original.h"
+
+// Declared from third_party.
+void ProcessBuffer2(int* buffer, int size) {
+  for (int i = 0; i < size; i++) {
+    buffer[i] = buffer[i] * 2;
+  }
+}
+
+// Declared from first party.
+void ProcessBuffer3(int* buffer, int size) {
+  for (int i = 0; i < size; i++) {
+    buffer[i] = buffer[i] + 1;
+  }
+}
