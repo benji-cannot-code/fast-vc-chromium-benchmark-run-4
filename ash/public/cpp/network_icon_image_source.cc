cfcec76ef2ce6e6b0bf7870c214865c8ab918cc0
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-02 18:10:30 +0000

    spanification: Add `#pragma allow_unsafe_buffers` to chrome/* and ash/ #cleanup
    This is a preparation to fix each files.
    This CL has no behavior changes.
    
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 40285824
    Change-Id: Ie5429eca38cdb31543a49265d1527e3322e1eda7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756729
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336708}

--- ash/public/cpp/network_icon_image_source.cc
+++ ash/public/cpp/network_icon_image_source.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "ash/public/cpp/network_icon_image_source.h"
 
 #include "third_party/skia/include/core/SkPath.h"
