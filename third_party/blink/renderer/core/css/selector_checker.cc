6358f7d03ff60536336e55ddacc2eb4eb6b8ddd7
Daniil Sakhapov <sakhapov@chromium.org>
2024-08-02 13:49:11 +0000

    Support making ::scroll-marker active by setting :checked
    This CL adds ability to specify :checked on ::scroll-marker and
    tracking the active ::scroll-marker inside ::scroll-marker-group.
    
    Also, it adds ability to navigate with keyboard between ::scroll-marker
    pseudo elements and make selected element active.
    
    Bug: 332396355
    Change-Id: Ie630ff200577b538195d2a4eadf0e98c9288e9d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752603
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336595}

--- third_party/blink/renderer/core/css/selector_checker.cc
+++ third_party/blink/renderer/core/css/selector_checker.cc
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_computed_style.h"
 #include "third_party/blink/renderer/core/dom/nth_index_cache.h"
 #include "third_party/blink/renderer/core/dom/popover_data.h"
+#include "third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.h"
 #include "third_party/blink/renderer/core/dom/shadow_root.h"
 #include "third_party/blink/renderer/core/dom/slot_assignment_engine.h"
 #include "third_party/blink/renderer/core/dom/text.h"
@@ -1767,6 +1768,10 @@ bool SelectorChecker::CheckPseudoClass(const SelectorCheckingContext& context,
         if (option_element->Selected()) {
           return true;
         }
+      } else if (PseudoElement* scroll_marker =
+                     element.GetPseudoElement(kPseudoIdScrollMarker);
+                 context.pseudo_id == kPseudoIdScrollMarker && scroll_marker) {
+        return To<ScrollMarkerPseudoElement>(scroll_marker)->IsSelected();
       }
       break;
     }
