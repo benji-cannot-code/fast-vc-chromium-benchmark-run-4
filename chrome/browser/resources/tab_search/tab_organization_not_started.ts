ee7fb383dd4d33d192262f8932677c92e405ea5b
Emily Shack <emshack@chromium.org>
2024-08-02 20:58:25 +0000

    Tab Organization: Use correct sign in listener
    Bug: 356898613
    Change-Id: Ibaf11a33a6c0d0902544f4ee6b6d34cf6adf7db4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758041
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Auto-Submit: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336797}

--- chrome/browser/resources/tab_search/tab_organization_not_started.ts
+++ chrome/browser/resources/tab_search/tab_organization_not_started.ts
@@ -57,7 +57,7 @@ export class TabOrganizationNotStartedElement extends
     super.connectedCallback();
 
     this.syncBrowserProxy_.getSignInState().then(this.setSignedIn_.bind(this));
-    this.addWebUiListener('has-accoun-changed', this.setSignedIn_.bind(this));
+    this.addWebUiListener('account-info-changed', this.setSignedIn_.bind(this));
   }
 
   private setSignedIn_(signedIn: boolean) {
