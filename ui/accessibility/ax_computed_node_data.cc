c4995cd7a6c4b5dd03a5e450f89a3aebe8a5d6d6
Kevin Ellis <kevers@google.com>
2024-08-02 16:51:34 +0000

    [a11y] Optimize fetching integer valued properties from AX Nodes
    Avoid multiple scans over the integer properties. Previously, we
    checked if the value existed or could be computed before trying to
    fetch it; however, we can determine when the fetch fails, and can
    call it directly. Furthermore, the GetOrComputeAttribute was checking
    if the value existed or could be computed after the initial fetch of
    an existing problem failed, but this in turn leads to another scan
    over the existing properties.
    
    It appears the String and IntList attributes would benefit from the
    same optimization, but deferring to a followup CL since not directly
    related to the regression.
    
    Bug: 356917007
    Change-Id: Ifec40342e6c43e883e05ffbb98e7377cf2639308
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758762
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336663}

--- ui/accessibility/ax_computed_node_data.cc
+++ ui/accessibility/ax_computed_node_data.cc
@@ -108,6 +108,11 @@ bool AXComputedNodeData::HasOrCanComputeAttribute(
     return true;
   }
 
+  return CanComputeAttribute(attribute);
+}
+
+bool AXComputedNodeData::CanComputeAttribute(
+    const ax::mojom::IntAttribute attribute) const {
   if (!::features::IsAccessibilityPruneRedundantInlineConnectivityEnabled()) {
     return false;
   }
@@ -172,11 +177,12 @@ bool AXComputedNodeData::HasOrCanComputeAttribute(
 
 std::optional<int> AXComputedNodeData::GetOrComputeAttribute(
     const ax::mojom::IntAttribute attribute) const {
-  if (owner_->data().HasIntAttribute(attribute)) {
-    return owner_->data().GetIntAttribute(attribute);
+  int value;
+  if (owner_->data().GetIntAttribute(attribute, &value)) {
+    return value;
   }
 
-  if (!HasOrCanComputeAttribute(attribute)) {
+  if (!CanComputeAttribute(attribute)) {
     return std::nullopt;
   }
 
