ba2489007119afa9bb85396605249cf62fcb5412
Mingyu Lei <leimy@chromium.org>
2024-08-02 03:30:51 +0000

    Revert "soda: Update caption_bubble with non-live-translate visibility."
    This reverts commit d7c0246d56d4fdb500138510baca89732132cf34.
    
    Reason for revert: linux-chromeos-chrome fails because of seg fault
    
    Original change's description:
    > soda: Update caption_bubble with non-live-translate visibility.
    >
    > This change adds a further number of changes to make the labels
    > appropriate in multilingual but non translate environment. Fixes some ugliness when showing otherwise.
    >
    > Bug: b:356496449, b/338322372, b/257166700
    > Change-Id: I921b387e4f8fcd11ca1b436e21fc877678ee33d4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753913
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Commit-Queue: Rob Schonberger <robsc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1336387}
    
    Bug: b:356496449, b/338322372, b/257166700, b/356981468
    Change-Id: Ibc0c0873bacc48f56813bc3f12c4f1f40a8398d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758840
    Owners-Override: Mingyu Lei <leimy@chromium.org>
    Auto-Submit: Mingyu Lei <leimy@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336413}

--- components/live_caption/views/caption_bubble.cc
+++ components/live_caption/views/caption_bubble.cc
@@ -837,8 +837,7 @@ void CaptionBubble::Init() {
   header_container_ = AddChildView(std::move(header_container));
   AddChildView(std::move(content_container));
 
-  if (media::IsLiveTranslateEnabled() ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (media::IsLiveTranslateEnabled()) {
     std::vector<raw_ptr<views::View, VectorExperimental>> buttons =
         GetButtons();
     for (views::View* button : buttons) {
@@ -848,11 +847,10 @@ void CaptionBubble::Init() {
     }
 
     language_label_->layer()->SetFillsBoundsOpaquely(false);
-    if (media::IsLiveTranslateEnabled()) {
-      download_progress_label_->SetPaintToLayer();
-      download_progress_label_->layer()->SetFillsBoundsOpaquely(false);
-      download_progress_label_->layer()->SetOpacity(0);
-    }
+
+    download_progress_label_->SetPaintToLayer();
+    download_progress_label_->layer()->SetFillsBoundsOpaquely(false);
+    download_progress_label_->layer()->SetOpacity(0);
   }
 
   UpdateContentSize();
@@ -878,8 +876,7 @@ bool CaptionBubble::ShouldShowCloseButton() const {
 std::unique_ptr<views::NonClientFrameView>
 CaptionBubble::CreateNonClientFrameView(views::Widget* widget) {
   std::vector<raw_ptr<views::View, VectorExperimental>> buttons = GetButtons();
-  if (media::IsLiveTranslateEnabled() ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (media::IsLiveTranslateEnabled()) {
     caption_bubble_event_observer_ =
         std::make_unique<CaptionBubbleEventObserver>(this, widget);
   }
@@ -909,8 +906,7 @@ void CaptionBubble::OnWidgetActivationChanged(views::Widget* widget,
     active = true;
   }
 
-  if (media::IsLiveTranslateEnabled() ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (media::IsLiveTranslateEnabled()) {
     UpdateControlsVisibility(active);
   }
 
@@ -1035,18 +1031,17 @@ void CaptionBubble::SetModel(CaptionBubbleModel* model) {
 }
 
 void CaptionBubble::AnimationProgressed(const gfx::Animation* animation) {
-  if (!media::IsLiveTranslateEnabled() &&
-      !base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (!media::IsLiveTranslateEnabled()) {
     return;
   }
+
   std::vector<raw_ptr<views::View, VectorExperimental>> buttons = GetButtons();
   for (views::View* button : buttons) {
     button->layer()->SetOpacity(animation->GetCurrentValue());
   }
+
   language_label_->layer()->SetOpacity(animation->GetCurrentValue());
-  if (media::IsLiveTranslateEnabled()) {
-    download_progress_label_->layer()->SetOpacity(animation->GetCurrentValue());
-  }
+  download_progress_label_->layer()->SetOpacity(animation->GetCurrentValue());
 }
 
 void CaptionBubble::OnTextChanged() {
@@ -1087,8 +1082,7 @@ void CaptionBubble::OnLanguagePackInstalled() {
 }
 
 void CaptionBubble::OnAutoDetectedLanguageChanged() {
-  if (!media::IsLiveTranslateEnabled() &&
-      !base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (!media::IsLiveTranslateEnabled()) {
     return;
   }
 
@@ -1283,14 +1277,11 @@ void CaptionBubble::SetTextSizeAndFontFamily() {
   label_->SetLineHeight(kLineHeightDip * textScaleFactor);
   label_->SetMaximumWidth(kMaxWidthDip * textScaleFactor - kSidePaddingDip * 2);
   title_->SetLineHeight(kLineHeightDip * textScaleFactor);
-  if (media::IsLiveTranslateEnabled() ||
-      !base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
-    if (media::IsLiveTranslateEnabled()) {
-      download_progress_label_->SetLineHeight(kLiveTranslateLabelLineHeightDip *
-                                              textScaleFactor);
-      download_progress_label_->SetFontList(
-          GetFontList(kLiveTranslateLabelFontSizePx));
-    }
+  if (media::IsLiveTranslateEnabled()) {
+    download_progress_label_->SetLineHeight(kLiveTranslateLabelLineHeightDip *
+                                            textScaleFactor);
+    download_progress_label_->SetFontList(
+        GetFontList(kLiveTranslateLabelFontSizePx));
     language_label_->GetLabel()->SetLineHeight(
         kLiveTranslateLabelLineHeightDip * textScaleFactor);
     language_label_->GetLabel()->SetFontList(
@@ -1323,9 +1314,6 @@ void CaptionBubble::SetTextColor() {
   // the CSS color string.
   if (media::IsLiveTranslateEnabled()) {
     download_progress_label_->SetEnabledColor(primary_color);
-  }
-  if (media::IsLiveTranslateEnabled() ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
     language_label_->SetBaseColor();
     language_label_->SetImageModel(
         views::Button::ButtonState::STATE_NORMAL,
@@ -1515,14 +1503,9 @@ void CaptionBubble::UpdateContentSize() {
   left_header_container_->SetPreferredSize(
       gfx::Size(left_header_width, button_size.height()));
 
-  auto is_live_translate_enabled = media::IsLiveTranslateEnabled();
-
-  if (is_live_translate_enabled) {
+  if (media::IsLiveTranslateEnabled()) {
     download_progress_label_->SetPreferredSize(
         gfx::Size(width, content_height));
-  }
-  if (is_live_translate_enabled ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
     language_label_->SetPreferredSize(
         language_label_->CalculatePreferredSize({}));
   }
@@ -1630,8 +1613,7 @@ CaptionBubble::GetButtons() {
       back_to_tab_button_.get(), close_button_.get(), expand_button_.get(),
       collapse_button_.get(),    pin_button_.get(),   unpin_button_.get()};
 
-  if (media::IsLiveTranslateEnabled() ||
-      base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
+  if (media::IsLiveTranslateEnabled()) {
     buttons.push_back(language_label_.get());
   }
 
