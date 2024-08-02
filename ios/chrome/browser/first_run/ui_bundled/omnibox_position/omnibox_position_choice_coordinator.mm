b155d9d0841ad28054a447a43a0216cc9b921560
Scott Yoder <scottyoder@google.com>
2024-08-02 19:30:36 +0000

    [ios] Fix issue with TipsNotification and OmniboxChoice EG tests
    This change fixes a few CHECKs related to the Omnibox Choice screen,
    which should be allowed to show if either TipsNotifications or the
    Bottom Omnibox feature is enabled.
    
    Fixed: 353981779
    Change-Id: I7562f55b7c6fffce38a2ae67245c223afca310a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757695
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336762}

--- ios/chrome/browser/first_run/ui_bundled/omnibox_position/omnibox_position_choice_coordinator.mm
+++ ios/chrome/browser/first_run/ui_bundled/omnibox_position/omnibox_position_choice_coordinator.mm
@@ -67,7 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)start {
   CHECK(IsIOSTipsNotificationsEnabled() ||
-        IsBottomOmniboxPromoFlagEnabled(BottomOmniboxPromoType::kAppLaunch));
+        IsBottomOmniboxPromoFlagEnabled(BottomOmniboxPromoType::kAny));
   [super start];
 
   _mediator =
