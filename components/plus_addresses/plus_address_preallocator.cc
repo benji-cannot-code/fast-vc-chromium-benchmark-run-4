0b71dccfc0112e239d4c33cb84a44d4f1ab99efe
Jan Keitel <jkeitel@google.com>
2024-08-02 15:52:18 +0000

    Add stub implementation for PlusAddressPreallocator.
    This CL adds a PlusAddressPreallocator class that does not do anything
    yet apart from return an when a new plus address is requested.
    
    Subsequent CLs will add functionality.
    
    Bug: 324559503
    Change-Id: I27426952775530f370269a48382c8835d72c7fd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756789
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336639}

---
+++ components/plus_addresses/plus_address_preallocator.cc
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/plus_addresses/plus_address_preallocator.h"
+
+#include <utility>
+
+#include "base/check_deref.h"
+#include "components/plus_addresses/plus_address_types.h"
+
+namespace plus_addresses {
+
+PlusAddressPreallocator::PlusAddressPreallocator(
+    PlusAddressHttpClient* http_client)
+    : http_client_(CHECK_DEREF(http_client)) {}
+
+PlusAddressPreallocator::~PlusAddressPreallocator() = default;
+
+void PlusAddressPreallocator::AllocatePlusAddress(
+    const url::Origin& origin,
+    AllocationMode mode,
+    PlusAddressRequestCallback callback) {
+  std::move(callback).Run(base::unexpected(PlusAddressRequestError(
+      PlusAddressRequestErrorType::kRequestNotSupportedError)));
+}
+
+bool PlusAddressPreallocator::IsRefreshingSupported(
+    const url::Origin& origin) const {
+  return true;
+}
+
+}  // namespace plus_addresses
