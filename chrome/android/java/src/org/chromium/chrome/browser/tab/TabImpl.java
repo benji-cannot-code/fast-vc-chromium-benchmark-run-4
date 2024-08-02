90bab67c516ba11793a92bec05e1c29b7f62a807
Jingping Sun <jings@microsoft.com>
2024-08-02 18:17:24 +0000

    Update the Tab title if the title is empty string
    TabImpl#getTitle return the empty string directly if current title is "". Update the title if the title is empty string is better than return it directly.
    
    Bug: 357014500
    Change-Id: If324b635dac362ab3eb365604c8035dcfddfd6f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759302
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336714}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabImpl.java
@@ -477,7 +477,7 @@ class TabImpl implements Tab {
     @CalledByNative
     @Override
     public String getTitle() {
-        if (mTitle == null) updateTitle();
+        if (TextUtils.isEmpty(mTitle)) updateTitle();
         return mTitle;
     }
 
