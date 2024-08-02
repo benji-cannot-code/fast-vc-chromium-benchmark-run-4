9e6ad6d6e908be2e2b84aa0892613a6e41e67b04
Adithya Srinivasan <adithyas@chromium.org>
2024-08-02 19:07:18 +0000

    SpeculationRules: Handle detached document in SpeculationRuleLoader::NotifyFinished
    Ordinarily we cancel resource loads when the document is detached,
    but there's a small window of time where the document could detach
    after the load completes but before the notification is sent to
    SpeculationRuleLoader.
    
    Bug: 356767669
    Change-Id: I8885ee597c425139ae3088e7042739cf128a4796
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756187
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336747}

--- third_party/blink/renderer/core/loader/speculation_rule_loader.cc
+++ third_party/blink/renderer/core/loader/speculation_rule_loader.cc
@@ -84,6 +84,10 @@ void SpeculationRuleLoader::NotifyFinished() {
     return;
   }
 
+  if (!document_->GetExecutionContext()) {
+    return;
+  }
+
   String source_text = resource_->DecodedText();
   auto* source = SpeculationRuleSet::Source::FromRequest(
       source_text, response.ResponseUrl(), resource_->InspectorId());
