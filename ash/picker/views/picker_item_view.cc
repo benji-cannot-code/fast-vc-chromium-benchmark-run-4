80cf1d610f4dbfb8284d107adc05f786aecdcc64
Michelle <michellegc@google.com>
2024-08-02 02:59:56 +0000

    Adjust hover highlight for Picker item views.
    Use SetBackground to set the "highlight" when the item is in a hovered
    state, rather than using an ink drop hover on highlight effect. This
    is to make the item's "mouse hover highlight" consistent with its
    "pseudo focus highlight" (when keyboard navigating to the item), and
    also ensures the highlight doesn't get doubly applied when an item is
    both mouse hovered and pseudo focused.
    
    Bug: b:345539469, b:348066552
    Change-Id: Ibdf4e47d49a1f9198bcef90a21291edf0b2f0c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758898
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336402}

--- ash/picker/views/picker_item_view.cc
+++ ash/picker/views/picker_item_view.cc
@@ -42,18 +42,6 @@ constexpr float kPseudoFocusClipInset =
 
 constexpr auto kPickerItemFocusIndicatorMargins = gfx::Insets::VH(6, 0);
 
-std::unique_ptr<views::Background> GetPickerItemBackground(
-    PickerItemView::ItemState item_state,
-    int corner_radius) {
-  switch (item_state) {
-    case PickerItemView::ItemState::kNormal:
-      return nullptr;
-    case PickerItemView::ItemState::kPseudoFocused:
-      return views::CreateThemedRoundedRectBackground(
-          cros_tokens::kCrosSysHoverOnSubtle, corner_radius);
-  }
-}
-
 }  // namespace
 
 PickerItemView::PickerItemView(SelectItemCallback select_item_callback,
@@ -61,11 +49,7 @@ PickerItemView::PickerItemView(SelectItemCallback select_item_callback,
     : views::Button(select_item_callback),
       select_item_callback_(select_item_callback),
       focus_indicator_style_(focus_indicator_style) {
-  StyleUtil::SetUpInkDropForButton(this, gfx::Insets(),
-                                   /*highlight_on_hover=*/true,
-                                   /*highlight_on_focus=*/true);
-  views::InkDrop::Get(this)->GetInkDrop()->SetHoverHighlightFadeDuration(
-      base::TimeDelta());
+  StyleUtil::SetUpInkDropForButton(this);
 
   switch (focus_indicator_style_) {
     case FocusIndicatorStyle::kFocusRingWithInsetGap:
@@ -88,6 +72,10 @@ PickerItemView::PickerItemView(SelectItemCallback select_item_callback,
 }
 
 PickerItemView::~PickerItemView() = default;
+
+void PickerItemView::StateChanged(ButtonState old_state) {
+  UpdateBackground();
+}
 
 void PickerItemView::PaintButtonContents(gfx::Canvas* canvas) {
   views::Button::PaintButtonContents(canvas);
@@ -123,7 +111,7 @@ void PickerItemView::SetCornerRadius(int corner_radius) {
   corner_radius_ = corner_radius;
   StyleUtil::InstallRoundedCornerHighlightPathGenerator(
       this, gfx::RoundedCornersF(corner_radius_));
-  SetBackground(GetPickerItemBackground(item_state_, corner_radius_));
+  UpdateBackground();
 }
 
 PickerSubmenuController* PickerItemView::GetSubmenuController() {
@@ -145,7 +133,8 @@ void PickerItemView::SetItemState(ItemState item_state) {
   }
 
   item_state_ = item_state;
-  SetBackground(GetPickerItemBackground(item_state_, corner_radius_));
+  UpdateBackground();
+
   switch (focus_indicator_style_) {
     case FocusIndicatorStyle::kFocusRingWithInsetGap:
       UpdateClipPathForFocusRingWithInsetGap();
@@ -173,6 +162,16 @@ void PickerItemView::UpdateClipPathForFocusRingWithInsetGap() {
     clip_path.addRoundRect(gfx::RectFToSkRect(inset_bounds), radius, radius);
   }
   SetClipPath(clip_path);
+}
+
+void PickerItemView::UpdateBackground() {
+  if (GetState() == views::Button::ButtonState::STATE_HOVERED ||
+      item_state_ == PickerItemView::ItemState::kPseudoFocused) {
+    SetBackground(views::CreateThemedRoundedRectBackground(
+        cros_tokens::kCrosSysHoverOnSubtle, corner_radius_));
+  } else {
+    SetBackground(nullptr);
+  }
 }
 
 BEGIN_METADATA(PickerItemView)
