2e0840a02bbfb78237d67d8af581a09f7b8ec5db
Calder Kitagawa <ckitagawa@chromium.org>
2024-08-02 19:21:14 +0000

    [Tab Model] Create TabClosureParams
    Create TabClosureParams which will be used to replace all the flavors
    of tab closure on TabModel in favor of a single closeTabs method that
    takes these params as input.
    
    This allows for
    1. A unified codepath to allow seamless forwarding of params for
       wrappers/interceptors of tab closures.
    2. A much cleaner interface than lists of booleans that were very
       difficult to follow.
    
    Bug: 356445932
    Change-Id: I7832fd745677ce6a3ef63d5d9f72937102cb2398
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758302
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336757}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelImpl.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
-import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
@@ -35,8 +34,6 @@ import org.chromium.ui.mojom.WindowOpenDisposition;
 import org.chromium.url.GURL;
 import org.chromium.url.Origin;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -48,19 +45,6 @@ import java.util.Map;
  * ChromeTabbedActivity}.
  */
 public class TabModelImpl extends TabModelJniBridge {
-    @IntDef({TabCloseType.SINGLE, TabCloseType.MULTIPLE, TabCloseType.ALL})
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface TabCloseType {
-        /** A single tab is closing. */
-        int SINGLE = 0;
-
-        /** Multiple tabs are closing. This may be a tab group or just a selection of tabs. */
-        int MULTIPLE = 1;
-
-        /** All tabs are closing. */
-        int ALL = 2;
-    }
-
     /**
      * The application ID used for tabs opened from an application that does not specify an app ID
      * in its VIEW intent extras.
