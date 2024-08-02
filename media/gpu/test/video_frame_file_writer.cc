0313ca4e23efea4825795fdb09ff6cab2c3b765c
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-08-02 11:58:07 +0000

    Revert "Enable build of VEA test for Windows."
    This reverts commit 219fc086fb9fcb8f5cfe9c390d1c5acec311762a.
    
    Reason for revert:
    LUCI Bisection has identified this change as the culprit of a build failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/compile-analysis/b/8740717483366665521
    
    Sample failed build: https://ci.chromium.org/b/8740717483366665521
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5681527&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Fcompile-analysis%2Fb%2F8740717483366665521&type=BUG
    
    Original change's description:
    > Enable build of VEA test for Windows.
    >
    > Make VEA test built for Windows besides CrOS. As the first step we only
    > enabled input of shared memory type. GMB support will be added later.
    >
    > Single VEA perf test at quality + speed mode for all resolutions takes
    > more than 2 minutes to finish, while media_unittests currently without
    > these perf test bundled takes around 3 minutes to finish on my Xeon
    > Platinum 8385 platform.
    >
    > To build the vea_tests on Windows:
    > autoninja -C <output_path> video_encode_accelerator_tests
    > To run vea_test on Windows:
    > <output_path>\video_encode_accelerator_tests.exe
    >
    > To build the vea_perf_tests on Windows:
    > autoninja -C <output_path> video_encode_accelerator_perf_tests
    > To run the vea_perf_tests on Windows(for example):
    > <output_path>\video_encode_accelerator_perf_tests.exe --speed
    > --codec=h264 --svc_mode=L1T1 --content_type=camera --bitrate_mode=cbr
    > --output_bitstream <video_path> <video_meta_data_path>
    >
    > Bug: 350994517
    > Change-Id: I9ebae8729b1455b595acb86513e75c48b53e8906
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5681527
    > Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1336559}
    >
    
    Bug: 350994517
    Change-Id: I5b7128e17e4ef9129c41faa6594ef885c551f6b9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758051
    Commit-Queue: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Bot-Commit: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Owners-Override: luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1336563}

--- media/gpu/test/video_frame_file_writer.cc
+++ media/gpu/test/video_frame_file_writer.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/gpu/test/video_frame_file_writer.h"
 
+#include <sys/mman.h>
 #include <utility>
 #include <vector>
 
@@ -26,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/media_buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/codec/png_codec.h"
-
-#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
-#include <sys/mman.h>
-#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 
 #if BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
 #include "media/gpu/chromeos/chromeos_compressed_gpu_memory_buffer_video_frame_utils.h"
@@ -160,19 +157,11 @@ void VideoFrameFileWriter::ProcessVideoFrameTask(
   DCHECK_CALLED_ON_VALID_SEQUENCE(writer_thread_sequence_checker_);
 
   const gfx::Size& visible_size = video_frame->visible_rect().size();
-
-  base::FilePath file_path;
-  if (!output_file_prefix_.empty()) {
-    file_path = base::FilePath(output_file_prefix_)
-                    .AppendASCII("_")
-                    .Append(base::FilePath::FromASCII(base::StringPrintf(
-                        "frame_%04zu_%dx%d", frame_index, visible_size.width(),
-                        visible_size.height())));
-  } else {
-    file_path = base::FilePath::FromASCII(
-        base::StringPrintf("frame_%04zu_%dx%d", frame_index,
-                           visible_size.width(), visible_size.height()));
-  }
+  base::FilePath::StringType filename =
+      base::StringPrintf(FILE_PATH_LITERAL("frame_%04zu_%dx%d"), frame_index,
+                         visible_size.width(), visible_size.height());
+  if (!output_file_prefix_.empty())
+    filename = output_file_prefix_ + FILE_PATH_LITERAL("_") + filename;
 
   // Copies to |frame| in this function so that |video_frame| stays alive until
   // in the end of function.
@@ -225,10 +214,10 @@ void VideoFrameFileWriter::ProcessVideoFrameTask(
 #endif  // BUILDFLAG(USE_CHROMEOS_MEDIA_ACCELERATION)
   switch (output_format_) {
     case OutputFormat::kPNG:
-      WriteVideoFramePNG(frame, file_path);
+      WriteVideoFramePNG(frame, base::FilePath(filename));
       break;
     case OutputFormat::kYUV:
-      WriteVideoFrameYUV(frame, file_path);
+      WriteVideoFrameYUV(frame, base::FilePath(filename));
       break;
   }
 
