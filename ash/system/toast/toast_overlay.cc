1a7ed16f19b7c5ca59d242a29c4ce6232c8b7109
Sammie Quon <sammiequon@chromium.org>
2024-08-02 00:40:44 +0000

    desks: Fix undo toast not working with chromevox in overview
    The toast is activatable with chromevox on since users need to be able
    to keyboard traverse to it. However, using a mouse would first
    activate the toast, and activating a window in overview would exit
    overview.
    
    This CL makes activating toasts not exit overview.
    
    Test: manual
    Test: ash_unittests  *DesksCloseAllTest*
    Change-Id: I63a4581a9b5efba541eba9377165c4fde983fef7
    Fixed: b/356460723
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757980
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336367}

--- ash/system/toast/toast_overlay.cc
+++ ash/system/toast/toast_overlay.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_provider.h"
 #include "ash/style/pill_button.h"
 #include "ash/system/toast/system_toast_view.h"
+#include "ash/wm/window_properties.h"
 #include "ash/wm/work_area_insets.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
@@ -178,6 +179,8 @@ ToastOverlay::ToastOverlay(Delegate* delegate,
   params.activatable = toast_data.activatable
                            ? views::Widget::InitParams::Activatable::kYes
                            : views::Widget::InitParams::Activatable::kNo;
+  params.init_properties_container.SetProperty(kStayInOverviewOnActivationKey,
+                                               true);
   overlay_widget_->Init(std::move(params));
   overlay_widget_->SetVisibilityChangedAnimationsEnabled(true);
   overlay_widget_->SetContentsView(overlay_view_.get());
