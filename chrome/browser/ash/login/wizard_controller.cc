5b8349ad516a5e76597f47e76e971388b2d72541
Brahim Chikhaoui <bchikhaoui@google.com>
2024-08-02 08:09:45 +0000

    [OOBE][Perks] Add browsertest for PerksDiscoveryScreen
    Bug: b:351985347
    Change-Id: Icb8ce76d1730f7a96f1d895536916848db4c7365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5741236
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336492}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -3165,6 +3165,8 @@ void WizardController::AdvanceToScreen(OobeScreenId screen_id) {
     ShowCategoriesSelectionScreen();
   } else if (screen_id == PersonalizedRecommendAppsScreenView::kScreenId) {
     ShowPersonalizedRecomendAppsScreen();
+  } else if (screen_id == PerksDiscoveryScreenView::kScreenId) {
+    ShowPerksDiscoveryScreen();
   } else if (screen_id == TpmErrorView::kScreenId ||
              screen_id == InstallAttributesErrorView::kScreenId ||
              screen_id == FamilyLinkNoticeView::kScreenId ||
