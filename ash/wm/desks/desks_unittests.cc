4db77108c1abfd69637a402b7b45090a52a8674a
Eric Sum <esum@google.com>
2024-08-02 03:48:44 +0000

    Fix focus ring around library button in overview mode.
    Bug: b:356478733
    Change-Id: Iffcf38c2c094a641a3247f32c72daf686955db2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758609
    Commit-Queue: Eric Sum <esum@google.com>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336419}

--- ash/wm/desks/desks_unittests.cc
+++ ash/wm/desks/desks_unittests.cc
@@ -156,6 +156,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/background.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/focus_ring.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/test/views_test_utils.h"
 #include "ui/views/widget/any_widget_observer.h"
@@ -9256,6 +9257,7 @@ class DeskBarTest
     // should show up.
     auto* overview_controller = OverviewController::Get();
     ASSERT_TRUE(overview_controller->InOverviewSession());
+    RunScheduledLayoutForAllOverviewDeskBars();
     auto* overview_session = overview_controller->overview_session();
     EXPECT_TRUE(overview_session &&
                 overview_session->IsShowingSavedDeskLibrary());
@@ -9729,6 +9731,11 @@ TEST_P(DeskBarTest, LibraryButton) {
       DesksTestApi::WaitForDeskBarUiUpdate(
           GetDeskBarView(root, DeskBarViewBase::Type::kOverview));
     }
+    const views::FocusRing* library_button_focus_ring =
+        views::FocusRing::Get(GetPrimaryRootDesksBarView()->library_button());
+    ASSERT_TRUE(library_button_focus_ring);
+    ASSERT_TRUE(library_button_focus_ring->GetVisible());
+    EXPECT_FALSE(library_button_focus_ring->GetVisibleBounds().IsEmpty());
 
     CloseDeskBar(root, DeskBarViewBase::Type::kOverview);
   };
