944063b2d889a5a1113830cd7463196de5f80345
Aimen Moten <aimenmoten@google.com>
2024-08-02 15:31:14 +0000

    Displays issuer token count in private state tokens list item regardless of redemptions
    Screenshot: https://screenshot.googleplex.com/8JbCqt3DyfCfAhn.png
    
    Bug: 348590926
    
    Change-Id: I90f35340c74b334a7a14aeabdea5823214144fa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749715
    Commit-Queue: Aimen Moten <aimenmoten@google.com>
    Reviewed-by: Kevin Graney <kmg@google.com>
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336630}

--- chrome/browser/resources/privacy_sandbox/internals/private_state_tokens/test_data.ts
+++ chrome/browser/resources/privacy_sandbox/internals/private_state_tokens/test_data.ts
@@ -34,6 +34,11 @@ export const dummyListItemData: ListItem[] = [
     numTokens: 0,
     redemptions: [],  // No redemptions for this issuer
   },
+  {
+    issuerOrigin: 'issuer4.com',
+    numTokens: 1,
+    redemptions: [],  // No redemptions for this issuer
+  },
 ];
 
 export type {ListItem, Redemption};
--- chrome/test/data/webui/privacy_sandbox/internals/private_state_tokens/test_data.ts
+++ chrome/test/data/webui/privacy_sandbox/internals/private_state_tokens/test_data.ts
@@ -25,3 +25,9 @@ export const dummyListItemData: ListItem[] = [
     redemptions: [],  // No redemptions for this issuer
   },
 ];
+
+export const testListItemData: ListItem = {
+  issuerOrigin: 'issuer6.com',
+  numTokens: 1,
+  redemptions: [],  // No redemptions for this issuer
+};
