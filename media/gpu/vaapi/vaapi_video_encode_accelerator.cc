d207966dcbcf98c802628f8691f954c90dc7cb5b
Hang Nguyen <hnt@google.com>
2024-08-02 21:03:50 +0000

    vaapi: Set peak bps for bitrate allocation in VaapiVEA
    This CL specifies the peak bps for video bitrate allocation when
    VaapiVEA requests encoding parameter change.
    
    Bug: 348661735
    Change-Id: Id436afa47b77b2360cd14cee366715b4afad4835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760270
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336804}

--- media/gpu/vaapi/vaapi_video_encode_accelerator.cc
+++ media/gpu/vaapi/vaapi_video_encode_accelerator.cc
@@ -1069,6 +1069,7 @@ void VaapiVideoEncodeAccelerator::RequestEncodingParametersChange(
 
   VideoBitrateAllocation allocation(bitrate.mode());
   allocation.SetBitrate(0, 0, bitrate.target_bps());
+  allocation.SetPeakBps(bitrate.peak_bps());
   encoder_task_runner_->PostTask(
       FROM_HERE,
       base::BindOnce(
