2c240e0258159bafa8a76475261bc354b416022f
Jérôme Lebel <jlebel@chromium.org>
2024-08-02 18:59:18 +0000

    [iOS] Add notification methods in TrustedVaultClientBackend
    To be able to do EGTests in settings UI, related to
    http://crrev.com/c/5722711, FakeTrustedVaultClientBackend is now used
    in EGTests.
    This was introduced with http://crrev.com/c/5739155.
    The issue is FakeTrustedVaultClientBackend doesn't implement methods
    related to observers.
    
    This series of patches moves the following methods from
    InternalTrustedVaultClientBackend to TrustedVaultClientBackend:
    * InternalTrustedVaultClientBackend::AddObserver()
    * InternalTrustedVaultClientBackend::RemoveObserver()
    * InternalTrustedVaultClientBackend::NotifyKeysChanged()
    * InternalTrustedVaultClientBackend::NotifyRecoverabilityChanged()
    
    - http://crrev.com/i/7553542 Rename notification methods
    > http://crrev.com/c/5756603 Implement observer and notification methods
    - http://crrev.com/i/7553543 Remove notification methods
    - http://crrev.com/c/5756787 Remove virtual for observer methods
    
    Bug: 343007092
    Change-Id: I08a29d8d3925647761c7d7a92731119944b7369a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756603
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336741}

--- ios/chrome/browser/signin/model/trusted_vault_client_backend.h
+++ ios/chrome/browser/signin/model/trusted_vault_client_backend.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
+#import <map>
 #import <string>
 #import <vector>
 
@@ -52,9 +53,9 @@ class TrustedVaultClientBackend : public KeyedService {
 
   // Adds/removes observers.
   virtual void AddObserver(Observer* observer,
-                           const std::string& security_domain_path) = 0;
+                           const std::string& security_domain_path);
   virtual void RemoveObserver(Observer* observer,
-                              const std::string& security_domain_path) = 0;
+                              const std::string& security_domain_path);
 
   // Registers a delegate-like callback that implements device registration
   // verification.
@@ -115,6 +116,16 @@ class TrustedVaultClientBackend : public KeyedService {
   // Returns the member public key used to enroll the local device.
   virtual void GetPublicKeyForIdentity(id<SystemIdentity> identity,
                                        GetPublicKeyCallback callback) = 0;
+
+ protected:
+  // Functions to notify observers.
+  void NotifyKeysChanged(const std::string& security_domain_path);
+  void NotifyRecoverabilityChanged(const std::string& security_domain_path);
+
+ private:
+  // List of observers per security domain path.
+  std::map<std::string, base::ObserverList<Observer>>
+      observer_lists_per_security_domain_path_;
 };
 
 #endif  // IOS_CHROME_BROWSER_SIGNIN_MODEL_TRUSTED_VAULT_CLIENT_BACKEND_H_
