f8eafc04a8881a9b1a101e5b817508a63e59581d
Mickey Burks <mickeyburks@chromium.org>
2024-08-02 23:51:11 +0000

    Add default module for payment settings
    Adding a browser command to be triggered from the whats new page.
    This command will open the payments settings page in a new tab.
    
    To locally test:
    - Build patch and run Chrome (branded build).
    - Navigate to chrome://whats-new, open devtools in iframe, and run:
    
    window.top.postMessage(
      {'data': {'commandId': 14, 'clickInfo': {}}},
      'chrome://whats-new'
    );
    
    Low-Coverage-Reason: OTHER Internals page is not exposed to users
    Bug: 355629905
    Change-Id: Idebbec845d54c15ece148be7406a00aceb9aa69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743550
    Reviewed-by: Brendon Tiszka <tiszka@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336817}

--- chrome/browser/ui/webui/browser_command/browser_command_handler.cc
+++ chrome/browser/ui/webui/browser_command/browser_command_handler.cc
@@ -126,6 +126,9 @@ void BrowserCommandHandler::CanExecuteCommand(
     case Command::kOpenSafetyCheckFromWhatsNew:
       can_execute = true;
       break;
+    case Command::kOpenPaymentsSettings:
+      can_execute = true;
+      break;
   }
   std::move(callback).Run(can_execute);
 }
@@ -204,6 +207,10 @@ void BrowserCommandHandler::ExecuteCommandWithDisposition(
       break;
     case Command::kOpenSafetyCheckFromWhatsNew:
       NavigateToURL(GURL(chrome::GetSettingsUrl(chrome::kSafetyCheckSubPage)),
+                    disposition);
+      break;
+    case Command::kOpenPaymentsSettings:
+      NavigateToURL(GURL(chrome::GetSettingsUrl(chrome::kPaymentsSubPage)),
                     disposition);
       break;
     default:
