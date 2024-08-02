3237868f9838b4a3b8d3ccf3151326fe683f8725
Adem Derinel <derinel@google.com>
2024-08-02 06:08:01 +0000

    Add IWYU statement for base::raw_ptr
    clangd suggests to include "partition_alloc/pointers/raw_ptr.h" meanwhile docs suggest to include "base/memory/raw_ptr.h".
    Fix this with an IWYU pragma.
    
    Change-Id: I9b889a4a0cb2ce6cc569a927898aadfc264173e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753185
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Bartek Nowierski <bartekn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336458}

--- base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_cast.h
+++ base/allocator/partition_allocator/src/partition_alloc/pointers/raw_ptr_cast.h
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// IWYU pragma: private, include "base/memory/raw_ptr_cast.h"
+
 #ifndef PARTITION_ALLOC_POINTERS_RAW_PTR_CAST_H_
 #define PARTITION_ALLOC_POINTERS_RAW_PTR_CAST_H_
 
