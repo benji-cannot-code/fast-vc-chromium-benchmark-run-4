bcc1097b6c9d71443807c30d0d946a880b357c33
Qihui Zhao <qihuizhao@google.com>
2024-08-02 17:45:15 +0000

    Deprecate length variable from IBAN class
    This CL deprecates length_ variable from IBAN class as IBAN displayed
    identifier will not be constructed with length, the database migration
    will be the follow-up CL.
    
    Bug: 355071734
    Change-Id: I8157b25594b466143d8ce29125a96ad12326f672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749577
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1336693}

--- components/autofill/core/browser/autofill_test_utils.cc
+++ components/autofill/core/browser/autofill_test_utils.cc
@@ -290,7 +290,6 @@ Iban GetServerIban() {
   Iban iban(Iban::InstrumentId(1234567));
   iban.set_prefix(u"FR76");
   iban.set_suffix(u"0189");
-  iban.set_length(27);
   iban.set_nickname(u"My doctor's IBAN");
   return iban;
 }
@@ -299,7 +298,6 @@ Iban GetServerIban2() {
   Iban iban(Iban::InstrumentId(1234568));
   iban.set_prefix(u"BE71");
   iban.set_suffix(u"8676");
-  iban.set_length(16);
   iban.set_nickname(u"My sister's IBAN");
   return iban;
 }
@@ -308,7 +306,6 @@ Iban GetServerIban3() {
   Iban iban(Iban::InstrumentId(1234569));
   iban.set_prefix(u"DE91");
   iban.set_suffix(u"6789");
-  iban.set_length(22);
   iban.set_nickname(u"My IBAN");
   return iban;
 }
