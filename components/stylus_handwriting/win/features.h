25e0992f7da2b649709f9bfa01ba4ad70fe08bff
Adam Ettenberger <Adam.Ettenberger@microsoft.com>
2024-08-02 18:03:22 +0000

    Add stylus handwriting feature flag for Windows
    Bug: 355578906
    Change-Id: I9421c057a0ae6c09a9327bcaf94e763c1e560884
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751341
    Commit-Queue: Adam Ettenberger <Adam.Ettenberger@microsoft.com>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Reviewed-by: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336704}

---
+++ components/stylus_handwriting/win/features.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_STYLUS_HANDWRITING_WIN_FEATURES_H_
+#define COMPONENTS_STYLUS_HANDWRITING_WIN_FEATURES_H_
+
+#include "base/component_export.h"
+#include "base/feature_list.h"
+
+namespace stylus_handwriting::win {
+
+// Enables stylus writing for Windows platforms.
+COMPONENT_EXPORT(STYLUS_HANDWRITING_WIN)
+BASE_DECLARE_FEATURE(kStylusHandwritingWin);
+COMPONENT_EXPORT(STYLUS_HANDWRITING_WIN)
+extern bool IsStylusHandwritingWinEnabled();
+
+}  // namespace stylus_handwriting::win
+
+#endif  // COMPONENTS_STYLUS_HANDWRITING_WIN_FEATURES_H_
