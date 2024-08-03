1867c0a960c12b7f23f0267e027f82b450603995
dljames <dljames@chromium.org>
2024-08-03 00:20:39 +0000

    [TabGroupSync] Update ExtensionTabUtil to use Wrapper Service
    Update ExtensionTabUtil to use TabGroupServiceWrapper instead of direct
    usage of SavedTabGroupKeyedService.
    
    This is done to support the migration from STGKS -> TabGroupSyncService.
    
    Change-Id: Ieb39d270b730d4087254d5cd6ef53cc35bf1b5d7
    Bug: 350514491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759329
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336867}

--- chrome/browser/extensions/extension_tab_util.cc
+++ chrome/browser/extensions/extension_tab_util.cc
@@ -39,8 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/scoped_tabbed_browser_displayer.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_iterator.h"
-#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_keyed_service.h"
-#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_service_factory.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/tab_group_service_wrapper.h"
 #include "chrome/browser/ui/tabs/tab_enums.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_utils.h"
@@ -1165,12 +1164,12 @@ bool ExtensionTabUtil::TabIsInSavedTabGroup(content::WebContents* contents,
     tab_strip_model = browser->tab_strip_model();
   }
 
-  tab_groups::SavedTabGroupKeyedService* saved_tab_group_service =
-      tab_groups::SavedTabGroupServiceFactory::GetForProfile(
+  const auto wrapper_service =
+      tab_groups::TabGroupServiceWrapper::GetForProfile(
           tab_strip_model->profile());
 
   // If the service failed to start, then there are no saved tab groups.
-  if (!saved_tab_group_service) {
+  if (!wrapper_service) {
     return false;
   }
 
@@ -1182,7 +1181,7 @@ bool ExtensionTabUtil::TabIsInSavedTabGroup(content::WebContents* contents,
     return false;
   }
 
-  return saved_tab_group_service->model()->Contains(tab_group_id.value());
+  return wrapper_service->GetGroup(tab_group_id.value()).has_value();
 }
 
 }  // namespace extensions
