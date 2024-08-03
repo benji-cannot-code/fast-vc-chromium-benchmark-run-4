0ce4c396f130f2c737d7704ec67f47f36f123dbd
Joel Hockey <joelhockey@chromium.org>
2024-08-03 01:05:04 +0000

    Route events for both source and target RWH
    Drag and drop can start or end (or both) in a WebContents.
    
    It helps to keep a track of both source and target RWH so we can route
    the appropriate events to each.
    
    This fixes the code to call WebContents::DragSourceEndedAt() on the
    source RWH rather than that target RWH in DragEnded().
    
    This code is not especially related to the OOPIF changes, but it is
    guarded by the same flag for convenience so that we have a kill switch
    if needed.
    
    Bug: b/350860794
    Change-Id: I435dc3adb98154bb959a3d4fd336ccdb985538d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759138
    Auto-Submit: Joel Hockey <joelhockey@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336896}

--- content/browser/web_contents/web_contents_view_android.cc
+++ content/browser/web_contents/web_contents_view_android.cc
@@ -388,6 +388,7 @@ void WebContentsViewAndroid::StartDragging(
     const gfx::Rect& drag_obj_rect,
     const blink::mojom::DragEventSourceInfo& event_info,
     RenderWidgetHostImpl* source_rwh) {
+  current_source_rwh_for_drag_ = source_rwh->GetWeakPtr();
   if (!IsDragEnabledForDropData(drop_data)) {
     // Need to clear drag and drop state in blink.
     OnSystemDragEnded(source_rwh);
@@ -590,12 +591,12 @@ void WebContentsViewAndroid::DragEnteredCallback(
     return;
   }
 
-  current_rwh_for_drag_ = target_rwh->GetWeakPtr();
+  current_target_rwh_for_drag_ = target_rwh->GetWeakPtr();
 
   blink::DragOperationsMask allowed_ops =
       static_cast<blink::DragOperationsMask>(blink::kDragOperationCopy |
                                              blink::kDragOperationMove);
-  current_rwh_for_drag_->DragTargetDragEnterWithMetaData(
+  current_target_rwh_for_drag_->DragTargetDragEnterWithMetaData(
       drag_metadata_, location, screen_location, allowed_ops, 0,
       base::DoNothing());
 }
@@ -656,18 +657,18 @@ void WebContentsViewAndroid::DragUpdatedCallback(
     return;
   }
 
-  if (target_rwh != current_rwh_for_drag_.get()) {
-    if (current_rwh_for_drag_) {
+  if (target_rwh != current_target_rwh_for_drag_.get()) {
+    if (current_target_rwh_for_drag_) {
       gfx::PointF transformed_leave_point = location;
       static_cast<RenderWidgetHostViewBase*>(
           web_contents_->GetRenderWidgetHostView())
           ->TransformPointToCoordSpaceForView(
               location,
               static_cast<RenderWidgetHostViewBase*>(
-                  current_rwh_for_drag_->GetView()),
+                  current_target_rwh_for_drag_->GetView()),
               &transformed_leave_point);
-      current_rwh_for_drag_->DragTargetDragLeave(transformed_leave_point,
-                                                 screen_location);
+      current_target_rwh_for_drag_->DragTargetDragLeave(transformed_leave_point,
+                                                        screen_location);
     }
     DragEnteredCallback(location, screen_location, target);
   }
@@ -681,8 +682,9 @@ void WebContentsViewAndroid::DragUpdatedCallback(
 
 void WebContentsViewAndroid::OnDragExited() {
   if (drag_drop_oopif_enabled_) {
-    if (current_rwh_for_drag_) {
-      current_rwh_for_drag_->DragTargetDragLeave(gfx::PointF(), gfx::PointF());
+    if (current_target_rwh_for_drag_) {
+      current_target_rwh_for_drag_->DragTargetDragLeave(gfx::PointF(),
+                                                        gfx::PointF());
     }
   } else {
     web_contents_->GetRenderViewHost()->GetWidget()->DragTargetDragLeave(
@@ -726,10 +728,10 @@ void WebContentsViewAndroid::PerformDropCallback(
     return;
   }
 
-  if (target_rwh != current_rwh_for_drag_.get()) {
-    if (current_rwh_for_drag_) {
-      current_rwh_for_drag_->DragTargetDragLeave(*transformed_pt,
-                                                 screen_location);
+  if (target_rwh != current_target_rwh_for_drag_.get()) {
+    if (current_target_rwh_for_drag_) {
+      current_target_rwh_for_drag_->DragTargetDragLeave(*transformed_pt,
+                                                        screen_location);
     }
     DragEnteredCallback(location, screen_location, target);
   }
@@ -760,10 +762,12 @@ void WebContentsViewAndroid::OnSystemDragEnded(RenderWidgetHost* source_rwh) {
 
 void WebContentsViewAndroid::OnDragEnded() {
   if (drag_drop_oopif_enabled_) {
-    if (current_rwh_for_drag_) {
-      current_rwh_for_drag_->DragSourceEndedAt(
-          drag_location_, drag_screen_location_,
-          ui::mojom::DragOperation::kNone, base::DoNothing());
+    if (current_source_rwh_for_drag_) {
+      web_contents_->DragSourceEndedAt(
+          drag_location_.x(), drag_location_.y(), drag_screen_location_.x(),
+          drag_screen_location_.y(), ui::mojom::DragOperation::kNone,
+          current_source_rwh_for_drag_.get());
+      OnSystemDragEnded(current_source_rwh_for_drag_.get());
     }
     drag_security_info_.OnDragEnded();
   } else {
@@ -774,7 +778,8 @@ void WebContentsViewAndroid::OnDragEnded() {
   }
 
   drag_metadata_.clear();
-  current_rwh_for_drag_.reset();
+  current_source_rwh_for_drag_.reset();
+  current_target_rwh_for_drag_.reset();
   is_active_drag_ = false;
   drag_exceeded_movement_threshold_ = false;
   drag_entered_location_ = gfx::PointF();
