d4957f5747b8b0b8a0eba4bf8a41335378884e18
Brett Wilson <brettw@chromium.org>
2024-08-03 00:52:41 +0000

    Remove unnecessary "new tab ui" include.
    The TopSites extension API included new_tab_ui.h which was never
    referenced.
    
    Change-Id: Ib2b7df9ff1fd9260d16401e52464dee074ade373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760555
    Auto-Submit: Brett Wilson <brettw@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336890}

--- chrome/browser/extensions/api/top_sites/top_sites_api.cc
+++ chrome/browser/extensions/api/top_sites/top_sites_api.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/history/top_sites_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "components/history/core/browser/top_sites.h"
 
 namespace extensions {
