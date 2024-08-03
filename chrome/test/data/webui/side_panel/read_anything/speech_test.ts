d961816fb168f0c0fc86ec2a9f1b1470fad22b86
Lauren Winston <lwinston@google.com>
2024-08-03 00:05:17 +0000

    [Read Aloud] Ensure at least one language / voice is selected.
    On non-ChromeOS platforms, there may be no available voices when
    settings are first restored. Later, an onvoiceschanged event
    will get the correct voices.
    
    This means that previous voice selection settings are ignored. It
    also means that when Read Aloud is opened for the first time, before
    a voice has been selected or a language has been enabled, there will
    be no voices available, even though there are available languages.
    
    To fix this, check if we need to restore settings in onVoicesChanged.
    
    Bug: 355038327
    Change-Id: Ic337bb95c2d31b40843ba104ebcb6f3b66222c7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5743974
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Cr-Commit-Position: refs/heads/main@{#1336842}

--- chrome/test/data/webui/side_panel/read_anything/speech_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/speech_test.ts
@@ -86,6 +86,7 @@ suite('Speech', () => {
     chrome.readingMode.setContentForTesting(axTree, leafIds);
     speechSynthesis = new FakeSpeechSynthesis();
     app.synth = speechSynthesis;
+    speechSynthesis.setDefaultVoices();
 
     app.enabledLangs = ['en'];
     app.getSpeechSynthesisVoice();
@@ -377,6 +378,7 @@ suite('Speech', () => {
       // Remote voices already reduce the size of a speech segment to avoid
       // the bug where speech stops without an error callback.
       speechSynthesis.useLocalVoices();
+      speechSynthesis.setDefaultVoices();
       chrome.readingMode.onVoiceChange = () => {};
       emitEvent(
           app, 'select-voice',
