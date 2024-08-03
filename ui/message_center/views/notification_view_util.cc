32d6e07f62be9f1ec699428bfede8bf2d10e25e3
Sean Kau <skau@chromium.org>
2024-08-03 00:28:54 +0000

    Remove the kJelly feature flag in ui/message_center
    Feature is launched and can be removed.
    
    This CL was uploaded by git cl split.
    
    R=dewittj@chromium.org
    
    Bug: None
    Change-Id: Ic9cb9c3e7622f29f2989a7eb8c701c0313a756d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730428
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1336881}

--- ui/message_center/views/notification_view_util.cc
+++ ui/message_center/views/notification_view_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chromeos/constants/chromeos_features.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
@@ -36,8 +35,7 @@ std::unique_ptr<ui::Event> ConvertToBoundedLocatedEvent(const ui::Event& event,
 
 std::optional<size_t> GetLargeImageCornerRadius() {
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-  return chromeos::features::IsJellyEnabled() ? kJellyImageCornerRadius
-                                              : kImageCornerRadius;
+  return kJellyImageCornerRadius;
 #else
   return std::nullopt;
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
