033bfca182e2cd4bab269a5a522e7f53e40e10d7
Mike Dougherty <michaeldo@chromium.org>
2024-08-03 00:32:29 +0000

    Update GelocationLogger to cache last received value
    This allows access the last known authorization status at any time
    without the risk of introducing a hang which can occur when checking the
    status of CLLocationManager immediately after creation.
    
    Change-Id: I4f8f0069da7735e7179d7dc439b775fb2dfad592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758191
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1336887}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -66,7 +66,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/first_run/model/first_run.h"
 #import "ios/chrome/browser/first_run/ui_bundled/omnibox_position/promo/omnibox_position_choice_scene_agent.h"
-#import "ios/chrome/browser/geolocation/model/geolocation_logger.h"
+#import "ios/chrome/browser/geolocation/model/geolocation_manager.h"
 #import "ios/chrome/browser/history/ui_bundled/history_coordinator.h"
 #import "ios/chrome/browser/history/ui_bundled/history_coordinator_delegate.h"
 #import "ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_coordinator.h"
@@ -1112,9 +1112,8 @@ void OnListFamilyMembersResponse(
   [self.browserViewWrangler loadSession];
   [self createInitialUI:[self initialUIMode]];
 
-  // Make sure the geolocation controller is created to observe permission
-  // events.
-  [GeolocationLogger sharedInstance];
+  // Make sure the GeolocationManager is created to observe permission events.
+  [GeolocationManager sharedInstance];
 
   if (ShouldPromoManagerDisplayPromos()) {
     [sceneState addAgent:[[PromosManagerSceneAgent alloc]
