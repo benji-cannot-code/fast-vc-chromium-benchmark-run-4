b0d3fb13a5e9b7e7c0a52d2d64ad22282261a363
Dmitry Vykochko <vykochko@google.com>
2024-08-04 12:35:01 +0000

    [mullhet] Add password details bubble triggering API.
    This CL adds the API to trigger the password details bubble from
    `PasswordManagerClient`.
    
    Bug: 324242001
    Change-Id: Icf92ee69c6bf73674645e59d6369583a147bfc11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5703399
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337041}

--- chrome/browser/ui/passwords/manage_passwords_state.cc
+++ chrome/browser/ui/passwords/manage_passwords_state.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_manager_util.h"
 #include "components/password_manager/core/common/password_manager_ui.h"
+#include "url/gurl.h"
+#include "url/origin.h"
 
 using password_manager::PasswordForm;
 using password_manager::PasswordFormManagerForUI;
@@ -328,11 +330,18 @@ void ManagePasswordsState::ChooseCredential(const PasswordForm* form) {
   std::move(credentials_callback_).Run(form);
 }
 
+void ManagePasswordsState::OpenPasswordDetailsBubble(
+    const password_manager::PasswordForm& form) {
+  single_credential_mode_credential_ = form;
+  SetState(password_manager::ui::State::MANAGE_STATE);
+}
+
 void ManagePasswordsState::ClearData() {
   form_manager_.reset();
   local_credentials_forms_.clear();
   credentials_callback_.Reset();
   unsynced_credentials_.clear();
+  single_credential_mode_credential_.reset();
 }
 
 bool ManagePasswordsState::AddForm(const PasswordForm& form) {
--- chrome/browser/ui/passwords/manage_passwords_state.h
+++ chrome/browser/ui/passwords/manage_passwords_state.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_PASSWORDS_MANAGE_PASSWORDS_STATE_H_
 
 #include <memory>
+#include <optional>
 #include <vector>
 
 #include "base/functional/callback.h"
@@ -127,6 +128,9 @@ class ManagePasswordsState {
   // CREDENTIAL_REQUEST_STATE state.
   void ChooseCredential(const password_manager::PasswordForm* form);
 
+  // Move to MANAGE_STATE with initial credential to show its details.
+  void OpenPasswordDetailsBubble(const password_manager::PasswordForm& form);
+
   password_manager::ui::State state() const { return state_; }
   const std::vector<password_manager::PasswordForm>& unsynced_credentials()
       const {
@@ -152,6 +156,11 @@ class ManagePasswordsState {
   }
   void clear_selected_password() { selected_password_.reset(); }
 
+  const std::optional<password_manager::PasswordForm>&
+  single_credential_mode_credential() const {
+    return single_credential_mode_credential_;
+  }
+
   bool auth_for_account_storage_opt_in_failed() const {
     return auth_for_account_storage_opt_in_failed_;
   }
@@ -172,6 +181,10 @@ class ManagePasswordsState {
     return local_credentials_forms_;
   }
 
+  void ClearSingleCredentialModeCredential() {
+    single_credential_mode_credential_ = std::nullopt;
+  }
+
  private:
   // Removes all the PasswordForms stored in this object.
   void ClearData();
@@ -190,6 +203,10 @@ class ManagePasswordsState {
 
   // Contains password selected for moving to the account.
   std::unique_ptr<password_manager::PasswordForm> selected_password_;
+
+  // The credential for the bubble in the single credential mode.
+  std::optional<password_manager::PasswordForm>
+      single_credential_mode_credential_;
 
   // Contains all the current forms.
   std::vector<std::unique_ptr<password_manager::PasswordForm>>
