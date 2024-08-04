1121bc079b25397863c0d0b4ab7e774bac317d86
Muyao Xu <muyaoxu@google.com>
2024-08-04 02:05:45 +0000

    [Permission] Refactor ExtractServiceInfo() to use base::SplitString
    Bug: 354231463
    Change-Id: Id7671d242d8fb22970dbb8a03e4d7062c879215d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5747367
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337013}

--- chrome/browser/local_discovery/service_discovery_client_mac_unittest.mm
+++ chrome/browser/local_discovery/service_discovery_client_mac_unittest.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/local_discovery/service_discovery_client.h"
+
 #import <Cocoa/Cocoa.h>
 #include <stdint.h>
 
@@ -11,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/run_loop.h"
 #include "base/threading/thread.h"
-#include "chrome/browser/local_discovery/service_discovery_client.h"
 #include "chrome/browser/local_discovery/service_discovery_client_mac.h"
+#include "chrome/browser/local_discovery/service_discovery_client_mac_util.h"
 #import "chrome/browser/ui/cocoa/test/cocoa_test_helper.h"
 #include "content/public/test/browser_task_environment.h"
 #include "net/base/ip_endpoint.h"
