f8f4a7d81b33aa40f2e00703916a29bb0ce24737
Tanushree Bisht <tanub@google.com>
2024-08-05 15:49:36 +0000

    Introduce PageLoadStatistics to FPF ThrottleManager.
    This will allow us to obtain PageLoad histogram metrics for AFP.
    
    Bug: 40280666
    Change-Id: If36bebeb1d809da7ef78d54125a9e63c86fd3a65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757685
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Commit-Queue: Tanushree Bisht <tanub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337331}

--- components/fingerprinting_protection_filter/browser/throttle_manager.h
+++ components/fingerprinting_protection_filter/browser/throttle_manager.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/supports_user_data.h"
+#include "components/subresource_filter/content/shared/browser/page_load_statistics.h"
 #include "components/subresource_filter/core/browser/verified_ruleset_dealer.h"
 #include "components/subresource_filter/core/mojom/subresource_filter.mojom.h"
 #include "content/public/browser/document_user_data.h"
@@ -266,6 +267,8 @@ class ThrottleManager : public base::SupportsUserData::Data {
   // activation is triggered. Will go away when there are no more activated
   // RenderFrameHosts (i.e. activated_frame_hosts_ is empty).
   std::unique_ptr<subresource_filter::VerifiedRuleset::Handle> ruleset_handle_;
+
+  std::unique_ptr<subresource_filter::PageLoadStatistics> statistics_;
 
   // TODO(https://crbug.com/40280666): Add statistics once they are available in
   // a shared SubresourceFilter directory.
