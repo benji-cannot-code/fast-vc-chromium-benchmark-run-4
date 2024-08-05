84dbda11f85bfc90a38720f15199b5cdef0af423
Anthony Garant <anthonygarant@chromium.org>
2024-08-05 19:18:31 +0000

    Add Attribution Support to requests handled by a service worker
    Adds the attribution support on fetch_api_request data. As such, request
    initiated outside of a service worker and then handled by service worker
    will have the attribution support already set.
    
    A remaining limitation is that requests sent from a service worker
    still won't have the attribution support set. However, attribution
    reporting isn't currently supported for these requests.
    
    Bug: 352077671
    Change-Id: I9d750f06902130bcfbc1752508f4980d9e533bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5730940
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Anthony Garant <anthonygarant@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337415}

--- third_party/blink/renderer/core/fetch/request.cc
+++ third_party/blink/renderer/core/fetch/request.cc
@@ -112,6 +112,7 @@ FetchRequestData* CreateCopyOfFetchRequestDataForFetch(
   request->SetTrustTokenParams(original->TrustTokenParams());
   request->SetAttributionReportingEligibility(
       original->AttributionReportingEligibility());
+  request->SetAttributionReportingSupport(original->AttributionSupport());
   request->SetServiceWorkerRaceNetworkRequestToken(
       original->ServiceWorkerRaceNetworkRequestToken());
 
