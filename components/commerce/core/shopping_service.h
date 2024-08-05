17ccf2b3004d0537fffae470253616d401b7ee99
Matt Jones <mdjones@chromium.org>
2024-08-05 17:29:01 +0000

    Remove entries from "recently viewed" when deleted from history
    This patch has the shopping service listen to deletion events in
    history. If an item is removed from history, it is also removed from
    the list of recently viewed tabs in product specifications. This
    also applies when deleting all history.
    
    Bug: 349629913
    Change-Id: I40b48738cf73183e76e78bcfd48d39df84ac9352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664836
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337379}

--- components/commerce/core/shopping_service.h
+++ components/commerce/core/shopping_service.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/subscriptions/commerce_subscription.h"
 #include "components/commerce/core/web_extractor.h"
 #include "components/history/core/browser/history_service.h"
+#include "components/history/core/browser/history_service_observer.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/unified_consent/consent_throttle.h"
@@ -193,7 +194,9 @@ using UrlProductIdentifierTupleCallback =
 //         browser()->profile()));
 // clang-format on
 
-class ShoppingService : public KeyedService, public base::SupportsUserData {
+class ShoppingService : public KeyedService,
+                        public base::SupportsUserData,
+                        public history::HistoryServiceObserver {
  public:
   ShoppingService(
       const std::string& country_on_startup,
@@ -426,6 +429,10 @@ class ShoppingService : public KeyedService, public base::SupportsUserData {
   virtual ProductSpecificationsService* GetProductSpecificationsService();
 
   virtual ClusterManager* GetClusterManager();
+
+  // history::HistoryServiceObserver:
+  void OnHistoryDeletions(history::HistoryService* history_service,
+                          const history::DeletionInfo& deletion_info) override;
 
   // Get a weak pointer for this service instance.
   base::WeakPtr<ShoppingService> AsWeakPtr();
@@ -697,6 +704,10 @@ class ShoppingService : public KeyedService, public base::SupportsUserData {
   // This is used to avoid repeated calls to get product info for the same URL.
   std::map<GURL, std::vector<ProductInfoCallback>>
       on_demand_product_info_callbacks_;
+
+  base::ScopedObservation<history::HistoryService,
+                          history::HistoryServiceObserver>
+      history_service_observation_{this};
 
   // Ensure certain functions are being executed on the same thread.
   SEQUENCE_CHECKER(sequence_checker_);
