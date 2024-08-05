452bb4e1772cce8e53be25a316387c59e662cec8
Erik Chen <erikchen@chromium.org>
2024-08-05 01:55:45 +0000

    Modularize //chrome/browser/task_manager.
    This CL is a refactor with no intended user-visible change.
    
    In addition to the modularization, this CL makes the following small
    changes:
    * task_manager::WebContentsTags is no longer compiled onto or referenced
      from android. This seems to have been an oversight.
    * To avoid circular dependencies in headers, process_snapshot_server.h
      and process_snapshot_server.cc are pulled into a new target
      //chrome/browser/ash:primitives.
    * profile_manager_observer.h is pulled into
      //chrome/browser/profiles:profile.
    
    Bug: 353332589, 356747849
    Change-Id: Ie04eeda098a28658b134092fa0e74609db84b1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757304
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1337111}

--- chrome/browser/task_manager/providers/web_contents/background_contents_task.h
+++ chrome/browser/task_manager/providers/web_contents/background_contents_task.h
@@ -6,8 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_TASK_MANAGER_PROVIDERS_WEB_CONTENTS_BACKGROUND_CONTENTS_TASK_H_
 #define CHROME_BROWSER_TASK_MANAGER_PROVIDERS_WEB_CONTENTS_BACKGROUND_CONTENTS_TASK_H_
 
-#include "chrome/browser/background/background_contents.h"
 #include "chrome/browser/task_manager/providers/web_contents/renderer_task.h"
+
+class BackgroundContents;
 
 namespace task_manager {
 
