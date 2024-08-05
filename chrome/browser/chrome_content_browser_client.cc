fadb33a6584628dcc79bf209bf9d551e118b6ba6
Sam Davis Omekara <samomekarajr@microsoft.com>
2024-08-05 21:58:55 +0000

    Fix forced colors disabled web pref
    This CL addresses an issue where the `is_forced_colors_disabled` web
    pref could be true even when forced colors mode is not enabled. The fix
    involves adding a boolean parameter to the
    `ShouldDisableForcedColorsForWebContent` function to indicate forced
    colors mode. The function now returns early if forced colors mode is
    not enabled.
    
    Bug: 40779801
    Change-Id: Ic373193233bd75e073f3141939bcec3a2ef8a5fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757744
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1337521}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -3821,8 +3821,9 @@ bool ChromeContentBrowserClient::ShouldDenyRequestOnCertificateError(
 
 namespace {
 
-bool ShouldDisableForcedColorsForWebContent(content::WebContents* contents) {
-  if (!contents) {
+bool ShouldDisableForcedColorsForWebContent(content::WebContents* contents,
+                                            bool in_forced_colors) {
+  if (!contents || !in_forced_colors) {
     return false;
   }
 
@@ -3860,7 +3861,8 @@ bool ShouldDisableForcedColorsForWebContent(content::WebContents* contents) {
 bool IsForcedColorsEnabledForWebContent(content::WebContents* contents,
                                         const ui::NativeTheme* native_theme) {
   return native_theme->InForcedColorsMode() &&
-         !ShouldDisableForcedColorsForWebContent(contents);
+         !ShouldDisableForcedColorsForWebContent(contents,
+                                                 /*in_forced_colors=*/true);
 }
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -4542,8 +4544,8 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
   web_prefs->in_forced_colors =
       IsForcedColorsEnabledForWebContent(web_contents, GetWebTheme());
 
-  web_prefs->is_forced_colors_disabled =
-      ShouldDisableForcedColorsForWebContent(web_contents);
+  web_prefs->is_forced_colors_disabled = ShouldDisableForcedColorsForWebContent(
+      web_contents, GetWebTheme()->InForcedColorsMode());
 
   UpdatePreferredColorScheme(
       web_prefs,
@@ -4645,8 +4647,8 @@ bool ChromeContentBrowserClient::OverrideWebPreferencesAfterNavigation(
   prefs_changed |= (web_prefs->in_forced_colors != in_forced_colors);
   web_prefs->in_forced_colors = in_forced_colors;
 
-  const bool is_forced_colors_disabled =
-      ShouldDisableForcedColorsForWebContent(web_contents);
+  const bool is_forced_colors_disabled = ShouldDisableForcedColorsForWebContent(
+      web_contents, GetWebTheme()->InForcedColorsMode());
   prefs_changed |=
       (web_prefs->is_forced_colors_disabled != is_forced_colors_disabled);
   web_prefs->is_forced_colors_disabled = is_forced_colors_disabled;
