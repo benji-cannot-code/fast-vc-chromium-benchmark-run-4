6d481cab7d90a02c6af58d59decb78427c7cfd0b
Nikhil Nayunigari <nikhilcn@google.com>
2024-08-05 08:02:01 +0000

    [CrOS Cellular] Add metric to track APN policy state at login
    When user logs in, these changes will get the state of the
    AllowAPNModification policy and will log a metric.
    
    Bug=b/162365553
    
    Change-Id: I30b6d02e4559a8582a8e723c7ad00bcb268659ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758066
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337177}

--- chromeos/ash/components/network/managed_network_configuration_handler_impl.cc
+++ chromeos/ash/components/network/managed_network_configuration_handler_impl.cc
@@ -1074,7 +1074,6 @@ ManagedNetworkConfigurationHandlerImpl::GetAllowTextMessages() const {
 }
 
 bool ManagedNetworkConfigurationHandlerImpl::AllowApnModification() const {
-  CHECK(ash::features::IsApnRevampAndAllowApnModificationPolicyEnabled());
   return FindGlobalPolicyBool(
              ::onc::global_network_config::kAllowAPNModification)
       .value_or(true);
