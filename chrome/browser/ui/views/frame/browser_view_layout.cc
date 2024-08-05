e7893a5341cf86b91b48fc1b6d466c71669c9405
Keren Zhu <kerenzhu@chromium.org>
2024-08-05 05:30:00 +0000

    views: update modal dialog position on browser window move
    This CL updates the dialog position whenever the browser window moves.
    This fixes the issue of misplaced modal dialogs when the browser window
    is dragging across window boundaries.
    
    For security reason, the modal dialog was re-positioned to avoid
    partially visible on a screen when the browser window switched screen.
    This placed the dialog in a temporary place, which should be updated as
    soon as the browser becomes fully visible again on a screen.
    
    Bug: 351852291
    Change-Id: Id3c7bfe480adfb1707aeed3dcf1a8006590df08b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761601
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337142}

--- chrome/browser/ui/views/frame/browser_view_layout.cc
+++ chrome/browser/ui/views/frame/browser_view_layout.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/i18n/rtl.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
+#include "base/scoped_observation.h"
 #include "base/trace_event/common/trace_event_common.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
@@ -48,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/view_utils.h"
 #include "ui/views/widget/widget.h"
+#include "ui/views/widget/widget_observer.h"
 #include "ui/views/window/client_view.h"
 #include "ui/views/window/hit_test_utils.h"
 
@@ -79,11 +81,18 @@ bool ConvertedHitTest(views::View* src, views::View* dst, gfx::Point* point) {
 constexpr int BrowserViewLayout::kMainBrowserContentsMinimumWidth;
 
 class BrowserViewLayout::WebContentsModalDialogHostViews
-    : public WebContentsModalDialogHost {
+    : public WebContentsModalDialogHost,
+      public views::WidgetObserver {
  public:
   explicit WebContentsModalDialogHostViews(
       BrowserViewLayout* browser_view_layout)
-      : browser_view_layout_(browser_view_layout) {}
+      : browser_view_layout_(browser_view_layout) {
+    // browser_view might be nullptr in unit tests.
+    if (browser_view_layout->browser_view_) {
+      browser_widget_observation_.Observe(
+          browser_view_layout->browser_view_->GetWidget());
+    }
+  }
 
   WebContentsModalDialogHostViews(const WebContentsModalDialogHostViews&) =
       delete;
@@ -129,6 +138,20 @@ class BrowserViewLayout::WebContentsModalDialogHostViews
         browser_view_layout_->delegate_->GetHostViewForAnchoring());
   }
 
+  // views::WidgetObserver:
+  void OnWidgetDestroying(views::Widget* browser_widget) override {
+    browser_widget_observation_.Reset();
+  }
+  void OnWidgetBoundsChanged(views::Widget* browser_widget,
+                             const gfx::Rect& new_bounds) override {
+    // Update the modal dialogs' position when the browser window bounds change.
+    // This is used to adjust the modal dialog's position when the browser
+    // window is being dragged across screen boundaries. We avoid having the
+    // modal dialog partially visible as it may display security-sensitive
+    // information.
+    NotifyPositionRequiresUpdate();
+  }
+
  private:
   gfx::NativeView GetHostView() const override {
     views::Widget* const host_widget = GetHostWidget();
@@ -144,6 +167,8 @@ class BrowserViewLayout::WebContentsModalDialogHostViews
   }
 
   const raw_ptr<BrowserViewLayout> browser_view_layout_;
+  base::ScopedObservation<views::Widget, views::WidgetObserver>
+      browser_widget_observation_{this};
 
   base::ObserverList<ModalDialogHostObserver>::Unchecked observer_list_;
 };
