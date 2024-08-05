aa6bf82cde275b0d17e0206f91f828662fa07620
Mingyu Lei <leimy@chromium.org>
2024-08-05 03:37:56 +0000

    Prompt API: fix crash due to moved receiver
    The `CreateTextSessionInternal` doesn't need to take the receiver
    since we are binding it from the caller now.
    
    This is essentially cherry-picking part of the https://chromium-review.googlesource.com/c/chromium/src/+/5739986/3..4 .
    
    Bug: 352461866, 357174789
    Change-Id: I1fe67802a12001e7f2e5d3611ead1d3e3657f6f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759006
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337127}

--- chrome/browser/ai/ai_manager_keyed_service.cc
+++ chrome/browser/ai/ai_manager_keyed_service.cc
@@ -129,7 +129,6 @@ void AIManagerKeyedService::CanCreateTextSession(
 }
 
 std::unique_ptr<AITextSession> AIManagerKeyedService::CreateTextSessionInternal(
-    mojo::PendingReceiver<blink::mojom::AITextSession> receiver,
     const blink::mojom::AITextSessionSamplingParamsPtr& sampling_params,
     const std::optional<const AITextSession::Context>& context) {
   CHECK(browser_context_);
@@ -167,10 +166,15 @@ void AIManagerKeyedService::CreateTextSession(
     const std::optional<std::string>& system_prompt,
     CreateTextSessionCallback callback) {
   std::unique_ptr<AITextSession> session =
-      CreateTextSessionInternal(std::move(receiver), sampling_params);
+      CreateTextSessionInternal(sampling_params);
   if (!session) {
     std::move(callback).Run(false);
+    return;
   }
+
+  // TODO(crbug.com/356809696): instead of using `mojo::MakeSelfOwnedReceiver`,
+  // the session's lifetime should be associated with either the host of the
+  // document or worker.
 
   if (!system_prompt.has_value()) {
     // The new `AITextSession` shares the same lifetime with the `receiver`.
@@ -235,7 +239,7 @@ void AIManagerKeyedService::CreateTextSessionForCloning(
     const AITextSession::Context& context,
     base::OnceCallback<void(bool)> callback) {
   std::unique_ptr<AITextSession> session =
-      CreateTextSessionInternal(std::move(receiver), sampling_params, context);
+      CreateTextSessionInternal(sampling_params, context);
   if (!session) {
     std::move(callback).Run(false);
     return;
