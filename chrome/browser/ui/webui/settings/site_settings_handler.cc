250e8a6b9ac75333244df05ff484a9fb10991606
Jan Lanik <janlanik@google.com>
2024-08-05 14:51:25 +0000

    Limiting the UI changes to CrOS
    Limiting the UI changes for browser permission consistency to CrOS.
    
    Bug: b/357550964
    Change-Id: Iff6ace2095f43691282225042c154552a34f57a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5762277
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Jan Láník <janlanik@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337303}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -2501,6 +2501,8 @@ base::Value SiteSettingsHandler::GetOSGlobalPermissionStatus() {
   // TODO(b/331784136): Make the settings link clickable.
   base::Value::Dict block_messages_dict;
 
+#if BUILDFLAG(IS_CHROMEOS)
+  // This functionality is targeting cros.
   if (system_permission_settings::IsDenied(
           ContentSettingsType::MEDIASTREAM_CAMERA)) {
     block_messages_dict.Set(
@@ -2530,6 +2532,7 @@ base::Value SiteSettingsHandler::GetOSGlobalPermissionStatus() {
             l10n_util::GetStringUTF16(
                 IDS_PAGE_INFO_SETTINGS_OF_A_SYSTEM_LINK))));
   }
+#endif
 
   return base::Value(std::move(block_messages_dict));
 }
--- chrome/browser/ui/webui/settings/site_settings_handler_unittest.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler_unittest.cc
@@ -6422,6 +6422,7 @@ TEST_F(SiteSettingsHandlerTest, IsolatedWebAppClearUnpartitionedUsage) {
       /*expected_fps_member_count_string=*/"", /*expected_fps_policy=*/false);
 }
 
+#if BUILDFLAG(IS_CHROMEOS)
 class SiteSettingsGlobalPermissionTest
     : public SiteSettingsHandlerBaseTest,
       public testing::WithParamInterface<std::tuple<bool, bool, bool>> {
@@ -6476,5 +6477,6 @@ INSTANTIATE_TEST_SUITE_P(All,
                          testing::Combine(testing::Bool(),
                                           testing::Bool(),
                                           testing::Bool()));
+#endif
 
 }  // namespace settings
