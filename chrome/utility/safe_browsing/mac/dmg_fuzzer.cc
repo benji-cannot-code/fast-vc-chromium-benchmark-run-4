90f900813685e041fcd6705a4a40d79c3e7d9f0e
Daniel Rubery <drubery@chromium.org>
2024-08-05 17:10:48 +0000

    Spanify safe_browsing fuzzers
    These fuzzers take data directly from libfuzzer. To be consistent with
    other fuzzers, we immediately convert it to a span.
    
    Bug: 356368033
    Change-Id: Ieb1732638e8afc91b5e96b0d8251af9c799bcf73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757979
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Javier Castro <jacastro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337367}

--- chrome/utility/safe_browsing/mac/dmg_fuzzer.cc
+++ chrome/utility/safe_browsing/mac/dmg_fuzzer.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/356368033): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <stddef.h>
 #include <stdint.h>
 
@@ -31,8 +26,9 @@ extern "C" int LLVMFuzzerInitialize(int* argc, char*** argv) {
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::span<const uint8_t> stream(data, size);
-  safe_browsing::dmg::MemoryReadStream input(stream);
+  // SAFETY: libfuzzer guarantees a valid pointer and size pair.
+  safe_browsing::dmg::MemoryReadStream input(
+      UNSAFE_BUFFERS(base::span(data, size)));
   safe_browsing::dmg::UDIFParser udif_parser(&input);
 
   if (!udif_parser.Parse())
--- chrome/utility/safe_browsing/mac/hfs_fuzzer.cc
+++ chrome/utility/safe_browsing/mac/hfs_fuzzer.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/356368033): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/utility/safe_browsing/mac/hfs.h"
 
 #include <stddef.h>
@@ -22,8 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/libfuzzer/libfuzzer_exports.h"
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  base::span<const uint8_t> stream(data, size);
-  safe_browsing::dmg::MemoryReadStream input(stream);
+  // SAFETY: libfuzzer guarantees a valid pointer and size pair.
+  safe_browsing::dmg::MemoryReadStream input(
+      UNSAFE_BUFFERS(base::span(data, size)));
   safe_browsing::dmg::HFSIterator hfs_iterator(&input);
 
   if (!hfs_iterator.Open())
