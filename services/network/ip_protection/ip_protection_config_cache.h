a137df255a86177d5e0b33fbf9c32e218b4f615b
Abhi Patel <abhipatel@google.com>
2024-08-05 14:10:03 +0000

    Prefactor `IpProtectionProxyListManager` to utilize token caching by geo.
    Token caching by geo requires additional set up for when a geo changes. This cl focuses on adding the logic for such features inside the proxy list manager. All functionality is behind a feature parameter. At this point, enabling this feature will cause errors. The final upcoming cl will finally utilize the prefactors made in this cl and the prior prefactor @ cl 5692779 of the `IpProtectionTokenCacheManager`.
    R=abhipatel@chromium.org
    
    Bug: 338975162
    Change-Id: I78665f20d81e636b214cb5ac98abd52fa368e2bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751242
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Abhi Patel <abhipatel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337283}

--- services/network/ip_protection/ip_protection_config_cache.h
+++ services/network/ip_protection/ip_protection_config_cache.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 #include <optional>
+#include <vector>
 
 #include "base/component_export.h"
 #include "services/network/ip_protection/ip_protection_data_types.h"
@@ -62,6 +63,9 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) IpProtectionConfigCache {
   // Fetch the proxy chain list manager.
   virtual IpProtectionProxyListManager*
   GetIpProtectionProxyListManagerForTesting() = 0;
+
+  // Returns the current geo of the config cache.
+  virtual const std::string& CurrentGeoForTesting() = 0;
 
   // Check whether a proxy chain list is available.
   virtual bool IsProxyListAvailable() = 0;
