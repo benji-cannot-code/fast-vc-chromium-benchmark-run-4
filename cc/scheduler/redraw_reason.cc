0033e2a61245c8ad754fc346f6bb93b616d181ab
Bo Liu <boliu@chromium.org>
2024-08-05 19:13:50 +0000

    cc: Compute has_only_content_frame_interval_updates
    Rough idea is track RedrawReason for each frame, and split out the
    interested reason (animated image) from the rest. If there is no "rest",
    aka kUntracked, then has_only_content_frame_interval_updates is true.
    
    Keep multiple EnumSets in SchedulerStateMachine for impl invalidation,
    activation, and needs redraw. Merge reasons where appropriate (eg
    activation reason is merged into redraw reason on activation).
    
    Reasons set is then passed to LayerTreeHostImpl in FrameData.
    
    This CL plus
    https://chromium-review.googlesource.com/c/chromium/src/+/5739006
    already landed has reporting gif frame rate working end-to-end.
    
    Bug: 346732738
    Change-Id: Ieba03c20303ceb5bdd5e8c09fedbc27aedf5768c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756231
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337407}

--- cc/scheduler/redraw_reason.cc
+++ cc/scheduler/redraw_reason.cc
@@ -11,6 +11,8 @@ std::string RedrawReasonToString(RedrawReason reason) {
   switch (reason) {
     case RedrawReason::kUntracked:
       return "kUntracked";
+    case RedrawReason::kAnimatedImage:
+      return "kAnimatedImage";
   }
 }
 
