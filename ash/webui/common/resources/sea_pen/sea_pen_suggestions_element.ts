7384aa3d0db8a75c81cd2778cc8918236dfce44e
Erica Lee <ericamlee@google.com>
2024-08-05 23:53:19 +0000

    p13n: fix suggestion shuffling
    Suggestions were not being shuffled on load -- there was some code for
    shuffling the suggestions when thumbnails appeared, which did not work
    because the suggestions were hidden at that point.
    
    Removed the broken code and updated the suggestions to be shuffled every
    time they appear. Also added some more legit suggestions.
    http://shortn/_xmL4H34WnV
    
    BUG=b:357137465
    TEST=browser_tests *SeaPenInputQuery*
    
    Change-Id: Ida3ffa734a62ac03aef2258de3c0fa9b38508532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759359
    Commit-Queue: Erica Lee <ericamlee@google.com>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337573}

--- ash/webui/common/resources/sea_pen/sea_pen_suggestions_element.ts
+++ ash/webui/common/resources/sea_pen/sea_pen_suggestions_element.ts
@@ -18,10 +18,9 @@ import {CrButtonElement} from 'chrome://resources/ash/common/cr_elements/cr_butt
 import {assert} from 'chrome://resources/js/assert.js';
 import {IronA11yKeysElement} from 'chrome://resources/polymer/v3_0/iron-a11y-keys/iron-a11y-keys.js';
 import {IronSelectorElement} from 'chrome://resources/polymer/v3_0/iron-selector/iron-selector.js';
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
-import {SeaPenThumbnail} from './sea_pen.mojom-webui.js';
 import {logSuggestionClicked, logSuggestionShuffleClicked} from './sea_pen_metrics_logger.js';
-import {WithSeaPenStore} from './sea_pen_store.js';
 import {getTemplate} from './sea_pen_suggestions_element.html.js';
 import {SEA_PEN_SUGGESTIONS} from './sea_pen_untranslated_constants.js';
 import {isArrayEqual, shuffle} from './sea_pen_utils.js';
@@ -51,7 +50,7 @@ export interface SeaPenSuggestionsElement {
   };
 }
 
-export class SeaPenSuggestionsElement extends WithSeaPenStore {
+export class SeaPenSuggestionsElement extends PolymerElement {
   static get is() {
     return 'sea-pen-suggestions';
   }
@@ -69,11 +68,6 @@ export class SeaPenSuggestionsElement extends WithSeaPenStore {
 
       hiddenSuggestions_: Object,
 
-      thumbnails_: {
-        type: Object,
-        observer: 'resetSuggestions_',
-      },
-
       /** The button currently highlighted by keyboard navigation. */
       ironSelectedSuggestion_: Object,
     };
@@ -81,7 +75,6 @@ export class SeaPenSuggestionsElement extends WithSeaPenStore {
 
   private suggestions_: string[];
   private hiddenSuggestions_: Set<string>;
-  private thumbnails_: SeaPenThumbnail[]|null;
   private ironSelectedSuggestion_: CrButtonElement;
 
   override ready() {
@@ -91,14 +84,8 @@ export class SeaPenSuggestionsElement extends WithSeaPenStore {
 
   override connectedCallback(): void {
     super.connectedCallback();
-    this.watch<SeaPenSuggestionsElement['thumbnails_']>(
-        'thumbnails_', state => state.thumbnails);
     this.hiddenSuggestions_ = new Set();
     this.suggestions_ = [...SEA_PEN_SUGGESTIONS];
-  }
-
-  private resetSuggestions_() {
-    this.hiddenSuggestions_ = new Set();
     this.shuffleSuggestions_();
   }
 
