2126bbb8ce42812dd3970bec42341fd3cdde7946
dljames <dljames@chromium.org>
2024-08-05 21:22:46 +0000

    [TabGroupSync] Update BookmarkBarView to use Wrapper Service
    Update BookmarkBarView to use TabGroupServiceWrapper instead of direct
    usage of SavedTabGroupKeyedService.
    
    This is done to support the migration from STGKS -> TabGroupSyncService.
    
    Change-Id: If8f68ee9d3d066b6b5a1e95f85a615c04dbb7681
    Bug: 350514491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760410
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337493}

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -54,7 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/color/chrome_color_id.h"
 #include "chrome/browser/ui/layout_constants.h"
-#include "chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_service_factory.h"
+#include "chrome/browser/ui/tabs/saved_tab_groups/tab_group_service_wrapper.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/view_ids.h"
@@ -2221,15 +2221,14 @@ void BookmarkBarView::MaybeShowSavedTabGroupsIntroPromo() const {
   }
 
   // Check whether to show the synced, or unsyned version of the promo.
-  tab_groups::SavedTabGroupKeyedService* const service =
-      tab_groups::SavedTabGroupServiceFactory::GetForProfile(
-          browser_->profile());
-  if (!service) {
+  const auto wrapper_service =
+      tab_groups::TabGroupServiceWrapper::GetForProfile(browser_->profile());
+  if (!wrapper_service) {
     return;
   }
 
   // In order for IPH's for V2 to show up, there must be at least 1 group.
-  if (service->model()->saved_tab_groups().empty()) {
+  if (wrapper_service->GetAllGroups().empty()) {
     return;
   }
 
@@ -2243,7 +2242,7 @@ void BookmarkBarView::MaybeShowSavedTabGroupsIntroPromo() const {
   // substitutions, add them in.
 
   // If tabs groups are syncing...
-  if (service->AreSavedTabGroupsSynced()) {
+  if (wrapper_service->AreSavedTabGroupsSyncedForProfile(browser_->profile())) {
     // Anchor the IPH to the bookmarks bar if the everything button is visible.
     // Otherwise, anchor to the AppMenu.
     if (everything_button_is_visible) {
