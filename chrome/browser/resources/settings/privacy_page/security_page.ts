e9e02d78a22c65e85a1527658e8653688d2ab69c
Chris Thompson <cthomp@chromium.org>
2024-08-05 22:07:30 +0000

    Update HTTPS-First Mode settings to include Balanced Mode
    This replaces the previously prototyped HFM-in-Incognito tri-state
    settings UI with a new toggle+selection UI. A main toggle button
    controls whether to show a group of radio buttons that allow the user
    to choose between "Strict Mode" (the existing HFM) and the new
    "Balanced Mode" (where we try to show fewer warnings). When the main
    toggle is off, the radio buttons are hidden and both strict and
    balanced mode prefs are disabled. The new settings UI includes new
    strings (although they are not finalized yet).
    
    This also updates the generated_https_first_mode_pref.cc logic to map
    between the three settings states (ENABLED_FULL, ENABLED_BALANCED, and
    DISABLED) and the two prefs (kHttpsOnlyModeEnabled and
    kHttpsFirstBalancedMode).
    
    This also hooks up the kHttpsFirstBalancedMode pref to control whether
    balanced mode interstitials be triggered, now that it is
    user-controllable.
    
    Finally, this replaces the previous HFM-in-Incognito pref tests (which
    are obsolete) to instead test that the Balanced Mode pref/setting is
    correctly logged.
    
    All of this is gated behind the kHttpsFirstBalancedMode feature flag.
    
    Screenshot:
    https://drive.google.com/file/d/1jl8rAcSRLTEd-gVkRilooPIgiBWRmMmE/view?usp=sharing
    
    Bug: 349860796
    Change-Id: Ic848c7fc1f28b2df492bb375f993f975cd5b3fea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758682
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337527}

--- chrome/browser/resources/settings/privacy_page/security_page.ts
+++ chrome/browser/resources/settings/privacy_page/security_page.ts
@@ -59,8 +59,10 @@ export enum SafeBrowsingSetting {
  */
 export enum HttpsFirstModeSetting {
   DISABLED = 0,
-  ENABLED_INCOGNITO = 1,
+  // DEPRECATED: A separate Incognito setting never shipped.
+  // ENABLED_INCOGNITO = 1,
   ENABLED_FULL = 2,
+  ENABLED_BALANCED = 3,
 }
 
 export interface SettingsSecurityPageElement {
@@ -148,6 +150,14 @@ export class SettingsSecurityPageElement extends
       httpsFirstModeSettingEnum_: {
         type: Object,
         value: HttpsFirstModeSetting,
+      },
+
+      /**
+       * Setting for HTTPS-First Mode when the toggle is off.
+       */
+      httpsFirstModeUncheckedValues_: {
+        type: Array,
+        value: () => [HttpsFirstModeSetting.DISABLED],
       },
 
       enableHttpsFirstModeNewSettings_: {
@@ -312,11 +322,11 @@ export class SettingsSecurityPageElement extends
       this.safeBrowsingStateOnOpen_ = prefValue;
 
       // The HTTPS-First Mode generated pref should never be set to
-      // ENABLED_INCOGNITO if the feature flag is not enabled.
+      // ENABLED_BALANCED if the feature flag is not enabled.
       if (!loadTimeData.getBoolean('enableHttpsFirstModeNewSettings')) {
         assert(
             this.getPref('generated.https_first_mode_enabled').value !==
-            HttpsFirstModeSetting.ENABLED_INCOGNITO);
+            HttpsFirstModeSetting.ENABLED_BALANCED);
       }
     });
 
@@ -526,6 +536,10 @@ export class SettingsSecurityPageElement extends
         generatedPref.userControlDisabled ?
             'httpsOnlyModeDescriptionAdvancedProtection' :
             'httpsOnlyModeDescription');
+  }
+
+  private isHttpsFirstModeEnabled_(value: number): boolean {
+    return value !== HttpsFirstModeSetting.DISABLED;
   }
 
   private onManageCertificatesClick_() {
--- chrome/browser/ui/webui/settings/settings_ui.cc
+++ chrome/browser/ui/webui/settings/settings_ui.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engine_choice/search_engine_choice_service_factory.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/ssl/https_upgrades_util.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/hats/hats_service.h"
 #include "chrome/browser/ui/hats/hats_service_factory.h"
@@ -386,8 +387,7 @@ SettingsUI::SettingsUI(content::WebUI* web_ui)
                               IsHashRealTimeLookupEligibleInSession());
 
   html_source->AddBoolean("enableHttpsFirstModeNewSettings",
-                          base::FeatureList::IsEnabled(
-                              features::kHttpsFirstModeIncognitoNewSettings));
+                          IsBalancedModeAvailable());
 
   html_source->AddBoolean(
       "enableKeyboardAndPointerLockPrompt",
