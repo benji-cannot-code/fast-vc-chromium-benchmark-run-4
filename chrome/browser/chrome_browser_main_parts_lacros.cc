02b1c65cee0f9172307a8f810d0ed8c0738a53b6
Michael Ershov <miersh@google.com>
2024-08-05 14:17:07 +0000

    ChromeBrowserMainPartsLacros: Remove KcerFactory::Shutdown()
    KcerFactory::Shutdown() is no-op at this point and should be
    removed everywhere.
    
    Bug: b:351347146
    Test: Existing automated tests
    Change-Id: Ibce81bf81c5f868962b1982f88856102179382e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756894
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337286}

--- chrome/browser/chrome_browser_main_parts_lacros.cc
+++ chrome/browser/chrome_browser_main_parts_lacros.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check.h"
 #include "base/command_line.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/kcer/kcer_factory.h"
 #include "chrome/browser/lacros/metrics_reporting_observer.h"
 #include "chrome/browser/lacros/prefs_ash_observer.h"
 #include "chrome/browser/metrics/metrics_reporting_state.h"
@@ -104,10 +103,6 @@ void ChromeBrowserMainPartsLacros::PostMainMessageLoopRun() {
   // `g_browser_process->metrics_service()` is destructed as
   // MetricsReportingObserver depends on metrics service.
   metrics_reporting_observer_.reset();
-
-  // Contains a raw_ptr to ChapsService (an object owned by LacrosService) and
-  // should be shut down before LacrosService.
-  kcer::KcerFactory::Shutdown();
 
   ChromeBrowserMainParts::PostMainMessageLoopRun();
 
