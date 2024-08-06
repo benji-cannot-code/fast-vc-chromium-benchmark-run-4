6f124b147c94ad4f5092123f194575a22f45d84a
Qiu Jianlin <jianlin.qiu@intel.com>
2024-08-06 04:03:39 +0000

    Reland "Enable build of VEA test for Windows."
    This is a reland of commit 219fc086fb9fcb8f5cfe9c390d1c5acec311762a
    
    Fix build issues when tests are built for Linux instead of CrOS.
    
    Original change's description:
    > Enable build of VEA test for Windows.
    >
    > Make VEA test built for Windows besides CrOS. As the first step we only
    > enabled input of shared memory type. GMB support will be added later.
    >
    > Single VEA perf test at quality + speed mode for all resolutions takes
    > more than 2 minutes to finish, while media_unittests currently without
    > these perf test bundled takes around 3 minutes to finish on my Xeon
    > Platinum 8385 platform.
    >
    > To build the vea_tests on Windows:
    > autoninja -C <output_path> video_encode_accelerator_tests
    > To run vea_test on Windows:
    > <output_path>\video_encode_accelerator_tests.exe
    >
    > To build the vea_perf_tests on Windows:
    > autoninja -C <output_path> video_encode_accelerator_perf_tests
    > To run the vea_perf_tests on Windows(for example):
    > <output_path>\video_encode_accelerator_perf_tests.exe --speed
    > --codec=h264 --svc_mode=L1T1 --content_type=camera --bitrate_mode=cbr
    > --output_bitstream <video_path> <video_meta_data_path>
    >
    > Bug: 350994517
    > Change-Id: I9ebae8729b1455b595acb86513e75c48b53e8906
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5681527
    > Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    > Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1336559}
    
    Bug: 350994517
    Change-Id: I628af56f6608710545712425da9990d1fcacfabf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761854
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Jianlin Qiu <jianlin.qiu@intel.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337661}

--- media/gpu/test/video_decode_accelerator_perf_tests.cc
+++ media/gpu/test/video_decode_accelerator_perf_tests.cc
@@ -22,8 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/test/video_player/decoder_wrapper.h"
 #include "media/gpu/test/video_player/frame_renderer_dummy.h"
 #include "media/gpu/test/video_player/video_player_test_environment.h"
-#include "sandbox/linux/services/resource_limits.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
+#include "sandbox/linux/services/resource_limits.h"
+#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 
 namespace media {
 namespace test {
@@ -407,11 +410,13 @@ TEST_F(VideoDecoderTest, MeasureCappedPerformance) {
 // then decide how to aggregate/report those metrics.
 // Play multiple videos simultaneously from start to finish.
 TEST_F(VideoDecoderTest, MeasureUncappedPerformance_TenConcurrentDecoders) {
+#if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
   // Set RLIMIT_NOFILE soft limit to its hard limit value.
   if (sandbox::ResourceLimits::AdjustCurrent(
           RLIMIT_NOFILE, std::numeric_limits<long long int>::max())) {
     DPLOG(ERROR) << "Unable to increase soft limit of RLIMIT_NOFILE";
   }
+#endif  // BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX)
 
   constexpr size_t kNumConcurrentDecoders = 10;
 
