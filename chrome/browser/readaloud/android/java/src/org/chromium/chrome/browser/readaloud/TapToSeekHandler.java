d0e77b699e4c56f9b1d67f0e23c25b1521cc8449
Andrea Gomez <andreaxg@google.com>
2024-08-06 23:26:58 +0000

    Keep playback play state after Tap To Seek
    Seeking playback causes it to play, no matter the previous state. This
    change makes sure that if it was paused, it stays paused.
    
    Bug: 357640482
    Change-Id: Ib8cc513f6c1bc0766c453e69abbecaf85e54d0ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5764006
    Reviewed-by: Basia Zimirska <basiaz@google.com>
    Commit-Queue: Andrea Gomez <andreaxg@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1338181}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekHandler.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekHandler.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.readaloud;
 
 import org.chromium.base.supplier.ObservableSupplier;
-import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.modules.readaloud.Playback;
 import org.chromium.chrome.modules.readaloud.Playback.PlaybackTextPart;
@@ -32,7 +31,6 @@ import java.util.Comparator;
  */
 public class TapToSeekHandler {
     ObservableSupplier<Tab> mCurrentTabSupplier;
-    Supplier<Playback> mPlaybackSupplier;
 
     public TapToSeekHandler(ObservableSupplier<Tab> currentTabSupplier) {
         mCurrentTabSupplier = currentTabSupplier;
@@ -47,13 +45,12 @@ public class TapToSeekHandler {
      * @param content Selected word and surrounding content
      * @param beginOffset index of where the selected word starts within the content
      * @param endOffset index of where the selected word ends within the content
+     * @param playback the playback that will be seeked
+     * @param playing whether the playback is in the play state when the tap to seek request was
+     *     started
      */
     public void tapToSeek(
-            String content,
-            int beginOffset,
-            int endOffset,
-            Playback playback,
-            Tab currentlyPlayingTab) {
+            String content, int beginOffset, int endOffset, Playback playback, boolean playing) {
         char[] fullText = playback.getMetadata().fullText().toCharArray();
         // Set the needle to the word +- 15 characters on either side.
         int substringStartIndex = Math.max(0, beginOffset - 15);
@@ -66,7 +63,7 @@ public class TapToSeekHandler {
                         .replaceAll("\\s+", " "); // replaces any white-spaces with a space.
         int found = BoyerMoore.indexOf(fullText, needle.toCharArray());
         if (found > 0) {
-            maybeTapToSeek(found + beginOffset - substringStartIndex, content, playback);
+            maybeTapToSeek(found + beginOffset - substringStartIndex, content, playback, playing);
         } else {
             // Last needle not matched, try with the word and -15 characters.
             substringStartIndex = Math.max(0, beginOffset - 15);
@@ -80,7 +77,8 @@ public class TapToSeekHandler {
                             .replaceAll("\\s+", " "); // replaces any white-spaces with a space.
             found = BoyerMoore.indexOf(fullText, needle.toCharArray());
             if (found > 0) {
-                maybeTapToSeek(found + beginOffset - substringStartIndex, content, playback);
+                maybeTapToSeek(
+                        found + beginOffset - substringStartIndex, content, playback, playing);
             } else {
                 // Last needle not matched, try with the word and +15 characters.
                 substringStartIndex = beginOffset;
@@ -94,7 +92,7 @@ public class TapToSeekHandler {
                                 .replaceAll("\\s+", " "); // replaces any white-space with a space.
                 found = BoyerMoore.indexOf(fullText, needle.toCharArray());
                 if (found > 0) {
-                    maybeTapToSeek(found, content, playback);
+                    maybeTapToSeek(found, content, playback, playing);
                 } else {
                     // TODO: b/325654229 Improve heuristics with more substrings to match with.
                     ReadAloudMetrics.recordHasTapToSeekFoundMatch(false);
@@ -119,14 +117,18 @@ public class TapToSeekHandler {
      * @param content selected word and surrounding content
      * @param playback playback that will be seeked
      */
-    private void maybeTapToSeek(int index, String content, Playback playback) {
+    private void maybeTapToSeek(int index, String content, Playback playback, boolean playing) {
         int paragraphIndex = findParagraph(playback.getMetadata().paragraphs(), index);
         int wordIndex = findWord(playback.getMetadata().paragraphs()[paragraphIndex], index);
         if (wordIndex < 0) {
             ReadAloudMetrics.recordHasTapToSeekFoundMatch(false);
         } else {
             playback.seekToWord(paragraphIndex, wordIndex);
-            playback.play();
+            if (playing) {
+                playback.play();
+            } else {
+                playback.pause();
+            }
             ReadAloudMetrics.recordHasTapToSeekFoundMatch(true);
         }
     }
--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekHandlerUnitTest.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/TapToSeekHandlerUnitTest.java
@@ -28,7 +28,6 @@ import org.chromium.base.test.util.HistogramWatcher;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.profiles.Profile;
-import org.chromium.chrome.browser.tab.MockTab;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.modules.readaloud.Playback;
 import org.chromium.chrome.modules.readaloud.Playback.PlaybackTextPart;
@@ -49,7 +48,6 @@ public class TapToSeekHandlerUnitTest {
 
     @Mock private Profile mProfile;
     @Mock private ObservableSupplier<Tab> mMockTabProvider;
-    private MockTab mTab;
     @Mock private Playback mPlayback;
     @Mock private Playback.Metadata mMetadata;
     private static final GURL sTestGURL = JUnitTestGURLs.EXAMPLE_URL;
@@ -61,9 +59,6 @@ public class TapToSeekHandlerUnitTest {
     public void setUp() {
         MockitoAnnotations.initMocks(this);
         doReturn(false).when(mProfile).isOffTheRecord();
-        mTab = new MockTab(1, mProfile);
-        mTab.setGurlOverrideForTesting(sTestGURL);
-        doReturn(mTab).when(mMockTabProvider).get();
         when(mPlayback.getMetadata()).thenReturn(mMetadata);
         mJniMocker.mock(ReadAloudFeaturesJni.TEST_HOOKS, mReadAloudFeaturesNatives);
 
@@ -81,7 +76,8 @@ public class TapToSeekHandlerUnitTest {
         var histogram =
                 HistogramWatcher.newSingleRecordWatcher(
                         ReadAloudMetrics.HAS_TAP_TO_SEEK_FOUND_MATCH, true);
-        tapToSeek("brown\nfox jumps", 6, 9, "The\nquick brown fox  jumps\nover the lazy dog.");
+        tapToSeek(
+                "brown\nfox jumps", 6, 9, "The\nquick brown fox  jumps\nover the lazy dog.", true);
         histogram.assertExpected();
         verify(mPlayback, times(1)).seekToWord(0, 16);
 
@@ -89,7 +85,12 @@ public class TapToSeekHandlerUnitTest {
         histogram =
                 HistogramWatcher.newSingleRecordWatcher(
                         ReadAloudMetrics.HAS_TAP_TO_SEEK_FOUND_MATCH, true);
-        tapToSeek("over the lazy cat", 5, 13, "The\nquick brown fox  jumps\nover the lazy dog.");
+        tapToSeek(
+                "over the lazy cat",
+                5,
+                13,
+                "The\nquick brown fox  jumps\nover the lazy dog.",
+                true);
         histogram.assertExpected();
         verify(mPlayback, times(1)).seekToWord(0, 32);
 
@@ -97,7 +98,8 @@ public class TapToSeekHandlerUnitTest {
         histogram =
                 HistogramWatcher.newSingleRecordWatcher(
                         ReadAloudMetrics.HAS_TAP_TO_SEEK_FOUND_MATCH, true);
-        tapToSeek("cat jumps  over", 4, 9, "The\nquick brown fox  jumps\nover the lazy dog.");
+        tapToSeek(
+                "cat jumps  over", 4, 9, "The\nquick brown fox  jumps\nover the lazy dog.", false);
         histogram.assertExpected();
         verify(mPlayback, times(1)).seekToWord(0, 21);
 
@@ -109,7 +111,8 @@ public class TapToSeekHandlerUnitTest {
                 "The quick (remove me) brown [It should match] fox",
                 4,
                 9,
-                "The\nquick brown fox  jumps\nover the lazy dog.");
+                "The\nquick brown fox  jumps\nover the lazy dog.",
+                true);
         histogram.assertExpected();
         verify(mPlayback, times(1)).seekToWord(0, 4);
     }
@@ -119,13 +122,30 @@ public class TapToSeekHandlerUnitTest {
         var histogram =
                 HistogramWatcher.newSingleRecordWatcher(
                         ReadAloudMetrics.HAS_TAP_TO_SEEK_FOUND_MATCH, false);
-        tapToSeek("grey\nmouse danced", 5, 10, "The\nquick brown fox  jumps\nover the lazy dog.");
+        tapToSeek(
+                "grey\nmouse danced",
+                5,
+                10,
+                "The\nquick brown fox  jumps\nover the lazy dog.",
+                true);
         histogram.assertExpected();
 
         verify(mPlayback, never()).seekToWord(anyInt(), anyInt());
     }
 
-    private void tapToSeek(String content, int beginOffset, int endOffset, String fullText) {
+    @Test
+    public void testPlayPauseState() {
+        tapToSeek(
+                "brown\nfox jumps", 6, 9, "The\nquick brown fox  jumps\nover the lazy dog.", true);
+        verify(mPlayback).play();
+
+        tapToSeek(
+                "cat jumps  over", 4, 9, "The\nquick brown fox  jumps\nover the lazy dog.", false);
+        verify(mPlayback).pause();
+    }
+
+    private void tapToSeek(
+            String content, int beginOffset, int endOffset, String fullText, boolean playing) {
         // tap to seek
         when(mMetadata.fullText()).thenReturn(fullText);
         PlaybackTextPart p =
@@ -152,6 +172,6 @@ public class TapToSeekHandlerUnitTest {
                 };
         PlaybackTextPart[] paragraphs = new PlaybackTextPart[] {p};
         when(mMetadata.paragraphs()).thenReturn(paragraphs);
-        mTapToSeekHandler.tapToSeek(content, beginOffset, endOffset, mPlayback, mTab);
+        mTapToSeekHandler.tapToSeek(content, beginOffset, endOffset, mPlayback, playing);
     }
 }
