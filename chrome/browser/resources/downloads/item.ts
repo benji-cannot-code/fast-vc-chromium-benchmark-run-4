c4fe738af4aae9fd0f7895ce4887963bf2964738
Kerry Meng-Lin <kmenglin@google.com>
2024-08-06 16:14:47 +0000

    Add copy download link quick-action to download item.
    This adds a new link icon that is used on a download item. Clicking the
    link button will copy a download's url to clipboard. A toast will also
    appear (without 'undo') conveying such.
    
    Screenshots: 
    https: //drive.google.com/corp/drive/u/0/folders/1GpkVNe2CCi8MBjdNAGtvjTNisXom5mmh
    Bug: 345286846
    Change-Id: I2ccff693cde464d6e317f50499534703f24894d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751002
    Commit-Queue: Kerry Meng-Lin <kmenglin@google.com>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337906}

--- chrome/browser/resources/downloads/item.ts
+++ chrome/browser/resources/downloads/item.ts
@@ -1063,6 +1063,10 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
         this.isReviewable_;      // review-dangerous
   }
 
+  private computeShowCopyDownloadLink_(): boolean {
+    return !!(this.data && this.data.url);
+  }
+
   private computeShowQuickRemove_(): boolean {
     return this.isReviewable_ || this.computeShowRemove_() ||
         this.computeShowControlsForDangerous_();
@@ -1223,6 +1227,14 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
   }
   // </if>
 
+  private onCopyDownloadLinkClick_(e: Event) {
+    if (!this.data.url) {
+      return;
+    }
+    navigator.clipboard.writeText(this.data.url.url);
+    this.displayCopyToast_(e);
+  }
+
   private onMoreActionsClick_() {
     const button = this.getMoreActionsButton();
     // The menu button is not always shown, but if this handler is invoked, then
@@ -1322,6 +1334,24 @@ export class DownloadsItemElement extends DownloadsItemElementBase {
       this.doResume_();
     }
     this.getMoreActionsMenu().close();
+  }
+
+  private displayCopyToast_(e: Event) {
+    if (!this.data.url) {
+      return;
+    }
+
+    const pieces = loadTimeData.getSubstitutedStringPieces(
+                       loadTimeData.getString('toastCopiedDownloadLink'),
+                       this.data.url.url) as unknown as
+        Array<{collapsible: boolean, value: string, arg: string}>;
+    pieces.forEach(p => {
+      p.collapsible = !!p.arg;
+    });
+    getToastManager().showForStringPieces(pieces, /*hideSlotted=*/ true);
+
+    e.stopPropagation();
+    e.preventDefault();
   }
 
   private displayRemovedToast_(canUndo: boolean, e: Event) {
