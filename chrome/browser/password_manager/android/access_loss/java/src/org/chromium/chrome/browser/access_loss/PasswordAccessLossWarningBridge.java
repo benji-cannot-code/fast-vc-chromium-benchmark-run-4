836b823c5561397349863d530b02cb6f7b3f40d2
Ivana Žužić <izuzic@google.com>
2024-08-06 09:59:59 +0000

    [UPMAccessLossWarning] Trigger the access loss warning after filling
    This CL introduces the bridge to trigger showing the access loss warning
    from cpp and uses it to trigger showing the sheet after filling
    credentials from the touch to fill sheet.
    
    The decision to show or not to show the sheet will be common for all the
    trigger points and it will be defined in a follow-up CL, for now
    the only criteria is the feature flag.
    
    The decision about the content on the sheet will be done on the Java
    side as a follow-up.
    
    Bug: 356627466
    Change-Id: I13663966d7570c4d074205064fa602e293e22b21
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5752350
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337759}

---
+++ chrome/browser/password_manager/android/access_loss/java/src/org/chromium/chrome/browser/access_loss/PasswordAccessLossWarningBridge.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.access_loss;
+
+import org.jni_zero.CalledByNative;
+
+class PasswordAccessLossWarningBridge {
+
+    @CalledByNative
+    private static PasswordAccessLossWarningBridge create() {
+        return new PasswordAccessLossWarningBridge();
+    }
+
+    @CalledByNative
+    private void show() {
+        // TODO: crbug.com/356627466 - Show the sheet.
+    }
+}
---
+++ chrome/browser/password_manager/android/access_loss/password_access_loss_warning_bridge_impl.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_ACCESS_LOSS_PASSWORD_ACCESS_LOSS_WARNING_BRIDGE_IMPL_H_
+#define CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_ACCESS_LOSS_PASSWORD_ACCESS_LOSS_WARNING_BRIDGE_IMPL_H_
+
+#include <jni.h>
+
+#include "chrome/browser/password_manager/android/access_loss/password_access_loss_warning_bridge.h"
+
+class PasswordAccessLossWarningBridgeImpl
+    : public PasswordAccessLossWarningBridge {
+ public:
+  PasswordAccessLossWarningBridgeImpl();
+  PasswordAccessLossWarningBridgeImpl(
+      const PasswordAccessLossWarningBridgeImpl&) = delete;
+  PasswordAccessLossWarningBridgeImpl& operator=(
+      const PasswordAccessLossWarningBridgeImpl&) = delete;
+  ~PasswordAccessLossWarningBridgeImpl() override;
+
+  void MaybeShowAccessLossNoticeSheet() override;
+  bool ShouldShowAccessLossNoticeSheet() override;
+};
+
+#endif  // CHROME_BROWSER_PASSWORD_MANAGER_ANDROID_ACCESS_LOSS_PASSWORD_ACCESS_LOSS_WARNING_BRIDGE_IMPL_H_
