5d7d3d32eec6859d29d37939f5f61b8ca9744f08
Georg Neis <neis@chromium.org>
2024-08-06 06:45:23 +0000

    ash: Move c/b/ash/vm_*_observer* files to c/b/ash/guest_os
    Fixed: b:357064271
    Change-Id: I4af01a560f784bb467239d6132fe5eabca2bab39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757268
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337697}

--- chrome/browser/ash/guest_os/vm_starting_observer.h
+++ chrome/browser/ash/guest_os/vm_starting_observer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_VM_STARTING_OBSERVER_H_
-#define CHROME_BROWSER_ASH_VM_STARTING_OBSERVER_H_
+#ifndef CHROME_BROWSER_ASH_GUEST_OS_VM_STARTING_OBSERVER_H_
+#define CHROME_BROWSER_ASH_GUEST_OS_VM_STARTING_OBSERVER_H_
 
 #include "base/observer_list_types.h"
 
@@ -17,4 +17,4 @@ class VmStartingObserver : public base::CheckedObserver {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_VM_STARTING_OBSERVER_H_
+#endif  // CHROME_BROWSER_ASH_GUEST_OS_VM_STARTING_OBSERVER_H_
