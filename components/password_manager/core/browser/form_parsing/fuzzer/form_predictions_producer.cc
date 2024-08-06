e92458e6cdf696cdb6ddc85c7bed163fc7aa18cd
Din Nezametdinov <shaikhitdin@google.com>
2024-08-06 15:23:31 +0000

    [Passwords] Fix the form data parser fuzzer bug
    One field can have at most one `ServerPrediction` which is then deduced
    into `FieldType` in PwM.
    (https://source.chromium.org/chromium/chromium/src/+/main:components/password_manager/core/browser/form_parsing/password_field_prediction.cc;l=30;drc=4b307952489a0b546f6b52d75abe88c0ed122b38)
    
    The fuzzer was randomly assigning server predictions (`FieldType`)
    to non-existing fields by assigning server prediction to random
    renderer id. But, there was no check that field with the
    random renderer id doesn't already exist in the form.
    The test contained 2 server predictions on the same field
    (password, not password). This way, crbug/355520829#comment5 situation
    occurred.
    
    Bug: 355520829
    Change-Id: I9a63f4fab0b9e1335eb9983a2a19f3249acd3a66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763479
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337873}

--- components/password_manager/core/browser/form_parsing/fuzzer/form_predictions_producer.cc
+++ components/password_manager/core/browser/form_parsing/fuzzer/form_predictions_producer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <fuzzer/FuzzedDataProvider.h>
 
+#include <algorithm>
 #include <bitset>
 #include <string>
 #include <utility>
@@ -78,6 +79,17 @@ FormPredictions GenerateFormPredictions(const FormData& form_data,
 
     autofill::FieldRendererId renderer_id(
         provider.ConsumeIntegralInRange(-32, 31));
+    // Generate unique `renderer_id` not matching any existing field
+    // renderer_id.
+    while (std::any_of(form_data.fields().begin(), form_data.fields().end(),
+                       [renderer_id](const autofill::FormFieldData& field) {
+                         return renderer_id.value() ==
+                                field.renderer_id().value();
+                       })) {
+      renderer_id =
+          autofill::FieldRendererId(provider.ConsumeIntegralInRange(-32, 31));
+    }
+
     autofill::FieldSignature signature(provider.ConsumeIntegralInRange(0, 500));
     predictions.fields.emplace_back(
         renderer_id, signature,
