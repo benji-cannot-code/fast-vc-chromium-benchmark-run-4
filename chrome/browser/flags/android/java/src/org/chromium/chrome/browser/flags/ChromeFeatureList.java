ba8e411ef1030bc9f8e99a931dfca879d48524fe
Sirisha Kavuluru <skavuluru@google.com>
2024-08-06 07:27:54 +0000

    [IncognitoMigration] Create incognito button on strip when FF is disabled
    Screenshot without the button: https://screenshot.googleplex.com/8jx5UdYMhu79V9s
    
    Fixed: 356897418
    Change-Id: I6df999de6f50b1ca2b781d17c420093ed269c5a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5762708
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337708}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureList.java
@@ -489,6 +489,7 @@ public abstract class ChromeFeatureList {
     public static final String TAB_STRIP_GROUP_COLLAPSE = "TabStripGroupCollapseAndroid";
     public static final String TAB_STRIP_GROUP_INDICATORS = "TabStripGroupIndicatorsAndroid";
     public static final String TAB_STRIP_GROUP_CONTEXT_MENU = "TabStripGroupContextMenuAndroid";
+    public static final String TAB_STRIP_INCOGNITO_MIGRATION = "TabStripIncognitoMigration";
     public static final String TAB_STRIP_LAYOUT_OPTIMIZATION = "TabStripLayoutOptimization";
     public static final String TAB_STRIP_STARTUP_REFACTORING = "TabStripStartupRefactoring";
     public static final String TAB_STRIP_TRANSITION_IN_DESKTOP_WINDOW =
@@ -683,6 +684,8 @@ public abstract class ChromeFeatureList {
             newCachedFlag(TAB_RESUMPTION_MODULE_ANDROID, false);
     public static final CachedFlag sTabStateFlatBuffer =
             newCachedFlag(TAB_STATE_FLAT_BUFFER, false);
+    public static final CachedFlag sTabStripIncognitoMigration =
+            newCachedFlag(TAB_STRIP_INCOGNITO_MIGRATION, false);
     public static final CachedFlag sTabStripLayoutOptimization =
             newCachedFlag(TAB_STRIP_LAYOUT_OPTIMIZATION, true);
     public static final CachedFlag sTabStripStartupRefactoring =
@@ -794,6 +797,7 @@ public abstract class ChromeFeatureList {
                     sTabStateFlatBuffer,
                     sTabStripGroupCollapse,
                     sTabStripGroupIndicators,
+                    sTabStripIncognitoMigration,
                     sTabStripLayoutOptimization,
                     sTabStripStartupRefactoring,
                     sTabletToolbarReordering,
