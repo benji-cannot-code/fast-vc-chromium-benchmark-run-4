f59e8f82e0769bd2b7ba6c62412f1545cb65de09
Olivier ROBIN <olivierrobin@google.com>
2024-08-06 13:30:32 +0000

    Rename Get/SetLastActiveTime to Get/SetLastActiveTimeTicks
    Get/SetLastActiveTime is misleadingly named as it returns
    a TimeTicks, which does not have the same semantic.
    Rename it to Get/SetLastActiveTimeTicks so a new variant
    returning a Time can be added.
    
    Bug: 349542284
    Change-Id: I67cc61b5446af23b753e6b55e07906808c43bd4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5762604
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Owners-Override: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337818}

--- chrome/browser/optimization_guide/optimization_guide_tab_url_provider.cc
+++ chrome/browser/optimization_guide/optimization_guide_tab_url_provider.cc
@@ -24,7 +24,7 @@ bool IsWebContentsCandidateForRefresh(
     content::WebContents* web_contents,
     const base::TimeDelta& duration_since_last_shown) {
   return web_contents &&
-         (base::TimeTicks::Now() - web_contents->GetLastActiveTime()) <
+         (base::TimeTicks::Now() - web_contents->GetLastActiveTimeTicks()) <
              duration_since_last_shown;
 }
 
@@ -46,7 +46,7 @@ const std::vector<GURL> OptimizationGuideTabUrlProvider::GetUrlsOfActiveTabs(
                                          duration_since_last_shown)) {
       urls_and_active_time.emplace_back(
           std::make_pair(web_contents->GetLastCommittedURL(),
-                         web_contents->GetLastActiveTime()));
+                         web_contents->GetLastActiveTimeTicks()));
     }
   }
   std::sort(urls_and_active_time.begin(), urls_and_active_time.end(),
--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -2406,8 +2406,8 @@ IN_PROC_BROWSER_TEST_F(SmartSessionRestoreTest, MAYBE_PRE_CorrectLoadingOrder) {
     GURL expected_url = GURL(kUrls[activation_order[kExpectedNumTabs - i - 1]]);
     ASSERT_EQ(expected_url, web_contents[i]->GetLastCommittedURL());
     if (i > 0) {
-      ASSERT_GT(web_contents[i - 1]->GetLastActiveTime(),
-                web_contents[i]->GetLastActiveTime());
+      ASSERT_GT(web_contents[i - 1]->GetLastActiveTimeTicks(),
+                web_contents[i]->GetLastActiveTimeTicks());
     }
   }
 
@@ -2455,8 +2455,8 @@ IN_PROC_BROWSER_TEST_F(SmartSessionRestoreTest, MAYBE_CorrectLoadingOrder) {
     GURL expected_url = GURL(kUrls[activation_order[kExpectedNumTabs - i]]);
     ASSERT_EQ(expected_url, web_contents[i]->GetLastCommittedURL());
     if (i > 0) {
-      ASSERT_GT(web_contents[i - 1]->GetLastActiveTime(),
-                web_contents[i]->GetLastActiveTime());
+      ASSERT_GT(web_contents[i - 1]->GetLastActiveTimeTicks(),
+                web_contents[i]->GetLastActiveTimeTicks());
     }
   }
 }
