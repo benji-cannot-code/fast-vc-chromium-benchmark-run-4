3cc82164ebb42152fb6ff984bc779cf999fde7e2
Eli Hayoon <elius@google.com>
2024-08-06 21:17:51 +0000

    Add UMA metric Ads.InterestGroup.Auction.AdNavigationStarted, which counts the number of times urns/fenced frame configs returned from Protected Audience are loaded.
    Bug: 345515313
    Change-Id: Iecc90aa4d5797de61980f24c9b075e4826936835
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5732279
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Commit-Queue: Eli Hayoon <elius@google.com>
    Reviewed-by: Garrett Tanzer <gtanzer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338109}

--- content/browser/interest_group/interest_group_browsertest.cc
+++ content/browser/interest_group/interest_group_browsertest.cc
@@ -8971,6 +8971,7 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, RunAdAuctionWithWinner) {
   URLLoaderMonitor url_loader_monitor;
+  base::HistogramTester histogram_tester;
 
   GURL test_url =
       embedded_https_test_server().GetURL("a.test", "/page_with_iframe.html");
@@ -9096,6 +9097,9 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, RunAdAuctionWithWinner) {
                 ->trusted_params->isolation_info.network_isolation_key(),
             url_loader_monitor.GetRequestInfo(kExpectedReportUrls[1])
                 ->trusted_params->isolation_info.network_isolation_key());
+  // Two reporting requests should result in two AdNavigationStarted.
+  histogram_tester.ExpectTotalCount(
+      "Ads.InterestGroup.Auction.AdNavigationStarted", 2);
 }
 
 // Runs auction just like test InterestGroupBrowserTest.RunAdAuctionWithWinner,
@@ -18075,6 +18079,7 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, DeprecatedURNToURLValidURN) {
       }};
 
   for (const auto& test_case : kTestCases) {
+    base::HistogramTester histogram_tester;
     GURL test_url =
         embedded_https_test_server().GetURL(test_case.host, "/echo");
     ASSERT_TRUE(NavigateToURL(shell(), test_url));
@@ -18111,6 +18116,8 @@ IN_PROC_BROWSER_TEST_F(InterestGroupBrowserTest, DeprecatedURNToURLValidURN) {
     EXPECT_EQ(url::kUrnScheme, urn_url.scheme_piece());
     EXPECT_EQ(ad_url,
               ConvertFencedFrameURNToURLInJS(urn_url, test_case.send_reports));
+    histogram_tester.ExpectTotalCount(
+        "Ads.InterestGroup.Auction.AdNavigationStarted", 1);
   }
 
   // Only the `send_reports` == true case should have sent a report. Wait for
