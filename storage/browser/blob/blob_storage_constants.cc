413c2feeb49b9d9ef661200e8c3720d20dd03edf
Peter Kasting <pkasting@chromium.org>
2024-08-06 20:55:04 +0000

    Switch to [[(un)likely]]: storage/
    C++20 adds [[(un)likely]], which is being allowed. This should replace
    our existing macro usage.
    
    Bug: 40256217
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR mechanical-only change
    Change-Id: I64e10f952c4437fbef91c3eafd3a62498be0d10a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766903
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Joshua Bell <jsbell@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338094}

--- storage/browser/blob/blob_storage_constants.cc
+++ storage/browser/blob/blob_storage_constants.cc
@@ -34,8 +34,8 @@ static_assert(kDefaultMinPageFileSize < kDefaultMaxBlobInMemorySpace,
 
 BlobStorageLimits::BlobStorageLimits() {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (UNLIKELY(
-          command_line->HasSwitch(kBlobFileTransportByFileTriggerSwitch))) {
+  if (command_line->HasSwitch(kBlobFileTransportByFileTriggerSwitch))
+      [[unlikely]] {
     CHECK(base::StringToUint64(command_line->GetSwitchValueASCII(
                                    kBlobFileTransportByFileTriggerSwitch),
                                &override_file_transport_min_size))
@@ -43,8 +43,8 @@ BlobStorageLimits::BlobStorageLimits() {
         << command_line->GetSwitchValueASCII(
                kBlobFileTransportByFileTriggerSwitch);
   }
-  if (UNLIKELY(
-          command_line->HasSwitch(kBlobSharedMemoryTransportMaxSizeSwitch))) {
+  if (command_line->HasSwitch(kBlobSharedMemoryTransportMaxSizeSwitch))
+      [[unlikely]] {
     CHECK(base::StringToSizeT(command_line->GetSwitchValueASCII(
                                   kBlobSharedMemoryTransportMaxSizeSwitch),
                               &max_shared_memory_size))
@@ -52,7 +52,8 @@ BlobStorageLimits::BlobStorageLimits() {
         << command_line->GetSwitchValueASCII(
                kBlobSharedMemoryTransportMaxSizeSwitch);
   }
-  if (UNLIKELY(command_line->HasSwitch(kBlobFileTransportMinFileSizeSwitch))) {
+  if (command_line->HasSwitch(kBlobFileTransportMinFileSizeSwitch))
+      [[unlikely]] {
     CHECK(base::StringToUint64(
         command_line->GetSwitchValueASCII(kBlobFileTransportMinFileSizeSwitch),
         &min_page_file_size))
@@ -60,7 +61,8 @@ BlobStorageLimits::BlobStorageLimits() {
         << command_line->GetSwitchValueASCII(
                kBlobSharedMemoryTransportMaxSizeSwitch);
   }
-  if (UNLIKELY(command_line->HasSwitch(kBlobFileTransportMaxFileSizeSwitch))) {
+  if (command_line->HasSwitch(kBlobFileTransportMaxFileSizeSwitch))
+      [[unlikely]] {
     CHECK(base::StringToUint64(
         command_line->GetSwitchValueASCII(kBlobFileTransportMaxFileSizeSwitch),
         &max_file_size))
