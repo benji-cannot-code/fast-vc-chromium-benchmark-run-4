a03126fa2d2c212b79039f5b5f83f811b6961407
Awad Osman <awado@google.com>
2024-08-06 22:20:40 +0000

    Delete kSafeBrowsingEsbOptInWithFriendlierSettings pref
    Removes pref that checks whether a user opted into ESB with the
    friendlier settings UI or not. We are removing this pref because we have
    fully launched friendlier settings and that is now the default.
    
    Bug: 357855820
    Change-Id: If40e20be843d232e5ced21dc76cf883a316ffb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765816
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338136}

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -1047,6 +1047,10 @@ constexpr char kObsoleteUserReceivedGMSCoreError[] =
     "user_received_gmscore_error";
 #endif
 
+// Deprecated 08/2024.
+constexpr char kSafeBrowsingEsbOptInWithFriendlierSettings[] =
+    "safebrowsing.esb_opt_in_with_friendlier_settings";
+
 // Register local state used only for migration (clearing or moving to a new
 // key).
 void RegisterLocalStatePrefsForMigration(PrefRegistrySimple* registry) {
@@ -1449,6 +1453,9 @@ void RegisterProfilePrefsForMigration(
       kObsoleteTimesAttemptedToReenrollToGoogleMobileServices, 0);
   registry->RegisterBooleanPref(kObsoleteUserReceivedGMSCoreError, false);
 #endif
+  // Deprecated 08/2024.
+  registry->RegisterBooleanPref(kSafeBrowsingEsbOptInWithFriendlierSettings,
+                                false);
 }
 
 void ClearSyncRequestedPrefAndMaybeMigrate(PrefService* profile_prefs) {
@@ -2752,6 +2759,8 @@ void MigrateObsoleteProfilePrefs(PrefService* profile_prefs,
       kObsoleteTimesAttemptedToReenrollToGoogleMobileServices);
   profile_prefs->ClearPref(kObsoleteUserReceivedGMSCoreError);
 #endif
+  // Added 08/2024.
+  profile_prefs->ClearPref(kSafeBrowsingEsbOptInWithFriendlierSettings);
 
   // Please don't delete the following line. It is used by PRESUBMIT.py.
   // END_MIGRATE_OBSOLETE_PROFILE_PREFS
