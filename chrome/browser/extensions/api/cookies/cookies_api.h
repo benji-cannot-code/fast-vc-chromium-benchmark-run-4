6e85edede230c941585458b62525f475689c6197
Brett Wilson <brettw@chromium.org>
2024-08-06 23:00:35 +0000

    CookiesAPI observes profiles instead of browsers.
    Previously the Cookies API added a BrowserList observer as an indirect
    way to detect when an off-the-record profile was created for the
    profile.
    
    This moves that functionality to the Profile observer which has a
    notification specifically for this event. This is more semantically
    correct and removes a dependency on browser/ui code.
    
    There should be no behavior change.
    
    Bug: None
    
    Change-Id: I18254f4830be99449e185d51aaf4598d0d802f6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760382
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338160}

--- chrome/browser/extensions/api/cookies/cookies_api.h
+++ chrome/browser/extensions/api/cookies/cookies_api.h
@@ -14,8 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/raw_ptr.h"
+#include "base/scoped_observation.h"
 #include "base/values.h"
-#include "chrome/browser/ui/browser_list_observer.h"
+#include "chrome/browser/profiles/profile_observer.h"
 #include "chrome/common/extensions/api/cookies.h"
 #include "extensions/browser/browser_context_keyed_api_factory.h"
 #include "extensions/browser/event_router.h"
@@ -34,7 +35,7 @@ namespace extensions {
 
 // Observes CookieManager Mojo messages and routes them as events to the
 // extension system.
-class CookiesEventRouter : public BrowserListObserver {
+class CookiesEventRouter : public ProfileObserver {
  public:
   explicit CookiesEventRouter(content::BrowserContext* context);
 
@@ -43,8 +44,8 @@ class CookiesEventRouter : public BrowserListObserver {
 
   ~CookiesEventRouter() override;
 
-  // BrowserListObserver:
-  void OnBrowserAdded(Browser* browser) override;
+  // ProfileObserver:
+  void OnOffTheRecordProfileCreated(Profile* off_the_record) override;
 
  private:
   // This helper class connects to the CookieMonster over Mojo, and relays Mojo
@@ -85,6 +86,8 @@ class CookiesEventRouter : public BrowserListObserver {
                      const GURL& cookie_domain);
 
   raw_ptr<Profile> profile_;
+
+  base::ScopedObservation<Profile, ProfileObserver> profile_observation_;
 
   // To listen to cookie changes in both the original and the off the record
   // profiles, we need a pair of bindings, as well as a pair of
