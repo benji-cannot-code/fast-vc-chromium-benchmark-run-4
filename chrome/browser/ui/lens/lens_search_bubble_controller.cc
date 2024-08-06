2c76726df17f6716d9d2d7ab12cf9cd422c2b0ca
Nihar Majmudar <niharm@google.com>
2024-08-06 17:36:09 +0000

    [lensoverlay] Close lens overlay when search bubble is manually closed.
    Now the lens search bubble Close() function is only called through the
    LensOverlayController.
    
    Screencast: https://screencast.googleplex.com/cast/NDY3OTU4NDYxNTIzNTU4NHwxZTIwYTQ3NS02Mg
    
    Change-Id: I13b27b2bc40912b172cc63108d65bbfa66c4344b
    Bug: 357728594
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757872
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337968}

--- chrome/browser/ui/lens/lens_search_bubble_controller.cc
+++ chrome/browser/ui/lens/lens_search_bubble_controller.cc
@@ -47,7 +47,9 @@ class LensSearchBubbleDialogView : public WebUIBubbleDialogView {
   void CloseUI() override {
     WebUIBubbleDialogView::CloseUI();
     search_bubble_controller_->RemoveLensOverlayControllerGlue();
-    search_bubble_controller_->Close();
+    // The lens overlay controller's CloseUIAsync() will eventually call the
+    // search bubble's Close() function.
+    search_bubble_controller_->CloseLensOverlay();
   }
 
  private:
@@ -109,6 +111,11 @@ void LensSearchBubbleController::Close() {
   contextual_searchbox_handler_.reset();
   bubble_view_ = nullptr;
   web_contents_ = nullptr;
+}
+
+void LensSearchBubbleController::CloseLensOverlay() {
+  lens_overlay_controller_->CloseUISync(
+      lens::LensOverlayDismissalSource::kSearchBubbleCloseButton);
 }
 
 void LensSearchBubbleController::RemoveLensOverlayControllerGlue() {
