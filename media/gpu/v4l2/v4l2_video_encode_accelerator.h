03499e1d960ac8fec76fbb817628b2f5d6ecef03
Fritz Koenig <frkoenig@chromium.org>
2024-08-06 01:55:33 +0000

    media/gpu/v4l2: Use VideoBitrateAllocation for RequestEncodingParametersChange
    VideoBitrateAllocation also encompasses spatial and temporal layers.
    
    Bug: b:350572907
    Test: video.EncodeAccel.*
    Change-Id: I92caa7a2076ae8d684a35cb5d9dfc237ef25058b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749426
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337622}

--- media/gpu/v4l2/v4l2_video_encode_accelerator.h
+++ media/gpu/v4l2/v4l2_video_encode_accelerator.h
@@ -64,6 +64,10 @@ class MEDIA_GPU_EXPORT V4L2VideoEncodeAccelerator
       const Bitrate& bitrate,
       uint32_t framerate,
       const std::optional<gfx::Size>& size) override;
+  void RequestEncodingParametersChange(
+      const VideoBitrateAllocation& bitrate_allocation,
+      uint32_t framerate,
+      const std::optional<gfx::Size>& size) override;
   void Destroy() override;
   void Flush(FlushCallback flush_callback) override;
   bool IsFlushSupported() override;
@@ -201,7 +205,7 @@ class MEDIA_GPU_EXPORT V4L2VideoEncodeAccelerator
 
   // Change encoding parameters.
   void RequestEncodingParametersChangeTask(
-      const Bitrate& bitrate,
+      const VideoBitrateAllocation& bitrate_allocation,
       uint32_t framerate,
       const std::optional<gfx::Size>& size);
 
@@ -302,7 +306,7 @@ class MEDIA_GPU_EXPORT V4L2VideoEncodeAccelerator
   size_t output_buffer_byte_size_;
   uint32_t output_format_fourcc_;
 
-  Bitrate current_bitrate_;
+  VideoBitrateAllocation current_bitrate_allocation_;
   size_t current_framerate_;
 
   // Encoder state, owned and operated by |encoder_task_runner_|.
