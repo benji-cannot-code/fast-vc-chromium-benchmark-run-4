04cf4182dd037f631495dfdef4df8b590a3ad588
Norman Bintang <normanbt@chromium.org>
2024-08-06 05:48:03 +0000

    vc-ui: Shorten the mic indicator cycle
    Shorten the mic indicator animation cycle from 2*8 + 1 to 8, to make the
    animation more responsive to the sound input. Also changes the
    animation. Previously it goes from 0 -> actual length -> 0. Now it goes
    from actual length -> longer -> shorter -> actual length.
    
    Bug: 349261564
    Change-Id: I1e9aec68cf66b9eee9445a43bcca03b2c82d7e27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759569
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Commit-Queue: Norman Bintang <normanbt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337684}

--- ash/system/video_conference/bubble/mic_indicator.cc
+++ ash/system/video_conference/bubble/mic_indicator.cc
@@ -43,6 +43,7 @@ const float kLogEwmaMin = std::log(0.00002);
 const float kLogEwmaDiff = kLogEwmaMax - kLogEwmaMin;
 
 constexpr int kMaxStep = 8;
+constexpr float phaseLengths[] = {1.0, 1.1, 1.3, 1.1, 1.0, 0.9, 0.7, 0.9};
 constexpr auto kMicIndicatorInsets = gfx::Insets::TLBR(16, 16, 16, 16);
 
 float ScalePower(float power) {
@@ -82,7 +83,7 @@ MicIndicator::~MicIndicator() {
 }
 
 void MicIndicator::UpdateProgress() {
-  step_ = (step_ + 1) % (2 * kMaxStep + 1);
+  step_ = (step_ + 1) % kMaxStep;
   if (step_ == 0) {
     bool sidetone_enabled =
         VideoConferenceTrayController::Get()->GetSidetoneEnabled();
@@ -96,16 +97,6 @@ void MicIndicator::UpdateProgress() {
 void MicIndicator::OnPaint(gfx::Canvas* canvas) {
   const float multiplier = ScalePower(power_);
 
-  // Use 1-base to avoid 0 in length calculation;
-  int step = step_ + 1;
-
-  // [1..kMaxStep]              -> Growing phase
-  // [kMaxStep+1]               -> Peak
-  // [kMaxStep+2..2*kMaxStep+1] -> Shrinking phase
-  if (step > kMaxStep) {
-    step = kMaxStep - (step - kMaxStep);
-  }
-
   cc::PaintFlags flags;
   flags.setAntiAlias(true);
   flags.setStrokeWidth(kIndicatorWidth);
@@ -117,14 +108,11 @@ void MicIndicator::OnPaint(gfx::Canvas* canvas) {
   const int view_width = GetContentsBounds().width();
   float x = (view_width - kIndicatorTotalWidth) / 2;
   for (int i = 0; i < kIndicatorLines; i++) {
-    float length = step * view_height * kIndicatorLengths[i] / kMaxStep;
-
-    // Special case for the last line.
-    // It is shorter than the previouos line during the growing phase,
-    // but has its own length during the shrinking phase.
-    if (i == kIndicatorLines - 1 && step_ <= kMaxStep) {
-      length = 0.65 * step * view_height * kIndicatorLengths[i - 1] / kMaxStep;
-    }
+    float length = view_height *
+                   // Set each line's animation cycle based on the previous
+                   // line, creating a wave-like effect.
+                   phaseLengths[(step_ + kMaxStep - i) % kMaxStep] *
+                   kIndicatorLengths[i];
 
     length = length * multiplier;
 
