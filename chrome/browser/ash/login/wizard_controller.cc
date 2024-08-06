9fd7463c615e997c191147e581daaec8f005c4a7
Brahim Chikhaoui <bchikhaoui@google.com>
2024-08-06 15:04:01 +0000

    [OOBE][Perks] Fix querying feature flag
    remove checking feature flag when showing the screen. the feature
    flag is included in the maybeSkip and onPerksDiscoveryExit will
    keep same logic.
    
    Bug: b:357823593
    Change-Id: I507e8095fe570786418f8d81f489af7b82f92cd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763024
    Reviewed-by: Bohdan Tyshchenko <bohdanty@google.com>
    Commit-Queue: Brahim Chikhaoui <bchikhaoui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337864}

--- chrome/browser/ash/login/wizard_controller.cc
+++ chrome/browser/ash/login/wizard_controller.cc
@@ -1914,15 +1914,7 @@ void WizardController::OnPersonalizedRecomendAppsScreenExit(
     case PersonalizedRecommendAppsScreen::Result::kDataMalformed:
     case PersonalizedRecommendAppsScreen::Result::kError:
     case PersonalizedRecommendAppsScreen::Result::kTimeout:
-      if (features::IsOobePerksDiscoveryEnabled()) {
         ShowPerksDiscoveryScreen();
-      } else if (features::IsOobeAiIntroEnabled()) {
-        ShowAiIntroScreen();
-      } else if (features::IsOobeGeminiIntroEnabled()) {
-        ShowGeminiIntroScreen();
-      } else {
-        ShowAssistantOptInFlowScreen();
-      }
       break;
   }
 }
@@ -2594,15 +2586,7 @@ void WizardController::OnRecommendAppsScreenExit(
     case RecommendAppsScreen::Result::kSkipped:
     case RecommendAppsScreen::Result::kNotApplicable:
     case RecommendAppsScreen::Result::kLoadError:
-      if (features::IsOobePerksDiscoveryEnabled()) {
         ShowPerksDiscoveryScreen();
-      } else if (features::IsOobeAiIntroEnabled()) {
-        ShowAiIntroScreen();
-      } else if (features::IsOobeGeminiIntroEnabled()) {
-        ShowGeminiIntroScreen();
-      } else {
-        ShowAssistantOptInFlowScreen();
-      }
       break;
   }
 }
@@ -2626,16 +2610,7 @@ void WizardController::OnRemoteActivityNotificationScreenExit() {
 
 void WizardController::OnAppDownloadingScreenExit() {
   OnScreenExit(AppDownloadingScreenView::kScreenId, kDefaultExitReason);
-
-  if (features::IsOobePerksDiscoveryEnabled()) {
-    ShowPerksDiscoveryScreen();
-  } else if (features::IsOobeAiIntroEnabled()) {
-    ShowAiIntroScreen();
-  } else if (features::IsOobeGeminiIntroEnabled()) {
-    ShowGeminiIntroScreen();
-  } else {
-    ShowAssistantOptInFlowScreen();
-  }
+  ShowPerksDiscoveryScreen();
 }
 
 void WizardController::OnAiIntroScreenExit(AiIntroScreen::Result result) {
