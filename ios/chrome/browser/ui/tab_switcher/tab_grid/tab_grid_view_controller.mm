3792c31a404fecac075409a9e2c8b3d2c9cae131
Louis Romero <lpromero@google.com>
2024-08-06 13:01:09 +0000

    Don't try presenting Inactive Tabs while animating
    When the Regular grid is animating in (from the Incognito grid or from
    the third panel), the user can still tap on the Inactive Tabs button.
    This CL changes a CHECK for the current page into a no-op early return
    instead, as tapping the Inactive Tabs button while it's animating in
    could enter that situation and trip the CHECK.
    
    Fixed: 41492208
    Change-Id: I522cb046ae0b78b6dd20793c3abd326209b93d17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765064
    Auto-Submit: Louis Romero <lpromero@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337807}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_grid_view_controller.mm
@@ -1881,7 +1881,9 @@ NSUInteger GetPageIndexFromPage(TabGridPage page) {
 - (void)didTapInactiveTabsButtonInGridViewController:
     (BaseGridViewController*)gridViewController {
   CHECK(IsInactiveTabsEnabled());
-  CHECK_EQ(self.currentPage, TabGridPageRegularTabs);
+  if (self.currentPage != TabGridPageRegularTabs) {
+    return;
+  }
   base::RecordAction(base::UserMetricsAction("MobileTabGridShowInactiveTabs"));
   [self.delegate showInactiveTabs];
   [self tabGridDidPerformAction:TabGridActionType::kInPageAction];
