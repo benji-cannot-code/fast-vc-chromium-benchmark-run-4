f3f7ed3147e636af19641be8f1471d448743a2df
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-08-06 17:01:22 +0000

    [sync/metrics] Rename ModelType to DataType in enums and variants
    But not histogram names.
    
    NO_IFTTT=Some bug when changing IFTTT blocks
    
    Bug: 356649891
    Change-Id: Ie8578cb6d998e9f61bb0e4466bf4dd324af42bae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766553
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337944}

--- components/sync/model/client_tag_based_data_type_processor.h
+++ components/sync/model/client_tag_based_data_type_processor.h
@@ -153,7 +153,7 @@ class ClientTagBasedDataTypeProcessor : public DataTypeProcessor,
 
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused. Public for tests.
-  // LINT.IfChange(SyncModelTypeErrorSite)
+  // LINT.IfChange(SyncDataTypeErrorSite)
   enum class ErrorSite {
     kReportedByBridge = 0,
     kApplyFullUpdates = 1,
@@ -162,7 +162,7 @@ class ClientTagBasedDataTypeProcessor : public DataTypeProcessor,
     kSupportsIncrementalUpdatesMismatch = 4,
     kMaxValue = kSupportsIncrementalUpdatesMismatch,
   };
-  // LINT.ThenChange(/tools/metrics/histograms/metadata/sync/enums.xml:SyncModelTypeErrorSite)
+  // LINT.ThenChange(/tools/metrics/histograms/metadata/sync/enums.xml:SyncDataTypeErrorSite)
 
  private:
   friend class DataTypeDebugInfo;
