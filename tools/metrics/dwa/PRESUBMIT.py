c5cedd4e533664f5b6545a236639759ea3b4677f
Jay Zhou <zhouzj@google.com>
2024-08-06 18:32:16 +0000

    Add model for DWA metrics
    This model will be used for dwa.xml, which will contain the metrics for
    de-identified web analytics reports.
    
    Change-Id: I5fcad6ddc6c0be15bdf13f35886400d216923b9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5713958
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Commit-Queue: Jay Zhou <zhouzj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338006}

---
+++ tools/metrics/dwa/PRESUBMIT.py
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit script for dwa.xml.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details on the presubmit API built into gcl.
+"""
+
+import os
+import sys
+
+sys.path.append(
+    os.path.join(os.path.dirname(os.path.abspath('__file__')), '..', 'common'))
+import presubmit_util
+
+_DWA_XML = 'dwa.xml'
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return presubmit_util.CheckChange(_DWA_XML, input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return presubmit_util.CheckChange(_DWA_XML, input_api, output_api)
---
+++ tools/metrics/dwa/pretty_print.py
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python3
+# Copyright 2024 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+import dwa_model
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'common'))
+import presubmit_util
+
+
+def main(argv):
+  presubmit_util.DoPresubmitMain(argv, 'dwa.xml', 'dwa.old.xml',
+                                 dwa_model.PrettifyXML)
+
+
+if '__main__' == __name__:
+  sys.exit(main(sys.argv))
