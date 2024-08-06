5970ef1a4ef77fa60f416cdb8fa04f94b45c1e97
Wan-Teh Chang <wtc@google.com>
2024-08-06 01:52:36 +0000

    Include "testing/gtest/include/gtest/gtest.h"
    Do not include
    "third_party/googletest/src/googletest/include/gtest/gtest.h".
    
    Merge third_party/libvpx/tests/DEPS into third_party/libvpx/DEPS.
    
    Remove unneeded entries in third_party/libaom/DEPS and
    third_party/libvpx/DEPS.
    
    Verified with the commands:
      buildtools/checkdeps/checkdeps.py third_party/libaom
      buildtools/checkdeps/checkdeps.py third_party/libvpx
    
    Change-Id: I2f59d125c28e58223469c2478dde6314f1ad27bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763747
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: James Zern <jzern@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1337619}

--- third_party/libaom/fuzz/av1_encoder_fuzz_test.cc
+++ third_party/libaom/fuzz/av1_encoder_fuzz_test.cc
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 #include <vector>
 
+#include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/fuzztest/src/fuzztest/fuzztest.h"
-#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 #include "third_party/libaom/source/libaom/aom/aom_codec.h"
 #include "third_party/libaom/source/libaom/aom/aom_encoder.h"
 #include "third_party/libaom/source/libaom/aom/aom_image.h"
