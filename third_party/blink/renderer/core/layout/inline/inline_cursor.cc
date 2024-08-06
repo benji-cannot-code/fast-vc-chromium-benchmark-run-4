387dce63935760b60b5da0f66d40226f434c1822
Peter Kasting <pkasting@chromium.org>
2024-08-06 03:10:33 +0000

    Switch to [[(un)likely]]: third_party/blink/renderer/core/layout/inline/
    C++20 adds [[(un)likely]], which is being allowed. This should replace
    our existing macro usage.
    
    Bug: 40256217
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR mechanical-only change
    Change-Id: I707a992369b7df05d288a65a4dc4553c4dd7b7e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759050
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1337652}

--- third_party/blink/renderer/core/layout/inline/inline_cursor.cc
+++ third_party/blink/renderer/core/layout/inline/inline_cursor.cc
@@ -107,7 +107,7 @@ bool ShouldIgnoreForPositionForPoint(const FragmentItem& item) {
     case FragmentItem::kGeneratedText:
       return true;
     case FragmentItem::kText:
-      if (UNLIKELY(item.IsLayoutObjectDestroyedOrMoved())) {
+      if (item.IsLayoutObjectDestroyedOrMoved()) [[unlikely]] {
         // See http://crbug.com/1217079
         NOTREACHED_IN_MIGRATION() << item;
         return true;
@@ -178,7 +178,7 @@ void InlineCursor::SetRoot(const PhysicalBoxFragment& box_fragment,
 bool InlineCursor::TrySetRootFragmentItems() {
   DCHECK(root_block_flow_);
   DCHECK(!fragment_items_ || fragment_items_->Equals(items_));
-  if (UNLIKELY(!root_block_flow_->MayHaveFragmentItems())) {
+  if (!root_block_flow_->MayHaveFragmentItems()) [[unlikely]] {
 #if EXPENSIVE_DCHECKS_ARE_ON()
     DCHECK(!root_block_flow_->PhysicalFragments().SlowHasFragmentItems());
 #endif
@@ -732,9 +732,12 @@ PositionWithAffinity InlineCursor::PositionForPointInInlineBox(
          container->Type() == FragmentItem::kBox);
   const auto* const text_combine =
       DynamicTo<LayoutTextCombine>(container->GetLayoutObject());
-  const PhysicalOffset point =
-      UNLIKELY(text_combine) ? text_combine->AdjustOffsetForHitTest(point_in)
-                             : point_in;
+  PhysicalOffset point;
+  if (text_combine) [[unlikely]] {
+    point = text_combine->AdjustOffsetForHitTest(point_in);
+  } else {
+    point = point_in;
+  }
   const auto writing_direction = container->Style().GetWritingDirection();
   const PhysicalSize& container_size = container->Size();
   const LayoutUnit point_inline_offset =
@@ -995,8 +998,9 @@ void InlineCursor::MoveTo(const InlineCursor& cursor) {
 
 void InlineCursor::MoveToParent() {
   wtf_size_t count = 0;
-  if (UNLIKELY(!Current()))
+  if (!Current()) [[unlikely]] {
     return;
+  }
   for (;;) {
     MoveToPrevious();
     if (!Current())
@@ -1320,9 +1324,10 @@ bool InlineCursor::TryMoveToLastChild() {
 
 void InlineCursor::MoveToNext() {
   DCHECK(HasRoot());
-  if (UNLIKELY(!current_.item_))
+  if (!current_.item_) [[unlikely]] {
     return;
-  // Expensive DCHECK as MoveToNext() is callled frequently.
+  }
+  // Expensive DCHECK as MoveToNext() is called frequently.
   DCHECK(current_.item_iter_ != items_.end());
   if (++current_.item_iter_ != items_.end()) {
     current_.item_ = &*current_.item_iter_;
@@ -1333,8 +1338,9 @@ void InlineCursor::MoveToNext() {
 
 void InlineCursor::MoveToNextSkippingChildren() {
   DCHECK(HasRoot());
-  if (UNLIKELY(!current_.item_))
+  if (!current_.item_) [[unlikely]] {
     return;
+  }
   // If the current item has |DescendantsCount|, add it to move to the next
   // sibling, skipping all children and their descendants.
   if (wtf_size_t descendants_count = current_.item_->DescendantsCount())
@@ -1344,8 +1350,9 @@ void InlineCursor::MoveToNextSkippingChildren() {
 
 void InlineCursor::MoveToPrevious() {
   DCHECK(HasRoot());
-  if (UNLIKELY(!current_.item_))
+  if (!current_.item_) [[unlikely]] {
     return;
+  }
   if (current_.item_iter_ == items_.begin())
     return MakeNull();
   --current_.item_iter_;
@@ -1415,7 +1422,7 @@ void InlineCursor::SlowMoveToNextForSameLayoutObject(
 
 void InlineCursor::MoveTo(const LayoutObject& layout_object) {
   DCHECK(layout_object.IsInLayoutNGInlineFormattingContext());
-  if (UNLIKELY(layout_object.IsOutOfFlowPositioned())) {
+  if (layout_object.IsOutOfFlowPositioned()) [[unlikely]] {
     NOTREACHED_IN_MIGRATION();
     MakeNull();
     return;
@@ -1427,7 +1434,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
     const LayoutBlockFlow* root = layout_object.FragmentItemsContainer();
     DCHECK(root);
     SetRoot(*root);
-    if (UNLIKELY(!HasRoot())) {
+    if (!HasRoot()) [[unlikely]] {
       MakeNull();
       return;
     }
@@ -1437,7 +1444,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
   }
 
   wtf_size_t item_index = layout_object.FirstInlineFragmentItemIndex();
-  if (UNLIKELY(!item_index)) {
+  if (!item_index) [[unlikely]] {
 #if EXPENSIVE_DCHECKS_ARE_ON()
     const LayoutBlockFlow* root = layout_object.FragmentItemsContainer();
     InlineCursor check_cursor(*root);
@@ -1502,9 +1509,9 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
 #endif
 
     // Skip items before |items_|, in case |this| is part of IFC.
-    if (UNLIKELY(is_descendants_cursor)) {
+    if (is_descendants_cursor) [[unlikely]] {
       const wtf_size_t span_begin_item_index = SpanBeginItemIndex();
-      while (UNLIKELY(item_index < span_begin_item_index)) {
+      while (item_index < span_begin_item_index) [[unlikely]] {
         const FragmentItem& item = fragment_items_->Items()[item_index];
         const wtf_size_t next_delta = item.DeltaToNextForSameLayoutObject();
         if (!next_delta) {
@@ -1513,7 +1520,7 @@ void InlineCursor::MoveTo(const LayoutObject& layout_object) {
         }
         item_index += next_delta;
       }
-      if (UNLIKELY(item_index >= span_begin_item_index + items_.size())) {
+      if (item_index >= span_begin_item_index + items_.size()) [[unlikely]] {
         MakeNull();
         return;
       }
@@ -1727,7 +1734,7 @@ void InlineCursor::MoveToIncludingCulledInline(
 }
 
 void InlineCursor::MoveToNextForSameLayoutObject() {
-  if (UNLIKELY(culled_inline_)) {
+  if (culled_inline_) [[unlikely]] {
     MoveToNextForCulledInline();
     return;
   }
