2325a53b77bafe750aa4f544b46b07269afaf180
Andre Le <leandre@chromium.org>
2024-08-07 18:29:00 +0000

    Mahi: Update accessible attributes on some components in main panel
    - Make sure that the feedback buttons are accessed after the summary
      text/error view when traverse in the main panel.
    - Make sure ChromeVox announce when retry and loading states happen.
    
    Fixed: b:356224281, b:356211144, b:356164028
    Change-Id: Ieb6a37e5f1242d9461799c749363a52bddf291b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756236
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338616}

--- ash/system/mahi/mahi_error_status_view.cc
+++ ash/system/mahi/mahi_error_status_view.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/size.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -121,14 +122,19 @@ class ErrorContentsView : public views::FlexLayoutView,
 
         retry_link_->SetVisible(
             mahi_utils::CalculateRetryLinkVisible(error.status));
-        retry_link_->SetCallback(retry_link_->GetVisible()
-                                     ? base::BindRepeating(
-                                           [](MahiUiController* controller,
-                                              VisibilityState origin_state) {
-                                             controller->Retry(origin_state);
-                                           },
-                                           ui_controller_, error.origin_state)
-                                     : base::RepeatingClosure());
+        retry_link_->SetCallback(
+            retry_link_->GetVisible()
+                ? base::BindRepeating(
+                      [](MahiUiController* controller,
+                         VisibilityState origin_state,
+                         views::View* retry_link) {
+                        controller->Retry(origin_state);
+                        retry_link->GetViewAccessibility().AnnounceText(
+                            l10n_util::GetStringUTF16(
+                                IDS_ASH_MAHI_RETRY_LINK_CLICK_ACTIVATION_ACCESSIBLE_NAME));
+                      },
+                      ui_controller_, error.origin_state, retry_link_)
+                : base::RepeatingClosure());
         return;
       }
       case MahiUiUpdateType::kAnswerLoaded:
--- ash/system/mahi/summary_outlines_section.cc
+++ ash/system/mahi/summary_outlines_section.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "ash/strings/grit/ash_strings.h"
 #include "ash/style/typography.h"
 #include "ash/system/mahi/mahi_animation_utils.h"
 #include "ash/system/mahi/mahi_constants.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
 #include "ui/gfx/text_constants.h"
 #include "ui/gfx/vector_icon_types.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/animated_image_view.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -87,6 +89,8 @@ SummaryOutlinesSection::SummaryOutlinesSection(MahiUiController* ui_controller)
       views::Builder<views::AnimatedImageView>()
           .CopyAddressTo(&summary_loading_animated_image_)
           .SetID(mahi_constants::ViewId::kSummaryLoadingAnimatedImage)
+          .SetAccessibleName(
+              l10n_util::GetStringUTF16(IDS_ASH_MAHI_LOADING_ACCESSIBLE_NAME))
           .SetAnimatedImage(mahi_animation_utils::GetLottieAnimationData(
               IDR_MAHI_LOADING_SUMMARY_ANIMATION))
           .Build());
@@ -130,6 +134,8 @@ SummaryOutlinesSection::SummaryOutlinesSection(MahiUiController* ui_controller)
           .SetID(mahi_constants::ViewId::kOutlinesLoadingAnimatedImage)
           .SetAnimatedImage(mahi_animation_utils::GetLottieAnimationData(
               IDR_MAHI_LOADING_OUTLINES_ANIMATION))
+          .SetAccessibleName(
+              l10n_util::GetStringUTF16(IDS_ASH_MAHI_LOADING_ACCESSIBLE_NAME))
           .Build());
 
   outlines_section_container->AddChildView(
@@ -180,6 +186,16 @@ void SummaryOutlinesSection::OnUpdated(const MahiUiUpdate& update) {
   }
 }
 
+void SummaryOutlinesSection::AddedToWidget() {
+  // When the view is first constructed, we need to wait until the view is added
+  // to the widget to announce loading state.
+  if (summary_loading_animated_image_->GetVisible() ||
+      outlines_loading_animated_image_->GetVisible()) {
+    GetViewAccessibility().AnnounceText(
+        l10n_util::GetStringUTF16(IDS_ASH_MAHI_LOADING_ACCESSIBLE_NAME));
+  }
+}
+
 void SummaryOutlinesSection::HandleOutlinesLoaded(
     const std::vector<chromeos::MahiOutline>& outlines) {
   outlines_container_->RemoveAllChildViews();
@@ -200,7 +216,10 @@ void SummaryOutlinesSection::HandleOutlinesLoaded(
 
   outlines_loading_animated_image_->Stop();
   outlines_loading_animated_image_->SetVisible(false);
-  // TODO(b/330643995): Show the outlines section once it is ready.
+  // TODO(b/330643995): Show the outlines section once it is ready. Note that
+  // when enabling outlines, we need to make sure that the state of fully
+  // loaded is announced in accessibility (similar to what is done in
+  // `HandleSummaryLoaded()`).
   outlines_container_->SetVisible(false);
 
   // TODO(b/333916944): Add metrics recording the outline loading animation time
@@ -216,6 +235,9 @@ void SummaryOutlinesSection::HandleSummaryLoaded(
 
   base::UmaHistogramTimes(mahi_constants::kSummaryLoadingTimeHistogramName,
                           base::Time::Now() - summary_start_loading_time_);
+
+  GetViewAccessibility().AnnounceText(
+      l10n_util::GetStringUTF16(IDS_ASH_MAHI_LOADED_ACCESSIBLE_NAME));
 }
 
 void SummaryOutlinesSection::LoadSummaryAndOutlines() {
@@ -243,6 +265,9 @@ void SummaryOutlinesSection::LoadSummaryAndOutlines() {
       mahi_animation_utils::GetLottiePlaybackConfig(
           *outlines_loading_animated_image_->animated_image()->skottie(),
           IDR_MAHI_LOADING_OUTLINES_ANIMATION));
+
+  GetViewAccessibility().AnnounceText(
+      l10n_util::GetStringUTF16(IDS_ASH_MAHI_LOADING_ACCESSIBLE_NAME));
 
   summary_start_loading_time_ = base::Time::Now();
 
