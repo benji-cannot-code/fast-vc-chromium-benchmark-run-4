baca38a00a628ad2feb12811791a1122d95ba83c
Mustafa Emre Acer <meacer@chromium.org>
2024-08-07 21:53:01 +0000

    [HFM] Implement logic to auto-enable balanced mode
    Use the HttpsFirstBalancedModeAutoEnable flag to auto-enable Balanced
    Mode when possible. Don't auto-enable if the user already modified
    their HFM settings.
    
    Bug: 349860796
    Change-Id: I80fc05338df56418a30a7b91608e3e4b6df372ad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766624
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338700}

--- chrome/browser/ssl/generated_https_first_mode_pref_unittest.cc
+++ chrome/browser/ssl/generated_https_first_mode_pref_unittest.cc
@@ -63,7 +63,6 @@ class GeneratedHttpsFirstModePrefTest : public testing::Test {
   content::BrowserTaskEnvironment task_environment_;
 
  private:
-  // network::TestURLLoaderFactory test_url_loader_factory_;
   std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_adaptor_;
