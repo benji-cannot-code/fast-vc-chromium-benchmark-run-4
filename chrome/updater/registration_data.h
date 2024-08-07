3ebe2664f59c41eba427503a825cc54fdb9c62a6
Xiaoling Bao <xiaolingbao@google.com>
2024-08-07 22:23:48 +0000

    Skip app registration if it's on read-only file system (mac).
    Bug: 350103459
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Change-Id: I51627d230fb5743d07aec2b5c293f8fa3db64769
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5714854
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338720}

--- chrome/updater/registration_data.h
+++ chrome/updater/registration_data.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace updater {
 
 inline constexpr int kRegistrationSuccess = 0;
+inline constexpr int kRegistrationError = 1;
 
 struct RegistrationRequest {
   RegistrationRequest();
