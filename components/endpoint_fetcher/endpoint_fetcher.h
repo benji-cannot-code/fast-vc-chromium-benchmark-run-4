b19273158de9a8aa5172b447513e76a2919b386a
David Maunder <davidjm@google.com>
2024-08-07 14:18:37 +0000

    Make maximum number of retries configurable in EndpointFetcher
    Bug: 355230190
    Change-Id: Ie19e64d63d6196654749422dec693c9171a8a5cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763630
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338457}

--- components/endpoint_fetcher/endpoint_fetcher.h
+++ components/endpoint_fetcher/endpoint_fetcher.h
@@ -91,6 +91,7 @@ class EndpointFetcher {
     ~RequestParams() = default;
 
     std::optional<CredentialsMode> credentials_mode;
+    std::optional<int> max_retries;
 
     class Builder final {
      public:
@@ -105,6 +106,11 @@ class EndpointFetcher {
 
       Builder& SetCredentialsMode(const CredentialsMode& mode) {
         request_params_->credentials_mode = mode;
+        return *this;
+      }
+
+      Builder& SetMaxRetries(const int retries) {
+        request_params_->max_retries = retries;
         return *this;
       }
 
@@ -206,6 +212,7 @@ class EndpointFetcher {
                             data_decoder::JsonSanitizer::Result result);
 
   network::mojom::CredentialsMode GetCredentialsMode();
+  int GetMaxRetries();
 
   enum AuthType { CHROME_API_KEY, OAUTH, NO_AUTH };
   AuthType auth_type_;
