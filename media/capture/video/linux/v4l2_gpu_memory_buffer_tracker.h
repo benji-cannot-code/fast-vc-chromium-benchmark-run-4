2143141454602422130d7b4c3242bebe2d15a8db
vikas soni <vikassoni@chromium.org>
2024-08-07 22:40:16 +0000

    [MappableSI] Convert V4L2GpuMemoryBufferTracker to use MappableSI.
    Convert V4L2GpuMemoryBufferTracker to use MappableSI instead of
    GpuMemoryBuffer. Future CLs will rename the class, file names and unit
    tests to remove GpuMemoryBuffer naming convention and update it with
    appropriate name.
    
    Bug: 40263579
    Change-Id: Ibbe282763362e481461fbe004e8df9fb06fe5598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753370
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338748}

--- media/capture/video/linux/v4l2_gpu_memory_buffer_tracker.h
+++ media/capture/video/linux/v4l2_gpu_memory_buffer_tracker.h
@@ -12,9 +12,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/video_capture_buffer_tracker.h"
 #include "media/capture/video/video_capture_gpu_channel_host.h"
 
+namespace gpu {
+class ClientSharedImage;
+}
+
 namespace media {
 
-// Tracker specifics for Linux GpuMemoryBuffer.
+// Tracker specifics for Linux Mappable shared image.
 // This class is owned by `VideoCaptureBufferPoolImpl`, which instantiates
 // `this` using its `CreateTracker()` method. It is then accessed on both the
 // main thread and the `VideoCaptureDeviceLinux::task_runner_`'s thread pool.
@@ -34,6 +38,9 @@ namespace media {
 // from the thread pool to the tracker's `IsReusableForFormat()` method will
 // then return false. The `VideoCaptureBufferPoolImpl` will then no longer
 // use this tracker and will release it when the buffer pool is full.
+// TODO(crbug.com/40263579): Rename this class, file name, comments as well as
+// related unit tests in follow up CL as it uses MappableSI now instead of
+// GpuMemoryBuffers.
 class CAPTURE_EXPORT V4L2GpuMemoryBufferTracker final
     : public VideoCaptureBufferTracker,
       public VideoCaptureGpuContextLostObserver {
@@ -66,7 +73,7 @@ class CAPTURE_EXPORT V4L2GpuMemoryBufferTracker final
 
  private:
   std::atomic_bool is_valid_{false};
-  std::unique_ptr<gfx::GpuMemoryBuffer> buffer_;
+  scoped_refptr<gpu::ClientSharedImage> shared_image_;
   base::WeakPtrFactory<VideoCaptureGpuContextLostObserver> weak_ptr_factory_{
       this};
 };
