66e74291cd33ed39b84dcb7d14f21679efc9b8a5
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-08-07 16:51:10 +0000

    Remove BuiltInBackendToAndroidBackendMigrator::MigrationType
    No behavior change. This class used to be able to perform different
    MigrationTypes before crrev.com/c/5754903, but now only kNone and
    kForLocalUsers are used. Remove the enum and replace it with `bool`
    where appropriate.
    
    This change also removes StartAccountMigrationIfNecessary(), which
    was unused.
    
    OBSOLETE_HISTOGRAMS=These migrations were removed along with the corresponding histograms: InitialMigrationForSyncUsers, NonSyncableDataMigrationToAndroidBackend, NonSyncableDataMigrationToBuiltInBackend, ReenrollmentAttemptMigration
    
    Fixed: 346556567
    Change-Id: I54e954716e5ea289ff49d070d6c249dcad795ff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756591
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338521}

--- chrome/browser/password_manager/android/password_store_backend_migration_decorator.cc
+++ chrome/browser/password_manager/android/password_store_backend_migration_decorator.cc
@@ -99,9 +99,7 @@ bool PasswordStoreBackendMigrationDecorator::IsAbleToSavePasswords() {
   // Suppress saving while the migration of local passwords is ongoing, to avoid
   // the migration "forgetting" any new passwords.
   return active_backend()->IsAbleToSavePasswords() &&
-         !(migrator_ && migrator_->migration_in_progress_type() ==
-                            BuiltInBackendToAndroidBackendMigrator::
-                                MigrationType::kForLocalUsers);
+         !(migrator_ && migrator_->migration_in_progress());
 }
 
 void PasswordStoreBackendMigrationDecorator::GetAllLoginsAsync(
