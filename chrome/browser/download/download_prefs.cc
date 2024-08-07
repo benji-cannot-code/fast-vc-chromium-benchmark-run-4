eb201b98f1822178e4f2a44185a290fd787dffeb
Daniel Rubery <drubery@chromium.org>
2024-08-07 23:41:36 +0000

    Add impression cap for app verification prompt
    Based on feedback from privacy, "once per session" is too
    naggy. Instead, store the timestamps in a pref and show at most 3 times
    in 90 days.
    
    Bug: 343226682
    Change-Id: I6914425b88e466a9957d72d890ecd22fe2c7ce58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765676
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338784}

--- chrome/browser/download/download_prefs.cc
+++ chrome/browser/download/download_prefs.cc
@@ -319,6 +319,8 @@ void DownloadPrefs::RegisterProfilePrefs(
 
   registry->RegisterBooleanPref(prefs::kShowMissingSdCardErrorAndroid, true);
   registry->RegisterBooleanPref(prefs::kAutoOpenPdfEnabled, false);
+  registry->RegisterListPref(prefs::kDownloadAppVerificationPromptTimestamps,
+                             {});
 #endif
 }
 
