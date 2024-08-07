2919085db050f3b8f6b3be8e586647bb99512d39
Takashi Sakamoto <tasak@google.com>
2024-08-07 07:42:43 +0000

    [PA] Support PartitionAlloc-Everywhere on Windows component build.
    To use allocator_shim for the Windows component build, all shared
    libraries and executables must be linked with allocator_shim.dll.
    (i.e. perfetto, icu, v8, blink_core, blink_modules, and so on).
    Instead of modifying BUILD.gn-s, update //build/config/BUILD.gn
    to make all shared libraries and executables depend on
    PartitionAlloc with build_with_chromium guard.
    
    Bug: b/40262684
    
    Change-Id: I3532f94c46d6fd3823cf78d522feb8bb2a395672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5674204
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338304}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/stringprintf_pa_unittest.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/strings/stringprintf_pa_unittest.cc
@@ -12,11 +12,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace partition_alloc::internal::base {
 
-TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfEmpty) {
+#if PA_BUILDFLAG(IS_WIN) && defined(COMPONENT_BUILD) && \
+    PA_BUILDFLAG(USE_PARTITION_ALLOC_AS_MALLOC)
+// TODO(crbug.com/1429450): TruncatingStringPrintf() defined in
+// allocator_base.dll allocates string from system allocator, but
+// base_unittests deallocates the string by using PartitionAlloc. This causes
+// crashes.
+#define MAYBE_PartitionAllocStringPrintfTest \
+  DISABLED_PartitionAllocStringPrintfTest
+#else
+#define MAYBE_PartitionAllocStringPrintfTest PartitionAllocStringPrintfTest
+#endif
+
+TEST(MAYBE_PartitionAllocStringPrintfTest, TruncatingStringPrintfEmpty) {
   EXPECT_EQ("", TruncatingStringPrintf("%s", ""));
 }
 
-TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfMisc) {
+TEST(MAYBE_PartitionAllocStringPrintfTest, TruncatingStringPrintfMisc) {
   EXPECT_EQ("123hello w",
             TruncatingStringPrintf("%3d%2s %1c", 123, "hello", 'w'));
 }
@@ -24,7 +36,8 @@ TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfMisc) {
 // Test that TruncatingStringPrintf truncates too long result.
 // The original TruncatingStringPrintf does not truncate. Instead, it allocates
 // memory and returns an entire result.
-TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfTruncatesResult) {
+TEST(MAYBE_PartitionAllocStringPrintfTest,
+     TruncatingStringPrintfTruncatesResult) {
   std::vector<char> buffer;
   buffer.resize(kMaxLengthOfTruncatingStringPrintfResult + 1);
   std::fill(buffer.begin(), buffer.end(), 'a');
@@ -35,7 +48,7 @@ TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfTruncatesResult) {
 }
 
 // Test that TruncatingStringPrintf does not change errno.
-TEST(PartitionAllocStringPrintfTest, TruncatingStringPrintfErrno) {
+TEST(MAYBE_PartitionAllocStringPrintfTest, TruncatingStringPrintfErrno) {
   errno = 1;
   EXPECT_EQ("", TruncatingStringPrintf("%s", ""));
   EXPECT_EQ(1, errno);
