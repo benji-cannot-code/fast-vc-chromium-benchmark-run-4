a903f2eca1b49f31aed3e6ce6912f8982789d19e
Sylvain Defresne <sdefresne@chromium.org>
2024-08-07 14:09:02 +0000

    [ios] Remove BrowserStateInfoCacheObserver
    Convert the only BrowserStateInfoCacheObserver sub-class into a
    ChromeBrowserStateManagerObserver. While doing this, refactor it
    to reduce the number of independent objects allocated, and also
    simplify the logic (no need to store a count of OTR WebState when
    the code is only checking whether there is a non-zero number of
    OTR WebState, storing a boolean is enough).
    
    Fixed: 357029211
    Change-Id: I4bbf7eecb4db41a18d1125ce62969845d5802e3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766253
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338451}

--- ios/chrome/browser/shared/model/browser_state/browser_state_info_cache.h
+++ ios/chrome/browser/shared/model/browser_state/browser_state_info_cache.h
@@ -14,10 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/memory/raw_ptr.h"
-#include "base/observer_list.h"
+#include "base/time/time.h"
 #include "base/values.h"
 
-class BrowserStateInfoCacheObserver;
 class PrefRegistrySimple;
 class PrefService;
 
@@ -39,10 +38,6 @@ class BrowserStateInfoCache {
 
   // Returns the count of known browser states.
   size_t GetNumberOfBrowserStates() const;
-
-  // Adds and removes an observer.
-  void AddObserver(BrowserStateInfoCacheObserver* observer);
-  void RemoveObserver(BrowserStateInfoCacheObserver* observer);
 
   // Gets and sets information related to browser states.
   size_t GetIndexOfBrowserStateWithName(std::string_view name) const;
@@ -80,7 +75,6 @@ class BrowserStateInfoCache {
 
   raw_ptr<PrefService> prefs_;
   std::vector<std::string> sorted_keys_;
-  base::ObserverList<BrowserStateInfoCacheObserver, true> observer_list_;
 };
 
 #endif  // IOS_CHROME_BROWSER_SHARED_MODEL_BROWSER_STATE_BROWSER_STATE_INFO_CACHE_H_
