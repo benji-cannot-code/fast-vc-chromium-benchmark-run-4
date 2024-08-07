864dd5651723aaf484357414b5133be31379c257
Mihai Sardarescu <msarda@chromium.org>
2024-08-07 13:40:20 +0000

    Add explicit APIs to access primary account event source
    This CL removes the confusing API to access the event source and
    replaces it with 2 APIs that access the access point for setting the
    primary account and the source for clearing it.
    
    This CL is just code clean-up and should have bring no functional
    change.
    
    Bug: None
    Change-Id: Iee6cf3d6f0727e24864030b2b872c27768cbf349
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756771
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1338439}

--- components/signin/internal/identity_manager/primary_account_manager.cc
+++ components/signin/internal/identity_manager/primary_account_manager.cc
@@ -70,21 +70,18 @@ void LogPrimaryAccountChangeMetrics(PrimaryAccountChangeEvent event_details) {
         break;
       }
 
-      DCHECK(absl::holds_alternative<signin_metrics::AccessPoint>(
-          event_details.GetEventSource()));
+      DCHECK(event_details.GetSetPrimaryAccountAccessPoint().has_value());
       base::UmaHistogramEnumeration(
           "Signin.SignIn.Completed",
-          absl::get<signin_metrics::AccessPoint>(
-              event_details.GetEventSource()),
+          event_details.GetSetPrimaryAccountAccessPoint().value(),
           signin_metrics::AccessPoint::ACCESS_POINT_MAX);
       break;
 
     case PrimaryAccountChangeEvent::Type::kCleared:
-      DCHECK(absl::holds_alternative<signin_metrics::ProfileSignout>(
-          event_details.GetEventSource()));
-      base::UmaHistogramEnumeration("Signin.SignOut.Completed",
-                                    absl::get<signin_metrics::ProfileSignout>(
-                                        event_details.GetEventSource()));
+      DCHECK(event_details.GetClearPrimaryAccountSource().has_value());
+      base::UmaHistogramEnumeration(
+          "Signin.SignOut.Completed",
+          event_details.GetClearPrimaryAccountSource().value());
       break;
   }
 
@@ -93,21 +90,18 @@ void LogPrimaryAccountChangeMetrics(PrimaryAccountChangeEvent event_details) {
       break;
 
     case PrimaryAccountChangeEvent::Type::kSet:
-      DCHECK(absl::holds_alternative<signin_metrics::AccessPoint>(
-          event_details.GetEventSource()));
+      DCHECK(event_details.GetSetPrimaryAccountAccessPoint().has_value());
       base::UmaHistogramEnumeration(
           "Signin.SyncOptIn.Completed",
-          absl::get<signin_metrics::AccessPoint>(
-              event_details.GetEventSource()),
+          event_details.GetSetPrimaryAccountAccessPoint().value(),
           signin_metrics::AccessPoint::ACCESS_POINT_MAX);
       break;
 
     case PrimaryAccountChangeEvent::Type::kCleared:
-      DCHECK(absl::holds_alternative<signin_metrics::ProfileSignout>(
-          event_details.GetEventSource()));
-      base::UmaHistogramEnumeration("Signin.SyncTurnOff.Completed",
-                                    absl::get<signin_metrics::ProfileSignout>(
-                                        event_details.GetEventSource()));
+      DCHECK(event_details.GetClearPrimaryAccountSource().has_value());
+      base::UmaHistogramEnumeration(
+          "Signin.SyncTurnOff.Completed",
+          event_details.GetClearPrimaryAccountSource().value());
       break;
   }
 }
@@ -744,9 +738,9 @@ void PrimaryAccountManager::ComputeExplicitBrowserSignin(
       }
       return;
     case PrimaryAccountChangeEvent::Type::kSet:
-      CHECK(event_details.GetAccessPoint().has_value());
+      CHECK(event_details.GetSetPrimaryAccountAccessPoint().has_value());
       signin_metrics::AccessPoint access_point =
-          event_details.GetAccessPoint().value();
+          event_details.GetSetPrimaryAccountAccessPoint().value();
 
       if (access_point == signin_metrics::AccessPoint::ACCESS_POINT_UNKNOWN ||
           access_point ==
