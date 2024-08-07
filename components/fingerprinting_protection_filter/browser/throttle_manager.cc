80bde5f19a03586545ede948ccfbd583b9f1fdb6
Martin Verde <thesalsa@google.com>
2024-08-07 15:53:55 +0000

    [FPF] Remove DumpWithoutCrashing from ThrottleManager
    This dump was added purely for debugging and is causing crashes in some
    tests.
    
    Bug: 40280666,331488870,357863466
    Change-Id: I1f0be7419a259be3469848000c311ad839b5ac4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765518
    Commit-Queue: Martin Verde <thesalsa@google.com>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338503}

--- components/fingerprinting_protection_filter/browser/throttle_manager.cc
+++ components/fingerprinting_protection_filter/browser/throttle_manager.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/containers/contains.h"
-#include "base/debug/crash_logging.h"
-#include "base/debug/dump_without_crashing.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_macros.h"
@@ -212,16 +210,6 @@ void ThrottleManager::DidFinishInFrameNavigation(
   if (!navigation_handle->HasCommitted() && !is_initial_navigation) {
     // TODO(https://crbug.com/40280666): Figure out how we can have a
     // navigation that is not the initial with no frame filter.
-    SCOPED_CRASH_KEY_STRING1024(
-        "crbug40280666", "navigation-url",
-        navigation_handle->GetURL().possibly_invalid_spec());
-    SCOPED_CRASH_KEY_STRING256(
-        "crbug40280666", "navigation-error-code",
-        net::ErrorToString(navigation_handle->GetNetErrorCode()));
-    SCOPED_CRASH_KEY_STRING1024(
-        "crbug40280666", "last-committed-url",
-        frame_host->GetLastCommittedURL().possibly_invalid_spec());
-    base::debug::DumpWithoutCrashing();
     return;
   }
 
