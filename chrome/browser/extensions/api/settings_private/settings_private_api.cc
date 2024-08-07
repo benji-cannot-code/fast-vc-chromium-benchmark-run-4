d49717a3300d9bf484d75d64bb64aa8eca3e11df
Antonio Gomes <tonikitoo@igalia.com>
2024-08-07 11:41:38 +0000

    Modularize //chrome/browser/ui/zoom
    This CL is a refactor with no intended behavior change.
    
    R=erikchen@chromium.org
    
    Change-Id: I0bd17b35c4759168ac3a6cf9a1dc4f2e7d8a6add
    Bug: 357784804
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757549
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338395}

--- chrome/browser/extensions/api/settings_private/settings_private_api.cc
+++ chrome/browser/extensions/api/settings_private/settings_private_api.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/settings_private/settings_private_delegate.h"
 #include "chrome/browser/extensions/api/settings_private/settings_private_delegate_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/zoom/chrome_zoom_level_prefs.h"
 #include "chrome/common/extensions/api/settings_private.h"
 #include "content/public/common/page_zoom.h"
 #include "extensions/browser/extension_function_registry.h"
