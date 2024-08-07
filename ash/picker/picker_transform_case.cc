2347d08e82673bf6c22bda5e85a897aa0b2e56e8
Darren Shen <shend@chromium.org>
2024-08-07 07:17:19 +0000

    Use icu title case.
    - Fixes a bug with title case where letters after the first one are not
      converted to lower case.
    - Gets i18n for free.
    
    Bug: b:333490858
    Change-Id: I7dd16b2b60356796d9ef2ee03aa68f472aa165dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768513
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338299}

--- ash/picker/picker_transform_case.cc
+++ ash/picker/picker_transform_case.cc
@@ -9,15 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/i18n/case_conversion.h"
+#include "base/i18n/unicodestring.h"
+#include "third_party/icu/source/common/unicode/unistr.h"
 
 namespace ash {
-namespace {
 
-bool u16_isalpha(char16_t ch) {
-  return (ch >= u'A' && ch <= u'Z') || (ch >= u'a' && ch <= u'z');
-}
-
-}  // namespace
+// TODO: b/333490858 - These functions should take in a locale as a parameter
+// instead of using the default locale.
 
 std::u16string PickerTransformToLowerCase(std::u16string_view text) {
   return base::i18n::ToLower(text);
@@ -28,14 +26,9 @@ std::u16string PickerTransformToUpperCase(std::u16string_view text) {
 }
 
 std::u16string PickerTransformToTitleCase(std::u16string_view text) {
-  std::u16string result(text);
-  std::u16string uppercase_text = base::i18n::ToUpper(text);
-  for (size_t i = 0; i < result.length(); i++) {
-    if (u16_isalpha(result[i]) && (i == 0 || result[i - 1] == u' ')) {
-      result[i] = uppercase_text[i];
-    }
-  }
-  return result;
+  icu::UnicodeString unicode_text(text.data(), text.length());
+  return base::i18n::UnicodeStringToString16(
+      unicode_text.toTitle(/*titleIter=*/nullptr));
 }
 
 }  // namespace ash
