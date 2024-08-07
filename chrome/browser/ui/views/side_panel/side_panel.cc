df7710fece871905e3bb21118e61aefdd2029e50
dljames <dljames@chromium.org>
2024-08-07 01:10:48 +0000

    [SidePanel] Add customizable bookmarks panel width preference
    Create a preference for the bookmarks side panel size. Initialize
    the side panel width using this preference. Update the preference
    value on resize to persist user changes. Added tests to verify
    the preference is correctly set and updated during resizing.
    
    Change-Id: I928e191866c5d817d1ab374dd3705e763a4c3818
    Bug: 357101118
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5689289
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Commit-Queue: Jingqi Sun <sunjingqi47@gmail.com>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338219}

--- chrome/browser/ui/views/side_panel/side_panel.cc
+++ chrome/browser/ui/views/side_panel/side_panel.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/lens/lens_features.h"
+#include "components/prefs/pref_service.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
@@ -309,9 +310,21 @@ SidePanel::SidePanel(BrowserView* browser_view,
   SetVisible(false);
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
-  // TODO(pbos): Reconsider if SetPanelWidth() should add borders, if so move
-  // accounting for the border into SetPanelWidth(), otherwise remove this TODO.
-  SetPanelWidth(GetMinimumSize().width());
+  // Get the width from user preferences, defaulting to the minimum size if not
+  // set.
+  PrefService* prefs = browser_view_->browser()->profile()->GetPrefs();
+  int width = GetMinimumSize().width();  // Default to minimum size
+
+  if (base::FeatureList::IsEnabled(features::kSidePanelResizing)) {
+    int pref_width = prefs->GetInteger(prefs::kSidePanelBookmarksWidth);
+    if (pref_width > 0) {
+      width = pref_width;
+    }
+  }
+
+  // Set the panel width from the preference or use the minimum size as the
+  // default.
+  SetPanelWidth(width);
 
   SetBorder(views::CreateEmptyBorder(GetBorderInsets()));
 
@@ -490,11 +503,19 @@ void SidePanel::OnResize(int resize_amount, bool done_resizing) {
   if (done_resizing) {
     starting_width_on_resize_ = -1;
   }
+
   const int minimum_width = GetMinimumSize().width();
   if (proposed_width < minimum_width) {
     proposed_width = minimum_width;
   }
+
   if (width() != proposed_width) {
+    PrefService* pref_service = browser_view_->browser()->profile()->GetPrefs();
+    if (pref_service &&
+        base::FeatureList::IsEnabled(features::kSidePanelResizing)) {
+      pref_service->SetInteger(prefs::kSidePanelBookmarksWidth, proposed_width);
+    }
+
     SetPanelWidth(proposed_width);
     did_resize_ = true;
   }
