4be5b63353c41e98ca73d9f07a426cce5d39da82
Philip Jägenstedt <foolip@chromium.org>
2024-08-07 11:27:03 +0000

    Add WebDXFeature counter for image-set()
    Change-Id: Ib4f7b18e374f110b2dfbdc0d9bb9238e699aeca7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756571
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Philip Jägenstedt <foolip@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338390}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -418,6 +418,7 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kCanvasRenderingContext2DRoundRect,
            WebDXFeature::kCanvasRoundrect},
           {WebFeature::kCSSColorMixFunction, WebDXFeature::kColorMix},
+          {WebFeature::kImageSet, WebDXFeature::kImageSet},
       });
 
   return *kMap;
