fe7862d33a583a52011a2a79b3af9c620b8a14ae
Ewann Pelle <ewannpv@chromium.org>
2024-08-07 11:45:09 +0000

    [iOS][TGS] Dismiss group view when group is deleted
    Dismiss the group view if it has been deleted during a batch update.
    
    Fixed: 357779165
    Change-Id: I59c5656eec827f5c0eed11f5aa4adabdc0c51534
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766677
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338396}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/tab_groups/tab_group_mediator.mm
@@ -428,6 +428,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (_tabGroup) {
     [_groupConsumer setGroupTitle:_tabGroup->GetTitle()];
     [_groupConsumer setGroupColor:_tabGroup->GetColor()];
+  } else {
+    [self.tabGroupsHandler hideTabGroup];
   }
 }
 
