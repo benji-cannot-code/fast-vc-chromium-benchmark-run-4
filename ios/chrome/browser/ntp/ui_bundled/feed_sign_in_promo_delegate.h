a352fccada35396a23a1fe50f2be2165f6250e5c
Chris Lu <thegreenfrog@chromium.org>
2024-08-07 15:36:52 +0000

    [ios, CodeHealth] move //ui/ntp header files to //ntp/ui_bundled
    This change moves the dependency targets that are needed downstream
    so that downstream can be migrated before doing the bulk
    //ntp/ui_bundled migration.
    
    Bug: 324233176
    Change-Id: Icd76faa0b23988c453ea7222769fc4009214270f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761814
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338495}

---
+++ ios/chrome/browser/ntp/ui_bundled/feed_sign_in_promo_delegate.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NTP_UI_BUNDLED_FEED_SIGN_IN_PROMO_DELEGATE_H_
+#define IOS_CHROME_BROWSER_NTP_UI_BUNDLED_FEED_SIGN_IN_PROMO_DELEGATE_H_
+
+// Temporary include for downstream compatibility.
+#import "ios/chrome/browser/ui/ntp/feed_sign_in_promo_delegate.h"
+
+#endif  // IOS_CHROME_BROWSER_NTP_UI_BUNDLED_FEED_SIGN_IN_PROMO_DELEGATE_H_
