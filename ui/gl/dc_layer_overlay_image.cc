dd57d8eae31b9e3e860fc93bcbba0b2e184aa2ec
Michael Tang <tangm@microsoft.com>
2024-08-07 22:35:19 +0000

    viz: win: Make swap chain trace tests use a different trace event
    This change replaces the event, DCLayerTree::VisualTree::UpdateOverlay
    with SkiaOutputDeviceDComp::BeginOverlayAccess. This is because in
    SkiaOutputDeviceDComp, we know more about the image being overlaid than
    in DCLayerTree, notably the SharedImage debug label.
    
    The CheckSwapChainPath tests previously checked for the presence of a
    non-root-surface DCompVisualContent, which would indicate the presence
    of a swap chain. This causes issues with delegated compositing, which
    uses non-root-surface DCompVisualContents for all delegated quads.
    Instead, we emit a trace event in the output device that
    reports "SwapChainBuffer" in the case of a swap chain buffer.
    
    Change-Id: I2624d7d3d7155843165e92666b36d5a5080bcbe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768053
    Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    Commit-Queue: Michael Tang <tangm@microsoft.com>
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Maggie Chen <magchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338739}

--- ui/gl/dc_layer_overlay_image.cc
+++ ui/gl/dc_layer_overlay_image.cc
@@ -10,24 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <d3d11.h>
 #include <dcomp.h>
 
-#include "base/notreached.h"
-
 namespace gl {
-
-const char* DCLayerOverlayTypeToString(DCLayerOverlayType overlay_type) {
-  switch (overlay_type) {
-    case DCLayerOverlayType::kNV12Texture:
-      return "NV12Texture";
-    case DCLayerOverlayType::kNV12Pixmap:
-      return "NV12Pixmap";
-    case DCLayerOverlayType::kDCompVisualContent:
-      return "DCompVisualContent";
-    case DCLayerOverlayType::kDCompSurfaceProxy:
-      return "DCompSurfaceProxy";
-  }
-
-  NOTREACHED_NORETURN();
-}
 
 DCLayerOverlayImage::DCLayerOverlayImage(
     const gfx::Size& size,
