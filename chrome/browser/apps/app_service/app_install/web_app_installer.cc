9abde4031ee98f80e7875d572d6f9dab310ccc67
Tim Sergeant <tsergeant@chromium.org>
2024-08-07 06:37:16 +0000

    AIS: Support API option to install websites in Browser display mode
    App Install Service's HTTP response has a new boolean parameter
    indicating whether a website shortcut should open in a window or in a
    browser tab. We can use the WebAppInstallParams option to modify the
    UserDisplayMode of the installed app based on this value.
    
    Bug: b:353822302
    Change-Id: I4deb640867beb6fce459b01dd0e4d39efa74bcdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5751542
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338285}

--- chrome/browser/apps/app_service/app_install/web_app_installer.cc
+++ chrome/browser/apps/app_service/app_install/web_app_installer.cc
@@ -13,8 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/crosapi/crosapi_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/commands/install_app_from_verified_manifest_command.h"
+#include "chrome/browser/web_applications/mojom/user_display_mode.mojom-shared.h"
 #include "chrome/browser/web_applications/web_app_command_manager.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
+#include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chromeos/crosapi/mojom/web_app_types.mojom.h"
@@ -240,13 +242,21 @@ void WebAppInstaller::OnManifestRetrieved(
       }
     }();
 
+    web_app::WebAppInstallParams install_params;
+    if (data.package_id.package_type() == PackageType::kWebsite) {
+      install_params.user_display_mode =
+          web_app_data.open_as_window
+              ? web_app::mojom::UserDisplayMode::kStandalone
+              : web_app::mojom::UserDisplayMode::kBrowser;
+    }
+
     provider->command_manager().ScheduleCommand(
         std::make_unique<web_app::InstallAppFromVerifiedManifestCommand>(
             install_source,
             /*document_url=*/web_app_data.document_url,
             /*verified_manifest_url=*/web_app_data.original_manifest_url,
             /*verified_manifest_contents=*/std::move(*response),
-            expected_app_id, /*install_params=*/std::nullopt,
+            expected_app_id, install_params,
             base::BindOnce(&WebAppInstaller::OnAppInstalled,
                            weak_ptr_factory_.GetWeakPtr(), surface,
                            std::move(callback))));
