718135e920a48dd4e6695e0bd924c349f80f33de
Joemer Ramos <joemerramos@chromium.org>
2024-08-07 12:20:56 +0000

    [iOS] Create QueryData Struct for SafeBrowsingQueryManager
    Some of the Safe Browsing functions had 3+ parameters and ongoing async
    safe browsing implementations will add more parameters. To improve
    readability and promote cleaner code, we create a struct to hold the
    current parameters. The reason we overload HandlePolicyDecision is to
    ensure that we aren't changing behaviors for the current Safe Browsing
    implementation. The "older" function version will eventually be deleted
    in a clean up CL.
    
    Bug: 337243708
    Change-Id: Ic91cc75a4bbf0c8e99c696e95b512754c13cf187
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765737
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338410}

--- ios/components/security_interstitials/safe_browsing/safe_browsing_tab_helper.h
+++ ios/components/security_interstitials/safe_browsing/safe_browsing_tab_helper.h
@@ -77,6 +77,12 @@ class SafeBrowsingTabHelper
         safe_browsing::SafeBrowsingUrlCheckerImpl::PerformedCheck
             performed_check);
 
+    // Uses `query_data` to store the `policy_decision` for a non-stale query
+    // with a specific PerformedCheck.
+    void HandlePolicyDecision(
+        const SafeBrowsingQueryManager::QueryData& query_data,
+        const web::WebStatePolicyDecider::PolicyDecision& policy_decision);
+
     // Notifies the policy decider that a new main frame document has been
     // loaded.
     void UpdateForMainFrameDocumentChange();
@@ -177,11 +183,7 @@ class SafeBrowsingTabHelper
         safe_browsing::SafeBrowsingUrlCheckerImpl::PerformedCheck
             performed_check) override;
     void SafeBrowsingSyncQueryFinished(
-        SafeBrowsingQueryManager* manager,
-        const SafeBrowsingQueryManager::Query& query,
-        const SafeBrowsingQueryManager::Result& result,
-        safe_browsing::SafeBrowsingUrlCheckerImpl::PerformedCheck
-            performed_check) override;
+        const SafeBrowsingQueryManager::QueryData& query_data) override;
     void SafeBrowsingQueryManagerDestroyed(
         SafeBrowsingQueryManager* manager) override;
 
