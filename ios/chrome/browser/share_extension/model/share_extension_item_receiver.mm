cfd38e71127f8737bc3f6c1ff72c5a4e13d82dcc
Olivier ROBIN <olivierrobin@google.com>
2024-08-07 08:20:15 +0000

    Remove notification unsubscription from item receiver
    Low-Coverage-Reason: Hard to test - Requires multi process test to trigger item receiver
    Bug: 357141063
    Change-Id: Ia629b917b034f884191880fad6641fd2e91b9de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758173
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338327}

--- ios/chrome/browser/share_extension/model/share_extension_item_receiver.mm
+++ ios/chrome/browser/share_extension/model/share_extension_item_receiver.mm
@@ -62,13 +62,7 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 
 }  // namespace
 
-@interface ShareExtensionItemReceiver () <NSFilePresenter> {
-  BOOL _isObservingReadingListFolder;
-  BOOL _readingListFolderCreated;
-  raw_ptr<ReadingListModel> _readingListModel;
-  raw_ptr<bookmarks::BookmarkModel> _bookmarkModel;
-  scoped_refptr<base::SequencedTaskRunner> _taskRunner;
-}
+@interface ShareExtensionItemReceiver () <NSFilePresenter>
 
 // Checks if the reading list folder is already created and if not, create it.
 - (void)createReadingListFolder;
@@ -108,7 +102,14 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 
 @end
 
-@implementation ShareExtensionItemReceiver
+@implementation ShareExtensionItemReceiver {
+  BOOL _isObservingReadingListFolder;
+  BOOL _readingListFolderCreated;
+  BOOL _shutdownCalled;
+  raw_ptr<ReadingListModel> _readingListModel;
+  raw_ptr<bookmarks::BookmarkModel> _bookmarkModel;
+  scoped_refptr<base::SequencedTaskRunner> _taskRunner;
+}
 
 #pragma mark - NSObject lifetime
 
@@ -155,7 +156,7 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)shutdown {
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  _shutdownCalled = YES;
   if (_isObservingReadingListFolder) {
     [NSFileCoordinator removeFilePresenter:self];
   }
@@ -168,6 +169,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 
 - (void)createReadingListFolder {
   {
+    if (_shutdownCalled) {
+      return;
+    }
     base::ScopedBlockingCall scoped_blocking_call(
         FROM_HERE, base::BlockingType::WILL_BLOCK);
     NSFileManager* manager = [NSFileManager defaultManager];
@@ -187,6 +191,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)readingListFolderCreated {
+  if (_shutdownCalled) {
+    return;
+  }
   UIApplication* application = [UIApplication sharedApplication];
   if ([application applicationState] == UIApplicationStateActive) {
     _readingListFolderCreated = YES;
@@ -195,6 +202,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (BOOL)receivedData:(NSData*)data withCompletion:(ProceduralBlock)completion {
+  if (_shutdownCalled) {
+    return NO;
+  }
   NSError* error = nil;
   NSKeyedUnarchiver* unarchiver =
       [[NSKeyedUnarchiver alloc] initForReadingFromData:data error:&error];
@@ -272,7 +282,7 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
                        title:(NSString*)entryNSTitle
                          URL:(NSURL*)entryNSURL
                   completion:(ProceduralBlock)completion {
-  if (!_readingListModel || !_bookmarkModel) {
+  if (_shutdownCalled || !_readingListModel || !_bookmarkModel) {
     // Models may have been deleted after the file
     // processing started.
     return;
@@ -316,6 +326,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)handleFileAtURL:(NSURL*)url withCompletion:(ProceduralBlock)completion {
+  if (_shutdownCalled) {
+    return;
+  }
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::WILL_BLOCK);
   if (![[NSFileManager defaultManager] fileExistsAtPath:[url path]]) {
@@ -350,6 +363,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)deleteFileAtURL:(NSURL*)url withCompletion:(ProceduralBlock)completion {
+  if (_shutdownCalled) {
+    return;
+  }
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::WILL_BLOCK);
   void (^deletingAccessor)(NSURL*) = ^(NSURL* newURL) {
@@ -372,7 +388,8 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)applicationDidBecomeActive {
-  if (!_readingListFolderCreated || _isObservingReadingListFolder) {
+  if (_shutdownCalled || !_readingListFolderCreated ||
+      _isObservingReadingListFolder) {
     return;
   }
   _isObservingReadingListFolder = YES;
@@ -390,6 +407,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)processExistingFiles {
+  if (_shutdownCalled) {
+    return;
+  }
   base::ScopedBlockingCall scoped_blocking_call(FROM_HERE,
                                                 base::BlockingType::WILL_BLOCK);
   NSMutableArray<NSURL*>* files = [NSMutableArray array];
@@ -419,7 +439,7 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 - (void)entriesReceived:(NSArray<NSURL*>*)files {
   UMA_HISTOGRAM_COUNTS_100("IOS.ShareExtension.ReceivedEntriesCount",
                            [files count]);
-  if (!_taskRunner) {
+  if (_shutdownCalled || !_taskRunner) {
     return;
   }
 
@@ -441,7 +461,7 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 }
 
 - (void)applicationWillResignActive {
-  if (!_isObservingReadingListFolder) {
+  if (_shutdownCalled || !_isObservingReadingListFolder) {
     return;
   }
   _isObservingReadingListFolder = NO;
@@ -451,6 +471,9 @@ void LogHistogramReceivedItem(ShareExtensionItemReceived type) {
 #pragma mark - NSFilePresenter methods
 
 - (void)presentedSubitemDidChangeAtURL:(NSURL*)url {
+  if (_shutdownCalled) {
+    return;
+  }
   if (_taskRunner) {
     __weak ShareExtensionItemReceiver* weakSelf = self;
     _taskRunner->PostTask(FROM_HERE, base::BindOnce(^{
