9e04e1b89409109feed727f140d3ea2ab48fa2c5
Noah Rose Ledesma <noahrose@google.com>
2024-08-07 17:29:53 +0000

    Impersonate logged-in user for networking on Mac
    Determine the logged in user by querying the system configuration
    dynamic store and impersonate them via `launchctl asuser` when launching
    the out-of-process networking job on Mac.
    
    Bug: 353730464
    Change-Id: I39556e2a9a816952c30dfa3db471e706fb711b54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767319
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338558}

--- chrome/enterprise_companion/url_loader_factory_provider.cc
+++ chrome/enterprise_companion/url_loader_factory_provider.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process/launch.h"
 #include "base/process/process.h"
 #include "base/sequence_checker.h"
+#include "base/strings/stringprintf.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/threading/sequence_bound.h"
@@ -39,6 +40,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/wrapper_shared_url_loader_factory.h"
 #include "services/network/public/mojom/url_loader_factory.mojom.h"
 #include "services/network/transitional_url_loader_factory_owner.h"
+
+#if BUILDFLAG(IS_MAC)
+#include <sys/types.h>
+
+#include "chrome/enterprise_companion/mac/mac_utils.h"
+#endif
 
 namespace enterprise_companion {
 
@@ -220,6 +227,17 @@ base::SequenceBound<URLLoaderFactoryProvider> CreateOutOfProcessNetWorker(
 
   base::CommandLine command_line(exe_path);
   command_line.AppendSwitch(kNetWorkerSwitch);
+
+  std::optional<uid_t> uid = GuessLoggedInUser();
+  if (!uid) {
+    LOG(ERROR)
+        << "Could not determine a logged-in user to impersonate for "
+           "networking. The root bootstrap namespace will be used, "
+           "which may cause proxy configuration or authorization to fail.";
+  } else {
+    command_line.PrependWrapper(
+        base::StringPrintf("/bin/launchctl asuser %d", *uid));
+  }
 
   channel.PrepareToPassRemoteEndpoint(&options, &command_line);
   base::Process process = base::LaunchProcess(command_line, options);
