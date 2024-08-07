3afc99ec14618a96f1e02041b719c7b1f315c930
Eriko Kurimoto <elkurin@chromium.org>
2024-08-07 04:51:13 +0000

    ash: Create //chrome/browser/ash/schedqos,
    Bug: b:356476538
    Change-Id: I5edd5a587b1402ea1f1d8992531456050c3da532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5754972
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338262}

--- chrome/browser/ash/schedqos/dbus_schedqos_state_handler.h
+++ chrome/browser/ash/schedqos/dbus_schedqos_state_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_DBUS_SCHEDQOS_STATE_HANDLER_H_
-#define CHROME_BROWSER_ASH_DBUS_SCHEDQOS_STATE_HANDLER_H_
+#ifndef CHROME_BROWSER_ASH_SCHEDQOS_DBUS_SCHEDQOS_STATE_HANDLER_H_
+#define CHROME_BROWSER_ASH_SCHEDQOS_DBUS_SCHEDQOS_STATE_HANDLER_H_
 
 #include <map>
 
@@ -145,4 +145,4 @@ class DBusSchedQOSStateHandler
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_DBUS_SCHEDQOS_STATE_HANDLER_H_
+#endif  // CHROME_BROWSER_ASH_SCHEDQOS_DBUS_SCHEDQOS_STATE_HANDLER_H_
--- chrome/browser/ash/schedqos/dbus_schedqos_state_handler_unittest.cc
+++ chrome/browser/ash/schedqos/dbus_schedqos_state_handler_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/dbus_schedqos_state_handler.h"
+#include "chrome/browser/ash/schedqos/dbus_schedqos_state_handler.h"
 
 #include <memory>
 
