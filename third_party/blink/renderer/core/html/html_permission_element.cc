90b9f3f7b6674fda14b4a2a5f93c4a4ad4922135
Thomas Nguyen <tungnh@chromium.org>
2024-08-07 10:44:52 +0000

    [PEPC] Record invalid style reasons
    More than 80% of user interaction denied cases are caused by invalid
    style. We need more details information to improve our grant rate.
    
    Bug: 357791469
    Change-Id: Id7e69493ace452bfc2505ce212efc72ee6c326b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5764982
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338378}

--- third_party/blink/renderer/core/html/html_permission_element.cc
+++ third_party/blink/renderer/core/html/html_permission_element.cc
@@ -1120,13 +1120,19 @@ bool HTMLPermissionElement::IsStyleValid() {
     AddConsoleWarning(
         String::Format("Cannot compute style for the permission element '%s'",
                        GetType().Utf8().c_str()));
+    base::UmaHistogramEnumeration("Blink.PermissionElement.InvalidStyleReason",
+                                  InvalidStyleReason::kNoComputedStyle);
     return false;
   }
 
   if (AreColorsNonOpaque(GetComputedStyle())) {
-    AddConsoleWarning(String::Format(
-        "Color or background color of the permission element '%s' is opaque",
-        GetType().Utf8().c_str()));
+    AddConsoleWarning(
+        String::Format("Color or background color of the permission element "
+                       "'%s' is non-opaque",
+                       GetType().Utf8().c_str()));
+    base::UmaHistogramEnumeration(
+        "Blink.PermissionElement.InvalidStyleReason",
+        InvalidStyleReason::kNonOpaqueColorOrBackgroundColor);
     return false;
   }
 
@@ -1136,6 +1142,9 @@ bool HTMLPermissionElement::IsStyleValid() {
         String::Format("Contrast between color and background color of the "
                        "permission element '%s' is too low",
                        GetType().Utf8().c_str()));
+    base::UmaHistogramEnumeration(
+        "Blink.PermissionElement.InvalidStyleReason",
+        InvalidStyleReason::kLowConstrastColorAndBackgroundColor);
     return false;
   }
 
@@ -1161,6 +1170,8 @@ bool HTMLPermissionElement::IsStyleValid() {
     AddConsoleWarning(
         String::Format("Font size of the permission element '%s' is too small",
                        GetType().Utf8().c_str()));
+    base::UmaHistogramEnumeration("Blink.PermissionElement.InvalidStyleReason",
+                                  InvalidStyleReason::kTooSmallFontSize);
     return false;
   }
 
@@ -1174,6 +1185,8 @@ bool HTMLPermissionElement::IsStyleValid() {
     AddConsoleWarning(
         String::Format("Font size of the permission element '%s' is too large",
                        GetType().Utf8().c_str()));
+    base::UmaHistogramEnumeration("Blink.PermissionElement.InvalidStyleReason",
+                                  InvalidStyleReason::kTooLargeFontSize);
     return false;
   }
 
