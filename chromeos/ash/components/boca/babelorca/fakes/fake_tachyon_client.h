c54abf5fa264de702afd9f32052bc25bca5dac9b
Ahmed Nasr <anasr@google.com>
2024-08-07 17:05:46 +0000

    [BabelOrca] Add fake authed client for testing.
    Also move fake classes to a subdir fakes.
    
    Bug: b:353974384
    Change-Id: Ieda912f515fd6f4b50006872a68166eab3462ced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758611
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338530}

--- chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_client.h
+++ chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_client.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKE_TACHYON_CLIENT_H_
-#define CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKE_TACHYON_CLIENT_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKES_FAKE_TACHYON_CLIENT_H_
+#define CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKES_FAKE_TACHYON_CLIENT_H_
 
 #include <memory>
 #include <string>
@@ -44,4 +44,4 @@ class FakeTachyonClient : public TachyonClient {
 
 }  // namespace babelorca
 
-#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKE_TACHYON_CLIENT_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_FAKES_FAKE_TACHYON_CLIENT_H_
