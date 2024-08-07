962b628e6076cf4ef4764ad58a278378d327ff6a
Sophie Wen <sophiewen@google.com>
2024-08-07 01:24:51 +0000

    sunfish: Add debug shortcut to start Sunfish Session
    This CL adds the debug shortcut Ctrl + Alt + Shift + S to call
    `CaptureModeController::StartSunfishSession()`, currently only a stub.
    Please see the dependent CL:5763896 for the initial implementation.
    
    Test: manual
    Bug: b/357658506
    Change-Id: Ibf6259b2fe708d4e90a9b93253afd3c71d8df12c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763350
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338222}

--- ash/accelerators/debug_commands.cc
+++ ash/accelerators/debug_commands.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/accelerators/accelerator_commands.h"
+#include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
@@ -324,6 +325,13 @@ void HandleShowSystemNudge() {
   Shell::Get()->anchored_nudge_manager()->Show(nudge_data);
 }
 
+void HandleStartSunfishSession() {
+  if (features::IsSunfishFeatureEnabled() &&
+      !Shell::Get()->session_controller()->IsUserSessionBlocked()) {
+    CaptureModeController::Get()->StartSunfishSession();
+  }
+}
+
 // TODO(b/318897434): Remove this shortcut after testing is complete.
 void HandleToggleFocusModeState() {
   auto* controller = FocusModeController::Get();
@@ -380,6 +388,9 @@ void PerformDebugActionIfEnabled(AcceleratorAction action) {
       break;
     case AcceleratorAction::kDebugPrintWindowHierarchy:
       HandlePrintWindowHierarchy();
+      break;
+    case AcceleratorAction::kDebugStartSunfishSession:
+      HandleStartSunfishSession();
       break;
     case AcceleratorAction::kDebugShowInformedRestore:
       HandleShowInformedRestore();
