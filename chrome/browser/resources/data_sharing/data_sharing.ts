990f6145fc339243ae1162f06fc427f6ee440977
Pengchao Cai <pengchaocai@chromium.org>
2024-08-07 17:45:43 +0000

    [data sharing] Parse url to invoke different dialogs in webui
    Also update the DEP ref tag.
    
    Bug: 357877332
    Change-Id: Ice42c0db3fced4f2854e8e4d51e6867acd5e118d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766329
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: pengchao Cai <pengchaocai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338576}

--- chrome/browser/resources/data_sharing/data_sharing.ts
+++ chrome/browser/resources/data_sharing/data_sharing.ts
@@ -5,7 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './data_sharing_sdk.js';
 
+import {$} from 'chrome-untrusted://resources/js/util.js';
+
 import {BrowserProxy} from './browser_proxy.js';
+
+// Match 'flow' param from the loaded URL to invoke corresponding dialog.
+const FLOW = {
+  share: 'share',
+  join: 'join',
+  manage: 'manage',
+};
+Object.freeze(FLOW);
 
 let initialized: boolean = false;
 
@@ -16,11 +26,41 @@ function onDOMContentLoaded() {
         window.data_sharing_sdk.setOauthAccessToken({accessToken});
         if (!initialized) {
           initialize();
+          processUrl();
           browserProxy.handler.showUI();
           initialized = true;
         }
       },
   );
+}
+
+// TODO(pengchaocai): Test in follow up CLs.
+async function processUrl() {
+  const currentUrl = window.location.href;
+  const params = new URL(currentUrl).searchParams;
+  const flow = params.get('flow');
+  const groupId = params.get('group_id');
+  const tokenSecret = params.get('token_secret');
+
+  switch (flow) {
+    case FLOW.share:
+      await window.data_sharing_sdk.runInviteFlow({});
+      break;
+    case FLOW.join:
+      // group_id and token_secret cannot be null for join flow.
+      await window.data_sharing_sdk.runJoinFlow(
+          {groupId: groupId!, tokenSecret: tokenSecret!});
+      break;
+    case FLOW.manage:
+      // group_id cannot be null for manage flow.
+      await window.data_sharing_sdk.runManageFlow({groupId: groupId!});
+      break;
+    default:
+      const debugContainer: HTMLDivElement|null =
+          $('debug-container') as HTMLDivElement;
+      debugContainer.setAttribute('debug', 'true');
+      break;
+  }
 }
 
 function getGroupId() {
@@ -67,7 +107,7 @@ async function initialize() {
 
   const inviteFlowButton = document.getElementById('create-invite-flow-button');
   inviteFlowButton?.addEventListener('click', () => {
-    maybeRunIfGroupId(window.data_sharing_sdk.runInviteFlow);
+    window.data_sharing_sdk.runInviteFlow({});
   });
 
   const manageFlowButton = document.getElementById('create-manage-flow-button');
