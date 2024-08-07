68036010e9fa4f416bfbd0a701215594dc90db77
Daniel Cheng <dcheng@chromium.org>
2024-08-07 18:19:16 +0000

    Remove unnecessary use of make_span<N>(...) in //chrome/browser
    `make_span<T>(...)` isn't needed if the arrays are statically sized.
    
    Bug: 341907909
    Change-Id: Ibc79ab70248bd9f9a0a04968abf8720a85e912cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768692
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338606}

--- chrome/browser/resources_integrity_unittest.cc
+++ chrome/browser/resources_integrity_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/resources_integrity.h"
 
+#include <algorithm>
+
 #include "base/functional/bind.h"
 #include "base/path_service.h"
 #include "base/task/sequenced_task_runner.h"
@@ -45,10 +47,10 @@ TEST_F(CheckResourceIntegrityTest, Mismatch) {
   base::FilePath test_data_path;
   ASSERT_TRUE(base::PathService::Get(chrome::DIR_TEST_DATA, &test_data_path));
 
-  std::vector<uint8_t> unexpected(crypto::kSHA256Length, 'a');
+  uint8_t unexpected[crypto::kSHA256Length];
+  std::ranges::fill(unexpected, 'a');
   base::RunLoop loop;
-  CheckResourceIntegrity(test_data_path.AppendASCII("circle.svg"),
-                         base::make_span<32>(unexpected),
+  CheckResourceIntegrity(test_data_path.AppendASCII("circle.svg"), unexpected,
                          base::SequencedTaskRunner::GetCurrentDefault(),
                          base::BindLambdaForTesting([&](bool matches) {
                            EXPECT_FALSE(matches);
@@ -58,12 +60,12 @@ TEST_F(CheckResourceIntegrityTest, Mismatch) {
 }
 
 TEST_F(CheckResourceIntegrityTest, NonExistentFile) {
-  std::vector<uint8_t> unexpected(crypto::kSHA256Length, 'a');
+  uint8_t unexpected[crypto::kSHA256Length];
+  std::ranges::fill(unexpected, 'a');
   base::RunLoop loop;
   CheckResourceIntegrity(
       base::FilePath(FILE_PATH_LITERAL("this file does not exist.moo")),
-      base::make_span<crypto::kSHA256Length>(unexpected),
-      base::SequencedTaskRunner::GetCurrentDefault(),
+      unexpected, base::SequencedTaskRunner::GetCurrentDefault(),
       base::BindLambdaForTesting([&](bool matches) {
         EXPECT_FALSE(matches);
         loop.Quit();
