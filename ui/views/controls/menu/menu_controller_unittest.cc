44b893c8c3b0f52b93a3c0cb6ff86913f7313109
Tim Barry <oregongraperoot@gmail.com>
2024-08-08 23:49:49 +0000

    Fix top-level Bubble Menu positioning
    This change allows the shadow of top-level Bubble menus
    to extend offscreen (like submenus already do), since the
    shadow is not an important part of the content.
    
    Bug: 338585369
    Change-Id: I2f8183b7209b99c3ab10761ada45b3eec0d3dc9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5742509
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339379}

--- ui/views/controls/menu/menu_controller_unittest.cc
+++ ui/views/controls/menu/menu_controller_unittest.cc
@@ -1117,7 +1117,7 @@ TEST_F(MenuControllerTest, VerifyMenuBubblePositionAfterSizeChanges) {
   const gfx::Point anchor_point(kMonitorBounds.width() / 2,
                                 kMonitorBounds.bottom() + 1 -
                                     kMenuSize.height() +
-                                    border_and_shadow_insets.top());
+                                    border_and_shadow_insets.height());
 
   MenuBoundsOptions options = {
       .anchor_bounds = gfx::Rect(anchor_point, gfx::Size()),
@@ -1127,7 +1127,7 @@ TEST_F(MenuControllerTest, VerifyMenuBubblePositionAfterSizeChanges) {
   // Case 1: There is insufficient space for the menu below `anchor_point` and
   // there is no cached menu position. The menu should show above the anchor.
   options.menu_size = kMenuSize;
-  EXPECT_GT(options.anchor_bounds.y() - border_and_shadow_insets.top() +
+  EXPECT_GT(options.anchor_bounds.y() - border_and_shadow_insets.height() +
                 kMenuSize.height(),
             kMonitorBounds.bottom());
   CalculateBubbleMenuBoundsWithoutInsets(options);
@@ -1144,7 +1144,7 @@ TEST_F(MenuControllerTest, VerifyMenuBubblePositionAfterSizeChanges) {
   // Case 3: There is enough space for the menu below `anchor_point`. The cached
   // menu position is above the anchor. The menu should show above the anchor.
   constexpr gfx::Size kUpdatedSize(kMenuSize.width(), kMenuSize.height() / 2);
-  EXPECT_LE(options.anchor_bounds.y() - border_and_shadow_insets.top() +
+  EXPECT_LE(options.anchor_bounds.y() - border_and_shadow_insets.height() +
                 kUpdatedSize.height(),
             kMonitorBounds.bottom());
   options.menu_size = kUpdatedSize;
@@ -1171,7 +1171,7 @@ TEST_F(MenuControllerTest, VerifyContextMenuBubblePositionAfterSizeChanges) {
   const gfx::Point anchor_point(kMonitorBounds.width() / 2,
                                 kMonitorBounds.bottom() + 1 -
                                     kMenuSize.height() +
-                                    border_and_shadow_insets.top());
+                                    border_and_shadow_insets.height());
 
   MenuBoundsOptions options = {
       .anchor_bounds = gfx::Rect(anchor_point, gfx::Size()),
@@ -1181,7 +1181,7 @@ TEST_F(MenuControllerTest, VerifyContextMenuBubblePositionAfterSizeChanges) {
   // Case 1: There is insufficient space for the menu below `anchor_point` and
   // there is no cached menu position. The menu should show above the anchor.
   options.menu_size = kMenuSize;
-  EXPECT_GT(options.anchor_bounds.y() - border_and_shadow_insets.top() +
+  EXPECT_GT(options.anchor_bounds.y() - border_and_shadow_insets.height() +
                 kMenuSize.height(),
             kMonitorBounds.bottom());
   CalculateBubbleMenuBoundsWithoutInsets(options);
@@ -1199,7 +1199,7 @@ TEST_F(MenuControllerTest, VerifyContextMenuBubblePositionAfterSizeChanges) {
   // Case 3: There is enough space for the menu below `anchor_point`. The cached
   // menu position is above the anchor. The menu should show above the anchor.
   constexpr gfx::Size kUpdatedSize(kMenuSize.width(), kMenuSize.height() / 2);
-  EXPECT_LE(options.anchor_bounds.y() - border_and_shadow_insets.top() +
+  EXPECT_LE(options.anchor_bounds.y() - border_and_shadow_insets.height() +
                 kUpdatedSize.height(),
             kMonitorBounds.bottom());
   options.menu_size = kUpdatedSize;
