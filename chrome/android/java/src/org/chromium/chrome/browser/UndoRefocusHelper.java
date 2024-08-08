d4ba1312176f636b72c929efd8955fc743f1fec5
Calder Kitagawa <ckitagawa@chromium.org>
2024-08-08 18:34:12 +0000

    Fix NPE in UndoRefocusHelper#destroy
    Typically mLayoutManager is set before destroy is called, but in rare
    circumstances it may not be. Fix this with a null check.
    
    Prior to a recent CL we were not calling destroy() correctly so this
    was a "hidden" bug when I fixed it to start being called.
    
    Fixed: 358190575
    Change-Id: Ic17ba4b99097efdbadab02bc23e62387d3d100df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771245
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1339201}

--- chrome/android/java/src/org/chromium/chrome/browser/UndoRefocusHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/UndoRefocusHelper.java
@@ -63,7 +63,9 @@ public class UndoRefocusHelper {
     public void destroy() {
         mTabModelSelectorTabModelObserver.destroy();
         mLayoutManagerObservableSupplier.removeObserver(mLayoutManagerSupplierCallback);
-        mLayoutManager.removeObserver(mLayoutStateObserver);
+        if (mLayoutManager != null) {
+            mLayoutManager.removeObserver(mLayoutStateObserver);
+        }
     }
 
     private void observeTabModel() {
