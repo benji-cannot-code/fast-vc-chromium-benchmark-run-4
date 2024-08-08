f8902de1ff92446a0e819e308d52e82bdc5a862e
Eriko Kurimoto <elkurin@chromium.org>
2024-08-08 02:45:00 +0000

    ash: Move //c/b/ash/set_time_dialog.* to //c/b/ui/webui/ash/set_time_dialog
    Bug: b:357061645
    Change-Id: I8719654dd8510d152a5212841380d211774d18eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769072
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338840}

--- chrome/browser/ui/webui/ash/set_time_dialog/set_time_dialog.h
+++ chrome/browser/ui/webui/ash/set_time_dialog/set_time_dialog.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_SET_TIME_DIALOG_H_
-#define CHROME_BROWSER_ASH_SET_TIME_DIALOG_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_SET_TIME_DIALOG_SET_TIME_DIALOG_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_SET_TIME_DIALOG_SET_TIME_DIALOG_H_
 
 #include "chrome/browser/ui/webui/ash/system_web_dialog_delegate.h"
 #include "ui/gfx/native_widget_types.h"
@@ -34,4 +34,4 @@ class SetTimeDialog : public SystemWebDialogDelegate {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_SET_TIME_DIALOG_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_SET_TIME_DIALOG_SET_TIME_DIALOG_H_
