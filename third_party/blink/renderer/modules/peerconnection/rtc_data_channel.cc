02bb01e29e434d6e307f52ab75231d3b272dc784
Florent Castelli <orphis@chromium.org>
2024-08-08 13:34:46 +0000

    Implement RTCDataChannel::priority
    The feature is being the flag RTCDataChannelPriority and is also gated
    by another feature flag internal to WebRTC to enable the WFQ scheduler
    inside the SCTP implementation.
    
    Bug: 40692348
    Change-Id: Ida4350e7db47d3e0d639e32d329429615d86dcc0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766683
    Reviewed-by: Mirko Bonadei <mbonadei@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339031}

--- third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_data_channel.cc
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/wtf/cross_thread_copier_std.h"
 #include "third_party/blink/renderer/platform/wtf/cross_thread_functional.h"
 #include "third_party/blink/renderer/platform/wtf/thread_safe_ref_counted.h"
+#include "third_party/webrtc/api/priority.h"
 
 namespace WTF {
 
@@ -391,6 +392,21 @@ std::optional<uint16_t> RTCDataChannel::id() const {
   id_ = static_cast<uint16_t>(id);
 
   return id;
+}
+
+String RTCDataChannel::priority() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  webrtc::PriorityValue priority = channel()->priority();
+  if (priority <= webrtc::PriorityValue(webrtc::Priority::kVeryLow)) {
+    return "very-low";
+  }
+  if (priority <= webrtc::PriorityValue(webrtc::Priority::kLow)) {
+    return "low";
+  }
+  if (priority <= webrtc::PriorityValue(webrtc::Priority::kMedium)) {
+    return "medium";
+  }
+  return "high";
 }
 
 String RTCDataChannel::readyState() const {
