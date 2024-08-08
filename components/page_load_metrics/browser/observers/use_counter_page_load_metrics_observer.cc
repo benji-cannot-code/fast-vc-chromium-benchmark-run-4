7ab075fbe634df8ec4f9813a6c8c02833d331fa4
Jeremy Roman <jbroman@chromium.org>
2024-08-08 17:55:45 +0000

    Add a UseCounter for structuredClone() and map it to a DX feature.
    It's probably not interesting to track usage on Window and
    WorkerGlobalScope separately, so a single UseCounter is used.
    
    Change-Id: I8565234aa1af72e1160c5e0e9907bfce9d4c74d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771973
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339172}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -419,6 +419,7 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
            WebDXFeature::kCanvasRoundrect},
           {WebFeature::kCSSColorMixFunction, WebDXFeature::kColorMix},
           {WebFeature::kImageSet, WebDXFeature::kImageSet},
+          {WebFeature::kStructuredCloneMethod, WebDXFeature::kStructuredClone},
       });
 
   return *kMap;
