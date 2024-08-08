7dd1369453695da73bfa740b19abdfe235b04917
Ian Kilpatrick <ikilpatrick@chromium.org>
2024-08-08 18:11:37 +0000

    [layout] Fix content-contributions of calc-size().
    Previously we'd just treat calc-size() as effectively the same as auto.
    
    This was incorrect. Instead:
     - Attempt to resolve the main length, if this succeeds this is our
       contribution. (This will capture the calc-size(min-content, size)
       case.
     - If this returns an indefinite size, we force the sizing keyword
       as auto, and for the min-size treat auto as min-content, and for
       the max-size treat auto as max-content.
    
    This patch also fixes some "depends_on_block_constraints" logic
    (accidentally) in cases like:
      <div style="min-width: min-content; width: 0px;"></div>
    where we weren't properly calculating this value.
    
    Bug: 40339056
    Change-Id: I22ee162deb49c37b948e5a22a12218b392710603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5750362
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: Ian Kilpatrick <ikilpatrick@chromium.org>
    Auto-Submit: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339188}

--- third_party/blink/renderer/core/layout/length_utils.h
+++ third_party/blink/renderer/core/layout/length_utils.h
@@ -80,7 +80,8 @@ ResolveInlineLengthInternal(const ConstraintSpace&,
                             const Length&,
                             const Length* auto_length,
                             LayoutUnit override_available_size,
-                            LayoutUnit unresolvable_length_result);
+                            LayoutUnit unresolvable_length_result,
+                            CalcSizeKeywordBehavior calc_size_keyword_behavior);
 
 // Same as ResolveInlineLengthInternal, except here |intrinsic_size| roughly
 // plays the part of |MinMaxSizes|.
@@ -106,7 +107,7 @@ inline LayoutUnit ResolveMinInlineLength(
   return ResolveInlineLengthInternal(
       constraint_space, style, border_padding, min_max_sizes_func, length,
       /* auto_length */ &Length::Auto(), override_available_size,
-      border_padding.InlineSum());
+      border_padding.InlineSum(), CalcSizeKeywordBehavior::kAsSpecified);
 }
 
 // Used for resolving max inline lengths, (|ComputedStyle::MaxLogicalWidth|).
@@ -121,7 +122,8 @@ inline LayoutUnit ResolveMaxInlineLength(
   // this LayoutUnit::Max that we pass to ResolveInlineLengthInternal.
   return ResolveInlineLengthInternal(
       constraint_space, style, border_padding, min_max_sizes_func, length,
-      /* auto_length */ nullptr, override_available_size, LayoutUnit::Max());
+      /* auto_length */ nullptr, override_available_size, LayoutUnit::Max(),
+      CalcSizeKeywordBehavior::kAsSpecified);
 }
 
 // Used for resolving main inline lengths, (|ComputedStyle::LogicalWidth|).
@@ -132,10 +134,13 @@ inline LayoutUnit ResolveMainInlineLength(
     MinMaxSizesFunctionRef min_max_sizes_func,
     const Length& length,
     const Length* auto_length,
-    LayoutUnit override_available_size = kIndefiniteSize) {
+    LayoutUnit override_available_size = kIndefiniteSize,
+    CalcSizeKeywordBehavior calc_size_keyword_behavior =
+        CalcSizeKeywordBehavior::kAsSpecified) {
   return ResolveInlineLengthInternal(constraint_space, style, border_padding,
                                      min_max_sizes_func, length, auto_length,
-                                     override_available_size, kIndefiniteSize);
+                                     override_available_size, kIndefiniteSize,
+                                     calc_size_keyword_behavior);
 }
 
 // Used for resolving min block lengths, (|ComputedStyle::MinLogicalHeight|).
