9c4a0fe024d2d6c4d0f048f39f0004ab9a045ae1
Awad Osman <awado@google.com>
2024-08-08 16:25:25 +0000

    Delete 1+ year old prefs
    This is a follow up to crrev.com/c/5765816 which deprecates the
    kSafeBrowsingEsbOptInWithFriendlierSettings pref.
    
    Bug: 357855820
    Change-Id: Ib8a5230d244d8c6aede41a0b21a784cdfccedf9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768994
    Commit-Queue: Awad Osman <awado@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339097}

--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -115,15 +115,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Deprecated 01/2023.
-const char* kTrialGroupMICeAndDefaultBrowserVersionPrefName =
-    "fre_refactoring_mice_and_default_browser.trial_version";
-
-// Deprecated 07/2023.
-const char kUnifiedConsentMigrationState[] = "unified_consent.migration_state";
-// Deprecated 07/2023.
-const char kNewTabPageFieldTrialPref[] = "new_tab_page.trial_version";
-
 // Deprecated 09/2023.
 const char kObsoleteIosSettingsPromoAlreadySeen[] =
     "ios.settings.promo_already_seen";
@@ -558,9 +549,6 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
   registry->RegisterBooleanPref(kAutofillBrandingKeyboardAccessoriesTapped,
                                 false);
 
-  registry->RegisterIntegerPref(kTrialGroupMICeAndDefaultBrowserVersionPrefName,
-                                -1);
-
   registry->RegisterIntegerPref(
       prefs::kIosCredentialProviderPromoLastActionTaken, -1);
 
@@ -851,9 +839,6 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   // Register pref used to determine if OS Lockdown Mode is enabled.
   registry->RegisterBooleanPref(prefs::kOSLockdownModeEnabled, false);
 
-  // Deprecated 07/2023.
-  registry->RegisterIntegerPref(kUnifiedConsentMigrationState, 0);
-
   // Register pref used to detect addresses in web page
   registry->RegisterBooleanPref(prefs::kDetectAddressesEnabled, true);
   registry->RegisterBooleanPref(prefs::kDetectAddressesAccepted, false);
@@ -1020,9 +1005,6 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
 
 // This method should be periodically pruned of year+ old migrations.
 void MigrateObsoleteLocalStatePrefs(PrefService* prefs) {
-  // Added 01/2023
-  prefs->ClearPref(kTrialGroupMICeAndDefaultBrowserVersionPrefName);
-
   // Added 10/2023.
   prefs->ClearPref(kAutofillBrandingKeyboardAccessoriesTapped);
 
@@ -1070,15 +1052,6 @@ void MigrateObsoleteBrowserStatePrefs(const base::FilePath& state_path,
                                       PrefService* prefs) {
   // Check MigrateDeprecatedAutofillPrefs() to see if this is safe to remove.
   autofill::prefs::MigrateDeprecatedAutofillPrefs(prefs);
-
-  // Added 07/2023.
-  prefs->ClearPref(kUnifiedConsentMigrationState);
-  syncer::SyncPrefs::MigrateAutofillWalletImportEnabledPref(prefs);
-
-  // Added 07/2023.
-  if (prefs->HasPrefPath(kNewTabPageFieldTrialPref)) {
-    prefs->ClearPref(kNewTabPageFieldTrialPref);
-  }
 
   // Added 08/2023.
   invalidation::InvalidatorRegistrarWithMemory::ClearDeprecatedPrefs(prefs);
