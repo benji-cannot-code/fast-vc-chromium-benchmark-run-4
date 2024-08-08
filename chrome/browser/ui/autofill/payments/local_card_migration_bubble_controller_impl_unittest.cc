c4352d7fc00186f972c35c93f36ac371a055ed75
Karol Sygiet <sygiet@google.com>
2024-08-08 13:01:25 +0000

    Remove usage of TestAutofillClock::Advance in tests
    This CL starts the cleanup process of removing AutofillClock in favour
    of base::Time. Since the number of affected files will be big, this
    refactor will be split into multiple CL, this being the first of them.
    
    Bug: 40100455
    Change-Id: Idec8e40f7177f7e3f1de50522ec1b6fd45925158
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773135
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1339013}

--- chrome/browser/ui/autofill/payments/local_card_migration_bubble_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/payments/local_card_migration_bubble_controller_impl_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/browser/metrics/payments/local_card_migration_metrics.h"
-#include "components/autofill/core/browser/test_autofill_clock.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "content/public/test/mock_navigation_handle.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -61,7 +60,9 @@ class TestLocalCardMigrationBubbleControllerImpl
 class LocalCardMigrationBubbleControllerImplTest
     : public BrowserWithTestWindowTest {
  public:
-  LocalCardMigrationBubbleControllerImplTest() = default;
+  LocalCardMigrationBubbleControllerImplTest()
+      : BrowserWithTestWindowTest(
+            base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   LocalCardMigrationBubbleControllerImplTest(
       const LocalCardMigrationBubbleControllerImplTest&) = delete;
   LocalCardMigrationBubbleControllerImplTest& operator=(
@@ -95,8 +96,6 @@ class LocalCardMigrationBubbleControllerImplTest
         TestLocalCardMigrationBubbleControllerImpl::FromWebContents(
             browser()->tab_strip_model()->GetActiveWebContents()));
   }
-
-  TestAutofillClock test_clock_;
 
  private:
   static void LocalCardMigrationCallback() {}
@@ -149,7 +148,7 @@ TEST_F(LocalCardMigrationBubbleControllerImplTest,
        StickyBubble_ShouldNotDismissUponNavigation) {
   ShowBubble();
   base::HistogramTester histogram_tester;
-  test_clock_.Advance(base::Seconds(10));
+  task_environment()->FastForwardBy(base::Seconds(10));
   controller()->SimulateNavigation();
 
   histogram_tester.ExpectTotalCount(
--- chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl_unittest.cc
+++ chrome/browser/ui/autofill/payments/offer_notification_bubble_controller_impl_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/autofill/core/browser/payments/offer_notification_options.h"
-#include "components/autofill/core/browser/test_autofill_clock.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/strings/grit/components_strings.h"
@@ -48,7 +47,9 @@ class TestOfferNotificationBubbleControllerImpl
 class OfferNotificationBubbleControllerImplTest
     : public BrowserWithTestWindowTest {
  public:
-  OfferNotificationBubbleControllerImplTest() = default;
+  OfferNotificationBubbleControllerImplTest()
+      : BrowserWithTestWindowTest(
+            base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   OfferNotificationBubbleControllerImplTest(
       const OfferNotificationBubbleControllerImplTest&) = delete;
   OfferNotificationBubbleControllerImplTest& operator=(
@@ -142,7 +143,6 @@ class OfferNotificationBubbleControllerImplTest
     controller->coupon_service_ = coupon_service;
   }
 
-  TestAutofillClock test_clock_;
   MockCouponService mock_coupon_service_;
 
  private:
@@ -167,14 +167,15 @@ TEST_F(OfferNotificationBubbleControllerImplTest,
       /*eligible_instrument_ids=*/{123});
   ShowBubble(offer);
   EXPECT_TRUE(controller()->GetOfferNotificationBubbleView());
-  test_clock_.Advance(kAutofillBubbleSurviveNavigationTime - base::Seconds(1));
+  task_environment()->FastForwardBy(kAutofillBubbleSurviveNavigationTime -
+                                    base::Seconds(1));
   controller()->ShowOfferNotificationIfApplicable(
       offer, nullptr, {.notification_has_been_shown = true});
   // Ensure the bubble is still there if
   // kOfferNotificationBubbleSurviveNavigationTime hasn't been reached yet.
   EXPECT_TRUE(controller()->GetOfferNotificationBubbleView());
 
-  test_clock_.Advance(base::Seconds(2));
+  task_environment()->FastForwardBy(base::Seconds(2));
   controller()->ShowOfferNotificationIfApplicable(
       offer, nullptr, {.notification_has_been_shown = true});
   // Ensure new page does not have an active offer notification bubble.
