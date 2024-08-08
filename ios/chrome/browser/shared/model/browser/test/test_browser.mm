7214071f910562287029abdadb1609467af55417
Mark Cogan <marq@google.com>
2024-08-08 15:09:10 +0000

    [iOS] Add Browser::GetProfile()
    Adds GetProfile() that forwards to GetBrowserState().
    
    Bug: 358301001
    Change-Id: I79756f014c98d3ad629339a1fd894966dc70962d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773138
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339073}

--- ios/chrome/browser/shared/model/browser/test/test_browser.mm
+++ ios/chrome/browser/shared/model/browser/test/test_browser.mm
@@ -65,7 +65,13 @@ Browser::Type TestBrowser::type() const {
   return type_;
 }
 
+// TODO(crbug.com/358301380): After all usage has changed to GetProfile(),
+// remove this method.
 ChromeBrowserState* TestBrowser::GetBrowserState() {
+  return GetProfile();
+}
+
+ChromeBrowserState* TestBrowser::GetProfile() {
   return browser_state_;
 }
 
