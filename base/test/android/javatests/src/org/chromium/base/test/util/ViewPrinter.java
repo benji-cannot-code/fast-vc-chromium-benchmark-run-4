db38f939afcfd65cde442b2efd5c99107013de14
Henrique Nakashima <hnakashima@chromium.org>
2024-08-08 21:20:52 +0000

    [Android] More informative DisplayedCondition status messages
    Match a View more eagerly (without filtering by effective visibility or
    displayed %) and provide more information about the matched View when
    these criteria are not met.
    
    Reduce the Condition description.
    
    Before:
    [2] [ENTER ] [OK  ] View: (view.getId() is <2130773057/org.chromium.chrome.tests:id/home_button> and (getGlobalVisibleRect() > 90%)) {fulfilled after 19~506 ms}
                 19ms (  1x): NO   | NoMatchingViewException
                506ms (  1x): OK   |
    
    After:
    [2] [ENTER ] [OK  ] View: view.getId() is <2130773057/org.chromium.chrome.tests:id/home_button> (>= 90% displayed, enabled) {fulfilled after 2~534 ms}
                  2ms (  1x): NO   | @id/home_button | HomeButton | [l 0, t 72, w 144, h 168]; visibility of ancestor [~ @id/toolbar | ToolbarPhone] = INVISIBLE; 100% displayed
                534ms (  1x): OK   | @id/home_button | HomeButton | [l 0, t 72, w 144, h 168]; 100% displayed
    
    Bug: 345492572
    Change-Id: Ia4f682e445f2c3628a20ae18c914d2cd27f0f9bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765740
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339303}

--- base/test/android/javatests/src/org/chromium/base/test/util/ViewPrinter.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/ViewPrinter.java
@@ -27,15 +27,21 @@ public class ViewPrinter {
 
     /** Options to customize rendering and printing. */
     public static class Options {
-        private static final Options DEFAULT = new Options();
+        public static final Options DEFAULT = new Options();
 
         private String mLogTag = "ViewPrinter";
+        private boolean mPrintChildren = true;
         private boolean mPrintNonVisibleViews;
         private boolean mPrintResourcePackage;
         public boolean mPrintViewBounds;
 
         public Options setLogTag(String logTag) {
             mLogTag = logTag;
+            return this;
+        }
+
+        public Options setPrintChildren(boolean printChildren) {
+            mPrintChildren = printChildren;
             return this;
         }
 
@@ -176,6 +182,10 @@ public class ViewPrinter {
             stringBuilder.append(", h ");
             stringBuilder.append(rootView.getHeight());
             stringBuilder.append("]");
+        }
+
+        if (!options.mPrintChildren) {
+            return new TreeOutput(stringBuilder.toString());
         }
 
         stringBuilder.append('\n');
