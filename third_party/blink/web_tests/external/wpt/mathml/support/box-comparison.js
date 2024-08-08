a5df7132c3ec425a55cd0b5413e0efcffe9c0314
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-08-08 01:16:15 +0000

    Import wpt@ae857199938a096a3207904679b7bc106acfa9cb
    https://github.com/web-platform-tests/wpt/compare/6a8ddd681...ae8571999
    
    Using wpt-import in Chromium 1e76b4ac3e83092e5fa5d8f49d01487742bf181e.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    igrigorik@chromium.org, yoavweiss@chromium.org:
      external/wpt/preload
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ic74ab6ddf87d1d8c5e5faf7349b2fdfaab4b30ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5770298
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1338821}

--- third_party/blink/web_tests/external/wpt/mathml/support/box-comparison.js
+++ third_party/blink/web_tests/external/wpt/mathml/support/box-comparison.js
@@ -24,8 +24,8 @@ function compareSpaceWithAndWithoutStyle(tag, style, parentStyle, direction) {
     if (!direction)
       direction = "ltr";
     document.body.insertAdjacentHTML("beforeend", `<div style="position: absolute;">\
-<div style="display: inline-block"><math><mrow dir="${direction}">${MathMLFragments[tag]}</mrow></math></div>\
-<div style="display: inline-block"><math><mrow dir="${direction}">${MathMLFragments[tag]}</mrow></math></div>\
+<div style="display: inline-block"><math display="block"><mrow dir="${direction}">${MathMLFragments[tag]}</mrow></math></div>\
+<div style="display: inline-block"><math display="block"><mrow dir="${direction}">${MathMLFragments[tag]}</mrow></math></div>\
 </div>`);
     var div = document.body.lastElementChild;
 
@@ -37,6 +37,7 @@ function compareSpaceWithAndWithoutStyle(tag, style, parentStyle, direction) {
     var styleElement = FragmentHelper.element(styleMath);
     styleElement.setAttribute("style", style);
     var styleChild = FragmentHelper.forceNonEmptyElement(styleElement);
+    FragmentHelper.forceNonEmptyDescendants(styleElement);
     var styleMathBox = styleMath.getBoundingClientRect();
     var styleElementBox = styleElement.getBoundingClientRect();
     var styleChildBox = styleChild.getBoundingClientRect();
@@ -46,6 +47,7 @@ function compareSpaceWithAndWithoutStyle(tag, style, parentStyle, direction) {
     var noStyleMath = noStyleDiv.firstElementChild;
     var noStyleElement = FragmentHelper.element(noStyleMath);
     var noStyleChild = FragmentHelper.forceNonEmptyElement(noStyleElement);
+    FragmentHelper.forceNonEmptyDescendants(noStyleElement);
     var noStyleMathBox = noStyleMath.getBoundingClientRect();
     var noStyleElementBox = noStyleElement.getBoundingClientRect();
     var noStyleChildBox = noStyleChild.getBoundingClientRect();
