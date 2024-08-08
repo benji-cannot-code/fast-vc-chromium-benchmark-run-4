25fba3c4fe04bda221054c8d2d4ba433606f451f
Brandon Wylie <wylieb@chromium.org>
2024-08-08 20:16:23 +0000

    [declutter] Fix NPE in QuickDeleteController
    This field is nullable, but accessed without an if statement. The field
    is unnecessary, and is left over after responding to a CL suggestion.
    
    Follow-up from crrev.com/c/5742192
    
    Bug: 357721132
    Change-Id: I4eeb4e5fb457d619a90ed3e85b89dd2f8abc1ba0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5770656
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339257}

--- chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteController.java
+++ chrome/browser/quick_delete/android/java/src/org/chromium/chrome/browser/quick_delete/QuickDeleteController.java
@@ -52,8 +52,6 @@ public class QuickDeleteController {
     private final @NonNull LayoutManager mLayoutManager;
     private final @NonNull Profile mProfile;
     private final @NonNull TabModel mTabModel;
-    // Null when declutter is disabled.
-    private final @Nullable TabModel mArchivedTabModel;
     private final QuickDeleteBridge mQuickDeleteBridge;
     private final QuickDeleteMediator mQuickDeleteMediator;
     private final PropertyModel mPropertyModel;
@@ -87,7 +85,6 @@ public class QuickDeleteController {
         mLayoutManager = layoutManager;
 
         mTabModel = tabModelSelector.getModel(/* incognito= */ false);
-        mArchivedTabModel = archivedTabModelSelector.getModel(/* incognito= */ false);
         mDeleteRegularTabsFilter =
                 new QuickDeleteTabsFilter(
                         (TabGroupModelFilter)
