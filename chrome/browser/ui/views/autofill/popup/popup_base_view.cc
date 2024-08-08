0ddf35adc0e904ea0c3385717a099dc8f8c377ce
Dmitry Vykochko <vykochko@google.com>
2024-08-08 08:03:07 +0000

    Rename PopupBaseView::GetOptionalPosition..() to GetOptimalPosition..().
    Fixing a typo, the position is "optimal" by its semantic, not
    "optional".
    
    Bug: 324242001
    Change-Id: I30262f5655f54a44dfa89bf5ff8c2c1eb964c64c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769177
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338916}

--- chrome/browser/ui/views/autofill/popup/popup_base_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_base_view.cc
@@ -455,7 +455,7 @@ gfx::Rect PopupBaseView::GetTopWindowBounds() const {
   return gfx::Rect();
 }
 
-gfx::Rect PopupBaseView::GetOptionalPositionAndPlaceArrowOnPopup(
+gfx::Rect PopupBaseView::GetOptimalPositionAndPlaceArrowOnPopup(
     const gfx::Rect& element_bounds,
     const gfx::Rect& max_bounds_for_popup,
     const gfx::Size& preferred_size,
@@ -542,7 +542,7 @@ bool PopupBaseView::DoUpdateBoundsAndRedrawPopup() {
     return false;
   }
 
-  gfx::Rect popup_bounds = GetOptionalPositionAndPlaceArrowOnPopup(
+  gfx::Rect popup_bounds = GetOptimalPositionAndPlaceArrowOnPopup(
       element_bounds, max_bounds_for_popup, preferred_size,
       kDefaultPreferredPopupSides);
 
