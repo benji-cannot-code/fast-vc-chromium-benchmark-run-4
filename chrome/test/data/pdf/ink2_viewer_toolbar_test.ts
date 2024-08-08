14768b6c46fd6e6d37d2a22b87c26fe9887b7a70
Andy Phan <andyphan@chromium.org>
2024-08-08 19:04:20 +0000

    Move PDF test mock plugin setup to global scope
    There are quite a few tests in ink2_viewer_toolbar_test.ts that need to
    use a mock plugin. Rather than getting the PluginController and creating
    a new mock plugin each time, just move the controller variable and mock
    plugin variable to the global scope and re-use them across tests. Clear
    plugin messages at the start of every test that uses the mock plugin.
    
    Change-Id: Ia31520c4b2d64cdda9fe4be78f6b8644dc55eb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767413
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339213}

--- chrome/test/data/pdf/ink2_viewer_toolbar_test.ts
+++ chrome/test/data/pdf/ink2_viewer_toolbar_test.ts
@@ -12,6 +12,9 @@ import {assertCheckboxMenuButton, createMockPdfPluginForTest, enterFullscreenWit
 
 const viewer = document.body.querySelector('pdf-viewer')!;
 const viewerToolbar = viewer.$.toolbar;
+const controller = PluginController.getInstance();
+const mockPlugin = createMockPdfPluginForTest();
+controller.setPluginForTesting(mockPlugin);
 
 function getUndoRedoModifier() {
   return isMac ? 'meta' : 'ctrl';
@@ -73,10 +76,6 @@ chrome.test.runTests([
   async function testToggleAnnotationModeSendsMessage() {
     chrome.test.assertFalse(viewerToolbar.annotationMode);
 
-    const controller = PluginController.getInstance();
-    const mockPlugin = createMockPdfPluginForTest();
-    controller.setPluginForTesting(mockPlugin);
-
     viewerToolbar.toggleAnnotation();
     chrome.test.assertTrue(viewerToolbar.annotationMode);
 
@@ -125,9 +124,7 @@ chrome.test.runTests([
   },
   // Test the behavior of the undo and redo buttons.
   function testUndoRedo() {
-    const controller = PluginController.getInstance();
-    const mockPlugin = createMockPdfPluginForTest();
-    controller.setPluginForTesting(mockPlugin);
+    mockPlugin.clearMessages();
 
     const undoButton =
         getRequiredElement<HTMLButtonElement>(viewerToolbar, '#undo');
@@ -185,16 +182,14 @@ chrome.test.runTests([
   // Test that the undo and redo buttons are disabled when a text form field is
   // focused.
   function testUndoRedoButtonsDisabledOnFormFieldFocus() {
+    mockPlugin.clearMessages();
+
     chrome.test.assertTrue(viewerToolbar.annotationMode);
 
     // Exit annotation mode, since form fields can only be focused outside of
     // annotation mode.
     viewerToolbar.toggleAnnotation();
     chrome.test.assertFalse(viewerToolbar.annotationMode);
-
-    const controller = PluginController.getInstance();
-    const mockPlugin = createMockPdfPluginForTest();
-    controller.setPluginForTesting(mockPlugin);
 
     const undoButton =
         getRequiredElement<HTMLButtonElement>(viewerToolbar, '#undo');
@@ -240,15 +235,13 @@ chrome.test.runTests([
   },
   // Test the behavior of the undo redo keyboard shortcuts.
   function testUndoRedoKeyboardShortcuts() {
+    mockPlugin.clearMessages();
+
     chrome.test.assertFalse(viewerToolbar.annotationMode);
 
     // Enable annotation mode.
     viewerToolbar.toggleAnnotation();
     chrome.test.assertTrue(viewerToolbar.annotationMode);
-
-    const controller = PluginController.getInstance();
-    const mockPlugin = createMockPdfPluginForTest();
-    controller.setPluginForTesting(mockPlugin);
 
     finishInkStroke(controller);
 
@@ -270,11 +263,9 @@ chrome.test.runTests([
   // Test that the undo and redo keyboard shortcuts are disabled when a text
   // form field is focused.
   function testUndoRedoShortcutsDisabledOnFormFieldFocus() {
-    chrome.test.assertTrue(viewerToolbar.annotationMode);
+    mockPlugin.clearMessages();
 
-    const controller = PluginController.getInstance();
-    const mockPlugin = createMockPdfPluginForTest();
-    controller.setPluginForTesting(mockPlugin);
+    chrome.test.assertTrue(viewerToolbar.annotationMode);
 
     // Draw two strokes and undo, so that both undo and redo buttons are
     // enabled.
