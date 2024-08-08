3e7f3d1349d546aa8d60bdbef0184378d729a50e
Chris Lu <thegreenfrog@chromium.org>
2024-08-08 13:53:58 +0000

    [ios] Fix MVT metrics logging
    Most MVT metrics are logged via the contentSuggestionsMetricsRecorder,
    and during a refactor, the MostVisitedTilesMediator was not passed
    the object reference.
    
    Change-Id: I13404f83735d932f971178dbd422fc5653425d3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771811
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339034}

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_coordinator.mm
@@ -266,6 +266,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
        URLLoadingBrowserAgent:UrlLoadingBrowserAgent::FromBrowser(
                                   self.browser)];
   _mostVisitedTilesMediator.contentSuggestionsDelegate = self.delegate;
+  _mostVisitedTilesMediator.contentSuggestionsMetricsRecorder =
+      self.contentSuggestionsMetricsRecorder;
   _mostVisitedTilesMediator.actionFactory = [[BrowserActionFactory alloc]
       initWithBrowser:self.browser
              scenario:kMenuScenarioHistogramMostVisitedEntry];
