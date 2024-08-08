7254b3175619202f9fd42c35217e328a21385021
Jérôme Lebel <jlebel@chromium.org>
2024-08-08 15:36:40 +0000

    [iOS] Dismiss automatically resignin infobar when sign-in
    ReSignInInfoBarDelegate needs to listen for sign-in event. As soon as
    the user is signed in, the re-sign-in info bar needs to be removed.
    
    The scenario to reproduce this crash: while the resignin infobar is
    displayed, do a sign-in quickly, and then tap on "Verify" in
    the infobar.
    
    Fixed: 345446209
    Change-Id: I98a7f89766ae1ca6e8f001c035f4db610ab348a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757209
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339082}

--- ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
+++ ios/chrome/browser/ui/authentication/re_signin_infobar_delegate.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // static
 std::unique_ptr<ReSignInInfoBarDelegate> ReSignInInfoBarDelegate::Create(
     AuthenticationService* authentication_service,
+    signin::IdentityManager* identity_manager,
     AppState* app_state,
     id<SigninPresenter> signin_presenter) {
   if (app_state.initStage != InitStageFinal) {
@@ -62,15 +63,17 @@ std::unique_ptr<ReSignInInfoBarDelegate> ReSignInInfoBarDelegate::Create(
 
   signin_metrics::RecordSigninImpressionUserActionForAccessPoint(
       signin_metrics::AccessPoint::ACCESS_POINT_RESIGNIN_INFOBAR);
-  return base::WrapUnique(
-      new ReSignInInfoBarDelegate(authentication_service, signin_presenter));
+  return base::WrapUnique(new ReSignInInfoBarDelegate(
+      authentication_service, identity_manager, signin_presenter));
 }
 
 ReSignInInfoBarDelegate::ReSignInInfoBarDelegate(
     AuthenticationService* authentication_service,
+    signin::IdentityManager* identity_manager,
     id<SigninPresenter> signin_presenter)
     : authentication_service_(authentication_service),
       signin_presenter_(signin_presenter) {
+  identity_manager_observer_.Observe(identity_manager);
   CHECK(authentication_service_);
   CHECK(signin_presenter_);
 }
@@ -123,10 +126,25 @@ bool ReSignInInfoBarDelegate::Accept() {
 
   // Stop displaying the infobar once user interacted with it.
   authentication_service_->ResetReauthPromptForSignInAndSync();
+  identity_manager_observer_.Reset();
   return true;
 }
 
 void ReSignInInfoBarDelegate::InfoBarDismissed() {
   // Stop displaying the infobar once user interacted with it.
   authentication_service_->ResetReauthPromptForSignInAndSync();
+  identity_manager_observer_.Reset();
+}
+
+void ReSignInInfoBarDelegate::OnPrimaryAccountChanged(
+    const signin::PrimaryAccountChangeEvent& event) {
+  switch (event.GetEventTypeFor(signin::ConsentLevel::kSignin)) {
+    case signin::PrimaryAccountChangeEvent::Type::kSet:
+      identity_manager_observer_.Reset();
+      infobar()->RemoveSelf();
+      break;
+    case signin::PrimaryAccountChangeEvent::Type::kCleared:
+    case signin::PrimaryAccountChangeEvent::Type::kNone:
+      break;
+  }
 }
