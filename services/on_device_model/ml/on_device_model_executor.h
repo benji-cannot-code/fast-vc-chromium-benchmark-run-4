33eeda049aa607af1416116da2c21da04a9a8242
Steven Holte <holte@google.com>
2024-08-08 18:58:09 +0000

    Revert dependent CLs for "Use the separated ChromeMLTSAPI."
    * Revert "Call the right ClassifyTextSafety."
     - https://chromium-review.googlesource.com/c/chromium/src/+/5770532
    * Revert "Remove pre-session API."
     - https://chromium-review.googlesource.com/c/chromium/src/+/5766474
    * Revert "Use the separated ChromeMLTSAPI."
     - https://chromium-review.googlesource.com/c/chromium/src/+/5767092
    
    Reason for revert: Crashes observed
    
    Bug: 356932149
    Change-Id: Ieab6b44000203dd7502efe326f8ef31d1dcc9357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773733
    Reviewed-by: Clark DuVall <cduvall@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339211}

--- services/on_device_model/ml/on_device_model_executor.h
+++ services/on_device_model/ml/on_device_model_executor.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/remote.h"
 #include "services/on_device_model/ml/chrome_ml.h"
 #include "services/on_device_model/ml/session_accessor.h"
-#include "services/on_device_model/ml/ts_model.h"
 #include "services/on_device_model/public/cpp/model_assets.h"
 #include "services/on_device_model/public/cpp/on_device_model.h"
 #include "services/on_device_model/public/mojom/on_device_model.mojom.h"
@@ -64,8 +63,9 @@ class OnDeviceModelExecutor : public on_device_model::OnDeviceModel {
 
   const raw_ref<const ChromeML> chrome_ml_;
 
+  base::MemoryMappedFile ts_data_;
+  base::MemoryMappedFile ts_sp_model_;
   scoped_refptr<LanguageDetector> language_detector_;
-  std::unique_ptr<TsModel> ts_model_;
 
   // TODO(b/323572952): Allow disposing of adaptation weights.
   std::vector<std::unique_ptr<base::MemoryMappedFile>> adaptation_data_;
