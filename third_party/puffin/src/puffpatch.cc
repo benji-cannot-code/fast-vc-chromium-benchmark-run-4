d3e259a0742d4c889a6bb32435e347fb879a9cf6
Daniel Cheng <dcheng@chromium.org>
2024-08-08 04:16:12 +0000

    Migrate make_span<N>(...) to to_fixed_extent<N>() in puffin
    Bug: 341907909
    Change-Id: I203c09a3f4b643cd891a455d65393bf1c078c31a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767467
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1338866}

--- third_party/puffin/src/puffpatch.cc
+++ third_party/puffin/src/puffpatch.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/numerics/byte_conversions.h"
 #include "zucchini/patch_reader.h"
 #include "zucchini/zucchini.h"
@@ -76,8 +77,8 @@ Status DecodePatch(const uint8_t* patch,
       // TODO(crbug.com/40284755): This span construction is unsound as we can't
       // know the length of the patch allocation. The function should be
       // accepting a span instead of a pointer.
-      UNSAFE_BUFFERS(base::make_span<sizeof(header_size)>(
-          patch + offset, sizeof(header_size))));
+      UNSAFE_BUFFERS(*base::span(patch + offset, sizeof(header_size))
+                          .to_fixed_extent<sizeof(header_size)>()));
   offset += sizeof(header_size);
   TEST_AND_RETURN_VALUE(header_size <= (patch_length - offset),
                         Status::P_BAD_PUFFIN_HEADER);
