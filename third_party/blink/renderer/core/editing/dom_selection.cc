ff03cdaec852df871e6ff1082be3cfbbee43f2d1
Di Zhang <dizhangg@chromium.org>
2024-08-08 02:15:40 +0000

    [Selection API] Change getComposedRanges() from rest parameter to a dictionary
    Per https://github.com/w3c/selection-api/issues/176, we should update
    selection.getComposedRanges() from using a rest `shadowRoots` parameter
    to using a dictionary that contains the array `shadowRoots`.
    
    Change-Id: I6b5b3fed786cf6d75fa4a20c0b2a583635cd2aca
    Bug: 40286116
    Fixed: 355577223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5770701
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1338832}

--- third_party/blink/renderer/core/editing/dom_selection.cc
+++ third_party/blink/renderer/core/editing/dom_selection.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/editing/dom_selection.h"
 
+#include "third_party/blink/renderer/bindings/core/v8/v8_get_composed_ranges_options.h"
 #include "third_party/blink/renderer/core/dom/document.h"
 #include "third_party/blink/renderer/core/dom/node.h"
 #include "third_party/blink/renderer/core/dom/range.h"
@@ -588,7 +589,7 @@ Range* DOMSelection::getRangeAt(unsigned index,
 
 // https://www.w3.org/TR/selection-api/#dom-selection-getcomposedranges
 const StaticRangeVector DOMSelection::getComposedRanges(
-    const HeapVector<Member<ShadowRoot>>& shadowRoots) const {
+    const GetComposedRangesOptions* options) const {
   StaticRangeVector ranges;
   // 1. If this is empty, return an empty array.
   if (!IsAvailable()) {
@@ -604,14 +605,14 @@ const StaticRangeVector DOMSelection::getComposedRanges(
   Node* startNode = range->composedStartContainer();
   unsigned startOffset = range->composedStartOffset();
   // 3. Rescope startNode and startOffset with listed shadow roots.
-  Rescope(startNode, startOffset, shadowRoots, /*isEnd=*/false);
+  Rescope(startNode, startOffset, options->shadowRoots(), /*isEnd=*/false);
 
   // 4. Let endNode be end node of the range associated with this, and let
   // endOffset be end offset of the range.
   Node* endNode = range->composedEndContainer();
   unsigned endOffset = range->composedEndOffset();
   // 5. Rescope endNode and endOffset with listed shadow roots.
-  Rescope(endNode, endOffset, shadowRoots, /*isEnd=*/true);
+  Rescope(endNode, endOffset, options->shadowRoots(), /*isEnd=*/true);
 
   // 6. Return an array consisting of new StaticRange whose start node is
   // startNode, start offset is startOffset, end node is endNode, and end
--- third_party/blink/renderer/core/editing/dom_selection.h
+++ third_party/blink/renderer/core/editing/dom_selection.h
@@ -46,6 +46,7 @@ namespace blink {
 
 class ExceptionState;
 class FrameSelection;
+class GetComposedRangesOptions;
 class Node;
 class Range;
 class SetSelectionOptions;
@@ -107,8 +108,9 @@ class CORE_EXPORT DOMSelection final : public ScriptWrappable,
 
   // Selection API across shadow DOM
   String direction() const;
+
   const StaticRangeVector getComposedRanges(
-      const HeapVector<Member<ShadowRoot>>&) const;
+      const GetComposedRangesOptions*) const;
 
   void Trace(Visitor*) const override;
 
