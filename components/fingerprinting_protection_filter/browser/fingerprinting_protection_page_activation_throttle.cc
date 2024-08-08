b5153cc971500014c85cbdf4b5381a537dcb9d70
Zainab Rizvi <rizvis@google.com>
2024-08-08 20:39:59 +0000

    [FPF] Consolidates FPF checks & fixes activation bug
    Adds an Incognito flag and pipes `Profile::IsIncognitoProfile` through
    to web contents where it is used.
    
    Fixes a bug where FPF was only working with UX flags enabled.
    
    After this change, there are three types of FPF checks:
    1. `TrackingProtectionSettings::IsFingerprintingProtectionEnabled()`
    - checks UX flags only
    2. `features::IsFingerprintingProtectionFeatureEnabled())`
    - checks feature flags only
    3. `IsFingerprintingProtectionEnabledInIncognito()`
    and `IsFingerprintingProtectionEnabledInNonIncognito`
    for explicitly checking if a profile is Incognito and
    the correct flag is enabled. Combined together in
    `IsFingerprintingProtectionEnabledForIncognitoState`
    
    Bug: 354698651
    Change-Id: I2b74d3ce9f1a5ce34b7e9bf56f4ec8de73d23a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5762940
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339271}

--- components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
+++ components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.cc
@@ -59,8 +59,7 @@ FingerprintingProtectionPageActivationThrottle::GetNameForLogging() {
 
 ActivationDecision
 FingerprintingProtectionPageActivationThrottle::GetActivationDecision() const {
-  if (!base::FeatureList::IsEnabled(
-          features::kEnableFingerprintingProtectionFilter)) {
+  if (!features::IsFingerprintingProtectionFeatureEnabled()) {
     return ActivationDecision::UNKNOWN;
   }
   if (fingerprinting_protection_filter::features::kActivationLevel.Get() ==
@@ -77,7 +76,8 @@ void FingerprintingProtectionPageActivationThrottle::NotifyResult(
   if (decision == ActivationDecision::UNKNOWN) {
     return;
   }
-  ActivationLevel activation_level = features::kActivationLevel.Get();
+  ActivationLevel activation_level =
+      fingerprinting_protection_filter::features::kActivationLevel.Get();
   if (profile_interaction_manager_.get()) {
     activation_level = profile_interaction_manager_->OnPageActivationComputed(
         navigation_handle(), activation_level, &decision);
