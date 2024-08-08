f583a3d0846e1d6d6f90a07b2154615a6f78eefe
Niko Tsirakis <ntsirakis@google.com>
2024-08-08 00:37:37 +0000

    [artemis] Refactor batch logic
    The original logic was not sufficient for the case
    where we are processing a large amount of logs from
    numerous sources.
    
    This commit adds a new pending payloads queue to add
    separation between payloads that are ready for upload
    and the "active" payload, which will be modified upon
    receiving new data. Since both the data acquisition
    and the enqueue responses occur asynchronously, it was
    possible to get in a state where the active payload
    was being written to while we were in the middle of
    an enqueue attempt. Example:
    
    - Fetch src 1, schedule callback
    - Fetch src 2, schedule callback
    - Callback for src 1 fires, and we add data to the payload.
      Payload is full and we call enqueue, which schedules an
      enqueue callback.
    - Callback for src 2 fires, and we add more data. Payload
      is still full, so we call enqueue again, with another
      scheduled callback (bad)
    - Enqueue callback for src 1 fires, and it succeeded. We
      wipe the payload, which includes the data from src 2.
    - Enqueue callback for src 2 fires, and it failed. We
      reattempt the enqueue with the active payload, but
      all of the data from src 2 has already been wiped!
    
    Bug: b:353994195
    Change-Id: Iea3f2a8524684e1f0d3c40faf1f1e7c764ce4247
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758313
    Reviewed-by: Kyle Williams <kdgwill@chromium.org>
    Commit-Queue: Niko Tsirakis <ntsirakis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1338810}

--- chrome/browser/ash/chromebox_for_meetings/artemis/data_aggregator_service.h
+++ chrome/browser/ash/chromebox_for_meetings/artemis/data_aggregator_service.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_CHROMEBOX_FOR_MEETINGS_ARTEMIS_DATA_AGGREGATOR_SERVICE_H_
 #define CHROME_BROWSER_ASH_CHROMEBOX_FOR_MEETINGS_ARTEMIS_DATA_AGGREGATOR_SERVICE_H_
 
+#include <queue>
+
 #include "base/time/time.h"
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/command_source.h"
 #include "chrome/browser/ash/chromebox_for_meetings/artemis/log_source.h"
@@ -95,7 +97,8 @@ class DataAggregatorService : public CfmObserver,
       const std::string& source_name,
       const std::vector<std::string>& serialized_entries);
   bool IsPayloadReadyForUpload() const;
-  void EnqueueTransportPayload();
+  void AddActivePayloadToPendingQueue();
+  void EnqueueNextPendingTransportPayload();
   void HandleEnqueueResponse(chromeos::cfm::mojom::LoggerStatusPtr status);
 
   chromeos::cfm::ServiceAdaptor service_adaptor_;
@@ -118,6 +121,11 @@ class DataAggregatorService : public CfmObserver,
   // CfmLogger. This will collect data until certain conditions are met
   // (see IsPayloadReadyForUpload() method for details).
   proto::TransportPayload active_transport_payload_;
+
+  // A queue of currently pending transport payloads that are waiting
+  // to be enqueued. Payloads are only popped off the queue if they
+  // are uploaded successfully, or if the queue grows too large.
+  std::queue<proto::TransportPayload> pending_transport_payloads_;
 
   // Used to track the time since we last pushed a payload to the wire.
   // Will be used as a timeout of sorts for the next push.
