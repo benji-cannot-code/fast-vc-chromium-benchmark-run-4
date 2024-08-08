82e4dfd93af151fcd2dde8bc95a0ef80b0796597
Avi Drissman <avi@chromium.org>
2024-08-08 20:21:10 +0000

    Remove OSX.NSException
    This is expired and not particularly useful anyway.
    
    Bug: none
    Change-Id: I32f7c61a878558aaaea182e135bd512398ec8936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5774163
    Reviewed-by: Leonard Grey <lgrey@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Leonard Grey <lgrey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339261}

--- chrome/browser/mac/exception_processor.h
+++ chrome/browser/mac/exception_processor.h
@@ -12,9 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chrome {
 
-// Installs the Objective-C exception preprocessor. This records UMA and crash
-// keys for NSException objects. The preprocessor will also make fatal any
-// exception that is not handled.
+// Installs the Objective-C exception preprocessor. This records crash keys for
+// NSException objects. The preprocessor will also make fatal any exception that
+// is not handled.
 void InstallObjcExceptionPreprocessor();
 
 // The items below are exposed only for testing.
@@ -22,16 +22,6 @@ void InstallObjcExceptionPreprocessor();
 
 // Removes the exception preprocessor if it is installed.
 void UninstallObjcExceptionPreprocessor();
-
-// Bin for unknown exceptions.
-extern const size_t kUnknownNSException;
-
-// Returns the histogram bin for |exception| if it is one we track
-// specifically, or |kUnknownNSException| if unknown.
-size_t BinForException(NSException* exception);
-
-// Use UMA to track exception occurance.
-void RecordExceptionWithUma(NSException* exception);
 
 }  // namespace chrome
 
