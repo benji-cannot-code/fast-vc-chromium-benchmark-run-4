a64091afbcdf881d5340fb52fd1e59e3fa792127
Kent Tamura <tkent@chromium.org>
2024-08-08 16:33:27 +0000

    WTF: Add a Vector template argument for iterator configuration
    Since this CL, template arguments of WTF::Vector are:
      * typename T
      * wtf_size_t inlineCapacity
      * typename Allocator
      * bool checked_iter
    
    The default of the fourth argument is `false` for now.  It indicates
    that the Vector uses UncheckedIterator as ever.
    The default will be changed to `true` later.  It indicates
    base::CheckedContiguousIterator.
    
    HeapVector also has an additional checked_iter argument.
    
    This CL has no behavior changes.
    
    Binary-Size: Increase should be temporary.
    Bug: 355003172
    Change-Id: Ib28982dcb23f42530b9d4de81fa60303280bb08c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768295
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339110}

--- third_party/blink/renderer/platform/heap/collection_support/heap_vector.h
+++ third_party/blink/renderer/platform/heap/collection_support/heap_vector.h
@@ -18,12 +18,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-template <typename T, wtf_size_t inlineCapacity = 0>
-class HeapVector final : public GarbageCollected<HeapVector<T, inlineCapacity>>,
-                         public Vector<T, inlineCapacity, HeapAllocator> {
+// TODO(crbug.com/355003172): The default value of checked_iter should be true.
+template <typename T, wtf_size_t inlineCapacity = 0, bool checked_iter = false>
+class HeapVector final
+    : public GarbageCollected<HeapVector<T, inlineCapacity, checked_iter>>,
+      public Vector<T, inlineCapacity, HeapAllocator, checked_iter> {
   DISALLOW_NEW();
 
-  using BaseVector = Vector<T, inlineCapacity, HeapAllocator>;
+  using BaseVector = Vector<T, inlineCapacity, HeapAllocator, checked_iter>;
 
  public:
   HeapVector() = default;
@@ -33,7 +35,7 @@ class HeapVector final : public GarbageCollected<HeapVector<T, inlineCapacity>>,
   HeapVector(wtf_size_t size, const T& val) : BaseVector(size, val) {}
 
   template <wtf_size_t otherCapacity>
-  HeapVector(const HeapVector<T, otherCapacity>& other)  // NOLINT
+  HeapVector(const HeapVector<T, otherCapacity, checked_iter>& other)  // NOLINT
       : BaseVector(other) {}
 
   HeapVector(const HeapVector& other)
@@ -52,7 +54,7 @@ class HeapVector final : public GarbageCollected<HeapVector<T, inlineCapacity>>,
       typename Proj,
       typename = std::enable_if_t<
           std::is_invocable_v<Proj, typename BaseVector::const_reference>>>
-  HeapVector(const HeapVector<U, otherSize>& other, Proj proj)
+  HeapVector(const HeapVector<U, otherSize, checked_iter>& other, Proj proj)
       : BaseVector(static_cast<const BaseVector&>(other), std::move(proj)) {}
 
   template <typename Collection,
@@ -100,8 +102,9 @@ class HeapVector final : public GarbageCollected<HeapVector<T, inlineCapacity>>,
 template <typename T>
 concept IsHeapVector = requires { typename HeapVector<T>; };
 
-template <typename T, wtf_size_t inlineCapacity>
-constexpr HeapVector<T, inlineCapacity>::TypeConstraints::TypeConstraints() {
+template <typename T, wtf_size_t inlineCapacity, bool checked_iter>
+constexpr HeapVector<T, inlineCapacity, checked_iter>::TypeConstraints::
+    TypeConstraints() {
   static_assert(std::is_trivially_destructible_v<HeapVector> || inlineCapacity,
                 "HeapVector must be trivially destructible.");
   static_assert(!WTF::IsWeak<T>::value,
