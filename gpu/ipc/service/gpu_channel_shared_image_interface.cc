adfd1261d7f8c48d9e007a2ed23233d22cb3e070
Saifuddin Hitawala <hitawala@chromium.org>
2024-08-08 18:36:54 +0000

    [media] Use SharedImageFormat in FuchsiaVideoDecoder
    Use SharedImageFormat instead of BufferFormat in FuchsiaVideoDecoder
    when creating shared images.
    
    Also update the RegisterSysmemBufferCollection api to take in
    SharedImageFormat instead of BufferFormat.
    
    Bug: 356649879
    Change-Id: I15280f827a740233f0f7c7ff9ab4234ccb67d908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766973
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339202}

--- gpu/ipc/service/gpu_channel_shared_image_interface.cc
+++ gpu/ipc/service/gpu_channel_shared_image_interface.cc
@@ -503,7 +503,7 @@ void GpuChannelSharedImageInterface::PresentSwapChain(
 void GpuChannelSharedImageInterface::RegisterSysmemBufferCollection(
     zx::eventpair service_handle,
     zx::channel sysmem_token,
-    gfx::BufferFormat format,
+    const viz::SharedImageFormat& format,
     gfx::BufferUsage usage,
     bool register_with_image_pipe) {
   NOTREACHED_IN_MIGRATION();
