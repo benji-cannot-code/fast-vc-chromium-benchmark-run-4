4f70db69654ef87afafc9272920e603cfffee7ad
Henrique Nakashima <hnakashima@chromium.org>
2024-08-09 22:26:21 +0000

    [Android] Remove workarounds for toolbar appearing under tab switcher
    Now that the ANDROID_HUB is launched, the buttons don't appear anymore
    in the tab switcher.
    
    Also create descendant() factory methods for ViewSpec to simplify
    declarations of ViewSpecs with isDescendantOfA().
    
    Bug: 41497463
    Change-Id: Ic17be0a350cea70cbd9c7195c62980c3b2f339c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777727
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339914}

--- base/test/android/javatests/src/org/chromium/base/test/transit/ViewSpec.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/ViewSpec.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.transit;
 
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -23,6 +24,8 @@ import org.hamcrest.StringDescription;
 
 import org.chromium.base.test.util.ViewPrinter;
 
+import java.util.Arrays;
+
 /** A spec to generate ViewElements representing a view characteristic of a ConditionalState. */
 public class ViewSpec {
 
@@ -38,6 +41,19 @@ public class ViewSpec {
     @SafeVarargs
     public static ViewSpec viewSpec(Matcher<View>... viewMatchers) {
         return new ViewSpec(allOf(viewMatchers));
+    }
+
+    /** Create a ViewSpec for a descendant of this ViewSpec. */
+    public final ViewSpec descendant(Matcher<View> viewMatcher) {
+        return viewSpec(viewMatcher, isDescendantOfA(mViewMatcher));
+    }
+
+    /** Create a ViewSpec for a descendant of this ViewSpec that matches multiple Matchers<View>. */
+    @SafeVarargs
+    public final ViewSpec descendant(Matcher<View>... viewMatchers) {
+        Matcher<View>[] allViewMatchers = Arrays.copyOf(viewMatchers, viewMatchers.length + 1);
+        allViewMatchers[viewMatchers.length] = isDescendantOfA(mViewMatcher);
+        return viewSpec(allViewMatchers);
     }
 
     private ViewSpec(Matcher<View> viewMatcher) {
