f4e6a1563658b4e5464a4a052e5013c3c66436fb
Kristi Saney <kristislee@google.com>
2024-08-09 19:13:09 +0000

    [Read Aloud] Don't open the voice menu when the language menu closes
    We already leave the voice menu open when the language menu opens, so
    opening it again draws it in the wrong spot.
    
    Change-Id: Ic92d0a90eeca2bb1bf70c40c0780475b7255a7a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775798
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Auto-Submit: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1339794}

--- chrome/browser/resources/side_panel/read_anything/voice_selection_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/voice_selection_menu.ts
@@ -223,7 +223,6 @@ export class VoiceSelectionMenuElement extends VoiceSelectionMenuElementBase {
     event.stopPropagation();
 
     this.showLanguageMenuDialog_ = false;
-    openMenu(this.$.voiceSelectionMenu.get(), this.$.voiceSelectionMenu);
   }
 
   protected onClose_() {
