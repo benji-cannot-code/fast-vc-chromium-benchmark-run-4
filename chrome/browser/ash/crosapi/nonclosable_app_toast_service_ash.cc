34db0197e200a39714b3b80a55d422212d4333d9
Eriko Kurimoto <elkurin@chromium.org>
2024-08-09 02:11:40 +0000

    ash: Create //chromeos/ash/components/nonclosable_app_ui
    Bug: b:356712294
    Change-Id: Ie31b0d294b128af5372174febeb8c5d84b1e8619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753072
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339431}

--- chrome/browser/ash/crosapi/nonclosable_app_toast_service_ash.cc
+++ chrome/browser/ash/crosapi/nonclosable_app_toast_service_ash.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "chrome/browser/ash/nonclosable_app_ui_utils.h"
+#include "chromeos/ash/components/nonclosable_app_ui/nonclosable_app_ui_utils.h"
 
 namespace crosapi {
 
--- chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
+++ chrome/browser/ui/web_applications/web_app_ui_manager_impl.cc
@@ -84,9 +84,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/shelf_model.h"
 #include "chrome/browser/ash/app_list/app_list_syncable_service.h"
 #include "chrome/browser/ash/app_list/app_list_syncable_service_factory.h"
-#include "chrome/browser/ash/nonclosable_app_ui_utils.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
+#include "chromeos/ash/components/nonclosable_app_ui/nonclosable_app_ui_utils.h"
 #include "components/sync/model/string_ordinal.h"
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
