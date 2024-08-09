48bdaa6c2a055b0410f4a8c1c06a14be7e8f351e
Noam Rosenthal <nrosenthal@chromium.org>
2024-08-09 12:21:12 +0000

    Nested view transitions: support cross-document exit animations
    Since the new state doesn't have the nested element, we have to
    rely on the previous capture. In the cross-document scenario,
    this comes from the old document and has to be serialized together
    with the ViewTransitionElement.
    
    Added a test that works with both cross-doc and same-doc as
    test variants.
    
    Bug: 347947051
    Change-Id: Ie8bb06136d9183a9251a6f5b604d925a9a227c6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766878
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339580}

--- third_party/blink/web_tests/external/wpt/css/css-view-transitions/nested/resources/compute-test.js
+++ third_party/blink/web_tests/external/wpt/css/css-view-transitions/nested/resources/compute-test.js
@@ -1,8 +1,33 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 failIfNot(document.startViewTransition, "Missing document.startViewTransition");
 
-function runTest() {
-    document.startViewTransition().ready.then(() => takeScreenshot());
+function add_rule() {
+    const style = document.createElement("style");
+    style.innerHTML = "@view-transition { navigation: auto }";
+    document.head.append(style);
 }
 
-onload = () => requestAnimationFrame(() => requestAnimationFrame(runTest));
+const mode = new URLSearchParams(location.search).get("vtmode");
+if (mode === "crossdoc") {
+    onload = () => {
+        const url = new URL(location.href);
+        url.searchParams.set("vtmode", "crossdoc-newpage");
+        location.replace(url.href);
+    };
+    add_rule();
+} else if (mode === "crossdoc-newpage") {
+    document.documentElement.classList.add("vt-new");
+    add_rule();
+    takeScreenshot();
+} else {
+    onload = async() => {
+        const transition = document.startViewTransition(() => {
+            document.documentElement.classList.add("vt-new");
+        });
+        transition.finished.then(() => {
+            document.documentElement.classList.remove("vt-new");
+        });
+        transition.ready.then(() => takeScreenshot());
+    }
+}
+
