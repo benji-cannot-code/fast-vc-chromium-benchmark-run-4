939fd3a3c1bafb8ee7bfb2839931a00937bb1c37
Jan Keitel <jkeitel@google.com>
2024-08-09 13:13:29 +0000

    Implement PlusAddressHttpClientImpl::PreallocatePlusAddresses.
    None of the code is used anywhere in production yet - wiring up
    PlusAddressPreallocator that uses it will happen later.
    
    Bug: 324559503
    Change-Id: Ic39934700ba35ee0eb71450b5f1c4f8372aea80b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769175
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339600}

--- components/plus_addresses/plus_address_test_utils.cc
+++ components/plus_addresses/plus_address_test_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/json/json_writer.h"
 #include "base/strings/string_util.h"
 #include "components/affiliations/core/browser/affiliation_utils.h"
+#include "components/plus_addresses/plus_address_types.h"
 #include "testing/gmock/include/gmock/gmock.h"
 
 namespace plus_addresses::test {
@@ -76,6 +77,21 @@ std::string MakeListResponse(const std::vector<PlusProfile>& profiles) {
   std::optional<std::string> json = base::WriteJson(response);
   DCHECK(json.has_value());
   return json.value();
+}
+
+std::string MakePreallocateResponse(
+    const std::vector<PreallocatedPlusAddress>& addresses) {
+  base::Value::List profiles;
+  for (const PreallocatedPlusAddress& address : addresses) {
+    profiles.Append(
+        base::Value::Dict()
+            .Set("emailAddress", address.plus_address)
+            .Set("reservationLifetime",
+                 base::NumberToString(address.lifetime.InSeconds()) + "s"));
+  }
+  return base::WriteJson(
+             base::Value::Dict().Set("emailAddresses", std::move(profiles)))
+      .value();
 }
 
 std::string MakePlusProfile(const PlusProfile& profile) {
