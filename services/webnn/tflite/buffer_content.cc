318316460bd53835524d9f29128b53ff17ace368
Reilly Grant <reillyg@chromium.org>
2024-08-09 21:53:26 +0000

    webnn: Initialize memory allocated for CoreML and TFLite-based MLBuffers
    base::AlignedAlloc and +[MLMultiArray alloc] do not initialize the
    memory they allocate so we need to do it ourselves. Adds a new test to
    check that the value of a newly created MLBuffer is well-defined.
    
    Fixed: 358409285
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel,mac14-blink-rel
    Change-Id: I1a0bdce492108c0ce5f340db627c6fcb20b6f054
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773800
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339893}

--- services/webnn/tflite/buffer_content.cc
+++ services/webnn/tflite/buffer_content.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/webnn/tflite/buffer_content.h"
 
 #include "base/compiler_specific.h"
+#include "base/ranges/algorithm.h"
 #include "third_party/tflite/buildflags.h"
 #include "third_party/tflite/src/tensorflow/lite/util.h"
 
@@ -31,7 +32,10 @@ size_t AddPaddingIfNecessary(size_t size) {
 BufferContent::BufferContent(size_t size)
     : buffer_(base::AlignedAlloc(AddPaddingIfNecessary(size),
                                  ::tflite::kDefaultTensorAlignment)),
-      size_(size) {}
+      size_(size) {
+  // `base::AlignedAlloc` does not return initialized memory.
+  base::ranges::fill(AsSpan(), 0);
+}
 
 BufferContent::~BufferContent() = default;
 
