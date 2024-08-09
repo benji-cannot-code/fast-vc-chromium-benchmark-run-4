e60bf827e2021dc3ae91adcfca53280a69767028
Erik Chen <erikchen@chromium.org>
2024-08-09 02:02:51 +0000

    Minor clean up.
    This CL removes an unnecessary include and adds some comments.
    
    Change-Id: I85099da7c99f60750febec7d12a6d3ea21f91c48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767354
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339429}

--- chrome/browser/ui/views/performance_controls/memory_saver_chip_view.cc
+++ chrome/browser/ui/views/performance_controls/memory_saver_chip_view.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
 #include "chrome/browser/ui/performance_controls/memory_saver_chip_tab_helper.h"
 #include "chrome/browser/ui/performance_controls/memory_saver_utils.h"
 #include "chrome/browser/ui/performance_controls/performance_controls_metrics.h"
