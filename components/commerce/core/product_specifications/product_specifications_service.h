6378b6abc014fef9a70f2b2b46151b346c58b919
David Maunder <davidjm@google.com>
2024-08-09 18:13:34 +0000

    Add title support to AddProductSpecifications API
    This CL updates AddProductSpecifications to use UrlInfo instead
    of GURL in preparation for adding title support in
    ProductSpecificationsService. The title field in UrlInfo will
    be currently ignored, but support will be added soon. This approach
    has been chosen to keep CLs tractable.
    
    Bug: 358566607
    Change-Id: I71d4f5fa4f8725d2748f53c14124ac8d0dc623d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5774586
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339737}

--- components/commerce/core/product_specifications/product_specifications_service.h
+++ components/commerce/core/product_specifications/product_specifications_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "base/task/sequenced_task_runner.h"
+#include "components/commerce/core/commerce_types.h"
 #include "components/commerce/core/product_specifications/product_specifications_set.h"
 #include "components/commerce/core/product_specifications/product_specifications_sync_bridge.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -47,10 +48,12 @@ class ProductSpecificationsService
           callback);
 
   // Add new product specifications set called |name| with product pages
-  // corresponding to |urls|.
+  // corresponding the urls in |url_infos|. Note, title support is being
+  // added to ProductSpecificationsService (crbug.com/357561655), although
+  // the title field in UrlInfo is currently unused.
   virtual const std::optional<ProductSpecificationsSet>
   AddProductSpecificationsSet(const std::string& name,
-                              const std::vector<GURL>& urls);
+                              const std::vector<UrlInfo>& url_infos);
 
   // Set the URLs for a product specifications set associated with the provided
   // Uuid. If a set with the provided Uuid exists, an updated
