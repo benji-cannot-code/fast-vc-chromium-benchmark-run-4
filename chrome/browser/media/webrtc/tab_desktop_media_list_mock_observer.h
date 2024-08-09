9fd5f3c1e7ae5238adec39821ab149f3d09f97a9
Magdalena Skarbinska <mskarbinska@google.com>
2024-08-09 13:17:12 +0000

    Separate MockObserver for TabDesktopMediaList tests
    MockObserver efinition was duplicated in both unit and browser tests
    of TabDesktopMediaList.This cl moves observer definition to separate
    files so that it will be reusable across all tests.
    
    
    move mock observer for TabMediaList to separate file
    
    Change-Id: I11e1ec08e36b41bb4a6135efc92d92adcb2f0464
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766686
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Cr-Commit-Position: refs/heads/main@{#1339604}

---
+++ chrome/browser/media/webrtc/tab_desktop_media_list_mock_observer.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_WEBRTC_TAB_DESKTOP_MEDIA_LIST_MOCK_OBSERVER_H_
+#define CHROME_BROWSER_MEDIA_WEBRTC_TAB_DESKTOP_MEDIA_LIST_MOCK_OBSERVER_H_
+
+#include <cstddef>
+
+#include "chrome/browser/media/webrtc/desktop_media_list_observer.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+class DesktopMediaListMockObserver : public DesktopMediaListObserver {
+ public:
+  DesktopMediaListMockObserver();
+  ~DesktopMediaListMockObserver() override;
+
+  MOCK_METHOD(void, OnSourceAdded, (int index), (override));
+  MOCK_METHOD(void, OnSourceRemoved, (int index), (override));
+  MOCK_METHOD(void, OnSourceMoved, (int old_index, int new_index), (override));
+  MOCK_METHOD(void, OnSourceNameChanged, (int index), (override));
+  MOCK_METHOD(void, OnSourceThumbnailChanged, (int index), (override));
+  MOCK_METHOD(void, OnSourcePreviewChanged, (size_t index), (override));
+  MOCK_METHOD(void, OnDelegatedSourceListSelection, (), (override));
+  MOCK_METHOD(void, OnDelegatedSourceListDismissed, (), (override));
+
+  void VerifyAndClearExpectations();
+};
+
+#endif  // CHROME_BROWSER_MEDIA_WEBRTC_TAB_DESKTOP_MEDIA_LIST_MOCK_OBSERVER_H_
