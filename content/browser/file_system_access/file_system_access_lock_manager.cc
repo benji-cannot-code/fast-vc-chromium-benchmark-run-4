e6260ac0da6eb3dd36d58cf3fcee92f4b1ab382c
Nathan Memmott <memmott@chromium.org>
2024-08-09 22:33:49 +0000

    Change Observer: Make the FileSystemAccessLockManager ref counted
    The FileSystemAccessLockManager currently assumes that all locks will be
    released before it is destroyed by the FileSystemAccessManager. This is
    because all holders of locks are owned by FileSystemAccessManager who
    orders its members such that the holders will be destroyed before the
    FileSystemAccessLockManager.
    
    But this is the wrong assumption since other sequences end up holding
    locks. For example, FileSystemAccessFileHandleImpl::DidTakeSwapLock
    passes it to DoFileSystemOperation by binding the lock to a callback.
    
    The solution is to make FileSystemAccessLockManager ref counted. And
    have the FileSystemAccessManagerImpl hold a scoped_refptr and each
    RootLock hold a scoped_ref. That way the FileSystemAccessLockManager
    stays alive as long as either the FileSystemAccessManagerImpl is alive
    or if it has a lock.
    
    Bug: 339898769
    Change-Id: I80e49469d4e4439d21fe9fdaca1e00f7009e2f39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775696
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339918}

--- content/browser/file_system_access/file_system_access_lock_manager.cc
+++ content/browser/file_system_access/file_system_access_lock_manager.cc
@@ -459,7 +459,7 @@ class Lock {
 class RootLock : public Lock {
  public:
   explicit RootLock(
-      base::WeakPtr<FileSystemAccessLockManager> lock_manager,
+      scoped_refptr<FileSystemAccessLockManager> lock_manager,
       const FileSystemAccessLockManager::RootLocator& root_locator)
       : Lock({},
              lock_manager->ancestor_lock_type_,
@@ -471,7 +471,7 @@ class RootLock : public Lock {
  private:
   void DestroySelf() override { lock_manager_->ReleaseRoot(root_locator_); }
 
-  base::WeakPtr<FileSystemAccessLockManager> lock_manager_;
+  scoped_refptr<FileSystemAccessLockManager> lock_manager_;
   FileSystemAccessLockManager::RootLocator root_locator_;
 };
 
@@ -540,7 +540,9 @@ LockHandle::~LockHandle() {
 }
 
 FileSystemAccessLockManager::FileSystemAccessLockManager(
-    base::PassKey<FileSystemAccessManagerImpl> /*pass_key*/) {}
+    base::PassKey<FileSystemAccessManagerImpl> /*pass_key*/)
+    : base::RefCountedDeleteOnSequence<FileSystemAccessLockManager>(
+          base::SequencedTaskRunner::GetCurrentDefault()) {}
 
 FileSystemAccessLockManager::~FileSystemAccessLockManager() = default;
 
@@ -621,6 +623,8 @@ void FileSystemAccessLockManager::TakeLock(
 void FileSystemAccessLockManager::ReleaseRoot(const RootLocator& root_locator) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
+  // May destroy `this` if `FileSystemAccessManagerImpl` no longer holds a
+  // `scoped_refptr` to `this`, and this is the last root lock.
   size_t count_removed = root_locks_.erase(root_locator);
 
   DCHECK_EQ(1u, count_removed);
@@ -639,7 +643,7 @@ RootLock* FileSystemAccessLockManager::GetOrCreateRootLock(
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   RootLock* root_lock = GetRootLock(root_locator);
   if (!root_lock) {
-    root_lock = new RootLock(weak_factory_.GetWeakPtr(), root_locator);
+    root_lock = new RootLock(this, root_locator);
     root_locks_.emplace(std::move(root_locator),
                         base::WrapUnique<RootLock>(root_lock));
   }
@@ -659,6 +663,12 @@ FileSystemAccessLockManager::GetExclusiveLockType() {
 FileSystemAccessLockManager::LockType
 FileSystemAccessLockManager::GetAncestorLockTypeForTesting() {
   return ancestor_lock_type_;
+}
+
+base::WeakPtr<FileSystemAccessLockManager>
+FileSystemAccessLockManager::GetWeakPtrForTesting() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return weak_factory_.GetWeakPtr();
 }
 
 }  // namespace content
