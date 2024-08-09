28074569120670971636c0d9df7e0ca3272450d4
David Black <dmblack@google.com>
2024-08-09 20:59:06 +0000

    Flatten InProgressAshDownload into InProgressDownload.
    Previously there existed both InProgressAshDownload and
    InProgressLacrosDownload, each inheriting from InProgressDownload. Now
    that InProgressLacrosDownload has been removed, InProgressAshDownload
    can be flattened into InProgressDownload.
    
    Note that this CL also removes the use of crosapi download types so that
    those types can later be removed w/o the need to modify holding space
    code.
    
    Bug: 356911185
    Change-Id: I409b4d16371b11ec13f9a38a464f8d23089976ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763387
    Commit-Queue: David Black <dmblack@google.com>
    Reviewed-by: Andrew Xu <andrewxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339858}

--- chrome/browser/ui/ash/holding_space/holding_space_downloads_delegate.h
+++ chrome/browser/ui/ash/holding_space/holding_space_downloads_delegate.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/arc/fileapi/arc_file_system_bridge.h"
 #include "chrome/browser/download/notification/multi_profile_download_notifier.h"
 #include "chrome/browser/ui/ash/holding_space/holding_space_keyed_service_delegate.h"
-#include "chromeos/crosapi/mojom/download_controller.mojom-forward.h"
 
 namespace content {
 class DownloadManager;
@@ -50,7 +49,6 @@ class HoldingSpaceDownloadsDelegate
 
  private:
   class InProgressDownload;
-  class InProgressAshDownload;
 
   // HoldingSpaceKeyedServiceDelegate:
   void OnPersistenceRestored() override;
