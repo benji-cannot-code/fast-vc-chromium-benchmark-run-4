b7c0497288089644558716d1a7388db59f3f0529
Yulun Wu <yulunwu@chromium.org>
2024-08-09 23:25:11 +0000

    Update coral provider to observe browser activity.
    This CL sets up the framework to send web content metadata to the
    coral ML backend to generate cached embeddings.
    
    Bug: b:356684805
    Change-Id: Id43e0638889e736f08dcf9a9a82243db040a4175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773665
    Commit-Queue: Yulun Wu <yulunwu@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339940}

--- ash/birch/birch_model.cc
+++ ash/birch/birch_model.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "ash/birch/birch_coral_provider.h"
 #include "ash/birch/birch_data_provider.h"
 #include "ash/birch/birch_icon_cache.h"
 #include "ash/birch/birch_item.h"
@@ -69,6 +70,9 @@ BirchModel::BirchModel()
       icon_cache_(std::make_unique<BirchIconCache>()) {
   if (features::IsBirchWeatherEnabled()) {
     weather_provider_ = std::make_unique<BirchWeatherProvider>(this);
+  }
+  if (features::IsBirchCoralEnabled()) {
+    coral_provider_ = std::make_unique<BirchCoralProvider>(this);
   }
   Shell::Get()->session_controller()->AddObserver(this);
   SimpleGeolocationProvider::GetInstance()->AddObserver(this);
