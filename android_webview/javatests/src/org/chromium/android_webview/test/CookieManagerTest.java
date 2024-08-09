9368a231064af1b6c40cbcb886da2bfa12003f8a
Rupert Ben Wiser <bewise@chromium.org>
2024-08-09 14:54:38 +0000

    [webview] Re-enable auto grant SAA tests
    This reverts commit 121143604cdce7f865890488034ac5d5a82e816a.
    
    The tests were flaky because there were competing strings.xml files. Removed the one that was only used for this test, and added the needed site to the other one.
    
    Original change's description:
    > [Android] Disable 2 CookieManagerTests and 2 AwPermissionManagerTests
    >
    > Was not able to bisect as the same tests failed when going back in
    > commits.
    >
    > Bug: 358114957,358122228,358119171
    > Change-Id: I6cffc025d0c57402ad24ea7385746532dd395c28
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769814
    > Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    > Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    > Reviewed-by: Mohamed Heikal <mheikal@chromium.org>
    > Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1338633}
    
    Bug: 358114957,358122228,358119171
    Change-Id: I8582983117491b8fb6aefbf07b7e95c15e8ca782
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5772811
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Auto-Submit: Rupert Wiser <bewise@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339644}

--- android_webview/javatests/src/org/chromium/android_webview/test/CookieManagerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/CookieManagerTest.java
@@ -1442,7 +1442,6 @@ public class CookieManagerTest extends AwParameterizedTest {
     @MediumTest
     @Feature({"AndroidWebView", "Privacy"})
     @Features.EnableFeatures({AwFeatures.WEBVIEW_AUTO_SAA})
-    @DisabledTest(message = "crbug.com/358114957")
     public void testPartitionedJSCookies() throws Throwable {
         String partitionedCookie = "partitioned-cookie=123";
         String unpartitionedCookie = "regular-cookie=456";
@@ -1539,7 +1538,6 @@ public class CookieManagerTest extends AwParameterizedTest {
     @Feature({"AndroidWebView", "Privacy"})
     @CommandLineFlags.Add("disable-partitioned-cookies")
     @Features.EnableFeatures({AwFeatures.WEBVIEW_AUTO_SAA})
-    @DisabledTest(message = "crbug.com/358122228")
     public void testDisabledPartitionedJSCookies() throws Throwable {
         String partitionedCookie = "partitioned-cookie=123";
         String unpartitionedCookie = "regular-cookie=456";
