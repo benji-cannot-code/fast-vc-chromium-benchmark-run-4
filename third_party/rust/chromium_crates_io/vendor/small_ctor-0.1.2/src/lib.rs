64a3c2f3695797018ec7d2241e321279c7c706a3
Lukasz Anforowicz <lukasza@chromium.org>
2024-08-09 22:46:45 +0000

    Roll small_ctor: 0.1.1 => 0.1.2 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * small_ctor: 0.1.1 => 0.1.2
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * small_ctor@0.1.2: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I6956fde30f7952fe35809c1a1defea3f961c6731
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5774123
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339925}

--- third_party/rust/chromium_crates_io/vendor/small_ctor-0.1.2/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/small_ctor-0.1.2/src/lib.rs
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! IO system is already unusable.  More importantly on many platforms these
 //! do not run properly.  For instance on macOS destructors do not run when
 //! thread local storage is in use.  If you do want to use something like this
-//! you can do something like invoke `libc::at_exit` from within a `#[ctor]`
+//! you can do something like invoke `libc::atexit` from within a `#[ctor]`
 //! function.
 use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
 
@@ -210,13 +210,13 @@ pub fn ctor(args: TokenStream, input: TokenStream) -> TokenStream {
                 TokenTree::Group(Group::new(
                     Delimiter::Parenthesis,
                     tokens![
-                        TokenTree::Ident(Ident::new("target_os", Span::call_site())),
+                        TokenTree::Ident(Ident::new("target_vendor", Span::call_site())),
                         TokenTree::Punct(Punct::new('=', Spacing::Alone)),
-                        TokenTree::Literal(Literal::string("macos")),
+                        TokenTree::Literal(Literal::string("apple")),
                         TokenTree::Punct(Punct::new(',', Spacing::Alone)),
                         TokenTree::Ident(Ident::new("link_section", Span::call_site())),
                         TokenTree::Punct(Punct::new('=', Spacing::Alone)),
-                        TokenTree::Literal(Literal::string("__DATA_CONST,__mod_init_func")),
+                        TokenTree::Literal(Literal::string("__DATA,__mod_init_func")),
                     ],
                 ))
             ],
