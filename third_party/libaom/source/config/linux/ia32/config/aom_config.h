a16d3c8e41293e895fc874d585a499605a834235
Marco Paniconi <marpan@google.com>
2024-08-09 18:24:57 +0000

    Roll src/third_party/libaom/source/libaom/ d8ef6563e..12c64e8fe (26 commits)
    https://aomedia.googlesource.com/aom.git/+log/d8ef6563e0ac..12c64e8fe114
    
    $ git log d8ef6563e..12c64e8fe --date=short --no-merges --format='%ad %ae %s'
    2024-08-08 wtc Remove the INLINE macro
    2024-08-08 wtc Use the saturate_cast_double_to_int() function
    2024-08-08 wtc Define AOM_INLINE and INLINE as inline
    2024-08-08 jzern debug_util.h: fix prototype typo
    2024-08-06 marpan rtc: Convert the max_consec_drop to time units
    2024-08-08 wtc Add the saturate_cast_double_to_int() function
    2024-08-07 jzern variance_neon_dotprod.c: add missing !CONFIG_REALTIME_ONLY check
    2024-08-07 jzern av1.cmake: rm arm src w/CONFIG_AV1_HIGHBITDEPTH=0
    2024-08-06 jzern cmake: rm some arm srcs w/CONFIG_REALTIME_ONLY=1
    2024-08-06 jzern variance_neon.c: add missing !CONFIG_REALTIME_ONLY check
    (...)
    2024-08-06 jzern add missing CONFIG_AV1_HIGHBITDEPTH checks
    2024-08-06 jzern update libwebm to libwebm-1.0.0.31-10-g3b63004
    2024-08-05 jonathan.wright Refactor 4-tap convolve_2d_sr Neon I8MM path
    2024-08-02 jonathan.wright Use Arm Neon USMMLA for horiz. 6-tap path for convolve_2d_sr
    2024-08-01 jzern update AV1E_SET_POSTENCODE_DROP_RTC comment
    2024-08-02 salome.thirot Add overflow check in HBD compute_stats unit tests
    2024-08-02 salome.thirot Fix accumulator value for Neon implementation of compute_stats
    2024-08-02 jonathan.wright Add Arm Neon USMMLA impl. for horiz. 12-tap convolve_2d_sr
    2024-08-01 wtc Compile C++ files with -Wundef
    2024-08-02 wtc Fix -Wundef warnings in libyuv's row_common.cc
    
    Created with:
      roll-dep src/third_party/libaom/source/libaom
    R=jzern@google.com
    
    Bug: b:307414544
    Change-Id: I62d70a239d1bdfcb0efe5572bd0bcb18effdef04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777819
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Marco Paniconi <marpan@google.com>
    Reviewed-by: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Urvang Joshi <urvang@chromium.org>
    Commit-Queue: Urvang Joshi <urvang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339748}

--- third_party/libaom/source/config/linux/ia32/config/aom_config.h
+++ third_party/libaom/source/config/linux/ia32/config/aom_config.h
@@ -95,6 +95,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_UNISTD_H 1
 #define HAVE_VSX 0
 #define HAVE_WXWIDGETS 0
-#define INLINE inline
 #define STATIC_LINK_JXL 0
 #endif  // AOM_CONFIG_H_
