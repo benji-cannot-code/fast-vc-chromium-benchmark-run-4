802ee96e66920b4e1fdd10217f3b39cebd491377
Scott Violet <sky@chromium.org>
2024-08-09 23:44:00 +0000

    gpu: add ability to yield rasterization
    This makes rasterization check if it should yield every so
    often. This is useful when clients generate a lot of ops.
    
    Bug: 358591562
    
    Change-Id: Ibb4a950032657576b960557703bf06139c943bb0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5605788
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339953}

--- gpu/ipc/service/command_buffer_stub.h
+++ gpu/ipc/service/command_buffer_stub.h
@@ -155,6 +155,7 @@ class GPU_IPC_SERVICE_EXPORT CommandBufferStub
   void OnRescheduleAfterFinished() override;
   void ScheduleGrContextCleanup() override;
   void HandleReturnData(base::span<const uint8_t> data) override;
+  bool ShouldYield() override;
 
   using MemoryTrackerFactory =
       base::RepeatingCallback<std::unique_ptr<MemoryTracker>()>;
