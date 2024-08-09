93eb000cf45481fb74b4d9d8b8f46c1674786174
Jun Ishiguro <junis@google.com>
2024-08-09 04:05:27 +0000

    Add timestamp parameter on WindowRestoreTracker's callbacks
    This CL is a preparation for b/353644000, which will be fixed in
    upcoming changes.
    
    Bug: b:353644000
    Change-Id: I400d5bfd67c4a1602d1b2898f574c5da1bcf7da8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5770179
    Commit-Queue: Jun Ishiguro <junis@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339463}

--- ash/metrics/login_unlock_throughput_recorder.cc
+++ ash/metrics/login_unlock_throughput_recorder.cc
@@ -104,9 +104,10 @@ bool HasPendingIcon(const ShelfModel* model) {
 WindowRestoreTracker::WindowRestoreTracker() = default;
 WindowRestoreTracker::~WindowRestoreTracker() = default;
 
-void WindowRestoreTracker::Init(base::OnceClosure on_all_window_created,
-                                base::OnceClosure on_all_window_shown,
-                                base::OnceClosure on_all_window_presented) {
+void WindowRestoreTracker::Init(
+    WindowRestoreTracker::NotifyCallback on_all_window_created,
+    WindowRestoreTracker::NotifyCallback on_all_window_shown,
+    WindowRestoreTracker::NotifyCallback on_all_window_presented) {
   on_created_ = std::move(on_all_window_created);
   on_shown_ = std::move(on_all_window_shown);
   on_presented_ = std::move(on_all_window_presented);
@@ -133,7 +134,7 @@ void WindowRestoreTracker::OnCreated(int window_id) {
 
   const bool all_created = CountWindowsInState(State::kNotCreated) == 0;
   if (all_created && on_created_) {
-    std::move(on_created_).Run();
+    std::move(on_created_).Run(base::TimeTicks::Now());
   }
 }
 
@@ -150,7 +151,7 @@ void WindowRestoreTracker::OnShown(int window_id, ui::Compositor* compositor) {
   const bool all_shown = CountWindowsInState(State::kNotCreated) == 0 &&
                          CountWindowsInState(State::kCreated) == 0;
   if (all_shown && on_shown_) {
-    std::move(on_shown_).Run();
+    std::move(on_shown_).Run(base::TimeTicks::Now());
   }
 
   if (compositor &&
@@ -160,21 +161,23 @@ void WindowRestoreTracker::OnShown(int window_id, ui::Compositor* compositor) {
                        weak_ptr_factory_.GetWeakPtr(), window_id));
   } else if (compositor) {
     // Primary display not detected. Assume it's a headless unit.
-    OnPresented(window_id);
+    OnPresented(window_id, base::TimeTicks::Now());
   }
 }
 
 void WindowRestoreTracker::OnPresentedForTesting(int window_id) {
-  OnPresented(window_id);
+  OnPresented(window_id, base::TimeTicks::Now());
 }
 
 void WindowRestoreTracker::OnCompositorFramePresented(
     int window_id,
     const viz::FrameTimingDetails& details) {
-  OnPresented(window_id);
+  // TODO(b/353644000): Use details.presentation_feedback.timestamp.
+  OnPresented(window_id, base::TimeTicks::Now());
 }
 
-void WindowRestoreTracker::OnPresented(int window_id) {
+void WindowRestoreTracker::OnPresented(int window_id,
+                                       base::TimeTicks presentation_time) {
   auto iter = windows_.find(window_id);
   if (iter == windows_.end()) {
     return;
@@ -188,7 +191,7 @@ void WindowRestoreTracker::OnPresented(int window_id) {
                              CountWindowsInState(State::kCreated) == 0 &&
                              CountWindowsInState(State::kShown) == 0;
   if (all_presented && on_presented_) {
-    std::move(on_presented_).Run();
+    std::move(on_presented_).Run(presentation_time);
   }
 }
 
@@ -525,30 +528,27 @@ void LoginUnlockThroughputRecorder::OnPostLoginDeferredTaskTimerFired() {
   post_login_deferred_task_runner_->Start();
 }
 
-void LoginUnlockThroughputRecorder::OnAllWindowsCreated() {
-  auto now = base::TimeTicks::Now();
+void LoginUnlockThroughputRecorder::OnAllWindowsCreated(base::TimeTicks time) {
   for (auto& obs : observers_) {
-    obs.OnAllBrowserWindowsCreated(now);
+    obs.OnAllBrowserWindowsCreated(time);
   }
 }
 
-void LoginUnlockThroughputRecorder::OnAllWindowsShown() {
-  auto now = base::TimeTicks::Now();
+void LoginUnlockThroughputRecorder::OnAllWindowsShown(base::TimeTicks time) {
   for (auto& obs : observers_) {
-    obs.OnAllBrowserWindowsShown(now);
+    obs.OnAllBrowserWindowsShown(time);
   }
 }
 
-void LoginUnlockThroughputRecorder::OnAllWindowsPresented() {
-  auto now = base::TimeTicks::Now();
+void LoginUnlockThroughputRecorder::OnAllWindowsPresented(
+    base::TimeTicks time) {
   for (auto& obs : observers_) {
-    obs.OnAllBrowserWindowsPresented(now);
+    obs.OnAllBrowserWindowsPresented(time);
   }
 
   DCHECK(!window_restore_done_);
   window_restore_done_ = true;
   ScheduleWaitForShelfAnimationEndIfNeeded();
 }
-
 
 }  // namespace ash
