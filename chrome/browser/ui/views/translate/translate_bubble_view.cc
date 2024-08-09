90d64a3b4928575d34d0a708092cfbfb938571b9
Joseph Park <josephjoopark@chromium.org>
2024-08-09 17:40:23 +0000

    [toolbarpinning] Don't create pinned actions when ToolbarPinning is disabled
    This CL fixes some crashes caused by this change by putting ToolbarPinning related code behind its feature flag.
    
    Bug: 358278631
    Change-Id: I8b3a298b722d10ea54dd94683461260820f72d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773884
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339715}

--- chrome/browser/ui/views/translate/translate_bubble_view.cc
+++ chrome/browser/ui/views/translate/translate_bubble_view.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/chrome_pages.h"
 #include "chrome/browser/ui/translate/translate_bubble_model_impl.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "chrome/browser/ui/views/translate/translate_icon_view.h"
 #include "chrome/common/url_constants.h"
@@ -120,7 +121,7 @@ TranslateBubbleView::~TranslateBubbleView() {
   // is referred by Combobox's destructor. Before destroying the models,
   // removing the child views is needed.
   RemoveAllChildViews();
-  if (translate_action_item_) {
+  if (features::IsToolbarPinningEnabled() && translate_action_item_) {
     translate_action_item_->SetIsShowingBubble(false);
   }
 }
@@ -178,11 +179,14 @@ void TranslateBubbleView::Init() {
     model_->ShowError(error_type_);
   }
 
-  Browser* browser = chrome::FindLastActive();
-  if (browser) {
-    translate_action_item_ = actions::ActionManager::Get().FindAction(
-        kActionShowTranslate, browser->browser_actions()->root_action_item());
-    translate_action_item_->SetIsShowingBubble(true);
+  if (features::IsToolbarPinningEnabled()) {
+    Browser* browser = chrome::FindLastActive();
+    if (browser) {
+      translate_action_item_ = actions::ActionManager::Get().FindAction(
+          kActionShowTranslate, browser->browser_actions()->root_action_item());
+      CHECK(translate_action_item_);
+      translate_action_item_->SetIsShowingBubble(true);
+    }
   }
 }
 
--- chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler_unittest.cc
+++ chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar_handler_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser_actions.h"
 #include "chrome/browser/ui/toolbar/pinned_toolbar/pinned_toolbar_actions_model_factory.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/side_panel/customize_chrome/customize_toolbar/customize_toolbar.mojom.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/generated_resources.h"
@@ -132,7 +133,8 @@ class CustomizeToolbarHandlerTest : public BrowserWithTestWindowTest {
   }
 
   virtual void SetupFeatureList() {
-    feature_list_.InitAndEnableFeature(lens::features::kLensOverlay);
+    feature_list_.InitWithFeatures(
+        {features::kToolbarPinning, lens::features::kLensOverlay}, {});
   }
 
   CustomizeToolbarHandler& handler() { return *handler_; }
