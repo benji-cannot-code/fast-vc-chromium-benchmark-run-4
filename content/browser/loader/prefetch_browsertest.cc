09c70e357d2572acc3b8bbaa3803d209fe8079d2
Andrew Williams <awillia@chromium.org>
2024-08-09 12:09:30 +0000

    Fix several disabled PrefetchBrowsertests
    PrefetchBrowserTest.DISABLED_CrossOriginDocumentReusedAsNavigation
    is currently disabled for being flaky, but when run manually it fails
    consistently. Using bisect_test.py, I found the following CL as the
    culprit:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/5277068
    
    That CL had a fix for a similar test, so applying that fix again
    corrected the issue with the disabled test (I confirmed this manually
    using:
    ./out/Debug/content_browsertests --gtest_also_run_disabled_tests --gtest_filter="*DISABLED_CrossOriginDocumentReusedAsNavigation/3"
    
    DISABLED_CrossOriginWithPreloadHasNoSameSiteCookies and
    DISABLED_CrossOriginDocumentHasNoSameSiteCookies also fail
    consistently but because of a change I introduced that changes the
    IsolationInfo::RequestType that as="document" prefetches use:
    
    https://chromium-review.googlesource.com/c/chromium/src/+/5721990
    
    This CL fixes that as well. I'm not sure how to fix the flakiness,
    so leaving these tests disabled.
    
    Bug: 347934841, 324418206
    Change-Id: Ic5c3be6cea3d586b2d3c4a888c4d892cbb2c16fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771034
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Andrew Williams <awillia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339577}

--- content/browser/loader/prefetch_browsertest.cc
+++ content/browser/loader/prefetch_browsertest.cc
@@ -204,7 +204,7 @@ IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest,
   ASSERT_TRUE(request->trusted_params);
   url::Origin cross_origin = url::Origin::Create(cross_origin_target_url);
   EXPECT_TRUE(net::IsolationInfo::Create(
-                  net::IsolationInfo::RequestType::kOther, cross_origin,
+                  net::IsolationInfo::RequestType::kMainFrame, cross_origin,
                   cross_origin, net::SiteForCookies())
                   .IsEqualForTesting(request->trusted_params->isolation_info));
 }
@@ -214,9 +214,11 @@ IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest,
                        DISABLED_CrossOriginDocumentReusedAsNavigation) {
   const char* prefetch_path = "/prefetch.html";
   const char* target_path = "/target.html";
-  RegisterResponse(
-      target_path,
-      ResponseEntry("<head><title>Prefetch Target</title></head>"));
+  RegisterResponse(target_path,
+                   ResponseEntry("<head><title>Prefetch Target</title></head>",
+                                 // The empty content type prevents this
+                                 // response from being blocked by ORB.
+                                 /*content_types=*/""));
 
   base::RunLoop prefetch_waiter;
   auto request_counter = RequestCounter::CreateAndMonitor(
@@ -723,7 +725,7 @@ IN_PROC_BROWSER_TEST_P(PrefetchBrowserTest,
   ASSERT_TRUE(request->trusted_params);
   url::Origin cross_origin = url::Origin::Create(cross_origin_target_url);
   EXPECT_TRUE(net::IsolationInfo::Create(
-                  net::IsolationInfo::RequestType::kOther, cross_origin,
+                  net::IsolationInfo::RequestType::kMainFrame, cross_origin,
                   cross_origin, net::SiteForCookies())
                   .IsEqualForTesting(request->trusted_params->isolation_info));
 }
