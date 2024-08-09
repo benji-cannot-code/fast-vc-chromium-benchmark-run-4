b96a3e288710cf36a068fc90c8361c909e28cb25
Maksim Sadym <sadym@google.com>
2024-08-09 13:08:39 +0000

    Provide navigation type in `Page.navigatedWithinDocument` event
    The `Page.navigatedWithinDocument` event is emitted for both "realm fragment navigation" [1] and "history update" [2]. However, the HTML specification distinguishes this 2 cases. Adding navigation type to provide that info to the user.
    
    [1] https://html.spec.whatwg.org/multipage/browsing-the-web.html#navigate-fragid
    [2] https://html.spec.whatwg.org/multipage/browsing-the-web.html#url-and-history-update-steps
    
    Bug: 358097882
    Change-Id: Ie2d35bc19f7bb3a8e248fabf4be7479af8804606
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767581
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339598}

--- third_party/blink/renderer/core/inspector/inspector_page_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_page_agent.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/origin_trials/trial_token.h"
 #include "third_party/blink/public/common/web_preferences/web_preferences.h"
 #include "third_party/blink/public/mojom/ad_tagging/ad_evidence.mojom-blink.h"
+#include "third_party/blink/public/mojom/loader/same_document_navigation_type.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/local_window_proxy.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_controller.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_evaluation_result.h"
@@ -978,11 +979,32 @@ protocol::Response InspectorPageAgent::setDocumentContent(
   return protocol::Response::Success();
 }
 
-void InspectorPageAgent::DidNavigateWithinDocument(LocalFrame* frame) {
+namespace {
+const char* NavigationTypeToProtocolString(
+    mojom::blink::SameDocumentNavigationType navigation_type) {
+  switch (navigation_type) {
+    case mojom::blink::SameDocumentNavigationType::kFragment:
+      return protocol::Page::NavigatedWithinDocument::NavigationTypeEnum::
+          Fragment;
+    case mojom::blink::SameDocumentNavigationType::kHistoryApi:
+      return protocol::Page::NavigatedWithinDocument::NavigationTypeEnum::
+          HistoryApi;
+    case mojom::blink::SameDocumentNavigationType::kNavigationApiIntercept:
+    case mojom::blink::SameDocumentNavigationType::
+        kPrerenderNoVarySearchActivation:
+      return protocol::Page::NavigatedWithinDocument::NavigationTypeEnum::Other;
+  }
+}
+}  // namespace
+
+void InspectorPageAgent::DidNavigateWithinDocument(
+    LocalFrame* frame,
+    mojom::blink::SameDocumentNavigationType navigation_type) {
   Document* document = frame->GetDocument();
   if (document) {
     return GetFrontend()->navigatedWithinDocument(
-        IdentifiersFactory::FrameId(frame), document->Url());
+        IdentifiersFactory::FrameId(frame), document->Url(),
+        NavigationTypeToProtocolString(navigation_type));
   }
 }
 
