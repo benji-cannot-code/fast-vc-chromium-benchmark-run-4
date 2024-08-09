9ff932efefb31578586fd71b36b695e8960d54c8
Solomon Kinard <solomonkinard@chromium.org>
2024-08-09 21:18:43 +0000

    Extensions: WAR: GUID: Clarify the use of script.src for a web request
    crrev.com/c/5766980 was created specifically for changing a script src
    using a web request and a content script. Therefore, variables and
    filenames are aligned.
    
    Bug: chromium:352267920
    Change-Id: I1d18a1a19e4884a1cad111e95d2b212df926b62d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5778340
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1339875}

--- chrome/test/data/extensions/web_accessible_resources/web_request/background.js
+++ chrome/test/data/extensions/web_accessible_resources/web_request/background.js
@@ -4,11 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.webRequest.onBeforeRequest.addListener(details => {
-  if (details.url !== 'http://example.org/redirect.html') {
-    return;
+  if (details.url.endsWith('redirect.js')) {
+    return {redirectUrl: chrome.runtime.getURL('war.js')};
   }
-
-  return {redirectUrl: chrome.runtime.getURL('war.html')};
 }, {urls: ['<all_urls>']}, ['blocking']);
 
 chrome.test.sendMessage('ready');
--- chrome/test/data/extensions/web_accessible_resources/web_request/content_script.js
+++ chrome/test/data/extensions/web_accessible_resources/web_request/content_script.js
@@ -3,6 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const iframe = document.createElement('script');
-document.body.appendChild(iframe);
-iframe.src = 'http://example.org/redirect.html';
+const script = document.createElement('script');
+document.body.appendChild(script);
+script.src = 'http://example.org/redirect.js';
