01ae5aad5f0b950e2fad8c02afb48c51dd8f9cbf
Gauthier Ambard <gambard@chromium.org>
2024-08-12 10:10:19 +0000

    [iOS] Add DataSharing delegate classes
    This CL adds the skeleton of the DataSharing {UI|SDK} Delegates.
    They will be used and implemented later.
    
    Bug: 358359374, 358359277
    Change-Id: Ie7f2b694ab3f677a2b48323dcb2cc771c0fb3de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776025
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340290}

--- components/data_sharing/public/data_sharing_ui_delegate.h
+++ components/data_sharing/public/data_sharing_ui_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_DATA_SHARING_PUBLIC_DATA_SHARING_UI_DELEGATE_H_
 #define COMPONENTS_DATA_SHARING_PUBLIC_DATA_SHARING_UI_DELEGATE_H_
 
+class GURL;
+
 namespace data_sharing {
 
 // An interface for the data sharing service to communicate with UI elements.
---
+++ ios/chrome/browser/data_sharing/model/data_sharing_ui_delegate_ios.mm
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/data_sharing/model/data_sharing_ui_delegate_ios.h"
+
+#import "base/notimplemented.h"
+
+namespace data_sharing {
+
+void DataSharingUIDelegateIOS::HandleShareURLIntercepted(const GURL& url) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace data_sharing
