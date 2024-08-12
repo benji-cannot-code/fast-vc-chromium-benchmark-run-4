b1bbc3e2c67d1b23875d4af10f11466cf9ebed29
Norge Vizcay <vizcay@google.com>
2024-08-12 15:02:47 +0000

    Add parsing logic to PlusAddressBlocklistData.
    The CL adds logic to `PopulateDataFromComponent`. The function is in
    charge of parsing the binary proto sent by the Component Updater and
    populating the regular expressions of PlusAddressBlocklistData that are
    exposed via getters.
    
    Bug: 324556906
    Change-Id: I6a1a63e48fd18ad67d5f6efbff579e795467e3b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771424
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340387}

--- components/plus_addresses/plus_address_blocklist_data.cc
+++ components/plus_addresses/plus_address_blocklist_data.cc
@@ -8,8 +8,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/no_destructor.h"
+#include "components/plus_addresses/blocked_facets.pb.h"
+#include "third_party/re2/src/re2/re2.h"
 
 namespace plus_addresses {
+
+namespace {
+
+std::unique_ptr<re2::RE2> ConstructRegex(std::string pattern) {
+  if (pattern.empty()) {
+    return nullptr;
+  }
+
+  re2::RE2::Options options;
+  options.set_case_sensitive(false);
+  return std::make_unique<re2::RE2>(std::move(pattern), options);
+}
+}  // namespace
 
 // static
 PlusAddressBlocklistData& PlusAddressBlocklistData::GetInstance() {
@@ -22,18 +37,29 @@ PlusAddressBlocklistData::~PlusAddressBlocklistData() = default;
 
 bool PlusAddressBlocklistData::PopulateDataFromComponent(
     const std::string& binary_pb) {
-  // TODO(crbug.com/324556906): Parse binary data.
-  return false;
+  if (binary_pb.empty()) {
+    // TODO(crbug.com/324556906): Emit parsing metrics.
+    return false;
+  }
+
+  CompactPlusAddressBlockedFacets blocked_facets;
+  if (!blocked_facets.ParseFromString(binary_pb)) {
+    // TODO(crbug.com/324556906): Emit parsing metrics.
+    return false;
+  }
+
+  exclusion_pattern_ = ConstructRegex(blocked_facets.exclusion_pattern());
+  exception_pattern_ = ConstructRegex(blocked_facets.exception_pattern());
+
+  return true;
 }
 
 const re2::RE2* PlusAddressBlocklistData::GetExclusionPattern() const {
-  // TODO(crbug.com/324556906): Complete.
-  return nullptr;
+  return exclusion_pattern_.get();
 }
 
 const re2::RE2* PlusAddressBlocklistData::GetExceptionPattern() const {
-  // TODO(crbug.com/324556906): Complete.
-  return nullptr;
+  return exception_pattern_.get();
 }
 
 }  // namespace plus_addresses
