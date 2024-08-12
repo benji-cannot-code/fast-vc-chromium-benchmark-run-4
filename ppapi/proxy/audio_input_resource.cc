49707d18a54f484c2db860ca3e6d439954194b09
Jan Keitel <jkeitel@google.com>
2024-08-12 13:30:54 +0000

    Use spanified version of SyncSocket in //ppapi/
    Bug: 40284755
    Change-Id: Iadd2481835dccd88a58ad73b2074ec35cbb3df6a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776843
    Reviewed-by: Fabian Sommer <fabiansommer@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340345}

--- ppapi/proxy/audio_input_resource.cc
+++ ppapi/proxy/audio_input_resource.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/check_op.h"
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/numerics/safe_conversions.h"
 #include "ipc/ipc_platform_file.h"
@@ -265,7 +266,8 @@ void AudioInputResource::Run() {
 
   while (true) {
     int pending_data = 0;
-    size_t bytes_read = socket_->Receive(&pending_data, sizeof(pending_data));
+    size_t bytes_read =
+        socket_->Receive(base::byte_span_from_ref(pending_data));
     if (bytes_read != sizeof(pending_data)) {
       DCHECK_EQ(bytes_read, 0U);
       break;
@@ -282,7 +284,7 @@ void AudioInputResource::Run() {
 
     // Inform other side that we have read the data from the shared memory.
     ++buffer_index;
-    size_t bytes_sent = socket_->Send(&buffer_index, sizeof(buffer_index));
+    size_t bytes_sent = socket_->Send(base::byte_span_from_ref(buffer_index));
     if (bytes_sent != sizeof(buffer_index)) {
       DCHECK_EQ(bytes_sent, 0U);
       break;
