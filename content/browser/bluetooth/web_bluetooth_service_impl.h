17491ece1da68c9741e20b6ae061e9a48b6dd075
Alex N. Jose <alexnj@chromium.org>
2024-08-12 23:24:15 +0000

    Revert "Implement graceful removal of an emulated Bluetooth adapter"
    This reverts commit 3770ef2ce98c16faf64fa91fa01651c1f75e6a44.
    
    Reason for revert: https://issues.chromium.org/issues/358778893
    
    The CL seems to have introduced a crash in WinRT and we've narrowed
    it down to a the way the CL modified the order in which
    AdapterObservers are attached. Reverting this CL to fix the crash, and will follow up with a reland with the correct ordering.
    
    Original change's description:
    > Implement graceful removal of an emulated Bluetooth adapter
    >
    > This CL implements a graceful removal of a Bluetooth override adapter
    > that would restore the original adapter that was in place before
    > the override. The change is made to better support the newly created
    > CDP Bluetooth domain's `disable` operation. It achieves this by
    > maintaining the override adapter separately, and introducing
    > a `GetActiveAdapter()` that switches between real and override
    > adapters for most operations.
    >
    > Bug: 342191615
    > Change-Id: I7ccafe62eb0bdccaeaee94c98ac2b85619ad799c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765953
    > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1339927}
    
    Bug: 342191615, 358778893
    Change-Id: I4acda02e313a2422d432a5b4f41fb6a9f90e0f46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783510
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340689}

--- content/browser/bluetooth/web_bluetooth_service_impl.h
+++ content/browser/bluetooth/web_bluetooth_service_impl.h
@@ -166,8 +166,6 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
                            TwoWatchAdvertisementsReqFail);
   FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
                            SecWatchAdvertisementsReqAfterFirstSuccess);
-  FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTestWithBaseAdapter,
-                           EmulatedAdapterRemovalRestoresOriginalAdapter);
 
 #if PAIR_BLUETOOTH_ON_DEMAND()
   FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
@@ -178,8 +176,6 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
 
   friend class FrameConnectedBluetoothDevicesTest;
   friend class WebBluetoothServiceImplTest;
-  friend class WebBluetoothServiceImplTestWithBaseAdapter;
-
   using PrimaryServicesRequestCallback =
       base::OnceCallback<void(device::BluetoothDevice*)>;
   using ScanFilters = std::vector<blink::mojom::WebBluetoothLeScanFilterPtr>;
