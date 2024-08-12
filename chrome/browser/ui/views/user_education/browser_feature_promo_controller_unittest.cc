7971d03593daf21c10155b1eef72969212a48c51
Dana Fried <dfried@chromium.org>
2024-08-12 18:33:40 +0000

    [User Education] IPH auto-configuration improvements
    This CL adds a check to ensure that none of the Feature Engagement
    event thresholds for an IPH have already been crossed before trying
    to show the promo.
    
    This addresses a corner case where an IPH passes all checks except
    e.g. "this feature must not have actually been used"; this IPH can
    then get far enough into the process of showing to override existing
    IPHs and Tutorials before the controller "discovers" that it actually
    can't show due to configuration constraints.
    
    This was especially evident in a case where the user had customized
    their browser theme before triggering the theme customization IPH.
    In this case, the IPH would try to show on every New Tab Page load,
    which could cause a running Tutorial to abort.
    
    This CL also improves the API for adding additional Feature
    Engagement conditions to a FeaturePromoSpecification that are
    respected by the auto-configure system, changing existing setters to
    factory methods.
    
    Change-Id: I65110a1dafc83158abba901f39a815a31a2e2186
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771951
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340521}

--- chrome/browser/ui/views/user_education/browser_feature_promo_controller_unittest.cc
+++ chrome/browser/ui/views/user_education/browser_feature_promo_controller_unittest.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/user_education/user_education_service_factory.h"
 #include "chrome/grit/generated_resources.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/feature_engagement/public/configuration.h"
 #include "components/feature_engagement/public/feature_list.h"
 #include "components/feature_engagement/public/tracker.h"
 #include "components/feature_engagement/test/mock_tracker.h"
@@ -447,6 +448,8 @@ class BrowserFeaturePromoControllerTest : public TestWithBrowserView {
     EXPECT_CALL(*tracker, ShouldTriggerHelpUI(_))
         .Times(AnyNumber())
         .WillRepeatedly(Return(false));
+    EXPECT_CALL(*tracker, ListEvents)
+        .WillRepeatedly(Return(feature_engagement::Tracker::EventList()));
 
     return tracker;
   }
@@ -470,6 +473,32 @@ TEST_F(BrowserFeaturePromoControllerTest, NotifyFeatureUsedIfValidIsValid) {
   EXPECT_CALL(*mock_tracker_, NotifyUsedEvent(testing::Ref(kTestIPHFeature)))
       .Times(1);
   controller_->NotifyFeatureUsedIfValid(kTestIPHFeature);
+}
+
+TEST_F(BrowserFeaturePromoControllerTest,
+       FeatureEngagementTrackerEvents_DoNotBlockPromo) {
+  feature_engagement::EventConfig config;
+  config.name = "foo";
+  config.comparator = feature_engagement::Comparator(
+      feature_engagement::ComparatorType::LESS_THAN, 2);
+  EXPECT_CALL(*mock_tracker_, ListEvents(testing::Ref(kTestIPHFeature)))
+      .WillOnce(Return(feature_engagement::Tracker::EventList{{config, 1}}));
+  EXPECT_CALL(*mock_tracker_, WouldTriggerHelpUI(testing::Ref(kTestIPHFeature)))
+      .WillOnce(Return(true));
+  EXPECT_EQ(FeaturePromoResult::Success(),
+            controller_->CanShowPromo(kTestIPHFeature));
+}
+
+TEST_F(BrowserFeaturePromoControllerTest,
+       FeatureEngagementTrackerEvents_DoBlockPromo) {
+  feature_engagement::EventConfig config;
+  config.name = "foo";
+  config.comparator = feature_engagement::Comparator(
+      feature_engagement::ComparatorType::LESS_THAN, 2);
+  EXPECT_CALL(*mock_tracker_, ListEvents(testing::Ref(kTestIPHFeature)))
+      .WillOnce(Return(feature_engagement::Tracker::EventList{{config, 2}}));
+  EXPECT_EQ(FeaturePromoResult::kBlockedByConfig,
+            controller_->CanShowPromo(kTestIPHFeature));
 }
 
 TEST_F(BrowserFeaturePromoControllerTest, AsksBackendIfPromoShouldBeShown) {
