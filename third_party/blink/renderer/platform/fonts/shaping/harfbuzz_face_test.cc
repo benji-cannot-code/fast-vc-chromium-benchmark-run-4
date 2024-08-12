fea6f148fcbc0c54945e958fec562fc59243422f
Munira Tursunova <moonira@google.com>
2024-08-12 14:09:53 +0000

    Ignore vs during system fallback on various platforms
    Chrome is using platform specific implementation of
    `PlatformFallbackFontForCharacter` to get fallback font on each
    platform. It relies on FontFallbackPriority value in order to retrieve
    a font with the correct emoji presentation.
    
    Emoji System Fonts on Mac, Win and Android either do not have cmap 14
    subtable or it does not include all emojis from their cmap table. We use
    cmap 14 subtable to identify whether there is a colored (emoji
    presentation) or a monochromatic (text presentation) glyph in the font.
    This may lead to the cases when we will not be able to get the glyph ID
    for the requested variation sequence using fallback system font and will
    continue the second shaping fallback list pass ignoring variation
    selectors, see [0]. For example:
    
    <div style=”font-family:web-mono-font”>&#x1FAE8;&#xFE0F</div>
    
    if "web-mono-font" has a glyph for U+1FAE8 codepoint, we will use this
    monochromatic web font instead of using system colored font.
    
    To prevent that once we reached system fallback fonts, we can ignore
    emoji variation selectors since we will get the font with the correct
    presentation relying on FontFallbackPriority.
    
    [0] https://docs.google.com/document/d/1NyIKm0PnWUwX6j0smDwxDAPSUoiyBHPL95oH2lvjjpc
    
    Bug: 40628044
    Change-Id: If0195171ab7f76f10a6f65ad686c2fd8427c0502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771131
    Commit-Queue: Munira Tursunova <moonira@google.com>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340368}

--- third_party/blink/renderer/platform/fonts/shaping/harfbuzz_face_test.cc
+++ third_party/blink/renderer/platform/fonts/shaping/harfbuzz_face_test.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/testing/font_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/runtime_enabled_features_test_helpers.h"
 #include "third_party/blink/renderer/platform/testing/unit_test_helpers.h"
+#include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 
 namespace blink {
 
@@ -248,5 +249,36 @@ TEST(HarfBuzzFaceTest, HarfBuzzGetNominalGlyph_TestVSOverrideVariantEmoji) {
       GetGlyphForEmojiVSFromFontWithBaseCharOnly(character, variation_selector);
   EXPECT_EQ(glyph_from_font_without_vs, kUnmatchedVSGlyphId);
 }
+
+// Test emoji variation selectors support in system fallback. We are only
+// enabling this feature on Windows, Android and Mac platforms.
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_WIN)
+TEST(HarfBuzzFaceTest, HarfBuzzGetNominalGlyph_TestSystemFallbackEmojiVS) {
+  ScopedFontVariationSequencesForTest scoped_variation_sequences_feature(true);
+  ScopedFontVariantEmojiForTest scoped_variant_emoji_feature(true);
+  ScopedSystemFallbackEmojiVSSupportForTest scoped_system_emoji_vs_feature(
+      true);
+
+  HarfBuzzFace::SetVariationSelectorMode(kUseSpecifiedVariationSelector);
+  HarfBuzzFace::SetIsSystemFallbackStage(true);
+
+  UChar32 character = kShakingFaceEmoji;
+
+  hb_codepoint_t glyph_from_font_with_vs15 = GetGlyphForEmojiVSFromFontWithVS15(
+      character, kVariationSelector15Character);
+  EXPECT_TRUE(glyph_from_font_with_vs15);
+  EXPECT_NE(glyph_from_font_with_vs15, kUnmatchedVSGlyphId);
+
+  hb_codepoint_t glyph_from_font_with_vs16 = GetGlyphForEmojiVSFromFontWithVS16(
+      character, kVariationSelector16Character);
+  EXPECT_TRUE(glyph_from_font_with_vs16);
+  EXPECT_NE(glyph_from_font_with_vs16, kUnmatchedVSGlyphId);
+
+  hb_codepoint_t glyph_from_font_without_vs =
+      GetGlyphForEmojiVSFromFontWithBaseCharOnly(character, 0);
+  EXPECT_TRUE(glyph_from_font_without_vs);
+  EXPECT_NE(glyph_from_font_without_vs, kUnmatchedVSGlyphId);
+}
+#endif
 
 }  // namespace blink
