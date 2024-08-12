ad27fb3538db3d442e3516df2f7172476b903bca
Zachary Tan <tanzachary@chromium.org>
2024-08-12 16:55:20 +0000

    [FedCM] Fix dark mode placeholder color on desktop button mode
    Bug: 348664592
    Change-Id: Ibe801dcaec1302e247b16f664ee98af2915b763f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777071
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340450}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/gfx/canvas.h"
+#include "ui/gfx/color_palette.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/image/canvas_image_source.h"
 #include "ui/gfx/image/image_skia_operations.h"
@@ -202,13 +203,19 @@ void AccountSelectionModalView::InitDialogWidget() {
 
 std::unique_ptr<views::View>
 AccountSelectionModalView::CreatePlaceholderAccountRow() {
+  const SkColor kPlaceholderColor =
+      color_utils::IsDark(web_contents_->GetColorProvider().GetColor(
+          ui::kColorDialogBackground))
+          ? gfx::kGoogleGrey800
+          : gfx::kGoogleGrey200;
+
   std::unique_ptr<views::View> placeholder_account_icon =
       std::make_unique<views::View>();
   placeholder_account_icon->SetPreferredSize(
       gfx::Size(kModalAvatarSize, kModalAvatarSize));
   placeholder_account_icon->SizeToPreferredSize();
-  placeholder_account_icon->SetBackground(views::CreateRoundedRectBackground(
-      gfx::kGoogleGrey200, kModalAvatarSize));
+  placeholder_account_icon->SetBackground(
+      views::CreateRoundedRectBackground(kPlaceholderColor, kModalAvatarSize));
 
   constexpr int kPlaceholderAccountRowPadding = 16;
   auto row = std::make_unique<views::View>();
@@ -240,7 +247,7 @@ AccountSelectionModalView::CreatePlaceholderAccountRow() {
       gfx::Size(kPlaceholderAccountNameWidth, kPlaceholderTextHeight));
   placeholder_account_name->SizeToPreferredSize();
   placeholder_account_name->SetBackground(views::CreateRoundedRectBackground(
-      gfx::kGoogleGrey200, kPlaceholderRadius));
+      kPlaceholderColor, kPlaceholderRadius));
 
   views::View* placeholder_account_email =
       text_column->AddChildView(std::make_unique<views::View>());
@@ -248,7 +255,7 @@ AccountSelectionModalView::CreatePlaceholderAccountRow() {
       gfx::Size(kPlaceholderAccountEmailWidth, kPlaceholderTextHeight));
   placeholder_account_email->SizeToPreferredSize();
   placeholder_account_email->SetBackground(views::CreateRoundedRectBackground(
-      gfx::kGoogleGrey200, kPlaceholderRadius));
+      kPlaceholderColor, kPlaceholderRadius));
 
   return row;
 }
