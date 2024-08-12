0d3927d0eb9d107f75d7cbd7902a5946155105e2
Sophie Chang <sophiechang@chromium.org>
2024-08-12 23:01:33 +0000

    Remove unused import in user annotations service
    Change-Id: Ib18a30f9f811165cecdca1c31027db5924962c61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784935
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340678}

--- components/user_annotations/user_annotations_service.h
+++ components/user_annotations/user_annotations_service.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "components/keyed_service/core/keyed_service.h"
-#include "components/optimization_guide/proto/features/compose.pb.h"
 
 namespace autofill {
 class FormData;
