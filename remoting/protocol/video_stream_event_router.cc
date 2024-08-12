3db0a7addcd5f5bb14f0e0f9689931696f777192
Daniel Cheng <dcheng@chromium.org>
2024-08-12 22:47:29 +0000

    Use base::RSplitStringOnce in video_stream_event_router.cc
    Bug: 358404373
    Change-Id: Iaeed05ac3bc966856b0ea7998fb400edcd9e93bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775647
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340675}

--- remoting/protocol/video_stream_event_router.cc
+++ remoting/protocol/video_stream_event_router.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
 
 namespace remoting::protocol {
 
@@ -46,15 +47,14 @@ void VideoStreamEventRouter::SetVideoChannelStateObserver(
   // Clear out the single stream observer in case the mode changed.
   single_stream_state_observer_ = nullptr;
 
-  auto screen_id_index = stream_name.find_last_of('_');
-  if (screen_id_index == std::string::npos) {
+  auto parts = base::RSplitStringOnce(stream_name, '_');
+  if (!parts) {
     LOG(ERROR) << "Unexpected stream name format: " << stream_name;
     return;
   }
 
   int64_t screen_id;
-  if (!base::StringToInt64(stream_name.substr(screen_id_index + 1),
-                           &screen_id)) {
+  if (!base::StringToInt64(parts->second, &screen_id)) {
     LOG(ERROR) << "Failed to extract screen id from: " << stream_name;
     return;
   }
