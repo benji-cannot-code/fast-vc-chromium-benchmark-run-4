6e4a919f1d91cdbb079e2588f1226df188556639
Sophie Chang <sophiechang@chromium.org>
2024-08-12 21:54:44 +0000

    Add test user annotations service to be usable by future features unit tests
    Change-Id: Ia7d82e961f332dfffa3930702d469d6e9c9e6de3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784534
    Commit-Queue: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340632}

---
+++ components/user_annotations/test_user_annotations_service.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/user_annotations/test_user_annotations_service.h"
+
+#include "components/optimization_guide/proto/features/common_quality_data.pb.h"
+
+namespace user_annotations {
+
+TestUserAnnotationsService::TestUserAnnotationsService() = default;
+TestUserAnnotationsService::~TestUserAnnotationsService() = default;
+
+void TestUserAnnotationsService::ReplaceAllEntries(
+    std::vector<optimization_guide::proto::UserAnnotationsEntry> entries) {
+  entries_ = std::move(entries);
+}
+
+void TestUserAnnotationsService::RetrieveAllEntries(
+    base::OnceCallback<
+        void(std::vector<optimization_guide::proto::UserAnnotationsEntry>)>
+        callback) {
+  std::move(callback).Run(entries_);
+}
+
+}  // namespace user_annotations
