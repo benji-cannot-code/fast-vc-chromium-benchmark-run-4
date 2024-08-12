4a853db4215afae018559038db93980ab14443b0
S. Ganesh <ganesh@chromium.org>
2024-08-12 18:31:15 +0000

    updater: check if the Process is valid before waiting on it
    It's good practice, and also `base::Process::Wait` and variants `CHECK`
    on Mac builds if the `Process` is invalid.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Fixed: 358467991
    Change-Id: Ib68010be1e03ad21a12c53515dd63667dc962e13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5774454
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340520}

--- chrome/updater/app/app_recover.cc
+++ chrome/updater/app/app_recover.cc
@@ -141,7 +141,10 @@ int AppRecover::ReinstallUpdater() const {
   if (IsSystemInstall(updater_scope())) {
     uninstall_command.AppendSwitch(kSystemSwitch);
   }
-  if (!base::LaunchProcess(uninstall_command, {}).WaitForExit(&exit_code)) {
+  const base::Process uninstall_process =
+      base::LaunchProcess(uninstall_command, {});
+  if (!uninstall_process.IsValid() ||
+      !uninstall_process.WaitForExit(&exit_code)) {
     VLOG(0) << "Failed to wait for the uninstaller to exit.";
     return kErrorWaitFailedUninstall;
   }
@@ -155,7 +158,9 @@ int AppRecover::ReinstallUpdater() const {
   if (IsSystemInstall(updater_scope())) {
     install_command.AppendSwitch(kSystemSwitch);
   }
-  if (!base::LaunchProcess(install_command, {}).WaitForExit(&exit_code)) {
+  const base::Process install_process =
+      base::LaunchProcess(install_command, {});
+  if (!install_process.IsValid() || !install_process.WaitForExit(&exit_code)) {
     VLOG(0) << "Failed to wait for the installer to exit.";
     return kErrorWaitFailedInstall;
   }
