b254d5a1f9a5b0aaf045a4b7838c7c77913f1889
James Lee <ljjlee@google.com>
2024-08-12 15:56:57 +0000

    Check that the API key is not set in both request param and header
    The checking is in test code only, not in production builds (to avoid
    any risk of regressions).
    
    Bug: 355652328
    Change-Id: Ib607f73d86aaeea3029e2b1266e68145b846cc86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758565
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: James Lee <ljjlee@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340416}

--- google_apis/common/api_key_request_test_util.cc
+++ google_apis/common/api_key_request_test_util.cc
@@ -13,12 +13,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace google_apis::test_util {
 
 bool HasAPIKey(const network::ResourceRequest& request) {
-  return google_apis::internal::HasAPIKey(request.headers);
+  return google_apis::internal::GetAPIKey(request.url).has_value() ||
+         google_apis::internal::GetAPIKey(request.headers).has_value();
 }
 
 std::optional<std::string> GetAPIKeyFromRequest(
     const network::ResourceRequest& request) {
-  return request.headers.GetHeader(google_apis::internal::kApiKeyHeaderName);
+  std::optional<std::string> header_api_key =
+      google_apis::internal::GetAPIKey(request.headers);
+  std::optional<std::string> url_api_key =
+      google_apis::internal::GetAPIKey(request.url);
+  if (header_api_key) {
+    // The API key should not be present in both the URL query parameter and the
+    // HTTP header.
+    CHECK(!url_api_key);
+    return header_api_key;
+  }
+  return url_api_key;
 }
 
 }  // namespace google_apis::test_util
