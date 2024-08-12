937bf8cc7fff1053be99d8e6d26303fa8fc41743
Sammie Quon <sammiequon@chromium.org>
2024-08-12 23:50:48 +0000

    desks: Reduce runtime of desks tests
    Shorten the tests by simulating the toast dismiss callback and
    shortening the timer and post task delays for testing. Added a note
    to discourage further use of waiting.
    
    This shaves off about 400 seconds from all Desk related tests in ash.
    
    Also fixed two tests that fail with the SavedDeskRevampUi flag on.
    
    Test: ash_unittests *Desk*
    Change-Id: I6825c46e443fd13917819e7673a1e7f8008bbf03
    Fixed: b/337044676
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773883
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340708}

--- ash/wm/desks/desks_controller.cc
+++ ash/wm/desks/desks_controller.cc
@@ -124,6 +124,10 @@ constexpr int kDeskTraversalsMaxValue = 20;
 // interval.
 constexpr base::TimeDelta kDeskTraversalsTimeout = base::Seconds(5);
 
+// The timeout duration that we allow an app window on a closed desk to run
+// its "close" hooks before being forcefully closed.
+base::TimeDelta g_close_all_window_close_timeout = base::Seconds(1);
+
 constexpr int kDeskDefaultNameIds[] = {
     IDS_ASH_DESKS_DESK_1_MINI_VIEW_TITLE,
     IDS_ASH_DESKS_DESK_2_MINI_VIEW_TITLE,
@@ -2187,7 +2191,7 @@ void DesksController::FinalizeDeskRemoval(RemovedDeskData* removed_desk_data) {
       base::BindOnce(&DesksController::CleanUpClosedAppWindowsTask,
                      weak_ptr_factory_.GetWeakPtr(),
                      std::move(closing_window_tracker)),
-      kCloseAllWindowCloseTimeout);
+      g_close_all_window_close_timeout);
 
   // `temporary_removed_desk_` should not be set at this point.
   DCHECK(!temporary_removed_desk_);
@@ -2378,6 +2382,18 @@ void DesksController::ReportCustomDeskNames() const {
                               custom_names_count);
   base::UmaHistogramPercentage(kPercentageOfCustomNamesHistogramName,
                                custom_names_count * 100 / desks_.size());
+}
+
+// static
+base::TimeDelta DesksController::GetCloseAllWindowCloseTimeoutForTest() {
+  return g_close_all_window_close_timeout;
+}
+
+// static
+base::AutoReset<base::TimeDelta>
+DesksController::SetCloseAllWindowCloseTimeoutForTest(
+    base::TimeDelta interval) {
+  return {&g_close_all_window_close_timeout, interval};
 }
 
 }  // namespace ash
