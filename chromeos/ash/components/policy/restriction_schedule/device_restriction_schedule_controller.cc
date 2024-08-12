e9e82749135652fe8ca41be1465dc2179dd9e6ef
Ivan Šandrk <isandrk@chromium.org>
2024-08-12 20:37:35 +0000

    [Bedtime Hours] Observe policy updates in controller
    Bug: b/345186543
    Change-Id: Idf96abafeea023dd68a01d9d7facdc04aed3346a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783098
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340588}

--- chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.cc
+++ chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.cc
@@ -5,13 +5,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller.h"
 
+#include "base/functional/bind.h"
 #include "chromeos/constants/pref_names.h"
 #include "components/prefs/pref_registry_simple.h"
 
 namespace policy {
 
-DeviceRestrictionScheduleController::DeviceRestrictionScheduleController() =
-    default;
+DeviceRestrictionScheduleController::DeviceRestrictionScheduleController(
+    PrefService& pref_service) {
+  registrar_.Init(&pref_service);
+  registrar_.Add(
+      chromeos::prefs::kDeviceRestrictionSchedule,
+      base::BindRepeating(&DeviceRestrictionScheduleController::OnPolicyUpdated,
+                          weak_factory_.GetWeakPtr()));
+}
 
 DeviceRestrictionScheduleController::~DeviceRestrictionScheduleController() =
     default;
@@ -20,6 +27,10 @@ DeviceRestrictionScheduleController::~DeviceRestrictionScheduleController() =
 void DeviceRestrictionScheduleController::RegisterLocalStatePrefs(
     PrefRegistrySimple* registry) {
   registry->RegisterListPref(chromeos::prefs::kDeviceRestrictionSchedule);
+}
+
+void DeviceRestrictionScheduleController::OnPolicyUpdated() {
+  // TODO(b/355636599): Implement function.
 }
 
 }  // namespace policy
