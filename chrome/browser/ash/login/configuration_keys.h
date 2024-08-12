819df5e861f8cb2b71b1c6eb7e13699911cf32dd
Jackson Tadie <jacksontadie@google.com>
2024-08-12 21:33:30 +0000

    Add "source" key to indicate origin of OOBE config for metrics
    Currently, we don't have a way in Chrome to distinguish between the two
    products (Remote Deployment, Flex Packaging Tool) that trigger
    token-based enrollment. To solve this, we add a "source" key to OOBE
    config, so each agent can pass along this info to Chrome.
    
    We add this info to EnrollmentConfig so it's accessible throughout the
    enrollment flow, specifically to be used in metrics (which will be
    added in a child CL).
    
    Change-Id: Ib543ab005041ebb97109d254ef309e729f3994ce
    Bug: b:355657383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5759298
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340621}

--- chrome/browser/ash/login/configuration_keys.h
+++ chrome/browser/ash/login/configuration_keys.h
@@ -38,6 +38,8 @@ extern const char kEnrollmentLocation[];
 extern const char kEnrollmentAutoAttributes[];
 extern const char kEnrollmentToken[];
 
+extern const char kSource[];
+
 enum class ConfigurationHandlerSide : unsigned int {
   HANDLER_JS,    // Handled by JS code
   HANDLER_CPP,   // Handled by C++ code
