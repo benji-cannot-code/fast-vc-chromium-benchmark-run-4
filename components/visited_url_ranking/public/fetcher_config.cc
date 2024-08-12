078935f6d72d9fcd8645cee28945da2eae4ed217
Roman Arora <romanarora@chromium.org>
2024-08-12 17:17:18 +0000

    Reland "[visited-url-ranking] Additional time based signals"
    This is a reland of commit 5230899ff9dbc8c3e118db603cb67904d7576e3f
    
    What changed:
    - Disabled a flaky test that will be fixed in a follow up CL.
    
    Original change's description:
    > [visited-url-ranking] Additional time based signals
    >
    > Low-Coverage-Reason: TRIVIAL_CHANGE
    > Bug: 356853232
    > Change-Id: Ic5ef2e12b73a2f9e45225570ccee55e846e306a4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757083
    > Reviewed-by: Siddhartha S <ssid@chromium.org>
    > Auto-Submit: Roman Arora <romanarora@chromium.org>
    > Commit-Queue: Siddhartha S <ssid@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1338578}
    
    Bug: 356853232
    Change-Id: Ia3ad373102e1a730182852724eaf37e8c0a169b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783774
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340460}

--- components/visited_url_ranking/public/fetcher_config.cc
+++ components/visited_url_ranking/public/fetcher_config.cc
@@ -5,12 +5,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/visited_url_ranking/public/fetcher_config.h"
 
+#include "base/logging.h"
+#include "base/time/clock.h"
 #include "components/url_deduplication/url_deduplication_helper.h"
 
 namespace visited_url_ranking {
 
+FetcherConfig::FetcherConfig(base::Clock* clock_arg) : clock(clock_arg) {
+  DCHECK(clock);
+}
+
 FetcherConfig::FetcherConfig(
-    url_deduplication::URLDeduplicationHelper* deduplication_helper)
-    : deduplication_helper(deduplication_helper) {}
+    url_deduplication::URLDeduplicationHelper* deduplication_helper,
+    base::Clock* clock_arg)
+    : deduplication_helper(deduplication_helper), clock(clock_arg) {
+  DCHECK(clock);
+}
 
 }  // namespace visited_url_ranking
