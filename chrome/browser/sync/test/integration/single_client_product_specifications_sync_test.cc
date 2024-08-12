ce4a1b4f06dff4e8ee63f7295b92dd6b0db902df
David Maunder <davidjm@google.com>
2024-08-12 21:08:07 +0000

    Add title support for ProductSpecificationsService
    Title support is being added to ProductSpecificationsService and this
    CL adds support to AddProductSpecifications and
    GetAllProductSpecifications(...). These APIs and
    ProductSpecificationsSet are updated to use UrlInfo instead of GURL
    to support these changes. These changes are put behind a flag
    kProductSpecificationsSyncTitle as the title changes are being
    phased in over several CLs. Final testing should only be completed
    when all changes are landed (at which point the flag will be
    enabled).
    
    Bug: 359191779
    Change-Id: I6ae04612348dee9be1adbd373136b8d7ddbea88b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5780179
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340605}

--- chrome/browser/sync/test/integration/single_client_product_specifications_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_product_specifications_sync_test.cc
@@ -103,6 +103,8 @@ MATCHER_P2(HasUnknownFields, uuid, unknown_fields, "") {
              unknown_fields;
 }
 
+// TODO(crbug.com/359225122) add integration test which includes
+// title.
 class SingleClientProductSpecificationsSyncTest : public SyncTest {
  public:
   SingleClientProductSpecificationsSyncTest() : SyncTest(SINGLE_CLIENT) {
@@ -174,7 +176,8 @@ IN_PROC_BROWSER_TEST_F(SingleClientProductSpecificationsSyncTest,
   commerce::ProductSpecificationsService* service = GetService();
 
   commerce::ProductSpecificationsSet expected_set(
-      kUuid, kCreationTimeEpochMillis, kUpdateTimeEpochMillis, {}, kName);
+      kUuid, kCreationTimeEpochMillis, kUpdateTimeEpochMillis,
+      std::vector<GURL>{}, kName);
   ASSERT_TRUE(
       commerce::ProductSpecificationsChecker(service, &expected_set).Wait());
 
