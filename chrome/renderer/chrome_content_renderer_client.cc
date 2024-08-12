ca27a27e51b22470ab798453a50730cb12aa9b75
Alex Moshchuk <alexmos@chromium.org>
2024-08-12 18:06:20 +0000

    Move PDF storage access fix to be behind the PDF enforcements feature.
    This is a followup to r1338897, which implemented a check in Blink to
    disable access to local/session storage in PDF renderers to prevent a
    browser-side renderer kill. It just moves the check to be behind the
    PDF enforcements feature flag (which is moved to content/public), so
    that this access check also has a kill switch, just in case.
    
    Bug: 357014503
    Change-Id: I3c53d4d88f32f9534def838bdf2a2519890b5bbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783756
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340488}

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -119,6 +119,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/web_cache/renderer/web_cache_impl.h"
 #include "components/webapps/renderer/web_page_metadata_agent.h"
 #include "content/public/common/content_constants.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/page_visibility_state.h"
 #include "content/public/common/url_constants.h"
@@ -865,6 +866,10 @@ bool ChromeContentRendererClient::IsPluginHandledExternally(
 }
 
 bool ChromeContentRendererClient::IsDomStorageDisabled() const {
+  if (!base::FeatureList::IsEnabled(features::kPdfEnforcements)) {
+    return false;
+  }
+
 #if BUILDFLAG(ENABLE_PDF) && BUILDFLAG(ENABLE_EXTENSIONS)
   // PDF renderers shouldn't need to access DOM storage interfaces. Note that
   // it's still possible to access localStorage or sessionStorage in a PDF
--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_features.h"
 #include "content/public/common/url_constants.h"
 #include "net/base/filename_util.h"
 #include "net/base/url_util.h"
