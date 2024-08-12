d451b6b4c88b7883796c9319266ca0bbaad1c23b
Tanushree Bisht <tanub@google.com>
2024-08-12 20:15:34 +0000

    [FPF] Move FPF RulesetService browser tests to their corresponding directory.
    This also creates a FingerprintingProtectionFilterBrowserTest class
    that will be used for future FPF browser tests.
    
    Also adds a new ruleset service browser test dependent on Incognito flag
    (kEnableFingerprintingProtectionFilterInIncognito).
    
    Bug: 358371545
    Change-Id: I5ed19e1291147cd065fcdb876eb1a3571b17d17f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769767
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Alex Turner <alexmt@chromium.org>
    Commit-Queue: Tanushree Bisht <tanub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340581}

---
+++ chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.h"
+
+FingerprintingProtectionFilterBrowserTest::
+    FingerprintingProtectionFilterBrowserTest() = default;
+
+FingerprintingProtectionFilterBrowserTest::
+    ~FingerprintingProtectionFilterBrowserTest() = default;
+
+void FingerprintingProtectionFilterBrowserTest::SetUp() {
+  PlatformBrowserTest::SetUp();
+}
+
+void FingerprintingProtectionFilterBrowserTest::TearDown() {
+  PlatformBrowserTest::TearDown();
+}
+
+void FingerprintingProtectionFilterBrowserTest::SetUpOnMainThread() {
+  PlatformBrowserTest::SetUpOnMainThread();
+}
---
+++ chrome/browser/fingerprinting_protection/fingerprinting_protection_filter_browser_test_harness.h
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_FINGERPRINTING_PROTECTION_FINGERPRINTING_PROTECTION_FILTER_BROWSER_TEST_HARNESS_H_
+#define CHROME_BROWSER_FINGERPRINTING_PROTECTION_FINGERPRINTING_PROTECTION_FILTER_BROWSER_TEST_HARNESS_H_
+
+#include "build/build_config.h"
+#include "chrome/browser/subresource_filter/subresource_filter_browser_test_harness.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/test/base/android/android_browser_test.h"
+#else
+#include "chrome/test/base/in_process_browser_test.h"
+#endif
+
+class FingerprintingProtectionFilterBrowserTest : public PlatformBrowserTest {
+ public:
+  FingerprintingProtectionFilterBrowserTest();
+
+  FingerprintingProtectionFilterBrowserTest(
+      const FingerprintingProtectionFilterBrowserTest&) = delete;
+  FingerprintingProtectionFilterBrowserTest& operator=(
+      const FingerprintingProtectionFilterBrowserTest&) = delete;
+
+  ~FingerprintingProtectionFilterBrowserTest() override;
+
+ protected:
+  // InProcessBrowserTest:
+  void SetUp() override;
+  void TearDown() override;
+  void SetUpOnMainThread() override;
+};
+
+#endif  // CHROME_BROWSER_FINGERPRINTING_PROTECTION_FINGERPRINTING_PROTECTION_FILTER_BROWSER_TEST_HARNESS_H_
