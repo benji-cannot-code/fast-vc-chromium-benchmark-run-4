9930d3008583c58df62289d074d5f2103418d1aa
Dave Tapuska <dtapuska@chromium.org>
2024-08-12 17:25:02 +0000

    Convert base::WriteFile
    Use variant that doesn't use unsafe buffers
    
    Bug: 41134632
    Change-Id: I58767363c62742510257d97c2177cd7cc5cee9fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777879
    Reviewed-by: Xiangdong Kong <xiangdongkong@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340469}

--- components/feedback/feedback_util_unittest.cc
+++ components/feedback/feedback_util_unittest.cc
@@ -36,7 +36,7 @@ class FeedbackUtilTest : public ::testing::Test {
 TEST_F(FeedbackUtilTest, ReadEndOfFileEmpty) {
   base::FilePath file_path = temp_dir_.GetPath().Append("test_empty.txt");
 
-  WriteFile(file_path, "", 0);
+  EXPECT_TRUE(WriteFile(file_path, ""));
 
   EXPECT_THAT(feedback_util::ReadEndOfFile(file_path, 10),
               testing::Optional(std::string()));
@@ -46,7 +46,7 @@ TEST_F(FeedbackUtilTest, ReadEndOfFileSmall) {
   const char kTestData[] = "0123456789";  // Length of 10
   base::FilePath file_path = temp_dir_.GetPath().Append("test_small.txt");
 
-  WriteFile(file_path, kTestData, strlen(kTestData));
+  EXPECT_TRUE(WriteFile(file_path, kTestData));
 
   EXPECT_THAT(feedback_util::ReadEndOfFile(file_path, 15),
               testing::Optional(std::string(kTestData)));
@@ -70,7 +70,7 @@ TEST_F(FeedbackUtilTest, ReadEndOfFileWithZeros) {
 
   base::FilePath file_path = temp_dir_.GetPath().Append("test_zero.txt");
 
-  WriteFile(file_path, test_data.data(), test_size);
+  EXPECT_TRUE(WriteFile(file_path, test_data));
 
   EXPECT_THAT(feedback_util::ReadEndOfFile(file_path, 15),
               testing::Optional(test_data));
@@ -95,7 +95,7 @@ TEST_F(FeedbackUtilTest, ReadEndOfFileMedium) {
 
   base::FilePath file_path = temp_dir_.GetPath().Append("test_med.txt");
 
-  WriteFile(file_path, test_data.data(), test_size);
+  EXPECT_TRUE(WriteFile(file_path, test_data));
 
   EXPECT_THAT(feedback_util::ReadEndOfFile(file_path, 15000),
               testing::Optional(test_data));
