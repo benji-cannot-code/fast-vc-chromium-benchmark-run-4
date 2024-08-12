ff5557b21f107454c2daa39ff9e465ba357ceebc
Brandon Wylie <wylieb@chromium.org>
2024-08-12 21:13:49 +0000

    [declutter] Block touch events when the archived tabs dialog animating
    Bug: 357875534
    Change-Id: I774ceca90ec754ca9b50c42b82d495a510b046d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777701
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340610}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/ArchivedTabsDialogCoordinator.java
@@ -61,6 +61,7 @@ import org.chromium.ui.modelutil.LayoutViewBuilder;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.util.TokenHolder;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -201,7 +202,7 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
             new TabListEditorCoordinator.LifecycleObserver() {
                 @Override
                 public void willHide() {
-                    mRecyclerView.removeOnScrollListener(mRecyclerScrollListener);
+                    mDialogRecyclerView.removeOnScrollListener(mRecyclerScrollListener);
                     mSnackbarManager.popParentViewFromOverrideStack(mSnackbarOverrideToken);
                     // In case we were hidden by TabListEditor in some other case, force the
                     // animation to finish.
@@ -245,7 +246,8 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
     private final @NonNull ViewGroup mTabSwitcherView;
     private final @NonNull FadingShadowView mShadowView;
 
-    private RecyclerView mRecyclerView;
+    private TabListRecyclerView mDialogRecyclerView;
+    private WeakReference<TabListRecyclerView> mTabSwitcherRecyclerView;
     private @TabActionState int mTabActionState = TabActionState.CLOSABLE;
     private TabListEditorCoordinator mTabListEditorCoordinator;
     private OnTabSelectingListener mOnTabSelectingListener;
@@ -338,6 +340,9 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
     public void show(OnTabSelectingListener onTabSelectingListener) {
         if (mIsShowing) return;
         mIsShowing = true;
+        mTabSwitcherRecyclerView =
+                new WeakReference<>(mTabSwitcherView.findViewById(R.id.tab_list_recycler_view));
+        mTabSwitcherRecyclerView.get().setBlockTouchInput(true);
 
         boolean tabListFirstShown = false;
         if (mTabListEditorCoordinator == null) {
@@ -357,9 +362,10 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
                 R.string.accessibility_archived_tabs_dialog,
                 R.string.accessibility_archived_tabs_dialog_back_button);
 
-        mRecyclerView = mDialogView.findViewById(R.id.tab_list_recycler_view);
-        mRecyclerView.addOnScrollListener(mRecyclerScrollListener);
-        mShadowView.setVisibility(mRecyclerView.canScrollVertically(1) ? View.VISIBLE : View.GONE);
+        mDialogRecyclerView = mDialogView.findViewById(R.id.tab_list_recycler_view);
+        mDialogRecyclerView.addOnScrollListener(mRecyclerScrollListener);
+        mShadowView.setVisibility(
+                mDialogRecyclerView.canScrollVertically(1) ? View.VISIBLE : View.GONE);
 
         // Register the dialog to handle back press events.
         mBackPressManager.addHandler(controller, BackPressHandler.Type.ARCHIVED_TABS_DIALOG);
@@ -418,6 +424,7 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
     }
 
     private void animateOut(int duration, Runnable animationFinishCallback) {
+        mDialogRecyclerView.setBlockTouchInput(true);
         // TODO(crbug.com/358430208): Use AnimatorSet here.
         mDialogView
                 .animate()
@@ -435,6 +442,7 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
                             @Override
                             public void onAnimationEnd(@NonNull Animator animator) {
                                 animationFinishCallback.run();
+                                mDialogRecyclerView.setBlockTouchInput(false);
                             }
                         })
                 .start();
@@ -459,6 +467,8 @@ public class ArchivedTabsDialogCoordinator implements SnackbarManager.SnackbarMa
         mArchivedTabModel.getTabCountSupplier().removeObserver(mTabCountObserver);
         mSnackbarOverrideToken = TokenHolder.INVALID_TOKEN;
         mIsShowing = false;
+        mTabSwitcherRecyclerView.get().setBlockTouchInput(false);
+        mTabSwitcherRecyclerView.clear();
     }
 
     void moveToState(@TabActionState int tabActionState) {
