a99c76ea70e664314cc1a9a32aa05aec852eae48
Christine Hollingsworth <christinesm@chromium.org>
2024-08-12 15:41:34 +0000

    [FSA] Support change types on Mac
    Implementation for supporting change types (recursive, non-
    recursive) via `WatchWithChangeInfo` on Mac, using FSEvents.
    
    The test cases that are disabled in this change will be enabled
    in follow-up CLs.
    
    Bug: 321980270
    Change-Id: I49499b9519a22097e2bb7a52ce6c7376ba1ab197
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594288
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340411}

--- content/browser/file_system_access/file_path_watcher/file_path_watcher.h
+++ content/browser/file_system_access/file_path_watcher/file_path_watcher.h
@@ -126,7 +126,7 @@ class CONTENT_EXPORT FilePathWatcher {
     Type type = Type::kNonRecursive;
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_ANDROID) || \
-    BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_WIN)
+    BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
     // The callback will return the full path to a changed file instead of
     // the watched path supplied as |path| when Watch is called.
     // So the full path can be different from the watched path when a folder is
@@ -134,6 +134,7 @@ class CONTENT_EXPORT FilePathWatcher {
     bool report_modified_path = false;
 #endif  // BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS) ||
         // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_WIN)
+        // || BUILDFLAG(IS_MAC)
   };
 
   // Callback type for Watch(). |path| points to the file that was updated,
