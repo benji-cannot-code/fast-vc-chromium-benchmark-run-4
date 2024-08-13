94375e99f5e4558eb90f7477dbe91b78f6ea894d
Jason Zhang <jiajunz@google.com>
2024-08-13 03:32:23 +0000

    [CrOS Passpoint] Add fake passpoint implementation for testing
    Adds the fake passpoint service so that we could use for the interactive
    ui tests.
    
    Bug: b/356717790
    Change-Id: Iad0cf237080b6d75b539864ea696ac6481db3f34
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5772554
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340779}

--- chromeos/ash/services/connectivity/public/cpp/connectivity_services.cc
+++ chromeos/ash/services/connectivity/public/cpp/connectivity_services.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "chromeos/ash/components/mojo_service_manager/connection.h"
+#include "chromeos/ash/services/connectivity/public/cpp/fake_passpoint_service.h"
 #include "third_party/cros_system_api/mojo/service_constants.h"
 
 namespace ash::connectivity {
@@ -16,9 +17,16 @@ namespace ash::connectivity {
 void BindToPasspointService(
     mojo::PendingReceiver<chromeos::connectivity::mojom::PasspointService>
         receiver) {
+#if !defined(USE_REAL_DBUS_CLIENTS)
+  if (!FakePasspointService::IsInitialized()) {
+    FakePasspointService::Initialize();
+  }
+  FakePasspointService::Get()->BindPendingReceiver(std::move(receiver));
+#else
   mojo_service_manager::GetServiceManagerProxy()->Request(
       chromeos::mojo_services::kCrosPasspointService, std::nullopt,
       std::move(receiver).PassPipe());
+#endif  // !defined(USE_REAL_DBUS_CLIENTS)
 }
 
 }  // namespace ash::connectivity
---
+++ chromeos/ash/services/connectivity/public/cpp/fake_passpoint_service.h
@@ -0,0 +1,73 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_SERVICES_CONNECTIVITY_PUBLIC_CPP_FAKE_PASSPOINT_SERVICE_H_
+#define CHROMEOS_ASH_SERVICES_CONNECTIVITY_PUBLIC_CPP_FAKE_PASSPOINT_SERVICE_H_
+
+#include "base/containers/flat_map.h"
+#include "chromeos/ash/services/connectivity/public/mojom/passpoint.mojom.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+#include "mojo/public/cpp/bindings/receiver_set.h"
+#include "mojo/public/cpp/bindings/remote.h"
+#include "mojo/public/cpp/bindings/remote_set.h"
+
+namespace ash::connectivity {
+
+class FakePasspointSubscription;
+
+// Implements a fake version of the PasspointService mojo interface for testing
+// purpose.
+class FakePasspointService
+    : public chromeos::connectivity::mojom::PasspointService {
+ public:
+  // Creates the global instance with a fake implementation.
+  static void Initialize();
+  static bool IsInitialized();
+  static void Shutdown();
+  static FakePasspointService* Get();
+
+  FakePasspointService(const FakePasspointService&) = delete;
+  FakePasspointService& operator=(const FakePasspointService&) = delete;
+
+  // chromeos::connectivity::mojom::PasspointService
+  void GetPasspointSubscription(
+      const std::string& id,
+      GetPasspointSubscriptionCallback callback) override;
+  void ListPasspointSubscriptions(
+      ListPasspointSubscriptionsCallback callback) override;
+  void DeletePasspointSubscription(
+      const std::string& id,
+      DeletePasspointSubscriptionCallback callback) override;
+  void RegisterPasspointListener(
+      mojo::PendingRemote<
+          chromeos::connectivity::mojom::PasspointEventsListener> listener)
+      override;
+
+  // Binds a PendingReceiver to this instance.
+  void BindPendingReceiver(
+      mojo::PendingReceiver<chromeos::connectivity::mojom::PasspointService>
+          pending_receiver);
+
+  void AddFakePasspointSubscription(
+      FakePasspointSubscription fake_subscription);
+
+ private:
+  FakePasspointService();
+  ~FakePasspointService() override;
+
+  void NotifyListenersSubscriptionAdded(const std::string& id);
+  void NotifyListenersSubscriptionRemoved(const std::string& id);
+
+  base::flat_map<std::string, FakePasspointSubscription>
+      id_to_subscription_map_;
+
+  mojo::RemoteSet<chromeos::connectivity::mojom::PasspointEventsListener>
+      listeners_;
+  mojo::ReceiverSet<chromeos::connectivity::mojom::PasspointService> receivers_;
+};
+
+}  // namespace ash::connectivity
+
+#endif  // CHROMEOS_ASH_SERVICES_CONNECTIVITY_PUBLIC_CPP_FAKE_PASSPOINT_SERVICE_H_
