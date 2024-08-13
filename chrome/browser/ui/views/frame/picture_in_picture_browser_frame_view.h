327be3dfc69987c74a84aaca77916bcb4a69fd16
Benjamin Keen <bkeen@google.com>
2024-08-13 20:21:53 +0000

    Fix Document Pip title bar icon animation fade order
    This change updates the title bar animation to fade the close and back
    to tab (if present) buttons at the same time, when there are no content
    setting views present.
    
    Bug: 355226218
    Change-Id: I047a11e3b82176dbada9dbfcd4d56ab2e89f2f8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769865
    Reviewed-by: Tommy Steimel <steimel@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341221}

--- chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
+++ chrome/browser/ui/views/frame/picture_in_picture_browser_frame_view.h
@@ -209,6 +209,9 @@ class PictureInPictureBrowserFrameView
   gfx::Insets GetClientAreaInsets(HMONITOR monitor) const;
 #endif
 
+  // Returns true if `content_setting_views_` has any visible views.
+  bool HasAnyVisibleContentSettingViews() const;
+
   // Helper functions for testing.
   std::vector<gfx::Animation*> GetRenderActiveAnimationsForTesting();
   std::vector<gfx::Animation*> GetRenderInactiveAnimationsForTesting();
@@ -367,6 +370,8 @@ class PictureInPictureBrowserFrameView
   gfx::MultiAnimation hide_back_to_tab_button_animation_;
   gfx::MultiAnimation show_close_button_animation_;
   gfx::MultiAnimation hide_close_button_animation_;
+  gfx::LinearAnimation show_all_buttons_animation_;
+  gfx::LinearAnimation hide_all_buttons_animation_;
 
   // The foreground color given the current state of the
   // `top_bar_color_animation_`.
