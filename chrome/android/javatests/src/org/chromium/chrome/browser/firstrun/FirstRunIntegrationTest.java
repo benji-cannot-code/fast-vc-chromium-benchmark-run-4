dcee349d2e47f2384e46df2c02f85088fb75f367
Menghan Yang <myuu@google.com>
2024-08-13 11:20:34 +0000

    [UNOp2][Android] Merge TabbedModeFirstRunActivity with FirstRunActivity
    `TabbedModeFirstRunActivity` was used on tablet and Android Auto
    to use a different theme to avoid visual glitch, and to wrap the
    FRE in a custom layout to mimic `DialogWhenLarge` behavior.
    
    With the change in https://crrev.com/c/5741209,
    `TabbedModeFirstRunActivity` became tiny, and it doesn't seem relevant
    to keep it as a separate activity anymore.
    Also, the custom behaviour implemented by
    `DialogWhenLargeContentLayout` seems able to replace the
    `DialogWhenLarge` theme, used on small and large (e.g. foldable)
    phones to makes the UI more adaptable in the future.
    
    This CL merge `TabbedModeFirstRunActivity` into `FirstRunActivity` to
    improve consistency and simplify the display logic, by:
    
    * Programmatically changing the theme on tablet and auto
    * Changing the `FirstRunActivity` theme from
    `Theme.Chromium.DialogWhenLarge` to
    `Theme.Chromium.Activity.Fullscreen` (used by the new sign-in flow)
    and always wrapping the FRE content view in
    `DialogWhenLargeContentLayout` instead.
    
    See https://crbug.com/347597920#comment7 for recordings.
    
    Bug: 347597920, 349787455
    Fixed: 346521033
    Change-Id: I39d47117225927ae6300282f3ac12ae989e5792e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776806
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340894}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunIntegrationTest.java
@@ -94,7 +94,6 @@ import org.chromium.components.signin.AccountManagerFacadeProvider;
 import org.chromium.components.signin.base.CoreAccountInfo;
 import org.chromium.components.signin.test.util.FakeAccountManagerDelegate;
 import org.chromium.content_public.common.ContentUrlConstants;
-import org.chromium.ui.base.DeviceFormFactor;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -129,7 +128,6 @@ public class FirstRunIntegrationTest {
             CollectionUtil.newHashSet(
                     ChromeLauncherActivity.class,
                     FirstRunActivity.class,
-                    TabbedModeFirstRunActivity.class,
                     ChromeTabbedActivity.class,
                     CustomTabActivity.class);
     private final Map<Class, ActivityMonitor> mMonitorMap = new HashMap<>();
@@ -138,7 +136,6 @@ public class FirstRunIntegrationTest {
 
     private FirstRunActivityTestObserver mTestObserver = new FirstRunActivityTestObserver();
     private Activity mLastActivity;
-    private Class mFirstRunActivityClass;
 
     @Before
     public void setUp() {
@@ -156,10 +153,6 @@ public class FirstRunIntegrationTest {
 
         mInstrumentation = InstrumentationRegistry.getInstrumentation();
         mContext = mInstrumentation.getTargetContext();
-        mFirstRunActivityClass =
-                DeviceFormFactor.isTablet()
-                        ? TabbedModeFirstRunActivity.class
-                        : FirstRunActivity.class;
         for (Class clazz : mSupportedActivities) {
             ActivityMonitor monitor = new ActivityMonitor(clazz.getName(), null, false);
             mMonitorMap.put(clazz, monitor);
@@ -284,7 +277,7 @@ public class FirstRunIntegrationTest {
     }
 
     private FirstRunActivity waitForFirstRunActivity() {
-        return (FirstRunActivity) waitForActivity(mFirstRunActivityClass);
+        return (FirstRunActivity) waitForActivity(FirstRunActivity.class);
     }
 
     /**
@@ -300,7 +293,7 @@ public class FirstRunIntegrationTest {
                     for (Activity runningActivity : ApplicationStatus.getRunningActivities()) {
                         @ActivityState
                         int state = ApplicationStatus.getStateForActivity(runningActivity);
-                        if (runningActivity.getClass() == mFirstRunActivityClass
+                        if (runningActivity.getClass() == FirstRunActivity.class
                                 && runningActivity != previousFreActivity
                                 && (state == ActivityState.STARTED
                                         || state == ActivityState.RESUMED)) {
@@ -377,7 +370,7 @@ public class FirstRunIntegrationTest {
         Assert.assertFalse(mLastActivity.isFinishing());
 
         // First run should be skipped for this Activity.
-        Assert.assertEquals(0, getMonitor(mFirstRunActivityClass).getHits());
+        Assert.assertEquals(0, getMonitor(FirstRunActivity.class).getHits());
     }
 
     @Test
