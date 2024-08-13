656a9882f55a2decb949ef9a09eacf518746f4c2
Solomon Kinard <solomonkinard@chromium.org>
2024-08-13 18:51:22 +0000

    Extensions: WAR: GUID: Added a test for page initiated redirection
    This version is content script free, unlike crrev.com/c/5766980. Script
    and page based redirection from web request are being exercised here to
    ensure that specific code won't be added that would cause an inadvertent
    crash. In the case of the test that redirects from page, it's only being
    added as a complement of the content script test. However, it doesn't
    test for the crash scenario.
    
    The TODOs could have been deleted without adding this test, but it's
    being added now because it can't hurt. Due to this addition, the
    pre-existing test for a renderer initiated redirect has been moved
    accordingly.
    
    For example, the todo to create a test for guid based on
    accessible_link_resource.html;drc=9a60d160b6dfb2351ae0dad28341c3ca80f1c.
    has been removed since it was introduced in crrev.org/r/22935006 with
    ExtensionResourceRequestPolicyTest.LinkToWebAccessibleResources.
    
    Bug: chromium:352267920
    Change-Id: If26d84f61ebc56c87d883d57bb55445e7aad5587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777900
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341152}

---
+++ chrome/test/data/extensions/web_accessible_resources/web_request/redirect_from_page/background.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.webRequest.onBeforeRequest.addListener(details => {
+  if (details.url.endsWith('test.js')) {
+    return {redirectUrl: chrome.runtime.getURL('war.js')};
+  }
+}, {urls: ['<all_urls>']}, ['blocking']);
+
+chrome.test.sendMessage('ready');
--- chrome/test/data/extensions/web_accessible_resources/web_request/redirect_from_script/content_script.js
+++ chrome/test/data/extensions/web_accessible_resources/web_request/redirect_from_script/content_script.js
@@ -5,4 +5,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const script = document.createElement('script');
 document.body.appendChild(script);
-script.src = 'http://example.org/redirect.js';
+script.src = '/redirect.js';
