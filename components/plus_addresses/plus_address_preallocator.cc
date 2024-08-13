f197ad0f09f52085aeffb229adff5f1234569def
Jan Keitel <jkeitel@google.com>
2024-08-13 13:57:43 +0000

    Only use PlusAddressSettingService if features are enabled.
    It is possible that we roll out pre-allocation before global settings
    are supported. In that case, we should ignore what the setting service
    has to say.
    
    Bug: 324559503
    Change-Id: I3fa58adce72ca980d17f6a155387a6e4ad32effe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782864
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Kirubel Aklilu <kaklilu@chromium.org>
    Commit-Queue: Kirubel Aklilu <kaklilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340948}

--- components/plus_addresses/plus_address_preallocator.cc
+++ components/plus_addresses/plus_address_preallocator.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_deref.h"
+#include "base/feature_list.h"
 #include "base/json/values_util.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/values.h"
@@ -73,7 +74,9 @@ PlusAddressPreallocator::PlusAddressPreallocator(
   PrunePreallocatedPlusAddresses();
 
   // If the notice has not been accepted, we do not preemptively pre-allocate.
-  if (settings_->GetHasAcceptedNotice()) {
+  if (settings_->GetHasAcceptedNotice() ||
+      !base::FeatureList::IsEnabled(
+          features::kPlusAddressUserOnboardingEnabled)) {
     base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
         FROM_HERE,
         base::BindOnce(
@@ -145,7 +148,8 @@ void PlusAddressPreallocator::MaybeRequestNewPreallocatedPlusAddresses() {
     return;
   }
 
-  if (!settings_->GetIsPlusAddressesEnabled()) {
+  if (base::FeatureList::IsEnabled(features::kPlusAddressGlobalToggle) &&
+      !settings_->GetIsPlusAddressesEnabled()) {
     return;
   }
 
--- components/plus_addresses/plus_address_preallocator_unittest.cc
+++ components/plus_addresses/plus_address_preallocator_unittest.cc
@@ -85,8 +85,13 @@ class PlusAddressPreallocatorTest : public ::testing::Test {
  public:
   PlusAddressPreallocatorTest() {
     prefs::RegisterProfilePrefs(pref_service_.registry());
-    // By default, assume that the notice has been accepted and plus addresses
-    // are enabled.
+    feature_list_.InitWithFeatures(
+        /*enabled_features=*/{features::kPlusAddressUserOnboardingEnabled,
+                              features::kPlusAddressGlobalToggle},
+        /*disabled_features=*/{});
+
+    // By default, assume that the notice has been accepted and plus
+    // addresses are enabled.
     setting_service().set_has_accepted_notice(true);
     setting_service().set_is_plus_addresses_enabled(true);
     // Forward to make sure we can subtract from `base::Time::Now()` without
@@ -115,6 +120,7 @@ class PlusAddressPreallocatorTest : public ::testing::Test {
   }
 
  private:
+  base::test::ScopedFeatureList feature_list_;
   base::test::TaskEnvironment task_environment_{
       base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   TestingPrefServiceSimple pref_service_;
