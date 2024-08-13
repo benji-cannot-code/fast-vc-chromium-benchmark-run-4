94a3983d03b598401a6d1d2c4c6973554e9b8bf1
Alex N. Jose <alexnj@chromium.org>
2024-08-13 21:36:07 +0000

    Reland "Implement graceful removal of an emulated Bluetooth adapter"
    This is a reland of commit 3770ef2ce98c16faf64fa91fa01651c1f75e6a44
    
    Original change's description:
    > Implement graceful removal of an emulated Bluetooth adapter
    >
    > This CL implements a graceful removal of a Bluetooth override adapter
    > that would restore the original adapter that was in place before
    > the override. The change is made to better support the newly created
    > CDP Bluetooth domain's `disable` operation. It achieves this by
    > maintaining the override adapter separately, and introducing
    > a `GetActiveAdapter()` that switches between real and override
    > adapters for most operations.
    >
    > Bug: 342191615
    > Change-Id: I7ccafe62eb0bdccaeaee94c98ac2b85619ad799c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5765953
    > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1339927}
    
    Bug: 342191615
    Change-Id: I3e5fc8a75033be8f554aac6035dfeb9639aea397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785058
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Alex N. Jose <alexnj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341281}

--- content/browser/bluetooth/web_bluetooth_service_impl.h
+++ content/browser/bluetooth/web_bluetooth_service_impl.h
@@ -166,6 +166,8 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
                            TwoWatchAdvertisementsReqFail);
   FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
                            SecWatchAdvertisementsReqAfterFirstSuccess);
+  FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTestWithBaseAdapter,
+                           EmulatedAdapterRemovalRestoresOriginalAdapter);
 
 #if PAIR_BLUETOOTH_ON_DEMAND()
   FRIEND_TEST_ALL_PREFIXES(WebBluetoothServiceImplTest,
@@ -176,6 +178,8 @@ class CONTENT_EXPORT WebBluetoothServiceImpl
 
   friend class FrameConnectedBluetoothDevicesTest;
   friend class WebBluetoothServiceImplTest;
+  friend class WebBluetoothServiceImplTestWithBaseAdapter;
+
   using PrimaryServicesRequestCallback =
       base::OnceCallback<void(device::BluetoothDevice*)>;
   using ScanFilters = std::vector<blink::mojom::WebBluetoothLeScanFilterPtr>;
