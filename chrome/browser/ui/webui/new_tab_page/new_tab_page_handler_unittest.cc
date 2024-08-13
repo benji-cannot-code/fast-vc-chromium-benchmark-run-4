ee39f4a93d45bc58b9153861ca972d57376538a0
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-08-13 12:04:58 +0000

    Revert "[User Education] Move NTP promos to auto-configuration."
    This reverts commit fd02f7bf81ed03a0b4ee0d3c493efaa5116178bd.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5641302646456320
    
    Sample build with failed test: https://ci.chromium.org/b/8739784933850014513
    Affected test(s):
    [ninja://chrome/test:unit_tests/NewTabPageFeaturePromoHelperTest.RecordFeatureUsage_CustomizeChrome](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fchrome%2Ftest:unit_tests%2FNewTabPageFeaturePromoHelperTest.RecordFeatureUsage_CustomizeChrome?q=VHash%3A71dfa4f93ed3fdfc)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5641302646456320&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5773736&type=BUG
    
    Original change's description:
    > [User Education] Move NTP promos to auto-configuration.
    >
    > This removes hard-coded configuration from desktop NTP customization
    > promos and switches to using "promo feature used" calls.
    >
    > The old events are added to the promo specifications to respect users
    > who already used the feature under the old configuration, but are
    > otherwise no longer used.
    >
    > Change-Id: Iae9d4823012e6fe8f12b0d222db275ae118637a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5773736
    > Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Dana Fried <dfried@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1340524}
    >
    
    Change-Id: I89081bb56e81712b9a2ea41c99cbfea5e106054f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783470
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Reviewed-by: Thomas Nguyen <tungnh@chromium.org>
    Owners-Override: Thomas Nguyen <tungnh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340908}

--- chrome/browser/ui/webui/new_tab_page/new_tab_page_handler_unittest.cc
+++ chrome/browser/ui/webui/new_tab_page/new_tab_page_handler_unittest.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/feature_engagement/public/event_constants.h"
-#include "components/feature_engagement/public/feature_constants.h"
 #include "components/feature_engagement/test/scoped_iph_feature_list.h"
 #include "components/keyed_service/content/browser_context_keyed_service_factory.h"
 #include "components/keyed_service/core/keyed_service.h"
@@ -225,8 +224,8 @@ class MockCustomizeChromeTabHelper
 class MockFeaturePromoHelper : public NewTabPageFeaturePromoHelper {
  public:
   MOCK_METHOD(void,
-              RecordPromoFeatureUsage,
-              (const base::Feature& feature, content::WebContents*),
+              RecordFeatureUsage,
+              (const std::string& event, content::WebContents*),
               (override));
   MOCK_METHOD(void,
               MaybeShowFeaturePromo,
@@ -996,30 +995,10 @@ TEST_F(NewTabPageHandlerTest, OpenSidePanel) {
                                testing::SaveArg<1>(&section)));
   EXPECT_CALL(
       *mock_feature_promo_helper_,
-      RecordPromoFeatureUsage(
-          testing::Ref(feature_engagement::kIPHDesktopCustomizeChromeFeature),
-          web_contents_.get()))
+      RecordFeatureUsage(feature_engagement::events::kCustomizeChromeOpened,
+                         web_contents_.get()))
       .Times(1);
-  EXPECT_CALL(
-      *mock_feature_promo_helper_,
-      RecordPromoFeatureUsage(
-          testing::Ref(
-              feature_engagement::kIPHDesktopCustomizeChromeRefreshFeature),
-          web_contents_.get()))
-      .Times(1);
-  EXPECT_CALL(
-      *mock_feature_promo_helper_,
-      CloseFeaturePromo(
-          testing::Ref(feature_engagement::kIPHDesktopCustomizeChromeFeature),
-          web_contents_.get()))
-      .Times(1);
-  EXPECT_CALL(
-      *mock_feature_promo_helper_,
-      CloseFeaturePromo(
-          testing::Ref(
-              feature_engagement::kIPHDesktopCustomizeChromeRefreshFeature),
-          web_contents_.get()))
-      .Times(1);
+  EXPECT_CALL(*mock_feature_promo_helper_, CloseFeaturePromo).Times(1);
 
   handler_->SetCustomizeChromeSidePanelVisible(
       /*visible=*/true,
@@ -1031,7 +1010,7 @@ TEST_F(NewTabPageHandlerTest, OpenSidePanel) {
 
 TEST_F(NewTabPageHandlerTest, CloseSidePanel) {
   EXPECT_CALL(*mock_customize_chrome_tab_helper_, CloseSidePanel).Times(1);
-  EXPECT_CALL(*mock_feature_promo_helper_, RecordPromoFeatureUsage).Times(0);
+  EXPECT_CALL(*mock_feature_promo_helper_, RecordFeatureUsage).Times(0);
   EXPECT_CALL(*mock_feature_promo_helper_, CloseFeaturePromo).Times(0);
 
   handler_->SetCustomizeChromeSidePanelVisible(
@@ -1130,10 +1109,8 @@ TEST_F(NewTabPageHandlerTest,
 TEST_F(NewTabPageHandlerTest, OnModuleUsedRecordFeatureUsageAndClosePromo) {
   EXPECT_CALL(
       *mock_feature_promo_helper_,
-      RecordPromoFeatureUsage(
-          testing::Ref(
-              feature_engagement::kIPHDesktopNewTabPageModulesCustomizeFeature),
-          web_contents_.get()))
+      RecordFeatureUsage(feature_engagement::events::kDesktopNTPModuleUsed,
+                         web_contents_.get()))
       .Times(1);
   EXPECT_CALL(*mock_feature_promo_helper_, CloseFeaturePromo).Times(1);
 
