6ebaa73012abc48ae3c5551b1cfe68ff77348cd8
Jeremie Boulic <jboulic@chromium.org>
2024-08-13 02:00:44 +0000

    Revert "Prevent ui::PaintCache from being used by multiple paint recorders"
    This reverts commit 89ed9842cbe119cd403674cef55da19b066ead82.
    
    Reason for revert: The following bot has been failing consistently since this change has landed
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-chrome-dchecks
    Testhaus link associated with one of those runs:
    https://tests.chromeos.goog/p/chromeos/logs/unified/chromeos-test-logs/test-runner/prod/2024-08-12/43346bcb-86a4-4643-a167-9f113f65a2e3?accountId=1&treeIgnoreCase=true&selectedLogs=cros-test-2f958e1c%2Fcros-test%2Fresults%2Ftauto%2Fresults-1-tast.chrome-from-gcs%2Ftast%2Fresults%2Ftests%2Fa11y.Smoke%2Fchrome.log&treeQuery=Live
    "a11y.LiveCaption" for example include Chrome logs with "[paint_cache.cc(38)] Check failed: *recorder_location_ == location..."
    Most other tests don't include Chrome logs, but it does seem like Chrome crashed, which would be likely to also be related to the
    reverted change.
    
    Original change's description:
    > Prevent ui::PaintCache from being used by multiple paint recorders
    >
    > If a ui::PaintCache is used by two paint recorders, one of them might
    > think it has a cache while the cache actually stores the draw OPs from
    > the other recorder. This patch prevents this from happening with a DCHECK. See a bug caused by this issue in https://crrev.com/c/5778160.
    >
    > Bug: None
    > Change-Id: I014c04ae548aa226ae65088538578a644b667fd4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5778320
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1339995}
    
    Bug: None
    Change-Id: I585adb0b48f8b15727b661f093bb689c80af48cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785273
    Commit-Queue: Jeremie Boulic <jboulic@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jeremie Boulic <jboulic@chromium.org>
    Reviewed-by: Wenbo Jie <wenbojie@chromium.org>
    Owners-Override: Jeremie Boulic <jboulic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340762}

--- ui/compositor/paint_recorder.h
+++ ui/compositor/paint_recorder.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_COMPOSITOR_PAINT_RECORDER_H_
 #define UI_COMPOSITOR_PAINT_RECORDER_H_
 
-#include "base/location.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ref.h"
 #include "cc/paint/record_paint_canvas.h"
@@ -34,8 +33,7 @@ class COMPOSITOR_EXPORT PaintRecorder {
                 const gfx::Size& recording_size,
                 float recording_scale_x,
                 float recording_scale_y,
-                PaintCache* cache,
-                const base::Location& from_here = FROM_HERE);
+                PaintCache* cache);
   PaintRecorder(const PaintContext& context, const gfx::Size& recording_size);
 
   PaintRecorder(const PaintRecorder&) = delete;
