e1f375676c40b868485de2e5980c42dffc528032
Aliona DANGLA <alionadangla@google.com>
2024-08-13 15:32:49 +0000

    [iOS] Replace chrome_browser_state_manager.h imports
    This CL update chrome_browser_state_manager.h imports to use
    profile_manager_ios.h header instead.
    
    This CL was uploaded by git cl split.
    
    Bug: 358072368
    Change-Id: Ice5dd1d8e7caee5f92452b83e46dd538e67c8dc2
    Fixed: 359176329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782903
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340997}

--- ios/chrome/browser/translate/model/translate_ranker_metrics_provider.mm
+++ ios/chrome/browser/translate/model/translate_ranker_metrics_provider.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/translate/core/browser/translate_ranker.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
 #import "ios/chrome/browser/translate/model/translate_ranker_factory.h"
 #import "ios/web/public/browser_state.h"
 #import "third_party/metrics_proto/chrome_user_metrics_extension.pb.h"
