138c6fba3352eb4c1479b516fd07f28873d3d6e5
Edman Anjos <edmanp@google.com>
2024-08-13 15:48:36 +0000

    kiosk: Rename kiosk_profile_loader to load_profile
    The KioskProfileLoader class became an implementation detail defined in
    the anonymous namespace in the .cc. Rename the file to better reflect
    the current implementation.
    
    Bug: 290172046
    Change-Id: I344d27fa75bb852dc5f2aad0c4269ec529bf3767
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783094
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341010}

--- chrome/browser/ash/app_mode/load_profile.h
+++ chrome/browser/ash/app_mode/load_profile.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_APP_MODE_KIOSK_PROFILE_LOADER_H_
-#define CHROME_BROWSER_ASH_APP_MODE_KIOSK_PROFILE_LOADER_H_
+#ifndef CHROME_BROWSER_ASH_APP_MODE_LOAD_PROFILE_H_
+#define CHROME_BROWSER_ASH_APP_MODE_LOAD_PROFILE_H_
 
 #include <memory>
 
@@ -53,4 +53,4 @@ using LoadProfileCallback = base::OnceCallback<decltype(LoadProfile)>;
 
 }  // namespace ash::kiosk
 
-#endif  // CHROME_BROWSER_ASH_APP_MODE_KIOSK_PROFILE_LOADER_H_
+#endif  // CHROME_BROWSER_ASH_APP_MODE_LOAD_PROFILE_H_
