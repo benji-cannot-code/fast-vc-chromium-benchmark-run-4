2fcbfacb03e64f7c70942199b9004f8ef4086fae
Theresa <twellington@chromium.org>
2024-08-13 21:36:27 +0000

    [E2E] Register synthetic fieldtrial for edge to edge eligibility
    Many of the edge-to-edge feature flags are cached, so the experiment
    group data will contain metrics from all clients. Register a synthetic
    fieldtrial so that experiment data can be filtered to only clients that
    are on a device configuration that supports the chin / drawing edge to
    edge.
    
    Bug: 359600223
    Change-Id: I6c369d43c0a2cc9278542ca6fbbde402041901b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786630
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341282}

--- chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
+++ chrome/browser/ui/android/edge_to_edge/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeUtils.java
@@ -100,8 +100,9 @@ public class EdgeToEdgeUtils {
      * why it is ineligible.
      *
      * @param activity The current active activity.
+     * @return Whether the activity is eligible for edge to edge based on device configuration.
      */
-    public static void recordEligibility(@NonNull Activity activity) {
+    public static boolean recordEligibility(@NonNull Activity activity) {
         boolean eligible = true;
 
         if (hasTappableBottomBar(activity.getWindow())) {
@@ -136,6 +137,8 @@ public class EdgeToEdgeUtils {
                     IneligibilityReason.NUM_TYPES);
         }
         RecordHistogram.recordBooleanHistogram(ELIGIBLE_HISTOGRAM, eligible);
+
+        return eligible;
     }
 
     /**
