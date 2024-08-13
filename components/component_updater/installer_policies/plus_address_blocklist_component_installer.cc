30b13dd7db3b307b4a483699ff3145c113241142
Norge Vizcay <vizcay@google.com>
2024-08-13 13:51:53 +0000

    Run populate on the original sequence.
    The CL makes sure that populating the plus address blocklist data
    happens from the UI thread.
    
    Bug: 324556906
    Change-Id: I51e2cb27bba8b33c3b9cada2c37ddb63e30bea46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785893
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340947}

--- components/component_updater/installer_policies/plus_address_blocklist_component_installer.cc
+++ components/component_updater/installer_policies/plus_address_blocklist_component_installer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/component_updater/installer_policies/plus_address_blocklist_component_installer.h"
 
 #include <memory>
+#include <optional>
 #include <vector>
 
 #include "base/feature_list.h"
@@ -34,15 +35,24 @@ base::FilePath GetInstalledPath(const base::FilePath& base) {
   return base.Append(kPlusAddressBlocklistBinaryPbFileName);
 }
 
-void LoadPlusAddressBlocklistFromDisk(const base::FilePath& pb_path) {
+std::optional<std::string> LoadPlusAddressBlocklistFromDisk(
+    const base::FilePath& pb_path) {
   std::string binary_pb;
   if (!base::ReadFileToString(pb_path, &binary_pb)) {
     DVLOG(1) << "Failed reading from " << pb_path.value();
-    return;
+    return std::nullopt;
   }
 
+  return binary_pb;
+}
+
+void PopulatePlusAddressBlocklistData(const base::FilePath& pb_path,
+                                      std::optional<std::string> binary_pb) {
+  if (!binary_pb) {
+    return;
+  }
   bool parsing_result = plus_addresses::PlusAddressBlocklistData::GetInstance()
-                            .PopulateDataFromComponent(binary_pb);
+                            .PopulateDataFromComponent(std::move(*binary_pb));
   if (!parsing_result) {
     DVLOG(1) << "Failed parsing proto " << pb_path.value();
     return;
@@ -81,10 +91,12 @@ void PlusAddressBlocklistInstallerPolicy::ComponentReady(
     base::Value::Dict /* manifest */) {
   VLOG(1) << "Component ready, version " << version.GetString() << " in "
           << install_dir.value();
-  base::ThreadPool::PostTask(
+  const base::FilePath pb_path = GetInstalledPath(install_dir);
+
+  base::ThreadPool::PostTaskAndReplyWithResult(
       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
-      base::BindOnce(&LoadPlusAddressBlocklistFromDisk,
-                     GetInstalledPath(install_dir)));
+      base::BindOnce(&LoadPlusAddressBlocklistFromDisk, pb_path),
+      base::BindOnce(&PopulatePlusAddressBlocklistData, pb_path));
 }
 
 // Called during startup and installation before ComponentReady().
