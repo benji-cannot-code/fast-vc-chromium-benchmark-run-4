1a957d4c44003960cb5c6843c364509813bc7e3b
Dominik Röttsches <drott@chromium.org>
2024-08-13 08:45:09 +0000

    Roll Fontations crates to new major versions in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * font-types: 0.5.5 => 0.6.0
    * read-fonts: 0.19.3 => 0.20.0
    * skrifa: 0.19.3 => 0.20.0
    
    A new extra_src_roots was added for Skrifa to account
    for generated files in that crate.
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * font-types@0.6.0, read-fonts@0.20.0, skrifa@0.20.0: crypto-safe,
      safe-to-deploy, ub-risk-2
    
    Bug: chromium:40045339
    Change-Id: I1fc08255ab91fbedc82c494694a9bfdd90bb1cec
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775924
    Reviewed-by: Ben Wagner <bungeman@google.com>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1340858}

---
+++ third_party/rust/chromium_crates_io/vendor/skrifa-0.20.0/src/outline/autohint/mod.rs
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+//! Runtime autohinting support.
+
+// Remove when the work is complete.
+#![allow(dead_code)]
+
+mod cycling;
+mod latin;
+mod metrics;
+mod outline;
+mod script;
+mod style;
