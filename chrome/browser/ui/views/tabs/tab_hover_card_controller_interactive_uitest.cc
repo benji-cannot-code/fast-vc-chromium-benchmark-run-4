9b3aaf2eba237c1776e3d31ad09aee8edbfc08c8
Alison Gale <agale@chromium.org>
2024-08-13 20:18:15 +0000

    Re-enable previously flaky linux hovercard tests
    I tested all three locally and they passed 20/20 times. Also tested
    many times on CQ. Leaving one disabled on lacros since that will be
    cleaned up later.
    
    Change-Id: I7619dd54ed6e1f859458bfc95601f818ed4f8974
    Bug: 41481726
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777248
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1341218}

--- chrome/browser/ui/views/tabs/tab_hover_card_controller_interactive_uitest.cc
+++ chrome/browser/ui/views/tabs/tab_hover_card_controller_interactive_uitest.cc
@@ -191,8 +191,7 @@ IN_PROC_BROWSER_TEST_F(TabHoverCardInteractiveUiTest,
                   CheckHovercardIsClosed());
 }
 
-// TODO(crbug.com/41481726):  Enable once failing test is fixed.
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if BUILDFLAG(IS_CHROMEOS_LACROS)
 #define MAYBE_HoverCardShownOnTabFocus DISABLED_HoverCardShownOnTabFocus
 #else
 #define MAYBE_HoverCardShownOnTabFocus HoverCardShownOnTabFocus
@@ -272,17 +271,8 @@ IN_PROC_BROWSER_TEST_F(TabHoverCardInteractiveUiTest,
                   SelectTab(kTabStripElementId, 0), CheckHovercardIsClosed());
 }
 
-// TODO(crbug.com/41481726):  Enable once failing test is fixed.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_HoverCardVisibleOnTabFocusFromKeyboardAccelerator \
-  DISABLED_HoverCardVisibleOnTabFocusFromKeyboardAccelerator
-#else
-#define MAYBE_HoverCardVisibleOnTabFocusFromKeyboardAccelerator \
-  HoverCardVisibleOnTabFocusFromKeyboardAccelerator
-#endif
-IN_PROC_BROWSER_TEST_F(
-    TabHoverCardInteractiveUiTest,
-    MAYBE_HoverCardVisibleOnTabFocusFromKeyboardAccelerator) {
+IN_PROC_BROWSER_TEST_F(TabHoverCardInteractiveUiTest,
+                       HoverCardVisibleOnTabFocusFromKeyboardAccelerator) {
   TabStrip* const tab_strip = GetTabStrip(browser());
 
   ASSERT_TRUE(
@@ -331,16 +321,8 @@ IN_PROC_BROWSER_TEST_F(TabHoverCardInteractiveUiTest,
       BrowserView::GetBrowserViewForBrowser(inactive_window)->IsActive());
 }
 
-// TODO(crbug.com/41481726):  Enable once failing test is fixed.
-#if BUILDFLAG(IS_LINUX)
-#define MAYBE_UpdatesHoverCardOnHoverDifferentTab \
-  DISABLED_UpdatesHoverCardOnHoverDifferentTab
-#else
-#define MAYBE_UpdatesHoverCardOnHoverDifferentTab \
-  UpdatesHoverCardOnHoverDifferentTab
-#endif
 IN_PROC_BROWSER_TEST_F(TabHoverCardInteractiveUiTest,
-                       MAYBE_UpdatesHoverCardOnHoverDifferentTab) {
+                       UpdatesHoverCardOnHoverDifferentTab) {
   TabStrip* const tab_strip = GetTabStrip(browser());
   ASSERT_TRUE(
       AddTabAtIndex(1, GURL(url::kAboutBlankURL), ui::PAGE_TRANSITION_TYPED));
