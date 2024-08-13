d4f907c1412660319bc3367d2d58875f1f26442b
Richard Chen <richche@chromium.org>
2024-08-13 13:12:20 +0000

    Update kExtensionTelemetryForEnterprise flag check location
    Move the kExtensionTelemetryForEnterprise flag check location to
    also cover the ConnectorService subscriber.
    
    Bug: 339658287
    Change-Id: I1e037de2848700242ca11cb1973cf9692680ed82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782375
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Commit-Queue: Richard Chen <richche@google.com>
    Cr-Commit-Position: refs/heads/main@{#1340927}

--- chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service.cc
+++ chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service.cc
@@ -360,18 +360,22 @@ ExtensionTelemetryService::ExtensionTelemetryService(
       base::BindRepeating(&ExtensionTelemetryService::OnESBPrefChanged,
                           base::Unretained(this)));
 
-  // Register for enterprise policy changes.
-  auto* connector_service =
-      enterprise_connectors::ConnectorsServiceFactory::GetForBrowserContext(
-          profile);
-  connector_service->ObserveTelemetryReporting(
-      base::BindRepeating(&ExtensionTelemetryService::OnEnterprisePolicyChanged,
-                          base::Unretained(this)));
-
-  // Set initial enable/disable states.
+  // Set initial enable/disable state for ESB.
   SetEnabledForESB(IsEnhancedProtectionEnabled(*pref_service_));
-  SetEnabledForEnterprise(
-      GetExtensionTelemetryEventRouter(profile_)->IsPolicyEnabled());
+
+  if (base::FeatureList::IsEnabled(kExtensionTelemetryForEnterprise)) {
+    // Register for enterprise policy changes.
+    auto* connector_service =
+        enterprise_connectors::ConnectorsServiceFactory::GetForBrowserContext(
+            profile);
+    connector_service->ObserveTelemetryReporting(base::BindRepeating(
+        &ExtensionTelemetryService::OnEnterprisePolicyChanged,
+        base::Unretained(this)));
+
+    // Set initial enable/disable state for enterprise.
+    SetEnabledForEnterprise(
+        GetExtensionTelemetryEventRouter(profile_)->IsPolicyEnabled());
+  }
 }
 
 void ExtensionTelemetryService::RecordSignalType(
@@ -485,8 +489,7 @@ void ExtensionTelemetryService::SetEnabledForESB(bool enable) {
 // - Off-store data collection
 void ExtensionTelemetryService::SetEnabledForEnterprise(bool enable) {
   // Make call idempotent.
-  if (!base::FeatureList::IsEnabled(kExtensionTelemetryForEnterprise) ||
-      enterprise_enabled_ == enable) {
+  if (enterprise_enabled_ == enable) {
     return;
   }
 
