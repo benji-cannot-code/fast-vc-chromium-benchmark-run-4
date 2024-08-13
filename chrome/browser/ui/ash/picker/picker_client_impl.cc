eb0ea77fac62092b3fd7f6bd5724d0251e581973
Michael Cui <mlcui@google.com>
2024-08-13 00:08:25 +0000

    Revert "Post task in `StartCrosSearch` to avoid keydown latency"
    This reverts commit ef8fa8eedfbac305880c074d3031297cde92f62c.
    
    Reason for revert: Might introduce races
    
    Original change's description:
    > Post task in `StartCrosSearch` to avoid keydown latency
    >
    > This makes `views::Textfield::UpdateAfterChange` on the key down event
    > 1.67x faster.
    >
    > Bug: b:357772460
    > Change-Id: I6a6a2100daf9c1795175f9983d72b7ab61defc3d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775464
    > Commit-Queue: Michael Cui <mlcui@google.com>
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1340255}
    
    Bug: b:357772460
    Change-Id: I9b6f62a83a5f3df5bdeb15a7f4c16688fae08404
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785113
    Auto-Submit: Michael Cui <mlcui@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1340718}

--- chrome/browser/ui/ash/picker/picker_client_impl.cc
+++ chrome/browser/ui/ash/picker/picker_client_impl.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/picker/picker_controller.h"
-#include "ash/public/cpp/picker/picker_category.h"
 #include "ash/public/cpp/picker/picker_search_result.h"
 #include "ash/public/cpp/picker/picker_web_paste_target.h"
 #include "base/check.h"
@@ -24,13 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_enumerator.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
-#include "base/location.h"
 #include "base/logging.h"
 #include "base/notimplemented.h"
 #include "base/ranges/algorithm.h"
 #include "base/ranges/functional.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/task/sequenced_task_runner.h"
 #include "chrome/browser/ash/app_list/app_list_controller_delegate.h"
 #include "chrome/browser/ash/app_list/search/chrome_search_result.h"
 #include "chrome/browser/ash/app_list/search/files/drive_search_provider.h"
@@ -261,19 +258,6 @@ PickerClientImpl::~PickerClientImpl() {
 
 void PickerClientImpl::StartCrosSearch(
     const std::u16string& query,
-    std::optional<ash::PickerCategory> category,
-    CrosSearchResultsCallback callback) {
-  // This method is called during keydown events - avoid slow synchronous work.
-  // As `AutocompleteController::Start` is slow, post a task to avoid
-  // introducing keydown latency.
-  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
-      FROM_HERE, base::BindOnce(&PickerClientImpl::StartCrosSearchInternal,
-                                weak_factory_.GetWeakPtr(), query,
-                                std::move(category), std::move(callback)));
-}
-
-void PickerClientImpl::StartCrosSearchInternal(
-    std::u16string query,
     std::optional<ash::PickerCategory> category,
     CrosSearchResultsCallback callback) {
   ranker_categories_ = CreateRankerCategories();
