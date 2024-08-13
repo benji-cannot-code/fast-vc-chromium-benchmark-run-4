c912e476ee439afaf0cdd468b7abc71e16c5bb0a
Juan Mojica <juanmojica@google.com>
2024-08-13 23:12:29 +0000

    [lensoverlay] Language picker can now cycle through languages.
    This adds access to languageSettingsPrivate to the Lens untrusted WebUI.
    
    Change-Id: I3f59144ff932e5b815de46aee02566386a89fe0b
    Bug: b:354275185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783464
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341339}

---
+++ chrome/browser/resources/lens/overlay/language_browser_proxy.ts
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview A browser proxy for fetching language settings.
+ */
+let instance: LanguageBrowserProxy|null = null;
+
+export interface LanguageBrowserProxy {
+  getLanguageList(): Promise<chrome.languageSettingsPrivate.Language[]>;
+  getTranslateTargetLanguage(): Promise<string>;
+}
+
+export class LanguageBrowserProxyImpl implements LanguageBrowserProxy {
+  static getInstance(): LanguageBrowserProxy {
+    return instance || (instance = new LanguageBrowserProxyImpl());
+  }
+
+  static setInstance(obj: LanguageBrowserProxy) {
+    instance = obj;
+  }
+
+  getLanguageList(): Promise<chrome.languageSettingsPrivate.Language[]> {
+    return chrome.languageSettingsPrivate.getLanguageList();
+  }
+
+  getTranslateTargetLanguage(): Promise<string> {
+    return chrome.languageSettingsPrivate.getTranslateTargetLanguage();
+  }
+}
