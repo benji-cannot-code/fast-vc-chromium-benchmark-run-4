581b65922d5f69c6f3e1177cf8cd9bfc1ed84589
Gang Wu <gangwu@chromium.org>
2024-08-13 23:05:20 +0000

    Add a flag to allow credentials_mode can be include for debug
    Change-Id: I837a2f57050c9f6d60948e8df87f94bb44454774
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5778426
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341336}

--- components/contextual_search/core/browser/contextual_search_field_trial.h
+++ components/contextual_search/core/browser/contextual_search_field_trial.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 
+BASE_DECLARE_FEATURE(kContextualSearchWithCredentialsForDebug);
+
 // Manages the Contextual Search field trials for native classes.
 class ContextualSearchFieldTrial {
  public:
