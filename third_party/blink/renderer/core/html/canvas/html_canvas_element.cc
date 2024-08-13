7be45489f711aa5b21064f03cec6844b9a6df5af
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-08-13 20:07:03 +0000

    Set canvas_is_clear_ on Reset for all context types
    Right now, HTMLCanvasElement::Reset sets canvas_is_clear_ to true
    only for 2d contexts. canvas_is_clear_ however is set to false in
    HTMLCanvasElement::DidDraw for all context types. This change makes
    the logic symmetrical.
    
    This boolean is only used in a single place, to mark the first
    contentful paint in HTMLCanvasPainter::PaintReplaced:
    https://crsrc.org/c/third_party/blink/renderer/core/paint/html_canvas_painter.cc;l=42-45;drc=105770df485ace262780d95126bb60b1a16ec340
    
    Setting canvas_is_clear_ to true on canvas reset should prevent this
    first contentful paint to be marked, but this doesn't happen because
    PaintReplace also marks the first contentful paint via a call to
    HTMLCanvasElement::Paint here:
    https://crsrc.org/c/third_party/blink/renderer/core/paint/html_canvas_painter.cc;l=81;drc=105770df485ace262780d95126bb60b1a16ec340
    
    Thus, setting canvas_is_clear_ doesn't have any observable effect.
    
    Fixed: 354124879
    Change-Id: I1c661ad223588c7d52a614b07760f61a388f609c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5749561
    Reviewed-by: Yi Xu <yiyix@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341210}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.cc
@@ -758,6 +758,7 @@ void HTMLCanvasElement::Reset() {
     context_->Reset();
     origin_clean_ = true;
   }
+  canvas_is_clear_ = true;
 
   gfx::Size old_size = Size();
   gfx::Size new_size(w, h);
@@ -766,7 +767,6 @@ void HTMLCanvasElement::Reset() {
   // This optimization is only done for 2D canvases for now.
   if (IsRenderingContext2D() && ResourceProvider() != nullptr &&
       old_size == new_size) {
-    canvas_is_clear_ = true;
     return;
   }
 
