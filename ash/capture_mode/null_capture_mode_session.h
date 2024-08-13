8e13e789bc1b25ba5b85e03750124d42f74c5caf
Sammie Quon <sammiequon@chromium.org>
2024-08-13 20:04:11 +0000

    capture mode: Fix bar not placed correctly when display removed
    The bar is refreshed whenever the root changes, and for region mode,
    this usually happens during a located event. However, when a display is
    removed, there is no located event, so we must update the bar bounds
    without an event.
    
    Test: manual
    Test: ash_unittests CaptureModeTest.DisplayRemoval
    Change-Id: I835d0008c0df3ea48b6c660c5538bedad00ea3ac
    Fixed: b/303094552
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785193
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341207}

--- ash/capture_mode/null_capture_mode_session.h
+++ ash/capture_mode/null_capture_mode_session.h
@@ -46,7 +46,8 @@ class ASH_EXPORT NullCaptureModeSession : public BaseCaptureModeSession {
       bool did_bounds_or_visibility_change) override;
   void OnCameraPreviewDestroyed() override;
   void MaybeDismissUserNudgeForever() override;
-  void MaybeChangeRoot(aura::Window* new_root) override;
+  void MaybeChangeRoot(aura::Window* new_root,
+                       bool root_window_will_shutdown) override;
   std::set<aura::Window*> GetWindowsToIgnoreFromWidgets() override;
 
  private:
