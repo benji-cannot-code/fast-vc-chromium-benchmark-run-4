30176267b9d5a040e5e7dae015ef21c2a623d939
Issack John <issackjohn@microsoft.com>
2024-08-14 17:06:25 +0000

    Add Metric for Dropped Crash Reports with Suppressed Error Dialogs
    This change adds a metric to count occurrences where crash reports for
    unresponsive renderers are not sent due to suppressed error
    dialogs (`--noerrdialogs`). The goal is to measure how often this code
    path is triggered and to use this data to decide whether to change the
    behavior or find mitigation strategies based on the volume of dropped
    reports.
    
    Bug: 40268201
    Change-Id: Icbd8f9735f90b835bf363378c27983c2b92cb0d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739695
    Commit-Queue: Issack John <issackjohn@microsoft.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341756}

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/base_switches.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/containers/contains.h"
@@ -269,6 +270,19 @@ enum class PrimaryPointerType {
   kFine = 2,
   kMaxValue = kFine
 };
+
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+//
+// LINT.IfChange(CrashRepHandlingOutcome)
+enum class CrashRepHandlingOutcome {
+  // Dialog wasn't suppressed and the crash report will be potentially queued.
+  kPotentiallyQueued = 0,
+  // Dialog was suppressed so the crash report was dropped.
+  kDropped = 1,
+  kMaxValue = kDropped,
+};
+// LINT.ThenChange(//tools/metrics/histograms/enums.xml:CrashRepHandlingOutcome)
 
 // The window which we dobounce load info updates in.
 constexpr auto kUpdateLoadStatesInterval = base::Milliseconds(250);
@@ -9314,6 +9328,14 @@ void WebContentsImpl::RendererUnresponsive(
   if (!render_widget_host->renderer_initialized()) {
     return;
   }
+
+  CrashRepHandlingOutcome outcome =
+      base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kNoErrorDialogs)
+          ? CrashRepHandlingOutcome::kDropped
+          : CrashRepHandlingOutcome::kPotentiallyQueued;
+  base::UmaHistogramEnumeration(
+      "ReportingAndNEL.UnresponsiveRenderer.CrashReportOutcome", outcome);
 
   if (base::FeatureList::IsEnabled(features::kCrashReporting) &&
       base::FeatureList::IsEnabled(
