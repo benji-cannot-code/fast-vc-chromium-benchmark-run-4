7f4c86f82fa2cdae693c7ac8c11789db8d86c2c3
Georg Neis <neis@chromium.org>
2024-08-14 02:05:01 +0000

    ash: Move ScopedTestSystemNSSKeySlotMixin to //chrome/test/base/ash
    Bug: b:356749323
    Change-Id: I0bf9a2c0a2e07005eabd36e59a4558a7edea046e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782614
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341415}

--- chrome/test/base/ash/scoped_test_system_nss_key_slot_mixin.cc
+++ chrome/test/base/ash/scoped_test_system_nss_key_slot_mixin.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/scoped_test_system_nss_key_slot_mixin.h"
+#include "chrome/test/base/ash/scoped_test_system_nss_key_slot_mixin.h"
 
 #include <memory>
 
--- chrome/test/base/ash/scoped_test_system_nss_key_slot_mixin.h
+++ chrome/test/base/ash/scoped_test_system_nss_key_slot_mixin.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
-#define CHROME_BROWSER_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
+#ifndef CHROME_TEST_BASE_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
+#define CHROME_TEST_BASE_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
 
 #include <pk11pub.h>
 
@@ -48,4 +48,4 @@ class ScopedTestSystemNSSKeySlotMixin final : public InProcessBrowserTestMixin {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
+#endif  // CHROME_TEST_BASE_ASH_SCOPED_TEST_SYSTEM_NSS_KEY_SLOT_MIXIN_H_
