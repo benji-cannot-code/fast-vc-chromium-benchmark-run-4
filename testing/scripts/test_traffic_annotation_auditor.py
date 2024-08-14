59489fd03b84294b31c89acb57b23a90651780e0
Lina Ismail <ismaill@google.com>
2024-08-14 22:06:06 +0000

    Improving network annotation build error message.
    Bug: 40211021
    Change-Id: I09f52984e7144a7cf713b6f5bc33c56c81533902
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784005
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Lina Ismail <ismaill@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341979}

--- testing/scripts/test_traffic_annotation_auditor.py
+++ testing/scripts/test_traffic_annotation_auditor.py
@@ -71,9 +71,11 @@ def get_current_platform_from_gn_args(build_path):
 
 
 def main_run(args):
-  annotations_file = tempfile.NamedTemporaryFile()
-  annotations_filename = annotations_file.name
-  annotations_file.close()
+  annotations_file, annotations_filename = tempfile.mkstemp()
+  os.close(annotations_file)
+
+  errors_file, errors_filename = tempfile.mkstemp()
+  os.close(errors_file)
 
   build_path = os.path.join(args.paths['checkout'], 'out', args.build_config_fs)
   command_line = [
@@ -84,6 +86,8 @@ def main_run(args):
       build_path,
       '--annotations-file',
       annotations_filename,
+      '--errors-file',
+      errors_filename,
   ]
   rc = common.run_command(command_line)
 
@@ -112,16 +116,22 @@ def main_run(args):
       ]
       rc = common.run_command(command_line)
       cleanup_file(config_filename)
+
+      failures = ['Please refer to stdout for errors.'] if rc else []
+      common.record_local_script_results('test_traffic_annotation_auditor',
+                                         args.output, failures, True)
     else:
       print('Test failed without updating the annotations sheet.')
+      failures = []
+      with open(errors_filename, encoding='utf-8') as f:
+        failures = json.load(f) or ['Please refer to stdout for errors.']
+      common.record_local_script_results('test_traffic_annotation_auditor',
+                                         args.output, failures, True)
   except (ValueError, OSError) as e:
     print('Error updating the annotations sheet', e)
     traceback.print_exc()
   finally:
     cleanup_file(annotations_filename)
-    failures = ['Please refer to stdout for errors.'] if rc else []
-    common.record_local_script_results('test_traffic_annotation_auditor',
-                                       args.output, failures, True)
 
   return rc
 
