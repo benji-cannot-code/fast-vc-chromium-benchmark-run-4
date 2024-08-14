b7c2665b2000a58f1112ca1cd260a369dcb9099a
Cristian Ciacu <cristianciacu@google.com>
2024-08-14 10:21:25 +0000

    [Page Info] Refactor the use of base::Time in Page Info History Data Source
    Considering the desire of deprecating base::Time's `null` concept
    (crbug/40247732), we wanted to refactor the
    `page_info_history_data_source` to use `std::optional<base::Time>`
    instead of the normal `base::Time`.
    
    Change-Id: I3516ce25c4267a68bddfeac589764abfe8f2f024
    Bug: 359438939
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782819
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Commit-Queue: Cristian Ciacu <cristianciacu@google.com>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341547}

--- components/page_info/core/page_info_history_data_source.h
+++ components/page_info/core/page_info_history_data_source.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PAGE_INFO_CORE_PAGE_INFO_HISTORY_DATA_SOURCE_H_
 #define COMPONENTS_PAGE_INFO_CORE_PAGE_INFO_HISTORY_DATA_SOURCE_H_
 
+#include <optional>
 #include <string>
 
 #include "base/functional/callback_forward.h"
@@ -34,11 +35,12 @@ class PageInfoHistoryDataSource {
       base::Time now = base::Time::Now());
 
   // Gets a version of the last time any webpage on the `site_url` host was
-  // visited, formatted as a string, by using the min("last navigation time", x
-  // minutes ago) as the upper bound of the GetLastVisitToHost query. This is
-  // done in order to provide the user with a more useful sneak peak into their
-  // navigation history, by excluding the site(s) they were just on.
-  void GetLastVisitedTimestamp(base::OnceCallback<void(base::Time)> callback);
+  // visited by using the min("last navigation time", x minutes ago) as the
+  // upper bound of the GetLastVisitToHost query. This is done in order to
+  // provide the user with a more useful sneak peak into their navigation
+  // history, by excluding the site(s) they were just on.
+  void GetLastVisitedTimestamp(
+      base::OnceCallback<void(std::optional<base::Time>)> callback);
 
  private:
   // Callback from the history system when the last visit query has completed.
@@ -46,13 +48,13 @@ class PageInfoHistoryDataSource {
   void OnLastVisitBeforeRecentNavigationsComplete(
       const std::string& host_name,
       base::Time query_start_time,
-      base::OnceCallback<void(base::Time)> callback,
+      base::OnceCallback<void(std::optional<base::Time>)> callback,
       history::HistoryLastVisitResult result);
 
   // Callback from the history system when the last visit query has completed
   // the second time.
   void OnLastVisitBeforeRecentNavigationsComplete2(
-      base::OnceCallback<void(base::Time)> callback,
+      base::OnceCallback<void(std::optional<base::Time>)> callback,
       history::HistoryLastVisitResult result);
 
   raw_ptr<history::HistoryService> history_service_ = nullptr;
