06d7ef075f4b81cea80b76a34d944167e3db896c
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-08-14 16:30:30 +0000

    [views-ax] Migrating AboutThisSiteSidePanel view to new system
    This CL migrates the AboutThisSiteSidePanel view to use the new system
    for accessible attributes.
    
    We use the name and role that are being exposed by `WebView` as those
    are the ones we were exposing before.
    
    We can't leave as is, because we'll need to migrate the call
    to `web_view_->GetAccessibleNodeData` to use
    `web_view_->GetViewAccessibility().GetAccessibleNodeData()` since
    that is the correct pattern, but in this case we would then end up
    with ALL the a11y attributes from the `WebView` being exposed in
    `AboutThisSiteSidePanel` as well, which is undesirable.
    
    Bug: 325137417
    
    Change-Id: I23e00210ddaa6ec0761c785cf0d71b1af0bfab1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783976
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1341724}

--- chrome/browser/ui/views/page_info/about_this_site_side_panel_view.cc
+++ chrome/browser/ui/views/page_info/about_this_site_side_panel_view.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/url_util.h"
 #include "third_party/blink/public/common/loader/loader_constants.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/layout/flex_layout_types.h"
 #include "ui/views/layout/flex_layout_view.h"
 #include "url/origin.h"
@@ -80,6 +81,10 @@ AboutThisSiteSidePanelView::AboutThisSiteSidePanelView(
       kAboutThisSiteWebContentsUserDataKey,
       std::make_unique<AboutThisSiteWebContentsUserData>(AsWeakPtr()));
   Observe(web_contents);
+
+  GetViewAccessibility().SetRole(ax::mojom::Role::kWebView);
+  GetViewAccessibility().SetName(
+      std::u16string(), ax::mojom::NameFrom::kAttributeExplicitlyEmpty);
 }
 
 void AboutThisSiteSidePanelView::LoadProgressChanged(double progress) {
@@ -198,11 +203,6 @@ GURL AboutThisSiteSidePanelView::CleanUpQueryParams(const GURL& url) {
 void AboutThisSiteSidePanelView::SetContentVisible(bool visible) {
   web_view_->SetVisible(visible);
   loading_indicator_web_view_->SetVisible(!visible);
-}
-
-void AboutThisSiteSidePanelView::GetAccessibleNodeData(
-    ui::AXNodeData* node_data) {
-  return static_cast<View*>(web_view_)->GetAccessibleNodeData(node_data);
 }
 
 AboutThisSiteSidePanelView::~AboutThisSiteSidePanelView() = default;
