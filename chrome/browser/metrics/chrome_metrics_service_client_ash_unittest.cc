b71b6a833bdcf65b3079bc1732fe0c0f4a030cc8
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-08-14 19:12:01 +0000

    Fix IWYU in chrome_metrics_service_client_ash_unittest.cc
    Helps in an upcoming change.
    
    Change-Id: I96013ff01bb27544a494a327e37dc2ca181e4ae2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785841
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341853}

--- chrome/browser/metrics/chrome_metrics_service_client_ash_unittest.cc
+++ chrome/browser/metrics/chrome_metrics_service_client_ash_unittest.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/metrics/test/test_enabled_state_provider.h"
 #include "components/metrics/unsent_log_store.h"
 #include "components/prefs/testing_pref_service.h"
+#include "components/sync/protocol/sync_enums.pb.h"
 #include "components/sync/test/test_sync_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "components/ukm/ukm_pref_names.h"
