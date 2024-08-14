4d72b36be4383ee41e186d1f6a933b42e3da83d8
Kenichi Ishibashi <bashi@chromium.org>
2024-08-14 11:30:27 +0000

    HttpStreamPool: Delay TCP-based stream attempts in favor of QUIC
    When QuicSessionPool provides the delay based on SRTT, block TCP-based
    stream attempts until the delay passed. This behavior is similar to
    the existing HttpStreamFactory::Job::DoInitConnectionImplQuic().
    
    Bug: 346835898
    Change-Id: Iffc038cc12057e52bcd9ca6a076af7b7dbec2bfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768517
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341569}

--- net/log/net_log_event_type_list.h
+++ net/log/net_log_event_type_list.h
@@ -1408,6 +1408,11 @@ EVENT_TYPE(HTTP_STREAM_POOL_GROUP_JOB_CREATED)
 EVENT_TYPE(HTTP_STREAM_POOL_GROUP_JOB_DESTROYED)
 
 // Marks the start/end of a HttpStreamPool::Job.
+// For the BEGIN event, the event parameters are:
+//   {
+//     "stream_attempt_delay": <The stream attempt delay in milliseconds>,
+//     "source_dependency": <The source identifier of the parent group>
+//   }
 EVENT_TYPE(HTTP_STREAM_POOL_JOB_ALIVE)
 
 // Emitted when an HttpStreamPool::Job started a StreamAttempt.
@@ -1415,6 +1420,13 @@ EVENT_TYPE(HTTP_STREAM_POOL_JOB_ATTEMPT_START)
 
 // Emitted when an HttpStreamPool::Job received completion from a StreamAttempt.
 EVENT_TYPE(HTTP_STREAM_POOL_JOB_ATTEMPT_END)
+
+// Emitted when the stream attempt delay has passed on an HttpStreamPool::Job.
+// The event parameter is:
+//   {
+//     "stream_attempt_delay": <The stream attempt delay in milliseconds>
+//   }
+EVENT_TYPE(HTTP_STREAM_POOL_JOB_STREAM_ATTEMPT_DELAY_PASSED)
 
 // ------------------------------------------------------------------------
 // HttpNetworkTransaction
