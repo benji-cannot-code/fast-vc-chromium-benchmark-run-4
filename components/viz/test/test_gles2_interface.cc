e95d8d312b4b8cf7adc5ee61aa7bc044929f5fd5
Saifuddin Hitawala <hitawala@chromium.org>
2024-08-14 21:26:30 +0000

    [gpu] Add GLCaps::max_texture_size to be used by Gles2Impl
    Add max_texture_size to GLCapabilities that has functionality similar to
    that in Capabilities but is used from Gles2 implementation. This will
    allow us to move towards removing Capabilities from NaCL ppapi client
    thus allowing to remove legacy IPC for Capabilities.
    
    Bug: 356649879
    Change-Id: I7c80a2dd15b675078c95518901b58420ec41c8d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786437
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341949}

--- components/viz/test/test_gles2_interface.cc
+++ components/viz/test/test_gles2_interface.cc
@@ -119,7 +119,7 @@ void TestGLES2Interface::BindTexture(GLenum target, GLuint texture) {
 
 void TestGLES2Interface::GetIntegerv(GLenum pname, GLint* params) {
   if (pname == GL_MAX_TEXTURE_SIZE)
-    *params = test_capabilities_.max_texture_size;
+    *params = test_gl_capabilities_.max_texture_size;
   else if (pname == GL_ACTIVE_TEXTURE)
     *params = GL_TEXTURE0;
   else if (pname == GL_UNPACK_ALIGNMENT)
@@ -447,6 +447,7 @@ void TestGLES2Interface::set_gpu_rasterization(bool gpu_rasterization) {
 }
 
 void TestGLES2Interface::set_max_texture_size(int size) {
+  test_gl_capabilities_.max_texture_size = size;
   test_capabilities_.max_texture_size = size;
 }
 
