ef1b728e561fd74fb2734662ead0233f03ea597d
S. Ganesh <ganesh@chromium.org>
2024-08-14 19:16:32 +0000

    Chrome rename: add ProcessLauncher UMA stats for system startup
    This CL adds UMA stats for `ProcessLauncher` CoCreate during system
    startup. This will allow seeing if the system startup case requires more
    of a delay than other cases.
    
    OBSOLETE_HISTOGRAM[Startup.CreateProcessLauncher]=Replaced by Startup.CreateProcessLauncher2
    
    Bug: 348199036,40792898
    Change-Id: Ie1c2a3e48b4b51cc0de09c86bc0aec48eedcfcd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785318
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341855}

--- chrome/browser/first_run/upgrade_util_win.cc
+++ chrome/browser/first_run/upgrade_util_win.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/synchronization/waitable_event.h"
+#include "base/system/sys_info.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
@@ -130,6 +131,7 @@ void CreateAndMarshalProcessLauncher(
 // without a timeout.
 Microsoft::WRL::ComPtr<IUnknown> CreateProcessLauncher() {
   constexpr int kDefaultTimeoutIncrementSeconds = 15;
+  constexpr base::TimeDelta kMaxTimeAfterSystemStartup = base::Seconds(150);
 
   auto result = base::MakeRefCounted<CreateProcessLauncherResult>();
   if (base::ThreadPool::CreateCOMSTATaskRunner(
@@ -144,9 +146,14 @@ Microsoft::WRL::ComPtr<IUnknown> CreateProcessLauncher() {
             .GetIfInt()
             .value_or(kDefaultTimeoutIncrementSeconds));
     const base::ElapsedTimer timer;
+    const bool is_at_startup =
+        base::SysInfo::Uptime() <= kMaxTimeAfterSystemStartup;
     if (!result->completion_event.TimedWait(timeout)) {
       base::UmaHistogramMediumTimes(
-          "Startup.CreateProcessLauncher.TimedWaitFailed", timer.Elapsed());
+          is_at_startup
+              ? "Startup.CreateProcessLauncher2.TimedWaitFailedAtStartup"
+              : "Startup.CreateProcessLauncher2.TimedWaitFailed",
+          timer.Elapsed());
       creation_timeout.Set(base::Value(static_cast<int>(timeout.InSeconds()) +
                                        kDefaultTimeoutIncrementSeconds));
       TRACE_EVENT_INSTANT0(
@@ -160,7 +167,10 @@ Microsoft::WRL::ComPtr<IUnknown> CreateProcessLauncher() {
       return {};
     }
     base::UmaHistogramMediumTimes(
-        "Startup.CreateProcessLauncher.TimedWaitSucceeded", timer.Elapsed());
+        is_at_startup
+            ? "Startup.CreateProcessLauncher2.TimedWaitSucceededAtStartup"
+            : "Startup.CreateProcessLauncher2.TimedWaitSucceeded",
+        timer.Elapsed());
 
     Microsoft::WRL::ComPtr<IUnknown> unknown;
     const HRESULT hr =
