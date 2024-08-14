9f3ba8113b8d4ab0bab15dde09cb199a48608b09
Ewann Pelle <ewannpv@chromium.org>
2024-08-14 10:33:20 +0000

    [iOS][TGS][5/N] Show snackbar when closing tab groups
    Removes the local mapping for groups closed by the "Close other Tabs"
    action. If one or more groups have been closed by this action, shows
    the snackbar.
    
    Makes sure that the snackbar is only displayed after closing regular
    groups.
    
    Low-Coverage-Reason: Adding tests in a follow-up CL.
    Bug: 359174446, 358351829
    Change-Id: I2df37f1bc8c21c561fb880b8623ea7fd85c08881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783923
    Auto-Submit: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341554}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_coordinator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_coordinator.mm
@@ -271,7 +271,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)showTabStripTabGroupSnackbarAfterClosingGroups:
     (int)numberOfClosedGroups {
-  if (!IsTabGroupSyncEnabled()) {
+  if (!IsTabGroupSyncEnabled() ||
+      self.browser->GetBrowserState()->IsOffTheRecord()) {
     return;
   }
 
