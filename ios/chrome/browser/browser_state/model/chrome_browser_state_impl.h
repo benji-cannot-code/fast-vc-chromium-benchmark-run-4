b5df53d7461cfae00adb208512e8039af2370c0e
Aliona DANGLA <alionadangla@google.com>
2024-08-14 09:46:29 +0000

    [iOS] Replace chrome_browser_state_impl_io_data.h imports
    Bug: 358072368
    Change-Id: I15734d23851f8612b5ea1bbac6b82381ff4c842f
    Fixed: 359175487
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783301
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341541}

--- ios/chrome/browser/browser_state/model/chrome_browser_state_impl.h
+++ ios/chrome/browser/browser_state/model/chrome_browser_state_impl.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/task/sequenced_task_runner.h"
-#include "ios/chrome/browser/browser_state/model/chrome_browser_state_impl_io_data.h"
+#include "ios/chrome/browser/profile/model/profile_ios_impl_io_data.h"
 #include "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 
 namespace policy {
