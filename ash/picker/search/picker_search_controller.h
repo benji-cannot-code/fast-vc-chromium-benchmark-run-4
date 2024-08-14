ee8cf97b29006c8c0216f8fc6d01cf83dab45d2a
jhtin <jhtin@google.com>
2024-08-14 07:39:20 +0000

    picker: Load new emoji data on prefs change
    When the enabled IME's change, we load any new data that is added.
    
    Bug: b/346457889
    Change-Id: I8334deb8df18f6d2b56bea74807bf945ebf875ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782478
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Jon Htin <jhtin@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341506}

--- ash/picker/search/picker_search_controller.h
+++ ash/picker/search/picker_search_controller.h
@@ -18,8 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/picker/picker_category.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ref.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "chromeos/ash/components/emoji/emoji_search.h"
+#include "components/prefs/pref_change_registrar.h"
 
 namespace ash {
 
@@ -47,7 +49,11 @@ class ASH_EXPORT PickerSearchController {
       PickerViewDelegate::EmojiSearchResultsCallback callback);
 
  private:
+  void LoadEmojiLanguages(PrefService* pref);
+
   const raw_ref<PickerClient> client_;
+
+  PrefChangeRegistrar pref_change_registrar_;
 
   base::TimeDelta burn_in_period_;
 
@@ -56,6 +62,8 @@ class ASH_EXPORT PickerSearchController {
   // destructed first.
   std::unique_ptr<PickerSearchAggregator> aggregator_;
   std::unique_ptr<PickerSearchRequest> search_request_;
+
+  base::WeakPtrFactory<PickerSearchController> weak_ptr_factory_{this};
 };
 
 }  // namespace ash
