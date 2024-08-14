d1ca9ad711b2c47185a47554b00b0edbc1f24eca
Aliona DANGLA <alionadangla@google.com>
2024-08-14 10:46:33 +0000

    [iOS] Replace chrome_browser_state_manager.h imports
    This CL update chrome_browser_state_manager.h imports to use
    profile_manager_ios.h header instead.
    
    This CL was uploaded by git cl split.
    
    R=danieltwhite@google.com
    
    Bug: 358072368
    Change-Id: Ia34a6735560d08b45cc0c0b08064da6f0866c6ad
    Fixed: 359176329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785958
    Reviewed-by: Daniel White <danieltwhite@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341559}

--- ios/chrome/browser/push_notification/model/push_notification_account_context_manager.mm
+++ ios/chrome/browser/push_notification/model/push_notification_account_context_manager.mm
@@ -15,9 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/push_notification/model/push_notification_client_manager.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/profile_attributes_storage_ios.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
 
 namespace {
 
--- ios/chrome/browser/push_notification/model/push_notification_client.mm
+++ ios/chrome/browser/push_notification/model/push_notification_client.mm
@@ -4,11 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "ios/chrome/browser/push_notification/model/push_notification_client.h"
+
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
