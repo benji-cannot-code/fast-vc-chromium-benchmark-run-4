0d29acaf5872bdd737aef07c1e139c6fe98b5c8a
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-08-14 17:38:39 +0000

    [Desktop Android CRX] Compile the "test" API
    We are experimenting with desktop-android configurations for Chrome.
    
    This CL adds the "test" API to the compilation target for the
    desktop-android build config. Before this CL, all API implementations
    were excluded from desktop-android builds. Now, we separate these into
    blocks to allow individual APIs to be targeted at the API layer. This
    allows these core pieces of the API system to be compiled, along with
    the sources for the test API itself (test_api.h/cc).
    
    This is currently experimental and has no production behavior changes.
    
    Cq-Include-Trybots: luci.chromium.try:android-desktop-arm64-compile-rel,android-desktop-x64-compile-rel
    
    Bug: 356905053
    Change-Id: I2cfe7bec5011986eb920ea62487c5b1c54e97096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784002
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341782}

--- extensions/browser/browser_context_keyed_service_factories.cc
+++ extensions/browser/browser_context_keyed_service_factories.cc
@@ -6,21 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/browser_context_keyed_service_factories.h"
 
 #include "extensions/browser/core_browser_context_keyed_service_factories.h"
-#include "extensions/buildflags/buildflags.h"
-
-#if BUILDFLAG(ENABLE_EXTENSIONS)
 #include "extensions/browser/api/api_browser_context_keyed_service_factories.h"
-#endif
 
 namespace extensions {
 
 void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   EnsureCoreBrowserContextKeyedServiceFactoriesBuilt();
-// TODO(https://crbug.com/356905053): Remove this when APIs are compiled into
-// the experimental desktop-android build.
-#if BUILDFLAG(ENABLE_EXTENSIONS)
   EnsureApiBrowserContextKeyedServiceFactoriesBuilt();
-#endif
 }
 
 }  // namespace extensions
