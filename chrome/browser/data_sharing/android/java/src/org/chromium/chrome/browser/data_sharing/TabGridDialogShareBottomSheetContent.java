e679f34c328f3c4ed95d6e3ce0a181e2feeca30d
ssid <ssid@chromium.org>
2024-08-14 23:16:47 +0000

    Remove data_sharing -> tab_ui dependency
    Move the data sharing related resources and files to data_sharing. The
    bottom sheet is not placed in its own mvc component because we will be
    deleting the bottom sheet soon after the migration to updated sdk UI.
    
    Bug: b/358153912
    Change-Id: I8e56daa09f8682827cb7600139e91cba131c383c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784428
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342020}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/TabGridDialogShareBottomSheetContent.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/TabGridDialogShareBottomSheetContent.java
@@ -3,13 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.chrome.browser.tasks.tab_management;
+package org.chromium.chrome.browser.data_sharing;
 
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
-import org.chromium.chrome.tab_ui.R;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetContent;
 
 /** Bottom sheet content to provide data sharing service to TabGridDialog. */
