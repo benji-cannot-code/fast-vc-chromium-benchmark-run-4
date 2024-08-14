3e9f8de998badd808f30e9549b05102ed6aaf197
mlcui <mlcui@google.com>
2024-08-14 05:25:08 +0000

    Trim whitespace in search queries
    This prevents searches from starting if the query is entirely
    whitespace.
    
    Bug: b:356977533
    Change-Id: I6a6a21005ad14733cb6bfe4778fe5afad5315748
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783379
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341465}

--- ash/picker/views/picker_view.cc
+++ ash/picker/views/picker_view.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/location.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/branding_buildflags.h"
 #include "chromeos/ash/grit/ash_resources.h"
@@ -560,7 +561,8 @@ void PickerView::UpdateSearchQueryAndActivePage(std::u16string query) {
 }
 
 void PickerView::UpdateActivePage() {
-  std::u16string_view query = search_field_view_->GetQueryText();
+  std::u16string_view query =
+      base::TrimWhitespace(search_field_view_->GetQueryText(), base::TRIM_ALL);
 
   delegate_->GetSessionMetrics().UpdateSearchQuery(query);
 
