01ed98fc6dd90323941117958aed8523db84d208
vikas soni <vikassoni@chromium.org>
2024-08-14 00:57:58 +0000

    [MappableSI] Convert ChromeOS media capture code to use MappableSI.
    Convert ChromeOS media capture code to use mappable shared images
    instead of using GpuMemoryBuffers.
    
    Note that GpuMemoryBufferTrackerCros still creates and uses
    GpuMemoruBuffers which will be converted in future CLs.
    
    This is ChromeOS only change. Although some of unit tests have
    function signature change in override methods, they are all no-ops.
    
    Bug: 40264379
    Change-Id: I1d04ac52756a5eb43b246cf67ea679280fae89d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785316
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341386}

--- media/capture/video/chromeos/camera_device_context.cc
+++ media/capture/video/chromeos/camera_device_context.cc
@@ -106,9 +106,9 @@ void CameraDeviceContext::SubmitCapturedVideoCaptureBuffer(
       std::move(metadata));
 }
 
-void CameraDeviceContext::SubmitCapturedGpuMemoryBuffer(
+void CameraDeviceContext::SubmitCapturedImage(
     ClientType client_type,
-    gfx::GpuMemoryBuffer* buffer,
+    scoped_refptr<gpu::ClientSharedImage> shared_image,
     const VideoCaptureFormat& frame_format,
     base::TimeTicks reference_time,
     base::TimeDelta timestamp) {
@@ -118,9 +118,9 @@ void CameraDeviceContext::SubmitCapturedGpuMemoryBuffer(
     return;
   }
 
-  client->second->OnIncomingCapturedGfxBuffer(
-      buffer, frame_format, GetCameraFrameRotation(), reference_time, timestamp,
-      std::nullopt);
+  client->second->OnIncomingCapturedImage(
+      std::move(shared_image), frame_format, GetCameraFrameRotation(),
+      reference_time, timestamp, std::nullopt);
 }
 
 void CameraDeviceContext::SetSensorOrientation(int sensor_orientation) {
