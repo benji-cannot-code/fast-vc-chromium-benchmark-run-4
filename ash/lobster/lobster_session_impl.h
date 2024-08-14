1695cd04e546ba91eb41c434a4bfd5da7a73109c
Chuong Ho <hdchuong@google.com>
2024-08-14 06:14:23 +0000

    lobster: add mojo interface between chromium and webui
    This CL adds the mojo interface between chromium and webui.
    
    Bug: b:348281533
    Change-Id: If28a7a119a4026b55945ebd605de78859ada9b97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757962
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341482}

--- ash/lobster/lobster_session_impl.h
+++ ash/lobster/lobster_session_impl.h
@@ -11,11 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/lobster/lobster_candidate_store.h"
-#include "ash/lobster/lobster_session_impl.h"
 #include "ash/public/cpp/lobster/lobster_enums.h"
 #include "ash/public/cpp/lobster/lobster_image_candidate.h"
 #include "ash/public/cpp/lobster/lobster_session.h"
 #include "ash/public/cpp/lobster/lobster_system_state.h"
+#include "base/functional/callback.h"
 
 namespace ash {
 
@@ -23,11 +23,15 @@ class LobsterClient;
 
 class ASH_EXPORT LobsterSessionImpl : public LobsterSession {
  public:
+  using ActionCallback = base::OnceCallback<void(std::string_view)>;
+
   explicit LobsterSessionImpl(std::unique_ptr<LobsterClient> client);
   ~LobsterSessionImpl() override;
 
   // LobsterSession overrides
   void DownloadCandidate(int candidate_id, StatusCallback callback) override;
+  void CommitAsInsert(int candidate_id, StatusCallback callback) override;
+  void CommitAsDownload(int candidate_id, StatusCallback callback) override;
   void RequestCandidates(const std::string& query,
                          int num_candidates,
                          RequestCandidatesCallback) override;
@@ -35,6 +39,10 @@ class ASH_EXPORT LobsterSessionImpl : public LobsterSession {
  private:
   void OnRequestCandidates(RequestCandidatesCallback callback,
                            const LobsterResult& image_candidates);
+
+  void InflateCandidateAndPerformAction(int candidate_id,
+                                        ActionCallback action_callback,
+                                        StatusCallback status_callback);
 
   std::unique_ptr<LobsterClient> client_;
 
