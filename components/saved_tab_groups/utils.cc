051c6d6f14ba441d649e1d4fa5931c62219bd303
Shakti Sahu <shaktisahu@chromium.org>
2024-08-14 19:49:32 +0000

    [iOS TG Sync] Replace unsupported URLs with NTP when sending to sync
    Tab group sync is only allowed to send http, https and new tabs to
    sync. Hence there are a few situations where a URL in a different
    scheme needs to be converted to NTP URL, e.g.
    1. If a tab with a unsuppoted scheme is added to a group, a
    chrome://newtab will be sent to sync. The corresponding tab title
    is changed to Unsavable tab.
    2. If a tab is navigated to an unsupported scheme, the navigation
    is ignored and not sent to sync.
    3. If a tab is received from sync with a unsupported URL (which
    shouldn't happen anyway), it will be converted to chrome://newtab
    when updated to local tab.
    
    Bug: 359726089, 358074137
    Change-Id: I8d54eeee586a4d98420fdbbdc22e87e63db558b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788102
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341880}

--- components/saved_tab_groups/utils.cc
+++ components/saved_tab_groups/utils.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/saved_tab_groups/utils.h"
 
+#include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "components/saved_tab_groups/types.h"
 
 namespace tab_groups {
 namespace {
 const char kChromeUINewTabURL[] = "chrome://newtab/";
+
+// Tab title to be shown or synced when tab URL is in an unsupported scheme.
+const char* kDefaultTitleOverride = "Unsavable tab";
 }  // namespace
 
 bool AreLocalIdsPersisted() {
@@ -41,6 +45,11 @@ std::optional<LocalTabGroupID> LocalTabGroupIDFromString(
 
 bool IsURLValidForSavedTabGroups(const GURL& gurl) {
   return gurl.SchemeIsHTTPOrHTTPS() || gurl == GURL(kChromeUINewTabURL);
+}
+
+std::pair<GURL, std::u16string> GetDefaultUrlAndTitle() {
+  return std::make_pair(GURL(kChromeUINewTabURL),
+                        base::ASCIIToUTF16(kDefaultTitleOverride));
 }
 
 }  // namespace tab_groups
--- ios/chrome/browser/saved_tab_groups/model/tab_group_local_update_observer.mm
+++ ios/chrome/browser/saved_tab_groups/model/tab_group_local_update_observer.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "components/saved_tab_groups/tab_group_sync_service.h"
 #import "components/saved_tab_groups/types.h"
+#import "components/saved_tab_groups/utils.h"
 #import "ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util.h"
 #import "ios/chrome/browser/sessions/model/session_restoration_service.h"
 #import "ios/chrome/browser/sessions/model/session_restoration_service_factory.h"
@@ -287,10 +288,16 @@ void TabGroupLocalUpdateObserver::UpdateLocalWebStateInSyncedGroup(
   LocalTabInfo tab_info =
       utils::GetLocalTabInfo(browser_list_, web_state->GetUniqueIdentifier());
 
+  GURL url = web_state->GetVisibleURL();
+  std::u16string title = web_state->GetTitle();
+  if (!IsURLValidForSavedTabGroups(url)) {
+    url = GetDefaultUrlAndTitle().first;
+    title = GetDefaultUrlAndTitle().second;
+  }
+
   sync_service_->UpdateTab(tab_info.tab_group->tab_group_id(),
-                           web_state->GetUniqueIdentifier().identifier(),
-                           web_state->GetTitle(), web_state->GetVisibleURL(),
-                           std::nullopt);
+                           web_state->GetUniqueIdentifier().identifier(), title,
+                           url, std::nullopt);
 }
 
 void TabGroupLocalUpdateObserver::AddLocalWebStateToSyncedGroup(
