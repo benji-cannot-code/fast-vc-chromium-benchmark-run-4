951223eed8a27ca380425fa8ac2fc4cc9e505b02
Jocelyn Tran <jocelyntran@google.com>
2024-08-14 15:41:12 +0000

    [A11y] Remove redundant role methods from AXObject
    IsARIAInput() is only used in IsARIAControl() and IsARIAControl() is
    only used once in AXNodeObject. Remove these two methods and use
    ui::IsControl(), which contains a more comprehensive check for control
    related roles, instead.
    
    Change-Id: I696687711841e74e0c45006928dac50bfab7af6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5769919
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341686}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2478,7 +2478,7 @@ bool AXNodeObject::IsControl() const {
 
   auto* element = DynamicTo<Element>(node);
   return ((element && element->IsFormControlElement()) ||
-          AXObject::IsARIAControl(RawAriaRole()));
+          ui::IsControl(RawAriaRole()));
 }
 
 bool AXNodeObject::IsAutofillAvailable() const {
--- third_party/blink/renderer/modules/accessibility/ax_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_object.h
@@ -1449,8 +1449,6 @@ class MODULES_EXPORT AXObject : public GarbageCollected<AXObject> {
 
   // Static helper functions.
   // TODO(accessibility) Move these to a static helper util class.
-  static bool IsARIAControl(ax::mojom::blink::Role);
-  static bool IsARIAInput(ax::mojom::blink::Role);
   static bool IsFrame(const Node*);
   static bool HasARIAOwns(Element* element);
   // Should this own a child tree (e.g. an iframe).
