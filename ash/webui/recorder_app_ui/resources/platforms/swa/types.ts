fadb6c864cbc900b9778ec6b77ba28495e876783
Pi-Hsun Shih <pihsun@chromium.org>
2024-08-14 05:23:16 +0000

    recorder: Move prompt formatting to platform side
    Since the LoRA prompt is effectively coupled with the training process,
    it's better to be handled at platform side instead of being in the
    frontend.
    
    Bug: b:356550635
    Test: manually
    Change-Id: I41dcd199fc036ab47a357d6aaedb8f5c7167a28d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767057
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341463}

--- ash/webui/recorder_app_ui/resources/platforms/swa/types.ts
+++ ash/webui/recorder_app_ui/resources/platforms/swa/types.ts
@@ -18,6 +18,9 @@ export {
   StreamingResponderCallbackRouter,
 } from '../../mojom/on_device_model.mojom-webui.js';
 export {
+  FormatFeature,
+} from '../../mojom/on_device_model_service.mojom-webui.js';
+export {
   type ModelState,
   ModelStateMonitorReceiver,
   ModelStateType,
