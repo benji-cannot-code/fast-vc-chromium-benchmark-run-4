2fc9af397327e858e1a207908c147860ebb52925
Kene Okoye <kenok@google.com>
2024-08-14 18:59:39 +0000

    [PS] Cleanup - Migrate FirstPartySets to RelatedWebsiteSets in TS/HTML
    Bug: b:331453627
    Change-Id: I42025c5ae7f0dfaf36c14649edfe492dd4165fd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777881
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Kene Okoye <kenok@google.com>
    Reviewed-by: Matt Dembski <dembski@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1341844}

--- chrome/browser/ui/webui/settings/site_settings_handler.cc
+++ chrome/browser/ui/webui/settings/site_settings_handler.cc
@@ -148,8 +148,8 @@ constexpr char kHasPermissionSettings[] = "hasPermissionSettings";
 constexpr char kHasInstalledPWA[] = "hasInstalledPWA";
 constexpr char kIsInstalled[] = "isInstalled";
 constexpr char kRwsOwner[] = "fpsOwner";
-constexpr char kRwsNumMembers[] = "fpsNumMembers";
-constexpr char kRwsEnterpriseManaged[] = "fpsEnterpriseManaged";
+constexpr char kRwsNumMembers[] = "rwsNumMembers";
+constexpr char kRwsEnterpriseManaged[] = "rwsEnterpriseManaged";
 constexpr char kZoom[] = "zoom";
 
 constexpr uint16_t kHttpsDefaultPort = 443;
@@ -658,7 +658,7 @@ void SiteSettingsHandler::RegisterMessages() {
       base::BindRepeating(&SiteSettingsHandler::HandleFetchUsageTotal,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      "getFpsMembershipLabel",
+      "getRwsMembershipLabel",
       base::BindRepeating(&SiteSettingsHandler::HandleGetRwsMembershipLabel,
                           base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
