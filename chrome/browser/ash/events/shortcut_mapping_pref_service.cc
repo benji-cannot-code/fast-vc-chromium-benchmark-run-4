9a5916623c618d0927ce47db6d0436ed4d4dddbd
Eriko Kurimoto <elkurin@chromium.org>
2024-08-14 01:24:22 +0000

    ash: Move shortcut_mapping_pref_service to //c/b/ash/events
    Bug: b:357060745
    Change-Id: I73bf716b51b0cdc988e7856241a156e1a41784b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783600
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341400}

--- chrome/browser/ash/events/shortcut_mapping_pref_service.cc
+++ chrome/browser/ash/events/shortcut_mapping_pref_service.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/shortcut_mapping_pref_service.h"
+#include "chrome/browser/ash/events/shortcut_mapping_pref_service.h"
 
 #include "ash/constants/ash_pref_names.h"
 #include "chrome/browser/browser_process.h"
