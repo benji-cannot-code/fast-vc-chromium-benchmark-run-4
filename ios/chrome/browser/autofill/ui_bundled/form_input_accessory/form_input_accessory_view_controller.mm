2a9d25acace60096ca8ddeef3c63b68cf05ef7eb
Noémie St-Onge <noemies@google.com>
2024-08-14 21:30:07 +0000

    [iOSKAUpgrade] Track expanded view openings with user actions
    This CL records taps on the keyboard accessory's expand, password,
    payment and address icons that open the new expanded view.
    
    Mocks: go/ios-keyboard-accessory-upgrade-mocks
    
    Fixed: 354696299
    Change-Id: I43573d2ca5e3a160c4a3eef9a4b807299871bfe8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782200
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341954}

--- ios/chrome/browser/autofill/ui_bundled/form_input_accessory/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/autofill/ui_bundled/form_input_accessory/form_input_accessory_view_controller.mm
@@ -31,6 +31,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using autofill::FillingProduct;
 using manual_fill::ManualFillDataType;
 
+namespace {
+
+// Logs the right user action when the manual fallback menu is opened from the
+// keyboard accessory's expand icon.
+void LogManualFallbackEntryThroughExpandIcon(ManualFillDataType data_type) {
+  switch (data_type) {
+    case ManualFillDataType::kPassword:
+      base::RecordAction(
+          base::UserMetricsAction("ManualFallback_ExpandIcon_OpenPassword"));
+      break;
+    case ManualFillDataType::kPaymentMethod:
+      base::RecordAction(base::UserMetricsAction(
+          "ManualFallback_ExpandIcon_OpenPaymentMethod"));
+      break;
+    case ManualFillDataType::kAddress:
+      base::RecordAction(
+          base::UserMetricsAction("ManualFallback_ExpandIcon_OpenAddress"));
+      break;
+    case manual_fill::ManualFillDataType::kOther:
+      // The expand icon should only be available if the mapped `data_type` is
+      // either associated with passwords, payment methods or addresses.
+      NOTREACHED_NORETURN();
+  }
+}
+
+}  // namespace
+
 @interface FormInputAccessoryViewController () <
     FormSuggestionViewDelegate,
     ManualFillAccessoryViewControllerDelegate>
@@ -201,23 +228,26 @@ using manual_fill::ManualFillDataType;
 }
 
 - (void)manualFillButtonPressed:(UIButton*)button {
-  [self manualFillButtonPressed:button
-                    forDataType:[ManualFillUtil
-                                    manualFillDataTypeFromFillingProduct:
-                                        _mainFillingProduct]];
+  ManualFillDataType dataType =
+      [ManualFillUtil manualFillDataTypeFromFillingProduct:_mainFillingProduct];
+  LogManualFallbackEntryThroughExpandIcon(dataType);
+  [self manualFillButtonPressed:button forDataType:dataType];
 }
 
 - (void)passwordManualFillButtonPressed:(UIButton*)button {
+  base::RecordAction(base::UserMetricsAction("ManualFallback_OpenPassword"));
   [self manualFillButtonPressed:button
                     forDataType:ManualFillDataType::kPassword];
 }
 
 - (void)creditCardManualFillButtonPressed:(UIButton*)button {
+  base::RecordAction(base::UserMetricsAction("ManualFallback_OpenCreditCard"));
   [self manualFillButtonPressed:button
                     forDataType:ManualFillDataType::kPaymentMethod];
 }
 
 - (void)addressManualFillButtonPressed:(UIButton*)button {
+  base::RecordAction(base::UserMetricsAction("ManualFallback_OpenProfile"));
   [self manualFillButtonPressed:button
                     forDataType:ManualFillDataType::kAddress];
 }
