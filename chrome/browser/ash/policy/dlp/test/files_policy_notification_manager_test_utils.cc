6a5645e8f8d17cc8e32738952bac6526d6b02577
Dave Tapuska <dtapuska@chromium.org>
2024-08-14 17:14:04 +0000

    Convert base::WriteFile
    Use variant that doesn't use unsafe buffers
    
    Bug: 41134632
    Change-Id: Ibe4d6d25763c685ccccc86e2c23d07356387d8b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5777385
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341767}

--- chrome/browser/ash/policy/dlp/test/files_policy_notification_manager_test_utils.cc
+++ chrome/browser/ash/policy/dlp/test/files_policy_notification_manager_test_utils.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace policy {
 
 bool CreateDummyFile(const base::FilePath& path) {
-  return WriteFile(path, "42", sizeof("42")) == sizeof("42");
+  return WriteFile(path, "42");
 }
 
 storage::FileSystemURL CreateFileSystemURL(const blink::StorageKey key,
