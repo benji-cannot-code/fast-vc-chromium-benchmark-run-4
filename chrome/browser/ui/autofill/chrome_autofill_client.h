a1180367772dcef026ee97078812ddd32e12cdce
Dmitry Vykochko <vykochko@google.com>
2024-08-14 07:14:56 +0000

    Remove SaveAddressProfilePromptOptions struct.
    The struct has only one field which should be used
    directly without wrapping it with a struct.
    
    Bug: 325440757
    Change-Id: Ia02f8f3eb16a552c8899003d4f08a7f1f1c1c4d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5725657
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1341500}

--- chrome/browser/ui/autofill/chrome_autofill_client.h
+++ chrome/browser/ui/autofill/chrome_autofill_client.h
@@ -135,7 +135,7 @@ class ChromeAutofillClient : public ContentAutofillClient,
   void ConfirmSaveAddressProfile(
       const AutofillProfile& profile,
       const AutofillProfile* original_profile,
-      SaveAddressProfilePromptOptions options,
+      bool is_migration_to_account,
       AddressProfileSavePromptCallback callback) override;
   void ShowAutofillSuggestions(
       const PopupOpenArgs& open_args,
