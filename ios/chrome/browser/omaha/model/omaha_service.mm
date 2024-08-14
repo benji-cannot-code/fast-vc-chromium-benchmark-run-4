e92e56b53266e43147672494134497801aa85876
Aliona DANGLA <alionadangla@google.com>
2024-08-14 15:30:08 +0000

    [iOS] Remove import after bad rebase
    This import been removed here https://crrev.com/c/5783104 but after a
    rebase it has been reintroduce here https://crrev.com/c/5597548. This
    CL removes it again.
    
    Bug: 359824082
    Change-Id: Ia14b62e4dafa7dc0ada59968556304ce9c7a5fd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785844
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341676}

--- ios/chrome/browser/omaha/model/omaha_service.mm
+++ ios/chrome/browser/omaha/model/omaha_service.mm
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/browser_state_metrics/model/browser_state_metrics.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state_manager.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/upgrade/model/upgrade_constants.h"
 #import "ios/chrome/browser/upgrade/model/upgrade_recommended_details.h"
