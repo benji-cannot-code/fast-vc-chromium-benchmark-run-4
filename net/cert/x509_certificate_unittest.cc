cd2368cfa72e4be5b6f8335bc671a50fd667b2f5
Matt Mueller <mattm@chromium.org>
2024-08-14 16:20:16 +0000

    net/cert/x509_certificate spanification fixes
    Bug: 40284755
    Change-Id: Ifcd0e32d85354fe56779ddef0c5fe645f71e41a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787453
    Reviewed-by: Nick Harper <nharper@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1341713}

--- net/cert/x509_certificate_unittest.cc
+++ net/cert/x509_certificate_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "net/cert/x509_certificate.h"
 
 #include <stdint.h>
@@ -15,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/hash/sha1.h"
@@ -377,10 +373,7 @@ TEST(X509CertificateTest, SerialNumbers) {
     0x01,0x2a,0x39,0x76,0x0d,0x3f,0x4f,0xc9,
     0x0b,0xe7,0xbd,0x2b,0xcf,0x95,0x2e,0x7a,
   };
-
-  ASSERT_EQ(sizeof(google_serial), google_cert->serial_number().size());
-  EXPECT_TRUE(memcmp(google_cert->serial_number().data(), google_serial,
-                     sizeof(google_serial)) == 0);
+  EXPECT_EQ(google_cert->serial_number(), base::as_string_view(google_serial));
 }
 
 TEST(X509CertificateTest, SerialNumberZeroPadded) {
@@ -393,9 +386,7 @@ TEST(X509CertificateTest, SerialNumberZeroPadded) {
   // Check a serial number where the first byte is >= 0x80, the DER returned by
   // serial() should contain the leading 0 padding byte.
   static const uint8_t expected_serial[3] = {0x00, 0x80, 0x01};
-  ASSERT_EQ(sizeof(expected_serial), cert->serial_number().size());
-  EXPECT_TRUE(memcmp(cert->serial_number().data(), expected_serial,
-                     sizeof(expected_serial)) == 0);
+  EXPECT_EQ(cert->serial_number(), base::as_string_view(expected_serial));
 }
 
 TEST(X509CertificateTest, SerialNumberZeroPadded21BytesLong) {
@@ -411,9 +402,7 @@ TEST(X509CertificateTest, SerialNumberZeroPadded21BytesLong) {
   static const uint8_t expected_serial[21] = {
       0x00, 0x80, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
       0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13};
-  ASSERT_EQ(sizeof(expected_serial), cert->serial_number().size());
-  EXPECT_TRUE(memcmp(cert->serial_number().data(), expected_serial,
-                     sizeof(expected_serial)) == 0);
+  EXPECT_EQ(cert->serial_number(), base::as_string_view(expected_serial));
 }
 
 TEST(X509CertificateTest, SerialNumberNegative) {
@@ -426,9 +415,7 @@ TEST(X509CertificateTest, SerialNumberNegative) {
   // RFC 5280 does not allow serial numbers to be negative, but serial number
   // parsing is currently permissive, so this does not cause an error.
   static const uint8_t expected_serial[2] = {0x80, 0x01};
-  ASSERT_EQ(sizeof(expected_serial), cert->serial_number().size());
-  EXPECT_TRUE(memcmp(cert->serial_number().data(), expected_serial,
-                     sizeof(expected_serial)) == 0);
+  EXPECT_EQ(cert->serial_number(), base::as_string_view(expected_serial));
 }
 
 TEST(X509CertificateTest, SerialNumber37BytesLong) {
@@ -445,9 +432,7 @@ TEST(X509CertificateTest, SerialNumber37BytesLong) {
       0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14,
       0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e,
       0x1f, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25};
-  ASSERT_EQ(sizeof(expected_serial), cert->serial_number().size());
-  EXPECT_TRUE(memcmp(cert->serial_number().data(), expected_serial,
-                     sizeof(expected_serial)) == 0);
+  EXPECT_EQ(cert->serial_number(), base::as_string_view(expected_serial));
 }
 
 TEST(X509CertificateTest, SHA256FingerprintsCorrectly) {
@@ -548,17 +533,13 @@ TEST(X509CertificateTest, ParseSubjectAltNames) {
   static const uint8_t kIPv4Address[] = {
       0x7F, 0x00, 0x00, 0x02
   };
-  ASSERT_EQ(std::size(kIPv4Address), ip_addresses[0].size());
-  EXPECT_EQ(
-      0, memcmp(ip_addresses[0].data(), kIPv4Address, std::size(kIPv4Address)));
+  EXPECT_EQ(ip_addresses[0], base::as_string_view(kIPv4Address));
 
   static const uint8_t kIPv6Address[] = {
       0xFE, 0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01
   };
-  ASSERT_EQ(std::size(kIPv6Address), ip_addresses[1].size());
-  EXPECT_EQ(
-      0, memcmp(ip_addresses[1].data(), kIPv6Address, std::size(kIPv6Address)));
+  EXPECT_EQ(ip_addresses[1], base::as_string_view(kIPv6Address));
 
   // Ensure the subjectAltName dirName has not influenced the handling of
   // the subject commonName.
@@ -637,9 +618,7 @@ TEST(X509CertificateTest, ExtractExtension) {
   static constexpr uint8_t kNonsenseOID[] = {0x56, 0x1d, 0x13};
   ASSERT_TRUE(asn1::ExtractExtensionFromDERCert(
       x509_util::CryptoBufferAsStringPiece(cert->cert_buffer()),
-      std::string_view(reinterpret_cast<const char*>(kNonsenseOID),
-                       sizeof(kNonsenseOID)),
-      &present, &critical, &contents));
+      base::as_string_view(kNonsenseOID), &present, &critical, &contents));
   ASSERT_FALSE(present);
 
   scoped_refptr<X509Certificate> uid_cert =
@@ -944,16 +923,14 @@ TEST(X509CertificateTest, IsIssuedByEncoded) {
       ImportCertFromFile(certs_dir, "mit.davidben.der"));
   ASSERT_NE(static_cast<X509Certificate*>(nullptr), mit_davidben_cert.get());
 
-  std::string mit_issuer(reinterpret_cast<const char*>(MITDN),
-                         sizeof(MITDN));
+  std::string mit_issuer{base::as_string_view(MITDN)};
 
   // Test a certificate from Google, issued by Thawte
   scoped_refptr<X509Certificate> google_cert(
       ImportCertFromFile(certs_dir, "google.single.der"));
   ASSERT_NE(static_cast<X509Certificate*>(nullptr), google_cert.get());
 
-  std::string thawte_issuer(reinterpret_cast<const char*>(ThawteDN),
-                            sizeof(ThawteDN));
+  std::string thawte_issuer{base::as_string_view(ThawteDN)};
 
   // Check that the David Ben certificate is issued by MIT, but not
   // by Thawte.
@@ -1032,8 +1009,7 @@ TEST(X509CertificateTest, IsIssuedByEncodedWithIntermediates) {
   EXPECT_FALSE(cert_chain->IsIssuedByEncoded({}));
 
   // Check that the chain is not issued by Verisign
-  std::string verisign_issuer(reinterpret_cast<const char*>(VerisignDN),
-                              sizeof(VerisignDN));
+  std::string verisign_issuer{base::as_string_view(VerisignDN)};
   EXPECT_FALSE(cert_chain->IsIssuedByEncoded({verisign_issuer}));
 
   // Check that the chain is issued by root, though the extraneous Verisign
@@ -1044,7 +1020,7 @@ TEST(X509CertificateTest, IsIssuedByEncodedWithIntermediates) {
 const struct CertificateFormatTestData {
   const char* file_name;
   X509Certificate::Format format;
-  SHA256HashValue* chain_fingerprints[3];
+  std::array<SHA256HashValue*, 3> chain_fingerprints;
 } kFormatTestData[] = {
     // DER Parsing - single certificate, DER encoded
     {"google.single.der",
