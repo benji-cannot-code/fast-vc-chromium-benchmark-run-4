81ac65a85ae3431bc8193633f617115476a42dda
Tomasz Wiszkowski <ender@google.com>
2024-08-15 16:59:04 +0000

    Remove unused OmniboxFocusSource focus source from GetPageClass call
    The parameter is no longer used by this call.
    
    It should be safe to remove the entire enum, too. If our common
    understanding is this is the right thing to do - i will address this
    in a separate change.
    
    Bug: b/357961079
    Change-Id: I58e848d34870b033405eadb8748f7feef5f75abe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789363
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342324}

--- chrome/browser/ui/webui/searchbox/realbox_handler.cc
+++ chrome/browser/ui/webui/searchbox/realbox_handler.cc
@@ -95,7 +95,6 @@ class RealboxOmniboxClient final : public OmniboxClient {
   std::u16string GetURLForDisplay() const override;
   GURL GetNavigationEntryURL() const override;
   metrics::OmniboxEventProto::PageClassification GetPageClassification(
-      OmniboxFocusSource focus_source,
       bool is_prefetch) override;
   security_state::SecurityLevel GetSecurityLevel() const override;
   net::CertStatus GetCertStatus() const override;
@@ -229,8 +228,7 @@ GURL RealboxOmniboxClient::GetNavigationEntryURL() const {
 }
 
 metrics::OmniboxEventProto::PageClassification
-RealboxOmniboxClient::GetPageClassification(OmniboxFocusSource focus_source,
-                                            bool is_prefetch) {
+RealboxOmniboxClient::GetPageClassification(bool is_prefetch) {
   if (lens_searchbox_client_) {
     return lens_searchbox_client_->GetPageClassification();
   }
@@ -401,7 +399,6 @@ void RealboxHandler::QueryAutocomplete(const std::u16string& input,
   // RealboxOmniboxClient::GetPageClassification() ignores the arguments.
   const auto page_classification =
       omnibox_controller()->client()->GetPageClassification(
-          OmniboxFocusSource::INVALID,
           /*is_prefetch=*/false);
   AutocompleteInput autocomplete_input(
       input, page_classification, ChromeAutocompleteSchemeClassifier(profile_));
