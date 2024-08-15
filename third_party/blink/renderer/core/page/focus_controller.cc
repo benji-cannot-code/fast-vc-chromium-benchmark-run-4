e80c96f2ccf0e5ba418a19cb9fc0b16ac80fe139
Di Zhang <dizhangg@chromium.org>
2024-08-15 16:28:07 +0000

    Add PseudoElementsFocusable flag
    We should only ElementTraversal::NextIncludingPseudo and
    ElementTraversal::PseudoElementsFocusable when new feature
    PseudoElementsFocusable is enabled. This enables the normal DOM
    focus navigation to traverse to next and previous pseudo elements.
    
    Note we still need to update FocusNavigation First(), Last()
    and CSS reading-flow traversals.
    
    Bug: 332396355
    Change-Id: Id4c68e30e5eb353ebab52a3b4765ae6ce1d1f52b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787682
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342304}

--- third_party/blink/renderer/core/page/focus_controller.cc
+++ third_party/blink/renderer/core/page/focus_controller.cc
@@ -225,9 +225,18 @@ class FocusNavigation : public GarbageCollected<FocusNavigation> {
   }
 
   const Element* NextInDomOrder(const Element& current) {
-    Element* next = ElementTraversal::NextIncludingPseudo(current, root_);
-    while (next && !IsOwnedByRoot(*next))
-      next = ElementTraversal::NextIncludingPseudo(*next, root_);
+    Element* next;
+    if (RuntimeEnabledFeatures::PseudoElementsFocusableEnabled()) {
+      next = ElementTraversal::NextIncludingPseudo(current, root_);
+      while (next && !IsOwnedByRoot(*next)) {
+        next = ElementTraversal::NextIncludingPseudo(*next, root_);
+      }
+    } else {
+      next = ElementTraversal::Next(current, root_);
+      while (next && !IsOwnedByRoot(*next)) {
+        next = ElementTraversal::Next(*next, root_);
+      }
+    }
     return next;
   }
 
@@ -280,12 +289,24 @@ class FocusNavigation : public GarbageCollected<FocusNavigation> {
   }
 
   const Element* PreviousInDomOrder(const Element& current) {
-    Element* previous =
-        ElementTraversal::PreviousIncludingPseudo(current, root_);
-    if (previous == root_)
-      return nullptr;
-    while (previous && !IsOwnedByRoot(*previous))
-      previous = ElementTraversal::PreviousIncludingPseudo(*previous, root_);
+    Element* previous;
+    if (RuntimeEnabledFeatures::PseudoElementsFocusableEnabled()) {
+      previous = ElementTraversal::PreviousIncludingPseudo(current, root_);
+      if (previous == root_) {
+        return nullptr;
+      }
+      while (previous && !IsOwnedByRoot(*previous)) {
+        previous = ElementTraversal::PreviousIncludingPseudo(*previous, root_);
+      }
+    } else {
+      previous = ElementTraversal::Previous(current, root_);
+      if (previous == root_) {
+        return nullptr;
+      }
+      while (previous && !IsOwnedByRoot(*previous)) {
+        previous = ElementTraversal::Previous(*previous, root_);
+      }
+    }
     return previous;
   }
 
@@ -1665,6 +1686,7 @@ HTMLElement* FocusController::FindScopeOwnerSlotOrReadingFlowContainer(
     const Element& current) {
   Element* element = const_cast<Element*>(&current);
   if (element->IsPseudoElement()) {
+    DCHECK(RuntimeEnabledFeatures::PseudoElementsFocusableEnabled());
     return nullptr;
   }
   while (element) {
