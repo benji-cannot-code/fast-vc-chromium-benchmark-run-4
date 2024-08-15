d1b6434530b12ac9a2d880b2ea8626c2d50b8f7f
Elijah Hewer <hewer@chromium.org>
2024-08-15 02:05:38 +0000

    Saved Desks: Enable UI revamp
    Enable the Saved Desk UI feature by default, and change the criteria so
    Forest must also be enabled.
    
    Bug: b/327639285
    Change-Id: I1c54657dc6a9c603ba9b1d526ad14a4407dd6d37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789532
    Auto-Submit: Elijah Hewer <hewer@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342088}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2624,7 +2624,7 @@ BASE_FEATURE(kSmartReader, "SmartReader", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSavedDeskUiRevamp,
              "SavedDeskUiRevamp",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kQuickAppAccessTestUI,
              "QuickAppAccessTestUI",
@@ -4558,8 +4558,11 @@ bool IsSamlNotificationOnPasswordChangeSuccessEnabled() {
 }
 
 bool IsSavedDeskUiRevampEnabled() {
-  return base::FeatureList::IsEnabled(kForestFeature) &&
-         base::FeatureList::IsEnabled(kSavedDeskUiRevamp);
+  if (!base::FeatureList::IsEnabled(kForestFeature)) {
+    return false;
+  }
+
+  return base::FeatureList::IsEnabled(kSavedDeskUiRevamp);
 }
 
 bool IsScalableIphEnabled() {
