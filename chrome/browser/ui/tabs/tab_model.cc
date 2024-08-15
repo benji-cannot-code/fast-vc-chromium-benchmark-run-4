631bb8f713945c308c6d6c56ca7a7d38e8445af9
Erik Chen <erikchen@chromium.org>
2024-08-15 00:41:50 +0000

    Rename and add documentation for TabInterface::MaybeGetFromContents
    Change-Id: Ia1453e56cdaa90b3a9130d121a67cdd6d8d51642
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789608
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342061}

--- chrome/browser/ui/tabs/tab_model.cc
+++ chrome/browser/ui/tabs/tab_model.cc
@@ -232,7 +232,7 @@ TabInterface* TabInterface::GetFromContents(
 }
 
 // static
-TabInterface* TabInterface::MaybeGetFromContentsHackForCrashBug(
+TabInterface* TabInterface::MaybeGetFromContents(
     content::WebContents* web_contents) {
   TabLookupFromWebContents* lookup =
       TabLookupFromWebContents::FromWebContents(web_contents);
