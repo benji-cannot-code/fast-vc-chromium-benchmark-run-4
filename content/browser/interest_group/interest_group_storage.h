2d4f573854ad2c2d02c22c30598ab9af7cd70d18
Caleb Raitto <caraitto@chromium.org>
2024-08-15 18:58:58 +0000

    [Interest group] Add mechanism for dumping all fields to a .sql file
    This will allow the ability to easily add upgrade tests from multiple
    versions, which will land in a subsequent CL.
    
    NOTE: The results of generation are included in the subsequent CL,
    https://crrev.com/c/5769739, to make it simpler (fewer merge conflicts)
    to `git reset --hard` and cherry-pick this CL onto older merged commits,
    to produce .sql dumps for the older database versions that existed at
    those commits. That said, most of the value will be in testing upgrades
    from recent versions.
    
    Bug: 355010821,356878398
    Change-Id: I418d35149fb9f62dfd34a4927179528950d3aaad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756691
    Commit-Queue: Caleb Raitto <caraitto@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342406}

--- content/browser/interest_group/interest_group_storage.h
+++ content/browser/interest_group/interest_group_storage.h
@@ -205,6 +205,8 @@ class CONTENT_EXPORT InterestGroupStorage {
 
   base::Time GetLastMaintenanceTimeForTesting() const;
 
+  static int GetCurrentVersionNumberForTesting();
+
  private:
   bool EnsureDBInitialized();
   bool InitializeDB();
