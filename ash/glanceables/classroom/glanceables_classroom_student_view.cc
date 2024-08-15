73833e25012cfc32bf5ae42250396bd623cfd022
Wen-Chien Wang <wcwang@chromium.org>
2024-08-15 22:28:36 +0000

    [gtm] Time Management refactor part 7
    No major code change
    1. Removed unused header files in *.cc
    2. Reordered the functions in *.cc so that it matches the order in
       header files.
    3. Removed AnnounceListStateOnComboBoxAccessibility() as there is no
       reference nor implementation for this function.
    
    Bug: b/333770880
    Change-Id: I1b109a27eef7bfd7a9320499b6947dab0e34133b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784660
    Commit-Queue: Wen-Chien Wang <wcwang@chromium.org>
    Reviewed-by: Artsiom Mitrokhin <amitrokhin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342530}

--- ash/glanceables/classroom/glanceables_classroom_student_view.cc
+++ ash/glanceables/classroom/glanceables_classroom_student_view.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-#include "ash/constants/ash_features.h"
 #include "ash/glanceables/classroom/glanceables_classroom_client.h"
 #include "ash/glanceables/classroom/glanceables_classroom_item_view.h"
 #include "ash/glanceables/classroom/glanceables_classroom_types.h"
@@ -28,10 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/combobox.h"
-#include "ash/style/counter_expand_button.h"
-#include "ash/style/icon_button.h"
 #include "ash/style/typography.h"
-#include "ash/system/unified/glanceable_tray_child_bubble.h"
 #include "base/check.h"
 #include "base/functional/bind.h"
 #include "base/metrics/user_metrics.h"
@@ -51,13 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/label.h"
-#include "ui/views/layout/box_layout.h"
-#include "ui/views/layout/box_layout_view.h"
-#include "ui/views/layout/flex_layout.h"
-#include "ui/views/layout/flex_layout_types.h"
-#include "ui/views/layout/flex_layout_view.h"
 #include "ui/views/layout/layout_types.h"
-#include "ui/views/metadata/view_factory_internal.h"
 #include "ui/views/view.h"
 #include "ui/views/view_class_properties.h"
 #include "url/gurl.h"
@@ -218,6 +208,12 @@ void GlanceablesClassroomStudentView::CancelUpdates() {
   weak_ptr_factory_.InvalidateWeakPtrs();
 }
 
+void GlanceablesClassroomStudentView::OnHeaderIconPressed() {
+  RecordClassroomHeaderIconPressed();
+
+  OpenUrl(GURL(kClassroomHomePage));
+}
+
 void GlanceablesClassroomStudentView::OnFooterButtonPressed() {
   base::RecordAction(
       base::UserMetricsAction("Glanceables_Classroom_SeeAllPressed"));
@@ -234,6 +230,45 @@ void GlanceablesClassroomStudentView::OnFooterButtonPressed() {
   }
 }
 
+void GlanceablesClassroomStudentView::SelectedListChanged() {
+  SelectedAssignmentListChanged(/*initial_update=*/false);
+}
+
+void GlanceablesClassroomStudentView::AnimateResize(
+    ResizeAnimation::Type resize_type) {
+  const int current_height = size().height();
+  if (current_height == 0) {
+    return;
+  }
+  resize_animation_.reset();
+
+  if (!ui::ScopedAnimationDurationScaleMode::duration_multiplier()) {
+    PreferredSizeChanged();
+    return;
+  }
+
+  // Check if the available height is large enough for the preferred height, so
+  // that the target height for the animation is correctly bounded.
+  const views::SizeBound available_height =
+      parent()->GetAvailableSize(this).height();
+  const int preferred_height = GetPreferredSize().height();
+  const int target_height =
+      available_height.is_bounded()
+          ? std::min(available_height.value(), preferred_height)
+          : preferred_height;
+  if (current_height == target_height) {
+    return;
+  }
+
+  SetUpResizeThroughputTracker(target_height > current_height
+                                   ? kExpandAnimationSmoothnessHistogramName
+                                   : kCollapseAnimationSmoothnessHistogramName);
+  resize_animation_ = std::make_unique<ResizeAnimation>(
+      current_height, target_height, this,
+      ResizeAnimation::Type::kContainerExpandStateChanged);
+  resize_animation_->Start();
+}
+
 void GlanceablesClassroomStudentView::OpenUrl(const GURL& url) const {
   NewWindowDelegate::GetPrimary()->OpenUrl(
       url, NewWindowDelegate::OpenUrlFrom::kUserInteraction,
@@ -246,12 +281,6 @@ void GlanceablesClassroomStudentView::OnItemViewPressed(
   RecordStudentAssignmentPressed(/*default_list=*/initial_list_selected);
 
   OpenUrl(url);
-}
-
-void GlanceablesClassroomStudentView::OnHeaderIconPressed() {
-  RecordClassroomHeaderIconPressed();
-
-  OpenUrl(GURL(kClassroomHomePage));
 }
 
 void GlanceablesClassroomStudentView::SelectedAssignmentListChanged(
@@ -321,45 +350,6 @@ void GlanceablesClassroomStudentView::SelectedAssignmentListChanged(
           IDS_GLANCEABLES_CLASSROOM_STUDENT_EMPTY_ITEM_DONE_LIST));
       return client->GetCompletedStudentAssignments(std::move(callback));
   }
-}
-
-void GlanceablesClassroomStudentView::SelectedListChanged() {
-  SelectedAssignmentListChanged(/*initial_update=*/false);
-}
-
-void GlanceablesClassroomStudentView::AnimateResize(
-    ResizeAnimation::Type resize_type) {
-  const int current_height = size().height();
-  if (current_height == 0) {
-    return;
-  }
-  resize_animation_.reset();
-
-  if (!ui::ScopedAnimationDurationScaleMode::duration_multiplier()) {
-    PreferredSizeChanged();
-    return;
-  }
-
-  // Check if the available height is large enough for the preferred height, so
-  // that the target height for the animation is correctly bounded.
-  const views::SizeBound available_height =
-      parent()->GetAvailableSize(this).height();
-  const int preferred_height = GetPreferredSize().height();
-  const int target_height =
-      available_height.is_bounded()
-          ? std::min(available_height.value(), preferred_height)
-          : preferred_height;
-  if (current_height == target_height) {
-    return;
-  }
-
-  SetUpResizeThroughputTracker(target_height > current_height
-                                   ? kExpandAnimationSmoothnessHistogramName
-                                   : kCollapseAnimationSmoothnessHistogramName);
-  resize_animation_ = std::make_unique<ResizeAnimation>(
-      current_height, target_height, this,
-      ResizeAnimation::Type::kContainerExpandStateChanged);
-  resize_animation_->Start();
 }
 
 void GlanceablesClassroomStudentView::OnGetAssignments(
@@ -433,7 +423,6 @@ void GlanceablesClassroomStudentView::OnGetAssignments(
             &GlanceablesClassroomStudentView::MaybeDismissErrorMessage,
             base::Unretained(this)),
         GlanceablesErrorMessageView::ButtonActionType::kDismiss);
-    error_message()->SetProperty(views::kViewIgnoredByLayoutKey, true);
   }
 }
 
