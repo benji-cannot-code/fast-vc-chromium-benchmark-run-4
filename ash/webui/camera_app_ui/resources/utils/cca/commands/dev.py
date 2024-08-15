2f6cb8d2c40ccd51708e1d0b3be80ad5d96bf940
Dorah <dorahkim@google.com>
2024-08-15 03:21:52 +0000

    cca: Check if CCA is allowed in the allowed urls list
    This CL checks if the policy disallows camera but allows CCA in allowed
    urls list.
    
    BUG=b:359294746
    Test=tast run $DUT camera.CCAUIPolicy
    
    Change-Id: I52a73331ad30088a8313b60b2ed7c39145e14f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782484
    Reviewed-by: Sean Li <seannli@google.com>
    Commit-Queue: Dorah Kim <dorahkim@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342104}

--- ash/webui/camera_app_ui/resources/utils/cca/commands/dev.py
+++ ash/webui/camera_app_ui/resources/utils/cca/commands/dev.py
@@ -89,7 +89,7 @@ class RequestHandler:
             "is_test_image": True,
             "os_version": "local-dev",
             "textdirection": "ltr",
-            "video_capture_disallowed": False,
+            "cca_disallowed": False,
             "timeLapse": True,
             "digital_zoom": True,
             "preview_ocr": True,
