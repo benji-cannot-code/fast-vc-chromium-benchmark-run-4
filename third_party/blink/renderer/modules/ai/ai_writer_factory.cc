aa487905de8972f5a9894dd65b964165ff1e60e5
Tsuyoshi Horo <horo@chromium.org>
2024-08-15 07:16:36 +0000

    Cleanup ai_writer_factory.cc
    This CL cleanup ai_writer_factory.cc by following toyoshim@'s comment
    on ai_rewriter_factory.cc.
    https://crrev.com/c/5772131/comment/a900bdff_1d2aeac4/
    
    Bug: 357967382
    Change-Id: I59b2ba89cdc675ae658b73ef8b13d59bf75dae0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5790081
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342159}

--- third_party/blink/renderer/modules/ai/ai_writer_factory.cc
+++ third_party/blink/renderer/modules/ai/ai_writer_factory.cc
@@ -37,6 +37,7 @@ class CreateWriterClient : public GarbageCollected<CreateWriterClient>,
         resolver_(resolver),
         shared_context_string_(shared_context_string),
         abort_signal_(signal) {
+    CHECK(resolver_);
     SetContextLifecycleNotifier(ai->GetExecutionContext());
     if (abort_signal_) {
       CHECK(!abort_signal_->aborted());
@@ -66,18 +67,18 @@ class CreateWriterClient : public GarbageCollected<CreateWriterClient>,
   }
 
   void OnResult(mojo::PendingRemote<mojom::blink::AIWriter> writer) override {
-    if (resolver_) {
-      if (writer) {
-        resolver_->Resolve(MakeGarbageCollected<AIWriter>(
-            ai_->GetExecutionContext(), ai_->GetTaskRunner(), std::move(writer),
-            shared_context_string_));
-      } else {
-        resolver_->Reject(DOMException::Create(
-            kExceptionMessageUnableToCreateWriter,
-            DOMException::GetErrorName(DOMExceptionCode::kInvalidStateError)));
-      }
+    if (!resolver_) {
+      return;
     }
-    Cleanup();
+    if (writer) {
+      resolver_->Resolve(MakeGarbageCollected<AIWriter>(
+          ai_->GetExecutionContext(), ai_->GetTaskRunner(), std::move(writer),
+          shared_context_string_));
+    } else {
+      resolver_->Reject(DOMException::Create(
+          kExceptionMessageUnableToCreateWriter,
+          DOMException::GetErrorName(DOMExceptionCode::kInvalidStateError)));
+    }
   }
 
   // ContextLifecycleObserver methods
@@ -85,11 +86,11 @@ class CreateWriterClient : public GarbageCollected<CreateWriterClient>,
 
  private:
   void OnAborted() {
-    if (resolver_) {
-      resolver_->Reject(DOMException::Create(
-          "Aborted",
-          DOMException::GetErrorName(DOMExceptionCode::kAbortError)));
+    if (!resolver_) {
+      return;
     }
+    resolver_->Reject(DOMException::Create(
+        "Aborted", DOMException::GetErrorName(DOMExceptionCode::kAbortError)));
     Cleanup();
   }
 
@@ -107,6 +108,7 @@ class CreateWriterClient : public GarbageCollected<CreateWriterClient>,
   HeapMojoReceiver<mojom::blink::AIManagerCreateWriterClient,
                    CreateWriterClient>
       receiver_;
+  // `resolver_` will be reset on Cleanup().
   Member<ScriptPromiseResolver<AIWriter>> resolver_;
   const String shared_context_string_;
   SelfKeepAlive<CreateWriterClient> keep_alive_{this};
