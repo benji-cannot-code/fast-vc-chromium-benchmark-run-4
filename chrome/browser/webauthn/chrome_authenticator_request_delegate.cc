c1b95907ad61bf34ab802e2b7dd2fe2c8e4f202a
Ken Buchanan <kenrb@chromium.org>
2024-08-15 22:27:36 +0000

    [WebAuthn] Cleanup kWebAuthnFilterGooglePasskeys flag
    Change-Id: Ic6add2f9d1a83a3faa7d2fa8d804e080aa671066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787314
    Auto-Submit: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342529}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate.cc
@@ -1668,8 +1668,7 @@ void ChromeAuthenticatorRequestDelegate::GetPhoneContactableGpmPasskeysForRpId(
 
 void ChromeAuthenticatorRequestDelegate::FilterRecognizedCredentials(
     device::FidoRequestHandlerBase::TransportAvailabilityInfo* tai) {
-  if (base::FeatureList::IsEnabled(device::kWebAuthnFilterGooglePasskeys) &&
-      dialog_model()->relying_party_id == kGoogleRpId &&
+  if (dialog_model()->relying_party_id == kGoogleRpId &&
       base::ranges::any_of(tai->recognized_credentials,
                            IsCredentialFromPlatformAuthenticator)) {
     // Regrettably, Chrome will create webauthn credentials for things other
