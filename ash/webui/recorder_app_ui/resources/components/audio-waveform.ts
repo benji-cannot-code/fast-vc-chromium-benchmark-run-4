2601e1ebe152486d611fff1f26c822cd70e0958f
Pi-Hsun Shih <pihsun@chromium.org>
2024-08-15 11:38:39 +0000

    recorder: Fix audio waveform bar blocking hover style
    Bug: b:336963138
    Test: manually
    Change-Id: If806ed023888a5df16f1df40c792eee477978e3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5790232
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342213}

--- ash/webui/recorder_app_ui/resources/components/audio-waveform.ts
+++ ash/webui/recorder_app_ui/resources/components/audio-waveform.ts
@@ -167,6 +167,9 @@ export class AudioWaveform extends ReactiveLitElement {
       }
 
       .bar {
+        /* Don't block hover on the background. */
+        pointer-events: none;
+
         .speaker-single & {
           fill: var(--cros-sys-primary);
 
@@ -242,6 +245,9 @@ export class AudioWaveform extends ReactiveLitElement {
 
       .playhead {
         fill: var(--cros-sys-on_surface_variant);
+
+        /* Don't block hover on the background. */
+        pointer-events: none;
       }
     `,
   ];
