8ca72737eee034847959a84d4ca95a7a4d0932f5
Lukasz Anforowicz <lukasza@chromium.org>
2024-08-15 20:50:45 +0000

    Roll serde_json: 1.0.122 => 1.0.124 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * serde_json: 1.0.122 => 1.0.124
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * serde_json@1.0.124: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I70ca0eef5b5d5c4ab6b9a87f0e95d8d947071195
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789656
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342478}

--- third_party/rust/chromium_crates_io/vendor/serde_json-1.0.124/src/lib.rs
+++ third_party/rust/chromium_crates_io/vendor/serde_json-1.0.124/src/lib.rs
@@ -300,7 +300,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! [macro]: crate::json
 //! [`serde-json-core`]: https://github.com/rust-embedded-community/serde-json-core
 
-#![doc(html_root_url = "https://docs.rs/serde_json/1.0.122")]
+#![doc(html_root_url = "https://docs.rs/serde_json/1.0.124")]
 // Ignored clippy lints
 #![allow(
     clippy::collapsible_else_if,
@@ -341,6 +341,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     clippy::wildcard_imports,
     // things are often more readable this way
     clippy::cast_lossless,
+    clippy::items_after_statements,
     clippy::module_name_repetitions,
     clippy::redundant_else,
     clippy::shadow_unrelated,
