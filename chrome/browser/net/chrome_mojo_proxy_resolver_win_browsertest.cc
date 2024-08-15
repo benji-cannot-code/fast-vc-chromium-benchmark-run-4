88cf1ffb5988ab37a5a9371ff2cbb4f8dff2ab8a
Kuan Huang <kuanhuang@chromium.org>
2024-08-15 23:17:56 +0000

    Revert "Disable ChromeMojoProxyResolverWinBrowserTest for win11-arm64"
    This reverts commit 81f2fc06ec0fb8aef49ce6849027c918214b4a8b.
    
    Reason for revert: sandbox flaky were fixed in http://b/40223285
    
    Original change's description:
    > Disable ChromeMojoProxyResolverWinBrowserTest for win11-arm64
    >
    > The following tests were flaky on win11-arm64:
    >
    > * ChromeMojoProxyResolverWinBrowserTest.DestroyAndCreateService
    > * ChromeMojoProxyResolverWinBrowserTest.DestroyResolver
    > * ChromeMojoProxyResolverWinBrowserTest.ServiceLifecycle
    >
    > This CL disables the tests until the fix for the network sandbox reland.
    >
    > Bug: 349437220
    > Change-Id: Icec8fc57b2f6ed7125b35c1560941c019b0770b5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5656511
    > Reviewed-by: Adam Langley <agl@chromium.org>
    > Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1319478}
    
    Bug: 349437220
    Change-Id: Ibe0d5549c43caa24644d716239f1055f9f6ddaef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791821
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342550}

--- chrome/browser/net/chrome_mojo_proxy_resolver_win_browsertest.cc
+++ chrome/browser/net/chrome_mojo_proxy_resolver_win_browsertest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/time/time.h"
-#include "build/build_config.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/service_process_host.h"
 #include "content/public/test/browser_test.h"
@@ -79,14 +78,8 @@ using ChromeMojoProxyResolverWinBrowserTest = InProcessBrowserTest;
 
 // Ensures the proxy resolver service is started correctly and stopped when no
 // resolvers are open.
-// TODO(crbug.com/349437220): Flaky on win11-arm64
-#if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
-#define MAYBE_ServiceLifecycle DISABLED_ServiceLifecycle
-#else
-#define MAYBE_ServiceLifecycle ServiceLifecycle
-#endif
 IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest,
-                       MAYBE_ServiceLifecycle) {
+                       ServiceLifecycle) {
   // Set up the ProxyResolverFactory.
   mojo::Remote<proxy_resolver_win::mojom::WindowsSystemProxyResolver>
       proxy_resolver_win(
@@ -125,14 +118,7 @@ IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest,
 
 // Same as above, but destroys the WindowsSystemProxyResolver, which should have
 // no impact on service lifetime.
-// TODO(crbug.com/349437220): Flaky on win11-arm64
-#if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
-#define MAYBE_DestroyResolver DISABLED_DestroyResolver
-#else
-#define MAYBE_DestroyResolver DestroyResolver
-#endif
-IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest,
-                       MAYBE_DestroyResolver) {
+IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest, DestroyResolver) {
   mojo::Remote<proxy_resolver_win::mojom::WindowsSystemProxyResolver>
       proxy_resolver_win(
           ChromeMojoProxyResolverWin::CreateWithSelfOwnedReceiverForTesting(
@@ -158,14 +144,8 @@ IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest,
 }
 
 // Make sure the service can be started again after it's been stopped.
-// TODO(crbug.com/349437220): Flaky on win11-arm64
-#if BUILDFLAG(IS_WIN) && defined(ARCH_CPU_ARM64)
-#define MAYBE_DestroyAndCreateService DISABLED_DestroyAndCreateService
-#else
-#define MAYBE_DestroyAndCreateService DestroyAndCreateService
-#endif
 IN_PROC_BROWSER_TEST_F(ChromeMojoProxyResolverWinBrowserTest,
-                       MAYBE_DestroyAndCreateService) {
+                       DestroyAndCreateService) {
   mojo::Remote<proxy_resolver_win::mojom::WindowsSystemProxyResolver>
       proxy_resolver_win(
           ChromeMojoProxyResolverWin::CreateWithSelfOwnedReceiverForTesting(
