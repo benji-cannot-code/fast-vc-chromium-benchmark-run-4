172108daeea119774520dbe91f5cabcc153d30ce
Lingqi Chi <lingqi@chromium.org>
2024-08-15 09:53:09 +0000

    Prefetch: clean up includes and methods in PrefetchCanaryChecker
    Bug: 40215782
    Change-Id: Iff20218d269771874d7be56350937fa83049b435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784167
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342194}

--- content/browser/preloading/prefetch/prefetch_canary_checker_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_canary_checker_unittest.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/simple_test_clock.h"
-#include "base/test/simple_test_tick_clock.h"
 #include "base/time/time.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/network_service_instance.h"
