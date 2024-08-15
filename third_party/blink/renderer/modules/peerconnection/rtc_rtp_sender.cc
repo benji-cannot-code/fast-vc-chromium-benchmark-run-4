489ca06fe346975ec512e4c8a9cbc08e2c938a50
Lucia <alonsomozol@google.com>
2024-08-15 14:55:20 +0000

    [EncodedTransform] Audio and video sender writable
    This CL includes the logic for setting the RTCRtpScriptTransform to the
    sender and configuring the audio/video writable through the
    RTCRtpScriptTransformer.
    
    Bug: 354881878
    
    Change-Id: Ic4d22be5d787d43994631e5561c2ef0153954621
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760150
    Commit-Queue: Lucía Alonso Mozo <alonsomozol@google.com>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342262}

--- third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.cc
+++ third_party/blink/renderer/modules/peerconnection/rtc_rtp_sender.cc
@@ -1383,7 +1383,7 @@ void RTCRtpSender::setTransform(RTCRtpScriptTransform* transform,
   transform_ = transform;
   transform_->Attach();
   if (kind_ == "audio") {
-    transform_->CreateUnderlyingSourceAndSetAudioTransformer(
+    transform_->CreateAudioUnderlyingSourceAndSink(
         WTF::CrossThreadBindOnce(
             &RTCRtpSender::UnregisterEncodedAudioStreamCallback,
             WrapCrossThreadWeakPersistent(this)),
@@ -1391,7 +1391,7 @@ void RTCRtpSender::setTransform(RTCRtpScriptTransform* transform,
     return;
   }
   CHECK_EQ(kind_, "video");
-  transform_->CreateUnderlyingSourceAndSetVideoTransformer(
+  transform_->CreateVideoUnderlyingSourceAndSink(
       WTF::CrossThreadBindOnce(
           &RTCRtpSender::UnregisterEncodedVideoStreamCallback,
           WrapCrossThreadWeakPersistent(this)),
