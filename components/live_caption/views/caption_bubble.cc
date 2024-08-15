522911dd041a2f1fd832eee81cc5567e1e8b9c94
weidongliu <weidongliu@chromium.org>
2024-08-15 02:04:11 +0000

    Fixed the issue where caption content with fixed height.
    In the bug, the label seems to ignore the preferred height. This is
    because Label::GetHeightForWidth() used to ignore the preferred size
    before https://crrev.com/c/5588447. After that, the default behavior is
    to respect preferred size.
    
    This CL fixes this issue.
    
    Change-Id: Ib8fcbc5f66afbcb499dcdd90bbd75b34b7325b55
    Bug: b:357019648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787876
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342087}

--- components/live_caption/views/caption_bubble.cc
+++ components/live_caption/views/caption_bubble.cc
@@ -354,6 +354,15 @@ class CaptionBubbleLabel : public views::Label {
   CaptionBubbleLabel(const CaptionBubbleLabel&) = delete;
   CaptionBubbleLabel& operator=(const CaptionBubbleLabel&) = delete;
 
+  void SetMinimumHeight(int height) {
+    if (minimum_height_ == height) {
+      return;
+    }
+
+    minimum_height_ = height;
+    PreferredSizeChanged();
+  }
+
   void SetText(const std::u16string& text) override {
     views::Label::SetText(text);
 
@@ -424,9 +433,20 @@ class CaptionBubbleLabel : public views::Label {
     }
   }
 
+  // Label:
+  gfx::Size CalculatePreferredSize(
+      const views::SizeBounds& available_size) const override {
+    gfx::Size preferred_size =
+        views::Label::CalculatePreferredSize(available_size);
+    preferred_size.set_height(
+        std::max(minimum_height_, preferred_size.height()));
+    return preferred_size;
+  }
+
 #if defined(NEED_FOCUS_FOR_ACCESSIBILITY)
   std::unique_ptr<CaptionBubbleLabelAXModeObserver> ax_mode_observer_;
 #endif
+  int minimum_height_ = 0;
 };
 
 BEGIN_METADATA(CaptionBubbleLabel)
@@ -1485,17 +1505,13 @@ void CaptionBubble::UpdateContentSize() {
   int label_height = title_->GetVisible()
                          ? content_height - kLineHeightDip * text_scale_factor
                          : content_height;
-  label_->SetPreferredSize(gfx::Size(width - kSidePaddingDip, label_height));
+  label_->SetMinimumHeight(label_height);
   auto button_size = close_button_->GetPreferredSize({});
   auto left_header_width = width - 3 * button_size.width();
   left_header_container_->SetPreferredSize(
       gfx::Size(left_header_width, button_size.height()));
 
-  auto is_live_translate_enabled = media::IsLiveTranslateEnabled();
-
-  download_progress_label_->SetPreferredSize(gfx::Size(width, content_height));
-
-  if (is_live_translate_enabled ||
+  if (media::IsLiveTranslateEnabled() ||
       base::FeatureList::IsEnabled(media::kLiveCaptionMultiLanguage)) {
     language_label_->SetPreferredSize(
         language_label_->CalculatePreferredSize({}));
