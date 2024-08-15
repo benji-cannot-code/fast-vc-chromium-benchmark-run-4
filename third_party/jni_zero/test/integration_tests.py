783aa3077618d3b95186716f42faf5e96976f945
Mohamed Heikal <mheikal@chromium.org>
2024-08-15 21:30:05 +0000

    Allow --pkg-prefix to only apply to filtered packages
    This cl adds a --package-prefix-filter param that scopes out which
    packages get the prefix rather than *all* of them by default. Needed so
    that cronet can rename their jni_zero classes but leave the rest
    undisturbed.
    
    Bug: 353534209
    Change-Id: I474fad6463b9779377e9341f7424c0d5cecee94c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783309
    Reviewed-by: Sam Maier <smaier@chromium.org>
    Owners-Override: Sam Maier <smaier@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342493}

--- third_party/jni_zero/test/integration_tests.py
+++ third_party/jni_zero/test/integration_tests.py
@@ -53,6 +53,7 @@ class CliOptions:
     self.header_path = None
     self.enable_jni_multiplexing = False
     self.package_prefix = None
+    self.package_prefix_filter = None
     self.use_proxy_hash = False
     self.extra_include = None if is_final else _EXTRA_INCLUDES
     self.module_name = None
@@ -70,6 +71,8 @@ class CliOptions:
       ret.append('--enable-jni-multiplexing')
     if self.package_prefix:
       ret += ['--package-prefix', self.package_prefix]
+    if self.package_prefix_filter:
+      ret += ['--package-prefix-filter', self.package_prefix_filter]
     if self.use_proxy_hash:
       ret.append('--use-proxy-hash')
     if self.output_dir:
@@ -461,6 +464,12 @@ class Tests(BaseTest):
     self._TestEndToEndGeneration(['SampleForTests.java'],
                                  srcjar=True,
                                  package_prefix='this.is.a.package.prefix')
+
+  def testPackagePrefixWithFilter(self):
+    self._TestEndToEndGeneration(['SampleForTests.java'],
+                                 srcjar=True,
+                                 package_prefix='this.is.a.package.prefix',
+                                 package_prefix_filter='org.jni_zero')
 
   def testPackagePrefixWithManualRegistration(self):
     self._TestEndToEndRegistration(['SampleForAnnotationProcessor.java'],
