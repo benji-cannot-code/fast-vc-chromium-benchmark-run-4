b6a3d07fc44f22c11756143faa8302f6e9884990
Calder Kitagawa <ckitagawa@chromium.org>
2024-08-15 22:05:55 +0000

    [Tab Switcher] Introduce revised tab card labels
    Create a UI element for revised tab card labels and add a view stub to
    inflate it to the tab grid card layout.
    
    This element will replace the PriceCardView and will also be used for
    activity updates. A render test has been added to test the UI in
    isolation.
    
    A follow up CL will add a new PropertyKey to TabProperties to set the
    TabCardLabelData on the TabCardLabelView using tab ID as a key.
    
    The text and image data for the label are resolved dynamically to avoid
    storing many small, but potentially expensive elements. The UI will
    reset to an "empty state" when the data is removed.
    
    Bug: 353332369
    Change-Id: I4ecc949a7c5c83a5773690de3f24a9531428fe76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786865
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1342515}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabCardLabelData.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import androidx.annotation.Nullable;
+
+import org.chromium.components.browser_ui.widget.async_image.AsyncImageView;
+
+/** Data for the {@link TabCardLabelView}. */
+public class TabCardLabelData {
+    public final @TabCardLabelType int labelType;
+    public final TextResolver textResolver;
+    public final @Nullable AsyncImageView.Factory asyncImageFactory;
+
+    /**
+     * @param labelType The {@link TabCardLabelType} for the appearance of the card.
+     * @param textResolver A {@link TextResolver} to resolve the text.
+     * @param asyncImageFactory To produce icon images. If null no icon is provided.
+     */
+    public TabCardLabelData(
+            @TabCardLabelType int labelType,
+            TextResolver textResolver,
+            @Nullable AsyncImageView.Factory asyncImageFactory) {
+        this.labelType = labelType;
+        this.textResolver = textResolver;
+        this.asyncImageFactory = asyncImageFactory;
+    }
+}
---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TextResolver.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+/** Resolver of dynamic text. */
+@FunctionalInterface
+public interface TextResolver {
+    /**
+     * @param context The context to use for resolving the text.
+     * @return the character sequence to show.
+     */
+    @Nullable
+    CharSequence resolve(Context context);
+}
