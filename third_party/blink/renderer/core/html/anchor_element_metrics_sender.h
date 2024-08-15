c12e25ddf1d695569f0fd596f5e6d478d996cf52
Adithya Srinivasan <adithyas@chromium.org>
2024-08-15 21:34:05 +0000

    NavigationPredictor: Observe N largest anchors with IntersectionObserver
    This patch adds functionality to prioritize observing (with IO) the
    N largest anchors in the page when the number of sampled in anchors is
    more than the maximum number of intersection observations. We
    currently only prioritize based on "ratio_area" but could extend this
    to other features in the future.
    
    Bug: 331670939
    Change-Id: I6835c03dd165d8b3a994728a86438c16eb1c5c87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784034
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342495}

--- third_party/blink/renderer/core/html/anchor_element_metrics_sender.h
+++ third_party/blink/renderer/core/html/anchor_element_metrics_sender.h
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_HTML_ANCHOR_ELEMENT_METRICS_SENDER_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_HTML_ANCHOR_ELEMENT_METRICS_SENDER_H_
 
+#include <compare>
+
 #include "third_party/blink/public/mojom/loader/navigation_predictor.mojom-blink.h"
 #include "third_party/blink/public/mojom/preloading/anchor_element_interaction_host.mojom-blink.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/frame/local_frame_view.h"
+#include "third_party/blink/renderer/platform/allow_discouraged_type.h"
 #include "third_party/blink/renderer/platform/heap/collection_support/heap_hash_set.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/mojo/heap_mojo_remote.h"
@@ -224,8 +227,6 @@ class CORE_EXPORT AnchorElementMetricsSender final
 
   bool is_registered_for_lifecycle_notifications_ = false;
 
-  bool intersection_observer_limit_exceeded_ = false;
-
   // The y-coordinate of the last pointerdown (in the visual viewport coordinate
   // space and offset by the height of the browser top-controls), reported in
   // |RecordPointerDown|. Used to compute |position_update_messages_|.
@@ -237,6 +238,37 @@ class CORE_EXPORT AnchorElementMetricsSender final
   // |MaybeReportAnchorElementsPositionOnScrollEnd| is called. The timer is
   // stopped when |UpdateVisibleAnchors| is called.
   HeapTaskRunnerTimer<AnchorElementMetricsSender> position_update_timer_;
+
+  // These two sets, together, contain the anchors sampled in to be observed
+  // by `intersection_observer_, ordered by their priority (currently,
+  // `ratio_area`).
+  //
+  // The top `max_number_of_observations_` entries are observed at any one
+  // time (and exist in `observed_anchors_`).
+  //
+  //  non_observed_anchors_     observed_anchors_ (size capped)
+  // +-----------------------+ +-------------------------------+
+  // | .1 A1 | .2 A2 | .3 A3 | | .4 A4 | .5 A5 | .6 A6 | .7 A7 |
+  // +-----------------------+ +-------------------------------+
+  //
+  // If an anchor is added, the first element of `observed_anchors_`
+  // might be moved to `non_observed_anchors_` to make room.
+  // If an observed anchor is removed, the last element of
+  // `non_observed_anchors_` is moved to `observed_anchors_`
+  struct AnchorObservation {
+    // mojom::blink::AnchorElementMetrics::ratio_area * 100 (see documentation
+    // in navigation_predictor.mojom).
+    int percent_area;
+    // DOMNodeId for the anchor this AnchorObservation is created for.
+    DOMNodeId dom_node_id;
+
+    bool operator==(const AnchorObservation&) const = default;
+    auto operator<=>(const AnchorObservation&) const = default;
+  };
+  std::set<AnchorObservation> observed_anchors_
+      ALLOW_DISCOURAGED_TYPE("WTF::HashSet lacks key sorting.");
+  std::set<AnchorObservation> not_observed_anchors_
+      ALLOW_DISCOURAGED_TYPE("WTF::HashSet lacks key sorting.");
 
   SEQUENCE_CHECKER(sequence_checker_);
 };
