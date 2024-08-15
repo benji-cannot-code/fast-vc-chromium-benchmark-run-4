e7c6b09c2ee772a550f81a358f7cd395346a4891
Kenichi Ishibashi <bashi@chromium.org>
2024-08-15 10:20:01 +0000

    Do not use the existing SPDY session for "http" request
    HttpStreamPool should check the scheme of a request before checking
    the existing SPDY session. Otherwise, it may use the existing SPDY
    session for a plain HTTP request since the SpdySessionKey only has
    host and port and doesn't have scheme. This CL adds some checks
    before using the existing SPDY session.
    
    To avoid unexpected session match, create empty {Spdy,Quic}SessionKey
    when scheme is not crptographic. This CL also fixes a bug in
    HttpStreamRequest::ToQuicSessionKey() that used
    `disable_cert_network_fetches()` as `require_dns_https_alpn`.
    
    This fixes HttpNetworkTransaction.DoNotUseSpdySessionForHttp when
    HttpStreamPool is used.
    
    Bug: 346835898
    Change-Id: I5c4f6e9a388fa095c469faaaf3693eed98954951
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788802
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342198}

--- net/http/http_stream_key.cc
+++ net/http/http_stream_key.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/public/secure_dns_policy.h"
 #include "net/socket/socket_tag.h"
 #include "net/spdy/spdy_session_key.h"
+#include "url/gurl.h"
 #include "url/scheme_host_port.h"
 
 namespace net {
@@ -64,19 +65,23 @@ base::Value::Dict HttpStreamKey::ToValue() const {
 }
 
 SpdySessionKey HttpStreamKey::ToSpdySessionKey() const {
-  return SpdySessionKey(HostPortPair::FromSchemeHostPort(destination()),
-                        privacy_mode(), ProxyChain::Direct(),
-                        SessionUsage::kDestination, socket_tag(),
-                        network_anonymization_key(), secure_dns_policy(),
-                        disable_cert_network_fetches());
+  HostPortPair host_port = GURL::SchemeIsCryptographic(destination().scheme())
+                               ? HostPortPair::FromSchemeHostPort(destination())
+                               : HostPortPair();
+  return SpdySessionKey(std::move(host_port), privacy_mode(),
+                        ProxyChain::Direct(), SessionUsage::kDestination,
+                        socket_tag(), network_anonymization_key(),
+                        secure_dns_policy(), disable_cert_network_fetches());
 }
 
 QuicSessionKey HttpStreamKey::ToQuicSessionKey() const {
-  return QuicSessionKey(HostPortPair::FromSchemeHostPort(destination()),
-                        privacy_mode(), ProxyChain::Direct(),
-                        SessionUsage::kDestination, socket_tag(),
-                        network_anonymization_key(), secure_dns_policy(),
-                        disable_cert_network_fetches());
+  HostPortPair host_port = GURL::SchemeIsCryptographic(destination().scheme())
+                               ? HostPortPair::FromSchemeHostPort(destination())
+                               : HostPortPair();
+  return QuicSessionKey(std::move(host_port), privacy_mode(),
+                        ProxyChain::Direct(), SessionUsage::kDestination,
+                        socket_tag(), network_anonymization_key(),
+                        secure_dns_policy(), /*require_dns_https_alpn=*/false);
 }
 
 }  // namespace net
