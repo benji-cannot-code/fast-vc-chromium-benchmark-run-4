28f8fd28768d5c619838f0da967cd62cc725c3f7
Min Qin <qinmin@chromium.org>
2024-08-16 17:10:56 +0000

    Adding a method to get the original WebContents that triggers a download
    Creating the native page will cause the primary page to change, so
    download can no longer find its WebContents afterwards. This CL adds
    a new method to expose the original WebContents.
    
    Bug: 359428270
    Change-Id: I3dc9752fec51509d8c4923fa3dd64ae78203bd87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792125
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342891}

--- content/browser/download/download_item_utils.cc
+++ content/browser/download/download_item_utils.cc
@@ -78,9 +78,9 @@ void DownloadItemData::Detach(download::DownloadItem* download_item) {
 }
 
 void DownloadItemData::PrimaryPageChanged(Page& page) {
-  // To prevent reuse of a render in a different primary page, we will reset
-  // web_contents when the primary page changed.
-  Observe(nullptr);
+  // To prevent reuse of a render in a different primary page,
+  // DownloadItemUtils::GetWebContents() will return null after the primary page
+  // changed.
   id_ = GlobalRenderFrameHostId();
 }
 
@@ -104,8 +104,9 @@ BrowserContext* DownloadItemUtils::GetBrowserContext(
 WebContents* DownloadItemUtils::GetWebContents(
     const download::DownloadItem* download_item) {
   DownloadItemData* data = DownloadItemData::Get(download_item);
-  if (!data)
+  if (!data || !data->id()) {
     return nullptr;
+  }
   return data->web_contents();
 }
 
@@ -116,6 +117,16 @@ RenderFrameHost* DownloadItemUtils::GetRenderFrameHost(
   if (!data)
     return nullptr;
   return RenderFrameHost::FromID(data->id());
+}
+
+// static
+WebContents* DownloadItemUtils::GetOriginalWebContents(
+    const download::DownloadItem* download_item) {
+  DownloadItemData* data = DownloadItemData::Get(download_item);
+  if (!data) {
+    return nullptr;
+  }
+  return data->web_contents();
 }
 
 // static
