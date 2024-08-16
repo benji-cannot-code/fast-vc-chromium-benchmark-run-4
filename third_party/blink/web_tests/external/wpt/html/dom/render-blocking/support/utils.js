c266a8faf84933239223475008abfc15bea7224d
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-08-16 01:44:33 +0000

    Import wpt@cbb2cfb326c5e86cd607f1a380419a31ac942a27
    https://github.com/web-platform-tests/wpt/compare/940df8235...cbb2cfb32
    
    Using wpt-import in Chromium c7ef849702eaf4b20a8c98066772702611c56431.
    With Chromium commits locally applied on WPT:
      a6e96c9dbf "Reference Target for Cross-Root ARIA"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    jfernandez@igalia.com:
      external/wpt/css/css-align
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Iaf5d39a0ed13f8c17dc82985aabb670e4e5950ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783550
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1342617}

--- third_party/blink/web_tests/external/wpt/html/dom/render-blocking/support/utils.js
+++ third_party/blink/web_tests/external/wpt/html/dom/render-blocking/support/utils.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function generateParserDelay(seconds = 1) {
+  seconds += (Math.random() / 10.0);
   document.write(`
     <script src="/loading/resources/dummy.js?pipe=trickle(d${seconds})"></script>
   `);
