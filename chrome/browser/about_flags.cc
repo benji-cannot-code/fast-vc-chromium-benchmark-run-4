dd93db7aae21a812e431365119cc0d6191af970e
Imran Ziad <imranziad@google.com>
2024-08-16 09:39:48 +0000

    Add flag AUTO for VC inference backend
    This lets platform camera service to choose the optimal default.
    Platform side change: https://crrev.com/c/5790169.
    
    Bug: b:359062442
    Test: Manually tested on Rex
    Change-Id: Id7d35a0b37939afd5cce18b49e54486caf7a019a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787889
    Commit-Queue: Imran Ziad <imranziad@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342721}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -3423,6 +3423,10 @@ const FeatureEntry::FeatureVariation kTpcdHeuristicsGrantsVariations[] = {
      nullptr}};
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+const FeatureEntry::FeatureParam kVcInferenceBackendAuto[] = {
+    {"inference_backend", "AUTO"},
+};
+
 const FeatureEntry::FeatureParam kVcInferenceBackendGpu[] = {
     {"inference_backend", "GPU"},
 };
@@ -3432,13 +3436,17 @@ const FeatureEntry::FeatureParam kVcInferenceBackendNpu[] = {
 };
 
 const FeatureEntry::FeatureVariation kVcRelightingInferenceBackendVariations[] =
-    {{"GPU", kVcInferenceBackendGpu, std::size(kVcInferenceBackendGpu),
+    {{"AUTO", kVcInferenceBackendAuto, std::size(kVcInferenceBackendAuto),
+      nullptr},
+     {"GPU", kVcInferenceBackendGpu, std::size(kVcInferenceBackendGpu),
       nullptr},
      {"NPU", kVcInferenceBackendNpu, std::size(kVcInferenceBackendNpu),
       nullptr}};
 
 const FeatureEntry::FeatureVariation
     kVcSegmentationInferenceBackendVariations[] = {
+        {"AUTO", kVcInferenceBackendAuto, std::size(kVcInferenceBackendAuto),
+         nullptr},
         {"GPU", kVcInferenceBackendGpu, std::size(kVcInferenceBackendGpu),
          nullptr},
         {"NPU", kVcInferenceBackendNpu, std::size(kVcInferenceBackendNpu),
