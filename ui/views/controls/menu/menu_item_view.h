8ae495dc24150ea54a66f964cda4e72ce2c87bb7
Gaurav Kumar <gauravkum@microsoft.com>
2024-08-16 22:51:05 +0000

    [views-ax] Migration of kCheckedState in CheckBoxMenuItemView
    The change to update the kCheckedState in CheckBoxMenuItemView whenever
    its value changes, rather than querying and computing it only when
    needed, aims to improve performance and ensure the state is always
    accurate. This approach reduces the overhead of repeated calculations,
    maintains consistency, and simplifies code maintenance, ultimately
    enhancing the user experience by providing immediate feedback.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Icdf5e9fc8327deabf9d1854c8ff534d32bec1294
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757964
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1343086}

--- ui/views/controls/menu/menu_item_view.h
+++ ui/views/controls/menu/menu_item_view.h
@@ -417,6 +417,8 @@ class VIEWS_EXPORT MenuItemView : public View, public LayoutDelegate {
   // Returns the corresponding border padding from the `MenuConfig`.
   int GetItemHorizontalBorder() const;
 
+  virtual void UpdateAccessibleCheckedState();
+
   void SetTriggerActionWithNonIconChildViews(
       bool trigger_action_with_non_icon_child_views) {
     trigger_action_with_non_icon_child_views_ =
@@ -475,7 +477,6 @@ class VIEWS_EXPORT MenuItemView : public View, public LayoutDelegate {
     return parent_menu_item_->GetSubmenu();
   }
 
-  void UpdateAccessibleCheckedState();
   // The RunXXX methods call into this to set up the necessary state before
   // running.
   void PrepareForRun(bool has_mnemonics, bool show_mnemonics);
