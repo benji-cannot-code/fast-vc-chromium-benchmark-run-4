3d8f4c66f56b91e0f354f7e878b4a190b19b5e59
Elijah Hewer <hewer@chromium.org>
2024-08-16 19:47:26 +0000

    Reland "Saved Desks: Enable UI revamp"
    This is a reland of commit 755d431901225aa268ad416c047202dd5ca014b2
    
    No functional changes, though accounted for a rebase. Tast side changes
    are here: CL:5791917.
    
    Checking if the UI revamp is enabled now takes into account the
    holdback on the Forest feature.
    
    Original change's description:
    > Saved Desks: Enable UI revamp
    >
    > Enable the Saved Desk UI feature by default, and change the criteria so
    > Forest must also be enabled.
    >
    > Bug: b/327639285
    > Change-Id: I1c54657dc6a9c603ba9b1d526ad14a4407dd6d37
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789532
    > Auto-Submit: Elijah Hewer <hewer@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1342088}
    
    Bug: b/327639285
    Change-Id: I6088381894df39cc5f86ce444964c7bdee804044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792197
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Ana Salazar Maldonado <anasalazar@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342989}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -2637,7 +2637,7 @@ BASE_FEATURE(kSmartReader, "SmartReader", base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSavedDeskUiRevamp,
              "SavedDeskUiRevamp",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kQuickAppAccessTestUI,
              "QuickAppAccessTestUI",
@@ -4587,7 +4587,7 @@ bool IsSamlNotificationOnPasswordChangeSuccessEnabled() {
 }
 
 bool IsSavedDeskUiRevampEnabled() {
-  return base::FeatureList::IsEnabled(kForestFeature) &&
+  return IsForestFeatureEnabled() &&
          base::FeatureList::IsEnabled(kSavedDeskUiRevamp);
 }
 
