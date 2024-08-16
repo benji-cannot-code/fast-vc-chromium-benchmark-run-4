200cd4bf4583769de1f65bbaa27a1cb0dc260a62
David Padlipsky <dpad@google.com>
2024-08-16 19:58:21 +0000

    Only show picker shortcut when the controller is initialized
    Change-Id: I4bb7790b508a1adf5bce0578f007cc04fad37044
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791677
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: David Padlipsky <dpad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342995}

--- ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
+++ ash/webui/shortcut_customization_ui/backend/accelerator_configuration_provider.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accelerators/ash_accelerator_configuration.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/picker/picker_controller.h"
 #include "ash/public/cpp/accelerator_actions.h"
 #include "ash/public/cpp/accelerators_util.h"
 #include "ash/public/mojom/accelerator_configuration.mojom-shared.h"
@@ -469,7 +470,8 @@ bool ShouldExcludeItem(const AcceleratorLayoutDetails& details) {
     case kToggleSnapGroupWindowsMinimizeAndRestore:
       return true;
     case kTogglePicker:
-      return !ash::features::IsPickerUpdateEnabled();
+      return !(ash::features::IsPickerUpdateEnabled() &&
+               Shell::Get()->picker_controller());
   }
 
   return false;
