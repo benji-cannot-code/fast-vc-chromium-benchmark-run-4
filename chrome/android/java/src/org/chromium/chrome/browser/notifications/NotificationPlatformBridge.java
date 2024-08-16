5ab97a9d0e6b3ca569c0cf46c41092c6fa9a8cd8
Balazs Engedy <engedy@chromium.org>
2024-08-16 15:33:41 +0000

    Make NotificationPlatformBridge less native-dependent (3/5).
    Mark as `static` several helper methods in `NotificationPlatformBridge`. This is in preparation to enable calling methods like `displayProvisionallyUnsubscribedNotification` without an instance of
    `NotificationPlatformBridge` existing, which requires native
    libraries to be loaded.
    
    As these methods already access no instance variables, there should be no functional changes.
    
    Bug: 359616819
    Change-Id: I56e68b0c05b9788b8c805e178f36530765a389a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5790363
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342834}

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/NotificationPlatformBridge.java
@@ -398,15 +398,15 @@ public class NotificationPlatformBridge {
     }
 
     /**
-     * Returns a bogus Uri used to make each intent unique according to Intent#filterEquals.
-     * Without this, the pending intents derived from the intent may be reused, because extras are
-     * not taken into account for the filterEquals comparison.
+     * Returns a bogus Uri used to make each intent unique according to Intent#filterEquals. Without
+     * this, the pending intents derived from the intent may be reused, because extras are not taken
+     * into account for the filterEquals comparison.
      *
      * @param notificationId The id of the notification.
      * @param origin The origin to whom the notification belongs.
      * @param actionIndex The zero-based index of the action button, or -1 if not applicable.
      */
-    private Uri makeIntentData(String notificationId, String origin, int actionIndex) {
+    private static Uri makeIntentData(String notificationId, String origin, int actionIndex) {
         return Uri.parse(origin).buildUpon().fragment(notificationId + "," + actionIndex).build();
     }
 
@@ -423,13 +423,19 @@ public class NotificationPlatformBridge {
      * @param mutable Whether the pending intent is mutable, see {@link
      *     PendingIntent#FLAG_IMMUTABLE}.
      */
-    private PendingIntentProvider makePendingIntent(
+    private static PendingIntentProvider makePendingIntent(
             NotificationIdentifyingAttributes attributes,
             String action,
             int actionIndex,
             boolean mutable) {
         Context context = ContextUtils.getApplicationContext();
         Uri intentData = makeIntentData(attributes.notificationId, attributes.origin, actionIndex);
+        // TODO(crbug.com/359909538): Telemetry shows that startService-type intents are even more
+        // unreliable than broadcasts. Furthermore, checking the feature state is currently the only
+        // place in this method that in theory requires native startup. In practice, we will only
+        // ever get called with ACTION_PRE_UNSUBSCRIBE when displaying a web notification, which
+        // implies native is running, making this a non-issue. Neverthelerss, removing support for
+        // startService-type intents would be the cleanest solution here.
         boolean useServiceIntent =
                 NotificationConstants.ACTION_PRE_UNSUBSCRIBE.equals(action)
                         && NotificationIntentInterceptor
@@ -849,7 +855,7 @@ public class NotificationPlatformBridge {
                 .storeNotificationResourcesIfSuspended(notification);
     }
 
-    private NotificationBuilderBase prepareNotificationBuilder(
+    private static NotificationBuilderBase prepareNotificationBuilder(
             NotificationIdentifyingAttributes identifyingAttributes,
             boolean vibrateEnabled,
             String title,
@@ -1062,7 +1068,7 @@ public class NotificationPlatformBridge {
                 NotificationUmaTracker.ActionType.PRE_UNSUBSCRIBE);
     }
 
-    private void addProvisionallyUnsubscribedNotificationAction(
+    private static void addProvisionallyUnsubscribedNotificationAction(
             NotificationBuilderBase notificationBuilder,
             NotificationIdentifyingAttributes identifyingAttributes,
             String action,
@@ -1074,7 +1080,7 @@ public class NotificationPlatformBridge {
                 /* iconId= */ 0, actionLabel, intentProvider, umaActionType);
     }
 
-    private NotificationWrapper buildNotificationWrapper(
+    private static NotificationWrapper buildNotificationWrapper(
             NotificationBuilderBase notificationBuilder, String notificationId) {
         return notificationBuilder.build(
                 new NotificationMetadata(
@@ -1084,7 +1090,7 @@ public class NotificationPlatformBridge {
     }
 
     /** Returns whether to set a channel id when building a notification. */
-    private boolean shouldSetChannelId(boolean forWebApk) {
+    private static boolean shouldSetChannelId(boolean forWebApk) {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && !forWebApk;
     }
 
@@ -1096,7 +1102,7 @@ public class NotificationPlatformBridge {
      * @param body Textual contents of the notification.
      * @return A character sequence containing the ticker's text.
      */
-    private CharSequence createTickerText(String title, String body) {
+    private static CharSequence createTickerText(String title, String body) {
         SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
 
         spannableStringBuilder.append(title);
