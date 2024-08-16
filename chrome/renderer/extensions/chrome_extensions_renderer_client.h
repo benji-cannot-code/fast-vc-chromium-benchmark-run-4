cef64eb8af3419bf6cfcd53c3458a8e078265245
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-08-16 18:04:31 +0000

    [Extensions] Move some ChromeExtensionsRendererClient methods
    Move ChromeExtensionsRendererClient methods that don't rely on
    //chrome bits into ExtensionsRendererClient.
    
    This CL has no behavior change.
    
    Bug: 359904696
    Change-Id: If82c631de1b863462f1919768852949f2f8d3e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789568
    Reviewed-by: Tim <tjudkins@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342916}

--- chrome/renderer/extensions/chrome_extensions_renderer_client.h
+++ chrome/renderer/extensions/chrome_extensions_renderer_client.h
@@ -10,24 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "extensions/renderer/extensions_renderer_client.h"
-#include "services/service_manager/public/cpp/binder_registry.h"
 #include "ui/base/page_transition_types.h"
-#include "v8/include/v8-local-handle.h"
 
 class GURL;
 
 namespace blink {
-enum class ProtocolHandlerSecurityLevel;
-class WebElement;
-class WebFrame;
 class WebLocalFrame;
-struct WebPluginParams;
 class WebURL;
-class WebView;
 }
 
 namespace content {
-class RenderFrame;
 struct WebPluginInfo;
 }
 
@@ -40,18 +32,9 @@ namespace net {
 class SiteForCookies;
 }
 
-namespace url {
-class Origin;
-}
-
 namespace ukm {
 class MojoUkmRecorder;
 }
-
-namespace v8 {
-class Isolate;
-class Object;
-}  // namespace v8
 
 class ChromeExtensionsRendererClient
     : public extensions::ExtensionsRendererClient {
@@ -77,14 +60,6 @@ class ChromeExtensionsRendererClient
       const extensions::ExtensionId& extension_id) override;
 
   // See ChromeContentRendererClient methods with the same names.
-  void WebViewCreated(blink::WebView* web_view,
-                      const url::Origin* outermost_origin);
-  void RenderFrameCreated(content::RenderFrame* render_frame,
-                          service_manager::BinderRegistry* registry);
-  bool OverrideCreatePlugin(content::RenderFrame* render_frame,
-                            const blink::WebPluginParams& params);
-  bool AllowPopup();
-  blink::ProtocolHandlerSecurityLevel GetProtocolHandlerSecurityLevel();
   void WillSendRequest(blink::WebLocalFrame* frame,
                        ui::PageTransition transition_type,
                        const blink::WebURL& upstream_url,
@@ -92,9 +67,6 @@ class ChromeExtensionsRendererClient
                        const net::SiteForCookies& site_for_cookies,
                        const url::Origin* initiator_origin,
                        GURL* new_url);
-  v8::Local<v8::Object> GetScriptableObject(
-      const blink::WebElement& plugin_element,
-      v8::Isolate* isolate);
 
   static void DidBlockMimeHandlerViewForDisallowedPlugin(
       const blink::WebElement& plugin_element);
@@ -103,12 +75,6 @@ class ChromeExtensionsRendererClient
       const GURL& resource_url,
       const std::string& mime_type,
       const content::WebPluginInfo& plugin_info);
-  static blink::WebFrame* FindFrame(blink::WebLocalFrame* relative_to_frame,
-                                    const std::string& name);
-
-  void RunScriptsAtDocumentStart(content::RenderFrame* render_frame);
-  void RunScriptsAtDocumentEnd(content::RenderFrame* render_frame);
-  void RunScriptsAtDocumentIdle(content::RenderFrame* render_frame);
 
  private:
   std::unique_ptr<ukm::MojoUkmRecorder> ukm_recorder_;
