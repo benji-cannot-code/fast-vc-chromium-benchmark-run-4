7566b0ccd9854a889bcc3e88d0fed9929786493b
Xi Han <hanxi@google.com>
2024-08-16 01:35:49 +0000

    [TabResumption] Add default reason for local Tab.
    This is a follow up CL of https://crrev.com/c/5786741. We also show
    the default reason for local Tab. Screenshot:
    https://docs.google.com/document/d/1E-FX8Rtl0s-8mO7QENGV6ivXMuFYv6g7DW2QXr8p_fQ/edit?tab=t.0#bookmark=id.dkyrbwmpvmb
    
    Bug: 358399176
    Change-Id: Icafa2da32e7555bd47e13a9080543a3f11e6488f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791580
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1342613}

--- chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionTileView.java
+++ chrome/browser/tab_resumption/java/src/org/chromium/chrome/browser/tab_resumption/TabResumptionTileView.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tab_resumption;
 
+import static org.chromium.chrome.browser.tab_resumption.TabResumptionModuleUtils.DISPLAY_TEXT_MAX_LINES_DEFAULT;
+import static org.chromium.chrome.browser.tab_resumption.TabResumptionModuleUtils.DISPLAY_TEXT_MAX_LINES_WITH_REASON;
+
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
@@ -26,8 +29,6 @@ import org.chromium.components.browser_ui.widget.chips.ChipView;
 public class TabResumptionTileView extends RelativeLayout {
     static final String SEPARATE_COMMA = ", ";
     static final String SEPARATE_PERIOD = ". ";
-    static final int DISPLAY_TEXT_MAX_LINES_DEFAULT = 3;
-    static final int DISPLAY_TEXT_MAX_LINES_WITH_REASON = 2;
 
     private RoundedCornerImageView mIconView;
     private TextView mTilePreInfoView;
