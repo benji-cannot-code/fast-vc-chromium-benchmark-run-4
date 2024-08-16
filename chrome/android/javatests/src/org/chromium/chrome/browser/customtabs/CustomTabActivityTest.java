d6dd0b55a464a00e3857cb959ee9cd28d196814e
Theresa <twellington@chromium.org>
2024-08-16 23:16:53 +0000

    Fix UrlBar accessibility description for Search in CCT
    In CustomTabToolbar, the UrlBar is disabled to prevent the normal
    EditText behavior. When Search in CCT is enabled, the TalkBack
    description for UrlBar should indicate to the user that there are
    available actions.
    
    Set up an AccessibilityDelegate to get the correct TalkBack description.
    
    Bug: 359923551
    Change-Id: I5443bdd2003f97730a7c473b83ea47cb6dcf744c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5794126
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343094}

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -2787,6 +2787,10 @@ public class CustomTabActivityTest {
                 mCustomTabActivityTestRule.getActivity().findViewById(R.id.title_url_container);
         Assert.assertTrue(titleBar.hasOnClickListeners());
 
+        var url = mCustomTabActivityTestRule.getActivity().findViewById(R.id.url_bar);
+        Assert.assertNotNull(
+                "Url bar should have an accessibility delegate.", url.getAccessibilityDelegate());
+
         UserActionTester userActionTester = new UserActionTester();
         ThreadUtils.runOnUiThreadBlocking(() -> titleBar.performClick());
         assertThat(userActionTester.getActions(), Matchers.hasItem("CustomTabs.OmniboxClicked"));
