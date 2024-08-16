72c0d10b77e4e3cc30466f8f80a0ad9b18c04f44
Ella Ge <eirage@chromium.org>
2024-08-16 17:39:35 +0000

    [CodeHealth] Cleanup feature flag kInstallPromptGlobalGuardrails
    Bug: 345466316
    Change-Id: I42a4681687b966024396e518c32bbc9ea649361c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793633
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342905}

--- components/webapps/browser/android/install_prompt_prefs_unittest.cc
+++ components/webapps/browser/android/install_prompt_prefs_unittest.cc
@@ -27,9 +27,6 @@ class InstallPromptPrefsTest : public ::testing::Test {
 };
 
 TEST_F(InstallPromptPrefsTest, PromptDismissedRecently) {
-  base::test::ScopedFeatureList scoped_list;
-  scoped_list.InitAndEnableFeature(features::kInstallPromptGlobalGuardrails);
-
   base::Time time = base::Time::Now();
   // Dismiss the prompt once.
   InstallPromptPrefs::RecordInstallPromptDismissed(prefs(), time);
@@ -48,9 +45,6 @@ TEST_F(InstallPromptPrefsTest, PromptDismissedRecently) {
 }
 
 TEST_F(InstallPromptPrefsTest, PromptIgnoredRecently) {
-  base::test::ScopedFeatureList scoped_list;
-  scoped_list.InitAndEnableFeature(features::kInstallPromptGlobalGuardrails);
-
   base::Time time = base::Time::Now();
   // Ignore the prompt once.
   InstallPromptPrefs::RecordInstallPromptIgnored(prefs(), time);
@@ -69,9 +63,6 @@ TEST_F(InstallPromptPrefsTest, PromptIgnoredRecently) {
 }
 
 TEST_F(InstallPromptPrefsTest, RecentCountResetByClicks) {
-  base::test::ScopedFeatureList scoped_list;
-  scoped_list.InitAndEnableFeature(features::kInstallPromptGlobalGuardrails);
-
   base::Time time = base::Time::Now();
   // Record 3 dismiss and 3 ignore.
   for (int i = 0; i < 3; i++) {
