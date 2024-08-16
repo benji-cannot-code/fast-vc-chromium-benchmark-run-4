d7b0cd5c41e918aff37ce92ccc229f8c02cd2f29
Owen Min <zmin@chromium.org>
2024-08-16 21:27:25 +0000

    Delete NewExtensionReportPermissionApi feature flag
    Code cleanup after feathre has been launched for a while.
    
    Bug: None
    Change-Id: Ie79c616906a78f1527829097245c63b6783609c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793881
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343045}

--- chrome/browser/enterprise/reporting/extension_info.cc
+++ chrome/browser/enterprise/reporting/extension_info.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/feature_list.h"
 #include "base/ranges/algorithm.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
@@ -25,10 +24,6 @@ namespace em = ::enterprise_management;
 namespace enterprise_reporting {
 
 namespace {
-
-BASE_FEATURE(kNewExtensionReportPermissionApi,
-             "NewExtensionReportPermissionApi",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 em::Extension_InstallType GetExtensionInstallType(
     ManifestLocation extension_location) {
@@ -61,22 +56,16 @@ void AddPermission(const extensions::Extension* extension,
     extension_info->add_permissions(permission);
   };
 
-  if (base::FeatureList::IsEnabled(kNewExtensionReportPermissionApi)) {
-    base::ranges::for_each(
-        extensions::PermissionsParser::GetRequiredPermissions(extension)
-            .GetAPIsAsStrings(),
-        add_permission);
-
-    base::ranges::for_each(
-        extensions::PermissionsParser::GetOptionalPermissions(extension)
-            .GetAPIsAsStrings(),
-        add_permission);
-    return;
-  }
+  base::ranges::for_each(
+      extensions::PermissionsParser::GetRequiredPermissions(extension)
+          .GetAPIsAsStrings(),
+      add_permission);
 
   base::ranges::for_each(
-      extension->permissions_data()->active_permissions().GetAPIsAsStrings(),
+      extensions::PermissionsParser::GetOptionalPermissions(extension)
+          .GetAPIsAsStrings(),
       add_permission);
+  return;
 }
 
 void AddHostPermission(const extensions::Extension* extension,
@@ -84,22 +73,18 @@ void AddHostPermission(const extensions::Extension* extension,
   auto add_permission = [extension_info](const URLPattern& url) {
     extension_info->add_host_permissions(url.GetAsString());
   };
-  if (base::FeatureList::IsEnabled(kNewExtensionReportPermissionApi)) {
-    base::ranges::for_each(
-        extensions::PermissionsParser::GetRequiredPermissions(extension)
-            .explicit_hosts(),
-        add_permission);
 
-    base::ranges::for_each(
-        extensions::PermissionsParser::GetOptionalPermissions(extension)
-            .explicit_hosts(),
-        add_permission);
-
-    return;
-  }
   base::ranges::for_each(
-      extension->permissions_data()->active_permissions().explicit_hosts(),
+      extensions::PermissionsParser::GetRequiredPermissions(extension)
+          .explicit_hosts(),
       add_permission);
+
+  base::ranges::for_each(
+      extensions::PermissionsParser::GetOptionalPermissions(extension)
+          .explicit_hosts(),
+      add_permission);
+
+  return;
 }
 
 void AddExtensions(const extensions::ExtensionSet& extensions,
