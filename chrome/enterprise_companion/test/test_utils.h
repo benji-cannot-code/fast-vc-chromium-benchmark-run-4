aa552b6c70aa395f4035989fac5b358716dcbf61
Noah Rose Ledesma <noahrose@google.com>
2024-08-16 18:26:40 +0000

    Use a retry loop when deleting install dir
    Installer tests on Windows Release builds have been failing due to the
    install directory not being deletable. A manual investigation
    revealed that DeletePathRecursively failed due to
    ERROR_SHARING_VIOLATION.
    
    I suspect that optimized release builds execute tests quickly enough
    that that the test cleans up before Windows can notice that the files
    are no longer open. Using a retry loop to delete the install dir
    should help.
    
    Trybots are skipped due to http://crbug.com/c/360367240
    
    Bug: 360376218
    Change-Id: I6e50f0308e31ea8d48b5c27e1cdbf5db005d2692
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793936
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342931}

--- chrome/enterprise_companion/test/test_utils.h
+++ chrome/enterprise_companion/test/test_utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_ENTERPRISE_COMPANION_TEST_TEST_UTILS_H_
 #define CHROME_ENTERPRISE_COMPANION_TEST_TEST_UTILS_H_
 
+#include "base/functional/function_ref.h"
 #include "base/process/process.h"
 
 namespace enterprise_companion {
@@ -14,6 +15,13 @@ namespace enterprise_companion {
 // sequence, returning its exit code. Expects the process to exit within the
 // test action timeout.
 int WaitForProcess(base::Process&);
+
+// Waits for a given `predicate` to become true. Invokes `still_waiting`
+// periodically to provide a indication of progress. Returns true if the
+// predicate becomes true before a timeout, otherwise returns false.
+[[nodiscard]] bool WaitFor(
+    base::FunctionRef<bool()> predicate,
+    base::FunctionRef<void()> still_waiting = [] {});
 
 }  // namespace enterprise_companion
 
