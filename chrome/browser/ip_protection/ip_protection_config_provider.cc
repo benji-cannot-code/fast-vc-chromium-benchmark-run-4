8ef6c35ea2eef72851647b26c9c292799b131f7e
Ciara McMullin <ciaramcmullin@google.com>
2024-08-16 19:36:21 +0000

    Move token generation off the UI thread for android webview config provider.
    This CL refactors the existing IpProtectionTokenFetcher class to be an interface and implements 
    relevant fetchers for webview and chrome (IPC and direct, respectively). Both fetchers are bound
    to the thread pool task runner and therefore should not do any work on the UI thread.
    
    Bug: 355482870
    Change-Id: I4f38aec2993a51959e167e2df7e0bd46807bed2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784925
    Commit-Queue: Ciara McMullin <ciaramcmullin@google.com>
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342983}

--- chrome/browser/ip_protection/ip_protection_config_provider.cc
+++ chrome/browser/ip_protection/ip_protection_config_provider.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/channel_info.h"
 #include "components/ip_protection/common/ip_protection_config_provider_helper.h"
 #include "components/ip_protection/common/ip_protection_proxy_config_fetcher.h"
-#include "components/ip_protection/common/ip_protection_token_fetcher.h"
+#include "components/ip_protection/common/ip_protection_token_direct_fetcher.h"
 #include "components/prefs/pref_service.h"
 #include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "components/privacy_sandbox/tracking_protection_prefs.h"
@@ -67,14 +67,14 @@ IpProtectionConfigProvider::IpProtectionConfigProvider(
 
 void IpProtectionConfigProvider::SetUp() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!ip_protection_token_fetcher_) {
+  if (!ip_protection_token_direct_fetcher_) {
     if (!url_loader_factory_) {
       CHECK(profile_);
       url_loader_factory_ = profile_->GetDefaultStoragePartition()
                                 ->GetURLLoaderFactoryForBrowserProcess();
     }
-    ip_protection_token_fetcher_ =
-        base::SequenceBound<ip_protection::IpProtectionTokenFetcher>(
+    ip_protection_token_direct_fetcher_ =
+        base::SequenceBound<ip_protection::IpProtectionTokenDirectFetcher>(
             token_fetcher_task_runner_, url_loader_factory_->Clone());
   }
   if (!ip_protection_proxy_config_fetcher_) {
@@ -94,11 +94,11 @@ void IpProtectionConfigProvider::SetUpForTesting(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   // Carefully destroy any existing values in the correct order.
   ip_protection_proxy_config_fetcher_ = nullptr;
-  ip_protection_token_fetcher_.Reset();
+  ip_protection_token_direct_fetcher_.Reset();
   url_loader_factory_ = nullptr;
 
-  ip_protection_token_fetcher_ =
-      base::SequenceBound<ip_protection::IpProtectionTokenFetcher>(
+  ip_protection_token_direct_fetcher_ =
+      base::SequenceBound<ip_protection::IpProtectionTokenDirectFetcher>(
           token_fetcher_task_runner_, url_loader_factory->Clone(),
           std::move(bsa));
   ip_protection_proxy_config_fetcher_ =
@@ -303,9 +303,9 @@ void IpProtectionConfigProvider::FetchBlindSignedToken(
     TryGetAuthTokensCallback callback) {
   std::optional<std::string> access_token = access_token_info.value().token;
   auto bsa_get_tokens_start_time = base::TimeTicks::Now();
-  ip_protection_token_fetcher_
+  ip_protection_token_direct_fetcher_
       .AsyncCall(
-          &ip_protection::IpProtectionTokenFetcher::FetchBlindSignedToken)
+          &ip_protection::IpProtectionTokenDirectFetcher::FetchBlindSignedToken)
       .WithArgs(
           std::move(access_token), batch_size, quiche_proxy_layer,
           base::BindPostTaskToCurrentDefault(base::BindOnce(
@@ -503,7 +503,7 @@ void IpProtectionConfigProvider::Shutdown() {
   tracking_protection_settings_ = nullptr;
   pref_service_ = nullptr;
   profile_ = nullptr;
-  ip_protection_token_fetcher_.Reset();
+  ip_protection_token_direct_fetcher_.Reset();
   // If we are shutting down, we can't process messages anymore because we
   // rely on having `identity_manager_` to get the OAuth token. Thus, just
   // reset the receiver set.
