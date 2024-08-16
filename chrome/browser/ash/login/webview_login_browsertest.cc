b59fc47e24028fc55420e0a3da1c16ed4891e2c4
Aya El Gendy <ayag@chromium.org>
2024-08-16 15:03:40 +0000

    [Autoreload] Terminate autoreload when error screen is shown
    This is a follow up CL on the authentication flow automatic reload
    feature for handling the termination of the automatic reload when the
    error screen is shown.
    Error screen is shown in multiple cases some of which are mentioned here in
    https://docs.google.com/presentation/d/1Cb6Ul2iM9vMCMsF6Qr_Ib3M0mrHAeQ2PBSBf3KxAMFE/edit#slide=id.p
    
    Bug: b/352023931
    Change-Id: I036f69c1ff0b813a0a809427bb264e0f50bbdb4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788591
    Reviewed-by: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Andrey Davydov <andreydav@google.com>
    Commit-Queue: Aya Elgendy‎ <ayag@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342816}

--- chrome/browser/ash/login/webview_login_browsertest.cc
+++ chrome/browser/ash/login/webview_login_browsertest.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/login/test/fake_recovery_service_mixin.h"
 #include "chrome/browser/ash/login/test/js_checker.h"
 #include "chrome/browser/ash/login/test/login_manager_mixin.h"
+#include "chrome/browser/ash/login/test/network_portal_detector_mixin.h"
 #include "chrome/browser/ash/login/test/oobe_base_test.h"
 #include "chrome/browser/ash/login/test/oobe_screen_exit_waiter.h"
 #include "chrome/browser/ash/login/test/oobe_screen_waiter.h"
@@ -991,8 +992,19 @@ class AutoReloadWebviewLoginTest : public WebviewLoginTest {
   }
 
  protected:
+  bool IsAutoReloadActive() {
+    return LoginDisplayHost::default_host()
+        ->GetOobeUI()
+        ->GetHandler<GaiaScreenHandler>()
+        ->GetAutoReloadManagerForTesting()
+        .IsTimerActiveForTesting();
+  }
+
   std::unique_ptr<base::SimpleTestClock> test_clock_;
   std::unique_ptr<base::SimpleTestTickClock> test_tick_clock_;
+
+  // TODO(b/360343807): Use NetworkStateTestHelper instead.
+  NetworkPortalDetectorMixin network_portal_detector_{&mixin_host_};
 
  private:
   policy::DevicePolicyBuilder device_policy_builder_;
@@ -1010,11 +1022,7 @@ IN_PROC_BROWSER_TEST_F(AutoReloadWebviewLoginTest,
       ash::prefs::kAuthenticationFlowAutoReloadInterval);
   EXPECT_EQ(pref_reload_interval, 0);
 
-  EXPECT_FALSE(LoginDisplayHost::default_host()
-                   ->GetOobeUI()
-                   ->GetHandler<GaiaScreenHandler>()
-                   ->GetAutoReloadManagerForTesting()
-                   .IsTimerActiveForTesting());
+  EXPECT_FALSE(IsAutoReloadActive());
 
   std::string frame_url = "$('gaia-signin').authenticator.reloadUrl_";
   test::OobeJS().ExpectTrue(frame_url +
@@ -1057,11 +1065,31 @@ IN_PROC_BROWSER_TEST_F(AutoReloadWebviewLoginTest,
 
   SetAutoReloadInterval(0);  // 0 minutes
 
-  EXPECT_FALSE(LoginDisplayHost::default_host()
-                   ->GetOobeUI()
-                   ->GetHandler<GaiaScreenHandler>()
-                   ->GetAutoReloadManagerForTesting()
-                   .IsTimerActiveForTesting());
+  EXPECT_FALSE(IsAutoReloadActive());
+}
+
+IN_PROC_BROWSER_TEST_F(AutoReloadWebviewLoginTest,
+                       AutoreloadOnErrorScreenShown) {
+  SetAutoReloadInterval(10);  // 10 minutes
+
+  WaitForGaiaPageLoad();
+
+  AdvanceTime(base::Minutes(5));
+  EXPECT_TRUE(IsAutoReloadActive());
+
+  // Simulate network going offline.
+  network_portal_detector_.SimulateDefaultNetworkState(
+      NetworkPortalDetectorMixin::NetworkStatus::kOffline);
+  OobeScreenWaiter(ErrorScreenView::kScreenId).Wait();
+
+  EXPECT_FALSE(IsAutoReloadActive());
+
+  network_portal_detector_.SimulateDefaultNetworkState(
+      NetworkPortalDetectorMixin::NetworkStatus::kOnline);
+
+  WaitForGaiaPageReload();
+
+  EXPECT_TRUE(IsAutoReloadActive());
 }
 
 class ReauthWebviewLoginTest : public WebviewLoginTest {
