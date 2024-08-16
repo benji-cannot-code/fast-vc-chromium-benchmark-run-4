abb3e362470fe65d092849262c77608f9b44006f
Rasika Navarange <rasikan@google.com>
2024-08-16 14:12:56 +0000

    [Chrome Stdlib] Remove banned DROP in stdlib
    DROP is banned in the stdlib since
    https://android-review.googlesource.com/c/platform/external/perfetto/+/3220884
    
    This CL syncs the Chrome stdlib to comply with this.
    
    Change-Id: I415c34c023c1941d9afe1afe6fd30c4852eb1877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793219
    Commit-Queue: Rasika Navarange <rasikan@google.com>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342796}

--- base/tracing/stdlib/chrome/histograms.sql
+++ base/tracing/stdlib/chrome/histograms.sql
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 -- Use of this source code is governed by a BSD-style license that can be
 -- found in the LICENSE file.
 
-DROP VIEW IF EXISTS chrome_histograms;
-
 -- A helper view on top of the histogram events emitted by Chrome.
 -- Requires "disabled-by-default-histogram_samples" Chrome category.
 CREATE PERFETTO TABLE chrome_histograms(
