8e5b1e90d96a2fbc8efb47341927d4a816c7f1e5
Tao Wu <wutao@chromium.org>
2024-08-16 05:59:21 +0000

    growth: Add a switch for device current time
    We add a switch to provide a fake device current time for testing.
    
    Bug: 358675970
    Test: Added tests
    Change-Id: Ide8f6cc67f62296d6d9dfaff95e08e3ae942cbfa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792132
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342676}

--- chromeos/ash/components/growth/campaigns_matcher.cc
+++ chromeos/ash/components/growth/campaigns_matcher.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
+#include "ash/constants/ash_switches.h"
+#include "base/command_line.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/features.h"
@@ -63,6 +65,21 @@ inline constexpr char kEventGroupDismissalParam[] =
 
 inline constexpr char kUserPrefName[] = "name";
 inline constexpr char kUserPrefValue[] = "value";
+
+base::Time GetDeviceCurrentTimeForScheduling() {
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(
+          ash::switches::kGrowthCampaignsCurrentTimeSecondsSinceUnixEpoch)) {
+    const auto& value = command_line->GetSwitchValueASCII(
+        ash::switches::kGrowthCampaignsCurrentTimeSecondsSinceUnixEpoch);
+
+    double seconds_since_epoch;
+    CHECK(base::StringToDouble(value, &seconds_since_epoch));
+    return base::Time::FromSecondsSinceUnixEpoch(seconds_since_epoch);
+  }
+
+  return base::Time::Now();
+}
 
 bool MatchPref(const base::Value::List* criterias,
                std::string_view pref_path,
@@ -117,7 +134,7 @@ bool MatchSchedulings(const std::vector<std::unique_ptr<TimeWindowTargeting>>&
     return true;
   }
 
-  const auto now = base::Time::Now();
+  const auto now = GetDeviceCurrentTimeForScheduling();
   for (const auto& scheduling_targeting : scheduling_targetings) {
     if (MatchTimeWindow(*scheduling_targeting, now)) {
       return true;
