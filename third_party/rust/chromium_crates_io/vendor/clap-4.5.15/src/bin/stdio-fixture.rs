c1af2bb203dc236720dfeed6ddc28a0660ae6e03
Lukasz Anforowicz <lukasza@chromium.org>
2024-08-16 17:47:06 +0000

    Roll clap: 4.5.9 => 4.5.15 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * clap: 4.5.9 => 4.5.15
    * clap_builder: 4.5.9 => 4.5.15
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * clap@4.5.15, clap_builder@4.5.15: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: If46bd7315cc0d25a907cd276edfef7bbffb6abf0
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789614
    Reviewed-by: Adrian Taylor <adetaylor@google.com>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342909}

---
+++ third_party/rust/chromium_crates_io/vendor/clap-4.5.15/src/bin/stdio-fixture.rs
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+fn main() {
+    #[allow(unused_mut)]
+    let mut cmd = clap::Command::new("stdio-fixture")
+        .version("1.0")
+        .long_version("1.0 - a2132c")
+        .arg_required_else_help(true)
+        .subcommand(clap::Command::new("more"))
+        .arg(
+            clap::Arg::new("verbose")
+                .long("verbose")
+                .help("log")
+                .action(clap::ArgAction::SetTrue)
+                .long_help("more log"),
+        );
+    #[cfg(feature = "color")]
+    {
+        use clap::builder::styling;
+        const STYLES: styling::Styles = styling::Styles::styled()
+            .header(styling::AnsiColor::Green.on_default().bold())
+            .usage(styling::AnsiColor::Green.on_default().bold())
+            .literal(styling::AnsiColor::Blue.on_default().bold())
+            .placeholder(styling::AnsiColor::Cyan.on_default());
+        cmd = cmd.styles(STYLES);
+    }
+    cmd.get_matches();
+}
