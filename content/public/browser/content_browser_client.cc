df2260c3ddb09688b5e3557d1aa50531615cdb5e
Rakina Zata Amni <rakina@chromium.org>
2024-08-16 23:48:40 +0000

    Skip RenderDocument on WebView when initial page scale is non-default
    Some persistent settings can be changed by WebView apps, including
    ones affecting the initial page scale. Typically these settings changes
    are sent to the renderer everytime the RenderViewHost changes, so
    they can handle same-site RFH/RVH changes triggered by RenderDocument.
    However, settings that affect initial page scale needs to be set
    very early on, when the renderer-side swapping of RenderFrames happen,
    which happens earlier than the WebPreferences updates from the
    browser side. This might result in the initial page scale not being
    set correctly when RenderDocument is enabled.
    
    Fixing the copying of initial page scale is a bit complicated as the
    page scale calculation uses lots of inputs and triggers. While we work
    on fixing this, this CL allows WebView to opt-out of doing same-site
    RenderFrame swaps when the initial page scale is set to a non-default
    value, avoiding this problem altogether while still allowing some
    experimentation with RenderDocument on WebView to be done.
    
    Bug: 936696
    Change-Id: I0766c4e68a65916b12ea52c9c206d198620886f5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791938
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343109}

--- content/public/browser/content_browser_client.cc
+++ content/public/browser/content_browser_client.cc
@@ -1165,6 +1165,11 @@ bool ContentBrowserClient::ShouldOverrideUrlLoading(
 }
 #endif
 
+bool ContentBrowserClient::ShouldAllowSameSiteRenderFrameHostChange(
+    const RenderFrameHost& rfh) {
+  return true;
+}
+
 bool ContentBrowserClient::AllowRenderingMhtmlOverHttp(
     NavigationUIData* navigation_ui_data) {
   return false;
