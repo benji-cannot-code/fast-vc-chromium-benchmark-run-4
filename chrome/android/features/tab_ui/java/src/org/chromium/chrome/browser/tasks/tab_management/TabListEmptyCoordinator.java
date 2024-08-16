8a818d649924e6e5a8d89284052938569910872e
Calder Kitagawa <ckitagawa@chromium.org>
2024-08-16 18:41:17 +0000

    [Tab Switcher] Defer showing empty state until removal animations finish
    Bug: 346777141
    Change-Id: Ib75e13454e0209667bae89f3cd5c2f779c8044e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783551
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342940}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEmptyCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListEmptyCoordinator.java
@@ -11,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.chromium.base.Callback;
 import org.chromium.chrome.tab_ui.R;
 import org.chromium.ui.modelutil.ListObservable;
 import org.chromium.ui.modelutil.ListObservable.ListObserver;
@@ -29,12 +30,15 @@ class TabListEmptyCoordinator {
     private Context mContext;
     private TabListModel mModel;
     private ListObserver<Void> mListObserver;
+    private Callback<Runnable> mRunOnItemAnimatorFinished;
     private boolean mIsTabSwitcherShowing;
     private boolean mIsListObserverAttached;
 
-    public TabListEmptyCoordinator(ViewGroup rootView, TabListModel model) {
+    public TabListEmptyCoordinator(
+            ViewGroup rootView, TabListModel model, Callback<Runnable> runOnItemAnimatorFinished) {
         mRootView = rootView;
         mContext = rootView.getContext();
+        mRunOnItemAnimatorFinished = runOnItemAnimatorFinished;
 
         // Observe TabListModel to determine when to add / remove empty state view.
         mModel = model;
@@ -81,13 +85,24 @@ class TabListEmptyCoordinator {
         mImageView.setImageResource(imageResId);
     }
 
-    private void updateEmptyView() {
-        boolean isInEmptyState = mModel.size() == 0 && mIsTabSwitcherShowing;
-        boolean isEmptyViewAttached = mEmptyView != null && mEmptyView.getParent() != null;
+    private boolean isEmptyViewAttached() {
+        return mEmptyView != null && mEmptyView.getParent() != null;
+    }
 
-        if (isEmptyViewAttached) {
-            if (isInEmptyState) {
-                setEmptyViewVisibility(View.VISIBLE);
+    private boolean isInEmptyState() {
+        return mModel.size() == 0 && mIsTabSwitcherShowing;
+    }
+
+    private void updateEmptyView() {
+        if (isEmptyViewAttached()) {
+            if (isInEmptyState()) {
+                mRunOnItemAnimatorFinished.onResult(
+                        () -> {
+                            // Re-check requirements since this is now async.
+                            if (isEmptyViewAttached() && isInEmptyState()) {
+                                setEmptyViewVisibility(View.VISIBLE);
+                            }
+                        });
             } else {
                 setEmptyViewVisibility(View.GONE);
             }
--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListItemAnimator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListItemAnimator.java
@@ -92,8 +92,8 @@ public class TabListItemAnimator extends SimpleItemAnimator {
             for (var entry : mPendingAnimators.entrySet()) {
                 Animator animator = entry.getValue();
                 animator.setStartDelay(delay);
-                animator.start();
                 mRunningAnimators.put(entry.getKey(), animator);
+                animator.start();
             }
             mPendingAnimators.clear();
         }
@@ -106,8 +106,8 @@ public class TabListItemAnimator extends SimpleItemAnimator {
             Animator animator = mPendingAnimators.get(holder);
             mPendingAnimators.remove(holder);
             if (animator != null) {
-                animator.start();
                 mRunningAnimators.put(holder, animator);
+                animator.start();
                 // This call should remove the animator for mRunningAnimators.
                 animator.end();
             }
