391b2bb231235b08fc69a18d2fd4c5676193867f
Lucas Radaelli <lucasradaelli@google.com>
2024-08-16 18:45:00 +0000

    [a11y] Generate name for list markers with pseudo element styling.
    This change generates an accessible name for accessibility nodes that:
    - Are list markers;
    - Have pseudo element styling to alter the default bullet point.
    
    Bug: 354007664
    Test: DumpAccessibilityTreeTest.PseudoElementListMarker
    Change-Id: Ib8b9ae01210271963d258bd245be96f4d90d1860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763539
    Reviewed-by: Lucas Radaelli <lucasradaelli@google.com>
    Commit-Queue: Lucas Radaelli <lucasradaelli@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342944}

--- third_party/blink/renderer/modules/accessibility/ax_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object.cc
@@ -7550,6 +7550,7 @@ bool AXObject::SupportsNameFromContents(bool recursive) const {
     case ax::mojom::blink::Role::kLineBreak:
     case ax::mojom::blink::Role::kLink:
     case ax::mojom::blink::Role::kListBoxOption:
+    case ax::mojom::blink::Role::kListMarker:
     case ax::mojom::blink::Role::kMath:
     case ax::mojom::blink::Role::kMenuItem:
     case ax::mojom::blink::Role::kMenuItemCheckBox:
@@ -7766,7 +7767,6 @@ bool AXObject::SupportsNameFromContents(bool recursive) const {
     case ax::mojom::blink::Role::kLegend:
     case ax::mojom::blink::Role::kList:
     case ax::mojom::blink::Role::kListItem:
-    case ax::mojom::blink::Role::kListMarker:
     case ax::mojom::blink::Role::kMark:
     case ax::mojom::blink::Role::kNone:
     case ax::mojom::blink::Role::kParagraph:
