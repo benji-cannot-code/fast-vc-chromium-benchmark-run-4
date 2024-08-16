7a03eef082cd32ed59837f330547e3f8c1caa959
Kurt Catti-Schmidt <kschmi@microsoft.com>
2024-08-16 15:07:18 +0000

    [RenderText] Remove unused parameter from OnLayoutTextAttributeChanged
    Removing an unused parameter, no behavioral changes.
    
    Bug: 40270295, 40267908
    Change-Id: I62fc94d3d30ba8f534c1e4410a21ce975f98a5fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792122
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1342818}

--- ui/gfx/render_text_harfbuzz.cc
+++ ui/gfx/render_text_harfbuzz.cc
@@ -1727,8 +1727,8 @@ SelectionModel RenderTextHarfBuzz::AdjacentLineSelectionModel(
   return next;
 }
 
-void RenderTextHarfBuzz::OnLayoutTextAttributeChanged(bool text_changed) {
-  RenderText::OnLayoutTextAttributeChanged(text_changed);
+void RenderTextHarfBuzz::OnLayoutTextAttributeChanged() {
+  RenderText::OnLayoutTextAttributeChanged();
 
   update_layout_run_list_ = true;
   OnDisplayTextAttributeChanged();
