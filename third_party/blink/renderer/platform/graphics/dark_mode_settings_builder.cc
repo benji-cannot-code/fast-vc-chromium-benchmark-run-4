4eff1451ceb82da8c89f1630511096b1f3fd9f8e
Takashi Toyoshima <toyoshim@chromium.org>
2024-08-16 09:11:11 +0000

    Call base::FeatureParam<>::Get instead of base::GetFieldTrialParam...
    As base::FeatureParam<> has a embedded call to obtain the corresponding
    parameter, let's use it instead of the direct call of
    base::GetFieldTrialParamByFeatureAsInt().
    
    Bug: 360291576
    Change-Id: I237814ae9f0cf0882da21e37a86f51d0138aee82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786292
    Auto-Submit: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342715}

--- third_party/blink/renderer/platform/graphics/dark_mode_settings_builder.cc
+++ third_party/blink/renderer/platform/graphics/dark_mode_settings_builder.cc
@@ -124,9 +124,8 @@ DarkModeImagePolicy GetImagePolicy(const SwitchParams& switch_params) {
 }
 
 int GetForegroundBrightnessThreshold(const SwitchParams& switch_params) {
-  const int flag_value = base::GetFieldTrialParamByFeatureAsInt(
-      features::kForceWebContentsDarkMode,
-      features::kForceDarkForegroundLightnessThresholdParam.name, -1);
+  const int flag_value =
+      features::kForceDarkForegroundLightnessThresholdParam.Get();
   return flag_value >= 0 ? flag_value
                          : GetIntegerSwitchParamValue<int>(
                                switch_params, "ForegroundBrightnessThreshold",
@@ -134,9 +133,8 @@ int GetForegroundBrightnessThreshold(const SwitchParams& switch_params) {
 }
 
 int GetBackgroundBrightnessThreshold(const SwitchParams& switch_params) {
-  const int flag_value = base::GetFieldTrialParamByFeatureAsInt(
-      features::kForceWebContentsDarkMode,
-      features::kForceDarkBackgroundLightnessThresholdParam.name, -1);
+  const int flag_value =
+      features::kForceDarkBackgroundLightnessThresholdParam.Get();
   return flag_value >= 0 ? flag_value
                          : GetIntegerSwitchParamValue<int>(
                                switch_params, "BackgroundBrightnessThreshold",
