fc465cca4c63c65e8fec8a4a3927ca50fc9123f1
Ahmed Moussa <ahmedmoussa@google.com>
2024-08-16 23:33:18 +0000

    [code-health-rotation] MiraclePtr rewrite  ExtensionsMenuMainPageView
    Remove RAW_PTR_EXCLUSION, and convert `views::View*` to  `raw_ptr<views::View>`.
    
    Bug: 346693498
    Change-Id: I0f4995917b7badd107f3eb667997c130eb994d07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789650
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Ahmed Moussa <ahmedmoussa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343099}

--- chrome/browser/ui/views/extensions/extensions_menu_main_page_view.h
+++ chrome/browser/ui/views/extensions/extensions_menu_main_page_view.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_MAIN_PAGE_VIEW_H_
 #define CHROME_BROWSER_UI_VIEWS_EXTENSIONS_EXTENSIONS_MENU_MAIN_PAGE_VIEW_H_
 
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/toolbar/toolbar_actions_model.h"
 #include "chrome/browser/ui/views/extensions/extensions_menu_item_view.h"
-#include "ui/views/view.h"
-
 #include "ui/base/metadata/metadata_header_macros.h"
+#include "ui/views/view.h"
 
 namespace content {
 class WebContents;
@@ -139,9 +138,7 @@ class ExtensionsMenuMainPageView : public views::View {
   // The view containing the menu items. This is separated for easy insertion
   // and iteration of menu items. The children are guaranteed to only be
   // ExtensionMenuItemViews.
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION views::View* menu_items_ = nullptr;
+  raw_ptr<views::View> menu_items_ = nullptr;
 };
 
 BEGIN_VIEW_BUILDER(/* no export */, ExtensionsMenuMainPageView, views::View)
