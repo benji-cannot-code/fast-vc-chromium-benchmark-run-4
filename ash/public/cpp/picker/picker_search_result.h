26a9518e737c69f329a1421d46698bf53be268f8
Michelle <michellegc@google.com>
2024-08-16 05:49:48 +0000

    Show link icon for Picker clipboard items containing a url.
    Add a kUrl display format for Picker clipboard items, which behaves
    similarly to KText items but shows a link icon instead of a text icon.
    This is to match the multipaste clipboard UI.
    
    Bug: b:341799941
    Change-Id: If7a6487e21bb8d7567225d28c3284031add638e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792817
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342675}

--- ash/public/cpp/picker/picker_search_result.h
+++ ash/public/cpp/picker/picker_search_result.h
@@ -79,6 +79,7 @@ class ASH_PUBLIC_EXPORT PickerSearchResult {
       kText,
       kImage,
       kHtml,
+      kUrl,
     };
 
     // Unique ID that specifies which item in the clipboard this refers to.
