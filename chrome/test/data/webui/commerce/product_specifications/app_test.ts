7334410b8aa6f06a522a0c7b91ac4d097b4eba7f
Matt Jones <mdjones@chromium.org>
2024-08-16 15:37:05 +0000

    [Compare] Update more strings
    This patch updates the empty state title and description as well as the
    default title for comparisons that aren't created through a endpoint
    that uses clustering.
    
    Bug: 360180857, 346381503
    Change-Id: Id3bf24385370d344fb5f4c9b6e6120b9e671652a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791912
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342839}

--- chrome/test/data/webui/commerce/product_specifications/app_test.ts
+++ chrome/test/data/webui/commerce/product_specifications/app_test.ts
@@ -146,8 +146,11 @@ suite('AppTest', () => {
 
   setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
-    loadTimeData.overrideValues(
-        {priceRowTitle: 'price', productSummaryRowTitle: 'summary'});
+    loadTimeData.overrideValues({
+      defaultTableTitle: 'title',
+      priceRowTitle: 'price',
+      productSummaryRowTitle: 'summary',
+    });
     shoppingServiceApi.reset();
     shoppingServiceApi.setResultFor('getCallbackRouter', callbackRouter);
     shoppingServiceApi.setResultFor(
@@ -171,7 +174,7 @@ suite('AppTest', () => {
     assertEquals(
         1, shoppingServiceApi.getCallCount('addProductSpecificationsSet'));
     assertEquals(
-        'Product specs',
+        'title',
         shoppingServiceApi.getArgs('addProductSpecificationsSet')[0][0]);
     assertArrayEquals(
         urlsParam.map(url => ({url})),
@@ -254,7 +257,7 @@ suite('AppTest', () => {
     assertEquals(
         1, shoppingServiceApi.getCallCount('addProductSpecificationsSet'));
     assertEquals(
-        'Product specs',
+        'title',
         shoppingServiceApi.getArgs('addProductSpecificationsSet')[0][0]);
     const mappedUrlsParams = urlsParam.map(url => ({url}));
     assertArrayEquals(
@@ -1314,7 +1317,7 @@ suite('AppTest', () => {
 
       // TODO(b/338427523): Parameterize this test once there is UI for
       // choosing the name.
-      assertEquals('Product specs', appElement.$.header.subtitle);
+      assertEquals('title', appElement.$.header.subtitle);
     });
 
     test('displays correct subtitle for empty state', async () => {
