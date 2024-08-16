ab2639c245a513f80cdf77fedd9767db6e01592d
Sirisha Kavuluru <skavuluru@google.com>
2024-08-16 18:12:18 +0000

    [TabGroupContextMenu] Anchor menu to title on strip.
    Switch to smaller width if window width < max menu width. Color picker moves to 2 rows in this case.
    
    Demo: https://screencast.googleplex.com/cast/NDg0NzA2MTg5ODg4NzE2OHwyNDY5YmQxMi1jNQ
    
    Screenshots:
    1-row picker: https://screenshot.googleplex.com/AQ4yi9bBaPin4FP
    2-row picker: https://screenshot.googleplex.com/6ep8cTY2UDKLqJt
    
    Bug: 354258700
    Change-Id: Iaf3a85a0dc962041a07c891e92e3f825ddfbaa22
    Low-Coverage-Reason: Planning to add tests for a method in a followup CL to not bloat this one.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5781783
    Reviewed-by: Zhe Li <zheliooo@google.com>
    Commit-Queue: Sirisha Kavuluru <skavuluru@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342924}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutGroupTitle.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutGroupTitle.java
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.compositor.overlays.strip;
 
+import android.content.Context;
+import android.graphics.Rect;
 import android.util.FloatProperty;
 
 import androidx.annotation.ColorInt;
 
 import org.chromium.base.MathUtils;
+import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.ui.base.LocalizationUtils;
@@ -20,6 +23,26 @@ import org.chromium.ui.base.LocalizationUtils;
  * onto the GL canvas.
  */
 public class StripLayoutGroupTitle extends StripLayoutView {
+
+    private final Context mContext;
+
+    /**
+     * Get the bounds of the view w.r.t screen.
+     *
+     * @param out Screen coordinates of the view to populate.
+     * @param windowRectSupplier Supplier for holder window bounds for computation.
+     */
+    public void getDrawBoundsOnScreen(Rect out, Supplier<Rect> windowRectSupplier) {
+        float dpToPx = mContext.getResources().getDisplayMetrics().density;
+        int leftWithOffset = (int) (getPaddedX() * dpToPx) + windowRectSupplier.get().left;
+        int topWithOffset = (int) (getPaddedY() * dpToPx) + windowRectSupplier.get().top;
+        out.set(
+                leftWithOffset,
+                topWithOffset,
+                (int) (leftWithOffset + (getPaddedWidth() * dpToPx)),
+                (int) (topWithOffset + (getPaddedHeight() * dpToPx)));
+    }
+
     /** Delegate for additional group title functionality. */
     public interface StripLayoutGroupTitleDelegate {
         /**
@@ -95,10 +118,14 @@ public class StripLayoutGroupTitle extends StripLayoutView {
      * @param rootId The root ID for the tab group.
      */
     public StripLayoutGroupTitle(
-            StripLayoutGroupTitleDelegate delegate, boolean incognito, int rootId) {
+            Context context,
+            StripLayoutGroupTitleDelegate delegate,
+            boolean incognito,
+            int rootId) {
         super(incognito);
         assert rootId != Tab.INVALID_TAB_ID : "Tried to create a group title for an invalid group.";
         mRootId = rootId;
+        mContext = context;
         mDelegate = delegate;
     }
 
