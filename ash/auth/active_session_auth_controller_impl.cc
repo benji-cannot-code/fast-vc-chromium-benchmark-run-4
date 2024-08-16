c8a953d31665c8694c50ff811814116c311b5811
Istvan Nagy <iscsi@google.com>
2024-08-16 11:53:39 +0000

    [High5] Fix the SetState call, to only call when contents_view created
    Bug: b:359638290
    Change-Id: Ia77cab9cbed4bc7107dbd4924251938410de2614
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793352
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Elie Maamari <emaamari@google.com>
    Cr-Commit-Position: refs/heads/main@{#1342750}

--- ash/auth/active_session_auth_controller_impl.cc
+++ ash/auth/active_session_auth_controller_impl.cc
@@ -219,7 +219,6 @@ void ActiveSessionAuthControllerImpl::OnAuthSessionStarted(
     return;
   }
 
-  SetState(ActiveSessionAuthState::kInitialized);
   uma_recorder_.RecordShow(reason_);
 
   auth_factor_editor_->GetAuthFactorsConfiguration(
@@ -265,6 +264,7 @@ void ActiveSessionAuthControllerImpl::InitUi() {
   widget_ = CreateAuthDialogWidget(std::move(contents_view));
   contents_view_observer_.Observe(contents_view_);
   contents_view_->AddObserver(this);
+  SetState(ActiveSessionAuthState::kInitialized);
 
   MoveToTheCenter();
   widget_->Show();
@@ -416,7 +416,7 @@ void ActiveSessionAuthControllerImpl::SetState(ActiveSessionAuthState state) {
   LOG(WARNING) << "SetState is requested from: "
                << ActiveSessionAuthStateToString(state_)
                << " state to : " << ActiveSessionAuthStateToString(state)
-               << "state.";
+               << " state.";
   switch (state) {
     case ActiveSessionAuthState::kWaitForInit:
       break;
@@ -424,9 +424,7 @@ void ActiveSessionAuthControllerImpl::SetState(ActiveSessionAuthState state) {
       CHECK(state_ == ActiveSessionAuthState::kWaitForInit ||
             state_ == ActiveSessionAuthState::kPasswordAuthStarted ||
             state_ == ActiveSessionAuthState::kPinAuthStarted);
-      if (contents_view_) {
-        contents_view_->SetInputEnabled(true);
-      }
+      contents_view_->SetInputEnabled(true);
       break;
     case ActiveSessionAuthState::kPasswordAuthStarted:
       // Disable the UI while we are waiting for the response, except the close
