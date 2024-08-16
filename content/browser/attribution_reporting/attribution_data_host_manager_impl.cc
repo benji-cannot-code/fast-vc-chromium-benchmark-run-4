162d65392c5b7af355fd4b5cda887d63e34cc175
Nan Lin <linnan@chromium.org>
2024-08-16 15:30:22 +0000

    Remove NotFatalUntil in AttributionDataHostManagerImpl
    We have not seen these be triggered in the wild. Additionally, these are
    now fatal as M128 has been reached; so, removing the argument should be
    a no-op.
    
    Change-Id: I150675175f7d1d78415219f31cf5be9078ce0ee1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793552
    Auto-Submit: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1342831}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.cc
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "base/metrics/field_trial_params.h"
 #include "base/metrics/histogram_functions.h"
-#include "base/not_fatal_until.h"
 #include "base/notreached.h"
 #include "base/time/time.h"
 #include "base/types/expected.h"
@@ -591,8 +590,7 @@ struct AttributionDataHostManagerImpl::HeaderPendingDecode {
         reporting_url(std::move(reporting_url)),
         report_header_errors(report_header_errors),
         registration_type(registration_type) {
-    CHECK_EQ(*this->reporting_origin, url::Origin::Create(this->reporting_url),
-             base::NotFatalUntil::M128);
+    CHECK_EQ(*this->reporting_origin, url::Origin::Create(this->reporting_url));
   }
 
   HeaderPendingDecode(const HeaderPendingDecode&) = delete;
@@ -698,7 +696,7 @@ class AttributionDataHostManagerImpl::Registrations {
   bool operator<(const Registrations& other) const { return id() < other.id(); }
 
   void CompleteRegistrations() {
-    CHECK(!registrations_complete_, base::NotFatalUntil::M128);
+    CHECK(!registrations_complete_);
     registrations_complete_ = true;
   }
 
@@ -952,8 +950,7 @@ struct AttributionDataHostManagerImpl::PendingRegistrationData {
       : headers(std::move(headers)),
         reporting_origin(std::move(reporting_origin)),
         reporting_url(std::move(reporting_url)) {
-    CHECK_EQ(*this->reporting_origin, url::Origin::Create(this->reporting_url),
-             base::NotFatalUntil::M128);
+    CHECK_EQ(*this->reporting_origin, url::Origin::Create(this->reporting_url));
   }
 
   PendingRegistrationData(const PendingRegistrationData&) = delete;
@@ -1404,7 +1401,7 @@ void AttributionDataHostManagerImpl::NotifyNavigationRegistrationStarted(
     // when the navigation data host disconnects.
     auto [__, inserted] =
         ongoing_background_datahost_registrations_.emplace(navigation_id);
-    CHECK(inserted, base::NotFatalUntil::M128);
+    CHECK(inserted);
 
     receivers_.Add(
         this, std::move(it->second),
@@ -1742,7 +1739,7 @@ void AttributionDataHostManagerImpl::SourceDataAvailable(
     attribution_reporting::SourceRegistration data,
     bool was_fetched_via_service_worker) {
   // This is validated by the Mojo typemapping.
-  CHECK(reporting_origin.IsValid(), base::NotFatalUntil::M128);
+  CHECK(reporting_origin.IsValid());
 
   const RegistrationContext* context =
       GetReceiverRegistrationContextForSource();
@@ -1784,7 +1781,7 @@ void AttributionDataHostManagerImpl::TriggerDataAvailable(
     attribution_reporting::TriggerRegistration data,
     bool was_fetched_via_service_worker) {
   // This is validated by the Mojo typemapping.
-  CHECK(reporting_origin.IsValid(), base::NotFatalUntil::M128);
+  CHECK(reporting_origin.IsValid());
 
   const RegistrationContext* context =
       GetReceiverRegistrationContextForTrigger();
@@ -1903,7 +1900,7 @@ void AttributionDataHostManagerImpl::NotifyFencedFrameReportingBeaconData(
     return;
   }
 
-  CHECK(!it->registrations_complete(), base::NotFatalUntil::M128);
+  CHECK(!it->registrations_complete());
   if (is_final_response) {
     it->CompleteRegistrations();
   }
