c1b02feb7e404c2ca4f624602c5f1e963a83c520
Matt Jones <mdjones@chromium.org>
2024-08-16 23:54:44 +0000

    [Compare] Gate fetching specs on parental controls and model execution flag
    This patch adds checks to the data fetching logic for compare to ensure
    that the account is not subject to parental controls and is allowed to
    run model execution features.
    
    Bug: 359870770
    Change-Id: Iafd01bbf9c0a92423bfcba781c2a578cfd1355d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788085
    Reviewed-by: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343115}

--- components/commerce/core/mock_account_checker.h
+++ components/commerce/core/mock_account_checker.h
@@ -37,6 +37,8 @@ class MockAccountChecker : public AccountChecker {
 
   MOCK_METHOD(bool, IsSubjectToParentalControls, (), (override));
 
+  MOCK_METHOD(bool, CanUseModelExecutionFeatures, (), (override));
+
   MOCK_METHOD(std::string, GetCountry, (), (override));
 
   MOCK_METHOD(std::string, GetLocale, (), (override));
@@ -50,6 +52,8 @@ class MockAccountChecker : public AccountChecker {
   void SetAnonymizedUrlDataCollectionEnabled(bool enabled);
 
   void SetIsSubjectToParentalControls(bool subject_to_parental_controls);
+
+  void SetCanUseModelExecutionFeatures(bool can_use_model_execution_features);
 
   void SetCountry(std::string country);
 
