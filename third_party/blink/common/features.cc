c5ef7b8743df63247d0ce51cbd44b84531df2735
Brad Triebwasser <btriebw@chromium.org>
2024-08-17 00:50:02 +0000

    Enable IndexedDBCompressValuesWithSnappy feature by default.
    Bug: 40274247, 342779913
    Change-Id: I4d9069a749a5f6f0c927176f03ff879b1c2cf0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784562
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343136}

--- third_party/blink/common/features.cc
+++ third_party/blink/common/features.cc
@@ -1093,7 +1093,7 @@ BASE_FEATURE(kImageLoadingPrioritizationFix,
 
 BASE_FEATURE(kIndexedDBCompressValuesWithSnappy,
              "IndexedDBCompressValuesWithSnappy",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 constexpr base::FeatureParam<int>
     kIndexedDBCompressValuesWithSnappyCompressionThreshold{
         &features::kIndexedDBCompressValuesWithSnappy,
