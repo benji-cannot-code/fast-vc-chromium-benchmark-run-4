2a9db97634274633b4e89614deefe22cd0c25144
Wenyu Fu <wenyufu@chromium.org>
2024-08-17 02:20:44 +0000

    Reland "[E2E] Add EdgeToEdgePTTest"
    This is a reland of commit 6fc7a3a152d7ccf8b1f244253d22fb17a7d4a885
    
    Adding test filter so the test runs only on phones, and override the
    3-button nav bar check.
    Reland: Base -> PS#1
    Fixes: PS#1 -> latest
    
    Original change's description:
    > [E2E] Add EdgeToEdgePTTest
    >
    > Adds a basic instrumentation test that verifies the chin exists.
    >
    > The preview of added web page can be found at https://chrome-edge-to-edge-v2.glitch.me/sub-frames.html
    >
    > Bug: 352402600
    > Change-Id: Ibe5d8008e5717ca59c87550a9fcf85c4abaf96a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789861
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Charles Hager <clhager@google.com>
    > Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1342694}
    
    Bug: 352402600
    Change-Id: Idfb13bc32829b6ba863262cd35f606bb9756f94e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792523
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1343158}

--- chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ui/RootUiCoordinator.java
@@ -2189,6 +2189,10 @@ public class RootUiCoordinator
         mActivity = null;
     }
 
+    public BottomControlsStacker getBottomControlsStackerForTesting() {
+        return mBottomControlsStacker;
+    }
+
     public DataSharingTabManager getDataSharingTabManager() {
         return mDataSharingTabManager;
     }
