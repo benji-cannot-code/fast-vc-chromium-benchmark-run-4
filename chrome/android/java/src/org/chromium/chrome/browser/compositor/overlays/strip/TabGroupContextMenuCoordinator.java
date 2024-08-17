15c4d55ec43f931cb19c50ff6aa8a9a2db0d7427
Zhe Li <zheliooo@google.com>
2024-08-17 00:10:50 +0000

    [TabGroupContextMenu] Record user action
    Record for menuShown, titleChanged, titleReset, colorChanged, ungroup, closeGroup, deleteGroup, newTabInGroup
    
    Bug: 358415453
    Change-Id: I1bd8742b341e7d30230855aecc722e65952cd933
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL records user action for the menu actions, where the changes are trivial.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788251
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343118}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabGroupContextMenuCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/TabGroupContextMenuCoordinator.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.compositor.overlays.strip;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewStub;
 import android.widget.EditText;
@@ -15,6 +16,7 @@ import androidx.annotation.VisibleForTesting;
 import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.content.res.ResourcesCompat;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.tab.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.TabCreator;
@@ -47,6 +49,7 @@ import org.chromium.ui.widget.RectProvider;
  * responsible for creating a list of menu items, setting up the menu and displaying the menu.
  */
 public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordinator {
+    private static final String MENU_USER_ACTION_PREFIX = "MobileToolbarTabGroupMenu.";
     private View mContentView;
     private EditText mGroupTitleEditText;
     private ColorPickerCoordinator mColorPickerCoordinator;
@@ -103,6 +106,7 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
                         actionConfirmationManager,
                         tabId,
                         isTabGroupSyncEnabled);
+                recordUserAction("Ungroup");
             } else if (menuId == org.chromium.chrome.R.id.close_tab) {
                 TabUiUtils.closeTabGroup(
                         tabGroupModelFilter,
@@ -111,6 +115,7 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
                         /* hideTabGroups= */ true,
                         isTabGroupSyncEnabled,
                         /* didCloseCallback= */ null);
+                recordUserAction("CloseGroup");
             } else if (menuId == org.chromium.chrome.R.id.delete_tab) {
                 TabUiUtils.closeTabGroup(
                         tabGroupModelFilter,
@@ -119,9 +124,11 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
                         /* hideTabGroups= */ false,
                         isTabGroupSyncEnabled,
                         /* didCloseCallback= */ null);
+                recordUserAction("DeleteGroup");
             } else if (menuId == org.chromium.chrome.R.id.open_new_tab_in_group) {
                 TabUiUtils.openNtpInGroup(
                         tabGroupModelFilter, tabCreator, tabId, TabLaunchType.FROM_TAB_GROUP_UI);
+                recordUserAction("NewTabInGroup");
             }
         };
     }
@@ -142,6 +149,7 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
                 /* verticalOverlapAnchor= */ false,
                 /* animStyle= */ ResourcesCompat.ID_NULL,
                 mWindowAndroid.getActivity().get());
+        recordUserAction("Shown");
     }
 
     @Override
@@ -222,6 +230,7 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
 
     @Override
     protected void onMenuDismissed() {
+        // TODO(Crbug.com/360044398) Record user action dismiss without any action taken.
         updateTabGroupTitle();
         mWindowAndroid
                 .getKeyboardDelegate()
@@ -235,15 +244,20 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
 
     private void updateTabGroupColor() {
         @TabGroupColorId int newColor = mColorPickerCoordinator.getSelectedColorSupplier().get();
-        TabUiUtils.updateTabGroupColor(mTabGroupModelFilter, mGroupRootId, newColor);
+        if (TabUiUtils.updateTabGroupColor(mTabGroupModelFilter, mGroupRootId, newColor)) {
+            recordUserAction("ColorChanged");
+        }
     }
 
     @VisibleForTesting
     void updateTabGroupTitle() {
         String newTitle = mGroupTitleEditText.getText().toString();
-        TabUiUtils.updateTabGroupTitle(mTabGroupModelFilter, mGroupRootId, newTitle);
-        if (mTabGroupModelFilter.getTabGroupTitle(mGroupRootId) == null) {
+        if (newTitle == null || TextUtils.isEmpty(newTitle)) {
+            mTabGroupModelFilter.deleteTabGroupTitle(mGroupRootId);
+            recordUserAction("TitleReset");
             setEditTextToDefaultGroupTitle();
+        } else if (TabUiUtils.updateTabGroupTitle(mTabGroupModelFilter, mGroupRootId, newTitle)) {
+            recordUserAction("TitleChanged");
         }
     }
 
@@ -311,6 +325,10 @@ public class TabGroupContextMenuCoordinator extends TabGroupOverflowMenuCoordina
         @TabGroupColorId
         int curGroupColor = mTabGroupModelFilter.getTabGroupColorWithFallback(mGroupRootId);
         mColorPickerCoordinator.setSelectedColorItem(curGroupColor);
+    }
+
+    private static void recordUserAction(String action) {
+        RecordUserAction.record(MENU_USER_ACTION_PREFIX + action);
     }
 
     EditText getGroupTitleEditTextForTesting() {
