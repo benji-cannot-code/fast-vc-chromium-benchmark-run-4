f6c2d1df8a44cab91050acfeea4993bf6a7a0cde
Yuki Awano <yawano@google.com>
2024-08-17 01:56:47 +0000

    quick-answers: refactor UserConsentView
    - As a preparation for a design refresh of UserConsentView, refactor
      UserConsentView as it adopts views::Builder and a flag to switch its
      design.
    - views::Builder makes the structure of ui tree clear, which will make
      it easier to implement a design variant.
    - Refreshed design will be implemented by a follow up CL.
    
    Bug: b:340628664
    Test: browser_test --gtest_filter=*QuickAnswersPixelTest*
    Cq-Include-Trybots: luci.chromium.try:linux-chromeos-asan-rel,linux_chromium_chromeos_asan_rel_ng
    Change-Id: Ibc906050ded6c85898c68c9decbf77fc9101109d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789932
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343153}

--- chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.h
+++ chrome/browser/ui/chromeos/read_write_cards/read_write_cards_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ref.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/views/metadata/view_factory.h"
 #include "ui/views/view.h"
 
 namespace views {
@@ -66,6 +67,11 @@ class ReadWriteCardsView : public views::View {
       read_write_cards_ui_controller_;
 };
 
+BEGIN_VIEW_BUILDER(/* no export */, ReadWriteCardsView, views::View)
+END_VIEW_BUILDER
+
 }  // namespace chromeos
+
+DEFINE_VIEW_BUILDER(/* no export */, chromeos::ReadWriteCardsView)
 
 #endif  // CHROME_BROWSER_UI_CHROMEOS_READ_WRITE_CARDS_READ_WRITE_CARDS_VIEW_H_
