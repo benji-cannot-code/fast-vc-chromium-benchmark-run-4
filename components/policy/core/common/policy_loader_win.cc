49e1a55e385fc66145c9a1109dff2b6ebaa0b805
Owen Min <zmin@chromium.org>
2024-08-19 09:37:55 +0000

    Delete kCriticalPolicySection feature flag
    The feature has been launched for a while, hence cleanup the code.
    
    Bug: None
    Change-Id: Ie3d1755818787c8e3ae26f3b83041c2eb9661635
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792515
    Auto-Submit: Owen Min <zmin@chromium.org>
    Commit-Queue: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343388}

--- components/policy/core/common/policy_loader_win.cc
+++ components/policy/core/common/policy_loader_win.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/enterprise_util.h"
-#include "base/feature_list.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -75,11 +74,6 @@ enum WindowsProfileType {
 const char kKeyMandatory[] = "policy";
 const char kKeyRecommended[] = "recommended";
 const char kKeyThirdParty[] = "3rdparty";
-
-// Kill switcher for critical policy section API usage.
-BASE_FEATURE(kCriticalPolicySection,
-             "CriticalPolicySection",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Parses |gpo_dict| according to |schema| and writes the resulting policy
 // settings to |policy| for the given |scope| and |level|.
@@ -353,11 +347,6 @@ PolicyBundle PolicyLoaderWin::Load() {
 }
 
 void PolicyLoaderWin::Reload(bool force) {
-  if (!base::FeatureList::IsEnabled(kCriticalPolicySection)) {
-    AsyncPolicyLoader::Reload(force);
-    return;
-  }
-
   // If we need to get management bit first, no need to enter the critical
   // section as we won't actual read the policy.
   if (NeedManagementBitBeforeLoad()) {
