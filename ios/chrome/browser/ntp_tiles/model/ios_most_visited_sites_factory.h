e6c3958a0873adb8c1635225f3a8d45b462cf0d7
Mark Cogan <marq@google.com>
2024-08-19 20:44:57 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ['ios/chrome/browser/ntp_tiles/model', 'ios/chrome/browser/visited_url_ranking'].
    
    This CL was uploaded by git cl split.
    
    R=tiborg@chromium.org
    
    Bug: 359819865
    Change-Id: I9bc03657865c5d99e1f7ebc261a7ff9276c19daa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797343
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343737}

--- ios/chrome/browser/ntp_tiles/model/ios_most_visited_sites_factory.h
+++ ios/chrome/browser/ntp_tiles/model/ios_most_visited_sites_factory.h
@@ -6,9 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_NTP_TILES_MODEL_IOS_MOST_VISITED_SITES_FACTORY_H_
 #define IOS_CHROME_BROWSER_NTP_TILES_MODEL_IOS_MOST_VISITED_SITES_FACTORY_H_
 
-#include <memory>
+#import <memory>
 
-class ChromeBrowserState;
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 namespace ntp_tiles {
 class MostVisitedSites;
