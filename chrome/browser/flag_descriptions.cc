83e48ad8aca8790e7ac0b05a73f0952beaaae5c9
Akihiro Ota <akihiroota@chromium.org>
2024-08-19 22:17:40 +0000

    FaceGaze: Add feature flag for gravity wells behavior
    Bug: 356131679
    Change-Id: I0ac586f1edf39f8183ef1acf25ec99a89bbd727d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796386
    Reviewed-by: Tim <tjudkins@chromium.org>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343796}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1268,6 +1268,11 @@ const char kAccessibilityFaceGazeName[] = "Experimental FaceGaze integration";
 const char kAccessibilityFaceGazeDescription[] =
     "This option enables the experimental FaceGaze ChromeOS integration";
 
+const char kAccessibilityFaceGazeGravityWellsName[] =
+    "Experimental FaceGaze gravity wells";
+const char kAccessibilityFaceGazeGravityWellsDescription[] =
+    "This option enables the experimental gravity wells behavior for FaceGaze";
+
 const char kAccessibilityMagnifierFollowsChromeVoxName[] =
     "Magnifier follows ChromeVox focus";
 const char kAccessibilityMagnifierFollowsChromeVoxDescription[] =
--- chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
@@ -214,6 +214,7 @@ declare global {
         DICTATION_CONTEXT_CHECKING = 'dictationContextChecking',
         FACE_GAZE = 'faceGaze',
         GOOGLE_TTS_HIGH_QUALITY_VOICES = 'googleTtsHighQualityVoices',
+        FACE_GAZE_GRAVITY_WELLS = 'faceGazeGravityWells',
       }
 
       export enum SelectToSpeakPanelAction {
