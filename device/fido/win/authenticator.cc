de0adbd926e16fea4efb7fac038b9b62b7c4e2f4
Martin Kreichgauer <martinkr@google.com>
2024-08-19 22:59:45 +0000

    webauthn: don't override IsUVPAA in Incognito on Windows 10
    We historically have disabled credential creation in the Windows
    platform authenticator in Incognito to avoid leaking credentials beyond
    the lifetime of the Incognito window. In CL:4200862, we relaxed this
    override on versions of Windows where the native API lets us display an
    incognito mode warning message.
    
    But we shouldn't probably override IsUVPAA in Incognito at all. Even if
    we disable credential **creation** because the privacy implications
    aren't clear otherwise, **signin** in Incognito still does and should
    work. But if IsUVPAA returns false, some websites may chose not to offer
    passkey login, which is unfortunate.
    
    Fixed: 40918391
    Change-Id: Ib390a1134011488dc328ff7828d9af5693dcd547
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791932
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343822}

--- device/fido/win/authenticator.cc
+++ device/fido/win/authenticator.cc
@@ -91,7 +91,6 @@ void FilterFoundCredentials(
 
 // static
 void WinWebAuthnApiAuthenticator::IsUserVerifyingPlatformAuthenticatorAvailable(
-    bool is_off_the_record,
     WinWebAuthnApi* api,
     base::OnceCallback<void(bool is_available)> callback) {
   base::ThreadPool::PostTaskAndReplyWithResult(
@@ -99,19 +98,16 @@ void WinWebAuthnApiAuthenticator::IsUserVerifyingPlatformAuthenticatorAvailable(
       {base::TaskPriority::USER_VISIBLE, base::MayBlock(),
        base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
       base::BindOnce(
-          [](bool is_off_the_record, WinWebAuthnApi* api) {
+          [](WinWebAuthnApi* api) {
             BOOL result;
             if (!api || !api->IsAvailable()) {
-              return false;
-            }
-            if (is_off_the_record && api->Version() < WEBAUTHN_API_VERSION_4) {
               return false;
             }
             return api->IsUserVerifyingPlatformAuthenticatorAvailable(
                        &result) == S_OK &&
                    result == TRUE;
           },
-          is_off_the_record, api),
+          api),
       std::move(callback));
 }
 
