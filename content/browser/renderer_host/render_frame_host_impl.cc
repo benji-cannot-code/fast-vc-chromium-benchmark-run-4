e8b8eac5277b05c262a5e3f434b20a4fb1c478cd
Dylan Cutler <dylancutler@google.com>
2024-08-19 19:51:51 +0000

    Do not check if document is sandboxed for UB without PolicyContainerHost
    This CL introduces a temporary method to RenderFrameHost for checking
    if PolicyContainerHost is defined.
    
    This is a retry of https://crrev.com/c/5582480, which was abandoned.
    
    Bug: 346386726, 327681542
    Change-Id: I3328cdcba436827cf1d6c80483b8855a5307379f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783639
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Michelle Abreo <michelleabreo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343688}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -17288,6 +17288,10 @@ void RenderFrameHostImpl::MarkClipboardOwner(
   SetLastClipboardWrite(*this, seqno);
 }
 
+bool RenderFrameHostImpl::HasPolicyContainerHost() const {
+  return policy_container_host_ != nullptr;
+}
+
 void RenderFrameHostImpl::GetBoundInterfacesForTesting(
     std::vector<std::string>& out) {
   broker_.GetBinderMapInterfacesForTesting(out);  // IN-TEST
