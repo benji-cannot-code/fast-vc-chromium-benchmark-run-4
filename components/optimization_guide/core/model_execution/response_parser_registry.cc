f7e25c7ebac7bcd3749de0d36c0b914720b41129
Zekun Jiang <zekunjiang@google.com>
2024-08-19 19:24:15 +0000

    Add parser for history search output.
    Bug: b/354029147
    Change-Id: I0e2b8fae10df64bad7c20c39d1d7b098127d223a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789461
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Jun Zou <junzou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343646}

--- components/optimization_guide/core/model_execution/response_parser_registry.cc
+++ components/optimization_guide/core/model_execution/response_parser_registry.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/no_destructor.h"
+#include "components/optimization_guide/core/model_execution/aqa_response_parser.h"
 #include "components/optimization_guide/core/model_execution/json_response_parser.h"
 #include "components/optimization_guide/core/model_execution/simple_response_parser.h"
 
@@ -21,6 +22,8 @@ ResponseParserRegistry::ResponseParserRegistry() {
                      std::make_unique<SimpleResponseParserFactory>());
   factories_.emplace(proto::PARSER_KIND_JSON,
                      std::make_unique<JsonResponseParserFactory>());
+  factories_.emplace(proto::PARSER_KIND_AQA,
+                     std::make_unique<AqaResponseParserFactory>());
 }
 ResponseParserRegistry::~ResponseParserRegistry() = default;
 
