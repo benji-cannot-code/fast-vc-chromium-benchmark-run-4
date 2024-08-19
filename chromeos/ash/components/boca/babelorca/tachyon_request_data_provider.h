f3af3b583518e0d2d51f51203a8e7842e7f39320
Ahmed Nasr <anasr@google.com>
2024-08-19 19:16:59 +0000

    [BabelOrca] Add send transcript client.
    Bug: b:358140667
    Change-Id: I91f60e66ed4b04c674f23b18cecdc28e6ccdab24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787036
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343640}

---
+++ chromeos/ash/components/boca/babelorca/tachyon_request_data_provider.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_TACHYON_REQUEST_DATA_PROVIDER_H_
+#define CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_TACHYON_REQUEST_DATA_PROVIDER_H_
+
+#include <string>
+
+namespace ash::babelorca {
+
+// Provider for tachyon requests common data.
+class TachyonRequestDataProvider {
+ public:
+  TachyonRequestDataProvider(const TachyonRequestDataProvider&) = delete;
+  TachyonRequestDataProvider& operator=(const TachyonRequestDataProvider&) =
+      delete;
+
+  virtual ~TachyonRequestDataProvider() = default;
+
+  virtual std::string client_uuid() = 0;
+  virtual std::string tachyon_token() = 0;
+  virtual std::string group_id() = 0;
+
+ protected:
+  TachyonRequestDataProvider() = default;
+};
+
+}  // namespace ash::babelorca
+
+#endif  // CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_TACHYON_REQUEST_DATA_PROVIDER_H_
