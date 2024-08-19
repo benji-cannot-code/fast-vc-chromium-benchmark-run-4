415317b472fcae232a4831b8a925e17e0d469029
Morten Stenshorne <mstensho@chromium.org>
2024-08-19 14:09:48 +0000

    Apply size containment to ::scroll-marker-group.
    There was code for this, but it was never executed, and it was wrong (if
    executed, DCHECKs would fail).
    
    It was never executed, because ApplyBaseStyleNoCache() is called for the
    originating element, not the pseudo element. Additionally,
    ScrollMarkerGroupUserAgentDeclaration() would set the `content` property
    to a simple keyword, but it expects a list. See
    Contain::ParseSingleValue().
    
    Two tests had to be updated, so that they set a fixed block-size on the
    scroll marker group, since the children are not supposed to affect its
    size (which is what the tests used to assume).
    
    scroll-marker-002.html actually relied on two layout passes in order to
    render as expected, since it places the scroll marker group before the
    scroller (and the scroll marker group is populated after the scroller
    has been laid out, which will not replace the scroll marker group
    fragment itself, only its children (the markers)). We *would* typically
    get two layout passes though, since currently we assume that the
    viewport gets a vertical scrollbar, and, if that turns out not to be the
    case (like in this case), we'll lay out again without the scrollbar. Not
    a safe thing to rely on.
    
    Add an additional test that doesn't set an explicit block-size on the
    scroll marker group, to make sure that a tall child doesn't affect its
    size.
    
    Bug: 332396355
    Change-Id: I2ace302c872ff25aaa352b44d688b4c97a7fbb24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5794890
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343478}

--- third_party/blink/renderer/core/css/resolver/style_resolver.cc
+++ third_party/blink/renderer/core/css/resolver/style_resolver.cc
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_selector_watch.h"
 #include "third_party/blink/renderer/core/css/css_style_declaration.h"
 #include "third_party/blink/renderer/core/css/css_style_rule.h"
+#include "third_party/blink/renderer/core/css/css_value_list.h"
 #include "third_party/blink/renderer/core/css/element_rule_collector.h"
 #include "third_party/blink/renderer/core/css/font_face.h"
 #include "third_party/blink/renderer/core/css/out_of_flow_data.h"
@@ -482,9 +483,9 @@ static CSSPropertyValueSet* ScrollMarkerGroupUserAgentDeclaration() {
       (MakeGarbageCollected<MutableCSSPropertyValueSet>(kHTMLStandardMode)));
 
   if (decl->IsEmpty()) {
-    decl->SetProperty(CSSPropertyID::kContain,
-                      *CSSIdentifierValue::Create(CSSValueID::kSize),
-                      true /* important */);
+    CSSValueList* list = CSSValueList::CreateSpaceSeparated();
+    list->Append(*CSSIdentifierValue::Create(CSSValueID::kSize));
+    decl->SetProperty(CSSPropertyID::kContain, *list, /*important=*/true);
   }
   return decl;
 }
@@ -1483,16 +1484,16 @@ void StyleResolver::ApplyBaseStyleNoCache(
     }
   }
 
-  if (element->IsScrollMarkerGroupPseudoElement()) {
-    cascade.MutableMatchResult().AddMatchedProperties(
-        ScrollMarkerGroupUserAgentDeclaration(), CascadeOrigin::kUserAgent);
-  }
-
   ElementRuleCollector collector(state.ElementContext(), style_recalc_context,
                                  selector_filter_, cascade.MutableMatchResult(),
                                  state.InsideLink());
 
   if (style_request.IsPseudoStyleRequest()) {
+    if (style_request.pseudo_id == kPseudoIdScrollMarkerGroup) {
+      cascade.MutableMatchResult().AddMatchedProperties(
+          ScrollMarkerGroupUserAgentDeclaration(), CascadeOrigin::kUserAgent);
+    }
+
     collector.SetPseudoElementStyleRequest(style_request);
     GetDocument().GetStyleEngine().EnsureUAStyleForPseudoElement(
         style_request.pseudo_id);
