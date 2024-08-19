492e3921fdd98c1a91b2da077fdb5220ede2a0af
Ivana Žužić <izuzic@google.com>
2024-08-19 08:50:43 +0000

    [UPMAccessLossWarning] Add the visual properties of the sheet
    The simple notice sheet can display a title, a paragraph of text and a
    titled button. Password manager icon is placed at the top. In case the
    icon needs to be changed later, there could be a property for it, but
    there is no need for custom icons now.
    
    Bug: 353283268
    Change-Id: Ibef05728ad07be4ce3c236688a5e4dd2baab2e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5772715
    Reviewed-by: Friedrich Horschig <fhorschig@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1343372}

--- chrome/browser/password_manager/android/bottom_sheet/java/src/org/chromium/chrome/browser/bottom_sheet/SimpleNoticeSheetViewBinder.java
+++ chrome/browser/password_manager/android/bottom_sheet/java/src/org/chromium/chrome/browser/bottom_sheet/SimpleNoticeSheetViewBinder.java
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.bottom_sheet;
 
+import static org.chromium.chrome.browser.bottom_sheet.SimpleNoticeSheetProperties.BUTTON_TITLE;
+import static org.chromium.chrome.browser.bottom_sheet.SimpleNoticeSheetProperties.SHEET_TEXT;
+import static org.chromium.chrome.browser.bottom_sheet.SimpleNoticeSheetProperties.SHEET_TITLE;
+
 import org.chromium.ui.modelutil.PropertyKey;
 import org.chromium.ui.modelutil.PropertyModel;
 
@@ -22,8 +26,15 @@ class SimpleNoticeSheetViewBinder {
      */
     static void bindSimpleNoticeSheetView(
             PropertyModel model, SimpleNoticeSheetView view, PropertyKey propertyKey) {
-        // TODO: crbug.com/353283268 - Add properties.
-        assert false : "Unhandled update to property:" + propertyKey;
+        if (propertyKey == SHEET_TITLE) {
+            view.setTitle(model.get(SHEET_TITLE));
+        } else if (propertyKey == SHEET_TEXT) {
+            view.setText(model.get(SHEET_TEXT));
+        } else if (propertyKey == BUTTON_TITLE) {
+            view.setButtonText(model.get(BUTTON_TITLE));
+        } else {
+            assert false : "Unhandled update to property:" + propertyKey;
+        }
     }
 
     private SimpleNoticeSheetViewBinder() {}
