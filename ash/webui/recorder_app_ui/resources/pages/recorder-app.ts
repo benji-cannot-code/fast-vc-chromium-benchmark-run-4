9556247168c6e20d7b8ba0414d219bab2296a6ac
Kam Kwankajornkiet <kamchonlathorn@google.com>
2024-08-19 09:53:23 +0000

    recorder: Add test helper for Tast test
    Add TestHelper class to connect with Tast side, and add /test path to be
    launched from the test. TestHelper provides functionalities to Tast side
    including importing recordings, config settings, and resolving i18n
    strings.
    
    Bug: b:355374546
    Test: Manually test with CL:5767465
    Change-Id: I255260d23af47320345d0c8a8f201b1c0228044b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5780711
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343390}

--- ash/webui/recorder_app_ui/resources/pages/recorder-app.ts
+++ ash/webui/recorder_app_ui/resources/pages/recorder-app.ts
@@ -81,6 +81,9 @@ export class RecorderApp extends ReactiveLitElement {
     if (path === '/dev') {
       return html`<dev-page></dev-page>`;
     }
+    if (path === '/test') {
+      return '';
+    }
 
     return this.render404();
   }
