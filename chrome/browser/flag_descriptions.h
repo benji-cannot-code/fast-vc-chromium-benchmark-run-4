0f05c0386707198610fceddb07cd106cebedf5be
Sophia Tran <sophiatran@google.com>
2024-08-19 20:06:13 +0000

    Add a feature flag for a Load More button to chrome://flags.
    Add a flag to hide the button from the Reading Mode UI unless enabled.
    The button will be implemented in a future CL.
    
    Change-Id: I1563cc18cdc245d6fb06255d15d1db4d6c911732
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793906
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Commit-Queue: Sophia Tran <sophiatran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343705}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3048,6 +3048,9 @@ extern const char kReadAloudLanguagePackDownloadingDescription[];
 extern const char kReadAnythingDocsIntegrationName[];
 extern const char kReadAnythingDocsIntegrationDescription[];
 
+extern const char kReadAnythingDocsLoadMoreButtonName[];
+extern const char kReadAnythingDocsLoadMoreButtonDescription[];
+
 extern const char kGlobalMediaControlsCastStartStopName[];
 extern const char kGlobalMediaControlsCastStartStopDescription[];
 
