081e986a13c3b5818e8714aecae6bcecdc40c7d0
Pi-Hsun Shih <pihsun@chromium.org>
2024-08-19 11:10:33 +0000

    recorder: Don't show speaker label in waveform when only one speaker
    Bug: b:336963138
    Test: manually
    Change-Id: Ide01623372125a510ac3b619a3fc844b27773db6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796262
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343420}

--- ash/webui/recorder_app_ui/resources/components/audio-waveform.ts
+++ ash/webui/recorder_app_ui/resources/components/audio-waveform.ts
@@ -139,6 +139,12 @@ export class AudioWaveform extends ReactiveLitElement {
         position: absolute;
       }
 
+      .speaker-single {
+        & .range {
+          display: none;
+        }
+      }
+
       .speaker-duo,
       .speaker-multiple {
         & .no-speaker {
