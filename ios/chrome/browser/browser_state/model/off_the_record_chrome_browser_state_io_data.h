d6919be76265eb91d364f2de4e4dabc493e66819
Mark Cogan <marq@google.com>
2024-08-19 18:08:27 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/browser_state/model.
    
    This CL was uploaded by git cl split.
    
    R=sdefresne@chromium.org
    
    Bug: 359819865
    Change-Id: I37290d50b0e5de52863a7f86cd5b8d61208d7423
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797203
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343606}

--- ios/chrome/browser/browser_state/model/off_the_record_chrome_browser_state_io_data.h
+++ ios/chrome/browser/browser_state/model/off_the_record_chrome_browser_state_io_data.h
@@ -6,16 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_BROWSER_STATE_MODEL_OFF_THE_RECORD_CHROME_BROWSER_STATE_IO_DATA_H_
 #define IOS_CHROME_BROWSER_BROWSER_STATE_MODEL_OFF_THE_RECORD_CHROME_BROWSER_STATE_IO_DATA_H_
 
-#include <memory>
+#import <memory>
 
-#include "base/files/file_path.h"
-#include "base/functional/callback.h"
+#import "base/files/file_path.h"
+#import "base/functional/callback.h"
 #import "base/memory/raw_ptr.h"
-#include "base/memory/ref_counted.h"
-#include "ios/chrome/browser/net/model/net_types.h"
-#include "ios/chrome/browser/profile/model/profile_ios_io_data.h"
+#import "base/memory/ref_counted.h"
+#import "ios/chrome/browser/net/model/net_types.h"
+#import "ios/chrome/browser/profile/model/profile_ios_io_data.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
-class ChromeBrowserState;
 class IOSChromeURLRequestContextGetter;
 
 // OffTheRecordChromeBrowserState owns a
