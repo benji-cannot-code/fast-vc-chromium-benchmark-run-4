5151085f651e6471c027fd75355ab407c6cb58d9
Joseph Park <josephjoopark@chromium.org>
2024-08-19 21:57:10 +0000

    [toolbarpinning] Keep button popped out when something is anchored on it
    Currently, if attempting to open the context menu of a popped out button (that is not in active state) the button will immediately disappear, making it impossible to access the context menu. This CL keeps the button popped out if the context menu is open.
    
    Bug: 360195129
    Change-Id: I02c70fdba56fc05a1dcfdf91da59503f5f9a8087
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5795358
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343789}

--- chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/pinned_action_toolbar_button.cc
@@ -222,6 +222,10 @@ void PinnedActionToolbarButton::UpdateIcon() {
   }
 }
 
+bool PinnedActionToolbarButton::ShouldShowEphemerallyInToolbar() {
+  return should_show_in_toolbar_ || has_anchor_;
+}
+
 void PinnedActionToolbarButton::SetActionEngaged(bool action_engaged) {
   if (!IsActive()) {
     SetProperty(
@@ -281,6 +285,7 @@ void PinnedActionToolbarButton::OnAnchorCountChanged(size_t anchor_count) {
         static_cast<std::underlying_type_t<PinnedToolbarActionFlexPriority>>(
             PinnedToolbarActionFlexPriority::kHigh));
     InvalidateLayout();
+    has_anchor_ = true;
   } else {
     SetProperty(
         kToolbarButtonFlexPriorityKey,
@@ -292,6 +297,8 @@ void PinnedActionToolbarButton::OnAnchorCountChanged(size_t anchor_count) {
                   std::underlying_type_t<PinnedToolbarActionFlexPriority>>(
                   PinnedToolbarActionFlexPriority::kLow));
     InvalidateLayout();
+    has_anchor_ = false;
+    container_->MaybeRemovePoppedOutButtonFor(GetActionId());
   }
 }
 
