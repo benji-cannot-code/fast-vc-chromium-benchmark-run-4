1443330f888e6ac11a63244f7403974f641f63b0
Thomas Sepez <tsepez@google.com>
2024-08-19 18:40:41 +0000

    Revert "Spanify argument to base::WaitableEvent::WaitMany()."
    This reverts commit 46311f9251481e9a7286b70b7ba1ebf03462aece.
    
    Reason for revert: Possible cause of 360513102
    
    Original change's description:
    > Spanify argument to base::WaitableEvent::WaitMany().
    >
    > Replace discrete ptr/length pairs with span<> and adjust callers,
    > typically using implicit conversion to simplify the code.
    >
    > Then remove allow_unsafe_buffers pragmas as possible, by converting
    > to std::array in one place.
    >
    > -- Make argument names match between header and cc files.
    > -- Avoid unsigned underflow in posix should empty span be passed.
    > -- Fix indentation in windows code.
    > -- Fix longstanding tricium emplace_back() warning.
    > -- Remove else-after-return in apple code.
    >
    > Change-Id: I384035d6e23bf4e46c6e3711628709c2c68678ca
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753315
    > Reviewed-by: Ted Choc <tedchoc@chromium.org>
    > Reviewed-by: John Rummell <jrummell@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Commit-Queue: Tom Sepez <tsepez@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1336169}
    
    Change-Id: Ic3940aa4fb060a5a771493f5456d9217a3dc4ed0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798142
    Commit-Queue: John Rummell <jrummell@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Auto-Submit: Thomas Sepez <tsepez@google.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343619}

--- base/synchronization/waitable_event_win.cc
+++ base/synchronization/waitable_event_win.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40284755): Remove this and spanify to fix the errors.
+#pragma allow_unsafe_buffers
+#endif
+
 #include "base/synchronization/waitable_event.h"
 
 #include <windows.h>
@@ -10,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 
 #include <algorithm>
-#include <array>
 #include <optional>
 #include <utility>
 
@@ -113,21 +117,21 @@ bool WaitableEvent::TimedWaitImpl(TimeDelta wait_delta) {
 }
 
 // static
-size_t WaitableEvent::WaitManyImpl(base::span<WaitableEvent*> raw_waitables) {
-  std::array<HANDLE, MAXIMUM_WAIT_OBJECTS> handles;
-  CHECK_LE(raw_waitables.size(), handles.size())
+size_t WaitableEvent::WaitManyImpl(WaitableEvent** events, size_t count) {
+  HANDLE handles[MAXIMUM_WAIT_OBJECTS];
+  CHECK_LE(count, static_cast<size_t>(MAXIMUM_WAIT_OBJECTS))
       << "Can only wait on " << MAXIMUM_WAIT_OBJECTS << " with WaitMany";
 
-  for (size_t i = 0; i < raw_waitables.size(); ++i) {
-    handles[i] = raw_waitables[i]->handle();
-  }
+  for (size_t i = 0; i < count; ++i)
+    handles[i] = events[i]->handle();
 
-  // The cast is safe because size is small - see the CHECK above.
-  DWORD result = WaitForMultipleObjects(
-      static_cast<DWORD>(raw_waitables.size()), handles.data(),
-      FALSE,      // don't wait for all the objects
-      INFINITE);  // no timeout
-  if (result >= WAIT_OBJECT_0 + raw_waitables.size()) {
+  // The cast is safe because count is small - see the CHECK above.
+  DWORD result =
+      WaitForMultipleObjects(static_cast<DWORD>(count),
+                             handles,
+                             FALSE,      // don't wait for all the objects
+                             INFINITE);  // no timeout
+  if (result >= WAIT_OBJECT_0 + count) {
     DPLOG(FATAL) << "WaitForMultipleObjects failed";
     return 0;
   }
