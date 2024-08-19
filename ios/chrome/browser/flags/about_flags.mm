a1cea5fe750e78e805352e62cc39defc82ea5d4c
Ernesto Izquierdo Clua <eic@google.com>
2024-08-19 22:05:54 +0000

    [auto-iso] Propagate flag for isolated worlds to JS
    Propagates the feature flag for enabling Autofil in the isolated content
    world to JS scripts. Makes the feature flag configurable through
    about://flags.
    
    context: go/autofill-isolated-content-world
    
    R=vincb@google.com
    NO_IFTTT=All the appropriate files were changed.
    
    Fixed: 357891501
    Change-Id: Ic7ef2de70910f3457253b9a18824e3401992de04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783352
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343793}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -1955,6 +1955,11 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kEnableTraitCollectionRegistrationName,
      flag_descriptions::kEnableTraitCollectionRegistrationDescription,
      flags_ui::kOsIos, FEATURE_VALUE_TYPE(kEnableTraitCollectionRegistration)},
+    {"autofill-isolated-world-ios",
+     flag_descriptions::kAutofillIsolatedWorldForJavascriptIOSName,
+     flag_descriptions::kAutofillIsolatedWorldForJavascriptIOSDescription,
+     flags_ui::kOsIos,
+     FEATURE_VALUE_TYPE(kAutofillIsolatedWorldForJavascriptIos)},
 };
 
 bool SkipConditionalFeatureEntry(const flags_ui::FeatureEntry& entry) {
