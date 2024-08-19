dc5a56896a7b439430fe6419fc8b9e072f391141
Peter Boström <pbos@chromium.org>
2024-08-19 23:00:56 +0000

    IWYU a few std::unordered_map
    These were transitively included through <functional> =>
    __functional/boyer_moore_searcher.h in C++17.
    
    Bug: None
    Change-Id: Ie7cc9c1af99f80bc62739a7ddf878f987e75788d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792881
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343824}

--- chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+++ chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cmath>
 #include <deque>
 #include <optional>
+#include <unordered_map>
 
 #include "base/apple/bridging.h"
 #include "base/apple/foundation_util.h"
