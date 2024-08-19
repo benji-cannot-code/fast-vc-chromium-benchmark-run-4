c1c9608f19ce0fedb27222d780bf2a24ff455963
Daniel Rubery <drubery@chromium.org>
2024-08-19 23:07:58 +0000

    Check for null profile in download_controller.cc
    We have a few crashes due to nullptr accesses in
    download_controller.cc. This CL skips the pref changes if the profile is
    null.
    
    Fixed: 360641909
    Change-Id: I1f73b9e152332036a364626b390b827cc9470d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798147
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Xinghui Lu <xinghuilu@chromium.org>
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343829}

--- chrome/browser/download/android/download_controller.cc
+++ chrome/browser/download/android/download_controller.cc
@@ -241,6 +241,9 @@ void RecordDownloadBlocklistState(download::DownloadItem* item) {
 void CleanupAppVerificationTimestamps(download::DownloadItem* item) {
   Profile* profile = Profile::FromBrowserContext(
       content::DownloadItemUtils::GetBrowserContext(item));
+  if (!profile || !profile->GetPrefs()) {
+    return;
+  }
   ScopedListPrefUpdate update(profile->GetPrefs(),
                               prefs::kDownloadAppVerificationPromptTimestamps);
   update->EraseIf([](const base::Value& timestamp) {
@@ -259,6 +262,9 @@ bool HasSeenTooManyAppVerificationPrompts(download::DownloadItem* item) {
   constexpr size_t kMaxImpressions = 3;
   Profile* profile = Profile::FromBrowserContext(
       content::DownloadItemUtils::GetBrowserContext(item));
+  if (!profile || !profile->GetPrefs()) {
+    return false;
+  }
   return profile->GetPrefs()
              ->GetList(prefs::kDownloadAppVerificationPromptTimestamps)
              .size() >= kMaxImpressions;
@@ -267,6 +273,9 @@ bool HasSeenTooManyAppVerificationPrompts(download::DownloadItem* item) {
 void LogAppVerificationPromptToPrefs(download::DownloadItem* item) {
   Profile* profile = Profile::FromBrowserContext(
       content::DownloadItemUtils::GetBrowserContext(item));
+  if (!profile || !profile->GetPrefs()) {
+    return;
+  }
   ScopedListPrefUpdate update(profile->GetPrefs(),
                               prefs::kDownloadAppVerificationPromptTimestamps);
   update->Append(base::TimeToValue(base::Time::Now()));
