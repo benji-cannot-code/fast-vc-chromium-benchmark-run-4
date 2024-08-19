2f700da4f859f3548c534b2328ec6e31e3501abe
Jason Zhang <jiajunz@google.com>
2024-08-19 21:37:11 +0000

    test: Add passpoint ui tests
    This CL use the FakePasspointService to add a fake passpoint
    subscription and add interactive UI test to verify its UI. Additionally,
    it also adds the common wifi utility to configure a wifi service,
    navigate to Known Networks subpage and detail subscription page.
    
    Fixed: b/338646691, b/344007293, b/338647045
    Change-Id: Ie3aa54395bf15f2d90459fbcf9ece3ef994d57eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784557
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343771}

--- chrome/test/base/ash/interactive/cellular/cellular_util.cc
+++ chrome/test/base/ash/interactive/cellular/cellular_util.cc
@@ -7,10 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/strings/stringprintf.h"
+#include "chrome/test/base/ash/interactive/network/shill_service_util.h"
 #include "chromeos/ash/components/dbus/hermes/hermes_euicc_client.h"
 #include "chromeos/ash/components/dbus/hermes/hermes_profile_client.h"
-#include "chromeos/ash/components/network/network_connection_handler.h"
-#include "chromeos/ash/components/network/network_handler.h"
 #include "dbus/object_path.h"
 #include "third_party/cros_system_api/dbus/hermes/dbus-constants.h"
 
@@ -40,14 +39,11 @@ SimInfo::SimInfo(unsigned int id)
 SimInfo::~SimInfo() = default;
 
 void SimInfo::Connect() const {
-  NetworkHandler::Get()->network_connection_handler()->ConnectToNetwork(
-      service_path_, base::DoNothing(), base::DoNothing(),
-      /*check_error_state=*/false, ConnectCallbackMode::ON_COMPLETED);
+  ConnectShillService(service_path_);
 }
 
 void SimInfo::Disconnect() const {
-  NetworkHandler::Get()->network_connection_handler()->DisconnectNetwork(
-      service_path_, base::DoNothing(), base::DoNothing());
+  DisconnectShillService(service_path_);
 }
 
 void ConfigureEsimProfile(const EuiccInfo& euicc_info,
