a9ed4ea4e69aa1d3fc454a867f14ead6ec95ae77
Steve Kobes <skobes@chromium.org>
2024-08-19 21:14:19 +0000

    Clear the last known mouse position during WebFrameTestProxy::Reset.
    This currently happens on every same-site navigation, but we want to
    change that behavior.  A separate call to explicitly reset will let us
    re-land crrev.com/1340838 without introducing new state leakage between
    tests.  This change is temporary until we have more robust resetting in
    place, which is tracked at crbug.com/40946696.
    
    Bug: 354649089
    Change-Id: I8f271dc4451c6bdc6632e4ac83afcae7264dcfd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792209
    Reviewed-by: Mustaq Ahmed <mustaq@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343763}

--- third_party/blink/renderer/core/web_test/web_test_web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/web_test/web_test_web_frame_widget_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/web/web_widget.h"
 #include "third_party/blink/renderer/core/exported/web_view_impl.h"
 #include "third_party/blink/renderer/core/frame/web_local_frame_impl.h"
+#include "third_party/blink/renderer/core/input/event_handler.h"
 #include "third_party/blink/renderer/core/view_transition/view_transition_supplement.h"
 #include "third_party/blink/renderer/platform/scheduler/public/thread.h"
 
@@ -204,6 +205,10 @@ void WebTestWebFrameWidgetImpl::Reset() {
 
     SetMainFrameOverlayColor(SK_ColorTRANSPARENT);
     SetTextZoomFactor(1);
+    LocalRootImpl()
+        ->GetFrame()
+        ->GetEventHandler()
+        .ResetLastMousePositionForWebTest();
   }
 }
 
