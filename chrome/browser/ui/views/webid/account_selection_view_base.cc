92ad816f0a42d8910b85edbdfc7f811c4ee0bb12
Zachary Tan <tanzachary@chromium.org>
2024-08-20 16:15:53 +0000

    [FedCM] Remove globe icon on desktop loading modal
    The globe icon in the loading dialog makes it seem like something is
    broken especially if there is no IDP icon and the globe continues to be
    shown throughout the flow. Removing it helps make the globe look more
    like a placeholder rather than a bug.
    
    Screenshot
    https://issues.chromium.org/action/issues/350021932/attachments/58643323?download=false
    
    Bug: 350021932
    Change-Id: Id1ff61dc6f77891a22a5d8fd1179db8f09c74869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787677
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344195}

--- chrome/browser/ui/views/webid/account_selection_view_base.cc
+++ chrome/browser/ui/views/webid/account_selection_view_base.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/webid/account_selection_view_base.h"
 
+#include "chrome/app/vector_icons/vector_icons.h"
 #include "chrome/browser/image_fetcher/image_decoder_impl.h"
 #include "chrome/browser/ui/views/controls/hover_button.h"
 #include "chrome/browser/ui/views/webid/account_selection_bubble_view.h"
@@ -471,7 +472,8 @@ std::unique_ptr<views::View> AccountSelectionViewBase::CreateAccountRow(
               background_color);
       brand_icon_image_view_ptr = brand_icon_image_view.get();
       ConfigureBrandImageView(brand_icon_image_view_ptr,
-                              idp_display_data.idp_metadata.brand_icon_url);
+                              idp_display_data.idp_metadata.brand_icon_url,
+                              /*show_placeholder=*/false);
 
       icon_container->AddChildView(std::move(brand_icon_image_view));
 
@@ -569,9 +571,17 @@ void AccountSelectionViewBase::AddIdpImage(const GURL& image_url,
 
 void AccountSelectionViewBase::ConfigureBrandImageView(
     BrandIconImageView* image_view,
-    const GURL& brand_icon_url) {
+    const GURL& brand_icon_url,
+    bool show_placeholder) {
   bool is_valid_icon_url = brand_icon_url.is_valid();
   if (!is_valid_icon_url) {
+    if (show_placeholder) {
+      image_view->SetImage(ui::ImageModel::FromVectorIcon(
+          kWebidGlobeIcon, ui::kColorIconSecondary, kModalIdpIconSize));
+      image_view->SetVisible(true);
+      return;
+    }
+    image_view->SetVisible(false);
     return;
   }
 
