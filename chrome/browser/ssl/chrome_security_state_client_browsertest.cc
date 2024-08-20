731abe7c4c43308b2668c636a220907d4840ae9b
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-08-20 20:02:17 +0000

    Clean up miscategorized PlatformBrowserTests (Android)
    PlatformBrowserTest is a utility designed to run tests on both
    Android and Desktop platforms and, under the hood, is aliased to either
    InProcessBrowserTest or AndroidBrowserTest. However, some of the classes
    that are written with it are not intended to run on both sets of
    platforms.
    
    This CL cleans up instances of inheriting from PlatformBrowserTest where
    the test only runs on Android to instead use AndroidBrowserTest
    directly.
    
    Bug: 361002665
    Change-Id: Iaa053b7fc4f30f2eb5c02fb2af1faa770ff7fa0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799549
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344341}

--- chrome/browser/ssl/chrome_security_state_client_browsertest.cc
+++ chrome/browser/ssl/chrome_security_state_client_browsertest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test.h"
 
-class ChromeSecurityStateClientTest : public PlatformBrowserTest {
+class ChromeSecurityStateClientTest : public AndroidBrowserTest {
  public:
   ChromeSecurityStateClientTest() = default;
   ~ChromeSecurityStateClientTest() override = default;
