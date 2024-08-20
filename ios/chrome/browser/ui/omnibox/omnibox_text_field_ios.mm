980d2d00d0f6b15025329efb514984a5931666a3
Stepan Khapugin <stkhapugin@chromium.org>
2024-08-20 18:19:51 +0000

    [iOS][omnibox] Fix exiting preedit in iOS 18.
    In iOS 18, exiting preedit resets the selection and doesn't show the
    callout menu, causing tests to fail.
    Calling -[UITextField selectAll:] selects all text and, if a non-nil
    sender is passed, summons the menu. Seemingly call doesn't affect
    existing pre-iOS 18 behavior, so no gating on iOS 18 is added.
    
    Fixed: 350460961
    Change-Id: I94e4ff81bff0e7fa10092cb9017dae97e3fd49fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800691
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344280}

--- ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_text_field_ios.mm
@@ -495,6 +495,7 @@ NSString* const kOmniboxFadeAnimationKey = @"OmniboxFadeAnimation";
 - (void)handleTap:(UITapGestureRecognizer*)sender {
   if (self.isPreEditing) {
     [self exitPreEditState];
+    [super selectAll:self];
   }
   if (self.hasAutocompleteText) {
     [self acceptAutocompleteText];
