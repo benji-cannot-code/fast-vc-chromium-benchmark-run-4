3281c6b49d08038f1228005770dd4bd496fbcd0e
Brandon Fong <bjfong@google.com>
2024-08-20 19:17:52 +0000

    [Data Sharing] Add notification bubble on tab strip items
    Add a notification bubble on each tab item on the tab strip when they
    receive updates as part of a tab group.
    
    Demo: https://drive.google.com/file/d/1jPkXNEv9MTXilvifBksywCIwMQDpX5ZE/view?usp=sharing
    
    Bug: 348731046
    Change-Id: I039e90dac8aa6dfaddb43279b5593da853c46df9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786449
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1344318}

---
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListNotificationHandler.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.tasks.tab_management;
+
+import java.util.Set;
+
+/** Interface for the notification handler for tab group share related components. */
+public interface TabListNotificationHandler {
+    /**
+     * Update the notification bubble indicating a new status for all tab strip items that are part
+     * of a shared tab group.
+     *
+     * @param tabIdsToBeUpdated The set of tab ids that require an update.
+     * @param hasUpdate Whether the tab items should show a new update status or not.
+     */
+    void updateTabStripNotificationBubble(Set<Integer> tabIdsToBeUpdated, boolean hasUpdate);
+}
