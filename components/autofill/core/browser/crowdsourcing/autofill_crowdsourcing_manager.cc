75f0cfee4caaeb18d0bdd9291f74c56cbeba2e14
Piotr Domaradzki <doland@google.com>
2024-08-20 09:34:43 +0000

    Refactor out the deprecated message group
    Change-Id: I96c9036e5ee794d5ac56d888bae4496d7a2d9b99
    Bug: 356373240
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766074
    Commit-Queue: Piotr Domaradzki <doland@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1344031}

--- components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_manager.cc
+++ components/autofill/core/browser/crowdsourcing/autofill_crowdsourcing_manager.cc
@@ -389,7 +389,7 @@ LogBuffer& operator<<(LogBuffer& out, const AutofillUploadContents& upload) {
   }
 
   out << Tr{} << "form_signature:" << upload.form_signature();
-  for (const auto& field : upload.field()) {
+  for (const auto& field : upload.field_data()) {
     out << Tr{} << Attrib{"style", "font-weight: bold"}
         << "field_signature:" << field.signature();
 
@@ -741,7 +741,8 @@ bool AutofillCrowdsourcingManager::StartUploadRequest(
           /*form_submission_source_for_vote_upload=*/std::nullopt)) {
     for (AutofillUploadContents& upload : upload_contents) {
       upload.clear_randomized_form_metadata();
-      for (AutofillUploadContents::Field& field : *upload.mutable_field()) {
+      for (AutofillUploadContents::Field& field :
+           *upload.mutable_field_data()) {
         field.clear_randomized_field_metadata();
       }
     }
--- components/autofill/core/browser/test_utils/vote_uploads_test_matchers.h
+++ components/autofill/core/browser/test_utils/vote_uploads_test_matchers.h
@@ -48,7 +48,7 @@ inline ::testing::Matcher<AutofillUploadContents> FormSignatureIs(
 template <typename... Matchers>
 inline ::testing::Matcher<AutofillUploadContents> FieldsAre(
     Matchers... matchers) {
-  return ::testing::Property("field", &AutofillUploadContents::field,
+  return ::testing::Property("field_data", &AutofillUploadContents::field_data,
                              ::testing::ElementsAre(matchers...));
 }
 
@@ -60,7 +60,7 @@ template <typename... Matchers>
 inline ::testing::Matcher<AutofillUploadContents> FieldsContain(
     Matchers... matchers) {
   return ::testing::Property(
-      "field", &AutofillUploadContents::field,
+      "field_data", &AutofillUploadContents::field_data,
       ::testing::IsSupersetOf(
           std::initializer_list<
               ::testing::Matcher<AutofillUploadContents::Field>>{matchers...}));
