fb0d6b37b4f16030d0a92cd6f0746c6978a52a6c
dpapad <dpapad@chromium.org>
2024-08-20 21:58:48 +0000

    [Reland] Signin: Migrate sync-confirmation, signin-email-confirmation to Lit.
    Reland notes: Fixed CSS style that was broken after changing iron-icon
    to cr-icon, causing pixel tests to fail.
    
    Also changing iron-icon dependency to cr-icon.
    
    Bug: 40943652
    Change-Id: Ifd93640214a3e7ee99f381fcd5be4bdd4625921a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799813
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344418}

--- chrome/test/data/webui/signin/sync_confirmation_test.ts
+++ chrome/test/data/webui/signin/sync_confirmation_test.ts
@@ -5,11 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://sync-confirmation/sync_confirmation_app.js';
 
+import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {SyncConfirmationAppElement} from 'chrome://sync-confirmation/sync_confirmation_app.js';
 import {ScreenMode, SyncConfirmationBrowserProxyImpl} from 'chrome://sync-confirmation/sync_confirmation_browser_proxy.js';
 import {assertArrayEquals, assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestSyncConfirmationBrowserProxy} from './test_sync_confirmation_browser_proxy.js';
 
@@ -17,7 +18,7 @@ suite(`SigninSyncConfirmationTest`, function() {
   let app: SyncConfirmationAppElement;
   let browserProxy: TestSyncConfirmationBrowserProxy;
 
-  function testButtonClick(buttonSelector: string) {
+  async function testButtonClick(buttonSelector: string) {
     const allButtons =
         Array.from(app.shadowRoot!.querySelectorAll('cr-button'));
     const actionButton =
@@ -29,6 +30,7 @@ suite(`SigninSyncConfirmationTest`, function() {
 
     assertTrue(!!actionButton);
     actionButton.click();
+    await microtasksFinished();
 
     allButtons.forEach(button => assertTrue(button.disabled));
     assertTrue(spinner!.active);
@@ -61,19 +63,19 @@ suite(`SigninSyncConfirmationTest`, function() {
 
   // Tests clicking on confirm button.
   test('ConfirmClicked', async function() {
-    testButtonClick('#confirmButton');
+    await testButtonClick('#confirmButton');
     await browserProxy.whenCalled('confirm');
   });
 
   // Tests clicking on cancel button.
   test('CancelClicked', async function() {
-    testButtonClick('#notNowButton');
+    await testButtonClick('#notNowButton');
     await browserProxy.whenCalled('undo');
   });
 
   // Tests clicking on settings button.
   test('SettingsClicked', async function() {
-    testButtonClick('#settingsButton');
+    await testButtonClick('#settingsButton');
     await browserProxy.whenCalled('goToSettings');
   });
 });
