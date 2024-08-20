f2c562fb2fe961ea4a430bee9350140737aa18de
Christoph Schwering <schwering@google.com>
2024-08-20 16:08:39 +0000

    [Autofill] Route GetFourDigitCombinationsFromDom() to the main frame
    AutofillDriver::GetFourDigitCombinationsFromDOM() was always passed to
    the driver's frame. This CL redirects the call to the main frame.
    
    Bug: 358291477, 40209327
    Change-Id: Ie6c1a937444d613c463d1d89b52d928321e6823a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5771599
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1344187}

--- components/autofill/content/browser/content_autofill_driver.cc
+++ components/autofill/content/browser/content_autofill_driver.cc
@@ -305,7 +305,7 @@ void ContentAutofillDriver::TriggerFormExtractionInAllFrames(
   }
 }
 
-void ContentAutofillDriver::GetFourDigitCombinationsFromDOM(
+void ContentAutofillDriver::GetFourDigitCombinationsFromDom(
     base::OnceCallback<void(const std::vector<std::string>&)>
         potential_matches) {
   if (!IsActive()) {
@@ -313,8 +313,12 @@ void ContentAutofillDriver::GetFourDigitCombinationsFromDOM(
     std::move(potential_matches).Run({});
     return;
   }
-  GetAutofillAgent()->GetPotentialLastFourCombinationsForStandaloneCvc(
-      std::move(potential_matches));
+  content::RenderFrameHost* main_rfh = render_frame_host_->GetMainFrame();
+  if (auto* main_driver = GetForRenderFrameHost(main_rfh)) {
+    main_driver->GetAutofillAgent()
+        ->GetPotentialLastFourCombinationsForStandaloneCvc(
+            std::move(potential_matches));
+  }
 }
 
 // static
--- components/autofill/core/browser/test_autofill_driver.h
+++ components/autofill/core/browser/test_autofill_driver.h
@@ -100,7 +100,7 @@ class TestAutofillDriverTemplate : public T {
   void ExtractForm(
       FormGlobalId form,
       AutofillDriver::BrowserFormHandler response_handler) override {}
-  void GetFourDigitCombinationsFromDOM(
+  void GetFourDigitCombinationsFromDom(
       base::OnceCallback<void(const std::vector<std::string>&)>
           potential_matches) override {}
 
