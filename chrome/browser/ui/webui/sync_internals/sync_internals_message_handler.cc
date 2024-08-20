4c3f7dac1d79d5f64767fc6459742e29c0d21b9f
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-08-20 08:20:53 +0000

    [lacros] Remove chrome://sync-internals <-> os://sync-internals link
    This reverts crrev.com/c/4823234.
    
    Bug: 360321896
    Change-Id: I7c7bad88d908d8a7697344d49eacc428cf0441a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5788719
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343997}

--- chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
+++ chrome/browser/ui/webui/sync_internals/sync_internals_message_handler.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_invalidations_service_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
@@ -32,12 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync_user_events/user_event_service.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_ui.h"
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ash/crosapi/browser_manager.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chrome/common/webui_url_constants.h"
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 using syncer::SyncInvalidationsService;
 using syncer::SyncService;
@@ -150,18 +143,6 @@ void SyncInternalsMessageHandler::RegisterMessages() {
       syncer::sync_ui_util::kGetAllNodes,
       base::BindRepeating(&SyncInternalsMessageHandler::HandleGetAllNodes,
                           base::Unretained(this)));
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  web_ui()->RegisterMessageCallback(
-      syncer::sync_ui_util::kIsLacrosEnabled,
-      base::BindRepeating(&SyncInternalsMessageHandler::IsLacrosEnabled,
-                          base::Unretained(this)));
-
-  web_ui()->RegisterMessageCallback(
-      syncer::sync_ui_util::kOpenLacrosSyncInternals,
-      base::BindRepeating(&SyncInternalsMessageHandler::OpenLacrosSyncInternals,
-                          base::Unretained(this)));
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 }
 
 void SyncInternalsMessageHandler::HandleRequestDataAndRegisterForUpdates(
@@ -306,30 +287,6 @@ void SyncInternalsMessageHandler::HandleTriggerRefresh(
 
   service->TriggerRefresh(syncer::DataTypeSet::All());
 }
-
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-void SyncInternalsMessageHandler::IsLacrosEnabled(
-    const base::Value::List& args) {
-  CHECK_EQ(1U, args.size());
-
-  AllowJavascript();
-  const bool is_lacros_enabled = crosapi::browser_util::IsLacrosEnabled();
-  std::string callback_id = args[0].GetString();
-  ResolveJavascriptCallback(base::Value(callback_id),
-                            base::Value(is_lacros_enabled));
-}
-
-void SyncInternalsMessageHandler::OpenLacrosSyncInternals(
-    const base::Value::List& args) {
-  CHECK_EQ(0U, args.size());
-
-  // Note: This will only be called by the UI when Lacros is available.
-  DCHECK(crosapi::BrowserManager::Get());
-  crosapi::BrowserManager::Get()->SwitchToTab(
-      GURL(chrome::kChromeUISyncInternalsUrl),
-      /*path_behavior=*/NavigateParams::RESPECT);
-}
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
 void SyncInternalsMessageHandler::OnReceivedAllNodes(
     const std::string& callback_id,
