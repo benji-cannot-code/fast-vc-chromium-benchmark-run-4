e98360aaf4a192fe00750929b1cbb1e993e38b54
Darren Shen <shend@chromium.org>
2024-08-20 07:27:39 +0000

    Add interactive UI tests for file previews.
    Bug: b:360229206
    Change-Id: I8cec365e3808d7637c05da97e3dfff3566643bb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799225
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343983}

--- ash/picker/views/picker_preview_bubble.cc
+++ ash/picker/views/picker_preview_bubble.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <string_view>
 
+#include "ash/ash_element_identifiers.h"
 #include "ash/bubble/bubble_constants.h"
 #include "ash/bubble/bubble_utils.h"
 #include "ash/style/typography.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/image_view.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/box_layout_view.h"
+#include "ui/views/view_class_properties.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -88,23 +90,23 @@ PickerPreviewBubbleView::PickerPreviewBubbleView(views::View* anchor_view)
       .set_margins(kMargins)
       .set_corner_radius(kPickerBubbleCornerRadius)
       .SetButtons(ui::DIALOG_BUTTON_NONE)
-      .AddChildren(
-          views::Builder<RoundedPreviewImageView>(
-              std::make_unique<RoundedPreviewImageView>(
-                  kPreviewImageSize, kPreviewBackgroundBorderRadius))
-              .CopyAddressTo(&image_view_),
-          views::Builder<views::BoxLayoutView>()
-              .SetOrientation(views::BoxLayout::Orientation::kVertical)
-              .SetCrossAxisAlignment(
-                  views::BoxLayout::CrossAxisAlignment::kStart)
-              .SetInsideBorderInsets(kLabelPadding)
-              .SetVisible(false)
-              .CopyAddressTo(&box_layout_view_)
-              .AddChildren(views::Builder<views::Label>(
-                               ash::bubble_utils::CreateLabel(
-                                   TypographyToken::kCrosBody2, u"",
-                                   cros_tokens::kCrosSysOnSurface))
-                               .CopyAddressTo(&main_label_)))
+      .SetProperty(views::kElementIdentifierKey, kPickerPreviewBubbleElementId)
+      .AddChildren(views::Builder<RoundedPreviewImageView>(
+                       std::make_unique<RoundedPreviewImageView>(
+                           kPreviewImageSize, kPreviewBackgroundBorderRadius))
+                       .CopyAddressTo(&image_view_),
+                   views::Builder<views::BoxLayoutView>()
+                       .SetOrientation(views::BoxLayout::Orientation::kVertical)
+                       .SetCrossAxisAlignment(
+                           views::BoxLayout::CrossAxisAlignment::kStart)
+                       .SetInsideBorderInsets(kLabelPadding)
+                       .SetVisible(false)
+                       .CopyAddressTo(&box_layout_view_)
+                       .AddChildren(views::Builder<views::Label>(
+                                        ash::bubble_utils::CreateLabel(
+                                            TypographyToken::kCrosBody2, u"",
+                                            cros_tokens::kCrosSysOnSurface))
+                                        .CopyAddressTo(&main_label_)))
       .BuildChildren();
 
   // Show the widget.
