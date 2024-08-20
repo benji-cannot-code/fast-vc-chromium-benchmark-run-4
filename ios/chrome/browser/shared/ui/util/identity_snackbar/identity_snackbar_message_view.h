8e46611c46870f4c549b31e369f8cda70b8b8d5e
Arthur Milchior <arthurmilchior@chromium.org>
2024-08-20 16:25:49 +0000

    [iOS][Identity snackbar] create the snackbar
    It also uses it when the user switch account.
    
    
    Result can be seen on https://drive.google.com/file/d/188nhel1nKRuoF5wyNPq6zhQGApSpJYDV/view?usp=drive_link and https://drive.google.com/file/d/1MrkE6vHgOcEZb1P4jia2C5XdXdxCUmIX/view?usp=drive_link
    
    MDCSnackbar internals
    
    Low-Coverage-Reason: HARD_TO_TEST The code is too dependent on
    Bug: 336720134
    Change-Id: Idb724910af5bef3a398f8e74187a81525df2d287
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5756583
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344199}

---
+++ ios/chrome/browser/shared/ui/util/identity_snackbar/identity_snackbar_message_view.h
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_UI_UTIL_IDENTITY_SNACKBAR_IDENTITY_SNACKBAR_MESSAGE_VIEW_H_
+#define IOS_CHROME_BROWSER_SHARED_UI_UTIL_IDENTITY_SNACKBAR_IDENTITY_SNACKBAR_MESSAGE_VIEW_H_
+
+#import <MaterialComponents/MaterialSnackbar.h>
+
+// The identity snackbar view.
+@interface IdentitySnackbarMessageView : MDCSnackbarMessageView
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_SHARED_UI_UTIL_IDENTITY_SNACKBAR_IDENTITY_SNACKBAR_MESSAGE_VIEW_H_
--- ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.h"
 
+#import <MaterialComponents/MaterialSnackbar.h>
+
 #import "base/check.h"
 #import "base/strings/sys_string_conversions.h"
 #import "components/signin/public/base/signin_metrics.h"
@@ -22,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/show_signin_command.h"
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
 #import "ios/chrome/browser/shared/ui/table_view/table_view_utils.h"
+#import "ios/chrome/browser/shared/ui/util/identity_snackbar/identity_snackbar_message.h"
 #import "ios/chrome/browser/shared/ui/util/snackbar_util.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
@@ -75,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _syncEncryptionPassphraseTableViewController;
   // ApplicationCommands handler.
   id<ApplicationCommands> _applicationHandler;
+  ChromeAccountManagerService* _accountManagerService;
 }
 
 - (void)dealloc {
@@ -88,7 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _syncService = SyncServiceFactory::GetForBrowserState(browserState);
   _authenticationService =
       AuthenticationServiceFactory::GetForBrowserState(browserState);
-  ChromeAccountManagerService* accountManagerService =
+  _accountManagerService =
       ChromeAccountManagerServiceFactory::GetForBrowserState(browserState);
   signin::IdentityManager* identityManager =
       IdentityManagerFactory::GetForBrowserState(browserState);
@@ -112,7 +116,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   _mediator =
       [[AccountMenuMediator alloc] initWithSyncService:_syncService
-                                 accountManagerService:accountManagerService
+                                 accountManagerService:_accountManagerService
                                            authService:_authenticationService
                                        identityManager:identityManager];
   _mediator.delegate = self;
@@ -155,6 +159,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   _applicationHandler = nil;
   _syncService = nullptr;
   _authenticationService = nullptr;
+  _accountManagerService = nullptr;
   [self stopSignoutActionSheetCoordinator];
   [self stopAccountsCoordinator];
   [super stop];
@@ -326,12 +331,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)triggerAccountSwitchSnackbarWithIdentity:
     (id<SystemIdentity>)systemIdentity {
-  NSString* accountName = systemIdentity.userGivenName
-                              ? systemIdentity.userGivenName
-                              : systemIdentity.userEmail;
-  MDCSnackbarMessage* snackbarTitle = CreateSnackbarMessage(
-      l10n_util::GetNSStringF(IDS_IOS_ACCOUNT_MENU_SWITCH_CONFIRMATION_TITLE,
-                              base::SysNSStringToUTF16(accountName)));
+  UIImage* avatar = _accountManagerService->GetIdentityAvatarWithIdentity(
+      systemIdentity, IdentityAvatarSize::Regular);
+  MDCSnackbarMessage* snackbarTitle =
+      [[IdentitySnackbarMessage alloc] initWithName:systemIdentity.userGivenName
+                                              email:systemIdentity.userEmail
+                                             avatar:avatar];
   CommandDispatcher* dispatcher = self.browser->GetCommandDispatcher();
   id<SnackbarCommands> snackbarCommandsHandler =
       HandlerForProtocol(dispatcher, SnackbarCommands);
