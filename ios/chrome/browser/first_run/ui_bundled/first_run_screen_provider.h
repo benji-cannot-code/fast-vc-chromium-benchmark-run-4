5d79241358042414404e4f07645d9e057f28fa76
Mark Cogan <marq@google.com>
2024-08-20 11:04:11 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/first_run/ui_bundled.
    
    This CL was uploaded by git cl split.
    
    R=arthurmilchior@chromium.org
    
    Bug: 359819865
    Change-Id: Iaf894f1077bff0e2dcc08ba634dc6913d704ffec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797025
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344058}

--- ios/chrome/browser/first_run/ui_bundled/first_run_screen_provider.h
+++ ios/chrome/browser/first_run/ui_bundled/first_run_screen_provider.h
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 #import "ios/chrome/browser/ui/screen/screen_provider.h"
-
-class ChromeBrowserState;
 
 // The class that provides a list of first run screens.
 @interface FirstRunScreenProvider : ScreenProvider
