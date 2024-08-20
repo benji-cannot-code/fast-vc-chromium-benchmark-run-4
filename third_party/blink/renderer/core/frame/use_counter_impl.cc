fac69f8ade3db4bb3f15f453cea3d992acd08bdb
Johnny Stenback <jstenback@google.com>
2024-08-20 19:18:57 +0000

    Replace WebFeature::kNumberOfFeatures with kMaxValue.
    The Mojo enum WebFeature has a kNumberOfFeatures member at the end of
    the enum which is no longer needed as the mojo code automatically
    generates a kMaxValue constant in all enums. This change switches
    over to using the auto-generated kMaxValue constant and removes
    kNumberOfFeatures.
    
    Bug: 40529934
    Change-Id: Iac499c839d3ffd369408b4ed97825bd1eabac1be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789635
    Reviewed-by: Josh Karlin <jkarlin@chromium.org>
    Commit-Queue: Johnny Stenback <jstenback@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344319}

--- third_party/blink/renderer/core/frame/use_counter_impl.cc
+++ third_party/blink/renderer/core/frame/use_counter_impl.cc
@@ -99,8 +99,8 @@ bool UseCounterImpl::IsCounted(WebFeature web_feature) const {
     return false;
 
   // PageVisits is reserved as a scaling factor.
-  DCHECK_NE(WebFeature::kPageVisits, web_feature);
-  DCHECK_GE(WebFeature::kNumberOfFeatures, web_feature);
+  DCHECK_NE(web_feature, WebFeature::kPageVisits);
+  DCHECK_LE(web_feature, WebFeature::kMaxValue);
 
   return feature_tracker_.Test(
       {mojom::blink::UseCounterFeatureType::kWebFeature,
@@ -223,8 +223,8 @@ void UseCounterImpl::Count(CSSPropertyID property,
 void UseCounterImpl::Count(WebFeature web_feature,
                            const LocalFrame* source_frame) {
   // PageVisits is reserved as a scaling factor.
-  DCHECK_NE(WebFeature::kPageVisits, web_feature);
-  DCHECK_GE(WebFeature::kNumberOfFeatures, web_feature);
+  DCHECK_NE(web_feature, WebFeature::kPageVisits);
+  DCHECK_LE(web_feature, WebFeature::kMaxValue);
 
   Count({mojom::blink::UseCounterFeatureType::kWebFeature,
          static_cast<uint32_t>(web_feature)},
@@ -271,12 +271,11 @@ void UseCounterImpl::CountFeature(WebFeature feature) const {
       NOTREACHED_IN_MIGRATION();
       return;
     case kExtensionContext:
-      UMA_HISTOGRAM_ENUMERATION("Blink.UseCounter.Extensions.Features", feature,
-                                WebFeature::kNumberOfFeatures);
+      UMA_HISTOGRAM_ENUMERATION("Blink.UseCounter.Extensions.Features",
+                                feature);
       return;
     case kFileContext:
-      UMA_HISTOGRAM_ENUMERATION("Blink.UseCounter.File.Features", feature,
-                                WebFeature::kNumberOfFeatures);
+      UMA_HISTOGRAM_ENUMERATION("Blink.UseCounter.File.Features", feature);
       return;
     case kDisabledContext:
       NOTREACHED_IN_MIGRATION();
