1958afb9341d69519c2255a60f4e87d3af6d316e
Mark Cogan <marq@google.com>
2024-08-20 06:19:00 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/price_insights.
    
    This CL was uploaded by git cl split.
    
    R=cheickcisse@google.com
    
    Bug: 359819865
    Change-Id: I5c46a99fd49fef8d802d060c39f1e2946cfba066
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797344
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1343952}

--- ios/chrome/browser/price_insights/model/price_insights_feature.h
+++ ios/chrome/browser/price_insights/model/price_insights_feature.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <string>
 
-class ChromeBrowserState;
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 // Feature flag parameter in Price Insights to determine the text displayed for
 // a low price.
