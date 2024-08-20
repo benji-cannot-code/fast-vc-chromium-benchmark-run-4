df1b6c6a34e44db939af323c1171490fe95ca3cc
Tommy Steimel <steimel@chromium.org>
2024-08-20 18:22:27 +0000

    [autopip] Record PromptResult metric even when we don't show the prompt
    This CL adds new values for the Media.AutoPictureInPicture.PromptResult
    metric that are used when the prompt is not shown. These record *why*
    the prompt wasn't shown (e.g. is the user has already allowed the
    permission).
    
    Bug: 360357709
    Change-Id: I7710873ab2dcb6d07b0f9db4c30e70dbe21d3ee8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793749
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344283}

--- chrome/browser/picture_in_picture/auto_pip_setting_helper.cc
+++ chrome/browser/picture_in_picture/auto_pip_setting_helper.cc
@@ -97,6 +97,7 @@ AutoPipSettingHelper::CreateOverlayViewIfNeeded(
       // If the user already said to allow once, then continue allowing.  It's
       // assumed that we're used for at most one visit to a site.
       if (already_selected_allow_once_) {
+        RecordResult(PromptResult::kNotShownAllowedOnce);
         return nullptr;
       }
       // Create and return the UI to ask the user.
@@ -106,9 +107,11 @@ AutoPipSettingHelper::CreateOverlayViewIfNeeded(
           browser_view_overridden_bounds, anchor_view, arrow);
     case CONTENT_SETTING_ALLOW:
       // Nothing to do -- allow the auto pip to proceed.
+      RecordResult(PromptResult::kNotShownAllowedOnEveryVisit);
       return nullptr;
     case CONTENT_SETTING_BLOCK:
       // Auto-pip is not allowed.  Close the window.
+      RecordResult(PromptResult::kNotShownBlocked);
       std::move(close_pip_cb).Run();
       return nullptr;
     default:
@@ -116,6 +119,14 @@ AutoPipSettingHelper::CreateOverlayViewIfNeeded(
       std::move(close_pip_cb).Run();
       return nullptr;
   }
+}
+
+void AutoPipSettingHelper::OnAutoPipBlockedByPermission() {
+  RecordResult(PromptResult::kNotShownBlocked);
+}
+
+void AutoPipSettingHelper::OnAutoPipBlockedByIncognito() {
+  RecordResult(PromptResult::kNotShownIncognito);
 }
 
 void AutoPipSettingHelper::OnUiResult(base::OnceClosure close_pip_cb,
@@ -144,6 +155,6 @@ void AutoPipSettingHelper::OnUiResult(base::OnceClosure close_pip_cb,
 }
 
 void AutoPipSettingHelper::RecordResult(PromptResult result) {
-  base::UmaHistogramEnumeration("Media.AutoPictureInPicture.PromptResult",
+  base::UmaHistogramEnumeration("Media.AutoPictureInPicture.PromptResultV2",
                                 result);
 }
