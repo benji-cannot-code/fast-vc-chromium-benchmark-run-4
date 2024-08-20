75f65b7de028b5050b2bc4326fb74ceb70e55a70
stluong <stluong@chromium.org>
2024-08-20 23:47:13 +0000

    Implement Toast Registry
    This CL implements the toast registry which keeps track of the
    registered toasts and its specifications.
    
    Bug: 358607638
    Change-Id: Ie9dea62b7c5ab1862e777623402a70a114b6ee5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799084
    Reviewed-by: Alison Gale <agale@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Auto-Submit: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344487}

---
+++ chrome/browser/ui/toasts/toast_id.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TOASTS_TOAST_ID_H_
+#define CHROME_BROWSER_UI_TOASTS_TOAST_ID_H_
+
+// Each toast is supposed to have its own unique toast id.
+enum class ToastId {
+  kLinkCopied = 0,
+  kMin = kLinkCopied,
+  kImageCopied = 1,
+  kMax = kImageCopied
+};
+
+#endif  // CHROME_BROWSER_UI_TOASTS_TOAST_ID_H_
