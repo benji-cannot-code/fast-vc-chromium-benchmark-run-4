20635c67361ca5a127789b17cb56d6af976cb2f5
Jack Yammine <jyammine@google.com>
2024-08-20 12:35:46 +0000

    [waffle]Remove keyword milestone row metadata table (1/2)
    This is part of a multi-CL change. The next CL will cleanup and delete
    the unused code when the kSearchEnginesSortingCleanup feature is enabled.
    
    We clear the milestone row when the feature is enabled as part of the
    database cleanup.
    
    Bug: b:340412113
    Change-Id: Id72cd47a0b5c59024f58b3849ceab13817bf35e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797052
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344083}

--- components/search_engines/keyword_web_data_service.cc
+++ components/search_engines/keyword_web_data_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/search_engines/keyword_table.h"
 #include "components/search_engines/template_url_data.h"
 #include "components/webdata/common/web_data_results.h"
+#include "components/webdata/common/web_database.h"
 #include "components/webdata/common/web_database_service.h"
 
 namespace {
@@ -63,6 +64,12 @@ WebDatabase::State SetBuiltinKeywordMilestoneImpl(int milestone_version,
                                                   WebDatabase* db) {
   return KeywordTable::FromWebDatabase(db)->SetBuiltinKeywordMilestone(
              milestone_version)
+             ? WebDatabase::COMMIT_NEEDED
+             : WebDatabase::COMMIT_NOT_NEEDED;
+}
+
+WebDatabase::State ClearBuiltinKeywordMilestoneImpl(WebDatabase* db) {
+  return KeywordTable::FromWebDatabase(db)->ClearBuiltinKeywordMilestone()
              ? WebDatabase::COMMIT_NEEDED
              : WebDatabase::COMMIT_NOT_NEEDED;
 }
@@ -172,6 +179,11 @@ void KeywordWebDataService::SetBuiltinKeywordDataVersion(int version) {
 void KeywordWebDataService::SetBuiltinKeywordMilestone(int version) {
   wdbs_->ScheduleDBTask(
       FROM_HERE, base::BindOnce(&SetBuiltinKeywordMilestoneImpl, version));
+}
+
+void KeywordWebDataService::ClearBuiltinKeywordMilestone() {
+  wdbs_->ScheduleDBTask(FROM_HERE,
+                        base::BindOnce(&ClearBuiltinKeywordMilestoneImpl));
 }
 
 void KeywordWebDataService::SetBuiltinKeywordCountry(int version) {
