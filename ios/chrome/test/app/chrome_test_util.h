c3549f6a867486c78b08e372994ca9eb0c6e6303
Mark Cogan <marq@google.com>
2024-08-20 05:56:30 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ['ios/chrome/browser/metrics/model', 'ios/chrome/browser/share_extension/model', 'ios/chrome/test/app'].
    
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Bug: 359819865
    Change-Id: I2c31c52628b743cdcb41af4dd917f643b3f679b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797204
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343943}

--- ios/chrome/test/app/chrome_test_util.h
+++ ios/chrome/test/app/chrome_test_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_TEST_APP_CHROME_TEST_UTIL_H_
 
 #import "base/ios/block_types.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 #import "ios/chrome/browser/shared/public/commands/browser_commands.h"
 #import "ios/chrome/browser/shared/public/commands/browser_coordinator_commands.h"
 
@@ -15,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @protocol UnitConversionCommands;
 
 class Browser;
-class ChromeBrowserState;
 @class MainController;
 @class NewTabPageController;
 @class SceneController;
