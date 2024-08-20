0dc6e8386a859e823ac61a3dace7a201a50143b2
kelsen liu <kelsenliu21@gmail.com>
2024-08-20 03:19:19 +0000

    Spanify MaskWebSocketFramePayload method to avoid unsafe buffer usage
    This CL spanifies the `MaskWebSocketFramePayload` and
    `MaskWebSocketFramePayloadByBytes` methods, replaces unchecked pointers
    with `base::span`, and enables unsafe-buffer-usage warnings in
    `websocket_frame`.
    
    Bug: 40284755,354307327
    Change-Id: I98710ab69174ef6d2049a7deaf6d203c528101ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5764863
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: kelsen liu <kelsenliu21@gmail.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343917}

--- net/websockets/websocket_frame.h
+++ net/websockets/websocket_frame.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include <array>
 #include <memory>
 #include <vector>
 
@@ -59,7 +60,7 @@ struct NET_EXPORT WebSocketFrameHeader {
 
   // Contains four-byte data representing "masking key" of WebSocket frames.
   struct WebSocketMaskingKey {
-    uint8_t key[WebSocketFrameHeader::kMaskingKeyLength];
+    std::array<uint8_t, WebSocketFrameHeader::kMaskingKeyLength> key;
   };
 
   // Constructor to avoid a lot of repetitive initialisation.
@@ -199,8 +200,7 @@ NET_EXPORT WebSocketMaskingKey GenerateWebSocketMaskingKey();
 NET_EXPORT void MaskWebSocketFramePayload(
     const WebSocketMaskingKey& masking_key,
     uint64_t frame_offset,
-    char* data,
-    int data_size);
+    base::span<uint8_t> data);
 
 }  // namespace net
 
