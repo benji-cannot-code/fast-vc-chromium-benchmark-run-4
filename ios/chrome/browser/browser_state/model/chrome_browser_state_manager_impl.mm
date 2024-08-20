683ec7f1c5ceb80157d274868f75e070e76fea00
Aliona DANGLA <alionadangla@google.com>
2024-08-20 15:20:55 +0000

    [iOS] Replace imports for sign in info and push notification
    This is part of renaming Chrome browser state project.
    
    Fixed: 360843134, 360843194
    Change-Id: Ibd5074b4532f3d892b4ee4212577e385aee5f2f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797148
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344156}

--- ios/chrome/browser/browser_state/model/chrome_browser_state_manager_impl.mm
+++ ios/chrome/browser/browser_state/model/chrome_browser_state_manager_impl.mm
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/page_info/about_this_site_service_factory.h"
 #import "ios/chrome/browser/plus_addresses/model/plus_address_service_factory.h"
 #import "ios/chrome/browser/profile/model/off_the_record_profile_ios_impl.h"
-#import "ios/chrome/browser/push_notification/model/push_notification_browser_state_service_factory.h"
+#import "ios/chrome/browser/push_notification/model/push_notification_profile_service_factory.h"
 #import "ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/profile/profile_attributes_storage_ios.h"
--- ios/chrome/browser/signin/model/signin_profile_info_updater_factory.mm
+++ ios/chrome/browser/signin/model/signin_profile_info_updater_factory.mm
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/signin/model/signin_browser_state_info_updater_factory.h"
+#import "ios/chrome/browser/signin/model/signin_profile_info_updater_factory.h"
 
 #import "base/no_destructor.h"
 #import "components/keyed_service/ios/browser_state_dependency_manager.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
-#import "ios/chrome/browser/signin/model/signin_browser_state_info_updater.h"
 #import "ios/chrome/browser/signin/model/signin_error_controller_factory.h"
+#import "ios/chrome/browser/signin/model/signin_profile_info_updater.h"
 
 // static
 SigninBrowserStateInfoUpdater*
