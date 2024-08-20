22c99cbd3bbcb00dfc0707825bf7d138887647dd
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-20 15:38:30 +0000

    spanification: Refresh `#pragma allow_unsafe_buffers`.
    This patch was fully automated using script:
    https://paste.googleplex.com/5614491201175552
    
    This is a "refresh" upon previously approved directories. This is
    mostly //chrome, because enabling the warning got reverted just before I
    went on vacation, and new occurrences got added:
    https://chromium-review.googlesource.com/c/chromium/src/+/5767579
    
    See internal doc about it:
    https://docs.google.com/document/d/1erdcokeh6rfBqs_h0drHqSLtbDbB61j7j3O2Pz8NH78/edit?resourcekey=0-hNe6w1hYAYyVXGEpWI7HVA&tab=t.0
    
    Bug: 40285824
    Change-Id: I402573ae6e9bd1c01939e06210b4141dfe79bc6f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797215
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Owners-Override: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344165}

--- chrome/browser/ui/web_applications/web_share_target_browsertest.cc
+++ chrome/browser/ui/web_applications/web_share_target_browsertest.cc
@@ -3,6 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#ifdef UNSAFE_BUFFERS_BUILD
+// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
+#pragma allow_unsafe_buffers
+#endif
+
 #include <limits>
 #include <string>
 #include <string_view>
