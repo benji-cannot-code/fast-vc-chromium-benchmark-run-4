fd124a17444315f9601aac00cc6e6dd4e34fbdba
JunHo Seo <junho0924.seo@lge.com>
2024-08-20 06:28:20 +0000

    Snav: Fix focus to document wrong way in snav-testharness.js
    In https://crrev.com/c/5782429 ,
    If current focused element is null, give focus to document
    as start point. But focus() function should be called for
    document.body, not document.
    This CL fixes the mistake.
    
    Bug: 351244340
    Change-Id: Id555b1330a7cb4ff8df0d899d1ba0698d870b97c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797376
    Reviewed-by: Stefan Zager <szager@chromium.org>
    Commit-Queue: JunHo Seo <junho0924.seo@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1343959}

--- third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
+++ third_party/blink/web_tests/fast/spatial-navigation/resources/snav-testharness.js
@@ -160,7 +160,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         // Some test pages give focus to arbitrary element as start point.
         // Otherwise, ensure root document as start point.
         if (!focusedElement())
-          document.focus();
+          document.body.focus();
         stepAndAssertMoves(expectedMoves);
       }));
     }
