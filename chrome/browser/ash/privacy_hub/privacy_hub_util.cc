8a0dd3bbe2d532919ccebeb61de218c7e24affae
Georg Neis <neis@chromium.org>
2024-08-20 02:25:04 +0000

    ash: Create //chromeos/ash/components/camera_presence_notifier
    Move chrome/browser/ash/camera_presence_notifier* there.
    
    Bug: b:356476366
    Change-Id: I28abdc535d559bc2f60aada2f86f41d1b7ef06ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782518
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343902}

--- chrome/browser/ash/privacy_hub/privacy_hub_util.cc
+++ chrome/browser/ash/privacy_hub/privacy_hub_util.cc
@@ -27,11 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/notreached.h"
 #include "base/supports_user_data.h"
-#include "chrome/browser/ash/camera_presence_notifier.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/app_access_notifier.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
+#include "chromeos/ash/components/camera_presence_notifier/camera_presence_notifier.h"
 #include "components/prefs/pref_change_registrar.h"
 #include "components/prefs/pref_service.h"
 
