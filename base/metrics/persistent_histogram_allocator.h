63a8889d1e1610de913fd184c2dd7743cf8b7eca
Dave Tapuska <dtapuska@chromium.org>
2024-08-20 15:54:15 +0000

    [allocator] Remove UMA.PersistentAllocator.*.Errors histogram
    The histogram was not logged since M85. Remove the code for
    allocs_histogram_ as well since it hasn't been re-enabled in 7 years.
    
    OBSOLETE_HISTOGRAMS=UMA.PersistentAllocator.{PersistentAllocatorType}.Errors
    
    Bug: 40920284
    Change-Id: I259e4b94d5acbfe5ad8eba567a0615c45633417b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797012
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344172}

--- base/metrics/persistent_histogram_allocator.h
+++ base/metrics/persistent_histogram_allocator.h
@@ -288,7 +288,6 @@ class BASE_EXPORT PersistentHistogramAllocator {
   //
   // IMPORTANT: tools/metrics/histograms/metadata/uma/histograms.xml must
   // be updated with the following histograms for each |name| param:
-  //    UMA.PersistentAllocator.name.Errors
   //    UMA.PersistentAllocator.name.UsedPct
   void CreateTrackingHistograms(std::string_view name);
   void UpdateTrackingHistograms();
@@ -524,4 +523,4 @@ class BASE_EXPORT GlobalHistogramAllocator
 
 }  // namespace base
 
-#endif  // BASE_METRICS_PERSISTENT_HISTOGRAM_ALLOCATOR_H__
+#endif  // BASE_METRICS_PERSISTENT_HISTOGRAM_ALLOCATOR_H_
