54d3d6edd3ca012e7f645b8d5aa1b3cfb63f8ac8
David Bertoni <dbertoni@chromium.org>
2024-08-20 01:05:17 +0000

    [Extensions] Move favicon utils into chrome/browser/extensions.
    These files don't implement an API, so this CL moves them into the
    chrome/browser/extensions/favicon.
    
    Bug: 0593486
    Change-Id: I195d3e5fd489a6a626ed7fa60759b52272f542c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5787043
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343883}

--- chrome/browser/extensions/favicon/favicon_util.h
+++ chrome/browser/extensions/favicon/favicon_util.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_API_FAVICON_FAVICON_UTIL_H_
-#define CHROME_BROWSER_EXTENSIONS_API_FAVICON_FAVICON_UTIL_H_
+#ifndef CHROME_BROWSER_EXTENSIONS_FAVICON_FAVICON_UTIL_H_
+#define CHROME_BROWSER_EXTENSIONS_FAVICON_FAVICON_UTIL_H_
 
 #include "base/functional/callback.h"
 #include "base/memory/scoped_refptr.h"
@@ -48,4 +48,4 @@ bool ParseFaviconPath(const GURL& favicon_url,
 }  // namespace favicon_util
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_API_FAVICON_FAVICON_UTIL_H_
+#endif  // CHROME_BROWSER_EXTENSIONS_FAVICON_FAVICON_UTIL_H_
