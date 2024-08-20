5c6bb4bdef7225b2ca4441404b45106a3ec6eecc
Peter Boström <pbos@chromium.org>
2024-08-20 22:05:57 +0000

    IWYU for std::unordered_map in event_validator.h
    This was transitively included through some <functional> =>
    __functional/boyer_moore_searcher.h in C++17.
    
    Bug: None
    Change-Id: Ib62e989f304e66fbbb0bb2a41c9a087af0dbd44b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799722
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Mark Pearson <mpearson@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344424}

--- components/metrics/structured/event_validator.h
+++ components/metrics/structured/event_validator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 
 #include "components/metrics/structured/enums.h"
 #include "components/metrics/structured/event.h"
