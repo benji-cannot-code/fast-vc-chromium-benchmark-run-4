402ef3d10f1045c3dc7d00026016cc4cc5c54883
Prashanth Swaminathan <prashanthsw@google.com>
2024-08-20 08:54:15 +0000

    Remove references to android_toolchain_canary
    This canary build of the toolchain was created for the purposes of
    enabling RISC-V builds for initial testing. Now that the NDK r27
    toolchain supports building with RISC-V and we no longer use the canary
    toolchain, remove it to reclaim the space.
    
    Test: CQ
    Bug: 360867127
    Change-Id: I1f15b1e878ba158339f7d0454e32af9d8417ac03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798143
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344018}

--- third_party/android_toolchain/3pp/install.sh
+++ third_party/android_toolchain/3pp/install.sh
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# THIS MUST BE KEPT IN SYNC WITH ../../android_toolchain_canary/3pp/install.sh.
-
 set -e
 set -x
 set -o pipefail
