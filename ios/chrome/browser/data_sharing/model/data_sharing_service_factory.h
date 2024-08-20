f630e6f2b08bdb4a0b90cfbd4e295bc55187483a
Mark Cogan <marq@google.com>
2024-08-20 00:02:23 +0000

    [iOS] Migrate ChromeBrowserState forward declaration to header.
    This CL migrates forward declarations of ChromeBrowserState in header
    files to instead import a forward header. This header can be updated as
    ChromeBrowserState is migrated to ProfileIOS without needing to (again)
    touch hundreds of header files. BUILD files are also updated for this
    change.
    
    Where needed, this CL also changes #include directives to #import, which
    is the preferred usage in //ios.
    
    This specific CL updates files in ios/chrome/browser/data_sharing.
    
    This CL was uploaded by git cl split.
    
    R=nyquist@chromium.org
    
    Bug: 359819865
    Change-Id: Ia6e47454ae167663a5cdca90b9dabe1a50b12b57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797165
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1343855}

--- ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h
+++ ios/chrome/browser/data_sharing/model/data_sharing_service_factory.h
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/no_destructor.h"
 #import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-
-class ChromeBrowserState;
+#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 namespace data_sharing {
 
