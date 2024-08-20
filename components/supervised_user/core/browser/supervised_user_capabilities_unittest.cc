f0067544ce2620cdb63a45a4389d00703f207554
Nohemi Fernandez <fernandex@chromium.org>
2024-08-20 15:58:23 +0000

    Add tests for SupervisedUserCapabilitiesObserver.
    Verifies that changes to the primary account capabilities state are
    tracked by SupervisedUserCapabilitiesObserver.
    
    Follow-up to crrev.com/c/5788404. Verified that the test suite would have caught the regression to supervision state update notifications.
    
    Bug: b:358092862
    Change-Id: I09a83bf480868c60b43e3e120c844422f8f71709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797275
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344175}

--- components/supervised_user/core/browser/supervised_user_capabilities_unittest.cc
+++ components/supervised_user/core/browser/supervised_user_capabilities_unittest.cc
@@ -9,11 +9,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/identity_test_environment.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace supervised_user {
 namespace {
-constexpr char kChildEmail[] = "name@gmail.com";
+constexpr char kEmail[] = "name@gmail.com";
+
+class MockSupervisedUserCapabilitiesObserver
+    : SupervisedUserCapabilitiesObserver {
+ public:
+  explicit MockSupervisedUserCapabilitiesObserver(
+      signin::IdentityManager* identity_manager)
+      : SupervisedUserCapabilitiesObserver(identity_manager) {}
+
+  ~MockSupervisedUserCapabilitiesObserver() override = default;
+
+  MOCK_METHOD1(OnIsSubjectToParentalControlsCapabilityChanged,
+               void(CapabilityUpdateState));
+};
+
 }  // namespace
 
 class SupervisedUserCapabilitiesTest : public ::testing::Test {
@@ -24,6 +39,12 @@ class SupervisedUserCapabilitiesTest : public ::testing::Test {
 
 TEST_F(SupervisedUserCapabilitiesTest,
        SignedOutUserNotSubjectToParentalControls) {
+  // Expect no change to account capabilities state.
+  MockSupervisedUserCapabilitiesObserver observer(
+      identity_test_env_.identity_manager());
+  EXPECT_CALL(observer, OnIsSubjectToParentalControlsCapabilityChanged)
+      .Times(0);
+
   EXPECT_EQ(IsPrimaryAccountSubjectToParentalControls(
                 identity_test_env_.identity_manager()),
             signin::Tribool::kFalse);
@@ -31,8 +52,18 @@ TEST_F(SupervisedUserCapabilitiesTest,
 
 TEST_F(SupervisedUserCapabilitiesTest,
        SignedInAdultNotSubjectToParentalControls) {
+  // Expect account capabilities to notify of parental controls change.
+  base::RunLoop run_loop;
+  MockSupervisedUserCapabilitiesObserver observer(
+      identity_test_env_.identity_manager());
+  EXPECT_CALL(observer, OnIsSubjectToParentalControlsCapabilityChanged)
+      .WillOnce([&](supervised_user::CapabilityUpdateState state) {
+        ASSERT_EQ(supervised_user::CapabilityUpdateState::kSetToFalse, state);
+        run_loop.Quit();
+      });
+
   AccountInfo account_info = identity_test_env_.MakePrimaryAccountAvailable(
-      kChildEmail, signin::ConsentLevel::kSignin);
+      kEmail, signin::ConsentLevel::kSignin);
   AccountCapabilitiesTestMutator mutator(&account_info.capabilities);
   mutator.set_is_subject_to_parental_controls(false);
   identity_test_env_.UpdateAccountInfoForAccount(account_info);
@@ -43,8 +74,18 @@ TEST_F(SupervisedUserCapabilitiesTest,
 }
 
 TEST_F(SupervisedUserCapabilitiesTest, SignedInChildSubjectToParentalControls) {
+  // Expect account capabilities to notify of parental controls change.
+  base::RunLoop run_loop;
+  MockSupervisedUserCapabilitiesObserver observer(
+      identity_test_env_.identity_manager());
+  EXPECT_CALL(observer, OnIsSubjectToParentalControlsCapabilityChanged)
+      .WillOnce([&](supervised_user::CapabilityUpdateState state) {
+        ASSERT_EQ(supervised_user::CapabilityUpdateState::kSetToTrue, state);
+        run_loop.Quit();
+      });
+
   AccountInfo account_info = identity_test_env_.MakePrimaryAccountAvailable(
-      kChildEmail, signin::ConsentLevel::kSignin);
+      kEmail, signin::ConsentLevel::kSignin);
   AccountCapabilitiesTestMutator mutator(&account_info.capabilities);
   mutator.set_is_subject_to_parental_controls(true);
   identity_test_env_.UpdateAccountInfoForAccount(account_info);
@@ -56,12 +97,38 @@ TEST_F(SupervisedUserCapabilitiesTest, SignedInChildSubjectToParentalControls) {
 
 TEST_F(SupervisedUserCapabilitiesTest,
        SignedInUnknownIsSubjectToParentalControls) {
+  // Expect no change to account capabilities state.
+  MockSupervisedUserCapabilitiesObserver observer(
+      identity_test_env_.identity_manager());
+  EXPECT_CALL(observer, OnIsSubjectToParentalControlsCapabilityChanged)
+      .Times(0);
   AccountInfo account_info = identity_test_env_.MakePrimaryAccountAvailable(
-      kChildEmail, signin::ConsentLevel::kSignin);
+      kEmail, signin::ConsentLevel::kSignin);
 
   EXPECT_EQ(IsPrimaryAccountSubjectToParentalControls(
                 identity_test_env_.identity_manager()),
             signin::Tribool::kUnknown);
 }
+
+// ChromeOS does not support sign-out in tests.
+#if !BUILDFLAG(IS_CHROMEOS)
+TEST_F(SupervisedUserCapabilitiesTest,
+       SignOutTriggersIsSubjectToParentalControlsUpdate) {
+  AccountInfo account_info = identity_test_env_.MakePrimaryAccountAvailable(
+      kEmail, signin::ConsentLevel::kSignin);
+
+  // Expect detached account capabilities update.
+  base::RunLoop run_loop;
+  MockSupervisedUserCapabilitiesObserver observer(
+      identity_test_env_.identity_manager());
+  EXPECT_CALL(observer, OnIsSubjectToParentalControlsCapabilityChanged)
+      .WillOnce([&](supervised_user::CapabilityUpdateState state) {
+        ASSERT_EQ(supervised_user::CapabilityUpdateState::kDetached, state);
+        run_loop.Quit();
+      });
+
+  identity_test_env_.ClearPrimaryAccount();
+}
+#endif
 
 }  // namespace supervised_user
