c5c0f30f321723167a21828cb37a9b1642614368
Arjun Srinivasan <asriniva@google.com>
2024-08-20 20:51:32 +0000

    Disable instance limit of 5 under flag DisableInstanceLimit
    Bug: b/354605663
    Change-Id: Ifac5af45b2b615bf143bea7e821ba44e7bb3ba0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799064
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Arjun Srinivasan <asriniva@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344372}

--- chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
+++ chrome/android/java/src/org/chromium/chrome/browser/multiwindow/MultiWindowUtils.java
@@ -175,7 +175,9 @@ public class MultiWindowUtils implements ActivityStateListener {
         return sMaxInstancesForTesting != null
                 ? sMaxInstancesForTesting
                 : (isMultiInstanceApi31Enabled()
-                        ? TabWindowManager.MAX_SELECTORS_S
+                        ? (ChromeFeatureList.sDisableInstanceLimit.isEnabled()
+                                ? TabWindowManager.MAX_SELECTORS
+                                : TabWindowManager.MAX_SELECTORS_S)
                         : TabWindowManager.MAX_SELECTORS_LEGACY);
     }
 
