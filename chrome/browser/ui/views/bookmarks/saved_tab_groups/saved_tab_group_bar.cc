bd280b1048922b22a5a4ab337713acbdbeb44574
dljames <dljames@chromium.org>
2024-08-20 21:44:36 +0000

    [TabGroupSync] Use TabGroupSyncService in Browser
    Update Browser to use TabGroupSyncService instead of direct usage of
    SavedTabGroupKeyedService.
    
    This is done to support the migration from STGKS -> TabGroupSyncService.
    
    Also moves SavedTabGroupModelObserver and TabGroupSyncService::Observer
    functions into a new TabGroupSyncObserver object which lives in
    BrowserWindowFeatures.
    
    Change-Id: I5aa18a8f7db67ac1c74c87d539284afa2add61e0
    Bug: 350514491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5766545
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344410}

--- chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar.cc
+++ chrome/browser/ui/views/bookmarks/saved_tab_groups/saved_tab_group_bar.cc
@@ -224,7 +224,7 @@ SavedTabGroupBar::SavedTabGroupBar(Browser* browser,
                           base::Unretained(this))));
 
   // Add the observer.
-  // TODO(crbug.com/359715038): Consider consolidating logic by forwarding
+  // TODO(crbug.com/361110303): Consider consolidating logic by forwarding
   // observer in proxy.
   if (tab_groups::IsTabGroupSyncServiceDesktopMigrationEnabled()) {
     tab_group_service_->AddObserver(this);
@@ -263,7 +263,7 @@ SavedTabGroupBar::~SavedTabGroupBar() {
   // Remove all buttons from the hierarchy
   RemoveAllButtons();
 
-  // TODO(crbug.com/359715038): Consider consolidating logic by forwarding
+  // TODO(crbug.com/361110303): Consider consolidating logic by forwarding
   // observer in proxy.
   if (tab_groups::IsTabGroupSyncServiceDesktopMigrationEnabled()) {
     tab_group_service_->RemoveObserver(this);
