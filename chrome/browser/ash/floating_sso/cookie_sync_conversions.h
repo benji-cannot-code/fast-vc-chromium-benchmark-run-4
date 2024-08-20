e459eb874790b1334d6408a99e6a3717efadcdda
Jihad Hanna <jihadghanna@google.com>
2024-08-20 14:48:22 +0000

    Revert "[Floating SSO] Apply changes from Sync server to the browser"
    This reverts commit efb684a5c76be753eadca6a3d2e4e60e77311ee8.
    
    Reason for revert: Possible culprit of failures in many
    FloatingSsoTest.
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20(1)/59846/overview
    
    
    Original change's description:
    > [Floating SSO] Apply changes from Sync server to the browser
    >
    > FloatingSsoSyncBridge now sends notifications to observers about all
    > incoming changes from the Sync server. FloatingSsoService observes the
    > bridge and applies corresponding cookie changes to the browser. Note
    > that cookie changes generated in this way also trigger `OnCookieChange`
    > events in the service, and special care is taken to avoid routing these
    > notifications back to the bridge (besides being an optimization, it
    > removes a lot of noise during debugging).
    >
    > Existing bridge tests are modified to check that they send notifications
    > as expected. Test helpers from cookie_sync_test_util.h are changed to
    > allow creating persistent cookies and to allow specifying creation time
    > instead of using a hardcoded value (which could lead to unexpected test
    > failures in the future - e.g. Chrome doesn't allow a cookie to live
    > longer than 400 days).
    >
    > Bug: b:353225533
    > Change-Id: Ic725a1a7900a4fdb199aa3f008c6a928f7aff3e4
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776503
    > Reviewed-by: Marc Treib <treib@chromium.org>
    > Commit-Queue: Andrey Davydov <andreydav@google.com>
    > Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1344050}
    
    Bug: b:353225533
    Change-Id: I7f0bafcb885cf87fe29bf02ee22c32f93178451e
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800295
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344139}

--- chrome/browser/ash/floating_sso/cookie_sync_conversions.h
+++ chrome/browser/ash/floating_sso/cookie_sync_conversions.h
@@ -6,14 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_FLOATING_SSO_COOKIE_SYNC_CONVERSIONS_H_
 #define CHROME_BROWSER_ASH_FLOATING_SSO_COOKIE_SYNC_CONVERSIONS_H_
 
-#include <stdint.h>
-
 #include <memory>
 #include <optional>
-
-namespace base {
-class Time;
-}
 
 namespace net {
 class CanonicalCookie;
@@ -24,9 +18,6 @@ class CookieSpecifics;
 }
 
 namespace ash::floating_sso {
-
-// Time format used in CookieSpecifics.
-int64_t ToMicrosSinceWindowsEpoch(const base::Time& time);
 
 // Returns nullptr if some members of `proto` can't be deserialized or
 // if the cookie saved in `proto` is not canonical.
--- chrome/browser/ash/floating_sso/floating_sso_sync_bridge.h
+++ chrome/browser/ash/floating_sso/floating_sso_sync_bridge.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
-#include "base/observer_list_types.h"
 #include "components/sync/model/conflict_resolution.h"
 #include "components/sync/model/data_type_store.h"
 #include "components/sync/model/data_type_store_base.h"
@@ -29,22 +27,10 @@ class MetadataChangeList;
 class ModelError;
 }  // namespace syncer
 
-namespace net {
-class CanonicalCookie;
-}  // namespace net
-
 namespace ash::floating_sso {
 
 class FloatingSsoSyncBridge : public syncer::DataTypeSyncBridge {
  public:
-  class Observer : public base::CheckedObserver {
-   public:
-    virtual void OnCookiesAddedOrUpdatedRemotely(
-        const std::vector<net::CanonicalCookie>& cookies) = 0;
-    virtual void OnCookiesRemovedRemotely(
-        const std::vector<net::CanonicalCookie>& cookies) = 0;
-  };
-
   using CookieSpecificsEntries =
       std::map<std::string, sync_pb::CookieSpecifics>;
 
@@ -71,15 +57,11 @@ class FloatingSsoSyncBridge : public syncer::DataTypeSyncBridge {
       const std::string& storage_key,
       const syncer::EntityData& remote_data) const override;
 
-  // Methods to notify about local changes. Made virtual for mocking in tests.
-  virtual void AddOrUpdateCookie(const sync_pb::CookieSpecifics& specifics);
-  virtual void DeleteCookie(const std::string& storage_key);
-
-  void AddObserver(Observer* observer);
-  void RemoveObserver(Observer* observer);
+  void AddOrUpdateCookie(const sync_pb::CookieSpecifics& specifics);
+  void DeleteCookie(const std::string& storage_key);
 
   // Assumes that the `store_` is initialized.
-  const CookieSpecificsEntries& CookieSpecificsInStore() const;
+  const CookieSpecificsEntries& CookieSpecificsEntriesForTest() const;
   bool IsInitialDataReadFinishedForTest() const;
   void SetOnCommitCallbackForTest(base::RepeatingClosure callback);
 
@@ -109,9 +91,6 @@ class FloatingSsoSyncBridge : public syncer::DataTypeSyncBridge {
   // processor are not ready.
   std::map<std::string, sync_pb::CookieSpecifics> deferred_cookie_additions_;
   std::set<std::string> deferred_cookie_deletions_;
-
-  // Observers which are notified about all incoming remote changes.
-  base::ObserverList<Observer> observers_;
 
   base::WeakPtrFactory<FloatingSsoSyncBridge> weak_ptr_factory_{this};
 };
