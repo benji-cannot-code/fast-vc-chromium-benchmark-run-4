19503b0c15725050ad74b7d76a56b3c58fd471c1
Jood Hajeer <jood@google.com>
2024-08-21 16:56:43 +0000

    [iOS] Dismiss the account menu when the primary account is removed.
    Before this CL, the topmost view controller gets dismissed.
    After this CL, presentingViewController and all controllers presented on
    top of it will be dismissed.
    
    Also this CL adds an egtest to verify this case.
    
    Bug: 336719423
    Change-Id: I9f226149426cef81f0918c2e5ddc4eeb3f7a6bb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783201
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344874}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
@@ -140,7 +140,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     std::move(_accountDetailsControllerDismissCallback).Run(/*animated=*/false);
   }
   [self stopAccountsCoordinator];
-  [_navigationController dismissViewControllerAnimated:YES completion:nil];
+  [_navigationController.presentingViewController
+      dismissViewControllerAnimated:YES
+                         completion:nil];
   _authenticationService = nil;
   _navigationController.delegate = nil;
   _navigationController = nil;
