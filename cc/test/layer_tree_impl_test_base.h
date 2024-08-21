a4bf5852fd0fdabcab79eda6e6f457aad9533764
Gastón Rodríguez <gastonr@microsoft.com>
2024-08-21 17:55:56 +0000

    Add test for Fluent scrollbars when no thumb color is set.
    CL:5786520 added a new early return for
    PaintedScrollbarLayerImpl::AppendThumbQuads when Fluent scrollbars are
    enabled and no thumb color is set.
    
    This CL adds a new unit test to check that the early return is hit when
    these conditions are met.
    
    Bug: 359623669
    Change-Id: I8f68cb778e566217f10da42253e597d3988135b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800411
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Commit-Queue: Gaston Rodriguez <gastonr@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1344914}

--- cc/test/layer_tree_impl_test_base.h
+++ cc/test/layer_tree_impl_test_base.h
@@ -54,6 +54,7 @@ class LayerTreeImplTestBase {
   }
 
   void CalcDrawProps(const gfx::Size& viewport_size);
+  void AppendQuads(LayerImpl* layer_impl);
   void AppendQuadsWithOcclusion(LayerImpl* layer_impl,
                                 const gfx::Rect& occluded);
   void AppendQuadsForPassWithOcclusion(
