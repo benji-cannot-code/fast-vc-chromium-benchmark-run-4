7ac5da20a8853bb7c7bb0695b452220994f66fe5
Georg Neis <neis@chromium.org>
2024-08-21 06:08:44 +0000

    ash: Create //chrome/browser/ash/fwupd
    ... and move c/b/ui/ash/fwupd_download_client* there.
    
    Bug: b:358244714
    Change-Id: I6ffc628d6dceb9cf52fe464ff3258932871bd2c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796282
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344602}

--- chrome/browser/ash/fwupd/fwupd_download_client_impl.h
+++ chrome/browser/ash/fwupd/fwupd_download_client_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
-#define CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
+#ifndef CHROME_BROWSER_ASH_FWUPD_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
+#define CHROME_BROWSER_ASH_FWUPD_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
 
 #include "ash/public/cpp/fwupd_download_client.h"
 #include "base/memory/scoped_refptr.h"
@@ -29,4 +29,4 @@ class FwupdDownloadClientImpl : public ash::FwupdDownloadClient {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_ASH_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
+#endif  // CHROME_BROWSER_ASH_FWUPD_FWUPD_DOWNLOAD_CLIENT_IMPL_H_
