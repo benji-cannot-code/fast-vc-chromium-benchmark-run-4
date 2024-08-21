0349d133bb8eb2ab27c0627a293e63d031b0fd36
Norge Vizcay <vizcay@google.com>
2024-08-21 13:26:22 +0000

    Clean up flag AutofillPreferLabelsInSomeCountries.
    The feature was default enabled on M128. This CL cleans up the flag two
    releases later.
    
    Bug: 40275657
    Change-Id: I888b4c88c1bc2913e6fcf3bcb9f1f535c91f3eaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803199
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344750}

--- components/autofill/core/browser/heuristic_classification_unittests.cc
+++ components/autofill/core/browser/heuristic_classification_unittests.cc
@@ -566,7 +566,6 @@ TEST_P(HeuristicClassificationTests, EndToEnd) {
       features::kAutofillEnableRationalizationEngineForMX,
       // Other improvements.
       features::kAutofillDefaultToCityAndNumber,
-      features::kAutofillPreferLabelsInSomeCountries,
       features::kAutofillEnableCacheForRegexMatching};
   std::vector<base::test::FeatureRef> disabled_features = {};
 
