8a972674c6f25fea8e73939812ee86dfd39dcee9
Qihui Zhao <qihuizhao@google.com>
2024-08-21 18:55:17 +0000

    Remove length column from masked_ibans table
    SQL diff compare: https://diff.googleplex.com/#key=sRufJwgz1tdm
    
    Bug: 355074921
    Change-Id: I5ca1e3f3a96f04e67966feb6a7319af5b8733adb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5786892
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Qihui Zhao <qihuizhao@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1344962}

--- components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
+++ components/autofill/core/browser/webdata/payments/payments_autofill_table.cc
@@ -131,7 +131,6 @@ constexpr std::string_view kMaskedIbansTable = "masked_ibans";
 // kInstrumentId = "instrument_id"
 constexpr std::string_view kPrefix = "prefix";
 // kSuffix = "suffix";
-constexpr std::string_view kLength = "length";
 // kNickname = "nickname"
 
 constexpr std::string_view kMaskedIbansMetadataTable = "masked_ibans_metadata";
@@ -579,6 +578,9 @@ bool PaymentsAutofillTable::MigrateToVersion(int version,
     case 131:
       *update_compatible_version = true;
       return MigrateToVersion131RemoveGenericPaymentInstrumentTypeColumn();
+    case 133:
+      *update_compatible_version = true;
+      return MigrateToVersion133RemoveLengthColumnFromMaskedIbansTable();
   }
   return true;
 }
@@ -1268,10 +1270,10 @@ bool PaymentsAutofillTable::GetCreditCardCloudTokenData(
 
 bool PaymentsAutofillTable::GetServerIbans(std::vector<std::unique_ptr<Iban>>& ibans) {
   sql::Statement s;
-  SelectBuilder(db_, s, kMaskedIbansTable,
-                {kInstrumentId, kUseCount, kUseDate, kNickname, kPrefix,
-                 kSuffix, kLength},
-                "LEFT OUTER JOIN masked_ibans_metadata USING (instrument_id)");
+  SelectBuilder(
+      db_, s, kMaskedIbansTable,
+      {kInstrumentId, kUseCount, kUseDate, kNickname, kPrefix, kSuffix},
+      "LEFT OUTER JOIN masked_ibans_metadata USING (instrument_id)");
 
   ibans.clear();
   while (s.Step()) {
@@ -1288,7 +1290,6 @@ bool PaymentsAutofillTable::GetServerIbans(std::vector<std::unique_ptr<Iban>>& i
     iban->set_nickname(s.ColumnString16(index++));
     iban->set_prefix(s.ColumnString16(index++));
     iban->set_suffix(s.ColumnString16(index++));
-    index++;  // Skip unused `length` field.
     ibans.push_back(std::move(iban));
   }
 
@@ -1306,7 +1307,7 @@ bool PaymentsAutofillTable::SetServerIbansData(const std::vector<Iban>& ibans) {
 
   sql::Statement s;
   InsertBuilder(db_, s, kMaskedIbansTable,
-                {kInstrumentId, kNickname, kPrefix, kSuffix, kLength});
+                {kInstrumentId, kNickname, kPrefix, kSuffix});
   for (const Iban& iban : ibans) {
     CHECK_EQ(Iban::RecordType::kServerIban, iban.record_type());
     int index = 0;
@@ -1314,8 +1315,6 @@ bool PaymentsAutofillTable::SetServerIbansData(const std::vector<Iban>& ibans) {
     s.BindString16(index++, iban.nickname());
     s.BindString16(index++, iban.prefix());
     s.BindString16(index++, iban.suffix());
-    // TODO(b/355074921): Clean up length field from kMaskedIbansTable table.
-    s.BindInt64(index++, 0);
     if (!s.Run()) {
       return false;
     }
@@ -2084,7 +2083,7 @@ bool PaymentsAutofillTable::
                      {{kInstrumentId, "VARCHAR PRIMARY KEY NOT NULL"},
                       {kPrefix, "VARCHAR NOT NULL"},
                       {kSuffix, "VARCHAR NOT NULL"},
-                      {kLength, "INTEGER NOT NULL DEFAULT 0"},
+                      {"length", "INTEGER NOT NULL DEFAULT 0"},
                       {kNickname, "VARCHAR"}}) &&
          CreateTable(db_, kMaskedIbansMetadataTable,
                      {{kInstrumentId, "VARCHAR PRIMARY KEY NOT NULL"},
@@ -2135,6 +2134,11 @@ bool PaymentsAutofillTable::
     MigrateToVersion131RemoveGenericPaymentInstrumentTypeColumn() {
   return DropColumnIfExists(db_, kGenericPaymentInstrumentsTable,
                             "payment_instrument_type");
+}
+
+bool PaymentsAutofillTable::
+    MigrateToVersion133RemoveLengthColumnFromMaskedIbansTable() {
+  return DropColumnIfExists(db_, kMaskedIbansTable, "length");
 }
 
 void PaymentsAutofillTable::AddMaskedCreditCards(
@@ -2249,7 +2253,6 @@ bool PaymentsAutofillTable::InitMaskedIbansTable() {
       {{kInstrumentId, "VARCHAR PRIMARY KEY NOT NULL"},
        {kPrefix, "VARCHAR NOT NULL"},
        {kSuffix, "VARCHAR NOT NULL"},
-       {kLength, "INTEGER NOT NULL DEFAULT 0"},
        {kNickname, "VARCHAR"}});
 }
 
--- components/autofill/core/browser/webdata/payments/payments_autofill_table.h
+++ components/autofill/core/browser/webdata/payments/payments_autofill_table.h
@@ -182,7 +182,6 @@ struct ServerCvc {
 //                      shown when in a masked format.
 //   suffix             Contains the suffix of the full IBAN value that is
 //                      shown when in a masked format.
-//   length             Length of the full IBAN value.
 //   nickname           A nickname for the IBAN, entered by the user.
 // -----------------------------------------------------------------------------
 // masked_ibans_metadata
@@ -589,6 +588,7 @@ class PaymentsAutofillTable : public WebDatabaseTable {
   bool MigrateToVersion125DeleteFullServerCardsTable();
   bool MigrateToVersion129AddGenericPaymentInstrumentsTable();
   bool MigrateToVersion131RemoveGenericPaymentInstrumentTypeColumn();
+  bool MigrateToVersion133RemoveLengthColumnFromMaskedIbansTable();
 
  private:
   // Adds to |masked_credit_cards| and updates |server_card_metadata|.
