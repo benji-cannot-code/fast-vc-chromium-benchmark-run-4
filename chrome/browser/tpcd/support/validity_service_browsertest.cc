80457198282ff066a85bc2be57dabb753756bf24
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-08-21 16:49:55 +0000

    Make TPCD platform tests run on android
    There are a handful of TPCD tests that are written as a
    PlatformBrowserTest, which is intended to be cross-platform to run
    on both Desktop and Android. However, these only included
    in_process_browser_test.h and were only included in the
    `browser_tests` target, so they never ran on Android.
    
    Change these to also run on Android.
    
    Bug: 361002665
    Change-Id: If705fc249fd9cef188682026a43f3799697af68b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5801306
    Reviewed-by: Jonathan Njeunje <njeunje@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344869}

--- chrome/browser/tpcd/support/validity_service_browsertest.cc
+++ chrome/browser/tpcd/support/validity_service_browsertest.cc
@@ -42,6 +42,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/test/base/android/android_browser_test.h"
+#else
+#include "chrome/test/base/in_process_browser_test.h"
+#endif
+
 using content::URLLoaderInterceptor;
 using content::WebContents;
 
