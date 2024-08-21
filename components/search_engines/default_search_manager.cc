fa141bc850209724a1c4934e333bc988412dd6e7
Tomasz Wiszkowski <ender@google.com>
2024-08-21 19:04:46 +0000

    Add histograms capturing Reconciliation data
    This change introduces several histograms tracking:
    - what kind of reconciliation was made to match TemplateURLs,
    - whether reconciliation (of specific type) was successful,
    - whether regulatory extensions were expanded (and if so, which).
    
    Bug: b/358683018
    Change-Id: I058fffbe5499f0d81b99efcab1edb2b71c995c43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798071
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344978}

--- components/search_engines/default_search_manager.cc
+++ components/search_engines/default_search_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/i18n/case_conversion.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
@@ -294,6 +295,14 @@ void DefaultSearchManager::OnOverridesPrefChanged() {
 }
 
 void DefaultSearchManager::MergePrefsDataWithPrepopulated() {
+  enum class ReconciliationType {
+    kNone,
+    kByID,
+    kByKeyword,
+    kByDomainBasedKeyword,
+    kMaxValue = kByDomainBasedKeyword
+  };
+
   if (!prefs_default_search_) {
     return;
   }
@@ -310,6 +319,8 @@ void DefaultSearchManager::MergePrefsDataWithPrepopulated() {
 
   // Don't call GetPrepopulatedEngines() if we don't have anything to reconcile.
   if (!(reconcile_by_id || reconcile_by_keyword)) {
+    base::UmaHistogramEnumeration("Omnibox.TemplateUrl.Reconciliation.Type",
+                                  ReconciliationType::kNone);
     return;
   }
 
@@ -319,6 +330,8 @@ void DefaultSearchManager::MergePrefsDataWithPrepopulated() {
   auto matching_engine = prepopulated_urls.end();
 
   if (reconcile_by_keyword) {
+    bool is_by_domain_based_keyword = false;
+
     std::u16string keyword = prefs_default_search_->keyword();
 
     if (prefs_default_search_->keyword() == u"yahoo.com") {
@@ -333,16 +346,31 @@ void DefaultSearchManager::MergePrefsDataWithPrepopulated() {
       std::string_view country_code =
           yahoo_search_host.substr(0, yahoo_search_host.find('.'));
       keyword = base::UTF8ToUTF16(country_code) + u".yahoo.com";
+      is_by_domain_based_keyword = true;
     }
 
     // Match by keyword.
     matching_engine = base::ranges::find(prepopulated_urls, keyword,
                                          &TemplateURLData::keyword);
+
+    base::UmaHistogramBoolean(
+        is_by_domain_based_keyword
+            ? "Omnibox.TemplateUrl.Reconciliation.ByDomainBasedKeyword.Result"
+            : "Omnibox.TemplateUrl.Reconciliation.ByKeyword.Result",
+        matching_engine != prepopulated_urls.end());
+    base::UmaHistogramEnumeration(
+        "Omnibox.TemplateUrl.Reconciliation.Type",
+        is_by_domain_based_keyword ? ReconciliationType::kByDomainBasedKeyword
+                                   : ReconciliationType::kByKeyword);
   } else if (reconcile_by_id) {
     // Match by prepopulate_id.
     matching_engine = base::ranges::find(prepopulated_urls,
                                          prefs_default_search_->prepopulate_id,
                                          &TemplateURLData::prepopulate_id);
+    base::UmaHistogramBoolean("Omnibox.TemplateUrl.Reconciliation.ByID.Result",
+                              matching_engine != prepopulated_urls.end());
+    base::UmaHistogramEnumeration("Omnibox.TemplateUrl.Reconciliation.Type",
+                                  ReconciliationType::kByID);
   }
 
   if (matching_engine == prepopulated_urls.end()) {
