e6895168045655c43e83d2285b1420534a89aea4
Muyao Xu <muyaoxu@google.com>
2024-08-21 16:47:44 +0000

    [Permission] Suppress cast permission rejected errors if dismissed
    If users disable the Local Network permission on mac, the GMC dialog
    shows the cast icon with an error, prompting users to enable the
    permission to use the cast feature. However, it can be distracting for
    users who don't need this feature.
    
    So this CL uses the PrefService to suppress showing the error again
    if users have dismissed it.
    
    Bug: 358821864
    Change-Id: I23621e166253f636c4fd55ff21fa862cbf30252d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784547
    Reviewed-by: Yiren Wang <yrw@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344866}

--- chrome/browser/ui/views/global_media_controls/cast_device_selector_view.cc
+++ chrome/browser/ui/views/global_media_controls/cast_device_selector_view.cc
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/global_media_controls/cast_device_selector_view.h"
 
 #include "base/metrics/histogram_functions.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/views/global_media_controls/media_item_ui_helper.h"
 #include "chrome/browser/ui/views/global_media_controls/media_notification_device_entry_ui.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/global_media_controls/public/views/media_item_ui_updated_view.h"
 #include "components/media_router/browser/media_router_metrics.h"
+#include "components/media_router/common/pref_names.h"
+#include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -220,9 +223,9 @@ void CastDeviceSelectorView::OnDevicesUpdated(
 }
 
 void CastDeviceSelectorView::OnPermissionRejected() {
-  // TODO(crbug.com/358821864): Do not show the permission error if users have
-  // dismissed it.
-  if (has_permission_rejected_issue_) {
+  if (has_permission_rejected_issue_ ||
+      g_browser_process->local_state()->GetBoolean(
+          media_router::prefs::kSuppressLocalDiscoveryPermissionError)) {
     return;
   }
   has_permission_rejected_issue_ = true;
@@ -356,7 +359,11 @@ void CastDeviceSelectorView::CloseButtonPressed() {
         RecordMediaRouterUiPermissionRejectedViewEvents(
             media_router::MediaRouterUiPermissionRejectedViewEvents::
                 kGmcDialogErrorDismissed);
+    g_browser_process->local_state()->SetBoolean(
+        media_router::prefs::kSuppressLocalDiscoveryPermissionError, true);
+    has_permission_rejected_issue_ = false;
   }
+
   HideDevices();
 }
 
--- components/media_router/common/pref_names.h
+++ components/media_router/common/pref_names.h
@@ -9,7 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media_router {
 namespace prefs {
 
+// Local State
 extern const char kMediaRouterCastAllowAllIPs[];
+extern const char kSuppressLocalDiscoveryPermissionError[];
+
+// Profile Prefs
 extern const char kMediaRouterMediaRemotingEnabled[];
 extern const char kMediaRouterReceiverIdHashToken[];
 extern const char kMediaRouterShowCastSessionsStartedByOtherDevices[];
