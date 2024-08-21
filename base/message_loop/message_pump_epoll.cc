e046219aad205194757c9ff8fdaee8c867ef67be
Michael Thiessen <mthiesse@chromium.org>
2024-08-21 15:43:29 +0000

    Add metric for the number of entries in the epoll event list
    Recorded periodically (every minute) for each epoll message pump.
    
    Bug: 360829950, 356125521
    Change-Id: I000fb3f758f9aa5bfbae0346b687fe6b38d34983
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798522
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344811}

--- base/message_loop/message_pump_epoll.cc
+++ base/message_loop/message_pump_epoll.cc
@@ -48,6 +48,8 @@ MessagePumpEpoll::MessagePumpEpoll() {
   epoll_event wake{.events = EPOLLIN, .data = {.ptr = &wake_event_}};
   int rv = epoll_ctl(epoll_.get(), EPOLL_CTL_ADD, wake_event_.get(), &wake);
   PCHECK(rv == 0);
+
+  next_metrics_time_ = base::TimeTicks::Now() + base::Minutes(1);
 }
 
 MessagePumpEpoll::~MessagePumpEpoll() = default;
@@ -118,6 +120,10 @@ void MessagePumpEpoll::Run(Delegate* delegate) {
       break;
     }
 
+    if (next_work_info.recent_now > next_metrics_time_) {
+      RecordPeriodicMetrics();
+    }
+
     // Reset the native work flag before processing IO events.
     native_work_started_ = false;
 
@@ -152,10 +158,15 @@ void MessagePumpEpoll::Run(Delegate* delegate) {
       break;
     }
 
+    TimeDelta next_metrics_delay =
+        next_metrics_time_ - next_work_info.recent_now;
     TimeDelta timeout = TimeDelta::Max();
     DCHECK(!next_work_info.delayed_run_time.is_null());
     if (!next_work_info.delayed_run_time.is_max()) {
       timeout = next_work_info.remaining_delay();
+    }
+    if (timeout > next_metrics_delay) {
+      timeout = next_metrics_delay;
     }
     delegate->BeforeWait();
     WaitForEpollEvents(timeout);
@@ -163,6 +174,12 @@ void MessagePumpEpoll::Run(Delegate* delegate) {
       break;
     }
   }
+}
+
+void MessagePumpEpoll::RecordPeriodicMetrics() {
+  UMA_HISTOGRAM_COUNTS_1000("MessagePumpEpoll.WatchedFileDescriptors",
+                            (int)entries_.size());
+  next_metrics_time_ += base::Minutes(1);
 }
 
 void MessagePumpEpoll::Quit() {
