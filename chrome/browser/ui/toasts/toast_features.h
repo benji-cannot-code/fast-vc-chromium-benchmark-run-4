11012d457fdacb45ce74ccdcc5db2e69e452b408
Alison Gale <agale@chromium.org>
2024-08-21 16:51:24 +0000

    Add the toast flags for mvp
    Bug: 357929155
    Change-Id: I4443db98cc8f1f7d67b8e46a76ef35f4b128b26a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804368
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344872}

---
+++ chrome/browser/ui/toasts/toast_features.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_TOASTS_TOAST_FEATURES_H_
+#define CHROME_BROWSER_UI_TOASTS_TOAST_FEATURES_H_
+
+#include "base/feature_list.h"
+
+namespace features {
+
+// Base feature
+BASE_DECLARE_FEATURE(kToastFramework);
+
+// Individual toasts
+BASE_DECLARE_FEATURE(kLinkCopiedToast);
+BASE_DECLARE_FEATURE(kImageCopiedToast);
+BASE_DECLARE_FEATURE(kLinkToHighlightCopiedToast);
+BASE_DECLARE_FEATURE(kReadingListToast);
+BASE_DECLARE_FEATURE(kLensOverlayToast);
+
+}  // namespace features
+
+#endif  // CHROME_BROWSER_UI_TOASTS_TOAST_FEATURES_H_
