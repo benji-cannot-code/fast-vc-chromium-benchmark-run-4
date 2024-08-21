4d8c36f0babc3b6f1b8415bbfab3ed740ac80655
Andres Calderon Jaramillo <andrescj@chromium.org>
2024-08-21 00:40:52 +0000

    GpuArcVD: Handle Reset() during frame request
    This CL is a port of CL:3113262 to the newer GpuArcVideoDecoder-based
    stack. The goal is to address the situation in which a Reset() is
    received on the Chrome side while in the middle of a request for frames.
    In this case, the VdaVideoFramePool is blocked (due to a
    base::WaitableEvent) waiting for the first frame [1] to come from ARC.
    Since that frame may never come because of the Reset(), it's necessary
    to unblock the VdaVideoFramePool in some other way. To do so, this CL
    makes the GpuArcVideoFramePool call the |notify_layout_changed_cb_| with
    kResetRequired when this situation occurs.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:media/gpu/chromeos/vda_video_frame_pool.cc;l=87;drc=83786cefc38b3e9cc29275b975ed091bf706a7ae
    
    Bug: b:360919987
    Test: ran "CtsMediaDecoderTestCases android.media.decoder.cts.DecoderTest#testCodecResetsH264WithSurface" 20 times (flaky until follow-up fixes land)
    Change-Id: I9d4ed48711d04919f0e344e9bf8601cc961b60df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800775
    Reviewed-by: Pilar Molina Lopez <pmolinalopez@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344517}

--- ash/components/arc/video_accelerator/gpu_arc_video_decoder.cc
+++ ash/components/arc/video_accelerator/gpu_arc_video_decoder.cc
@@ -87,7 +87,7 @@ void GpuArcVideoDecoder::Initialize(
   init_callback_ = std::move(callback);
   video_frame_pool_ = std::make_unique<GpuArcVideoFramePool>(
       std::move(video_frame_pool),
-      base::BindRepeating(&GpuArcVideoDecoder::OnRequestVideoFrames,
+      base::BindRepeating(&GpuArcVideoDecoder::ReleaseClientVideoFrames,
                           weak_this_),
       protected_buffer_manager_);
 
@@ -306,16 +306,15 @@ void GpuArcVideoDecoder::OnResetDone() {
     return;
   }
 
+  CHECK(video_frame_pool_);
+  video_frame_pool_->OnDecoderResetDone();
   std::move(reset_callback_).Run();
   HandleRequests();
 }
 
-void GpuArcVideoDecoder::OnRequestVideoFrames() {
+void GpuArcVideoDecoder::ReleaseClientVideoFrames() {
   DVLOGF(4);
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-
-  // Clear the list of video frames currently in use by the client, as new video
-  // frames will be added to the pool using the same ids.
   client_video_frames_.clear();
 }
 
@@ -328,6 +327,8 @@ void GpuArcVideoDecoder::OnError(media::DecoderStatus status) {
   }
 
   if (reset_callback_) {
+    CHECK(video_frame_pool_);
+    video_frame_pool_->OnDecoderResetDone();
     std::move(reset_callback_).Run();
   }
 
@@ -396,6 +397,12 @@ void GpuArcVideoDecoder::HandleResetRequest(ResetCallback callback) {
     OnError(media::DecoderStatus::Codes::kInvalidArgument);
     return;
   }
+
+  // HandleResetRequest() doesn't run if there's an unfinished Reset(). See
+  // HandleRequests().
+  CHECK(!reset_callback_);
+  CHECK(video_frame_pool_);
+  video_frame_pool_->WillResetDecoder();
 
   reset_callback_ = std::move(std::move(callback));
   decoder_->Reset(base::BindOnce(&GpuArcVideoDecoder::OnResetDone, weak_this_));
