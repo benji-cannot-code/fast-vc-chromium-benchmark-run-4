c8e5cb18c43158e5023defcb4e5102b524c60abc
Kent Tamura <tkent@chromium.org>
2024-08-21 05:32:05 +0000

    shape-outside: Fix an ellipse() issue in vertical writing modes
    EllipseShape should swap the horizontal radius and the vertical radius
    in vertical writing modes.
    
    Bug: 40149424, 360903551
    Change-Id: Id00db929cc5330c1c53ad0d02857a28b66841b63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799228
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344594}

--- third_party/blink/renderer/core/layout/shapes/ellipse_shape.cc
+++ third_party/blink/renderer/core/layout/shapes/ellipse_shape.cc
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/core/layout/shapes/ellipse_shape.h"
 
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
+
 namespace blink {
 
 static inline float EllipseXIntercept(float y, float rx, float ry) {
@@ -37,10 +39,21 @@ static inline float EllipseXIntercept(float y, float rx, float ry) {
   return rx * sqrt(1 - (y * y) / (ry * ry));
 }
 
-LogicalRect EllipseShape::ShapeMarginLogicalBoundingBox() const {
-  DCHECK_GE(ShapeMargin(), 0);
+std::pair<float, float> EllipseShape::InlineAndBlockRadiiIncludingMargin()
+    const {
   float margin_radius_x = radius_x_ + ShapeMargin();
   float margin_radius_y = radius_y_ + ShapeMargin();
+  if (!RuntimeEnabledFeatures::ShapeOutsideWritingModeFixEnabled() ||
+      IsHorizontalWritingMode(writing_mode_)) {
+    return {margin_radius_x, margin_radius_y};
+  }
+  return {margin_radius_y, margin_radius_x};
+}
+
+LogicalRect EllipseShape::ShapeMarginLogicalBoundingBox() const {
+  DCHECK_GE(ShapeMargin(), 0);
+  auto [margin_radius_x, margin_radius_y] =
+      InlineAndBlockRadiiIncludingMargin();
   return LogicalRect(LayoutUnit(center_.x() - margin_radius_x),
                      LayoutUnit(center_.y() - margin_radius_y),
                      LayoutUnit(margin_radius_x * 2),
@@ -49,8 +62,8 @@ LogicalRect EllipseShape::ShapeMarginLogicalBoundingBox() const {
 
 LineSegment EllipseShape::GetExcludedInterval(LayoutUnit logical_top,
                                               LayoutUnit logical_height) const {
-  float margin_radius_x = radius_x_ + ShapeMargin();
-  float margin_radius_y = radius_y_ + ShapeMargin();
+  auto [margin_radius_x, margin_radius_y] =
+      InlineAndBlockRadiiIncludingMargin();
   if (!margin_radius_x || !margin_radius_y)
     return LineSegment();
 
