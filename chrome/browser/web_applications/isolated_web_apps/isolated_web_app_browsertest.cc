531be39c9590942600f8c749c96649bacedb6382
Magdalena Skarbinska <mskarbinska@google.com>
2024-08-21 10:55:46 +0000

    Test if default display mode for IWA is minimal-ui
    This cl adds a check for correct default display mode for IWAs.
    Additionally, it removes display mode overwrite from IWA manifest for
    tests, as it does not affect any of the other tests and it facilitates
    testing the default mode.
    
    Bug: b/357674662
    Change-Id: Id60dfedc5479089518025d2fe0bd37f5e124e7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796839
    Reviewed-by: Simon Hangl <simonha@google.com>
    Commit-Queue: Magdalena Skarbińska <mskarbinska@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1344697}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_browsertest.cc
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_browsertest.cc
@@ -384,6 +384,7 @@ IN_PROC_BROWSER_TEST_F(
   EXPECT_NE(app_browser, browser());
   EXPECT_TRUE(
       AppBrowserController::IsForWebApp(app_browser, url_info.app_id()));
+  EXPECT_TRUE(app_browser->app_controller()->HasMinimalUiButtons());
   EXPECT_EQ(content::WebExposedIsolationLevel::kIsolatedApplication,
             app_frame->GetWebExposedIsolationLevel());
 }
