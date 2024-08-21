582a144b88c08936851d1484c842994faec78900
Janice Liu <janiceliu@chromium.org>
2024-08-21 18:39:10 +0000

    Add Fetching Implementation with Cross-Partitioned Blob URL
    Bug: 357484649
    Change-Id: I074b15fd78114185f1d7bfddc2647e2dd76bbed7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5762280
    Commit-Queue: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Auto-Submit: Janice Liu <janiceliu@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344948}

---
+++ storage/browser/blob/features.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "storage/browser/blob/features.h"
+
+namespace features {
+
+// Please keep features in alphabetical order.
+BASE_FEATURE(kBlockCrossPartitionBlobUrlFetching,
+             "BlockCrossPartitionBlobUrlFetching",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+// Please keep features in alphabetical order.
+
+}  // namespace features
---
+++ storage/browser/blob/features.h
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef STORAGE_BROWSER_BLOB_FEATURES_H_
+#define STORAGE_BROWSER_BLOB_FEATURES_H_
+
+#include "base/component_export.h"
+#include "base/features.h"
+
+namespace features {
+
+// Please keep features in alphabetical order.
+// Enables blob URL fetches to fail when cross-partition.
+COMPONENT_EXPORT(STORAGE_BROWSER)
+BASE_DECLARE_FEATURE(kBlockCrossPartitionBlobUrlFetching);
+
+// Please keep features in alphabetical order.
+
+}  // namespace features
+
+#endif  // STORAGE_BROWSER_BLOB_FEATURES_H_
