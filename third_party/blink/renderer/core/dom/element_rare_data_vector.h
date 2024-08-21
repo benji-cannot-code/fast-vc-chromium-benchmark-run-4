6eb7069bf84dc92ee546bb1a23ff71c27d620a19
Xianzhu Wang <wangxianzhu@chromium.org>
2024-08-21 21:06:12 +0000

    Move core/paint/sparse_vector.h to platform
    Change-Id: Ibc51474a2e1ec849e634a2e842dcf4d5cb624269
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800885
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345056}

--- third_party/blink/renderer/core/dom/element_rare_data_vector.h
+++ third_party/blink/renderer/core/dom/element_rare_data_vector.h
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/dom/node_rare_data.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element_data.h"
-#include "third_party/blink/renderer/core/paint/sparse_vector.h"
 #include "third_party/blink/renderer/platform/heap/trace_traits.h"
 #include "third_party/blink/renderer/platform/region_capture_crop_id.h"
 #include "third_party/blink/renderer/platform/restriction_target_id.h"
+#include "third_party/blink/renderer/platform/sparse_vector.h"
 #include "third_party/blink/renderer/platform/wtf/text/atomic_string.h"
 
 namespace blink {
--- third_party/blink/renderer/platform/sparse_vector_test.cc
+++ third_party/blink/renderer/platform/sparse_vector_test.cc
@@ -3,10 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "third_party/blink/renderer/core/paint/sparse_vector.h"
+#include "third_party/blink/renderer/platform/sparse_vector.h"
 
 #include "third_party/blink/renderer/platform/heap/persistent.h"
-#include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/googletest/src/googletest/include/gtest/gtest.h"
 
 namespace blink {
@@ -113,7 +112,6 @@ class SparseVectorTest : public testing::Test {
   auto& sparse_vector() { return test_->sparse_vector; }
 
  private:
-  test::TaskEnvironment task_environment_;
   typename TestType::Handle test_ = TestType::MakeTest();
 };
 
