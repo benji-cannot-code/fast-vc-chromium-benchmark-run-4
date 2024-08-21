de41bf79d41fde3f4f6e28c3a728c74f0b8d16fb
Will Harris <wfh@chromium.org>
2024-08-21 01:34:16 +0000

    Add two new apis to the Encryptor to determine status
    This CL adds two new APIs to the OSCrypt Async Encryptor which
    can be used to determine the status of the Encryptor.
    
    Encryption is available if there is a key loaded into the
    Encryptor that marks itself as available for encryption,
    or OSCrypt Sync encryption services are available.
    
    Decryption is available if there is any key available that
    might have previously encrypted data, or OSCrypt Sync
    encryption services are available.
    
    This API can be used to distinguish between encryption system
    failures (e.g. DPAPI failure, or the keychain
    is locked) and corrupt data.
    
    BUG=340606797
    
    Change-Id: If8fc8a4cb06c06907879127b88e435041c35a379
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5597805
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344541}

--- components/os_crypt/async/browser/os_crypt_async_unittest.cc
+++ components/os_crypt/async/browser/os_crypt_async_unittest.cc
@@ -21,6 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/hkdf.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if BUILDFLAG(IS_LINUX)
+#include "components/os_crypt/sync/key_storage_linux.h"
+#endif
+
 namespace os_crypt_async {
 
 namespace {
@@ -65,6 +69,28 @@ class OSCryptAsyncTest : public ::testing::Test {
                             option);
     run_loop.Run();
     return std::move(*encryptor);
+  }
+
+  // Simulate a 'locked' OSCrypt keychain on platforms that need it, which makes
+  // OSCrypt::IsEncryptionAvailable return false, without hitting a CHECK on
+  // Linux. Note this is different from using the full OSCryptMocker, because in
+  // this state, no key is available for encryption. Returns a
+  // ScopedClosureRunner that will reset the behavior back to default when it
+  // goes out of scope.
+  [[nodiscard]] static std::optional<base::ScopedClosureRunner>
+  MaybeSimulateLockedKeyChain() {
+#if BUILDFLAG(IS_LINUX)
+    OSCrypt::UseMockKeyStorageForTesting(base::BindOnce(
+        []() -> std::unique_ptr<KeyStorageLinux> { return nullptr; }));
+    return std::nullopt;
+#elif BUILDFLAG(IS_APPLE)
+    OSCrypt::UseLockedMockKeychainForTesting(/*use_locked=*/true);
+    return base::ScopedClosureRunner(base::BindOnce([]() {
+      OSCrypt::UseLockedMockKeychainForTesting(/*use_locked=*/false);
+    }));
+#else
+    return std::nullopt;
+#endif
   }
 
   base::test::TaskEnvironment task_environment_;
@@ -264,6 +290,7 @@ TEST_F(OSCryptAsyncTest, EncryptorOption) {
   }
   // Check that with just BLAH provider, data can still be decrypted.
   {
+    auto cleanup = MaybeSimulateLockedKeyChain();
     ProviderList providers;
     providers.emplace_back(
         /*precedence=*/5u,
@@ -271,6 +298,14 @@ TEST_F(OSCryptAsyncTest, EncryptorOption) {
                                           /*use_for_encryption=*/false));
     OSCryptAsync factory(std::move(providers));
     Encryptor encryptor = GetInstanceSync(factory);
+
+    // The only provider has indicated that it is not to be used for encryption,
+    // so encryption should not be available, as OSCrypt fallback is not
+    // available.
+    ASSERT_FALSE(encryptor.IsEncryptionAvailable());
+    // Decryption is possible, as long as the data is encrypted with the BLAH
+    // key.
+    ASSERT_TRUE(encryptor.IsDecryptionAvailable());
 
     auto plaintext = encryptor.DecryptData(*blah_ciphertext);
     // The correct provider based on the encrypted data header should have been
