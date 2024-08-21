5c84d2acb9a6037fb36defbb781c89808d077b53
Joshua Peraza <jperaza@chromium.org>
2024-08-21 16:59:16 +0000

    Remove dependency on internal url_constants.cc
    Bug: 358224255
    Change-Id: Ic5b96d3bf27cacc95bac1c011d520119c153153f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5801703
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Joshua Peraza <jperaza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344876}

--- components/crash/core/app/crash_reporter_client.cc
+++ components/crash/core/app/crash_reporter_client.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/crash/core/app/crash_reporter_client.h"
 
+#include "build/branding_buildflags.h"
 #include "build/build_config.h"
-#include "components/crash/core/app/url_constants.h"
 
 // On Windows don't use FilePath and logging.h.
 // http://crbug.com/604923
@@ -23,6 +23,10 @@ namespace crash_reporter {
 namespace {
 
 CrashReporterClient* g_client = nullptr;
+
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && defined(OFFICIAL_BUILD)
+const char kDefaultUploadURL[] = "https://clients2.google.com/cr/report";
+#endif
 
 }  // namespace
 
@@ -191,7 +195,11 @@ void CrashReporterClient::GetSanitizationInformation(
 #endif
 
 std::string CrashReporterClient::GetUploadUrl() {
-  return kDefaultUploadUrl;
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING) && defined(OFFICIAL_BUILD)
+  return kDefaultUploadURL;
+#else
+  return std::string();
+#endif
 }
 
 bool CrashReporterClient::ShouldMonitorCrashHandlerExpensively() {
