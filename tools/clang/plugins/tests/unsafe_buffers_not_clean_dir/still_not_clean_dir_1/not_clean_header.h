9234b63610ff67b90929083acacee3a496b7a445
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-08-21 07:51:15 +0000

    Unsafe-buffer error: Handle duplicates.
    Handle duplicates in opt-in/opt-out path prefixes to avoid unexpected
    behavior.
    
    This change normalizes the opt-in and opt-out path prefixes by sorting
    and removing duplicates or sub-prefixes. This ensures that adding
    additional opt-outs, even if redundant, does not inadvertently opt-in
    other targets. This is important, especially when landing concurrent
    patches, or revert/relanding some of them.
    
    Bug: 357649176
    Change-Id: I253b38ff330d33e5053703ecafe178de91471844
    Fixed: 357649176
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796896
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344630}

---
+++ tools/clang/plugins/tests/unsafe_buffers_not_clean_dir/still_not_clean_dir_1/not_clean_header.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_BUFFERS_NOT_CLEAN_DIR_STILL_NOT_CLEAN_DIR_1_NOT_CLEAN_HEADER_H_
+#define TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_BUFFERS_NOT_CLEAN_DIR_STILL_NOT_CLEAN_DIR_1_NOT_CLEAN_HEADER_H_
+
+inline int unchecked_violation_1(int* i, unsigned s) {
+  return i[s];  // This is in a known-bad header, so no error is emitted.
+}
+
+#endif  // TOOLS_CLANG_PLUGINS_TESTS_UNSAFE_BUFFERS_NOT_CLEAN_DIR_STILL_NOT_CLEAN_DIR_1_NOT_CLEAN_HEADER_H_
--- tools/clang/plugins/tests/unsafe_buffers_unchecked.cpp
+++ tools/clang/plugins/tests/unsafe_buffers_unchecked.cpp
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "unsafe_buffers_not_clean_dir/clean_header.h"
 #include "unsafe_buffers_not_clean_dir/not_clean_header.h"
+#include "unsafe_buffers_not_clean_dir/still_not_clean_dir_1/not_clean_header.h"
+#include "unsafe_buffers_not_clean_dir/still_not_clean_dir_2/not_clean_header.h"
 
 // This is in a known-bad cc file, so no error is emitted.
 DO_UNSAFE_THING_FROM_CHECKED_HEADER(UncheckedCpp, N, i, s);    // No error.
