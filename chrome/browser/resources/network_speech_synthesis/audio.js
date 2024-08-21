5c13e2a8020d5f78a712379d18d790e4c1aec04d
David Tseng <dtseng@google.com>
2024-08-21 23:37:23 +0000

    [a11y-mv3]: migrate network tts to mv3
    Done as a way to estimate cost of doing this type of work.
    
    In-depth writeup here
    https://docs.google.com/document/d/1L0i2mJ4kERqNjM3zeJqZIue5BapAa-NgA00K04Gzag0/edit?tab=t.0
    
    Bug: none
    Test: existing tts extension api browser tests; locally on Mac
    Change-Id: Ic59b0f0d4be25b7a1171ca3e8c5dbd190884f2d6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792343
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345144}

---
+++ chrome/browser/resources/network_speech_synthesis/audio.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Initialize the audio element and event listeners on it.
+window.audioElement_ = document.createElement('audio');
+document.body.appendChild(window.audioElement_);
+window.audioElement_.addEventListener('canplaythrough', onStart_, false);
+window.audioElement_.addEventListener('ended', onStop_, false);
+
+/**
+ * Handler for the canplaythrough event on the audio element.
+ * Called when the audio element has buffered enough audio to begin
+ * playback. Send the 'start' event to the ttsEngine callback and
+ * then begin playing the audio element.
+ * @private
+ */
+function onStart_() {
+  if (!window.currentUtterance_) {
+    return;
+  }
+
+  if (window.currentUtterance_.options.volume !== undefined) {
+    // Both APIs use the same range for volume, between 0.0 and 1.0.
+    window.audioElement_.volume = window.currentUtterance_.options.volume;
+  }
+  window.audioElement_.play();
+  chrome.runtime.sendMessage({command: 'onStart'});
+}
+
+function onStop_() {
+  window.currentUtterance_ = null;
+  chrome.runtime.sendMessage({command: 'onStop'});
+}
+
+chrome.runtime.onMessage.addListener(message => {
+  switch (message['command']) {
+    case 'pause':
+      window.audioElement_.pause();
+      break;
+    case 'play':
+      window.audioElement_.play();
+      break;
+    case 'setCurrentUtterance':
+      window.currentUtterance_ = message.currentUtterance;
+      break;
+    case 'setUrl':
+      window.audioElement_.src = message.url;
+      break;
+  }
+});
