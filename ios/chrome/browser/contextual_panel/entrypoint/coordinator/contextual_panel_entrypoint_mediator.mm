c6a0fdc106b760058917f1da108b25834e993314
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-08-21 15:47:15 +0000

    [iOS cont. panel] Dismiss in-product help when infobar badges are shown
    Dismiss in-product help bubble when infobar badges are shown, as they
    clash.
    
    Bug: b:361161504
    Change-Id: I6a70566a499ddfceb5a491b0fd1339a5221fadda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797488
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1344814}

--- ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_mediator.mm
+++ ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_mediator.mm
@@ -254,6 +254,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
   _infobarBadgesCurrentlyShown = infobarBadgesCurrentlyShown;
 
+  if (_infobarBadgesCurrentlyShown) {
+    [self dismissEntrypointIPHAnimated:YES];
+  }
+
   [self.consumer setInfobarBadgesCurrentlyShown:_infobarBadgesCurrentlyShown];
 }
 
