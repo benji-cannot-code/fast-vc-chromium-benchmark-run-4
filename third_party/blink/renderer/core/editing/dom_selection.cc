fd91c15674b79708260afd9d50ac50cbd877d2e4
Di Zhang <dizhangg@chromium.org>
2024-08-21 23:09:47 +0000

    [Selection API] Fix selection isCollapsed in ShadowDOM
    Selection isCollapsed should return true if and only if the anchor
    and focus are the same [1]. This should be true even if the selection
    is not in the light tree, but in the shadow tree [2].
    
    Note that if selection is across multiple shadow trees, it will still
    return true because the range endpoints are not valid and were
    collapsed. That should be re-evaluated in SelectionAcrossShadowDOM.
    
    [1] https://w3c.github.io/selection-api/#dom-selection-iscollapsed
    [2] https://w3c.github.io/selection-api/#h-note-3
    
    Change-Id: I4aaaef288ac1879644a3f719386d58c6025302bf
    Bug: 40400558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5792085
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345127}

--- third_party/blink/renderer/core/editing/dom_selection.cc
+++ third_party/blink/renderer/core/editing/dom_selection.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/bindings/exception_state.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
+#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -175,11 +176,13 @@ bool DOMSelection::isCollapsed() const {
   DomWindow()->document()->UpdateStyleAndLayout(
       DocumentUpdateReason::kSelection);
 
-  Node* node =
-      Selection().ComputeVisibleSelectionInDOMTree().Anchor().AnchorNode();
-  if (node && node->IsInShadowTree() &&
-      DomWindow()->document()->AncestorInThisScope(node)) {
-    return true;
+  if (!RuntimeEnabledFeatures::SelectionIsCollapsedShadowDOMSupportEnabled()) {
+    Node* node =
+        Selection().ComputeVisibleSelectionInDOMTree().Anchor().AnchorNode();
+    if (node && node->IsInShadowTree() &&
+        DomWindow()->document()->AncestorInThisScope(node)) {
+      return true;
+    }
   }
 
   TemporaryRange temp_range(this, PrimaryRangeOrNull());
