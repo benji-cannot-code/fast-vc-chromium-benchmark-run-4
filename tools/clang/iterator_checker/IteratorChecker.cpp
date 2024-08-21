0834d22ebedc504f4d00a9957f136c866ce552ad
Pasquale Riello <pas.riello@gmail.com>
2024-08-21 09:34:12 +0000

    iterator-checker: enable `ChromiumCheckModel`
    Enable `ChromiumCheckModel` that will handle Chromium's `CHECK` macros.
    
    Change-Id: I0ac0655e5f778dfa6a86e2eb34be2fa323591b0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5758688
    Commit-Queue: Paul Semel <paulsemel@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Paul Semel <paulsemel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1344662}

--- tools/clang/iterator_checker/IteratorChecker.cpp
+++ tools/clang/iterator_checker/IteratorChecker.cpp
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "clang/Analysis/FlowSensitive/AdornedCFG.h"
 #include "clang/Analysis/FlowSensitive/DataflowAnalysis.h"
 #include "clang/Analysis/FlowSensitive/DataflowLattice.h"
+#include "clang/Analysis/FlowSensitive/Models/ChromiumCheckModel.h"
 #include "clang/Analysis/FlowSensitive/NoopLattice.h"
 #include "clang/Analysis/FlowSensitive/Value.h"
 #include "clang/Analysis/FlowSensitive/WatchedLiteralsSolver.h"
@@ -727,6 +728,8 @@ class InvalidIteratorAnalysis
   void transfer(const clang::CFGElement& elt,
                 clang::dataflow::NoopLattice& state,
                 clang::dataflow::Environment& env) {
+    check_model_.transfer(elt, env);
+
     if (auto cfg_stmt = elt.getAs<clang::CFGStmt>()) {
       Transfer(*cfg_stmt->getStmt(), env);
     }
@@ -1897,6 +1900,9 @@ class InvalidIteratorAnalysis
         location, diagnostic_.getCustomDiagID(
                       clang::DiagnosticsEngine::Level::Error, error_message));
   }
+
+  // The check model that will handle Chromium's `CHECK` macros.
+  clang::dataflow::ChromiumCheckModel check_model_;
 
   // The diagnostic engine that will issue potential errors.
   clang::DiagnosticsEngine& diagnostic_;
