915517e1dbac706fc80bb951846f0f3976647fc2
Olesia Marukhno <olesiamarukhno@google.com>
2024-08-22 16:31:54 +0000

    [settings] Remove embeddedOnAnyHost tag for geolocation exceptions
    Geolocation exceptions are tagged as "embedded on any host" even though they are TOP_ORIGIN_ONLY_SCOPE. This is a leftover from a time when geolocation was granted to the requesting+embedding origin pair.
    
    Bug: 361550485
    Change-Id: I428a596ece0b6e85a1e1c357f24f137148f9f387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805428
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Olesia Marukhno <olesiamarukhno@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345475}

--- chrome/browser/resources/settings/site_settings/site_list_entry.ts
+++ chrome/browser/resources/settings/site_settings/site_list_entry.ts
@@ -240,8 +240,6 @@ export class SiteListEntryElement extends SiteListEntryElementBase {
         description = loadTimeData.getStringF(
             'embeddedOnHost', this.sanitizePort(this.model.embeddingOrigin));
       }
-    } else if (this.model.category === ContentSettingsTypes.GEOLOCATION) {
-      description = loadTimeData.getString('embeddedOnAnyHost');
     }
 
     try {
