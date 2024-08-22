3a0be4032f5c81643cd5e98566cbe3fc56839d39
Daniil Sakhapov <sakhapov@chromium.org>
2024-08-22 08:18:20 +0000

    [carousel] Implement focusgroup behavior for ::scroll-markers
    This CL implements behavior of focusgroup for ::scroll-marker pseudo
    elements, which consists of:
    1) When looking for prev/next focusable element from ::scroll-marker
    with `Tab` key,
    we should skip every other ::scroll-marker from the same
    ::scroll-marker-group;
    2) When prev/next focusable element is found to be ::scroll-marker,
    adjust it to be the currently selected (a.k.a last focused)
    ::scroll-marker of its ::scroll-marker-group.
    
    Explainers:
    https://github.com/flackr/carousel/tree/main/scroll-marker#scrolltargetelement
    https://open-ui.org/components/focusgroup.explainer/#last-focused-memory
    
    Bug: 332396355
    Change-Id: I16e5aea54fa4fa24a65327eb32074ca816f45674
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804322
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345289}

--- third_party/blink/renderer/core/dom/scroll_marker_group_pseudo_element.h
+++ third_party/blink/renderer/core/dom/scroll_marker_group_pseudo_element.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_CORE_DOM_SCROLL_MARKER_GROUP_PSEUDO_ELEMENT_H_
 #define THIRD_PARTY_BLINK_RENDERER_CORE_DOM_SCROLL_MARKER_GROUP_PSEUDO_ELEMENT_H_
 
+#include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/dom/pseudo_element.h"
 #include "third_party/blink/renderer/core/dom/scroll_marker_pseudo_element.h"
 
@@ -32,7 +33,7 @@ class ScrollMarkerGroupPseudoElement : public PseudoElement {
   const HeapVector<Member<ScrollMarkerPseudoElement>>& ScrollMarkers() {
     return focus_group_;
   }
-  void SetSelected(ScrollMarkerPseudoElement& scroll_marker);
+  CORE_EXPORT void SetSelected(ScrollMarkerPseudoElement& scroll_marker);
   ScrollMarkerPseudoElement* Selected() { return selected_marker_; }
   void ActivateNextScrollMarker();
   void ActivatePrevScrollMarker();
