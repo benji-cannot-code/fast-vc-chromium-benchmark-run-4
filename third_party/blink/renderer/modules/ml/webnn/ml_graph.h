1bd027664f4b3196bb7bb0e229a9258e1c4172ef
mingmingtasd <mingming1.xu@intel.com>
2024-08-22 11:22:37 +0000

    WebNN: Expose MLGraph::destroy interface to release graph
    Like `MLBuffer` and `MLContext`, `MLGraph` also holds a bunch of
    buffers. This CL exposes `MLGraph::destroy` interface to release graph.
    
    Bug: 357382539
    Change-Id: I1f462eb207aa571d459eef646b6f37891ff1f2a7
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799069
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1345340}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph.h
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph.h
@@ -85,6 +85,9 @@ class MODULES_EXPORT MLGraph : public ScriptWrappable {
 
   void Trace(Visitor* visitor) const override;
 
+  // ml_graph.idl
+  void destroy();
+
   const NamedOperandDescriptors& GetInputConstraints() const;
   const NamedOperandDescriptors& GetOutputConstraints() const;
 
@@ -113,8 +116,6 @@ class MODULES_EXPORT MLGraph : public ScriptWrappable {
 
   const MLContext* Context() const;
 
-  void OnConnectionError();
-
  private:
   void DidCompute(
       ScopedMLTrace scoped_trace,
@@ -124,6 +125,8 @@ class MODULES_EXPORT MLGraph : public ScriptWrappable {
       std::unique_ptr<Vector<std::pair<String, ArrayBufferViewInfo>>>
           outputs_info,
       webnn::mojom::blink::ComputeResultPtr mojo_result);
+
+  void OnConnectionError();
 
   // Describes the constraints on the inputs or outputs to this graph.
   // Note that `WTF::HashMap` values must be nullable, but
