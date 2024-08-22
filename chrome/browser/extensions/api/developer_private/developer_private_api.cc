0993391d90304df94a26d3b89599802231c10dcc
Kelvin Jiang <kelvinjiang@chromium.org>
2024-08-22 00:14:32 +0000

    [Extensions] DeveloperPrivate: Prevent repairing non CWS extensions
    Prevent repairing non policy, non CWS extensions using the
    developerPrivate repair function. This is another step to ensure that
    non CWS extension ids do not get sent over to CWS APIs
    
    Bug: 325314721
    Change-Id: I4ae9c2272bfda53a6e13ae82e07740c22b5f7c07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798284
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345159}

--- chrome/browser/extensions/api/developer_private/developer_private_api.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/devtools_util.h"
 #include "chrome/browser/extensions/error_console/error_console_factory.h"
 #include "chrome/browser/extensions/extension_commands_global_registry.h"
+#include "chrome/browser/extensions/extension_management.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
@@ -176,6 +177,9 @@ const char kUserCancelledError[] = "User cancelled uninstall";
 const char kNoExtensionError[] = "Extension with ID '*' doesn't exist.";
 const char kExtensionNotAffectedByMV2Deprecation[] =
     "Extension with ID '*' is not affected by the MV2 deprecation.";
+const char kCannotRepairNonWebstoreExtension[] =
+    "Cannot repair an extension that is not installed from the Chrome Web "
+    "Store.";
 
 const char kUnpackedAppsFolder[] = "apps_target";
 const char kManifestFile[] = "manifest.json";
@@ -2173,6 +2177,12 @@ DeveloperPrivateRepairExtensionFunction::Run() {
   content::WebContents* web_contents = GetSenderWebContents();
   if (!web_contents)
     return RespondNow(Error(kCouldNotFindWebContentsError));
+
+  ExtensionManagement* extension_management =
+      ExtensionManagementFactory::GetForBrowserContext(browser_context());
+  if (!extension_management->UpdatesFromWebstore(*extension)) {
+    return RespondNow(Error(kCannotRepairNonWebstoreExtension));
+  }
 
   auto reinstaller = base::MakeRefCounted<WebstoreReinstaller>(
       web_contents, params->extension_id,
