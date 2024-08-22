2643d9e338565c9da1ed7e00b51b3293a839ad14
Stephen Nusko <nuskos@chromium.org>
2024-08-22 17:00:04 +0000

    Ensure bounds checking of temp arrays by using std::array
    Accessing a static array (as tempstype and tempsorder were) isn't bounds
    checked and can cause memory safety issues. We're working on
    automatically switching such occurrences to use spans or std::arrays
    (which do bounds checking in Chromium code). Our automatic clang tooling
    struggles when multiple static arrays are declared on the same line.
    
    Switching the static array to an std::array prevents the plugin
    encountering such occurrences (which are rare) and ensures all accesses
    are bounds checked. Bring us one step closer to removing the pragma at
    the top of the file.
    
    Bug: 360641491
    Change-Id: Icc3c6726d3475737bfe507e070a4b1bdfaad7d8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803401
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345495}

--- third_party/blink/renderer/core/xml/xslt_unicode_sort.cc
+++ third_party/blink/renderer/core/xml/xslt_unicode_sort.cc
@@ -36,7 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <libxslt/templates.h>
 #include <libxslt/xsltutils.h>
+
+#include <array>
 #include <memory>
+
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/icu/source/common/unicode/uloc.h"
 #include "third_party/icu/source/i18n/unicode/ucol.h"
@@ -79,7 +82,7 @@ void XsltUnicodeSortFunction(xsltTransformContextPtr ctxt,
   xmlNodeSetPtr list = nullptr;
   int depth;
   xmlNodePtr node;
-  int tempstype[XSLT_MAX_SORT], temporder[XSLT_MAX_SORT];
+  std::array<int, XSLT_MAX_SORT> tempstype, temporder;
 
   if (!ctxt || !sorts || nbsorts <= 0 || nbsorts >= XSLT_MAX_SORT)
     return;
