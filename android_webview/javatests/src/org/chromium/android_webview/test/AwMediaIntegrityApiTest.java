3afefde4d058fd116fdfc50f25c779c6a830b449
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-08-22 13:18:31 +0000

    Switch to using type safe methods for media integrity [3/5]
    This is the third commit in sequence, which switches from the generic to
    the type safe implementation of media integrity in WebView.
    
    It should only be submitted after https://crrev.com/i/7603922 has
    landed.
    
    Commit list:
    * https://crrev.com/c/5759781
    * https://crrev.com/i/7603922
    * https://crrev.com/c/5800297 <-
    * https://crrev.com/i/7603942
    * https://crrev.com/c/5797280
    
    Internal commit overview at go/type-safe-media-integrity-error.
    
    Bug: 359452901
    Change-Id: Iccc89ec9ea6c566a8411b46f777e9f629e831ea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800297
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345385}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwMediaIntegrityApiTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwMediaIntegrityApiTest.java
@@ -25,6 +25,7 @@ import org.chromium.android_webview.WebMessageListener;
 import org.chromium.android_webview.common.AwFeatures;
 import org.chromium.android_webview.common.MediaIntegrityApiStatus;
 import org.chromium.android_webview.common.MediaIntegrityErrorCode;
+import org.chromium.android_webview.common.MediaIntegrityErrorWrapper;
 import org.chromium.android_webview.common.MediaIntegrityProvider;
 import org.chromium.android_webview.common.PlatformServiceBridge;
 import org.chromium.android_webview.common.PlatformServiceBridgeImpl;
@@ -1034,13 +1035,22 @@ public class AwMediaIntegrityApiTest extends AwParameterizedTest {
 
         public void addRequestError(
                 @Nullable String contentBinding, @MediaIntegrityErrorCode int errorCode) {
-            mResponses.computeIfAbsent(contentBinding, s -> new LinkedList<>()).offer(errorCode);
+            mResponses
+                    .computeIfAbsent(contentBinding, s -> new LinkedList<>())
+                    .offer(new MediaIntegrityErrorWrapper(errorCode));
         }
 
         @Override
         public void requestToken(
                 @Nullable String contentBinding,
                 @NonNull ValueOrErrorCallback<String, Integer> callback) {
+            Assert.fail("This method should not be called");
+        }
+
+        @Override
+        public void requestToken2(
+                @Nullable String contentBinding,
+                @NonNull ValueOrErrorCallback<String, MediaIntegrityErrorWrapper> callback) {
             mCallCount++;
             Queue<Object> responseQueue = mResponses.get(contentBinding);
             mCallCounts.compute(contentBinding, (s, count) -> count == null ? 1 : count + 1);
@@ -1050,8 +1060,8 @@ public class AwMediaIntegrityApiTest extends AwParameterizedTest {
                     callback.onResult(token);
                     return;
                 }
-                if (response instanceof Integer errorCode) {
-                    callback.onError(errorCode);
+                if (response instanceof MediaIntegrityErrorWrapper error) {
+                    callback.onError(error);
                     return;
                 }
             }
@@ -1104,7 +1114,9 @@ public class AwMediaIntegrityApiTest extends AwParameterizedTest {
                 @MediaIntegrityApiStatus int apiStatus,
                 @MediaIntegrityErrorCode int errorCode) {
             CallKey key = new CallKey(cloudProjectNumber, apiStatus);
-            mResponses.computeIfAbsent(key, k -> new LinkedList<>()).offer(errorCode);
+            mResponses
+                    .computeIfAbsent(key, k -> new LinkedList<>())
+                    .offer(new MediaIntegrityErrorWrapper(errorCode));
         }
 
         public int getProviderCallCount(
@@ -1122,6 +1134,14 @@ public class AwMediaIntegrityApiTest extends AwParameterizedTest {
                 long cloudProjectNumber,
                 @MediaIntegrityApiStatus int apiStatus,
                 ValueOrErrorCallback<MediaIntegrityProvider, Integer> callback) {
+            Assert.fail("This method should not be called");
+        }
+
+        @Override
+        public void getMediaIntegrityProvider2(
+                long cloudProjectNumber,
+                @MediaIntegrityApiStatus int apiStatus,
+                ValueOrErrorCallback<MediaIntegrityProvider, MediaIntegrityErrorWrapper> callback) {
             CallKey key = new CallKey(cloudProjectNumber, apiStatus);
             Queue<Object> responseQueue = mResponses.get(key);
             mCallCounts.compute(key, (callKey, counts) -> counts == null ? 1 : counts + 1);
@@ -1133,8 +1153,8 @@ public class AwMediaIntegrityApiTest extends AwParameterizedTest {
                     callback.onResult(provider);
                     return;
                 }
-                if (response instanceof Integer errorCode) {
-                    callback.onError(errorCode);
+                if (response instanceof MediaIntegrityErrorWrapper error) {
+                    callback.onError(error);
                     return;
                 }
             }
