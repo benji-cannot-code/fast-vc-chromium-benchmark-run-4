edb83939db08f0eb7f694f3728a10643acba1b71
S. Ganesh <ganesh@chromium.org>
2024-08-22 17:50:18 +0000

    winhttp: add unit test for gzip compression
    Bug: 343943622
    Change-Id: I84715539df981d767d3d8ae5cbdfbe8db729a383
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5806051
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345533}

--- components/winhttp/network_fetcher_unittest.cc
+++ components/winhttp/network_fetcher_unittest.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/winhttp/network_fetcher.h"
 
+#include <memory>
+#include <string>
+
 #include "base/files/file.h"
 #include "base/functional/bind.h"
 #include "base/memory/scoped_refptr.h"
@@ -12,8 +15,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/bind.h"
 #include "base/test/task_environment.h"
 #include "components/winhttp/scoped_hinternet.h"
+#include "net/test/embedded_test_server/embedded_test_server.h"
+#include "net/test/embedded_test_server/http_request.h"
+#include "net/test/embedded_test_server/http_response.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/zlib/google/compression_utils.h"
 #include "url/gurl.h"
 
 namespace winhttp {
@@ -80,6 +87,54 @@ TEST(WinHttpNetworkFetcher, NullSession) {
               ::testing::AnyOf(MAKE_HRESULT(SEVERITY_ERROR, FACILITY_WIN32,
                                             ERROR_WINHTTP_NOT_INITIALIZED),
                                E_HANDLE));
+}
+
+TEST(WinHttpNetworkFetcher, GZip) {
+  static const std::string kResponse = "hello response";
+  net::EmbeddedTestServer test_server;
+  test_server.RegisterRequestHandler(base::BindRepeating(
+      [](const net::test_server::HttpRequest& request)
+          -> std::unique_ptr<net::test_server::HttpResponse> {
+        auto response = std::make_unique<net::test_server::BasicHttpResponse>();
+        if (!request.headers.contains("Accept-Encoding") ||
+            request.headers.at("Accept-Encoding").find("gzip") ==
+                std::string::npos) {
+          ADD_FAILURE() << "gzip `Accept-Encoding` not found in request";
+          response->set_code(net::HTTP_INTERNAL_SERVER_ERROR);
+          return response;
+        }
+
+        std::string compressed_body;
+        if (!compression::GzipCompress(kResponse, &compressed_body)) {
+          ADD_FAILURE() << "gzip compression failed";
+          response->set_code(net::HTTP_INTERNAL_SERVER_ERROR);
+          return response;
+        }
+        response->AddCustomHeader("Content-Encoding", "gzip");
+        response->set_content(compressed_body);
+        return response;
+      }));
+  ASSERT_TRUE(test_server.Start());
+
+  base::test::TaskEnvironment environment;
+  base::RunLoop run_loop;
+  auto network_fetcher = base::MakeRefCounted<NetworkFetcher>(
+      base::MakeRefCounted<SharedHInternet>(CreateSessionHandle(
+          L"WinHttpNetworkFetcherTest.GZip", WINHTTP_ACCESS_TYPE_NO_PROXY)),
+      base::MakeRefCounted<ProxyConfiguration>());
+  network_fetcher->PostRequest(
+      /*url=*/test_server.GetURL("/"),
+      /*content_type=*/"text/plain",
+      /*post_data=*/"hello request",
+      /*post_additional_headers=*/{},
+      /*fetch_started_callback=*/base::DoNothing(),
+      /*fetch_progress_callback=*/base::DoNothing(),
+      /*fetch_complete_callback=*/
+      base::BindLambdaForTesting(
+          [&run_loop](int /*response_code*/) { run_loop.Quit(); }));
+  run_loop.Run();
+  ASSERT_HRESULT_SUCCEEDED(network_fetcher->GetNetError());
+  ASSERT_EQ(network_fetcher->GetResponseBody(), kResponse);
 }
 
 }  // namespace winhttp
