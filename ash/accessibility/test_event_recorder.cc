3aa71e0f4844f14b6a3846fa54221ee9ed15db31
Anastasia Helfinstein <anastasi@google.com>
2024-08-22 18:50:29 +0000

    [a11y] Share TestEventRecorder
    As a follow-up to crrev.com/c/5801286, create a shared class to capture
    events for the purposes of testing.
    
    AX-Relnotes: n/a.
    Bug: b/324272616
    Change-Id: I54766c9cac2b0f386b83ae0440283cbc8ce91fb6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804620
    Commit-Queue: Anastasia Helfinstein <anastasi@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Auto-Submit: Anastasia Helfinstein <anastasi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345569}

---
+++ ash/accessibility/test_event_recorder.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/accessibility/test_event_recorder.h"
+
+namespace ash {
+
+TestEventRecorder::TestEventRecorder() = default;
+TestEventRecorder::~TestEventRecorder() = default;
+
+ui::EventDispatchDetails TestEventRecorder::RewriteEvent(
+    const ui::Event& event,
+    const Continuation continuation) {
+  events_.push_back(event.Clone());
+  return SendEvent(continuation, &event);
+}
+
+}  // namespace ash
