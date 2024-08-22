f6036a495fa943fee891dad8f757a607f7709699
Stephen Nusko <nuskos@chromium.org>
2024-08-22 19:33:24 +0000

    Ensure bounds checking of ident/invert arrays by using std::array
    Accessing a static array (as identity and invert were) isn't bounds
    checked and can cause memory safety issues. We're working on
    automatically switching such occurrences to use spans or std::arrays
    (which do bounds checking in Chromium code). Our automatic clang tooling
    struggles when multiple static arrays are declared on the same line.
    
    Switching the static array to an std::array prevents the plugin
    encountering such occurrences (which are rare) and ensures all accesses
    are bounds checked. For this file this allows us to remove the pragma at
    the top of the file.
    
    Bug: 360641491
    Change-Id: Ib506dffa0fff30332dfa3360e695698604fcb07b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803044
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Auto-Submit: Stephen Nusko <nuskos@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345588}

--- third_party/blink/renderer/platform/graphics/dark_mode_color_filter.cc
+++ third_party/blink/renderer/platform/graphics/dark_mode_color_filter.cc
@@ -3,12 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/graphics/dark_mode_color_filter.h"
+
+#include <array>
 
 #include "base/check.h"
 #include "base/notreached.h"
@@ -167,13 +164,13 @@ std::unique_ptr<DarkModeColorFilter> DarkModeColorFilter::FromSettings(
     const DarkModeSettings& settings) {
   switch (settings.mode) {
     case DarkModeInversionAlgorithm::kSimpleInvertForTesting:
-      uint8_t identity[256], invert[256];
+      std::array<uint8_t, 256> identity, invert;
       for (int i = 0; i < 256; ++i) {
         identity[i] = i;
         invert[i] = 255 - i;
       }
-      return ColorFilterWrapper::Create(
-          cc::ColorFilter::MakeTableARGB(identity, invert, invert, invert));
+      return ColorFilterWrapper::Create(cc::ColorFilter::MakeTableARGB(
+          identity.data(), invert.data(), invert.data(), invert.data()));
 
     case DarkModeInversionAlgorithm::kInvertBrightness:
       return ColorFilterWrapper::Create(
