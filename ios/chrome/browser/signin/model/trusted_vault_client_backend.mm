9346acc023b734af681ecbbfcdbf4d06bfb3100f
Arthur Milchior <arthurmilchior@chromium.org>
2024-08-22 15:10:40 +0000

    [iOS][Trusted Vault] SecurityDomain types become stronger
    The linked bug was caused by a string having the wrong value.
    Using an enum instead of a string ensure that the right type is
    passed. Transforming the type into a string can now be done directly
    before the string is sent to the SSO API. This will make it easier to
    ensure that we only need the Security Domain Name, and that no part of
    the code actually needed the security domain path.
    
    The string parameter will be removed in a future commit.
    
    Note that BindOnce does not support overloading function, thus it was
    necessary to rename some of the functions. Hence the new suffix
    "WithTwoSecurityDomainParameter".
    
    It should also be noted that, as far as observer goes, the string was
    simply a key to a map. Ensuring that all observers gets only notified
    about the event that concerns the security domain they are listening
    to.
    So, having the key type be an enum is as valid as using a string,
    and cause less concern for path/name ambiguity thanks to typing.
    
    
    This is part of the chain of CL:
    * crrev.com/c/5803084
    > crrev.com/c/5804145
    * crrev.com/i/7609043
    * crrev.com/c/5803588
    * crrev.com/i/7608423
    * crrev.com/c/5804146
    
    chain
    
    Low-Coverage-Reason: OTHER The code will be removed in a CL in this
    Bug: 361196003
    Change-Id: I408d68841a6db280da26bcb2dccae6d8c98b957c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804145
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345434}

--- ios/chrome/browser/signin/model/trusted_vault_client_backend.mm
+++ ios/chrome/browser/signin/model/trusted_vault_client_backend.mm
@@ -5,22 +5,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/signin/model/trusted_vault_client_backend.h"
 
+#import "base/functional/callback.h"
+
 TrustedVaultClientBackend::TrustedVaultClientBackend() = default;
 
 TrustedVaultClientBackend::~TrustedVaultClientBackend() = default;
 
 void TrustedVaultClientBackend::AddObserver(
     TrustedVaultClientBackend::Observer* observer,
-    const std::string& security_domain_path) {
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id) {
   observer_lists_per_security_domain_path_[security_domain_path].AddObserver(
       observer);
 }
 
 void TrustedVaultClientBackend::RemoveObserver(
     Observer* observer,
-    const std::string& security_domain_path) {
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id) {
   observer_lists_per_security_domain_path_[security_domain_path].RemoveObserver(
       observer);
+}
+
+void TrustedVaultClientBackend::NotifyKeysChangedWithTwoSecurityDomainParameter(
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id) {
+  NotifyKeysChanged(security_domain_path);
 }
 
 void TrustedVaultClientBackend::NotifyKeysChanged(
@@ -34,6 +44,12 @@ void TrustedVaultClientBackend::NotifyKeysChanged(
   }
 }
 
+void TrustedVaultClientBackend::
+    NotifyRecoverabilityChangedWithTwoSecurityDomainParameter(
+        const std::string& security_domain_path,
+        trusted_vault::SecurityDomainId security_domain_id) {
+  TrustedVaultClientBackend::NotifyRecoverabilityChanged(security_domain_path);
+}
 void TrustedVaultClientBackend::NotifyRecoverabilityChanged(
     const std::string& security_domain_path) {
   auto it = observer_lists_per_security_domain_path_.find(security_domain_path);
@@ -43,4 +59,105 @@ void TrustedVaultClientBackend::NotifyRecoverabilityChanged(
   for (Observer& observer : it->second) {
     observer.OnTrustedVaultRecoverabilityChanged();
   }
+}
+
+#pragma mark - Deprecated methods, use for migration only.
+
+void TrustedVaultClientBackend::FetchKeys(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    KeyFetchedCallback completion) {
+  NOTREACHED();
+}
+
+void TrustedVaultClientBackend::MarkLocalKeysAsStale(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    base::OnceClosure completion) {
+  NOTREACHED();
+}
+
+void TrustedVaultClientBackend::GetDegradedRecoverabilityStatus(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    base::OnceCallback<void(bool)> completion) {
+  NOTREACHED();
+}
+
+TrustedVaultClientBackend::CancelDialogCallback
+TrustedVaultClientBackend::Reauthentication(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    UIViewController* presenting_view_controller,
+    CompletionBlock completion) {
+  NOTREACHED();
+}
+
+TrustedVaultClientBackend::CancelDialogCallback
+TrustedVaultClientBackend::FixDegradedRecoverability(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    UIViewController* presenting_view_controller,
+    CompletionBlock completion) {
+  NOTREACHED();
+}
+void TrustedVaultClientBackend::ClearLocalData(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    base::OnceCallback<void(bool)> completion) {
+  NOTREACHED();
+}
+
+#pragma mark - Virtual functions
+// Those functions call the deprecated function. They will be purely virtual
+// after the migraiton is done.
+void TrustedVaultClientBackend::FetchKeys(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    KeyFetchedCallback completion) {
+  FetchKeys(identity, security_domain_path, std::move(completion));
+}
+void TrustedVaultClientBackend::MarkLocalKeysAsStale(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    base::OnceClosure completion) {
+  MarkLocalKeysAsStale(identity, security_domain_path, std::move(completion));
+}
+void TrustedVaultClientBackend::GetDegradedRecoverabilityStatus(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    base::OnceCallback<void(bool)> completion) {
+  GetDegradedRecoverabilityStatus(identity, security_domain_path,
+                                  std::move(completion));
+}
+TrustedVaultClientBackend::CancelDialogCallback
+TrustedVaultClientBackend::Reauthentication(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    UIViewController* presenting_view_controller,
+    CompletionBlock completion) {
+  return Reauthentication(identity, security_domain_path,
+                          presenting_view_controller, std::move(completion));
+}
+TrustedVaultClientBackend::CancelDialogCallback
+TrustedVaultClientBackend::FixDegradedRecoverability(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    UIViewController* presenting_view_controller,
+    CompletionBlock completion) {
+  return FixDegradedRecoverability(identity, security_domain_path,
+                                   presenting_view_controller,
+                                   std::move(completion));
+}
+void TrustedVaultClientBackend::ClearLocalData(
+    id<SystemIdentity> identity,
+    const std::string& security_domain_path,
+    trusted_vault::SecurityDomainId security_domain_id,
+    base::OnceCallback<void(bool)> completion) {
+  return ClearLocalData(identity, security_domain_path, std::move(completion));
 }
