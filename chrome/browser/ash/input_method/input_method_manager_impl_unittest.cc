d1ec6a5a1d22fd42a32231d9d463fd1a0e9274ec
Georg Neis <neis@chromium.org>
2024-08-22 04:12:23 +0000

    ash: Create //chrome/browser/ui/ash/input_method
    Bug: b:358249580
    Change-Id: Id84203bae9b639e9c4281e861b485fa3af54f942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791074
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345244}

--- chrome/browser/ash/input_method/input_method_manager_impl_unittest.cc
+++ chrome/browser/ash/input_method/input_method_manager_impl_unittest.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/input_method/mock_input_method_engine.h"
 #include "chrome/browser/ash/login/users/fake_chrome_user_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/ash/ime_controller_client_impl.h"
+#include "chrome/browser/ui/ash/input_method/ime_controller_client_impl.h"
 #include "chrome/browser/ui/ash/keyboard/chrome_keyboard_controller_client_test_helper.h"
 #include "chrome/browser/ui/ash/test_ime_controller.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
--- chrome/browser/ash/login/lock_screen_utils.cc
+++ chrome/browser/ash/login/lock_screen_utils.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/ash/ime_controller_client_impl.h"
+#include "chrome/browser/ui/ash/input_method/ime_controller_client_impl.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/ash/components/settings/cros_settings.h"
 #include "components/account_id/account_id.h"
