c17f6460df581714655ef7ce55c18879809dd3c5
Georg Neis <neis@chromium.org>
2024-08-22 04:00:58 +0000

    ash: Create //chrome/browser/ui/ash/media_client
    ... and move the media_client_impl* files there.
    
    Bug: b:358249306
    Change-Id: I9dc74514b34a917ae14bdadb58a34551f288cc86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782803
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345236}

--- chrome/browser/extensions/extension_keybinding_registry.cc
+++ chrome/browser/extensions/extension_keybinding_registry.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/mojom/context_type.mojom.h"
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
-#include "chrome/browser/ui/ash/media_client_impl.h"
+#include "chrome/browser/ui/ash/media_client/media_client_impl.h"
 #endif
 
 namespace {
--- chrome/browser/ui/ash/media_client/media_client_impl.h
+++ chrome/browser/ui/ash/media_client/media_client_impl.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_IMPL_H_
-#define CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_IMPL_H_
+#ifndef CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_MEDIA_CLIENT_IMPL_H_
+#define CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_MEDIA_CLIENT_IMPL_H_
 
 #include "ash/public/cpp/media_client.h"
 #include "ash/public/cpp/media_controller.h"
@@ -211,4 +211,4 @@ class MediaClientImpl : public ash::MediaClient,
   base::WeakPtrFactory<MediaClientImpl> weak_ptr_factory_{this};
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_IMPL_H_
+#endif  // CHROME_BROWSER_UI_ASH_MEDIA_CLIENT_MEDIA_CLIENT_IMPL_H_
