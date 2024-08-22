eaae0e1247f8ad0d55d5122fa6559fe9ede79c27
Hongyu Long <hongyulong@chromium.org>
2024-08-22 23:04:43 +0000

    Fix the tooltip position incorrect after scrolling
    This bug fixes the issue that when tabbing through the focus panel to
    make a view show in the visible rect of the scroll view, the tooltip use
    the old position instead of the new position after scrolling.
    
    Bug: b/358153219
    Test: Manual
    Change-Id: I067236a292f057897d26bc19b78ad449f8cbd867
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5801476
    Commit-Queue: Hongyu Long <hongyulong@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345717}

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -2626,9 +2626,6 @@ void View::OnBlur() {}
 void View::Focus() {
   OnFocus();
 
-  // TODO(crbug.com/40285437) - Get this working on Lacros as well.
-  UpdateTooltipForFocus();
-
   // TODO(pbos): Investigate if parts of this can run unconditionally.
   if (!suppress_default_focus_handling_) {
     // Clear the native focus. This ensures that no visible native view has the
@@ -2657,6 +2654,11 @@ void View::Focus() {
   } else {
     ScrollViewToVisible();
   }
+
+  // Update tooltip after scrolling view to place tooltip according to the new
+  // position.
+  // TODO(crbug.com/40285437) - Get this working on Lacros as well.
+  UpdateTooltipForFocus();
 
   for (ViewObserver& observer : observers_) {
     observer.OnViewFocused(this);
