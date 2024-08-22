b193483479efafa4d55b8abba4627543642c5796
Brandon Wylie <wylieb@chromium.org>
2024-08-22 18:27:11 +0000

    [declutter] Separate metrics for eligibility/action for archive/delete
    This will make the metrics easier to understand in the dashboards.
    
    
    OBSOLETE_HISTOGRAM[Tabs.TabEligibleForArchive.AfterNDays]=Replaced by Tabs.TabArchiveEligibilityCheck.AfterNDays
    OBSOLETE_HISTOGRAM[Tabs.TabEligibleForDeletion.AfterNDays]=Replaced by Tabs.TabAutoDeleteEligibilityCheck.AfterNDays
    
    Bug: 361420832
    Change-Id: I307edd69d4a1c49e480ec66db485fdb4c2f7e3b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805881
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345558}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
@@ -243,8 +243,10 @@ public class TabArchiverTest {
         assertEquals(0, mArchivedTabModel.getCount());
         HistogramWatcher watcher =
                 HistogramWatcher.newBuilder()
-                        .expectIntRecords("Tabs.TabEligibleForArchive.AfterNDays", 0, 0)
+                        .expectIntRecords("Tabs.TabArchiveEligibilityCheck.AfterNDays", 0, 0)
+                        .expectIntRecords("Tabs.TabArchived.AfterNDays", 0, 0)
                         .build();
+
         // Send an event, similar to how TabWindowManager would.
         runOnUiThreadBlocking(
                 () ->
@@ -292,8 +294,10 @@ public class TabArchiverTest {
 
         Tab archivedTab = mArchivedTabModel.getTabAt(0);
         HistogramWatcher watcher =
-                HistogramWatcher.newSingleRecordWatcher(
-                        "Tabs.TabEligibleForAutoDeletion.AfterNDays", 0);
+                HistogramWatcher.newBuilder()
+                        .expectIntRecords("Tabs.TabAutoDeleteEligibilityCheck.AfterNDays", 0)
+                        .expectIntRecords("Tabs.TabAutoDeleted.AfterNDays", 0)
+                        .build();
         runOnUiThreadBlocking(
                 () -> {
                     mTabArchiveSettings.setAutoDeleteEnabled(true);
@@ -301,10 +305,9 @@ public class TabArchiverTest {
                     mTabArchiver.deleteEligibleArchivedTabs();
                 });
 
-        assertEquals(1, mRegularTabModel.getCount());
         CriteriaHelper.pollInstrumentationThread(() -> mArchivedTabModel.getCount() == 0);
         CriteriaHelper.pollInstrumentationThread(() -> archivedTab.isDestroyed());
-        watcher.assertExpected();
+        assertEquals(1, mRegularTabModel.getCount());
 
         runOnUiThreadBlocking(
                 () -> {
@@ -316,6 +319,7 @@ public class TabArchiverTest {
                             });
                 });
         callbackHelper.waitForNext();
+        watcher.assertExpected();
     }
 
     @Test
