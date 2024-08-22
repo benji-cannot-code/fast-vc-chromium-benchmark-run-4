34275b0ed57f77a53c01cc5dd4b9b686d1b8f998
Jack Yammine <jyammine@google.com>
2024-08-22 14:58:49 +0000

    Enable TrustedTypes tests for chrome://search-engine-choice
    Add the command line arguments required to render the
    chrome://search-engine-choice page.
    
    Bug: b:355681485
    Change-Id: I7704bdc87492e4b8550c03580e65caab06d68f05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5806897
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345428}

--- chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
+++ chrome/browser/ui/webui/chrome_url_data_manager_browsertest.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/welcome/helpers.h"
 #include "chrome/common/chrome_features.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/enterprise/browser/controller/fake_browser_dm_token_storage.h"
@@ -27,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/password_manager/core/common/password_manager_features.h"
 #include "components/search/ntp_features.h"
+#include "components/search_engines/search_engines_switches.h"
 #include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/url_data_source.h"
@@ -256,15 +258,27 @@ class ChromeURLDataManagerWebUITrustedTypesTest
   }
 
  protected:
-#if BUILDFLAG(IS_CHROMEOS_ASH)
   void SetUpCommandLine(base::CommandLine* command_line) override {
+    InProcessBrowserTest::SetUpCommandLine(command_line);
+    if (GetParam() ==
+        std::string_view(chrome::kChromeUISearchEngineChoiceURL)) {
+      // Command line arguments needed to render chrome://search-engine-choice.
+      command_line->AppendSwitchASCII(switches::kSearchEngineChoiceCountry,
+                                      "BE");
+      command_line->AppendSwitch(switches::kForceSearchEngineChoiceScreen);
+      command_line->AppendSwitch(
+          switches::kIgnoreNoFirstRunForSearchEngineChoiceScreen);
+    }
+#if BUILDFLAG(IS_CHROMEOS_ASH)
     command_line->AppendSwitchASCII(ash::switches::kSamlPasswordChangeUrl,
                                     "http://password-change.example");
     if (GetParam() == std::string_view("chrome://shimless-rma")) {
       command_line->AppendSwitchASCII(ash::switches::kLaunchRma, "");
     }
+#endif
   }
 
+#if BUILDFLAG(IS_CHROMEOS_ASH)
   void SetUpOnMainThread() override {
     browser()->profile()->GetPrefs()->SetBoolean(
         ash::prefs::kSamlInSessionPasswordChangeEnabled, true);
@@ -280,7 +294,7 @@ class ChromeURLDataManagerWebUITrustedTypesTest
     service->RegisterProvider(std::move(fake_provider));
 #endif  // BUILDFLAG(IS_CHROMEOS)
   }
-#endif
+#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 
  private:
   base::test::ScopedFeatureList feature_list_;
@@ -370,6 +384,7 @@ static constexpr const char* const kChromeUrls[] = {
     "chrome://read-later.top-chrome",
     "chrome://reset-password",
     "chrome://safe-browsing",
+    "chrome://search-engine-choice",
     "chrome://serviceworker-internals",
     "chrome://segmentation-internals",
     "chrome://settings",
