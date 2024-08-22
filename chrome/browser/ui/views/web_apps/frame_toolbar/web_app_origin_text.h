f0af5d167325a75caa657395ff5570ec2c9a9900
Divyansh Mangal <dmangal@microsoft.com>
2024-08-22 20:09:34 +0000

    [views-ax] Migrates kName in chrome/browser/ui/views
    This CL migrates the kName attribute in views
    whenever its value changes, rather than querying the value
    and computing it only when needed.
    
    This CL also contains new unit tests for checking that the accessible
    name is indeed correctly getting updated.
    
    The migration is applied to following views:
    1. desktop_media_list_view
    2. desktop_media_source_view.cc
    3. extension_install_dialog_view
    4. web_app_origin_text
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I24fd5f2fdada3ddeeef0d7d13c85c139f0e1c8df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797862
    Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    Commit-Queue: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345608}

--- chrome/browser/ui/views/web_apps/frame_toolbar/web_app_origin_text.h
+++ chrome/browser/ui/views/web_apps/frame_toolbar/web_app_origin_text.h
@@ -45,9 +45,6 @@ class WebAppOriginText : public views::View,
   // Fades the text in and out.
   void StartFadeAnimation();
 
-  // views::View:
-  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
-
   // ui::LayerAnimationObserver:
   void OnLayerAnimationStarted(ui::LayerAnimationSequence* sequence) override {}
   void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override;
@@ -58,6 +55,8 @@ class WebAppOriginText : public views::View,
   const std::u16string& GetLabelTextForTesting();
 
  private:
+  friend class WebAppFrameToolbarTestHelper;
+
   // TabStripModelObserver:
   void OnTabStripModelChanged(
       TabStripModel* tab_strip_model,
@@ -66,6 +65,8 @@ class WebAppOriginText : public views::View,
 
   // content::WebContentsObserver:
   void DidFinishNavigation(content::NavigationHandle* handle) override;
+
+  void UpdateAccessibleName();
 
   // origin_text_ is populated by ReadyToCommitNavigation.
   std::u16string origin_text_;
@@ -76,6 +77,8 @@ class WebAppOriginText : public views::View,
 
   // Owned by the views hierarchy.
   raw_ptr<views::Label, DanglingUntriaged> label_ = nullptr;
+
+  base::CallbackListSubscription label_text_changed_callback_;
 
   base::WeakPtrFactory<WebAppOriginText> weak_factory_{this};
 };
