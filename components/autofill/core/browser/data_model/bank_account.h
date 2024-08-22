5db82d30bb5c51e7847f61df7122e77e3de788d3
Christoph Schwering <schwering@google.com>
2024-08-22 08:20:37 +0000

    [Autofill] Do not wrap BankAccount in std::unique_ptr
    This CL stores BankAccounts as std::vector<BankAccount> instead of
    std::vector<std::unique_ptr<BankAccount>>. This is reasonable because
    BankAccount is a value type and bank accounts are never null.
    
    As a result, the CL uses AreAnyItemsDifferent() instead of the overload
    for std::vector<std::unique_ptr<T>>.
    
    As a result, the CL removes PaymentsInstrument::Compare() and
    BankAccount::Compare(), which removes a lot of code.
    
    Bug: 40278516
    Change-Id: I6335bd451dc57968f76c743793e67e649acd193f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802937
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1345291}

--- components/autofill/core/browser/data_model/bank_account.h
+++ components/autofill/core/browser/data_model/bank_account.h
@@ -42,9 +42,9 @@ class BankAccount {
               AccountType account_type);
   ~BankAccount();
 
+  friend std::strong_ordering operator<=>(const BankAccount&,
+                                          const BankAccount&);
   friend bool operator==(const BankAccount&, const BankAccount&);
-
-  int Compare(const BankAccount& other) const;
 
   const PaymentInstrument& payment_instrument() const {
     return payment_instrument_;
