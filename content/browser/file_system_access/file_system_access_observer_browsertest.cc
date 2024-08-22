8ff74342b6bb5cb925f5b0f6ea2da217381b7da7
Christine Hollingsworth <christinesm@chromium.org>
2024-08-22 20:33:10 +0000

    [FSA] Mac - Disable ReceivesFileUpdatesAfterReturningFromBFCache browsertest
    Disabling a flaky test on Mac. This flake is similar to the other
    existing flaky browsertests being tracked by b/360153904, and the
    followup resolution will be monitored there.
    
    Bug: 360758384
    Change-Id: If56faa0aa813778cdda8057d5f53d68251715f00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5806933
    Reviewed-by: Nathan Memmott <memmott@chromium.org>
    Commit-Queue: Christine Hollingsworth <christinesm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345624}

--- content/browser/file_system_access/file_system_access_observer_browsertest.cc
+++ content/browser/file_system_access/file_system_access_observer_browsertest.cc
@@ -27,10 +27,11 @@ namespace content {
 
 namespace {
 
-#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_FUCHSIA)
+#if !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) && !BUILDFLAG(IS_FUCHSIA) && \
+    !BUILDFLAG(IS_MAC)
 constexpr int kBFCacheTestTimeoutMs = 3000;
 #endif  // !BUILDFLAG(IS_ANDROID) && !BUILDFLAG(IS_IOS) &&
-        // !BUILDFLAG(IS_FUCHSIA)
+        // !BUILDFLAG(IS_FUCHSIA) && !BUILDFLAG(IS_MAC)
 
 enum class TestFileSystemType {
   kBucket,
@@ -1210,6 +1211,10 @@ class FileSystemAccessObserverWithBFCacheBrowserTest
   base::test::ScopedFeatureList feature_list_for_back_forward_cache_;
 };
 
+// TODO(b/360153904): This test is flaky on Mac, likely as a result of FSEvents
+// reporting events later than expected, on occasion. Re-enable once flake is
+// resolved.
+#if !BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(FileSystemAccessObserverWithBFCacheBrowserTest,
                        ReceivesFileUpdatesAfterReturningFromBFCache) {
   base::FilePath file_path = CreateFileToBePicked();
@@ -1296,6 +1301,7 @@ IN_PROC_BROWSER_TEST_F(FileSystemAccessObserverWithBFCacheBrowserTest,
                 .ExtractInt(),
             1);
 }
+#endif  // !BUILDFLAG(IS_MAC)
 
 IN_PROC_BROWSER_TEST_F(FileSystemAccessObserverWithBFCacheBrowserTest,
                        NotifyOnReturnFromBFCacheWhenFileUpdates) {
