8aa1a54b6760c3a6a7a6ce2df108005718ef6226
Florian Leimgruber <fleimgruber@google.com>
2024-08-22 12:16:54 +0000

    Fix dangling pointer in forwarding_data_type_controller_delegate.h
    ForwardingDataTypeControllerDelegate::other_ becomes dangling during the
    destruction of the derived DataTypeControllers
    ArcPackageSyncDataTypeControllers and
    SyncableServiceBasedDataTypeControllers in the same way:
    
    - The derived controllers own a DataTypeControllerDelegate that is
      destroyed together with the derived controller [1, 2]. In [2], the
      ownership is indirect through the bridge_.
    - During construction, the derived controllers pass
      ForwardingDataTypeControllerDelegates referencing their
      DataTypeControllerDelegate to the base class through
      InitDataTypeController() [3, 4]. These forwarding
      controllers are stored by the base class in delegate_map_ [5].
    - Since derived classes are destroyed before base classes in C++,
      their DataTypeControllerDelegate is destroyed before delegate_map_.
      As a result, when the DataTypeControllerDelegate is destroyed,
      the forwarding controllers in delegate_map_ become dangling.
    
    This CL fixes the problem by clearing delegate_map_ in the destructors
    of the derived controllers.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:components/sync/service/syncable_service_based_data_type_controller.h;l=56;drc=41d2a6c29f4b0a3ed5321841ddf8b3d715f02d58
    [2] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/app_list/arc/arc_package_sync_data_type_controller.cc;l=45-46;drc=31a57c5eef28c0b7b6098ba7549efc39726d3a30
    [3] https://source.chromium.org/chromium/chromium/src/+/main:components/sync/service/syncable_service_based_data_type_controller.cc;l=133-148;drc=41d2a6c29f4b0a3ed5321841ddf8b3d715f02d58
    [4] https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/ash/app_list/arc/arc_package_sync_data_type_controller.cc;l=51-54;drc=31a57c5eef28c0b7b6098ba7549efc39726d3a30
    [5] https://source.chromium.org/chromium/chromium/src/+/main:components/sync/service/data_type_controller.cc;l=86-90;drc=82dff63dbf9db05e9274e11d9128af7b9f51ceaa
    
    Fixed: 41485277
    Change-Id: I54fdf7cb273bf7d8e9b567ece0f6951f5cde4153
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797186
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345356}

--- components/sync/service/data_type_controller.cc
+++ components/sync/service/data_type_controller.cc
@@ -372,6 +372,11 @@ bool DataTypeController::CalledOnValidThread() const {
   return sequence_checker_.CalledOnValidSequence();
 }
 
+void DataTypeController::ClearDelegateMap() {
+  delegate_ = nullptr;
+  delegate_map_.clear();
+}
+
 void DataTypeController::RecordStartFailure() const {
   DCHECK(CalledOnValidThread());
   UMA_HISTOGRAM_ENUMERATION("Sync.DataTypeStartFailures2",
