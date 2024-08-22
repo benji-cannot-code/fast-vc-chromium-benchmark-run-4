3986b6b95e299f971f9aaf03027055f8c077689c
Mark Schillaci <mschillaci@google.com>
2024-08-22 23:48:40 +0000

    Add more verbose errors for accessibility crashes
    AX-Relnotes: N/A
    Bug: 360919317
    Change-Id: Icbc5d23b74783814c2280acf5c78c5cbdf9b6c25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807382
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345730}

--- ui/accessibility/ax_tree.cc
+++ ui/accessibility/ax_tree.cc
@@ -1273,7 +1273,7 @@ bool AXTree::Unserialize(const AXTreeUpdate& update) {
     if (!root_) {
       ACCESSIBILITY_TREE_UNSERIALIZE_ERROR_HISTOGRAM(
           AXTreeUnserializeError::kNoRoot);
-      RecordError(update_state, "Tree has no root.");
+      RecordError(update_state, "Tree has no root.", true);
       return false;
     }
 
