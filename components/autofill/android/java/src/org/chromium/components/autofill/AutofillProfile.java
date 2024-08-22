c0e8ae06b6e26fb84d19951a41a6daf441e5f1f4
Mohamed Heikal <mheikal@chromium.org>
2024-08-22 15:58:56 +0000

    Migrate AutofillProfile to use @JniType
    Reduce boilerplace by converting java types to c++ types automatically.
    
    Change-Id: I00f8f00fafd28f74cdecef34c37af86103086dde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5739154
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345455}

--- components/autofill/android/java/src/org/chromium/components/autofill/AutofillProfile.java
+++ components/autofill/android/java/src/org/chromium/components/autofill/AutofillProfile.java
@@ -10,6 +10,7 @@ import androidx.annotation.VisibleForTesting;
 
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
+import org.jni_zero.JniType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -225,7 +226,10 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    private AutofillProfile(String guid, @Source int source, String languageCode) {
+    private AutofillProfile(
+            @JniType("std::string") String guid,
+            @Source int source,
+            @JniType("std::string") String languageCode) {
         mGUID = guid;
         mSource = source;
         mLanguageCode = languageCode;
@@ -273,17 +277,17 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    private int[] getFieldTypes() {
+    private @JniType("std::vector<int32_t>") int[] getFieldTypes() {
         return mFields.keySet().stream().mapToInt(i -> i).toArray();
     }
 
     @CalledByNative
-    public String getGUID() {
+    public @JniType("std::string") String getGUID() {
         return mGUID;
     }
 
     @CalledByNative
-    public @Source int getSource() {
+    public @JniType("AutofillProfile::Source") @Source int getSource() {
         return mSource;
     }
 
@@ -292,7 +296,7 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    public String getInfo(@FieldType int fieldType) {
+    public @JniType("std::u16string") String getInfo(@FieldType int fieldType) {
         if (!mFields.containsKey(fieldType)) {
             return "";
         }
@@ -300,7 +304,8 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    public @VerificationStatus int getInfoStatus(@FieldType int fieldType) {
+    public @JniType("VerificationStatus") @VerificationStatus int getInfoStatus(
+            @FieldType int fieldType) {
         if (!mFields.containsKey(fieldType)) {
             return VerificationStatus.NO_STATUS;
         }
@@ -383,7 +388,7 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    public String getCountryCode() {
+    public @JniType("std::string") String getCountryCode() {
         return getInfo(FieldType.ADDRESS_HOME_COUNTRY);
     }
 
@@ -408,7 +413,7 @@ public class AutofillProfile {
     }
 
     @CalledByNative
-    public String getLanguageCode() {
+    public @JniType("std::string") String getLanguageCode() {
         return mLanguageCode;
     }
 
@@ -426,7 +431,9 @@ public class AutofillProfile {
 
     @CalledByNative
     public void setInfo(
-            @FieldType int fieldType, @Nullable String value, @VerificationStatus int status) {
+            @FieldType int fieldType,
+            @JniType("std::u16string") @Nullable String value,
+            @VerificationStatus int status) {
         value = value == null ? "" : value;
         mFields.put(fieldType, new ValueWithStatus(value, status));
     }
