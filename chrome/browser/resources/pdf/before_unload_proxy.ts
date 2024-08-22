beaac1d96e18458c78ff706b0e873ab85c4e115d
Andy Phan <andyphan@chromium.org>
2024-08-22 23:48:27 +0000

    [PDF Ink Signatures] Create and use BeforeUnloadProxy
    Create and use a new BeforeUnloadProxy interface to make handling
    beforeunload events in the PDF easier to test.
    
    Bug: 353942911
    Change-Id: Ic0b391a7ff3baaef24e82897c4bb5a8c71df30dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798865
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345729}

---
+++ chrome/browser/resources/pdf/before_unload_proxy.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Helper object to prevent the beforeunload event's default action. This proxy
+// is useful for testing.
+export interface BeforeUnloadProxy {
+  preventDefault(event: BeforeUnloadEvent): void;
+}
+
+export class BeforeUnloadProxyImpl implements BeforeUnloadProxy {
+  preventDefault(event: BeforeUnloadEvent) {
+    event.preventDefault();
+  }
+
+  static getInstance(): BeforeUnloadProxy {
+    return instance || (instance = new BeforeUnloadProxyImpl());
+  }
+
+  static setInstance(obj: BeforeUnloadProxy): void {
+    instance = obj;
+  }
+}
+
+let instance: BeforeUnloadProxy|null = null;
