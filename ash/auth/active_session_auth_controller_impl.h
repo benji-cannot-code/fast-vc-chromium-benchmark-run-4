76bb5b033eab4ac7280cc0670fe479d9d0df8495
Zi Lin <lziest@google.com>
2024-08-22 18:09:00 +0000

    active session auth controller logic optimization.
    1. Eliminate ListAuthFactors() call through auth factor editor, because
       StartAuthSession() already returns usable auth factors. This paves
       the way to see legacy fingerprint listed as an auth factor.
       Otherwise, ListAuthFactors() doesn't list legacy fingerprints.
    
    2. Use the signal observer to process AuthFactorStatusUpdate. In
       particular, pin status update is processed and pin input view is
       disabled when the pin is locked out.
    
    BUG=b:341733466
    TEST=unit tests and CUJ tests in a VM.
    
    Change-Id: Id6982b56450954a9d2413919860b2f674ddaf7bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5794222
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Istvan Nagy <iscsi@google.com>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345545}

--- ash/auth/active_session_auth_controller_impl.h
+++ ash/auth/active_session_auth_controller_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_AUTH_ACTIVE_SESSION_AUTH_CONTROLLER_IMPL_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "ash/ash_export.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/in_session_auth_token_provider.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chromeos/ash/components/dbus/userdataauth/userdataauth_client.h"
 #include "chromeos/ash/components/login/auth/auth_factor_editor.h"
 #include "chromeos/ash/components/login/auth/auth_performer.h"
 #include "chromeos/ash/components/osauth/public/common_types.h"
@@ -36,6 +38,7 @@ namespace ash {
 class ASH_EXPORT ActiveSessionAuthControllerImpl
     : public ActiveSessionAuthController,
       public ActiveSessionAuthView::Observer,
+      public UserDataAuthClient::AuthFactorStatusUpdateObserver,
       public views::ViewObserver,
       public InSessionAuthTokenProvider {
  public:
@@ -85,6 +88,10 @@ class ASH_EXPORT ActiveSessionAuthControllerImpl
   void OnPinSubmit(const std::u16string& pin) override;
   void OnClose() override;
 
+  // UserDataAuthClient::AuthFactorStatusUpdateObserver:
+  void OnAuthFactorStatusUpdate(
+      const user_data_auth::AuthFactorStatusUpdate& update) override;
+
   // InSessionAuthTokenProvider:
   void ExchangeForToken(
       std::unique_ptr<UserContext> user_context,
@@ -115,22 +122,16 @@ class ASH_EXPORT ActiveSessionAuthControllerImpl
       bool user_exists,
       std::unique_ptr<UserContext> user_context,
       std::optional<AuthenticationError> authentication_error);
-  void OnAuthFactorsListed(
-      base::OnceClosure callback,
-      std::unique_ptr<UserContext> user_context,
-      std::optional<AuthenticationError> authentication_error);
   void OnAuthComplete(AuthInputType input_type,
                       std::unique_ptr<UserContext> user_context,
                       std::optional<AuthenticationError> authentication_error);
   void NotifySuccess(const AuthProofToken& token, base::TimeDelta timeout);
+  void ProcessAuthFactorStatusUpdate(
+      const user_data_auth::AuthFactorStatusUpdate& update);
 
   // Initialize the UI after we retrieve the available auth factors from
   // cryptohome.
   void InitUi();
-
-  // After a failed pin attempt we have to retrieve the updated auth factors
-  // configuration to can check the pin is still available or not.
-  void OnFailedPinAttempt();
 
   // Checks the pin factor is still available using user_context_. It shpuld be
   // called after auth factors configuration is updated.
@@ -158,6 +159,9 @@ class ASH_EXPORT ActiveSessionAuthControllerImpl
   ActiveSessionAuthState state_ = ActiveSessionAuthState::kWaitForInit;
 
   Reason reason_;
+  std::optional<user_data_auth::AuthFactorStatusUpdate>
+      pending_pin_factor_status_update_ = std::nullopt;
+
   ActiveSessionAuthMetricsRecorder uma_recorder_;
 
   base::WeakPtrFactory<ActiveSessionAuthControllerImpl> weak_ptr_factory_{this};
