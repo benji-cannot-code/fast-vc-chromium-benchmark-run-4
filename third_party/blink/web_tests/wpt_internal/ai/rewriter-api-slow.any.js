d16e481686b20ab82e1fe5dcdb807f8259feef7f
Tsuyoshi Horo <horo@chromium.org>
2024-08-22 00:17:16 +0000

    Move slow writer/rewriter tests to a new file with timeout=long
    Some tests in writer/rewriter-api.any.js are slow when executed with AI
    models. So this CL moves such tests with a new file with
    "META: timeout=long" to avoid timeout errors.
    
    Bug: 357967382, 358214322, 361192155
    Change-Id: Ib89744991dd9619f33e267956e10569544eb4a30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802650
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345162}

---
+++ third_party/blink/web_tests/wpt_internal/ai/rewriter-api-slow.any.js
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
+
+promise_test(async () => {
+  const rewriter = await ai.rewriter.create();
+  const result = await rewriter.rewrite('hello');
+  assert_equals(typeof result, 'string');
+}, 'Simple AIRewriter.rewrite() call');
+
+promise_test(async () => {
+  const rewriter = await ai.rewriter.create();
+  const streamingResponse = rewriter.rewriteStreaming('hello');
+  assert_equals(
+      Object.prototype.toString.call(streamingResponse),
+      '[object ReadableStream]');
+  let result = '';
+  for await (const chunk of streamingResponse) {
+    result += chunk;
+  }
+  assert_true(result.length > 0);
+}, 'Simple AIRewriter.rewriteStreaming() call');
+
+promise_test(async (t) => {
+  const rewriter = await ai.rewriter.create();
+  const controller = new AbortController();
+  const streamingResponse =
+      rewriter.rewriteStreaming('hello', {signal: controller.signal});
+  for await (const chunk of streamingResponse) {
+  }
+  controller.abort();
+}, 'Aborting AIRewriter.rewriteStreaming() after finished reading');
