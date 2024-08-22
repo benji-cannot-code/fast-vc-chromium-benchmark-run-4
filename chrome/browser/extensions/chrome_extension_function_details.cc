4580c14204deeddcfa52f877a0e3a2a6ce49391a
Brett Wilson <brettw@google.com>
2024-08-22 21:13:35 +0000

    Remove unnecessary include.
    The tab util is included but never referenced.
    
    Bug: None
    Change-Id: I427666961c823dce384e382dd0f2dec607f20f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807334
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Brett Wilson <brettw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345649}

--- chrome/browser/extensions/chrome_extension_function_details.cc
+++ chrome/browser/extensions/chrome_extension_function_details.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/chrome_extension_function_details.h"
 
-#include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/extensions/window_controller.h"
 #include "chrome/browser/extensions/window_controller_list.h"
 #include "chrome/browser/profiles/profile.h"
