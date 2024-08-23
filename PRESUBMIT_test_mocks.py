438f4fb13a40202bab151dda095f76db15887ec9
Georg Neis <neis@chromium.org>
2024-08-23 01:50:29 +0000

    ash: Add presubmit check warning about indirect Ash sources
    Implement a presubmit check in c/b/PRESUBMIT.py that scans changes
    to BUILD.gn files under a set of Ash directories. It generates a
    warning for each such BUILD.gn files to which source entries were
    added that contain a slash, thus e.g. denoting a file in a subdirectory.
    
    For example, if a new file c/b/ash/foo/BUILD.gn is added containing
      static_library("foo") {
        sources = [ "bar/baz.cc", "x.cc", "../bla.h" ]
        ...
      }
    then the check will warn about bar/baz.cc and ../bla.h.
    
    (The parsing is done via regexps and is not 100% precise.)
    
    This presubmit check is meant to protect against regressions in the
    context of the ongoing Ash refactoring project (b/332804822) where we
    are creating BUILD.gn files for subdirectories.
    
    Bug: b:357772015
    Change-Id: Ibb7b99c2531bc129fd06ef14cd7ed07a361be33a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5768197
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345766}

--- PRESUBMIT_test_mocks.py
+++ PRESUBMIT_test_mocks.py
@@ -64,6 +64,7 @@ class MockInputApi(object):
   DEFAULT_FILES_TO_SKIP = ()
 
   def __init__(self):
+    self.basename = os.path.basename
     self.canned_checks = MockCannedChecks()
     self.fnmatch = fnmatch
     self.json = json
@@ -105,6 +106,9 @@ class MockInputApi(object):
         yield (af, line[0], line[1])
 
   def AffectedSourceFiles(self, file_filter=None):
+    return self.AffectedFiles(file_filter=file_filter, include_deletes=False)
+
+  def AffectedTestableFiles(self, file_filter=None):
     return self.AffectedFiles(file_filter=file_filter, include_deletes=False)
 
   def FilterSourceFile(self, file,
@@ -208,7 +212,7 @@ class MockFile(object):
             (local_path, len(new_contents)))
       for l in new_contents:
         self._scm_diff += "+%s\n" % l
-    self._old_contents = old_contents
+    self._old_contents = old_contents or []
 
   def __str__(self):
     return self._local_path
