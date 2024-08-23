340f7a31f66f1d8ae5ffb5aed594eb7787507a9e
Aliona DANGLA <alionadangla@google.com>
2024-08-23 23:29:04 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=holte@chromium.org
    
    Bug: 359177266
    Change-Id: I2a095b71b9585bf5365fd61f65a1cfddf01c975f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808940
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346270}

--- ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory_unittest.mm
+++ ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory_unittest.mm
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "components/optimization_guide/core/optimization_guide_features.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "testing/gtest/include/gtest/gtest.h"
 #import "testing/platform_test.h"
 
--- ios/chrome/browser/optimization_guide/model/optimization_guide_tab_helper_unittest.mm
+++ ios/chrome/browser/optimization_guide/model/optimization_guide_tab_helper_unittest.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_service_factory.h"
 #import "ios/chrome/browser/optimization_guide/model/optimization_guide_test_utils.h"
 #import "ios/chrome/browser/shared/model/browser_state/chrome_browser_state.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/web/public/test/fakes/fake_navigation_context.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
 #import "testing/gmock/include/gmock/gmock.h"
