3933cda51efac17fd519174189a70229fe46b92b
Istvan Nagy <iscsi@google.com>
2024-08-23 12:19:29 +0000

    [High5] Implement fingerprint view.
    Bug: b:333713449, b:348326316
    Change-Id: I0ba8a0595652435eecb5c514d3b2cf810a743e65
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802946
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345913}

--- ash/in_session_auth/auth_dialog_contents_view.cc
+++ ash/in_session_auth/auth_dialog_contents_view.cc
@@ -295,6 +295,8 @@ class AuthDialogContentsView::FingerprintView : public views::View {
                 kFingerprintFailedAnimationNumFrames),
             AnimatedRoundedImageView::Playback::kSingle);
         break;
+      case FingerprintState::AVAILABLE_WITH_FAILED_ATTEMPT:
+        NOTREACHED();
     }
   }
 
@@ -312,6 +314,7 @@ class AuthDialogContentsView::FingerprintView : public views::View {
         }
         return IDS_ASH_IN_SESSION_AUTH_FINGERPRINT_PASSWORD_REQUIRED;
       case FingerprintState::UNAVAILABLE:
+      case FingerprintState::AVAILABLE_WITH_FAILED_ATTEMPT:
         NOTREACHED();
     }
   }
--- ash/login/ui/fingerprint_auth_factor_model.cc
+++ ash/login/ui/fingerprint_auth_factor_model.cc
@@ -97,6 +97,8 @@ FingerprintAuthFactorModel::GetAuthFactorState() const {
       [[fallthrough]];
     case FingerprintState::DISABLED_FROM_TIMEOUT:
       return AuthFactorState::kErrorPermanent;
+    case FingerprintState::AVAILABLE_WITH_FAILED_ATTEMPT:
+      NOTREACHED();
   }
 }
 
@@ -125,6 +127,8 @@ int FingerprintAuthFactorModel::GetLabelId() const {
     case FingerprintState::DISABLED_FROM_TIMEOUT:
       return can_use_pin_ ? IDS_AUTH_FACTOR_LABEL_PASSWORD_OR_PIN_REQUIRED
                           : IDS_AUTH_FACTOR_LABEL_PASSWORD_REQUIRED;
+    case FingerprintState::AVAILABLE_WITH_FAILED_ATTEMPT:
+      NOTREACHED();
   }
   NOTREACHED();
 }
@@ -157,6 +161,8 @@ void FingerprintAuthFactorModel::UpdateIcon(AuthIconView* icon) {
     case FingerprintState::AVAILABLE_WITH_TOUCH_SENSOR_WARNING:
       icon->SetIcon(kLockScreenFingerprintIcon);
       break;
+    case FingerprintState::AVAILABLE_WITH_FAILED_ATTEMPT:
+      NOTREACHED();
     case FingerprintState::UNAVAILABLE:
       [[fallthrough]];
     case FingerprintState::DISABLED_FROM_TIMEOUT:
