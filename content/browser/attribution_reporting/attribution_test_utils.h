3cfff406555e724f87e476969ade3e3c075c2c9e
Thomas Quintanilla <tquintanilla@google.com>
2024-08-23 20:15:29 +0000

    Implements attribution and source registration logic for scopes
    https://github.com/WICG/attribution-reporting-api/pull/1365
    
    Bug: 344081621
    Change-Id: Ib6f858593ebed3db597ce3b723fd332f83a00964
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5598387
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346175}

--- content/browser/attribution_reporting/attribution_test_utils.h
+++ content/browser/attribution_reporting/attribution_test_utils.h
@@ -220,6 +220,9 @@ class TriggerBuilder {
   TriggerBuilder& SetAggregatableDebugReportingConfig(
       attribution_reporting::AggregatableDebugReportingConfig);
 
+  TriggerBuilder& SetAttributionScopes(
+      attribution_reporting::AttributionScopesSet);
+
   TriggerBuilder& SetAggregatableFilteringIdMaxBytes(
       attribution_reporting::AggregatableFilteringIdsMaxBytes);
 
@@ -250,6 +253,7 @@ class TriggerBuilder {
       aggregatable_filtering_id_max_bytes_;
   attribution_reporting::AggregatableDebugReportingConfig
       aggregatable_debug_reporting_config_;
+  attribution_reporting::AttributionScopesSet attribution_scopes_;
 };
 
 // Helper class to construct an `AttributionInfo` for tests using default data.
@@ -453,6 +457,11 @@ MATCHER_P(RemainingAggregatableAttributionBudgetIs, matcher, "") {
 
 MATCHER_P(AttributionScopesDataIs, matcher, "") {
   return ExplainMatchResult(matcher, arg.attribution_scopes_data(),
+                            result_listener);
+}
+
+MATCHER_P(AttributionScopesSetIs, matcher, "") {
+  return ExplainMatchResult(matcher, arg->attribution_scopes_set(),
                             result_listener);
 }
 
