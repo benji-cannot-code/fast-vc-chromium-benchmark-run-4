d8997698d0d1f077f5d93f65b8419e29e01f7aea
Aliona DANGLA <alionadangla@google.com>
2024-08-23 13:20:23 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=lpromero@google.com
    
    Bug: 359177266
    Change-Id: I5b99cfdee243802d7b91b53ed65fe8a25985d825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808998
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345932}

--- ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
+++ ios/chrome/browser/saved_tab_groups/model/ios_tab_group_sync_util_unittest.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser_list.h"
 #import "ios/chrome/browser/shared/model/browser/browser_list_factory.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
--- ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory_unittest.mm
+++ ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory_unittest.mm
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/test/scoped_feature_list.h"
 #import "ios/chrome/browser/saved_tab_groups/model/tab_group_sync_service_factory.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
