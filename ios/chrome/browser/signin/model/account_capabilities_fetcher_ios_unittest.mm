e285164c89f3d7e721ed0c54ff745ad825145e3f
Aliona DANGLA <alionadangla@google.com>
2024-08-23 17:36:59 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=msarda@chromium.org
    
    Bug: 359177266
    Change-Id: I01e7ae7b334dc781185f87289d927c478367ab13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808980
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346067}

--- ios/chrome/browser/signin/model/account_capabilities_fetcher_ios_unittest.mm
+++ ios/chrome/browser/signin/model/account_capabilities_fetcher_ios_unittest.mm
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "components/signin/public/identity_manager/identity_test_environment.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/signin/model/capabilities_dict.h"
 #import "ios/chrome/browser/signin/model/capabilities_types.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service.h"
--- ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_coordinator_unittest.mm
+++ ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_coordinator_unittest.mm
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/task_environment.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity.h"
 #import "ios/chrome/browser/signin/model/fake_system_identity_manager.h"
 #import "ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_view_controller.h"
