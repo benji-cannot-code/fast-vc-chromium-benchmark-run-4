6d9bf8ee2be44ae1f4107c798058914c352811d5
Mei Liang <meiliang@chromium.org>
2024-08-23 23:34:47 +0000

    [Discounts][Clank] Expose discount API
    This CL	exposes the following APIs to Clank:
      * GetDiscountInfoForUrl
      * IsDiscountEligibleToShowOnNavigation
    
    Bug: 361453979
    Change-Id: Idc77317aa2a303d4993a76f6f559ef24a30576d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804911
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346276}

--- components/commerce/core/android/shopping_service_android.h
+++ components/commerce/core/android/shopping_service_android.h
@@ -55,6 +55,11 @@ class ShoppingServiceAndroid : public base::SupportsUserData::Data,
                                   const JavaParamRef<jobject>& j_gurl,
                                   const JavaParamRef<jobject>& j_callback);
 
+  void GetDiscountInfoForUrl(JNIEnv* env,
+                             const JavaParamRef<jobject>& obj,
+                             const JavaParamRef<jobject>& j_gurl,
+                             const JavaParamRef<jobject>& j_callback);
+
   void FetchPriceEmailPref(JNIEnv* env, const JavaParamRef<jobject>& obj);
 
   void ScheduleSavedProductUpdate(JNIEnv* env,
@@ -108,6 +113,9 @@ class ShoppingServiceAndroid : public base::SupportsUserData::Data,
 
   bool IsPriceInsightsEligible(JNIEnv* env, const JavaParamRef<jobject>& obj);
 
+  bool IsDiscountEligibleToShowOnNavigation(JNIEnv* env,
+                                            const JavaParamRef<jobject>& obj);
+
   ScopedJavaGlobalRef<jobject> java_ref() { return java_ref_; }
 
  private:
@@ -126,6 +134,11 @@ class ShoppingServiceAndroid : public base::SupportsUserData::Data,
       const ScopedJavaGlobalRef<jobject>& callback,
       const GURL& url,
       const std::optional<PriceInsightsInfo>& info);
+
+  void HandleDiscountInfoCallback(JNIEnv* env,
+                                  const ScopedJavaGlobalRef<jobject>& callback,
+                                  const GURL& url,
+                                  const std::vector<DiscountInfo> info);
 
   void OnSubscribe(const CommerceSubscription& sub, bool succeeded) override;
   void OnUnsubscribe(const CommerceSubscription& sub, bool succeeded) override;
