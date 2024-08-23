f0e4dd29d40e71681dd00559259abff0627455c0
Andy Phan <andyphan@chromium.org>
2024-08-23 00:21:48 +0000

    [PDF Ink Signatures] Show beforeunload menu if there are unsaved edits
    Show the beforeunload menu if the user attempts to close the PDF viewer
    when there are unsaved annotation edits.
    
    More specifically, if the user has never saved edits in a PDF viewer
    session, show the menu on exit if there are any actions that can be
    undone. If the user has saved before, then show the menu on exit if the
    user performs any additional stroke action.
    
    Fixed: 353942911
    Change-Id: I74a27ae8832b8c7689066cb7e21a70817eafa59c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5770464
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345744}

---
+++ chrome/test/data/pdf/ink2_before_unload_stroke_test.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PluginController} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
+
+import {finishInkStroke, getNewTestBeforeUnloadProxy} from './test_util.js';
+
+chrome.test.runTests([
+  // Test that the save unedited dialog appears when the user navigates away
+  // from the PDF and there is a stroke on a page.
+  async function testBeforeUnloadStroke() {
+    const toolbar = document.body.querySelector('pdf-viewer')!.$.toolbar;
+
+    chrome.test.assertFalse(toolbar.annotationMode);
+
+    toolbar.toggleAnnotation();
+    await microtasksFinished();
+    chrome.test.assertTrue(toolbar.annotationMode);
+
+    finishInkStroke(PluginController.getInstance());
+
+    const testProxy = getNewTestBeforeUnloadProxy();
+    window.location.href = 'about:blank';
+
+    chrome.test.assertEq(1, testProxy.getCallCount('preventDefault'));
+    chrome.test.succeed();
+  },
+]);
---
+++ chrome/test/data/pdf/ink2_before_unload_undo_test.ts
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PluginController} from 'chrome-extension://mhjfbmdgcfjbbpaeojofohoefgiehjai/pdf_viewer_wrapper.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
+
+import {finishInkStroke, getNewTestBeforeUnloadProxy} from './test_util.js';
+
+chrome.test.runTests([
+  // Test that the save unedited dialog does not appear when the user navigates
+  // away from the PDF after undoing all strokes.
+  async function testBeforeUnloadUndo() {
+    const toolbar = document.body.querySelector('pdf-viewer')!.$.toolbar;
+
+    chrome.test.assertFalse(toolbar.annotationMode);
+
+    toolbar.toggleAnnotation();
+    await microtasksFinished();
+    chrome.test.assertTrue(toolbar.annotationMode);
+
+    finishInkStroke(PluginController.getInstance());
+    await microtasksFinished();
+
+    toolbar.undo();
+
+    const testProxy = getNewTestBeforeUnloadProxy();
+    window.location.href = 'about:blank';
+
+    chrome.test.assertEq(0, testProxy.getCallCount('preventDefault'));
+    chrome.test.succeed();
+  },
+]);
