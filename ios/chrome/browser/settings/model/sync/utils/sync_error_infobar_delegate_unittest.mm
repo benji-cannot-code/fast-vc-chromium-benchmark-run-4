1e72b715ed61002d8c2505b298a7b68e1b7ffa58
Aliona DANGLA <alionadangla@google.com>
2024-08-23 17:45:24 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=treib@chromium.org
    
    Bug: 359177266
    Change-Id: If8e07e589750fdd8193bd7044425ba4362aeb808
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803147
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346074}

--- ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate_unittest.mm
+++ ios/chrome/browser/settings/model/sync/utils/sync_error_infobar_delegate_unittest.mm
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/infobars/model/infobar_ios.h"
 #import "ios/chrome/browser/infobars/model/infobar_utils.h"
 #import "ios/chrome/browser/settings/model/sync/utils/sync_presenter.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/sync/model/mock_sync_service_utils.h"
--- ios/chrome/browser/sync/model/sync_service_factory_unittest.cc
+++ ios/chrome/browser/sync/model/sync_service_factory_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/service/sync_service_impl.h"
 #include "ios/chrome/browser/favicon/model/favicon_service_factory.h"
 #include "ios/chrome/browser/history/model/history_service_factory.h"
-#include "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#include "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #include "ios/chrome/browser/webdata_services/model/web_data_service_factory.h"
 #import "ios/chrome/test/ios_chrome_scoped_testing_local_state.h"
 #include "ios/web/public/test/web_task_environment.h"
