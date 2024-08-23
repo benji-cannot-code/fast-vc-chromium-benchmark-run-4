9cfe65371139da9791966ca6af8504007c2b4484
Rushan Suleymanov <rushans@google.com>
2024-08-23 09:38:36 +0000

    [Sync] Return all devices from GetAllDeviceInfo
    Prior to this CL only Chrome devices were returned. This CL removes this
    filtration, all the call sites were checked manually to have expected
    behavior. The only place where the filtration was required is metrics
    which might be skewed, and GetAllChromeDeviceInfo() was introduced for
    it.
    
    Bug: 357821400
    Change-Id: I54cbc18c12952a0fe29dd40e6e9afce5cbb69036
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803982
    Reviewed-by: Salvador Guerrero Ramos <salg@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345858}

--- components/sync_device_info/fake_device_info_tracker.h
+++ components/sync_device_info/fake_device_info_tracker.h
@@ -39,6 +39,7 @@ class FakeDeviceInfoTracker : public DeviceInfoTracker {
   bool IsSyncing() const override;
   const DeviceInfo* GetDeviceInfo(const std::string& client_id) const override;
   std::vector<const DeviceInfo*> GetAllDeviceInfo() const override;
+  std::vector<const DeviceInfo*> GetAllChromeDeviceInfo() const override;
   void AddObserver(Observer* observer) override;
   void RemoveObserver(Observer* observer) override;
   std::map<DeviceInfo::FormFactor, int> CountActiveDevicesByType()
