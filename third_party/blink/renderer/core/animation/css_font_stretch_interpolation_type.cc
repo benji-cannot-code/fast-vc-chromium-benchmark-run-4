e394d5eaceb126553e8e78f223f95184f498afe7
Daniil Sakhapov <sakhapov@chromium.org>
2024-08-23 14:31:46 +0000

    Add type info and checking for InterpolableNumber
    Also fix every (that I found) call site that doesn't provide type info
    to do so.
    
    Bug: 40946458
    Change-Id: I7f6fec0c8e613c54d214c771440b53f78116a4b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796898
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345954}

--- third_party/blink/renderer/core/animation/css_font_stretch_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_font_stretch_interpolation_type.cc
@@ -41,7 +41,8 @@ InterpolationValue CSSFontStretchInterpolationType::CreateFontStretchValue(
 InterpolationValue CSSFontStretchInterpolationType::MaybeConvertNeutral(
     const InterpolationValue&,
     ConversionCheckers&) const {
-  return InterpolationValue(MakeGarbageCollected<InterpolableNumber>(0));
+  return InterpolationValue(MakeGarbageCollected<InterpolableNumber>(
+      0, CSSPrimitiveValue::UnitType::kPercentage));
 }
 
 InterpolationValue CSSFontStretchInterpolationType::MaybeConvertInitial(
