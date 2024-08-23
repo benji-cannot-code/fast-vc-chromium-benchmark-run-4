589a7c01d617336b4f23bae0548a00eb5d4c2f7b
anthie@google.com <anthie@google.com>
2024-08-23 12:41:47 +0000

    Fix screen reader focus on Waiting interstitial screen
    This change sets the focus on the updated top level content
    area when the blocked url interstitial is updates, after
    a remote approval request.
    Previously the focus was explicitly set on the first
    actionable button, and as a consequence the screen reader did
    not announce the content change.
    
    See the bug for a screen recording.
    
    Bug: 361142785
    Change-Id: I8fb079402b159cab352311e0733d61616847735b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804329
    Reviewed-by: Nohemi Fernandez <fernandex@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345919}

--- components/supervised_user/core/browser/resources/supervised_user_block_interstitial_v2.js
+++ components/supervised_user/core/browser/resources/supervised_user_block_interstitial_v2.js
@@ -180,13 +180,11 @@ function requestCreated(isSuccessful, isMainFrame) {
           event) {
         sendCommand('requestUrlAccessLocal');
       };
-      $('local-approvals-remote-request-sent-button').focus();
     } else {
       $('back-button').hidden = !isMainFrame;
       $('back-button').onclick = function(event) {
         sendCommand('back');
       };
-      $('back-button').focus();
     }
     $('error-page-illustration').hidden = true;
     $('waiting-for-approval-illustration').hidden = false;
@@ -197,6 +195,8 @@ function requestCreated(isSuccessful, isMainFrame) {
     $('remote-approvals-button').disabled = false;
     $('show-details-link').hidden = false;
   }
+  // After updating the contents, focus the top-level div for screen readers.
+  $('frame-blocked').focus();
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
