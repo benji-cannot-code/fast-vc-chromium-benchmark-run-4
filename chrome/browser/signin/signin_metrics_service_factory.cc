655bcf6f82d4f4d6adf673cc62f4e096c80032f8
Ryan Sultanem <rsult@google.com>
2024-08-23 12:43:00 +0000

    [Uno-D] Record Signin.SyncPaused.ResolutionTime.*
    Add a sync-paused histogram to match
    Signin.SigninPending.ResolutionTime.*
    
    Both for Reauth and Signout resolution sources.
    
    Also fixing a bug in the existing SigninPending histogram where errors
    that were generated externally caused the histograms to emit
    Reauth (no errors being treated as reauth because errors were not
    persisted) on start up then being notified again for the actual error.
    
    Fixed: b:360810673
    Change-Id: Iab89f16120b1723f47ef4ce68a4156f2210fdc8c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797048
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345920}

--- chrome/browser/signin/signin_metrics_service_factory.cc
+++ chrome/browser/signin/signin_metrics_service_factory.cc
@@ -12,10 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/core/browser/signin_metrics_service.h"
 
 SigninMetricsServiceFactory::SigninMetricsServiceFactory()
-    : ProfileKeyedServiceFactory("SigninMetricsHelper",
-                                 ProfileSelections::Builder()
-                                     .WithAshInternals(ProfileSelection::kNone)
-                                     .Build()) {
+    : ProfileKeyedServiceFactory("SigninMetricsHelper") {
   DependsOn(IdentityManagerFactory::GetInstance());
 }
 
