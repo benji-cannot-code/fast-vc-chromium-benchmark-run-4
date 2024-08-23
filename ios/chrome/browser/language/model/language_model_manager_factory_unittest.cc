afa098f2f6331937bc1fc6f09909f83ffeea2b33
Aliona DANGLA <alionadangla@google.com>
2024-08-23 19:53:45 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=iwells@chromium.org
    
    Bug: 359177266
    Change-Id: I9c6229b21c0ad4d7d65c2a7dae3c8c3af711c62c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5806973
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Ian Wells <iwells@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346165}

--- ios/chrome/browser/language/model/language_model_manager_factory_unittest.cc
+++ ios/chrome/browser/language/model/language_model_manager_factory_unittest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ios/chrome/browser/language/model/language_model_manager_factory.h"
 
-#include "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#include "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #include "ios/web/public/test/web_task_environment.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/platform_test.h"
