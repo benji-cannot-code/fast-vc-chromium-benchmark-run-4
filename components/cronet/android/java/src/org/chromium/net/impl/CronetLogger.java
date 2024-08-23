84761c3057aae0ff6d00a1db255b56ee1a54aeb7
Mohannad Farrag <aymanm@google.com>
2024-08-23 16:57:33 +0000

    Revert "Log network error codes, UID, close source and socket reuse to CronetTrafficInfo"
    This reverts commit a5dccc6633bac137147e67d550e0c3b6180c2f89.
    
    Reason for revert: b/361725824
    
    Original change's description:
    > Log network error codes, UID, close source and socket reuse to CronetTrafficInfo
    >
    > We were just logging the terminal state of the cronet request without
    > any indicator of why the request has failed. This CL adds the network
    > error code for failures and quic error code if QUIC was used for the
    > failing request.
    >
    > Added failure reason which indiciates why did the request fail, currently
    > only network failure reason exist, we should add callbacks, upload
    > exceptions aswell.
    >
    > JavaUrlRequest does not depend on chromium networking stack hence it
    > has its own networking errors which are not translated to cronet errors
    > and are currently being reported as 0 to indicate that they are not
    > implemented. Ideally we should have an indicator of the source of
    > the request, whether it is from fallback or impl but that's a problem
    > for another day.
    >
    > Added UID to cronetTrafficInfo to help debug the problem that we are
    > seeing with AGSA's traffic for NETWORK_IDLE_TIMEOUT.
    >
    > Bug: 344551116
    > Bug: 343937464
    > Change-Id: Id3987d4c3662d674fab99cc92e40efdd859015d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5594221
    > Auto-Submit: Mohannad Farrag <aymanm@google.com>
    > Reviewed-by: Stefano Duo <stefanoduo@google.com>
    > Commit-Queue: Mohannad Farrag <aymanm@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1334775}
    
    Bug: 344551116
    Bug: 343937464
    Change-Id: I722bc4085cf789b85d05b962f7e8d5a984f30653
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808972
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Commit-Queue: Mohannad Farrag <aymanm@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346031}

--- components/cronet/android/java/src/org/chromium/net/impl/CronetLogger.java
+++ components/cronet/android/java/src/org/chromium/net/impl/CronetLogger.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.net.impl;
 
-import org.chromium.net.ConnectionCloseSource;
-
 import java.time.Duration;
 import java.util.List;
 
@@ -196,13 +194,6 @@ public abstract class CronetLogger {
             CANCELLED,
         }
 
-        // TODO(b/355615357): Add more specific failure reasons.
-        public static enum RequestFailureReason {
-            UNKNOWN,
-            NETWORK,
-            OTHER,
-        }
-
         private final long mRequestHeaderSizeInBytes;
         private final long mRequestBodySizeInBytes;
         private final long mResponseHeaderSizeInBytes;
@@ -219,12 +210,6 @@ public abstract class CronetLogger {
         private final int mOnUploadReadCount;
         private final boolean mIsBidiStream;
         private final boolean mFinalUserCallbackThrew;
-        private final int mUid;
-        private final int mNetworkInternalErrorCode;
-        private final int mQuicErrorCode;
-        private final @ConnectionCloseSource int mSource;
-        private final RequestFailureReason mFailureReason;
-        private final boolean mSocketReused;
 
         public CronetTrafficInfo(
                 long requestHeaderSizeInBytes,
@@ -242,13 +227,7 @@ public abstract class CronetLogger {
                 int readCount,
                 int uploadReadCount,
                 boolean isBidiStream,
-                boolean finalUserCallbackThrew,
-                int uid,
-                int networkInternalErrorCode,
-                int quicErrorCode,
-                @ConnectionCloseSource int source,
-                RequestFailureReason failureReason,
-                boolean sockedReused) {
+                boolean finalUserCallbackThrew) {
             mRequestHeaderSizeInBytes = requestHeaderSizeInBytes;
             mRequestBodySizeInBytes = requestBodySizeInBytes;
             mResponseHeaderSizeInBytes = responseHeaderSizeInBytes;
@@ -265,12 +244,6 @@ public abstract class CronetLogger {
             mOnUploadReadCount = uploadReadCount;
             mIsBidiStream = isBidiStream;
             mFinalUserCallbackThrew = finalUserCallbackThrew;
-            mUid = uid;
-            mNetworkInternalErrorCode = networkInternalErrorCode;
-            mQuicErrorCode = quicErrorCode;
-            mSource = source;
-            mFailureReason = failureReason;
-            mSocketReused = sockedReused;
         }
 
         /**
@@ -357,30 +330,6 @@ public abstract class CronetLogger {
 
         public boolean getFinalUserCallbackThrew() {
             return mFinalUserCallbackThrew;
-        }
-
-        public int getUid() {
-            return mUid;
-        }
-
-        public int getNetworkInternalErrorCode() {
-            return mNetworkInternalErrorCode;
-        }
-
-        public int getQuicErrorCode() {
-            return mQuicErrorCode;
-        }
-
-        public @ConnectionCloseSource int getConnectionCloseSource() {
-            return mSource;
-        }
-
-        public RequestFailureReason getFailureReason() {
-            return mFailureReason;
-        }
-
-        public boolean getIsSocketReused() {
-            return mSocketReused;
         }
     }
 
