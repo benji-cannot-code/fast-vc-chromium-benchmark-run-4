91613f794d0e6212a9a16d4fbb66ed8305cfaa51
dpapad <dpapad@chromium.org>
2024-08-23 21:51:28 +0000

    Signin: Migrate managed-user-profile-notice to Lit, part 1.
    In this part migrating:
     - managed_user_profile_notice_disclosure.ts
     - managed_user_profile_notice_state.ts
    
    Also changing iron-icon dependency to cr-icon.
    
    Bug: 40943652
    Change-Id: I954bdc719aaafe7d4823b89055d597b551e60d30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805529
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346223}

--- chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_state.ts
+++ chrome/browser/resources/signin/managed_user_profile_notice/managed_user_profile_notice_state.ts
@@ -3,30 +3,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 import './strings.m.js';
-import './tangible_sync_style_shared.css.js';
 
-import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
-import {getTemplate} from './managed_user_profile_notice_state.html.js';
+import {getCss} from './managed_user_profile_notice_state.css.js';
+import {getHtml} from './managed_user_profile_notice_state.html.js';
 
-export class ManagedUserProfileNoticeStateElement extends PolymerElement {
+export class ManagedUserProfileNoticeStateElement extends CrLitElement {
   static get is() {
     return 'managed-user-profile-notice-state';
   }
 
-  static get template() {
-    return getTemplate();
+  static override get styles() {
+    return getCss();
   }
 
-  static get properties() {
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
     return {
-      icon: String,
-      title: String,
-      subtitle: String,
+      title: {type: String},
+      subtitle: {type: String},
     };
   }
+
+  override title: string;
+  subtitle: string;
 }
 
 declare global {
