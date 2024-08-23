1293cc773a2b8e8090d5409b2361b9d7b77d92f6
Solomon Kinard <solomonkinard@chromium.org>
2024-08-23 02:15:33 +0000

    Extensions: WAR: DNR and extensionPath support for dynamic urls
    The `use_dynamic_url` feature for web accessible resources requires that
    the requested url be a dynamic url. A dynamic url is one where a session
    GUID used for the host instead of the static extension id.
    
    Doc:
    https://docs.google.com/document/d/1p7-uW_z6zDWX0IXM4H5RcCu_n2w-npxDMKqxXaABYhI
    
    Bug: chromium:346952289
    Change-Id: I0b5d5b2cfc7f256293bcfbb7f33a3e7b5da17330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804127
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345774}

--- extensions/browser/api/web_request/extension_web_request_event_router.cc
+++ extensions/browser/api/web_request/extension_web_request_event_router.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/api/declarative_webrequest/webrequest_constants.h"
 #include "extensions/browser/api/declarative_webrequest/webrequest_rules_registry.h"
 #include "extensions/browser/api/extensions_api_client.h"
+#include "extensions/browser/api/web_accessible_resources/web_accessible_resources_router.h"
 #include "extensions/browser/api/web_request/permission_helper.h"
 #include "extensions/browser/api/web_request/web_request_api_constants.h"
 #include "extensions/browser/api/web_request/web_request_api_helpers.h"
@@ -391,6 +392,16 @@ void OnDNRActionMatched(content::BrowserContext* browser_context,
   // `request.ShouldRecordMatchedAllowRuleInOnHeadersReceived` will only record
   // an allow rule matched in OnHeadersReceived with a greater priority than one
   // matched in OnBeforeRequest.
+}
+
+// The `use_dynamic_url` feature for web accessible resources requires that the
+// requested url be a dynamic url. A dynamic url is one where a session GUID is
+// used for the host instead of the static extension id.
+GURL GetNewUrl(const GURL& redirect_url,
+               content::BrowserContext* browser_context) {
+  auto dynamic_url =
+      TransformToDynamicURLIfNecessary(redirect_url, browser_context);
+  return dynamic_url.value_or(redirect_url);
 }
 
 using CallbacksForPageLoad = std::list<base::OnceClosure>;
@@ -995,7 +1006,7 @@ int WebRequestEventRouter::OnBeforeRequest(
           DCHECK_EQ(1u, actions.size());
           DCHECK(action.redirect_url);
           OnDNRActionMatched(browser_context, *request, action);
-          *new_url = action.redirect_url.value();
+          *new_url = GetNewUrl(action.redirect_url.value(), browser_context);
           // Collect redirect action data for the Extension Telemetry Service.
           if (action.type == DNRRequestAction::Type::REDIRECT) {
             ExtensionsBrowserClient::Get()
@@ -1223,7 +1234,8 @@ int WebRequestEventRouter::OnHeadersReceived(
 
           extension_web_request_api_helpers::
               RedirectRequestAfterHeadersReceived(
-                  *action.redirect_url, **override_response_headers,
+                  GetNewUrl(action.redirect_url.value(), browser_context),
+                  **override_response_headers,
                   preserve_fragment_on_redirect_url);
           return net::OK;
         case DNRRequestAction::Type::MODIFY_HEADERS:
