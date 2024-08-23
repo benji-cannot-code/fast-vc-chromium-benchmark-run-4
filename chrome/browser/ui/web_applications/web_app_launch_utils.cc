57a7e45a3f81801ac4e3d59f6645e09bdbadff82
Marijn Kruisselbrink <mek@chromium.org>
2024-08-23 22:17:11 +0000

    [PWA] Consolidate PWA bookkeeping to tab helper & browser controller 3/x
    This is intended to be a non-functional refactoring change to clean up
    how launching and PWA bookkeeping occurs for launches, window, and tabs.
    
    This saves `is_in_app_window` in WebAppTabHelper, allowing some
    logic to be simplified and allows less reliance on WebAppUiManager.
    It also slightly tweaks what code updates WebAppTabHelper with new
    values, removing some special casing from launch code.
    
    Bug: b:356628797, 351775835
    Change-Id: Ibe0c37301321b8ae1484aa40b792f63df2e36c84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5801449
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346238}

--- chrome/browser/ui/web_applications/web_app_launch_utils.cc
+++ chrome/browser/ui/web_applications/web_app_launch_utils.cc
@@ -467,13 +467,6 @@ Browser* ReparentWebContentsIntoAppBrowser(content::WebContents* contents,
                                            as_pinned_home_tab);
 }
 
-void SetWebContentsActingAsApp(content::WebContents* contents,
-                               const webapps::AppId& app_id) {
-  auto* helper = WebAppTabHelper::FromWebContents(contents);
-  helper->SetAppId(app_id);
-  helper->set_acting_as_app(true);
-}
-
 void SetWebContentsIsPinnedHomeTab(content::WebContents* contents) {
   auto* helper = WebAppTabHelper::FromWebContents(contents);
   helper->set_is_pinned_home_tab(true);
@@ -669,10 +662,6 @@ content::WebContents* NavigateWebAppUsingParams(const std::string& app_id,
 
   content::WebContents* const web_contents =
       nav_params.navigated_or_inserted_contents;
-
-  if (web_contents) {
-    SetWebContentsActingAsApp(web_contents, app_id);
-  }
 
   return web_contents;
 }
