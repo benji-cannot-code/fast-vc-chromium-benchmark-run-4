ab5d6b6be011d4179044a740d071c8d555d0a68e
Alexander Timin <altimin@chromium.org>
2024-08-23 18:43:45 +0000

    Use TerminatingFlow instead of Flow in ThreadController active
    Ensure that we don't emit "TC active" -> "TC active" and "TC active" -> "ScheduleWork".
    
    Change-Id: Iee0097e88973f72d46c68ddeddff6d1353f0cc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803939
    Reviewed-by: Sean Maher <spvw@chromium.org>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Auto-Submit: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346114}

--- base/task/sequence_manager/thread_controller.cc
+++ base/task/sequence_manager/thread_controller.cc
@@ -638,8 +638,8 @@ void ThreadController::RunLevelTracker::TimeKeeper::MaybeEmitIncomingWakeupFlow(
     return;
   }
 
-  perfetto::Flow::ProcessScoped(reinterpret_cast<uint64_t>(&(outer_.get())))(
-      ctx);
+  perfetto::TerminatingFlow::ProcessScoped(
+      reinterpret_cast<uint64_t>(&(outer_.get())))(ctx);
 #endif
 }
 
