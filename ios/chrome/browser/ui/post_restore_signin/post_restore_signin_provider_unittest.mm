1b2b421016c9a66d542b441906e23eeb62591de4
Aliona DANGLA <alionadangla@google.com>
2024-08-23 18:03:55 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=bwwilliams@google.com
    
    Bug: 359177266
    Change-Id: I48734ea89e0f5303634bb180e67a588d0fd770dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805948
    Reviewed-by: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346087}

--- ios/chrome/browser/ui/post_restore_signin/post_restore_signin_provider_unittest.mm
+++ ios/chrome/browser/ui/post_restore_signin/post_restore_signin_provider_unittest.mm
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/promos_manager/model/promo_config.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/test/test_browser.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/public/commands/promos_manager_commands.h"
 #import "ios/chrome/browser/signin/model/signin_util.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
