200d234ac99c8f8cee4a01b4db028f3d23096931
Aliona DANGLA <alionadangla@google.com>
2024-08-23 17:50:22 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=samarchehade@google.com
    
    Bug: 359177266
    Change-Id: Ia198c3e5391b4bd9a0d9a2a0aeaf00df19e2c1d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808963
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346077}

--- ios/chrome/browser/search_engine_choice/model/search_engine_choice_util_unittest.mm
+++ ios/chrome/browser/search_engine_choice/model/search_engine_choice_util_unittest.mm
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/signin/public/base/signin_switches.h"
 #import "ios/chrome/browser/policy/model/browser_state_policy_connector_mock.h"
 #import "ios/chrome/browser/search_engines/model/template_url_service_factory.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/gtest/include/gtest/gtest.h"
