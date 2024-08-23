3385eafe51e3b5b472778d179217d7e4c15e81f0
Noémie St-Onge <noemies@google.com>
2024-08-23 19:06:43 +0000

    [iOSKAUpgrade] Record context-specific suggestion accepted index metrics
    This CL creates a new histogram, similar to
    `Autofill.SuggestionAcceptedIndex{SuggestionType}`, to record the index
    of the accepted suggestion per iOS context (i.e., keyboard accessory,
    bottom sheet or manual fallback). This will help better understand how
    good each of these surfaces are at ranking suggestions and how users
    interact with each of them.
    
    Fixed: 354695796
    Change-Id: Ibd954c15e5df1fe74f0d1aa8bf1eb0cdcbd4d08e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789639
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346132}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_card_cell.mm
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_card_cell.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_card_cell.h"
 
+#import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "base/strings/sys_string_conversions.h"
 #import "base/strings/utf_string_conversions.h"
@@ -820,6 +821,10 @@ CGFloat GPayIconTopAnchorOffset() {
 // Called when the "Autofill Form" button is tapped. Fills the current form with
 // the card's data.
 - (void)onAutofillFormButtonTapped {
+  base::UmaHistogramSparse(
+      "Autofill.UserAcceptedSuggestionAtIndex.CreditCard.ManualFallback",
+      _cellIndex);
+
   autofill::SuggestionType type =
       autofill::VirtualCardFeatureEnabled() &&
               [self.card recordType] == kVirtualCard
