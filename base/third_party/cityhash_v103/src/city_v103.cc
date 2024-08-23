223cade918d8866307a49108bd879cd823f92430
Mohannad Farrag <aymanm@google.com>
2024-08-23 18:06:38 +0000

    Move cityhash_v103 to its own individual BUILD.gn
    cityhash_v103 is a third_party library which should have its own BUILD.gn
    file, in order for the licensing to correctly identify that this is
    a third_party, it needs to live in its own BUILD.gn with its own target.
    
    This is also impacting Cronet in AOSP where the license is not being
    forwarded correctly since it lives with base.
    
    Bug: 358541596
    
    Change-Id: I35a1d7fbfc0f3087fc98ad463d3df8e829befd86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808945
    Reviewed-by: Stefano Duo <stefanoduo@google.com>
    Auto-Submit: Mohannad Farrag <aymanm@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346090}

--- base/third_party/cityhash_v103/src/city_v103.cc
+++ base/third_party/cityhash_v103/src/city_v103.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>  // for memcpy and memset
 #include <algorithm>
 
-#include "base/compiler_specific.h"
 #include "build/build_config.h"
 
 namespace base {
