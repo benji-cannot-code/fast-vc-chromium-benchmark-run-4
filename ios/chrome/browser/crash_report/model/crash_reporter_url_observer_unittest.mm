9164a432d0374f76e564d5e81fdec60218978fcd
Aliona DANGLA <alionadangla@google.com>
2024-08-23 15:26:12 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=justincohen@chromium.org
    
    Bug: 359177266
    Change-Id: Id19d7182153eb89bc5f02c975dbecd4c4dd0094b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808680
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345984}

--- ios/chrome/browser/crash_report/model/crash_reporter_url_observer_unittest.mm
+++ ios/chrome/browser/crash_report/model/crash_reporter_url_observer_unittest.mm
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import <Foundation/Foundation.h>
-
 #import "ios/chrome/browser/crash_report/model/crash_reporter_url_observer.h"
+
+#import <Foundation/Foundation.h>
 
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/task_environment.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/model/web_state_list/test/fake_web_state_list_delegate.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_opener.h"
