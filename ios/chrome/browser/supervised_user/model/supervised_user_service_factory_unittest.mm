f53762c83b85337e45b637cb1536e08dec521be0
Aliona DANGLA <alionadangla@google.com>
2024-08-23 21:30:10 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=courtneywong@chromium.org
    
    Bug: 359177266
    Change-Id: I15e5f7c636432b92db5380b12649e681e58b74b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809002
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346212}

--- ios/chrome/browser/supervised_user/model/supervised_user_service_factory_unittest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_service_factory_unittest.mm
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/supervised_user/core/browser/supervised_user_service.h"
 #import "components/sync_preferences/testing_pref_service_syncable.h"
 #import "ios/chrome/browser/first_run/model/first_run.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/shared/model/prefs/browser_prefs.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 
--- ios/chrome/browser/supervised_user/model/supervised_user_settings_service_factory_unittest.mm
+++ ios/chrome/browser/supervised_user/model/supervised_user_settings_service_factory_unittest.mm
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/supervised_user/model/supervised_user_settings_service_factory.h"
 
 #import "components/supervised_user/core/browser/supervised_user_settings_service.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/web/public/test/web_task_environment.h"
 #import "testing/platform_test.h"
 
