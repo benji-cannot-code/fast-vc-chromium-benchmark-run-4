1da15fcc2dfa6df39a5073c93d50614de01190d2
Tanmoy Mollik <triploblastic@google.com>
2024-08-23 14:50:38 +0000

    [Android] Use SigninTestRule in FeedV2NewTAbPageTest
    This cl replaces AccountManagerTestRule with it's subclass
    SigninTestRule in FeedV2NewTAbPageTest
    
    Bug: 344816076
    Change-Id: Iab775ed2af715c2b1a551bf06a409f2f747dfd25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797177
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345965}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/SigninTestRule.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/util/browser/signin/SigninTestRule.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.test.util.browser.signin;
 
 import static org.hamcrest.Matchers.is;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import org.chromium.base.ThreadUtils;
@@ -37,6 +38,12 @@ import org.chromium.components.sync.SyncService;
  */
 public class SigninTestRule extends AccountManagerTestRule {
     private boolean mIsSignedIn;
+
+    public SigninTestRule() {}
+
+    public SigninTestRule(@NonNull FakeAccountManagerFacade fakeAccountManagerFacade) {
+        super(fakeAccountManagerFacade);
+    }
 
     /** Signs out if user is signed in. */
     @Override
