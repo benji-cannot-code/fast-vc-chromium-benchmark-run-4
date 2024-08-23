7b9e789ab7ef6f828a83362a9703487eb23ec309
Ken Rockot <rockot@google.com>
2024-08-23 05:34:22 +0000

    Remove MessagePumpLibevent
    This has effectively been a thin facade around MessagePumpEpoll
    for months. An alias for MessagePumpLibevent -> MessagePumpEpoll
    is left in message_pump_libevent.h to avoid rewriting all
    references in this CL. This will be addressed in a follow-up
    change.
    
    The MessagePumpEpoll feature and field trial config are also removed.
    
    Bug: 1243354
    Change-Id: I818c751cdfd8eb72f04b3509061fd7c7c55e1f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4339356
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Ken Rockot <rockot@google.com>
    Cr-Commit-Position: refs/heads/main@{#1345814}

--- base/message_loop/message_pump_unittest.cc
+++ base/message_loop/message_pump_unittest.cc
@@ -32,10 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
-#if BUILDFLAG(IS_POSIX) && !BUILDFLAG(IS_NACL)
-#include "base/message_loop/message_pump_libevent.h"
-#endif
-
 using ::testing::_;
 using ::testing::AnyNumber;
 using ::testing::AtMost;
