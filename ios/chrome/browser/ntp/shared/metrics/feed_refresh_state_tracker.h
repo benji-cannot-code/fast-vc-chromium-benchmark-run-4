28e6627b1c1ed71bd7d7ae704782a14579b0edaa
Chris Lu <thegreenfrog@chromium.org>
2024-08-23 11:52:18 +0000

    [ios] Move //ios/c/b/ntp/ui_bundled/metrics to //ntp/shared
    Per guidance in crrev.com/c/5805403
    
    Bug: 324233176
    Change-Id: I2b774c98a98fc3a540ec8d6c690e238f8b90e0e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805932
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345899}

---
+++ ios/chrome/browser/ntp/shared/metrics/feed_refresh_state_tracker.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NTP_SHARED_METRICS_FEED_REFRESH_STATE_TRACKER_H_
+#define IOS_CHROME_BROWSER_NTP_SHARED_METRICS_FEED_REFRESH_STATE_TRACKER_H_
+
+// Temporary include for downstream compatibility.
+#import "ios/chrome/browser/ui/ntp/metrics/feed_refresh_state_tracker.h"
+
+#endif  // IOS_CHROME_BROWSER_NTP_SHARED_METRICS_FEED_REFRESH_STATE_TRACKER_H_
---
+++ ios/chrome/browser/ntp/shared/metrics/new_tab_page_metrics_constants.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_NTP_SHARED_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_NTP_SHARED_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
+
+// Temporary include for downstream compatibility.
+#import "ios/chrome/browser/ui/ntp/metrics/new_tab_page_metrics_constants.h"
+
+#endif  // IOS_CHROME_BROWSER_NTP_SHARED_METRICS_NEW_TAB_PAGE_METRICS_CONSTANTS_H_
