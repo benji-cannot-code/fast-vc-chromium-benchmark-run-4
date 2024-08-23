e0fb331245dfefa45d2c53e115b24bc383f4420a
Aliona DANGLA <alionadangla@google.com>
2024-08-23 22:21:54 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=carlosil@chromium.org
    
    Bug: 359177266
    Change-Id: I6fd5cadb7471aa45fe7d6af60801d9512f086b4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809001
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346242}

--- ios/chrome/browser/ssl/model/ios_ssl_error_handler_unittest.mm
+++ ios/chrome/browser/ssl/model/ios_ssl_error_handler_unittest.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/functional/bind.h"
 #import "base/run_loop.h"
 #import "base/test/ios/wait_util.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/ssl/model/captive_portal_tab_helper.h"
 #import "ios/components/security_interstitials/ios_blocking_page_tab_helper.h"
 #import "ios/web/public/test/fakes/fake_navigation_manager.h"
