ae8e0e2be7c77b74bdc179576701d1f39b9eb1d5
Wan-Teh Chang <wtc@google.com>
2024-08-23 05:57:11 +0000

    Roll src/third_party/dav1d/libdav1d/ 5ef6b241f..472b31f83 (5 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/5ef6b241f05a..472b31f83825
    
    $ git log 5ef6b241f..472b31f83 --date=short --no-merges --format='%ad %ae %s'
    2024-08-22 Arpad.Panyik AArch64: SVE MS armasm64 fix of HBD subpel filters
    2024-08-22 martin aarch64: mc16: Optimize the BTI landing pads in put/prep_neon
    2024-08-12 Arpad.Panyik AArch64: Add HBD subpel filters using 128-bit SVE2
    2024-08-12 Arpad.Panyik AArch64: Add USMMLA impl. for SBD 6-tap H/HV filters
    2024-08-12 Arpad.Panyik AArch64: Fix typo in SBD 6-tap 2D/HV subpel filter
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: I31b613dcf67914a814e3281bea53b557935918bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804924
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1345818}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.4.2-16-g5ef6b24"
+#define DAV1D_VERSION "1.4.2-21-g472b31f"
