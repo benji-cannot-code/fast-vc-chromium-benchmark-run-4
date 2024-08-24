5d38fcbf014dfbceda656fcd6a57983686b64269
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-08-24 12:50:55 +0000

    Import wpt@2a3226304a809439fc0ce88f1e4b806f4ce6b91c
    https://github.com/web-platform-tests/wpt/compare/8b44cce3c...2a3226304
    
    Using wpt-import in Chromium 111d24bc14a1cbda88aa67171d2e2ec946bee615.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    adamk@chromium.org, ahaas@chromium.org:
      external/wpt/wasm
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I8aadae06822990315a6a194cb549a0b1df3777f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807687
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1346351}

---
+++ third_party/blink/web_tests/external/wpt/wasm/jsapi/js-string/imports.tentative.any.js
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,jsshell,shadowrealm
+// META: script=/wasm/jsapi/wasm-module-builder.js
+
+test(() => {
+  let builder = new WasmModuleBuilder();
+
+  // Import a string constant
+  builder.addImportedGlobal("constants", "constant", kWasmExternRef, false);
+
+  // Import a builtin function
+  builder.addImport(
+    "wasm:js-string",
+    "test",
+    {params: [kWasmExternRef], results: [kWasmI32]});
+
+  let buffer = builder.toBuffer();
+  let module = new WebAssembly.Module(buffer, {
+    builtins: ["js-string"],
+    importedStringConstants: "constants"
+  });
+  let imports = WebAssembly.Module.imports(module);
+
+  // All imports that refer to a builtin module are suppressed from import
+  // reflection.
+  assert_equals(imports.length, 0);
+});
