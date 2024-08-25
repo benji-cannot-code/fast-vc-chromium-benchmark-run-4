aa5e9132b20709296650ad7aa9b44f6c925965e7
Alison Maher <almaher@microsoft.com>
2024-08-25 22:33:26 +0000

    Add histograms for all Windows high contrast themes
    I removed the Accessibility.WinHighContrastTheme histogram in
    CL:4633409. This was originally added in CL:2118693. The reason I am
    adding this back is that it would be interesting to keep an eye on
    usage of this metric in correspondence with metrics related to the
    setting being added via  https://issues.chromium.org/issues/40779801.
    
    Bug: 1448122
    Change-Id: I760eb0a6fed72c778f9adf96e2a84ebff1e2580a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809526
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1346475}

--- content/browser/accessibility/browser_accessibility_state_impl.cc
+++ content/browser/accessibility/browser_accessibility_state_impl.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/ax_platform_node.h"
 #include "ui/events/base_event_utils.h"
 #include "ui/gfx/color_utils.h"
+#include "ui/native_theme/native_theme.h"
 
 namespace content {
 
@@ -387,6 +388,14 @@ void BrowserAccessibilityStateImpl::UpdateHistogramsOnUIThread() {
   UMA_HISTOGRAM_BOOLEAN(
       "Accessibility.ManuallyEnabled",
       !GetAccessibilityMode().is_mode_off() && !allow_ax_mode_changes_);
+
+#if BUILDFLAG(IS_WIN)
+  base::UmaHistogramEnumeration(
+      "Accessibility.WinHighContrastTheme",
+      ui::NativeTheme::GetInstanceForNativeUi()
+          ->GetPlatformHighContrastColorScheme(),
+      ui::NativeTheme::PlatformHighContrastColorScheme::kMaxValue);
+#endif
 
   ui_thread_done_ = true;
   if (other_thread_done_ && background_thread_done_callback_)
