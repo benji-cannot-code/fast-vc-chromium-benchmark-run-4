d4adb311ef7edcc3bcac3b904dfe43c3a5514550
dileep maurya <dileepmaurya@microsoft.com>
2024-08-26 18:53:48 +0000

    [views-ax] Migrating OmniboxViewViews and Textfield kValue attribute update to new system
    This CL migrates the kValue attribute in OmniboxViewViews and
    TextField while it gets updated by calling the view accessibility
    kValue setter methods. It also adds a conditional check in kValue
    setter, if value already exists with matching string, then don't
    fire the accessibility event. It introduces a getter method in
    view_accessibility to get the kValue attribute data from cache.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I51b0dce18e38db2d41947a042bc441089dfcd42a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5672084
    Reviewed-by: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dileep Maurya <dileepmaurya@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1346880}

--- ui/views/controls/textfield/textfield.cc
+++ ui/views/controls/textfield/textfield.cc
@@ -268,7 +268,6 @@ Textfield::Textfield()
   // accessibility since a plain text field should always be a leaf node in the
   // accessibility trees of all the platforms we support.
   GetViewAccessibility().SetIsLeaf(true);
-  UpdateAccessibilityTextDirection();
   UpdateAccessibleDefaultActionVerb();
 }
 
@@ -285,6 +284,11 @@ Textfield::~Textfield() {
 
 void Textfield::SetController(TextfieldController* controller) {
   controller_ = controller;
+}
+
+void Textfield::AddedToWidget() {
+  UpdateAccessibilityTextDirection();
+  UpdateAccessibleValue();
 }
 
 bool Textfield::GetReadOnly() const {
@@ -1052,16 +1056,11 @@ void Textfield::GetAccessibleNodeData(ui::AXNodeData* node_data) {
   // Editable state indicates support of editable interface, and is always set
   // for a textfield, even if disabled or readonly.
   node_data->AddState(ax::mojom::State::kEditable);
-  if (text_input_type_ == ui::TEXT_INPUT_TYPE_PASSWORD) {
-    node_data->SetValue(std::u16string(
-        GetText().size(), gfx::RenderText::kPasswordReplacementChar));
-  } else {
-    node_data->SetValue(GetText());
-  }
 
 #if BUILDFLAG(SUPPORTS_AX_TEXT_OFFSETS)
-  std::u16string ax_value =
-      node_data->GetString16Attribute(ax::mojom::StringAttribute::kValue);
+  // TODO(https://crbug.com/325137417): Recompute the text offsets whenever
+  // the value changes, not when GetAccessibleNodeData is different.
+  std::u16string ax_value = GetViewAccessibility().GetValue();
   // If the accessible value changed since the last time we computed the text
   // offsets, we need to recompute them.
   if (::ui::AXPlatform::GetInstance().IsUiaProviderEnabled() &&
@@ -2701,7 +2700,7 @@ void Textfield::UpdateAfterChange(
   if (text_change_type != TextChangeType::kNone) {
     if ((text_change_type == TextChangeType::kUserTriggered) && controller_)
       controller_->ContentsChanged(this, GetText());
-    NotifyAccessibilityEvent(ax::mojom::Event::kValueChanged, true);
+    UpdateAccessibleValue();
   }
   UpdateAccessibilityTextDirection();
   if (cursor_changed) {
@@ -2723,6 +2722,15 @@ void Textfield::UpdateAccessibilityTextDirection() {
       static_cast<int32_t>(GetTextDirection() == base::i18n::RIGHT_TO_LEFT
                                ? ax::mojom::WritingDirection::kRtl
                                : ax::mojom::WritingDirection::kLtr));
+}
+
+void Textfield::UpdateAccessibleValue() {
+  if (text_input_type_ == ui::TEXT_INPUT_TYPE_PASSWORD) {
+    GetViewAccessibility().SetValue(std::u16string(
+        GetText().size(), gfx::RenderText::kPasswordReplacementChar));
+  } else {
+    GetViewAccessibility().SetValue(GetText());
+  }
 }
 
 void Textfield::UpdateCursorVisibility() {
