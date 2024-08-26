1d673585a4ae9385b42445921d78d357232e1034
Aliona DANGLA <alionadangla@google.com>
2024-08-26 15:45:32 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=cheickcisse@google.com
    
    Bug: 359177266
    Change-Id: I5e2d641357144f02cd5e71c771b3a6c8cec76d9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808962
    Reviewed-by: Cheick Cisse <cheickcisse@google.com>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346751}

--- ios/chrome/browser/price_insights/model/price_insights_model_unittest.mm
+++ ios/chrome/browser/price_insights/model/price_insights_model_unittest.mm
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/commerce/model/shopping_service_factory.h"
 #import "ios/chrome/browser/contextual_panel/model/contextual_panel_item_configuration.h"
 #import "ios/chrome/browser/price_insights/model/price_insights_feature.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/web/public/test/fakes/fake_navigation_manager.h"
 #import "ios/web/public/test/fakes/fake_web_state.h"
