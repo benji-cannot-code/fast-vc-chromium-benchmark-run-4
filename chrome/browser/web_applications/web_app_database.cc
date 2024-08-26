7379fcbf4cc25e4552c306fa511ed46dee5ebc56
Dibyajyoti Pal <dibyapal@google.com>
2024-08-26 21:02:15 +0000

    [PWA] Add test that verifies scope parsing from web_app DB matches spec
    WebApp::SetScope already takes care of ensuring that queries and
    fragments are cleared from an url while reading from a WebAppProto, and as such, no extra manipulation is needed while loading the proto data from the WebAppDatabase.
    
    This CL adds a test that verifies this behavior is correctly maintained, and adds some improvement to the testing infrastructure, where queries and fragments are randomly added to the scope while creating a random web app. This shouldn't cause any existing tests to fail if the scope is being correctly parsed.
    
    Fixed: 339701741
    Change-Id: Ifd48e7dadb15caf8b48d8923c718a737d1fb5fa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808763
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346956}

--- chrome/browser/web_applications/web_app_database.cc
+++ chrome/browser/web_applications/web_app_database.cc
@@ -1094,6 +1094,9 @@ std::unique_ptr<WebApp> WebAppDatabase::CreateWebApp(
                   << scope.possibly_invalid_spec();
       return nullptr;
     }
+
+    // WebApp::SetScope() takes care of removing the queries and fragments from
+    // the scope before storing it in memory.
     web_app->SetScope(scope);
   }
 
