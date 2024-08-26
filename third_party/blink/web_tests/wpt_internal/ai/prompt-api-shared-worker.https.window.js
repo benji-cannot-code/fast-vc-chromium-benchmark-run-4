3ddddcee57c2bc61cff5208ed349b1e68f6d681b
Tsuyoshi Horo <horo@chromium.org>
2024-08-26 03:52:30 +0000

    Add timeout=long for tests which wait for the result of AI prompt
    Bug: 361192155
    Change-Id: Ia790374b4d863b566d0818988c0935568a1a6a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811103
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346524}

--- third_party/blink/web_tests/wpt_internal/ai/prompt-api-shared-worker.https.window.js
+++ third_party/blink/web_tests/wpt_internal/ai/prompt-api-shared-worker.https.window.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: timeout=long
+
 promise_test(async () => {
   const result = await new Promise(async resolve => {
     const worker = new SharedWorker("test-shared-worker.js");
--- third_party/blink/web_tests/wpt_internal/ai/prompt-api.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/prompt-api.any.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=resources/utils.js
+// META: timeout=long
 
 promise_test(async () => {
   const result = await testPromptAPI();
