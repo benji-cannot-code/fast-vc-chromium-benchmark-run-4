556a2f396a4777ae4bb4f323b70f0c0cf952f0ed
Mohamed Heikal <mheikal@chromium.org>
2024-08-26 19:18:25 +0000

    Roll Error Prone from YqeN_FF5AQD-nio02... to chEm55HMuY69gy5q0...
    Modification: Remove useless @NonNulls
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://skia-autoroll.corp.goog/r/error-prone-chromium
    Please CC agrieve@google.com,clank-build@google.com,clank-library-failures@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Chromium: https://bugs.chromium.org/p/chromium/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-dbg
    Tbr: clank-library-failures@google.com
    Change-Id: I1c4756f289b60d320748a9d7abac08b5d27616a8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796619
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346890}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/DeferredIMEWindowInsetApplicationCallback.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/DeferredIMEWindowInsetApplicationCallback.java
@@ -93,12 +93,10 @@ public class DeferredIMEWindowInsetApplicationCallback
         mDeferredKeyboardHeight = NO_DEFERRED_KEYBOARD_HEIGHT;
     }
 
-    @NonNull
     @Override
     public void onStart(
             @NonNull WindowInsetsAnimationCompat animation, @NonNull BoundsCompat bounds) {}
 
-    @NonNull
     @Override
     public void onProgress(
             @NonNull WindowInsetsCompat windowInsetsCompat,
