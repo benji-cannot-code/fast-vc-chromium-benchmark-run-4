bbfcdd3871c7bd264845fadaa1f3e4dadb41485b
Stephen McGruer <smcgruer@chromium.org>
2024-08-26 20:59:45 +0000

    Make full server cards NOTREACHED in shouldEditCardFromPaymentsWebPage
    Full server cards are a temporary cached state and should not be offered
    for edit (from payments web page or otherwise).
    
    Bug: 40939195
    Change-Id: I7967f7f1d7252e85c8b36979b0aff6fb09352155
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807513
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346955}

--- ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_util.mm
+++ ios/chrome/browser/autofill/ui_bundled/autofill_credit_card_util.mm
@@ -107,11 +107,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 + (BOOL)shouldEditCardFromPaymentsWebPage:(const autofill::CreditCard*)card {
   switch (card->record_type()) {
     case autofill::CreditCard::RecordType::kLocalCard:
-    case autofill::CreditCard::RecordType::kFullServerCard:
     case autofill::CreditCard::RecordType::kVirtualCard:
       return NO;
     case autofill::CreditCard::RecordType::kMaskedServerCard:
       return YES;
+    case autofill::CreditCard::RecordType::kFullServerCard:
+      // Full server cards are a temporary cached state and should not be
+      // offered for edit (from payments web page or otherwise).
+      NOTREACHED();
   }
 }
 
