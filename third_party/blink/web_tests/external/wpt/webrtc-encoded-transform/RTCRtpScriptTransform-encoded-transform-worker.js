c60ce465701e32c39aefef802663791c3490b4e3
Lucia <alonsomozol@google.com>
2024-08-26 15:22:54 +0000

    [EncodedTransform] Fix comma in error message
    Bug: 354881878
    
    Change-Id: I6d8a53aca0b1141d22dd77e0f4385496baaf9e29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811965
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Commit-Queue: Lucía Alonso Mozo <alonsomozol@google.com>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346742}

--- third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/RTCRtpScriptTransform-encoded-transform-worker.js
+++ third_party/blink/web_tests/external/wpt/webrtc-encoded-transform/RTCRtpScriptTransform-encoded-transform-worker.js
@@ -27,7 +27,7 @@ function RestoreAndWrite(chunk, transformer) {
             ", expected " + modification +
             "),  frame data length (got " + frameData.byteLength +
             ", expected 0), rtpTimestamp (got " +
-             chunk.value.getMetadata().rtpTimestamp, ", expected null)");
+             chunk.value.getMetadata().rtpTimestamp + ", expected null)");
     }
 }
 onrtctransform = (event) => {
