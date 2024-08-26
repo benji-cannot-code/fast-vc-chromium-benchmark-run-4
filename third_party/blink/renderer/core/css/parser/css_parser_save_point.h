39dd0d71f53892d0090f22428b75d7dc58defed6
Steinar H. Gunderson <sesse@chromium.org>
2024-08-26 12:15:39 +0000

    Remove the CSSParserSavePoint overload.
    We no longer use this with the non-streaming parser.
    
    Change-Id: I27dfafb85d4759f065efb00a8a7b49df82ebafa4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807498
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346653}

--- third_party/blink/renderer/core/css/parser/css_parser_save_point.h
+++ third_party/blink/renderer/core/css/parser/css_parser_save_point.h
@@ -14,24 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-// A generic RAII helper that allows you to rewind the parser if needed
+// A RAII helper that allows you to rewind the parser if needed
 // (e.g., if parsing fails after you've already consumed a few tokens).
-// It is written generically so that it works for both the streaming
-// and non-streaming parser with the same syntax.
-//
-// Rewind happens automatically in the destructor, unless you've called
-// Release() to commit to the position in the stream.
-template <class T>
-  requires std::is_same_v<T, CSSParserTokenStream> ||
-           std::is_same_v<T, CSSParserTokenRange>
-class CSSParserSavePoint;
-
-// Deduction guide to pick the correct template.
-template <class T>
-CSSParserSavePoint(T& stream_or_range) -> CSSParserSavePoint<T>;
-
-template <>
-class CSSParserSavePoint<CSSParserTokenStream> {
+class CSSParserSavePoint {
   STACK_ALLOCATED();
 
  public:
@@ -53,31 +38,6 @@ class CSSParserSavePoint<CSSParserTokenStream> {
  private:
   CSSParserTokenStream& stream_;
   CSSParserTokenStream::State savepoint_;
-  bool released_ = false;
-};
-
-template <>
-class CSSParserSavePoint<CSSParserTokenRange> {
-  STACK_ALLOCATED();
-
- public:
-  explicit CSSParserSavePoint(CSSParserTokenRange& range)
-      : range_(range), saved_range_(range) {}
-
-  ~CSSParserSavePoint() {
-    if (!released_) {
-      range_ = saved_range_;
-    }
-  }
-
-  void Release() {
-    DCHECK(!released_);
-    released_ = true;
-  }
-
- private:
-  CSSParserTokenRange& range_;
-  CSSParserTokenRange saved_range_;
   bool released_ = false;
 };
 
