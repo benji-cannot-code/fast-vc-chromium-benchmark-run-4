ab0e66deb5b19eba65d529b4a108f2b0c0636b19
James Lee <ljjlee@google.com>
2024-08-26 16:28:20 +0000

    Remove unused KidsManagement URLs from supervised_user_constants.
    Change-Id: I364e7c0149600a2315b98f4f3252973d74c76bd7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813127
    Auto-Submit: James Lee <ljjlee@google.com>
    Commit-Queue: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346774}

--- components/supervised_user/core/common/supervised_user_constants.h
+++ components/supervised_user/core/common/supervised_user_constants.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "ui/base/page_transition_types.h"
-#include "url/gurl.h"
 
 namespace supervised_user {
 
@@ -117,11 +116,6 @@ extern const char* const kCustodianInfoPrefs[10];
 extern const base::FilePath::CharType kSupervisedUserSettingsFilename[];
 
 extern const char kSyncGoogleDashboardURL[];
-
-// URLs for RPCs in the KidsManagement service.
-GURL KidsManagementGetFamilyMembersURL();
-GURL KidsManagementPermissionRequestsURL();
-GURL KidsManagementClassifyURLRequestURL();
 
 // Histogram name to log FamilyLink user type segmentation.
 extern const char kFamilyLinkUserLogSegmentHistogramName[];
