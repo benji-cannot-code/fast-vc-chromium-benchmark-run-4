93fae0c9e5a4463416acd1755df00b4d2c9122a7
Thomas Quintanilla <tquintanilla@google.com>
2024-08-26 12:51:45 +0000

    Adds attribution scopes data column in internals UI
    We render the scopes data using its native JSON serialization for
    simplicity.
    
    Screenshot can be found at https://issues.chromium.org/357041458
    
    Bug: 344081621
    Change-Id: I7ce0b2ca0b0490e8f2b38dcd49d35d561f6f1f4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5753317
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346675}

--- content/browser/resources/attribution_reporting/attribution_internals.ts
+++ content/browser/resources/attribution_reporting/attribution_internals.ts
@@ -207,6 +207,7 @@ interface Source {
   debugCookieSet: boolean;
   remainingAggregatableDebugBudget: number;
   aggregatableDebugKeyPiece: string;
+  attributionScopesData: string;
 }
 
 function newSource(mojo: WebUISource): Source {
@@ -237,6 +238,7 @@ function newSource(mojo: WebUISource): Source {
     debugCookieSet: mojo.debugCookieSet,
     remainingAggregatableDebugBudget: mojo.remainingAggregatableDebugBudget,
     aggregatableDebugKeyPiece: mojo.aggregatableDebugKeyPiece,
+    attributionScopesData: mojo.attributionScopesDataJson,
   };
 }
 
@@ -265,6 +267,7 @@ function initSourceTable(panel: HTMLElement):
         valueColumn('Filter Data', 'filterData', asCode),
         valueColumn('Debug Cookie Set', 'debugCookieSet', asStringOrBool),
         'Event-Level Fields',
+        valueColumn('Attribution Scopes Data', 'attributionScopesData', asCode),
         valueColumn(
             'Epsilon', 'eventLevelEpsilon',
             asCustomNumber((v: number) => v.toFixed(3))),
