82525672b5cfdef549a2b0a3f272d034b2da633d
Colin Blundell <blundell@chromium.org>
2024-08-26 08:46:51 +0000

    [Blink] Remove CanvasResourceProvider::GetBackingMailboxForOverwrite()
    Have last caller call GetBackingClientSharedImageForOverwrite().
    
    NOTE: As part of this CL I noticed an oddity with the existing code,
    which I've added a TODO for resolving.
    
    Bug: 352263194
    Change-Id: I122108feb6070689a5022c0247666cf1975c8901
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5735390
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346596}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.h
@@ -229,10 +229,6 @@ class PLATFORM_EXPORT CanvasResourceProvider
     NOTREACHED_IN_MIGRATION();
     return nullptr;
   }
-  virtual gpu::Mailbox GetBackingMailboxForOverwrite() {
-    NOTREACHED_IN_MIGRATION();
-    return gpu::Mailbox();
-  }
   virtual void* GetPixelBufferAddressForOverwrite() {
     NOTREACHED_IN_MIGRATION();
     return nullptr;
