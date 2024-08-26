9f2d1a09f7cf88fc092e0e38132642978d6612c7
Sam Davis Omekara (from Dev Box) <samomekarajr@microsoft.com>
2024-08-26 19:29:44 +0000

    [Masonry] Introduce parsing for masonry-track shorthand
    This CL introduces the parsing for css masonry property:
    masonry-track
    
    This is part of: https://chromestatus.com/feature/5149560434589696
    
    The properties are behind the CSSMasonryLayout feature flag.
    
    Bug: 343257585
    Change-Id: Id435852bc7b9c178b885d49feee3dcf0fd90a2a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5798158
    Commit-Queue: Sam Davis Omekara <samomekarajr@microsoft.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Alison Maher <almaher@microsoft.com>
    Reviewed-by: Ethan Jimenez <ethavar@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1346899}

--- third_party/blink/renderer/core/css/css_property_equality.cc
+++ third_party/blink/renderer/core/css/css_property_equality.cc
@@ -1239,6 +1239,7 @@ bool CSSPropertyEquality::PropertiesEqual(const PropertyHandle& property,
     case CSSPropertyID::kMargin:
     case CSSPropertyID::kMarker:
     case CSSPropertyID::kMask:
+    case CSSPropertyID::kMasonryTrack:
     case CSSPropertyID::kOffset:
     case CSSPropertyID::kOutline:
     case CSSPropertyID::kOverflow:
