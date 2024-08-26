e277ffefa9272d16a804cf15d4b4f0f034534c72
Peter Boström <pbos@chromium.org>
2024-08-26 16:46:15 +0000

    Migrate to NOTREACHED() in pdf/
    NOTREACHED() and NOTREACHED_IN_MIGRATION() are both CHECK-fatal now.
    The former is [[noreturn]] so this CL also performs dead-code removal
    after the NOTREACHED().
    
    This CL does not attempt to do additional rewrites of any surrounding
    code, like:
    
    if (!foo) {
      NOTREACHED();
    }
    
    to CHECK(foo);
    
    Those transforms take a non-trivial amount of time (and there are
    thousands of instances). Cleanup can be left as an exercise for the
    reader.
    
    Bug: 40580068
    Low-Coverage-Reason: OTHER Should-be-unreachable code
    Change-Id: I0d9e7d1dc3324cdb0fd4b3ce3ce9a7ee2c6079ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802701
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346787}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -1540,10 +1540,10 @@ void PdfViewWebPlugin::HandleSaveMessage(const base::Value::Dict& message) {
       // using the plugin data.
       pdf_host_->SetPluginCanSave(true);
       SaveToBuffer(token);
-#else
-      NOTREACHED_IN_MIGRATION();
-#endif  // BUILDFLAG(ENABLE_INK)
       break;
+#else
+      NOTREACHED();
+#endif  // BUILDFLAG(ENABLE_INK)
     case SaveRequestType::kOriginal:
       pdf_host_->SetPluginCanSave(false);
       SaveToFile(token);
@@ -1737,7 +1737,7 @@ void PdfViewWebPlugin::SaveToBuffer(const std::string& token) {
         data_to_save = base::Value(std::move(data));
     }
 #else
-    NOTREACHED_IN_MIGRATION();
+    NOTREACHED();
 #endif  // BUILDFLAG(ENABLE_INK)
   }
 
@@ -1994,8 +1994,7 @@ void PdfViewWebPlugin::UpdateScaledValues() {
 
 void PdfViewWebPlugin::UpdateScale(float scale) {
   if (scale <= 0.0f) {
-    NOTREACHED_IN_MIGRATION();
-    return;
+    NOTREACHED();
   }
 
   viewport_to_dip_scale_ = scale;
--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -348,8 +348,7 @@ std::string ConvertViewIntToViewString(unsigned long view_int) {
     case PDFDEST_VIEW_UNKNOWN_MODE:
       return "";
     default:
-      NOTREACHED_IN_MIGRATION();
-      return "";
+      NOTREACHED();
   }
 }
 
@@ -493,8 +492,7 @@ void ParamsTransformPageToScreen(unsigned long view_fit_type,
     case PDFDEST_VIEW_UNKNOWN_MODE:
       break;
     default:
-      NOTREACHED_IN_MIGRATION();
-      break;
+      NOTREACHED();
   }
 }
 
@@ -2322,8 +2320,7 @@ void PDFiumEngine::HandleAccessibilityAction(
       break;
     }
     case AccessibilityAction::kNone:
-      NOTREACHED_IN_MIGRATION();
-      break;
+      NOTREACHED();
   }
 }
 
