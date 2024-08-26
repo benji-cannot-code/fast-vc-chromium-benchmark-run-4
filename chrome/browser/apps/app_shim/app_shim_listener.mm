a7cda436e348469351e2bd75807d78217723b058
Mark Rowe <markrowe@chromium.org>
2024-08-26 18:19:56 +0000

    Identify app shim processes by audit tokens for signature validation
    The signature validation functions that take a process ID will be going
    away in the future. The application that a process ID represents can
    change if it execs a new binary or if the process exits and the kernel
    reuses the PID. Audit tokens uniquely identify a specific process and
    are the correct choice when making trust decisions.
    
    This primarily involves updating AppShimHostBootstrap to store an audit
    token and plumbing it up from MachBootstrapAcceptor::HandleRequest. Most
    clients of AppShimHostBootstrap have been left working with the PID.
    AppShimManager validates the app shim code signature so it is updated to
    use the audit token.
    
    Bug: 361784552
    Change-Id: I6da1a106429d5af5f27ce9430f2e5d866b0ea0d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808009
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346854}

--- chrome/browser/apps/app_shim/app_shim_listener.mm
+++ chrome/browser/apps/app_shim/app_shim_listener.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_shim/app_shim_listener.h"
 
 #import <Foundation/Foundation.h>
+#include <bsm/libbsm.h>
 #include <unistd.h>
 
 #include "base/check_op.h"
@@ -100,14 +101,15 @@ void AppShimListener::InitOnBackgroundThread() {
 }
 
 void AppShimListener::OnClientConnected(mojo::PlatformChannelEndpoint endpoint,
-                                        base::ProcessId peer_pid) {
+                                        audit_token_t audit_token) {
   // TODO(crbug.com/40674145): Remove NSLog logging, and move to an
   // internal debugging URL.
-  NSLog(@"AppShim: Connection received from pid %d", peer_pid);
+  NSLog(@"AppShim: Connection received from pid %d",
+        audit_token_to_pid(audit_token));
   content::GetUIThreadTaskRunner({})->PostTask(
       FROM_HERE,
-      base::BindOnce(&AppShimHostBootstrap::CreateForChannelAndPeerID,
-                     std::move(endpoint), peer_pid));
+      base::BindOnce(&AppShimHostBootstrap::CreateForChannelAndPeerAuditToken,
+                     std::move(endpoint), audit_token));
 }
 
 void AppShimListener::OnServerChannelCreateError() {
--- chrome/browser/apps/app_shim/mach_bootstrap_acceptor_unittest.mm
+++ chrome/browser/apps/app_shim/mach_bootstrap_acceptor_unittest.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_shim/mach_bootstrap_acceptor.h"
 
+#include <bsm/libbsm.h>
+
 #include "base/process/process_handle.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
@@ -41,9 +43,9 @@ class TestMachBootstrapAcceptorDelegate
       : quit_closure_(std::move(quit_closure)) {}
 
   void OnClientConnected(mojo::PlatformChannelEndpoint endpoint,
-                         base::ProcessId pid) override {
+                         audit_token_t audit_token) override {
     endpoint_ = std::move(endpoint);
-    pid_ = pid;
+    pid_ = audit_token_to_pid(audit_token);
     std::move(quit_closure_).Run();
   }
 
