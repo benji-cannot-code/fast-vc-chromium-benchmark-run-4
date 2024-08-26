3768349dbc7b480c2d4dca8ee034998c10fa3faf
Sophie Wen <sophiewen@google.com>
2024-08-26 23:10:54 +0000

    sunfish: Show search results panel on drag release
    This CL hooks up the search results panel to be shown immediately when
    a region is selected. In production, the panel might not be shown until
    the results URL is fetched, but this immediate binding helps with
    debugging.
    
    This also sets the image data to be shown in the search box for UI
    debugging.
    
    This also parents the panel widget to the overlay container so it can
    be stacked above the capture mode overlay.
    
    Demo: https://b.corp.google.com/issues/359317857#comment3
    
    Design: https://docs.google.com/document/d/1qYhaEC99x_Jnk0E7ktygSxqSdT4ihCSDO38f0z6RuQk/edit?resourcekey=0-lbfvLBs73jcGMkuqfS7vIw&tab=t.0#bookmark=id.d11je2ppmwaj
    
    Test: manual + modified
    Bug: b/359317857
    Change-Id: I7d2fe4a2a33e295528cb8972905c5f0afece2edf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804838
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347040}

--- ash/capture_mode/search_results_panel.h
+++ ash/capture_mode/search_results_panel.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 class AshWebView;
+class SunfishSearchBoxView;
 
 // Container for the search results view and other UI such as the search box,
 // close button, etc.
@@ -27,13 +28,14 @@ class ASH_EXPORT SearchResultsPanel : public views::View {
   SearchResultsPanel& operator=(const SearchResultsPanel&) = delete;
   ~SearchResultsPanel() override;
 
-  // Currently only used in tests.
-  static std::unique_ptr<views::Widget> CreateWidget();
+  static std::unique_ptr<views::Widget> CreateWidget(aura::Window* const root);
 
-  AshWebView* search_results_view() const { return search_results_view_; }
+  // Sets the search box image thumbnail.
+  void SetSearchBoxImage(const gfx::ImageSkia& image);
 
  private:
   // Owned by the views hierarchy.
+  raw_ptr<SunfishSearchBoxView> search_box_view_;
   raw_ptr<AshWebView> search_results_view_;
 };
 
