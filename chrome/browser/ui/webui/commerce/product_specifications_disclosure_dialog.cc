b4f4e6cf26d64536d3531666d735291a64583044
Yue Zhang <yuezhanggg@chromium.org>
2024-08-26 21:21:27 +0000

    [Compare] Fix disclosure scroll bar
    On Mac, the dialog width is not large enough that one item in the
    disclosure got broken into two lines which is different from expected
    UI and leading to a scroll bar. This CL fixes this issue by making
    the dialog slightly wider so that that item can fit in one line.
    
    Before: https://screenshot.googleplex.com/48osrZcuqvmnSdh
    After: https://screenshot.googleplex.com/4x5SvyApeFfiwEy
    
    Bug: 362290369
    Change-Id: Idb85daedbd05edcda2ed4b07bb867f247e1b3cc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813473
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346965}

--- chrome/browser/ui/webui/commerce/product_specifications_disclosure_dialog.cc
+++ chrome/browser/ui/webui/commerce/product_specifications_disclosure_dialog.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-constexpr gfx::Size kDialogSize{400, 350};
+constexpr gfx::Size kDialogSize{410, 350};
 
 void UpdateDialogPosition(views::Widget* widget,
                           content::WebContents* web_contents) {
