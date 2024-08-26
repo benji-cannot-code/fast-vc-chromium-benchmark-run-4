c6e7343c7c1c723c5440ac314104251109207370
Ewann Pelle <ewannpv@chromium.org>
2024-08-26 13:06:47 +0000

    [iOS] Removes `_dragItemID` from tab mediators
    Removing the `_dragItemID` prevents unnecessary calls to
    `GetWebStateIndex:` during drag events. This improves performance.
    If a dragged item is closed during the drag, the drop operation
    won't be updated correctly.
    If the user drop a closed tab, it will try to move it to the current
    browser. However, since the tab no longer exists, the drop operation will fail and be cancelled.
    See: https://crsrc.org/c/ios/chrome/browser/shared/model/web_state_list/browser_util.mm;l=92?q=%22if%20(!tab_info.browser)%22
    
    Some methods within the `TabCollectionDragDropHandler` protocol have
    been made optional. This provides greater flexibility for different tab
    collection implementations, allowing them to avoid implementing methods
    that aren't relevant to their specific use case.
    
    Fixed: 361289705
    Change-Id: I9ab8846823193542ec333c620d5fe88840ded0f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812022
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346683}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_mediator.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/coordinator/tab_strip_mediator.mm
@@ -201,9 +201,6 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
   // Browser list.
   BrowserList* _browserList;
 
-  // ItemID of the dragged tab. Used to check if the dropped tab is from the
-  // same Chrome window.
-  web::WebStateID _dragItemID;
   // List of items in the tab strip when a drag operation starts.
   // Should be set back to `nil` when the drag operation ends.
   NSMutableArray<TabStripItemIdentifier*>* _dragItems;
@@ -889,7 +886,6 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
 }
 
 - (void)dragWillBeginForTabSwitcherItem:(TabSwitcherItem*)item {
-  _dragItemID = item.identifier;
   _dragItems = CreateItemIdentifiers(_webStateList,
                                      /*including_hidden_tab_items=*/false);
   // When a tab is dragged, it is visually removed from the collection view.
@@ -911,7 +907,6 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
 }
 
 - (void)dragSessionDidEnd {
-  _dragItemID = web::WebStateID();
   _dragItems = nil;
 }
 
@@ -1112,11 +1107,6 @@ NSMutableArray<TabStripItemIdentifier*>* CreateItemIdentifiers(
         });
       };
   [itemProvider loadObjectOfClass:[NSURL class] completionHandler:loadHandler];
-}
-
-- (NSArray<UIDragItem*>*)allSelectedDragItems {
-  NOTREACHED() << "You should not be able to drag and drop multiple "
-                  "items. There is no selection mode in tab strip.";
 }
 
 #pragma mark - Private
