d8cd83f4fbc20fc19360ea2ded87057bdaa5744b
Zhenyao Mo <zmo@chromium.org>
2024-08-26 23:34:04 +0000

    Remove allow_unsafe_buffers from gl_bindings_autogen_[gl|egl].cc
    -- remove memset() and memcmp() calls in the process
    
    TEST=bots compile
    R=tsepez@chromium.org
    
    Bug: 40285824
    Change-Id: I993a7d423245cf02246eb7641152c241bcc75b69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813547
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347058}

--- ui/gl/gl_bindings_autogen_egl.cc
+++ ui/gl/gl_bindings_autogen_egl.cc
@@ -9,13 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <string>
 
+#include "base/containers/span.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
@@ -29,10 +25,13 @@ namespace gl {
 DriverEGL g_driver_egl;  // Exists in .bss
 
 void DriverEGL::InitializeStaticBindings() {
+#if DCHECK_IS_ON()
   // Ensure struct has been zero-initialized.
-  char* this_bytes = reinterpret_cast<char*>(this);
-  DCHECK(this_bytes[0] == 0);
-  DCHECK(memcmp(this_bytes, this_bytes + 1, sizeof(*this) - 1) == 0);
+  auto bytes = base::byte_span_from_ref(*this);
+  for (auto byte : bytes) {
+    DCHECK_EQ(0, byte);
+  };
+#endif
 
   fn.eglAcquireExternalContextANGLEFn =
       reinterpret_cast<eglAcquireExternalContextANGLEProc>(
@@ -427,7 +426,8 @@ void DisplayExtensionsEGL::InitializeExtensionSettings(EGLDisplay display) {
 }
 
 void DriverEGL::ClearBindings() {
-  memset(this, 0, sizeof(*this));
+  auto bytes = base::byte_span_from_ref(*this);
+  std::ranges::fill(bytes, 0);
 }
 
 void EGLApiBase::eglAcquireExternalContextANGLEFn(EGLDisplay dpy,
--- ui/gl/gl_bindings_autogen_gl.cc
+++ ui/gl/gl_bindings_autogen_gl.cc
@@ -9,13 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //    clang-format -i -style=chromium filename
 // DO NOT EDIT!
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include <string>
 
+#include "base/containers/span.h"
 #include "base/trace_event/trace_event.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
@@ -27,10 +23,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gl {
 
 void DriverGL::InitializeStaticBindings() {
+#if DCHECK_IS_ON()
   // Ensure struct has been zero-initialized.
-  char* this_bytes = reinterpret_cast<char*>(this);
-  DCHECK(this_bytes[0] == 0);
-  DCHECK(memcmp(this_bytes, this_bytes + 1, sizeof(*this) - 1) == 0);
+  auto bytes = base::byte_span_from_ref(*this);
+  for (auto byte : bytes) {
+    DCHECK_EQ(0, byte);
+  };
+#endif
 
   fn.glActiveTextureFn = reinterpret_cast<glActiveTextureProc>(
       GetGLProcAddress("glActiveTexture"));
@@ -2560,7 +2559,8 @@ void DriverGL::InitializeDynamicBindings(const GLVersionInfo* ver,
 }
 
 void DriverGL::ClearBindings() {
-  memset(this, 0, sizeof(*this));
+  auto bytes = base::byte_span_from_ref(*this);
+  std::ranges::fill(bytes, 0);
 }
 
 void GLApiBase::glAcquireTexturesANGLEFn(GLuint numTextures,
@@ -13540,21 +13540,6 @@ void LogGLApi::glShaderSourceFn(GLuint shader,
                                   << static_cast<const void*>(str) << ", "
                                   << static_cast<const void*>(length) << ")");
   gl_api_->glShaderSourceFn(shader, count, str, length);
-
-  GL_SERVICE_LOG_CODE_BLOCK({
-    for (GLsizei ii = 0; ii < count; ++ii) {
-      if (str[ii]) {
-        if (length && length[ii] >= 0) {
-          std::string source(str[ii], length[ii]);
-          GL_SERVICE_LOG("  " << ii << ": ---\n" << source << "\n---");
-        } else {
-          GL_SERVICE_LOG("  " << ii << ": ---\n" << str[ii] << "\n---");
-        }
-      } else {
-        GL_SERVICE_LOG("  " << ii << ": NULL");
-      }
-    }
-  });
 }
 
 void LogGLApi::glSignalSemaphoreEXTFn(GLuint semaphore,
