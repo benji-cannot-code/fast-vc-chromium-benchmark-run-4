cbac28107d12e9d3826d911562a8eaaea2d6a03f
Christine Ying <chrstne@google.com>
2024-08-26 21:30:54 +0000

    [PrivacyPolicy] Create link from Topics notice to Privacy Policy page.
    Behavior is hardcoded behind a flag for now.
    Command to build & run
    
    ```
    autoninja -C out/Default chrome && out/Default/chrome --user-data-dir=$(mktemp -d) --enable-features="PrivacySandboxSettings4:force-show-consent-for-testing/true/consent-required/true,PrivacySandboxPrivacyPolicy"
    ```
    
    Screencast: https://screencast.googleplex.com/cast/NTI0NTg3NjY1NzE5Mjk2MHw3YjdiNGNhNi02NA
    Change-Id: I7eeb2c6757358a60d9c92d9e50d26094c91211e9
    Bug: 358087159
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5801150
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Christine Ying <chrstne@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346975}

--- chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.cc
+++ chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.cc
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/test/base/web_ui_mocha_browser_test.h"
+#include "components/privacy_sandbox/privacy_sandbox_features.h"
 #include "content/public/test/browser_test.h"
 #include "ui/views/widget/widget.h"
 
 class PrivacySandboxDialogTest : public WebUIMochaBrowserTest {
  protected:
   PrivacySandboxDialogTest() {
+    feature_list()->InitAndEnableFeature(
+        privacy_sandbox::kPrivacySandboxPrivacyPolicy);
     set_test_loader_host(chrome::kChromeUIPrivacySandboxDialogHost);
   }
 
@@ -21,6 +24,11 @@ class PrivacySandboxDialogTest : public WebUIMochaBrowserTest {
         "privacy_sandbox/privacy_sandbox_dialog_test.js",
         base::StringPrintf("runMochaSuite('%s');", testCase.c_str()));
   }
+
+  base::test::ScopedFeatureList* feature_list() { return &feature_list_; }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
 };
 
 class PrivacySandboxDialogSmallWindowTest : public PrivacySandboxDialogTest {
