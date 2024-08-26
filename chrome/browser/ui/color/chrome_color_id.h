473438a8b736c178a355e74299e6ae39131d051e
Chase Hartsell <chazzy@chromium.org>
2024-08-26 22:22:34 +0000

    Add buying options section element for product specifications
    This patch adds a buying options section element for displaying a link
    to the jackpot URL for a product.
    
    http://screen/4nfdVNntFhhwgkN
    
    Bug: 361134858
    Change-Id: I916474b7252acede55d63aa4beb0b3b641b803b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807604
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347013}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -407,6 +407,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorProductSpecificationsDivider) \
   E_CPONLY(kColorProductSpecificationsHorizontalCarouselScrollbarThumb) \
   E_CPONLY(kColorProductSpecificationsIcon) \
+  E_CPONLY(kColorProductSpecificationsLink) \
   E_CPONLY(kColorProductSpecificationsPageBackground) \
   E_CPONLY(kColorProductSpecificationsPrimaryTitle) \
   E_CPONLY(kColorProductSpecificationsSecondaryTitle) \
