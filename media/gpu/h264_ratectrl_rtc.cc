ed82b1cf44b7092e0806233a5c23b4f06f21657f
Hang Nguyen <hnt@google.com>
2024-08-26 06:49:11 +0000

    media/gpu: Have H264 SW rate controller return frame drop decision
    This CL introduces the enum FrameDropDecision for H264RateCtrlRTC to
    return the frame drop decision for its method ComputeQP().
    
    Bug: 195407733
    Test: H264RateCtrlRTCTest in media_unittest
    Change-Id: I31723827c46e7108e35ff685c70551cd1a48f87d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5806118
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Hang Nguyen <hnt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346571}

--- media/gpu/h264_ratectrl_rtc.cc
+++ media/gpu/h264_ratectrl_rtc.cc
@@ -109,7 +109,8 @@ void H264RateCtrlRTC::UpdateRateControl(
   config_changed_ = true;
 }
 
-void H264RateCtrlRTC::ComputeQP(const H264FrameParamsRTC& frame_params) {
+H264RateCtrlRTC::FrameDropDecision H264RateCtrlRTC::ComputeQP(
+    const H264FrameParamsRTC& frame_params) {
   DVLOG(3) << "Compute QP - "
            << "temporal_layer_id: " << frame_params.temporal_layer_id
            << ", timestamp: " << frame_params.timestamp.InMilliseconds()
@@ -188,7 +189,7 @@ void H264RateCtrlRTC::ComputeQP(const H264FrameParamsRTC& frame_params) {
     frame_qp_ = -1;  // Drop frame.
     DVLOG(3) << "Rate controller estimated QP: " << frame_qp_
              << " - frame drop";
-    return;
+    return FrameDropDecision::kDrop;
   }
 
   frame_qp_ =
@@ -196,6 +197,7 @@ void H264RateCtrlRTC::ComputeQP(const H264FrameParamsRTC& frame_params) {
                  static_cast<int>(h264_rate_control_util::kQPMax));
 
   DVLOG(3) << "Rate controller estimated QP: " << frame_qp_;
+  return FrameDropDecision::kOk;
 }
 
 int H264RateCtrlRTC::GetQP() {
