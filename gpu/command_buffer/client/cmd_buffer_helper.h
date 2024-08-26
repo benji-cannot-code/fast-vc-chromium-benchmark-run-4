e195b3dd8120e9ef22b5e63722e6909fe60d0217
Yuzhu Shen <yzshen@chromium.org>
2024-08-26 21:54:40 +0000

    CommandBufferHelper: InsertFenceSync fix
    Previous code has the following problems:
    1) if allocating an InsertFenceSync command fails, the
       generated sync tokens are not released;
    2) if a flush happens while allocating the InsertFenceSync
       command, the flush reports the release count of the
       generated sync token, while the InsertFenceSync command
       corresponding to the release count is not yet added.
    
    Currently the problems are not causing trouble because the
    release count reported hasn't yet been checked and enforced,
    which is going to change with the work of switching to
    graph-based sync point validation.
    
    This change makes sure that a sync token is generated only
    after allocating the InsertFenceSync command.
    
    Bug: b/324276400
    Change-Id: I6b0c09200098de79ee5f21f00ef1b07aec6d3511
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813493
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346988}

--- gpu/command_buffer/client/cmd_buffer_helper.h
+++ gpu/command_buffer/client/cmd_buffer_helper.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string.h>
 
 #include "base/check_op.h"
+#include "base/functional/function_ref.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/time/time.h"
@@ -263,11 +264,27 @@ class GPU_EXPORT CommandBufferHelper {
     }
   }
 
-  void InsertFenceSync(uint64_t release_count) {
-    cmd::InsertFenceSync* c = GetCmdSpace<cmd::InsertFenceSync>();
-    if (c) {
-      c->Init(release_count);
+  uint64_t InsertFenceSync(base::FunctionRef<uint64_t()> sync_token_generator) {
+    cmd::InsertFenceSync* cmd = GetCmdSpace<cmd::InsertFenceSync>();
+
+    // Please note that it is important to generate the sync token after
+    // GetCmdSpace().
+    // 1) If InsertFenceSync command `cmd` is not successfully allocated, a sync
+    //    token shouldn't be created either. Otherwise, it results in waiting
+    //    for a fence sync that is never released.
+    // 2) Even if `cmd` is successfully allocated, we still need to generate the
+    //    sync token afterwards: The GetCmdSpace() call may result in a flush of
+    //    the command buffer. On the other hand, command buffer implementations
+    //    (such as CommandBufferProxyImpl) may assume that when a flush happens,
+    //    the commands releasing the previously-generated sync tokens are
+    //    already in the buffer and thus all flushed.
+    if (cmd) {
+      uint64_t release_count = sync_token_generator();
+      cmd->Init(release_count);
+      return release_count;
     }
+
+    return 0;
   }
 
   CommandBuffer* command_buffer() const { return command_buffer_; }
