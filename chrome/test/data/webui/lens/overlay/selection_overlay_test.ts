9744da2008bd6719b705527beb3b476ef9181d85
Justin Donnelly <jdonnelly@chromium.org>
2024-08-26 20:10:01 +0000

    [lensoverlay] Re-enable selection overlay browser tests on Linux.
    I think these were fixed by some tweaks I made in a later patchset on crrev.com/c/5803848. Those changes were intended to fix CrOS but may have helped on Linux as well. I'll monitor the bots* after this lands to make sure they're not flaky.
    
    * https://ci.chromium.org/ui/test/chromium/ninja%3A%2F%2Fchrome%2Ftest%3Abrowser_tests%2FLensOverlayTest.SelectionOverlay
    
    Bug: b:360923748
    Change-Id: I7cdeb21ff1ee5188e5ae271705383ca76507edf1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812849
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1346927}

--- chrome/test/data/webui/lens/overlay/selection_overlay_test.ts
+++ chrome/test/data/webui/lens/overlay/selection_overlay_test.ts
@@ -14,10 +14,7 @@ import type {OverlayObject} from 'chrome-untrusted://lens/overlay_object.mojom-w
 import {ScreenshotBitmapBrowserProxyImpl} from 'chrome-untrusted://lens/screenshot_bitmap_browser_proxy.js';
 import type {SelectionOverlayElement} from 'chrome-untrusted://lens/selection_overlay.js';
 import {loadTimeData} from 'chrome-untrusted://resources/js/load_time_data.js';
-import {assertDeepEquals, assertEquals, assertNotEquals, assertStringContains} from 'chrome-untrusted://webui-test/chai_assert.js';
-// <if expr="not is_linux">
-import {assertFalse, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
-// </if>
+import {assertDeepEquals, assertEquals, assertFalse, assertNotEquals, assertStringContains, assertTrue} from 'chrome-untrusted://webui-test/chai_assert.js';
 import {flushTasks, waitAfterNextRender} from 'chrome-untrusted://webui-test/polymer_test_util.js';
 
 import {fakeScreenshotBitmap, waitForScreenshotRendered} from '../utils/image_utils.js';
@@ -279,7 +276,7 @@ suite('SelectionOverlay', function() {
         verifyRegionRequest(expectedRect, /*expectedIsClick=*/ false);
       });
 
-  // <if expr="not chromeos_lacros and not is_linux">
+  // <if expr="not chromeos_lacros">
   test(
       'verify that region search over text triggers detected text context menu',
       async () => {
@@ -490,7 +487,6 @@ suite('SelectionOverlay', function() {
     assertEquals(67, imageSize.height);
   });
 
-  // <if expr="not is_linux">
   test('verify that you can drag text over post selection', async () => {
     // Add the words
     await addWords();
@@ -610,7 +606,6 @@ suite('SelectionOverlay', function() {
         assertTrue(
             selectionOverlayElement.getShowSelectedTextContextMenuForTesting());
       });
-  // </if>
 
   test(
       'verify that dragging on post selection over an object does not tap that object',
