b7644239c85089472aae8bf971f32d2e703df122
Gyuyoung Kim <gyuyoung@igalia.com>
2024-08-26 23:19:47 +0000

    [CodeHealth][BackgroundFetch] Add curly braces as per the latest style guide
    Add a curly brace when the body is only one line.
    
    Bug: 346952289
    Change-Id: Ib516763d23494ee79b4a5f7f354c1e8b6f9e79e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808629
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1347046}

--- components/background_fetch/background_fetch_delegate_base.cc
+++ components/background_fetch/background_fetch_delegate_base.cc
@@ -132,8 +132,9 @@ void BackgroundFetchDelegateBase::DownloadUrl(
 void BackgroundFetchDelegateBase::PauseDownload(const std::string& job_id) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   JobDetails* job_details = GetJobDetails(job_id, /*allow_null=*/true);
-  if (!job_details)
+  if (!job_details) {
     return;
+  }
 
   if (job_details->job_state == JobDetails::State::kDownloadsComplete ||
       job_details->job_state == JobDetails::State::kJobComplete) {
@@ -202,8 +203,9 @@ JobDetails* BackgroundFetchDelegateBase::GetJobDetails(
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   auto job_details_iter = job_details_map_.find(job_id);
   if (job_details_iter == job_details_map_.end()) {
-    if (!allow_null)
+    if (!allow_null) {
       NOTREACHED_IN_MIGRATION();
+    }
 
     return nullptr;
   }
@@ -224,8 +226,9 @@ void BackgroundFetchDelegateBase::Abort(const std::string& job_id) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
   JobDetails* job_details = GetJobDetails(job_id, /*allow_null=*/true);
-  if (!job_details)
+  if (!job_details) {
     return;
+  }
 
   job_details->job_state = JobDetails::State::kCancelled;
 
@@ -271,8 +274,9 @@ void BackgroundFetchDelegateBase::OnDownloadStarted(
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
   // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
   // OnDownload* methods, then this can be a DCHECK.
-  if (download_job_id_iter == download_job_id_map_.end())
+  if (download_job_id_iter == download_job_id_map_.end()) {
     return;
+  }
 
   const std::string& job_id = download_job_id_iter->second;
   JobDetails* job_details = GetJobDetails(job_id);
@@ -296,8 +300,9 @@ void BackgroundFetchDelegateBase::OnDownloadUpdated(
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
   // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
   // OnDownload* methods, then this can be a DCHECK.
-  if (download_job_id_iter == download_job_id_map_.end())
+  if (download_job_id_iter == download_job_id_map_.end()) {
     return;
+  }
 
   const std::string job_id = download_job_id_iter->second;
 
@@ -331,8 +336,9 @@ void BackgroundFetchDelegateBase::OnDownloadFailed(
   // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs
   // potentially calling OnDownloadFailed with a reason other than
   // CANCELLED/ABORTED, we should add a DCHECK here.
-  if (download_job_id_iter == download_job_id_map_.end())
+  if (download_job_id_iter == download_job_id_map_.end()) {
     return;
+  }
 
   const std::string& job_id = download_job_id_iter->second;
   JobDetails* job_details = GetJobDetails(job_id);
@@ -361,8 +367,9 @@ void BackgroundFetchDelegateBase::OnDownloadSucceeded(
   auto download_job_id_iter = download_job_id_map_.find(download_guid);
   // TODO(crbug.com/40546930): When DownloadService fixes cancelled jobs calling
   // OnDownload* methods, then this can be a DCHECK.
-  if (download_job_id_iter == download_job_id_map_.end())
+  if (download_job_id_iter == download_job_id_map_.end()) {
     return;
+  }
 
   const std::string& job_id = download_job_id_iter->second;
   JobDetails* job_details = GetJobDetails(job_id);
@@ -421,12 +428,14 @@ void BackgroundFetchDelegateBase::OnDownloadReceived(
 bool BackgroundFetchDelegateBase::IsGuidOutstanding(
     const std::string& guid) const {
   auto job_id_iter = download_job_id_map_.find(guid);
-  if (job_id_iter == download_job_id_map_.end())
+  if (job_id_iter == download_job_id_map_.end()) {
     return false;
+  }
 
   auto job_details_iter = job_details_map_.find(job_id_iter->second);
-  if (job_details_iter == job_details_map_.end())
+  if (job_details_iter == job_details_map_.end()) {
     return false;
+  }
 
   return base::Contains(
       job_details_iter->second.fetch_description->outstanding_guids, guid);
@@ -436,8 +445,9 @@ void BackgroundFetchDelegateBase::RestartPausedDownload(
     const std::string& download_guid) {
   auto job_it = download_job_id_map_.find(download_guid);
 
-  if (job_it == download_job_id_map_.end())
+  if (job_it == download_job_id_map_.end()) {
     return;
+  }
 
   const std::string& job_id = job_it->second;
 
@@ -530,8 +540,9 @@ void BackgroundFetchDelegateBase::DidGetUploadData(
 base::WeakPtr<content::BackgroundFetchDelegate::Client>
 BackgroundFetchDelegateBase::GetClient(const std::string& job_id) {
   auto it = job_details_map_.find(job_id);
-  if (it == job_details_map_.end())
+  if (it == job_details_map_.end()) {
     return nullptr;
+  }
   return it->second.client;
 }
 
