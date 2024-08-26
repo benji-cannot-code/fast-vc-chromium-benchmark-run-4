bc6b838a8d62c79f13f4ff4a6928aba93d35784c
Nohemi Fernandez <fernandex@chromium.org>
2024-08-26 16:50:19 +0000

    Add runtime check to verify that URL classification applies to supervised users only.
    Bug: 358285869
    Change-Id: I894de2eb4990abed3e9e7f23ca14f050ffe91ae1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797211
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Cr-Commit-Position: refs/heads/main@{#1346792}

--- components/supervised_user/core/browser/supervised_user_url_filter.cc
+++ components/supervised_user/core/browser/supervised_user_url_filter.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/supervised_user/core/browser/kids_chrome_management_url_checker_client.h"
+#include "components/supervised_user/core/browser/supervised_user_capabilities.h"
 #include "components/supervised_user/core/browser/supervised_user_preferences.h"
 #include "components/supervised_user/core/browser/supervised_user_utils.h"
 #include "components/supervised_user/core/common/supervised_user_constants.h"
@@ -776,6 +777,8 @@ bool SupervisedUserURLFilter::RunAsyncChecker(
     return true;
   }
 
+  // The primary account must be supervised to run async URL classification.
+  CHECK(supervised_user::IsSubjectToParentalControls(user_prefs_.get()));
   CHECK(async_url_checker_);
   return async_url_checker_->CheckURL(
       url_matcher::util::Normalize(url),
