770e4cea95904aaeaeab5d9af140edd2e1fd88b4
Bryan Nguyen <nguyenbryan@google.com>
2024-08-27 22:56:27 +0000

    [lensoverlay] Hide translate context menu item when in translate mode.
    Demo:
    https://screencast.googleplex.com/cast/NDYxMTY1NTE3NzYwMTAyNHw4NWUyNDc4MC1kNQ
    
    Bug: b:362507284
    Change-Id: Ida24d13d08767c949cbe0db3f1f8a9285aee1192
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817744
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Bryan Nguyen <nguyenbryan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347693}

--- chrome/browser/resources/lens/overlay/selection_overlay.ts
+++ chrome/browser/resources/lens/overlay/selection_overlay.ts
@@ -37,6 +37,7 @@ import {getTemplate} from './selection_overlay.html.js';
 import {CursorType, DRAG_THRESHOLD, DragFeature, emptyGestureEvent, focusShimmerOnRegion, GestureState, ShimmerControlRequester, unfocusShimmer} from './selection_utils.js';
 import type {GestureEvent, OverlayShimmerFocusedRegion} from './selection_utils.js';
 import type {TextLayerElement} from './text_layer.js';
+import type {TranslateState} from './translate_button.js';
 import {toPercent} from './values_converter.js';
 
 // The amount of margins in pixels to add to the screenshot when the window is
@@ -140,6 +141,10 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
         type: Boolean,
         reflectToAttribute: true,
       },
+      showTranslateContextMenuItem: {
+        type: Boolean,
+        reflectToAttribute: true,
+      },
       showSelectedTextContextMenu: {
         type: Boolean,
         value: false,
@@ -197,6 +202,7 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
   // Whether the selection overlay is its initial size, or has changed size.
   private isResized: boolean = false;
   private isInitialSize: boolean = true;
+  private showTranslateContextMenuItem: boolean = true;
   private showSelectedTextContextMenu: boolean;
   private showDetectedTextContextMenu: boolean;
   // Location at which to show the context menus.
@@ -284,6 +290,11 @@ export class SelectionOverlayElement extends SelectionOverlayElementBase {
           } else {
             this.resetCursor();
           }
+        });
+    this.eventTracker_.add(
+        document, 'translate-mode-state-changed',
+        (e: CustomEvent<TranslateState>) => {
+          this.showTranslateContextMenuItem = !e.detail.translateModeEnabled;
         });
     this.eventTracker_.add(
         document, 'show-selected-text-context-menu',
