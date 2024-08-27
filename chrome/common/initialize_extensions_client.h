ae7cea2345762644702595c23cc56ac7c60fcdbc
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-08-27 00:27:46 +0000

    [Desktop Android CRX] Use desktop-android extensions clients
    We are experimenting with desktop-android configurations for Chrome.
    
    This CL wires up the introduced DesktopAndroidExtensionsBrowserClient to
    be used in the browser on the relevant desktop-android experimental
    configurations. This is the first step in making the extensions code
    "reachable" in a built chrome APK, and enables functionality like
    properly parsing an extension.
    
    Add a new browsertest to exercise this functionality (parse and validate
    an extension from a test directory).
    
    This CL should have no production behavior change, since the
    desktop-android configuration is still highly experimental.
    
    Cq-Include-Trybots: luci.chromium.try:android-desktop-arm64-compile-rel,android-desktop-x64-compile-rel
    
    Bug: 356905053
    Change-Id: I2a573db8a68dd173b9f44defda18d13b1dbc3450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805712
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347081}

--- chrome/common/initialize_extensions_client.h
+++ chrome/common/initialize_extensions_client.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "extensions/buildflags/buildflags.h"
 
-#if !BUILDFLAG(ENABLE_EXTENSIONS)
+#if !BUILDFLAG(ENABLE_EXTENSIONS_CORE)
 #error "Extensions must be enabled"
 #endif
 
