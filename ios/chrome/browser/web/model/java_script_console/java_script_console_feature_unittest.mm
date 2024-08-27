d604aab674be82da0ad8f7fd375f5d14e10655fa
Aliona DANGLA <alionadangla@google.com>
2024-08-27 10:16:14 +0000

    [iOS] Use forwarding header of test_chrome_browser_state.h
    Replace ".../browser_state/test_chrome_browser_state.h" by
    ".../profile/test/test_profile_ios.h" to simplify the chrome
    browser state renaming project.
    
    This CL was uploaded by git cl split.
    
    R=michaeldo@chromium.org
    
    Bug: 359177266
    Change-Id: I91f40d1e6e2da6ab9c9e0370b853229d616cb7d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805952
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Mike Dougherty <michaeldo@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347258}

--- ios/chrome/browser/web/model/java_script_console/java_script_console_feature_unittest.mm
+++ ios/chrome/browser/web/model/java_script_console/java_script_console_feature_unittest.mm
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "base/strings/sys_string_conversions.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/web/model/java_script_console/java_script_console_feature_delegate.h"
 #import "ios/chrome/browser/web/model/java_script_console/java_script_console_feature_factory.h"
 #import "ios/chrome/browser/web/model/java_script_console/java_script_console_message.h"
--- ios/chrome/browser/webui/model/net_export_tab_helper_unittest.mm
+++ ios/chrome/browser/webui/model/net_export_tab_helper_unittest.mm
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Foundation/Foundation.h>
 
 #import "components/strings/grit/components_strings.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/webui/model/net_export_tab_helper_delegate.h"
 #import "ios/chrome/browser/webui/model/show_mail_composer_context.h"
 #import "ios/web/public/test/web_task_environment.h"
