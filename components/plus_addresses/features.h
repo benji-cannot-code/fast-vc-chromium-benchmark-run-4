6ff4a607d04a0052489a0582251d383198782271
Norge Vizcay <vizcay@google.com>
2024-08-27 15:20:23 +0000

    Remove plus address affiliation flag.
    Besides removing the flag, this CL also removes the (now unnecessary)
    GetPSLExtensions function and adjusts tests.
    
    Bug: 324553908
    Change-Id: I2d0dca7cae90bfad07f66065e4c593a6fc2bb7f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811963
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347372}

--- components/plus_addresses/features.h
+++ components/plus_addresses/features.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace plus_addresses::features {
 
-COMPONENT_EXPORT(PLUS_ADDRESSES_FEATURES)
-BASE_DECLARE_FEATURE(kPlusAddressAffiliations);
-
 #if BUILDFLAG(IS_ANDROID)
 COMPONENT_EXPORT(PLUS_ADDRESSES_FEATURES)
 BASE_DECLARE_FEATURE(kPlusAddressAndroidManualFallbackEnabled);
