8f05392102d7cade7b5ebeadaed298d61396f7f2
Noah Rose Ledesma <noahrose@google.com>
2024-08-27 18:16:59 +0000

    Allow constants to be overridden in test binary
    An overrides.json file can be provided to change the definitions of
    certain constants in the CECA binary under test. This will allow us to
    mock DMServer interactions, for instance.
    
    Bug: 344887937
    Change-Id: I1432a293faea749888a7e1ac48495e35e47b99c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815068
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347513}

--- chrome/enterprise_companion/crash_client.cc
+++ chrome/enterprise_companion/crash_client.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/enterprise_companion/enterprise_companion_branding.h"
 #include "chrome/enterprise_companion/enterprise_companion_client.h"
 #include "chrome/enterprise_companion/enterprise_companion_version.h"
+#include "chrome/enterprise_companion/global_constants.h"
 #include "chrome/enterprise_companion/installer_paths.h"
 #include "third_party/crashpad/crashpad/client/crash_report_database.h"
 #include "third_party/crashpad/crashpad/client/crashpad_client.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/crashpad/crashpad/client/simulate_crash.h"
 #include "third_party/crashpad/crashpad/handler/handler_main.h"
 #include "third_party/crashpad/crashpad/util/misc/tri_state.h"
+#include "url/gurl.h"
 
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
@@ -214,12 +216,13 @@ class CrashClient {
       attachments.push_back(*log_file);
     }
 #endif
-    if (!client->StartHandler(
-            base::PathService::CheckedGet(base::FILE_EXE), database_path,
-            /*metrics_dir=*/base::FilePath(), CRASH_UPLOAD_URL, annotations,
-            MakeCrashHandlerArgs(),
-            /*restartable=*/true,
-            /*asynchronous_start=*/false)) {
+    if (!client->StartHandler(base::PathService::CheckedGet(base::FILE_EXE),
+                              database_path,
+                              /*metrics_dir=*/base::FilePath(),
+                              GetGlobalConstants()->CrashUploadURL().spec(),
+                              annotations, MakeCrashHandlerArgs(),
+                              /*restartable=*/true,
+                              /*asynchronous_start=*/false)) {
       VLOG(1) << "Failed to start handler.";
       return false;
     }
--- chrome/enterprise_companion/event_logger.cc
+++ chrome/enterprise_companion/event_logger.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "chrome/enterprise_companion/enterprise_companion_branding.h"
+#include "chrome/enterprise_companion/global_constants.h"
 #include "chrome/enterprise_companion/installer_paths.h"
 #include "chrome/enterprise_companion/proto/enterprise_companion_event.pb.h"
 #include "chrome/enterprise_companion/proto/log_request.pb.h"
@@ -48,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "services/network/public/mojom/cookie_manager.mojom.h"
 #include "services/network/public/mojom/url_response_head.mojom.h"
+#include "url/gurl.h"
 #include "url/origin.h"
 
 namespace enterprise_companion {
@@ -177,13 +179,16 @@ class EventLogUploaderImpl : public EventLogUploader {
     DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
     CHECK(!url_loader_) << "Overlapping log requests are not permitted.";
 
+    const GURL event_logging_url =
+        GetGlobalConstants()->EnterpriseCompanionEventLoggingURL();
+
     std::unique_ptr<network::ResourceRequest> resource_request =
         std::make_unique<network::ResourceRequest>();
-    resource_request->url = GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL);
+    resource_request->url = event_logging_url;
     resource_request->request_initiator =
-        url::Origin::Create(GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL));
-    resource_request->site_for_cookies = net::SiteForCookies::FromUrl(
-        GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL));
+        url::Origin::Create(event_logging_url);
+    resource_request->site_for_cookies =
+        net::SiteForCookies::FromUrl(event_logging_url);
     resource_request->method = net::HttpRequestHeaders::kPostMethod;
     url_loader_ = network::SimpleURLLoader::Create(std::move(resource_request),
                                                    kTrafficAnnotation);
@@ -364,7 +369,8 @@ class EventLoggerCookieHandlerImpl : public EventLoggerCookieHandler,
     cookie_manager_remote_ =
         mojo::Remote(std::move(cookie_manager_pending_remote));
     cookie_manager_remote_->AddCookieChangeListener(
-        GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL), kLoggingCookieName,
+        GetGlobalConstants()->EnterpriseCompanionEventLoggingURL(),
+        kLoggingCookieName,
         cookie_listener_receiver_.BindNewPipeAndPassRemote());
     InitLoggingCookie(std::move(callback));
   }
@@ -386,6 +392,9 @@ class EventLoggerCookieHandlerImpl : public EventLoggerCookieHandler,
       cookie_value.resize(*bytes_read);
     }
 
+    const GURL event_logging_url =
+        GetGlobalConstants()->EnterpriseCompanionEventLoggingURL();
+
     net::CookieOptions cookie_options;
     cookie_options.set_include_httponly();
     cookie_options.set_same_site_cookie_context(
@@ -393,10 +402,10 @@ class EventLoggerCookieHandlerImpl : public EventLoggerCookieHandler,
             net::CookieOptions::SameSiteCookieContext::ContextType::
                 SAME_SITE_STRICT));
     url::SchemeHostPort event_logging_scheme_host_port =
-        url::SchemeHostPort(GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL));
+        url::SchemeHostPort(event_logging_url);
     std::unique_ptr<net::CanonicalCookie> cookie =
         net::CanonicalCookie::CreateSanitizedCookie(
-            GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL), "NID", cookie_value,
+            event_logging_url, "NID", cookie_value,
             base::StrCat({".", event_logging_scheme_host_port.host()}),
             /*path=*/"/", /*creation_time=*/base::Time::Now(),
             /*expiration_time=*/base::Time::Now() + base::Days(180),
@@ -407,7 +416,7 @@ class EventLoggerCookieHandlerImpl : public EventLoggerCookieHandler,
 
     if (cookie->IsCanonical()) {
       cookie_manager_remote_->SetCanonicalCookie(
-          *cookie, GURL(ENTERPRISE_COMPANION_EVENT_LOGGING_URL), cookie_options,
+          *cookie, event_logging_url, cookie_options,
           base::BindOnce([](net::CookieAccessResult result) {
             LOG_IF(ERROR, !result.status.IsInclude())
                 << "Failed to set logging cookie: " << result.status;
