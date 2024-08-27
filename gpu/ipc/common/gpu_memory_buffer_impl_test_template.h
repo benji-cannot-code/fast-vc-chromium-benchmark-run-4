94590fbe5b32decc79980e6b1d8bf60d3c3292bc
Colin Blundell <blundell@chromium.org>
2024-08-27 06:44:20 +0000

    [//gpu] Eliminate GpuMemoryBufferImplAHB being created
    The public-facing creation functions were changed to return nullptr with
    a reverse killswitch in 127 [1], and there have been no reports of any
    problems. This CL removes those functions and changes their callers to
    assume nullptr. A followup CL will continue with the elimination of
    the class, which is slightly more involved, as well as further
    streamlining those callsites as feasible.
    
    Note that removing these creation functions necessitates removing the
    unittest testing these functions as well. This is not a problem, as
    there is no way to create an instance of this class anymore in
    production and the entire class will imminently be removed.
    
    [1] https://chromiumdash.appspot.com/commit/2f9e4bc85e2f1d35e53a4deadffcdc0787ac39f7
    
    Bug: 343584529
    Change-Id: Iac181b266e27eb4798a3935e0ca3e612551e9021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811786
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347182}

--- gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
+++ gpu/ipc/common/gpu_memory_buffer_impl_test_template.h
@@ -35,10 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/mojom/buffer_types.mojom.h"
 #include "ui/gl/gl_display.h"
 
-#if BUILDFLAG(IS_ANDROID)
-#include "gpu/config/gpu_finch_features.h"
-#endif
-
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_OZONE)
 #include "ui/gl/init/gl_factory.h"
 #include "ui/gl/test/gl_surface_test_support.h"
@@ -50,15 +46,7 @@ namespace gpu {
 template <typename GpuMemoryBufferImplType>
 class GpuMemoryBufferImplTest : public testing::Test {
  public:
-  GpuMemoryBufferImplTest() {
-#if BUILDFLAG(IS_ANDROID)
-    // Allow for creation of this class to preserve unittest coverage while we
-    // roll out its elimination via Finch.
-    // TODO(crbug.com/343584529): Remove post-safe rollout.
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kEnableGpuMemoryBufferImplAHB);
-#endif
-  }
+  GpuMemoryBufferImplTest() = default;
 
   GpuMemoryBufferImpl::DestructionCallback CreateGpuMemoryBuffer(
       const gfx::Size& size,
@@ -137,9 +125,6 @@ class GpuMemoryBufferImplTest : public testing::Test {
   }
 
  private:
-#if BUILDFLAG(IS_ANDROID)
-  base::test::ScopedFeatureList scoped_feature_list_;
-#endif
   bool run_gpu_test_ = false;
   GpuMemoryBufferSupport gpu_memory_buffer_support_;
   raw_ptr<gl::GLDisplay> display_ = nullptr;
@@ -157,25 +142,13 @@ class GpuMemoryBufferImplTest : public testing::Test {
 template <typename GpuMemoryBufferImplType>
 class GpuMemoryBufferImplCreateTest : public testing::Test {
  public:
-  GpuMemoryBufferImplCreateTest() {
-#if BUILDFLAG(IS_ANDROID)
-    // Allow for creation of this class to preserve unittest coverage while we
-    // roll out its elimination via Finch.
-    // TODO(crbug.com/343584529): Remove post-safe rollout.
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kEnableGpuMemoryBufferImplAHB);
-#endif
-  }
+  GpuMemoryBufferImplCreateTest() = default;
 
   GpuMemoryBufferSupport* gpu_memory_buffer_support() {
     return &gpu_memory_buffer_support_;
   }
 
  private:
-#if BUILDFLAG(IS_ANDROID)
-  base::test::ScopedFeatureList scoped_feature_list_;
-#endif
-
   GpuMemoryBufferSupport gpu_memory_buffer_support_;
 };
 
