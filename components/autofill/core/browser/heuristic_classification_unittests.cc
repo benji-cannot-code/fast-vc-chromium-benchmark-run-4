51b6b1aad191158c05882adec6c9783f05770841
Norge Vizcay <vizcay@google.com>
2024-08-27 08:22:56 +0000

    Clean up feature AutofillEnableRationalizationEngineForMX.
    The feature was default enabled on M128. This CL cleans up the flag two
    releases later.
    
    Bug: 40275657
    Change-Id: Ie36c9f0e6de3dc606e8b9db59fc1ac9ed9c5f3a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811946
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347205}

--- components/autofill/core/browser/heuristic_classification_unittests.cc
+++ components/autofill/core/browser/heuristic_classification_unittests.cc
@@ -558,7 +558,6 @@ TEST_P(HeuristicClassificationTests, EndToEnd) {
       features::kAutofillUseMXAddressModel,
       features::kAutofillUsePLAddressModel,
       features::kAutofillEnableExpirationDateImprovements,
-      features::kAutofillEnableRationalizationEngineForMX,
       // Other improvements.
       features::kAutofillEnableCacheForRegexMatching};
   std::vector<base::test::FeatureRef> disabled_features = {};
