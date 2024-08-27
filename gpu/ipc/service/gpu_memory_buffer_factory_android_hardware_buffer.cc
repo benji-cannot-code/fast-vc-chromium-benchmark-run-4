0cb17560a488fdb57b36f1a7e5c469a371c79e02
Colin Blundell <blundell@chromium.org>
2024-08-27 09:28:35 +0000

    [Android] Remove vestigial factory usage of GMBImplAHB
    The factory no longer ever creates GMBImplAHB instances, so its code
    relating to maintaining a map of those instances is dead and can be
    removed.
    
    Bug: 343584529
    Change-Id: I1b6e3dcc00d5ad35e489a4c869d55db8e9cbcb83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811845
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347230}

--- gpu/ipc/service/gpu_memory_buffer_factory_android_hardware_buffer.cc
+++ gpu/ipc/service/gpu_memory_buffer_factory_android_hardware_buffer.cc
@@ -5,15 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "gpu/ipc/service/gpu_memory_buffer_factory_android_hardware_buffer.h"
 
-#include "base/android/android_hardware_buffer_compat.h"
-#include "base/android/scoped_hardware_buffer_handle.h"
-#include "base/containers/contains.h"
-#include "base/logging.h"
-#include "build/build_config.h"
-#include "gpu/command_buffer/service/ahardwarebuffer_utils.h"
-#include "gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.h"
-#include "ui/gfx/android/android_surface_control_compat.h"
-
 namespace gpu {
 
 GpuMemoryBufferFactoryAndroidHardwareBuffer::
@@ -36,11 +27,7 @@ GpuMemoryBufferFactoryAndroidHardwareBuffer::CreateGpuMemoryBuffer(
 
 void GpuMemoryBufferFactoryAndroidHardwareBuffer::DestroyGpuMemoryBuffer(
     gfx::GpuMemoryBufferId id,
-    int client_id) {
-  base::AutoLock lock(lock_);
-  BufferMapKey key(id, client_id);
-  buffer_map_.erase(key);
-}
+    int client_id) {}
 
 bool GpuMemoryBufferFactoryAndroidHardwareBuffer::
     FillSharedMemoryRegionWithBufferContents(
