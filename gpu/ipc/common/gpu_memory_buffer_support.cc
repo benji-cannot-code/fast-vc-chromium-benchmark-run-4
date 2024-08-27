60a1d9a04df5c2c06aaba35ece0f5a2b65b18314
Colin Blundell <blundell@chromium.org>
2024-08-27 13:03:15 +0000

    [//gpu] Remove GpuMemoryBufferImplAHB
    This class is now dead code \o/.
    
    Bug: 343584529
    Change-Id: I7d8c172b5d0ce45fd30cda1675c35f84d50db42b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812146
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347316}

--- gpu/ipc/common/gpu_memory_buffer_support.cc
+++ gpu/ipc/common/gpu_memory_buffer_support.cc
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/android_hardware_buffer_compat.h"
-#include "gpu/ipc/common/gpu_memory_buffer_impl_android_hardware_buffer.h"
 #endif
 
 namespace gpu {
