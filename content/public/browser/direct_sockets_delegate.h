7f5534ac3ca8d455f1db5543109b6837c81b86b2
Andrew Rayskiy <greengrape@google.com>
2024-08-27 16:05:56 +0000

    [DirectSockets] Rely on extensions::ProcessMap() instead of ProcessLock
    This CL also eliminates an unused mojom field (local_addr).
    
    Bug: 362209971
    Change-Id: Ib21bd9389c0fa4273d4482562375188c69cc234c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812024
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347402}

--- content/public/browser/direct_sockets_delegate.h
+++ content/public/browser/direct_sockets_delegate.h
@@ -10,11 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "content/common/content_export.h"
-#include "url/gurl.h"
 
 namespace content {
 
-class BrowserContext;
 class RenderFrameHost;
 
 // Allows the embedder to alter the logic of some operations in
@@ -29,10 +27,8 @@ class CONTENT_EXPORT DirectSocketsDelegate {
   virtual bool IsAPIAccessAllowed(content::RenderFrameHost& rfh) = 0;
 
   // Allows embedders to introduce additional rules for specific
-  // addresses/ports. |lock_url| is the URL to which the renderer
-  // process is locked.
-  virtual bool ValidateAddressAndPort(content::BrowserContext* browser_context,
-                                      const GURL& lock_url,
+  // addresses/ports.
+  virtual bool ValidateAddressAndPort(content::RenderFrameHost& rfh,
                                       const std::string& address,
                                       uint16_t port,
                                       ProtocolType) = 0;
