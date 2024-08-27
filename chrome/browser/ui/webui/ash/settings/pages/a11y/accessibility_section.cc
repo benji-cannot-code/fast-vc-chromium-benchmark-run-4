fb78037918ff60132d726aed337356d8f1eef8a7
LK Kupoluyi <lkupo@chromium.org>
2024-08-27 19:22:23 +0000

    [Disable Trackpad] Add disable internal trackpad dropdown
    Bug: b:354187572
    Change-Id: I8084bf7f97c0e873bb7c03ae9ed8b290ebdb17ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5800154
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Jesulayomi Kupoluyi <lkupo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347549}

--- chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_section.cc
@@ -579,6 +579,10 @@ bool IsAccessibilityMouseKeysEnabled() {
   return ::features::IsAccessibilityMouseKeysEnabled();
 }
 
+bool IsAccessibilityDisableTrackpadEnabled() {
+  return ::features::IsAccessibilityDisableTrackpadEnabled();
+}
+
 bool IsAccessibilityOverscrollSettingFeatureEnabled() {
   return ::features::IsAccessibilityOverscrollSettingFeatureEnabled();
 }
@@ -890,6 +894,11 @@ void AccessibilitySection::AddLoadTimeData(
        IDS_SETTINGS_ACCESSIBILITY_DICTATION_SUBTITLE_SODA_DOWNLOAD_ERROR},
       {"dictationLocaleSubLabelOffline",
        IDS_SETTINGS_ACCESSIBILITY_DICTATION_LOCALE_SUB_LABEL_OFFLINE},
+      {"disableTrackpadLabel", IDS_SETTINGS_DISABLE_TRACKPAD_LABEL},
+      {"disableTrackpadAlways", IDS_SETTINGS_DISABLE_TRACKPAD_ALWAYS},
+      {"disableTrackpadMouseConnected",
+       IDS_SETTINGS_DISABLE_TRACKPAD_MOUSE_CONNECTED},
+      {"disableTrackpadNever", IDS_SETTINGS_DISABLE_TRACKPAD_NEVER},
       {"displayAndMagnificationLinkTitle",
        IDS_SETTINGS_ACCESSIBILITY_DISPLAY_AND_MAGNIFICATION_LINK_TITLE},
       {"displayHeading", IDS_SETTINGS_ACCESSIBILITY_DISPLAY_HEADING},
@@ -1359,6 +1368,9 @@ void AccessibilitySection::AddLoadTimeData(
 
   html_source->AddBoolean("isAccessibilityFaceGazeEnabled",
                           IsAccessibilityFaceGazeEnabled());
+
+  html_source->AddBoolean("isAccessibilityDisableTrackpadEnabled",
+                          IsAccessibilityDisableTrackpadEnabled());
 
   html_source->AddBoolean("isAccessibilityMouseKeysEnabled",
                           IsAccessibilityMouseKeysEnabled());
