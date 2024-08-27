a7a09f274f3ca4da9e8a848cd044ee469ba9c3ba
Gayane Petrosyan <gayane@google.com>
2024-08-27 18:20:52 +0000

    [iOS] Dismiss default browser blue dot on overflow menu open
    Now when we show blue dot on overflow menu button on toolbar we should
    dismiss default browser blue dot on overflow menu open instead of
    dismiss.
    
    Bug: chroimum:362308602
    Change-Id: I550b77aa23c5ea538255b51e1bd5a79714d98016
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812570
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347516}

--- ios/chrome/browser/ui/popup_menu/popup_menu_coordinator.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_coordinator.mm
@@ -236,6 +236,10 @@ using base::UserMetricsAction;
                 initWithWebStateList:self.browser->GetWebStateList()
       webContentAreaOverlayPresenter:overlayPresenter];
 
+  feature_engagement::Tracker* tracker =
+      feature_engagement::TrackerFactory::GetForBrowserState(
+          self.browser->GetBrowserState());
+
   // Create the overflow menu mediator first so the popup mediator isn't created
   // if not needed.
   self.toolsMenuOpenTime = [NSDate timeIntervalSinceReferenceDate];
@@ -303,9 +307,7 @@ using base::UserMetricsAction;
         ReadingListModelFactory::GetInstance()->GetForBrowserState(
             self.browser->GetBrowserState());
     mediator.browserStatePrefs = self.browser->GetBrowserState()->GetPrefs();
-    mediator.engagementTracker =
-        feature_engagement::TrackerFactory::GetForBrowserState(
-            self.browser->GetBrowserState());
+    mediator.engagementTracker = tracker;
     mediator.webContentAreaOverlayPresenter = overlayPresenter;
     mediator.browserPolicyConnector =
         GetApplicationContext()->GetBrowserPolicyConnector();
@@ -328,7 +330,6 @@ using base::UserMetricsAction;
         TabBasedIPHBrowserAgent::FromBrowser(self.browser);
     mediator.hasSettingsBlueDot =
         [self.popupMenuHelpCoordinator hasBlueDotForOverflowMenu];
-
     self.contentBlockerMediator.consumer = mediator;
 
     NSInteger highlightDestination =
@@ -395,6 +396,13 @@ using base::UserMetricsAction;
                      [weakSelf.popupMenuHelpCoordinator
                          showIPHAfterOpenOfOverflowMenu:menu];
                    }];
+
+    // Log to FET overflow menu opened if opened with blue dot.
+    if ([self.popupMenuHelpCoordinator hasBlueDotForOverflowMenu] && tracker) {
+      tracker->NotifyEvent(
+          feature_engagement::events::kBlueDotPromoOverflowMenuOpened);
+    }
+
     return;
   }
 
@@ -405,9 +413,7 @@ using base::UserMetricsAction;
                                     self.browser->GetBrowserState())
          browserPolicyConnector:GetApplicationContext()
                                     ->GetBrowserPolicyConnector()];
-  self.mediator.engagementTracker =
-      feature_engagement::TrackerFactory::GetForBrowserState(
-          self.browser->GetBrowserState());
+  self.mediator.engagementTracker = tracker;
   self.mediator.webStateList = self.browser->GetWebStateList();
   self.mediator.readingListBrowserAgent =
       ReadingListBrowserAgent::FromBrowser(self.browser);
