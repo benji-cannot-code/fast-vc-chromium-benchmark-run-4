46354b93e85650ce4337d47fef4830d0d6a3b1d7
Zhenyao Mo <zmo@chromium.org>
2024-08-27 02:51:39 +0000

    Remove all khronos references from ui/
    TEST=bots compiles
    
    Bug: 353736708
    Change-Id: I8dc38f36a255af184198d6e2f506d97d0f9fc6f3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813491
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347135}

--- gpu/config/gpu_info.cc
+++ gpu/config/gpu_info.cc
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "gpu/config/gpu_util.h"
 
-#if BUILDFLAG(IS_MAC)
-#include <GLES2/gl2.h>
-#include <GLES2/gl2extchromium.h>
-#endif  // BUILDFLAG(IS_MAC)
-
 namespace {
 
 void EnumerateGPUDevice(const gpu::GPUInfo::GPUDevice& device,
@@ -153,19 +148,6 @@ const char* OverlaySupportToString(gpu::OverlaySupport support) {
   }
 }
 #endif  // BUILDFLAG(IS_WIN)
-
-#if BUILDFLAG(IS_MAC)
-GPU_EXPORT bool ValidateMacOSSpecificTextureTarget(int target) {
-  switch (target) {
-    case GL_TEXTURE_2D:
-    case GL_TEXTURE_RECTANGLE_ARB:
-      return true;
-
-    default:
-      return false;
-  }
-}
-#endif  // BUILDFLAG(IS_MAC)
 
 VideoDecodeAcceleratorCapabilities::VideoDecodeAcceleratorCapabilities()
     : flags(0) {}
