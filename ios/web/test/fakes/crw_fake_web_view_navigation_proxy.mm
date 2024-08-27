b5e3489b248e731d6e64b72a777f363171d35f5a
Yuan Yin <yuanyin@microsoft.com>
2024-08-27 15:29:05 +0000

    [iOS] Support title for the navigation item when new navigation created by history.pushState()
    This problem occurs with iOS WKWebView because the interface provided by WKWebView is not perfect.
    
    Normally, The WKBackforwardList.title equals to the document.title when
    the page loads. But it's not always accurate. For WKWebView, The
    WKBackforwardList.title is empty when the new page is opened via
    history.pushState(). But the document.title (WKWebView.title) doesn't
    have to be empty or changed. So when opening a new page via
    history.pushState(), its title should be the current document.title.
    
    To solve this problem, When the WKBackforwardList.title is empty for current navigation, the document.title is read as the title of the new item.
    
    Bug: 41438347
    Change-Id: I1e545dd42638fac12642e75bff37abef9406d32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808626
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: yuan yin <yuanyin@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1347377}

--- ios/web/test/fakes/crw_fake_web_view_navigation_proxy.mm
+++ ios/web/test/fakes/crw_fake_web_view_navigation_proxy.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @implementation CRWFakeWebViewNavigationProxy {
   NSURL* _URL;
+  NSString* _title;
   CRWFakeBackForwardList* _backForwardList;
 }
 
@@ -34,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSURL*)URL {
   return _URL;
+}
+
+- (NSString*)title {
+  return _title;
 }
 
 @end
