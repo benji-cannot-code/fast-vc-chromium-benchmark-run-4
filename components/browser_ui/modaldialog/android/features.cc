71f8723e14718414d72213ff175cf5ba4b8a33b0
Aishwarya Rajesh <aishwaryarj@google.com>
2024-08-27 21:31:43 +0000

    Create ModalDialogFeatureMap
    Add a feature flag to guard changes made to modal dialog dimension
    calculations while taking system bars insets into account.
    
    Bug: 339304231
    Change-Id: I90723701a956b676f88bded13623126ccc1cbe9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789637
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Aishwarya Rajesh <aishwaryarj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347640}

---
+++ components/browser_ui/modaldialog/android/features.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/browser_ui/modaldialog/android/features.h"
+
+#include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
+
+namespace browser_ui {
+
+BASE_FEATURE(kModalDialogLayoutWithSystemInsets,
+             "ModalDialogLayoutWithSystemInsets",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+}  // namespace browser_ui
