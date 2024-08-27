4a66a2cb6985e490b0e0462c069ddd429f77e8ce
Lingqi Chi <lingqi@chromium.org>
2024-08-27 06:47:19 +0000

    cleanup: remove unused methods from widget_base
    - remove GetWidgetHostRemote
    - remove SetMouseCapture
    - remove set_screen_infos
    - merge two local_surface_id_from_parent() with different signatures
      into one
    
    Bug: N/A
    Change-Id: If38ed6a0a26306907b859a010a1c0da509770c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5239794
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347185}

--- third_party/blink/renderer/platform/widget/widget_base.cc
+++ third_party/blink/renderer/platform/widget/widget_base.cc
@@ -1501,13 +1501,6 @@ void WidgetBase::CursorVisibilityChange(bool is_visible) {
   client_->SetCursorVisibilityState(is_visible);
 }
 
-void WidgetBase::SetMouseCapture(bool capture) {
-  if (mojom::blink::WidgetInputHandlerHost* host =
-          widget_input_handler_manager_->GetWidgetInputHandlerHost()) {
-    host->SetMouseCapture(capture);
-  }
-}
-
 void WidgetBase::ImeSetComposition(
     const String& text,
     const Vector<ui::ImeTextSpan>& ime_text_spans,
