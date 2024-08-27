ded85f92c42a3d40be305c69c8a5f5a8c5f29cfc
Andrey Davydov <andreydav@google.com>
2024-08-27 11:48:18 +0000

    [SAML] Remove `usedSaml` flag in TS code
    Also clarify the meaning of `isDefaultSsoProvider`.
    
    Bug: b:361784878
    Change-Id: Id4498d95599645b582b75a454950f54f7dbfae03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808947
    Reviewed-by: Michael Slutskii <slutskii@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347294}

--- chrome/browser/resources/chromeos/login/screens/common/gaia_signin.ts
+++ chrome/browser/resources/chromeos/login/screens/common/gaia_signin.ts
@@ -143,15 +143,6 @@ export class GaiaSigninElement extends GaiaSigninElementBase {
       },
 
       /**
-       * Whether the authenticator is or has been in the |SAML| AuthFlow during
-       * the current authentication attempt.
-       */
-      usedSaml: {
-        type: Boolean,
-        value: false,
-      },
-
-      /**
        * Contains the security token PIN dialog parameters object when the
        * dialog is shown. Is null when no PIN dialog is shown.
        */
@@ -226,7 +217,9 @@ export class GaiaSigninElement extends GaiaSigninElementBase {
       },
 
       /**
-       * Whether the default SAML 3rd-party page is visible.
+       * Whether the default SAML 3rd-party page is visible. We need to track
+       * this in addition to `isSamlSsoVisible` because it has some UI
+       * implications (e.g. user needs to be able to switch to non-default IdP).
        */
       isDefaultSsoProvider: {
         type: Boolean,
@@ -254,7 +247,6 @@ export class GaiaSigninElement extends GaiaSigninElementBase {
   private isLoadingUiShown: boolean;
   private navigationEnabled: boolean;
   private isSaml: boolean;
-  private usedSaml: boolean;
   private pinDialogParameters: OobeTypes.SecurityTokenPinDialogParameters|null;
   private isSamlSsoVisible: boolean;
   private videoEnabled: boolean;
@@ -563,10 +555,6 @@ export class GaiaSigninElement extends GaiaSigninElementBase {
     this.authCompleted = false;
     this.navigationButtonsHidden = false;
 
-    // Reset SAML
-    this.isSaml = false;
-    this.usedSaml = false;
-
     // Reset the PIN dialog, in case it's shown.
     this.closePinDialog();
 
@@ -639,10 +627,6 @@ export class GaiaSigninElement extends GaiaSigninElementBase {
    * Observer that is called when the |isSaml| property gets changed.
    */
   private onSamlChanged(): void {
-    if (this.isSaml) {
-      this.usedSaml = true;
-    }
-
     chrome.send('samlStateChanged', [this.isSaml]);
 
     this.classList.toggle('saml', this.isSaml);
