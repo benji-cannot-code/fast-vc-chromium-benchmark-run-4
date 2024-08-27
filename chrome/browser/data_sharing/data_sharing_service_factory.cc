2b37d5222234ed92ac19dfec4e55cb885211e93d
Ritika Gupta <ritikagup@google.com>
2024-08-27 22:46:20 +0000

    [Data Sharing] Moving UiDelegate native bridge for android to android sub-directory.
    Bug: b/348412332
    Change-Id: I49c2be0c9a05f6a5a214e77ed7f69def065a2da6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5643924
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Ritika Gupta <ritikagup@google.com>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347689}

--- chrome/browser/data_sharing/data_sharing_service_factory.cc
+++ chrome/browser/data_sharing/data_sharing_service_factory.cc
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 
 #if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/data_sharing/android/data_sharing_ui_delegate_android.h"
 #include "chrome/browser/data_sharing/data_sharing_service_factory_bridge.h"
-#include "chrome/browser/data_sharing/data_sharing_ui_delegate_android.h"
 #else  // BUILDFLAG(IS_ANDROID)
 #include "chrome/browser/data_sharing/desktop/data_sharing_sdk_delegate_desktop.h"
 #include "chrome/browser/data_sharing/desktop/data_sharing_ui_delegate_desktop.h"
