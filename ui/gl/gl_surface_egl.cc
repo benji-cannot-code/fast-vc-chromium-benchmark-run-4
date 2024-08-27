cb00b7d27a987f4d6b94c7a144c7764f032229ae
Zhenyao Mo <zmo@chromium.org>
2024-08-27 21:36:03 +0000

    Remove allow_unsafe_buffers from gl_surface_egl.cc
    Wrap UNSAFE_TODO around raw pointer for now, to prevent
    more unsafe buffer cases from creeping in.
    
    TEST=bots compile
    R=​tsepez@chromium.org
    
    Bug: 351564777
    Change-Id: I2fc6d3702f50922b654ed356124a904fba00e0bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5814826
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347642}

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "ui/gl/gl_surface_egl.h"
 
 #include <stddef.h>
@@ -19,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/compiler_specific.h"
 #include "base/containers/heap_array.h"
 #include "base/lazy_instance.h"
 #include "base/logging.h"
@@ -712,7 +708,7 @@ void NativeViewGLSurfaceEGL::TraceSwapEvents(EGLuint64KHR oldFrameId) {
 
   const char* pending_symbols = valid_symbols.c_str();
   for (size_t i = 1; i < tracePairs.size(); i++) {
-    pending_symbols++;
+    UNSAFE_TODO(pending_symbols++);
     TRACE_EVENT_COPY_NESTABLE_ASYNC_BEGIN_WITH_TIMESTAMP0(
         kSwapEventTraceCategories, pending_symbols, trace_id,
         tracePairs[i - 1].time);
