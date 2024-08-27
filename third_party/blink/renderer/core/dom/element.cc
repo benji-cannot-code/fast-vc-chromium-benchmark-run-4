8c981fd09ee3255e696f5b191948d42e7a63cbc2
Joey Arhar <jarhar@chromium.org>
2024-08-27 13:22:24 +0000

    Remove URLAttributeFix flag
    This has been enabled by default since M120, so it can be removed now.
    
    Bug: 1493146
    Change-Id: I9ffa9190740860e2f19b8a3bf5645149934f7928
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809247
    Auto-Submit: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Traian Captan <tcaptan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347323}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -8485,7 +8485,7 @@ String Element::GetURLAttribute(const QualifiedName& name) const {
 #endif
   KURL url = GetDocument().CompleteURL(
       StripLeadingAndTrailingHTMLSpaces(getAttribute(name)));
-  return url.IsValid() || !RuntimeEnabledFeatures::URLAttributeFixEnabled()
+  return url.IsValid()
              ? url
              : StripLeadingAndTrailingHTMLSpaces(getAttribute(name));
 }
