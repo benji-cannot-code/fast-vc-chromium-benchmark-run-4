6bd51d69de3e8218cd8881d69c8e15fade3795b0
Kazuki Takise <takise@google.com>
2024-08-27 07:10:46 +0000

    Clean up XDG flag
    Now that it's very unlikely to resume the project in the future,
    so should be safe to clean these up.
    
    BUG=b/360959964
    TEST=build
    
    Change-Id: I29654a9bb70eb52ae35ea6be9844d5770e3cc32d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815431
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Kazuki Takise <takise@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347193}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3497,9 +3497,6 @@ extern const char kArcVmmSwapKBShortcutDesc[];
 extern const char kArcVmMemorySizeName[];
 extern const char kArcVmMemorySizeDesc[];
 
-extern const char kArcXdgModeName[];
-extern const char kArcXdgModeDesc[];
-
 extern const char kArcAAudioMMAPLowLatencyName[];
 extern const char kArcAAudioMMAPLowLatencyDescription[];
 
