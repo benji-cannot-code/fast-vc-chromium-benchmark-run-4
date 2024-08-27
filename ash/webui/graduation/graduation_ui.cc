ffbd3dea4cc2bfcc219a51f7383684e0b229083b
Courtney Wong <courtneywong@chromium.org>
2024-08-27 20:30:31 +0000

    Reland "graduation: Check Graduation policy when enabling the SWA"
    This reverts commit a01139c3fc244433bc0aeefe73796a57830e26c8.
    
    Reason for revert: MSAN tests have been fixed by initializing the
    uninitialized exploded struct values.
    
    Tested: Test passes locally and on MSAN trybot
    
    Original change's description:
    > Revert "graduation: Check Graduation policy when enabling the SWA"
    >
    > This reverts commit c9789948f840e0d617e9174359b90556451eea34.
    >
    > Reason for revert: breaks ash_unittest on builder "chromium/ci/Linux ChromiumOS MSan Tests"
    >
    > Original change's description:
    > > graduation: Check Graduation policy when enabling the SWA
    > >
    > > Bug: b:357883712
    > > Change-Id: I77fcbcc553dca937f6699842af0e200b9a98b1b7
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804862
    > > Reviewed-by: Aga Wronska <agawronska@chromium.org>
    > > Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1347027}
    >
    > Bug: b:357883712, 362339860
    > Change-Id: Iddcdf370acff34a614df9a7cd19067ecc1a94081
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815810
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Ming-Ying Chung <mych@chromium.org>
    > Owners-Override: Ming-Ying Chung <mych@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1347153}
    
    Bug: b:357883712, 362339860
    Change-Id: Ia17a793d5268f651c2a81021e8d4e010c93e482a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815277
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347596}

--- ash/webui/graduation/graduation_ui.cc
+++ ash/webui/graduation/graduation_ui.cc
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "ash/constants/ash_features.h"
+#include "ash/edusumer/graduation_utils.h"
+#include "ash/session/session_controller_impl.h"
+#include "ash/shell.h"
 #include "ash/webui/common/trusted_types_util.h"
 #include "ash/webui/grit/ash_graduation_resources.h"
 #include "ash/webui/grit/ash_graduation_resources_map.h"
@@ -38,8 +41,10 @@ void AddResources(content::WebUIDataSource* source) {
 
 bool GraduationUIConfig::IsWebUIEnabled(
     content::BrowserContext* browser_context) {
-  // TODO(b/357883712): Check Graduation policy status
-  return features::IsGraduationEnabled();
+  return features::IsGraduationEnabled() &&
+         IsEligibleForGraduation(Shell::Get()
+                                     ->session_controller()
+                                     ->GetLastActiveUserPrefService());
 }
 
 GraduationUI::GraduationUI(content::WebUI* web_ui)
