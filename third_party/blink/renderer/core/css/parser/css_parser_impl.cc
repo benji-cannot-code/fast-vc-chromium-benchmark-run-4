c2cf13ca222b13c1a28a777ac549560bfdb9b35c
Anders Hartvoll Ruud <andruud@chromium.org>
2024-08-27 10:57:39 +0000

    Avoid CSSParserTokenRange for @supports prelude
    CSSParserImpl::ConsumeSupportsRule is using both the streaming parser
    *and* CSSParserTokenRange to check if the prelude ended correctly.
    Avoid ConsumeAtRulePrelude, and just use the new pattern for the streaming parser.
    
    Change-Id: I57411551938561e24008fb76e38b2295fb010c77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812154
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347272}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -1375,12 +1375,9 @@ StyleRuleSupports* CSSParserImpl::ConsumeSupportsRule(
   wtf_size_t prelude_offset_start = stream.LookAheadOffset();
   CSSSupportsParser::Result supported =
       CSSSupportsParser::ConsumeSupportsCondition(stream, *this);
-  // Check whether the entire prelude was consumed. If it wasn't, ensure we
-  // consume any leftovers plus the block before returning a parse error.
-  stream.ConsumeWhitespace();
-  CSSParserTokenRange prelude_remainder = ConsumeAtRulePrelude(stream);
-  if (!prelude_remainder.AtEnd()) {
-    supported = CSSSupportsParser::Result::kParseFailure;
+  if (supported == CSSSupportsParser::Result::kParseFailure) {
+    ConsumeErroneousAtRule(stream, CSSAtRuleID::kCSSAtRuleSupports);
+    return nullptr;
   }
   wtf_size_t prelude_offset_end = stream.LookAheadOffset();
   if (!ConsumeEndOfPreludeForAtRuleWithBlock(stream,
@@ -1388,10 +1385,6 @@ StyleRuleSupports* CSSParserImpl::ConsumeSupportsRule(
     return nullptr;
   }
   CSSParserTokenStream::BlockGuard guard(stream);
-
-  if (supported == CSSSupportsParser::Result::kParseFailure) {
-    return nullptr;  // Parse error, invalid @supports condition
-  }
 
   if (observer_) {
     observer_->StartRuleHeader(StyleRule::kSupports, prelude_offset_start);
