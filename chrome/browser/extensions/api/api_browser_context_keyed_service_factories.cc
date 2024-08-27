0cd2b468d525670bc9a544214d7d94b156fd27a4
David Bertoni <dbertoni@chromium.org>
2024-08-27 16:33:21 +0000

    [Extensions] Move some source files into separate source_sets.
    These files are responsible for building Extension API KeyedService
    factories. Moving them into their own source_sets breaks circular
    dependencies between API implementations and
    //chrome/browser/extensions.
    
    Bug: 40593486
    Change-Id: Ia693db7f963d2809ce71871fe905d09b86c62fff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812217
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347420}

--- chrome/browser/extensions/api/api_browser_context_keyed_service_factories.cc
+++ chrome/browser/extensions/api/api_browser_context_keyed_service_factories.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/web_authentication_proxy/web_authentication_proxy_api.h"
 #include "chrome/browser/extensions/api/web_navigation/web_navigation_api.h"
 #include "chrome/browser/extensions/api/webrtc_audio_private/webrtc_audio_private_api.h"
-#include "chrome/browser/extensions/browser_context_keyed_service_factories.h"
 #include "chrome/browser/extensions/commands/command_service.h"
 #include "chrome/common/buildflags.h"
 #include "extensions/browser/api/bluetooth_low_energy/bluetooth_low_energy_api.h"
