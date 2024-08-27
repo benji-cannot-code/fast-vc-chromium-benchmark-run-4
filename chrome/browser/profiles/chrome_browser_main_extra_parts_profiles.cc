1a85cbcb6608c586631f676e112e008fba629ca9
Alvin Lam <alvingo@google.com>
2024-08-27 15:10:56 +0000

    [PS Survey Service] Initialize a survey service skeleton
    Bug: 308320418
    Change-Id: I9687773928d6b7aa0c01ec4a4bcf1ef7b9adcdb5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812424
    Commit-Queue: Alvin Lam <alvingo@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Alvin Lam <alvingo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347368}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -146,6 +146,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_notice_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.h"
 #include "chrome/browser/privacy_sandbox/privacy_sandbox_settings_factory.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_survey_factory.h"
 #include "chrome/browser/privacy_sandbox/tracking_protection_onboarding_factory.h"
 #include "chrome/browser/privacy_sandbox/tracking_protection_settings_factory.h"
 #include "chrome/browser/private_network_access/private_network_device_permission_context_factory.h"
@@ -1124,6 +1125,7 @@ void ChromeBrowserMainExtraPartsProfiles::
   PrivacySandboxNoticeServiceFactory::GetInstance();
   PrivacySandboxServiceFactory::GetInstance();
   PrivacySandboxSettingsFactory::GetInstance();
+  PrivacySandboxSurveyFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   PrivacySandboxSurveyDesktopControllerFactory::GetInstance();
 #endif
---
+++ components/privacy_sandbox/privacy_sandbox_survey_service.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "privacy_sandbox_survey_service.h"
+
+namespace privacy_sandbox {
+
+PrivacySandboxSurveyService::PrivacySandboxSurveyService(
+    PrefService* pref_service)
+    : pref_service_(pref_service) {}
+
+PrivacySandboxSurveyService::~PrivacySandboxSurveyService() = default;
+
+}  // namespace privacy_sandbox
