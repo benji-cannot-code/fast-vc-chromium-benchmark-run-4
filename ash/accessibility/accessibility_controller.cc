3261c54904e190819f53559781cf484ee85d2f58
LK Kupoluyi <lkupo@chromium.org>
2024-08-27 15:40:15 +0000

    [Disable Trackpad] Add disable trackpad mode pref
    Introducing new pref to control the behavior of internal trackpad disabling.
    
    Bug: 354187572
    Change-Id: I965778a409b82d1c53f4cd82b87ee5f1a48d8615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797888
    Reviewed-by: Ramin Halavati <rhalavati@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Commit-Queue: Jesulayomi Kupoluyi <lkupo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347384}

--- ash/accessibility/accessibility_controller.cc
+++ ash/accessibility/accessibility_controller.cc
@@ -276,6 +276,7 @@ constexpr const char* const kCopiedOnSigninAccessibilityPrefs[]{
     prefs::kAccessibilityDictationLocale,
     prefs::kAccessibilityDictationLocaleOfflineNudge,
     prefs::kAccessibilityDisableTrackpadEnabled,
+    prefs::kAccessibilityDisableTrackpadMode,
     prefs::kAccessibilityFocusHighlightEnabled,
     prefs::kAccessibilityHighContrastEnabled,
     prefs::kAccessibilityLargeCursorEnabled,
@@ -1204,6 +1205,8 @@ void AccessibilityController::RegisterProfilePrefs(
   registry->RegisterBooleanPref(prefs::kAccessibilityFaceGazeEnabled, false);
   registry->RegisterBooleanPref(prefs::kAccessibilityDisableTrackpadEnabled,
                                 false);
+  registry->RegisterIntegerPref(prefs::kAccessibilityDisableTrackpadMode,
+                                static_cast<int>(DisableTrackpadMode::kNever));
 
   // Not syncable because it might change depending on application locale,
   // user settings, and because different languages can cause speech recognition
@@ -2611,6 +2614,13 @@ void AccessibilityController::ObservePrefs(PrefService* prefs) {
             &AccessibilityController::UpdateFlashNotificationsFromPrefs,
             base::Unretained(this)));
   }
+  if (::features::IsAccessibilityDisableTrackpadEnabled()) {
+    pref_change_registrar_->Add(
+        prefs::kAccessibilityDisableTrackpadMode,
+        base::BindRepeating(
+            &AccessibilityController::UpdateDisableTrackpadFromPrefs,
+            base::Unretained(this)));
+  }
 
   for (const std::unique_ptr<Feature>& feature : features_) {
     // Log previous duration and clear duration metric if necessary
@@ -2647,6 +2657,9 @@ void AccessibilityController::ObservePrefs(PrefService* prefs) {
   }
   if (::features::IsAccessibilityFlashScreenFeatureEnabled()) {
     UpdateFlashNotificationsFromPrefs();
+  }
+  if (::features::IsAccessibilityDisableTrackpadEnabled()) {
+    UpdateDisableTrackpadFromPrefs();
   }
 }
 
@@ -2872,6 +2885,18 @@ void AccessibilityController::UpdateFlashNotificationsFromPrefs() {
       prefs::kAccessibilityFlashNotificationsEnabled));
   flash_screen_controller_->set_color(active_user_prefs_->GetInteger(
       prefs::kAccessibilityFlashNotificationsColor));
+}
+
+void AccessibilityController::UpdateDisableTrackpadFromPrefs() {
+  if (!disable_trackpad_event_rewriter_ ||
+      !::features::IsAccessibilityDisableTrackpadEnabled()) {
+    return;
+  }
+  // TODO(b:354176487): Send trackpad mode to event rewriter
+  disable_trackpad_event_rewriter_->SetEnabled(
+      active_user_prefs_->GetInteger(
+          prefs::kAccessibilityDisableTrackpadMode) !=
+      static_cast<int>(DisableTrackpadMode::kNever));
 }
 
 void AccessibilityController::UpdateColorCorrectionFromPrefs() {
--- chrome/browser/prefs/pref_service_incognito_allowlist.cc
+++ chrome/browser/prefs/pref_service_incognito_allowlist.cc
@@ -135,6 +135,7 @@ const char* const kPersistentPrefNames[] = {
     ash::prefs::kScreenMagnifierAcceleratorDialogHasBeenAccepted,
     ash::prefs::kShouldAlwaysShowAccessibilityMenu,
     ash::prefs::kAccessibilityDisableTrackpadEnabled,
+    ash::prefs::kAccessibilityDisableTrackpadMode,
 #endif  // BUILDFLAG(IS_CHROMEOS_ASH)
 #if !BUILDFLAG(IS_ANDROID)
     kAnimationPolicyAllowed,
