4b174d445b80fd4d2369d797f9efe35cdf2746bd
Oksana Zhuravlova <oksamyt@chromium.org>
2024-08-27 17:46:26 +0000

    [CodeHealth] Remove WebViewBrotliSupport and WebViewClearFunctorInBackground feature flags
    Since both features have been launched, the respective flags can be
    cleaned up.
    
    Bug: 362247538, 362247492
    Change-Id: I465d05f441ed268284a45e703aec9b4f1f9f939d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5813647
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347480}

--- android_webview/browser/aw_feature_map.cc
+++ android_webview/browser/aw_feature_map.cc
@@ -39,7 +39,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kWebViewRestrictSensitiveContent,
     &metrics::kAndroidMetricsAsyncMetricLogging,
     &features::kWebViewZoomKeyboardShortcuts,
-    &features::kWebViewClearFunctorInBackground,
     &safe_browsing::kHashPrefixRealTimeLookups,
     &features::kWebViewSupervisedUserSiteDetection,
     &features::kWebViewSupervisedUserSiteBlock,
