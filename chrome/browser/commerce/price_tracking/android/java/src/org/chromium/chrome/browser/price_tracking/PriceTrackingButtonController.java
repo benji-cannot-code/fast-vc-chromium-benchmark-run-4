7afebfdc17c4088139a0211ecf377c9602d35047
Salvador Guerrero <salg@google.com>
2024-08-27 14:02:53 +0000

    [CPA Price Tracking] Fixed usage of ObservableSupplier
    This CL fixes an incorrect use of ObservableSupplier's
    add/removeObserver, when called with a this::methodName method reference
    the observer never gets removed, because each invocation creates a
    different instance.
    
    Bug: 352206454, 352205950
    Change-Id: Idf35d28e5dcb0a331295ead50c55237dbe7a3ca1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809092
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347339}

--- chrome/browser/commerce/price_tracking/android/java/src/org/chromium/chrome/browser/price_tracking/PriceTrackingButtonController.java
+++ chrome/browser/commerce/price_tracking/android/java/src/org/chromium/chrome/browser/price_tracking/PriceTrackingButtonController.java
@@ -11,6 +11,7 @@ import android.view.View;
 
 import androidx.appcompat.content.res.AppCompatResources;
 
+import org.chromium.base.Callback;
 import org.chromium.base.supplier.ObservableSupplier;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.R;
@@ -46,6 +47,7 @@ public class PriceTrackingButtonController extends BaseButtonDataProvider {
     private final ObservableSupplier<BookmarkModel> mBookmarkModelSupplier;
     private final ObservableSupplier<Profile> mProfileSupplier;
     private final BottomSheetObserver mBottomSheetObserver;
+    private final Callback<Boolean> mPriceTrackingStateChangedCallback = this::updateButtonIcon;
     private final ButtonSpec mFilledButtonSpec;
     private final ButtonSpec mEmptyButtonSpec;
     private boolean mIsCurrentTabPriceTracked;
@@ -107,7 +109,7 @@ public class PriceTrackingButtonController extends BaseButtonDataProvider {
                 };
         mBottomSheetController.addObserver(mBottomSheetObserver);
 
-        mPriceTrackingCurrentTabStateSupplier.addObserver(this::updateButtonIcon);
+        mPriceTrackingCurrentTabStateSupplier.addObserver(mPriceTrackingStateChangedCallback);
     }
 
     private void updateButtonIcon(boolean isCurrentTabPriceTracked) {
@@ -125,7 +127,7 @@ public class PriceTrackingButtonController extends BaseButtonDataProvider {
         super.destroy();
 
         mBottomSheetController.removeObserver(mBottomSheetObserver);
-        mPriceTrackingCurrentTabStateSupplier.removeObserver(this::updateButtonIcon);
+        mPriceTrackingCurrentTabStateSupplier.removeObserver(mPriceTrackingStateChangedCallback);
     }
 
     @Override
