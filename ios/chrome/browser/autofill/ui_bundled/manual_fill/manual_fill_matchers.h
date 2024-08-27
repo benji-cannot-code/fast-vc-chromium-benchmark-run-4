b407e72a717311321d6d026b36719f7bdac6a83a
Vidhan Jain <vidhanj@google.com>
2024-08-27 12:19:57 +0000

    [iOS] Create a separate egtest file for plus address tests
    Bug: 327838014
    Change-Id: I132c10dd463ccc4bdf3d2ae538823237d199cca9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803089
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347301}

--- ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_matchers.h
+++ ios/chrome/browser/autofill/ui_bundled/manual_fill/manual_fill_matchers.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <Foundation/Foundation.h>
 
+#import "base/strings/utf_string_conversions.h"
+
 @protocol GREYMatcher;
 
 namespace manual_fill {
@@ -77,6 +79,24 @@ id<GREYMatcher> CreditCardTableViewWindowMatcher();
 // Returns a matcher for the button to trigger password generation on manual
 // fallback.
 id<GREYMatcher> SuggestPasswordMatcher();
+
+// Returns a matcher for the header view in the fallback view.
+id<GREYMatcher> ExpandedManualFillHeaderView();
+
+// Matcher for the expanded manual fill view.
+id<GREYMatcher> ExpandedManualFillView();
+
+// Matcher for the keyboard accessory's manual fill button.
+id<GREYMatcher> KeyboardAccessoryManualFillButton();
+
+// Matcher for the segmented control's address tab.
+id<GREYMatcher> SegmentedControlAddressTab();
+
+// Matcher for the segmented control's password tab.
+id<GREYMatcher> SegmentedControlPasswordTab();
+
+// Matcher for the chip button with the given `title`.
+id<GREYMatcher> ChipButton(std::u16string title);
 
 }  // namespace manual_fill
 
