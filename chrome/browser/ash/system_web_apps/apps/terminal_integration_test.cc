bfda9918d3b1c431b6c7204ef588afdce2a88375
Yuta Hijikata <ythjkt@google.com>
2024-08-27 05:51:16 +0000

    Disable AshIntegrationTest.Crosh.
    Turning on Chromevox enables screen ai, which is not supported in
    crosier right now. This test is currently blocking LKGM uprev.
    Once b/362289622 is fixed, the test should be re-enabled.
    
    Bug: 362178916
    Change-Id: Ief1f8622292fd7b4c17a798e804b91f54744d771
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815713
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Commit-Queue: Yuta Hijikata <ythjkt@chromium.org>
    Owners-Override: Yuta Hijikata <ythjkt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347173}

--- chrome/browser/ash/system_web_apps/apps/terminal_integration_test.cc
+++ chrome/browser/ash/system_web_apps/apps/terminal_integration_test.cc
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-IN_PROC_BROWSER_TEST_F(AshIntegrationTest, Crosh) {
+// TODO(b/362289622): Re-enable this test after fallback for chrome
+// screen ai has been implemented.
+IN_PROC_BROWSER_TEST_F(AshIntegrationTest, DISABLED_Crosh) {
   ui::ScopedAnimationDurationScaleMode zero_duration(
       ui::ScopedAnimationDurationScaleMode::ZERO_DURATION);
 
