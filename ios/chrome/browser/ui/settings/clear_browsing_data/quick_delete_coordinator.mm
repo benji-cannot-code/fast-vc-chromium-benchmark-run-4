13db1d0ae851c9954e300ed554043e050b0ab681
Mariam Ali <alimariam@google.com>
2024-08-27 10:24:36 +0000

    [iOS][QD] Fix layout on iPad to anchor in the center of the screen
    Screenshots:
    Before: https://screenshot.googleplex.com/7CdAQd8JrnwEL58
    After: https://screenshot.googleplex.com/BGvXVysgmpjb97h
    
    Fixed: 349537296
    Bug: 335387869
    Change-Id: Iaeb2fe0101a05d7a7484bb1e1ac4f293f7ce2aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796897
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347259}

--- ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_coordinator.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_coordinator.mm
@@ -83,6 +83,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           canPerformTabsClosureAnimation:_canPerformTabsClosureAnimation];
 
   _viewController = [[QuickDeleteViewController alloc] init];
+  _viewController.modalPresentationStyle = UIModalPresentationFormSheet;
   _mediator.consumer = _viewController;
   _mediator.presentationHandler = self;
 
