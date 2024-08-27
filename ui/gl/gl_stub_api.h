4c956f33e736e8e4023ea716e46691f42159168f
Zhenyao Mo <zmo@chromium.org>
2024-08-27 22:52:01 +0000

    Remove allow_unsafe_buffers from gl_stub_api.h
    Since it's OpenGL function signatures, there is nothing
    we can do. Wrap UNSAFE_BUFFERS around it.
    
    TEST=bots compile
    
    Bug: 351564777
    Change-Id: If816b6a05c386905db36f583597dbcecec9e39af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818373
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347692}

--- ui/gl/gl_stub_api.h
+++ ui/gl/gl_stub_api.h
@@ -3,16 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef UI_GL_GL_STUB_API_H_
 #define UI_GL_GL_STUB_API_H_
 
 #include <string>
 
+#include "base/compiler_specific.h"
 #include "ui/gl/gl_export.h"
 #include "ui/gl/gl_stub_api_base.h"
 
@@ -81,8 +77,10 @@ class GL_EXPORT GLStubApi: public GLStubApiBase {
   // and GPU fuzzers. We get a new GLStubApi for every case executed by
   // fuzzers, so we don't have to worry about ID exhaustion.
   void GenHelper(GLsizei count, GLuint* objects) {
-    for (GLsizei i = 0; i < count; ++i)
-      objects[i] = next_id_++;
+    for (GLsizei i = 0; i < count; ++i) {
+      // SAFETY: required from OpenGL across C API.
+      UNSAFE_BUFFERS(objects[i] = next_id_++);
+    }
   }
 
   std::string version_;
