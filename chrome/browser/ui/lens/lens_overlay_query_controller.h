7aefad98e0aa735eb473809a1cc10da5d0d75e5a
Juan Mojica <juanmojica@google.com>
2024-08-27 22:37:54 +0000

    [lensoverlay] Fix crash when sending multiple full image requests.
    Bug: b:362548243, 362572441
    Change-Id: I4d3c44caaffe1c0210da51330d39f1d222a46c0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818346
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347681}

--- chrome/browser/ui/lens/lens_overlay_query_controller.h
+++ chrome/browser/ui/lens/lens_overlay_query_controller.h
@@ -202,6 +202,7 @@ class LensOverlayQueryController {
   // Handles the endpoint fetch response for the initial request.
   void FullImageFetchResponseHandler(
       int64_t query_start_time_ms,
+      int request_sequence_id,
       std::unique_ptr<EndpointResponse> response);
 
   // Handles the response from a latency gen204 request.
@@ -382,6 +383,10 @@ class LensOverlayQueryController {
 
   // The current gen204 id for logging, set on each overlay invocation.
   uint64_t gen204_id_;
+
+  // The latest full image request sequence id. Used for cancelling any full
+  // image requests that have been superseded by another.
+  int latest_full_image_sequence_id_;
 
   base::WeakPtrFactory<LensOverlayQueryController> weak_ptr_factory_{this};
 };
