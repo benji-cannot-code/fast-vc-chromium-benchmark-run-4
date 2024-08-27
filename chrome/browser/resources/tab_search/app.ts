739a7db528bd50d321598ec9dad5222ecfbbee14
Emily Shack <emshack@chromium.org>
2024-08-27 18:13:58 +0000

    Declutter: Add TabOrganizationSelectorElement
    This adds a basic root element for selecting between the auto tab groups
    and declutter features. Currently just 2 buttons, no styling apart from
    a CSS placeholder color.
    
    Screenshot: https://screenshot.googleplex.com/AqdinsyfgPY5y4z
    Bug: 358383322
    Change-Id: I3530ccd3c79f04185f1a223b4bb30f01c25494c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807090
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1347511}

--- chrome/browser/resources/tab_search/app.ts
+++ chrome/browser/resources/tab_search/app.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import './auto_tab_groups/auto_tab_groups_page.js';
-import './declutter/declutter_page.js';
+import './tab_organization_selector.js';
 import './tab_search_page.js';
 import 'chrome://resources/cr_elements/cr_tabs/cr_tabs.js';
 import 'chrome://resources/cr_elements/cr_page_selector/cr_page_selector.js';
---
+++ chrome/browser/resources/tab_search/tab_organization_selector.html.ts
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {TabOrganizationSelectorElement} from './tab_organization_selector.js';
+import {OrganizationFeature} from './tab_organization_selector.js';
+
+export function getHtml(this: TabOrganizationSelectorElement) {
+  // clang-format off
+  return html`
+<!--_html_template_start_-->
+<div id="buttonContainer"
+    ?hidden=${this.selectedState_ !== OrganizationFeature.NONE}>
+  <cr-button id="autoTabGroupsButton" @click="${this.onAutoTabGroupsClick_}">
+    Auto tab groups
+  </cr-button>
+  <cr-button id="declutterButton" @click="${this.onDeclutterClick_}">
+    Declutter
+  </cr-button>
+</div>
+
+<div ?hidden=${this.selectedState_ !== OrganizationFeature.AUTO_TAB_GROUPS}>
+  <auto-tab-groups-page></auto-tab-groups-page>
+</div>
+
+<div ?hidden=${this.selectedState_ !== OrganizationFeature.DECLUTTER}>
+  <declutter-page></declutter-page>
+</div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
