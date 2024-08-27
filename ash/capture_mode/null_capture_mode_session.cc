2d28e493c3666e2ffd64301cc41eaf32ae84e832
luci-bisection@appspot.gserviceaccount.com <luci-bisection@appspot.gserviceaccount.com>
2024-08-27 08:43:06 +0000

    Revert "sunfish: Show search results panel on drag release"
    This reverts commit eebca4123517e945559a41d1f057deca212c2aa3.
    
    Reason for revert:
    LUCI Bisection has identified this change as the cause of a test failure. See the analysis: https://ci.chromium.org/ui/p/chromium/bisection/test-analysis/b/5714439362838528
    
    Sample build with failed test: https://ci.chromium.org/b/8738493679970262497
    Affected test(s):
    [ninja://ash:ash_unittests/SunfishTest.CaptureLabelView](https://ci.chromium.org/ui/test/chromium/ninja:%2F%2Fash:ash_unittests%2FSunfishTest.CaptureLabelView?q=VHash%3A676f19355540b1aa)
    
    If this is a false positive, please report it at http://b.corp.google.com/createIssue?component=1199205&description=Analysis%3A+https%3A%2F%2Fci.chromium.org%2Fui%2Fp%2Fchromium%2Fbisection%2Ftest-analysis%2Fb%2F5714439362838528&format=PLAIN&priority=P3&title=Wrongly+blamed+https%3A%2F%2Fchromium-review.googlesource.com%2Fc%2Fchromium%2Fsrc%2F%2B%2F5804838&type=BUG
    
    Original change's description:
    > sunfish: Show search results panel on drag release
    >
    > This CL hooks up the search results panel to be shown immediately when
    > a region is selected. In production, the panel might not be shown until
    > the results URL is fetched, but this immediate binding helps with
    > debugging.
    >
    > This also sets the image data to be shown in the search box for UI
    > debugging.
    >
    > This also parents the panel widget to the overlay container so it can
    > be stacked above the capture mode overlay.
    >
    > Demo: https://b.corp.google.com/issues/359317857#comment3
    >
    > Design: https://docs.google.com/document/d/1qYhaEC99x_Jnk0E7ktygSxqSdT4ihCSDO38f0z6RuQk/edit?resourcekey=0-lbfvLBs73jcGMkuqfS7vIw&tab=t.0#bookmark=id.d11je2ppmwaj
    >
    > Test: manual + modified
    > Bug: b/359317857
    > Change-Id: I7d2fe4a2a33e295528cb8972905c5f0afece2edf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804838
    > Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1347040}
    >
    
    Bug: b/359317857
    Change-Id: Iffa3828afa015076f8097e9d1bea4d1c13c3a7e7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5814795
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Sergey Poromov <poromov@chromium.org>
    Owners-Override: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347214}

--- ash/capture_mode/null_capture_mode_session.cc
+++ ash/capture_mode/null_capture_mode_session.cc
@@ -114,9 +114,6 @@ NullCaptureModeSession::GetWindowsToIgnoreFromWidgets() {
   return std::set<aura::Window*>();
 }
 
-void NullCaptureModeSession::ShowSearchResultsPanel(
-    const gfx::ImageSkia& image) {}
-
 void NullCaptureModeSession::InitInternal() {
   layer()->SetName("NullCaptureModeSession");
 }
--- ash/capture_mode/search_results_panel.h
+++ ash/capture_mode/search_results_panel.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 class AshWebView;
-class SunfishSearchBoxView;
 
 // Container for the search results view and other UI such as the search box,
 // close button, etc.
@@ -28,14 +27,13 @@ class ASH_EXPORT SearchResultsPanel : public views::View {
   SearchResultsPanel& operator=(const SearchResultsPanel&) = delete;
   ~SearchResultsPanel() override;
 
-  static std::unique_ptr<views::Widget> CreateWidget(aura::Window* const root);
+  // Currently only used in tests.
+  static std::unique_ptr<views::Widget> CreateWidget();
 
-  // Sets the search box image thumbnail.
-  void SetSearchBoxImage(const gfx::ImageSkia& image);
+  AshWebView* search_results_view() const { return search_results_view_; }
 
  private:
   // Owned by the views hierarchy.
-  raw_ptr<SunfishSearchBoxView> search_box_view_;
   raw_ptr<AshWebView> search_results_view_;
 };
 
