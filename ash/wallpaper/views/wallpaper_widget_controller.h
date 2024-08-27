efe67c006d70bc0cdb2b57deac37320c56b3b890
Mark Rowe <markrowe@chromium.org>
2024-08-27 18:05:29 +0000

    Change WallpaperWidgetController::wallpaper_view_ to be a raw_ptr
    Fixed: 346694064
    Change-Id: Ice7a4f22be48b739d75c6b107797761a264e95e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5814803
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Commit-Queue: Jason Thai <jasontt@chromium.org>
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347499}

--- ash/wallpaper/views/wallpaper_widget_controller.h
+++ ash/wallpaper/views/wallpaper_widget_controller.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wallpaper/wallpaper_constants.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "ui/color/color_provider_source_observer.h"
 #include "ui/compositor/layer_animation_observer.h"
 #include "ui/display/display_observer.h"
@@ -121,9 +120,7 @@ class ASH_EXPORT WallpaperWidgetController
   std::unique_ptr<ui::LayerTreeOwner> old_layer_tree_owner_;
 
   // Pointer to the wallpaper view owned by |widget_|.
-  // This field is not a raw_ptr<> because it was filtered by the rewriter
-  // for: #addr-of
-  RAW_PTR_EXCLUSION WallpaperView* wallpaper_view_ = nullptr;
+  raw_ptr<WallpaperView> wallpaper_view_ = nullptr;
 
   // A solid-color layer stacked below the clipped `wallpaper_view_`
   // layer. Note that it can't be stacked at bottom since the `shield_view_` may
