b1f850a64e1c7ba70d908d67450a3620674f53a7
Aya El Gendy <ayag@chromium.org>
2024-08-28 14:38:03 +0000

    [CodeHelath] Fix argument comments syntax
    This is a pure refactoring CL.
    
    Bug: b/362241941
    Change-Id: Ib0ab9340480ed7ec580e06b95a58ed79c79767f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812905
    Commit-Queue: Aya Elgendy‎ <ayag@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347997}

--- chromeos/ash/components/tether/tether_network_disconnection_handler_unittest.cc
+++ chromeos/ash/components/tether/tether_network_disconnection_handler_unittest.cc
@@ -124,7 +124,7 @@ class TetherNetworkDisconnectionHandlerTest : public testing::Test {
   }
 
   const base::test::TaskEnvironment task_environment_;
-  NetworkStateTestHelper helper_{true /* use_default_devices_and_services */};
+  NetworkStateTestHelper helper_{/*use_default_devices_and_services=*/true};
 
   std::string wifi_service_path_;
 
