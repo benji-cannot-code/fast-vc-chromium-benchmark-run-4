08acc24e43ec68b138305d12ec93469b3b664eed
Lukasz Anforowicz <lukasza@chromium.org>
2024-08-28 14:32:45 +0000

    Roll serde_json: 1.0.124 => 1.0.127 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * serde_json: 1.0.124 => 1.0.127
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * serde_json@1.0.127: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I2e75ed81dc60ed7ae0a58cdfd8c2fe3e32b8c6db
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5811887
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347990}

--- third_party/rust/chromium_crates_io/vendor/serde_json-1.0.127/src/lexical/large_powers.rs
+++ third_party/rust/chromium_crates_io/vendor/serde_json-1.0.127/src/lexical/large_powers.rs
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 //! Precalculated large powers for limbs.
 
-#[cfg(limb_width_32)]
+#[cfg(fast_arithmetic = "32")]
 pub(crate) use super::large_powers32::*;
 
-#[cfg(limb_width_64)]
+#[cfg(fast_arithmetic = "64")]
 pub(crate) use super::large_powers64::*;
--- third_party/rust/chromium_crates_io/vendor/serde_json-1.0.127/src/lexical/small_powers.rs
+++ third_party/rust/chromium_crates_io/vendor/serde_json-1.0.127/src/lexical/small_powers.rs
@@ -4,19 +4,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //! Pre-computed small powers.
 
 // 32 BIT
-#[cfg(limb_width_32)]
+#[cfg(fast_arithmetic = "32")]
 pub(crate) const POW5_32: [u32; 14] = [
     1, 5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625,
     1220703125,
 ];
 
-#[cfg(limb_width_32)]
+#[cfg(fast_arithmetic = "32")]
 pub(crate) const POW10_32: [u32; 10] = [
     1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
 ];
 
 // 64 BIT
-#[cfg(limb_width_64)]
+#[cfg(fast_arithmetic = "64")]
 pub(crate) const POW5_64: [u64; 28] = [
     1,
     5,
