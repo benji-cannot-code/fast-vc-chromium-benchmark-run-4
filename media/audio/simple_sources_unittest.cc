7b1feab6d887ff5916a12343f470a6db78536ed4
Tom Sepez <tsepez@chromium.org>
2024-08-28 18:09:22 +0000

    Avoid unsafe forms for base::File::WriteAtCurrentPos()
    Use the span based forms instead. Adjust callers to accommodate
    an optional<size_t> return.
    
    This CL was uploaded by git cl split.
    
    R=mfoltz@chromium.org
    
    Bug: 42271176
    Change-Id: Ia7d1ff4ef276a11bbbb9cc5ec53ccd7babfc15f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5819671
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348156}

--- media/audio/simple_sources_unittest.cc
+++ media/audio/simple_sources_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <memory>
 
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/time/time.h"
 #include "media/audio/audio_io.h"
@@ -124,7 +125,7 @@ TEST(SimpleSources, FileSourceTestDataWithoutLooping) {
   ASSERT_TRUE(base::CreateTemporaryFile(&temp_path));
   base::File temp(temp_path,
                   base::File::FLAG_WRITE | base::File::FLAG_OPEN_ALWAYS);
-  temp.WriteAtCurrentPos(kTestAudioData, kTestAudioDataSize);
+  temp.WriteAtCurrentPos(base::byte_span_from_cstring(kTestAudioData));
   ASSERT_EQ(kTestAudioDataSize, static_cast<size_t>(temp.GetLength()));
   temp.Close();
 
@@ -162,7 +163,7 @@ TEST(SimpleSources, FileSourceTestDataWithLooping) {
   ASSERT_TRUE(base::CreateTemporaryFile(&temp_path));
   base::File temp(temp_path,
                   base::File::FLAG_WRITE | base::File::FLAG_OPEN_ALWAYS);
-  temp.WriteAtCurrentPos(kTestAudioData, kTestAudioDataSize);
+  temp.WriteAtCurrentPos(base::byte_span_from_cstring(kTestAudioData));
   ASSERT_EQ(kTestAudioDataSize, static_cast<size_t>(temp.GetLength()));
   temp.Close();
 
@@ -217,7 +218,7 @@ TEST(SimpleSources, FileSourceCorruptTestDataFails) {
   ASSERT_TRUE(base::CreateTemporaryFile(&temp_path));
   base::File temp(temp_path,
                   base::File::FLAG_WRITE | base::File::FLAG_OPEN_ALWAYS);
-  temp.WriteAtCurrentPos(kTestAudioData, kTestAudioDataSize);
+  temp.WriteAtCurrentPos(base::byte_span_from_cstring(kTestAudioData));
 
   // Corrupt the header.
   temp.Write(3, "0x00", 1);
