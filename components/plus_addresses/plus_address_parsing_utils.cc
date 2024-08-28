55fed185176c5b7c464a437c57805dc7c9f92d92
Norge Vizcay <vizcay@google.com>
2024-08-28 13:11:54 +0000

    Stop invalidating reserve requests on http domains.
    The CL fixes an issue where ReservePlusAddress calls on http domains are
    considered invalid. The reason is because the FacetURI class
    does not consider http domain valid facets. This means that the parsing
    of server responses when this contains http facets fails.
    
    Bug: 362237423
    Change-Id: I758dc439f1f27eb8c60b9d73e574d5f1e0e1096c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5821833
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347959}

--- components/plus_addresses/plus_address_parsing_utils.cc
+++ components/plus_addresses/plus_address_parsing_utils.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/plus_address_types.h"
 #include "components/plus_addresses/webdata/plus_address_webdata_service.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
+#include "url/origin.h"
 
 namespace plus_addresses {
 
@@ -68,9 +69,13 @@ std::optional<PlusProfile> ParsePlusProfileFromV1Dict(base::Value::Dict dict) {
       !is_confirmed.has_value()) {
     return std::nullopt;
   }
+  GURL url = GURL(facet_str);
   affiliations::FacetURI facet =
       affiliations::FacetURI::FromPotentiallyInvalidSpec(facet_str);
-  if (!facet.is_valid()) {
+  // An exception is made for `http` domains as these are considered invalid by
+  // the `FacetURI` class.
+  if (!facet.is_valid() &&
+      (!url.is_valid() || !url.SchemeIs(url::kHttpScheme))) {
     return std::nullopt;
   }
   return PlusProfile(std::move(profile_id), std::move(facet),
