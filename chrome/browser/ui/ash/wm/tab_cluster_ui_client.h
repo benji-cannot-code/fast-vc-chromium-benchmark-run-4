f7c75aba731cfe2aa8fd4af95f395c8c7ce94033
Eriko Kurimoto <elkurin@chromium.org>
2024-08-28 03:34:48 +0000

    ash: Create //chrome/browser/ui/ash/wm
    Bug: b:359062448
    Change-Id: I5476c3f523f457dbdc548f5321a32b2909b115d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799924
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347802}

--- chrome/browser/ui/ash/wm/tab_cluster_ui_client.h
+++ chrome/browser/ui/ash/wm/tab_cluster_ui_client.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_TAB_CLUSTER_UI_CLIENT_H_
-#define CHROME_BROWSER_UI_ASH_TAB_CLUSTER_UI_CLIENT_H_
+#ifndef CHROME_BROWSER_UI_ASH_WM_TAB_CLUSTER_UI_CLIENT_H_
+#define CHROME_BROWSER_UI_ASH_WM_TAB_CLUSTER_UI_CLIENT_H_
 
 #include <map>
 
@@ -51,4 +51,4 @@ class TabClusterUIClient : public TabStripModelObserver {
   std::map<content::WebContents*, ash::TabClusterUIItem*> contents_item_map_;
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_TAB_CLUSTER_UI_CLIENT_H_
+#endif  // CHROME_BROWSER_UI_ASH_WM_TAB_CLUSTER_UI_CLIENT_H_
