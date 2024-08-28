3b987cf536d514ee3cf27236f3910bdd1ec2304c
Fred Shih <ffred@chromium.org>
2024-08-28 22:12:25 +0000

    Revert "Add chrome://flags entry for toasts"
    This reverts commit d639505290d0ae3bb7fdb2ba6f7d513782bb0a4e.
    
    Reason for revert: Breaking the build, redefinition of "kToastTimeout"
    Original change's description:
    > Add chrome://flags entry for toasts
    >
    > Used the same variable name/comment from Steven's CL:
    > https://chromium-review.googlesource.com/c/chromium/src/+/5814817
    >
    > Bug: 361173871
    > Change-Id: I8cba206d4aaf351db7ad9ef2c1b0f3d8ae17c12f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818292
    > Reviewed-by: Steven Luong <stluong@chromium.org>
    > Commit-Queue: Alison Gale <agale@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1348303}
    
    Bug: 361173871
    Change-Id: I1c5061d2b28cf7fa287020665cbd885b3a15a5b8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823643
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Fred Shih <ffred@chromium.org>
    Commit-Queue: Fred Shih <ffred@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348308}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2138,11 +2138,6 @@ extern const char kToolbarPinningName[];
 extern const char kToolbarPinningDescription[];
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
-extern const char kTopChromeToastsName[];
-extern const char kTopChromeToastsDescription[];
-#endif
-
 extern const char kTopChromeTouchUiName[];
 extern const char kTopChromeTouchUiDescription[];
 
