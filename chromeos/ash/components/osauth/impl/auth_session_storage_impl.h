714c3b2cae29e147c4e6c9451bef6cec8458672a
Renato Silva <rrsilva@google.com>
2024-08-28 13:38:35 +0000

    AuthSessionStorage: Add utility for tests
    Add a helper method to  AuthSessionStorage so that tests can check
    the number of keep alive counters for a specific token. This will
    allow us to ensure that a ScopedSessionRefresher is being employed.
    
    Bug: b/356348917, b/348326316
    Change-Id: Iad687911ae0f7e4a20ad0155bff21e74933a7bc9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803404
    Reviewed-by: Simon Hangl <simonha@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Renato Silva <rrsilva@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347970}

--- chromeos/ash/components/osauth/impl/auth_session_storage_impl.h
+++ chromeos/ash/components/osauth/impl/auth_session_storage_impl.h
@@ -70,6 +70,8 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_OSAUTH) AuthSessionStorageImpl
   std::unique_ptr<ScopedSessionRefresher> KeepAlive(
       const AuthProofToken& token) override;
 
+  bool CheckHasKeepAliveForTesting(const AuthProofToken& token) const override;
+
  private:
   friend class ScopedSessionRefresherImpl;
   enum class TokenState {
