d796c6da4e1353533294937292597311cc0ca8af
Mark Rowe <markrowe@chromium.org>
2024-08-28 01:48:10 +0000

    Change VideoDecodeStatsReporterTest::interceptor_ to be a raw_ptr
    Fixed: 346693973
    Change-Id: I332c6c1c07337b0c9b80a3dfe459cb40df728133
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817902
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347770}

--- third_party/blink/renderer/platform/media/video_decode_stats_reporter_unittest.cc
+++ third_party/blink/renderer/platform/media/video_decode_stats_reporter_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 
 #include "base/functional/bind.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/task/single_thread_task_runner.h"
@@ -123,6 +123,9 @@ class VideoDecodeStatsReporterTest : public ::testing::Test {
   }
 
   void TearDown() override {
+    // Depends on `reporter_` so must be cleared before it is destroyed.
+    interceptor_ = nullptr;
+
     // Break the IPC connection if reporter still around.
     reporter_.reset();
 
@@ -165,7 +168,7 @@ class VideoDecodeStatsReporterTest : public ::testing::Test {
   // VideoDecodeStatsRecorder. The interceptor serves as a mock recorder to
   // verify reporter/recorder interactions.
   mojo::PendingRemote<media::mojom::VideoDecodeStatsRecorder>
-  SetupRecordInterceptor(RecordInterceptor** interceptor_ptr) {
+  SetupRecordInterceptor(raw_ptr<RecordInterceptor>* interceptor_ptr) {
     // Capture a the interceptor pointer for verifying recorder calls. Lifetime
     // will be managed by the |recorder_remote|.
     auto interceptor = std::make_unique<RecordInterceptor>();
@@ -364,8 +367,7 @@ class VideoDecodeStatsReporterTest : public ::testing::Test {
   // Points to the interceptor that acts as a VideoDecodeStatsRecorder. The
   // object is owned by mojo::Remote<VideoDecodeStatsRecorder>, which is itself
   // owned by |reporter_|.
-  // RAW_PTR_EXCLUSION: #addr-of
-  RAW_PTR_EXCLUSION RecordInterceptor* interceptor_ = nullptr;
+  raw_ptr<RecordInterceptor> interceptor_ = nullptr;
 
   // The VideoDecodeStatsReporter being tested.
   std::unique_ptr<VideoDecodeStatsReporter> reporter_;
