81cca05fe27f49dcc043cd22a2b2248f764bbf40
Sophie Wen <sophiewen@google.com>
2024-08-28 00:57:53 +0000

    sunfish: Make SearchResultsPanel draggable
    This CL makes `SearchResultsPanel` draggable by extending
    `SystemPanelView`.
    
    Also updates some UI and adds padding to make it easier to drag.
    
    Demo: https://b.corp.google.com/issues/358605540#comment6
    
    Test: ash_unittests
    Fixed: b/358605540
    Change-Id: I7f4566d3e2db2184812adfd9613badd884ee8728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5789216
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347750}

--- ash/capture_mode/search_results_panel.cc
+++ ash/capture_mode/search_results_panel.cc
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/ash_web_view.h"
 #include "ash/public/cpp/ash_web_view_factory.h"
 #include "ash/public/cpp/shell_window_ids.h"
+#include "ash/public/cpp/style/color_provider.h"
 #include "ash/shell.h"
 #include "components/vector_icons/vector_icons.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/image_model.h"
 #include "ui/chromeos/styles/cros_tokens_color_mappings.h"
+#include "ui/compositor/layer.h"
 #include "ui/display/screen.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
@@ -28,8 +30,12 @@ namespace ash {
 namespace {
 
 // TODO(sophiewen): Remove hardcoded values when we get UX specs.
-constexpr int kSearchResultsPanelWidth = 600;
+inline constexpr int kPanelCornerRadius = 16;
+inline constexpr int kSearchResultsPanelWidth = 600;
+constexpr char kValidSearchUrl[] =
+    "https://www.google.com/search?q=cat&gsc=1&masfc=c";
 const std::u16string kSearchBoxPlaceholderText = u"Add to your search";
+inline constexpr gfx::Insets kPanelPadding = gfx::Insets::TLBR(12, 15, 15, 15);
 
 }  // namespace
 
@@ -93,6 +99,7 @@ SearchResultsPanel::SearchResultsPanel() {
       ->SetOrientation(views::LayoutOrientation::kVertical)
       .SetMainAxisAlignment(views::LayoutAlignment::kCenter)
       .SetCrossAxisAlignment(views::LayoutAlignment::kStart)
+      .SetInteriorMargin(kPanelPadding)
       .SetCollapseMargins(true);
   search_box_view_ = AddChildView(std::make_unique<SunfishSearchBoxView>());
   search_results_view_ =
@@ -104,10 +111,16 @@ SearchResultsPanel::SearchResultsPanel() {
 
   // TODO(b/356878705): Replace this when the backend is hooked up. Currently
   // used for UI debugging.
-  search_results_view_->Navigate(GURL("https://www.google.com/search?q=cat"));
+  search_results_view_->Navigate(GURL(kValidSearchUrl));
 
-  SetBackground(views::CreateThemedSolidBackground(
-      cros_tokens::kCrosSysSystemBaseElevated));
+  SetBackground(views::CreateThemedRoundedRectBackground(
+      cros_tokens::kCrosSysSystemBaseElevated, kPanelCornerRadius));
+  SetPaintToLayer();
+  layer()->SetRoundedCornerRadius(gfx::RoundedCornersF{kPanelCornerRadius});
+  layer()->SetFillsBoundsOpaquely(false);
+  layer()->SetIsFastRoundedCorner(true);
+  layer()->SetBackgroundBlur(ColorProvider::kBackgroundBlurSigma);
+  layer()->SetBackdropFilterQuality(ColorProvider::kBackgroundBlurQuality);
 }
 
 SearchResultsPanel::~SearchResultsPanel() = default;
--- ash/capture_mode/search_results_panel.h
+++ ash/capture_mode/search_results_panel.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/wm/system_panel_view.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/widget/widget.h"
 
@@ -19,8 +20,8 @@ class SunfishSearchBoxView;
 
 // Container for the search results view and other UI such as the search box,
 // close button, etc.
-class ASH_EXPORT SearchResultsPanel : public views::View {
-  METADATA_HEADER(SearchResultsPanel, views::View)
+class ASH_EXPORT SearchResultsPanel : public SystemPanelView {
+  METADATA_HEADER(SearchResultsPanel, SystemPanelView)
 
  public:
   SearchResultsPanel();
@@ -29,6 +30,8 @@ class ASH_EXPORT SearchResultsPanel : public views::View {
   ~SearchResultsPanel() override;
 
   static std::unique_ptr<views::Widget> CreateWidget(aura::Window* const root);
+
+  AshWebView* search_results_view() const { return search_results_view_; }
 
   // Sets the search box image thumbnail.
   void SetSearchBoxImage(const gfx::ImageSkia& image);
