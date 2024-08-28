c6b9e928a2f78277ff89c2ca9d8c052c894bffe8
Tom Sepez <tsepez@chromium.org>
2024-08-28 00:18:20 +0000

    Avoid unsafe forms for base::File::WriteAtCurrentPos()
    Use the span based forms instead. Adjust callers to accommodate
    an optional<size_t> return.
    
    This CL was uploaded by git cl split.
    
    R=yzshen@chromium.org
    
    Bug: 42271176
    Change-Id: I41c90face2ee431676cd42e43cfa324548b1e646
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5819691
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347733}

--- mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
+++ mojo/public/tools/fuzzers/mojo_fuzzer_message_dump.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stddef.h>
 #include <stdint.h>
 
+#include "base/compiler_specific.h"
 #include "base/containers/flat_map.h"
+#include "base/containers/span.h"
 #include "base/files/file.h"
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
@@ -63,9 +65,8 @@ class MessageDumper : public mojo::MessageFilter {
     }
 
     size_t size = message->data_num_bytes();
-    const char* data = reinterpret_cast<const char*>(message->data());
-    int ret = file.WriteAtCurrentPos(data, size);
-    if (ret != static_cast<int>(size)) {
+    const uint8_t* data = message->data();
+    if (!file.WriteAtCurrentPosAndCheck(UNSAFE_TODO(base::span(data, size)))) {
       LOG(ERROR) << "Failed to write " << size << " bytes.";
       return false;
     }
