2ce8e4208762ccc3c25994214266e869361d79c6
Radu Nitescu <radunitescu@google.com>
2024-08-28 14:43:31 +0000

    [iOS][LO] Lock orientation to portrait for Lens Overlay
    Fixed: 359659375
    Change-Id: I76fac7d8840d2c0c5f708ccb497bdf77eca79d33
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817128
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348002}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
@@ -56,6 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.selectionViewController didMoveToParentViewController:self];
 }
 
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations {
+  return UIInterfaceOrientationMaskPortrait;
+}
+
 #pragma mark - Accessibility
 
 - (BOOL)accessibilityPerformEscape {
