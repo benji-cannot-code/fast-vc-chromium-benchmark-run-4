6410c1ca3fd92fafaa6cdd074df514c2ca410705
Darren Shen <shend@chromium.org>
2024-08-28 07:18:33 +0000

    Move PickerController::emoji_suggester_ to Session.
    Bug: b:360229206
    Change-Id: I55f7c5e73a87022cba5a7fee995099d4649e1709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820531
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347858}

--- ash/picker/picker_controller.h
+++ ash/picker/picker_controller.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/picker/metrics/picker_feature_usage_metrics.h"
 #include "ash/picker/metrics/picker_session_metrics.h"
 #include "ash/picker/model/picker_emoji_history_model.h"
+#include "ash/picker/model/picker_emoji_suggester.h"
 #include "ash/picker/model/picker_model.h"
 #include "ash/picker/picker_asset_fetcher_impl_delegate.h"
 #include "ash/picker/picker_caps_lock_bubble_controller.h"
@@ -49,7 +50,6 @@ namespace ash {
 
 class PickerAssetFetcher;
 class PickerClient;
-class PickerEmojiSuggester;
 class PickerModel;
 class PickerPasteRequest;
 class PickerSearchController;
@@ -166,11 +166,13 @@ class ASH_EXPORT PickerController : public PickerViewDelegate,
   struct Session {
     PickerModel model;
     PickerEmojiHistoryModel emoji_history_model;
+    PickerEmojiSuggester emoji_suggester;
 
     Session(PrefService* prefs,
             ui::TextInputClient* focused_client,
             input_method::ImeKeyboard* ime_keyboard,
-            PickerModel::EditorStatus editor_status);
+            PickerModel::EditorStatus editor_status,
+            PickerEmojiSuggester::GetNameCallback get_name);
   };
 
   void ShowWidget(base::TimeTicks trigger_event_timestamp,
@@ -187,7 +189,6 @@ class ASH_EXPORT PickerController : public PickerViewDelegate,
   PickerFeatureTour feature_tour_;
   PickerCapsLockBubbleController caps_lock_bubble_controller_;
   std::unique_ptr<Session> session_;
-  std::unique_ptr<PickerEmojiSuggester> emoji_suggester_;
   views::UniqueWidgetPtr widget_;
   std::unique_ptr<PickerAssetFetcher> asset_fetcher_;
   std::unique_ptr<PickerInsertMediaRequest> insert_media_request_;
