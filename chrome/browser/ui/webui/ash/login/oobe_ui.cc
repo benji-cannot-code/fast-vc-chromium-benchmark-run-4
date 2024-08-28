9bb8df80076ae2e77e2c06db6798cf64e7fa7316
Bohdan Tyshchenko <bohdanty@google.com>
2024-08-28 03:22:38 +0000

    [OOBE] - Add base files for the SplitModifier screen
    Bug: b/357019947
    Change-Id: I1df55052354d6ae0d0e7694119ecd6d1d3b48bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817052
    Commit-Queue: Bohdan Tyshchenko <bohdanty@google.com>
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347799}

--- chrome/browser/ui/webui/ash/login/oobe_ui.cc
+++ chrome/browser/ui/webui/ash/login/oobe_ui.cc
@@ -376,6 +376,9 @@ void CreateAndAddOobeUIDataSource(Profile* profile,
                      base::FeatureList::IsEnabled(
                          remoting::features::kEnableCrdAdminRemoteAccessV2));
 
+  source->AddBoolean("isSplitModifierKeyboardInfoEnabled",
+                     features::IsOobeSplitModifierKeyboardInfoEnabled());
+
   // Configure shared resources
   AddProductLogoResources(source);
   if (ash::features::IsBootAnimationEnabled()) {
