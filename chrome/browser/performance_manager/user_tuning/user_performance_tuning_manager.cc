3df3378ae3020b3f221003a05c07f6b22e8c3ac2
stluong <stluong@chromium.org>
2024-08-28 20:27:32 +0000

    Update PreDiscardResourceUsage data when discarding multiple times
    Before the discard refactor, a new web contents is created each time we
    discard so discarding a tab multiple times will cause a new
    PreDiscardResourceUsage to be created each time and thus have the most
    up to date memory usage. However, after the refactor, a discarded tab
    no longer replaces its contents with a new web contents and cause the
    existing PreDiscardResource usage tab helper to be out of date.
    
    This CL fixes this bug by updating the existing PreDiscardResourceUsage
    data rather then attempting and failing to create a new
    PreDiscardResourceUsage tab helper.
    
    Fixed: 362364193
    Change-Id: Ia7817469b77fed179cd63286e5f34d52d34e07f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818211
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1348245}

--- chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
+++ chrome/browser/performance_manager/user_tuning/user_performance_tuning_manager.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback_helpers.h"
 #include "base/notreached.h"
 #include "base/run_loop.h"
+#include "base/time/time.h"
 #include "base/values.h"
 #include "chrome/browser/performance_manager/metrics/page_resource_monitor.h"
 #include "chrome/browser/performance_manager/policies/memory_saver_mode_policy.h"
@@ -99,10 +100,19 @@ UserPerformanceTuningManager::PreDiscardResourceUsage::PreDiscardResourceUsage(
     : content::WebContentsUserData<PreDiscardResourceUsage>(*contents),
       memory_footprint_estimate_(memory_footprint_estimate),
       discard_reason_(discard_reason),
-      discard_liveticks_(base::LiveTicks::Now()) {}
+      discard_live_ticks_(base::LiveTicks::Now()) {}
 
 UserPerformanceTuningManager::PreDiscardResourceUsage::
     ~PreDiscardResourceUsage() = default;
+
+void UserPerformanceTuningManager::PreDiscardResourceUsage::UpdateDiscardInfo(
+    uint64_t memory_footprint_estimate_kb,
+    ::mojom::LifecycleUnitDiscardReason discard_reason,
+    base::LiveTicks discard_live_ticks) {
+  memory_footprint_estimate_ = memory_footprint_estimate_kb;
+  discard_reason_ = discard_reason;
+  discard_live_ticks_ = discard_live_ticks;
+}
 
 // static
 bool UserPerformanceTuningManager::HasInstance() {
