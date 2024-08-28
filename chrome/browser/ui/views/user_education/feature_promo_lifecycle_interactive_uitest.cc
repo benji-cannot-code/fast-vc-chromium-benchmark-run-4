901e2f1ff796947fb1ba34d2e78612737bb80b92
Lingqi Chi <lingqi@chromium.org>
2024-08-28 06:03:10 +0000

    Disable flaky FeaturePromoLifecycleAppUiTest.ShowForTwoApps test on Linux
    Sample: https://ci.chromium.org/ui/p/chrome/builders/ci/linux64-rel-ready/31359/overview
    
    Bug: 362182859
    Change-Id: I19563cc5a726393371f03dac64dbb9ef93bd3e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820832
    Owners-Override: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Auto-Submit: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347837}

--- chrome/browser/ui/views/user_education/feature_promo_lifecycle_interactive_uitest.cc
+++ chrome/browser/ui/views/user_education/feature_promo_lifecycle_interactive_uitest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sstream>
 #include <utility>
 
+#include "build/build_config.h"
 #include "base/containers/contains.h"
 #include "base/feature_list.h"
 #include "base/functional/callback_helpers.h"
@@ -611,7 +612,13 @@ IN_PROC_BROWSER_TEST_F(FeaturePromoLifecycleAppUiTest, HasPromoBeenDismissed) {
       CheckDismissed(true, &kFeaturePromoLifecycleTestPromo, app1_id_));
 }
 
-IN_PROC_BROWSER_TEST_F(FeaturePromoLifecycleAppUiTest, ShowForTwoApps) {
+// TODO(crbug.com/362182859): flaky on Linux.
+#if BUILDFLAG(IS_LINUX)
+#define MAYBE_ShowForTwoApps DISABLED_ShowForTwoApps
+#else
+#define MAYBE_ShowForTwoApps ShowForTwoApps
+#endif
+IN_PROC_BROWSER_TEST_F(FeaturePromoLifecycleAppUiTest, MAYBE_ShowForTwoApps) {
   Browser* const app_browser = LaunchWebAppBrowser(app1_id_);
   Browser* const app_browser2 = LaunchWebAppBrowser(app2_id_);
   RunTestSequenceInContext(
