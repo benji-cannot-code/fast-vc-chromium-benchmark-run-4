ab73173e1583254bc8315b21371c5806336d8e8e
Jason Zhang <jiajunz@google.com>
2024-08-28 23:09:34 +0000

    [CrOS Network] Do not show out-of-range when first open config dialog
    The internet-config-dialog gets the error from NetworkState::GetError().
    This error is actually the cached error from previous attempts. So, if
    the last attempt got out-of-range error, the error would still be shown
    even if the user reopens the dialog. But the trick is if the user are
    able to open up the dialog, it most likely the network is already in
    range because we only show the networks in-range in QS.
    
    Fixed: b/353886177
    Change-Id: I1c0681eaacf857ab827f47595bb5580fda9f8d76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817882
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348331}

--- chrome/browser/resources/chromeos/internet_config_dialog/internet_config_dialog.ts
+++ chrome/browser/resources/chromeos/internet_config_dialog/internet_config_dialog.ts
@@ -87,6 +87,11 @@ export class InternetConfigDialogElement extends
       enableConnect_: Boolean,
 
       /**
+       * Whether the connection has been attempted.
+       */
+      connectClicked_: Boolean,
+
+      /**
        * Set by network-config when a configuration error occurs.
        */
       error_: {
@@ -102,6 +107,7 @@ export class InternetConfigDialogElement extends
   private type_: string;
   private prefilledProperties_: ConfigProperties|null;
   private enableConnect_: boolean;
+  private connectClicked_: boolean;
   private error_: string;
 
   override connectedCallback() {
@@ -121,6 +127,7 @@ export class InternetConfigDialogElement extends
       this.guid_ = params.get('guid') || '';
       this.prefilledProperties_ = null;
     }
+    this.connectClicked_ = false;
 
     ColorChangeUpdater.forDocument().start();
 
@@ -138,6 +145,14 @@ export class InternetConfigDialogElement extends
     return this.i18n('internetJoinType', type);
   }
 
+  private shouldShowError_(): boolean {
+    // Do not show "out-of-range" error if the dialog is just opened.
+    if (!this.connectClicked_ && this.error_ === 'out-of-range') {
+      return false;
+    }
+    return !!this.error_;
+  }
+
   private getError_(): string {
     if (this.i18nExists(this.error_)) {
       return this.i18n(this.error_);
@@ -151,6 +166,11 @@ export class InternetConfigDialogElement extends
 
   private onConnectClick_(): void {
     this.$.networkConfig.connect();
+    this.connectClicked_ = true;
+  }
+
+  setErrorForTesting(error: string): void {
+    this.error_ = error;
   }
 }
 
