67f0e6b0300de0758c86ea2a2c74cce0a413673c
David Maunder <davidjm@google.com>
2024-08-28 00:19:52 +0000

    UI for price tracking promo in magic stack UI
    The following adds the UI for the price tracking promo card in
    the magic stack. It uses the fallback image (displayed in the
    event that we can't get the product image for the user's most
    recent subscription). The product image will be added in a later CL.
    
    Screenshot: https://screenshot.googleplex.com/6S5t347NqUKtkDN
    
    Bug: 361106621
    Change-Id: Iae2ec2d47f751f087ae9031b6b07ddf593c44194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805930
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347735}

---
+++ ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_view+testing.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_PRICE_TRACKING_PROMO_PRICE_TRACKING_PROMO_VIEW_TESTING_H_
+#define IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_PRICE_TRACKING_PROMO_PRICE_TRACKING_PROMO_VIEW_TESTING_H_
+
+#import "ios/chrome/browser/ui/content_suggestions/price_tracking_promo/price_tracking_promo_view.h"
+
+// Category for exposing internal state for testing.
+@interface PriceTrackingPromoModuleView (ForTesting)
+
+- (NSString*)titleLabelTextForTesting;
+
+- (NSString*)descriptionLabelTextForTesting;
+
+- (NSString*)allowLabelTextForTesting;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_CONTENT_SUGGESTIONS_PRICE_TRACKING_PROMO_PRICE_TRACKING_PROMO_VIEW_TESTING_H_
