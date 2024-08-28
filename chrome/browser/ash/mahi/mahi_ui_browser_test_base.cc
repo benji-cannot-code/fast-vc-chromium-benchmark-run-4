9d1f97bc9a553656e05dc282a263853280975284
Xinglong Luan <alanlxl@google.com>
2024-08-28 11:14:30 +0000

    [mahi] add title and url field to mahi request
    For T&S purpose. Controlled by two separate flags.
    
    Bug: b:359407527
    Test: unit_tests --gtest_filter="MahiManagerImplTest.*"
    Test: browser_tests --gtest_filter="MahiUiBrowserTest.*"
    Change-Id: Ie30c88d2098cc8401e0400c616a8ceb3d0201388
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820891
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Commit-Queue: Xinglong Luan <alanlxl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347933}

--- chrome/browser/ash/mahi/mahi_ui_browser_test_base.cc
+++ chrome/browser/ash/mahi/mahi_ui_browser_test_base.cc
@@ -53,7 +53,8 @@ class MockMahiProvider : public manta::MahiProvider {
             /*identity_manager=*/nullptr,
             manta::ProviderParams()) {
     ON_CALL(*this, Summarize)
-        .WillByDefault([](const std::string& input,
+        .WillByDefault([](const std::string& input, const std::string&,
+                          const std::optional<std::string>&,
                           manta::MantaGenericCallback done_callback) {
           base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
               FROM_HERE,
@@ -67,7 +68,8 @@ class MockMahiProvider : public manta::MahiProvider {
 
     ON_CALL(*this, QuestionAndAnswer)
         .WillByDefault(
-            [](const std::string& content,
+            [](const std::string& content, const std::string&,
+               const std::optional<std::string>&,
                const std::vector<manta::MahiProvider::MahiQAPair> QAHistory,
                const std::string& question,
                manta::MantaGenericCallback done_callback) {
@@ -85,11 +87,16 @@ class MockMahiProvider : public manta::MahiProvider {
   // manta::MahiProvider:
   MOCK_METHOD(void,
               Summarize,
-              (const std::string&, manta::MantaGenericCallback),
+              (const std::string&,
+               const std::string&,
+               const std::optional<std::string>&,
+               manta::MantaGenericCallback),
               (override));
   MOCK_METHOD(void,
               QuestionAndAnswer,
               (const std::string&,
+               const std::string&,
+               const std::optional<std::string>&,
                const std::vector<manta::MahiProvider::MahiQAPair>,
                const std::string&,
                manta::MantaGenericCallback),
