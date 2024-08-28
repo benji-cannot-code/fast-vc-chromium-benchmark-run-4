20c51ab9257f004c0b1405ce74aacaffc23b4df9
Istvan Nagy <iscsi@google.com>
2024-08-28 11:54:15 +0000

    [High5] Add fingerprint UI API calls to the ActiveSessionAuthView
    With this the ActiveSessionAuthController can manipulate the
    fingerprint part of the ActiveSessionAuthView.
    
    Bug: b:333713449, b:348326316
    Change-Id: I81d54d42df553f9cbee4361bff08fad8a1425ae5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5822054
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347942}

--- ash/auth/views/active_session_auth_view.h
+++ ash/auth/views/active_session_auth_view.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/auth/views/auth_container_view.h"
 #include "ash/auth/views/auth_header_view.h"
+#include "ash/public/cpp/login_types.h"
 #include "ash/style/icon_button.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -110,6 +111,10 @@ class ASH_EXPORT ActiveSessionAuthView : public views::View,
   void ResetInputfields();
 
   void OnTitleChanged(const std::u16string& error_str) override;
+
+  // FingerprintView actions:
+  void SetFingerprintState(FingerprintState state);
+  void NotifyFingerprintAuthFailure();
 
  private:
   // Internal methods for managing views.
