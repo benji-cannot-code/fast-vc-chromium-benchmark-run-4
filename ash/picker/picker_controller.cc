9042f3ee3669245091b8736a3bb8427067c1c68e
Darren Shen <shend@chromium.org>
2024-08-28 06:42:54 +0000

    Inline OnFeatureTourLearnMore.
    Bug: b:360229206
    Change-Id: I840cdfd9c3ea645455f982ca2661a09f62310347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5819452
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1347845}

--- ash/picker/picker_controller.cc
+++ ash/picker/picker_controller.cc
@@ -108,6 +108,9 @@ constexpr int kCapsLockMinimumTopDisplayCount = 5;
 constexpr float kCapsLockRatioThresholdForTop = 0.8;
 constexpr float kCapsLockRatioThresholdForBottom = 0.2;
 
+constexpr std::string_view kSupportUrl =
+    "https://support.google.com/chromebook?p=dugong";
+
 PickerFeatureKeyType MatchPickerFeatureKeyHash() {
   static const PickerFeatureKeyType key_type = []() {
     // Command line looks like:
@@ -356,8 +359,7 @@ void PickerController::ToggleWidget(
           client_->IsEligibleForEditor()
               ? PickerFeatureTour::EditorStatus::kEligible
               : PickerFeatureTour::EditorStatus::kNotEligible,
-          base::BindRepeating(&PickerController::OnFeatureTourLearnMore,
-                              weak_ptr_factory_.GetWeakPtr()),
+          base::BindRepeating(OpenLink, GURL(kSupportUrl)),
           base::BindRepeating(&PickerController::ShowWidgetPostFeatureTour,
                               weak_ptr_factory_.GetWeakPtr()))) {
     return;
@@ -657,10 +659,6 @@ void PickerController::CloseWidget() {
   session_metrics_->SetOutcome(
       PickerSessionMetrics::SessionOutcome::kAbandoned);
   widget_->Close();
-}
-
-void PickerController::OnFeatureTourLearnMore() {
-  OpenLink(GURL("https://support.google.com/chromebook?p=dugong"));
 }
 
 void PickerController::ShowWidgetPostFeatureTour() {
