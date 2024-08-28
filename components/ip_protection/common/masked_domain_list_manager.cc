20e2df02f8c1152c6542957eb62a0ab161c6142f
Dustin J. Mitchell <djmitche@chromium.org>
2024-08-28 18:26:26 +0000

    Move MDL metric to IpProtectionTelemetry
    Bug: 352005196
    Change-Id: Idfb6967444a8ac8c8e2e90598c44cc63a99a1855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5763699
    Reviewed-by: Alex Kallam <aakallam@chromium.org>
    Reviewed-by: Ashley Newson <ashleynewson@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348164}

--- components/ip_protection/common/masked_domain_list_manager.cc
+++ components/ip_protection/common/masked_domain_list_manager.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/trace_event/memory_usage_estimator.h"
+#include "components/ip_protection/common/ip_protection_telemetry.h"
 #include "components/ip_protection/common/url_matcher_with_bypass.h"
 #include "components/privacy_sandbox/masked_domain_list/masked_domain_list.pb.h"
 #include "net/base/features.h"
@@ -253,11 +254,7 @@ void MaskedDomainListManager::UpdateMaskedDomainList(
     }
     AddPublicSuffixListRules(psl_private_domains);
   }
-  // TODO(crbug.com/356109549): Consider renaming this metric.
-  base::UmaHistogramMemoryKB(
-      "NetworkService.MaskedDomainList.NetworkServiceProxyAllowList."
-      "EstimatedMemoryUsageInKB",
-      EstimateMemoryUsage() / 1024);
+  ip_protection::Telemetry().MdlEstimatedMemoryUsage(EstimateMemoryUsage());
 }
 
 bool MaskedDomainListManager::MatchesPublicSuffixList(
