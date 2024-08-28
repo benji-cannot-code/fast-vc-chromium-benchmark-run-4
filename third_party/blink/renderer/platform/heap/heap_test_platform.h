52382d0225c9647743c7a79441d8d2b49be59f38
Etienne Pierre-doray <etiennep@chromium.org>
2024-08-28 19:24:13 +0000

    [v8] Update GetForegroundTaskRunner override
    Bug: 1448758
    Change-Id: I909d3445d86fc7ced843ffbffbf2c7d77b7da8b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823952
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348198}

--- third_party/blink/renderer/platform/heap/heap_test_platform.h
+++ third_party/blink/renderer/platform/heap/heap_test_platform.h
@@ -55,7 +55,8 @@ class HeapTestingPlatformAdapter final : public v8::Platform {
     return platform_->NumberOfWorkerThreads();
   }
   std::shared_ptr<v8::TaskRunner> GetForegroundTaskRunner(
-      v8::Isolate* isolate) final {
+      v8::Isolate* isolate,
+      v8::TaskPriority priority) final {
     // Provides task runner that allows for incremental tasks even in detached
     // mode.
     return task_runner_;
