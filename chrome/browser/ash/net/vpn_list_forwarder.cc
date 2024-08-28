6d4655113c9bd79f0ec9a1518c694db76d1876dc
Eriko Kurimoto <elkurin@chromium.org>
2024-08-28 03:35:57 +0000

    ash: Move //c/b/ui/ash/vpn_list_forward* to //c/b/ash/net
    Bug: b:359064716
    Change-Id: I857b15f7432b8f8588188d7b0d44b21d952ebd85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802551
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347804}

--- chrome/browser/ash/net/vpn_list_forwarder.cc
+++ chrome/browser/ash/net/vpn_list_forwarder.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/ash/vpn_list_forwarder.h"
+#include "chrome/browser/ash/net/vpn_list_forwarder.h"
 
 #include "ash/public/cpp/network_config_service.h"
 #include "base/check_op.h"
--- chrome/browser/ash/net/vpn_list_forwarder.h
+++ chrome/browser/ash/net/vpn_list_forwarder.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_VPN_LIST_FORWARDER_H_
-#define CHROME_BROWSER_UI_ASH_VPN_LIST_FORWARDER_H_
+#ifndef CHROME_BROWSER_ASH_NET_VPN_LIST_FORWARDER_H_
+#define CHROME_BROWSER_ASH_NET_VPN_LIST_FORWARDER_H_
 
 #include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
@@ -107,4 +107,4 @@ class VpnListForwarder
   base::WeakPtrFactory<VpnListForwarder> weak_factory_{this};
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_VPN_LIST_FORWARDER_H_
+#endif  // CHROME_BROWSER_ASH_NET_VPN_LIST_FORWARDER_H_
