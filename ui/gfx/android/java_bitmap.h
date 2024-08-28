95b4e4ab4827327fe67716dbf51c418c53d9ea15
Mark Rowe <markrowe@chromium.org>
2024-08-28 13:38:03 +0000

    Change gfx::JavaBitmap::pixels_ to be a raw_ptr
    Fixed: 346694386
    Change-Id: If20d526e9983bda294c807518992ab79521cdcfe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5816209
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347969}

--- ui/gfx/android/java_bitmap.h
+++ ui/gfx/android/java_bitmap.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdint.h>
 
 #include "base/android/scoped_java_ref.h"
-#include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/raw_ptr.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/gfx_export.h"
@@ -50,9 +50,7 @@ class GFX_EXPORT JavaBitmap {
 
  private:
   base::android::ScopedJavaGlobalRef<jobject> bitmap_;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION void* pixels_;
+  raw_ptr<void> pixels_ = nullptr;
   gfx::Size size_;
   BitmapFormat format_;
   uint32_t bytes_per_row_;
