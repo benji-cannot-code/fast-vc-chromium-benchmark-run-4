92fcdd6f0bd379c7ee1e17be5b8f9eb3a10d7745
Ari Chivukula <arichiv@chromium.org>
2024-08-28 15:53:32 +0000

    [WindowProxyAccess] Fix double logging
    There are two postMessage implementations and one called the other, but
    we were updating metrics in both. Let's fix that.
    
    Bug: 40737536
    Change-Id: I68131567da2950a25b3247e1fd5a65f0a09272ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812428
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348051}

--- chrome/browser/chrome_web_platform_security_metrics_browsertest.cc
+++ chrome/browser/chrome_web_platform_security_metrics_browsertest.cc
@@ -1837,11 +1837,7 @@ IN_PROC_BROWSER_TEST_F(ChromeWebPlatformSecurityMetricsBrowserTest,
     CheckCounter(test.property_access_from_other_page, 0);
     const auto& entries =
         test_ukm_recorder->GetEntriesByName("WindowProxyUsage");
-    ASSERT_EQ(
-        entries.size(),
-        test.access_type == blink::mojom::WindowProxyAccessType::kPostMessage
-            ? 2u
-            : 1u);
+    ASSERT_EQ(entries.size(), 1u);
     const auto& entry = entries.back();
     test_ukm_recorder->ExpectEntryMetric(entry, "AccessType",
                                          (int)test.access_type);
@@ -2124,11 +2120,7 @@ IN_PROC_BROWSER_TEST_F(ChromeWebPlatformSecurityMetricsBrowserTest,
     CheckCounter(test.property_access_from_other_page, 1);
     const auto& entries =
         test_ukm_recorder->GetEntriesByName("WindowProxyUsage");
-    ASSERT_EQ(
-        entries.size(),
-        test.access_type == blink::mojom::WindowProxyAccessType::kPostMessage
-            ? 2u
-            : 1u);
+    ASSERT_EQ(entries.size(), 1u);
     const auto& entry = entries.back();
     test_ukm_recorder->ExpectEntryMetric(entry, "AccessType",
                                          (int)test.access_type);
@@ -2262,11 +2254,7 @@ IN_PROC_BROWSER_TEST_F(ChromeWebPlatformSecurityMetricsBrowserTest,
     CheckCounter(test.property_access_from_other_page, 1);
     const auto& entries =
         test_ukm_recorder->GetEntriesByName("WindowProxyUsage");
-    ASSERT_EQ(
-        entries.size(),
-        test.access_type == blink::mojom::WindowProxyAccessType::kPostMessage
-            ? 2u
-            : 1u);
+    ASSERT_EQ(entries.size(), 1u);
     const auto& entry = entries.back();
     test_ukm_recorder->ExpectEntryMetric(entry, "AccessType",
                                          (int)test.access_type);
