fce7ca030bd499854a9105f4df6347c1b24fe811
Eriko Kurimoto <elkurin@chromium.org>
2024-08-28 01:40:18 +0000

    ash: Create //chrome/browser/ash/accelerator
    Bug: b:358243750
    Change-Id: Ia77cca7515a8f7b1d417c523d47f7254df0b8e95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802734
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1347765}

--- chrome/browser/ui/ash/accelerator/chrome_accelerator_prefs_delegate.h
+++ chrome/browser/ui/ash/accelerator/chrome_accelerator_prefs_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
-#define CHROME_BROWSER_UI_ASH_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
+#ifndef CHROME_BROWSER_UI_ASH_ACCELERATOR_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
+#define CHROME_BROWSER_UI_ASH_ACCELERATOR_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
 
 #include "ash/accelerators/accelerator_prefs_delegate.h"
 
@@ -22,4 +22,4 @@ class ChromeAcceleratorPrefsDelegate : public ash::AcceleratorPrefsDelegate {
   bool IsUserEnterpriseManaged() const override;
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
+#endif  // CHROME_BROWSER_UI_ASH_ACCELERATOR_CHROME_ACCELERATOR_PREFS_DELEGATE_H_
