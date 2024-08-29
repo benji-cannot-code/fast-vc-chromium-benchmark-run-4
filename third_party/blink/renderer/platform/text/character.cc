72d8ff4e9bab8be5d3f5c45b9be489bb98a7cfc4
Kent Tamura <tkent@chromium.org>
2024-08-29 04:57:32 +0000

    Remove "TextAlignJustifyBidiIsolate" runtime flag
    It has been enabled since M127, and we have never seen issues.
    
    Bug: 331680200
    Change-Id: I69a12723bd05e27d7ffa7aaa6a023981d771250e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5822962
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348445}

--- third_party/blink/renderer/platform/text/character.cc
+++ third_party/blink/renderer/platform/text/character.cc
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/synchronization/lock.h"
-#include "third_party/blink/renderer/platform/runtime_enabled_features.h"
 #include "third_party/blink/renderer/platform/text/character_property_data.h"
 #include "third_party/blink/renderer/platform/text/icu_error.h"
 #include "third_party/blink/renderer/platform/wtf/std_lib_extras.h"
@@ -186,9 +185,7 @@ unsigned Character::ExpansionOpportunityCount(
         count++;
         is_after_expansion = true;
         continue;
-      } else if (!RuntimeEnabledFeatures::
-                     TextAlignJustifyBidiIsolateEnabled() ||
-                 !IsDefaultIgnorable(character)) {
+      } else if (!IsDefaultIgnorable(character)) {
         is_after_expansion = false;
       }
     }
@@ -210,9 +207,7 @@ unsigned Character::ExpansionOpportunityCount(
         count++;
         is_after_expansion = true;
         continue;
-      } else if (!RuntimeEnabledFeatures::
-                     TextAlignJustifyBidiIsolateEnabled() ||
-                 !IsDefaultIgnorable(character)) {
+      } else if (!IsDefaultIgnorable(character)) {
         is_after_expansion = false;
       }
     }
