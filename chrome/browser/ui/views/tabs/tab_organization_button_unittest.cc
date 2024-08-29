a78227f6ddc8c11d9663458f491d4f891b531ac3
Shibalik Mohapatra <shibalik@chromium.org>
2024-08-29 17:28:30 +0000

    [TabStripDeclutter] Refactor constructor of TabOrganizationButton.
    This refactor will allow declutter button to be created using the new constructor as they share the same functionality otherwise.
    
    Change-Id: Ife48581b4270bcafc6196be7ea13cbf3f03a2d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824888
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348721}

--- chrome/browser/ui/views/tabs/tab_organization_button_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_organization_button_unittest.cc
@@ -6,11 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/tabs/tab_organization_button.h"
 
 #include "base/time/time.h"
+#include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "chrome/grit/generated_resources.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/l10n/l10n_util.h"
 
 class TabOrganizationButtonTest : public ChromeViewsTestBase {
  public:
@@ -24,7 +27,10 @@ class TabOrganizationButtonTest : public ChromeViewsTestBase {
                             base::Unretained(this)),
         base::BindRepeating(&TabOrganizationButtonTest::MockButtonCallback,
                             base::Unretained(this)),
-        Edge::kRight);
+        l10n_util::GetStringUTF16(IDS_TAB_ORGANIZE),
+        l10n_util::GetStringUTF16(IDS_TOOLTIP_TAB_ORGANIZE),
+        l10n_util::GetStringUTF16(IDS_ACCNAME_TAB_ORGANIZE),
+        kTabOrganizationButtonElementId, Edge::kRight);
   }
 
   void MockButtonCallback() { button_callback_count_++; }
