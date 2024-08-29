35f012e4540276f75de7bd08c40f82416dc589a1
Cristian Lopez <cristiandlopez@google.com>
2024-08-29 20:49:23 +0000

    Clean up future variations seed histograms.
    Variations.HasFutureSeed and Variations.SeedFreshness.Future 
    histograms are not needed after M118. 
    This CL removes them and the related code.
    
    OBSOLETE_HISTOGRAMS=M117 Stable data showed that future fetch times are rare.
    
    Bug: 40274902
    Change-Id: I836d297802d633c0df02bc10e437d7f6d41a75d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818471
    Commit-Queue: Cristian Lopez <cristiandlopez@google.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348865}

--- components/variations/service/variations_service_utils.cc
+++ components/variations/service/variations_service_utils.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/build_time.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/time/time.h"
 #include "components/country_codes/country_codes.h"
 #include "components/variations/service/variations_service.h"
@@ -17,20 +16,7 @@ namespace variations {
 namespace {
 
 // Maximum age permitted for a variations seed, in days.
-const int kMaxVariationsSeedAgeDays = 30;
-
-// Emits to future-seed-related histograms.
-void RecordFutureSeedMetrics(int seed_age_days) {
-  // The seed age is the difference between base::Time::Now() and the
-  // client-provided timestamp of when the seed was fetched. If the seed age is
-  // negative, then client timestamp must be in the future.
-  bool seed_has_future_age = seed_age_days < 0;
-  base::UmaHistogramBoolean("Variations.HasFutureSeed", seed_has_future_age);
-  if (seed_has_future_age) {
-    base::UmaHistogramCounts1000("Variations.SeedFreshness.Future",
-                                 -seed_age_days);
-  }
-}
+const int kMaxSeedAgeDays = 30;
 
 // Helper function for "HasSeedExpiredSinceTime" that exposes |build_time| and
 // makes it overridable by tests.
@@ -39,13 +25,10 @@ bool HasSeedExpiredSinceTimeHelper(base::Time fetch_time,
   // TODO(crbug.com/40274989): Consider comparing the server-provided fetch time
   // with the network time.
   const base::TimeDelta seed_age = base::Time::Now() - fetch_time;
-  const int seed_age_days = seed_age.InDays();
-  RecordFutureSeedMetrics(seed_age_days);
 
   // base::TimeDelta::InDays() rounds down to the nearest integer, so the seed
-  // would not be considered expired if it is less than
-  // `kMaxVariationsSeedAgeDays + 1`.
-  return seed_age_days > kMaxVariationsSeedAgeDays && build_time > fetch_time;
+  // would not be considered expired if it is less than `kMaxSeedAgeDays + 1`.
+  return seed_age.InDays() > kMaxSeedAgeDays && build_time > fetch_time;
 }
 
 }  // namespace
