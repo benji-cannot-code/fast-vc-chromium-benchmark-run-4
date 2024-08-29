70bf0c81462d1144fd3fe3b725834627e41fc9c7
Nafis Abedin <nafisabedin@google.com>
2024-08-29 14:45:45 +0000

    [base/android] Log long-running UI thread Binder calls
    Binder has a hidden @SystemApi that can be used to intercept Binder calls. Android utilizes this with the command "adb shell am trace-ipc
    start|stop", and in SysUI to detect jank from UI thread Binder calls.
    This suggests that it would be beneficial in Chromium too. The CL is a
    continuation of @lizeb's draft CL to log slow (> 2ms) Binder calls.
    
    Binder.setProxyTransactListener() has both @hide and @SystemApi and
    android.os.Binder$ProxyTransactListener has the @hide property. Therefore, double/meta reflection and a proxy is utilized to bypass these restrictions.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Bug: 40251111
    Change-Id: I37ef5bcd5fa155d117d0c4bf9f896fe580946c0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5799523
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348628}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -320,6 +320,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kTabWindowManagerReportIndicesMismatch,
     &kTestDefaultDisabled,
     &kTestDefaultEnabled,
+    &kTraceBinderIpc,
     &kStartSurfaceReturnTime,
     &kAccountReauthenticationRecentTimeWindow,
     &kSurfacePolish,
@@ -998,6 +999,10 @@ BASE_FEATURE(kTestDefaultDisabled,
 BASE_FEATURE(kTestDefaultEnabled,
              "TestDefaultEnabled",
              base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kTraceBinderIpc,
+             "TraceBinderIpc",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 
 BASE_FEATURE(kSearchInCCT, "SearchInCCT", base::FEATURE_DISABLED_BY_DEFAULT);
 
