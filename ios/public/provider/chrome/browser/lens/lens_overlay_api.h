b013e34a517242e7dd10b46e763a0144cce17147
Radu Nitescu <radunitescu@google.com>
2024-08-29 23:02:38 +0000

    [iOS][LO] Expose clearing Lens selection method
    Bug: 362800179
    Change-Id: I83f4678a76c1d77fc627e548ff57569989f2baed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823531
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348942}

--- ios/public/provider/chrome/browser/lens/lens_overlay_api.h
+++ ios/public/provider/chrome/browser/lens/lens_overlay_api.h
@@ -44,6 +44,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Reloads a previous result in the overlay.
 - (void)reloadResult:(id<ChromeLensOverlayResult>)result;
 
+// Removes the current selection and optionally clears the query text.
+- (void)removeSelectionWithClearText:(BOOL)clearText;
+
 @end
 
 namespace ios {
