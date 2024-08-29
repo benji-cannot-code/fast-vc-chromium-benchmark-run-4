bbadffcfcc8197cc0f615432a029f708e1dd2a60
James Cook <jamescook@chromium.org>
2024-08-29 17:35:31 +0000

    Allow desktop Android to install any web site as a PWA
    This is a temporary feature during desktop Android bring-up. We allow
    non-PWA sites to be installed as PWAs by:
    * Skipping the shortcut install flow, triggering the WEBAPK_DIY flow
    * Generating an icon if necessary
    * Setting the PWA display mode to "standalone"
    
    Sites that ordinarily would be installed as PWAs are not affected.
    
    Bug: b/350859016
    Test: components_unittests
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Change-Id: Ic4124137a36aa18acc90b91f264ed419fd8dd1b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818731
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348733}

--- components/webapps/browser/installable/installable_icon_fetcher.cc
+++ components/webapps/browser/installable/installable_icon_fetcher.cc
@@ -6,10 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/webapps/browser/installable/installable_icon_fetcher.h"
 
 #include "base/check_is_test.h"
+#include "base/functional/callback.h"
+#include "base/memory/scoped_refptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
+#include "base/task/task_traits.h"
 #include "base/task/thread_pool.h"
 #include "base/threading/thread_restrictions.h"
+#include "build/android_buildflags.h"
 #include "components/favicon/content/large_icon_service_getter.h"
 #include "components/favicon/core/large_icon_service.h"
 #include "components/favicon_base/favicon_types.h"
@@ -20,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/common/manifest/manifest_icon_selector.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/codec/png_codec.h"
+#include "url/gurl.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "components/webapps/browser/android/webapps_icon_utils.h"
@@ -115,6 +120,20 @@ void ProcessFaviconInBackground(
       FROM_HERE, base::BindOnce(std::move(success_callback), decoded));
 }
 
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+// Generates a homescreen icon for `page_url` and posts a task to invoke
+// `callback` on `ui_thread_task_runner.`
+void GenerateHomeScreenIconInBackground(
+    const GURL& page_url,
+    scoped_refptr<base::SequencedTaskRunner> ui_thread_task_runner,
+    base::OnceCallback<void(const GURL& url, const SkBitmap&)> callback) {
+  SkBitmap bitmap =
+      WebappsIconUtils::GenerateHomeScreenIconInBackground(page_url);
+  ui_thread_task_runner->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), page_url, bitmap));
+}
+#endif  // BUILDFLAG(IS_DESKTOP_ANDROID)
+
 }  // namespace
 
 namespace test {
@@ -174,7 +193,7 @@ void InstallableIconFetcher::TryFetchingNextIcon() {
     return;
   }
 
-  EndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
+  MaybeEndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
 }
 
 void InstallableIconFetcher::OnManifestIconFetched(const GURL& icon_url,
@@ -192,7 +211,7 @@ void InstallableIconFetcher::FetchFavicon() {
   favicon::LargeIconService* favicon_service =
       favicon::GetLargeIconService(web_contents_->GetBrowserContext());
   if (!favicon_service) {
-    EndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
+    MaybeEndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
     return;
   }
 
@@ -209,7 +228,7 @@ void InstallableIconFetcher::FetchFavicon() {
 void InstallableIconFetcher::OnFaviconFetched(
     const favicon_base::LargeIconResult& result) {
   if (!result.bitmap.is_valid()) {
-    EndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
+    MaybeEndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
     return;
   }
 
@@ -222,7 +241,7 @@ void InstallableIconFetcher::OnFaviconFetched(
                      base::BindOnce(&InstallableIconFetcher::OnIconFetched,
                                     weak_ptr_factory_.GetWeakPtr(),
                                     result.bitmap.icon_url, IconPurpose::ANY),
-                     base::BindOnce(&InstallableIconFetcher::EndWithError,
+                     base::BindOnce(&InstallableIconFetcher::MaybeEndWithError,
                                     weak_ptr_factory_.GetWeakPtr())));
 }
 
@@ -233,9 +252,40 @@ void InstallableIconFetcher::OnIconFetched(const GURL& icon_url,
   std::move(finish_callback_).Run(InstallableStatusCode::NO_ERROR_DETECTED);
 }
 
+void InstallableIconFetcher::MaybeEndWithError(InstallableStatusCode code) {
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+  // Desktop android will generate an icon if none is available.
+  base::ThreadPool::PostTask(
+      FROM_HERE,
+      {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
+       base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN},
+      base::BindOnce(
+          &GenerateHomeScreenIconInBackground,
+          web_contents_->GetLastCommittedURL(),
+          base::SingleThreadTaskRunner::GetCurrentDefault(),
+          base::BindOnce(&InstallableIconFetcher::OnHomeScreenIconGenerated,
+                         weak_ptr_factory_.GetWeakPtr())));
+  return;
+#else
+  // Other platforms report an error if no icon is available.
+  EndWithError(code);
+#endif  // BUILDFLAG(IS_DESKTOP_ANDROID)
+}
+
 void InstallableIconFetcher::EndWithError(InstallableStatusCode code) {
   page_data_->OnPrimaryIconFetchedError(code);
   std::move(finish_callback_).Run(code);
 }
+
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+void InstallableIconFetcher::OnHomeScreenIconGenerated(const GURL& page_url,
+                                                       const SkBitmap& bitmap) {
+  if (bitmap.drawsNothing()) {
+    EndWithError(InstallableStatusCode::NO_ACCEPTABLE_ICON);
+    return;
+  }
+  OnIconFetched(page_url, IconPurpose::ANY, bitmap);
+}
+#endif
 
 }  // namespace webapps
--- components/webapps/browser/installable/installable_icon_fetcher.h
+++ components/webapps/browser/installable/installable_icon_fetcher.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback.h"
 #include "base/task/cancelable_task_tracker.h"
+#include "build/android_buildflags.h"
 #include "components/webapps/browser/installable/installable_logging.h"
 #include "components/webapps/browser/installable/installable_page_data.h"
 #include "third_party/blink/public/mojom/manifest/manifest.mojom.h"
@@ -61,7 +62,16 @@ class InstallableIconFetcher {
                      const blink::mojom::ManifestImageResource_Purpose purpose,
                      const SkBitmap& bitmap);
 
+  // Gives a chance for desktop android to generate an icon instead of
+  // ending with an error. Other platforms end immediately with an error.
+  void MaybeEndWithError(InstallableStatusCode code);
+
+  // Ends the fetch with an error.
   void EndWithError(InstallableStatusCode code);
+
+#if BUILDFLAG(IS_DESKTOP_ANDROID)
+  void OnHomeScreenIconGenerated(const GURL& page_url, const SkBitmap& bitmap);
+#endif
 
   base::WeakPtr<content::WebContents> web_contents_;
 
