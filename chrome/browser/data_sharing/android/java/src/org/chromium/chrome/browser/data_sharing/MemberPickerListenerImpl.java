a0b5422fb1a90232370d592b2eceaaecdef60cd9
Yoichi Osato <yoichio@chromium.org>
2024-08-29 03:43:25 +0000

    Revert "[Data Sharing] Adding tests for create flow."
    This reverts commit 11881c7fbc957d33112c55951faf410507dd08db.
    
    Reason for revert: Caused bot failure
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-lacros-chrome/3176/test-results?sortby=&groupby=
    
    Original change's description:
    > [Data Sharing] Adding tests for create flow.
    >
    > Bug: b/358153912
    > Change-Id: Ib9a7b30d6c767f3a8b0de01db43a2acbbb2e61f9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809570
    > Reviewed-by: Siddhartha S <ssid@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Ritika Gupta <ritikagup@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1348172}
    
    Bug: b/358153912
    Change-Id: Ife072cc21f865656a5cbbe54448d577e07836b0f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825991
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Owners-Override: Yoichi Osato <yoichio@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348431}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/MemberPickerListenerImpl.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/MemberPickerListenerImpl.java
@@ -21,10 +21,6 @@ public class MemberPickerListenerImpl implements DataSharingUIDelegate.MemberPic
         mCallback = callback;
     }
 
-    public Callback<List<String>> getCallback() {
-        return mCallback;
-    }
-
     @Override
     public void onSelectionDone(List<String> selectedMemberIds, List<String> emails) {
         int count = 0;
