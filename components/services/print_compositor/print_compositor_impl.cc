91f1f934416cdfc5e1810bc19fc73cf912e76b03
Nasser Al-shawwa <alshawwa@chromium.org>
2024-08-29 03:15:32 +0000

    Reland "Implement enterprise watermark rendering in `PrintCompositorImpl`."
    This is a reland of commit 9faee3b74f0c658d059380fefcb780b09a8c336e
    
    This reland makes the `watermark.h` include conditional on the
    `ENTERPRISE_WATERMARK` build flag and adds a `// nogncheck` to avoid
    false negatives caused by conditional includes. This caused fuchsia
    build failures in the original CL.
    
    
    Original change's description:
    > Implement enterprise watermark rendering in `PrintCompositorImpl`.
    >
    > This CL draws a watermark on top of the printed page where required.
    > Its scope is limited to the rendering code; it does not draw the
    > watermark retrieved from the URL filtering DLP rule. That will be
    > added in a subsequent CL that modifies the `PrintCompositor` mojo
    > interface.
    >
    > Bug: 356446812
    > Change-Id: I0dfc158d8e39d9361063da89cb7fb914ae5eedc1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5784051
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Lei Zhang <thestig@chromium.org>
    > Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1347598}
    
    Cq-Include-Trybots: luci.chromium.try:fuchsia-x64-accessibility-rel
    Bug: 356446812
    Change-Id: I78c6d822e0092cb9467ad2edb20bb7f7de5c58fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5819932
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348421}

--- components/services/print_compositor/print_compositor_impl.cc
+++ components/services/print_compositor/print_compositor_impl.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if BUILDFLAG(ENTERPRISE_WATERMARK)
 #include "components/enterprise/watermarking/features.h"  // nogncheck
+#include "components/enterprise/watermarking/watermark.h"  // nogncheck
 #endif
 
 using MojoDiscardableSharedMemoryManager =
@@ -53,6 +54,12 @@ using MojoDiscardableSharedMemoryManager =
 namespace printing {
 
 namespace {
+
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+// Print UX requirement for watermarking. Values are in pixels.
+constexpr int kWatermarkBlockWidth = 350;
+constexpr float kTextSize = 24.0f;
+#endif
 
 sk_sp<SkDocument> MakeDocument(
     const std::string& creator,
@@ -79,6 +86,13 @@ void DrawEnterpriseWatermark(SkCanvas* canvas, SkSize size) {
           enterprise_watermark::features::kEnablePrintWatermark)) {
     return;
   }
+
+  // TODO(b/356446812): For now, use this hard-coded string to facilitate
+  // implementing UI tests. We must update the PrintCompositor mojo interface in
+  // order to pass the watermark string here from the browser process.
+  std::string text = "Private! Confidential!\n2024-05-24\nexample@gmail.com";
+  enterprise_watermark::DrawWatermark(canvas, size, text, kWatermarkBlockWidth,
+                                      kTextSize);
 }
 #endif
 
