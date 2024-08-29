528d2fb02b7e93d94b57d0d0ff8ed247ab23f3ee
Colin Blundell <blundell@chromium.org>
2024-08-29 11:56:49 +0000

    [//gpu] Build ExternalSemaphore(Pool) only on Linux/Fuchsia/Win
    These classes are used only by ExternalVkImageBacking and related code,
    which itself is used only on Linux/Fuchsia/Win.
    
    Bug: 336837285
    Change-Id: Ic42fb7b57cade1f7542bcf73a11e96dc85cfcb13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5821712
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348565}

--- gpu/command_buffer/service/shared_image/skia_vk_ozone_image_representation.cc
+++ gpu/command_buffer/service/shared_image/skia_vk_ozone_image_representation.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/gpu/vulkan_context_provider.h"
 #include "components/viz/common/resources/shared_image_format_utils.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
-#include "gpu/command_buffer/service/external_semaphore_pool.h"
 #include "gpu/command_buffer/service/memory_tracking.h"
 #include "gpu/command_buffer/service/shared_context_state.h"
 #include "gpu/command_buffer/service/shared_image/shared_image_format_service_utils.h"
