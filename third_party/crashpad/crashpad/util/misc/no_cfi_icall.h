10cba5fd2098ad22b155ab712be48e21d9de2dc2
Peter Kasting <pkasting@chromium.org>
2024-08-29 01:43:27 +0000

    Update Crashpad to f9fa2d112bef458ea765043f1ba3104ce013b1fe
    adb22c816987 Switch from `ALIGNAS()` to `alignas()`
    458bdec98522 Don't redefine macros from base/compiler_specific.h
    756637881a1c Roll crashpad/third_party/mini_chromium/mini_chromium/
                 7eca85315..e04c707cb (3 commits)
    50faaf111ee6 Revert "Don't redefine macros from
                 base/compiler_specific.h."
    f9fa2d112bef [reland] Don't redefine macros from
                 base/compiler_specific.h
    
    Bug: none
    Change-Id: I096d57e196a9ad84b74b772be919b25f87019513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805903
    Reviewed-by: Joshua Peraza <jperaza@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Owners-Override: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348394}

--- third_party/crashpad/crashpad/util/misc/no_cfi_icall.h
+++ third_party/crashpad/crashpad/util/misc/no_cfi_icall.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 #include <utility>
 
+#include "base/compiler_specific.h"
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -28,24 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crashpad {
 
 namespace {
-
-// Sanitizers annotations.
-#if defined(__has_attribute)
-#if __has_attribute(no_sanitize)
-#define NO_SANITIZE(what) __attribute__((no_sanitize(what)))
-#endif
-#endif
-#if !defined(NO_SANITIZE)
-#define NO_SANITIZE(what)
-#endif
-
-// DISABLE_CFI_ICALL -- Disable Control Flow Integrity indirect call checks.
-#if BUILDFLAG(IS_WIN)
-// Windows also needs __declspec(guard(nocf)).
-#define DISABLE_CFI_ICALL NO_SANITIZE("cfi-icall") __declspec(guard(nocf))
-#else
-#define DISABLE_CFI_ICALL NO_SANITIZE("cfi-icall")
-#endif
 
 template <typename Functor>
 struct FunctorTraits;
