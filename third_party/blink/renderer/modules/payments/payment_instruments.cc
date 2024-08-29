b80ec34d08fc35c0858f3e9b716fa6b9181ecf85
Andrey Kosyakov <caseq@chromium.org>
2024-08-29 21:43:03 +0000

    Make PaymentInstruments::get() return Promise<PaymentInstrument>
    ... not Promise<any>. It already does, just make it official in the IDL now.
    
    Change-Id: Ib958d802b5782629986acc980e9d2a543ae9e787
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824331
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348899}

--- third_party/blink/renderer/modules/payments/payment_instruments.cc
+++ third_party/blink/renderer/modules/payments/payment_instruments.cc
@@ -129,9 +129,10 @@ ScriptPromise<IDLBoolean> PaymentInstruments::deleteInstrument(
   return promise;
 }
 
-ScriptPromise<IDLAny> PaymentInstruments::get(ScriptState* script_state,
-                                              const String& instrument_key,
-                                              ExceptionState& exception_state) {
+ScriptPromise<PaymentInstrument> PaymentInstruments::get(
+    ScriptState* script_state,
+    const String& instrument_key,
+    ExceptionState& exception_state) {
   if (!AllowedToUsePaymentFeatures(script_state)) {
     ThrowNotAllowedToUsePaymentFeatures(exception_state);
     return EmptyPromise();
@@ -143,15 +144,15 @@ ScriptPromise<IDLAny> PaymentInstruments::get(ScriptState* script_state,
     return EmptyPromise();
   }
 
-  auto* resolver = MakeGarbageCollected<ScriptPromiseResolver<IDLAny>>(
-      script_state, exception_state.GetContext());
-  auto promise = resolver->Promise();
+  auto* resolver =
+      MakeGarbageCollected<ScriptPromiseResolver<PaymentInstrument>>(
+          script_state, exception_state.GetContext());
 
   payment_manager_->manager()->GetPaymentInstrument(
       instrument_key,
       WTF::BindOnce(&PaymentInstruments::onGetPaymentInstrument,
                     WrapPersistent(this), WrapPersistent(resolver)));
-  return promise;
+  return resolver->Promise();
 }
 
 ScriptPromise<IDLSequence<IDLString>> PaymentInstruments::keys(
@@ -354,7 +355,7 @@ void PaymentInstruments::onDeletePaymentInstrument(
 }
 
 void PaymentInstruments::onGetPaymentInstrument(
-    ScriptPromiseResolver<IDLAny>* resolver,
+    ScriptPromiseResolver<PaymentInstrument>* resolver,
     payments::mojom::blink::PaymentInstrumentPtr stored_instrument,
     payments::mojom::blink::PaymentHandlerStatus status) {
   DCHECK(resolver);
@@ -384,8 +385,7 @@ void PaymentInstruments::onGetPaymentInstrument(
   instrument->setIcons(icons);
   instrument->setMethod(stored_instrument->method);
 
-  resolver->Resolve(ToV8Traits<PaymentInstrument>::ToV8(
-      resolver->GetScriptState(), instrument));
+  resolver->Resolve(instrument);
 }
 
 void PaymentInstruments::onKeysOfPaymentInstruments(
