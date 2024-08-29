39032e7aa3720b27be14d55789f5401fc3748d7f
Seung Jae Lim <andysjlim@chromium.org>
2024-08-29 00:28:32 +0000

    Enable study ClientSideDetectionKeyboardPointerLockRequest by default
    Bug: 1498000
    Change-Id: I1fded13f3ffaf3955e2044d9513ac28495e67d66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824013
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348372}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -38,7 +38,7 @@ BASE_FEATURE(kClientSideDetectionKillswitch,
 
 BASE_FEATURE(kClientSideDetectionKeyboardPointerLockRequest,
              "ClientSideDetectionKeyboardPointerLockRequest",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kClientSideDetectionNotificationPrompt,
              "ClientSideDetectionNotificationPrompt",
