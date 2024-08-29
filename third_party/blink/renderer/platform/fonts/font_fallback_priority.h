e3415aa52600864c1bc52263dc349c21ffd9cce8
Koji Ishii <kojii@chromium.org>
2024-08-29 06:03:52 +0000

    Add comments to clarify dependencies in `InlineItemSegment`
    This patch adds comments to clarify the dependencies from
    `enum`s in fonts to `InlineItemSegment`.
    
    This patch has no behavior changes.
    
    Change-Id: I1c842369b382c58968b5fcfc6a73e23ebebd7c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823493
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348455}

--- third_party/blink/renderer/platform/fonts/font_fallback_priority.h
+++ third_party/blink/renderer/platform/fonts/font_fallback_priority.h
@@ -21,6 +21,9 @@ enum class FontFallbackPriority {
   // For emoji in emoji presentation
   kEmojiEmoji,
   kInvalid
+
+  // When adding values, ensure `kFontFallbackPriorityBits` in
+  // `InlineItemSegment` is in sync.
 };
 
 bool IsNonTextFallbackPriority(FontFallbackPriority);
--- third_party/blink/renderer/platform/fonts/orientation_iterator.h
+++ third_party/blink/renderer/platform/fonts/orientation_iterator.h
@@ -21,6 +21,9 @@ class PLATFORM_EXPORT OrientationIterator {
     kOrientationKeep,
     kOrientationRotateSideways,
     kOrientationInvalid
+
+    // When adding values, ensure `kRenderOrientationBits` in
+    // `InlineItemSegment` is in sync.
   };
 
   OrientationIterator(const UChar* buffer,
