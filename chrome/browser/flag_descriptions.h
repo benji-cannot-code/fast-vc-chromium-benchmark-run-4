4ba0a5be214b86f3a695052f7eb678d23365c79d
Jacob Stanley <jacobstanley@google.com>
2024-08-29 22:05:35 +0000

    Add PrivacySandboxPrivacyGuideAdTopics to chrome://flags
    Adding this flag temporarily for Bug Bash testing on Clank.
    
    Change-Id: I3a10deb47bf3d87f465aeb2194ff2797accb8b01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825730
    Reviewed-by: Kevin Graney <kmg@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1348905}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1804,6 +1804,9 @@ extern const char kPrivacySandboxEnrollmentOverridesDescription[];
 extern const char kPrivacySandboxInternalsName[];
 extern const char kPrivacySandboxInternalsDescription[];
 
+extern const char kPrivacySandboxPrivacyGuideAdTopicsName[];
+extern const char kPrivacySandboxPrivacyGuideAdTopicsDescription[];
+
 extern const char kPrivateStateTokensDevUIName[];
 extern const char kPrivateStateTokensDevUIDescription[];
 
