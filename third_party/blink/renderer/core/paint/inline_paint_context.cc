0bb5df384b7ab897743ec37a082dd8c2dedc4d14
David Baron <dbaron@chromium.org>
2024-08-29 19:38:33 +0000

    Remove TextDecoratingBox feature flag, which shipped in M105.
    Bug: 1008951
    Change-Id: I7c0152612c8c8efa08bd398fd724b7ebc02bd791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827130
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348813}

--- third_party/blink/renderer/core/paint/inline_paint_context.cc
+++ third_party/blink/renderer/core/paint/inline_paint_context.cc
@@ -23,8 +23,6 @@ void InlinePaintContext::ClearDecoratingBoxes(
 InlinePaintContext::ScopedInlineItem::ScopedInlineItem(
     const FragmentItem& item,
     InlinePaintContext* inline_context) {
-  if (!RuntimeEnabledFeatures::TextDecoratingBoxEnabled())
-    return;
   DCHECK(inline_context);
   inline_context_ = inline_context;
   last_decorations_ = inline_context->last_decorations_;
@@ -42,7 +40,6 @@ InlinePaintContext::ScopedInlineItem::ScopedInlineItem(
 wtf_size_t InlinePaintContext::SyncDecoratingBox(
     const FragmentItem& item,
     DecoratingBoxList* saved_decorating_boxes) {
-  DCHECK(RuntimeEnabledFeatures::TextDecoratingBoxEnabled());
   DCHECK(!saved_decorating_boxes || saved_decorating_boxes->empty());
 
   // Compare the instance addresses of |AppliedTextDecorations| because it is
@@ -261,8 +258,6 @@ wtf_size_t InlinePaintContext::SyncDecoratingBox(
 InlinePaintContext::ScopedInlineBoxAncestors::ScopedInlineBoxAncestors(
     const InlineCursor& inline_box,
     InlinePaintContext* inline_context) {
-  if (!RuntimeEnabledFeatures::TextDecoratingBoxEnabled())
-    return;
   DCHECK(inline_context);
   inline_context_ = inline_context;
   inline_context->PushDecoratingBoxAncestors(inline_box);
@@ -270,7 +265,6 @@ InlinePaintContext::ScopedInlineBoxAncestors::ScopedInlineBoxAncestors(
 
 void InlinePaintContext::PushDecoratingBoxAncestors(
     const InlineCursor& inline_box) {
-  DCHECK(RuntimeEnabledFeatures::TextDecoratingBoxEnabled());
   DCHECK(inline_box.Current());
   DCHECK(inline_box.Current().IsInlineBox());
   DCHECK(decorating_boxes_.empty());
@@ -302,15 +296,12 @@ void InlinePaintContext::PushDecoratingBoxes(
 InlinePaintContext::ScopedLineBox::ScopedLineBox(
     const InlineCursor& line_cursor,
     InlinePaintContext* inline_context) {
-  if (!RuntimeEnabledFeatures::TextDecoratingBoxEnabled())
-    return;
   DCHECK(inline_context);
   inline_context_ = inline_context;
   inline_context->SetLineBox(line_cursor);
 }
 
 void InlinePaintContext::SetLineBox(const InlineCursor& line_cursor) {
-  DCHECK(RuntimeEnabledFeatures::TextDecoratingBoxEnabled());
   DCHECK_EQ(line_cursor.Current()->Type(), FragmentItem::kLine);
   line_cursor_ = line_cursor;
   DCHECK(decorating_boxes_.empty());
