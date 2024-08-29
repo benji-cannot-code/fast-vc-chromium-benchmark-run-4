8f91c0a92ff0e6528cedc91ab6451795e1f277d5
Mark Rowe <markrowe@chromium.org>
2024-08-29 21:34:39 +0000

    Change ClipboardDataWaiter::clipboard_data_ to be a raw_ptr
    Fixed: 346693868
    Change-Id: Ie19b9c73c40088c7b8dfa57c084927c70bbf65e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818372
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348890}

--- chrome/browser/ui/ash/clipboard/clipboard_history_browsertest.cc
+++ chrome/browser/ui/ash/clipboard/clipboard_history_browsertest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/view_drawn_waiter.h"
 #include "base/containers/adapters.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/scoped_observation.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_number_conversions.h"
@@ -130,8 +129,7 @@ class ClipboardDataWaiter : public ui::ClipboardObserver {
     return clipboard_data == nullptr || *clipboard_data == *clipboard_data_;
   }
 
-  // RAW_PTR_EXCLUSION: #addr-of
-  RAW_PTR_EXCLUSION const ui::ClipboardData* clipboard_data_ = nullptr;
+  raw_ptr<const ui::ClipboardData> clipboard_data_ = nullptr;
   std::unique_ptr<base::RunLoop> run_loop_;
 };
 
