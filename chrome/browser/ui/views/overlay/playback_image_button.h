70c38c25644bb1d4bea9c5a57cd61650ebaf9358
Tommy Steimel <steimel@chromium.org>
2024-08-29 19:34:55 +0000

    [video pip] Move play/pause button to the center
    This CL moves video picture-in-picture's play/pause button into the
    center of the widget and updates its sizing, colors, and background to
    match the mocks for the new 2024 UI update.
    
    This change is behind the off-by-default feature flag for the 2024
    updated UI.
    
    Bug: 360357715
    Change-Id: I3b48b2fa2af3ea9be92b29c0f775aa484fc7872c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815583
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348807}

--- chrome/browser/ui/views/overlay/playback_image_button.h
+++ chrome/browser/ui/views/overlay/playback_image_button.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_OVERLAY_PLAYBACK_IMAGE_BUTTON_H_
 #define CHROME_BROWSER_UI_VIEWS_OVERLAY_PLAYBACK_IMAGE_BUTTON_H_
 
+#include <optional>
+
 #include "chrome/browser/ui/views/overlay/overlay_window_image_button.h"
 #include "chrome/browser/ui/views/overlay/video_overlay_window_views.h"
 #include "ui/base/metadata/metadata_header_macros.h"
@@ -25,12 +27,21 @@ class PlaybackImageButton : public OverlayWindowImageButton {
   void SetPlaybackState(
       const VideoOverlayWindowViews::PlaybackState playback_state);
 
+  // Updates the position of this button within the new bounds of the window.
+  void SetWindowSize(const gfx::Size& window_size);
+
  protected:
   // Overridden from views::View.
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
  private:
   void UpdateImageAndText();
+  void UpdatePosition();
+
+  void SetPlayButtonBackground();
+  void SetPauseButtonBackground();
+
+  std::optional<gfx::Size> window_size_;
 
   VideoOverlayWindowViews::PlaybackState playback_state_ =
       VideoOverlayWindowViews::PlaybackState::kEndOfVideo;
