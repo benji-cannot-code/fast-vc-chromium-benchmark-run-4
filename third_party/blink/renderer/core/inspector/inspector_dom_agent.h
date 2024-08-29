5307769facd24520da8570897bba4a433b8f7609
Jeffrey Cohen <jeffreycohen@chromium.org>
2024-08-29 18:12:52 +0000

    Revert "Expose node scrollability to DevTools"
    This reverts commit 09e29df0c6bcaea816cd84234f4413e697f737ae.
    
    Reason for revert: Failing on Mac12 and Mac14 Arm 64 builds
    https://issues.chromium.org/362994479
    
    
    Original change's description:
    > Expose node scrollability to DevTools
    >
    > - Added the isScrollable property to the DOM.Node type in the CDP protocol. This property indicates whether an element has a scrollable area, as determined by Blink's layout engine.
    > - Added a new probe to the Blink rendering pipeline to track changes in scrollability. This probe signals when an element gains or loses a scrollable area.
    > - Implemented a new protocol event that fires to update the frontend about node scrollability state changes.
    >
    >
    > Bug: 40670442
    > Change-Id: I2a1e0500c774abf64b4fe8638b28b49a764105ac
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797175
    > Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    > Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Commit-Queue: Ionuț Marius Voicilă <ionutvoicila@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1348602}
    
    Bug: 40670442
    Change-Id: I6c11eaffc761c8abad386d35edc061b5f59afa1a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827296
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Jeffrey Cohen <jeffreycohen@chromium.org>
    Reviewed-by: Sophey Dong <sophey@chromium.org>
    Owners-Override: Jeffrey Cohen <jeffreycohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348766}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.h
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.h
@@ -312,7 +312,6 @@ class CORE_EXPORT InspectorDOMAgent final
   void TopLayerElementsChanged();
   void PseudoElementDestroyed(PseudoElement*);
   void NodeCreated(Node* node);
-  void UpdateScrollableFlag(Node* node);
 
   Node* NodeForId(int node_id) const;
   int BoundNodeId(Node*) const;
@@ -413,8 +412,6 @@ class CORE_EXPORT InspectorDOMAgent final
   void DiscardFrontendBindings();
 
   InspectorRevalidateDOMTask* RevalidateTask();
-
-  bool isNodeScrollable(Node*);
 
   v8::Isolate* isolate_;
   Member<InspectedFrames> inspected_frames_;
