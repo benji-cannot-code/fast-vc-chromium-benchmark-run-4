b41eba6ba453c8b7b04694f01d58273ad7d32b21
Steinar H. Gunderson <sesse@chromium.org>
2024-08-29 13:16:51 +0000

    Remove the non-streaming overload of ConsumeIntegerOrNumberCalc().
    Change-Id: I9b5aa1296231dbb29e4431b84598a530d0359320
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823340
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Auto-Submit: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1348589}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -1606,9 +1606,7 @@ StyleRuleFontFeature* CSSParserImpl::ConsumeFontFeatureRule(
 
     CSSValueList* numbers = CSSValueList::CreateSpaceSeparated();
 
-    CSSParserTokenRange list =
-        stream.ConsumeUntilPeekedTypeIs<kSemicolonToken>();
-    list.ConsumeWhitespace();
+    stream.ConsumeWhitespace();
 
     do {
       if (numbers->length() == max_allowed_values) {
@@ -1616,13 +1614,17 @@ StyleRuleFontFeature* CSSParserImpl::ConsumeFontFeatureRule(
       }
       CSSPrimitiveValue* parsed_number =
           css_parsing_utils::ConsumeIntegerOrNumberCalc(
-              list, *context_,
+              stream, *context_,
               CSSPrimitiveValue::ValueRange::kNonNegativeInteger);
       if (!parsed_number) {
         return nullptr;
       }
       numbers->Append(*parsed_number);
-    } while (!list.AtEnd());
+    } while (stream.Peek().GetType() != kSemicolonToken && !stream.AtEnd());
+
+    if (!stream.AtEnd()) {
+      stream.ConsumeIncludingWhitespace();  // kSemicolonToken
+    }
 
     if (!numbers->length()) {
       return nullptr;
