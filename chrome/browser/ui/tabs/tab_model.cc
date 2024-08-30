81c4947896f70bde8022b1ea6f583d226feee785
Erik Chen <erikchen@chromium.org>
2024-08-30 05:50:04 +0000

    Create TabHelpers in the correct location.
    TabHelpers should be created in exactly one location, which is when the
    tab is created. This CL adds this call, and subsequent CLs will remove
    the other calls.
    
    Bug: 362038320, 362038317
    Change-Id: Id5a0aa92d6344c21f3cbdd906e5168a66fa443ac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827704
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349055}

--- chrome/browser/ui/tabs/tab_model.cc
+++ chrome/browser/ui/tabs/tab_model.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/tab_model.h"
 
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/tab_helpers.h"
 #include "chrome/browser/ui/tabs/features.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -50,6 +51,9 @@ TabModel::TabModel(std::unique_ptr<content::WebContents> contents,
       soon_to_be_owning_model_(soon_to_be_owning_model) {
   TabLookupFromWebContents::CreateForWebContents(contents_, this);
 
+  // TODO(https://crbug.com/362038317): Tab-helpers should be created in exactly
+  // one place, which is here.
+  TabHelpers::AttachTabHelpers(contents_);
   tab_features_ = TabFeatures::CreateTabFeatures();
 
   // Once tabs are pulled into a standalone module, TabFeatures and its
