056660f43758d41f9d15e4e3f302da2a70d28c62
Radu Nitescu <radunitescu@google.com>
2024-08-30 13:20:10 +0000

    [iOS][LO] Add accessibility configuration class for HUD elements
    Bug: None
    Change-Id: I8b13dc0dfaf9d60ec336b070b98a55d32f5f9a64
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827531
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349174}

---
+++ ios/public/provider/chrome/browser/lens/lens_overlay_accessibility_configuration.mm
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/public/provider/chrome/browser/lens/lens_overlay_accessibility_configuration.h"
+
+@implementation ChromeLensAccessibilityConfiguration
+@end
