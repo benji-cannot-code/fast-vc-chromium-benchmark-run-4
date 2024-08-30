b2b06843853fd54785c55dfa1f0e6c7edde8de7a
Timofey Chudakov <tchudakov@google.com>
2024-08-30 13:51:10 +0000

    Move ManualFillingViewAndroid from passwords/ to autofill/
    This change is done to simplify ownership over
    ManualFillingViewAndroid.
    
    Bug: n/a
    Change-Id: Ic88631ae41c20370ce2b3fd57e1e3c1a98d5849e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823418
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Friedrich Hauser <fhorschig@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1349183}

--- chrome/browser/ui/android/autofill/manual_filling_view_android.cc
+++ chrome/browser/ui/android/autofill/manual_filling_view_android.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/android/passwords/manual_filling_view_android.h"
+#include "chrome/browser/ui/android/autofill/manual_filling_view_android.h"
 
 #include <jni.h>
 
--- chrome/browser/ui/android/autofill/manual_filling_view_android.h
+++ chrome/browser/ui/android/autofill/manual_filling_view_android.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ANDROID_PASSWORDS_MANUAL_FILLING_VIEW_ANDROID_H_
-#define CHROME_BROWSER_UI_ANDROID_PASSWORDS_MANUAL_FILLING_VIEW_ANDROID_H_
+#ifndef CHROME_BROWSER_UI_ANDROID_AUTOFILL_MANUAL_FILLING_VIEW_ANDROID_H_
+#define CHROME_BROWSER_UI_ANDROID_AUTOFILL_MANUAL_FILLING_VIEW_ANDROID_H_
 
 #include <jni.h>
 
@@ -14,10 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/thread_pool.h"
 #include "chrome/browser/autofill/manual_filling_view_interface.h"
 #include "chrome/browser/keyboard_accessory/android/accessory_sheet_data.h"
-
-namespace gfx {
-class Image;
-}
 
 namespace content {
 class WebContents;
@@ -91,4 +87,4 @@ class ManualFillingViewAndroid : public ManualFillingViewInterface {
   base::android::ScopedJavaGlobalRef<jobject> java_object_internal_;
 };
 
-#endif  // CHROME_BROWSER_UI_ANDROID_PASSWORDS_MANUAL_FILLING_VIEW_ANDROID_H_
+#endif  // CHROME_BROWSER_UI_ANDROID_AUTOFILL_MANUAL_FILLING_VIEW_ANDROID_H_
