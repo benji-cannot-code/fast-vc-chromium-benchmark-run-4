dcc9a44a8c291161b0806e001ba6fac9f31ca1c3
Xinyi Ji <xinyiji@google.com>
2024-08-30 17:16:40 +0000

    [EducationalTip] Implements infrastructure to support multiple tip cards.
    This CL implements infrastructure to support multiple tip cards in the module. It uses the default browser promo card as an example, as illustrated in the screenshot below:
    https://docs.google.com/document/d/1ljR6br1dhznkmXQ1FfT1xBaZ12931qvrq78Vh9-8-dQ/edit?usp=sharing&resourcekey=0-gD3hg0r1FrRpBv2hq9Xlug
    
    Low-Coverage-Reason: TRIVIAL_CHANGE More unit tests will be added in the following CLs.
    Bug: 355015904
    Change-Id: I29f82505bab7e85a7dffa83e98a18de3dbcb4847
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5817540
    Commit-Queue: Xinyi Ji <xinyiji@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349290}

--- chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/EducationalTipModuleProperties.java
+++ chrome/browser/educational_tip/java/src/org/chromium/chrome/browser/educational_tip/EducationalTipModuleProperties.java
@@ -5,9 +5,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.educational_tip;
 
+import android.view.View.OnClickListener;
+
 import org.chromium.ui.modelutil.PropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
+import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
 /** Properties for the educational tip module. */
 interface EducationalTipModuleProperties {
-    PropertyKey[] ALL_KEYS = new PropertyKey[] {};
+    WritableObjectPropertyKey<String> MODULE_CONTENT_TITLE_STRING =
+            new WritableObjectPropertyKey<>();
+    WritableObjectPropertyKey<String> MODULE_CONTENT_DESCRIPTION_STRING =
+            new WritableObjectPropertyKey<>();
+    WritableObjectPropertyKey<OnClickListener> MODULE_BUTTON_ON_CLICK_LISTENER =
+            new WritableObjectPropertyKey<>();
+    WritableIntPropertyKey MODULE_CONTENT_IMAGE = new WritableIntPropertyKey();
+
+    PropertyKey[] ALL_KEYS =
+            new PropertyKey[] {
+                MODULE_CONTENT_TITLE_STRING,
+                MODULE_CONTENT_DESCRIPTION_STRING,
+                MODULE_BUTTON_ON_CLICK_LISTENER,
+                MODULE_CONTENT_IMAGE
+            };
 }
