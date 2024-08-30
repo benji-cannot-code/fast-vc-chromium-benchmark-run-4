abb87e0b61a477d1c19d3d98c933c17418edf13f
Morten Stenshorne <mstensho@chromium.org>
2024-08-30 19:23:35 +0000

    Support z-index on page margin boxes.
    If page margin boxes overlap with each other (which should be rare and
    normally not ideal and probably ugly), z-index may be used to specify
    paint order (default paint order being clockwise from top left corner
    margin box).
    
    This CL implements that, except that we cannot control the actual
    paginated content (the page area), which will be painted on top of
    everything else unconditionally, since it's in a separate layer. See
    crbug.com/363031541
    
    Move paginated root / page container paint code into a separate function
    (rather than adding more code to the main paint machinery), and add
    support for z-index on children there.
    
    Also make sure that page border box fragments are set up to paint
    atomically, just like page margin box fragmets, or the page border would
    be painted in the background phase, and margin borders in the foreground
    phase (which is the phase that paints everything when painting is
    atomic).
    
    Bug: 40341678
    Change-Id: I480ebf8e8fc2478dfa28163cd001b22371295270
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828544
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Xianzhu Wang <wangxianzhu@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349371}

--- third_party/blink/renderer/core/paint/paint_info.h
+++ third_party/blink/renderer/core/paint/paint_info.h
@@ -173,6 +173,7 @@ struct CORE_EXPORT PaintInfo {
   bool DescendantPaintingBlocked() const {
     return descendant_painting_blocked_;
   }
+  void SetDescendantPaintingBlocked() { descendant_painting_blocked_ = true; }
 
   GraphicsContext& context;
   PaintPhase phase;
@@ -197,8 +198,6 @@ struct CORE_EXPORT PaintInfo {
 
   bool is_painting_background_in_contents_space = false;
   bool skips_background_ = false;
-
-  // Used by display-locking.
   bool descendant_painting_blocked_ = false;
 };
 
