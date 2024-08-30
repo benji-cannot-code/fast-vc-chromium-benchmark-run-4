66cbda1579267ef5b55aad93a0f94c4b86307642
curranmax <curranmax@chromium.org>
2024-08-30 23:04:27 +0000

    Add ModelPrototypingLoggingData to LogAiDataRequest.
    Bug: b/363029050
    Change-Id: I55f1356a0055840918e19d3b2fd2fcc2c8fcefea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828477
    Commit-Queue: Max Curran <curranmax@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Auto-Submit: Max Curran <curranmax@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349468}

--- components/optimization_guide/core/model_quality/feature_type_map.h
+++ components/optimization_guide/core/model_quality/feature_type_map.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/proto/features/forms_predictions.pb.h"
 #include "components/optimization_guide/proto/features/history_answer.pb.h"
 #include "components/optimization_guide/proto/features/history_query.pb.h"
+#include "components/optimization_guide/proto/features/model_prototyping.pb.h"
 #include "components/optimization_guide/proto/features/tab_organization.pb.h"
 #include "components/optimization_guide/proto/features/wallpaper_search.pb.h"
 #include "components/optimization_guide/proto/model_execution.pb.h"
@@ -136,7 +137,18 @@ class FormsPredictionsFeatureTypeMap {
     return ai_data_request.mutable_forms_predictions();
   }
 
-  static std::string_view ToString() { return "FormsPredcitions"; }
+  static std::string_view ToString() { return "FormsPredictions"; }
+};
+
+class ModelPrototypingFeatureTypeMap {
+ public:
+  using LoggingData = proto::ModelPrototypingLoggingData;
+
+  static LoggingData* GetLoggingData(proto::LogAiDataRequest& ai_data_request) {
+    return ai_data_request.mutable_model_prototyping();
+  }
+
+  static std::string_view ToString() { return "ModelPrototyping"; }
 };
 
 }  // namespace optimization_guide
