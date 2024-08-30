ec06c84f52512dd961d139d8e9af192e1e2eb6b7
Ken Rockot <rockot@google.com>
2024-08-30 19:09:21 +0000

    VizLayers: Sync more properties
    Syncs more property tree state to display trees and fixes some minor
    issues with existing synchronization.
    
    Bug: 40902503
    Change-Id: Iaae82fd7114fd3345a6a1fca8048f58a2ae257e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827142
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349360}

--- cc/trees/property_tree.cc
+++ cc/trees/property_tree.cc
@@ -49,6 +49,9 @@ AnchorPositionScrollData::AnchorPositionScrollData(
 bool AnchorPositionScrollData::operator==(
     const AnchorPositionScrollData& other) const = default;
 
+bool StickyPositionNodeData::operator==(
+    const StickyPositionNodeData& other) const = default;
+
 template <typename T>
 PropertyTree<T>::PropertyTree(PropertyTrees* property_trees)
     : needs_update_(false), property_trees_(property_trees) {
