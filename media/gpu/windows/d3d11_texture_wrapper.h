4583be52a9b9ef7a1e8660d65c67c987018a0f8d
Mingjing Zhang <mjzhang@chromium.org>
2024-08-30 21:27:24 +0000

    Remove the UseClientSharedImageForD3D11Video feature
    This CL removes the UseClientSharedImageForD3D11Video feature now that
    M128 becomes 100% stable on desktop.
    
    Bug: 1494911
    Change-Id: If483002b1789b5cc4947a934a68a7d6dfe92a3af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5826077
    Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349425}

--- media/gpu/windows/d3d11_texture_wrapper.h
+++ media/gpu/windows/d3d11_texture_wrapper.h
@@ -28,8 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-BASE_DECLARE_FEATURE(kUseClientSharedImageForD3D11Video);
-
 class D3D11PictureBuffer;
 
 using CommandBufferHelperPtr = scoped_refptr<CommandBufferHelper>;
@@ -131,7 +129,6 @@ class MEDIA_GPU_EXPORT DefaultTexture2DWrapper : public Texture2DWrapper {
    public:
     GpuResources(OnErrorCB on_error_cb,
                  GetCommandBufferHelperCB get_helper_cb,
-                 const gpu::Mailbox& mailbox,
                  const gfx::Size& size,
                  const gfx::ColorSpace& color_space,
                  DXGI_FORMAT dxgi_format,
