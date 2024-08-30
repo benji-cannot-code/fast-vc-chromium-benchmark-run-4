00cb9b621afcdb8e66bda153031684710da899cf
Sylvain Defresne <sdefresne@chromium.org>
2024-08-30 13:52:04 +0000

    [ios] Add {Refcounted,}ProfileKeyedServiceFactoryIOS
    Add new base class for KeyedServiceFactory for ProfileIOS. They
    should simplify writing new factories by
    
    - automatically registering themselves with the DependencyManager
    - offering customisation via traits instead of overriding methods
    - exposing GetServiceForProfileAs<...> which cast the service
    
    This should reduce the boilerplate for new KeyedService factories
    to the following:
    
        class MyServiceFactory
            : public ProfileKeyedServiceFactoryIOS {
         public:
          static MyServiceFactory* GetInstance() {
            static base::NoDestructor<MyServiceFactory> instance;
            return instance.get();
          }
    
          static MyService* GetForProfile(ProfileIOS* profile) {
            return GetInstance()
                ->GetServiceForProfileAs<MyService>(profile, true);
          }
    
         private:
          friend class base::NoDestructor<MyServiceFactory>;
    
          // Declares that the service should be the same for
          // regular and off-the-record profile, that it must
          // be created at the same time as the profile and
          // that it should not be created for test profiles.
          MyServiceFactory()
              : ProfileKeyedServiceFactoryIOS(
                  "MyService",
                  ProfileSelection::kRedirectedInIncognito,
                  ServiceCreation::kCreateWithProfile,
                  TestingCreation::kNoServiceForTests) {
            DependsOn(OtherServiceFactory::GetInstance());
          }
    
          // KeyedServiceFactory:
          std::unique_ptr<KeyedService> BuildServiceInstanceFor(
              web::BrowserState* context) const override {
            ProfileIOS* profile =
                ProfileIOS::FromBrowserState(context);
            return std::make_unique<MyService>(
                profile->GetPrefs(),
                OtherServiceFactory::GetForProfile(profile));
          }
        };
    
    Bug: 363173340
    Change-Id: I4acdd478f91092e86615ccf1e8310015566c389d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823483
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1349184}

---
+++ ios/chrome/browser/shared/model/profile/profile_keyed_service_utils.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_PROFILE_KEYED_SERVICE_UTILS_H_
+#define IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_PROFILE_KEYED_SERVICE_UTILS_H_
+
+#include "ios/chrome/browser/shared/model/profile/profile_keyed_service_traits.h"
+#include "ios/web/public/browser_state.h"
+
+// Returns the context to use for KeyedServiceFactory according for `context`
+// according to `profile_selection`.
+web::BrowserState* GetContextToUseForKeyedServiceFactory(
+    web::BrowserState* context,
+    ProfileSelection profile_selection);
+
+#endif  // IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_PROFILE_KEYED_SERVICE_UTILS_H_
