2f6c0db5d826a297056a2cf7feba3d01c605ee8b
Aiden Chiavatti <chiav@chromium.org>
2024-08-30 20:56:45 +0000

    Reland "update_annotations_sheet.py: Retry HTTP requests up to 5 times"
    This is a reland of commit e2a69e283c11d6f8064ca3a36ff96a315f99ba7f
    
    It should fix "NameError: name 'googleapiclient' is not defined"
    
    Test: vpython3 tools/traffic_annotation/scripts/update_annotations_sheet.py
    
    Original change's description:
    > update_annotations_sheet.py: Retry HTTP requests up to 5 times
    >
    > This is flaking pretty frequently on waterfall due to 502 errors. Add
    > retries with exponential backoff, and hopefully reduce flakes.
    >
    > Bug: b/363052228
    > Change-Id: I2c1d7e0a2586d3f56a0a8716347fcba521688caf
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827394
    > Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    > Reviewed-by: Aiden Chiavatti <chiav@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1348915}
    
    Bug: b/363052228
    Change-Id: I72a15b8c6e95d9dd56cadd8d2254b5b41035aecd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828574
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Commit-Queue: Aiden Chiavatti <chiav@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349408}

--- tools/traffic_annotation/scripts/update_annotations_sheet.py
+++ tools/traffic_annotation/scripts/update_annotations_sheet.py
@@ -26,7 +26,7 @@ import os
 import re
 import sys
 
-from apiclient import discovery
+from apiclient import discovery, http as googlehttp
 from infra_libs import luci_auth
 from oauth2client import client
 from oauth2client import tools
@@ -50,6 +50,13 @@ def GetCurrentChromeVersion():
 
 def VersionTupleToString(version_tuple):
   return '.'.join(map(str, version_tuple))
+
+
+class HttpRequestWithRetries(googlehttp.HttpRequest):
+  """Same as HttpRequest, but all requests are retried up to 5 times."""
+
+  def execute(self, http=None, num_retries=1):
+    return super().execute(http=http, num_retries=5)
 
 
 class SheetEditor():
@@ -114,7 +121,10 @@ class SheetEditor():
     """
     http = credentials.authorize(httplib2.Http())
     discoveryUrl = ("https://sheets.googleapis.com/$discovery/rest?version=v4")
-    return discovery.build("sheets", "v4", http=http,
+    return discovery.build("sheets",
+                           "v4",
+                           http=http,
+                           requestBuilder=HttpRequestWithRetries,
                            discoveryServiceUrl=discoveryUrl)
 
 
