b35380c76eabb29fdb5b94c803d7cb1d45653d7c
Michael Thiessen <mthiesse@chromium.org>
2024-08-30 15:37:40 +0000

    Add UsePollForMessagePumpEpoll to ProductionSupportedFlagList
    Purely performance-related experiment that will affect WebView as it
    changes MessagePumpEpoll to use poll() instead of epoll() with the goal
    being better scheduling and lower latency for IPCs.
    
    Bug: 356125521
    Change-Id: I50ea66ea06f74b2f47dce1e4194ac7e80ba0098c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824961
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Benoit Lize <lizeb@chromium.org>
    Reviewed-by: Benoit Lize <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349230}

--- android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
+++ android_webview/java/src/org/chromium/android_webview/common/ProductionSupportedFlagList.java
@@ -1045,6 +1045,9 @@ public final class ProductionSupportedFlagList {
         Flag.baseFeature(
                 "PartitionAllocLargeEmptySlotSpanRing",
                 "Tuning memory allocator for speed - large empty slot span ring"),
+        Flag.baseFeature(
+                "UsePollForMessagePumpEpoll",
+                "Uses poll() instead of epoll() for MessagePumpEpoll"),
 
         // Add new commandline switches and features above. The final entry should have a
         // trailing comma for cleaner diffs.
