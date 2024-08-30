5477919fe8ae1bf1d8dbe6a0d24f9a213efb48e9
Andrew Grieve <agrieve@chromium.org>
2024-08-30 22:51:29 +0000

    Revert "Reland "Android: AppHooks->ServiceLoader for AccountManagerFacadeProvider""
    This reverts commit 59a922647631be5b984fc601c2887ad807531b53.
    
    Reason for revert: Release builds not working
    
    Original change's description:
    > Reland "Android: AppHooks->ServiceLoader for AccountManagerFacadeProvider"
    >
    > This reverts commit 538e386386cf7767b41a1b855b0edc58e14ece88.
    >
    > Reason for reland: Debug support has been added (separately)
    >
    > Original change's description:
    > > Revert "Android: AppHooks->ServiceLoader for AccountManagerFacadeProvider"
    > >
    > > This reverts commit 6c1b63c6f88089b648501c9bac617dff331af634.
    > >
    > > Reason for revert: Might not work in debug builds
    > >
    > > Original change's description:
    > > > Android: AppHooks->ServiceLoader for AccountManagerFacadeProvider
    > > >
    > > > Bug: 40901855
    > > > Change-Id: I8ecba2cf1f6aa5f3054bbecd8023959813e3adee
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5738736
    > > > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > > > Reviewed-by: Sam Maier <smaier@chromium.org>
    > > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > > Cr-Commit-Position: refs/heads/main@{#1342968}
    > >
    > > Bug: 40901855
    > > Change-Id: I8b2a628a9f44a77dd1d3d49f39abc121553e8a54
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5791597
    > > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > > Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1343020}
    >
    > Bug: 40901855
    > Change-Id: I3c55a1fe18498323f8e52ebec2055b973afc9760
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827125
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    > Owners-Override: Andrew Grieve <agrieve@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1349168}
    
    Bug: 40901855
    Change-Id: I36075818491ee19699a6888169b7b7a920ce1d55
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830121
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1349463}

--- components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
+++ components/signin/public/android/junit/src/org/chromium/components/signin/AccountManagerFacadeImplTest.java
@@ -488,6 +488,11 @@ public class AccountManagerFacadeImplTest {
                 originalToken.getToken());
     }
 
+    @Test(expected = IllegalStateException.class)
+    public void testAccountManagerFacadeProviderGetNullInstance() {
+        AccountManagerFacadeProvider.getInstance();
+    }
+
     @Test
     public void testGetAccountCapabilitiesResponseYes() throws Exception {
         AccountManagerFacade facade = new AccountManagerFacadeImpl(mDelegate);
