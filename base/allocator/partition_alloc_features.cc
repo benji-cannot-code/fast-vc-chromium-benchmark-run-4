25adb457ed393198564d88444e8d4fee5dd17a35
Kalvin Lee <kdlee@chromium.org>
2024-08-30 02:38:33 +0000

    PA: Make `free()` a no-op in browser shutdown
    This change is limited to CrOS. Experimentation shows that we can reduce
    shutdown hangs by reducing (nullifying) `free()` action in the shutdown
    process.
    
    Bug: 40802063
    Change-Id: Ib22c37868f1b8e8f70e612bb4781155131861191
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825832
    Auto-Submit: Kalvin Lee <kdlee@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1349027}

--- base/allocator/partition_alloc_features.cc
+++ base/allocator/partition_alloc_features.cc
@@ -406,7 +406,12 @@ BASE_FEATURE(kUsePoolOffsetFreelists,
 
 BASE_FEATURE(kPartitionAllocMakeFreeNoOpOnShutdown,
              "PartitionAllocMakeFreeNoOpOnShutdown",
-             FEATURE_DISABLED_BY_DEFAULT);
+#if PA_BUILDFLAG(IS_CHROMEOS)
+             FEATURE_ENABLED_BY_DEFAULT
+#else
+             FEATURE_DISABLED_BY_DEFAULT
+#endif
+);
 
 constexpr FeatureParam<WhenFreeBecomesNoOp>::Option
     kPartitionAllocMakeFreeNoOpOnShutdownOptions[] = {
@@ -430,7 +435,7 @@ constexpr FeatureParam<WhenFreeBecomesNoOp>::Option
 const base::FeatureParam<WhenFreeBecomesNoOp>
     kPartitionAllocMakeFreeNoOpOnShutdownParam{
         &kPartitionAllocMakeFreeNoOpOnShutdown, "callsite",
-        WhenFreeBecomesNoOp::kBeforeShutDownThreads,
+        WhenFreeBecomesNoOp::kBeforePreShutdown,
         &kPartitionAllocMakeFreeNoOpOnShutdownOptions};
 
 void MakeFreeNoOp(WhenFreeBecomesNoOp callsite) {
