5cd2abe0f55ca91c83ffaf74a5f154e62762df8b
Mark Rowe <markrowe@chromium.org>
2024-08-30 16:36:50 +0000

    Change MojoAudioInputStreamTest::delegate_event_handler_ to be a raw_ptr
    This required reordering / explicit clearing of these fields to avoid dangling pointers.
    
    Fixed: 346694007
    Change-Id: I5a7f98c19abffc24321c76d48c8d89025d840d40
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818071
    Auto-Submit: Mark Rowe <markrowe@chromium.org>
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349259}

--- media/mojo/services/mojo_audio_input_stream_unittest.cc
+++ media/mojo/services/mojo_audio_input_stream_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
-#include "base/memory/raw_ptr_exclusion.h"
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/run_loop.h"
 #include "base/sync_socket.h"
@@ -185,16 +184,13 @@ class MojoAudioInputStreamTest : public Test {
   base::CancelableSyncSocket local_;
   std::unique_ptr<TestCancelableSyncSocket> foreign_socket_;
   base::ReadOnlySharedMemoryRegion mem_;
-  raw_ptr<StrictMock<MockDelegate>, DanglingUntriaged> delegate_ = nullptr;
-  // This field is not a raw_ptr<> because it was filtered by the rewriter for:
-  // #addr-of
-  RAW_PTR_EXCLUSION AudioInputDelegate::EventHandler* delegate_event_handler_ =
-      nullptr;
   StrictMock<MockDelegateFactory> mock_delegate_factory_;
   StrictMock<MockDeleter> deleter_;
   StrictMock<MockClient> client_;
   mojo::Receiver<media::mojom::AudioInputStreamClient> client_receiver_;
   std::unique_ptr<MojoAudioInputStream> impl_;
+  raw_ptr<StrictMock<MockDelegate>> delegate_ = nullptr;
+  raw_ptr<AudioInputDelegate::EventHandler> delegate_event_handler_ = nullptr;
 };
 
 TEST_F(MojoAudioInputStreamTest, NoDelegate_SignalsError) {
@@ -239,6 +235,11 @@ TEST_F(MojoAudioInputStreamTest, DestructWithCallPending_Safe) {
                                            std::move(foreign_socket_),
                                            kInitiallyNotMuted);
   audio_input->Record();
+
+  // Depends on `impl_` so must be cleared before it is destroyed.
+  delegate_event_handler_ = nullptr;
+  delegate_ = nullptr;
+
   impl_.reset();
   base::RunLoop().RunUntilIdle();
 }
