75d0b3c28692c89aa1b02803843f6475adc73660
Radu Nitescu <radunitescu@google.com>
2024-08-30 16:39:22 +0000

    [iOS][LO] Make lens overlay fullscreen
    Bug: None
    Change-Id: I12182a6b4876b1aa49e353593d72e9a5c744c019
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829042
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349261}

--- ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_overlay_container_view_controller.mm
@@ -44,9 +44,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       NO;
   [NSLayoutConstraint activateConstraints:@[
     [self.selectionViewController.view.topAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.topAnchor],
+        constraintEqualToAnchor:self.view.topAnchor],
     [self.selectionViewController.view.bottomAnchor
-        constraintEqualToAnchor:self.view.safeAreaLayoutGuide.bottomAnchor],
+        constraintEqualToAnchor:self.view.bottomAnchor],
     [self.selectionViewController.view.leftAnchor
         constraintEqualToAnchor:self.view.safeAreaLayoutGuide.leftAnchor],
     [self.selectionViewController.view.rightAnchor
