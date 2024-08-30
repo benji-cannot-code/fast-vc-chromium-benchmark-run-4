80b0292fb3c1fc132a410349f23d192be3de3fa2
Ella Ge <eirage@chromium.org>
2024-08-30 16:51:32 +0000

    [CodeHealth] Replace WebappDataStorage#Clock with TimeUtils
    Change-Id: Ifa7d6deae9eebaea848c6f82dcf10218b6c203c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823741
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Glenn Hartmann <hartmanng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349274}

--- chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebappDirectoryManagerTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/webapps/WebappDirectoryManagerTest.java
@@ -19,6 +19,7 @@ import org.robolectric.shadows.ShadowApplication;
 import org.robolectric.shadows.ShadowLooper;
 
 import org.chromium.base.ContextUtils;
+import org.chromium.base.FakeTimeTestRule;
 import org.chromium.base.PathUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.metrics.RecordHistogram;
@@ -37,7 +38,7 @@ import java.util.concurrent.TimeUnit;
         shadows = {CustomShadowAsyncTask.class})
 @LooperMode(LooperMode.Mode.LEGACY)
 public class WebappDirectoryManagerTest {
-    @Rule public MockWebappDataStorageClockRule mClockRule = new MockWebappDataStorageClockRule();
+    @Rule public FakeTimeTestRule mClockRule = new FakeTimeTestRule();
 
     private static final String WEBAPK_PACKAGE_NAME_1 = "webapk_1";
     private static final String WEBAPK_PACKAGE_NAME_2 = "webapk_2";
@@ -124,7 +125,7 @@ public class WebappDirectoryManagerTest {
         registerWebapp(WEBAPK_ID_1);
         WebappDataStorage storage = WebappRegistry.getInstance().getWebappDataStorage(WEBAPK_ID_1);
         storage.updateTimeOfLastCheckForUpdatedWebManifest();
-        mClockRule.advance(TimeUnit.DAYS.toMillis(30));
+        mClockRule.advanceMillis(TimeUnit.DAYS.toMillis(30));
 
         runCleanup();
         Assert.assertEquals(
@@ -146,7 +147,7 @@ public class WebappDirectoryManagerTest {
         registerWebapp(WEBAPK_ID_1);
         WebappDataStorage storage = WebappRegistry.getInstance().getWebappDataStorage(WEBAPK_ID_1);
         storage.updateTimeOfLastCheckForUpdatedWebManifest();
-        mClockRule.advance(1);
+        mClockRule.advanceMillis(1);
 
         runCleanup();
         Assert.assertEquals(
