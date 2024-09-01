f8035d5367c6a93321598d10821e39e071cbf709
Antonio Gomes <tonikitoo@igalia.com>
2024-09-01 17:31:55 +0000

    Move inheritance of TabStripModelObserver from ProductSpecificationsEntryPointController
    .. into an inner class and shadow its declaration to the respective .cc.
    
    This is a temporary solution but a step foward to the modularization
    of //c/b/ui/commerce, and can be reverted once //c/b/ui/tabs gets
    fully modularized.
    
    In practice, this CL allows product_specifications_entry_point_controller.h
    not include //c/b/ui/tabs/tab_strip_model_observer.h
    (from //c/b/ui:ui GN target), avoiding the circular dependency to
    //c/b/ui:ui from a header.
    
    DanglingUntriaged-notes: the lifetime of TabStripModelObserverImpl is
    tied to ProductSpecificationsEntryPointController, and does not impose
    risks of accessing a dangling pointer.
    
    Bug: 362675963
    Change-Id: I68672e1d671e0a931b0b805032f5a88dcec60baa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824395
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1349652}

--- chrome/browser/ui/commerce/product_specifications_entry_point_controller.h
+++ chrome/browser/ui/commerce/product_specifications_entry_point_controller.h
@@ -8,14 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "components/commerce/core/compare/cluster_manager.h"
 
 class Browser;
-
-namespace content {
-class WebContents;
-}
 
 namespace commerce {
 
@@ -23,8 +18,7 @@ class ShoppingService;
 class ProductSpecificationsService;
 
 class ProductSpecificationsEntryPointController
-    : public TabStripModelObserver,
-      public ClusterManager::Observer {
+    : public ClusterManager::Observer {
  public:
   // Observer that will listen to ProductSpecificationsEntryPointController for
   // updates regarding visibility and content of the entry point.
@@ -47,15 +41,6 @@ class ProductSpecificationsEntryPointController
 
   explicit ProductSpecificationsEntryPointController(Browser* browser);
   ~ProductSpecificationsEntryPointController() override;
-
-  // TabStripModelObserver:
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
-  void TabChangedAt(content::WebContents* contents,
-                    int index,
-                    TabChangeType change_type) override;
 
   // Registers an observer.
   void AddObserver(Observer* observer);
@@ -119,6 +104,9 @@ class ProductSpecificationsEntryPointController
 
   // Helper method to show the entry point with title.
   void ShowEntryPointWithTitle(std::optional<EntryPointInfo> entry_point_info);
+
+  class TabStripModelObserverImpl;
+  std::unique_ptr<TabStripModelObserverImpl> tab_strip_model_observer_impl_;
 
   // Info of the entry point that is currently showing, when available.
   std::optional<EntryPointInfo> current_entry_point_info_;
