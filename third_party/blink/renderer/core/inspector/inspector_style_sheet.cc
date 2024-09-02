1cd0ce182544b25b24fa62a1bfb59f85cea0b353
Morten Stenshorne <mstensho@chromium.org>
2024-09-02 13:24:59 +0000

    @font-palette-values cannot have child rules.
    No need for rule flattening then.
    
    crrev.com/c/5001710 introduced inspector support for
    @font-palette-values, but with inconsistent behavior in
    FlattenSourceData() vs. CollectFlatRules(), with one attempting
    flattening, the other not.
    
    Just don't attempt to flatten.
    
    No behavior changes intended.
    
    Bug: 40942094
    Change-Id: Ic762f8d622574e684238468a8fa61aa01f49871a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829105
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349831}

--- third_party/blink/renderer/core/inspector/inspector_style_sheet.cc
+++ third_party/blink/renderer/core/inspector/inspector_style_sheet.cc
@@ -1047,6 +1047,7 @@ void FlattenSourceData(const CSSRuleSourceDataList& data_list,
       case StyleRule::kFontFeature:
       case StyleRule::kPositionTry:
       case StyleRule::kViewTransition:
+      case StyleRule::kFontPaletteValues:
         result->push_back(data);
         break;
       case StyleRule::kStyle:
@@ -1058,7 +1059,6 @@ void FlattenSourceData(const CSSRuleSourceDataList& data_list,
       case StyleRule::kLayerBlock:
       case StyleRule::kFontFeatureValues:
       case StyleRule::kProperty:
-      case StyleRule::kFontPaletteValues:
         result->push_back(data);
         FlattenSourceData(data->child_rules, result);
         break;
