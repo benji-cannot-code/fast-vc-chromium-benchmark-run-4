7271ef12ad6da0d783552679708d4c39fa532b63
Darren Shen <shend@chromium.org>
2024-09-02 11:21:44 +0000

    Add button to an image row container to show more results.
    Bug: b:362129770
    Change-Id: I0b284eae0f8d49970ec8731ee95fb39e349ebd6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824116
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349802}

--- ash/picker/views/picker_image_item_row_view.h
+++ ash/picker/views/picker_image_item_row_view.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/view.h"
 
 namespace views {
+class ImageButton;
 class ImageView;
 class View;
 }  // namespace views
@@ -31,7 +32,8 @@ class ASH_EXPORT PickerImageItemRowView
   METADATA_HEADER(PickerImageItemRowView, views::BoxLayoutView)
 
  public:
-  PickerImageItemRowView();
+  explicit PickerImageItemRowView(
+      base::RepeatingClosure more_items_button = {});
   PickerImageItemRowView(const PickerImageItemRowView&) = delete;
   PickerImageItemRowView& operator=(const PickerImageItemRowView&) = delete;
   ~PickerImageItemRowView() override;
@@ -54,12 +56,16 @@ class ASH_EXPORT PickerImageItemRowView
   bool ContainsItem(views::View* item) override;
 
   views::View::Views GetItemsForTesting() const;
+  views::ImageButton* GetMoreItemsButtonForTesting() {
+    return more_items_button_;
+  }
 
  private:
   views::View* GetLeftmostItem();
 
   raw_ptr<views::ImageView> leading_icon_view_ = nullptr;
   raw_ptr<views::View> items_container_ = nullptr;
+  raw_ptr<views::ImageButton> more_items_button_ = nullptr;
 };
 
 BEGIN_VIEW_BUILDER(ASH_EXPORT, PickerImageItemRowView, views::BoxLayoutView)
