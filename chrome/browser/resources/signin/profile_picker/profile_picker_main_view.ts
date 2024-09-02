7398d7066fd7e36124c2f79e23557f432c968d20
anthie@google.com <anthie@google.com>
2024-09-02 10:34:12 +0000

    Reland "Update guest mode in the profile picker on adding/removing a SU"
    This is a reland of commit 80e17e82e12238b62666a9627cdbbd51f2987acd
    Difference to original change: On the Lacros only tests,
    in profile_picker_handler_unittest, the method
    `CreateTestingProfile` had been accidentally renamed. Restored the
    original name.
    
    Original change's description:
    > Update guest mode in the profile picker on adding/removing a SU
    >
    > The ui of the profile picker is updating the guest mode
    > button, following an addition or deletion of a supervised
    > profile, or change in supervision status for a profile.
    >
    > Bug: b/361570723, b/361725332
    > Change-Id: I88ab08b3c8d28e1b4fd326cce23e5b5360087f91
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809498
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Reviewed-by: Alex Ilin <alexilin@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1349145}
    
    Bug: b/361570723, b/361725332, 363931550
    Change-Id: I0af10302dc9f6c78b57904ae12dcd0f3c5d98d9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829049
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Cr-Commit-Position: refs/heads/main@{#1349794}

--- chrome/browser/resources/signin/profile_picker/profile_picker_main_view.ts
+++ chrome/browser/resources/signin/profile_picker/profile_picker_main_view.ts
@@ -25,7 +25,7 @@ import {DragDropReorderTileListDelegate} from './drag_drop_reorder_tile_list_del
 import type {ManageProfilesBrowserProxy, ProfileState} from './manage_profiles_browser_proxy.js';
 import {ManageProfilesBrowserProxyImpl} from './manage_profiles_browser_proxy.js';
 import {navigateTo, NavigationMixin, Routes} from './navigation_mixin.js';
-import {isAskOnStartupAllowed, isGuestModeEnabled, isProfileCreationAllowed} from './policy_helper.js';
+import {isAskOnStartupAllowed, isProfileCreationAllowed} from './policy_helper.js';
 import {getCss} from './profile_picker_main_view.css.js';
 import {getHtml} from './profile_picker_main_view.html.js';
 
@@ -67,6 +67,7 @@ export class ProfilePickerMainViewElement extends
       profilesListLoaded_: {type: Boolean},
       hideAskOnStartup_: {type: Boolean},
       askOnStartup_: {type: Boolean},
+      guestModeEnabled_: {type: Boolean},
       forceSigninErrorDialogTitle_: {type: String},
       forceSigninErrorDialogBody_: {type: String},
       forceSigninErrorProfilePath_: {type: String},
@@ -78,6 +79,10 @@ export class ProfilePickerMainViewElement extends
   protected profilesListLoaded_: boolean = false;
   protected hideAskOnStartup_: boolean = false;
   protected askOnStartup_: boolean = loadTimeData.getBoolean('askOnStartup');
+  // Initial value when the page is rendered.
+  // Potentially updated on profile addition/removal/sign-in.
+  protected guestModeEnabled_: boolean =
+      loadTimeData.getBoolean('isGuestModeEnabled');
   private manageProfilesBrowserProxy_: ManageProfilesBrowserProxy =
       ManageProfilesBrowserProxyImpl.getInstance();
   private resizeObserver_: ResizeObserver|null = null;
@@ -95,7 +100,7 @@ export class ProfilePickerMainViewElement extends
   protected shouldShownSigninButton_: boolean = false;
 
   override firstUpdated() {
-    if (!isGuestModeEnabled()) {
+    if (!this.guestModeEnabled_) {
       this.$.browseAsGuestButton.style.display = 'none';
     }
 
@@ -119,6 +124,9 @@ export class ProfilePickerMainViewElement extends
         'display-force-signin-error-dialog',
         (title: string, body: string, profilePath: string) =>
             this.showForceSigninErrorDialog(title, body, profilePath));
+    this.addWebUiListener(
+        'guest-mode-availability-updated',
+        this.maybeUpdateGuestMode_.bind(this));
     this.manageProfilesBrowserProxy_.initializeMainView();
   }
 
@@ -222,10 +230,22 @@ export class ProfilePickerMainViewElement extends
   }
 
   protected onLaunchGuestProfileClick_() {
-    if (!isGuestModeEnabled()) {
+    if (!this.guestModeEnabled_) {
       return;
     }
     this.manageProfilesBrowserProxy_.launchGuestProfile();
+  }
+
+  private maybeUpdateGuestMode_(enableGuestMode: boolean) {
+    if (enableGuestMode === this.guestModeEnabled_) {
+      return;
+    }
+    this.guestModeEnabled_ = enableGuestMode;
+    if (enableGuestMode) {
+      this.$.browseAsGuestButton.style.display = '';
+    } else {
+      this.$.browseAsGuestButton.style.display = 'none';
+    }
   }
 
   private handleProfileRemoved_(profilePath: string) {
