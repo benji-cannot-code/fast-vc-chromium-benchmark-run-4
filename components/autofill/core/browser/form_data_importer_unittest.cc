176e3f932acb7a4121ad08e420deae7e7678362b
Karol Sygiet <sygiet@google.com>
2024-09-02 12:09:20 +0000

    Remove usage of TestAutofillClock::Advance from form data importer test
    Makes the tests use the task_enviroment to manage the time to align
    autofills code with the rest of the Chromium.
    
    Bug: 40100455
    Change-Id: I2bbcb53abad831e48e6b3bab9cd22f75345aa687
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830487
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1349813}

--- components/autofill/core/browser/form_data_importer_unittest.cc
+++ components/autofill/core/browser/form_data_importer_unittest.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
+#include "base/time/time.h"
 #include "base/uuid.h"
 #include "build/build_config.h"
 #include "components/autofill/core/browser/address_data_manager.h"
@@ -55,7 +56,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/personal_data_manager.h"
 #include "components/autofill/core/browser/strike_databases/payments/iban_save_strike_database.h"
 #include "components/autofill/core/browser/test_autofill_client.h"
-#include "components/autofill/core/browser/test_autofill_clock.h"
 #include "components/autofill/core/browser/webdata/autofill_webdata_service.h"
 #include "components/autofill/core/common/autocomplete_parsing_util.h"
 #include "components/autofill/core/common/autofill_constants.h"
@@ -532,6 +532,9 @@ class FormDataImporterTest : public testing::Test {
          features::kAutofillUseINAddressModel,
          features::kAutofillUseITAddressModel},
         {});
+
+    // Advance the clock to year 20XX.
+    task_environment_.FastForwardBy(base::Days(365) * 31);
   }
 
   void SetUp() override {
@@ -753,7 +756,8 @@ class FormDataImporterTest : public testing::Test {
   }
 
   base::test::SingleThreadTaskEnvironment task_environment_{
-      base::test::SingleThreadTaskEnvironment::MainThreadType::UI};
+      base::test::SingleThreadTaskEnvironment::MainThreadType::UI,
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   test::AutofillUnitTestEnvironment autofill_test_environment_;
   std::unique_ptr<PrefService> prefs_;
   syncer::TestSyncService sync_service_;
@@ -3726,13 +3730,11 @@ TEST_F(FormDataImporterTest, MultiStepImport_DifferentOrigin) {
 
 // Tests that multi-step candidates profiles are invalidated after some TTL.
 TEST_F(FormDataImporterTest, MultiStepImport_TTL) {
-  TestAutofillClock test_clock;
-
   std::unique_ptr<FormStructure> form_structure =
       ConstructSplitDefaultProfileFormStructure(/*part=*/1);
   ExtractAddressProfilesAndVerifyExpectation(*form_structure, {});
 
-  test_clock.Advance(kMultiStepImportTTL + base::Minutes(1));
+  task_environment_.FastForwardBy(kMultiStepImportTTL + base::Minutes(1));
 
   form_structure = ConstructSplitDefaultProfileFormStructure(/*part=*/2);
   ImportAddressProfileAndVerifyImportOfNoProfile(*form_structure);
