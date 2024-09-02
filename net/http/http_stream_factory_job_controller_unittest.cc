d12b8dd4b2ee074089bdbce83ca751c22c92f984
Kenichi Ishibashi <bashi@chromium.org>
2024-09-02 08:10:36 +0000

    Explicitly disable HEv3 for most HttpStreamFactoryJobController tests
    These tests don't make sense for the HappyEyeballsV3 code path because
    these tests examine states of HttpStreamFactory::Jobs, which aren't
    used by the HappyEyeballsV3 code path.
    
    Bug: 346835898
    Change-Id: I2504a79e5400e41c9c0d0435b3f3068801bf6d10
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831542
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349752}

--- net/http/http_stream_factory_job_controller_unittest.cc
+++ net/http/http_stream_factory_job_controller_unittest.cc
@@ -260,15 +260,22 @@ class HttpStreamFactoryJobControllerTestBase : public TestWithTaskEnvironment {
  public:
   explicit HttpStreamFactoryJobControllerTestBase(
       bool dns_https_alpn_enabled,
+      bool happy_eyeballs_v3_enabled,
       std::vector<base::test::FeatureRef> enabled_features = {})
       : TestWithTaskEnvironment(
             base::test::TaskEnvironment::TimeSource::MOCK_TIME),
-        dns_https_alpn_enabled_(dns_https_alpn_enabled) {
+        dns_https_alpn_enabled_(dns_https_alpn_enabled),
+        happy_eyeballs_v3_enabled_(happy_eyeballs_v3_enabled) {
     std::vector<base::test::FeatureRef> disabled_features;
     if (dns_https_alpn_enabled_) {
-      enabled_features.push_back(features::kUseDnsHttpsSvcbAlpn);
+      enabled_features.emplace_back(features::kUseDnsHttpsSvcbAlpn);
     } else {
-      disabled_features.push_back(features::kUseDnsHttpsSvcbAlpn);
+      disabled_features.emplace_back(features::kUseDnsHttpsSvcbAlpn);
+    }
+    if (happy_eyeballs_v3_enabled_) {
+      enabled_features.emplace_back(features::kHappyEyeballsV3);
+    } else {
+      disabled_features.emplace_back(features::kHappyEyeballsV3);
     }
     feature_list_.InitWithFeatures(enabled_features, disabled_features);
     FLAGS_quic_enable_http3_grease_randomness = false;
@@ -421,18 +428,22 @@ class HttpStreamFactoryJobControllerTestBase : public TestWithTaskEnvironment {
 
   void SetAsyncQuicSession(bool async_quic_session) {
     std::vector<base::test::FeatureRef> enabled_features = {};
+    std::vector<base::test::FeatureRef> disabled_features = {};
     if (dns_https_alpn_enabled_) {
-      enabled_features.push_back(features::kUseDnsHttpsSvcbAlpn);
+      enabled_features.emplace_back(features::kUseDnsHttpsSvcbAlpn);
+    }
+    if (happy_eyeballs_v3_enabled_) {
+      enabled_features.emplace_back(features::kHappyEyeballsV3);
+    } else {
+      disabled_features.emplace_back(features::kHappyEyeballsV3);
     }
     if (async_quic_session) {
-      feature_list_.Reset();
-      enabled_features.push_back(features::kAsyncQuicSession);
-      feature_list_.InitWithFeatures(enabled_features, {});
+      enabled_features.emplace_back(features::kAsyncQuicSession);
     } else {
-      feature_list_.Reset();
-      feature_list_.InitWithFeatures(enabled_features,
-                                     {features::kAsyncQuicSession});
+      disabled_features.emplace_back(features::kAsyncQuicSession);
     }
+    feature_list_.Reset();
+    feature_list_.InitWithFeatures(enabled_features, disabled_features);
   }
 
   void TestAltJobSucceedsAfterMainJobFailed(
@@ -509,7 +520,8 @@ class HttpStreamFactoryJobControllerTestBase : public TestWithTaskEnvironment {
   bool should_check_data_consumed_ = true;
 
  private:
-  bool dns_https_alpn_enabled_;
+  const bool dns_https_alpn_enabled_;
+  const bool happy_eyeballs_v3_enabled_;
   bool create_job_controller_ = true;
 
   base::test::ScopedFeatureList feature_list_;
@@ -520,7 +532,9 @@ class HttpStreamFactoryJobControllerTest
       public ::testing::WithParamInterface<bool> {
  protected:
   HttpStreamFactoryJobControllerTest()
-      : HttpStreamFactoryJobControllerTestBase(GetParam()) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/GetParam(),
+            /*happy_eyeballs_v3_enabled=*/false) {}
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
@@ -636,7 +650,9 @@ class JobControllerReconsiderProxyAfterErrorTest
     : public HttpStreamFactoryJobControllerTestBase {
  public:
   JobControllerReconsiderProxyAfterErrorTest()
-      : HttpStreamFactoryJobControllerTestBase(false) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/false,
+            /*happy_eyeballs_v3_enabled=*/false) {}
   void Initialize(
       std::unique_ptr<ProxyResolutionService> proxy_resolution_service,
       std::unique_ptr<ProxyDelegate> proxy_delegate = nullptr,
@@ -4331,7 +4347,9 @@ class JobControllerLimitMultipleH2Requests
     : public HttpStreamFactoryJobControllerTestBase {
  protected:
   JobControllerLimitMultipleH2Requests()
-      : HttpStreamFactoryJobControllerTestBase(false) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/false,
+            /*happy_eyeballs_v3_enabled=*/false) {}
   const int kNumRequests = 5;
   void SetUp() override { SkipCreatingJobController(); }
 };
@@ -4807,7 +4825,9 @@ class HttpStreamFactoryJobControllerMisdirectedRequestRetry
       public ::testing::WithParamInterface<::testing::tuple<bool, bool>> {
  public:
   HttpStreamFactoryJobControllerMisdirectedRequestRetry()
-      : HttpStreamFactoryJobControllerTestBase(false) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/false,
+            /*happy_eyeballs_v3_enabled=*/false) {}
 };
 
 INSTANTIATE_TEST_SUITE_P(All,
@@ -4868,7 +4888,9 @@ class HttpStreamFactoryJobControllerPreconnectTest
       public ::testing::WithParamInterface<bool> {
  protected:
   HttpStreamFactoryJobControllerPreconnectTest()
-      : HttpStreamFactoryJobControllerTestBase(false) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/false,
+            /*happy_eyeballs_v3_enabled=*/false) {}
 
   void SetUp() override {
     if (!GetParam()) {
@@ -5175,8 +5197,10 @@ class HttpStreamFactoryJobControllerDnsHttpsAlpnTest
  protected:
   explicit HttpStreamFactoryJobControllerDnsHttpsAlpnTest(
       std::vector<base::test::FeatureRef> enabled_features = {})
-      : HttpStreamFactoryJobControllerTestBase(true,
-                                               std::move(enabled_features)) {}
+      : HttpStreamFactoryJobControllerTestBase(
+            /*dns_https_alpn_enabled=*/true,
+            /*happy_eyeballs_v3_enabled=*/false,
+            std::move(enabled_features)) {}
 
   void SetUp() override { SkipCreatingJobController(); }
 
@@ -6747,7 +6771,7 @@ class HttpStreamFactoryJobControllerPoolTest
   HttpStreamFactoryJobControllerPoolTest()
       : HttpStreamFactoryJobControllerTestBase(
             /*dns_https_alpn_enabled=*/false,
-            /*enabled_features=*/{features::kHappyEyeballsV3}) {}
+            /*happy_eyeballs_v3_enabled=*/true) {}
 
   ~HttpStreamFactoryJobControllerPoolTest() override = default;
 
