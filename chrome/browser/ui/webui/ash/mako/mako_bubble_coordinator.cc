ed4aa0422ed8920af079425783a221ab2dd29155
Chuong Ho <hdchuong@google.com>
2024-09-02 01:39:17 +0000

    editor:  pass magic-boost flag to webui
    In webui, clicking settings icon will lead to wrong setting toggle when
    magic boost is enabled. This CL passes the magic boost flag as a query
    parameter, then webui will decide which settings link to open.
    
    Bug: b:362651418
    Change-Id: Ia1d6957c16aa570aeba760293a9e6b62ec8769fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830702
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349683}

--- chrome/browser/ui/webui/ash/mako/mako_bubble_coordinator.cc
+++ chrome/browser/ui/webui/ash/mako/mako_bubble_coordinator.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "chrome/grit/orca_resources.h"
 #include "chrome/grit/orca_resources_map.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "content/public/common/url_constants.h"
 #include "net/base/url_util.h"
 #include "ui/base/ime/ash/ime_bridge.h"
@@ -82,6 +83,10 @@ void MakoBubbleCoordinator::LoadEditorUI(
                                            GetSystemLocale());
   url = net::AppendOrReplaceQueryParameter(url, kOrcaFeedbackEnabledParamKey,
                                            feedback_enabled ? "true" : "false");
+  url = net::AppendOrReplaceQueryParameter(
+      url, kOrcaMagicBoostParamKey,
+      chromeos::features::IsMagicBoostEnabled() ? "true" : "false");
+
   if (base::FeatureList::IsEnabled(ash::features::kOrcaResizingSupport)) {
     url = net::AppendOrReplaceQueryParameter(url, kOrcaResizingEnabledParamKey,
                                              "true");
