e031b5d4d1b63731d944ee209e58a5b9eec826a5
Minoru Chikamune <chikamune@chromium.org>
2024-09-02 01:11:27 +0000

    ReportBadMessage on duplicate call of BindNonAssociatedLocalFrameHost
    Bug: 362922564
    Change-Id: I3bf44bbedd9e2f27da8557728e47622e38fea5f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824096
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349676}

--- content/browser/renderer_host/render_frame_host_impl.cc
+++ content/browser/renderer_host/render_frame_host_impl.cc
@@ -12761,6 +12761,9 @@ void RenderFrameHostImpl::BindRenderAccessibilityHost(
 
 void RenderFrameHostImpl::BindNonAssociatedLocalFrameHost(
     mojo::PendingReceiver<blink::mojom::NonAssociatedLocalFrameHost> receiver) {
+  if (non_associated_local_frame_host_receiver_.is_bound()) {
+    mojo::ReportBadMessage("NonAssociatedLocalFrameHost is already bound.");
+  }
   non_associated_local_frame_host_receiver_.Bind(std::move(receiver));
 }
 
