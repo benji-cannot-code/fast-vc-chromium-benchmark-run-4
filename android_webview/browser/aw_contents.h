a01248e744e98271cb706a34c21172e2a3e51fa6
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-09-03 10:22:33 +0000

    Make all AppDefinedDomain read async and cached
    The AppDefinedDomains are read from a system service, which may have a
    significant delay. On the other hand, since these are values from the
    apps manifest, they are not expected to change during the runtime of the
    app.
    
    This CL ensures that the information is always read on a background
    thread, as it was already done by the
    masked_domain_list_component_loader.
    It also caches results in memory to speed up subsequent calls.
    
    It is possible that GetAppDefinedDomains may be called multiple times
    for the same criteria before the first callback returns with a result
    and caches it. In this case the following calls will overwrite the
    cached value. This is not a problem per se, but slightly inefficient,
    and will be fixed in a follow-up CL.
    
    Bug: 353556455
    Change-Id: Ie9c2e0cdc35fbcb7e149cfb9d4158a3dd4eed52e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823049
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Pakkenberg <pbirk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350067}

--- android_webview/browser/aw_contents.h
+++ android_webview/browser/aw_contents.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/jni_weak_ref.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/functional/callback_forward.h"
+#include "base/memory/weak_ptr.h"
 #include "components/content_relationship_verification/digital_asset_links_handler.h"
 #include "components/js_injection/browser/js_communication_host.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -341,6 +342,14 @@ class AwContents : public FindHelper::Listener,
 
   void SetDipScaleInternal(float dip_scale);
 
+  // Callback for RequestStorageAccess to continue once the app_domain_list has
+  // been loaded.
+  void RequestStorageAccessWithAppDomainList(
+      const url::Origin& top_level_origin,
+      base::TimeTicks time_requested,
+      PermissionCallback callback,
+      const std::vector<std::string>& app_domain_list);
+
   JavaObjectWeakGlobalRef java_ref_;
   BrowserViewRenderer browser_view_renderer_;  // Must outlive |web_contents_|.
   std::unique_ptr<content::WebContents> web_contents_;
@@ -369,6 +378,8 @@ class AwContents : public FindHelper::Listener,
   std::list<OriginCallback> pending_geolocation_prompts_;
 
   base::TimeDelta preferred_frame_interval_;
+
+  base::WeakPtrFactory<AwContents> weak_ptr_factory_{this};
 };
 
 }  // namespace android_webview
