a7099a39177b41a04cd2c80952c68b62b46f3630
Federica Germinario <fedegermi@google.com>
2024-09-03 12:22:39 +0000

    [ios] Fix error in ConsecutiveLoadsWithoutLaunch metrics
    Metrics in ConsecutiveLoadsWithoutLaunch are wrongly collected because
    value of NSUserDefaultKey collecting the number of loads was never
    updated.
    
    That code was removed in:
    https://chromium-review.googlesource.com/c/chromium/src/+/5397338
    
    This CL adds the code needed to collect the correct number.
    
    Change-Id: I7831c25994ce137ea75a5678896a2149e98c4bc6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831217
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350101}

--- ios/chrome/app/application_delegate/metrics_mediator.mm
+++ ios/chrome/app/application_delegate/metrics_mediator.mm
@@ -65,7 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 // The key to a NSUserDefaults entry logging the number of times classes are
 // loaded before a scene is attached.
-NSString* const kLoadTimePreferenceKey = @"LoadTimePreferenceKey";
+NSString* const kAppStartupCounterKey = @"LoadTimePreferenceKey";
 
 // The amount of time (in seconds) to wait for the user to start a new task.
 const NSTimeInterval kFirstUserActionTimeout = 30.0;
@@ -212,6 +212,18 @@ std::string WarmStartHistogramPrefix(bool version_mismatch) {
                           : kHistogramPrefix;
 }
 }  // namespace
+
+// A class to log the "load" count in uma.
+@interface AppStartupCounter : NSObject
+@end
+
+@implementation AppStartupCounter
++ (void)load {
+  NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
+  [defaults setInteger:[defaults integerForKey:kAppStartupCounterKey] + 1
+                forKey:kAppStartupCounterKey];
+}
+@end
 
 namespace metrics_mediator {
 NSString* const kAppEnteredBackgroundDateKey = @"kAppEnteredBackgroundDate";
@@ -374,8 +386,8 @@ BOOL _credentialExtensionWasUsed = NO;
       now - [startupInformation firstSceneConnectionTime];
 
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
-  int consecutiveLoads = [defaults integerForKey:kLoadTimePreferenceKey];
-  [defaults removeObjectForKey:kLoadTimePreferenceKey];
+  int consecutiveLoads = [defaults integerForKey:kAppStartupCounterKey];
+  [defaults removeObjectForKey:kAppStartupCounterKey];
   int consecutiveDidFinishLaunching =
       [defaults integerForKey:kAppDidFinishLaunchingConsecutiveCallsKey];
   [defaults removeObjectForKey:kAppDidFinishLaunchingConsecutiveCallsKey];
