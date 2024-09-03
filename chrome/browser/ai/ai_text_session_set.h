92bb367772049078bbef66dd67b3bb24107e0a5d
Ivan Murashov <ivan.murashov@lge.com>
2024-09-03 15:48:38 +0000

    IWYU: Add missing include for std::variant usage in ai_text_session_set.h
    In the CL https://crrev.com/c/5775466 used std::variant in the
    chrome/browser/ai/ai_text_session_set.h, but corresponding include
    was missed. Added missing include for std::variant usage.
    Otherwise the build with libstdc++ fails with error:
    ../../chrome/browser/ai/ai_text_session_set.h:25:12:
    error: ‘variant’ in namespace ‘std’ does not name a template type
    
    Bug: 41455655
    Change-Id: Ic512c4332bc85abcad210b750018f5ce188837e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831375
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1350192}

--- chrome/browser/ai/ai_text_session_set.h
+++ chrome/browser/ai/ai_text_session_set.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_AI_AI_TEXT_SESSION_SET_H_
 #define CHROME_BROWSER_AI_AI_TEXT_SESSION_SET_H_
 
+#include <variant>
+
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ai/ai_text_session.h"
