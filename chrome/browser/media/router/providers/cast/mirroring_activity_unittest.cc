2bf9cefe86c05e88c06ca678a80c2e3f2bad5629
Benjamin Zielinski <bzielinski@google.com>
2024-09-03 22:22:24 +0000

    cast mirroring low latency experiment cleanup
    Remove the kCastMirroringPlayoutDelay finch feature which was enabled
    by default in 128 with a default value of 200ms. Also, cleanup
    mirroring_settings.h where the default playout delay is currently
    defined separately depending on platform, since the latest experiment
    confirmed that 200ms is a good value for all desktop platforms.
    This change also updates the chrome://flags entry which currently shows
    that 400 ms is the default.
    
    Bug: b/327950363
    Change-Id: I18e2216e3431038132f894878a2f9776bb899582
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829628
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Benjamin Zielinski <bzielinski@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350438}

--- chrome/browser/media/router/providers/cast/mirroring_activity_unittest.cc
+++ chrome/browser/media/router/providers/cast/mirroring_activity_unittest.cc
@@ -786,11 +786,9 @@ TEST_F(MirroringActivityTest, MultipleMediaControllersNotified) {
 }
 
 TEST_F(MirroringActivityTest, TargetPlayoutDelaySetInRequest) {
-  base::test::ScopedFeatureList feature_list;
-  base::FieldTrialParams feature_params;
-  feature_params[media_router::kCastMirroringPlayoutDelayMs.name] = "300";
-  feature_list.InitAndEnableFeatureWithParameters(
-      media_router::kCastMirroringPlayoutDelay, feature_params);
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  command_line->AppendSwitchASCII(switches::kCastMirroringTargetPlayoutDelay,
+                                  "300");
 
   static constexpr char kUrl[] =
       "cast:0F5096E8?streamingCaptureAudio=1&streamingTargetPlayoutDelayMillis="
@@ -807,11 +805,9 @@ TEST_F(MirroringActivityTest, TargetPlayoutDelaySetInRequest) {
 }
 
 TEST_F(MirroringActivityTest, TargetPlayoutDelayFeatureFlagParam) {
-  base::test::ScopedFeatureList feature_list;
-  base::FieldTrialParams feature_params;
-  feature_params[media_router::kCastMirroringPlayoutDelayMs.name] = "300";
-  feature_list.InitAndEnableFeatureWithParameters(
-      media_router::kCastMirroringPlayoutDelay, feature_params);
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  command_line->AppendSwitchASCII(switches::kCastMirroringTargetPlayoutDelay,
+                                  "300");
 
   static constexpr char kUrl[] = "cast:0F5096E8?streamingCaptureAudio=1";
   GURL url(kUrl);
@@ -825,13 +821,11 @@ TEST_F(MirroringActivityTest, TargetPlayoutDelayFeatureFlagParam) {
 }
 
 TEST_F(MirroringActivityTest, CastStreamingSenderUma) {
-  base::HistogramTester uma_recorder;
-  base::test::ScopedFeatureList feature_list;
-  base::FieldTrialParams feature_params;
-  feature_params[media_router::kCastMirroringPlayoutDelayMs.name] = "200";
-  feature_list.InitAndEnableFeatureWithParameters(
-      media_router::kCastMirroringPlayoutDelay, feature_params);
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+  command_line->AppendSwitchASCII(switches::kCastMirroringTargetPlayoutDelay,
+                                  "200");
 
+  base::HistogramTester uma_recorder;
   static constexpr char kJsonStats[] = R"({
     "audio": {
       "TRANSMISSION_KBPS": 20.0,
