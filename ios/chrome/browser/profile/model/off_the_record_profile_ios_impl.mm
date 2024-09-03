ac271e62dc97b0fa1948c276590e21aa2e729088
Aliona DANGLA <alionadangla@google.com>
2024-09-03 09:49:04 +0000

    [iOS] Move off_the_record_chrome_browser_state_impl.{h, mm} files
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR
    Fixed: 358069459
    Change-Id: I023e6fa59219dd2a95434c1320768273ff80042a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827538
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350055}

--- ios/chrome/browser/profile/model/off_the_record_profile_ios_impl.mm
+++ ios/chrome/browser/profile/model/off_the_record_profile_ios_impl.mm
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "ios/chrome/browser/profile/model/off_the_record_profile_ios_impl.h"
+
 #import "base/metrics/histogram_functions.h"
 #import "base/metrics/user_metrics.h"
 #import "base/notreached.h"
@@ -15,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/user_prefs/user_prefs.h"
 #import "ios/chrome/browser/prefs/model/ios_chrome_pref_service_factory.h"
 #import "ios/chrome/browser/profile/model/ios_chrome_url_request_context_getter.h"
-#import "ios/chrome/browser/profile/model/off_the_record_profile_ios_impl.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 
 OffTheRecordChromeBrowserStateImpl::OffTheRecordChromeBrowserStateImpl(
