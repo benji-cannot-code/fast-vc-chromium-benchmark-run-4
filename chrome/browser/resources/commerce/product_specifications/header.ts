a93256abca334a827d1a6beb662a49e0a322d6d6
Chase Hartsell <chazzy@chromium.org>
2024-09-03 21:53:25 +0000

    Make product specifications title tabbable and editable
    This patch updates the product specifications table title to be in the
    tab order and editable when "Enter" is pressed during keyboard
    navigation when in focus.
    
    http://go/scrcast/NjI3NzEzOTg3ODkwMzgwOHxmMmVhYzc0Ni00Yg
    
    Bug: 360429625
    Change-Id: I23e356e84775a61217ac28bcf5d55fa4f0a6af3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830102
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350424}

--- chrome/browser/resources/commerce/product_specifications/header.ts
+++ chrome/browser/resources/commerce/product_specifications/header.ts
@@ -19,6 +19,7 @@ import type {HeaderMenuElement} from './header_menu.js';
 
 export interface HeaderElement {
   $: {
+    divider: HTMLElement,
     menuButton: CrIconButtonElement,
     menu: HeaderMenuElement,
   };
@@ -106,6 +107,13 @@ export class HeaderElement extends PolymerElement {
         name: inputValue,
       },
     }));
+  }
+
+  private onSubtitleKeyDown_(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      event.stopPropagation();
+      this.onRenaming_();
+    }
   }
 }
 
