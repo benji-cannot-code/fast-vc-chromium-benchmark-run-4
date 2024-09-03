1826797fd9dc4675af789fddb17ca73a86eeca82
Dmitry Vykochko <vykochko@google.com>
2024-09-03 20:00:24 +0000

    Add a new settings page for improved autofill predictions.
    An empty page and the router set up were added in this CL,
    the functionality and page content in particular are
    coming in subsequent CLs.
    
    Implementation:
    https://screenshot.googleplex.com/AqM93cwuCuV5yPW
    https://screenshot.googleplex.com/Qzx5Y23xdK7eDzB
    
    Bug: 361437117
    Change-Id: If6dda98e712a450df744dd91cebd76b97e0aff5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5816513
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350363}

---
+++ chrome/browser/resources/settings/autofill_page/autofill_prediction_improvements_section.ts
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview 'settings-autofill-prediction-improvements-section' is
+ * the section containing configuration options for prediction improvements.
+ */
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './autofill_prediction_improvements_section.html.js';
+
+
+export class SettingsAutofillPredictionImprovementsSectionElement extends
+    PolymerElement {
+  static get is() {
+    return 'settings-autofill-prediction-improvements-section';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'settings-autofill-prediction-improvements-section':
+        SettingsAutofillPredictionImprovementsSectionElement;
+  }
+}
+
+customElements.define(
+    SettingsAutofillPredictionImprovementsSectionElement.is,
+    SettingsAutofillPredictionImprovementsSectionElement);
--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_constants.h"
 #include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_payments_features.h"
+#include "components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_features.h"
 #include "components/content_settings/core/common/features.h"
 #include "components/device_reauth/device_authenticator.h"
 #include "components/dom_distiller/core/dom_distiller_features.h"
@@ -1262,7 +1263,8 @@ void AddAutofillStrings(content::WebUIDataSource* html_source,
        IDS_SETTINGS_EXPERIMENTAL_ADVANCED_FEATURE3_LABEL},
       {"experimentalAdvancedFeature3Sublabel",
        IDS_SETTINGS_EXPERIMENTAL_ADVANCED_FEATURE3_SUBLABEL},
-  };
+      {"autofillPredictionImprovementsPageTitle",
+       IDS_SETTINGS_AUTOFILL_PREDICTION_IMPROVEMENTS_PAGE_TITLE}};
 
   GURL google_password_manager_url = GetGooglePasswordManagerURL(
       password_manager::ManagePasswordsReferrer::kChromeSettings);
@@ -1363,6 +1365,11 @@ void AddAutofillStrings(content::WebUIDataSource* html_source,
       "requireValidLocalCards",
       base::FeatureList::IsEnabled(
           autofill::features::kAutofillRequireValidLocalCardsInSettings));
+
+  html_source->AddBoolean(
+      "autofillPredictionImprovementsEnabled",
+      base::FeatureList::IsEnabled(
+          autofill_prediction_improvements::kAutofillPredictionImprovements));
 }
 
 void AddSignOutDialogStrings(content::WebUIDataSource* html_source,
