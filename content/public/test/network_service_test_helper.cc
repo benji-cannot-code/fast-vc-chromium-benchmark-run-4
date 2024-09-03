2a3ea6219253429384ed05329517b79dc54354d7
Rushan Suleymanov <rushans@google.com>
2024-09-03 11:18:02 +0000

    Use WeakPtr in NetworkServiceTestImpl for callbacks
    Fixed: 363071121
    Change-Id: I5368813a9195f2da24e96606189a408cc3c51286
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830792
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350084}

--- content/public/test/network_service_test_helper.cc
+++ content/public/test/network_service_test_helper.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/test/network_service_test_helper.h"
 
+#include <optional>
 #include <utility>
 #include <vector>
 
@@ -426,14 +427,13 @@ class NetworkServiceTestHelper::NetworkServiceTestImpl
     : public network::mojom::NetworkServiceTest,
       public base::CurrentThread::DestructionObserver {
  public:
-  NetworkServiceTestImpl()
-      : test_host_resolver_(new TestHostResolver()),
-        memory_pressure_listener_(
-            FROM_HERE,
-            base::DoNothing(),
-            base::BindRepeating(&NetworkServiceTestHelper::
-                                    NetworkServiceTestImpl::OnMemoryPressure,
-                                base::Unretained(this))) {
+  NetworkServiceTestImpl() : test_host_resolver_(new TestHostResolver()) {
+    memory_pressure_listener_.emplace(
+        FROM_HERE, base::DoNothing(),
+        base::BindRepeating(
+            &NetworkServiceTestHelper::NetworkServiceTestImpl::OnMemoryPressure,
+            weak_factory_.GetWeakPtr()));
+
     if (base::CommandLine::ForCurrentProcess()->HasSwitch(
             switches::kUseMockCertVerifierForTesting)) {
       mock_cert_verifier_ = std::make_unique<net::MockCertVerifier>();
@@ -818,7 +818,7 @@ class NetworkServiceTestHelper::NetworkServiceTestImpl
   std::unique_ptr<net::MockCertVerifier> mock_cert_verifier_;
   std::unique_ptr<net::ScopedTransportSecurityStateSource>
       transport_security_state_source_;
-  base::MemoryPressureListener memory_pressure_listener_;
+  std::optional<base::MemoryPressureListener> memory_pressure_listener_;
   base::MemoryPressureListener::MemoryPressureLevel
       latest_memory_pressure_level_ =
           base::MemoryPressureListener::MEMORY_PRESSURE_LEVEL_NONE;
