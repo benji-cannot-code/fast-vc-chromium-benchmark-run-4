8d531f3157e90fe25309ba02110ab6ca0611489e
Morten Stenshorne <mstensho@chromium.org>
2024-09-03 12:32:29 +0000

    Implement background-origin for @page backgrounds.
    Since the @page background is painted at the page container fragment,
    whereas borders and padding are part of the page border box child
    fragment, some extra work is required.
    
    Paginated fragment structure documented here:
    https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/layout/paginated_root_layout_algorithm.h;l=24-61;drc=01ab59ae08a38a361da7dac41e36f387f6600ed5
    
    Bug: 40286153
    Change-Id: I778c40a0c6d3cecd48c7f5d32a4f9071a5e618bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828871
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1350103}

--- third_party/blink/renderer/core/paint/box_background_paint_context.h
+++ third_party/blink/renderer/core/paint/box_background_paint_context.h
@@ -38,6 +38,7 @@ class BoxBackgroundPaintContext {
   // Constructor for LayoutView where the coordinate space is different.
   BoxBackgroundPaintContext(
       const LayoutView&,
+      const PhysicalBoxFragment*,
       const PhysicalOffset& element_positioning_area_offset);
 
   // Generic constructor for all other elements.
--- third_party/blink/renderer/core/paint/view_painter.cc
+++ third_party/blink/renderer/core/paint/view_painter.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/frame/settings.h"
 #include "third_party/blink/renderer/core/layout/layout_box.h"
 #include "third_party/blink/renderer/core/layout/layout_view.h"
+#include "third_party/blink/renderer/core/layout/pagination_utils.h"
 #include "third_party/blink/renderer/core/layout/physical_box_fragment.h"
 #include "third_party/blink/renderer/core/paint/box_background_paint_context.h"
 #include "third_party/blink/renderer/core/paint/box_decoration_data.h"
@@ -343,6 +344,13 @@ void ViewPainter::PaintRootElementGroup(
     } else {
       background_image_offset = -root_object->FirstFragment().PaintOffset();
     }
+
+    if (box_fragment_.GetBoxType() == PhysicalFragment::kPageContainer) {
+      // Background image origin is at the border box edge. A page container
+      // fragment covers the entire page box. Add the offset to the page border
+      // box fragment, to get past the margins.
+      background_image_offset -= GetPageBorderBoxLink(box_fragment_).offset;
+    }
   }
 
   bool should_clear_canvas =
@@ -423,7 +431,7 @@ void ViewPainter::PaintRootElementGroup(
     context.FillRect(paint_rect, Color(), auto_dark_mode, SkBlendMode::kClear);
   }
 
-  BoxBackgroundPaintContext bg_paint_context(layout_view,
+  BoxBackgroundPaintContext bg_paint_context(layout_view, &box_fragment_,
                                              background_image_offset);
   BoxModelObjectPainter box_model_painter(layout_view);
   for (const auto* fill_layer : base::Reversed(reversed_paint_list)) {
