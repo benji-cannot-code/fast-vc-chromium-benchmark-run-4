336d2ea7f1a94e6502f2e73090931dc8927389ec
Eriko Kurimoto <elkurin@chromium.org>
2024-09-03 01:20:44 +0000

    ash: Move //chrome/browser/ui/webui/ash/image_source* to //c/b/ash
    Bug: b:362677693
    Change-Id: I0d065788a772d7e33e5e9d1a786cd4a3525e1906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820237
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349959}

--- chrome/browser/ash/image_source/image_source.cc
+++ chrome/browser/ash/image_source/image_source.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma allow_unsafe_buffers
 #endif
 
-#include "chrome/browser/ui/webui/ash/image_source.h"
+#include "chrome/browser/ash/image_source/image_source.h"
 
 #include <stddef.h>
 
