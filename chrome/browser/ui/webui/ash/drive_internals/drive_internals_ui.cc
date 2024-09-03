89b17339c6b50c8dcec563e35cc07bb4cfe2aaf2
Eriko Kurimoto <elkurin@chromium.org>
2024-09-03 01:22:17 +0000

    ash: Create //chrome/browser/ui/webui/ash/drive_internals
    Bug: b:362868931
    Change-Id: I106e69433b82403e1e9252e2fa916e1dac126936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824029
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1349961}

--- chrome/browser/ui/webui/ash/drive_internals/drive_internals_ui.cc
+++ chrome/browser/ui/webui/ash/drive_internals/drive_internals_ui.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma allow_unsafe_buffers
 #endif
 
-#include "chrome/browser/ui/webui/ash/drive_internals_ui.h"
+#include "chrome/browser/ui/webui/ash/drive_internals/drive_internals_ui.h"
 
 #include <stddef.h>
 #include <stdint.h>
--- chrome/browser/ui/webui/ash/drive_internals/drive_internals_ui.h
+++ chrome/browser/ui/webui/ash/drive_internals/drive_internals_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_DRIVE_INTERNALS_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_DRIVE_INTERNALS_UI_H_
 
 #include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -36,4 +36,4 @@ class DriveInternalsUI : public content::WebUIController {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_DRIVE_INTERNALS_DRIVE_INTERNALS_UI_H_
