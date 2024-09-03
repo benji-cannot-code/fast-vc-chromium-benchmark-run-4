fe05146721850fe7c32a236c8b3b5f8f9339aef9
Aliona DANGLA <alionadangla@google.com>
2024-09-03 11:08:52 +0000

    [iOS] Remove test_chrome_browser_state.h
    This header been migrated, see http://go/crbm/359177266 for more.
    This CL replace the last occurrences and remove the header.
    
    Bug: 358267438
    Change-Id: I69abb5afdc2fe2a1d2bb1a2a6da56c43098b1aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5822602
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350080}

--- ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate_unittest.mm
+++ ios/chrome/browser/tabs/model/ios_chrome_synced_tab_delegate_unittest.mm
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ntp/ui_bundled/new_tab_page_feature.h"
 #import "ios/chrome/browser/sessions/model/ios_chrome_session_tab_helper.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
-#import "ios/chrome/browser/shared/model/browser_state/test_chrome_browser_state.h"
+#import "ios/chrome/browser/shared/model/profile/test/test_profile_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/fake_authentication_service_delegate.h"
