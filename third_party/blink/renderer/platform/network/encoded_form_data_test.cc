540c4a14acc2408691ea75583545c85a8b085c90
Yoichi Osato <yoichio@chromium.org>
2024-09-03 06:37:52 +0000

    Check EncodedFormData type early in FormDataBytesConsumer.
    FormDataBytesConsumer assumes EncodedFormData has typical data set and
    EncodedFormData looks always fulfill the condition.
    This CL introduces EncodedFormData.GetType() to compute the type and
    check it at FormDataBytesConsumer and mojo conversion earlier than
    existing CHECK().
    
    Bug: 40921917
    Change-Id: I52c59cd5b993a82a92b2cae55427425788cd2ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820592
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350001}

--- third_party/blink/renderer/platform/network/encoded_form_data_test.cc
+++ third_party/blink/renderer/platform/network/encoded_form_data_test.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "third_party/blink/renderer/platform/network/encoded_form_data.h"
+
 #include <utility>
 
 #include "base/task/sequenced_task_runner.h"
@@ -16,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/blob/blob_registry.mojom-blink.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/blob/blob_data.h"
-#include "third_party/blink/renderer/platform/network/encoded_form_data.h"
+#include "third_party/blink/renderer/platform/network/wrapped_data_pipe_getter.h"
 
 namespace blink {
 
@@ -101,6 +103,36 @@ TEST_F(EncodedFormDataTest, DeepCopy) {
   // ThreadSafeRefCounted.
   // filename_ and blob_uuid_ are now thread safe, so they don't need a
   // deep copy.
+}
+
+TEST_F(EncodedFormDataTest, GetType) {
+  scoped_refptr<EncodedFormData> form_data(EncodedFormData::Create());
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataOnly, form_data->GetType());
+
+  form_data->AppendData("Foo", 3);
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataOnly, form_data->GetType());
+
+  form_data->AppendFile("Bar.txt", base::Time());
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataAndEncodedFileOrBlob,
+            form_data->GetType());
+
+  form_data->AppendDataPipe(nullptr);
+  EXPECT_EQ(EncodedFormData::FormDataType::kInvalid, form_data->GetType());
+}
+
+TEST_F(EncodedFormDataTest, GetType2) {
+  scoped_refptr<EncodedFormData> form_data(EncodedFormData::Create());
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataOnly, form_data->GetType());
+
+  form_data->AppendData("Foo", 3);
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataOnly, form_data->GetType());
+
+  form_data->AppendDataPipe(nullptr);
+  EXPECT_EQ(EncodedFormData::FormDataType::kDataAndDataPipe,
+            form_data->GetType());
+
+  form_data->AppendFile("Bar.txt", base::Time());
+  EXPECT_EQ(EncodedFormData::FormDataType::kInvalid, form_data->GetType());
 }
 
 }  // namespace
