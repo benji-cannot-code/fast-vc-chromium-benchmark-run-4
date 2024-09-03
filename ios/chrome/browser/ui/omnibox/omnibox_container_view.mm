6c4d16e148e05cc2190f5d03194979e3c8ed20c9
Christian Xu <christianxu@google.com>
2024-09-03 10:26:35 +0000

    [iOS][LO] Resize thumbnail image
    Resize the thumbnail image to AspectFill.
    
    Bug: None
    Change-Id: Id474fda862a6f710f1815ab6485ce70e6c608c50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829045
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350068}

--- ios/chrome/browser/ui/omnibox/omnibox_container_view.mm
+++ ios/chrome/browser/ui/omnibox/omnibox_container_view.mm
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/material_timing.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
+#import "ios/chrome/common/ui/util/image_util.h"
 #import "ios/chrome/common/ui/util/pointer_interaction_util.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/grit/ios_theme_resources.h"
@@ -279,6 +280,10 @@ const CGFloat kClearButtonSize = 28.5f;
 }
 
 - (void)setThumbnailImage:(UIImage*)image {
+  if (image) {
+    image = ResizeImage(image, CGSizeMake(kThumbnailWidth, kThumbnailHeight),
+                        ProjectionMode::kAspectFill);
+  }
   _thumbnailImageView.image = image;
   _thumbnailImageView.hidden = !image;
 
