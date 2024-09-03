78e80d90c127e48d17cbea448e2dbbbee4d7e664
Ernesto Izquierdo Clua <eic@google.com>
2024-09-03 19:44:39 +0000

    [ios] Clean up stale entries in ChildFrameRegistrar
    Deletes all entries in the registrar pointing to deleted frames.
    
    Fixed: 359535071
    Change-Id: I775aead2d0e282e06409821485aaf63976856e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823838
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350354}

--- components/autofill/ios/form_util/child_frame_registrar.h
+++ components/autofill/ios/form_util/child_frame_registrar.h
@@ -10,8 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <optional>
 
 #import "base/observer_list.h"
+#import "base/scoped_multi_source_observation.h"
+#import "base/scoped_observation.h"
 #import "components/autofill/core/common/unique_ids.h"
 #import "ios/web/public/js_messaging/java_script_feature.h"
+#import "ios/web/public/js_messaging/web_frames_manager.h"
+#import "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
 namespace autofill {
@@ -29,7 +33,9 @@ class ChildFrameRegistrarObserver : public base::CheckedObserver {
 // remote frame token) to a child frame, establishing a relationship between
 // that frame in the DOM (and JS) and the corresponding WebFrame object in C++.
 // This class maintains those mappings.
-class ChildFrameRegistrar : public web::WebStateUserData<ChildFrameRegistrar> {
+class ChildFrameRegistrar : public web::WebStateUserData<ChildFrameRegistrar>,
+                            public web::WebFramesManager::Observer,
+                            public web::WebStateObserver {
  public:
   ~ChildFrameRegistrar() override;
 
@@ -78,10 +84,21 @@ class ChildFrameRegistrar : public web::WebStateUserData<ChildFrameRegistrar> {
   // Removes |observer| from the list of observers.
   void RemoveObserver(ChildFrameRegistrarObserver* observer);
 
+  // web::WebFramesManager::Observer:
+  void WebFrameBecameUnavailable(web::WebFramesManager* web_frames_manager,
+                                 const std::string& frame_id) override;
+
+  // web::WebStateObserver
+  void WebStateDestroyed(web::WebState* web_state) override;
+
  private:
   explicit ChildFrameRegistrar(web::WebState* web_state);
   friend class web::WebStateUserData<ChildFrameRegistrar>;
   WEB_STATE_USER_DATA_KEY_DECL();
+
+  // Deletes all entries in `lookup_map_` that contain `frame_id` as local frame
+  // token.
+  void RemoveFrameID(const std::string& frame_id);
 
   // Maintains the mapping used by `LookupChildFrame`. The value containing the
   // local frame token is set to std::nullopt if there was at least one attempt
@@ -96,6 +113,16 @@ class ChildFrameRegistrar : public web::WebStateUserData<ChildFrameRegistrar> {
       pending_callbacks_;
 
   base::ObserverList<ChildFrameRegistrarObserver> observers_;
+
+  // Observation for listening to WebFrame events. Multiple observations are
+  // required because there are two WebFramesManager per WebState, one for each
+  // ContentWorld.
+  base::ScopedMultiSourceObservation<web::WebFramesManager,
+                                     web::WebFramesManager::Observer>
+      web_frames_managers_observation_{this};
+
+  base::ScopedObservation<web::WebState, web::WebStateObserver>
+      web_state_observation_{this};
 };
 
 }  // namespace autofill
