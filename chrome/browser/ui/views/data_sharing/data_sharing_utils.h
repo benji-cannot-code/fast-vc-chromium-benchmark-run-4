3f0953503f2e3a44c4570fb803fb872fb25b60d9
Jose Dapena Paz <jdapena@igalia.com>
2024-09-03 17:53:05 +0000

    IWYU: missing include for usage of std::variant in data_sharing_utils.h
    Bug: 41455655
    
    Change-Id: Ibd60ef7b361ccb1126ddda8f5f08145b70372b68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833391
    Reviewed-by: pengchao Cai <pengchaocai@chromium.org>
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1350262}

--- chrome/browser/ui/views/data_sharing/data_sharing_utils.h
+++ chrome/browser/ui/views/data_sharing/data_sharing_utils.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_DATA_SHARING_DATA_SHARING_UTILS_H_
 #define CHROME_BROWSER_UI_VIEWS_DATA_SHARING_DATA_SHARING_UTILS_H_
 
+#include <variant>
+
 #include "base/uuid.h"
 #include "chrome/browser/profiles/profile.h"
 #include "components/data_sharing/public/group_data.h"
