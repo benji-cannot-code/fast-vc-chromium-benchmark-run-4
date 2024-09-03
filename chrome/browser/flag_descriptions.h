0029c959a8d2a6b677d5d5f4ba4eec025376954e
David Benjamin <davidben@chromium.org>
2024-09-03 19:37:52 +0000

    Add a flag to use ML-KEM instead of Kyber
    The flag is disabled by default for now. We'll enable it by default
    after server early adopters have had time to deploy the new codepoint.
    
    For the next KEM transition, we'll need
    draft-ietf-tls-key-share-prediction to make this transition smoother.
    But as Kyber was an experimental codepoint doomed to die, it is fine for
    this transition.
    
    ML-KEM and Kyber are very similar, so the compatibility and
    performance risks between the two are expected to be identical. Thus,
    to minimize interruption to enterprise administrators, ML-KEM is still
    gated on the same flags as Kyber was. The new flag changes the meaning
    of the old flags to gate Kyber instead.
    
    In particular, the same admin policy, PostQuantumKeyAgreementEnabled,
    gates both. I've updated the description to not specifically reference
    Kyber. Also, as we now have the final standard, I've sent the removal
    date for the policy at roughly a year from now. We can adjust it if
    needed, but having an initial estimate helps give more deadline-driven
    administrators and middleware developers something to target. (This
    also matches our standard practice of setting target removal dates
    when we know from day 1 that a policy is temporary.)
    
    Bug: 40910498
    Change-Id: I16ccf39f950c6fa2ed7375e6749c6b4d338a922a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823718
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Julian Pastarmov <pastarmovj@chromium.org>
    Reviewed-by: David Schinazi <dschinazi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350346}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -701,6 +701,9 @@ extern const char kEnableTLS13EarlyDataDescription[];
 extern const char kEnableTLS13KyberName[];
 extern const char kEnableTLS13KyberDescription[];
 
+extern const char kUseMLKEMName[];
+extern const char kUseMLKEMDescription[];
+
 extern const char kAccessibilityAcceleratorName[];
 extern const char kAccessibilityAcceleratorDescription[];
 
