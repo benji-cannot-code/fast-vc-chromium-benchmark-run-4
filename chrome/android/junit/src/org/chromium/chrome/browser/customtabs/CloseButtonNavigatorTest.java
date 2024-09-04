8449ffacca77fd31f3167b69b3f7639d7c07b517
Wenyu Fu <wenyufu@chromium.org>
2024-09-04 19:48:49 +0000

    Remove CustomTabs.TabCounts histograms
    OBSOLETE_HISTOGRAM[CustomTabs.TabCounts.OnClosingAllTabs]=Not needed
    OBSOLETE_HISTOGRAM[CustomTabs.TabCounts.UniqueTabsSeen]=Not needed
    
    Fixed: 357607198
    Change-Id: I6d6855488092edad8167ea146f576c5627067cc8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834644
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351001}

--- chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CloseButtonNavigatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/customtabs/CloseButtonNavigatorTest.java
@@ -29,7 +29,6 @@ import org.robolectric.ParameterizedRobolectricTestRunner.Parameters;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.Callback;
-import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.BaseRobolectricTestRule;
 import org.chromium.base.test.util.Batch;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
@@ -178,7 +177,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertTrue(mTabs.empty());
-        assertOnAllTabsClosedRecorded(1);
     }
 
     @Test
@@ -194,7 +192,6 @@ public class CloseButtonNavigatorTest {
             verify(currentTabsNavigationController(), never()).goToNavigationIndex(anyInt());
         } else {
             assertTrue(mTabs.empty());
-            assertOnAllTabsClosedRecorded(2);
         }
     }
 
@@ -206,7 +203,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertTrue(mTabs.empty());
-        assertOnAllTabsClosedRecorded(1);
     }
 
     @Test
@@ -223,7 +219,6 @@ public class CloseButtonNavigatorTest {
             verify(currentTabsNavigationController(), never()).goToNavigationIndex(anyInt());
         } else {
             assertTrue(mTabs.empty());
-            assertOnAllTabsClosedRecorded(2);
         }
     }
 
@@ -240,7 +235,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertFalse(mTabs.isEmpty());
-        assertOnAllTabsClosedRecorded(0);
         verify(currentTabsNavigationController()).goToNavigationIndex(eq(1));
         // Ensure it was only called with that value.
         verify(currentTabsNavigationController()).goToNavigationIndex(anyInt());
@@ -256,7 +250,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertEquals(1, mTabs.size());
-        assertOnAllTabsClosedRecorded(0);
         verify(currentTabsNavigationController(), never()).goToNavigationIndex(anyInt());
     }
 
@@ -270,7 +263,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertEquals(1, mTabs.size());
-        assertOnAllTabsClosedRecorded(0);
         if (mIsWebapp) {
             verify(currentTabsNavigationController(), never()).goToNavigationIndex(anyInt());
         } else {
@@ -299,7 +291,6 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertEquals(1, mTabs.size());
-        assertOnAllTabsClosedRecorded(0);
         verify(currentTabsNavigationController()).goToNavigationIndex(eq(1));
         verify(currentTabsNavigationController()).goToNavigationIndex(anyInt());
     }
@@ -318,23 +309,7 @@ public class CloseButtonNavigatorTest {
         mCloseButtonNavigator.navigateOnClose(mFinishCallback);
 
         assertEquals(1, mTabs.size());
-        assertOnAllTabsClosedRecorded(0);
         verify(currentTabsNavigationController()).goToNavigationIndex(eq(1));
         verify(currentTabsNavigationController()).goToNavigationIndex(anyInt());
-    }
-
-    private void assertOnAllTabsClosedRecorded(int count) {
-        String histogram = "CustomTabs.TabCounts.OnClosingAllTabs";
-        if (count > 0) {
-            assertEquals(
-                    String.format("<%s> not recorded with sample <%d>.", histogram, count),
-                    1,
-                    RecordHistogram.getHistogramValueCountForTesting(histogram, count));
-        } else {
-            assertEquals(
-                    String.format("<%s> should not be recorded.", histogram),
-                    0,
-                    RecordHistogram.getHistogramTotalCountForTesting(histogram));
-        }
     }
 }
