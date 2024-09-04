b683e565f5ee2352a3cb7cb9dc873dd9e655aa2d
Jan Lanik <janlanik@google.com>
2024-09-04 13:50:33 +0000

    Update strings for PrivacyHub 2.3
    Updated the strings according to the UX spec.
    
    Bug: b/364010560
    Change-Id: I10b7a493b1d4c1aede1b657a54cf99e9b03ecd92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832483
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Jan Láník <janlanik@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350761}

--- chrome/test/data/webui/settings/site_details_permission_test.ts
+++ chrome/test/data/webui/settings/site_details_permission_test.ts
@@ -355,8 +355,9 @@ suite('SiteDetailsPermission', function() {
           sensor = 'location';
         }
 
-        const variant =
-            warningElement.innerHTML.includes('Chrome') ? 'Chrome' : 'Chromium';
+        const variant = warningElement.innerHTML.includes('Chromium') ?
+            'Chromium' :
+            'Chrome';
 
         assertEquals(
             `To use your ${sensor}, give ${variant} access in ` +
