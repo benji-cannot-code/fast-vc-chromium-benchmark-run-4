b9181d544240657f779f872d269c5479771eb163
Marc Treib <treib@chromium.org>
2024-09-04 16:00:34 +0000

    Sync: sessions_helper.h/cc cleanup
    Turns out sessions_helper.h/cc contained a bunch of helpers that
    weren't actually used, and some that aren't used outside of other
    helpers (and are unlikely to be generally useful).
    This CL removes and makes private, respectively, those helpers.
    
    Bug: none
    Change-Id: I26d748facf96c02d26554501ed8eed7e3046ffc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836959
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Marc Treib <treib@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350834}

--- chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
@@ -62,7 +62,6 @@ using fake_server::SessionsHierarchy;
 using history_helper::GetUrlFromClient;
 using sessions_helper::CheckInitialState;
 using sessions_helper::CloseTab;
-using sessions_helper::ExecJs;
 using sessions_helper::GetLocalWindows;
 using sessions_helper::GetSessionData;
 using sessions_helper::MoveTab;
