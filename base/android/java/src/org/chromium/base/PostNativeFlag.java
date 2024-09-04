7daf69faaee36ba51ae3ea6c6d51466c6047be6a
Henrique Nakashima <hnakashima@chromium.org>
2024-09-04 17:26:28 +0000

    [Android] Move PostNativeFlag out of .cached_flags
    Bug: 327638236
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Change-Id: I0c274de657e104b72da484c017cce37d983d07cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833814
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350897}

--- base/android/java/src/org/chromium/base/PostNativeFlag.java
+++ base/android/java/src/org/chromium/base/PostNativeFlag.java
@@ -3,11 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.base.cached_flags;
-
-import org.chromium.base.FeatureList;
-import org.chromium.base.FeatureMap;
-import org.chromium.base.Flag;
+package org.chromium.base;
 
 /**
  * Flags of this type assume native is loaded and the value can be retrieved directly from native.
