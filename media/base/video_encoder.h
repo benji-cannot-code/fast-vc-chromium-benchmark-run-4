69550880163b3f4b0947031547115059eabc41fb
Eugene Zemtsov <eugene@chromium.org>
2024-09-04 21:05:11 +0000

    webcodecs: Manual reference frame control in Av1VideoEncoder
    Extension of media::VideoEncoder API to support manual control of
    inter-frame dependencies. So far the only implementation is
    Av1VideoEncoder.
    
    Bug: 347676170
    Change-Id: Ia81228763148a6b481a8688f9a805fe1394f074f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809671
    Reviewed-by: Erik Språng <sprang@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351052}

--- media/base/video_encoder.h
+++ media/base/video_encoder.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_BASE_VIDEO_ENCODER_H_
 #define MEDIA_BASE_VIDEO_ENCODER_H_
 
+#include <cstdint>
 #include <optional>
+#include <vector>
 
 #include "base/containers/heap_array.h"
 #include "base/functional/callback.h"
@@ -18,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/svc_scalability_mode.h"
 #include "media/base/video_codecs.h"
 #include "media/base/video_types.h"
+#include "third_party/abseil-cpp/absl/container/inlined_vector.h"
 #include "ui/gfx/color_space.h"
 #include "ui/gfx/geometry/size.h"
 
@@ -100,6 +103,9 @@ class MEDIA_EXPORT VideoEncoder {
 
     // Only used for HEVC encoding.
     HevcOptions hevc;
+
+    // Allow clients to choose reference pattern for frames.
+    bool manual_reference_buffer_control = false;
   };
 
   struct MEDIA_EXPORT EncodeOptions {
@@ -111,6 +117,18 @@ class MEDIA_EXPORT VideoEncoder {
     // Per-frame codec-specific quantizer value.
     // Should only be used when encoder configured with kExternal bitrate mode.
     std::optional<int> quantizer;
+
+    // Ids of the encoder's buffers (past frames) that this frame can reference
+    // for inter-frame prediction.
+    // Valid values: [0..VideoEncoderInfo::number_of_manual_reference_buffers)
+    absl::InlinedVector<uint8_t, 4> reference_buffers;
+
+    // Id of the encoder's buffer where this frame should be kept after
+    // encoding. This frames can later be referenced via `reference_buffers`.
+    // If empty, this frame can't be used for inter-frame prediction by future
+    // frames.
+    // Valid values: [0..VideoEncoderInfo::number_of_manual_reference_buffers)
+    std::optional<uint8_t> update_buffer;
   };
 
   // A sequence of codec specific bytes, commonly known as extradata.
