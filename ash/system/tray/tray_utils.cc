3e93d4e764097a2107c4895b45362065c6112016
amehfooz <amehfooz@google.com>
2024-09-04 17:54:02 +0000

    Use default TrayBubble corner radius
    When the BubbleCornerRadiusUpdate feature is
    enabled we do not want to set any custom corner
    radius for tray bubbles.
    
    Bug: b/363341908
    Change-Id: I9a8dfae0041995a658dd3514eb65a8c4ac2697ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834624
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350921}

--- ash/system/tray/tray_utils.cc
+++ ash/system/tray/tray_utils.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/bubble/bubble_constants.h"
 #include "ash/constants/ash_constants.h"
+#include "ash/constants/ash_features.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
@@ -176,7 +177,9 @@ TrayBubbleView::InitParams CreateInitParamsForTrayBubble(
   init_params.preferred_width = kTrayMenuWidth;
   init_params.close_on_deactivate = true;
   init_params.translucent = true;
-  init_params.corner_radius = kTrayItemCornerRadius;
+  if (!features::IsBubbleCornerRadiusUpdateEnabled()) {
+    init_params.corner_radius = kTrayItemCornerRadius;
+  }
   init_params.reroute_event_handler = true;
   init_params.anchor_to_shelf_corner = anchor_to_shelf_corner;
 
