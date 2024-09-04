f0b1066a79b99a67e6a35cb2b3b128d7397ba378
Avi Drissman <avi@chromium.org>
2024-09-04 21:24:51 +0000

    Use an opaque type for FrameTreeNode IDs, part 13
    content/browser/shared_storage
    
    Bug: 361344235
    Change-Id: Ibca0f85a0f409e33e878a78c56c32e0400243fd2
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837179
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351059}

--- content/browser/shared_storage/shared_storage_worklet_host_manager.h
+++ content/browser/shared_storage/shared_storage_worklet_host_manager.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "content/browser/shared_storage/shared_storage_event_params.h"
 #include "content/common/content_export.h"
+#include "content/public/browser/frame_tree_node_id.h"
 #include "third_party/blink/public/mojom/origin_trial_feature/origin_trial_feature.mojom-shared.h"
 #include "third_party/blink/public/mojom/shared_storage/shared_storage.mojom.h"
 
@@ -102,7 +103,7 @@ class CONTENT_EXPORT SharedStorageWorkletHostManager {
 
   void NotifySharedStorageAccessed(
       SharedStorageObserverInterface::AccessType type,
-      int main_frame_id,
+      FrameTreeNodeId main_frame_id,
       const std::string& owner_origin,
       const SharedStorageEventParams& params);
 
