02ab8dcd422fb71476a52b4a7db32eedcb2d197c
Jan Keitel <jkeitel@google.com>
2024-09-04 12:38:16 +0000

    Trigger reservation if inline suggestion has a loading state.
    Inline creation suggestions for plus addresses work as follows:
    1) If there is a pre-allocated plus address when generating an inline
       suggestion, then it is set as payload. Otherwise, the payload is
       left empty.
    2) When the suggestion is shown and the payload is empty, a network
       request is started to reserve additional plus addresses.
    3) Once the network request returns, the suggestion is updated.
    
    This CL adds step 2). The reason why the network request is sent after
    the suggestion is shown (and not before) is as follows:
    - At some point in the future, BAM may trigger suggestion generation,
      but then not actually show the suggestion. In that case, we should
      not sent a network request if the suggestion is never displayed.
    - We want to only update the suggestion UI is its still showing when the
      network request completes in step 3. For that, we need to know the
      SuggestionUiSessionId and that is only available once the UI is
      showing.
    
    Bug: 362445807
    Change-Id: I76430d828b6af820c50976b3f62a11924e0fe8b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829210
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350729}

--- components/plus_addresses/plus_address_suggestion_generator.cc
+++ components/plus_addresses/plus_address_suggestion_generator.cc
@@ -154,6 +154,14 @@ Suggestion PlusAddressSuggestionGenerator::GetManagePlusAddressSuggestion() {
   return suggestion;
 }
 
+// static
+void PlusAddressSuggestionGenerator::SetSuggestedPlusAddressForSuggestion(
+    const PlusAddress& plus_address,
+    autofill::Suggestion& suggestion) {
+  suggestion.payload =
+      Suggestion::PlusAddressPayload(base::UTF8ToUTF16(*plus_address));
+}
+
 autofill::Suggestion
 PlusAddressSuggestionGenerator::CreateNewPlusAddressSuggestion() {
   if (IsInlineGenerationEnabled()) {
@@ -204,8 +212,7 @@ PlusAddressSuggestionGenerator::CreateNewPlusAddressInlineSuggestion() {
   if (std::optional<PlusProfile> profile =
           allocator_->AllocatePlusAddressSynchronously(
               origin_, PlusAddressAllocator::AllocationMode::kNewPlusAddress)) {
-    suggestion.payload = Suggestion::PlusAddressPayload(
-        base::UTF8ToUTF16(profile->plus_address.value()));
+    SetSuggestedPlusAddressForSuggestion(profile->plus_address, suggestion);
   } else {
     suggestion.payload = Suggestion::PlusAddressPayload();
   }
