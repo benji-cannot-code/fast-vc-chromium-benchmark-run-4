e11623a00cf41d9564ba7423bb0a9706a4dbdc01
Christian Biesinger <cbiesinger@chromium.org>
2024-09-04 14:35:34 +0000

    Reland "[FedCM] Pass the list of requested permission fields to the UI"
    This reverts commit 5c4f72879e8e6b7b12df244b44b7286415f2093c.
    
    Reason for revert: Relanding the CL now that https://crrev.com/c/5829302 has relanded. No changes in this CL as it was only reverted as a dependency of that CL.
    
    Original change's description:
    > Revert "[FedCM] Pass the list of requested permission fields to the UI"
    >
    > This reverts commit e0aa1818b9f7ea965cb2c62bb6dfd1e95bf0ba3c.
    >
    > Reason for revert: Causes test failures in ASAN build.
    >
    > Bug: 361159651
    >
    > Original change's description:
    > > [FedCM] Pass the list of requested permission fields to the UI
    > >
    > > This is behind a flag and the UI does not make use of this yet.
    > >
    > > The UI will use this later on along the lines of
    > > https://crrev.com/c/5807355
    > >
    > > The Android UI code will be updated separately.
    > >
    > > Bug: 361370177
    > > Change-Id: I4ec5628f8320239158db5b86f7aa4b0928699853
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5816437
    > > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > > Reviewed-by: Nicolás Peña <npm@chromium.org>
    > > Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1348920}
    >
    > Bug: 361370177
    > Change-Id: I08a27ef2eb464d34d65a22fe5dcf50f8ffa8eab3
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823047
    > Owners-Override: Titouan Rigoudy <titouan@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Titouan Rigoudy <titouan@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1349110}
    
    Bug: 361159651
    Bug: 361370177
    Change-Id: I73cd91aa3ed5dafad40bc45fb69fe0b1532b06ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832169
    Reviewed-by: Nicolás Peña <npm@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350779}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_browsertest.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop_browsertest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/public/tab_features.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
+#include "chrome/browser/ui/views/webid/account_selection_view_test_base.h"
 #include "chrome/browser/ui/views/webid/fake_delegate.h"
 #include "chrome/browser/ui/views/webid/fedcm_account_selection_view_controller.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -51,7 +52,7 @@ class FedCmAccountSelectionViewBrowserTest : public DialogBrowserTest {
         "rp-example.com",
         {{"idp-example.com", accounts, content::IdentityProviderMetadata(),
           content::ClientMetadata(GURL(), GURL(), GURL()),
-          blink::mojom::RpContext::kSignIn, /*request_permission=*/true,
+          blink::mojom::RpContext::kSignIn, kDefaultDisclosureFields,
           /*has_login_status_mismatch=*/false}},
         mode, blink::mojom::RpMode::kWidget,
         /*new_account_idp*/ std::nullopt);
