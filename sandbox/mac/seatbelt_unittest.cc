6e29b46867cd3838b602531ba478ce2eef8d1a06
Mark Rowe <markrowe@chromium.org>
2024-09-04 18:10:23 +0000

    Update SeatbeltTest to avoid unsafe buffer operations
    An array of integers used to specify the sysctl name is replaced with a
    `std::array` since that knows its size.
    
    Change-Id: If4f8c700633a8ec72cfe6387087c204e9cc600d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818791
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350937}

--- sandbox/mac/seatbelt_unittest.cc
+++ sandbox/mac/seatbelt_unittest.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "sandbox/mac/seatbelt.h"
 
 #include <errno.h>
@@ -126,16 +121,17 @@ MULTIPROCESS_TEST_MAIN(ProcessSelfInfo) {
   std::string error;
   CHECK(Seatbelt::Init(profile, 0, &error)) << error;
 
-  int mib[] = {CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()};
+  std::array<int, 4> mib = {CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()};
   kinfo_proc proc;
   size_t size = sizeof(proc);
 
-  int rv = sysctl(mib, std::size(mib), &proc, &size, nullptr, 0);
+  int rv = sysctl(mib.data(), mib.size(), &proc, &size, nullptr, 0);
   PCHECK(rv == 0);
 
-  mib[std::size(mib) - 1] = getppid();
+  mib.back() = getppid();
+
   errno = 0;
-  rv = sysctl(mib, std::size(mib), &proc, &size, nullptr, 0);
+  rv = sysctl(mib.data(), mib.size(), &proc, &size, nullptr, 0);
   PCHECK(rv == -1);
   PCHECK(errno == EPERM);
 
