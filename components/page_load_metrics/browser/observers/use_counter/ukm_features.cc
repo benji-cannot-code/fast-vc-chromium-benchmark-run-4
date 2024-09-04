93b7f72d0ebadd17d8c3550ff4b3b9f1b89cee51
Sandor Major <sandormajor@google.com>
2024-09-04 17:04:52 +0000

    [Partitioned Popins] Implement `popinContextTypesSupported()` JS API
    This CL implements `window.popinContextTypesSupported()` that returns
    the supported popin context types.
    
    Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED Tested through WPTs.
    
    Bug: 340606651
    Bug: b/362973176
    Change-Id: I02b2a75778610a5d23ca929b328fbcdd21fa9403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5822620
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350876}

--- components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
+++ components/page_load_metrics/browser/observers/use_counter/ukm_features.cc
@@ -407,6 +407,7 @@ UseCounterMetricsRecorder::GetAllowedUkmFeatures() {
           WebFeature::kFormValidationShowedMessage,
           WebFeature::kPartitionedPopin_OpenAttempt,
           WebFeature::kPartitionedPopin_Opened,
+          WebFeature::kV8Window_PopinContextTypesSupported_Method,
       }));
   return *opt_in_features;
 }
