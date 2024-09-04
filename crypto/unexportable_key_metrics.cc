43036f12db2d0985fd05c746882fa5284a04bd7e
Mark Rowe <markrowe@chromium.org>
2024-09-04 05:35:43 +0000

    [Mac] Improve metrics for unexportable keys
    GetUnexportableKeyProvider returns null on Mac if the device does not
    support creating keys in the secure enclave. Report this as
    TPMSupport::kNone.
    
    Bug: 353772955
    Change-Id: I8fbf61fca8493a8a6686d04d57c0270430982961
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830083
    Commit-Queue: Mark Rowe <markrowe@chromium.org>
    Reviewed-by: Kristian Monsen <kristianm@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350603}

--- crypto/unexportable_key_metrics.cc
+++ crypto/unexportable_key_metrics.cc
@@ -189,6 +189,7 @@ void MeasureTpmOperationsInternal(UnexportableKeyProvider::Config config) {
   std::unique_ptr<UnexportableKeyProvider> provider =
       GetUnexportableKeyProvider(std::move(config));
   if (!provider) {
+    base::UmaHistogramEnumeration("Crypto.TPMSupportType", supported_algo);
     return;
   }
 
