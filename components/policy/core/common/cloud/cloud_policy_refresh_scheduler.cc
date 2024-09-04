3ce62b1c73c9c145cc9044347f155ae5485519e0
Igor <igorcov@chromium.org>
2024-09-04 10:16:24 +0000

    Avoid device local accounts policy fetch on sign out
    This is achieved by storing a file in memory
    on the first device policy fetch and checking for
    its existence every time Chrome is started.
    
    BUG=b:257959909
    TEST=Manual test on device
    
    Change-Id: I3a5aa876e6965befd687bb554d91ae3cc7cadfd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782834
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Igor <igorcov@chromium.org>
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1350695}

--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
@@ -97,7 +97,8 @@ CloudPolicyRefreshScheduler::CloudPolicyRefreshScheduler(
     CloudPolicyStore* store,
     CloudPolicyService* service,
     const scoped_refptr<base::SequencedTaskRunner>& task_runner,
-    network::NetworkConnectionTrackerGetter network_connection_tracker_getter)
+    network::NetworkConnectionTrackerGetter network_connection_tracker_getter,
+    bool skip_first_policy_fetch)
     : client_(client),
       store_(store),
       service_(service),
@@ -112,6 +113,7 @@ CloudPolicyRefreshScheduler::CloudPolicyRefreshScheduler(
   store_->AddObserver(this);
   network_connection_tracker_->AddNetworkConnectionObserver(this);
 
+  skip_first_policy_fetch_ = skip_first_policy_fetch;
   UpdateLastRefreshFromPolicy();
   ScheduleRefresh();
 }
@@ -435,6 +437,20 @@ void CloudPolicyRefreshScheduler::RefreshAfter(int delta_ms,
   // refresh in order to spread out server load.
   if (!delay.is_zero())
     delay += base::Milliseconds(refresh_delay_salt_ms_);
+
+#if BUILDFLAG(IS_CHROMEOS_ASH)
+  // On browser start the `last_refresh_` is not set yet, and the calculated
+  // delay ends up being zero. If we need to skip the initial policy fetch,
+  // we reset the delay here to the input value received.
+  if (skip_first_policy_fetch_) {
+    skip_first_policy_fetch_ = false;
+    delay = delta;
+
+    // We have to refresh the last_refresh_ values here to enforce the delay.
+    last_refresh_ = GetClock()->Now();
+    last_refresh_ticks_ = GetTickClock()->NowTicks();
+  }
+#endif
 
   refresh_callback_.Reset(
       base::BindOnce(&CloudPolicyRefreshScheduler::PerformRefresh,
