6c69b90b93099a503ec76221979ab9b9d148587c
Robert Ogden <robertogden@chromium.org>
2024-09-04 17:47:31 +0000

    TF Text: Roll to HEAD and include two additional kernels
    additional kernels for b/339037155
    
    Change-Id: Ie439d5f5c459091eccb0a4ab91c1b4c49decbe42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837822
    Auto-Submit: Robert Ogden <robertogden@chromium.org>
    Reviewed-by: Xiang Xiao <xiangxiao@google.com>
    Commit-Queue: Robert Ogden <robertogden@chromium.org>
    Commit-Queue: Xiang Xiao <xiangxiao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350917}

--- third_party/tensorflow-text/src/tensorflow_text/core/kernels/wordpiece_tokenizer.h
+++ third_party/tensorflow-text/src/tensorflow_text/core/kernels/wordpiece_tokenizer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright 2023 TF.Text Authors.
+// Copyright 2024 TF.Text Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
