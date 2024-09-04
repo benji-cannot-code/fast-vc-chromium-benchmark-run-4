7cc3dcb37d218e346039e8258551d0ae700479d6
Daniil Sakhapov <sakhapov@chromium.org>
2024-09-04 11:09:16 +0000

    Use length resolver for palette-mix()
    This CL adds length resolver to resolve calc() expressions inside
    palette-mix().
    It also changes some code around it, that depends on the same functions.
    In some places where we don't have access to length resolver, for now,
    we will use default length resolver, with TODOs being placed there not
    to forget to deal with them later.
    Also, in this CL the correct interpolation is not addressed and it will be
    rewritten in following CL.
    
    Bug: 40946458
    Change-Id: I74e808429ef50bcd689efc100e388af814c6c729
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831439
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350706}

--- third_party/blink/renderer/core/animation/css_font_palette_interpolation_type.cc
+++ third_party/blink/renderer/core/animation/css_font_palette_interpolation_type.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/core/animation/interpolable_font_palette.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/core/css/resolver/style_builder_converter.h"
 #include "third_party/blink/renderer/core/style/computed_style.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
@@ -69,8 +70,11 @@ InterpolationValue CSSFontPaletteInterpolationType::MaybeConvertValue(
     const CSSValue& value,
     const StyleResolverState* state,
     ConversionCheckers& conversion_checkers) const {
-  return ConvertFontPalette(
-      StyleBuilderConverterBase::ConvertFontPalette(value));
+  // TODO(40946458): Don't resolve anything here, rewrite to
+  // interpolate unresolved palettes.
+  return ConvertFontPalette(StyleBuilderConverterBase::ConvertFontPalette(
+      state ? state->CssToLengthConversionData() : CSSToLengthConversionData(),
+      value));
 }
 
 InterpolationValue
--- third_party/blink/renderer/core/css/css_color_mix_value.cc
+++ third_party/blink/renderer/core/css/css_color_mix_value.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/css/css_color_mix_value.h"
 
 #include "third_party/blink/renderer/core/css/css_numeric_literal_value.h"
+#include "third_party/blink/renderer/core/css/css_to_length_conversion_data.h"
 #include "third_party/blink/renderer/platform/wtf/math_extras.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 
@@ -15,14 +16,19 @@ bool CSSColorMixValue::NormalizePercentages(
     const CSSPrimitiveValue* percentage1,
     const CSSPrimitiveValue* percentage2,
     double& mix_amount,
-    double& alpha_multiplier) {
+    double& alpha_multiplier,
+    const CSSLengthResolver& length_resolver) {
   double p1 = 0.5;
   if (percentage1) {
-    p1 = ClampTo<double>(percentage1->GetDoubleValue(), 0.0, 100.0) / 100.0;
+    p1 = ClampTo<double>(percentage1->ComputePercentage(length_resolver), 0.0,
+                         100.0) /
+         100.0;
   }
   double p2 = 0.5;
   if (percentage2) {
-    p2 = ClampTo<double>(percentage2->GetDoubleValue(), 0.0, 100.0) / 100.0;
+    p2 = ClampTo<double>(percentage2->ComputePercentage(length_resolver), 0.0,
+                         100.0) /
+         100.0;
   }
 
   if (percentage1 && !percentage2) {
@@ -55,9 +61,14 @@ bool CSSColorMixValue::NormalizePercentages(
 }
 
 Color CSSColorMixValue::Mix(const Color& color1, const Color& color2) const {
+  CHECK(!Percentage1() || Percentage1()->IsNumericLiteralValue());
+  CHECK(!Percentage2() || Percentage2()->IsNumericLiteralValue());
   double alpha_multiplier;
   double mix_amount;
-  if (!NormalizePercentages(mix_amount, alpha_multiplier)) {
+  // Using default length resolver here as we only call it with numeric %, no
+  // calc().
+  if (!NormalizePercentages(mix_amount, alpha_multiplier,
+                            CSSToLengthConversionData())) {
     return Color();
   }
   return Color::FromColorMix(ColorInterpolationSpace(),
