47a47b8105faf1ee03e823967935426bb749d618
Artem Sumaneev <asumaneev@google.com>
2024-09-04 12:46:12 +0000

    Add WeakPtrFactory to policy refersh in CloudPolicyRefreshScheduler
    The scheduler was sending raw pointer to CloudPolicyService with
    OnRefresh callback.
    
    Even though lifetimes of the scheduler and the service are fine (both
    live in CloudPolicyCore, and both destroyed together), its better to
    pass a weak pointer.
    
    Bug: b:355388750
    Change-Id: Ic7921758e5b40d033ed6913cf39c6aa3f625c9a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823847
    Reviewed-by: Igor <igorcov@chromium.org>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350732}

--- components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
+++ components/policy/core/common/cloud/cloud_policy_refresh_scheduler.cc
@@ -408,7 +408,7 @@ void CloudPolicyRefreshScheduler::PerformRefresh(PolicyFetchReason reason) {
     // OnPolicyFetched().
     service_->RefreshPolicy(
         base::BindOnce(&CloudPolicyRefreshScheduler::OnPolicyRefreshed,
-                       base::Unretained(this)),
+                       weak_factory_.GetWeakPtr()),
         reason);
     return;
   }
