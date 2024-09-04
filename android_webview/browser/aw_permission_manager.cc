88f35d00b7d1b20c831a68ccebda963abfe17109
Adam Rice <ricea@chromium.org>
2024-09-04 00:26:26 +0000

    Replace if () NOTREACHED() with CHECK() in android_webview/browser
    Replace simple cases of if (foo) NOTREACHED(); and if (!bar)
    NOTREACHED() with CHECK(!foo); and CHECK(bar) in
    android_webview/browser.
    
    This aligns with current guidance:
    https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/checks.md#:~:text=Use%20CHECK(bar)%3B%20instead
    
    Most changes were automated with regular expression substitutions.
    
    This CL was uploaded by git cl split.
    
    R=peconn@chromium.org
    
    Change-Id: I68ab92b2721fe844e43a62d914ce9542bff2e7b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5822811
    Auto-Submit: Adam Rice <ricea@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350513}

--- android_webview/browser/aw_permission_manager.cc
+++ android_webview/browser/aw_permission_manager.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/browser/aw_context_permissions_delegate.h"
 #include "android_webview/browser/aw_settings.h"
 #include "android_webview/common/aw_features.h"
+#include "base/check.h"
 #include "base/containers/contains.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -81,9 +82,7 @@ class LastRequestResultCache {
     }
 
     std::string key = GetCacheKey(requesting_origin, embedding_origin);
-    if (key.empty()) {
-      NOTREACHED();
-    }
+    CHECK(!key.empty());
     pmi_result_cache_[key] = status;
   }
 
