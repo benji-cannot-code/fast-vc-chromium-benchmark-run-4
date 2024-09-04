4e30b96ed737cc31659406c9caeede5d9b36ec30
Daniel Rubery <drubery@chromium.org>
2024-09-04 19:51:38 +0000

    Handle official webstore downloads in IsTrustedExtensionDownload
    There is an existing integration to treat downloads satisfying
    IsTrustedExtensionDownload as safe. This function only allows offstore
    extension downloads though. This CL improves it to handle webstore
    downloads as well.
    
    Existing check: https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/download/download_target_determiner.cc;drc=fafaac70b3842866188e1e139d149dbdbe294bb1;l=1310
    
    Change-Id: I5cf7a6184017e56acb1238fc73c40c6886029c41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832093
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351004}

--- chrome/browser/download/download_crx_util.cc
+++ chrome/browser/download/download_crx_util.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_item_utils.h"
 #include "extensions/browser/extension_system.h"
+#include "extensions/common/extension_urls.h"
 #include "extensions/common/user_script.h"
 
 using content::BrowserThread;
@@ -118,7 +119,9 @@ bool IsExtensionDownload(const DownloadItem& download_item) {
 
 bool IsTrustedExtensionDownload(Profile* profile, const DownloadItem& item) {
   return IsExtensionDownload(item) &&
-         OffStoreInstallAllowedByPrefs(profile, item);
+         (OffStoreInstallAllowedByPrefs(profile, item) ||
+          extension_urls::IsWebstoreUpdateUrl(item.GetURL()) ||
+          extension_urls::IsWebstoreDomain(item.GetURL()));
 }
 
 std::unique_ptr<base::AutoReset<bool>> OverrideOffstoreInstallAllowedForTesting(
