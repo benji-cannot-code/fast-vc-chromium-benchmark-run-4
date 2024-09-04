1e5e5aa42eca5b84873161400883dd89516f5be7
Shuhei Takahashi <nya@chromium.org>
2024-09-04 00:39:28 +0000

    [Settings] Convert onAttachFragment to FragmentLifecycleCallbacks
    Rest of onAttachFragment have been converted to
    FragmentLifecycleCallbacks, so let us convert the last logging one to it
    as well. Also, the new callback checks whether the fragment being
    attached is the main fragment so that it does not incorrectly report
    other fragments (currently there are none, but we are soon adding them).
    
    Bug: b/356743945
    Change-Id: I024d5e227570f6c1cd3a1744e1ad09f80dc6e21a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831984
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350516}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsActivity.java
@@ -142,6 +142,8 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                 new TitleUpdater(), false /* recursive */);
         fragmentManager.registerFragmentLifecycleCallbacks(
                 new WideDisplayPaddingApplier(), false /* recursive */);
+        fragmentManager.registerFragmentLifecycleCallbacks(
+                new SettingsMetricsReporter(), false /* recursive */);
 
         super.onCreate(savedInstanceState);
 
@@ -375,20 +377,6 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
     }
 
     @Override
-    public void onAttachFragment(Fragment fragment) {
-        String className = fragment.getClass().getSimpleName();
-        RecordHistogram.recordSparseHistogram("Settings.FragmentAttached", className.hashCode());
-        // Log hashCode to easily add new class names to enums.xml.
-        Log.d(
-                "SettingsActivity",
-                String.format(
-                        Locale.ENGLISH,
-                        "Settings.FragmentAttached: <int value=\"%d\" label=\"%s\"/>",
-                        className.hashCode(),
-                        className));
-    }
-
-    @Override
     public SnackbarManager getSnackbarManager() {
         return mSnackbarManagerSupplier.get();
     }
@@ -482,6 +470,31 @@ public class SettingsActivity extends ChromeBaseAppCompatActivity
                 // completes.
                 WideDisplayPadding.apply(fragment, SettingsActivity.this);
             }
+        }
+    }
+
+    private static class SettingsMetricsReporter
+            extends FragmentManager.FragmentLifecycleCallbacks {
+        @Override
+        public void onFragmentAttached(
+                @NonNull FragmentManager fragmentManager,
+                @NonNull Fragment fragment,
+                @NonNull Context context) {
+            if (!MAIN_FRAGMENT_TAG.equals(fragment.getTag())) {
+                return;
+            }
+
+            String className = fragment.getClass().getSimpleName();
+            RecordHistogram.recordSparseHistogram(
+                    "Settings.FragmentAttached", className.hashCode());
+            // Log hashCode to easily add new class names to enums.xml.
+            Log.d(
+                    "SettingsActivity",
+                    String.format(
+                            Locale.ENGLISH,
+                            "Settings.FragmentAttached: <int value=\"%d\" label=\"%s\"/>",
+                            className.hashCode(),
+                            className));
         }
     }
 }
