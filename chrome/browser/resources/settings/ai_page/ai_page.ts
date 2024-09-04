d5b64de1cb9c17ec5004a2bd60c22a29d9a741d3
Zaina Al-Mashni <zalmashni@google.com>
2024-09-04 18:36:42 +0000

    [Synapse] Refactor feature specific rows in experimental page
    - Hide the existing toggles in the Ai page.
    - Create new cr-link-row for every feature.
    - Hide/Show the rows based on feature state.
    
    This is just adding the UI of the new AI page, the click action will be
    implemented in follow up changes.
    
    Screenshot: https://screenshot.googleplex.com/7GLopvnZjDPLB4P
    Bug: 362225975
    Change-Id: I50ff3d2cd044538c7969ceea2ccc9a70bb68f96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830492
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350952}

--- chrome/browser/resources/settings/ai_page/ai_page.ts
+++ chrome/browser/resources/settings/ai_page/ai_page.ts
@@ -55,6 +55,11 @@ export class SettingsAiPageElement extends SettingsAiPageElementBase {
         notify: true,
       },
 
+      enableAiSettingsPageRefresh_: {
+        type: Boolean,
+        value: () => loadTimeData.getBoolean('enableAiSettingsPageRefresh'),
+      },
+
       showComposeControl_: {
         type: Boolean,
         value: () => loadTimeData.getBoolean('showComposeControl'),
@@ -88,6 +93,7 @@ export class SettingsAiPageElement extends SettingsAiPageElementBase {
     };
   }
 
+  private enableAiSettingsPageRefresh_: boolean;
   private showComposeControl_: boolean;
   private showHistorySearchControl_: boolean;
   private showTabOrganizationControl_: boolean;
