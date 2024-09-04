602b72f611193f2ce6a4d0253d1c2a727f75c9f6
Behnood Momenzadeh <behnoodm@chromium.org>
2024-09-04 17:18:00 +0000

    Add ResetSettings to ProfileResetter
    With the future addition of Sanitize, this new feature would need to take advantage of resetting settings. As the Sanitize feature could not sit inside the ResetSettingsHandler, this CL aims to bring in the initialization that was done inside the ResetSettingsHandler into ProfileResetter so the Sanitize feature can directly call ProfileResetter.
    
    BUG=b:357420360, b:317898420
    TEST=unit_tests --gtest_filter="*ResetSettingsHandlerTest.*"
    unit_tests --gtest_filter="*ProfileResetterTest.*"
    
    Change-Id: I80c366ae01dc86167d68e0681ce1f5d1b03629db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761827
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Behnood Momenzadeh <behnoodm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350888}

---
+++ chrome/browser/profile_resetter/fake_profile_resetter.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/profile_resetter/fake_profile_resetter.h"
+
+FakeProfileResetter::FakeProfileResetter(TestingProfile* profile)
+    : ProfileResetter(profile) {}
+
+bool FakeProfileResetter::IsActive() const {
+  return false;
+}
+
+void FakeProfileResetter::ResetSettings(
+    ResettableFlags resettable_flags,
+    std::unique_ptr<BrandcodedDefaultSettings> initial_settings,
+    base::OnceClosure callback) {
+  ++reset_count_;
+  std::move(callback).Run();
+}
+
+size_t FakeProfileResetter::Resets() const {
+  return reset_count_;
+}
