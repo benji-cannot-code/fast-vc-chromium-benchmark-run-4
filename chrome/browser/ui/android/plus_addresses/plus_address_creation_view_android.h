d9d2c0984a02c7085403cf33fd5b307c3f3bffa6
Timofey Chudakov <tchudakov@google.com>
2024-09-04 07:14:58 +0000

    [Android] Remove web content from the PA creation view.
    PA - plus address.
    
    This CL moves NativeView and TabModel retrieval to the controller
    to follow the generally used pattern. It also adds necessary
    sanity checks on the retrieved native view.
    
    Bug: 364181540
    Change-Id: I2a9d97e4723760d28294ad79bf78237ad0929f50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5830609
    Reviewed-by: Friedrich Hauser <fhorschig@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350630}

--- chrome/browser/ui/android/plus_addresses/plus_address_creation_view_android.h
+++ chrome/browser/ui/android/plus_addresses/plus_address_creation_view_android.h
@@ -8,26 +8,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <jni.h>
 
-#include "chrome/browser/ui/plus_addresses/plus_address_creation_controller.h"
 #include "components/plus_addresses/plus_address_types.h"
-#include "content/public/browser/web_contents.h"
+#include "ui/gfx/native_widget_types.h"
+
+class TabModel;
 
 namespace plus_addresses {
 
+class PlusAddressCreationController;
+
 // A class intended as a thin wrapper around a Java object, which calls out to
-// the `PlusAddressCreationController`. This shields the controller from JNI
-// complications, allowing a consistent interface for clients (e.g., autofill).
-// Note that it is likely that either the controller will morph to do what this
-// class does now, or a similar wrapper will be created for desktop, with a
-// single controller implementation.
+// the `PlusAddressCreationControllerAndroid`. This shields the controller from
+// JNI complications, allowing a consistent interface for clients (e.g.,
+// autofill). Note that it is likely that either the controller will morph to do
+// what this class does now, or a similar wrapper will be created for desktop,
+// with a single controller implementation.
 class PlusAddressCreationViewAndroid {
  public:
-  PlusAddressCreationViewAndroid(
-      base::WeakPtr<PlusAddressCreationController> controller,
-      content::WebContents* web_contents);
+  explicit PlusAddressCreationViewAndroid(
+      base::WeakPtr<PlusAddressCreationController> controller);
   ~PlusAddressCreationViewAndroid();
 
-  void ShowInit(const std::string& primary_email_address,
+  void ShowInit(gfx::NativeView native_view,
+                TabModel* tab_model,
+                const std::string& primary_email_address,
                 bool refresh_supported,
                 bool has_accepted_notice);
   void OnRefreshClicked(JNIEnv* env,
@@ -51,7 +55,6 @@ class PlusAddressCreationViewAndroid {
   // The corresponding java object.
   base::android::ScopedJavaGlobalRef<jobject> java_object_;
   base::WeakPtr<PlusAddressCreationController> controller_;
-  raw_ptr<content::WebContents> web_contents_;
 };
 
 }  // namespace plus_addresses
