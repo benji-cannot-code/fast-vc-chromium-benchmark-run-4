05911fea45261857158a2ac918c8ac52f9d9eb16
Danila Kuzmin <dkuzmin@google.com>
2024-09-04 15:49:23 +0000

    [Gardening] Disable ChromeVoxBackgroundTtsTest.Preprocess on MSan
    Test is consistently failing on MSan builds. First failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/45863/
    
    Bug: b:364477797
    Change-Id: I5f5085a5c5897cbd97d36664d61b3fed3f5f4c82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837277
    Auto-Submit: Danila Kuzmin <dkuzmin@google.com>
    Commit-Queue: Danila Kuzmin <dkuzmin@google.com>
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Owners-Override: Danila Kuzmin <dkuzmin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1350825}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/tts_background_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/tts_background_test.js
@@ -27,7 +27,8 @@ ChromeVoxBackgroundTtsTest = class extends ChromeVoxE2ETest {
   }
 };
 
-AX_TEST_F('ChromeVoxBackgroundTtsTest', 'Preprocess', function() {
+// TODO(b/364477797): Failing on MSAN.
+AX_TEST_F('ChromeVoxBackgroundTtsTest', 'MAYBE_Preprocess', function() {
   const preprocess = tts.preprocess.bind(tts);
 
   // Punctuation.
@@ -85,7 +86,13 @@ AX_TEST_F('ChromeVoxBackgroundTtsTest', 'Preprocess', function() {
   assertEquals('3 square bullets', preprocess('\u25a0\u25a0\u25a0'));
 
   assertEquals('space', preprocess('\u00a0'));
-});
+}, `
+#if defined(MEMORY_SANITIZER)
+#define MAYBE_Preprocess DISABLED_Preprocess
+#else
+#define MAYBE_Preprocess Preprocess
+#endif
+`);
 
 TEST_F('ChromeVoxBackgroundTtsTest', 'UpdateVoice', function() {
   const voices = [
