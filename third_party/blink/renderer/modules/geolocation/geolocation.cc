b8e40678a68203371082ab9ee628a4601db9eee5
Alvin Ji <alvinji@chromium.org>
2024-09-04 16:24:31 +0000

    geolocation: Implement fallback mechanism when Wi-Fi is Disabled
    This change implements a fallback mechanism from `CoreLocationProvider`
    to the `NetworkLocationProvider` when Wi-Fi is disabled. The goal is to
    continue providing location information even when the
    `CoreLocationProvider` fails due to Wi-Fi unavailability.
    
    Key Changes:
    - New error code: Adds a new `kWifiDisabled` error code to
      `GeopositionErrorCode` to signal the fallback scenario.
    - NetworkChangeObserver: Implement
      `net::NetworkChangeNotifier::NetworkChangeObserver` in
      `SystemGeolocationSourceApple`. Ensure fallback is started after
      network status is settled.
    - Thread safety: Add thread check in `SystemGeolocationSourceApple` to
      ensure critical position methods are running on main thread so all
      access to `network_changed_timer_` is safe.
    - Code organization: Renanmed from
     'GeolocationSystemPermissionManagerDelegate` to
     `LocationManagerDelegate` for better readability.
    
    Bug: 346842084
    Change-Id: If6368a222785a0f61876bf2d7b0ffaaa2a856aaa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5767326
    Reviewed-by: Adam Langley <agl@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Commit-Queue: Alvin Ji <alvinji@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1350850}

--- third_party/blink/renderer/modules/geolocation/geolocation.cc
+++ third_party/blink/renderer/modules/geolocation/geolocation.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/notreached.h"
 #include "base/task/single_thread_task_runner.h"
 #include "services/device/public/mojom/geoposition.mojom-blink.h"
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom-blink.h"
@@ -89,6 +90,11 @@ GeolocationPositionError* CreatePositionError(
     case device::mojom::blink::GeopositionErrorCode::kPositionUnavailable:
       error_code = GeolocationPositionError::kPositionUnavailable;
       break;
+    default:
+      // On Blink side, it should only handles W3C defined error codes.
+      // If it reaches here that means an unexpected error type being propagated
+      // to Blink. This should never happen.
+      NOTREACHED_NORETURN();
   }
   return MakeGarbageCollected<GeolocationPositionError>(error_code,
                                                         error.error_message);
