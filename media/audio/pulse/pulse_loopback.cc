55e43f3ed263ee606c5832410b2be442042db533
AlexiaBojian <bojianalexia4@gmail.com>
2024-09-04 19:43:49 +0000

    Implement local muting for pulseaudio loopback capture.
    link to design doc: https://docs.google.com/document/d/1yDRmzvmQhnUng0Y1PlUMCl4B0PfMP2CA6CUyx4jttNM/edit#heading=h.7nki9mck5t64
    
    
    Bug id: 348313403
    
    Change-Id: I2532703e2b1b6fb4cd14727959e0d439fa11da0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5664732
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexia Bojian <bojianalexia4@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1351000}

--- media/audio/pulse/pulse_loopback.cc
+++ media/audio/pulse/pulse_loopback.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "audio_manager_pulse.h"
 #include "pulse_input.h"
+#include "pulse_util.h"
 
 namespace media {
 
@@ -16,12 +17,14 @@ PulseLoopbackAudioStream::PulseLoopbackAudioStream(
     const AudioParameters& params,
     pa_threaded_mainloop* mainloop,
     pa_context* context,
-    AudioManager::LogCallback log_callback)
+    AudioManager::LogCallback log_callback,
+    bool mute_system_audio)
     : release_stream_callback_(std::move(release_stream_callback)),
       params_(params),
       mainloop_(mainloop),
       context_(context),
       log_callback_(std::move(log_callback)),
+      mute_system_audio_(mute_system_audio),
       sink_(nullptr),
       stream_(new PulseAudioInputStream(nullptr,
                                         source_name,
@@ -41,6 +44,14 @@ AudioInputStream::OpenOutcome PulseLoopbackAudioStream::Open() {
   OpenOutcome open_outcome = stream_->Open();
   if (open_outcome == OpenOutcome::kSuccess) {
     stream_opened_ = true;
+    if (mute_system_audio_) {
+      std::string default_sink_name = media::pulse::GetRealDefaultDeviceId(
+          mainloop_, context_, pulse::RequestType::OUTPUT);
+      std::string monitor_source_name =
+          media::pulse::GetMonitorSourceNameForSink(mainloop_, context_,
+                                                    default_sink_name);
+      pulse::MuteAllSinksExcept(mainloop_, context_, monitor_source_name);
+    }
   }
 
   return open_outcome;
@@ -62,6 +73,9 @@ void PulseLoopbackAudioStream::Stop() {
 void PulseLoopbackAudioStream::Close() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   CHECK(!sink_);
+  if (mute_system_audio_) {
+    pulse::UnmuteAllSinks(mainloop_, context_);
+  }
   CloseWrappedStream();
   std::move(release_stream_callback_).Run(this);
 }
