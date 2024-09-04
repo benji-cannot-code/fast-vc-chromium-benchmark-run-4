1df0aeab40cc5dab00b57528e0cfa6455b411069
Dave Tapuska <dtapuska@chromium.org>
2024-09-04 20:49:29 +0000

    [ios] Fix failure building blink on iOS
    The geolocation service is compiled for iOS blink.
    https://chromium-review.googlesource.com/c/chromium/src/+/5767326 introduced a dependency in the common Apple code on CoreWLAN.
    CoreWLAN is only available on MacOS so ensure the code conditionally
    compiles in that code.
    
    Fixed failures https://ci.chromium.org/ui/p/chromium/builders/ci/ios-blink-dbg-fyi/35062/overview
    
    Change-Id: I1589e6b17d1a7dfb2af191ea9d5036284f4f5f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835654
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351041}

--- services/device/public/cpp/geolocation/system_geolocation_source_apple.mm
+++ services/device/public/cpp/geolocation/system_geolocation_source_apple.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/device/public/cpp/geolocation/system_geolocation_source_apple.h"
 
-#import <CoreWLAN/CoreWLAN.h>
-
 #include <memory>
 
 #include "base/functional/callback_helpers.h"
@@ -14,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "build/build_config.h"
 #include "services/device/public/cpp/geolocation/location_manager_delegate.h"
+
+#if BUILDFLAG(IS_MAC)
+#import <CoreWLAN/CoreWLAN.h>
+#endif
 
 namespace device {
 
@@ -24,9 +26,13 @@ bool SystemGeolocationSourceApple::IsWifiEnabled() {
   if (mock_wifi_status_.has_value()) {
     return *mock_wifi_status_;
   }
+#if BUILDFLAG(IS_MAC)
   CWWiFiClient* wifi_client = [CWWiFiClient sharedWiFiClient];
   CWInterface* interface = wifi_client.interface;
   return (interface && interface.powerOn);
+#else
+  return true;
+#endif
 }
 
 SystemGeolocationSourceApple::SystemGeolocationSourceApple()
