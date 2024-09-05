f8b02c2a4cf5ff94fb895da566a22af6b8d2bdc6
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 02:31:07 +0000

    ash: Create //chrome/browser/ui/webui/ash/sanitize_dialog
    sanitize_dialog is under development and currently not used from
    anywhere, but //chrome/browser and //chrome/browser/ui builds it with
    this cl as one of allow_circular_includes_from target.
    
    Bug: b:364155590
    Change-Id: If43b5762e1fb93afd633be9cd86ff80da0eb379f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832767
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351183}

--- chrome/browser/ui/webui/ash/sanitize_dialog/sanitize_dialog.h
+++ chrome/browser/ui/webui/ash/sanitize_dialog/sanitize_dialog.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_SANITIZE_DIALOG_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_SANITIZE_DIALOG_H_
 
 #include "chrome/browser/ui/webui/ash/system_web_dialog/system_web_dialog_delegate.h"
 #include "ui/gfx/native_widget_types.h"
@@ -50,4 +50,4 @@ class SanitizeDialog : public SystemWebDialogDelegate {
 };
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_SANITIZE_DIALOG_SANITIZE_DIALOG_H_
