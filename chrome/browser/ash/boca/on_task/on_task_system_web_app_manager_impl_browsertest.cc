ac30313e70ccc2164cd29f510ff1566194fc7538
caott <caott@google.com>
2024-09-05 01:19:22 +0000

    [OnTask] Add navigation observer to browsertest to fix flakiness.
    https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/44552/overview
    
    Bug: b/353758782
    Change-Id: I440ef2008a283e31b5d79a968d6a0ccb83a7b19b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836750
    Reviewed-by: Matthew Zhu <zhumatthew@google.com>
    Commit-Queue: Luke Cao <caott@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351159}

--- chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl_browsertest.cc
+++ chrome/browser/ash/boca/on_task/on_task_system_web_app_manager_impl_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/test/browser_test.h"
+#include "content/public/test/test_navigation_observer.h"
 #include "content/public/test/test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "url/gurl.h"
@@ -144,12 +145,12 @@ IN_PROC_BROWSER_TEST_F(OnTaskSystemWebAppManagerImplBrowserTest,
   // Create tab from the url and verify that Boca has the tab.
   system_web_app_manager.CreateBackgroundTabWithUrl(
       boca_app_browser->session_id(), GURL(kTestUrl));
-  content::RunAllTasksUntilIdle();
   EXPECT_EQ(boca_app_browser->tab_strip_model()->count(), 2);
-  EXPECT_EQ(boca_app_browser->tab_strip_model()
-                ->GetWebContentsAt(1)
-                ->GetLastCommittedURL(),
-            GURL(kTestUrl));
+  content::WebContents* web_contents =
+      boca_app_browser->tab_strip_model()->GetWebContentsAt(1);
+  content::TestNavigationObserver observer(web_contents);
+  observer.Wait();
+  EXPECT_EQ(web_contents->GetLastCommittedURL(), GURL(kTestUrl));
 }
 
 }  // namespace
