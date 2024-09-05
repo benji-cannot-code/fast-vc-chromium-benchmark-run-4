ab8494f2b98ea182fb6332b5b22788af2a71fc6b
Gayane Petrosyan <gayane@google.com>
2024-09-05 15:58:25 +0000

    [iOS] Add custom icon and blue dot view for toolbar
    Add new custom icon for tools menu button on toolbar that will appear
    at the same time as the blue dot.
    In this CL also add/remove the blue dot view.
    
    Bug: chromium:362309731, chromium:362309579
    Change-Id: Iae9d2ec5d7edd5d19294d1c24c5273c4fc1ccb9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5823430
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351455}

--- ios/chrome/browser/ui/toolbar/buttons/toolbar_button.mm
+++ ios/chrome/browser/ui/toolbar/buttons/toolbar_button.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/shared/ui/util/util_swift.h"
+#import "ios/chrome/browser/ui/toolbar/buttons/buttons_constants.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
 #import "ios/chrome/browser/ui/toolbar/public/toolbar_constants.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -17,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 const CGFloat kSpotlightSize = 38;
 const CGFloat kSpotlightCornerRadius = 7;
+const CGFloat kToolsMenuButtonImageSize = 35;
+const CGFloat kBlueDotSize = 10;
+const CGFloat kButtonImageInset = 3;
 }  // namespace
 
 @interface ToolbarButton () {
@@ -34,6 +38,8 @@ const CGFloat kSpotlightCornerRadius = 7;
 // iphHighlighted effect will be replacing the default image with this one,
 // instead of using tint color OR `self.spotlightView`.
 @property(nonatomic, strong) UIImage* IPHHighlightedImage;
+// View used to display the blue dot on the icon.
+@property(nonatomic, strong) UIView* blueDotView;
 @end
 
 @implementation ToolbarButton
@@ -120,14 +126,18 @@ const CGFloat kSpotlightCornerRadius = 7;
   [self updateTintColor];
 }
 
-- (void)hasBlueDot:(BOOL)hasBlueDot {
+- (void)setHasBlueDot:(BOOL)hasBlueDot {
   if (_hasBlueDot == hasBlueDot) {
     return;
   }
 
   _hasBlueDot = hasBlueDot;
 
-  // TODO(crbug.com/338249447): Add or remove the blue dot.
+  if (hasBlueDot) {
+    [self addBlueDotViewIfNeeded];
+  } else {
+    [self removeBlueDotViewIfNeeded];
+  }
 }
 
 #pragma mark - Accessors
@@ -242,6 +252,44 @@ const CGFloat kSpotlightCornerRadius = 7;
 // Whether there is an IPH highlighted image can be used.
 - (BOOL)canUseIPHHighlightedImage {
   return _IPHHighlightedImageLoader != nil;
+}
+
+// Adds blue dot view to the button if there is none yet.
+- (void)addBlueDotViewIfNeeded {
+  if (self.blueDotView) {
+    return;
+  }
+
+  self.blueDotView = [[UIView alloc] init];
+  self.blueDotView.translatesAutoresizingMaskIntoConstraints = NO;
+  self.blueDotView.accessibilityIdentifier = kToolbarButtonBlueDotViewID;
+  self.blueDotView.layer.cornerRadius = kBlueDotSize / 2;
+  self.blueDotView.backgroundColor = [UIColor colorNamed:kBlue600Color];
+  [self addSubview:self.blueDotView];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [self.blueDotView.widthAnchor constraintEqualToConstant:kBlueDotSize],
+    [self.blueDotView.heightAnchor constraintEqualToConstant:kBlueDotSize],
+    // Position the blue dot at right top corner of the button image.
+    [self.blueDotView.centerXAnchor
+        constraintEqualToAnchor:self.centerXAnchor
+                       constant:kToolsMenuButtonImageSize / 2 -
+                                kButtonImageInset],
+    [self.blueDotView.centerYAnchor
+        constraintEqualToAnchor:self.centerYAnchor
+                       constant:-kToolsMenuButtonImageSize / 2 +
+                                kButtonImageInset],
+  ]];
+}
+
+// Removes blue dot view from the button if there is one.
+- (void)removeBlueDotViewIfNeeded {
+  if (!self.blueDotView) {
+    return;
+  }
+
+  [self.blueDotView removeFromSuperview];
+  self.blueDotView = nil;
 }
 
 @end
