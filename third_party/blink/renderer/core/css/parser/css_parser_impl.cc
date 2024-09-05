35f794b2f8ba2fcb043d4f466191d07ebaa1206c
Steinar H. Gunderson <sesse@chromium.org>
2024-09-05 19:23:02 +0000

    Change error recovery into using the streaming parser.
    These strictly speaking were using the streaming parser before,
    but the members functions returned non-streaming token ranges
    (which also will disappear), so they need to go.
    
    Change-Id: I50ba8ab4ede89c2591ae4dcee8f950cc1aedf031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839471
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351626}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.cc
@@ -2633,12 +2633,11 @@ StyleRule* CSSParserImpl::ConsumeStyleRule(
   if (selector_vector.empty()) {
     // Read the rest of the prelude if there was an error
     stream.EnsureLookAhead();
-    while (!stream.UncheckedAtEnd() &&
-           stream.UncheckedPeek().GetType() != kLeftBraceToken &&
-           !AbortsNestedSelectorParsing(stream.UncheckedPeek().GetType(),
-                                        semicolon_aborts_nested_selector,
-                                        nesting_type)) {
-      stream.UncheckedConsumeComponentValue();
+    if (semicolon_aborts_nested_selector &&
+        nesting_type != CSSNestingType::kNone) {
+      stream.SkipUntilPeekedTypeIs<kLeftBraceToken, kSemicolonToken>();
+    } else {
+      stream.SkipUntilPeekedTypeIs<kLeftBraceToken>();
     }
   }
 
@@ -2877,11 +2876,7 @@ void CSSParserImpl::ConsumeDeclarationList(
         // Function tokens should start parsing a declaration
         // (which then immediately goes into error recovery mode).
       case CSSParserTokenType::kFunctionToken:
-        while (!stream.UncheckedAtEnd() &&
-               stream.UncheckedPeek().GetType() != kSemicolonToken) {
-          stream.UncheckedConsumeComponentValue();
-        }
-
+        stream.SkipUntilPeekedTypeIs<kSemicolonToken>();
         if (!stream.UncheckedAtEnd()) {
           stream.UncheckedConsume();  // kSemicolonToken
         }
