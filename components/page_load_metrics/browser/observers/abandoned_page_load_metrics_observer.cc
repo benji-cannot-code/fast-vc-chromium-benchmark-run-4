5f09d9393f2c04899b8ecc96b5f35548b933b3a2
Shunya Shishido <sisidovski@chromium.org>
2024-09-05 09:09:06 +0000

    Fix duplicated prefix for TimeToAbandonFromNavigationStart UMA
    There was a careless mistake in TimeToAbandonFromNavigationStart
    metrics. The prefix part `PageLoad.Clients.Leakage2.` was duplicated and
    that makes us unavailable to see the logs in the dashboard.
    
    Removed the duplicated prefix and update the test more strict.
    
    Bug: 360903474
    Change-Id: I3ab070e16fbd9ad29bccbfd936baf48aff175ae7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836645
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Auto-Submit: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351290}

--- components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.cc
@@ -322,8 +322,7 @@ AbandonedPageLoadMetricsObserver::GetMilestoneHistogramNameWithoutPrefixSuffix(
 std::string AbandonedPageLoadMetricsObserver::
     GetTimeToAbandonFromNavigationStartWithoutPrefixSuffix(
         NavigationMilestone milestone) {
-  return std::string(internal::kAbandonedPageLoadMetricsHistogramPrefix) +
-         internal::kTimeToAbandonFromNavigationStart +
+  return internal::kTimeToAbandonFromNavigationStart +
          NavigationMilestoneToString(milestone);
 }
 
