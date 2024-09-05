76b512055ec60bcf646b0d99721fc856af15dce0
Gayane Petrosyan <gayane@google.com>
2024-09-05 12:42:19 +0000

    [iOS] Wait for FET to initialize for displaying toolbar button blue dot.
    Bug: chromium:362731656
    Change-Id: I084a9b3097e03dbf2bf0edaf84425d9cff292b95
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829971
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Gayane Petrosyan <gayane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351362}

--- ios/chrome/browser/ui/popup_menu/popup_menu_help_coordinator.mm
+++ ios/chrome/browser/ui/popup_menu/popup_menu_help_coordinator.mm
@@ -301,7 +301,7 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
   self.popupMenuBubblePresenter = nil;
 }
 
-- (void)prepareToShowPopupMenuBubble {
+- (void)prepareToShowPopupMenuIPHs {
   // There must be a feature engagment tracker to show a bubble.
   if (!self.featureEngagementTracker) {
     return;
@@ -316,7 +316,7 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
           if (!success) {
             return;
           }
-          [weakSelf prepareToShowPopupMenuBubble];
+          [weakSelf showPopupMenuIPHs];
         }));
     return;
   }
@@ -326,11 +326,16 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
                                  kPromoDisplayDelayForTests.InNanoseconds()),
                    dispatch_get_main_queue(), ^{
-                     [weakSelf showPopupMenuBubbleIfNecessary];
+                     [weakSelf showPopupMenuIPHs];
                    });
   } else {
-    [self showPopupMenuBubbleIfNecessary];
+    [self showPopupMenuIPHs];
   }
+}
+
+- (void)showPopupMenuIPHs {
+  [self showPopupMenuBubbleIfNecessary];
+  [self updateBlueDotVisibility];
 }
 
 - (void)showPopupMenuBubbleIfNecessary {
@@ -482,8 +487,7 @@ base::TimeDelta kPromoDisplayDelayForTests = base::Seconds(1);
   if (level <= SceneActivationLevelBackground) {
     self.inSessionWithHistoryMenuItemIPH = NO;
   } else if (level >= SceneActivationLevelForegroundActive) {
-    [self prepareToShowPopupMenuBubble];
-    [self updateBlueDotVisibility];
+    [self prepareToShowPopupMenuIPHs];
   }
 }
 
