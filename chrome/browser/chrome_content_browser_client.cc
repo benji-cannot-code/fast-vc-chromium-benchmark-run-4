f9d81a25af2027360cfe30841f64e99a6f97e47b
Alex Gough <ajgo@chromium.org>
2024-09-05 20:53:12 +0000

    Make PDF font proxy default and remove feature
    PDFs now get non-embedded fonts via a skia/mojo proxy
    rather than using "direct" access via sandbox hooks &
    sandbox IPCs.
    
    This CL removes the WinPdfUseFontProxy feature and
    associated plumbing content code for allowing access to the
    local fonts dir.
    
    Bug: 344643689
    Change-Id: Idbb5a9ef2e6c24a4f1c08c6505437c9ca1696096
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805910
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351684}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -713,9 +713,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/pdf/browser/pdf_navigation_throttle.h"
 #include "components/pdf/browser/pdf_url_loader_request_interceptor.h"
 #include "components/pdf/common/constants.h"
-#if BUILDFLAG(IS_WIN)
-#include "pdf/pdf_features.h"
-#endif  // BUILDFLAG(IS_WIN)
 #endif  // BUILDFLAG(ENABLE_PDF)
 
 
@@ -5231,15 +5228,6 @@ bool ChromeContentBrowserClient::IsRendererCodeIntegrityEnabled() {
   // controlled pref being set to false.
   return !local_state->HasPrefPath(prefs::kRendererCodeIntegrityEnabled) ||
          local_state->GetBoolean(prefs::kRendererCodeIntegrityEnabled);
-}
-
-bool ChromeContentBrowserClient::IsPdfFontProxyEnabled() {
-#if BUILDFLAG(ENABLE_PDF)
-  return base::FeatureList::IsEnabled(
-      chrome_pdf::features::kWinPdfUseFontProxy);
-#else
-  return false;
-#endif
 }
 
 // Note: Only use sparingly to add Chrome specific sandbox functionality here.
--- pdf/pdfium/pdfium_font_win_unittest.cc
+++ pdf/pdfium/pdfium_font_win_unittest.cc
@@ -9,8 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/heap_array.h"
 #include "base/memory/raw_ptr.h"
-#include "base/test/scoped_feature_list.h"
-#include "pdf/pdf_features.h"
 #include "pdf/pdfium/pdfium_engine.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/pdfium/public/fpdf_sysfontinfo.h"
@@ -27,9 +25,7 @@ class PDFiumFontWinTest : public testing::Test {
   // Secretly this is a skia typeface id.
   using FontId = void*;
 
-  PDFiumFontWinTest() {
-    scoped_feature_list_.InitWithFeatures({features::kWinPdfUseFontProxy}, {});
-  }
+  PDFiumFontWinTest() = default;
   PDFiumFontWinTest(const PDFiumFontWinTest&) = delete;
   PDFiumFontWinTest& operator=(const PDFiumFontWinTest&) = delete;
   ~PDFiumFontWinTest() override = default;
@@ -69,7 +65,6 @@ class PDFiumFontWinTest : public testing::Test {
 
  private:
   raw_ptr<FPDF_SYSFONTINFO> mapper_;
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 }  // namespace
