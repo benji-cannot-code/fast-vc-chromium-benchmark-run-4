a37d2aecc0600d2a6db64ac1950ab93f995eb524
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 01:28:54 +0000

    ash: Move edu_account_login_handler* to //c/b/ui/webui/signin/ash
    Bug: b:363859211
    Change-Id: I88df28fe8c99a54001f5dd9014d6cc98533c5223
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831129
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351162}

--- chrome/browser/ui/webui/signin/ash/edu_account_login_handler.h
+++ chrome/browser/ui/webui/signin/ash/edu_account_login_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_SIGNIN_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
+#define CHROME_BROWSER_UI_WEBUI_SIGNIN_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
 
 #include <map>
 #include <memory>
@@ -164,4 +164,4 @@ class EduAccountLoginHandler : public content::WebUIMessageHandler,
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_SIGNIN_ASH_EDU_ACCOUNT_LOGIN_HANDLER_H_
