5b39e8c5d80ead78025b18f31392d508d906711e
Nohemi Fernandez <fernandex@chromium.org>
2024-09-05 08:50:32 +0000

    Remove obsolete histogram recording in supervision fetcher metrics.
    The API and access token latency metrics were used for targeted feature analysis and are no longer in use following the completion of the feature experiment. Remove these metrics to allow for a more generic metrics tracking system in future iterations of this code.
    
    Bug: b:363202182
    Change-Id: I6debc59c7f43a6dcae78ba16094b0b688fb1f15f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836821
    Reviewed-by: Tomek Jurkiewicz <tju@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Nohemi Fernandez <fernandex@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351260}

--- components/supervised_user/core/browser/proto_fetcher_unittest.cc
+++ components/supervised_user/core/browser/proto_fetcher_unittest.cc
@@ -485,13 +485,6 @@ TEST_P(ProtoFetcherTest, RecordsMetrics) {
         base::StrCat({*GetConfig().histogram_basename, ".Latency"}),
         /*expected_count(grew by)*/ 1);
     histogram_tester.ExpectTotalCount(
-        base::StrCat(
-            {*GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
-        /*expected_count(grew by)*/ 1);
-    histogram_tester.ExpectTotalCount(
-        base::StrCat({*GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
-        /*expected_count(grew by)*/ 1);
-    histogram_tester.ExpectTotalCount(
         base::StrCat({*GetConfig().histogram_basename, ".NoError.Latency"}),
         /*expected_count(grew by)*/ 1);
 
@@ -575,16 +568,6 @@ TEST_P(ProtoFetcherTest, RetryingFetcherTerminatesOnOkStatusAndRecordsMetrics) {
         base::StrCat({*GetConfig().histogram_basename, ".NoError.Latency"}),
         /*expected_count(grew by)*/ 1);
 
-    // System made it through access token phase three times.
-    histogram_tester.ExpectTotalCount(
-        base::StrCat(
-            {*GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
-        /*expected_count(grew by)*/ 3);
-    // Only one successful api call.
-    histogram_tester.ExpectTotalCount(
-        base::StrCat({*GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
-        /*expected_count(grew by)*/ 1);
-
     histogram_tester.ExpectTotalCount(
         base::StrCat(
             {*GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
@@ -666,16 +649,6 @@ TEST_P(ProtoFetcherTest,
         base::StrCat({*GetConfig().histogram_basename, ".ParseError.Latency"}),
         /*expected_count(grew by)*/ 1);
 
-    // System made it through access token phase two times.
-    histogram_tester.ExpectTotalCount(
-        base::StrCat(
-            {*GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
-        /*expected_count(grew by)*/ 2);
-    // Only one successful api call (parse error is a successful api call).
-    histogram_tester.ExpectTotalCount(
-        base::StrCat({*GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
-        /*expected_count(grew by)*/ 1);
-
     histogram_tester.ExpectTotalCount(
         base::StrCat(
             {*GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
@@ -745,16 +718,6 @@ TEST_P(ProtoFetcherTest, RetryingFetcherContinuesOnTransientError) {
         base::StrCat(
             {*GetConfig().histogram_basename, ".HttpStatusOrNetError.Latency"}),
         /*expected_count(grew by)*/ 2);
-
-    // System made it through access token phase two times.
-    histogram_tester.ExpectTotalCount(
-        base::StrCat(
-            {*GetConfig().histogram_basename, ".NONE.AccessTokenLatency"}),
-        /*expected_count(grew by)*/ 3);
-    // Server only responds with error.
-    histogram_tester.ExpectTotalCount(
-        base::StrCat({*GetConfig().histogram_basename, ".HTTP_OK.ApiLatency"}),
-        /*expected_count(grew by)*/ 0);
 
     EXPECT_THAT(
         histogram_tester.GetAllSamples(
