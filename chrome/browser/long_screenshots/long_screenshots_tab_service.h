bc4eb42637b73847bc61b4ecb32b9e6e188a49d5
Avi Drissman <avi@chromium.org>
2024-09-05 16:42:41 +0000

    Use an opaque type for FrameTreeNode IDs, part 35
    chrome/browser/long_screenshots
    
    Bug: 361344235
    Change-Id: Idd1ace9b8e94b399a465c7c0fdad3d14b6f569e4
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835584
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351493}

--- chrome/browser/long_screenshots/long_screenshots_tab_service.h
+++ chrome/browser/long_screenshots/long_screenshots_tab_service.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "components/paint_preview/browser/paint_preview_base_service.h"
 #include "components/paint_preview/browser/paint_preview_policy.h"
+#include "content/public/browser/frame_tree_node_id.h"
 #include "third_party/re2/src/re2/re2.h"
 
 namespace content {
@@ -102,7 +103,7 @@ class LongScreenshotsTabService
   // (confirming that the contents are alive using the |frame_routing_id|).
   // Calls PaintPreviewBaseService to retrieve the bitmap and write it to file.
   void CaptureTabInternal(int tab_id,
-                          int frame_tree_node_id,
+                          content::FrameTreeNodeId frame_tree_node_id,
                           content::GlobalRenderFrameHostId frame_routing_id,
                           int clip_x,
                           int clip_y,
