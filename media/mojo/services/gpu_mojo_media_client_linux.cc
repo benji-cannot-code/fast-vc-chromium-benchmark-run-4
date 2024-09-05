53eebe8c82ddbeab1758fe78abdc5dcdfe8a203f
Robert Mader <robert.mader@collabora.com>
2024-09-05 23:56:20 +0000

    media: Add feature to allow zero-copy video formats with GL on Linux
    With EGL formats like NV12 can usually be imported directly from VA-API
    and V4L2 decoders. Doing so can improve performance a lot - and in cases
    where there is no PP to convert to RGB, not supporting it can break HW
    decoding altogether.
    Unfortunately allowing it unconditionally could cause regressions in
    various scenarios - such as when using GLX or a driver that doesn't
    support sampling NV12.
    
    Thus introduce a feature - disabled by default for now - to make it easy
    to enable these formats.
    
    Note that on Wayland it might be required to disable overlay delegation
    with `--disable-features=WaylandOverlayDelegation`.
    
    Change-Id: I8ee396de5f6d4958278b0f0bd53bfa9a7007c8c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5038617
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Andres Calderon Jaramillo <andrescj@chromium.org>
    Commit-Queue: Andres Calderon Jaramillo <andrescj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351792}

--- media/mojo/services/gpu_mojo_media_client_linux.cc
+++ media/mojo/services/gpu_mojo_media_client_linux.cc
@@ -19,6 +19,10 @@ namespace media {
 
 namespace {
 
+BASE_FEATURE(kVaapiVideoDecodeLinuxZeroCopyGL,
+             "VaapiVideoDecodeLinuxZeroCopyGL",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 VideoDecoderType GetPreferredLinuxDecoderImplementation() {
   // VaapiVideoDecoder flag is required for VaapiVideoDecoder.
   if (!base::FeatureList::IsEnabled(kVaapiVideoDecodeLinux)) {
@@ -51,8 +55,15 @@ std::vector<Fourcc> GetPreferredRenderableFourccs(
   if (gpu_preferences.gr_context_type == gpu::GrContextType::kVulkan) {
     renderable_fourccs.emplace_back(Fourcc::NV12);
     renderable_fourccs.emplace_back(Fourcc::P010);
-  }
+  } else
 #endif  // BUILDFLAG(ENABLE_VULKAN)
+    // Allow zero-copy formats with GL for testing or in controlled
+    // environments.
+    if (gpu_preferences.gr_context_type == gpu::GrContextType::kGL &&
+        base::FeatureList::IsEnabled(kVaapiVideoDecodeLinuxZeroCopyGL)) {
+      renderable_fourccs.emplace_back(Fourcc::NV12);
+      renderable_fourccs.emplace_back(Fourcc::P010);
+    }
 
   // Support 1-copy argb textures.
   //
