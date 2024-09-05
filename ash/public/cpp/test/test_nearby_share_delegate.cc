40094aca6b06f74c93ebbe356c3437e3e44b12b9
Brando Socarras <brandosocarras@google.com>
2024-09-05 19:02:03 +0000

    [Nearby Share] Nearby Share delegate sets visibility
    Necessary for Quick Share v2 tile function: tile and detailed view
    need to set the device's visibility.
    
    Tested: utest in this.
    Bug: b/339703396
    Change-Id: I112871e7a2c938082b05aa3fc8be9c29c6410c18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807462
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351611}

--- ash/public/cpp/test/test_nearby_share_delegate.cc
+++ ash/public/cpp/test/test_nearby_share_delegate.cc
@@ -61,4 +61,9 @@ std::u16string TestNearbyShareDelegate::GetPlaceholderFeatureName() const {
   return visibility_;
 }
 
+void TestNearbyShareDelegate::SetVisibility(
+    ::nearby_share::mojom::Visibility visibility) {
+  visibility_ = visibility;
+}
+
 }  // namespace ash
