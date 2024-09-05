141d66398efce06f5584882052b4a1300743155a
Kyungjun Lee <kyungjunlee@google.com>
2024-09-05 21:42:57 +0000

    Enable two disabled crosier tests due to screen-ai
    This CL re-enables SpokenFeedbackIntegrationTest.KeyboardShortcut and
    AshIntegrationTest.Crosh tests that were disabled due to the channel
    limitation hit by the 126.5 screen-ai.
    
    This channel limitation is fixed in crrev.com/c/5837999.
    
    AX-Relnotes: n/a.
    Bug: 362178916,362289622
    Test: manually run the tests on DUT on CrOS 16011 with the change made in crrev.com/c/5837999.
    Change-Id: Ieef13a1a96b188c199dc107c6a86bdb3c9d5bb70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836028
    Commit-Queue: Kyungjun Lee <kyungjunlee@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351693}

--- chrome/browser/ash/system_web_apps/apps/terminal_integration_test.cc
+++ chrome/browser/ash/system_web_apps/apps/terminal_integration_test.cc
@@ -17,9 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// TODO(b/362289622): Re-enable this test after fallback for chrome
-// screen ai has been implemented.
-IN_PROC_BROWSER_TEST_F(AshIntegrationTest, DISABLED_Crosh) {
+IN_PROC_BROWSER_TEST_F(AshIntegrationTest, Crosh) {
   ui::ScopedAnimationDurationScaleMode zero_duration(
       ui::ScopedAnimationDurationScaleMode::ZERO_DURATION);
 
