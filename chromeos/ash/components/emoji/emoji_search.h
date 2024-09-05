f9233b462e389f06accb476a2bb2dd833aff7772
mlcui <mlcui@google.com>
2024-09-05 04:12:49 +0000

    emoji_search: Store keywords as UTF-16
    This allows us to remove two UTF-8 / UTF-16 conversions.
    
    Bug: b:357772460
    Change-Id: I6a6a21002f63cba6225432cb1e4dd69d94f0bb3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833076
    Reviewed-by: Darren Shen <shend@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351216}

--- chromeos/ash/components/emoji/emoji_search.h
+++ chromeos/ash/components/emoji/emoji_search.h
@@ -39,7 +39,7 @@ struct EmojiSearchResult {
 };
 
 using EmojiEntryMap =
-    std::map<std::string, std::vector<EmojiSearchEntry>, std::less<>>;
+    std::map<std::u16string, std::vector<EmojiSearchEntry>, std::less<>>;
 
 enum class EmojiLanguageCode {
   kDa,  // Danish
