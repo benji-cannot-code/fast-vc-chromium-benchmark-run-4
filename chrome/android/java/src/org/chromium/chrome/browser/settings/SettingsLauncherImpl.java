ae86ebe916691d26cdfdfb752c1afcf0446e7051
Shuhei Takahashi <nya@chromium.org>
2024-09-05 23:48:01 +0000

    [Settings] Introduce a central place to construct intents
    SettingsIntentUtil becomes the central place to construct intents
    launching the settings activity.
    
    Bug: b/356743945
    Change-Id: Id6988af4a182fe6603bb02c9dae8b2ac48f80fed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832868
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1351789}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncherImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsLauncherImpl.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.settings;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -98,19 +97,8 @@ public class SettingsLauncherImpl implements SettingsLauncher {
             Context context,
             @Nullable Class<? extends Fragment> fragment,
             @Nullable Bundle fragmentArgs) {
-        Intent intent = new Intent();
-        intent.setClass(context, SettingsActivity.class);
-        if (!(context instanceof Activity)) {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        }
-        if (fragment != null) {
-            intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT, fragment.getName());
-        }
-        if (fragmentArgs != null) {
-            intent.putExtra(SettingsActivity.EXTRA_SHOW_FRAGMENT_ARGUMENTS, fragmentArgs);
-        }
-        return intent;
+        String fragmentName = fragment == null ? null : fragment.getName();
+        return SettingsIntentUtil.createIntent(context, fragmentName, fragmentArgs);
     }
 
     @Override
