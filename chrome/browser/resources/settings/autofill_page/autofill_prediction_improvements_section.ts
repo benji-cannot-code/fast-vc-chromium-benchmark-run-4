d5060850aac3ff626e2aebd7b40eaa458034f75d
Dmitry Vykochko <vykochko@google.com>
2024-09-05 09:09:17 +0000

    Add content describing the purpose of improved predictions settings.
    What was added:
    https://screenshot.googleplex.com/7WYYnVzqihwBuwt
    
    Bug: 361437117
    Change-Id: Ifbf4984feb05e5e6f9c5b9e17d1742c89464efcc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836857
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351291}

--- chrome/browser/resources/settings/autofill_page/autofill_prediction_improvements_section.ts
+++ chrome/browser/resources/settings/autofill_page/autofill_prediction_improvements_section.ts
@@ -8,6 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * the section containing configuration options for prediction improvements.
  */
 
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
+import 'chrome://resources/cr_elements/cr_shared_style.css.js';
+import '../icons.html.js';
+import '../settings_columned_section.css.js';
+
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './autofill_prediction_improvements_section.html.js';
