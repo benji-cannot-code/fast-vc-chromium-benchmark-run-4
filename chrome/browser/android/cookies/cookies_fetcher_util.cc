f4f7d6d9933b7281eed716091bf92fc1e9db4139
Zaina Al-Mashni <zalmashni@google.com>
2024-09-05 22:04:28 +0000

    eCCT: Move cookies/ out of profile/
    profile <-> cookies bi-directional dependency no longer exists:
    crrev.com/c/5827215
    
    This change moves cookies/* out of c/b/profile in preparation to
    introduce a profile dependency in crypto/
    
    Bug: 360159401
    Change-Id: I0fdc4df49583793dab28207a2e132337c95f45b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839680
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351730}

--- chrome/browser/android/cookies/cookies_fetcher_util.cc
+++ chrome/browser/android/cookies/cookies_fetcher_util.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/network/public/mojom/cookie_manager.mojom.h"
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
-#include "chrome/browser/profiles/android/jni_headers/CookiesFetcher_jni.h"
+#include "chrome/browser/android/cookies/jni_headers/CookiesFetcher_jni.h"
 
 using base::android::JavaParamRef;
 using base::android::ScopedJavaLocalRef;
