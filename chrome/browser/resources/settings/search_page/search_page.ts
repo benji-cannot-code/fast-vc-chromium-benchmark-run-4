9fc4baef075cf6974555eda50fd28af2830bada4
Jack Yammine <jyammine@google.com>
2024-09-05 09:02:02 +0000

    [waffle]Cleanup settings ui after feature launch
    The kSearchEngineChoiceTrigger feature is now fully launched.
    We previously had 2 UI versions for chrome://settings/search. We
    remove the version that was displayed when the feature is disabled.
    
    Fixed: b:362453946
    Change-Id: Iea5a043449e4919adde8e40939c1ec4289a41649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836818
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Auto-Submit: Jack Yammine <jyammine@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351279}

--- chrome/browser/resources/settings/search_page/search_page.ts
+++ chrome/browser/resources/settings/search_page/search_page.ts
@@ -11,7 +11,6 @@ import '/shared/settings/controls/cr_policy_pref_indicator.js';
 import 'chrome://resources/cr_elements/cr_shared_style.css.js';
 import 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
-import 'chrome://resources/cr_elements/md_select.css.js';
 import 'chrome://resources/polymer/v3_0/iron-flex-layout/iron-flex-layout-classes.js';
 import '/shared/settings/controls/extension_controlled_indicator.js';
 import '../settings_page/settings_animated_pages.js';
@@ -23,7 +22,6 @@ import '../site_favicon.js';
 import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
 import {WebUiListenerMixin} from 'chrome://resources/cr_elements/web_ui_listener_mixin.js';
-import {assert} from 'chrome://resources/js/assert.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {BaseMixin} from '../base_mixin.js';
@@ -31,7 +29,7 @@ import {loadTimeData} from '../i18n_setup.js';
 import {routes} from '../route.js';
 import {Router} from '../router.js';
 import type {SearchEngine, SearchEnginesBrowserProxy, SearchEnginesInfo} from '../search_engines_page/search_engines_browser_proxy.js';
-import {ChoiceMadeLocation, SearchEnginesBrowserProxyImpl} from '../search_engines_page/search_engines_browser_proxy.js';
+import {SearchEnginesBrowserProxyImpl} from '../search_engines_page/search_engines_browser_proxy.js';
 
 import {getTemplate} from './search_page.html.js';
 
@@ -55,14 +53,6 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
        * List of search engines available.
        */
       searchEngines_: Array,
-
-      // Whether the `SearchEngineChoiceTrigger` feature is enabled.
-      searchEngineChoiceSettingsUi_: {
-        type: Boolean,
-        value() {
-          return loadTimeData.getBoolean('searchEngineChoiceSettingsUi');
-        },
-      },
 
       // Whether we need to set the icon size to large because they are loaded
       // in the binary or smaller because we get them from the favicon service.
@@ -96,7 +86,6 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
   prefs: Object;
   private searchEngines_: SearchEngine[];
   private searchEnginesFilter_: string;
-  private searchEngineChoiceSettingsUi_: boolean;
   private showSearchEngineListDialog_: boolean;
   private defaultSearchEngine_: SearchEngine|null;
   private focusConfig_: Map<string, string>|null;
@@ -127,15 +116,6 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
     this.setFaviconSize_();
   }
 
-  private onChange_() {
-    assert(!this.searchEngineChoiceSettingsUi_);
-    const select = this.shadowRoot!.querySelector('select');
-    assert(select);
-    const searchEngine = this.searchEngines_[select.selectedIndex];
-    this.browserProxy_.setDefaultSearchEngine(
-        searchEngine.modelIndex, ChoiceMadeLocation.SEARCH_SETTINGS);
-  }
-
   private onDisableExtension_() {
     this.dispatchEvent(new CustomEvent('refresh-pref', {
       bubbles: true,
@@ -160,7 +140,7 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
   }
 
   private computeDefaultSearchEngine_() {
-    if (!this.searchEngines_.length || !this.searchEngineChoiceSettingsUi_) {
+    if (!this.searchEngines_.length) {
       return null;
     }
 
@@ -168,7 +148,6 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
   }
 
   private onOpenDialogButtonClick_() {
-    assert(this.searchEngineChoiceSettingsUi_);
     this.showSearchEngineListDialog_ = true;
     chrome.metricsPrivate.recordUserAction('ChooseDefaultSearchEngine');
   }
@@ -181,7 +160,6 @@ export class SettingsSearchPageElement extends SettingsSearchPageElementBase {
   }
 
   private onSearchEngineListDialogClose_() {
-    assert(this.searchEngineChoiceSettingsUi_);
     this.showSearchEngineListDialog_ = false;
   }
 
