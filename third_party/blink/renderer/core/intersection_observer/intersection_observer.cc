5862e088e4bf7ca23567088f8a6dd4b36329a064
Steinar H. Gunderson <sesse@chromium.org>
2024-09-05 08:54:03 +0000

    Convert ParseMargin() to the streaming parser.
    This is one of the last holdouts for the non-streaming parser before we
    can remove it.
    
    Change-Id: Id527cf5af20c282bd093aa9567efd2d965839f85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836680
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351266}

--- third_party/blink/renderer/core/intersection_observer/intersection_observer.cc
+++ third_party/blink/renderer/core/intersection_observer/intersection_observer.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/v8_intersection_observer_init.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_document_element.h"
 #include "third_party/blink/renderer/bindings/core/v8/v8_union_double_doublesequence.h"
-#include "third_party/blink/renderer/core/css/parser/css_parser_token_range.h"
+#include "third_party/blink/renderer/core/css/parser/css_parser_token_stream.h"
 #include "third_party/blink/renderer/core/css/parser/css_tokenizer.h"
 #include "third_party/blink/renderer/core/dom/element.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
@@ -42,7 +42,6 @@ namespace {
 // IntersectionObserver with an EventCallback.
 class IntersectionObserverDelegateImpl final
     : public IntersectionObserverDelegate {
-
  public:
   IntersectionObserverDelegateImpl(
       ExecutionContext* context,
@@ -102,11 +101,9 @@ void ParseMargin(const String& margin_parameter,
   // "1px 2px 3px" = top left/right bottom
   // "1px 2px 3px 4px" = top left right bottom
 
-  CSSTokenizer tokenizer(margin_parameter);
-  const auto tokens = tokenizer.TokenizeToEOF();
-  CSSParserTokenRange token_range(tokens);
-  token_range.ConsumeWhitespace();
-  while (token_range.Peek().GetType() != kEOFToken &&
+  CSSParserTokenStream stream(margin_parameter);
+  stream.ConsumeWhitespace();
+  while (stream.Peek().GetType() != kEOFToken &&
          !exception_state.HadException()) {
     if (margin.size() == 4) {
       exception_state.ThrowDOMException(
@@ -114,10 +111,11 @@ void ParseMargin(const String& margin_parameter,
           "Extra text found at the end of " + marginName + "Margin.");
       break;
     }
-    const CSSParserToken& token = token_range.ConsumeIncludingWhitespace();
+    const CSSParserToken token = stream.Peek();
     switch (token.GetType()) {
       case kPercentageToken:
         margin.push_back(Length::Percent(token.NumericValue()));
+        stream.ConsumeIncludingWhitespace();
         break;
       case kDimensionToken:
         switch (token.GetUnitType()) {
@@ -133,6 +131,7 @@ void ParseMargin(const String& margin_parameter,
                 DOMExceptionCode::kSyntaxError,
                 marginName + "Margin must be specified in pixels or percent.");
         }
+        stream.ConsumeIncludingWhitespace();
         break;
       default:
         exception_state.ThrowDOMException(
@@ -142,10 +141,9 @@ void ParseMargin(const String& margin_parameter,
   }
 }
 
-void ParseThresholds(
-    const V8UnionDoubleOrDoubleSequence* threshold_parameter,
-    Vector<float>& thresholds,
-    ExceptionState& exception_state) {
+void ParseThresholds(const V8UnionDoubleOrDoubleSequence* threshold_parameter,
+                     Vector<float>& thresholds,
+                     ExceptionState& exception_state) {
   switch (threshold_parameter->GetContentType()) {
     case V8UnionDoubleOrDoubleSequence::ContentType::kDouble:
       thresholds.push_back(
