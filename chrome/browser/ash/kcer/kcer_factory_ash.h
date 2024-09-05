7a284c58853b23d1e2bf7feb0643b1c79c5cf477
Mahmoud Rashad <mmrashad@google.com>
2024-09-05 12:57:53 +0000

    Revert "Kcer: Move chromeos/components/kcer to ash/components/kcer"
    This reverts commit a5ad0e0892886b39d1df441ea3bda54fcfb0b983.
    
    Reason for revert: http://b/364852517
    
    Original change's description:
    > Kcer: Move chromeos/components/kcer to ash/components/kcer
    >
    > Kcer is now only used in Ash and moving it will allow depending
    > on Ash specific directories and components.
    >
    > Bug: b:351347146
    > Test: Compilation
    > Change-Id: I58ad497f8064578718b31cc793e2bdf9ee4d6b3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803081
    > Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    > Commit-Queue: Gavin Williams <gavinwill@chromium.org>
    > Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    > Reviewed-by: Matt Mueller <mattm@chromium.org>
    > Auto-Submit: Michael Ershov <miersh@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1351324}
    
    Bug: b:364852517
    Change-Id: I5b33a695ad037bc02b12df269769684bc62b3042
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837228
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Owners-Override: Mahmoud Rashad <mmrashad@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1351366}

--- chrome/browser/ash/kcer/kcer_factory_ash.h
+++ chrome/browser/ash/kcer/kcer_factory_ash.h
@@ -6,15 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ASH_KCER_KCER_FACTORY_ASH_H_
 #define CHROME_BROWSER_ASH_KCER_KCER_FACTORY_ASH_H_
 
-#include "ash/components/kcer/chaps/session_chaps_client.h"
-#include "ash/components/kcer/kcer.h"
-#include "ash/components/kcer/kcer_token.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "base/feature_list.h"
 #include "base/no_destructor.h"
 #include "chrome/browser/ash/kcer/nssdb_migration/kcer_rollback_helper.h"
 #include "chrome/browser/profiles/profile_keyed_service_factory.h"
 #include "chromeos/ash/components/tpm/tpm_token_info_getter.h"
+#include "chromeos/components/kcer/chaps/session_chaps_client.h"
+#include "chromeos/components/kcer/kcer.h"
+#include "chromeos/components/kcer/kcer_token.h"
 
 class Profile;
 class PrefService;
