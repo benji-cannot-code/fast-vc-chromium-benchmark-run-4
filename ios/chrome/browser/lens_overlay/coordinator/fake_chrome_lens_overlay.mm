cd3d777bc8b34be75498e01de33a27b8d73daa3a
Radu Nitescu <radunitescu@google.com>
2024-09-05 20:28:34 +0000

    Revert "[iOS][LO] Update occlusion insets when the sheet is presented"
    This reverts commit 1b7a680c5746b6c64ff52a21bb396b50593122d4.
    
    Reason for revert: Change in internal did not land yet. Will re submit this CL when it lands
    
    Original change's description:
    > [iOS][LO] Update occlusion insets when the sheet is presented
    >
    > This ensures that selections in the bottom half of the screen don't
    > get covered by the sheet.
    >
    > Fixed: 364358220
    > Change-Id: I833ea3c7abd494352f756eb9cc3520be105b35ef
    > Low-Coverage-Reason: HARD_TO_TEST Interfaces with google3 Lens Codebase
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839598
    > Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    > Commit-Queue: Radu Nitescu <radunitescu@google.com>
    > Auto-Submit: Radu Nitescu <radunitescu@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1351552}
    
    Change-Id: I7e9edf46be5b04eed894c967dc8f8e4ff3ab6e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839725
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351670}

--- ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
+++ ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
@@ -68,12 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self sendNewResult];
 }
 
-- (void)setOcclusionInsets:(UIEdgeInsets)occlusionInsets
-                reposition:(BOOL)reposition
-                  animated:(BOOL)animated {
-  // NO-OP
-}
-
 #pragma mark - Public
 
 - (void)simulateSelectionUpdate {
--- ios/chrome/test/providers/lens/test_lens_overlay_controller.mm
+++ ios/chrome/test/providers/lens/test_lens_overlay_controller.mm
@@ -30,10 +30,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // NO-OP
 }
 
-- (void)setOcclusionInsets:(UIEdgeInsets)occlusionInsets
-                reposition:(BOOL)reposition
-                  animated:(BOOL)animated {
-  // NO-OP
-}
-
 @end
