4a092aade3dcb9c5a4359ce00dedfe4e560f68fd
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 02:39:32 +0000

    ash: Move web_ui_test_handler to //chrome/test/base/ash
    Bug: b:364449687
    Change-Id: I5a25c9b1a0c7d7ab11f11894a6013bc11bcae8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835439
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351190}

--- chrome/test/base/ash/web_ui_test_handler.h
+++ chrome/test/base/ash/web_ui_test_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_WEB_UI_TEST_HANDLER_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_WEB_UI_TEST_HANDLER_H_
+#ifndef CHROME_TEST_BASE_ASH_WEB_UI_TEST_HANDLER_H_
+#define CHROME_TEST_BASE_ASH_WEB_UI_TEST_HANDLER_H_
 
 #include <optional>
 #include <string>
@@ -80,4 +80,4 @@ class WebUITestHandler {
   base::RepeatingClosure quit_closure_;
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_WEB_UI_TEST_HANDLER_H_
+#endif  // CHROME_TEST_BASE_ASH_WEB_UI_TEST_HANDLER_H_
