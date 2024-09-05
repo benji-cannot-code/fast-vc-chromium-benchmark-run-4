750503befc1e2803e231be090fd9f2c97c59bcee
dpapad <dpapad@chromium.org>
2024-09-05 14:16:47 +0000

    [Waffle]: Post Lit migration cleanup, check-in generated html.ts files.
    Bug: 40943652
    Change-Id: Ie8e35a0d7351415810a8880de4310067bea5f1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836161
    Commit-Queue: Jack Yammine <jyammine@google.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Jack Yammine <jyammine@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351397}

--- chrome/browser/resources/search_engine_choice/app.html.ts
+++ chrome/browser/resources/search_engine_choice/app.html.ts
@@ -1,4 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+import type {AppElement} from './app.js';
+
+export function getHtml(this: AppElement) {
+  return html`<!--_html_template_start_-->
 <img class="tangible-sync-style-left-banner" id="leftBanner" alt="">
 <img class="tangible-sync-style-right-banner" id="rightBanner" alt="">
 <div class="content-container">
@@ -68,3 +77,5 @@ ${this.showInfoDialog_ ? html`
     </div>
   </cr-dialog>
 ` : ''}
+<!--_html_template_end_-->`;
+}
