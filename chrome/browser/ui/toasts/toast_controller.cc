b9683fd72e92c3a48f0b25e1645f3b18d74c349c
Charles Meng <charlesmeng@chromium.org>
2024-09-05 18:12:38 +0000

    Add toast close reason
    Adds the toast close reason enum that will eventually be logged to UMA
    (will be in a separate CL). This is because the toast close reason is
    needed for toast coordination and for determining whether to show the
    height animation when the toast closes.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE This CL is just replacing an argument to a function with another type.
    Bug: 358610872
    Change-Id: I153ac49b3fe7d103a03bd7d8e09035dcd623e2c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838742
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Reviewed-by: Steven Luong <stluong@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Auto-Submit: Charles Meng <charlesmeng@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1351571}

--- chrome/browser/ui/toasts/toast_controller.cc
+++ chrome/browser/ui/toasts/toast_controller.cc
@@ -35,9 +35,7 @@ ToastController::ToastController(
     : browser_window_interface_(browser_window_interface),
       toast_registry_(toast_registry) {}
 
-ToastController::~ToastController() {
-  CloseToast();
-}
+ToastController::~ToastController() = default;
 
 bool ToastController::IsShowingToast() const {
   return current_toast_params_.has_value();
@@ -67,7 +65,7 @@ bool ToastController::MaybeShowToast(ToastParams params) {
     return false;
   }
 
-  CloseToast();
+  CloseToast(toasts::ToastCloseReason::kPreempted);
 
   if (IsShowingToast()) {
     // TODO(crbug.com/358610190): Record that a toast was preempted if
@@ -88,14 +86,13 @@ void ToastController::ClosePersistentToast(ToastId id) {
   CHECK_EQ(current_toast_params_.value().toast_id_, id);
   // TODO(crbug.com/358610787): close the persistent toast and have internal
   // state reflect that.
-  CloseToast();
+  CloseToast(toasts::ToastCloseReason::kAborted);
 }
 
 void ToastController::OnWidgetDestroyed(views::Widget* widget) {
   current_toast_params_ = std::nullopt;
   toast_ = nullptr;
   toast_observer_.Reset();
-  toast_widget_ = nullptr;
   toast_close_timer_.Stop();
 
   // TODO(crbug.com/358610190): Record toast closed reason.
@@ -118,20 +115,20 @@ void ToastController::ShowToast(ToastParams params) {
   if (!current_toast_spec->is_persistent_toast()) {
     toast_close_timer_.Start(
         FROM_HERE, toast_features::kToastTimeout.Get(),
-        base::BindOnce(&ToastController::CloseToast, base::Unretained(this)));
+        base::BindOnce(&ToastController::CloseToast, base::Unretained(this),
+                       toasts::ToastCloseReason::kAutoDismissed));
   }
 
   CreateToast(current_toast_spec);
 }
 
-void ToastController::CloseToast() {
+void ToastController::CloseToast(toasts::ToastCloseReason reason) {
   if (!IsShowingToast()) {
     return;
   }
 
   CHECK(toast_);
-  CHECK(toast_widget_);
-  toast_->Close(views::Widget::ClosedReason::kUnspecified);
+  toast_->Close(reason);
 }
 
 void ToastController::CreateToast(const ToastSpecification* spec) {
@@ -154,10 +151,10 @@ void ToastController::CreateToast(const ToastSpecification* spec) {
         spec->action_button_callback());
   }
   toast_ = toast.get();
-  toast_widget_ =
+  views::Widget* const toast_widget =
       views::BubbleDialogDelegateView::CreateBubble(std::move(toast));
-  toast_observer_.Observe(toast_widget_);
-  toast_widget_->ShowInactive();
+  toast_observer_.Observe(toast_widget);
+  toast_widget->ShowInactive();
   // TODO(crbug.com/358615317): Make the toast animate in.
 }
 
