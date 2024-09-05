9492cde5eb7d7bc68a0d84d9635a50a385dfa5b3
David Bertoni <dbertoni@chromium.org>
2024-09-05 22:44:51 +0000

    [Extensions] Add a BUILD.gn file for the proxy API.
    This CL separates the prefs transformer out of the proxy API file.
    The transformer is not really a part of the API itself, and this
    allows adding a BUILD.gn for the API files.
    
    The other files will stay in this directory but will be built into
    //chrome/browser/extensions.
    
    This CL also cleans up some extraneous header files, but the new files
    are otherwise cut-and-paste from the existing files.
    
    Bug: 40593486
    Change-Id: I1449e709cdc620b9c16ec363452a5a52d7634eda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840586
    Auto-Submit: David Bertoni <dbertoni@chromium.org>
    Reviewed-by: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351761}

--- chrome/browser/extensions/chrome_extensions_browser_client.cc
+++ chrome/browser/extensions/chrome_extensions_browser_client.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/preference/network_prediction_transformer.h"
 #include "chrome/browser/extensions/api/preference/privacy_sandbox_transformer.h"
 #include "chrome/browser/extensions/api/preference/protected_content_enabled_transformer.h"
-#include "chrome/browser/extensions/api/proxy/proxy_api.h"
+#include "chrome/browser/extensions/api/proxy/proxy_pref_transformer.h"
 #include "chrome/browser/extensions/api/runtime/chrome_runtime_api_delegate.h"
 #include "chrome/browser/extensions/chrome_component_extension_resource_manager.h"
 #include "chrome/browser/extensions/chrome_content_browser_client_extensions_part.h"
