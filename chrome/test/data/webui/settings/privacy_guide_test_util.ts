b009a483554e6c2f17c39e2cba72b2cc5b58bfbc
Fiona Macintosh <fmacintosh@google.com>
2024-09-05 21:51:30 +0000

    [Settings] Remove legacy generated cookies pref from privacy guide
    This pref was originally used on the combined 3PC/1PC settings page.
    That combined 3PC/1PC settings page has been removed for almost a year
    now, and this is the only remaining usage of this pref. Once we clean
    this up we can remove the pref entirely.
    
    Change-Id: I37c50aa751ddd423284349fdc3e6fa4ed1eaf2d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833109
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1351708}

--- chrome/test/data/webui/settings/privacy_guide_test_util.ts
+++ chrome/test/data/webui/settings/privacy_guide_test_util.ts
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {webUIListenerCallback} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import type {SettingsPrivacyGuidePageElement} from 'chrome://settings/lazy_load.js';
-import {CookiePrimarySetting, PrivacyGuideStep, SafeBrowsingSetting} from 'chrome://settings/lazy_load.js';
+import {ContentSetting, CookieControlsMode, PrivacyGuideStep, SafeBrowsingSetting} from 'chrome://settings/lazy_load.js';
 import type {SettingsPrefsElement} from 'chrome://settings/settings.js';
 import {Router, routes, SignedInState, StatusAction} from 'chrome://settings/settings.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
@@ -74,11 +74,17 @@ export function setupSync({
   webUIListenerCallback('sync-prefs-changed', event);
 }
 
-// Set the cookies setting for the privacy guide.
-export function setCookieSetting(
-    page: SettingsPrivacyGuidePageElement,
-    setting: CookiePrimarySetting): void {
-  page.set('prefs.generated.cookie_primary_setting', {
+export function setFirstPartyCookieSetting(
+    page: SettingsPrivacyGuidePageElement, setting: ContentSetting) {
+  page.set('prefs.generated.cookie_default_content_setting', {
+    type: chrome.settingsPrivate.PrefType.STRING,
+    value: setting,
+  });
+}
+
+export function setThirdPartyCookieSetting(
+    page: SettingsPrivacyGuidePageElement, setting: CookieControlsMode): void {
+  page.set('prefs.profile.cookie_controls_mode', {
     type: chrome.settingsPrivate.PrefType.NUMBER,
     value: setting,
   });
@@ -86,9 +92,10 @@ export function setCookieSetting(
 
 export function shouldShowCookiesCard(page: SettingsPrivacyGuidePageElement):
     boolean {
-  const setting = page.getPref('generated.cookie_primary_setting').value;
-  return setting === CookiePrimarySetting.BLOCK_THIRD_PARTY ||
-      setting === CookiePrimarySetting.BLOCK_THIRD_PARTY_INCOGNITO;
+  return page.getPref('profile.cookie_controls_mode').value !==
+      CookieControlsMode.OFF &&
+      page.getPref('generated.cookie_default_content_setting').value !==
+      ContentSetting.BLOCK;
 }
 
 // Set the safe browsing setting for the privacy guide.
@@ -132,11 +139,12 @@ export function setupPrivacyGuidePageForTest(
     page: SettingsPrivacyGuidePageElement,
     syncBrowserProxy: TestSyncBrowserProxy): void {
   setSafeBrowsingSetting(page, SafeBrowsingSetting.STANDARD);
-  // TODO(b:306414714): Remove once 3pcd launched.
+  // TODO(b:306414714): Clean up with Mode B.
   if (!loadTimeData.getBoolean('is3pcdCookieSettingsRedesignEnabled')) {
-    setCookieSetting(page, CookiePrimarySetting.BLOCK_THIRD_PARTY_INCOGNITO);
+    setThirdPartyCookieSetting(page, CookieControlsMode.INCOGNITO_ONLY);
   }
-  setupSync({
+  setFirstPartyCookieSetting(page, ContentSetting.ALLOW);
+    setupSync({
     syncBrowserProxy: syncBrowserProxy,
     syncOn: true,
     syncAllDataTypes: true,
@@ -172,9 +180,9 @@ export function setParametersForSafeBrowsingStep(
 export function setParametersForCookiesStep(
     page: SettingsPrivacyGuidePageElement, isEligible: boolean): void {
   page.setPrefValue(
-      'generated.cookie_primary_setting',
-      isEligible ? CookiePrimarySetting.BLOCK_THIRD_PARTY :
-                   CookiePrimarySetting.ALLOW_ALL);
+      'profile.cookie_controls_mode',
+      isEligible ? CookieControlsMode.BLOCK_THIRD_PARTY :
+                   CookieControlsMode.OFF);
   assertEquals(
       isEligible, shouldShowCookiesCard(page),
       'Parameters for Cookies are set incorrectly.');
