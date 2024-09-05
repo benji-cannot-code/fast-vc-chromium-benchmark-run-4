e33628b44a0299cbbc418ab31516a58a1d8929ef
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 02:31:22 +0000

    ash: Create //chrome/browser/ui/webui/ash/curtain_ui
    Bug: b:364160708
    Change-Id: If2ddce3e69dea19c51f24d6342dba0624b52605c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832784
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Giovanni Ortuno Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351184}

--- chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.cc
+++ chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma allow_unsafe_buffers
 #endif
 
-#include "chrome/browser/ui/webui/ash/remote_maintenance_curtain_ui.h"
+#include "chrome/browser/ui/webui/ash/curtain_ui/remote_maintenance_curtain_ui.h"
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/ash/login/oobe_ui.h"
