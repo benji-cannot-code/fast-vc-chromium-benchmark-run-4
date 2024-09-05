e7e0be2f08c543ce43d4c8ca8f308c928ab0161f
Daniel Cheng <dcheng@chromium.org>
2024-09-05 23:23:19 +0000

    Fix dangling pointer error in PersistedTabDataAndroid::From()
    The root cause is that `PersistedTabDataAndroid` and its subclasses live
    on the UI thread; however, `From()` posts a task to the IO thread to
    call `Deserialize() on the `PersistedTabDataAndroid`. By the time the
    task runs, the `PersistedTabDataAndroid` might already be destroyed,
    e.g. if the tab is closed.
    
    The simple solution is to simply skip the thread hop:
    1. `Deserialize()` performs no actual IO.
    2. Despite its name, the IO thread is not actually intended for blocking
       IO. It would be more accurate to call it the "IPC thread", and it is
       actually important to avoid blocking tasks on this thread :)
    3. Since `Deserialize()` is reconstituting an object bound to the UI
       thread, it can really only be called on the UI thread anyway.
    
    This CL also includes some miscenalleous cleanups to simplify the code
    and update it to follow general best practices:
    - `SupplierCallback` now takes a `TabAndroid*` argument supplied by
      `PersistedTabDataAndroid`. Every caller bound an unprotected pointer
      to `TabAndroid` in its `SupplierCallback`; as an implementation detail,
      `PersistedTabDataAndroid` used a `WeakPtr<TabAndroid>` internally,
      making this "safe". However, the safety is non-obvious and the pattern
      was an invitation for future trouble.
    - `std::deque` converted to `base::circular_deque` to follow the //base
      container recommendations. `std::deque<T>` is quite inefficient,
      especially when `sizeof(T)` is small.
    - Reduce the number of redundant map lookups by using `operator[]` and
      `extract()` appropriately.
    - Make `DeferredRequest` movable and store it directly in the request
      queue, rather than using a `std::unique_ptr` indirection.
    - Move the definition of `DeferredRequest` to the .cc file. The big
      advantage of this is the complex ctor warning does not trigger for
      classes/structs defined entirely in .cc files; dropping the
      explicitly defaulted constructors allows aggregate initialization with
      designated initializers. One small downside is this requires prefixing
      `DeferredRequests` with `PersistedTabDataAndroid` to avoid naming
      collisions.
    
    Bug: 364561025
    Change-Id: Iea178cab5ec8112cad49fdbe949b3613dcf83797
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838067
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351775}

--- chrome/browser/android/persisted_tab_data/persisted_tab_data_android.h
+++ chrome/browser/android/persisted_tab_data/persisted_tab_data_android.h
@@ -6,9 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_ANDROID_PERSISTED_TAB_DATA_PERSISTED_TAB_DATA_ANDROID_H_
 #define CHROME_BROWSER_ANDROID_PERSISTED_TAB_DATA_PERSISTED_TAB_DATA_ANDROID_H_
 
-#include <deque>
+#include <memory>
 #include <vector>
 
+#include "base/containers/circular_deque.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
@@ -19,7 +20,10 @@ class PersistedTabDataAndroidHelper;
 class PersistedTabDataStorageAndroid;
 class TabAndroid;
 
-// Base class for native PersistedTabData. Clients extend off this class.
+struct PersistedTabDataAndroidDeferredRequest;
+
+// Helper base class for native PersistedTabData. This class lives on, and must
+// only be used on, the UI thread.
 class PersistedTabDataAndroid
     : public TabAndroidUserData<PersistedTabDataAndroid> {
  public:
@@ -28,7 +32,7 @@ class PersistedTabDataAndroid
 
   using FromCallback = base::OnceCallback<void(PersistedTabDataAndroid*)>;
   using SupplierCallback =
-      base::OnceCallback<std::unique_ptr<PersistedTabDataAndroid>()>;
+      base::OnceCallback<std::unique_ptr<PersistedTabDataAndroid>(TabAndroid*)>;
 
  protected:
   // Handles PersistedTabData acquisition by:
@@ -36,6 +40,10 @@ class PersistedTabDataAndroid
   // ...
   // - Restore PersistedTabData from disk (if possible). If not there ...
   // - Re-acquire PersistedTabData using the supplier
+  //
+  // `supplier_callback` and `from_callback` will never be synchronously
+  // invoked, i.e. they will not be invoked while the call to `From()` is still
+  // on the stack.
   static void From(base::WeakPtr<TabAndroid> tab_android,
                    const void* user_data_key,
                    SupplierCallback supplier_callback,
@@ -92,25 +100,16 @@ class PersistedTabDataAndroid
   static void OnDeferredStartup();
 
   static std::map<std::string,
-                  std::vector<PersistedTabDataAndroid::FromCallback>>*
+                  std::vector<PersistedTabDataAndroid::FromCallback>>&
   GetCachedCallbackMap();
 
-  static void RunCallbackOnUIThread(
-      base::WeakPtr<TabAndroid> tab_android,
-      const void* user_data_key,
-      PersistedTabDataAndroid* persisted_tab_data_android);
+  void RunCallbackOnUIThread(const TabAndroid* tab_android,
+                             const void* user_data_key);
 
   // PersistedTabData::From requests are delayed until deferred
   // startup occurs to mitigate the risk of jank.
-  struct DeferredRequest {
-    DeferredRequest();
-    ~DeferredRequest();
-    base::WeakPtr<TabAndroid> tab_android;
-    raw_ptr<const void> user_data_key;
-    SupplierCallback supplier_callback;
-    FromCallback from_callback;
-  };
-  static std::deque<std::unique_ptr<DeferredRequest>>* GetDeferredRequests();
+  static base::circular_deque<PersistedTabDataAndroidDeferredRequest>&
+  GetDeferredRequests();
   static bool deferred_startup_complete_;
 
   TAB_ANDROID_USER_DATA_KEY_DECL();
