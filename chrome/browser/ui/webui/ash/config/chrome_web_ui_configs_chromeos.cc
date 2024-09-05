b18b6e9bde3841d9d9cce16d2ef32076e24a80a0
Eriko Kurimoto <elkurin@chromium.org>
2024-09-05 02:28:58 +0000

    ash: Create //chrome/browser/ui/webui/ash/power_ui
    Bug: b:364142968
    Change-Id: If9624936151ea4ed38e0424af1b84dcc833d1d72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831933
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351180}

--- chrome/browser/ui/webui/ash/config/chrome_web_ui_configs_chromeos.cc
+++ chrome/browser/ui/webui/ash/config/chrome_web_ui_configs_chromeos.cc
@@ -103,7 +103,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/notification_tester/notification_tester_ui.h"
 #include "chrome/browser/ui/webui/ash/office_fallback/office_fallback_ui.h"
 #include "chrome/browser/ui/webui/ash/parent_access/parent_access_ui.h"
-#include "chrome/browser/ui/webui/ash/power_ui.h"
+#include "chrome/browser/ui/webui/ash/power_ui/power_ui.h"
 #include "chrome/browser/ui/webui/ash/remote_maintenance_curtain_ui.h"
 #include "chrome/browser/ui/webui/ash/sensor_info/sensor_info_ui.h"
 #include "chrome/browser/ui/webui/ash/set_time_ui.h"
--- chrome/browser/ui/webui/ash/power_ui/power_ui.h
+++ chrome/browser/ui/webui/ash/power_ui/power_ui.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_POWER_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_POWER_UI_H_
 
 #include "chrome/common/webui_url_constants.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -34,4 +34,4 @@ class PowerUI : public content::WebUIController {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_ASH_POWER_UI_POWER_UI_H_
