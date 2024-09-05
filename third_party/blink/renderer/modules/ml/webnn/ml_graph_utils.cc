11aa7e8f7db40e9a6283e31f525a5e3d86ea7665
Phillis Tang <phillis@chromium.org>
2024-09-05 00:59:55 +0000

    webnn: optimize away unneeded constant reshape
    Reshapes done to constants can be removed from the graph as long as
    there are no other operators dependent on the constants.
    Do this graph optimization before serializing to mojo.
    
    Change-Id: I7a5ca16b444dde431ec1ecfdf286027bd905ece1
    Bug: 364348897
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824345
    Auto-Submit: Phillis Tang <phillis@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351153}

--- third_party/blink/renderer/modules/ml/webnn/ml_graph_utils.cc
+++ third_party/blink/renderer/modules/ml/webnn/ml_graph_utils.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <numeric>
-
 #include "third_party/blink/renderer/modules/ml/webnn/ml_graph_utils.h"
+
+#include <numeric>
 
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_gemm_options.h"
 #include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
