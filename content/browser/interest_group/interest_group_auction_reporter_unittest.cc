639e24ff6dbe98cc77d4e9247c3ff186198878f9
Maks Orlovich <morlovich@chromium.org>
2024-09-05 16:33:22 +0000

    FLEDGE: Refactor PA mojo validation.
    This also changes validation failure behavior on reporting:
    all PA requests are discarded rather than just the violator.
    
    Change-Id: I2ec89bb8cb977477dd4506bef37b8a4c1a64f24c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836982
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Qingxin Wu <qingxinwu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351486}

--- content/browser/interest_group/interest_group_auction_reporter_unittest.cc
+++ content/browser/interest_group/interest_group_auction_reporter_unittest.cc
@@ -1657,16 +1657,9 @@ TEST_F(InterestGroupAuctionReporterTest, InvalidPrivateAggregationRequests) {
   EXPECT_EQ("Private Aggregation request using disabled features",
             TakeBadMessage());
 
-  // The invalid PA stuff got discarded.
-  EXPECT_THAT(
-      private_aggregation_manager_.TakePrivateAggregationRequests(),
-      testing::UnorderedElementsAre(
-          testing::Pair(
-              kSellerOrigin,
-              ElementsAreRequests(kReportResultPrivateAggregationRequest)),
-          testing::Pair(
-              kWinningBidderOrigin,
-              ElementsAreRequests(kReportWinPrivateAggregationRequest))));
+  // Due to the invalid messages, incoming PA stuff got discarded.
+  EXPECT_THAT(private_aggregation_manager_.TakePrivateAggregationRequests(),
+              testing::UnorderedElementsAre());
 
   WaitForCompletion();
 }
@@ -1703,15 +1696,8 @@ TEST_F(InterestGroupAuctionReporterTest, InvalidPrivateAggregationRequests2) {
             TakeBadMessage());
 
   // The invalid PA stuff got discarded.
-  EXPECT_THAT(
-      private_aggregation_manager_.TakePrivateAggregationRequests(),
-      testing::UnorderedElementsAre(
-          testing::Pair(
-              kSellerOrigin,
-              ElementsAreRequests(kReportResultPrivateAggregationRequest)),
-          testing::Pair(
-              kWinningBidderOrigin,
-              ElementsAreRequests(kReportWinPrivateAggregationRequest))));
+  EXPECT_THAT(private_aggregation_manager_.TakePrivateAggregationRequests(),
+              testing::UnorderedElementsAre());
 
   WaitForCompletion();
 }
--- content/browser/interest_group/interest_group_pa_report_util.h
+++ content/browser/interest_group/interest_group_pa_report_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_INTEREST_GROUP_INTEREST_GROUP_PA_REPORT_UTIL_H_
 
 #include <optional>
+#include <string>
 #include <vector>
 
 #include "base/time/time.h"
@@ -141,6 +142,12 @@ CONTENT_EXPORT bool HasValidFilteringId(
 
 // Returns true if filtering ID is valid.
 CONTENT_EXPORT bool IsValidFilteringId(std::optional<uint64_t> filtering_id);
+
+// If `pa_requests` contains malformed requests, returns an error message.
+// Otherwise returns nullopt.
+CONTENT_EXPORT std::optional<std::string> ValidatePrivateAggregationRequests(
+    const std::vector<auction_worklet::mojom::PrivateAggregationRequestPtr>&
+        pa_requests);
 
 }  // namespace content
 
