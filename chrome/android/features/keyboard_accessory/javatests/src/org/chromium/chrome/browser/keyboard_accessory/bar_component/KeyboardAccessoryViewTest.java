c89768229eb21093ff563717c2ae25c65e5d8342
Harsh Shukla <hvs@google.com>
2024-09-05 09:03:27 +0000

    Fixing the disabled UI for keyboard accessory VCN.
    Existing implementation highlights the chip with IPH:
    https://screenshot.googleplex.com/ArsP6umS9BHbrMm
    
    Fix: https://screenshot.googleplex.com/8yZ5r6icfC9Lw6P
    Screencast:
    https://screencast.googleplex.com/cast/NTg1MjA1Mzg0NTgzNTc3Nnw1YjZiMTZjOC1kNA
    Mocks: https://screenshot.googleplex.com/3Mddi2LKxCSLhBj
    
    Bug: 327931044
    Change-Id: I89f896fc52c028478ed731cc75a245a196391aac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831507
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Harsh Shukla <hvs@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351282}

--- chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
+++ chrome/android/features/keyboard_accessory/javatests/src/org/chromium/chrome/browser/keyboard_accessory/bar_component/KeyboardAccessoryViewTest.java
@@ -827,6 +827,7 @@ public class KeyboardAccessoryViewTest {
 
         onView(withText("Virtual Card")).perform(click());
         assertFalse(clickRecorded.get());
+        onView(withText("Virtual Card")).check(matches(not(isSelected())));
     }
 
     private static AutofillSuggestion.Builder getDefaultAutofillSuggestionBuilder() {
