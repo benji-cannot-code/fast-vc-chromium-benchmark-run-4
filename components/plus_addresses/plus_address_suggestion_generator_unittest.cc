a1c765a12aac7ca36c6f24e60005e8d1a67c22dd
Norge Vizcay <vizcay@google.com>
2024-09-05 14:02:43 +0000

    Limit plus address creation on http domains.
    The CL:
    - Adds support for plus address filling on http domains.
    - Removes support for plus address creation on http domains.
    
    Bug: 362237423
    Change-Id: If98b1fee1a8bf8e595bdb6589cb39e16bac84b2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839737
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1351389}

--- components/plus_addresses/plus_address_suggestion_generator_unittest.cc
+++ components/plus_addresses/plus_address_suggestion_generator_unittest.cc
@@ -113,10 +113,10 @@ TEST_F(PlusAddressSuggestionGeneratorTest,
   allocator().set_is_next_allocation_synchronous(false);
   PlusAddressSuggestionGenerator generator(
       &setting_service(), &allocator(),
-      url::Origin::Create(GURL("https://foo.bar")),
-      /*is_off_the_record=*/false);
+      url::Origin::Create(GURL("https://foo.bar")));
   EXPECT_THAT(generator.GetSuggestions(
-                  PasswordFormClassification(), FormFieldData(),
+                  /*is_creation_enabled=*/true, PasswordFormClassification(),
+                  FormFieldData(),
                   AutofillSuggestionTriggerSource::kFormControlElementClicked,
                   /*affiliated_profiles=*/{}),
               ElementsAre(IsCreateInlineSuggestion(
@@ -133,10 +133,10 @@ TEST_F(PlusAddressSuggestionGeneratorTest,
   allocator().set_is_next_allocation_synchronous(true);
   PlusAddressSuggestionGenerator generator(
       &setting_service(), &allocator(),
-      url::Origin::Create(GURL("https://foo.bar")),
-      /*is_off_the_record=*/false);
+      url::Origin::Create(GURL("https://foo.bar")));
   EXPECT_THAT(generator.GetSuggestions(
-                  PasswordFormClassification(), FormFieldData(),
+                  /*is_creation_enabled=*/true, PasswordFormClassification(),
+                  FormFieldData(),
                   AutofillSuggestionTriggerSource::kFormControlElementClicked,
                   /*affiliated_profiles=*/{}),
               ElementsAre(IsCreateInlineSuggestion(
