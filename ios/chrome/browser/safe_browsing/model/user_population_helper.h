99c35fbad0a11513111cbc299e8c32821a1fd84c
Aliona DANGLA <alionadangla@google.com>
2024-09-06 16:38:55 +0000

    [iOS] Rename GetUserPopulationForBrowserState function
    Fixed: 358337577, 359814484
    Change-Id: Id2feb81231fc1c588a0b16508e0207555cd361b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839732
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352097}

--- ios/chrome/browser/safe_browsing/model/user_population_helper.h
+++ ios/chrome/browser/safe_browsing/model/user_population_helper.h
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/safe_browsing/core/common/proto/csd.pb.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
-// Creates a ChromeUserPopulation proto for the given `browser_state`.
-safe_browsing::ChromeUserPopulation GetUserPopulationForBrowserState(
-    ChromeBrowserState* browser_state);
+// Creates a ChromeUserPopulation proto for the given `profile`.
+safe_browsing::ChromeUserPopulation GetUserPopulationForProfile(
+    ProfileIOS* profile);
 
 #endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_MODEL_USER_POPULATION_HELPER_H_
