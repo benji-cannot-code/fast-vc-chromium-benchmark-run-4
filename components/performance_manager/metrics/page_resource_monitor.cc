02de15df72c60243a348d03697792bef7b8f13e5
Youngsoo Choi <youngsoo.choi@lge.com>
2024-09-06 03:57:59 +0000

    [PM] Move PageResourceMonitor metric to components/.
    This CL moves the PageResourceMonitor metric to //components, since the
    class doesn't have any dependencies on //chrome.
    
    Bug: 40623222
    Change-Id: If23228f4c2ace2800d7c87345f23bdf93937a00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5820331
    Commit-Queue: Youngsoo Choi <youngsoo.choi@lge.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351853}

--- components/performance_manager/metrics/page_resource_monitor.cc
+++ components/performance_manager/metrics/page_resource_monitor.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/performance_manager/metrics/page_resource_monitor.h"
+#include "components/performance_manager/public/metrics/page_resource_monitor.h"
 
 #include <stdint.h>
 
--- components/performance_manager/public/metrics/page_resource_monitor.h
+++ components/performance_manager/public/metrics/page_resource_monitor.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_PERFORMANCE_MANAGER_METRICS_PAGE_RESOURCE_MONITOR_H_
-#define CHROME_BROWSER_PERFORMANCE_MANAGER_METRICS_PAGE_RESOURCE_MONITOR_H_
+#ifndef COMPONENTS_PERFORMANCE_MANAGER_PUBLIC_METRICS_PAGE_RESOURCE_MONITOR_H_
+#define COMPONENTS_PERFORMANCE_MANAGER_PUBLIC_METRICS_PAGE_RESOURCE_MONITOR_H_
 
 #include <map>
 #include <memory>
@@ -111,4 +111,4 @@ class PageResourceMonitor : public resource_attribution::QueryResultObserver,
 
 }  // namespace performance_manager::metrics
 
-#endif  // CHROME_BROWSER_PERFORMANCE_MANAGER_METRICS_PAGE_RESOURCE_MONITOR_H_
+#endif  // COMPONENTS_PERFORMANCE_MANAGER_PUBLIC_METRICS_PAGE_RESOURCE_MONITOR_H_
