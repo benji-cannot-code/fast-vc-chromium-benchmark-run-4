1091abb28065e7401503965901f7d11438c9118e
Fredrik Söderquist <fs@opera.com>
2024-09-06 10:32:11 +0000

    spanification: WebAssociatedURLLoaderClient
    Pass a span<const char> to DidReceiveData().
    
    Bug: 351564777
    Change-Id: Iccf9c41cf7af89b913870ba60acff5e18fe29114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829129
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Derek Schuff <dschuff@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351934}

--- third_party/blink/public/web/web_associated_url_loader_client.h
+++ third_party/blink/public/web/web_associated_url_loader_client.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ASSOCIATED_URL_LOADER_CLIENT_H_
 #define THIRD_PARTY_BLINK_PUBLIC_WEB_WEB_ASSOCIATED_URL_LOADER_CLIENT_H_
 
+#include "base/containers/span.h"
+
 namespace blink {
 
 class WebURL;
@@ -22,7 +24,7 @@ class WebAssociatedURLLoaderClient {
                            uint64_t total_bytes_to_be_sent) {}
   virtual void DidReceiveResponse(const WebURLResponse&) {}
   virtual void DidDownloadData(uint64_t data_length) {}
-  virtual void DidReceiveData(const char* data, int data_length) {}
+  virtual void DidReceiveData(base::span<const char> data) {}
   virtual void DidFinishLoading() {}
   virtual void DidFail(const WebURLError&) {}
 
--- third_party/blink/renderer/core/loader/web_associated_url_loader_impl.cc
+++ third_party/blink/renderer/core/loader/web_associated_url_loader_impl.cc
@@ -260,7 +260,7 @@ void WebAssociatedURLLoaderImpl::ClientAdapter::DidReceiveData(
     return;
   }
 
-  client_->DidReceiveData(data.data(), base::checked_cast<int>(data.size()));
+  client_->DidReceiveData(data);
 }
 
 void WebAssociatedURLLoaderImpl::ClientAdapter::DidFinishLoading(
