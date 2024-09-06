62a1093f697817d248a9dd4bd8af54225340935e
Michael Thiessen <mthiesse@chromium.org>
2024-09-06 20:55:55 +0000

    Turn on CCTPrewarmTab by default
    1% Stable experiment shows positive results, launching.
    
    Bug: 40286920
    Change-Id: Ib13e7e4acb8836e07e62122ec05e076905e1814b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5842786
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Auto-Submit: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352270}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -608,9 +608,7 @@ BASE_FEATURE(kCCTGoogleBottomBarVariantLayouts,
              "CCTGoogleBottomBarVariantLayouts",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
-BASE_FEATURE(kCCTPrewarmTab,
-             "CCTPrewarmTab",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kCCTPrewarmTab, "CCTPrewarmTab", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTReportParallelRequestStatus,
              "CCTReportParallelRequestStatus",
