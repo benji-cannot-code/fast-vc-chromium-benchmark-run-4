1600775e6daf7c5b678f5cde8308c7f59f2e8526
Andrey Kosyakov <caseq@chromium.org>
2024-09-06 17:09:32 +0000

    Make C++ implementations of GLboolean WebGL methods return bool
    GLboolean is aliased to `boolean` in the IDL[1], whereas it's a `char`
    in the C GL bindings, so for a WebIDL implementation, a correct type
    for method to return would be `bool`, not `GLboolean` AKA char.
    
    This does not change the way return values were exposed to script,
    since we already coerce primitive types to those specified in IDL[2].
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.idl;l=29
    
    [2] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/bindings/scripts/bind_gen/interface.py;l=1887
    
    Bug: 364947659
    Change-Id: Ieb2d5388d8cb42e520210a8995e215de608427cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838275
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352122}

--- third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl2_rendering_context_base.h
@@ -827,7 +827,7 @@ class WebGL2RenderingContextBase : public WebGLRenderingContextBase {
   /* Query Objects */
   WebGLQuery* createQuery();
   void deleteQuery(WebGLQuery*);
-  GLboolean isQuery(WebGLQuery*);
+  bool isQuery(WebGLQuery*);
   void beginQuery(GLenum, WebGLQuery*);
   void endQuery(GLenum);
   ScriptValue getQuery(ScriptState*, GLenum, GLenum);
@@ -836,7 +836,7 @@ class WebGL2RenderingContextBase : public WebGLRenderingContextBase {
   /* Sampler Objects */
   WebGLSampler* createSampler();
   void deleteSampler(WebGLSampler*);
-  GLboolean isSampler(WebGLSampler*);
+  bool isSampler(WebGLSampler*);
   void bindSampler(GLuint, WebGLSampler*);
   void samplerParameteri(WebGLSampler*, GLenum, GLint);
   void samplerParameterf(WebGLSampler*, GLenum, GLfloat);
@@ -844,7 +844,7 @@ class WebGL2RenderingContextBase : public WebGLRenderingContextBase {
 
   /* Sync objects */
   WebGLSync* fenceSync(GLenum, GLbitfield);
-  GLboolean isSync(WebGLSync*);
+  bool isSync(WebGLSync*);
   void deleteSync(WebGLSync*);
   GLenum clientWaitSync(WebGLSync*, GLbitfield, GLuint64);
   void waitSync(WebGLSync*, GLbitfield, GLint64);
@@ -854,7 +854,7 @@ class WebGL2RenderingContextBase : public WebGLRenderingContextBase {
   /* Transform Feedback */
   WebGLTransformFeedback* createTransformFeedback();
   void deleteTransformFeedback(WebGLTransformFeedback*);
-  GLboolean isTransformFeedback(WebGLTransformFeedback*);
+  bool isTransformFeedback(WebGLTransformFeedback*);
   void bindTransformFeedback(GLenum, WebGLTransformFeedback*);
   void beginTransformFeedback(GLenum);
   void endTransformFeedback();
@@ -888,7 +888,7 @@ class WebGL2RenderingContextBase : public WebGLRenderingContextBase {
   /* Vertex Array Objects */
   WebGLVertexArrayObject* createVertexArray();
   void deleteVertexArray(WebGLVertexArrayObject*);
-  GLboolean isVertexArray(WebGLVertexArrayObject*);
+  bool isVertexArray(WebGLVertexArrayObject*);
   void bindVertexArray(WebGLVertexArrayObject*);
 
   /* Reading */
