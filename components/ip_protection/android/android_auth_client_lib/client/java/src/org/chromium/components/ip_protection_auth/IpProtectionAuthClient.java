43bdcc0ec52eef3bb1c19e54f69348a0aac4765c
Ashley Newson <ashleynewson@chromium.org>
2024-09-06 12:33:46 +0000

    Add null check in IPP auth client onBindingDied
    In rare cases, onBindingDied can be run without a valid client object.
    This change adds a null check to avoid a crash.
    
    Bug: b:364887579
    Change-Id: I4881d2666d5be4884b6b4f7823036a033da58b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837054
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ashley Newson <ashleynewson@chromium.org>
    Reviewed-by: Abhijith Nair <abhijithnair@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351969}

--- components/ip_protection/android/android_auth_client_lib/client/java/src/org/chromium/components/ip_protection_auth/IpProtectionAuthClient.java
+++ components/ip_protection/android/android_auth_client_lib/client/java/src/org/chromium/components/ip_protection_auth/IpProtectionAuthClient.java
@@ -78,9 +78,9 @@ public final class IpProtectionAuthClient implements AutoCloseable {
 
     /** This class must be used exclusively from the main thread. */
     private static final class ConnectionSetup implements ServiceConnection {
+        @NonNull private final Context mContext;
         @Nullable private IpProtectionAuthServiceCallback mCallback;
-        private final Context mContext;
-        private IpProtectionAuthClient mIpProtectionClient;
+        @Nullable private IpProtectionAuthClient mIpProtectionClient;
         private boolean mBound;
 
         ConnectionSetup(
@@ -114,8 +114,10 @@ public final class IpProtectionAuthClient implements AutoCloseable {
             try (TraceEvent event =
                     TraceEvent.scoped("IpProtectionAuthClient.Create.OnServiceDisconnected")) {
                 unbindIfBound();
-                mIpProtectionClient.mCallbackTracker.rejectUnresolvedCallbacks(
-                        AuthRequestError.OTHER);
+                if (mIpProtectionClient != null) {
+                    mIpProtectionClient.mCallbackTracker.rejectUnresolvedCallbacks(
+                            AuthRequestError.OTHER);
+                }
             }
         }
 
@@ -124,8 +126,10 @@ public final class IpProtectionAuthClient implements AutoCloseable {
             try (TraceEvent event =
                     TraceEvent.scoped("IpProtectionAuthClient.Create.OnBindingDied")) {
                 unbindIfBound();
-                mIpProtectionClient.mCallbackTracker.rejectUnresolvedCallbacks(
-                        AuthRequestError.OTHER);
+                if (mIpProtectionClient != null) {
+                    mIpProtectionClient.mCallbackTracker.rejectUnresolvedCallbacks(
+                            AuthRequestError.OTHER);
+                }
             }
         }
 
