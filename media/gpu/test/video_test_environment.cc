df3696e01b10801c18cde69c1352878cae4f681e
Steve Cho <stevecho@chromium.org>
2024-09-06 23:34:32 +0000

    video: Increase timeout for v_d_a_tests on ARM64 VM
    This CL increases timeout for v_d_a_tests with V4L2 virtual driver
    (VISL) running on arm64-generic-vm. This is needed because the timeout
    sometimes happens while running with Swarming Task through CQ.
    
    BUG=b:360384931
    TEST=video_decode_accelerator_tests crowd_run_256X144_fr15_5frames.ivf crowd_run_256X144_fr15_5frames.ivf.json --gtest_filter=VideoDecoderTest.FlushAtEndOfStream --validator_type=none --single-process-tests --enable-primary-node-access-for-vkms-testing
    
    Change-Id: I19228594b9e239ce306deeb528b61799874d2276
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5807716
    Reviewed-by: Hirokazu Honda <hiroh@chromium.org>
    Commit-Queue: Steve Cho <stevecho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352363}

--- media/gpu/test/video_test_environment.cc
+++ media/gpu/test/video_test_environment.cc
@@ -12,6 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/gpu/buildflags.h"
 #include "mojo/core/embedder/embedder.h"
 
+#if BUILDFLAG(USE_V4L2_CODEC)
+#include "media/gpu/v4l2/v4l2_utils.h"
+#endif
+
 #if BUILDFLAG(USE_VAAPI)
 #include "media/gpu/vaapi/vaapi_wrapper.h"
 #endif
@@ -93,6 +97,14 @@ base::FilePath VideoTestEnvironment::GetTestOutputFilePath() const {
   test_suite_name = test_info->test_suite_name();
 #endif  // BUILDFLAG(IS_WIN)
   return base::FilePath(test_suite_name).Append(test_name);
+}
+
+bool VideoTestEnvironment::IsV4L2VirtualDriver() const {
+#if BUILDFLAG(USE_V4L2_CODEC)
+  return IsVislDriver();
+#else
+  return false;
+#endif
 }
 
 }  // namespace test
