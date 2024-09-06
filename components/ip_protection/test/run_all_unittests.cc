4b6920bdb64931e370d749cda8fd5715bc106914
Dustin J. Mitchell <djmitche@chromium.org>
2024-09-06 20:21:12 +0000

    Support running only ip_protection unittests
    This saves time spent linking the entire `componenets_unittests` binary.
    It is only intended for local development, and these tests still run in
    the usual `components_unittests` invocation in CQ.
    
    Change-Id: Ie4aa5019a106cf1b6daf7a73dc73ed229e2e0764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835786
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352249}

---
+++ components/ip_protection/test/run_all_unittests.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/functional/bind.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "mojo/core/embedder/embedder.h"
+
+int main(int argc, char** argv) {
+  base::TestSuite test_suite(argc, argv);
+
+  mojo::core::Init();
+
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::BindOnce(&base::TestSuite::Run, base::Unretained(&test_suite)));
+}
