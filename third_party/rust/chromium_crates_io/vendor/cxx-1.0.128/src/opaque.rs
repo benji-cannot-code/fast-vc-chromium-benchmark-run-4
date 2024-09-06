aab1bddef61dad06c7bf45a436baffb1b42226d2
danakj <danakj@chromium.org>
2024-09-06 20:43:39 +0000

    Roll cxx: 1.0.126 => 1.0.128 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * cxx: 1.0.126 => 1.0.128
    * cxxbridge-flags: 1.0.126 => 1.0.128
    * cxxbridge-macro: 1.0.126 => 1.0.128
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * cxx@1.0.128, cxxbridge-flags@1.0.128, cxxbridge-macro@1.0.128:
      crypto-safe, safe-to-deploy, ub-risk-2
    
    Bug: None
    Change-Id: I50bab1e0cfd6d24b5a14bd04387f93ef4f6018f7
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839554
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352261}

--- third_party/rust/chromium_crates_io/vendor/cxx-1.0.128/src/opaque.rs
+++ third_party/rust/chromium_crates_io/vendor/cxx-1.0.128/src/opaque.rs
@@ -2,8 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #![allow(missing_docs)]
 
 use crate::void;
+use core::cell::UnsafeCell;
 use core::marker::{PhantomData, PhantomPinned};
 use core::mem;
+use core::panic::RefUnwindSafe;
 
 // . size = 0
 // . align = 1
@@ -11,11 +13,22 @@ use core::mem;
 // . !Send
 // . !Sync
 // . !Unpin
+// . not readonly
+// . unwind-safe
 #[repr(C, packed)]
 pub struct Opaque {
     _private: [*const void; 0],
     _pinned: PhantomData<PhantomPinned>,
+    _mutable: SyncUnsafeCell<PhantomData<()>>,
 }
+
+impl RefUnwindSafe for Opaque {}
+
+// TODO: https://github.com/rust-lang/rust/issues/95439
+#[repr(transparent)]
+struct SyncUnsafeCell<T>(UnsafeCell<T>);
+
+unsafe impl<T> Sync for SyncUnsafeCell<T> {}
 
 const_assert_eq!(0, mem::size_of::<Opaque>());
 const_assert_eq!(1, mem::align_of::<Opaque>());
