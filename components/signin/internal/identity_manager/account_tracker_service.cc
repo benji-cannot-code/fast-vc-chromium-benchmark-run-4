47126eb281fddb2ae492393436065c0776d5eb72
Ryan Sultanem <rsult@google.com>
2024-09-06 12:33:58 +0000

    [Metrics] Record multiple account state for availability histograms
    Records state at:
    - Startup: whether it is known and have valid refresh token,
    - When fetching account info from server: whether the account in pref
    is valid or not, and if it is fully valid.
    
    Bug: 364842118
    Change-Id: I0784eb48ebc12d5babd4a96cb28e3652b5850e11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839521
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1351970}

--- components/signin/internal/identity_manager/account_tracker_service.cc
+++ components/signin/internal/identity_manager/account_tracker_service.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/metrics/histogram_functions_internal_overloads.h"
 #include "base/metrics/histogram_macros.h"
 #include "base/notreached.h"
 #include "base/ranges/algorithm.h"
@@ -83,6 +84,17 @@ const base::FilePath::CharType kAccountsFolder[] =
     FILE_PATH_LITERAL("Accounts");
 const base::FilePath::CharType kAvatarImagesFolder[] =
     FILE_PATH_LITERAL("Avatar Images");
+
+// Marks the state of the account that are read from prefs.
+// These values are persisted to logs. Entries should not be renumbered and
+// numeric values should never be reused.
+enum class AccountInPrefState {
+  kValid = 0,
+  kEmptyAccount = 1,
+  kEmptyEmailOrGaiaId = 2,
+
+  kMaxValue = kEmptyEmailOrGaiaId,
+};
 
 // Reads a PNG image from disk and decodes it. If the reading/decoding attempt
 // was unsuccessful, an empty image is returned.
@@ -332,6 +344,15 @@ void AccountTrackerService::SetAccountInfoFromUserInfo(
   DCHECK(base::Contains(accounts_, account_id));
   AccountInfo& account_info = accounts_[account_id];
 
+  AccountInPrefState state = AccountInPrefState::kValid;
+  if (account_info.IsEmpty()) {
+    state = AccountInPrefState::kEmptyAccount;
+  } else if (account_info.gaia.empty() || account_info.email.empty()) {
+    // This may happen if account capabilities are fetched first.
+    state = AccountInPrefState::kEmptyEmailOrGaiaId;
+  }
+  base::UmaHistogramEnumeration("Signin.AccountInPref.State", state);
+
   std::optional<AccountInfo> maybe_account_info =
       AccountInfoFromUserInfo(user_info);
   if (maybe_account_info) {
@@ -340,7 +361,13 @@ void AccountTrackerService::SetAccountInfoFromUserInfo(
     maybe_account_info->account_id = PickAccountIdForAccount(
         maybe_account_info->gaia, maybe_account_info->email);
 
-    if (maybe_account_info->account_id == account_info.account_id) {
+    // Whether the existing account in pref matches the fetched account.
+    bool accounts_matching =
+        maybe_account_info->account_id == account_info.account_id;
+    base::UmaHistogramBoolean("Signin.AccountInPref.MatchingFetchedAccount",
+                              accounts_matching);
+
+    if (accounts_matching) {
       account_info.UpdateWith(maybe_account_info.value());
     } else {
       DLOG(ERROR) << "Cannot set account info from user info as account ids "
