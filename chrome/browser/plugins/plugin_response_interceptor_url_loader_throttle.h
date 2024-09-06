9932ea3dd4c1eb2e40ad5d9766f360c2e87e3ff2
Avi Drissman <avi@chromium.org>
2024-09-06 05:50:55 +0000

    Use an opaque type for FrameTreeNode IDs, part 38
    chrome/browser/plugins
    
    Bug: 361344235
    Change-Id: Ic0e59a843648dbceb1f6864c724ce28e423ac548
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840031
    Commit-Queue: K. Moon <kmoon@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: K. Moon <kmoon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351870}

--- chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.h
+++ chrome/browser/plugins/plugin_response_interceptor_url_loader_throttle.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_PLUGINS_PLUGIN_RESPONSE_INTERCEPTOR_URL_LOADER_THROTTLE_H_
 
 #include "base/memory/weak_ptr.h"
+#include "content/public/browser/frame_tree_node_id.h"
 #include "services/network/public/mojom/fetch_api.mojom-shared.h"
 #include "third_party/blink/public/common/loader/url_loader_throttle.h"
 
@@ -23,7 +24,7 @@ class PluginResponseInterceptorURLLoaderThrottle
  public:
   PluginResponseInterceptorURLLoaderThrottle(
       network::mojom::RequestDestination request_destination,
-      int frame_tree_node_id);
+      content::FrameTreeNodeId frame_tree_node_id);
 
   PluginResponseInterceptorURLLoaderThrottle(
       const PluginResponseInterceptorURLLoaderThrottle&) = delete;
@@ -42,7 +43,7 @@ class PluginResponseInterceptorURLLoaderThrottle
   void ResumeLoad();
 
   const network::mojom::RequestDestination request_destination_;
-  const int frame_tree_node_id_;
+  const content::FrameTreeNodeId frame_tree_node_id_;
 
   base::WeakPtrFactory<PluginResponseInterceptorURLLoaderThrottle>
       weak_factory_{this};
