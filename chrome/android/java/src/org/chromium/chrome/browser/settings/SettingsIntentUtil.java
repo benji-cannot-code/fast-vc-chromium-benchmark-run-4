4edcba86f7758ed055c662fedb3e3775ad394730
Shuhei Takahashi <nya@chromium.org>
2024-09-06 00:21:00 +0000

    [Settings] Start building the single activity mode
    In the single activity mode, there is only a single settings activity at
    a time, and navigation is implemented as fragment transactions. To avoid
    starting a new settings activity on navigation, we set
    FLAG_ACTIVITY_SINGLE_TOP to intents launching the activity.
    
    The new mode has several known bugs (such as no animation and crashes
    in several pages), but the new mode is guarded by a feature flag, and I
    will fix them in upcoming patches.
    
    Bug: b/356743945
    Change-Id: I5ff1336f5a24906c35fae0e69b0b1b7281b929ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5825998
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sirisha Kavuluru <skavuluru@google.com>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351802}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsIntentUtil.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/SettingsIntentUtil.java
@@ -13,6 +13,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
+
 public class SettingsIntentUtil {
     private SettingsIntentUtil() {}
 
@@ -31,6 +33,9 @@ public class SettingsIntentUtil {
             @Nullable Bundle fragmentArgs) {
         Intent intent = new Intent();
         intent.setClass(context, SettingsActivity.class);
+        if (ChromeFeatureList.sSettingsSingleActivity.isEnabled()) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        }
         if (!(context instanceof Activity)) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
