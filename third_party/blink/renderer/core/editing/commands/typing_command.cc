911e39c3f09d8de6015711c84c17e7454b68c466
Shweta Bindal <shwetabindal@microsoft.com>
2024-09-06 05:58:57 +0000

    [Editing] Reset |input_type| to |kNone| before character input
    There is an edge case where |last_typing_command| that is reused for
    inserting a character does not reset its |input_type| to none.  The
    actual |input_type| will be determined later in |GetInputType()| based
    on the |command_type|.
    
    The changed helpers are:
    |TypingCommand::InsertParagraphSeparator|
    |TypingCommand::InsertParagraphSeparatorInQuotedContent|
    |TypingCommand::InsertLineBreak|
    |TypingCommand::DeleteKeyPressed|
    |TypingCommand::DeleteSelection|
    
    Bug: 41478041
    Change-Id: I7ba178968e95cdba9c6c3c6e2dc5123996cedd1c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5796102
    Commit-Queue: Shweta Bindal <shwetabindal@microsoft.com>
    Reviewed-by: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351871}

--- third_party/blink/renderer/core/editing/commands/typing_command.cc
+++ third_party/blink/renderer/core/editing/commands/typing_command.cc
@@ -221,6 +221,10 @@ void TypingCommand::DeleteSelection(Document& document, Options options) {
           LastTypingCommandIfStillOpenForTyping(frame)) {
     UpdateSelectionIfDifferentFromCurrentSelection(last_typing_command, frame);
 
+    if (RuntimeEnabledFeatures::
+            ResetInputTypeToNoneBeforeCharacterInputEnabled()) {
+      last_typing_command->input_type_ = InputEvent::InputType::kNone;
+    }
     // InputMethodController uses this function to delete composition
     // selection.  It won't be aborted.
     last_typing_command->DeleteSelection(options & kSmartDelete,
@@ -266,6 +270,10 @@ void TypingCommand::DeleteKeyPressed(Document& document,
         UpdateSelectionIfDifferentFromCurrentSelection(last_typing_command,
                                                        frame);
         EditingState editing_state;
+        if (RuntimeEnabledFeatures::
+                ResetInputTypeToNoneBeforeCharacterInputEnabled()) {
+          last_typing_command->input_type_ = InputEvent::InputType::kNone;
+        }
         last_typing_command->DeleteKeyPressed(granularity, options & kKillRing,
                                               &editing_state);
         return;
@@ -489,6 +497,10 @@ bool TypingCommand::InsertLineBreak(Document& document) {
           LastTypingCommandIfStillOpenForTyping(document.GetFrame())) {
     EditingState editing_state;
     EventQueueScope event_queue_scope;
+    if (RuntimeEnabledFeatures::
+            ResetInputTypeToNoneBeforeCharacterInputEnabled()) {
+      last_typing_command->input_type_ = InputEvent::InputType::kNone;
+    }
     last_typing_command->InsertLineBreak(&editing_state);
     return !editing_state.IsAborted();
   }
@@ -503,6 +515,10 @@ bool TypingCommand::InsertParagraphSeparatorInQuotedContent(
           LastTypingCommandIfStillOpenForTyping(document.GetFrame())) {
     EditingState editing_state;
     EventQueueScope event_queue_scope;
+    if (RuntimeEnabledFeatures::
+            ResetInputTypeToNoneBeforeCharacterInputEnabled()) {
+      last_typing_command->input_type_ = InputEvent::InputType::kNone;
+    }
     last_typing_command->InsertParagraphSeparatorInQuotedContent(
         &editing_state);
     return !editing_state.IsAborted();
@@ -518,6 +534,10 @@ bool TypingCommand::InsertParagraphSeparator(Document& document) {
           LastTypingCommandIfStillOpenForTyping(document.GetFrame())) {
     EditingState editing_state;
     EventQueueScope event_queue_scope;
+    if (RuntimeEnabledFeatures::
+            ResetInputTypeToNoneBeforeCharacterInputEnabled()) {
+      last_typing_command->input_type_ = InputEvent::InputType::kNone;
+    }
     last_typing_command->InsertParagraphSeparator(&editing_state);
     return !editing_state.IsAborted();
   }
