d5934c5536a9d772f19251f955587124379af541
dpapad <dpapad@chromium.org>
2024-09-06 06:50:46 +0000

    WebUI: Bulk migrate iron-icon to cr-icon, part 5.
    In this part updating several smaller WebUI surfaces.
    
    This is in preparation of removing iron-icon from Desktop builds.
    
    Bug: 40943652
    Change-Id: Ib7859ba733e64df0445f1cba8679436cef17e51d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839419
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351887}

--- chrome/browser/resources/discards/database_tab.ts
+++ chrome/browser/resources/discards/database_tab.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import 'chrome://resources/cr_elements/icons.html.js';
-import 'chrome://resources/polymer/v3_0/iron-icon/iron-icon.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 
 import type {CrInputElement} from 'chrome://resources/cr_elements/cr_input/cr_input.js';
 import {assertNotReached} from 'chrome://resources/js/assert.js';
--- chrome/browser/resources/signin/profile_picker/profile_creation_flow/account_selection_lacros.ts
+++ chrome/browser/resources/signin/profile_picker/profile_creation_flow/account_selection_lacros.ts
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://resources/cr_elements/cr_icon_button/cr_icon_button.js';
+import 'chrome://resources/cr_elements/cr_icon/cr_icon.js';
 import 'chrome://resources/cr_elements/cr_shared_vars.css.js';
 
 import {I18nMixin} from 'chrome://resources/cr_elements/i18n_mixin.js';
