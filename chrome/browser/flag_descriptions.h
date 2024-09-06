c78570d0537801dcc6613d3dd8ef6fbc2982d49d
Gang Wu <gangwu@chromium.org>
2024-09-06 18:07:48 +0000

    Add a new flag for the bottom toolbar
    Change-Id: I38891d6774a0037552164d1400ceb492354a348a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836213
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352164}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -64,6 +64,9 @@ extern const char kAllowInsecureLocalhostDescription[];
 extern const char kAndroidAppIntegrationName[];
 extern const char kAndroidAppIntegrationDescription[];
 
+extern const char kAndroidBottomToolbarName[];
+extern const char kAndroidBottomToolbarDescription[];
+
 extern const char kAndroidExtendedKeyboardShortcutsName[];
 extern const char kAndroidExtendedKeyboardShortcutsDescription[];
 
