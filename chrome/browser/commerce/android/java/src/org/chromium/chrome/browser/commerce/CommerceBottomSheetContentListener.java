b7c74609482555ce3e9a77087e31ffcd89becdb0
Mei Liang <meiliang@chromium.org>
2024-09-06 21:22:01 +0000

    [Discounts][clank] Add interfaces
    This CL adds two interface CommerceBottomSheetContentProvider and
    ContentListener. The implementation will land in another CL.
    
    Bug: 364463482
    Change-Id: Iab7a59b27a1a99fc5dd287183bfd6d0e34f909ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835280
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352293}

---
+++ chrome/browser/commerce/android/java/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentListener.java
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.commerce;
+
+import org.chromium.ui.modelutil.PropertyModel;
+
+/** A listener for individual feature content. */
+public interface CommerceBottomSheetContentListener {
+
+    /**
+     * Called when the content is ready to show.
+     *
+     * @param model The PropertyModel contains all relevant content for the CommerceBottomSheet. The
+     *     model should contain the following PropertyKey: TYPE, HAS_TITLE, TITLE_TEXT, and
+     *     CONTENT_VIEW.
+     */
+    void onContentReady(PropertyModel model);
+}
---
+++ chrome/browser/commerce/android/java/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentProvider.java
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.commerce;
+
+/**
+ * The interface to be implemented by the individual feature to show a View in the
+ * CommerceBottomSheet.
+ */
+public interface CommerceBottomSheetContentProvider {
+
+    /**
+     * Request the content to show in the CommerceBottomSheetContent.
+     *
+     * @param listener The listener that will run after the feature is finished gathering all
+     *     content.
+     */
+    void requestShowContent(CommerceBottomSheetContentListener listener);
+
+    /** Called when the BottomSheet hides. */
+    void hideContentView();
+}
