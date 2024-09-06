a3102dfc984cb391548bb90d35e399010110c897
Alexander Cooper <alcooper@chromium.org>
2024-09-06 22:58:48 +0000

    Fix inverted WebXr image
    https://crrev.com/c/5826430 recently changed the behavior of the
    ImageToBufferCopier class to properly respect the passed-in image's
    Origin and ensure that it aligned with the (default) origin specified on
    the destination shared image. This however meant that a y-flip was added
    to the WebXr rendering path on Windows (the only platform that makes use
    of this class). Given that this update means that the image matches the
    actual accompanying metadata, it was decided to patch this by updating
    OpenXr's expectations of when a flip should occur, rather than reverting
    this change.
    
    This change simply indicates that the DirectX binding expects an image
    flip to be needed all the time now, instead of just when using
    SharedImages (which presumably would have added their own flip
    somewhere along the way anyway).
    
    Bug: 364924725
    Change-Id: I4b22195d1c0e8479c82f856087f5e4006681e359
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5841864
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352347}

--- device/vr/openxr/windows/openxr_graphics_binding_d3d11.cc
+++ device/vr/openxr/windows/openxr_graphics_binding_d3d11.cc
@@ -275,7 +275,7 @@ void OpenXrGraphicsBindingD3D11::CleanupWithoutSubmit() {
 }
 
 bool OpenXrGraphicsBindingD3D11::ShouldFlipSubmittedImage() {
-  return IsUsingSharedImages();
+  return true;
 }
 
 void OpenXrGraphicsBindingD3D11::OnSwapchainImageSizeChanged() {
