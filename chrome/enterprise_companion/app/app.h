cd7804ab363e21dac9df7b6bb11d5a5c7b602b72
Noah Rose Ledesma <noahrose@google.com>
2024-09-06 17:00:05 +0000

    Create uninstaller for CECA
    Create an uninstaller which removes all traces of the app from the
    system. Like the installer, it is triggered via a command-line flag.
    
    Fixed: 364906907
    Change-Id: I1811ab136d6a0e912687e85281bbf96ea5cbca50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840407
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1352114}

--- chrome/enterprise_companion/app/app.h
+++ chrome/enterprise_companion/app/app.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/functional/bind.h"
+#include "base/functional/callback.h"
 #include "build/build_config.h"
 #include "chrome/enterprise_companion/enterprise_companion_client.h"
 #include "chrome/enterprise_companion/enterprise_companion_status.h"
@@ -57,7 +59,9 @@ std::unique_ptr<App> CreateAppInstall(
         base::BindOnce([] { return CreateAppShutdown()->Run(); }),
     base::OnceCallback<std::unique_ptr<ScopedLock>(base::TimeDelta timeout)>
         lock_provider = base::BindOnce(&CreateScopedLock),
-    base::OnceCallback<bool()> install_task = base::BindOnce(&Install));
+    base::OnceCallback<bool()> task = base::BindOnce(&Install));
+
+std::unique_ptr<App> CreateAppUninstall();
 
 #if BUILDFLAG(IS_MAC)
 // Creates an App which handles network requests for another process. If
