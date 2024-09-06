84bc4c16dd268a43e1f957f43a9854f0ef866a70
Yann Dago <ydago@chromium.org>
2024-09-06 19:18:42 +0000

    Add flag for the new enterprise profile creation screens
    Bug: 362282348
    Change-Id: I7933acfd9021243822d8accbe15dc46bb2942aa8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837606
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Commit-Queue: Igor Ruvinov <igorruvinov@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Reviewed-by: Igor Ruvinov <igorruvinov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352213}

--- chrome/browser/flag_descriptions.cc
+++ chrome/browser/flag_descriptions.cc
@@ -1121,6 +1121,12 @@ const char kEnterpriseProfileBadgingName[] =
 const char kEnterpriseProfileBadgingDescription[] =
     "Enable enterprise profile badging on the toolbar avatar and profile menu";
 
+const char kEnterpriseUpdatedProfileCreationScreenName[] =
+    "Enable enterprise updated profile creation screens";
+const char kEnterpriseUpdatedProfileCreationScreenDescription[] =
+    "Enable enterprise updated profile creation screens in the profile picker "
+    "and forced profile creation.";
+
 const char kEnableExperimentalCookieFeaturesName[] =
     "Enable experimental cookie features";
 const char kEnableExperimentalCookieFeaturesDescription[] =
