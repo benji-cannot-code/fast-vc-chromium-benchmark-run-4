cffca2309d4ec9b2e9d87f5db8aada5548a5620f
Ted Choc <tedchoc@chromium.org>
2024-09-06 04:38:37 +0000

    Move temporary OTR cookie file to the original profile dir.
    Previous location:
    <chrome_install_dir>/files/COOKIES.DAT
    
    New location:
    <chrome_install_dir>/app_chrome/[Profile Directory -- e.g. Default]/OTRTempState/COOKIES.DAT
    
    Bug: 40254448
    Change-Id: I33f33bb26dd7751978ae0d6e6a51fedbc6b700e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838406
    Commit-Queue: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351858}

--- chrome/browser/profiles/profile_android.cc
+++ chrome/browser/profiles/profile_android.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile_key.h"
 #include "chrome/browser/profiles/profile_key_android.h"
+#include "chrome/common/chrome_constants.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 
@@ -64,6 +65,11 @@ ScopedJavaLocalRef<jobject> JNI_Profile_GetOriginalProfile(JNIEnv* env,
   Profile* original_profile = self->GetOriginalProfile();
   DCHECK(original_profile);
   return original_profile->GetJavaObject();
+}
+
+jboolean JNI_Profile_IsInitialProfile(JNIEnv* env, jlong ptr) {
+  Profile* self = reinterpret_cast<Profile*>(ptr);
+  return self->GetBaseName().value() == chrome::kInitialProfile;
 }
 
 ScopedJavaLocalRef<jobject> JNI_Profile_GetOffTheRecordProfile(
