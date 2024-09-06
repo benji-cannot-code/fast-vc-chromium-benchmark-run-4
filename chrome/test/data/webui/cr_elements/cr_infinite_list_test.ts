a4aa4ce519cda43f5edcb06d65979140e4cf3b81
rbpotter <rbpotter@chromium.org>
2024-09-06 17:24:41 +0000

    WebUI: Move infinite-list to cr_elements
    This is in preparation for using it from the downloads manager element,
    which is being migrated to Lit in a followup.
    
    Bug: 40943652
    Change-Id: I7fb69004c2aa413ca805becc601fbdabeadd7618
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5834639
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352139}

--- chrome/test/data/webui/cr_elements/cr_infinite_list_test.ts
+++ chrome/test/data/webui/cr_elements/cr_infinite_list_test.ts
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'chrome://resources/cr_components/history_clusters/infinite_list.js';
+import 'chrome://resources/cr_elements/cr_infinite_list/cr_infinite_list.js';
 
-import type {InfiniteListElement} from 'chrome://resources/cr_components/history_clusters/infinite_list.js';
+import type {CrInfiniteListElement} from 'chrome://resources/cr_elements/cr_infinite_list/cr_infinite_list.js';
 import {CrLitElement, html} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 import {assertEquals, assertLT, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
@@ -60,18 +60,18 @@ class TestApp extends CrLitElement {
 
   override render() {
     return html`
-    <infinite-list .items="${this.listItems}" .scrollTarget="${this}"
+    <cr-infinite-list .items="${this.listItems}" .scrollTarget="${this}"
         .template=${(item: {name: string}, idx: number, tabidx: number) => html`
             <test-item name="${item.name}" id="item-${idx}"
                 tabindex="${tabidx}">
             </test-item>`}>
-    </infinite-list>`;
+    </cr-infinite-list>`;
   }
 }
 
 customElements.define('test-app', TestApp);
 
-function queryItems(infiniteList: InfiniteListElement): NodeListOf<TestItem> {
+function queryItems(infiniteList: CrInfiniteListElement): NodeListOf<TestItem> {
   return infiniteList.querySelectorAll<TestItem>('test-item');
 }
 
@@ -93,7 +93,8 @@ function getTestItems(count: number): Array<{name: string}> {
   return items.slice(0, count);
 }
 
-function getKeyboardFocusableItem(infiniteList: InfiniteListElement): TestItem {
+function getKeyboardFocusableItem(infiniteList: CrInfiniteListElement):
+    TestItem {
   const item = infiniteList.querySelector<TestItem>('test-item[tabindex="0"]');
   assertTrue(!!item);
   return item;
@@ -111,7 +112,7 @@ function createTestApp(): TestApp {
 }
 
 suite('InfiniteListTest', () => {
-  let infiniteList: InfiniteListElement;
+  let infiniteList: CrInfiniteListElement;
   let testApp: TestApp;
   let innerList: HTMLElement;
 
@@ -120,7 +121,7 @@ suite('InfiniteListTest', () => {
     testApp = createTestApp();
     testApp.listItems = sampleData;
 
-    infiniteList = testApp.shadowRoot!.querySelector('infinite-list')!;
+    infiniteList = testApp.shadowRoot!.querySelector('cr-infinite-list')!;
     const lazyList = infiniteList.querySelector('cr-lazy-list');
     assertTrue(!!lazyList);
     innerList = lazyList;
@@ -189,7 +190,7 @@ suite('InfiniteListTest', () => {
 });
 
 suite('InfiniteListFocusTest', () => {
-  let infiniteList: InfiniteListElement;
+  let infiniteList: CrInfiniteListElement;
   let testApp: TestApp;
   let innerList: HTMLElement;
 
@@ -198,7 +199,7 @@ suite('InfiniteListFocusTest', () => {
     testApp = createTestApp();
     testApp.listItems = sampleData;
 
-    infiniteList = testApp.shadowRoot!.querySelector('infinite-list')!;
+    infiniteList = testApp.shadowRoot!.querySelector('cr-infinite-list')!;
     const lazyList = infiniteList.querySelector('cr-lazy-list');
     assertTrue(!!lazyList);
     innerList = lazyList;
