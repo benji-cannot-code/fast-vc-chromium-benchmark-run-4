af41f12769e1db85848a227847c0cf8e2c6b7aa6
Aaron Leventhal <aleventhal@google.com>
2024-09-06 23:05:10 +0000

    [A11y] Reenable live region event tests
    Bug: 40716698, 1078490, 786848
    Change-Id: I1fb0703be598ba7c3d5e5a1f0f4246717e0815af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5842654
    Reviewed-by: Dirk Pranke <dpranke@google.com>
    Commit-Queue: Dirk Pranke <dpranke@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352351}

--- content/browser/accessibility/dump_accessibility_events_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_events_browsertest.cc
@@ -791,15 +791,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("listbox-next.html"));
 }
 
-// TODO(crbug.com/40716698): This is failing on Windows.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_AccessibilityEventsLiveRegionAdd \
-  DISABLED_AccessibilityEventsLiveRegionAdd
-#else
-#define MAYBE_AccessibilityEventsLiveRegionAdd AccessibilityEventsLiveRegionAdd
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
-                       MAYBE_AccessibilityEventsLiveRegionAdd) {
+                       AccessibilityEventsLiveRegionAdd) {
   RunEventTest(FILE_PATH_LITERAL("live-region-add.html"));
 }
 
@@ -813,16 +806,8 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("live-region-change.html"));
 }
 
-// Flaky on Windows: https://crbug.com/1078490.
-#if BUILDFLAG(IS_WIN)
-#define MAYBE_AccessibilityEventsLiveRegionCreate \
-  DISABLED_AccessibilityEventsLiveRegionCreate
-#else
-#define MAYBE_AccessibilityEventsLiveRegionCreate \
-  AccessibilityEventsLiveRegionCreate
-#endif
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
-                       MAYBE_AccessibilityEventsLiveRegionCreate) {
+                       AccessibilityEventsLiveRegionCreate) {
   RunEventTest(FILE_PATH_LITERAL("live-region-create.html"));
 }
 
@@ -836,15 +821,13 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
   RunEventTest(FILE_PATH_LITERAL("live-region-elem-reparent.html"));
 }
 
-// TODO(aboxhall): Fix flakiness.
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
                        AccessibilityEventsLiveRegionIgnoresClick) {
   RunEventTest(FILE_PATH_LITERAL("live-region-ignores-click.html"));
 }
 
-// http:/crbug.com/786848
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityEventsTest,
-                       DISABLED_AccessibilityEventsLiveRegionRemove) {
+                       AccessibilityEventsLiveRegionRemove) {
   RunEventTest(FILE_PATH_LITERAL("live-region-remove.html"));
 }
 
