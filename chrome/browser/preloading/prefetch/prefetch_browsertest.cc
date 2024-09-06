557b4c1cf629454442a79e8c2716dced8e15fe0c
Taiyo Mizuhashi <taiyo@chromium.org>
2024-09-06 07:04:42 +0000

    [CCT Prefetch] PreloadingAttempt for CCT prefetch triggers
    This CL prepares PreloadingPredictor and PreloadingAttempt for CCT
    prefetch triggers.
    
    Notes:
    - Normally, prefetch uses PrefetchServiceMatcher for the NVS-aware
      matching for is_accurate_triggering_ calculation performed on
      PreloadingDataImpl::DidStartNavigation. However, since current CCT
      prefetch doesn't support NVS, SameURLMatcher is sufficient.
    - Since the CCT prefetch's trigger is Android App,
      triggering_primary_page_source_id will be ukm::kInvalidSourceId.
      Thus, Preloading.Attempt.PreviousPrimaryPage will not be recorded and
      only Preloading.Attempt is recorded for now.
    
    Bug: 335331245
    Change-Id: Iab60e4a519951feeeb97db83909c21528a54e5ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5815488
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Commit-Queue: Taiyo Mizuhashi <taiyo@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Domenic Denicola <domenic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1351891}

--- chrome/browser/preloading/prefetch/prefetch_browsertest.cc
+++ chrome/browser/preloading/prefetch/prefetch_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/path_service.h"
 #include "base/test/scoped_feature_list.h"
+#include "base/timer/elapsed_timer.h"
+#include "chrome/browser/preloading/chrome_preloading.h"
 #include "chrome/browser/preloading/prefetch/chrome_prefetch_manager.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/test/base/chrome_test_utils.h"
@@ -13,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_test.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/prefetch_test_util.h"
+#include "content/public/test/preloading_test_util.h"
 #include "net/dns/mock_host_resolver.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -46,6 +49,12 @@ class PrefetchBrowserTest : public PlatformBrowserTest {
     ssl_server_.ServeFilesFromDirectory(
         base::PathService::CheckedGet(chrome::DIR_TEST_DATA));
     ASSERT_TRUE(ssl_server_.Start());
+
+    test_ukm_recorder_ = std::make_unique<ukm::TestAutoSetUkmRecorder>();
+    attempt_entry_builder_ =
+        std::make_unique<content::test::PreloadingAttemptUkmEntryBuilder>(
+            PredictorToExpectInUkm());
+    test_timer_ = std::make_unique<base::ScopedMockElapsedTimersForTest>();
   }
 
   void TearDownOnMainThread() override {
@@ -65,10 +74,30 @@ class PrefetchBrowserTest : public PlatformBrowserTest {
     return content::NavigateToURL(GetActiveWebContents(), url);
   }
 
+  ukm::TestAutoSetUkmRecorder* test_ukm_recorder() {
+    return test_ukm_recorder_.get();
+  }
+
+  const content::test::PreloadingAttemptUkmEntryBuilder&
+  attempt_entry_builder() {
+    return *attempt_entry_builder_;
+  }
+
+ protected:
+  // Used when creating `test_ukm_recorder_`.
+  content::PreloadingPredictor PredictorToExpectInUkm() {
+    return chrome_preloading_predictor::kChromeCustomTabs;
+  }
+
  private:
   net::test_server::EmbeddedTestServer ssl_server_{
       net::test_server::EmbeddedTestServer::TYPE_HTTPS};
   base::test::ScopedFeatureList feature_list_;
+
+  std::unique_ptr<ukm::TestAutoSetUkmRecorder> test_ukm_recorder_;
+  std::unique_ptr<content::test::PreloadingAttemptUkmEntryBuilder>
+      attempt_entry_builder_;
+  std::unique_ptr<base::ScopedMockElapsedTimersForTest> test_timer_;
 };
 
 #if BUILDFLAG(IS_ANDROID)
@@ -94,6 +123,24 @@ IN_PROC_BROWSER_TEST_F(PrefetchBrowserTest, CCTPrefetch) {
   EXPECT_EQ(
       test_prefetch_watcher.GetPrefetchContainerIdForTestingInLastNavigation(),
       prefetch_container_id);
+
+  auto cct_attempt_entry_builder =
+      std::make_unique<content::test::PreloadingAttemptUkmEntryBuilder>(
+          chrome_preloading_predictor::kChromeCustomTabs);
+
+  ukm::SourceId ukm_source_id =
+      GetActiveWebContents()->GetPrimaryMainFrame()->GetPageUkmSourceId();
+  content::test::ExpectPreloadingAttemptUkm(
+      *test_ukm_recorder(),
+      {cct_attempt_entry_builder->BuildEntry(
+          ukm_source_id, content::PreloadingType::kPrefetch,
+          content::PreloadingEligibility::kEligible,
+          content::PreloadingHoldbackStatus::kAllowed,
+          content::PreloadingTriggeringOutcome::kSuccess,
+          content::PreloadingFailureReason::kUnspecified,
+          /*accurate=*/true,
+          /*ready_time=*/
+          base::ScopedMockElapsedTimersForTest::kMockElapsedTime)});
 }
 #endif  // BUILDFLAG(IS_ANDROID)
 
