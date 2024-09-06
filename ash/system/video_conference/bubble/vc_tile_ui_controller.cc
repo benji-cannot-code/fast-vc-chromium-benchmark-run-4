63b3788d6d52561e5c2f6d565750462919e39646
Alex Newcomer <newcomer@google.com>
2024-09-06 20:26:30 +0000

    Add scoped crash key
    To detect which effect is causing the crash, add a crash key.
    
    Bug: b:356399890
    Change-Id: Id4f63c14555693ff134ca1dbecb674b8ccbfebaf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838713
    Auto-Submit: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Andre Le <leandre@chromium.org>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352252}

--- ash/system/video_conference/bubble/vc_tile_ui_controller.cc
+++ ash/system/video_conference/bubble/vc_tile_ui_controller.cc
@@ -16,7 +16,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/video_conference/video_conference_utils.h"
 #include "base/barrier_callback.h"
 #include "base/containers/flat_set.h"
+#include "base/debug/crash_logging.h"
 #include "base/metrics/histogram_functions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice.pb.h"
 #include "chromeos/ash/components/dbus/dlcservice/dlcservice_client.h"
 #include "chromeos/utils/haptics_util.h"
@@ -230,6 +232,9 @@ void VcTileUiController::OnDlcDownloadStateFetched(
   if (!tile_) {
     return;
   }
+
+  SCOPED_CRASH_KEY_STRING32("VCTileUIC", "label",
+                            base::UTF16ToUTF8(effect_state_label_for_debug_));
 
   CHECK(effect_state_)
       << "DLC State retrieved, but `effect_state_` is no longer valid for: "
