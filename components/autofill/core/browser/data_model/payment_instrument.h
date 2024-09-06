5880cdbefec37be112e1a306e666cd0c7508250b
Junhui He <junhuihe@google.com>
2024-09-06 19:25:01 +0000

    Add is_fido_enrolled to PaymentInstrument data model
    This field will be used to to indicate whether the user need to complete
    a one-time verification in the FOP selector during eWallet payment flow.
    
    Bug: 40280186
    Change-Id: I5a182da2b25541d1211170dc6bc9da9eff4b227e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5827730
    Reviewed-by: Siddharth Shah <siashah@chromium.org>
    Commit-Queue: Junhui He <junhuihe@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352216}

--- components/autofill/core/browser/data_model/payment_instrument.h
+++ components/autofill/core/browser/data_model/payment_instrument.h
@@ -39,7 +39,8 @@ class PaymentInstrument final {
   PaymentInstrument(int64_t instrument_id,
                     std::u16string nickname,
                     GURL display_icon_url,
-                    DenseSet<PaymentInstrument::PaymentRail> supported_rails);
+                    DenseSet<PaymentInstrument::PaymentRail> supported_rails,
+                    bool is_fido_enrolled = false);
   PaymentInstrument(const PaymentInstrument& other);
   PaymentInstrument& operator=(const PaymentInstrument& other);
   ~PaymentInstrument();
@@ -59,6 +60,8 @@ class PaymentInstrument final {
 
   const GURL& display_icon_url() const { return display_icon_url_; }
 
+  bool is_fido_enrolled() const { return is_fido_enrolled_; }
+
  private:
   // This is the ID assigned by the payments backend to uniquely identify this
   // PaymentInstrument.
@@ -72,6 +75,9 @@ class PaymentInstrument final {
 
   // All the payment rails that are supported by this instrument.
   DenseSet<PaymentRail> supported_rails_;
+
+  // Whether the device is enrolled in FIDO for this instrument.
+  bool is_fido_enrolled_;
 };
 
 }  // namespace autofill
--- components/autofill/core/browser/data_model/payment_instrument_unittest.cc
+++ components/autofill/core/browser/data_model/payment_instrument_unittest.cc
@@ -15,7 +15,8 @@ TEST(PaymentInstrumentTest, VerifyFieldValues) {
       100, u"test_nickname", GURL("http://www.example.com"),
       DenseSet<PaymentInstrument::PaymentRail>(
           {PaymentInstrument::PaymentRail::kPix,
-           PaymentInstrument::PaymentRail::kUnknown}));
+           PaymentInstrument::PaymentRail::kUnknown}),
+      /*is_fido_enrolled=*/true);
 
   EXPECT_EQ(100, payment_instrument.instrument_id());
   EXPECT_EQ(u"test_nickname", payment_instrument.nickname());
@@ -25,6 +26,7 @@ TEST(PaymentInstrumentTest, VerifyFieldValues) {
       payment_instrument.IsSupported(PaymentInstrument::PaymentRail::kPix));
   EXPECT_TRUE(
       payment_instrument.IsSupported(PaymentInstrument::PaymentRail::kUnknown));
+  EXPECT_TRUE(payment_instrument.is_fido_enrolled());
 }
 
 TEST(PaymentInstrumentTest, IsSupported_ReturnsFalseForUnsupportedPaymentRail) {
@@ -34,6 +36,15 @@ TEST(PaymentInstrumentTest, IsSupported_ReturnsFalseForUnsupportedPaymentRail) {
 
   EXPECT_FALSE(
       payment_instrument.IsSupported(PaymentInstrument::PaymentRail::kPix));
+}
+
+TEST(PaymentInstrumentTest, IsFidoEnrolled_ReturnsFalse) {
+  PaymentInstrument payment_instrument(
+      100, u"test_nickname", GURL("http://www.example.com"),
+      DenseSet<PaymentInstrument::PaymentRail>({}),
+      /*is_fido_enrolled=*/false);
+
+  EXPECT_FALSE(payment_instrument.is_fido_enrolled());
 }
 
 }  // namespace autofill
