1fddeacc5683d992b53a9d9b5c1131bd0d4331cf
danakj <danakj@chromium.org>
2024-09-06 13:45:08 +0000

    Remove RefCountedBytes unsafe ctor, use spans in devtools fromSpan
    Bug: 40284755
    Low-Coverage-Reason: changing types in existing code
    Change-Id: Ifcd1248c45e631df7f78bc3637538fdf597dabce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836909
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352005}

--- third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
+++ third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
+#include "base/containers/span.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/values.h"
@@ -113,7 +114,7 @@ class CRDTP_EXPORT Binary : public Serializable {
   static Binary fromRefCounted(scoped_refptr<base::RefCountedMemory> memory);
   static Binary fromVector(std::vector<uint8_t> data);
   static Binary fromString(std::string data);
-  static Binary fromSpan(const uint8_t* data, size_t size);
+  static Binary fromSpan(base::span<const uint8_t> data);
 
  private:
   explicit Binary(scoped_refptr<base::RefCountedMemory> bytes);
