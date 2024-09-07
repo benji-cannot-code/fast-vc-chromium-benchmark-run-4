09cb0be933518bf9fb2d4b3c99271994bf58c884
Dmitry Vykochko <vykochko@google.com>
2024-09-07 06:48:30 +0000

    Implement the UI of user annotations entries on the settings page.
    The API calls are not yet implemented, stub data is used.
    
    UI implementation:
    https://screenshot.googleplex.com/B4RTXNEkYtSQDMX
    https://screenshot.googleplex.com/3UULwxjbwPDg55m
    
    Bug: 361437117
    Change-Id: I978646c8838cfb7df8e8a1be04e5821c3c545ff6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836859
    Reviewed-by: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Auto-Submit: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Kelvin Jiang <kelvinjiang@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352417}

---
+++ chrome/browser/resources/settings/autofill_page/user_annotations_manager_proxy.ts
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+type UserAnnotationsEntry = chrome.autofillPrivate.UserAnnotationsEntry;
+
+/**
+ * This interface defines the autofill API wrapper that combines user
+ * annotations related methods.
+ */
+export interface UserAnnotationsManagerProxy {
+  /**
+   * Returns user annotations entries.
+   */
+  getEntries(): Promise<UserAnnotationsEntry[]>;
+}
+
+export class UserAnnotationsManagerProxyImpl implements
+    UserAnnotationsManagerProxy {
+  getEntries(): Promise<UserAnnotationsEntry[]> {
+    return chrome.autofillPrivate.getUserAnnotationsEntries();
+  }
+
+  static getInstance(): UserAnnotationsManagerProxy {
+    return instance || (instance = new UserAnnotationsManagerProxyImpl());
+  }
+
+  static setInstance(obj: UserAnnotationsManagerProxy): void {
+    instance = obj;
+  }
+}
+
+let instance: UserAnnotationsManagerProxy|null = null;
