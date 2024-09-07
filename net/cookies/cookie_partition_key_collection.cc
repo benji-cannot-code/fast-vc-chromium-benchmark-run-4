2bfc6315324d941e4dd133eae9777d12d21f43c9
selya <selya@chromium.org>
2024-09-07 14:55:47 +0000

    Update extensions to include partitioned cookie ancestor chain bit
    Add new optional bool hasCrossSiteAncestor to the CookiePartitionKey object in cookies.json and update implementations that use the CookiePartitionKey to take into account the new value.
    
    Add new cases to unit tests to provide coverage for the new value and update the existing unit tests to use a CookiePartitionKey object.
    
    More background information on why this change is being made can be found at the following github link: https://github.com/privacycg/CHIPS/issues/6
    
    
    
    Bug: 330778448, 361862746, 362480909
    Change-Id: I78e87cf2db806792ba50d477a59b8c2ffcc58ae9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5386307
    Commit-Queue: Aaron Selya <selya@google.com>
    Reviewed-by: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352445}

--- net/cookies/cookie_partition_key_collection.cc
+++ net/cookies/cookie_partition_key_collection.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
+#include "base/types/expected.h"
 #include "net/base/schemeful_site.h"
 #include "net/cookies/cookie_access_delegate.h"
 #include "net/cookies/cookie_partition_key.h"
@@ -63,6 +64,22 @@ bool operator==(const CookiePartitionKeyCollection& lhs,
   }
 
   return lhs.PartitionKeys() == rhs.PartitionKeys();
+}
+
+CookiePartitionKeyCollection CookiePartitionKeyCollection::MatchesSite(
+    const net::SchemefulSite& top_level_site) {
+  base::expected<net::CookiePartitionKey, std::string> same_site_key =
+      CookiePartitionKey::FromWire(
+          top_level_site, CookiePartitionKey::AncestorChainBit::kSameSite);
+  base::expected<net::CookiePartitionKey, std::string> cross_site_key =
+      CookiePartitionKey::FromWire(
+          top_level_site, CookiePartitionKey::AncestorChainBit::kCrossSite);
+
+  CHECK(cross_site_key.has_value());
+  CHECK(same_site_key.has_value());
+
+  return net::CookiePartitionKeyCollection(
+      {same_site_key.value(), cross_site_key.value()});
 }
 
 std::ostream& operator<<(std::ostream& os,
