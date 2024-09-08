38cebc1adef53cc9cdd69e03f8c5477fad914f60
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-09-08 23:49:22 +0000

    PA: Revert "Allow std::hardware_destructive_interference_size."
    For a few months at least, Skia still supports C++17. PartitionAlloc
    can't use std::hardware_destructive_interference_size
    Failed build:
    https://skia-review.googlesource.com/c/skia/+/862356/15
    
    This reverts parts of commit fc838e8cc887adbe95110045d146b9d5885bf2a9.
    https://chromium-review.googlesource.com/c/chromium/src/+/5767325
    
    CC: pkasting@chromium.org
    CC: jbroman@chromium.org
    Bug: 351867706
    Change-Id: I4a1cd578fcb6bec37cd423d1a62c994f31d42da7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5841144
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Auto-Submit: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1352526}

--- base/allocator/partition_allocator/src/partition_alloc/partition_root.h
+++ base/allocator/partition_allocator/src/partition_alloc/partition_root.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstddef>
 #include <cstdint>
 #include <limits>
-#include <new>
 #include <optional>
 #include <utility>
 
@@ -227,7 +226,7 @@ struct alignas(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
   //
   // Careful! PartitionAlloc's performance is sensitive to its layout.  Please
   // put the fast-path objects in the struct below.
-  struct alignas(std::hardware_destructive_interference_size) Settings {
+  struct alignas(internal::kPartitionCachelineSize) Settings {
     // Chromium-style: Complex constructor needs an explicit out-of-line
     // constructor.
     Settings();
@@ -285,7 +284,7 @@ struct alignas(64) PA_COMPONENT_EXPORT(PARTITION_ALLOC) PartitionRoot {
 
   // Not used on the fastest path (thread cache allocations), but on the fast
   // path of the central allocator.
-  alignas(std::hardware_destructive_interference_size) internal::Lock lock_;
+  alignas(internal::kPartitionCachelineSize) internal::Lock lock_;
 
   Bucket buckets[internal::kNumBuckets] = {};
   Bucket sentinel_bucket{};
