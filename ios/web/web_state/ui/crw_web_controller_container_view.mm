03ed0b90249a71235bf02a4d2e40f3f1fe952ed9
Daniel White <danieltwhite@google.com>
2024-09-09 15:02:49 +0000

    [iOS] Deprecate Old UITrait function in CRWWebControllerContainerView
    Since didTraitCollectionChange is deprecated in iOS 17,
    CRWWebControllerContainerView was updated to use the new alternative
    system, registerForTraitChanges.
    
    Fixed: 349390653
    Change-Id: I78f6f949927b2c9607ac55d2681ff47dea916c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837437
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352745}

--- ios/web/web_state/ui/crw_web_controller_container_view.mm
+++ ios/web/web_state/ui/crw_web_controller_container_view.mm
@@ -38,6 +38,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.backgroundColor = [UIColor whiteColor];
     self.autoresizingMask =
         UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    if (@available(iOS 17, *)) {
+      __weak __typeof(self) weakSelf = self;
+      UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
+                                       UITraitCollection* previousCollection) {
+        [weakSelf updateUIOnTraitChange:previousCollection];
+      };
+      NSArray<UITrait>* traits = @[
+        UITraitVerticalSizeClass.self, UITraitHorizontalSizeClass.self,
+        UITraitPreferredContentSizeCategory.self
+      ];
+      [self registerForTraitChanges:traits withHandler:handler];
+    }
   }
   return self;
 }
@@ -81,28 +93,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark Layout
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if ((self.traitCollection.verticalSizeClass !=
-       previousTraitCollection.verticalSizeClass) ||
-      (self.traitCollection.horizontalSizeClass !=
-       previousTraitCollection.horizontalSizeClass) ||
-      self.traitCollection.preferredContentSizeCategory !=
-          previousTraitCollection.preferredContentSizeCategory) {
-    // Reset zoom scale when the window is resized (portrait to landscape,
-    // landscape to portrait or multi-window resizing), or if text size is
-    // modified as websites can adjust to the preferred content size (using
-    // font: -apple-system-body;). It avoids being in a different zoomed
-    // position from where the user initially zoomed.
-    UIScrollView* scrollView = self.contentViewProxy.contentView.scrollView;
-    scrollView.zoomScale = scrollView.minimumZoomScale;
+  if (@available(iOS 17, *)) {
+    return;
   }
-  if (previousTraitCollection.preferredContentSizeCategory !=
-      self.traitCollection.preferredContentSizeCategory) {
-    // In case the preferred content size changes, the layout is dirty.
-    [self setNeedsLayout];
-  }
+
+  [self updateUIOnTraitChange:previousTraitCollection];
 }
+#endif
 
 - (void)layoutSubviews {
   [super layoutSubviews];
@@ -200,6 +200,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [self.webViewContentView
           setFrame:UIEdgeInsetsInsetRect(self.bounds, self.safeAreaInsets)];
     }
+  }
+}
+
+//
+- (void)updateUIOnTraitChange:(UITraitCollection*)previousTraitCollection {
+  if ((self.traitCollection.verticalSizeClass !=
+       previousTraitCollection.verticalSizeClass) ||
+      (self.traitCollection.horizontalSizeClass !=
+       previousTraitCollection.horizontalSizeClass) ||
+      self.traitCollection.preferredContentSizeCategory !=
+          previousTraitCollection.preferredContentSizeCategory) {
+    // Reset zoom scale when the window is resized (portrait to landscape,
+    // landscape to portrait or multi-window resizing), or if text size is
+    // modified as websites can adjust to the preferred content size (using
+    // font: -apple-system-body;). It avoids being in a different zoomed
+    // position from where the user initially zoomed.
+    UIScrollView* scrollView = self.contentViewProxy.contentView.scrollView;
+    scrollView.zoomScale = scrollView.minimumZoomScale;
+  }
+  if (previousTraitCollection.preferredContentSizeCategory !=
+      self.traitCollection.preferredContentSizeCategory) {
+    // In case the preferred content size changes, the layout is dirty.
+    [self setNeedsLayout];
   }
 }
 
