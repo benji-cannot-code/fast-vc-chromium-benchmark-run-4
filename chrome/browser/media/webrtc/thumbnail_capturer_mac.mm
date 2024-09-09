d07ab686f88ed42b60007f88dd5988ef22a28349
Palak Agarwal <agpalak@chromium.org>
2024-09-09 14:17:02 +0000

    Remove UseSCContentSharingPickerSonoma flag
    This flag can be removed now as the purpose of the flag was to allow for
    testing on Sonoma, but since the behavior of the picker is different on
    Sonoma and Sequoia, this won't help us in preparing the picker for the
    Sequoia launch.
    
    
    Bug: 365525059
    Change-Id: I0ffc112ec20f7ed86aaa81dbd13d54703083e4d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844636
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Palak Agarwal <agpalak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352721}

--- chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
+++ chrome/browser/media/webrtc/thumbnail_capturer_mac.mm
@@ -42,13 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BASE_FEATURE(kUseSCContentSharingPicker,
              "UseSCContentSharingPicker",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-// Use the built-in MacOS screen-sharing picker (SCContentSharingPicker) on
-// MacOS 14 Sonoma and later. This flag will use the built-in picker for all
-// MacOS versions where it is supported.
-BASE_FEATURE(kUseSCContentSharingPickerSonoma,
-             "UseSCContentSharingPickerSonoma",
-             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 using SampleCallback =
@@ -1010,11 +1003,6 @@ void ThumbnailCapturerMac::OnCapturedFrame(
 bool ShouldUseSCContentSharingPicker() {
   if (@available(macOS 15.0, *)) {
     if (base::FeatureList::IsEnabled(kUseSCContentSharingPicker)) {
-      return true;
-    }
-  }
-  if (@available(macOS 14.4, *)) {
-    if (base::FeatureList::IsEnabled(kUseSCContentSharingPickerSonoma)) {
       return true;
     }
   }
