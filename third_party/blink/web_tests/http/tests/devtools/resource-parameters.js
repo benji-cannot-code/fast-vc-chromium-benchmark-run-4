8e7fe9e7970e634015658dc6d8fa52334d5a0329
Benedikt Meurer <bmeurer@chromium.org>
2024-09-09 19:10:19 +0000

    [DevTools] Rebaseline tests after https://crrev.com/c/5836833.
    Bug: 361717594
    Change-Id: Id0dc99539660c4e4adf6e61aa242b11f898b5557
    Fixed: 365511944
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844933
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352877}

--- third_party/blink/web_tests/http/tests/devtools/resource-parameters.js
+++ third_party/blink/web_tests/http/tests/devtools/resource-parameters.js
@@ -32,7 +32,7 @@ import * as SDK from 'devtools/core/sdk/sdk.js';
     var request = event.data;
     TestRunner.addResult(request.url());
     TestRunner.addObject(
-        await NetworkTestRunner.buildHARLogEntry(request),
+        await NetworkTestRunner.buildHARLogEntry(request, {sanitize: false}),
         NetworkTestRunner.HARPropertyFormattersWithSize);
     TestRunner.completeTest();
   }
