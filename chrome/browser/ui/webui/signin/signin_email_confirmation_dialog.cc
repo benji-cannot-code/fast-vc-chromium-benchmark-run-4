8d2cd1dbfb0e73229244c5651437254a67e08271
Maksim Sisov <msisov@igalia.com>
2024-09-09 09:04:52 +0000

    modularize //c/b/ui/signin + partial //c/b/ui/w/signin
    ... and //c/b/signin
    
    This CL does full modularization of //c/b/ui/signin and
    partial modularization of //c/b/ui/w/signin and
    //c/b/signin
    
    There is a partial modularization as it blocks
    //c/b/ui/signin. Currently, webui/signin and //c/b/signin
    cannot be completely modularized as it they have other
    dependencies, which also need to modularized (or they
    depend on eacher other from their headers), but there are
    quite a few circular deps, which hard to resolve at once
    
    Thus, this CL has a small kick-off for those additional
    two, which unblocks //c/b/ui/signin at least..
    
    R=erikchen@chromium.org
    CC=tonikitoo@igalia.com
    
    Bug: 364179196, 364179196, 364667553
    Change-Id: I0c9c2ea343d8e164c8d33c48d90eb68441a6c574
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839720
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1352602}

--- chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.cc
+++ chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/values.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/webui/constrained_web_dialog_ui.h"
 #include "chrome/browser/ui/webui/signin/signin_email_confirmation_ui.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/host_zoom_map.h"
--- chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h
+++ chrome/browser/ui/webui/signin/signin_email_confirmation_dialog.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "chrome/browser/ui/signin/signin_view_controller_delegate.h"
-#include "chrome/browser/ui/webui/constrained_web_dialog_ui.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
 
