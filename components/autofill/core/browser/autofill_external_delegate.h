d37a6d66ce414630bf3c157c14a56cc51b6464e6
Jan Keitel <jkeitel@google.com>
2024-09-09 09:35:26 +0000

    Add method to AED to create a safe 'hide popup' callback
    The method will be used in a follow-up CL to allow hiding the popup
    not directly on accepting a suggestion, but only once a subsequent
    network call has completed.
    
    In addition, this CL adds a method to AutofillPlusAddressDelegate
    for accepting inline suggestions and passes a hide popup callback to
    it.
    
    This is done in this CL to test the hide popup callback, since
    creating it is not exposed publicly in AED.
    
    Bug: 362445807
    Change-Id: Ifaa9eff20112986902816b9389ac5da387ae6415
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839730
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352615}

--- components/autofill/core/browser/autofill_external_delegate.h
+++ components/autofill/core/browser/autofill_external_delegate.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/suggestions_list_metrics.h"
 #include "components/autofill/core/browser/ui/autofill_suggestion_delegate.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
+#include "components/autofill/core/browser/ui/suggestion_hiding_reason.h"
 #include "components/autofill/core/browser/ui/suggestion_type.h"
 #include "components/autofill/core/common/aliases.h"
 #include "components/autofill/core/common/form_data.h"
@@ -171,6 +172,14 @@ class AutofillExternalDelegate : public AutofillSuggestionDelegate,
   base::RepeatingCallback<void(std::vector<Suggestion>,
                                AutofillSuggestionTriggerSource)>
   CreateUpdateSuggestionsCallback();
+
+  // Returns a callback that, when run, attempts to close the currently shown
+  // suggestion UI. If the `SuggestionUiSessionId` of the currently showing UI
+  // surface has changed between when this callback is created and when it is
+  // run, running it is a no-op. The callback is also safe to call even if
+  // `this` is no longer alive.
+  base::OnceCallback<void(SuggestionHidingReason)>
+  CreateHideSuggestionsCallback();
 
   // Private handler for DidAcceptSuggestions for address related suggestions.
   void DidAcceptAddressSuggestion(const Suggestion& suggestion,
--- components/autofill/core/browser/autofill_plus_address_delegate.h
+++ components/autofill/core/browser/autofill_plus_address_delegate.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/callback_forward.h"
 #include "components/autofill/core/browser/autofill_client.h"
+#include "components/autofill/core/browser/ui/suggestion_hiding_reason.h"
 #include "components/autofill/core/browser/ui/suggestion_type.h"
 #include "components/autofill/core/common/aliases.h"
 
@@ -131,6 +132,19 @@ class AutofillPlusAddressDelegate {
       const url::Origin& primary_main_frame_origin,
       base::span<const Suggestion> current_suggestions,
       UpdateSuggestionsCallback update_suggestions_callback) = 0;
+
+  using HideSuggestionsCallback =
+      base::OnceCallback<void(SuggestionHidingReason)>;
+  // Attempts to create the plus address in
+  // `current_suggestions[current_suggestion_index]` for
+  // `primary_main_frame_origin`.
+  virtual void OnAcceptedInlineSuggestion(
+      const url::Origin& primary_main_frame_origin,
+      base::span<const Suggestion> current_suggestions,
+      size_t current_suggestion_index,
+      UpdateSuggestionsCallback update_suggestions_callback,
+      HideSuggestionsCallback hide_suggestions_callback,
+      PlusAddressCallback fill_field_callback) = 0;
 };
 
 }  // namespace autofill
