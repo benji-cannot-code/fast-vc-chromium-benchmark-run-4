29b1130b4deb1cce68e8c37696f806eca339ec39
Mikel Astiz <mastiz@chromium.org>
2024-09-09 17:11:14 +0000

    [sync] Move controller creation to SyncServiceFactory in chrome/
    Refactoring without expected behavioral changes.
    
    The code is moved with minimal changes from ChromeSyncClient to
    a newly-introduced file and class, ChromeSyncControllerBuilder, which
    gets exercised directly SyncServiceFactory. CommonControllerBuilder is
    now also directly exercised from the factory, leaving ChromeSyncClient
    outside the business of constructing controllers.
    
    Most notably, this moves the exercising of factories closer to the
    declaration of dependencies across factories.
    
    Change-Id: I004d652d89069dd50f4b760c9598a856a39f12e2
    Bug: 335688372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5841671
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352840}

--- chrome/browser/sync/sync_service_factory_unittest.cc
+++ chrome/browser/sync/sync_service_factory_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/feature_list.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
+#include "base/test/bind.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/commerce/core/commerce_feature_list.h"
 #include "components/data_sharing/public/features.h"
 #include "components/saved_tab_groups/features.h"
+#include "components/spellcheck/spellcheck_buildflags.h"
 #include "components/sync/base/command_line_switches.h"
 #include "components/sync/base/data_type.h"
 #include "components/sync/base/features.h"
@@ -29,6 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/browser_task_environment.h"
 #include "extensions/buildflags/buildflags.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+#if BUILDFLAG(ENABLE_SPELLCHECK)
+#include "chrome/browser/spellchecker/spellcheck_factory.h"
+#include "chrome/browser/spellchecker/spellcheck_service.h"
+#endif  // BUILDFLAG(ENABLE_SPELLCHECK)
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
 #include "ash/constants/ash_features.h"
@@ -67,6 +74,17 @@ class SyncServiceFactoryTest : public testing::Test {
     // Some services will only be created if there is a WebDataService.
     builder.AddTestingFactory(WebDataServiceFactory::GetInstance(),
                               WebDataServiceFactory::GetDefaultFactory());
+
+#if BUILDFLAG(ENABLE_SPELLCHECK)
+    builder.AddTestingFactory(
+        SpellcheckServiceFactory::GetInstance(),
+        base::BindRepeating(base::BindLambdaForTesting(
+            [](content::BrowserContext* browser_context) {
+              return std::unique_ptr<KeyedService>(
+                  std::make_unique<SpellcheckService>(browser_context));
+            })));
+#endif  // BUILDFLAG(ENABLE_SPELLCHECK)
+
     profile_ = builder.Build();
   }
 
