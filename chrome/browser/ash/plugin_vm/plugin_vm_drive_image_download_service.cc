025c0f7fd5d6268d684c28fd28222e3056e0c9e9
Elly <ellyjones@chromium.org>
2024-09-09 16:20:21 +0000

    crypto: spanify uses of SecureHash 1/n
    This change either:
    * Migrates uses of SecureHash's (pointer, length) APIs to span APIs, or
    * Migrates uses that could use a one-shot hash to the simpler API in
      crypto/sha2.h
    
    Bug: 364687923
    Change-Id: I44956b2418563a162bb01df223e2a53ee393db97
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840148
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352786}

--- chrome/browser/ash/plugin_vm/plugin_vm_drive_image_download_service.cc
+++ chrome/browser/ash/plugin_vm/plugin_vm_drive_image_download_service.cc
@@ -119,8 +119,7 @@ PluginVmDriveImageDownloadService::PluginVmDriveImageDownloadService(
     PluginVmInstaller* plugin_vm_installer,
     Profile* profile)
     : plugin_vm_installer_(plugin_vm_installer),
-      secure_hash_service_(
-          crypto::SecureHash::Create(crypto::SecureHash::SHA256)) {
+      hasher_(crypto::SecureHash::Create(crypto::SecureHash::SHA256)) {
   signin::IdentityManager* identity_manager =
       IdentityManagerFactory::GetForProfile(profile);
 
@@ -188,7 +187,7 @@ void PluginVmDriveImageDownloadService::CancelDownload() {
 }
 
 void PluginVmDriveImageDownloadService::ResetState() {
-  secure_hash_service_ = crypto::SecureHash::Create(crypto::SecureHash::SHA256);
+  hasher_ = crypto::SecureHash::Create(crypto::SecureHash::SHA256);
   total_bytes_downloaded_ = 0;
 }
 
@@ -226,7 +225,7 @@ void PluginVmDriveImageDownloadService::DownloadActionCallback(
   completion_info.path = download_file_path_;
   completion_info.bytes_downloaded = total_bytes_downloaded_;
   std::array<uint8_t, 32> sha256_hash;
-  secure_hash_service_->Finish(sha256_hash.data(), sha256_hash.size());
+  hasher_->Finish(sha256_hash);
   completion_info.hash256 = base::HexEncode(sha256_hash);
   plugin_vm_installer_->OnDownloadCompleted(completion_info);
 }
@@ -244,7 +243,7 @@ void PluginVmDriveImageDownloadService::GetContentCallback(
   if (first_chunk)
     ResetState();
 
-  secure_hash_service_->Update(content->c_str(), content->length());
+  hasher_->Update(base::as_byte_span(*content));
   total_bytes_downloaded_ += content->length();
 }
 
