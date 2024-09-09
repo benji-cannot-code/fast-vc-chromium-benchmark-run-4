0baff59ac712b8f99fb7b8cd697aa51b24743329
Liza Burakova <liza@chromium.org>
2024-09-09 17:00:50 +0000

    Initialize image decoder settings dynamically.
    This CL uses fuzzer input to initialize each image decoder in the
    jpeg, bmp, and png decoder fuzzers dynamically in order to improve
    fuzzing converage.
    
    This CL also adds some utility functions to reduce code-reuse in
    the fuzzers, and moves the bmp_image_decoder_fuzzer into the same
    BUILD file as the other two fuzzers for consistency.
    
    Bug: 323934468
    Change-Id: I3d415255d11e0b1a253c56e82281441713775988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5775712
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Liza Burakova <liza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352826}

--- third_party/blink/renderer/platform/image-decoders/jpeg/jpeg_image_decoder_fuzzer.cc
+++ third_party/blink/renderer/platform/image-decoders/jpeg/jpeg_image_decoder_fuzzer.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/image-decoders/jpeg/jpeg_image_decoder.h"
 
+#include <fuzzer/FuzzedDataProvider.h>
 #include <stddef.h>
 #include <stdint.h>
 
@@ -12,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/graphics/color_behavior.h"
 #include "third_party/blink/renderer/platform/image-decoders/image_decoder.h"
+#include "third_party/blink/renderer/platform/image-decoders/image_decoder_fuzzer_utils.h"
 #include "third_party/blink/renderer/platform/testing/blink_fuzzer_test_support.h"
 #include "third_party/blink/renderer/platform/testing/task_environment.h"
 #include "third_party/blink/renderer/platform/wtf/shared_buffer.h"
@@ -19,27 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-std::unique_ptr<ImageDecoder> CreateJPEGDecoder() {
-  // TODO(crbug.com/323934468): Initialize decoder settings dynamically using
-  // fuzzer input.
-  return std::make_unique<JPEGImageDecoder>(
-      ImageDecoder::kAlphaPremultiplied, ColorBehavior::kTransformToSRGB,
-      cc::AuxImage::kDefault, ImageDecoder::kNoDecodedImageByteLimit);
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   static BlinkFuzzerTestSupport test_support;
-  test::TaskEnvironment task_environment;
-  auto buffer = SharedBuffer::Create(data, size);
-  auto decoder = CreateJPEGDecoder();
-  const bool kAllDataReceived = true;
-  decoder->SetData(buffer.get(), kAllDataReceived);
-  for (wtf_size_t frame = 0; frame < decoder->FrameCount(); ++frame) {
-    decoder->DecodeFrameBufferAtIndex(frame);
-    if (decoder->Failed()) {
-      return 0;
-    }
-  }
+  FuzzedDataProvider fdp(data, size);
+  FuzzDecoder(DecoderType::kJpegDecoder, fdp);
   return 0;
 }
 
