350147de431c0049966fce4c7a278a38298a8192
Adam Rice <ricea@chromium.org>
2024-09-09 11:43:25 +0000

    Stop using CompareCaseInsensitiveASCII() for equality
    Replace uses of CompareCaseInsensitiveASCII() for equality in //net and
    //services/network with more efficient constructs. In three cases, this
    results in use of base::FixedFlatMap. In the other case, we use
    EqualsCaseInsensitiveASCII() instead, as it avoids examining the
    characters when the lengths differ.
    
    Align net::ProcessReferrerPolicyHeaderOnRedirect() and
    network::ParseReferrerPolicy() to use the same underlying
    implementation.
    
    Change-Id: I14e59091c1b4d466d32416c1ecb3968c34bb53c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5832828
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352661}

--- net/url_request/referrer_policy.h
+++ net/url_request/referrer_policy.h
@@ -6,6 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_URL_REQUEST_REFERRER_POLICY_H_
 #define NET_URL_REQUEST_REFERRER_POLICY_H_
 
+#include <optional>
+#include <string_view>
+
+#include "net/base/net_export.h"
+
 namespace net {
 
 // A ReferrerPolicy controls the contents of the Referer header when URLRequest
@@ -45,6 +50,17 @@ enum class ReferrerPolicy {
   NO_REFERRER = 7,
   MAX = NO_REFERRER,
 };
+
+// Convert the last known-valid value of a pre-concatenated "Referrer-Policy"
+// header to the corresponding ReferrerPolicy. For example, the input "origin,
+// strict-origin" would result in output of
+// ReferrerPolicy::ORIGIN_CLEAR_ON_TRANSITION_FROM_SECURE_TO_INSECURE. If no
+// recognized values were found then std::nullopt is returned.
+
+// TODO(crbug.com/40217150): Consider updating
+// blink::SecurityPolicy::ReferrerPolicyFromString() to use this.
+NET_EXPORT std::optional<ReferrerPolicy> ReferrerPolicyFromHeader(
+    std::string_view referrer_policy_header_value);
 
 }  // namespace net
 
