6ee6deb8b78d8a21118f0db0d9963b4e519a7d6e
Aman Verma <amanvr@google.com>
2024-09-09 11:18:00 +0000

    Adds HitTestDataProvider implementation to FrameSinkManagerImpl.
    This CL implements HitTestDataProvider interface in FrameSinkManagerImpl
    which would be useful for enabling Viz hit testing on VizCompositor
    thread with InputVizard.
    HitTestQuery on Viz would directly query updated HitTestData from
    HitTestAggregator, whereas in the browser it would keep store a copy.
    This avoids an unnecessary memory copy of HitTestData.
    
    See
    https://docs.google.com/document/d/1Z7zA77GX0qeov3chL96wzbN41ucAjZenSZgdMp6voRs/edit?usp=sharing
    
    Bug: b:358569153
    Change-Id: I3049405519ed1f99a85b89765fa23bb5db252e59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776030
    Commit-Queue: Aman Verma <amanvr@google.com>
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352657}

--- components/viz/common/hit_test/hit_test_query.h
+++ components/viz/common/hit_test/hit_test_query.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/functional/callback.h"
+#include "base/memory/safe_ref.h"
 #include "components/viz/common/hit_test/aggregated_hit_test_region.h"
 #include "components/viz/common/viz_common_export.h"
 #include "ui/gfx/geometry/point_f.h"
@@ -36,7 +37,15 @@ enum class EventSource {
 // TODO(crbug.com/41460939): Handle 3d space cases correctly.
 class VIZ_COMMON_EXPORT HitTestQuery {
  public:
-  HitTestQuery();
+  class DataProvider {
+   public:
+    virtual ~DataProvider() = default;
+    // Gets HitTestData from `HitTestAggregator` on VizCompositor thread.
+    virtual const std::vector<AggregatedHitTestRegion>& GetHitTestData()
+        const = 0;
+  };
+
+  explicit HitTestQuery(std::optional<base::SafeRef<DataProvider>> provider);
 
   HitTestQuery(const HitTestQuery&) = delete;
   HitTestQuery& operator=(const HitTestQuery&) = delete;
@@ -155,7 +164,16 @@ class VIZ_COMMON_EXPORT HitTestQuery {
                                        size_t region_index,
                                        gfx::Transform* transform) const;
 
+  // Returns updated aggregated hit test data from stored `hit_test_data` in the
+  // browser and uses `provider_` to get updated hit test data on VizCompositor
+  // thread.
+  const std::vector<AggregatedHitTestRegion>& GetHitTestRegionData() const;
+
   std::vector<AggregatedHitTestRegion> hit_test_data_;
+
+  // DataProvider is expected to outlive |this|. `HitTestAggregator` is the
+  // provider on VizCompositorThread whereas it's null on CrBrowserMain.
+  std::optional<base::SafeRef<DataProvider>> provider_;
 };
 
 }  // namespace viz
--- components/viz/host/host_frame_sink_manager.cc
+++ components/viz/host/host_frame_sink_manager.cc
@@ -173,7 +173,8 @@ void HostFrameSinkManager::CreateRootCompositorFrameSink(
       maybe_wait_on_destruction && params->gpu_compositing;
 
   frame_sink_manager_->CreateRootCompositorFrameSink(std::move(params));
-  display_hit_test_query_[frame_sink_id] = std::make_unique<HitTestQuery>();
+  display_hit_test_query_[frame_sink_id] =
+      std::make_unique<HitTestQuery>(std::nullopt);
 }
 
 void HostFrameSinkManager::CreateCompositorFrameSink(
@@ -497,7 +498,8 @@ void HostFrameSinkManager::EvictCachedBackBuffer(uint32_t cache_id) {
 
 void HostFrameSinkManager::CreateHitTestQueryForSynchronousCompositor(
     const FrameSinkId& frame_sink_id) {
-  display_hit_test_query_[frame_sink_id] = std::make_unique<HitTestQuery>();
+  display_hit_test_query_[frame_sink_id] =
+      std::make_unique<HitTestQuery>(std::nullopt);
 }
 void HostFrameSinkManager::EraseHitTestQueryForSynchronousCompositor(
     const FrameSinkId& frame_sink_id) {
