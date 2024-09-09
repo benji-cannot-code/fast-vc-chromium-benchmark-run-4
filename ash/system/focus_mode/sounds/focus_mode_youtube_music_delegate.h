821ed2f4593ebc8dc97e4dc0eb0467dbb6f2cc0a
Yongshun Liu <yongshun@chromium.org>
2024-09-09 23:10:54 +0000

    focus_mode: Add retries to `ReportPlayback()` interface
    This adds error handling and reties to `ReportPlayback()` interface. The
    following retry policies are used:
      - HTTP error 429:
        Fixed 30 seconds wait time. Max 3 retries.
      - HTTP error 408, 500, 502, 503, and 504:
        Exponential backoff with 1 second initial wait time. Max 5 retries.
    
    The playback reporting retry is special from other retries in the
    following aspects:
      - It may not use the same token since the token may be used by other
        requests. So there is some mechanism to ensure it always grabs the
        newest token.
      - It always tries to aggregate with previous failed data for new
        attempts, so uncommitted data will be carried forward.
    
    Bug: b/354240276
    Test: Manual
    Change-Id: I66139645614b894d4dc45a24ffb14ba2ef2bf051
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839248
    Reviewed-by: Richard Chui <richui@chromium.org>
    Commit-Queue: Yongshun Liu <yongshun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353043}

--- ash/system/focus_mode/sounds/focus_mode_youtube_music_delegate.h
+++ ash/system/focus_mode/sounds/focus_mode_youtube_music_delegate.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_FOCUS_MODE_SOUNDS_FOCUS_MODE_YOUTUBE_MUSIC_DELEGATE_H_
 
 #include <array>
+#include <memory>
 #include <optional>
 #include <string>
 #include <vector>
@@ -51,7 +52,7 @@ class ASH_EXPORT FocusModeYouTubeMusicDelegate
   void SetNoPremiumCallback(base::RepeatingClosure callback);
 
   // Reports music playback.
-  bool ReportPlayback(const youtube_music::PlaybackData& playback_data);
+  void ReportPlayback(const youtube_music::PlaybackData& playback_data);
 
   // Reserves a playlist for the returned playlists.
   void ReservePlaylistForGetPlaylists(const std::string& playlist_id);
@@ -118,16 +119,10 @@ class ASH_EXPORT FocusModeYouTubeMusicDelegate
     ReportPlaybackRequestState();
     ~ReportPlaybackRequestState();
 
-    // Checks if it can report the playback for `url`.
-    bool CanReportPlaybackForUrl(const GURL& url);
-
-    // URL to `PlaybackState` map. It contains all playback data for the
-    // requests.
-    base::flat_map<GURL, youtube_music::PlaybackState> url_to_playback_state;
-
-    // URL to playback reporting token map. It contains all tokens for the
-    // requests.
-    base::flat_map<GURL, std::string> url_to_token;
+    youtube_music::PlaybackState playback_state;
+    std::optional<youtube_music::PlaybackData> staged_playback_data;
+    std::string token;
+    FocusModeRetryState retry_state;
   };
 
   // Triggers request to query for specific playlist for the given bucket.
@@ -160,6 +155,8 @@ class ASH_EXPORT FocusModeYouTubeMusicDelegate
       google_apis::ApiErrorCode http_error_code,
       std::optional<const youtube_music::PlaybackContext> playback_context);
 
+  void ReportPlaybackInternal(const GURL& url);
+
   // Called when report playback request is done.
   void OnReportPlaybackDone(
       const GURL& url,
@@ -172,8 +169,9 @@ class ASH_EXPORT FocusModeYouTubeMusicDelegate
   // Next track request state for `GetPlaylists`.
   GetNextTrackRequestState next_track_state_;
 
-  // Report playback request state for `ReportPlayback`.
-  ReportPlaybackRequestState report_playback_state_;
+  // Report playback request state per track for `ReportPlayback`.
+  base::flat_map<GURL, std::unique_ptr<ReportPlaybackRequestState>>
+      report_playback_states_;
 
   // Callback to run when the request fails with HTTP 403.
   base::RepeatingClosure no_premium_callback_;
