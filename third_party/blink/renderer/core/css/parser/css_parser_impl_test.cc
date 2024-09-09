8163f707b1622051caad84cf53106d28f0ef5aae
Andreu Botella <abotella@igalia.com>
2024-09-09 15:23:16 +0000

    [line-clamp] Make `-webkit-line-clamp: none` parse
    The valid values of the `-webkit-line-clamp` property are the keyword
    `none` and a positive integer. Chromium represents it internally as an
    `int`, where `none` maps to the zero value.
    
    However, although the default value is set to zero, and zero
    serializes to `none`, `none` is treated as an invalid value when
    parsing. This means setting `-webkit-line-clamp: none` on a
    declaration higher in the cascade order will not override other
    declarations.
    
    The reason for this seems to be that, if the `none` keyword was
    parsed, converting it into an `int` would fail. However, for the
    unprefixed `line-clamp` property, this very same issue was solved by
    adding a `ConvertIntegerOrNone` converter in
    https://crrev.com/c/5419122. This patch uses this converter on
    `-webkit-line-clamp` as well.
    
    Bug: 40336192
    Change-Id: I05059830315821dbf0cab1bc3d3f253ad70b9c7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844930
    Commit-Queue: Andreu Botella <abotella@igalia.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352760}

--- third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
+++ third_party/blink/renderer/core/css/parser/css_parser_impl_test.cc
@@ -1321,7 +1321,7 @@ TEST(CSSParserImplTest, AllPropertiesCanParseImportant) {
   }
 
   // So that we don't introduce more, or break the entire test inadvertently.
-  EXPECT_EQ(broken_properties, 19);
+  EXPECT_EQ(broken_properties, 18);
 }
 
 TEST(CSSParserImplTest, ParseSupportsBlinkFeature) {
