b8ae0a28442fe040d6cf14e89dcf0b078ea35ba7
Jérôme Lebel <jlebel@chromium.org>
2024-09-09 12:03:11 +0000

    [iOS][MIM2] Show profile name in the settings
    The profile name is added as detail text in the switching profile item.
    
    Bug: 331783685
    Change-Id: Ia660440e58b8787e5a5e3d83b459252afa3d9936
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844828
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352672}

--- ios/chrome/browser/ui/settings/settings_table_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_table_view_controller.mm
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/passwords/model/password_checkup_utils.h"
 #import "ios/chrome/browser/photos/model/photos_service.h"
 #import "ios/chrome/browser/photos/model/photos_service_factory.h"
+#import "ios/chrome/browser/profile/model/constants.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_client_id.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_settings_util.h"
 #import "ios/chrome/browser/search_engines/model/search_engine_observer_bridge.h"
@@ -69,7 +70,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_backed_boolean.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
+#import "ios/chrome/browser/shared/model/profile/profile_attributes_ios.h"
+#import "ios/chrome/browser/shared/model/profile/profile_attributes_storage_ios.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
 #import "ios/chrome/browser/shared/model/utils/first_run_util.h"
 #import "ios/chrome/browser/shared/public/commands/application_commands.h"
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
@@ -1124,11 +1128,20 @@ struct EnhancedSafeBrowsingActivePromoData
 }
 
 - (TableViewItem*)switchProfileItem {
+  NSString* detailText = nil;
+  std::string profileName = _browserState->GetProfileName();
+  // TODO(crbug.com/331783685): Remove assumption that "Default" is the
+  // personal profile.
+  if (profileName == kIOSChromeInitialBrowserState) {
+    detailText = @"Personal";
+  } else {
+    detailText = base::SysUTF8ToNSString(profileName);
+  }
   return [self
            detailItemWithType:SettingsItemTypeSwitchProfile
                          text:l10n_util::GetNSString(
                                   IDS_IOS_SWITCH_PROFILE_MANAGEMENT_SETTINGS)
-                   detailText:nil
+                   detailText:detailText
                        symbol:DefaultSettingsRootSymbol(kMultiIdentitySymbol)
         symbolBackgroundColor:[UIColor colorNamed:kGrey400Color]
       accessibilityIdentifier:nil];
