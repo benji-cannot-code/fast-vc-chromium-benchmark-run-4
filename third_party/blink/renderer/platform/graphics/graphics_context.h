455a9f358e6e0cccbab0980b27b1f1195ba8730d
Lingqi Chi <lingqi@chromium.org>
2024-09-09 10:14:44 +0000

    Remove dead code from GraphicsContext.
    GetTextPaintOrder, CompositeRecord, ClipOut(const Path&), and Rotate are
    no longer used.
    
    Bug: 40248136
    Change-Id: Iffaa0ca69782eff371ecffa500ac48e162864848
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833757
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352633}

--- third_party/blink/renderer/platform/graphics/graphics_context.h
+++ third_party/blink/renderer/platform/graphics/graphics_context.h
@@ -254,9 +254,6 @@ class PLATFORM_EXPORT GraphicsContext {
   void SetTextPaintOrder(const TextPaintOrder& order) {
     MutableState()->SetTextPaintOrder(order);
   }
-  TextPaintOrder GetTextPaintOrder() const {
-    return ImmutableState()->GetTextPaintOrder();
-  }
 
   void SetImageInterpolationQuality(InterpolationQuality quality) {
     MutableState()->SetInterpolationQuality(quality);
@@ -327,10 +324,6 @@ class PLATFORM_EXPORT GraphicsContext {
                   const AutoDarkMode& auto_dark_mode);
 
   void DrawRecord(PaintRecord);
-  void CompositeRecord(PaintRecord,
-                       const gfx::RectF& dest,
-                       const gfx::RectF& src,
-                       SkBlendMode);
   void DrawImage(Image&,
                  Image::ImageDecodingMode,
                  const ImageAutoDarkMode& auto_dark_mode,
@@ -390,7 +383,6 @@ class PLATFORM_EXPORT GraphicsContext {
   void ClipOut(const gfx::RectF& rect) {
     ClipRect(gfx::RectFToSkRect(rect), kNotAntiAliased, SkClipOp::kDifference);
   }
-  void ClipOut(const Path&);
   void ClipOutRoundedRect(const FloatRoundedRect&);
   void ClipPath(const SkPath&,
                 AntiAliasingMode = kNotAntiAliased,
@@ -471,7 +463,6 @@ class PLATFORM_EXPORT GraphicsContext {
   void ConcatCTM(const AffineTransform&);
 
   void Scale(float x, float y);
-  void Rotate(float angle_in_radians);
   void Translate(float x, float y);
   // ---------- End transformation methods -----------------
 
