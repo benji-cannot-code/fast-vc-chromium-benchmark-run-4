4ae0e22f59fbdeb0334607e3d2522fe79d800f80
Peter Kasting <pkasting@chromium.org>
2024-09-09 09:38:34 +0000

    Mark `RunLoop::Quit[WhenIdle]Closure()` as lvalue-ref-qualified.
    There's no reason to run these on an rvalue, and this can prevent
    erroneous usage earlier.
    
    Bug: none
    Change-Id: I50f10b3384d9feab6cf7ba504d8e0c177a1b902d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840307
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352616}

--- base/run_loop.cc
+++ base/run_loop.cc
@@ -197,7 +197,7 @@ void RunLoop::QuitWhenIdle() {
   quit_when_idle_called_ = true;
 }
 
-RepeatingClosure RunLoop::QuitClosure() {
+RepeatingClosure RunLoop::QuitClosure() & {
   // Obtaining the QuitClosure() is not thread-safe; either obtain the
   // QuitClosure() from the owning thread before Run() or invoke Quit() directly
   // (which is thread-safe).
@@ -208,7 +208,7 @@ RepeatingClosure RunLoop::QuitClosure() {
       BindRepeating(&RunLoop::Quit, weak_factory_.GetWeakPtr()));
 }
 
-RepeatingClosure RunLoop::QuitWhenIdleClosure() {
+RepeatingClosure RunLoop::QuitWhenIdleClosure() & {
   // Obtaining the QuitWhenIdleClosure() is not thread-safe; either obtain the
   // QuitWhenIdleClosure() from the owning thread before Run() or invoke
   // QuitWhenIdle() directly (which is thread-safe).
