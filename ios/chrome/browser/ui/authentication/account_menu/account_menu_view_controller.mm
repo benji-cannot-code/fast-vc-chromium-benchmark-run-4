5e766b649b1700df9382d9adb66642994b7a7b7c
Arthur Milchior <arthurmilchior@chromium.org>
2024-09-09 13:55:18 +0000

    [iOS][Account Menu] radius corner is 10
    Result can be seen on
    https://drive.google.com/file/d/1x4wM8wjlEFnfOhUEh9NaqI97fHkBYqUZ/view?usp=drive_link
    
    Fixed: 356099798
    Bug: 365051946
    Change-Id: I8890044491971435af8138db7dc4c589d6e24283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836811
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352711}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_view_controller.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_view_controller.mm
@@ -46,7 +46,7 @@ constexpr CGFloat kErrorSymbolSize = 22.;
 // Height and width of the buttons.
 constexpr CGFloat kButtonSize = 22;
 
-constexpr CGFloat kHalfSheetCornerRadius = 20.0;
+constexpr CGFloat kHalfSheetCornerRadius = 10.0;
 
 // Sections used in the account menu.
 typedef NS_ENUM(NSUInteger, SectionIdentifier) {
@@ -84,7 +84,6 @@ NSString* const kCustomExpandedDetentIdentifier = @"customExpandedDetent";
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  self.view.layer.cornerRadius = 10.;
   self.tableView.accessibilityIdentifier = kAccountMenuTableViewId;
   self.tableView.backgroundColor =
       [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
