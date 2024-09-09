74007794470706c5125085978c3d1147b70be899
Din Nezametdinov <shaikhitdin@google.com>
2024-09-09 13:09:38 +0000

    [Passwords] Enable manual password generation on server predicted fields
    Add check in the context menu on whether password generation can be
    triggered on the focused field. If so, allow manual generation on the
    field, even if it is a text field.
    
    Use `PasswordGenerationFrameHelper` class to store the manual
    generation enabled fields.
    
    Modify the existing
    `MarkServerPredictedClearTextPasswordFieldEligibleForGeneration` test
    to check the behavior.
    
    Bug: 356817239
    Change-Id: Iec96c27a1f88bcf095e58c096b8ecf566e6d6a2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5776926
    Commit-Queue: Din Nezametdinov <shaikhitdin@google.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352697}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -1137,12 +1137,7 @@ bool RenderViewContextMenu::IsInProgressiveWebApp() const {
 void RenderViewContextMenu::InitMenu() {
   RenderViewContextMenuBase::InitMenu();
 
-  if (content_type_->SupportsGroup(
-          ContextMenuContentType::ITEM_GROUP_PASSWORD) &&
-      !base::FeatureList::IsEnabled(
-          password_manager::features::kPasswordManualFallbackAvailable)) {
-    AppendPasswordItems();
-  }
+  AppendPasswordItems();
 
   if (content_type_->SupportsGroup(ContextMenuContentType::ITEM_GROUP_PAGE)) {
     AppendPageItems();
@@ -2676,11 +2671,18 @@ void RenderViewContextMenu::AppendProtocolHandlerSubMenu() {
 }
 
 void RenderViewContextMenu::AppendPasswordItems() {
-  bool add_separator = false;
-
   password_manager::ContentPasswordManagerDriver* driver =
       password_manager::ContentPasswordManagerDriver::GetForRenderFrameHost(
           GetRenderFrameHost());
+
+  if (!driver ||
+      !driver->IsPasswordFieldForPasswordManager(
+          autofill::FieldRendererId(params_.field_renderer_id), params_)) {
+    return;
+  }
+
+  bool add_separator = false;
+
   // Don't show the item for guest or incognito profiles and also when the
   // automatic generation feature is disabled.
   if (password_manager_util::ManualPasswordGenerationEnabled(driver)) {
--- components/password_manager/core/browser/password_generation_frame_helper.cc
+++ components/password_manager/core/browser/password_generation_frame_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/proto/password_requirements.pb.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/autofill/core/common/signatures.h"
+#include "components/autofill/core/common/unique_ids.h"
 #include "components/password_manager/core/browser/browser_save_password_progress_logger.h"
 #include "components/password_manager/core/browser/generation/password_generator.h"
 #include "components/password_manager/core/browser/password_feature_manager.h"
@@ -148,6 +149,16 @@ bool PasswordGenerationFrameHelper::IsGenerationEnabled(
   }
 
   return false;
+}
+
+bool PasswordGenerationFrameHelper::IsManualGenerationEnabledField(
+    autofill::FieldRendererId field_renderer_id) const {
+  return generation_enabled_fields_.contains(field_renderer_id);
+}
+
+void PasswordGenerationFrameHelper::AddManualGenerationEnabledField(
+    autofill::FieldRendererId field_renderer_id) {
+  generation_enabled_fields_.insert(field_renderer_id);
 }
 
 std::u16string PasswordGenerationFrameHelper::GeneratePassword(
