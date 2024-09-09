317dfecd79549e1c9b0b776ff35c9aa57becbe6b
Oksana Zhuravlova <oksamyt@chromium.org>
2024-09-09 17:21:32 +0000

    [tracing] Record metadata on flush and on stop
    When a tracing session is cloned, OnFlush() is not called on a
    DataSource, but OnStop() is not. This CL updates MetadataDataSource
    to also write the metadata in OnFlush()
    since this is the scenario that is common in the field.
    
    Bug: b:354749410, b:356888214
    Change-Id: I70a18da05a14549b4853fa8deff1eca999a77426
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836269
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352850}

--- services/tracing/public/cpp/perfetto/metadata_data_source.cc
+++ services/tracing/public/cpp/perfetto/metadata_data_source.cc
@@ -26,7 +26,15 @@ void MetadataDataSource::Register() {
 
 void MetadataDataSource::OnStart(const StartArgs&) {}
 
+void MetadataDataSource::OnFlush(const FlushArgs&) {
+  WriteMetadata();
+}
+
 void MetadataDataSource::OnStop(const StopArgs&) {
+  WriteMetadata();
+}
+
+void MetadataDataSource::WriteMetadata() {
   Trace([&](TraceContext ctx) {
     auto packet = ctx.NewTracePacket();
     packet->set_timestamp(
@@ -61,6 +69,7 @@ void MetadataDataSource::OnStop(const StopArgs&) {
     }
   });
 }
+
 }  // namespace tracing
 
 PERFETTO_DEFINE_DATA_SOURCE_STATIC_MEMBERS_WITH_ATTRS(
