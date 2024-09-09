4840879a56fe6416a7c5c91f4643f7d63e292e21
Evan Stade <estade@chromium.org>
2024-09-09 17:20:08 +0000

    IDB: push some LevelDB dependencies into IndexedDBBackingStore.
    No behavioral change expected.
    
    Bug: 365076682
    Change-Id: I935fc9ef6a9849992259e1b01b3c227342530bed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838062
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1352849}

--- content/browser/indexed_db/indexed_db_connection_coordinator.h
+++ content/browser/indexed_db/indexed_db_connection_coordinator.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "content/browser/indexed_db/indexed_db_bucket_context.h"
 #include "content/browser/indexed_db/indexed_db_task_helper.h"
-#include "content/browser/indexed_db/list_set.h"
 #include "content/common/content_export.h"
 #include "third_party/leveldatabase/src/include/leveldb/status.h"
 
