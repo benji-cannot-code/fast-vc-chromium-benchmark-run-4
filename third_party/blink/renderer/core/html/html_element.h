a40c5b0f02ab4d6cec97510fb3f27800acf99e78
Mason Freed <masonf@chromium.org>
2024-09-09 23:43:10 +0000

    Add PopoverAnchorRelationships flag and basic imperative API
    This adds the PopoverAnchorRelationships feature flag, and
    updates the IDL to add options bags to the relevant popover
    APIs.
    
    Chromestatus:
      https://chromestatus.com/feature/5120638407409664
    
    Bug: 364669918
    Change-Id: I30cd45ff2a1f184e9e1b56b4cef7c5dc8e7557dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837806
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353062}

--- third_party/blink/renderer/core/html/html_element.h
+++ third_party/blink/renderer/core/html/html_element.h
@@ -49,6 +49,8 @@ class HTMLSelectListElement;
 class KeyboardEvent;
 class TextControlElement;
 class V8UnionStringLegacyNullToEmptyStringOrTrustedScript;
+class V8UnionBooleanOrTogglePopoverOptions;
+class ShowPopoverOptions;
 
 enum TranslateAttributeMode {
   kTranslateAttributeYes,
@@ -261,8 +263,11 @@ class CORE_EXPORT HTMLElement : public Element {
                       bool include_event_handler_text,
                       Document* expected_document) const;
   bool togglePopover(ExceptionState& exception_state);
-  bool togglePopover(bool force, ExceptionState& exception_state);
+  bool togglePopover(V8UnionBooleanOrTogglePopoverOptions* options_or_force,
+                     ExceptionState& exception_state);
   void showPopover(ExceptionState& exception_state);
+  void showPopover(ShowPopoverOptions* options,
+                   ExceptionState& exception_state);
   void hidePopover(ExceptionState& exception_state);
   // |exception_state| can be nullptr when exceptions can't be thrown, such as
   // when the browser hides a popover during light dismiss or shows a popover in
