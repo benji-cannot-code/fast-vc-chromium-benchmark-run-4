5427be3c9208d7ad364784f1a217097aac1b3362
Radu Nitescu <radunitescu@google.com>
2024-09-09 12:38:24 +0000

    [iOS][LO] Dismiss Lens Overlay when bottom sheet is dismissed
    Bug: None
    Change-Id: I52ab41e12492fd0da777630dc10f017c77da74b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840220
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1352685}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -351,14 +351,13 @@ const CGFloat kMenuSymbolSize = 18;
                           .identifier]);
 }
 
-- (void)presentationControllerDidDismiss:
+- (void)presentationControllerWillDismiss:
     (UIPresentationController*)presentationController {
   UIViewController* presentedViewController =
       presentationController.presentedViewController;
 
   if (presentedViewController == _resultViewController) {
-    [self stopResultPage];
-    return;
+    [self destroyLensUI:YES];
   }
 }
 
