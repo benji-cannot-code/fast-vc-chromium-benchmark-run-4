e988e3d798448df81cac655b1815bc1124d18d3a
Michael Wojcicka <mwoj@google.com>
2024-09-10 17:52:11 +0000

    Add Feature Flag for Task Manager Desktop Refresh
    Change-Id: Ibfee4d850c15ae1bda892dbea2e5f89d04f26508
    Bug: b:364933740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840651
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353460}

--- chrome/browser/browser_features.h
+++ chrome/browser/browser_features.h
@@ -141,6 +141,10 @@ BASE_DECLARE_FEATURE(kSandboxExternalProtocolBlocked);
 BASE_DECLARE_FEATURE(kSandboxExternalProtocolBlockedWarning);
 BASE_DECLARE_FEATURE(kSupportSearchSuggestionForPrerender2);
 
+#if !BUILDFLAG(IS_ANDROID)
+BASE_DECLARE_FEATURE(kTaskManagerDesktopRefresh);
+#endif  // BUILDFLAG(IS_ANDROID)
+
 BASE_DECLARE_FEATURE(kTriggerNetworkDataMigration);
 
 #if BUILDFLAG(IS_CHROMEOS)
@@ -157,6 +161,7 @@ BASE_DECLARE_FEATURE(kBrowserDynamicCodeDisabled);
 BASE_DECLARE_FEATURE(kReportPakFileIntegrity);
 
 BASE_DECLARE_FEATURE(kRemovalOfIWAsFromTabCapture);
+
 }  // namespace features
 
 #endif  // CHROME_BROWSER_BROWSER_FEATURES_H_
