d8d257513df58ab36c46736a26d22b3b8b827700
Teresa Mao <temao@chromium.org>
2024-09-10 23:18:36 +0000

    Modularize //chrome/browser/ai
    Bug: 353332589
    Change-Id: If1a03ba850e4193c52b514da62e3535407bcb45f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5842733
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353662}

--- chrome/browser/ai/ai_manager_keyed_service.h
+++ chrome/browser/ai/ai_manager_keyed_service.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ai/ai_context_bound_object_set.h"
 #include "chrome/browser/ai/ai_summarizer.h"
 #include "chrome/browser/ai/ai_text_session.h"
-#include "chrome/browser/optimization_guide/optimization_guide_keyed_service.h"
 #include "components/keyed_service/core/keyed_service.h"
 #include "content/public/browser/browser_context.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
--- chrome/browser/ai/ai_test_utils.cc
+++ chrome/browser/ai/ai_test_utils.cc
@@ -7,12 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ai/ai_manager_keyed_service.h"
 #include "chrome/browser/ai/ai_manager_keyed_service_factory.h"
-#include "chrome/browser/optimization_guide/mock_optimization_guide_keyed_service.h"
 #include "chrome/browser/optimization_guide/optimization_guide_keyed_service_factory.h"
-#include "chrome/test/base/chrome_render_view_host_test_harness.h"
-#include "testing/gmock/include/gmock/gmock.h"
-#include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/blink/public/mojom/ai/model_streaming_responder.mojom.h"
 
 AITestUtils::MockModelStreamingResponder::MockModelStreamingResponder() =
     default;
