155cc65f4fed60b8906c07819d9e50bc134e7f15
Danny Wang <wangdanny@google.com>
2024-09-10 15:42:35 +0000

    shortcut: Fix accessibility icon in Launcher
    Screenshots:
    before:
    http://screen/8uK77YQpxveBWPA
    http://screen/6sMqdt6HYSRJxvC
    after:
    http://screen/3kwgeUrJarx4EiN
    http://screen/9VVUQ4ghFP6K6Xt
    
    Bug: b/365625885
    Test: Test in dut
    Change-Id: I21ec7b1c1c5598e31e8ba77d9cc31236e3021280
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5847053
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353372}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -686,6 +686,7 @@ class ASH_PUBLIC_EXPORT SearchResultTextItem {
     kKeyboardShortcutKeyboardBrightnessDown,
     kKeyboardShortcutKeyboardBrightnessUp,
     kKeyboardShortcutKeyboardRightAlt,
+    kKeyboardShortcutAccessibility,
   };
 
   // Only used for SearchResultTextItemType kString
