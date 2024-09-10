e727960616bc9a400a1575b1cd22d880253b5754
Björn Terelius <terelius@chromium.org>
2024-09-10 15:53:36 +0000

    Fix for updated "VideoResourceUpdater::ObtainFrameResource" TRACE_EVENT name.
    The label was changed in https://chromium-review.googlesource.com/c/chromium/src/+/5835585
    
    This CL also adds some logging to help debug in case this happens again.
    
    Bug: b/365068670
    Change-Id: I40cce708a926df54884902cc15514b9fc04dad30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850270
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Commit-Queue: Jeremy Leconte <jleconte@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353377}

--- chrome/browser/media/webrtc/webrtc_video_display_perf_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_video_display_perf_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <tuple>
 
 #include "base/json/json_reader.h"
+#include "base/logging.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/trace_event_analyzer.h"
@@ -46,7 +47,7 @@ static const char kSetFrameEventName[] =
 static const char kGetFrameEventName[] =
     "WebMediaPlayerMSCompositor::GetCurrentFrame";
 static const char kVideoResourceEventName[] =
-    "VideoResourceUpdater::ObtainFrameResources";
+    "VideoResourceUpdater::ObtainFrameResource";
 static const char kVsyncEventName[] = "Display::DrawAndSwap";
 
 // VideoFrameSubmitter dumps the delay from the handover of a decoded remote
@@ -233,8 +234,10 @@ class WebRtcVideoDisplayPerfBrowserTest
     TraceEventVector start_render_events;
     FindEvents(analyzer, kStartRenderEventName, match_process_id,
                &start_render_events);
-    if (start_render_events.empty())
+    if (start_render_events.empty()) {
+      DLOG(WARNING) << "No " << kStartRenderEventName << " events";
       return false;
+    }
 
     // We are only interested in vsync events coming after the first render
     // event. Earlier ones are already missed.
@@ -244,8 +247,10 @@ class WebRtcVideoDisplayPerfBrowserTest
     TraceEventVector vsync_events;
     FindEvents(analyzer, kVsyncEventName, after_first_render_event,
                &vsync_events);
-    if (vsync_events.empty())
+    if (vsync_events.empty()) {
+      DLOG(WARNING) << "No " << kVsyncEventName << " events";
       return false;
+    }
 
     size_t found_vsync_index = 0;
     size_t skipped_frame_count = 0;
@@ -309,8 +314,10 @@ class WebRtcVideoDisplayPerfBrowserTest
       total_durations_.push_back(total_duration);
     }
 
-    if (start_render_events.size() == skipped_frame_count)
+    if (start_render_events.size() == skipped_frame_count) {
+      DLOG(WARNING) << "All frames skipped";
       return false;
+    }
 
     // Calculate the percentage by dividing by the number of frames received.
     skipped_frame_percentage_ =
