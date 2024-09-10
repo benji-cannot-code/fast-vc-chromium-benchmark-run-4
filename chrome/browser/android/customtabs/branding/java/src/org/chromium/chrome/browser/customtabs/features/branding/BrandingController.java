ec0fbc66108079681e6bf14c1d9832ea18b8ffb6
Jinsuk Kim <jinsukkim@chromium.org>
2024-09-10 16:20:27 +0000

    [AuthView] Display branding on start
    Shows the branding toast for every launch. The existing
    BrandingController gets updated to accommodate the activity type
    AUTH_TAB.
    
    Bug: 357144226
    Change-Id: Id2da274f265ede18ac2b5eb7779b890333dfe45a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5845599
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353401}

--- chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/BrandingController.java
+++ chrome/browser/android/customtabs/branding/java/src/org/chromium/chrome/browser/customtabs/features/branding/BrandingController.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.CallbackController;
@@ -53,6 +54,7 @@ public class BrandingController {
     private final BrandingChecker mBrandingChecker;
     private final Context mContext;
     private final String mBrowserName;
+    private final int mToastTemplateId;
     @Nullable private final PureJavaExceptionReporter mExceptionReporter;
     private ToolbarBrandingDelegate mToolbarBrandingDelegate;
     private @Nullable Toast mToast;
@@ -61,18 +63,22 @@ public class BrandingController {
 
     /**
      * Branding controller responsible for showing branding.
+     *
      * @param context Context used to fetch package information for embedded app.
-     * @param appId The ID for the embedded app.
+     * @param appId The ID for the embedded app. Can be {@code null}
      * @param browserName The browser name shown on the branding toast.
+     * @param toastTemplateId Resource ID of the string to be shown on Toast branding UI.
      * @param exceptionReporter Optional reporter that reports wrong state quietly.
      */
     public BrandingController(
             Context context,
             String appId,
             String browserName,
+            @StringRes int toastTemplateId,
             @Nullable PureJavaExceptionReporter exceptionReporter) {
         mContext = context;
         mBrowserName = browserName;
+        mToastTemplateId = toastTemplateId;
         mExceptionReporter = exceptionReporter;
         mBrandingDecision.onAvailable(
                 mCallbackController.makeCancelable((decision) -> maybeMakeBrandingDecision()));
@@ -163,9 +169,7 @@ public class BrandingController {
             return;
         }
 
-        String toastText =
-                mContext.getResources()
-                        .getString(R.string.twa_running_in_chrome_template, mBrowserName);
+        String toastText = mContext.getResources().getString(mToastTemplateId, mBrowserName);
         TextView runInChromeTextView =
                 (TextView)
                         LayoutInflater.from(mContext)
