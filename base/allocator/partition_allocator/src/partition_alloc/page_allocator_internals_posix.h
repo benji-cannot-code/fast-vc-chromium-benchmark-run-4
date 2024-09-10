3d9a3802f5c2956df316fc491f3c06ee15ec4a72
Stephen Roettger <sroettger@google.com>
2024-09-10 09:59:17 +0000

    [cfi] Implement PageAllocator::SealPages on Linux
    The V8 support landed in crrev.com/c/5791010 (behind a build flag).
    
    Bug: 360048056
    Change-Id: I65e1b1dc6b8246174e95f1e4913f2c8b3cfd36a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803658
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Stephen Röttger <sroettger@google.com>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353229}

--- base/allocator/partition_allocator/src/partition_alloc/page_allocator_internals_posix.h
+++ base/allocator/partition_allocator/src/partition_alloc/page_allocator_internals_posix.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PARTITION_ALLOC_PAGE_ALLOCATOR_INTERNALS_POSIX_H_
 
 #include <sys/mman.h>
+#include <sys/syscall.h>
 
 #include <algorithm>
 #include <atomic>
@@ -428,6 +429,17 @@ void DiscardSystemPagesInternal(uintptr_t address, size_t length) {
   // Therefore, we just do the simple thing: MADV_DONTNEED.
   PA_PCHECK(0 == madvise(ptr, length, MADV_DONTNEED));
 #endif  // PA_BUILDFLAG(IS_APPLE)
+}
+
+bool SealSystemPagesInternal(uintptr_t address, size_t length) {
+  // TODO(sroettger): we either need to ensure that __NR_mseal is defined in the
+  // headers used by builders or define it ourselves.
+#if PA_BUILDFLAG(IS_LINUX) && defined(__NR_mseal)
+  long ret = syscall(__NR_mseal, address, length, 0);
+  return ret == 0;
+#else
+  return false;
+#endif
 }
 
 }  // namespace partition_alloc::internal
