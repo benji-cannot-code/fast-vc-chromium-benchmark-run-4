746ca8096293cae559ecfeb63f3aa62a188b71c8
Thomas Nguyen <tungnh@chromium.org>
2024-09-10 19:10:07 +0000

    Revert "[PEPC] Use document attachment to keep track of PEPC quota per page"
    This reverts commit ae3f501a05a0f666e8da3207796951d952590bf5.
    
    Reason for revert: Regression https://g-issues.chromium.org/issues/365602125
    
    Original change's description:
    > [PEPC] Use document attachment to keep track of PEPC quota per page
    >
    > DetachLayoutTree isn't always triggered, which means that the
    > unexisting PEPC would use up the quota, and new PEPCs wouldn't be able
    > to be registered.
    > To fix this problem, we moved the code block from DetachLayoutTree and
    > AttachLayoutTree to InsertedInto and RemovedFrom. More details:
    > https://groups.google.com/a/google.com/g/pepc-team/c/R9vQaUHbybw
    >
    > Bug: 358063391
    > Change-Id: I22f613d4c0d3fae9791f22236de928e3cd7776d8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783393
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    > Reviewed-by: Mason Freed <masonf@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1351916}
    
    Bug: 358063391, 365602125
    Change-Id: Icb3cef1abaffbfcf73880d603dd657e35eb457f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850576
    Commit-Queue: Thomas Nguyen <tungnh@chromium.org>
    Owners-Override: Slobodan Pejic <slobodan@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353517}

--- third_party/blink/renderer/core/html/html_permission_element.h
+++ third_party/blink/renderer/core/html/html_permission_element.h
@@ -54,9 +54,8 @@ class CORE_EXPORT HTMLPermissionElement final
 
   void Trace(Visitor*) const override;
 
-  InsertionNotificationRequest InsertedInto(ContainerNode&) override;
-  void RemovedFrom(ContainerNode&) override;
-  void DidNotifySubtreeInsertionsToDocument() override;
+  void AttachLayoutTree(AttachContext& context) override;
+  void DetachLayoutTree(bool performing_reattach) override;
   void Focus(const FocusParams& params) override;
   FocusableState SupportsFocus(UpdateBehavior) const override;
   int DefaultTabIndex() const override;
@@ -98,7 +97,7 @@ class CORE_EXPORT HTMLPermissionElement final
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementIntersectionTest,
                            ContainerDivClipPath);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementIntersectionTest,
-                           IntersectionVisibleOverlapsRecentInsertedInterval);
+                           IntersectionVisibleOverlapsRecentAttachedInterval);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementFencedFrameTest,
                            NotAllowedInFencedFrame);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementSimTest,
@@ -107,8 +106,6 @@ class CORE_EXPORT HTMLPermissionElement final
                            EnableClickingAfterDelay);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementSimTest,
                            FontSizeCanDisableElement);
-  FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementSimTest,
-                           MovePEPCToAnotherDocument);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementLayoutChangeTest,
                            InvalidatePEPCAfterMove);
   FRIEND_TEST_ALL_PREFIXES(HTMLPemissionElementLayoutChangeTest,
@@ -130,8 +127,8 @@ class CORE_EXPORT HTMLPermissionElement final
     kUnknown,
 
     // This element is temporarily disabled for a short period
-    // (`kDefaultDisableTimeout`) after being inserted into DOM.
-    kRecentlyInsertedIntoDOM,
+    // (`kDefaultDisableTimeout`) after being attached to the layout tree.
+    kRecentlyAttachedToLayoutTree,
 
     // This element is temporarily disabled for a short period
     // (`kDefaultDisableTimeout`) after its intersection status changed from
@@ -162,7 +159,7 @@ class CORE_EXPORT HTMLPermissionElement final
   enum class UserInteractionDeniedReason {
     kInvalidType = 0,
     kFailedOrHasNotBeenRegistered = 1,
-    kRecentlyInsertedIntoDOM = 2,
+    kRecentlyAttachedToLayoutTree = 2,
     kIntersectionRecentlyFullyVisible = 3,
     kInvalidStyle = 4,
     kUntrustedEvent = 5,
@@ -411,13 +408,12 @@ class CORE_EXPORT HTMLPermissionElement final
   // Computes the intersection rect of the element with the viewport.
   gfx::Rect ComputeIntersectionRectWithViewport(const Page* page);
 
-  // When the element is first inserted into DOM and rendered on the page,
+  // When the element is first attached to layout and rendered on the page,
   // there would be events causing an extra unresponsive delay that we don't
   // want, for example: the IntersectionObserver will trigger a series of events
   // from invisible to visible, with delays. We will throttle the cooldown
-  // time of the events to match the recently_inserted_into_dom cooldown time.
-  std::optional<base::TimeDelta> GetRecentlyInsertedIntoDOMTimeoutRemaining()
-      const;
+  // time of the events to match the recently_attached cooldown time.
+  std::optional<base::TimeDelta> GetRecentlyAttachedTimeoutRemaining() const;
 
   IntersectionVisibility IntersectionVisibilityForTesting() const {
     return intersection_visibility_;
