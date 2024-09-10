1efba3867fd953cc9d4bf415f7977560a967b570
Nafis Abedin <nafisabedin@google.com>
2024-09-10 11:35:58 +0000

    [Clank Startup Metrics] Fix Overcounting Clank Cold Start Metric
    ColdStartTracker.wasColdOnFirstActivityCreationOrNow() should be paired
    with SimpleStartupForegroundSessionDetector to avoid overcounting
    when continuing across foreground -> background and back.
    
    Bug: 365532007
    Change-Id: Id515c8eb9f0644b47d142872e7732e8013c850fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5846168
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1353254}

--- chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/ChromeActivity.java
@@ -122,6 +122,7 @@ import org.chromium.chrome.browser.layouts.LayoutManagerAppUtils;
 import org.chromium.chrome.browser.media.FullscreenVideoPictureInPictureController;
 import org.chromium.chrome.browser.metrics.LaunchMetrics;
 import org.chromium.chrome.browser.metrics.LegacyTabStartupMetricsTracker;
+import org.chromium.chrome.browser.metrics.SimpleStartupForegroundSessionDetector;
 import org.chromium.chrome.browser.metrics.StartupMetricsTracker;
 import org.chromium.chrome.browser.metrics.UmaActivityObserver;
 import org.chromium.chrome.browser.modaldialog.TabModalLifetimeHandler;
@@ -1156,7 +1157,9 @@ public abstract class ChromeActivity<C extends ChromeActivityComponent>
         if (isMainIntentLaunch) {
             RecordHistogram.recordBooleanHistogram(
                     "Startup.Android.MainIntentIsColdStart",
-                    ColdStartTracker.wasColdOnFirstActivityCreationOrNow());
+                    ColdStartTracker.wasColdOnFirstActivityCreationOrNow()
+                            && SimpleStartupForegroundSessionDetector
+                                    .runningCleanForegroundSession());
         }
 
         Tab tab = getActivityTab();
