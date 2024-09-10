52589e49ec4198510c1a646bb4c373344c54b298
Erik Chen <erikchen@chromium.org>
2024-09-10 23:00:49 +0000

    Reduce fragility in SidePanelCoordinator (part 4 / N)
    This CL is primarily a refactor with no intended behavior change. It
    removes redundant logic from SidePanelCoordinator.
    
    This CL fixes a minor bug from part 2 where we were not correctly
    checking whether a closing extension was using the tab-scoped or
    window-scoped registry. This is not expected to have any user-visible
    effect, but is good to fix anyways.
    
    Bug: 363743081
    Change-Id: I9455e3ce611c562bbbc5fa216249588804c24e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5841268
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353647}

--- chrome/browser/ui/views/side_panel/extensions/extension_side_panel_manager.cc
+++ chrome/browser/ui/views/side_panel/extensions/extension_side_panel_manager.cc
@@ -41,11 +41,13 @@ ExtensionSidePanelManager::ExtensionSidePanelManager(
     Profile* profile,
     Browser* browser,
     content::WebContents* web_contents,
-    SidePanelRegistry* registry)
+    SidePanelRegistry* registry,
+    bool for_tab)
     : profile_(profile),
       browser_(browser),
       web_contents_(web_contents),
-      registry_(registry) {
+      registry_(registry),
+      for_tab_(for_tab) {
   side_panel_registry_observation_.Observe(registry_);
   profile_observation_.Observe(profile);
 
@@ -63,7 +65,8 @@ void ExtensionSidePanelManager::CreateForBrowser(
   // std::make_unique<ExtensionSidePanelManager> to access a private
   // constructor.
   auto manager = absl::WrapUnique(new ExtensionSidePanelManager(
-      browser->profile(), browser, /*web_contents=*/nullptr, window_registry));
+      browser->profile(), browser, /*web_contents=*/nullptr, window_registry,
+      /*for_tab=*/false));
   browser->SetUserData(kExtensionSidePanelManagerKey, std::move(manager));
 }
 
@@ -83,8 +86,9 @@ void ExtensionSidePanelManager::CreateForTab(Profile* profile,
   // Use absl::WrapUnique(new ExtensionSidePanelManager(...)) instead of
   // std::make_unique<ExtensionSidePanelManager> to access a private
   // constructor.
-  auto manager = absl::WrapUnique(new ExtensionSidePanelManager(
-      profile, /*browser=*/nullptr, web_contents, tab_registry));
+  auto manager = absl::WrapUnique(
+      new ExtensionSidePanelManager(profile, /*browser=*/nullptr, web_contents,
+                                    tab_registry, /*for_tab=*/true));
   web_contents->SetUserData(kExtensionSidePanelManagerKey, std::move(manager));
 }
 
@@ -228,7 +232,7 @@ void ExtensionSidePanelManager::MaybeCreateExtensionSidePanelCoordinator(
     coordinators_.emplace(
         extension->id(),
         std::make_unique<ExtensionSidePanelCoordinator>(
-            profile_, browser_, web_contents_, extension, registry_));
+            profile_, browser_, web_contents_, extension, registry_, for_tab_));
   }
 }
 
