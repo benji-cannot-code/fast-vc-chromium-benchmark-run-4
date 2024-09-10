889af582c963af215b8294922efa8a37479c6e63
Andy Phan <andyphan@chromium.org>
2024-09-10 22:40:21 +0000

    [PDF Ink Signatures] Make form highlight and text selection public
    Make SetFormHighlight() and ClearTextSelection() public methods, so that
    they can be used by callers. These are used in
    https://crrev.com/c/5841317.
    
    Add tests for these methods.
    
    Also re-use SetFormHighlight() in some places.
    
    Bug: 353923998
    Change-Id: Ia943b12329ffbe9cd01e5eee94e8161075fd4522
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5846532
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1353631}

--- pdf/pdfium/pdfium_engine.h
+++ pdf/pdfium/pdfium_engine.h
@@ -244,6 +244,8 @@ class PDFiumEngine : public DocumentLoader::Client, public IFSDK_PAUSE {
 
   virtual void SelectAll();
 
+  void ClearTextSelection();
+
   // Gets the list of DocumentAttachmentInfo from the document.
   virtual const std::vector<DocumentAttachmentInfo>&
   GetDocumentAttachmentInfoList() const;
@@ -428,6 +430,9 @@ class PDFiumEngine : public DocumentLoader::Client, public IFSDK_PAUSE {
   PDFiumPage* GetPage(size_t index);
 
   bool IsValidLink(const std::string& url);
+
+  // Sets whether form highlight should be enabled or cleared.
+  void SetFormHighlight(bool enable_form);
 
  private:
   // This helper class is used to detect the difference in selection between
@@ -850,11 +855,6 @@ class PDFiumEngine : public DocumentLoader::Client, public IFSDK_PAUSE {
   // Checks whether a given `page_index` exists in `pending_thumbnails_`. If so,
   // requests the thumbnail for that page.
   void MaybeRequestPendingThumbnail(int page_index);
-
-  // Sets whether form highlight should be enabled or cleared.
-  void SetFormHighlight(bool enable_form);
-
-  void ClearTextSelection();
 
   const raw_ptr<PDFiumEngineClient> client_;
 
