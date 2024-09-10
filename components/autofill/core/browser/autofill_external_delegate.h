2873b607fc23e8e79b0ada28e027cabc268258d5
Wolfgang Billenstein <bwolfgang@google.com>
2024-09-10 07:38:58 +0000

    Wire up suggestions for prediction improvements
    Adds an entrypoint for retrieving prediction improvements via an
    Autofill suggestion. When invoked, a loading indicator is shown until
    the predictions are received and shown to the user in another
    suggestion.
    
    Fixed: 361414075,363946711
    Change-Id: I09c721e394e2930d61925d17d138580662b8f8a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838368
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1353180}

--- components/autofill/core/browser/autofill_external_delegate.h
+++ components/autofill/core/browser/autofill_external_delegate.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/address_data_manager.h"
 #include "components/autofill/core/browser/autofill_client.h"
 #include "components/autofill/core/browser/autofill_trigger_details.h"
+#include "components/autofill/core/browser/form_filler.h"
 #include "components/autofill/core/browser/metrics/suggestions_list_metrics.h"
 #include "components/autofill/core/browser/ui/autofill_suggestion_delegate.h"
 #include "components/autofill/core/browser/ui/suggestion.h"
@@ -255,6 +256,9 @@ class AutofillExternalDelegate : public AutofillSuggestionDelegate,
   void FillCreditCardFieldByFieldFillingSuggestion(
       const CreditCard& credit_card,
       const Suggestion& suggestion);
+
+  // Fills `values_to_fill` into the fields of `query_form_`.
+  void FillPredictionImprovements(const Suggestion& suggestion);
 
   // Triggered when the user closes the authentication flow needed to access
   // the number and cvc of the `credit_card`.
