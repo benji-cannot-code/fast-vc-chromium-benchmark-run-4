aea1f9ca8f039acab712618ef94ae2f317655308
Colin Blundell <blundell@chromium.org>
2024-09-10 07:25:59 +0000

    [Blink] Remove unneeded conditional from Canvas2DLayerBridge (redux)
    This conditional checks whether `resource_host_` is null. However,
    `resource_host_` was dereferenced to make method calls earlier in this
    method, and none of the method calls being made could result in
    `resource_host_` becoming null (that effectively happens only when
    Canvas2DLayerBridge is destroyed [1]).
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/blink/renderer/core/html/canvas/html_canvas_element.cc;l=269-272?q=HTMLCanvasElement::D&ss=chromium
    
    Bug: 40280152
    Change-Id: Ib426cc498a7fd005520b91e1b822130390c560da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5845035
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353176}

--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
@@ -374,8 +374,7 @@ bool Canvas2DLayerBridge::Restore() {
     }
   }
 
-  if (resource_host_)
-    resource_host_->UpdateMemoryUsage();
+  resource_host_->UpdateMemoryUsage();
 
   return ResourceProvider();
 }
