f5b79b245a35e264a134ddb1eb4cc658c809a31d
Mason Freed <masonf@chromium.org>
2024-09-10 15:43:24 +0000

    Fix errant CHECK in popover=hint code
    Since the entire popover=hint stack gets closed just above
    this, it's possible (see new test) that event handlers also
    close the target popover. In that case, just return.
    
    Fixed: 363243904
    Change-Id: I868228a9c80df1919e5856b1cafc249f5c7ed820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5848008
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353373}

--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1753,7 +1753,10 @@ void HTMLElement::HideAllPopoversUntil(
 
   // Now check the auto stack.
   auto& auto_stack = document.PopoverAutoStack();
-  CHECK(auto_stack.Contains(endpoint));
+  if (!auto_stack.Contains(endpoint)) {
+    // Event handlers from hint popovers could have closed our endpoint.
+    return;
+  }
   hide_stack_until(endpoint, auto_stack);
 }
 
