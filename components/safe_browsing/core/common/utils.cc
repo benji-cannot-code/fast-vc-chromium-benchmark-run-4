a160dd49257ee8c64c9b890e356020df4c0fd98a
Daniel Rubery <drubery@chromium.org>
2024-09-10 17:54:33 +0000

    Log which Safe Browsing endpoints are setting cookies
    Some endpoints seem to be processing cookies even for authenticated
    requests. To decrease the risk of removing the cookies from
    authenticated requests, we should identify which ones and clean them up.
    
    Bug: 40789301, b/360949211
    Change-Id: If4d8f6e9563a0bf47eca9d3d8ccd8f5a0903b9b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5845597
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353463}

--- components/safe_browsing/core/common/utils.cc
+++ components/safe_browsing/core/common/utils.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/http/http_status_code.h"
 #include "services/network/public/cpp/resource_request.h"
+#include "services/network/public/mojom/url_response_head.mojom.h"
 
 #if BUILDFLAG(IS_WIN)
 #include "base/enterprise_util.h"
@@ -118,6 +119,15 @@ bool CanGetReputationOfUrl(const GURL& url) {
   }
 
   return true;
+}
+
+void MaybeLogCookieReset(const network::SimpleURLLoader& loader,
+                         SafeBrowsingAuthenticatedEndpoint endpoint) {
+  if (loader.ResponseInfo() &&
+      loader.ResponseInfo()->headers->HasHeader("Set-Cookie")) {
+    base::UmaHistogramEnumeration(
+        "SafeBrowsing.AuthenticatedCookieResetEndpoint", endpoint);
+  }
 }
 
 void SetAccessTokenAndClearCookieInResourceRequest(
