12f1a1b41304ea80a307d780e81d37e65274dca3
Michael Checo <michaelcheco@google.com>
2024-09-10 15:22:14 +0000

    [WE] Ensure build used for Almanac requests contains official API keys
    Bug: b/361246860
    Change-Id: I853c3b3dfce66827d89c2b9b1b887fc1827ba1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839259
    Commit-Queue: Michael Checo <michaelcheco@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353357}

--- chrome/browser/ash/input_device_settings/peripherals_app_delegate_impl.cc
+++ chrome/browser/ash/input_device_settings/peripherals_app_delegate_impl.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/services/app_service/public/cpp/package_id.h"
 #include "components/version_info/version_info.h"
+#include "google_apis/google_api_keys.h"
 #include "services/network/public/cpp/shared_url_loader_factory.h"
 #include "services/network/public/cpp/simple_url_loader.h"
 #include "ui/base/webui/web_ui_util.h"
@@ -106,6 +107,15 @@ void PeripheralsAppDelegateImpl::OnDeviceInfoFetched(
 void PeripheralsAppDelegateImpl::GetCompanionAppInfo(
     const std::string& device_key,
     GetCompanionAppInfoCallback callback) {
+  // Ensure that the build uses the Google-internal file containing the
+  // official API keys, which are required to make queries to the Almanac.
+  if (!google_apis::IsGoogleChromeAPIKeyUsed() && !is_testing_) {
+    LOG(ERROR) << "Invalid API key or missing internal file. Cannot make "
+                  "queries to Almanac. Device key: "
+               << device_key;
+    return;
+  }
+
   Profile* active_user_profile = GetActiveUserProfile();
   device_info_manager_ =
       std::make_unique<apps::DeviceInfoManager>(active_user_profile);
