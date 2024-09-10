e67d230836596cfb33f2573357cd2ffe0c6abdae
Daniel Rubery <drubery@chromium.org>
2024-09-10 22:56:42 +0000

    Don't use operator<< for pointers in check_op.h
    There's only a few operator<< defined for pointers, but this includes
    `char*` and `unsigned char*`. Without this change, we log the pointed-to
    string or binary data whenever we have a check failure comparing two
    pointers. `base::span` does a lot of checks of `uint8_t*`, so it's nice
    to get better error messages there.
    
    See the failure of
    SafeBrowsingModuleVerifierWinTest.VerifyModuleLongModification in
    https://chromium-swarm.appspot.com/task?id=6be137c2ccf7ca10 for an
    example (the binary data seems to break the nice LUCI test parsing).
    
    Change-Id: I37683860f37819bf1cd8a7325621a71ae9028843
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840732
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353645}

---
+++ base/types/is_arc_pointer_unittest.mm
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/types/is_arc_pointer.h"
+
+#import <Foundation/Foundation.h>
+
+namespace base {
+namespace {
+
+// Some basic pointers that should not be ARC managed.
+static_assert(!IsArcPointer<int*>);
+static_assert(!IsArcPointer<void*>);
+
+// Objective-C object pointers
+static_assert(IsArcPointer<id>);
+static_assert(IsArcPointer<NSString*>);
+
+// Block pointers
+static_assert(IsArcPointer<void (^)()>);
+
+}  // namespace
+}  // namespace base
