3b323dd14e3fce7b37eb28225fa7de789f3a24e3
Istvan Nagy <iscsi@google.com>
2024-09-10 18:32:43 +0000

    [High5] Add countdown functionality to the PinStatusView
    Bug: b:365487979, b:348326316
    Change-Id: I2fa1e6a975217a160ca553637923e0e2a547f7bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844859
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Zi Lin <lziest@google.com>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353493}

--- ash/auth/views/pin_status_view.cc
+++ ash/auth/views/pin_status_view.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list_types.h"
 #include "base/strings/utf_string_conversions.h"
+#include "base/task/task_runner.h"
 #include "base/time/time.h"
 #include "chromeos/ash/components/cryptohome/auth_factor.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -41,7 +42,7 @@ std::u16string BuildPinStatusMessage(cryptohome::PinStatus* pin_status) {
     return u"";
   }
 
-  if (pin_status->AvailableAt() == base::Time::Max()) {
+  if (pin_status->AvailableAt().is_max()) {
     return l10n_util::GetStringUTF16(
         IDS_ASH_IN_SESSION_AUTH_PIN_TOO_MANY_ATTEMPTS);
   }
@@ -125,8 +126,35 @@ const std::u16string& PinStatusView::GetCurrentText() const {
 
 void PinStatusView::SetPinStatus(
     std::unique_ptr<cryptohome::PinStatus> pin_status) {
+  lockout_timer_.Stop();
+
   pin_status_ = std::move(pin_status);
+
   SetText(BuildPinStatusMessage(pin_status_.get()));
+  if (pin_status_ == nullptr) {
+    return;
+  }
+  if (!pin_status_->IsLockedFactor()) {
+    return;
+  }
+  if (pin_status_->AvailableAt().is_max()) {
+    return;
+  }
+
+  // We need to update the label every second. The timer is scheduled to be run
+  // more often to avoid possibly missing some updates due to timer imprecision.
+  lockout_timer_.Start(FROM_HERE, base::Milliseconds(500),
+                       base::BindRepeating(&PinStatusView::UpdateLockoutStatus,
+                                           weak_ptr_factory_.GetWeakPtr()));
+}
+
+void PinStatusView::UpdateLockoutStatus() {
+  CHECK(pin_status_);
+  SetText(BuildPinStatusMessage(pin_status_.get()));
+
+  if (!pin_status_->IsLockedFactor()) {
+    lockout_timer_.Stop();
+  }
 }
 
 BEGIN_METADATA(PinStatusView)
--- ash/auth/views/pin_status_view.h
+++ ash/auth/views/pin_status_view.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list_types.h"
+#include "base/timer/timer.h"
 #include "components/account_id/account_id.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/views/controls/label.h"
@@ -65,9 +66,13 @@ class ASH_EXPORT PinStatusView : public views::View {
   void SetPinStatus(std::unique_ptr<cryptohome::PinStatus> pin_status);
 
  private:
+  void UpdateLockoutStatus();
+
   raw_ptr<views::Label> text_label_ = nullptr;
 
   std::unique_ptr<cryptohome::PinStatus> pin_status_;
+
+  base::RepeatingTimer lockout_timer_;
 
   base::WeakPtrFactory<PinStatusView> weak_ptr_factory_{this};
 };
