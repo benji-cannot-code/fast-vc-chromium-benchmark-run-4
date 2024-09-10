9b1cd186a8ed8699591f3b9aaefb0970faafb9c4
Kam Kwankajornkiet <kamchonlathorn@google.com>
2024-09-10 09:25:28 +0000

    recorder: Add button role and tabindex to timestamp, paragraph
    Add button role to the transcription timestamp. Also add button role
    and tabindex to the paragraph to be navigated through keyboard.
    
    Bug: b:365599464
    Test: Chromevox, use search + space to trigger
    Change-Id: I699ef59abb81f17ec6856884e5f3d356efa31588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850009
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353217}

--- ash/webui/recorder_app_ui/resources/components/transcription-view.ts
+++ ash/webui/recorder_app_ui/resources/components/transcription-view.ts
@@ -151,8 +151,9 @@ export class TranscriptionView extends ReactiveLitElement {
         .seekable & {
           cursor: pointer;
 
-          &:hover {
+          &:hover, &:focus {
             background: var(--cros-sys-highlight_shape);
+            outline: none;
           }
         }
 
@@ -396,6 +397,8 @@ export class TranscriptionView extends ReactiveLitElement {
         return html`${leadingSpace ? ' ' : ''}<span
             class="sentence"
             data-start-ms=${ifDefined(sentence[0]?.timeRange?.startMs)}
+            tabindex=${this.seekable ? 0 : -1}
+            role="button"
             >${this.renderSentence(sentence)}</span
           >`;
       },
@@ -462,6 +465,7 @@ export class TranscriptionView extends ReactiveLitElement {
               class="timestamp"
               tabindex=${this.seekable ? 0 : -1}
               data-start-ms=${ifDefined(startTimeRange?.startMs)}
+              role="button"
             >
               ${startTimeDisplay}
               ${this.seekable ? html`<md-focus-ring></md-focus-ring>` : nothing}
