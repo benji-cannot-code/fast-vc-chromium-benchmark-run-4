f6b8ecb2d957bff44f7f5c0c8f01f79e600a4a5d
Calder Kitagawa <ckitagawa@chromium.org>
2024-09-10 13:42:52 +0000

    [Data Sharing] Change null check for synced group to assert
    This is essentially a revert of the following CL with minor changes.
    https://chromium-review.googlesource.com/c/chromium/src/+/5786896
    
    Instead of throwing an NPE, assert with a useful message.
    
    Also mark the test that was impacted by the original bug as disabled
    due to this invariant not holding as well.
    
    Bug: 360072870, 362762206
    Change-Id: Id9dea1402951236f9965036cc0897b4eaee63e69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5845648
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353298}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingTabManagerUnitTest.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingTabManagerUnitTest.java
@@ -355,6 +355,9 @@ public class DataSharingTabManagerUnitTest {
                 .when(mDistillerUrlUtilsJniMock)
                 .getOriginalUrlFromDistillerUrl(any(String.class));
 
+        mSavedTabGroup.collaborationId = null;
+        doReturn(mSavedTabGroup).when(mTabGroupSyncService).getGroup(LOCAL_ID);
+
         mDataSharingTabManager.createGroupFlow(
                 mActivity, TEST_GROUP_DISPLAY_NAME, LOCAL_ID, mCreateGroupFinishedCallback);
 
