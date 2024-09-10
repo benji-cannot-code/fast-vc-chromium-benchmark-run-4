8e647cd8c92d37b8d7df6f391a9d8767805a6594
Jan Keitel <jkeitel@google.com>
2024-09-10 10:15:45 +0000

    Show non-affiliation errors from inline creation suggestion
    Bug: 362445807
    Change-Id: I64af2d41ec3af9190c9a2dacb3ce1573fb051078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850468
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353235}

--- components/autofill/core/browser/mock_autofill_plus_address_delegate.h
+++ components/autofill/core/browser/mock_autofill_plus_address_delegate.h
@@ -71,7 +71,8 @@ class MockAutofillPlusAddressDelegate : public AutofillPlusAddressDelegate {
                UpdateSuggestionsCallback,
                HideSuggestionsCallback,
                PlusAddressCallback,
-               ShowAffiliationErrorDialogCallback),
+               ShowAffiliationErrorDialogCallback,
+               ShowErrorDialogCallback),
               (override));
 };
 
