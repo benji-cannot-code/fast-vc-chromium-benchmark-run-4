d1dcbcff9e11babb9226bcb99c16032e871ad3b7
Charles Hager <clhager@google.com>
2024-09-10 16:37:50 +0000

    Fix missing top controls after leaving fullscreen in splitscreen with edge-to-edge and add report
    This change handles for an odd issue where Android seems to pass in odd values to the CompositorViewHolder and ContentView, which make it seem like exiting fullscreen increases the size of the viewport. This confuses a check in the exit fullscreen `#onLayoutChange()`, skipping over the call to return the browser controls.
    
    This change ensures that the browser controls call always happens when in edge-to-edge, since it seems like the Chrome opt-out of system enforced edge-to-edge is leading to the odd values passed it. This also adds a report to help confirm whether this bug only happens during multi-window mode.
    
    https://screencast.googleplex.com/cast/NDY5MjgzNTkzMTUyMTAyNHw4YTkwMGUwOS04OQ
    
    Bug: 363349568
    Change-Id: I34098381633101c26057fa30ccfccbd332a9c669
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840694
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353409}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -251,6 +251,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kExperimentsForAgsa,
     &kFeedPositionAndroid,
     &kFocusOmniboxInIncognitoTabIntents,
+    &kForceBrowserControlsUponExitingFullscreen,
     &kForceListTabSwitcher,
     &kFullscreenInsetsApiMigration,
     &kFullscreenInsetsApiMigrationOnAutomotive,
@@ -728,6 +729,10 @@ BASE_FEATURE(kEducationalTipModule,
 
 BASE_FEATURE(kExperimentsForAgsa,
              "ExperimentsForAgsa",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
+BASE_FEATURE(kForceBrowserControlsUponExitingFullscreen,
+             "ForceBrowserControlsUponExitingFullscreen",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kFullscreenInsetsApiMigration,
