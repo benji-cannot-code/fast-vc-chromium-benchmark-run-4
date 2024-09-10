9d07b1dc7d2717707529207908a353ae0fd98afc
François Beaufort <beaufort.francois@gmail.com>
2024-09-10 14:06:01 +0000

    WebGPU: Remove broken SPIR-V support when creating shader module
    Bug: 365487252
    Change-Id: Iae23c6cd06241ef8c08ff67a083b4cd218ef1d49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850033
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353316}

--- third_party/blink/renderer/modules/webgpu/gpu_shader_module.h
+++ third_party/blink/renderer/modules/webgpu/gpu_shader_module.h
@@ -14,15 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 class GPUCompilationInfo;
 class GPUShaderModuleDescriptor;
-class ExceptionState;
 
 class GPUShaderModule : public DawnObject<wgpu::ShaderModule> {
   DEFINE_WRAPPERTYPEINFO();
 
  public:
   static GPUShaderModule* Create(GPUDevice* device,
-                                 const GPUShaderModuleDescriptor* webgpu_desc,
-                                 ExceptionState& exception_state);
+                                 const GPUShaderModuleDescriptor* webgpu_desc);
   explicit GPUShaderModule(GPUDevice* device,
                            wgpu::ShaderModule shader_module,
                            const String& label);
