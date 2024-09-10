b0752455d753aeac5c1a490b8181c1b42d1fbe0a
Sana Akbani <sanaakbani@google.com>
2024-09-10 18:06:42 +0000

    Remove and clean up old Translate Infobar code. Leaving translate_infobar_delegate because there are still iOS dependencies attached.
    Fixed: 353735844
    Bug: 40213244
    Change-Id: Ief6abe1485ef372f1ac44426b1edeb3b69b89208
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5727132
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Sana Akbani <sanaakbani@google.com>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353471}

--- chrome/browser/translate/fake_translate_agent.h
+++ chrome/browser/translate/fake_translate_agent.h
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/infobars/core/infobar_manager.h"
 #include "components/translate/content/browser/content_translate_driver.h"
 #include "components/translate/content/common/translate.mojom.h"
-#include "components/translate/core/browser/translate_infobar_delegate.h"
 #include "components/translate/core/browser/translate_manager.h"
 #include "components/translate/core/browser/translate_ui_delegate.h"
 #include "components/translate/core/common/language_detection_details.h"
--- ios/chrome/browser/translate/model/chrome_ios_translate_client.mm
+++ ios/chrome/browser/translate/model/chrome_ios_translate_client.mm
@@ -135,10 +135,6 @@ ChromeIOSTranslateClient::GetAcceptLanguagesService() {
       ChromeBrowserState::FromBrowserState(web_state_->GetBrowserState()));
 }
 
-int ChromeIOSTranslateClient::GetInfobarIconID() const {
-  return IDR_IOS_INFOBAR_TRANSLATE;
-}
-
 bool ChromeIOSTranslateClient::IsTranslatableURL(const GURL& url) {
   return TranslateServiceIOS::IsTranslatableURL(url);
 }
