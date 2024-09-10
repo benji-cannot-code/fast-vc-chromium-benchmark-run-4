5fd8ff16d9cdddb8c9541953793911fafc79724a
Sylvain Defresne <sdefresne@chromium.org>
2024-09-10 07:42:23 +0000

    [ios] Mark AccountConsistencyServiceFactory as created with Profile
    Manually creating the service in DoFinalInitForServices() was a
    work-around for unsafety in ServiceIsCreatedWithBrowserState().
    
    Since the unsafety in ServiceIsCreatedWithBrowserState() has been
    fixed with https://crrev.com/c/5558505, it is now the recommended
    way to have the service created with the Profile.
    
    Bug: none
    Change-Id: I9d4cd4c4cc1396609eb0267e8d3f8c77811364fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837099
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353183}

--- ios/chrome/browser/signin/model/account_consistency_service_factory.mm
+++ ios/chrome/browser/signin/model/account_consistency_service_factory.mm
@@ -17,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ios {
 
 AccountConsistencyServiceFactory::AccountConsistencyServiceFactory()
-    : ProfileKeyedServiceFactoryIOS("AccountConsistencyService") {
+    : ProfileKeyedServiceFactoryIOS("AccountConsistencyService",
+                                    ServiceCreation::kCreateWithProfile,
+                                    TestingCreation::kNoServiceForTests) {
   DependsOn(ios::AccountReconcilorFactory::GetInstance());
   DependsOn(ios::CookieSettingsFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
