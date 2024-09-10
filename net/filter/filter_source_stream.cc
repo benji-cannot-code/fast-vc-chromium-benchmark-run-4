d280a1da6a705903258916eb5f372be2e546dba9
Minoru Chikamune <chikamune@chromium.org>
2024-09-10 07:21:51 +0000

    Revert MiracleParameter code from FilterSourceStream
    This CL reverts the following CL because the experiment has ended.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/4827812
    
    Bug: 1475915
    Change-Id: I5bfbc1ebb0b4fa609a4484dfd4faa3ac96a0dec5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5848510
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353173}

--- net/filter/filter_source_stream.cc
+++ net/filter/filter_source_stream.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_util.h"
-#include "components/miracle_parameter/common/public/miracle_parameter.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 
@@ -29,14 +28,7 @@ constexpr char kXGZip[] = "x-gzip";
 constexpr char kBrotli[] = "br";
 constexpr char kZstd[] = "zstd";
 
-BASE_FEATURE(kBufferSizeForFilterSourceStreamFeature,
-             "BufferSizeForFilterSourceStreamFeature",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-MIRACLE_PARAMETER_FOR_INT(GetBufferSizeForFilterSourceStream,
-                          kBufferSizeForFilterSourceStreamFeature,
-                          "BufferSizeForFilterSourceStream",
-                          32 * 1024)
+const size_t kBufferSize = 32 * 1024;
 
 }  // namespace
 
@@ -57,8 +49,7 @@ int FilterSourceStream::Read(IOBuffer* read_buffer,
 
   // Allocate a BlockBuffer during first Read().
   if (!input_buffer_) {
-    input_buffer_ = base::MakeRefCounted<IOBufferWithSize>(
-        GetBufferSizeForFilterSourceStream());
+    input_buffer_ = base::MakeRefCounted<IOBufferWithSize>(kBufferSize);
     // This is first Read(), start with reading data from |upstream_|.
     next_state_ = STATE_READ_DATA;
   } else {
@@ -141,10 +132,9 @@ int FilterSourceStream::DoReadData() {
 
   next_state_ = STATE_READ_DATA_COMPLETE;
   // Use base::Unretained here is safe because |this| owns |upstream_|.
-  int rv =
-      upstream_->Read(input_buffer_.get(), GetBufferSizeForFilterSourceStream(),
-                      base::BindOnce(&FilterSourceStream::OnIOComplete,
-                                     base::Unretained(this)));
+  int rv = upstream_->Read(input_buffer_.get(), kBufferSize,
+                           base::BindOnce(&FilterSourceStream::OnIOComplete,
+                                          base::Unretained(this)));
 
   return rv;
 }
