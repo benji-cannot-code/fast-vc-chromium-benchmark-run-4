5cdca2d37f7cb81b3861e538329d7abb42483aab
Jian Li <jianli@chromium.org>
2024-09-10 17:47:34 +0000

    Enable feed containment feature by default
    Bug: none
    Change-Id: I3536238ab66ae6ab4b06af1337affde003dab7cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851691
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Commit-Queue: Jian Li <jianli@chromium.org>
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Auto-Submit: Jian Li <jianli@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353456}

--- components/feed/feed_feature_list.cc
+++ components/feed/feed_feature_list.cc
@@ -129,7 +129,7 @@ BASE_FEATURE(kRefreshFeedOnRestart,
 
 BASE_FEATURE(kFeedContainment,
              "FeedContainment",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kWebFeedKillSwitch,
              "WebFeedKillSwitch",
