4a66a08f99a0aece2b298f5b80d3a248e1f26c21
Charles Meng <charlesmeng@chromium.org>
2024-09-11 18:43:30 +0000

    Decouple ToastView colors from WebUI toasts
    Changed the definition for kColorToastBackground in a previous CL
    because I thought it wasn't being used elsewhere, but turns out it is
    used for WebUI toasts. This CL splits the tokens so that ToastView can
    use a different color without affecting WebUI toasts.
    
    Fixed: 362783137
    Change-Id: I538f10c3cb8a7223ed83a4c9a1006df2e11905e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851555
    Auto-Submit: Charles Meng <charlesmeng@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354094}

--- chrome/browser/ui/toasts/toast_view.cc
+++ chrome/browser/ui/toasts/toast_view.cc
@@ -107,7 +107,7 @@ void ToastView::Init() {
                                 ToastCloseReason::kActionButton)),
         action_button_text_));
     action_button_->SetEnabledTextColorIds(ui::kColorToastButton);
-    action_button_->SetBgColorIdOverride(ui::kColorToastBackground);
+    action_button_->SetBgColorIdOverride(ui::kColorToastBackgroundProminent);
     action_button_->SetStrokeColorIdOverride(ui::kColorToastButton);
     action_button_->SetPreferredSize(gfx::Size(
         action_button_->GetPreferredSize().width(),
@@ -172,7 +172,7 @@ void ToastView::AnimateIn() {
       GetScaleTransformation(bubble_frame_view->bounds()));
   bubble_frame_view->layer()->SetOpacity(0);
   GetDialogClientView()->SetBackground(
-      views::CreateThemedSolidBackground(ui::kColorToastBackground));
+      views::CreateThemedSolidBackground(ui::kColorToastBackgroundProminent));
   GetDialogClientView()->SetPaintToLayer();
   GetDialogClientView()->layer()->SetOpacity(0);
   views::AnimationBuilder()
@@ -227,7 +227,7 @@ gfx::Rect ToastView::GetBubbleBounds() {
 void ToastView::OnThemeChanged() {
   BubbleDialogDelegateView::OnThemeChanged();
   const auto* color_provider = GetColorProvider();
-  set_color(color_provider->GetColor(ui::kColorToastBackground));
+  set_color(color_provider->GetColor(ui::kColorToastBackgroundProminent));
   icon_view_->SetImage(ui::ImageModel::FromVectorIcon(
       *icon_, color_provider->GetColor(ui::kColorToastForeground),
       ChromeLayoutProvider::Get()->GetDistanceMetric(
--- ui/color/color_id.h
+++ ui/color/color_id.h
@@ -482,6 +482,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorThrobber) \
   E_CPONLY(kColorThrobberPreconnect) \
   E_CPONLY(kColorToastBackground) \
+  E_CPONLY(kColorToastBackgroundProminent) \
   E_CPONLY(kColorToastButton) \
   E_CPONLY(kColorToastForeground) \
   E_CPONLY(kColorToggleButtonHover) \
