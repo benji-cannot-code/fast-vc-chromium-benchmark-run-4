8119a9c167a7c761a858ba3a65c5be849bafd376
mlcui <mlcui@google.com>
2024-09-11 09:29:06 +0000

    Fix sending old Chrome versions to the server
    `prefs::kProfileCreatedByVersion` is the first Chrome version that
    created the profile. For example, the author's version of Chrome has a
    `profile.created_by_version` of M114, even though it's currently running
    on M128.
    
    Fix this by using `version_info::GetVersion()`, which is the same method
    that Finch uses to get the current version [1].
    
    [1]: https://crsrc.org/c/components/variations/service/variations_field_trial_creator_base.cc;l=642;drc=130.0.6709.1
    
    Bug: None
    Change-Id: I6a6a21002bf22f7a378071b018b9056cf1ec3e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5848789
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353819}

--- chrome/browser/manta/manta_service_factory.cc
+++ chrome/browser/manta/manta_service_factory.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <memory>
 
+#include "base/version.h"
 #include "base/version_info/channel.h"
+#include "base/version_info/version_info.h"
 #include "build/chromeos_buildflags.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
@@ -73,16 +75,16 @@ MantaServiceFactory::BuildServiceInstanceForBrowserContext(
                                               signin::ConsentLevel::kSync);
   bool is_otr_profile = !profile->IsRegularProfile() || !is_signed_in;
 
-  std::string chrome_version, locale;
-  if (PrefService* pref_service = profile->GetPrefs()) {
-    chrome_version = pref_service->GetString(prefs::kProfileCreatedByVersion);
-    // Check to make sure that the locale pref is set before accessing.
+  std::string chrome_version = version_info::GetVersion().GetString();
+  std::string locale;
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+  if (PrefService* pref_service = profile->GetPrefs()) {
+    // Check to make sure that the locale pref is set before accessing.
     locale = pref_service->GetString(language::prefs::kApplicationLocale);
-#else
-    locale = g_browser_process->GetApplicationLocale();
-#endif
   }
+#else
+  locale = g_browser_process->GetApplicationLocale();
+#endif
 
   return std::make_unique<MantaService>(
       profile->GetDefaultStoragePartition()
