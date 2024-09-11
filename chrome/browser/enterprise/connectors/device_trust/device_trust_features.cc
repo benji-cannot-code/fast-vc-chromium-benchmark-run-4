db3f5f1abdc6dbea6ae61f54098bceea5f784be5
Parastoo Geranmayeh <parastoog@google.com>
2024-09-11 14:29:20 +0000

    Enable DTCKeyUploadedBySharedAPIEnabled by default.
    It's recommended to enable a feature by default on ToT before fully launching it by finch.
    
    Bug: 317367921, 352348248
    Change-Id: Ic568a68463386c370bed457c31f3b0455b73e141
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850616
    Reviewed-by: Sébastien Lalancette <seblalancette@chromium.org>
    Commit-Queue: Sébastien Lalancette <seblalancette@chromium.org>
    Auto-Submit: Parastoo Geranmayeh <parastoog@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353924}

--- chrome/browser/enterprise/connectors/device_trust/device_trust_features.cc
+++ chrome/browser/enterprise/connectors/device_trust/device_trust_features.cc
@@ -17,7 +17,7 @@ bool IsKeyRotationEnabled() {
 
 BASE_FEATURE(kDTCKeyUploadedBySharedAPIEnabled,
              "DTCKeyUploadedBySharedAPIEnabled",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 bool IsDTCKeyUploadedBySharedAPI() {
   return base::FeatureList::IsEnabled(kDTCKeyUploadedBySharedAPIEnabled);
