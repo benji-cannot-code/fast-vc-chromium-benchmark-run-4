9558e0c745f6173515331e19973bf927c9a0b76f
François Beaufort <beaufort.francois@gmail.com>
2024-09-11 17:34:18 +0000

    WebGPU: Deprecate GPUAdapter requestAdapterInfo() method
    Spec PR: https://github.com/gpuweb/gpuweb/pull/4662
    Intent to deprecate: https://groups.google.com/a/chromium.org/g/blink-dev/c/HxOgGf4NzQ4
    
    Bug: 335383516
    Change-Id: Ie396d3d462d07b77aaeb1ffeaca008c701478e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5578020
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Fr <beaufort.francois@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1354048}

---
+++ third_party/blink/web_tests/http/tests/inspector-protocol/issues/gpuadapter-requestadapterinfo-deprecation.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(async function(/** @type {import('test_runner').TestRunner} */ testRunner) {
+  const { session, dp } = await testRunner.startBlank(`Tests that GPUAdapter requestAdapterInfo() deprecation issues are reported`);
+  await dp.Audits.enable();
+  const promise = dp.Audits.onceIssueAdded();
+  session.evaluate(`(async function() {
+    const adapter = await navigator.gpu.requestAdapter();
+    await adapter.requestAdapterInfo();
+  })()`);
+  const result = await promise;
+  testRunner.log(result.params, "Inspector issue: ");
+  testRunner.completeTest();
+})
