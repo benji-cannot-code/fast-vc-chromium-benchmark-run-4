73204204a9bc08d6b9c2f8d5c396536e51b1742b
Maksim Sisov <msisov@igalia.com>
2024-09-11 09:12:45 +0000

    modularization: pull out browser_list(_observer).
    There are quite a few headers in the //c/b/ui that include the
    observer and the browser_list from their header, creating
    a circular dep from their source set when modularized. Thus,
    pull this out into a separate target.
    
    PS it's basically a blocker of //c/b/ui/webui/signin. thus,
    adding that component's modularization bug. Though, there
    are ton of others that include these headers.
    
    Bug: 364667551
    Change-Id: I2441598a032ed30c5bb91f91da4eb9b578cf317e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849968
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1353792}

--- chrome/browser/apps/app_service/media_access_browsertest.cc
+++ chrome/browser/apps/app_service/media_access_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/media/webrtc/media_stream_capture_indicator.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/web_applications/test/web_app_browsertest_util.h"
 #include "chrome/browser/ui/web_applications/web_app_browsertest_base.h"
--- chrome/browser/extensions/api/cookies/cookies_api.cc
+++ chrome/browser/extensions/api/cookies/cookies_api.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/extension_telemetry/cookies_get_signal.h"
 #include "chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service.h"
 #include "chrome/browser/safe_browsing/extension_telemetry/extension_telemetry_service_factory.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/common/extensions/api/cookies.h"
 #include "components/safe_browsing/core/common/features.h"
