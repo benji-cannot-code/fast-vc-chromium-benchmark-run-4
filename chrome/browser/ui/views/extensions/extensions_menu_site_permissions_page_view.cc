e1e9692c20031edbc6f349aa8545ba409bde1fe1
EmiliaPaz <emiliapaz@chromium.org>
2024-09-11 17:16:43 +0000

    [Extensions c2s] Add close button in extensions menu
    Add close button to both pages of the extensions menu. as of latest
    mocks. Menu is behind extensions_features::kExtensionsMenuAccessControl
    feature flag.
    
    Screenshots: https://drive.google.com/file/d/1MhtmjOr-EbbssZx3YdoGs9a1wZqgomPV/view?usp=sharing
    
    Bug: 40879945
    Change-Id: I8800696b1febe9a6cd2aa5a1068578ade2a51254
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851893
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354026}

--- chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_site_permissions_page_view.cc
@@ -179,7 +179,7 @@ ExtensionsMenuSitePermissionsPageView::ExtensionsMenuSitePermissionsPageView(
           DISTANCE_CONTROL_LIST_VERTICAL) /
       2;
 
-  // Views that need confirgutation after construction (e.g access size after a
+  // Views that need configuration after construction (e.g access size after a
   // separate view is constructed).
   views::Label* toggle_label;
 
@@ -245,17 +245,14 @@ ExtensionsMenuSitePermissionsPageView::ExtensionsMenuSitePermissionsPageView(
       .SetLayoutManager(std::make_unique<views::BoxLayout>(
           views::BoxLayout::Orientation::kVertical))
       .AddChildren(
-          // Subheader.
+          // Header.
           views::Builder<views::FlexLayoutView>()
-              .SetCrossAxisAlignment(views::LayoutAlignment::kStart)
               // Add top dialog margins, since its the first element, and
               // horizontal dialog margins. Vertical margins will be handled
               // in between the contents.
               .SetInteriorMargin(gfx::Insets::TLBR(dialog_insets.top(),
                                                    dialog_insets.left(), 0,
                                                    dialog_insets.right()))
-              .SetProperty(views::kBoxLayoutFlexKey,
-                           views::BoxLayoutFlexSpecification())
               .AddChildren(
                   // Back button.
                   views::Builder<views::ImageButton>(
@@ -291,7 +288,13 @@ ExtensionsMenuSitePermissionsPageView::ExtensionsMenuSitePermissionsPageView(
                                   kColorExtensionsMenuSecondaryText)
                               .SetProperty(views::kMarginsKey,
                                            gfx::Insets::TLBR(
-                                               0, horizontal_spacing, 0, 0)))),
+                                               0, horizontal_spacing, 0, 0))),
+                  // Close button.
+                  views::Builder<views::Button>(
+                      views::BubbleFrameView::CreateCloseButton(
+                          base::BindRepeating(
+                              &ExtensionsMenuHandler::CloseBubble,
+                              base::Unretained(menu_handler))))),
           create_separator_builder(/*full_width=*/true,
                                    /*is_bottom_hover_button=*/false),
           // Content.
