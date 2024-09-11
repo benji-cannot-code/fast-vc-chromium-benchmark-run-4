b191667c058a516d24b4c79c215aed9cac8b7a96
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-09-11 22:41:44 +0000

    [Extensions] Instantiate PermissionsManager factory early
    KeyedService factories should be instantiated before profile
    initialization is complete, and should normally be done in the
    `EnsureCoreBrowserContextKeyedServiceFactoriesBuilt()` methods.
    PermissionsManager wasn't -- in production, this didn't cause issues
    because other classes called into PermissionsManager as part of their
    setup, which resulted in implicitly creating the PermissionsManager.
    However, this is an implementation detail, and not guaranteed to be
    consistent (and isn't consistent in the experimental android-desktop
    build config).
    
    Register PermissionsManager's factory as part of the standard
    `EnsureCoreBrowserContextKeyedServiceFactoriesBuilt()` method.
    
    Bug: 356905053
    Change-Id: I521120b078730a128f5224510c0b69baed95cced
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855301
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Auto-Submit: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354232}

--- extensions/browser/core_browser_context_keyed_service_factories.cc
+++ extensions/browser/core_browser_context_keyed_service_factories.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_prefs_helper_factory.h"
 #include "extensions/browser/extension_protocols.h"
 #include "extensions/browser/image_loader_factory.h"
+#include "extensions/browser/permissions_manager.h"
 #include "extensions/browser/process_manager_factory.h"
 #include "extensions/browser/renderer_startup_helper.h"
 #include "extensions/browser/service_worker/service_worker_keepalive.h"
@@ -50,6 +51,7 @@ void EnsureCoreBrowserContextKeyedServiceFactoriesBuilt() {
 #if BUILDFLAG(ENABLE_GUEST_VIEW)
   MimeHandlerStreamManager::EnsureFactoryBuilt();
 #endif
+  PermissionsManager::GetFactory();
   ProcessManagerFactory::GetInstance();
   RendererStartupHelperFactory::GetInstance();
   ServiceWorkerKeepalive::EnsureShutdownNotifierFactoryBuilt();
