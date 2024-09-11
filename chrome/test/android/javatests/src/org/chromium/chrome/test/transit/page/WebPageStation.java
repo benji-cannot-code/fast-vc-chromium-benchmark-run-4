4cb8a11848978ab5ee7a042b7dc7435dbbacca4a
Henrique Nakashima <hnakashima@chromium.org>
2024-09-11 17:23:28 +0000

    [Android] Add #scrollToBottom to WebPageStation
    TopBottomLinksPageStation still keeps a version using a drag gesture
    which will trigger browser controls to disappear.
    
    Bug: 363308203
    Change-Id: Ib7d28d39ce7b6b44fd5c139f7e6508370515757d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5841753
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354033}

--- chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/WebPageStation.java
+++ chrome/test/android/javatests/src/org/chromium/chrome/test/transit/page/WebPageStation.java
@@ -15,6 +15,7 @@ import org.chromium.base.test.transit.ConditionStatus;
 import org.chromium.base.test.transit.ConditionStatusWithResult;
 import org.chromium.base.test.transit.ConditionWithResult;
 import org.chromium.base.test.transit.Elements;
+import org.chromium.base.test.transit.Transition;
 import org.chromium.base.test.transit.UiThreadCondition;
 import org.chromium.base.test.transit.ViewElement;
 import org.chromium.base.test.transit.ViewSpec;
@@ -92,6 +93,20 @@ public class WebPageStation extends PageStation {
     public IncognitoWebPageAppMenuFacility openIncognitoTabAppMenu() {
         assert mIncognito;
         return enterFacilitySync(new IncognitoWebPageAppMenuFacility(), MENU_BUTTON::click);
+    }
+
+    /** Trigger to scroll WebContents to the bottom. */
+    public Transition.Trigger scrollToBottomTrigger() {
+        return () -> {
+            assertSuppliersCanBeUsed();
+            try {
+                JavaScriptUtils.executeJavaScriptAndWaitForResult(
+                        mWebContentsSupplier.get(),
+                        "window.scrollTo(0, document.body.scrollHeight)");
+            } catch (TimeoutException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 
     private static class WebContentsPresentCondition extends ConditionWithResult<WebContents> {
