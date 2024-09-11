532ce30e178009b7e0905da7ce14b27d8230530b
Mariam Ali <alimariam@google.com>
2024-09-11 09:41:46 +0000

    [iOS][QD] Add a blocking overlay in multiwindow while data is deleted
    Video of the flow: http://dr/file/d/1RYd7yAP1Ulm1IHVa-hoBFPm2sDON4DMM/view
    
    Fixed: 353954043
    Bug: 335387869
    Low-Coverage-Reason: HARD_TO_TEST
    Change-Id: Ied5265f175dc516f85f9ee0576b95f61bbef0506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5844871
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Cr-Commit-Position: refs/heads/main@{#1353839}

--- ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_coordinator.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_coordinator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/browsing_data/model/browsing_data_remover_factory.h"
 #import "ios/chrome/browser/browsing_data/model/tabs_closure_util.h"
 #import "ios/chrome/browser/discover_feed/model/discover_feed_service_factory.h"
+#import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/shared/model/url/chrome_url_constants.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/tabs_animation_commands.h"
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
+#import "ios/chrome/browser/ui/scoped_ui_blocker/scoped_ui_blocker.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/browsing_data_counter_wrapper_producer.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/clear_browsing_data_ui_constants.h"
 #import "ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_browsing_data_coordinator.h"
@@ -42,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   QuickDeleteViewController* _viewController;
   QuickDeleteMediator* _mediator;
   QuickDeleteBrowsingDataCoordinator* _browsingDataCoordinator;
+  std::unique_ptr<ScopedUIBlocker> _windowUIBlocker;
 
   // The tabs closure animation should only be performed if Quick Delete is
   // opened on top of a tab or the tab grid.
@@ -196,6 +199,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                          completion:dismissCompletionBlock];
 }
 
+- (void)blockOtherWindows {
+  SceneState* sceneState = self.browser->GetSceneState();
+  _windowUIBlocker = std::make_unique<ScopedUIBlocker>(sceneState);
+}
+
+- (void)releaseOtherWindows {
+  _windowUIBlocker.reset();
+}
+
 #pragma mark - UIAdaptivePresentationControllerDelegate
 
 - (void)presentationControllerDidDismiss:
@@ -231,7 +243,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                     allInactiveTabs:(BOOL)animateAllInactiveTabs
                 browsingDataRemover:(BrowsingDataRemover*)browsingDataRemover {
   base::OnceClosure onRemoverCompletion = base::BindOnce(
-      [](UIWindow* window) {
+      [](UIWindow* window, std::unique_ptr<ScopedUIBlocker> uiBlocker) {
+        uiBlocker.reset();
         window.userInteractionEnabled = YES;
         window.accessibilityElementsHidden = NO;
 
@@ -246,7 +259,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         // rearrange.
         TriggerHapticFeedbackForNotification(UINotificationFeedbackTypeSuccess);
       },
-      self.baseViewController.view.window);
+      self.baseViewController.view.window, std::move(_windowUIBlocker));
 
   base::OnceClosure onAnimationCompletion = base::BindOnce(
       &BrowsingDataRemover::RemoveInRange, browsingDataRemover->AsWeakPtr(),
@@ -265,6 +278,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Disconnects all instances.
 - (void)disconnect {
+  if (_windowUIBlocker) {
+    _windowUIBlocker.reset();
+  }
   _viewController.presentationHandler = nil;
   _viewController.mutator = nil;
   _viewController.presentationController.delegate = nil;
--- ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_presentation_commands.h
+++ ios/chrome/browser/ui/settings/clear_browsing_data/quick_delete_presentation_commands.h
@@ -30,6 +30,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                   cachedTabsInfo:
                                       (tabs_closure_util::WebStateIDToTime)
                                           cachedTabsInfo;
+
+// Method invoked on deletion in progress to block other windows to avoid having
+// multiple deletions occur concurrently.
+- (void)blockOtherWindows;
+
+// Method invoked on deletion completed to release other blocked windows.
+- (void)releaseOtherWindows;
+
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_SETTINGS_CLEAR_BROWSING_DATA_QUICK_DELETE_PRESENTATION_COMMANDS_H_
