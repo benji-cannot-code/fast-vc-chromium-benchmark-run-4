80e5f5e444b9a5288750c5fd3031712ad3036113
Tommy Steimel <steimel@chromium.org>
2024-09-11 18:15:53 +0000

    Remove metrics for when file dialogs and pip windows are both open
    This CL removes the metrics we added to understand how often file
    dialogs and picture-in-picture windows are both open. These are no
    longer needed as we've settled on a solution based on the data
    already collected.
    
    Bug: 40076120
    Change-Id: I323211d46c8399a7911592027c985d084057a70e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5852356
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354082}

--- chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
+++ chrome/browser/picture_in_picture/picture_in_picture_window_manager.cc
@@ -128,11 +128,6 @@ void PictureInPictureWindowManager::EnterPictureInPictureWithController(
   pip_window_controller_->Show();
 
 #if !BUILDFLAG(IS_ANDROID)
-  RecordFileDialogOpenMetric(
-      number_of_open_file_dialogs_ > 0
-          ? FileDialogOpenState::kPictureInPictureOpenWithFileDialog
-          : FileDialogOpenState::kPictureInPictureOpenWithoutFileDialog);
-
   if (number_of_existing_scoped_disallow_picture_in_pictures_ > 0) {
     // Don't exit picture-in-picture synchronously since exiting in the middle
     // of opening leaves us in a bad state.
@@ -436,11 +431,6 @@ void PictureInPictureWindowManager::CreateWindowInternal(
   pip_window_controller_ = video_pip_window_controller;
 
 #if !BUILDFLAG(IS_ANDROID)
-  RecordFileDialogOpenMetric(
-      number_of_open_file_dialogs_ > 0
-          ? FileDialogOpenState::kPictureInPictureOpenWithFileDialog
-          : FileDialogOpenState::kPictureInPictureOpenWithoutFileDialog);
-
   if (number_of_existing_scoped_disallow_picture_in_pictures_ > 0) {
     // Don't exit picture-in-picture synchronously since exiting in the middle
     // of opening leaves us in a bad state.
@@ -542,19 +532,6 @@ void PictureInPictureWindowManager::CreateOcclusionTrackerIfNecessary() {
   }
 }
 
-void PictureInPictureWindowManager::OnFileDialogOpened() {
-  number_of_open_file_dialogs_++;
-  RecordFileDialogOpenMetric(
-      pip_window_controller_
-          ? FileDialogOpenState::kFileDialogOpenWithPictureInPicture
-          : FileDialogOpenState::kFileDialogOpenWithoutPictureInPicture);
-}
-
-void PictureInPictureWindowManager::OnFileDialogClosed() {
-  CHECK_NE(number_of_open_file_dialogs_, 0u);
-  number_of_open_file_dialogs_--;
-}
-
 bool PictureInPictureWindowManager::ShouldFileDialogBlockPictureInPicture(
     content::WebContents* owner_web_contents) {
   if (!base::FeatureList::IsEnabled(media::kFileDialogsBlockPictureInPicture)) {
@@ -633,12 +610,6 @@ void PictureInPictureWindowManager::
         "Media.DocumentPictureInPicture.RequestedSizeToScreenRatio",
         recorded_percent);
   }
-}
-
-void PictureInPictureWindowManager::RecordFileDialogOpenMetric(
-    FileDialogOpenState state) {
-  base::UmaHistogramEnumeration("Media.PictureInPicture.FileDialogOpenState",
-                                state);
 }
 
 void PictureInPictureWindowManager::RecordPictureInPictureDisallowed(
