d529c43b67c527e16fb28c010a40f05d97e61036
Dominic Battre <battre@chromium.org>
2024-09-11 09:14:52 +0000

    Fix crash in address parsing
    crbug.com/365252089 is a CHECK()-based crash because the address parser
    tried to assign an apartment or floor number in an address model that
    did not support these fields:
    
    // Setting the value should always work unless the regular expression is
    // invalid.
    CHECK(SetValueForType(...));
    
    Famous last words...
    
    What happened was that for a street address like "2 Foo, Apt 2\n" the
    country specific address regex failed to match and
    StreetAddressNode::GetParseRegularExpressionsByRelevance() was consulted
    to provide fallback regex. These fallback regex came from the legacy
    model and tried to assign an apartment number and floor which don't
    exist in the new address models.
    
    Fixed: 365252089
    Change-Id: I9af146389178014d907ac64eb7c44ab7be574c8b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851073
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353795}

--- components/autofill/core/browser/data_model/autofill_structured_address.cc
+++ components/autofill/core/browser/data_model/autofill_structured_address.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/core/browser/autofill_type.h"
+#include "components/autofill/core/browser/data_model/autofill_i18n_api.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_component.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_regex_provider.h"
 #include "components/autofill/core/browser/data_model/autofill_structured_address_utils.h"
@@ -93,6 +94,13 @@ StreetAddressNode::GetParseRegularExpressionsByRelevance() const {
   DCHECK(pattern_provider);
   const std::string country_code =
       base::UTF16ToUTF8(GetRootNode().GetValueForType(ADDRESS_HOME_COUNTRY));
+  // Countries with custom hierarchies are not guaranteed to support street
+  // names, house numbers, floors and apartment numbers. Therefore, we don't
+  // apply the hardcoded fallback regular expressions.
+  if (i18n_model_definition::IsCustomHierarchyAvailableForCountry(
+          AddressCountryCode(country_code))) {
+    return {};
+  }
   return {pattern_provider->GetRegEx(RegEx::kParseHouseNumberStreetName,
                                      country_code),
           pattern_provider->GetRegEx(RegEx::kParseStreetNameHouseNumber,
