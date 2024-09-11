f4234c90b672ea3addcbedee0bd26102a9bb9b6e
Asami Doi <asamidoi@chromium.org>
2024-09-11 14:56:33 +0000

    [ios] Add test config for kNewSyncOptInIllustration
    The original CL was reverted due to the test failure.
    https://chromium-review.googlesource.com/c/chromium/src/+/5851558
    
    This CL adds test config for kNewSyncOptInIllustration with a fix
    for the test.
    
    Bug: 361249212
    Change-Id: I4f10e1d2125e5f4d4d9aa617332411feddfb9d73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851037
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353942}

--- ios/chrome/browser/first_run/ui_bundled/first_run_without_search_engine_choice_egtest.mm
+++ ios/chrome/browser/first_run/ui_bundled/first_run_without_search_engine_choice_egtest.mm
@@ -1314,7 +1314,9 @@ id<GREYMatcher> ManageUMALinkMatcher() {
               grey_accessibilityID(
                   kPromoStyleHeaderViewBackgroundAccessibilityIdentifier),
               chrome_test_util::ImageViewWithImageNamed(
-                  @"history_sync_opt_in_background"),
+                  IsNewSyncOptInIllustration()
+                      ? @"sync_opt_in_background"
+                      : @"history_sync_opt_in_background"),
               grey_sufficientlyVisible(), nil)]
       assertWithMatcher:grey_notNil()];
 }
