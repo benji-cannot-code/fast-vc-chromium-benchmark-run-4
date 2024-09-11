18be516b4ec42a884f4149a01a73570a27a774a2
Georg Neis <neis@chromium.org>
2024-09-11 01:46:17 +0000

    Create //chromeos/ash/experiences/screenshot_area
    Bug: b:358251266
    Change-Id: I70fae0cda0aa80678d0b89e5c720978b54b1267e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5782691
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353714}

--- chrome/browser/ash/policy/dlp/dlp_content_manager_ash.h
+++ chrome/browser/ash/policy/dlp/dlp_content_manager_ash.h
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_content_restriction_set.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
-#include "chrome/browser/ui/ash/screenshot_area.h"
+#include "chromeos/ash/experiences/screenshot_area/screenshot_area.h"
 #include "components/exo/window_properties.h"
 #include "content/public/browser/desktop_media_id.h"
 #include "content/public/browser/media_stream_request.h"
--- chromeos/ash/experiences/screenshot_area/screenshot_area.h
+++ chromeos/ash/experiences/screenshot_area/screenshot_area.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_ASH_SCREENSHOT_AREA_H_
-#define CHROME_BROWSER_UI_ASH_SCREENSHOT_AREA_H_
+#ifndef CHROMEOS_ASH_EXPERIENCES_SCREENSHOT_AREA_SCREENSHOT_AREA_H_
+#define CHROMEOS_ASH_EXPERIENCES_SCREENSHOT_AREA_SCREENSHOT_AREA_H_
 
 #include <optional>
 
@@ -44,4 +44,4 @@ struct ScreenshotArea {
                  std::optional<const gfx::Rect> rect);
 };
 
-#endif  // CHROME_BROWSER_UI_ASH_SCREENSHOT_AREA_H_
+#endif  // CHROMEOS_ASH_EXPERIENCES_SCREENSHOT_AREA_SCREENSHOT_AREA_H_
