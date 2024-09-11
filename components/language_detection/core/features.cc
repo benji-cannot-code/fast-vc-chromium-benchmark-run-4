bb41be610a3ea1d48f4bb9f17f68c52238f4f745
Keita Suzuki <suzukikeita@chromium.org>
2024-09-11 00:29:18 +0000

    add `LazyUpdateTranslateModel` feature
    This CL adds `LazyUpdateTranslateModel` feature, which lazily updates
    the Translation Model within `LanguageDetectionModel` when enabled.
    By enabling this, we will be able to optimize the renderer
    tasks particualarly around navigation commit.
    
    Doc (internal): https://docs.google.com/document/d/1W74fOQIYSPImxFSxvhOugCbrv3jQIr8lyNWrKahJ_RE
    
    Bug: 361215212
    Change-Id: Iac506586d1e942cf74bdf7eac3ab951dcb7bce73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5802552
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Trevor Perrier <perrier@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353687}

---
+++ components/language_detection/core/features.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/language_detection/core/features.h"
+
+#include "base/feature_list.h"
+
+namespace language_detection::features {
+
+// If enabled, we lazily initiate `TranslateAgent` in
+// `ChromeRenderFrameObserver` (crbug/361215212).
+BASE_FEATURE(kLazyUpdateTranslateModel,
+             "LazyUpdateTranslateModel",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+}  // namespace language_detection::features
---
+++ components/language_detection/core/features.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_LANGUAGE_DETECTION_CORE_FEATURES_H_
+#define COMPONENTS_LANGUAGE_DETECTION_CORE_FEATURES_H_
+
+#include "base/component_export.h"
+#include "base/feature_list.h"
+
+namespace language_detection::features {
+
+COMPONENT_EXPORT(LANGUAGE_DETECTION)
+BASE_DECLARE_FEATURE(kLazyUpdateTranslateModel);
+
+}  // namespace language_detection::features
+
+#endif  // COMPONENTS_LANGUAGE_DETECTION_CORE_FEATURES_H_
