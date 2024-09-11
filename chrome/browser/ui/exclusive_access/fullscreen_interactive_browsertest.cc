d38fa39c1eeaaf2d28d60e70e0e18d321997597e
Antonio Gomes <tonikitoo@igalia.com>
2024-09-11 23:10:44 +0000

    Finish modularization of //chrome/browser/ui/exclusive_access/
    It has been started by erikchen@ in [1], but tests were left off.
    This CL finishes it up.
    
    Bug: 339497734
    
    [1] https://crrev.com/c/5715347
    
    R=erikchen@chromium.org
    
    Change-Id: I600785d75e08694a6b594831e66a9861240ddcf7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853659
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1354252}

--- chrome/browser/ui/exclusive_access/fullscreen_interactive_browsertest.cc
+++ chrome/browser/ui/exclusive_access/fullscreen_interactive_browsertest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/run_until.h"
 #include "build/build_config.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/test/fullscreen_test_util.h"
 #include "chrome/browser/ui/test/popup_test_base.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
