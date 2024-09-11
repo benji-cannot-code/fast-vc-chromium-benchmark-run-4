f0cc28c219248647d60ae404be6514ed2d8f41c3
오백원짜리 <yongsangpark980813@gmail.com>
2024-09-11 02:25:25 +0000

    Delete duplicated code. tab_strip_model_observer.h already has it.
    Change-Id: Ia6e6347251cfb996bb0ecc3e89786343ac638bfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5772172
    Reviewed-by: Bret Sepulveda <bsep@chromium.org>
    Reviewed-by: Joonghun Park <pjh0718@gmail.com>
    Commit-Queue: Joonghun Park <pjh0718@gmail.com>
    Cr-Commit-Position: refs/heads/main@{#1353725}

--- chrome/browser/ui/tabs/tab_strip_model_observer.cc
+++ chrome/browser/ui/tabs/tab_strip_model_observer.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check_op.h"
-#include "base/memory/raw_ptr.h"
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/web_contents.h"
