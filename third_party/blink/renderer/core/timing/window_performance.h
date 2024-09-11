61559a448db6173bd2f7e885981b55bd7114d177
Annie Sullivan <sullivan@chromium.org>
2024-09-11 13:39:22 +0000

    Update EventTiming trace events to use more fine-grained timing data.
    * Report DOMHighResTimestamps as doubles
    * Remove rounding from `duration`
    
    Bug: 40942879
    Change-Id: I4599400030ceea01ed895914f3bb4e049ca327bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805269
    Auto-Submit: Annie Sullivan <sullivan@chromium.org>
    Commit-Queue: Michal Mocny <mmocny@chromium.org>
    Reviewed-by: Michal Mocny <mmocny@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353907}

--- third_party/blink/renderer/core/timing/window_performance.h
+++ third_party/blink/renderer/core/timing/window_performance.h
@@ -73,6 +73,8 @@ class CORE_EXPORT WindowPerformance final : public Performance,
   explicit WindowPerformance(LocalDOMWindow*);
   ~WindowPerformance() override;
 
+  static base::TimeTicks GetTimeOrigin(LocalDOMWindow* window);
+
   ExecutionContext* GetExecutionContext() const override;
 
   PerformanceTiming* timing() const override;
