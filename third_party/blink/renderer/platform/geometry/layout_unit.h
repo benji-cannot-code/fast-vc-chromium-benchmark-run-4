52491f020e4217bbfb28b26901083abd3551ba40
Koji Ishii <kojii@chromium.org>
2024-09-11 06:26:29 +0000

    [layout-unit] Remove unnecessary specialization for `To()`
    This patch removes unnecessary specialization for `To()`. The
    specializations originally done in `To()` were moved to
    `FromFixed()`, but the specialization for `To()` was left
    over. This patch removes it.
    
    This patch has no behavior changes.
    
    Bug: 351092748
    Change-Id: Ic45151b194a731d6f11c1b2096b0f62e956292e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849646
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353783}

--- third_party/blink/renderer/platform/geometry/layout_unit.h
+++ third_party/blink/renderer/platform/geometry/layout_unit.h
@@ -204,12 +204,6 @@ class PLATFORM_EXPORT FixedPoint {
 
   // Convert to a `FixedPoint` with a different storage and/or precision.
   template <typename Target>
-    requires(Target::kIntegralBits >= kIntegralBits)
-  constexpr Target To() const {
-    return Target::template FromFixed<kFractionalBits>(RawValue());
-  }
-  template <typename Target>
-    requires(Target::kIntegralBits < kIntegralBits)
   constexpr Target To() const {
     return Target::template FromFixed<kFractionalBits>(RawValue());
   }
