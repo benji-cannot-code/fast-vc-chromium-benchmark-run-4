5ae5f32649d1f3696bf33ab94537b948d08f58de
Kenichi Ishibashi <bashi@chromium.org>
2024-09-11 09:34:36 +0000

    Fix TlsStreamAttempt::SetTcpHandshakeCompletionCallback()
    When StreamAttempt::Start() returns ERR_IO_PENDING, the AttemptManager
    starts a timer for slow attempt detection and set a callback that is
    called when TCP handshake is completed. In some tests, there could
    be cases where a TlsStreamAttempt::Start() returns ERR_IO_PENDING but
    the attempt has already completed TCP handshake, resulted in hitting
    CHECK() failure. This CL adds IsTcpHandshakeCompleted() method to fix
    the problem. AttemptManager only sets a callback when the method
    returns true.
    
    Bug: 346835898
    Change-Id: Iaeab30e303a106ee94ea9b486d41fe50b3aae508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5838784
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353828}

--- net/socket/tls_stream_attempt.h
+++ net/socket/tls_stream_attempt.h
@@ -69,6 +69,8 @@ class NET_EXPORT_PRIVATE TlsStreamAttempt final : public StreamAttempt {
   // `this` has already completed the TCP handshake.
   void SetTcpHandshakeCompletionCallback(CompletionOnceCallback callback);
 
+  bool IsTcpHandshakeCompleted() { return tcp_handshake_completed_; }
+
   bool IsTlsHandshakeStarted() { return tls_handshake_started_; }
 
  private:
@@ -101,6 +103,7 @@ class NET_EXPORT_PRIVATE TlsStreamAttempt final : public StreamAttempt {
   std::unique_ptr<TcpStreamAttempt> nested_attempt_;
   CompletionOnceCallback tcp_handshake_completion_callback_;
 
+  bool tcp_handshake_completed_ = false;
   bool tls_handshake_started_ = false;
   base::OneShotTimer tls_handshake_timeout_timer_;
   std::unique_ptr<SSLClientSocket> ssl_socket_;
