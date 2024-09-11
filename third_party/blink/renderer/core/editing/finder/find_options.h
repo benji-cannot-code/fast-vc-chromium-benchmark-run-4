ed7fdba15b89c78476283d1a2f66e902270cbee4
Kent Tamura <tkent@chromium.org>
2024-09-11 09:12:32 +0000

    FindRuby: Add RubySupported flag to FindOptions
    and set it for the find-in-page feature. It is not set for window.find()
    and document.execCommand('find', ...).
    
    This CL has no behavior changes.
    
    Bug: 40755728
    Change-Id: I0409fa1d08c30ca70666f984b360c3c66a343dd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849751
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353791}

--- third_party/blink/renderer/core/editing/finder/find_options.h
+++ third_party/blink/renderer/core/editing/finder/find_options.h
@@ -77,6 +77,12 @@ class FindOptions {
     return *this;
   }
 
+  bool IsRubySupported() const { return ruby_supported_; }
+  constexpr FindOptions& SetRubySupported(bool flag) {
+    ruby_supported_ = flag;
+    return *this;
+  }
+
  private:
   bool case_insensitive_ : 1 = false;
   bool backwards_ : 1 = false;
@@ -84,6 +90,7 @@ class FindOptions {
   bool starting_in_selection_ : 1 = false;
   bool whole_word_ : 1 = false;
   bool find_api_call_ : 1 = false;
+  bool ruby_supported_ : 1 = false;
 };
 
 }  // namespace blink
