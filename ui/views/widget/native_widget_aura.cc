15f32b36b22cd8fdffbb11f9e97d9087737000eb
Allen Bauer <kylixrd@chromium.org>
2024-09-11 16:45:28 +0000

    Add more hardening between NativeWidgetAura and Widget.
    Change-Id: Ic01bd6c5ddd7a5fb2a3f385efe4d3c3fe7752721
    Bug: 359667456
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854140
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1353997}

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -1095,8 +1095,10 @@ void NativeWidgetAura::OnPaint(const ui::PaintContext& context) {
 void NativeWidgetAura::OnDeviceScaleFactorChanged(
     float old_device_scale_factor,
     float new_device_scale_factor) {
-  GetWidget()->DeviceScaleFactorChanged(old_device_scale_factor,
-                                        new_device_scale_factor);
+  if (Widget* widget = GetWidget()) {
+    widget->DeviceScaleFactorChanged(old_device_scale_factor,
+                                     new_device_scale_factor);
+  }
 }
 
 void NativeWidgetAura::OnWindowDestroying(aura::Window* window) {
