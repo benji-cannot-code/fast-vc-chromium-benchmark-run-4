747fe50f3317fb4fd4825e945c88f3afa98500aa
Clark DuVall <cduvall@chromium.org>
2024-09-11 22:47:56 +0000

    Add SizeInTokens API that takes mojom::Input instead of string
    This allows for more accurately getting the size of the text that will
    be processed.
    
    Bug: 365922096
    Change-Id: Iddb50d2e46c2ddf6db1cf98bd36c50bbd6c2e252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855311
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1354237}

--- services/on_device_model/fake/fake_chrome_ml_api.cc
+++ services/on_device_model/fake/fake_chrome_ml_api.cc
@@ -179,9 +179,16 @@ bool SessionExecuteModel(ChromeMLSession session,
   return true;
 }
 
-void SessionSizeInTokens(ChromeMLSession session,
-                         const std::string& text,
-                         const ChromeMLSizeInTokensFn& fn) {
+void SessionSizeInTokensInputPiece(ChromeMLSession session,
+                                   ChromeMLModel model,
+                                   const ml::InputPiece* input,
+                                   size_t input_size,
+                                   const ChromeMLSizeInTokensFn& fn) {
+  std::string text;
+  for (size_t i = 0; i < input_size; i++) {
+    // SAFETY: `input_size` describes how big `input` is.
+    text += UNSAFE_BUFFERS(PieceToString(input[i]));
+  }
   fn(text.size());
 }
 
@@ -233,7 +240,7 @@ const ChromeMLAPI g_api = {
 
     .SessionCreateModel = &SessionCreateModel,
     .SessionExecuteModel = &SessionExecuteModel,
-    .SessionSizeInTokens = &SessionSizeInTokens,
+    .SessionSizeInTokensInputPiece = &SessionSizeInTokensInputPiece,
     .SessionScore = &SessionScore,
     .CreateSession = &CreateSession,
     .CloneSession = &CloneSession,
