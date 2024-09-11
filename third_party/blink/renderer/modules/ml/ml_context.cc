6eb6433a1205cce940a87b56ad3a428d5c065b55
Ningxin Hu <ningxin.hu@intel.com>
2024-09-11 06:25:13 +0000

    WebNN: Add IDL and mojo definitions for `scatterND` operator
    The `scatterND` operator is proposed by WebML WG [1] for improving
    performance of passing partial MLBuffer/MLTensor between transformers
    decoder iterations for key-value reuse.
    
    This CL also implements data type limits, inputs validation and adds
    validation tests for `scatterND` operator.
    
    [1]: https://github.com/webmachinelearning/webnn/issues/375#issuecomment-2292466613
    
    Bug: 363677531
    Change-Id: Ib68db63c7b51b99f9976b1eb3c06f8b7f0de9f97
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829103
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1353781}

--- third_party/blink/renderer/modules/ml/ml_context.cc
+++ third_party/blink/renderer/modules/ml/ml_context.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_power_preference.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_prelu_support_limits.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_quantize_dequantize_linear_support_limits.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_ml_scatter_support_limits.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_single_input_support_limits.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_support_limits.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_ml_tensor_descriptor.h"
@@ -791,6 +792,17 @@ const MLOpSupportLimits* MLContext::opSupportLimits(ScriptState* script_state) {
   reshape->setOutput(
       SupportedDataTypesToSupportLimits(data_type_limits.reshape_input));
   op_support_limits->setReshape(reshape);
+
+  MLScatterSupportLimits* scatter_nd = MLScatterSupportLimits::Create();
+  scatter_nd->setInput(
+      SupportedDataTypesToSupportLimits(data_type_limits.scatter_nd_input));
+  scatter_nd->setIndices(
+      SupportedDataTypesToSupportLimits(data_type_limits.scatter_nd_indices));
+  scatter_nd->setUpdates(
+      SupportedDataTypesToSupportLimits(data_type_limits.scatter_nd_input));
+  scatter_nd->setOutput(
+      SupportedDataTypesToSupportLimits(data_type_limits.scatter_nd_input));
+  op_support_limits->setScatterND(scatter_nd);
 
   MLSingleInputSupportLimits* sigmoid = MLSingleInputSupportLimits::Create();
   sigmoid->setInput(
