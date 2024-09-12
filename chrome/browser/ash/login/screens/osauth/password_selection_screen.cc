2222cf05b6e55750409927b4f7a40f03a1456f51
Hardik Goyal <hardikgoyal@google.com>
2024-09-12 09:36:55 +0000

    Change Update to UpdateOrSet
    The rationale for this change is that we now add recovery authfactor
    on as soon as the user accepts T&C. So we are finding that users are
    getting in a situation where they only have Recovery AuthFactor setup.
    Ideally that is fine, and in a truly passwordless world that is the
    expected scenario. Therefore when we go through something like that,
    we either update or set.
    
    BUG=b:328912970
    
    Change-Id: I4f36528656c5c54cc62eb913e9dd40f0d6200eef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757067
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354430}

--- chrome/browser/ash/login/screens/osauth/password_selection_screen.cc
+++ chrome/browser/ash/login/screens/osauth/password_selection_screen.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_features.h"
 #include "base/check.h"
 #include "base/check_is_test.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
@@ -160,11 +162,18 @@ void PasswordSelectionScreen::ProcessOptions() {
       exit_callback_.Run(Result::NOT_APPLICABLE);
       return;
     case WizardContext::AuthChangeFlow::kRecovery:
-      CHECK(auth_factors_config_.HasConfiguredFactor(
-          cryptohome::AuthFactorType::kPassword))
-          << "User need to have a password that should be updated";
-      if (auth::IsLocalPassword(*auth_factors_config_.FindFactorByType(
-              cryptohome::AuthFactorType::kPassword))) {
+      if (!auth_factors_config_.HasConfiguredFactor(
+              cryptohome::AuthFactorType::kPassword)) {
+        // Here if the user does not have any password configured then we can
+        // let them set their password according to the same condition as OOBE.
+        // What is to note here is that after recovery, we already know the GAIA
+        // password so both GAIA and local password factor are possible.
+        LOG(ERROR) << "User does not have password configured when "
+                      "performing recovery unexpectedly.";
+        base::debug::DumpWithoutCrashing();
+        break;
+      } else if (auth::IsLocalPassword(*auth_factors_config_.FindFactorByType(
+                     cryptohome::AuthFactorType::kPassword))) {
         exit_callback_.Run(Result::LOCAL_PASSWORD_FORCED);
         return;
       } else {
@@ -214,7 +223,6 @@ void PasswordSelectionScreen::ProcessOptions() {
     exit_callback_.Run(Result::GAIA_PASSWORD_ENTERPRISE);
     return;
   }
-
   if (!has_online_password_) {
     LOG(WARNING)
         << "User does not have online password, forcing local password";
@@ -222,6 +230,7 @@ void PasswordSelectionScreen::ProcessOptions() {
     exit_callback_.Run(Result::LOCAL_PASSWORD_FORCED);
     return;
   }
+
   EstablishKnowledgeFactorGuard(
       base::BindOnce(&PasswordSelectionScreen::ShowPasswordChoice,
                      weak_ptr_factory_.GetWeakPtr()));
