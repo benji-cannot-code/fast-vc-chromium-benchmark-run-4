b59b17755becc2d63791a7b5696855b46fc571a4
Duncan Mercer <mercerd@google.com>
2024-09-12 17:40:56 +0000

    [lensoverlay] Only update custom live blur when necessary
    Starts the page updates disabled so that we don't waste resources on
    blurring while the blur isn't visible. Also disables taking screenshots
    while the tab is in the background.
    
    Bug: 364902039
    Change-Id: Ia2f75893e7f126b22d4bfe4bfb8f26da7431dfa7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855903
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354680}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -1326,6 +1326,19 @@ void LensOverlayController::AddBoundingBoxesToInitializationData(
       std::move(significant_region_boxes);
 }
 
+void LensOverlayController::SetLiveBlur(bool enabled) {
+  if (!lens_overlay_blur_layer_delegate_) {
+    return;
+  }
+
+  if (enabled) {
+    lens_overlay_blur_layer_delegate_->StartBackgroundImageCapture();
+    return;
+  }
+
+  lens_overlay_blur_layer_delegate_->StopBackgroundImageCapture();
+}
+
 void LensOverlayController::ShowOverlay() {
   // Listen to WebContents events
   tab_contents_observer_ = std::make_unique<UnderlyingWebContentsObserver>(
@@ -1342,6 +1355,9 @@ void LensOverlayController::ShowOverlay() {
     CHECK(!overlay_view_->GetVisible());
 
     overlay_view_->SetVisible(true);
+
+    // Restart the live blur since the view is visible again.
+    SetLiveBlur(true);
 
     // The overlay needs to be focused on show to immediately begin
     // receiving key events.
@@ -1373,6 +1389,7 @@ void LensOverlayController::ShowOverlay() {
 
 void LensOverlayController::BackgroundUI() {
   overlay_view_->SetVisible(false);
+  SetLiveBlur(false);
   HidePreselectionBubble();
   CloseSearchBubble();
   // Re-enable mouse and keyboard events to the tab contents web view.
@@ -1632,6 +1649,13 @@ void LensOverlayController::OnFullscreenStateChanged() {
 
 void LensOverlayController::OnViewBoundsChanged(views::View* observed_view) {
   CHECK(observed_view == overlay_view_->parent());
+
+  // We now want to start the live blur since the screenshot has resized to
+  // allow the blur to peek through.
+  if (IsOverlayShowing()) {
+    SetLiveBlur(true);
+  }
+
   gfx::Rect bounds = observed_view->GetLocalBounds();
   overlay_view_->SetBoundsRect(bounds);
 }
