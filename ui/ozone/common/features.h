e863cec601a97d8808f8145801522ff9fc5a1777
Robert Mader <robert.mader@collabora.com>
2024-09-12 19:11:15 +0000

    ozone/wayland: Add support for xdg-toplevel-drag
    Rebased and slighty updated version of https://chromium-review.googlesource.com/c/chromium/src/+/4400967
    
    This implements a new protocol landed in wayland-protocols 1.34 that can
    be found at
    https://gitlab.freedesktop.org/wayland/wayland-protocols/-/blob/main/staging/xdg-toplevel-drag/xdg-toplevel-drag-v1.xml
    
    It is intended to be a drop-in replacement for extended-drag-unstable-v1
    with some features removed that were never implemented in Exo/Chromium.
    
    If both the `extended-drag` and `xdg-toplevel-drag` protocols are
    supported by the compositor - i.e. in Exo - the former is kept being
    used by default. The later can be enabled via the
    `WaylandXdgToplevelDrag` feature.
    
    Original author: David Redondo <kde@david-redondo.de>
    
    Bug: b:315000518
    Bug: b:324170129
    Change-Id: If251ac9944ec4395f2d8630b7c4ac74ca56a662d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5088752
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Antonio Gomes <tonikitoo@igalia.com>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1354718}

--- ui/ozone/common/features.h
+++ ui/ozone/common/features.h
@@ -19,6 +19,7 @@ BASE_DECLARE_FEATURE(kUseDynamicCursorSize);
 bool IsWaylandSurfaceSubmissionInPixelCoordinatesEnabled();
 bool IsWaylandOverlayDelegationEnabled();
 bool IsWaylandFractionalScaleV1Enabled();
+bool IsWaylandXdgToplevelDragEnabled();
 bool IsPrettyPrintDrmModesetConfigLogsEnabled();
 bool IsUseDynamicCursorSizeEnabled();
 
