2206db40ca472dae703584119a252b95fadd929f
Tom Anderson <thomasanderson@chromium.org>
2024-09-12 23:05:13 +0000

    [Metrics] Move Linux metrics to linux directory
    R=rkaplow
    
    Bug: None
    Change-Id: Ib51f7af94d20c8c43a156dc672f254928e080d9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854153
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354877}

--- tools/metrics/histograms/validate_format.py
+++ tools/metrics/histograms/validate_format.py
@@ -19,7 +19,7 @@ import xml_utils
 # split across multiple files.
 _NAMESPACES_IN_MULTIPLE_FILES = [
     'ash', 'autocomplete', 'chromeos', 'fcminvalidations', 'graphics', 'launch',
-    'linux', 'usereducation'
+    'usereducation'
 ]
 
 
