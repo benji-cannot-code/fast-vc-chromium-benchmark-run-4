0d8c4ee247b5e36567db318f39c06b2b99a5f120
Kent Tamura <tkent@chromium.org>
2024-09-12 08:17:51 +0000

    Spanify WTF::VisitCharacters()
    Make the functor argument a base::span<T>, and add Span8() and Span16()
    to AtomicString.
    This CL should have no user-visible behavior changes.
    
    Bug: 351564777
    Change-Id: I0bece4c74a485b2e9a18b0089882c4a72c57310a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856962
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354407}

--- third_party/blink/renderer/platform/wtf/text/character_visitor.h
+++ third_party/blink/renderer/platform/wtf/text/character_visitor.h
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-// Visits the characters of a WTF::String, StringView or compatible type.
+// Visits the characters of a WTF::String, WTF::AtomicString, StringView or
+// compatible type.
 //
 // Intended to be used with a generic lambda or other functor overloaded to
 // handle either LChar* or UChar*. Reduces code duplication in many cases.
-// The functor should return the same type in both branches.
+// The functor should receive a base::span<T> and should return the same type
+// in both branches.
 //
 // Callers should ensure that characters exist (i.e. the string is not null)
 // first.
@@ -22,10 +24,10 @@ namespace WTF {
 //   if (string.IsNull())
 //     return false;
 //
-//   return WTF::VisitCharacters(string, [&](const auto* chars, unsigned len) {
+//   return WTF::VisitCharacters(string, [&](auto chars) {
 //     bool contains_space = false;
-//     for (unsigned i = 0; i < len; i++)
-//       contains_space |= IsASCIISpace(chars[i]);
+//     for (auto ch : chars)
+//       contains_space |= IsASCIISpace(ch);
 //     return contains_space;
 //   });
 //
@@ -34,8 +36,7 @@ namespace WTF {
 template <typename StringType, typename Functor>
 decltype(auto) VisitCharacters(const StringType& string,
                                const Functor& functor) {
-  return string.Is8Bit() ? functor(string.Characters8(), string.length())
-                         : functor(string.Characters16(), string.length());
+  return string.Is8Bit() ? functor(string.Span8()) : functor(string.Span16());
 }
 
 }  // namespace WTF
