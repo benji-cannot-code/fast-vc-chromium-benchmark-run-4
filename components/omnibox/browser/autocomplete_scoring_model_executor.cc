d3e0baa393bfd9af9426c656cbc2887d07ba8a81
Khalid Peer <khalidpeer@chromium.org>
2024-09-12 19:54:25 +0000

    [omnibox][ml] Patch intermittent crash during ML scoring.
    This CL patches a crash that occurs for a very small minority of users
    when executing ML scoring logic in the Omnibox.
    
    In addition to fixing this bug, this CL also introduces a new UMA
    histogram that will help us track the proportion of inputs sent to the
    ML scoring model that trigger this kind of fatal error.
    
    Low-Coverage-Reason: HARD_TO_TEST The exact mechanism by which this edge-case is triggered is currently not well understood.
    Bug: 348395027
    Change-Id: Ic7a88708f91170e9c0df8cd16327db75d3de489e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840693
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Michael Crouse <mcrouse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354766}

--- components/omnibox/browser/autocomplete_scoring_model_executor.cc
+++ components/omnibox/browser/autocomplete_scoring_model_executor.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/check_op.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notreached.h"
 #include "third_party/tflite/src/tensorflow/lite/c/common.h"
 #include "third_party/tflite_support/src/tensorflow_lite_support/cc/task/core/task_utils.h"
@@ -19,7 +20,13 @@ AutocompleteScoringModelExecutor::~AutocompleteScoringModelExecutor() = default;
 bool AutocompleteScoringModelExecutor::Preprocess(
     const std::vector<TfLiteTensor*>& input_tensors,
     ModelInput input) {
-  DCHECK_EQ(input.size(), input_tensors.size());
+  const bool valid_input_size = input.size() == input_tensors.size();
+  base::UmaHistogramBoolean(
+      "Omnibox.URLScoringModelExecuted.Preprocess.ValidInputSize",
+      valid_input_size);
+  if (!valid_input_size) {
+    return false;
+  }
   DCHECK_EQ(kTfLiteFloat32, input_tensors[0]->type);
   for (size_t i = 0; i < input.size(); ++i) {
     std::vector<float> data = {input[i]};
