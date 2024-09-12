ece95aff18bf430dc8c691458d3862efc1e5232a
Zhenyao Mo <zmo@chromium.org>
2024-09-12 20:52:29 +0000

    [cc-slimming] Audit and fortify logic around all_tiles_done flags.
    All cases that could cause a Tiling's all_tiles_done flag to be
    updated are:
    
    1) CopyTilingsAndPropertiesFromPendingTwin
    2) UpdateTilingsToCurrentRasterSourceForCommit
    3) UpdateTilingsToCurrentRasterSourceForActivation
    4) Invalidate
    
    In this CL, we recompute TilingSet's flag in all the above
    four cases, so we don't need the awkward OnAllTilesDoneCleared()
    wiring. Besides, previously we had multiple cases where
    we set Tiling's all_tiles_done to false but without calling
    OnAllTilesDoneCleared() and forwarding it to TilingSet. Now
    it's all taken care of.
    
    TEST=cc_unittests
    
    Bug: 338977414
    Change-Id: I6df0c27d01b03a6c1f3f1457ce3e12dd22c880c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855942
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Auto-Submit: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Zhenyao Mo <zmo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354804}

--- cc/tiles/picture_layer_tiling.cc
+++ cc/tiles/picture_layer_tiling.cc
@@ -158,6 +158,9 @@ void PictureLayerTiling::TakeTilesAndPropertiesFrom(
     pending_twin->tiles_.erase(pending_iter);
   }
 
+  if (all_tiles_done_ && !pending_twin->all_tiles_done_) {
+    all_tiles_done_ = false;
+  }
   ComputeTilePriorityRects(
       pending_twin->current_visible_rect_in_layer_space_,
       pending_twin->current_skewport_rect_in_layer_space_,
@@ -165,11 +168,6 @@ void PictureLayerTiling::TakeTilesAndPropertiesFrom(
       pending_twin->current_eventually_rect_in_layer_space_,
       pending_twin->current_content_to_screen_scale_ * contents_scale_key(),
       pending_twin->current_occlusion_in_layer_space_);
-
-  if (all_tiles_done_ && !pending_twin->all_tiles_done_) {
-    all_tiles_done_ = false;
-    client_->OnAllTilesDoneCleared();
-  }
 
   DCHECK(pending_twin->tiles_.empty());
   pending_twin->all_tiles_done_ = true;
@@ -473,7 +471,7 @@ void PictureLayerTiling::ComputeTilePriorityRects(
   // we reiterate the tiles for rasterization.
   if (occlusion_in_layer_space.HasOcclusion() ||
       current_occlusion_in_layer_space_.HasOcclusion()) {
-    set_all_tiles_done(false);
+    all_tiles_done_ = false;
   }
 
   TileMemoryLimitPolicy memory_limit_policy =
@@ -621,8 +619,10 @@ void PictureLayerTiling::SetLiveTilesRect(
       }
     }
   }
-
   live_tiles_rect_ = new_live_tiles_rect;
+  if (tiles_.size() == 0) {
+    all_tiles_done_ = true;
+  }
   VerifyTiles();
 }
 
