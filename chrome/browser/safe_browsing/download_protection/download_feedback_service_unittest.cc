6096b66296632858b2740227aa605d552ada6d63
Daniel Rubery <drubery@chromium.org>
2024-09-12 19:31:16 +0000

    Rename StealDangerousDownload to CopyDownload
    All callers were setting `delete_file_afterwards` to false, so make that
    the only behavior and update tests/documentation accordingly.
    
    Bug: 364924720
    Change-Id: I5183ec742eff18bab1e4929f3068d956f4c06a35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855570
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354746}

--- chrome/browser/safe_browsing/download_protection/download_feedback_service_unittest.cc
+++ chrome/browser/safe_browsing/download_protection/download_feedback_service_unittest.cc
@@ -230,10 +230,9 @@ TEST_F(DownloadFeedbackServiceTest, SingleFeedbackComplete) {
   EXPECT_CALL(item, GetDangerType())
       .WillRepeatedly(Return(download::DOWNLOAD_DANGER_TYPE_UNCOMMON_CONTENT));
   EXPECT_CALL(item, GetReceivedBytes()).WillRepeatedly(Return(1000));
-  EXPECT_CALL(item,
-              StealDangerousDownload(false /*delete_file_after_feedback*/, _))
+  EXPECT_CALL(item, CopyDownload(_))
       .WillOnce([&download_discarded_callback](
-                    bool _, download::DownloadItem::AcquireFileCallback arg) {
+                    download::DownloadItem::AcquireFileCallback arg) {
         download_discarded_callback = std::move(arg);
       });
 
@@ -276,9 +275,9 @@ TEST_F(DownloadFeedbackServiceTest, MultiplePendingFeedbackComplete) {
         .WillRepeatedly(
             Return(download::DOWNLOAD_DANGER_TYPE_UNCOMMON_CONTENT));
     EXPECT_CALL(item[i], GetReceivedBytes()).WillRepeatedly(Return(1000));
-    EXPECT_CALL(item[i], StealDangerousDownload(false, _))
-        .WillOnce([&download_discarded_callback, i](
-                      bool _, download::DownloadItem::AcquireFileCallback arg) {
+    EXPECT_CALL(item[i], CopyDownload(_))
+        .WillOnce([&download_discarded_callback,
+                   i](download::DownloadItem::AcquireFileCallback arg) {
           download_discarded_callback[i] = std::move(arg);
         });
     DownloadFeedbackService::MaybeStorePingsForDownload(
@@ -349,9 +348,9 @@ TEST_F(DownloadFeedbackServiceTest, DISABLED_MultiFeedbackWithIncomplete) {
         .WillRepeatedly(
             Return(download::DOWNLOAD_DANGER_TYPE_UNCOMMON_CONTENT));
     EXPECT_CALL(item[i], GetReceivedBytes()).WillRepeatedly(Return(1000));
-    EXPECT_CALL(item[i], StealDangerousDownload(true, _))
-        .WillOnce([&download_discarded_callback, i](
-                      bool _, download::DownloadItem::AcquireFileCallback arg) {
+    EXPECT_CALL(item[i], CopyDownload(_))
+        .WillOnce([&download_discarded_callback,
+                   i](download::DownloadItem::AcquireFileCallback arg) {
           download_discarded_callback[i] = std::move(arg);
         });
     DownloadFeedbackService::MaybeStorePingsForDownload(
--- content/public/test/fake_download_item.cc
+++ content/public/test/fake_download_item.cc
@@ -287,8 +287,7 @@ void FakeDownloadItem::ValidateInsecureDownload() {
   NOTREACHED_IN_MIGRATION();
 }
 
-void FakeDownloadItem::StealDangerousDownload(bool delete_file_afterward,
-                                              AcquireFileCallback callback) {
+void FakeDownloadItem::CopyDownload(AcquireFileCallback callback) {
   NOTREACHED_IN_MIGRATION();
   std::move(callback).Run(base::FilePath());
 }
