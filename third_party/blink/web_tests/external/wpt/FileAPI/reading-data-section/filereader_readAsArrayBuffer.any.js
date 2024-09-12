55d49949cbb621a44a474d77896f90a60fb0863c
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-09-12 06:30:19 +0000

    Import wpt@9326ce54c0cebf4fca58c2b51ab263421bdea44e
    https://github.com/web-platform-tests/wpt/compare/90d460ad2...9326ce54c
    
    Using wpt-import in Chromium ac74a0ce1b0055fd778b25acad11a18a9e3352ca.
    With Chromium commits locally applied on WPT:
      0dfa0868aa "WebNN: Flatten adjacent dimensions for GEMM > 4D for [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    futhark@chromium.org, andruud@chromium.org:
      external/wpt/css/css-conditional
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Validate-Test-Flakiness: skip
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I99eb2943307e971f273b28c58732575d16fb3539
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856699
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1354375}

--- third_party/blink/web_tests/external/wpt/FileAPI/reading-data-section/filereader_readAsArrayBuffer.any.js
+++ third_party/blink/web_tests/external/wpt/FileAPI/reading-data-section/filereader_readAsArrayBuffer.any.js
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       reader.onload = this.step_func(function(evt) {
         assert_equals(reader.result.byteLength, 4, "The byteLength is 4");
         assert_true(reader.result instanceof ArrayBuffer, "The result is instanceof ArrayBuffer");
+        assert_array_equals(new Uint8Array(reader.result), [84, 69, 83, 84]);
         assert_equals(reader.readyState, reader.DONE);
         this.done();
       });
