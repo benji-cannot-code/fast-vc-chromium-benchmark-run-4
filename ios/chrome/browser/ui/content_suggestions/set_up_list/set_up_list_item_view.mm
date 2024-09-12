dc378cc8378b734f6aef638bbc008541b2ab5126
Daniel White <danieltwhite@google.com>
2024-09-12 20:03:03 +0000

    [iOS] Deprecate didTraitCollectionChange in SetupListItemView
    Since didTraitCollectionChange is deprecated in iOS 17,
    SetupListItemView was updated to use the new alternative system,
    registerForTraitChanges.
    
    Fixed: 349392680
    Change-Id: I4023189fa95517b2d90a102c63b64af9e0fbe264
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854092
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354772}

--- ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_view.mm
+++ ios/chrome/browser/ui/content_suggestions/set_up_list/set_up_list_item_view.mm
@@ -152,6 +152,12 @@ struct ViewConfig {
           kTextSpacing,
       };
     }
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+          @[ UITraitPreferredContentSizeCategory.self ]);
+      [self registerForTraitChanges:traits
+                         withAction:@selector(hideDescriptionOnTraitChange)];
+    }
   }
   return self;
 }
@@ -171,17 +177,19 @@ struct ViewConfig {
 
 #pragma mark - UITraitEnvironment
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
   if (previousTraitCollection.preferredContentSizeCategory !=
       self.traitCollection.preferredContentSizeCategory) {
-    _description.hidden = self.traitCollection.preferredContentSizeCategory >
-                          UIContentSizeCategoryExtraExtraLarge;
-    // Force a layout since the size of text components may have changed.
-    [self setNeedsLayout];
-    [self layoutIfNeeded];
+    [self hideDescriptionOnTraitChange];
   }
 }
+#endif
 
 #pragma mark - Public methods
 
@@ -418,6 +426,16 @@ struct ViewConfig {
     case SetUpListItemType::kFollow:
       return set_up_list::kFollowItemID;
   }
+}
+
+// Hides `_description` if the font size category is larger than
+// extra-extra-large.
+- (void)hideDescriptionOnTraitChange {
+  _description.hidden = self.traitCollection.preferredContentSizeCategory >
+                        UIContentSizeCategoryExtraExtraLarge;
+  // Force a layout since the size of text components may have changed.
+  [self setNeedsLayout];
+  [self layoutIfNeeded];
 }
 
 #pragma mark - Private methods (animation helpers)
