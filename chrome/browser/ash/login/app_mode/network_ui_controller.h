83217e01cc5e283b50d423cf99a94c7fa784843d
Edman Anjos <edmanp@google.com>
2024-09-12 14:56:47 +0000

    kiosk: Remove NetworkMonitor forward declaration
    The declaration in an anonymous namespace in network_ui_controller.h is
    unused and not necessary. The NetworkMonitor that's actually used is
    declared below within the NetworkUiController.
    
    Bug: none
    Change-Id: I2f0efa9bcbff83de40ac7a4f8ea779c8a3834659
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853949
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Commit-Queue: Edman Anjos <edman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354541}

--- chrome/browser/ash/login/app_mode/network_ui_controller.h
+++ chrome/browser/ash/login/app_mode/network_ui_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
 #include "base/time/time.h"
@@ -22,10 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/ash/login/network_state_informer.h"
 
 class Profile;
-
-namespace {
-class NetworkMonitor;
-}
 
 namespace ash {
 
