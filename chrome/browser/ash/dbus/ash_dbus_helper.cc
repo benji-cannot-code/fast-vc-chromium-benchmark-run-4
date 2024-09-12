eb7c1750f466fe7925cc877f9a63a718de2b7416
Mihai Sardarescu <msarda@chromium.org>
2024-09-12 15:23:00 +0000

    [ash][google_apis] Load MissiveClient after features are initialized.
    This CL ensures that DBUS client MissiveClient is only loaded after
    the feature list was initialized. This will ensure that the APIKey
    is also initialized only after the feature list was initialized.
    
    Bug: 366181521
    
    Bug: None
    Change-Id: I9685d094ffc637529f3fc791f21bfcd9e73f0806
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853877
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354556}

--- chrome/browser/ash/dbus/ash_dbus_helper.cc
+++ chrome/browser/ash/dbus/ash_dbus_helper.cc
@@ -183,7 +183,6 @@ void InitializeDBus() {
   InitializeDBusClient<LorgnetteManagerClient>(bus);
   InitializeDBusClient<chromeos::MachineLearningClient>(bus);
   InitializeDBusClient<MediaAnalyticsClient>(bus);
-  InitializeDBusClient<chromeos::MissiveClient>(bus);
   InitializeDBusClient<OobeConfigurationClient>(bus);
   InitializeDBusClient<OsInstallClient>(bus);
   InitializeDBusClient<PatchPanelClient>(bus);
@@ -230,6 +229,11 @@ void InitializeFeatureListDependentDBus() {
   using chromeos::InitializeDBusClient;
 
   dbus::Bus* bus = DBusThreadManager::Get()->GetSystemBus();
+
+  // MissiveClient depends on APIKey which needs to be accessed after the
+  // feature list is initialized.
+  InitializeDBusClient<chromeos::MissiveClient>(bus);
+
   if (floss::features::IsFlossEnabled()) {
     InitializeDBusClient<floss::FlossDBusManager>(bus);
     if (bus) {
