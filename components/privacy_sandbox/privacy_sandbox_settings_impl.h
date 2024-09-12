b8eafffb069db8d38e66a0dae3a513ce8f91bef9
Xiaochen Zhou <xiaochenzh@chromium.org>
2024-09-12 15:35:23 +0000

    [Privacy Sandbox] Add test for where both pre-installed and downloaded attestation files are available and of the same version.
    1. Update test util functions to support installing pre-installed
    attestation list.
    
    2. Add browser test for where both pre-installed and downloaded attestation files are available and of the same version. Component
    installer prefers the pre-installed component when they have the same
    version.
    
    Change-Id: Id66ec41f77d2bb34bfddae75266d8dd8a04ad883
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5835651
    Reviewed-by: Liam Brady <lbrady@google.com>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Xiaochen Zhou <xiaochenzh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354566}

--- components/privacy_sandbox/privacy_sandbox_settings_impl.h
+++ components/privacy_sandbox/privacy_sandbox_settings_impl.h
@@ -125,6 +125,9 @@ class PrivacySandboxSettingsImpl : public PrivacySandboxSettings,
   bool AreRelatedWebsiteSetsEnabled() const override;
 
  private:
+  // TODO(crbug.com/366168654): Browser tests should not reach into the private
+  // method or states of this class. Consider exposing the required functions
+  // via a test helper class or test only functions.
   friend class PrivacySandboxSettingsTest;
   friend class PrivacySandboxAttestations;
   friend class PrivacySandboxAttestationsTestBase;
@@ -135,6 +138,9 @@ class PrivacySandboxSettingsImpl : public PrivacySandboxSettings,
                            SentinelFilePreventsSubsequentParsings);
   FRIEND_TEST_ALL_PREFIXES(PrivacySandboxAttestationsBrowserTest,
                            DifferentHistogramAfterAttestationsFileCheck);
+  FRIEND_TEST_ALL_PREFIXES(
+      PrivacySandboxAttestationPreInstallInteractionWithDownloadTest,
+      BothPreinstalledAndDownloadedAttestationsAvailable);
   FRIEND_TEST_ALL_PREFIXES(PrivacySandboxSettingsTest, FledgeJoiningAllowed);
   FRIEND_TEST_ALL_PREFIXES(PrivacySandboxSettingsTest, NonEtldPlusOneBlocked);
   FRIEND_TEST_ALL_PREFIXES(PrivacySandboxSettingsTest,
