63de32977a4d6f90e75962d5c0e1e82485a05529
Timofey Chudakov <tchudakov@google.com>
2024-09-12 16:35:41 +0000

    [Android] Create ERROR_STATE_INFO property.
    This property is responsible for a new error handling mechanism
    in the plus address creation bottom sheet, it's used only when
    the PlusAddressAndroidErrorStatesEnabled feature flag is enabled.
    
    Bug: 364181540
    Change-Id: Ic4dfd0640c9ea4c270d783cdf9525ea05ad4c908
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854743
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354622}

--- chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationMediator.java
+++ chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationMediator.java
@@ -8,6 +8,7 @@ package org.chromium.chrome.browser.ui.plus_addresses;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.CANCEL_BUTTON_VISIBLE;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.CONFIRM_BUTTON_ENABLED;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.CONFIRM_BUTTON_VISIBLE;
+import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.ERROR_STATE_INFO;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.LEGACY_ERROR_REPORTING_INSTRUCTION_VISIBLE;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.LOADING_INDICATOR_VISIBLE;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.PROPOSED_PLUS_ADDRESS;
@@ -55,7 +56,6 @@ import org.chromium.url.GURL;
     private final TabModel mTabModel;
     private final PlusAddressCreationViewBridge mBridge;
     private PropertyModel mModel;
-    private PlusAddressCreationBottomSheetContent mBottomSheetContent;
 
     /**
      * Creates the mediator.
@@ -92,15 +92,6 @@ import org.chromium.url.GURL;
         mModel = model;
     }
 
-    /**
-     * TODO: crbug.com/364181540 - Remove dependency on the View component.
-     *
-     * @param bottomSheetContent - The UI surface this mediator manages.
-     */
-    void setBottomSheetContent(PlusAddressCreationBottomSheetContent bottomSheetContent) {
-        mBottomSheetContent = bottomSheetContent;
-    }
-
     /** Requests to show the bottom sheet content. */
     void requestShowContent() {
         mModel.set(VISIBLE, true);
@@ -121,8 +112,9 @@ import org.chromium.url.GURL;
             }
             mModel.set(LEGACY_ERROR_REPORTING_INSTRUCTION_VISIBLE, true);
             mModel.set(LOADING_INDICATOR_VISIBLE, false);
+            return;
         }
-        mBottomSheetContent.showError(errorStateInfo);
+        mModel.set(ERROR_STATE_INFO, errorStateInfo);
     }
 
     void hideRefreshButton() {
