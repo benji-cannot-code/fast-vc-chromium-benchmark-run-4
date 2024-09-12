78ac5438875960e16c1e60881a8a97142f6bcae8
weidongliu <weidongliu@chromium.org>
2024-09-12 23:33:01 +0000

    Set more appropriate flex behavior keys
    This is part of crrev.com/c/5828724. After we merge GetHeightForWidth
    and CalculatePreferredSize, we can let Boxlayout reduce some layout
    calculations at the right time. Then some inaccurate flex behavior
    settings will cause it to stretch incorrectly.
    
    Bug: 363806071
    Change-Id: I8fe340ff92efcb4c1ee9c59db692e102e152c485
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854305
    Reviewed-by: Andre Le <leandre@chromium.org>
    Commit-Queue: Weidong Liu <weidongliu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354895}

--- ash/system/mahi/refresh_banner_view.cc
+++ ash/system/mahi/refresh_banner_view.cc
@@ -122,6 +122,7 @@ RefreshBannerView::RefreshBannerView(MahiUiController* ui_controller)
                   .SetHorizontalAlignment(gfx::ALIGN_LEFT)
                   .SetProperty(views::kFlexBehaviorKey,
                                views::FlexSpecification(
+                                   views::LayoutOrientation::kHorizontal,
                                    views::MinimumFlexSizeRule::kScaleToZero,
                                    views::MaximumFlexSizeRule::kUnbounded))
                   .SetProperty(views::kMarginsKey, kTitleLabelMargin))
