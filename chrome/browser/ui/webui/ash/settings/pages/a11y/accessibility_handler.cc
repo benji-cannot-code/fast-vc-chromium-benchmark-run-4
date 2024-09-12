4274aeb1d13aa494f63fb4ac2b7aeb0abc458e0d
Georg Neis <neis@chromium.org>
2024-09-12 02:05:19 +0000

    ash: Remove obsolete uses of ash::TryLaunchOsUrlHandler
    Lacros cannot be enabled anymore, so
    crosapi::browser_util::IsAshWebBrowserEnabled() is always true in
    ash builds.
    
    Bug: b:365741912
    Change-Id: I84111a31e9c620ad477f0f51997d2a0b3b483288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849929
    Reviewed-by: Yuta Hijikata <ythjkt@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354316}

--- chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_handler.cc
+++ chrome/browser/ui/webui/ash/settings/pages/a11y/accessibility_handler.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/accessibility/accessibility_manager.h"
 #include "chrome/browser/ash/accessibility/dictation.h"
 #include "chrome/browser/ash/crosapi/browser_util.h"
-#include "chrome/browser/ash/url_handler/os_url_handler.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/profiles/profile.h"
@@ -187,22 +186,8 @@ void AccessibilityHandler::OpenExtensionOptionsPage(const char extension_id[]) {
     return;
   }
 
-  // If Lacros is the only browser, we need to open the options page in an Ash
-  // app window instead of a regular Ash browser window so that the user can't
-  // navigate in Ash. We do so using the OsUrlHandler SWA. Exception: Kiosk mode
-  // doesn't support SWA but already hide the navigation bar.
-  bool open_with_os_url_handler =
-      !crosapi::browser_util::IsAshWebBrowserEnabled() &&
-      !chromeos::IsKioskSession();
-  if (open_with_os_url_handler) {
-    DCHECK(extensions::OptionsPageInfo::ShouldOpenInTab(extension));
-    GURL url = extensions::OptionsPageInfo::GetOptionsPage(extension);
-    bool launched = ash::TryLaunchOsUrlHandler(url);
-    DCHECK(launched);
-  } else {
-    extensions::ExtensionTabUtil::OpenOptionsPage(
-        extension, chrome::FindBrowserWithTab(web_ui()->GetWebContents()));
-  }
+  extensions::ExtensionTabUtil::OpenOptionsPage(
+      extension, chrome::FindBrowserWithTab(web_ui()->GetWebContents()));
 }
 
 void AccessibilityHandler::MaybeAddSodaInstallerObserver() {
