4d590080801e3dd89102e6309c041c409565b828
Jenny Zhang <jennyz@chromium.org>
2024-09-12 17:12:41 +0000

    [Desktop Android CRX] Enable more tests for extensions_unittests
    We are experimenting with desktop-android configurations for Chrome.
    
    Key changes in this CL:
    * Enables tests located under //extensions/renderer, with the exception
    of some API tests due to the related APIs not being enabled yet.
    * Modifies `extensions::binding::GetPlatformString()` to return
    `desktop_android` in desktop-android build.
    
    This CL should not affect production behavior as the
    desktop-android configuration remains highly experimental.
    
    Cq-Include-Trybots: luci.chromium.try:android-desktop-arm64-compile-rel,android-desktop-x64-compile-rel
    
    Bug: 356905053
    Change-Id: Ibe6117c9d26b4fea59e1aaf514b799280c79c258
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5851285
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354656}

--- extensions/renderer/bindings/api_binding_util.cc
+++ extensions/renderer/bindings/api_binding_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/observer_list.h"
 #include "base/supports_user_data.h"
+#include "build/android_buildflags.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
 #include "extensions/renderer/bindings/get_per_context_data.h"
@@ -141,6 +142,8 @@ std::string GetPlatformString() {
   return "win";
 #elif BUILDFLAG(IS_FUCHSIA)
   return "fuchsia";
+#elif BUILDFLAG(IS_DESKTOP_ANDROID)
+  return "desktop_android";
 #else
   NOTREACHED_IN_MIGRATION();
   return std::string();
