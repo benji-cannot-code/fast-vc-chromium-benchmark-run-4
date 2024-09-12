892a683c0d584f08e3adf07cd4859a820c07fbd6
Eriko Kurimoto <elkurin@chromium.org>
2024-09-12 01:41:43 +0000

    ash: Move system_token_cert_db_* to //c/b/ash/cert_provisioning
    Bug: b:357063604
    Change-Id: Ia30187a80a42468c13f6b7f5a123e0b8c85a635c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837590
    Reviewed-by: Michael Ershov <miersh@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354305}

--- chrome/browser/ash/certs/system_token_cert_db_initializer.cc
+++ chrome/browser/ash/certs/system_token_cert_db_initializer.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/system_token_cert_db_initializer.h"
+#include "chrome/browser/ash/certs/system_token_cert_db_initializer.h"
 
 #include <pk11pub.h>
 
@@ -219,8 +219,9 @@ void SystemTokenCertDBInitializer::OnGetTpmNonsensitiveStatus(
 void SystemTokenCertDBInitializer::MaybeStartInitializingDatabase() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
-  if (started_initializing_)
+  if (started_initializing_) {
     return;
+  }
   started_initializing_ = true;
   VLOG(1)
       << "SystemTokenCertDBInitializer: TPM is ready, loading system token.";
--- chrome/browser/ash/certs/system_token_cert_db_initializer.h
+++ chrome/browser/ash/certs/system_token_cert_db_initializer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
-#define CHROME_BROWSER_ASH_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
+#ifndef CHROME_BROWSER_ASH_CERTS_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
+#define CHROME_BROWSER_ASH_CERTS_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
 
 #include <memory>
 
@@ -108,4 +108,4 @@ class SystemTokenCertDBInitializer
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
+#endif  // CHROME_BROWSER_ASH_CERTS_SYSTEM_TOKEN_CERT_DB_INITIALIZER_H_
