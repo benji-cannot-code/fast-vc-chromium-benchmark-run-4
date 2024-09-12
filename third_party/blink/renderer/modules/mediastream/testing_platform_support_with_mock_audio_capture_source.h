78a0a37787e6c17f878c6347cba787cd1a72df63
David Benjamin <davidben@chromium.org>
2024-09-12 16:03:23 +0000

    Properly define MockAudioCapturerSource in a header
    Putting it in an anonymous namespace in a header means that every file
    that includes the header defines a distinct type. This is tripping
    -fsanitize=vptr errors like the following:
    https://ci.chromium.org/ui/labs/p/chromium/inv/build-8737084650289924497/test/ninja%3A%2F%2Fthird_party%2Fblink%2Frenderer%2Fcontroller%3Ablink_unittests%2FRTCPeerConnectionHandlerTest.SpeedLimitUmaListenerCreatedWhenStreamAdded/variant/a1ab97ef5f768b3f
    
    Bug: 40248746
    Change-Id: Iae43b8a04d7b15fdcea05c401f1277b44bd7da36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857019
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Auto-Submit: David Benjamin <davidben@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354594}

--- third_party/blink/renderer/modules/mediastream/testing_platform_support_with_mock_audio_capture_source.h
+++ third_party/blink/renderer/modules/mediastream/testing_platform_support_with_mock_audio_capture_source.h
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace blink {
 
-namespace {
-
 class MockAudioCapturerSource : public media::AudioCapturerSource {
  public:
   MockAudioCapturerSource() = default;
@@ -35,8 +33,6 @@ class MockAudioCapturerSource : public media::AudioCapturerSource {
  protected:
   ~MockAudioCapturerSource() override = default;
 };
-
-}  // namespace
 
 // Test Platform implementation that overrides the known methods needed
 // by the tests, including creation of AudioCapturerSource instances.
