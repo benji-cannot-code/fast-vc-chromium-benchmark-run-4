592d1fc3546839bfe52c232a2449dd2ddd26bba4
Keren Zhu <kerenzhu@chromium.org>
2024-09-12 17:31:16 +0000

    page load metrics: Rename NewTabPagePageLoadMetricsObserver
    Rename NewTabPagePageLoadMetricsObserver to
    NewTabPageInitiatedPageLoadMetricsObserver as it only observes
    navigations that are initiated from the New Tab Page.
    
    This change clarifies the role of the observer and the histogram
    descriptions to make them more accurate.
    
    This prepares for follow up changes to add load metrics for the NTP
    itself.
    
    Bug: 365784990
    Change-Id: I79be43a7a9b797dcd62d5967eb03e3f57ef5a754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854883
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Huanpo Lin <robertlin@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Ken Okada <kenoss@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354671}

--- chrome/browser/page_load_metrics/observers/new_tab_page_initiated_page_load_metrics_observer_browsertest.cc
+++ chrome/browser/page_load_metrics/observers/new_tab_page_initiated_page_load_metrics_observer_browsertest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/page_load_metrics/observers/new_tab_page_page_load_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/new_tab_page_initiated_page_load_metrics_observer.h"
 
 #include "base/test/metrics/histogram_tester.h"
 #include "build/build_config.h"
--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/page_load_metrics/observers/loading_predictor_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/local_network_requests_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/multi_tab_loading_page_load_metrics_observer.h"
-#include "chrome/browser/page_load_metrics/observers/new_tab_page_page_load_metrics_observer.h"
+#include "chrome/browser/page_load_metrics/observers/new_tab_page_initiated_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/omnibox_suggestion_used_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/optimization_guide_page_load_metrics_observer.h"
 #include "chrome/browser/page_load_metrics/observers/page_anchors_metrics_observer.h"
@@ -235,7 +235,8 @@ void PageLoadMetricsEmbedder::RegisterEmbedderObservers(
         std::make_unique<TabStripPageLoadMetricsObserver>(web_contents()));
     tracker->AddObserver(std::make_unique<PreviewPageLoadMetricsObserver>());
     tracker->AddObserver(std::make_unique<BookmarkBarMetricsObserver>());
-    tracker->AddObserver(std::make_unique<NewTabPagePageLoadMetricsObserver>());
+    tracker->AddObserver(
+        std::make_unique<NewTabPageInitiatedPageLoadMetricsObserver>());
     tracker->AddObserver(
         std::make_unique<ThirdPartyCookieDeprecationMetricsObserver>(
             web_contents()->GetBrowserContext()));
