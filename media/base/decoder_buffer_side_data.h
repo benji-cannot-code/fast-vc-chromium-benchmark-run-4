ddcc0727e23fcc68b68907caa1fa45191980ab26
Dale Curtis <dalecurtis@chromium.org>
2024-09-12 22:12:52 +0000

    Move DecoderBuffer::discard_padding into DecoderBufferSideData.
    Only a handful of buffers during a playback will have discard
    padding set; we shouldn't carry the memory cost for all buffers.
    
    This is an 4 to 8 byte reduction in size for DecoderBuffer
    depending on 32/64-bit arch. It's 88 bytes total on 64-bit arch.
    
    Bug: 365814210
    Change-Id: I6e1247fe947c8d63027d2daa40098ae7aa01c3f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857451
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354845}

--- media/base/decoder_buffer_side_data.h
+++ media/base/decoder_buffer_side_data.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <vector>
 
+#include "base/time/time.h"
 #include "media/base/media_export.h"
 
 namespace media {
@@ -31,6 +32,13 @@ struct MEDIA_EXPORT DecoderBufferSideData {
   // Secure buffer handle corresponding to the decrypted contents of the
   // associated DecoderBuffer. A non-zero value indicates this was set.
   uint64_t secure_handle = 0;
+
+  // Duration of (audio) samples from the beginning and end of this frame
+  // which should be discarded after decoding. A value of kInfiniteDuration
+  // for the first value indicates the entire frame should be discarded; the
+  // second value must be base::TimeDelta() in this case.
+  using DiscardPadding = std::pair<base::TimeDelta, base::TimeDelta>;
+  DiscardPadding discard_padding;
 };
 
 }  // namespace media
