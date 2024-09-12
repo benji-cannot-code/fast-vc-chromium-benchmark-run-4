b399cba80eb7f60dfd61779a9d4711c84570c6fd
Timofey Chudakov <tchudakov@google.com>
2024-09-12 16:33:18 +0000

    [Android] Create NORMAL_STATE_INFO property.
    All UI strings are currently passed to the Android frontend in
    the PlusAddressCreationNormalStateInfo class. This CL makes this
    parameter a property of the model, which is always set.
    
    Bug: 364181540
    Change-Id: I2e3cfb067c43dc40110a65aae0956d006a3fade1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854699
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354618}

--- chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationViewBinder.java
+++ chrome/browser/ui/android/plus_addresses/java/src/org/chromium/chrome/browser/ui/plus_addresses/PlusAddressCreationViewBinder.java
@@ -9,6 +9,7 @@ import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationP
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.CONFIRM_BUTTON_ENABLED;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.CONFIRM_BUTTON_VISIBLE;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.DELEGATE;
+import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.NORMAL_STATE_INFO;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.PROPOSED_PLUS_ADDRESS;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.REFRESH_ICON_ENABLED;
 import static org.chromium.chrome.browser.ui.plus_addresses.PlusAddressCreationProperties.REFRESH_ICON_VISIBLE;
@@ -25,7 +26,9 @@ class PlusAddressCreationViewBinder {
             PropertyModel model,
             PlusAddressCreationBottomSheetContent view,
             PropertyKey propertyKey) {
-        if (propertyKey == DELEGATE) {
+        if (propertyKey == NORMAL_STATE_INFO) {
+            view.setNormalStateInfo(model.get(NORMAL_STATE_INFO));
+        } else if (propertyKey == DELEGATE) {
             view.setDelegate(model.get(DELEGATE));
         } else if (propertyKey == SHOW_ONBOARDING_NOTICE) {
             // This property doesn't require any binding logic.
