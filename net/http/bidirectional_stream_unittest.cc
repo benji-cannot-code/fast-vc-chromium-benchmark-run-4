3793d6f04227f6f25804e5f4c912e3a311c338ce
Kenichi Ishibashi <bashi@chromium.org>
2024-09-12 12:27:16 +0000

    Disable HEv3 for BidirectionalStreamTest
    We don't support bidirectional streams when HEv3 is enabled yet.
    Explicitly disable the feature as a preparation for enabling the
    feature for other tests.
    
    Bug: 346835898
    Change-Id: I317967c3bf2d823a0685474bf77756c867177020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849511
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354486}

--- net/http/bidirectional_stream_unittest.cc
+++ net/http/bidirectional_stream_unittest.cc
@@ -20,11 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/run_loop.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/time/time.h"
 #include "base/timer/mock_timer.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "net/base/completion_once_callback.h"
+#include "net/base/features.h"
 #include "net/base/load_timing_info.h"
 #include "net/base/load_timing_info_test_util.h"
 #include "net/base/net_errors.h"
@@ -410,6 +412,11 @@ class BidirectionalStreamTest : public TestWithTaskEnvironment {
       : default_url_(kDefaultUrl),
         host_port_pair_(HostPortPair::FromURL(default_url_)),
         ssl_data_(SSLSocketDataProvider(ASYNC, OK)) {
+    // Explicitly disable HappyEyeballsV3 because it doesn't support
+    // bidirectional streams.
+    // TODO(crbug.com/346835898): Support bidirectional streams in
+    // HappyEyeballsV3.
+    feature_list_.InitAndDisableFeature(features::kHappyEyeballsV3);
     ssl_data_.next_proto = kProtoHTTP2;
     ssl_data_.ssl_info.cert =
         ImportCertFromFile(GetTestCertsDirectory(), "ok_cert.pem");
@@ -459,6 +466,7 @@ class BidirectionalStreamTest : public TestWithTaskEnvironment {
  private:
   SSLSocketDataProvider ssl_data_;
   base::WeakPtr<SpdySession> session_;
+  base::test::ScopedFeatureList feature_list_;
 };
 
 TEST_F(BidirectionalStreamTest, CreateInsecureStream) {
