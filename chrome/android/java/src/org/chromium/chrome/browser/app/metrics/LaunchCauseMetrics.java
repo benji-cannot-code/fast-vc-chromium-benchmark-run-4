6b30d2e5af45ee607f1a47f3f21ace69e26122c9
Michael Thiessen <mthiesse@chromium.org>
2024-09-12 17:50:19 +0000

    Fix Activity leak in LaunchCauseMetrics
    The anonymous inner class used for the static ActivityStateListener was
    holding onto a reference to the LaunchCauseMetrics class, which was in
    turn holding onto a reference to the Activity.
    
    Instead, construct the listener in a static context.
    
    Bug: 365163101
    Change-Id: Iee13c19f7b2ef13f1c87d1ba4e5f2e87d0344d2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857269
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354687}

--- chrome/android/java/src/org/chromium/chrome/browser/app/metrics/LaunchCauseMetrics.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/metrics/LaunchCauseMetrics.java
@@ -50,6 +50,27 @@ public abstract class LaunchCauseMetrics
 
     private static ApplicationStatus.ActivityStateListener sAppActivityListener;
 
+    static {
+        doStaticInit();
+    }
+
+    private static void doStaticInit() {
+        sAppActivityListener =
+                new ApplicationStatus.ActivityStateListener() {
+                    @Override
+                    public void onActivityStateChange(Activity activity, int newState) {
+                        if (newState == ActivityState.RESUMED) sLastResumedActivity = activity;
+                        if (newState == ActivityState.DESTROYED) {
+                            if (activity == sLastResumedActivity) sLastResumedActivity = null;
+                        }
+                    }
+                };
+        ApplicationStatus.registerStateListenerForAllActivities(sAppActivityListener);
+        if (ApplicationStatus.getStateForApplication() == ApplicationState.HAS_RUNNING_ACTIVITIES) {
+            sLastResumedActivity = ApplicationStatus.getLastTrackedFocusedActivity();
+        }
+    }
+
     // State pertaining to the current launch, reset when Chrome is backgrounded,
     // and after computing LaunchCause.
     private static class PerLaunchState {
@@ -124,23 +145,6 @@ public abstract class LaunchCauseMetrics
      */
     public LaunchCauseMetrics(final Activity activity) {
         mActivity = activity;
-        if (sAppActivityListener == null) {
-            sAppActivityListener =
-                    new ApplicationStatus.ActivityStateListener() {
-                        @Override
-                        public void onActivityStateChange(Activity activity, int newState) {
-                            if (newState == ActivityState.RESUMED) sLastResumedActivity = activity;
-                            if (newState == ActivityState.DESTROYED) {
-                                if (activity == sLastResumedActivity) sLastResumedActivity = null;
-                            }
-                        }
-                    };
-            ApplicationStatus.registerStateListenerForAllActivities(sAppActivityListener);
-            if (ApplicationStatus.getStateForApplication()
-                    == ApplicationState.HAS_RUNNING_ACTIVITIES) {
-                sLastResumedActivity = ApplicationStatus.getLastTrackedFocusedActivity();
-            }
-        }
         ApplicationStatus.registerApplicationStateListener(this);
         ApplicationStatus.registerStateListenerForActivity(this, activity);
     }
@@ -296,6 +300,7 @@ public abstract class LaunchCauseMetrics
             sAppActivityListener = null;
         }
         sLastResumedActivity = null;
+        doStaticInit();
     }
 
     @CheckDiscard("")
