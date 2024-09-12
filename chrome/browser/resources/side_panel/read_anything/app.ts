1ce696a9ffd62e21296108328160e3c7d905521d
Lauren Winston <lwinston@google.com>
2024-09-12 17:47:26 +0000

    [Read Aloud] Fix previous highlight with word highlighting.
    Video: https://drive.google.com/file/d/1hyT7cvO8Y5mkDX_9KwyQ2RS9ajjrGHbP/view?usp=sharing&resourcekey=0-Wt5cuFfc5aTHoIlQlf6kQw
    
    Note that in video, the previous highlight color was adjusted
    to red to make it easier to validate that the highlight was being
    changed, but the color changes were removed before uploading this CL.
    
    Fixed: 351887259
    Change-Id: Iec2f619d3debc5a093f54bb6370756b78e4237b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856111
    Commit-Queue: Lauren Winston <lwinston@google.com>
    Reviewed-by: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354686}

--- chrome/browser/resources/side_panel/read_anything/app.ts
+++ chrome/browser/resources/side_panel/read_anything/app.ts
@@ -1972,9 +1972,10 @@ export class AppElement extends AppElementBase {
   // TODO(b/301131238): Verify all edge cases.
   private highlightCurrentWordOrPhrase_(highlightPhrases: boolean) {
     // Word highlights can be called quite frequently which can create some
-    // misordering, so just make sure we've cleared the previous word highlight
-    // before showing the next one.
-    this.removeCurrentHighlight();
+    // misordering, so just make sure we've cleared the prior current word
+    // highlight before showing the next one.
+    this.resetCurrentHighlight();
+    this.resetPreviousHighlight_();
     const index = this.wordBoundaryState.speechUtteranceStartIndex +
         this.wordBoundaryState.previouslySpokenIndex;
     const highlightNodes =
@@ -2281,6 +2282,8 @@ export class AppElement extends AppElementBase {
     this.resetPreviousHighlight_();
   }
 
+  // Resets formatting on the current highlight, including previous highlight
+  // formatting.
   private removeCurrentHighlight() {
     // The most recent highlight could have been spread across multiple segments
     // so clear the formatting for all of the segments.
@@ -2290,6 +2293,16 @@ export class AppElement extends AppElementBase {
         lastElement.classList.remove(currentReadHighlightClass);
       }
     }
+  }
+
+  // Resets the current highlight. Does not change how this element will
+  // be considered for previous highlighting.
+  private resetCurrentHighlight() {
+    const elements =
+        this.shadowRoot?.querySelectorAll('.' + currentReadHighlightClass);
+    elements?.forEach(element => {
+      element.classList.remove(currentReadHighlightClass);
+    });
   }
 
   private resetPreviousHighlight_() {
