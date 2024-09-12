592a1a2087370b40eae5b571bc8da4b69ea0fb9c
Michael Ershov <miersh@google.com>
2024-09-12 13:34:25 +0000

    Kcer: Move chromeos/components/kcer to ash/components/kcer
    Kcer is now only used in Ash and moving it will allow depending
    on Ash specific directories and components.
    
    Bug: b:351347146
    Test: Compilation
    Change-Id: I1d137211b40043a5c8d9d09b327c5a5b46b4b021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850938
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Gavin Williams <gavinwill@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Michael Ershov <miersh@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1354504}

--- chrome/browser/ui/webui/certificate_manager/client_cert_sources.cc
+++ chrome/browser/ui/webui/certificate_manager/client_cert_sources.cc
@@ -58,6 +58,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
+#include "ash/components/kcer/kcer.h"
+#include "ash/components/kcer/kcer_histograms.h"
 #include "ash/constants/ash_features.h"
 #include "chrome/browser/ash/kcer/kcer_factory_ash.h"
 #include "chrome/browser/ash/net/client_cert_store_ash.h"
@@ -65,8 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/net/nss_service.h"
 #include "chrome/browser/net/nss_service_factory.h"
-#include "chromeos/components/kcer/kcer.h"
-#include "chromeos/components/kcer/kcer_histograms.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/user_manager/user.h"
 #include "net/cert/nss_cert_database.h"
