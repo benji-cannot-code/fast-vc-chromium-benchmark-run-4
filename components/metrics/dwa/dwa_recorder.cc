c4a099c95cf90e19111b97c99545a8a247070eb6
Zainab Rizvi <rizvis@google.com>
2024-09-13 17:18:59 +0000

    [DWA] Add skeleton DWA mojo and implementation
    Next step is implementing the `DwaRecorderClientInterface`.
    
    Design: go/dwa-lipa
    
    Bug: 359556688
    Change-Id: Ia12e9db542cf4a138d8084189227dc7f68a36b01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5783031
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355282}

---
+++ components/metrics/dwa/dwa_recorder.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/metrics/dwa/dwa_recorder.h"
+
+#include "base/no_destructor.h"
+
+namespace metrics::dwa {
+
+DwaRecorder::DwaRecorder() = default;
+
+DwaRecorder::~DwaRecorder() = default;
+
+DwaRecorder* DwaRecorder::Get() {
+  static base::NoDestructor<DwaRecorder> recorder;
+  return recorder.get();
+}
+
+// static
+void DwaRecorder::AddEntry(metrics::dwa::mojom::DwaEntryPtr entry) {
+  // TODO(b/359556688): To be implemented, `static` to be re-evaluated.
+  return;
+}
+
+}  // namespace metrics::dwa
