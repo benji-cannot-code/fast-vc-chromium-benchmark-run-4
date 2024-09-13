4d9d2bdbdc61b932fba353495e1837fae4118442
Anders Hartvoll Ruud <andruud@chromium.org>
2024-09-13 12:33:12 +0000

    Don't consume into block in ConsumeAttributeReference
    The streaming parser does not allow using Consume[IncludingWhitespace]
    into a block. Fixed by peeking before consuming.
    
    Fixes crash (DCHECK) for invalid values like "attr(())".
    
    Change-Id: Ic1d26b21d5e82be8375f768b9626cec9e3da375a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860123
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Munira Tursunova <moonira@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355134}

--- third_party/blink/renderer/core/css/parser/css_variable_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_variable_parser.cc
@@ -179,10 +179,10 @@ static bool ConsumeAttributeReference(CSSParserTokenStream& stream,
   CSSParserTokenStream::BlockGuard guard(stream);
   stream.ConsumeWhitespace();
   // Parse <attr-name>.
-  auto token = stream.ConsumeIncludingWhitespace();
-  if (token.GetType() != kIdentToken) {
+  if (stream.Peek().GetType() != kIdentToken) {
     return false;
   }
+  stream.ConsumeIncludingWhitespace();  // kIdentToken
   if (stream.AtEnd()) {
     // attr = attr(<attr-name>) is allowed, so return true.
     return true;
@@ -190,7 +190,7 @@ static bool ConsumeAttributeReference(CSSParserTokenStream& stream,
 
   if (stream.Peek().GetType() == kIdentToken) {
     // Parse <attr-type>.
-    token = stream.ConsumeIncludingWhitespace();
+    CSSParserToken token = stream.ConsumeIncludingWhitespace();
     if (!CSSAttrType::Parse(token.Value()).IsValid()) {
       return false;
     }
