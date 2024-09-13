f57f76cccaddbe8e07e8fe3b96b0397f33b66a77
Jood <jood@google.com>
2024-09-13 08:46:20 +0000

    [iOS] Add missing a11y strings for APD with AccountMenu
    Fixed: 365736264
    Change-Id: Ic42fa7c9ee56f03a625657b7dd1c4ea8fa4d2032
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850482
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Auto-Submit: Jood Hajeer <jood@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355059}

--- ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
+++ ios/chrome/browser/ntp/ui_bundled/new_tab_page_mediator.mm
@@ -384,7 +384,9 @@ const char kFeedLearnMoreURL[] = "https://support.google.com/chrome/"
   BOOL primaryIdentityHasError =
       identity && _syncService->GetUserActionableError() !=
                       syncer::SyncService::UserActionableError::kNone;
-  [self.headerConsumer updateADPBadgeWithErrorFound:primaryIdentityHasError];
+  [self.headerConsumer updateADPBadgeWithErrorFound:primaryIdentityHasError
+                                               name:identity.userFullName
+                                              email:identity.userEmail];
 }
 
 @end
