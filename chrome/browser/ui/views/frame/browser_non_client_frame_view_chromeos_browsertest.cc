3935ee02f792039147d706a6fb97200e547384a5
Eric Sum <esum@google.com>
2024-09-13 16:04:24 +0000

    Reland "Don't invalidate BrowserView layout if it's not a webapp."
    This reverts commit 0e48cb85c89e5cf4f7691e63e0582d8614faa3a5.
    
    Reason for revert: Reland with fix for lacros. A BrowserView layout is
    required on lacros for completely separate reasons. Since lacros is
    being deprecated anyways, just exclude the test expectation on
    lacros builds.
    
    Cq-Include-Trybots: luci.chrome.try:linux-lacros-chrome
    
    Original change's description:
    > Revert "Don't invalidate BrowserView layout if it's not a webapp."
    >
    > This reverts commit 4c6d3dd788eac90f39a963305882661086d3f32c.
    >
    > Reason for revert: Caused the failure in https://ci.chromium.org/ui/p/chrome/builders/ci/linux-lacros-chrome/3422/overview
    >
    > Original change's description:
    > > Don't invalidate BrowserView layout if it's not a webapp.
    > >
    > > Item 5 in the doc in the bug. This is targeting overview mode in
    > > ChromeOS.
    > >
    > > Before:
    > > https://screenshot.googleplex.com/4uRdAjaNKL8WfNC.png
    > > After:
    > > https://screenshot.googleplex.com/6EJPrkx9Nr4SmgJ.png
    > >
    > > Bug: b:362835104
    > > Change-Id: Iae2bddfde6410d0cb63f5c5a3cfd111ff34c47ba
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5852444
    > > Reviewed-by: Scott Violet <sky@chromium.org>
    > > Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    > > Commit-Queue: Eric Sum <esum@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1354587}
    >
    > Bug: b:362835104
    > Change-Id: Ic04f5c589bba296d3f4455625254dbfc97d4b5c1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858497
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Owners-Override: Chan Li <chanli@chromium.org>
    > Auto-Submit: Chan Li <chanli@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1354803}
    
    Bug: b:362835104
    Change-Id: I6d0030aa08f3e293226c7cc3bf9c31b665d86906
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858181
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Eric Sum <esum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355213}

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_chromeos_browsertest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/test/run_until.h"
+#include "base/test/scoped_feature_list.h"
 #include "base/test/test_future.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
@@ -151,8 +152,16 @@ bool WaitForPaintAsActive(bool expected, views::FrameCaptionButton* button) {
 
 }  // namespace
 
-using BrowserNonClientFrameViewChromeOSTest =
-    TopChromeMdParamTest<ChromeOSBrowserUITest>;
+class BrowserNonClientFrameViewChromeOSTest
+    : public TopChromeMdParamTest<ChromeOSBrowserUITest> {
+ protected:
+  BrowserNonClientFrameViewChromeOSTest()
+      : scoped_features_(
+            chromeos::features::kOverviewSessionInitOptimizations) {}
+
+  base::test::ScopedFeatureList scoped_features_;
+};
+
 using BrowserNonClientFrameViewChromeOSTestNoWebUiTabStrip =
     WebUiTabStripOverrideTest<false, BrowserNonClientFrameViewChromeOSTest>;
 using BrowserNonClientFrameViewChromeOSTestWithWebUiTabStrip =
@@ -1000,6 +1009,16 @@ IN_PROC_BROWSER_TEST_P(BrowserNonClientFrameViewChromeOSTest,
 
   EnterOverviewMode();
   EXPECT_FALSE(frame_view->caption_button_container()->GetVisible());
+#if !BUILDFLAG(IS_CHROMEOS_LACROS)
+  // b/362835104: Browser windows that are not web apps do not require a layout
+  // after the header is made invisible. This is an optimization with no visual
+  // impact. Depends on `kOverviewSessionInitOptimizations` being enabled.
+  //
+  // Lacros builds require a layout of the browser view for unrelated reasons.
+  ASSERT_FALSE(browser_view->GetIsWebAppType());
+  EXPECT_FALSE(browser_view->needs_layout());
+#endif
+
   ExitOverviewMode();
 
   // Caption buttons are not supported when using the WebUI tab strip.
