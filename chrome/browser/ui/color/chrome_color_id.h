11937b8b1ade34159dbbb8ca17393b3f946029f7
Chase Hartsell <chazzy@chromium.org>
2024-09-13 19:01:44 +0000

    Update open in new tab button and icon color in Compare table
    http://go/scrcast/NjQ4MTU1ODg4MDkxMTM2MHw5Mzc1ZTAzYi05Nw
    
    Bug: 366211810
    Change-Id: I25be54b63483288827b00bd228657ec18f1ae8dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5848335
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355318}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -408,6 +408,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorProductSpecificationsHorizontalCarouselScrollbarThumb) \
   E_CPONLY(kColorProductSpecificationsIcon) \
   E_CPONLY(kColorProductSpecificationsIconButtonBackground) \
+  E_CPONLY(kColorProductSpecificationsIconButtonHoveredBackground) \
   E_CPONLY(kColorProductSpecificationsLink) \
   E_CPONLY(kColorProductSpecificationsPageBackground) \
   E_CPONLY(kColorProductSpecificationsPrimaryTitle) \
