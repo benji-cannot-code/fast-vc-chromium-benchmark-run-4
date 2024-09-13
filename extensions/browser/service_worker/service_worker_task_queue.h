f25581fd8e6d4ea77f7a5b8aa3a6f5a1b320242d
Andrii Natiahlyi <natiahlyi@google.com>
2024-09-13 10:12:10 +0000

    Revert "[Extensions] Change start worker metric emit logic."
    This reverts commit 9ee66911621c01aa89c995291dd7f096455bfed6.
    
    Reason for revert: culprit CL for https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/46127/overview
    
    Original change's description:
    > [Extensions] Change start worker metric emit logic.
    >
    > Before this change blink::ServiceWorkerStatusCode::kErrorAbort was
    > always counted as a failure when starting a worker.
    >
    > After this change, it is only counted if it does not occur during
    > browser context shutdown.
    >
    > This helps whittle away at expected failures so that we can identify
    > failures that actually affect the reliability of extension service
    > workers.
    >
    > Bug: 346732739
    > Change-Id: Id984db754027faa5a8a7a5b53efc51fd0b78fc9d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824577
    > Reviewed-by: David Bertoni <dbertoni@chromium.org>
    > Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1354767}
    
    Bug: 346732739
    Change-Id: I2bd22bed11acbbdde42d5ec73c9ba249a09b14d9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860121
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Andrii Natiahlyi <natiahlyi@google.com>
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1355089}

--- extensions/browser/service_worker/service_worker_task_queue.h
+++ extensions/browser/service_worker/service_worker_task_queue.h
@@ -404,9 +404,6 @@ class ServiceWorkerTaskQueue
                           base::Time start_time,
                           blink::ServiceWorkerStatusCode status_code);
 
-  bool IsStartWorkerFailureUnexpected(
-      blink::ServiceWorkerStatusCode status_code);
-
   // Records that the extension with |extension_id| and |version| successfully
   // registered a Service Worker.
   void SetRegisteredServiceWorkerInfo(const ExtensionId& extension_id,
