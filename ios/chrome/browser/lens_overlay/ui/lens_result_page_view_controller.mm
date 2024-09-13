86c4b31a8b44d61f0621169d9677fc1e2c64ca32
Christian Xu <christianxu@google.com>
2024-09-13 16:59:07 +0000

    [iOS][LO] Move progress bar inside of omnibox container
    Move the progress bar inside of the omnibox container to match the
    mocks.
    
    Bug: 365984651
    Change-Id: Iae8d259d9725bb3bf91ab5ee13a5fb429b0cfce0
    Low-Coverage-Reason: HARD_TO_TEST egtest will be added in later CL
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857168
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355272}

--- ios/chrome/browser/lens_overlay/ui/lens_result_page_view_controller.mm
+++ ios/chrome/browser/lens_overlay/ui/lens_result_page_view_controller.mm
@@ -145,6 +145,7 @@ const CGFloat kProgressBarFull = 1.0f;
   _omniboxContainer.backgroundColor =
       [UIColor colorNamed:kSecondaryBackgroundColor];
   _omniboxContainer.layer.cornerRadius = kOmniboxContainerCornerRadius;
+  _omniboxContainer.clipsToBounds = YES;
   [_omniboxContainer
       setContentHuggingPriority:UILayoutPriorityDefaultLow
                         forAxis:UILayoutConstraintAxisHorizontal];
@@ -195,7 +196,7 @@ const CGFloat kProgressBarFull = 1.0f;
   // Progress bar.
   _progressBar.translatesAutoresizingMaskIntoConstraints = NO;
   _progressBar.hidden = YES;
-  [self.view addSubview:_progressBar];
+  [_omniboxContainer addSubview:_progressBar];
 
   NSLayoutConstraint* omniboxLeadingConstraint =
       [_omniboxContainer.leadingAnchor
@@ -220,12 +221,12 @@ const CGFloat kProgressBarFull = 1.0f;
                        constant:kWebContainerTopPadding],
     [_omniboxPopupContainer.topAnchor
         constraintEqualToAnchor:_horizontalStackView.bottomAnchor],
-    [_progressBar.topAnchor
-        constraintEqualToAnchor:_webViewContainer.topAnchor],
     [_progressBar.leadingAnchor
-        constraintEqualToAnchor:_webViewContainer.leadingAnchor],
+        constraintEqualToAnchor:_omniboxContainer.leadingAnchor],
     [_progressBar.trailingAnchor
-        constraintEqualToAnchor:_webViewContainer.trailingAnchor],
+        constraintEqualToAnchor:_omniboxContainer.trailingAnchor],
+    [_progressBar.bottomAnchor
+        constraintEqualToAnchor:_omniboxContainer.bottomAnchor],
     [_progressBar.heightAnchor constraintEqualToConstant:kProgressBarHeight],
   ]];
   AddSameConstraintsToSides(
