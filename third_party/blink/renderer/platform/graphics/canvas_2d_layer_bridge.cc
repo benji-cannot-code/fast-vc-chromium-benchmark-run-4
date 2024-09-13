ab7271f4c31b8700f14479e89acbea530f8cd50e
Colin Blundell <blundell@chromium.org>
2024-09-13 06:42:01 +0000

    [Blink] Remove final `resource_host_` conditional from C2DLayerBridge
    This conditional is in a method that has CHECK(resource_host_) at its
    entry.
    
    Bug: 40280152
    Change-Id: Idc14bddb6a822fc78c0cba4d988bd2a2a5f22abb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853947
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355019}

--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.cc
@@ -231,10 +231,9 @@ CanvasResourceProvider* Canvas2DLayerBridge::GetOrCreateResourceProvider() {
   hibernation_handler_.Clear();
   DCHECK(!IsHibernating());
 
-  if (resource_host_) {
-    // shouldBeDirectComposited() may have changed.
-    resource_host_->SetNeedsCompositingUpdate();
-  }
+  // shouldBeDirectComposited() may have changed.
+  resource_host_->SetNeedsCompositingUpdate();
+
   return resource_provider;
 }
 
