a7a58f73224c6905c49360d7f5be3afaa4883d4b
Vidhan Jain <vidhanj@google.com>
2024-09-13 14:54:14 +0000

    [iOS] GetForBrowserState -> GetForProfile in plus_addresses
    Fixed: 365531685
    Change-Id: Ie661d7537f6d502d86d0d0da68d014024a685508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860897
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Vidhan Jain <vidhanj@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355175}

--- ios/chrome/browser/plus_addresses/model/plus_address_service_factory.mm
+++ ios/chrome/browser/plus_addresses/model/plus_address_service_factory.mm
@@ -70,7 +70,7 @@ PlusAddressServiceFactory::BuildServiceInstanceFor(
 
   // `groups_manager` can be null in tests.
   GoogleGroupsManager* groups_manager =
-      GoogleGroupsManagerFactory::GetForBrowserState(profile);
+      GoogleGroupsManagerFactory::GetForProfile(profile);
   plus_addresses::PlusAddressService::FeatureEnabledForProfileCheck
       feature_check =
           (groups_manager &&
