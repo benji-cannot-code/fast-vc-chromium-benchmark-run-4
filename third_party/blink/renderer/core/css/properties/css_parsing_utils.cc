9999afeb450136a7a6fb19ecc81235f337cc7924
Anders Hartvoll Ruud <andruud@chromium.org>
2024-09-13 21:57:41 +0000

    Don't consume into block in css_parsing_utils::ConsumeSteps
    Fixes crash for e.g. steps(2, ()).
    
    Bug: 40941162
    Change-Id: I50eeb55cb7b8c837082ad620ba3b732026cf641c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860080
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355443}

--- third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
+++ third_party/blink/renderer/core/css/properties/css_parsing_utils.cc
@@ -328,7 +328,7 @@ CSSValue* ConsumeSteps(CSSParserTokenStream& stream,
     StepsTimingFunction::StepPosition position =
         StepsTimingFunction::StepPosition::END;
     if (ConsumeCommaIncludingWhitespace(stream)) {
-      switch (stream.ConsumeIncludingWhitespace().Id()) {
+      switch (stream.Peek().Id()) {
         case CSSValueID::kStart:
           position = StepsTimingFunction::StepPosition::START;
           break;
@@ -356,6 +356,7 @@ CSSValue* ConsumeSteps(CSSParserTokenStream& stream,
         default:
           return nullptr;
       }
+      stream.ConsumeIncludingWhitespace();  // kIdentToken
     }
 
     if (!stream.AtEnd()) {
