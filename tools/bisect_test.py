cdcea65a1b95460eccd6a36337700d3fac525a61
Kuan Huang <kuanhuang@chromium.org>
2024-09-13 23:03:48 +0000

    Run commands in shell without chrdir to provide better support for customized commands
    We want to use bisect-builds.py to bisect automated test failures using
    third-party test frameworks like pytest and mocha. It's very common for
    third-party web automated tests to use environment variables to set the
    browser executable path. This CL changed the way --command runs under
    the current folder and with a shell, making it easier to set environment
    variables like `-c "BROWSER_EXECUTABLE_PATH=%p pytest"`.
    
    Bug: 352344089
    Change-Id: I7475f025d6adf54930163ec5cf35a50990dd0d15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858467
    Commit-Queue: Kuan Huang <kuanhuang@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355469}

--- tools/bisect_test.py
+++ tools/bisect_test.py
@@ -341,8 +341,9 @@ class ArchiveBuildTest(BisectTestCase):
     build = self.create_build()
     build._launch_revision('temp-dir', 'temp-dir/linux64/chrome', [])
     mock_Popen.assert_called_once_with(
-        ['temp-dir/linux64/chrome', '--user-data-dir=profile'],
-        cwd='temp-dir',
+        'temp-dir/linux64/chrome --user-data-dir=profile',
+        cwd=None,
+        shell=True,
         bufsize=-1,
         stdout=ANY,
         stderr=ANY)
@@ -875,8 +876,9 @@ class LinuxReleaseBuildTest(BisectTestCase):
     self.assertIsInstance(build, bisect_builds.LinuxReleaseBuild)
     build._launch_revision('temp-dir', 'temp-dir/linux64/chrome', [])
     mock_Popen.assert_called_once_with(
-        ['temp-dir/linux64/chrome', '--user-data-dir=profile', '--no-sandbox'],
-        cwd='temp-dir',
+        'temp-dir/linux64/chrome --user-data-dir=profile --no-sandbox',
+        cwd=None,
+        shell=True,
         bufsize=-1,
         stdout=ANY,
         stderr=ANY)
@@ -936,6 +938,7 @@ class IOSReleaseBuildTest(BisectTestCase):
             'canary.ipa'
         ],
              cwd=None,
+             shell=False,
              bufsize=-1,
              stdout=-1,
              stderr=-1),
@@ -944,6 +947,7 @@ class IOSReleaseBuildTest(BisectTestCase):
             'Payload/canary.app/Info.plist'
         ],
              cwd=None,
+             shell=False,
              bufsize=-1,
              stdout=-1,
              stderr=-1)
@@ -967,6 +971,7 @@ class IOSReleaseBuildTest(BisectTestCase):
         'com.google.chrome.ios', 'args1', 'args2'
     ],
                                cwd=None,
+                               shell=False,
                                bufsize=-1,
                                stdout=-1,
                                stderr=-1)
