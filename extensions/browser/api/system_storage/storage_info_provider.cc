f05e63ac7a99cf57546433c5493fc96f3f6d874e
Solomon Kinard <solomonkinard@chromium.org>
2024-09-13 01:28:18 +0000

    Extensions: Add curly braces to reduce cognitive load & range-based loop
    * Use range-based for loop instead. clang-tidymodernize-loop-convert
    * Use emplace_back instead of push_back. clang-tidymodernize-use-emplace
    
    This CL was uploaded by git cl split.
    
    R=ningxin.hu@intel.com
    
    Bug: chromium:346952289
    Change-Id: Ice66ae0e96e2613fc9d622ccc076cdb146bf63e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858259
    Auto-Submit: Solomon Kinard <solomonkinard@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1354941}

--- extensions/browser/api/system_storage/storage_info_provider.cc
+++ extensions/browser/api/system_storage/storage_info_provider.cc
@@ -58,11 +58,9 @@ void StorageInfoProvider::GetAllStoragesIntoInfoList() {
   std::vector<StorageInfo> storage_list =
       StorageMonitor::GetInstance()->GetAllAvailableStorages();
 
-  for (std::vector<StorageInfo>::const_iterator it = storage_list.begin();
-       it != storage_list.end();
-       ++it) {
+  for (const auto& info : storage_list) {
     StorageUnitInfo unit;
-    systeminfo::BuildStorageUnitInfo(*it, &unit);
+    systeminfo::BuildStorageUnitInfo(info, &unit);
     info_.push_back(std::move(unit));
   }
 }
@@ -76,12 +74,11 @@ double StorageInfoProvider::GetStorageFreeSpaceFromTransientIdAsync(
       StorageMonitor::GetInstance()->GetDeviceIdForTransientId(transient_id);
 
   // Lookup the matched storage info by |device_id|.
-  for (std::vector<StorageInfo>::const_iterator it = storage_list.begin();
-       it != storage_list.end();
-       ++it) {
-    if (device_id == it->device_id())
-      return static_cast<double>(
-          base::SysInfo::AmountOfFreeDiskSpace(base::FilePath(it->location())));
+  for (const auto& info : storage_list) {
+    if (device_id == info.device_id()) {
+      return static_cast<double>(base::SysInfo::AmountOfFreeDiskSpace(
+          base::FilePath(info.location())));
+    }
   }
 
   return -1;
@@ -89,8 +86,9 @@ double StorageInfoProvider::GetStorageFreeSpaceFromTransientIdAsync(
 
 // static
 StorageInfoProvider* StorageInfoProvider::Get() {
-  if (provider_.Get().get() == nullptr)
+  if (provider_.Get().get() == nullptr) {
     provider_.Get() = new StorageInfoProvider();
+  }
   return provider_.Get().get();
 }
 
