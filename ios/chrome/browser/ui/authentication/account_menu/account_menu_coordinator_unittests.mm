74dd3c07bfd2e55f3ba8db94d8333a792d856d67
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2024-09-13 13:55:59 +0000

    [ios] Tweak sign-out snackbar position
    Make it so the sign-out snackbar doesn't overlap with the browser
    toolbar.
    
    Bug: 365507133
    Change-Id: I57cbf4f5e33a348e07491721b31be53ba2607315
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857836
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355168}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator_unittests.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator_unittests.mm
@@ -216,8 +216,7 @@ TEST_F(AccountMenuCoordinatorNonManagedTest, testSignOut) {
   CGRect rect = CGRect();
   OCMExpect([delegate_ acountMenuCoordinatorShouldStop:coordinator_]);
   OCMExpect([mock_snackbar_commands_handler_
-      showSnackbarMessage:[OCMArg isNotNil]
-             bottomOffset:0]);
+      showSnackbarMessageOverBrowserToolbar:[OCMArg isNotNil]]);
   [coordinator_ signOutFromTargetRect:rect
                              callback:^(BOOL success) {
                                EXPECT_TRUE(success);
@@ -263,8 +262,8 @@ TEST_F(AccountMenuCoordinatorNonManagedTest, testMediatorWantsToBeDismissed) {
 // Tests that `triggerSignoutWithTargetRect` shows a snackbar and calls its
 // completion.
 TEST_F(AccountMenuCoordinatorNonManagedTest, testTriggerSignout) {
-  OCMExpect([mock_snackbar_commands_handler_ showSnackbarMessage:[OCMArg any]
-                                                    bottomOffset:0]);
+  OCMExpect([mock_snackbar_commands_handler_
+      showSnackbarMessageOverBrowserToolbar:[OCMArg any]]);
 
   base::RunLoop run_loop;
   base::RepeatingClosure closure = run_loop.QuitClosure();
