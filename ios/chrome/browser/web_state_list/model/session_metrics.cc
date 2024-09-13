2f72f35629bded1587c013b88a5f36091b601d0b
Mark Cogan <marq@google.com>
2024-09-13 13:19:36 +0000

    [iOS] Updates files under ios/chrome/browser/web* directories
    This CL replaces:
    * ChromeBrowserState => ProfileIOS
    * GetBrowserState => GetProfile
    * browser_state => profile
    * browserState => profile
    * GetOriginalChromeBrowserState => GetOriginalProfile
    * HasOffTheRecordChromeBrowserState => HasOffTheRecordProfile
    * GetOffTheRecordChromeBrowserState => GetOffTheRecordProfile
    * DestroyOffTheRecordChromeBrowserState => DestroyOffTheRecordProfile
    
    in i/c/b/webauthn, web_state_list, and web_selection.
    
    Bug: 365533814, 365533164, 365533485
    Change-Id: I0df553de18c1dae804a35f4bc713526bc6938c44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860519
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355140}

--- ios/chrome/browser/web_state_list/model/session_metrics.cc
+++ ios/chrome/browser/web_state_list/model/session_metrics.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 // Global whose address is used as a unique key to find the
-// SessionMetrics associated to a particular ChromeBrowserState.
+// SessionMetrics associated to a particular ProfileIOS.
 const int kSessionMetricsKey = 0;
 }  // namespace
 
