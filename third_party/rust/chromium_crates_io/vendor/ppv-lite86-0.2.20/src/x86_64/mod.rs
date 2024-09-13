2063c075703d6861223460775980e2f98591381d
danakj <danakj@chromium.org>
2024-09-13 21:48:59 +0000

    Roll ppv-lite86: 0.2.17 => 0.2.20 in //third_party/rust.
    This CL has been created semi-automatically.  The expected review
    process and other details can be found at
    //tools/crates/create_update_cl.md
    
    Updated crates:
    
    * ppv-lite86: 0.2.17 => 0.2.20
    
    Chromium `supply-chain/config.toml` policy requires that the following
    audit criteria are met (note that these are the *minimum* required
    criteria and `supply-chain/audits.toml` can and should record a stricter
    certification if possible;  see also //docs/rust-unsafe.md):
    
    * ppv-lite86@0.2.20: crypto-safe, safe-to-run
    
    Bug: None
    Change-Id: I4c5f47dac6ea3d50b14c412bb6fde86d2601b2c8
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Disable-Rts: True
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840666
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Adrian Taylor <adetaylor@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355440}

--- third_party/rust/chromium_crates_io/vendor/ppv-lite86-0.2.20/src/x86_64/mod.rs
+++ third_party/rust/chromium_crates_io/vendor/ppv-lite86-0.2.20/src/x86_64/mod.rs
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 use crate::types::*;
 use core::arch::x86_64::{__m128i, __m256i};
+use zerocopy::{AsBytes, FromBytes, FromZeroes};
 
 mod sse2;
 
@@ -107,7 +108,8 @@ pub type AVX2 = Avx2Machine<NoNI>;
 /// Converting into and out of this type should be essentially free, although it may be more
 /// aligned than a particular impl requires.
 #[allow(non_camel_case_types)]
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, FromBytes, AsBytes, FromZeroes)]
+#[repr(C)]
 pub union vec128_storage {
     u32x4: [u32; 4],
     u64x2: [u64; 2],
