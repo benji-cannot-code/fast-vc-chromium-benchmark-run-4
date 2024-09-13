8e667be950ae5a5b4e723b4af117f5c4a165e0e4
Xiaoling Bao <xiaolingbao@google.com>
2024-09-13 20:10:31 +0000

    Do not infer usage stats opt-in from CECA.
    Bug: 366491782
    Change-Id: I6f3c155368d8e1a1a05505d7504286e5baf72837
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5861869
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Reviewed-by: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355385}

--- chrome/updater/update_usage_stats_task_win.cc
+++ chrome/updater/update_usage_stats_task_win.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "base/win/windows_types.h"
+#include "chrome/updater/app/app_utils.h"
 #include "chrome/updater/constants.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/win_constants.h"
@@ -45,7 +46,7 @@ bool AppUsageStatsAllowed(UpdaterScope scope, const std::wstring& app_id) {
 bool OtherAppUsageStatsAllowed(const std::vector<std::string>& app_ids,
                                UpdaterScope scope) {
   for (auto app_id : app_ids) {
-    if (base::EqualsCaseInsensitiveASCII(app_id, kUpdaterAppId)) {
+    if (IsUpdaterOrCompanionApp(app_id)) {
       continue;
     }
 
