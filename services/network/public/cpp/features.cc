8f2ccb2fd32f5b3a45da34fecf07846c595b77f0
Sam LeDoux <sledoux@chromium.org>
2024-09-13 19:00:41 +0000

    Enable `kStorageAccessHeadersTrial` by default
    This CL enables the network feature `kStorageAccessHeadersTrial` by
    default in preparation for a Storage Access Header Origin Trial. With
    this change, the Storage Access Header flow will function only for
    origins pariticipating in a Storage Access Header OT, once such an OT
    exists.
    
    Bug: 332335089
    Change-Id: Iab650f59cd426a1273746d8051561f99f1738eab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856493
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Sam LeDoux <sledoux@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355317}

--- services/network/public/cpp/features.cc
+++ services/network/public/cpp/features.cc
@@ -488,6 +488,6 @@ BASE_FEATURE(kStorageAccessHeaders,
 
 BASE_FEATURE(kStorageAccessHeadersTrial,
              "StorageAccessHeadersTrial",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace network::features
