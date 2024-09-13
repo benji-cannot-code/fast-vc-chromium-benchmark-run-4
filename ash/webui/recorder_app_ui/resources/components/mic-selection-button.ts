b053db32bf11478284c036e2ff790376ca601128
Kam Kwankajornkiet <kamchonlathorn@google.com>
2024-09-13 07:47:32 +0000

    recorder: Update mic selection role to menu
    Update mic selection role to menu and its items' role to be
    menuitemcheckbox.
    
    Bug: b:365591454
    Test: Chromevox
    Change-Id: Iaa6d4bdcb4c676002292978af3c4502836df141f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855005
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355029}

--- ash/webui/recorder_app_ui/resources/components/mic-selection-button.ts
+++ ash/webui/recorder_app_ui/resources/components/mic-selection-button.ts
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import
-  'chrome://resources/cros_components/icon_dropdown/icon-dropdown-option.js';
 import 'chrome://resources/cros_components/menu/menu_separator.js';
 import './cra/cra-icon.js';
 import './cra/cra-icon-dropdown.js';
+import './cra/cra-icon-dropdown-option.js';
 
 import {css, html, map, nothing} from 'chrome://resources/mwc/lit/index.js';
 
@@ -52,14 +51,15 @@ export class MicSelectionButton extends ReactiveLitElement {
     };
     // TODO(kamchonlathorn): Get status and render noise cancellation warning.
     return html`
-      <cros-icon-dropdown-option
+      <cra-icon-dropdown-option
         headline=${mic.label}
         itemStart="icon"
         ?checked=${isSelectedMic}
         @cros-icon-dropdown-option-triggered=${onSelectMic}
+        data-role="menuitemcheckbox"
       >
         <cra-icon slot="start" name=${micIcon}></cra-icon>
-      </cros-icon-dropdown-option>
+      </cra-icon-dropdown-option>
     `;
   }
 
@@ -72,7 +72,7 @@ export class MicSelectionButton extends ReactiveLitElement {
 
     return html`
       <cros-menu-separator></cros-menu-separator>
-      <cros-icon-dropdown-option
+      <cra-icon-dropdown-option
         headline=${i18n.micSelectionMenuChromebookAudioOption}
         itemStart="icon"
         itemEnd="switch"
@@ -80,7 +80,7 @@ export class MicSelectionButton extends ReactiveLitElement {
         @cros-menu-item-triggered=${this.toggleSystemAudio}
       >
         <cra-icon slot="start" name="laptop_chromebook"></cra-icon>
-      </cros-icon-dropdown-option>
+      </cra-icon-dropdown-option>
     `;
   }
 
@@ -94,7 +94,7 @@ export class MicSelectionButton extends ReactiveLitElement {
         shape="circle"
         anchor-corner="start-start"
         menu-corner="end-start"
-        role="combobox"
+        menu-type="menu"
         aria-label=${i18n.micSelectionMenuButtonTooltip}
       >
         <cra-icon slot="button-icon" name="mic"></cra-icon>
