bfd37a8340b949907123170f615d46640eb3e093
Steinar H. Gunderson <sesse@chromium.org>
2024-09-13 12:09:04 +0000

    Rename types_ to data_.
    Nobody knows anymore why this was called types_.
    
    Change-Id: I74bf08ca2cfd02fa8048fba43bc914fe7fb288ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860157
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355124}

--- third_party/blink/renderer/core/css/resolver/cascade_expansion.cc
+++ third_party/blink/renderer/core/css/resolver/cascade_expansion.cc
@@ -16,7 +16,7 @@ CascadeFilter AddValidPropertiesFilter(
     CascadeFilter filter,
     const MatchedProperties& matched_properties) {
   switch (static_cast<ValidPropertyFilter>(
-      matched_properties.types_.valid_property_filter)) {
+      matched_properties.data_.valid_property_filter)) {
     case ValidPropertyFilter::kNoFilter:
       return filter;
     case ValidPropertyFilter::kCue:
@@ -42,7 +42,7 @@ CascadeFilter AddValidPropertiesFilter(
 
 CascadeFilter AddLinkFilter(CascadeFilter filter,
                             const MatchedProperties& matched_properties) {
-  switch (matched_properties.types_.link_match_type) {
+  switch (matched_properties.data_.link_match_type) {
     case CSSSelector::kMatchVisited:
       return filter.Add(CSSProperty::kVisited, false);
     case CSSSelector::kMatchLink:
