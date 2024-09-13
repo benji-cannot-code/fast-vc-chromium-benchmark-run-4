d5c1972876e12debb4f82bbe9d2be9e1a972702a
Jacob Stanley <jacobstanley@google.com>
2024-09-13 16:12:55 +0000

    Clank Consent Dialog - Privacy Policy Link
    Adds the functionality to create a ThinWebView which displays the Privacy Policy when the "Privacy Policy" link is clicked in the Dialog.
    
    Bug: 358087476
    Change-Id: I7022d612d390b5b99eb477e14f68f96de04389d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804642
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Fiona Macintosh <fmacintosh@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355228}

--- chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabbed_mode/TabbedRootUiCoordinator.java
@@ -864,7 +864,7 @@ public class TabbedRootUiCoordinator extends RootUiCoordinator {
 
         if (!shouldSuppressPSDialog) {
             return PrivacySandboxDialogController.maybeLaunchPrivacySandboxDialog(
-                    mActivity, profile, SurfaceType.BR_APP);
+                    mActivity, profile, SurfaceType.BR_APP, mWindowAndroid);
         }
 
         return false;
