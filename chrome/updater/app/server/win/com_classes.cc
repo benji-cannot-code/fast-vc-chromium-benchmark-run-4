ec1e16e44fdfed5817f95cd343af4f4343276d99
S. Ganesh <ganesh@chromium.org>
2024-09-13 16:47:58 +0000

    updater: allow periodic task triggers for Windows system-scoped installs
    This CL allows non-admin callers to call the following methods on the
    `system` `IUpdater`, and in addition, all methods on `IUpdaterInternal`:
    * GetVersion
    * GetAppStates
    * Update
    * CheckForUpdate
    * RunPeriodicTasks
    * UpdateAll
    
    Low-Coverage-Reason: HARD_TO_TEST
    Fixed: 366151405
    Change-Id: I9f1b1efc5debde69494482c53162787ba24ada20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858192
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355260}

--- chrome/updater/app/server/win/com_classes.cc
+++ chrome/updater/app/server/win/com_classes.cc
@@ -211,7 +211,7 @@ STDMETHODIMP CompleteStatusImpl::get_statusMessage(BSTR* message) {
 }
 
 HRESULT UpdaterImpl::RuntimeClassInitialize() {
-  return IsCOMCallerAllowed();
+  return S_OK;
 }
 
 HRESULT UpdaterImpl::GetVersion(BSTR* version) {
@@ -262,6 +262,10 @@ HRESULT UpdaterImpl::RegisterApp(const wchar_t* app_id,
                                  const wchar_t* version,
                                  const wchar_t* existence_checker_path,
                                  IUpdaterCallback* callback) {
+  if (FAILED(IsCOMCallerAllowed())) {
+    return E_ACCESSDENIED;
+  }
+
   if (!callback) {
     return E_INVALIDARG;
   }
@@ -573,6 +577,10 @@ HRESULT UpdaterImpl::Install(const wchar_t* app_id,
                              const wchar_t* install_data_index,
                              LONG priority,
                              IUpdaterObserver* observer) {
+  if (FAILED(IsCOMCallerAllowed())) {
+    return E_ACCESSDENIED;
+  }
+
   if (!observer) {
     return E_INVALIDARG;
   }
@@ -690,6 +698,10 @@ HRESULT UpdaterImpl::RunInstaller(const wchar_t* app_id,
                                   const wchar_t* install_data,
                                   const wchar_t* install_settings,
                                   IUpdaterObserver* observer) {
+  if (FAILED(IsCOMCallerAllowed())) {
+    return E_ACCESSDENIED;
+  }
+
   VLOG(1) << __func__;
 
   if (!observer) {
@@ -822,7 +834,7 @@ HRESULT UpdaterImpl::GetAppStates(IUpdaterAppStatesCallback* callback) {
 }
 
 HRESULT UpdaterInternalImpl::RuntimeClassInitialize() {
-  return IsCOMCallerAllowed();
+  return S_OK;
 }
 
 // See the comment for the UpdaterImpl::Update.
