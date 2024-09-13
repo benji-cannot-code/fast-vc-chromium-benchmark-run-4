98a4bca200ce18a3879dcab5177c7f72e66b0d4b
Filipa Senra <fsenra@google.com>
2024-09-13 16:02:00 +0000

    [iOS][QD] Use same blue in animation for dark and light themes
    This CL creates kStaticBlueColor which uses the light mode blue color
    from kBlueColor for both light and night mode. This is necessary for the
    tabs closure animation for Quick Delete and makes the animation color
    consistent with the rest of the blues in the tab grid which also don't
    change between light and dark themes.
    
    Fixed: 365940434
    Bug: 335387869, 354112735
    Change-Id: Ie18bea59c2e3f9cdf333735a4ac33f00edc50957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853014
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355209}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/tabs_closure_animation.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/tabs_closure_animation.mm
@@ -224,9 +224,12 @@ CAGradientLayer* GetAnimatedWipeEffect(CGRect frame,
   CAGradientLayer* gradient_layer = [CAGradientLayer layer];
   gradient_layer.type = kCAGradientLayerRadial;
   gradient_layer.colors = @[
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.6].CGColor,
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.4].CGColor,
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.0].CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.6]
+        .CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.4]
+        .CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.0]
+        .CGColor,
   ];
 
   // The gradient shouldn't be visible at the start.
@@ -267,9 +270,12 @@ CAGradientLayer* GetAnimatedWipeEffect(CGRect frame,
   colors_animation.duration = kColorGradientOpacityDuration;
   colors_animation.byValue = gradient_layer.colors;
   colors_animation.toValue = @[
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.7].CGColor,
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.45].CGColor,
-    (id)[[UIColor colorNamed:kBlueColor] colorWithAlphaComponent:0.0].CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.7]
+        .CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.45]
+        .CGColor,
+    (id)[[UIColor colorNamed:kStaticBlueColor] colorWithAlphaComponent:0.0]
+        .CGColor,
   ];
 
   // Prolong the end state of the animation, so the window continues to be blue.
