82af89c918d22753d174c0575c1d467e8b770e39
Takashi Sakamoto <tasak@google.com>
2024-09-13 02:47:33 +0000

    Spanification tool: add namespace while spanification.
    This cl does the same things as crrev.com/c/5836794 for arrays.
    
    To determine whether it is possible to get the original type from the
    source text or not, we see whether
    `array_type_loc.getElementLoc().getSourceRange()` fully contains
    `array_type_loc.getBracketsRange()` or not. If fully contains,
    we use `getAsString()`. Otherwise, we get the original type.
    
    For example if the given array is an array of function pointers:
    `int (*func[16])[]`. The getElementLoc().getSourceRange() contains
    the brackets range: `[16]`. So we use `getAsString()`.
    
    Bug: 359754054
    Change-Id: Iaaca0857aed642f634d8da6b39d88a9f64ffd194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854569
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1354962}

--- tools/clang/spanify/tests/array-tests-expected.cc
+++ tools/clang/spanify/tests/array-tests-expected.cc
@@ -3,10 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdint.h>
+
 #include <array>
 
 // No rewrite expected.
 extern const int kPropertyVisitedIDs[];
+
+namespace ns1 {
+
+struct Type1 {
+  int value;
+};
+
+}  // namespace ns1
 
 void fct() {
   // Expected rewrite:
@@ -30,6 +40,32 @@ void fct() {
   // std::array<int, buf3[0]> buf4;
   std::array<int, buf3[0]> buf4;
   buf4[index] = 11;
+
+  // Expected rewrite:
+  // std::array<ns1::Type, 5> buf5 = {{1}, {1}, {1}, {1}, {1}};
+  std::array<ns1::Type1, 5> buf5 = {{1}, {1}, {1}, {1}, {1}};
+  buf5[index].value = 11;
+
+  // Expected rewrite:
+  // std::array<uint16_t, 3> buf6;
+  std::array<uint16_t, 3> buf6 = {1, 1, 1};
+  buf6[index] = 1;
+
+  // Expected rewrite:
+  // std::array<int (*)(int), 16> buf7 = {nullptr};
+  std::array<int (*)(int), 16> buf7 = {nullptr};
+  buf7[index] = nullptr;
+
+  // Expected rewrite:
+  // std::array<int (**)[], 16> buf8 = {nullptr};
+  std::array<int(**)[], 16> buf8 = {nullptr};
+  buf8[index] = nullptr;
+
+  using Arr = int(**)[];
+  // Expected rewrite:
+  // std::array<Arr, buf3[0]> buf9 = {nullptr};
+  std::array<Arr, buf3[0]> buf9 = {nullptr};
+  buf9[index] = nullptr;
 
   index = kPropertyVisitedIDs[index];
 }
--- tools/clang/spanify/tests/array-tests-original.cc
+++ tools/clang/spanify/tests/array-tests-original.cc
@@ -3,8 +3,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdint.h>
+
 // No rewrite expected.
 extern const int kPropertyVisitedIDs[];
+
+namespace ns1 {
+
+struct Type1 {
+  int value;
+};
+
+}  // namespace ns1
 
 void fct() {
   // Expected rewrite:
@@ -28,6 +38,32 @@ void fct() {
   // std::array<int, buf3[0]> buf4;
   int buf4[buf3[0]];
   buf4[index] = 11;
+
+  // Expected rewrite:
+  // std::array<ns1::Type, 5> buf5 = {{1}, {1}, {1}, {1}, {1}};
+  ns1::Type1 buf5[] = {{1}, {1}, {1}, {1}, {1}};
+  buf5[index].value = 11;
+
+  // Expected rewrite:
+  // std::array<uint16_t, 3> buf6;
+  uint16_t buf6[] = {1, 1, 1};
+  buf6[index] = 1;
+
+  // Expected rewrite:
+  // std::array<int (*)(int), 16> buf7 = {nullptr};
+  int (*buf7[16])(int) = {nullptr};
+  buf7[index] = nullptr;
+
+  // Expected rewrite:
+  // std::array<int (**)[], 16> buf8 = {nullptr};
+  int(**buf8[16])[] = {nullptr};
+  buf8[index] = nullptr;
+
+  using Arr = int(**)[];
+  // Expected rewrite:
+  // std::array<Arr, buf3[0]> buf9 = {nullptr};
+  Arr buf9[buf3[0]] = {nullptr};
+  buf9[index] = nullptr;
 
   index = kPropertyVisitedIDs[index];
 }
