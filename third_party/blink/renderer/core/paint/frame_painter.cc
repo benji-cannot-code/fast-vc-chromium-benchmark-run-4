e6a423aefdef5cc35fc9866bca9f223bac17f8d5
François Doray <fdoray@chromium.org>
2024-09-13 20:45:03 +0000

    [blink] Remove unused method blink::WebCache::SetCapacity.
    And all code that becomes unused as a result.
    
    Motivation: Drive-by while investigating the behavior of the
    memory cache.
    
    Change-Id: I3de15e70b52090d6b66a3f67b06bcd6c6025edf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840958
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355404}

--- third_party/blink/renderer/core/paint/frame_painter.cc
+++ third_party/blink/renderer/core/paint/frame_painter.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/platform/graphics/graphics_context.h"
 #include "third_party/blink/renderer/platform/graphics/paint/drawing_recorder.h"
 #include "third_party/blink/renderer/platform/graphics/paint/scoped_display_item_fragment.h"
-#include "third_party/blink/renderer/platform/loader/fetch/memory_cache.h"
 
 namespace blink {
 
@@ -100,7 +99,6 @@ void FramePainter::Paint(GraphicsContext& context, PaintFlags paint_flags) {
   if (is_top_level_painter) {
     // Everything that happens after paintContents completions is considered
     // to be part of the next frame.
-    MemoryCache::Get()->UpdateFramePaintTimestamp();
     in_paint_contents_ = false;
   }
 }
