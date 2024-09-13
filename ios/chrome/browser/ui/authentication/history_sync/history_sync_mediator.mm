7af3e147dbaa750d6fe87b00ed563b100f26f1cf
Jérôme Lebel <jlebel@chromium.org>
2024-09-13 20:11:30 +0000

    [iOS] Add more information in CHECK in HistorySyncMediator
    AuthenticationService::GetPrimaryIdentity() can return nil according
    to crbug.com/366198713.
    
    This doesn't possible since the mediator listens for IdentityManager
    to know when the primary account disappear. When it happens,
    the mediator sends the signal to the coordinator to dismiss itself.
    
    Two possible issues seems possible:
    1. There is a mismatch between IdentityManager and
       AuthenticaitonService.
    2. The history sync opt-in dialog is not removed on sign out.
    
    This patch update CHECK to add in the log if there is primary
    account in AuthenticationService and IdentityManager to understand
    if 1- is possible.
    
    When the primary identity is removed to device, 1. could be explained
    by a race condition. The id<SystemIdentity> for the primary account
    is removed from ChromeAccountManagerService, and IdentityManager
    didn't update itself yet.
    Therefore, the mediator didn't receive the notification yet, and
    AuthenticationService cannot find id<SystemIdentity>.
    
    This seems very unlikely but it would be interesting to prove if
    this is the cause of this CHECK failure.
    
    Bug: 366198713
    Change-Id: I1543c937f904ef8a0251842e4b9b650b0af37084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860423
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355386}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
@@ -49,6 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       _accountCapabilitiesLatencyTracker;
   // Capabilities fetcher to determine minor mode restriction.
   HistorySyncCapabilitiesFetcher* _capabilitiesFetcher;
+  // This boolean should help to understand CHECK failure with
+  // crbug.com/366198713. This variable can be removed once the bug is fixed.
+  BOOL _signoutNotificationCalled;
 }
 
 - (instancetype)
@@ -100,7 +103,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)enableHistorySyncOptin {
   id<SystemIdentity> identity =
       _authenticationService->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
-  CHECK(identity);
+  bool hasPrimaryAccount =
+      _identityManager->HasPrimaryAccount(signin::ConsentLevel::kSignin);
+  // It is possible to have no identity from AuthenticationService here
+  // (see crbug.com/366198713).
+  // The mediator listens for IdentityManagerObserverBridgeDelegate to know
+  // if the user is signed out. If it happens, the dialog is supposed to be
+  // dissmissed automatically.
+  // to understand if there is a difference between AuthenticationService and
+  // IdentityManager, the CHECK logs if there is primary identity
+  // from AuthenticationService and from IdentityManager.
+  CHECK(identity) << "IdentityManager has primary identity: "
+                  << hasPrimaryAccount << ", _signoutNotificationCalled: "
+                  << _signoutNotificationCalled;
   // TODO(crbug.com/40068130): Record the history sync opt-in when the new
   // consent type will be available.
   syncer::SyncUserSettings* syncUserSettings = _syncService->GetUserSettings();
@@ -158,6 +173,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (const signin::PrimaryAccountChangeEvent&)event {
   if (event.GetEventTypeFor(signin::ConsentLevel::kSignin) ==
       signin::PrimaryAccountChangeEvent::Type::kCleared) {
+    _signoutNotificationCalled = YES;
     [self.delegate historySyncMediatorPrimaryAccountCleared:self];
   }
 }
