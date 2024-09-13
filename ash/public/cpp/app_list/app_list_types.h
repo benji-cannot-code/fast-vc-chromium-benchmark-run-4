46546d88d7280dfaa61c7d0cd13b761eae49d43d
Danny Wang <wangdanny@google.com>
2024-09-13 18:50:03 +0000

    shortcut: Fix browser back icon in Launcher
    Screenshots:
    before:
    http://screen/6tvsPXKTf9YbZnE
    http://screen/95AY2atRHfyzGav
    after:
    http://screen/4qCVFfvSfFWzsuS
    http://screen/3PZh3ueNNdkqwdx
    
    Bug: b/366266174
    Test: Test in dut
    Change-Id: I2a788d0d74408af635ac96a493acb876c2e81eff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5858121
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Danny Wang <wangdanny@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355299}

--- ash/public/cpp/app_list/app_list_types.h
+++ ash/public/cpp/app_list/app_list_types.h
@@ -687,6 +687,7 @@ class ASH_PUBLIC_EXPORT SearchResultTextItem {
     kKeyboardShortcutKeyboardBrightnessUp,
     kKeyboardShortcutKeyboardRightAlt,
     kKeyboardShortcutAccessibility,
+    kKeyboardShortcutBrowserHome,
   };
 
   // Only used for SearchResultTextItemType kString
