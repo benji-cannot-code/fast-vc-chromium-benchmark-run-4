08ab27c0aac00972ef028cb8bc3d9051d1ec57eb
Yao Xiao <yaoxia@chromium.org>
2024-09-13 17:35:57 +0000

    [shared storage] Provide BrowserInterfaceBroker to the worklet
    Why:
    This change will allow the reuse of existing components that already
    utilize BrowserInterfaceBroker for Mojo connection setup. One
    potential example is blink::LockManager for Web Locks API support,
    which sets up a mojom::blink::LockManager connection. Regardless,
    the BrowserInterfaceBroker provides a streamlined approach to
    bootstrap Mojo service connections.
    
    Note:
    - 1. No behavior change -- I went through the caller sites for
      WorkletGlobalScope::GetBrowserInterfaceBroker() and identified
      ukm::mojom::UkmRecorderFactory as the only potential interface that
      can be requested within a SharedStorageWorklet. To maintain code
      simplicity, we avoid special-casing renderer logic and disable
      ukm::mojom::UkmRecorderFactory on the browser side.
    - 2. Other usages appear to originate from non-(SharedStorage)Worklet
      contexts. If any usages were overlooked, this could lead to
      renderer crashes. We will actively monitor overall crash reports
      to promptly address any issues arising from this change.
    
    Bug: 365578420
    Change-Id: Ie0df93761f3194b6a90a4b7e7b867a43047a69b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5847692
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355290}

--- third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.cc
+++ third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/exported/web_shared_storage_worklet_thread_impl.h"
 
+#include "third_party/blink/public/mojom/browser_interface_broker.mojom.h"
 #include "third_party/blink/public/mojom/loader/code_cache.mojom.h"
 #include "third_party/blink/public/mojom/origin_trial_feature/origin_trial_feature.mojom-shared.h"
 #include "third_party/blink/public/mojom/shared_storage/shared_storage_worklet_service.mojom-blink.h"
@@ -30,6 +31,8 @@ mojom::blink::WorkletGlobalScopeCreationParamsPtr ToBlinkMojomType(
           std::move(global_scope_creation_params->devtools_host)),
       CrossVariantMojoRemote<mojom::CodeCacheHostInterfaceBase>(
           std::move(global_scope_creation_params->code_cache_host)),
+      CrossVariantMojoRemote<mojom::BrowserInterfaceBrokerInterfaceBase>(
+          std::move(global_scope_creation_params->browser_interface_broker)),
       global_scope_creation_params->wait_for_debugger);
 }
 
