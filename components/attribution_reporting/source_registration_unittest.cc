fe9e95a7ad30d760550d64d72f62d272163a338e
Thomas Quintanilla <tquintanilla@google.com>
2024-09-13 19:19:18 +0000

    Record number of scopes in source/trigger registration
    Trigger scope registrations aren't bounded to a small number like
    sources are, so we can't record the exact number. That being said, it's
    doubtful that a trigger registration will exceed 100 scopes, so we bound
    the metric by that amount.
    
    Change-Id: Ic77f01f0b49cc1da57730c2654f3bcaa9ad15c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5861469
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Reviewed-by: Andrew Paseltiner <apaseltiner@chromium.org>
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Thomas Quintanilla <tquintanilla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355348}

--- components/attribution_reporting/source_registration_unittest.cc
+++ components/attribution_reporting/source_registration_unittest.cc
@@ -771,6 +771,14 @@ TEST(SourceRegistrationTest, ParseAttributionScopesConfig) {
                                              /*max_event_states=*/1))),
       },
       {
+          "no_scopes",
+          R"json({
+            "destination": "https://d.example"
+          })json",
+          ValueIs(Field(&SourceRegistration::attribution_scopes_data,
+                        std::nullopt)),
+      },
+      {
           "invalid",
           R"json({
               "destination": "https://d.example",
@@ -787,11 +795,18 @@ TEST(SourceRegistrationTest, ParseAttributionScopesConfig) {
       features::kAttributionScopes);
 
   for (const auto& test_case : kTestCases) {
+    base::HistogramTester histograms;
     SCOPED_TRACE(test_case.desc);
 
-    EXPECT_THAT(
-        SourceRegistration::Parse(test_case.json, SourceType::kNavigation),
-        test_case.matches);
+    auto source =
+        SourceRegistration::Parse(test_case.json, SourceType::kNavigation);
+    EXPECT_THAT(source, test_case.matches);
+
+    if (source.has_value()) {
+      histograms.ExpectUniqueSample(
+          "Conversions.ScopesPerSourceRegistration",
+          source->attribution_scopes_data.has_value() ? 1 : 0, 1);
+    }
   }
 }
 
--- components/attribution_reporting/trigger_registration.cc
+++ components/attribution_reporting/trigger_registration.cc
@@ -167,6 +167,9 @@ base::expected<TriggerRegistration, TriggerRegistrationError> ParseDict(
             : TriggerRegistrationError::kAggregatableValuesValueInvalid);
   }
 
+  base::UmaHistogramCounts100("Conversions.ScopesPerTriggerRegistration",
+                              registration.attribution_scopes.scopes().size());
+
   return registration;
 }
 
