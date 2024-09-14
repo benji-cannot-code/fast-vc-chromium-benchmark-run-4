14d20831896cea0fd13f11ae965ff6aac5dd67e0
Orin Jaworski <orinj@google.com>
2024-09-14 00:27:26 +0000

    Exclude stop words from query terms using search strings and hashes list
    Bug: 363085347
    Change-Id: I189ff3456a6c21ba128d682cee20601e62ce3a92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860781
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355508}

--- components/history_embeddings/search_strings_update_listener.cc
+++ components/history_embeddings/search_strings_update_listener.cc
@@ -57,7 +57,7 @@ void SearchStringsUpdateListener::OnSearchStringsUpdate(
 
 void SearchStringsUpdateListener::SetFilterWordsHashes(
     const std::string_view filter_words_hashes) {
-  for (std::string_view& hash_string : base::SplitStringPiece(
+  for (std::string_view hash_string : base::SplitStringPiece(
            filter_words_hashes, ",", base::WhitespaceHandling::TRIM_WHITESPACE,
            base::SplitResult::SPLIT_WANT_NONEMPTY)) {
     uint32_t hash;
@@ -65,6 +65,11 @@ void SearchStringsUpdateListener::SetFilterWordsHashes(
       filter_words_hashes_.insert(hash);
     }
   }
+}
+
+void SearchStringsUpdateListener::ResetForTesting() {
+  filter_words_hashes_.clear();
+  stop_words_hashes_.clear();
 }
 
 void SearchStringsUpdateListener::SetSearchStrings(
@@ -78,6 +83,14 @@ void SearchStringsUpdateListener::SetSearchStrings(
     uint32_t hash;
     if (base::StringToUint(hash_string, &hash)) {
       filter_words_hashes_.insert(hash);
+    }
+  }
+
+  stop_words_hashes_.clear();
+  for (std::string_view hash_string : strings->stop_words()) {
+    uint32_t hash;
+    if (base::StringToUint(hash_string, &hash)) {
+      stop_words_hashes_.insert(hash);
     }
   }
 }
