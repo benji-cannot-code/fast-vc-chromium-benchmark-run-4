21ac26a867c927da64379e3b3f0a031b9809be76
Brando Socarras <brandosocarras@google.com>
2024-09-14 00:20:43 +0000

    [Quick Share v2] Add Visibility column to quick settings detailed view
    When Quick Share v2 is enabled:
    A column of Quick Share v2 visibilities (Your devices, Contacts,
    Everyone) and a toggle denoting whether Everyone should only be active
    for 10 minutes show in the Quick Share quick settings detailed view.
    
    Clicking the added buttons/toggle does not do anything.
    
    The icons next to the visibilities are all that of Your devices.
    Accurate icons will be added in a later CL.
    
    UX Mock
    -------
    go/cros-quicksharev2-ux slide 8
    
    Screenshot
    ----------
    https://screenshot.googleplex.com/8Fg3oGNtjweCBE9.png
    
    Tested: manually via DUT.
    Bug: b/360150790
    Change-Id: I02df2a396bb582f3f9f754d92cbbaa7f68bb1dbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5852439
    Commit-Queue: Brando Socarras <brandosocarras@google.com>
    Reviewed-by: Daniel Classon <dclasson@google.com>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355504}

--- ash/system/nearby_share/nearby_share_detailed_view_impl.h
+++ ash/system/nearby_share/nearby_share_detailed_view_impl.h
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "ui/base/metadata/metadata_header_macros.h"
 
-namespace view {
+namespace views {
 class Button;
 class View;
-}  // namespace view
+}  // namespace views
 
 namespace ash {
 
@@ -44,10 +44,25 @@ class ASH_EXPORT NearbyShareDetailedViewImpl : public NearbyShareDetailedView,
 
   // TrayDetailedView:
   void CreateExtraTitleRowButtons() override;
+  void HandleViewClicked(views::View* view) override;
 
   void CreateIsEnabledContainer();
+  void CreateVisibilitySelectionContainer();
+  void CreateYourDevicesRow();
+  void CreateContactsRow();
+  void CreateEveryoneRow();
+  void CreateVisibilityRow(HoverHighlightView* visibility_row,
+                           const gfx::VectorIcon& vector_icon,
+                           const std::u16string& label,
+                           const std::u16string& sublabel);
+  void CreateOnlyForTenMinutesRow();
+
   void OnSettingsButtonClicked();
-  void OnToggleClicked();
+  void OnQuickShareToggleClicked();
+  void OnYourDevicesSelected();
+  void OnContactsSelected();
+  void OnEveryoneSelected();
+  void OnTenMinutesToggleClicked();
 
   raw_ptr<views::Button> settings_button_ = nullptr;
   raw_ptr<RoundedContainer> is_enabled_container_ = nullptr;
@@ -55,6 +70,11 @@ class ASH_EXPORT NearbyShareDetailedViewImpl : public NearbyShareDetailedView,
   // TODO(brandosocarras, b/360150790): use `toggle_switch_` in this class, not
   // just in utest.
   raw_ptr<Switch> toggle_switch_ = nullptr;
+  raw_ptr<RoundedContainer> visibility_selection_container_ = nullptr;
+  raw_ptr<HoverHighlightView> your_devices_row_ = nullptr;
+  raw_ptr<HoverHighlightView> contacts_row_ = nullptr;
+  raw_ptr<HoverHighlightView> everyone_row_ = nullptr;
+  raw_ptr<HoverHighlightView> only_for_ten_minutes_row_ = nullptr;
 
   const raw_ptr<NearbyShareDelegate> nearby_share_delegate_;
 
