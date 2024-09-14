5584774da1d6bf06af6902db9ebc53a2ac9e4746
Nikhil Nayunigari <nikhilcn@google.com>
2024-09-14 01:07:55 +0000

    [CrOS Cellular] Add guardrail metrics for APN policy
    UI shouldn't allow APN operations such as create/modify/delete when
    policy is restricted. These metrics logs the state of the policy when
    the network config API is called.
    
    Change-Id: If07e3712d0ebcab678828a4795616a151e8ec7c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859175
    Commit-Queue: Nikhil Nayunigari <nikhilcn@google.com>
    Reviewed-by: Jason Zhang <jiajunz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355520}

--- chromeos/ash/components/network/managed_network_configuration_handler_impl.cc
+++ chromeos/ash/components/network/managed_network_configuration_handler_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/uuid.h"
 #include "base/values.h"
@@ -928,6 +929,10 @@ void ManagedNetworkConfigurationHandlerImpl::ModifyCustomAPNs() {
                                                /*configured_only=*/false,
                                                /*visible_only=*/false,
                                                /*limit=*/0, &networks);
+
+  base::UmaHistogramBoolean(
+      "Network.Ash.Cellular.Apn.Policy.AllowApnModification",
+      AllowApnModification());
 
   for (const NetworkState* network : networks) {
     if (network->IsManagedByPolicy()) {
--- chromeos/ash/services/network_config/cros_network_config.cc
+++ chromeos/ash/services/network_config/cros_network_config.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/containers/flat_map.h"
 #include "base/i18n/time_formatting.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/not_fatal_until.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_number_conversions.h"
@@ -96,6 +97,13 @@ const char kErrorUserIsProhibitedFromConfiguringVpn[] =
 
 const char kDefaultCellularProviderName[] = "MobileNetwork";
 const char kDefaultCellularProviderCode[] = "000000";
+
+const char kCreateCustomApnPolicyHistogram[] =
+    "Network.Ash.Cellular.Apn.CreateCustomApn.AllowApnModification";
+const char kModifyCustomApnPolicyHistogram[] =
+    "Network.Ash.Cellular.Apn.ModifyCustomApn.AllowApnModification";
+const char kRemoveCustomApnPolicyHistogram[] =
+    "Network.Ash.Cellular.Apn.RemoveCustomApn.AllowApnModification";
 
 std::string ShillToOnc(const std::string& shill_string,
                        const onc::StringTranslationEntry table[]) {
@@ -3607,6 +3615,12 @@ void CrosNetworkConfig::CreateCustomApn(const std::string& network_guid,
     return;
   }
 
+  if (features::IsApnRevampAndAllowApnModificationPolicyEnabled()) {
+    base::UmaHistogramBoolean(
+        kCreateCustomApnPolicyHistogram,
+        network_configuration_handler_->AllowApnModification());
+  }
+
   if (features::IsApnRevampAndAllowApnModificationPolicyEnabled() &&
       !network_configuration_handler_->AllowApnModification()) {
     NET_LOG(ERROR)
@@ -3725,6 +3739,12 @@ void CrosNetworkConfig::RemoveCustomApn(const std::string& network_guid,
     return;
   }
 
+  if (features::IsApnRevampAndAllowApnModificationPolicyEnabled()) {
+    base::UmaHistogramBoolean(
+        kRemoveCustomApnPolicyHistogram,
+        network_configuration_handler_->AllowApnModification());
+  }
+
   if (features::IsApnRevampAndAllowApnModificationPolicyEnabled() &&
       !network_configuration_handler_->AllowApnModification()) {
     NET_LOG(ERROR)
@@ -3812,6 +3832,12 @@ void CrosNetworkConfig::ModifyCustomApn(const std::string& network_guid,
         "ModifyCustomApn: Cannot be called if the APN Revamp feature flag is "
         "disabled.");
     return;
+  }
+
+  if (features::IsApnRevampAndAllowApnModificationPolicyEnabled()) {
+    base::UmaHistogramBoolean(
+        kModifyCustomApnPolicyHistogram,
+        network_configuration_handler_->AllowApnModification());
   }
 
   if (features::IsApnRevampAndAllowApnModificationPolicyEnabled() &&
