d4a551a28846bc8bbe8a200fa7d96864c3448f53
Dibyajyoti Pal <dibyapal@google.com>
2024-09-14 00:39:03 +0000

    [PWA/Nav Capturing] Consolidate post web contents tasks for web apps
    This CL consolidates post web contents creation tasks like enqueueing launch params and showing the navigation capturing IPH at the very end of the ::Navigate() function, and moves all implementation details closer to where the initial navigation capturing logic exists.
    
    Tested by running the automated link capturing parameterized tests as well as manually triggering navigation capturing in an installed app.
    
    Bug: 364657540
    Change-Id: I7ce88f04308299f92cf66d8e230ac5aafa4183c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5861548
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Auto-Submit: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355515}

--- chrome/browser/ui/web_applications/app_browser_controller.h
+++ chrome/browser/ui/web_applications/app_browser_controller.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Browser;
 class BrowserThemePack;
 class CustomThemeSupplier;
-struct NavigateParams;
 class TabMenuModelFactory;
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -48,7 +47,6 @@ class ImageModel;
 
 namespace web_app {
 
-struct AppNavigationResult;
 class WebAppBrowserController;
 
 // Returns true if |app_url| and |page_url| are the same origin. To avoid
@@ -282,10 +280,6 @@ class AppBrowserController : public ui::ColorProviderKey::InitializerSupplier,
   // button to appear in the toolbar & the user can use it to navigate back to
   // that location.
   void MaybeSetInitialUrlOnReparentTab();
-
-  void DidStartNavigation(
-      const web_app::AppNavigationResult& app_navigation_result,
-      const NavigateParams& params);
 
  protected:
   AppBrowserController(Browser* browser,
