a3a7d1d5b8d7616fd158eccc87767c2bcdf10d18
Bruno Braga <brunobraga@google.com>
2024-09-15 20:35:24 +0000

    Use prediction improvement details suggestion
    This CL only uses the suggestion, therefore it does not deal with the
    proper string.
    
    Bug: 361434879
    Change-Id: I28188f9a054214652fb2f88ac59962257a69247e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854714
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355681}

--- components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager_unittest.cc
+++ components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager_unittest.cc
@@ -253,7 +253,7 @@ TEST_F(AutofillPredictionImprovementsManagerTest,
 }
 
 // Tests that `address_suggestions` only contains the
-// `kRetrievePredictionImprovements` suggestion if it was empty before calling
+// triggering improved predictions suggestions if it was empty before calling
 // `MaybeUpdateSuggestions()`.
 TEST_F(AutofillPredictionImprovementsManagerTest,
        MaybeUpdateSuggestionsOnEmptyAddressSuggestionsAddsTriggerSuggestion) {
@@ -263,11 +263,12 @@ TEST_F(AutofillPredictionImprovementsManagerTest,
       address_suggestions, field, /*should_add_trigger_suggestion=*/true));
   EXPECT_THAT(
       address_suggestions,
-      ElementsAre(HasType(SuggestionType::kRetrievePredictionImprovements)));
+      ElementsAre(HasType(SuggestionType::kRetrievePredictionImprovements),
+                  HasType(SuggestionType::kPredictionImprovementsDetails)));
 }
 
 // Tests that `address_suggestions` contains the
-// `kRetrievePredictionImprovements` suggestion (incl. a separator) if
+// triggering improved prediction suggestions (incl. a separator) if
 // `address_suggestions` contained entries before calling
 // `MaybeUpdateSuggestions()`. The pre-existing entries contained a
 // `kUndoOrClear` suggestion which is the case for autofilled fields.
@@ -287,13 +288,14 @@ TEST_F(
       ElementsAre(HasType(SuggestionType::kAddressEntry),
                   HasType(SuggestionType::kSeparator),
                   HasType(SuggestionType::kRetrievePredictionImprovements),
+                  HasType(SuggestionType::kPredictionImprovementsDetails),
                   HasType(SuggestionType::kSeparator),
                   HasType(SuggestionType::kUndoOrClear),
                   HasType(SuggestionType::kManageAddress)));
 }
 
 // Tests that `address_suggestions` contains the
-// `kRetrievePredictionImprovements` suggestion (incl. a separator) if
+// improved prediction suggestions (incl. a separator) if
 // `address_suggestions` contained entries before calling
 // `MaybeUpdateSuggestions()`. The pre-existing entries did not contain a
 // `kUndoOrClear` suggestion which is the case for fields that aren't
@@ -312,6 +314,7 @@ TEST_F(AutofillPredictionImprovementsManagerTest,
       ElementsAre(HasType(SuggestionType::kAddressEntry),
                   HasType(SuggestionType::kSeparator),
                   HasType(SuggestionType::kRetrievePredictionImprovements),
+                  HasType(SuggestionType::kPredictionImprovementsDetails),
                   HasType(SuggestionType::kSeparator),
                   HasType(SuggestionType::kManageAddress)));
 }
