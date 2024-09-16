614a7820b41704f3e79a646900b323a8d38e03c4
Ameur Hosni <ameurhosni@google.com>
2024-09-16 17:36:47 +0000

    [IOS][LO] Refine consent dialog to match UI mocks
    This CL updates the lens overlay consent view controller to match the
    UI mocks.
    
    Bug: 354942727
    Change-Id: I8cfcd9f3817ff058ca445a2c34df165715b95619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854742
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355983}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -68,6 +68,9 @@ LensEntrypoint LensEntrypointFromOverlayEntrypoint(
 
 const CGFloat kSelectionOffsetPadding = 50.0f;
 
+NSString* const kCustomConsentSheetDetentIdentifier =
+    @"kCustomConsentSheetDetentIdentifier";
+
 #if BUILDFLAG(IOS_USE_BRANDED_SYMBOLS)
 const CGFloat kMenuSymbolSize = 18;
 #endif
@@ -751,11 +754,22 @@ const CGFloat kMenuSymbolSize = 18;
   sheet.prefersEdgeAttachedInCompactHeight = YES;
   sheet.largestUndimmedDetentIdentifier =
       [UISheetPresentationControllerDetent largeDetent].identifier;
-  sheet.detents = @[
-    [UISheetPresentationControllerDetent mediumDetent],
-    [UISheetPresentationControllerDetent largeDetent]
-  ];
   sheet.prefersGrabberVisible = YES;
+
+  __weak LensOverlayConsentViewController* weakConsentViewController =
+      _consentViewController;
+
+  auto resolver = ^CGFloat(
+      id<UISheetPresentationControllerDetentResolutionContext> context) {
+    return [weakConsentViewController preferredContentSize].height;
+  };
+
+  UISheetPresentationControllerDetent* customDetent =
+      [UISheetPresentationControllerDetent
+          customDetentWithIdentifier:kCustomConsentSheetDetentIdentifier
+                            resolver:resolver];
+
+  sheet.detents = @[ customDetent ];
 
   [_containerViewController presentViewController:_consentViewController
                                          animated:YES
