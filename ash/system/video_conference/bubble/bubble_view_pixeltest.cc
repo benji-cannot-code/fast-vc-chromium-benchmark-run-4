0d71eb057976b30739a4bd3f48b7567a1ed6d4b4
Ryota Okuji <okuji@chromium.org>
2024-09-16 17:33:11 +0000

    vc-ui: Fix BubbleViewPixelTest
    Fixes BubbleViewPixelTest failures caused by enabling vc-studio-look by
    default.
    
    Bug: b/299049278
    Test: CQ
    Change-Id: I21399a4269aff59841bca65c31436c974c47f4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5861411
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Juliana Chang <julianachang@chromium.org>
    Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355980}

--- ash/system/video_conference/bubble/bubble_view_pixeltest.cc
+++ ash/system/video_conference/bubble/bubble_view_pixeltest.cc
@@ -235,7 +235,7 @@ TEST_P(BubbleViewPixelTest, Basic) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "video_conference_bubble_view_basic",
-      /*revision_number=*/14, bubble_view()));
+      /*revision_number=*/15, bubble_view()));
 }
 
 // Pixel test that tests toggled on/off and focused/not focused for the toggle
@@ -272,6 +272,7 @@ TEST_P(BubbleViewPixelTest, ToggleButton) {
   LeftClickOn(first_toggle_effect_button);
   ASSERT_EQ(2, office_bunny()->num_activations_for_testing());
   auto* event_generator = GetEventGenerator();
+  event_generator->PressAndReleaseKey(ui::KeyboardCode::VKEY_TAB);
   event_generator->PressAndReleaseKey(ui::KeyboardCode::VKEY_TAB);
   event_generator->PressAndReleaseKey(ui::KeyboardCode::VKEY_TAB);
   ASSERT_TRUE(first_toggle_effect_button->HasFocus());
@@ -438,7 +439,7 @@ TEST_P(BubbleViewPixelTest, DLCUIInErrorShowsWarningLabelSingleError) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "one-toggle-effects-view",
-      /*revision_number=*/2, bubble_view()));
+      /*revision_number=*/3, bubble_view()));
 
   // Add one set-value effect.
   controller()->GetEffectsManager().RegisterDelegate(shaggy_fur());
@@ -454,7 +455,7 @@ TEST_P(BubbleViewPixelTest, DLCUIInErrorShowsWarningLabelSingleError) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "additional-set-value-view",
-      /*revision_number=*/2, bubble_view()));
+      /*revision_number=*/3, bubble_view()));
 }
 
 TEST_P(BubbleViewPixelTest, DLCUIInErrorShowsWarningLabelMaxErrors) {
@@ -475,7 +476,7 @@ TEST_P(BubbleViewPixelTest, DLCUIInErrorShowsWarningLabelMaxErrors) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "one-toggle-effects-view",
-      /*revision_number=*/2, bubble_view()));
+      /*revision_number=*/3, bubble_view()));
 
   // Add one set-value effect.
   controller()->GetEffectsManager().RegisterDelegate(shaggy_fur());
@@ -492,7 +493,7 @@ TEST_P(BubbleViewPixelTest, DLCUIInErrorShowsWarningLabelMaxErrors) {
 
   EXPECT_TRUE(GetPixelDiffer()->CompareUiComponentsOnPrimaryScreen(
       "additional-set-value-view",
-      /*revision_number=*/2, bubble_view()));
+      /*revision_number=*/3, bubble_view()));
 }
 
 }  // namespace ash::video_conference
