bc817fe6f253673ef47afaa926f9a5d81457ab24
Andy Phan <andyphan@chromium.org>
2024-09-16 18:31:25 +0000

    [PDF] Fix PDF.LoadStatus double count for embedded PDFs
    On any embedded PDF load, PDFIFrameNavigationThrottle increments the
    PDF.LoadStatus metric bucket for loading a PDF iframe without a PDF
    viewer, even if the embedded PDF actually does load in a PDF viewer. Fix
    this by only incrementing the metric when the placeholder HTML is loaded
    (and not the PDF viewer).
    
    Update the metric name to "PDF.LoadStatus2", since this will impact the
    metric.
    
    OBSOLETE_HISTOGRAM[PDF.LoadStatus]=Replaced by PDF.LoadStatus2
    
    Fixed: 365953522
    Change-Id: Ic9ab5d52376658246061d26849cc0a69057e5860
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856492
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355989}

--- chrome/browser/pdf/pdf_extension_test.cc
+++ chrome/browser/pdf/pdf_extension_test.cc
@@ -2936,11 +2936,11 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest, Metrics) {
   EXPECT_EQ(1, actions.GetActionCount("PDF.LoadSuccess"));
 }
 
-// Test that the PDF.LoadStatus metric is incremented correctly when the PDF is
+// Test that the PDF.LoadStatus2 metric is incremented correctly when the PDF is
 // loaded with PDFium.
 IN_PROC_BROWSER_TEST_P(PDFExtensionTest,
                        MetricsPDFLoadStatusLoadedPdfWithPdfium) {
-  const char kPdfLoadStatusMetric[] = "PDF.LoadStatus";
+  const char kPdfLoadStatusMetric[] = "PDF.LoadStatus2";
   base::HistogramTester histograms;
 
   histograms.ExpectBucketCount(kPdfLoadStatusMetric,
@@ -2971,9 +2971,8 @@ IN_PROC_BROWSER_TEST_P(PDFExtensionTest,
   histograms.ExpectBucketCount(
       kPdfLoadStatusMetric, PDFLoadStatus::kTriggeredNoGestureDriveByDownload,
       0);
-  // TODO(crbug.com/365953522): Count should be 0.
   histograms.ExpectBucketCount(
-      kPdfLoadStatusMetric, PDFLoadStatus::kLoadedIframePdfWithNoPdfViewer, 1);
+      kPdfLoadStatusMetric, PDFLoadStatus::kLoadedIframePdfWithNoPdfViewer, 0);
   histograms.ExpectBucketCount(
       kPdfLoadStatusMetric,
       PDFLoadStatus::kViewPdfClickedInPdfPluginPlaceholder, 0);
@@ -4166,10 +4165,10 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionOopifTest,
   base::RunLoop().RunUntilIdle();
 }
 
-// Test that the PDF.LoadStatus metric is incremented only after the PDF fully
+// Test that the PDF.LoadStatus2 metric is incremented only after the PDF fully
 // loads.
 IN_PROC_BROWSER_TEST_F(PDFExtensionOopifTest, MetricsPDFLoadStatusPartialLoad) {
-  const char kPdfLoadStatusMetric[] = "PDF.LoadStatus";
+  const char kPdfLoadStatusMetric[] = "PDF.LoadStatus2";
   base::HistogramTester histograms;
 
   histograms.ExpectBucketCount(kPdfLoadStatusMetric,
@@ -4192,7 +4191,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionOopifTest, MetricsPDFLoadStatusPartialLoad) {
   test_pdf_viewer_stream_manager->WaitUntilPdfExtensionNavigationStarted(
       primary_main_frame);
 
-  // The PDF.LoadStatus metric should not be incremented yet.
+  // The PDF.LoadStatus2 metric should not be incremented yet.
   histograms.ExpectBucketCount(kPdfLoadStatusMetric,
                                PDFLoadStatus::kLoadedFullPagePdfWithPdfium, 0);
 
@@ -4202,7 +4201,7 @@ IN_PROC_BROWSER_TEST_F(PDFExtensionOopifTest, MetricsPDFLoadStatusPartialLoad) {
   EXPECT_TRUE(
       test_pdf_viewer_stream_manager->WaitUntilPdfLoaded(primary_main_frame));
 
-  // The PDF.LoadStatus metric should be incremented.
+  // The PDF.LoadStatus2 metric should be incremented.
   histograms.ExpectBucketCount(kPdfLoadStatusMetric,
                                PDFLoadStatus::kLoadedFullPagePdfWithPdfium, 1);
 }
--- components/pdf/common/pdf_util.cc
+++ components/pdf/common/pdf_util.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
 
 void ReportPDFLoadStatus(PDFLoadStatus status) {
-  UMA_HISTOGRAM_ENUMERATION("PDF.LoadStatus", status,
+  UMA_HISTOGRAM_ENUMERATION("PDF.LoadStatus2", status,
                             PDFLoadStatus::kPdfLoadStatusCount);
 }
 
