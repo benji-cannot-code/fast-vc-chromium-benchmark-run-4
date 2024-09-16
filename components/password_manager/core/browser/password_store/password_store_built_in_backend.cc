2ea6a82294895f6e4c0fbad01a1b5fcc480ea71e
Viktor Semeniuk <vsemeniuk@google.com>
2024-09-16 16:05:49 +0000

    Fix build error for Android with USE_LOGIN_DATABASE_AS_BACKEND flag
    Build was broken by this CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/5762611
    
    Change-Id: I1837551c37141685a69e3f176a484a451f260faa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5864849
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355909}

--- components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
+++ components/password_manager/core/browser/password_store/password_store_built_in_backend.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "components/os_crypt/async/browser/os_crypt_async.h"
 #include "components/password_manager/core/browser/affiliation/affiliated_match_helper.h"
+#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/password_manager_buildflags.h"
 #include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store/get_logins_with_affiliations_request_handler.h"
@@ -30,14 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/model/proxy_data_type_controller_delegate.h"
 
 #if !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
-#include "components/password_manager/core/browser/features/password_features.h"
 #include "components/password_manager/core/browser/password_store/password_data_type_controller_delegate_android.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #endif  // !BUILDFLAG(USE_LOGIN_DATABASE_AS_BACKEND)
-
-#if !BUILDFLAG(IS_ANDROID)
-#include "components/password_manager/core/browser/features/password_features.h"
-#endif  // !BUILDFLAG(IS_ANDROID)
 
 namespace password_manager {
 
