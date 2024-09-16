8ed6cab3cd89753e91309e7cf613877523c1c06e
Adam Langley <agl@chromium.org>
2024-09-16 18:46:44 +0000

    webauthn: don't use the Touch ID sheet in a PWA.
    On macOS, a PWA renders the UI in a separate process so that macOS
    will treat it as a separate app. This means that we can't place an
    LAAuthenticationView into the UI from the browser process. (Currently
    Chrome will crash if GPM passkey biometrics are attempted in a PWA.)
    
    With this change, GPM passkeys in will use the system Touch ID prompt
    when working in a PWA so that they function mostly normally.
    
    Bug: 364926914
    Change-Id: I2532c56cccb092eb0ab938d23de1bb3bffafadea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859760
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356009}

--- chrome/browser/webauthn/gpm_enclave_controller.cc
+++ chrome/browser/webauthn/gpm_enclave_controller.cc
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/system_network_context_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/passwords/passwords_client_ui_delegate.h"
 #include "chrome/browser/ui/webauthn/user_actions.h"
 #include "chrome/browser/webauthn/authenticator_request_dialog_model.h"
@@ -315,7 +317,14 @@ EnclaveUserVerificationMethod PickEnclaveUserVerificationMethod(
     bool have_added_device,
     bool has_pin,
     EnclaveManager::UvKeyState uv_key_state,
-    bool platform_has_biometrics) {
+    bool platform_has_biometrics,
+    bool browser_is_app) {
+#if BUILDFLAG(IS_MAC)
+  constexpr bool kIsMac = true;
+#else
+  constexpr bool kIsMac = false;
+#endif
+
   if (have_added_device) {
     return EnclaveUserVerificationMethod::kImplicit;
   }
@@ -345,7 +354,14 @@ EnclaveUserVerificationMethod PickEnclaveUserVerificationMethod(
       return EnclaveUserVerificationMethod::kDeferredUVKeyWithSystemUI;
 
     case EnclaveManager::UvKeyState::kUsesChromeUI:
-      return EnclaveUserVerificationMethod::kUVKeyWithChromeUI;
+      if (browser_is_app && kIsMac) {
+        // When running in an app (i.e. a PWA) the UI is out of process. Thus we
+        // cannot position the LAAuthenticationView in the UI and so we use the
+        // system Touch ID dialog instead.
+        return EnclaveUserVerificationMethod::kUVKeyWithSystemUI;
+      } else {
+        return EnclaveUserVerificationMethod::kUVKeyWithChromeUI;
+      }
   }
 }
 
@@ -548,7 +564,7 @@ void GPMEnclaveController::OnEnclaveLoaded() {
           user_verification_requirement_, /*have_added_device=*/false,
           enclave_manager_->has_wrapped_pin(),
           enclave_manager_->uv_key_state(/*platform_has_biometrics=*/false),
-          /*platform_has_biometrics=*/false)) {
+          /*platform_has_biometrics=*/false, BrowserIsApp())) {
         case EnclaveUserVerificationMethod::kPIN:
           FIDO_LOG(EVENT)
               << "Checking security domain service because a GPM PIN will be "
@@ -929,7 +945,7 @@ void GPMEnclaveController::OnEnclaveAccountSetUpComplete() {
       user_verification_requirement_, have_added_device_,
       enclave_manager_->has_wrapped_pin(),
       enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
-      *model_->platform_has_biometrics);
+      *model_->platform_has_biometrics, BrowserIsApp());
   // `have_added_device_` is set, which satisfies UV, so we must have picked
   // "implicit" UV.
   CHECK_EQ(*uv_method_, EnclaveUserVerificationMethod::kImplicit);
@@ -960,7 +976,7 @@ void GPMEnclaveController::OnGPMSelected() {
           user_verification_requirement_, have_added_device_,
           enclave_manager_->has_wrapped_pin(),
           enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
-          *model_->platform_has_biometrics);
+          *model_->platform_has_biometrics, BrowserIsApp());
 
       switch (*uv_method_) {
         case EnclaveUserVerificationMethod::kUVKeyWithSystemUI:
@@ -1013,7 +1029,7 @@ void GPMEnclaveController::OnGPMPasskeySelected(
           user_verification_requirement_, have_added_device_,
           enclave_manager_->has_wrapped_pin(),
           enclave_manager_->uv_key_state(*model_->platform_has_biometrics),
-          *model_->platform_has_biometrics);
+          *model_->platform_has_biometrics, BrowserIsApp());
 
       switch (*uv_method_) {
         case EnclaveUserVerificationMethod::kUVKeyWithSystemUI:
@@ -1483,6 +1499,17 @@ void GPMEnclaveController::HandlePINValidationResult(
       model_->SetStep(Step::kGPMLockedPin);
       break;
   }
+}
+
+bool GPMEnclaveController::BrowserIsApp() const {
+  content::WebContents* web_contents =
+      content::WebContents::FromRenderFrameHost(
+          content::RenderFrameHost::FromID(render_frame_host_id_));
+  if (!web_contents) {
+    return false;
+  }
+  Browser* browser = chrome::FindBrowserWithTab(web_contents);
+  return browser && browser->is_type_app();
 }
 
 void GPMEnclaveController::OnGpmPasskeysReset(bool success) {
