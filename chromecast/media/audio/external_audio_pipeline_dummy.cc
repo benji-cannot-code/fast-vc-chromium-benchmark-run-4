4c50f5d2bfb23df5246834bed2f44910c6300c2b
Lei Zhang <thestig@chromium.org>
2024-09-16 18:37:37 +0000

    Always define NOTREACHED() in chromecast/system/reboot/reboot_dummy.cc
    Fix a small error from https://crrev.com/1355936
    
    NOTREECHECKS=true
    
    Change-Id: Icc6d49c214738c9c6996481058e7a0c558cc9403
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867320
    Reviewed-by: Zijie He <zijiehe@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Owners-Override: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355999}

--- chromecast/media/audio/external_audio_pipeline_dummy.cc
+++ chromecast/media/audio/external_audio_pipeline_dummy.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // swap in their implementation it complains about missing symbols.
 #include <stdlib.h>  // abort()
 #define NOTREACHED() abort()
-#endif
 
 namespace chromecast {
 namespace media {
--- chromecast/system/reboot/reboot_dummy.cc
+++ chromecast/system/reboot/reboot_dummy.cc
@@ -5,10 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromecast/public/reboot_shlib.h"
 
-#if !defined(NDEBUG) || defined(DCHECK_ALWAYS_ON)
 #include <stdlib.h>  // abort()
 #define NOTREACHED() abort()
-#endif
 
 namespace chromecast {
 
