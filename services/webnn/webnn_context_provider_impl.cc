a10397e1cc3e75692ad4fffc9a5d5b9bc6c7b016
Will Harris <wfh@chromium.org>
2024-09-16 15:34:07 +0000

    Revert "WebNN: Handle device-removal error on DirectML"
    This reverts commit 3a4889b10ae431826f47b005e0d36c13aab95a2d.
    
    Reason for revert: suspected cause of issue 366573639
    
    Original change's description:
    > WebNN: Handle device-removal error on DirectML
    >
    > This CL kills the GPU process to destroy all contexts if
    > device-removal[1] error happens on DirectML on services side.
    >
    > GPU/NPU contexts on DirectML rely on the same device[2]. If the
    > device enters a "device-removed" state, all affected contexts become
    > unavailable and should be destroyed immediately.
    >
    > Additionally, since other components besides WebNN may reference the
    > device, so we have to terminate the GPU process to allow for the
    > re-creation of the device and recovery from device removal.
    >
    > [1] https://learn.microsoft.com/en-us/windows/ai/directml/dml-errors
    > [2] https://learn.microsoft.com/en-us/windows/win32/api/d3d12/nn-d3d12-id3d12device
    >
    > Bug: 349640008
    > Change-Id: I89b3bfefadfe4f5c92e0c5c0fd5104080822e6a2
    > Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833441
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Commit-Queue: Mingming1 Xu <mingming1.xu@intel.com>
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1355090}
    
    Bug: 349640008,366573639
    Change-Id: I49f2d0f7e8d23fcfed54a0574b765f0c500addd5
    Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5863325
    Owners-Override: Daniel Yip <danielyip@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355873}

--- services/webnn/webnn_context_provider_impl.cc
+++ services/webnn/webnn_context_provider_impl.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check_is_test.h"
 #include "base/types/expected_macros.h"
-#include "components/viz/service/gl/gpu_service_impl.h"  // nogncheck
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
 #include "services/webnn/buildflags.h"
 #include "services/webnn/error.h"
@@ -68,7 +67,7 @@ base::expected<scoped_refptr<dml::Adapter>, mojom::ErrorPtr> GetDmlGpuAdapter(
 
   if (!shared_context_state) {
     // Unit tests do not pass in a SharedContextState, since a reference to
-    // a viz::GpuServiceImpl must be initialized to obtain a SharedContextState.
+    // a GpuServiceImpl must be initialized to obtain a SharedContextState.
     // Instead, we just enumerate the first DXGI adapter.
     CHECK_IS_TEST();
     return dml::Adapter::GetGpuInstanceForTesting();
@@ -114,12 +113,10 @@ bool ShouldCreateDmlContext(const mojom::CreateContextOptions& options) {
 WebNNContextProviderImpl::WebNNContextProviderImpl() = default;
 #else
 WebNNContextProviderImpl::WebNNContextProviderImpl(
-    viz::GpuServiceImpl* gpu_service,
     scoped_refptr<gpu::SharedContextState> shared_context_state,
     gpu::GpuFeatureInfo gpu_feature_info,
     gpu::GPUInfo gpu_info)
-    : gpu_service_(gpu_service),
-      shared_context_state_(std::move(shared_context_state)),
+    : shared_context_state_(std::move(shared_context_state)),
       gpu_feature_info_(std::move(gpu_feature_info)),
       gpu_info_(std::move(gpu_info)) {}
 #endif  // BUILDFLAG(IS_CHROMEOS)
@@ -137,12 +134,13 @@ void WebNNContextProviderImpl::Create(
 
 #else
 std::unique_ptr<WebNNContextProviderImpl> WebNNContextProviderImpl::Create(
-    viz::GpuServiceImpl* gpu_service,
-    scoped_refptr<gpu::SharedContextState> shared_context_state) {
+    scoped_refptr<gpu::SharedContextState> shared_context_state,
+    gpu::GpuFeatureInfo gpu_feature_info,
+    gpu::GPUInfo gpu_info) {
   CHECK_NE(shared_context_state, nullptr);
   return base::WrapUnique(new WebNNContextProviderImpl(
-      gpu_service, std::move(shared_context_state),
-      gpu_service->gpu_feature_info(), gpu_service->gpu_info()));
+      std::move(shared_context_state), std::move(gpu_feature_info),
+      std::move(gpu_info)));
 }
 
 void WebNNContextProviderImpl::BindWebNNContextProvider(
@@ -182,7 +180,6 @@ void WebNNContextProviderImpl::CreateForTesting(
 
   mojo::MakeSelfOwnedReceiver<WebNNContextProvider>(
       base::WrapUnique(new WebNNContextProviderImpl(
-          /*gpu_service=*/nullptr,
           /*shared_context_state=*/nullptr, std::move(gpu_feature_info),
           std::move(gpu_info))),
       std::move(receiver));
@@ -194,19 +191,6 @@ void WebNNContextProviderImpl::OnConnectionError(WebNNContextImpl* impl) {
   CHECK(it != impls_.end());
   impls_.erase(it);
 }
-
-#if BUILDFLAG(IS_WIN)
-void WebNNContextProviderImpl::DestroyContextsAndKillGpuProcess(
-    std::string_view reason) {
-  // Send the contexts lost reason to the renderer process.
-  for (const auto& impl : impls_) {
-    impl->ResetReceiverWithReason(reason);
-  }
-
-  // Terminates the GPU process and then all contexts will be destroyed.
-  gpu_service_->LoseAllContexts();
-}
-#endif  // BUILDFLAG(IS_WIN)
 
 // static
 void WebNNContextProviderImpl::SetBackendForTesting(
