3879605204401752febfe1ae226467886a74d4d9
Hardik Goyal <hardikgoyal@chromium.org>
2024-09-16 13:33:07 +0000

    Reland "Change Update to UpdateOrSet"
    This is a reland of commit 0535df47a9fabf301a18bfdadb016a8f82358e78
    
    Unfortunately, there is a bug that needs to be fixed
    with OOBE waiter, so the new tests will have to start
    with disabled for now.
    
    It is failing on MSAN because of this: b:337379954
    
    It is fixed here: crrev/c/5853965
    
    For this CL I want to keep them disabled to get this moving.
    I will followup to enable in a separate CL. I have tested the CL
    locally though.
    
    Original change's description:
    > Change Update to UpdateOrSet
    >
    > The rationale for this change is that we now add recovery authfactor
    > on as soon as the user accepts T&C. So we are finding that users are
    > getting in a situation where they only have Recovery AuthFactor setup.
    > Ideally that is fine, and in a truly passwordless world that is the
    > expected scenario. Therefore when we go through something like that,
    > we either update or set.
    >
    > BUG=b:328912970
    >
    > Change-Id: I4f36528656c5c54cc62eb913e9dd40f0d6200eef
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5757067
    > Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    > Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    > Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1354430}
    
    Bug: b:328912970
    Change-Id: I772f13736926ba882a91f919b1775a7bd5c0b4ca
    
    ----
    
    Change-Id: I772f13736926ba882a91f919b1775a7bd5c0b4ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854736
    Auto-Submit: Hardik Goyal <hardikgoyal@chromium.org>
    Commit-Queue: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355815}

--- chrome/browser/ash/login/screens/osauth/apply_online_password_screen.cc
+++ chrome/browser/ash/login/screens/osauth/apply_online_password_screen.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/check.h"
+#include "base/debug/crash_logging.h"
+#include "base/debug/dump_without_crashing.h"
 #include "base/functional/bind.h"
 #include "base/logging.h"
 #include "base/memory/weak_ptr.h"
@@ -106,9 +108,12 @@ void ApplyOnlinePasswordScreen::SetOnlinePassword() {
         base::BindOnce(&ApplyOnlinePasswordScreen::OnOnlinePasswordSet,
                        weak_ptr_factory_.GetWeakPtr()));
   } else {
-    CHECK(auth_factors_config_.HasConfiguredFactor(
-        cryptohome::AuthFactorType::kPassword));
-    password_factor_editor.UpdateOnlinePassword(
+    if (!auth_factors_config_.HasConfiguredFactor(
+            cryptohome::AuthFactorType::kPassword)) {
+      LOG(WARNING) << "User does not have password configured.";
+      base::debug::DumpWithoutCrashing();
+    }
+    password_factor_editor.UpdateOrSetOnlinePassword(
         GetToken(), online_password_.value().value(),
         base::BindOnce(&ApplyOnlinePasswordScreen::OnOnlinePasswordSet,
                        weak_ptr_factory_.GetWeakPtr()));
