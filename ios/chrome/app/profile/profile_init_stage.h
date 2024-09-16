443a150b26e1bd5622b39bd8b5a44cd89b461846
Sylvain Defresne <sdefresne@chromium.org>
2024-09-16 10:17:11 +0000

    [ios] Expose ProfileInitStageFromAppInitStage(...)
    The function ProfileInitStageFromAppInitStage(...) will need to be
    used by both AppState and MainController so expose it publicly.
    
    Bug: 333863468
    Change-Id: Icfe544aab2e50eb863d3af63b45691b01ca58ec7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5853919
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355765}

--- ios/chrome/app/profile/profile_init_stage.h
+++ ios/chrome/app/profile/profile_init_stage.h
@@ -6,6 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_APP_PROFILE_PROFILE_INIT_STAGE_H_
 #define IOS_CHROME_APP_PROFILE_PROFILE_INIT_STAGE_H_
 
+// TODO(crbug.com/353683675): remove when profile init stage and app
+// init stage are fully separate.
+#include "ios/chrome/app/application_delegate/app_init_stage.h"
+
 // Profile initialisation stages. The app will go sequentially in-order through
 // each stage each time a new profile is added.
 enum class ProfileInitStage {
@@ -55,5 +59,10 @@ enum class ProfileInitStage {
   // Final stage, no transition until the profile is shut down.
   InitStageFinal
 };
+
+// Returns the equivalent ProfileInitStage from application InitStage.
+// TODO(crbug.com/353683675): remove when profile init stage and app
+// init stage are fully separate.
+ProfileInitStage ProfileInitStageFromAppInitStage(InitStage app_init_stage);
 
 #endif  // IOS_CHROME_APP_PROFILE_PROFILE_INIT_STAGE_H_
