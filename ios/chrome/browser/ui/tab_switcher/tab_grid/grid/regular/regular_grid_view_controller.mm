656b264d588e426493a5c71a0f3602b5b5638766
Filipa Senra <fsenra@google.com>
2024-09-16 16:52:02 +0000

    [iOS][QD] Refactor animation construction and add metric
    This CL refactors the animation construction code to be more efficient
    and adds a metric to monitor the delay of animation start.
    
    Bug: 337008330, 335387869, 365960324
    Change-Id: Icbc7ad65a00106c3c29027b250575cd519ca6a03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865473
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Sean Harrison <harrisonsean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355940}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/grid/regular/regular_grid_view_controller.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/apple/foundation_util.h"
 #import "base/functional/bind.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/task/sequenced_task_runner.h"
 #import "components/tab_groups/tab_group_id.h"
 #import "ios/chrome/browser/shared/model/web_state_list/tab_group.h"
@@ -115,6 +116,7 @@ NSArray<UIView*>* GetTabGroupViewsToAnimateClosure(
                                    groupsWithTabsToClose
                   allInactiveTabs:(BOOL)animateAllInactiveTabs
                 completionHandler:(ProceduralBlock)completionHandler {
+  base::Time startTime = base::Time::Now();
   NSMutableArray<UIView*>* gridCells = [[NSMutableArray alloc] init];
 
   for (NSIndexPath* path in self.collectionView.indexPathsForVisibleItems) {
@@ -122,20 +124,31 @@ NSArray<UIView*>* GetTabGroupViewsToAnimateClosure(
         [self.diffableDataSource itemIdentifierForIndexPath:path];
     UICollectionViewCell* collectionViewCell =
         [self.collectionView cellForItemAtIndexPath:path];
-    if (item.type == GridItemType::kTab &&
-        tabsToClose.contains(item.tabSwitcherItem.identifier)) {
-      [gridCells addObject:collectionViewCell];
-    } else if (item.type == GridItemType::kGroup &&
-               groupsWithTabsToClose.contains(
-                   item.tabGroupItem.tabGroup->tab_group_id())) {
-      [gridCells addObjectsFromArray:
-                     GetTabGroupViewsToAnimateClosure(
-                         ObjCCastStrict<GroupGridCell>(collectionViewCell),
-                         groupsWithTabsToClose[item.tabGroupItem.tabGroup
-                                                   ->tab_group_id()])];
-    } else if (item.type == GridItemType::kInactiveTabsButton &&
-               animateAllInactiveTabs) {
-      [gridCells addObject:collectionViewCell];
+
+    switch (item.type) {
+      case GridItemType::kTab:
+        if (tabsToClose.contains(item.tabSwitcherItem.identifier)) {
+          [gridCells addObject:collectionViewCell];
+        }
+        break;
+      case GridItemType::kGroup:
+        if (groupsWithTabsToClose.contains(
+                item.tabGroupItem.tabGroup->tab_group_id())) {
+          [gridCells addObjectsFromArray:
+                         GetTabGroupViewsToAnimateClosure(
+                             ObjCCastStrict<GroupGridCell>(collectionViewCell),
+                             groupsWithTabsToClose[item.tabGroupItem.tabGroup
+                                                       ->tab_group_id()])];
+        }
+        break;
+      case GridItemType::kInactiveTabsButton:
+        if (animateAllInactiveTabs) {
+          [gridCells addObject:collectionViewCell];
+        }
+        break;
+      case GridItemType::kSuggestedActions:
+        // No-op.
+        break;
     }
   }
 
@@ -146,6 +159,11 @@ NSArray<UIView*>* GetTabGroupViewsToAnimateClosure(
   [_tabsClosureAnimation animateWithCompletion:^{
     [weakSelf onTabsClosureAnimationEndWithCompletion:completionHandler];
   }];
+
+  base::TimeDelta delta = base::Time::Now() - startTime;
+  base::UmaHistogramMicrosecondsTimes(
+      "Privacy.DeleteBrowsingData.Duration.TabsClosureAnimationStartDelay",
+      delta);
 }
 
 #pragma mark - Parent's functions
