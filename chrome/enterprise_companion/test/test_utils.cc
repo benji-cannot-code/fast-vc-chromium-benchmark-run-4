4e9f16446c20033b3192a39ffbbdbff8c9bd76e8
Noah Rose Ledesma <noahrose@google.com>
2024-09-16 19:22:46 +0000

    Create an "install if needed" option for CECA
    Add a --install-if-needed switch to CECA, allowing the application to
    be installed if it is not already and the device requires management.
    
    Bug: 362267332
    Change-Id: I2f536d7bdd7618daf8c428b85223325177a3aec4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867613
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356063}

--- chrome/enterprise_companion/test/test_utils.cc
+++ chrome/enterprise_companion/test/test_utils.cc
@@ -41,6 +41,17 @@ constexpr char kTestExe[] = "enterprise_companion_test";
 constexpr char kTestExe[] = "enterprise_companion_test.exe";
 #endif
 
+void RunAppUnderTest(const std::string& switch_string) {
+  const base::FilePath test_exe_path =
+      base::PathService::CheckedGet(base::DIR_EXE).AppendASCII(kTestExe);
+  ASSERT_TRUE(base::PathExists(test_exe_path));
+
+  base::CommandLine command_line(test_exe_path);
+  command_line.AppendSwitch(switch_string);
+  base::Process installer_process = base::LaunchProcess(command_line, {});
+  ASSERT_EQ(WaitForProcess(installer_process), 0);
+}
+
 }  // namespace
 
 int WaitForProcess(base::Process& process) {
@@ -136,14 +147,11 @@ void TestMethods::ExpectInstalled() {
 }
 
 void TestMethods::Install() {
-  const base::FilePath test_exe_path =
-      base::PathService::CheckedGet(base::DIR_EXE).AppendASCII(kTestExe);
-  ASSERT_TRUE(base::PathExists(test_exe_path));
+  RunAppUnderTest(kInstallSwitch);
+}
 
-  base::CommandLine command_line(test_exe_path);
-  command_line.AppendSwitch(kInstallSwitch);
-  base::Process installer_process = base::LaunchProcess(command_line, {});
-  ASSERT_EQ(WaitForProcess(installer_process), 0);
+void TestMethods::InstallIfNeeded() {
+  RunAppUnderTest(kInstallIfNeededSwitch);
 }
 
 }  // namespace enterprise_companion
