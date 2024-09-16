0009fc3b894fe43f5836815ac957b128a3facfed
Mike Dougherty <michaeldo@chromium.org>
2024-09-16 16:51:26 +0000

    Wrap all exposed gCrWeb functions in `catchAndReportErrors`
    Bug: 40269960
    Change-Id: I67dd1ffba212d9711023f57758c4275e366bec94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5861673
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355939}

---
+++ ios/web/js_features/window_error/resources/wrap_gcrweb_functions.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {catchAndReportErrors} from '//ios/web/public/js_messaging/resources/error_reporting.js';
+import {gCrWeb} from '//ios/web/public/js_messaging/resources/gcrweb.js';
+
+for (const namespace in gCrWeb) {
+  const namespaceObject = gCrWeb[namespace];
+  for (const itemName in namespaceObject) {
+    const exposedItem = namespaceObject[itemName];
+    if (typeof exposedItem == 'function') {
+      gCrWeb[namespace][itemName] = function(...args: unknown[]) {
+        return catchAndReportErrors.apply(null, [exposedItem, args]);
+      }
+    }
+  }
+}
