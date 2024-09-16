715a7fb5f84073470594740934c780692797644e
Harsh Shukla <hvs@google.com>
2024-09-16 09:45:05 +0000

    Implementation of prefetching risk signals for latency optimization
    With https://chromium-review.googlesource.com/c/chromium/src/+/5598094
    Risk data will now be cached in chrome_autofill_client and prefetching it will cache the risk_data when credit card form is clicked.
    Design doc: go/pay-autofill-chrome-risk-data-latency-explorations-and-optimization
    
    Bug: 330282552
    Change-Id: I8bcc118d28af6922772579e09e5b25f1b6685add
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5466222
    Commit-Queue: Harsh Shukla <hvs@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1355752}

--- components/autofill/core/browser/payments/test_payments_autofill_client.cc
+++ components/autofill/core/browser/payments/test_payments_autofill_client.cc
@@ -46,6 +46,7 @@ TestPaymentsAutofillClient::~TestPaymentsAutofillClient() = default;
 
 void TestPaymentsAutofillClient::LoadRiskData(
     base::OnceCallback<void(const std::string&)> callback) {
+  risk_data_loaded_ = true;
   std::move(callback).Run("some risk data");
 }
 
