994a3edb404b88d48d812a5ac7fc95f7e947904e
Zachary Tan <tanzachary@chromium.org>
2024-09-16 18:47:55 +0000

    [FedCM] Record interaction UMA metrics for button mode pop-ups
    These UMA metrics are used to determine if the modal behind the pop-up
    is a distraction for users.
    
    Blink.FedCm.Button.LoginToIdpPopupInteraction
    Records whether the user lost focus of the pop-up and whether its the IDP or the user that closes the pop-up. Recorded once per login to IDP
    pop-up window closed in button mode.
    
    Blink.FedCm.Button.UseOtherAccountPopupInteraction
    Records whether the user lost focus of the pop-up and whether its the IDP or the user that closes the pop-up. Recorded once per use other account pop-up window closed in button mode.
    
    Bug: 362967502
    Change-Id: I02e0fe5a6f26316e538948dcf1dabbcd89a93f89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5852099
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356011}

--- chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
+++ chrome/browser/ui/views/webid/fedcm_account_selection_view_desktop.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 using DismissReason = content::IdentityRequestDialogController::DismissReason;
-
+using SheetType = AccountSelectionView::SheetType;
 
 // static
 int AccountSelectionView::GetBrandIconMinimumSize(
@@ -808,8 +808,11 @@ content::WebContents* FedCmAccountSelectionView::ShowModalDialog(
   // modal and direct user attention to the pop-up window. Note that this change
   // does not apply to other pop-up windows such as use other account, instead
   // they will be shown centred.
-  if (rp_mode == blink::mojom::RpMode::kButton && state_ == State::LOADING) {
-    popup_window_->SetCustomYPosition(web_contents()->GetViewBounds().y());
+  if (rp_mode == blink::mojom::RpMode::kButton) {
+    popup_window_->SetButtonModeSheetType(GetSheetType());
+    if (state_ == State::LOADING) {
+      popup_window_->SetCustomYPosition(web_contents()->GetViewBounds().y());
+    }
   }
 
   // The modal should not be hidden when the pop-up window is displayed for
@@ -917,7 +920,7 @@ bool FedCmAccountSelectionView::ShowVerifyingSheet(
   return true;
 }
 
-FedCmAccountSelectionView::SheetType FedCmAccountSelectionView::GetSheetType() {
+SheetType FedCmAccountSelectionView::GetSheetType() {
   switch (state_) {
     case State::IDP_SIGNIN_STATUS_MISMATCH:
       return SheetType::SIGN_IN_TO_IDP_STATIC;
