c94e19d4307605b4a0e0189ce382f8d499b7256d
Robbie Gibson <rkgibson@google.com>
2024-09-16 20:55:17 +0000

    Reland "[iOS] Improve Contextual Panel Price Insights gesture handling"
    This reverts commit c286281f2366ac8815e7c910a05b5256b38bd766.
    
    Reason for revert: Fixed compilation error by adding compile guard
    
    Original change's description:
    > Revert "[iOS] Improve Contextual Panel Price Insights gesture handling"
    >
    > This reverts commit fa2fd1598c52514b839a3288c1a6714747978f68.
    >
    > Reason for revert: This CL causes the following build error:
    >
    > price_history.swift:132:38: error: extra argument 'name' in call
    >       content.gesture(gesture, name: kPanelContentGestureRecognizerName)
    >                      ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > error: fatalError
    >
    >
    > Original change's description:
    > > [iOS] Improve Contextual Panel Price Insights gesture handling
    > >
    > > The old solution only worked on iOS 18. The new solution works on all
    > > iOS versions.
    > >
    > > There are 3 gestures interacting here: dragging on the graph, scrolling
    > > the panel, and dragging to expand the panel. The solution uses 2
    > > different methods to handle the graph drag's interaction with each of
    > > the other two gestures. First, vs scrolling the panel, using a
    > > DragGesture with a minimum distance allows the scrolling to supercede
    > > the drag if the user moves their finger vertically, but still allows the
    > > drag to go off if the user moves their finger horizontally. The scroll
    > > view looks like it requires the user to move their finger some short
    > > distance vertically before scrolling begins. So if the user moves
    > > vertically, the scroll gesture activates before the minimum distance is
    > > hit. But if the user moves horizontally, the graph's minimum distance
    > > is hit first, activating that one.
    > >
    > > For the interaction with the gesture to expand the sheet,
    > > UIGestureRecognizerDelegate methods are used to add a hierarchical
    > > relationship. This makes sure that the expansion gesture doesn't
    > > activate until after the graph drag gesture fails. It doesn't matter
    > > whether the graph gesture actually fails or not because the sheet
    > > expand gesture should never activate when dragging on the graph.
    > >
    > > See https://drive.google.com/file/d/15pXDKYzPr6wxnEWmovkrr1g6vBz2ZJGs/view?usp=sharing&resourcekey=0-ngQmDnHE7IJ1jZj8JsECow
    > > for a video of the behavior.
    > >
    > > Bug: 348218995
    > > Change-Id: I4955627633c46570377bdbac81ebb5ceeb8ae67b
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855024
    > > Reviewed-by: Guillaume Jenkins <gujen@google.com>
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Commit-Queue: Robbie Gibson <rkgibson@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1355492}
    >
    > Bug: 348218995
    > Change-Id: Ie8328582dad04829f10d1e7330e7a25ca386abc1
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5864350
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1355769}
    
    Bug: 348218995
    Change-Id: Id79f65f790594c8a4814df5e8905ba237bacf6b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866192
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356121}

---
+++ ios/chrome/browser/contextual_panel/ui/contextual_panel_view_constants.h
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_UI_CONTEXTUAL_PANEL_VIEW_CONSTANTS_H_
+#define IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_UI_CONTEXTUAL_PANEL_VIEW_CONSTANTS_H_
+
+#import <Foundation/Foundation.h>
+
+// Identifier for any gesture recognizer inside a panel content view.
+extern NSString* const kPanelContentGestureRecognizerName;
+
+#endif  // IOS_CHROME_BROWSER_CONTEXTUAL_PANEL_UI_CONTEXTUAL_PANEL_VIEW_CONSTANTS_H_
