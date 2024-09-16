fedbb1cc9cc99274f33c41c9eeb3330c7dfb9e28
Sandor Major <sandormajor@google.com>
2024-09-16 01:34:15 +0000

    [Popins] Run ReportBadMessage of CreateNewWindow on the correct receiver
    Without this the browser is crashing when
    `window.open("", "_blank", "popin")` is called.
    Error message: FrameHost::CreateNewWindowCallback was destroyed without
    first either being run or its corresponding binding being closed.
    
    Calling `ReportBadMessage()` on `frame_host_associated_receiver_ ` is
    crashing the renderer as expected but keeps the browser alive.
    
    In addition to this, in a followup CL, we will fix the renderer-side
    check to fail on "" url or open about:blank like regular popups do.
    
    Bug: 340606651
    Change-Id: I18c9d1b1472e4debcd0f0a0167bb9d5c90cafce8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854463
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Auto-Submit: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1355696}

--- content/browser/renderer_host/render_frame_host_impl.h
+++ content/browser/renderer_host/render_frame_host_impl.h
@@ -3186,6 +3186,7 @@ class CONTENT_EXPORT RenderFrameHostImpl
   friend class WebContentsSplitCacheBrowserTest;
   friend class RenderFrameHostManagerUnloadBrowserTest;
   friend class NavigationBrowserTest;
+  friend class FrameHostInterceptorForPopins;
 
   FRIEND_TEST_ALL_PREFIXES(NavigatorTest, TwoNavigationsRacingCommit);
   FRIEND_TEST_ALL_PREFIXES(RenderFrameHostImplBeforeUnloadBrowserTest,
