a260e9f638f829efa03b6362358924ec022e84a1
Hira Mahmood <hiramahmood@google.com>
2024-09-17 15:59:55 +0000

    [iOS] Prevent send tab infobar display when push notification is pressed
    The send tab infobar should not be presented if the corresponding iOS
    push notification is interacted with. There are two cases to consider:
    1) Notification interaction before infobar is added, and 2) notification
    interaction after infobar is added.
    
    This CL addresses the first case and prevents the infobar from
    displaying if the infobar has not yet been added.
    
    The second case is taken care of by opening the URL in a new tab (since
    the added infobar is scoped to the previous tab).
    
    The new SendTabToSelfModel API is used in this CL: crrev.com/c/5850376
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Bug: 343495218
    Change-Id: I86d69fa16704dcce6ad103cbc87b4b0b729e773a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850621
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356501}

--- components/send_tab_to_self/send_tab_to_self_bridge.h
+++ components/send_tab_to_self/send_tab_to_self_bridge.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
@@ -154,8 +155,17 @@ class SendTabToSelfBridge : public syncer::DataTypeSyncBridge,
 
   void DeleteAllEntries();
 
+  void EraseEntryInBatch(const std::string& guid,
+                         syncer::DataTypeStore::WriteBatch* batch);
+
   // |entries_| is keyed by GUIDs.
   SendTabToSelfEntries entries_;
+
+  // Stores guids of entries that have been opened from a layer other than
+  // SendTabToSelfModel. Once the bridge receives the respective entries, they
+  // will be marked opened. Entries are in-memory only and will be lost on
+  // browser restart.
+  base::flat_set<std::string> unknown_opened_entries_;
 
   // |clock_| isn't owned.
   const raw_ptr<const base::Clock> clock_;
