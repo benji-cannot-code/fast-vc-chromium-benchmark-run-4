b56b27edde5520ae50b87d043491dba983d0249a
Colin Blundell <blundell@chromium.org>
2024-09-17 14:18:59 +0000

    [Blink] Remove unneeded FinalizeFrame() calls in C2DLayerBridge test
    None of these calls are necessary for the tests to pass nor fundamental
    to the flow being tested.
    
    Bug: 40280152
    Change-Id: I5a5807600bc8d18c916a5183c49be7710b403a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869829
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356450}

--- third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge_test.cc
@@ -191,7 +191,6 @@ TEST_F(Canvas2DLayerBridgeTest, PrepareMailboxAndLoseResource) {
   {
     std::unique_ptr<Canvas2DLayerBridge> bridge =
         MakeBridge(gfx::Size(300, 150), RasterModeHint::kPreferGPU, kNonOpaque);
-    bridge->FinalizeFrame(FlushReason::kTesting);
     viz::TransferableResource resource;
     viz::ReleaseCallback release_callback;
     EXPECT_TRUE(Host()->PrepareTransferableResource(nullptr, &resource,
@@ -209,7 +208,6 @@ TEST_F(Canvas2DLayerBridgeTest, PrepareMailboxAndLoseResource) {
     {
       std::unique_ptr<Canvas2DLayerBridge> bridge = MakeBridge(
           gfx::Size(300, 150), RasterModeHint::kPreferGPU, kNonOpaque);
-      bridge->FinalizeFrame(FlushReason::kTesting);
       Host()->PrepareTransferableResource(nullptr, &resource,
                                           &release_callback);
       // |bridge| goes out of scope and would normally be destroyed, but
@@ -232,7 +230,6 @@ TEST_F(Canvas2DLayerBridgeTest, ReleaseCallbackWithNullContextProviderWrapper) {
   {
     std::unique_ptr<Canvas2DLayerBridge> bridge =
         MakeBridge(gfx::Size(300, 150), RasterModeHint::kPreferGPU, kNonOpaque);
-    bridge->FinalizeFrame(FlushReason::kTesting);
     EXPECT_TRUE(Host()->PrepareTransferableResource(nullptr, &resource,
                                                     &release_callback));
   }
