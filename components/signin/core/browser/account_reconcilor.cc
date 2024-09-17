fe847404041454abe67921e0c103afd9e4df6b09
Monica Basta <msalama@chromium.org>
2024-09-17 15:29:02 +0000

    [Uno-D] Fix Token not removed when signing out from pending state
    On signout, we rely on Gaia cookie update to trigger the reconcilor and
    revoke refresh tokens.
    
    In case of a pending state, Gaia cookies might be empty, therefore a
    signout will remove the primary account but would not affect the cookies
    nor trigger the reconcilor. This result in inconsistency between tokens
    and cookies.
    
    This CL triggers a reconciliation cycle on primary account cleared.
    
    Bug: 367066324
    Change-Id: Iaa1a58cc172138c2393b8dd8348a0e0ff7e40850
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870051
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356482}

--- components/signin/core/browser/account_reconcilor.cc
+++ components/signin/core/browser/account_reconcilor.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/signin/core/browser/account_reconcilor_delegate.h"
 #include "components/signin/public/base/account_consistency_method.h"
+#include "components/signin/public/base/consent_level.h"
 #include "components/signin/public/base/signin_client.h"
 #include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_pref_names.h"
@@ -384,6 +385,13 @@ void AccountReconcilor::OnPrimaryAccountChanged(
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
   // Perform the "clear on exit" migration if applicable.
   MaybeMigrateClearOnExit(*client_, *identity_manager_);
+
+  if (switches::IsExplicitBrowserSigninUIOnDesktopEnabled() &&
+      event_details.GetEventTypeFor(ConsentLevel::kSignin) ==
+          signin::PrimaryAccountChangeEvent::Type::kCleared) {
+    VLOG(1) << "AccountReconcilor::OnPrimaryAccountChanged";
+    StartReconcile(Trigger::kPrimaryAccountChanged);
+  }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 }
 
--- components/signin/core/browser/account_reconcilor.h
+++ components/signin/core/browser/account_reconcilor.h
@@ -249,6 +249,8 @@ class AccountReconcilor
                            DeleteCookieForSignedInUser);
   FRIEND_TEST_ALL_PREFIXES(AccountReconcilorDiceTestWithUnoDesktop,
                            DeleteCookieForSyncingUser);
+  FRIEND_TEST_ALL_PREFIXES(AccountReconcilorDiceTestWithUnoDesktop,
+                           PendingStateThenClearPrimaryAccount);
   FRIEND_TEST_ALL_PREFIXES(AccountReconcilorMirrorTest, TokensNotLoaded);
   FRIEND_TEST_ALL_PREFIXES(AccountReconcilorMirrorTest,
                            StartReconcileCookiesDisabled);
@@ -340,6 +342,8 @@ class AccountReconcilor
   // Event triggering a call to StartReconcile().
   // These values are persisted to logs. Entries should not be renumbered and
   // numeric values should never be reused.
+  //
+  // LINT.IfChange(Trigger)
   enum class Trigger {
     kInitialized = 0,
     kTokensLoaded = 1,
@@ -350,9 +354,11 @@ class AccountReconcilor
     kCookieChange = 6,
     kCookieSettingChange = 7,
     kForcedReconcile = 8,
+    kPrimaryAccountChanged = 9,
 
-    kMaxValue = kForcedReconcile
+    kMaxValue = kPrimaryAccountChanged
   };
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/signin/enums.xml:SigninReconcilerTrigger)
 
   void set_timer_for_testing(std::unique_ptr<base::OneShotTimer> timer);
 
