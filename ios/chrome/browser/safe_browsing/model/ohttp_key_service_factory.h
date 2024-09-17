a1cffff9f44961759ef4180c2565c2738d4a08bf
Sylvain Defresne <sdefresne@chromium.org>
2024-09-17 11:21:29 +0000

    [ios] Fix OhttpKeyServiceFactory
    The factory OhttpKeyServiceFactory was declared to take
    a web::BrowserState* which is incorrect, all KeyedServiceFactory in
    //ios/chrome/browser must take a ProfileIOS (or ChromeBrowserState
    historically).
    
    Fix OhttpKeyServiceFactory to take the parameter as the correct type
    and fix client code.
    
    Also remove OhttpKeyServiceAllowerForTesting and instead expose the
    recommended way to using a service in test via GetDefaultFactory()
    method.
    
    Bug: none
    Change-Id: Iccccf733b9b976a365b339d8d7dc17403106eddd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865356
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356400}

--- ios/chrome/browser/safe_browsing/model/ohttp_key_service_factory.h
+++ ios/chrome/browser/safe_browsing/model/ohttp_key_service_factory.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 class KeyedService;
 
@@ -15,21 +16,24 @@ namespace safe_browsing {
 class OhttpKeyService;
 }
 
-namespace web {
-class BrowserState;
-}
-
 // Singleton that owns OhttpKeyService objects, one for each active
 // BrowserState. It returns nullptr for incognito BrowserStates.
 class OhttpKeyServiceFactory : public BrowserStateKeyedServiceFactory {
  public:
-  // Returns the instance of OhttpKeyService associated with this browser state,
+  // Returns the instance of OhttpKeyService associated with this profile,
   // creating one if none exists.
+  static safe_browsing::OhttpKeyService* GetForProfile(ProfileIOS* profile);
+
+  // Deprecated: use GetForProfile(...).
   static safe_browsing::OhttpKeyService* GetForBrowserState(
-      web::BrowserState* browser_state);
+      ProfileIOS* profile);
 
   // Returns the singleton instance of OhttpKeyServiceFactory.
   static OhttpKeyServiceFactory* GetInstance();
+
+  // Returns the default factory. Can be used to force instantiation during
+  // testing.
+  static TestingFactory GetDefaultFactory();
 
   OhttpKeyServiceFactory(const OhttpKeyServiceFactory&) = delete;
   OhttpKeyServiceFactory& operator=(const OhttpKeyServiceFactory&) = delete;
@@ -45,24 +49,6 @@ class OhttpKeyServiceFactory : public BrowserStateKeyedServiceFactory {
       web::BrowserState* browser_state) const override;
   bool ServiceIsCreatedWithBrowserState() const override;
   bool ServiceIsNULLWhileTesting() const override;
-
-  static std::optional<std::string> GetCountry();
-};
-
-// Used only for tests. By default, the OHTTP key service is null for tests,
-// since when it's created it tries to fetch the OHTTP key, which can cause
-// errors for unrelated tests. To allow the OHTTP key service in tests, create
-// an object of this type and keep it in scope for as long as the override
-// should exist. The constructor will set the override, and the destructor will
-// clear it.
-class OhttpKeyServiceAllowerForTesting {
- public:
-  OhttpKeyServiceAllowerForTesting();
-  OhttpKeyServiceAllowerForTesting(const OhttpKeyServiceAllowerForTesting&) =
-      delete;
-  OhttpKeyServiceAllowerForTesting& operator=(
-      const OhttpKeyServiceAllowerForTesting&) = delete;
-  ~OhttpKeyServiceAllowerForTesting();
 };
 
 #endif  // IOS_CHROME_BROWSER_SAFE_BROWSING_MODEL_OHTTP_KEY_SERVICE_FACTORY_H_
