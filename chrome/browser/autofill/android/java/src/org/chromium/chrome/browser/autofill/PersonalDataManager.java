d3a8533e8a15ad6172b9108b52c897b82b9be519
Verina Armanyous <averina@google.com>
2024-09-17 17:56:35 +0000

    Rename isAutofillCreditCardEnabled methods to AutofillPaymentMethod
    Rename isAutofillCreditCardEnabled methods to isAutofillPaymentMethodEnabled since this pref is now used for both
    credit cards and IBANs.
    
    Note that the pref name remains AUTOFILL_CREDIT_CARD_ENABLED
    because it's more complicated to change pref names, so only methods
    names are renamed in this CL.
    
    Bug: 40903277
    Change-Id: I8abd1654c84535ef25333ce35106edaa83369f1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5833490
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356598}

--- chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java
@@ -1056,7 +1056,7 @@ public class PersonalDataManager implements Destroyable {
      * @return Whether FIDO authentication is available.
      */
     public boolean isFidoAuthenticationAvailable() {
-        return isAutofillCreditCardEnabled()
+        return isAutofillPaymentMethodsEnabled()
                 && PersonalDataManagerJni.get()
                         .isFidoAuthenticationAvailable(mPersonalDataManagerAndroid);
     }
@@ -1069,9 +1069,10 @@ public class PersonalDataManager implements Destroyable {
     }
 
     /**
-     * @return Whether the Autofill feature for Credit Cards is enabled.
+     * @return Whether the Autofill feature for Payment Methods is enabled.
      */
-    public boolean isAutofillCreditCardEnabled() {
+    public boolean isAutofillPaymentMethodsEnabled() {
+        // TODO(crbug.com/40903277): Rename pref to AUTOFILL_PAYMENT_METHODS_ENABLED.
         return mPrefService.getBoolean(Pref.AUTOFILL_CREDIT_CARD_ENABLED);
     }
 
