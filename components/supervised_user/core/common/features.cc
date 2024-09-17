dc267a09114ecb08f34d09bef902314fb6b03319
Duong Dac <ddac@google.com>
2024-09-17 23:15:14 +0000

    Add feature flag for supervised user subframe re-auth interstitials
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    Bug: 367642943
    Change-Id: I5124526be57c52970f0218609712c17fda5d178f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870055
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356798}

--- components/supervised_user/core/common/features.cc
+++ components/supervised_user/core/common/features.cc
@@ -131,6 +131,10 @@ BASE_FEATURE(kForceSupervisedUserReauthenticationForYouTube,
 BASE_FEATURE(kForceSupervisedUserReauthenticationForBlockedSites,
              "ForceSupervisedUserReauthenticationForBlockedSites",
              base::FEATURE_DISABLED_BY_DEFAULT);
+
+BASE_FEATURE(kAllowSupervisedUserReauthenticationForSubframes,
+             "EnableSupervisedUserReauthenticationForSubframes",
+             base::FEATURE_DISABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kUncredentialedFilteringFallbackForSupervisedUsers,
