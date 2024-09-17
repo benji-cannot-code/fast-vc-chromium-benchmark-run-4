27a2940a7ae88e346d12f44325e02d30e1ec506f
Chris Fredrickson <cfredric@chromium.org>
2024-09-17 14:06:47 +0000

    Use base::optional_ref<T> instead of std::optional<T>&
    std::optional<T> is a value that might absent, whereas
    base::optional_ref<T> is a reference that might be absent. Thus, use
    of std::optional<T> results in an unnecessary copy when implicitly
    converting from a T&; base::optional_ref avoids that copy.
    
    (A function parameter of type base::optional_ref<T> is similar to a
    parameter of type T*, except that the base::optional_ref allows the
    use of temporaries.)
    
    Change-Id: Ib627b18105e6a6d18d892f4731f5049b183a9df0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866870
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: Aaron Selya <selya@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356442}

--- components/content_settings/core/common/cookie_settings_base.h
+++ components/content_settings/core/common/cookie_settings_base.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/containers/fixed_flat_set.h"
+#include "base/types/optional_ref.h"
 #include "components/content_settings/core/common/content_settings.h"
 #include "net/cookies/cookie_constants.h"
 #include "net/cookies/cookie_setting_override.h"
@@ -234,7 +235,7 @@ class CookieSettingsBase {
   bool IsFullCookieAccessAllowed(
       const GURL& url,
       const net::SiteForCookies& site_for_cookies,
-      const std::optional<url::Origin>& top_frame_origin,
+      base::optional_ref<const url::Origin> top_frame_origin,
       net::CookieSettingOverrides overrides,
       CookieSettingWithMetadata* cookie_settings = nullptr) const;
 
@@ -335,7 +336,7 @@ class CookieSettingsBase {
   std::optional<net::cookie_util::StorageAccessStatus> GetStorageAccessStatus(
       const GURL& url,
       const net::SiteForCookies& site_for_cookies,
-      const std::optional<url::Origin>& top_frame_origin,
+      base::optional_ref<const url::Origin> top_frame_origin,
       net::CookieSettingOverrides overrides) const;
 
  protected:
