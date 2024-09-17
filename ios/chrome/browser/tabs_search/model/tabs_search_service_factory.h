ccf0a0ef4c383d097c89e66e4390f0e5f1201527
Mark Cogan <marq@google.com>
2024-09-17 18:01:46 +0000

    [iOS] Updates files under ios/chrome/browser/tabs_search
    This CL replaces:
    * ChromeBrowserState => ProfileIOS
    * GetBrowserState => GetProfile
    * browser_state => profile
    * browserState => profile
    * GetOriginalChromeBrowserState => GetOriginalProfile
    * HasOffTheRecordChromeBrowserState => HasOffTheRecordProfile
    * GetOffTheRecordChromeBrowserState => GetOffTheRecordProfile
    * DestroyOffTheRecordChromeBrowserState => DestroyOffTheRecordProfile
    
    web::BrowserState code is not updated.
    
    Bug: 365532923
    Change-Id: I4b7cf14dab67321fa0c2805e7c33d9bafd258336
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867977
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356601}

--- ios/chrome/browser/tabs_search/model/tabs_search_service_factory.h
+++ ios/chrome/browser/tabs_search/model/tabs_search_service_factory.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TabsSearchService;
 
 // Singleton that owns all TabsSearchServices and associates them with
-// ChromeBrowserState.
+// ProfileIOS.
 class TabsSearchServiceFactory : public BrowserStateKeyedServiceFactory {
  public:
   TabsSearchServiceFactory(const TabsSearchServiceFactory&) = delete;
