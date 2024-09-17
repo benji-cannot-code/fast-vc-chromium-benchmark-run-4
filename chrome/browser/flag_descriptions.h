edf204ad7c2167611b033e74636a9bf054aea7b0
Bo Majewski <majewski@chromium.org>
2024-09-17 00:44:50 +0000

    [Files]: Removes the flag that used to control FSP in Recent
    The fsps-in-recents used to control whether or not the recent model was
    looking for recent files among file system provided volumes. This was
    enabled by default in M124. Dependencies on this flag have been removed
    in 130.0.6674.0 (http://crrev.com/c/5802925). This completely removes
    the flag for M131.
    
    Bug: b:361122156
    Change-Id: I757510cce0f5508439108e0ae01b39f467961dea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5828736
    Reviewed-by: Alexander Bolodurin <alexbn@chromium.org>
    Commit-Queue: Bo Majewski <majewski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356227}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4058,9 +4058,6 @@ extern const char kFocusModeYTMDescription[];
 extern const char kForceReSyncDriveName[];
 extern const char kForceReSyncDriveDescription[];
 
-extern const char kFSPsInRecentsName[];
-extern const char kFSPsInRecentsDescription[];
-
 extern const char kFuseBoxDebugName[];
 extern const char kFuseBoxDebugDescription[];
 
