61e22becacc611d9234e4bed14f75167eb7b039c
CJ Huang <chenjih@google.com>
2024-09-17 04:30:48 +0000

    sparky: adds history screenshot.
    This CL modifies the proto to match the change of cl/671247418, and move
    the dialog history from SparkyManagerImpl to SparkyProvider to make the
    screenshot attach possible.
    
    This CL only contains the minimal change required to make history
    screenshot working. DialogTurn together with its conversion functions
    will be removed in a follow-up CL so that only proto is used for
    sparky cache.
    
    Bug: b/365674359
    Low-Coverage-Reason: EXPERIMENTAL_CODE experimental feature
    Change-Id: Ic57e26efe3a78a59caadf6186bcca1a90ab40ffa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856003
    Reviewed-by: Xinglong Luan <alanlxl@chromium.org>
    Reviewed-by: Tony Yeoman <tby@chromium.org>
    Commit-Queue: CJ Huang <chenjih@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356272}

--- components/manta/sparky/sparky_context.cc
+++ components/manta/sparky/sparky_context.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace manta {
 
-SparkyContext::SparkyContext(const std::vector<DialogTurn>& dialog)
-    : dialog(dialog) {}
+SparkyContext::SparkyContext(const DialogTurn& latest_turn)
+    : latest_turn(latest_turn) {}
 
-SparkyContext::SparkyContext(const std::vector<DialogTurn>& dialog,
+SparkyContext::SparkyContext(const DialogTurn& latest_turn,
                              const std::string& page_content)
-    : dialog(dialog), page_content(page_content) {}
+    : latest_turn(latest_turn), page_content(page_content) {}
 
 SparkyContext::~SparkyContext() = default;
 
