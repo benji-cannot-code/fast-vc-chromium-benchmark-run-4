778b197cf0ecf8e6b3a050dc4f99f8a2c1f38f5a
Arthur Sonzogni <arthursonzogni@chromium.org>
2024-09-17 16:15:31 +0000

    Fuchsia: Fix use-after-dtor
    This test doesn't pass the ASAN `-fsanitize-memory-use-after-dtor`
    check.
    
    This was found independently by the pending MiraclePtr rewrite:
    https://chromium-review.googlesource.com/c/chromium/src/+/5854176
    
    StackTrace in BrowserAccessibilityManagerFuchsiaTest.TestDeleteNodes
    1. BrowserAccessibilityManagerFuchsia::GetAccessibilityBridge()
    2. BrowserAccessibilityFuchsia::GetAccessibilityBridge()
    3. BrowserAccessibilityFuchsia::DeleteNode()
    4. BrowserAccessibilityFuchsia::~BrowserAccessibilityFuchsia()
    5. BrowserAccessibilityFuchsia::~BrowserAccessibilityFuchsia()
    6. BrowserAccessibilityManager::~BrowserAccessibilityManager()
    7. BrowserAccessibilityManagerFuchsia::~BrowserAccessibilityManagerFuchsia()
    
    Since the parent destructor is calling its "already" deleted child, this
    is C++ undefined behavior. They could be surprising for developers.
    
    This "resolve" the issue by moving the accessed variable outside of the
    class, and adding a comment to warn future developers this function
    shouldn't depend on `this`.
    
    Bug: 366045345
    Change-Id: I035cd54b29eb35408f6c3d088ada089f5a0a201e
    AX-Relnotes: n/a.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865471
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356506}

--- ui/accessibility/platform/fuchsia/browser_accessibility_manager_fuchsia.cc
+++ ui/accessibility/platform/fuchsia/browser_accessibility_manager_fuchsia.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/platform/fuchsia/browser_accessibility_fuchsia.h"
 
 namespace ui {
+namespace {
+// Accessibility bridge instance to use for tests, if set.
+AccessibilityBridgeFuchsia* g_accessibility_bridge_for_test = nullptr;
+}  // namespace
 
 // static
 BrowserAccessibilityManager* BrowserAccessibilityManager::Create(
@@ -68,8 +72,16 @@ BrowserAccessibilityManagerFuchsia::~BrowserAccessibilityManagerFuchsia() =
 
 AccessibilityBridgeFuchsia*
 BrowserAccessibilityManagerFuchsia::GetAccessibilityBridge() const {
-  if (accessibility_bridge_for_test_)
-    return accessibility_bridge_for_test_;
+  // !!! Safety warning !!! This function is directly called during the
+  // parent class destructor. As such, it shouldn't depend on any member
+  // variables from this class, as they have already been destroyed.
+  //
+  // Failing to follow this rule would be undefined behavior, and can lead to
+  // unsafe/unexpected behaviors. See ASAN options
+  // `-fsanitize-memory-use-after-dtor`
+  if (g_accessibility_bridge_for_test) {
+    return g_accessibility_bridge_for_test;
+  }
 
   gfx::NativeWindow top_level_native_window =
       delegate_ ? delegate_->GetTopLevelNativeWindow() : gfx::NativeWindow();
@@ -160,9 +172,14 @@ void BrowserAccessibilityManagerFuchsia::UpdateDeviceScaleFactor() {
     BrowserAccessibilityManager::UpdateDeviceScaleFactor();
 }
 
+// static
 void BrowserAccessibilityManagerFuchsia::SetAccessibilityBridgeForTest(
     AccessibilityBridgeFuchsia* accessibility_bridge_for_test) {
-  accessibility_bridge_for_test_ = accessibility_bridge_for_test;
+  // Only allow transition from nullptr to non-nullptr, or vice versa.
+  CHECK(!g_accessibility_bridge_for_test || !accessibility_bridge_for_test)
+      << "Setting the accessibility bridge to two different values is not "
+         "allowed.";
+  g_accessibility_bridge_for_test = accessibility_bridge_for_test;
 }
 
 }  // namespace ui
