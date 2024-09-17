dbd5813569a2112fa11b68ce436d1004662ae26d
Sylvain Defresne <sdefresne@chromium.org>
2024-09-17 09:42:58 +0000

    [ios] Fix DomainDiversityReporterFactory
    The factory DomainDiversityReporterFactory was declared to take
    a web::BrowserState* which is incorrect, all KeyedServiceFactory in
    //ios/chrome/browser must take a ProfileIOS (or ChromeBrowserState
    historically).
    
    Fix DomainDiversityReporterFactory to take the parameter as the
    correct type and fix client code.
    
    Bug: none
    Low-Coverage-Reason: Refactoring existing untested code.
    Change-Id: I52454aeadf1e388b448d49ca80b1051808763a89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865113
    Auto-Submit: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356369}

--- ios/chrome/browser/history/model/domain_diversity_reporter_factory.h
+++ ios/chrome/browser/history/model/domain_diversity_reporter_factory.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/no_destructor.h"
 #include "components/keyed_service/ios/browser_state_keyed_service_factory.h"
+#include "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 class DomainDiversityReporter;
 
@@ -17,16 +18,13 @@ namespace user_prefs {
 class PrefRegistrySyncable;
 }
 
-namespace web {
-class BrowserState;
-}
-
 // Singleton that creates all DomainDiversityReporter instances and associates
 // them with BrowserState.
 class DomainDiversityReporterFactory : public BrowserStateKeyedServiceFactory {
  public:
-  static DomainDiversityReporter* GetForBrowserState(
-      web::BrowserState* browser_state);
+  static DomainDiversityReporter* GetForProfile(ProfileIOS* profile);
+  // Deprecated: use GetForProfile(...).
+  static DomainDiversityReporter* GetForBrowserState(ProfileIOS* profile);
 
   static DomainDiversityReporterFactory* GetInstance();
 
