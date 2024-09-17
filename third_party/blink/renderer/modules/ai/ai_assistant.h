a6e92af255d782b436eef0e23a134b0cf56cef77
Lei Mingyu <leimy@chromium.org>
2024-09-17 06:13:51 +0000

    Prompt API: fix crash when getting attributes after destroy the session
    Instead of getting the information from the underlying text_session
    every time, we store the info in the assistant when it's set to the
    text_session.
    
    Bug: 366098079
    Change-Id: I1779131a5208a0a87dc7bf905944cb33ac241e6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856859
    Reviewed-by: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356292}

--- third_party/blink/renderer/modules/ai/ai_assistant.h
+++ third_party/blink/renderer/modules/ai/ai_assistant.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_MODULES_AI_AI_ASSISTANT_H_
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_AI_AI_ASSISTANT_H_
 
+#include "third_party/blink/public/mojom/ai/ai_assistant.mojom-blink-forward.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/core/streams/readable_stream.h"
@@ -34,12 +35,12 @@ class AIAssistant final : public ScriptWrappable,
   ReadableStream* promptStreaming(ScriptState* script_state,
                                   const WTF::String& input,
                                   ExceptionState& exception_state);
-  uint64_t maxTokens() const;
-  uint64_t tokensSoFar() const;
-  uint64_t tokensLeft() const;
+  uint64_t maxTokens() const { return max_tokens_; }
+  uint64_t tokensSoFar() const { return current_tokens_; }
+  uint64_t tokensLeft() const { return max_tokens_ - current_tokens_; }
 
-  uint32_t topK() const;
-  float temperature() const;
+  uint32_t topK() const { return top_k_; }
+  float temperature() const { return temperature_; }
 
   ScriptPromise<AIAssistant> clone(ScriptState* script_state,
                                    ExceptionState& exception_state);
@@ -47,8 +48,16 @@ class AIAssistant final : public ScriptWrappable,
 
  private:
   void OnResponseComplete(std::optional<uint64_t> current_tokens);
+  // If the `AIAssistant` is created by the factory, the `text_session_` should
+  // have the up-to-date info, and this will be called from the constructor.
+  // If the `AIAssistant` is created by cloning another `AIAssistant`, this will
+  // be set later by the `clone()` method.
+  void SetInfo(const blink::mojom::blink::AIAssistantInfoPtr info);
 
   uint64_t current_tokens_ = 0;
+  uint64_t max_tokens_ = 0;
+  uint32_t top_k_ = 0;
+  float temperature_ = 0.0;
 
   Member<AITextSession> text_session_;
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
