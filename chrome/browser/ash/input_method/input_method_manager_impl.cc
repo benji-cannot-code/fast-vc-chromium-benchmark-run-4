6e1ca54f004e234e5ee6e2d854bce5354ca99353
Darren Shen <shend@chromium.org>
2024-09-17 03:27:52 +0000

    Remove unused dep.
    Bug: b:360229206
    Change-Id: I4e48dfed17e66d05f72dd2b78a7de0a4f2ac5a43
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868749
    Auto-Submit: Darren Shen <shend@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356259}

--- chrome/browser/ash/input_method/input_method_manager_impl.cc
+++ chrome/browser/ash/input_method/input_method_manager_impl.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/trace_event/trace_event.h"
 #include "chrome/browser/ash/input_method/assistive_window_controller.h"
 #include "chrome/browser/ash/input_method/candidate_window_controller.h"
-#include "chrome/browser/ash/login/session/user_session_manager.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browser_process_platform_part_ash.h"
 #include "chrome/browser/lifetime/termination_notification.h"
