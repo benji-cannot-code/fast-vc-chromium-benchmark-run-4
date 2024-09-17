bebf1bb4130dc16d94fe1981b918c1ea86eee48b
Ryan Sultanem <rsult@google.com>
2024-09-17 18:36:13 +0000

    [BatchUpload] Implement web ui view
    Core implementation of the HTML/TS code. Uses dummy value for display
    currently. Data is hardcoded in the typescript function where we expect
    later to receive the data from the browser.
    Strings in the dialog are also hardcoded for now.
    
    There may still be some issues to be tackled in follow up CLs:
    - Multiple TODO were added in the code for followup CLs.
    - No specific code was added for Dark theme yet (if needed).
    - Output logic management.
    - Issues with updates/initial display.
    
    Screenshots:
    - Mock expanded: https://screenshot.googleplex.com/5HYHHZyEpUNVy2B
    - Mock collapsed: https://screenshot.googleplex.com/BxuYHZqWhxLgTWT
    - Impl expanded: https://screenshot.googleplex.com/5fx3rTD5Skcf2Yz
    - Impl collapsed: https://screenshot.googleplex.com/48MJWcGwDvov2fD
    
    Bug: b:359796914
    Change-Id: I10ec1b556fdfa831fd65ed0fc65773649ab40d86
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5824638
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356624}

---
+++ chrome/test/data/webui/signin/test_batch_upload_browser_proxy.ts
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PageCallbackRouter} from 'chrome://batch-upload/batch_upload.js';
+import type {BatchUploadBrowserProxy, PageHandlerInterface} from 'chrome://batch-upload/batch_upload.js';
+import {TestBrowserProxy} from 'chrome://webui-test/test_browser_proxy.js';
+
+class TestBatchUploadHandler extends TestBrowserProxy implements
+    PageHandlerInterface {
+  constructor() {
+    super([
+      'updateViewHeight',
+      'close',
+    ]);
+  }
+
+  updateViewHeight(height: number) {
+    this.methodCalled('updateViewHeight', height);
+  }
+
+  close() {
+    this.methodCalled('close');
+  }
+}
+
+export class TestBatchUploadBrowserProxy implements BatchUploadBrowserProxy {
+  callbackRouter: PageCallbackRouter = new PageCallbackRouter();
+  handler: TestBatchUploadHandler = new TestBatchUploadHandler();
+}
