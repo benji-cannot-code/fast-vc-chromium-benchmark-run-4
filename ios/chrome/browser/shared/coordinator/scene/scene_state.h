63271222ff87effdd2829f16c5f79b43e548dc88
Sylvain Defresne <sdefresne@chromium.org>
2024-09-17 14:36:32 +0000

    [ios] Connect ProfileState to SceneState
    Connect the ProfileState to use for each SceneState when the app is
    ready to use the profile and to present the scene. This allow each
    scene to use a different profile (tough only appState.mainProfile
    will be correctly initialized).
    
    Bug: 333863468
    Change-Id: Ia42c1ff6c96144fcac732571c5d72343cd1772d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854701
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356457}

--- ios/chrome/browser/shared/coordinator/scene/scene_state.h
+++ ios/chrome/browser/shared/coordinator/scene/scene_state.h
@@ -14,9 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/window_activities/model/window_activity_helpers.h"
 
 @class AppState;
+@protocol BrowserProviderInterface;
+@class ProfileState;
 @class SceneController;
 @class SceneState;
-@protocol BrowserProviderInterface;
 
 // Scene agents are objects owned by a scene state and providing some
 // scene-scoped function. They can be driven by SceneStateObserver events.
@@ -39,6 +40,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // The app state for the app that owns this scene. Set in init.
 @property(nonatomic, weak, readonly) AppState* appState;
+
+// The profile state for profile that owns this scene.
+@property(nonatomic, weak) ProfileState* profileState;
 
 // The current activation level.
 @property(nonatomic, assign) SceneActivationLevel activationLevel;
