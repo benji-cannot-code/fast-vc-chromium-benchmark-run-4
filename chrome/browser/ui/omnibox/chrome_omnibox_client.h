1ef55764af7fe0ae265f5cf64266cd06e0530aec
Nicolás Peña <npm@chromium.org>
2024-09-17 15:04:47 +0000

    Revert "Hide Toast when Omnibox is focused"
    This reverts commit 971c86a85e2493b00979a2fd5c5e62cd0db8d04c.
    
    Reason for revert: likely cause of Linux CFI failures at
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20CFI
    
    Original change's description:
    > Hide Toast when Omnibox is focused
    >
    > Currently, when the omnibox is focused as we are showing the toast,
    > the part of the toast that overlaps with the omnibox is showing
    > even when the rest of the toast is hidden behind the omnibox search
    > results.
    >
    > This CL makes it so the toast will hide while the omnibox is focused
    > and show the toast if the omnibox loses focus.
    >
    > Fixed: 364731409
    > Change-Id: I46e5ad40ff37cf8face0262dcf2480993c4176d6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5842614
    > Commit-Queue: Steven Luong <stluong@chromium.org>
    > Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    > Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1356215}
    
    Change-Id: Ie196f9bc41a3c16dd91ed1dcb5d602bd51c1a8f2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867495
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Nicolás Peña <npm@chromium.org>
    Auto-Submit: Nicolás Peña <npm@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356471}

--- chrome/browser/ui/omnibox/chrome_omnibox_client.h
+++ chrome/browser/ui/omnibox/chrome_omnibox_client.h
@@ -116,7 +116,7 @@ class ChromeOmniboxClient final : public OmniboxClient {
       const AutocompleteMatch& alternative_nav_match,
       IDNA2008DeviationCharacter deviation_char_in_hostname) override;
   void OnInputInProgress(bool in_progress) override;
-  void OnPopupVisibilityChanged(bool popup_is_open) override;
+  void OnPopupVisibilityChanged() override;
   void OpenIphLink(GURL gurl) override;
   base::WeakPtr<OmniboxClient> AsWeakPtr() override;
 
