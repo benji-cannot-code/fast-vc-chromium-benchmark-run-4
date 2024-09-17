23be766c271329265618739c7c94f68dfbf85f3b
Tsuyoshi Horo <horo@chromium.org>
2024-09-17 02:10:46 +0000

    Revert "Rename SafeBrowsingService to SafeBrowsingServiceImpl"
    This reverts commit 0600e35e79a22fad6a38395f16a9a27716ea7835.
    
    Reason for revert: Caused failure on chromium/ci/linux-ubsan-vptr bot.
    Bug: 367498284
    
    Original change's description:
    > Rename SafeBrowsingService to SafeBrowsingServiceImpl
    >
    > This CL renames the existing class to SafeBrowsingServiceImpl in
    > anticipation of making SafeBrowsingServiceInterface called
    > SafeBrowsingService.
    >
    > Future CLs will improve the interface until we no longer need the
    > circular dependencies, then rename the service.
    >
    > Bug: 41437292
    > Change-Id: I815a784ac8b4a4887b003d59dae82eec54fe423d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5862165
    > Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    > Commit-Queue: Daniel Rubery <drubery@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1356155}
    
    Bug: 41437292
    Change-Id: I4770f9e979cce9d6906bf7f26fe0acf25ec146f7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869211
    Auto-Submit: Tsuyoshi Horo <horo@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Tsuyoshi Horo <horo@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356244}

--- chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
+++ chrome/browser/safe_browsing/incident_reporting/incident_reporting_service.cc
@@ -315,7 +315,7 @@ bool IncidentReportingService::IsEnabledForProfile(Profile* profile) {
 }
 
 IncidentReportingService::IncidentReportingService(
-    SafeBrowsingServiceImpl* safe_browsing_service)
+    SafeBrowsingService* safe_browsing_service)
     : IncidentReportingService(safe_browsing_service,
                                base::Milliseconds(kDefaultCallbackIntervalMs),
                                GetBackgroundTaskRunner()) {
@@ -391,7 +391,7 @@ void IncidentReportingService::AddDownloadManager(
 }
 
 IncidentReportingService::IncidentReportingService(
-    SafeBrowsingServiceImpl* safe_browsing_service,
+    SafeBrowsingService* safe_browsing_service,
     base::TimeDelta delayed_task_interval,
     const scoped_refptr<base::TaskRunner>& delayed_task_runner)
     : url_loader_factory_(nullptr),
