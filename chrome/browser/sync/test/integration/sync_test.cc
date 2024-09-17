606c8d3c061d82b2a29286b81f35d9528ac36fa7
Boris Sazonov <bsazonov@chromium.org>
2024-09-17 14:50:48 +0000

    [Android] Rename SetUp/TearDown methods in SyncTestSigninUtils
    sync_test_utils_android::{SetUpAuthForTesting, TearDownAuthForTesting}
    interact with the fake sign-in infra that uses fake identities, so
    this CL renames those methods accordingly. Subsequent CLs will
    introduce a teardown method for live sign-in infra.
    
    No behavior change.
    
    Bug: 340938464
    Change-Id: Id5f8c2ac0f8709805fb21cc213f38140d7bd03f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860160
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356462}

--- chrome/browser/sync/test/integration/sync_test.cc
+++ chrome/browser/sync/test/integration/sync_test.cc
@@ -220,7 +220,7 @@ SyncTest::~SyncTest() = default;
 void SyncTest::SetUp() {
 #if BUILDFLAG(IS_ANDROID)
   if (server_type_ == IN_PROCESS_FAKE_SERVER) {
-    sync_test_utils_android::SetUpAuthForTesting();
+    sync_test_utils_android::SetUpFakeAuthForTesting();
   }
 #endif
 
@@ -270,7 +270,7 @@ void SyncTest::PostRunTestOnMainThread() {
 
 #if BUILDFLAG(IS_ANDROID)
   if (server_type_ == IN_PROCESS_FAKE_SERVER) {
-    sync_test_utils_android::TearDownAuthForTesting();
+    sync_test_utils_android::TearDownFakeAuthForTesting();
   }
 #endif
 }
