d9103730aeb85b49a657b7fe63af0598dcefaf34
amehfooz <amehfooz@google.com>
2024-09-17 17:26:55 +0000

    Fix channel indicator icon background
    Draw the background on the image_view since it makes more
    logical sense. Add padding to ensure the background is 20x20.
    
    Add Pixel tests.
    
    Bug: b/365185732
    Change-Id: I6a6f7f17788721020c07d3ad8e7762df06890f03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866914
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Commit-Queue: Ahmed Mehfooz <amehfooz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356571}

--- ash/system/channel_indicator/channel_indicator.cc
+++ ash/system/channel_indicator/channel_indicator.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/strcat.h"
 #include "base/strings/string_util.h"
+#include "base/version_info/channel.h"
 #include "chromeos/constants/chromeos_features.h"
 #include "components/session_manager/session_manager_types.h"
 #include "components/version_info/channel.h"
@@ -48,8 +49,8 @@ constexpr int kBorderInset = 6;
 // Size of the vector icon.
 constexpr int kVectorIconSize = 16;
 
-// Insets in the layout manager.
-constexpr int kLayoutManagerInset = 2;
+// Insets for the channel indicator icon's background.
+constexpr int kIconBackgroundInset = 2;
 
 }  // namespace
 
@@ -87,7 +88,7 @@ void ChannelIndicatorView::OnThemeChanged() {
       session_manager::SessionState::ACTIVE) {
     // User is logged in, set image view colors.
     if (image_view()) {
-      SetBackground(views::CreateThemedRoundedRectBackground(
+      image_view()->SetBackground(views::CreateThemedRoundedRectBackground(
           channel_indicator_utils::GetBgColorJelly(channel_),
           (IsHorizontalAlignment() ? GetLocalBounds().width()
                                    : GetLocalBounds().height()) /
@@ -145,10 +146,9 @@ void ChannelIndicatorView::SetImageOrText() {
     SetBorder(views::CreateEmptyBorder(IsHorizontalAlignment()
                                            ? gfx::Insets::VH(kBorderInset, 0)
                                            : gfx::Insets::VH(0, kBorderInset)));
-
-    box_layout_->set_inside_border_insets(
-        gfx::Insets::VH(kLayoutManagerInset, kLayoutManagerInset));
-    SetBackground(views::CreateThemedRoundedRectBackground(
+    image_view()->SetBorder(
+        views::CreateEmptyBorder(gfx::Insets(kIconBackgroundInset)));
+    image_view()->SetBackground(views::CreateThemedRoundedRectBackground(
         channel_indicator_utils::GetBgColorJelly(channel_),
         (IsHorizontalAlignment() ? GetLocalBounds().width()
                                  : GetLocalBounds().height()) /
@@ -171,7 +171,6 @@ void ChannelIndicatorView::SetImageOrText() {
   // where side-shelf isn't possible (for now at least!), so nothing here is
   // adjusted for shelf alignment.
   DCHECK(IsHorizontalAlignment());
-  SetBackground(nullptr);
   box_layout_->set_inside_border_insets(gfx::Insets());
   SetBorder(views::CreateEmptyBorder(gfx::Insets::VH(kBorderInset, 0)));
   label()->SetBorder(
