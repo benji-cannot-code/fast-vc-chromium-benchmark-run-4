cf0e0600ad6f0935cf910713e0a9abfc82cad8f8
Joe Downing <joedow@google.com>
2024-09-17 19:16:46 +0000

    Add an interface to allow Heartbeating to an arbitrary API
    This interface will be used to allow the HeartbeatSender to
    heartbeat to an arbitrary endpoint without knowing details about
    which endpoint is being called. A follow-up CL will introduce
    implementations which wrap a specific service_client impl.
    
    Bug: 325448647, 346826154
    Change-Id: I2f20d9a670310e5fb2bc72f4e0f2546054fa1bbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866920
    Commit-Queue: Gary Kacmarcik <garykac@chromium.org>
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Reviewed-by: Gary Kacmarcik <garykac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356649}

---
+++ remoting/host/heartbeat_service_client.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/heartbeat_service_client.h"
+
+namespace remoting {
+
+HeartbeatServiceClient::HeartbeatServiceClient(const std::string& directory_id)
+    : directory_id_(directory_id) {}
+
+HeartbeatServiceClient::~HeartbeatServiceClient() = default;
+
+}  // namespace remoting
