3fd44d56b6f88d6d705f2b4a7e61612063006991
Mark Cogan <marq@google.com>
2024-09-17 15:41:55 +0000

    [iOS] Updates files under ios/chrome/browser/voice
    This CL replaces:
    * ChromeBrowserState => ProfileIOS
    * GetBrowserState => GetProfile
    * browser_state => profile
    * browserState => profile
    * GetOriginalChromeBrowserState => GetOriginalProfile
    * HasOffTheRecordChromeBrowserState => HasOffTheRecordProfile
    * GetOffTheRecordChromeBrowserState => GetOffTheRecordProfile
    * DestroyOffTheRecordChromeBrowserState => DestroyOffTheRecordProfile
    
    web::BrowserState code is not updated.
    
    Bug: 365533484
    Change-Id: Ib81f9a822bea88915ba269fa6e5fa663ab100470
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860901
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Auto-Submit: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356490}

--- ios/chrome/browser/voice/ui_bundled/text_to_speech_playback_controller_factory.mm
+++ ios/chrome/browser/voice/ui_bundled/text_to_speech_playback_controller_factory.mm
@@ -15,12 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // static
 TextToSpeechPlaybackController*
-TextToSpeechPlaybackControllerFactory::GetForBrowserState(ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
-TextToSpeechPlaybackController*
 TextToSpeechPlaybackControllerFactory::GetForProfile(ProfileIOS* profile) {
   return static_cast<TextToSpeechPlaybackController*>(
       GetInstance()->GetServiceForBrowserState(profile, true));
