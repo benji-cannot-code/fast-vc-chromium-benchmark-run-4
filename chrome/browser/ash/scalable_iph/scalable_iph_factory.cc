3af0ac26496cad12f8dc7d042d0a2de454375b37
Eriko Kurimoto <elkurin@chromium.org>
2024-09-17 18:38:27 +0000

    ash: Move //c/b/scalable_iph to //c/b/ash
    Because it's ash-only.
    
    Bug: b:365927737
    Change-Id: I19b703021547d2ca9ae7c16c44683bd27100ddea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849412
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356627}

--- chrome/browser/ash/scalable_iph/scalable_iph_factory.cc
+++ chrome/browser/ash/scalable_iph/scalable_iph_factory.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/scalable_iph/scalable_iph_factory.h"
+#include "chrome/browser/ash/scalable_iph/scalable_iph_factory.h"
 
 #include "base/check_is_test.h"
 #include "base/logging.h"
--- chrome/browser/ui/webui/ash/scalable_iph/scalable_iph_debug_ui.cc
+++ chrome/browser/ui/webui/ash/scalable_iph/scalable_iph_debug_ui.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/strings/strcat.h"
-#include "chrome/browser/scalable_iph/scalable_iph_factory.h"
+#include "chrome/browser/ash/scalable_iph/scalable_iph_factory.h"
 #include "chromeos/ash/components/scalable_iph/logger.h"
 #include "chromeos/ash/components/scalable_iph/scalable_iph.h"
 #include "content/public/browser/web_contents.h"
