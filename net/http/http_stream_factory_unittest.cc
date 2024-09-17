1a6a753f930c25746feb194f821864ae7c1064b0
Kenichi Ishibashi <bashi@chromium.org>
2024-09-17 05:31:27 +0000

    Disable HEv3 in HttpStreamFactory bidirectional stream tests
    The HEv3 code path doesn't support bidirectional streams yet. Disable
    the feature in some HttpStreamFactoryTest tests that use bidirectional
    streams.
    
    Bug: 346835898
    Change-Id: I4f8111bb9ad6d732314952ad4f46bc6f402cf59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856741
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356285}

--- net/http/http_stream_factory_unittest.cc
+++ net/http/http_stream_factory_unittest.cc
@@ -2183,6 +2183,13 @@ TEST_F(HttpStreamFactoryTest, TwoSpdyConnects) {
 }
 
 TEST_F(HttpStreamFactoryTest, RequestBidirectionalStreamImpl) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  // Explicitly disable HappyEyeballsV3 because it doesn't support bidirectional
+  // streams yet.
+  // TODO(crbug.com/346835898): Support bidirectional streams in
+  // HappyEyeballsV3.
+  scoped_feature_list.InitAndDisableFeature(features::kHappyEyeballsV3);
+
   SpdySessionDependencies session_deps(
       ConfiguredProxyResolutionService::CreateDirect());
 
@@ -2779,6 +2786,13 @@ INSTANTIATE_TEST_SUITE_P(VersionIncludeStreamDependencySequence,
 
 TEST_P(HttpStreamFactoryBidirectionalQuicTest,
        RequestBidirectionalStreamImplQuicAlternative) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  // Explicitly disable HappyEyeballsV3 because it doesn't support bidirectional
+  // streams yet.
+  // TODO(crbug.com/346835898): Support bidirectional streams in
+  // HappyEyeballsV3.
+  scoped_feature_list.InitAndDisableFeature(features::kHappyEyeballsV3);
+
   MockQuicData mock_quic_data(version());
   // Set priority to default value so that
   // QuicTestPacketMaker::MakeRequestHeadersPacket() does not add mock
@@ -2869,6 +2883,13 @@ TEST_P(HttpStreamFactoryBidirectionalQuicTest,
 // BidirectionalStreamQuicImpl.
 TEST_P(HttpStreamFactoryBidirectionalQuicTest,
        RequestBidirectionalStreamImplHttpJobFailsQuicJobSucceeds) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  // Explicitly disable HappyEyeballsV3 because it doesn't support bidirectional
+  // streams yet.
+  // TODO(crbug.com/346835898): Support bidirectional streams in
+  // HappyEyeballsV3.
+  scoped_feature_list.InitAndDisableFeature(features::kHappyEyeballsV3);
+
   // Set up Quic data.
   MockQuicData mock_quic_data(version());
   // Set priority to default value so that
@@ -2959,6 +2980,13 @@ TEST_P(HttpStreamFactoryBidirectionalQuicTest,
 }
 
 TEST_F(HttpStreamFactoryTest, RequestBidirectionalStreamImplFailure) {
+  base::test::ScopedFeatureList scoped_feature_list;
+  // Explicitly disable HappyEyeballsV3 because it doesn't support bidirectional
+  // streams yet.
+  // TODO(crbug.com/346835898): Support bidirectional streams in
+  // HappyEyeballsV3.
+  scoped_feature_list.InitAndDisableFeature(features::kHappyEyeballsV3);
+
   SpdySessionDependencies session_deps(
       ConfiguredProxyResolutionService::CreateDirect());
 
