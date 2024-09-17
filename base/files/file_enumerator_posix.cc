f9908f5f8ae2159fde68cdf7208f1484f7fcc148
Joel Hockey <joelhockey@chromium.org>
2024-09-17 06:42:24 +0000

    Support FileSystemDirectoryHandle in android
    * Add java-side functions for getting file info and listing files
      in a dir.
    * Update file_util_posix to use new GetFileInfo which handles dirs.
    * Update FileEnumerators for handling content-URIs.
    * Update FSADirectoryHandle to handle content-URIs and join child
      documentIds with parent dir treeUris.
    
    Bug: 40101963
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED tests from c++ exercise Java
    Change-Id: I7e9b50400c8690862050e2e4bba6af80e4054a22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5785427
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356301}

--- base/files/file_enumerator_posix.cc
+++ base/files/file_enumerator_posix.cc
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 #include "build/build_config.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/content_uri_utils.h"
+#endif
+
 namespace base {
 namespace {
 
@@ -60,6 +64,20 @@ bool ShouldTrackVisitedDirectories(int file_type) {
 FileEnumerator::FileInfo::FileInfo() {
   memset(&stat_, 0, sizeof(stat_));
 }
+
+#if BUILDFLAG(IS_ANDROID)
+FileEnumerator::FileInfo::FileInfo(base::FilePath content_uri,
+                                   base::FilePath filename,
+                                   bool is_directory,
+                                   off_t size,
+                                   Time time)
+    : content_uri_(std::move(content_uri)), filename_(std::move(filename)) {
+  memset(&stat_, 0, sizeof(stat_));
+  stat_.st_mode = is_directory ? S_IFDIR : S_IFREG;
+  stat_.st_size = size;
+  stat_.st_mtime = time.ToTimeT();
+}
+#endif
 
 bool FileEnumerator::FileInfo::IsDirectory() const {
   return S_ISDIR(stat_.st_mode);
@@ -126,6 +144,14 @@ FileEnumerator::FileEnumerator(const FilePath& root_path,
   // INCLUDE_DOT_DOT must not be specified if recursive.
   DCHECK(!(recursive && (INCLUDE_DOT_DOT & file_type_)));
 
+#if BUILDFLAG(IS_ANDROID)
+  // Content-URIs have limited support.
+  if (root_path.IsContentUri()) {
+    CHECK(!recursive);
+    CHECK_EQ(file_type_, FileType::FILES | FileType::DIRECTORIES);
+  }
+#endif
+
   if (file_type_ & FileType::NAMES_ONLY) {
     DCHECK(!recursive_);
     DCHECK_EQ(file_type_ & ~(FileType::NAMES_ONLY | FileType::INCLUDE_DOT_DOT),
@@ -157,6 +183,17 @@ FilePath FileEnumerator::Next() {
     root_path_ = pending_paths_.top();
     root_path_ = root_path_.StripTrailingSeparators();
     pending_paths_.pop();
+
+#if BUILDFLAG(IS_ANDROID)
+    if (root_path_.IsContentUri()) {
+      directory_entries_ = ListContentUriDirectory(root_path_);
+      current_directory_entry_ = 0;
+      if (directory_entries_.empty()) {
+        return base::FilePath();
+      }
+      break;
+    }
+#endif
 
     DIR* dir = opendir(root_path_.value().c_str());
     if (!dir) {
@@ -248,6 +285,12 @@ FilePath FileEnumerator::Next() {
     if (folder_search_policy_ == FolderSearchPolicy::MATCH_ONLY)
       pattern_.clear();
   }
+
+#if BUILDFLAG(IS_ANDROID)
+  if (root_path_.IsContentUri()) {
+    return directory_entries_[current_directory_entry_].content_uri_;
+  }
+#endif
 
   return root_path_.Append(
       directory_entries_[current_directory_entry_].filename_);
--- content/browser/file_system_access/file_system_access_directory_handle_impl.cc
+++ content/browser/file_system_access/file_system_access_directory_handle_impl.cc
@@ -36,6 +36,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_file_handle.mojom.h"
 #include "third_party/blink/public/mojom/file_system_access/file_system_access_transfer_token.mojom.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/content_uri_utils.h"
+#endif
+
 using blink::mojom::FileSystemAccessEntry;
 using blink::mojom::FileSystemAccessEntryPtr;
 using blink::mojom::FileSystemAccessHandle;
@@ -781,9 +785,17 @@ FileSystemAccessDirectoryHandleImpl::GetChildURL(
         FileSystemAccessStatus::kInvalidArgument, "Name is not allowed.");
   }
 
+#if BUILDFLAG(IS_ANDROID)
+  base::FilePath child_path =
+      parent.virtual_path().IsContentUri()
+          ? ContentUriBuildDocumentUriUsingTree(parent.virtual_path(), basename)
+          : parent.virtual_path().Append(basename);
+#else
+  base::FilePath child_path =
+      parent.virtual_path().Append(base::FilePath::FromUTF8Unsafe(basename));
+#endif
   *result = file_system_context()->CreateCrackedFileSystemURL(
-      parent.storage_key(), parent.mount_type(),
-      parent.virtual_path().Append(base::FilePath::FromUTF8Unsafe(basename)));
+      parent.storage_key(), parent.mount_type(), child_path);
   // Child URLs inherit their parent's storage bucket.
   if (parent.bucket()) {
     result->SetBucket(parent.bucket().value());
