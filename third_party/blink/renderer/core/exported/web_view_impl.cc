106c5ecb737cd657fc95021fafd9f8f1e5b04b8a
Dave Tapuska <dtapuska@chromium.org>
2024-09-17 13:54:58 +0000

    Remove WeakPtrFactory from WebView
    This isn't used anymore, so remove it.
    
    Change-Id: I525dee0900afd8239a0a336bfd3e8f045a155a25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867033
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356435}

--- third_party/blink/renderer/core/exported/web_view_impl.cc
+++ third_party/blink/renderer/core/exported/web_view_impl.cc
@@ -1177,8 +1177,6 @@ void WebViewImpl::Close() {
   // want page popups to re-enter WebViewImpl during our shutdown.
   CancelPagePopup();
 
-  // Invalidate any weak ptrs as we are starting to shutdown.
-  weak_ptr_factory_.InvalidateWeakPtrs();
   receiver_.reset();
 
   dev_tools_emulator_->Shutdown();
