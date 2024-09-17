0d297072c71798a14864e7a584f2f96d5c67dc68
Duncan Mercer <mercerd@google.com>
2024-09-17 19:27:02 +0000

    [lensoverlay] Remove banner from permission dialog
    Not entirely sure if required, but removing the branded Google G from
    the non branded version of the permissions dialog.
    
    Bug: 359895335
    Change-Id: Ia63974ac19664acf57317bc655139fc27d57e30c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870565
    Reviewed-by: Ken Rockot <rockot@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1356658}

--- chrome/browser/ui/lens/lens_permission_bubble_controller.cc
+++ chrome/browser/ui/lens/lens_permission_bubble_controller.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_functions.h"
 #include "base/metrics/user_metrics.h"
 #include "base/metrics/user_metrics_action.h"
+#include "build/branding_buildflags.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
 #include "chrome/grit/generated_resources.h"
@@ -115,9 +116,11 @@ LensPermissionBubbleController::CreateLensPermissionDialogModel() {
       .SetInternalName(kLensPermissionDialogName)
       .SetTitle(
           l10n_util::GetStringUTF16(IDS_LENS_PERMISSION_BUBBLE_DIALOG_TITLE))
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
       .SetBannerImage(ui::ImageModel::FromImageSkia(
           *ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
               IDR_LENS_PERMISSION_MODAL_IMAGE)))
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
       .AddParagraph(ui::DialogModelLabel::CreateWithReplacement(
           IDS_LENS_PERMISSION_BUBBLE_DIALOG_DESCRIPTION, link))
       .AddOkButton(
