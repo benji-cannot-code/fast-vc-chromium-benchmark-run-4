4ea6c45b19f0522261a38f14ebb9f7db32137900
Benjamin Williams <bwwilliams@google.com>
2024-09-18 15:15:36 +0000

    Fix misspelling of "received" in components/invalidation.
    This CL was uploaded by git cl split.
    
    R=asumaneev@google.com
    
    Bug: 367997490
    Change-Id: I87395c8259f2fb569b2b42f3dc26f20708558709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873839
    Reviewed-by: Artem Sumaneev <asumaneev@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Artem Sumaneev <asumaneev@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357102}

--- components/invalidation/impl/fcm_network_handler_unittests.cc
+++ components/invalidation/impl/fcm_network_handler_unittests.cc
@@ -258,7 +258,7 @@ class FCMNetworkHandlerTestWithTTL : public FCMNetworkHandlerTest {
   base::test::ScopedFeatureList override_features_;
 };
 
-TEST_F(FCMNetworkHandlerTest, ShouldPassTheTokenOnceRecieved) {
+TEST_F(FCMNetworkHandlerTest, ShouldPassTheTokenOnceReceived) {
   std::unique_ptr<FCMNetworkHandler> handler = MakeHandler();
 
   MockOnTokenCallback mock_on_token_callback;
