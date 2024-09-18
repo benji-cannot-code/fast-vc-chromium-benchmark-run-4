1c3a09ca6da62c133ad49fe5d713ae1a042b832e
Minoru Chikamune <chikamune@chromium.org>
2024-09-18 07:24:37 +0000

    Clean up: Remove kServiceWorkerScopeCacheLimit feature
    kServiceWorkerScopeCache and kServiceWorkerScopeCacheLimit features are
    already enabled by default. This CL removes
    kServiceWorkerScopeCacheLimit feature.
    
    Bug: 40254732
    Change-Id: I47c2520e99d53dbd74a9f08acca00d473c930f96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869657
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356936}

--- content/browser/service_worker/service_worker_registry_unittest.cc
+++ content/browser/service_worker/service_worker_registry_unittest.cc
@@ -1685,8 +1685,7 @@ TEST_F(ServiceWorkerScopeAndRegistrationCacheTest,
   base::test::ScopedFeatureList scoped_feature_list;
   scoped_feature_list.InitWithFeaturesAndParameters(
       {{storage::kServiceWorkerScopeCache, {}},
-       {kServiceWorkerRegistrationCache, {}},
-       {kServiceWorkerScopeCacheLimit, {}}},
+       {kServiceWorkerRegistrationCache, {}}},
       {});
   // Restart to apply the above feature params.
   SimulateRestart();
