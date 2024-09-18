f20b4813e2dd91653346cc5e7257faaaaaeb0f03
Georg Neis <neis@chromium.org>
2024-09-18 00:28:53 +0000

    ash: Remove Lacros-specific code from picker
    crosapi::browser_util::IsLacrosEnabled() always returns true now.
    Remove obsolete code.
    
    Bug: b:365741912
    Change-Id: I1e2eccb7aa205b026980d8aa29e6f0da228f695d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859497
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yuta Hijikata <ythjkt@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356836}

--- chrome/browser/ash/crosapi/search_controller_factory_ash_unittest.cc
+++ chrome/browser/ash/crosapi/search_controller_factory_ash_unittest.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/task_environment.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ash/crosapi/search_controller_ash.h"
-#include "chrome/browser/ui/ash/picker/picker_lacros_omnibox_search_provider.h"
 #include "chromeos/crosapi/mojom/launcher_search.mojom-forward.h"
 #include "chromeos/crosapi/mojom/launcher_search.mojom-shared.h"
 #include "chromeos/crosapi/mojom/launcher_search.mojom.h"
