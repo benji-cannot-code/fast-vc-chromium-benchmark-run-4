02a5684a3aa1980f5b524203a698942572d6a93f
Hardik Goyal <hardikgoyal@google.com>
2024-09-18 21:44:39 +0000

    Add promo in pw manager app for screen lock reauth for pw fill
    This CL is adding a promo in password manager app, asking users to
    enable screen lock reauthentation when filling in passwords. This
    feature is available on MacOS and now we are adding it to ChromeOS.
    
    BUG=b:362915479
    BUG=367336383
    
    Change-Id: Ic10f688a697263c3b44a3407c33874c9473e9e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872901
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Mohamed Amir Yosef <mamir@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357335}

--- chrome/browser/ui/webui/password_manager/promo_cards_handler.cc
+++ chrome/browser/ui/webui/password_manager/promo_cards_handler.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/password_manager/promo_cards/relaunch_chrome_promo.h"
 #endif
 
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS_ASH)
 #include "chrome/browser/ui/webui/password_manager/promo_cards/screenlock_reauth_promo.h"
 #endif
 
@@ -81,7 +81,7 @@ std::vector<std::unique_ptr<PasswordPromoCardBase>> GetAllPromoCardsForProfile(
       std::make_unique<RelaunchChromePromo>(profile->GetPrefs()));
 #endif
 
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS_ASH)
   promo_cards.push_back(std::make_unique<ScreenlockReauthPromo>(profile));
 #endif
   return promo_cards;
