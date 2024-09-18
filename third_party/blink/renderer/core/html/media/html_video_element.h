bc180b69a1d4cb8a20d986b7ca06bb4bddb15764
Andrew Naumov <andrew-naumov@yandex-team.ru>
2024-09-18 11:25:30 +0000

    Remove cache for HTMLVideoElement::CreateStaticBitmapImage by timer
    Also prevent unnecessary cache recreation when kN32_SkColorType is
    kBGRA_8888_SkColorType or accelerated canvas is not available,
    and add check for CanvasResourceProvider::IsGpuContextLost.
    
    Change-Id: I975d8c3ea215998275ced9468ec6a6b9ea3e5728
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854733
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: Andrew Naumov <andrew-naumov@yandex-team.ru>
    Cr-Commit-Position: refs/heads/main@{#1357008}

--- third_party/blink/renderer/core/html/media/html_video_element.h
+++ third_party/blink/renderer/core/html/media/html_video_element.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/html/media/html_media_element.h"
 #include "third_party/blink/renderer/core/imagebitmap/image_bitmap_source.h"
 #include "third_party/blink/renderer/platform/graphics/canvas_resource_provider.h"
+#include "third_party/blink/renderer/platform/timer.h"
 
 namespace blink {
 
@@ -231,6 +232,8 @@ class CORE_EXPORT HTMLVideoElement final
 
   void ReportVisibility(bool meets_visibility_threshold);
 
+  void ResetCache(TimerBase*);
+
   Member<HTMLImageLoader> image_loader_;
   Member<MediaCustomControlsFullscreenDetector>
       custom_controls_fullscreen_detector_;
@@ -270,6 +273,9 @@ class CORE_EXPORT HTMLVideoElement final
   // Used to fulfill blink::Image requests (CreateImage(),
   // GetSourceImageForCanvas(), etc). Created on demand.
   std::unique_ptr<CanvasResourceProvider> resource_provider_;
+  SkImageInfo resource_provider_info_;
+  bool allow_accelerated_images_ = true;
+  HeapTaskRunnerTimer<HTMLVideoElement> cache_deleting_timer_;
 };
 
 }  // namespace blink
