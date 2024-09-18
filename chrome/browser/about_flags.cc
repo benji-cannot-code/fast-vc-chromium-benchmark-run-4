a5b3ff548b046f76a8b92f8beae70d5369a8143c
Tim Sergeant <tsergeant@chromium.org>
2024-09-18 06:03:06 +0000

    AIS: Remove unlaunched Borealis integration
    This code has been sitting behind a disabled flag for several months now
    with no particular path to launch. If we do want to launch this in the
    future, it will be easy to bring back.
    
    Bug: 367431454
    Change-Id: Ibdc6777b07490ba3654938ab1a9b07dcf8055dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869472
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Auto-Submit: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356919}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -2985,8 +2985,6 @@ constexpr char kGlanceablesTimeManagementClassroomStudentViewInternalName[] =
 constexpr char kGlanceablesTimeManagementTasksViewInternalName[] =
     "glanceables-time-management-tasks-view";
 constexpr char kBackgroundListeningName[] = "background-listening";
-constexpr char kAppInstallServiceUriBorealisName[] =
-    "app-install-service-uri-borealis";
 constexpr char kBorealisBigGlInternalName[] = "borealis-big-gl";
 constexpr char kBorealisDGPUInternalName[] = "borealis-dgpu";
 constexpr char kBorealisEnableUnsupportedHardwareInternalName[] =
@@ -9387,10 +9385,6 @@ const FeatureEntry kFeatureEntries[] = {
     {kBorealisBigGlInternalName, flag_descriptions::kBorealisBigGlName,
      flag_descriptions::kBorealisBigGlDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kBorealisBigGl)},
-    {kAppInstallServiceUriBorealisName,
-     flag_descriptions::kAppInstallServiceUriBorealisName,
-     flag_descriptions::kAppInstallServiceUriBorealisDescription, kOsCrOS,
-     FEATURE_VALUE_TYPE(ash::features::kAppInstallServiceUriBorealis)},
     {kBorealisDGPUInternalName, flag_descriptions::kBorealisDGPUName,
      flag_descriptions::kBorealisDGPUDescription, kOsCrOS,
      FEATURE_VALUE_TYPE(ash::features::kBorealisDGPU)},
@@ -12131,8 +12125,7 @@ bool ShouldSkipConditionalFeatureEntry(const flags_ui::FlagsStorage* storage,
   }
 
   // Only show Borealis flags on enabled devices.
-  if (!strcmp(kAppInstallServiceUriBorealisName, entry.internal_name) ||
-      !strcmp(kBorealisBigGlInternalName, entry.internal_name) ||
+  if (!strcmp(kBorealisBigGlInternalName, entry.internal_name) ||
       !strcmp(kBorealisDGPUInternalName, entry.internal_name) ||
       !strcmp(kBorealisEnableUnsupportedHardwareInternalName,
               entry.internal_name) ||
