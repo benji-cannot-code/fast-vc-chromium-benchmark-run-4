1fe6f6b82d2102b30e4d5c438fc29371e750d0f2
Haihan Chen <haihan@google.com>
2024-09-18 14:53:49 +0000

    [File Download Access Prevention] Obfuscate download file for enterprise deep scan
    Initial implementation of data chunk obfuscation for each download file before they are written to the file.
    
    - Added obfuscator_ to track encryption state for each download.
    - Hash: we also calculate and send the hash of the original file instead when download is complete. We keep doing the hash for the encrypted file. For interrupted downloads we will have an implementation in a subsequent CL.
    - Pass total_bytes to download_file through save_info to protect against truncation. If total_bytes is not given, we append an empty obfuscated chunk at the end instead (20 bytes).
    - Any interrupted errors due to obfuscation operations is returned as DOWNLOAD_INTERRUPT_REASON_FILE_FAILED for now. Will add more detailed errors in a subsequent CL.
    - Support for parallel download is not a priority right now, as it would require changes to the encryption scheme.
    
    Bug: b/349858060
    Change-Id: I10b33afbae2e597b29e2c7e4fe24a7c6b831d116
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854143
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357084}

--- components/download/public/common/download_file_impl.h
+++ components/download/public/common/download_file_impl.h
@@ -31,8 +31,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/download/public/common/download_item.h"
 #include "components/download/public/common/download_save_info.h"
 #include "components/download/public/common/rate_estimator.h"
+#include "components/enterprise/buildflags/buildflags.h"
 #include "components/services/quarantine/public/mojom/quarantine.mojom.h"
 #include "mojo/public/cpp/system/simple_watcher.h"
+
+#if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
+#include "components/enterprise/obfuscation/core/download_obfuscator.h"  // nogncheck
+#endif  // BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
 
 namespace download {
 
@@ -388,6 +393,10 @@ class COMPONENTS_DOWNLOAD_EXPORT DownloadFileImpl : public DownloadFile {
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
 
   SEQUENCE_CHECKER(sequence_checker_);
+
+#if BUILDFLAG(ENTERPRISE_CONTENT_ANALYSIS)
+  std::unique_ptr<enterprise_obfuscation::DownloadObfuscator> obfuscator_;
+#endif
 
   base::WeakPtr<DownloadDestinationObserver> observer_;
   base::WeakPtrFactory<DownloadFileImpl> weak_factory_{this};
