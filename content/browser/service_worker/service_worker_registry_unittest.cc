22d34d82189885060a5e196b9165237c8598be30
Minoru Chikamune <chikamune@chromium.org>
2024-09-18 11:27:34 +0000

    Clean up: Remove kServiceWorkerScopeCache feature flag
    Since kServiceWorkerScopeCache feature is already enabled by
    default. This CL removes kServiceWorkerScopeCache feature flag.
    
    Bug: 40254732
    Change-Id: I3f1aa89d2f7b5e776c73ae5ad5b39b880d4d9765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869660
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357010}

--- content/browser/service_worker/service_worker_registry_unittest.cc
+++ content/browser/service_worker/service_worker_registry_unittest.cc
@@ -1320,8 +1320,6 @@ class ServiceWorkerScopeAndRegistrationCacheTest
 };
 
 TEST_F(ServiceWorkerScopeAndRegistrationCacheTest, SkipMojoCallIfPossible) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures({storage::kServiceWorkerScopeCache}, {});
   const GURL kScript("http://www.example.com/script.js");
   const GURL kScope1("http://www.example.com/scope1/");
   const GURL kScope2("http://www.example.com/scope2/");
@@ -1448,9 +1446,6 @@ TEST_F(ServiceWorkerScopeAndRegistrationCacheTest, SkipMojoCallIfPossible) {
 
 TEST_F(ServiceWorkerScopeAndRegistrationCacheTest,
        RegistrationCacheSizeAndScopeCacheLimitPerKey) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeaturesAndParameters(
-      {{storage::kServiceWorkerScopeCache, {}}}, {});
   const size_t kMaxScopeUrlCount = 2;
   storage::OverrideMaxServiceWorkerScopeUrlCountForTesting(kMaxScopeUrlCount);
   base::ScopedClosureRunner reset(base::BindOnce([]() {
@@ -1614,8 +1609,6 @@ TEST_F(ServiceWorkerScopeAndRegistrationCacheTest,
 }
 
 TEST_F(ServiceWorkerScopeAndRegistrationCacheTest, CanHandleNewRegistration) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeatures({storage::kServiceWorkerScopeCache}, {});
   const GURL kScript("http://www.example.com/script.js");
   const GURL kScope1("http://www.example.com/scope/");
   const GURL kScope2("http://www.example.com/");
@@ -1687,9 +1680,6 @@ TEST_F(ServiceWorkerScopeAndRegistrationCacheTest, CanHandleNewRegistration) {
 
 TEST_F(ServiceWorkerScopeAndRegistrationCacheTest,
        ServiceWorkerScopeCacheLimit) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitWithFeaturesAndParameters(
-      {{storage::kServiceWorkerScopeCache, {}}}, {});
   // Restart to apply the above feature params.
   SimulateRestart();
   {
