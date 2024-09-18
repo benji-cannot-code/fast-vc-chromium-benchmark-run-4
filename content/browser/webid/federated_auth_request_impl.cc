9eaabc90a7c6729eadef32d1cd42b27850a1fb03
Peter Kotwicz <pkotwicz@chromium.org>
2024-09-18 20:39:48 +0000

    Fix explanation for edge case in FederatedAuthRequestImpl
    This CL updates the explanation for the edge case in
    FederatedAuthRequestImpl::CancelTokenRequest() and adds test which
    executes the edge case.
    
    BUG=40940748
    
    Change-Id: I800959c721f13364801110eb20b1e1dff0b45059
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871992
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Christian Biesinger <cbiesinger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357306}

--- content/browser/webid/federated_auth_request_impl.cc
+++ content/browser/webid/federated_auth_request_impl.cc
@@ -1145,9 +1145,8 @@ void FederatedAuthRequestImpl::RequestUserInfo(
 
 void FederatedAuthRequestImpl::CancelTokenRequest() {
   if (!auth_request_token_callback_) {
-    // TODO(crbug.com/40940748): this should only happen with a compromised
-    // renderer process but for some reason that is not the case. We should
-    // investigate what could go wrong about the abort controller.
+    // This can happen if the renderer requested an abort() after the browser
+    // invoked the callback but before the renderer received the callback.
     return;
   }
 
