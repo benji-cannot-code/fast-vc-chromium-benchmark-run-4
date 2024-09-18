474fb27c62977f709f98cc9afd723525dd8e7208
dileep maurya <dileepmaurya@microsoft.com>
2024-09-18 19:25:04 +0000

    [views-ax] Migrating a11y properties in PasswordGenerationPopupViewViews
    This CL migrates the setting of the a11y properties from
    View::GetAccessibleNodeData to the view constructor. This ensures that
    the a11y properties are updated in the accessibility cache as soon as
    the view is constructed. These changes are trivial moves: these
    properties never change, so we don't need to add any logic to keep
    the cache up to date.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ica44960396c523a59476c76f1ff262c61b6d92bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5855937
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Dileep Maurya <dileepmaurya@microsoft.com>
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1357261}

--- chrome/browser/ui/views/passwords/password_generation_popup_view_views.cc
+++ chrome/browser/ui/views/passwords/password_generation_popup_view_views.cc
@@ -377,6 +377,7 @@ PasswordGenerationPopupViewViews::PasswordGenerationPopupViewViews(
   // `autofill::PopupViewViews`. See crrev.com/c/2545285 for details.
   // Consider using a more appropriate role (e.g. kMenuListPopup or similar).
   GetViewAccessibility().SetRole(ax::mojom::Role::kListBox);
+  UpdateInvisibleAccessibleState();
   UpdateExpandedCollapsedAccessibleState();
 }
 
@@ -397,6 +398,7 @@ void PasswordGenerationPopupViewViews::Hide() {
   // always be non null there, so there is no need to update the accessible
   // state there.
   UpdateExpandedCollapsedAccessibleState();
+  UpdateInvisibleAccessibleState();
   if (password_view_) {
     password_view_->reset_controller();
   }
@@ -507,8 +509,12 @@ void PasswordGenerationPopupViewViews::CreateLayoutAndChildren() {
       gfx::Insets::VH(kVerticalPadding, kHorizontalMargin)));
 }
 
-void PasswordGenerationPopupViewViews::UpdateExpandedCollapsedAccessibleState()
-    const {
+void PasswordGenerationPopupViewViews::UpdateInvisibleAccessibleState() {
+  GetViewAccessibility().SetIsInvisible(!controller_ ? true : false);
+}
+
+void PasswordGenerationPopupViewViews::
+    UpdateExpandedCollapsedAccessibleState() {
   if (controller_) {
     GetViewAccessibility().SetIsExpanded();
   } else {
