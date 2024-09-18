1eefc84dea15f9d133415d92b3570ac4d6db1ab4
jialihuang <jialihuang@google.com>
2024-09-18 12:04:38 +0000

    [CVV Storage Enhancement] Return cvc for standalone cvc form type
    Add CREDIT_CARD_STANDALONE_VERIFICATION_CODE case to GetRawInfo function to allow us to retrieve cvc value if form type is
    CREDIT_CARD_STANDALONE_VERIFICATION_CODE
    
    Bug: 356694842
    Change-Id: Ifb218b98b9229b51bc77a4a1106ac04f28c10db5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5862007
    Commit-Queue: Jiali Huang <jialihuang@google.com>
    Reviewed-by: Sujie Zhu <sujiezhu@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Alexander Tekle <alexandertekle@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357024}

--- components/autofill/core/browser/data_model/credit_card.cc
+++ components/autofill/core/browser/data_model/credit_card.cc
@@ -489,6 +489,7 @@ std::u16string CreditCard::GetRawInfo(FieldType type) const {
       return number_;
 
     case CREDIT_CARD_VERIFICATION_CODE:
+    case CREDIT_CARD_STANDALONE_VERIFICATION_CODE:
       return cvc_;
 
     default:
