d261b87f24ad3fd883872be068283416ab1d0b71
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-09-18 12:18:12 +0000

    Revert "Add conditional focus wait to DataProtectionClipboardBrowserTests"
    This reverts commit 27ba25de77176f093866d4e425d013cec637f64a.
    
    Reason for revert: See crbug.com/360052665#comment9
    
    Original change's description:
    > Add conditional focus wait to DataProtectionClipboardBrowserTests
    >
    > On local repros of the `widget_activation_waiter.cc:58: Failure`
    > error, the tests would pass if the browser window was focused manually
    > by clicking on it. This suggests the wait call might not be required
    > in all test runs, so this CL tentatively adds a check to not wait if
    > a sub-frame of the tested WebContents is already focused.
    >
    > Bug: 360052665
    > Change-Id: I1ceab5fac8b33f3b5ee967efcc290d3aa29ad39c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5812445
    > Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Reviewed-by: Nasser Al-shawwa <alshawwa@chromium.org>
    > Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1356489}
    
    Bug: 360052665
    Change-Id: Ifb9e7fe916f9d65ab67e638a6e30652ec63f5b51
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873519
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1357026}

--- chrome/browser/enterprise/data_protection/clipboard_browsertest.cc
+++ chrome/browser/enterprise/data_protection/clipboard_browsertest.cc
@@ -32,6 +32,13 @@ namespace enterprise_data_protection {
 
 namespace {
 
+// TODO(crbug.com/360052665): Flaky on Mac
+#if BUILDFLAG(IS_MAC)
+#define MAYBE(x) DISABLED_##x
+#else
+#define MAYBE(x) x
+#endif
+
 // Browser tests that test data protection integration with Chrome's clipboard
 // logic. If a browser test you're adding is specific to a single
 // function/class, consider using a browsertest.cc file specific to that code.
@@ -68,14 +75,9 @@ class DataProtectionClipboardBrowserTest : public InProcessBrowserTest {
   }
 
   void FocusWebContents() {
-    auto* web_contents = browser()->tab_strip_model()->GetActiveWebContents();
-    ASSERT_TRUE(web_contents);
-
-    web_contents->Focus();
-    if (!web_contents->GetFocusedFrame()) {
-      views::test::WaitForWidgetActive(
-          BrowserView::GetBrowserViewForBrowser(browser())->GetWidget(), true);
-    }
+    browser()->tab_strip_model()->GetActiveWebContents()->Focus();
+    views::test::WaitForWidgetActive(
+        BrowserView::GetBrowserViewForBrowser(browser())->GetWidget(), true);
   }
 
   void WriteTextToClipboard(const std::string& text) {
@@ -116,7 +118,7 @@ class DataProtectionClipboardBrowserTest : public InProcessBrowserTest {
 }  // namespace
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       CopyBlockedByDataControls) {
+                       MAYBE(CopyBlockedByDataControls)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "sources": { "urls": ["*"] },
                     "restrictions": [
@@ -153,7 +155,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       CopyWarnedByDataControls_Cancel) {
+                       MAYBE(CopyWarnedByDataControls_Cancel)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "sources": { "urls": ["*"] },
                     "restrictions": [
@@ -190,7 +192,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       CopyWarnedByDataControls_Bypass) {
+                       MAYBE(CopyWarnedByDataControls_Bypass)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "sources": { "urls": ["*"] },
                     "restrictions": [
@@ -228,7 +230,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       CopyAllowedByDataControls) {
+                       MAYBE(CopyAllowedByDataControls)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "sources": { "urls": ["google.com"] },
                     "restrictions": [
@@ -253,7 +255,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       PasteBlockedByDataControls) {
+                       MAYBE(PasteBlockedByDataControls)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "destinations": { "urls": ["*"] },
                     "restrictions": [
@@ -282,7 +284,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       PasteWarnedByDataControls_Cancel) {
+                       MAYBE(PasteWarnedByDataControls_Cancel)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "destinations": { "urls": ["*"] },
                     "restrictions": [
@@ -311,7 +313,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       PasteWarnedByDataControls_Bypass) {
+                       MAYBE(PasteWarnedByDataControls_Bypass)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "destinations": { "urls": ["*"] },
                     "restrictions": [
@@ -340,7 +342,7 @@ IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DataProtectionClipboardBrowserTest,
-                       PasteAllowedByDataControls) {
+                       MAYBE(PasteAllowedByDataControls)) {
   data_controls::SetDataControls(browser()->profile()->GetPrefs(), {R"({
                     "destinations": { "urls": ["google.com"] },
                     "restrictions": [
