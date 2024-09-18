88c2601583f083c5a0897222c46e86c073fa9636
Georg Neis <neis@chromium.org>
2024-09-18 08:09:16 +0000

    Create chrome/browser/ash/main_parts
    ... and move chrome_browser_main_parts_ash.* there.
    
    Bug: b:367543200, b:361318089
    Change-Id: Ie8c27028019bd3d38ad0bc5980dfaac0ce2496b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869349
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356955}

--- chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.h
+++ chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_ASH_CHROME_BROWSER_MAIN_PARTS_ASH_H_
-#define CHROME_BROWSER_ASH_CHROME_BROWSER_MAIN_PARTS_ASH_H_
+#ifndef CHROME_BROWSER_ASH_MAIN_PARTS_CHROME_BROWSER_MAIN_PARTS_ASH_H_
+#define CHROME_BROWSER_ASH_MAIN_PARTS_CHROME_BROWSER_MAIN_PARTS_ASH_H_
 
 #include <memory>
 
@@ -109,10 +109,6 @@ class FileIndexServiceRegistry;
 
 namespace internal {
 class DBusServices;
-}
-
-namespace mojo_service_manager {
-class Helper;
 }
 
 namespace platform_keys {
@@ -308,4 +304,4 @@ class ChromeBrowserMainPartsAsh : public ChromeBrowserMainPartsLinux {
 
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_ASH_CHROME_BROWSER_MAIN_PARTS_ASH_H_
+#endif  // CHROME_BROWSER_ASH_MAIN_PARTS_CHROME_BROWSER_MAIN_PARTS_ASH_H_
