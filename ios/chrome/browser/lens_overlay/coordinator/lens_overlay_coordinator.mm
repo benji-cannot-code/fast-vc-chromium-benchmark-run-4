37ea131a894955c3a5d403230d7eaafb1401582e
Christian Xu <christianxu@google.com>
2024-09-18 14:03:07 +0000

    [iOS][LO] Fix sheet corner radius
    Adjust corner radius to match the mocks.
    
    Bug: 359790446
    Change-Id: Ie7a13d6c789c07376959367a8c12813f2ab5da24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872724
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357062}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -806,7 +806,7 @@ const CGFloat kMenuSymbolSize = 18;
     [UISheetPresentationControllerDetent largeDetent]
   ];
   sheet.prefersGrabberVisible = YES;
-  sheet.preferredCornerRadius = 20;
+  sheet.preferredCornerRadius = 14;
 
   __weak __typeof(self) weakSelf = self;
   [_containerViewController
