7f6f40d5026557f78f4982cc8257f3eccb50f62d
Anders Hartvoll Ruud <andruud@chromium.org>
2024-09-18 11:33:54 +0000

    [css-nesting] Zero specificity for contextually invalid selectors
    Brings Interop 2024 score to 100% for the CSS Nesting focus area.
    
    Fixed: 41494436
    Change-Id: I696ef4a2216d3b136b54442e5955c48ed1eac22e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872482
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357014}

--- third_party/blink/renderer/core/css/css_selector.h
+++ third_party/blink/renderer/core/css/css_selector.h
@@ -589,6 +589,7 @@ class CORE_EXPORT CSSSelector {
   }
 
   bool MatchesPseudoElement() const;
+  bool IsAllowedInParentPseudo() const;
   bool IsTreeAbidingPseudoElement() const;
   bool IsAllowedAfterPart() const;
 
