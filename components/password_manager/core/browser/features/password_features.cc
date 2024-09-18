ccd2a42ace96b134cc0f0b7ad3d4b41146ad7e38
Anna Tsvirchkova <atsvirchkova@google.com>
2024-09-18 17:06:53 +0000

    [IdCheck] Add feature flag for Identity Check feature
    The feature flag will guard the identity check feature for passwords.
    The feature makes biometric re-auth mandatory before passwords filling
    if the phone is in non-trusted location.
    
    Bug: 367682771
    Change-Id: I9cfeb5c63c1755ef05a9dbaf5393939d720f71a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868026
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357169}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -133,6 +133,10 @@ BASE_FEATURE(kUnifiedPasswordManagerLocalPasswordsAndroidAccessLossWarning,
              "UnifiedPasswordManagerLocalPasswordsAndroidAccessLossWarning",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kBiometricAuthIdentityCheck,
+             "BiometricAuthIdentityCheck",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kClearLoginDatabaseForAllMigratedUPMUsers,
              "ClearLoginDatabaseForAllMigratedUPMUsers",
              base::FEATURE_DISABLED_BY_DEFAULT);
