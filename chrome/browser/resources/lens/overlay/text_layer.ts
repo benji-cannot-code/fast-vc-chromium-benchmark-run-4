bc00b5d3730c14624478dc1514b7e0e9600a2bff
Juan Mojica <juanmojica@google.com>
2024-09-18 16:43:19 +0000

    [lensoverlay] Background image data should have same rotation as line.
    Bug: 367808757, b:367804916
    Change-Id: I11c0bee3f9fab5febc2ab3e0711b9c8fed617c69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871133
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357156}

--- chrome/browser/resources/lens/overlay/text_layer.ts
+++ chrome/browser/resources/lens/overlay/text_layer.ts
@@ -1051,6 +1051,7 @@ export class TextLayerElement extends PolymerElement {
           toPercent(
               lineBoundingBox.box.x - (lineBoundingBox.box.width / 2) -
               (0.5 * horizontalPadding))}`,
+      `transform: rotate(${lineBoundingBox.rotation}rad)`,
     ];
     return styles.join(';');
   }
