cc485bbbf4fa94012a818f2859a297d91b83fab0
Brandon Jones <bajones@chromium.org>
2024-09-18 18:01:09 +0000

    Check session/device state at WebXR layer creation
    Ensures that the XRSession and GPUDevice are still valid when creating
    a new layer with an XRGPUBinding.
    
    Bug: 366060352
    Change-Id: I7100b2f8d6a7b60844d0259b40db8e12c7cfd4a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870766
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Auto-Submit: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357207}

--- third_party/blink/renderer/modules/xr/xr_gpu_binding.h
+++ third_party/blink/renderer/modules/xr/xr_gpu_binding.h
@@ -58,6 +58,8 @@ class XRGPUBinding final : public ScriptWrappable, public XRGraphicsBinding {
   void Trace(Visitor*) const override;
 
  private:
+  bool CanCreateLayer(ExceptionState& exception_state);
+
   Member<GPUDevice> device_;
 };
 
