973bdc0d48c9f74f59709ef81d176e22bf3d674e
Ryan Tarpine <rtarpine@chromium.org>
2024-09-18 21:20:31 +0000

    Stop clangd from including histogram_functions_internal_overloads.h.
    Now that the const char* and const string& overloads are in an internal
    header, vscode-clangd started adding includes for that header.
    
    Use an IWYU pragma to prevent that and keep adding histogram_functions.h
    instead.
    
    Bug: 358538894
    Change-Id: I09dfd6f76553bc4ab1fa6ac9d32eee489f72d6e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873285
    Auto-Submit: Ryan Tarpine <rtarpine@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357317}

--- base/metrics/histogram_functions.h
+++ base/metrics/histogram_functions.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/histogram_base.h"
-#include "base/metrics/histogram_functions_internal_overloads.h"
+#include "base/metrics/histogram_functions_internal_overloads.h"  // IWYU pragma: export
 #include "base/time/time.h"
 
 // TODO(crbug.com/40801421): Update this file's function comments to provide
