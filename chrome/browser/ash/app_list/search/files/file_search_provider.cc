809a49f1e452d07ab5eb77c94a9dc12ddf5487d4
Darren Shen <shend@chromium.org>
2024-09-18 00:34:44 +0000

    Add file search filtering based on extension to FileSearchProvider
    This corrects the number of results returned when the caller needs to
    filter on the extension. Without this, the caller may only receive 25
    results that all need to be filtered out, giving 0 results even though
    there could be 25 valid results that are ranked lower.
    
    Bug: b:360229206
    Change-Id: I0e7ad0feb0cab31d023e4e8f7c52385bbaf9d940
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869269
    Reviewed-by: Michael Cui <mlcui@google.com>
    Reviewed-by: CJ Huang <chenjih@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356838}

--- chrome/browser/ash/app_list/search/files/file_search_provider.cc
+++ chrome/browser/ash/app_list/search/files/file_search_provider.cc
@@ -96,7 +96,8 @@ std::vector<FileSearchProvider::FileInfo> SearchFilesByPattern(
     const std::u16string& query,
     const base::TimeTicks& query_start_time,
     const std::vector<base::FilePath> trash_paths,
-    const int file_type) {
+    const int file_type,
+    const base::span<const std::string> allowed_extensions) {
   base::FileEnumerator enumerator(
       root_path,
       /*recursive=*/true, file_type, CreateFnmatchQuery(query),
@@ -112,6 +113,14 @@ std::vector<FileSearchProvider::FileInfo> SearchFilesByPattern(
                              [&path](const base::FilePath& trash_path) {
                                return trash_path.IsParent(path);
                              })) {
+      continue;
+    }
+    // Exclude any results that are not in the allowed extensions.
+    if (!allowed_extensions.empty() &&
+        !base::ranges::any_of(allowed_extensions,
+                              [&path](const std::string& extension) {
+                                return path.MatchesFinalExtension(extension);
+                              })) {
       continue;
     }
 
@@ -132,12 +141,16 @@ std::vector<FileSearchProvider::FileInfo> SearchFilesByPattern(
 
 }  // namespace
 
-FileSearchProvider::FileSearchProvider(Profile* profile, int file_type)
+FileSearchProvider::FileSearchProvider(
+    Profile* profile,
+    int file_type,
+    std::vector<std::string> allowed_extensions)
     : SearchProvider(SearchCategory::kFiles),
       profile_(profile),
       thumbnail_loader_(profile),
       root_path_(file_manager::util::GetMyFilesFolderForProfile(profile)),
-      file_type_(file_type) {
+      file_type_(file_type),
+      allowed_extensions_(std::move(allowed_extensions)) {
   DCHECK(profile_);
   DCHECK(!root_path_.empty());
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
@@ -177,7 +190,7 @@ void FileSearchProvider::Start(const std::u16string& query) {
                      (file_manager::trash::IsTrashEnabledForProfile(profile_)
                           ? trash_paths_
                           : std::vector<base::FilePath>()),
-                     file_type_),
+                     file_type_, allowed_extensions_),
       base::BindOnce(&FileSearchProvider::OnSearchComplete,
                      weak_factory_.GetWeakPtr()));
 }
