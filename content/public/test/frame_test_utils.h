cd33df650913c2ecafde1bb6ca5d78c4848cb1da
Peter Kasting <pkasting@chromium.org>
2024-09-18 16:44:22 +0000

    Clean up frame_test_utils.*.
    This came out of work to eliminate calls to
    `StringPrintfNonConstexpr()`, but became a larger refactor when I
    decided to also try to eliminate duplicated code/speed things up.
    
    Bug: none
    Change-Id: Ib5d1720a78a720d9ec0f2c1a537a0a2404513953
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871468
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357157}

--- content/public/test/frame_test_utils.h
+++ content/public/test/frame_test_utils.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_TEST_FRAME_TEST_UTILS_H_
 #define CONTENT_PUBLIC_TEST_FRAME_TEST_UTILS_H_
 
+#include <string>
+#include <string_view>
 #include <vector>
 
 #include "content/public/test/browser_test_utils.h"
@@ -14,35 +16,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// Returns the text contents of the leaf frame. `frame_tree_pattern` contains a
-// format template for the query string to be passed to
-// cross_site_iframe_factory, where the URL of the desired leaf frame is
-// `leaf_url`, e.g. some URL that ends up at `/echoheader?Cookie`. `leaf_path`
-// is the sequence of frame indices to choose when traversing from the top level
-// frame to the leaf frame.
+// Returns the text contents of the leaf frame. `frame_tree` contains the query
+// string to be passed to cross_site_iframe_factory, where the desired leaf
+// frame is some URL that ends up at `/echoheader?Cookie`. `leaf_path` is the
+// sequence of frame indices to choose when traversing from the top level frame
+// to the leaf frame.
 std::string ArrangeFramesAndGetContentFromLeaf(
     WebContents* web_contents,
     net::EmbeddedTestServer* server,
-    const std::string& frame_tree_pattern,
-    const std::vector<int>& leaf_path,
-    const GURL& leaf_url);
+    std::string_view frame_tree,
+    const std::vector<int>& leaf_path);
 
-// Returns the cookies for the leaf's origin.
-// `frame_tree_pattern` contains a format template for the query string to be
-// passed to cross_site_iframe_factory, where the URL of the desired leaf
-// frame is `leaf_url`, e.g. some URL that ends up at `/set-cookie`.
+// Returns the cookies for a specified `cookie_url`.
 std::vector<net::CanonicalCookie> ArrangeFramesAndGetCanonicalCookiesForLeaf(
     WebContents* web_contents,
     net::EmbeddedTestServer* server,
-    const std::string& frame_tree_pattern,
-    const GURL& leaf_url);
-
-// Same as above but returns cookies for a separately specified `cookie_url`.
-std::vector<net::CanonicalCookie> ArrangeFramesAndGetCanonicalCookiesForLeaf(
-    WebContents* web_contents,
-    net::EmbeddedTestServer* server,
-    const std::string& frame_tree_pattern,
-    const GURL& leaf_url,
+    std::string_view frame_tree,
     const GURL& cookie_url);
 
 }  // namespace content
