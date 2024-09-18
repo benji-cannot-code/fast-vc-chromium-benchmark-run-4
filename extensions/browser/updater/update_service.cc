ab37006bfbff51e5c95988d28d8e1c492fbca723
Solomon Kinard <solomonkinard@chromium.org>
2024-09-18 18:07:24 +0000

    Extensions: Add curly braces to reduce cognitive load
    This CL was uploaded by git cl split.
    
    R=burunduk@chromium.org
    
    Bug: chromium:346952289
    Change-Id: Ied157a7c67ed4f9e0fa0cff529d08341dad08f2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5857658
    Reviewed-by: Oleg Davydov <burunduk@chromium.org>
    Commit-Queue: Solomon Kinard <solomonkinard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357211}

--- extensions/browser/updater/update_service.cc
+++ extensions/browser/updater/update_service.cc
@@ -168,8 +168,9 @@ void UpdateService::StartUpdateCheck(
 
   if (!ExtensionsBrowserClient::Get()->IsBackgroundUpdateAllowed()) {
     VLOG(1) << "UpdateService - Extension update not allowed.";
-    if (!callback.is_null())
+    if (!callback.is_null()) {
       std::move(callback).Run();
+    }
     return;
   }
 
@@ -243,20 +244,23 @@ void UpdateService::UpdateCheckComplete(InProgressUpdate update) {
     }
   }
 
-  if (!update.callback.is_null())
+  if (!update.callback.is_null()) {
     std::move(update.callback).Run();
+  }
 }
 
 void UpdateService::AddUpdateClientObserver(
     update_client::UpdateClient::Observer* observer) {
-  if (update_client_)
+  if (update_client_) {
     update_client_->AddObserver(observer);
+  }
 }
 
 void UpdateService::RemoveUpdateClientObserver(
     update_client::UpdateClient::Observer* observer) {
-  if (update_client_)
+  if (update_client_) {
     update_client_->RemoveObserver(observer);
+  }
 }
 
 base::Value::Dict UpdateService::GetExtensionOmahaAttributes(
@@ -269,8 +273,9 @@ base::Value::Dict UpdateService::GetExtensionOmahaAttributes(
     // or does not exist.
     // Only create the attribute if it's defined in the custom update check
     // data. We want to distinguish true, false and undefined values.
-    if (iter != update_item.custom_updatecheck_data.end())
+    if (iter != update_item.custom_updatecheck_data.end()) {
       attributes.Set(key, iter->second == "true");
+    }
   }
   return attributes;
 }
