e9310cac545665dd21d3049a79adeb761c6268c0
Qingxin Wu <qingxinwu@google.com>
2024-09-18 18:43:06 +0000

    Rename Base64ToBase64URL to Base64URLToBase64.
    ... to make it match what the function does.
    
    Change-Id: Iad97e1e18181bf03f84f4359ec15a2b866a97c9c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873343
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Commit-Queue: Qingxin Wu <qingxinwu@google.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357232}

--- base/base64url.cc
+++ base/base64url.cc
@@ -43,7 +43,7 @@ class StringViewOrString {
 };
 
 // Converts the base64url `input` into a plain base64 string.
-std::optional<StringViewOrString> Base64ToBase64URL(
+std::optional<StringViewOrString> Base64UrlToBase64(
     std::string_view input,
     Base64UrlDecodePolicy policy) {
   // Characters outside of the base64url alphabet are disallowed, which includes
@@ -133,7 +133,7 @@ bool Base64UrlDecode(std::string_view input,
                      Base64UrlDecodePolicy policy,
                      std::string* output) {
   std::optional<StringViewOrString> base64_input =
-      Base64ToBase64URL(input, policy);
+      Base64UrlToBase64(input, policy);
   if (!base64_input) {
     return false;
   }
@@ -144,7 +144,7 @@ std::optional<std::vector<uint8_t>> Base64UrlDecode(
     std::string_view input,
     Base64UrlDecodePolicy policy) {
   std::optional<StringViewOrString> base64_input =
-      Base64ToBase64URL(input, policy);
+      Base64UrlToBase64(input, policy);
   if (!base64_input) {
     return std::nullopt;
   }
