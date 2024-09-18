82660f590abeceb0d9d829f8c9d8997b46e06e96
Kent Tamura <tkent@chromium.org>
2024-09-18 01:58:46 +0000

    Remove "InlineCursorMultiColFix" runtime flag
    It has been enabled since M128, and no issues were reported.
    
    Bug: 343749045
    Change-Id: Ifb034815f17df3960f554b10bfce59b9740a80d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869655
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1356860}

--- third_party/blink/renderer/core/layout/inline/inline_cursor.cc
+++ third_party/blink/renderer/core/layout/inline/inline_cursor.cc
@@ -1780,8 +1780,7 @@ void InlineCursor::MoveToVisualFirstOrLastForCulledInline(bool last) {
   }
 
   DCHECK(found_position);
-  if (RuntimeEnabledFeatures::InlineCursorMultiColFixEnabled() &&
-      fragment_index_ > found_fragment_index) {
+  if (fragment_index_ > found_fragment_index) {
     while (fragment_index_ > found_fragment_index) {
       DecrementFragmentIndex();
     }
