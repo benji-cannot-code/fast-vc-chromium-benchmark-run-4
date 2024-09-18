d4900f1d292d9411b74893dacc3c9b72e06779f8
Eitan Goldberger <eitang@google.com>
2024-09-18 22:11:25 +0000

    [Read aloud] Ensure isAutoVoiceSwitchingEnabled is true in all cases.
    This removes an occurrence where we were setting the flag to false.
    Also, now that isAutoVoiceSwitchingEnabled is enabled by default in
    prod, we don't need to explicitly set the flag to true, so I removed
    explicit enabling of the feature from tests.
    
    Bug: 355004453
    Change-Id: Ie0d9f61a1a8401e47b4ded7dfde26a13c461e67a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873284
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Eitan Goldberger <eitang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357346}

--- chrome/test/data/webui/side_panel/read_anything/prefs_test.ts
+++ chrome/test/data/webui/side_panel/read_anything/prefs_test.ts
@@ -43,7 +43,6 @@ suite('PrefsTest', () => {
 
     suite('with no initial voices', () => {
       setup(() => {
-        chrome.readingMode.isAutoVoiceSwitchingEnabled = false;
         chrome.readingMode.baseLanguageForSpeech = 'en';
 
         // Set synthesis to have no available voices
