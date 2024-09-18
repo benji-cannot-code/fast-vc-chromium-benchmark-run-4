b7c97bc6f03b3b51fd1dcf80b4565ab55c6213e7
Haihan Chen <haihan@google.com>
2024-09-18 21:53:40 +0000

    [File Download Access Prevention] Add deobfuscation on-the-fly part 1
    Add logic to deobfuscate the file on-the-fly before they are sent to WP servers for deep scanning analysis.
    
    1. Since we need to keep track of the current chunk as we might not be able to write it all at once to pipe, added a helper method in download_obfuscator to keep track of the current offset within the chunk and to avoid doing deobfuscating the same chunk multiple times.
    
    2. For the request to send successfully, we need to update the file size provided to the request body, this will be done in a subsequent CL.
    
    Bug: b/349858060
    Change-Id: Ia6c85e5163c5389821de52882e1696901dc91a3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871570
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357338}

--- chrome/browser/safe_browsing/cloud_content_scanning/connector_data_pipe_getter.h
+++ chrome/browser/safe_browsing/cloud_content_scanning/connector_data_pipe_getter.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/read_only_shared_memory_region.h"
 #include "base/memory/shared_memory_mapping.h"
 #include "base/time/time.h"
+#include "components/enterprise/obfuscation/core/download_obfuscator.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "services/network/public/mojom/data_pipe_getter.mojom.h"
 
@@ -188,6 +189,8 @@ class ConnectorDataPipeGetter : public network::mojom::DataPipeGetter {
 
   // Indicates if this data pipe streams a pipe. If false, it streams a page.
   bool file_data_pipe_;
+
+  std::unique_ptr<enterprise_obfuscation::DownloadObfuscator> deobfuscator_;
 
   mojo::ScopedDataPipeProducerHandle pipe_;
   std::unique_ptr<mojo::SimpleWatcher> watcher_;
