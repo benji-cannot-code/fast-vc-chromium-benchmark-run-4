8ca549c495cdbe30be973a3c37c58cdbfa0d7e21
Dale Curtis <dalecurtis@chromium.org>
2024-09-19 21:59:00 +0000

    Strip unused generic prioritization code from DecoderSelector.
    We don't have plans for the generic priority code, it ends up making
    the specific case we care about (resolution based priority for hw/sw
    decoders) harder to reason about too.
    
    R=sandersd
    
    Bug: 361823989
    Change-Id: I8299c76b793ca06e6e4ec129fafc5549b35fe9c9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874908
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357876}

--- media/filters/decoder_stream_traits.h
+++ media/filters/decoder_stream_traits.h
@@ -143,10 +143,6 @@ class MEDIA_EXPORT DecoderStreamTraits<DemuxerStream::VIDEO> {
   void OnStreamReset(DemuxerStream* stream);
   void OnOutputReady(OutputType* output);
 
-  // Set whether or not software decoder implementations will be preferred.
-  void SetPreferNonPlatformDecoders(bool);
-  bool GetPreferNonPlatformDecoders() const;
-
  private:
   base::TimeDelta last_keyframe_timestamp_;
   base::MovingAverage<base::TimeDelta, base::TimeDelta>
@@ -163,8 +159,6 @@ class MEDIA_EXPORT DecoderStreamTraits<DemuxerStream::VIDEO> {
   PipelineStatistics stats_;
 
   VideoTransformation transform_ = kNoTransformation;
-
-  bool prefer_non_platform_decoders_ = false;
 
   base::WeakPtr<DecoderStreamTraits<DemuxerStream::VIDEO>> weak_this_;
   base::WeakPtrFactory<DecoderStreamTraits<DemuxerStream::VIDEO>> weak_factory_{
