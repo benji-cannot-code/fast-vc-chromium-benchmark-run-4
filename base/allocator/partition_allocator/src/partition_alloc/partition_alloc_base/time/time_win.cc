2a778f18a9011dc1b6b31393b39b0d611d0ba5ff
Sergey Markelov <sergionso@gmail.com>
2024-09-19 23:55:25 +0000

    base: Improve CurrentWallclockMicroseconds() on Windows
    GetSystemTimeAsFileTime() is not precise and can make the wall clock
    timer Time::Now() jump back up to ~15ms depending on the last hardware
    timer interruption, that breaks the monotonic time flow.
    
    GetSystemTimePreciseAsFileTime() is precise and eliminates possible
    monotonic time jumps back, however is not always fast on some hardware.
    It's still worth to use the functions, since it's called to adjust the
    wall clock timer only once a minute.
    
    The perftest demonstrates different results on distinct hardware.
    
    GetSystemTimePreciseAsFileTime() is faster:
    -----------------------------------------------------
               Min Time   Avg Time   Max Time  Iterations
    -----------------------------------------------------
    Precise       24 ns      26 ns      81 ns     1869000
    Imprecise     31 ns      36 ns     125 ns     1367000
    
    GetSystemTimePreciseAsFileTime() is slower:
    -----------------------------------------------------
               Min Time   Avg Time   Max Time  Iterations
    -----------------------------------------------------
    Precise       19 ns      19 ns      90 ns     2503000
    Imprecise      6 ns       6 ns      47 ns     7854000
    
    [1] https://learn.microsoft.com/en-us/windows/win32/sysinfo/acquiring-high-resolution-time-stamps
    [2] https://devblogs.microsoft.com/oldnewthing/20170921-00/?p=97057
    [3] https://github.com/dotnet/coreclr/pull/9736
    
    Change-Id: Id2f02f640f8c90d95b14b7967d9b4baefdb977cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5389576
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357910}

--- base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/time/time_win.cc
+++ base/allocator/partition_allocator/src/partition_alloc/partition_alloc_base/time/time_win.cc
@@ -8,8 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // A good article:  http://www.ddj.com/windows/184416651
 // A good mozilla bug:  http://bugzilla.mozilla.org/show_bug.cgi?id=363258
 //
-// The default windows timer, GetSystemTimeAsFileTime is not very precise.
-// It is only good to ~15.5ms.
+// The default windows timer, GetSystemTimePreciseAsFileTime is quite precise.
+// However it is not always fast on some hardware and is slower than the
+// performance counters.
 //
 // QueryPerformanceCounter is the logical choice for a high-precision timer.
 // However, it is known to be buggy on some hardware.  Specifically, it can
@@ -78,7 +79,7 @@ FILETIME MicrosecondsToFileTime(int64_t us) {
 
 int64_t CurrentWallclockMicroseconds() {
   FILETIME ft;
-  ::GetSystemTimeAsFileTime(&ft);
+  ::GetSystemTimePreciseAsFileTime(&ft);
   return FileTimeToMicroseconds(ft);
 }
 
@@ -114,8 +115,8 @@ Time TimeNowIgnoringOverride() {
   }
 
   // We implement time using the high-resolution timers so that we can get
-  // timeouts which are smaller than 10-15ms.  If we just used
-  // CurrentWallclockMicroseconds(), we'd have the less-granular timer.
+  // timeouts which likely are smaller than those if we just used
+  // CurrentWallclockMicroseconds().
   //
   // To make this work, we initialize the clock (g_initial_time) and the
   // counter (initial_ctr).  To compute the initial time, we can check
