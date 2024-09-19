e46b010c826e2c6b8afbbed638a1d1d0135f6200
Bruno Braga <brunobraga@google.com>
2024-09-19 10:23:31 +0000

    Modularize chrome/browser/user_annotation
    This is required because in a follow up I need to add it as a dep
    of chrome/common/extensions. It also better in general to have fine
    grained dependencies.
    
    UserAnnotationsServiceFactory was removed from android in this CL. There
    is no android uses that needs/uses it now nor in the past.
    
    Bug: b/366172297
    Change-Id: I40c0013142bd76edb9d1a4186067789f15d84cca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870056
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357532}

--- chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
+++ chrome/browser/browsing_data/chrome_browsing_data_remover_delegate_unittest.cc
@@ -2539,6 +2539,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateTest,
   ASSERT_FALSE(tester.HasProfileAndCard());
 }
 
+#if !BUILDFLAG(IS_ANDROID)
 class ChromeBrowsingDataRemoverDelegateUserAnnotationsTest
     : public ChromeBrowsingDataRemoverDelegateTest {
  public:
@@ -2581,6 +2582,7 @@ TEST_F(ChromeBrowsingDataRemoverDelegateUserAnnotationsTest,
               testing::Pair(testing::Eq(time_now),
                             testing::Eq(time_now + base::Hours(1))));
 }
+#endif
 
 TEST_F(ChromeBrowsingDataRemoverDelegateTest, ZeroSuggestPrefsBasedCacheClear) {
   // Disable in-memory ZPS caching.
--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -1351,7 +1351,9 @@ void ChromeBrowserMainExtraPartsProfiles::
 #if !BUILDFLAG(IS_ANDROID)
   UsbConnectionTrackerFactory::GetInstance();
 #endif
+#if !BUILDFLAG(IS_ANDROID)
   UserAnnotationsServiceFactory::GetInstance();
+#endif
 #if !BUILDFLAG(IS_ANDROID)
   UserEducationServiceFactory::GetInstance();
 #endif
