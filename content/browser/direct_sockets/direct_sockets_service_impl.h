36fe897b9c8e3e9a6f0a442f86064b39ba46b04d
Andrew Rayskiy <greengrape@google.com>
2024-09-19 22:29:10 +0000

    ds: Catch connection attempts to private endpoints
    This CL catches connection attempts to private endpoints and queries
    the embedder on whether PNA should be allowed or not.
    
    * For TCPSocket / UDPSocket (conn mode), PNA is requested if the
      resolved address falls into the private space;
    * For UDPSocket (bound mode), PNA is always requested;
    * For TCPServerSocket, no PNA considerations are put in place.
    
    Bug: 367744286
    Change-Id: I929401d4cb4be2bb4a36d8d7f2920018bdd1d9c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5809279
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357887}

--- content/browser/direct_sockets/direct_sockets_service_impl.h
+++ content/browser/direct_sockets/direct_sockets_service_impl.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DIRECT_SOCKETS_DIRECT_SOCKETS_SERVICE_IMPL_H_
 #define CONTENT_BROWSER_DIRECT_SOCKETS_DIRECT_SOCKETS_SERVICE_IMPL_H_
 
+#include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/document_service.h"
 #include "content/public/browser/render_frame_host.h"
@@ -81,6 +82,13 @@ class CONTENT_EXPORT DirectSocketsServiceImpl
       const std::optional<net::AddressList>& resolved_addresses,
       const std::optional<net::HostResolverEndpointResults>&);
 
+  void CreateTCPConnectedSocketImpl(
+      const net::AddressList& resolved_addresses,
+      network::mojom::TCPConnectedSocketOptionsPtr options,
+      mojo::PendingReceiver<network::mojom::TCPConnectedSocket>,
+      mojo::PendingRemote<network::mojom::SocketObserver>,
+      OpenTCPSocketCallback);
+
   void OnResolveCompleteForUDPSocket(
       blink::mojom::DirectConnectedUDPSocketOptionsPtr,
       mojo::PendingReceiver<network::mojom::RestrictedUDPSocket>,
@@ -91,12 +99,23 @@ class CONTENT_EXPORT DirectSocketsServiceImpl
       const std::optional<net::AddressList>& resolved_addresses,
       const std::optional<net::HostResolverEndpointResults>&);
 
+  void CreateRestrictedUDPSocketImpl(
+      const net::IPEndPoint& peer_addr,
+      network::mojom::RestrictedUDPSocketMode mode,
+      network::mojom::RestrictedUDPSocketParamsPtr options,
+      mojo::PendingReceiver<network::mojom::RestrictedUDPSocket>,
+      mojo::PendingRemote<network::mojom::UDPSocketListener>,
+      base::OnceCallback<void(int32_t, const std::optional<net::IPEndPoint>&)>
+          callback);
+
   std::unique_ptr<network::SimpleHostResolver> resolver_;
 
 #if BUILDFLAG(IS_CHROMEOS)
   class FirewallHoleDelegate;
   std::unique_ptr<FirewallHoleDelegate> firewall_hole_delegate_;
 #endif  // BUILDFLAG(IS_CHROMEOS)
+
+  base::WeakPtrFactory<DirectSocketsServiceImpl> weak_factory_{this};
 };
 
 }  // namespace content
