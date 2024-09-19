a21babecb10471a168b271104695256bd1a8a6d8
Joseph Park <josephjoopark@chromium.org>
2024-09-19 22:41:21 +0000

    Prevent buttons that shouldn't be visible from showing in overflow menu
    Bug: 368059209
    Change-Id: Ie19878954b8f87b2485ec0d73361bc89de9fee1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874729
    Commit-Queue: Joseph Park <josephjoopark@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Auto-Submit: Joseph Park <josephjoopark@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357893}

--- chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.cc
+++ chrome/browser/ui/views/toolbar/pinned_toolbar_actions_container.cc
@@ -573,7 +573,9 @@ bool PinnedToolbarActionsContainer::IsOverflowed(const actions::ActionId& id) {
   // button but is in the process of revealing it.
   const auto* const layout =
       GetAnimatingLayoutManager()->target_layout().GetLayoutFor(pinned_button);
-  return layout && (!GetVisible() || !layout->visible);
+  return GetAnimatingLayoutManager()->target_layout_manager()->CanBeVisible(
+             pinned_button) &&
+         layout && (!GetVisible() || !layout->visible);
 }
 
 views::View* PinnedToolbarActionsContainer::GetContainerView() {
@@ -593,7 +595,9 @@ bool PinnedToolbarActionsContainer::ShouldAnyButtonsOverflow(
   for (PinnedActionToolbarButton* pinned_button : pinned_buttons_) {
     if (views::ChildLayout* child_layout =
             proposed_layout.GetLayoutFor(pinned_button)) {
-      if (!child_layout->visible) {
+      if (GetAnimatingLayoutManager()->target_layout_manager()->CanBeVisible(
+              pinned_button) &&
+          !child_layout->visible) {
         return true;
       }
     }
