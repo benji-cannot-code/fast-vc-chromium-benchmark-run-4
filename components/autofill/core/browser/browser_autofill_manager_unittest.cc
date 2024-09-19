29c6addf89a8374c375619cfe77426fda086edfe
Wolfgang Billenstein <bwolfgang@google.com>
2024-09-19 17:54:52 +0000

    Check for user annotations readiness in Autofill
    Adds check for the existence of data in user annotations before
    potentially showing prediction improvements suggestions.
    
    Fixed: 366219069,365721733
    Change-Id: I287d8ded9958f5beacd614304f1a004843e4ede7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854737
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357715}

--- components/autofill/core/browser/browser_autofill_manager_unittest.cc
+++ components/autofill/core/browser/browser_autofill_manager_unittest.cc
@@ -7234,6 +7234,9 @@ TEST_F(BrowserAutofillManagerTest, ShowPredictionImprovementsSuggestions) {
       .WillByDefault(Return(&delegate));
   ON_CALL(delegate, ShouldProvidePredictionImprovements)
       .WillByDefault(Return(true));
+  EXPECT_CALL(delegate, HasDataStored)
+      .WillOnce(RunOnceCallback<0>(
+          AutofillPredictionImprovementsDelegate::HasData(true)));
   EXPECT_CALL(delegate, MaybeUpdateSuggestions)
       .WillOnce([](std::vector<Suggestion>& address_suggestions,
                    const FormFieldData& field,
