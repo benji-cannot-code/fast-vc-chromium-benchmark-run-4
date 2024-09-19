24449e61643be5311a39e0b91588b0010bf2e075
Mark Schillaci <mschillaci@google.com>
2024-09-19 18:08:33 +0000

    [Page Zoom] Enabled AccessibilityPageZoom feature by default
    AX-Relnotes: N/A
    Bug: 40484405
    Change-Id: I66520596f5045312472bf66ca2a99fbbd60db61a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874118
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357726}

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -1277,7 +1277,7 @@ BASE_FEATURE(kAccessibilityIncludeLongClickAction,
 // updated UI to replace existing Chrome Accessibility Settings.
 BASE_FEATURE(kAccessibilityPageZoom,
              "AccessibilityPageZoom",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 // Controls whether the OS-level font setting is adjusted for.
 const base::FeatureParam<bool> kAccessibilityPageZoomOSLevelAdjustment{
