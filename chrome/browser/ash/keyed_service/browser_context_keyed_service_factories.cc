4068991274a791c64f8ccd03b835b3c25287bb32
Georg Neis <neis@chromium.org>
2024-09-19 00:28:01 +0000

    Create chrome/browser/ash/keyed_service
    ... and move browser_context_keyed_service_factories.* there.
    
    Bug: b:367544996, b:361318089
    Change-Id: Ife0dfd2dc6aa3b66adc03ad130b64e73713ef77c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869491
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357419}

--- chrome/browser/ash/keyed_service/browser_context_keyed_service_factories.cc
+++ chrome/browser/ash/keyed_service/browser_context_keyed_service_factories.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ash/browser_context_keyed_service_factories.h"
+#include "chrome/browser/ash/keyed_service/browser_context_keyed_service_factories.h"
 
 #include "ash/constants/ash_features.h"
 #include "ash/webui/help_app_ui/help_app_manager_factory.h"
