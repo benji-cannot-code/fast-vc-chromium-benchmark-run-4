89239e93ef30e26b5aae6c6b0181ba7d8cfa8322
Awad Osman <awado@google.com>
2024-09-19 16:40:36 +0000

    Remove ESB ripple flag
    We are no longer pursuing this project as it didn't meet desktop
    requirements.
    
    Bug: 358404369
    Change-Id: Ib7b7b4ed1369776f69ea6473512dc52beec60cfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873581
    Reviewed-by: thefrog <thefrog@chromium.org>
    Commit-Queue: Awad Osman <awado@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357658}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11834,11 +11834,6 @@ const FeatureEntry kFeatureEntries[] = {
          "SensitiveContent")},
 #endif  // BUILDFLAG(IS_ANDROID)
 
-    {"safe-browsing-ripple-effect",
-     flag_descriptions::kRippleForEnhancedProtectionName,
-     flag_descriptions::kRippleForEnhancedProtectionDescription, kOsDesktop,
-     FEATURE_VALUE_TYPE(safe_browsing::kRippleForEnhancedProtection)},
-
     {"data-sharing", flag_descriptions::kDataSharingName,
      flag_descriptions::kDataSharingDescription, kOsAll,
      FEATURE_VALUE_TYPE(data_sharing::features::kDataSharingFeature)},
