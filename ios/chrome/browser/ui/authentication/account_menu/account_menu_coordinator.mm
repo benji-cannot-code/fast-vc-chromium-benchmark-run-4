8fe16759f9114d9b7ec6fb89339acf095db0f8ea
Arthur Milchior <arthurmilchior@chromium.org>
2024-09-19 13:35:23 +0000

    [iOS][Account menu] Remove an unused method
    Bug: None
    Change-Id: Ie6b0167e84ee110fb064ea87f0f9a4b78bd5e3d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873129
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357566}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_coordinator.mm
@@ -186,12 +186,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #pragma mark - AccountMenuMediatorDelegate
 
-- (void)viewControllerWantsToBeClosed:
-    (AccountMenuViewController*)viewController {
-  CHECK_EQ(_viewController, viewController);
-  [self.delegate acountMenuCoordinatorShouldStop:self];
-}
-
 - (void)didTapManageYourGoogleAccount {
   __weak __typeof(self) weakSelf = self;
   _accountDetailsControllerDismissCallback =
