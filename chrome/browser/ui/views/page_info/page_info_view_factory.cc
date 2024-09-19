1283c9d584ae45d9925eee5c590aba1e6cea1740
Andrew Rayskiy <greengrape@google.com>
2024-09-19 18:07:24 +0000

    Display the WebPrinting content setting in PageInfo
    These are the missing bits from crrev.com/c/5140170.
    
    go/web-printing-content-setting provides the necessary UX context.
    
    The CL also changes the behavior of WebPrinting in site settings helper
    to reflect that this is an IWA-centric content setting.
    
    Low-Coverage-Reason: TRIVIAL_CHANGE Adding just one entry to a switch.
    Bug: 365113423
    Change-Id: Ie9ae1f4bd80571d30882b1f3cd7a9c0783fba073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840215
    Reviewed-by: Filipa Senra <fsenra@google.com>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357724}

--- chrome/browser/ui/views/page_info/page_info_view_factory.cc
+++ chrome/browser/ui/views/page_info/page_info_view_factory.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/page_info/page_info_navigation_handler.h"
 #include "chrome/browser/ui/views/page_info/page_info_permission_content_view.h"
 #include "chrome/browser/ui/views/page_info/page_info_security_content_view.h"
+#include "components/content_settings/core/common/content_settings_types.h"
 #include "components/page_info/core/features.h"
 #include "components/page_info/core/proto/about_this_site_metadata.pb.h"
 #include "components/page_info/page_info.h"
@@ -554,6 +555,9 @@ const ui::ImageModel PageInfoViewFactory::GetPermissionIcon(
       break;
     case ContentSettingsType::POINTER_LOCK:
       icon = &vector_icons::kPointerLockIcon;
+      break;
+    case ContentSettingsType::WEB_PRINTING:
+      icon = &vector_icons::kPrinterIcon;
       break;
     default:
       // All other |ContentSettingsType|s do not have icons on desktop or are
