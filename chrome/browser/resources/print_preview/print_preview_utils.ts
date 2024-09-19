9fab1b1aa72e457b1c33bd63699d4dd3196f69e0
dpapad <dpapad@chromium.org>
2024-09-19 18:30:42 +0000

    WebUI: Remove iron-iconset-svg from Desktop builds.
    Also remove backwards compatibility with iron-iconset-svg in
    iconset_map.ts.
    
    Bug: 40943652
    Change-Id: I83530c3835725ba238ef701bef2e3bbc1d9d568f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5869465
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1357748}

--- chrome/browser/resources/print_preview/print_preview_utils.ts
+++ chrome/browser/resources/print_preview/print_preview_utils.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type {Iconset} from 'chrome://resources/cr_elements/cr_icon/iconset_map.js';
+import type {CrIconsetElement} from 'chrome://resources/cr_elements/cr_icon/cr_iconset.js';
 import {assert} from 'chrome://resources/js/assert.js';
 
 import {inDarkMode} from './dark_mode_mixin.js';
@@ -75,7 +75,7 @@ export function observerDepsDefined(args: any[]): boolean {
  *     url(path_to_arrow) separated by a comma.
  */
 export function getSelectDropdownBackground(
-    iconset: Iconset, iconName: string, el: HTMLElement): string {
+    iconset: CrIconsetElement, iconName: string, el: HTMLElement): string {
   const serializer = new XMLSerializer();
   const iconElement = iconset.createIcon(iconName);
   assert(iconElement);
