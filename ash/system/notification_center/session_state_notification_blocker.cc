1fff85d43752a9a5534d193f8df2923545a23138
Ivan Šandrk <isandrk@chromium.org>
2024-09-19 20:41:35 +0000

    [Bedtime Hours] Display post-logout notification
    Add code to the delegate to display the post-logout notification.
    
    Bug: b:345186543
    Change-Id: Ib1a7c4f804af0ae19260f6335216295f66a26cc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5854175
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ahmed Mehfooz <amehfooz@chromium.org>
    Commit-Queue: Ivan Šandrk <isandrk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357834}

--- ash/system/notification_center/session_state_notification_blocker.cc
+++ ash/system/notification_center/session_state_notification_blocker.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/lock_screen_notification_controller.h"
 #include "ash/system/power/battery_notification.h"
 #include "base/containers/contains.h"
+#include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller_delegate_impl.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
 #include "ui/message_center/public/cpp/notification_types.h"
@@ -68,14 +69,14 @@ bool CalculateShouldShowPopup() {
 
 bool IsAllowedDuringOOBE(std::string_view notification_id) {
   static const std::string_view kAllowedSystemNotificationIDs[] = {
-      BatteryNotification::kNotificationId};
+      BatteryNotification::kNotificationId,
+      policy::DeviceRestrictionScheduleControllerDelegateImpl::
+          kPostLogoutNotificationId};
   static const std::string_view kAllowedProfileBoundNotificationIDs[] = {
       kOOBELocaleSwitchNotificationId, kOOBEGnubbyNotificationId};
 
-  for (const auto& id : kAllowedSystemNotificationIDs) {
-    if (notification_id == id) {
-      return true;
-    }
+  if (base::Contains(kAllowedSystemNotificationIDs, notification_id)) {
+    return true;
   }
 
   // Check here not for a full name equivalence, but for a substring existence
--- ash/system/notification_center/session_state_notification_blocker_unittest.cc
+++ ash/system/notification_center/session_state_notification_blocker_unittest.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/system_notification_controller.h"
 #include "ash/test/ash_test_base.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chromeos/ash/components/policy/restriction_schedule/device_restriction_schedule_controller_delegate_impl.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/public/cpp/notification.h"
 
@@ -364,6 +365,9 @@ TEST_F(SessionStateNotificationBlockerTest, NotificationAllowedDuringOOBE) {
           {BatteryNotification::kNotificationId, true},
           {kOOBELocaleSwitchNotificationId, true},
           {kOOBEGnubbyNotificationId, true},
+          {policy::DeviceRestrictionScheduleControllerDelegateImpl::
+               kPostLogoutNotificationId,
+           true},
           {"new-fancy-notification", false},
       };
   const SessionState kOOBEStates[] = {SessionState::OOBE,
