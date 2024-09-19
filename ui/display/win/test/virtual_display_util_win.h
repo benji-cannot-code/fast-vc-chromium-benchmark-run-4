d36d7c0587d772eb2556eff08207ff9c8e54741d
Brad Triebwasser <btriebw@chromium.org>
2024-09-19 20:00:01 +0000

    Reland "Remove display id in VirtualDisplayUtil::AddDisplay."
    This is a reland of commit 557a678e03118da342b0b98c75932901857314ee
    
    Serial numbers need to start at one on mac. A serial number of 0 was causing a crash.
    
    Original change's description:
    > Remove display id in VirtualDisplayUtil::AddDisplay.
    >
    > Removes the ability for callers to specify a display ID on
    > VirtualDisplayUtil::AddDisplay. Most platforms (except for Mac) cannot
    > influence the resulting display::Screen id. Instead, each virtual
    > display controller maintains its own internal ids.
    >
    >
    > Bug: 40271794
    > Change-Id: If8707c2ed4ae98e59ea831f43e7fdd28797a1c3f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5840551
    > Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    > Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    > Reviewed-by: Mike Wasserman <msw@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1356226}
    
    Bug: 40271794
    Change-Id: Ia6127882163faf7db7082570a3d77df0d07da71a
    Include-Ci-Only-Tests: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870624
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357809}

--- ui/display/win/test/virtual_display_util_win.h
+++ ui/display/win/test/virtual_display_util_win.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_map.h"
+#include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
 #include "third_party/win_virtual_display/controller/display_driver_controller.h"
@@ -40,7 +41,7 @@ class VirtualDisplayUtilWin : public display::DisplayObserver,
   static bool IsAPIAvailable();
 
   // VirtualDisplayUtil overrides:
-  int64_t AddDisplay(uint8_t id, const DisplayParams& display_params) override;
+  int64_t AddDisplay(const DisplayParams& display_params) override;
   void RemoveDisplay(int64_t display_id) override;
   void ResetDisplays() override;
 
@@ -55,6 +56,9 @@ class VirtualDisplayUtilWin : public display::DisplayObserver,
   void StartWaiting();
   void StopWaiting();
 
+  // Creates a new internal display ID to identify the display to the driver.
+  static uint8_t SynthesizeInternalDisplayId();
+
   raw_ptr<Screen> screen_;
   // True if the environment was considered headless during initialization.
   const bool is_headless_;
@@ -62,7 +66,7 @@ class VirtualDisplayUtilWin : public display::DisplayObserver,
   DisplayDriverController driver_controller_;
   // Contains the last configuration that was set.
   DriverProperties current_config_;
-  // Map of virtual display ID (product code) to corresponding display ID.
+  // Map of internal display ID (product code) to corresponding display ID.
   base::flat_map<unsigned short, int64_t> virtual_displays_;
   // Copy of the display list when this utility was constructed.
   std::vector<display::Display> initial_displays_;
