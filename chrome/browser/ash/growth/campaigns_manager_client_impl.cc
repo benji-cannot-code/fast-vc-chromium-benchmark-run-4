a0dcd8658f88e3493889251b456d4b1f3c4eaecf
Tao Wu <wutao@chromium.org>
2024-09-19 16:39:56 +0000

    growth: Guard the App Mall campaign by its feature
    Only trigger campaigns when IsCrosMallSwaEnabled is true.
    With this guard, the users not in the experiments will not trigger the
    campaign, otherwise it may cause an error when trying to show the nudge
    on not existing shelf icon.
    
    Bug: 367354166
    Test: Will add ui test in 368145269
    Change-Id: Iefe0498ab7df4c3d9c2f8e0729b494b4d1bad1c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874236
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: Li Lin <llin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357657}

--- chrome/browser/ash/growth/campaigns_manager_client_impl.cc
+++ chrome/browser/ash/growth/campaigns_manager_client_impl.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/growth/campaigns_manager.h"
 #include "chromeos/ash/components/growth/campaigns_utils.h"
 #include "chromeos/ash/components/growth/growth_metrics.h"
+#include "chromeos/constants/chromeos_features.h"
 #include "components/component_updater/ash/component_manager_ash.h"
 #include "components/feature_engagement/public/feature_constants.h"
 #include "components/feature_engagement/public/tracker.h"
@@ -209,8 +210,17 @@ void CampaignsManagerClientImpl::RecordEvent(const std::string& event_name,
                        << growth::ToString(trigger_campaigns);
   tracker->NotifyEvent(AddEventPrefix(event_name));
 
-  if (auto* session = CampaignsManagerSession::Get();
-      session && trigger_campaigns) {
+  if (!trigger_campaigns) {
+    return;
+  }
+
+  // If the App Mall app is not enabled, do not trigger by the event.
+  if (event_name == growth::kGrowthCampaignsEventHotseatHover &&
+      !chromeos::features::IsCrosMallSwaEnabled()) {
+    return;
+  }
+
+  if (auto* session = CampaignsManagerSession::Get()) {
     session->MaybeTriggerCampaignsOnEvent(event_name);
   }
 }
