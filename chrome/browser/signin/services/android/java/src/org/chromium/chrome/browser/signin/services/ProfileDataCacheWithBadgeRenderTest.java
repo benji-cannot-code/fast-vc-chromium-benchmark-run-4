fc8d9763042e5c1a1616c5b9e6541fa869831314
Samar <samarchehade@google.com>
2024-09-19 05:42:16 +0000

    [Signin][Fixit] Use constant AccountInfo in ProfileDataCache renderTests
    This will make it easier to update the tests in
    https://crrev.com/c/5865417
    
    Bug: 40890215
    Change-Id: I4211bc2b16890a70c47e1c1595ede62874bbad53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872725
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357479}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheWithBadgeRenderTest.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheWithBadgeRenderTest.java
@@ -38,7 +38,6 @@ import java.io.IOException;
 @RunWith(ChromeJUnit4ClassRunner.class)
 @Batch(ProfileDataCacheRenderTest.PROFILE_DATA_BATCH_NAME)
 public class ProfileDataCacheWithBadgeRenderTest extends BlankUiTestActivityTestCase {
-    private static final String TEST_ACCOUNT_NAME = "test@example.com";
 
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
@@ -55,7 +54,7 @@ public class ProfileDataCacheWithBadgeRenderTest extends BlankUiTestActivityTest
 
     @Before
     public void setUp() {
-        mAccountManagerTestRule.addAccount(TEST_ACCOUNT_NAME);
+        mAccountManagerTestRule.addAccount(AccountManagerTestRule.TEST_ACCOUNT_1);
 
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
@@ -138,14 +137,16 @@ public class ProfileDataCacheWithBadgeRenderTest extends BlankUiTestActivityTest
                 () -> {
                     return !TextUtils.isEmpty(
                             mProfileDataCache
-                                    .getProfileDataOrDefault(TEST_ACCOUNT_NAME)
+                                    .getProfileDataOrDefault(
+                                            AccountManagerTestRule.TEST_ACCOUNT_1.getEmail())
                                     .getFullName());
                 });
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     mImageView.setImageDrawable(
                             mProfileDataCache
-                                    .getProfileDataOrDefault(TEST_ACCOUNT_NAME)
+                                    .getProfileDataOrDefault(
+                                            AccountManagerTestRule.TEST_ACCOUNT_1.getEmail())
                                     .getImage());
                 });
     }
@@ -159,14 +160,16 @@ public class ProfileDataCacheWithBadgeRenderTest extends BlankUiTestActivityTest
                 () -> {
                     return !TextUtils.isEmpty(
                             mProfileDataCache
-                                    .getProfileDataOrDefault(TEST_ACCOUNT_NAME)
+                                    .getProfileDataOrDefault(
+                                            AccountManagerTestRule.TEST_ACCOUNT_1.getEmail())
                                     .getFullName());
                 });
         ThreadUtils.runOnUiThreadBlocking(
                 () -> {
                     mImageView.setImageDrawable(
                             mProfileDataCache
-                                    .getProfileDataOrDefault(TEST_ACCOUNT_NAME)
+                                    .getProfileDataOrDefault(
+                                            AccountManagerTestRule.TEST_ACCOUNT_1.getEmail())
                                     .getImage());
                 });
     }
