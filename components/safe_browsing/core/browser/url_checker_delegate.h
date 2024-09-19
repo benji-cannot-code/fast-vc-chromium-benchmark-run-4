1fb67e0cb250700a93c5f62eee280eb8808a1234
Zack Han <zackhan@chromium.org>
2024-09-19 13:37:12 +0000

    Send conditional CSBRRs for background HPRT lookups
    This CL builds upon the previous change that added background HPRT
    lookups alongside regular URL real-time lookups for ESB. It adds logic
    to conditionally send a qualified CSBRR when the verdicts from these two
    lookups differ. The following changes were made in this CL:
    
    1. Store verdict information: An object was added to the class to store
    the verdict info from HPRT lookups.
    2. Reorder code in url_realtime_mechanism.cc for testing: The order of
    code execution for sending HPRT lookups was adjusted to improve
    testability due to the synchronous execution nature in tests.
    3. Construct and send CSBRR: Logic was implemented to generate a CSBRR
    when discrepancies are detected between the URL real-time and HPRT
    verdicts.
    4. Add extensive unit tests: Completed the to-do items we added in the
    tests to validate the conditional CSBRR reporting logic, covering
    various scenarios and edge cases.
    
    Bug: 359609447
    Change-Id: Id5cdbbcb50e3577d869dd61967d7305ed5fb3174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866251
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Zack Han <zackhan@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357567}

--- components/safe_browsing/core/browser/url_checker_delegate.h
+++ components/safe_browsing/core/browser/url_checker_delegate.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/optional_ref.h"
 #include "base/unguessable_token.h"
 #include "components/safe_browsing/core/browser/db/v4_protocol_manager_util.h"
+#include "components/safe_browsing/core/common/proto/csd.pb.h"
 
 namespace content {
 class WebContents;
@@ -88,6 +89,13 @@ class UrlCheckerDelegate
   // detected. |web_contents_getter| is used to determine which tab the site
   // was detected on.
   virtual void NotifySuspiciousSiteDetected(
+      const base::RepeatingCallback<content::WebContents*()>&
+          web_contents_getter) = 0;
+
+  // Send a CSBRR through UI manager to report the discrepancy info from URL
+  // real-time and hash real-time lookups.
+  virtual void SendUrlRealTimeAndHashRealTimeDiscrepancyReport(
+      std::unique_ptr<ClientSafeBrowsingReportRequest> report,
       const base::RepeatingCallback<content::WebContents*()>&
           web_contents_getter) = 0;
 
