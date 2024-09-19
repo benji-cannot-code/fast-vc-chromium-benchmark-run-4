b9242cfcdda9314f2968745d3dff0fddec487be8
Hira Mahmood <hiramahmood@google.com>
2024-09-19 13:44:32 +0000

    [iOS] Disable kSendTabToSelfIOSPushNotifications for SyncPreferencesTest
    This CL disables kSendTabToSelfIOSPushNotifications for
    SyncPreferencesTestCase/testAccountPrefValueCleanedUpOnSignout. The
    Feature causes the flaky test to fail.
    
    Bug: 368056380
    Change-Id: I140b5b77a222a64c453b311df84883009b6855f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873923
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Reviewed-by: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357571}

--- ios/chrome/browser/sync/model/prefs/sync_preferences_egtest.mm
+++ ios/chrome/browser/sync/model/prefs/sync_preferences_egtest.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "base/test/ios/wait_util.h"
 #import "base/time/time.h"
+#import "components/send_tab_to_self/features.h"
 #import "components/sync/base/command_line_switches.h"
 #import "components/sync/base/features.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
@@ -71,6 +72,13 @@ void WaitForPreferenceValue(int pref_value) {
   config.additional_args.push_back(std::string("--") +
                                    syncer::kSyncShortNudgeDelayForTest);
   config.features_enabled.push_back(syncer::kEnablePreferencesAccountStorage);
+  // TODO(crbug.com/368056380): Re-enable after fixing failure.
+  // The test fails when kSendTabToSelfIOSPushNotifications is enabled.
+  // Moreover, the test is already a bit flaky.
+  if ([self isRunningTest:@selector(testAccountPrefValueCleanedUpOnSignout)]) {
+    config.features_disabled.push_back(
+        send_tab_to_self::kSendTabToSelfIOSPushNotifications);
+  }
   return config;
 }
 
