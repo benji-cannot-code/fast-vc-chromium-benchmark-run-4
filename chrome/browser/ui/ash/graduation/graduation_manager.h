7d41fa4bee84c12c921a47a768da648d3ba6680b
Courtney Wong <courtneywong@chromium.org>
2024-09-19 17:04:11 +0000

    graduation: Pin and unpin graduation app according to policy status
    policy state apply the pin and unpin state.
    
    Tested: checked that policy changes during session and also initial
    Bug: b:357882466
    Change-Id: I3b4d0616e659be3579a35a02e099a1f4a7de6b44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870517
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357682}

--- chrome/browser/ui/ash/graduation/graduation_manager.h
+++ chrome/browser/ui/ash/graduation/graduation_manager.h
@@ -6,18 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_ASH_GRADUATION_GRADUATION_MANAGER_H_
 #define CHROME_BROWSER_UI_ASH_GRADUATION_GRADUATION_MANAGER_H_
 
-#include "base/component_export.h"
+#include "base/memory/weak_ptr.h"
 #include "base/scoped_observation.h"
+#include "components/prefs/pref_change_registrar.h"
 #include "components/session_manager/core/session_manager.h"
 #include "components/session_manager/core/session_manager_observer.h"
+
+class Profile;
 
 namespace ash::graduation {
 
 // Manages the state of the Graduation app depending on the status of the
 // Graduation enablement policy. The GraduationManager is a singleton that
 // should be created once per user session.
-class COMPONENT_EXPORT(GRADUATION_MANAGER) GraduationManager
-    : public session_manager::SessionManagerObserver {
+class GraduationManager : public session_manager::SessionManagerObserver {
  public:
   GraduationManager();
   GraduationManager(const GraduationManager&) = delete;
@@ -30,9 +32,20 @@ class COMPONENT_EXPORT(GRADUATION_MANAGER) GraduationManager
   void OnUserSessionStarted(bool is_primary) override;
 
  private:
+  void OnAppsSynchronized();
+  void OnWebAppProviderReady();
+  void UpdateAppPinnedState();
+
+  PrefChangeRegistrar pref_change_registrar_;
+
+  // Profile object will be nullptr until the user session begins.
+  raw_ptr<Profile> profile_ = nullptr;
+
   base::ScopedObservation<session_manager::SessionManager,
                           session_manager::SessionManagerObserver>
       session_manager_observation_{this};
+
+  base::WeakPtrFactory<GraduationManager> weak_ptr_factory_{this};
 };
 }  // namespace ash::graduation
 
