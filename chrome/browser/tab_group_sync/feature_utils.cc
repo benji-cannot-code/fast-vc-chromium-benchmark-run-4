ad6474772e04253406a3182c5e32ed9a7d4686fa
Shakti Sahu <shaktisahu@chromium.org>
2024-09-19 15:25:41 +0000

    [TG Sync] Return null service if the feature flags aren't enabled
    There is a chance that someone can invoke TabGroupSyncServiceFactory
    even when the feature flags aren't enabled. This CL returns a
    nullptr for those cases in native.
    Also as a result, the build target //chrome/browser/tab_group_sync:utils
    was moved from android-only to all platform.
    For desktop, since we use TabGroupSyncService only on the migration
    path, the feature check was redirected to `IsTabGroupSyncServiceDesktopMigrationEnabled()`.
    
    Bug: 368143498
    Change-Id: I27739fb5115c96cea1a8d1e0908a8d75297f0e7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875119
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357613}

--- chrome/browser/tab_group_sync/feature_utils.cc
+++ chrome/browser/tab_group_sync/feature_utils.cc
@@ -7,13 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "build/build_config.h"
 #include "components/prefs/pref_service.h"
+#include "components/saved_tab_groups/features.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/jni_android.h"
 #include "base/android/scoped_java_ref.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_group_sync/utils_jni_headers/TabGroupSyncFeatures_jni.h"
-#include "components/saved_tab_groups/features.h"
 #include "components/saved_tab_groups/pref_names.h"
 #endif  // BUILDFLAG(IS_ANDROID)
 
@@ -38,7 +38,7 @@ bool IsTabGroupSyncEnabled(PrefService* pref_service) {
 
   return base::FeatureList::IsEnabled(tab_groups::kTabGroupSyncAndroid);
 #else
-  return false;
+  return IsTabGroupSyncServiceDesktopMigrationEnabled();
 #endif  // BUILDFLAG(IS_ANDROID)
 }
 
