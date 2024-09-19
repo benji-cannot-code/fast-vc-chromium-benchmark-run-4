2c0fb01fd8d1a36ac80f620a862ebc80df6568e3
Alice Boxhall <alice@igalia.com>
2024-09-19 00:06:05 +0000

    Move methods only used by InspectorAccessibilityAgent into InspectorAccessibilityAgent
    Change-Id: I856d9526815e2a8a906be0c4e145d19e1de1b900
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5837359
    Commit-Queue: Alice Boxhall <alice@igalia.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357410}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.h
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.h
@@ -88,8 +88,6 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
   AXObject* ActiveDescendant() const override;
   String AriaAccessibilityDescription() const;
   String AutoComplete() const override;
-  void AccessibilityChildrenFromAOMProperty(AOMRelationListProperty,
-                                            AXObject::AXObjectVector&) const;
 
   // For table objects.
   bool IsDataTable() const override;
@@ -224,8 +222,6 @@ class MODULES_EXPORT AXNodeObject : public AXObject {
 
   // ARIA attributes.
   ax::mojom::blink::Role RawAriaRole() const final;
-  void AriaDescribedbyElements(AXObjectVector&) const override;
-  void AriaOwnsElements(AXObjectVector&) const override;
   ax::mojom::blink::HasPopup HasPopup() const override;
   ax::mojom::blink::IsPopup IsPopup() const override;
   bool IsEditableRoot() const override;
