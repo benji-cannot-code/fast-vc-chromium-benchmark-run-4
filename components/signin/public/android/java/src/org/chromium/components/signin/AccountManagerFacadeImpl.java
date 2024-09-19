ed3b8bb1926645eedec45a2a2a3a9cb09a00bf29
Boris Sazonov <bsazonov@chromium.org>
2024-09-19 17:28:36 +0000

    [Signin][Android] Add AccountManagerFacade.waitForPendingTokenRequestsToComplete
    This method will be used in subsequent CLs to wait for pending token
    requests when shutting down live sync tests.
    
    Bug: 340938464
    Change-Id: I7429904be3c1803a5d7f76f575eab4da0768e2ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868089
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357697}

--- components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/AccountManagerFacadeImpl.java
@@ -83,6 +83,9 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
     private int mNumberOfRetries;
     private boolean mDidAccountFetchSucceed;
 
+    private int mPendingTokenRequests;
+    private Runnable mTokenRequestsCompletedCallback;
+
     /**
      * @param delegate the AccountManagerDelegate to use as a backend
      */
@@ -139,6 +142,7 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
         ThreadUtils.assertOnUiThread();
         assert coreAccountInfo != null;
         assert scope != null;
+        pendingRequestStarted();
         ConnectionRetry.runAuthTask(
                 new AuthTask<AccessTokenData>() {
                     @Override
@@ -151,13 +155,31 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
                     @Override
                     public void onSuccess(AccessTokenData token) {
                         callback.onGetTokenSuccess(token);
+                        pendingRequestFinished();
                     }
 
                     @Override
                     public void onFailure(boolean isTransientError) {
                         callback.onGetTokenFailure(isTransientError);
+                        pendingRequestFinished();
                     }
                 });
+    }
+
+    private void pendingRequestStarted() {
+        ThreadUtils.assertOnUiThread();
+        mPendingTokenRequests++;
+    }
+
+    private void pendingRequestFinished() {
+        ThreadUtils.assertOnUiThread();
+        mPendingTokenRequests--;
+        assert mPendingTokenRequests >= 0;
+        if (mPendingTokenRequests == 0 && mTokenRequestsCompletedCallback != null) {
+            Runnable callback = mTokenRequestsCompletedCallback;
+            mTokenRequestsCompletedCallback = null;
+            callback.run();
+        }
     }
 
     @Override
@@ -192,6 +214,18 @@ public class AccountManagerFacadeImpl implements AccountManagerFacade {
                         }
                     }
                 });
+    }
+
+    @Override
+    public void waitForPendingTokenRequestsToComplete(Runnable requestsCompletedCallback) {
+        ThreadUtils.assertOnUiThread();
+        assert mTokenRequestsCompletedCallback == null;
+        if (mPendingTokenRequests == 0) {
+            requestsCompletedCallback.run();
+            return;
+        }
+        // The callback will be invoked when the all pending token requests are finished.
+        mTokenRequestsCompletedCallback = requestsCompletedCallback;
     }
 
     @Override
