54fb3f65a1a4700937d2b88b6e21c2c801499ae8
Benjamin Williams <bwwilliams@google.com>
2024-09-19 16:48:45 +0000

    [iOS] Safety Check Notifications Opt-in Improvements
    This CL improves the Safety Check notifications opt-in feature in the
    Magic Stack.
    
    The following changes were made:
    
    * Added a new list preference to store the positions of the Safety Check
      module (with notifications opt-in) within the Magic Stack.
    
    * Added new experimental parameters to control:
    
        * How notifications opt-in button impressions are counted (e.g.,
          only when the module is at the top, or always).
    
        * The maximum number of impressions allowed before the opt-in button
          stops being displayed in the Safety Check module.
    
    * Refreshed the Magic Stack when notification settings permissions
      change to ensure the module(s) reflect an accurate notifications
      permissions state.
    
    * Ensured the Safety Check state correctly reflects the notifications
      opt-in state.
    
    These improvements enhance the tracking and display logic of the Safety
    Check notifications opt-in button, providing a more reliable and
    controlled user experience.
    
    See demo videos:
    
    - Tapping the opt-in button immediately enables notifications:
    https://drive.google.com/file/d/1v4MOJaI0iz-Dz1Ztumi1nWR2nm0DmnhB/view?usp=drive_link&resourcekey=0-Jw-x1K3icx4qjeam0BROmw
    
    - Opt-in button stops being shown after max impressions:
    https://drive.google.com/file/d/1lcf4qeyvIwT-hl-FKKWk16z6cKJctccW/view?usp=sharing&resourcekey=0-BOVQfESYVAViceQ3lP_6xg
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Change-Id: I9e61cfa5c9ce95e482b1352ceeb48d37c8057532
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873466
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Adam Arcaro <adamta@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357669}

--- ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_magic_stack_mediator.mm
+++ ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_magic_stack_mediator.mm
@@ -8,20 +8,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <optional>
 
 #import "base/memory/raw_ptr.h"
+#import "base/values.h"
 #import "components/pref_registry/pref_registry_syncable.h"
 #import "components/prefs/ios/pref_observer_bridge.h"
 #import "components/prefs/pref_service.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/app_state_observer.h"
 #import "ios/chrome/browser/passwords/model/password_checkup_utils.h"
+#import "ios/chrome/browser/push_notification/model/push_notification_client_id.h"
+#import "ios/chrome/browser/push_notification/model/push_notification_settings_util.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_constants.h"
 #import "ios/chrome/browser/safety_check/model/ios_chrome_safety_check_manager_observer_bridge.h"
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/public/features/system_flags.h"
+#import "ios/chrome/browser/ui/content_suggestions/content_suggestions_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_consumer.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_view_controller_audience.h"
+#import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_module.h"
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_audience.h"
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_consumer_source.h"
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_magic_stack_consumer.h"
@@ -29,7 +34,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/safety_check_state.h"
 #import "ios/chrome/browser/ui/content_suggestions/safety_check/utils.h"
 
+namespace {
+
+// Returns the number of times the Safety Check module with the
+// notifications opt-in button has been shown to the user in the Magic Stack.
+//
+// If `only_include_top_module` is `true`, only impressions where the module
+// was shown at the top of the Magic Stack are counted.
+int ImpressionsCount(const base::Value::List& impressions,
+                     bool only_include_top_module) {
+  int count = 0;
+
+  for (const base::Value& impression : impressions) {
+    std::optional<int> index = impression.GetIfInt();
+
+    if (index.has_value() && (!only_include_top_module || index.value() == 0)) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+}  // namespace
+
 @interface SafetyCheckMagicStackMediator () <AppStateObserver,
+                                             MagicStackModuleDelegate,
                                              PrefObserverDelegate,
                                              SafetyCheckAudience,
                                              SafetyCheckConsumerSource,
@@ -101,6 +131,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
       _safetyCheckState = [self initialSafetyCheckState];
 
+      _safetyCheckState.delegate = self;
+
       if (ShouldHideSafetyCheckModuleIfNoIssues()) {
         [self updateIssueCount:[_safetyCheckState numberOfIssues]
                withPrefService:localState];
@@ -154,6 +186,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                   passwordState:PasswordSafetyCheckState::kDefault
               safeBrowsingState:SafeBrowsingSafetyCheckState::kDefault
                    runningState:RunningSafetyCheckState::kDefault];
+
+  if (IsSafetyCheckNotificationsEnabled()) {
+    _safetyCheckState.showNotificationsOptIn =
+        [self shouldShowNotificationsOptIn];
+  }
+
+  _safetyCheckState.delegate = self;
   _safetyCheckState.audience = self;
   _safetyCheckState.safetyCheckConsumerSource = self;
 }
@@ -240,6 +279,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     if (!IsSafetyCheckNotificationsEnabled()) {
       _safetyCheckManager->StartSafetyCheck();
     }
+  }
+}
+
+#pragma mark - MagicStackModuleDelegate
+
+// Stores the index at which the Safety Check module (with notifications
+// opt-in button) was displayed in the Magic Stack. This is used to track
+// impressions for the Safety Check Notifications feature.
+- (void)magicStackModule:(MagicStackModule*)magicStackModule
+     wasDisplayedAtIndex:(NSUInteger)index {
+  if (magicStackModule.type != ContentSuggestionsModuleType::kSafetyCheck ||
+      !magicStackModule.showNotificationsOptIn) {
+    return;
+  }
+
+  if (IsSafetyCheckNotificationsEnabled()) {
+    CHECK(_localState);
+
+    base::Value::List impressions =
+        _localState->GetList(prefs::kMagicStackSafetyCheckNotificationsShown)
+            .Clone();
+
+    impressions.Append(static_cast<int>(index));
+
+    _localState->SetList(prefs::kMagicStackSafetyCheckNotificationsShown,
+                         std::move(impressions));
   }
 }
 
@@ -346,8 +411,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                            ? RunningSafetyCheckState::kRunning
                            : RunningSafetyCheckState::kDefault;
 
+  if (IsSafetyCheckNotificationsEnabled()) {
+    state.showNotificationsOptIn = [self shouldShowNotificationsOptIn];
+  }
+
   state.audience = self;
   state.safetyCheckConsumerSource = self;
+
   return state;
 }
 
@@ -386,6 +456,34 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   localPrefService->SetInteger(
       prefs::kHomeCustomizationMagicStackSafetyCheckIssuesCount, issuesCount);
+}
+
+// Returns `YES` if the notifications opt-in button should be displayed.
+- (BOOL)shouldShowNotificationsOptIn {
+  CHECK(IsSafetyCheckNotificationsEnabled());
+
+  BOOL isOptedIn = push_notification_settings::
+      GetMobileNotificationPermissionStatusForClient(
+          PushNotificationClientId::kSafetyCheck, "");
+
+  if (isOptedIn) {
+    return NO;
+  }
+
+  base::Value::List impressions =
+      _localState->GetList(prefs::kMagicStackSafetyCheckNotificationsShown)
+          .Clone();
+
+  SafetyCheckNotificationsImpressionTrigger trigger =
+      SafetyCheckNotificationsImpressionTriggerEnabled();
+
+  int impressionsCount = ImpressionsCount(
+      impressions,
+      trigger == SafetyCheckNotificationsImpressionTrigger::kOnlyWhenTopModule);
+
+  int impressionsLimit = SafetyCheckNotificationsImpressionLimit();
+
+  return impressionsCount < impressionsLimit;
 }
 
 @end
