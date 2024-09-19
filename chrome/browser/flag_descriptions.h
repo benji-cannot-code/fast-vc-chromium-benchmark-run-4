72f4350e8eee6ba7a653dc4c06b0f4f807fcf02b
Darren Shen <shend@chromium.org>
2024-09-19 02:22:27 +0000

    Remove abandoned flag for extended emoji search.
    Bug: b:209513583
    Change-Id: I49053c8f4660347fa87c245be3708cae0b47ba6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868761
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357453}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4156,9 +4156,6 @@ extern const char kImeSystemEmojiPickerMojoSearchDescription[];
 extern const char kImeSystemEmojiPickerClipboardName[];
 extern const char kImeSystemEmojiPickerClipboardDescription[];
 
-extern const char kImeSystemEmojiPickerSearchExtensionName[];
-extern const char kImeSystemEmojiPickerSearchExtensionDescription[];
-
 extern const char kImeSystemEmojiPickerVariantGroupingName[];
 extern const char kImeSystemEmojiPickerVariantGroupingDescription[];
 
