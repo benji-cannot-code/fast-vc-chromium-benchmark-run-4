007ee1c4d156a688f38e7c437ea70758df1b003b
Ernesto Izquierdo Clua <eic@google.com>
2024-09-19 14:19:49 +0000

    [ios] Removed unused deps from form handlers js feature
    form_handlers.ts no longer depends on password_controller.ts. Removing
    the unused dependency.
    
    Change-Id: I0c83c3ffb23fae2e439e81d24cef7707092ce8b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873853
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Ernesto Izquierdo Clua <eic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1357585}

--- components/autofill/ios/form_util/form_handlers_java_script_feature.mm
+++ components/autofill/ios/form_util/form_handlers_java_script_feature.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/autofill/ios/form_util/child_frame_registrar.h"
 #import "components/autofill/ios/form_util/form_activity_tab_helper.h"
 #import "components/autofill/ios/form_util/form_util_java_script_feature.h"
-#import "components/password_manager/ios/password_manager_java_script_feature.h"
 #import "ios/web/public/js_messaging/java_script_feature_util.h"
 #import "ios/web/public/js_messaging/script_message.h"
 
@@ -40,9 +39,7 @@ FormHandlersJavaScriptFeature::FormHandlersJavaScriptFeature()
               FeatureScript::ReinjectionBehavior::
                   kReinjectOnDocumentRecreation)},
           {web::java_script_features::GetCommonJavaScriptFeature(),
-           autofill::FormUtilJavaScriptFeature::GetInstance(),
-           password_manager::PasswordManagerJavaScriptFeature::GetInstance()}) {
-}
+           autofill::FormUtilJavaScriptFeature::GetInstance()}) {}
 
 FormHandlersJavaScriptFeature::~FormHandlersJavaScriptFeature() = default;
 
