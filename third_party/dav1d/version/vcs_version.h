779b1f91fda3845ad71e520371e93a1d781cbdde
Wan-Teh Chang <wtc@google.com>
2024-09-19 16:43:29 +0000

    Roll src/third_party/dav1d/libdav1d/ 8d9b1e26b..a7a40a3fd (2 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/8d9b1e26b37e..a7a40a3fde96
    
    $ git log 8d9b1e26b..a7a40a3fd --date=short --first-parent --format='%ad %ae %s'
    2024-09-03 ccawley2011 Define __ARM_ARCH with older compilers
    2024-09-02 ccawley2011 Support older ARM versions with checkasm
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org,eugene@chromium.org
    
    Change-Id: Ic4af775e8b31f286739b72db2497e712f4dc8d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876100
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357661}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.4.2-63-g8d9b1e2"
+#define DAV1D_VERSION "1.4.2-65-ga7a40a3"
