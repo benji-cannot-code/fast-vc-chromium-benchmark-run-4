e94bcd75d5e42800ce00ca64acbc876ca853b4d1
Jose Dapena Paz <jdapena@igalia.com>
2024-09-19 11:04:49 +0000

    IWYU: missing include for usage of std::variant in exception_context.h
    Bug: 41455655
    Change-Id: I7c4a26c28481de40b22646f5a9f018235079dbca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872753
    Commit-Queue: José Dapena Paz <jdapena@igalia.com>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357540}

--- third_party/blink/renderer/platform/bindings/exception_context.h
+++ third_party/blink/renderer/platform/bindings/exception_context.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_EXCEPTION_CONTEXT_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_BINDINGS_EXCEPTION_CONTEXT_H_
 
+#include <variant>
+
 #include "base/check_op.h"
 #include "base/dcheck_is_on.h"
 #include "base/notreached.h"
