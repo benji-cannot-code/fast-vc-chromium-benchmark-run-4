b5f95f9b5adf689f2304c9a39cc63f679433fc22
Shuhei Takahashi <nya@chromium.org>
2024-09-20 00:30:28 +0000

    [Settings] Do not finish the settings activity
    With the single-activity mode we're soon enabling in
    crrev.com/c/5869178, we use a single activity instance throughout the
    settings hierarchy, so settings fragments should not finish the current
    activity when the user is done with the page.
    
    Bug: b/356743945
    Change-Id: If88b29210c18b28b0faabe0c797c6debf49a15f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872362
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357939}

--- chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubSubpageFragment.java
+++ chrome/browser/safety_hub/android/java/src/org/chromium/chrome/browser/safety_hub/SafetyHubSubpageFragment.java
@@ -70,7 +70,7 @@ public abstract class SafetyHubSubpageFragment extends SafetyHubBaseFragment {
                     @Override
                     public void onClick(View view) {
                         mBulkActionConfirmed = true;
-                        getActivity().finish();
+                        getActivity().onBackPressed();
                     }
                 });
         view.addView(bottomView);
