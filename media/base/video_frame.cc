99f07edafa2387c7db599bbabb08125d44dcb481
vikas soni <vikassoni@chromium.org>
2024-09-20 00:20:18 +0000

    [MappableSI] Make VideoFrame::GetGpuMemoryBuffer() private.
    1. Make VideoFrame::GetGpuMemoryBuffer() private and friend client which
       is still using it.
    
    2. Add VideoFrame::GetGpuMemoryBufferForTesting() which will be used by
       tests only until they are converted to use MappableSI.
    
    Bug: 40263579
    Change-Id: I5fbfd6e799927cc98f188caf914d2c57e0d70207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873615
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357930}

--- media/base/video_frame.cc
+++ media/base/video_frame.cc
@@ -1315,6 +1315,10 @@ bool VideoFrame::HasNativeGpuMemoryBuffer() const {
   return false;
 }
 
+gfx::GpuMemoryBuffer* VideoFrame::GetGpuMemoryBufferForTesting() const {
+  return GetGpuMemoryBuffer();
+}
+
 gfx::GpuMemoryBuffer* VideoFrame::GetGpuMemoryBuffer() const {
   return wrapped_frame_ ? wrapped_frame_->GetGpuMemoryBuffer()
                         : gpu_memory_buffer_.get();
