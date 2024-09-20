0b0c021a1d824e7c7b9ec68f45b5bb80006a316a
Colin Blundell <blundell@chromium.org>
2024-09-20 17:36:34 +0000

    [//media] Have VideoFrames always take texture target from ClientSI
    This feature was enabled in 128 [1], which has rolled out without any
    problems. This CL removes the feature and makes the new codepath
    definitive. A followup will strip `texture_target` from incoming APIs.
    
    [1] https://chromiumdash.appspot.com/commit/19421cba6a3c7a507e92e13c4107e1f86a21a486
    
    Bug: 346197746
    Change-Id: Ibecb812270f537183c6fe5d0ae7527339bb69cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878321
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358255}

--- content/renderer/media/android/stream_texture_wrapper_impl.cc
+++ content/renderer/media/android/stream_texture_wrapper_impl.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "gpu/command_buffer/client/client_shared_image.h"
 #include "gpu/command_buffer/client/shared_image_interface.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
-#include "media/base/media_switches.h"
 
 namespace {
 // Non-member function to allow it to run even after this class is deleted.
@@ -74,32 +73,18 @@ void StreamTextureWrapperImpl::CreateVideoFrame(
   // raster. The latter will be over GL if not using OOP-R. NOTE: GL usage can
   // be eliminated once OOP-R ships definitively.
   scoped_refptr<gpu::ClientSharedImage> shared_image;
-  if (base::FeatureList::IsEnabled(
-          media::kVideoFrameUseClientSITextureTarget)) {
-    // If the VideoFrame will be using the texture target from this
-    // ClientSharedImage, then we need to ensure that the ClientSI holds the
-    // correct texture target (which is *not* the texture target that
-    // ClientSharedImage would compute internally for these parameters).
-    // NOTE: We do this only under the killswitch because it changes the texture
-    // target that the ClientSI is holding and thus has potential other
-    // behavioral implications.
-    shared_image =
-        sii->NotifyMailboxAdded(mailbox, viz::SinglePlaneFormat::kRGBA_8888,
-                                coded_size, gfx::ColorSpace::CreateSRGB(),
-                                kTopLeft_GrSurfaceOrigin, kPremul_SkAlphaType,
-                                gpu::SHARED_IMAGE_USAGE_DISPLAY_READ |
-                                    gpu::SHARED_IMAGE_USAGE_GLES2_READ |
-                                    gpu::SHARED_IMAGE_USAGE_RASTER_READ,
-                                texture_target);
-  } else {
-    shared_image =
-        sii->NotifyMailboxAdded(mailbox, viz::SinglePlaneFormat::kRGBA_8888,
-                                coded_size, gfx::ColorSpace::CreateSRGB(),
-                                kTopLeft_GrSurfaceOrigin, kPremul_SkAlphaType,
-                                gpu::SHARED_IMAGE_USAGE_DISPLAY_READ |
-                                    gpu::SHARED_IMAGE_USAGE_GLES2_READ |
-                                    gpu::SHARED_IMAGE_USAGE_RASTER_READ);
-  }
+
+  // Ensure that the ClientSI holds the correct texture target (which is *not*
+  // the texture target that ClientSharedImage would compute internally for
+  // these parameters).
+  shared_image =
+      sii->NotifyMailboxAdded(mailbox, viz::SinglePlaneFormat::kRGBA_8888,
+                              coded_size, gfx::ColorSpace::CreateSRGB(),
+                              kTopLeft_GrSurfaceOrigin, kPremul_SkAlphaType,
+                              gpu::SHARED_IMAGE_USAGE_DISPLAY_READ |
+                                  gpu::SHARED_IMAGE_USAGE_GLES2_READ |
+                                  gpu::SHARED_IMAGE_USAGE_RASTER_READ,
+                              texture_target);
 
   // The pixel format doesn't matter here as long as it's valid for texture
   // frames. But SkiaRenderer wants to ensure that the format of the resource
