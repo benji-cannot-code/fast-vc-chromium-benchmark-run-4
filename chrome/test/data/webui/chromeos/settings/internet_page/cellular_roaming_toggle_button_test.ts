c255f6b638a28b4dad62888c9a543106ea4fe844
dpapad <dpapad@chromium.org>
2024-09-20 22:38:41 +0000

    WebUI: Move remaining CrOS tests to chrome/test/data/webui/chromeos/
    The corresponding source code has been moved from [1] to [2] a long time
    ago, but the corresponding tests had been left in [3].
    
    [1] ui/webui/resources/cr_components/chromeos/ to
    [2] ash/webui/common/resources/
    [3] chrome/test/data/webui/cr_components/chromeos/
    
    Bug: None
    Change-Id: Id74f0a2d48da6791f0c00b2dbbd6ae53ef1ad04d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878609
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358427}

--- chrome/test/data/webui/chromeos/settings/internet_page/cellular_roaming_toggle_button_test.ts
+++ chrome/test/data/webui/chromeos/settings/internet_page/cellular_roaming_toggle_button_test.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import 'chrome://os-settings/lazy_load.js';
-import 'chrome://webui-test/cr_components/chromeos/network/cr_policy_strings.js';
+import 'chrome://webui-test/chromeos/network/cr_policy_strings.js';
 
 import {CellularRoamingToggleButtonElement} from 'chrome://os-settings/lazy_load.js';
 import {CrToggleElement} from 'chrome://os-settings/os_settings.js';
