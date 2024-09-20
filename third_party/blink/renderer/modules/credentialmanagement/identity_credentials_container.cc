730192228a9350f6cffdcda8dacb74a94eae7d3a
Peter Kotwicz <pkotwicz@chromium.org>
2024-09-20 21:50:14 +0000

    [Credential Management] Change order of abort check to match spec
    This CL changes order in which
    AuthenticationCredentialsContainer::get() checks if the request has
    been aborted to match the spec
    https://www.w3.org/TR/credential-management-1/
    
    Specifically options.signal should be checked prior to processing
    the relevant credential interface objects.
    
    Change-Id: I64eb79aa816515d5bf9e8c8db4e7577954d49f6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872253
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358396}

--- third_party/blink/renderer/modules/credentialmanagement/identity_credentials_container.cc
+++ third_party/blink/renderer/modules/credentialmanagement/identity_credentials_container.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_credential_request_options.h"
+#include "third_party/blink/renderer/core/dom/abort_signal.h"
 #include "third_party/blink/renderer/core/dom/dom_exception.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context.h"
 #include "third_party/blink/renderer/core/frame/navigator.h"
@@ -45,6 +46,12 @@ ScriptPromise<IDLNullable<Credential>> IdentityCredentialsContainer::get(
   auto* resolver =
       MakeGarbageCollected<ScriptPromiseResolver<IDLNullable<Credential>>>(
           script_state, exception_state.GetContext());
+
+  if (options->hasSignal() && options->signal()->aborted()) {
+    resolver->Reject(MakeGarbageCollected<DOMException>(
+        DOMExceptionCode::kAbortError, "Request has been aborted."));
+    return resolver->Promise();
+  }
 
   if (IsDigitalIdentityCredentialType(*options)) {
     DiscoverDigitalIdentityCredentialFromExternalSource(
