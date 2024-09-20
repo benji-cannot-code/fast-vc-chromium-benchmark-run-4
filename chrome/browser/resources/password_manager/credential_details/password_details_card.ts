37309f76b3a43023ccd575d23529749be380be88
Andrii Natiahlyi <natiahlyi@google.com>
2024-09-20 07:16:29 +0000

    Fix sharing IPH is overlapped by other passwords
    Before: https://screenshot.googleplex.com/8xeUkwFoG9ovqJg.png
    After: https://screenshot.googleplex.com/BGq82UayeA33FX7.png
    
    Fixed: 347114486
    Change-Id: I5bd8510e7cb330202ead1eeed91dce3b49a88481
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5877003
    Reviewed-by: Adem Derinel <derinel@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358034}

--- chrome/browser/resources/password_manager/credential_details/password_details_card.ts
+++ chrome/browser/resources/password_manager/credential_details/password_details_card.ts
@@ -62,6 +62,7 @@ export interface PasswordDetailsCardElement {
     showPasswordButton: CrIconButtonElement,
     usernameValue: CredentialFieldElement,
     shareButton: CrButtonElement,
+    shareButtonContainer: HTMLElement,
   };
 }
 
@@ -319,7 +320,7 @@ export class PasswordDetailsCardElement extends PasswordDetailsCardElementBase {
     }
 
     this.registerHelpBubble(
-        PASSWORD_SHARE_BUTTON_BUTTON_ELEMENT_ID, this.$.shareButton);
+        PASSWORD_SHARE_BUTTON_BUTTON_ELEMENT_ID, this.$.shareButtonContainer);
   }
 }
 
