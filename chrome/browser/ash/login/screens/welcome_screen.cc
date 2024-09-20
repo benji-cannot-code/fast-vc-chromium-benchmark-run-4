f6bb7a8902bde33969ee2b2f06face7bfccada7c
Brittany Hartmire <bhartmire@google.com>
2024-09-20 21:14:57 +0000

    [QuickStart]: Add QuickStart.EntryPointVisible histogram
    A follow-up CL will emit this metric on the gaia info and gaia screens.
    
    Test: manually test and add new browsertests
    Change-Id: I5d5d1df1ea9ec75f6f20a8af32630cf2eeb2abfb
    Bug: b/367795201
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871244
    Reviewed-by: Michael Hansen <hansenmichael@google.com>
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358379}

--- chrome/browser/ash/login/screens/welcome_screen.cc
+++ chrome/browser/ash/login/screens/welcome_screen.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/grit/branded_strings.h"
 #include "chrome/grit/generated_resources.h"
+#include "chromeos/ash/components/quick_start/quick_start_metrics.h"
 #include "chromeos/dbus/constants/dbus_switches.h"
 #include "components/language/core/browser/pref_names.h"
 #include "components/prefs/pref_service.h"
@@ -589,6 +590,11 @@ void WelcomeScreen::SetQuickStartButtonVisibility(bool visible) {
     view_->SetQuickStartEnabled();
     base::UmaHistogramBoolean(
         "QuickStart.WelcomeScreen.QuickStartButtonVisible", visible);
+    if (!has_emitted_quick_start_visible) {
+      has_emitted_quick_start_visible = true;
+      quick_start::QuickStartMetrics::RecordEntryPointVisible(
+          quick_start::QuickStartMetrics::EntryPoint::WELCOME_SCREEN);
+    }
   }
 }
 
