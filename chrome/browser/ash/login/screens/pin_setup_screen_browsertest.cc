bb10156114bc4d7f4eb4695e7cf528e1b2858c7a
Hardik Goyal <hardikgoyal@google.com>
2024-09-20 08:18:43 +0000

    Fix enum style on pin setup screen
    NO_IFTTT=This is just a simple rename. No need for histogram update.
    BUG=b:365059362
    
    
    Change-Id: Ibea5e6cf85eca0aa2615ae1a9771a54f4e9a3ce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873390
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358056}

--- chrome/browser/ash/login/screens/pin_setup_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/pin_setup_screen_browsertest.cc
@@ -215,7 +215,7 @@ class PinSetupScreenTest : public OobeBaseTest {
   void ExpectExitResultAndMetric(PinSetupScreen::Result result) {
     EXPECT_EQ(screen_result_.value(), result);
 
-    if (result == PinSetupScreen::Result::NOT_APPLICABLE) {
+    if (result == PinSetupScreen::Result::kNotApplicable) {
       histogram_tester_.ExpectTotalCount(kPinSetupScreenCompletionTime,
                                          /*expected_count=*/0);
     } else {
@@ -256,7 +256,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, ShownByDefault) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
 }
 
 // The screen should be skipped when the 'extra_factors_token' isn't present.
@@ -268,7 +268,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, SkipWhenExtraFactorsTokenMissing) {
   ShowPinSetupScreen();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::NOT_APPLICABLE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kNotApplicable);
 }
 
 // The screen should be skipped when the token is invalid.
@@ -281,7 +281,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, SkipWhenTokenInvalid) {
   ShowPinSetupScreen();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::NOT_APPLICABLE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kNotApplicable);
 }
 
 // If the PIN setup screen is shown, auth session should be cleared afterwards.
@@ -293,7 +293,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, AuthSessionIsClearedOnManualSkip) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
   CheckCredentialsWereCleared();
 }
 
@@ -306,7 +306,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, SkipWhenNotAllowedByPolicy) {
   ShowPinSetupScreen();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::NOT_APPLICABLE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kNotApplicable);
 }
 
 // The PIN screen should be shown when policy allows PIN for unlock.
@@ -318,7 +318,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, ShowWhenPinAllowedForUnlock) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
 }
 
 // The PIN screen should be shown when policy allows PIN for WebAuthN.
@@ -330,7 +330,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, ShowWhenPinAllowedForWebAuthn) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
 }
 
 // Skip the flow in the beginning and expect the proper metrics.
@@ -341,7 +341,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, ManualSkipOnStart) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
   ExpectUserActionMetric(PinSetupScreen::UserAction::kSkipButtonClickedOnStart);
 }
 
@@ -355,7 +355,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, ManualSkipInFlow) {
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
   ExpectUserActionMetric(PinSetupScreen::UserAction::kSkipButtonClickedInFlow);
 }
 
@@ -371,7 +371,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTest, FinishedFlow) {
   TapDoneButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::DONE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kDone);
   ExpectUserActionMetric(PinSetupScreen::UserAction::kDoneButtonClicked);
 }
 
@@ -390,7 +390,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTestWithoutLoginSupport,
   ShowPinSetupScreen();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::NOT_APPLICABLE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kNotApplicable);
 }
 
 IN_PROC_BROWSER_TEST_F(PinSetupScreenTestWithoutLoginSupport,
@@ -399,7 +399,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTestWithoutLoginSupport,
   ShowPinSetupScreen();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::NOT_APPLICABLE);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kNotApplicable);
   CheckCredentialsWereCleared();
 }
 
@@ -414,7 +414,7 @@ IN_PROC_BROWSER_TEST_F(PinSetupScreenTestWithoutLoginSupport,
   TapSkipButton();
   WaitForScreenExit();
 
-  ExpectExitResultAndMetric(PinSetupScreen::Result::USER_SKIP);
+  ExpectExitResultAndMetric(PinSetupScreen::Result::kUserSkip);
 }
 
 }  // namespace ash
