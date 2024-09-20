db3218cb96b924385fd3ec602ec4ac73b114de94
Samuel Huang <huangs@chromium.org>
2024-09-20 20:34:25 +0000

    [Android FRE] Animate transitions between successive fragments.
    FirstRunActivity uses ViewPager2 to handle fragment changes. However,
    transitions are instantaneous, and didn't follow animation specs, which
    specify subtle horizontal and opacity changes.
    
    This CL implements the required transition animation. Details:
    * FirstRunActivity:
      * Add isRtl(), needed for sign change decisions for animations.
      * Add advanceToNextPageInternal() with |smoothScroll| param. This is
        needed to handle 2 use cases of advanceToNextPage():
        * true: Page transition due to UI.
        * false: skipPagesIfNecessary() to jump without animation.
      * Add FirstRunPageTransformer to manage interpolation details for
        fragments.
        * Instantiated as |mPageTransformer|, and passed to |mPager|.
        * Need to account for direction of travel: Interpolation value
          depends on transition and not scroll state.
      * setCurrentItemForPager(): Add |smoothScroll| param. If true, follow
        ViewPager2 guideline and use fake drag to control transition
        duration and animation. This flow replaces setCurrentItem() usage.
    * Interpolators: Add {LEGACY_ACCELERATE, LEGACY_DECELERATE}.
    
    Testing:
    * FirstRunActivity:
      * Add isAnimating() for FirstRunActivitySigninAndSyncTest to add and
        use waitUntilAnimationStops().
      * Add disableAnimationForTesting() for FirstRunIntegrationTest. Doing
        this instead of waiting because clickButton() performs clicks
        asynchronously via PostTask.runOrPostTask(). Waiting for animation
        risks flakes and timeout.
    
    Bug: 40933116
    Change-Id: I92e6b2f218fd03a785a1c00ecdc80dbfa48ec75d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5663861
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358362}

--- chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/firstrun/FirstRunActivitySigninAndSyncTest.java
@@ -167,6 +167,7 @@ public class FirstRunActivitySigninAndSyncTest {
         onView(withId(R.id.signin_fre_selected_account)).check(matches(isDisplayed()));
 
         clickButton(R.id.signin_fre_continue_button);
+        waitUntilAnimationStops();
 
         signinStartedWatcher.assertExpected();
         waitUntilCurrentPageIs(SyncConsentFirstRunFragment.class);
@@ -607,6 +608,11 @@ public class FirstRunActivitySigninAndSyncTest {
                         Criteria.checkThat(
                                 mFirstRunActivity.getCurrentFragmentForTesting(),
                                 Matchers.instanceOf(fragmentClass)));
+    }
+
+    private void waitUntilAnimationStops() {
+        CriteriaHelper.pollUiThread(
+                () -> Criteria.checkThat(mFirstRunActivity.isAnimating(), Matchers.is(false)));
     }
 
     private void launchFirstRunActivityAndWaitForNativeInitialization() {
