ab34372c99beaf9330a37f5cfa54a824969ca8c1
Junhao Huang (from Dev Box) <junhaohuang@microsoft.com>
2024-09-20 02:05:19 +0000

    Reland "Prevent off-store extensions from being repeatedly enabled/disabled in low-trust environments"
    Included fix for failed ToolbarActionHoverCardBubbleViewUITest.
    This is a reland of commit f830cbd6f6a0089da5e940fcf897e7055c0597bd
    
    Original change's description:
    > Prevent off-store extensions from being repeatedly enabled/disabled
    > in low-trust environments
    >
    > This change addresses the issue where off-store extensions
    > force-installed by policies (e.g., `ExtensionInstallForcelist`) are
    > repeatedly enabled and disabled in environments with
    > `ManagementAuthorityTrustworthiness` less than `TRUSTED`.
    >
    > The problem occurs due to the handling of `disable_reasons` in
    > `ExtensionRegistrar::DisableExtension`. The `DISABLE_NOT_VERIFIED`
    > reason is cleared by a bitwise AND operation with an internal mask,
    > which prevents consistent disabling of extensions in non domain-join
    > environments.
    >
    > The fix involves including `DISABLE_NOT_VERIFIED` in the
    > `internal_disable_reason_mask` to ensure it is considered a valid
    > reason to disable an extension, even for controlled extensions. This
    > change prevents the repeated status change and enforces the policy
    > restriction more reliably.
    >
    > Bug: 364340030
    > Change-Id: Idbef1d22feadbf84b897b71c91b4069fa6b5d6d7
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5831156
    > Auto-Submit: Junhao Huang <junhaohuang@microsoft.com>
    > Commit-Queue: Junhao Huang <junhaohuang@microsoft.com>
    > Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1353723}
    
    Bug: 364340030
    Change-Id: I6f90c2c8cb0faa9edbff6b4bc6112d186123c098
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5849830
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Junhao Huang <junhaohuang@microsoft.com>
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357977}

--- chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view_interactive_uitest.cc
+++ chrome/browser/ui/views/toolbar/toolbar_action_hover_card_bubble_view_interactive_uitest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 #include "chrome/browser/extensions/permissions/scripting_permissions_modifier.h"
 #include "chrome/browser/extensions/permissions/site_permissions_helper.h"
+#include "chrome/browser/policy/policy_test_utils.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/test/test_browser_dialog.h"
 #include "chrome/browser/ui/views/extensions/extensions_toolbar_container.h"
@@ -212,6 +213,8 @@ IN_PROC_BROWSER_TEST_F(ToolbarActionHoverCardBubbleViewUITest,
 // since such class computes the hover card state.
 IN_PROC_BROWSER_TEST_F(ToolbarActionHoverCardBubbleViewUITest,
                        WidgetUpdatedWhenHoveringBetweenActionViews) {
+  // Mark as enterprise managed.
+  policy::ScopedDomainEnterpriseManagement scoped_domain;
   ASSERT_TRUE(embedded_test_server()->Start());
 
   // Install four extensions with different policy and site access permissions
