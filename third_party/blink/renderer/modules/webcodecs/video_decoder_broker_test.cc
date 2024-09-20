1384eb766246d364f62abbceba8219db54d8743b
Dale Curtis <dalecurtis@chromium.org>
2024-09-20 00:21:58 +0000

    Switch Android to using resolution based priority.
    This removes the artificial software caps from MediaCodecVideoDecoder
    and instead enforces them through the resolution based priority
    feature -- which is only enabled for Android for now.
    
    Bug: 361823989
    Change-Id: I94acd879e069fd97f383ad1193034586f8c6f484
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875629
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1357933}

--- third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
+++ third_party/blink/renderer/modules/webcodecs/video_decoder_broker_test.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/run_loop.h"
 #include "base/task/single_thread_task_runner.h"
-#include "base/test/scoped_feature_list.h"
 #include "base/threading/thread.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -453,19 +452,13 @@ TEST_F(VideoDecoderBrokerTest, Decode_MultipleAccelerationPreferences) {
 
   auto normal_config = media::TestVideoConfig::Normal(media::VideoCodec::kVP8);
   InitializeDecoder(normal_config);
-  if (base::FeatureList::IsEnabled(media::kResolutionBasedDecoderPriority)) {
-    // Use a small frame to force software decode, without changing the
-    // acceleration preference.
-    DecodeBuffer(media::ReadTestDataFile("vp8-I-frame-320x120"));
-  } else {
-    // VideoDecoderBroker doesn't have any inherent preference for software
-    // decoders based on resolution, so we'll still end up with a hardware
-    // decoder even though this is a small size clip.
-    // TODO(crbug.com/361823989): We should update the VideoDecoderBroker to
-    // always enable resolution based priority in DecoderSelector.
-    DecodeBuffer(media::CreateFakeVideoBufferForTest(
-        normal_config, base::TimeDelta(), base::Milliseconds(33)));
-  }
+  // VideoDecoderBroker doesn't have any inherent preference for software
+  // decoders based on resolution, so we'll still end up with a hardware
+  // decoder even though this is a small size clip.
+  // TODO(crbug.com/361823989): We should update the VideoDecoderBroker to
+  // always enable resolution based priority in DecoderSelector.
+  DecodeBuffer(media::CreateFakeVideoBufferForTest(
+      normal_config, base::TimeDelta(), base::Milliseconds(33)));
   DecodeBuffer(media::DecoderBuffer::CreateEOSBuffer());
   ASSERT_EQ(4U, output_frames_.size());
 
