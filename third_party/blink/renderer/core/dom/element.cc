e0db009fe6ec3315155d74d81fd34c21e52968df
Rune Lillesveen <futhark@chromium.org>
2024-09-20 09:42:18 +0000

    Make ::scroll-*-button focusable
    Allow ::scroll-*-button:focus selectors and otherwise make the pseudo
    elements focusable. Instead of adding yet another boolean flag to
    SelectorCheckingContext, start storing the PseudoId of the last seen
    pseudo element instead.
    
    Bug: 366415125
    Change-Id: I62f6a1d0ea32922f8d60f64c4827af07391282bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872871
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358074}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -6758,7 +6758,7 @@ FocusableState Element::SupportsFocus(UpdateBehavior update_behavior) const {
   }
   if (HasElementFlag(ElementFlags::kTabIndexWasSetExplicitly) ||
       IsRootEditableElementWithCounting(*this) ||
-      IsScrollMarkerPseudoElement() || SupportsSpatialNavigationFocus()) {
+      IsScrollControlPseudoElement() || SupportsSpatialNavigationFocus()) {
     return FocusableState::kFocusable;
   }
   if (CanBeKeyboardFocusableScroller(update_behavior)) {
