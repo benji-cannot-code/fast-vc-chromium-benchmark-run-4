6220cc6223083774c0c8d5001ddb0a272ac94f0a
Keren Zhu <kerenzhu@chromium.org>
2024-09-20 18:51:02 +0000

    base: use ObserverList::Notify() in ui/base
    Bug: 40727208
    Change-Id: I597d81894d9fa63c6fcecd108615b4204260777e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874994
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358302}

--- ui/base/cursor/cursor_factory.cc
+++ ui/base/cursor/cursor_factory.cc
@@ -52,8 +52,7 @@ void CursorFactory::RemoveObserver(CursorFactoryObserver* observer) {
 }
 
 void CursorFactory::NotifyObserversOnThemeLoaded() {
-  for (auto& observer : observers_)
-    observer.OnThemeLoaded();
+  observers_.Notify(&CursorFactoryObserver::OnThemeLoaded);
 }
 
 scoped_refptr<PlatformCursor> CursorFactory::GetDefaultCursor(
