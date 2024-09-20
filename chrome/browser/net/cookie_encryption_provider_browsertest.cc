3291f81d860fff0b005fcb9a16a9220bbca253f5
Will Harris <wfh@chromium.org>
2024-09-20 19:28:22 +0000

    Re-enable CookieEncryptionProviderBrowserTest on all platforms
    https://crrev.com/c/5855421 accidentally disabled this test on
    non-Linux platforms.
    
    This CL fixes the glitch.
    
    BUG=40086962
    
    Change-Id: Icc8d3468d057d48f7bb21f14a78bea7393ecd413
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876495
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358333}

--- chrome/browser/net/cookie_encryption_provider_browsertest.cc
+++ chrome/browser/net/cookie_encryption_provider_browsertest.cc
@@ -36,12 +36,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/install_static/test/scoped_install_details.h"
 #endif  // BUILDFLAG(IS_WIN)
 
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
 #include "chrome/browser/chrome_browser_main.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 #include "components/os_crypt/async/browser/secret_portal_key_provider.h"
 #include "components/os_crypt/async/browser/test_secret_portal.h"
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
 
 namespace {
 
@@ -76,10 +76,10 @@ enum TestConfiguration {
   // have been data encrypted with this key before the policy was disabled.
   kOSCryptAsyncWithAppBoundProviderDisabledByPolicy,
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
   // The Secret Portal key provider is being registered with Chrome.
   kOSCryptAsyncWithSecretPortalProvider,
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
 };
 
 enum MetricsExpectation {
@@ -89,9 +89,9 @@ enum MetricsExpectation {
   kAppBoundEncryptMetrics,
   kAppBoundDecryptMetrics,
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
   kSecretPortalMetrics,
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
   kNoMetrics,
 };
 
@@ -137,7 +137,7 @@ class CookieEncryptionProviderBrowserTest
             std::make_unique<os_crypt::FakeInstallDetails>()) {}
 #endif  // BUILDFLAG(IS_WIN)
 
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
   void CreatedBrowserMainParts(content::BrowserMainParts* parts) override {
     InProcessBrowserTest::CreatedBrowserMainParts(parts);
 
@@ -167,7 +167,7 @@ class CookieEncryptionProviderBrowserTest
         std::make_unique<PostCreateThreadsObserver>(
             std::move(initialize_server)));
   }
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
 
   void SetUp() override {
 #if BUILDFLAG(IS_WIN)
@@ -196,9 +196,9 @@ class CookieEncryptionProviderBrowserTest
         disabled_features.push_back(
             features::kRegisterAppBoundEncryptionProvider);
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
         disabled_features.push_back(features::kDbusSecretPortal);
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
         break;
 #if BUILDFLAG(IS_WIN)
       case kOSCryptAsyncWithAppBoundProvider:
@@ -264,7 +264,7 @@ class CookieEncryptionProviderBrowserTest
             &policy_provider_);
         break;
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
       case kOSCryptAsyncWithSecretPortalProvider:
         enabled_features.push_back(
             features::kUseOsCryptAsyncForCookieEncryption);
@@ -272,7 +272,7 @@ class CookieEncryptionProviderBrowserTest
         enabled_features.push_back(
             features::kSecretPortalKeyProviderUseForEncryption);
         break;
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
     }
     scoped_feature_list_.InitWithFeatures(enabled_features, disabled_features);
 
@@ -323,7 +323,7 @@ class CookieEncryptionProviderBrowserTest
             "OSCrypt.AppBoundProvider.Encrypt.ResultCode", 0);
         break;
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
       case kSecretPortalMetrics:
         histogram_tester_.ExpectBucketCount(
             os_crypt_async::SecretPortalKeyProvider::kUmaInitStatusEnum,
@@ -331,7 +331,7 @@ class CookieEncryptionProviderBrowserTest
         histogram_tester_.ExpectTotalCount(
             os_crypt_async::SecretPortalKeyProvider::kUmaNewInitFailureEnum, 0);
         break;
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
       case kNoMetrics:
         histogram_tester_.ExpectTotalCount(
             "OSCrypt.AppBoundProvider.Decrypt.ResultCode", 0);
@@ -353,9 +353,9 @@ class CookieEncryptionProviderBrowserTest
   std::optional<base::ScopedClosureRunner> maybe_uninstall_service_;
   testing::NiceMock<policy::MockConfigurationPolicyProvider> policy_provider_;
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
   std::unique_ptr<os_crypt_async::TestSecretPortal> test_secret_portal_;
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
 };
 
 IN_PROC_BROWSER_TEST_P(CookieEncryptionProviderBrowserTest, PRE_CookieStorage) {
@@ -486,12 +486,12 @@ INSTANTIATE_TEST_SUITE_P(
          .metrics_expectation_before = kAppBoundEncryptMetrics,
          .metrics_expectation_after = kAppBoundDecryptMetrics},
 #endif  // BUILDFLAG(IS_WIN)
-#if BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
         {.name = "secret_portal",
          .before = kOSCryptAsyncWithSecretPortalProvider,
          .after = kOSCryptAsyncWithSecretPortalProvider,
          .metrics_expectation_before = kSecretPortalMetrics,
          .metrics_expectation_after = kSecretPortalMetrics},
-#endif  // BUILDFLAG(IS_LINUX)
+#endif  // BUILDFLAG(IS_LINUX) && defined(USE_DBUS)
     }),
     [](const auto& info) { return info.param.name; });
