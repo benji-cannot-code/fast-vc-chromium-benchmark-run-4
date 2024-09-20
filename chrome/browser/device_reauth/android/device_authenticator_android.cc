aefc359599bcf33eacecad01bbb51910d653b9fe
Anna Tsvirchkova <atsvirchkova@google.com>
2024-09-20 14:44:13 +0000

    [IdCheck] Implement authenticate in controller
    The CL implements authenticate in MandatoryAuthenticatorControllerImpl. It sets the mandatory authentication bit in the biometric prompt which should restrict it from falling back to pin or pattern.
    
    Bug: 367683792
    Change-Id: I111070681caf3dc6b1e14c135f571b0f193184d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873264
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Anna Tsvirchkova <atsvirchkova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358154}

--- chrome/browser/device_reauth/android/device_authenticator_android.cc
+++ chrome/browser/device_reauth/android/device_authenticator_android.cc
@@ -53,6 +53,7 @@ DeviceAuthFinalResult MapUIResultToFinal(DeviceAuthUIResult result) {
     case DeviceAuthUIResult::kCanceledByUser:
       return DeviceAuthFinalResult::kCanceledByUser;
     case DeviceAuthUIResult::kFailed:
+    case DeviceAuthUIResult::kLockout:
       return DeviceAuthFinalResult::kFailed;
   }
 }
