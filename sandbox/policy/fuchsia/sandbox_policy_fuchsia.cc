dc1c9f033a0e9c0fa0acc20ce1da769363b4f93d
John Bauman <jbauman@chromium.org>
2024-09-21 00:54:36 +0000

    [fuchsia] Add fuchsia.scheduler.RoleManager to sandbox
    fuchsia.scheduler.RoleManager was added in f20, and takes the place of
    fuchsia.media.ProfileProvider. Vulkan ICDs will soon start to use
    fuchsia.scheduler.RoleManager, so add it to the sandbox.
    
    Cq-Include-Trybots: luci.chrome.try:fuchsia-smoke-sherlock
    Bug: 42050308
    Change-Id: I4735385aae56775d586431fcaff131a036cf5d11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5793878
    Reviewed-by: Wez <wez@chromium.org>
    Auto-Submit: John Bauman <jbauman@chromium.org>
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358479}

--- sandbox/policy/fuchsia/sandbox_policy_fuchsia.cc
+++ sandbox/policy/fuchsia/sandbox_policy_fuchsia.cc
@@ -5,11 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sandbox/policy/fuchsia/sandbox_policy_fuchsia.h"
 
-#include <lib/fdio/spawn.h>
-#include <stdio.h>
-#include <zircon/processargs.h>
-#include <zircon/syscalls/policy.h>
-
+#include <fidl/fuchsia.scheduler/cpp/fidl.h>
 #include <fuchsia/buildinfo/cpp/fidl.h>
 #include <fuchsia/camera3/cpp/fidl.h>
 #include <fuchsia/fonts/cpp/fidl.h>
@@ -25,8 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <fuchsia/tracing/perfetto/cpp/fidl.h>
 #include <fuchsia/tracing/provider/cpp/fidl.h>
 #include <fuchsia/ui/composition/cpp/fidl.h>
+#include <lib/fdio/spawn.h>
 #include <lib/sys/cpp/component_context.h>
 #include <lib/sys/cpp/service_directory.h>
+#include <stdio.h>
+#include <zircon/processargs.h>
+#include <zircon/syscalls/policy.h>
 
 #include <memory>
 #include <utility>
@@ -102,6 +102,7 @@ constexpr SandboxConfig kGpuConfig = {
         // TODO(crbug.com/42050308): Use the fuchsia.scheduler API instead.
         fuchsia::media::ProfileProvider::Name_,
         fuchsia::mediacodec::CodecFactory::Name_,
+        fidl::DiscoverableProtocolName<fuchsia_scheduler::RoleManager>,
         fuchsia::sysmem::Allocator::Name_,
         fuchsia::sysmem2::Allocator::Name_,
         "fuchsia.vulkan.loader.Loader",
@@ -129,6 +130,7 @@ constexpr SandboxConfig kRendererConfig = {
         // TODO(crbug.com/42050308): Use the fuchsia.scheduler API instead.
         fuchsia::media::ProfileProvider::Name_,
         fuchsia::memorypressure::Provider::Name_,
+        fidl::DiscoverableProtocolName<fuchsia_scheduler::RoleManager>,
         fuchsia::sysmem::Allocator::Name_,
         fuchsia::sysmem2::Allocator::Name_,
         fuchsia::ui::composition::Allocator::Name_,
