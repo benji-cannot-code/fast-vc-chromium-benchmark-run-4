b393b993cdcc6520ae36bb44182f40d027f45dba
Nate Chapin <japhet@chromium.org>
2024-09-23 19:12:09 +0000

    Use v8::ExceptionPropagationCallback to set exception context information, attempt #2
    This new v8 callback provides us an opportunity to modify the
    exception immediately before it is thrown. We can use this callback
    to add context information to the exception message, rather than
    rely on the current system of having a stack-allocated object
    (ExceptionState) apply this information, with the eventual goal
    of either removing this stack-allocated class or rendering its
    constructor/destructor trivial.
    
    Practical results of this change:
    1. Special handling is need for any case that throws an exception
       asynchronously, or rejects a promise with an exception. In those
       cases, ApplyContextToException() must be called manually.
    2. A bunch of cases where we previiously couldn't apply exception
       context information now get it for free, so there are a lot of
       test updates to add exception context.
    
    Include-Ci-Only-Tests: true
    Change-Id: I67d96340e9037397abcefed65ad6215fc03a2bb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867470
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Owners-Override: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Nate Chapin <japhet@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358944}

--- third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
+++ third_party/blink/renderer/bindings/core/v8/v8_wasm_response_extensions.cc
@@ -458,9 +458,12 @@ class WasmDataLoaderClient final
 // No further methods should be called on the WasmStreaming object afterwards,
 // hence we receive the shared_ptr by reference and clear it.
 void PropagateExceptionToWasmStreaming(
+    ScriptState* script_state,
     ExceptionState& exception_state,
     std::shared_ptr<v8::WasmStreaming>& streaming) {
   DCHECK(exception_state.HadException());
+  ApplyContextToException(script_state, exception_state.GetException(),
+                          exception_state.GetContext());
   streaming->Abort(exception_state.GetException());
   streaming.reset();
   exception_state.ClearException();
@@ -542,7 +545,7 @@ void StreamFromResponseCallback(
     exception_state.ThrowTypeError(
         "An argument must be provided, which must be a "
         "Response or Promise<Response> object");
-    PropagateExceptionToWasmStreaming(exception_state, streaming);
+    PropagateExceptionToWasmStreaming(script_state, exception_state, streaming);
     return;
   }
 
@@ -550,7 +553,7 @@ void StreamFromResponseCallback(
     base::UmaHistogramEnumeration("V8.WasmStreamingInputType",
                                   WasmStreamingInputType::kResponseNotOK);
     exception_state.ThrowTypeError("HTTP status code is not ok");
-    PropagateExceptionToWasmStreaming(exception_state, streaming);
+    PropagateExceptionToWasmStreaming(script_state, exception_state, streaming);
     return;
   }
 
@@ -562,7 +565,7 @@ void StreamFromResponseCallback(
                                   WasmStreamingInputType::kWrongMimeType);
     exception_state.ThrowTypeError(
         "Incorrect response MIME type. Expected 'application/wasm'.");
-    PropagateExceptionToWasmStreaming(exception_state, streaming);
+    PropagateExceptionToWasmStreaming(script_state, exception_state, streaming);
     return;
   }
 
@@ -571,7 +574,7 @@ void StreamFromResponseCallback(
                                   WasmStreamingInputType::kReponseLocked);
     exception_state.ThrowTypeError(
         "Cannot compile WebAssembly.Module from an already read Response");
-    PropagateExceptionToWasmStreaming(exception_state, streaming);
+    PropagateExceptionToWasmStreaming(script_state, exception_state, streaming);
     return;
   }
 
@@ -581,7 +584,7 @@ void StreamFromResponseCallback(
     // Since the status is 2xx (ok), this must be status 204 (No Content),
     // status 205 (Reset Content) or a malformed status 200 (OK).
     exception_state.ThrowWasmCompileError("Empty WebAssembly module");
-    PropagateExceptionToWasmStreaming(exception_state, streaming);
+    PropagateExceptionToWasmStreaming(script_state, exception_state, streaming);
     return;
   }
 
