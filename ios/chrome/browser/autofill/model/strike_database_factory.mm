bce136c5c0249149116a4214db3db78dbde6ce5c
Vidhan Jain <vidhanj@google.com>
2024-09-23 13:24:54 +0000

    [iOS] Update ChromeBrowserState occurrences under i/c/b/autofill/
    Fixed: 365525467
    Change-Id: I908ecf64cf0f89e20ea325f8542fd7bee56dd271
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882035
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Vidhan Jain <vidhanj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358770}

--- ios/chrome/browser/autofill/model/strike_database_factory.mm
+++ ios/chrome/browser/autofill/model/strike_database_factory.mm
@@ -15,11 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace autofill {
 
 // static
-StrikeDatabase* StrikeDatabaseFactory::GetForBrowserState(ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
 StrikeDatabase* StrikeDatabaseFactory::GetForProfile(ProfileIOS* profile) {
   return static_cast<StrikeDatabase*>(
       GetInstance()->GetServiceForBrowserState(profile, true));
