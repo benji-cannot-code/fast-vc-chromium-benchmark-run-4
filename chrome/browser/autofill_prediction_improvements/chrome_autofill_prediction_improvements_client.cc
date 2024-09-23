300d8770956e0876928ecae3ef4f302863eeff6d
Wolfgang Billenstein <bwolfgang@google.com>
2024-09-23 18:57:42 +0000

    Correct link to API settings
    Corrects the link to Autofill prediction improvements settings that is
    opened when the "manage prediction improvements" link is clicked from
    suggestions.
    
    Bug: 365709538
    Change-Id: I6ad7695ddd0b9c5ba2829c6d4cf6e4697925eea3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881919
    Reviewed-by: Matthias Körber <koerber@google.com>
    Commit-Queue: Matthias Körber <koerber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358919}

--- chrome/browser/autofill_prediction_improvements/chrome_autofill_prediction_improvements_client.cc
+++ chrome/browser/autofill_prediction_improvements/chrome_autofill_prediction_improvements_client.cc
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const char kPredictionImprovementsSettingsURL[] =
-    "chrome://settings/autofill_prediction_improvements";
+    "chrome://settings/autofillPredictionImprovements";
 
 }  // namespace
 
