bcd3454fa8ef78b2b538841de1e6f0527d6ed343
Ahmed Nasr <anasr@google.com>
2024-09-23 15:44:43 +0000

    [BabelOrca] Add Tachyon parsing sandboxed service.
    Add service to parse Tachyon receive response stream and respond
    with the parsing status along with the BabelOrca messages to
    the caller if detected.
    
    Bug: b:364351312
    Change-Id: Icd1300d1752ccf0293c6f0a95e3829f166f2df6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5870621
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358828}

---
+++ chromeos/ash/services/boca/babelorca/cpp/tachyon_parsing_service.h
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_ASH_SERVICES_BOCA_BABELORCA_CPP_TACHYON_PARSING_SERVICE_H_
+#define CHROMEOS_ASH_SERVICES_BOCA_BABELORCA_CPP_TACHYON_PARSING_SERVICE_H_
+
+#include <memory>
+#include <string>
+
+#include "chromeos/ash/services/boca/babelorca/mojom/tachyon_parsing_service.mojom-shared.h"
+#include "chromeos/ash/services/boca/babelorca/mojom/tachyon_parsing_service.mojom.h"
+#include "mojo/public/cpp/bindings/pending_receiver.h"
+#include "mojo/public/cpp/bindings/receiver.h"
+
+namespace ash::babelorca {
+
+class ProtoHttpStreamParser;
+
+class TachyonParsingService : public mojom::TachyonParsingService {
+ public:
+  explicit TachyonParsingService(
+      mojo::PendingReceiver<mojom::TachyonParsingService> receiver);
+
+  TachyonParsingService(const TachyonParsingService&) = delete;
+  TachyonParsingService& operator=(const TachyonParsingService&) = delete;
+
+  ~TachyonParsingService() override;
+
+ private:
+  // mojom::TachyonParsingService:
+  void Parse(const std::string& stream_data, ParseCallback callback) override;
+
+  mojo::Receiver<mojom::TachyonParsingService> receiver_;
+
+  std::unique_ptr<ProtoHttpStreamParser> stream_parser_;
+
+  mojom::ParsingState parsing_state_ = mojom::ParsingState::kOk;
+};
+
+}  // namespace ash::babelorca
+
+#endif  // CHROMEOS_ASH_SERVICES_BOCA_BABELORCA_CPP_TACHYON_PARSING_SERVICE_H_
