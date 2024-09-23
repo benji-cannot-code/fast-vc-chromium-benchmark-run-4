bf7b44c8cf6c0b2af52c43b4fe5c4537d4ea3674
Clark DuVall <cduvall@chromium.org>
2024-09-23 21:51:52 +0000

    Change network::ResourceRequest to use non-const ref in mojo
    There wasa performance optimization that was previously using
    const_cast. This change switches to using non-const ref in mojo to get
    rid of the const_cast.
    
    Bug: 332706093
    Change-Id: I7e61ab7babd354fb2cf7563295a92896217259b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882996
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359017}

--- services/network/cors/cors_url_loader_factory.h
+++ services/network/cors/cors_url_loader_factory.h
@@ -84,6 +84,13 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CorsURLLoaderFactory final
                             mojo::PendingRemote<mojom::URLLoaderClient> client,
                             const net::MutableNetworkTrafficAnnotationTag&
                                 traffic_annotation) override;
+  void CreateLoaderAndStart(mojo::PendingReceiver<mojom::URLLoader> receiver,
+                            int32_t request_id,
+                            uint32_t options,
+                            ResourceRequest& resource_request,
+                            mojo::PendingRemote<mojom::URLLoaderClient> client,
+                            const net::MutableNetworkTrafficAnnotationTag&
+                                traffic_annotation) override;
   void Clone(mojo::PendingReceiver<mojom::URLLoaderFactory> receiver) override;
 
   // Methods for use by network::URLLoaderFactory.
@@ -141,7 +148,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) CorsURLLoaderFactory final
   bool GetAllowAnyCorsExemptHeaderForBrowser() const;
 
   mojo::PendingRemote<mojom::DevToolsObserver> GetDevToolsObserver(
-      const ResourceRequest& resource_request) const;
+      ResourceRequest& resource_request) const;
 
   template <class T>
   void OnLoaderCreated(
