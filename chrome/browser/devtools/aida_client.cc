952c4dc6f1833c781354bcb2c11885411f52072a
Wolfgang Beyer <wolfi@chromium.org>
2024-09-23 09:34:38 +0000

    [DevTools] Drop the requirement for having Chrome sync enabled
    Define and allow-list `kAidaOAuth2Scope`, switch to consent level
    `kSignin` in AidaClient.
    
    corresponding DevTools CL: https://crrev.com/c/5865371
    
    Bug: b/352007443
    Change-Id: I9fef1c91266b20159e90a22bd55cd491b350389a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5865869
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358718}

--- chrome/browser/devtools/aida_client.cc
+++ chrome/browser/devtools/aida_client.cc
@@ -24,11 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/variations/service/variations_service.h"
 #include "components/variations/service/variations_service_utils.h"
+#include "google_apis/gaia/gaia_constants.h"
 #include "net/base/load_flags.h"
 
 const char kAidaEndpointUrl[] =
     "https://aida.googleapis.com/v1/aida:doConversation";
-const char kAidaScopeUrl[] = "https://www.googleapis.com/auth/aida";
 
 constexpr auto kLoggingDisallowedCountries =
     base::MakeFixedFlatSet<std::string_view>(
@@ -60,7 +60,7 @@ constexpr auto kAidaSupportedCountries =
 AidaClient::AidaClient(Profile* profile)
     : profile_(*profile),
       aida_endpoint_(kAidaEndpointUrl),
-      aida_scope_(kAidaScopeUrl) {}
+      aida_scope_(GaiaConstants::kAidaOAuth2Scope) {}
 
 AidaClient::~AidaClient() = default;
 
@@ -168,7 +168,7 @@ void AidaClient::PrepareRequestOrFail(
     return;
   }
   CoreAccountId account_id =
-      identity_manager->GetPrimaryAccountId(signin::ConsentLevel::kSync);
+      identity_manager->GetPrimaryAccountId(signin::ConsentLevel::kSignin);
   access_token_fetcher_ = identity_manager->CreateAccessTokenFetcherForAccount(
       account_id, "AIDA client", signin::ScopeSet{aida_scope_},
       base::BindOnce(&AidaClient::AccessTokenFetchFinished,
