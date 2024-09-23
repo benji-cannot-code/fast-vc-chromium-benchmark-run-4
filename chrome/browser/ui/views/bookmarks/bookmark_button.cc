8e62538830ce442e09fbcafea9260b13b7ad4c4a
Gaurav Kumar <gauravkum@microsoft.com>
2024-09-23 15:30:37 +0000

    [views-ax] Migrates kRoleDescription a11y attribute to new system
    This CL migrates the kRoleDescription attribute in
    BookmarkButtonBase's constructor, since it is not changing again
    rather than querying the value and computing it only when needed.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I99084be01ec7a770ef4b016c0b9a28af31e4f28c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859478
    Reviewed-by: Divyansh Mangal <dmangal@microsoft.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1358821}

--- chrome/browser/ui/views/bookmarks/bookmark_button.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_button.cc
@@ -77,6 +77,8 @@ BookmarkButtonBase::BookmarkButtonBase(PressedCallback callback,
   } else {
     show_animation_->Show();
   }
+  GetViewAccessibility().SetRoleDescription(
+      l10n_util::GetStringUTF8(IDS_ACCNAME_BOOKMARK_BUTTON_ROLE_DESCRIPTION));
 }
 
 BookmarkButtonBase::~BookmarkButtonBase() = default;
@@ -96,13 +98,6 @@ bool BookmarkButtonBase::IsTriggerableEvent(const ui::Event& e) {
 std::unique_ptr<views::LabelButtonBorder>
 BookmarkButtonBase::CreateDefaultBorder() const {
   return bookmark_button_util::CreateBookmarkButtonBorder();
-}
-
-void BookmarkButtonBase::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  views::LabelButton::GetAccessibleNodeData(node_data);
-  node_data->AddStringAttribute(
-      ax::mojom::StringAttribute::kRoleDescription,
-      l10n_util::GetStringUTF8(IDS_ACCNAME_BOOKMARK_BUTTON_ROLE_DESCRIPTION));
 }
 
 BEGIN_METADATA(BookmarkButtonBase)
@@ -137,11 +132,6 @@ std::u16string BookmarkButton::GetTooltipText(const gfx::Point& p) const {
         max_tooltip_width_, tooltip_manager->GetFontList(), *url_, GetText());
   }
   return tooltip_text_;
-}
-
-void BookmarkButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  BookmarkButtonBase::GetAccessibleNodeData(node_data);
-  const std::u16string name = GetViewAccessibility().GetCachedName();
 }
 
 void BookmarkButton::AdjustAccessibleName(std::u16string& new_name,
