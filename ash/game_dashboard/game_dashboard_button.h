046acedeef4bc028423123e6c878aea8edf7d02f
Khaya Kingston <khayak@google.com>
2024-09-23 23:16:55 +0000

    gd: Make Game Dashboard button oval
    This Cl adjusts the previously hardcoded corner radius value of the
    Game Dashboard Main Menu button to make the button oval at all times.
    
    Bug: b:367468739
    Demo: http://b/367468739#comment5
    Test: Verified on emulator and DUT
    Change-Id: I05784e79f78d57897cc9791c019b4685683dfe2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871233
    Reviewed-by: Cici Ruan <cuicuiruan@google.com>
    Reviewed-by: Prameet Shah <phshah@chromium.org>
    Commit-Queue: Khaya Kingston <khayak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359061}

--- ash/game_dashboard/game_dashboard_button.h
+++ ash/game_dashboard/game_dashboard_button.h
@@ -48,7 +48,7 @@ class GameDashboardButton : public views::Button {
   METADATA_HEADER(GameDashboardButton, views::Button)
 
  public:
-  explicit GameDashboardButton(PressedCallback callback);
+  GameDashboardButton(PressedCallback callback, float corner_radius);
   GameDashboardButton(const GameDashboardButton&) = delete;
   GameDashboardButton& operator=(const GameDashboardButton&) = delete;
   ~GameDashboardButton() override;
@@ -105,6 +105,8 @@ class GameDashboardButton : public views::Button {
   raw_ptr<views::ImageView> arrow_icon_view_;
   // Ensures the ink drop is painted above the button's background.
   raw_ptr<views::InkDropContainerView> ink_drop_container_ = nullptr;
+
+  const float container_corner_radius_;
 
   // If true, the game window is being recorded, otherwise false.
   bool is_recording_ = false;
