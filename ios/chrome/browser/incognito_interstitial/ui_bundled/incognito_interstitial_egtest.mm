adb817bfff77ccde1eee76667aa5950b7c96b9b9
Federica Germinario <fedegermi@google.com>
2024-09-23 19:19:17 +0000

    [ios] Make key incognito_interstitial_enabled app specific
    The PrefService key was profile specific but should be application
    specific. This CL updates the code accordingly.
    
    Fixed: 369047559
    Change-Id: Ic7e51bd8dfeae8cb6e82364c6e40b9fb22708a9b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876475
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1358956}

--- ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_egtest.mm
+++ ios/chrome/browser/incognito_interstitial/ui_bundled/incognito_interstitial_egtest.mm
@@ -33,7 +33,7 @@ using chrome_test_util::NTPIncognitoView;
 - (void)setUp {
   [super setUp];
   [ChromeEarlGrey setBoolValue:YES
-                   forUserPref:prefs::kIncognitoInterstitialEnabled];
+             forLocalStatePref:prefs::kIncognitoInterstitialEnabled];
 
   // Set Incognito Mode to "available",
   // as this is an assumption for most of these tests.
@@ -46,7 +46,7 @@ using chrome_test_util::NTPIncognitoView;
 - (void)tearDown {
   scopedPolicies.Reset();
   [ChromeEarlGrey setBoolValue:NO
-                   forUserPref:prefs::kIncognitoInterstitialEnabled];
+             forLocalStatePref:prefs::kIncognitoInterstitialEnabled];
   [super tearDown];
 }
 
--- ios/chrome/browser/shared/model/prefs/browser_prefs.mm
+++ ios/chrome/browser/shared/model/prefs/browser_prefs.mm
@@ -762,6 +762,10 @@ void RegisterLocalStatePrefs(PrefRegistrySimple* registry) {
 
   registry->RegisterIntegerPref(
       prefs::kIdentityConfirmationSnackbarDisplayCount, 0);
+
+  // Register pref storing whether the Incognito interstitial for third-party
+  // intents is enabled.
+  registry->RegisterBooleanPref(prefs::kIncognitoInterstitialEnabled, false);
 }
 
 void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
@@ -879,8 +883,7 @@ void RegisterBrowserStatePrefs(user_prefs::PrefRegistrySyncable* registry) {
   registry->RegisterBooleanPref(prefs::kHttpsOnlyModeEnabled, false);
   registry->RegisterBooleanPref(prefs::kMixedContentAutoupgradeEnabled, true);
 
-  // Register pref storing whether the Incognito interstitial for third-party
-  // intents is enabled.
+  // Deprecated pref, moved to localState prefs.
   registry->RegisterBooleanPref(prefs::kIncognitoInterstitialEnabled, false);
 
   // Register pref used to determine whether the User Policy notification was
@@ -1349,6 +1352,10 @@ void MigrateObsoleteBrowserStatePrefs(const base::FilePath& state_path,
 
   MigrateIntegerPrefFromProfilePrefsToLocalStatePrefs(
       prefs::kIdentityConfirmationSnackbarDisplayCount, prefs);
+
+  // Added 09/2024.
+  MigrateBooleanPrefFromProfilePrefsToLocalStatePrefs(
+      prefs::kIncognitoInterstitialEnabled, prefs);
 }
 
 void MigrateObsoleteUserDefault() {
