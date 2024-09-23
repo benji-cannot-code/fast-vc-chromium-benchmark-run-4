59d6a8fa327e52f69dd79a830bbc773fe1f42f94
Noah Rose Ledesma <noahrose@google.com>
2024-09-23 16:47:19 +0000

    Install CECA during policy fetch
    The enterprise companion app may be included in the versioned install
    directory for O4. The updater should run the app with the "install if
    needed" flag prior to a policy fetch.
    
    Bug: 362267332
    Change-Id: I78dc2f80943268a4452475b5e086c1e130e83200
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867085
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358859}

--- chrome/enterprise_companion/enterprise_companion.h
+++ chrome/enterprise_companion/enterprise_companion.h
@@ -28,10 +28,6 @@ extern const char kCrashMeSwitch[];
 // Install the application.
 extern const char kInstallSwitch[];
 
-// Install the application if the device requires cloud management and it is not
-// already present.
-extern const char kInstallIfNeededSwitch[];
-
 // Remove all traces of the application from the system.
 extern const char kUninstallSwitch[];
 
--- chrome/enterprise_companion/enterprise_companion_client.h
+++ chrome/enterprise_companion/enterprise_companion_client.h
@@ -26,6 +26,8 @@ namespace enterprise_companion {
 // Controls the transmission of usage stats (i.e. crash reports).
 extern const char kEnableUsageStatsSwitch[];
 
+extern const char kInstallIfNeededSwitch[];
+
 // Returns the server name for establishing IPC via NamedMojoIpcServer.
 mojo::NamedPlatformChannel::ServerName GetServerName();
 
