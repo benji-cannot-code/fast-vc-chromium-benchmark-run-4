448e3e322e499065a0f24be9fd4bfed0cda7d5d8
Chase Phillips <cmp@chromium.org>
2024-09-23 22:39:08 +0000

    controlledframe: Async methods should only support promises
    Previously Controlled Frame's async methods supported both callback and
    promise forms. This CL updates the promise-creating system that would
    support callback forms to only support the promise forms, completing our
    transition to promise-based async calls.
    
    This CL also fixes a bug where the `originalGo` that was captured was
    the promise-wrapped version that was created by
    `promisifyMethodHandler`. Due to that bug, when the callback form
    support was deleted, this broke the back/forward calls which were
    defined using that `originalGo`.
    
    To get back/forward working, those back/forward calls had to use an
    `originalGo` which was not the result of `promisifyMethodHandler` but instead was the original `go`. I broke down the `forwardApiMethods` call
    so we could capture the original `go` before the promise wrapping
    occurs.
    
    BYPASS_LARGE_CHANGE_WARNING
    
    Bug: 332195254, 40191772
    Change-Id: I0df54837f7de503ed4f4022dca7047d5d3b6bd96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5879735
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359042}

--- chrome/renderer/resources/controlled_frame/controlled_frame.js
+++ chrome/renderer/resources/controlled_frame/controlled_frame.js
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 var ControlledFrameImpl = require('controlledFrameImpl').ControlledFrameImpl;
 var forwardApiMethods = require('guestViewContainerElement').forwardApiMethods;
+var promiseWrap = require('guestViewContainerElement').promiseWrap;
 var ChromeWebViewImpl = require('chromeWebView').ChromeWebViewImpl;
 var CONTROLLED_FRAME_API_METHODS =
     require('controlledFrameApiMethods').CONTROLLED_FRAME_API_METHODS;
@@ -37,6 +38,16 @@ forwardApiMethods(
     ControlledFrameElement, ControlledFrameImpl, WebViewInternal,
     CONTROLLED_FRAME_API_METHODS, CONTROLLED_FRAME_PROMISE_API_METHODS);
 
+// Since |back| and |forward| are implemented in terms of |go|, we need to
+// keep a reference to the real |go| function, since user code may override
+// ControlledFrameElement.prototype.go|.
+var originalGo = ControlledFrameElement.prototype.go;
+
+// Wrap callback methods in promise handlers. Note: This disables the callback
+// forms.
+promiseWrap(ControlledFrameElement, ControlledFrameImpl, WebViewInternal,
+            CONTROLLED_FRAME_PROMISE_API_METHODS);
+
 // Delete GuestView methods that should not be part of the Controlled Frame API.
 (function() {
   for (const methodName of CONTROLLED_FRAME_DELETED_API_METHODS) {
@@ -47,10 +58,5 @@ forwardApiMethods(
     }
   }
 })();
-
-// Since |back| and |forward| are implemented in terms of |go|, we need to
-// keep a reference to the real |go| function, since user code may override
-// ControlledFrameElement.prototype.go|.
-var originalGo = ControlledFrameElement.prototype.go;
 
 registerElement('ControlledFrame', ControlledFrameElement);
