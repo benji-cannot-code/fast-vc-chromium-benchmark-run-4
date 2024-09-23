7b5b2d6a485e47de352017a33152958c98b80f0f
Hzj_jie <zijiehe@google.com>
2024-09-23 19:12:58 +0000

    [fuchsia] avoid update_product_bundles_test cleaning up images
    `git cl upload` runs update_product_bundles_test and unexpectedly cleans
    up the images; make_clean_directory needs to be mocked.
    
    Change-Id: Ic66dc2c9842f6cc432579d2a41238e7b06b2cfbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5879376
    Auto-Submit: Zijie He <zijiehe@google.com>
    Reviewed-by: David Song <wintermelons@google.com>
    Commit-Queue: David Song <wintermelons@google.com>
    Cr-Commit-Position: refs/heads/main@{#1358945}

--- build/fuchsia/update_product_bundles_test.py
+++ build/fuchsia/update_product_bundles_test.py
@@ -104,6 +104,7 @@ class TestUpdateProductBundles(unittest.TestCase):
                          'workstation-eng.chromebook-x64'))
       ])
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('update_product_bundles.running_unattended', return_value=True)
   # Disallow reading sdk_override.
   @mock.patch('os.path.isfile', return_value=False)
@@ -141,6 +142,7 @@ class TestUpdateProductBundles(unittest.TestCase):
         ])
     ])
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('common.get_hash_from_sdk', return_value='2.2.2')
   @mock.patch('update_product_bundles.get_current_signature',
               return_value='2.2.2')
@@ -156,6 +158,7 @@ class TestUpdateProductBundles(unittest.TestCase):
       update_product_bundles.main()
     self.assertFalse(self._ffx_mock.called)
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('common.get_hash_from_sdk', return_value='2.2.2')
   @mock.patch('update_product_bundles.get_current_signature',
               return_value='0.0')
@@ -189,6 +192,7 @@ class TestUpdateProductBundles(unittest.TestCase):
         ])
     ])
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('update_sdk.GetSDKOverrideGCSPath',
               return_value='gs://my-bucket/sdk')
   @mock.patch('common.get_hash_from_sdk', return_value='2.2.2')
@@ -221,6 +225,7 @@ class TestUpdateProductBundles(unittest.TestCase):
         ])
     ])
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('update_product_bundles.internal_hash', return_value='1.1.1')
   @mock.patch('update_product_bundles.get_current_signature',
               return_value='1.1.1')
@@ -237,6 +242,7 @@ class TestUpdateProductBundles(unittest.TestCase):
       update_product_bundles.main()
     self.assertFalse(self._ffx_mock.called)
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('update_product_bundles.get_current_signature',
               return_value='0.0')
   @mock.patch('update_product_bundles.internal_hash', return_value='1.1.1')
@@ -271,6 +277,7 @@ class TestUpdateProductBundles(unittest.TestCase):
         ])
     ])
 
+  @mock.patch('common.make_clean_directory')
   @mock.patch('update_sdk.GetSDKOverrideGCSPath',
               return_value='gs://my-bucket/sdk')
   @mock.patch('update_product_bundles.internal_hash', return_value='1.1.1')
