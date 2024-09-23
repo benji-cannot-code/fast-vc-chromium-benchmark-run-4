3d5b24ead47c4d3598541f33cb3e3b8ba9fcf4e2
Ryan Sultanem <rsult@google.com>
2024-09-23 11:41:20 +0000

    [BatchUpload] Link strings to use translateable strings
    Dialog had hardcoded strings, link to the proper strings.
    
    Bug: b:365953592
    Change-Id: I7e01e69214c1b68fb877298a651ed07dbf0cf825
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872621
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1358750}

--- chrome/browser/ui/webui/signin/batch_upload_ui.cc
+++ chrome/browser/ui/webui/signin/batch_upload_ui.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/batch_upload_resources.h"
 #include "chrome/grit/batch_upload_resources_map.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/strings/grit/components_strings.h"
 #include "content/public/browser/web_ui_data_source.h"
 
 BatchUploadUI::BatchUploadUI(content::WebUI* web_ui)
@@ -28,6 +30,16 @@ BatchUploadUI::BatchUploadUI(content::WebUI* web_ui)
   webui::SetupWebUIDataSource(
       source, base::make_span(kBatchUploadResources, kBatchUploadResourcesSize),
       IDR_BATCH_UPLOAD_BATCH_UPLOAD_HTML);
+
+  static constexpr webui::LocalizedString kLocalizedStrings[] = {
+      {"batchUploadTitle", IDS_BATCH_UPLOAD_TITLE},
+      {"saveToAccount", IDS_BATCH_UPLOAD_SAVE_TO_ACCOUNT_OK_BUTTON_LABEL},
+      {"cancel", IDS_CANCEL},
+  };
+  source->AddLocalizedStrings(kLocalizedStrings);
+
+  source->UseStringsJs();
+  source->EnableReplaceI18nInJS();
 }
 
 BatchUploadUI::~BatchUploadUI() = default;
