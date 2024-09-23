c4b4b747e4a8838e16495eeec370ac09a41181ca
Min Qin <qinmin@chromium.org>
2024-09-23 16:10:14 +0000

    Fix download policy block message and action button text
    This CL fixes the following 2 issues:
    If 1 out of multiple failed download is blocked, Chrome should show
    "1 was blocked by your organization" instead of "Blocked by your
    organization".
    Also message UI shouldn't show the "details" button since if all
    failed download is blocked.
    
    Bug: 368481336
    Change-Id: I3926ff64090f0898089ae90a832c561c9aa307f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5877688
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358843}

--- chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/DownloadMessageUiControllerImpl.java
+++ chrome/browser/download/internal/android/java/src/org/chromium/chrome/browser/download/DownloadMessageUiControllerImpl.java
@@ -178,6 +178,7 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
         public String description;
         public String link;
         public int icon;
+        public boolean ignoreAction;
 
         public @IconType int iconType = IconType.DRAWABLE;
 
@@ -226,6 +227,7 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
             forceShow = other.forceShow;
             downloadCount = other.downloadCount;
             resultState = other.resultState;
+            ignoreAction = other.ignoreAction;
         }
     }
 
@@ -235,6 +237,7 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
         public int pending;
         public int failed;
         public int completed;
+        // Download is blocked, each blocked downloaded is also counted in failed.
         public int blocked;
 
         /**
@@ -752,15 +755,30 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
                 // TODO(shaktisahu): Incorporate various types of failure messages.
                 // TODO(shaktisahu, xingliu): Consult UX to handle multiple schedule variations.
                 if (downloadCount.blocked > 0) {
-                    info.description =
-                            getContext()
-                                    .getResources()
-                                    .getQuantityString(
-                                            R.plurals.download_message_multiple_download_blocked,
-                                            downloadCount.blocked,
-                                            downloadCount.blocked);
+                    if (downloadCount.failed == 1) {
+                        info.description =
+                                getContext()
+                                        .getString(
+                                                R.string.download_message_single_download_blocked);
+                    } else {
+                        info.description =
+                                getContext()
+                                        .getResources()
+                                        .getQuantityString(
+                                                R.plurals
+                                                        .download_message_multiple_download_blocked,
+                                                downloadCount.blocked,
+                                                downloadCount.blocked);
+                    }
                 }
-                info.link = getContext().getString(R.string.details_link);
+                boolean allFailedDownloadsAreBlocked =
+                        (downloadCount.blocked == downloadCount.failed);
+                if (allFailedDownloadsAreBlocked) {
+                    info.link = getContext().getString(R.string.ok);
+                    info.ignoreAction = true;
+                } else {
+                    info.link = getContext().getString(R.string.details_link);
+                }
             }
         }
 
@@ -885,7 +903,8 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
         mPropertyModel.set(MessageBannerProperties.PRIMARY_BUTTON_TEXT, info.link);
         mPropertyModel.set(MessageBannerProperties.ON_DISMISSED, this::onMessageDismissed);
         mPropertyModel.set(
-                MessageBannerProperties.ON_PRIMARY_ACTION, () -> onPrimaryAction(info.id));
+                MessageBannerProperties.ON_PRIMARY_ACTION,
+                () -> onPrimaryAction(info.id, info.ignoreAction));
         final MessageDispatcher dispatcher = getMessageDispatcher();
         mDismissRunnable =
                 () -> {
@@ -1003,9 +1022,14 @@ public class DownloadMessageUiControllerImpl implements DownloadMessageUiControl
         mNotificationIds.remove(contentId);
     }
 
-    private @PrimaryActionClickBehavior int onPrimaryAction(ContentId itemId) {
+    private @PrimaryActionClickBehavior int onPrimaryAction(
+            ContentId itemId, boolean ignoreAction) {
         OfflineItem offlineItem = mTrackedItems.remove(itemId);
         removeNotification(itemId);
+        if (ignoreAction) {
+            return PrimaryActionClickBehavior.DISMISS_IMMEDIATELY;
+        }
+
         if (itemId != null) {
             mDelegate.openDownload(
                     itemId,
