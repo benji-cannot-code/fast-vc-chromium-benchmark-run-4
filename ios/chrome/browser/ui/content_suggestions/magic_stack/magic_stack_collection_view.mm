70bbd6173ccfd6fd3cee07406900d46cd727013e
Chris Lu <thegreenfrog@chromium.org>
2024-09-23 21:29:37 +0000

    [ios] Cap next scrollable MagicStack card page to the index of last card
    Bad math. Last index is moduleCount - 1. Also disabled
    SegmentationPlatformEphemeralCardRanker by default, which is what it
    should have been from the beginning.
    
    Fixed: 365098640
    Change-Id: If14efeee085bacc09437bd8f35768cb9e975bebb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5883034
    Auto-Submit: Chris Lu <thegreenfrog@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359001}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_collection_view.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_collection_view.mm
@@ -355,9 +355,6 @@ typedef NSDiffableDataSourceSnapshot<NSString*, MagicStackModule*>
   CGFloat moduleWidth =
       self.view.frame.size.width -
       ModuleNarrowerWidthToAllowPeekingForTraitCollection(self.traitCollection);
-  NSUInteger moduleCount = [[self.diffableDataSource.snapshot
-      itemIdentifiersInSectionWithIdentifier:kMagicStackSectionIdentifier]
-      count];
 
   // Find closest page to the current scroll offset.
   CGFloat closestPage = roundf(offset / moduleWidth);
@@ -372,13 +369,13 @@ typedef NSDiffableDataSourceSnapshot<NSString*, MagicStackModule*>
     UMA_HISTOGRAM_EXACT_LINEAR(kMagicStackScrollToIndexHistogram, closestPage,
                                kMaxModuleHistogramIndex);
   }
-  closestPage = std::clamp<CGFloat>(closestPage, 0, moduleCount);
+  NSArray<MagicStackModule*>* items =
+      [self.diffableDataSource.snapshot itemIdentifiers];
+  closestPage = std::clamp<CGFloat>(closestPage, 0, [items count] - 1);
   _magicStackPage = closestPage;
   if (base::FeatureList::IsEnabled(
           segmentation_platform::features::
               kSegmentationPlatformEphemeralCardRanker)) {
-    NSArray<MagicStackModule*>* items =
-        [self.diffableDataSource.snapshot itemIdentifiers];
     if ([items count] > 0 && !_hasSeenEphemeralCard &&
         [self isCardEphemeral:items[_magicStackPage]]) {
       [self.audience logEphemeralCardVisibility:items[_magicStackPage].type];
