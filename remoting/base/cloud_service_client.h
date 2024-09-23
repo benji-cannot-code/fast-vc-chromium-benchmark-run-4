6fd0d6d23a2a399ac2f6897298b01e81830a9153
Joe Downing <joedow@google.com>
2024-09-23 21:46:00 +0000

    Implement SessionAuthz methods in CloudServiceClient
    This CL adds the callbacks and methods needed to call the session
    authz service in the Cloud API.  Note that I'm reusing the existing
    network annotations so I can split the CLs up a bit (network
    annotation reviews are done by folks in another timezone so splitting
    it up means this CL chain will not be blocked on that review).
    
    Bug: 325448647
    Change-Id: Ifd8aab8351450a1171167c8a5096431e9eacc405
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882117
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359013}

--- remoting/base/cloud_service_client.h
+++ remoting/base/cloud_service_client.h
@@ -14,8 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace google::internal::remoting::cloud::v1alpha {
 class Empty;
+class GenerateHostTokenResponse;
 class ProvisionGceInstanceResponse;
+class ReauthorizeHostResponse;
 class RemoteAccessHost;
+class VerifySessionTokenResponse;
 }  // namespace google::internal::remoting::cloud::v1alpha
 
 namespace google::protobuf {
@@ -37,6 +40,10 @@ class ProtobufHttpStatus;
 // A service client that communicates with the directory service.
 class CloudServiceClient {
  public:
+  using GenerateHostTokenCallback = base::OnceCallback<void(
+      const ProtobufHttpStatus&,
+      std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
+                          GenerateHostTokenResponse>)>;
   using LegacyProvisionGceInstanceCallback = base::OnceCallback<void(
       const ProtobufHttpStatus&,
       std::unique_ptr<apis::v1::ProvisionGceInstanceResponse>)>;
@@ -44,6 +51,10 @@ class CloudServiceClient {
       const ProtobufHttpStatus&,
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
                           ProvisionGceInstanceResponse>)>;
+  using ReauthorizeHostCallback = base::OnceCallback<void(
+      const ProtobufHttpStatus&,
+      std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
+                          ReauthorizeHostResponse>)>;
   using SendHeartbeatCallback = base::OnceCallback<void(
       const ProtobufHttpStatus&,
       std::unique_ptr<::google::internal::remoting::cloud::v1alpha::Empty>)>;
@@ -51,6 +62,10 @@ class CloudServiceClient {
       const ProtobufHttpStatus&,
       std::unique_ptr<
           ::google::internal::remoting::cloud::v1alpha::RemoteAccessHost>)>;
+  using VerifySessionTokenCallback = base::OnceCallback<void(
+      const ProtobufHttpStatus&,
+      std::unique_ptr<::google::internal::remoting::cloud::v1alpha::
+                          VerifySessionTokenResponse>)>;
 
   // TODO: joedow - Remove the single param c'tor when we no longer support the
   // legacy provisioning path.
@@ -91,6 +106,15 @@ class CloudServiceClient {
                               std::optional<std::string> os_name,
                               std::optional<std::string> os_version,
                               UpdateRemoteAccessHostCallback callback);
+
+  void GenerateHostToken(GenerateHostTokenCallback callback);
+
+  void VerifySessionToken(const std::string& session_token,
+                          VerifySessionTokenCallback callback);
+
+  void ReauthorizeHost(const std::string& session_reauth_token,
+                       const std::string& session_id,
+                       ReauthorizeHostCallback callback);
 
   void CancelPendingRequests();
 
