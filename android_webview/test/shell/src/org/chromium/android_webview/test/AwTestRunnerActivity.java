27dff38e8ef455b0b906a1037b3a3b049bb22abd
Zakarie Yaris <yaris@google.com>
2024-09-23 10:02:19 +0000

    Handle edge_to_edge behaviour for Webview test runner.
    This change makes the test runner activity account
    for the window insets. Without it when edge-to-edge mode
    is enabled, the page contents will overlap with the system status bar.
    
    Test: I have re-enabled the tests locally and tested them
    on a physical device with android 15.
    
    Bug: 362627263, 354846826, 362633222
    Change-Id: Ie6fe54b68b684c5b2f41484d626dc90ed477fa23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5845613
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Zakarie Yaris <yaris@google.com>
    Reviewed-by: Alex Mitra <alexmitra@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358726}

--- android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/AwTestRunnerActivity.java
@@ -12,6 +12,10 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.LinearLayout;
 
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.android_webview.shell.AwShellResourceProvider;
 import org.chromium.base.StrictModeContext;
@@ -42,6 +46,7 @@ public class AwTestRunnerActivity extends Activity {
         hideActionBarIfNecessary();
 
         setContentView(mLinearLayout);
+        setupEdgeToEdge();
     }
 
     private void hideActionBarIfNecessary() {
@@ -86,5 +91,24 @@ public class AwTestRunnerActivity extends Activity {
 
     public void setIgnoreStartActivity(boolean ignore) {
         mIgnoreStartActivity = ignore;
+    }
+
+    private void setupEdgeToEdge() {
+        ViewCompat.setOnApplyWindowInsetsListener(
+                mLinearLayout,
+                (v, windowInsets) -> {
+                    Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+                    // Ensure the LinearLayout (container) view does not overlap with the system
+                    // status bar by adjusting its top padding based on the system window insets.
+                    v.setPadding(
+                            v.getPaddingLeft(),
+                            insets.top,
+                            v.getPaddingRight(),
+                            v.getPaddingBottom());
+
+                    // Return CONSUMED to indicate we have handled the insets for this view
+                    // and don't want them to be passed down to descendant views.
+                    return WindowInsetsCompat.CONSUMED;
+                });
     }
 }
