567d1b1258a9294dbee282fc8e4da1dad497bbaa
Lei Zhang <thestig@chromium.org>
2024-09-23 23:52:35 +0000

    [PDF] Centralize message converter code
    Instead of having message handlers convert message details to structured
    data themselves, move the conversion logic into its own file. Then if
    the data format changes, the conversion logic only has to be changed in
    one place.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED See crrev.com/c/5882466 try jobs
    Change-Id: I93c233a802930eff6b7d60311601dbf61e233b52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5883095
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359074}

---
+++ chrome/browser/resources/pdf/message_converter.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import type {FormFieldFocusType} from './constants.js';
+import type {DocumentDimensionsMessageData} from './pdf_viewer_utils.js';
+
+export function convertDocumentDimensionsMessage(message: any) {
+  return message as unknown as DocumentDimensionsMessageData;
+}
+
+export function convertFormFocusChangeMessage(message: any) {
+  return message as unknown as {focused: FormFieldFocusType};
+}
+
+export function convertLoadProgressMessage(message: any) {
+  return message as unknown as {progress: number};
+}
--- chrome/browser/resources/pdf/pdf_internal_plugin_wrapper.ts
+++ chrome/browser/resources/pdf/pdf_internal_plugin_wrapper.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {FormFieldFocusType} from './constants.js';
 import type {PinchEventDetail} from './gesture_detector.js';
 import {GestureDetector} from './gesture_detector.js';
+import {convertFormFocusChangeMessage} from './message_converter.js';
 import type {SwipeDirection} from './swipe_detector.js';
 import {SwipeDetector} from './swipe_detector.js';
 
@@ -34,9 +35,8 @@ plugin.addEventListener('message', e => {
     case 'formFocusChange':
       // TODO(crbug.com/40810904): Ideally, the plugin would just consume
       // interesting keyboard events first.
-      isFormFieldFocused =
-          (message as {focused: FormFieldFocusType}).focused !==
-          FormFieldFocusType.NONE;
+      const focusedData = convertFormFocusChangeMessage(message);
+      isFormFieldFocused = focusedData.focused !== FormFieldFocusType.NONE;
       break;
   }
 
