9ffae3b9e42d24ab2d7e8d316fec20727cfd311b
Aliona DANGLA <alionadangla@google.com>
2024-09-23 06:09:34 +0000

    [iOS] Updates files under ios/chrome/browser/contextual_panel
    Fixed: 365527976
    Change-Id: Id8c7978150d36722b05f02825af9139c52649613
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878050
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1358688}

--- ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_coordinator.mm
+++ ios/chrome/browser/contextual_panel/entrypoint/coordinator/contextual_panel_entrypoint_coordinator.mm
@@ -63,8 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       HandlerForProtocol(dispatcher, ContextualPanelEntrypointIPHCommands);
 
   feature_engagement::Tracker* engagementTracker =
-      feature_engagement::TrackerFactory::GetForBrowserState(
-          self.browser->GetBrowserState());
+      feature_engagement::TrackerFactory::GetForProfile(
+          self.browser->GetProfile());
 
   _mediator = [[ContextualPanelEntrypointMediator alloc]
         initWithWebStateList:webStateList
