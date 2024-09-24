e16d3f239b87cb137b90844307571db5ec763409
Chris Fredrickson <cfredric@chromium.org>
2024-09-24 00:13:08 +0000

    [SAH] Move `load` header impl from //net to //services/network
    Bug: 366284840
    Change-Id: Id3fcbe06043c347010461c4ffefaeedb0db887f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882777
    Commit-Queue: Chris Fredrickson <cfredric@chromium.org>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Auto-Submit: Chris Fredrickson <cfredric@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359084}

--- services/network/url_loader.cc
+++ services/network/url_loader.cc
@@ -124,6 +124,8 @@ constexpr size_t kBlockedBodyAllocationSize = 1;
 // Size to allocate for `discard_buffer_`.
 constexpr size_t kDiscardBufferSize = 128 * 1024;
 
+constexpr char kActivateStorageAccessHeader[] = "activate-storage-access";
+
 // A subclass of net::UploadBytesElementReader which owns
 // ResourceRequestBody.
 class BytesElementReader : public net::UploadBytesElementReader {
@@ -1440,8 +1442,7 @@ mojom::URLResponseHeadPtr URLLoader::BuildResponseHead() const {
   response->client_address_space =
       private_network_access_checker_.ClientAddressSpace();
 
-  response->load_with_storage_access =
-      url_request_->ShouldSetLoadWithStorageAccess();
+  response->load_with_storage_access = ShouldSetLoadWithStorageAccess();
 
   return response;
 }
@@ -3026,6 +3027,26 @@ bool URLLoader::CoepAllowCredentials(const GURL& url) {
 bool URLLoader::ShouldSendTransferSizeUpdated() const {
   return devtools_request_id() || url_request_->ad_tagged() ||
          !base::FeatureList::IsEnabled(features::kReduceTransferSizeUpdatedIPC);
+}
+
+bool URLLoader::ShouldSetLoadWithStorageAccess() const {
+  CHECK(url_request_);
+  if (!url_request_context_->network_delegate()->IsStorageAccessHeaderEnabled(
+          base::OptionalToPtr(
+              url_request_->isolation_info().top_frame_origin()),
+          url_request_->url())) {
+    return false;
+  }
+  switch (url_request_->StorageAccessStatus()) {
+    case net::cookie_util::StorageAccessStatus::kNone:
+      return false;
+    case net::cookie_util::StorageAccessStatus::kInactive:
+    case net::cookie_util::StorageAccessStatus::kActive:
+      return url_request_->response_headers() &&
+             url_request_->response_headers()->HasHeaderValue(
+                 kActivateStorageAccessHeader, "load");
+  }
+  NOTREACHED();
 }
 
 void URLLoader::RecordRequestMetrics() {
