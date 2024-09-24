76b32548f0a44d4622b25a4ca6f20700f7822f71
Aliona DANGLA <alionadangla@google.com>
2024-09-24 14:42:47 +0000

    [iOS] Updates files under ios/chrome/browser/credential_provider_promo
    Fixed: 365526899
    Change-Id: Ib2f6da12868e529444a5efb8c219b5ca3f699e67
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878297
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359344}

--- ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_coordinator.mm
+++ ios/chrome/browser/credential_provider_promo/ui_bundled/credential_provider_promo_coordinator.mm
@@ -57,7 +57,7 @@ using credential_provider_promo::IOSCredentialProviderPromoAction;
       startDispatchingToTarget:self
                    forProtocol:@protocol(CredentialProviderPromoCommands)];
   PromosManager* promosManager =
-      PromosManagerFactory::GetForBrowserState(self.browser->GetBrowserState());
+      PromosManagerFactory::GetForProfile(self.browser->GetProfile());
   self.mediator = [[CredentialProviderPromoMediator alloc]
       initWithPromosManager:promosManager];
 }
@@ -88,9 +88,8 @@ using credential_provider_promo::IOSCredentialProviderPromoAction;
   }
   self.viewController = [[CredentialProviderPromoViewController alloc] init];
   self.mediator.consumer = self.viewController;
-  self.mediator.tracker =
-      feature_engagement::TrackerFactory::GetForBrowserState(
-          self.browser->GetBrowserState());
+  self.mediator.tracker = feature_engagement::TrackerFactory::GetForProfile(
+      self.browser->GetProfile());
   self.viewController.actionHandler = self;
   self.viewController.presentationController.delegate = self;
   if (trigger == CredentialProviderPromoTrigger::SetUpList) {
