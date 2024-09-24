e574aa4c3ff6666b9496c59e91faca1062380639
Alex Gough <ajgo@chromium.org>
2024-09-24 19:09:46 +0000

    Remove exception for c:\windows\fonts\* from sandbox
    Now that PDFs proxy fonts this helper is not necessary.
    
    Bug: 344643689, 41494527
    Change-Id: I2e2dd62627011a1a44a1454af246cc67b9f2a039
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805413
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359512}

--- content/public/common/sandboxed_process_launcher_delegate.cc
+++ content/public/common/sandboxed_process_launcher_delegate.cc
@@ -45,10 +45,6 @@ bool SandboxedProcessLauncherDelegate::ShouldUnsandboxedRunInJob() {
 bool SandboxedProcessLauncherDelegate::CetCompatible() {
   return true;
 }
-
-bool SandboxedProcessLauncherDelegate::AllowWindowsFontsDir() {
-  return false;
-}
 #endif  // BUILDFLAG(IS_WIN)
 
 #if BUILDFLAG(IS_WIN)
