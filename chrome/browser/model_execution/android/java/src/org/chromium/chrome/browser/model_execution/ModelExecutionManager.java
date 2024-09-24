facd93653baa26d60962ae034816f8d739004c00
Mohamed Heikal <mheikal@chromium.org>
2024-09-24 16:33:00 +0000

    Prepare ModelExecutionSessionImpl to use ServiceLoaderUtil
    Add a factory interface to satisfy the no-arg constraint for using
    ServiceLoader
    
    Bug: 40901855
    Change-Id: I6b22769dad4f23299c46fc003084fb92c18bc330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882439
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359410}

--- chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ModelExecutionManager.java
+++ chrome/browser/model_execution/android/java/src/org/chromium/chrome/browser/model_execution/ModelExecutionManager.java
@@ -5,10 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.model_execution;
 
+import org.chromium.base.ServiceLoaderUtil;
+
 /** Class in charge of creating and maintaining model execution sessions for multiple features. */
 public class ModelExecutionManager {
 
     public ModelExecutionSession createSession(@ModelExecutionFeature int feature) {
-        return new ModelExecutionSessionImpl(feature);
+        ModelExecutionSessionFactory factory =
+                ServiceLoaderUtil.maybeCreate(ModelExecutionSessionFactory.class);
+        if (factory == null) {
+            return new ModelExecutionSessionImpl(feature);
+        }
+        return factory.forFeature(feature);
     }
 }
