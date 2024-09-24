6d2d92c69e3dce35e8f8a7d5a8887e735ecce760
Joe Downing <joedow@google.com>
2024-09-24 18:21:39 +0000

    Add Heartbeat related methods to CorpServiceClient
    These are analogous to the methods on the Cloud service client
    but with the service details in //remoting/internal.
    
    Internal CL: https://chrome-internal-review.googlesource.com/c/chrome/remoting/internal/+/7698773
    
    Change-Id: I711e09fe1fad84f3d2be6b4e753902eec674c043
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5880597
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359488}

--- remoting/proto/internal_stubs.cc
+++ remoting/proto/internal_stubs.cc
@@ -68,7 +68,7 @@ std::unique_ptr<ProvisionCorpMachineRequest> GetMachineProvisioningRequest(
     const std::string& fqdn,
     const std::string& public_key,
     const std::string& version,
-    const std::optional<std::string>& existing_host_id) {
+    const std::optional<std::string>& existing_directory_id) {
   return std::make_unique<ProvisionCorpMachineRequest>();
 }
 
@@ -92,7 +92,7 @@ std::string GetReportProvisioningErrorRequestPath() {
 }
 
 std::unique_ptr<ReportProvisioningErrorRequest>
-GetReportProvisioningErrorRequest(const std::string& host_id,
+GetReportProvisioningErrorRequest(const std::string& directory_id,
                                   const std::string& error_message,
                                   const std::string& version) {
   return std::make_unique<ReportProvisioningErrorRequest>();
@@ -105,7 +105,7 @@ std::string GetSendHeartbeatRequestPath() {
 }
 
 std::unique_ptr<SendHeartbeatRequest> GetSendHeartbeatRequest(
-    const std::string& host_id) {
+    const std::string& directory_id) {
   return std::make_unique<SendHeartbeatRequest>();
 }
 
@@ -116,7 +116,12 @@ std::string GetUpdateRemoteAccessHostRequestPath() {
 }
 
 std::unique_ptr<UpdateRemoteAccessHostRequest> GetUpdateRemoteAccessHostRequest(
-    const std::string& host_id) {
+    const std::string& directory_id,
+    std::optional<std::string> host_version,
+    std::optional<std::string> signaling_id,
+    std::optional<std::string> offline_reason,
+    std::optional<std::string> os_name,
+    std::optional<std::string> os_version) {
   return std::make_unique<UpdateRemoteAccessHostRequest>();
 }
 
