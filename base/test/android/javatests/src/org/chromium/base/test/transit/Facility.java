25006cee6f621fdd63cca18274d814bf8587e124
Henrique Nakashima <hnakashima@chromium.org>
2024-09-24 18:45:35 +0000

    Reland "[Android] Create OmniboxPTTest"
    Fixed by adding URL_BAR to RegularNewTabPageStation's elements in
    tablets.
    
    This is a reland of commit 75a06c00e3d88e90bb99a388df490427a37e03f4
    
    Original change's description:
    > [Android] Create OmniboxPTTest
    >
    > Test entering the omnibox, typing, deleting the text from different
    > contexts:
    > - NTP
    > - Incognito NTP
    > - Webpage
    >
    > BYPASS_LARGE_CHANGE_WARNING: Miscounting this code as production code
    >
    > Bug: 328275416
    > Change-Id: I5bbdeb67073f770c50261ff801f438537491056b
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5740828
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Commit-Queue: Michael Thiessen <mthiesse@chromium.org>
    > Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1358167}
    
    Bug: 328275416
    Change-Id: Ifef141f9039502a846227e8ec14f524499efb886
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878459
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359500}

--- base/test/android/javatests/src/org/chromium/base/test/transit/Facility.java
+++ base/test/android/javatests/src/org/chromium/base/test/transit/Facility.java
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.base.test.transit;
 
-import androidx.annotation.Nullable;
-
 import org.chromium.base.test.transit.Transition.Trigger;
 
 /**
@@ -31,7 +29,7 @@ import org.chromium.base.test.transit.Transition.Trigger;
 public abstract class Facility<HostStationT extends Station> extends ConditionalState {
     private static int sLastFacilityId = 1000;
     private final int mId = ++sLastFacilityId;
-    protected @Nullable HostStationT mHostStation;
+    protected HostStationT mHostStation;
 
     void setHostStation(Station station) {
         assert mHostStation == null
