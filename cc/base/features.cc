11c849303a0dbb1f8f549c0260d4ce78bce96961
Ari Chivukula <arichiv@chromium.org>
2024-09-24 00:45:38 +0000

    Clean up stale base::Feature "kReducedFrameRateEstimation"
    Enabled by default for a while and not used in finch.
    
    Fixed: 356235475
    Change-Id: I6fb8a8e64a7ad35a0a454379a6f0d142814233be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884213
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359105}

--- cc/base/features.cc
+++ cc/base/features.cc
@@ -118,10 +118,6 @@ BASE_FEATURE(kReclaimResourcesDelayedFlushInBackground,
              "ReclaimResourcesDelayedFlushInBackground",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
-BASE_FEATURE(kReducedFrameRateEstimation,
-             "kReducedFrameRateEstimation",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kDetectHiDpiForMsaa,
              "DetectHiDpiForMsaa",
              base::FEATURE_DISABLED_BY_DEFAULT);
