bf0fe0cf033435c70d7bbcdb5e51b3d0a645a065
Kristi Saney <kristislee@google.com>
2024-09-24 00:22:14 +0000

    [Read Aloud] Show no internet error in voice menu
    https://screenshot.googleplex.com/7JaAoScuyEqPBBt
    
    Fixed: 367783736
    Change-Id: I97d8494b4e6a8d9f8fe011dca223d52ef200b189
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5879120
    Reviewed-by: Lauren Winston <lwinston@google.com>
    Commit-Queue: Kristi Saney <kristislee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359090}

--- chrome/browser/resources/side_panel/read_anything/voice_selection_menu.ts
+++ chrome/browser/resources/side_panel/read_anything/voice_selection_menu.ts
@@ -94,6 +94,7 @@ export class VoiceSelectionMenuElement extends VoiceSelectionMenuElementBase
   // The current notifications that should be used in the voice menu.
   private currentNotifications_: {[language: string]: NotificationType} = {};
 
+  protected errorMessages_: string[] = [];
   protected downloadingMessages_: string[] = [];
   protected voiceGroups_: VoiceDropdownGroup[] = [];
   protected showLanguageMenuDialog_: boolean = false;
@@ -120,6 +121,7 @@ export class VoiceSelectionMenuElement extends VoiceSelectionMenuElementBase
     const changedPrivateProperties =
         changedProperties as Map<PropertyKey, unknown>;
     if (changedPrivateProperties.has('currentNotifications_')) {
+      this.errorMessages_ = this.computeErrorMessages_();
       this.downloadingMessages_ = this.computeDownloadingMessages_();
     }
   }
@@ -253,6 +255,7 @@ export class VoiceSelectionMenuElement extends VoiceSelectionMenuElementBase
 
   protected onClose_() {
     this.notificationManager_.removeListener(this);
+    this.currentNotifications_ = {};
     this.dispatchEvent(new CustomEvent('voice-menu-close', {
       bubbles: true,
       composed: true,
@@ -342,16 +345,30 @@ export class VoiceSelectionMenuElement extends VoiceSelectionMenuElementBase
     return this.voiceGroups_[groupIndex].voices[voiceIndex];
   }
 
+  // TODO(crbug.com/352070610): Add allocation error messages here too.
+  private computeErrorMessages_(): string[] {
+    return this.computeMessages_(
+        ([_, notification]) => notification === NotificationType.NO_INTERNET,
+        'readingModeVoiceMenuNoInternet');
+  }
+
   private computeDownloadingMessages_(): string[] {
-    return Object.entries(this.currentNotifications_)
-        .filter(
-            ([_, notification]) =>
-                notification === NotificationType.DOWNLOADING)
+    return this.computeMessages_(
+        ([_, notification]) => notification === NotificationType.DOWNLOADING,
+        'readingModeVoiceMenuDownloading');
+  }
+
+  private computeMessages_(
+      filterFn: (value: [string, NotificationType]) => boolean,
+      message: string) {
+    // We need to redeclare the type here otherwise the filterFn type
+    // declaration doesn't work.
+    const entries: Array<[string, NotificationType]> =
+        Object.entries(this.currentNotifications_);
+    return entries.filter(filterFn)
         .map(([lang, _]) => this.getDisplayNameForLocale(lang))
         .filter(possibleName => possibleName.length > 0)
-        .map(
-            displayName => loadTimeData.getStringF(
-                'readingModeVoiceMenuDownloading', displayName));
+        .map(displayName => loadTimeData.getStringF(message, displayName));
   }
 
   private getDisplayNameForLocale(language: string): string {
