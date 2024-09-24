8aa70bfbe6bbd95e919582c5f62c2af57c1420b7
Duncan Mercer <mercerd@google.com>
2024-09-24 17:43:13 +0000

    [lensoverlay] Fix ChromeOS crash caused by layer->size() being empty
    Bug: 366443419
    Change-Id: I662c08da73b786ae9383661dd6f4b185a66250b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5887887
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359460}

--- chrome/browser/ui/lens/lens_overlay_blur_layer_delegate.cc
+++ chrome/browser/ui/lens/lens_overlay_blur_layer_delegate.cc
@@ -131,7 +131,7 @@ void LensOverlayBlurLayerDelegate::FetchBackgroundImage() {
 
 void LensOverlayBlurLayerDelegate::UpdateBackgroundImage(
     const SkBitmap& bitmap) {
-  if (bitmap.drawsNothing() ||
+  if (bitmap.drawsNothing() || layer_->size().IsEmpty() ||
       AreBitmapsEqual(background_screenshot_, bitmap)) {
     return;
   }
