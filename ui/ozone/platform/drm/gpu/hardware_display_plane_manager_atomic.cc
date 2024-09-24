df0fac357fa3de1fc2be080916605d32d5b38043
Kramer Ge <fangzhoug@chromium.org>
2024-09-24 15:28:51 +0000

    [Ozone/DRM]Dump plane configs when commit properties for page flip fails
    Sometimes we do not get useful information from DRM when commits fail.
    Print the plane configs that failed the commit and the current scanned
    out config to facilitate debugging.
    
    Example log:
    Failed to commit properties for page flip: [plane_id=31:{crtc_id=80,crtc_x=92,crtc_y=136,crtc_w=2376,crtc_h=1337,fb_id=283,src_x=0,src_y=0,src_w=1280,src_h=720,type=DRM_PLANE_TYPE_PRIMARY,rotation=FLIP_HORIZONTAL,in_fence_fd=156,plane_color_encoding="ITU-R BT.601 YCbCr",plane_color_range="YCbCr limited range",}, plane_id=40:{crtc_id=80,crtc_x=0,crtc_y=0,crtc_w=2560,crtc_h=1600,fb_id=281,src_x=0,src_y=0,src_w=2560,src_h=1600,type=DRM_PLANE_TYPE_OVERLAY,rotation=NONE,in_fence_fd=157,plane_color_encoding="ITU-R BT.601 YCbCr",plane_color_range="YCbCr limited range",}], Current properties in scanout: [plane_id=31:{crtc_id=80,crtc_x=92,crtc_y=136,crtc_w=2376,crtc_h=1337,fb_id=283,src_x=0,src_y=0,src_w=1280,src_h=720,type=DRM_PLANE_TYPE_PRIMARY,rotation=FLIP_HORIZONTAL,in_fence_fd=155,plane_color_encoding="ITU-R BT.601 YCbCr",plane_color_range="YCbCr limited range",}]: Invalid argument (22)
    
    Which can be prettified to:
    Failed to commit properties for page flip:
    [
        plane_id=31:{
            crtc_id=80,
            crtc_x=92,
            crtc_y=136,
            crtc_w=2376,
            crtc_h=1337,
            fb_id=283,
            src_x=0,
            src_y=0,
            src_w=1280,
            src_h=720,
            type=DRM_PLANE_TYPE_PRIMARY,
            rotation=FLIP_HORIZONTAL,
            in_fence_fd=156,
            plane_color_encoding="ITU-R BT.601 YCbCr",
            plane_color_range="YCbCr limited range",
        },
        plane_id=40:{
            crtc_id=80,
            crtc_x=0,
            crtc_y=0,
            crtc_w=2560,
            crtc_h=1600,
            fb_id=281,
            src_x=0,
            src_y=0,
            src_w=2560,
            src_h=1600,
            type=DRM_PLANE_TYPE_OVERLAY,
            rotation=NONE,
            in_fence_fd=157,
            plane_color_encoding="ITU-R BT.601 YCbCr",
            plane_color_range="YCbCr limited range",
        }
    ], Current properties in scanout:
    [
        plane_id=31:{
            crtc_id=80,
            crtc_x=92,
            crtc_y=136,
            crtc_w=2376,
            crtc_h=1337,
            fb_id=283,
            src_x=0,
            src_y=0,
            src_w=1280,
            src_h=720,
            type=DRM_PLANE_TYPE_PRIMARY,
            rotation=FLIP_HORIZONTAL,
            in_fence_fd=155,
            plane_color_encoding="ITU-R BT.601 YCbCr",
            plane_color_range="YCbCr limited range",
        }
    ]: Invalid argument(22)
    
    Change-Id: I1810f1a54a6126dd3062ade1259e7ea05582624a
    Bug: b/356385925
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829724
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Commit-Queue: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359375}

--- ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
+++ ui/ozone/platform/drm/gpu/hardware_display_plane_manager_atomic.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <xf86drmMode.h>
 
 #include <memory>
+#include <sstream>
 #include <utility>
 
 #include "base/containers/contains.h"
@@ -400,7 +401,18 @@ bool HardwareDisplayPlaneManagerAtomic::Commit(
     if (!drm_->CommitProperties(atomic_property_set.get(), flags,
                                 crtcs.size(), page_flip_request)) {
       if (!test_only) {
-        PLOG(ERROR) << "Failed to commit properties for page flip.";
+        std::ostringstream ss;
+        ss << "Failed to commit properties for page flip: [";
+        for (const auto* plane : plane_list->plane_list) {
+          plane->DumpProperties(ss);
+          ss << (plane != plane_list->plane_list.back() ? ", " : "]");
+        }
+        ss << ", Current properties in scanout: [";
+        for (const auto* plane : plane_list->old_plane_list) {
+          plane->DumpProperties(ss);
+          ss << (plane != plane_list->old_plane_list.back() ? ", " : "]");
+        }
+        PLOG(ERROR) << ss.str();
       } else {
         VPLOG(2) << "Failed to commit properties for MODE_ATOMIC_TEST_ONLY.";
       }
