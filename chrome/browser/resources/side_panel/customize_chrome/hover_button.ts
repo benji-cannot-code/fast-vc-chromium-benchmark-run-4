d815c67ef3b6a7610829a9a0068de03fe5ea5478
Caroline Rising <corising@chromium.org>
2024-09-24 14:09:23 +0000

    Fix a11y announcement on third party button in customize chrome side panel
    The a11y announcement was repetitive on cros and macos and wasn't being
    announced on windows. This fix moves the aria-label into the button so
    that it replaces the existing one (making it no longer repetitive) and
    being on the button should make it announce on  windows as well.
    
    Bug: 360401166
    Change-Id: Iec9efd95899e4b75c77cff54ead424212de35c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882994
    Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    Commit-Queue: Caroline Rising <corising@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1359332}

--- chrome/browser/resources/side_panel/customize_chrome/hover_button.ts
+++ chrome/browser/resources/side_panel/customize_chrome/hover_button.ts
@@ -31,11 +31,13 @@ export class HoverButtonElement extends CrLitElement {
 
   static override get properties() {
     return {
+      ariaButtonLabel: {type: String},
       label: {type: String},
       labelDescription: {type: String},
     };
   }
 
+  ariaButtonLabel: string|null = null;
   label: string = '';
   labelDescription: string|null = null;
 
