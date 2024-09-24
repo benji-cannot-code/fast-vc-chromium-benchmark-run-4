aeacef35a9823bcfae2fe71f667ccb52f8b3bcce
Wenyu Fu <wenyufu@chromium.org>
2024-09-24 19:17:23 +0000

    [E2E] Add kill switch to disable e2e on individual native surface
    crrev.com/c/5846890 has cause some bots to fail with unknown reason.
    In order to enable experiemnts in running E2E on other native surfaces,
    add kill switches to individual native surfaces.
    
    Bug: 365607068
    Change-Id: Icf9b2b5737cf9e75917284aad800d85fb172c63f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5888024
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359519}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -47,6 +47,7 @@ import org.chromium.chrome.browser.tabpersistence.TabStateFileManager;
 import org.chromium.chrome.browser.tasks.ReturnToChromeUtil;
 import org.chromium.chrome.browser.tasks.tab_groups.TabGroupModelFilter;
 import org.chromium.chrome.browser.tasks.tab_management.TabManagementFieldTrial;
+import org.chromium.chrome.browser.ui.edge_to_edge.EdgeToEdgeUtils;
 import org.chromium.chrome.browser.ui.google_bottom_bar.BottomBarConfigCreator;
 import org.chromium.chrome.browser.webapps.WebappLauncherActivity;
 import org.chromium.components.omnibox.OmniboxFeatures;
@@ -112,6 +113,9 @@ public class ChromeCachedFlags {
                         BottomBarConfigCreator
                                 .GOOGLE_BOTTOM_BAR_SINGLE_DECKER_HEIGHT_DP_PARAM_VALUE,
                         BottomBarConfigCreator.IS_GOOGLE_DEFAULT_SEARCH_ENGINE_CHECK_ENABLED,
+                        EdgeToEdgeUtils.DISABLE_HUB_E2E,
+                        EdgeToEdgeUtils.DISABLE_INCOGNITO_NTP_E2E,
+                        EdgeToEdgeUtils.DISABLE_NTP_E2E,
                         HubFieldTrial.ALTERNATIVE_FAB_COLOR,
                         HubFieldTrial.PANE_SWITCHER_USES_TEXT,
                         HubFieldTrial.SUPPORTS_OTHER_TABS,
