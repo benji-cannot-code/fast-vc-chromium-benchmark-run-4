15eeb3bbd39ecb4cbf16d0e51652d1639ae90efc
Antonio Rivera <antoniori@google.com>
2024-09-24 17:29:43 +0000

    Upstream go/eurcl/958506.
    This fixes a bug with the video plane, e.g. when casting multiple TikTok
    videos in a row.
    
    Bug: b:313705910
    
    Test: Ran unit tests
    Change-Id: I3bd5d7a816f48dfe7da4b818599c90fde606395d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884021
    Reviewed-by: Vigen Issahhanjan <vigeni@google.com>
    Commit-Queue: Antonio Rivera <antoniori@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359447}

--- chromecast/starboard/media/media/starboard_video_plane.cc
+++ chromecast/starboard/media/media/starboard_video_plane.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "starboard_video_plane.h"
 
+#include "base/logging.h"
+
 namespace chromecast {
 namespace media {
 
@@ -24,6 +26,10 @@ void StarboardVideoPlane::SetGeometry(const RectF& display_rect,
                        weak_factory_.GetWeakPtr(), display_rect, transform));
     return;
   }
+
+  // We store the current plane size so that we can run any newly-added
+  // callbacks with this resolution.
+  current_plane_ = std::make_pair(display_rect, transform);
 
   for (const auto& token_and_callback : token_to_callback_) {
     token_and_callback.second.Run(display_rect, transform);
@@ -52,6 +58,15 @@ void StarboardVideoPlane::RegisterCallbackForToken(
     int64_t token,
     GeometryChangedCallback callback) {
   DCHECK(task_runner_->RunsTasksInCurrentSequence());
+
+  if (current_plane_) {
+    LOG(INFO) << "Running pending geometry callback. Setting video plane to "
+              << current_plane_->first.width << "x"
+              << current_plane_->first.height << ", offset ("
+              << current_plane_->first.x << ", " << current_plane_->first.y
+              << ")";
+    callback.Run(current_plane_->first, current_plane_->second);
+  }
   token_to_callback_[token] = std::move(callback);
 }
 
