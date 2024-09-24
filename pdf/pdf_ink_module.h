fa5346296ada947e03790b42b6839fde62424bb9
Takashi Sakamoto <tasak@google.com>
2024-09-24 01:10:17 +0000

    Revert "Reland "[PDF Ink Signatures] Switch DrawRenderTransform test case to call Draw()""
    This reverts commit d46dcdd094d23fd642f21101318c341941700dc6.
    
    Reason for revert: suspect causing PdfInkModuleStrokeTest.DrawRenderTransform failure on Linux MSan Tests and Linux ChromiumOS MSan Tests
    
    pdf_unittests failed because of:
    PdfInkModuleStrokeTest.DrawRenderTransform
    
    First build failure:
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20ChromiumOS%20MSan%20Tests/46353/overview
    https://ci.chromium.org/ui/p/chromium/builders/ci/Linux%20MSan%20Tests/50742/overview
    
    Original change's description:
    > Reland "[PDF Ink Signatures] Switch DrawRenderTransform test case to call Draw()"
    >
    > This is a reland of commit 9d82486a9370a787bbdce9d887edc7186fcf4936
    >
    > This reland changes the pixel comparators. Where possible, the tests use
    > cc::ExactPixelComparator(). For build configs where the rendering is not
    > pixel perfect, use cc::FuzzyPixelComparator, configured as a
    > "FuzzyPixelOffByTwoComparator".
    >
    > Original change's description:
    > > [PDF Ink Signatures] Switch DrawRenderTransform test case to call Draw()
    > >
    > > Remove the code that checks the transformed Draw() would have used, and
    > > instead check the output from Draw(). Add a FuzzyMatchesPngFile() test
    > > utility to check against new expectation PNGs in //pdf/test/data/ink.
    > > The existing exact-pixel MatchesPngFile() function mostly works, but not
    > > for ASAN/TSAN.
    > >
    > > Bug: 351990827
    > > Change-Id: Ie11244bcfe91a413f102b8a3bbc34d4c199f789c
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5871447
    > > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > > Reviewed-by: Alan Screen <awscreen@chromium.org>
    > > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1357215}
    >
    > Bug: 351990827
    > Change-Id: Ibe0fa6b02cf163b690131711d0d360bf4ec35c48
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874106
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Alan Screen <awscreen@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1358916}
    
    Bug: 351990827
    Change-Id: I041a3974fc8443ec2f9db4d0086c6db629ca74c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881145
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Owners-Override: Takashi Sakamoto <tasak@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1359119}

--- pdf/pdf_ink_module.h
+++ pdf/pdf_ink_module.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/containers/flat_set.h"
+#include "base/functional/callback.h"
 #include "base/memory/raw_ref.h"
 #include "base/time/time.h"
 #include "base/values.h"
@@ -35,6 +36,10 @@ class WebInputEvent;
 class WebMouseEvent;
 }  // namespace blink
 
+namespace ink {
+class AffineTransform;
+}
+
 namespace chrome_pdf {
 
 class PdfInkBrush;
@@ -51,6 +56,9 @@ class PdfInkModule {
   // Mapping of a 0-based page index to the input points that make up the
   // strokes for that page.
   using DocumentStrokeInputPointsMap = std::map<int, PageStrokeInputPoints>;
+
+  using RenderTransformCallback =
+      base::RepeatingCallback<void(const ink::AffineTransform& transform)>;
 
   explicit PdfInkModule(PdfInkModuleClient& client);
   PdfInkModule(const PdfInkModule&) = delete;
@@ -90,6 +98,11 @@ class PdfInkModule {
   DocumentStrokeInputPointsMap GetStrokesInputPositionsForTesting() const;
   DocumentStrokeInputPointsMap GetVisibleStrokesInputPositionsForTesting()
       const;
+
+  // For testing only. Provide a callback to use whenever the rendering
+  // transform is determined for `Draw()`.
+  void SetDrawRenderTransformCallbackForTesting(
+      RenderTransformCallback callback);
 
  private:
   using StrokeInputSegment = std::vector<ink::StrokeInput>;
@@ -259,6 +272,8 @@ class PdfInkModule {
   DocumentStrokesMap strokes_;
 
   PdfInkUndoRedoModel undo_redo_model_;
+
+  RenderTransformCallback draw_render_transform_callback_for_testing_;
 };
 
 }  // namespace chrome_pdf
