1a77f65a950ee9724e983effbfb49a1b4b053aa2
Anders Hartvoll Ruud <andruud@chromium.org>
2024-09-24 11:49:50 +0000

    [css-nesting] Keep parsing after {}-block for rules with invalid selector
    When encountering a rule with an invalid selector, we currently recover
    by consuming until the next semicolon, which isn't correct. Instead,
    we should simply do nothing, and keep parsing right after the {}-block.
    
    The spec used to be less clear about this, but at least at the time
    of writing, nothing invokes the skip-until-semicolon behavior except
    for rule preludes that look like custom properties.
    
    This CL fixes this issue by adding a `invalid_rule_error` out-param
    to ConsumeStyleRule. There are two other things that are worth noting:
    
     - The "consume a qualified rule" algorithm in the spec mentions
       a `nested` flag which modifies its behavior. This flag is
       actually equivalent to `semicolon_aborts_nested_selector`
       in our parser, so I've renamed this.
     - During handling of `custom_property_ambiguity`, nested=false case,
       we're supposed to consume a block, and do nothing.
       This can't be tested using CSSParserImpl's public API, since
       failing to consume a block there would just cause the next attempt
       at parsing a rule to consume an empty prelude (then the block).
       But I don't think we should rely on that, hence I'm using unit
       tests to cover that line.
    
    [1] https://drafts.csswg.org/css-syntax/#consume-qualified-rule
    
    Test: external/wpt/css/css-syntax/custom-property-rule-ambiguity.html
    Change-Id: I04dc0c3745be5eafe792fd6c40971545274d9794
    Fixed: 343979378
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881815
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359288}

--- third_party/blink/renderer/core/css/parser/css_parser_impl.h
+++ third_party/blink/renderer/core/css/parser/css_parser_impl.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <optional>
 
+#include "base/gtest_prod_util.h"
 #include "css_at_rule_id.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
@@ -179,6 +180,8 @@ class CORE_EXPORT CSSParserImpl {
   CSSParserMode GetMode() const;
 
  private:
+  friend class TestCSSParserImpl;
+
   enum RuleListType {
     kTopLevelRuleList,
     kRegularRuleList,
@@ -260,10 +263,21 @@ class CORE_EXPORT CSSParserImpl {
       std::unique_ptr<Vector<KeyframeOffset>> key_list,
       const RangeOffset& prelude_offset,
       CSSParserTokenStream& block);
+
+  // https://drafts.csswg.org/css-syntax/#consume-a-qualified-rule
+  //
+  // - CSSNestingType determines which implicit selector to insert for relative
+  //   selectors ('&' for kNesting, and ':scope' for kScope).
+  // - `parent_rule_for_nesting` determines what '&' points to.
+  // - `nested` refers to the "nested" flag referenced by the linked
+  //    parser algorithm.
+  // - `invalid_rule_error` (output parameter) is set when the selector list
+  //   didn't parse, but we have a valid rule otherwise.
   StyleRule* ConsumeStyleRule(CSSParserTokenStream&,
                               CSSNestingType,
                               StyleRule* parent_rule_for_nesting,
-                              bool semicolon_aborts_nested_selector);
+                              bool nested,
+                              bool& invalid_rule_error);
   StyleRule* ConsumeStyleRuleContents(base::span<CSSSelector> selector_vector,
                                       CSSParserTokenStream& stream);
 
@@ -288,7 +302,8 @@ class CORE_EXPORT CSSParserImpl {
                                    StyleRule::RuleType parent_rule_type,
                                    CSSParserTokenStream& stream,
                                    CSSNestingType,
-                                   StyleRule* parent_rule_for_nesting);
+                                   StyleRule* parent_rule_for_nesting,
+                                   bool& invalid_rule_error);
 
   // Returns true if a declaration was parsed and added to parsed_properties_,
   // and false otherwise.
