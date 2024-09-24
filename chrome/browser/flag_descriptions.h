a4e850eb2a662046cbf4202d5bf299cd07ab4a02
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-09-24 09:05:28 +0000

    [desktop] Add flag to enable UserSelectableType::kBookmarks on sign-in
    It's a test-only flag, it'll never be launched.
    Normally, if kReplaceSyncPromosWithSignInPromos is disabled,
    UserSelectableType::kBookmarks is disabled by default upon sign-in. This
    flag makes the type enabled by default, for manual testing.
    
    Bug: 303613231
    Change-Id: I204ea716db82ef939fd0ec7cb78b43e5dabdca5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882258
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359245}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -220,6 +220,9 @@ extern const char kEnableBenchmarkingChoiceDisabled[];
 extern const char kEnableBenchmarkingChoiceDefaultFeatureStates[];
 extern const char kEnableBenchmarkingChoiceMatchFieldTrialTestingConfig[];
 
+extern const char kEnableBookmarksSelectedTypeOnSigninForTestingName[];
+extern const char kEnableBookmarksSelectedTypeOnSigninForTestingDescription[];
+
 extern const char kFontationsFontBackendName[];
 extern const char kFontationsFontBackendDescription[];
 
