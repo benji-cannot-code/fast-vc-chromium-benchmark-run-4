960e744e6db7ce867d472ae4e6317704e8473aa0
Keren Zhu <kerenzhu@chromium.org>
2024-09-24 06:01:25 +0000

    base: allows const reference args in ObserverList::Notify()
    This CL updates ObserverList::Notify() to accept const reference
    arguments (const arg&). Previously, Notify() rejects any move-only
    arguments even if they are passed by const-reference.
    
    This allows more flexible use of ObserverList with move-only types while
    maintaining safety.
    
    Bug: 40727208
    Change-Id: Ice874bc689b19fa18b67ddf88d65865a4a9a29c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878439
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1359185}

--- base/observer_list_internal.h
+++ base/observer_list_internal.h
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_OBSERVER_LIST_INTERNAL_H_
 #define BASE_OBSERVER_LIST_INTERNAL_H_
 
-#include <concepts>
 #include <string>
+#include <type_traits>
 
 #include "base/base_export.h"
 #include "base/check.h"
@@ -175,9 +175,6 @@ class WeakLinkNode : public base::LinkNode<WeakLinkNode<ObserverList>> {
   // based on analysis of sampling profiler data and tab_search:top100:2020.
   RAW_PTR_EXCLUSION ObserverList* list_ = nullptr;
 };
-
-template <typename T>
-concept IsMoveOnly = std::move_constructible<T> && !std::copy_constructible<T>;
 
 }  // namespace internal
 }  // namespace base
