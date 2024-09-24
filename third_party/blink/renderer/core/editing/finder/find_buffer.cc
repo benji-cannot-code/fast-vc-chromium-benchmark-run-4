f437acb8c026c99b853deb30e9cc3eb44912efdc
Kent Tamura <tkent@chromium.org>
2024-09-24 07:00:37 +0000

    finder: Skip Text nodes without LayoutObjects
    FindBuffer::ForwardVisibleTextNode() returned a Text node which has no
    LayoutObject.  For example, whitespace-only Text nodes between blocks.
    
    This CL updates ForwardVisibleTextNode() so that it skips such Text
    nodes.  This should have no user-visible behavior changes, but improves
    the runtime performance slightly.
    
    Bug: 40755728
    Change-Id: Iae0e920e812be31b4a31006763ef8b0976c2032a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874687
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359203}

--- third_party/blink/renderer/core/editing/finder/find_buffer.cc
+++ third_party/blink/renderer/core/editing/finder/find_buffer.cc
@@ -165,7 +165,9 @@ Node* GetVisibleTextNode(Node& start_node) {
       continue;
     }
     if (style && style->UsedVisibility() == EVisibility::kVisible &&
-        node->IsTextNode()) {
+        node->IsTextNode() &&
+        (!RuntimeEnabledFeatures::FindTextSkipCollapsedTextEnabled() ||
+         node->GetLayoutObject())) {
       return node;
     }
     // This element is hidden, but node might be visible,
