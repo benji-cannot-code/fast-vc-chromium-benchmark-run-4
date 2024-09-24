95049c2893389965c41bd7d19e472f58e1d4643d
Darren Shen <shend@chromium.org>
2024-09-24 07:57:23 +0000

    Break emoji ties by length of emoji name.
    This allows symbols like 'superset of' to be found correctly.
    
    Bug: b:369223047
    Change-Id: I50a9668e692fb30eb3bfb5b515467a362df91914
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882276
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359223}

--- chromeos/ash/components/emoji/emoji_search_unittest.cc
+++ chromeos/ash/components/emoji/emoji_search_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace emoji {
 namespace {
 
+using ::testing::_;
 using ::testing::ElementsAre;
 using ::testing::FieldsAre;
 using ::testing::FloatNear;
@@ -565,6 +566,31 @@ TEST_F(EmojiSearchTest, KeywordPartialScoresHigherThanFullKeywordMatch) {
                           FieldsAre(FloatNear(0.0021f, 0.00005f), "😀a")));
   EXPECT_THAT(result.symbols, IsEmpty());
   EXPECT_THAT(result.emoticons, IsEmpty());
+}
+
+TEST_F(EmojiSearchTest, BreakTiesByNameLength) {
+  ScopedFakeResourceBundleDelegate mock_resource_delegate(
+      {{FakeResource{IDR_EMOJI_PICKER_EMOJI_15_0_ORDERING_JSON_START, R"([])"},
+        FakeResource{IDR_EMOJI_PICKER_EMOJI_15_0_ORDERING_JSON_REMAINING,
+                     R"([])"},
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+        FakeResource{IDR_EMOJI_PICKER_EN_INTERNAL, R"([])"},
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
+        FakeResource{IDR_EMOJI_PICKER_SYMBOL_ORDERING_JSON,
+                     R"([{"emoji":[
+                     {"base": {"string":"ab","name":"a b"}},
+                     {"base": {"string":"abc","name":"a b c"}},
+                     {"base": {"string":"abcd","name":"a b c d"}}
+                     ]}])"},
+        FakeResource{IDR_EMOJI_PICKER_EMOTICON_ORDERING_JSON, R"([])"}}});
+
+  EmojiSearch search;
+
+  EmojiSearchResult result = search.SearchEmoji(u"a b", {{"en"}});
+
+  EXPECT_THAT(result.symbols,
+              ElementsAre(FieldsAre(_, "ab"), FieldsAre(_, "abc"),
+                          FieldsAre(_, "abcd")));
 }
 
 }  // namespace
