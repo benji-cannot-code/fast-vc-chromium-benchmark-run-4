6467469313884960321ac458773cc3af045090a5
Haihan Chen <haihan@google.com>
2024-09-24 13:57:18 +0000

    Remove unnecessary logging in content analysis downloads
    This is gated but additional logging was added to this file in crrev.com/c/5875783 by mistake, apologies. Removing it.
    
    Change-Id: I08b218f7d992658b47f99ca19f2d2da46f9441d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5885016
    Commit-Queue: Haihan Chen <haihan@google.com>
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359325}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.cc
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.cc
@@ -71,7 +71,6 @@ void ContentAnalysisDownloadsDelegate::BypassWarnings(
               enterprise_obfuscation::DownloadObfuscationData::kUserDataKey));
 
   if (obfuscation_data && obfuscation_data->is_obfuscated) {
-    LOG(ERROR) << "is obfuscated!";
     base::ThreadPool::PostTaskAndReplyWithResult(
         FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
         base::BindOnce(&enterprise_obfuscation::DeobfuscateFileInPlace,
@@ -90,10 +89,7 @@ void ContentAnalysisDownloadsDelegate::OnDeobfuscationComplete(
   if (!deobfuscation_result.has_value()) {
     // TODO(b/367259664): Add better error handling for deobfuscation.
     DVLOG(1) << "Failed to deobfuscate file.";
-    LOG(ERROR) << "failed deobfuscation";
   }
-  LOG(ERROR) << "deobfuscation was successful";
-
   Open();
 }
 
