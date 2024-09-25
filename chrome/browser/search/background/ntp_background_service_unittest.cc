8a1806c428265113103f3c35eef7ce1cfe37d5b0
Yoichi Osato <yoichio@chromium.org>
2024-09-25 00:57:19 +0000

    Revert "[panorama] Move relevant error detection metrics to frontend."
    This reverts commit d215706e01461a68ac2a87565f8c7c0da4a4d89e.
    
    Reason for revert: Bot failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-win-cross-rel/2591/test-results?sortby=&groupby=
    
    Original change's description:
    > [panorama] Move relevant error detection metrics to frontend.
    >
    > Moves the recording of `NewTabPage.BackgroundService.Images.Headers.ErrorDetected`
    > for collection previews and theme previews to the frontend
    > as the errors now initiate there.
    >
    > *The background metric [1] will remain in the backend.
    >
    > [1]https://source.chromium.org/chromium/chromium/src/+/main:chrome/browser/search/background/ntp_custom_background_service.cc;l=775?q=-file:out%20%22NtpImageType%22#:~:text=773-,774,-775
    >
    > Change-Id: Id90bc0475bc9a5abc1dda7e59759e410b9667596
    > Bug: 367699101, 367702048
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874231
    > Reviewed-by: Roman Arora <romanarora@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1359562}
    
    Bug: 367699101, 367702048
    Change-Id: I6eb697ca612d13e06d938c85dad4d4fa34f040ea
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889510
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Owners-Override: Yoichi Osato <yoichio@chromium.org>
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359693}

--- chrome/browser/search/background/ntp_background_service_unittest.cc
+++ chrome/browser/search/background/ntp_background_service_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/run_loop.h"
 #include "base/test/bind.h"
 #include "base/test/metrics/histogram_tester.h"
-#include "base/test/run_until.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
 #include "chrome/browser/browser_process.h"
@@ -47,13 +46,17 @@ class MockNtpBackgroundServiceObserver : public NtpBackgroundServiceObserver {
 
 }  // namespace
 
-class NtpBackgroundServiceTest : public testing::Test {
+class NtpBackgroundServiceTest : public testing::Test,
+                                 public ::testing::WithParamInterface<bool> {
  public:
   NtpBackgroundServiceTest()
       : task_environment_(content::BrowserTaskEnvironment::IO_MAINLOOP),
         test_shared_loader_factory_(
             base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
                 &test_url_loader_factory_)) {
+    feature_list_.InitWithFeatureState(
+        std::move(ntp_features::kNtpBackgroundImageErrorDetection),
+        BackgroundImageErrorDetectionEnabled());
   }
 
   void TearDown() override {
@@ -93,6 +96,10 @@ class NtpBackgroundServiceTest : public testing::Test {
     return &test_url_loader_factory_;
   }
 
+  // TODO(b:367699101) - Remove test parameters once image error
+  // detection logic is initiated solely in the frontend.
+  bool BackgroundImageErrorDetectionEnabled() const { return GetParam(); }
+
  protected:
   // Required to run tests from UI and threads.
   content::BrowserTaskEnvironment task_environment_;
@@ -105,14 +112,14 @@ class NtpBackgroundServiceTest : public testing::Test {
   std::unique_ptr<NtpBackgroundService> service_;
 };
 
-TEST_F(NtpBackgroundServiceTest, CollectionRequest) {
+INSTANTIATE_TEST_SUITE_P(All, NtpBackgroundServiceTest, ::testing::Bool());
+
+TEST_P(NtpBackgroundServiceTest, CollectionRequest) {
   g_browser_process->SetApplicationLocale("foo");
-
   service()->FetchCollectionInfo();
-  EXPECT_TRUE(base::test::RunUntil([&]() {
-    return test_url_loader_factory()->pending_requests()->size() == 1u;
-  }));
+  base::RunLoop().RunUntilIdle();
 
+  EXPECT_EQ(1u, test_url_loader_factory()->pending_requests()->size());
   std::string request_body(test_url_loader_factory()
                                ->pending_requests()
                                ->at(0)
@@ -123,7 +130,13 @@ TEST_F(NtpBackgroundServiceTest, CollectionRequest) {
   ntp::background::GetCollectionsRequest collection_request;
   EXPECT_TRUE(collection_request.ParseFromString(request_body));
   EXPECT_EQ("foo", collection_request.language());
-  EXPECT_EQ(4, collection_request.filtering_label_size());
+  if (BackgroundImageErrorDetectionEnabled()) {
+    EXPECT_EQ(5, collection_request.filtering_label_size());
+    EXPECT_EQ("chrome_desktop_ntp.error_detection",
+              collection_request.filtering_label(4));
+  } else {
+    EXPECT_EQ(4, collection_request.filtering_label_size());
+  }
   EXPECT_EQ("chrome_desktop_ntp", collection_request.filtering_label(0));
   EXPECT_EQ("chrome_desktop_ntp.M" + version_info::GetMajorVersionNumber(),
             collection_request.filtering_label(1));
@@ -132,39 +145,7 @@ TEST_F(NtpBackgroundServiceTest, CollectionRequest) {
   EXPECT_EQ("chrome_desktop_ntp.gm3", collection_request.filtering_label(3));
 }
 
-TEST_F(NtpBackgroundServiceTest,
-       CollectionRequestWithImageErrorDetectionEnabled) {
-  base::test::ScopedFeatureList scoped_feature_list;
-  scoped_feature_list.InitAndEnableFeature(
-      ntp_features::kNtpBackgroundImageErrorDetection);
-  g_browser_process->SetApplicationLocale("foo");
-
-  service()->FetchCollectionInfo();
-  EXPECT_TRUE(base::test::RunUntil([&]() {
-    return test_url_loader_factory()->pending_requests()->size() == 1u;
-  }));
-
-  std::string request_body(test_url_loader_factory()
-                               ->pending_requests()
-                               ->at(0)
-                               .request.request_body->elements()
-                               ->at(0)
-                               .As<network::DataElementBytes>()
-                               .AsStringPiece());
-  ntp::background::GetCollectionsRequest collection_request;
-  EXPECT_TRUE(collection_request.ParseFromString(request_body));
-  EXPECT_EQ("foo", collection_request.language());
-  EXPECT_EQ(5, collection_request.filtering_label_size());
-  EXPECT_EQ("chrome_desktop_ntp.error_detection",
-            collection_request.filtering_label(4));
-  EXPECT_EQ("chrome_desktop_ntp.M" + version_info::GetMajorVersionNumber(),
-            collection_request.filtering_label(1));
-  EXPECT_EQ("chrome_desktop_ntp.panorama",
-            collection_request.filtering_label(2));
-  EXPECT_EQ("chrome_desktop_ntp.gm3", collection_request.filtering_label(3));
-}
-
-TEST_F(NtpBackgroundServiceTest, CollectionInfoNetworkError) {
+TEST_P(NtpBackgroundServiceTest, CollectionInfoNetworkError) {
   SetUpResponseWithNetworkError(service()->GetCollectionsLoadURLForTesting());
 
   ASSERT_TRUE(service()->collection_info().empty());
@@ -178,7 +159,7 @@ TEST_F(NtpBackgroundServiceTest, CollectionInfoNetworkError) {
             ErrorType::NET_ERROR);
 }
 
-TEST_F(NtpBackgroundServiceTest, BadCollectionsResponse) {
+TEST_P(NtpBackgroundServiceTest, BadCollectionsResponse) {
   SetUpResponseWithData(service()->GetCollectionsLoadURLForTesting(),
                         "bad serialized GetCollectionsResponse");
 
@@ -193,7 +174,7 @@ TEST_F(NtpBackgroundServiceTest, BadCollectionsResponse) {
             ErrorType::SERVICE_ERROR);
 }
 
-TEST_F(NtpBackgroundServiceTest, GoodCollectionsResponse) {
+TEST_P(NtpBackgroundServiceTest, GoodCollectionsResponse) {
   ntp::background::Collection collection;
   collection.set_collection_id("shapes");
   collection.set_collection_name("Shapes");
@@ -223,7 +204,7 @@ TEST_F(NtpBackgroundServiceTest, GoodCollectionsResponse) {
   EXPECT_EQ(service()->collection_error_info().error_type, ErrorType::NONE);
 }
 
-TEST_F(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasNoReplacement) {
+TEST_P(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasNoReplacement) {
   ntp::background::Collection collection;
   collection.set_collection_id("shapes");
   collection.set_collection_name("Shapes");
@@ -251,9 +232,15 @@ TEST_F(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasNoReplacement) {
   service()->FetchReplacementCollectionPreviewImage(
       collection.collection_id(), std::move(replacement_image_callback));
   run_loop.Run();
+
+  histogram_tester_.ExpectTotalCount(
+      "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 1);
+  ASSERT_EQ(1, histogram_tester_.GetBucketCount(
+                   "NewTabPage.BackgroundService.Images.Headers.ErrorDetected",
+                   NtpImageType::kCollections));
 }
 
-TEST_F(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasReplacement) {
+TEST_P(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasReplacement) {
   ntp::background::Collection collection;
   collection.set_collection_id("shapes");
   collection.set_collection_name("Shapes");
@@ -290,9 +277,16 @@ TEST_F(NtpBackgroundServiceTest, BrokenCollectionPreviewImageHasReplacement) {
     service()->FetchReplacementCollectionPreviewImage(
         collection.collection_id(), std::move(replacement_image_callback));
     run_loop.Run();
+
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 1);
+    ASSERT_EQ(1,
+              histogram_tester_.GetBucketCount(
+                  "NewTabPage.BackgroundService.Images.Headers.ErrorDetected",
+                  NtpImageType::kCollections));
 }
 
-TEST_F(NtpBackgroundServiceTest, CollectionImagesNetworkError) {
+TEST_P(NtpBackgroundServiceTest, CollectionImagesNetworkError) {
   SetUpResponseWithNetworkError(service()->GetImagesURLForTesting());
 
   ASSERT_TRUE(service()->collection_images().empty());
@@ -301,12 +295,16 @@ TEST_F(NtpBackgroundServiceTest, CollectionImagesNetworkError) {
   service()->FetchCollectionImageInfo("shapes");
   base::RunLoop().RunUntilIdle();
 
+  if (BackgroundImageErrorDetectionEnabled()) {
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 0);
+  }
   EXPECT_TRUE(service()->collection_images().empty());
   EXPECT_EQ(service()->collection_images_error_info().error_type,
             ErrorType::NET_ERROR);
 }
 
-TEST_F(NtpBackgroundServiceTest, BadCollectionImagesResponse) {
+TEST_P(NtpBackgroundServiceTest, BadCollectionImagesResponse) {
   SetUpResponseWithData(service()->GetImagesURLForTesting(),
                         "bad serialized GetImagesInCollectionResponse");
 
@@ -316,12 +314,16 @@ TEST_F(NtpBackgroundServiceTest, BadCollectionImagesResponse) {
   service()->FetchCollectionImageInfo("shapes");
   base::RunLoop().RunUntilIdle();
 
+  if (BackgroundImageErrorDetectionEnabled()) {
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 0);
+  }
   EXPECT_TRUE(service()->collection_images().empty());
   EXPECT_EQ(service()->collection_images_error_info().error_type,
             ErrorType::SERVICE_ERROR);
 }
 
-TEST_F(NtpBackgroundServiceTest, ImageInCollectionHasNetworkError) {
+TEST_P(NtpBackgroundServiceTest, ImageInCollectionHasNetworkError) {
   ntp::background::Image image;
   image.set_asset_id(12345);
   image.set_image_url(kTestImageUrl);
@@ -356,7 +358,7 @@ TEST_F(NtpBackgroundServiceTest, ImageInCollectionHasNetworkError) {
               ErrorType::NONE);
 }
 
-TEST_F(NtpBackgroundServiceTest, GoodCollectionImagesResponse) {
+TEST_P(NtpBackgroundServiceTest, GoodCollectionImagesResponse) {
   ntp::background::Image image;
   image.set_asset_id(12345);
   image.set_image_url(kTestImageUrl);
@@ -385,13 +387,17 @@ TEST_F(NtpBackgroundServiceTest, GoodCollectionImagesResponse) {
   collection_image.attribution.push_back(image.attribution(0).text());
   collection_image.attribution_action_url = GURL(image.action_url());
 
+  if (BackgroundImageErrorDetectionEnabled()) {
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 0);
+  }
   EXPECT_FALSE(service()->collection_images().empty());
   EXPECT_THAT(service()->collection_images().at(0), Eq(collection_image));
   EXPECT_EQ(service()->collection_images_error_info().error_type,
             ErrorType::NONE);
 }
 
-TEST_F(NtpBackgroundServiceTest,
+TEST_P(NtpBackgroundServiceTest,
        CollectionImageInfoRequestsAreIgnoredIfAnotherIsInProgress) {
   ntp::background::Collection collection;
   collection.set_collection_id("shapes");
@@ -442,13 +448,17 @@ TEST_F(NtpBackgroundServiceTest,
       GURL(image.image_url() + service()->GetImageOptionsForTesting());
   collection_image.attribution.push_back(image.attribution(0).text());
 
+  if (BackgroundImageErrorDetectionEnabled()) {
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 0);
+  }
   EXPECT_FALSE(service()->collection_info().empty());
   EXPECT_THAT(service()->collection_info().at(0), Eq(collection_info));
   EXPECT_FALSE(service()->collection_images().empty());
   EXPECT_THAT(service()->collection_images().at(0), Eq(collection_image));
 }
 
-TEST_F(NtpBackgroundServiceTest,
+TEST_P(NtpBackgroundServiceTest,
        CollectionImageInfoCanBeSuccessfullyFetchedMultipleTimes) {
   ntp::background::Image image;
   image.set_image_url(kTestImageUrl);
@@ -473,7 +483,7 @@ TEST_F(NtpBackgroundServiceTest,
   EXPECT_THAT(service()->collection_images().at(0).collection_id, "colors");
 }
 
-TEST_F(NtpBackgroundServiceTest, NextImageNetworkError) {
+TEST_P(NtpBackgroundServiceTest, NextImageNetworkError) {
   SetUpResponseWithNetworkError(service()->GetNextImageURLForTesting());
 
   service()->FetchNextCollectionImage("shapes", std::nullopt);
@@ -483,7 +493,7 @@ TEST_F(NtpBackgroundServiceTest, NextImageNetworkError) {
               Eq(ErrorType::NET_ERROR));
 }
 
-TEST_F(NtpBackgroundServiceTest, BadNextImageResponse) {
+TEST_P(NtpBackgroundServiceTest, BadNextImageResponse) {
   SetUpResponseWithData(service()->GetNextImageURLForTesting(),
                         "bad serialized GetImageFromCollectionResponse");
 
@@ -494,7 +504,7 @@ TEST_F(NtpBackgroundServiceTest, BadNextImageResponse) {
               Eq(ErrorType::SERVICE_ERROR));
 }
 
-TEST_F(NtpBackgroundServiceTest, GoodNextImageResponse) {
+TEST_P(NtpBackgroundServiceTest, GoodNextImageResponse) {
   ntp::background::Image image;
   image.set_asset_id(12345);
   image.set_image_url(kTestImageUrl);
@@ -531,7 +541,7 @@ TEST_F(NtpBackgroundServiceTest, GoodNextImageResponse) {
               Eq(ErrorType::NONE));
 }
 
-TEST_F(NtpBackgroundServiceTest, MultipleRequestsNextImage) {
+TEST_P(NtpBackgroundServiceTest, MultipleRequestsNextImage) {
   ntp::background::Image image;
   image.set_asset_id(12345);
   image.set_image_url(kTestImageUrl);
@@ -571,7 +581,7 @@ TEST_F(NtpBackgroundServiceTest, MultipleRequestsNextImage) {
               Eq(ErrorType::NONE));
 }
 
-TEST_F(NtpBackgroundServiceTest, CheckValidAndInvalidBackdropUrls) {
+TEST_P(NtpBackgroundServiceTest, CheckValidAndInvalidBackdropUrls) {
   ntp::background::Image image;
   image.set_asset_id(12345);
   image.set_image_url(kTestImageUrl);
@@ -590,6 +600,10 @@ TEST_F(NtpBackgroundServiceTest, CheckValidAndInvalidBackdropUrls) {
   service()->FetchCollectionImageInfo("shapes");
   base::RunLoop().RunUntilIdle();
 
+  if (BackgroundImageErrorDetectionEnabled()) {
+    histogram_tester_.ExpectTotalCount(
+        "NewTabPage.BackgroundService.Images.Headers.ErrorDetected", 0);
+  }
   EXPECT_TRUE(service()->IsValidBackdropUrl(
       GURL(image.image_url() + service()->GetImageOptionsForTesting())));
   EXPECT_FALSE(service()->IsValidBackdropUrl(
@@ -598,7 +612,7 @@ TEST_F(NtpBackgroundServiceTest, CheckValidAndInvalidBackdropUrls) {
       GURL("https://wallpapers.co/another_image")));
 }
 
-TEST_F(NtpBackgroundServiceTest, GetThumbnailUrl) {
+TEST_P(NtpBackgroundServiceTest, GetThumbnailUrl) {
   const GURL kInvalidUrl("foo");
   const GURL kValidUrl("https://www.foo.com");
   const GURL kValidThumbnailUrl("https://www.foo.com/thumbnail");
@@ -609,7 +623,7 @@ TEST_F(NtpBackgroundServiceTest, GetThumbnailUrl) {
   EXPECT_EQ(GURL(), service()->GetThumbnailUrl(kInvalidUrl));
 }
 
-TEST_F(NtpBackgroundServiceTest, OverrideBaseUrl) {
+TEST_P(NtpBackgroundServiceTest, OverrideBaseUrl) {
   base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
       "collections-base-url", "https://foo.com");
   service()->FetchCollectionInfo();
@@ -620,7 +634,7 @@ TEST_F(NtpBackgroundServiceTest, OverrideBaseUrl) {
             test_url_loader_factory()->pending_requests()->at(0).request.url);
 }
 
-TEST_F(NtpBackgroundServiceTest, VerifyURLMetricsWithNetworkSuccess) {
+TEST_P(NtpBackgroundServiceTest, VerifyURLMetricsWithNetworkSuccess) {
   SetUpResponseWithNetworkSuccess(GURL(kTestImageUrl));
   histogram_tester_.ExpectTotalCount(
       "NewTabPage.BackgroundService.Images.Headers.RequestLatency", 0);
@@ -646,7 +660,7 @@ TEST_F(NtpBackgroundServiceTest, VerifyURLMetricsWithNetworkSuccess) {
                    net::HTTP_OK));
 }
 
-TEST_F(NtpBackgroundServiceTest, VerifyURLMetricsWithNetworkError) {
+TEST_P(NtpBackgroundServiceTest, VerifyURLMetricsWithNetworkError) {
   SetUpResponseWithNetworkError(GURL(kTestImageUrl));
   histogram_tester_.ExpectTotalCount(
       "NewTabPage.BackgroundService.Images.Headers.RequestLatency", 0);
