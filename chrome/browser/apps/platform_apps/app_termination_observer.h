dc591304d8f5e6fc22d55b2cbdcbfee754a4228b
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-09-25 23:32:44 +0000

    Decouple AppTerminationObserver from its factory
    No behavior change. Details in the linked bug.
    
    Bug: 368246456
    Change-Id: I559916b4cfcc87abc96a1e064da74101f42784fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889689
    Reviewed-by: Stefan Kuhne <skuhne@chromium.org>
    Commit-Queue: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360265}

--- chrome/browser/apps/platform_apps/app_termination_observer.h
+++ chrome/browser/apps/platform_apps/app_termination_observer.h
@@ -12,8 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "components/keyed_service/core/keyed_service.h"
 
-class BrowserContextKeyedServiceFactory;
-
 namespace content {
 class BrowserContext;
 }
@@ -28,8 +26,6 @@ class AppTerminationObserver : public KeyedService {
   AppTerminationObserver(const AppTerminationObserver&) = delete;
   AppTerminationObserver& operator=(const AppTerminationObserver&) = delete;
   ~AppTerminationObserver() override;
-
-  static BrowserContextKeyedServiceFactory* GetFactoryInstance();
 
   // KeyedService:
   void Shutdown() override;
--- chrome/browser/apps/platform_apps/browser_context_keyed_service_factories.cc
+++ chrome/browser/apps/platform_apps/browser_context_keyed_service_factories.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/platform_apps/browser_context_keyed_service_factories.h"
 
 #include "chrome/browser/apps/platform_apps/app_load_service_factory.h"
-#include "chrome/browser/apps/platform_apps/app_termination_observer.h"
+#include "chrome/browser/apps/platform_apps/app_termination_observer_factory.h"
 #include "chrome/browser/apps/platform_apps/shortcut_manager_factory.h"
 
 namespace chrome_apps {
@@ -14,7 +14,7 @@ namespace chrome_apps {
 void EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   apps::AppLoadServiceFactory::GetInstance();
   AppShortcutManagerFactory::GetInstance();
-  AppTerminationObserver::GetFactoryInstance();
+  AppTerminationObserverFactory::GetInstance();
 }
 
 }  // namespace chrome_apps
