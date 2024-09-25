9443f65667f95876833a7ccab805a0fb9c2f4413
Florian Leimgruber <fleimgruber@google.com>
2024-09-25 09:45:58 +0000

    Remove unused import
    Bug: 40100455
    Change-Id: I1469fe5b094fc138d046615a2183b32389af69e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890684
    Auto-Submit: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1359836}

--- components/autofill/core/browser/metrics/shadow_prediction_metrics.cc
+++ components/autofill/core/browser/metrics/shadow_prediction_metrics.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
-#include "components/autofill/core/browser/form_parsing/autofill_parsing_utils.h"
 #include "components/autofill/core/browser/form_parsing/buildflags.h"
 #include "components/autofill/core/browser/heuristic_source.h"
 #include "components/autofill/core/common/autofill_features.h"
