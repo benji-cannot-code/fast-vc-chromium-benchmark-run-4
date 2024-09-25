d80185310ccf7fe29a4caa49570f65a6bddc0108
Mahmoud Rashad <mmrashad@google.com>
2024-09-25 13:06:51 +0000

    Clean up kSyncAndroidLimitNTPPromoImpressions
    Bug: 331768008
    Change-Id: If66043653bf8be9612acac3f11cdc6069c16d62d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5886175
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359891}

--- chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/signin/SigninManagerImplTest.java
@@ -441,9 +441,6 @@ public class SigninManagerImplTest {
     }
 
     @Test
-    @EnableFeatures({
-        ChromeFeatureList.SYNC_ANDROID_LIMIT_NTP_PROMO_IMPRESSIONS,
-    })
     public void syncPromoShowCountResetWhenSignOutSyncingAccount() {
         createSigninManager();
         ChromeSharedPreferences.getInstance()
@@ -707,11 +704,6 @@ public class SigninManagerImplTest {
     }
 
     @Test
-    // TODO(crbug.com/40858677): Disabling the feature explicitly, because native is not available
-    // to
-    // provide a default value. This should be enabled if the feature is enabled by default or
-    // removed if the flag is removed.
-    @DisableFeatures(ChromeFeatureList.SYNC_ANDROID_LIMIT_NTP_PROMO_IMPRESSIONS)
     public void callbackNotifiedOnSignout() {
         createSigninManager();
         doAnswer(
@@ -816,7 +808,6 @@ public class SigninManagerImplTest {
     }
 
     @Test
-    @DisableFeatures(ChromeFeatureList.SYNC_ANDROID_LIMIT_NTP_PROMO_IMPRESSIONS)
     public void signInStateObserverCallOnSignOut() {
         createSigninManager();
         when(mIdentityManagerNativeMock.getPrimaryAccountInfo(
