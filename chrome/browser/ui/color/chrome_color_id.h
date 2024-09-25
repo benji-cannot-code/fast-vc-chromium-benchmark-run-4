4bcfcc23b78431a9a57e99c57d5b5a4e4716cb10
Nihar Majmudar <niharm@google.com>
2024-09-25 18:33:40 +0000

    [lensoverlay] Change preselection toast theming.
    Switches color tokens for preselection toast to spec.
    
    Change-Id: Id39e8191550c2a5dc3f103f5e5379735cc9d23d2
    Bug: 368657407
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5886698
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Reviewed-by: Thomas Lukaszewicz <tluk@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360064}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -180,7 +180,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorHoverButtonBackgroundHovered) \
   /* Lens overlay colors. */ \
   E_CPONLY(kColorLensOverlayToastBackground) \
-  E_CPONLY(kColorLensOverlayToastButtonText) \
+  E_CPONLY(kColorLensOverlayToastButtonBorder) \
+  E_CPONLY(kColorLensOverlayToastForeground) \
   /* Location bar colors. */ \
   E_CPONLY(kColorLocationBarBackground) \
   E_CPONLY(kColorLocationBarBackgroundHovered) \
