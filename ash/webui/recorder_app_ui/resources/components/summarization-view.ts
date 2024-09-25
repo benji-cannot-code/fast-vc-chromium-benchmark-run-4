9511773b6125c1251247c8232ed3138b74e65fb2
hsuanling <hsuanling@google.com>
2024-09-25 07:44:50 +0000

    recorder: Style summary as unordered list
    Use standard <li> bullets instead of hyphen for proper indentation.
    
    and <ul>
    
    Bug: b:367738441
    Test: manually - Run summary and see margin and padding of div#summary
    Change-Id: I274b2e628f936a075c5fbc57cace784a695aedd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875425
    Reviewed-by: Shik Chen <shik@chromium.org>
    Commit-Queue: Jennifer Ling <hsuanling@google.com>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359789}

--- ash/webui/recorder_app_ui/resources/components/summarization-view.ts
+++ ash/webui/recorder_app_ui/resources/components/summarization-view.ts
@@ -19,6 +19,7 @@ import {
   css,
   CSSResultGroup,
   html,
+  map,
   nothing,
   PropertyDeclarations,
   ref,
@@ -126,8 +127,9 @@ export class SummarizationView extends ReactiveLitElement {
 
     #summary {
       font: var(--cros-body-1-font);
-      padding: 12px 16px;
-      white-space: pre-wrap;
+      list-style-position: outside;
+      margin: 12px 12px 12px 22px;
+      padding: 0 0 0 10px;
     }
 
     #footer {
@@ -254,6 +256,14 @@ export class SummarizationView extends ReactiveLitElement {
     `;
   }
 
+  private renderSummaryResult(result: string) {
+    const sentences = result.split('\n');
+    return map(sentences, (sentence) => {
+      // Remove the leading hyphen and space from the sentence, if any.
+      return html`<li>${sentence.replace(/^-\s+/, '')}</li>`;
+    });
+  }
+
   private renderSummaryContent() {
     const summary = this.summary.value;
     if (summary === null) {
@@ -276,15 +286,15 @@ export class SummarizationView extends ReactiveLitElement {
           >
           </genai-error>`;
       case 'success':
-        // Don't add space around ${summary.result}
-        // prettier-ignore
         return html`<spoken-message role="status" aria-live="polite">
             ${i18n.summaryFinishedStatusMessage}
           </spoken-message>
-          <div
+          <ul
             id="summary"
             ${ref(this.summaryContainer)}
-          >${summary.result}</div>
+          >
+            ${this.renderSummaryResult(summary.result)}
+          </ul>
           ${this.renderSummaryFooter()}`;
       default:
         assertExhaustive(summary);
