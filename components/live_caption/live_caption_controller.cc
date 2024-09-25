4055612c4593ab5b8af58e687944cb00b401678e
Yoichi Osato <yoichio@chromium.org>
2024-09-25 02:18:21 +0000

    Revert "[BabelOrca] Implement User Microphone Captioning"
    This reverts commit 86d3a561f66efb6c7c16cb1959b0fb696b934bcd.
    
    Reason for revert: Culprit of the bot error:
    https://ci.chromium.org/ui/p/chrome/builders/ci/chromeos-betty-chrome-dchecks/548/overview
    
    Original change's description:
    > [BabelOrca] Implement User Microphone Captioning
    >
    > This CL implements User Microphone Captioning for Babel Orca. This
    > completes the implementation for go/babel-orca-mic-integration.
    >
    > This CL completes the process of hooking up microphone input to
    > a modified SystemLiveCaptionService in order to facilitate
    > transcribing speech in audio picked up by the device's microphone.
    >
    > Since the microphone captions are triggered on a slightly different
    > set of preconditions to the traditional SystemLiveCaptionService we
    > modify the SpeechRecognitionClientBrowserInterface to notify a
    > separate set of client observers that observe events for BabelOrca.
    >
    > TESTED: CQ, SystemLiveCaptionTests.*, Manually verified behavior on DUT
    > AX-Relnotes: N/A
    > Design: go/babel-orca-mic-integration
    > Bug: b/365820466
    > Change-Id: I89fe396e9baae1ce98a0a92275fd76ffcb0e7df2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818342
    > Reviewed-by: Evan Liu <evliu@google.com>
    > Reviewed-by: Rob Schonberger <robsc@chromium.org>
    > Reviewed-by: Alex Gough <ajgo@chromium.org>
    > Reviewed-by: Scott Violet <sky@chromium.org>
    > Commit-Queue: Avynn Donaghe <avynn@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1359525}
    
    Bug: b/365820466
    Change-Id: Iee42955dc52b5af3ce23da3843c58180b16dd25b
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889168
    Reviewed-by: Yoichi Osato <yoichio@chromium.org>
    Owners-Override: Yoichi Osato <yoichio@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Yoichi Osato <yoichio@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1359710}

--- components/live_caption/live_caption_controller.cc
+++ components/live_caption/live_caption_controller.cc
@@ -144,7 +144,8 @@ void LiveCaptionController::OnLiveCaptionEnabledChanged() {
 
 void LiveCaptionController::OnLiveCaptionLanguageChanged() {
   if (enabled_) {
-    const auto language_code = GetLanguageCode();
+    const auto language_code =
+        prefs::GetLiveCaptionLanguageCode(profile_prefs_);
     auto* soda_installer = speech::SodaInstaller::GetInstance();
     // Only trigger an install when the language is not already installed.
     if (!soda_installer->IsSodaInstalled(
@@ -155,12 +156,7 @@ void LiveCaptionController::OnLiveCaptionLanguageChanged() {
 }
 
 bool LiveCaptionController::IsLiveCaptionEnabled() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  return enabled_via_babel_orca_ ||
-         profile_prefs_->GetBoolean(prefs::kLiveCaptionEnabled);
-#else
   return profile_prefs_->GetBoolean(prefs::kLiveCaptionEnabled);
-#endif
 }
 
 void LiveCaptionController::StartLiveCaption() {
@@ -170,7 +166,8 @@ void LiveCaptionController::StartLiveCaption() {
   // SODAInstaller calls OnSodaInstalled on its observers. The UI is created at
   // that time.
   if (speech::SodaInstaller::GetInstance()->IsSodaInstalled(
-          speech::GetLanguageCode(GetLanguageCode()))) {
+          speech::GetLanguageCode(
+              prefs::GetLiveCaptionLanguageCode(profile_prefs_)))) {
     CreateUI();
   } else {
     speech::SodaInstaller::GetInstance()->AddObserver(this);
@@ -186,22 +183,9 @@ void LiveCaptionController::StopLiveCaption() {
 
 void LiveCaptionController::OnSodaInstalled(
     speech::LanguageCode language_code) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  // Return early IFF the language code is neither for live caption nor
-  // microphone caption and we're waiting on babel orca.
-  if (!prefs::IsLanguageCodeForLiveCaption(language_code, profile_prefs_)
-          ? !enabled_via_babel_orca_ ||
-                !prefs::IsLanguageCodeForMicrophoneCaption(language_code,
-                                                           profile_prefs_)
-          : false) {
-    return;
-  }
-#else
   if (!prefs::IsLanguageCodeForLiveCaption(language_code, profile_prefs_)) {
     return;
   }
-#endif
-
   // Live Caption should always be enabled when this is called. If Live Caption
   // has been disabled, then this should not be observing the SodaInstaller
   // anymore.
@@ -266,16 +250,6 @@ void LiveCaptionController::DestroyUI() {
   }
 }
 
-std::string LiveCaptionController::GetLanguageCode() {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
-  if (enabled_via_babel_orca_) {
-    return prefs::GetUserMicrophoneCaptionLanguage(profile_prefs_);
-  }
-#endif
-
-  return prefs::GetLiveCaptionLanguageCode(profile_prefs_);
-}
-
 bool LiveCaptionController::DispatchTranscription(
     CaptionBubbleContext* caption_bubble_context,
     const media::SpeechRecognitionResult& result) {
@@ -330,11 +304,6 @@ void LiveCaptionController::OnToggleFullscreen(
   CreateUI();
 }
 #endif
-
-void LiveCaptionController::ToggleLiveCaptionForBabelOrca(bool enabled) {
-  enabled_via_babel_orca_ = enabled;
-  OnLiveCaptionEnabledChanged();
-}
 
 void LiveCaptionController::OnCaptionStyleUpdated() {
   // Metrics are recorded when passing the caption prefs to the browser, so do
