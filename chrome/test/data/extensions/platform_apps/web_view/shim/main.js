02009c7196a238abdbfa3172fb588d92f400d3c1
Kevin McNee <mcnee@chromium.org>
2024-09-25 19:31:20 +0000

    Add test that guest view cannot lock keyboard
    Keyboard lock does not support this scenario (see here [1]), but I
    couldn't find any guest view tests for this. The new test confirms that
    calling navigator.keyboard.lock() within a webview will reject.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:content/browser/web_contents/web_contents_impl.cc;drc=f522344e45882da4c7f7cb1b3a0a7bd747d654bb;l=4586
    
    Bug: 368519836
    Change-Id: Ied03202f0f5d6b1a16d362b5b81b3e5a20ee88e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891253
    Auto-Submit: Kevin McNee <mcnee@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360097}

--- chrome/test/data/extensions/platform_apps/web_view/shim/main.js
+++ chrome/test/data/extensions/platform_apps/web_view/shim/main.js
@@ -3890,6 +3890,27 @@ function testBluetoothDisabled() {
   document.body.appendChild(webview);
 }
 
+function testCannotLockKeyboard() {
+  const webview = document.createElement('webview');
+  webview.src = embedder.emptyGuestURL;
+
+  webview.addEventListener('loadstop', async () => {
+    const lockKeyboard = () => {
+      return navigator.keyboard.lock();
+    };
+
+    try {
+      await evalInWebView(webview, lockKeyboard, []);
+      // The attempt to lock the keyboard should fail.
+      embedder.test.fail();
+    } catch {
+      embedder.test.succeed();
+    }
+  });
+
+  document.body.appendChild(webview);
+}
+
 embedder.test.testList = {
   'testAllowTransparencyAttribute': testAllowTransparencyAttribute,
   'testAutosizeHeight': testAutosizeHeight,
@@ -4038,6 +4059,7 @@ embedder.test.testList = {
   'testCannotRequestFonts': testCannotRequestFonts,
   'testSerialDisabled': testSerialDisabled,
   'testBluetoothDisabled': testBluetoothDisabled,
+  'testCannotLockKeyboard': testCannotLockKeyboard,
 };
 
 onload = function() {
