e0d757bf033780d7f35e1cb8f429c924847511df
Mei Liang <meiliang@chromium.org>
2024-09-25 18:59:05 +0000

    [Compare] Add flag for showing the added to set confirmation toast
    Bug: 369238920
    Change-Id: Id6d6501cf12da41c0a1d96a485f344c224813b73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884740
    Auto-Submit: Mei Liang <meiliang@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360078}

--- components/commerce/core/flag_descriptions.h
+++ components/commerce/core/flag_descriptions.h
@@ -28,6 +28,9 @@ extern const char kProductSpecificationsDescription[];
 extern const char kProductSpecificationsMultiSpecificsName[];
 extern const char kProductSpecificationsMultiSpecificsDescription[];
 
+extern const char kCompareConfirmationToastName[];
+extern const char kCompareConfirmationToastDescription[];
+
 extern const char kShoppingIconColorVariantName[];
 extern const char kShoppingIconColorVariantDescription[];
 
