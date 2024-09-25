7f9e748b47761d93c91f431e11f18850c0c56405
Andrew Rayskiy <greengrape@google.com>
2024-09-25 09:38:07 +0000

    iwa: Extract IWA pieces from WebAppInternalsHandler
    Bug: 369087817
    Change-Id: Iba5a6bcd2f477d19408cffe2763c0b664c0ca905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881918
    Reviewed-by: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Auto-Submit: Andrew Rayskiy <greengrape@google.com>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1359823}

--- chrome/browser/web_applications/isolated_web_apps/key_distribution/iwa_key_distribution_info_provider.h
+++ chrome/browser/web_applications/isolated_web_apps/key_distribution/iwa_key_distribution_info_provider.h
@@ -22,13 +22,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/version.h"
 #include "chrome/browser/web_applications/isolated_web_apps/key_distribution/proto/key_distribution.pb.h"
 
-class WebAppInternalsHandler;
-
 namespace base {
 class FilePath;
 }  // namespace base
 
 namespace web_app {
+
+class IwaInternalsHandler;
 
 // Enables the key distribution dev mode UI on chrome://web-app-internals.
 BASE_DECLARE_FEATURE(kIwaKeyDistributionDevMode);
@@ -92,7 +92,7 @@ class IwaKeyDistributionInfoProvider {
   // triggers an `OnComponentUpdateSuccess()` event. The usage of this function
   // is intentionally limited to chrome://web-app-internals.
   void RotateKeyForDevMode(
-      base::PassKey<WebAppInternalsHandler>,
+      base::PassKey<IwaInternalsHandler>,
       const std::string& web_bundle_id,
       const std::optional<std::vector<uint8_t>>& rotated_key);
 
