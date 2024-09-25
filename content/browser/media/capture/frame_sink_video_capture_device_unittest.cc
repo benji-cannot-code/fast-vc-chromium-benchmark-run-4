a7754b4b9ea0635a5e89cfd3d4ace3e6127dc43f
mark a. foltz <mfoltz@chromium.org>
2024-09-25 18:39:41 +0000

    [LaCrOS] Remove LaCrOS screen capturer.
    No longer used.
    
    Bug: 365902693
    Fixed: 354938786
    
    Change-Id: Ic25a60d43fdd7e008bfcd63b4f6c00b5d59b2114
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889025
    Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360065}

--- content/browser/media/capture/frame_sink_video_capture_device_unittest.cc
+++ content/browser/media/capture/frame_sink_video_capture_device_unittest.cc
@@ -33,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/native_widget_types.h"
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/lacros/lacros_test_helper.h"
-#endif
-
 using testing::_;
 using testing::ByRef;
 using testing::Eq;
@@ -445,12 +441,6 @@ class FrameSinkVideoCaptureDeviceTest : public testing::Test {
  protected:
   // See the threading notes at top of this file.
   BrowserTaskEnvironment task_environment_;
-
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-  // Instantiate LacrosService for WakeLock support.
-  chromeos::ScopedLacrosServiceTestHelper scoped_lacros_service_test_helper_;
-#endif
-
   NiceMock<MockFrameSinkVideoCapturer> capturer_;
   std::unique_ptr<FrameSinkVideoCaptureDevice> device_;
 };
