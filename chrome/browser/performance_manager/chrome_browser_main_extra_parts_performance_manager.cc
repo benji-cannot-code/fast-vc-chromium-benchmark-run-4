9c2ba420e7490b49a1f7d9ead70cb1ab9e1cbba5
Joe Mason <joenotcharles@google.com>
2024-09-25 18:00:48 +0000

    Add LoadingScenarioObserver
    Observes OnLoadingStateChanged of all pages and sets the global loading
    scenario as needed.
    
    Bug: 365586676
    Change-Id: I70c856668d06da138dca6e42320fcd8d67176295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5867498
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360038}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/features.h"
 #include "components/performance_manager/public/graph/graph.h"
 #include "components/performance_manager/public/metrics/page_resource_monitor.h"
+#include "components/performance_manager/public/scenarios/loading_scenario_observer.h"
 #include "components/performance_manager/public/scenarios/performance_scenarios.h"
 #include "components/performance_manager/public/user_tuning/tab_revisit_tracker.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -114,6 +115,8 @@ ChromeBrowserMainExtraPartsPerformanceManager::GetInstance() {
 // static
 void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
     performance_manager::Graph* graph) {
+  graph->PassToGraph(
+      std::make_unique<performance_manager::LoadingScenarioObserver>());
   graph->PassToGraph(
       std::make_unique<performance_manager::ProcessMetricsDecorator>());
   graph->PassToGraph(
