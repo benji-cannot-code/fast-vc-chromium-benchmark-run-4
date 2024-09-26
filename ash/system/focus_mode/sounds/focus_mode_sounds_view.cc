c35924cafb38de51f17ef1013c6170ae35a9f14c
Sean Kau <skau@chromium.org>
2024-09-26 18:07:47 +0000

    Change the callback for DownloadPlaylists to include Playlists
    Pass the Playlists through the callback instead of storing them on the
    FocusModeSoundsController. This allows for the elimination of the Playlist caches and clarifies how the FocusModeSoundsView is populated.
    
    Bug: None
    Change-Id: I70bf2ce6a45cd25ad085e14ff0eb94e32679d317
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889447
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360660}

--- ash/system/focus_mode/sounds/focus_mode_sounds_view.cc
+++ ash/system/focus_mode/sounds/focus_mode_sounds_view.cc
@@ -275,14 +275,14 @@ void FocusModeSoundsView::OnPlayerError() {
       ErrorMessageToast::ButtonActionType::kDismiss);
 }
 
-void FocusModeSoundsView::UpdateSoundsView(bool is_soundscape_type) {
-  auto* sounds_controller =
-      FocusModeController::Get()->focus_mode_sounds_controller();
+void FocusModeSoundsView::UpdateSoundsView(
+    bool is_soundscape_type,
+    const std::vector<std::unique_ptr<FocusModeSoundsController::Playlist>>&
+        playlists) {
   if (is_soundscape_type) {
     if (!soundscape_container_) {
       return;
     }
-    const auto& playlists = sounds_controller->soundscape_playlists();
     if (playlists.size() != kFocusModePlaylistViewsNum) {
       ShowErrorMessageForType(
           is_soundscape_type,
@@ -295,7 +295,6 @@ void FocusModeSoundsView::UpdateSoundsView(bool is_soundscape_type) {
     if (!youtube_music_container_) {
       return;
     }
-    const auto& playlists = sounds_controller->youtube_music_playlists();
     if (playlists.size() != kFocusModePlaylistViewsNum) {
       if (!youtube_music_container_->IsAlternateViewVisible()) {
         ShowErrorMessageForType(
