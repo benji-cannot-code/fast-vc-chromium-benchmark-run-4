f1154e55de3c2768f4df1bb9dc5cea960d8509c3
Tim Sergeant <tsergeant@chromium.org>
2024-09-26 07:04:22 +0000

    Mall: Allow overriding Mall URL
    This will be useful for debugging/testing, to allow team members to
    test staging versions of the site within the SWA context.
    
    Bug: b:368160044
    Change-Id: I39e68b5a574b7efa331ed41ce3eb4b293c87dc7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872327
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Tim Sergeant <tsergeant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360393}

--- chrome/browser/ash/mall/mall_url.cc
+++ chrome/browser/ash/mall/mall_url.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/mall/mall_url.h"
 
+#include "ash/webui/mall/url_constants.h"
 #include "base/base64.h"
 #include "base/feature_list.h"
 #include "base/strings/strcat.h"
@@ -25,8 +26,8 @@ GURL GetMallLaunchUrl(const apps::DeviceInfo& info) {
 
   constexpr std::string_view kContextParameter = "context";
 
-  return net::AppendOrReplaceQueryParameter(GURL(chromeos::kAppMallBaseUrl),
-                                            kContextParameter, encoded_context);
+  return net::AppendOrReplaceQueryParameter(GetMallBaseUrl(), kContextParameter,
+                                            encoded_context);
 }
 
 }  // namespace ash
