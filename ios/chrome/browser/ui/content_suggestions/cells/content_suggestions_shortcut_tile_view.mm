7e09807ff6ff1a23ffd8f6a6b8e4f9fd3ba5e7ed
Daniel White <danieltwhite@google.com>
2024-09-26 18:19:38 +0000

    [iOS] Deprecate API in ContentSuggestionsShortcutTileView
    Since didTraitCollectionChange is deprecated in iOS 17,
    ContentSuggestionsShortcutTileView was updated to use the new
    alternative system, registerForTraitChanges.
    
    Fixed: 349391713
    Change-Id: Idd2063c4fc3223e21c1162929122255593523b13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868539
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1360664}

--- ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_shortcut_tile_view.mm
+++ ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_shortcut_tile_view.mm
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_cells_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_action_item.h"
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
@@ -39,6 +40,9 @@ const CGFloat kCountBorderWidth = 24;
     ]];
 
     self.imageBackgroundView.tintColor = [UIColor colorNamed:kBlueHaloColor];
+    if (@available(iOS 17, *)) {
+      [self registerViewForTraitChanges];
+    }
   }
   return self;
 }
@@ -52,6 +56,10 @@ const CGFloat kCountBorderWidth = 24;
     _iconView.contentMode = UIViewContentModeCenter;
 
     [self updateConfiguration:config];
+
+    if (@available(iOS 17, *)) {
+      [self registerViewForTraitChanges];
+    }
   }
   return self;
 }
@@ -130,13 +138,19 @@ const CGFloat kCountBorderWidth = 24;
 
 #pragma mark - UIView
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
   if (previousTraitCollection.preferredContentSizeCategory !=
       self.traitCollection.preferredContentSizeCategory) {
-    self.titleLabel.font = [self titleLabelFont];
+    [self updateTitleLabelFontOnTraitChange];
   }
 }
+#endif
 
 #pragma mark - Private
 
@@ -145,6 +159,21 @@ const CGFloat kCountBorderWidth = 24;
       UIFontTextStyleCaption2,
       self.traitCollection.preferredContentSizeCategory,
       UIContentSizeCategoryAccessibilityLarge);
+}
+
+// Registers a list of UITraits to observe and invokes the
+// `updateTitleLabelFontOnTraitChange` function whenever one of the observed
+// trait's values change.
+- (void)registerViewForTraitChanges API_AVAILABLE(ios(17.0)) {
+  NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+      @[ UITraitPreferredContentSizeCategory.self ]);
+  [self registerForTraitChanges:traits
+                     withAction:@selector(updateTitleLabelFontOnTraitChange)];
+}
+
+// Update the `titleLabel` font when the device's content size changes.
+- (void)updateTitleLabelFontOnTraitChange {
+  self.titleLabel.font = [self titleLabelFont];
 }
 
 @end
