e768f4ec8834a0db6f41f548eeec46f9a55c512a
Michelle Chen <michellegc@google.com>
2024-09-26 04:52:23 +0000

    scanner: Add ScannerText struct and basic text region painting.
    ScannerText data will be fetched and parsed by the Scanner backend. This
    text data can then be shown on an overlay to indicate detected text,
    translations, etc.
    
    For now, just add support for painting basic detected text regions, to
    demonstrate roughly how ScannerText will be used. Styling and support
    for showing translated text will be added in followups.
    
    Bug: b:363101913, b:367548979
    Change-Id: I5c2f514e31c5e65cb316488c24c6b0bc390f4aff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872167
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360358}

---
+++ ash/scanner/scanner_text.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/scanner/scanner_text.h"
+
+namespace ash {
+
+ScannerText::Line::Line() = default;
+ScannerText::Line::Line(ScannerText::Line&& other) = default;
+ScannerText::Line& ScannerText::Line::operator=(ScannerText::Line&& other) =
+    default;
+ScannerText::Line::~Line() = default;
+
+ScannerText::Paragraph::Paragraph() = default;
+ScannerText::Paragraph::Paragraph(ScannerText::Paragraph&& other) = default;
+ScannerText::Paragraph& ScannerText::Paragraph::operator=(
+    ScannerText::Paragraph&& other) = default;
+ScannerText::Paragraph::~Paragraph() = default;
+
+ScannerText::ScannerText() = default;
+ScannerText::ScannerText(ScannerText&& other) = default;
+ScannerText& ScannerText::operator=(ScannerText&& other) = default;
+ScannerText::~ScannerText() = default;
+
+}  // namespace ash
