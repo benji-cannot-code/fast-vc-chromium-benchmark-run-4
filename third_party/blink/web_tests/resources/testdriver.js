977e2653ca343aeba707699d07962e328633065e
Jonathan Lee <jonathanjlee@google.com>
2024-09-26 22:15:45 +0000

    Roll wpt javascript
    This rolls wpt javascript to latest commit at
    https://github.com/web-platform-tests/wpt.
    
    Change-Id: Iaf059dadffaf5a458a2d0d11176eb4067231e347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893910
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Reviewed-by: An Sung <ansung@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360806}

--- third_party/blink/web_tests/resources/testdriver.js
+++ third_party/blink/web_tests/resources/testdriver.js
@@ -177,11 +177,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         },
 
         /**
-         * Triggers a user-initiated click
+         * Triggers a user-initiated mouse click.
          *
-         * If ``element`` isn't inside the
-         * viewport, it will be scrolled into view before the click
-         * occurs.
+         * If ``element`` isn't inside the viewport, it will be
+         * scrolled into view before the click occurs.
          *
          * If ``element`` is from a different browsing context, the
          * command will be run in that context.
