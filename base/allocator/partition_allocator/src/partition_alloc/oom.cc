4f7aeb7f43b3423f823ec22a1127d42d06c6dffe
Marshall Greenblatt <marshall@chromium.org>
2024-09-26 18:57:15 +0000

    win: Add missing <limits> include
    Fixes the following error when building on Windows with
    `use_custom_libcxx=false`:
    
    oom.cc(52,36): error: no member named 'numeric_limits' in namespace 'std'
       52 |   constexpr size_t kInvalid = std::numeric_limits<ULONG_PTR>::max();
          |                               ~~~~~^
    
    Change-Id: I78f1c3fcadac649dcd4f32d0620ba4c6a06bac88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891459
    Commit-Queue: Marshall Greenblatt <marshall@chromium.org>
    Reviewed-by: Francois Pierre Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360693}

--- base/allocator/partition_allocator/src/partition_alloc/oom.cc
+++ base/allocator/partition_allocator/src/partition_alloc/oom.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <array>
 #include <cstdlib>
+#include <limits>
 #endif  // PA_BUILDFLAG(IS_WIN)
 
 namespace partition_alloc {
