672c40e289397a18649dd384af3b0c8f29273ca8
Duong Dac <ddac@google.com>
2024-09-26 18:23:18 +0000

    [iOS] Remove prefetch system capabilities feature flags
    Minor mode restrictions experiment on iOS has been launched
    (crrev.com/c/5860077).
    
    The relevant flags kPrefetchSystemCapabilitiesOnFirstRun and
    kPrefetchSystemCapabilitiesOnAppStartup are enabled by default and
    will be removed.
    
    Bug: b/318349361
    Change-Id: Ic035c5e7abd7aacbc2fdf1007dbcca4912937414
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891555
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360670}

--- ios/chrome/app/first_run_app_state_agent.mm
+++ ios/chrome/app/first_run_app_state_agent.mm
@@ -167,12 +167,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // TODO(crbug.com/343699504): Remove pre-fetching capabilities once these are
   // loaded in iSL.
-  if (IsPrefetchingSystemCapabilitiesOnFirstRun()) {
-    RunSystemCapabilitiesPrefetch(
-        ChromeAccountManagerServiceFactory::GetForBrowserState(
-            self.mainBrowser->GetBrowserState())
-            ->GetAllIdentities());
-  }
+  RunSystemCapabilitiesPrefetch(
+      ChromeAccountManagerServiceFactory::GetForBrowserState(
+          self.mainBrowser->GetBrowserState())
+          ->GetAllIdentities());
 
   FirstRunScreenProvider* provider = [[FirstRunScreenProvider alloc]
       initForBrowserState:self.mainBrowser->GetBrowserState()];
