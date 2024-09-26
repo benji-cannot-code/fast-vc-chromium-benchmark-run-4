867bc9a49180ba3088197e9413c996dd63b3a1cf
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2024-09-26 08:51:03 +0000

    Convert WTF::TextCodecCJK to safer constructs.
    Let `TextCodec::GetUnencodableReplacement()` return `String` instead of
    taking the fixed-size array and returning the fulfilled length.
    
    Bug: 351564777
    Change-Id: Idd4d27c7e32f53abc6359b08759001b2953b247d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892153
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360420}

--- third_party/blink/renderer/platform/wtf/text/text_codec_cjk.cc
+++ third_party/blink/renderer/platform/wtf/text/text_codec_cjk.cc
@@ -24,11 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/wtf/text/text_codec_cjk.h"
 
 #include <utility>
@@ -79,12 +74,11 @@ constexpr std::array<const char*, 6> kSupportedCanonicalNames{
 void AppendUnencodableReplacement(UChar32 code_point,
                                   UnencodableHandling handling,
                                   Vector<uint8_t>& result) {
-  UnencodableReplacementArray replacement;
-  uint32_t replacement_len =
-      TextCodec::GetUnencodableReplacement(code_point, handling, replacement);
-  result.reserve(result.size() + replacement_len);
-  for (uint32_t i = 0; i < replacement_len; ++i) {
-    result.UncheckedAppend(replacement[i]);
+  String replacement =
+      TextCodec::GetUnencodableReplacement(code_point, handling);
+  result.reserve(result.size() + replacement.length());
+  for (uint8_t r : replacement) {
+    result.UncheckedAppend(r);
   }
 }
 
