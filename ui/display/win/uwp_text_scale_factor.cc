e5a7a0b8cc887e75bfa6518ed9710dd56c9198bc
Keren Zhu <kerenzhu@chromium.org>
2024-09-26 22:56:46 +0000

    base: use ObserverList::Notify() in ui/display/win
    This CL was uploaded by git cl split.
    
    R=robliao@chromium.org
    
    Bug: 40727208
    Change-Id: I641a8d0cae61d76b61c6a749be885478cfd1416c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891265
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360826}

--- ui/display/win/uwp_text_scale_factor.cc
+++ ui/display/win/uwp_text_scale_factor.cc
@@ -174,9 +174,7 @@ class UwpTextScaleFactorImpl : public UwpTextScaleFactor {
 UwpTextScaleFactor::UwpTextScaleFactor() = default;
 
 UwpTextScaleFactor::~UwpTextScaleFactor() {
-  for (auto& observer : observer_list_) {
-    observer.OnUwpTextScaleFactorCleanup(this);
-  }
+  observer_list_.Notify(&Observer::OnUwpTextScaleFactorCleanup, this);
 }
 
 float UwpTextScaleFactor::GetTextScaleFactor() const {
@@ -192,9 +190,7 @@ void UwpTextScaleFactor::RemoveObserver(Observer* observer) {
 }
 
 void UwpTextScaleFactor::NotifyUwpTextScaleFactorChanged() {
-  for (auto& observer : observer_list_) {
-    observer.OnUwpTextScaleFactorChanged();
-  }
+  observer_list_.Notify(&Observer::OnUwpTextScaleFactorChanged);
 }
 
 void UwpTextScaleFactor::SetImplementationForTesting(
