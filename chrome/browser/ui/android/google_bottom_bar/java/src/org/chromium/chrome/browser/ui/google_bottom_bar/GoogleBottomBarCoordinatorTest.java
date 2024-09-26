7c444731b9902590d4f715a87594281f99f16597
Andrew Grieve <agrieve@chromium.org>
2024-09-26 18:52:53 +0000

    Android: Add missing @Test annotations
    Found via ErrorProne check JUnit4TestNotRun
    
    Bug: 40661145
    Change-Id: Iafaed91957fd6589f709f784c16409d008081992
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890331
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360689}

--- chrome/browser/ui/android/google_bottom_bar/java/src/org/chromium/chrome/browser/ui/google_bottom_bar/GoogleBottomBarCoordinatorTest.java
+++ chrome/browser/ui/android/google_bottom_bar/java/src/org/chromium/chrome/browser/ui/google_bottom_bar/GoogleBottomBarCoordinatorTest.java
@@ -291,6 +291,7 @@ public class GoogleBottomBarCoordinatorTest {
                 CUSTOM_BUTTON_PARAM_ID_TO_BUTTON_ID_MAP.keySet(), supportedGoogleBottomBarButtons);
     }
 
+    @Test
     @EnableFeatures(ChromeFeatureList.CCT_GOOGLE_BOTTOM_BAR_VARIANT_LAYOUTS)
     public void
             getSupportedCustomButtonParamIds_variantLayoutsEnabled_singleDecker_returnsEmptySet() {
@@ -306,6 +307,7 @@ public class GoogleBottomBarCoordinatorTest {
         assertEquals(Set.of(), supportedGoogleBottomBarButtons);
     }
 
+    @Test
     @EnableFeatures(ChromeFeatureList.CCT_GOOGLE_BOTTOM_BAR_VARIANT_LAYOUTS)
     public void
             getSupportedCustomButtonParamIds_variantLayoutsEnabledButNotSupported_singleDecker_returnsSetWithAllSupportedCustomParams() {
