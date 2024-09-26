e3fe78427a569071994689d0f7cd7a8d351cbbb1
Min Qin <qinmin@chromium.org>
2024-09-26 04:33:09 +0000

    Show a toast when policy blocked context menu download option is clicked
    This CL will make download option clickable, when the context menu
    option is disabled. It will show a toast "Blocked by your organization"
    when clicked.
    
    Bug: 368481336
    Change-Id: I9a87f1bdd15165fa1639814d0d664caa3c048a13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889356
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360354}

--- chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/contextmenu/ContextMenuTest.java
@@ -57,6 +57,7 @@ import org.chromium.base.test.util.RequiresRestart;
 import org.chromium.chrome.browser.compositor.layouts.LayoutManagerImpl;
 import org.chromium.chrome.browser.download.DownloadTestRule;
 import org.chromium.chrome.browser.download.DownloadTestRule.CustomMainActivityStart;
+import org.chromium.chrome.browser.download.DownloadUtils;
 import org.chromium.chrome.browser.ephemeraltab.EphemeralTabCoordinator;
 import org.chromium.chrome.browser.firstrun.FirstRunStatus;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
@@ -188,6 +189,7 @@ public class ContextMenuTest {
         Tab tab = sDownloadTestRule.getActivity().getActivityTab();
         CriteriaHelper.pollUiThread(() -> tab.isUserInteractable() && !tab.isLoading());
         setupLensChipDelegate();
+        DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(false);
     }
 
     @After
@@ -200,6 +202,7 @@ public class ContextMenuTest {
                         mMenuCoordinator = null;
                     }
                 });
+        DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(null);
     }
 
     @Test
@@ -572,6 +575,28 @@ public class ContextMenuTest {
     @LargeTest
     public void testSaveVideo() throws TimeoutException, SecurityException, IOException {
         saveMediaFromContextMenu("videoDOMElement", R.id.contextmenu_save_video, FILENAME_WEBM);
+    }
+
+    @Test
+    @MediumTest
+    public void testSaveImageBlockedByPolicy()
+            throws TimeoutException, SecurityException, IOException {
+        DownloadUtils.setIsDownloadRestrictedByPolicyForTesting(true);
+        int downloadCount = sDownloadTestRule.getAllDownloads().size();
+        Tab tab = sDownloadTestRule.getActivity().getActivityTab();
+        mMenuCoordinator = ContextMenuUtils.openContextMenu(tab, "testImage");
+
+        // Click should not trigger any download
+        InstrumentationRegistry.getInstrumentation()
+                .runOnMainSync(
+                        () ->
+                                mMenuCoordinator.clickListItemForTesting(
+                                        R.id.contextmenu_save_image));
+        int newCount = sDownloadTestRule.getAllDownloads().size();
+        Assert.assertEquals(downloadCount, newCount);
+
+        // The context menu should still show.
+        Assert.assertTrue(mMenuCoordinator.getDialogForTest().isShowing());
     }
 
     /**
