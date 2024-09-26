0962f713d72594a465a2e44f32a10c6d94d048b9
Calder Kitagawa <ckitagawa@chromium.org>
2024-09-26 22:55:14 +0000

    Move constants to TabStripViewBinder
    For some reason these constants were in another file.
    
    Change-Id: I4ebb7f9ded6d6132546516f8a2c568819b700d92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894284
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360823}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabStripViewBinder.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabStripViewBinder.java
@@ -5,9 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
-import static org.chromium.chrome.browser.tasks.tab_management.TabGroupColorFaviconProvider.FAVICON_BACKGROUND_DEFAULT_ALPHA;
-import static org.chromium.chrome.browser.tasks.tab_management.TabGroupColorFaviconProvider.FAVICON_BACKGROUND_SELECTED_ALPHA;
-
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
@@ -31,6 +28,9 @@ import org.chromium.ui.widget.ViewLookupCachingFrameLayout;
 
 /** {@link org.chromium.ui.modelutil.SimpleRecyclerViewMcp.ViewBinder} for tab strip. */
 class TabStripViewBinder {
+    private static final int FAVICON_BACKGROUND_DEFAULT_ALPHA = 255;
+    private static final int FAVICON_BACKGROUND_SELECTED_ALPHA = 0;
+
     /**
      * Partially or fully update the given ViewHolder based on the given model over propertyKey.
      * @param model The model to use.
