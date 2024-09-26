8a0991d91ae7540636063454b12b9e3e3769172c
Joe Downing <joedow@google.com>
2024-09-26 21:42:56 +0000

    Update std::optional usage to be consistent in //remoting
    I figured I may as well just fix our usages to be consistent everywhere
    so here is the CL.
    
    Change-Id: I1006db9a0c4c7c73221254d48b9cbf9ab369aebf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889278
    Auto-Submit: Joe Downing <joedow@chromium.org>
    Reviewed-by: Yuwei Huang <yuweih@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360763}

--- remoting/host/chromeos/file_session_storage.cc
+++ remoting/host/chromeos/file_session_storage.cc
@@ -97,7 +97,7 @@ FileSessionStorage::FileSessionStorage(const base::FilePath& storage_directory)
 
 void FileSessionStorage::StoreSession(const base::Value::Dict& information,
                                       base::OnceClosure on_done) {
-  WriteFileAsync(session_file(), base::WriteJson(information).value(),
+  WriteFileAsync(session_file(), *base::WriteJson(information),
                  base::BindOnce([](bool success) {
                    LOG_IF(ERROR, !success)
                        << "Failed to create CRD session information file";
@@ -121,8 +121,7 @@ void FileSessionStorage::RetrieveSession(
                     return make_nullopt<base::Value::Dict>();
                   }
 
-                  auto dict_optional =
-                      base::JSONReader::ReadDict(content.value());
+                  auto dict_optional = base::JSONReader::ReadDict(*content);
                   LOG_IF(ERROR, !dict_optional.has_value())
                       << "Failed to parse stored CRD session information";
                   return dict_optional;
