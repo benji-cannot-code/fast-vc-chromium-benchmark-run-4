bab72a2c5f8ebe76f88b35b05407b4d1289565e4
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-09-26 14:13:59 +0000

    Remove most legacy Sync.ModelType* histograms
    The new Sync.DataType* ones reached stable in M129. This CL removes
    most of the legacy ones, with 2 exceptions: Sync.ModelTypeCount4 and
    Sync.ModelTypeEntityChange3, which still have chirp alerts set on them.
    
    OBSOLETE_HISTOGRAM[Sync.InvalidationPerModelType]=Replaced with Sync.InvalidationPerDataType
    OBSOLETE_HISTOGRAM[Sync.ModelTypeClearedOnce]=Replaced with Sync.DataTypeClearedOnce
    OBSOLETE_HISTOGRAM[Sync.ModelTypeCommitMessageHasDepletedQuota]=Replaced with Sync.DataTypeCommitMessageHasDepletedQuota
    OBSOLETE_HISTOGRAM[Sync.ModelTypeCommitWithDepletedQuota]=Replaced with Sync.DataTypeCommitWithDepletedQuota
    OBSOLETE_HISTOGRAM[Sync.ModelTypeMemoryKB]=Replaced with Sync.DataTypeMemoryKB
    OBSOLETE_HISTOGRAM[Sync.ModelTypeStoreBackendError]=Replaced with Sync.DataTypeStoreBackendError
    OBSOLETE_HISTOGRAM[Sync.ModelTypeStoreBackendInitializationSuccess]=Replaced with Sync.DataTypeStoreBackendInitializatio
    
    Bug: 358120886
    Change-Id: I083badf302f20cf0f9899b7bcd35c1568ef8cfbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891182
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Auto-Submit: Victor Vianna <victorvianna@google.com>
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360511}

--- components/sync/engine/cycle/data_type_tracker.cc
+++ components/sync/engine/cycle/data_type_tracker.cc
@@ -229,10 +229,6 @@ void DataTypeTracker::RecordSuccessfulCommitMessage() {
       base::UmaHistogramEnumeration(
           "Sync.DataTypeCommitMessageHasDepletedQuota",
           DataTypeHistogramValue(type_));
-      // Legacy equivalent, before the metric was renamed.
-      base::UmaHistogramEnumeration(
-          "Sync.ModelTypeCommitMessageHasDepletedQuota",
-          DataTypeHistogramValue(type_));
     }
   }
 }
@@ -385,9 +381,6 @@ base::TimeDelta DataTypeTracker::GetLocalChangeNudgeDelay(
     bool is_single_client) const {
   if (quota_ && !quota_->HasTokensAvailable()) {
     base::UmaHistogramEnumeration("Sync.DataTypeCommitWithDepletedQuota",
-                                  DataTypeHistogramValue(type_));
-    // Legacy equivalent, before the metric was renamed.
-    base::UmaHistogramEnumeration("Sync.ModelTypeCommitWithDepletedQuota",
                                   DataTypeHistogramValue(type_));
     return depleted_quota_nudge_delay_;
   }
