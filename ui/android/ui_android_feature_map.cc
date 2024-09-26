a8b37b09d70bd4d2b624c6342e04196a18b6160c
Min Qin <qinmin@chromium.org>
2024-09-26 22:00:24 +0000

    Check caller permission before sending file selection results
    If the caller doesn't have permissions for the Uri it gaves to
    Chrome, Chrome shouldn't resolves the Uris and pass them bac
    as the file selection results.
    
    Bug: 281539662
    Change-Id: I5dc7ec819ff73699de216cb05fc8ef8dae3d73ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884295
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360792}

--- ui/android/ui_android_feature_map.cc
+++ ui/android/ui_android_feature_map.cc
@@ -25,6 +25,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &ui::kReportAllAvailablePointerTypes,
     &ui::kRequireLeadingInTextViewWithLeading,
     &ui::kSelectFileOpenDocument,
+    &ui::kCheckIntentCallerPermission,
 };
 
 // static
