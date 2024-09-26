74049602509115d4b5bcfa664c8a6697b9eb5c81
Mohamed Heikal <mheikal@chromium.org>
2024-09-26 18:55:38 +0000

    Finish migrating to ServiceLoaderUtil for readaloud
    Renames the upstream implementation to be more clear of its function.
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-binary-size
    Bug: 40901855
    Change-Id: I9c0d9679efe4bac94a493469d0b9cef46083f2b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893610
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360692}

--- chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooksUpstreamImpl.java
+++ chrome/browser/readaloud/android/java/src/org/chromium/chrome/browser/readaloud/ReadAloudReadabilityHooksUpstreamImpl.java
@@ -13,8 +13,8 @@ import org.chromium.chrome.browser.tab.Tab;
 import java.util.HashSet;
 
 /** Empty implementation of ReadAloudReadabilityHooks. */
-public class ReadAloudReadabilityHooksImpl implements ReadAloudReadabilityHooks {
-    public ReadAloudReadabilityHooksImpl(Context context, Profile profile) {}
+public class ReadAloudReadabilityHooksUpstreamImpl implements ReadAloudReadabilityHooks {
+    public ReadAloudReadabilityHooksUpstreamImpl(Context context, Profile profile) {}
 
     @Override
     public boolean isEnabled() {
