a8a29de0372c57ce1b3f9c73bfa51ab576a9f308
Benjamin Williams <bwwilliams@google.com>
2024-09-26 16:48:19 +0000

    Remove redundant base::Time forward declarations
    Improve code hygiene by removing redundant forward declarations of
    base::Time in chrome/browser/ash/child_accounts. Since base/time/time.h or base/timer/timer.h
    are included, the full definition is available, making forward
    declarations unnecessary.
    
    This CL was uploaded by git cl split.
    
    R=courtneywong@chromium.org
    
    Bug: 369814311
    Change-Id: I19f6e9ab97b2c6d323a799124980c302189329da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891485
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Commit-Queue: Courtney Wong <courtneywong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360592}

--- chrome/browser/ash/child_accounts/family_user_metrics_service.h
+++ chrome/browser/ash/child_accounts/family_user_metrics_service.h
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class PrefRegistrySimple;
 class PrefService;
 
-namespace base {
-class Time;
-}  // namespace base
-
 namespace content {
 class BrowserContext;
 }  // namespace content
