18899b08b9fb18a93cb127eefee823fb747ad4e6
Chih-Yu Huang <akahuang@google.com>
2024-09-26 05:46:07 +0000

    CrOS: remove the NetworkPortalDetectorImpl class
    The kRemoveDetectPortalFromChrome flag is enabled by default for more
    than one milestone. It's time to clean up the portal detection.
    
    This CL removes the NetworkPortalDetectorImpl class and its related
    tests.
    
    BUG=b:336931625
    TEST=build unit_tests
    TEST=vpython3 tools/traffic_annotation/scripts/auditor/auditor.py
    
    Change-Id: Ic98de83b602b60dde5283bd3925806202b90b545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5850491
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Chih-Yu Huang <akahuang@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360376}

--- chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
+++ chrome/browser/ash/main_parts/chrome_browser_main_parts_ash.cc
@@ -125,7 +125,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/net/apn_migrator.h"
 #include "chrome/browser/ash/net/bluetooth_pref_state_observer.h"
 #include "chrome/browser/ash/net/network_health/network_health_manager.h"
-#include "chrome/browser/ash/net/network_portal_detector_impl.h"
 #include "chrome/browser/ash/net/network_pref_state_observer.h"
 #include "chrome/browser/ash/net/network_throttling_observer.h"
 #include "chrome/browser/ash/net/rollback_network_config/rollback_network_config_service.h"
@@ -309,15 +308,8 @@ void InitializeNetworkPortalDetector() {
   if (network_portal_detector::SetForTesting()) {
     return;
   }
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          ::switches::kTestType) ||
-      features::IsRemoveDetectPortalFromChromeEnabled()) {
-    network_portal_detector::SetNetworkPortalDetector(
-        new NetworkPortalDetectorStub());
-  } else {
-    network_portal_detector::SetNetworkPortalDetector(
-        new NetworkPortalDetectorImpl());
-  }
+  network_portal_detector::SetNetworkPortalDetector(
+      new NetworkPortalDetectorStub());
   network_portal_detector::GetInstance()->Enable();
 }
 
