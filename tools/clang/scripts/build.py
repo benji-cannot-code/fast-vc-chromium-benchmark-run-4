798c94970de6f4c33127579da92f0287e1f10058
danakj <danakj@chromium.org>
2024-09-26 20:13:57 +0000

    Remove clang-tools-extra from the clang build when passed --no-tools
    This will speed up the Rust toolchain build by avoiding building
    clang-tidy and other tools not needed by the Rust toolchain build
    process.
    
    The help comment for the flag even mentioned that these are omitted
    but they were not.
    
    Bug: 40212971
    Change-Id: Iea91de77f63aa8c37c4fbac84896bd15cc8fa5e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889633
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360747}

--- tools/clang/scripts/build.py
+++ tools/clang/scripts/build.py
@@ -774,7 +774,9 @@ def main():
   ldflags = []
 
   targets = 'AArch64;ARM;LoongArch;Mips;PowerPC;RISCV;SystemZ;WebAssembly;X86'
-  projects = 'clang;lld;clang-tools-extra'
+  projects = 'clang;lld'
+  if not args.no_tools:
+    projects += ';clang-tools-extra'
   if args.bolt:
     projects += ';bolt'
 
