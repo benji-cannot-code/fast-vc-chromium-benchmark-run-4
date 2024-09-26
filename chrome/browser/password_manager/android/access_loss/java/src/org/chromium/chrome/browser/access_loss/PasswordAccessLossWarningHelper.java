78c40302d906d53b949e0116e8d484b175d6d286
Ivana Žužić <izuzic@google.com>
2024-09-26 16:23:44 +0000

    [UPMAccessLossWarning] Log user action
    The user action metric tracks if the user dismissed the sheet, opened
    the help center article or clicked the button.
    
    Bug: 367592142
    Change-Id: Ie0b6c5084e8dc72ffc1e0b643c03da9fed3b4966
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5883374
    Reviewed-by: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360572}

--- chrome/browser/password_manager/android/access_loss/java/src/org/chromium/chrome/browser/access_loss/PasswordAccessLossWarningHelper.java
+++ chrome/browser/password_manager/android/access_loss/java/src/org/chromium/chrome/browser/access_loss/PasswordAccessLossWarningHelper.java
@@ -17,18 +17,27 @@ import android.app.Activity;
 import android.text.SpannableString;
 import android.view.View;
 
+import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.Callback;
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.bottom_sheet.SimpleNoticeSheetCoordinator;
 import org.chromium.chrome.browser.password_manager.CustomTabIntentHelper;
 import org.chromium.chrome.browser.password_manager.GmsUpdateLauncher;
 import org.chromium.chrome.browser.password_manager.PasswordExportLauncher;
 import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.StateChangeReason;
+import org.chromium.components.browser_ui.bottomsheet.BottomSheetObserver;
+import org.chromium.components.browser_ui.bottomsheet.EmptyBottomSheetObserver;
 import org.chromium.ui.modelutil.PropertyModel;
 import org.chromium.ui.text.NoUnderlineClickableSpan;
 import org.chromium.ui.text.SpanApplier;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 /** Entry-point to the access loss warning UI surface. */
 public class PasswordAccessLossWarningHelper {
@@ -36,6 +45,26 @@ public class PasswordAccessLossWarningHelper {
     final BottomSheetController mBottomSheetController;
     final Profile mProfile;
     final HelpUrlLauncher mHelpUrlLauncher;
+
+    // These values are persisted to logs. Entries should not be renumbered and
+    // numeric values should never be reused.
+    @IntDef({
+        PasswordAccessLossWarningUserAction.MAIN_ACTION,
+        PasswordAccessLossWarningUserAction.HELP_CENTER,
+        PasswordAccessLossWarningUserAction.DISMISS,
+        PasswordAccessLossWarningUserAction.COUNT
+    })
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface PasswordAccessLossWarningUserAction {
+        int MAIN_ACTION = 0;
+        int HELP_CENTER = 1;
+        int DISMISS = 2;
+        int COUNT = 3;
+    }
+
+    public static final String PASSWORD_ACCESS_LOSS_WARNING_USER_ACTION_PREFIX =
+            "PasswordManager.PasswordAccessLossWarningSheet.";
+    public static final String PASSWORD_ACCESS_LOSS_WARNING_USER_ACTION_SUFFIX = ".UserAction";
 
     public PasswordAccessLossWarningHelper(
             Activity activity,
@@ -49,6 +78,8 @@ public class PasswordAccessLossWarningHelper {
     }
 
     public void show(@PasswordAccessLossWarningType int warningType) {
+        mBottomSheetController.addObserver(
+                createBottomSheetObserver(mBottomSheetController, warningType));
         SimpleNoticeSheetCoordinator coordinator =
                 new SimpleNoticeSheetCoordinator(mActivity, mBottomSheetController);
         PropertyModel model = getModelForWarningType(warningType);
@@ -67,13 +98,12 @@ public class PasswordAccessLossWarningHelper {
             case PasswordAccessLossWarningType.NO_UPM:
                 // Fallthrough, same as ONLY_ACCOUNT_UPM.
             case PasswordAccessLossWarningType.ONLY_ACCOUNT_UPM:
-                return buildAccessLossWarningAboutGmsUpdate();
+                return buildAccessLossWarningAboutGmsUpdate(warningType);
             case PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED:
                 return buildAccessLossWarningAboutManualMigration();
-            default:
-                assert false : "Unhandled warning type.";
-                return null;
         }
+        assert false : "Unhandled warning type: " + warningType;
+        return null;
     }
 
     /** GMS Core doesn't exist on the device so the user is asked to export their passwords. */
@@ -87,10 +117,13 @@ public class PasswordAccessLossWarningHelper {
                         getBottomSheetTextWithLink(
                                 mActivity.getString(
                                         R.string.pwd_access_loss_warning_no_gms_core_text),
-                                (unusedView) ->
-                                        mHelpUrlLauncher.showHelpArticle(
-                                                mActivity,
-                                                GOOGLE_PLAY_SUPPORTED_DEVICES_SUPPORT_URL)))
+                                (unusedView) -> {
+                                    mHelpUrlLauncher.showHelpArticle(
+                                            mActivity, GOOGLE_PLAY_SUPPORTED_DEVICES_SUPPORT_URL);
+                                    logAccessLossWarningSheetUserAction(
+                                            PasswordAccessLossWarningType.NO_GMS_CORE,
+                                            PasswordAccessLossWarningUserAction.HELP_CENTER);
+                                }))
                 .with(
                         BUTTON_TITLE,
                         mActivity.getString(
@@ -99,12 +132,21 @@ public class PasswordAccessLossWarningHelper {
                         BUTTON_ACTION,
                         () -> {
                             PasswordExportLauncher.showMainSettingsAndStartExport(mActivity);
+                            logAccessLossWarningSheetUserAction(
+                                    PasswordAccessLossWarningType.NO_GMS_CORE,
+                                    PasswordAccessLossWarningUserAction.MAIN_ACTION);
                         })
                 .build();
     }
 
-    /** GMS Core on the device doesn't support UPM so the user is asked to update GMS Core. */
-    PropertyModel buildAccessLossWarningAboutGmsUpdate() {
+    /**
+     * GMS Core on the device doesn't support UPM so the user is asked to update GMS Core.
+     *
+     * @param warningType that is used to track user action metrics.
+     * @return the property model for the warning sheet.
+     */
+    PropertyModel buildAccessLossWarningAboutGmsUpdate(
+            @PasswordAccessLossWarningType int warningType) {
         return new PropertyModel.Builder(ALL_KEYS)
                 .with(
                         SHEET_TITLE,
@@ -114,10 +156,14 @@ public class PasswordAccessLossWarningHelper {
                         getBottomSheetTextWithLink(
                                 mActivity.getString(
                                         R.string.pwd_access_loss_warning_update_gms_core_text),
-                                (unusedView) ->
-                                        mHelpUrlLauncher.showHelpArticle(
-                                                mActivity,
-                                                KEEP_APPS_AND_DEVICES_WORKING_WITH_GMS_CORE_SUPPORT_URL)))
+                                (unusedView) -> {
+                                    mHelpUrlLauncher.showHelpArticle(
+                                            mActivity,
+                                            KEEP_APPS_AND_DEVICES_WORKING_WITH_GMS_CORE_SUPPORT_URL);
+                                    logAccessLossWarningSheetUserAction(
+                                            warningType,
+                                            PasswordAccessLossWarningUserAction.HELP_CENTER);
+                                }))
                 .with(
                         BUTTON_TITLE,
                         mActivity.getString(
@@ -126,6 +172,8 @@ public class PasswordAccessLossWarningHelper {
                         BUTTON_ACTION,
                         () -> {
                             GmsUpdateLauncher.launch(mActivity);
+                            logAccessLossWarningSheetUserAction(
+                                    warningType, PasswordAccessLossWarningUserAction.MAIN_ACTION);
                         })
                 .build();
     }
@@ -153,6 +201,9 @@ public class PasswordAccessLossWarningHelper {
                         BUTTON_ACTION,
                         () -> {
                             PasswordExportLauncher.showMainSettingsAndStartExport(mActivity);
+                            logAccessLossWarningSheetUserAction(
+                                    PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED,
+                                    PasswordAccessLossWarningUserAction.MAIN_ACTION);
                         })
                 .build();
     }
@@ -162,5 +213,55 @@ public class PasswordAccessLossWarningHelper {
                 sheetText,
                 new SpanApplier.SpanInfo(
                         "<link>", "</link>", new NoUnderlineClickableSpan(mActivity, callback)));
+    }
+
+    private static void logAccessLossWarningSheetUserAction(
+            @PasswordAccessLossWarningType int warningType,
+            @PasswordAccessLossWarningUserAction int action) {
+        RecordHistogram.recordEnumeratedHistogram(
+                getUserActionHistogramName(warningType),
+                action,
+                PasswordAccessLossWarningUserAction.COUNT);
+    }
+
+    @VisibleForTesting
+    static String getUserActionHistogramName(@PasswordAccessLossWarningType int warningType) {
+        return PASSWORD_ACCESS_LOSS_WARNING_USER_ACTION_PREFIX
+                + getAccessLossWarningTypeName(warningType)
+                + PASSWORD_ACCESS_LOSS_WARNING_USER_ACTION_SUFFIX;
+    }
+
+    private static String getAccessLossWarningTypeName(
+            @PasswordAccessLossWarningType int warningType) {
+        switch (warningType) {
+            case PasswordAccessLossWarningType.NO_GMS_CORE:
+                return "NoGmsCore";
+            case PasswordAccessLossWarningType.NO_UPM:
+                return "NoUPM";
+            case PasswordAccessLossWarningType.ONLY_ACCOUNT_UPM:
+                return "OnlyAccountUpm";
+            case PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED:
+                return "NewGmsCoreMigrationFailed";
+        }
+        assert false : "Unhandled warning type: " + warningType;
+        return null;
+    }
+
+    private BottomSheetObserver createBottomSheetObserver(
+            BottomSheetController bottomSheetController,
+            @PasswordAccessLossWarningType int warningType) {
+        return new EmptyBottomSheetObserver() {
+            @Override
+            public void onSheetClosed(@StateChangeReason int reason) {
+                if (reason == StateChangeReason.SWIPE
+                        || reason == StateChangeReason.BACK_PRESS
+                        || reason == StateChangeReason.TAP_SCRIM
+                        || reason == StateChangeReason.OMNIBOX_FOCUS) {
+                    logAccessLossWarningSheetUserAction(
+                            warningType, PasswordAccessLossWarningUserAction.DISMISS);
+                }
+                bottomSheetController.removeObserver(this);
+            }
+        };
     }
 }
