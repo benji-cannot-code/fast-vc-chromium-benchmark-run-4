b3c1bb9176cdeb0091b5a7863bbd94cf450c6da1
David Bienvenu <davidbienvenu@chromium.org>
2024-09-26 23:11:42 +0000

    codehealth: Remove ExtensionsZipFileInstalledInProfileDir experiment
    Bug: 356624445
    Change-Id: I18238e42c1c3be3feb2a8302bb6eb14e41fa190f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894130
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bienvenu <davidbienvenu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360842}

--- chrome/browser/extensions/extension_garbage_collector.cc
+++ chrome/browser/extensions/extension_garbage_collector.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extension_util.h"
 #include "extensions/common/extension.h"
-#include "extensions/common/extension_features.h"
 #include "extensions/common/extension_id.h"
 #include "extensions/common/file_util.h"
 
@@ -232,10 +231,6 @@ void ExtensionGarbageCollector::GarbageCollectExtensions() {
     NOTREACHED_IN_MIGRATION();
   }
 
-  if (!base::FeatureList::IsEnabled(
-          extensions_features::kExtensionsZipFileInstalledInProfileDir)) {
-    return;
-  }
   if (!GetExtensionFileTaskRunner()->PostTask(
           FROM_HERE, base::BindOnce(&GarbageCollectExtensionsOnFileThread,
                                     service->unpacked_install_directory(),
