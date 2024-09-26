94e1aa1a8519821e790c7fad9810afec47e0d0ed
Amanda Lin Dietz <aldietz@google.com>
2024-09-26 18:02:58 +0000

    [FaceGaze] Localize strings for actions/macros
    * Add action/macro strings to localization
    * Update UI to use localized strings
    
    Screenshots:
    https://screenshot.googleplex.com/33dMDD6LpwjQbQ3
    https://screenshot.googleplex.com/5CAgoRQLgiBJfWs
    
    AX-Relnotes: N/A
    Bug: 322510917
    Change-Id: I0be3dadf1a0d1e5f87b67b82db321e7d159495e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5888685
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360656}

--- chrome/browser/resources/ash/settings/os_a11y_page/facegaze_actions_add_dialog.ts
+++ chrome/browser/resources/ash/settings/os_a11y_page/facegaze_actions_add_dialog.ts
@@ -337,9 +337,9 @@ export class FaceGazeAddActionDialogElement extends
   private getLocalizedSelectGestureTitle_(): string {
     return this.i18n(
         'faceGazeActionsDialogSelectGestureTitle',
-        this.selectedAction_ ?
-            FaceGazeUtils.getMacroDisplayText(this.selectedAction_) :
-            '');
+        this.selectedAction_ ? this.i18n(FaceGazeUtils.getMacroDisplayTextName(
+                                   this.selectedAction_)) :
+                               '');
   }
 
   private getLocalizedGestureThresholdTitle_(): string {
@@ -364,7 +364,7 @@ export class FaceGazeAddActionDialogElement extends
   }
 
   private getActionDisplayText_(action: MacroName): string {
-    return FaceGazeUtils.getMacroDisplayText(action);
+    return this.i18n(FaceGazeUtils.getMacroDisplayTextName(action));
   }
 
   private getGestureDisplayText_(gesture: FacialGesture|null): string {
