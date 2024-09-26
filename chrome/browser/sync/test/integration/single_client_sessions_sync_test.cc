fe588622ca3c3d148e5b4e5022b8c40f6f03ac71
Marc Treib <treib@chromium.org>
2024-09-26 10:33:12 +0000

    Update local session tag after stopping and restarting Sessions sync
    The local session tag (aka cache GUID) is passed to the SessionStore
    (and from there, on to the SyncedSessionTracker) on store creation.
    
    Before this CL, the store itself got created exactly once, and never
    destroyed. That meant that the local session tag never got updated -
    however, it *is* possible for the cache GUID to change, e.g. if the
    user signs out and signs back in with a different account, or if
    Sync is reset from the dashboard.
    
    This CL destroys the SessionStore when sync gets disabled
    (ApplyDisableSyncChanges), and re-creates it on demand, passing in the
    new cache GUID / session tag. One wrinkle is that the creation of the
    underlying DataTypeStore is asynchronous, which means it cannot be
    done again after the bridge has called ModelReadyToSync(). To work
    around this, the bridge keeps the underlying store around (bound into
    a callback), to allow synchronous re-creation of the SessionStore.
    
    Bug: 361256057
    Change-Id: I37c3f377e85a004477b5bd86e1d6e017927e3b0b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882176
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360449}

--- chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
+++ chrome/browser/sync/test/integration/single_client_sessions_sync_test.cc
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/sync/protocol/session_specifics.pb.h"
 #include "components/sync/protocol/sync.pb.h"
 #include "components/sync/protocol/sync_entity.pb.h"
+#include "components/sync/service/glue/sync_transport_data_prefs.h"
 #include "components/sync/test/sessions_hierarchy.h"
 #include "components/sync_sessions/session_store.h"
 #include "components/sync_sessions/session_sync_service.h"
@@ -53,6 +54,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/test/embedded_test_server/embedded_test_server.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -70,10 +72,17 @@ using sessions_helper::NavigateTabForward;
 using sessions_helper::OpenTab;
 using sessions_helper::OpenTabAtIndex;
 using sessions_helper::ScopedWindowMap;
+using sessions_helper::SessionEntitiesChecker;
 using sessions_helper::SyncedSessionVector;
 using sessions_helper::WaitForTabsToLoad;
 using sessions_helper::WindowsMatch;
 using sync_sessions::SessionSyncTestHelper;
+using testing::_;
+using testing::UnorderedElementsAre;
+
+MATCHER_P(SessionHeader, session_tag, "") {
+  return arg.has_header() && arg.session_tag() == session_tag;
+}
 
 static const char* kBaseFragmentURL =
     "data:text/html,<html><title>Fragment</title><body></body></html>";
@@ -227,8 +236,7 @@ class SingleClientSessionsSyncTest : public SyncTest {
   }
 
   // Block until the expected hierarchy is recorded on the FakeServer for
-  // profile 0. This will time out if the hierarchy is never
-  // recorded.
+  // profile 0. This will time out if the hierarchy is never recorded.
   void WaitForHierarchyOnServer(
       const fake_server::SessionsHierarchy& hierarchy) {
     SessionHierarchyMatchChecker checker(hierarchy, GetSyncService(0),
@@ -375,6 +383,49 @@ IN_PROC_BROWSER_TEST_F(SingleClientSessionsSyncTest, SessionStartTime) {
   }
   EXPECT_TRUE(found_header);
 }
+
+#if !BUILDFLAG(IS_CHROMEOS_ASH)
+// Regression test for crbug.com/361256057.
+IN_PROC_BROWSER_TEST_F(SingleClientSessionsSyncTest, UpdateSessionTag) {
+  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
+
+  ASSERT_TRUE(CheckInitialState(0));
+
+  std::string first_cache_guid;
+  {
+    syncer::SyncTransportDataPrefs prefs(
+        GetProfile(0)->GetPrefs(),
+        GetClient(0)->GetGaiaIdHashForPrimaryAccount());
+    first_cache_guid = prefs.GetCacheGuid();
+  }
+
+  // Note: The only tab that exists is an NTP, which is not interesting and thus
+  // not uploaded. So the only SESSIONS entity on the server is now a header.
+  ASSERT_TRUE(SessionEntitiesChecker(
+                  UnorderedElementsAre(SessionHeader(first_cache_guid)))
+                  .Wait());
+
+  // Disable Sync, then turn it on again with a different account.
+  GetClient(0)->SignOutPrimaryAccount();
+  GetClient(0)->SetUsernameForFutureSignins("account2@gmail.com");
+  ASSERT_TRUE(GetClient(0)->SetupSync());
+
+  std::string second_cache_guid;
+  {
+    syncer::SyncTransportDataPrefs prefs(
+        GetProfile(0)->GetPrefs(),
+        GetClient(0)->GetGaiaIdHashForPrimaryAccount());
+    second_cache_guid = prefs.GetCacheGuid();
+  }
+  ASSERT_NE(first_cache_guid, second_cache_guid);
+
+  // Ensure that a session header with the new cache GUID was uploaded.
+  EXPECT_TRUE(SessionEntitiesChecker(
+                  UnorderedElementsAre(SessionHeader(first_cache_guid),
+                                       SessionHeader(second_cache_guid)))
+                  .Wait());
+}
+#endif  // !BUILDFLAG(IS_CHROMEOS_ASH)
 
 IN_PROC_BROWSER_TEST_F(SingleClientSessionsSyncTest, NavigateInTab) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
