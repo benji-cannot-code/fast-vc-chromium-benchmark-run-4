4bc062723ca13f76e5dd5d9b83a170a934158c9b
John Lee <johntlee@chromium.org>
2024-09-26 23:58:17 +0000

    Add new component for images for history embedding results
    - Allow history_embeddings to use Lit
    - Create new cr-history-embeddings-result-image element that takes in
    a search result which will be used to display an image for the result
    - Show an illustration as a placeholder for now
    
    http://screen/9r8azNVUA9JYT5b
    http://screen/5AFC9JPEgC6jytu
    
    Bug: 369210842
    Change-Id: I0d9c7ccf5eb667a0f1ae9dc9dad935c1d45cf838
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892657
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360867}

---
+++ chrome/test/data/webui/cr_components/history_embeddings/result_image_test.ts
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'chrome://resources/cr_components/history_embeddings/result_image.js';
+
+import type {HistoryEmbeddingsResultImageElement} from 'chrome://resources/cr_components/history_embeddings/result_image.js';
+import {assertTrue} from 'chrome://webui-test/chai_assert.js';
+
+suite('cr-history-embeddings-result-image', () => {
+  let element: HistoryEmbeddingsResultImageElement;
+
+  setup(() => {
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
+    element = document.createElement('cr-history-embeddings-result-image');
+    document.body.appendChild(element);
+  });
+
+  test('ShowsSvgByDefault', () => {
+    const svg = element.shadowRoot!.querySelector('svg');
+    assertTrue(!!svg);
+  });
+});
