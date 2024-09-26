63239ea4a71377d63353eede19dba3a060615df4
Akihiro Ota <akihiroota@chromium.org>
2024-09-26 16:24:43 +0000

    FaceGaze: Ensure top bar UI matches UI spec
    This change ensures that the FaceGaze top bar UI matches the spec
    provided by UI design [1]. Specifically, we increase the icon size,
    text size, and margins so that the UI is more prominent on the screen.
    
    See the screenshot [2] for reference.
    
    [1] https://www.figma.com/design/6sUKOl2TmRmW349YBV3hHv/FaceGaze?node-id=820-13506&t=NkA88RzhCOB1qUIa-4
    
    [2] https://drive.google.com/file/d/1Ea9Neuize9c7dlFUxK-t_RjLcrFWZ21c/view?usp=sharing&resourcekey=0-w02R8wweybIHJKUgtBYfBQ
    
    Bug: 367774352
    Change-Id: I385054bdab6b43f48fad50b2221b2d0c96910e49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889542
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360573}

--- ash/system/accessibility/facegaze_bubble_view.cc
+++ ash/system/accessibility/facegaze_bubble_view.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_enums.mojom.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/mojom/dialog_button.mojom.h"
+#include "ui/base/resource/resource_bundle.h"
 #include "ui/color/color_id.h"
 #include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/controls/image_view.h"
@@ -24,19 +25,25 @@ namespace ash {
 
 namespace {
 
-constexpr int kIconSizeDip = 16;
-constexpr int kSpaceBetweenIconAndTextDip = 4;
+constexpr int kIconSizeDip = 24;
+constexpr int kSpaceBetweenIconAndTextDip = 8;
+
+const ui::ResourceBundle::FontStyle kKeyLabelFontStyle =
+    ui::ResourceBundle::MediumFont;
 
 std::unique_ptr<views::Label> CreateLabelView(
     raw_ptr<views::Label>* destination_view,
     const std::u16string& text,
     ui::ColorId enabled_color_id) {
+  ui::ResourceBundle* rb = &ui::ResourceBundle::GetSharedInstance();
+
   return views::Builder<views::Label>()
       .CopyAddressTo(destination_view)
       .SetText(text)
       .SetEnabledColorId(enabled_color_id)
       .SetHorizontalAlignment(gfx::HorizontalAlignment::ALIGN_CENTER)
       .SetMultiLine(false)
+      .SetFontList(rb->GetFontList(kKeyLabelFontStyle))
       .Build();
 }
 
@@ -74,7 +81,8 @@ void FaceGazeBubbleView::Init() {
       views::BoxLayout::Orientation::kHorizontal);
   layout->set_between_child_spacing(kSpaceBetweenIconAndTextDip);
   SetLayoutManager(std::move(layout));
-  UseCompactMargins();
+  set_margins(
+      gfx::Insets().set_top(8).set_bottom(8).set_left(16).set_right(16));
   AddChildView(CreateImageView(&image_, kFacegazeIcon));
   AddChildView(
       CreateLabelView(&label_, std::u16string(), kColorAshTextColorPrimary));
