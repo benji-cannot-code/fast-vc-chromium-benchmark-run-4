8ea4066a123c6a73f7ceee741c54fe4684a99f45
Greg Thompson <grt@chromium.org>
2024-09-26 15:17:48 +0000

    [mac] Diagnosts for ContentBrowserTest failure
    Add diagnostics for a failure to create the pseudo "Content Shell" file
    in ContentBrowserTest.
    
    Bug: 345765743
    Change-Id: I4f031159a7eee1c147aa441d6dd43a4b7d5c10d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890405
    Commit-Queue: Alexander Timin <altimin@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360547}

--- content/public/test/content_browser_test.cc
+++ content/public/test/content_browser_test.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file.h"
 #include "base/files/file_util.h"
+#include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/task/current_thread.h"
@@ -65,9 +66,13 @@ ContentBrowserTest::ContentBrowserTest() {
   content_shell_path = content_shell_path.Append(
       FILE_PATH_LITERAL("Content Shell.app/Contents/MacOS/Content Shell"));
   CHECK(base::CreateDirectory(content_shell_path.DirName()));
-  CHECK(base::File(content_shell_path,
-                   base::File::FLAG_OPEN_ALWAYS | base::File::FLAG_WRITE)
-            .IsValid());
+  if (base::File file(content_shell_path,
+                      base::File::FLAG_OPEN_ALWAYS | base::File::FLAG_WRITE);
+      !file.IsValid()) {
+    // Diagnostics for https://crbug.com/345765743.
+    NOTREACHED() << "Failed to create \"" << content_shell_path
+                 << "\": " << base::File::ErrorToString(file.error_details());
+  }
   file_exe_override_.emplace(base::FILE_EXE, content_shell_path,
                              /*is_absolute=*/false, /*create=*/false);
 #endif
