77be924e13a83cf2dfa9d79141d0f97048b686c3
Alex Ilin <alexilin@chromium.org>
2024-09-26 10:12:27 +0000

    [DBSC] Split CoveredRequestWasDeferred metric by result
    This CL adds a "Success" and "Failure" histogram suffix split to
    Signin.BoundSessionCredentials.CoveredRequestWasDeferred UMA. Also keep
    unsuffixed variant to avoid disrupting existing metrics.
    
    Signin.BoundSessionCredentials.DeferredRequestUnblockTrigger* histogram
    is currently split into "Success" and "Failure" suffixes.
    Signin.BoundSessionCredentials.CoveredRequestWasDeferred could serve to
    compute the total number of covered/deferred requests but we don't know
    how many of them succeed or fail.
    
    Bug: 324424704
    Change-Id: I8af2a9cfe32d9c7a257990a0a6346e3512fafc84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890647
    Commit-Queue: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360445}

--- chrome/common/google_url_loader_throttle.cc
+++ chrome/common/google_url_loader_throttle.cc
@@ -82,13 +82,35 @@ bool IsCoveredRequestBoundSessionStatus(RequestBoundSessionStatus status) {
 }
 
 void RecordBoundSessionStatusMetrics(bool was_deferred,
-                                     bool is_main_frame_navigation) {
+                                     bool is_main_frame_navigation,
+                                     bool is_request_succeeded) {
   UMA_HISTOGRAM_BOOLEAN(
       "Signin.BoundSessionCredentials.CoveredRequestWasDeferred", was_deferred);
+  if (is_request_succeeded) {
+    UMA_HISTOGRAM_BOOLEAN(
+        "Signin.BoundSessionCredentials.CoveredRequestWasDeferred.Success",
+        was_deferred);
+  } else {
+    UMA_HISTOGRAM_BOOLEAN(
+        "Signin.BoundSessionCredentials.CoveredRequestWasDeferred.Failure",
+        was_deferred);
+  }
+
   if (is_main_frame_navigation) {
     UMA_HISTOGRAM_BOOLEAN(
         "Signin.BoundSessionCredentials.CoveredNavigationRequestWasDeferred",
         was_deferred);
+    if (is_request_succeeded) {
+      UMA_HISTOGRAM_BOOLEAN(
+          "Signin.BoundSessionCredentials.CoveredNavigationRequestWasDeferred."
+          "Success",
+          was_deferred);
+    } else {
+      UMA_HISTOGRAM_BOOLEAN(
+          "Signin.BoundSessionCredentials.CoveredNavigationRequestWasDeferred."
+          "Failure",
+          was_deferred);
+    }
   }
 }
 #endif  // BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
@@ -282,7 +304,8 @@ void GoogleURLLoaderThrottle::WillProcessResponse(
 #if BUILDFLAG(ENABLE_BOUND_SESSION_CREDENTIALS)
   if (is_covered_by_bound_session_) {
     RecordBoundSessionStatusMetrics(is_deferred_for_bound_session_,
-                                    is_main_frame_navigation_);
+                                    is_main_frame_navigation_,
+                                    /*is_request_succeeded=*/true);
   }
   if (deferred_request_resume_trigger_) {
     UMA_HISTOGRAM_ENUMERATION(
@@ -317,7 +340,8 @@ void GoogleURLLoaderThrottle::WillOnCompleteWithError(
     const network::URLLoaderCompletionStatus& status) {
   if (is_covered_by_bound_session_) {
     RecordBoundSessionStatusMetrics(is_deferred_for_bound_session_,
-                                    is_main_frame_navigation_);
+                                    is_main_frame_navigation_,
+                                    /*is_request_succeeded=*/false);
   }
   if (deferred_request_resume_trigger_) {
     UMA_HISTOGRAM_ENUMERATION(
