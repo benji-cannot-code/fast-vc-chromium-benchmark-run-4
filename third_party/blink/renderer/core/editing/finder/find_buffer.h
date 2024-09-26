5aac835e5032f13496d64fefeecdf33c7433fca8
Kent Tamura <tkent@chromium.org>
2024-09-26 00:59:18 +0000

    FindRuby: Make `RubySupport` tri-state
    Change `RubySupport` to an enum with three items. kEnabledForcefully
    is helpful for testing.
    
    Also adds a RubySupport argument to FindBuffer in FindTask::Invoke(),
    which is a part of the find-in-page feature.
    
    This CL has no behavior changes yet.
    
    Bug: 40755728
    Change-Id: I9ad20ec64c106974ef9a6dccce549f8a71291373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889036
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360297}

--- third_party/blink/renderer/core/editing/finder/find_buffer.h
+++ third_party/blink/renderer/core/editing/finder/find_buffer.h
@@ -22,6 +22,15 @@ class Node;
 class OffsetMapping;
 class WebString;
 
+enum class RubySupport {
+  // No support.
+  kDisabled,
+  // Support if a target IFC contains a <ruby>.
+  kEnabledIfNecessary,
+  // Support always.
+  kEnabledForcefully,
+};
+
 // Buffer for find-in-page, collects text until it meets a block/other
 // delimiters. Uses TextSearcherICU to find match in buffer.
 // See doc at https://goo.gl/rnXjBu
@@ -29,10 +38,8 @@ class CORE_EXPORT FindBuffer {
   STACK_ALLOCATED();
 
  public:
-  using RubySupport = base::StrongAlias<class RubySupportTag, bool>;
-
   explicit FindBuffer(const EphemeralRangeInFlatTree& range,
-                      RubySupport ruby_support = RubySupport(false));
+                      RubySupport ruby_support = RubySupport::kDisabled);
 
   static EphemeralRangeInFlatTree FindMatchInRange(
       const EphemeralRangeInFlatTree& range,
