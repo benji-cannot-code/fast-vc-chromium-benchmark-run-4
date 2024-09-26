5fff50c0925b341341ec8274878a3c7d70304bf0
EmiliaPaz <emiliapaz@chromium.org>
2024-09-26 23:06:47 +0000

    [Extensions Cleanup] Remove unused methods in ExtensionActionTestHelper
    Last uses of these methods were removed on crrev.com/c/5892660. We
    can remove them now.
    
    Bug: 40889653
    Change-Id: I24aaf3a3cd7dfd7a1b47c6ecc3726605da86d30a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893917
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Auto-Submit: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360838}

--- chrome/browser/ui/extensions/extension_action_test_helper.h
+++ chrome/browser/ui/extensions/extension_action_test_helper.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/native_widget_types.h"
 
 class Browser;
-class ExtensionsContainer;
 
 namespace gfx {
 class Image;
@@ -72,17 +71,11 @@ class ExtensionActionTestHelper {
   // Hides the given popup and returns whether the hide was successful.
   virtual bool HidePopup() = 0;
 
-  // Returns the associated ExtensionsContainer.
-  virtual ExtensionsContainer* GetExtensionsContainer() = 0;
-
   // Waits for the ExtensionContainer's layout to be done.
   virtual void WaitForExtensionsContainerLayout() = 0;
 
   // Returns the minimum allowed size of an extension popup.
   virtual gfx::Size GetMinPopupSize() = 0;
-
-  // Returns the size of the toolbar actions.
-  virtual gfx::Size GetToolbarActionSize() = 0;
 
   // Returns the maximum allowed size of an extension popup.
   virtual gfx::Size GetMaxPopupSize() = 0;
