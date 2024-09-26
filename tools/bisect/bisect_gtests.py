1ccb50e0ec1338b21205358494a665aca09b96f4
Justin Lulejian <jlulejian@chromium.org>
2024-09-26 17:34:07 +0000

    Update bisect_gtests.py instruction comment.
    Bug: N/A
    Change-Id: Ieff0fd3eb13779e4e4d7b2e639a97260c5a294a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892986
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Sven Zheng <svenzheng@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360633}

--- tools/bisect/bisect_gtests.py
+++ tools/bisect/bisect_gtests.py
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # 8 if the test pass, `git bisect good`, otherwise `git bisect bad`.
 # 9 repeat 5 - 8 until finding the culprit.
 # This script will help you on 2 - 9. You first do 1, then run
-# `python3 tools/bisect/bisect.py -g <good commit id> -b <bad commit id>
+# `python3 tools/bisect/bisect_gtests.py -g <good commit id> -b <bad commit id>
 #   --build_command 'autoninja -C out/Default browser_tests'
 #   --test_command 'out/Default/browser_tests
 #                   --gtest_filter=BrowserTest.TestCase1'`
