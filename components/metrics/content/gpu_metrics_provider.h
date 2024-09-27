54762c03ee3a38536705df38d7cea91771488ae6
kylechar <kylechar@chromium.org>
2024-09-27 16:05:57 +0000

    Add GPU.HardwareSupportsvVulkan
    This CL adds a GPU.HardwareSupportsVulkan histogram and
    GPUInfo::hardware_supports_vulkan. The original bool is set true after
    GPU process successfully checks that system can support running with
    Vulkan. The browser will receive that value in GPUInfo after first
    successful GPU launch and remember the result since subsequent GPU
    process launches could be after GPU mode fallback has occurred and
    Vulkan might not be available anymore.
    
    GPU.HardwareSupportsVulkan is emitted once per UMA ping so it can be
    used as UMA filter.
    
    Bug: 363031527
    Change-Id: Ic79fdfb0a09f319155cba4c6f783b29f6b2b8cf2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5866938
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361183}

--- components/metrics/content/gpu_metrics_provider.h
+++ components/metrics/content/gpu_metrics_provider.h
@@ -23,6 +23,8 @@ class GPUMetricsProvider : public MetricsProvider {
   // MetricsProvider:
   void ProvideSystemProfileMetrics(
       SystemProfileProto* system_profile_proto) override;
+  void ProvideCurrentSessionData(
+      ChromeUserMetricsExtension* uma_proto) override;
 };
 
 }  // namespace metrics
