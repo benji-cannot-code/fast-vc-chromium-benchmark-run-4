25214f154590e016b5a7ff9faf57ad0aad919e01
anthie@google.com <anthie@google.com>
2024-09-27 17:21:16 +0000

    Add checks for getting into pending state in Superivion Mixin
    Bug: b/362420913
    Change-Id: I288d848ba157210765a6d49033ca9693aab9f6dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890979
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Reviewed-by: James Lee <ljjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361242}

--- chrome/test/supervised_user/supervision_mixin.cc
+++ chrome/test/supervised_user/supervision_mixin.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/mixin_based_in_process_browser_test.h"
 #include "components/keyed_service/content/browser_context_dependency_manager.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/account_capabilities_test_mutator.h"
 #include "components/signin/public/identity_manager/account_info.h"
 #include "components/signin/public/identity_manager/accounts_in_cookie_jar_info.h"
@@ -147,6 +148,10 @@ void SupervisionMixin::SetParentalControlsAccountCapability(
 
 void SupervisionMixin::SetPendingStateForPrimaryAccount() {
   CHECK_NE(sign_in_mode_, SignInMode::kSignedOut);
+  // Getting into pending state pre-Uno requires the user to sync.
+  CHECK(consent_level_ == signin::ConsentLevel::kSync ||
+        base::FeatureList::IsEnabled(
+            switches::kExplicitBrowserSigninUIOnDesktop));
 
   auto* identity_manager = GetIdentityTestEnvironment()->identity_manager();
 
