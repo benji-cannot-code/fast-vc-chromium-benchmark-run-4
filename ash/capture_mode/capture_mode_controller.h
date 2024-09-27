769fb9ed79e986a5f41a0ba289a3e57180d74210
Michelle Chen <michellegc@google.com>
2024-09-27 04:48:25 +0000

    scanner: Send captured image from CaptureModeController to Scanner.
    After an image has been captured for search, send the image to the
    Scanner backend if needed.
    
    Followups will handle the Scanner response (e.g. to create action chips).
    
    Bug: b:367866079, b:369470078
    Change-Id: I0e60a4e8b6a9300db0b599ad919e08a577731060
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890204
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1360939}

--- ash/capture_mode/capture_mode_controller.h
+++ ash/capture_mode/capture_mode_controller.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/capture_mode/capture_mode_types.h"
 #include "ash/capture_mode/video_recording_watcher.h"
 #include "ash/public/cpp/capture_mode/capture_mode_delegate.h"
+#include "ash/public/cpp/scanner/scanner_action.h"
 #include "ash/public/cpp/session/session_observer.h"
 #include "base/files/file_path.h"
 #include "base/functional/callback_forward.h"
@@ -481,6 +482,10 @@ class ASH_EXPORT CaptureModeController
   void OnImageCapturedForSearch(
       bool was_cursor_originally_blocked,
       scoped_refptr<base::RefCountedMemory> jpeg_bytes);
+
+  // Called back when the Scanner feature has processed a captured image to
+  // suggest available Scanner actions.
+  void OnScannerActionsFetched(std::vector<ScannerAction> scanner_actions);
 
   // Called back when an attempt to save the image file has been completed, with
   // `file_saved_path` indicating whether the attempt succeeded or failed. If
