395278427cb2e2d66c4c2fae992a5b8e04ea81da
Ari Chivukula <arichiv@chromium.org>
2024-09-27 20:23:00 +0000

    Clean up stale base::Feature "ViewsTaskManager"
    This seems to be the last stub of an already removed feature, it was
    only not causing issues because this file wasn't built for macOS.
    
    Fixed: 356235353
    Change-Id: I19cc505b155cef33b51c7c1fff2d26d0e428c8ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893977
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361346}

--- chrome/browser/ui/views/task_manager_view_browsertest.cc
+++ chrome/browser/ui/views/task_manager_view_browsertest.cc
@@ -64,11 +64,7 @@ using browsertest_util::WaitForTaskManagerRows;
 
 class TaskManagerViewTest : public InProcessBrowserTest {
  public:
-  TaskManagerViewTest() {
-#if BUILDFLAG(IS_MAC)
-    feature_list_.InitAndEnableFeature(features::kViewsTaskManager);
-#endif
-  }
+  TaskManagerViewTest() = default;
 
   TaskManagerViewTest(const TaskManagerViewTest&) = delete;
   TaskManagerViewTest& operator=(const TaskManagerViewTest&) = delete;
