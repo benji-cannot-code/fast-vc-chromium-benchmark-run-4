469fd7870db840d2be79e390e955f210abbc725c
François Doray <fdoray@chromium.org>
2024-09-27 17:20:01 +0000

    Initialize //base features on Webview.
    There are parts of //base that can be configured differently based on
    the FeatureList. For this configuration to work, base::features::Init
    must be invoked when the FeatureList is ready. Note that these parts
    of //base need this initialization step, instead of checking the
    FeatureList directly, because they may be used before or concurrently
    with FeatureList initialization.
    
    This CL adds the call to base::features::Init on Webview (it was
    already present for Chrome and Bling).
    
    Change-Id: Ib0d677e1d4bd763ac4b35fcbe324e6baa561f073
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895754
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Auto-Submit: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361240}

--- android_webview/lib/aw_main_delegate.cc
+++ android_webview/lib/aw_main_delegate.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/cpu.h"
+#include "base/features.h"
 #include "base/functional/bind.h"
 #include "base/i18n/icu_util.h"
 #include "base/i18n/rtl.h"
@@ -331,6 +332,11 @@ std::optional<int> AwMainDelegate::PostEarlyInitialization(
     InitIcuAndResourceBundleBrowserSide();
     aw_feature_list_creator_->CreateFeatureListAndFieldTrials();
     content::InitializeMojoCore();
+
+    // Initialize //base features that depend on the `FeatureList`. Don't force
+    // emitting profiler metadata since the profiler doesn't run on Webview.
+    base::features::Init(base::features::EmitThreadControllerProfilerMetadata::
+                             kFeatureDependent);
 
     // WebView apps can override WebView#computeScroll to achieve custom
     // scroll/fling. As a result, fling animations may not be ticked,
