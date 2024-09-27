2b73a5026e92287972f6c2509730373c0f865387
Federica Germinario <fedegermi@google.com>
2024-09-27 12:45:24 +0000

    [ios] Update files in i/c/b/default_promo/*
    This CL replaces:
    * ChromeBrowserState => ProfileIOS
    * GetForBrowserState => GetForProfile at call sites.
    * GetBrowserState => GetProfile
    * browser_state, browserState => profile
    
    web::BrowserState code is not updated.
    
    Fixed: 365527258
    Change-Id: I56bff4e5f48abf7cb79126fe4f209a708dfa91db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892742
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361082}

--- ios/chrome/browser/default_promo/ui_bundled/default_browser_promo_non_modal_coordinator.mm
+++ ios/chrome/browser/default_promo/ui_bundled/default_browser_promo_non_modal_coordinator.mm
@@ -142,9 +142,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Records that a default browser promo has been shown.
 - (void)recordDefaultBrowserPromoShown {
-  ChromeBrowserState* browserState = self.browser->GetBrowserState();
+  ProfileIOS* profile = self.browser->GetProfile();
   LogToFETDefaultBrowserPromoShown(
-      feature_engagement::TrackerFactory::GetForBrowserState(browserState));
+      feature_engagement::TrackerFactory::GetForProfile(profile));
 }
 
 @end
--- ios/chrome/browser/default_promo/ui_bundled/generic/default_browser_generic_promo_coordinator.mm
+++ ios/chrome/browser/default_promo/ui_bundled/generic/default_browser_generic_promo_coordinator.mm
@@ -53,17 +53,16 @@ using base::UserMetricsAction;
   [super start];
   [self recordVideoDefaultBrowserPromoShown];
 
-  ChromeBrowserState* browserState = self.browser->GetBrowserState();
-  _tracker =
-      feature_engagement::TrackerFactory::GetForBrowserState(browserState);
+  ProfileIOS* profile = self.browser->GetProfile();
+  _tracker = feature_engagement::TrackerFactory::GetForProfile(profile);
 
   if (IsSegmentedDefaultBrowserPromoEnabled()) {
     segmentation_platform::SegmentationPlatformService* segmentationService =
         segmentation_platform::SegmentationPlatformServiceFactory::
-            GetForProfile(browserState);
+            GetForProfile(profile);
     segmentation_platform::DeviceSwitcherResultDispatcher* dispatcher =
         segmentation_platform::SegmentationPlatformServiceFactory::
-            GetDispatcherForProfile(browserState);
+            GetDispatcherForProfile(profile);
 
     _mediator = [[DefaultBrowserGenericPromoMediator alloc]
            initWithSegmentationService:segmentationService
@@ -154,7 +153,7 @@ using base::UserMetricsAction;
         feature_engagement::events::kDefaultBrowserPromoRemindMeLater);
   }
   PromosManager* promosManager =
-      PromosManagerFactory::GetForBrowserState(self.browser->GetBrowserState());
+      PromosManagerFactory::GetForProfile(self.browser->GetProfile());
   promosManager->RegisterPromoForSingleDisplay(
       promos_manager::Promo::DefaultBrowserRemindMeLater);
 
