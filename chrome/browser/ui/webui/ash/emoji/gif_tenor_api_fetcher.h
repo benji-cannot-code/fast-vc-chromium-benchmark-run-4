46d0c95cb5232d5b7e1dc8f04a7810517fa41574
Darren Shen <shend@chromium.org>
2024-09-27 02:13:48 +0000

    Forward declare where possible in GifTenorApiFetcher.
    Bug: b:325123679
    Change-Id: If1649be6c4bf326505f74328de9560f313d13cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890018
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360914}

--- chrome/browser/ui/webui/ash/emoji/gif_tenor_api_fetcher.h
+++ chrome/browser/ui/webui/ash/emoji/gif_tenor_api_fetcher.h
@@ -10,12 +10,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 #include <string_view>
+#include <vector>
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/scoped_refptr.h"
+#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/webui/ash/emoji/emoji_picker.mojom.h"
-#include "components/endpoint_fetcher/endpoint_fetcher.h"
 #include "services/data_decoder/public/cpp/data_decoder.h"
-#include "url/gurl.h"
+
+struct EndpointResponse;
+class EndpointFetcher;
+class GURL;
+
+namespace net {
+struct NetworkTrafficAnnotationTag;
+}
 
 namespace network {
 class SharedURLLoaderFactory;
