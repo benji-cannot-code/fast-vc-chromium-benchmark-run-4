6a2fdf6e86c4d3c3581d732afbc9a03370094935
Justin Cohen <justincohen@google.com>
2024-09-27 16:11:32 +0000

    ios: Change order_file.c to cc
    Change-Id: Ib3b590d16a4fa46921a6da6f7f58785e7d27c1c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895761
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Owners-Override: Alexis Hétu <sugoi@google.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Commit-Queue: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361187}

--- ios/components/order_file/order_file.cc
+++ ios/components/order_file/order_file.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/components/order_file/order_file_common.h"
-
 #import <stdlib.h>
+
+#import "ios/components/order_file/order_file_common.h"
 
 // Queue containing the ordered procedure calls.
 OSQueueHead gCRWSanitizerQueue = OS_ATOMIC_QUEUE_INIT;
@@ -53,7 +53,7 @@ void __sanitizer_cov_trace_pc_guard(uint32_t* guard) {  // NOLINT
   void* procedureCall = __builtin_return_address(0);
   CRWProcedureCallNode* node =
       (CRWProcedureCallNode*)malloc(sizeof(CRWProcedureCallNode));
-  *node = (CRWProcedureCallNode){procedureCall, NULL};
+  *node = (CRWProcedureCallNode){procedureCall, nullptr};
   OSAtomicEnqueue(&gCRWSanitizerQueue, node,
                   offsetof(CRWProcedureCallNode, next));
 }
