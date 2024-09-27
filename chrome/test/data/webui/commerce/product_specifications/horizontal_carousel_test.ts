c7fe688cbb4eeceb361a2364649363f19adb785f
Chase Hartsell <chazzy@chromium.org>
2024-09-27 23:31:15 +0000

    [Compare] Update table to fit viewport width
    This patch updates the table to fit the viewport width, down to a min
    width of 3 columns. The table empty state is updated to fit the
    viewport width and height with a fixed bottom padding.
    
    Several CSS variables used in multiple places are also refactored into
    a common `shared_vars.css`. This allows us to calculate the body's
    min width and height using the min sizes of other elements on the page.
    
    Bug: 362766210
    Change-Id: I3253ea9d83102d8fd86120412351413ccd18a55f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875378
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361417}

--- chrome/test/data/webui/commerce/product_specifications/horizontal_carousel_test.ts
+++ chrome/test/data/webui/commerce/product_specifications/horizontal_carousel_test.ts
@@ -30,6 +30,7 @@ suite('HorizontalCarouselTest', () => {
       });
     }
     const carouselElement = document.body.querySelector('horizontal-carousel')!;
+    carouselElement.style.maxWidth = '1140px';
     const eventPromise =
         eventToPromise('intersection-observed', carouselElement);
     tableElement.columns = columns;
@@ -69,14 +70,6 @@ suite('HorizontalCarouselTest', () => {
           // Arrange.
           const carouselElement =
               document.body.querySelector('horizontal-carousel')!;
-          const carouselContainer = carouselElement.$.carouselContainer;
-          // Remove restrictions on `carouselContainer`'s width,
-          // by setting `carouselElement`'s width to the maximum width
-          // `carouselContainer` can have.
-          const maxWidth =
-              carouselContainer.computedStyleMap().get('max-width');
-          carouselElement.style.width =
-              maxWidth !== undefined ? maxWidth.toString() : 'initial';
 
           // Act.
           await setupColumns({numColumns: numColumns});
