8201153684bd278a70c9cd3feee943c86da5ce26
Keren Zhu <kerenzhu@chromium.org>
2024-09-27 19:40:40 +0000

    base: use ObserverList::Notify() in ui/ozone/platform/wayland
    This CL was uploaded by git cl split.
    
    R=nickdiego@igalia.com
    
    Bug: 40727208
    Change-Id: I37a78dc60d58ea142aef3d80f060f2968f2d16ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893531
    Auto-Submit: Keren Zhu <kerenzhu@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1361326}

--- ui/ozone/platform/wayland/host/wayland_screen.h
+++ ui/ozone/platform/wayland/host/wayland_screen.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/observer_list.h"
 #include "base/values.h"
 #include "ui/display/display_list.h"
 #include "ui/display/display_observer.h"
@@ -141,8 +140,6 @@ class WaylandScreen : public PlatformScreen
 
   base::flat_map<WaylandOutput::Id, int64_t> display_id_map_;
   display::DisplayList display_list_;
-
-  base::ObserverList<display::DisplayObserver> observers_;
 
   std::optional<gfx::BufferFormat> image_format_alpha_;
   std::optional<gfx::BufferFormat> image_format_no_alpha_;
