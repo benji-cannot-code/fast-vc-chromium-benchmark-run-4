357dab7e88323007ecd9efca533d680d90f9cfef
Eliot Courtney <edcourtney@google.com>
2024-09-27 08:37:19 +0000

    Add feature flag for Android media picking.
    This is CL 1/? for Android media picking.
    
    Bug: 352186941
    Test: CQ
    Change-Id: I2be6ebb51a601a21c690f03e4e1766e3dfb1031e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874685
    Reviewed-by: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360985}

--- chrome/browser/media/webrtc/desktop_media_picker.cc
+++ chrome/browser/media/webrtc/desktop_media_picker.cc
@@ -3,9 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "desktop_media_picker.h"
-
 #include "chrome/browser/media/webrtc/desktop_media_picker.h"
+
+#if BUILDFLAG(IS_ANDROID)
+BASE_FEATURE(kAndroidMediaPicker,
+             "AndroidMediaPicker",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif
 
 DesktopMediaPicker::Params::Params(RequestSource request_source)
     : request_source(request_source) {}
--- chrome/browser/media/webrtc/desktop_media_picker.h
+++ chrome/browser/media/webrtc/desktop_media_picker.h
@@ -10,8 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/feature_list.h"
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
+#include "build/buildflag.h"
 #include "content/public/browser/desktop_media_id.h"
 #include "content/public/browser/media_stream_request.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -24,6 +26,10 @@ class DesktopMediaList;
 namespace content {
 class WebContents;
 }
+
+#if BUILDFLAG(IS_ANDROID)
+BASE_DECLARE_FEATURE(kAndroidMediaPicker);
+#endif
 
 // Base class for desktop media picker UI. It's used by Desktop Media API, and
 // by ARC to let user choose a desktop media source.
