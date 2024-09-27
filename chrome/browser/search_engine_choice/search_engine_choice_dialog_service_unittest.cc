d3830904cdf8828eab40f3e9f454deca5cfb067b
Christian Dullweber <dullweber@chromium.org>
2024-09-27 12:45:44 +0000

    Reland "[waffle]Don't show the dialog in a guest session if the pref is set"
    This is a reland of commit cf18d29dd6d004879f57685a452eb92bc1fe9f39
    
    Original change's description:
    > [waffle]Don't show the dialog in a guest session if the pref is set
    >
    > The search engine choice dialog should not be displayed in all guest
    > sessions if the users decide to share the default search engine across
    > guest sessions.
    >
    > Demo: http://b/364254781#comment2
    > Fixed: b:364254781
    > Change-Id: I196b7c75b357fb351167e933f58d2ef777939e44
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5829416
    > Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Auto-Submit: Jack Yammine <jyammine@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1353983}
    
    Cq-Include-Trybots: luci.chromium.try:mac13-tests
    Change-Id: Ic886af24761256c90c14640c0bdad7cb020c3660
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891297
    Auto-Submit: Christian Dullweber <dullweber@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361083}

--- chrome/browser/search_engine_choice/search_engine_choice_dialog_service_unittest.cc
+++ chrome/browser/search_engine_choice/search_engine_choice_dialog_service_unittest.cc
@@ -16,8 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/dialog_test_browser_window.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "chrome/test/base/testing_profile_manager.h"
 #include "components/country_codes/country_codes.h"
 #include "components/search_engines/prepopulated_engines.h"
+#include "components/search_engines/search_engine_choice/search_engine_choice_service.h"
 #include "components/search_engines/search_engine_choice/search_engine_choice_utils.h"
 #include "components/search_engines/search_engine_utils.h"
 #include "components/search_engines/search_engines_pref_names.h"
@@ -347,10 +349,16 @@ TEST_F(SearchEngineChoiceDialogServiceTest,
   EXPECT_FALSE(g_browser_process->local_state()->HasPrefPath(
       prefs::kDefaultSearchProviderGuestModePrepopulatedId));
 
-  SearchEngineChoiceDialogService* search_engine_choice_dialog_service =
-      SearchEngineChoiceDialogServiceFactory::GetForProfile(profile());
-  profile()->SetGuestSession(/*guest=*/true);
+  TestingProfile* parent_guest = profile_manager()->CreateGuestProfile();
+  Profile* child_guest = parent_guest->GetOffTheRecordProfile(
+      Profile::OTRProfileID::PrimaryID(), false);
 
+  TemplateURLServiceFactory::GetInstance()->SetTestingFactory(
+      parent_guest,
+      base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
+
+  SearchEngineChoiceDialogService* search_engine_choice_dialog_service =
+      SearchEngineChoiceDialogServiceFactory::GetForProfile(child_guest);
   const int kPrepopulatedId =
       search_engine_choice_dialog_service->GetSearchEngines()
           .at(0)
@@ -372,10 +380,16 @@ TEST_F(SearchEngineChoiceDialogServiceTest,
   EXPECT_FALSE(g_browser_process->local_state()->HasPrefPath(
       prefs::kDefaultSearchProviderGuestModePrepopulatedId));
 
-  SearchEngineChoiceDialogService* search_engine_choice_dialog_service =
-      SearchEngineChoiceDialogServiceFactory::GetForProfile(profile());
-  profile()->SetGuestSession(/*guest=*/true);
+  TestingProfile* parent_guest = profile_manager()->CreateGuestProfile();
+  Profile* child_guest = parent_guest->GetOffTheRecordProfile(
+      Profile::OTRProfileID::PrimaryID(), false);
 
+  TemplateURLServiceFactory::GetInstance()->SetTestingFactory(
+      parent_guest,
+      base::BindRepeating(&TemplateURLServiceFactory::BuildInstanceFor));
+
+  SearchEngineChoiceDialogService* search_engine_choice_dialog_service =
+      SearchEngineChoiceDialogServiceFactory::GetForProfile(child_guest);
   const int kPrepopulatedId =
       search_engine_choice_dialog_service->GetSearchEngines()
           .at(0)
