54c5a989143a3b517779b83c00aa203cf3acac95
Benjamin Williams <bwwilliams@google.com>
2024-09-27 11:36:55 +0000

    Remove redundant base::Time forward declarations
    Improve code hygiene by removing redundant forward declarations of
    base::Time in ios/chrome/test/app. Since base/time/time.h or base/timer/timer.h
    are included, the full definition is available, making forward
    declarations unnecessary.
    
    This CL was uploaded by git cl split.
    
    R=jood@google.com
    
    Bug: 369814311
    Change-Id: I4a97f4271e577599ea295d6846101a3c1cb84846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892216
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Jood Hajeer <jood@google.com>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361054}

--- ios/chrome/test/app/sync_test_util.h
+++ ios/chrome/test/app/sync_test_util.h
@@ -15,10 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/metrics_proto/user_demographics.pb.h"
 #include "url/gurl.h"
 
-namespace base {
-class Time;
-}  // namespace base
-
 namespace synced_sessions {
 struct DistantSession;
 }  // namespace synced_sessions
