5aba4460f10dc52c367cf7719b75115dc27ead3f
Greg Thompson <grt@chromium.org>
2024-09-27 09:39:35 +0000

    [mac] More diagnosts for ContentBrowserTest failure
    Include the errno in the failure message, and only crash if the desired
    file does not exist.
    
    Bug: 345765743
    Change-Id: I487c7fe9d3c4c4808bff93019bb680240cd65e4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893206
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Auto-Submit: Greg Thompson <grt@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361008}

--- content/public/test/content_browser_test.cc
+++ content/public/test/content_browser_test.cc
@@ -5,11 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/test/content_browser_test.h"
 
+#include "base/check.h"
 #include "base/check_op.h"
 #include "base/command_line.h"
 #include "base/files/file.h"
 #include "base/files/file_util.h"
-#include "base/notreached.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
 #include "base/task/current_thread.h"
@@ -70,8 +70,11 @@ ContentBrowserTest::ContentBrowserTest() {
                       base::File::FLAG_OPEN_ALWAYS | base::File::FLAG_WRITE);
       !file.IsValid()) {
     // Diagnostics for https://crbug.com/345765743.
-    NOTREACHED() << "Failed to create \"" << content_shell_path
-                 << "\": " << base::File::ErrorToString(file.error_details());
+    const auto last_errno = errno;
+    CHECK(base::PathExists(content_shell_path))
+        << "Failed to create \"" << content_shell_path
+        << "\": " << base::File::ErrorToString(file.error_details())
+        << "; errno = " << last_errno;
   }
   file_exe_override_.emplace(base::FILE_EXE, content_shell_path,
                              /*is_absolute=*/false, /*create=*/false);
