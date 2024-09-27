b7b007f2b304761b1c1c8dc235d159e5c4b6ba18
Andrew Grieve <agrieve@chromium.org>
2024-09-27 21:38:02 +0000

    Android: Fix some Error Prone violations in cast code.
    Bug: 40661145
    Change-Id: Idcb14d183424f2fb48564b6bd97b67475145948c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895813
    Owners-Override: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361382}

--- components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
+++ components/webxr/android/java/src/org/chromium/components/webxr/XrHostActivity.java
@@ -52,7 +52,7 @@ public class XrHostActivity extends Activity {
         super.onAttachedToWindow();
 
         boolean result = XrSessionCoordinator.onXrHostActivityReady(this);
-        assert (result);
+        assert result;
     }
 
     @Override
