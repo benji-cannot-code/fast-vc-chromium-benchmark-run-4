99d04eab43c53af28d26b1b277e72c42d9e56a82
Akihiro Ota <akihiroota@chromium.org>
2024-09-27 14:46:22 +0000

    FaceGaze: Add messages about state
    This change adds support for messages about state. There are a few
    cases where we'd like to display this type of message in the
    FaceGaze bubble UI:
    
    1. When scroll mode is active
    2. When FaceGaze is paused
    
    In the above cases, these are states that apply until the user
    explicitly toggles them again. Displaying these messages can remind
    users the current state so that they don't forget the FaceGaze mode
    they're in.
    
    Bug: 369426561
    Change-Id: I38a27c89e79d1bccab2aa13f813443f6973ecd59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893964
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361134}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/gesture_handler.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/gesture_handler.ts
@@ -79,6 +79,10 @@ export class GestureHandler {
     this.macrosToCompleteLater_.clear();
   }
 
+  isPaused(): boolean {
+    return this.paused_;
+  }
+
   private updateFromPrefs_(prefs: PrefObject[]): void {
     prefs.forEach(pref => {
       switch (pref.key) {
