6f0b996a5bd053d5cbb8fa77f6d5627959760fcc
Sorin Jianu <sorin@chromium.org>
2024-09-27 04:04:58 +0000

    updater: define crash keys for GetLocalizedString debugging
    Bug: 359667990
    Change-Id: Icaae19733335c844ab4df735a516131df7988f8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894405
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1360922}

--- chrome/updater/win/ui/l10n_util.cc
+++ chrome/updater/win/ui/l10n_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/debug/alias.h"
+#include "base/debug/crash_logging.h"
 #include "base/logging.h"
 #include "base/notreached.h"
 #include "base/strings/string_util.h"
@@ -49,6 +50,8 @@ std::wstring GetPreferredLanguage() {
 
 std::wstring GetLocalizedString(unsigned int base_message_id,
                                 const std::wstring& lang) {
+  ::SetLastError(ERROR_SUCCESS);
+
   // Map `base_message_id` to the base id for the current install mode.
   const unsigned int message_id =
       static_cast<UINT>(base_message_id + GetLanguageOffset(lang));
@@ -64,9 +67,11 @@ std::wstring GetLocalizedString(unsigned int base_message_id,
   DEBUG_ALIAS_FOR_CSTR(dbg_lang, base::WideToUTF8(lang).c_str(), 16);
   VLOG(2) << base_message_id << ", " << message_id << ", " << error_code << ", "
           << lang;
-  NOTREACHED_IN_MIGRATION()
-      << "Unable to find resource, " << base_message_id << ", " << message_id
-      << ", " << error_code << ", " << lang;
+  SCOPED_CRASH_KEY_NUMBER("l10_util", "base_message_id", base_message_id);
+  SCOPED_CRASH_KEY_NUMBER("l10_util", "message_id", message_id);
+  SCOPED_CRASH_KEY_NUMBER("l10_util", "error_code", error_code);
+  SCOPED_CRASH_KEY_STRING32("l10_util", "lang", dbg_lang);
+  NOTREACHED_IN_MIGRATION();
   return std::wstring();
 }
 
