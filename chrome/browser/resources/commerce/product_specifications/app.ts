083736f191af8586f86ca96d05b4982e539efa46
Chase Hartsell <chazzy@chromium.org>
2024-09-27 23:36:06 +0000

    [Compare] Add fade and slide effects to table loading animation
    This patch updates the summary container to fade/slide in and out when
    the app transitions to and from the loading state. This is similar to
    the loading effect used by auto tab groups.
    
    Initial load with URLs:
    http://go/scrcast/NTMwNTU1MDQ2MzA0MTUzNnxjZWQ2N2Q5ZS0yZQ
    Adding a URL:
    http://go/scrcast/NjA5ODcxMDMyNzk4NDEyOHw0Nzc4YjQ0Yy0zNg
    
    Bug: 368658843
    Change-Id: I8ddd1a6fbf311b37fdd0e456416f48f01613a1f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878882
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361425}

--- chrome/browser/resources/commerce/product_specifications/app.ts
+++ chrome/browser/resources/commerce/product_specifications/app.ts
@@ -77,6 +77,7 @@ export interface ProductSpecificationsElement {
     offlineToast: CrToastElement,
     productSelector: ProductSelectorElement,
     specs: HTMLElement,
+    summaryContainer: HTMLElement,
     summaryTable: TableElement,
     syncPromo: HTMLElement,
     turnOnSyncButton: CrButtonElement,
@@ -227,6 +228,8 @@ export class ProductSpecificationsElement extends PolymerElement {
   private eventTracker_: EventTracker = new EventTracker();
   private id_: Uuid|null = null;
   private listenerIds_: number[] = [];
+  private loadingAnimationSlidePx_: number = 16;
+  private loadingAnimationSlideDurationMs_: number = 200;
   private minLoadingAnimationMs_: number = 500;
   private productSpecificationsFeatureState_: ProductSpecificationsFeatureState;
   private shoppingApi_: BrowserProxy = BrowserProxyImpl.getInstance();
@@ -412,9 +415,10 @@ export class ProductSpecificationsElement extends PolymerElement {
   }
 
   private async populateTable_(urls: string[]) {
+    await this.enterLoadingState_(urls.length);
+
     const start = Date.now();
     this.showEmptyState_ = false;
-    this.loadingState_ = {loading: true, urlCount: urls.length};
     this.$.errorToast.hide();
 
     const tableColumns: TableColumn[] = [];
@@ -461,7 +465,7 @@ export class ProductSpecificationsElement extends PolymerElement {
 
     this.tableColumns_ = tableColumns;
     this.showEmptyState_ = this.tableColumns_.length === 0;
-    this.loadingState_ = {loading: false, urlCount: 0};
+    this.exitLoadingState_();
   }
 
   private get isOffline_(): boolean {
@@ -747,6 +751,64 @@ export class ProductSpecificationsElement extends PolymerElement {
   private getDisclaimerText_(): string {
     return loadTimeData.getStringF(
         'experimentalFeatureDisclaimer', loadTimeData.getString('userEmail'));
+  }
+
+  private fadeAndSlideOutSummaryContainer_(): Animation {
+    return this.$.summaryContainer.animate(
+        [
+          {opacity: 1, transform: 'translateY(0px)'},
+          {
+            opacity: 0,
+            transform: `translateY(-${this.loadingAnimationSlidePx_}px)`,
+          },
+        ],
+        {
+          duration: this.loadingAnimationSlideDurationMs_,
+          easing: 'ease-out',
+          fill: 'forwards',
+        });
+  }
+
+  private fadeAndSlideInSummaryContainer_(): Animation {
+    return this.$.summaryContainer.animate(
+        [
+          {
+            opacity: 0,
+            transform: `translateY(${this.loadingAnimationSlidePx_}px)`,
+          },
+          {opacity: 1, transform: 'translateY(0px)'},
+        ],
+        {
+          duration: this.loadingAnimationSlideDurationMs_,
+          easing: 'ease-out',
+          fill: 'forwards',
+        });
+  }
+
+  // Resolves upon updating the loading state.
+  private async enterLoadingState_(urlCount: number): Promise<void> {
+    if ([AppState.ERROR, AppState.SYNC_SCREEN, AppState.LOADING].includes(
+            this.appState_)) {
+      this.loadingState_ = {loading: true, urlCount};
+      return Promise.resolve();
+    }
+
+    const anim = this.fadeAndSlideOutSummaryContainer_();
+    return new Promise<void>(resolve => {
+      anim.addEventListener('finish', () => {
+        this.loadingState_ = {loading: true, urlCount};
+        resolve();
+        this.fadeAndSlideInSummaryContainer_();
+      });
+    });
+  }
+
+  private exitLoadingState_() {
+    const anim = this.fadeAndSlideOutSummaryContainer_();
+    anim.addEventListener('finish', () => {
+      this.loadingState_ = {loading: false, urlCount: 0};
+      this.fadeAndSlideInSummaryContainer_();
+    });
   }
 }
 
