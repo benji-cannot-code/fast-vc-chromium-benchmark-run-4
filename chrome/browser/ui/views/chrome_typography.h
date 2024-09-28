4b0f683cd996a834db8dde69772136d09191e6d3
stluong <stluong@chromium.org>
2024-09-28 00:01:10 +0000

    Fix toast text size and align leading icon
    The toast body text was using a 12pt font size but this CL increases the
    size to 13pt instead to match the figma mocks.
    
    In addition, this CL adds a 2dp margin to the left and right side of the
    toast leading icon to match the spacing in the mocks.
    
    Before fix: https://screenshot.googleplex.com/4SyjFK9dscW9Vmf.png
    After fix: https://screenshot.googleplex.com/3abE759mFSGcAeH.png
    
    Fixed: 369984157
    Change-Id: I8e8b6a137c0f414a7107108ea768c037ce2b6696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5896533
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Steven Luong <stluong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361437}

--- chrome/browser/ui/views/chrome_typography.h
+++ chrome/browser/ui/views/chrome_typography.h
@@ -84,6 +84,9 @@ enum ChromeTextContext {
 
   // Title label in the browser side panel. Usually 13pt.
   CONTEXT_SIDE_PANEL_TITLE,
+
+  // Body label in the toast. Usually 13pt.
+  CONTEXT_TOAST_BODY_TEXT,
 };
 
 enum ChromeTextStyle {
