a5c3f8c48deb0a10130a5ca79c31ccfc9cf3d1db
Amber Haynes <amberhaynes@chromium.org>
2024-09-28 02:30:41 +0000

    graduation: Add Error and Offline Screens
    + The offline screen is shown when the app is offline, and the welcome
    screen is shown when the app is back online.
    + The error screen is shown on loadabort of the Takeout webview and is
    permanent.
    
    Offline Screen: https://screenshot.googleplex.com/6evoxGfyjaexxjP
    Error Screen: https://screenshot.googleplex.com/YZz2tsRpFmJohsK
    
    Bug: b:357877855
    Change-Id: I0e6c33f4a1ba34ac88290d3fe85d3fdaa3913e91
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891421
    Reviewed-by: Courtney Wong <courtneywong@chromium.org>
    Commit-Queue: Amber Haynes <amberhaynes@chromium.org>
    Reviewed-by: Aga Wronska <agawronska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361462}

---
+++ ash/webui/graduation/resources/js/graduation_offline.ts
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+
+import {getTemplate} from './graduation_offline.html.js';
+
+class GraduationOffline extends PolymerElement {
+  static get is() {
+    return 'graduation-offline';
+  }
+
+  static get template() {
+    return getTemplate();
+  }
+}
+customElements.define(GraduationOffline.is, GraduationOffline);
