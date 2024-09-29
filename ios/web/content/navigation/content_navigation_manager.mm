e08167c15a21a17ded114bb92714b16bedf1ea8a
Gyuyoung Kim <gyuyoung@igalia.com>
2024-09-29 00:49:16 +0000

    [iOS Blink] Remove AddRestoreCompletionCallback method
    When https://crrev.com/c/5589988 removed unused old
    web state restoration logic, it missed removing
    AddRestoreCompletionCallback in ContentNavigationManager
    class. This CL removes it.
    
    Bug: 40276021
    Change-Id: I61885c676a45de443cecd8204daedccea288449b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5895790
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Gyuyoung Kim <gyuyoung@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1361555}

--- ios/web/content/navigation/content_navigation_manager.mm
+++ ios/web/content/navigation/content_navigation_manager.mm
@@ -223,10 +223,4 @@ void ContentNavigationManager::Restore(
   NOTIMPLEMENTED();
 }
 
-void ContentNavigationManager::AddRestoreCompletionCallback(
-    base::OnceClosure callback) {
-  // TODO(crbug.com/40257932)
-  NOTIMPLEMENTED();
-}
-
 }  // namespace web
