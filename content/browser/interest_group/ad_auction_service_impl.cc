f3e730390fe4e64bf725115ec1ee930abe4f4336
Abigail Katcoff <abigailkatcoff@chromium.org>
2024-09-30 23:13:39 +0000

    PA: add metric Ads.InterestGroup.Auction.NumOwnerOriginsCachedForPreconnect
    Add a metric to help us gain more insight into how well
    FledgeUsePreconnectCache is doing. This will also be useful for a
    future study starting processes at the start of auctions, which
    will take advantage of the same cache.
    
    Bug: 353480639
    Change-Id: Icf1331fddf7d7d6c23c42871d712eca2f6233e8e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5897973
    Commit-Queue: Abigail Katcoff <abigailkatcoff@chromium.org>
    Reviewed-by: Orr Bernstein <orrb@google.com>
    Reviewed-by: Russ Hamilton <behamilton@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362081}

--- content/browser/interest_group/ad_auction_service_impl.cc
+++ content/browser/interest_group/ad_auction_service_impl.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <cstddef>
 #ifdef UNSAFE_BUFFERS_BUILD
 // TODO(crbug.com/342213636): Remove this and spanify to fix the errors.
 #pragma allow_unsafe_buffers
@@ -449,13 +450,16 @@ void AdAuctionServiceImpl::RunAdAuction(
   // on-device auction.
   if (base::FeatureList::IsEnabled(features::kFledgeUsePreconnectCache) &&
       !config.server_response.has_value()) {
-    PreconnectToBuyerOrigins(config);
+    size_t n_owners_cached = PreconnectToBuyerOrigins(config);
     for (const blink::AuctionConfig& component_config :
          config.non_shared_params.component_auctions) {
       if (!component_config.server_response.has_value()) {
-        PreconnectToBuyerOrigins(component_config);
+        n_owners_cached += PreconnectToBuyerOrigins(component_config);
       }
     }
+    base::UmaHistogramCounts100(
+        "Ads.InterestGroup.Auction.NumOwnerOriginsCachedForPreconnect",
+        n_owners_cached);
   }
 
   std::unique_ptr<AuctionRunner> auction = AuctionRunner::CreateAndStart(
@@ -1249,11 +1253,12 @@ AdAuctionPageData* AdAuctionServiceImpl::GetAdAuctionPageData() {
       render_frame_host().GetPage());
 }
 
-void AdAuctionServiceImpl::PreconnectToBuyerOrigins(
+size_t AdAuctionServiceImpl::PreconnectToBuyerOrigins(
     const blink::AuctionConfig& config) {
   if (!config.non_shared_params.interest_group_buyers) {
-    return;
+    return 0;
   }
+  size_t n_owners_cached = 0;
   for (const auto& buyer : *config.non_shared_params.interest_group_buyers) {
     std::optional<url::Origin> signals_origin;
     if (GetInterestGroupManager().GetCachedOwnerAndSignalsOrigins(
@@ -1262,6 +1267,7 @@ void AdAuctionServiceImpl::PreconnectToBuyerOrigins(
           net::NetworkAnonymizationKey::CreateSameSite(
               net::SchemefulSite(buyer));
       PreconnectSocket(buyer.GetURL(), network_anonymization_key);
+      n_owners_cached += 1;
       if (signals_origin) {
         // We preconnect to the signals origin and not the full signals URL so
         // that we do not need to store the full URL in memory. Preconnecting
@@ -1271,6 +1277,7 @@ void AdAuctionServiceImpl::PreconnectToBuyerOrigins(
       }
     }
   }
+  return n_owners_cached;
 }
 
 }  // namespace content
