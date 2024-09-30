7ed2efd7374e864b2b8765d35c281d5097efc331
Travis Crumley <crumley@google.com>
2024-09-30 22:15:36 +0000

    Render sharing flows inside the container
    This renders the Data Sharing SDK inside the container, and sets up theming using GM3 variable names.
    
    Change-Id: I432b7c47be701be15b6e6366ee0ca4d415e8c577
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5877031
    Reviewed-by: pengchao Cai <pengchaocai@chromium.org>
    Commit-Queue: Travis Crumley <crumley@google.com>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362050}

--- chrome/browser/resources/data_sharing/data_sharing_app.ts
+++ chrome/browser/resources/data_sharing/data_sharing_app.ts
@@ -14,7 +14,6 @@ import './dummy_data_sharing_sdk.js';
 
 import {ColorChangeUpdater} from '//resources/cr_components/color_change_listener/colors_css_updater.js';
 import {CustomElement} from 'chrome-untrusted://resources/js/custom_element.js';
-
 import {BrowserProxyImpl} from './browser_proxy.js';
 import type {BrowserProxy} from './browser_proxy.js';
 import {getTemplate} from './data_sharing_app.html.js';
@@ -86,11 +85,13 @@ export class DataSharingApp extends CustomElement {
     const groupId = params.get(UrlQueryParams.GROUP_ID);
     const tokenSecret = params.get(UrlQueryParams.TOKEN_SECRET);
     const tabGroupId = params.get(UrlQueryParams.TAB_GROUP_ID);
+    const parent = this.getRequiredElement('#dialog-container');
 
     switch (flow) {
       case FlowValues.SHARE:
         this.dataSharingSdk_
             .runInviteFlow({
+              parent,
               getShareLink: (params: DataSharingSdkGetLinkParams):
                   Promise<string> => {
                     this.makeTabGroupShared(tabGroupId!, params.groupId);
@@ -104,7 +105,7 @@ export class DataSharingApp extends CustomElement {
       case FlowValues.JOIN:
         // group_id and token_secret cannot be null for join flow.
         this.dataSharingSdk_
-            .runJoinFlow({groupId: groupId!, tokenSecret: tokenSecret!})
+            .runJoinFlow({parent, groupId: groupId!, tokenSecret: tokenSecret!})
             .then((res) => {
               this.browserProxy_.closeUi(res.status);
             });
@@ -113,6 +114,7 @@ export class DataSharingApp extends CustomElement {
         // group_id cannot be null for manage flow.
         this.dataSharingSdk_
             .runManageFlow({
+              parent,
               groupId: groupId!,
               getShareLink: (params: DataSharingSdkGetLinkParams):
                   Promise<string> => {
