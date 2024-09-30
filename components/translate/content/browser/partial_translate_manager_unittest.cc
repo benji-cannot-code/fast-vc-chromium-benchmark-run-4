b813de20a194a8c9bacb3c0f7d53e01e45eb42c8
Sam Fortiner <samfort@microsoft.com>
2024-09-30 18:10:24 +0000

    Fix dangling pointer in PartialTranslateManagerTest
    The delegate_ pointer refers to memory owned by the manager. Changing
    member variable order lets the delegate_ get cleared first, resolving
    the dangling pointer warning.
    
    Bug: 40212619
    Change-Id: Id360b6be7261710651a33997a7db4b2fda4023a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5818536
    Reviewed-by: Anthony Cui <cuianthony@chromium.org>
    Commit-Queue: Sam Fortiner <samfort@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1361901}

--- components/translate/content/browser/partial_translate_manager_unittest.cc
+++ components/translate/content/browser/partial_translate_manager_unittest.cc
@@ -55,9 +55,9 @@ class PartialTranslateManagerTest : public testing::Test {
   void TearDown() override {}
 
  protected:
-  // Owned by manager_.
-  raw_ptr<FakeContextualSearchDelegate, DanglingUntriaged> delegate_;
   std::unique_ptr<PartialTranslateManager> manager_;
+  // Owned by manager_.
+  raw_ptr<FakeContextualSearchDelegate> delegate_;
 };
 
 TEST_F(PartialTranslateManagerTest, CreateContext) {
