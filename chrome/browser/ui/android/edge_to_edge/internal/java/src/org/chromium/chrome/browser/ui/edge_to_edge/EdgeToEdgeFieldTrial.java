ae7f060d299a19444cdf23c6f8655c24f3e842b0
Wenyu Fu <wenyufu@chromium.org>
2024-09-30 19:36:08 +0000

    [E2E] Remove default min version override for samsung
    We found a potential fix for the issue attached, so no longer need to
    default override the min version for samsung. This CL also make the
    overrides for any OEMS fully depend on field trial params.
    
    Bug: 41487034
    Change-Id: Ic905709995ccb53dbf191c87d3a001cd3e791148
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900592
    Auto-Submit: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Commit-Queue: Charles Hager <clhager@google.com>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361965}

--- chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeFieldTrial.java
+++ chrome/browser/ui/android/edge_to_edge/internal/java/src/org/chromium/chrome/browser/ui/edge_to_edge/EdgeToEdgeFieldTrial.java
@@ -61,8 +61,6 @@ public final class EdgeToEdgeFieldTrial {
 
     private EdgeToEdgeFieldTrial() {
         mOemMinVersionOverrides = new HashMap<>();
-        // Some version of Samsung OneUi can draw a solid nav bar with e2e. See crbug.com/41487034.
-        mOemMinVersionOverrides.put("samsung", Build.VERSION_CODES.UPSIDE_DOWN_CAKE);
         initializeOverrides();
     }
 
