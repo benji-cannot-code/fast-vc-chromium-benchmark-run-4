026ec44d38a4f2f546cf3c21d4789a24402e4fcf
Wan-Teh Chang <wtc@google.com>
2024-09-30 21:55:22 +0000

    Roll src/third_party/dav1d/libdav1d/ a7a40a3fd..ed004fe95 (36 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/a7a40a3fde96..ed004fe95d47
    
    $ git log a7a40a3fd..ed004fe95 --date=short --first-parent --format='%ad %ae %s'
    2024-09-26 jinbo loongarch: minor improvement on decode_symbol_adapt
    2024-09-10 yuanhecai loongarch: rewrite optimization functions in loongarch/itx.S
    2024-09-10 guxiwei-hf LoongArch: Add save_tmvs_lsx
    2024-09-04 jinbo loongarch: refactor loopfilter
    2024-08-22 yuanhecai loongarch: add lasx implementation of sgr_3x3 for 8 bpc
    2024-08-21 yuanhecai loongarch: rewirte warp_8x8/8x8t_lsx for 8 bpc
    2024-08-20 jinbo loongarch: Refine prep_8tap_8bpc_lasx
    2024-08-16 yuanhecai loongarch: add lasx implementation of wiener filter for 8 bpc
    2024-09-06 zhoupeng Loongarch: Optimized load_tmvs_c function by LSX
    2024-08-06 pengxu Loongarch: Optimized ipred_z1 8bpc functions by LSX
    (...)
    2024-06-25 jinbo Refine mc_put_8tap
    2024-06-25 guxiwei-hf msac: Add msac_decode_bool_equia_lsx and msac_decode_hi_tok_lsx
    2024-06-24 pengxu Loongarch: Optimized cdef_find_dir_8bpc function by LSX
    2024-07-04 yuanhecai loongarch: opt inv_txfm_add_identity_identity_8x32_8bpc_lsx
    2024-06-25 yuanhecai loongarch: Minor improvement on identity4*, identity8* and dct32*
    2024-06-15 yuanhecai loongarch: add lsx implementation of itx_8bpc.add_8x16 series function for 8 bpc
    2024-06-14 yuanhecai loongarch: add lsx implementation of itx_8bpc.add_4x16 series function for 8 bpc
    2024-05-21 yuanhecai loongarch: add lsx implementation of itx_8bpc.add_4x8 series function for 8 bpc
    2024-06-21 yuanhecai loongarch: Opt one functions of itx_8bpc.add_16x32 series
    2024-09-21 mycisco meson: supports the iOS platform
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=dalecurtis@chromium.org
    
    Change-Id: Ib36caf346bd475d13fd024bfc37b36722375d941
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898276
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362038}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.4.2-65-ga7a40a3"
+#define DAV1D_VERSION "1.4.2-101-ged004fe"
