af315c85ee408474ab19684cb0ff8b37afb5d19c
Hiroki Nakagawa <nhiroki@chromium.org>
2024-09-30 00:25:40 +0000

    Revert "[Mac] Gather metrics related to ProcessRequirement"
    This reverts commit 745b35db455785996db7233e55fbe060e05992f5.
    
    Reason for revert:
    This is a preparation for reverting https://crrev.com/c/5883776 that
    added ProcessRequirementTest failing on mac11-arm64-rel-tests:
    https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/46206/overview 
    
    Original change's description:
    > [Mac] Gather metrics related to ProcessRequirement
    >
    > Metrics are gathered on a background thread shortly after launch. These
    > metrics will help gauge the reliability of retrieving the team
    > identifier and validation category. Additionally, we gather timing data
    > around constructing a ProcessRequirement and validating the current
    > process against it to get an understanding of what overhead this work
    > will add to Mach port rendezvous.
    >
    > Bug: 362302761
    > Change-Id: Ieaabcb17f8872c9a3c1de82d9ee1bb5d9517f4d2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892152
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Mark Mentovai <mark@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Leonard Grey <lgrey@chromium.org>
    > Commit-Queue: Mark Rowe <markrowe@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1361376}
    
    Bug: 362302761
    Change-Id: I2ae46876766fbe0fae4a829b0a47969e1077e29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5896901
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361630}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -128,7 +128,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(IS_MAC)
-#include "base/mac/process_requirement.h"
 #include "chrome/common/chrome_version.h"
 #endif  // BUILDFLAG(IS_MAC)
 
@@ -880,10 +879,6 @@ void RecordStartupMetrics() {
 
 #if BUILDFLAG(IS_CHROMEOS_LACROS)
   RecordChromeOSChannel();
-#endif
-
-#if BUILDFLAG(IS_MAC)
-  base::mac::ProcessRequirement::MaybeGatherMetrics();
 #endif
 }
 
