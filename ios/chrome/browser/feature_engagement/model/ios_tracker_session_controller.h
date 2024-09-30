87a4824f5cb5da191febfee538e4076acd8d6e7e
Huiting Yu <huitingyu@google.com>
2024-09-30 16:14:36 +0000

    [ios] remove flag kFeatureEngagementSessionReset.
    Gated code has been fully launched.
    
    Fix: b/332871581
    
    Change-Id: Ia67ad90177cfa60974f6f2a50b912330fefdef08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894370
    Commit-Queue: Huiting Yu <huitingyu@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361837}

--- ios/chrome/browser/feature_engagement/model/ios_tracker_session_controller.h
+++ ios/chrome/browser/feature_engagement/model/ios_tracker_session_controller.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/default_clock.h"
 #import "base/time/time.h"
 
-BASE_DECLARE_FEATURE(kFeatureEngagementSessionReset);
-
 // An implementation with iOS-specific logic of managing the lifecycle of the
 // SessionController. Injected into the feature engagement tracker replacing
 // the default implementation.
