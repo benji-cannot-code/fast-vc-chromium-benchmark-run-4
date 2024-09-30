88cf194637c7603afc12317d36b398b412ebb1b7
Fredrik Söderquist <fs@opera.com>
2024-09-30 00:56:54 +0000

    spanification: DetectTextEncoding()
    Bug: 351564777
    Change-Id: I7b87ef538adcfee17b63cf7f71c5030c4f701d93
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893770
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1361634}

--- third_party/blink/renderer/platform/text/text_encoding_detector_test.cc
+++ third_party/blink/renderer/platform/text/text_encoding_detector_test.cc
@@ -17,8 +17,8 @@ TEST(TextEncodingDetectorTest, RespectIso2022Jp) {
       " \x1B"
       "$BKL3$F;F|K\\%O%`%U%!%$%?!<%:$,%=%U%H%P%s%/$H$N%W%l!<%*%U$r@)$7!\"";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(iso2022jp.c_str(), iso2022jp.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(iso2022jp), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("ISO-2022-JP"), encoding);
 }
@@ -29,8 +29,8 @@ TEST(TextEncodingDetectorTest, Ignore7BitEncoding) {
   std::string hz_gb2312 =
       " ~{\x54\x42\x31\x7D\x37\x22\x55\x39\x35\x3D\x3D\x71~} abc";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(hz_gb2312.c_str(), hz_gb2312.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(hz_gb2312), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("US-ASCII"), encoding);
 }
@@ -42,8 +42,8 @@ TEST(TextEncodingDetectorTest, NonWHATWGEncodingBecomesAscii) {
       "\xff\xd7\xff\xe0\x00\x10JFIF foo bar baz\xff\xe1\x00\xa5"
       "\x87\x01\xd7\xff\x01\x57\x33\x44\x55\x66\x77\xed\xcb\xa9";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(pseudo_jpg.c_str(), pseudo_jpg.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(pseudo_jpg), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("US-ASCII"), encoding);
 }
@@ -55,15 +55,15 @@ TEST(TextEncodingDetectorTest, UrlHintHelpsEUCJP) {
       "\xBB\xF1\xBE\xF0\xCA\xF3\xA4\xCE\xA5\xD5\xA5\xA3\xA5\xB9\xA5\xB3</"
       "TITLE>";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(eucjp_bytes.c_str(), eucjp_bytes.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(eucjp_bytes), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("GBK"), encoding)
       << "Without language hint, it's detected as GBK";
 
   KURL url_jp_domain("http://example.co.jp/");
-  result = DetectTextEncoding(eucjp_bytes.c_str(), eucjp_bytes.length(),
-                              nullptr, url_jp_domain, nullptr, &encoding);
+  result = DetectTextEncoding(base::as_byte_span(eucjp_bytes), nullptr,
+                              url_jp_domain, nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("EUC-JP"), encoding)
       << "With URL hint including '.jp', it's detected as EUC-JP";
@@ -76,22 +76,22 @@ TEST(TextEncodingDetectorTest, LanguageHintHelpsEUCJP) {
       "\xBB\xF1\xBE\xF0\xCA\xF3\xA4\xCE\xA5\xD5\xA5\xA3\xA5\xB9\xA5\xB3</"
       "TITLE>";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(eucjp_bytes.c_str(), eucjp_bytes.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(eucjp_bytes), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("GBK"), encoding)
       << "Without language hint, it's detected as GBK";
 
   KURL url("http://example.com/");
-  result = DetectTextEncoding(eucjp_bytes.c_str(), eucjp_bytes.length(),
-                              nullptr, url, "ja", &encoding);
+  result = DetectTextEncoding(base::as_byte_span(eucjp_bytes), nullptr, url,
+                              "ja", &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("GBK"), encoding)
       << "Language hint doesn't help for normal URL. Should be detected as GBK";
 
   KURL file_url("file:///text.txt");
-  result = DetectTextEncoding(eucjp_bytes.c_str(), eucjp_bytes.length(),
-                              nullptr, file_url, "ja", &encoding);
+  result = DetectTextEncoding(base::as_byte_span(eucjp_bytes), nullptr,
+                              file_url, "ja", &encoding);
   EXPECT_TRUE(result);
   EXPECT_EQ(WTF::TextEncoding("EUC-JP"), encoding)
       << "Language hint works for file resource. Should be detected as EUC-JP";
@@ -102,8 +102,8 @@ TEST(TextEncodingDetectorTest, UTF8DetectionShouldFail) {
       "tnegirjji gosa gii beare s\xC3\xA1htt\xC3\xA1 \xC4\x8D\xC3"
       "\xA1llit artihkkaliid. Maid don s\xC3\xA1ht\xC3\xA1t dievasmah";
   WTF::TextEncoding encoding;
-  bool result = DetectTextEncoding(utf8_bytes.c_str(), utf8_bytes.length(),
-                                   nullptr, NullURL(), nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(utf8_bytes), nullptr,
+                                   NullURL(), nullptr, &encoding);
   EXPECT_FALSE(result);
 }
 
@@ -113,8 +113,8 @@ TEST(TextEncodingDetectorTest, RespectUTF8DetectionForFileResource) {
       "\xA1llit artihkkaliid. Maid don s\xC3\xA1ht\xC3\xA1t dievasmah";
   WTF::TextEncoding encoding;
   KURL file_url("file:///text.txt");
-  bool result = DetectTextEncoding(utf8_bytes.c_str(), utf8_bytes.length(),
-                                   nullptr, file_url, nullptr, &encoding);
+  bool result = DetectTextEncoding(base::as_byte_span(utf8_bytes), nullptr,
+                                   file_url, nullptr, &encoding);
   EXPECT_TRUE(result);
 }
 
