d8ed1f96aa2848f1cafbad3a9666e40251c7d185
Hiroki Nakagawa <nhiroki@chromium.org>
2024-09-30 01:21:53 +0000

    Gardening: Disable ProductSpecificationsTest.HorizontalCarousel on win
    ProductSpecificationsTest.HorizontalCarousel has been failing on
    win11-arm64-rel-tests since
    https://ci.chromium.org/ui/p/chromium/builders/ci/win11-arm64-rel-tests/2542/overview.
    
    Due to the long conflict CL chain, this CL disables the test instead of
    reverting them all.
    
    Bug: 370252258
    Change-Id: I5764ffa3d14ff8ded710306976cde99bba456080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5897653
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Auto-Submit: Hiroki Nakagawa <nhiroki@chromium.org>
    Owners-Override: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361638}

--- chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
+++ chrome/test/data/webui/commerce/product_specifications/product_specifications_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "build/buildflag.h"
 #include "chrome/browser/commerce/shopping_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -103,7 +104,13 @@ IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, Header) {
   RunTest("commerce/product_specifications/header_test.js", "mocha.run()");
 }
 
-IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, HorizontalCarousel) {
+// TODO(crbug.com/370252258): Flaky on win11-arm64-rel-tests.
+#if BUILDFLAG(IS_WIN)
+#define MAYBE_HorizontalCarousel DISABLED_HorizontalCarousel
+#else
+#define MAYBE_HorizontalCarousel HorizontalCarousel
+#endif
+IN_PROC_BROWSER_TEST_F(ProductSpecificationsTest, MAYBE_HorizontalCarousel) {
   RunTest("commerce/product_specifications/horizontal_carousel_test.js",
           "mocha.run()");
 }
