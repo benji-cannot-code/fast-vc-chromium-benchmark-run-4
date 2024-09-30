2f4ca191c8b4e3c69e33e6dd08810e249320b69d
Sorin Jianu <sorin@chromium.org>
2024-09-30 19:40:01 +0000

    crash: include missing build_config.h header
    The header must be included because the compilation unit is using
    ARCH_CPU_64_BITS macro.
    
    This was discovered during a LSC for an unrelated reason.
    
    Change-Id: I8faeaed0979b5b590e9cfb41abb93a9c4df51175
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898523
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361967}

--- components/crash/core/app/fallback_crash_handler_win.cc
+++ components/crash/core/app/fallback_crash_handler_win.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
+#include "build/build_config.h"
 #include "components/crash/core/app/minidump_with_crashpad_info.h"
 
 namespace crash_reporter {
