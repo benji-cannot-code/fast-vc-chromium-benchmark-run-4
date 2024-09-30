ddfbe0b90db92a7584a52963c4e1affccde49551
Christopher Cameron <ccameron@chromium.org>
2024-09-30 18:14:07 +0000

    createImageBitmap: Respect colorSpaceConversion for video
    Plumb the parameter to reinterpret the video as being in sRGB from
    - blink::ImageBitmap::ImageBitmap (read from options)
    - blink::HTMLVideoElement::CreateStaticBitmapImage
    - blink::CreateImageFromVideoFrame
    - media::PaintCanvasVideoRenderer::Paint (via new parameter)
    
    Add a WPT test for this. For the WPT test, add a Display P3 mp4 and
    webm test video. (There exists a Rec2020 test video, but it is 10-bit,
    which has bugs on some platforms, and uses the Rec709 transfer function,
    which has different interpretations on all browsers).
    
    Bug: 362431889
    Change-Id: I3c12eb720b927a8e6466ab35ae29a044342df123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878044
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Fernando Serboncini <fserb@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361907}

--- third_party/blink/renderer/core/html/media/html_video_element.cc
+++ third_party/blink/renderer/core/html/media/html_video_element.cc
@@ -576,7 +576,8 @@ bool HTMLVideoElement::IsDefaultPosterImageURL() const {
 
 scoped_refptr<StaticBitmapImage> HTMLVideoElement::CreateStaticBitmapImage(
     bool allow_accelerated_images,
-    std::optional<gfx::Size> size) {
+    std::optional<gfx::Size> size,
+    bool reinterpret_as_srgb) {
   media::PaintCanvasVideoRenderer* video_renderer = nullptr;
   scoped_refptr<media::VideoFrame> media_video_frame;
   if (auto* wmp = GetWebMediaPlayer()) {
@@ -596,7 +597,9 @@ scoped_refptr<StaticBitmapImage> HTMLVideoElement::CreateStaticBitmapImage(
   // is inappropriate in many circumstances.
   const auto resource_provider_info = SkImageInfo::Make(
       gfx::SizeToSkISize(dest_size), kN32_SkColorType, kPremul_SkAlphaType,
-      media_video_frame->CompatRGBColorSpace().ToSkColorSpace());
+      reinterpret_as_srgb
+          ? SkColorSpace::MakeSRGB()
+          : media_video_frame->CompatRGBColorSpace().ToSkColorSpace());
   if (!resource_provider_ ||
       (resource_provider_->IsAccelerated() &&
        resource_provider_->IsGpuContextLost()) ||
@@ -621,10 +624,11 @@ scoped_refptr<StaticBitmapImage> HTMLVideoElement::CreateStaticBitmapImage(
   cache_deleting_timer_.StartOneShot(kTemporaryResourceDeletionDelay,
                                      FROM_HERE);
 
-  auto image = CreateImageFromVideoFrame(std::move(media_video_frame),
-                                         /*allow_zero_copy_images=*/true,
-                                         resource_provider_.get(),
-                                         video_renderer, gfx::Rect(dest_size));
+  auto image = CreateImageFromVideoFrame(
+      std::move(media_video_frame),
+      /*allow_zero_copy_images=*/true, resource_provider_.get(), video_renderer,
+      gfx::Rect(dest_size),
+      /*prefer_tagged_orientation=*/true, reinterpret_as_srgb);
   if (image)
     image->SetOriginClean(!WouldTaintOrigin());
   return image;
