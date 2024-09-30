89fa3e090e193c58c93dee2bd17faf1c8c8ed4fc
Fikre Mengistu <fikremengistu@chromium.org>
2024-09-30 18:58:12 +0000

    Adding "is_authentication_interaction" field to DIPS popups table
    To allow 3p cookie access based on web authentications in 3p popups, we add this column to the popups table to distinguish if a received interaction came from a user activation (ex: mouse click) or a web authentication. More info can be found here: go/webauthn-as-3pc-heuristic
    
    Change-Id: Ie5ea1ebde4b67cfcb1b479d7ea78eafb023994e1
    Bug: 40948689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5877492
    Reviewed-by: Ryan Tarpine <rtarpine@chromium.org>
    Commit-Queue: Fikre Mengistu <fikremengistu@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361941}

--- chrome/browser/dips/dips_database.cc
+++ chrome/browser/dips/dips_database.cc
@@ -199,6 +199,7 @@ bool DIPSDatabase::InitTables() {
       "access_id INT64,"
       "last_popup_time INTEGER,"
       "is_current_interaction BOOLEAN,"
+      "is_authentication_interaction BOOLEAN,"
       "PRIMARY KEY (`opener_site`,`popup_site`)"
     ")";
   // clang-format on
@@ -394,7 +395,8 @@ bool DIPSDatabase::WritePopup(const std::string& opener_site,
                               const std::string& popup_site,
                               const uint64_t access_id,
                               const base::Time& popup_time,
-                              bool is_current_interaction) {
+                              bool is_current_interaction,
+                              bool is_authentication_interaction) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (!CheckDBInit()) {
     return false;
@@ -406,8 +408,9 @@ bool DIPSDatabase::WritePopup(const std::string& opener_site,
       "popup_site,"
       "access_id,"
       "last_popup_time,"
-      "is_current_interaction"
-    ") VALUES(?,?,?,?,?)";
+      "is_current_interaction,"
+      "is_authentication_interaction"
+    ") VALUES(?,?,?,?,?,?)";
   // clang-format on
   DCHECK(db_->IsSQLValid(kWriteSql));
 
@@ -419,6 +422,7 @@ bool DIPSDatabase::WritePopup(const std::string& opener_site,
   statement.BindInt64(2, access_id);
   statement.BindTime(3, popup_time);
   statement.BindBool(4, is_current_interaction);
+  statement.BindBool(5, is_authentication_interaction);
 
   return statement.Run();
 }
@@ -539,7 +543,8 @@ std::optional<PopupsStateValue> DIPSDatabase::ReadPopup(
         "popup_site,"
         "access_id,"
         "last_popup_time, "
-        "is_current_interaction "
+        "is_current_interaction, "
+        "is_authentication_interaction "
         "FROM popups "
         "WHERE opener_site=? AND popup_site=?";
   // clang-format on
@@ -562,8 +567,10 @@ std::optional<PopupsStateValue> DIPSDatabase::ReadPopup(
   }
   bool is_current_interaction = statement.ColumnBool(4);
 
-  return PopupsStateValue{access_id, popup_time.value(),
-                          is_current_interaction};
+  bool is_authentication_interaction = statement.ColumnBool(5);
+
+  return PopupsStateValue{access_id, popup_time.value(), is_current_interaction,
+                          is_authentication_interaction};
 }
 
 std::vector<PopupWithTime> DIPSDatabase::ReadRecentPopupsWithInteraction(
