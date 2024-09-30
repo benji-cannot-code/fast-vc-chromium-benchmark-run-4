f3a9d71b335de3bffdfb5df29fa1c6f5913e64bf
Fredrik Söderquist <fs@opera.com>
2024-09-30 22:31:49 +0000

    spanification: WebVector
    Drop the Assign(const U* values, size_t size) in favor of the Assign()
    that handles generic containers, rewriting users to use spans.
    
    Bug: 351564777
    Change-Id: Id6750245adf6a1003fd7dd6d71d66d20b2d451b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898833
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362061}

--- third_party/blink/public/platform/web_vector.h
+++ third_party/blink/public/platform/web_vector.h
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #ifndef THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_VECTOR_H_
 #define THIRD_PARTY_BLINK_PUBLIC_PLATFORM_WEB_VECTOR_H_
 
@@ -144,11 +139,6 @@ class WebVector {
   template <typename C>
   void Assign(const C& other) {
     data_.assign(other.begin(), other.end());
-  }
-
-  template <typename U>
-  void Assign(const U* values, size_t size) {
-    data_.assign(values, values + size);
   }
 
   size_t size() const { return data_.size(); }
