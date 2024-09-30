baec86ded17d3e910913f26e42220c7facda11d1
Fredrik Söderquist <fs@opera.com>
2024-09-30 00:36:13 +0000

    spanification: WTF::StringImpl::Create8BitIfPossible
    Bug: 351564777
    Change-Id: I573c68ac85a58e691f6b48aad7533f0b72eec140
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894913
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361632}

--- third_party/blink/renderer/platform/wtf/text/string_view.cc
+++ third_party/blink/renderer/platform/wtf/text/string_view.cc
@@ -197,8 +197,8 @@ String StringView::ToString() const {
   if (StringImpl* impl = SharedImpl())
     return impl;
   if (Is8Bit())
-    return String(Characters8(), length_);
-  return StringImpl::Create8BitIfPossible(Characters16(), length_);
+    return String(Span8());
+  return StringImpl::Create8BitIfPossible(Span16());
 }
 
 AtomicString StringView::ToAtomicString() const {
