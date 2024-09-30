668c291301b91eadcdf54d7d98a8b52fd27674c4
Ritika Gupta <ritikagup@google.com>
2024-09-30 12:00:05 +0000

    [Data Sharing] Populating config params for DataSharing UI flows.
    In this CL, adding config params for DataSharing UI flows.
    These params are not final, and will change based on the requirements.
    
    Bug: b/367373591
    Change-Id: I0ce62c62c36c107391876809c4e1545ca6ff5587
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874291
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361743}

--- components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingManageUiConfig.java
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/configs/DataSharingManageUiConfig.java
@@ -5,9 +5,99 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.data_sharing.configs;
 
-/** Config class for Data Sharing Manage UI. */
+import org.chromium.base.Callback;
+import org.chromium.components.data_sharing.GroupToken;
+import org.chromium.components.sync.protocol.GroupData;
+import org.chromium.components.sync.protocol.GroupMember;
+
+/** Config class for the Data Sharing Manage UI. */
 public class DataSharingManageUiConfig {
 
-    /** Constructor */
-    public DataSharingManageUiConfig() {}
+    // --- Group related Info ---
+    private GroupToken mGroupToken;
+
+    // --- Manage Usage Config ---
+    private ManageCallback mManageCallback;
+    private DataSharingUiConfig mCommonConfig;
+
+    /** Callback interface for data sharing Manage UI events. */
+    public interface ManageCallback {
+        default void onLinkSharingToggled(boolean isLinkSharingEnabled, GroupData groupData) {}
+
+        default void onStopSharingInitiated(GroupData groupData, Callback<Boolean> readyToStop) {}
+
+        default void onStopSharingSuccess(GroupData groupData) {}
+
+        default void onMemberRemoved(GroupMember member) {}
+
+        default void onMemberBlocked(GroupMember member) {}
+
+        default void onMemberRemovedAndStopSharingInitiated(
+                GroupMember member, GroupData groupData, Callback<Boolean> readyToStop) {}
+
+        default void onMemberBlockedAndLeaveGroup(GroupMember member, GroupData groupData) {}
+
+        default void onLeaveGroup() {}
+
+        default void getDataSharingUrl(GroupToken groupToken, Callback<String> url) {}
+    }
+
+    private DataSharingManageUiConfig(Builder builder) {
+        this.mGroupToken = builder.mGroupToken;
+        this.mManageCallback = builder.mManageCallback;
+        this.mCommonConfig = builder.mCommonConfig;
+    }
+
+    public GroupToken getGroupToken() {
+        return mGroupToken;
+    }
+
+    public ManageCallback getManageCallback() {
+        return mManageCallback;
+    }
+
+    public DataSharingUiConfig getCommonConfig() {
+        return mCommonConfig;
+    }
+
+    // Builder class
+    public static class Builder {
+        private GroupToken mGroupToken;
+        private ManageCallback mManageCallback;
+        private DataSharingUiConfig mCommonConfig;
+
+        /**
+         * Sets the group token for the data sharing group.
+         *
+         * @param groupToken The token for the data sharing group includes groupId and token.
+         */
+        public Builder setGroupToken(GroupToken groupToken) {
+            this.mGroupToken = groupToken;
+            return this;
+        }
+
+        /**
+         * Sets the callback for manage UI events.
+         *
+         * @param manageCallback The callback to use for managing UI events.
+         */
+        public Builder setManageCallback(ManageCallback manageCallback) {
+            this.mManageCallback = manageCallback;
+            return this;
+        }
+
+        /**
+         * Sets the data sharing common UI config.
+         *
+         * @param commonConfig The common UI config.
+         */
+        public Builder setCommonConfig(DataSharingUiConfig commonConfig) {
+            this.mCommonConfig = commonConfig;
+            return this;
+        }
+
+        public DataSharingManageUiConfig build() {
+            return new DataSharingManageUiConfig(this);
+        }
+    }
 }
