18a933267616ee9a653ce975177b32bb65f326b1
Yao Xiao <yaoxia@chromium.org>
2024-09-30 23:17:39 +0000

    [locks] Template the LockManager class on LockGroupIdType
    This allows lock requests to be identified by types beyond just storage
    bucket IDs. This generalization supports use cases like the shared
    storage web locks proposal ([1]), which requires its own lock scope
    (i.e. per-origin) without integrating with storage buckets or the quota
    database. The term "bucket" has been replaced with the more generic
    "lock_group" throughout the code.
    
    [1] https://github.com/WICG/shared-storage/pull/199
    
    Bug: 368816425
    Change-Id: Iaabd48c4fc5e64d302b2efdadba5cac6f99ebe3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5879028
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362083}

--- content/browser/storage_partition_impl.h
+++ content/browser/storage_partition_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_BROWSER_STORAGE_PARTITION_IMPL_H_
 
 #include <stdint.h>
+
 #include <map>
 #include <memory>
 #include <set>
@@ -28,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/content_index/content_index_context_impl.h"
 #include "content/browser/dom_storage/dom_storage_context_wrapper.h"
+#include "content/browser/locks/lock_manager.h"
 #include "content/browser/notifications/platform_notification_context_impl.h"
 #include "content/browser/service_worker/service_worker_context_wrapper.h"
 #include "content/browser/worker_host/dedicated_worker_service_impl.h"
@@ -103,7 +105,6 @@ class FontAccessManager;
 class GeneratedCodeCacheContext;
 class HostZoomLevelContext;
 class InterestGroupManagerImpl;
-class LockManager;
 class NavigationStateKeepAlive;
 class PaymentAppContextImpl;
 class PrivateAggregationDataModel;
@@ -189,7 +190,8 @@ class CONTENT_EXPORT StoragePartitionImpl
   storage::DatabaseTracker* GetDatabaseTracker() override;
   DOMStorageContextWrapper* GetDOMStorageContext() override;
   storage::mojom::LocalStorageControl* GetLocalStorageControl() override;
-  LockManager* GetLockManager();  // override; TODO: Add to interface
+  LockManager<storage::BucketId>*
+  GetLockManager();  // override; TODO: Add to interface
   // TODO(crbug.com/40185706): Add this method to the StoragePartition
   // interface, which would also require making SharedStorageWorkletHostManager
   // an interface accessible in //content/public/.
@@ -742,7 +744,7 @@ class CONTENT_EXPORT StoragePartitionImpl
   scoped_refptr<storage::FileSystemContext> filesystem_context_;
   scoped_refptr<storage::DatabaseTracker> database_tracker_;
   scoped_refptr<DOMStorageContextWrapper> dom_storage_context_;
-  std::unique_ptr<LockManager> lock_manager_;
+  std::unique_ptr<LockManager<storage::BucketId>> lock_manager_;
   std::unique_ptr<indexed_db::IndexedDBControlWrapper>
       indexed_db_control_wrapper_;
   std::unique_ptr<CacheStorageControlWrapper> cache_storage_control_wrapper_;
