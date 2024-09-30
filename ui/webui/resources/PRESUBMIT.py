2da1b115b6976e15d7c7c9bc7c9330d40f5a0026
Erik Chen <erikchen@chromium.org>
2024-09-30 05:56:51 +0000

    Disable custom ESLint rules for //ui
    Most of the custom rules were unnecessary. //ui/file_manager was given a
    1-month exception. The rules were hoisted into src/.eslintrc.js for that
    time. The eqeqeq rule, which is in the process of being applied to the
    whole codebase, is migrated to toplevel .eslintrc.js.
    
    This CL was verified with
    `git cl presubmit --files "ui*.js;ui*.ts"`.
    
    I confirmed that the eqeqeq rule correctly applies to //ui by manually
    modifying a file to break the rule, and confirming that an error
    triggers in presubmit.
    
    Change-Id: Ib913f724898f4cd3befcfec9577b8e328249560b
    Bug: 369766161, 368085620
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894200
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1361670}

--- ui/webui/resources/PRESUBMIT.py
+++ ui/webui/resources/PRESUBMIT.py
@@ -87,7 +87,6 @@ def CheckNoDisallowedJS(input_api, output_api):
   # Also exempt any externs or eslint files, which must be in JS.
   EXCLUDE_PATH_SUFFIXES = [
     '_externs.js',
-    '.eslintrc.js',
   ]
 
   def allow_js(f):
