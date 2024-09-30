51e4b23133978c6c8ace2bc431a9d5acf5f081cb
Brittany Hartmire <bhartmire@google.com>
2024-09-30 19:50:14 +0000

    [QuickStart: Add SetupComplete histogram
    Test: update existing browsertest and manually test
    Change-Id: I42a30475119f3c4bd857ab23d0b34b0df02e7122
    Bug: b/370062760
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894160
    Reviewed-by: Renato Silva <rrsilva@google.com>
    Commit-Queue: Brittany Hartmire <bhartmire@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361971}

--- chrome/browser/ash/login/quickstart_controller.cc
+++ chrome/browser/ash/login/quickstart_controller.cc
@@ -676,6 +676,7 @@ void QuickStartController::HandleTransitionToQuickStartScreen() {
       UpdateUiState(UiState::SETUP_COMPLETE);
       SavePhoneInstanceID();
       bootstrap_controller_->OnSetupComplete();
+      QuickStartMetrics::RecordSetupComplete();
       return;
     }
 
