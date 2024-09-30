3c64b457dd40b9cf22c4898ade1ab8301a8a480e
Jood <jood@google.com>
2024-09-30 09:51:34 +0000

    [iOS] Remove unneeded account menu reloads when cancelling actions
    There are an unneeded account menu UI reloads, to update the primary
    account info even if the info was not updated, which looks odd.
    To resolve it, this CL stores the displayed info of the primary
    account, and uses these values to check if an update is needed.
    
    See the attached bug for repro steps.
    
    Fixed: 367305716
    Change-Id: I45f16ead32a922eb88255069e66ebcb275fa6f38
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890433
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Jood Hajeer <jood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1361711}

--- ios/chrome/browser/ui/authentication/account_menu/account_menu_view_controller.mm
+++ ios/chrome/browser/ui/authentication/account_menu/account_menu_view_controller.mm
@@ -520,6 +520,8 @@ NSString* const kCustomExpandedDetentIdentifier = @"customExpandedDetent";
   }
   [snapshot deleteItemsWithIdentifiers:accountsIdentifiersToRemove];
   [_accountMenuDataSource applySnapshot:snapshot animatingDifferences:YES];
+
+  [self.tableView reloadData];
 }
 
 #pragma mark - UIResponder
