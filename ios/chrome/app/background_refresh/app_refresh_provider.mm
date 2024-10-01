9bfe1ce91586de95f69fe8af405d6d0eac6df2ab
Mark Cogan <marq@google.com>
2024-10-01 14:54:39 +0000

    [iOS] threading for background refresh.
    This is a basic threading model for background refresh. The core ideas:
    
    (1) The thread that BGTaskSceduler invokes tasks on is used as little as
        possible, since it's not a Chromium thread.
    (2) The core logic for dispatching and tracking individual refresh jobs
        is done on the main thread.
    (3) By default each actual refresh job (which is concretely an
        implementation of the -task: method in a subclass of
        AppRefreshProvider) runs on the IO thread, but that's configurable
        for each subclass.
    
    The actual tasks are wrapped in CancellableCallbacks so they can be
    stopped if the overall refresh task is expired.
    
    Bug: 354919106
    Change-Id: I9f7de1c872e9274859268c10af9ed4c7784aa85e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5803251
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362422}

--- ios/chrome/app/background_refresh/app_refresh_provider.mm
+++ ios/chrome/app/background_refresh/app_refresh_provider.mm
@@ -5,7 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/app/background_refresh/app_refresh_provider.h"
 
+#import "base/cancelable_callback.h"
+#import "base/functional/bind.h"
 #import "base/notreached.h"
+#import "base/task/thread_pool.h"
+#import "ios/web/public/thread/web_task_traits.h"
+#import "ios/web/public/thread/web_thread.h"
 
 @interface AppRefreshProvider ()
 
@@ -14,7 +19,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-@implementation AppRefreshProvider
+@implementation AppRefreshProvider {
+  base::CancelableOnceCallback<void()> _task;
+  SEQUENCE_CHECKER(_sequenceChecker);
+}
 
 - (instancetype)init {
   if ((self = [super init])) {
@@ -25,7 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return self;
 }
 
+#pragma mark - Public properties
+
 - (base::Time)lastRun {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+
   NSDate* lastRunDate =
       [[NSUserDefaults standardUserDefaults] objectForKey:self.defaultsKey];
   // If the provider was never run before, return a time in the distant past.
@@ -36,14 +48,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setLastRun:(base::Time)lastRun {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+
   // Store the last run time.
   NSUserDefaults* defaults = [NSUserDefaults standardUserDefaults];
   [defaults setObject:lastRun.ToNSDate() forKey:self.defaultsKey];
 }
 
 - (BOOL)isDue {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+
   return base::Time::Now() - self.lastRun > self.refreshInterval;
 }
+
+- (scoped_refptr<base::SingleThreadTaskRunner>)taskThread {
+  return web::GetIOThreadTaskRunner({});
+}
+
+#pragma mark - Private properties
 
 - (NSString*)defaultsKey {
   CHECK(self.identifier.length > 0)
@@ -52,14 +74,46 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [@"AppRefreshProvider_lastRun_" stringByAppendingString:self.identifier];
 }
 
+#pragma mark - Public methods
+
+// Called on the main thread, runs tasks on (by default) the IO thread.
 - (void)handleRefreshWithCompletion:(ProceduralBlock)completion {
-  NOTREACHED() << "Subclasses of AppRefreshProvider must implement "
-                  "handleRefreshWithCompletion:";
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+  id<AppRefreshProviderTask> task = [self task];
+  _task.Reset(base::BindOnce(^{
+    [task execute];
+  }));
+
+  __weak __typeof(self) weakSelf = self;
+  base::OnceCallback callback = base::BindOnce(^{
+    [weakSelf refreshTaskFinishedWithCompletion:completion];
+  });
+
+  // `_task` has a cancelable wrapper, so `_task.callback()` is the underlying
+  // OnceCallback to execute.
+  self.taskThread->PostTaskAndReply(FROM_HERE, _task.callback(),
+                                    std::move(callback));
 }
 
 // Terminate the running task immediately.
 - (void)cancelRefresh {
-  // TODO(crbug.com/354918188): Implement cancellation.
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+
+  _task.Cancel();
+}
+
+- (id<AppRefreshProviderTask>)task {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+  NOTREACHED() << "Subclasses of AppRefreshProvider must implement -task";
+}
+
+#pragma mark - Private methods
+
+- (void)refreshTaskFinishedWithCompletion:(ProceduralBlock)completion {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
+  self.lastRun = base::Time::Now();
+
+  completion();
 }
 
 @end
--- ios/chrome/app/background_refresh/test_refresher.mm
+++ ios/chrome/app/background_refresh/test_refresher.mm
@@ -11,9 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/time/time.h"
 #import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/background_refresh/background_refresh_metrics.h"
+#import "ios/web/public/thread/web_task_traits.h"
+#import "ios/web/public/thread/web_thread.h"
+
+@interface TestRefresherTask : NSObject <AppRefreshProviderTask>
+
+- (instancetype)initWithAppState:(AppState*)appState;
+
+@end
 
 @implementation TestRefresher {
-  __weak AppState* _appState;
+  TestRefresherTask* _task;
 }
 
 @synthesize identifier = _identifier;
@@ -21,17 +29,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (instancetype)initWithAppState:(AppState*)appState {
   if ((self = [super init])) {
     _identifier = @"TestRefresher";
-    _appState = appState;
+    _task = [[TestRefresherTask alloc] initWithAppState:(AppState*)appState];
   }
   return self;
 }
 
 #pragma mark AppRefreshProvider
 
-- (void)handleRefreshWithCompletion:(ProceduralBlock)completion {
-  // TODO(crbug.com/354918403): If this provider is used outside of canary, it
-  // *must* post the logging task (which reads from AppState) to the main
-  // thread!
+// This provider runs on the main thread.
+- (scoped_refptr<base::SingleThreadTaskRunner>)taskThread {
+  return web::GetUIThreadTaskRunner({});
+}
+
+- (id<AppRefreshProviderTask>)task {
+  return _task;
+}
+
+@end
+
+@implementation TestRefresherTask {
+  SEQUENCE_CHECKER(_sequenceChecker);
+  __weak AppState* _appState;
+}
+
+- (instancetype)initWithAppState:(AppState*)appState {
+  if ((self = [super init])) {
+    _appState = appState;
+  }
+  return self;
+}
+
+- (void)execute {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(_sequenceChecker);
   InitStageDuringBackgroundRefreshActions stage =
       InitStageDuringBackgroundRefreshActions::kUnknown;
   switch (_appState.initStage) {
@@ -76,8 +105,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   base::UmaHistogramEnumeration(kInitStageDuringBackgroundRefreshHistogram,
                                 stage);
-
-  completion();
 }
 
 @end
