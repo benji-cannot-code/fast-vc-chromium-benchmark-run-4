1f5ec10e04a6fa481bec06f597b64b5c9d483777
Fiorella Barrientos Villalta <fiorellab@google.com>
2024-10-01 09:34:38 +0000

    Fix UI Bug in Eye Icon of Editable Password Combobox
    As a result of how the EditableCombobox operates with layering when hovering the eye icon is disappearing. This CL fixes it.
    
    Video of After: https://drive.google.com/file/d/14Awxz8OS2jaE_IyC5_SIfi8aE9j57abZ/view?usp=sharing&resourcekey=0-cE9yK9SDY8ubkrTHMfBgyg
    
    
    Bug: 353926181
    Change-Id: I163ac7c5e4d774aa0db624ee89b2d93e64af56f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899435
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Commit-Queue: Fiorella Barrientos Villalta <fiorellab@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362276}

--- ui/views/controls/editable_combobox/editable_password_combobox.cc
+++ ui/views/controls/editable_combobox/editable_password_combobox.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/metadata/metadata_header_macros.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/base/models/combobox_model.h"
+#include "ui/compositor/layer.h"
 #include "ui/gfx/render_text.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
@@ -52,6 +53,11 @@ std::unique_ptr<ToggleImageButton> CreateEye(
       button.get(), kEyeCrossedIcon, ui::kColorIcon, ui::kColorIconDisabled);
 
   ConfigureComboboxButtonInkDrop(button.get());
+  // We need this so the eye icon is not covered when the combo box view is
+  // hovered
+  button->SetPaintToLayer();
+  button->layer()->SetFillsBoundsOpaquely(false);
+
   return button;
 }
 
