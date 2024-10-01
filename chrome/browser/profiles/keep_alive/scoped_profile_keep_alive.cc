25bb6e2a53cd74c6cf567254f7738197a6147905
Nicolas Ouellet-Payeur <nicolaso@chromium.org>
2024-10-01 15:17:56 +0000

    Add crashkey in RemoveKeepAliveOnUIThread
    ScopedProfileKeepAlive::RemoveKeepAliveOnUIThread() is crashing due to
    a leaked SPKA. It's hard to tell *which* of our 30+ call-sites is
    leaking, as that's not captured in the crash report.
    
    Add a "ProfileKeepAlive.origin" crash key to help debug the issue.
    
    Bug: 368360956
    Change-Id: Ifb3269a901842af0f225d6a7aff9b5890ae71ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899264
    Reviewed-by: Parastoo Geranmayeh <parastoog@google.com>
    Commit-Queue: Nicolas Ouellet-Payeur <nicolaso@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362434}

--- chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.cc
+++ chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/profiles/keep_alive/scoped_profile_keep_alive.h"
 
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/browser_features.h"
@@ -13,6 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
+
+namespace {
+
+std::string GetOriginName(ProfileKeepAliveOrigin origin) {
+  std::ostringstream oss;
+  oss << origin;
+  return oss.str();
+}
+
+}  // namespace
 
 ScopedProfileKeepAlive::ScopedProfileKeepAlive(const Profile* profile,
                                                ProfileKeepAliveOrigin origin)
@@ -46,20 +57,25 @@ ScopedProfileKeepAlive::~ScopedProfileKeepAlive() {
 void ScopedProfileKeepAlive::RemoveKeepAliveOnUIThread(
     base::WeakPtr<const Profile> profile,
     ProfileKeepAliveOrigin origin) {
+  SCOPED_CRASH_KEY_STRING32("ProfileKeepAlive", "origin",
+                            GetOriginName(origin));
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   // |g_browser_process| could be nullptr if this is called during shutdown,
   // e.g. in tests.
-  if (!g_browser_process)
+  if (!g_browser_process) {
     return;
+  }
   // If the BrowserProcess is shutting down, then |profile| may be deleted
   // already. Doing anything here would be dangerous, and |profile| will be
   // deleted very soon in any case.
-  if (g_browser_process->IsShuttingDown())
+  if (g_browser_process->IsShuttingDown()) {
     return;
+  }
   // |profile_manager| can also be null in tests.
   auto* profile_manager = g_browser_process->profile_manager();
-  if (!profile_manager)
+  if (!profile_manager) {
     return;
+  }
   // TODO(crbug.com/41484323): |profile| was unexpectedly destroyed
   // early. Convert this to CHECK(profile) once the root cause is fixed.
   if (!profile) {
