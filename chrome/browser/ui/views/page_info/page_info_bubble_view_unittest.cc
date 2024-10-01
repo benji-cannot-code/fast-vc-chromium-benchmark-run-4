57325ffdb0be875e5832f746198e842e15d66463
Fiona Macintosh <fmacintosh@google.com>
2024-10-01 15:34:07 +0000

    [Page Info] Explicitly turn on `kTrackingProtection3pcdUx` in 3PCD tests
    Bug: b:333527273
    Change-Id: I37c2241c9f4085c89686b138d427cae0bf5e624b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899764
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Auto-Submit: Fiona Macintosh <fmacintosh@google.com>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362448}

--- chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
+++ chrome/browser/ui/views/page_info/page_info_bubble_view_unittest.cc
@@ -1149,8 +1149,10 @@ class PageInfoBubbleViewCookies3pcdButtonTest
       public testing::WithParamInterface<bool> {
  public:
   PageInfoBubbleViewCookies3pcdButtonTest() {
-    feature_list_.InitAndEnableFeature(
-        content_settings::features::kTrackingProtection3pcd);
+    feature_list_.InitWithFeatures(
+        {content_settings::features::kTrackingProtection3pcd,
+         privacy_sandbox::kTrackingProtection3pcdUx},
+        {});
     web_contents_helper_ =
         std::make_unique<ScopedWebContentsTestHelper>(GetParam());
   }
@@ -1241,7 +1243,9 @@ class PageInfoBubbleViewTrackingProtectionSubpageTitleTest
  public:
   PageInfoBubbleViewTrackingProtectionSubpageTitleTest() {
     feature_list_.InitWithFeatures(
-        {content_settings::features::kTrackingProtection3pcd}, {});
+        {content_settings::features::kTrackingProtection3pcd,
+         privacy_sandbox::kTrackingProtection3pcdUx},
+        {});
     web_contents_helper_ = std::make_unique<ScopedWebContentsTestHelper>(
         testing::get<2>(GetParam()));
   }
