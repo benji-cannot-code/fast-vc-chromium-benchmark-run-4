91eb7ac294f9be61ac3d04df80b66cf7448f6b92
harthuang <harthuang@google.com>
2024-10-01 20:07:03 +0000

    Enable TemporalSkipOverlaysWithRootCopyOutputRequests feature by default
    This feature is used to resolve flickering in certain
    ChromeOS devices because of overlay toggling on and
    off every other frame.
    
    Bug: 331347152, 361616140
    Change-Id: Ia2c7adb4f9e8a1170e3105d637ef9df6d8c5ff0a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899835
    Commit-Queue: Hart Huang <harthuang@google.com>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Peter McNeeley <petermcneeley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362630}

--- components/viz/common/features.cc
+++ components/viz/common/features.cc
@@ -68,7 +68,7 @@ BASE_FEATURE(kUseFrameIntervalDecider,
 
 BASE_FEATURE(kTemporalSkipOverlaysWithRootCopyOutputRequests,
              "TemporalSkipOverlaysWithRootCopyOutputRequests",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kUseMultipleOverlays,
              "UseMultipleOverlays",
