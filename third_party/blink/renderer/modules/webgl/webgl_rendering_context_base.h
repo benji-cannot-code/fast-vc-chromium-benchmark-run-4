01eae6a19fb9c41635872234e59ff6e28c0c7bd0
Colin Blundell <blundell@chromium.org>
2024-10-01 07:29:44 +0000

    [Blink] Make CopyRenderingResultsFromDrawingBuffer non-virtual
    It's called only from the CanvasRenderingContext subclasses that
    implement the method.
    
    Bug: 352263194
    Change-Id: I4586a18490ee2addd83f2227b9117d33a3174bda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899215
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362221}

--- third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
+++ third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.h
@@ -721,6 +721,8 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
       std::unique_ptr<WebGraphicsContext3DProvider>,
       const Platform::GraphicsInfo& graphics_info);
   void SetupFlags();
+  bool CopyRenderingResultsFromDrawingBuffer(CanvasResourceProvider*,
+                                             SourceDrawingBuffer);
 
   // CanvasRenderingContext implementation.
   bool IsComposited() const override { return true; }
@@ -728,8 +730,6 @@ class MODULES_EXPORT WebGLRenderingContextBase : public CanvasRenderingContext,
   bool IsOriginTopLeft() const override;
   void PageVisibilityChanged() override;
   bool PaintRenderingResultsToCanvas(SourceDrawingBuffer) override;
-  bool CopyRenderingResultsFromDrawingBuffer(CanvasResourceProvider*,
-                                             SourceDrawingBuffer) override;
   bool CopyRenderingResultsToVideoFrame(
       WebGraphicsContext3DVideoFramePool*,
       SourceDrawingBuffer,
