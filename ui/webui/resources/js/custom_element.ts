acd71cc67655683c4ef60ce2b8afc379e99dc039
dpapad <dpapad@chromium.org>
2024-10-01 01:30:28 +0000

    FlagsUI: Leverage Lit HTML expressions in FlagsAppElement.
    Bug: 40246381
    Change-Id: Ifc513bf0453fb9778590b4eba43fdfcc11716410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890627
    Reviewed-by: Teresa Mao <temao@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362140}

--- ui/webui/resources/js/custom_element.ts
+++ ui/webui/resources/js/custom_element.ts
@@ -11,7 +11,7 @@ import {assert} from './assert.js';
  * chrome/test/data/webui/js/custom_element_test.js
  */
 
-export function emptyHTML(): string|TrustedHTML {
+function emptyHTML(): string|TrustedHTML {
   return window.trustedTypes ? window.trustedTypes.emptyHTML : '';
 }
 
