13a0b362bbe10df8158744ba68e0ddd8d30584b6
Chase Hartsell <chazzy@chromium.org>
2024-10-01 18:27:57 +0000

    [Compare] Deflake loading state tests
    Previously, the loading state tests were flaky because the
    ResizeObserver callback that displays the last column gradient had not
    always triggered before the visibility of the gradient was checked.
    Using a ResizeObserver in the test to check when the container is
    resized ensures the check occurs after the callback.
    
    Bug: 370155343
    Change-Id: I2f4db9b81aee13df2a29b30a17fc6d4fbe532f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900498
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: Chase Hartsell <chazzy@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362559}

--- chrome/test/data/webui/commerce/product_specifications/loading_state_test.ts
+++ chrome/test/data/webui/commerce/product_specifications/loading_state_test.ts
@@ -18,6 +18,19 @@ suite('LoadingStateTest', () => {
     document.body.appendChild(loadingElement);
   });
 
+  function resizeContainer(width: number): Promise<void> {
+    // A ResizeObserver is used to ensure the ResizeObserver callback that shows
+    // the last column gradient has completed.
+    return new Promise<void>(resolve => {
+      const observer = new ResizeObserver(() => {
+        resolve();
+        observer.unobserve(loadingElement.$.loadingContainer);
+      });
+      observer.observe(loadingElement.$.loadingContainer);
+      loadingElement.$.loadingContainer.style.width = `${width}px`;
+    });
+  }
+
   test(
       'last column gradient appears when SVG is wider than its container',
       async () => {
@@ -32,7 +45,7 @@ suite('LoadingStateTest', () => {
 
         // Container should be smaller than the loading gradient, so the last
         // column gradient should appear.
-        document.body.style.width = '200px';
+        await resizeContainer(200);
         await microtasksFinished();
         assertTrue(isVisible(lastColumnGradient));
       });
