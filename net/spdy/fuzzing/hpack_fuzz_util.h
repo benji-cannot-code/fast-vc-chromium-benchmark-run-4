105fb4b6231a53cbeb4263964b50a15afd124c24
Nidhi Jaju <nidhijaju@chromium.org>
2024-10-01 02:35:09 +0000

    Roll src/net/third_party/quiche/src/ a1732d1ec..84a396b72 (14 commits)
    https://quiche.googlesource.com/quiche.git/+log/a1732d1ec714..84a396b720e9
    
    $ git log a1732d1ec..84a396b72 --date=short --no-merges --format='%ad %ae %s'
    2024-09-30 vasilvv Update WebTransport header names.
    2024-09-30 vasilvv Simplify some of the framing code.
    2024-09-30 vasilvv Record QUIC traces in moqt_simulator.
    2024-09-30 martinduke Implement MoQT Peeps and Object message changes for draft-06. This is the minimum for interoperability; this code always sends subgroup_id = 0 and ignores the incoming subgroup_id.
    2024-09-30 martinduke Update existing messages for draft-06. This is mostly turning track_namespace into a tuple.  Also generalizes Subscribe parameters.
    2024-09-26 birenroy Migrates code from `//third_party/spdy/core:spdy_protocol_lib` to `//third_party/http2/core:spdy_protocol`.
    2024-09-26 martinduke fix asan error because the log message uses the variable about to be initialized, rather than the value being assigned.
    2024-09-26 birenroy Deletes unused artifacts from //third_party/spdy.
    2024-09-26 martinduke Complete processing of RESET_STREAM_AT frames. Negotiate with transport parameters and pass the frame from QuicConnection to QuicStream.
    2024-09-26 birenroy Moves //third_party/spdy/core:http2_frame_decoder_adapter_fuzzer to //third_party/http2/core.
    2024-09-26 birenroy Migrates code from `//third_party/spdy/core:spdy_protocol_lib` to `//third_party/http2/core:spdy_protocol`.
    2024-09-26 birenroy Moves libraries from //third_party/spdy/core/hpack to //third_party/http2/hpack.
    2024-09-26 rch No public description
    2024-09-26 birenroy Migrates the old cc_fuzz_target in //third_party/spdy to the modern go/fuzztest framework.
    
    Created with:
      roll-dep src/net/third_party/quiche/src src/third_party/quic_trace/src
    
    Fix hpack file paths
    
    Change-Id: I4b6ff0ecefc32d00e5c2bf8a375ac819af4461cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901095
    Commit-Queue: Nidhi Jaju <nidhijaju@chromium.org>
    Auto-Submit: Nidhi Jaju <nidhijaju@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362159}

--- net/spdy/fuzzing/hpack_fuzz_util.h
+++ net/spdy/fuzzing/hpack_fuzz_util.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/containers/span.h"
 #include "net/third_party/quiche/src/quiche/http2/core/recording_headers_handler.h"
+#include "net/third_party/quiche/src/quiche/http2/hpack/hpack_decoder_adapter.h"
 #include "net/third_party/quiche/src/quiche/http2/hpack/hpack_encoder.h"
-#include "net/third_party/quiche/src/quiche/spdy/core/hpack/hpack_decoder_adapter.h"
 
 namespace quiche {
 class HttpHeaderBlock;
