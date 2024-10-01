94156b1c9c9ad2989e13208dabfd04bed13fb95f
Ivana Žužić <izuzic@google.com>
2024-10-01 14:06:55 +0000

    [UPMAccessLossWarning] Cache the warning type
    After export with deletion happens, the user has no more local
    passwords. The pref we now use since crrev.com/c/5872867 is updated and
    the state of Chrome no longer requires showing any UPM warnings.
    
    The ongoing export flow user journey still has to finish its flow so
    the warning type that was determined at the beginning of the flow needs
    to remain cached so the flow would have the correct steps.
    
    Change-Id: I02559e010b884fa22c58fd479c29bff8fffc6f0d
    Bug: 369076084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898917
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Ivana Žužić <izuzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362401}

--- chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordAccessLossExportFlowCoordinator.java
+++ chrome/browser/password_manager/android/java/src/org/chromium/chrome/browser/password_manager/settings/PasswordAccessLossExportFlowCoordinator.java
@@ -36,6 +36,8 @@ public class PasswordAccessLossExportFlowCoordinator
     private final Runnable mChromeShutDownRunnable;
     private final PasswordAccessLossExportDialogCoordinator mExportDialogCoordinator;
 
+    private final @PasswordAccessLossWarningType int mWarningType;
+
     public PasswordAccessLossExportFlowCoordinator(
             FragmentActivity activity,
             Profile profile,
@@ -43,11 +45,11 @@ public class PasswordAccessLossExportFlowCoordinator
         mActivity = activity;
         mProfile = profile;
         mModalDialogManagerSupplier = modalDialogManagerSupplier;
+        mWarningType = getAccessLossWarningType();
         // In case the warning is prompted for the NEW_GMS_CORE_MIGRATION_FAILED case, the user will
         // be redirected to the import flow in GMS Core. Therefore, Chrome should be restarted
         // instead of shut down so that it doesn't interfere with GMS Core opening.
-        boolean shouldRestartChrome =
-                getAccessLossWarningType() == PasswordAccessLossWarningType.NO_GMS_CORE;
+        boolean shouldRestartChrome = mWarningType == PasswordAccessLossWarningType.NO_GMS_CORE;
         mChromeShutDownRunnable = () -> ApplicationLifetime.terminate(shouldRestartChrome);
         mExportDialogCoordinator =
                 new PasswordAccessLossExportDialogCoordinator(mActivity, mProfile, this);
@@ -63,6 +65,7 @@ public class PasswordAccessLossExportFlowCoordinator
         mActivity = activity;
         mProfile = profile;
         mModalDialogManagerSupplier = modalDialogManagerSupplier;
+        mWarningType = getAccessLossWarningType();
         mChromeShutDownRunnable = chromeShutDownRunnable;
         mExportDialogCoordinator = exportDialogCoordinator;
     }
@@ -90,8 +93,7 @@ public class PasswordAccessLossExportFlowCoordinator
 
     @Override
     public void onPasswordsDeletionFinished() {
-        if (getAccessLossWarningType()
-                != PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED) {
+        if (mWarningType != PasswordAccessLossWarningType.NEW_GMS_CORE_MIGRATION_FAILED) {
             mChromeShutDownRunnable.run();
             return;
         }
