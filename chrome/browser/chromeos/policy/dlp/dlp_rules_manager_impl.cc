b891a782e5d22c50350987903bedd7c396c80efe
Georg Neis <neis@chromium.org>
2024-10-01 13:20:12 +0000

    Remove is_chromeos_lacros code, part 5
    Remove is_chromeos_lacros blocks from build files, remove associated
    files if they aren't used elsewhere, and merge is_chromeos_ash blocks
    into the unconditional part.
    
    This CL concerns chrome/browser/chromeos/policy.
    
    Bug: b:369519665
    Change-Id: I8291f44c7a8d9bb8d0980a1974dea1cc6c2cff57
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5897761
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362374}

--- chrome/browser/chromeos/policy/dlp/dlp_rules_manager_impl.cc
+++ chrome/browser/chromeos/policy/dlp/dlp_rules_manager_impl.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/dlp/dlp_files_controller_ash.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/policy/dlp/data_transfer_dlp_controller.h"
-#include "chrome/browser/chromeos/policy/dlp/dlp_files_controller_lacros.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_policy_constants.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_rules_manager.h"
 #include "chrome/browser/chromeos/policy/dlp/dlp_scoped_file_access_delegate.h"
