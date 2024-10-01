371827ed738ef93e2186c7462e9f5a2599c998d2
Keita Suzuki <suzukikeita@chromium.org>
2024-10-01 07:45:11 +0000

    Update fetchStart in resource timing when cache is used
    This CL updates fetchStart in resource timing when ServiceWorker static
    routing API resource timing is used with cache as the actual source
    type. This is to be compatible with other fields.
    
    Explainer: https://github.com/WICG/service-worker-static-routing-api/blob/main/resource-timing-api.md
    Bug: 41496865
    Change-Id: I7c9b90f8be8cd7bef5d62edafb32b9f41448f18a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900017
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362229}

--- third_party/blink/renderer/core/timing/performance_resource_timing.cc
+++ third_party/blink/renderer/core/timing/performance_resource_timing.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/notreached.h"
 #include "services/network/public/mojom/service_worker_router_info.mojom-blink-forward.h"
+#include "third_party/blink/public/common/features_generated.h"
 #include "third_party/blink/public/mojom/fetch/fetch_api_request.mojom-blink.h"
 #include "third_party/blink/public/mojom/timing/performance_mark_or_measure.mojom-blink.h"
 #include "third_party/blink/public/mojom/timing/resource_timing.mojom-blink-forward.h"
@@ -265,6 +266,19 @@ DOMHighResTimeStamp PerformanceResourceTiming::fetchStart() const {
 
   if (DOMHighResTimeStamp worker_ready_time = WorkerReady())
     return worker_ready_time;
+
+  // If the fetch came from service worker static routing API and the actual
+  // source type is cache, we will not have a fetch start. For compatibility,
+  // we set this to responseStart (as written in explainer
+  // https://github.com/WICG/service-worker-static-routing-api/blob/main/resource-timing-api.md
+  // ).
+  if (base::FeatureList::IsEnabled(
+          features::kServiceWorkerStaticRouterTimingInfo) &&
+      info_->service_worker_router_info &&
+      info_->service_worker_router_info->actual_source_type ==
+          network::mojom::ServiceWorkerRouterSourceType::kCache) {
+    return responseStart();
+  }
 
   return PerformanceEntry::startTime();
 }
