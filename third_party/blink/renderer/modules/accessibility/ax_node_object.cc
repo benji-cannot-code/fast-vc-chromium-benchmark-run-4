dde156fc8fdca9876c7147e95607743dd4f27515
Adithya Srinivasan <adithyas@chromium.org>
2024-10-01 15:35:46 +0000

    Fix incomplete casts in ax_node_object.cc
    In order to handle both <a> and <area> elements, these lines should
    convert to HTMLAnchorElementBase.
    
    Bug: 369219144
    Change-Id: I5cfa47c262d09223fa8c331f78ce621136ac646d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900073
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362450}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -2677,8 +2677,7 @@ bool AXNodeObject::IsLinked() const {
     return false;
   }
 
-  // TODO(crbug.com/369219144): Should this be DynamicTo<HTMLAnchorElementBase>?
-  if (auto* anchor = DynamicTo<HTMLAnchorElement>(AnchorElement())) {
+  if (auto* anchor = DynamicTo<HTMLAnchorElementBase>(AnchorElement())) {
     return !anchor->Href().IsEmpty();
   }
   return false;
@@ -3371,8 +3370,7 @@ const AtomicString& AXNodeObject::EffectiveTarget() const {
   // submitted, and could also be overridden by a "formTarget" attribute on e.g.
   // a form's submit button. However, screen reader users have no need to know
   // to which target (browser context) a form would be submitted.
-  // TODO(crbug.com/369219144): Should this be DynamicTo<HTMLAnchorElementBase>?
-  const auto* anchor = DynamicTo<HTMLAnchorElement>(GetNode());
+  const auto* anchor = DynamicTo<HTMLAnchorElementBase>(GetNode());
   if (anchor) {
     const AtomicString self_value("_self");
     const AtomicString& effective_target = anchor->GetEffectiveTarget();
