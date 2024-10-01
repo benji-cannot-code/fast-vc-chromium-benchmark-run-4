77c745c8588321f7466cd9cdc252301d761da293
Jason Zhang <jiajunz@google.com>
2024-10-01 21:50:47 +0000

    ts-migration(part3): convert network_proxy_test.js file to typescript
    Bug: b/367735238
    Change-Id: Ia712a3fe2316b4284a8cb2971aeb04c23098bc83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900672
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362694}

--- ash/webui/common/resources/network/network_proxy.ts
+++ ash/webui/common/resources/network/network_proxy.ts
@@ -536,4 +536,10 @@ export class NetworkProxyElement extends NetworkProxyElementBase {
   }
 }
 
+declare global {
+  interface HTMLElementTagNameMap {
+    [NetworkProxyElement.is]: NetworkProxyElement;
+  }
+}
+
 customElements.define(NetworkProxyElement.is, NetworkProxyElement);
