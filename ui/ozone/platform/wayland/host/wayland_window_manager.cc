c5baf474a242ed3cf50be31a9fcb820e2014f6c6
Nick Diego Yamane <nickdiego@igalia.com>
2024-10-01 22:30:36 +0000

    wayland ui scaling: sanitize font scale value
    This is part of the set of changes required to support Desktop
    Environment's "Large Text" accessibility setting in Chromium's Wayland
    backend. It actually can also be triggered by other system tools, such
    as, Gnome Tweaks app as well as gsettings command line tool in Gnome
    ecosystem.
    
    Design doc:
    https://docs.google.com/document/d/16kwPm_S0dmxe4kSgKY0_PA7aFvkFcftx6AMzNqT8Nfg
    
    Clamp font scale values before setting as ui scale to 0.5 .. 3.0, which
    avoids arbitrary values coming from the host system's settings, as they
    might result in large GPU resources allocation, etc.
    
    The follow command line flags must be used to enable the feature:
    
    `gtk-version=4 --enable-features=WaylandPerSurfaceScale,WaylandUiScale`
    
    R=fanzhoug, orko@igalia.com
    
    Bug: 40856031
    Test: Automated: CQ (ozone_unittests)
    Change-Id: I4f7c3737fbd50b673ba6bf042d70f2b79758dcfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901279
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Reviewed-by: Orko Garai <orko@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1362709}

--- ui/ozone/platform/wayland/host/wayland_window_manager.cc
+++ ui/ozone/platform/wayland/host/wayland_window_manager.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/ozone/platform/wayland/host/wayland_window_manager.h"
 
+#include <algorithm>
+
+#include "base/check.h"
 #include "base/observer_list.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom.h"
 #include "ui/ozone/platform/wayland/host/wayland_connection.h"
@@ -282,6 +285,13 @@ void WaylandWindowManager::SetFontScale(float new_font_scale) {
   for (WaylandWindow* window : GetAllWindows()) {
     window->OnFontScaleFactorChanged();
   }
+}
+
+float WaylandWindowManager::GetFontScale() const {
+  constexpr float kMinFontScale = 0.5f;
+  constexpr float kMaxFontScale = 3.0f;
+  CHECK(connection_->IsUiScaleEnabled() || font_scale_ == 1.0f) << font_scale_;
+  return std::clamp(font_scale_, kMinFontScale, kMaxFontScale);
 }
 
 }  // namespace ui
