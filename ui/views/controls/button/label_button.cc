be603935037dce4f81dfba6d01ecc4a2c29baa87
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-10-01 20:21:44 +0000

    [views-ax] Removing no-op references to View::GetAccessibleNodeData
    This CL removes references to GetAccessibleNodeData that are no longer
    needed as they no longer do anything.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: I8de3e14e6049dc231fde42172674d90c1816c9e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892709
    Auto-Submit: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362640}

--- ui/views/controls/button/label_button.cc
+++ ui/views/controls/button/label_button.cc
@@ -453,10 +453,6 @@ ProposedLayout LabelButton::CalculateProposedLayout(
   return layouts;
 }
 
-void LabelButton::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  Button::GetAccessibleNodeData(node_data);
-}
-
 ui::NativeTheme::Part LabelButton::GetThemePart() const {
   return ui::NativeTheme::kPushButton;
 }
