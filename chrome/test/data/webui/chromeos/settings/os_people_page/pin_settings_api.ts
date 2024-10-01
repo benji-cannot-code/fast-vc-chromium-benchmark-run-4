ccfaf6fd3ce4c2782e27a2f46307f76099c00eb1
Hardik Goyal <hardikgoyal@google.com>
2024-10-01 13:31:51 +0000

    Make sure pin cannot be removed during pin-only setup
    BUG=b:368244506
    
    Change-Id: Ie306022e83d7cdcb7824f5028b174416ebd3650c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5882095
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Yunke Zhou <yunkez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362378}

--- chrome/test/data/webui/chromeos/settings/os_people_page/pin_settings_api.ts
+++ chrome/test/data/webui/chromeos/settings/os_people_page/pin_settings_api.ts
@@ -293,6 +293,14 @@ export class PinSettingsApi implements PinSettingsApiInterface {
     return toggle !== null && toggle.checked;
   }
 
+  private isMoreButtonDisabled(): boolean {
+    const button = this.moreButton();
+    if (button === null) {
+      return true;
+    }
+    return (button as HTMLButtonElement).disabled;
+  }
+
   async assertPinAutosubmitEnabled(isEnabled: boolean): Promise<void> {
     const check = () => this.isPinAutosubmitEnabled() === isEnabled;
     await assertAsync(check);
@@ -369,5 +377,11 @@ export class PinSettingsApi implements PinSettingsApiInterface {
     await assertAsync(() => this.isPinAutosubmitEnabled() === true);
     (await retryUntilSome(() => this.autosubmitToggle())).click();
     await assertAsync(() => this.isPinAutosubmitEnabled() === false);
+  }
+
+  async assertMoreButtonDisabled(disabled: boolean): Promise<void> {
+    const check = () => this.isMoreButtonDisabled() === disabled;
+    await assertAsync(check);
+    await assertForDuration(check);
   }
 }
