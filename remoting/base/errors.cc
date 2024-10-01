8138e2ebfa41c4c452daa04ad9b695fed39d17c4
Yuwei Huang <yuweih@chromium.org>
2024-10-01 21:46:06 +0000

    [crd host] Add new error codes
     Add `LOGIN_SCREEN_NOT_SUPPORTED` and `SESSION_POLICIES_CHANGED`. The
    former is only reported on Mac so it's mapped to `kFailureUnknownError`
    for CrOS remote command.
    
    Change-Id: Id7dac59ae336ae8d6c2834cb5ddd0b67d8896c99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889664
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Reviewed-by: Jeroen Dhollander <jeroendh@google.com>
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362692}

--- remoting/base/errors.cc
+++ remoting/base/errors.cc
@@ -37,6 +37,8 @@ const NameMapElement<ErrorCode> kErrorCodeNames[] = {
     {ErrorCode::UNAUTHORIZED_ACCOUNT, "UNAUTHORIZED_ACCOUNT"},
     {ErrorCode::REAUTHZ_POLICY_CHECK_FAILED, "REAUTHZ_POLICY_CHECK_FAILED"},
     {ErrorCode::NO_COMMON_AUTH_METHOD, "NO_COMMON_AUTH_METHOD"},
+    {ErrorCode::LOGIN_SCREEN_NOT_SUPPORTED, "LOGIN_SCREEN_NOT_SUPPORTED"},
+    {ErrorCode::SESSION_POLICIES_CHANGED, "SESSION_POLICIES_CHANGED"},
 };
 
 }  // namespace
@@ -97,6 +99,10 @@ proto::ErrorCode ErrorCodeToProtoEnum(ErrorCode error) {
       return proto::ErrorCode::REAUTHORIZATION_FAILED;
     case ErrorCode::NO_COMMON_AUTH_METHOD:
       return proto::ErrorCode::NO_COMMON_AUTH_METHOD;
+    case ErrorCode::LOGIN_SCREEN_NOT_SUPPORTED:
+      return proto::ErrorCode::LOGIN_SCREEN_NOT_SUPPORTED;
+    case ErrorCode::SESSION_POLICIES_CHANGED:
+      return proto::ErrorCode::SESSION_POLICIES_CHANGED;
   }
 }
 
--- remoting/host/curtain_mode_mac.cc
+++ remoting/host/curtain_mode_mac.cc
@@ -18,8 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/login_util.h"
 #include "base/memory/ptr_util.h"
 #include "base/task/single_thread_task_runner.h"
+#include "remoting/base/errors.h"
 #include "remoting/host/client_session_control.h"
-#include "remoting/protocol/errors.h"
 
 namespace remoting {
 
@@ -89,7 +89,7 @@ class SessionWatcher : public base::RefCountedThreadSafe<SessionWatcher> {
   void RemoveEventHandler();
 
   // Disconnects the client session.
-  void DisconnectSession(protocol::ErrorCode error);
+  void DisconnectSession(ErrorCode error);
 
   // Handlers for the switch-in event.
   static OSStatus SessionActivateHandler(EventHandlerCallRef handler,
@@ -152,17 +152,16 @@ void SessionWatcher::ActivateCurtain() {
     // In case of fast user switch, there will be two host processes running,
     // one as the logged on user and another one as root. AgentProcessBroker
     // will terminate the root host process in that case.
-    // TODO: crbug.com/366071356 - Create a new error code for this situation.
     LOG(ERROR)
         << "Connecting to the console login session is not yet supported.";
-    DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+    DisconnectSession(ErrorCode::LOGIN_SCREEN_NOT_SUPPORTED);
     return;
   }
   // Try to install the switch-in handler. Do this before switching out the
   // current session so that the console session is not affected if it fails.
   if (!InstallEventHandler()) {
     LOG(ERROR) << "Failed to install the switch-in handler.";
-    DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+    DisconnectSession(ErrorCode::HOST_CONFIGURATION_ERROR);
     return;
   }
 
@@ -197,12 +196,12 @@ void SessionWatcher::ActivateCurtain() {
       // Disconnect the session since we are unable to enter curtain mode.
       LOG(ERROR) << "SACSwitchToLoginWindow unavailable - unable to enter "
                     "curtain mode.";
-      DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+      DisconnectSession(ErrorCode::HOST_CONFIGURATION_ERROR);
       return;
     }
     if (err.value() != noErr) {
       OSSTATUS_LOG(ERROR, err.value()) << "Failed to switch to login window";
-      DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+      DisconnectSession(ErrorCode::HOST_CONFIGURATION_ERROR);
       return;
     }
     if (is_headless) {
@@ -213,7 +212,7 @@ void SessionWatcher::ActivateCurtain() {
       LOG(ERROR) << "Machine is running in headless mode (no monitors "
                  << "attached), we attempted to curtain the session but "
                  << "SACSwitchToLoginWindow is likely to fail in this mode.";
-      DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+      DisconnectSession(ErrorCode::HOST_CONFIGURATION_ERROR);
       return;
     }
   }
@@ -231,7 +230,7 @@ bool SessionWatcher::InstallEventHandler() {
       &event_handler_);
   if (result != noErr) {
     event_handler_ = nullptr;
-    DisconnectSession(protocol::ErrorCode::HOST_CONFIGURATION_ERROR);
+    DisconnectSession(ErrorCode::HOST_CONFIGURATION_ERROR);
     return false;
   }
 
@@ -247,7 +246,7 @@ void SessionWatcher::RemoveEventHandler() {
   }
 }
 
-void SessionWatcher::DisconnectSession(protocol::ErrorCode error) {
+void SessionWatcher::DisconnectSession(ErrorCode error) {
   if (!caller_task_runner_->BelongsToCurrentThread()) {
     caller_task_runner_->PostTask(
         FROM_HERE,
@@ -263,8 +262,7 @@ void SessionWatcher::DisconnectSession(protocol::ErrorCode error) {
 OSStatus SessionWatcher::SessionActivateHandler(EventHandlerCallRef handler,
                                                 EventRef event,
                                                 void* user_data) {
-  static_cast<SessionWatcher*>(user_data)->DisconnectSession(
-      protocol::ErrorCode::OK);
+  static_cast<SessionWatcher*>(user_data)->DisconnectSession(ErrorCode::OK);
   return noErr;
 }
 
