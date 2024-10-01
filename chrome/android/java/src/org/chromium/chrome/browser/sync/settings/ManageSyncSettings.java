2919180ad224aeb367707290de22b3a1bb323082
Ankush Singh <ankushkush@google.com>
2024-10-01 10:11:01 +0000

    [sync] Use CallbackController for all posted tasks in ManageSyncSettings
    This helps avoid any cases where the parent activity might have been
    destroyed by the time the posted task is invoked. Consequently, this
    fixes the flakiness of testTrustedVaultKeyRetrievalForSignedInUsers.
    
    Bug: 359644250
    Change-Id: I8d6f02b98ff3c8cab1088afc1e4252736432223f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894914
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Ankush Singh <ankushkush@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362299}

--- chrome/android/java/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/sync/settings/ManageSyncSettings.java
@@ -29,6 +29,7 @@ import androidx.lifecycle.Lifecycle.State;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
 
+import org.chromium.base.CallbackController;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.IntentUtils;
 import org.chromium.base.metrics.RecordUserAction;
@@ -230,6 +231,8 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
     private SyncService.SyncSetupInProgressHandle mSyncSetupInProgressHandle;
 
     private final ObservableSupplierImpl<String> mPageTitle = new ObservableSupplierImpl<>();
+
+    private CallbackController mCallbackController = new CallbackController();
 
     /**
      * Creates an argument bundle for this fragment.
@@ -488,6 +491,10 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if (mCallbackController != null) {
+            mCallbackController.destroy();
+            mCallbackController = null;
+        }
         if (!mShouldReplaceSyncSettingsWithAccountSettings) {
             mSyncSetupInProgressHandle.close();
         }
@@ -601,7 +608,9 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
         // A change to Preference state hasn't been applied yet. Defer
         // updateSyncStateFromSelectedTypes so it gets the updated state from
         // isChecked().
-        PostTask.postTask(TaskTraits.UI_DEFAULT, this::updateSyncStateFromSelectedTypes);
+        PostTask.postTask(
+                TaskTraits.UI_DEFAULT,
+                mCallbackController.makeCancelable(this::updateSyncStateFromSelectedTypes));
         return true;
     }
 
@@ -615,7 +624,9 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
     public void syncStateChanged() {
         // This is invoked synchronously from SyncService.setSelectedTypes, postpone the
         // update to let updateSyncStateFromSelectedTypes finish saving the state.
-        PostTask.postTask(TaskTraits.UI_DEFAULT, this::updateSyncPreferences);
+        PostTask.postTask(
+                TaskTraits.UI_DEFAULT,
+                mCallbackController.makeCancelable(this::updateSyncPreferences));
     }
 
     /** IdentityManager.Observer implementation. */
@@ -673,16 +684,18 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
                 getUserSelectedTypes());
 
         // Some calls to setSelectedTypes don't trigger syncStateChanged, so schedule update here.
-        PostTask.postTask(TaskTraits.UI_DEFAULT, this::updateSyncPreferences);
+        PostTask.postTask(
+                TaskTraits.UI_DEFAULT,
+                mCallbackController.makeCancelable(this::updateSyncPreferences));
     }
 
     /**
      * Update the encryption state.
      *
-     * If sync's engine is initialized, the button is enabled and the dialog will present the
-     * valid encryption options for the user. Otherwise, any encryption dialogs will be closed
-     * and the button will be disabled because the engine is needed in order to know and
-     * modify the encryption state.
+     * <p>If sync's engine is initialized, the button is enabled and the dialog will present the
+     * valid encryption options for the user. Otherwise, any encryption dialogs will be closed and
+     * the button will be disabled because the engine is needed in order to know and modify the
+     * encryption state.
      */
     private void updateEncryptionState() {
         boolean isEngineInitialized = mSyncService.isEngineInitialized();
@@ -900,7 +913,8 @@ public class ManageSyncSettings extends ChromeBaseSettingsFragment
                                 ((ChromeSwitchPreference) preference).setChecked(true);
                                 PostTask.postTask(
                                         TaskTraits.UI_DEFAULT,
-                                        this::updateSyncStateFromSelectedTypes);
+                                        mCallbackController.makeCancelable(
+                                                this::updateSyncStateFromSelectedTypes));
                             }
                         });
         PropertyModel dialog =
