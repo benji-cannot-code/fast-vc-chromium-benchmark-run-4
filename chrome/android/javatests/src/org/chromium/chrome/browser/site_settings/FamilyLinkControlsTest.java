2dbcfdba4dfa6e2e3cebe355fe70d95fa0c1e823
Shuhei Takahashi <nya@chromium.org>
2024-10-01 10:03:34 +0000

    [Settings] Fix leaks of SettingsActivity
    FamilyLinkControlsTest starts a SettingsActivity in setUp, so it should
    finish an existing one before starting a new one. This fixes test
    failures that would be caused on enabling the single-activity mode.
    
    Bug: b/356743945
    Change-Id: Id05c0f4c8081465dfc7d31caac027aa61a23de9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898052
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362292}

--- chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/FamilyLinkControlsTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/site_settings/FamilyLinkControlsTest.java
@@ -56,7 +56,6 @@ import org.chromium.components.signin.identitymanager.ConsentLevel;
 public class FamilyLinkControlsTest {
 
     public final SigninTestRule mSigninTestRule = new SigninTestRule();
-    private SettingsActivity mSettingsActivity;
     private CoreAccountInfo mAccountInfo;
 
     @Rule
@@ -72,7 +71,10 @@ public class FamilyLinkControlsTest {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mSettingsActivity = SiteSettingsTestUtils.startSiteSettingsMenu("");
+
+        // Initialize the browser.
+        SiteSettingsTestUtils.startSiteSettingsMenu("").finish();
+
         ThreadUtils.runOnUiThreadBlocking(
                 () -> SigninCheckerProvider.get(ProfileManager.getLastUsedRegularProfile()));
         mAccountInfo = mSigninTestRule.addChildTestAccountThenWaitForSignin();
@@ -89,11 +91,11 @@ public class FamilyLinkControlsTest {
     @Test
     @SmallTest
     public void testDeletingOnDeviceDataBlockedForSupervisedUsers() {
-        mSettingsActivity =
+        SettingsActivity settingsActivity =
                 SiteSettingsTestUtils.startSiteSettingsCategory(
                         SiteSettingsCategory.Type.SITE_DATA);
         PreferenceFragmentCompat preferenceFragment =
-                (PreferenceFragmentCompat) mSettingsActivity.getMainFragment();
+                (PreferenceFragmentCompat) settingsActivity.getMainFragment();
         PreferenceScreen preferenceScreen = preferenceFragment.getPreferenceScreen();
         ChromeSwitchPreference binary_toggle = preferenceScreen.findPreference("binary_toggle");
 
@@ -114,10 +116,10 @@ public class FamilyLinkControlsTest {
                         matches(
                                 withText(
                                         containsString(
-                                                mSettingsActivity.getString(
+                                                settingsActivity.getString(
                                                         org.chromium.chrome.test.R.string
                                                                 .managed_by_your_parent)))));
-        mSettingsActivity.finish();
+        settingsActivity.finish();
     }
 
     @Test
@@ -131,16 +133,16 @@ public class FamilyLinkControlsTest {
                         any(), eq(ContentSettingsType.COOKIES)))
                 .thenReturn(false);
 
-        mSettingsActivity =
+        SettingsActivity settingsActivity =
                 SiteSettingsTestUtils.startSiteSettingsCategory(
                         SiteSettingsCategory.Type.SITE_DATA);
         PreferenceFragmentCompat preferenceFragment =
-                (PreferenceFragmentCompat) mSettingsActivity.getMainFragment();
+                (PreferenceFragmentCompat) settingsActivity.getMainFragment();
         PreferenceScreen preferenceScreen = preferenceFragment.getPreferenceScreen();
         ChromeSwitchPreference binary_toggle = preferenceScreen.findPreference("binary_toggle");
 
         // When deleting cookies are not blocked through Family Link the toggle will be enabled
         Assert.assertTrue(binary_toggle.isEnabled());
-        mSettingsActivity.finish();
+        settingsActivity.finish();
     }
 }
