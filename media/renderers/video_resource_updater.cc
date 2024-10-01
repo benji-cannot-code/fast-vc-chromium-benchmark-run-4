a507307d6d1d5ba9d6dc24e70367b5d388284d84
Dale Curtis <dalecurtis@chromium.org>
2024-10-01 22:47:55 +0000

    Add a histogram for source VideoFrame formats uploaded to viz.
    Will help us estimate impact of changes and issues which affect
    only a single format in the future. Expiry set for one year, we
    can remove it if it proves not useful then.
    
    R=evliu
    
    Bug: 370303497
    Change-Id: I02e77911e1f540d4b40f486499ae777cfd6e29c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902290
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362722}

--- media/renderers/video_resource_updater.cc
+++ media/renderers/video_resource_updater.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/memory/shared_memory_mapping.h"
 #include "base/memory/unsafe_shared_memory_region.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/stringprintf.h"
 #include "base/task/single_thread_task_runner.h"
@@ -776,6 +777,9 @@ VideoResourceUpdater::~VideoResourceUpdater() {
 
 void VideoResourceUpdater::ObtainFrameResource(
     scoped_refptr<VideoFrame> video_frame) {
+  UMA_HISTOGRAM_ENUMERATION("Media.VideoResourceUpdater.FrameFormat",
+                            video_frame->format(), PIXEL_FORMAT_MAX + 1);
+
   if (video_frame->metadata().overlay_plane_id.has_value()) {
     // This is a hole punching VideoFrame, there is nothing to display.
     overlay_plane_id_ = *video_frame->metadata().overlay_plane_id;
