0c9de16c37e9647c69c045b2432990f665fa99a0
Benjamin Williams <bwwilliams@google.com>
2024-10-01 21:18:38 +0000

    [iOS] Add Lens Shop tip experiment arms for product images
    This CL adds experimental arms to control the display of product images
    within Lens Shop tips in the Tips (Magic Stack) module on iOS. It also
    incorporates a check for the kSegmentationTipsManager enabled state when
    evaluating whether the Tips feature is enabled.
    
    Experiment Arms:
    
    * kWithProductImage: Displays the product image in the Lens Shop tip (if
      available).
    
    * kWithoutProductImage: Does not display the product image.
    
    Fixed: 370722780
    Change-Id: I86e4af2c4e2a6705794119d4edd11ea7b4eb931a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900523
    Commit-Queue: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Auto-Submit: Benjamin Williams <bwwilliams@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362680}

--- ios/chrome/browser/shared/public/features/features.mm
+++ ios/chrome/browser/shared/public/features/features.mm
@@ -62,6 +62,8 @@ BASE_FEATURE(kTipsMagicStack,
              "TipsMagicStack",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+const char kTipsLensShopExperimentType[] = "TipsLensShopExperimentType";
+
 const char kSafetyCheckNotificationsExperimentType[] =
     "SafetyCheckNotificationsExperimentType";
 
@@ -341,7 +343,16 @@ int SafetyCheckNotificationsImpressionLimit() {
 }
 
 bool IsTipsMagicStackEnabled() {
-  return base::FeatureList::IsEnabled(kTipsMagicStack);
+  return IsSegmentationTipsManagerEnabled() &&
+         base::FeatureList::IsEnabled(kTipsMagicStack);
+}
+
+TipsLensShopExperimentType TipsLensShopExperimentTypeEnabled() {
+  return static_cast<TipsLensShopExperimentType>(
+      base::GetFieldTrialParamByFeatureAsInt(
+          kTipsMagicStack, kTipsLensShopExperimentType,
+          /*default_value=*/
+          (int)TipsLensShopExperimentType::kWithoutProductImage));
 }
 
 BASE_FEATURE(kIOSChooseFromDrive,
