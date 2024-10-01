81040b07fb8485a205b53d50d2ebf72c02fbf666
Mike Wasserman <msw@chromium.org>
2024-10-01 19:37:48 +0000

    IDB: Encapsulate WriteToIndexedDBForTesting LevelDB use in BackingStore
    Avoid this small test-only direct LevelDB usage in BucketContext.
    Minor cleanup.
    
    Bug: 365076682
    Test: Automated
    Change-Id: Ie2fe402e1866aff2cdd5a0365d8d993b4894524d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902074
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Auto-Submit: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362610}

--- content/browser/indexed_db/instance/bucket_context.h
+++ content/browser/indexed_db/instance/bucket_context.h
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "third_party/blink/public/mojom/indexeddb/indexeddb.mojom.h"
-#include "third_party/leveldatabase/env_chromium.h"
-#include "third_party/leveldatabase/src/include/leveldb/status.h"
 
 namespace storage {
 class QuotaManagerProxy;
