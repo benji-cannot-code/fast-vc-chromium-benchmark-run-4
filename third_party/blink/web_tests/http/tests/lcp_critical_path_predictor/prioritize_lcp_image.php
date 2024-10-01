4c3600cd2f2193222d5c5ee24f29abf4b7740d4d
Yoichi Osato <yoichio@chromium.org>
2024-10-01 06:08:05 +0000

    [LCPP] Refactoring web tests
    This CL makes all LCPP tests use setupLCPTest() in
    ../lcp_critical_path_predictor/resources/common.js to initialize
    LCPPNavigationHint.
    
    Change-Id: I51a4943d946d591c42e5c7e006f2d16dc87719b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5897759
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362206}

--- third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/prioritize_lcp_image.php
+++ third_party/blink/web_tests/http/tests/lcp_critical_path_predictor/prioritize_lcp_image.php
@@ -3,13 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <script src="/priorities/resources/common.js"></script>
 <script type=module>
 import {setupLCPTest} from "./resources/common.js";
-
-if (!window.testRunner) {
-  console.log("This test requires window.testRunner.")
-}
-
-testRunner.dumpAsText();
-testRunner.waitUntilDone();
 await setupLCPTest(["lcp_image_id.pb"]);
 </script>
 <?php
