c9c2a42af7d02a1e288175a42654b5f15ef46157
Takashi Sakamoto <tasak@google.com>
2024-10-01 00:14:58 +0000

    Spanification tool: escape special characters contained by replacement text
    If replacement text contains the special characters, e.g. '@', ',',
    ':', and so on, replace them with `urllib.parse.quote`-like formatted
    string, e.g. %40', '%2C', '%3A'. So extract_edits.py easily splits
    the given text by using ',' and '@'.
    
    Bug: 359754054
    Change-Id: I1a38dd2ea7939aebcfff446b13c23e5f1a00e16a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5875412
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362105}

---
+++ tools/clang/spanify/tests/init-list-has-special-characters-original.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+void fct() {
+  // Expected rewrite:
+  // std::array<const char*, 6> buf = {
+  //     {"\\,", "+++", "%%%2C", "@", "<empty>", ":::"}};
+  // Remove extra {} from the initializer list. i.e.
+  // `std::array<const char*, 6> buf = {"\\,", ... ":::"};`
+  const char* buf[] = {"\\,", "+++", "%%%2C", "@", "<empty>", ":::"};
+  int index = 0;
+  buf[index] = nullptr;
+}
