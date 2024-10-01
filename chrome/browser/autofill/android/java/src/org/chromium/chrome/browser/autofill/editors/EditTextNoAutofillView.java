0ff98082c6adcb38611aa2afef851bcf652291d2
Nick Navarro <npnavarro@chromium.org>
2024-10-01 17:55:16 +0000

    [Autofill][Clank] Create EditTextNoAutofillView for autofill editors
    Since setImportantForAutofill is ignored starting with Android V, the
    EditText views in autofill editors should instead override
    onProvideAutofillStructure and provide no structure in order to
    suppress system autofill suggestions.
    
    Confirmed locally that this works, system autofill suggestions are not
    shown in the credit card editor.
    
    Bug: 41496585
    Change-Id: I198ac9f86d4d0de9451ca9a413b19c1887730ee4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891424
    Reviewed-by: Stephen McGruer <smcgruer@chromium.org>
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Nick Navarro <npnavarro@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362503}

---
+++ chrome/browser/autofill/android/java/src/org/chromium/chrome/browser/autofill/editors/EditTextNoAutofillView.java
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill.editors;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewStructure;
+import android.widget.EditText;
+
+/**
+ * A wrapper class around {@link EditText} to stop Android Autofill from suggesting cards. This is
+ * achieved by overriding the {@code onProvideAutofillStructure} method.
+ */
+public class EditTextNoAutofillView extends EditText {
+
+    public EditTextNoAutofillView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onProvideAutofillStructure(ViewStructure structure, int flags) {}
+}
