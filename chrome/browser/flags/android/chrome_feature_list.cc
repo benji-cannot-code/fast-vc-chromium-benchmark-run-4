79f3f2437a00b0d6c70affe55a4d4aa0b19bc637
Samar <samarchehade@google.com>
2024-10-02 13:28:24 +0000

    [Signin][Android] Add kCctSignInPrompt to signin feature map.
    The feature flag was created in https://crrev.com/c/5890811 but I forgot
    to add it to the feature map.
    
    Bug: 365544095
    Change-Id: Ia6e4fe33cc0011090fb51ba2f6de03bf17ab3842
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904016
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362997}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -378,7 +378,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
         kSegmentationPlatformAndroidHomeModuleRankerV2,
     &send_tab_to_self::kSendTabToSelfV2,
     &supervised_user::kKidFriendlyContentFeed,
-    &switches::kCctSignInPrompt,
     &switches::kForceStartupSigninPromo,
     &switches::kForceDisableExtendedSyncPromos,
     &sync_sessions::kOptimizeAssociateWindowsAndroid,
