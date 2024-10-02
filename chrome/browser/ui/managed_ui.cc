fc1a504dd738b8df3777d4dd6b26b5a0d5f1fef7
Salma Elmahallawy <esalma@google.com>
2024-10-02 18:43:26 +0000

    Updating styling for the profile menu's management notice
    This CL adjusts the styling of the management notice to match the
    UX design in the following ways:
    1.Capitalize the `m` in "managed by".
    2. Use the default underlined font for the link
    3. Use the domain name and not the label for the management
    disclosure. The old behaviour is moved behind a disabled flag
    in case this changes short-term.
    
    Bug: 370842110
    Change-Id: If02ca0908dbfb40e8c1e019c35abce3aa7bc7971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5903039
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363201}

--- chrome/browser/ui/managed_ui.cc
+++ chrome/browser/ui/managed_ui.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_attributes_entry.h"
 #include "chrome/browser/profiles/profile_attributes_storage.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/management/management_ui.h"
 #include "chrome/browser/ui/webui/management/management_ui_handler.h"
 #include "chrome/common/pref_names.h"
@@ -440,13 +441,16 @@ std::optional<std::string> GetDeviceManagerIdentity() {
   return connector->GetEnterpriseDomainManager();
 #else
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-  std::string custom_management_label =
-      g_browser_process->local_state()
-          ? g_browser_process->local_state()->GetString(
-                prefs::kEnterpriseCustomLabel)
-          : std::string();
-  if (!custom_management_label.empty()) {
-    return custom_management_label;
+  if (base::FeatureList::IsEnabled(
+          features::kEnterpriseManagementDisclaimerUsesCustomLabel)) {
+    std::string custom_management_label =
+        g_browser_process->local_state()
+            ? g_browser_process->local_state()->GetString(
+                  prefs::kEnterpriseCustomLabel)
+            : std::string();
+    if (!custom_management_label.empty()) {
+      return custom_management_label;
+    }
   }
 #endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
   // The device is managed as
@@ -474,10 +478,13 @@ std::optional<std::string> GetAccountManagerIdentity(Profile* profile) {
     return std::nullopt;
 
 #if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
-  std::string custom_management_label =
-      profile->GetPrefs()->GetString(prefs::kEnterpriseCustomLabel);
-  if (!custom_management_label.empty()) {
-    return custom_management_label;
+  if (base::FeatureList::IsEnabled(
+          features::kEnterpriseManagementDisclaimerUsesCustomLabel)) {
+    std::string custom_management_label =
+        profile->GetPrefs()->GetString(prefs::kEnterpriseCustomLabel);
+    if (!custom_management_label.empty()) {
+      return custom_management_label;
+    }
   }
 #endif
 
