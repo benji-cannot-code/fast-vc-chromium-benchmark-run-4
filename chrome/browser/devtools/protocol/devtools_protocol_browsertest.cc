600eac603d893e8c9dfcdb550ea01c27df9beb2e
Andrey Kosyakov <caseq@chromium.org>
2024-10-02 22:37:34 +0000

    DevTools: incrementCapturerCount() while taking a screenshot
    ... to prevent screenshots of inactive tabs to hang in headful chrome.
    
    Change-Id: Id4be98fa5de9a190e54b983f6232c0cb9599551d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899683
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363349}

--- chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
+++ chrome/browser/devtools/protocol/devtools_protocol_browsertest.cc
@@ -75,6 +75,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "third_party/blink/public/common/features.h"
 #include "third_party/boringssl/src/include/openssl/ssl.h"
+#include "ui/gfx/codec/png_codec.h"
 #include "url/origin.h"
 
 #if BUILDFLAG(IS_WIN)
@@ -925,6 +926,46 @@ IN_PROC_BROWSER_TEST_F(DevToolsProtocolTest, UntrustedClient) {
       "Memory.prepareForLeakDetection"));        // Implemented in content
   EXPECT_FALSE(SendCommandSync("Cast.enable"));  // Implemented in content
   EXPECT_TRUE(SendCommandSync("Accessibility.enable"));
+}
+
+class DevToolsProtocolScreenshotTest : public DevToolsProtocolTest {
+ protected:
+  void SetUp() override {
+    EnablePixelOutput();
+    DevToolsProtocolTest::SetUp();
+  }
+
+  SkBitmap CaptureScreenshot() {
+    SendCommandSync("Page.captureScreenshot");
+    CHECK(!error());
+    const std::string* base64_data = result()->FindString("data");
+    CHECK(base64_data);
+    std::string png_data;
+    CHECK(base::Base64Decode(*base64_data, &png_data));
+    SkBitmap bitmap;
+    CHECK(gfx::PNGCodec::Decode(
+        reinterpret_cast<unsigned const char*>(png_data.data()),
+        png_data.size(), &bitmap));
+    return bitmap;
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(DevToolsProtocolScreenshotTest, ScreenshotInactiveTab) {
+  static constexpr char kBluePageURL[] =
+      R"(data:text/html,<body style="background-color: blue"></body>)";
+  static constexpr char kRedPageURL[] =
+      R"(data:text/html,<body style="background-color: red"></body>)";
+  ui_test_utils::NavigateToURLBlockUntilNavigationsComplete(
+      browser(), GURL(kBluePageURL), 1);
+  EXPECT_TRUE(WaitForLoadStop(web_contents()));
+  Attach();
+  constexpr int kIndex = 1;
+  ASSERT_TRUE(AddTabAtIndex(kIndex, GURL(kRedPageURL),
+                            ui::PageTransition::PAGE_TRANSITION_TYPED));
+
+  SkBitmap bitmap = CaptureScreenshot();
+  SkColor pixel_color = bitmap.getColor(100, 100);
+  EXPECT_EQ(SK_ColorBLUE, pixel_color);
 }
 
 class ExtensionProtocolTest : public DevToolsProtocolTest {
