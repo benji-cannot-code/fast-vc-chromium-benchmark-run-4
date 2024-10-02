d296df95aa9afdc574a31727680aab499f4655bf
Robert Mader <robert.mader@collabora.com>
2024-10-02 19:21:17 +0000

    media: Rename some 'Vaapi' features to 'Accelerated'
    In preparation of patches that will allow V4L2 decoding to work by
    default on Linux.
    
    The VA-API and V4L2 backends provide essenitaly the same output -
    DMABufs in formats like NV12/P010 etc. - and face similar restrictions
    e.g. GL driver features. It therefore makes sense to have the same media
    switches and in fact some of them - most importantly
    `VaapiVideoDecodeLinux` - already apply to V4L2.
    
    Thus in order to reduce confusion and avoid code duplication, rename
    some of the `Vaapi` swiches used in common code to `Accelerated` but
    keep the `Linux` suffix, making it clearer to what they apply. Also
    ensure they are used for both APIs the same way and update the
    `vaapi.md`.
    
    Finally, add `AcceleratedVideoDecodeLinuxZeroCopyGL` to the VA-API
    documentation.
    
    Bug: b:334110478
    Change-Id: Iea58966d6ab94ce995defb1cf1099dd9ba9fae26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5893615
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Fritz Koenig <frkoenig@chromium.org>
    Commit-Queue: Fritz Koenig <frkoenig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363231}

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1019,7 +1019,7 @@ media::GpuVideoAcceleratorFactories* RenderThreadImpl::GetGpuFactories() {
 
   const bool enable_video_decode_accelerator =
 #if BUILDFLAG(IS_LINUX)
-      base::FeatureList::IsEnabled(media::kVaapiVideoDecodeLinux) &&
+      base::FeatureList::IsEnabled(media::kAcceleratedVideoDecodeLinux) &&
 #endif  // BUILDFLAG(IS_LINUX)
       !cmd_line->HasSwitch(switches::kDisableAcceleratedVideoDecode) &&
       (gpu_channel_host->gpu_feature_info()
@@ -1028,7 +1028,7 @@ media::GpuVideoAcceleratorFactories* RenderThreadImpl::GetGpuFactories() {
 
   const bool enable_video_encode_accelerator =
 #if BUILDFLAG(IS_LINUX)
-      base::FeatureList::IsEnabled(media::kVaapiVideoEncodeLinux) &&
+      base::FeatureList::IsEnabled(media::kAcceleratedVideoEncodeLinux) &&
 #else
       !cmd_line->HasSwitch(switches::kDisableAcceleratedVideoEncode) &&
 #endif  // BUILDFLAG(IS_LINUX)
