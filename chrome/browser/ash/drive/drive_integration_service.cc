ae72202e757a68d19ef86286ac4a20fdfe575c6a
Austin Tankiang <austinct@google.com>
2024-10-02 08:47:35 +0000

    Fix DriveIntegrationService not properly detecting online status
    In cases where DriveFS itself is down, DriveIntegrationService treats
    this as offline. But, the device itself could still be online, breaking
    some checks that DriveIntegrationService does. Return a device online
    state in GetDriveConnectionStatus() to fix this.
    
    Bug: b:342044261
    Change-Id: Ib1f390cdecfc72d3bf0ec0b788b02d8d49b890aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901070
    Reviewed-by: François Degros <fdegros@chromium.org>
    Commit-Queue: Austin Tankiang <austinct@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362902}

--- chrome/browser/ash/drive/drive_integration_service.cc
+++ chrome/browser/ash/drive/drive_integration_service.cc
@@ -1151,7 +1151,10 @@ void DriveIntegrationService::CreateOrDeleteBulkPinningManager() {
 
   pinning_manager_->AddObserver(this);
   pinning_manager_->SetDriveFsHost(GetDriveFsHost());
-  pinning_manager_->SetOnline(is_online_);
+
+  const ConnectionStatus status = util::GetDriveConnectionStatus(profile_);
+  pinning_manager_->SetOnline(status == util::ConnectionStatus::kConnected ||
+                              status == util::ConnectionStatus::kMetered);
 
   OnProgress(pinning_manager_->GetProgress());
   StartOrStopBulkPinning();
@@ -1799,11 +1802,12 @@ void DriveIntegrationService::GetMirrorSyncStatusForDirectory(
 }
 
 void DriveIntegrationService::OnNetworkChanged() {
-  const ConnectionStatus status = util::GetDriveConnectionStatus(profile_);
-  VLOG(1) << "OnNetworkChanged: " << status;
+  const ConnectionStatus status =
+      util::GetDriveConnectionStatus(profile_, &is_online_);
+  VLOG(1) << "OnNetworkChanged: status=" << status
+          << " is_online_=" << is_online_;
 
   using enum ConnectionStatus;
-  is_online_ = status == kMetered || status == kConnected;
 
   if (DriveFs* const drivefs = GetDriveFsInterface()) {
     const bool pause_syncing = status == kMetered;
@@ -1822,7 +1826,7 @@ void DriveIntegrationService::OnNetworkChanged() {
   }
 
   if (pinning_manager_) {
-    pinning_manager_->SetOnline(is_online_);
+    pinning_manager_->SetOnline(status == kMetered || status == kConnected);
   }
 }
 
