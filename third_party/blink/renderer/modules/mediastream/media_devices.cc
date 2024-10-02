c580d76c0710311ae4f0c39fc2feac670106a851
Simon Hangl <simonha@google.com>
2024-10-02 15:55:40 +0000

    Fix error for getAllScreensMedia in IWAs with permissions policy unset
    Change-Id: I6365c95b877bd87b812079c74bd79772b3bb45ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5881995
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Auto-Submit: Simon Hangl <simonha@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363090}

--- third_party/blink/renderer/modules/mediastream/media_devices.cc
+++ third_party/blink/renderer/modules/mediastream/media_devices.cc
@@ -82,10 +82,6 @@ BASE_FEATURE(kEnumerateDevicesRequestAudioCapabilities,
 
 namespace {
 
-const char kFeaturePolicyBlocked[] =
-    "Access to the feature \"display-capture\" is disallowed by permission "
-    "policy.";
-
 template <typename IDLResolvedType>
 class PromiseResolverCallbacks final : public UserMediaRequest::Callbacks {
  public:
@@ -617,7 +613,9 @@ ScriptPromise<IDLSequence<MediaStream>> MediaDevices::getAllScreensMedia(
   if (context->IsIsolatedContext() && !capture_allowed_by_permissions_policy) {
     resolver->RecordAndThrowDOMException(
         exception_state, DOMExceptionCode::kNotAllowedError,
-        kFeaturePolicyBlocked, UserMediaRequestResult::kNotAllowedError);
+        "Access to the feature \"all-screenscapture\" is disallowed by "
+        "permissions policy.",
+        UserMediaRequestResult::kNotAllowedError);
     return promise;
   }
 
@@ -673,7 +671,9 @@ ScriptPromise<MediaStream> MediaDevices::getDisplayMedia(
   if (!capture_allowed_by_permissions_policy) {
     resolver->RecordAndThrowDOMException(
         exception_state, DOMExceptionCode::kNotAllowedError,
-        kFeaturePolicyBlocked, UserMediaRequestResult::kNotAllowedError);
+        "Access to the feature \"display-capture\" is disallowed by "
+        "permissions policy.",
+        UserMediaRequestResult::kNotAllowedError);
     return promise;
   }
 
