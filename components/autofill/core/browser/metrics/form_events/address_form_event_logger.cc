afe4fe790ba6228414c97bcb17e9f1898e9fa6dc
Dominic Battre <battre@chromium.org>
2024-10-02 08:02:13 +0000

    Fix Autofill.Funnel.ParsedAsType.* metrics
    Chrome did not log
    Autofill.Funnel.ParsedAsType.{EmailOnly,PostalAddress}.
    This is fixed in this CL.
    
    Bug: 333085230
    Change-Id: I15dfbf50c2d0bc72ebdaa62a2669759010f00321
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898709
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362886}

--- components/autofill/core/browser/metrics/form_events/address_form_event_logger.cc
+++ components/autofill/core/browser/metrics/form_events/address_form_event_logger.cc
@@ -162,7 +162,9 @@ bool AddressFormEventLogger::HasLoggedDataToFillAvailable() const {
 
 DenseSet<FormTypeNameForLogging>
 AddressFormEventLogger::GetSupportedFormTypeNamesForLogging() const {
-  return {FormTypeNameForLogging::kAddressForm};
+  return {FormTypeNameForLogging::kAddressForm,
+          FormTypeNameForLogging::kEmailOnlyForm,
+          FormTypeNameForLogging::kPostalAddressForm};
 }
 
 DenseSet<FormTypeNameForLogging> AddressFormEventLogger::GetFormTypesForLogging(
