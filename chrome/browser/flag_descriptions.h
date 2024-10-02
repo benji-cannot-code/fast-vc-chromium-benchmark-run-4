1c24da8171e4a4d90184b6d99e7ff4433e8e74c6
Robbie Gibson <rkgibson@google.com>
2024-10-02 16:28:41 +0000

    Add feature flag for NTP Mobile promo feature
    This CL just sets up the flag so future work can use it.
    
    Bug: 370804936
    Change-Id: If5feb725a211d9fd93dec646e7d4b83c69c4bc28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900770
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363114}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3040,6 +3040,9 @@ extern const char kNtpMostRelevantTabResumptionModuleDeviceIconDescription[];
 extern const char kNtpMiddleSlotPromoDismissalName[];
 extern const char kNtpMiddleSlotPromoDismissalDescription[];
 
+extern const char kNtpMobilePromoName[];
+extern const char kNtpMobilePromoDescription[];
+
 extern const char kNtpModulesDragAndDropName[];
 extern const char kNtpModulesDragAndDropDescription[];
 
