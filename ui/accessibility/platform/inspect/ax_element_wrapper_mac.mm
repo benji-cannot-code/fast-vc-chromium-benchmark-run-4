f56330f2d722726ea90333e1a529ed1746428dee
Alexander Surkov <asurkov@igalia.com>
2024-10-02 19:07:57 +0000

    [AX Mac] infrastructure for migrating from old API
    Spin off crrev.com/c/5841311 to support migration from
    the old API to the new API:
    * Do not expose new API methods if the migration flag is off.
    * Do not expose old API attributes if the migration flag is on.
    * Ensure that AX dump tests operate on attributes independently
    of the migration flag.
    
    Bug: 363275809
    Change-Id: Ifa5514c6c0caed11a960e1f5ccb0643189e7b268
    AX-Relnotes: n/a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5896506
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Alexander Surkov <asurkov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1363220}

--- ui/accessibility/platform/inspect/ax_element_wrapper_mac.mm
+++ ui/accessibility/platform/inspect/ax_element_wrapper_mac.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/strings/pattern.h"
 #include "base/strings/sys_string_conversions.h"
+#include "ui/accessibility/platform/ax_platform_node_cocoa.h"
 #include "ui/accessibility/platform/ax_private_attributes_mac.h"
 
 // error: 'accessibilityAttributeNames' is deprecated: first deprecated in
@@ -161,6 +162,13 @@ NSPoint AXElementWrapper::Position() const {
 
 NSArray* AXElementWrapper::AttributeNames() const {
   if (IsNSAccessibilityElement()) {
+    // The NSAccessibility protocol implementation in AXPlatformNodeCocoa no
+    // longer exposes old-style attributes. Instead, it provides the
+    // internalAccessibilityAttributeNames method for backward compatibility in
+    // testing.
+    if ([node_ isKindOfClass:[AXPlatformNodeCocoa class]]) {
+      return [node_ internalAccessibilityAttributeNames];
+    }
     return [node_ accessibilityAttributeNames];
   }
 
