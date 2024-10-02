b234b0976ff66ad7fc001b875c639e0da73a4064
Dave Tapuska <dtapuska@chromium.org>
2024-10-02 22:41:26 +0000

    Convert vtt code to use bindings enumerations
    We had needless conversions from strings -> enums and back.
    
    Bug: 366411876, 370425456
    Change-Id: Ib4aab3240e070369c08bb20fbe546ddb702c38d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905442
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363352}

--- third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
+++ third_party/blink/renderer/core/html/track/vtt/vtt_cue.h
@@ -48,6 +48,7 @@ class V8UnionAutoKeywordOrDouble;
 class VTTCueBox;
 class VTTRegion;
 class VTTScanner;
+class V8DirectionSetting;
 
 using AlignSetting = V8AlignSetting::Enum;
 using VTTRegionMap = HeapHashMap<String, Member<VTTRegion>>;
@@ -108,8 +109,8 @@ class CORE_EXPORT VTTCue final : public TextTrackCue {
   VTTRegion* region() const { return region_.Get(); }
   void setRegion(VTTRegion*);
 
-  const String& vertical() const;
-  void setVertical(const String&);
+  V8DirectionSetting vertical() const;
+  void setVertical(const V8DirectionSetting&);
 
   bool snapToLines() const { return snap_to_lines_; }
   void setSnapToLines(bool);
