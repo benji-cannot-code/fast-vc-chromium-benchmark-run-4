ead0617d868840c9fca04d1d174cd97d6251b5a6
Xiyuan Xia <xiyuan@chromium.org>
2024-10-02 17:23:43 +0000

    guest: `User::GetProfilePrefs` should return OTR profile
    `ProfileUserManagerController::OnProfileAdded` is called with
    the original profile. Guest users should be using the primary
    OTR profile out of the profile. This CL ensures the correct
    Profile is used to seed User `profile_prefs_`.
    
    Bug: 370678159
    Change-Id: I5a2fec1c40ec0c07d74e0cab2bd142adfc782c6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902129
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363151}

--- chrome/browser/ash/login/users/profile_user_manager_controller.cc
+++ chrome/browser/ash/login/users/profile_user_manager_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/session_manager/core/session_manager.h"
 #include "components/user_manager/user.h"
 #include "components/user_manager/user_manager.h"
+#include "components/user_manager/user_type.h"
 
 namespace ash {
 
@@ -64,8 +65,17 @@ void ProfileUserManagerController::OnProfileAdded(Profile* profile) {
   // TODO(crbug.com/40225390): Use ash::AnnotatedAccountId::Get(), when
   // it gets fully ready for tests.
   user_manager::User* user = ProfileHelper::Get()->GetUserByProfile(profile);
-  if (user && user_manager_->OnUserProfileCreated(user->GetAccountId(),
-                                                  profile->GetPrefs())) {
+  if (!user) {
+    return;
+  }
+
+  // Guest users should use OTR profiles.
+  if (user->GetType() == user_manager::UserType::kGuest) {
+    profile = profile->GetPrimaryOTRProfile(/*create_if_needed=*/true);
+  }
+
+  if (user_manager_->OnUserProfileCreated(user->GetAccountId(),
+                                          profile->GetPrefs())) {
     // Add observer for graceful shutdown of User on Profile destruction.
     auto observation =
         std::make_unique<base::ScopedObservation<Profile, ProfileObserver>>(
