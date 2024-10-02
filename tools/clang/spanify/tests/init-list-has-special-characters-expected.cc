c4ad869e87a5621a2055995645f05da9ca8ae3fa
Takashi Sakamoto <tasak@google.com>
2024-10-02 05:02:44 +0000

    Spanification tool: remove unnecessary extra brace from std::array<>'s initializer list.
    We need to add extra braces when rewriting an array of structs,
    classes, or arrays. For example,
      struct A {int val;};
      A buf[] = {{1}, {2}};
    should be:
      struct A {int val;};
      std::array<A, 2> buf = {{{1}, {2}}}; // correct
      // std::array<A, 2> buf = {{1}, {2}}; // incorrect
    
    However we don't always need such extra braces. For example,
      std::string buf[] = {"a", "b", "c"};
      Aggregate buf[] = { BuildAggregate(0,1,2) };
    they should be:
      std::array<buf, 3> buf = {"a", "b", "c"};
      std::arrayAggregate, 1> buf = { BuildAggregate(0,1,2) };
    
    In the case, the inits of `InitListExpr' are not `InitListExpr`.
    (The previous example, the inits are still `InitListExpr`.)
    
    And pointer type doesn't need extra braces either, e.g.
      const char* buf[] = {"a", "b", "c"};
    it should be:
      std::array<const char*, 3> buf = {"a", "b", "c"};
    
    Bug: 366313081
    Change-Id: I55978dfd633123b7c3a6e548d6d89cf8c88423c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899582
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Commit-Queue: Takashi Sakamoto <tasak@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362848}

--- tools/clang/spanify/tests/init-list-has-special-characters-expected.cc
+++ tools/clang/spanify/tests/init-list-has-special-characters-expected.cc
@@ -7,12 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 void fct() {
   // Expected rewrite:
-  // std::array<const char*, 6> buf = {
-  //     {"\\,", "+++", "%%%2C", "@", "<empty>", ":::"}};
-  // Remove extra {} from the initializer list. i.e.
-  // `std::array<const char*, 6> buf = {"\\,", ... ":::"};`
-  std::array<const char*, 6> buf = {
-      {"\\,", "+++", "%%%2C", "@", "<empty>", ":::"}};
+  // std::array<const char*, 6> buf = {"\\,", "+++",     "%%%2C",
+  //                                   "@",   "<empty>", ":::"};
+  std::array<const char*, 6> buf = {"\\,", "+++",     "%%%2C",
+                                    "@",   "<empty>", ":::"};
   int index = 0;
   buf[index] = nullptr;
 }
