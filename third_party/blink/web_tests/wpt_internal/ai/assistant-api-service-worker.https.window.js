420289f0b3ab4b27ce6b50ced23cbcef10b1c34b
Mingyu Lei <leimy@chromium.org>
2024-10-02 07:06:03 +0000

    Prompt API: rename WPTs for prompt API
    to make them consistent with the rest of WPTs.
    
    Change-Id: I7a19f3c574e68906dff2fc66d33f1f839521e2da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898288
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362873}

--- third_party/blink/web_tests/wpt_internal/ai/assistant-api-service-worker.https.window.js
+++ third_party/blink/web_tests/wpt_internal/ai/assistant-api-service-worker.https.window.js
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 promise_test(async () => {
   const result = await new Promise(async resolve => {
-    navigator.serviceWorker.register('test-service-worker.js');
+    navigator.serviceWorker.register(
+      'assistant-api-test-service-worker.js'
+    );
     navigator.serviceWorker.ready.then(() => {
       navigator.serviceWorker.onmessage = e => {
         resolve(e.data);
--- third_party/blink/web_tests/wpt_internal/ai/assistant-api-shared-worker.https.window.js
+++ third_party/blink/web_tests/wpt_internal/ai/assistant-api-shared-worker.https.window.js
@@ -4,7 +4,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 promise_test(async () => {
   const result = await new Promise(async resolve => {
-    const worker = new SharedWorker("test-shared-worker.js");
+    const worker = new SharedWorker(
+      "assistant-api-test-shared-worker.js"
+    );
     worker.port.onmessage = e => {
       resolve(e.data);
     }
