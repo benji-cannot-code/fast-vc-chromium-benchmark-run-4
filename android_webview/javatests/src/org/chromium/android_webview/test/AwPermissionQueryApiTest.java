5003b1627963a849c3c0eba69d788685d4e89e0a
Muyao Xu <muyaoxu@google.com>
2024-10-02 18:24:10 +0000

    [KP Permission] Fix CHECK error on permission query on Clank
    The keyboard-lock and pointer-lock permissions are only available on
    Desktop platforms. Chrome should throw an exception when these
    permissions are queried on non-dekstop platforms. Currently, it hits
    a CHECK() failure in HostContentSettingsMap::GetContentSetting().
    
    Bug: 368085769
    Change-Id: I97c667b12e7439142f5df5e0b2e13597805b3c4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892400
    Reviewed-by: Rupert Wiser <bewise@chromium.org>
    Commit-Queue: Muyao Xu <muyaoxu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363189}

--- android_webview/javatests/src/org/chromium/android_webview/test/AwPermissionQueryApiTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwPermissionQueryApiTest.java
@@ -55,7 +55,11 @@ public class AwPermissionQueryApiTest extends AwParameterizedTest {
           }).catch(e => {
             if (e instanceof TypeError && e.message.includes("is not enabled")) {
                 resultListener.postMessage("not_enabled");
-            } else {
+            }
+            else if (e instanceof TypeError && e.message.includes("isn't available on Android")) {
+                resultListener.postMessage("not_available");
+            }
+            else {
                 resultListener.postMessage("" + e);
             }
           });
@@ -213,9 +217,7 @@ public class AwPermissionQueryApiTest extends AwParameterizedTest {
         runTestCase("display-capture", "denied");
         runTestCase("idle-detection", "denied");
         runTestCase("periodic-background-sync", "denied");
-        runTestCase("keyboard-lock", "denied");
         runTestCase("push", "denied", "{\"name\": \"push\", \"userVisibleOnly\": true}");
-        runTestCase("pointer-lock", "denied");
         runTestCase(
                 "fullscreen",
                 "denied",
@@ -234,6 +236,15 @@ public class AwPermissionQueryApiTest extends AwParameterizedTest {
         runTestCase("local-fonts", "not_enabled");
         runTestCase("captured-surface-control", "not_enabled");
         runTestCase("speaker-selection", "not_enabled");
+    }
+
+    @Test
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testPermissionsNotAvailable() throws Exception {
+        // These permissions aren't available on Android.
+        runTestCase("pointer-lock", "not_available");
+        runTestCase("keyboard-lock", "not_available");
     }
 
     @Test
