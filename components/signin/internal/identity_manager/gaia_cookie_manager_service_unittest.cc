17010c4689ed4bb46eab82607c4730f36caaaf4a
Boris Sazonov <bsazonov@chromium.org>
2024-10-02 16:13:05 +0000

    [Signin] Populate id for signed out accounts in GaiaCookieManagerService
    Before this CL, GaiaCookieManagerService::InitializeListedAccountsIds
    was populating id field for signed in accounts only (including
    potentially invalid ones). This CL changes the logic so that all
    account have the id field populated now. This is needed to for future
    CLs that will add an API to retrieve all accounts in cookies, without
    separating signed in and signed out ones.
    
    Bug: 324462717
    Change-Id: Id275ba4c11296f4fa8d705e4e9a17ff77ba4f352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899195
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363104}

--- components/signin/internal/identity_manager/gaia_cookie_manager_service_unittest.cc
+++ components/signin/internal/identity_manager/gaia_cookie_manager_service_unittest.cc
@@ -672,6 +672,7 @@ TEST_F(GaiaCookieManagerServiceTest, ListAccountsFindsSignedOutAccounts) {
   signed_in_account.gaia_id = "8";
   signed_in_account.raw_email = "a@b.com";
   gaia::ListedAccount signed_out_account;
+  signed_out_account.id = CoreAccountId::FromGaiaId("9");
   signed_out_account.email = "c@d.com";
   signed_out_account.gaia_id = "9";
   signed_out_account.raw_email = "c@d.com";
@@ -857,6 +858,7 @@ TEST_F(GaiaCookieManagerServiceTest, GaiaCookieLastListAccountsDataSaved) {
   signed_in_account.gaia_id = "8";
   signed_in_account.raw_email = "a@b.com";
   gaia::ListedAccount signed_out_account;
+  signed_out_account.id = CoreAccountId::FromGaiaId("9");
   signed_out_account.email = "c@d.com";
   signed_out_account.gaia_id = "9";
   signed_out_account.raw_email = "c@d.com";
@@ -1079,11 +1081,13 @@ TEST_F(GaiaCookieManagerServiceTest, RemoveLoggedOutAccountByGaiaId) {
           kTestGaiaId1.c_str(), kTestGaiaId2.c_str()));
 
   gaia::ListedAccount account1;
+  account1.id = CoreAccountId::FromGaiaId(kTestGaiaId1);
   account1.email = "a@d.com";
   account1.gaia_id = kTestGaiaId1;
   account1.raw_email = "a@d.com";
   account1.signed_out = true;
   gaia::ListedAccount account2;
+  account2.id = CoreAccountId::FromGaiaId(kTestGaiaId2);
   account2.email = "b@d.com";
   account2.gaia_id = kTestGaiaId2;
   account2.raw_email = "b@d.com";
@@ -1131,6 +1135,7 @@ TEST_F(GaiaCookieManagerServiceTest,
   helper.ForceOnCookieChangeProcessing();
 
   gaia::ListedAccount account;
+  account.id = CoreAccountId::FromGaiaId(kTestGaiaId1);
   account.email = "a@d.com";
   account.gaia_id = kTestGaiaId1;
   account.raw_email = "a@d.com";
@@ -1170,6 +1175,7 @@ TEST_F(GaiaCookieManagerServiceTest,
           kTestGaiaId1.c_str()));
 
   gaia::ListedAccount account;
+  account.id = CoreAccountId::FromGaiaId(kTestGaiaId1);
   account.email = "a@d.com";
   account.gaia_id = kTestGaiaId1;
   account.raw_email = "a@d.com";
