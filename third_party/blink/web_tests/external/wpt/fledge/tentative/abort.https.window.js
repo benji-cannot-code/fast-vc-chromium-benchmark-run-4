cd512c317f772da6572bacb564a84f1da0f86b34
Maks Orlovich <morlovich@chromium.org>
2024-10-02 14:26:27 +0000

    FLEDGE: always include testdriver-vendor along with testdriver
    ...since on some configurations that's what actually provides the
    implementation. Most of the places where we included testdriver
    we don't actually use it [1] (so we could get away with missing
    -vendor), but it's likely we will need it everywhere, so
    I think I would rather fix it than remove it.
    
    [1] It's only used for deleteAllCookies for now.
    
    Change-Id: Icf8f8e9f18cd08ce06b775412ccb08aa9153fe58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905378
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363024}

--- third_party/blink/web_tests/external/wpt/fledge/tentative/abort.https.window.js
+++ third_party/blink/web_tests/external/wpt/fledge/tentative/abort.https.window.js
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
 // META: script=/common/utils.js
 // META: script=resources/fledge-util.sub.js
 // META: timeout=long
