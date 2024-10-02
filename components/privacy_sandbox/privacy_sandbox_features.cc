5853edb1a6047b8ca3d10660a3229c024751c6b8
Aashna Sheth <aashnas@google.com>
2024-10-02 15:18:43 +0000

    [NoticeCollision] Add feature to enable addition of privacy sandbox notice to queue. Disabled by default.
    Bug: b:356409285
    
    Change-Id: Ia08adb9203344f6983fff4f568c85b086aed39c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900528
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363059}

--- components/privacy_sandbox/privacy_sandbox_features.cc
+++ components/privacy_sandbox/privacy_sandbox_features.cc
@@ -243,6 +243,10 @@ BASE_FEATURE(kPrivacySandboxPrivacyPolicy,
              "PrivacySandboxPrivacyPolicy",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kPrivacySandboxNoticeQueue,
+             "PrivacySandboxNoticeQueue",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kPrivacySandboxSentimentSurvey,
              "PrivacySandboxSentimentSurvey",
              base::FEATURE_DISABLED_BY_DEFAULT);
