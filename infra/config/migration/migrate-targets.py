44c80e23e3fa290930179b42844398bdc2fe941a
Garrett Beaty <gbeaty@chromium.org>
2024-10-02 17:47:01 +0000

    Fix issues in migration scripts found by type-checker.
    * TestValueBuilder was accidentally overriding final method output
      because it declared an attribute named output, this was renamed to
      test_output.
    * In handling the skip_merge_script, the compuite_edits function was
      assigning a bool when setting the use_android_merge_script_by_default
      item, but either a string or ValueBuilder was required.
    
    Change-Id: I50e8f0d691d34c84bbb56d14ea7019bbf3eb512c
    Bug: 40258588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906138
    Commit-Queue: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Wanda Mora <morawand@google.com>
    Commit-Queue: Wanda Mora <morawand@google.com>
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363162}

--- infra/config/migration/migrate-targets.py
+++ infra/config/migration/migrate-targets.py
@@ -238,7 +238,7 @@ def _compute_edits(
 
       case 'skip_merge_script':
         if value:
-          settings_builder['use_android_merge_script_by_default'] = False
+          settings_builder['use_android_merge_script_by_default'] = str(False)
 
       case _:
         raise Exception(f'unhandled key in builder config: "{key}"')
