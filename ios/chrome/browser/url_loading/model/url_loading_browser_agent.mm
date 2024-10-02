d6a49e3f0e5a4c4b78dea295bbccff6c7d06fbe7
Justin Cohen <justincohen@chromium.org>
2024-10-02 20:02:33 +0000

    ios: Add dwc to inducebrowsercrashforrealz.
    Add a URL parameter to dump the stack trace without crashing.
    
    Change-Id: Iecdb907a1b0c24dfab4f13ca4396e59b16434f30
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5903837
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363252}

--- ios/chrome/browser/url_loading/model/url_loading_browser_agent.mm
+++ ios/chrome/browser/url_loading/model/url_loading_browser_agent.mm
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/url_loading/model/url_loading_browser_agent.h"
 
 #import "base/compiler_specific.h"
+#import "base/debug/dump_without_crashing.h"
 #import "base/immediate_crash.h"
 #import "base/strings/string_number_conversions.h"
 #import "base/task/thread_pool.h"
@@ -62,6 +63,13 @@ NOINLINE void InduceBrowserCrash(const GURL& url) {
     if (base::StringToInt(delay_string, &delay) && delay > 0) {
       sleep(delay);
     }
+  }
+
+  std::string dump_without_crashing;
+  if (net::GetValueForKeyInQuery(url, "dwc", &dump_without_crashing) &&
+      (dump_without_crashing == "" || dump_without_crashing == "true")) {
+    base::debug::DumpWithoutCrashing();
+    return;
   }
 
 #if !TARGET_IPHONE_SIMULATOR  // Leaking memory does not cause UTE on simulator.
