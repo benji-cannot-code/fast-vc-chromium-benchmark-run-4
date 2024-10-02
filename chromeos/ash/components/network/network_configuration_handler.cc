127da88f762271d2a20afd745b503d56db26f96c
Irina <irfedorova@google.com>
2024-10-02 08:10:02 +0000

    network: format test helper files for future change
    This CL is a pre-work for adding kiosk WiFi exposure tests which will
    touch these files.
    
    Bug: b/365528789
    Change-Id: I6389cfb93f27aa7ed7a562274501cbc7591d8e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898959
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1362890}

--- chromeos/ash/components/network/network_configuration_handler.cc
+++ chromeos/ash/components/network/network_configuration_handler.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/dbus/shill/shill_service_client.h"
 #include "chromeos/ash/components/network/network_device_handler.h"
 #include "chromeos/ash/components/network/network_event_log.h"
+#include "chromeos/ash/components/network/network_handler.h"
 #include "chromeos/ash/components/network/network_state.h"
 #include "chromeos/ash/components/network/network_state_handler.h"
 #include "chromeos/ash/components/network/shill_property_util.h"
