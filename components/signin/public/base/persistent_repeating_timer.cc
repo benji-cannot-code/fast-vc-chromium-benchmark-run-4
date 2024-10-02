31ce6798c7a534b2648f17c5fea0ce97266a77f1
Marc Treib <treib@chromium.org>
2024-10-02 09:02:20 +0000

    Signin: Clean up kPersistentRepeaterTimerUseWallClock
    This was a kill switch for a fix to signin::PersistentRepeatingTimer
    that has rolled out in M129.
    
    Bug: 356109547
    Change-Id: I5cc90bce523ba446d3f7bca50bcaab0872ca9805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898982
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1362912}

--- components/signin/public/base/persistent_repeating_timer.cc
+++ components/signin/public/base/persistent_repeating_timer.cc
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace signin {
 
-// TODO(crbug.com/356109547): Remove this kill switch after M129 has rolled out.
-BASE_FEATURE(kPersistentRepeaterTimerUseWallClock,
-             "PersistentRepeaterTimerUseWallClock",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 PersistentRepeatingTimer::PersistentRepeatingTimer(
     PrefService* prefs,
     const char* timer_last_update_pref_name,
@@ -29,37 +24,25 @@ PersistentRepeatingTimer::PersistentRepeatingTimer(
       delay_(delay),
       user_task_(task),
       clock_(clock ? *clock : *base::DefaultClock::GetInstance()),
-      wall_timer_(&clock_.get(), tick_clock),
-      tick_timer_(tick_clock) {}
+      wall_timer_(&clock_.get(), tick_clock) {}
 
 PersistentRepeatingTimer::~PersistentRepeatingTimer() = default;
 
 void PersistentRepeatingTimer::Start() {
-  if (wall_timer_.IsRunning() || tick_timer_.IsRunning()) {
+  if (wall_timer_.IsRunning()) {
     return;  // Already started.
   }
 
-  if (base::FeatureList::IsEnabled(kPersistentRepeaterTimerUseWallClock)) {
-    const base::Time desired_run_time = GetLastFired() + delay_;
-    if (desired_run_time <= clock_->Now()) {
-      OnTimerFired();
-    } else {
-      wall_timer_.Start(FROM_HERE, desired_run_time,
-                        base::BindOnce(&PersistentRepeatingTimer::OnTimerFired,
-                                       base::Unretained(this)));
-    }
+  const base::Time desired_run_time = GetLastFired() + delay_;
+  if (desired_run_time <= clock_->Now()) {
+    OnTimerFired();
   } else {
-    const base::TimeDelta time_since_update =
-        base::Time::Now() - GetLastFired();
-    if (time_since_update >= delay_) {
-      OnTimerFired();
-    } else {
-      tick_timer_.Start(FROM_HERE, delay_ - time_since_update,
-                        base::BindOnce(&PersistentRepeatingTimer::OnTimerFired,
-                                       base::Unretained(this)));
-    }
+    wall_timer_.Start(FROM_HERE, desired_run_time,
+                      base::BindOnce(&PersistentRepeatingTimer::OnTimerFired,
+                                     base::Unretained(this)));
   }
-  DCHECK(wall_timer_.IsRunning() || tick_timer_.IsRunning());
+
+  DCHECK(wall_timer_.IsRunning());
 }
 
 base::Time PersistentRepeatingTimer::GetLastFired() {
@@ -71,7 +54,7 @@ void PersistentRepeatingTimer::SetLastFiredNow() {
 }
 
 void PersistentRepeatingTimer::OnTimerFired() {
-  DCHECK(!wall_timer_.IsRunning() && !tick_timer_.IsRunning());
+  DCHECK(!wall_timer_.IsRunning());
   SetLastFiredNow();
   user_task_.Run();
   Start();
