d8609fbf7c84d4ecc283c5f290a4bc814ade30af
Nick Diego Yamane <nickdiego@igalia.com>
2024-10-03 01:51:57 +0000

    wayand ui scaling: add wayland-ui-scaling chrome flag
    This is part of the set of changes required to support Desktop
    Environment's "Large Text" accessibility setting in Chromium's Wayland
    backend. It actually can also be triggered by other system tools, such
    as, Gnome Tweaks app as well as gsettings command line tool in Gnome
    ecosystem.
    
    Design doc:
    https://docs.google.com/document/d/16kwPm_S0dmxe4kSgKY0_PA7aFvkFcftx6AMzNqT8Nfg
    
    This adds a new chrome flag so that the feature can be enabled by via
    chrome://flags/#wayland-ui-scaling as well.
    
    R=fangzhoug, thomasanderson
    
    Fixed: 40856031
    Change-Id: I578dd15cc8f94653ce6825ed11995073d68ec2cb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902562
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1363438}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4666,6 +4666,9 @@ extern const char kWaylandPerWindowScalingDescription[];
 
 extern const char kWaylandTextInputV3Name[];
 extern const char kWaylandTextInputV3Description[];
+
+extern const char kWaylandUiScalingName[];
+extern const char kWaylandUiScalingDescription[];
 #endif  // BUILDFLAG(IS_LINUX)
 
 // All views-based platforms --------------------------------------------------
