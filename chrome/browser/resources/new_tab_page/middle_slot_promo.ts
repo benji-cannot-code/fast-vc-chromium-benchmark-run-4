fd1637a7449189b54632c078a7e96d145b611925
Paul Adedeji <pauladedeji@google.com>
2024-10-03 21:19:06 +0000

    [ntp][hpp] Implement base UI for desktop to bling NTP promo
    screen/3xo6SQsPM7PJwoV
    
    Change-Id: I95569146788134cd21bd71386c03c95b74da03d0
    Bug: 371022735
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906404
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363862}

--- chrome/browser/resources/new_tab_page/middle_slot_promo.ts
+++ chrome/browser/resources/new_tab_page/middle_slot_promo.ts
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './mobile_promo.js';
+
 import {CrAutoImgElement} from 'chrome://resources/cr_elements/cr_auto_img/cr_auto_img.js';
 import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {assert} from 'chrome://resources/js/assert.js';
@@ -139,6 +141,7 @@ export interface MiddleSlotPromoElement {
     promoAndDismissContainer: HTMLElement,
     dismissPromoButtonToast: CrToastElement,
     dismissPromoButtonToastMessage: HTMLElement,
+    mobilePromo: HTMLElement,
     undoDismissPromoButton: HTMLElement,
   };
 }
@@ -161,6 +164,8 @@ export class MiddleSlotPromoElement extends CrLitElement {
 
   static override get properties() {
     return {
+      mobilePromoEnabled_: {type: Boolean},
+
       shownMiddleSlotPromoId_: {
         type: String,
         reflect: true,
@@ -170,11 +175,13 @@ export class MiddleSlotPromoElement extends CrLitElement {
     };
   }
 
-  private eventTracker_: EventTracker = new EventTracker();
+  protected mobilePromoEnabled_: boolean =
+      loadTimeData.getBoolean('mobilePromoEnabled');
   protected shownMiddleSlotPromoId_: string;
-  private blocklistedMiddleSlotPromoId_: string;
   private promo_: Promo;
 
+  private blocklistedMiddleSlotPromoId_: string;
+  private eventTracker_: EventTracker = new EventTracker();
   private setPromoListenerId_: number|null = null;
 
   override connectedCallback() {
@@ -206,10 +213,23 @@ export class MiddleSlotPromoElement extends CrLitElement {
     }
   }
 
+
+  // Up to one promo type can show at any given time. If the mobile promo is
+  // enabled, it should show whenever the "default" promo is hidden.
+  // Note: To avoid immediately showing a new promo after a user dismisses one,
+  // this behavior is not used in |onDismissPromoButtonClick_()| or
+  // |onUndoDismissPromoButtonClick_()|.
+  private updatePromoVisibility_(showDefaultPromo: boolean) {
+    this.$.promoAndDismissContainer.hidden = !showDefaultPromo;
+    if (this.mobilePromoEnabled_) {
+      this.$.mobilePromo.hidden = showDefaultPromo;
+    }
+  }
+
   private onPromoChange_() {
     renderPromo(this.promo_).then(promo => {
       if (!promo) {
-        this.$.promoAndDismissContainer.hidden = true;
+        this.updatePromoVisibility_(false);
       } else {
         const promoContainer =
             this.shadowRoot!.getElementById('promoContainer');
@@ -222,7 +242,7 @@ export class MiddleSlotPromoElement extends CrLitElement {
         const renderedPromoContainer = promo.container;
         assert(renderedPromoContainer);
         this.$.promoAndDismissContainer.prepend(renderedPromoContainer);
-        this.$.promoAndDismissContainer.hidden = false;
+        this.updatePromoVisibility_(true);
       }
       this.fire('ntp-middle-slot-promo-loaded');
     });
---
+++ chrome/browser/resources/new_tab_page/mobile_promo.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
+
+import {getCss} from './mobile_promo.css.js';
+import {getHtml} from './mobile_promo.html.js';
+
+/**
+ * @fileoverview This file provides a custom element for displaying a mobile
+ * promo in the NTP's middle slot.
+ */
+export class MobilePromoElement extends CrLitElement {
+  static get is() {
+    return 'ntp-mobile-promo';
+  }
+
+  static override get styles() {
+    return getCss();
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ntp-mobile-promo': MobilePromoElement;
+  }
+}
+
+customElements.define(MobilePromoElement.is, MobilePromoElement);
