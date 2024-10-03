4c18fd5039083cad6a470e7e5a4c9b8427d3ac2e
Sammie Quon <sammiequon@chromium.org>
2024-10-03 21:19:27 +0000

    coral: Send post login data to coral service
    Retrieves data from informed restore controller. The data needs to be
    updated on the service side to contain all the info we want.
    
    Adds a debugging function to CoralRequest.
    
    Test: browser_tests BirchCoralProviderTest.CollectPostLoginData
    Bug: b/365839465
    Change-Id: I2fd423e41fbcb64ed72fc7470da00e2f77f35aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901420
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363863}

--- ash/wm/coral/coral_controller.h
+++ ash/wm/coral/coral_controller.h
@@ -31,6 +31,8 @@ class ASH_EXPORT CoralRequest {
 
   const std::vector<ContentItem>& content() const { return content_; }
 
+  std::string ToString() const;
+
  private:
   // Tab/app content with arbitrary ordering.
   std::vector<ContentItem> content_;
