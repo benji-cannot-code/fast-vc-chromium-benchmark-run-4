cb855b7ceb95d1930a0f6ebd8399d9aebbf01960
Haihan Chen <haihan@google.com>
2024-10-03 16:38:55 +0000

    [File Download Access Prevention] Fix deobfuscation issue on Windows
    On Windows, after deobfuscation, the file is not being renamed from a .crdownload to its target path. This is because the delegate gets destroyed before deobfuscation completes, so the open_file_callback is never called.
    
    Changed this by moving the open_file_callback and ensure it outlives the delegate object. Updated test to catch this scenario.
    
    Bug: b/369628037
    Change-Id: I4791cca8298f862680f4458a4b692d14087c4295
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905842
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363694}

--- chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.h
+++ chrome/browser/enterprise/connectors/analysis/content_analysis_downloads_delegate.h
@@ -27,8 +27,8 @@ class ContentAnalysisDownloadsDelegate
       const std::u16string& custom_message,
       GURL custom_learn_more_url,
       bool bypass_justification_required,
-      base::OnceCallback<void()> open_file_callback,
-      base::OnceCallback<void()> discard_file_callback,
+      base::OnceClosure open_file_callback,
+      base::OnceClosure discard_file_callback,
       download::DownloadItem* download_item,
       const ContentAnalysisResponse::Result::TriggeredRule::CustomRuleMessage&
           custom_rule_message);
@@ -67,10 +67,6 @@ class ContentAnalysisDownloadsDelegate
   // Called when the user opts to open the downloaded file.
   void Open();
 
-  // Callback for when deobfuscation of the file is complete.
-  void OnDeobfuscationComplete(
-      base::expected<void, enterprise_obfuscation::Error> deobfuscation_result);
-
   // Custom message for rule.
   ContentAnalysisResponse::Result::TriggeredRule::CustomRuleMessage
       custom_rule_message_;
@@ -79,12 +75,9 @@ class ContentAnalysisDownloadsDelegate
   std::u16string custom_message_;
   GURL custom_learn_more_url_;
   bool bypass_justification_required_;
-  base::OnceCallback<void()> open_file_callback_;
-  base::OnceCallback<void()> discard_file_callback_;
+  base::OnceClosure open_file_callback_;
+  base::OnceClosure discard_file_callback_;
   raw_ptr<download::DownloadItem> download_item_;
-
-  base::WeakPtrFactory<ContentAnalysisDownloadsDelegate> weak_ptr_factory_{
-      this};
 };
 
 }  // namespace enterprise_connectors
