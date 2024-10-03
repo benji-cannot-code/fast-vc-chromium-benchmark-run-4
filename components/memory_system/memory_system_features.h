b9ad859442934d89782d5e411adc410a1019138a
Tarcisio Fischer <tarcisio.fischer@arm.com>
2024-10-03 09:43:00 +0000

    Fix Allocation Trace Recorder browser test
    The main goal was to fix the test flakyness, but there were also some
    issues because the test became outdated wrt the current implementation,
    so the test was also updated to take in consideration implementation
    changes.
    
    There's no guarantee that the flakyness is fixed, but the hypothesis is
    that it was flaky due to the fact it was using the "system-wide"
    DIR_CRASH_DUMPS path. This CL changes the test to use a more controlled
    temporary directory instead. This CL may be reverted if flakyness
    persists.
    
    Bug: 40944196
    Change-Id: I5102c123d2162df81774c8566657169af343b86e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5816873
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/main@{#1363539}

--- components/memory_system/memory_system_features.h
+++ components/memory_system/memory_system_features.h
@@ -7,10 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_MEMORY_SYSTEM_MEMORY_SYSTEM_FEATURES_H_
 
 #include "base/feature_list.h"
+#include "base/metrics/field_trial_params.h"
 
 namespace memory_system::features {
 
 BASE_DECLARE_FEATURE(kAllocationTraceRecorder);
+BASE_DECLARE_FEATURE_PARAM(bool, kAllocationTraceRecorderForceAllProcesses);
 
 }  // namespace memory_system::features
 
