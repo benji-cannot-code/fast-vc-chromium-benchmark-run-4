8e163e88f2420f2742c0ac52c6b6f32f19c8c3e5
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-10-03 09:48:04 +0000

    clay: Polish and tweak feature params
    - silent_pending_duration_millis now can disable the pending dialog
      for non-positive values.
    - The response delay introduced by dialog_timeout_millis with
      use_fake_backend is now capped at 3s instead of always being
      set to dialog_timeout_millis / 2. By default that ended up
      blocking for 30 seconds.
    
    Bug: b:355054351
    Fixed: b:370638125
    Change-Id: If8a77875254df5ff0a8daddaed133c9fb2cb388f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904042
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363543}

---
+++ components/search_engines/android/java/src/org/chromium/components/search_engines/test/util/SearchEnginesFeaturesTestUtil.java
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.search_engines.test.util;
+
+import org.chromium.base.FeatureList;
+import org.chromium.components.search_engines.SearchEnginesFeatures;
+
+import java.util.Map;
+
+public final class SearchEnginesFeaturesTestUtil {
+
+    public static void configureClayBlockingFeatureParams(Map<String, String> paramAndValue) {
+        var testFeatures = new FeatureList.TestValues();
+        testFeatures.addFeatureFlagOverride(SearchEnginesFeatures.CLAY_BLOCKING, true);
+
+        for (var entry : paramAndValue.entrySet()) {
+            testFeatures.addFieldTrialParamOverride(
+                    SearchEnginesFeatures.CLAY_BLOCKING, entry.getKey(), entry.getValue());
+        }
+
+        FeatureList.setTestValues(testFeatures);
+    }
+
+    // Block instantiation
+    private SearchEnginesFeaturesTestUtil() {}
+}
