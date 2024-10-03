e85e0a767bcafe4fbbade11e4b7617c7efbf05dd
Kenneth Russell <kbr@chromium.org>
2024-10-03 00:59:45 +0000

    Revert "Create DawnInstance to initialize the access to D3D12 shader cache"
    This reverts commit 0a72672c7039f57b1b7f5efb300a46183b077a73.
    
    Reason for revert: perf regressions in rendering.desktop, crbug.com/370595533
    
    Original change's description:
    > Create DawnInstance to initialize the access to D3D12 shader cache
    >
    > This patch manually creates a DawnInstance and attempts to request
    > D3D12 adapters to initialize the access to D3D12 shader cache before
    > the launch of Chrome sandbox.
    >
    > Bug: chromium:40700602
    > Change-Id: Ie02a2dd27adb8a9aafae5d938581ede21106eb15
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892872
    > Reviewed-by: James Forshaw <forshaw@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1361723}
    
    Bug: chromium:40700602, chromium:370595533
    Change-Id: If5ff1f21e7f81b4c09b7c2524644ca1e700cf1ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906584
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363419}

--- gpu/ipc/service/gpu_init.cc
+++ gpu/ipc/service/gpu_init.cc
@@ -83,7 +83,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if BUILDFLAG(USE_DAWN) || BUILDFLAG(SKIA_USE_DAWN)
-#include "gpu/command_buffer/service/dawn_instance.h"
 #include "third_party/dawn/include/dawn/dawn_proc.h"          // nogncheck
 #include "third_party/dawn/include/dawn/native/DawnNative.h"  // nogncheck
 #endif
@@ -852,20 +851,6 @@ bool GpuInit::InitializeAndStartSandbox(base::CommandLine* command_line,
     watchdog_init.SetGpuWatchdogPtr(watchdog_thread_.get());
   }
 
-  InitializeDawnProcs();
-
-#if BUILDFLAG(IS_WIN)
-  // We need to create a D3D12Device to enable the access to D3D12 shader cache
-  // files on many GPU drivers before the launch of the GPU sandbox (otherwise
-  // such access will always be blocked by the GPU sandbox). To achieve this
-  // goal we create a Dawn instance and attempt to request an adapter for D3D12
-  // backend so that a D3D12Device will be saved in the Dawn instance.
-  // TODO(chromium:40700602): Fix WebGPU shader cache partitioning.
-  dawn_instance_for_d3d12_shader_cache_ =
-      gpu::DawnContextProvider::CreateDawnInstanceForD3D12ShaderCache(
-          gpu_preferences_);
-#endif
-
   UMA_HISTOGRAM_ENUMERATION("GPU.GLImplementation", gl::GetGLImplementation());
 
   if (!gpu_info_.sandboxed && !attempted_startsandbox) {
@@ -873,6 +858,8 @@ bool GpuInit::InitializeAndStartSandbox(base::CommandLine* command_line,
         watchdog_thread_.get(), &gpu_info_, gpu_preferences_);
   }
   UMA_HISTOGRAM_BOOLEAN("GPU.Sandboxed", gpu_info_.sandboxed);
+
+  InitializeDawnProcs();
 
   if (gpu_preferences_.gr_context_type == GrContextType::kGraphiteDawn) {
     if (!InitializeDawn()) {
