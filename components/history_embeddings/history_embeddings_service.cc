86b7b606c9896a52ea5678461fc2ad239203a79b
Orin Jaworski <orinj@google.com>
2024-10-03 00:40:31 +0000

    Add VLOG calls to debug intent and answer data flow
    Bug: 371049023
    Change-Id: I51e4b9813cb66bfa55f9b2f155f33f6954a94636
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906699
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363410}

--- components/history_embeddings/history_embeddings_service.cc
+++ components/history_embeddings/history_embeddings_service.cc
@@ -987,6 +987,7 @@ void HistoryEmbeddingsService::OnPrimarySearchResultReady(
   //  real ML intent classifier is working.
   if (answerer_ && intent_classifier_) {
     std::string query = result.query;
+    VLOG(3) << "ComputeQueryIntent for '" << query << "'";
     intent_classifier_->ComputeQueryIntent(
         std::move(query),
         base::BindOnce(&HistoryEmbeddingsService::OnQueryIntentComputed,
@@ -1005,6 +1006,7 @@ void HistoryEmbeddingsService::OnQueryIntentComputed(
     SearchResult result,
     ComputeIntentStatus status,
     bool query_is_answerable) {
+  VLOG(3) << "OnQueryIntentComputed for '" << result.query << "'";
   const bool answerable = status == ComputeIntentStatus::SUCCESS &&
                           query_is_answerable && answerer_;
   base::UmaHistogramBoolean("History.Embeddings.QueryAnswerable", answerable);
@@ -1033,8 +1035,10 @@ void HistoryEmbeddingsService::OnQueryIntentComputed(
               index));
     }
   }
+  std::string query = result.query;
+  VLOG(3) << "ComputeAnswer for '" << query << "'";
   answerer_->ComputeAnswer(
-      result.query, std::move(context),
+      std::move(query), std::move(context),
       base::BindOnce(&HistoryEmbeddingsService::OnAnswerComputed,
                      weak_ptr_factory_.GetWeakPtr(), callback,
                      std::move(result)));
