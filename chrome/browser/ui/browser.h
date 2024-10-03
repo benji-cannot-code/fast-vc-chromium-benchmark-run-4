3d42cc1ab04dd68a97375ddaeb2f60ec30635e2f
Anantanarayanan G Iyengar <aiyengar@nvidia.com>
2024-10-03 23:42:30 +0000

    Ensure pointer lock permission works after focus loss and regain
    When switching away from a webpage via Alt+Tab or clicking on other
    applications, Chromium’s occlusion detection triggers
    `RenderWidgetHostImpl::WasHidden`. This method aborts pending pointer
    lock requests and returns a `WrongDocument` exception. This behavior
    works fine unless a permission dialog (e.g., Keyboard and Pointer Lock
    prompt) is awaiting user input.
    
    If the user switches away while the prompt is displayed and then returns
    to the browser, selecting “Allow” in the dialog has no effect, as the
    pending pointer lock was already aborted while the dialog remains
    displayed. Calling `delegate_->LostPointerLock()` triggers
    `PointerLockController::ExitExclusiveAccessIfNecessary()` and calls
    `exclusive_access_manager()->UpdateBubble(base::NullCallback())`, but it
    does not close the permission dialog managed by the permission manager.
    
    The proposed fix detects if the pointer lock prompt is awaiting input
    and keeps the pending pointer lock active. Another option considered was
    to modify the exclusive access permission manager to terminate the
    dialog, but the chosen fix was simpler.
    
    Bug: 369319584
    Test: Updated `ExclusiveAccessPermissionPromptInteractiveTest` and added
    new unit tests:
    - `RenderWidgetHostTest.WasHiddenDoesNotRejectPointerLockIfWaitingForPrompt`
    - `RenderWidgetHostTest.WasHiddenRejectsPointerLockIfNotWaitingForPrompt`
    
    and new interactive tests:
    ExclusiveAccessPermissionPromptInteractiveTest.AllowPointerLockAfterLosingFocus, PointerLockPromptCheckIsPerRenderWidgetHost
    
    BUG=369319584, 371112529
    
    Change-Id: I07921af90dae9428d1c8d9950a3fba607f9aa486
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5887019
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Anantanarayanan Iyengar US <aiyengar@nvidia.com>
    Cr-Commit-Position: refs/heads/main@{#1363945}

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -1043,6 +1043,8 @@ class Browser : public TabStripModelObserver,
                           bool user_gesture,
                           bool last_unlocked_by_target) override;
   void LostPointerLock() override;
+  bool IsWaitingForPointerLockPrompt(
+      content::WebContents* web_contents) override;
   void RequestKeyboardLock(content::WebContents* web_contents,
                            bool esc_key_locked) override;
   void CancelKeyboardLockRequest(content::WebContents* web_contents) override;
