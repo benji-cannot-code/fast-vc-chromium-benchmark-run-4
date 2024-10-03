3d5ceaf38121f8cce47f9417b4694ad01313f8b2
Samar <samarchehade@google.com>
2024-10-03 16:22:01 +0000

    [MIM][CCT] Expose kCctSignInPrompt in chrome://flags
    Bug: 365544095
    Change-Id: I69a1bbccbf23c8d39973c4e743f0036bea2f9689
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907810
    Auto-Submit: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363686}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11847,6 +11847,11 @@ const FeatureEntry kFeatureEntries[] = {
      FEATURE_VALUE_TYPE(
          autofill::features::
              kAutofillEnableCvcStorageAndFillingStandaloneFormEnhancement)},
+#if BUILDFLAG(IS_ANDROID)
+    {"cct-sign-in-prompt", flag_descriptions::kCCTSignInPromptName,
+     flag_descriptions::kCCTSignInPromptDescription, kOsAndroid,
+     FEATURE_VALUE_TYPE(switches::kCctSignInPrompt)},
+#endif
 
     {"enable-bookmarks-selected-type-on-signin-for-testing",
      flag_descriptions::kEnableBookmarksSelectedTypeOnSigninForTestingName,
