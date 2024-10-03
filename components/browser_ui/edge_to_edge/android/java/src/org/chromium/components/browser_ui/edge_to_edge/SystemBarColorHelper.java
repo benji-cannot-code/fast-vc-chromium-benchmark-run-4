174da1034745d6feeabd18b679e99ff21f7f412f
Wenyu Fu <wenyufu@chromium.org>
2024-10-03 00:29:27 +0000

    [E2E] Add SystemBarHelper interface used to change system bar colors
    Setting the ground work of setting the system bar colors during edge
    to edge.
    
    Design: go/clank-e2e-everywhere
    
    Bug: 369841366, 369635657
    Change-Id: I600cf65ef04da78e91bf26f2858df87a66bf764d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890769
    Reviewed-by: Charles Hager <clhager@google.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363408}

---
+++ components/browser_ui/edge_to_edge/android/java/src/org/chromium/components/browser_ui/edge_to_edge/SystemBarColorHelper.java
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.browser_ui.edge_to_edge;
+
+import androidx.annotation.ColorInt;
+
+import org.chromium.base.lifetime.Destroyable;
+
+/** Interface for setting system bar color. */
+public interface SystemBarColorHelper extends Destroyable {
+
+    /** Set the status bar color. */
+    void setStatusBarColor(@ColorInt int color);
+
+    /** Set the navigation bar color. */
+    void setNavigationBarColor(@ColorInt int color);
+
+    /** Set the navigation bar color. */
+    void setNavigationBarDividerColor(@ColorInt int dividerColor);
+}
