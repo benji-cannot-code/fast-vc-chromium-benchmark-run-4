baee934b0e14aa417c7d218968b5fba7c927d685
Nina Satragno <nsatragno@chromium.org>
2024-10-03 20:43:21 +0000

    [webauthn] Move focus test
    Move the interactive WebAuthn focus test and rename. This way it can be
    owned by the WebAuthn team.
    
    Fixed: 371219662
    Change-Id: I507006b2f7a25f9fe7cf69fd074205c255ce96ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906489
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Nina Satragno <nsatragno@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363837}

--- chrome/browser/webauthn/webauthn_focus_interactive_uitest.cc
+++ chrome/browser/webauthn/webauthn_focus_interactive_uitest.cc
@@ -45,13 +45,13 @@ constexpr char kNotAllowedText[] =
     "NotAllowedError: The operation is not allowed at this time because the "
     "page does not have focus.";
 
-class WebAuthFocusTest : public InteractiveBrowserTest,
-                         public AuthenticatorRequestDialogModel::Observer {
+class WebAuthnFocusTest : public InteractiveBrowserTest,
+                          public AuthenticatorRequestDialogModel::Observer {
  public:
-  WebAuthFocusTest() : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
+  WebAuthnFocusTest() : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
 
-  WebAuthFocusTest(const WebAuthFocusTest&) = delete;
-  WebAuthFocusTest& operator=(const WebAuthFocusTest&) = delete;
+  WebAuthnFocusTest(const WebAuthnFocusTest&) = delete;
+  WebAuthnFocusTest& operator=(const WebAuthnFocusTest&) = delete;
 
   void SetUpOnMainThread() override {
     host_resolver()->AddRule("*", "127.0.0.1");
@@ -90,7 +90,7 @@ class WebAuthFocusTest : public InteractiveBrowserTest,
   net::EmbeddedTestServer https_server_;
 };
 
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWhenTabIsFocused) {
+IN_PROC_BROWSER_TEST_F(WebAuthnFocusTest, SucceedWhenTabIsFocused) {
   RunTestSequence(
       InstrumentTab(kFirstTabId),
       NavigateWebContents(kFirstTabId,
@@ -102,7 +102,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWhenTabIsFocused) {
           kOkText));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabIsNotFocused) {
+IN_PROC_BROWSER_TEST_F(WebAuthnFocusTest, FailWhenTabIsNotFocused) {
   // Web Authentication requests will often trigger machine-wide indications,
   // such as a Security Key flashing for a touch. If background tabs were able
   // to trigger this, there would be a risk of user confusion since the user
@@ -127,7 +127,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabIsNotFocused) {
           kOkText));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabFocusChangesWhileRunning) {
+IN_PROC_BROWSER_TEST_F(WebAuthnFocusTest, FailWhenTabFocusChangesWhileRunning) {
   // Start the request in the foreground and open a new tab between starting and
   // finishing the request. This should fail because we don't want foreground
   // pages to be able to start a request, open a trusted site in a new
@@ -151,7 +151,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, FailWhenTabFocusChangesWhileRunning) {
           kNotAllowedText));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWithDevtoolsOpen) {
+IN_PROC_BROWSER_TEST_F(WebAuthnFocusTest, SucceedWithDevtoolsOpen) {
   RunTestSequence(
       InstrumentTab(kFirstTabId),
       NavigateWebContents(kFirstTabId,
@@ -171,7 +171,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWithDevtoolsOpen) {
           kOkText));
 }
 
-IN_PROC_BROWSER_TEST_F(WebAuthFocusTest, SucceedWithNewWindowOpen) {
+IN_PROC_BROWSER_TEST_F(WebAuthnFocusTest, SucceedWithNewWindowOpen) {
   virtual_device_factory()->mutable_state()->simulate_press_callback =
       base::BindRepeating(
           [](Browser* browser, device::VirtualFidoDevice* device) {
