b3bfe2d559af089fa2aca47148b52759c72fe201
Dave Tapuska <dtapuska@chromium.org>
2024-10-03 19:25:39 +0000

    Convert lock code to use bindings enumerations
    We had needless conversions from strings -> enums and back.
    
    Bug: 366411876, 370425456
    Change-Id: I358df6c506ab56e156367a0d78591a9e483c7b2b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904444
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363782}

--- third_party/blink/renderer/modules/locks/lock.h
+++ third_party/blink/renderer/modules/locks/lock.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/mojom/locks/lock_manager.mojom-blink.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_promise_resolver.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_lock_mode.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/heap/garbage_collected.h"
@@ -40,7 +41,7 @@ class Lock final : public ScriptWrappable,
 
   // Lock.idl
   String name() const { return name_; }
-  String mode() const;
+  V8LockMode mode() const;
 
   // ExecutionContextLifecycleObserver
   void ContextDestroyed() override;
@@ -49,8 +50,8 @@ class Lock final : public ScriptWrappable,
   // the passed resolver is invoked with the promise's result.
   void HoldUntil(ScriptPromise<IDLAny>, ScriptPromiseResolver<IDLAny>*);
 
-  static mojom::blink::LockMode StringToMode(const String&);
-  static String ModeToString(mojom::blink::LockMode);
+  static mojom::blink::LockMode EnumToMode(V8LockMode::Enum);
+  static V8LockMode::Enum ModeToEnum(mojom::blink::LockMode);
 
  private:
   class ThenFunction;
