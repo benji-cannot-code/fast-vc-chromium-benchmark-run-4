ba323e45c12fedc36b310f26dbc76b5bf803033e
Federica Germinario <fedegermi@google.com>
2024-10-03 15:32:02 +0000

    [ios] Remove GetForBrowserState from IOSChromeGCMProfileServiceFactory
    Remove legacy GetForBrowserState.
    
    Bug: 359812085
    Change-Id: I2373bfca5b4b7f529f644c9b5d2e709b71ad210e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904008
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363654}

--- ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.mm
+++ ios/chrome/browser/gcm/model/ios_chrome_gcm_profile_service_factory.mm
@@ -57,12 +57,6 @@ void RequestProxyResolvingSocketFactory(
 }  // namespace
 
 // static
-gcm::GCMProfileService* IOSChromeGCMProfileServiceFactory::GetForBrowserState(
-    ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
 gcm::GCMProfileService* IOSChromeGCMProfileServiceFactory::GetForProfile(
     ProfileIOS* profile) {
   return static_cast<gcm::GCMProfileService*>(
