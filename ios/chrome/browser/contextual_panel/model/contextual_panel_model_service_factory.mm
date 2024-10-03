d0101392f42053c82ab652bbb731c5cca190fd49
Federica Germinario <fedegermi@google.com>
2024-10-03 18:54:36 +0000

    [ios] Remove GetForBrowserState from ContextualPanelModelServiceFactory
    Remove legacy GetForBrowserState.
    
    Bug: 359812085
    Change-Id: I561293ef3c1d48e22373bc4033087ef3fc46330f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904162
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Nicolas MacBeth <nicolasmacbeth@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1363768}

--- ios/chrome/browser/contextual_panel/model/contextual_panel_model_service_factory.mm
+++ ios/chrome/browser/contextual_panel/model/contextual_panel_model_service_factory.mm
@@ -18,12 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/features/features.h"
 
 // static
-ContextualPanelModelService*
-ContextualPanelModelServiceFactory::GetForBrowserState(ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
 ContextualPanelModelService* ContextualPanelModelServiceFactory::GetForProfile(
     ProfileIOS* profile) {
   return static_cast<ContextualPanelModelService*>(
