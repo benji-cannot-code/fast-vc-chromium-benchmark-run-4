463bafe81dc301506f2e74c1419efac09ac92f2f
Sorin Jianu <sorin@chromium.org>
2024-10-03 22:28:13 +0000

    updater: define crash keys for managed devices
    Bug: 370766368
    Change-Id: If513dcd15eff2c21b85e2f1f3ae710c33852cd9e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905607
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: S Ganesh <ganesh@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363912}

--- chrome/updater/configurator.cc
+++ chrome/updater/configurator.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/updater/prefs.h"
 #include "chrome/updater/updater_scope.h"
 #include "chrome/updater/util/util.h"
+#include "components/crash/core/common/crash_key.h"
 #include "components/crx_file/crx_verifier.h"
 #include "components/prefs/pref_service.h"
 #include "components/update_client/network.h"
@@ -76,6 +77,8 @@ Configurator::Configurator(scoped_refptr<UpdaterPrefs> prefs,
   // created.
   GetNetworkFetcherFactory();
 #endif
+  static crash_reporter::CrashKeyString<6> crash_key_managed("managed");
+  crash_key_managed.Set(is_managed_device_ ? "true" : "false");
 }
 Configurator::~Configurator() = default;
 
