60d0a5b90c643ab8c1690d58d710e53e9ba41835
Mikel Astiz <mastiz@chromium.org>
2024-10-03 12:37:26 +0000

    [sync] Delete logging code from SyncError
    ...and semi-related code that can be deleted or simplified.
    
    The logging itself is unnecessary and can spam test logs.
    
    Generally, the code before this cleanup seems overly sophisticated for
    what SyncError is used for nowadays, restricted to DataTypeManagerImpl's
    internal state.
    
    Change-Id: Idab41959ee762a4fa3f1908b58a9318276e2786f
    Bug: 40901755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904931
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363583}

--- components/sync/service/sync_error.cc
+++ components/sync/service/sync_error.cc
@@ -5,73 +5,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/sync/service/sync_error.h"
 
-#include <ostream>
-
 #include "base/location.h"
-#include "base/logging.h"
 #include "base/notreached.h"
 
 namespace syncer {
 
-SyncError::SyncError() {
-  Clear();
-}
+SyncError::SyncError() = default;
 
 SyncError::SyncError(const base::Location& location,
                      ErrorType error_type,
                      const std::string& message,
-                     DataType data_type) {
-  DCHECK(error_type != UNSET);
-  Init(location, message, data_type, error_type);
-  PrintLogError();
-}
-
-SyncError::SyncError(const SyncError& other) {
-  Copy(other);
+                     DataType data_type)
+    : location_(location),
+      message_(message),
+      data_type_(data_type),
+      error_type_(error_type) {
+  CHECK_NE(error_type, UNSET);
 }
 
 SyncError::~SyncError() = default;
-
-SyncError& SyncError::operator=(const SyncError& other) {
-  if (this == &other) {
-    return *this;
-  }
-  Copy(other);
-  return *this;
-}
-
-void SyncError::Copy(const SyncError& other) {
-  if (other.IsSet()) {
-    Init(other.location(), other.message(), other.data_type(),
-         other.error_type());
-  } else {
-    Clear();
-  }
-}
-
-void SyncError::Clear() {
-  location_.reset();
-  message_ = std::string();
-  data_type_ = UNSPECIFIED;
-  error_type_ = UNSET;
-}
-
-void SyncError::Reset(const base::Location& location,
-                      const std::string& message,
-                      DataType data_type) {
-  Init(location, message, data_type, MODEL_ERROR);
-  PrintLogError();
-}
-
-void SyncError::Init(const base::Location& location,
-                     const std::string& message,
-                     DataType data_type,
-                     ErrorType error_type) {
-  location_ = std::make_unique<base::Location>(location);
-  message_ = message;
-  data_type_ = data_type;
-  error_type_ = error_type;
-}
 
 bool SyncError::IsSet() const {
   return error_type_ != UNSET;
@@ -79,7 +31,7 @@ bool SyncError::IsSet() const {
 
 const base::Location& SyncError::location() const {
   DCHECK(IsSet());
-  return *location_;
+  return location_;
 }
 
 const std::string& SyncError::message() const {
@@ -95,19 +47,6 @@ DataType SyncError::data_type() const {
 SyncError::ErrorType SyncError::error_type() const {
   DCHECK(IsSet());
   return error_type_;
-}
-
-SyncError::Severity SyncError::GetSeverity() const {
-  switch (error_type_) {
-    case UNREADY_ERROR:
-    case DATATYPE_POLICY_ERROR:
-      return SYNC_ERROR_SEVERITY_INFO;
-    case UNSET:
-    case MODEL_ERROR:
-    case CONFIGURATION_ERROR:
-    case CRYPTO_ERROR:
-      return SYNC_ERROR_SEVERITY_ERROR;
-  }
 }
 
 std::string SyncError::GetMessagePrefix() const {
@@ -139,21 +78,8 @@ std::string SyncError::ToString() const {
   if (!IsSet()) {
     return std::string();
   }
-  return location_->ToString() + ", " + DataTypeToDebugString(data_type_) +
-         " " + GetMessagePrefix() + message_;
-}
-
-void SyncError::PrintLogError() const {
-  logging::LogSeverity logSeverity = (GetSeverity() == SYNC_ERROR_SEVERITY_INFO)
-                                         ? logging::LOGGING_VERBOSE
-                                         : logging::LOGGING_ERROR;
-
-  LAZY_STREAM(logging::LogMessage(location_->file_name(),
-                                  location_->line_number(), logSeverity)
-                  .stream(),
-              logSeverity >= ::logging::GetMinLogLevel())
-      << DataTypeToDebugString(data_type_) << " " << GetMessagePrefix()
-      << message_;
+  return location_.ToString() + ", " + DataTypeToDebugString(data_type_) + " " +
+         GetMessagePrefix() + message_;
 }
 
 }  // namespace syncer
