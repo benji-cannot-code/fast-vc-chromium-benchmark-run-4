c13000aac4fe6c58c18f20db04187e0b5fe9143d
Sylvain Defresne <sdefresne@chromium.org>
2024-10-03 15:38:03 +0000

    [ios] Make DefaultBrowserPromoSceneAgent multi-profile aware
    Change DefaultBrowserPromoSceneAgent to become multi-profile aware
    by looking at the per-profile initialisation stage in ProfileState.
    
    Convert it to access the ProfileIOS via the ProfileState instead of
    going through the BrowserProviderInterface.
    
    Remove unnecessary dependencies.
    
    Bug: 41492447
    Fixed: 40139309
    Change-Id: Ie49b925166393607fe6dfc86852b60a2010d60dd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904241
    Reviewed-by: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363659}

--- ios/chrome/browser/ui/main/default_browser_promo_scene_agent.mm
+++ ios/chrome/browser/ui/main/default_browser_promo_scene_agent.mm
@@ -7,30 +7,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "components/feature_engagement/public/event_constants.h"
 #import "components/feature_engagement/public/tracker.h"
-#import "ios/chrome/app/application_delegate/app_state.h"
-#import "ios/chrome/app/application_delegate/app_state_observer.h"
+#import "ios/chrome/app/profile/profile_init_stage.h"
+#import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/default_browser/model/utils.h"
 #import "ios/chrome/browser/default_promo/ui_bundled/post_default_abandonment/features.h"
 #import "ios/chrome/browser/feature_engagement/model/tracker_factory.h"
 #import "ios/chrome/browser/promos_manager/model/constants.h"
-#import "ios/chrome/browser/shared/model/browser/browser.h"
-#import "ios/chrome/browser/shared/model/browser/browser_provider.h"
-#import "ios/chrome/browser/shared/model/browser/browser_provider_interface.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 
 @interface DefaultBrowserPromoSceneAgent ()
 
-// Indicates whether the user has already seen the post restore default browser
-// promo in the current app session.
-@property(nonatomic, assign) BOOL postRestorePromoSeenInCurrentSession;
-
 // YES if the main profile for this scene is signed in.
 @property(nonatomic, readonly, getter=isSignedIn) BOOL signedIn;
 
+// The feature engagement tracker for self, if it exists.
+@property(nonatomic, readonly)
+    feature_engagement::Tracker* featureEngagementTracker;
+
 @end
 
-@implementation DefaultBrowserPromoSceneAgent
+@implementation DefaultBrowserPromoSceneAgent {
+  // Indicates whether the user has already seen the post restore default
+  // browser promo in the current app session.
+  BOOL _postRestorePromoSeenInCurrentSession;
+}
 
 #pragma mark - Private
 
@@ -103,18 +104,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 }
 
-- (BOOL)isSignedIn {
-  ProfileIOS* profile = self.sceneState.browserProviderInterface
-                            .mainBrowserProvider.browser->GetProfile();
-
-  AuthenticationService* authenticationService =
-      AuthenticationServiceFactory::GetForProfile(profile);
-  DCHECK(authenticationService);
-  DCHECK(authenticationService->initialized());
-  return authenticationService->HasPrimaryIdentity(
-      signin::ConsentLevel::kSignin);
-}
-
 // Signed in users are eligible for generic default browser promo. Notify FET if
 // user is currently signed in.
 - (void)notifyFETSigninStatus {
@@ -122,16 +111,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  Browser* browser =
-      self.sceneState.browserProviderInterface.mainBrowserProvider.browser;
-  if (!browser || !browser->GetProfile()) {
-    return;
+  if (feature_engagement::Tracker* tracker = self.featureEngagementTracker) {
+    tracker->NotifyEvent(
+        feature_engagement::events::kGenericDefaultBrowserPromoConditionsMet);
   }
-
-  feature_engagement::Tracker* tracker =
-      feature_engagement::TrackerFactory::GetForProfile(browser->GetProfile());
-  tracker->NotifyEvent(
-      feature_engagement::events::kGenericDefaultBrowserPromoConditionsMet);
 }
 
 - (void)maybeSetTriggerCriteriaExperimentStartTimestamp {
@@ -144,16 +127,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)maybeNotifyFETTriggerCriteriaExperimentConditionMet {
   if (IsDefaultBrowserTriggerCriteraExperimentEnabled() &&
       HasTriggerCriteriaExperimentStarted21days()) {
-    Browser* browser =
-        self.sceneState.browserProviderInterface.mainBrowserProvider.browser;
-    if (!browser || !browser->GetProfile()) {
-      return;
+    if (feature_engagement::Tracker* tracker = self.featureEngagementTracker) {
+      tracker->NotifyEvent(
+          feature_engagement::events::
+              kDefaultBrowserPromoTriggerCriteriaConditionsMet);
     }
-    feature_engagement::Tracker* tracker =
-        feature_engagement::TrackerFactory::GetForProfile(
-            browser->GetProfile());
-    tracker->NotifyEvent(feature_engagement::events::
-                             kDefaultBrowserPromoTriggerCriteriaConditionsMet);
   }
 }
 
@@ -163,7 +141,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     transitionedToActivationLevel:(SceneActivationLevel)level {
   DCHECK(self.promosManager);
 
-  if (self.sceneState.appState.initStage < AppInitStage::kFinal) {
+  if (self.sceneState.profileState.initStage < ProfileInitStage::kFinal) {
     return;
   }
 
@@ -179,6 +157,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     [self maybeSetTriggerCriteriaExperimentStartTimestamp];
     [self maybeNotifyFETTriggerCriteriaExperimentConditionMet];
   }
+}
+
+#pragma mark - Private properties
+
+- (BOOL)isSignedIn {
+  ProfileIOS* profile = self.sceneState.profileState.profile;
+  if (!profile) {
+    return NO;
+  }
+
+  AuthenticationService* authenticationService =
+      AuthenticationServiceFactory::GetForProfile(profile);
+  DCHECK(authenticationService);
+  DCHECK(authenticationService->initialized());
+  return authenticationService->HasPrimaryIdentity(
+      signin::ConsentLevel::kSignin);
+}
+
+- (feature_engagement::Tracker*)featureEngagementTracker {
+  ProfileIOS* profile = self.sceneState.profileState.profile;
+  if (!profile) {
+    return nullptr;
+  }
+
+  return feature_engagement::TrackerFactory::GetForProfile(profile);
 }
 
 @end
