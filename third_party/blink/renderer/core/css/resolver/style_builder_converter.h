73437552d1568e64da36ac6a853ac61c5f01824a
Noam Rosenthal <nrosenthal@chromium.org>
2024-10-04 12:59:37 +0000

    Implement view-transition-name: auto
    This allows generating view-transition-names automatically, reducing
    the burden of inventing unique names from the user, in cases where the
    element is the same (or has the same ID) between the two states.
    
    See spec PR: https://github.com/w3c/csswg-drafts/pull/10922
    
    Change-Id: I1b6c6752acc7106929b08ac6fcdda9578d6402d3
    Bug: 365997248
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5878355
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364180}

--- third_party/blink/renderer/core/css/resolver/style_builder_converter.h
+++ third_party/blink/renderer/core/css/resolver/style_builder_converter.h
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/style/style_overflow_clip_margin.h"
 #include "third_party/blink/renderer/core/style/style_reflection.h"
 #include "third_party/blink/renderer/core/style/style_view_transition_group.h"
+#include "third_party/blink/renderer/core/style/style_view_transition_name.h"
 #include "third_party/blink/renderer/core/style/transform_origin.h"
 #include "third_party/blink/renderer/platform/fonts/font_description.h"
 #include "third_party/blink/renderer/platform/fonts/font_variant_emoji.h"
@@ -372,8 +373,8 @@ class StyleBuilderConverter {
       const StyleResolverState&,
       const CSSValue&);
 
-  static ScopedCSSName* ConvertViewTransitionName(StyleResolverState&,
-                                                  const CSSValue&);
+  static StyleViewTransitionName* ConvertViewTransitionName(StyleResolverState&,
+                                                            const CSSValue&);
   static ScopedCSSNameList* ConvertViewTransitionClass(StyleResolverState&,
                                                        const CSSValue&);
   static StyleViewTransitionGroup ConvertViewTransitionGroup(
--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.h
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.h
@@ -229,6 +229,8 @@ class ViewTransitionStyleTracker
       const AtomicString& name,
       const StyleViewTransitionGroup& group) const;
 
+  AtomicString GenerateAutoName(Element&, const TreeScope*);
+
   struct ElementData : public GarbageCollected<ElementData> {
     void Trace(Visitor* visitor) const;
 
@@ -412,6 +414,8 @@ class ViewTransitionStyleTracker
     AtomicString contain;
   };
   HashMap<AtomicString, AncestorGroupNames> group_state_map_;
+
+  base::Token token_;
 };
 
 }  // namespace blink
