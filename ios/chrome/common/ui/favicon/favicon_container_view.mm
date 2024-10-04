356b17ecadb445c00f9314cde0c6ab93f7bedac6
Daniel White <danieltwhite@google.com>
2024-10-04 14:50:08 +0000

    [iOS] Deprecate traitCollectionDidChange in FaviconContainerView
    Since traitCollectionDidChange is deprecated in iOS 17
    FaviconContainerView was updated to use the new alternative system,
    registerForTraitChanges.
    
    Fixed: 349389034
    Change-Id: Ie945663e82d383e45bc9c6ba6607502642a1d34f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891342
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364221}

--- ios/chrome/common/ui/favicon/favicon_container_view.mm
+++ ios/chrome/common/ui/favicon/favicon_container_view.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/common/ui/favicon/favicon_container_view.h"
 
+#import <UIKit/UIKit.h>
+
 #import "ios/chrome/common/ui/colors/semantic_color_names.h"
 #import "ios/chrome/common/ui/favicon/favicon_view.h"
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
@@ -59,6 +61,17 @@ const CGFloat kFaviconContainerWidth = 30;
       [self.heightAnchor constraintEqualToConstant:kFaviconContainerWidth],
       [self.widthAnchor constraintEqualToAnchor:self.heightAnchor],
     ]];
+
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits = @[
+        UITraitUserInterfaceIdiom.self, UITraitUserInterfaceStyle.self,
+        UITraitDisplayGamut.self, UITraitAccessibilityContrast.self,
+        UITraitUserInterfaceLevel.self
+      ];
+
+      [self registerForTraitChanges:traits
+                         withAction:@selector(updateColorOnTraitChange:)];
+    }
   }
   return self;
 }
@@ -81,14 +94,16 @@ const CGFloat kFaviconContainerWidth = 30;
   }
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if ([self.traitCollection
-          hasDifferentColorAppearanceComparedToTraitCollection:
-              previousTraitCollection]) {
-    [self resetColor];
+  if (@available(iOS 17, *)) {
+    return;
   }
+
+  [self updateColorOnTraitChange:previousTraitCollection];
 }
+#endif
 
 - (void)resetColor {
   if (self.customBackgroundColor) {
@@ -102,6 +117,16 @@ const CGFloat kFaviconContainerWidth = 30;
   self.layer.borderColor = self.customBorderColor
                                ? self.customBorderColor.CGColor
                                : [UIColor colorNamed:kSeparatorColor].CGColor;
+}
+
+#pragma mark - Private
+
+- (void)updateColorOnTraitChange:(UITraitCollection*)previousTraitCollection {
+  if ([self.traitCollection
+          hasDifferentColorAppearanceComparedToTraitCollection:
+              previousTraitCollection]) {
+    [self resetColor];
+  }
 }
 
 @end
