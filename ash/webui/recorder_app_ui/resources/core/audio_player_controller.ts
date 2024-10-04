d1dc8947ee5d8cf9095db76114577cb516867c73
Pi-Hsun Shih <pihsun@chromium.org>
2024-10-04 10:40:48 +0000

    recorder: Set src to empty string on ReactiveAudio.revoke
    Originally, the system wide media control will be shown until the
    ReactiveAudio object is garbage collected. Setting the src to empty
    string on revoke fixes it.
    
    Bug: b:371511375
    Test: manually
    Change-Id: Ie98f676e12316b88d03944091f72c9c28dd7c7bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904501
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Shik Chen <shik@chromium.org>
    Auto-Submit: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364130}

--- ash/webui/recorder_app_ui/resources/core/audio_player_controller.ts
+++ ash/webui/recorder_app_ui/resources/core/audio_player_controller.ts
@@ -148,6 +148,7 @@ class ReactiveAudioImpl implements ReactiveAudio {
   revoke(): void {
     this.audio.pause();
     this.revokeAudio();
+    this.audio.src = '';
     this.animationFrameController.stop();
     this.recordingId = null;
   }
