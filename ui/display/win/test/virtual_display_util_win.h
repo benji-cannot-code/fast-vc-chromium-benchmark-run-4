645425d426dd1c8758506b06228f79668b022577
Brad Triebwasser <btriebw@chromium.org>
2024-10-04 19:10:33 +0000

    VirtualDisplayUtilWin: Ensure display topology is in extend mode.
    This was removed in https://crrev.com/c/5100640/23..24 but it turns out
    that it is needed.
    
    Bug: 40111905, 371121282
    Change-Id: I9076cd5d8db9fb135b36d6e142490cd2d4c31d44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907272
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Commit-Queue: Brad Triebwasser <btriebw@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364383}

--- ui/display/win/test/virtual_display_util_win.h
+++ ui/display/win/test/virtual_display_util_win.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/memory/weak_ptr.h"
 #include "base/run_loop.h"
+#include "base/timer/timer.h"
 #include "third_party/win_virtual_display/controller/display_driver_controller.h"
 #include "third_party/win_virtual_display/driver/public/properties.h"
 #include "ui/display/display_observer.h"
@@ -56,6 +57,9 @@ class VirtualDisplayUtilWin : public display::DisplayObserver,
   void StartWaiting();
   void StopWaiting();
 
+  // Ensures that display topology is in extend mode (not mirror).
+  void EnsureExtendMode();
+
   // Creates a new internal display ID to identify the display to the driver.
   static uint8_t SynthesizeInternalDisplayId();
 
@@ -63,6 +67,8 @@ class VirtualDisplayUtilWin : public display::DisplayObserver,
   // True if the environment was considered headless during initialization.
   const bool is_headless_;
   std::unique_ptr<base::RunLoop> run_loop_;
+  // Periodically ensures that display topology is in extended mode.
+  base::RepeatingTimer ensure_extended_timer_;
   DisplayDriverController driver_controller_;
   // Contains the last configuration that was set.
   DriverProperties current_config_;
