0be0b1dc807db1ffd478953ce4195841db8225cb
Ella Ge <eirage@chromium.org>
2024-10-04 19:57:16 +0000

    [DefaultBrowser] Add the role manager switch to chrome://flags
    Add an entry in chrome://flags for disabling the role manager default
    browser promo to make testing the new promo easier.
    
    Bug: 364906215
    Change-Id: Ia7d9c18c027e6d53a0715ec420624b283fcaa2aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5909707
    Commit-Queue: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Auto-Submit: Ella Ge <eirage@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364399}

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -210,6 +210,7 @@ extern const char kWinJumplistAction[];
 
 #if BUILDFLAG(IS_ANDROID)
 extern const char kAuthAndroidNegotiateAccountType[];
+extern const char kDisableDefaultBrowserPromo[];
 extern const char kForceDeviceOwnership[];
 extern const char kForceEnableNightMode[];
 extern const char kForceShowUpdateMenuBadge[];
