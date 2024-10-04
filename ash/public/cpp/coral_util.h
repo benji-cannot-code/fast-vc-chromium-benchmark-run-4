dd6a153be5867f9dabb002cc2df4b883a7847683
Ian Clelland <iclelland@chromium.org>
2024-10-04 15:23:13 +0000

    Revert "[CORAL] Update coral response mojom"
    This reverts commit 9c400c57e689409d722348520c29369c40c6254e.
    
    Reason for revert: Breaks the incremental build checks on ChromeOS
    
    Second compile should be a no-op, but somehow is not. There may be a deps issue here, but reverting to clear the failures.
    
    See:
    https://ci.chromium.org/ui/p/chromium/builders/ci/chromeos-arm-generic-dbg/102773
    https://ci.chromium.org/ui/b/8735000195335717345
    
    Original change's description:
    > [CORAL] Update coral response mojom
    >
    > Sync the ChromeOS change of updating the coral response mojom to return
    > the whole entity rather than just identifiers. This is because some
    > parts of the UI code will want to access title fields of entities.
    >
    > The CrOS CL changing the mojom is crrev.com/c/5901682.
    >
    > Bug: b:369980522
    > Test: CQ
    >
    > Change-Id: Ibacb937208d8c4cbcd62f1f803f5635abf43426a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900487
    > Commit-Queue: Howard Yang <hcyang@google.com>
    > Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    > Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    > Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1364210}
    
    Bug: b:369980522
    Change-Id: If2c45a9d4616256e82d57668fbc2ffcd34e166c1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908093
    Auto-Submit: Ian Clelland <iclelland@chromium.org>
    Owners-Override: Ian Clelland <iclelland@chromium.org>
    Commit-Queue: Ian Clelland <iclelland@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1364233}

--- ash/public/cpp/coral_util.h
+++ ash/public/cpp/coral_util.h
@@ -14,7 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash::coral_util {
 
 std::string ASH_PUBLIC_EXPORT
-GetIdentifier(const coral::mojom::EntityPtr& item);
+GetIdentifier(const coral::mojom::EntityKeyPtr& key);
+
+std::string ASH_PUBLIC_EXPORT GetIdentifier(const coral::mojom::EntityKey& key);
 
 std::string ASH_PUBLIC_EXPORT GetIdentifier(const coral::mojom::Entity& item);
 
