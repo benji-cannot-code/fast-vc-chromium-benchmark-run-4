65242d0bdecc56881584666775480937842dde2d
Darren Shen <shend@chromium.org>
2024-10-04 00:37:20 +0000

    Reduce caps lock notification duration to 2s.
    The caps lock notification only has an icon now, so it doesn't
    need 3s to read.
    
    Bug: b:366720518
    Change-Id: I15fe7c9f51b33b73cd7dd234dbcd2f31066cc260
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904403
    Commit-Queue: Darren Shen <shend@chromium.org>
    Auto-Submit: Darren Shen <shend@chromium.org>
    Reviewed-by: Jing Wang <jiwan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363971}

--- ash/picker/picker_caps_lock_bubble_controller.cc
+++ ash/picker/picker_caps_lock_bubble_controller.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace {
 
-constexpr base::TimeDelta kBubbleViewDisplayTime = base::Seconds(3);
+constexpr base::TimeDelta kBubbleViewDisplayTime = base::Seconds(2);
 
 // Starting at the time the bubble is shown, events that would normally close
 // the bubble are ignored for this grace period, to prevent the bubble from
