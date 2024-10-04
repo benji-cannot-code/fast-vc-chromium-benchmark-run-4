368ff9f5a580fc310362fdd30b9b94dabb0d6be7
Tsuyoshi Horo <horo@chromium.org>
2024-10-04 09:19:14 +0000

    Refactor OnDeviceTranslation service to use FileOperationProxy
    Before this CL, the browser process opened all language package files
    on a background thread before starting the OnDeviceTranslation service.
    This can cause unnecessary resource usage when many language packs were
    installed. This also required a task queue mechanism to wait for the
    file operations in OnDeviceTranslationServiceController.
    
    This CL introduces FileOperationProxy, which handles file operations on
    the browser process background thread when requested by the service.
    This reduces resource consumption, and simplifies
    OnDeviceTranslationServiceController by removing the need for a task
    queue.
    
    Bug: 358030919
    Change-Id: I846f62baea7732656bca7b5db83f1889bc23af4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5894844
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364109}

--- components/services/on_device_translation/translate_kit_client.h
+++ components/services/on_device_translation/translate_kit_client.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/pass_key.h"
 #include "components/services/on_device_translation/public/mojom/on_device_translation_service.mojom.h"
 #include "components/services/on_device_translation/translate_kit_structs.h"
+#include "mojo/public/cpp/bindings/remote.h"
 
 namespace on_device_translation {
 
@@ -188,9 +189,7 @@ class TranslateKitClient {
                                         std::uintptr_t user_data);
   TranslatorTranslateFn translator_translate_func_;
 
-  mojom::OnDeviceTranslationServiceConfigPtr config_;
-  std::set<std::string> directories_;
-  std::map<std::string, base::File> files_;
+  mojo::Remote<mojom::FileOperationProxy> file_operation_proxy_;
 };
 
 }  // namespace on_device_translation
