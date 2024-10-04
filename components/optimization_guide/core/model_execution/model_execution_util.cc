e69e4650355c2eab6c25d5127e8a427f505294a8
Steven Holte <holte@google.com>
2024-10-04 20:57:55 +0000

    Add Capability for HistoryQueryIntent.
    This just adds the skeleton structure for an on-device only capability with
    no MQLS logging.
    
    Bug: 356681248
    
    Change-Id: If115ad949f5fc5dabaeeca11c1a49ab54e37199d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906658
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364433}

--- components/optimization_guide/core/model_execution/model_execution_util.cc
+++ components/optimization_guide/core/model_execution/model_execution_util.cc
@@ -41,6 +41,7 @@ void SetExecutionRequest(
       SetExecutionRequestTemplate<HistoryAnswerFeatureTypeMap>(
           log_ai_request, request_metadata);
       return;
+    case ModelBasedCapabilityKey::kHistoryQueryIntent:
     case ModelBasedCapabilityKey::kFormsAnnotations:
     case ModelBasedCapabilityKey::kFormsPredictions:
     case ModelBasedCapabilityKey::kPromptApi:
@@ -74,6 +75,7 @@ void SetExecutionResponse(ModelBasedCapabilityKey feature,
       SetExecutionResponseTemplate<HistoryAnswerFeatureTypeMap>(
           log_ai_request, response_metadata);
       return;
+    case ModelBasedCapabilityKey::kHistoryQueryIntent:
     case ModelBasedCapabilityKey::kFormsAnnotations:
     case ModelBasedCapabilityKey::kFormsPredictions:
     case ModelBasedCapabilityKey::kPromptApi:
