86a750e4d8fbdf3d193c50d57c707a64178f265f
Dmitry Vykochko <vykochko@google.com>
2024-10-04 14:06:10 +0000

    Use EXPECT_CHECK_DEATH_WITH in PopupViewViewsDeathTest test.
    This CL is just following best practices to use more specific
    macros. It has no visible effect on the test execution today.
    
    Bug: 355521765
    Change-Id: I9a865c73cae4498a069eeebf619013fa22c3e34b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905142
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364206}

--- chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
+++ chrome/browser/ui/views/autofill/popup/popup_view_views_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string_util.h"
 #include "base/test/bind.h"
+#include "base/test/gtest_util.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/time/time.h"
 #include "build/build_config.h"
@@ -1409,8 +1410,8 @@ TEST_F(PopupViewViewsTest, CellSubPopupResetAfterSuggestionsUpdates) {
 #if !BUILDFLAG(IS_CHROMEOS_ASH)
 // `PopupViewViewsTest` is not used in death tests because it sets up a complex
 // environment (namely creates a `TestingProfile`) that fails to be created in
-// the sub-process (see `ASSERT_DEATH` doc for details). This fail hides
-// the real death reason to be tested.
+// the sub-process (see `EXPECT_CHECK_DEATH_WITH` doc for details). This fail
+// hides the real death reason to be tested.
 using PopupViewViewsDeathTest = ChromeViewsTestBase;
 TEST_F(PopupViewViewsDeathTest, OpenSubPopupWithNoChildrenCheckCrash) {
   NiceMock<MockAutofillPopupController> controller;
@@ -1433,7 +1434,7 @@ TEST_F(PopupViewViewsDeathTest, OpenSubPopupWithNoChildrenCheckCrash) {
   std::string expected_message = "can_open_sub_popup";
 #endif  // defined(NDEBUG) && defined(OFFICIAL_BUILD)
 
-  ASSERT_DEATH(
+  EXPECT_CHECK_DEATH_WITH(
       view_ptr->SetSelectedCell(CellIndex{0, CellType::kControl},
                                 PopupCellSelectionSource::kNonUserInput),
       expected_message);
