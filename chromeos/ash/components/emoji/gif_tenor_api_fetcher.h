f0f1478fd4b6b1fa2dff8d15e82e034e8c615455
Darren Shen <shend@chromium.org>
2024-10-04 00:45:28 +0000

    Move GifTenorApiFetcher to //chromeos/ash/components.
    This allows //ash to depend on it.
    
    Bug: b:368442959
    Change-Id: I3d9895e001829dcda6393eebaba7acc4caee4aef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890345
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Chris Mullins <crmullins@chromium.org>
    Reviewed-by: John Palmer <jopalmer@chromium.org>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1363976}

--- chromeos/ash/components/emoji/gif_tenor_api_fetcher.h
+++ chromeos/ash/components/emoji/gif_tenor_api_fetcher.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_ASH_EMOJI_GIF_TENOR_API_FETCHER_H_
-#define CHROME_BROWSER_UI_WEBUI_ASH_EMOJI_GIF_TENOR_API_FETCHER_H_
+#ifndef CHROMEOS_ASH_COMPONENTS_EMOJI_GIF_TENOR_API_FETCHER_H_
+#define CHROMEOS_ASH_COMPONENTS_EMOJI_GIF_TENOR_API_FETCHER_H_
 
 #include <memory>
 #include <optional>
@@ -122,4 +122,4 @@ class GifTenorApiFetcher {
 };
 }  // namespace ash
 
-#endif  // CHROME_BROWSER_UI_WEBUI_ASH_EMOJI_GIF_TENOR_API_FETCHER_H_
+#endif  // CHROMEOS_ASH_COMPONENTS_EMOJI_GIF_TENOR_API_FETCHER_H_
