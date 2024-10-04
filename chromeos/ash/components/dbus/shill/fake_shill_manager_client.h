73e826045d0359c3b1a17132a0cb30818e5dc1a8
Irina <irfedorova@google.com>
2024-10-04 11:59:45 +0000

    kiosk: add browser tests for WiFi exposure
    This CL also adds `FailedAttempt` function which is now called for
    failed passphrase attempt as well.
    
    To make sure the tests are waiting for the asynchronous WiFi exposure,
    `wifi_exposure_attempt_callback_for_testing_` callback was added.
    
    Fixed: b/365528789
    Test: KioskNetworkStateObserverTest.*
    Change-Id: Ib36c0066e6825283cb38641306ee61365f21af52
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899438
    Commit-Queue: Irina Fedorova <irfedorova@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364153}

--- chromeos/ash/components/dbus/shill/fake_shill_manager_client.h
+++ chromeos/ash/components/dbus/shill/fake_shill_manager_client.h
@@ -151,6 +151,8 @@ class COMPONENT_EXPORT(SHILL_CLIENT) FakeShillManagerClient
   bool GetFastTransitionStatus() override;
   void SetSimulateConfigurationResult(
       FakeShillSimulatedResult configuration_result) override;
+  void SetSimulateConfigurationError(std::string_view error_name,
+                                     std::string_view error_message) override;
   void SetSimulateTetheringEnableResult(
       FakeShillSimulatedResult tethering_enable_result,
       const std::string& result_string) override;
@@ -181,6 +183,12 @@ class COMPONENT_EXPORT(SHILL_CLIENT) FakeShillManagerClient
   static const char kFakeEthernetNetworkGuid[];
 
  private:
+  // Error message for configure service failure.
+  struct ConfigurationError {
+    std::string name;
+    std::string message;
+  };
+
   using ConnectToBestServicesCallbacks =
       std::tuple<base::OnceClosure, ErrorCallback>;
 
@@ -248,6 +256,8 @@ class COMPONENT_EXPORT(SHILL_CLIENT) FakeShillManagerClient
 
   FakeShillSimulatedResult simulate_configuration_result_ =
       FakeShillSimulatedResult::kSuccess;
+  ConfigurationError simulate_configuration_error_ = {"Error",
+                                                      "Simulated failure"};
   FakeShillSimulatedResult simulate_tethering_enable_result_ =
       FakeShillSimulatedResult::kSuccess;
   std::string simulate_enable_tethering_result_string_;
