4b71ddc937c6f5ab5b05bcbbc450859a89b177cc
Garrett Beaty <gbeaty@chromium.org>
2024-10-04 16:28:38 +0000

    [infra] Fix docstring for values.to_output.
    Change-Id: Ia78a24363bf1f21f6abd07c29735a3a9a17d4365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908872
    Commit-Queue: Wanda Mora <morawand@google.com>
    Auto-Submit: Garrett Beaty <gbeaty@google.com>
    Reviewed-by: Wanda Mora <morawand@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364272}

--- infra/config/migration/values.py
+++ infra/config/migration/values.py
@@ -71,7 +71,7 @@ Value: typing.TypeAlias = 'str | ValueBuilder'
 
 
 def to_output(value: Value) -> str | None:
-  """get the output for a starlark venue."""
+  """Get the output for a starlark value."""
   if isinstance(value, ValueBuilder):
     return value.output()
   return value
