2b2ba56d28ffcc558a248274106ffa46332411db
Kenichi Ishibashi <bashi@chromium.org>
2024-10-04 23:18:56 +0000

    Move SSLConnect result histograms outside SSLConnectJob
    So that we can record the same histograms for the HappyEyeballsV3
    code path. Note that the HEv3 code path doesn't support ECH yet.
    Subsequent CLs will implement it.
    
    Bug: 346835898
    Change-Id: Ib30d6e524eafb8c098c4baa3e266aa1eebf86508
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904579
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364493}

--- net/socket/ssl_client_socket.h
+++ net/socket/ssl_client_socket.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/observer_list.h"
+#include "net/base/load_timing_info.h"
 #include "net/base/net_export.h"
 #include "net/cert/cert_database.h"
 #include "net/cert/cert_verifier.h"
@@ -40,6 +41,15 @@ class TransportSecurityState;
 //
 class NET_EXPORT SSLClientSocket : public SSLSocket {
  public:
+  // Records some histograms based on the result of the SSL handshake.
+  static void RecordSSLConnectResult(
+      SSLClientSocket& ssl_socket,
+      int result,
+      bool is_ech_capable,
+      bool ech_enabled,
+      const std::optional<std::vector<uint8_t>>& ech_retry_configs,
+      const LoadTimingInfo::ConnectTiming& connect_timing);
+
   SSLClientSocket();
 
   // Called in response to |ERR_ECH_NOT_NEGOTIATED| in Connect(), to determine
