2ae83cb65b7608380b816ee74e26237017d2bf89
Tom Sepez <tsepez@chromium.org>
2024-10-04 20:46:23 +0000

    Avoid some unintentional WTF::Vector copies in renderer/platform.
    This CL was originally the result of git cl split, but now contains
    manual rewrites of affected functions to avoid the copies.
    
    R=pdr@chromium.org
    
    Bug: 369952985
    Change-Id: I3a608c424c11e5025340621017408be0fde203a0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905978
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364425}

--- third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
+++ third_party/blink/renderer/platform/loader/fetch/resource_fetcher.cc
@@ -2352,7 +2352,7 @@ void ResourceFetcher::WarnUnusedPreloads(
   }
 
   // Notify the unused preload list to the LCPP host.
-  std::move(callback).Run(unused_preloads);
+  std::move(callback).Run(std::move(unused_preloads));
 }
 
 void ResourceFetcher::HandleLoaderFinish(Resource* resource,
