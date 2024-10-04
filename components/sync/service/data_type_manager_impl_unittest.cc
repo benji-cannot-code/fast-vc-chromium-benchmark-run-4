21035e61e9c00c4f4d451fae1a0c19d4af5a523c
Mikel Astiz <mastiz@chromium.org>
2024-10-04 12:02:29 +0000

    [sync] Simplify enum DataTypeConfigState
    Refactoring without behavior changes, beyond the removal of DCHECKs.
    
    The various "disabled" cases lead to identical behavior, so it is
    pointless to distinguish different enum values and understand how to
    precisely draw a line between them.
    
    The non-trivial change in this patch is about the purging logic, where
    unready types are no longer excluded. This should be fine, because
    NIGORI never becomes unready (as it doesn't use a controller managed by
    DataTypeManager). This also means the implementation in
    BuildDataTypeConfigStateMap() can be simplified, because populating a
    merged DISABLED bucket can simply be done via GetFailedTypes(), which is
    already exercised before this patch (but is now made more explicit with
    a CHECK).
    
    Change-Id: Ifbd1f180e574e44bf9495df55e82d9220faf76b6
    Bug: 40901755, 40154783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905321
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364154}

--- components/sync/service/data_type_manager_impl_unittest.cc
+++ components/sync/service/data_type_manager_impl_unittest.cc
@@ -1513,8 +1513,6 @@ TEST_F(DataTypeManagerImplTest, PurgeDataOnStarting) {
   ASSERT_EQ(DataTypeManager::CONFIGURED, dtm_->state());
   ASSERT_EQ(1U, configurer_.connected_types().size());
 
-  // This should have purged the data for the excluded type.
-  EXPECT_TRUE(configurer_.last_params().to_purge.Has(BOOKMARKS));
   // Stop(CLEAR_METADATA) is called if (re)started without the type.
   EXPECT_EQ(1, GetController(BOOKMARKS)->model()->clear_metadata_count());
 }
@@ -1548,8 +1546,6 @@ TEST_F(DataTypeManagerImplTest, PurgeDataOnReconfiguring) {
   ASSERT_EQ(DataTypeManager::CONFIGURED, dtm_->state());
   ASSERT_EQ(1U, configurer_.connected_types().size());
 
-  // This should have purged the data for the excluded type.
-  EXPECT_TRUE(configurer_.last_params().to_purge.Has(BOOKMARKS));
   // Also Stop(CLEAR_METADATA) has been called on the controller since the type
   // is no longer enabled.
   EXPECT_EQ(1, GetController(BOOKMARKS)->model()->clear_metadata_count());
