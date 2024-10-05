436edf4e2baf776780ddbd8d6fe55f43462871c9
Wenyu Fu <wenyufu@chromium.org>
2024-10-05 00:26:03 +0000

    [E2E] Scrim do not color nav bar when edge to edge enabled.
    Scrim already draws on top of the nav bar region when edge to edge is
    enabled. No need to change the system bar color.
    
    This CL also moved out the check for edge to edge to
    TabbedNavigationBarColorController, setting up for crrev.com/c/5890770.
    
    Preview:
    https://screenshot.googleplex.com/3x9xpZydRWdR6Xb
    https://screenshot.googleplex.com/8VDsDiDj7zNsTTL
    
    Bug: 369635657
    Change-Id: Id1f46baa46915a971bdf80d3c4048606fd0579fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910372
    Commit-Queue: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Charles Hager <clhager@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364508}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogTest.java
@@ -76,6 +76,7 @@ import static org.chromium.ui.test.util.ViewUtils.waitForVisibleView;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
@@ -2117,18 +2118,22 @@ public class TabGridDialogTest {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O_MR1
                 || !resources.getBoolean(R.bool.window_light_navigation_bar)
                 || isTablet(cta)
-                || cta.getTabModelSelectorSupplier().get().isIncognitoBrandedModelSelected()
-                || EdgeToEdgeUtils.isEnabled()) {
+                || cta.getTabModelSelectorSupplier().get().isIncognitoBrandedModelSelected()) {
             return;
         }
-        @ColorInt int scrimDefaultColor = cta.getColor(R.color.default_scrim_color);
-        @ColorInt int navigationBarColor = SemanticColorUtils.getBottomSystemNavColor(cta);
-        @ColorInt
-        int navigationBarColorWithScrimOverlay =
-                ColorUtils.overlayColor(navigationBarColor, scrimDefaultColor);
 
-        assertEquals(cta.getWindow().getNavigationBarColor(), navigationBarColorWithScrimOverlay);
-        assertNotEquals(navigationBarColor, navigationBarColorWithScrimOverlay);
+        if (!EdgeToEdgeUtils.isEnabled()) {
+            @ColorInt int scrimDefaultColor = cta.getColor(R.color.default_scrim_color);
+            @ColorInt int navigationBarColor = SemanticColorUtils.getBottomSystemNavColor(cta);
+            @ColorInt
+            int navigationBarColorWithScrimOverlay =
+                    ColorUtils.overlayColor(navigationBarColor, scrimDefaultColor);
+            assertEquals(
+                    navigationBarColorWithScrimOverlay, cta.getWindow().getNavigationBarColor());
+            assertNotEquals(navigationBarColor, navigationBarColorWithScrimOverlay);
+        } else if (cta.getEdgeToEdgeSupplier().get().isDrawingToEdge()) {
+            assertEquals(Color.TRANSPARENT, cta.getWindow().getNavigationBarColor());
+        }
     }
 
     private boolean isPhone() {
