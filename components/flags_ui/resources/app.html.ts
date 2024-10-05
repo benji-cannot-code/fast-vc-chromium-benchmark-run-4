6982eeff7cb0045056722e278dc4266018a65950
dpapad <dpapad@chromium.org>
2024-10-05 05:24:39 +0000

    FlagsUI: Check-in previously auto-generated .html.ts files.
    Bug: 40246381
    Change-Id: I8c1521b2374f982f58243838c5e8e99ee80dfd66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906287
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364576}

--- components/flags_ui/resources/app.html.ts
+++ components/flags_ui/resources/app.html.ts
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {AppElement} from './app.js';
+
+export function getHtml(this: AppElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <div id="header">
   <div class="flex-container">
     <div class="flex search-container">
@@ -23,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <div id="body-container">
   <div id="flagsTemplate">
 <if expr="chromeos_lacros or chromeos_ash">
-      <div class="os-link-container" id="os-link-container"
+    <div class="os-link-container" id="os-link-container"
           ?hidden="${!this.data.showSystemFlagsLink}">
       <span id="os-flags-link-container" class="os-link-icon"></span>
       <span aria-hidden="true" id="os-link-desc">$i18n{os-flags-text1}</span>
@@ -135,3 +146,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </div>
   </div>
 </div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
--- components/flags_ui/resources/experiment.html.ts
+++ components/flags_ui/resources/experiment.html.ts
@@ -1,4 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ExperimentElement} from './experiment.js';
+
+export function getHtml(this: ExperimentElement) {
+  // clang-format off
+  return html`<!--_html_template_start_-->
 <div class="experiment" id="${this.feature_.internal_name}">
   <div class="flex-container">
     <div class="flex">
@@ -98,3 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     </div>
   </div>
 </div>
+<!--_html_template_end_-->`;
+  // clang-format on
+}
