7107a1c3afba32a47ab53c81c0c65fc8188f6607
Michael Wojcicka <mwoj@google.com>
2024-10-06 23:28:59 +0000

    Add Feature Flag for Task Manager on Clank
    Change-Id: Ia3e1462dd9aee896029c04f076c1bdc8d2c24961
    Bug: b:371440535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908814
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364739}

---
+++ chrome/browser/task_manager/task_manager_features.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/task_manager/task_manager_features.h"
+
+namespace features {
+
+// Enables the Task Manager on Clank.
+#if BUILDFLAG(IS_ANDROID)
+BASE_FEATURE(kTaskManagerClank,
+             "TaskManagerClank",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+#endif  // BUILDFLAG(IS_ANDROID)
+
+}  // namespace features
