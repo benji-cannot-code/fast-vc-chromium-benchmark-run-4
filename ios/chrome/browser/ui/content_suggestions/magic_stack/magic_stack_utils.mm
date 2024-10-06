96ef56e8030ddee26c7a20d8cef8c50270b7699c
David Maunder <davidjm@google.com>
2024-10-06 00:34:13 +0000

    Restrict Price Tracking Promo in magic stack to en-US
    The price tracking promo encourages price tracking users from other
    platforms to use the feature on iOS. The feature on iOS is only
    available for en-US users so the promo should have the same
    restriction.
    
    Note: I think it's technically not possible to hit this without
    the feature param kEphemeralCardRankerForceShowCardParam because
    service->IsShoppingListEligible checks for this. But since
    we're using kEphemeralCardRankerForceShowCardParam in the test
    instructions I'd like this to be fixed for our testers.
    
    Bug: 371613174
    Change-Id: Ib0c95bface9a992d7bbe77a6b4859884786549e8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910657
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364656}

--- ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_utils.mm
+++ ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_utils.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/segmentation_platform/public/features.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_client_id.h"
 #import "ios/chrome/browser/push_notification/model/push_notification_settings_util.h"
+#import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_constants.h"
 #import "ios/chrome/browser/ui/content_suggestions/magic_stack/magic_stack_utils.h"
@@ -40,6 +41,7 @@ bool IsPriceTrackingPromoCardEnabled(commerce::ShoppingService* service,
   id<SystemIdentity> identity =
       auth_service->GetPrimaryIdentity(signin::ConsentLevel::kSignin);
   return base::FeatureList::IsEnabled(commerce::kPriceTrackingPromo) &&
+         GetApplicationContext()->GetApplicationLocale() == "en-US" &&
          !push_notification_settings::
              GetMobileNotificationPermissionStatusForClient(
                  PushNotificationClientId::kCommerce,
