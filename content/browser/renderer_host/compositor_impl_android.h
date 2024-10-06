7dd6f40f91c6f1b6f9b7c36ece8dd3717cd08773
William Liu <liuwilliam@chromium.org>
2024-10-06 04:02:44 +0000

    [DNT] Decouple ReadbackRef from COR's result callback
    Before this CL, we could run into a situation where Viz sends back the
    screenshot via the CopyOutputRequest's callback, by posting the callback
    onto the UI thread but the UI thread is being destroyed, thus refusing
    to accept the task. However the ReadbackRef is bound to the callback,
    so the ReadbackRef is destroyed on a non UI thread.
    
    This CL fixes that by decoupling the ReadbackRef from the COR's
    callback. Instead we store the ScopedKeepSurfaceAlive on Android's
    compositor and track each ScopedKeepSurfaceAlive by an int.
    
    In the normal codepath the callback is executed on the UI thread and
    the ScopedKeepSurfaceAlive is untracked from the compositor; in the edge
    case since the UI thread is being destroyed, the compositor takes away
    all the outstanding ScopedKeepSurfaceAlive, and the COR result callback
    is never executed (so we don't run the callback on some other thread).
    
    Tested by running
    autoninja -C out/android android_browsertests && out/android/bin/run_android_browsertests --gtest_filter=All/SharedStoragePrefBrowserTest* --break-on-failure --avd-config=tools/android/avd/proto/android_32_google_apis_x64_foldable.textpb --gtest_repeat=40
    
    Fixed: 370781890
    Change-Id: Id51090cc50f1e8f5cb23573a32a986dbdcadcb1a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900459
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364672}

--- content/browser/renderer_host/compositor_impl_android.h
+++ content/browser/renderer_host/compositor_impl_android.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/time/time.h"
 #include "cc/paint/element_id.h"
+#include "cc/slim/layer_tree.h"
 #include "cc/slim/layer_tree_client.h"
 #include "cc/trees/layer_tree_host_client.h"
 #include "cc/trees/layer_tree_host_single_thread_client.h"
@@ -48,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc::slim {
 class Layer;
-class LayerTree;
 }  // namespace cc::slim
 
 namespace viz {
@@ -138,8 +138,8 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
   void DidLoseLayerTreeFrameSink() override;
 
   // WindowAndroidCompositor implementation.
-  std::unique_ptr<ReadbackRef> TakeReadbackRef(
-      const viz::SurfaceId& surface_id) override;
+  ui::WindowAndroidCompositor::ScopedKeepSurfaceAliveCallback
+  TakeScopedKeepSurfaceAliveCallback(const viz::SurfaceId& surface_id) override;
   void RequestCopyOfOutputOnRootLayer(
       std::unique_ptr<viz::CopyOutputRequest> request) override;
   void SetNeedsAnimate() override;
@@ -202,9 +202,12 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
   void InitializeVizLayerTreeFrameSink(
       scoped_refptr<viz::ContextProviderCommandBuffer> context_provider);
 
-  void DecrementPendingReadbacks();
-
   void MaybeUpdateObserveBeginFrame();
+
+  using PendingSurfaceCopyId =
+      base::StrongAlias<struct PendingSurfaceCopyIdTag, uint32_t>;
+  void RemoveScopedKeepSurfaceAlive(
+      const PendingSurfaceCopyId& scoped_keep_surface_alive_id);
 
   viz::FrameSinkId frame_sink_id_;
 
@@ -259,8 +262,6 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
 
   size_t num_of_consecutive_surface_failures_ = 0u;
 
-  uint32_t pending_readbacks_ = 0u;
-
   bool enable_swap_completion_callbacks_ = false;
 
   // Listen to display density change events and update painted device scale
@@ -274,6 +275,12 @@ class CONTENT_EXPORT CompositorImpl : public Compositor,
   std::unique_ptr<ui::HostBeginFrameObserver> host_begin_frame_observer_;
 
   base::ObserverList<FrameSubmissionObserver> frame_submission_observers_;
+
+  // Tracks a list of pending `viz::CopyOutputRequest`s.
+  PendingSurfaceCopyId pending_surface_copy_id_ = PendingSurfaceCopyId(0u);
+  base::flat_map<PendingSurfaceCopyId,
+                 std::unique_ptr<cc::slim::LayerTree::ScopedKeepSurfaceAlive>>
+      pending_surface_copies_;
 
   base::WeakPtrFactory<CompositorImpl> weak_factory_{this};
 };
