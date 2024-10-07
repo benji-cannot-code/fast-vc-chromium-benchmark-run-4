efdafe243f19194b2c6a00a4442efd5f1b92f0b1
Taylor Bergquist <tbergquist@chromium.org>
2024-10-07 20:28:10 +0000

    Switch WebContents* to TabHandle in TabData.
    This does several things for us:
    - removes an O(n) tab->index lookup in TabData::IsValidForOrganizing
    - replaces the manual TabID stuff in TabData with TabHandle's
    serializable form
    - simplifies observation since we can just observe the tab directly
    - paves the way towards more cleanup & refactoring in the page handler
    and TabOrganization service APIs
    
    The changes are mechanical outside of TabData, except for one or two
    places where I noted with review comments.
    
    I stopped short of doing alllll the cleanup to keep the CL more focused.
    Next steps could be:
    - replacing TabID in the TabOrganization service APIs with directly
    using TabHandle
    - replacing the use of ExtensionTabUtil::GetTabId (tab ids from
    sessions) with TabHandle's serialized form. N.B. there is shared
    infrastructure among the three features in this page handler that might
    make this weird.
    
    Bug: 367394540
    Change-Id: I8e2603dd184958a65c4d2db4109e98b4f79f793a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884617
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Taylor Bergquist <tbergquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365125}

--- chrome/browser/ui/tabs/organization/request_factory.cc
+++ chrome/browser/ui/tabs/organization/request_factory.cc
@@ -132,7 +132,7 @@ void PerformTabOrganizationExecution(
 
     auto* tab = tab_organization_request.add_tabs();
     tab->set_tab_id(tab_data->tab_id());
-    tab->set_title(base::UTF16ToUTF8(tab_data->web_contents()->GetTitle()));
+    tab->set_title(base::UTF16ToUTF8(tab_data->tab()->contents()->GetTitle()));
     tab->set_url(tab_data->original_url().spec());
   }
 
@@ -162,7 +162,8 @@ void PerformTabOrganizationExecution(
     for (const std::unique_ptr<TabData>& tab_data : group_data->tabs) {
       auto* tab = group->add_tabs();
       tab->set_tab_id(tab_data->tab_id());
-      tab->set_title(base::UTF16ToUTF8(tab_data->web_contents()->GetTitle()));
+      tab->set_title(
+          base::UTF16ToUTF8(tab_data->tab()->contents()->GetTitle()));
       tab->set_url(tab_data->original_url().spec());
     }
   }
--- chrome/browser/ui/tabs/organization/tab_organization_session.h
+++ chrome/browser/ui/tabs/organization/tab_organization_session.h
@@ -14,12 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/tabs/organization/metrics.h"
 #include "chrome/browser/ui/tabs/organization/tab_organization.h"
 #include "chrome/browser/ui/tabs/organization/tab_organization_request.h"
+#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 
 class Browser;
 
-namespace content {
-class WebContents;
-}  // namespace content
+namespace tabs {
+class TabModel;
+}  // namespace tabs
 
 class TabOrganizationSession : public TabOrganization::Observer {
  public:
@@ -41,7 +42,7 @@ class TabOrganizationSession : public TabOrganization::Observer {
   explicit TabOrganizationSession(
       std::unique_ptr<TabOrganizationRequest> request,
       TabOrganizationEntryPoint entrypoint = TabOrganizationEntryPoint::kNone,
-      const content::WebContents* base_session_webcontents = nullptr);
+      const tabs::TabModel* base_session_tab = nullptr);
   ~TabOrganizationSession() override;
 
   const TabOrganizationRequest* request() const { return request_.get(); }
@@ -51,14 +52,12 @@ class TabOrganizationSession : public TabOrganization::Observer {
   ID session_id() const { return session_id_; }
   std::u16string feedback_id() const { return feedback_id_; }
   optimization_guide::proto::UserFeedback feedback() const { return feedback_; }
-  const content::WebContents* base_session_webcontents() const {
-    return base_session_webcontents_;
-  }
+  const tabs::TabModel* base_session_tab() const { return base_session_tab_; }
 
   static std::unique_ptr<TabOrganizationSession> CreateSessionForBrowser(
       const Browser* browser,
       const TabOrganizationEntryPoint entrypoint,
-      const content::WebContents* base_session_webcontents = nullptr);
+      const tabs::TabModel* base_session_tab = nullptr);
 
   const TabOrganization* GetNextTabOrganization() const;
   TabOrganization* GetNextTabOrganization();
@@ -113,7 +112,7 @@ class TabOrganizationSession : public TabOrganization::Observer {
   TabOrganizationEntryPoint entrypoint_;
 
   // Active tab web contents tied to the session, if any.
-  raw_ptr<const content::WebContents> base_session_webcontents_;
+  raw_ptr<const tabs::TabModel> base_session_tab_;
 
   base::ObserverList<Observer>::Unchecked observers_;
 };
