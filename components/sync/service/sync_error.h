039f061b32f9d9bd74d7ae3d69b00a92e34cefa8
Mikel Astiz <mastiz@chromium.org>
2024-10-07 08:33:57 +0000

    [sync] Rename precondition errors in SyncError
    Before this patch, the outdated terms 'unready' and 'policy' were used
    in SyncError. These concepts are nowadays unfamiliar to most, because
    there are more modern replacements: DataTypeController APIs include the
    concept of 'preconditions', and enum PreconditionState distinguishes
    two flavors in which preconditions can fail: kMustStopAndKeepData and
    kMustStopAndClearData.
    
    Luckily, the old concepts (unready/policy errors) map trivially to the
    modern equivalents (kMustStopAndKeepData/kMustStopAndClearData), and
    therefore it boils down to renames in SyncError.
    
    As a side note: it would be even nicer if the two states could be
    bundled into a single SyncError enum value, but unfortunately this is
    not possible due to exactly one dependency:
    DataTypeManagerImpl::GetDataTypesWithPermanentErrors() relies on this
    separation to consider kMustStopAndClearData more permanent than
    kMustStopAndKeepData.
    
    Also included in this patch: DataType is removed from SyncError as it
    is unused.
    
    Change-Id: Icf335cd28434f892a46a1f3ecc8df4865bcd1704
    Bug: 40901755
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907410
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364820}

--- components/sync/service/sync_error.h
+++ components/sync/service/sync_error.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/location.h"
-#include "components/sync/base/data_type.h"
 
 namespace syncer {
 
@@ -29,27 +28,25 @@ class SyncError {
     // A cryptographer error was detected (i.e. the datatype is encrypted and
     // encryption keys are missing).
     CRYPTO_ERROR,
-    // A datatype is not ready to start yet, so should be neither purged nor
-    // enabled until it is ready.
-    UNREADY_ERROR,
-    // A datatype should be disabled and purged due to configuration
-    // constraints.
-    DATATYPE_POLICY_ERROR,
+    // A datatype cannot start because its controller determined that it doesn't
+    // meet all preconditions via `DataTypeController::GetPreconditionState()`.
+    // Specifically, it returned `kMustStopAndKeepData`.
+    PRECONDITION_ERROR_WITH_KEEP_DATA,
+    // Same as above, but the controller returned `kMustStopAndClearData`.
+    PRECONDITION_ERROR_WITH_CLEAR_DATA,
   };
 
-  // Create a new Sync error of type `error_type` triggered by `data_type`
-  // from the specified location.
+  // Create a new Sync error of type `error_type` triggered from the specified
+  // location.
   SyncError(const base::Location& location,
             ErrorType error_type,
-            const std::string& message,
-            DataType data_type);
+            const std::string& message);
   SyncError(const SyncError& other) = default;
   SyncError& operator=(const SyncError& other) = default;
   ~SyncError();
 
   const base::Location& location() const;
   const std::string& message() const;
-  DataType data_type() const;
   ErrorType error_type() const;
 
   // Type specific message prefix for logging and UI purposes.
@@ -58,7 +55,6 @@ class SyncError {
  private:
   base::Location location_;
   std::string message_;
-  DataType data_type_;
   ErrorType error_type_;
 };
 
