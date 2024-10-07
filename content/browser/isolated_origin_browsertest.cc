c0b834585d022fb17a55e2a68ddbe40c46218eee
Yoav Weiss <yoavweiss@chromium.org>
2024-10-07 10:17:52 +0000

    Enable COOP noopener-allow-popups
    I2S: https://groups.google.com/a/chromium.org/g/blink-dev/c/5wae0r2uCag/m/7ItYZel_AQAJ
    
    Bug: 344963946
    Change-Id: I0fadcb5ee7d36fe63f139464fc3e20076fabe5ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5872914
    Commit-Queue: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364853}

--- content/browser/isolated_origin_browsertest.cc
+++ content/browser/isolated_origin_browsertest.cc
@@ -6771,9 +6771,20 @@ IN_PROC_BROWSER_TEST_F(COOPIsolationTest, SameOriginAllowPopups) {
             coop_instance);
 }
 
+class COOPIsolationNoopenerTest : public COOPIsolationTest {
+ public:
+  COOPIsolationNoopenerTest() {
+    feature_list_.InitAndEnableFeature(
+        network::features::kCoopNoopenerAllowPopups);
+  }
+
+ private:
+  base::test::ScopedFeatureList feature_list_;
+};
+
 // Verify that the `noopener-allow-popups COOP header value triggers isolation,
 // and that this behaves sanely with window.open().
-IN_PROC_BROWSER_TEST_F(COOPIsolationTest, NoopenerAllowPopups) {
+IN_PROC_BROWSER_TEST_F(COOPIsolationNoopenerTest, NoopenerAllowPopups) {
   if (AreAllSitesIsolatedForTesting()) {
     return;
   }
@@ -6804,10 +6815,9 @@ IN_PROC_BROWSER_TEST_F(COOPIsolationTest, NoopenerAllowPopups) {
   RenderFrameHostImpl* popup_rfh = static_cast<RenderFrameHostImpl*>(
       popup->web_contents()->GetPrimaryMainFrame());
   EXPECT_EQ(popup_rfh->cross_origin_opener_policy().value,
-            network::mojom::CrossOriginOpenerPolicyValue::kUnsafeNone);
-  // TODO(https://crbug.com/344963946): Update the test values.
-  EXPECT_EQ(popup_rfh->GetSiteInstance(), coop_instance);
-  EXPECT_EQ(popup_rfh->GetSiteInstance()->GetProcess(),
+            network::mojom::CrossOriginOpenerPolicyValue::kNoopenerAllowPopups);
+  EXPECT_NE(popup_rfh->GetSiteInstance(), coop_instance);
+  EXPECT_NE(popup_rfh->GetSiteInstance()->GetProcess(),
             coop_instance->GetProcess());
 
   // Navigate the popup to another non-isolated site, staying in the same
@@ -6821,20 +6831,20 @@ IN_PROC_BROWSER_TEST_F(COOPIsolationTest, NoopenerAllowPopups) {
   SiteInstanceImpl* new_instance = static_cast<SiteInstanceImpl*>(
       popup->web_contents()->GetPrimaryMainFrame()->GetSiteInstance());
   EXPECT_FALSE(new_instance->RequiresDedicatedProcess());
-  EXPECT_EQ(new_instance, coop_instance);
+  EXPECT_NE(new_instance, coop_instance);
   FrameTreeNode* popup_child =
       static_cast<WebContentsImpl*>(popup->web_contents())
           ->GetPrimaryFrameTree()
           .root()
           ->child_at(0);
-  EXPECT_EQ(popup_child->current_frame_host()->GetSiteInstance(),
+  EXPECT_NE(popup_child->current_frame_host()->GetSiteInstance(),
             coop_instance);
 
   // Navigate the popup to coop.com again, staying in the same
   // BrowsingInstance, and verify that it goes back to the opener's
   // SiteInstance.
   EXPECT_TRUE(NavigateToURLFromRenderer(popup, popup_url));
-  EXPECT_EQ(popup->web_contents()->GetPrimaryMainFrame()->GetSiteInstance(),
+  EXPECT_NE(popup->web_contents()->GetPrimaryMainFrame()->GetSiteInstance(),
             coop_instance);
 }
 
