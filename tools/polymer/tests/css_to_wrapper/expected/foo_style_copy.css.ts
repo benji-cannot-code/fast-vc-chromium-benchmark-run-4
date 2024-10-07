3fd78c9dedd5552a20e94a41e7bc7900e75a69ec
dpapad <dpapad@chromium.org>
2024-10-07 20:53:21 +0000

    WebUI: Update css_to_wrapper() to use Lit as the canonical source.
    This is in preparation of using some cr_elements/ in platforms where
    Polymer isn't included (iOS/Android) and therefore the Polymer
    dependency is undesired.
    
    Effectively this reverses the previous logic where when a file pair
    <foo>.css and <foo>_lit.css existed, the former was treated as the
    canonical source to generate the <foo>_lit.html.ts wrapper. After this
    CL <foo>.css.ts wrapper is generated from the Lit version.
    
    Regardless of iOS/Android this is a step towards treating Lit as a
    first-class citizen in css_to_wrapper().
    
    Bug: 40246381
    Change-Id: I098a22a5add54e4a31c144b190efca28682758a4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910425
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365137}

---
+++ tools/polymer/tests/css_to_wrapper/expected/foo_style_copy.css.ts
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+import {html} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
+import './other1.css.js';
+import './other2.css.js';
+
+const styleMod = document.createElement('dom-module');
+styleMod.appendChild(html`
+  <template>
+    <style include="other1 other2">
+
+div {
+  font-size: 2rem;
+  --foo-bar: calc(var(--foo-bar1)
+      - var(--foo-bar2)
+      - 3 * var(--foo-bar3));
+}
+    </style>
+  </template>
+`.content);
+styleMod.register('foo-style-copy');
