6ff286d5d9883688d8f37e97b5a3b413a7fead62
John Lee <johntlee@chromium.org>
2024-10-07 19:21:36 +0000

    Switch between different date formats for history embeddings results
    Fixed: 371343187
    Change-Id: I5b536c9ec1aee6201e39969f106a9193dfa14393
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913558
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365082}

--- ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
+++ ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
@@ -107,6 +107,7 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
         computed: 'computeAnswerSource_(loadingAnswer_, searchResult_.items)',
         value: null,
       },
+      showRelativeTimes: {type: Boolean, value: false},
     };
   }
 
@@ -150,6 +151,7 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
    * search result for the same query is queued after it.
    */
   private searchResultPromise_: Promise<void>|null = null;
+  showRelativeTimes: boolean = false;
 
   override connectedCallback() {
     super.connectedCallback();
@@ -236,6 +238,17 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
       return this.i18n('historyEmbeddingsHeadingLoading', this.searchQuery);
     }
     return this.i18n('historyEmbeddingsHeading', this.searchQuery);
+  }
+
+  private getDateTime_(item: SearchResultItem|undefined): string {
+    if (!item) {
+      return '';
+    }
+
+    if (this.showRelativeTimes) {
+      return item.relativeTime;
+    }
+    return item.shortDateTime;
   }
 
   private hasAnswer_(): boolean {
