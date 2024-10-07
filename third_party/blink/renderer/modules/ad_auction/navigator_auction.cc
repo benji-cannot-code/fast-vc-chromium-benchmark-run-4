b9918dc1ebf5e2ff1c08a8f49586c97f774e5769
Tom Sepez <tsepez@chromium.org>
2024-10-07 20:09:52 +0000

    Avoid some unintentional WTF::Vector copies.
    This is a re-split of a larger CL where reviews were lost when the
    set of files changed.
    
    Applies one or more of the following (depending on split):
    -- std::move(argument) where possible.
    -- add 'const' where helpful.
    -- remove stray 'const' where not helpful.
    -- pass by ref where helpful.
    
    This CL was uploaded by git cl split.
    
    R=morlovich@chromium.org
    
    Bug: 369952985
    Change-Id: Ic2af12c51e70bbb1dc4ac4a1de14ab3a13b3d6a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913360
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365107}

--- third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
+++ third_party/blink/renderer/modules/ad_auction/navigator_auction.cc
@@ -3593,8 +3593,8 @@ ScriptPromise<IDLUndefined> NavigatorAuction::leaveAdInterestGroup(
 
 ScriptPromise<IDLUndefined> NavigatorAuction::clearOriginJoinedAdInterestGroups(
     ScriptState* script_state,
-    const String owner_string,
-    const Vector<String> interest_groups_to_keep,
+    const String& owner_string,
+    Vector<String> interest_groups_to_keep,
     ExceptionState& exception_state) {
   scoped_refptr<const SecurityOrigin> owner = ParseOrigin(owner_string);
   if (!owner) {
@@ -3622,7 +3622,7 @@ ScriptPromise<IDLUndefined> NavigatorAuction::clearOriginJoinedAdInterestGroups(
           WTF::BindOnce(&NavigatorAuction::ClearComplete,
                         WrapWeakPersistent(this), is_cross_origin));
 
-  PendingClear pending_clear{owner, interest_groups_to_keep,
+  PendingClear pending_clear{owner, std::move(interest_groups_to_keep),
                              std::move(callback)};
   if (is_cross_origin) {
     queued_cross_site_clears_.Enqueue(std::move(pending_clear));
@@ -3648,7 +3648,7 @@ ScriptPromise<IDLUndefined> NavigatorAuction::clearOriginJoinedAdInterestGroups(
     ScriptState* script_state,
     Navigator& navigator,
     const String owner,
-    const Vector<String> interest_groups_to_keep,
+    Vector<String> interest_groups_to_keep,
     ExceptionState& exception_state) {
   if (!navigator.DomWindow()) {
     exception_state.ThrowDOMException(DOMExceptionCode::kInvalidAccessError,
@@ -3666,8 +3666,9 @@ ScriptPromise<IDLUndefined> NavigatorAuction::clearOriginJoinedAdInterestGroups(
   }
 
   return From(context, navigator)
-      .clearOriginJoinedAdInterestGroups(
-          script_state, owner, interest_groups_to_keep, exception_state);
+      .clearOriginJoinedAdInterestGroups(script_state, owner,
+                                         std::move(interest_groups_to_keep),
+                                         exception_state);
 }
 
 void NavigatorAuction::updateAdInterestGroups() {
