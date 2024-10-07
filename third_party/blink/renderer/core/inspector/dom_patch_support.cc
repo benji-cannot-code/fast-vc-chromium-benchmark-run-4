6e64b0a95f7b7dcae12e156f18beb7fbe67d4047
Fredrik Söderquist <fs@opera.com>
2024-10-07 08:34:43 +0000

    spanification: Part of //third_party/blink/renderer/core/inspector/
    Bug: 351564777
    Change-Id: Ia3c8cb1f218a0142296e7b96425d96383ae12433
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904298
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1364821}

--- third_party/blink/renderer/core/inspector/dom_patch_support.cc
+++ third_party/blink/renderer/core/inspector/dom_patch_support.cc
@@ -29,11 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/core/inspector/dom_patch_support.h"
 
 #include "base/memory/scoped_refptr.h"
@@ -442,8 +437,7 @@ DOMPatchSupport::Digest* DOMPatchSupport::CreateDigest(
   DigestValue digest_result;
 
   Node::NodeType node_type = node->getNodeType();
-  digestor.Update(
-      {reinterpret_cast<const uint8_t*>(&node_type), sizeof(node_type)});
+  digestor.Update(base::byte_span_from_ref(node_type));
   digestor.UpdateUtf8(node->nodeName());
   digestor.UpdateUtf8(node->nodeValue());
 
