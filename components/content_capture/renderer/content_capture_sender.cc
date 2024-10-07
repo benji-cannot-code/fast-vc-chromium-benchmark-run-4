516950c86fbbc36dd1998cc1735fe23a213e14d8
Sorin Jianu <sorin@chromium.org>
2024-10-07 09:45:19 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/content_capture
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=elabadysayed@chromium.org
    
    Bug: 371316188
    Change-Id: I04ab49261a297b490cd48b360f4be3afc537588f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908507
    Reviewed-by: Sayed Elabady <elabadysayed@chromium.org>
    Commit-Queue: Sayed Elabady <elabadysayed@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364844}

--- components/content_capture/renderer/content_capture_sender.cc
+++ components/content_capture/renderer/content_capture_sender.cc
@@ -26,7 +26,7 @@ ContentCaptureSender::ContentCaptureSender(
       &ContentCaptureSender::BindPendingReceiver, base::Unretained(this)));
 }
 
-ContentCaptureSender::~ContentCaptureSender() {}
+ContentCaptureSender::~ContentCaptureSender() = default;
 
 void ContentCaptureSender::BindPendingReceiver(
     mojo::PendingAssociatedReceiver<mojom::ContentCaptureSender>
