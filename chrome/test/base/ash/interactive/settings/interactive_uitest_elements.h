7da3bcd94a178c8429d97f7667e80a21ae466f98
Jason Zhang <jiajunz@google.com>
2024-10-07 23:33:07 +0000

    test: Add apn policy interactive tests
    Bug: b/367797553
    Change-Id: I7c454b96cc9526228183cc4199c84d6a2b2c9a18
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905971
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Nikhil Nayunigari <nikhilcn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365227}

--- chrome/test/base/ash/interactive/settings/interactive_uitest_elements.h
+++ chrome/test/base/ash/interactive/settings/interactive_uitest_elements.h
@@ -149,6 +149,9 @@ WebContentsInteractionTestUtil::DeepQuery ApnSubpageActionMenuButton();
 // The "Create new APN" button in the action menu in APN subpage.
 WebContentsInteractionTestUtil::DeepQuery ApnSubpageCreateApnButton();
 
+// The policy icon on the top of the APN subpage.
+WebContentsInteractionTestUtil::DeepQuery ApnSubpagePolicyIcon();
+
 // The "Show known APNs" button in the action menu in APN subpage.
 WebContentsInteractionTestUtil::DeepQuery ApnSubpageShowKnownApnsButton();
 
@@ -208,6 +211,9 @@ WebContentsInteractionTestUtil::DeepQuery CellularNetworkListItemPolicyIcon();
 
 // The cellular networks subpage pSIM networks list title.
 WebContentsInteractionTestUtil::DeepQuery CellularSubpagePsimListTitle();
+
+// The APN policy icon in cellular network details subpage.
+WebContentsInteractionTestUtil::DeepQuery CellularDetailsSubpageApnPolicyIcon();
 
 // The auto connect toggle in cellular network details subpage.
 WebContentsInteractionTestUtil::DeepQuery
