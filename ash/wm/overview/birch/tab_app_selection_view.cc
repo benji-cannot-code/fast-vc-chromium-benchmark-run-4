ec9150adbd1291fa7989badf3dfb8988e6197cff
Sammie Quon <sammiequon@chromium.org>
2024-10-07 17:00:25 +0000

    coral: Add touch support to selector UI
    Tap once to show close button, tap again to hide. Also removes some
    hardcoded groups.
    
    Test: manual
    Change-Id: I9b19d224f7927a7123776e59e380f551c8bcc7ad
    Fixed: b/366262722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910093
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365002}

--- ash/wm/overview/birch/tab_app_selection_view.cc
+++ ash/wm/overview/birch/tab_app_selection_view.cc
@@ -148,9 +148,11 @@ class TabAppSelectionView::TabAppSelectionItemView
     auto* delegate = Shell::Get()->saved_desk_delegate();
     auto set_icon_image_callback = base::BindOnce(
         [](const base::WeakPtr<TabAppSelectionItemView>& item_view,
-           const gfx::ImageSkia& favicon) {
+           const gfx::ImageSkia& icon) {
           if (item_view) {
-            item_view->image_->SetImage(favicon);
+            item_view->image_->SetImage(
+                icon.isNull() ? ui::ImageModel::FromVectorIcon(kDefaultAppIcon)
+                              : ui::ImageModel::FromImageSkia(icon));
           }
         },
         weak_ptr_factory_.GetWeakPtr());
@@ -216,6 +218,11 @@ class TabAppSelectionView::TabAppSelectionItemView
   }
   void OnMouseExited(const ui::MouseEvent& event) override {
     SetSelected(false);
+  }
+  void OnGestureEvent(ui::GestureEvent* event) override {
+    if (event->type() == ui::EventType::kGestureTap) {
+      owner_->OnItemTapped(this);
+    }
   }
   void OnFocus() override { SetSelected(true); }
   void OnBlur() override { SetSelected(false); }
@@ -410,6 +417,16 @@ void TabAppSelectionView::OnCloseButtonPressed(
   // be called again.
   for (TabAppSelectionItemView* item : item_views_) {
     item->RemoveCloseButton();
+  }
+}
+
+void TabAppSelectionView::OnItemTapped(TabAppSelectionItemView* sender) {
+  for (TabAppSelectionItemView* item : item_views_) {
+    if (item != sender) {
+      item->SetSelected(false);
+    } else {
+      item->SetSelected(!item->selected());
+    }
   }
 }
 
