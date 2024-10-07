c07d4c3c15e5972efee4102aedb7a185c38105c7
Saifuddin Hitawala <hitawala@chromium.org>
2024-10-07 19:12:14 +0000

    [media] Remove VideoFrame::HasTextures
    Remove VideoFrame::HasTextures which is replaced in all use cases
    except OOPVD stable decoder with HasSharedImage.
    
    Restrict VideoFrame mailbox to OOPVD case with rename to oopvd_mailbox
    and buildflags. Also add getter and HasOOPVDMailbox for checks.
    Update mojo files accordingly.
    
    Bug: 343011436
    Change-Id: If2916b294e95d7e80ecf3598f0bf0c0859e57e7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904189
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365076}

--- media/mojo/clients/mojo_stable_video_decoder_unittest.cc
+++ media/mojo/clients/mojo_stable_video_decoder_unittest.cc
@@ -933,7 +933,7 @@ TEST_F(MojoStableVideoDecoderTest, Decode) {
   EXPECT_EQ(received_decoded_video_frame_1->natural_size(),
             kDecodedFrame1NaturalSize);
   EXPECT_EQ(received_decoded_video_frame_1->ColorSpace(), gfx::ColorSpace());
-  ASSERT_TRUE(received_decoded_video_frame_1->HasTextures());
+  ASSERT_TRUE(received_decoded_video_frame_1->HasSharedImage());
   EXPECT_EQ(received_decoded_video_frame_1->mailbox_holder(0).mailbox,
             kDecodedFrame1Mailbox);
   EXPECT_EQ(received_decoded_video_frame_1->mailbox_holder(0).sync_token,
@@ -1003,7 +1003,7 @@ TEST_F(MojoStableVideoDecoderTest, Decode) {
   EXPECT_EQ(received_decoded_video_frame_2->natural_size(),
             kDecodedFrame2NaturalSize);
   EXPECT_EQ(received_decoded_video_frame_2->ColorSpace(), gfx::ColorSpace());
-  ASSERT_TRUE(received_decoded_video_frame_2->HasTextures());
+  ASSERT_TRUE(received_decoded_video_frame_2->HasSharedImage());
   EXPECT_EQ(received_decoded_video_frame_2->mailbox_holder(0).mailbox,
             kDecodedFrame1Mailbox);
   EXPECT_EQ(received_decoded_video_frame_2->mailbox_holder(0).sync_token,
@@ -1085,7 +1085,7 @@ TEST_F(MojoStableVideoDecoderTest, Decode) {
             kDecodedFrame2NaturalSize);
   EXPECT_EQ(received_decoded_video_frame_3->ColorSpace(),
             kDecodedFrame3ColorSpace);
-  ASSERT_TRUE(received_decoded_video_frame_3->HasTextures());
+  ASSERT_TRUE(received_decoded_video_frame_3->HasSharedImage());
   EXPECT_EQ(received_decoded_video_frame_3->mailbox_holder(0).mailbox,
             kDecodedFrame3Mailbox);
   EXPECT_EQ(received_decoded_video_frame_3->mailbox_holder(0).sync_token,
@@ -1169,7 +1169,7 @@ TEST_F(MojoStableVideoDecoderTest, Decode) {
   EXPECT_EQ(received_decoded_video_frame_4->natural_size(),
             kDecodedFrame4NaturalSize);
   EXPECT_EQ(received_decoded_video_frame_4->ColorSpace(), gfx::ColorSpace());
-  ASSERT_TRUE(received_decoded_video_frame_4->HasTextures());
+  ASSERT_TRUE(received_decoded_video_frame_4->HasSharedImage());
   EXPECT_EQ(received_decoded_video_frame_4->mailbox_holder(0).mailbox,
             kDecodedFrame4Mailbox);
   EXPECT_EQ(received_decoded_video_frame_4->mailbox_holder(0).sync_token,
