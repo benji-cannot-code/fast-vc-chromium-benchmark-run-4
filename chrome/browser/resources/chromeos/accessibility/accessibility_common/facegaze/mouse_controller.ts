bddb1c85f595ef7f5bf107be3ad7b5b2d8954956
Akihiro Ota <akihiroota@chromium.org>
2024-10-07 20:27:51 +0000

    FaceGaze: Ensure scroll mode respects physical mouse movements
    Bug: 367526325
    Change-Id: Ia23e0981b1442a81e226c5779204107d7570db11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913615
    Reviewed-by: Amanda Lin Dietz <aldietz@google.com>
    Commit-Queue: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365124}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
@@ -547,6 +547,11 @@ export class MouseController {
       // Assume all synthesized mouse movements come from within FaceGaze.
       this.mouseLocation_ = {x: event.mouseX, y: event.mouseY};
       this.lastMouseMovedTime_ = new Date().getTime();
+
+      if (this.scrollModeController_.active()) {
+        // Scroll mode honors physical mouse movements.
+        this.scrollModeController_.updateScrollLocation(this.mouseLocation_);
+      }
     }
   }
 
