74713dc99ae785cb65322eabbf23bb61ce2cfe02
Keigo Oka <oka@google.com>
2024-10-07 04:54:04 +0000

    Zoom with ctrl + wheel
    Bug: b/236427994
    Change-Id: Id4928fb10f737db83058609d49b04c0e4c183819
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5839506
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Commit-Queue: Keigo Oka <oka@chromium.org>
    Auto-Submit: Keigo Oka <oka@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364774}

--- chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroidImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tab/TabWebContentsDelegateAndroidImpl.java
@@ -19,6 +19,7 @@ import org.chromium.base.BuildInfo;
 import org.chromium.base.Callback;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.ObserverList.RewindableIterator;
+import org.chromium.chrome.browser.ZoomController;
 import org.chromium.chrome.browser.app.bluetooth.BluetoothNotificationService;
 import org.chromium.chrome.browser.app.usb.UsbNotificationService;
 import org.chromium.chrome.browser.back_press.BackPressManager;
@@ -451,6 +452,16 @@ final class TabWebContentsDelegateAndroidImpl extends TabWebContentsDelegateAndr
     @Override
     public int getBackForwardTransitionFallbackUXPageBackgroundColor() {
         return ChromeColors.getSurfaceColor(mTab.getContext(), R.dimen.default_elevation_3);
+    }
+
+    @Override
+    public void contentsZoomChange(boolean zoomIn) {
+        WebContents wc = mTab.getWebContents();
+        if (zoomIn) {
+            ZoomController.zoomIn(wc);
+        } else {
+            ZoomController.zoomOut(wc);
+        }
     }
 
     void showFramebustBlockInfobarForTesting(String url) {
