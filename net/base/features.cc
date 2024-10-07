12fbec23c7089d579adadd1f46164451c8565411
David Benjamin <davidben@chromium.org>
2024-10-07 19:59:33 +0000

    Enable ML-KEM by default
    This switches PostQuantumKyber to toggle ML-KEM (the final NIST
    standard based on Kyber) instead of Kyber. The feature flag will
    remain for a short while as a Finch kill switch in case of issues.
    
    Bug: 40910498
    Change-Id: I15a890eb2e7de6942ccb7c43ffba8be2df2baaef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908098
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365101}

--- net/base/features.cc
+++ net/base/features.cc
@@ -161,7 +161,7 @@ BASE_FEATURE(kPostQuantumKyber,
              base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
-BASE_FEATURE(kUseMLKEM, "UseMLKEM", base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kUseMLKEM, "UseMLKEM", base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kNetUnusedIdleSocketTimeout,
              "NetUnusedIdleSocketTimeout",
