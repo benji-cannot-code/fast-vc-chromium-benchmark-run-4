637209281f73f0eaf24ecdd22773a25c382041b6
Ritika Gupta <ritikagup@google.com>
2024-10-07 16:26:57 +0000

    [Data Sharing] Cleaning up GroupMemberListView usage.
    Bug: b/369151888
    Change-Id: If85e44ea384d13c01f4361f0de425a56435d3694
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913353
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Ritika Gupta <ritikagup@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364977}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingTabManagerUnitTest.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/DataSharingTabManagerUnitTest.java
@@ -285,13 +285,6 @@ public class DataSharingTabManagerUnitTest {
 
         verify(mBottomSheetController).requestShowContent(any(), eq(true));
         verify(mBottomSheetController).addObserver(mBottomSheetObserverCaptor.capture());
-        verify(mDataSharingUIDelegate)
-                .createGroupMemberListView(
-                        eq(mActivity),
-                        /* view= */ any(),
-                        eq(GROUP_ID),
-                        /* tokenSecret= */ any(),
-                        /* config= */ any());
         mBottomSheetObserverCaptor.getValue().onSheetClosed(StateChangeReason.SWIPE);
     }
 
