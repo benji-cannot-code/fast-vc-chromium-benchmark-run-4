c834d677c045dbe88dd0dda4c5439e9894f36e10
Darren Shen <shend@chromium.org>
2024-10-07 01:33:20 +0000

    Revert "Remove image media for insertion"
    This reverts commit 6fbe1e815f347d6a99d39b2232a7f063ed1f6385.
    
    Reason for revert: Reland
    
    Original change's description:
    > Remove image media for insertion
    >
    > Bug: b:349891147
    > Change-Id: I6a6a2100a71201f5b79c90f148c1baf621dc275e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5670223
    > Reviewed-by: Darren Shen <shend@chromium.org>
    > Commit-Queue: Michael Cui <mlcui@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1324014}
    
    Bug: b:349891147
    Change-Id: I6a7fdf8e065ca012e9e4ddf19cd4d33405683764
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5884675
    Reviewed-by: Michael Cui <mlcui@google.com>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364750}

--- ash/picker/picker_rich_media.h
+++ ash/picker/picker_rich_media.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/files/file_path.h"
+#include "ui/gfx/geometry/size.h"
 #include "url/gurl.h"
 
 namespace ash {
@@ -21,6 +22,20 @@ struct ASH_EXPORT PickerTextMedia {
 
   explicit PickerTextMedia(std::u16string text);
   explicit PickerTextMedia(std::string_view text);
+};
+
+// TODO: b/349891147 - Remove GIFs.
+struct ASH_EXPORT PickerImageMedia {
+  GURL url;
+  // `dimensions` is std::nullopt if it's unknown.
+  std::optional<gfx::Size> dimensions;
+  std::u16string content_description;
+
+  explicit PickerImageMedia(base::span<const uint8_t> png);
+
+  explicit PickerImageMedia(GURL url,
+                            std::optional<gfx::Size> dimensions = std::nullopt,
+                            std::u16string content_description = u"");
 };
 
 struct ASH_EXPORT PickerLinkMedia {
@@ -37,8 +52,10 @@ struct ASH_EXPORT PickerLocalFileMedia {
 };
 
 // Rich media that can be inserted or copied, such as text and images.
-using PickerRichMedia =
-    std::variant<PickerTextMedia, PickerLinkMedia, PickerLocalFileMedia>;
+using PickerRichMedia = std::variant<PickerTextMedia,
+                                     PickerImageMedia,
+                                     PickerLinkMedia,
+                                     PickerLocalFileMedia>;
 
 }  // namespace ash
 
