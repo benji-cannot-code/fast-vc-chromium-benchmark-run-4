c78f3f1389b9bba64d2505bea8430737d3dcaf4c
Salma Elmahallawy <esalma@google.com>
2024-10-07 15:40:49 +0000

    [EnterprisePill] adding new policy to disable toolbar enterprise badging
    Bug: 371055913
    Change-Id: Ie50905091e8443b27db8133d1fcbbcb81825a3a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906509
    Commit-Queue: Salma Elmahallawy <esalma@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Owen Min <zmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364961}

--- chrome/browser/enterprise/util/managed_browser_utils.cc
+++ chrome/browser/enterprise/util/managed_browser_utils.cc
@@ -223,6 +223,11 @@ ManagementEnvironment GetManagementEnvironment(
                                      : ManagementEnvironment::kWork;
 }
 
+bool IsEnterpriseBadgingEnabledForToolbar(Profile* profile) {
+  return profile->GetPrefs()->GetInteger(
+             prefs::kEnterpriseProfileBadgeToolbarSettings) == 0;
+}
+
 bool CanShowEnterpriseBadging(Profile* profile) {
   if (!UserAcceptedAccountManagement(profile)) {
     return false;
