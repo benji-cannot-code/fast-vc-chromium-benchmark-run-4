f2677b48155023e2a6ce94e7e370225eb8c0836a
Satoshi Niwa <niwa@google.com>
2024-10-07 05:50:06 +0000

    Record Arc.Data.AppCategory.{Target}.DataSize UMA in ArcAppListPrefs
    UMA histograms.xml: CL:5869652
    Design doc: go/arcvm-data-metrics
    
    BUG=b:369958247
    BUG=b:366376000
    TEST=CQ
    TEST=Sign in and check chrome://histograms
    
    
    Change-Id: Ib72e9ed43bb4b2ab44c84761bc37efe3189eadb1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898195
    Commit-Queue: Satoshi Niwa <niwa@chromium.org>
    Reviewed-by: Momoko Hattori <momohatt@chromium.org>
    Reviewed-by: Long Cheng <lgcheng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1364785}

--- chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
+++ chrome/browser/ash/app_list/arc/arc_app_list_prefs.cc
@@ -70,6 +70,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_scale_factor.h"
 #include "ui/gfx/codec/png_codec.h"
 
+using arc::mojom::AppCategory;
+
 namespace {
 
 constexpr char kActivity[] = "activity";
@@ -147,6 +149,11 @@ constexpr const char kAppCountUmaPrefix[] = "Arc.AppCount.";
 constexpr int kAppCountUmaExclusiveMax = 101;
 // A smaller bucket size for apps with a lower count.
 constexpr int kAppCountUmaExclusiveMaxLower = 20;
+
+// Constants for "Arc.Data.AppCategory.{Target}.DataSize" UMA metric.
+constexpr int kUmaDataSizeNumBuckets = 50;
+constexpr int kUmaDataSizeInMBMin = 1;
+constexpr int kUmaDataSizeInMBMax = 1000000;  // 1 TB.
 
 // Accessor for deferred set notifications enabled requests in prefs.
 class NotificationsEnabledDeferred {
@@ -465,6 +472,16 @@ void OnArcAppListRefreshed(Profile* profile) {
         ->OnArcAppListReady();
   }
 }
+
+void RecordAppCategoryDataSizeUma(const std::string& category,
+                                  uint64_t data_size_in_bytes) {
+  const std::string metrics =
+      base::StringPrintf("Arc.Data.AppCategory.%s.DataSize", category);
+  base::UmaHistogramCustomCounts(metrics, data_size_in_bytes / 1000000,
+                                 kUmaDataSizeInMBMin, kUmaDataSizeInMBMax,
+                                 kUmaDataSizeNumBuckets);
+}
+
 }  // namespace
 
 // static
@@ -1382,6 +1399,40 @@ void ArcAppListPrefs::RecordAppIdsUma() {
       has_installed_apps);
 }
 
+void ArcAppListPrefs::RecordAppCategoryDataSizeListUma(
+    std::vector<arc::mojom::AppInfoPtr> apps) {
+  if (app_category_data_size_uma_recorded_) {
+    // "Arc.Data.AppCategory.{Target}.DataSize" should be recorded only once
+    // per session.
+    return;
+  }
+
+  // Calculate combined data bytes for each app category.
+  std::map<arc::mojom::AppCategory, uint64_t> data_bytes_map;
+  for (const auto& app : apps) {
+    if (app->app_storage.is_null()) {
+      continue;
+    }
+    data_bytes_map[app->app_category] += app->app_storage->data_size_in_bytes;
+  }
+
+  VLOG(1) << "Recording Arc.Data.AppCategory.{Target}.DataSize UMA";
+  RecordAppCategoryDataSizeUma("Game", data_bytes_map[AppCategory::kGame]);
+  RecordAppCategoryDataSizeUma("Audio", data_bytes_map[AppCategory::kAudio]);
+  RecordAppCategoryDataSizeUma("Video", data_bytes_map[AppCategory::kVideo]);
+  RecordAppCategoryDataSizeUma("Image", data_bytes_map[AppCategory::kImage]);
+  RecordAppCategoryDataSizeUma("Social", data_bytes_map[AppCategory::kSocial]);
+  RecordAppCategoryDataSizeUma("Productivity",
+                               data_bytes_map[AppCategory::kProductivity]);
+  const uint64_t data_bytes_for_other_category =
+      data_bytes_map[AppCategory::kUndefined] +
+      data_bytes_map[AppCategory::kNews] + data_bytes_map[AppCategory::kMaps] +
+      data_bytes_map[AppCategory::kAccessibility];
+  RecordAppCategoryDataSizeUma("Other", data_bytes_for_other_category);
+
+  app_category_data_size_uma_recorded_ = true;
+}
+
 void ArcAppListPrefs::OnPolicySent(const std::string& policy) {
   // Update set of packages installed by policy.
   packages_by_policy_ =
@@ -2073,6 +2124,9 @@ void ArcAppListPrefs::OnAppListRefreshed(
       MaybeSetDefaultAppLoadingTimeout();
     }
   }
+
+  RecordAppCategoryDataSizeListUma(std::move(apps));
+
   OnArcAppListRefreshed(profile_);
 }
 
