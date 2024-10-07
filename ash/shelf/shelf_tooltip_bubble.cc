15b5417e53178c812a999667521f97a57e67a67a
Sean Kau <skau@chromium.org>
2024-10-07 23:45:59 +0000

    Cleanup Jellyroll flag for ShelfTooltipBubble
    Bug: None
    Change-Id: I934049b20388dd8231c27650dbedea222d784110
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900414
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sean Kau <skau@chromium.org>
    Auto-Submit: Sean Kau <skau@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365235}

--- ash/shelf/shelf_tooltip_bubble.cc
+++ ash/shelf/shelf_tooltip_bubble.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/style/ash_color_id.h"
 #include "ash/style/style_util.h"
 #include "ash/wm/collision_detection/collision_detection_utils.h"
-#include "chromeos/constants/chromeos_features.h"
 #include "ui/aura/window.h"
 #include "ui/color/color_id.h"
 #include "ui/views/controls/label.h"
@@ -17,20 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/fill_layout.h"
 
 namespace ash {
-namespace {
-
-// Shelf item tooltip height.
-constexpr int kTooltipHeight = 24;
-
-// The maximum width of the tooltip bubble.  Borrowed the value from
-// ash/tooltip/tooltip_controller.cc
-constexpr int kTooltipMaxWidth = 250;
-
-// Shelf item tooltip internal text margins.
-constexpr int kTooltipTopBottomMargin = 4;
-constexpr int kTooltipLeftRightMargin = 8;
-
-}  // namespace
 
 ShelfTooltipBubble::ShelfTooltipBubble(
     views::View* anchor,
@@ -44,21 +29,9 @@ ShelfTooltipBubble::ShelfTooltipBubble(
   set_shadow(views::BubbleBorder::NO_SHADOW);
   SetLayoutManager(std::make_unique<views::FillLayout>());
 
-  if (chromeos::features::IsJellyrollEnabled()) {
-    set_margins(gfx::Insets(0));
-    auto* tooltip_view = AddChildView(StyleUtil::CreateAshStyleTooltipView());
-    tooltip_view->SetText(text);
-  } else {
-    set_margins(
-        gfx::Insets::VH(kTooltipTopBottomMargin, kTooltipLeftRightMargin));
-    auto label = std::make_unique<views::Label>(text);
-    label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-
-    // Initialize color ids
-    label->SetEnabledColorId(kColorAshShelfTooltipForegroundColor);
-    label->SetBackgroundColorId(kColorAshShelfTooltipBackgroundColor);
-    AddChildView(std::move(label));
-  }
+  set_margins(gfx::Insets(0));
+  auto* tooltip_view = AddChildView(StyleUtil::CreateAshStyleTooltipView());
+  tooltip_view->SetText(text);
 
   CreateBubble();
 
@@ -66,34 +39,12 @@ ShelfTooltipBubble::ShelfTooltipBubble(
       GetWidget()->GetNativeWindow());
 }
 
-void ShelfTooltipBubble::OnThemeChanged() {
-  ShelfBubble::OnThemeChanged();
-
-  if (chromeos::features::IsJellyrollEnabled()) {
-    return;
-  }
-
-  const auto* color_provider = GetColorProvider();
-
-  // TODO(b/261653838): Update this function to use color id instead.
-  set_color(color_provider->GetColor(kColorAshShelfTooltipBackgroundColor));
-
-  // Updates the background color in the bubble frame view.
-  GetBubbleFrameView()->SetBackgroundColor(color());
-}
-
 gfx::Size ShelfTooltipBubble::CalculatePreferredSize(
     const views::SizeBounds& available_size) const {
   const gfx::Size size =
       BubbleDialogDelegateView::CalculatePreferredSize(available_size);
 
-  if (chromeos::features::IsJellyrollEnabled()) {
-    return size;
-  }
-
-  const int kTooltipMinHeight = kTooltipHeight - 2 * kTooltipTopBottomMargin;
-  return gfx::Size(std::min(size.width(), kTooltipMaxWidth),
-                   std::max(size.height(), kTooltipMinHeight));
+  return size;
 }
 
 bool ShelfTooltipBubble::ShouldCloseOnPressDown() {
