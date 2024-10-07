a60c09e42d68894d8eedfb2a50370b497abb1898
Sorin Jianu <sorin@chromium.org>
2024-10-07 11:57:21 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/data_sharing
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=ssid@chromium.org
    
    Bug: 371316188
    Change-Id: If7b7695acbc6fded63a22595fd95d57752bf301a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906890
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364876}

--- components/data_sharing/test_support/mock_data_sharing_network_loader.cc
+++ components/data_sharing/test_support/mock_data_sharing_network_loader.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace data_sharing {
 
-MockDataSharingNetworkLoader::MockDataSharingNetworkLoader() {}
+MockDataSharingNetworkLoader::MockDataSharingNetworkLoader() = default;
 
-MockDataSharingNetworkLoader::~MockDataSharingNetworkLoader() {}
+MockDataSharingNetworkLoader::~MockDataSharingNetworkLoader() = default;
 
 }  // namespace data_sharing
