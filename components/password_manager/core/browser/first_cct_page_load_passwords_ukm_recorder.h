8033ad19c95f0edb930900c8303a02054e57f008
Takuto Ikuta <tikuta@chromium.org>
2024-10-07 15:03:42 +0000

    components: include necessary header for std::unique_ptr
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/try/android-x64-rel/220809/overview
    when we enable _LIBCPP_REMOVE_TRANSITIVE_INCLUDES.
    
    Bug: 371924177
    Change-Id: I5027b8223fd207c979fc41c21d208c9880e841b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904259
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1364934}

--- components/password_manager/core/browser/first_cct_page_load_passwords_ukm_recorder.h
+++ components/password_manager/core/browser/first_cct_page_load_passwords_ukm_recorder.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_FIRST_CCT_PAGE_LOAD_PASSWORDS_UKM_RECORDER_H_
 #define COMPONENTS_PASSWORD_MANAGER_CORE_BROWSER_FIRST_CCT_PAGE_LOAD_PASSWORDS_UKM_RECORDER_H_
 
+#include <memory>
+
 #include "services/metrics/public/cpp/ukm_source_id.h"
 
 namespace ukm {
