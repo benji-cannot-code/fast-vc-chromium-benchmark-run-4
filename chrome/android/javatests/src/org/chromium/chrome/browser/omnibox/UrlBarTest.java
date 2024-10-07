566b465a04826f013af808a278f6827480f9c113
dmblack <dmblack@google.com>
2024-10-07 20:35:44 +0000

    Reland "Add fieldtrial testing config entry for RetainOmniboxOnFocus experiment."
    This is a reland of commit 784f56bbe8138abcb19a2be68923bc04893e848d
    
    Original change's description:
    > Add fieldtrial testing config entry for RetainOmniboxOnFocus experiment.
    >
    > This CL adds a fieldtrial testing config entry for the
    > RetainOmniboxOnFocus experiment. This is required in order to initiate
    > a finch experiment on channels >= dev.
    >
    > Note that this CL also updates test failures which it introduces.
    >
    > Design doc: http://shortn/_uCcl1jb7oA
    >
    > Bug: 347632178
    > Change-Id: I722497c1858b8aee5768ce646fd53b3cb1c76ab5
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902119
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Commit-Queue: David Black <dmblack@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1362762}
    
    Bug: 347632178
    Change-Id: Ibae13139ee6917c6c569cf897d95e469a7145f5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904715
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: David Black <dmblack@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365129}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.omnibox;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assume.assumeFalse;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -41,6 +42,7 @@ import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
+import org.chromium.components.omnibox.OmniboxFeatures;
 import org.chromium.content_public.common.ContentUrlConstants;
 import org.chromium.ui.base.Clipboard;
 
@@ -673,6 +675,30 @@ public class UrlBarTest {
     @Test
     @SmallTest
     public void typingStarted_emittedOncePerFocus() {
+        typingStarted_emittedOncePerFocus(
+                /* expectRetainOmniboxOnFocus= */ ThreadUtils.runOnUiThreadBlocking(
+                        OmniboxFeatures::shouldRetainOmniboxOnFocus));
+    }
+
+    @Test
+    @SmallTest
+    public void typingStarted_emittedOncePerFocusWithRetainOmniboxOnFocusDisabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.FALSE);
+        typingStarted_emittedOncePerFocus(/* expectRetainOmniboxOnFocus= */ false);
+    }
+
+    @Test
+    @SmallTest
+    public void typingStarted_emittedOncePerFocusWithRetainOmniboxOnFocusEnabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.TRUE);
+        typingStarted_emittedOncePerFocus(/* expectRetainOmniboxOnFocus= */ true);
+    }
+
+    private void typingStarted_emittedOncePerFocus(boolean expectRetainOmniboxOnFocus) {
+        assumeFalse(
+                "TODO(crbug.com/347632178): Fix emit timing when retaining omnibox on focus.",
+                expectRetainOmniboxOnFocus);
+
         var listener = mock(Runnable.class);
 
         mOmnibox.clearFocus();
@@ -696,6 +722,30 @@ public class UrlBarTest {
     @Test
     @SmallTest
     public void typingStarted_emittedOnceEveryFocus() {
+        typingStarted_emittedOnceEveryFocus(
+                /* expectRetainOmniboxOnFocus= */ ThreadUtils.runOnUiThreadBlocking(
+                        OmniboxFeatures::shouldRetainOmniboxOnFocus));
+    }
+
+    @Test
+    @SmallTest
+    public void typingStarted_emittedOnceEveryFocusWithRetainOmniboxOnFocusDisabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.FALSE);
+        typingStarted_emittedOnceEveryFocus(/* expectRetainOmniboxOnFocus= */ false);
+    }
+
+    @Test
+    @SmallTest
+    public void typingStarted_emittedOnceEveryFocusWithRetainOmniboxOnFocusEnabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.TRUE);
+        typingStarted_emittedOnceEveryFocus(/* expectRetainOmniboxOnFocus= */ true);
+    }
+
+    private void typingStarted_emittedOnceEveryFocus(boolean expectRetainOmniboxOnFocus) {
+        assumeFalse(
+                "TODO(crbug.com/347632178): Fix emit timing when retaining omnibox on focus.",
+                expectRetainOmniboxOnFocus);
+
         var listener = mock(Runnable.class);
 
         mOmnibox.clearFocus();
@@ -722,6 +772,33 @@ public class UrlBarTest {
     @SmallTest
     @RequiresRestart("crbug.com/358170962")
     public void typingStarted_notEmittedForNonTypingCharacters() {
+        typingStarted_notEmittedForNonTypingCharacters(
+                /* expectRetainOmniboxOnFocus= */ ThreadUtils.runOnUiThreadBlocking(
+                        OmniboxFeatures::shouldRetainOmniboxOnFocus));
+    }
+
+    @Test
+    @SmallTest
+    @RequiresRestart("crbug.com/358170962")
+    public void typingStarted_notEmittedForNonTypingCharactersWithRetainOmniboxOnFocusDisabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.FALSE);
+        typingStarted_notEmittedForNonTypingCharacters(/* expectRetainOmniboxOnFocus= */ false);
+    }
+
+    @Test
+    @SmallTest
+    @RequiresRestart("crbug.com/358170962")
+    public void typingStarted_notEmittedForNonTypingCharactersWithRetainOmniboxOnFocusEnabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.TRUE);
+        typingStarted_notEmittedForNonTypingCharacters(/* expectRetainOmniboxOnFocus= */ true);
+    }
+
+    private void typingStarted_notEmittedForNonTypingCharacters(
+            boolean expectRetainOmniboxOnFocus) {
+        assumeFalse(
+                "TODO(crbug.com/347632178): Fix emit timing when retaining omnibox on focus.",
+                expectRetainOmniboxOnFocus);
+
         var listener = mock(Runnable.class);
 
         mOmnibox.clearFocus();
@@ -747,6 +824,32 @@ public class UrlBarTest {
     @Test
     @SmallTest
     public void typingStarted_clipboardPasteTriggersTypingStarted() {
+        typingStarted_clipboardPasteTriggersTypingStarted(
+                /* expectRetainOmniboxOnFocus= */ ThreadUtils.runOnUiThreadBlocking(
+                        OmniboxFeatures::shouldRetainOmniboxOnFocus));
+    }
+
+    @Test
+    @SmallTest
+    public void
+            typingStarted_clipboardPasteTriggersTypingStartedWithRetainOmniboxOnFocusDisabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.FALSE);
+        typingStarted_clipboardPasteTriggersTypingStarted(/* expectRetainOmniboxOnFocus= */ false);
+    }
+
+    @Test
+    @SmallTest
+    public void typingStarted_clipboardPasteTriggersTypingStartedWithRetainOmniboxOnFocusEnabled() {
+        OmniboxFeatures.setShouldRetainOmniboxOnFocusForTesting(Boolean.TRUE);
+        typingStarted_clipboardPasteTriggersTypingStarted(/* expectRetainOmniboxOnFocus= */ true);
+    }
+
+    private void typingStarted_clipboardPasteTriggersTypingStarted(
+            boolean expectRetainOmniboxOnFocus) {
+        assumeFalse(
+                "TODO(crbug.com/347632178): Fix emit timing when retaining omnibox on focus.",
+                expectRetainOmniboxOnFocus);
+
         var listener = mock(Runnable.class);
 
         mOmnibox.clearFocus();
@@ -758,8 +861,7 @@ public class UrlBarTest {
                 () -> {
                     Clipboard.getInstance().setText("");
                     // Paste directly. This is because Keyboard paste normally goes through an IME,
-                    // which
-                    // requires a lengthier process, rendering test flaky.
+                    // which requires a lengthier process, rendering test flaky.
                     mUrlBar.onTextContextMenuItem(android.R.id.paste);
                 });
         verifyNoInteractions(listener);
@@ -768,8 +870,7 @@ public class UrlBarTest {
                 () -> {
                     Clipboard.getInstance().setText("asdf");
                     // Paste directly. This is because Keyboard paste normally goes through an IME,
-                    // which
-                    // requires a lengthier process, rendering test flaky.
+                    // which requires a lengthier process, rendering test flaky.
                     mUrlBar.onTextContextMenuItem(android.R.id.paste);
                 });
         verify(listener).run();
