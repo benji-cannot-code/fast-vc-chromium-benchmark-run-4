59f43f008072158d1b94cdf1fda165bc44965989
EmiliaPaz <emiliapaz@chromium.org>
2024-10-08 20:22:28 +0000

    [Extensions] Add metrics for MV2 disabled dialog
    Chrome has announced that MV2 extensions will gradually no longer be
    supported in the browser. See more information at
    https://developer.chrome.com/blog/resuming-the-transition-to-mv3.
    
    The second and third stage of the deprecation will disable unsupported
    MV2 extensions, and shows a 'disabled dialog' on browser window startup
    with extensions that have been disabled due to the MV2 deprecation. This
    CL adds metrics for the remove and manage buttons for each stage.
    
    Bug: 367395349
    Change-Id: I2feab4a62d0cfac733e66ce368c2f21fec374e2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914717
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Reviewed-by: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365780}

--- chrome/browser/ui/extensions/mv2_disabled_dialog_controller.cc
+++ chrome/browser/ui/extensions/mv2_disabled_dialog_controller.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/barrier_closure.h"
 #include "base/check_is_test.h"
 #include "base/functional/bind.h"
+#include "base/metrics/user_metrics.h"
+#include "base/metrics/user_metrics_action.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/manifest_v2_experiment_manager.h"
 #include "chrome/browser/extensions/mv2_experiment_stage.h"
@@ -258,6 +260,16 @@ void Mv2DisabledDialogController::OnRemoveSelected() {
     extension_service->UninstallExtension(
         extension_info.id, UNINSTALL_REASON_USER_INITIATED, nullptr);
   }
+
+  if (experiment_stage_ == MV2ExperimentStage::kDisableWithReEnable) {
+    base::RecordAction(base::UserMetricsAction(
+        "Extensions.Mv2Deprecation.Disabled.RemoveExtension.DisabledDialog"));
+  } else {
+    CHECK_EQ(experiment_stage_, MV2ExperimentStage::kUnsupported);
+    base::RecordAction(
+        base::UserMetricsAction("Extensions.Mv2Deprecation.Unsupported."
+                                "RemoveExtension.DisabledDialog"));
+  }
 }
 
 void Mv2DisabledDialogController::OnManageSelected() {
@@ -276,6 +288,16 @@ void Mv2DisabledDialogController::UserAcknowledgedDialog() {
       GetDisabledDialogAcknowledgedPref(experiment_stage_);
   for (const auto& extension_info : affected_extensions_info_) {
     extension_prefs->SetBooleanPref(extension_info.id, dialog_ack_pref, true);
+  }
+
+  if (experiment_stage_ == MV2ExperimentStage::kDisableWithReEnable) {
+    base::RecordAction(base::UserMetricsAction(
+        "Extensions.Mv2Deprecation.Disabled.ManageExtension.DisabledDialog"));
+  } else {
+    CHECK_EQ(experiment_stage_, MV2ExperimentStage::kUnsupported);
+    base::RecordAction(
+        base::UserMetricsAction("Extensions.Mv2Deprecation.Unsupported."
+                                "ManageExtension.DisabledDialog"));
   }
 }
 
