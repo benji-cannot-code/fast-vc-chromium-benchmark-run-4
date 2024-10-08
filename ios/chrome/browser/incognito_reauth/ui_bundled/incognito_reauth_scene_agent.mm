377dbd736ea3f3cffce08d330298c7f825798506
Rubin Deliallisi <rubindl@chromium.org>
2024-10-08 16:08:51 +0000

    [iOS][TM] Add tri state incognito lock to the scene agent
    - Add a tri state enum to the IncognitoReauthStateAgent.
    - Incorporate the new enum into the class so that the current interface
    and features work as is and the new soft lock features can be supported
    in the future.
    - Introduce the 1 tap unlock for Soft Lock, with the existing reauth
    UI.
    
    Bug: 370804664
    Change-Id: Iaaa93b152113488933e9f3988c9f729a0a5126e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912898
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365600}

--- ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_scene_agent.mm
+++ ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_scene_agent.mm
@@ -14,6 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "components/pref_registry/pref_registry_syncable.h"
 #import "components/prefs/pref_service.h"
+#import "ios/chrome/browser/incognito_reauth/ui_bundled/features.h"
+#import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_constants.h"
+#import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_util.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/browser/browser_provider.h"
@@ -21,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/model/prefs/pref_names.h"
 #import "ios/chrome/browser/shared/model/web_state_list/web_state_list.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/incognito_reauth/ui_bundled/incognito_reauth_util.h"
 #import "ios/chrome/common/ui/reauthentication/reauthentication_protocol.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/web/public/web_state.h"
@@ -72,9 +74,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (BOOL)isAuthenticationRequired {
-  return [self featureEnabled] &&
-         self.windowHadIncognitoContentWhenBackgrounded &&
-         !self.authenticatedSinceLastForeground;
+  return self.incognitoLockState != IncognitoLockState::kNone;
+}
+
+- (IncognitoLockState)incognitoLockState {
+  if (self.windowHadIncognitoContentWhenBackgrounded &&
+      !self.authenticatedSinceLastForeground) {
+    if ([self isReauthFeatureEnabled]) {
+      return IncognitoLockState::kReauth;
+    } else if ([self isSoftLockFeatureEnabled]) {
+      return IncognitoLockState::kSoftLock;
+    }
+  }
+
+  return IncognitoLockState::kNone;
 }
 
 - (void)authenticateIncognitoContent {
@@ -85,8 +98,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (void (^)(BOOL success))completion {
   DCHECK(self.reauthModule);
 
-  if (!self.isAuthenticationRequired) {
-    if (self.featureEnabled) {
+  if (![self isAuthenticationRequired]) {
+    if ([self areLockFeaturesEnabled]) {
       [self notifyObservers];
     }
     // If reauthentication is not required, it should be considered a success
@@ -98,28 +111,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
 
-  base::RecordAction(base::UserMetricsAction(
-      "MobileIncognitoBiometricAuthenticationRequested"));
-
-  NSString* authReason = l10n_util::GetNSStringF(
-      IDS_IOS_INCOGNITO_REAUTH_SYSTEM_DIALOG_REASON,
-      base::SysNSStringToUTF16(BiometricAuthenticationTypeString()));
-
-  __weak IncognitoReauthSceneAgent* weakSelf = self;
-  void (^completionHandler)(ReauthenticationResult) =
-      ^(ReauthenticationResult result) {
-        BOOL success = (result == ReauthenticationResult::kSuccess);
-        base::UmaHistogramBoolean(
-            "IOS.Incognito.BiometricReauthAttemptSuccessful", success);
-
-        weakSelf.authenticatedSinceLastForeground = success;
-        if (completion) {
-          completion(success);
-        }
-      };
-  [self.reauthModule attemptReauthWithLocalizedReason:authReason
-                                 canReusePreviousAuth:false
-                                              handler:completionHandler];
+  if ([self isReauthFeatureEnabled]) {
+    [self reauthIncognitoContentWithCompletionBlock:completion];
+  } else if ([self isSoftLockFeatureEnabled]) {
+    [self unlockIncognitoContentWithCompletionBlock:completion];
+  }
 }
 
 - (void)addObserver:(id<IncognitoReauthObserver>)observer {
@@ -134,7 +130,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)setAuthenticatedSinceLastForeground:(BOOL)authenticated {
   _authenticatedSinceLastForeground = authenticated;
-  if (self.featureEnabled) {
+  if ([self areLockFeaturesEnabled]) {
     [self notifyObservers];
   }
 }
@@ -154,7 +150,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   self.windowHadIncognitoContentWhenBackgrounded = hasIncognitoContent;
 
-  if (self.featureEnabled) {
+  if ([self areLockFeaturesEnabled]) {
     [self notifyObservers];
   }
 }
@@ -164,13 +160,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return;
   }
   _windowHadIncognitoContentWhenBackgrounded = hadIncognitoContent;
-  if (self.featureEnabled) {
+  if ([self areLockFeaturesEnabled]) {
     [self notifyObservers];
   }
 }
 
 - (void)notifyObservers {
-  DCHECK(self.featureEnabled);
+  DCHECK([self areLockFeaturesEnabled]);
   [self.observers reauthAgent:self
       didUpdateAuthenticationRequirement:self.isAuthenticationRequired];
 }
@@ -227,16 +223,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Convenience method to check the pref associated with the reauth setting and
 // the feature flag.
-- (BOOL)featureEnabled {
+- (BOOL)isReauthFeatureEnabled {
   return self.localState &&
          self.localState->GetBoolean(prefs::kIncognitoAuthenticationSetting);
 }
 
-// Closes the media presentations to avoid having the fullscreen video on top of
-// the blocker.
+// Convenience method to check the pref associated with the soft lock setting
+// and the feature flag.
+- (BOOL)isSoftLockFeatureEnabled {
+  // TODO(crbug.com/370804664): Add pref check when the settings page is
+  // available.
+  return IsIOSSoftLockEnabled();
+}
+
+// Convenience method to check whether any of the locking features are enabled.
+- (BOOL)areLockFeaturesEnabled {
+  return [self isReauthFeatureEnabled] || [self isSoftLockFeatureEnabled];
+}
+
+// Closes the media presentations to avoid having the fullscreen video on
+// top of the blocker.
 - (void)closeMediaPresentations {
-  if (![self featureEnabled])
+  if (![self areLockFeaturesEnabled]) {
     return;
+  }
 
   Browser* browser =
       self.sceneState.browserProviderInterface.incognitoBrowserProvider.browser;
@@ -248,6 +258,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           ->CloseMediaPresentations();
     }
   }
+}
+
+// Marks the scene as authenticated and, upon completion, will notify observers
+// and call the completion block (passing authentication result).
+- (void)unlockIncognitoContentWithCompletionBlock:
+    (void (^)(BOOL success))completion {
+  self.authenticatedSinceLastForeground = YES;
+  if (completion) {
+    completion(YES);
+  }
+}
+
+// Requests authentication and marks the scene as authenticated until the next
+// scene foregrounding.
+// The authentication will require user interaction. Upon completion, will
+// notify observers and call the completion block (passing authentication
+// result).
+- (void)reauthIncognitoContentWithCompletionBlock:
+    (void (^)(BOOL success))completion {
+  base::RecordAction(base::UserMetricsAction(
+      "MobileIncognitoBiometricAuthenticationRequested"));
+
+  NSString* authReason = l10n_util::GetNSStringF(
+      IDS_IOS_INCOGNITO_REAUTH_SYSTEM_DIALOG_REASON,
+      base::SysNSStringToUTF16(BiometricAuthenticationTypeString()));
+
+  __weak IncognitoReauthSceneAgent* weakSelf = self;
+  void (^completionHandler)(ReauthenticationResult) =
+      ^(ReauthenticationResult result) {
+        BOOL success = (result == ReauthenticationResult::kSuccess);
+        base::UmaHistogramBoolean(
+            "IOS.Incognito.BiometricReauthAttemptSuccessful", success);
+
+        weakSelf.authenticatedSinceLastForeground = success;
+        if (completion) {
+          completion(success);
+        }
+      };
+  [self.reauthModule attemptReauthWithLocalizedReason:authReason
+                                 canReusePreviousAuth:false
+                                              handler:completionHandler];
 }
 
 @end
