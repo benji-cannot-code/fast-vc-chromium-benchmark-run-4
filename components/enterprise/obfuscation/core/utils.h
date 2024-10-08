906f59a9116297a08af4567f8a4211f9ea66bb44
Haihan Chen <haihan@google.com>
2024-10-08 17:52:19 +0000

    [File Download Access Prevention] Turn enterprise_obfuscation into a component
    This fixes issues with component builds if the feature is being imported in different locations, as the flag gets redefined.
    
    Bug: b/369634680
    Change-Id: I091d4066b1985d748049185bc1d0eb1e4a172e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913261
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365680}

--- components/enterprise/obfuscation/core/utils.h
+++ components/enterprise/obfuscation/core/utils.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_ENTERPRISE_OBFUSCATION_CORE_UTILS_H_
 #define COMPONENTS_ENTERPRISE_OBFUSCATION_CORE_UTILS_H_
 
+#include "base/component_export.h"
 #include "base/feature_list.h"
 #include "base/types/expected.h"
 
@@ -35,9 +36,11 @@ static constexpr size_t kChunkSizePrefixSize = 4u;
 
 // Feature to enable insecure obfuscation and deobfuscation of files sent to
 // WebProtect deep scanning service for enterprise users.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 BASE_DECLARE_FEATURE(kEnterpriseFileObfuscation);
 
 // Returns true if `kEnterpriseFileObfuscation` feature is enabled.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 bool IsFileObfuscationEnabled();
 
 // Error types for insecure obfuscation and deobfuscation operations.
@@ -52,6 +55,7 @@ enum class Error {
 // Returns the header and populates the derived key and nonce prefix values used
 // for obfuscating each chunk.
 // The header structure is: size of header (1 byte) | salt | noncePrefix.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<std::vector<uint8_t>, Error> CreateHeader(
     std::vector<uint8_t>* derived_key,
     std::vector<uint8_t>* nonce_prefix);
@@ -61,6 +65,7 @@ base::expected<std::vector<uint8_t>, Error> CreateHeader(
 // access deterrent. Master key is stored in memory and can be leaked.
 // Counter increments every chunk to protect against reordering/truncation.
 // The size of the encrypted chunk is prepended to the returned encrypted chunk.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<std::vector<uint8_t>, Error> ObfuscateDataChunk(
     base::span<const uint8_t> data,
     const std::vector<uint8_t>& key,
@@ -70,10 +75,12 @@ base::expected<std::vector<uint8_t>, Error> ObfuscateDataChunk(
 
 // Extracts the size of the obfuscated data chunk from the beginning of the
 // provided data.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<size_t, Error> GetObfuscatedChunkSize(
     base::span<const uint8_t> data);
 
 // Computes the derived key and extracts the nonce prefix from the header.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<std::pair</*derived key*/ std::vector<uint8_t>,
                          /*nonce prefix*/ std::vector<uint8_t>>,
                Error>
@@ -85,6 +92,7 @@ GetHeaderData(const std::vector<uint8_t>& header);
 // reordering/truncation.
 // The size of the encrypted chunk is expected to be prepended to the input
 // data.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<std::vector<uint8_t>, Error> DeobfuscateDataChunk(
     base::span<const uint8_t> data,
     const std::vector<uint8_t>& key,
@@ -94,6 +102,7 @@ base::expected<std::vector<uint8_t>, Error> DeobfuscateDataChunk(
 
 // Insecurely deobfuscate a file by replacing the original file with the
 // deobfuscated data. Master key is stored in memory and can be leaked.
+COMPONENT_EXPORT(ENTERPRISE_OBFUSCATION)
 base::expected<void, Error> DeobfuscateFileInPlace(
     const base::FilePath& file_path);
 
