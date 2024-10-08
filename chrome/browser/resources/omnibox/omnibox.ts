f1c548443f554891b3d1a23643d27c1619045517
Sven Zheng <svenzheng@chromium.org>
2024-10-08 21:30:42 +0000

    Revert "Show commandline variations on chrome://version page"
    This reverts commit 6c54aaa66c3d4647e3034bca6716ac8e08e34266.
    
    Reason for revert: There are some concerns about showing
    raw variations. See crbug.com/352162989#comment7
    
    Original change's description:
    > Show commandline variations on chrome://version page
    >
    > Launch bug: https://launch.corp.google.com/launch/4347542.
    > Show commandline variations on the chrome://version page
    > so it would be easier for user to report variations.
    >
    > Add a truncated commandline variations string, with a copy
    > button at the end. Screenshot can be seen in the png file.
    >
    > iOS specific: chrome://version/?show-variations-cmd didn't
    > work. Seems like a bug. Also fixed this issue that visiting
    > the link would show full variations commandline.
    >
    > iOS. Also tested screen reading works on Linux, Mac.
    >
    > Bug: b/352162989
    > Test: Tested the functionality works on Windows, Linux, Mac,
    > Change-Id: I7b289afc057c3790a9250f3db5df964f224ceb35
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874716
    > Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    > Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    > Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1363247}
    
    Bug: b/352162989
    Change-Id: I01a39e969e5f234b61a8017b4283b82f440d6526
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917329
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Sven Zheng <svenzheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365825}

--- chrome/browser/resources/omnibox/omnibox.ts
+++ chrome/browser/resources/omnibox/omnibox.ts
@@ -249,7 +249,7 @@ class ExportDelegate {
       batchExports.push(exportData);
     }
     const variationInfo =
-        await sendWithPromise('requestVariationInfo');
+        await sendWithPromise('requestVariationInfo', true);
     const pathInfo = await sendWithPromise('requestPathInfo');
 
     const now = new Date();
