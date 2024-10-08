c0ac3e315dff58f8fc5983a533455ed0c5278a57
Dominik Bylica <bylica@google.com>
2024-10-08 12:07:53 +0000

    Add IWA-specific managed configuration tests.
    Bug: 369338552
    Change-Id: Ie8818375ac4743c0ef51830c34bcc45ce435f95f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904919
    Commit-Queue: Dominik Bylica <bylica@google.com>
    Reviewed-by: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Auto-Submit: Dominik Bylica <bylica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365479}

--- third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
+++ third_party/blink/renderer/modules/managed_device/navigator_managed_data.cc
@@ -20,7 +20,10 @@ namespace blink {
 namespace {
 
 const char kNotHighTrustedAppExceptionMessage[] =
-    "This API is available only for managed apps.";
+    "Managed configuration is empty. This API is available only for "
+    "managed apps.";
+const char kServiceConnectionExceptionMessage[] =
+    "Service connection error. This API is available only for managed apps.";
 
 #if BUILDFLAG(IS_ANDROID)
 const char kManagedConfigNotSupported[] =
@@ -123,7 +126,7 @@ void NavigatorManagedData::OnServiceConnectionError() {
   for (ScriptPromiseResolverBase* resolver : pending_promises_) {
     resolver->Reject(
         MakeGarbageCollected<DOMException>(DOMExceptionCode::kNotAllowedError,
-                                           kNotHighTrustedAppExceptionMessage));
+                                           kServiceConnectionExceptionMessage));
   }
 }
 
