a7415196441dee43e4652fbea4f967224219c24b
Daniel White <danieltwhite@google.com>
2024-10-08 16:06:31 +0000

    [iOS][RBS] Fixed CardUnmaskView Crash
    The CardUnmaskView was crashing due to the improper usage of the
    TraitCollectionSetForTraits. The issue was that a single UITrait was
    being passed into the TraitCollectionSetForTraits function. By
    encapsulating this single UITrait in a NSArray, the crash was resolved.
    
    Fixed: 371734134
    Change-Id: I60da500cfb1fca2339f94d6f3a211278b06036bf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914309
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365596}

--- ios/chrome/browser/autofill/ui_bundled/cells/card_unmask_header_item.mm
+++ ios/chrome/browser/autofill/ui_bundled/cells/card_unmask_header_item.mm
@@ -122,7 +122,7 @@ const CGFloat kGooglePayBadgeHeight = 16;
 
     if (@available(iOS 17, *)) {
       [self registerForTraitChanges:TraitCollectionSetForTraits(
-                                        UITraitUserInterfaceStyle.self)
+                                        @[ UITraitUserInterfaceStyle.self ])
                          withAction:@selector(userInterfaceStyleDidChange)];
     }
   }
