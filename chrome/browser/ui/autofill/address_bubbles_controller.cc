df0b625e78676669f8f2bb73db3ceb8540e0a5b7
Nicolas MacBeth <nicolasmacbeth@google.com>
2024-10-08 14:55:01 +0000

    [Desktop to iOS promo] Maybe show iOS promo when user accepts flow
    Hook iOS address promo into the user flow for addresses autofill
    when user accepts the flow and hits save.
    
    Bug: b:331409768
    Change-Id: Idcb1a41053f3174a5086e3cf09c063d19eaf19d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900771
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Guillaume Jenkins <gujen@google.com>
    Reviewed-by: Dmitry Vykochko <vykochko@google.com>
    Commit-Queue: Nicolas MacBeth <nicolasmacbeth@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365552}

--- chrome/browser/ui/autofill/address_bubbles_controller.cc
+++ chrome/browser/ui/autofill/address_bubbles_controller.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/ui/ui_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/promos/promos_types.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
 #include "chrome/browser/ui/autofill/add_new_address_bubble_controller.h"
 #include "chrome/browser/ui/autofill/autofill_bubble_handler.h"
@@ -29,7 +30,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/promos/ios_promos_utils.h"
 #include "chrome/browser/ui/ui_features.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/grit/theme_resources.h"
 #include "components/autofill/content/browser/content_autofill_client.h"
 #include "components/autofill/core/browser/autofill_address_util.h"
@@ -175,6 +178,15 @@ void AddressBubblesController::OnUserDecision(
   if (address_profile_save_prompt_callback_) {
     std::move(address_profile_save_prompt_callback_).Run(decision, profile);
   }
+
+// TODO(crbug.com/372209715): Extract out of GOOGLE_CHROME_BRANDING flag.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  if ((decision == AutofillClient::AddressPromptUserDecision::kAccepted ||
+       decision == AutofillClient::AddressPromptUserDecision::kEditAccepted) &&
+      base::FeatureList::IsEnabled(::features::kIOSPromoAddressBubble)) {
+    MaybeShowIOSDektopAddressPromo();
+  }
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
 
 void AddressBubblesController::OnBubbleClosed() {
@@ -262,6 +274,19 @@ void AddressBubblesController::SetUpAndShowBubble(
   is_migration_to_account_ = is_migration_to_account;
 
   Show();
+}
+
+void AddressBubblesController::MaybeShowIOSDektopAddressPromo() {
+  // TODO(crbug.com/372209715): Extract out of GOOGLE_CHROME_BRANDING flag.
+#if BUILDFLAG(GOOGLE_CHROME_BRANDING)
+  Browser* browser = chrome::FindBrowserWithTab(web_contents());
+
+  // Verify if user is eligible for iOS promo, and attempt showing if they are.
+  ios_promos_utils::VerifyIOSPromoEligibility(
+      IOSPromoType::kAddress, browser->profile(),
+      BrowserView::GetBrowserViewForBrowser(browser)
+          ->toolbar_button_provider());
+#endif  // BUILDFLAG(GOOGLE_CHROME_BRANDING)
 }
 
 WEB_CONTENTS_USER_DATA_KEY_IMPL(AddressBubblesController);
