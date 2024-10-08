fc916211b753d181158fda5b2cd4a974024ad73f
nergi <nergi@google.com>
2024-10-08 04:30:59 +0000

    Cleanup feature flags for keyboard shortcut helper
    Previous CL: https://chromium-review.googlesource.com/c/chromium/src/+/3010907
    
    Bug: 368232833
    Test: autoninja
    Change-Id: Ic7c3372d06363943080e3a953d622cb3a1656831
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5911494
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Commit-Queue: Nergi Rahardi <nergi@google.com>
    Reviewed-by: Hiroki Sato <hirokisato@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365324}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3406,9 +3406,6 @@ extern const char kArcIgnoreHoverEventAnrDescription[];
 extern const char kArcInstantResponseWindowOpenName[];
 extern const char kArcInstantResponseWindowOpenDescription[];
 
-extern const char kArcKeyboardShortcutHelperIntegrationName[];
-extern const char kArcKeyboardShortcutHelperIntegrationDescription[];
-
 extern const char kArcNativeBridgeToggleName[];
 extern const char kArcNativeBridgeToggleDescription[];
 
