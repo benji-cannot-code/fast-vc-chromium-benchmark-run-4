8a9d0bbad25957e3fb185a731c811c6f0403aa5e
Zac Partridge <zacpartridge@google.com>
2024-10-08 04:12:35 +0000

    Add missing include for std::optional
    This is causing a build failure: https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/37649/overview
    
    Change-Id: Iaa3fb3a4d248f6ce3339693bac4ad1d4e2f49328
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913156
    Owners-Override: Qijiang Yūki Ishii <fqj@google.com>
    Commit-Queue: Zac Partridge <zacpartridge@google.com>
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365319}

--- chromeos/ash/components/boca/babelorca/tachyon_response_unittest.cc
+++ chromeos/ash/components/boca/babelorca/tachyon_response_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/boca/babelorca/tachyon_response.h"
 
 #include <memory>
+#include <optional>
 #include <string>
 
 #include "net/base/net_errors.h"
