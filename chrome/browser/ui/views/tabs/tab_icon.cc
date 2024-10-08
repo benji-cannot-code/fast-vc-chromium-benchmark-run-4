40b70d43c7d84dcbf2fc8e03e08a58d2da847e68
Kaan Alsan <alsan@chromium.org>
2024-10-08 14:43:37 +0000

    Remove tab icon's "network waiting" animation
    The tab icon had two distinct animations when loading a page:
    1) A counter-clockwise spinner, applied while waiting on the network.
    2) A clockwise spinner, applied after the network is ready and while
    loading content.
    
    This CL removes the former, applying only the clockwise spinner
    animation when loading a tab. This is animation is started from the
    second keyframe, where the spinner's arc size is minimal, rather than
    the first keyframe, where the spinner's arc size is at its largest (-270
    degrees).
    
    Before: https://screenshot.googleplex.com/8u7acfpYBnNUxhz.png
    After: https://screenshot.googleplex.com/SjFBsg5WbyYAGPd.png
    
    Bug: 326301356
    Change-Id: I687360566a235487a60c5ab0e7cd5a79c5eea59d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876439
    Reviewed-by: David Pennington <dpenning@chromium.org>
    Commit-Queue: Kaan Alsan <alsan@google.com>
    Reviewed-by: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365543}

--- chrome/browser/ui/views/tabs/tab_icon.cc
+++ chrome/browser/ui/views/tabs/tab_icon.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/gfx/image/image_skia_operations.h"
+#include "ui/gfx/paint_throbber.h"
 #include "ui/gfx/scoped_canvas.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/resources/grit/ui_resources.h"
@@ -141,7 +142,6 @@ void TabIcon::SetData(const TabRendererData& data) {
   if (was_showing_load && !showing_load) {
     // Loading animation transitioning from on to off.
     loading_animation_start_time_ = base::TimeTicks();
-    waiting_state_ = gfx::ThrobberWaitingState();
     SchedulePaint();
   } else if (!was_showing_load && showing_load) {
     // Loading animation transitioning from off to on. The animation painting
@@ -195,11 +195,6 @@ void TabIcon::SetCanPaintToLayer(bool can_paint_to_layer) {
 }
 
 void TabIcon::StepLoadingAnimation(const base::TimeDelta& elapsed_time) {
-  // Only update elapsed time in the kWaiting state. This is later used as a
-  // starting point for PaintThrobberSpinningAfterWaiting().
-  if (network_state_ == TabNetworkState::kWaiting) {
-    waiting_state_.elapsed_time = elapsed_time;
-  }
   if (GetShowingLoadingAnimation()) {
     SchedulePaint();
   }
@@ -338,25 +333,15 @@ void TabIcon::PaintLoadingAnimation(gfx::Canvas* canvas, gfx::Rect bounds) {
   TRACE_EVENT0("views", "TabIcon::PaintLoadingAnimation");
 
   const SkColor spinning_color = views::GetCascadingAccentColor(this);
-  const SkColor waiting_color = color_utils::AlphaBlend(
-      spinning_color, views::GetCascadingBackgroundColor(this),
-      gfx::kGoogleGreyAlpha400);
-  if (network_state_ == TabNetworkState::kWaiting) {
-    gfx::PaintThrobberWaiting(canvas, bounds, waiting_color,
-                              waiting_state_.elapsed_time,
-                              kLoadingAnimationStrokeWidthDp);
-  } else {
-    const base::TimeTicks current_time = clock_->NowTicks();
-    if (loading_animation_start_time_.is_null()) {
-      loading_animation_start_time_ = current_time;
-    }
-
-    waiting_state_.color = waiting_color;
-    gfx::PaintThrobberSpinningAfterWaiting(
-        canvas, bounds, spinning_color,
-        current_time - loading_animation_start_time_, &waiting_state_,
-        kLoadingAnimationStrokeWidthDp);
+  const base::TimeTicks current_time = clock_->NowTicks();
+  if (loading_animation_start_time_.is_null()) {
+    loading_animation_start_time_ = current_time;
   }
+
+  gfx::PaintThrobberSpinningWithSweepEasedIn(
+      canvas, bounds, spinning_color,
+      current_time - loading_animation_start_time_,
+      kLoadingAnimationStrokeWidthDp);
 }
 
 gfx::ImageSkia TabIcon::GetIconToPaint() {
