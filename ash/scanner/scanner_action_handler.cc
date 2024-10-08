6ca6cf078d2ad9d096686313a75af299202a98f8
mlcui <mlcui@google.com>
2024-10-08 06:38:59 +0000

    Add barebones new contact action
    Bug: b:367872174
    Change-Id: I6a6a21006cc309f2f52a508fc2d2732f6e011bcf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904740
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365352}

--- ash/scanner/scanner_action_handler.cc
+++ ash/scanner/scanner_action_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 #include <string_view>
+#include <utility>
 #include <variant>
 
 #include "ash/public/cpp/new_window_delegate.h"
@@ -29,6 +30,11 @@ const GURL& GetCalendarEventTemplateUrl() {
   return kGoogleCalendarEventTemplateUrl;
 }
 
+const GURL& GetGoogleContactsNewUrl() {
+  static GURL kGoogleContactsNewUrl("https://contacts.google.com/new");
+  return kGoogleContactsNewUrl;
+}
+
 GURL GetCalendarEventUrl(const NewCalendarEventAction& event) {
   std::string query = GetCalendarEventTemplateUrl().query();
   CHECK(!query.empty());
@@ -42,6 +48,19 @@ GURL GetCalendarEventUrl(const NewCalendarEventAction& event) {
   return GetCalendarEventTemplateUrl().ReplaceComponents(replacements);
 }
 
+GURL GetContactUrl(const NewContactAction& contact) {
+  std::string query = GetGoogleContactsNewUrl().query();
+  CHECK(query.empty());
+  if (!contact.given_name.empty()) {
+    query += "given_name=";
+    query += base::EscapeQueryParamValue(contact.given_name, /*use_plus=*/true);
+  }
+
+  GURL::Replacements replacements;
+  replacements.SetQueryStr(query);
+  return GetGoogleContactsNewUrl().ReplaceComponents(replacements);
+}
+
 void OpenInBrowserTab(const GURL& gurl) {
   NewWindowDelegate::GetPrimary()->OpenUrl(
       gurl, NewWindowDelegate::OpenUrlFrom::kUnspecified,
@@ -52,14 +71,17 @@ void OpenInBrowserTab(const GURL& gurl) {
 
 void HandleScannerAction(const ScannerAction& action,
                          base::OnceCallback<void(bool)> callback) {
-  std::visit(
-      base::Overloaded{
-          [&](const NewCalendarEventAction& action) {
-            OpenInBrowserTab(GetCalendarEventUrl(action));
-            std::move(callback).Run(true);
-          },
-      },
-      action);
+  std::visit(base::Overloaded{
+                 [&](const NewCalendarEventAction& action) {
+                   OpenInBrowserTab(GetCalendarEventUrl(action));
+                   std::move(callback).Run(true);
+                 },
+                 [&](const NewContactAction& action) {
+                   OpenInBrowserTab(GetContactUrl(action));
+                   std::move(callback).Run(true);
+                 },
+             },
+             action);
 }
 
 }  // namespace ash
