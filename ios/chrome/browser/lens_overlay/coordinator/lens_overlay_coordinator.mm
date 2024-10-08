785fd8b791da77aaf0f07f138d0b7c06da3ea600
Radu Nitescu <radunitescu@google.com>
2024-10-08 17:09:53 +0000

    [iOS][LO] Remove extra query when dismissing Lens
    Removing the selection now happens internally
    
    Fixed: 371044798
    Change-Id: I9ecac081eebb4dbaeafbc39ba3612b56f08bcc9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913389
    Auto-Submit: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Ameur Hosni <ameurhosni@google.com>
    Commit-Queue: Radu Nitescu <radunitescu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365637}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -846,7 +846,6 @@ typedef NS_ENUM(NSUInteger, SheetDetentState) {
 }
 
 - (void)stopResultPage {
-  [_selectionViewController removeSelectionWithClearText:YES];
   [_resultContextMenuProvider stop];
   _resultContextMenuProvider = nil;
   [_resultViewController.presentingViewController
