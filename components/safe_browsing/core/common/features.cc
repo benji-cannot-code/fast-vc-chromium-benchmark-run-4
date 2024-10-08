3615ccf9710e463bf2dbff56d1586dd155c1356a
Seung Jae Lim <andysjlim@chromium.org>
2024-10-08 16:50:06 +0000

    Accept high confidence allowlist at 95% for page load
    Bug: 344767732
    Change-Id: I24257009d08d3dddc6b8d06c0ca9052c9be138e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905627
    Commit-Queue: Seung Jae Lim <andysjlim@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365631}

--- components/safe_browsing/core/common/features.cc
+++ components/safe_browsing/core/common/features.cc
@@ -32,6 +32,10 @@ BASE_FEATURE(kAddWarningShownTSToClientSafeBrowsingReport,
              "AddWarningShownTSToClientSafeBrowsingReport",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+BASE_FEATURE(kClientSideDetectionAcceptHCAllowlist,
+             "ClientSideDetectionAcceptHCAllowlist",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kClientSideDetectionKillswitch,
              "ClientSideDetectionKillswitch",
              base::FEATURE_DISABLED_BY_DEFAULT);
