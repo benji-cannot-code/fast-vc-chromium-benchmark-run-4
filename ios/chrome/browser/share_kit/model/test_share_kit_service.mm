e1d21f718ab884b6d4712ba590d344816732b530
Gauthier Ambard <gambard@chromium.org>
2024-10-08 15:16:36 +0000

    [iOS] Use provider for ShareKitFactory
    Now that we have an implementation of the provider everywhere, use it
    in the factory to create the service.
    
    Bug: 358373145
    Change-Id: Ic6a41bc8f3b3163a7b503aae01823e9aedfa6c55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913879
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365561}

---
+++ ios/chrome/browser/share_kit/model/test_share_kit_service.mm
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/share_kit/model/test_share_kit_service.h"
+
+TestShareKitService::TestShareKitService() {}
+TestShareKitService::~TestShareKitService() {}
+
+bool TestShareKitService::IsSupported() const {
+  return true;
+}
+
+void TestShareKitService::ShareGroup(const TabGroup* group,
+                                     UIViewController* base_view_controller) {
+  // TODO(crbug.com/358373145): add fake implementation.
+}
