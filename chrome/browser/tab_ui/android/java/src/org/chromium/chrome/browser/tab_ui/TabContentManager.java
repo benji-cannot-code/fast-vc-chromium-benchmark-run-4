f99c7752e7d32c53e8382967dace6302b9591e6b
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2024-10-08 02:44:54 +0000

    Remove unused save_jpeg path in GetEtc1TabThumbnail
    The fallback path that converts an on-disk ETC1 image to JPEG was
    removed in ee6f468624ba5 ("[Thumbnail Cache] Cleanup refactor flag").
    As such, save_jpeg is always false and can be removed.
    
    BUG=b:370872849
    TEST=CQ
    
    Change-Id: Ia036ceee276d117ea0ef4b85cbaf3a5ec64f9fe5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905011
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Commit-Queue: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365293}

--- chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabContentManager.java
+++ chrome/browser/tab_ui/android/java/src/org/chromium/chrome/browser/tab_ui/TabContentManager.java
@@ -310,9 +310,7 @@ public class TabContentManager {
 
         // Do not capture a JPEG here because we likely already created one when capturing. We just
         // want to fetch the ETC1 off of disk for a higher resolution image to use for animations.
-        TabContentManagerJni.get()
-                .getEtc1TabThumbnail(
-                        mNativeTabContentManager, tabId, /* saveJpeg= */ false, callback);
+        TabContentManagerJni.get().getEtc1TabThumbnail(mNativeTabContentManager, tabId, callback);
     }
 
     /**
@@ -707,10 +705,7 @@ public class TabContentManager {
                 long nativeTabContentManager, int tabId, Callback<Boolean> callback);
 
         void getEtc1TabThumbnail(
-                long nativeTabContentManager,
-                int tabId,
-                boolean saveJpeg,
-                Callback<Bitmap> callback);
+                long nativeTabContentManager, int tabId, Callback<Bitmap> callback);
 
         void setCaptureMinRequestTimeForTesting(long nativeTabContentManager, int timeMs);
 
