4c13f2eeded447518ca7381b4396bc53bea4dd81
Florian Leimgruber <fleimgruber@google.com>
2024-10-08 13:03:16 +0000

    Remove AutofillSilentlyRemoveQuasiDuplicates
    As decided by feature sync, this feature has been enabled by default
    for one milestone, but is now removed completely [1].
    
    [1] https://docs.google.com/document/d/1oqCRE3_dlBiL8gkhMT86d9iEimYFcJ_-HvNFkhPAtqc/edit?tab=t.0#bookmark=id.j03dc8a70s7n
    
    Fixed: 325450676
    Change-Id: Ie3170deb08f868979d7fc37dbc9188926161c69e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912698
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365502}

--- components/autofill/core/common/autofill_features.h
+++ components/autofill/core/common/autofill_features.h
@@ -223,8 +223,6 @@ BASE_DECLARE_FEATURE(kAutofillLogDeduplicationMetrics);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillLogDeduplicationMetricsFollowup);
 COMPONENT_EXPORT(AUTOFILL)
-BASE_DECLARE_FEATURE(kAutofillSilentlyRemoveQuasiDuplicates);
-COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillUpdateLowQualityTokenOnImport);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillUKMExperimentalFields);
