a733f06d37a00cb50bf35db6a61ee6e843c72792
Takuto Ikuta <tikuta@chromium.org>
2024-10-08 03:00:14 +0000

    chromeos: include necessary header for std::optinal
    This is to fix build error like
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-chromeos-dbg/37649/overview
    when we enable _LIBCPP_REMOVE_TRANSITIVE_INCLUDES.
    
    Bug: 371924177
    Fixed: 372115717
    Change-Id: I49c290f4233795e01d9d898104ddad4b9c934672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912637
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Commit-Queue: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365300}

--- chromeos/ash/components/boca/babelorca/tachyon_response.h
+++ chromeos/ash/components/boca/babelorca/tachyon_response.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_ASH_COMPONENTS_BOCA_BABELORCA_TACHYON_RESPONSE_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 
 namespace ash::babelorca {
