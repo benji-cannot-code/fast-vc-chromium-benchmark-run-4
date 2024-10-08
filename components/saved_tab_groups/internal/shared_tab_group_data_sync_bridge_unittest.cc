0d21d02d61ae507d65f654434e082a91524f1eaa
Rushan Suleymanov <rushans@google.com>
2024-10-08 08:36:48 +0000

    [Tab Group Sync] Include collaboration ID to client tag
    This CL changes the client tag for shared tab group data including
    collaboration ID. This is required to make it sure that different
    collaborations will have isolated GUIDs.
    
    Note that changing client tags will prevent the existing entities from
    syncing, only new entities will be syncing correctly.
    
    Change-Id: I5a70759fe05690ed51d74149896bb7667100879b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907431
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365391}

--- components/saved_tab_groups/internal/shared_tab_group_data_sync_bridge_unittest.cc
+++ components/saved_tab_groups/internal/shared_tab_group_data_sync_bridge_unittest.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/saved_tab_groups/public/saved_tab_group.h"
 #include "components/saved_tab_groups/public/saved_tab_group_tab.h"
 #include "components/saved_tab_groups/public/types.h"
+#include "components/saved_tab_groups/public/utils.h"
 #include "components/saved_tab_groups/test_support/saved_tab_group_test_utils.h"
 #include "components/sync/base/client_tag_hash.h"
 #include "components/sync/base/data_type.h"
@@ -441,12 +442,11 @@ TEST_F(SharedTabGroupDataSyncBridgeTest, ShouldReturnClientTag) {
   ASSERT_TRUE(InitializeBridgeAndModel());
 
   EXPECT_TRUE(bridge()->SupportsGetClientTag());
-  EXPECT_FALSE(bridge()
-                   ->GetClientTag(CreateEntityData(
-                       MakeTabGroupSpecifics("test title",
-                                             sync_pb::SharedTabGroup::GREEN),
-                       "collaboration"))
-                   .empty());
+  sync_pb::SharedTabGroupDataSpecifics group_specifics =
+      MakeTabGroupSpecifics("title", sync_pb::SharedTabGroup::BLUE);
+  EXPECT_EQ(bridge()->GetClientTag(
+                CreateEntityData(group_specifics, "collaboration")),
+            group_specifics.guid() + "|collaboration");
 }
 
 TEST_F(SharedTabGroupDataSyncBridgeTest, ShouldCallModelReadyToSync) {
@@ -1540,6 +1540,11 @@ TEST_F(SharedTabGroupDataSyncBridgeTest, ShouldAssignLocalGroupId) {
 
   LocalTabGroupID local_group_id = test::GenerateRandomTabGroupID();
   model()->OnGroupOpenedInTabStrip(group.saved_guid(), local_group_id);
+
+  // Verify that the local group ID is persisted if supported.
+  if (!AreLocalIdsPersisted()) {
+    return;
+  }
 
   // Simulate browser restart to verify that the local group ID is persisted.
   StoreMetadataAndReset(kCollaborationId);
