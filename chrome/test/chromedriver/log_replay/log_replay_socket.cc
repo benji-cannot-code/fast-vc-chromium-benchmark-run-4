8498fed973c0afdaeda7ca20ca5b01c65fa073e3
Tom Sepez <tsepez@chromium.org>
2024-10-08 09:50:55 +0000

    Replace const-value arguments where const-ref intended.
    This is a re-split of a previousl split CL.
    
    This is of concern when passing vectors and strings, as unintended
    (and often expensive) copies occur under the covers.
    
    Apply some of the following (depending on split):
    -- Make non-const and move in callee where appropriate.
    -- Convert to string_view where appropriate.
    -- Avoid stray const in a few places
    
    This CL was uploaded by git cl split.
    
    R=nvitkov@chromium.org
    
    Bug: 369952985
    Change-Id: I8cf5c32fe6b5de3436c18da28f791b86c4d7c435
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914620
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365424}

--- chrome/test/chromedriver/log_replay/log_replay_socket.cc
+++ chrome/test/chromedriver/log_replay/log_replay_socket.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-std::string SessionIdJson(const std::string session_id) {
+std::string SessionIdJson(const std::string& session_id) {
   return session_id.empty() ? std::string()
                             : ",\"session_id\":\"" + session_id + "\"";
 }
