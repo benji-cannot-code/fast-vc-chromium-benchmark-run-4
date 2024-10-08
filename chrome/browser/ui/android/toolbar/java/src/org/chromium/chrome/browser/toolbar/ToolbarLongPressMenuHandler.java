7d4e727544bd9d3e04b5918dc85e6fb3d04c4c54
Patrick Noland <pnoland@google.com>
2024-10-08 17:27:03 +0000

    Reland: [Anchor] Use new eligibility check for settings and long press
    Since instrumentation tests run on various devices, the expected
    behavior should depend on the device, not just the flag state.
    
    Bug: 369134349, 372059752
    Change-Id: I9c984d82cb5eb63c97a439380b16a2ad6342f9a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913920
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365656}

--- chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarLongPressMenuHandler.java
+++ chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/ToolbarLongPressMenuHandler.java
@@ -10,9 +10,6 @@ import android.view.View.OnLongClickListener;
 
 import androidx.annotation.Nullable;
 
-import org.chromium.chrome.browser.flags.ChromeFeatureList;
-import org.chromium.ui.base.DeviceFormFactor;
-
 /** The handler for the toolbar long press menu. */
 public class ToolbarLongPressMenuHandler {
     @Nullable private OnLongClickListener mOnLongClickListener;
@@ -22,8 +19,8 @@ public class ToolbarLongPressMenuHandler {
      *
      * @param context current context
      */
-    public ToolbarLongPressMenuHandler(Context context) {
-        if (shouldEnableLongPress(context)) {
+    public ToolbarLongPressMenuHandler(Context context, boolean isCustomTab) {
+        if (ToolbarPositionController.isToolbarPositionCustomizationEnabled(context, isCustomTab)) {
             mOnLongClickListener =
                     (view) -> {
                         displayMenu();
@@ -43,9 +40,4 @@ public class ToolbarLongPressMenuHandler {
     }
 
     private void displayMenu() {}
-
-    private boolean shouldEnableLongPress(Context context) {
-        return ChromeFeatureList.sAndroidBottomToolbar.isEnabled()
-                && !DeviceFormFactor.isNonMultiDisplayContextOnTablet(context);
-    }
 }
