e1288bb662b46760b6d2c890d3c9286759e287a4
Mikel Astiz <mastiz@chromium.org>
2024-10-08 16:23:19 +0000

    Store wallet cards on disk for users explicitly signed in users
    Changes behind feature flag disabled by default, namely the
    pre-existing `ImprovedSigninUIOnDesktop`.
    
    The general idea is that, depending on feature flags and runtime
    state, the sync-ing of autofill wallet data should use on-disk
    storage on desktop platforms (it is already the case on mobile).
    
    In this patch, a simple approach is proposed that leverages the
    fact that the implicit signed-in state is an existing state for
    users even after `ImprovedSigninUIOnDesktop` rolls out, if they
    already were in such state when the flag rolled out, but it is
    not NEWLY reachable. More details about why this simplifies things
    and how to leverage it in comments added in this patch to predicate
    `ShouldUseInMemoryAutofillAccountDatabase()`.
    
    Change-Id: I5ddf1b6cb7043567994ccc85a2df0cdacc14d266
    Bug: 371184754
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5860535
    Reviewed-by: David Roger <droger@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365610}

--- components/webdata/common/webdata_constants.cc
+++ components/webdata/common/webdata_constants.cc
@@ -9,8 +9,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const base::FilePath::CharType kWebDataFilename[] =
     FILE_PATH_LITERAL("Web Data");
-
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
 const base::FilePath::CharType kAccountWebDataFilename[] =
     FILE_PATH_LITERAL("Account Web Data");
-#endif  // BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_IOS)
