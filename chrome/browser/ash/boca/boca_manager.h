64089e35de72272953dada1519f1937f5896252a
Dorian Brandon <dorianbrandon@google.com>
2024-10-08 18:18:27 +0000

    Init BabelOrcaManager and add as observer to BocaManager.
    Change-Id: I95eacbf45beea312f427687daf8a9e88d6e55f41
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914492
    Commit-Queue: Dorian Brandon <dorianbrandon@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365703}

--- chrome/browser/ash/boca/boca_manager.h
+++ chrome/browser/ash/boca/boca_manager.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_BOCA_BOCA_MANAGER_H_
 
 #include "chrome/browser/ash/boca/boca_app_client_impl.h"
+#include "chromeos/ash/components/boca/babelorca/babel_orca_manager.h"
 #include "chromeos/ash/components/boca/boca_session_manager.h"
 #include "chromeos/ash/components/boca/invalidations/invalidation_service_impl.h"
 #include "chromeos/ash/components/boca/on_task/on_task_session_manager.h"
@@ -24,7 +25,8 @@ class BocaManager : public KeyedService {
       std::unique_ptr<boca::OnTaskSessionManager> on_task_session_manager,
       std::unique_ptr<boca::SessionClientImpl> session_client_impl,
       std::unique_ptr<boca::BocaSessionManager> boca_session_manager,
-      std::unique_ptr<boca::InvalidationServiceImpl> invalidation_service_impl);
+      std::unique_ptr<boca::InvalidationServiceImpl> invalidation_service_impl,
+      std::unique_ptr<boca::BabelOrcaManager> babel_orca_manager);
 
   explicit BocaManager(Profile* profile);
   ~BocaManager() override;
@@ -38,6 +40,9 @@ class BocaManager : public KeyedService {
   boca::BocaSessionManager* GetBocaSessionManagerForTesting() {
     return boca_session_manager_.get();
   }
+  boca::BabelOrcaManager* GetBabelOrcaManagerForTesting() {
+    return babel_orca_manager_.get();
+  }
 
   void AddObservers();
 
@@ -46,6 +51,7 @@ class BocaManager : public KeyedService {
   std::unique_ptr<boca::SessionClientImpl> session_client_impl_;
   std::unique_ptr<boca::BocaSessionManager> boca_session_manager_;
   std::unique_ptr<boca::InvalidationServiceImpl> invalidation_service_impl_;
+  std::unique_ptr<boca::BabelOrcaManager> babel_orca_manager_;
 };
 }  // namespace ash
 
