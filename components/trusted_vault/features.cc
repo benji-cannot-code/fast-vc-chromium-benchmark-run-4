a70e949d4acff4f107ae2b88ed9c9a9444c05ec5
Maksim Moskvitin <mmoskvitin@google.com>
2024-10-08 17:44:39 +0000

    [Lacros] Cleanup c/b/ash/trusted_vault
    This was needed only to support lacros and can be removed now.
    
    Bug: 372221465
    Change-Id: I3bce86519926f64abb571107eb7f0700f317417e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914475
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365670}

--- components/trusted_vault/features.cc
+++ components/trusted_vault/features.cc
@@ -24,10 +24,6 @@ BASE_FEATURE(kSetClientEncryptionKeysJsApi,
 BASE_FEATURE(kChromeOSTrustedVaultUseWebUIDialog,
              "ChromeOSTrustedVaultUseWebUIDialog",
              base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kChromeOSTrustedVaultClientShared,
-             "ChromeOSTrustedVaultClientShared",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 }  // namespace trusted_vault
