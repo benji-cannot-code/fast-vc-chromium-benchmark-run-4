c0461d01f14baf93efdb94481e478710f298d34c
Wojciech Dzierżanowski <wdzierzanowski@opera.com>
2024-10-08 08:38:15 +0000

    Improve RAII in VTVideoEncodeAccelerator
    VTCompressionSessionInvalidate() is mandatory on all (valid) compression
    session objects. Wrapping it in a traits type for ScopedTypeRef seems
    like a better way to express that. This is also more fool-proof thanks
    to being in the spirit of RAII.
    
    No functionality changes are expected.
    
    Bug: 371224786
    Change-Id: I5115c72cced293a80e6b32027da6495fa13c6957
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904311
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Wojciech Dzierżanowski <wdzierzanowski@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1365392}

--- media/base/mac/videotoolbox_helpers.h
+++ media/base/mac/videotoolbox_helpers.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <VideoToolbox/VideoToolbox.h>
 
 #include "base/apple/scoped_cftyperef.h"
+#include "base/notreached.h"
 #include "media/base/media_export.h"
 #include "media/base/video_codecs.h"
 
@@ -27,6 +28,24 @@ MEDIA_EXPORT bool CopySampleBufferToAnnexBBuffer(VideoCodec codec,
                                                  size_t annexb_buffer_size,
                                                  char* annexb_buffer,
                                                  size_t* used_buffer_size);
+
+struct ScopedVTCompressionSessionRefTraits {
+  static VTCompressionSessionRef InvalidValue() { return nullptr; }
+  static VTCompressionSessionRef Retain(VTCompressionSessionRef session) {
+    NOTREACHED() << "Only compatible with ASSUME policy";
+  }
+  static void Release(VTCompressionSessionRef session) {
+    // Blocks until all pending frames have been flushed out.
+    VTCompressionSessionInvalidate(session);
+    CFRelease(session);
+  }
+};
+
+// A scoper for VTCompressionSessionRef that makes sure
+// VTCompressionSessionInvalidate() is called before releasing.
+using ScopedVTCompressionSessionRef =
+    base::apple::ScopedTypeRef<VTCompressionSessionRef,
+                               ScopedVTCompressionSessionRefTraits>;
 
 // Helper class to add session properties to a VTCompressionSessionRef.
 class MEDIA_EXPORT SessionPropertySetter {
