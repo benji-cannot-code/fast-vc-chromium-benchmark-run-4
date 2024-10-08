81218e0a042c78254126dfe5b5ce6c74eb4c2979
April Zhou <aprilzhou@google.com>
2024-10-08 17:10:28 +0000

    boca: Provide the event for when app reloaded.
    The event will be triggered when boca app reload. Downstream can handle
    accordingly.
    
    Test: Unit tested.
    Bug: b:372081608
    Change-Id: I0066bdaaad4618fbac155214091a3e1fc4765565
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914713
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365638}

--- chromeos/ash/components/boca/boca_session_manager.cc
+++ chromeos/ash/components/boca/boca_session_manager.cc
@@ -56,6 +56,8 @@ void BocaSessionManager::Observer::OnSessionRosterUpdated(
     const std::string& group_name,
     const std::vector<::boca::UserIdentity>& consumers) {}
 
+void BocaSessionManager::Observer::OnAppReloaded() {}
+
 void BocaSessionManager::OnNetworkStateChanged(
     chromeos::network_config::mojom::NetworkStatePropertiesPtr network_state) {
   // Check network types comment here:
@@ -144,6 +146,12 @@ void BocaSessionManager::NotifyLocalCaptionEvents(
     ::boca::CaptionsConfig caption_config) {
   for (auto& observer : observers_) {
     observer.OnLocalCaptionConfigUpdated(std::move(caption_config));
+  }
+}
+
+void BocaSessionManager::NotifyAppReload() {
+  for (auto& observer : observers_) {
+    observer.OnAppReloaded();
   }
 }
 
--- chromeos/ash/components/boca/boca_session_manager_unittest.cc
+++ chromeos/ash/components/boca/boca_session_manager_unittest.cc
@@ -75,6 +75,7 @@ class MockObserver : public BocaSessionManager::Observer {
               (const std::string& group_name,
                const std::vector<::boca::UserIdentity>& consumers),
               (override));
+  MOCK_METHOD(void, OnAppReloaded, (), (override));
 };
 
 class MockBocaAppClient : public BocaAppClient {
@@ -649,6 +650,14 @@ TEST_F(BocaSessionManagerTest, NotifyLocalCaptionConfigWhenLocalChange) {
 
   ::boca::CaptionsConfig config;
   BocaAppClient::Get()->GetSessionManager()->NotifyLocalCaptionEvents(config);
+}
+
+TEST_F(BocaSessionManagerTest, NotifyAppReloadEvent) {
+  EXPECT_CALL(*boca_app_client(), GetIdentityManager())
+      .WillOnce(Return(identity_manager()));
+  EXPECT_CALL(*observer(), OnAppReloaded()).Times(1);
+
+  BocaAppClient::Get()->GetSessionManager()->NotifyAppReload();
 }
 
 }  // namespace
