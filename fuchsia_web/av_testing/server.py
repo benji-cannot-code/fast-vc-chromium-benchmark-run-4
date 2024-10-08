26294c4adaea1f5d37863b5e77eb952da96d6bde
Hzj_jie <zijiehe@google.com>
2024-10-08 19:23:13 +0000

    [Fuchsia] Start camera during video playback
    Captured video can be found at
    https://cas-viewer.appspot.com/projects/chrome-swarming/instances/default_instance/blobs/b9ad805b5cbbaf6d5dbcc1b416cb6c66a3b21158ef151bc81393e534d36fc63b/595/tree
    
    To avoid bloating the cas output, this change reduces the length to 10
    seconds or ~2M video file before integrating with the analysis
    service.
    
    Bug: 40935291
    Change-Id: I952e2a4c9bccf3f07b1f496694152576517ff3a7
    Cq-Include-Trybots: luci.chrome.try:fuchsia-ava-nelson
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908591
    Reviewed-by: Brian Sheedy <bsheedy@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365738}

--- fuchsia_web/av_testing/server.py
+++ fuchsia_web/av_testing/server.py
@@ -1,5 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/usr/bin/env python3
 # Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -20,18 +19,15 @@ class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
 Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map = {
-    '.mp4': 'video/mp4',
     '.html': 'text/html',
-    '.css': 'text/css',
-    '.js': 'application/javascript',
-    '.jpg': 'image/jpeg',
     '': 'application/octet-stream',
 }
 
-PORT = int(sys.argv[-1])
 DIR = os.path.dirname(__file__)
 
-with ThreadedHTTPServer(("", PORT), Handler) as httpd:
-    os.chdir(DIR)
-    logging.warning('Http server is running on port %s at %s', PORT, DIR)
-    httpd.serve_forever()
+
+def start(port: int) -> None:
+    with ThreadedHTTPServer(("", port), Handler) as httpd:
+        os.chdir(DIR)
+        logging.warning('Http server is running on port %s at %s', port, DIR)
+        httpd.serve_forever()
