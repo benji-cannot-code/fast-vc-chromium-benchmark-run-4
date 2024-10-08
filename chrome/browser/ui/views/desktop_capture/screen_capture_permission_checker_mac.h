acddf09f8d358a76ab295cf83cd0e5aecb843b1f
Johannes Kron <kron@chromium.org>
2024-10-08 23:10:06 +0000

    Enable DesktopCapturePermissionCheckerMacos14_4Plus by default
    The features DesktopCapturePermissionChecker,
    ScreenCaptureKitStreamPickerSonoma, and ScreenCaptureKitPickerScreen
    have been rolled out in a joint experiment to macOS 14.4+.
    
    All features are related to the getDisplayMedia picker.
    - DesktopCapturePermissionChecker displays a notification and a
      button if the user has not given consent for Chrome to capture
      the screen.
    - ScreenCaptureKitStreamPickerSonoma and ScreenCaptureKitPickerScreen
      changes the underlying API that is used to produce thumbnails in
      the picker to be ScreenCaptureKit.
    
    This CL prepares for a full launch of the joint features to macOS
    14.4+ by changing the state of the features to enabled by default
    and changing the scope for DesktopCapturePermissionChecker to only
    affect macOS 14.4+. The name DesktopCapturePermissionChecker is
    suffixed with Macos14_4Plus to reflect this change.
    
    In addition, the CL also prepares a roll out of the feature
    DesktopCapturePermissionChecker to macOS <14.4 by adding
    a new feature DesktopCapturePermissionCheckerPreMacos14_4 that only
    affects macOS <14.4.
    
    Bug: 332916111
    Change-Id: Ibc619313364e86eb2a2b4e50120f5b340d6c398d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913876
    Reviewed-by: Tove Petersson <tovep@chromium.org>
    Commit-Queue: Johannes Kron <kron@chromium.org>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365881}

--- chrome/browser/ui/views/desktop_capture/screen_capture_permission_checker_mac.h
+++ chrome/browser/ui/views/desktop_capture/screen_capture_permission_checker_mac.h
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/timer/timer.h"
 #include "chrome/browser/ui/views/desktop_capture/screen_capture_permission_checker.h"
 
-BASE_DECLARE_FEATURE(kDesktopCapturePermissionChecker);
+// Declared in header file so that it can be used in unit tests.
+BASE_DECLARE_FEATURE(kDesktopCapturePermissionCheckerPreMacos14_4);
 
 class ScreenCapturePermissionCheckerMac
     : public ScreenCapturePermissionChecker {
