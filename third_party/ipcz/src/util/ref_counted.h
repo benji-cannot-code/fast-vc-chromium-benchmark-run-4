f8b83a71670704afc4d88f513e906dff6a3077b4
Takuto Ikuta <tikuta@chromium.org>
2024-10-08 05:41:23 +0000

    ipcz: remove unnecessary include
    This is to reduce included size e.g.
    https://commondatastorage.googleapis.com/chromium-browser-clang/chrome_includes_2024-10-07_014308.html#view=edges&filter=%5Ethird_party%2Fipcz%2Fsrc%2Futil%2Fref_counted%5C.h%24&sort=asize&reverse=&includer=%5Ethird_party%2Fipcz%2Fsrc%2Futil%2Fref_counted%5C.h%24&included=&limit=1000
    
    Change-Id: I7e1288150ab412b97b93974ea864edd300cf5ba5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908961
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Takuto Ikuta <tikuta@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365341}

--- third_party/ipcz/src/util/ref_counted.h
+++ third_party/ipcz/src/util/ref_counted.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IPCZ_SRC_UTIL_REF_COUNTED_H_
 #define IPCZ_SRC_UTIL_REF_COUNTED_H_
 
-#include <algorithm>
 #include <atomic>
 #include <cstddef>
 #include <type_traits>
