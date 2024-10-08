5937c00e2e32934eeb22221e36472664b4dd6a18
Joe Mason <joenotcharles@google.com>
2024-10-08 22:58:09 +0000

    Add PM sequence setters for performance scenarios.
    * Adds overloads of SetLoadingScenarioForProcess, etc that take a
      ProcessNode, to simplify calls from the PM sequence.
    * Changes the RenderProcessHost functions to take pointers, to match the
      common practice. This puts the null check in one place, and simplifies
      callers using RenderProcessHostProxy::Get() which can return null.
    * Swaps the parameter order to put the scenario first, which is more
      convenient to Bind().
    
    R=olivierli
    
    Bug: 365586676
    Change-Id: Iff611927224b5014350bbff91882a421bacd9612
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5897980
    Reviewed-by: Olivier Li <olivierli@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365872}

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -8841,7 +8841,7 @@ void ChromeContentBrowserClient::SetSamplingProfiler(
 base::ReadOnlySharedMemoryRegion
 ChromeContentBrowserClient::GetPerformanceScenarioRegionForProcess(
     content::RenderProcessHost* process_host) {
-  return performance_manager::GetSharedScenarioRegionForProcess(*process_host);
+  return performance_manager::GetSharedScenarioRegionForProcess(process_host);
 }
 
 base::ReadOnlySharedMemoryRegion
--- components/performance_manager/scenarios/performance_scenarios_unittest.cc
+++ components/performance_manager/scenarios/performance_scenarios_unittest.cc
@@ -6,15 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/public/scenarios/performance_scenarios.h"
 
 #include <atomic>
-#include <string>
+#include <utility>
 
-#include "base/location.h"
+#include "base/functional/callback.h"
 #include "base/memory/read_only_shared_memory_region.h"
-#include "content/public/browser/browser_context.h"
+#include "base/memory/weak_ptr.h"
+#include "base/task/bind_post_task.h"
+#include "base/test/bind.h"
+#include "base/test/task_environment.h"
+#include "components/performance_manager/public/graph/process_node.h"
+#include "components/performance_manager/public/performance_manager.h"
+#include "components/performance_manager/test_support/performance_manager_test_harness.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/test/mock_render_process_host.h"
-#include "content/public/test/test_renderer_host.h"
+#include "content/public/test/navigation_simulator.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/performance/performance_scenarios.h"
+#include "url/gurl.h"
 
 namespace performance_manager {
 
@@ -22,7 +30,8 @@ namespace {
 
 using blink::performance_scenarios::GetLoadingScenario;
 using blink::performance_scenarios::Scope;
-using PerformanceScenariosTest = content::RenderViewHostTestHarness;
+
+using PerformanceScenariosTest = PerformanceManagerTestHarness;
 
 TEST_F(PerformanceScenariosTest, SetWithoutSharedMemory) {
   // When the shared scenario memory isn't set up, setting a scenario should
@@ -31,8 +40,7 @@ TEST_F(PerformanceScenariosTest, SetWithoutSharedMemory) {
   EXPECT_EQ(GetLoadingScenario(Scope::kGlobal)->load(std::memory_order_relaxed),
             LoadingScenario::kNoPageLoading);
 
-  SetLoadingScenarioForProcess(*process(),
-                               LoadingScenario::kVisiblePageLoading);
+  SetLoadingScenarioForProcess(LoadingScenario::kVisiblePageLoading, process());
   EXPECT_EQ(GetLoadingScenario(Scope::kCurrentProcess)
                 ->load(std::memory_order_relaxed),
             LoadingScenario::kNoPageLoading);
@@ -51,9 +59,8 @@ TEST_F(PerformanceScenariosTest, SetWithSharedMemory) {
   // that process' state. Since it's called in the browser process and this
   // state is for a different process, it doesn't map in a read-only view.
   base::ReadOnlySharedMemoryRegion process_region =
-      GetSharedScenarioRegionForProcess(*process());
-  SetLoadingScenarioForProcess(*process(),
-                               LoadingScenario::kVisiblePageLoading);
+      GetSharedScenarioRegionForProcess(process());
+  SetLoadingScenarioForProcess(LoadingScenario::kVisiblePageLoading, process());
   EXPECT_EQ(GetLoadingScenario(Scope::kCurrentProcess)
                 ->load(std::memory_order_relaxed),
             LoadingScenario::kNoPageLoading);
@@ -66,6 +73,53 @@ TEST_F(PerformanceScenariosTest, SetWithSharedMemory) {
   EXPECT_EQ(GetLoadingScenario(Scope::kCurrentProcess)
                 ->load(std::memory_order_relaxed),
             LoadingScenario::kVisiblePageLoading);
+}
+
+TEST_F(PerformanceScenariosTest, SetFromPMSequence) {
+  // Load a page with FrameNodes and ProcessNodes.
+  SetContents(CreateTestWebContents());
+  content::NavigationSimulator::NavigateAndCommitFromBrowser(
+      web_contents(), GURL("https://www.example.com"));
+
+  // Create writable shared memory for the global state. This maps a read-only
+  // view of the memory in as well.
+  ScopedGlobalScenarioMemory global_shared_memory;
+
+  // Create writable shared memory for the renderer process, and map the
+  // read-only view as the "current process" state.
+  ASSERT_TRUE(process());
+  blink::performance_scenarios::ScopedReadOnlyScenarioMemory
+      process_shared_memory(Scope::kCurrentProcess,
+                            GetSharedScenarioRegionForProcess(process()));
+  EXPECT_EQ(GetLoadingScenario(Scope::kCurrentProcess)
+                ->load(std::memory_order_relaxed),
+            LoadingScenario::kNoPageLoading);
+
+  // Set the loading scenario from the PM sequence.
+  base::WeakPtr<ProcessNode> process_node =
+      PerformanceManager::GetProcessNodeForRenderProcessHost(process());
+  auto set_on_pm_sequence = base::BindLambdaForTesting([&] {
+    ASSERT_TRUE(process_node);
+    SetLoadingScenarioForProcessNode(LoadingScenario::kVisiblePageLoading,
+                                     process_node.get());
+    SetGlobalLoadingScenario(LoadingScenario::kFocusedPageLoading);
+  });
+
+  // SetLoadingScenarioForProcessNode posts to the main thread. Wait until the
+  // message is received to quit the run loop.
+  auto quit_run_loop_on_main_thread =
+      base::BindPostTaskToCurrentDefault(task_environment()->QuitClosure());
+
+  PerformanceManager::CallOnGraph(
+      FROM_HERE, std::move(set_on_pm_sequence)
+                     .Then(std::move(quit_run_loop_on_main_thread)));
+  task_environment()->RunUntilQuit();
+
+  EXPECT_EQ(GetLoadingScenario(Scope::kCurrentProcess)
+                ->load(std::memory_order_relaxed),
+            LoadingScenario::kVisiblePageLoading);
+  EXPECT_EQ(GetLoadingScenario(Scope::kGlobal)->load(std::memory_order_relaxed),
+            LoadingScenario::kFocusedPageLoading);
 }
 
 }  // namespace
