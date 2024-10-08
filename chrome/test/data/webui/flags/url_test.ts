86c3d9ef6bc7c54d82b29ce8b5b11f44c42f82b0
Sophey Dong <sophey@chromium.org>
2024-10-08 23:32:06 +0000

    Revert "FlagsUI: Leverage cr-tabs instead of a custom tab switcher."
    This reverts commit 89e61df0aea392f02948da79ad8d8c28d6b73661.
    
    Reason for revert: causing flag test failures: https://ci.chromium.org/ui/p/chromium/builders/ci/ios-simulator-noncq/50124/overview
    
    Original change's description:
    > FlagsUI: Leverage cr-tabs instead of a custom tab switcher.
    >
    > As a by-product of removing a custom tab switcher implementation, this
    > CL fixes a bug about not proper keyboard behavior for tab switching
    > (arrows vs tab).
    >
    > Also fixing a minor styling issue for the "Reset all" cr-button, on
    > very narrow windows.
    >
    > Bug: 40246381,361569076
    > Change-Id: I2e10b02086434ca788ae2d8474f612d0a998e650
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914752
    > Reviewed-by: John Lee <johntlee@chromium.org>
    > Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1365836}
    
    Bug: 40246381,361569076,372324645
    Change-Id: I0823c51d7da5d65aa5c8bb02c61c417d11c55c5f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917230
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Sophey Dong <sophey@chromium.org>
    Auto-Submit: Sophey Dong <sophey@chromium.org>
    Commit-Queue: Sophey Dong <sophey@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365886}

--- chrome/test/data/webui/flags/url_test.ts
+++ chrome/test/data/webui/flags/url_test.ts
@@ -69,11 +69,11 @@ suite('UrlWithSupportedFeatureTest', function() {
   let browserProxy: TestFlagsBrowserProxy;
 
   setup(async function() {
-    document.body.innerHTML = window.trustedTypes!.emptyHTML;
     browserProxy = new TestFlagsBrowserProxy();
     browserProxy.setFeatureData(Object.assign(
         {}, experimentalFeaturesData, {supportedFeatures: mockFeatures}));
     FlagsBrowserProxyImpl.setInstance(browserProxy);
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
     app = document.createElement('flags-app');
     document.body.appendChild(app);
     app.setAnnounceStatusDelayMsForTesting(0);
@@ -83,8 +83,8 @@ suite('UrlWithSupportedFeatureTest', function() {
 
   test('check referenced experiment is highlighted', async function() {
     // check the available tab is selected
-    const crTabs = app.getRequiredElement('cr-tabs');
-    assertEquals(0, crTabs.selected);
+    assertTrue(app.getRequiredElement('#tab-available')
+                   .classList.contains('selected'));
 
     const referencedExperiment =
         app.getRequiredElement<ExperimentElement>(window.location.hash);
@@ -101,11 +101,11 @@ suite('UrlWithUnsupportedFeatureTest', function() {
   let browserProxy: TestFlagsBrowserProxy;
 
   setup(async function() {
-    document.body.innerHTML = window.trustedTypes!.emptyHTML;
     browserProxy = new TestFlagsBrowserProxy();
     browserProxy.setFeatureData(Object.assign(
         {}, experimentalFeaturesData, {unsupportedFeatures: mockFeatures}));
     FlagsBrowserProxyImpl.setInstance(browserProxy);
+    document.body.innerHTML = window.trustedTypes!.emptyHTML;
     app = document.createElement('flags-app');
     document.body.appendChild(app);
     app.setAnnounceStatusDelayMsForTesting(0);
@@ -115,8 +115,8 @@ suite('UrlWithUnsupportedFeatureTest', function() {
 
   test('check referenced experiment is highlighted', async function() {
     // check the unavailable tab is selected
-    const crTabs = app.getRequiredElement('cr-tabs');
-    assertEquals(1, crTabs.selected);
+    assertTrue(app.getRequiredElement('#tab-unavailable')
+                   .classList.contains('selected'));
 
     const referencedExperiment =
         app.getRequiredElement<ExperimentElement>(window.location.hash);
