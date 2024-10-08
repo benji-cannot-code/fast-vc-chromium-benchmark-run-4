f808c205ffad6f5cafda94a530af07e8f5a57e7c
John Lee <johntlee@chromium.org>
2024-10-08 00:18:45 +0000

    Update scroll target for history-clusters when embeddings enabled
    If history embeddings feature is enabled, set a parent element as the
    scroll target so that <cr-history-embeddings> and <history-clusters>
    can share a scroll container. If disabled, leave <history-clusters>
    as its own scroll target.
    
    Bug: 368053282
    Change-Id: I99db28812eaa6f452ab03f6404b7a36d719b3ba8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914274
    Commit-Queue: John Lee <johntlee@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365255}

--- chrome/browser/resources/side_panel/history_clusters/app.ts
+++ chrome/browser/resources/side_panel/history_clusters/app.ts
@@ -12,6 +12,7 @@ import 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 
 import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_listener/colors_css_updater.js';
 import {BrowserProxyImpl} from 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
+import type {HistoryClustersElement} from 'chrome://resources/cr_components/history_clusters/clusters.js';
 import type {CrToolbarSearchFieldElement} from 'chrome://resources/cr_elements/cr_toolbar/cr_toolbar_search_field.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
@@ -21,8 +22,9 @@ import {getHtml} from './app.html.js';
 
 export interface HistoryClustersAppElement {
   $: {
+    embeddingsScrollContainer: HTMLElement,
     searchbox: CrToolbarSearchFieldElement,
-    historyClusters: HTMLElement,
+    historyClusters: HistoryClustersElement,
   };
 }
 
@@ -41,7 +43,7 @@ export class HistoryClustersAppElement extends CrLitElement {
 
   static override get properties() {
     return {
-      enableHistoryEmbeddings_: {type: Boolean},
+      enableHistoryEmbeddings_: {type: Boolean, reflect: true},
 
       /**
        * The current query for which related clusters are requested and shown.
@@ -84,7 +86,9 @@ export class HistoryClustersAppElement extends CrLitElement {
 
   override connectedCallback() {
     super.connectedCallback();
-    this.scrollTarget_ = this.$.historyClusters;
+    this.scrollTarget_ = this.enableHistoryEmbeddings_ ?
+        this.$.embeddingsScrollContainer :
+        this.$.historyClusters;
 
     if (this.enableHistoryEmbeddings_) {
       this.searchIcon_ = 'history-embeddings:search';
@@ -97,6 +101,12 @@ export class HistoryClustersAppElement extends CrLitElement {
     if (initialQuery) {
       this.$.searchbox.setValue(initialQuery);
     }
+  }
+
+  protected getClustersComponentClass_(): string {
+    return this.enableHistoryEmbeddings_ ?
+        '' :
+        'sp-scroller sp-scroller-bottom-of-page';
   }
 
   /**
