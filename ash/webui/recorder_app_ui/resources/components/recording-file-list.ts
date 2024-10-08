f0f62a34c4d75094d264d236bfe05fa3f86f3056
hsuanling <hsuanling@google.com>
2024-10-08 05:41:01 +0000

    recorder: Provide heading structure of main page
    Set recording list title to h1, and recording section header to h2.
    
    Bug: b:365616809
    Test: Chromevox
    Change-Id: Ie159f2e1df526e740255d31765cc6bd7e191a352
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912452
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Commit-Queue: Jennifer Ling <hsuanling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365340}

--- ash/webui/recorder_app_ui/resources/components/recording-file-list.ts
+++ ash/webui/recorder_app_ui/resources/components/recording-file-list.ts
@@ -113,9 +113,10 @@ export class RecordingFileList extends ReactiveLitElement {
       height: 80px;
       padding: 20px 16px 8px 32px;
 
-      & > span {
+      & > h1 {
         flex: 1;
         font: var(--cros-display-7-font);
+        margin: 0;
       }
     }
 
@@ -257,7 +258,7 @@ export class RecordingFileList extends ReactiveLitElement {
       selected: this.sortMenuOpened.value,
     };
     return html`<div id="header">
-        <span>${i18n.recordingListHeader}</span>
+        <h1>${i18n.recordingListHeader}</h1>
         <recording-search-box
           aria-label=${i18n.mainSearchLandmarkAriaLabel}
           role="search"
@@ -412,7 +413,7 @@ export class RecordingFileList extends ReactiveLitElement {
       (item) => {
         switch (item.kind) {
           case 'header':
-            return html`<div class="section-heading">${item.label}</div>`;
+            return html`<h2 class="section-heading">${item.label}</h2>`;
           case 'recording': {
             const {recording, searchHighlight} = item;
             const [playing, progress] = (() => {
