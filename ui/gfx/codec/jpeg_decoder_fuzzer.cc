770c34337954ecfa477f17733d01f1902af4f6ed
Avi Drissman <avi@chromium.org>
2024-10-08 07:21:09 +0000

    Clean up ui/gfx/codec for JPG
    This uses base::span as well as removes out parameters.
    This also does various other cleanup while in there.
    
    This maintains parallel new and deprecated APIs. The
    deprecated API usage will be migrated and removed.
    
    Bug: 370696612
    Change-Id: I5633dd2b1830eaeb3219d7352958c527bae481eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905621
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365359}

--- ui/gfx/codec/jpeg_decoder_fuzzer.cc
+++ ui/gfx/codec/jpeg_decoder_fuzzer.cc
@@ -11,10 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  // TODO(crbug.com/323934468): Initialize decoder settings dynamically using
-  // fuzzer input.
-  JPEGCodec::Decode(data, size);
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data_ptr, size_t size) {
+  // TODO(https://crbug.com/323934468): Initialize decoder settings dynamically
+  // using fuzzer input.
+  // SAFETY: libfuzzer gives a valid pointer and size pair.
+  auto data = UNSAFE_BUFFERS(base::span(data_ptr, size));
+  JPEGCodec::Decode(data);
   return 0;
 }
 
