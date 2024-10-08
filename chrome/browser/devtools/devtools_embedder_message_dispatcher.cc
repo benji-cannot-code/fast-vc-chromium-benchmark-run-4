b17e07f3a09de06eeb83cc3fb344af8263b91ada
Alex Rudenko <alexrudenko@chromium.org>
2024-10-08 14:19:19 +0000

    [DevTools] move feature flags to the devtools directory
    As instructed by the comment in chrome/browser/browser_features.h
    that indicates that new feature flags should not be added to
    chrome/browser/browser_features.h
    
    Fixed: 370684557
    Change-Id: Ic1690529893d07f05447359f7af683be51116c46
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913878
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365536}

--- chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
+++ chrome/browser/devtools/devtools_embedder_message_dispatcher.cc
@@ -9,8 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/functional/bind.h"
 #include "base/values.h"
-#include "chrome/browser/browser_features.h"
 #include "chrome/browser/devtools/devtools_settings.h"
+#include "chrome/browser/devtools/features.h"
 #include "chrome/browser/devtools/visual_logging.h"
 
 namespace {
