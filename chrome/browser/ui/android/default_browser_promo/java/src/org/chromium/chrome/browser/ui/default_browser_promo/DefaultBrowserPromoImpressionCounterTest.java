c8721ff2b7f80e417f8d386e12ace12b7380f7bf
Nicolas Dossou-Gbete <dgn@chromium.org>
2024-10-09 11:17:37 +0000

    clay: Suppress the default browser promo after OS choice
    Ensures that we do not bug the user about changing their choice right
    after they selected a different browser in the Android default browser
    and search engine choice screen.
    
    The delay between the choice being made and the first allowed promo
    is Finch-configurable as part of the ClayBlocking feature rollout,
    as "default_browser_promo_suppressed_millis", set to 24h by default.
    
    Bug: b:355196092
    Change-Id: I006978d06002a0028dbb7896eee796d4ae29562f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913053
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Reviewed-by: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366075}

--- chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoImpressionCounterTest.java
+++ chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoImpressionCounterTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.ui.default_browser_promo;
 
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 import android.text.format.DateUtils;
@@ -15,7 +16,10 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.robolectric.annotation.Config;
 
 import org.chromium.base.FakeTimeTestRule;
@@ -27,11 +31,15 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
 import org.chromium.chrome.browser.preferences.ChromeSharedPreferences;
+import org.chromium.components.search_engines.SearchEngineChoiceService;
 
 @RunWith(BaseRobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class DefaultBrowserPromoImpressionCounterTest {
+    @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
     @Rule public FakeTimeTestRule mClockRule = new FakeTimeTestRule();
+
+    @Mock private SearchEngineChoiceService mMockSearchEngineChoiceService;
 
     private DefaultBrowserPromoImpressionCounter mCounter;
     private SharedPreferencesManager mSharedPreferenceManager;
@@ -46,6 +54,9 @@ public class DefaultBrowserPromoImpressionCounterTest {
                 ChromePreferenceKeys.DEFAULT_BROWSER_PROMO_PROMOED_COUNT);
         mSharedPreferenceManager.removeKey(
                 ChromePreferenceKeys.DEFAULT_BROWSER_PROMO_LAST_PROMO_TIME);
+
+        doReturn(false).when(mMockSearchEngineChoiceService).isDefaultBrowserPromoSuppressed();
+        SearchEngineChoiceService.setInstanceForTests(mMockSearchEngineChoiceService);
     }
 
     @After
@@ -198,6 +209,12 @@ public class DefaultBrowserPromoImpressionCounterTest {
         mClockRule.advanceMillis(DateUtils.DAY_IN_MILLIS * 3);
         Assert.assertTrue(mCounter.shouldShowPromo(/* ignoreMaxCount= */ false));
 
+        // If the SearchEngineChoiceService suppresses it, it should not show.
+        doReturn(true).when(mMockSearchEngineChoiceService).isDefaultBrowserPromoSuppressed();
+        Assert.assertFalse(mCounter.shouldShowPromo(/* ignoreMaxCount= */ false));
+
+        // Lift the suppression and trigger the 2nd display
+        doReturn(false).when(mMockSearchEngineChoiceService).isDefaultBrowserPromoSuppressed();
         mCounter.onPromoShown();
 
         // Advance 6 days, not showing the 3rd promo
--- chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoUtilsTest.java
+++ chrome/browser/ui/android/default_browser_promo/java/src/org/chromium/chrome/browser/ui/default_browser_promo/DefaultBrowserPromoUtilsTest.java
@@ -44,6 +44,7 @@ import org.chromium.components.messages.ManagedMessageDispatcher;
 import org.chromium.components.messages.MessageBannerProperties;
 import org.chromium.components.messages.MessageIdentifier;
 import org.chromium.components.messages.MessagesFactory;
+import org.chromium.components.search_engines.SearchEngineChoiceService;
 import org.chromium.ui.base.ActivityWindowAndroid;
 import org.chromium.ui.base.IntentRequestTracker;
 import org.chromium.ui.base.WindowAndroid;
@@ -58,6 +59,7 @@ public class DefaultBrowserPromoUtilsTest {
     @Mock private Tracker mMockTracker;
     @Mock private Profile mProfile;
     @Mock private ManagedMessageDispatcher mMockMessageDispatcher;
+    @Mock private SearchEngineChoiceService mMockSearchEngineChoiceService;
 
     @Rule public MockitoRule mMockitoRule = MockitoJUnit.rule();
 
@@ -74,6 +76,7 @@ public class DefaultBrowserPromoUtilsTest {
                         mActivity, false, IntentRequestTracker.createFromActivity(mActivity));
         TrackerFactory.setTrackerForTests(mMockTracker);
         MessagesFactory.attachMessageDispatcher(mWindowAndroid, mMockMessageDispatcher);
+        SearchEngineChoiceService.setInstanceForTests(mMockSearchEngineChoiceService);
 
         mUtils = new DefaultBrowserPromoUtils(mCounter, mProvider);
         setDepsMockWithDefaultValues();
@@ -286,6 +289,8 @@ public class DefaultBrowserPromoUtilsTest {
     }
 
     private void setDepsMockWithDefaultValues() {
+        when(mMockSearchEngineChoiceService.isDefaultBrowserPromoSuppressed()).thenReturn(false);
+
         when(mCounter.shouldShowPromo(anyBoolean())).thenCallRealMethod();
         when(mCounter.getMinSessionCount()).thenReturn(3);
         when(mCounter.getSessionCount()).thenReturn(10);
