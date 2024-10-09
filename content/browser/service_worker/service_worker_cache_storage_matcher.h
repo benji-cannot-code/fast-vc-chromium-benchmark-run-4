d007c3806f4b5b3977bb6bbfcebb4a659029f989
Keita Suzuki <suzukikeita@chromium.org>
2024-10-09 23:56:11 +0000

    Expose cache lookup time of SW static routing to ServiceWorkerRouterInfo
    This CL exposes cache lookup time of ServiceWorker static routing to
    ServiceWorkerRouterInfo. This will be later used to be exposed to UKM
    for the ServiceWorker static routing timing info.
    
    UKM collection review doc:
    https://docs.google.com/document/d/1hPyA5Im3JNNkOVeJ0HFeu2Qo-GdnL2ON-EZQgAl71dk
    
    Change-Id: I4951cc91e91c101502f001f269f708c76ce13cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5915455
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Commit-Queue: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366538}

--- content/browser/service_worker/service_worker_cache_storage_matcher.h
+++ content/browser/service_worker/service_worker_cache_storage_matcher.h
@@ -50,6 +50,7 @@ class CONTENT_EXPORT ServiceWorkerCacheStorageMatcher {
   void Run();
 
   base::TimeTicks cache_lookup_start() { return cache_lookup_start_; }
+  base::TimeDelta cache_lookup_duration() { return cache_lookup_duration_; }
 
  private:
   void FailFallback();
@@ -67,6 +68,7 @@ class CONTENT_EXPORT ServiceWorkerCacheStorageMatcher {
 
   mojo::Remote<blink::mojom::CacheStorage> remote_;
   base::TimeTicks cache_lookup_start_;
+  base::TimeDelta cache_lookup_duration_;
 
   std::unique_ptr<ServiceWorkerInstalledScriptsSender>
       installed_scripts_sender_;
