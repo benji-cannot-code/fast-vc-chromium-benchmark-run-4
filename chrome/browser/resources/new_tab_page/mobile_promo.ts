414751cbbb3e7996836a496a3e5b308118e9a37d
Robbie Gibson <rkgibson@google.com>
2024-10-09 22:21:29 +0000

    [ntp] Add mobile promo dismiss button handling
    The outermost mobile promo container is in middle_slot_promo.ts, so that
    class needs to handle actually dismissing the mobile promo. So the
    mobile promo will fire a "dismiss" event and also expose handlers for
    the middle slot promo to call after it actually hides the UI.
    
    Bug: 372258671
    Change-Id: I73cd3d03eab82ede20ddb24098e6f86440bedefa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917030
    Reviewed-by: Paul Adedeji <pauladedeji@google.com>
    Commit-Queue: Robbie Gibson <rkgibson@google.com>
    Auto-Submit: Robbie Gibson <rkgibson@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366490}

--- chrome/browser/resources/new_tab_page/mobile_promo.ts
+++ chrome/browser/resources/new_tab_page/mobile_promo.ts
@@ -3,11 +3,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type {CrToastElement} from 'chrome://resources/cr_elements/cr_toast/cr_toast.js';
 import {CrLitElement} from 'chrome://resources/lit/v3_0/lit.rollup.js';
 
 import {getCss} from './mobile_promo.css.js';
 import {getHtml} from './mobile_promo.html.js';
 import {NewTabPageProxy} from './new_tab_page_proxy.js';
+
+export interface MobilePromoElement {
+  $: {
+    dismissPromoButtonToast: CrToastElement,
+    titleAndDismissContainer: HTMLElement,
+    promoContainer: HTMLElement,
+    undoDismissPromoButton: HTMLElement,
+  };
+}
 
 /**
  * @fileoverview This file provides a custom element for displaying a mobile
@@ -31,7 +41,6 @@ export class MobilePromoElement extends CrLitElement {
       qrCode: {
         type: String,
         notify: true,
-        reflect: true,
       },
     };
   }
@@ -44,6 +53,18 @@ export class MobilePromoElement extends CrLitElement {
         ({qrCode}) => {
           this.qrCode = qrCode;
         });
+  }
+
+  protected onDismissButtonClick_() {
+    this.$.promoContainer.hidden = true;
+    this.$.dismissPromoButtonToast.show();
+    // TODO(crbub.com/372306118): Handle dismiss in C++.
+  }
+
+  protected onUndoDismissPromoButtonClick_() {
+    this.$.promoContainer.hidden = false;
+    this.$.dismissPromoButtonToast.hide();
+    // TODO(crbub.com/372306118): Handle dismiss in C++.
   }
 }
 
