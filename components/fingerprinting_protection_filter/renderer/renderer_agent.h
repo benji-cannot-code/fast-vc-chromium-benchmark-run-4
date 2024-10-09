dc2a1b37e4dbb77bb62afd3ee33c912c16e718e6
Tanushree Bisht <tanub@google.com>
2024-10-09 17:26:27 +0000

    [FPF] Set Document Load Statistics.
    - Changes to complete set up of `DocumentLoadStatistics` to populate
    `PageLoadStatistics` histograms.
    - Adds histogram assertions to browser tests.
    - Adds browser test for verifying that performance measurement
    related histograms are populated.
    - Tested locally for non-incognito and incognito modes.
    
    Bug: 364688841,369142464
    
    Change-Id: I92173a031c6b79c5b7b2f39d9080d59725c32c3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910305
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Commit-Queue: Tanushree Bisht <tanub@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366289}

--- components/fingerprinting_protection_filter/renderer/renderer_agent.h
+++ components/fingerprinting_protection_filter/renderer/renderer_agent.h
@@ -71,6 +71,8 @@ class RendererAgent
 
   void DidFailProvisionalLoad() override;
 
+  void DidFinishLoad() override;
+
   // Used to delete `this` to avoid memory leaks and ensure `render_frame()` is
   // always valid.
   void OnDestruct() override;
@@ -135,6 +137,11 @@ class RendererAgent
   // resource loads.
   virtual void SetFilter(
       std::unique_ptr<subresource_filter::DocumentSubresourceFilter> filter);
+
+  // Sends statistics about the `DocumentSubresourceFilter`s work to the
+  // browser.
+  virtual void SendDocumentLoadStatistics(
+      const subresource_filter::mojom::DocumentLoadStatistics& statistics);
 
  private:
   // Initializes `filter_`. Assumes that activation has been computed.
