b515bf7f2c8323a104113dc723718ad1dad86190
Timofey Chudakov <tchudakov@google.com>
2024-10-09 07:37:16 +0000

    [Android] Move PlusAddressesHelper to //chrome/android.
    CustomTabActivity is part of //chrome/android. The PlusAddressesHelper
    is added as a dependency to the chrome_all_java. This dependency is
    removed by moving the PlusAddressesHelper directly to //chrome/android.
    
    Bug: 363733915
    Change-Id: I3cee5848b24e78169ed5f54e07c4a862bcf3266b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912805
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366011}

--- chrome/browser/ui/android/plus_addresses/plus_addresses_helper.cc
+++ chrome/browser/ui/android/plus_addresses/plus_addresses_helper.cc
@@ -7,11 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_string.h"
-#include "chrome/browser/ui/android/plus_addresses/jni_headers/PlusAddressesHelper_jni.h"
 #include "components/plus_addresses/features.h"
 #include "content/public/browser/web_contents.h"
 #include "ui/android/view_android.h"
 #include "ui/android/window_android.h"
+
+// Must come after all headers that specialize FromJniType() / ToJniType().
+#include "chrome/android/chrome_jni_headers/PlusAddressesHelper_jni.h"
 
 namespace plus_addresses {
 
