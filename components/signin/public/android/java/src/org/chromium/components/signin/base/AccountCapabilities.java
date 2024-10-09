e237721394fa1e3b4c874984baf598aa6f781891
Chuong Ho <hdchuong@google.com>
2024-10-09 07:52:35 +0000

    editor: add a new capability for copyeditor only
    This CL adds a new capability that is used exclusively for Copy editor
    feature.
    
    Server side definition: cl/669998559
    
    Bug: 321624868
    Change-Id: I2f0984ab3be5981e87989a353bc0e460bcffc474
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868819
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Andrew Moylan <amoylan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366012}

--- components/signin/public/android/java/src/org/chromium/components/signin/base/AccountCapabilities.java
+++ components/signin/public/android/java/src/org/chromium/components/signin/base/AccountCapabilities.java
@@ -123,6 +123,13 @@ public class AccountCapabilities {
     }
 
     /**
+     * @return canUseCopyeditorFeature capability value.
+     */
+    public @Tribool int canUseCopyeditorFeature() {
+        return getCapabilityByName(AccountCapabilitiesConstants.CAN_USE_COPYEDITOR_FEATURE_NAME);
+    }
+
+    /**
      * @return canUseDevToolsGenerativeAiFeatures capability value.
      */
     public @Tribool int canUseDevToolsGenerativeAiFeatures() {
