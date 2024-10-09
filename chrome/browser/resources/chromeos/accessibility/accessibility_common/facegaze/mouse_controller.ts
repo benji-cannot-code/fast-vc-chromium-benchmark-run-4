8768625fc65b863ec8be9ecb35c75e953e4fddf2
Amanda Lin Dietz <aldietz@google.com>
2024-10-09 21:09:34 +0000

    [FaceGaze] Add macro for long click/drag and drop
    * Add MouseClickLongMacro to handle long click and error checking.
    * Update drag and drop behavior to toggle drag when the gesture
    is detected and toggle release at the next detected gesture.
    This addresses feedback that drag and drop was difficult to use
    when needing to hold the gesture for the action duration.
    This also reduces the chances of error during dropped frames
    when the gesture is mistakenly no longer detected.
    * Update tests to reflect toggle behavior.
    
    TODO:
    * Update top bar UI to reflect drag state.
    * Remove long click logic in MouseClickMacro.
    * Add tests to check for drag events.
    
    AX-Relnotes: N/A
    Bug: 371052538
    Change-Id: Iab94251dbe804f1affaa2e335ff27696065b18a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914468
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366445}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/mouse_controller.ts
@@ -18,6 +18,7 @@ import AutomationNode = chrome.automation.AutomationNode;
 import RoleType = chrome.automation.RoleType;
 import ScreenRect = chrome.accessibilityPrivate.ScreenRect;
 import ScreenPoint = chrome.accessibilityPrivate.ScreenPoint;
+import SyntheticMouseEventButton = chrome.accessibilityPrivate.SyntheticMouseEventButton;
 
 type PrefObject = chrome.settingsPrivate.PrefObject;
 
@@ -89,6 +90,7 @@ export class MouseController {
 
   private scrollModeController_: ScrollModeController;
   private bubbleController_: BubbleController;
+  private longClickActive_ = false;
 
   constructor(bubbleController: BubbleController) {
     this.bubbleController_ = bubbleController;
@@ -129,6 +131,10 @@ export class MouseController {
 
   isScrollModeActive(): boolean {
     return this.scrollModeController_.active();
+  }
+
+  isLongClickActive(): boolean {
+    return this.longClickActive_;
   }
 
   async start(): Promise<void> {
@@ -506,6 +512,12 @@ export class MouseController {
   }
 
   stop(): void {
+    if (this.longClickActive_ && this.mouseLocation_) {
+      // Release the existing long click action when the mouse controller is
+      // stopped to ensure we do not leave the user in a permanent "drag" state.
+      EventGenerator.sendMouseRelease(
+          this.mouseLocation_.x, this.mouseLocation_.y);
+    }
     if (this.mouseInterval_ !== -1) {
       clearInterval(this.mouseInterval_);
       this.mouseInterval_ = -1;
@@ -541,6 +553,23 @@ export class MouseController {
     }
   }
 
+  toggleLongClick(): void {
+    if (!this.mouseLocation_) {
+      return;
+    }
+
+    this.longClickActive_ = !this.longClickActive_;
+
+    if (this.longClickActive_) {
+      EventGenerator.sendMousePress(
+          this.mouseLocation_.x, this.mouseLocation_.y,
+          SyntheticMouseEventButton.LEFT);
+    } else {
+      EventGenerator.sendMouseRelease(
+          this.mouseLocation_.x, this.mouseLocation_.y);
+    }
+  }
+
   /** Listener for when the mouse position changes. */
   private onMouseMovedOrDragged_(event: chrome.automation.AutomationEvent):
       void {
@@ -553,6 +582,13 @@ export class MouseController {
       if (this.scrollModeController_.active()) {
         // Scroll mode honors physical mouse movements.
         this.scrollModeController_.updateScrollLocation(this.mouseLocation_);
+      }
+
+      if (this.longClickActive_) {
+        // Send a synthetic drag event from the user's mouse move event.
+        // FaceGaze cursor control should already have sent a synthetic drag
+        // event, so this only needs to occur on user mouse movements.
+        EventGenerator.sendMouseMove(event.mouseX, event.mouseY);
       }
     }
   }
