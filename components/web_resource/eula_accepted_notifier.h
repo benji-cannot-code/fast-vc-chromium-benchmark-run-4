30d7363ce32f6cb2b8f82ec704e6c7b3da97d4eb
Sorin Jianu <sorin@chromium.org>
2024-10-09 11:32:49 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/web_resource
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=achuith@chromium.org
    
    Bug: 371316188
    Change-Id: I1663f4c7aa7ee4008497597aae1762acaefecfbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918057
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Commit-Queue: Achuith Bhandarkar <achuith@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366077}

--- components/web_resource/eula_accepted_notifier.h
+++ components/web_resource/eula_accepted_notifier.h
@@ -20,7 +20,7 @@ class EulaAcceptedNotifier {
   // Observes EULA accepted state changes.
   class Observer {
    public:
-    virtual ~Observer() {}
+    virtual ~Observer() = default;
     virtual void OnEulaAccepted() = 0;
   };
 
