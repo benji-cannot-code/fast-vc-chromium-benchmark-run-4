4ee23f2c67c5a4f326e5d21ce4b95686f4f9f316
Tom Sepez <tsepez@chromium.org>
2024-10-09 23:20:58 +0000

    Replace const-value arguments where const-ref intended.
    This is a re-split of a previousl split CL.
    
    This is of concern when passing vectors and strings, as unintended
    (and often expensive) copies occur under the covers.
    
    Apply some of the following (depending on split):
    -- Make non-const and move in callee where appropriate.
    -- Convert to string_view where appropriate.
    -- Avoid stray const in a few places
    
    This CL was uploaded by git cl split.
    
    R=chadduffin@chromium.org
    
    Bug: 369952985
    Change-Id: I8577e01c7e269c802f19fd8538dee1fa5cc350f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913370
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Auto-Submit: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366521}

--- chrome/test/base/ash/interactive/network/shill_service_util.cc
+++ chrome/test/base/ash/interactive/network/shill_service_util.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/ash/interactive/network/shill_service_util.h"
 
+#include <utility>
+
 #include "base/containers/contains.h"
 #include "base/strings/stringprintf.h"
 #include "chromeos/ash/components/dbus/shill/fake_shill_profile_client.h"
@@ -15,18 +17,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-ShillServiceInfo::ShillServiceInfo(unsigned int id,
-                                   const std::string service_type)
+ShillServiceInfo::ShillServiceInfo(unsigned int id, std::string service_type)
     : service_name_(base::StringPrintf("service_name_%u", id)),
       service_path_(base::StringPrintf("service_path_%u", id)),
       service_guid_(base::StringPrintf("service_guid_%u", id)),
-      service_type_(service_type) {
+      service_type_(std::move(service_type)) {
   const std::array<std::string, 4> valid_service_types = {
       shill::kTypeWifi, shill::kTypeCellular, shill::kTypeEthernet,
       shill::kTypeVPN};
-  if (!base::Contains(valid_service_types, service_type)) {
-    NOTREACHED_NORETURN();
-  }
+  CHECK(base::Contains(valid_service_types, service_type_));
 }
 
 ShillServiceInfo::~ShillServiceInfo() = default;
