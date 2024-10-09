25d6ce5f3bf216c3392dc9e8214c7469d02eeb45
Yuki Awano <yawano@google.com>
2024-10-09 02:34:02 +0000

    assistant: add opt-in ui rendering expectations
    Bug: b:331246490
    Test: browser_tests --gtest_filter=*AssistantOptInFlow*
    Change-Id: I1887bec86cebe1c520f943a198e35999b2d1d664
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906126
    Reviewed-by: Simon Ziegltrum <ziegltrum@google.com>
    Commit-Queue: Yuki Awano <yawano@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365943}

--- chrome/browser/ash/login/screens/assistant_optin_flow_screen_browsertest.cc
+++ chrome/browser/ash/login/screens/assistant_optin_flow_screen_browsertest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/metrics/histogram_base.h"
 #include "base/path_service.h"
+#include "base/strings/strcat.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/test_future.h"
 #include "chrome/browser/ash/login/login_wizard.h"
@@ -71,10 +72,26 @@ constexpr char kValueProp[] = "valueProp";
 constexpr char kRelatedInfo[] = "relatedInfo";
 constexpr char kVoiceMatch[] = "voiceMatch";
 
+constexpr char kSettingsZippyTitle[] = "Settings-Zippy-Title";
+constexpr char kSettingsZippyDescription[] = "Settings-Zippy-Description";
+constexpr char kSettingsZippyAdditionalInfo[] =
+    "Settings-Zippy-Additional-Info";
+constexpr char kSettingsZippyLearnMoreLink[] = "Learn more";
+
+// &ensp;
+constexpr char kEnsp[] = "\xe2\x80\x82";
+
 const test::UIPath kAssistantLoading = {kAssistantOptInId,
                                         kAssistantOptInFlowCard, kLoading};
 const test::UIPath kLoadingRetryButton = {
     kAssistantOptInId, kAssistantOptInFlowCard, kLoading, "retry-button"};
+const test::UIPath kSettingsZippyTitleFirst = {
+    kAssistantOptInId, kAssistantOptInFlowCard, kValueProp, "title-0"};
+const test::UIPath kSettingsZippyDescriptionFirst = {
+    kAssistantOptInId, kAssistantOptInFlowCard, kValueProp, "description-0"};
+const test::UIPath kSettingsZippyAdditionalInfoFirst = {
+    kAssistantOptInId, kAssistantOptInFlowCard, kValueProp,
+    "additional-info-0"};
 
 const test::UIPath kAssistantValueProp = {kAssistantOptInId,
                                           kAssistantOptInFlowCard, kValueProp};
@@ -254,11 +271,11 @@ class ScopedAssistantSettings : public assistant::AssistantSettings {
     activity_control_ui->add_footer_paragraph();
     activity_control_ui->set_footer_paragraph(0, "A footer");
     auto* setting = activity_control_ui->add_setting_zippy();
-    setting->set_title("Cool feature");
+    setting->set_title(kSettingsZippyTitle);
     setting->add_description_paragraph();
-    setting->set_description_paragraph(0, "But needs consent");
+    setting->set_description_paragraph(0, kSettingsZippyDescription);
     setting->add_additional_info_paragraph();
-    setting->set_additional_info_paragraph(0, "And it's really cool");
+    setting->set_additional_info_paragraph(0, kSettingsZippyAdditionalInfo);
     setting->set_icon_uri("assistant_icon");
     setting->set_setting_set_id(assistant::SettingSetId::WAA);
   }
@@ -998,6 +1015,14 @@ IN_PROC_BROWSER_TEST_F(AssistantOptInFlowMinorModeTest,
   test::OobeJS().CreateVisibilityWaiter(true, kAssistantValueProp)->Wait();
   EXPECT_FALSE(
       test::OobeJS().GetAttributeBool("inverse", kValuePropNextButton));
+  test::OobeJS().ExpectElementText(kSettingsZippyTitle,
+                                   kSettingsZippyTitleFirst);
+  test::OobeJS().ExpectElementText(
+      base::StrCat(
+          {kSettingsZippyDescription, kEnsp, kSettingsZippyLearnMoreLink}),
+      kSettingsZippyDescriptionFirst);
+  test::OobeJS().ExpectElementText(kSettingsZippyAdditionalInfo,
+                                   kSettingsZippyAdditionalInfoFirst);
   TapWhenEnabled(kValuePropNextButton);
   EXPECT_FALSE(
       test::OobeJS().GetAttributeBool("inverse", kValuePropNextButton));
