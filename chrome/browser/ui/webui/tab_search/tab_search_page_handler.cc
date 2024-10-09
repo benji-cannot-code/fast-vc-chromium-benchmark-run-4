b87b31cca9521a40492483d3856a135b6794994f
Zaina Al-Mashni <zalmashni@google.com>
2024-10-09 17:59:08 +0000

    [Synapse] Remove toggle from tab organization subpage
    Remove toggle and add a "learn more" HC link to the tab organization
    row in the subpage.
    
    Screenshot: https://screenshot.googleplex.com/UYs6WWU3UpDhQ7R
    Bug: 362225975
    Change-Id: Iceb6a456c267b213f90d9f263521306b80581cff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917145
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366309}

--- chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
+++ chrome/browser/ui/webui/tab_search/tab_search_page_handler.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/user_education/tutorial_identifiers.h"
 #include "chrome/browser/user_education/user_education_service.h"
 #include "chrome/browser/user_education/user_education_service_factory.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/common/webui_url_constants.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/optimization_guide/core/optimization_guide_model_executor.h"
@@ -726,7 +727,7 @@ void TabSearchPageHandler::TriggerSignIn() {
 
 void TabSearchPageHandler::OpenHelpPage() {
   Browser* browser = chrome::FindLastActive();
-  GURL help_url("https://support.google.com/chrome?p=auto_tab_group");
+  GURL help_url(chrome::kTabOrganizationLearnMorePageURL);
   NavigateParams params(browser, help_url,
                         ui::PageTransition::PAGE_TRANSITION_LINK);
   params.disposition = WindowOpenDisposition::NEW_FOREGROUND_TAB;
