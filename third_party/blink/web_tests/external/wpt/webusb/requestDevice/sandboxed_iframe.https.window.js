1b7474329af996530c7435b6d59b01cb44425969
Jonathan Lee <jonathanjlee@google.com>
2024-10-09 20:33:20 +0000

    [wpt] Fix missing `testdriver(-vendor).js` in `webusb/`
    https://web-platform-tests.org/writing-tests/testdriver.html#markup
    says:
    > The testdriver.js and testdriver-vendor.js must both be included ...
    > in the top-level test document when using testdriver from a different
    > context
    
    This is needed to make the test portable to non-content shell browsers,
    which implement `testdriver.js` with WebDriver and cross-context
    `message` events, not with internal JS bindings.
    
    Bug: None
    Change-Id: I0076f6c850fab42099637e764e0b890411ea1a2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5902557
    Auto-Submit: Jonathan Lee <jonathanjlee@google.com>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366424}

--- third_party/blink/web_tests/external/wpt/webusb/requestDevice/sandboxed_iframe.https.window.js
+++ third_party/blink/web_tests/external/wpt/webusb/requestDevice/sandboxed_iframe.https.window.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+
 'use strict';
 
 promise_test(async (t) => {
