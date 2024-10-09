2ffd6ac8b7f0043edf778ec15e1e20022c9c0e81
Sorin Jianu <sorin@chromium.org>
2024-10-09 03:52:03 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/web_modal
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=pbos@chromium.org
    
    Bug: 371316188
    Change-Id: I363d16272b1babad60bd590a19df643c8b0832b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917423
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365969}

--- components/web_modal/single_web_contents_dialog_manager.h
+++ components/web_modal/single_web_contents_dialog_manager.h
@@ -20,14 +20,14 @@ class WebContentsModalDialogHost;
 // WebContentsModalDialogManager.
 class SingleWebContentsDialogManagerDelegate {
  public:
-  SingleWebContentsDialogManagerDelegate() {}
+  SingleWebContentsDialogManagerDelegate() = default;
 
   SingleWebContentsDialogManagerDelegate(
       const SingleWebContentsDialogManagerDelegate&) = delete;
   SingleWebContentsDialogManagerDelegate& operator=(
       const SingleWebContentsDialogManagerDelegate&) = delete;
 
-  virtual ~SingleWebContentsDialogManagerDelegate() {}
+  virtual ~SingleWebContentsDialogManagerDelegate() = default;
 
   virtual content::WebContents* GetWebContents() const = 0;
 
@@ -50,7 +50,7 @@ class SingleWebContentsDialogManager {
   SingleWebContentsDialogManager& operator=(
       const SingleWebContentsDialogManager&) = delete;
 
-  virtual ~SingleWebContentsDialogManager() {}
+  virtual ~SingleWebContentsDialogManager() = default;
 
   // Makes the web contents modal dialog visible. Only one web contents modal
   // dialog is shown at a time per tab.
@@ -81,7 +81,7 @@ class SingleWebContentsDialogManager {
   virtual bool IsActive() const = 0;
 
  protected:
-  SingleWebContentsDialogManager() {}
+  SingleWebContentsDialogManager() = default;
 };
 
 }  // namespace web_modal
