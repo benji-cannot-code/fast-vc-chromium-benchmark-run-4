7fb63dd5e7005f2054eca5bf75e39f4952bebe1e
Hardik Goyal <hardikgoyal@chromium.org>
2024-10-09 21:02:14 +0000

    Remove references to auth_locked in auth_factor_conversions_unittest.cc
    auth_locked is deprecated and will be removed soon.
    
    Change-Id: I2a871d92ff3546c591cf85e697bb3e96a251cc13
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920955
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366442}

--- chromeos/ash/components/cryptohome/auth_factor_conversions_unittest.cc
+++ chromeos/ash/components/cryptohome/auth_factor_conversions_unittest.cc
@@ -216,9 +216,6 @@ TEST_F(AuthFactorConversionsTest, PinFactorStatusConversion) {
   // PinFactor indefinitely locked.
   {
     factor_with_status.clear_status_info();
-    factor_with_status.mutable_auth_factor()
-        ->mutable_pin_metadata()
-        ->set_auth_locked(true);
     auto* status_info = factor_with_status.mutable_status_info();
     status_info->set_time_available_in(std::numeric_limits<uint64_t>::max());
 
@@ -232,9 +229,6 @@ TEST_F(AuthFactorConversionsTest, PinFactorStatusConversion) {
   // PinFactor temporary locked with a timeout.
   {
     factor_with_status.clear_status_info();
-    factor_with_status.mutable_auth_factor()
-        ->mutable_pin_metadata()
-        ->set_auth_locked(true);
     auto* status_info = factor_with_status.mutable_status_info();
     status_info->set_time_available_in(in_a_while.InMilliseconds());
 
