e82da189f3d9227df374287309543f142b0660bf
Sorin Jianu <sorin@chromium.org>
2024-10-09 12:02:52 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/webrtc
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=orphis@chromium.org
    
    Bug: 371316188
    Change-Id: I1e3d7decb3f05f2af0d3f91ea832e72c266b52ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917340
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Florent Castelli <orphis@chromium.org>
    Commit-Queue: Florent Castelli <orphis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366084}

--- components/webrtc/fake_ssl_client_socket_unittest.cc
+++ components/webrtc/fake_ssl_client_socket_unittest.cc
@@ -60,7 +60,7 @@ enum {
 // Used by PassThroughMethods test.
 class MockClientSocket : public net::StreamSocket {
  public:
-  ~MockClientSocket() override {}
+  ~MockClientSocket() override = default;
 
   MOCK_METHOD3(Read, int(net::IOBuffer*, int, net::CompletionOnceCallback));
   MOCK_METHOD4(Write,
@@ -106,9 +106,9 @@ void AddChunkedOps(std::string_view data,
 
 class FakeSSLClientSocketTest : public testing::Test {
  protected:
-  FakeSSLClientSocketTest() {}
+  FakeSSLClientSocketTest() = default;
 
-  ~FakeSSLClientSocketTest() override {}
+  ~FakeSSLClientSocketTest() override = default;
 
   std::unique_ptr<net::StreamSocket> MakeClientSocket() {
     return mock_client_socket_factory_.CreateTransportClientSocket(
