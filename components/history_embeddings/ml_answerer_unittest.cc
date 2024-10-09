bf4b13f1ea8e1bbf6c3a47182774231870fd7c36
Zekun Jiang <zekunjiang@google.com>
2024-10-09 23:02:34 +0000

    Limit the tokens to ensure context is not interrupted.
    Bug: 352384806
    Change-Id: I47e6a06c4baa03f078e4a8b95b7433e5ae64d5a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921867
    Reviewed-by: Jun Zou <junzou@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Zekun Jiang <zekunjiang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366510}

--- components/history_embeddings/ml_answerer_unittest.cc
+++ components/history_embeddings/ml_answerer_unittest.cc
@@ -45,10 +45,7 @@ class HistoryEmbeddingsMlAnswererTest : public testing::Test {
  public:
   void SetUp() override {
     ml_answerer_ = std::make_unique<MlAnswerer>(&model_executor_);
-    token_limits_ = {0, 1000, 0, 0};
   }
-
-  optimization_guide::TokenLimits& GetTokenLimits() { return token_limits_; }
 
  protected:
   optimization_guide::StreamingResponse MakeResponse(
@@ -69,7 +66,6 @@ class HistoryEmbeddingsMlAnswererTest : public testing::Test {
   testing::NiceMock<MockModelExecutor> model_executor_;
   std::unique_ptr<MlAnswerer> ml_answerer_;
   testing::NiceMock<optimization_guide::MockSession> session_1_, session_2_;
-  optimization_guide::TokenLimits token_limits_;
 };
 
 TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerNoSession) {
@@ -81,11 +77,6 @@ TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerNoSession) {
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
           [&](optimization_guide::OptimizationGuideModelSizeInTokenCallback
                   callback) { std::move(callback).Run(100); })));
-
-  ON_CALL(session_1_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
 
   Answerer::Context context("1");
   context.url_passages_map.insert({"url_1", {"passage_11", "passage_12"}});
@@ -107,11 +98,6 @@ TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerExecutionFailure) {
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
           [&](optimization_guide::OptimizationGuideModelSizeInTokenCallback
                   callback) { std::move(callback).Run(100); })));
-
-  ON_CALL(session_1_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
 
   ON_CALL(session_1_, Score(_, _))
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
@@ -156,11 +142,6 @@ TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerSingleUrl) {
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
           [&](optimization_guide::OptimizationGuideModelSizeInTokenCallback
                   callback) { std::move(callback).Run(100); })));
-
-  ON_CALL(session_1_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
 
   ON_CALL(session_1_, Score(_, _))
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
@@ -215,16 +196,6 @@ TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerMultipleUrls) {
           [&](optimization_guide::OptimizationGuideModelSizeInTokenCallback
                   callback) { std::move(callback).Run(100); })));
 
-  ON_CALL(session_1_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
-
-  ON_CALL(session_2_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
-
   ON_CALL(session_1_, Score(_, _))
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
           [&](optimization_guide::OptimizationGuideModelScoreCallback
@@ -272,11 +243,6 @@ TEST_F(HistoryEmbeddingsMlAnswererTest, ComputeAnswerUnanswerable) {
       .WillByDefault(testing::WithArg<1>(testing::Invoke(
           [&](optimization_guide::OptimizationGuideModelSizeInTokenCallback
                   callback) { std::move(callback).Run(100); })));
-
-  ON_CALL(session_1_, GetTokenLimits())
-      .WillByDefault([&]() -> optimization_guide::TokenLimits& {
-        return GetTokenLimits();
-      });
 
   // Below the default 0.5 threshold.
   ON_CALL(session_1_, Score(_, _))
