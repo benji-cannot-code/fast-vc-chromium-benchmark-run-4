e0367f58efc1bf497ba1698e6ec91a91b9477b06
Mingyu Lei <leimy@chromium.org>
2024-10-09 03:18:27 +0000

    Prompt API: update the default temperature to 1.0
    Bug: 371019871
    Change-Id: Ib63aae5a6eb392114c38c0b7ba202f55c85fb7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912600
    Reviewed-by: Fergal Daly <fergal@chromium.org>
    Commit-Queue: Mingyu Lei <leimy@chromium.org>
    Reviewed-by: Jiacheng Guo <gjc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365962}

--- chrome/browser/ai/ai_manager_keyed_service.cc
+++ chrome/browser/ai/ai_manager_keyed_service.cc
@@ -66,6 +66,14 @@ int GetAssistantModelMaxTopK() {
   return max_top_k;
 }
 
+double GetAssistantModelDefaultTemperature() {
+  if (base::FeatureList::IsEnabled(
+          features::kAIAssistantOverrideConfiguration)) {
+    return features::kAIAssistantOverrideConfigurationDefaultTemperature.Get();
+  }
+  return optimization_guide::features::GetOnDeviceModelDefaultTemperature();
+}
+
 blink::mojom::ModelAvailabilityCheckResult
 ConvertOnDeviceModelEligibilityReasonToModelAvailabilityCheckResult(
     optimization_guide::OnDeviceModelEligibilityReason
@@ -379,6 +387,11 @@ std::unique_ptr<AIAssistant> AIManagerKeyedService::CreateAssistantInternal(
         .top_k = std::min(sampling_params->top_k,
                           uint32_t(GetAssistantModelMaxTopK())),
         .temperature = sampling_params->temperature};
+  } else {
+    config_params.sampling_params = optimization_guide::SamplingParams{
+        .top_k = uint32_t(
+            optimization_guide::features::GetOnDeviceModelDefaultTopK()),
+        .temperature = float(GetAssistantModelDefaultTemperature())};
   }
 
   std::unique_ptr<optimization_guide::OptimizationGuideModelExecutor::Session>
@@ -462,8 +475,7 @@ void AIManagerKeyedService::CreateSummarizer(
 void AIManagerKeyedService::GetModelInfo(GetModelInfoCallback callback) {
   std::move(callback).Run(blink::mojom::AIModelInfo::New(
       optimization_guide::features::GetOnDeviceModelDefaultTopK(),
-      GetAssistantModelMaxTopK(),
-      optimization_guide::features::GetOnDeviceModelDefaultTemperature()));
+      GetAssistantModelMaxTopK(), GetAssistantModelDefaultTemperature()));
 }
 
 void AIManagerKeyedService::CreateWriter(
