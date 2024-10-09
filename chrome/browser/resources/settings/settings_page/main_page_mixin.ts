749cd229dc090383aa41b10d9357f7a08eb84874
Zaina Al-Mashni <zalmashni@google.com>
2024-10-09 00:37:53 +0000

    [Settings] Fix navigation between unrelated subpages
    Handle SUBPAGE -> SUBPAGE navigation when both subpages reside under different sections.
    
    This is fixed by first navigating to the newRoute section before entering the newRoute subpage.
    
    Bug: 369997166
    Change-Id: I3b42d5f99fbf5bb17acd8244649899674a92cd3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918485
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1365912}

--- chrome/browser/resources/settings/settings_page/main_page_mixin.ts
+++ chrome/browser/resources/settings/settings_page/main_page_mixin.ts
@@ -347,6 +347,7 @@ export const MainPageMixin = dedupingMixin(
               if (!oldRoute!.contains(newRoute) &&
                   !newRoute.contains(oldRoute!)) {
                 this.enterMainPage_(oldRoute!).then(() => {
+                  this.switchToSections_(newRoute);
                   this.enterSubpage_(newRoute);
                 });
                 return;
