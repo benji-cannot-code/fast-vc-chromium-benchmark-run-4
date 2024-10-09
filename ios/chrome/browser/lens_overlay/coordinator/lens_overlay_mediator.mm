8a43ac8a6405de1dab7d8953823a18cbf7c16cfc
Christian Xu <christianxu@google.com>
2024-10-09 16:43:09 +0000

    [iOS][LO] Add lens omnibox focus user action
    Bug: 370499893
    Change-Id: I1d1b5b3c03acf480290aa2aeafa96e635f69ee17
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920199
    Commit-Queue: Robert Kaplow <rkaplow@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366248}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_mediator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_mediator.mm
@@ -180,6 +180,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark LensToolbarMutator
 
 - (void)focusOmnibox {
+  RecordAction(base::UserMetricsAction("Mobile.LensOverlay.FocusOmnibox"));
   [self.omniboxCoordinator focusOmnibox];
   [self.toolbarConsumer setOmniboxFocused:YES];
   [self.omniboxCoordinator.animatee setClearButtonFaded:NO];
