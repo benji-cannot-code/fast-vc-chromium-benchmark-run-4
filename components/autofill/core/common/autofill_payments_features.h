0fcae948ef638a89da29ee0df3f7d9c71d22c360
Slobodan Pejic <slobodan@chromium.org>
2024-10-09 14:51:39 +0000

    Clean up flag kAutofillEnableVirtualCardEnrollMetricsLogger.
    Bug: 370482420
    Change-Id: Ieb780c0c1e8b92fef4541f53e28c8d65fe9747b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913855
    Reviewed-by: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366165}

--- components/autofill/core/common/autofill_payments_features.h
+++ components/autofill/core/common/autofill_payments_features.h
@@ -89,8 +89,6 @@ BASE_DECLARE_FEATURE(kAutofillEnableVcnEnrollLoadingAndConfirmation);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableVcnGrayOutForMerchantOptOut);
 COMPONENT_EXPORT(AUTOFILL)
-BASE_DECLARE_FEATURE(kAutofillEnableVirtualCardEnrollMetricsLogger);
-COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableVerveCardSupport);
 COMPONENT_EXPORT(AUTOFILL)
 BASE_DECLARE_FEATURE(kAutofillEnableVirtualCardMetadata);
