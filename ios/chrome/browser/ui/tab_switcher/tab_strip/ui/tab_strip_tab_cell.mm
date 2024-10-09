82aa4d2d6cb3739a482b9f70e07570192dd0df3e
Gauthier Ambard <gambard@chromium.org>
2024-10-09 15:11:25 +0000

    [iOS] Add behaviours for TabStrip v3
    Add the first 4 arms of V3.
    Arm 1
    https://screenshot.googleplex.com/6vXbksbzjGBcGUb
    https://screenshot.googleplex.com/5bJ3K3pf6F2DnKc
    https://screenshot.googleplex.com/AtcEThshXPcUgWc
    https://screenshot.googleplex.com/BAdGEVB3dGncepd
    
    Arm 2
    https://screenshot.googleplex.com/wjeWHsComznh2j3
    https://screenshot.googleplex.com/7u8XszjiLFp4hQo
    https://screenshot.googleplex.com/Ac6V4UzDoJPQYix
    https://screenshot.googleplex.com/5o3mWD4pmLfyPRu
    
    Arm 3
    https://screenshot.googleplex.com/4kuqk3oatYQqQMs
    https://screenshot.googleplex.com/X5c8nNrbNJHixnQ
    https://screenshot.googleplex.com/7nNcFSDeyETS7tD
    https://screenshot.googleplex.com/6y8tUZxYaxxitFy
    
    Arm 4
    https://screenshot.googleplex.com/3qj8vW4SUj9QyA3
    https://screenshot.googleplex.com/9NUnPU9Yf5Efi3Y
    https://screenshot.googleplex.com/7C2mLnYy6JiKBZL
    https://screenshot.googleplex.com/9wp6KNnjbNxN5cn
    
    Bug: 372224636
    Change-Id: I16ed869f47da1b5db072b1961f21820ee0e40a0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917207
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366178}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_tab_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_tab_cell.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <MaterialComponents/MaterialActivityIndicator.h>
 
+#import <algorithm>
+
 #import "base/metrics/user_metrics.h"
 #import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
@@ -41,6 +43,9 @@ const CGFloat kCollapsedWidthThreshold = 150;
 // Separator constraints.
 const CGFloat kSeparatorHorizontalInset = 2;
 const CGFloat kSeparatorGradientWidth = 4;
+
+// Visibility constants.
+constexpr CGFloat kCloseButtonVisibilityThreshold = 0.3;
 
 // Content view constants.
 const CGFloat kFaviconLeadingMargin = 10;
@@ -385,6 +390,19 @@ UIImage* DefaultFavicon() {
   }
 }
 
+- (void)setCloseButtonVisibility:(CGFloat)visibility {
+  CGFloat closeButtonAlpha =
+      std::clamp<CGFloat>((visibility - kCloseButtonVisibilityThreshold) /
+                              (1 - kCloseButtonVisibilityThreshold),
+                          0, 1);
+  _closeButton.alpha = closeButtonAlpha;
+  // Check if the alpha is low and not just 0 to avoid potential rounding
+  // errors.
+  _closeButton.hidden = closeButtonAlpha < 0.01;
+  _titleContainerTrailingConstraint.constant =
+      -kTitleInset + (1 - visibility) * (kCloseButtonSize + kCloseButtonMargin);
+}
+
 #pragma mark - UICollectionViewCell
 
 - (void)applyLayoutAttributes:
@@ -503,6 +521,8 @@ UIImage* DefaultFavicon() {
 
 // Updates view colors.
 - (void)updateColors {
+  BOOL isSelected = self.isSelected;
+
   UIColor* backgroundColor;
   if (self.isHighlighted || self.configurationState.cellDragState !=
                                 UICellConfigurationDragStateNone) {
@@ -514,16 +534,28 @@ UIImage* DefaultFavicon() {
     backgroundColor = [UIColor colorNamed:kUpdatedTertiaryBackgroundColor];
   } else {
     backgroundColor =
-        self.isSelected ? [UIColor colorNamed:kGroupedSecondaryBackgroundColor]
-                        : [TabStripHelper backgroundColor];
+        isSelected ? [UIColor colorNamed:kGroupedSecondaryBackgroundColor]
+                   : [TabStripHelper backgroundColor];
   }
 
   if (TabStripFeaturesUtils.hasBlackBackground) {
-    if (self.isSelected) {
+    if (isSelected) {
       self.overrideUserInterfaceStyle = UIUserInterfaceStyleUnspecified;
     } else {
       self.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
     }
+  }
+
+  if (TabStripFeaturesUtils.hasHighContrastInactiveTabs) {
+    UIColor* inactiveColor = [UIColor
+        colorWithDynamicProvider:^UIColor*(UITraitCollection* traitCollection) {
+          if (traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark) {
+            return [UIColor colorNamed:kTextSecondaryColor];
+          }
+          return [UIColor colorNamed:kTextTertiaryColor];
+        }];
+    _titleLabel.textColor =
+        isSelected ? [UIColor colorNamed:kTextPrimaryColor] : inactiveColor;
   }
 
   // Needed to correctly update the `_titleGradientView` colors in incognito.
@@ -540,6 +572,10 @@ UIImage* DefaultFavicon() {
 
 // Hides the close button view if the cell is collapsed.
 - (void)updateCollapsedState {
+  if (TabStripFeaturesUtils.hasCloseButtonsVisible) {
+    return;
+  }
+
   BOOL collapsed = NO;
   if (self.frame.size.width < kCollapsedWidthThreshold) {
     // Don't hide the close button if the cell is selected.
--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_utils.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_utils.mm
@@ -15,12 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return UIColor.blackColor;
   } else if (TabStripFeaturesUtils.hasDarkerBackground) {
     return [UIColor colorNamed:kTabStripBackgroundColor];
+  } else if (TabStripFeaturesUtils.hasDarkerBackgroundV3) {
+    return [UIColor colorNamed:kTabStripV3BackgroundColor];
   }
   return [UIColor colorNamed:kGroupedPrimaryBackgroundColor];
 }
 
 + (UIColor*)newTabButtonSymbolColor {
-  if (TabStripFeaturesUtils.hasBlackBackground) {
+  if (TabStripFeaturesUtils.hasHighContrastNTB) {
+    return [UIColor colorNamed:kTextPrimaryColor];
+  } else if (TabStripFeaturesUtils.hasBlackBackground) {
     return [UIColor colorNamed:kStaticGrey600Color];
   } else if (TabStripFeaturesUtils.hasBiggerNTB) {
     return [UIColor colorNamed:kTabStripNewTabButtonColor];
