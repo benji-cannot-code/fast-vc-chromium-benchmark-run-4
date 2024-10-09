75d3b52e28e18cfa5fa40767a36b1589fe74737d
Tsuyoshi Horo <horo@chromium.org>
2024-10-09 00:16:06 +0000

    Fix Writer/Rewriter API tests, and mark them as flaky
    This CL add an English context string for tests of Writer/Rewriter API.
    This CL also marks Writer/Rewriter API tests as flaky because it will be
    broken until we get a new prompt. See crbug.com/372099809 for more
    details.
    
    Fixed: 372028332
    Bug: 372099809
    Change-Id: Ic144f6221d9a3fe141fb8486761d728267bb6244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914034
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365905}

--- third_party/blink/web_tests/wpt_internal/ai/rewriter-api-slow.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/rewriter-api-slow.any.js
@@ -1,15 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: timeout=long
 
+const kTestInputString = 'hello';
+// We need to have an English context string as Chrome's Rewriter API only
+// supports English.
+const kTestContextString = 'Hello world.';
+
 promise_test(async () => {
   const rewriter = await ai.rewriter.create();
-  const result = await rewriter.rewrite('hello');
+  const result =
+      await rewriter.rewrite(kTestInputString, {context: kTestContextString});
   assert_equals(typeof result, 'string');
 }, 'Simple AIRewriter.rewrite() call');
 
 promise_test(async () => {
   const rewriter = await ai.rewriter.create();
-  const streamingResponse = rewriter.rewriteStreaming('hello');
+  const streamingResponse = rewriter.rewriteStreaming(
+      kTestInputString, {context: kTestContextString});
   assert_equals(
       Object.prototype.toString.call(streamingResponse),
       '[object ReadableStream]');
@@ -23,8 +30,9 @@ promise_test(async () => {
 promise_test(async (t) => {
   const rewriter = await ai.rewriter.create();
   const controller = new AbortController();
-  const streamingResponse =
-      rewriter.rewriteStreaming('hello', {signal: controller.signal});
+  const streamingResponse = rewriter.rewriteStreaming(
+      kTestInputString,
+      {signal: controller.signal, context: kTestContextString});
   for await (const chunk of streamingResponse) {
   }
   controller.abort();
--- third_party/blink/web_tests/wpt_internal/ai/writer-api-slow.any.js
+++ third_party/blink/web_tests/wpt_internal/ai/writer-api-slow.any.js
@@ -1,15 +1,22 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: timeout=long
 
+const kTestInputString = 'hello';
+// We need to have an English context string as Chrome's Rewriter API only
+// supports English.
+const kTestContextString = 'Hello world.';
+
 promise_test(async () => {
   const writer = await ai.writer.create();
-  const result = await writer.write('hello');
+  const result =
+      await writer.write(kTestInputString, {context: kTestContextString});
   assert_equals(typeof result, 'string');
 }, 'Simple AIWriter.write() call');
 
 promise_test(async () => {
   const writer = await ai.writer.create();
-  const streamingResponse = writer.writeStreaming('hello');
+  const streamingResponse =
+      writer.writeStreaming(kTestInputString, {context: kTestContextString});
   assert_equals(
       Object.prototype.toString.call(streamingResponse),
       '[object ReadableStream]');
@@ -23,8 +30,10 @@ promise_test(async () => {
 promise_test(async (t) => {
   const writer = await ai.writer.create();
   const controller = new AbortController();
-  const streamingResponse =
-      writer.writeStreaming('hello', {signal: controller.signal});
+  const streamingResponse = writer.writeStreaming(kTestInputString, {
+    signal: controller.signal,
+    context: kTestContextString,
+  });
   for await (const chunk of streamingResponse) {
   }
   controller.abort();
