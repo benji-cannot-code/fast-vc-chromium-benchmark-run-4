8ce68cdc586b49560a435bbcb18a65934651f0dc
Sorin Jianu <sorin@chromium.org>
2024-10-09 07:12:04 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/keyed_service
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=blundell@chromium.org
    
    Bug: 371316188
    Change-Id: I234cb900208932adb02aedf6d470e74d655aef09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5916924
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366006}

--- components/keyed_service/core/simple_keyed_service_factory.cc
+++ components/keyed_service/core/simple_keyed_service_factory.cc
@@ -51,7 +51,7 @@ SimpleKeyedServiceFactory::SimpleKeyedServiceFactory(
     SimpleDependencyManager* manager)
     : KeyedServiceFactory(name, manager, SIMPLE) {}
 
-SimpleKeyedServiceFactory::~SimpleKeyedServiceFactory() {}
+SimpleKeyedServiceFactory::~SimpleKeyedServiceFactory() = default;
 
 KeyedService* SimpleKeyedServiceFactory::GetServiceForKey(SimpleFactoryKey* key,
                                                           bool create) {
