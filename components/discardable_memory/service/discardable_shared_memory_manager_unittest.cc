ca363d77281c16fa4772fdbd3bdfe555d5d64948
Sorin Jianu <sorin@chromium.org>
2024-10-09 18:02:58 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/discardable_memory
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=thiabaud@google.com
    
    Bug: 371316188
    Change-Id: I7d55a01b818cc5e0e26681e63ae57f6a015df080
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918177
    Reviewed-by: Thiabaud Engelbrecht <thiabaud@google.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Thiabaud Engelbrecht <thiabaud@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366314}

--- components/discardable_memory/service/discardable_shared_memory_manager_unittest.cc
+++ components/discardable_memory/service/discardable_shared_memory_manager_unittest.cc
@@ -23,7 +23,7 @@ const int kInvalidUniqueID = -1;
 
 class TestDiscardableSharedMemory : public base::DiscardableSharedMemory {
  public:
-  TestDiscardableSharedMemory() {}
+  TestDiscardableSharedMemory() = default;
 
   explicit TestDiscardableSharedMemory(base::UnsafeSharedMemoryRegion region)
       : DiscardableSharedMemory(std::move(region)) {}
@@ -284,7 +284,7 @@ class SetMemoryLimitRunner : public base::DelegateSimpleThread::Delegate {
  public:
   SetMemoryLimitRunner(DiscardableSharedMemoryManager* manager, size_t limit)
       : manager_(manager), limit_(limit) {}
-  ~SetMemoryLimitRunner() override {}
+  ~SetMemoryLimitRunner() override = default;
 
   void Run() override { manager_->SetMemoryLimit(limit_); }
 
