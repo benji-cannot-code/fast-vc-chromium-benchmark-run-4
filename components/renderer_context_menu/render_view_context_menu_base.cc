1fb605133862d94d52b16dba3be040ab6edc0c01
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-10-09 16:22:05 +0000

    Set science icon next to experimental menu items arriving from DevTools
    Bug: 368240775
    Change-Id: If6e6cf7d001a87f9db9e277aba366bdd394815e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899206
    Reviewed-by: Camille Lamy <clamy@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Auto-Submit: Kateryna Prokopenko <kprokopenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366222}

--- components/renderer_context_menu/render_view_context_menu_base.cc
+++ components/renderer_context_menu/render_view_context_menu_base.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#include "components/vector_icons/vector_icons.h"
 #include "content/public/browser/global_routing_id.h"
 #include "content/public/browser/render_frame_host.h"
 #include "content/public/browser/render_process_host.h"
@@ -105,6 +106,11 @@ void AddCustomItemsToMenu(
             RenderViewContextMenuBase::ConvertToContentCustomCommandId(
                 item->action),
             item->label);
+        if (item->is_experimental_feature) {
+          menu_model->SetMinorIcon(
+              menu_model->GetItemCount() - 1,
+              ui::ImageModel::FromVectorIcon(vector_icons::kScienceIcon));
+        }
         break;
       case blink::mojom::CustomContextMenuItemType::kCheckableOption:
         menu_model->AddCheckItem(
--- third_party/blink/public/common/context_menu_data/menu_item_info.h
+++ third_party/blink/public/common/context_menu_data/menu_item_info.h
@@ -51,6 +51,7 @@ struct MenuItemInfo {
         checked(false) {}
 
   std::u16string label;
+  bool is_experimental_feature = false;
   std::u16string tool_tip;
   Type type;
   unsigned action;
