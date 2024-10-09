473c6f307c044068c967ecaf5b411ab0e7fdcc06
Sylvain Defresne <sdefresne@chromium.org>
2024-10-09 16:47:26 +0000

    [ios] Remove disconnected scenes from ProfileState -connectedScenes
    Currently AppState -connectedScenes iterate over all UIWindowScene
    which does not include disconnected scenes. Similarly ProfileState
    should forgets about scenes when they are disconnected.
    
    To replicate this, remove the scenes when they reach the activation
    level SceneActivationLevelDisconnected in ProfileState (possible as
    ProfileState observes all connected Scenes).
    
    Add unit tests checking that the scenes are correctly removed when
    they reach the SceneActivationLevelDisconnected activation level.
    Simplify the unit tests by factoring some code and by using real
    SceneState instances instead of mocks.
    
    Bug: 333863468
    Change-Id: I5acbf839adebd04cd8e2b158420f0300a9d5312f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920794
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366253}

--- ios/chrome/app/profile/profile_state.mm
+++ ios/chrome/app/profile/profile_state.mm
@@ -218,11 +218,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)sceneState:(SceneState*)sceneState
     transitionedToActivationLevel:(SceneActivationLevel)level {
-  if (level == SceneActivationLevelForegroundActive) {
-    const ProfileInitStage initStage = self.initStage;
-    if (initStage >= ProfileInitStage::kUIReady) {
-      [_observers profileState:self sceneDidBecomeActive:sceneState];
-    }
+  switch (level) {
+    case SceneActivationLevelUnattached:
+      // Nothing to do.
+      break;
+
+    case SceneActivationLevelDisconnected:
+      [_connectedSceneStates removeObject:sceneState];
+      [sceneState removeObserver:self];
+      break;
+
+    case SceneActivationLevelBackground:
+    case SceneActivationLevelForegroundInactive:
+      // Nothing to do.
+      break;
+
+    case SceneActivationLevelForegroundActive:
+      if (self.initStage >= ProfileInitStage::kUIReady) {
+        [_observers profileState:self sceneDidBecomeActive:sceneState];
+      }
+      break;
   }
 }
 
