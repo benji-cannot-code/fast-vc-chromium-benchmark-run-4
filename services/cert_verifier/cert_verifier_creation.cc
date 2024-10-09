a3ebe3f85b487420c29619c4a360aa97decff54f
Mustafa Emre Acer <meacer@chromium.org>
2024-10-09 21:05:14 +0000

    [CT] Remove unused CT include
    Bug: 370724580
    Change-Id: I767db376ec05bcef97e5c650ad7df8e3d1c24d12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921715
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366444}

--- services/cert_verifier/cert_verifier_creation.cc
+++ services/cert_verifier/cert_verifier_creation.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/types/optional_util.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
-#include "components/certificate_transparency/chrome_ct_policy_enforcer.h"
 #include "components/network_time/time_tracker/time_tracker.h"
 #include "crypto/sha2.h"
 #include "net/base/features.h"
