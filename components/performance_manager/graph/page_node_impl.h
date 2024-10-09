e70a0c5c6c2751625ba9b63a3afa0a13475dbe17
Joe Mason <joenotcharles@google.com>
2024-10-09 21:26:36 +0000

    Track per-process loading scenario
    * Moves LoadingScenarioObserver::LoadingCounts to a NodeInlineData
      struct name LoadingScenarioCounts, so that it can be used for
      per-process counts.
    * Whenever a PageNode's loading state changes, also updates the state
      for all processes hosting frames on that page.
    * Adds a map of ProcessNode->frames in that process on each page,
      stored inline in the PageNode, so that the kCurrentProcess loading
      scenario can be updated whenever the last frame of a process is
      removed from the page.
    
    Bug: 365586676
    Change-Id: I4b67ad7125f223fb2d5ac3a3e53b48e78ee8a832
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5898855
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366454}

--- components/performance_manager/graph/page_node_impl.h
+++ components/performance_manager/graph/page_node_impl.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/performance_manager/graph/node_attached_data_storage.h"
 #include "components/performance_manager/graph/node_base.h"
 #include "components/performance_manager/public/graph/page_node.h"
+#include "components/performance_manager/scenarios/loading_scenario_data.h"
 #include "url/gurl.h"
 
 #if !BUILDFLAG(IS_ANDROID)
@@ -56,6 +57,7 @@ class PageNodeImpl
                                     SiteDataNodeData,
 #endif
                                     FrozenData,
+                                    LoadingScenarioPageFrameCounts,
                                     // Keep this last to avoid merge conflicts.
                                     NodeAttachedDataStorage> {
  public:
