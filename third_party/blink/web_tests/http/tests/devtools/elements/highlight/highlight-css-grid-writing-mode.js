a581e37d6904cb7f185fb8b0f0dfc555c937fbe9
Kent Tamura <tkent@chromium.org>
2024-10-09 03:17:27 +0000

    sideways: Pass correct writing-mode values to DevTools
    This is a preparation to fix DevTools grid-overlay.
    
    Bug: 40501131
    Change-Id: Ie8a98f79d215eef4a960fdcef8ed4ece84f51a0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912596
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365961}

--- third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-css-grid-writing-mode.js
+++ third_party/blink/web_tests/http/tests/devtools/elements/highlight/highlight-css-grid-writing-mode.js
@@ -29,6 +29,9 @@ import {ElementsTestRunner} from 'elements_test_runner';
       #sidewaysRl {
         writing-mode: sideways-rl;
       }
+      #sidewaysLr {
+        writing-mode: sideways-lr;
+      }
       </style>
 
       <p id="description">This test verifies that similarly-sized grids with different writing-modes share the same grid information but have a different writingMode value.</p>
@@ -57,6 +60,12 @@ import {ElementsTestRunner} from 'elements_test_runner';
             <div style="background: aquamarine">3</div>
             <div style="background: peachpuff">4</div>
           </div>
+          <div class="grid" id="sidewaysLr">
+            <div style="background: burlywood">1</div>
+            <div style="background: cadetblue">2</div>
+            <div style="background: aquamarine">3</div>
+            <div style="background: peachpuff">4</div>
+          </div>
       </div>
   `);
 
@@ -77,7 +86,7 @@ import {ElementsTestRunner} from 'elements_test_runner';
   TestRunner.addResult(`Node id #horizontalTb writing-mode: ${getWritingMode(horizontalTbHighlight)}`);
   TestRunner.addResult(`Grid info: ${horizontalTbInfo}`);
 
-  for (const id of ['verticalRl', 'verticalLr', 'sidewaysRl']) {
+  for (const id of ['verticalRl', 'verticalLr', 'sidewaysRl', 'sidewaysLr']) {
     const node = await ElementsTestRunner.nodeWithIdPromise(id);
     const result = await TestRunner.OverlayAgent.getHighlightObjectForTest(node.id);
     const gridInfo = getGridInfo(result);
