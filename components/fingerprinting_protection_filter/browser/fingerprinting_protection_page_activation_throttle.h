3c9377c39dea145c2c76c708b42ad221cd6133bd
Adam Abeshouse <aabeshouse@google.com>
2024-10-10 02:38:14 +0000

    Add `enable_console_logging` feature param
    Adds boolean `enable_console_logging` feature param to
    `EnableFingerprintingProtectionFilter` feature.
    This passes through to ActivationState::enable_logging, and
    in particular, for our purposes, is used to enable logging when
    blocking child navigation in ChildFrameNavigationFilteringThrottle
    (already implemented) and blocking subresource loading (to be
    implemented in later CL).
    
    Change-Id: I7fe4488fbeb68c12dc192527833f04fcc13d049e
    Bug: 369908040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904982
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Reviewed-by: Martin Verde <thesalsa@google.com>
    Commit-Queue: Adam Abeshouse <aabeshouse@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366599}

--- components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.h
+++ components/fingerprinting_protection_filter/browser/fingerprinting_protection_page_activation_throttle.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "components/fingerprinting_protection_filter/browser/fingerprinting_protection_profile_interaction_manager.h"
+#include "components/fingerprinting_protection_filter/browser/fingerprinting_protection_web_contents_helper.h"
 #include "content/public/browser/navigation_throttle.h"
 
 class PrefService;
@@ -67,6 +68,12 @@ class FingerprintingProtectionPageActivationThrottle
 
   void CheckCurrentUrl();
   virtual void NotifyResult(subresource_filter::ActivationDecision decision);
+
+  // Helper function to abstract getting the WebContentsHelper dependency.
+  // This structure is useful for testing.
+  virtual void NotifyPageActivationComputed(
+      subresource_filter::mojom::ActivationState activation_state,
+      subresource_filter::ActivationDecision activation_decision);
 
   void LogMetricsOnChecksComplete(
       subresource_filter::ActivationDecision decision,
