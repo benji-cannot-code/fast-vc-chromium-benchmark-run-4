70d04fd25f92e0c12c36d85e58cb5ba1df193845
Olga Sharonova <olka@chromium.org>
2024-10-10 08:26:17 +0000

    Parameters to fine-tune WebAudioBypassOutputBuffering base on latency hint
    Adding parameters for a more fine-grained control of a finch experiment,
    if needed.
    
    Bug: 367276023
    Change-Id: Ife3daa55971019b644e648aad663b79d19d3497e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920538
    Reviewed-by: Michael Wilson <mjwilson@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366672}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -1765,7 +1765,21 @@ BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kWebAppEnableScopeExtensions);
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kWebAppEnableUrlHandlers);
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kWebAppManifestLockScreen);
 
+// Enables synchronous AudioWorklet rendering.
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kWebAudioBypassOutputBuffering);
+// Parameters are used to control to which latency hints the feature is applied
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE_PARAM(
+    bool,
+    kWebAudioBypassOutputBufferingInteractive);
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE_PARAM(
+    bool,
+    kWebAudioBypassOutputBufferingBalanced);
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE_PARAM(
+    bool,
+    kWebAudioBypassOutputBufferingPlayback);
+BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE_PARAM(
+    bool,
+    kWebAudioBypassOutputBufferingExact);
 
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kWebFontsCacheAwareTimeoutAdaption);
 
