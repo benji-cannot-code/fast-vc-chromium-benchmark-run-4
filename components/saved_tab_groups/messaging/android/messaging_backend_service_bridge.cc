be1a6158acf9a6f62b4cc9764f67a10e1b59127c
Shakti Sahu <shaktisahu@chromium.org>
2024-10-10 04:05:59 +0000

    [Activity Log] Add Java API and JNI bridge
    This CL adds the Java API and JNI bridge for the activity log query API.
    
    Bug: 345856643
    Change-Id: Ib7fc72729cee24c316d2f388653be52dcca4fce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918617
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366619}

--- components/saved_tab_groups/messaging/android/messaging_backend_service_bridge.cc
+++ components/saved_tab_groups/messaging/android/messaging_backend_service_bridge.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/ptr_util.h"
 #include "base/notreached.h"
+#include "components/saved_tab_groups/messaging/activity_log.h"
 #include "components/saved_tab_groups/messaging/android/conversion_utils.h"
 #include "components/saved_tab_groups/messaging/message.h"
 #include "components/saved_tab_groups/public/android/tab_group_sync_conversions_bridge.h"
@@ -22,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Must come after all headers that specialize FromJniType() / ToJniType().
 #include "components/saved_tab_groups/messaging/android/jni_headers/MessagingBackendServiceBridge_jni.h"
+
+using base::android::ConvertJavaStringToUTF16;
 
 namespace tab_groups::messaging::android {
 namespace {
@@ -158,6 +161,18 @@ MessagingBackendServiceBridge::GetMessages(
   }
   auto messages = service_->GetMessages(type_opt);
   return PersistentMessagesToJava(env, messages);
+}
+
+base::android::ScopedJavaLocalRef<jobject>
+MessagingBackendServiceBridge::GetActivityLog(
+    JNIEnv* env,
+    const base::android::JavaParamRef<jobject>& j_caller,
+    jstring j_collaboration_id) {
+  ActivityLogQueryParams query_params;
+  query_params.collaboration_id = data_sharing::GroupId(
+      base::android::ConvertJavaStringToUTF8(env, j_collaboration_id));
+  auto activity_log_items = service_->GetActivityLog(query_params);
+  return ActivityLogItemsToJava(env, activity_log_items);
 }
 
 void MessagingBackendServiceBridge::RunInstantaneousMessageSuccessCallback(
