749ff80b37807ed78996c2f5aab930cba40dd5f6
Dominique Fauteux-Chapleau <domfc@chromium.org>
2024-10-10 13:48:26 +0000

    Remove unnecessary includes from enterprise interstitial pages
    Change-Id: I667cbd757409851ab5e246982f57cfa295794e73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920953
    Auto-Submit: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Reviewed-by: Haihan Chen <haihan@google.com>
    Commit-Queue: Haihan Chen <haihan@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366788}

--- chrome/browser/enterprise/connectors/interstitials/enterprise_block_page.h
+++ chrome/browser/enterprise/connectors/interstitials/enterprise_block_page.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string>
 
-#include "chrome/browser/enterprise/connectors/common.h"
 #include "components/safe_browsing/content/browser/safe_browsing_blocking_page.h"
 #include "components/security_interstitials/content/security_interstitial_page.h"
 
--- chrome/browser/enterprise/connectors/interstitials/enterprise_warn_page.cc
+++ chrome/browser/enterprise/connectors/interstitials/enterprise_warn_page.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/security_interstitials/core/common_string_util.h"
 #include "components/security_interstitials/core/urls.h"
 #include "components/strings/grit/components_strings.h"
-#include "content/public/browser/navigation_entry.h"
-#include "content/public/browser/web_contents.h"
 #include "net/base/net_errors.h"
 #include "ui/base/l10n/l10n_util.h"
 
