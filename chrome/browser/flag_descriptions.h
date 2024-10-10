1645e8dc051e1a58dbf1cee7a776b43069314a43
kamalchoudhury <kamalchoudhury@google.com>
2024-10-10 21:25:56 +0000

    Allow parallel experimentation on Open in Browser Button with dilution in embedder experiment arms and no dilution in finch arms.
    Bug: 370941631
    
    Change-Id: I937c94205b9c547dd04bcd5ce404fe65f2da9a15
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922873
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Kamal Choudhury <kamalchoudhury@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Reviewed-by: Edmund Wright <edmundw@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367125}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2508,6 +2508,12 @@ extern const char kCCTGoogleBottomBarDescription[];
 extern const char kCCTGoogleBottomBarVariantLayoutsName[];
 extern const char kCCTGoogleBottomBarVariantLayoutsDescription[];
 
+extern const char kCCTOpenInBrowserButtonIfAllowedByEmbedderName[];
+extern const char kCCTOpenInBrowserButtonIfAllowedByEmbedderDescription[];
+
+extern const char kCCTOpenInBrowserButtonIfEnabledByEmbedderName[];
+extern const char kCCTOpenInBrowserButtonIfEnabledByEmbedderDescription[];
+
 extern const char kCCTResizableForThirdPartiesName[];
 extern const char kCCTResizableForThirdPartiesDescription[];
 
