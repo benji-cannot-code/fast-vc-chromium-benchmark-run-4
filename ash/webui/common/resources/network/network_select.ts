91634a456bbc95ad49efb9ebc7dbbedb3e4a194e
xiangdong kong <xiangdongkong@google.com>
2024-10-10 20:23:14 +0000

    typescript: Convert network_select_test.js to TS
    Bug: 367735586
    Test: browser_tests --gtest_filter=NetworkComponentsNetworkSelectTestV3.All
    Change-Id: I2352da8bcf1587fe8df2c03ec43d30d563175d48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921235
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Commit-Queue: Xiangdong Kong <xiangdongkong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367064}

--- ash/webui/common/resources/network/network_select.ts
+++ ash/webui/common/resources/network/network_select.ts
@@ -119,7 +119,7 @@ export class NetworkSelectElement extends NetworkSelectElementBase {
   private globalPolicy_: GlobalPolicy|undefined;
 
   private defaultNetworkState_: OncMojo.NetworkStateProperties|undefined;
-  private scanIntervalId_: number|null;
+  private scanIntervalId_: number|undefined;
   private networkConfig_: CrosNetworkConfigInterface;
 
   constructor() {
@@ -127,7 +127,7 @@ export class NetworkSelectElement extends NetworkSelectElementBase {
 
     this.defaultNetworkState_ = undefined;
 
-    this.scanIntervalId_ = null;
+    this.scanIntervalId_ = undefined;
 
     this.networkConfig_ =
         MojoInterfaceProviderImpl.getInstance().getMojoServiceRemote();
@@ -144,6 +144,10 @@ export class NetworkSelectElement extends NetworkSelectElementBase {
     super.disconnectedCallback();
 
     this.clearScheduledScans_();
+  }
+
+  isScanning(): boolean {
+    return this.scanIntervalId_ !== null;
   }
 
   /**  Returns network list object for testing. */
@@ -259,9 +263,9 @@ export class NetworkSelectElement extends NetworkSelectElementBase {
    * Clears any scheduled Wi-FI scans; no-op if there were no scans scheduled.
    */
   private clearScheduledScans_() {
-    if (this.scanIntervalId_ !== null) {
+    if (this.isScanning()) {
       window.clearInterval(this.scanIntervalId_);
-      this.scanIntervalId_ = null;
+      this.scanIntervalId_ = undefined;
     }
   }
 
