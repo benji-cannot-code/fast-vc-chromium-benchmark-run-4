82cf005de1ee21cc1c6c6d30bf519ad808baa395
Verina Armanyous <averina@google.com>
2024-10-10 23:19:21 +0000

    [IBAN][Clank] Trigger save IBAN snackbar & edit bottomsheet hiding logic
    In crrev.com/c/5712840, I added a confirmation snackbar for save IBAN
    bottom sheet. This CL triggers the snackbar if the IBAN upload
    succeeds. This CL also updates the hiding logic for the bottom
    sheet,
    
    Mock: https://screenshot.googleplex.com/8WDndiWeW6NZpuR
    Implementation: https://screenshot.googleplex.com/4MoVvUNaZhd2KTe.png
    
    Bug: 339869144
    Change-Id: If95bba1ac3ef0a88662ac3d732328e39e18fafa9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5873381
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Commit-Queue: Verina Armanyous <averina@google.com>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367206}

--- chrome/android/junit/src/org/chromium/chrome/browser/autofill/iban/AutofillSaveIbanBottomSheetCoordinatorTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/autofill/iban/AutofillSaveIbanBottomSheetCoordinatorTest.java
@@ -89,7 +89,7 @@ public final class AutofillSaveIbanBottomSheetCoordinatorTest {
     @Test
     public void testDestroy() {
         mCoordinator.requestShowContent();
-        mCoordinator.destroy();
+        mCoordinator.destroy(BottomSheetController.StateChangeReason.NONE);
 
         verify(mBottomSheetController)
                 .hideContent(
