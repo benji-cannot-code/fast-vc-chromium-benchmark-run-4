6db19ae54c6a75db98a5a1e35d67b051f9aa0846
Tommy Nyquist <nyquist@chromium.org>
2024-10-10 22:01:14 +0000

    [messaging] Make attribution data easier for consumption.
    This CL splits out the tab and tab group metadata into its own structs
    to simplify consumption.
    
    Bug: 345856704
    Change-Id: I9510f732710b4733c7f2db528b7ef7ee40c56b31
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914932
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367155}

---
+++ components/saved_tab_groups/messaging/android/java/src/org/chromium/components/tab_group_sync/messaging/TabMessageMetadata.java
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.tab_group_sync.messaging;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Metadata about a tab group within {@link MessageAttribution}. This is a shim layer for the native
+ * representation of the object. See //components/saved_tab_groups/messaging/message.h for specific
+ * details.
+ */
+public class TabMessageMetadata {
+    public int localTabId = -1;
+    @Nullable public String syncTabId;
+    @Nullable public String lastKnownUrl;
+    @Nullable public String lastKnownTitle;
+}
