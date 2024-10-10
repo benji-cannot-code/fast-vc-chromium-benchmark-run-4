50af9efc4718beada9a2abd5c56da9b3aa831faa
Tao Wu <wutao@chromium.org>
2024-10-10 20:13:56 +0000

    growth: Add device board targeting
    Bug: 372556111
    Test: added unit tests
    Change-Id: I7246fbff8c5f602863183b5447dc7a408c381506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922740
    Reviewed-by: Li Lin <llin@chromium.org>
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367050}

--- chromeos/ash/components/growth/campaigns_matcher.h
+++ chromeos/ash/components/growth/campaigns_matcher.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_ASH_COMPONENTS_GROWTH_CAMPAIGNS_MATCHER_H_
 #define CHROMEOS_ASH_COMPONENTS_GROWTH_CAMPAIGNS_MATCHER_H_
 
+#include <optional>
 #include <string>
 
 #include "base/strings/cstring_view.h"
@@ -55,6 +56,7 @@ class CampaignsMatcher {
   const Campaign* GetCampaignBySlot(Slot slot) const;
 
   void SetMantaCapabilityForTesting(signin::Tribool value);
+  void SetBoardForTesting(std::optional<std::string> board);
 
  private:
   bool IsCampaignMatched(const Campaign* campaign, bool is_prematch) const;
@@ -79,6 +81,7 @@ class CampaignsMatcher {
   bool MatchRuntimeTargeting(const RuntimeTargeting& targeting,
                              int campaign_id,
                              std::optional<int> group_id) const;
+  bool MatchBoard(const StringListTargeting* board_targeting) const;
   bool MatchDeviceAge(
       const std::unique_ptr<NumberRangeTargeting>& device_age_in_hours) const;
   bool MatchEvents(std::unique_ptr<EventsTargeting> config,
@@ -109,7 +112,8 @@ class CampaignsMatcher {
   base::Time oobe_compelete_time_;
   bool is_user_owner_ = false;
   Trigger trigger_{TriggerType::kUnSpecified};
-  std::optional<signin::Tribool> manta_capability_for_testing_;
+  std::optional<signin::Tribool> manta_capability_for_testing_ = std::nullopt;
+  std::optional<std::string> board_for_testing_ = std::nullopt;
 };
 
 }  // namespace growth
