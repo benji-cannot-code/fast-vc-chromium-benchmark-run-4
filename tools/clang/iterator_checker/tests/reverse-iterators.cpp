ec36592013e61114fd38285f67af92a130f7d4d8
Nikhil Meena <iakhilmeena@gmail.com>
2024-10-10 15:47:50 +0000

    Added new tests for iterator checker
    Add new test for the GSOC 2024 - iterator checker:
    https://docs.google.com/document/d/1_kNdfe48YZhZol6cfN2sguSE7CqpVUwF_3VRXiKB2xo/edit#bookmark=id.ifdb6avhmtxx
    
    Added test for reverse iterators, since currently the tool doesn't takes those reverse iterators into account.
    Also added tests for reverse iterator wrapped via some constructs like Chromium's `base::ReversedAdapter` iterator.
    
    R=arthursonzogni@chromium.org, paulsemel@chromium.org
    
    Bug: 328661225, 329133423
    Change-Id: I763cd530a323810843c28f51f63d8c94f634471f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5360133
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Auto-Submit: Nikhil Meena <iakhilmeena@gmail.com>
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366865}

---
+++ tools/clang/iterator_checker/tests/reverse-iterators.cpp
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include <vector>
+
+void ReverseIteratorInvalid(std::vector<int>& v) {
+  auto it = std::rbegin(v);
+  // TODO(329133423): Support reverse iterators.
+  *it = 10;  // Iterator potentially invalid.
+}
+
+void ReverseIteratorValid(std::vector<int>& v) {
+  auto it = std::rbegin(v);
+  if (it == std::rend(v)) {
+    return;
+  }
+  *it = 10;
+}
