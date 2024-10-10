0eec9d93d0cf0ace8efddbdff0fe34c151f8f168
Siyu <siyua@chromium.org>
2024-10-10 20:18:01 +0000

    [Multiple Requests] Remove billable service number from IBAN logic.
    Clean up only. No-op change. iban_access_manager and iban_save_manager
    set billable service number in their logic and it is unnecessary.
    Move this to payments_network_interface.
    
    Bug: 362785288
    Change-Id: I7a694cd61989e9b642c8c1bd6201b0aeb9737172
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910427
    Reviewed-by: Olivia Saul <jsaul@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367056}

--- components/autofill/core/browser/payments/payments_request_details.h
+++ components/autofill/core/browser/payments/payments_request_details.h
@@ -140,7 +140,6 @@ struct UnmaskIbanRequestDetails {
   UnmaskIbanRequestDetails(const UnmaskIbanRequestDetails& other);
   ~UnmaskIbanRequestDetails();
 
-  int billable_service_number = 0;
   int64_t billing_customer_number = 0;
   int64_t instrument_id;
 };
@@ -326,7 +325,6 @@ struct UploadIbanRequestDetails {
   ~UploadIbanRequestDetails();
 
   std::string app_locale;
-  int billable_service_number = 0;
   int64_t billing_customer_number = 0;
   std::u16string context_token;
   std::u16string value;
