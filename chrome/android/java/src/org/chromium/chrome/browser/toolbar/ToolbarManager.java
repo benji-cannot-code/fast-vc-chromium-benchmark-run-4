a0e0a71ee81a7303d321d411927929ee4af1a54e
Patrick Noland <pnoland@google.com>
2024-10-10 21:23:55 +0000

    [Anchor] Set ControlsPosition to BOTTOM when pref is enabled
    This does not yet move the toolbar itself, but does allocate space for
    it. Controller logic is implemented that checks pref state, NTP state,
    and omnibox focus.
    
    Bug: 369134013, 369133658
    Change-Id: Ica3bce4d4698c534a96698f75f8b30bee34ba0c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924413
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Commit-Queue: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367124}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -33,6 +33,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.chromium.base.Callback;
 import org.chromium.base.CallbackController;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.JavaExceptionReporter;
 import org.chromium.base.TraceEvent;
 import org.chromium.base.ValueChangedCallback;
@@ -219,6 +220,8 @@ public class ToolbarManager
     private final ObservableSupplierImpl<Boolean> mHomepageEnabledSupplier =
             new ObservableSupplierImpl<>();
     private final ObservableSupplier<Boolean> mOmniboxFocusStateSupplier;
+    private final ObservableSupplierImpl<Boolean> mIsNtpShowingSupplier =
+            new ObservableSupplierImpl<>();
     private final ConstraintsProxy mConstraintsProxy = new ConstraintsProxy();
     private ObservableSupplierImpl<BottomControlsCoordinator> mBottomControlsCoordinatorSupplier =
             new ObservableSupplierImpl<>();
@@ -328,6 +331,7 @@ public class ToolbarManager
     private boolean mBackGestureInProgress;
     private boolean mStartNavDuringOngoingGesture;
     private WindowAndroid.ProgressBarConfig.Provider mProgressBarConfigProvider;
+    private ToolbarPositionController mToolbarPositionController;
 
     private static class TabObscuringCallback implements Callback<Boolean> {
         private final TabObscuringHandler mTabObscuringHandler;
@@ -1314,8 +1318,25 @@ public class ToolbarManager
                     }
                 };
         mWindowAndroid.setProgressBarConfigProvider(mProgressBarConfigProvider);
+        initializeToolbarPositionController();
 
         TraceEvent.end("ToolbarManager.ToolbarManager");
+    }
+
+    private void initializeToolbarPositionController() {
+        if (ToolbarPositionController.isToolbarPositionCustomizationEnabled(
+                mActivity, mIsCustomTab)) {
+            mIsNtpShowingSupplier.set(getNewTabPageForCurrentTab() != null);
+            mToolbarPositionController =
+                    new ToolbarPositionController(
+                            mBrowserControlsSizer,
+                            ContextUtils.getAppSharedPreferences(),
+                            mIsNtpShowingSupplier,
+                            mOmniboxFocusStateSupplier,
+                            mActivity
+                                    .getResources()
+                                    .getDimensionPixelSize(R.dimen.toolbar_height_no_shadow));
+        }
     }
 
     // TODO(b/315204103): add tests
@@ -2413,6 +2434,9 @@ public class ToolbarManager
             ntp.setOmniboxStub(mLocationBar.getOmniboxStub());
             mLocationBarModel.notifyNtpStartedLoading();
             maybeShowPriceDropIPH();
+            mIsNtpShowingSupplier.set(true);
+        } else {
+            mIsNtpShowingSupplier.set(false);
         }
     }
 
