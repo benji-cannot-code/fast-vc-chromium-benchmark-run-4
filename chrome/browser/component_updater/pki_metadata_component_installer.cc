d23901b3dfe4306f88d94e89f303312dbba4f426
Dominic Battré <battre@chromium.org>
2024-10-10 11:17:20 +0000

    Revert "[CT] Implement at least one RFC6962 log policy"
    This reverts commit d3399a9bf0b7dd723694e66f789d88d761e421ae.
    
    Reason for revert: Seems to break CSS loading in variations_smoke_tests.
    
    Original change's description:
    > [CT] Implement at least one RFC6962 log policy
    >
    > This CL parses the log type coming from the PKI metadata proto.
    > It then uses it to check that a valid SCT comes from at least one
    > RFC6962 log.
    >
    > This is the first step in Chrome's support for Static CT API support.
    >
    > Bug: 370724580
    > Change-Id: I1db808a5217da7404b2b7c6aa2523e8136f07b3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906378
    > Reviewed-by: Chris Thompson <cthomp@chromium.org>
    > Reviewed-by: mmenke <mmenke@chromium.org>
    > Commit-Queue: Mustafa Emre Acer <meacer@chromium.org>
    > Reviewed-by: Matt Mueller <mattm@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1366473}
    
    Bug: 370724580, 372622131
    Change-Id: I2eca2627ba98eea2522777521eaae7984e0f8de7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920505
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Owners-Override: Wolfgang Billenstein <bwolfgang@google.com>
    Auto-Submit: Dominic Battré <battre@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1366723}

--- chrome/browser/component_updater/pki_metadata_component_installer.cc
+++ chrome/browser/component_updater/pki_metadata_component_installer.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
-#include "base/notreached.h"
 #include "base/ranges/algorithm.h"
 #include "base/sequence_checker.h"
 #include "base/task/thread_pool.h"
@@ -106,22 +105,6 @@ std::string LoadBinaryProtoFromDisk(const base::FilePath& pb_path) {
     result.clear();
   }
   return result;
-}
-
-// Ideally we'd use EnumTraits for this method, but the conversion is only done
-// once here so it's not worth it.
-network::mojom::CTLogInfo::LogType ProtoLogTypeToLogType(
-    ::chrome_browser_certificate_transparency::CTLog_LogType log_type) {
-  switch (log_type) {
-    case ::chrome_browser_certificate_transparency::CTLog::LOG_TYPE_UNSPECIFIED:
-      return network::mojom::CTLogInfo::LogType::kUnspecified;
-    case ::chrome_browser_certificate_transparency::CTLog::RFC6962:
-      return network::mojom::CTLogInfo::LogType::kRFC6962;
-    case ::chrome_browser_certificate_transparency::CTLog::STATIC_CT_API:
-      return network::mojom::CTLogInfo::LogType::kStaticCTAPI;
-    default:
-      NOTREACHED();
-  }
 }
 
 }  // namespace
@@ -354,7 +337,6 @@ void PKIMetadataComponentInstallerService::UpdateNetworkServiceCTListOnUI(
     }
 
     log_ptr->mmd = base::Seconds(log.mmd_secs());
-    log_ptr->log_type = ProtoLogTypeToLogType(log.log_type());
     log_list_mojo_clone_network_service.push_back(log_ptr.Clone());
     log_list_mojo.push_back(std::move(log_ptr));
   }
