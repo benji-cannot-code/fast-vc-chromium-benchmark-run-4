5e3a1403cd378f732093ec988b4ade4919e4d22e
Daniel Rubery <drubery@chromium.org>
2024-10-10 19:49:27 +0000

    Remove number_of_open_profiles
    Computing this value runs into DCHECK failures. Given that it's fairly
    redundant with number_of_profiles and number_of_loaded_profiles, simply
    delete the field.
    
    Server-side proto change is http://cl/681559419
    
    Fixed: 369101443
    Change-Id: I34d37816173a017cdc1418839d2c59a56d2a3478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922555
    Auto-Submit: Daniel Rubery <drubery@chromium.org>
    Reviewed-by: thefrog <thefrog@chromium.org>
    Reviewed-by: Joemer Ramos <joemerramos@chromium.org>
    Commit-Queue: Joemer Ramos <joemerramos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367008}

--- ios/chrome/browser/safe_browsing/model/user_population_helper.mm
+++ ios/chrome/browser/safe_browsing/model/user_population_helper.mm
@@ -32,6 +32,5 @@ safe_browsing::ChromeUserPopulation GetUserPopulationForProfile(
       /*is_under_advanced_protection=*/false,
       GetApplicationContext()->GetBrowserPolicyConnector(),
       /*num_profiles=*/std::nullopt,
-      /*num_loaded_profiles=*/std::nullopt,
-      /*num_open_profiles=*/std::nullopt);
+      /*num_loaded_profiles=*/std::nullopt);
 }
