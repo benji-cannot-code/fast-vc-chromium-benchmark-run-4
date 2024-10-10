f7a18c5cfd2f45d8dbe9d822a58d2d3c6c8a475a
Nihar Majmudar <niharm@google.com>
2024-10-10 23:03:43 +0000

    [lensoverlay] Create CSB ghost loader component.
    Only creating the element with temporary strings for now while UX
    finalizes strings and some other specs. This CL doesn't make the loader
    show at all. This ghost loader is added to the side panel and the lens
    overlay.
    
    Follow up CL will add logic for showing and hiding the loader.
    
    Screencast overlay (enabled showing it for demo purposes): https://screencast.googleplex.com/cast/NTIwNjAzMDc2OTE5Mjk2MHxkOTM3ZDE4NS1lNQ
    
    Change-Id: I64daebc02a85c9b1b290e8b8cde0fb0ce3cda00f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905544
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367200}

--- chrome/browser/resources/lens/overlay/lens_overlay_app.ts
+++ chrome/browser/resources/lens/overlay/lens_overlay_app.ts
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import './cursor_tooltip.js';
 import './initial_gradient.js';
 import './selection_overlay.js';
+import './searchbox_ghost_loader.js';
 import './translate_button.js';
 import '/lens/shared/searchbox_shared_style.css.js';
 import '//resources/cr_elements/cr_icon_button/cr_icon_button.js';
