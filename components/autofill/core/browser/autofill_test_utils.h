5b5876cb3fc1ca7030023f86155d5feb83099b8f
Alexander Tekle <alexandertekle@google.com>
2024-10-10 20:16:37 +0000

    [BNPL] Add the BNPL data model
    Implementation for the BNPL issuer data model.
    
    Bug: 363320292, 356443046
    Change-Id: Ice05432c6ef69d455a1f6e6bfe2b442ac702868c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5876924
    Reviewed-by: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alexander Tekle <alexandertekle@google.com>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367053}

--- components/autofill/core/browser/autofill_test_utils.h
+++ components/autofill/core/browser/autofill_test_utils.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/data_model/autofill_offer_data.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/data_model/autofill_wallet_usage_data.h"
+#include "components/autofill/core/browser/data_model/bnpl_issuer.h"
 #include "components/autofill/core/browser/data_model/credit_card.h"
 #include "components/autofill/core/browser/data_model/credit_card_benefit.h"
 #include "components/autofill/core/browser/data_model/credit_card_cloud_token_data.h"
@@ -399,6 +400,9 @@ sync_pb::PaymentInstrument CreatePaymentInstrumentWithIban(
 // Returns a payment instrument with an eWallet account filled with fake data.
 sync_pb::PaymentInstrument CreatePaymentInstrumentWithEwalletAccount(
     int64_t instrument_id);
+
+// Returns a BNPL issuer with fake data.
+BnplIssuer GetTestBnplIssuer();
 
 }  // namespace test
 }  // namespace autofill
