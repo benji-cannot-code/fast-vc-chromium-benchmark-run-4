8847224429dfa7507a6b20505a896b1898c4b195
Luke Klimek <zgroza@chromium.org>
2024-10-10 23:04:57 +0000

    SmartCard: Add content settings page - improve tests
    Improved test coverage of both frontend and backend of the Smart Card
      Readers content settings page.
    
    Bug: 40275778
    Change-Id: I029f30b2ff5a7fc9596dc92f5441e656c370824a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918625
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Luke Klimek <zgroza@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367202}

--- chrome/browser/smart_card/smart_card_permission_context.h
+++ chrome/browser/smart_card/smart_card_permission_context.h
@@ -22,7 +22,10 @@ class Profile;
 
 namespace content {
 class RenderFrameHost;
-}
+}  // namespace content
+namespace settings {
+class SmartCardReaderPermissionsSiteSettingsHandlerTest;
+}  // namespace settings
 
 class SmartCardPermissionContext
     : public permissions::ObjectPermissionContextBase {
@@ -70,6 +73,7 @@ class SmartCardPermissionContext
 
  private:
   friend class SmartCardPermissionContextTest;
+  friend class settings::SmartCardReaderPermissionsSiteSettingsHandlerTest;
 
   class OneTimeObserver;
   class PowerSuspendObserver;
