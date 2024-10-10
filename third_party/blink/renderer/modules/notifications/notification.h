80dce6d0ebe373260a99860befb82e893ee73812
Dave Tapuska <dtapuska@chromium.org>
2024-10-10 02:11:23 +0000

    Convert notifications code to use bindings enumerations
    We had needless conversions from strings -> enums and back.
    
    Bug: 366411876, 370425456
    Change-Id: I1777e4666d50a6dc45292dd54550056a91d4bf16
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922533
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366587}

--- third_party/blink/renderer/modules/notifications/notification.h
+++ third_party/blink/renderer/modules/notifications/notification.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/bindings/core/v8/script_promise.h"
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
 #include "third_party/blink/renderer/bindings/core/v8/serialization/serialized_script_value.h"
+#include "third_party/blink/renderer/bindings/modules/v8/v8_notification_permission.h"
 #include "third_party/blink/renderer/core/dom/dom_time_stamp.h"
 #include "third_party/blink/renderer/core/execution_context/execution_context_lifecycle_observer.h"
 #include "third_party/blink/renderer/modules/event_target_modules.h"
@@ -55,7 +56,6 @@ class NotificationOptions;
 class NotificationResourcesLoader;
 class ScriptState;
 class TimestampTrigger;
-class V8NotificationPermission;
 class V8NotificationPermissionCallback;
 
 class MODULES_EXPORT Notification final
@@ -121,8 +121,9 @@ class MODULES_EXPORT Notification final
   TimestampTrigger* showTrigger() const { return show_trigger_.Get(); }
   String scenario() const;
 
-  static String PermissionString(mojom::blink::PermissionStatus permission);
-  static String permission(ExecutionContext* context);
+  static V8NotificationPermission::Enum PermissionToV8Enum(
+      mojom::blink::PermissionStatus permission);
+  static V8NotificationPermission permission(ExecutionContext* context);
   static ScriptPromise<V8NotificationPermission> requestPermission(
       ScriptState* script_state,
       V8NotificationPermissionCallback* deprecated_callback = nullptr);
