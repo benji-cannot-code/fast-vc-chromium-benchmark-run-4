66f4634c80a5fa95f41d1b487ef476d18bf2957e
Ameur Hosni <ameurhosni@google.com>
2024-10-10 12:58:19 +0000

    [IOS][LO] Disable overflow lens overlay entrypoint on landscape
    Bug: 370058971
    Change-Id: I76d84dd9423cd94b31b47aaa1b53ac41791af365
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907894
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366759}

--- ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
+++ ios/chrome/browser/ui/popup_menu/overflow_menu/overflow_menu_mediator.mm
@@ -1338,7 +1338,8 @@ OverflowMenuFooter* CreateOverflowMenuManagedFooter(
 
   if (IsLensOverlayAvailable()) {
     self.lensOverlayAction.enabled =
-        search_engines::SupportsSearchImageWithLens(self.templateURLService);
+        search_engines::SupportsSearchImageWithLens(self.templateURLService) &&
+        !IsCompactHeight(self.baseViewController.traitCollection);
   }
 }
 
