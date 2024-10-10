97626d3e5d26b1d932e498e2de466bf8446aa2b5
Dave Tapuska <dtapuska@chromium.org>
2024-10-10 22:15:12 +0000

    Convert canvas code to use bindings enumerations
    We had needless conversions from strings -> enums and back.
    
    Bug: 366411876, 370425456
    Change-Id: I874f86d7fb4aa23524382632484f3f7196805be4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921026
    Reviewed-by: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367168}

--- third_party/blink/renderer/core/html/canvas/image_data.h
+++ third_party/blink/renderer/core/html/canvas/image_data.h
@@ -48,6 +48,8 @@ namespace blink {
 
 class ExceptionState;
 class ImageBitmapOptions;
+class V8ImageDataStorageFormat;
+class V8PredefinedColorSpace;
 
 class CORE_EXPORT ImageData final : public ScriptWrappable,
                                     public ImageBitmapSource {
@@ -180,8 +182,8 @@ class CORE_EXPORT ImageData final : public ScriptWrappable,
   gfx::Size Size() const { return size_; }
   int width() const { return size_.width(); }
   int height() const { return size_.height(); }
-  String colorSpace() const;
-  String storageFormat() const;
+  V8PredefinedColorSpace colorSpace() const;
+  V8ImageDataStorageFormat storageFormat() const;
 
   // TODO(https://crbug.com/1198606): Remove this.
   ImageDataSettings* getSettings() const;
