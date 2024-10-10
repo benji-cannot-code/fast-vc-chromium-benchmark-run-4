ff7090d60413f09cdbcafb9ff52d239b49e62079
Arthur Milchior <arthurmilchior@chromium.org>
2024-10-10 13:12:03 +0000

    [iOS][Account cell]Solve layout ambiguity
    As the error icon is always the same tint, the tint is hard coded in
    the cell.
    
    Instead of having a property for the error icon, a setting for the
    image is given. This allows to hide or display the error.
    
    I checked both the account menu and the settings, and there was no
    ambiguity.
    
    Fixed: 371985756
    Change-Id: I29c5c5a02f68bb08e45be65bfc6831ba66dc7a04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913515
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366768}

--- ios/chrome/browser/ui/authentication/cells/table_view_account_item.mm
+++ ios/chrome/browser/ui/authentication/cells/table_view_account_item.mm
@@ -47,11 +47,10 @@ constexpr CGFloat KErrorIconImageSize = 22.;
   cell.textLabel.text = self.text;
   cell.detailTextLabel.text = self.detailText;
   if (self.shouldDisplayError) {
-    cell.errorIcon.image =
-        DefaultSymbolWithPointSize(kErrorCircleFillSymbol, KErrorIconImageSize);
-    cell.errorIcon.tintColor = [UIColor colorNamed:kRed500Color];
+    [cell setErrorImage:DefaultSymbolWithPointSize(kErrorCircleFillSymbol,
+                                                   KErrorIconImageSize)];
   } else {
-    cell.errorIcon.image = nil;
+    [cell setErrorImage:nil];
     cell.detailTextLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
   }
 
@@ -71,6 +70,13 @@ constexpr CGFloat KErrorIconImageSize = 22.;
         [accessoryImage.tintColor colorWithAlphaComponent:0.5];
   }
 }
+
+@end
+
+@interface TableViewAccountCell ()
+
+// Error icon that will be displayed on the left side of the cell.
+@property(nonatomic, readonly, strong) UIImageView* errorIcon;
 
 @end
 
@@ -106,6 +112,7 @@ constexpr CGFloat KErrorIconImageSize = 22.;
   [contentView addSubview:_imageView];
 
   _errorIcon = [[UIImageView alloc] init];
+  _errorIcon.tintColor = [UIColor colorNamed:kRed500Color];
   _errorIcon.translatesAutoresizingMaskIntoConstraints = NO;
   [_errorIcon setContentHuggingPriority:UILayoutPriorityRequired
                                 forAxis:UILayoutConstraintAxisHorizontal];
@@ -128,6 +135,7 @@ constexpr CGFloat KErrorIconImageSize = 22.;
   _detailTextLabel.adjustsFontForContentSizeCategory = YES;
   _detailTextLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
   [contentView addSubview:_detailTextLabel];
+  [self setErrorImage:nil];
 }
 
 // Set constraints on subviews.
@@ -146,6 +154,11 @@ constexpr CGFloat KErrorIconImageSize = 22.;
                        constant:kTableViewHorizontalSpacing],
     [_detailTextLabel.leadingAnchor
         constraintEqualToAnchor:_textLabel.leadingAnchor],
+    // Leading and width on the vertical centering view does not matter. It’s
+    // here to remove ambiguity.
+    [verticalCenteringView.widthAnchor constraintEqualToConstant:0],
+    [verticalCenteringView.leadingAnchor
+        constraintEqualToAnchor:_imageView.trailingAnchor],
 
     // Fix image widths. The account images have been resized to fit this size.
     // Update the resize if this changes.
@@ -207,6 +220,15 @@ constexpr CGFloat KErrorIconImageSize = 22.;
                                       forAxis:UILayoutConstraintAxisHorizontal];
 }
 
+- (void)setErrorImage:(UIImage*)errorImage {
+  _errorIcon.image = errorImage;
+  if (errorImage) {
+    _errorIcon.hidden = NO;
+  } else {
+    _errorIcon.hidden = YES;
+  }
+}
+
 #pragma mark - UITableViewCell
 
 - (void)prepareForReuse {
@@ -216,7 +238,7 @@ constexpr CGFloat KErrorIconImageSize = 22.;
   self.detailTextLabel.text = nil;
   self.textLabel.textColor = [UIColor colorNamed:kTextPrimaryColor];
   self.detailTextLabel.textColor = [UIColor colorNamed:kTextSecondaryColor];
-  self.errorIcon.image = nil;
+  [self setErrorImage:nil];
   self.userInteractionEnabled = YES;
   self.contentView.alpha = 1;
   UIImageView* accessoryImage =
