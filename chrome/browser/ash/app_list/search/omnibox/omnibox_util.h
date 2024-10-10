23deccfab70c2932fb0d9979a36b3a7f917c8bf0
Di Wu <diwux@google.com>
2024-10-10 03:50:30 +0000

    [Ash] Move PageTransitionToUiPageTransition() closer to call site
    This CL is one of four CLs that aim at ultimately dismantling
    //chrome//browser/chromeos/launcher_search/ directory. The code inside
    would be either deleted, if it is solely used by Lacros, or moved out of
    the current crosapi namespace to a proper namespace and directory, if it
    is still depended upon by Ash code.
    
    In this CL, PageTransitionToUiPageTransition() is moved closer to its
    three call sites, all under //c/b/a/a/s/omnibox, into
    omnibox_util.{h,cc}. Unfortunately, this CL won't break its connection
    with crosapi, as PageTransitionToUiPageTransition() depends on
    crosapi::mojom::SearchResult, which is deeply coupled with the omnibox
    code at this time and is too complex to tackle for the purpose of this
    series of CLs.
    
    Bug: 371119767
    Test: CQ
    Change-Id: I4737919a723ff7017cccf67c982bd60ebdf7b340
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908951
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Di Wu <diwux@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366616}

--- chrome/browser/ash/app_list/search/omnibox/omnibox_util.h
+++ chrome/browser/ash/app_list/search/omnibox/omnibox_util.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_list/app_list_types.h"
 #include "chromeos/crosapi/mojom/launcher_search.mojom.h"
 #include "net/traffic_annotation/network_traffic_annotation.h"
+#include "ui/base/page_transition_types.h"
 
 class GURL;
 
@@ -68,6 +69,10 @@ bool IsDriveUrl(const GURL& url);
 void RemoveDuplicateResults(
     std::vector<std::unique_ptr<OmniboxResult>>& results);
 
+// Returns the UI page transition that corresponds to the given crosapi page
+// transition.
+ui::PageTransition PageTransitionToUiPageTransition(
+    crosapi::mojom::SearchResult::PageTransition transition);
 }  // namespace app_list
 
 #endif  // CHROME_BROWSER_ASH_APP_LIST_SEARCH_OMNIBOX_OMNIBOX_UTIL_H_
