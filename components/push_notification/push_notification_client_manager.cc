80c70596ca0437b9f49e2c5541504e9faa6d4d86
Sorin Jianu <sorin@chromium.org>
2024-10-10 09:51:56 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/push_notification
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    This CL was uploaded by git cl split.
    
    R=julietlevesque@google.com
    
    Bug: 371316188
    Change-Id: I093673d38e915a0f7a6fc2988387eb5e666622b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918657
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366707}

--- components/push_notification/push_notification_client_manager.cc
+++ components/push_notification/push_notification_client_manager.cc
@@ -15,7 +15,7 @@ const char kClientIdKey[] = "type_id";
 
 namespace push_notification {
 
-PushNotificationClientManager::PushNotificationClientManager() {}
+PushNotificationClientManager::PushNotificationClientManager() = default;
 
 PushNotificationClientManager::~PushNotificationClientManager() = default;
 
