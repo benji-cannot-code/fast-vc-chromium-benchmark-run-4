7dae5360ea8dc16ac546136e89eed7404534cd76
Evan Liu <evliu@google.com>
2024-10-10 22:48:31 +0000

    Remove pin button from the caption bubble
    This CL removes the pin button from the caption bubble and prevents it
    from disappearing due to inactivity.
    
    Bug: 371233323
    Change-Id: I47554b3c7308ffda432965fe70795d5f94f0a6d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907911
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Evan Liu <evliu@google.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367190}

--- components/live_caption/live_caption_controller.cc
+++ components/live_caption/live_caption_controller.cc
@@ -97,9 +97,6 @@ void LiveCaptionController::RegisterProfilePrefs(
       prefs::kLiveCaptionBubbleExpanded, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
-      prefs::kLiveCaptionBubblePinned, false,
-      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
-  registry->RegisterBooleanPref(
       prefs::kLiveCaptionEnabled, false,
       user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
   registry->RegisterBooleanPref(
