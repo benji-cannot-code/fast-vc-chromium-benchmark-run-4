10ca7ecc35a23b4be9b733f7baa5901e73957e3c
Emily Shack <emshack@chromium.org>
2024-10-10 20:05:03 +0000

    Declutter: Add empty state
    Screenshot: https://screenshot.googleplex.com/8RxaUH6CLvsYLoi
    Bug: 370805463
    Change-Id: Ie0f2b809177f78020aff28cdca11ed561a883f0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921679
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Shibalik Mohapatra <shibalik@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367035}

--- chrome/browser/resources/tab_search/declutter/declutter_page.ts
+++ chrome/browser/resources/tab_search/declutter/declutter_page.ts
@@ -26,12 +26,6 @@ function getEventTargetIndex(e: Event): number {
   return Number((e.currentTarget as HTMLElement).dataset['index']);
 }
 
-export interface DeclutterPageElement {
-  $: {
-    scrollable: HTMLElement,
-  };
-}
-
 export class DeclutterPageElement extends CrLitElement {
   static get is() {
     return 'declutter-page';
@@ -100,7 +94,10 @@ export class DeclutterPageElement extends CrLitElement {
   }
 
   override firstUpdated() {
-    this.$.scrollable.addEventListener('scroll', this.updateScroll_.bind(this));
+    const scrollable = this.shadowRoot!.querySelector('#scrollable');
+    if (scrollable) {
+      scrollable.addEventListener('scroll', this.updateScroll_.bind(this));
+    }
   }
 
   logCtrValue(event: DeclutterCTREvent) {
@@ -111,14 +108,16 @@ export class DeclutterPageElement extends CrLitElement {
 
   private async updateScroll_() {
     await this.updateComplete;
-    const scrollable = this.$.scrollable;
-    scrollable.classList.toggle(
-        'can-scroll', scrollable.clientHeight < scrollable.scrollHeight);
-    scrollable.classList.toggle('is-scrolled', scrollable.scrollTop > 0);
-    scrollable.classList.toggle(
-        'scrolled-to-bottom',
-        scrollable.scrollTop + MAX_SCROLLABLE_HEIGHT >=
-            scrollable.scrollHeight);
+    const scrollable = this.shadowRoot!.querySelector('#scrollable');
+    if (scrollable) {
+      scrollable.classList.toggle(
+          'can-scroll', scrollable.clientHeight < scrollable.scrollHeight);
+      scrollable.classList.toggle('is-scrolled', scrollable.scrollTop > 0);
+      scrollable.classList.toggle(
+          'scrolled-to-bottom',
+          scrollable.scrollTop + MAX_SCROLLABLE_HEIGHT >=
+              scrollable.scrollHeight);
+    }
   }
 
   protected onBackClick_() {
