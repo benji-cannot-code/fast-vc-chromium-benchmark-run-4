f74cbef941ccaebf36cc84157e881595f1dc7e45
Ahmed Nasr <anasr@google.com>
2024-10-10 16:18:26 +0000

    [BabelOrca] Add tachyon token to receive request.
    Bug: b:364351312
    Change-Id: I3de7d05cb99d64a6f5e9dd9db6a8bff827c8ef2a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914728
    Auto-Submit: Ahmed Nasr <anasr@google.com>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Cr-Commit-Position: refs/heads/main@{#1366883}

--- chromeos/ash/components/boca/babelorca/transcript_receiver_unittest.cc
+++ chromeos/ash/components/boca/babelorca/transcript_receiver_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 
+#include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/scoped_refptr.h"
 #include "base/test/bind.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 #include "chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_authed_client.h"
 #include "chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_request_data_provider.h"
+#include "chromeos/ash/components/boca/babelorca/proto/tachyon.pb.h"
 #include "chromeos/ash/components/boca/babelorca/tachyon_authed_client.h"
 #include "chromeos/ash/components/boca/babelorca/tachyon_response.h"
 #include "chromeos/ash/components/boca/babelorca/tachyon_streaming_client.h"
@@ -333,6 +335,16 @@ TEST_F(TranscriptReceiverTest, NewRequestResetsTranscriptBuilder) {
   VerifyMessage(&result_future, kText1, /*is_final=*/false);
   VerifyMessage(&result_future, kText2, /*is_final=*/false);
   EXPECT_TRUE(result_future.IsEmpty());
+}
+
+TEST_F(TranscriptReceiverTest, RequestHasTachyonToken) {
+  ReceiveMessagesRequest request;
+
+  receiver_->StartReceiving(base::DoNothing(), base::DoNothing());
+
+  ASSERT_TRUE(request.ParseFromString(authed_client_ptr_->GetRequestString()));
+  EXPECT_EQ(request.header().auth_token_payload(),
+            data_provider_.tachyon_token());
 }
 
 }  // namespace
