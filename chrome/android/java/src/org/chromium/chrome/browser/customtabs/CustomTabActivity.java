bb66bee24d00971293b3847bb045acf4d7eb3837
Samar <samarchehade@google.com>
2024-10-10 15:13:25 +0000

    [MIM][CCT] Update maybeShowAccountMismatchNotification signature
    This CL is step 1/3 of the following 3-step change:
    => https://crrev.com/c/5915621
    * https://crrev.com/i/7733386
    * https://crrev.com/c/5920711
    
    Bug: 369563781
    Change-Id: I5473a264043496514c6a8713fc5105463245308d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5915621
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1366847}

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabActivity.java
@@ -247,7 +247,7 @@ public class CustomTabActivity extends BaseCustomTabActivity {
         super.finishNativeInitialization();
         if (SigninFeatureMap.isEnabled(SigninFeatures.CCT_SIGN_IN_PROMPT)) {
             mConnection.maybeShowAccountMismatchNotification(
-                    mSession, getIntent(), getWindowAndroid(), getProfileProviderSupplier());
+                    mSession, getIntent(), this, getWindowAndroid(), getProfileProviderSupplier());
         }
     }
 
