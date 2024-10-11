1092b73d33aa7e3d2b6d321d397dac49e56558de
John Lee <johntlee@chromium.org>
2024-10-11 00:32:09 +0000

    Allow deleting embeddings result visits from history clusters side panel
    This CL adds a new method to the history clusters handlers to remove
    visits by URL and timestamp. This is to support deleting visits
    associated with a history embeddings search result. This is done in
    the clusters handler since the history embeddings components exists
    inside the history clusters side panel UI and it needs to know if
    there are any pending deletes on the page. This is similar to how it's
    done by the BrowsingHistoryHandler on chrome://history.
    
    Bug: 368053282
    Change-Id: I7b23870fe12d31ba5918cd5b1a83cc8e1fb3108c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917406
    Reviewed-by: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367234}

--- chrome/browser/resources/side_panel/history_clusters/history_clusters.ts
+++ chrome/browser/resources/side_panel/history_clusters/history_clusters.ts
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import './app.js';
 
+export {BrowserProxyImpl} from 'chrome://resources/cr_components/history_clusters/browser_proxy.js';
+export {PageCallbackRouter, PageHandlerRemote} from 'chrome://resources/cr_components/history_clusters/history_clusters.mojom-webui.js';
 export {HistoryEmbeddingsBrowserProxyImpl} from 'chrome://resources/cr_components/history_embeddings/browser_proxy.js';
 export {PageHandlerRemote as HistoryEmbeddingsPageHandlerRemote} from 'chrome://resources/cr_components/history_embeddings/history_embeddings.mojom-webui.js';
 export type {HistoryClustersAppElement} from './app.js';
