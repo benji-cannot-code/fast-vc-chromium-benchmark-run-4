dda8cbf1d32e2560fa4fc408a902312ffd7e8d29
Jan Keitel <jkeitel@google.com>
2024-10-11 10:20:48 +0000

    Remove TODOs to refine reasons for why the service is disabled
    Initially, there was an idea to further refine reasons why
    PlusAddressService::IsEnabled may return false. Currently, there are
    the following reasons for it:
    1) The user has had too many 403 errors and is temporarily blocked
       from making more network requests.
    2) The feature is not enabled for the profile.
    3) The user is not signed in.
    
    Reason 1) will go away since the blocking logic was a temporary
    work-around to deal with users that continued to poll even though
    they were not enrolled into the feature (due to multi-user setups).
    Polling is now gone and multi-user setups are handled properly.
    
    Reason 2) will disappear for launch.
    
    The only leaves reason 3) once the feature is launched. Since it does
    not seem to make much sense to add more complexity that will just be
    removed for a public launch, this CL removes the TODOs.
    
    Change-Id: I1ecddc7b566950911f39dad407f945ccb377b181
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925373
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367407}

--- components/plus_addresses/plus_address_service_impl.cc
+++ components/plus_addresses/plus_address_service_impl.cc
@@ -368,8 +368,6 @@ void PlusAddressServiceImpl::ReservePlusAddress(
     const url::Origin& origin,
     PlusAddressRequestCallback on_completed) {
   if (!IsEnabled()) {
-    // TODO(crbug.com/366206137): Differentiate better between reasons why the
-    // service is not enabled.
     std::move(on_completed)
         .Run(base::unexpected(PlusAddressRequestError(
             PlusAddressRequestErrorType::kUserSignedOut)));
@@ -385,8 +383,6 @@ void PlusAddressServiceImpl::RefreshPlusAddress(
     const url::Origin& origin,
     PlusAddressRequestCallback on_completed) {
   if (!IsEnabled()) {
-    // TODO(crbug.com/366206137): Differentiate better between reasons why the
-    // service is not enabled.
     std::move(on_completed)
         .Run(base::unexpected(PlusAddressRequestError(
             PlusAddressRequestErrorType::kUserSignedOut)));
@@ -407,8 +403,6 @@ void PlusAddressServiceImpl::ConfirmPlusAddress(
     const PlusAddress& plus_address,
     PlusAddressRequestCallback on_completed) {
   if (!IsEnabled()) {
-    // TODO(crbug.com/366206137): Differentiate better between reasons why the
-    // service is not enabled.
     std::move(on_completed)
         .Run(base::unexpected(PlusAddressRequestError(
             PlusAddressRequestErrorType::kUserSignedOut)));
