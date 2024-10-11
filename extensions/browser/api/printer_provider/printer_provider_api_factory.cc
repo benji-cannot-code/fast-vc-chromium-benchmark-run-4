d0a2f7d2bb85d8b2f33a72bcee9adade2f1c0e3e
Arthur Milchior <arthurmilchior@chromium.org>
2024-10-11 19:05:41 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: Ia381adcf28202886da0611f905ca379349bcc9ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802166
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367674}

--- extensions/browser/api/printer_provider/printer_provider_api_factory.cc
+++ extensions/browser/api/printer_provider/printer_provider_api_factory.cc
@@ -44,7 +44,8 @@ PrinterProviderAPIFactory::PrinterProviderAPIFactory()
 PrinterProviderAPIFactory::~PrinterProviderAPIFactory() {
 }
 
-KeyedService* PrinterProviderAPIFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+PrinterProviderAPIFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   return PrinterProviderAPI::Create(context);
 }
