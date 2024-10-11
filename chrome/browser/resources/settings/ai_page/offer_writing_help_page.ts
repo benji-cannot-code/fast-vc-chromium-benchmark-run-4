64455c1d163caebfd1d017c269f69d45a43e4985
Zaina Al-Mashni <zalmashni@google.com>
2024-10-11 20:16:24 +0000

    [Synapse] Add strings for experimental features subpages
    Bug: 362225975
    Change-Id: Ib541bff0ccd44898d43817a66db345c08b996bf6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922826
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367736}

--- chrome/browser/resources/settings/ai_page/offer_writing_help_page.ts
+++ chrome/browser/resources/settings/ai_page/offer_writing_help_page.ts
@@ -34,10 +34,6 @@ export class SettingsOfferWritingHelpPageElement extends
 
   static get properties() {
     return {
-      prefs: {
-        type: Object,
-        notify: true,
-      },
       siteList_: {
         type: Array,
         value: [],
@@ -50,6 +46,10 @@ export class SettingsOfferWritingHelpPageElement extends
         type: Boolean,
         value: () => loadTimeData.getBoolean('enableComposeProactiveNudge'),
       },
+      disabledSitesLabel_: {
+        type: String,
+        computed: 'computeDisabledSitesLabel_(enableAiSettingsPageRefresh_)',
+      },
     };
   }
 
@@ -61,6 +61,7 @@ export class SettingsOfferWritingHelpPageElement extends
   private siteList_: string[];
   private enableAiSettingsPageRefresh_: boolean;
   private enableComposeProactiveNudge_: boolean;
+  private disabledSitesLabel_: string;
 
   private hasSites_(): boolean {
     return this.siteList_.length > 0;
@@ -82,6 +83,13 @@ export class SettingsOfferWritingHelpPageElement extends
 
   private getProactiveNudgeToggleHrCssClass_(): string {
     return this.enableAiSettingsPageRefresh_ ? 'hr' : '';
+  }
+
+  private computeDisabledSitesLabel_(): string {
+    return loadTimeData.getString(
+        this.enableAiSettingsPageRefresh_ ?
+            'offerWritingHelpDisabledSitesLabelV2' :
+            'offerWritingHelpDisabledSitesLabel');
   }
 }
 
