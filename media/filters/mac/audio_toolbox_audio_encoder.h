d545cdaa0688c63629603c0a4422ca270644737e
Dale Curtis <dalecurtis@chromium.org>
2024-10-11 21:32:17 +0000

    Clamp AudioConverter bitrate to nearest supported.
    Depending on sample rate and channel count, the max bitrate supported
    by the encoder may be less than requested. In these cases, just clamp
    to closest bitrate less than or equal to the requested one.
    
    Bug: 370072551
    Change-Id: I2e9a0ff6af8272dd8d2231ff5c8a3a34477c5d50
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922773
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367788}

--- media/filters/mac/audio_toolbox_audio_encoder.h
+++ media/filters/mac/audio_toolbox_audio_encoder.h
@@ -41,8 +41,7 @@ class MEDIA_EXPORT AudioToolboxAudioEncoder : public AudioEncoder {
   void Flush(EncoderStatusCB flush_cb) override;
 
  private:
-  bool CreateEncoder(const AudioEncoderConfig& config,
-                     const AudioStreamBasicDescription& output_format);
+  bool CreateEncoder(const AudioStreamBasicDescription& output_format);
 
   void DrainFifoOutput();
 
