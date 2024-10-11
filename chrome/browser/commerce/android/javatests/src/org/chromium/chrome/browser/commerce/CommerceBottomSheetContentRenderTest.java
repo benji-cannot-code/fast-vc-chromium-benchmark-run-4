dd7e333d169bb5a57f3c52b9121d96e147178998
Mei Liang <meiliang@chromium.org>
2024-10-11 21:08:23 +0000

    [Discounts][Clank] Use ListView instead of RecyclerView
    We discovered an issue with the ButtonCompat that uses compound drawable
    within a RecyclerView displayed in bottom sheet. Despite extensive
    investigation, the root cause remains unknow. To unblock further
    development, this CL replaces the RecyclerView with a ListView as a
    workaround.
    
    Bug: 372820957
    Change-Id: I592e7880dd9790dfcc1c20dad6aa2ff75c665d63
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924804
    Commit-Queue: Mei Liang <meiliang@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367768}

--- chrome/browser/commerce/android/javatests/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentRenderTest.java
+++ chrome/browser/commerce/android/javatests/src/org/chromium/chrome/browser/commerce/CommerceBottomSheetContentRenderTest.java
@@ -13,9 +13,9 @@ import static org.chromium.ui.test.util.RenderTestRule.Component.UI_BROWSER_SHOP
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.filters.SmallTest;
 
 import org.junit.Before;
@@ -32,7 +32,6 @@ import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController;
-import org.chromium.components.browser_ui.widget.RecyclerViewTestUtils;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
 import org.chromium.ui.modelutil.MVCListAdapter.ModelList;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -59,7 +58,7 @@ public class CommerceBottomSheetContentRenderTest {
 
     private ModelList mModelList;
     private View mContentView;
-    private RecyclerView mRecyclerView;
+    private ListView mListView;
     private CommerceBottomSheetContentCoordinator mCoordinator;
 
     private PropertyModel createPropertyModel(int type, boolean hasTitle) {
@@ -97,7 +96,7 @@ public class CommerceBottomSheetContentRenderTest {
                                     getActivity(), mBottomSheetController);
 
                     mContentView = mCoordinator.getContentViewForTesting();
-                    mRecyclerView = mCoordinator.getRecyclerViewForTesting();
+                    mListView = mCoordinator.getListViewForTesting();
                     mModelList = mCoordinator.getModelListForTesting();
                     rootView.addView(mContentView);
                 });
@@ -111,7 +110,6 @@ public class CommerceBottomSheetContentRenderTest {
                 () -> {
                     mModelList.add(new ListItem(0, createPropertyModel(0, true)));
                 });
-        RecyclerViewTestUtils.waitForStableMvcRecyclerView(mRecyclerView);
         mRenderTestRule.render(mContentView, "single_content");
     }
 
@@ -125,7 +123,6 @@ public class CommerceBottomSheetContentRenderTest {
                     mModelList.add(new ListItem(0, createPropertyModel(1, false)));
                     mModelList.add(new ListItem(0, createPropertyModel(2, false)));
                 });
-        RecyclerViewTestUtils.waitForStableMvcRecyclerView(mRecyclerView);
         mRenderTestRule.render(mContentView, "multiple_content");
     }
 }
