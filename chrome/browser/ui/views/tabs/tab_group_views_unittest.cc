6e34c1a5fd65393845365525e8d24606b6be101b
Gaurav Kumar <gauravkum@microsoft.com>
2024-10-11 07:51:41 +0000

    [views-ax] Migration of kName attribute in Tab
    This CL migrates the kName attribute in views to be updated
    whenever its value should change, rather than querying the value
    and computing it only when needed.
    
    As part of this change, the following views are updated:
    Tab, BrowserView, PermissionChipView, and TabSlotView. The Tab
    class extends TabSlotView. BrowserView::GetAccessibleTabLabel
     constructs the accessible name for a tab by gathering various
    properties such as the title, group, memory usage, and possibly
    other elements like permission chips, to create a comprehensive
    and accurate label.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Id1ca1da2e970c82575dff12bd48434f716d4548f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836477
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367340}

--- chrome/browser/ui/views/tabs/tab_group_views_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_group_views_unittest.cc
@@ -97,10 +97,10 @@ TEST_F(TabGroupViewsTest, UnderlineBoundsNoDrag) {
   TabGroupHeader* header = group_views_->header();
   Tab* tab_1 = tab_container_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_1->set_group(id_);
+  tab_1->SetGroup(id_);
   Tab* tab_2 = tab_container_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_2->set_group(id_);
+  tab_2->SetGroup(id_);
 
   header->SetBounds(0, 0, 100, 0);
   tab_1->SetBounds(50, 0, 100, 0);
@@ -128,10 +128,10 @@ TEST_F(TabGroupViewsTest, UnderlineBoundsWhenTabsAreNotVisible) {
   TabGroupHeader* header = group_views_->header();
   Tab* tab_1 = tab_container_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_1->set_group(id_);
+  tab_1->SetGroup(id_);
   Tab* tab_2 = tab_container_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_2->set_group(id_);
+  tab_2->SetGroup(id_);
 
   header->SetBounds(0, 0, 100, 0);
   tab_1->SetBounds(50, 0, 100, 0);
@@ -152,10 +152,10 @@ TEST_F(TabGroupViewsTest, UnderlineBoundsHeaderDrag) {
   drag_context_->AddChildView(header);
   Tab* tab_1 = drag_context_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_1->set_group(id_);
+  tab_1->SetGroup(id_);
   Tab* tab_2 = drag_context_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  tab_2->set_group(id_);
+  tab_2->SetGroup(id_);
 
   header->SetBounds(0, 0, 100, 0);
   tab_1->SetBounds(50, 0, 100, 0);
@@ -193,10 +193,10 @@ TEST_F(TabGroupViewsTest, UnderlineBoundsDragTabInGroup) {
   TabGroupHeader* header = group_views_->header();
   Tab* other_tab = tab_container_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  other_tab->set_group(id_);
+  other_tab->SetGroup(id_);
   Tab* dragged_tab = drag_context_->AddChildView(
       std::make_unique<Tab>(tab_slot_controller_.get()));
-  dragged_tab->set_group(id_);
+  dragged_tab->SetGroup(id_);
 
   header->SetBounds(0, 0, 100, 0);
   other_tab->SetBounds(50, 0, 100, 0);
