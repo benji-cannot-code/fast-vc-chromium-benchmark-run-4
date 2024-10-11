f039860db52641d79726889bec15025a92617146
Alexis Hetu <sugoi@chromium.org>
2024-10-11 14:46:30 +0000

    [iOS] Enable logging in the CPE when possible
    This CL adds a utility function to the CPE which verifies whether metrics are enabled, which will be used to enable logging in the CPE specific Framework when metrics are enabled.
    
    Bug: 370513825
    Change-Id: Ia08fa1fc6223f37717666f7378a3ba232ea58af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920856
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367524}

--- ios/chrome/credential_provider_extension/passkey_keychain_util.mm
+++ ios/chrome/credential_provider_extension/passkey_keychain_util.mm
@@ -25,6 +25,7 @@ void FetchSecurityDomainSecret(
     NSString* gaia,
     UINavigationController* navigation_controller,
     PasskeyKeychainProvider::ReauthenticatePurpose purpose,
+    BOOL enable_logging,
     FetchKeyCompletionBlock completion) {
   PasskeyKeychainProvider passkeyKeychainProvider;
   passkeyKeychainProvider.FetchKeys(
