492d50afe1877ea3f5e8fb4be8371a68bdd9140b
Matt Mueller <mattm@chromium.org>
2024-10-11 22:47:22 +0000

    Cert Management UI V2: hook up ServerCertificateDatabaseNSSMigrator
    Hooks up ServerCertificateDatabaseNSSMigrator into the profile loading
    on ChromeOS.
    
    Adds a pref to keep track if migration has been done, it will only be
    done the first time a profile is loaded after the feature flag is
    flipped.
    
    Configures the cert_verifier_creation_params to not load the user NSS
    DB when the feature flag is set, since the certs will be used from the
    server cert db after that.
    
    Bug: 40928765
    Change-Id: Ifc10f2cb77442a25cb9d3553938cca377932c473
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913946
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367829}

--- chrome/browser/net/server_certificate_database_service.h
+++ chrome/browser/net/server_certificate_database_service.h
@@ -11,6 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/net/server_certificate_database.h"
 #include "components/keyed_service/core/keyed_service.h"
 
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chrome/browser/net/server_certificate_database_nss_migrator.h"
+#endif
+
 class Profile;
 
 namespace net {
@@ -19,6 +23,34 @@ namespace net {
 // for TLS.
 class ServerCertificateDatabaseService : public KeyedService {
  public:
+#if BUILDFLAG(IS_CHROMEOS)
+  // These values are persisted to logs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  enum class NSSMigrationResultHistogram {
+    kNssDbEmpty = 0,
+    kSuccess = 1,
+    kPartialSuccess = 2,
+    kFailed = 3,
+    kMaxValue = kFailed,
+  };
+
+  // Enum that will record migration state in user's preferences. In the
+  // current implementation, migration is only attempted once, but saving state
+  // about whether there were any errors with the migration might be useful in
+  // case there are issues during the rollout and we need to add new code that
+  // can try again for anyone that had errors.
+  // These values are persisted to prefs. Entries should not be renumbered and
+  // numeric values should never be reused.
+  enum class NSSMigrationResultPref : int {
+    kNotMigrated = 0,
+    kMigratedSuccessfully = 1,
+    kMigrationHadErrors = 2,
+  };
+#endif
+
+  using GetCertificatesCallback = base::OnceCallback<void(
+      std::vector<net::ServerCertificateDatabase::CertInformation>)>;
+
   explicit ServerCertificateDatabaseService(Profile* profile);
 
   ServerCertificateDatabaseService(const ServerCertificateDatabaseService&) =
@@ -34,10 +66,17 @@ class ServerCertificateDatabaseService : public KeyedService {
       base::OnceCallback<void(bool)> callback);
 
   // Read all certificates from the database.
-  void GetAllCertificates(
-      base::OnceCallback<
-          void(std::vector<net::ServerCertificateDatabase::CertInformation>)>
-          callback);
+  void GetAllCertificates(GetCertificatesCallback callback);
+
+#if BUILDFLAG(IS_CHROMEOS)
+  // Migrate certificates from NSS and then read all certificates from the
+  // database. Migration will only be done once per profile. If called multiple
+  // times before migration completes, all the callbacks will be queued and
+  // processed once the migration is done. If called after migration is
+  // complete it is equivalent to calling `GetAllCertificates`.
+  void GetAllCertificatesMigrateFromNSSFirstIfNeeded(
+      GetCertificatesCallback callback);
+#endif
 
   // Run callback with `server_cert_database_`. The callback will be run on a
   // thread pool sequence where it is allowed to call methods on the database
@@ -47,9 +86,18 @@ class ServerCertificateDatabaseService : public KeyedService {
       base::OnceCallback<void(net::ServerCertificateDatabase*)> callback);
 
  private:
+#if BUILDFLAG(IS_CHROMEOS)
+  void NSSMigrationComplete(
+      ServerCertificateDatabaseNSSMigrator::MigrationResult result);
+#endif
+
   const raw_ptr<Profile> profile_;
 
   base::SequenceBound<net::ServerCertificateDatabase> server_cert_database_;
+#if BUILDFLAG(IS_CHROMEOS)
+  std::unique_ptr<ServerCertificateDatabaseNSSMigrator> nss_migrator_;
+  std::vector<GetCertificatesCallback> get_certificates_pending_migration_;
+#endif
 };
 
 }  // namespace net
