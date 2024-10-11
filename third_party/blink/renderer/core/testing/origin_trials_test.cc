c784acf691ae2a7dcfb00af19860ce9e0ec1f4fb
Philip Rogers <pdr@chromium.org>
2024-10-11 18:44:46 +0000

    Remove deadcode from 3 core unittests
    Change-Id: Ib503f0ddc84d0838fc379ea5eb7a76bafb7ff086
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925084
    Auto-Submit: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367649}

--- third_party/blink/renderer/core/testing/origin_trials_test.cc
+++ third_party/blink/renderer/core/testing/origin_trials_test.cc
@@ -17,7 +17,6 @@ OriginTrialsTest* OriginTrialsTest::Create() {
 
 bool OriginTrialsTest::throwingAttribute(ScriptState* script_state,
                                          ExceptionState& exception_state) {
-  String error_message;
   if (!RuntimeEnabledFeatures::OriginTrialsSampleAPIEnabled(
           ExecutionContext::From(script_state))) {
     exception_state.ThrowDOMException(
