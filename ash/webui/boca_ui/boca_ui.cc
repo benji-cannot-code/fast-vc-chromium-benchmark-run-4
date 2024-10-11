75f8f5d3621368a7f9011d23539292d69d7a4e1d
April Zhou <aprilzhou@google.com>
2024-10-11 21:22:00 +0000

    Reland "boca: Page level implementation to fire activity events to renderer."
    This is a reland of commit 34210d68ffe8cb437c24f37daaa59de5359c5c07
    
    Original change's description:
    > boca: Page level implementation to fire activity events to renderer.
    >
    > Also refactor unit test to get rid of the verification for uninterested
    > mock.
    >
    > Test: Unit tested.
    > Bug: b:369894145
    > Change-Id: I223f5da6e1e72409c79ee0de8704dd9aa8500dc6
    > Reviewed-on: https://chromium-
    
    This CL adds potential fix:
    In the previous unit test, boca_session_manager is destructed before
    boca_app_handler, so by the time we remove listener in boca_app_handler,
    it's already destructed. But we can't simply reverse the order because
    that would result in dangling pointer for session_client_impl. This fix
    is to make boca_app_handler take the same instance of
    session_client_impl instead of creating a new one, which should have
    already been fixed anyway, so that all session request is using the same
    task runner to avoid race.
    
    Bug: b:369894145
    Change-Id: I5faeb39d182b1e842ec67b655c682af70c6d2b9d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922461
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367780}

--- ash/webui/boca_ui/boca_ui.cc
+++ ash/webui/boca_ui/boca_ui.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/webui/common/chrome_os_webui_config.h"
 #include "ash/webui/grit/ash_boca_ui_resources.h"
 #include "ash/webui/grit/ash_boca_ui_resources_map.h"
+#include "chromeos/ash/components/boca/boca_app_client.h"
 #include "chromeos/ash/components/boca/boca_role_util.h"
 #include "chromeos/grit/chromeos_boca_app_bundle_resources.h"
 #include "chromeos/grit/chromeos_boca_app_bundle_resources_map.h"
@@ -126,7 +127,7 @@ void BocaUI::Create(
   page_handler_impl_ = std::make_unique<BocaAppHandler>(
       this, std::move(page_handler), std::move(page), web_ui_,
       std::make_unique<ClassroomPageHandlerImpl>(),
-      std::make_unique<SessionClientImpl>());
+      BocaAppClient::Get()->GetSessionManager()->session_client_impl());
 }
 
 WEB_UI_CONTROLLER_TYPE_IMPL(BocaUI)
