1536e8b1661ade635bbb6bac00e536afd01a9a67
Antonio Sartori <antoniosartori@chromium.org>
2024-10-11 07:37:19 +0000

    Revert "Guard Poland regex changes under address model flag."
    This reverts commit b90aa41254c1afeec0ed2187173a48e35d700efb.
    
    Reason for revert: Failing test AddressFieldParserTest.ParseStreetNameHouseNumbertAndAptNumPL, see https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/45499/overview
    
    Original change's description:
    > Guard Poland regex changes under address model flag.
    >
    > Bug: 334811484
    > Change-Id: Ib6ce6468ef1c0cf7eec150447addb8dc9800a773
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920347
    > Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    > Commit-Queue: Norge Vizcay <vizcay@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1366964}
    
    Bug: 334811484
    Change-Id: I307b11dd4b1f8d03b68d3081b1beb32f6ea9952a
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922704
    Owners-Override: Antonio Sartori <antoniosartori@chromium.org>
    Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1367333}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -23,8 +23,6 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillGreekRegexes;
     case RegexFeature::kAutofillUseITAddressModel:
       return &features::kAutofillUseITAddressModel;
-    case RegexFeature::kAutofillUsePLAddressModel:
-      return &features::kAutofillUsePLAddressModel;
   }
   NOTREACHED();
 }
