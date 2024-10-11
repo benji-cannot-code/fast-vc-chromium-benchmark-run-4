eab9905a43bd506f863bbd2ec9e5cf5fb1dfeb6c
Rakina Zata Amni <rakina@chromium.org>
2024-10-11 06:46:54 +0000

    Log abandonments after commit on new nav commits & renderer crashed
    Currently after a navigation commits and the page is loading, we
    won't log navigation abandonments outside of backgrounding/hiding
    since NavigationDiscardReason is NavigationHandle-based, which is
    already gone after navigation commit. This CL adds tracking for
    new navigation commits & renderer crash so that we can log these
    events as abandonment reason during loading. Additionally, this
    CL also makes sure we stop logging abandonments if a non-terminal
    abandonment already happen, or if we've logged all loading
    milestones.
    
    Bug: 347706997
    Change-Id: I3587baeaaf2b1133d883bb953333c21b09712478
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5859679
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Auto-Submit: Rakina Zata Amni <rakina@chromium.org>
    Commit-Queue: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367323}

--- components/page_load_metrics/browser/page_load_metrics_observer.h
+++ components/page_load_metrics/browser/page_load_metrics_observer.h
@@ -264,6 +264,7 @@ class PageLoadMetricsObserver : public PageLoadMetricsObserverInterface {
   void OnAdAuctionComplete(bool is_server_auction,
                            bool is_on_device_auction,
                            content::AuctionResult result) override {}
+  void OnPrimaryPageRenderProcessGone() override {}
 
  private:
   raw_ptr<PageLoadMetricsObserverDelegate> delegate_ = nullptr;
