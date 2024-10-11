21802cc3fe4f8f75e9642df995bbc5be270d401b
Norge Vizcay <vizcay@google.com>
2024-10-11 12:28:09 +0000

    Reland "Guard Poland regex changes under address model flag."
    This reverts commit c0847a19c026514c16d72d4510d28bdc81dda1c9.
    
    Reason for revert: The culprit is a missing flag that's been added to this reland.
    
    Original change's description:
    > Revert "Guard Poland regex changes under address model flag."
    >
    > This reverts commit b90aa41254c1afeec0ed2187173a48e35d700efb.
    >
    > Reason for revert: Failing test AddressFieldParserTest.ParseStreetNameHouseNumbertAndAptNumPL, see https://ci.chromium.org/ui/p/chrome/builders/ci/linux-chromeos-chrome/45499/overview
    >
    > Original change's description:
    > > Guard Poland regex changes under address model flag.
    > >
    > > Bug: 334811484
    > > Change-Id: Ib6ce6468ef1c0cf7eec150447addb8dc9800a773
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920347
    > > Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    > > Commit-Queue: Norge Vizcay <vizcay@google.com>
    > > Cr-Commit-Position: refs/heads/main@{#1366964}
    >
    > Bug: 334811484
    > Change-Id: I307b11dd4b1f8d03b68d3081b1beb32f6ea9952a
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922704
    > Owners-Override: Antonio Sartori <antoniosartori@chromium.org>
    > Auto-Submit: Antonio Sartori <antoniosartori@chromium.org>
    > Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1367333}
    
    Bug: 334811484, 372842861
    Change-Id: Ia47d1986871ee1a8851e3032e30a225b9517204d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925355
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367456}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -23,6 +23,8 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillGreekRegexes;
     case RegexFeature::kAutofillUseITAddressModel:
       return &features::kAutofillUseITAddressModel;
+    case RegexFeature::kAutofillUsePLAddressModel:
+      return &features::kAutofillUsePLAddressModel;
   }
   NOTREACHED();
 }
