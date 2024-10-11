c63d2adb30f5ced91412e270f692da27a38a6f42
Theo Cristea <theocristea@google.com>
2024-10-11 09:03:08 +0000

    Revert "[SSM] Create ThreadGroupProfilerClient interface and impl"
    This reverts commit 86d91b6ef5ebebe3ff604b35d65ed9764172f69b.
    
    Reason for revert: [Gardening] The CL introduces a test that failed from the first run. Sample failing build: https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-tests/27881/overview
    
    Original change's description:
    > [SSM] Create ThreadGroupProfilerClient interface and impl
    >
    > - Iinterface in base/ for use with future ThreadGroupProfiler
    > - Embedder impl in chrome/common with fixed thread type.
    > - Add kThreadPoolWorker thread type that maps to existing
    >   ThreadPoolWorker proto enum
    > - This is part 2 of a series of CL to implement SSM in Thread pool.
    >
    > Bug: 40226611
    > Change-Id: I078baaf9fe99a8cac3ff8754feda9293e96f03b0
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889543
    > Reviewed-by: Mike Wittman <wittman@chromium.org>
    > Commit-Queue: Rodney Ding <rodneyding@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1366918}
    
    Bug: 40226611
    Change-Id: I57c55ac03e826fd7fd523412df5f5854b8b5dc05
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925353
    Auto-Submit: Theo Cristea <theocristea@google.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367364}

--- components/sampling_profiler/process_type.h
+++ components/sampling_profiler/process_type.h
@@ -38,10 +38,7 @@ enum class ProfilerThreadType {
   // Service worker thread.
   kServiceWorker,
 
-  // Thread pool worker thread.
-  kThreadPoolWorker,
-
-  kMax = kThreadPoolWorker,
+  kMax = kServiceWorker,
 };
 
 }  // namespace sampling_profiler
