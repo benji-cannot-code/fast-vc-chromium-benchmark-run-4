657fed2012dc452f7d112210b921306157a7e06a
Brian Sheedy <bsheedy@chromium.org>
2024-10-11 18:15:29 +0000

    Add BMF Buganizer Support
    Adds support to the GPU bad machine finder for updating a specified
    bug with the script's findings. As a side effect, Blink's Buganizer
    integration now supports posting comments with Markdown enabled.
    
    Bug: 364508697
    Change-Id: Iecff96ede55af7c9d533647404684e3a369d7aad
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900455
    Reviewed-by: Yuly Novikov <ynovikov@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367625}

--- content/test/gpu/bad_machine_finder/__init__.py
+++ content/test/gpu/bad_machine_finder/__init__.py
@@ -2,3 +2,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright 2024 The Chromium Authors
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+import os
+import sys
+
+CHROMIUM_SRC_DIR = os.path.join(os.path.dirname(__file__), '..', '..', '..',
+                                '..')
+BLINK_TOOLS_PATH = os.path.join(CHROMIUM_SRC_DIR, 'third_party', 'blink',
+                                'tools')
+
+if BLINK_TOOLS_PATH not in sys.path:
+  sys.path.append(BLINK_TOOLS_PATH)
