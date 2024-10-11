11a02f9b5c05a3fda886ea272ed8404b3d3557cb
Sida Zhu <zhusida@bytedance.com>
2024-10-11 18:46:29 +0000

    Video: Add portrait resolutions for VTVideoEncodeAccelerator
    These resolutions are always supported, no reason not to add them.
    
    Bug: 40244485
    Change-Id: I57173cd15cf95dca0accb0ef85fc6cdce500ae24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920302
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Sida Zhu <zhusida@bytedance.com>
    Cr-Commit-Position: refs/heads/main@{#1367652}

--- media/gpu/mac/vt_video_encode_accelerator_mac.mm
+++ media/gpu/mac/vt_video_encode_accelerator_mac.mm
@@ -456,6 +456,11 @@ VTVideoEncodeAccelerator::GetSupportedProfiles() {
       }
       if (can_create_hardware_session[codec]) {
         supported_profiles.push_back(supported_profile);
+
+        SupportedProfile portrait_profile(supported_profile);
+        portrait_profile.max_resolution.Transpose();
+        portrait_profile.min_resolution.Transpose();
+        supported_profiles.push_back(portrait_profile);
       }
 
 #if SOFTWARE_ENCODING_SUPPORTED
@@ -466,6 +471,11 @@ VTVideoEncodeAccelerator::GetSupportedProfiles() {
       supported_profile.scalability_modes = always_supported_scalability_modes;
       supported_profile.is_software_codec = true;
       supported_profiles.push_back(supported_profile);
+
+      SupportedProfile portrait_profile(supported_profile);
+      portrait_profile.max_resolution.Transpose();
+      portrait_profile.min_resolution.Transpose();
+      supported_profiles.push_back(portrait_profile);
 #endif  // SOFTWARE_ENCODING_SUPPORTED
     }
   }
