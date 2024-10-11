408a6b132cdd1ea1a884b767c33538547a5def32
Vinny Persky <vinnypersky@google.com>
2024-10-11 18:40:50 +0000

    Announce bank verification website loading on VCN 3DS consent dialog acceptance
    This CL resolves an a11y bug brought up during VCN 3DS a11y testing,
    where there is no user announcement between the consent dialog being
    accepted and the pop-up contents loaded. Thus the user has no idea what
    is happening, which can be a bad experience if it takes several seconds
    to load the contents inside of the pop-up.
    
    Bug: 41490740
    Change-Id: Ifdebeecee27c9e7e6587a9d5a9c1cfa1af39a2e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922398
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vinny Persky <vinnypersky@google.com>
    Cr-Commit-Position: refs/heads/main@{#1367643}

--- chrome/browser/ui/views/autofill/payments/payments_window_user_consent_dialog_view.cc
+++ chrome/browser/ui/views/autofill/payments/payments_window_user_consent_dialog_view.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/mojom/dialog_button.mojom.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/base/ui_base_types.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/fill_layout.h"
@@ -126,8 +127,16 @@ std::u16string PaymentsWindowUserConsentDialogView::GetWindowTitle() const {
 
 void PaymentsWindowUserConsentDialogView::OnDialogClosing() {
   if (payments_window_user_consent_dialog_controller_) {
+    PaymentsWindowUserConsentDialogResult dialog_result =
+        GetDialogResultForClosedReason(GetWidget()->closed_reason());
+    if (dialog_result ==
+        PaymentsWindowUserConsentDialogResult::kAcceptButtonClicked) {
+      GetViewAccessibility().AnnounceText(
+          payments_window_user_consent_dialog_controller_
+              ->GetAcceptanceAccessibilityAnnouncement());
+    }
     payments_window_user_consent_dialog_controller_->OnDialogClosing(
-        GetDialogResultForClosedReason(GetWidget()->closed_reason()));
+        dialog_result);
   }
 }
 
