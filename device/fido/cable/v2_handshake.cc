cdb1cc5a4e397f264fe5898e895551890676fc5d
Nina Satragno <nsatragno@chromium.org>
2024-10-11 18:44:09 +0000

    [webauthn] Add flag to disable hybrid linking
    Add flag to disable hybrid linking:
    * Stop injecting phones not from the old cable extension into requests.
    * Stop advertising linking support.
    
    Bug: 372493822
    Change-Id: Ie1dc29d2dd43168fe3fabde5b922679637b5b591
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925691
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367648}

--- device/fido/cable/v2_handshake.cc
+++ device/fido/cable/v2_handshake.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <type_traits>
 
 #include "base/base64url.h"
+#include "base/feature_list.h"
 #include "base/functional/overloaded.h"
 #include "base/numerics/byte_conversions.h"
 #include "base/numerics/safe_conversions.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/device_event_log/device_event_log.h"
 #include "crypto/aead.h"
 #include "device/fido/cable/v2_constants.h"
+#include "device/fido/features.h"
 #include "device/fido/fido_constants.h"
 #include "device/fido/fido_parsing_utils.h"
 #include "third_party/boringssl/src/include/openssl/aes.h"
@@ -435,7 +437,8 @@ std::string Encode(base::span<const uint8_t, kQRKeySize> qr_key,
 
   qr_contents.emplace(3, static_cast<int64_t>(base::Time::Now().ToTimeT()));
 
-  qr_contents.emplace(4, true);  // client supports storing linking information.
+  qr_contents.emplace(
+      4, base::FeatureList::IsEnabled(device::kWebAuthnHybridLinking));
 
   qr_contents.emplace(5, RequestTypeToString(request_type));
 
