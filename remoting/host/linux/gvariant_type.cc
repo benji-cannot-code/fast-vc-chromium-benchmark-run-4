750d47dea59b7a265a823a92dc6f1787932427f0
Erik Jensen <rkjnsn@google.com>
2024-10-11 23:01:13 +0000

    Add GVariantRef type for working with GVariants
    Change-Id: Idc225d3b07ced534d24a2288512bca08af4c5588
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5761909
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Commit-Queue: Erik Jensen <rkjnsn@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367835}

---
+++ remoting/host/linux/gvariant_type.cc
@@ -0,0 +1,29 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/linux/gvariant_type.h"
+
+#include <glib.h>
+
+#include <cstddef>
+#include <string>
+#include <string_view>
+#include <utility>
+
+namespace remoting::gvariant {
+
+const GVariantType* TypeBase::gvariant_type() const {
+  return G_VARIANT_TYPE(contents().first);
+}
+
+TypeBase::operator const GVariantType*() const {
+  return gvariant_type();
+}
+
+Type<>::Type(const GVariantType* type)
+    : type_string_(g_variant_type_peek_string(type),
+                   g_variant_type_get_string_length(type)) {}
+
+}  // namespace remoting::gvariant
