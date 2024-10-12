a094437d47e93bde2e055e829b3aa08f0d18bdd7
Mike Wasserman <msw@chromium.org>
2024-10-12 05:16:26 +0000

    IDB: Encapsulate LevelDB maintenance timing and tasks in BackingStore
    Move LevelDB pre-close maintenance task determination to BackingStore.
    
    Move timing logic and state into indexed_db_leveldb_operations.cc.
    Replace BucketContext global time replication with atomic globals.
    Simplify code and naming; use more canonical timing utilities.
    
    Bug: 365076682
    Test: Automated, no behavior changes.
    Change-Id: I07b4a8be1120ce3cbe85052b3fd5e707facf11d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5910445
    Commit-Queue: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367906}

--- content/browser/indexed_db/indexed_db_unittest.cc
+++ content/browser/indexed_db/indexed_db_unittest.cc
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/indexed_db/indexed_db_context_impl.h"
 #include "content/browser/indexed_db/indexed_db_data_format_version.h"
 #include "content/browser/indexed_db/indexed_db_leveldb_coding.h"
+#include "content/browser/indexed_db/indexed_db_leveldb_operations.h"
 #include "content/browser/indexed_db/instance/backing_store.h"
 #include "content/browser/indexed_db/instance/backing_store_pre_close_task_queue.h"
 #include "content/browser/indexed_db/instance/bucket_context.h"
@@ -372,6 +373,8 @@ class IndexedDBTest
     context_->IDBTaskRunner()->PostTask(FROM_HERE, loop.QuitClosure());
     loop.Run();
   }
+
+  void SetUp() override { ResetGlobalSweepAndCompactionTimesForTest(); }
 
   void TearDown() override {
     factory_remote_.reset();
@@ -1771,8 +1774,7 @@ TEST_P(IndexedDBTest, PreCloseTasksStart) {
   // Move the clock to run the tasks in the next close sequence.
   // NOTE: The constants rate-limiting sweeps and compaction are currently the
   // same. This test may need to be restructured if these values diverge.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestGlobalSweepFromNow);
+  task_environment_.FastForwardBy(kMaxGlobalSweepDelay);
 
   // Note that once the closing sequence has started, as is the case in the next
   // block, and if the test does anything to spin the message loop, such as
@@ -1807,8 +1809,7 @@ TEST_P(IndexedDBTest, PreCloseTasksStart) {
 
     // Move clock forward to trigger next sweep, but storage key has longer
     // sweep minimum, so no tasks should execute.
-    task_environment_.FastForwardBy(
-        BucketContext::kMaxEarliestGlobalSweepFromNow);
+    task_environment_.FastForwardBy(kMaxGlobalSweepDelay);
 
     bucket_context_handle.Release();
     EXPECT_EQ(BucketContext::ClosingState::kPreCloseGracePeriod,
@@ -1826,8 +1827,7 @@ TEST_P(IndexedDBTest, PreCloseTasksStart) {
 
   {
     //  Finally, move the clock forward so the storage key should allow a sweep.
-    task_environment_.FastForwardBy(
-        BucketContext::kMaxEarliestBucketSweepFromNow);
+    task_environment_.FastForwardBy(kMaxBucketSweepDelay);
     BucketContextHandle bucket_context_handle = CreateBucketHandle();
     bucket_context = bucket_context_handle.bucket_context();
     storage::BucketId bucket_id = bucket_context_handle->bucket_locator().id;
@@ -1845,53 +1845,48 @@ TEST_P(IndexedDBTest, PreCloseTasksStart) {
 TEST_P(IndexedDBTest, TombstoneSweeperTiming) {
   // Open a connection.
   BucketContextHandle bucket_context_handle = CreateBucketHandle();
-  EXPECT_FALSE(bucket_context_handle->ShouldRunTombstoneSweeper());
+  BackingStore* backing_store = bucket_context_handle->backing_store();
+  EXPECT_FALSE(backing_store->ShouldRunTombstoneSweeper());
 
   // Move the clock to run the tasks in the next close sequence.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestGlobalSweepFromNow);
+  task_environment_.FastForwardBy(kMaxGlobalSweepDelay);
 
-  EXPECT_TRUE(bucket_context_handle->ShouldRunTombstoneSweeper());
+  EXPECT_TRUE(backing_store->ShouldRunTombstoneSweeper());
 
   // Move clock forward to trigger next sweep, but storage key has longer
   // sweep minimum, so no tasks should execute.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestGlobalSweepFromNow);
+  task_environment_.FastForwardBy(kMaxGlobalSweepDelay);
 
-  EXPECT_FALSE(bucket_context_handle->ShouldRunTombstoneSweeper());
+  EXPECT_FALSE(backing_store->ShouldRunTombstoneSweeper());
 
   //  Finally, move the clock forward so the storage key should allow a sweep.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestBucketSweepFromNow);
+  task_environment_.FastForwardBy(kMaxBucketSweepDelay);
 
-  EXPECT_TRUE(bucket_context_handle->ShouldRunTombstoneSweeper());
+  EXPECT_TRUE(backing_store->ShouldRunTombstoneSweeper());
 }
 
 TEST_P(IndexedDBTest, CompactionTaskTiming) {
   // Open a connection.
   BucketContextHandle bucket_context_handle = CreateBucketHandle();
-  bucket_context_handle->InitBackingStoreIfNeeded(/*create_if_missing=*/true);
-  EXPECT_FALSE(bucket_context_handle->ShouldRunCompaction());
+  BackingStore* backing_store = bucket_context_handle->backing_store();
+  EXPECT_FALSE(backing_store->ShouldRunCompaction());
 
   // Move the clock to run the tasks in the next close sequence.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestGlobalCompactionFromNow);
+  task_environment_.FastForwardBy(kMaxGlobalCompactionDelay);
 
-  EXPECT_TRUE(bucket_context_handle->ShouldRunCompaction());
+  EXPECT_TRUE(backing_store->ShouldRunCompaction());
 
   // Move clock forward to trigger next compaction, but storage key has longer
   // compaction minimum, so no tasks should execute.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestGlobalCompactionFromNow);
+  task_environment_.FastForwardBy(kMaxGlobalCompactionDelay);
 
-  EXPECT_FALSE(bucket_context_handle->ShouldRunCompaction());
+  EXPECT_FALSE(backing_store->ShouldRunCompaction());
 
   // Finally, move the clock forward so the storage key should allow a
   // compaction.
-  task_environment_.FastForwardBy(
-      BucketContext::kMaxEarliestBucketCompactionFromNow);
+  task_environment_.FastForwardBy(kMaxBucketCompactionDelay);
 
-  EXPECT_TRUE(bucket_context_handle->ShouldRunCompaction());
+  EXPECT_TRUE(backing_store->ShouldRunCompaction());
 }
 
 TEST_P(IndexedDBTest, InMemoryFactoriesStay) {
