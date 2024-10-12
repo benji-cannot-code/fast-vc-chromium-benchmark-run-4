2a832359b520ee6fa5ecae920e431a78618e5b03
Chunbo Hua <chunbo.hua@intel.com>
2024-10-12 03:31:25 +0000

    Format DVLOG output to system defined form for HRESULT
    Bug: None
    Change-Id: Ide74d450bc2c4de9828a3c5cfc4cc0b26fa81e1d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925167
    Commit-Queue: Chunbo Hua <chunbo.hua@intel.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1367898}

--- media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
+++ media/gpu/windows/media_foundation_video_encode_accelerator_win.cc
@@ -2601,7 +2601,7 @@ void MediaFoundationVideoEncodeAccelerator::ProcessOutput() {
   }
 
   RETURN_ON_HR_FAILURE(hr, "Couldn't get encoded data", );
-  DVLOG(3) << "Got encoded data " << hr;
+  DVLOG(3) << "Got encoded data: " << PrintHr(hr);
 
   ComMFSample output_sample;
   ComMFMediaBuffer output_buffer;
