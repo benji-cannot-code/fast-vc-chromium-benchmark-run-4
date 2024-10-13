d6b93ca7859899da88405b9b5b6a60fea5a95aa9
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2024-10-13 15:48:16 +0000

    Enable PlzDedicateWorker on all platforms
    This CL fixes the test failure on Fuchsia to make PlzDedicatedWorker
    fully enabled by default.  The following explains how the test failure
    is fixed.
    
    Fuchsia does HTTP headers rewrite for DedicatedWorkers.  To realize
    that, `WebEngineContentBrowserClient::CreateURLLoaderThrottles()`
    should return throttles.  However, since the original
    PlzDedicatedWorker did not pass valid `frame_tree_node_id` and
    `wc_getter`, the rewrite did not work.  Therefore, the fuchsia's
    browser test failed.
    This CL made them passed to fix the test failures.
    
    You may have concerns on the case `wc_getter` returns nullptr.
    As far as I understand from crbug.com/294086240, the corner case may
    not happen because it might not have nested worker creation use case.
    Even though, to improve the code health, I made
    `WebEngineContentBrowserClient::CreateURLLoaderThrottles()`
    to return {} just in the case.
    
    Bug: 40093136, 350785857
    Change-Id: Ic5a845cd8f7e0aa6392267e0fd9de33534f6ae77
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5779080
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368056}

--- content/browser/worker_host/worker_script_fetcher.cc
+++ content/browser/worker_host/worker_script_fetcher.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/network_service_instance.h"
 #include "content/public/browser/shared_cors_origin_access_list.h"
 #include "content/public/browser/url_loader_throttles.h"
+#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_url_loader_factory.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/content_features.h"
@@ -482,10 +483,28 @@ void WorkerScriptFetcher::CreateScriptLoader(
   // frame tree node ID has the same issue.
   base::RepeatingCallback<WebContents*()> wc_getter =
       base::BindRepeating([]() -> WebContents* { return nullptr; });
+  FrameTreeNodeId frame_tree_node_id = FrameTreeNodeId();
+#if BUILDFLAG(IS_FUCHSIA)
+  // To make `WebEngineContentBrowserClient::CreateURLLoaderThrottles()`
+  // returns throttles, valid `frame_tree_node_id` and `wc_getter` should
+  // be passed. See crbug.com/40244093#comment17.
+  // Upon `UrlRequestRewriteDoesNotAddHeadersForSharedWorkers`, SharedWorkers
+  // are not expected to return throttles and excluded.
+  //
+  // In case of the corner case a shared worker creates a dedicated worker after
+  // the closest ancestor's frame is gone, `wc_getter` will returns nullptr,
+  // and `WebEngineContentBrowserClient::CreateURLLoaderThrottles()` also
+  // returns {}.
+  if (absl::holds_alternative<blink::DedicatedWorkerToken>(worker_token)) {
+    frame_tree_node_id = ancestor_render_frame_host.GetFrameTreeNodeId();
+    wc_getter = base::BindRepeating(&WebContents::FromFrameTreeNodeId,
+                                    frame_tree_node_id);
+  }
+#endif
   std::vector<std::unique_ptr<blink::URLLoaderThrottle>> throttles =
       CreateContentBrowserURLLoaderThrottles(
           *resource_request, browser_context, wc_getter,
-          nullptr /* navigation_ui_data */, FrameTreeNodeId(),
+          nullptr /* navigation_ui_data */, frame_tree_node_id,
           /*navigation_id=*/std::nullopt);
 
   // Create a BrowserContext getter using |service_worker_context|.
