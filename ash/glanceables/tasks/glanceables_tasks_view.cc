853940462669d6d6faed267d7ef2e015859523dd
Sean Kau <skau@chromium.org>
2024-10-14 19:56:40 +0000

    Add a "Settings" Call to Action for ErrorMessageToast
    Adding a new action to the error message if we need to open settings.
    For Focus Mode, the action will be to initiate an OS update.
    
    Bug: b:372029553
    Change-Id: I82c82d1e75da3df0fde05c51869ae2f6b1fc068d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926582
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368398}

--- ash/glanceables/tasks/glanceables_tasks_view.cc
+++ ash/glanceables/tasks/glanceables_tasks_view.cc
@@ -744,6 +744,8 @@ void GlanceablesTasksView::ShowErrorMessageWithType(
                                      base::Unretained(this),
                                      ListShownContext::kInitialList);
       break;
+    case ErrorMessageToast::ButtonActionType::kSettings:
+      NOTREACHED();
   }
   ShowErrorMessage(GetErrorString(error_type), std::move(callback),
                    button_type);
--- ash/system/focus_mode/sounds/focus_mode_sounds_view.cc
+++ ash/system/focus_mode/sounds/focus_mode_sounds_view.cc
@@ -318,6 +318,9 @@ constexpr std::partial_ordering FocusModeSoundsView::ToastData::operator<=>(
         return std::partial_ordering::less;
       }
       break;
+    case ErrorMessageToast::ButtonActionType::kSettings:
+      // TODO(crbug.com/372029553): Reimplement prioritization.
+      NOTREACHED();
   }
 
   if (message != other.message) {
@@ -626,6 +629,9 @@ void FocusModeSoundsView::ShowErrorMessageForType(
           base::BindRepeating(&FocusModeSoundsView::DownloadPlaylistsForType,
                               weak_factory_.GetWeakPtr(), is_soundscape_type);
       break;
+    case ErrorMessageToast::ButtonActionType::kSettings:
+      // TODO(crbug.com/372029553): Change this to open settings.
+      NOTREACHED();
   }
 
   error_message_ = AddChildView(std::make_unique<ErrorMessageToast>(
