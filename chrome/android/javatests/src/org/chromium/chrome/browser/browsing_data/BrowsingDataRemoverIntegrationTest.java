3b199670e7bb558fff11fca5719df5bc76154392
Brandon Wylie <wylieb@chromium.org>
2024-10-14 19:58:13 +0000

    [sheriff] Disable flaky BrowsingDataRemoverIntegrationTest
    Bug: 373429766
    Change-Id: I10d1eace12dfbfe4fce6b0c17207c50be4351960
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5931703
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Owners-Override: Brandon Wylie <wylieb@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368400}

--- chrome/android/javatests/src/org/chromium/chrome/browser/browsing_data/BrowsingDataRemoverIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/browsing_data/BrowsingDataRemoverIntegrationTest.java
@@ -16,6 +16,7 @@ import org.junit.runner.RunWith;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.chrome.browser.browserservices.intents.BrowserServicesIntentDataProvider;
 import org.chromium.chrome.browser.browserservices.verification.ChromeVerificationResultStore;
 import org.chromium.chrome.browser.browsing_data.BrowsingDataBridge.OnClearBrowsingDataListener;
@@ -161,6 +162,7 @@ public class BrowsingDataRemoverIntegrationTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/373429766")
     public void testClearingTabs() throws TimeoutException {
         EmbeddedTestServer testServer = mActivityTestRule.getTestServer();
         String testUrl = testServer.getURL(TEST_PATH);
