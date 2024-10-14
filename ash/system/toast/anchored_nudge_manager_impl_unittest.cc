107489852c5ef14ab3c8e39dd4f99f8fd3cb5f93
Sammie Quon <sammiequon@chromium.org>
2024-10-14 21:13:27 +0000

    cros: Remove SystemToastStyle
    It's in deprecated namespace and has no uses. I was looking at
    ScopedA11yOverrideWindowSetter and was removing it from this class
    then I noticed the whole class can be removed.
    
    Test: none
    Bug: none
    Change-Id: Ic60de814847c28c893eeabcd5a73d4ece07fc1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930584
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Min Chen <minch@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368427}

--- ash/system/toast/anchored_nudge_manager_impl_unittest.cc
+++ ash/system/toast/anchored_nudge_manager_impl_unittest.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/hotseat_widget.h"
 #include "ash/shelf/shelf.h"
 #include "ash/shell.h"
-#include "ash/style/system_toast_style.h"
 #include "ash/system/toast/anchored_nudge.h"
 #include "ash/system/toast/nudge_constants.h"
 #include "ash/system/unified/unified_system_tray.h"
--- ash/wm/snap_group/snap_group_unittest.cc
+++ ash/wm/snap_group/snap_group_unittest.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/shell_delegate.h"
 #include "ash/style/close_button.h"
-#include "ash/style/system_toast_style.h"
 #include "ash/system/toast/toast_manager_impl.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/test/ash_test_util.h"
