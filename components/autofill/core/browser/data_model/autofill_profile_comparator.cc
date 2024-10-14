142d3b905fe0e5a333f56afa703d3a7cbcc30db5
Karol Sygiet <sygiet@google.com>
2024-10-14 16:50:10 +0000

    Limit the max length of the values that are compared for mergeability
    The profiles that have fields with values longer than 100 characters
    won't be considered for merging to address the performance issues.
    
    Bug: 325452461
    Change-Id: Ic5ba9312d41860546ef9e4ff26817bd8fba86605
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928864
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1368305}

--- components/autofill/core/browser/data_model/autofill_profile_comparator.cc
+++ components/autofill/core/browser/data_model/autofill_profile_comparator.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/geo/autofill_country.h"
 #include "components/autofill/core/browser/metrics/autofill_metrics.h"
 #include "components/autofill/core/common/autofill_clock.h"
+#include "components/autofill/core/common/autofill_features.h"
 #include "components/autofill/core/common/autofill_l10n_util.h"
 #include "third_party/libphonenumber/phonenumber_api.h"
 
@@ -720,6 +721,18 @@ AutofillProfileComparator::NonMergeableSettingVisibleTypes(
   }
   FieldTypeSet setting_visible_types = GetUserVisibleTypes();
   FieldTypeSet non_mergeable_types;
+  // If one the fields have too long value, do not merge this profile.
+  if (base::FeatureList::IsEnabled(
+          features::kAutofillLogDeduplicationMetrics) &&
+      std::ranges::any_of(setting_visible_types, [&](FieldType t) {
+        return std::max(a.GetRawInfo(t).size(), b.GetRawInfo(t).size()) >
+               features::
+                   kAutofillLogDeduplicationMetricsMaxFieldLengthForMergingParam
+                       .Get();
+      })) {
+    return std::nullopt;
+  }
+
   auto maybe_add_type = [&](FieldType type, bool is_mergeable) {
     // Ensure that `type` is actually a setting-visible type.
     CHECK_EQ(setting_visible_types.erase(type), 1u);
