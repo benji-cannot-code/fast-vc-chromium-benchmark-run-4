4a31e32f0e8695b739fc64eef651fa6c9fb1ad8d
Zhe Li <zheliooo@google.com>
2024-10-14 04:40:53 +0000

    Fix crash when dragging incognito tab
    demo: https://drive.google.com/file/d/11CyjYXyt7btPUmBpyT4Em7MwxDNGRU-H/view?usp=sharing
    Bug: 372853925
    Change-Id: I744057aed66fa9f6f0be3179bb22c6ab2ba2deda
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921552
    Reviewed-by: Neil Coronado <nemco@google.com>
    Commit-Queue: Zhe Li <zheliooo@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368106}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/overlays/strip/StripLayoutHelper.java
@@ -658,7 +658,8 @@ public class StripLayoutHelper
         mTabMenu.setModal(true);
 
         mActionConfirmationDelegate =
-                new ActionConfirmationDelegate(actionConfirmationManager, mToolbarContainerView);
+                new ActionConfirmationDelegate(
+                        actionConfirmationManager, mToolbarContainerView, mIncognito);
         mGroupIdToHideSupplier.addObserver((newIdToHide) -> rebuildStripViews());
 
         mIsFirstLayoutPass = true;
@@ -4029,7 +4030,8 @@ public class StripLayoutHelper
                             RecordUserAction.record("MobileToolbarReorderTab.TabRemovedFromGroup");
                         });
             } else if (StripLayoutUtils.getNumOfTabsInGroup(mTabGroupModelFilter, targetGroupTitle)
-                    > 1) {
+                            > 1
+                    || mIncognito) {
                 mTabGroupModelFilter.moveTabOutOfGroupInDirection(tabId, towardEnd);
                 RecordUserAction.record("MobileToolbarReorderTab.TabRemovedFromGroup");
             }
@@ -4927,10 +4929,11 @@ public class StripLayoutHelper
         if (selectedTab != null
                 && findTabById(selectedTab.getTabId()) != null
                 && selectedTab.isDraggedOffStrip()) {
+
             // Rebuild tab groups to unhide the interacting tab group as tab is restored back on tab
             // strip.
-            if (mActionConfirmationDelegate.isTabRemoveDialogSkipped()
-                    && isLastTabInGroup(selectedTab.getTabId())) {
+            if (isLastTabInGroup(selectedTab.getTabId())
+                    && mActionConfirmationDelegate.isTabRemoveDialogSkipped()) {
                 mGroupIdToHideSupplier.set(Tab.INVALID_TAB_ID);
             }
             dragActiveClickedTabOntoStrip(LayoutManagerImpl.time(), 0.0f, false);
@@ -5028,13 +5031,15 @@ public class StripLayoutHelper
         // Show group delete dialog when the last tab in group is being dragged off tab strip.
         boolean draggingLastTabInGroup = isLastTabInGroup(tabId);
         if (draggingLastTabInGroup && !mIncognito) {
-            mActionConfirmationDelegate.handleDeleteGroupAction(
-                    tab.getRootId(),
-                    /* draggingLastTabOffStrip= */ true,
-                    /* tabClosing= */ false,
-                    () -> {
-                        mTabGroupModelFilter.moveTabOutOfGroupInDirection(tabId, false);
-                    });
+            if (!mIncognito) {
+                mActionConfirmationDelegate.handleDeleteGroupAction(
+                        tab.getRootId(),
+                        /* draggingLastTabOffStrip= */ true,
+                        /* tabClosing= */ false,
+                        () -> {
+                            mTabGroupModelFilter.moveTabOutOfGroupInDirection(tabId, false);
+                        });
+            }
         }
 
         // Store reorder state, then exit reorder mode.
