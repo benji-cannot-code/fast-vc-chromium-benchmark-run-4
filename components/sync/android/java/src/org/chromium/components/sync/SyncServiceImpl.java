0a691f18f8c2f2265b9de9668c20bd1880675425
Mahmoud Rashad <mmrashad@google.com>
2024-10-14 16:03:58 +0000

    Remove an outdated isEngineInitialized() assertion in SyncServiceImpl
    Bug: 372869932
    Change-Id: Id28debde9354608931d3a95a245982782d178995
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928713
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368275}

--- components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
+++ components/sync/android/java/src/org/chromium/components/sync/SyncServiceImpl.java
@@ -329,7 +329,6 @@ public class SyncServiceImpl implements SyncService, AccountsChangeObserver {
     public boolean isUsingExplicitPassphrase() {
         mThreadChecker.assertOnValidThread();
         assert mSyncServiceAndroidBridge != 0;
-        assert isEngineInitialized();
         return SyncServiceImplJni.get().isUsingExplicitPassphrase(mSyncServiceAndroidBridge);
     }
 
