52514ba9b708213cadb53e05c4393b1ab4163803
Thomas Lukaszewicz <tluk@chromium.org>
2024-10-14 00:54:50 +0000

    Revert "[side search] Add a Side Search PLMO for the browser side panel"
    This reverts commit 5c8c8697e90a4894c1555bf255f0840951b836bf.
    
    With the new Lens integration Side Search is now deprecated.
    
    This CL removes the PageLoadMetricsObserver codepaths.
    
    OBSOLETE_HISTOGRAMS=Side Search has been deprecated.
    
    Bug: 357910230
    Change-Id: Ia0ad6fc29a3b1ad77bed2f21fdd41b3152a3d386
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921057
    Reviewed-by: John Delaney <johnidel@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368087}

--- chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -79,7 +79,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(TOOLKIT_VIEWS)
-#include "chrome/browser/page_load_metrics/observers/side_search_page_load_metrics_observer.h"
 #include "chrome/browser/ui/side_search/side_search_utils.h"
 #endif  // defined(TOOLKIT_VIEWS)
 
@@ -155,18 +154,6 @@ void PageLoadMetricsEmbedder::RegisterObservers(
   // about WebUI chrome pages (such as visibility-related things),
   // or because they depend on objects that don't exist for WebUI pages
   // (namely `TabHelper`s).
-
-  // TODO(crbug.com/40823327): Integrate side panel metrics with UKM.
-  if (IsSidePanel(web_contents())) {
-#if defined(TOOLKIT_VIEWS)
-    if (auto side_search_observer =
-            SideSearchPageLoadMetricsObserver::CreateIfNeeded(
-                tracker->GetWebContents())) {
-      tracker->AddObserver(std::move(side_search_observer));
-    }
-#endif  // defined(TOOLKIT_VIEWS)
-    return;
-  }
 
 #if !BUILDFLAG(IS_ANDROID)
   if (IsNonTabWebUI()) {
