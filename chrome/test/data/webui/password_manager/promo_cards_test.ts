391d904618232064327ecf3b0cc1bb43530ba457
Ryan Sultanem <rsult@google.com>
2024-10-14 18:48:01 +0000

    [BatchUpload] Disable newly added code in password manager for non dice platforms
    crrev.com/c/5923785 introduced a change in password manager that was
    meant for Dice enabled platforms only. This caused issues in JS tests
    running on ChromeOS since it was not protected against.
    
    This CL fixes this issue by protecting against ChromeOS.
    
    Fixed: 373393964
    Change-Id: I83bf949dbdee78c4a6466f860d6dc00f3357c651
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930703
    Owners-Override: Matt Reichhoff <mreichhoff@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Matt Reichhoff <mreichhoff@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368353}

--- chrome/test/data/webui/password_manager/promo_cards_test.ts
+++ chrome/test/data/webui/password_manager/promo_cards_test.ts
@@ -7,7 +7,9 @@ import 'chrome://password-manager/password_manager.js';
 
 import type {PasswordsSectionElement} from 'chrome://password-manager/password_manager.js';
 import {Page, PasswordManagerImpl, PromoCardsProxyImpl, Router, SyncBrowserProxyImpl, UrlParam} from 'chrome://password-manager/password_manager.js';
+// <if expr="not is_chromeos">
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+// </if>
 import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
 import {flushTasks} from 'chrome://webui-test/polymer_test_util.js';
 import {isVisible} from 'chrome://webui-test/test_util.js';
@@ -251,6 +253,7 @@ suite('PasswordsSectionTest', function() {
     assertTrue(isVisible(moveDialog.$.move));
   });
 
+  // <if expr="not is_chromeos">
   test('move passwords promo visible opens batch upload', async function() {
     loadTimeData.overrideValues({
       isBatchUploadDesktopEnabled: true,
@@ -293,6 +296,7 @@ suite('PasswordsSectionTest', function() {
 
     await promoCardsProxy.whenCalled('openBatchUpload');
   });
+  // </if>
 
   test('screenlock promo', async function() {
     promoCardsProxy.promo = {
