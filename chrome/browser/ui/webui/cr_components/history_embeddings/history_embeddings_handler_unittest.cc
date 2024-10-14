233f7180c1ea5adf3120adeba68d5111d370060a
Orin Jaworski <orinj@google.com>
2024-10-14 23:22:13 +0000

    Distinguish UI for logging and UMA; extend and slice UserActions on UI
    This CL makes the HistoryEmbeddingsHandler aware of its UI context so
    it can pass the right UiSurface to SendQualityLog and send sliced
    versions of the UserActions metrics.
    
    This change also prepares the structure for reporting actions shown,
    answer citations clicked, and other history results clicked.
    
    Bug: 372278248, 372268020
    Change-Id: I388bd9aeb3954b48f8bdc95e8ad17182f8bb1ef4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922121
    Reviewed-by: John Lee <johntlee@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Commit-Queue: Orin Jaworski <orinj@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368498}

--- chrome/browser/ui/webui/cr_components/history_embeddings/history_embeddings_handler_unittest.cc
+++ chrome/browser/ui/webui/cr_components/history_embeddings/history_embeddings_handler_unittest.cc
@@ -132,7 +132,7 @@ class HistoryEmbeddingsHandlerTest : public BrowserWithTestWindowTest {
 
     handler_ = std::make_unique<HistoryEmbeddingsHandler>(
         mojo::PendingReceiver<history_embeddings::mojom::PageHandler>(),
-        profile_->GetWeakPtr(), web_ui());
+        profile_->GetWeakPtr(), web_ui(), false);
     handler_->SetPage(page_.BindAndGetRemote());
   }
 
@@ -229,7 +229,7 @@ TEST_F(HistoryEmbeddingsHandlerTest, FormatsMojoResults) {
 }
 
 TEST_F(HistoryEmbeddingsHandlerTest, RecordsMetrics) {
-  handler_->RecordSearchResultsMetrics(false, false);
+  handler_->RecordSearchResultsMetrics(false, false, false, false, false);
   histogram_tester().ExpectBucketCount(
       "History.Embeddings.UserActions",
       HistoryEmbeddingsUserActions::kEmbeddingsSearch, 1);
@@ -239,8 +239,17 @@ TEST_F(HistoryEmbeddingsHandlerTest, RecordsMetrics) {
   histogram_tester().ExpectBucketCount(
       "History.Embeddings.UserActions",
       HistoryEmbeddingsUserActions::kEmbeddingsResultClicked, 0);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kAnswerShown, 0);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kAnswerCitationClicked, 0);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kOtherHistoryResultClicked, 0);
 
-  handler_->RecordSearchResultsMetrics(true, true);
+  handler_->RecordSearchResultsMetrics(true, true, true, true, true);
   histogram_tester().ExpectBucketCount(
       "History.Embeddings.UserActions",
       HistoryEmbeddingsUserActions::kEmbeddingsSearch, 2);
@@ -250,6 +259,34 @@ TEST_F(HistoryEmbeddingsHandlerTest, RecordsMetrics) {
   histogram_tester().ExpectBucketCount(
       "History.Embeddings.UserActions",
       HistoryEmbeddingsUserActions::kEmbeddingsResultClicked, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kAnswerShown, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kAnswerCitationClicked, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions",
+      HistoryEmbeddingsUserActions::kOtherHistoryResultClicked, 1);
+
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kEmbeddingsSearch, 2);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kEmbeddingsNonEmptyResultsShown, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kEmbeddingsResultClicked, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kAnswerShown, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kAnswerCitationClicked, 1);
+  histogram_tester().ExpectBucketCount(
+      "History.Embeddings.UserActions.HistoryPage",
+      HistoryEmbeddingsUserActions::kOtherHistoryResultClicked, 1);
 }
 
 TEST_F(HistoryEmbeddingsHandlerTest, ShowsPromo) {
--- ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
+++ ui/webui/resources/cr_components/history_embeddings/history_embeddings.ts
@@ -392,7 +392,8 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
     }));
 
     this.clickedIndices_.add(e.model.index);
-    this.browserProxy_.recordSearchResultsMetrics(true, true);
+    this.browserProxy_.recordSearchResultsMetrics(
+        true, true, this.hasAnswer_(), false, false);
   }
 
   private onSearchQueryChanged_() {
@@ -493,7 +494,8 @@ export class HistoryEmbeddingsElement extends HistoryEmbeddingsElementBase {
     if (canLog && !userClickedResult) {
       const nonEmptyResults: boolean = !!this.searchResult_ &&
           this.searchResult_.items && this.searchResult_.items.length > 0;
-      this.browserProxy_.recordSearchResultsMetrics(nonEmptyResults, false);
+      this.browserProxy_.recordSearchResultsMetrics(
+          nonEmptyResults, false, this.hasAnswer_(), false, false);
     }
 
     if (!this.forceSuppressLogging && canLog) {
