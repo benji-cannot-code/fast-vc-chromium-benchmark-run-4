986dae829f8b49fbcdd451ae75936da252b2439f
Nigel Tao <nigeltao@chromium.org>
2024-10-14 01:08:49 +0000

    Image Loader: log setupOffscreenDocument errors
    Bug: b:319190834
    Test: Manual
    Change-Id: I43503965baaeb0e1d1fb2f422e7d2efa11916e00
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5927903
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368088}

--- ui/file_manager/image_loader/service_worker.ts
+++ ui/file_manager/image_loader/service_worker.ts
@@ -55,7 +55,12 @@ async function sendToOffscreenDocument(
     msg: LoadImageRequest, senderOrigin: string,
     sendResponse: (r: LoadImageResponse) => void) {
   assert(msg);
-  await setupOffscreenDocument();
+  try {
+    await setupOffscreenDocument();
+  } catch (e) {
+    console.error('setupOffscreenDocument:', e);
+    return;
+  }
   msg.imageLoaderRequestId = senderOrigin + ':' + msg.taskId;
   chrome.runtime.sendMessage(
       null, msg, undefined, (response: LoadImageResponse) => {
