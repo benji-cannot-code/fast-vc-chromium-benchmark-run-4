0b625725a22b7293d0deb19e1f3f53e456540444
Xiaodan Zhu <zxdan@chromium.org>
2024-10-14 23:24:49 +0000

    coral: create a fake backend
    This cl creates a fake backend for Coral to generate groups by
    splitting the tab and app items into half and half.
    
    Add a new switch called --force-fake-birch-coral-backend.
    
    Change original --force-fake-birch-coral to
    --force-fake-birch-coral-group
    
    Example: https://screenshot.googleplex.com/5KePtVXnDTcsXxS
    
    Bug: b:363045903
    Change-Id: I1d1e7b9b01e421301bd54ccce27cae127228a631
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924740
    Reviewed-by: Howard Yang <hcyang@google.com>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368499}

--- ash/wm/coral/coral_controller.cc
+++ ash/wm/coral/coral_controller.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/coral/coral_controller.h"
 
+#include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/coral_delegate.h"
 #include "ash/shell.h"
+#include "ash/wm/coral/fake_coral_service.h"
 #include "ash/wm/desks/desks_controller.h"
+#include "base/command_line.h"
 #include "base/json/json_writer.h"
 #include "chromeos/ash/components/mojo_service_manager/connection.h"
 #include "chromeos/ash/services/coral/public/mojom/coral_service.mojom.h"
@@ -68,8 +71,8 @@ void CoralController::GenerateContentGroups(const CoralRequest& request,
     return;
   }
 
-  EnsureCoralService();
-  if (!coral_service_) {
+  CoralService* coral_service = EnsureCoralService();
+  if (!coral_service) {
     LOG(ERROR) << "Failed to connect to coral service.";
     std::move(callback).Run(nullptr);
     return;
@@ -88,7 +91,7 @@ void CoralController::GenerateContentGroups(const CoralRequest& request,
   for (size_t i = 0; i < items_in_request; i++) {
     group_request->entities.push_back(request.content()[i]->Clone());
   }
-  coral_service_->Group(
+  coral_service->Group(
       std::move(group_request),
       base::BindOnce(&CoralController::HandleGroupResult,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
@@ -96,8 +99,8 @@ void CoralController::GenerateContentGroups(const CoralRequest& request,
 
 void CoralController::CacheEmbeddings(const CoralRequest& request,
                                       base::OnceCallback<void(bool)> callback) {
-  EnsureCoralService();
-  if (!coral_service_) {
+  CoralService* coral_service = EnsureCoralService();
+  if (!coral_service) {
     LOG(ERROR) << "Failed to connect to coral service.";
     std::move(callback).Run(false);
     return;
@@ -110,21 +113,30 @@ void CoralController::CacheEmbeddings(const CoralRequest& request,
     cache_embeddings_request->entities.push_back(entity->Clone());
   }
 
-  coral_service_->CacheEmbeddings(
+  coral_service->CacheEmbeddings(
       std::move(cache_embeddings_request),
       base::BindOnce(&CoralController::HandleCacheEmbeddingsResult,
                      weak_factory_.GetWeakPtr(), std::move(callback)));
 }
 
-void CoralController::EnsureCoralService() {
-  if (coral_service_) {
-    return;
+CoralController::CoralService* CoralController::EnsureCoralService() {
+  // Generate a fake service if --force-birch-fake-coral-backend is enabled.
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kForceBirchFakeCoralBackend)) {
+    if (!fake_service_) {
+      fake_service_ = std::make_unique<FakeCoralService>();
+    }
+    return fake_service_.get();
   }
-  auto pipe_handle = coral_service_.BindNewPipeAndPassReceiver().PassPipe();
-  coral_service_.reset_on_disconnect();
-  ash::mojo_service_manager::GetServiceManagerProxy()->Request(
-      chromeos::mojo_services::kCrosCoralService, std::nullopt,
-      std::move(pipe_handle));
+
+  if (!coral_service_) {
+    auto pipe_handle = coral_service_.BindNewPipeAndPassReceiver().PassPipe();
+    coral_service_.reset_on_disconnect();
+    ash::mojo_service_manager::GetServiceManagerProxy()->Request(
+        chromeos::mojo_services::kCrosCoralService, std::nullopt,
+        std::move(pipe_handle));
+  }
+  return coral_service_.get();
 }
 
 void CoralController::HandleGroupResult(CoralResponseCallback callback,
--- ash/wm/coral/coral_controller.h
+++ ash/wm/coral/coral_controller.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class FakeCoralService;
+
 class ASH_EXPORT CoralRequest {
  public:
   using ContentItem = coral::mojom::EntityPtr;
@@ -84,7 +86,9 @@ class ASH_EXPORT CoralController {
  private:
   using CoralService = coral::mojom::CoralService;
 
-  void EnsureCoralService();
+  // Requests coral service from service manager and returns the pointer of the
+  // service instance.
+  CoralService* EnsureCoralService();
 
   // Used as the callback of mojom::CoralService::Group.
   void HandleGroupResult(CoralResponseCallback callback,
@@ -99,6 +103,8 @@ class ASH_EXPORT CoralController {
       coral::mojom::CacheEmbeddingsResultPtr result);
 
   mojo::Remote<CoralService> coral_service_;
+
+  std::unique_ptr<FakeCoralService> fake_service_;
 
   base::WeakPtrFactory<CoralController> weak_factory_{this};
 };
