8e4fa1af17acc81362aaed50e209a10265f4afdd
Aaron Leventhal <aleventhal@google.com>
2024-10-14 21:31:08 +0000

    [A11y] Diagnose why mark_all_dirty_ set at unexpected time
    Bug: 372508699
    Change-Id: I47636870b34383dab6518c25a72f94dba74be8f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930644
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368437}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.cc
@@ -3114,6 +3114,10 @@ void AXObjectCacheImpl::CommitAXUpdates(Document& document, bool force) {
   // Upon exiting this function, listen for tree updates again.
   absl::Cleanup lifecycle_returns_to_queueing_updates = [this] {
     lifecycle_.EnsureStateAtMost(AXObjectCacheLifecycle::kDeferTreeUpdates);
+#if DCHECK_IS_ON()
+    // TODO(https://crbug.com/372508699): Remove after bug fixed.
+    can_mark_all_dirty_ = true;
+#endif
   };
 
   SCOPED_DISALLOW_LIFECYCLE_TRANSITION();
@@ -3178,6 +3182,11 @@ void AXObjectCacheImpl::CommitAXUpdates(Document& document, bool force) {
 #endif
 
       mark_all_dirty_ = false;
+#if DCHECK_IS_ON()
+      // TODO(https://crbug.com/372508699): Remove after bug fixed.
+      // Do not mark document dirty after the point that we expect to.
+      can_mark_all_dirty_ = false;
+#endif
 
       // All tree updates have been processed.
       DUMP_WILL_BE_CHECK(!IsMainDocumentDirty());
@@ -4969,6 +4978,11 @@ void AXObjectCacheImpl::MarkSubtreeDirty(Node* node) {
 
 void AXObjectCacheImpl::MarkDocumentDirty() {
   CHECK(!IsFrozen());
+  // TODO(https://crbug.com/372508699): Remove after bug fixed.
+#if DCHECK_IS_ON()
+  CHECK(can_mark_all_dirty_);
+#endif
+
   mark_all_dirty_ = true;
 
   ScheduleAXUpdate();
