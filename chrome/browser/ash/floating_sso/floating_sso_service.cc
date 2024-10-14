3c9b30791ad5a2d067a2a94c506172a50496451b
Andrey Davydov <andreydav@google.com>
2024-10-14 16:47:07 +0000

    [Floating SSO] Fix how we check if Floating Workspace is enabled
    We now use ash::floating_workspace_util::IsFloatingWorkspaceV2Enabled()
    instead of having our own implementation which checked a wrong pref and
    unnecessary relied on a feature flag.
    
    Also, KeepsSessionCookiesIfFloatingWorkspaceEnabled browser test is
    adjusted to no longer require the safe mode.
    
    Bug: b:333526131, b:354907485
    Test: FloatingSsoTest.KeepsSessionCookiesIfFloatingWorkspaceEnabled
    Change-Id: I5715d3dbe5af27a342d40f9cdba8998d594756d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925671
    Reviewed-by: Maria Petrisor <mpetrisor@chromium.org>
    Reviewed-by: Matthew Zhu <zhumatthew@google.com>
    Commit-Queue: Andrey Davydov <andreydav@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368303}

--- chrome/browser/ash/floating_sso/floating_sso_service.cc
+++ chrome/browser/ash/floating_sso/floating_sso_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/ash/floating_sso/cookie_sync_conversions.h"
 #include "chrome/browser/ash/floating_sso/floating_sso_sync_bridge.h"
+#include "chrome/browser/ash/floating_workspace/floating_workspace_util.h"
 #include "chrome/common/pref_names.h"
 #include "components/google/core/common/google_util.h"
 #include "components/prefs/pref_change_registrar.h"
@@ -266,7 +267,8 @@ void FloatingSsoService::OnCookiesLoaded(const net::CookieList& cookies) {
 bool FloatingSsoService::ShouldSyncCookie(
     const net::CanonicalCookie& cookie) const {
   // Filter out session cookies (except when Floating Workspace is enabled).
-  if (!cookie.IsPersistent() && !IsFloatingWorkspaceEnabled()) {
+  if (!cookie.IsPersistent() &&
+      !ash::floating_workspace_util::IsFloatingWorkspaceV2Enabled()) {
     return false;
   }
 
@@ -296,13 +298,6 @@ bool FloatingSsoService::IsDomainAllowed(
 
   // The domain is not blocked if it doesn't have matches in the blocklist.
   return block_url_matcher_->MatchURL(cookie_domain_url).empty();
-}
-
-bool FloatingSsoService::IsFloatingWorkspaceEnabled() const {
-  bool floating_workspace_policy_enabled =
-      prefs_->GetBoolean(ash::prefs::kFloatingWorkspaceV2Enabled);
-  return floating_workspace_policy_enabled &&
-         ash::features::IsFloatingWorkspaceV2Enabled();
 }
 
 void FloatingSsoService::OnConnectionError() {
