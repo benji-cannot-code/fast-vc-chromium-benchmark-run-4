03540a8c6416fbe6b3efb7ae7dcbe0ee98b000cf
Piotr Kotynia <piotrkotynia@google.com>
2024-10-14 09:25:46 +0000

    Avoid needless SendTypePredictionsToRenderer calls
    The "SendTypePredictionsToRenderer" function sends type prediction
    data from the browser process to the renderer process.
    This data is only used by the "AutofillShowTypePredictions"
    debugging feature, which is typically disabled for regular users.
    
    This commit avoids sending this data when the
    "AutofillShowTypePredictions" feature is not enabled.
    
    This change reduces unnecessary IPC traffic, improving performance
    for typical user runs.
    
    In addition, this change turns SendTypePredictionsToRenderer's vector
    argument into span.
    
    Low-Coverage-Reason: EXPERIMENTAL_CODE
    Bug: 40753022
    Change-Id: I356a321e94c96b8bfe37ec44bdf3dfde1ddbeaa6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925375
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Piotr Kotynia <piotrkotynia@google.com>
    Reviewed-by: Karol Sygiet <sygiet@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368140}

--- components/autofill/ios/browser/autofill_driver_ios.mm
+++ components/autofill/ios/browser/autofill_driver_ios.mm
@@ -240,7 +240,7 @@ void AutofillDriverIOS::ExtractForm(
 }
 
 void AutofillDriverIOS::SendTypePredictionsToRenderer(
-    const std::vector<raw_ptr<FormStructure, VectorExperimental>>& forms) {
+    const base::span<const raw_ptr<FormStructure, VectorExperimental>> forms) {
   std::vector<FormDataPredictions> preds =
       FormStructure::GetFieldTypePredictions(forms);
 
