d06f24e639eeafd152a1722f3db77588dc1e546d
Jacob Stanley <jacobstanley@google.com>
2024-10-14 18:00:19 +0000

    Create PS Activity Type Service Factory
    Bug: 341978070
    Change-Id: Icee5b60251e89cbaf870a6827d67e9bb3b376460
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914144
    Reviewed-by: Abe Boujane <boujane@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1368332}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -281,6 +281,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/fast_checkout/fast_checkout_capabilities_fetcher_factory.h"
 #include "chrome/browser/flags/android/chrome_feature_list.h"
 #include "chrome/browser/media/android/cdm/media_drm_origin_id_manager_factory.h"
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_activity_types_factory.h"
 #include "chrome/browser/search_resumption/start_suggest_service_factory.h"
 #include "chrome/browser/signin/signin_manager_android_factory.h"
 #include "components/commerce/core/commerce_feature_list.h"
@@ -1099,6 +1100,9 @@ void ChromeBrowserMainExtraPartsProfiles::
   PrivacySandboxSurveyFactory::GetInstance();
 #if !BUILDFLAG(IS_ANDROID)
   PrivacySandboxSurveyDesktopControllerFactory::GetInstance();
+#endif
+#if BUILDFLAG(IS_ANDROID)
+  PrivacySandboxActivityTypesFactory::GetInstance();
 #endif
   ProfileNetworkContextServiceFactory::GetInstance();
   ProfilePasswordStoreFactory::GetInstance();
