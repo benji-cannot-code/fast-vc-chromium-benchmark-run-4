d130fca0d1de611216b321eaf6d0799f84706ece
Jacob Stanley <jacobstanley@google.com>
2024-10-14 16:59:05 +0000

    Create skeleton for PS Activity Type Service
    Bug: 341978070
    Change-Id: Ifa84f5cf9f9c2469faf3adc82238cfbaf3df12f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914352
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368309}

---
+++ chrome/browser/privacy_sandbox/privacy_sandbox_activity_types_service.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/privacy_sandbox/privacy_sandbox_activity_types_service.h"
+
+#include "components/prefs/pref_service.h"
+
+namespace privacy_sandbox {
+
+PrivacySandboxActivityTypesService::PrivacySandboxActivityTypesService(
+    PrefService* pref_service)
+    : pref_service_(pref_service) {
+  CHECK(pref_service_);
+}
+
+PrivacySandboxActivityTypesService::~PrivacySandboxActivityTypesService() =
+    default;
+
+void PrivacySandboxActivityTypesService::Shutdown() {
+  pref_service_ = nullptr;
+}
+
+}  // namespace privacy_sandbox
---
+++ chrome/browser/privacy_sandbox/privacy_sandbox_activity_types_service.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ACTIVITY_TYPES_SERVICE_H_
+#define CHROME_BROWSER_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ACTIVITY_TYPES_SERVICE_H_
+
+#include "components/keyed_service/core/keyed_service.h"
+#include "components/prefs/pref_service.h"
+
+namespace privacy_sandbox {
+
+class PrivacySandboxActivityTypesService : public KeyedService {
+ public:
+  explicit PrivacySandboxActivityTypesService(PrefService* pref_service);
+  ~PrivacySandboxActivityTypesService() override;
+
+  // KeyedService:
+  void Shutdown() override;
+
+ private:
+  raw_ptr<PrefService> pref_service_;
+};
+
+}  // namespace privacy_sandbox
+#endif  // CHROME_BROWSER_PRIVACY_SANDBOX_PRIVACY_SANDBOX_ACTIVITY_TYPES_SERVICE_H_
