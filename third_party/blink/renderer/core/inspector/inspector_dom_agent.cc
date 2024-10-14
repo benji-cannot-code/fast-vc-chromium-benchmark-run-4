6391bbac8257506d2ecab9e1528930894497fb18
Ergün Erdoğmuş <ergunsh@chromium.org>
2024-10-14 13:07:30 +0000

    [ScrollBadge] Don't call IsUserScrollable if the layout box doesn't have a style ref
    Calling `IsUserScrollable` causes a crash when the `box` doesn't have a StyleRef. See the bug.
    
    Fixed: 372963693
    Change-Id: I719702b8112abc4385eb85e399ff28e28c7189b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928787
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368202}

--- third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
+++ third_party/blink/renderer/core/inspector/inspector_dom_agent.cc
@@ -2443,6 +2443,9 @@ void InspectorDOMAgent::DidInvalidateStyleAttr(Node* node) {
 
 bool InspectorDOMAgent::isNodeScrollable(Node* node) {
   if (auto* box = DynamicTo<LayoutBox>(node->GetLayoutObject())) {
+    if (!box->Style()) {
+      return false;
+    }
     return box->IsUserScrollable();
   }
   return false;
