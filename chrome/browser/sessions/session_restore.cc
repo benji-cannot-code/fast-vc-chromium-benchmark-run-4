e2c3ae53d5774b56cf9d976443da597470b1b153
David Pennington <dpenning@chromium.org>
2024-10-14 23:06:50 +0000

    [SavedTabGroups] prevent size 0 group from crashing on connect.
    It is possible for a group to become size 0 if 2 clients delete
    different tabs at the same time or in some other cases we consider
    unexpected behaviour, but not CHECK bugs. When a group is of size zero, we hide it from the UI, and dont allow it to be opened in the tabstrip.
    
    Session restore, is able to store groups out of band from the saved
    tab group sync service, and it will try to populate the tabstrip with
    a group, and then STGKS will then try to update it to be similar to the
    saved group during connect. If the group is of size 0, this would cause
    crashes like https://issues.chromium.org/issues/371607366
    
    To fix this, and also to provide the correct behaviour for restoring a
    session, if the group is of size 0, and still exists in the STGKS, we
    will allow session restore's tabs to repopulate the group with tabs.
    
    This means the group will appear to become saved again, and the user
    will then be able to decide whether to delete or keep the group.
    
    A test is added which demonstrates the fix across sessions.
    
    Bug: 371607366
    Change-Id: I34ae9fe219fa66ab84e032f176ab164f1096d873
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926123
    Commit-Queue: David Pennington <dpenning@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368490}

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -1005,7 +1005,9 @@ class SessionRestoreImpl : public BrowserListObserver {
                          std::optional<std::string> sync_id) {
     tab_groups::TabGroupSyncService* service =
         tab_groups::SavedTabGroupUtils::GetServiceForProfile(profile);
-    CHECK(service);
+    if (!service) {
+      return;
+    }
 
     if (sync_id) {
       const base::Uuid& sync_guid = base::Uuid::ParseLowercase(sync_id.value());
