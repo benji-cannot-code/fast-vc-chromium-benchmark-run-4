2bbd346b9d0885202eeb2b1918f580c9bde6d877
Duncan Mercer <mercerd@google.com>
2024-10-14 22:02:14 +0000

    [lensoverlay] Stop contextualizing all text queries
    Only flows coming from the contextual searchbox should be contextual. This
    CL changes to contextualize based on the searchbox being contextual
    instead of based on if bytes are present.
    
    Bug: 372954507
    Change-Id: I411bdecc069675505646e884324b5e2ef5ece29c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925459
    Commit-Queue: Duncan Mercer <mercerd@google.com>
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368441}

--- chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
+++ chrome/browser/ui/lens/lens_overlay_query_controller_unittest.cc
@@ -857,7 +857,7 @@ TEST_F(LensOverlayQueryControllerTest,
       std::vector<lens::mojom::CenterRotatedBoxPtr>(), fake_content_bytes,
       lens::PageContentMimeType::kPdf, 0);
   task_environment_.RunUntilIdle();
-  query_controller.SendTextOnlyQuery(
+  query_controller.SendContextualTextQuery(
       kTestQueryText, lens::LensOverlaySelectionType::MULTIMODAL_SEARCH,
       additional_search_query_params);
   task_environment_.RunUntilIdle();
@@ -885,7 +885,7 @@ TEST_F(LensOverlayQueryControllerTest,
   auto sent_interaction_request = query_controller.sent_interaction_request_;
   ASSERT_TRUE(interaction_data_response_future.IsReady());
   ASSERT_EQ(
-      sent_interaction_request.request_context().request_id().sequence_id(), 3);
+      sent_interaction_request.request_context().request_id().sequence_id(), 2);
   ASSERT_EQ(
       sent_interaction_request.interaction_request_metadata().type(),
       lens::LensOverlayInteractionRequestMetadata::CONTEXTUAL_SEARCH_QUERY);
@@ -954,7 +954,7 @@ TEST_F(LensOverlayQueryControllerTest,
       std::vector<lens::mojom::CenterRotatedBoxPtr>(), fake_content_bytes,
       lens::PageContentMimeType::kHtml, 0);
   task_environment_.RunUntilIdle();
-  query_controller.SendTextOnlyQuery(
+  query_controller.SendContextualTextQuery(
       kTestQueryText, lens::LensOverlaySelectionType::MULTIMODAL_SEARCH,
       additional_search_query_params);
   task_environment_.RunUntilIdle();
@@ -982,7 +982,7 @@ TEST_F(LensOverlayQueryControllerTest,
   auto sent_interaction_request = query_controller.sent_interaction_request_;
   ASSERT_TRUE(interaction_data_response_future.IsReady());
   ASSERT_EQ(
-      sent_interaction_request.request_context().request_id().sequence_id(), 3);
+      sent_interaction_request.request_context().request_id().sequence_id(), 2);
   ASSERT_EQ(
       sent_interaction_request.interaction_request_metadata().type(),
       lens::LensOverlayInteractionRequestMetadata::CONTEXTUAL_SEARCH_QUERY);
@@ -1051,7 +1051,7 @@ TEST_F(LensOverlayQueryControllerTest,
       std::vector<lens::mojom::CenterRotatedBoxPtr>(), fake_content_bytes,
       lens::PageContentMimeType::kPlainText, 0);
   task_environment_.RunUntilIdle();
-  query_controller.SendTextOnlyQuery(
+  query_controller.SendContextualTextQuery(
       kTestQueryText, lens::LensOverlaySelectionType::MULTIMODAL_SEARCH,
       additional_search_query_params);
   task_environment_.RunUntilIdle();
@@ -1079,7 +1079,7 @@ TEST_F(LensOverlayQueryControllerTest,
   auto sent_interaction_request = query_controller.sent_interaction_request_;
   ASSERT_TRUE(interaction_data_response_future.IsReady());
   ASSERT_EQ(
-      sent_interaction_request.request_context().request_id().sequence_id(), 3);
+      sent_interaction_request.request_context().request_id().sequence_id(), 2);
   ASSERT_EQ(
       sent_interaction_request.interaction_request_metadata().type(),
       lens::LensOverlayInteractionRequestMetadata::CONTEXTUAL_SEARCH_QUERY);
