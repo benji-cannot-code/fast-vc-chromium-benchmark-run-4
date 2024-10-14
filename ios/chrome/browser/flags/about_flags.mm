6fc52f3bd5633f2296b279d30caf1ca62a5ce14b
Gauthier Ambard <gambard@chromium.org>
2024-10-14 11:12:41 +0000

    [iOS] Update flags for TabStrip experiments
    Make sure to use the right flags.
    
    Fixed: 372224636
    Change-Id: I7ee433d613c32ba0f55056eb5235900744c8a347
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928904
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Ewann Pellé <ewannpv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368167}

--- ios/chrome/browser/flags/about_flags.mm
+++ ios/chrome/browser/flags/about_flags.mm
@@ -599,12 +599,14 @@ const FeatureEntry::FeatureParam kModernTabStripBlackBackgroundOption[] = {
 const FeatureEntry::FeatureParam kModernTabStripCloseButtonVisibleOption[] = {
     {kModernTabStripCloseButtonsVisible, "true"},
     {kModernTabStripCloserNTB, "true"},
+    {kModernTabStripHighContrastNTB, "true"},
     {kModernTabStripBiggerNTB, "true"}};
 const FeatureEntry::FeatureParam
     kModernTabStripInactiveTabsHighContrastOption[] = {
         {kModernTabStripInactiveTabsHighContrast, "true"},
         {kModernTabStripCloserNTB, "true"},
         {kModernTabStripDarkerBackgroundV3, "true"},
+        {kModernTabStripHighContrastNTB, "true"},
         {kModernTabStripBiggerNTB, "true"}};
 const FeatureEntry::FeatureParam kModernTabStripHighContrastNTBOption[] = {
     {kModernTabStripHighContrastNTB, "true"},
@@ -623,6 +625,7 @@ const FeatureEntry::FeatureParam kModernTabStripDetachedTabsOptions[] = {
     {kModernTabStripCloseButtonsVisible, "true"},
     {kModernTabStripHighContrastNTB, "true"},
     {kModernTabStripInactiveTabsHighContrast, "true"},
+    {kModernTabStripDarkerBackgroundV3, "true"},
     {kModernTabStripDetachedTabs, "true"},
     {kModernTabStripCloserNTB, "true"},
     {kModernTabStripNTBNoBackground, "true"},
