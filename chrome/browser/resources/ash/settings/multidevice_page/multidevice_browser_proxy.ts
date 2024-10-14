32791f965bb820b1637f5442259cf731c645a45a
Wes Okuhara <wesokuhara@google.com>
2024-10-14 17:16:01 +0000

    CrOS Settings: Fix consistent-type-import ESLint rule for multidevice
    Bug: b:372733928
    Test: git cl presubmit --force
    Change-Id: I0a1cf2aac931e79d1fb3960d079b24e3b01b1cb7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926118
    Auto-Submit: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368317}

--- chrome/browser/resources/ash/settings/multidevice_page/multidevice_browser_proxy.ts
+++ chrome/browser/resources/ash/settings/multidevice_page/multidevice_browser_proxy.ts
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import {sendWithPromise} from 'chrome://resources/js/cr.js';
 
-import {MultiDeviceFeature, MultiDevicePageContentData, PhoneHubPermissionsSetupAction, PhoneHubPermissionsSetupFeatureCombination, PhoneHubPermissionsSetupFlowScreens} from './multidevice_constants.js';
+import type {MultiDeviceFeature, MultiDevicePageContentData, PhoneHubPermissionsSetupAction, PhoneHubPermissionsSetupFeatureCombination, PhoneHubPermissionsSetupFlowScreens} from './multidevice_constants.js';
 
 
 export interface MultiDeviceBrowserProxy {
--- chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_toggle.ts
+++ chrome/browser/resources/ash/settings/multidevice_page/multidevice_feature_toggle.ts
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import 'chrome://resources/ash/common/cr_elements/cr_toggle/cr_toggle.js';
 
-import {CrToggleElement} from 'chrome://resources/ash/common/cr_elements/cr_toggle/cr_toggle.js';
+import type {CrToggleElement} from 'chrome://resources/ash/common/cr_elements/cr_toggle/cr_toggle.js';
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {MultiDeviceFeature, MultiDeviceFeatureState} from './multidevice_constants.js';
