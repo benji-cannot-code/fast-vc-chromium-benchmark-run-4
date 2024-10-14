a50c41704db67ef1693679097927d20cde38da04
Lei Zhang <thestig@chromium.org>
2024-10-14 23:04:07 +0000

    Remove unused MimeHandler code from PDFExtensionTestBase
    LoadPdfGetMimeHandlerView() and LoadPdfInNewTabGetMimeHandlerView() have
    no callers now, so delete them. Also simplify / remove some
    GetOnlyMimeHandlerView() references along the way.
    
    Change-Id: If7e6676b87b863397118c2048c9cb3c177bf3410
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926752
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368485}

--- chrome/browser/pdf/pdf_extension_interactive_uitest.cc
+++ chrome/browser/pdf/pdf_extension_interactive_uitest.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 using ::pdf_extension_test_util::ConvertPageCoordToScreenCoord;
-using ::pdf_extension_test_util::GetOnlyMimeHandlerView;
 using ::pdf_extension_test_util::SetInputFocusOnPlugin;
 
 class PDFExtensionInteractiveUITest : public base::test::WithFeatureOverride,
