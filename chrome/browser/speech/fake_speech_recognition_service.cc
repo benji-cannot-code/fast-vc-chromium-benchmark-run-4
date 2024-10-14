d2e401bb31a8577cb04bc325a8cee9dcec168e08
Avynn Donaghe <avynn@google.com>
2024-10-14 21:16:24 +0000

    [BabelOrca] Reland SystemLiveCaptionChanges
    Relands just the system live caption changes from:
    https://chromium-review.googlesource.com/c/chromium/src/+/5818342
    
    The logic here is essentially identical to the previous CL in
    regards to the SystemLiveCaption changes.
    
    AX-Relnotes: N/A
    Design: go/babel-orca-mic-integration
    Bug: b/365820466
    Change-Id: Ic235306570393793db6b7428304de82b52585d74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924383
    Reviewed-by: Rob Schonberger <robsc@chromium.org>
    Commit-Queue: Avynn Donaghe <avynn@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368429}

--- chrome/browser/speech/fake_speech_recognition_service.cc
+++ chrome/browser/speech/fake_speech_recognition_service.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace speech {
 
 FakeSpeechRecognitionService::FakeSpeechRecognitionService() = default;
-
 FakeSpeechRecognitionService::~FakeSpeechRecognitionService() = default;
 
 void FakeSpeechRecognitionService::BindAudioSourceSpeechRecognitionContext(
