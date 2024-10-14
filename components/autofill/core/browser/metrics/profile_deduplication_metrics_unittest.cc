1bf33887a5eb847eeb5dd9719ea20c2ec2d907a3
Karol Sygiet <sygiet@google.com>
2024-10-14 13:18:34 +0000

    Split deduplication metrics calculation into batches
    This CL splits the metric calculation into profiles.size() batches with O(profiles.size()) complexity each. After each batch is completed
    there is a 2 seconds delay before the next one starts.
    
    Additionally this CL merges two flags related to recording
    deduplication metrics into one, for simpler rollout.
    
    Bug: 325452461
    Change-Id: Iad8e260a84d716771bbf873788c90c78628fe3c0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921738
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368207}

--- components/autofill/core/browser/metrics/profile_deduplication_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/profile_deduplication_metrics_unittest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/metrics/profile_deduplication_metrics.h"
 
 #include "base/test/metrics/histogram_tester.h"
+#include "base/test/task_environment.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/data_model/autofill_profile.h"
 #include "components/autofill/core/browser/field_types.h"
@@ -25,10 +26,12 @@ class ProfileDeduplicationMetricsTest : public testing::Test {
  public:
   ProfileDeduplicationMetricsTest() = default;
 
+  void FastForward() { task_environment.FastForwardBy(base::Seconds(10)); }
+
  protected:
+  base::test::SingleThreadTaskEnvironment task_environment{
+      base::test::TaskEnvironment::TimeSource::MOCK_TIME};
   base::HistogramTester histogram_tester_;
-  base::test::ScopedFeatureList scoped_feature_list_{
-      features::kAutofillLogDeduplicationMetricsFollowup};
 };
 
 TEST_F(ProfileDeduplicationMetricsTest,
@@ -38,9 +41,11 @@ TEST_F(ProfileDeduplicationMetricsTest,
   AutofillProfile b = test::GetFullProfile();
   b.SetRawInfo(COMPANY_NAME, u"different company");
   b.SetRawInfo(EMAIL_ADDRESS, u"different-email@gmail.com");
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
 
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
+
   // The same sample is emitted once for each profile.
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Deduplication.ExistingProfiles."
@@ -52,7 +57,7 @@ TEST_F(ProfileDeduplicationMetricsTest,
 TEST_F(ProfileDeduplicationMetricsTest,
        Startup_RankOfStoredQuasiDuplicateProfiles_NoProfiles) {
   AutofillProfile a = test::GetFullProfile();
-  const std::vector<const AutofillProfile*> profiles = {&a};
+  std::vector<AutofillProfile> profiles = {a};
 
   LogDeduplicationStartupMetrics(profiles, kLocale);
   histogram_tester_.ExpectTotalCount(
@@ -64,9 +69,11 @@ TEST_F(ProfileDeduplicationMetricsTest,
 TEST_F(ProfileDeduplicationMetricsTest,
        Startup_PercentageOfNonQuasiDuplicates_NotEnoughProfiles) {
   AutofillProfile a = test::GetFullProfile();
-  const std::vector<const AutofillProfile*> profiles = {&a};
+  std::vector<AutofillProfile> profiles = {a};
 
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
+
   histogram_tester_.ExpectTotalCount(
       "Autofill.Deduplication.ExistingProfiles."
       "PercentageOfNonQuasiDuplicates.1",
@@ -100,9 +107,11 @@ TEST_F(ProfileDeduplicationMetricsTest,
   AutofillProfile d = test::GetFullProfile();
   d.SetRawInfo(COMPANY_NAME, u"testing company");
   d.SetRawInfo(EMAIL_ADDRESS, u"test-email@gmail.com");
-  const std::vector<const AutofillProfile*> profiles = {&a, &b, &c, &d};
+  std::vector<AutofillProfile> profiles = {a, b, c, d};
 
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
+
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Deduplication.ExistingProfiles.PercentageOfNonQuasiDuplicates."
       "1",
@@ -130,8 +139,9 @@ TEST_F(ProfileDeduplicationMetricsTest, Startup_EditDistance) {
   a.SetRawInfo(COMPANY_NAME, u"A company");
   AutofillProfile b = test::GetFullProfile();
   b.SetRawInfo(COMPANY_NAME, u"B company");
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
 
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Deduplication.ExistingProfiles."
@@ -145,8 +155,9 @@ TEST_F(ProfileDeduplicationMetricsTest, Startup_EditDistanceNormalized) {
   AutofillProfile b = test::GetFullProfile();
   // The normalized distance should be 2 as only the suffix is different.
   b.SetRawInfo(COMPANY_NAME, u"jean francoiska");
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
 
   histogram_tester_.ExpectUniqueSample(
       "Autofill.Deduplication.ExistingProfiles."
@@ -161,9 +172,11 @@ TEST_F(ProfileDeduplicationMetricsTest, Startup_TypeOfQuasiDuplicateToken) {
   b.SetRawInfo(COMPANY_NAME, u"different company");
   AutofillProfile c = test::GetFullProfile();
   c.SetRawInfo(EMAIL_ADDRESS, u"different-email@gmail.com");
-  const std::vector<const AutofillProfile*> profiles = {&a, &b, &c};
+  std::vector<AutofillProfile> profiles = {a, b, c};
 
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
+
   // Expect two samples for `kCompany` and `kEmailAddress`, since:
   // - `kCompany` and `kEmailAddress` are each emitted once by `a`.
   // - `kCompany` is emitted once by `b`.
@@ -202,8 +215,9 @@ TEST_F(ProfileDeduplicationMetricsTest,
       .AddObservation(COMPANY_NAME, ProfileTokenQuality::ObservationType::
                                         kEditedToDifferentTokenOfSameProfile);
 
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
 
   // Lower score = -2 + 10(offset) => 8(coming from the profile b) should be
   // recorded.
@@ -239,8 +253,9 @@ TEST_F(ProfileDeduplicationMetricsTest,
       .AddObservation(COMPANY_NAME, ProfileTokenQuality::ObservationType::
                                         kEditedToDifferentTokenOfSameProfile);
 
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
 
   // Lower score = 1 + 10(offset) => 11 (coming from the profile a) should be
   // recorded.
@@ -259,8 +274,9 @@ TEST_F(ProfileDeduplicationMetricsTest, Startup_QuasiDuplicateAdoption) {
   a.set_use_count(50);
   b.set_use_count(100);
 
-  const std::vector<const AutofillProfile*> profiles = {&a, &b};
+  std::vector<AutofillProfile> profiles = {a, b};
   LogDeduplicationStartupMetrics(profiles, kLocale);
+  FastForward();
 
   // Lower score is equal 50, the total count is 150, but all entries are capped
   // at 99. The score is shifted 8 bits left to create space for the total use
