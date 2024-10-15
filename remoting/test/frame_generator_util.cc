f06720877afda2e68b32929c11b61ca3d6c2acbd
Avi Drissman <avi@chromium.org>
2024-10-15 22:03:40 +0000

    Modernize codec usage in remoting/
    Codec calls have new API that uses std::optional and spans. Switch usage
    to those new APIs, and lightly modernize the calling code.
    
    Bug: 370696612
    Change-Id: I47d63a1ba511b1bdc9e6813c9b7a2f9eb0f9588f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932313
    Commit-Queue: Lambros Lambrou <lambroslambrou@chromium.org>
    Reviewed-by: Lambros Lambrou <lambroslambrou@chromium.org>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369055}

--- remoting/test/frame_generator_util.cc
+++ remoting/test/frame_generator_util.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/test/frame_generator_util.h"
 
 #include "base/base_paths.h"
+#include "base/containers/span.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/path_service.h"
@@ -37,8 +38,7 @@ void CopyPixelsToBuffer(const SkBitmap& src,
 }
 }  // namespace
 
-namespace remoting {
-namespace test {
+namespace remoting::test {
 
 std::unique_ptr<webrtc::DesktopFrame> LoadDesktopFrameFromPng(
     const char* name) {
@@ -54,9 +54,8 @@ std::unique_ptr<webrtc::DesktopFrame> LoadDesktopFrameFromPng(
     LOG(FATAL) << "Failed to read " << file_path.MaybeAsASCII()
                << ". Please run remoting/test/data/download.sh";
   }
-  SkBitmap bitmap;
-  gfx::PNGCodec::Decode(reinterpret_cast<const uint8_t*>(file_content.data()),
-                        file_content.size(), &bitmap);
+  SkBitmap bitmap = gfx::PNGCodec::Decode(base::as_byte_span(file_content));
+  CHECK(!bitmap.isNull());
   std::unique_ptr<webrtc::DesktopFrame> frame(new webrtc::BasicDesktopFrame(
       webrtc::DesktopSize(bitmap.width(), bitmap.height())));
   CopyPixelsToBuffer(bitmap, frame->data(), frame->stride());
@@ -75,5 +74,4 @@ void DrawRect(webrtc::DesktopFrame* frame,
   }
 }
 
-}  // namespace test
-}  // namespace remoting
+}  // namespace remoting::test
