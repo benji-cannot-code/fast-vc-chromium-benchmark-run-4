1570d602950b910c8dac2a2918f516ed46316241
EmiliaPaz <emiliapaz@chromium.org>
2024-10-15 20:58:50 +0000

    [Extensions c2s] Remove MessageSection and add sections directly
    Latest UI mocks for the extensions menu under
    ExtensionsMenuAccessControl feature flag have changed some of the controls content and visibility based on the state:
      (a) No longer show a message for restricted sites, policy blocked
          sites and user blocked sites (unless a reload is needed)
      (b) Reload message is not dependent on the state. If shown, it will
          always have the same text
    
    Therefore, this CL introduces the following changes:
      (1) Remove MessageSection. MessageSection was introduced because it
          had 6 different states represented in 3 different "container"
          types. Now it only requires 2 states so there is no need to have
          an extra view.
      (2) Instead, we can have a SectionContainer that is a base class
          for a section inside the extensions menu. This container is used
          by the new two "messages" and the existent extension menu items
      (3) Add the two "message section" directly to the menu, which are
          named 'reload section' and 'requests section'
      (4) Update the test accordingly
    
    Functionality changed
      - Restricted sites, policy blocked sites and user blocked sites no
        longer have a message. Tests verify reload and requests section
        are hidden in those cases
    
    Functionality retained
      - Reload messages have the same triggers, but now their content is
        always the same
      - Requests are triggered and displayed in the same way
    
    Screencast:
    Video follows various states
      1. Extension added a request: request section is visible
      2. User blocked extensions: sections are hidden
      3. User allows extensions: reload section is visible (page needs a
         refresh for changes to be visible)
      4. After page refresh: request section is visible (request is still
         active)
    https://drive.google.com/file/d/1ysm9oaUQyr5DtwCdtBrNqHMUifdhP_GS/view?usp=sharing
    
    Bug: 40879945
    Change-Id: I82a1116aac2726246a779fc399216f32a7764465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923548
    Reviewed-by: Darryl James <dljames@chromium.org>
    Commit-Queue: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369015}

--- chrome/browser/ui/views/extensions/extensions_menu_view_controller.cc
+++ chrome/browser/ui/views/extensions/extensions_menu_view_controller.cc
@@ -893,12 +893,9 @@ void ExtensionsMenuViewController::OnSiteAccessRequestAdded(
     return;
   }
 
-  // Site access requests only affect the 'user customized access' section in
-  // the main page.
+  // Site access requests only affect the main page.
   ExtensionsMenuMainPageView* main_page = GetMainPage(current_page_.view());
-  if (!main_page || main_page->GetMessageSectionState() !=
-                        ExtensionsMenuMainPageView::MessageSectionState::
-                            kUserCustomizedAccess) {
+  if (!main_page) {
     return;
   }
 
@@ -926,12 +923,9 @@ void ExtensionsMenuViewController::OnSiteAccessRequestUpdated(
     return;
   }
 
-  // Site access requests only affect the 'user customized access' section in
-  // the main page.
+  // Site access requests only affect the main page.
   ExtensionsMenuMainPageView* main_page = GetMainPage(current_page_.view());
-  if (!main_page || main_page->GetMessageSectionState() !=
-                        ExtensionsMenuMainPageView::MessageSectionState::
-                            kUserCustomizedAccess) {
+  if (!main_page) {
     return;
   }
 
@@ -963,12 +957,9 @@ void ExtensionsMenuViewController::OnSiteAccessRequestRemoved(
     return;
   }
 
-  // Site access requests only affect the 'user customized access' section in
-  // the main page.
+  // Site access requests only affect the main page.
   ExtensionsMenuMainPageView* main_page = GetMainPage(current_page_.view());
-  if (!main_page || main_page->GetMessageSectionState() !=
-                        ExtensionsMenuMainPageView::MessageSectionState::
-                            kUserCustomizedAccess) {
+  if (!main_page) {
     return;
   }
 
@@ -988,9 +979,7 @@ void ExtensionsMenuViewController::OnSiteAccessRequestsCleared(int tab_id) {
   // Site access requests only affect the 'user customized access' section in
   // the main page.
   ExtensionsMenuMainPageView* main_page = GetMainPage(current_page_.view());
-  if (!main_page || main_page->GetMessageSectionState() !=
-                        ExtensionsMenuMainPageView::MessageSectionState::
-                            kUserCustomizedAccess) {
+  if (!main_page) {
     return;
   }
 
