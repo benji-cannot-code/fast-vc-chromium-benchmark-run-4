f9b3848e03e53b5855ec338ba53447eaad3f9009
mlcui <mlcui@google.com>
2024-10-15 10:26:47 +0000

    scanner: Take actions in by value instead of const ref
    These are already passed by move in the view model's `ToCallback`.
    This allows us to avoid some unnecessary string copies.
    
    Bug: b:363101106
    Change-Id: I6a6a2100aa593fc73c07ca5c654605b86bb3b3ef
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5915157
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368686}

--- ash/scanner/scanner_action_handler.h
+++ ash/scanner/scanner_action_handler.h
@@ -36,7 +36,7 @@ using ScannerCommandCallback = base::OnceCallback<void(bool success)>;
 // false.
 ASH_EXPORT void HandleScannerAction(
     base::WeakPtr<ScannerCommandDelegate> delegate,
-    const ScannerAction& action,
+    ScannerAction action,
     ScannerCommandCallback callback);
 
 }  // namespace ash
