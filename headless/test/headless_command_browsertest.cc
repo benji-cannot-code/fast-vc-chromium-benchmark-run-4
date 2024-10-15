9b32538c167dc0b1b06d6ff0748eb4e296aa8b6b
Avi Drissman <avi@chromium.org>
2024-10-15 22:15:35 +0000

    Modernize codec usage in headless
    Codec calls have new API that uses std::optional and spans. Switch usage
    to those new APIs, and lightly modernize the calling code.
    
    Bug: 370696612
    Change-Id: Ie86a0fd7cd259e31f4bc92e9f11e6f4e63fec2c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933248
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369064}

--- headless/test/headless_command_browsertest.cc
+++ headless/test/headless_command_browsertest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/files/scoped_temp_dir.h"
@@ -60,16 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 namespace headless {
-
-namespace {
-
-bool DecodePNG(const std::string& png_data, SkBitmap* bitmap) {
-  return gfx::PNGCodec::Decode(
-      reinterpret_cast<const unsigned char*>(png_data.data()), png_data.size(),
-      bitmap);
-}
-
-}  // namespace
 
 class HeadlessCommandBrowserTest : public HeadlessBrowserTest,
                                    public content::WebContentsObserver {
@@ -411,8 +402,8 @@ IN_PROC_BROWSER_TEST_F(HeadlessScreenshotCommandBrowserTest, Screenshot) {
   ASSERT_TRUE(base::ReadFileToString(screenshot_filename_, &png_data))
       << screenshot_filename_;
 
-  SkBitmap bitmap;
-  ASSERT_TRUE(DecodePNG(png_data, &bitmap));
+  SkBitmap bitmap = gfx::PNGCodec::Decode(base::as_byte_span(png_data));
+  ASSERT_FALSE(bitmap.isNull());
 
   ASSERT_EQ(800, bitmap.width());
   ASSERT_EQ(600, bitmap.height());
@@ -449,8 +440,8 @@ IN_PROC_BROWSER_TEST_F(HeadlessScreenshotWithBackgroundCommandBrowserTest,
   ASSERT_TRUE(base::ReadFileToString(screenshot_filename_, &png_data))
       << screenshot_filename_;
 
-  SkBitmap bitmap;
-  ASSERT_TRUE(DecodePNG(png_data, &bitmap));
+  SkBitmap bitmap = gfx::PNGCodec::Decode(base::as_byte_span(png_data));
+  ASSERT_FALSE(bitmap.isNull());
 
   ASSERT_EQ(800, bitmap.width());
   ASSERT_EQ(600, bitmap.height());
@@ -489,8 +480,8 @@ IN_PROC_BROWSER_TEST_F(HeadlessScreenshotCommandBrowserTestWithWindowSize,
   ASSERT_TRUE(base::ReadFileToString(screenshot_filename_, &png_data))
       << screenshot_filename_;
 
-  SkBitmap bitmap;
-  ASSERT_TRUE(DecodePNG(png_data, &bitmap));
+  SkBitmap bitmap = gfx::PNGCodec::Decode(base::as_byte_span(png_data));
+  ASSERT_FALSE(bitmap.isNull());
 
   EXPECT_EQ(bitmap.width(), kWindowSize.width());
   EXPECT_EQ(bitmap.height(), kWindowSize.height());
