8cea6206f09c7c23a7f13504f4611bccdc5b6c9a
Henrique Nakashima <hnakashima@chromium.org>
2024-10-15 19:19:31 +0000

    [Android] Put CachedFlag overrides for test in FeatureList
    Before this CL, CachedFlag used ValuesReturned to override values for
    testing, which is a relic of how it worked before being made into a
    generic implementation (used to be each flag would re-implement the
    system).
    
    Now FeatureList's sTestFeatures holds the overrides for all types of
    flags.
    
    Bug: 40281605
    Change-Id: I472a6224772a949e55e25121d321a6a18b25f685
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5916527
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368933}

--- components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFlagUnitTest.java
+++ components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFlagUnitTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.components.cached_flags;
 
+import static org.mockito.Mockito.when;
+
 import static org.chromium.base.test.util.BaseFlagTestRule.A_OFF_B_OFF;
 import static org.chromium.base.test.util.BaseFlagTestRule.A_OFF_B_ON;
 import static org.chromium.base.test.util.BaseFlagTestRule.A_ON_B_OFF;
@@ -17,12 +19,17 @@ import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.FeatureList;
+import org.chromium.base.FeatureMap;
 import org.chromium.base.cached_flags.ValuesOverridden;
 import org.chromium.base.cached_flags.ValuesReturned;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.BaseFlagTestRule;
+import org.chromium.base.test.util.Features;
 
 import java.util.Arrays;
 
@@ -30,6 +37,9 @@ import java.util.Arrays;
 @RunWith(BaseRobolectricTestRunner.class)
 public class CachedFlagUnitTest {
     @Rule public final BaseFlagTestRule mBaseFlagTestRule = new BaseFlagTestRule();
+    @Rule public final MockitoRule mMockitoRule = MockitoJUnit.rule();
+
+    @Mock private FeatureMap mFeatureMap;
 
     @After
     public void tearDown() {
@@ -39,17 +49,18 @@ public class CachedFlagUnitTest {
 
     @Test(expected = AssertionError.class)
     public void testDuplicateFeature_throwsException() {
-        new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, true);
-        new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, true);
+        new CachedFlag(mFeatureMap, FEATURE_A, true);
+        new CachedFlag(mFeatureMap, FEATURE_A, true);
     }
 
     @Test
     public void testNativeInitialized_getsFromChromeFeatureList() {
-        CachedFlag featureA = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, false);
-        CachedFlag featureB = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_B, false);
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, false);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, false);
 
         // Cache native flags, meaning values from ChromeFeatureList should be used from now on.
-        FeatureList.setTestFeatures(A_OFF_B_ON);
+        when(mFeatureMap.isEnabledInNative(FEATURE_A)).thenReturn(false);
+        when(mFeatureMap.isEnabledInNative(FEATURE_B)).thenReturn(true);
         CachedFlagUtils.cacheNativeFlags(Arrays.asList(featureA, featureB));
 
         // Assert {@link CachedFeatureFlags} uses the values from {@link ChromeFeatureList}.
@@ -58,11 +69,12 @@ public class CachedFlagUnitTest {
 
     @Test
     public void testNativeNotInitializedNotCached_useDefault() {
-        CachedFlag featureA = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, true);
-        CachedFlag featureB = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_B, false);
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, true);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, false);
 
         // Do not cache values from native. There are no values stored in prefs either.
-        FeatureList.setTestFeatures(A_OFF_B_ON);
+        when(mFeatureMap.isEnabledInNative(FEATURE_A)).thenReturn(false);
+        when(mFeatureMap.isEnabledInNative(FEATURE_B)).thenReturn(true);
 
         // Query the flags to make sure the default values are returned.
         assertIsEnabledMatches(A_ON_B_OFF, featureA, featureB);
@@ -76,11 +88,12 @@ public class CachedFlagUnitTest {
 
     @Test
     public void testNativeNotInitializedPrefsCached_getsFromPrefs() {
-        CachedFlag featureA = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, false);
-        CachedFlag featureB = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_B, false);
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, false);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, false);
 
         // Cache native flags, meaning values from ChromeFeatureList should be used from now on.
-        FeatureList.setTestFeatures(A_OFF_B_ON);
+        when(mFeatureMap.isEnabledInNative(FEATURE_A)).thenReturn(false);
+        when(mFeatureMap.isEnabledInNative(FEATURE_B)).thenReturn(true);
         CachedFlagUtils.cacheNativeFlags(Arrays.asList(featureA, featureB));
         assertIsEnabledMatches(A_OFF_B_ON, featureA, featureB);
 
@@ -89,7 +102,8 @@ public class CachedFlagUnitTest {
         ValuesOverridden.removeOverrides();
 
         // Simulate ChromeFeatureList retrieving new, different values for the flags.
-        FeatureList.setTestFeatures(A_ON_B_ON);
+        when(mFeatureMap.isEnabledInNative(FEATURE_A)).thenReturn(true);
+        when(mFeatureMap.isEnabledInNative(FEATURE_B)).thenReturn(true);
 
         // Do not cache new values, but query the flags to make sure the values stored to prefs
         // are returned. Neither the defaults (false/false) or the ChromeFeatureList values
@@ -111,9 +125,32 @@ public class CachedFlagUnitTest {
     }
 
     @Test
+    @Features.EnableFeatures(FEATURE_A)
+    @Features.DisableFeatures(FEATURE_B)
+    public void testAnnotationOverride_returnsForcedValue() {
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, false);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, true);
+
+        // Verify that the forced value is returned.
+        assertIsEnabledMatches(A_ON_B_OFF, featureA, featureB);
+    }
+
+    @Test
+    public void testFeatureListOverride_returnsForcedValue() {
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, false);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, true);
+
+        // Force different values
+        FeatureList.setTestFeatures(A_ON_B_OFF);
+
+        // Verify that the forced value is returned.
+        assertIsEnabledMatches(A_ON_B_OFF, featureA, featureB);
+    }
+
+    @Test
     public void testSetForTesting_returnsForcedValue() {
-        CachedFlag featureA = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_A, false);
-        CachedFlag featureB = new CachedFlag(BaseFlagTestRule.FEATURE_MAP, FEATURE_B, false);
+        CachedFlag featureA = new CachedFlag(mFeatureMap, FEATURE_A, false);
+        CachedFlag featureB = new CachedFlag(mFeatureMap, FEATURE_B, false);
 
         // Do not cache values from native. There are no values stored in prefs either.
         // Query the flags to make sure the default values are returned.
@@ -126,7 +163,7 @@ public class CachedFlagUnitTest {
         assertIsEnabledMatches(A_ON_B_OFF, featureA, featureB);
 
         // Remove the forcing.
-        featureA.setForTesting(null);
+        FeatureList.setTestValues(null);
 
         // Verify that the forced value is not returned anymore.
         assertIsEnabledMatches(A_OFF_B_OFF, featureA, featureB);
