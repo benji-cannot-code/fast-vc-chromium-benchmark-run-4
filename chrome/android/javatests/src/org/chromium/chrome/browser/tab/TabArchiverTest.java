9f96157d293ed957d28026743c1de8ec524a889a
Brandon Wylie <wylieb@chromium.org>
2024-10-15 19:40:45 +0000

    [declutter] Prevent NPE when first creating ArchivePersistedTabData
    The returns value of the callback can be null in some cases. This is
    causing NPE for some clients on stable.
    
    Bug: 371005041
    Change-Id: Ib9341c307a0201a431c410a0c01776e49ca0a716
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932399
    Reviewed-by: Sky Malice <skym@chromium.org>
    Auto-Submit: Brandon Wylie <wylieb@google.com>
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Commit-Queue: David Maunder <davidjm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368955}

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/TabArchiverTest.java
@@ -80,6 +80,7 @@ public class TabArchiverTest {
     private @Mock Clock mClock;
     private @Mock TabModelSelector mSelector;
     private @Mock TabWindowManager mTabWindowManager;
+    private @Mock Tab mTab;
 
     private ArchivedTabModelOrchestrator mArchivedTabModelOrchestrator;
     private TabArchiver mTabArchiver;
@@ -502,5 +503,25 @@ public class TabArchiverTest {
         CriteriaHelper.pollUiThread(() -> 1 == mRegularTabModel.getCount());
         assertEquals(1, mArchivedTabModel.getCount());
         watcher.assertExpected();
+    }
+
+    @Test
+    @MediumTest
+    public void testPersistedTabDataNull() throws Exception {
+        doReturn(false).when(mTab).isInitialized();
+        // This shouldn't NPE when the persisted tab data is null.
+        runOnUiThreadBlocking(() -> mTabArchiver.initializePersistedTabData(mTab));
+
+        CallbackHelper callbackHelper = new CallbackHelper();
+        runOnUiThreadBlocking(
+                () -> {
+                    ArchivePersistedTabData.from(
+                            mTab,
+                            (tabPersistedData) -> {
+                                assertNull(tabPersistedData);
+                                callbackHelper.notifyCalled();
+                            });
+                });
+        callbackHelper.waitForNext();
     }
 }
