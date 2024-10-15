2c3b9322c0e585d5a6115e64a8dc5d55f005fa75
Georg Neis <neis@chromium.org>
2024-10-15 02:01:55 +0000

    app_service: Remove some Lacros code related to app_install
    - Remove Lacros-specific rules from
      chrome/browser/apps/app_service/app_install/BUILD.gn
    - Delete the associated Lacros-only files
    - Remove Lacros-specific code from a browser test and prepare it
      for the removal of the crosapi TestController
    
    Bug: b:369519665, b:354842935, b:365741912
    Change-Id: Ic715358112cfc9d780b8263e6f7a51c5ab994e23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922914
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368553}

--- chrome/browser/apps/app_service/app_install/test_app_install_server.cc
+++ chrome/browser/apps/app_service/app_install/test_app_install_server.cc
@@ -5,10 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/apps/app_service/app_install/test_app_install_server.h"
 
+#include "chrome/browser/apps/almanac_api_client/almanac_api_util.h"
 #include "chrome/browser/apps/app_service/app_install/app_install.pb.h"
-#include "chrome/browser/chromeos/crosapi/test_util.h"
 #include "chrome/browser/web_applications/web_app_helpers.h"
-#include "chromeos/crosapi/mojom/test_controller.mojom-test-utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace apps {
@@ -27,10 +26,7 @@ bool TestAppInstallServer::SetUp() {
   }
 
   std::string server_url = server_.GetURL("/").spec();
-
-  crosapi::mojom::TestControllerAsyncWaiter(crosapi::GetTestController())
-      .SetAlmanacEndpointUrlForTesting(server_url);
-
+  apps::SetAlmanacEndpointUrlForTesting(server_url);
   return true;
 }
 
