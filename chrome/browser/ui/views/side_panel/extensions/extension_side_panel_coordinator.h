ed0cd6dc24e8d2985b5a98e4ddc1b8edcc540bef
Erik Chen <erikchen@chromium.org>
2024-10-15 22:02:41 +0000

    Reduce fragility in SidePanelCoordinator (part 15 / N)
    This CL simplifies registration and deregistration of side panel
    entries. The SidePanelRegistry now has logic that checks if an entry
    that is about to be deregistered is showing. If so, it synchronously
    closes the side panel.
    
    All other logic coupling registration/deregistration with
    showing/closing the side panel was removed. This logic was spread
    across several files but was only intended for extensions.
    
    User visible side-effects:
    * If an active tab-scoped entry is deregistered, the side panel will
      close as opposed to falling back to the window-scoped side panel. This
      change intentionally makes the deregister flow identical to the close
      flow.
    * Registering a tab-scoped entry will have no effect on the currently
      showing side panel. Previously, if a window-scoped entry with the same
      key was showing, the side panel would switch to the tab-scoped
      entry.
    * Disabling the tab-scoped side panel for a tab no longer prevents the
      window-scoped side panel for showing on that tab.
    
    Non user-visible side effects:
    * Many tests are no longer necessary as the new behavior is simpler.
    * The mechanism to get the cached view from a deregistered entry is no
      longer necessary.
    * The logic that dynamically registered/deregistered the extension
      window-scoped side-panel based on active tab is no longer necessary.
    
    Bug: 363743081
    Change-Id: I891e7d3b82281516b5ffd3f340c12e81815c0af6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5901055
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Caroline Rising <corising@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369054}

--- chrome/browser/ui/views/side_panel/extensions/extension_side_panel_coordinator.h
+++ chrome/browser/ui/views/side_panel/extensions/extension_side_panel_coordinator.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/side_panel/side_panel_service.h"
 #include "chrome/browser/extensions/extension_view_host.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/extensions/extension_view_views.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_entry.h"
 #include "chrome/browser/ui/views/side_panel/side_panel_view_state_observer.h"
@@ -40,8 +39,7 @@ class Extension;
 // contextual extension side panels given the difference in behavior betweeen
 // these two panel types.
 class ExtensionSidePanelCoordinator : public ExtensionViewViews::Observer,
-                                      public SidePanelService::Observer,
-                                      public TabStripModelObserver {
+                                      public SidePanelService::Observer {
  public:
   explicit ExtensionSidePanelCoordinator(Profile* profile,
                                          Browser* browser,
@@ -68,14 +66,6 @@ class ExtensionSidePanelCoordinator : public ExtensionViewViews::Observer,
 
   bool IsGlobalCoordinator() const;
 
-  // Returns if this extension's side panel is explicitly disabled for the given
-  // `tab_id`.
-  bool IsDisabledForTab(int tab_id) const;
-
-  // Deregisters this extension's SidePanelEntry from the global
-  // SidePanelRegistry and caches the entry's view into `global_entry_view_`.
-  void DeregisterGlobalEntryAndCacheView();
-
   // SidePanelService::Observer:
   void OnPanelOptionsChanged(
       const ExtensionId& extension_id,
@@ -85,15 +75,8 @@ class ExtensionSidePanelCoordinator : public ExtensionViewViews::Observer,
   // ExtensionViewViews::Observer
   void OnViewDestroying() override;
 
-  // TabStripModelObserver:
-  void OnTabStripModelChanged(
-      TabStripModel* tab_strip_model,
-      const TabStripModelChange& change,
-      const TabStripSelectionChange& selection) override;
-
   // Creates and registers the SidePanelEntry for this extension, and observes
-  // the entry. This is called if the extension has a default side panel path
-  // when the browser view is created or when the extension is loaded.
+  // the entry.
   void CreateAndRegisterEntry();
 
   // Creates a view for the extension's resource URL. This is called when this
@@ -156,10 +139,6 @@ class ExtensionSidePanelCoordinator : public ExtensionViewViews::Observer,
 
   // The extension's own icon for its side panel entry.
   std::unique_ptr<IconImage> extension_icon_;
-
-  // Cached view for global entry if it was disabled for a specific tab and may
-  // be shown again on a different tab where it's enabled.
-  std::unique_ptr<views::View> global_entry_view_;
 
   // Whether this coordinator is tab-scoped or window-scoped.
   const bool for_tab_;
