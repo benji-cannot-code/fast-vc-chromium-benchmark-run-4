3bf54ae774706987522ef0e89fc03a4a60ea2461
Shibalik Mohapatra <shibalik@chromium.org>
2024-10-15 20:56:51 +0000

    [TabDeclutter] Listen to events of stale tabs.
    After a tab is stale, it can become non-stale due to getting pinned, grouped, getting closed or activation. Nothing tracks this today apart from a 10 min timer. This would cause tabs that are not stale show up as stale intermittently in the tab search WebUI.
    
    This also fixes issues where action is taken from the page handler causing changes in the tabstrip as the process will get propogated without waiting for the timer.
    
    Bug: 370804939
    Change-Id: I664a9c57e6a9208484361858d9d35995a6c4ccba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5913984
    Reviewed-by: Emily Shack <emshack@chromium.org>
    Commit-Queue: Shibalik Mohapatra <shibalik@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369014}

--- chrome/browser/ui/tabs/tab_group_tab_collection.cc
+++ chrome/browser/ui/tabs/tab_group_tab_collection.cc
@@ -25,7 +25,7 @@ void TabGroupTabCollection::AddTab(std::unique_ptr<TabModel> tab_model,
   CHECK(tab_model);
 
   TabModel* inserted_tab_model = impl_->AddTab(std::move(tab_model), index);
-  inserted_tab_model->set_group(/*group=*/group_id_);
+  inserted_tab_model->SetGroup(/*group=*/group_id_);
   inserted_tab_model->OnReparented(this, GetPassKey());
 }
 
@@ -82,7 +82,7 @@ std::unique_ptr<TabModel> TabGroupTabCollection::MaybeRemoveTab(
   CHECK(tab_model);
 
   std::unique_ptr<TabModel> removed_tab_model = impl_->RemoveTab(tab_model);
-  removed_tab_model->set_group(/*group=*/std::nullopt);
+  removed_tab_model->SetGroup(/*group=*/std::nullopt);
   removed_tab_model->OnReparented(nullptr, GetPassKey());
   return removed_tab_model;
 }
