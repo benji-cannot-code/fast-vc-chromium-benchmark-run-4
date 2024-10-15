0f690b111de71265b139eb330f7e84ed69ec3710
Avi Drissman <avi@chromium.org>
2024-10-15 22:40:10 +0000

    Modernize codec usage in extensions/ and chrome/browser/extensions
    Codec calls have new API that uses std::optional and spans. Switch usage
    to those new APIs, and lightly modernize the calling code.
    
    Bug: 370696612
    Low-Coverage-Reason: LARGE_SCALE_REFACTOR This switches from a deprecated API to a non-deprecated one.
    Change-Id: I03fecacce6d5b7fcafd215e4c6930fbefda38402
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934167
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369070}

--- extensions/browser/api/web_contents_capture_client.h
+++ extensions/browser/api/web_contents_capture_client.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef EXTENSIONS_BROWSER_API_WEB_CONTENTS_CAPTURE_CLIENT_H_
 #define EXTENSIONS_BROWSER_API_WEB_CONTENTS_CAPTURE_CLIENT_H_
 
+#include <optional>
+#include <string>
+
 #include "extensions/browser/extension_function.h"
 #include "extensions/common/api/extension_types.h"
 
@@ -50,10 +53,13 @@ class WebContentsCaptureClient {
       content::WebContents* web_contents,
       const api::extension_types::ImageDetails* image_detail,
       base::OnceCallback<void(const SkBitmap&)> callback);
-  bool EncodeBitmap(const SkBitmap& bitmap, std::string* base64_result);
   virtual void OnCaptureFailure(CaptureResult result) = 0;
   virtual void OnCaptureSuccess(const SkBitmap& bitmap) = 0;
   void CopyFromSurfaceComplete(const SkBitmap& bitmap);
+
+  // Encodes `bitmap`, and returns a base-64 encoded string if successful.
+  // Returns nullopt if not successful.
+  std::optional<std::string> EncodeBitmap(const SkBitmap& bitmap);
 
  private:
   // The format (JPEG vs PNG) of the resulting image.  Set in RunAsync().
