47596d389639f516b5753d62e8e1323316dcb4ac
Alison Gale <agale@google.com>
2024-10-15 18:28:24 +0000

    Enable info bar monochrome by default
    This is in preparation for launching this to 100%
    
    No launch but tracked in crbug.com/338217810 and
    InfoBarIconMonochrome.gcl
    
    Change-Id: I1692e6e3daa9d6e12ed50eeccd8d9089eb577c49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5935490
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Auto-Submit: Alison Gale <agale@chromium.org>
    Commit-Queue: Alison Gale <agale@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368880}

--- chrome/browser/ui/color/color_features.cc
+++ chrome/browser/ui/color/color_features.cc
@@ -11,6 +11,6 @@ namespace features {
 // adjusts the text color to be less subtle.
 BASE_FEATURE(kInfoBarIconMonochrome,
              "InfoBarIconMonochrome",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 }  // namespace features
