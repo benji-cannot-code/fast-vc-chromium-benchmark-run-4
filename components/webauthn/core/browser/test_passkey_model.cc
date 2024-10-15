db1a618c87e5ae4ac66ecf912beb0e0308017f04
Adam Langley <agl@chromium.org>
2024-10-15 18:41:03 +0000

    webauthn: update the "last used" value of GPM passkeys when asserted.
    (This matches the behaviour on Android.)
    
    Change-Id: I3bfad896c0ac34ee9534b19c0cb97a7e15f78cde
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926188
    Commit-Queue: Adam Langley <agl@chromium.org>
    Reviewed-by: Nina Satragno <nsatragno@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368893}

--- components/webauthn/core/browser/test_passkey_model.cc
+++ components/webauthn/core/browser/test_passkey_model.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/notreached.h"
 #include "base/rand_util.h"
 #include "base/ranges/algorithm.h"
+#include "base/time/time.h"
 #include "components/sync/protocol/webauthn_credential_specifics.pb.h"
 #include "components/webauthn/core/browser/passkey_model_change.h"
 #include "components/webauthn/core/browser/passkey_model_utils.h"
--- device/fido/features.cc
+++ device/fido/features.cc
@@ -158,4 +158,9 @@ BASE_FEATURE(kWebAuthnHybridLinking,
              "WebAuthenticationHybridLinking",
              base::FEATURE_ENABLED_BY_DEFAULT);
 
+// Update the "last used" timestamp for GPM passkeys when asserted.
+BASE_FEATURE(kWebAuthnUpdateLastUsed,
+             "WebAuthenticationUpdateLastUsed",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 }  // namespace device
