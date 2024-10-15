0be60a049219e773113748abab932d5859824cf1
Eriko Kurimoto <elkurin@chromium.org>
2024-10-15 07:16:18 +0000

    arc: Remove chrome dependency from //c/b/ash/arc/util
    Bug: b:372610188
    Change-Id: I26db5d5338215d885167982afe0195be929b8c37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933507
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368624}

--- chrome/browser/ash/arc/util/arc_window_watcher.cc
+++ chrome/browser/ash/arc/util/arc_window_watcher.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/app_types_util.h"
 #include "ash/public/cpp/window_properties.h"
 #include "base/memory/raw_ptr.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chromeos/ui/base/app_types.h"
 #include "chromeos/ui/base/window_properties.h"
 #include "ui/aura/window.h"
