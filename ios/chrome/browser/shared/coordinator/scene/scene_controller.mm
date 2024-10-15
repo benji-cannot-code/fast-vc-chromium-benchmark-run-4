028fa32ca02513b3c0af027ccc00efa8d8e9098b
Aliona DANGLA <alionadangla@google.com>
2024-10-15 10:07:28 +0000

    [iOS] Use startup information from profile instead of app state
    Bug: 333863468
    Change-Id: Ie390f3d3d9d5dbfada4c7f9f2ee1d0867633be3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920254
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368683}

--- ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
+++ ios/chrome/browser/shared/coordinator/scene/scene_controller.mm
@@ -1157,7 +1157,7 @@ void OnListFamilyMembersResponse(
     [self.sceneState.window makeKeyAndVisible];
   }
 
-  if (!self.sceneState.appState.startupInformation.isFirstRun) {
+  if (!self.sceneState.profileState.startupInformation.isFirstRun) {
     [self reconcileEulaAsAccepted];
   }
 
@@ -3922,7 +3922,7 @@ using UserFeedbackDataCallback =
             applicationActive:active
                     tabOpener:self
         connectionInformation:self
-           startupInformation:self.sceneState.appState.startupInformation
+           startupInformation:self.sceneState.profileState.startupInformation
                   prefService:self.currentInterface.profile->GetPrefs()
                     initStage:self.sceneState.profileState.initStage];
   }
