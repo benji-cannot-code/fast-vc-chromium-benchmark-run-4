2e7e83a7f4948b082d0efb92daefc8a9419462c7
Istvan Nagy <iscsi@google.com>
2024-10-15 13:28:41 +0000

    [High5] Don't show the active session auth dialog without auth factor.
    Don't show the active session auth dialog for users has not any knowledge factor.
    
    Bug: b:371946350
    Change-Id: I0941fb45d0a8a45e97985d38d241d9ff78e2aab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920259
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368748}

--- ash/auth/active_session_auth_controller_impl.cc
+++ ash/auth/active_session_auth_controller_impl.cc
@@ -298,7 +298,6 @@ void ActiveSessionAuthControllerImpl::OnAuthSessionStarted(
 
   auth_session_broadcast_id_ = user_context_->GetBroadcastId();
 
-  uma_recorder_.RecordShow(auth_request_->GetAuthReason());
   UserDataAuthClient::Get()->AddAuthFactorStatusUpdateObserver(this);
 
   available_factors_.Clear();
@@ -314,6 +313,14 @@ void ActiveSessionAuthControllerImpl::OnAuthSessionStarted(
       available_factors_.Put(AuthInputType::kPin);
     }
   }
+
+  if (available_factors_.empty()) {
+    LOG(ERROR) << "No password/PIN found for user.";
+    StartClose();
+    return;
+  }
+
+  uma_recorder_.RecordShow(auth_request_->GetAuthReason());
 
   MaybePrepareFingerprint(
       BindOnce(&ActiveSessionAuthControllerImpl::AuthFactorsAreReady,
