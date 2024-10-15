3180b144ed39db84e92ace594a04906265a731e8
James Cook <jamescook@chromium.org>
2024-10-15 16:53:11 +0000

    extensions: Add stub RuntimeAPIDelegate for desktop Android
    I'm trying to bring up the chrome.alarms API test. Initializing the
    test extension requires the runtime API, which requires a delegate. The
    delegate doesn't have to do anything for this test, but it needs to
    exist, so add a stub with NOTIMPLEMENTED for most of the methods.
    
    Bug: 373434594
    Change-Id: I0d4f0acfd155f21e400bb443ffb4d96a22bd4558
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5931926
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368835}

--- chrome/browser/extensions/desktop_android/desktop_android_extensions_browser_client.cc
+++ chrome/browser/extensions/desktop_android/desktop_android_extensions_browser_client.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/desktop_android/desktop_android_extension_system.h"
 #include "chrome/browser/extensions/desktop_android/desktop_android_extension_web_contents_observer.h"
+#include "chrome/browser/extensions/desktop_android/desktop_android_runtime_api_delegate.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "components/version_info/version_info.h"
@@ -258,7 +259,7 @@ void DesktopAndroidExtensionsBrowserClient::
 std::unique_ptr<RuntimeAPIDelegate>
 DesktopAndroidExtensionsBrowserClient::CreateRuntimeAPIDelegate(
     content::BrowserContext* context) const {
-  return nullptr;
+  return std::make_unique<DesktopAndroidRuntimeApiDelegate>();
 }
 
 const ComponentExtensionResourceManager*
