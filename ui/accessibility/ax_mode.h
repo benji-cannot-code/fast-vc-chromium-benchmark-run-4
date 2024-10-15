902cc4a3cd2bc50057c25a9e2478a11404c8bbdb
Mark Schillaci <mschillaci@google.com>
2024-10-15 13:48:51 +0000

    Add inline text boxes metrics for Android
    This CL adds some metrics for tracking inline text box usage on Android.
    
    AX-Relnotes: N/A
    Bug: 372718628
    Change-Id: I1bb17e8dbd084791b6105070ee4adae1823a665a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930144
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368753}

--- ui/accessibility/ax_mode.h
+++ ui/accessibility/ax_mode.h
@@ -161,6 +161,8 @@ class AX_BASE_EXPORT AXMode {
   // IMPORTANT!
   // These values are written to logs. Do not renumber or delete
   // existing items; add new entries to the end of the list.
+  //
+  // LINT.IfChange
   enum class BundleHistogramValue {
     // The unnamed bucket is a catch all for modes that do not match one of the
     // named sets.
@@ -176,6 +178,7 @@ class AX_BASE_EXPORT AXMode {
     // increase, but none of the other enum values may change.
     kMaxValue = 5
   };
+  // LINT.ThenChange(/tools/metrics/histograms/metadata/accessibility/enums.xml:AccessibilityModeBundleEnum)
 
   // Experimental Flags
   // These are currently defined separately from existing flags to avoid
