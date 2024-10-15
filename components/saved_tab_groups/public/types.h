75715ef5e10c0588c0279d65f36b865cfb0a27ee
Jose Dapena Paz <jdapena@igalia.com>
2024-10-15 21:45:09 +0000

    IWYU: missing include for usage of std::variant in saved_tab_groups/public/types.h
    Bug: 41455655
    Change-Id: Ic4fdb5baeda2e4be3c35c976dfa6dfd396a60643
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899087
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369048}

--- components/saved_tab_groups/public/types.h
+++ components/saved_tab_groups/public/types.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SAVED_TAB_GROUPS_PUBLIC_TYPES_H_
 
 #include <optional>
+#include <variant>
 
 #include "base/logging.h"
 #include "base/token.h"
