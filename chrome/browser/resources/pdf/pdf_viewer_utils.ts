44cc747c7b3bdd1966782d72df7b2bd6a94f9804
Andy Phan <andyphan@chromium.org>
2024-10-15 19:16:34 +0000

    [PDF Ink Signatures] Refactor brush color selector
    Move brush color selection out of the viewer side panel element and
    into its own element. The viewer side panel will still use the new
    element.
    
    This will make it easier to re-use the brush color selector for other
    UIs.
    
    NO_IFTTT=Moved to another file.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED The new function in the
    file is covered in test PDFExtensionJSInk2Test.Ink2SidePanel. Also,
    the function used to exist in a different file; it was simply moved.
    
    Bug: 369653190
    Change-Id: Ic847fe70f658aa71946c2001ecd5cd23e984e9db
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926304
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368930}

--- chrome/browser/resources/pdf/pdf_viewer_utils.ts
+++ chrome/browser/resources/pdf/pdf_viewer_utils.ts
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import {assert} from 'chrome://resources/js/assert.js';
 import {getDeepActiveElement} from 'chrome://resources/js/util.js';
 
+// <if expr="enable_pdf_ink2">
+import type {Color} from './constants.js';
+// </if>
 import type {LayoutOptions, ViewportRect} from './viewport.js';
 
 export interface DocumentDimensionsMessageData {
@@ -68,3 +71,19 @@ export function shouldIgnoreKeyEvents(): boolean {
        (activeElement as HTMLInputElement).type !== 'radio') ||
       activeElement.tagName === 'TEXTAREA';
 }
+
+// <if expr="enable_pdf_ink2">
+/**
+ * @param hex A hex-coded color string, formatted as '#ffffff'.
+ * @returns The `Color` in RGB values.
+ */
+export function hexToColor(hex: string): Color {
+  assert(/^#[0-9a-f]{6}$/.test(hex));
+
+  return {
+    r: Number.parseInt(hex.substring(1, 3), 16),
+    g: Number.parseInt(hex.substring(3, 5), 16),
+    b: Number.parseInt(hex.substring(5, 7), 16),
+  };
+}
+// </if>
