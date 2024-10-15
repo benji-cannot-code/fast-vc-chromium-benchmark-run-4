be6f189e61e04fb99eeb8940232ba472a3f203c6
Ali Stanfield <stanfield@google.com>
2024-10-15 16:33:02 +0000

    [lensoverlay] Show IPH on eligible pages.
    Tested using instructions at
    https://chromium.googlesource.com/chromium/src/+/HEAD/components/user_education/feature-promos.md#manual-approach-1_fresh-profile_cooldowns-disabled.
    Both LensOverlayIPH and IPH_LensOverlay features must be enabled and
    feature params for URL filters should be set on LensOverlayIPH.
    
    Patched from http://crrev.com/c/5924243
    
    Bug: b:363084952, 372531753, 373503344
    Change-Id: I4f4a7cc056a7d9be1be48395ace0ec83b79c13d1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930451
    Commit-Queue: Ali Stanfield <stanfield@google.com>
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1368823}

--- chrome/browser/ui/lens/lens_overlay_controller.cc
+++ chrome/browser/ui/lens/lens_overlay_controller.cc
@@ -283,7 +283,8 @@ LensOverlayController::LensOverlayController(
       std::make_unique<lens::LensSearchBubbleController>(this);
   lens_overlay_event_handler_ =
       std::make_unique<lens::LensOverlayEventHandler>(this);
-  InitializeIPHUrlMatcher();
+
+  InitializeTutorialIPHUrlMatcher();
 
   // Listen to WebContents events
   tab_contents_observer_ = std::make_unique<UnderlyingWebContentsObserver>(
@@ -1208,6 +1209,11 @@ LensOverlayController::GetLensSuggestInputsForTesting() {
   return GetLensSuggestInputs();
 }
 
+bool LensOverlayController::IsUrlEligibleForTutorialIPHForTesting(
+    const GURL& url) {
+  return IsUrlEligibleForTutorialIPH(url);
+}
+
 std::unique_ptr<lens::LensOverlayQueryController>
 LensOverlayController::CreateLensQueryController(
     lens::LensOverlayFullImageResponseCallback full_image_callback,
@@ -1266,7 +1272,8 @@ class LensOverlayController::UnderlyingWebContentsObserver
           navigation_handle->IsInPrimaryMainFrame() &&
           !navigation_handle->IsSameDocument() &&
           navigation_handle->HasCommitted()) {
-        // TODO(crbug.com/372531753): Trigger IPH tutorial here.
+        lens_overlay_controller_->MaybeShowDelayedTutorialIPH(
+            navigation_handle->GetURL());
       }
       return;
     }
@@ -2642,31 +2649,43 @@ void LensOverlayController::MaybeLaunchSurvey() {
         base::NumberToString(lens_overlay_query_controller_->gen204_id())}});
 }
 
-void LensOverlayController::InitializeIPHUrlMatcher() {
+void LensOverlayController::InitializeTutorialIPHUrlMatcher() {
   if (!base::FeatureList::IsEnabled(
           feature_engagement::kIPHLensOverlayFeature)) {
     return;
   }
 
-  iph_url_matcher_ = std::make_unique<url_matcher::URLMatcher>();
+  tutorial_iph_url_matcher_ = std::make_unique<url_matcher::URLMatcher>();
   base::MatcherStringPattern::ID id(0);
   url_matcher::util::AddFilters(
-      iph_url_matcher_.get(), true, &id,
+      tutorial_iph_url_matcher_.get(), true, &id,
       JSONArrayToVector(
           feature_engagement::kIPHLensOverlayUrlAllowFilters.Get()),
       &iph_url_filters_);
   url_matcher::util::AddFilters(
-      iph_url_matcher_.get(), false, &id,
+      tutorial_iph_url_matcher_.get(), false, &id,
       JSONArrayToVector(
           feature_engagement::kIPHLensOverlayUrlBlockFilters.Get()),
       &iph_url_filters_);
 }
 
-bool LensOverlayController::IsUrlEligibleForIPH(const GURL& url) {
-  if (!iph_url_matcher_) {
+void LensOverlayController::MaybeShowDelayedTutorialIPH(const GURL& url) {
+  // If a tutorial IPH was already queued, cancel it.
+  tutorial_iph_timer_.Stop();
+
+  if (IsUrlEligibleForTutorialIPH(url)) {
+    tutorial_iph_timer_.Start(
+        FROM_HERE, feature_engagement::kIPHLensOverlayDelayTime.Get(),
+        base::BindOnce(&LensOverlayController::ShowTutorialIPH,
+                       weak_factory_.GetWeakPtr()));
+  }
+}
+
+bool LensOverlayController::IsUrlEligibleForTutorialIPH(const GURL& url) {
+  if (!tutorial_iph_url_matcher_) {
     return false;
   }
-  auto matches = iph_url_matcher_.get()->MatchURL(url);
+  auto matches = tutorial_iph_url_matcher_.get()->MatchURL(url);
   if (!matches.size()) {
     return false;
   }
@@ -2677,4 +2696,11 @@ bool LensOverlayController::IsUrlEligibleForIPH(const GURL& url) {
     }
   }
   return true;
+}
+
+void LensOverlayController::ShowTutorialIPH() {
+  if (auto* user_ed =
+          tab_->GetBrowserWindowInterface()->GetUserEducationInterface()) {
+    user_ed->MaybeShowFeaturePromo(feature_engagement::kIPHLensOverlayFeature);
+  }
 }
--- components/feature_engagement/public/feature_constants.h
+++ components/feature_engagement/public/feature_constants.h
@@ -51,6 +51,7 @@ BASE_DECLARE_FEATURE(kIPHMemorySaverModeFeature);
 BASE_DECLARE_FEATURE(kIPHLensOverlayFeature);
 extern const base::FeatureParam<std::string> kIPHLensOverlayUrlAllowFilters;
 extern const base::FeatureParam<std::string> kIPHLensOverlayUrlBlockFilters;
+extern const base::FeatureParam<base::TimeDelta> kIPHLensOverlayDelayTime;
 BASE_DECLARE_FEATURE(kIPHLensOverlayTranslateButtonFeature);
 BASE_DECLARE_FEATURE(kIPHLiveCaptionFeature);
 BASE_DECLARE_FEATURE(kIPHTabAudioMutingFeature);
