88db74436a11dd07803c59ae2b09dde1d8d0c8ff
Lin Guo <sinyu890807@gmail.com>
2024-10-15 03:33:59 +0000

    Migrate to the WindowInsetsController API for Android R and above.
    The setStatusBarIconColor() method in UiUtils.java still uses a
    deprecated approach for setting the status bar icon color.
    
    Specifically, View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR has been
    deprecated since Android R. Android now recommends using
    WindowInsetsController#APPEARANCE_LIGHT_STATUS_BARS. Refer link: https://developer.android.com/reference/android/view/View#SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
    
    This CL updates the setStatusBarIconColor() method to migrate to the
    WindowInsetsController API for Android R and above.
    
    Bug: 372753060
    
    Change-Id: Ib0cff4368b418a6a5ad5150b101fd294549c6444
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5923073
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Zengfeng Ye <zenye@microsoft.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368580}

--- components/browser_ui/edge_to_edge/android/java/src/org/chromium/components/browser_ui/edge_to_edge/WindowSystemBarColorHelper.java
+++ components/browser_ui/edge_to_edge/android/java/src/org/chromium/components/browser_ui/edge_to_edge/WindowSystemBarColorHelper.java
@@ -40,7 +40,7 @@ public final class WindowSystemBarColorHelper extends BaseSystemBarColorHelper {
     protected void applyStatusBarColor() {
         mWindow.setStatusBarColor(mStatusBarColor);
         UiUtils.setStatusBarIconColor(
-                mWindow.getDecorView(),
+                mWindow,
                 ColorUtils.isHighLuminance(ColorUtils.calculateLuminance(mStatusBarColor)));
     }
 
