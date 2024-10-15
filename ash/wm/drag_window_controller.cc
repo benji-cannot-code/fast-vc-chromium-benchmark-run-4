4762b909cba05edb542fea1aaeda0978b1563ffa
Zoraiz Naeem <zoraiznaeem@chromium.org>
2024-10-15 22:59:07 +0000

    Only create shadow for drag window associated with OverviewItemWidget
    Currently in overview, we create two drag windows: one for
    OverviewItemWidget and another for the window in overview. Currently,
    we create shadows for both the drag windows, which are overlapping.
    This wastes resources and is a bug.
    
    In this CL, we now only create shadow for the drag window associated
    with OverviewItemWidget.
    
    Bug: b:373615844
    Change-Id: Iad6703cb463498019d4d3c870fa08d6badecb21c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925082
    Commit-Queue: Zoraiz Naeem <zoraiznaeem@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369082}

--- ash/wm/drag_window_controller.cc
+++ ash/wm/drag_window_controller.cc
@@ -56,7 +56,7 @@ float GetDragWindowOpacity(aura::Window* root_window,
   // Return an opacity value based on what fraction of |dragged_window| is
   // contained in |root_window|.
   gfx::RectF dragged_window_bounds(dragged_window->bounds());
-  ::wm::TranslateRectToScreen(dragged_window->parent(), &dragged_window_bounds);
+  wm::TranslateRectToScreen(dragged_window->parent(), &dragged_window_bounds);
   dragged_window_bounds =
       gfx::TransformAboutPivot(dragged_window_bounds.origin(),
                                dragged_window->transform())
@@ -79,7 +79,9 @@ class DragWindowController::DragWindowDetails {
   DragWindowDetails& operator=(const DragWindowDetails&) = delete;
   ~DragWindowDetails() = default;
 
-  void Update(aura::Window* original_window, bool is_touch_dragging) {
+  void Update(aura::Window* original_window,
+              bool is_touch_dragging,
+              bool create_window_shadow) {
     const float opacity =
         GetDragWindowOpacity(root_window_, original_window, is_touch_dragging);
     if (opacity == 0.f) {
@@ -89,7 +91,7 @@ class DragWindowController::DragWindowDetails {
     }
 
     if (!widget_)
-      CreateDragWindow(original_window);
+      CreateDragWindow(original_window, create_window_shadow);
 
     gfx::Rect bounds = original_window->bounds();
     aura::Window* window = widget_->GetNativeWindow();
@@ -103,7 +105,8 @@ class DragWindowController::DragWindowDetails {
  private:
   friend class DragWindowController;
 
-  void CreateDragWindow(aura::Window* original_window) {
+  void CreateDragWindow(aura::Window* original_window,
+                        bool create_window_shadow) {
     DCHECK(!widget_);
     views::Widget::InitParams params(
         views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET,
@@ -116,6 +119,13 @@ class DragWindowController::DragWindowDetails {
     params.init_properties_container.SetProperty(kHideInDeskMiniViewKey, true);
     params.parent =
         root_window_->GetChildById(original_window->parent()->GetId());
+
+    if (create_window_shadow) {
+      params.shadow_elevation = wm::kShadowElevationActiveWindow;
+      params.shadow_type = views::Widget::InitParams::ShadowType::kDrop;
+    } else {
+      params.shadow_type = views::Widget::InitParams::ShadowType::kNone;
+    }
 
     widget_ = std::make_unique<views::Widget>();
     widget_->set_focus_on_creation(false);
@@ -133,7 +143,7 @@ class DragWindowController::DragWindowDetails {
     window->SetId(kShellWindowId_PhantomWindow);
     window->SetProperty(aura::client::kAnimationsDisabledKey, true);
     gfx::Rect bounds = original_window->bounds();
-    ::wm::ConvertRectToScreen(original_window->parent(), &bounds);
+    wm::ConvertRectToScreen(original_window->parent(), &bounds);
     window->SetBounds(bounds);
     wm::SetShadowElevation(window, wm::kShadowElevationActiveWindow);
 
@@ -152,9 +162,11 @@ class DragWindowController::DragWindowDetails {
 };
 
 DragWindowController::DragWindowController(aura::Window* window,
-                                           bool is_touch_dragging)
+                                           bool is_touch_dragging,
+                                           bool create_window_shadow)
     : window_(window),
       is_touch_dragging_(is_touch_dragging),
+      create_window_shadow_(create_window_shadow),
       old_opacity_(window->layer()->GetTargetOpacity()) {
   window->layer()->SetOpacity(1.f);
 
@@ -184,7 +196,7 @@ void DragWindowController::Update() {
   }
 
   for (std::unique_ptr<DragWindowDetails>& details : drag_windows_)
-    details->Update(window_, is_touch_dragging_);
+    details->Update(window_, is_touch_dragging_, create_window_shadow_);
 }
 
 int DragWindowController::GetDragWindowsCountForTest() const {
