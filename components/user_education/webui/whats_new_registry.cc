84fe0c8c74f4734a9576bfc28f7be053e0f91521
Mickey Burks <mickeyburks@chromium.org>
2024-10-15 13:32:17 +0000

    [What's New] Allow customization through FeatureParam
    This parameter can be used to send additional configuration to the
    rendered page. This is used in rare cases where an experiment may
    alter something other than Module content on the page.
    
    Bug: 373051059
    Change-Id: I6cd7b525d0c331f2b967f0544b3b07d4108c6372
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926514
    Reviewed-by: Dana Fried <dfried@chromium.org>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1368750}

--- components/user_education/webui/whats_new_registry.cc
+++ components/user_education/webui/whats_new_registry.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/user_education/webui/whats_new_registry.h"
 
 #include "base/containers/contains.h"
+#include "base/metrics/field_trial_params.h"
 #include "base/values.h"
 #include "ui/webui/resources/js/browser_command/browser_command.mojom.h"
 
@@ -45,6 +46,15 @@ const char* WhatsNewModule::GetFeatureName() const {
   return feature_->name;
 }
 
+const std::string WhatsNewModule::GetCustomization() const {
+  if (!feature_) {
+    return "";
+  }
+  std::string customization = base::GetFieldTrialParamValueByFeature(
+      *feature_, whats_new::kCustomizationParam);
+  return customization;
+}
+
 WhatsNewEdition::WhatsNewEdition(const base::Feature& feature,
                                  std::string owner,
                                  std::vector<BrowserCommand> browser_commands)
@@ -58,6 +68,12 @@ bool WhatsNewEdition::IsFeatureEnabled() const {
 
 const char* WhatsNewEdition::GetFeatureName() const {
   return feature_->name;
+}
+
+const std::string WhatsNewEdition::GetCustomization() const {
+  std::string customization = base::GetFieldTrialParamValueByFeature(
+      *feature_, whats_new::kCustomizationParam);
+  return customization;
 }
 
 void WhatsNewRegistry::RegisterModule(WhatsNewModule module) {
@@ -164,6 +180,33 @@ const std::vector<std::string_view> WhatsNewRegistry::GetRolledFeatureNames()
   }
 
   return feature_names;
+}
+
+const std::vector<std::string> WhatsNewRegistry::GetCustomizations() const {
+  std::vector<std::string> customizations;
+  for (const WhatsNewModule& module : modules_) {
+    // Modules without a feature are default-enabled.
+    const bool module_is_default_enabled = !module.HasFeature();
+    // If the module is tied to a feature, ensure the feature is available.
+    const bool module_is_available =
+        module.HasActiveFeature() || module.HasRolledFeature();
+    if (module_is_default_enabled || module_is_available) {
+      std::string customization = module.GetCustomization();
+      if (!customization.empty()) {
+        customizations.emplace_back(customization);
+      }
+    }
+  }
+  for (const WhatsNewEdition& edition : editions_) {
+    // The only requirement for an edition to show is that it is enabled.
+    if (edition.IsFeatureEnabled()) {
+      const auto customization = edition.GetCustomization();
+      if (!customization.empty()) {
+        customizations.emplace_back(customization);
+      }
+    }
+  }
+  return customizations;
 }
 
 void WhatsNewRegistry::SetEditionUsed(std::string_view edition_name) {
