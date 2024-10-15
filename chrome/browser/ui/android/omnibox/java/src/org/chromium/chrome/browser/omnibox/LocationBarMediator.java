b810bebe2a40d3ad07c9426087df3d333f60ee24
Tomasz Wiszkowski <ender@google.com>
2024-10-15 19:29:58 +0000

    Introduce additional JumpStart trigger guards.
    The change introduces two additional guards to ensure Jump-Start
    Omnibox does not trigger too frequently:
    - device type,
    - physical memory (tunable with Finch parameter),
    - time ellapsed since last interaction with Chrome (lower and upper
      bounds tunable with Finch).
    
    This implementation looks sub-optimal, hinting the need for dedicated
    Omnibox cache, however doing this in a straightforward manner has not
    produced elegant results. For the lack of better alternative I am
    resting cached preference values as part of the OmniboxFeatures.
    
    Bug: b:371987101
    Change-Id: Ic37c4e8f7f0c9b3e26c4662f88d8aad74a6d4868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932687
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368941}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -1611,6 +1611,7 @@ class LocationBarMediator
 
     @Override
     public void onPauseWithNative() {
+        OmniboxFeatures.updateLastExitTimestamp();
         if (OmniboxFeatures.sUseFusedLocationProvider.isEnabled()) {
             GeolocationHeader.stopListeningForLocationUpdates();
         }
