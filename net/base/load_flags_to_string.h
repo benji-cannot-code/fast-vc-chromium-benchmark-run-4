5cbba7cf03c60fea82a35db773462f9ec5c9103d
Adam Rice <ricea@chromium.org>
2024-10-15 03:49:36 +0000

    Add net::LoadFlagsToString()
    Add a LoadFlagsToString() function that converts an int to a string
    giving the corresponding load flags, like "LOAD_DO_NOT_SAVE_COOKIES |
    LOAD_PREFETCH". This is useful for debugging messages.
    
    This will be used when printing non-matching load_flags fields by
    the subresource prefetch matcher code.
    
    BUG=342445996
    
    Change-Id: Ia052b5c35e1ba7ed86f7e70904912b2392b22d4c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920057
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Reviewed-by: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368584}

---
+++ net/base/load_flags_to_string.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_BASE_LOAD_FLAGS_TO_STRING_H_
+#define NET_BASE_LOAD_FLAGS_TO_STRING_H_
+
+#include <string>
+
+#include "net/base/net_export.h"
+
+namespace net {
+
+// Convert `load_flags` to a string of the form "LOAD_ONLY_FROM_CACHE |
+// LOAD_SKIP_VARY_CHECK".
+NET_EXPORT_PRIVATE std::string LoadFlagsToString(int load_flags);
+
+}  // namespace net
+
+#endif  // NET_BASE_LOAD_FLAGS_TO_STRING_H_
