94b83f1a0b2f425ec109f5fe969323215d11d971
Corentin Wallez <cwallez@chromium.org>
2024-10-15 19:39:23 +0000

    Revert "Implement Associate/DissociateForBuffer For Dawn."
    This reverts commit 74dcee8825627cb7bb06983a0512c2b9ef83a4e8.
    
    Reason for revert: Reworked tests fail on ChromeOS
    
    Original change's description:
    > Implement Associate/DissociateForBuffer For Dawn.
    >
    > Adds Associate/DissociateForBuffer for Dawn. This allows Dawn to
    > import SharedImages backed by buffers. Includes tests on D3D12.
    >
    > Validate-Test-Flakiness: skip
    > Bug: chromium:368222757
    > Change-Id: Ibbc6285b19cfb8506a74210fcb5882b863036314
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5874610
    > Reviewed-by: Colin Blundell <blundell@chromium.org>
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Commit-Queue: Brandon1 Jones <brandon1.jones@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1367621}
    
    Bug: chromium:368222757
    Change-Id: I862c0cf5b66894b13b8174ebd259a8b1439532e4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934458
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1368953}

--- gpu/command_buffer/client/webgpu_interface_stub.cc
+++ gpu/command_buffer/client/webgpu_interface_stub.cc
@@ -51,11 +51,6 @@ ReservedTexture WebGPUInterfaceStub::ReserveTexture(
   return {nullptr, 0, 0, 0, 0};
 }
 
-ReservedBuffer WebGPUInterfaceStub::ReserveBuffer(WGPUDevice device,
-                                                  const WGPUBufferDescriptor*) {
-  return {nullptr, 0, 0, 0, 0};
-}
-
 WGPUDevice WebGPUInterfaceStub::DeprecatedEnsureDefaultDeviceSync() {
   return nullptr;
 }
@@ -71,13 +66,6 @@ void WebGPUInterfaceStub::AssociateMailbox(
     GLuint view_format_count,
     MailboxFlags flags,
     const Mailbox& mailbox) {}
-
-void WebGPUInterfaceStub::AssociateMailboxForBuffer(GLuint device_id,
-                                                    GLuint device_generation,
-                                                    GLuint id,
-                                                    GLuint generation,
-                                                    uint64_t usage,
-                                                    const Mailbox& mailbox) {}
 
 // Include the auto-generated part of this class. We split this because
 // it means we can easily edit the non-auto generated parts right here in
