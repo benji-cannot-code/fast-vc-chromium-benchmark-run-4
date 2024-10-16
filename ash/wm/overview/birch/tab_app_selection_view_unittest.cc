facdafd4669fcedafd368bc0877049ae557a06f9
Sammie Quon <sammiequon@chromium.org>
2024-10-16 20:49:49 +0000

    coral: Add a pixel test for the selector view
    Baseline is in comment 16 of the bug. Had to factor out some common
    code to be reused in the new test.
    
    For now, we will compare the scroll view of the widget first, since
    we are actively adding a thumbs up/down section. Once that is done we
    will compare the whole widget.
    
    Fixed: 361326120
    Change-Id: I9250fa7acf5ef40df9f2e0ebf6b124ed41e9f012
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933263
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369619}

--- ash/wm/overview/birch/tab_app_selection_view_unittest.cc
+++ ash/wm/overview/birch/tab_app_selection_view_unittest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/overview/birch/tab_app_selection_view.h"
 
-#include "ash/birch/birch_coral_provider.h"
 #include "ash/birch/birch_item_remover.h"
 #include "ash/birch/birch_model.h"
 #include "ash/birch/test_birch_client.h"
@@ -13,12 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/constants/ash_pref_names.h"
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
-#include "ash/wm/overview/birch/birch_bar_controller.h"
+#include "ash/wm/coral/coral_test_util.h"
 #include "ash/wm/overview/birch/birch_chip_button.h"
-#include "ash/wm/overview/birch/birch_chip_button_base.h"
 #include "ash/wm/overview/birch/tab_app_selection_host.h"
 #include "ash/wm/overview/birch/tab_app_selection_view.h"
-#include "ash/wm/overview/overview_grid_test_api.h"
 #include "ash/wm/overview/overview_utils.h"
 #include "base/test/scoped_feature_list.h"
 #include "ui/views/view_utils.h"
@@ -30,7 +27,7 @@ class TabAppSelectionViewTest : public AshTestBase {
   void SetUp() override {
     AshTestBase::SetUp();
 
-    // Create test birch client and test coral provider.
+    // Create test birch client.
     auto* birch_model = Shell::Get()->birch_model();
     birch_client_ = std::make_unique<TestBirchClient>(birch_model);
     birch_model->SetClientAndInit(birch_client_.get());
@@ -40,27 +37,10 @@ class TabAppSelectionViewTest : public AshTestBase {
         run_loop.QuitClosure());
     run_loop.Run();
 
-    // Prepare a coral group so we have a coral glanceable to click.
-    auto fake_response = std::make_unique<CoralResponse>();
-    auto fake_group = coral::mojom::Group::New();
-    fake_group->title = "Coral Group";
-    fake_group->entities.push_back(coral::mojom::Entity::NewTab(
-        coral::mojom::Tab::New("Reddit", GURL("https://www.reddit.com/"))));
-    fake_group->entities.push_back(coral::mojom::Entity::NewTab(
-        coral::mojom::Tab::New("Figma", GURL("https://www.figma.com/"))));
-    fake_group->entities.push_back(coral::mojom::Entity::NewTab(
-        coral::mojom::Tab::New("Notion", GURL("https://www.notion.so/"))));
-    fake_group->entities.push_back(
-        coral::mojom::Entity::NewApp(coral::mojom::App::New(
-            "Settings", "odknhmnlageboeamepcngndbggdpaobj")));
-    fake_group->entities.push_back(coral::mojom::Entity::NewApp(
-        coral::mojom::App::New("Files", "lgnggepjiihbfdbedefdhcffnmhcahbm")));
-
-    std::vector<coral::mojom::GroupPtr> fake_groups;
-    fake_groups.push_back(std::move(fake_group));
-    fake_response->set_groups(std::move(fake_groups));
-    BirchCoralProvider::Get()->OverrideCoralResponseForTest(
-        std::move(fake_response));
+    // Prepare a coral response so we have a coral glanceable to click.
+    std::vector<coral::mojom::GroupPtr> test_groups;
+    test_groups.push_back(CreateDefaultTestGroup());
+    OverrideTestResponse(std::move(test_groups));
   }
 
   void TearDown() override {
@@ -69,32 +49,15 @@ class TabAppSelectionViewTest : public AshTestBase {
     AshTestBase::TearDown();
   }
 
-  // Brings up the selector menu host object by entering overview and clicking
-  // the birch coral chip.
-  TabAppSelectionHost* ShowAndGetSelectorMenu() {
-    EnterOverview();
-
-    const std::vector<raw_ptr<BirchChipButtonBase>>& birch_chips =
-        OverviewGridTestApi(Shell::GetPrimaryRootWindow()).GetBirchChips();
-    CHECK_EQ(1u, birch_chips.size());
-
-    auto* coral_button = views::AsViewClass<BirchChipButton>(birch_chips[0]);
-    CHECK_EQ(BirchItemType::kCoral, coral_button->GetItem()->GetType());
-
-    LeftClickOn(coral_button->addon_view());
-    return coral_button->tab_app_selection_widget_.get();
-  }
-
  private:
   std::unique_ptr<TestBirchClient> birch_client_;
-  raw_ptr<TestBirchDataProvider<BirchCoralItem>> coral_provider_;
 
   base::test::ScopedFeatureList feature_list_{features::kCoralFeature};
 };
 
 // Tests that the menu can be toggled to show and hide.
 TEST_F(TabAppSelectionViewTest, ToggleMenu) {
-  TabAppSelectionHost* menu = ShowAndGetSelectorMenu();
+  TabAppSelectionHost* menu = ShowAndGetSelectorMenu(GetEventGenerator());
   ASSERT_TRUE(menu);
   EXPECT_TRUE(menu->IsVisible());
 
@@ -106,7 +69,7 @@ TEST_F(TabAppSelectionViewTest, ToggleMenu) {
 }
 
 TEST_F(TabAppSelectionViewTest, EscapeHidesMenu) {
-  TabAppSelectionHost* menu = ShowAndGetSelectorMenu();
+  TabAppSelectionHost* menu = ShowAndGetSelectorMenu(GetEventGenerator());
   ASSERT_TRUE(menu);
   EXPECT_TRUE(menu->IsVisible());
 
@@ -117,7 +80,7 @@ TEST_F(TabAppSelectionViewTest, EscapeHidesMenu) {
 
 // Tests clicking the close buttons on the selector menu.
 TEST_F(TabAppSelectionViewTest, CloseSelectorItems) {
-  TabAppSelectionHost* menu = ShowAndGetSelectorMenu();
+  TabAppSelectionHost* menu = ShowAndGetSelectorMenu(GetEventGenerator());
   ASSERT_TRUE(menu);
 
   auto* selection_view =
@@ -146,7 +109,7 @@ TEST_F(TabAppSelectionViewTest, CloseSelectorItems) {
 
 // Tests clicking outside the selector view closes it.
 TEST_F(TabAppSelectionViewTest, PressToHideMenu) {
-  TabAppSelectionHost* menu = ShowAndGetSelectorMenu();
+  TabAppSelectionHost* menu = ShowAndGetSelectorMenu(GetEventGenerator());
   ASSERT_TRUE(menu);
 
   // Clicks on the selector itself should not hide it.
@@ -159,7 +122,7 @@ TEST_F(TabAppSelectionViewTest, PressToHideMenu) {
   EXPECT_TRUE(!menu->IsVisible());
 
   // Test tapping outside the selector.
-  menu = ShowAndGetSelectorMenu();
+  menu = ShowAndGetSelectorMenu(GetEventGenerator());
   ASSERT_TRUE(menu);
   GetEventGenerator()->GestureTapAt(gfx::Point(1, 1));
   EXPECT_TRUE(!menu->IsVisible());
