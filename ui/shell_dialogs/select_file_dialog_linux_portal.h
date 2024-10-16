be6458a2e34683b5824196819fe9511d8b415597
Tom Anderson <thomasanderson@chromium.org>
2024-10-16 01:59:58 +0000

    Refactor SelectFileDialogLinuxPortal to use CheckForServiceAndStart
    R=thestig
    
    Change-Id: Ied14e11cc20b0d9bc071969c0fd3ef70b126986e
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5922978
    Reviewed-by: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369148}

--- ui/shell_dialogs/select_file_dialog_linux_portal.h
+++ ui/shell_dialogs/select_file_dialog_linux_portal.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_SHELL_DIALOGS_SELECT_FILE_DIALOG_LINUX_PORTAL_H_
 #define UI_SHELL_DIALOGS_SELECT_FILE_DIALOG_LINUX_PORTAL_H_
 
+#include <optional>
+
 #include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/shell_dialogs/select_file_dialog_linux.h"
 
 namespace ui {
+
 using OnSelectFileExecutedCallback =
     base::OnceCallback<void(std::vector<base::FilePath> paths,
                             std::string current_filter)>;
@@ -41,8 +44,8 @@ class SelectFileDialogLinuxPortal : public SelectFileDialogLinux {
   // around program start.
   static void StartAvailabilityTestInBackground();
 
-  // Checks if the file chooser portal is available. Blocks if the availability
-  // test from above has not yet completed (which should generally not happen).
+  // Checks if the file chooser portal is available. Logs a warning if the
+  // availability test has not yet completed.
   static bool IsPortalAvailable();
 
   // Destroys the connection to the bus.
@@ -141,7 +144,7 @@ class SelectFileDialogLinuxPortal : public SelectFileDialogLinux {
     void AppendOptions(dbus::MessageWriter* writer,
                        const std::string& response_handle_token,
                        const base::FilePath& default_path,
-                       const bool derfault_path_exists,
+                       const bool default_path_exists,
                        const PortalFilterSet& filter_set);
     void AppendFilterStruct(dbus::MessageWriter* writer,
                             const PortalFilter& filter);
@@ -175,8 +178,6 @@ class SelectFileDialogLinuxPortal : public SelectFileDialogLinux {
 
   // D-Bus configuration and initialization.
   static void CheckPortalAvailabilityOnBusThread();
-  static bool IsPortalRunningOnBusThread(dbus::ObjectProxy* dbus_proxy);
-  static bool IsPortalActivatableOnBusThread(dbus::ObjectProxy* dbus_proxy);
 
   // Returns a flag, written by the D-Bus thread and read by the UI thread,
   // indicating whether or not the availability test has completed.
