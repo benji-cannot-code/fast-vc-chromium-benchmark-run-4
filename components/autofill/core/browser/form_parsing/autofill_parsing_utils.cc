11202ac29c7a89a02c0c3941d48ddb87c10cb013
Norge Vizcay <vizcay@google.com>
2024-10-16 12:46:02 +0000

    Guard France regex changes under address model flag.
    Bug: 347859030
    Change-Id: I42acbbe29bee328de262f03b86d11fc605286cbf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938041
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369325}

--- components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
+++ components/autofill/core/browser/form_parsing/autofill_parsing_utils.cc
@@ -25,6 +25,8 @@ const base::Feature* GetFeatureOfRegexFeature(RegexFeature feature) {
       return &features::kAutofillUseITAddressModel;
     case RegexFeature::kAutofillUsePLAddressModel:
       return &features::kAutofillUsePLAddressModel;
+    case RegexFeature::kAutofillUseFRAddressModel:
+      return &features::kAutofillUseFRAddressModel;
   }
   NOTREACHED();
 }
