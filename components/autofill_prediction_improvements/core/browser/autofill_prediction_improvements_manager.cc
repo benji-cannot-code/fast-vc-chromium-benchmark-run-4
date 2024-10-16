285fdb28d57b2f8aaf1e8e6856a8258f413c00fb
Wolfgang Billenstein <bwolfgang@google.com>
2024-10-16 05:19:09 +0000

    Add UMA UserAnnotations.ImportSkipReason
    Bug: 372170223
    Change-Id: Ib2ee598baa26332a5000e4cefe371b925392dc84
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936050
    Auto-Submit: Wolfgang Billenstein <bwolfgang@google.com>
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369188}

--- components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager.cc
+++ components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
 #include "base/location.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/notimplemented.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
@@ -158,6 +159,11 @@ std::vector<autofill::Suggestion> CreateErrorSuggestions() {
 std::vector<autofill::Suggestion> CreateNoInfoSuggestions() {
   return CreateErrorOrNoInfoSuggestions(
       IDS_AUTOFILL_PREDICTION_IMPROVEMENTS_NO_INFO_POPUP_MAIN_TEXT);
+}
+
+void LogImportSkipReason(
+    AutofillPredictionImprovementsManager::ImportSkipReason reason) {
+  base::UmaHistogramEnumeration("UserAnnotations.ImportSkipReason", reason);
 }
 
 }  // namespace
@@ -649,24 +655,24 @@ void AutofillPredictionImprovementsManager::MaybeImportForm(
   // Apply the filter rules to mark potentially sensitive values.
   FilterSensitiveValues(*form.get());
 
-  bool skip_import = false;
-
+  ImportSkipReason skip_reason = ImportSkipReason::kNone;
   if (!client_->IsAutofillPredictionImprovementsEnabledPref()) {
     // `autofill::prefs::kAutofillPredictionImprovementsEnabled` is disabled.
-    skip_import = true;
+    skip_reason = ImportSkipReason::kPrefDisabled;
   } else if (!annotation_service) {
     // The import is skipped because the annotation service is not available.
-    skip_import = true;
+    skip_reason = ImportSkipReason::kUserAnnotationsServiceNotAvailable;
   } else if (!annotation_service->ShouldAddFormSubmissionForURL(
                  form->source_url())) {
     // The import is disabled because the origin criteria is not fulfilled.
-    skip_import = true;
+    skip_reason = ImportSkipReason::kUrl;
   } else if (!IsFormEligibleForImportByFieldCriteria(*form.get())) {
     // The form does not contain enough values that can be imported.
-    skip_import = true;
+    skip_reason = ImportSkipReason::kFormNotEligible;
   }
 
-  if (skip_import) {
+  if (skip_reason != ImportSkipReason::kNone) {
+    LogImportSkipReason(skip_reason);
     std::move(callback).Run(std::move(form),
                             /*to_be_upserted_entries=*/{},
                             /*prompt_acceptance_callback=*/base::DoNothing());
@@ -695,6 +701,7 @@ void AutofillPredictionImprovementsManager::OnReceivedAXTreeForFormImport(
     optimization_guide::proto::AXTreeUpdate ax_tree_update) {
   if (user_annotations::UserAnnotationsService* user_annotations_service =
           client_->GetUserAnnotationsService()) {
+    LogImportSkipReason(ImportSkipReason::kNone);
     user_annotations_service->AddFormSubmission(
         url, title, std::move(ax_tree_update), std::move(form),
         std::move(callback));
