246f3bc07933bc05efe8f2e7022240fb41b4f20e
Andrew Rayskiy <greengrape@google.com>
2024-10-16 15:40:39 +0000

    iwa: Correctly display favicons for Isolated Web Apps
    Currently the FaviconClient doesn't treat isolated-app:// URLs as
    belonging to native apps and attempts to fetch favicons from the history
    service (without much success). This CL attempts to bridge this gap
    by marking these URLs as supported and routing them through the
    WebAppIconManager.
    
    A generic globe icon will be shown as a fallback if the icon manager is unable to return icons for any reason -- this is usually handled on the
    requesting side.
    
    Bug: 370990855
    Change-Id: Ie9ae0fb6a490b0900be4f8766fed296f799d9404
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928988
    Reviewed-by: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369423}

--- chrome/browser/favicon/chrome_favicon_client.cc
+++ chrome/browser/favicon/chrome_favicon_client.cc
@@ -43,6 +43,12 @@ bool ChromeFaviconClient::IsNativeApplicationURL(const GURL& url) {
     return true;
 #endif
 
+#if !BUILDFLAG(IS_ANDROID)
+  if (url.SchemeIs(chrome::kIsolatedAppScheme)) {
+    return true;
+  }
+#endif
+
   return url.SchemeIs(content::kChromeUIScheme);
 }
 
