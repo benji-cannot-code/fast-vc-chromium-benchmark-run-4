920a55d2e6f02766271c4980ad521668e15717b8
Jan Keitel <jkeitel@google.com>
2024-10-16 15:05:46 +0000

    Control plus address request timeout via finch parameter
    When there is no server response prior within the defined timeout,
    the SimpleUrlLoader runs the result callback with an empty response
    and an empty response header. As a result, we currently do not log
    metrics for the error (since there is no response code) and show a
    generic error dialog.
    
    This CL makes the following improvements:
    - The client request timeout is now configurable via a Finch
      parameter. The default value for the parameter is the same as the
      currently hard-coded value (5 seconds).
    - There is a new PlusAddressRequestErrorType entry that responds to
      client timeouts. When requests time out in the URL loader (as
      opposed to receiving a timeout response from the server), this
      type is set.
    - PlusAddressRequestError::IsTimeout returns true for both client
      and server timeouts. As a result, we now show appropriate "timeout"
      error UI when the request times out on the client.
    - There is a new metric that records the NetError code of the URL
      loader.
    
    Bug: 370513944, 373806069
    Change-Id: I13787f2cb1a87b6f18df977a5b80c261d2274194
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933831
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369405}

--- components/plus_addresses/features.h
+++ components/plus_addresses/features.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/component_export.h"
 #include "base/feature_list.h"
 #include "base/metrics/field_trial_params.h"
+#include "base/time/time.h"
 
 namespace plus_addresses::features {
 
@@ -53,6 +54,11 @@ extern const base::FeatureParam<std::string> kPlusAddressExcludedSites;
 // Url for user to report issues with plus addresses.
 COMPONENT_EXPORT(PLUS_ADDRESSES_FEATURES)
 extern const base::FeatureParam<std::string> kPlusAddressErrorReportUrl;
+
+// The amount of time before the client aborts a request to the plus address
+// server.
+COMPONENT_EXPORT(PLUS_ADDRESSES_FEATURES)
+extern const base::FeatureParam<base::TimeDelta> kPlusAddressRequestTimeout;
 
 // Used to disable this feature when requests to the server repeatedly fail with
 // a 403.
