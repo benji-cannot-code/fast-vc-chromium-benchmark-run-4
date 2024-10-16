f25fb3c5b38f4d6a1244172399189d1bd8a868b0
Nigel Tao <nigeltao@chromium.org>
2024-10-16 00:40:16 +0000

    Image Loader: roll forward to MV3
    This undoes the recent "Image Loader: roll back to MV2" commit.
    
    But also, in manifest.json, this commit advances the version to "0.5".
    
    The offscreen.ts and service_worker.ts files were rolled forward in a
    separate commit (crrev.com/c/5912638) because the service_worker.ts file
    has had further changes between 5912638 and this commit.
    
    Bug: b:319190834
    Test: Manual
    Change-Id: I04a7977eb463d24edbd7bb5962b11189aaa2f161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5856004
    Reviewed-by: Luciano Pacheco <lucmult@chromium.org>
    Commit-Queue: Nigel Tao <nigeltao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369125}

--- ui/file_manager/image_loader/load_image_request.ts
+++ ui/file_manager/image_loader/load_image_request.ts
@@ -106,6 +106,7 @@ export interface LoadImageRequest {
   maxHeight?: number;
 
   // Parts that control the request flow.
+  imageLoaderRequestId?: string;
   taskId?: number;
   cancel?: boolean;
   crop?: boolean;
