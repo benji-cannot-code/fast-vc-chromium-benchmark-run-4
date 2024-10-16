97b5a953bfd4fe1cf3175257060a4c2275175b36
Alan Cutter <alancutter@chromium.org>
2024-10-16 06:27:11 +0000

    Revert "Add UMA UserAnnotations.ImportSkipReason"
    This reverts commit 707d3def5c5b66d7f34834c3caf920b6875cae30.
    
    Reason for revert: Needed to revert prior change that touches the same file:
    https://chromium-review.googlesource.com/c/chromium/src/+/5933527
    
    Original change's description:
    > Add UMA UserAnnotations.ImportSkipReason
    >
    > Bug: 372170223
    > Change-Id: Ib2ee598baa26332a5000e4cefe371b925392dc84
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936050
    > Auto-Submit: Wolfgang Billenstein <bwolfgang@google.com>
    > Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    > Commit-Queue: Wolfgang Billenstein <bwolfgang@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1369188}
    
    Bug: 372170223
    Change-Id: If98147ac8fa76d0cf08987f4def1a7bf9f61c850
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934154
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Alan Cutter <alancutter@google.com>
    Commit-Queue: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369200}

--- components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager_unittest.cc
+++ components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_manager_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/task/current_thread.h"
 #include "base/test/gmock_move_support.h"
-#include "base/test/metrics/histogram_tester.h"
 #include "base/test/mock_callback.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/test/task_environment.h"
@@ -981,13 +980,9 @@ TEST_F(AutofillPredictionImprovementsManagerTest,
       user_annotations_entries;
   EXPECT_CALL(import_form_callback, Run)
       .WillOnce(SaveArg<1>(&user_annotations_entries));
-  base::HistogramTester histogram_tester;
   manager_->MaybeImportForm(
       std::make_unique<autofill::FormStructure>(autofill::FormData()),
       import_form_callback.Get());
-  histogram_tester.ExpectUniqueSample(
-      "UserAnnotations.ImportSkipReason",
-      AutofillPredictionImprovementsManager::ImportSkipReason::kUrl, 1);
   EXPECT_TRUE(user_annotations_entries.empty());
 }
 
