d7f28e4e8a755b1d3024adf837c15f47c2c4bc80
Paul Adedeji <pauladedeji@google.com>
2024-10-16 16:45:41 +0000

    [panorama] Update default value for `WallpaperSearchHideCondition`.
    Updates default value of `NtpWallpaperSearchButtonHideCondition`
    to 2, to avoid the button showing by default.
    
    Change-Id: Ia02af9ccee6c651a45ae84298b46e2870e10b526
    Bug: 372233731
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5930845
    Auto-Submit: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Roman Arora <romanarora@chromium.org>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369465}

--- components/search/ntp_features.cc
+++ components/search/ntp_features.cc
@@ -433,6 +433,6 @@ int GetWallpaperSearchButtonAnimationShownThreshold() {
 int GetWallpaperSearchButtonHideCondition() {
   return base::GetFieldTrialParamByFeatureAsInt(
       kNtpWallpaperSearchButtonHideCondition,
-      kNtpWallpaperSearchButtonHideConditionParam, 0);
+      kNtpWallpaperSearchButtonHideConditionParam, 2);
 }
 }  // namespace ntp_features
