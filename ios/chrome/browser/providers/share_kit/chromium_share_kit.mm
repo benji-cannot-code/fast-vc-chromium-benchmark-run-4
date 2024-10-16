75e832aaa9f45db2ded4afb5149fc020ce40b9ab
Scott Yoder <scottyoder@google.com>
2024-10-16 14:37:05 +0000

    Revert "[iOS] Add support for the FacePile"
    This reverts commit f4e198390f2263f01fc4b2d62b3c6d1892d00b61.
    
    Reason for revert: causes build failures downstream
    
    Original change's description:
    > [iOS] Add support for the FacePile
    >
    > This CL adds support for creating a FacePile for shared tab groups, as
    > an invite or as a FacePile.
    >
    > https://crrev.com/i/7756506
    > http://cl/686428668
    >
    > Bug: 358373145
    > Change-Id: Ib471900dcb3f43bca184857a42172082736ed062
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5934268
    > Reviewed-by: Ewann Pellé <ewannpv@chromium.org>
    > Reviewed-by: Louis Romero <lpromero@google.com>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1369362}
    
    Bug: 358373145
    Change-Id: I406eac120a1b612ebf82a07b417c015f898e74e3
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936917
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Scott Yoder <scottyoder@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1369388}

--- ios/chrome/browser/providers/share_kit/chromium_share_kit.mm
+++ ios/chrome/browser/providers/share_kit/chromium_share_kit.mm
@@ -19,7 +19,6 @@ class ChromiumShareKitService final : public ShareKitService {
   bool IsSupported() const override { return false; }
   void ShareGroup(const TabGroup* group,
                   UIViewController* base_view_controller) override {}
-  UIViewController* FacePile(NSString* collab_id) override { return nil; }
 };
 
 }  // namespace
