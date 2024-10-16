4f4a6b0cd9d07174a58d982326d06dd78116bac0
Justin Lulejian <jlulejian@chromium.org>
2024-10-16 01:45:10 +0000

    [Extensions] Erase worker activation token on renderer extension unload
    Before this change we never cleared the worker activation token when an
    extension was unloaded in the renderer. This caused an issue where if
    the extension had a service worker, but then changed to no longer have a
    worker then renderer would crash since non-worker based extension should
    not have worker activation token.
    
    After this change we remove the worker activation token when the
    extension is unloaded in the renderer. This way there will not be a
    token leftover when the new worker-less extension version is loaded.
    
    Bug: 372753069
    Change-Id: I3443bc0775a6f424f5b304c3dbf054a4d60bbf49
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932684
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369146}

--- extensions/renderer/renderer_extension_registry.cc
+++ extensions/renderer/renderer_extension_registry.cc
@@ -82,6 +82,7 @@ bool RendererExtensionRegistry::Insert(
 bool RendererExtensionRegistry::Remove(const ExtensionId& id) {
   DCHECK(content::RenderThread::Get());
   base::AutoLock lock(lock_);
+  worker_activation_tokens_.erase(id);
   return extensions_.Remove(id);
 }
 
