96b1c5b3a00324d192018cc985445fc4c502e418
Samar <samarchehade@google.com>
2024-10-16 09:19:43 +0000

    [Signin][Android] Fix failing SigninAndHistorySync tests.
    `checkDialogRoot` should be set to true for tests that fail due to
    Espresso using the wrong root view for dialogs on API30+.
    See https://crbug.com/332025155.
    
    Fixed: 372785514
    Change-Id: Ib0160ab8cd0af59bcfd21fb001fbbdddfcfae033
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936051
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369275}

--- chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninAndHistorySyncIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/signin/SigninAndHistorySyncIntegrationTest.java
@@ -150,7 +150,7 @@ public class SigninAndHistorySyncIntegrationTest {
                 HistoryOptInMode.REQUIRED);
 
         verifyCollapsedBottomSheetAndSignin(AccountManagerTestRule.AADC_ADULT_ACCOUNT);
-        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ false);
+        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ true);
     }
 
     @Test
@@ -165,7 +165,7 @@ public class SigninAndHistorySyncIntegrationTest {
                 HistoryOptInMode.REQUIRED);
 
         verifyCollapsedBottomSheetAndSignin(AccountManagerTestRule.AADC_ADULT_ACCOUNT);
-        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ false);
+        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ true);
     }
 
     @Test
@@ -255,7 +255,10 @@ public class SigninAndHistorySyncIntegrationTest {
                                                                         .AADC_ADULT_ACCOUNT
                                                                         .getEmail()))))));
 
-        onView(allOf(withId(R.id.button_secondary), isCompletelyDisplayed())).perform(click());
+        // Dismiss history sync.
+        onViewWaiting(withId(R.id.history_sync_illustration), /* checkRootDialog= */ true)
+                .check(matches(isDisplayed()));
+        onViewWaiting(withId(R.id.button_secondary), /* checkRootDialog= */ true).perform(click());
 
         // Verify that the flow completion callback, which finishes the activity, is called.
         ApplicationTestUtils.waitForActivityState(mActivity, Stage.DESTROYED);
@@ -275,7 +278,7 @@ public class SigninAndHistorySyncIntegrationTest {
                 HistoryOptInMode.OPTIONAL);
 
         verifyCollapsedBottomSheetAndSignin(AccountManagerTestRule.AADC_ADULT_ACCOUNT);
-        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ false);
+        acceptHistorySyncAndVerifyFlowCompletion(/* checkDialogRoot= */ true);
     }
 
     @Test
