35bfae39133a7ba88d0e9812dfe151e1b5fb0ba1
Rahul Singh <rahsin@microsoft.com>
2024-10-16 17:29:25 +0000

    IDB WPTs: Extend several IDB WPTs to run on workers (Part 2)
    This set of IndexedDB WPTs currently only run in a window environment.
    This change extends them to also run in dedicated, shared, and service
    worker environments.
    
    Bug: 41455766
    Change-Id: Ib973b26a725d2b3c05e2696cee208c052e563632
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926431
    Commit-Queue: Rahul Singh <rahsin@microsoft.com>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369491}

---
+++ third_party/blink/web_tests/external/wpt/IndexedDB/cursor-overloads.any.js
@@ -0,0 +1,88 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=IndexedDB
+// META: global=window,worker
+// META: script=resources/support.js
+
+'use strict';
+
+async_test(t => {
+  let db;
+  let trans;
+  let store;
+  let index;
+  let request = createdb(t);
+
+  request.onupgradeneeded = function(e) {
+    db = e.target.result;
+    store = db.createObjectStore('store');
+    index = store.createIndex('index', 'value');
+    store.put({value: 0}, 0);
+    trans = request.transaction;
+    trans.oncomplete = verifyOverloads;
+  };
+
+  function verifyOverloads() {
+    trans = db.transaction('store', 'readonly', {durability: 'relaxed'});
+    store = trans.objectStore('store');
+    index = store.index('index');
+
+    checkCursorDirection(store.openCursor(), 'next');
+    checkCursorDirection(store.openCursor(0), 'next');
+    checkCursorDirection(store.openCursor(0, 'next'), 'next');
+    checkCursorDirection(store.openCursor(0, 'nextunique'), 'nextunique');
+    checkCursorDirection(store.openCursor(0, 'prev'), 'prev');
+    checkCursorDirection(store.openCursor(0, 'prevunique'), 'prevunique');
+
+    checkCursorDirection(store.openCursor(IDBKeyRange.only(0)), 'next');
+    checkCursorDirection(store.openCursor(IDBKeyRange.only(0), 'next'), 'next');
+    checkCursorDirection(
+        store.openCursor(IDBKeyRange.only(0), 'nextunique'), 'nextunique');
+    checkCursorDirection(store.openCursor(IDBKeyRange.only(0), 'prev'), 'prev');
+    checkCursorDirection(
+        store.openCursor(IDBKeyRange.only(0), 'prevunique'), 'prevunique');
+
+    checkCursorDirection(index.openCursor(), 'next');
+    checkCursorDirection(index.openCursor(0), 'next');
+    checkCursorDirection(index.openCursor(0, 'next'), 'next');
+    checkCursorDirection(index.openCursor(0, 'nextunique'), 'nextunique');
+    checkCursorDirection(index.openCursor(0, 'prev'), 'prev');
+    checkCursorDirection(index.openCursor(0, 'prevunique'), 'prevunique');
+
+    checkCursorDirection(index.openCursor(IDBKeyRange.only(0)), 'next');
+    checkCursorDirection(index.openCursor(IDBKeyRange.only(0), 'next'), 'next');
+    checkCursorDirection(
+        index.openCursor(IDBKeyRange.only(0), 'nextunique'), 'nextunique');
+    checkCursorDirection(index.openCursor(IDBKeyRange.only(0), 'prev'), 'prev');
+    checkCursorDirection(
+        index.openCursor(IDBKeyRange.only(0), 'prevunique'), 'prevunique');
+
+    checkCursorDirection(index.openKeyCursor(), 'next');
+    checkCursorDirection(index.openKeyCursor(0), 'next');
+    checkCursorDirection(index.openKeyCursor(0, 'next'), 'next');
+    checkCursorDirection(index.openKeyCursor(0, 'nextunique'), 'nextunique');
+    checkCursorDirection(index.openKeyCursor(0, 'prev'), 'prev');
+    checkCursorDirection(index.openKeyCursor(0, 'prevunique'), 'prevunique');
+
+    checkCursorDirection(index.openKeyCursor(IDBKeyRange.only(0)), 'next');
+    checkCursorDirection(
+        index.openKeyCursor(IDBKeyRange.only(0), 'next'), 'next');
+    checkCursorDirection(
+        index.openKeyCursor(IDBKeyRange.only(0), 'nextunique'), 'nextunique');
+    checkCursorDirection(
+        index.openKeyCursor(IDBKeyRange.only(0), 'prev'), 'prev');
+    checkCursorDirection(
+        index.openKeyCursor(IDBKeyRange.only(0), 'prevunique'), 'prevunique');
+
+    t.done();
+  }
+
+  function checkCursorDirection(request, direction) {
+    request.onsuccess = function(event) {
+      assert_not_equals(
+          event.target.result, null, 'Check the result is not null')
+      assert_equals(
+          event.target.result.direction, direction,
+          'Check the result direction');
+    };
+  }
+}, 'Validate the overloads of IDBObjectStore.openCursor(), IDBIndex.openCursor() and IDBIndex.openKeyCursor()');
