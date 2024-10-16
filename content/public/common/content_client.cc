9bc6cb896e03fac353487748341c851512bef7c5
Alex Yang <aycyang@chromium.org>
2024-10-16 17:41:12 +0000

    Add ResourceBundle::HasDataResource and expose it to Blink platform
    HasDataResource checks if there exists a resource for a given resource
    ID without actually decompressing the data.
    
    This is specifically useful to WebUI renderer processes when the WebUI
    In-Process Resource Loading feature is turned on. In this scenario, the
    renderer process receives resource IDs from the browser process which
    may or may not point to a file it has memory-mapped. (This uncertainty
    occurs because these resource IDs come from the browser process which
    has broader file access and assumes that it is the one accessing the
    resources. The browser process does not assume any knowledge about the
    access privileges of the renderer process, so it cannot pre-filter these
    resource IDs.)
    
    In production, the renderer process is not expected to receive resource
    IDs pointing to files other than 'resources.pak', so calling
    HasDataResource there would be more defensive than functional. Where
    this can happen is in some browser tests, for example, which add
    additional data sources pointing to an ad-hoc memory-mapped
    'browser_tests.pak' file, which is not memory-mapped in renderer
    processes. If the renderer process finds that it cannot access the
    resource, then it should fall back to requesting it from the browser
    process. This new method makes the resource existence check feasible.
    
    The aforementioned fallback logic will be added in a future commit.
    
    Bug: 362511750
    Change-Id: I567ed75d0c2a9d2a862673536f255e2eada32740
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932328
    Reviewed-by: Sean Topping <seantopping@chromium.org>
    Commit-Queue: Alex Yang <aycyang@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369499}

--- content/public/common/content_client.cc
+++ content/public/common/content_client.cc
@@ -110,6 +110,10 @@ std::u16string ContentClient::GetLocalizedString(
   return std::u16string();
 }
 
+bool ContentClient::HasDataResource(int resource_id) const {
+  return false;
+}
+
 std::string_view ContentClient::GetDataResource(
     int resource_id,
     ui::ResourceScaleFactor scale_factor) {
