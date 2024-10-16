aec236ea30318c07bc0fa2997360c6feda1335f2
Maksim Sisov <msisov@igalia.com>
2024-10-16 13:06:37 +0000

    Modularize //c/b/ui/bookmarks
    R=erikchen@chromium.org
    CC=tonikitoo@igalia.com
    
    Bug: 364052460
    Change-Id: Iae1e65b2f09367e7c3e3327769bf4b343db4608a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920207
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1369335}

--- chrome/browser/ui/bookmarks/bookmark_utils.h
+++ chrome/browser/ui/bookmarks/bookmark_utils.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "chrome/browser/bookmarks/bookmark_merged_surface_service.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/base/dragdrop/mojom/drag_drop_types.mojom-forward.h"
 #include "ui/base/models/image_model.h"
@@ -18,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class GURL;
 class Profile;
+
+struct BookmarkParentFolder;
 
 namespace bookmarks {
 class BookmarkNode;
