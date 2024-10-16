d871e1d6ade0a2db2ecd3730cc345720f411610d
Matt Mueller <mattm@chromium.org>
2024-10-16 18:41:16 +0000

    pass a bound netlog in NetworkContext::VerifyCertificateForTesting
    Allows easier debugging of tests that use this method, with
    --log-net-log.
    
    Change-Id: Ia1f93e2c532ca88e592a07f1fc77f8463178d3da
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926430
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369538}

--- services/network/network_context.cc
+++ services/network/network_context.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_request_headers.h"
 #include "net/http/http_server_properties.h"
 #include "net/http/http_transaction_factory.h"
+#include "net/log/net_log_source_type.h"
 #include "net/net_buildflags.h"
 #include "net/proxy_resolution/configured_proxy_resolution_service.h"
 #include "net/proxy_resolution/proxy_config.h"
@@ -2175,7 +2176,9 @@ void NetworkContext::VerifyCertificateForTesting(
       result,
       base::BindOnce(TestVerifyCertCallback, std::move(state),
                      std::move(callback)),
-      request, net::NetLogWithSource());
+      request,
+      net::NetLogWithSource::Make(net::NetLog::Get(),
+                                  net::NetLogSourceType::NONE));
 }
 
 void NetworkContext::PreconnectSockets(
