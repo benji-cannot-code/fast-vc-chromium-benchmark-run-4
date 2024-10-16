8f7d62d720ced31ee72fb32f1642465b8b8dda34
Leon Masopust <lmasopust@google.com>
2024-10-16 14:27:38 +0000

    Revert "[views-ax] Migrate kName in OmniboxResultView"
    This reverts commit f4a3bc04149e814b27f4a912c5b85536041c1cdc.
    
    Reason for revert: Leads to consistent tast test failures on ChromeOS (see b/373772807) 
    
    Original change's description:
    > [views-ax] Migrate kName in OmniboxResultView
    >
    > This CL migrates the accessible name setter for OmniboxResultView from
    > being calculated only when requested in `GetAccessibleNodeData`
    > ("pull") to updating the value in the ViewAccessibility cache whenever
    > it changes ("push").
    >
    > The logic that was used to calculate the name in
    > `GetAccessibleNodeData` was moved to a new private method named
    > `UpdateAccessibleName`, which gets called at time of construction,
    > and when the result view's selection state changes, since the
    > selection state can impact the value of the accessible name.
    >
    > `OmniboxPopupViewViewsTest.AccessibleResultName` is added to test
    > this functionality.
    >
    > Because the accessible names are being added to the cache at the
    > time of construction, and the selection state only changes the
    > value of the accessible name in certain circumstances, we no
    > longer expect a TextChanged event to be fired in the nominal
    > case where the View's selected state changes. As such, this CL
    > removes checks for the number of TextChanged events fired in
    > `OmniboxPopupViewViewsTest.EmitAccessibilityEvents`.
    >
    > This CL also updates how current OmniboxPopupViewViewsTests get
    > the value of the accessible name. Instead of using the
    > `TestAXEventObserver` to get the accessible name every time a
    > Selection event is fired (via `selected_option_name()`), the tests
    > directly use `ViewAccessibility::GetAccessibleNodeData` to get the
    > accessible node data from the cache as needed. This follows the test
    > patterns we are using throughout the Views code as part of the ViewsAX
    > project to mock AT behavior.
    >
    > This change is part of the ViewsAX project:
    > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit
    >
    > Bug: 325137417
    > Change-Id: I67e719566e4d5b5497f29c66a30fd041cffb8e29
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906787
    > Reviewed-by: Taylor Bergquist <tbergquist@chromium.org>
    > Reviewed-by: Patrick Noland <pnoland@chromium.org>
    > Reviewed-by: Javier Contreras <javiercon@microsoft.com>
    > Commit-Queue: Evelynn Kaplan <evelynn.kaplan@microsoft.com>
    > Cr-Commit-Position: refs/heads/main@{#1368482}
    
    Bug: 325137417
    Change-Id: I816c3f63eecbfed2669cf637ead1523111123c06
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938420
    Commit-Queue: Leon Masopust <lmasopust@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Leon Masopust <lmasopust@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369386}

--- chrome/browser/ui/views/omnibox/omnibox_result_view.h
+++ chrome/browser/ui/views/omnibox/omnibox_result_view.h
@@ -87,6 +87,9 @@ class OmniboxResultView : public views::View {
   void ButtonPressed(OmniboxPopupSelection::LineState state,
                      const ui::Event& event);
 
+  // Helper to emit accessibility events (may only emit if conditions are met).
+  void EmitTextChangedAccessiblityEvent();
+
   void UpdateAccessibilityProperties();
 
   // views::View:
@@ -95,6 +98,7 @@ class OmniboxResultView : public views::View {
   void OnMouseReleased(const ui::MouseEvent& event) override;
   void OnMouseEntered(const ui::MouseEvent& event) override;
   void OnMouseExited(const ui::MouseEvent& event) override;
+  void GetAccessibleNodeData(ui::AXNodeData* node_data) override;
   void OnThemeChanged() override;
 
  private:
@@ -120,9 +124,6 @@ class OmniboxResultView : public views::View {
   // not the view is selected.
   void UpdateAccessibilitySelectedState();
 
-  // Updates the accessible name based on the current state.
-  void UpdateAccessibleName();
-
   // views::View:
   void OnBoundsChanged(const gfx::Rect& previous_bounds) override;
 
@@ -134,6 +135,9 @@ class OmniboxResultView : public views::View {
 
   // The data this class is built to display (the "Omnibox Result").
   AutocompleteMatch match_;
+
+  // Accessible name (enables to emit certain events).
+  std::u16string accessible_name_;
 
   // Weak pointers for easy reference.
 
