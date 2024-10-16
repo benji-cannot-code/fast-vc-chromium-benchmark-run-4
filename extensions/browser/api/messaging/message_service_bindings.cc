5d15d122e0052785110ef3a438ce4b6870ffc213
Dylan Cutler <dylancutler@google.com>
2024-10-16 01:28:12 +0000

    Check if RFH has a PolicyContainerHost in IsPortContextSandboxed
    This function is causing crashes because it is querying the RFH's
    active sandbox flags before the RFH is fully initialized.
    
    I am adding an extra guard clause calling HasPolicyContainerHost
    which prevents the crash. I added a DUMP_WILL_BE_NOTREACHED so
    we still get reports of when this occurs.
    
    Bug: 371820630,346386726
    Change-Id: I50bfcc0e3791b614a460ff136f172d8b7522dce7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5919522
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Commit-Queue: Dylan Cutler <dylancutler@google.com>
    Reviewed-by: Devlin Cronin <rdevlin.cronin@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369139}

--- extensions/browser/api/messaging/message_service_bindings.cc
+++ extensions/browser/api/messaging/message_service_bindings.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 
+#include "base/debug/crash_logging.h"
 #include "base/feature_list.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/types/optional_util.h"
@@ -53,6 +54,16 @@ bool IsPortContextSandboxed(RenderProcessHost& process,
     // ExtensionFrameHost::OpenChannelToExtension()). Ensure there are no
     // unexpected reports of this and then remove the early return here and also
     // in IsValidSourceUrl().
+    DUMP_WILL_BE_NOTREACHED();
+    return false;
+  }
+
+  if (!frame->HasPolicyContainerHost()) {
+    // If the WebContents has not fully initialized the RenderFrameHost yet.
+    // TODO(crbug.com/346386726): Remove the HasPolicyContainerHost() call once
+    // RenderFrameHost initialization order is fixed.
+    SCOPED_CRASH_KEY_NUMBER("EMF_INVALID_RFH", "lifecycle_state",
+                            static_cast<size_t>(frame->GetLifecycleState()));
     DUMP_WILL_BE_NOTREACHED();
     return false;
   }
