9ff56402b43f05cf7c390dd86bb833f8d2dfa06e
Kene Okoye <kenok@google.com>
2024-10-16 15:19:55 +0000

    Clean up the PrivacyGuidePreloadAndroid feature
    Bug: 373436715
    Change-Id: I4e23e7934a65be2cd85d4f8322fcf9bf532c39b2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933244
    Commit-Queue: Kene Okoye <kenok@google.com>
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369414}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -140,7 +140,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &features::kLoadingPredictorLimitPreconnectSocketCount,
     &features::kNotificationOneTapUnsubscribe,
     &features::kPrivacyGuideAndroid3,
-    &features::kPrivacyGuidePreloadAndroid,
     &features::kPrefetchBrowserInitiatedTriggers,
     &features::kPushMessagingDisallowSenderIDs,
     &features::kPwaUpdateDialogForIcon,
