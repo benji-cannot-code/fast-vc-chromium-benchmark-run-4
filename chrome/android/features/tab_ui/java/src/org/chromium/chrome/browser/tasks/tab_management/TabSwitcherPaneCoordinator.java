d1b486da662bd205d6e7f91759e302beb058619b
Sky Malice <skym@chromium.org>
2024-10-16 20:03:57 +0000

    Apply labels for persistent messages.
    Bug: 369188289
    Change-Id: I7ab2b5f19fb872f4c3af725fb2cb9863d7c32350
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5918049
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369589}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherPaneCoordinator.java
@@ -39,6 +39,7 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.browser_controls.BrowserControlsStateProvider;
+import org.chromium.chrome.browser.data_sharing.DataSharingServiceFactory;
 import org.chromium.chrome.browser.data_sharing.DataSharingTabManager;
 import org.chromium.chrome.browser.data_sharing.ui.invitation_dialog.DataSharingInvitationDialogCoordinator;
 import org.chromium.chrome.browser.hub.HubFieldTrial;
@@ -64,6 +65,8 @@ import org.chromium.components.browser_ui.desktop_windowing.DesktopWindowStatePr
 import org.chromium.components.browser_ui.edge_to_edge.EdgeToEdgePadAdjuster;
 import org.chromium.components.browser_ui.widget.gesture.BackPressHandler;
 import org.chromium.components.browser_ui.widget.scrim.ScrimCoordinator;
+import org.chromium.components.data_sharing.DataSharingService;
+import org.chromium.components.data_sharing.ServiceStatus;
 import org.chromium.ui.base.DeviceFormFactor;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 import org.chromium.ui.modelutil.PropertyModel;
@@ -139,6 +142,7 @@ public class TabSwitcherPaneCoordinator implements BackPressHandler {
             new OneshotSupplierImpl<>();
     private final Callback<EdgeToEdgeController> mOnEdgeToEdgeControllerChangedCallback =
             new ValueChangedCallback<>(this::onEdgeToEdgeControllerChanged);
+    private final @Nullable TabGroupLabeller mTabGroupLabeller;
 
     /** Lazily initialized when shown. */
     private @Nullable TabGridDialogCoordinator mTabGridDialogCoordinator;
@@ -377,6 +381,19 @@ public class TabSwitcherPaneCoordinator implements BackPressHandler {
             mMessageManager = messageManager;
             mMessageManager.registerMessages(tabListCoordinator);
 
+            DataSharingService dataSharingService =
+                    DataSharingServiceFactory.getForProfile(profile);
+            @NonNull ServiceStatus serviceStatus = dataSharingService.getServiceStatus();
+            if (serviceStatus.isAllowedToJoin()) {
+                mTabGroupLabeller =
+                        new TabGroupLabeller(
+                                profile,
+                                mTabListCoordinator.getTabListNotificationHandler(),
+                                tabGroupModelFilterSupplier);
+            } else {
+                mTabGroupLabeller = null;
+            }
+
             mOnVisibilityChanged.onResult(isVisibleSupplier.addObserver(mOnVisibilityChanged));
         }
     }
@@ -399,6 +416,9 @@ public class TabSwitcherPaneCoordinator implements BackPressHandler {
         if (mEdgeToEdgePadAdjuster != null && mEdgeToEdgeSupplier.get() != null) {
             mEdgeToEdgeSupplier.get().unregisterAdjuster(mEdgeToEdgePadAdjuster);
             mEdgeToEdgePadAdjuster = null;
+        }
+        if (mTabGroupLabeller != null) {
+            mTabGroupLabeller.destroy();
         }
     }
 
@@ -433,6 +453,9 @@ public class TabSwitcherPaneCoordinator implements BackPressHandler {
                 tabList == null ? null : tabs, /* quickMode= */ false);
         mMessageManager.afterReset(tabs.size());
         mTabListOnScrollListener.postUpdate(mTabListCoordinator.getContainerView());
+        if (mTabGroupLabeller != null) {
+            mTabGroupLabeller.showAll();
+        }
     }
 
     /** Performs soft cleanup which removes thumbnails to relieve memory usage. */
