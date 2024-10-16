fa5d9a58597d9065d9c6f707793d0acefd2a8b86
Eriko Kurimoto <elkurin@chromium.org>
2024-10-16 01:12:21 +0000

    [1/N] ash: Remove AppType::LACROS
    This CL removes most of the part in //ash.
    
    Bug: b:373527988
    Change-Id: I926ee0a90ff2d7da23a60a061c0592509eabf92d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933452
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369134}

--- ash/wm/toplevel_window_event_handler.cc
+++ ash/wm/toplevel_window_event_handler.cc
@@ -853,9 +853,8 @@ aura::Window* ToplevelWindowEventHandler::GetTargetForClientAreaGesture(
     return nullptr;
   }
 
-  auto app_type = toplevel->GetProperty(chromeos::kAppTypeKey);
-  if (app_type == chromeos::AppType::BROWSER ||
-      app_type == chromeos::AppType::LACROS) {
+  if (toplevel->GetProperty(chromeos::kAppTypeKey) ==
+      chromeos::AppType::BROWSER) {
     return nullptr;
   }
 
