5be9db233dbbe75bd9d6a94356328486162d43bc
Yi Xu <yiyix@chromium.org>
2024-10-16 21:21:01 +0000

    Centralize the samplingOptions setting in paint_op
    Relocate the sampling options setting from base_rendering_context_2d
    to paint_op.
    
    Bug: 40044062
    
    Change-Id: I0b95811ba670a084943b8706db00be71f671ec4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921547
    Commit-Queue: Yi Xu <yiyix@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369639}

--- cc/paint/paint_op.cc
+++ cc/paint/paint_op.cc
@@ -1369,7 +1369,15 @@ void DrawImageRectOp::RasterWithFlags(const DrawImageRectOp* op,
 
   if (!params.image_provider) {
     SkRect adjusted_src = AdjustSrcRectForScale(op->src, op->scale_adjustment);
-    flags->DrawToSk(canvas, [op, adjusted_src](SkCanvas* c, const SkPaint& p) {
+    SkM44 matrix = canvas->getLocalToDevice() *
+                   SkM44(SkMatrix::RectToRect(adjusted_src, op->dst));
+    PaintFlags::ScalingOperation scale =
+        MatrixToScalingOperation(matrix.asM33());
+    PaintFlags::FilterQuality quality = sampling_to_quality(op->sampling);
+    SkSamplingOptions sampling =
+        PaintFlags::FilterQualityToSkSamplingOptions(quality, scale);
+    flags->DrawToSk(canvas, [op, adjusted_src, sampling](SkCanvas* c,
+                                                         const SkPaint& p) {
       sk_sp<SkImage> sk_image;
       if (op->image.IsTextureBacked()) {
         sk_image = op->image.GetAcceleratedSkImage();
@@ -1387,7 +1395,7 @@ void DrawImageRectOp::RasterWithFlags(const DrawImageRectOp* op,
         skia::DrawGainmapImageRect(
             c, op->image.cached_sk_image_, op->image.gainmap_sk_image_,
             op->image.gainmap_info_.value(), op->image.target_hdr_headroom_,
-            adjusted_src, op->dst, op->sampling, p);
+            adjusted_src, op->dst, sampling, p);
         return;
       }
 
@@ -1399,12 +1407,12 @@ void DrawImageRectOp::RasterWithFlags(const DrawImageRectOp* op,
             tonemap_paint, op->image, c->imageInfo().refColorSpace());
         sk_image =
             sk_image->reinterpretColorSpace(c->imageInfo().refColorSpace());
-        DrawImageRect(c, sk_image.get(), adjusted_src, op->dst, op->sampling,
+        DrawImageRect(c, sk_image.get(), adjusted_src, op->dst, sampling,
                       &tonemap_paint, op->constraint);
         return;
       }
 
-      DrawImageRect(c, sk_image.get(), adjusted_src, op->dst, op->sampling, &p,
+      DrawImageRect(c, sk_image.get(), adjusted_src, op->dst, sampling, &p,
                     op->constraint);
     });
     return;
@@ -1435,12 +1443,12 @@ void DrawImageRectOp::RasterWithFlags(const DrawImageRectOp* op,
                          decoded_image.src_rect_offset().height());
   adjusted_src = AdjustSrcRectForScale(adjusted_src, scale_adjustment);
   PaintFlags::ScalingOperation scale = MatrixToScalingOperation(matrix.asM33());
-  flags->DrawToSk(canvas, [op, &decoded_image, adjusted_src, scale](
+  SkSamplingOptions sampling = PaintFlags::FilterQualityToSkSamplingOptions(
+      decoded_image.filter_quality(), scale);
+  flags->DrawToSk(canvas, [op, &decoded_image, adjusted_src, sampling](
                               SkCanvas* c, const SkPaint& p) {
-    SkSamplingOptions options = PaintFlags::FilterQualityToSkSamplingOptions(
-        decoded_image.filter_quality(), scale);
     DrawImageRect(c, decoded_image.image().get(), adjusted_src, op->dst,
-                  options, &p, op->constraint);
+                  sampling, &p, op->constraint);
   });
 }
 
