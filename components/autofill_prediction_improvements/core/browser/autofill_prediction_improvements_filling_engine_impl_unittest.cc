dc1d6656171ef1e4129caf7ec59392ff082487dd
Steven Holte <holte@google.com>
2024-10-17 23:41:11 +0000

    Use AnyWrapProto in components/autofill_prediction_improvements.
    For clarity and consistency.
    
    This CL was uploaded by git cl split.
    
    Bug: 373711168
    Change-Id: I8776f96724e3d1c8a27bcf324e38c47ff93d1c51
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941511
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Auto-Submit: Steven Holte <holte@chromium.org>
    Commit-Queue: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370327}

--- components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_filling_engine_impl_unittest.cc
+++ components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_filling_engine_impl_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/common/autofill_test_utils.h"
 #include "components/autofill/core/common/form_data.h"
 #include "components/optimization_guide/core/mock_optimization_guide_model_executor.h"
+#include "components/optimization_guide/core/optimization_guide_proto_util.h"
 #include "components/optimization_guide/proto/features/common_quality_data.pb.h"
 #include "components/optimization_guide/proto/features/forms_predictions.pb.h"
 #include "components/user_annotations/test_user_annotations_service.h"
@@ -112,16 +113,14 @@ TEST_F(AutofillPredictionImprovementsFillingEngineImplTest, EndToEnd) {
                      "Country - response equals selected value, not filled", "",
                      "Spain", 5);
   AddFieldToResponse(response, "Field has value, not filled", "", "value", 6);
-  optimization_guide::proto::Any any;
-  any.set_type_url(response.GetTypeName());
-  response.SerializeToString(any.mutable_value());
   EXPECT_CALL(
       *model_executor(),
       ExecuteModel(
           optimization_guide::ModelBasedCapabilityKey::kFormsPredictions, _, _,
           An<optimization_guide::
                  OptimizationGuideModelExecutionResultCallback>()))
-      .WillOnce(base::test::RunOnceCallback<3>(any, /*log_entry=*/nullptr));
+      .WillOnce(base::test::RunOnceCallback<3>(
+          optimization_guide::AnyWrapProto(response), /*log_entry=*/nullptr));
 
   autofill::test::FormDescription form_description = {
       .fields = {
