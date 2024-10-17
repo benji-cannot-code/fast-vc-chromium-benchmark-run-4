8fa1f4b93fe64b4ebb7702bc921423492f21a0a5
Avi Drissman <avi@chromium.org>
2024-10-17 17:24:16 +0000

    Modernize codec usage in favicon
    Codec calls have new API that uses std::optional and spans. Switch usage
    to those new APIs, and lightly modernize the calling code.
    
    Bug: 370696612
    Change-Id: I748ab79746ec237e0477201bca235dd751ca664b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933686
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370109}

--- chrome/browser/android/favicon_helper.cc
+++ chrome/browser/android/favicon_helper.cc
@@ -335,9 +335,7 @@ void FaviconHelper::OnFaviconBitmapResultAvailable(
   // Convert favicon_image_result to java objects.
   ScopedJavaLocalRef<jobject> j_favicon_bitmap;
   if (result.is_valid()) {
-    SkBitmap favicon_bitmap;
-    gfx::PNGCodec::Decode(result.bitmap_data->front(),
-                          result.bitmap_data->size(), &favicon_bitmap);
+    SkBitmap favicon_bitmap = gfx::PNGCodec::Decode(*result.bitmap_data);
     if (!favicon_bitmap.isNull()) {
       j_favicon_bitmap = gfx::ConvertToJavaBitmap(favicon_bitmap);
     }
@@ -355,15 +353,12 @@ void FaviconHelper::OnComposedFaviconBitmapResultsAvailable(
   JNIEnv* env = AttachCurrentThread();
   std::vector<SkBitmap> result_bitmaps;
   std::vector<GURL> icon_url_vector;
-  for (size_t i = 0; i < results.size(); i++) {
-    favicon_base::FaviconRawBitmapResult result = results[i];
+  for (auto result : results) {
     if (!result.is_valid()) {
       continue;
     }
-    SkBitmap favicon_bitmap;
     icon_url_vector.push_back(result.icon_url);
-    gfx::PNGCodec::Decode(result.bitmap_data->front(),
-                          result.bitmap_data->size(), &favicon_bitmap);
+    SkBitmap favicon_bitmap = gfx::PNGCodec::Decode(*result.bitmap_data);
     result_bitmaps.push_back(std::move(favicon_bitmap));
   }
   ScopedJavaLocalRef<jobject> j_favicon_bitmap;
