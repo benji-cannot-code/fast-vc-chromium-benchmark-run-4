5a1ac6c8c5dade7c75770886f622845b05bf9e7f
Helmut Januschka <helmut@januschka.com>
2024-10-17 21:09:58 +0000

    use std::optional base::GetFileSize() in //content
    Bug: 371234479
    Change-Id: Ic324561e0c6fd20d85e5895fb45c89ca5866876a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905147
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370248}

--- content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
+++ content/browser/webrtc/webrtc_audio_debug_recordings_browsertest.cc
@@ -157,9 +157,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   std::vector<base::FilePath> input_files =
       GetRecordingFileNames(FILE_PATH_LITERAL("input"), base_file_path);
   EXPECT_EQ(input_files.size(), 1u);
-  int64_t file_size = 0;
-  EXPECT_TRUE(base::GetFileSize(input_files[0], &file_size));
-  EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+  std::optional<int64_t> file_size = base::GetFileSize(input_files[0]);
+  ASSERT_TRUE(file_size.has_value());
+  EXPECT_GT(file_size.value(), kWaveHeaderSizeBytes);
   EXPECT_TRUE(DeleteFileWithRetryAfterPause(input_files[0]));
 
   // Verify that the expected output audio files exist and contain some data.
@@ -169,9 +169,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   EXPECT_EQ(output_files.size(),
             media::IsChromeWideEchoCancellationEnabled() ? 1u : 2u);
   for (const base::FilePath& file_path : output_files) {
-    file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
-    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    file_size = base::GetFileSize(file_path);
+    ASSERT_TRUE(file_size.has_value());
+    EXPECT_GT(file_size.value(), kWaveHeaderSizeBytes);
     EXPECT_TRUE(DeleteFileWithRetryAfterPause(file_path));
   }
 
@@ -185,9 +185,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
   base::FilePath file_path =
       GetExpectedAecDumpFileName(base_file_path, render_process_id);
   EXPECT_TRUE(base::PathExists(file_path));
-  file_size = 0;
-  EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
-  EXPECT_GT(file_size, 0);
+  file_size = base::GetFileSize(file_path);
+  ASSERT_TRUE(file_size.has_value());
+  EXPECT_GT(file_size.value(), 0);
   EXPECT_TRUE(DeleteFileWithRetryAfterPause(file_path));
 
   // Verify that no other files exist and remove temp dir.
@@ -299,16 +299,14 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
   WebRTCInternals::GetInstance()->DisableAudioDebugRecordings();
 
-  int64_t file_size = 0;
-
   // Verify that the expected input audio files exist and contain some data.
   std::vector<base::FilePath> input_files =
       GetRecordingFileNames(FILE_PATH_LITERAL("input"), base_file_path);
   EXPECT_EQ(input_files.size(), 2u);
   for (const base::FilePath& file_path : input_files) {
-    file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
-    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    std::optional<int64_t> file_size = base::GetFileSize(file_path);
+    ASSERT_TRUE(file_size.has_value());
+    EXPECT_GT(file_size.value(), kWaveHeaderSizeBytes);
     EXPECT_TRUE(DeleteFileWithRetryAfterPause(file_path));
   }
 
@@ -319,9 +317,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
       GetRecordingFileNames(FILE_PATH_LITERAL("output"), base_file_path);
   EXPECT_EQ(output_files.size(), 4u);
   for (const base::FilePath& file_path : output_files) {
-    file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
-    EXPECT_GT(file_size, kWaveHeaderSizeBytes);
+    std::optional<int64_t> file_size = base::GetFileSize(file_path);
+    ASSERT_TRUE(file_size.has_value());
+    EXPECT_GT(file_size.value(), kWaveHeaderSizeBytes);
     EXPECT_TRUE(DeleteFileWithRetryAfterPause(file_path));
   }
 
@@ -336,9 +334,9 @@ IN_PROC_BROWSER_TEST_F(WebRtcAudioDebugRecordingsBrowserTest,
 
     file_path = GetExpectedAecDumpFileName(base_file_path, render_process_id);
     EXPECT_TRUE(base::PathExists(file_path));
-    file_size = 0;
-    EXPECT_TRUE(base::GetFileSize(file_path, &file_size));
-    EXPECT_GT(file_size, 0);
+    std::optional<int64_t> file_size = base::GetFileSize(file_path);
+    ASSERT_TRUE(file_size.has_value());
+    EXPECT_GT(file_size.value(), 0);
     EXPECT_TRUE(DeleteFileWithRetryAfterPause(file_path));
   }
 
