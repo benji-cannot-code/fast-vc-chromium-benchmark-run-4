dcc63d2df5153b5d391d78b8e838dfcc7c4af7a0
Marijn Kruisselbrink <mek@chromium.org>
2024-10-17 18:14:43 +0000

    [PWAs UNO] Add metrics to measure apps in "bad" states.
    Add metrics for apps that are user installed but not syncing while
    sync is enabled, as well as apps that are suggested by sync but sync
    is no longer enabled.
    
    Bug: 374010066
    Change-Id: I036ff6952ae3fc127396c27ea492fc6bff919515
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939782
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370145}

--- chrome/browser/web_applications/web_app_registrar.h
+++ chrome/browser/web_applications/web_app_registrar.h
@@ -71,6 +71,7 @@ struct ValueWithPolicy {
 using DiyAppCount = base::StrongAlias<class DiyAppCountTag, int>;
 using InstallableAppCount =
     base::StrongAlias<class InstallableAppCountTag, int>;
+using NonSyncingAppCount = base::StrongAlias<class NonSyncingAppCountTag, int>;
 
 // A registry model. This is a read-only container, which owns WebApp objects.
 class WebAppRegistrar {
@@ -677,8 +678,9 @@ class WebAppRegistrar {
   int CountUserInstalledNotLocallyInstalledApps() const;
 
   // Count number of all apps which are installed by user, including DIY apps.
+  // Also counts which ones of these are not included in sync.
   // Requires app registry to be in a ready state.
-  std::tuple<DiyAppCount, InstallableAppCount>
+  std::tuple<DiyAppCount, InstallableAppCount, NonSyncingAppCount>
   CountTotalUserInstalledAppsIncludingDiy() const;
 
   const raw_ptr<Profile> profile_;
