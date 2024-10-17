0c820422f9ffec76d070793f7859006bdcc04872
Vignesh Shenvi <vshenvi@google.com>
2024-10-17 23:01:34 +0000

    Restore OnTask bundle content on app launch
    This change facilitates the OnTask session manager to restore bundle
    content on app launch should the user close the app manually and launch
    it through the shelf.
    
    Bug: b:372384432
    Change-Id: Icbb734188754c625185eb37452a335f8115aaf9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940121
    Commit-Queue: Vignesh Shenvi <vshenvi@google.com>
    Reviewed-by: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370301}

--- chromeos/ash/components/boca/on_task/on_task_session_manager.h
+++ chromeos/ash/components/boca/on_task/on_task_session_manager.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::boca {
 
+class OnTaskSessionManagerTest;
+
 // Session manager implementation that is primarily used for configuring and
 // managing OnTask components and services throughout a Boca session.
 class OnTaskSessionManager : public boca::BocaSessionManager::Observer {
@@ -40,9 +42,13 @@ class OnTaskSessionManager : public boca::BocaSessionManager::Observer {
                         const ::boca::UserIdentity& producer) override;
   void OnSessionEnded(const std::string& session_id) override;
   void OnBundleUpdated(const ::boca::Bundle& bundle) override;
+  void OnAppReloaded() override;
+
   ActiveTabTracker* active_tab_tracker() { return &active_tab_tracker_; }
 
  private:
+  friend class OnTaskSessionManagerTest;
+
   // Helper class that is used to launch the Boca system web app as well as
   // manage all interactions with the Boca system web app while it is being
   // spawned.
