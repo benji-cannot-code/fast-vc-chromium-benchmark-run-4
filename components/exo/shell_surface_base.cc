12bdf3e8b582d097de69c09d877299ec1c8321df
Eriko Kurimoto <elkurin@chromium.org>
2024-10-17 00:57:19 +0000

    [5/N] ash: Remove AppType::LACROS
    This CL removes AppType::LACROS usage in FrameThrottlingController.
    
    It also removes `lacros_candidates` and lacros specific methods from
    FrameThrottlingController.
    
    Bug: b:373527988
    Change-Id: I856894036138b61c74a677081d4c49b618e79f24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932430
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Commit-Queue: Eriko Kurimoto <elkurin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369738}

--- components/exo/shell_surface_base.cc
+++ components/exo/shell_surface_base.cc
@@ -1024,8 +1024,7 @@ void ShellSurfaceBase::RebindRootSurface(Surface* root_surface,
   if (window) {
     // Int properties.
     for (auto* const key :
-         {aura::client::kSkipImeProcessing, chromeos::kFrameRestoreLookKey,
-          ash::kFrameRateThrottleKey}) {
+         {aura::client::kSkipImeProcessing, chromeos::kFrameRestoreLookKey}) {
       if (base::Contains(window->GetAllPropertyKeys(), key)) {
         OnWindowPropertyChanged(window, key,
                                 /*old_value(unused)=*/0);
@@ -1609,9 +1608,6 @@ void ShellSurfaceBase::OnWindowPropertyChanged(aura::Window* window,
         window->GetProperty(chromeos::kFrameRestoreLookKey));
   } else if (key == aura::client::kWindowWorkspaceKey) {
     root_surface()->OnDeskChanged(GetWindowDeskStateChanged(window));
-  } else if (key == ash::kFrameRateThrottleKey) {
-    root_surface()->ThrottleFrameRate(
-        window->GetProperty(ash::kFrameRateThrottleKey));
   }
 }
 
