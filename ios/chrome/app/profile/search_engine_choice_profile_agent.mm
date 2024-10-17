caaa2d587bdfc44e32f3c856e8418f428b8bfd8b
Federica Germinario <fedegermi@google.com>
2024-10-17 09:43:46 +0000

    [ios] Move SearchEngineChoiceProfileAgent under i/c/app/profile/
    FirstRunProfileAgent was recently refactored to become a profile
    agent (it was an app agent).
    This CL moves the code from:
       ios/chrome/app/
    to:
       ios/chrome/app/profile/
    
    Bug: none
    Change-Id: I781faf96713c6851525668bfe9389083a4c2eef0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938380
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1369888}

--- ios/chrome/app/profile/search_engine_choice_profile_agent.mm
+++ ios/chrome/app/profile/search_engine_choice_profile_agent.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/app/search_engine_choice_profile_agent.h"
+#import "ios/chrome/app/profile/search_engine_choice_profile_agent.h"
 
 #import <memory>
 
