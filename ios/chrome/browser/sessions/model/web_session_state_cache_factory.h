f2ac7beb1281baed3fe5b4bd469688666dba979b
Federica Germinario <fedegermi@google.com>
2024-10-17 09:59:52 +0000

    [ios] Remove GetForBrowserState from ios/chrome/browser/sessions/*
    Remove legacy GetForBrowserState.
    
    Bug: 359812085
    Change-Id: I0eabaf48cc47462908870ffd6b636e16df55316e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938782
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369895}

--- ios/chrome/browser/sessions/model/web_session_state_cache_factory.h
+++ ios/chrome/browser/sessions/model/web_session_state_cache_factory.h
@@ -18,9 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // ProfileIOS.
 class WebSessionStateCacheFactory : public BrowserStateKeyedServiceFactory {
  public:
-  // TODO(crbug.com/358301380): remove this method.
-  static WebSessionStateCache* GetForBrowserState(ProfileIOS* profile);
-
   static WebSessionStateCache* GetForProfile(ProfileIOS* profile);
   static WebSessionStateCacheFactory* GetInstance();
 
