61ac9e130adca03c450378cf5c5bb0645cf567da
Timofey Chudakov <tchudakov@google.com>
2024-10-17 09:24:52 +0000

    [Android] Access all plus addresses bottom sheet strings directly.
    Previously, these strings were passed from the C++ side.
    
    Bug: 373823038
    Change-Id: I6878fa0baed344d68f277926814e66cf35c3ea12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937823
    Reviewed-by: Friedrich Hauser <friedrichh@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369876}

--- chrome/browser/ui/android/plus_addresses/javatests/src/org/chromium/chrome/browser/ui/plus_addresses/AllPlusAddressesBottomSheetRenderTest.java
+++ chrome/browser/ui/android/plus_addresses/javatests/src/org/chromium/chrome/browser/ui/plus_addresses/AllPlusAddressesBottomSheetRenderTest.java
@@ -17,7 +17,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.mockito.quality.Strictness;
@@ -52,9 +51,6 @@ import java.util.List;
 @ParameterAnnotations.UseRunnerDelegate(ChromeJUnit4RunnerDelegate.class)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class AllPlusAddressesBottomSheetRenderTest {
-    private static final String BOTTOMSHEET_TITLE = "Bottom sheet title";
-    private static final String BOTTOMSHEET_WARNING = "Bottom sheet warning";
-    private static final String BOTTOMSHEET_QUERY_HINT = "Query hint";
     private static final PlusProfile PROFILE_1 =
             new PlusProfile("foo@gmail.com", "google.com", "https://google.com");
     private static final PlusProfile PROFILE_2 =
@@ -79,7 +75,7 @@ public class AllPlusAddressesBottomSheetRenderTest {
     @Rule
     public final ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(1)
+                    .setRevision(2)
                     .setBugComponent(Component.UI_BROWSER_AUTOFILL)
                     .build();
 
@@ -95,7 +91,6 @@ public class AllPlusAddressesBottomSheetRenderTest {
 
     @Before
     public void setUp() throws InterruptedException {
-        MockitoAnnotations.initMocks(this);
         mActivityTestRule.startMainActivityOnBlankPage();
         mActivityTestRule.waitForActivityCompletelyLoaded();
     }
@@ -108,9 +103,6 @@ public class AllPlusAddressesBottomSheetRenderTest {
                 () -> {
                     AllPlusAddressesBottomSheetUIInfo uiInfo =
                             new AllPlusAddressesBottomSheetUIInfo();
-                    uiInfo.setTitle(BOTTOMSHEET_TITLE);
-                    uiInfo.setWarning(BOTTOMSHEET_WARNING);
-                    uiInfo.setQueryHint(BOTTOMSHEET_QUERY_HINT);
                     uiInfo.setPlusProfiles(List.of(PROFILE_1, PROFILE_2, PROFILE_3, PROFILE_4));
 
                     ChromeTabbedActivity activity = mActivityTestRule.getActivity();
