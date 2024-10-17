5ff55585a5184ed4fce9371f02204df6aecbaaa0
Piotr Bialecki <bialpio@chromium.org>
2024-10-17 23:02:44 +0000

    Spanification: switch to base::HeapArray in network_interfaces_win.cc
    Bug: 40285824
    Change-Id: I7af9be62f4748798ea1cee8c971126d06b98ef2c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941498
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370303}

--- net/base/network_interfaces_win.cc
+++ net/base/network_interfaces_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <string_view>
 
+#include "base/containers/heap_array.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
 #include "base/strings/escape.h"
@@ -227,7 +228,7 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
   // Initial buffer allocated on stack.
   char initial_buf[INITIAL_BUFFER_SIZE];
   // Dynamic buffer in case initial buffer isn't large enough.
-  std::unique_ptr<char[]> buf;
+  base::HeapArray<char> buf;
 
   IP_ADAPTER_ADDRESSES* adapters = nullptr;
   {
@@ -245,8 +246,8 @@ bool GetNetworkList(NetworkInterfaceList* networks, int policy) {
     for (int tries = 1; result == ERROR_BUFFER_OVERFLOW &&
                         tries < MAX_GETADAPTERSADDRESSES_TRIES;
          ++tries) {
-      buf = std::make_unique<char[]>(len);
-      adapters = reinterpret_cast<IP_ADAPTER_ADDRESSES*>(buf.get());
+      buf = base::HeapArray<char>::Uninit(len);
+      adapters = reinterpret_cast<IP_ADAPTER_ADDRESSES*>(buf.data());
       result = GetAdaptersAddresses(AF_UNSPEC, flags, nullptr, adapters, &len);
     }
 
