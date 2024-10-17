5312ac744893fb72dbd31e5a80e4b2c8aff72fd8
Quentin Pubert <qpubert@google.com>
2024-10-17 13:11:56 +0000

    [iOS] Fix Drive file picker toolbar animation + detents animation
    This CL fixes an issue where changing the additionalSafeAreaInsets of
    the DriveFilePickerNavigationController didn't layout the associated
    view if a UIViewController was being pushed at the same time, leading to
    the toolbar staying in the middle of the screen. It also switches the
    sheet to a large detent when the keyboard has been shown, and disables
    the grabber + medium sheet detent on iPad.
    
    Bug: 373828021,373824678
    Change-Id: Ibecb5a90631f041ee8cdf2607df70371bef5a535
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938790
    Auto-Submit: Quentin Pubert <qpubert@google.com>
    Commit-Queue: Quentin Pubert <qpubert@google.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369957}

--- ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.mm
+++ ios/chrome/browser/drive_file_picker/ui/drive_file_picker_navigation_controller.mm
@@ -7,6 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/task/sequenced_task_runner.h"
 #import "ios/chrome/browser/drive_file_picker/ui/drive_file_picker_constants.h"
+#import "ui/base/device_form_factor.h"
+
+@interface DriveFilePickerNavigationController () <
+    UINavigationControllerDelegate>
+@end
 
 @implementation DriveFilePickerNavigationController
 
@@ -15,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.accessibilityIdentifier = kDriveFilePickerAccessibilityIdentifier;
+  self.delegate = self;
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -37,6 +43,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
+#pragma mark - UINavigationControllerDelegate
+
+- (void)navigationController:(UINavigationController*)navigationController
+       didShowViewController:(UIViewController*)viewController
+                    animated:(BOOL)animated {
+  // Layout `self.view` to account for new additional safe area insets.
+  [self.view setNeedsLayout];
+}
+
 #pragma mark - Private
 
 // Called when a `UIKeyboardWillChangeFrameNotification` is received.
@@ -46,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CGRect keyboardFrameEnd =
       [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
   if (CGRectGetMinY(keyboardFrameBegin) > CGRectGetMinY(keyboardFrameEnd)) {
+    // If keyboard is going up, do nothing for now.
     return;
   }
   // Update the additional safe area inset asynchronously to ensure the view has
@@ -62,7 +78,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   CGRect keyboardFrameEnd =
       [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
   if (CGRectGetMinY(keyboardFrameBegin) < CGRectGetMinY(keyboardFrameEnd)) {
+    // If the keyboard is going down, then `keyboardWillChangeFrame:` already
+    // updated the additional safe area insets to account for the keyboard.
     return;
+  }
+  // Since the keyboard went up, switch to a large detent on phones.
+  if (ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_PHONE) {
+    self.sheetPresentationController.selectedDetentIdentifier =
+        UISheetPresentationControllerDetentIdentifierLarge;
   }
   // Update the additional safe area inset asynchronously to ensure the view has
   // already moved to its new position, as it might move in reaction to the
