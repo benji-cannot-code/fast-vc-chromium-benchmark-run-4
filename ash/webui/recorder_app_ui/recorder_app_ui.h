1cc2fdf2b6479a4a63d736518489bc9a3b0885eb
hsuanling <hsuanling@google.com>
2024-10-17 07:57:30 +0000

    recorder: Add language param to mojom
    Browser (C++) and renderer (TS) pass language param when adding SODA
    monitor, installing SODA, and load speech recognizer. The following CL
    will expose the language parameter to `platformHandler` interface.
    
    Note that currently we only support en-US until language picker UI is
    ready.
    
    Bug: b:359505463
    Test: manually
    Change-Id: Icdbe4b23747dd7b1d2df488d68224ba9492c5ff7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940212
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Jennifer Ling <hsuanling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369841}

--- ash/webui/recorder_app_ui/recorder_app_ui.h
+++ ash/webui/recorder_app_ui/recorder_app_ui.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sequence_checker.h"
 #include "chromeos/services/machine_learning/public/mojom/machine_learning_service.mojom.h"
 #include "chromeos/services/machine_learning/public/mojom/soda.mojom.h"
+#include "components/soda/constants.h"
 #include "components/soda/soda_installer.h"
 #include "mojo/public/cpp/bindings/receiver_set.h"
 #include "mojo/public/cpp/bindings/remote_set.h"
@@ -91,7 +92,8 @@ class RecorderAppUI
 
   mojo::Remote<MachineLearningService>& GetMlService();
 
-  void UpdateSodaState(ModelState state);
+  void UpdateSodaState(const speech::LanguageCode& language_code,
+                       ModelState state);
 
   void GetPlatformModelStateCallback(
       const base::Uuid& model_id,
@@ -102,6 +104,10 @@ class RecorderAppUI
   void GetMicrophoneInfoWithDeviceId(
       GetMicrophoneInfoCallback callback,
       const std::optional<std::string>& device_id);
+
+  bool IsSodaAvailable(const speech::LanguageCode& language_code);
+
+  ModelState GetSodaState(const speech::LanguageCode& language_code);
 
   // recorder_app::mojom::PageHandler:
   void LoadModel(
@@ -125,13 +131,16 @@ class RecorderAppUI
       ::mojo::PendingRemote<recorder_app::mojom::ModelStateMonitor> monitor,
       AddModelMonitorCallback callback) override;
 
-  void LoadSpeechRecognizer(SodaClientMojoRemote soda_client,
+  void LoadSpeechRecognizer(const std::string& language,
+                            SodaClientMojoRemote soda_client,
                             SodaRecognizerMojoReceiver soda_recognizer,
                             LoadSpeechRecognizerCallback callback) override;
 
-  void InstallSoda(InstallSodaCallback callback) override;
+  void InstallSoda(const std::string& language,
+                   InstallSodaCallback callback) override;
 
   void AddSodaMonitor(
+      const std::string& language,
       ::mojo::PendingRemote<recorder_app::mojom::ModelStateMonitor> monitor,
       AddSodaMonitorCallback callback) override;
 
@@ -177,9 +186,11 @@ class RecorderAppUI
 
   mojo::ReceiverSet<recorder_app::mojom::PageHandler> page_receivers_;
 
-  mojo::RemoteSet<recorder_app::mojom::ModelStateMonitor> soda_monitors_;
+  std::map<speech::LanguageCode,
+           mojo::RemoteSet<recorder_app::mojom::ModelStateMonitor>>
+      soda_monitors_;
 
-  ModelState soda_state_;
+  base::flat_map<speech::LanguageCode, ModelState> soda_states_;
 
   std::map<base::Uuid, mojo::RemoteSet<recorder_app::mojom::ModelStateMonitor>>
       model_monitors_;
--- ash/webui/recorder_app_ui/resources/platforms/swa/handler.ts
+++ ash/webui/recorder_app_ui/resources/platforms/swa/handler.ts
@@ -46,6 +46,8 @@ import {
 } from './types.js';
 
 const CRASH_SERVER_PRODUCT_NAME = 'ChromeOS_RecorderApp';
+// TODO(hsuanling): Expose language parameter to platform handler interface.
+const DEFAULT_LANGUAGE = 'en-US';
 
 export class PlatformHandler extends PlatformHandlerBase {
   private readonly remote = MojoPageHandler.getRemote();
@@ -107,6 +109,7 @@ export class PlatformHandler extends PlatformHandlerBase {
     // return the cached state here, but we await here to avoid UI showing
     // temporary unavailabe state.
     const {state} = await this.remote.addSodaMonitor(
+      DEFAULT_LANGUAGE,
       monitor.$.bindNewPipeAndPassRemote(),
     );
     update(state);
@@ -128,7 +131,7 @@ export class PlatformHandler extends PlatformHandlerBase {
   override installSoda(): void {
     // We don't care about the returned promise as long as the request goes
     // through. The install progress is separately tracked in `sodaState`.
-    void this.remote.installSoda();
+    void this.remote.installSoda(DEFAULT_LANGUAGE);
   }
 
   override async newSodaSession(): Promise<SodaSession> {
@@ -136,6 +139,7 @@ export class PlatformHandler extends PlatformHandlerBase {
     const session = new MojoSodaSession(recognizer);
     const client = new SodaClientReceiver(session);
     const {result} = await this.remote.loadSpeechRecognizer(
+      DEFAULT_LANGUAGE,
       client.$.bindNewPipeAndPassRemote(),
       recognizer.$.bindNewPipeAndPassReceiver(),
     );
