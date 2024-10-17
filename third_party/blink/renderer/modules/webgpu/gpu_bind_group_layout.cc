e3a2799d00cc394571611f4ed508760f82a1c6b0
Piotr Bialecki <bialpio@chromium.org>
2024-10-17 18:16:01 +0000

    Spanification: switch to base::HeapArray in gpu_bind_group_layout.cc
    Bug: 40285824
    Change-Id: Ie61b7126b2ed2dacc4231a934150719dc069c2c7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939603
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Auto-Submit: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370147}

--- third_party/blink/renderer/modules/webgpu/gpu_bind_group_layout.cc
+++ third_party/blink/renderer/modules/webgpu/gpu_bind_group_layout.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/modules/webgpu/gpu_bind_group_layout.h"
 
+#include "base/containers/heap_array.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_bind_group_layout_descriptor.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_bind_group_layout_entry.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_gpu_buffer_binding_layout.h"
@@ -79,15 +80,15 @@ wgpu::BindGroupLayoutEntry AsDawnType(
 }
 
 // TODO(crbug.com/1069302): Remove when unused.
-std::unique_ptr<wgpu::BindGroupLayoutEntry[]> AsDawnType(
+base::HeapArray<wgpu::BindGroupLayoutEntry> AsDawnType(
     GPUDevice* device,
     const HeapVector<Member<GPUBindGroupLayoutEntry>>& webgpu_objects,
     Vector<std::unique_ptr<wgpu::ExternalTextureBindingLayout>>*
         externalTextureBindingLayouts,
     ExceptionState& exception_state) {
-  wtf_size_t count = webgpu_objects.size();
-  std::unique_ptr<wgpu::BindGroupLayoutEntry[]> dawn_objects(
-      new wgpu::BindGroupLayoutEntry[count]);
+  const wtf_size_t count = webgpu_objects.size();
+  auto dawn_objects =
+      base::HeapArray<wgpu::BindGroupLayoutEntry>::WithSize(count);
   for (wtf_size_t i = 0; i < count; ++i) {
     dawn_objects[i] =
         AsDawnType(device, webgpu_objects[i].Get(),
@@ -105,7 +106,7 @@ GPUBindGroupLayout* GPUBindGroupLayout::Create(
   DCHECK(webgpu_desc);
 
   uint32_t entry_count = 0;
-  std::unique_ptr<wgpu::BindGroupLayoutEntry[]> entries;
+  base::HeapArray<wgpu::BindGroupLayoutEntry> entries;
   Vector<std::unique_ptr<wgpu::ExternalTextureBindingLayout>>
       externalTextureBindingLayouts;
   entry_count = static_cast<uint32_t>(webgpu_desc->entries().size());
@@ -120,7 +121,7 @@ GPUBindGroupLayout* GPUBindGroupLayout::Create(
 
   wgpu::BindGroupLayoutDescriptor dawn_desc = {
       .entryCount = entry_count,
-      .entries = entries.get(),
+      .entries = entries.data(),
   };
   std::string label = webgpu_desc->label().Utf8();
   if (!label.empty()) {
