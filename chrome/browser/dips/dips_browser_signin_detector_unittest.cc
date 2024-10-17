e377ddd8254da106ca5b4479a23614bec0e51419
Ryan Tarpine <rtarpine@chromium.org>
2024-10-17 21:57:39 +0000

    [DIPS] Prepare dips_redirect_info.h for content/public/browser/.
    Because the public DIPSService interface includes an observer that provides redirect chain information, related types must be defined in the public Content API as well.
    
    That means that dips_redirect_info.h must be moved to content/public/browser/.
    
    Until now, that header itself included dips_utils.h, which pulls in lot more things. We don't want all those in the public API. dips_utils.h should go into content/browser/dips/.
    
    So instead, move the definitions of the few types that dips_redirect_info.h needs into dips_redirect_info.h itself.
    
    And while we're at it, try to break the dependence on dips_utils.h of other files that will be staying in //chrome. That means making local copies of the functions they call.
    
    Bug: 40883201
    Change-Id: I24c25a86eeeaf8d1da3350ad8490769c90d4fdfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5892231
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Commit-Queue: Ryan Tarpine <rtarpine@chromium.org>
    Reviewed-by: Anton Maliev <amaliev@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370274}

--- chrome/browser/dips/dips_browser_signin_detector_unittest.cc
+++ chrome/browser/dips/dips_browser_signin_detector_unittest.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/dips/dips_browser_signin_detector_factory.h"
 #include "chrome/browser/dips/dips_service.h"
 #include "chrome/browser/dips/dips_test_utils.h"
-#include "chrome/browser/dips/dips_utils.h"
 #include "chrome/browser/signin/chrome_signin_client_factory.h"
 #include "chrome/browser/signin/chrome_signin_client_test_util.h"
 #include "chrome/browser/signin/identity_test_environment_profile_adaptor.h"
--- chrome/browser/dips/dips_redirect_info.h
+++ chrome/browser/dips/dips_redirect_info.h
@@ -6,14 +6,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_DIPS_DIPS_REDIRECT_INFO_H_
 #define CHROME_BROWSER_DIPS_DIPS_REDIRECT_INFO_H_
 
+#include <optional>
 #include <string>
+#include <string_view>
 
 #include "base/functional/callback_forward.h"
 #include "base/time/time.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
 #include "url/gurl.h"
 
-#include "chrome/browser/dips/dips_utils.h"
+enum class DIPSCookieMode { kBlock3PC, kOffTheRecord_Block3PC };
+
+enum class DIPSRedirectType { kClient, kServer };
+
+// SiteDataAccessType:
+// NOTE: We use this type as a bitfield and log it. Don't
+// change the values or add additional members.
+enum class SiteDataAccessType {
+  kUnknown = -1,
+  kNone = 0,
+  kRead = 1,
+  kWrite = 2,
+  kReadWrite = 3
+};
+
+const char* DIPSCookieModeToString(DIPSCookieMode mode);
+const char* DIPSRedirectTypeToString(DIPSRedirectType type);
+std::string_view SiteDataAccessTypeToString(SiteDataAccessType type);
 
 struct UrlAndSourceId {
   GURL url;
