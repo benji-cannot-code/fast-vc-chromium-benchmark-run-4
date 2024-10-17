31cf645168450c46cc46938487eb5abc4eb69833
Daniel White <danieltwhite@google.com>
2024-10-17 15:41:30 +0000

    [iOS] Deprecate traitCollectionDidChange in TabStripTabCell
    Since traitCollectionDidChange is deprecated in iOS 17 TabStripTabCell
    was updated to use the new alternative system, registerForTraitChanges.
    
    Fixed: 349390648
    Change-Id: Ib8ca304eec9cd5c067589300dfd93651440894fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5891472
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370041}

--- ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_tab_cell.mm
+++ ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_tab_cell.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/symbols/symbols.h"
 #import "ios/chrome/browser/shared/ui/util/image/image_util.h"
 #import "ios/chrome/browser/shared/ui/util/rtl_geometry.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/tabs/ui_bundled/tab_strip_constants.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_strip/ui/swift_constants_for_objective_c.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_strip/ui/tab_strip_features_utils.h"
@@ -224,6 +225,11 @@ UIImage* DefaultFavicon() {
     [self updateGroupStroke];
 
     self.selected = NO;
+
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits = TraitCollectionSetForTraits(nil);
+      [self registerForTraitChanges:traits withAction:@selector(updateColors)];
+    }
   }
   return self;
 }
@@ -482,10 +488,15 @@ UIImage* DefaultFavicon() {
 
 #pragma mark - UITraitEnvironment
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
   [self updateColors];
 }
+#endif
 
 #pragma mark - UIAccessibility
 
