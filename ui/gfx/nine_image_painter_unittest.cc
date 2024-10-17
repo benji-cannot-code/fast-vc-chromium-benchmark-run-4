cfadd3502696efd7f11a2f58c129ba9b51994eca
Avi Drissman <avi@chromium.org>
2024-10-17 07:17:52 +0000

    Modernize codec usage in ui/
    Codec calls have new API that uses std::optional and spans. Switch usage
    to those new APIs, and lightly modernize the calling code.
    
    Bug: 370696612
    Change-Id: I19c5b8662bfc5c842e5d1bb8a92fbdca0a910da0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936919
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369828}

--- ui/gfx/nine_image_painter_unittest.cc
+++ ui/gfx/nine_image_painter_unittest.cc
@@ -21,9 +21,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 static std::string GetPNGDataUrl(const SkBitmap& bitmap) {
-  std::vector<unsigned char> png_data;
-  gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, false, &png_data);
-  return base::StrCat({"data:image/png;base64,", base::Base64Encode(png_data)});
+  std::optional<std::vector<uint8_t>> png_data =
+      gfx::PNGCodec::EncodeBGRASkBitmap(bitmap,
+                                        /*discard_transparency=*/false);
+  return base::StrCat(
+      {"data:image/png;base64,",
+       base::Base64Encode(png_data.value_or(std::vector<uint8_t>()))});
 }
 
 void ExpectRedWithGreenRect(const SkBitmap& bitmap,
