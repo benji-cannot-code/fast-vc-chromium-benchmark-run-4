4158a6d5ba4324eaf3212efd7dec2454021d5ae1
Kenichi Ishibashi <bashi@chromium.org>
2024-10-17 03:28:32 +0000

    HttpStreamPool: Add stream count consistency check
    This CL adds stream counts consistency check for debugging. When the
    "enable_consistency_check" feature param is enabled for the
    HappyEyeballsV3 feature, we periodically run a task that checks
    stream count consistency among the pool and groups. The task logs
    results to NetLog and/or VLOG.
    
    Bug: 346835898
    Change-Id: Ic68ce24c2d53f9a47d1ab29f21d725ec59522ca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933451
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1369780}

--- net/http/http_stream_pool.h
+++ net/http/http_stream_pool.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/flat_set.h"
 #include "base/containers/unique_ptr_adapters.h"
 #include "base/memory/raw_ptr.h"
+#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "base/values.h"
 #include "net/base/completion_once_callback.h"
@@ -94,6 +95,10 @@ class NET_EXPORT_PRIVATE HttpStreamPool
       "max_stream_per_pool";
   static constexpr std::string_view kMaxStreamSocketsPerGroupParamName =
       "max_stream_per_group";
+
+  // FeatureParam name for enabling consistency checks.
+  static constexpr std::string_view kEnableConsistencyCheckParamName =
+      "enable_consistency_check";
 
   // The time to wait between connection attempts.
   static constexpr base::TimeDelta kConnectionAttemptDelay =
@@ -274,6 +279,13 @@ class NET_EXPORT_PRIVATE HttpStreamPool
 
   void OnPooledStreamRequestComplete(PooledStreamRequestHelper* helper);
 
+  // Periodically checks the total active/idle/handed-out streams are consistent
+  // with per-group streams. Only used when the kEnableConsistencyCheckParamName
+  // FeatureParam is enabled.
+  // TODO(crbug.com/346835898): Remove this when we stabilize the
+  // implementation.
+  void CheckConsistency();
+
   const raw_ptr<HttpNetworkSession> http_network_session_;
 
   // Set to true when this is in the process of being destructed. When true,
@@ -283,6 +295,8 @@ class NET_EXPORT_PRIVATE HttpStreamPool
   const StreamAttemptParams stream_attempt_params_;
 
   const bool cleanup_on_ip_address_change_;
+
+  const NetLogWithSource net_log_;
 
   size_t max_stream_sockets_per_pool_;
   size_t max_stream_sockets_per_group_;
@@ -306,6 +320,8 @@ class NET_EXPORT_PRIVATE HttpStreamPool
       job_controllers_;
 
   std::unique_ptr<TestDelegate> delegate_for_testing_;
+
+  base::WeakPtrFactory<HttpStreamPool> weak_ptr_factory_{this};
 };
 
 }  // namespace net
