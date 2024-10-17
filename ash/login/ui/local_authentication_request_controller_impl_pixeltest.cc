b8a17acdfc0cc2fc6b4ba9427c70a80c9a393eca
Istvan Nagy <iscsi@google.com>
2024-10-17 21:01:08 +0000

    Pixeltest only valid with the old local authentication dialog
    The new dialog/views has a separate component based pixeltests.
    
    Bug: b:370867185
    Change-Id: I56218140c564b172d59bbd9c08c109fa4055a238
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939035
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370244}

--- ash/login/ui/local_authentication_request_controller_impl_pixeltest.cc
+++ ash/login/ui/local_authentication_request_controller_impl_pixeltest.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ash/login/ui/local_authentication_request_controller_impl.h"
-
 #include <memory>
 #include <string>
 #include <utility>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
+#include "ash/login/ui/local_authentication_request_controller_impl.h"
 #include "ash/login/ui/local_authentication_request_view.h"
 #include "ash/login/ui/local_authentication_request_widget.h"
 #include "ash/login/ui/login_button.h"
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/test/pixel/ash_pixel_differ.h"
 #include "base/functional/bind.h"
 #include "base/memory/raw_ptr.h"
+#include "base/test/scoped_feature_list.h"
 #include "chromeos/ash/components/cryptohome/system_salt_getter.h"
 #include "chromeos/ash/components/dbus/cryptohome/account_identifier_operators.h"
 #include "chromeos/ash/components/dbus/cryptohome/key.pb.h"
@@ -56,7 +57,10 @@ class LocalAuthenticationRequestControllerImplPixelTest : public AshTestBase {
       const LocalAuthenticationRequestControllerImplPixelTest&) = delete;
 
  protected:
-  LocalAuthenticationRequestControllerImplPixelTest() = default;
+  LocalAuthenticationRequestControllerImplPixelTest() {
+    scoped_features_.InitAndDisableFeature(
+        features::kLocalAuthenticationWithPin);
+  }
   ~LocalAuthenticationRequestControllerImplPixelTest() override = default;
 
   std::optional<pixel_test::InitParams> CreatePixelTestInitParams()
@@ -188,6 +192,8 @@ class LocalAuthenticationRequestControllerImplPixelTest : public AshTestBase {
     PressAndReleaseKey(ui::KeyboardCode::VKEY_RETURN);
     base::RunLoop().RunUntilIdle();
   }
+
+  base::test::ScopedFeatureList scoped_features_;
 
   // Number of times the view was dismissed with close button.
   int close_action_ = 0;
