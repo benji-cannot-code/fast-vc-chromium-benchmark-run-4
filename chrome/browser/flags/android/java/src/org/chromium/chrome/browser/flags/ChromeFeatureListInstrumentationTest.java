cc4304f52605ac943b837b4cf576921d1c559d1c
Henrique Nakashima <hnakashima@chromium.org>
2024-10-17 20:48:05 +0000

    [Android] Remove EXPERIMENTS_FOR_AGSA and AllCachedFieldTrialParams
    Bug: 40281605
    Change-Id: I9a63013ec2477a7b7873f4f3ebf5e13beeea93b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933546
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370235}

--- chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureListInstrumentationTest.java
+++ chrome/browser/flags/android/java/src/org/chromium/chrome/browser/flags/ChromeFeatureListInstrumentationTest.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.flags;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -26,7 +25,6 @@ import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /** Tests the behavior of {@link ChromeFeatureList} in instrumentation tests. */
@@ -70,22 +68,5 @@ public class ChromeFeatureListInstrumentationTest {
         assertFalse(ChromeFeatureList.isEnabled(ChromeFeatureList.TEST_DEFAULT_ENABLED));
 
         FeatureList.setTestFeatures(null);
-    }
-
-    @Test
-    @MediumTest
-    @EnableFeatures({ChromeFeatureList.EXPERIMENTS_FOR_AGSA + "<Trial"})
-    @CommandLineFlags.Add({
-        "force-fieldtrials=Trial/Group",
-        "force-fieldtrial-params=Trial.Group:101/x/y/z"
-    })
-    public void testGetFieldTrialParamsForFeature() {
-        Map<String, String> features =
-                ChromeFeatureList.getFieldTrialParamsForFeature(
-                        ChromeFeatureList.EXPERIMENTS_FOR_AGSA);
-        Map<String, String> expectedFeatures = new HashMap<String, String>();
-        expectedFeatures.put("101", "x");
-        expectedFeatures.put("y", "z");
-        assertEquals(expectedFeatures, features);
     }
 }
--- components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFieldTrialParameterUnitTest.java
+++ components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFieldTrialParameterUnitTest.java
@@ -15,14 +15,12 @@ import org.mockito.MockitoAnnotations;
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureList.TestValues;
 import org.chromium.base.FeatureMap;
-import org.chromium.base.cached_flags.CachedFlagsSharedPreferences;
 import org.chromium.base.cached_flags.ValuesOverridden;
 import org.chromium.base.cached_flags.ValuesReturned;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.BaseFlagTestRule;
 
 import java.util.List;
-import java.util.Map;
 
 /** Unit Tests for {@link CachedFieldTrialParameter} and its subclasses. */
 @RunWith(BaseRobolectricTestRunner.class)
@@ -40,7 +38,6 @@ public class CachedFieldTrialParameterUnitTest {
     private static final boolean BOOLEAN_PARAM_NATIVE = true;
     private static final String BOOLEAN_PARAM_NATIVE_STRING = "true";
     private static final boolean BOOLEAN_PARAM_TEST_OVERRIDE = true;
-    private static final String BOOLEAN_PARAM_TEST_OVERRIDE_STRING = "true";
     private static final String BOOLEAN_PARAM_BAD = "false";
 
     private static final String INT_PARAM_NAME = "ParamInt";
@@ -48,7 +45,6 @@ public class CachedFieldTrialParameterUnitTest {
     private static final int INT_PARAM_NATIVE = 2;
     private static final String INT_PARAM_NATIVE_STRING = "2";
     private static final int INT_PARAM_TEST_OVERRIDE = 3;
-    private static final String INT_PARAM_TEST_OVERRIDE_STRING = "3";
     private static final String INT_PARAM_BAD = "9";
 
     private static final String DOUBLE_PARAM_NAME = "ParamDouble";
@@ -56,7 +52,6 @@ public class CachedFieldTrialParameterUnitTest {
     private static final double DOUBLE_PARAM_NATIVE = 2.0;
     private static final String DOUBLE_PARAM_NATIVE_STRING = "2.0";
     private static final double DOUBLE_PARAM_TEST_OVERRIDE = 3.0;
-    private static final String DOUBLE_PARAM_TEST_OVERRIDE_STRING = "3.0";
     private static final String DOUBLE_PARAM_BAD = "9.0";
 
     // Different in that the native value will be "", which makes the default be cached.
@@ -84,20 +79,8 @@ public class CachedFieldTrialParameterUnitTest {
             new StringCachedFieldTrialParameter(
                     FEATURE_MAP, FEATURE_A, STRING2_PARAM_NAME, STRING2_PARAM_DEFAULT);
 
-    private static final String FEATURE_B = "FeatureB";
-
-    private static final Map<String, String> ALL_PARAM_TEST_OVERRIDE =
-            Map.of(
-                    STRING_PARAM_NAME, STRING_PARAM_TEST_OVERRIDE,
-                    BOOLEAN_PARAM_NAME, BOOLEAN_PARAM_TEST_OVERRIDE_STRING,
-                    INT_PARAM_NAME, INT_PARAM_TEST_OVERRIDE_STRING,
-                    DOUBLE_PARAM_NAME, DOUBLE_PARAM_TEST_OVERRIDE_STRING);
-
-    private static final AllCachedFieldTrialParameters ALL_PARAM =
-            new AllCachedFieldTrialParameters(FEATURE_MAP, FEATURE_B);
-
     private static final List<CachedFieldTrialParameter<?>> PARAMS_TO_CACHE =
-            List.of(STRING_PARAM, BOOLEAN_PARAM, INT_PARAM, DOUBLE_PARAM, STRING2_PARAM, ALL_PARAM);
+            List.of(STRING_PARAM, BOOLEAN_PARAM, INT_PARAM, DOUBLE_PARAM, STRING2_PARAM);
 
     @Before
     public void setUp() {
@@ -112,13 +95,6 @@ public class CachedFieldTrialParameterUnitTest {
         testValues.addFieldTrialParamOverride(
                 FEATURE_A, DOUBLE_PARAM_NAME, DOUBLE_PARAM_NATIVE_STRING);
         testValues.addFieldTrialParamOverride(FEATURE_A, STRING2_PARAM_NAME, STRING2_PARAM_NATIVE);
-
-        testValues.addFieldTrialParamOverride(FEATURE_B, STRING_PARAM_NAME, STRING_PARAM_NATIVE);
-        testValues.addFieldTrialParamOverride(
-                FEATURE_B, BOOLEAN_PARAM_NAME, BOOLEAN_PARAM_NATIVE_STRING);
-        testValues.addFieldTrialParamOverride(FEATURE_B, INT_PARAM_NAME, INT_PARAM_NATIVE_STRING);
-        testValues.addFieldTrialParamOverride(
-                FEATURE_B, DOUBLE_PARAM_NAME, DOUBLE_PARAM_NATIVE_STRING);
 
         FeatureList.setTestValues(testValues);
     }
@@ -161,11 +137,6 @@ public class CachedFieldTrialParameterUnitTest {
         testValues.addFieldTrialParamOverride(FEATURE_A, DOUBLE_PARAM_NAME, DOUBLE_PARAM_BAD);
         testValues.addFieldTrialParamOverride(FEATURE_A, STRING2_PARAM_NAME, STRING2_PARAM_BAD);
 
-        testValues.addFieldTrialParamOverride(FEATURE_B, STRING_PARAM_NAME, STRING_PARAM_BAD);
-        testValues.addFieldTrialParamOverride(FEATURE_B, BOOLEAN_PARAM_NAME, BOOLEAN_PARAM_BAD);
-        testValues.addFieldTrialParamOverride(FEATURE_B, INT_PARAM_NAME, INT_PARAM_BAD);
-        testValues.addFieldTrialParamOverride(FEATURE_B, DOUBLE_PARAM_NAME, DOUBLE_PARAM_BAD);
-
         FeatureList.setTestValues(testValues);
 
         // In the second run, should get cached values and not the new ones since
@@ -180,10 +151,6 @@ public class CachedFieldTrialParameterUnitTest {
         INT_PARAM.setForTesting(INT_PARAM_TEST_OVERRIDE);
         DOUBLE_PARAM.setForTesting(DOUBLE_PARAM_TEST_OVERRIDE);
         STRING2_PARAM.setForTesting(STRING2_PARAM_TEST_OVERRIDE);
-        String preferenceKey =
-                CachedFlagsSharedPreferences.generateParamSharedPreferenceKey(FEATURE_B, "");
-        String overrideValue = CachedFlagsSharedPreferences.encodeParams(ALL_PARAM_TEST_OVERRIDE);
-        ValuesOverridden.setOverrideForTesting(preferenceKey, overrideValue);
 
         // Should not take priority over the overrides
         CachedFlagUtils.cacheFieldTrialParameters(PARAMS_TO_CACHE);
@@ -193,11 +160,6 @@ public class CachedFieldTrialParameterUnitTest {
         assertEquals(INT_PARAM_TEST_OVERRIDE, INT_PARAM.getValue());
         assertEquals(DOUBLE_PARAM_TEST_OVERRIDE, DOUBLE_PARAM.getValue(), 1e-6f);
         assertEquals(STRING2_PARAM_TEST_OVERRIDE, STRING2_PARAM.getValue());
-        Map<String, String> featureBParams = ALL_PARAM.getParams();
-        assertEquals(STRING_PARAM_TEST_OVERRIDE, featureBParams.get(STRING_PARAM_NAME));
-        assertEquals(BOOLEAN_PARAM_TEST_OVERRIDE_STRING, featureBParams.get(BOOLEAN_PARAM_NAME));
-        assertEquals(INT_PARAM_TEST_OVERRIDE_STRING, featureBParams.get(INT_PARAM_NAME));
-        assertEquals(DOUBLE_PARAM_TEST_OVERRIDE_STRING, featureBParams.get(DOUBLE_PARAM_NAME));
     }
 
     private void assertValuesAreDefault() {
@@ -206,9 +168,6 @@ public class CachedFieldTrialParameterUnitTest {
         assertEquals(INT_PARAM_DEFAULT, INT_PARAM.getValue());
         assertEquals(DOUBLE_PARAM_DEFAULT, DOUBLE_PARAM.getValue(), 1e-6f);
         assertEquals(STRING2_PARAM_DEFAULT, STRING2_PARAM.getValue());
-
-        Map<String, String> featureBParams = ALL_PARAM.getParams();
-        assertEquals(0, featureBParams.size());
     }
 
     private void assertValuesAreFromNative() {
@@ -217,12 +176,5 @@ public class CachedFieldTrialParameterUnitTest {
         assertEquals(INT_PARAM_NATIVE, INT_PARAM.getValue());
         assertEquals(DOUBLE_PARAM_NATIVE, DOUBLE_PARAM.getValue(), 1e-6f);
         assertEquals(STRING2_PARAM_DEFAULT, STRING2_PARAM.getValue()); // Special case
-
-        Map<String, String> featureBParams = ALL_PARAM.getParams();
-        assertEquals(4, featureBParams.size());
-        assertEquals(STRING_PARAM_NATIVE, featureBParams.get(STRING_PARAM_NAME));
-        assertEquals(BOOLEAN_PARAM_NATIVE_STRING, featureBParams.get(BOOLEAN_PARAM_NAME));
-        assertEquals(INT_PARAM_NATIVE_STRING, featureBParams.get(INT_PARAM_NAME));
-        assertEquals(DOUBLE_PARAM_NATIVE_STRING, featureBParams.get(DOUBLE_PARAM_NAME));
     }
 }
