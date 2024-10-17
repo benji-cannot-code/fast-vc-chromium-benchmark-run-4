3860f2e2170a105942aee4dbb41e3b167ffdaeee
Brandon Jones <bajones@chromium.org>
2024-10-17 23:18:55 +0000

    WebGPU support for ARCore WebXR
    Enables WebGPU sessions with the ARCore backend as long as both the
    WebXRIncubations and WebXRLayers features are enabled and SharedBuffers
    will be used. WebXRIncubations and SharedBuffers have a flag in
    about:flags, but WebXRLayers doesn't, so it's not very trivial to turn
    the feature on just yet. I'll try to address that in a follow-up CL.
    
    Bug: 359418629
    Change-Id: I1f8b26b8ceca5219c264ea078c1c292f5c63e90e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939519
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370315}

--- device/vr/android/arcore/arcore_device.cc
+++ device/vr/android/arcore/arcore_device.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/android/compositor_delegate_provider.h"
 #include "device/vr/android/mailbox_to_surface_bridge.h"
 #include "device/vr/android/xr_java_coordinator.h"
+#include "device/vr/public/cpp/features.h"
 #include "device/vr/public/cpp/xr_frame_sink_client.h"
 #include "ui/android/window_android.h"
 #include "ui/display/display.h"
@@ -84,6 +85,13 @@ ArCoreDevice::ArCoreDevice(
   // Only support camera access if the device supports shared buffers.
   if (base::AndroidHardwareBufferCompat::IsSupportAvailable())
     device_features.emplace_back(mojom::XRSessionFeature::CAMERA_ACCESS);
+
+  // Only support WebGPU sessions if the appropriate feature flag is enabled
+  // and shared buffers will be used.
+  if (base::FeatureList::IsEnabled(features::kWebXrIncubations) &&
+      ArImageTransport::UseSharedBuffer()) {
+    device_features.emplace_back(mojom::XRSessionFeature::WEBGPU);
+  }
 
   SetSupportedFeatures(device_features);
 }
