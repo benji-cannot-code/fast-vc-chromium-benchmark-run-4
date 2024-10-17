030e6894c6df20cf2068979eacdb1ad38b0f9a74
Gang Wu <gangwu@chromium.org>
2024-10-17 18:34:00 +0000

    [Anchor] Show toolbar location in the main setting's page
    screenshot:
    https://screenshot.googleplex.com/8ZgchdYoEDetmi4
    
    Change-Id: I6c0cda8a26b1b876e59844d847f0b07818d92b3a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937666
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370158}

--- chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
+++ chrome/android/java/src/org/chromium/chrome/browser/settings/MainSettings.java
@@ -368,6 +368,7 @@ public class MainSettings extends ChromeBaseSettingsFragment
         updateSearchEnginePreference();
         updateAutofillPreferences();
         updatePlusAddressesPreference();
+        updateAddressBarPreference();
 
         boolean isTabGroupSyncAutoOpenConfigurable =
                 TabGroupSyncFeatures.isTabGroupSyncEnabled(getProfile())
@@ -379,12 +380,6 @@ public class MainSettings extends ChromeBaseSettingsFragment
             addPreferenceIfAbsent(PREF_TABS);
         } else {
             removePreferenceIfPresent(PREF_TABS);
-        }
-
-        if (ToolbarPositionController.isToolbarPositionCustomizationEnabled(getContext(), false)) {
-            addPreferenceIfAbsent(PREF_ADDRESS_BAR);
-        } else {
-            removePreferenceIfPresent(PREF_ADDRESS_BAR);
         }
 
         Preference homepagePref = addPreferenceIfAbsent(PREF_HOMEPAGE);
@@ -595,6 +590,15 @@ public class MainSettings extends ChromeBaseSettingsFragment
                     });
         } else {
             removePreferenceIfPresent(PREF_PLUS_ADDRESSES);
+        }
+    }
+
+    private void updateAddressBarPreference() {
+        if (ToolbarPositionController.isToolbarPositionCustomizationEnabled(getContext(), false)) {
+            Preference addressBarPreference = addPreferenceIfAbsent(PREF_ADDRESS_BAR);
+            addressBarPreference.setSummary(ToolbarPositionController.getToolbarPositionResId());
+        } else {
+            removePreferenceIfPresent(PREF_ADDRESS_BAR);
         }
     }
 
