4ab464e5abe7a7d8d2d6b8fe7a9eb1a5d36bd069
Federica Germinario <fedegermi@google.com>
2024-10-17 15:17:01 +0000

    [ios] Final cleanup of SigninBrowserStateInfoUpdater code
    Fixed: 361040177
    Change-Id: Id66ba0a0ab2334f09a59daef260579600371569f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938099
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370023}

--- ios/chrome/browser/signin/model/signin_profile_info_updater.h
+++ ios/chrome/browser/signin/model/signin_profile_info_updater.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This class listens to various signin events and updates the signin-related
 // fields of ProfileAttributesStorageIOS.
-// TODO(crbug.com/361040177): Rename this class to SigninProfileInfoUpdater.
 class SigninProfileInfoUpdater : public KeyedService,
                                  public SigninErrorController::Observer,
                                  public signin::IdentityManager::Observer {
