1156de9b38117be413bc8e7eb530499dba60cfe4
Federica Germinario <fedegermi@google.com>
2024-10-17 09:56:38 +0000

    [ios] Remove GetForBrowserState from ChromeAccountManagerService
    Remove legacy GetForBrowserState.
    
    Bug: 359812085
    Change-Id: Iee665e90e8a4216abfabf5c8e8ece854ce95b078
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937072
    Commit-Queue: David Roger <droger@chromium.org>
    Reviewed-by: David Roger <droger@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1369891}

--- ios/chrome/app/profile/first_run_profile_agent.mm
+++ ios/chrome/app/profile/first_run_profile_agent.mm
@@ -121,7 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // TODO(crbug.com/343699504): Remove pre-fetching capabilities once these are
   // loaded in iSL.
   RunSystemCapabilitiesPrefetch(
-      ChromeAccountManagerServiceFactory::GetForBrowserState(profile)
+      ChromeAccountManagerServiceFactory::GetForProfile(profile)
           ->GetAllIdentities());
 
   FirstRunScreenProvider* provider =
--- ios/chrome/browser/signin/model/chrome_account_manager_service_factory.mm
+++ ios/chrome/browser/signin/model/chrome_account_manager_service_factory.mm
@@ -16,12 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service.h"
 
 // static
-ChromeAccountManagerService*
-ChromeAccountManagerServiceFactory::GetForBrowserState(ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
 ChromeAccountManagerService* ChromeAccountManagerServiceFactory::GetForProfile(
     ProfileIOS* profile) {
   return static_cast<ChromeAccountManagerService*>(
