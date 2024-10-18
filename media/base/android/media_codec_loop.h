2189f066feffb238c1f9b54f456eb35bed65a7a5
Dale Curtis <dalecurtis@chromium.org>
2024-10-18 23:36:13 +0000

    Spanify input buffer processing on MediaCodecBridge.
    There are some crashes when filling input buffer, spanify to increase
    confidence in our code.
    
    Bug: 373986013
    Change-Id: Ib2d3b5e53a475ceb3d8bdfdd48a557e18e5015f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943700
    Auto-Submit: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370892}

--- media/base/android/media_codec_loop.h
+++ media/base/android/media_codec_loop.h
@@ -115,8 +115,7 @@ class MEDIA_EXPORT MediaCodecLoop {
     InputData(const InputData&);
     ~InputData();
 
-    raw_ptr<const uint8_t> memory = nullptr;
-    size_t length = 0;
+    base::span<const uint8_t> memory;
 
     std::string key_id;
     std::string iv;
