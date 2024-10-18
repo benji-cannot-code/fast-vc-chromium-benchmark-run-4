a1c3282bd76be037546f45defb532f96b6b9f7bc
Adem Derinel <derinel@google.com>
2024-10-18 13:40:46 +0000

    Remove mIsHybridRequest from Fido2CredentialRequest
    This is no longer needed as the hybrid flow is now handled by Play Services.
    
    Change-Id: I14421f3e5ac6c5e90336a1d672bbaba40a680373
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938179
    Commit-Queue: Adem Derinel <derinel@google.com>
    Reviewed-by: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370546}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2CredentialRequest.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2CredentialRequest.java
@@ -90,10 +90,6 @@ public class Fido2CredentialRequest
     private boolean mAppIdExtensionUsed;
     private boolean mEchoCredProps;
     private WebauthnBrowserBridge mBrowserBridge;
-    // mIsHybridRequest is true if this request comes from a hybrid (i.e. cross-device) flow rather
-    // than a WebContents. Handling the hybrid protocol can be delegated to Chrome (by Play
-    // Services).
-    private boolean mIsHybridRequest;
     // Values set when errors occur, for metrics recording.
     private @GetAssertionOutcome int mGetAssertionErrorOutcome = GetAssertionOutcome.OTHER_FAILURE;
     private @MakeCredentialOutcome int mMakeCredentialErrorOutcome =
@@ -173,16 +169,10 @@ public class Fido2CredentialRequest
 
     private Barrier.Mode getBarrierMode() {
         @CredManSupport int support = CredManSupportProvider.getCredManSupport();
-        if (support != CredManSupport.DISABLED && mIsHybridRequest) {
-            return Barrier.Mode.ONLY_CRED_MAN;
-        }
         switch (support) {
             case CredManSupport.DISABLED:
                 return Barrier.Mode.ONLY_FIDO_2_API;
             case CredManSupport.IF_REQUIRED:
-                if (mIsHybridRequest) {
-                    return Barrier.Mode.ONLY_CRED_MAN;
-                }
                 return Barrier.Mode.ONLY_FIDO_2_API;
             case CredManSupport.FULL_UNLESS_INAPPLICABLE:
                 return Barrier.Mode.ONLY_CRED_MAN;
@@ -778,10 +768,6 @@ public class Fido2CredentialRequest
             }
         }
         callback.onResponse(matchingCredentialIds);
-    }
-
-    public void setIsHybridRequest(boolean isHybridRequest) {
-        mIsHybridRequest = isHybridRequest;
     }
 
     public void overrideBrowserBridgeForTesting(WebauthnBrowserBridge bridge) {
