ee7796868ca407936fb3a893ad3ef037c5ae1ed7
David Baron <dbaron@chromium.org>
2024-10-18 17:17:42 +0000

    Shrink the arrays in ScriptRunIterator.
    We only need the arrays to be as big as the largest possible number of
    script extensions from the Unicode database as returned by ICU.
    
    Keeping the arrays smaller means we can spend less time initializing
    them when we expand them for ICU to fill in, and it may also mean that
    objects get placed closer together in memory.
    
    Change-Id: Ifb1e58a7e1dca417bd77e9a912642f0df573f35f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941596
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370682}

--- third_party/blink/renderer/platform/fonts/script_run_iterator.h
+++ third_party/blink/renderer/platform/fonts/script_run_iterator.h
@@ -34,7 +34,8 @@ class PLATFORM_EXPORT ScriptRunIterator {
 
   bool Consume(unsigned* limit, UScriptCode*);
 
-  static constexpr int kMaxScriptCount = 32;
+  static constexpr int kMaxUnicodeScriptExtensions = 21;
+  static constexpr int kMaxScriptCount = kMaxUnicodeScriptExtensions + 1;
   using UScriptCodeList = Vector<UScriptCode, kMaxScriptCount>;
 
  private:
@@ -98,6 +99,8 @@ class PLATFORM_EXPORT ScriptData {
     kBracketTypeCount
   };
 
+  static constexpr int kMaxUnicodeScriptExtensions =
+      ScriptRunIterator::kMaxUnicodeScriptExtensions;
   static constexpr int kMaxScriptCount = ScriptRunIterator::kMaxScriptCount;
   using UScriptCodeList = ScriptRunIterator::UScriptCodeList;
 
