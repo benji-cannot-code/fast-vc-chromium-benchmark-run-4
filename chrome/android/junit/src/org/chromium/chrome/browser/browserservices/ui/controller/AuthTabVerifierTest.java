c9c21b028ebe10c39060895f77ed6d4928edceed
Sinan Sahin <sinansahin@google.com>
2024-10-18 17:03:28 +0000

    [AuthView] Use AndroidX constants
    This CL replaces the constants used for the AuthTab implementation with
    the ones defined in the AndroidX APIs.
    
    Bug: 358167556
    Change-Id: I0dc41b6e7b11c1e9ef2f443f6895072ceda01662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939808
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370670}

--- chrome/android/junit/src/org/chromium/chrome/browser/browserservices/ui/controller/AuthTabVerifierTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/browserservices/ui/controller/AuthTabVerifierTest.java
@@ -15,11 +15,11 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import static org.chromium.chrome.browser.browserservices.ui.controller.AuthTabVerifier.RESULT_VERIFICATION_FAILED;
-import static org.chromium.chrome.browser.browserservices.ui.controller.AuthTabVerifier.RESULT_VERIFICATION_TIMED_OUT;
 import static org.chromium.chrome.browser.browserservices.ui.controller.AuthTabVerifier.VERIFICATION_TIMEOUT_MS;
 
 import android.app.Activity;
+
+import androidx.browser.auth.AuthTabIntent;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -149,7 +149,7 @@ public class AuthTabVerifierTest {
         simulateVerificationResultFromNetwork(url, false);
 
         mDelegate.returnAsActivityResult(new GURL(url));
-        verify(mActivity).setResult(eq(RESULT_VERIFICATION_FAILED), any());
+        verify(mActivity).setResult(eq(AuthTabIntent.RESULT_VERIFICATION_FAILED), any());
         verify(mActivity).finish();
         histograms.assertExpected();
     }
@@ -210,7 +210,7 @@ public class AuthTabVerifierTest {
         // Simulate timeout.
         mDelayedTask.run();
 
-        verify(mActivity).setResult(eq(RESULT_VERIFICATION_TIMED_OUT), any());
+        verify(mActivity).setResult(eq(AuthTabIntent.RESULT_VERIFICATION_TIMED_OUT), any());
         verify(mActivity).finish();
         histograms.assertExpected();
     }
