8ada10c29b7a1addb81df5af3bd28cb9ed2fe136
Arthur Milchior <arthurmilchior@chromium.org>
2024-10-18 23:19:05 +0000

    [iOS][Trusted Vault]Rename KeysFetchedCallback
    For the sake of the consistency with crrev.com/c/5939072.
    
    This is part of the chain:
    + crrev.com/c/5943126
    * crrev.com/i/7757070
    * crrev.com/c/5943103
    
    Bug: 355042392
    Change-Id: If36182f222b7d28bd4ae5f909f676396dace68f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943126
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Noémie St-Onge <noemies@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370884}

--- ios/chrome/test/providers/signin/fake_trusted_vault_client_backend.h
+++ ios/chrome/test/providers/signin/fake_trusted_vault_client_backend.h
@@ -21,7 +21,7 @@ class FakeTrustedVaultClientBackend final : public TrustedVaultClientBackend {
       VerifierCallback verifier) final;
   void FetchKeys(id<SystemIdentity> identity,
                  trusted_vault::SecurityDomainId security_domain_id,
-                 KeyFetchedCallback completion) final;
+                 KeysFetchedCallback completion) final;
   void MarkLocalKeysAsStale(id<SystemIdentity> identity,
                             trusted_vault::SecurityDomainId security_domain_id,
                             base::OnceClosure completion) final;
--- ios/chrome/test/providers/signin/fake_trusted_vault_client_backend.mm
+++ ios/chrome/test/providers/signin/fake_trusted_vault_client_backend.mm
@@ -69,7 +69,7 @@ void FakeTrustedVaultClientBackend::
 void FakeTrustedVaultClientBackend::FetchKeys(
     id<SystemIdentity> identity,
     trusted_vault::SecurityDomainId security_domain_id,
-    KeyFetchedCallback completion) {
+    KeysFetchedCallback completion) {
   // Do nothing.
 }
 
