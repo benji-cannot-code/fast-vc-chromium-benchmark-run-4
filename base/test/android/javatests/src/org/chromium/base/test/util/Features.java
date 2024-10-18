e83753eceff2b749a35667e1044cd9215e89abd7
Henrique Nakashima <hnakashima@chromium.org>
2024-10-18 18:57:55 +0000

    [Android] Move CachedParam test overrides from ValuesOverridden
    Also, delete ValuesOverridden. All test overrides are now in
    FeatureList.
    
    Cq-Include-Trybots: luci.chrome.try:android-internal-rel,android-internal-dbg
    Bug: 40281605
    Change-Id: I1f9f47ecf84b69e48df848df7588b5251667501a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933172
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370754}

--- base/test/android/javatests/src/org/chromium/base/test/util/Features.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/Features.java
@@ -11,7 +11,6 @@ import org.chromium.base.CommandLine;
 import org.chromium.base.FeatureList;
 import org.chromium.base.FeatureParam;
 import org.chromium.base.Flag;
-import org.chromium.base.cached_flags.ValuesOverridden;
 import org.chromium.base.cached_flags.ValuesReturned;
 
 import java.lang.annotation.Retention;
@@ -53,7 +52,6 @@ public class Features {
     static void resetCachedFlags() {
         // TODO(agrieve): Allow cached flags & field trials to be set in @BeforeClass.
         ValuesReturned.clearForTesting();
-        ValuesOverridden.removeOverrides();
         Flag.resetAllInMemoryCachedValuesForTesting();
         FeatureParam.resetAllInMemoryCachedValuesForTesting();
     }
--- components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFeatureFlagsSafeModeUnitTest.java
+++ components/cached_flags/android/java/src/org/chromium/components/cached_flags/CachedFeatureFlagsSafeModeUnitTest.java
@@ -24,7 +24,6 @@ import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.FeatureMap;
-import org.chromium.base.cached_flags.ValuesOverridden;
 import org.chromium.base.cached_flags.ValuesReturned;
 import org.chromium.base.task.test.PausedExecutorTestRule;
 import org.chromium.base.test.BaseRobolectricTestRunner;
@@ -732,7 +731,6 @@ public class CachedFeatureFlagsSafeModeUnitTest {
 
     private static void clearMemory() {
         ValuesReturned.clearForTesting();
-        ValuesOverridden.removeOverrides();
         CachedFlagsSafeMode.getInstance().clearMemoryForTesting();
     }
 }
