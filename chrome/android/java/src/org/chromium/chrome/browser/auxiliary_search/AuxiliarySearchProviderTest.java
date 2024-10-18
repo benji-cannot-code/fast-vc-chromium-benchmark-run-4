942a71adb7c266f994176a5d3fc453482aa96c64
Xi Han <hanxi@google.com>
2024-10-18 17:12:59 +0000

    [AppIntegration] Schedule AuxiliarySearchBackgroundTask.
    In this CL, when Chrome is onPause() and the initial 5 favicons are
    donated to auxiliary search, the remaining tabs' metadata is saved to
    a file, and create an AuxiliarySearchBackgroundTask immediately. The
    AuxiliarySearchBackgroundTask will be scheduled by JobScheduler and
    fetch the remaining favicons and donate.
    
    Bug: 370478696
    Change-Id: Ie8acdccc98c9d98c40839bde19d0fdc4aa915ae3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940497
    Commit-Queue: Xi Han <hanxi@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370677}

--- chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProviderTest.java
+++ chrome/android/java/src/org/chromium/chrome/browser/auxiliary_search/AuxiliarySearchProviderTest.java
@@ -53,6 +53,9 @@ import org.chromium.chrome.browser.ui.favicon.FaviconHelper;
 import org.chromium.chrome.browser.ui.favicon.FaviconHelper.FaviconImageCallback;
 import org.chromium.chrome.browser.ui.favicon.FaviconHelperJni;
 import org.chromium.chrome.test.util.browser.tabmodel.MockTabModel;
+import org.chromium.components.background_task_scheduler.BackgroundTaskScheduler;
+import org.chromium.components.background_task_scheduler.BackgroundTaskSchedulerFactory;
+import org.chromium.components.background_task_scheduler.TaskInfo;
 import org.chromium.url.GURL;
 
 import java.util.ArrayList;
@@ -83,6 +86,7 @@ public class AuxiliarySearchProviderTest {
     private @Mock FaviconHelper mFaviconHelper;
     private @Mock Context mContext;
     private @Mock Resources mResources;
+    private @Mock BackgroundTaskScheduler mBackgroundTaskScheduler;
 
     private AuxiliarySearchProvider mAuxiliarySearchProvider;
     private MockTabModel mMockNormalTabModel;
@@ -99,6 +103,8 @@ public class AuxiliarySearchProviderTest {
                 new AuxiliarySearchProvider(mContext, mProfile, mTabModelSelector);
         mMockNormalTabModel = new MockTabModel(mProfile, null);
         doReturn(mMockNormalTabModel).when(mTabModelSelector).getModel(false);
+
+        BackgroundTaskSchedulerFactory.setSchedulerForTesting(mBackgroundTaskScheduler);
     }
 
     private Tab createTab(int index, long timestamp) {
@@ -315,9 +321,12 @@ public class AuxiliarySearchProviderTest {
     @SmallTest
     @EnableFeatures(ChromeFeatureList.ANDROID_APP_INTEGRATION_WITH_FAVICON)
     public void testOnNonSensitiveTabsAvailable() {
-        int maxFaviconNumber = 5;
+        int maxFaviconNumber = 10;
         AuxiliarySearchProvider.MAX_FAVICON_NUMBER.setForTesting(maxFaviconNumber);
         assertEquals(maxFaviconNumber, AuxiliarySearchProvider.MAX_FAVICON_NUMBER.getValue());
+
+        mAuxiliarySearchProvider =
+                new AuxiliarySearchProvider(mContext, mProfile, mTabModelSelector);
 
         ArrayList<Tab> tabList = new ArrayList<>();
         int count = 100;
@@ -333,7 +342,7 @@ public class AuxiliarySearchProviderTest {
         }
 
         mAuxiliarySearchProvider.onNonSensitiveTabsAvailable(
-                mFaviconHelper, mCallback, mFaviconImageFetchedCallback, tabList);
+                mFaviconHelper, mCallback, mFaviconImageFetchedCallback, tabList, now);
 
         // Verifies that the tabs are sorted with timestamp descending, i.e., the most recent tab as
         // as the first.
@@ -348,5 +357,23 @@ public class AuxiliarySearchProviderTest {
                         any(GURL.class),
                         anyInt(),
                         any(FaviconImageCallback.class));
+    }
+
+    @Test
+    @SmallTest
+    @EnableFeatures(ChromeFeatureList.ANDROID_APP_INTEGRATION_WITH_FAVICON)
+    public void testScheduleBackgroundTask() {
+        long expectedWindowStartTimeMs = 10;
+        long expectedStartTimeMs = 1000;
+
+        TaskInfo taskInfo =
+                mAuxiliarySearchProvider.scheduleBackgroundTask(
+                        expectedWindowStartTimeMs, expectedStartTimeMs);
+        var bundle = taskInfo.getExtras();
+        assertEquals(
+                expectedStartTimeMs, bundle.getLong(AuxiliarySearchProvider.TASK_CREATED_TIME));
+        assertFalse(taskInfo.isUserInitiated());
+        assertTrue(taskInfo.shouldUpdateCurrent());
+        assertTrue(taskInfo.isPersisted());
     }
 }
