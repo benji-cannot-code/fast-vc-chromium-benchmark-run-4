4b2d77a70474edff07590050351b42475d89c651
Aliona DANGLA <alionadangla@google.com>
2024-10-18 17:16:32 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=shaktisahu@chromium.org
    
    Bug: 359821785
    Change-Id: Ib60629925a0fb1e68809f6121bcf968a3e1c99bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943122
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370680}

--- ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h
+++ ios/chrome/browser/segmentation_platform/model/segmentation_platform_service_factory.h
@@ -7,8 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define IOS_CHROME_BROWSER_SEGMENTATION_PLATFORM_MODEL_SEGMENTATION_PLATFORM_SERVICE_FACTORY_H_
 
 #import "base/no_destructor.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 #import "ios/chrome/browser/shared/model/profile/profile_keyed_service_factory_ios.h"
+
+class ProfileIOS;
 
 namespace segmentation_platform {
 
