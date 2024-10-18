308c047cc57a3f45a1fd39109f68027da3c79758
“Prasanaa” <prasanaa@google.com>
2024-10-18 13:59:11 +0000

    [iOS]: Fix bug when the search field in uneditable after closing history
    After closing the Search History screen, the search field become
    uneditable.
    This CL sets the TabGridMode to kNormal and save the text in the
    UISearchBar just before entering the Search History screen;
    and sets the TabGridMode to kSearch and restores the text back once the
    Search History screen is closed.
    
    Fixed: 368260425
    Change-Id: I816a6dbba84837c2c5f827c11cb7a868da04fedf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938088
    Auto-Submit: Prasanaa Vingadassamy <prasanaa@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Prasanaa Vingadassamy <prasanaa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370552}

--- ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_top_toolbar.h
+++ ios/chrome/browser/ui/tab_switcher/tab_grid/toolbars/tab_grid_top_toolbar.h
@@ -84,7 +84,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)respondBeforeResponder:(UIResponder*)nextResponder;
 // Relinquishs the searchBar status as first responder.
 - (void)unfocusSearchBar;
-
+// Sets the text of the UISearchBar.
+- (void)setSearchBarText:(NSString*)text;
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_TAB_SWITCHER_TAB_GRID_TOOLBARS_TAB_GRID_TOP_TOOLBAR_H_
