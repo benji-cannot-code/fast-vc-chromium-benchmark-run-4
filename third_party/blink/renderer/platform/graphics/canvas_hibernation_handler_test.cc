f1068eb11139d158d6463f30cd56fd8a608e6bce
Colin Blundell <blundell@chromium.org>
2024-10-18 11:39:54 +0000

    [Blink] Eliminate hibernation handler test's dep on Canvas2DLayerBridge
    This dep is now unused.
    
    Bug: 40280152
    Change-Id: Ice9503ccddb6df631d9cc99e42f082f0ebf65f11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940564
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370494}

--- third_party/blink/renderer/platform/graphics/canvas_hibernation_handler_test.cc
+++ third_party/blink/renderer/platform/graphics/canvas_hibernation_handler_test.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/test/test_context_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
-#include "third_party/blink/renderer/platform/graphics/canvas_2d_layer_bridge.h"
 #include "third_party/blink/renderer/platform/graphics/static_bitmap_image.h"
 #include "third_party/blink/renderer/platform/graphics/test/fake_canvas_resource_host.h"
 #include "third_party/blink/renderer/platform/graphics/test/gpu_memory_buffer_test_platform.h"
@@ -46,17 +45,6 @@ class CanvasHibernationHandlerTest
                                               {});
         break;
     }
-  }
-
-  std::unique_ptr<Canvas2DLayerBridge> MakeBridge(const gfx::Size& size,
-                                                  RasterModeHint raster_mode,
-                                                  OpacityMode opacity_mode) {
-    host_ = std::make_unique<FakeCanvasResourceHost>(size);
-    host_->SetPreferred2DRasterMode(raster_mode);
-    host_->SetOpacityMode(opacity_mode);
-    std::unique_ptr<Canvas2DLayerBridge> bridge =
-        std::make_unique<Canvas2DLayerBridge>(host_.get());
-    return bridge;
   }
 
   void SetUp() override {
@@ -358,13 +346,12 @@ TEST_P(CanvasHibernationHandlerTest, ForegroundFlipForAfterEncoding) {
   EXPECT_EQ(1u, TestSingleThreadTaskRunner::RunAll(task_runner->immediate()));
   // But the encoded version is dropped (epoch mismatch).
   EXPECT_FALSE(handler.is_encoded());
-  // Yet we are hibernating (since the bridge is in background).
+  // Yet we are hibernating (since the page is in the background).
   EXPECT_TRUE(handler.IsHibernating());
 
   EXPECT_EQ(1u, TestSingleThreadTaskRunner::RunAll(task_runner->delayed()));
   EXPECT_EQ(2u, TestSingleThreadTaskRunner::RunAll(task_runner->immediate()));
   EXPECT_TRUE(handler.is_encoded());
-  // Yet we are hibernating (since the bridge is in background).
   EXPECT_TRUE(handler.IsHibernating());
 }
 
@@ -396,7 +383,7 @@ TEST_P(CanvasHibernationHandlerTest, ForegroundFlipForBeforeEncoding) {
 
   // But the encoded version is dropped (epoch mismatch).
   EXPECT_FALSE(handler.is_encoded());
-  // Yet we are hibernating (since the bridge is in background).
+  // Yet we are hibernating (since the page is in the background).
   EXPECT_TRUE(handler.IsHibernating());
 }
 
