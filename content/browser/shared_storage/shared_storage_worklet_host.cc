c01fc16bc5379ae81aa168ee629bcf7f73987305
Yao Xiao <yaoxia@chromium.org>
2024-10-18 21:07:25 +0000

    [shared storage] Add result status and timing UMAs for interestGroups()
    Bug: 367992703
    Change-Id: I208b403b2ac2536aef05827f1f7ea1078e1fa540
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941655
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Commit-Queue: Yao Xiao <yaoxia@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370826}

--- content/browser/shared_storage/shared_storage_worklet_host.cc
+++ content/browser/shared_storage/shared_storage_worklet_host.cc
@@ -1154,7 +1154,7 @@ void SharedStorageWorkletHost::GetInterestGroups(
       /*devtools_auction_id=*/{},
       /*owner=*/shared_storage_origin_,
       base::BindOnce(
-          [](GetInterestGroupsCallback callback,
+          [](base::ElapsedTimer timer, GetInterestGroupsCallback callback,
              scoped_refptr<StorageInterestGroups> groups) {
             std::vector<blink::mojom::StorageInterestGroupPtr> mojom_groups;
 
@@ -1171,11 +1171,15 @@ void SharedStorageWorkletHost::GetInterestGroups(
               mojom_groups.push_back(std::move(mojom_group));
             }
 
+            base::UmaHistogramTimes(
+                "Storage.SharedStorage.InterestGroups.InBrowserRetrievalTime",
+                timer.Elapsed());
+
             std::move(callback).Run(
                 blink::mojom::GetInterestGroupsResult::NewGroups(
                     std::move(mojom_groups)));
           },
-          std::move(callback)));
+          base::ElapsedTimer(), std::move(callback)));
 }
 
 void SharedStorageWorkletHost::DidAddMessageToConsole(
