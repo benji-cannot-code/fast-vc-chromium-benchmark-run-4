85f4198a687e4eeb4d24fcf604445076b2863b0f
Steven Holte <holte@google.com>
2024-10-18 07:55:01 +0000

    Use AnyWrapProto in chrome/browser/v8_compile_hints.
    For clarity and consistency.
    
    This CL was uploaded by git cl split.
    
    Bug: 373711168
    Change-Id: I44ba66de64c6be8b6c29f127050f106e54ba68a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939286
    Auto-Submit: Steven Holte <holte@chromium.org>
    Reviewed-by: Marja Hölttä <marja@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370448}

--- chrome/browser/v8_compile_hints/v8_compile_hints_tab_helper_unittest.cc
+++ chrome/browser/v8_compile_hints/v8_compile_hints_tab_helper_unittest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "components/optimization_guide/core/optimization_guide_decision.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
+#include "components/optimization_guide/core/optimization_guide_proto_util.h"
 #include "content/public/test/navigation_simulator.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/public/common/features.h"
@@ -98,10 +99,8 @@ optimization_guide::OptimizationMetadata CreateMetadata(
   model.set_sample_count(1000);
   model.set_clear_zeros(clear_zeros);
   model.set_clear_ones(clear_ones);
-  optimization_guide::proto::Any any;
-  any.set_type_url(model.GetTypeName());
-  model.SerializeToString(any.mutable_value());
-  optimization_metadata.set_any_metadata(any);
+  optimization_metadata.set_any_metadata(
+      optimization_guide::AnyWrapProto(model));
   return optimization_metadata;
 }
 
