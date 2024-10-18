413dc3198ae51abbca40e48d6697c875a4aa64a4
François Doray <fdoray@chromium.org>
2024-10-18 19:05:02 +0000

    [freezing] Add "frozen with growing memory" discard reason.
    A frozen page cannot execute tasks and therefore typically doesn't
    grow memory usage. However, there exist cases for which its memory
    usage can grow, for example if another page or worker sends messages
    to it. In that case, we'll discard the frozen page with growing
    memory (ignoring all discard opt-outs, because the tab is already
    frozen and thus not providing useful functionality, and user state
    loss is considered better than unbounded memory usage growth). This
    intervention will be coupled with developer messaging to discourage
    sending a lot of messages to a frozen tab.
    
    This intervention is implemented in 2 CLs:
    
    1. (this CL) Add a new discard reason: FROZEN_WITH_GROWING_MEMORY.
    2. (crrev.com/c/5926052) Discard a page with
       FROZEN_WITH_GROWING_MEMORY when the private memory footprint of
       a tab grows above a threshold.
    
    Bug: 325954772
    Change-Id: I59307b771f535a7d9beb494374c90d59e0a62885
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5927211
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Francois Pierre Doray <fdoray@chromium.org>
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370761}

--- chrome/browser/resources/discards/discards_tab.ts
+++ chrome/browser/resources/discards/discards_tab.ts
@@ -199,6 +199,8 @@ class DiscardsTabElement extends DiscardsTabElementBase {
         return 'proactive';
       case LifecycleUnitDiscardReason.SUGGESTED:
         return 'suggested';
+      case LifecycleUnitDiscardReason.FROZEN_WITH_GROWING_MEMORY:
+        return 'frozen with growing memory';
     }
   }
 
