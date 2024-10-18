f028670cd96f0a742163063b997e28cf4c54fa60
Xinan Lin <linxinan@chromium.org>
2024-10-18 09:34:07 +0000

    Add comment to the logs consumed by IP protection e2e tests
    Also revert "IPATP::GetProxyConfig" back to "IPATP::GetProxyList".
    
    BUG=374105177
    TEST=tryjob
    
    Change-Id: I5abd0a5c62061b79c7f8bbb3a0aba6b7486ee553
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938125
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Xinan Lin <linxinan@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370469}

--- components/component_updater/installer_policies/masked_domain_list_component_installer_policy.cc
+++ components/component_updater/installer_policies/masked_domain_list_component_installer_policy.cc
@@ -117,6 +117,8 @@ void MaskedDomainListComponentInstallerPolicy::ComponentReady(
     return;
   }
 
+  // Log is consumed by E2E tests. Please CC potassium-engprod@google.com if you
+  // have to change this log.
   VLOG(1) << "Masked Domain List Component ready, version "
           << version.GetString() << " in " << install_dir.value();
 
