5d2f83c9e338c6784b477014812a8cf0361dbfe4
Aliona DANGLA <alionadangla@google.com>
2024-10-18 18:37:05 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=christianxu@chromium.org
    
    Bug: 359821785
    Change-Id: Ie0f789a7c2dc494d2ee2934705ae45b9296b105b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940568
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Christian Xu <christianxu@chromium.org>
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370741}

--- ios/chrome/browser/lens_overlay/coordinator/lens_omnibox_client.h
+++ ios/chrome/browser/lens_overlay/coordinator/lens_omnibox_client.h
@@ -13,10 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/omnibox/browser/autocomplete_match.h"
 #import "components/omnibox/browser/omnibox_client.h"
 #import "ios/chrome/browser/autocomplete/model/autocomplete_scheme_classifier_impl.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 
 @protocol LensOmniboxClientDelegate;
 @protocol LensWebProvider;
+class ProfileIOS;
+
 namespace feature_engagement {
 class Tracker;
 }
