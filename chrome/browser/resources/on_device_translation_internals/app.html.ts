09388f6cf1cd867c9952cb81c77a323ac1c98415
Tsuyoshi Horo <horo@chromium.org>
2024-10-18 01:31:19 +0000

    Use WebComponents in chrome://on-device-translation-internals
    This CL is a follow up CL of https://crrev.com/c/5919126.
    As recommended at https://crrev.com/c/5919126/comment/f4f694c2_6a18623c/
    this CL changes the chrome://on-device-translation-internals Web UI to
    use WebComponents.
    
    This CL also adds tests for the page.
    
    Bug: 372777020
    Change-Id: I1f0b5fed42a655a83d12c9360a9a1681a170726e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932624
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370371}

---
+++ chrome/browser/resources/on_device_translation_internals/app.html.ts
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {OnDeviceTranslationInternalsAppElement} from './app.js';
+
+export function getHtml(this: OnDeviceTranslationInternalsAppElement) {
+  return html`
+<h1>On Device Translation Internals</h1>
+<h2>Language Packages</h2>
+<table class="package-table">
+${this.languagePackStatus_.map((item, index) => html`
+  <tr class="package-tr">
+    <th class="package-name">${item.name}</th>
+    <td class="package-status">${this.getStatusString_(item.status)}</td>
+    <td>
+      <button @click="${this.onButtonClick_}"
+        data-index="${index}">${this.getButtonString_(item.status)}</button>
+    </td>
+  </tr>`)}
+</table>
+`;
+}
