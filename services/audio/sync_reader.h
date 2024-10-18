bcc5c156eef6fab8d764850f49bf7ddf0cf42ec1
Olga Sharonova <olka@chromium.org>
2024-10-18 16:22:32 +0000

    Remove kChromeWideEchoCancellationDynamicMixingTimeout
    Bug: 374115884
    Change-Id: Iabe8257e050d1896ebe2f09e0799fa9677a940d2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940779
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Fredrik Hernqvist <fhernqvist@google.com>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370641}

--- services/audio/sync_reader.h
+++ services/audio/sync_reader.h
@@ -59,7 +59,6 @@ class SyncReader : public OutputController::SyncReader {
 
   void set_max_wait_timeout_for_test(base::TimeDelta time) {
     maximum_wait_time_ = time;
-    maximum_wait_time_for_mixing_ = time;
   }
 
   // OutputController::SyncReader implementation.
@@ -72,7 +71,7 @@ class SyncReader : public OutputController::SyncReader {
  private:
   // Blocks until data is ready for reading or a timeout expires.  Returns false
   // if an error or timeout occurs.
-  bool WaitUntilDataIsReady(bool is_mixing);
+  bool WaitUntilDataIsReady();
 
   const base::RepeatingCallback<void(const std::string&)> log_callback_;
 
@@ -103,7 +102,6 @@ class SyncReader : public OutputController::SyncReader {
   // The maximum amount of time to wait for data from the renderer.  Calculated
   // from the parameters given at construction.
   base::TimeDelta maximum_wait_time_;
-  base::TimeDelta maximum_wait_time_for_mixing_;
 
   // The index of the audio buffer we're expecting to be sent from the renderer;
   // used to block with timeout for audio data.
