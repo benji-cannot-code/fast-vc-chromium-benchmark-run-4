42c5685d1c7125fcae17e80c3337b35b764599ac
Maria Kazinova <kazinova@google.com>
2024-10-18 15:47:07 +0000

    Add a feature and a flag for new clientside form classifications.
    Added a feature and a flag for experimentation.
    
    Bug: 371933424
    Change-Id: Ia48f1b0cd06b582b5208cbf33f3efe19168aaaa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938881
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370622}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1680,6 +1680,9 @@ extern const char kPageVisibilityPageContentAnnotationsDescription[];
 extern const char kParallelDownloadingName[];
 extern const char kParallelDownloadingDescription[];
 
+extern const char kPasswordFormClientsideClassifierName[];
+extern const char kPasswordFormClientsideClassifierDescription[];
+
 extern const char kPasswordFormGroupedAffiliationsName[];
 extern const char kPasswordFormGroupedAffiliationsDescription[];
 
