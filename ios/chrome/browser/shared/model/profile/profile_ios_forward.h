f924e36d7ca43f656d58f06942283ab9f60448ba
Aliona DANGLA <alionadangla@google.com>
2024-10-18 15:19:43 +0000

    [iOS] Remove ChromeBrowserState typedef
    Bug: 359821785
    Change-Id: I8948bcbfd51be74ca1d884ec82619a0ea86eae6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942360
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370602}

--- ios/chrome/browser/shared/model/profile/profile_ios_forward.h
+++ ios/chrome/browser/shared/model/profile/profile_ios_forward.h
@@ -13,6 +13,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // normal forward-declaration of `ProfileIOS` once the migration is complete.
 
 class ProfileIOS;
-using ChromeBrowserState = ProfileIOS;
 
 #endif  // IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_PROFILE_IOS_FORWARD_H_
