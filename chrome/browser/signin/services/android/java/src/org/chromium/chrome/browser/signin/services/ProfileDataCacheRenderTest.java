4da70d084e957e6989ea19446ad563c42dea73a3
Boris Sazonov <bsazonov@chromium.org>
2024-10-18 16:12:53 +0000

    [Signin] Don't use AccountInfo constructor directly in tests
    Replaces explicit usages of AccountInfo constructors in sign-in tests
    with either AccountInfo.Builder or TestAccounts.
    
    Bug: 348373729
    Change-Id: If10bec4aa25df9b6da94aec052ff83d55977f9e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937923
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370632}

--- chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheRenderTest.java
+++ chrome/browser/signin/services/android/java/src/org/chromium/chrome/browser/signin/services/ProfileDataCacheRenderTest.java
@@ -43,7 +43,6 @@ import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.test.ChromeJUnit4RunnerDelegate;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.browser.signin.AccountManagerTestRule;
-import org.chromium.components.signin.base.AccountCapabilities;
 import org.chromium.components.signin.base.AccountInfo;
 import org.chromium.components.signin.identitymanager.AccountInfoServiceProvider;
 import org.chromium.components.signin.identitymanager.IdentityManager;
@@ -54,7 +53,6 @@ import org.chromium.ui.widget.ChromeImageView;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 /** Tests for {@link ProfileDataCache} image scaling. */
@@ -182,14 +180,10 @@ public class ProfileDataCacheRenderTest extends BlankUiTestActivityTestCase {
                 () -> {
                     mIdentityManager.onExtendedAccountInfoUpdated(TestAccounts.ACCOUNT1);
                     final AccountInfo emptyAccountInfo =
-                            new AccountInfo(
-                                    TestAccounts.ACCOUNT1.getId(),
-                                    TestAccounts.ACCOUNT1.getEmail(),
-                                    TestAccounts.ACCOUNT1.getGaiaId(),
-                                    null,
-                                    null,
-                                    null,
-                                    new AccountCapabilities(new HashMap<>()));
+                            new AccountInfo.Builder(
+                                            TestAccounts.ACCOUNT1.getEmail(),
+                                            TestAccounts.ACCOUNT1.getGaiaId())
+                                    .build();
                     mIdentityManager.onExtendedAccountInfoUpdated(emptyAccountInfo);
                     checkImageIsScaled(TestAccounts.ACCOUNT1.getEmail());
                 });
