17a42cca19784b7cbc06f7098c53420eba52da13
Aliona DANGLA <alionadangla@google.com>
2024-10-18 15:59:26 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=sebsg@chromium.org
    
    Bug: 359821785
    Change-Id: Idfb964431abf7ab0eeb31091022716b392a32a88
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942722
    Reviewed-by: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Commit-Queue: Sebastien Seguin-Gagnon <sebsg@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370626}

--- ios/chrome/browser/autofill/model/autofill_tab_helper.h
+++ ios/chrome/browser/autofill/model/autofill_tab_helper.h
@@ -10,13 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/raw_ptr.h"
 #import "components/autofill/ios/form_util/child_frame_registrar.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
 #import "ios/web/public/web_state_observer.h"
 #import "ios/web/public/web_state_user_data.h"
 
 @class AutofillAgent;
 @protocol AutofillCommands;
 @protocol FormSuggestionProvider;
+class ProfileIOS;
 @class UIViewController;
 
 namespace autofill {
