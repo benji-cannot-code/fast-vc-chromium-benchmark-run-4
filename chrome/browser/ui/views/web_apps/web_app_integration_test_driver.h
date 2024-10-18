19682aa42a3d1bc0686635302a54af3c67490b3d
Daniel Murphy <dmurph@chromium.org>
2024-10-18 03:02:15 +0000

    [PWA] Fix the tab helper to not consider a 'suggested app'.
    The WebAppTabHelper (and other places) have been 'seeing' suggested
    apps that aren't installed, where they likely should not. This was
    likely a problem in a recent metrics CL. This solves the root cause
    by making the `FindAppWithUrlInScope` call to no longer use the
    SUGGESTED_FROM_ANOTHER_DEVICE install state.
    
    This fixes some tests that were OK to change, and one place where the
    code actually did want to see if a SUGGESTED_FROM_ANOTHER_DEVICE app
    was installed.
    
    Change-Id: I53e1226afaef3145864973fb4c5df46495a76984
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939304
    Auto-Submit: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370389}

--- chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
+++ chrome/browser/ui/views/web_apps/web_app_integration_test_driver.h
@@ -404,6 +404,7 @@ class WebAppIntegrationTestDriver : WebAppInstallManagerObserver {
   void CheckHasSubApp(Site parent_app, Site sub_app);
   void CheckNoSubApps(Site parent_app);
   void CheckAppLoadedInTab(Site site);
+  void CheckSiteLoadedInTab(Site site);
 
  protected:
   // WebAppInstallManagerObserver:
