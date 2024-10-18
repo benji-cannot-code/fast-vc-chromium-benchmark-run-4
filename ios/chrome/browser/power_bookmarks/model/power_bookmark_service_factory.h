46f0cc6ad2a744b328d370192165830498e60815
Aliona DANGLA <alionadangla@google.com>
2024-10-18 19:44:17 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=mdjones@chromium.org
    
    Bug: 359821785
    Change-Id: I10f1285a88cc08f36499796f73b02e538c83ee2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942505
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370783}

--- ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h
+++ ios/chrome/browser/power_bookmarks/model/power_bookmark_service_factory.h
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "base/memory/singleton.h"
 #import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
+
+class ProfileIOS;
 
 namespace power_bookmarks {
 class PowerBookmarkService;
