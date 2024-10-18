548bc7004a6bd4fac77c856774196505f31ad698
Sinan Sahin <sinansahin@google.com>
2024-10-18 23:54:32 +0000

    [AuthView] Flag guard HTTPS redirects
    This CL also makes the DAL verification timeout field trial
    configurable.
    
    Bug: 358167556
    Change-Id: Id1ce45fcd3fe7d84a7d4dd3072b8d814df750b5e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943566
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Reviewed-by: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370898}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -206,6 +206,7 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kCastDeviceFilter,
     &kCCTAuthTab,
     &kCCTAuthTabDisableAllExternalIntents,
+    &kCCTAuthTabEnableHttpsRedirects,
     &kCCTBeforeUnload,
     &kCCTClientDataHeader,
     &kCCTExtendTrustedCdnPublisher,
@@ -548,6 +549,9 @@ BASE_FEATURE(kCCTAuthTab, "CCTAuthTab", base::FEATURE_DISABLED_BY_DEFAULT);
 BASE_FEATURE(kCCTAuthTabDisableAllExternalIntents,
              "CCTAuthTabDisableAllExternalIntents",
              base::FEATURE_DISABLED_BY_DEFAULT);
+BASE_FEATURE(kCCTAuthTabEnableHttpsRedirects,
+             "CCTAuthTabEnableHttpsRedirects",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kCCTBeforeUnload,
              "CCTBeforeUnload",
