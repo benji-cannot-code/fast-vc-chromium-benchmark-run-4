7bad29ac4aa40e6f223948ac75a9ea750d7c798a
Shakti Sahu <shaktisahu@chromium.org>
2024-10-18 19:51:13 +0000

    [TG Sync] Extend flag expiration and remove TabGroupSyncForceOff
    This CL
    1. Extends all the tab group sync android related flags to M140
    since they are still under experiment.
    2. Removes a remnant feature and corresponding flag
    TabGroupSyncForceOff which isn't used anywhere any more.
    
    Bug: 373568802
    Change-Id: I93dbc459486209b374633d5b823db163c7f895bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937366
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Auto-Submit: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370789}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -11021,10 +11021,6 @@ const FeatureEntry kFeatureEntries[] = {
      flag_descriptions::kTabGroupSyncDisableNetworkLayerName,
      flag_descriptions::kTabGroupSyncDisableNetworkLayerDescription, kOsAndroid,
      FEATURE_VALUE_TYPE(tab_groups::kTabGroupSyncDisableNetworkLayer)},
-
-    {"tab-group-sync-force-off", flag_descriptions::kTabGroupSyncForceOffName,
-     flag_descriptions::kTabGroupSyncForceOffDescription, kOsAndroid,
-     FEATURE_VALUE_TYPE(tab_groups::kTabGroupSyncForceOff)},
 #endif  // BUILDFLAG(IS_ANDROID)
 
     {"autofill-shared-storage-server-card-data",
--- components/saved_tab_groups/public/features.h
+++ components/saved_tab_groups/public/features.h
@@ -16,8 +16,6 @@ BASE_DECLARE_FEATURE(kTabGroupPaneAndroid);
 
 BASE_DECLARE_FEATURE(kTabGroupSyncDisableNetworkLayer);
 
-BASE_DECLARE_FEATURE(kTabGroupSyncForceOff);
-
 BASE_DECLARE_FEATURE(kTabGroupsSaveV2);
 
 BASE_DECLARE_FEATURE(kTabGroupsSaveUIUpdate);
