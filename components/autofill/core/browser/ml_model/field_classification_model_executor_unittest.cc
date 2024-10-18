6eb99c80ece2407c9c71ade887e2ac7df262034a
Maria Kazinova <kazinova@google.com>
2024-10-18 14:00:34 +0000

    [Autofill] Rename Autofill Model Encoder and Executor classes.
    As the code is now going to be shared between Autofill and Password
    Manager, I'm refactoring and renaming some common classes (as per
    https://crrev.com/c/chromium/src/+/5939096/comment/6131ee4b_124d6bf6)
    to make it less confusing.
    
    In a follow up CLs I'll rename the model handler class and
    refactor the Encoder and Executor classes to not necessarily depend
    on kAutofillModelPredictions feature being enabled.
    
    Bug: 371933424
    Change-Id: Ib8346ba7d30b5bed307129a3e2909dc033104cbc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941620
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1370554}

--- components/autofill/core/browser/ml_model/field_classification_model_executor_unittest.cc
+++ components/autofill/core/browser/ml_model/field_classification_model_executor_unittest.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/autofill/core/browser/ml_model/autofill_model_executor.h"
+#include "components/autofill/core/browser/ml_model/field_classification_model_executor.h"
 
 #include "base/base_paths.h"
 #include "base/path_service.h"
@@ -22,15 +22,15 @@ namespace autofill {
 
 namespace {
 
-using ModelExecutor =
-    optimization_guide::ModelExecutor<AutofillModelEncoder::ModelOutput,
-                                      const AutofillModelEncoder::ModelInput&>;
-using TokenId = AutofillModelEncoder::TokenId;
+using ModelExecutor = optimization_guide::ModelExecutor<
+    FieldClassificationModelEncoder::ModelOutput,
+    const FieldClassificationModelEncoder::ModelInput&>;
+using TokenId = FieldClassificationModelEncoder::TokenId;
 
-class AutofillModelExecutorTest : public testing::Test {
+class FieldClassificationModelExecutorTest : public testing::Test {
  public:
-  AutofillModelExecutorTest() = default;
-  ~AutofillModelExecutorTest() override = default;
+  FieldClassificationModelExecutorTest() = default;
+  ~FieldClassificationModelExecutorTest() override = default;
 
   void SetUp() override {
     base::FilePath source_root_dir;
@@ -43,7 +43,7 @@ class AutofillModelExecutorTest : public testing::Test {
                            .AppendASCII("autofill_model-fold-one.tflite");
     execution_task_runner_ = base::ThreadPool::CreateSequencedTaskRunner(
         {base::MayBlock(), base::TaskPriority::BEST_EFFORT});
-    model_executor_ = std::make_unique<AutofillModelExecutor>();
+    model_executor_ = std::make_unique<FieldClassificationModelExecutor>();
     model_executor_->InitializeAndMoveToExecutionThread(
         /*model_inference_timeout=*/std::nullopt,
         optimization_guide::proto::
@@ -62,10 +62,10 @@ class AutofillModelExecutorTest : public testing::Test {
   scoped_refptr<base::SequencedTaskRunner> execution_task_runner_;
   base::test::ScopedFeatureList features_{features::kAutofillModelPredictions};
   base::FilePath model_file_path_;
-  std::unique_ptr<AutofillModelExecutor> model_executor_;
+  std::unique_ptr<FieldClassificationModelExecutor> model_executor_;
 };
 
-TEST_F(AutofillModelExecutorTest, ExecuteModel) {
+TEST_F(FieldClassificationModelExecutorTest, ExecuteModel) {
   // Update model file.
   execution_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&ModelExecutor::UpdateModelFile,
@@ -75,7 +75,7 @@ TEST_F(AutofillModelExecutorTest, ExecuteModel) {
   // Execute model on a dummy "form" consisting of two fields. Since the
   // executor works in terms of tokenized fields, this is represented as a
   // two arrays of tokens. The TokenIds are completely arbitrary.
-  AutofillModelEncoder::ModelInput input = {
+  FieldClassificationModelEncoder::ModelInput input = {
       {
           TokenId(14),  // CLS
           TokenId(1), TokenId(2), TokenId(3), TokenId(4),
@@ -95,7 +95,7 @@ TEST_F(AutofillModelExecutorTest, ExecuteModel) {
           TokenId(6),  // Attrib. 3
       }};
   base::test::TestFuture<
-      const std::optional<AutofillModelEncoder::ModelOutput>&>
+      const std::optional<FieldClassificationModelEncoder::ModelOutput>&>
       predictions;
   execution_task_runner_->PostTask(
       FROM_HERE, base::BindOnce(&ModelExecutor::SendForExecution,
