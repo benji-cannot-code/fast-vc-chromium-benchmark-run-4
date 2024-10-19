edaacf145082ccb054522d1143ef3de8a7bae2e8
Mitsuru Oshima <oshima@chromium.org>
2024-10-19 00:42:13 +0000

    Do not create InstallLimiter for non user / guest profile
    According to the original bug, this was added to improve the post
    login performance. Signin profile has no post-login, and Guest profile
    will not download extensions.
    
    Bug: 41488885, 374351946
    Change-Id: I0d72767823fdeb8ef15409394d7b77124f2a335d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5931148
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1370916}

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -1480,7 +1480,11 @@ void ExtensionService::OnAllExternalProvidersReady() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
 #if BUILDFLAG(IS_CHROMEOS)
-  InstallLimiter::Get(profile_)->OnAllExternalProvidersReady();
+  auto* install_limiter = InstallLimiter::Get(profile_);
+  if (install_limiter) {
+    install_limiter->OnAllExternalProvidersReady();
+  }
+
 #endif  // BUILDFLAG(IS_CHROMEOS)
 
   // Install any pending extensions.
@@ -2098,7 +2102,12 @@ bool ExtensionService::OnExternalExtensionFileFound(
                      ? GetPolicyVerifierFormat()
                      : GetExternalVerifierFormat());
 #if BUILDFLAG(IS_CHROMEOS)
-  InstallLimiter::Get(profile_)->Add(installer, file_info);
+  auto* install_limiter = InstallLimiter::Get(profile_);
+  if (install_limiter) {
+    install_limiter->Add(installer, file_info);
+  } else {
+    installer->InstallCrxFile(file_info);
+  }
 #else
   installer->InstallCrxFile(file_info);
 #endif
