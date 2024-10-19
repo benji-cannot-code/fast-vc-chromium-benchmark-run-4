75004e43ebc4be6251bf3a7c3f23037017317911
Sam Chen <chensa@google.com>
2024-10-19 19:07:35 +0000

    Create feature flag for CrosSafetyService
    BUG=b:357479669
    TEST='AboutFlags*' unittests
    
    Change-Id: Ide1cdaeb9e4a0f258eeac3d338f8f70eb7249fc4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5904736
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Sam Chen <chensa@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371029}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3587,6 +3587,9 @@ extern const char kCaptureModeGifRecordingDescription[];
 extern const char kCrosContentAdjustedRefreshRateName[];
 extern const char kCrosContentAdjustedRefreshRateDescription[];
 
+extern const char kCrosSafetyServiceName[];
+extern const char kCrosSafetyServiceDescription[];
+
 extern const char kCrosSoulName[];
 extern const char kCrosSoulDescription[];
 
