b9edc7e64a9bff6848656017150b49faa3b99c40
Theresa <twellington@chromium.org>
2024-10-19 03:05:51 +0000

    Cleanup android-google-sans-text flag
    Bug: 360956943
    Change-Id: I3903e7f67125dbab3dcef847b889e91c000736f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5944100
    Commit-Queue: Sinan Sahin <sinansahin@google.com>
    Auto-Submit: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1370947}

--- chrome/browser/flags/android/chrome_feature_list.cc
+++ chrome/browser/flags/android/chrome_feature_list.cc
@@ -178,7 +178,6 @@ const base::Feature* const kFeaturesExposedToJava[] = {
     &kAndroidAppIntegrationWithFavicon,
     &kAndroidBottomToolbar,
     &kAndroidElegantTextHeight,
-    &kAndroidGoogleSansText,
     &kAndroidHubFloatingActionButton,
     &kAndroidHubSearch,
     &kAndroidHubV2,
@@ -457,10 +456,6 @@ BASE_FEATURE(kAndroidBottomToolbar,
 BASE_FEATURE(kAndroidElegantTextHeight,
              "AndroidElegantTextHeight",
              base::FEATURE_DISABLED_BY_DEFAULT);
-
-BASE_FEATURE(kAndroidGoogleSansText,
-             "AndroidGoogleSansText",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 BASE_FEATURE(kAndroidHubFloatingActionButton,
              "AndroidHubFloatingActionButton",
