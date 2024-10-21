755bf13172aca45d6c084d5767fbd87fc94251af
Christian Xu <christianxu@google.com>
2024-10-21 15:46:00 +0000

    [iOS][LO] Add FakeChromeLensOverlayResult
    Move FakeChromeLensOverlayResult out of the fake_chrome_lens_overlay.
    It will be used independently in a future CL.
    
    Bug: None
    Change-Id: I3b9f877306ad56fd8a8844e4ddb1bb090427b5a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5946363
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371412}

---
+++ ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay_result.mm
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay_result.h"
+
+#import "url/gurl.h"
+
+@implementation FakeChromeLensOverlayResult
+@end
