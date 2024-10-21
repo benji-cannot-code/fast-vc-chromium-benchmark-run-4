71daf825158bf7c6dbd3e3b717472fcf9b1f39a3
Noam Rosenthal <nrosenthal@chromium.org>
2024-10-21 08:20:30 +0000

    View transitions layered capture: support overflow
    - The overflow, contain and overflow-clip-margin properties
      are copied to the ::view-transition-group.
    - The scrollbars are not drawn when the original element is captured.
    
    Bug: 373759981
    Change-Id: I8528187936c7dbe6283cb040585646310687cf8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937825
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371239}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -91,6 +91,9 @@ CSSPropertyID kLayeredCaptureProperties[] = {
     CSSPropertyID::kOutline,
     CSSPropertyID::kBorderImage,
     CSSPropertyID::kPadding,
+    CSSPropertyID::kOverflow,
+    CSSPropertyID::kOverflowClipMargin,
+    CSSPropertyID::kContain,
 };
 
 CSSPropertyID kPropertiesToAnimate[] = {
