2c8053c0296f24a72417a1a0aca513f346cc6165
Andrii Natiahlyi <natiahlyi@google.com>
2024-10-21 14:22:03 +0000

    Add Autofill Prediction Improvements Help Center Url
    Fixed: 374254306, 374716843
    Change-Id: Ie7d4062c4e8d5c10954743ecfeebf66f67450107
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5947443
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Auto-Submit: Andrii Natiahlyi <natiahlyi@google.com>
    Commit-Queue: Andrii Natiahlyi <natiahlyi@google.com>
    Reviewed-by: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371360}

--- chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/settings_localized_strings_provider.cc
@@ -1308,6 +1308,8 @@ void AddAutofillStrings(content::WebUIDataSource* html_source,
                          autofill::payments::GetManageInstrumentsUrl().spec());
   html_source->AddString("addressesAndPaymentMethodsLearnMoreURL",
                          chrome::kAddressesAndPaymentMethodsLearnMoreURL);
+  html_source->AddString("autofillPredictionImprovementsLearnMoreURL",
+                         chrome::kAutofillPredictionImprovementsLearnMoreURL);
   html_source->AddString(
       "signedOutUserLabel",
       l10n_util::GetStringFUTF16(IDS_SETTINGS_SIGNED_OUT_USER_LABEL,
@@ -1407,8 +1409,7 @@ void AddAutofillStrings(content::WebUIDataSource* html_source,
       "autofillPredictionImprovementsToggleSubLabel",
       l10n_util::GetStringFUTF16(
           IDS_SETTINGS_AUTOFILL_PREDICTION_IMPROVEMENTS_TOGGLE_SUB_LABEL,
-          base::ASCIIToUTF16(
-              std::string(chrome::kAddressesAndPaymentMethodsLearnMoreURL))));
+          chrome::kAutofillPredictionImprovementsLearnMoreURL));
 }
 
 void AddSignOutDialogStrings(content::WebUIDataSource* html_source,
