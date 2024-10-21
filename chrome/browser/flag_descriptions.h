2a2cbbfc83ad410dc2718e2e7786a015c57661a9
Shu Yang <shuyng@google.com>
2024-10-21 18:21:53 +0000

    Clean up flag DefaultViewportIsDeviceWidth
    Bug: 325983964
    Change-Id: Ie31833b95a7be20a89ebe382c0fbd4504663bcf0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5945263
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Auto-Submit: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371528}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2565,9 +2565,6 @@ extern const char kContextualSearchSuppressShortViewDescription[];
 extern const char kConvertTrackpadEventsToMouseName[];
 extern const char kConvertTrackpadEventsToMouseDescription[];
 
-extern const char kDefaultViewportIsDeviceWidthName[];
-extern const char kDefaultViewportIsDeviceWidthDescription[];
-
 extern const char kDeprecatedExternalPickerFunctionName[];
 extern const char kDeprecatedExternalPickerFunctionDescription[];
 
