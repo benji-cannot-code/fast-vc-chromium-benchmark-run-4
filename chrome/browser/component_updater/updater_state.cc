8fa7584a99fcd89bcc0808f5500e0b13bf36324d
Jan Keitel <jkeitel@google.com>
2024-10-21 09:39:19 +0000

    [Gardening] Revert "updater: use the same ProgramFiles(x86) directory for both 32 and 64 bit"
    This reverts commit ba46ea576353ce1501a5dbbba60d5e4f38d1c74b.
    
    Note: PS2->PS3 contains additional changes that were necessary to
    resolve an unrelated merge conflict that came up in the mean time.
    
    Reason for revert: Consistent test failures on win11-arm64-rel, see
    https://ci.chromium.org/ui/p/chromium/builders/ci/win11-arm64-rel-tests/2761/overview for an example.
    
    Original change's description:
    > updater: use the same ProgramFiles(x86) directory for both 32 and 64 bit
    >
    > Low-Coverage-Reason: COVERAGE_UNDERREPORTED
    > Bug: 374217027
    > Change-Id: I816313c31fdc11c5215c1fae34f678f7652c4d1a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941730
    > Auto-Submit: S Ganesh <ganesh@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Commit-Queue: S Ganesh <ganesh@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1370733}
    
    Bug: 374217027, 374382498
    Change-Id: I1a4f4fe86d05cfbb8b5c90cc40dfd49a012d22fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942702
    Owners-Override: Tanmoy Mollik <triploblastic@google.com>
    Reviewed-by: Tanmoy Mollik <triploblastic@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371264}

--- chrome/browser/component_updater/updater_state.cc
+++ chrome/browser/component_updater/updater_state.cc
@@ -28,6 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/update_client/persisted_data.h"
 #include "components/update_client/update_client_errors.h"
 
+#if BUILDFLAG(IS_WIN)
+#include "chrome/updater/util/win_util.h"
+#endif
+
 namespace component_updater {
 namespace {
 
@@ -53,10 +57,16 @@ std::unique_ptr<UpdaterState::StateReader> UpdaterState::StateReader::Create(
           [is_machine]() -> std::unique_ptr<StateReader> {
         // Create a `StateReaderChromiumUpdater` instance only if a prefs.json
         // file for the updater can be found and parsed successfully.
+        const updater::UpdaterScope updater_scope =
+            is_machine ? updater::UpdaterScope::kSystem
+                       : updater::UpdaterScope::kUser;
         const std::optional<base::FilePath> global_prefs_dir =
-            updater::GetInstallDirectory(is_machine
-                                             ? updater::UpdaterScope::kSystem
-                                             : updater::UpdaterScope::kUser);
+#if BUILDFLAG(IS_WIN)
+            // Google Chrome ships with an x86 updater.
+            updater::GetInstallDirectoryX86(updater_scope);
+#else
+            updater::GetInstallDirectory(updater_scope);
+#endif  //  IS_WIN
         if (!global_prefs_dir)
           return nullptr;
         std::string contents;
