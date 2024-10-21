b62770242fa5bc9eb07ecd127bd25d60149b7ae4
Aliona DANGLA <alionadangla@google.com>
2024-10-21 08:52:34 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=samarchehade@google.com
    
    Bug: 359821785
    Change-Id: Ibecc270a86d7b995c3e3fe4305eb4dd92d0fdd68
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943063
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371246}

--- ios/chrome/browser/search_engine_choice/model/search_engine_choice_util.h
+++ ios/chrome/browser/search_engine_choice/model/search_engine_choice_util.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_SEARCH_ENGINE_CHOICE_MODEL_SEARCH_ENGINE_CHOICE_UTIL_H_
 #define IOS_CHROME_BROWSER_SEARCH_ENGINE_CHOICE_MODEL_SEARCH_ENGINE_CHOICE_UTIL_H_
 
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
+class ProfileIOS;
 
 // Utilities for the search engine choice screen.
 
