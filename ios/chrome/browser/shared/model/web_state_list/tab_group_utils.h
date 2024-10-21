9f319e456c1c92fa4701d0a7d62278a397f5a532
Aliona DANGLA <alionadangla@google.com>
2024-10-21 10:15:01 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=asamidoi@chromium.org
    
    Bug: 359821785
    Change-Id: I9661780d4636cce511b06a8acb60b378021b62f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938549
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Asami Doi <asamidoi@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371275}

--- ios/chrome/browser/shared/model/web_state_list/tab_group_utils.h
+++ ios/chrome/browser/shared/model/web_state_list/tab_group_utils.h
@@ -8,10 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <set>
 
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
-
 class Browser;
 class BrowserList;
+class ProfileIOS;
 class TabGroup;
 
 namespace web {
