7ceac4c4c07bbc5d70372ad99ded66732feb3294
Sean Maher <spvw@chromium.org>
2024-10-21 17:43:21 +0000

    metrics: record whether users have removable/USB hard drives
    Catan needs this information for our efforts in go/prefetch-summary.
    
    This data isn't currently available, to our knowledge.
    
    Bug: 325307453
    Change-Id: I37fe083214afbcb63c1f171cb57838c39be16aee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921324
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Sean Maher <spvw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371489}

--- components/metrics/drive_metrics_provider.cc
+++ components/metrics/drive_metrics_provider.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/check_op.h"
 #include "base/files/drive_info.h"
-#include "base/files/file.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
@@ -21,6 +20,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/scoped_blocking_call.h"
 
 namespace metrics {
+
+namespace {
+void RecordTriStateMetric(const char* name, std::optional<bool> sample) {
+  base::UmaHistogramEnumeration(
+      name, !sample.has_value()
+                ? DriveMetricsProvider::OptionalBoolRecord::kUnknown
+                : (*sample ? DriveMetricsProvider::OptionalBoolRecord::kTrue
+                           : DriveMetricsProvider::OptionalBoolRecord::kFalse));
+}
+}  // namespace
 
 DriveMetricsProvider::DriveMetricsProvider(int local_state_path_key)
     : local_state_path_key_(local_state_path_key) {}
@@ -46,8 +55,7 @@ void DriveMetricsProvider::AsyncInit(base::OnceClosure done_callback) {
                      weak_ptr_factory_.GetWeakPtr(), std::move(done_callback)));
 }
 
-DriveMetricsProvider::SeekPenaltyResponse::SeekPenaltyResponse()
-    : success(false) {}
+DriveMetricsProvider::SeekPenaltyResponse::SeekPenaltyResponse() = default;
 
 // static
 DriveMetricsProvider::DriveMetrics
@@ -72,12 +80,18 @@ void DriveMetricsProvider::QuerySeekPenalty(
   if (!base::PathService::Get(path_service_key, &path))
     return;
 
-  response->success = HasSeekPenalty(path, &response->has_seek_penalty);
+  bool has_seek_penalty;
+  bool have_value = HasSeekPenalty(path, &has_seek_penalty);
+  if (have_value) {
+    response->has_seek_penalty = has_seek_penalty;
+  }
   std::optional<base::DriveInfo> drive_info = base::GetFileDriveInfo(path);
-  response->success_base =
-      drive_info.has_value() && drive_info->has_seek_penalty.has_value();
-  if (response->success_base) {
-    response->has_seek_penalty_base = *drive_info->has_seek_penalty;
+  if (drive_info.has_value()) {
+    response->has_seek_penalty_base = drive_info->has_seek_penalty;
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+    response->is_removable = drive_info->is_removable;
+    response->is_usb = drive_info->is_usb;
+#endif
   }
 }
 
@@ -92,22 +106,18 @@ void DriveMetricsProvider::GotDriveMetrics(
 void DriveMetricsProvider::FillDriveMetrics(
     const DriveMetricsProvider::SeekPenaltyResponse& response,
     metrics::SystemProfileProto::Hardware::Drive* drive) {
-  if (response.success) {
-    drive->set_has_seek_penalty(response.has_seek_penalty);
+  if (response.has_seek_penalty.has_value()) {
+    drive->set_has_seek_penalty(*response.has_seek_penalty);
   }
 
-  base::UmaHistogramEnumeration(
-      "UMA.SeekPenaltyResult.Provider",
-      !response.success
-          ? SeekPenaltyRecord::kUnknown
-          : (response.has_seek_penalty ? SeekPenaltyRecord::kTrue
-                                       : SeekPenaltyRecord::kFalse));
-  base::UmaHistogramEnumeration(
-      "UMA.SeekPenaltyResult.Base",
-      !response.success_base
-          ? SeekPenaltyRecord::kUnknown
-          : (response.has_seek_penalty_base ? SeekPenaltyRecord::kTrue
-                                            : SeekPenaltyRecord::kFalse));
+  RecordTriStateMetric("UMA.SeekPenaltyResult.Provider",
+                       response.has_seek_penalty);
+  RecordTriStateMetric("UMA.SeekPenaltyResult.Base",
+                       response.has_seek_penalty_base);
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC)
+  RecordTriStateMetric("UMA.DriveIsRemovableResult", response.is_removable);
+  RecordTriStateMetric("UMA.DriveIsUSBResult", response.is_usb);
+#endif
 }
 
 }  // namespace metrics
