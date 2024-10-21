6c645c9a980026b3d88145d957b819d723685abb
David Baron <dbaron@chromium.org>
2024-10-21 15:17:54 +0000

    Reorder Element::HideNonce.
    This function is relatively hot, so the early return that happens in the
    99.99% case should be as early as possible.
    
    (This is probably below the threshold of what we can measure, so I'm not
    going to spend time trying to measure it.)
    
    Change-Id: If71cc21f658d8ea2043b9a19be65ab6743433cd2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940455
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Dominic Farolino <dom@chromium.org>
    Auto-Submit: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371390}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -7309,12 +7309,13 @@ Node* Element::InsertAdjacent(const String& where,
 }
 
 void Element::HideNonce() {
-  if (GetDocument().StatePreservingAtomicMoveInProgress()) {
-    return;
-  }
-
+  // This is a relatively hot codepath.  Get to the common early return as
+  // fast as possible.
   const AtomicString& nonce_value = FastGetAttribute(html_names::kNonceAttr);
   if (nonce_value.empty()) {
+    return;
+  }
+  if (GetDocument().StatePreservingAtomicMoveInProgress()) {
     return;
   }
   if (!InActiveDocument()) {
