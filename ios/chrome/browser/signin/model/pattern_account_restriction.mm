992c525b2b7db37ffa65adde2e22c721422d610b
Marc Treib <treib@chromium.org>
2024-10-21 11:57:41 +0000

    [iOS] Cleanup: PatternAccountRestrictionFromValue() returns a value
    ...instead of an optional.
    The implementation never actually returned nullopt, and the single
    caller didn't handle a nullopt result either.
    
    Bug: none
    Change-Id: If7bc5c0ef3820305cc7f43f26f97f469f5ae67b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942946
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371304}

--- ios/chrome/browser/signin/model/pattern_account_restriction.mm
+++ ios/chrome/browser/signin/model/pattern_account_restriction.mm
@@ -91,7 +91,7 @@ bool ArePatternsValid(const base::Value* value) {
   return true;
 }
 
-std::optional<PatternAccountRestriction> PatternAccountRestrictionFromValue(
+PatternAccountRestriction PatternAccountRestrictionFromValue(
     const base::Value::List& list) {
   std::vector<Pattern> patterns;
   patterns.reserve(list.size());
--- ios/chrome/browser/signin/model/pattern_account_restriction_unittest.mm
+++ ios/chrome/browser/signin/model/pattern_account_restriction_unittest.mm
@@ -25,9 +25,9 @@ TEST_F(PatternAccountRestrictionTest, FilterEmailsWithRestrictions) {
   list.Append("*google.com");
   auto restriction = PatternAccountRestrictionFromValue(list);
 
-  EXPECT_EQ(restriction->IsAccountRestricted(email1), false);
-  EXPECT_EQ(restriction->IsAccountRestricted(email2), false);
-  EXPECT_EQ(restriction->IsAccountRestricted(email3), true);
+  EXPECT_EQ(restriction.IsAccountRestricted(email1), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email2), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email3), true);
 }
 
 // Tests that the PatternAccountRestriction does not filter emails when
@@ -36,9 +36,9 @@ TEST_F(PatternAccountRestrictionTest, FilterEmailsWithoutRestriction) {
   base::Value::List list;
   auto restriction = PatternAccountRestrictionFromValue(list);
 
-  EXPECT_EQ(restriction->IsAccountRestricted(email1), false);
-  EXPECT_EQ(restriction->IsAccountRestricted(email2), false);
-  EXPECT_EQ(restriction->IsAccountRestricted(email3), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email1), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email2), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email3), false);
 }
 
 // Tests that the PatternAccountRestriction does not filter emails when the
@@ -49,9 +49,9 @@ TEST_F(PatternAccountRestrictionTest, FilterEmailsWithBadPattern) {
   list.Append("*google.com");
   auto restriction = PatternAccountRestrictionFromValue(list);
 
-  EXPECT_EQ(restriction->IsAccountRestricted(email1), true);
-  EXPECT_EQ(restriction->IsAccountRestricted(email2), false);
-  EXPECT_EQ(restriction->IsAccountRestricted(email3), true);
+  EXPECT_EQ(restriction.IsAccountRestricted(email1), true);
+  EXPECT_EQ(restriction.IsAccountRestricted(email2), false);
+  EXPECT_EQ(restriction.IsAccountRestricted(email3), true);
 }
 
 // Tests that the pattern created by PatternFromString(chunk) correctlty matches
