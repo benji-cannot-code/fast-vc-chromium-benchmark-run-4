282d43859798c07248c0110957cecba748dcd100
Russ Hamilton <behamilton@google.com>
2024-10-21 20:16:53 +0000

    Fix size estimate in bidding and auction serializer
    We were comparing the value of a uint_64t to 0xFFFFFFFF + 1 + 4, which the C++ compiler decides is equal to 4, instead of 0x100000004. Add type annotation to the constant to specify that it really is a 64 bit number.
    
    Bug: 374327705
    Change-Id: I4807dc2cbf31d551878375952a35430f17e39998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5948304
    Commit-Queue: mmenke <mmenke@chromium.org>
    Auto-Submit: Russ Hamilton <behamilton@google.com>
    Reviewed-by: mmenke <mmenke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371613}

--- content/browser/interest_group/bidding_and_auction_serializer_unittest.cc
+++ content/browser/interest_group/bidding_and_auction_serializer_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/interest_group/bidding_and_auction_serializer.h"
 
+#include <limits>
+
 #include "base/strings/stringprintf.h"
 #include "base/test/metrics/histogram_tester.h"
 #include "base/time/time.h"
@@ -576,6 +578,39 @@ TEST_F(TargetSizeEstimatorTest, ProportionalWorstCase) {
   EXPECT_EQ(estimator.EstimateTargetSize(kOriginB, 100), 108 - kBidderOverhead);
   EXPECT_EQ(estimator.EstimateTargetSize(kOriginC, 208), 110 - kBidderOverhead);
   EXPECT_EQ(estimator.EstimateTargetSize(kOriginD, 318), 111 - kBidderOverhead);
+}
+
+TEST_F(TargetSizeEstimatorTest, LargeRequest) {
+  blink::mojom::AuctionDataConfigPtr config =
+      blink::mojom::AuctionDataConfig::New();
+  config->request_size = std::numeric_limits<uint32_t>::max();
+  config->per_buyer_configs[kOriginA] =
+      blink::mojom::AuctionDataBuyerConfig::New(
+          /*size=*/std::numeric_limits<uint32_t>::max());
+  config->per_buyer_configs[kOriginB] =
+      blink::mojom::AuctionDataBuyerConfig::New(
+          /*size=*/std::numeric_limits<uint32_t>::max());
+  config->per_buyer_configs[kOriginC] =
+      blink::mojom::AuctionDataBuyerConfig::New(
+          /*size=*/std::numeric_limits<uint32_t>::max());
+  config->per_buyer_configs[kOriginD] =
+      blink::mojom::AuctionDataBuyerConfig::New(
+          /*size=*/std::numeric_limits<uint32_t>::max());
+  BiddingAndAuctionSerializer::TargetSizeEstimator estimator(0, &*config);
+  // Values passed to UpdatePerBuyerMaxSize do not include overhead.
+  estimator.UpdatePerBuyerMaxSize(kOriginA, 100 - kBidderOverhead);
+  estimator.UpdatePerBuyerMaxSize(kOriginB, 100 - kBidderOverhead);
+  estimator.UpdatePerBuyerMaxSize(kOriginC, 100 - kBidderOverhead);
+  estimator.UpdatePerBuyerMaxSize(kOriginD, 100 - kBidderOverhead);
+  // Value returned from EstimateTargetSize do not include overhead.
+  EXPECT_EQ(estimator.EstimateTargetSize(kOriginA, 0),
+            1073741820 - kBidderOverhead);
+  EXPECT_EQ(estimator.EstimateTargetSize(kOriginB, 100),
+            1431655728 - kBidderOverhead);
+  EXPECT_EQ(estimator.EstimateTargetSize(kOriginC, 200),
+            2147483544 - kBidderOverhead);
+  EXPECT_EQ(estimator.EstimateTargetSize(kOriginD, 300),
+            4294966992 - kBidderOverhead);
 }
 
 }  // namespace
