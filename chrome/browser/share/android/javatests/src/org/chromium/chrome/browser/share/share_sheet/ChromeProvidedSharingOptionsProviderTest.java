555e234840233a427bcc0b3d7970f92ddaed63cd
Yuzhu Shen <yzshen@chromium.org>
2024-10-21 17:47:09 +0000

    Android desktop: move Print from Share bottom sheet to main menu.
    This change doesn't affect non-desktop Android configuration.
    
    Bug: 357158630
    Change-Id: Ieaf583d4b876822f5e57afc09ba76c235ed368c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941703
    Reviewed-by: Wenyu Fu <wenyufu@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371494}

--- chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProviderTest.java
+++ chrome/browser/share/android/javatests/src/org/chromium/chrome/browser/share/share_sheet/ChromeProvidedSharingOptionsProviderTest.java
@@ -39,6 +39,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.test.BaseRobolectricTestRunner;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.base.test.util.UserActionTester;
+import org.chromium.build.BuildConfig;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.feature_engagement.TrackerFactory;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -196,7 +197,7 @@ public class ChromeProvidedSharingOptionsProviderTest {
     }
 
     @Test
-    public void getPropertyModels_printingEnabled_includesPrinting() {
+    public void getPropertyModels_printingEnabled_includesPrintingIfNotDesktop() {
         setUpChromeProvidedSharingOptionsProviderTest(
                 /* isIncognito= */ false, /* printingEnabled= */ true, LinkGeneration.MAX);
         List<PropertyModel> propertyModels =
@@ -205,9 +206,15 @@ public class ChromeProvidedSharingOptionsProviderTest {
                         DetailedContentType.NOT_SPECIFIED,
                         /* isMultiWindow= */ false);
 
-        assertTrue(
-                "Property models should contain printing.",
-                propertyModelsContain(propertyModels, R.string.print_share_activity_title));
+        if (BuildConfig.IS_DESKTOP_ANDROID) {
+            assertFalse(
+                    "Property models should not contain printing for desktop.",
+                    propertyModelsContain(propertyModels, R.string.print_share_activity_title));
+        } else {
+            assertTrue(
+                    "Property models should contain printing.",
+                    propertyModelsContain(propertyModels, R.string.print_share_activity_title));
+        }
     }
 
     @Test
