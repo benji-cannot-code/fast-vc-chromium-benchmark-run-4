483a0eee96c00fcd491ff125874d1ce39d9255d8
Piotr Bialecki <bialpio@chromium.org>
2024-10-21 19:09:55 +0000

    Spanification: switch to base::HeapArray in arcore_plane_manager.cc
    Bug: 40285824
    Change-Id: I10f526b2db4945922101bd280fa0376ef79e3e2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5940453
    Reviewed-by: Alexander Cooper <alcooper@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371561}

--- device/vr/android/arcore/arcore_plane_manager.cc
+++ device/vr/android/arcore/arcore_plane_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/vr/android/arcore/arcore_plane_manager.h"
 
 #include "base/containers/contains.h"
+#include "base/containers/heap_array.h"
 #include "device/vr/android/arcore/vr_service_type_converters.h"
 
 namespace device {
@@ -246,9 +247,8 @@ mojom::XRPlaneDetectionDataPtr ArCorePlaneManager::GetDetectedPlanesData()
       // points.
       DCHECK(polygon_size % 2 == 0);
 
-      std::unique_ptr<float[]> vertices_raw =
-          std::make_unique<float[]>(polygon_size);
-      ArPlane_getPolygon(arcore_session_, ar_plane, vertices_raw.get());
+      auto vertices_raw = base::HeapArray<float>::Uninit(polygon_size);
+      ArPlane_getPolygon(arcore_session_, ar_plane, vertices_raw.data());
 
       std::vector<mojom::XRPlanePointDataPtr> vertices;
       for (int i = 0; i < polygon_size; i += 2) {
