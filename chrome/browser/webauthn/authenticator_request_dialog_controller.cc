bc1adf84d536dcedaff6f5da9320b271fd9d7d35
Martin Kreichgauer <martinkr@google.com>
2024-10-21 21:11:41 +0000

    webauthn: fix nullptr dereference crash
    Ensure callers of
    ChromeWebAuthnCredentialsDelegateFactory::GetDelegateForFrame() check
    the return value, which may be nullptr.
    
    Fixed: crbug.com/371632355
    Change-Id: I71d06b6ac007d418b64c27448c3f2ae5f0cbad66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5949966
    Auto-Submit: Martin Kreichgauer <martinkr@google.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371653}

--- chrome/browser/webauthn/authenticator_request_dialog_controller.cc
+++ chrome/browser/webauthn/authenticator_request_dialog_controller.cc
@@ -1035,9 +1035,12 @@ void AuthenticatorRequestDialogController::OnRequestComplete() {
     auto* web_contents =
         content::WebContents::FromRenderFrameHost(render_frame_host);
     if (web_contents && render_frame_host) {
-      ChromeWebAuthnCredentialsDelegateFactory::GetFactory(web_contents)
-          ->GetDelegateForFrame(render_frame_host)
-          ->NotifyWebAuthnRequestAborted();
+      ChromeWebAuthnCredentialsDelegate* delegate =
+          ChromeWebAuthnCredentialsDelegateFactory::GetFactory(web_contents)
+              ->GetDelegateForFrame(render_frame_host);
+      if (delegate) {
+        delegate->NotifyWebAuthnRequestAborted();
+      }
     }
   }
   SetCurrentStep(Step::kClosed);
@@ -1852,12 +1855,12 @@ void AuthenticatorRequestDialogController::StartConditionalMediationRequest() {
             weak_factory_.GetWeakPtr()));
   }
 
-  auto* webauthn_credentials_delegate_factory =
+  ChromeWebAuthnCredentialsDelegate* webauthn_credentials_delegate =
       ChromeWebAuthnCredentialsDelegateFactory::GetFactory(web_contents)
           ->GetDelegateForFrame(render_frame_host);
-  if (webauthn_credentials_delegate_factory) {
+  if (webauthn_credentials_delegate) {
     // May be null on tests.
-    webauthn_credentials_delegate_factory->OnCredentialsReceived(
+    webauthn_credentials_delegate->OnCredentialsReceived(
         std::move(credentials),
         ChromeWebAuthnCredentialsDelegate::SecurityKeyOrHybridFlowAvailable(
             is_security_key_or_hybrid_flow_available));
