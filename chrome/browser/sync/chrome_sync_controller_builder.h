cfae02dff2ffc873b69efc16df5326bb8631f7ad
Marc Treib <treib@chromium.org>
2024-10-21 13:52:25 +0000

    Clean up Lacros code in ChromeSyncControllerBuilder
    Bug: 372221465, 365742770
    Change-Id: Ieee57377ac5ec7cf01d6ff89f4f9d165ca6b0ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5946905
    Reviewed-by: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1371345}

--- chrome/browser/sync/chrome_sync_controller_builder.h
+++ chrome/browser/sync/chrome_sync_controller_builder.h
@@ -45,7 +45,7 @@ class WebAppProvider;
 class SpellcheckService;
 #endif  // BUILDFLAG(ENABLE_SPELLCHECK)
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
 namespace app_list {
 class AppListSyncableService;
 }  // namespace app_list
@@ -77,7 +77,7 @@ class DeskSyncService;
 namespace sync_preferences {
 class PrefServiceSyncable;
 }  // namespace sync_preferences
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 // Class responsible for instantiating sync controllers (DataTypeController)
 // for datatypes / features under chrome/.
@@ -114,7 +114,7 @@ class ChromeSyncControllerBuilder {
   void SetWebApkSyncService(webapk::WebApkSyncService* web_apk_sync_service);
 #endif  // BUILDFLAG(IS_ANDROID)
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   void SetAppListSyncableService(
       app_list::AppListSyncableService* app_list_syncable_service);
   void SetAuthorizationZonesManager(
@@ -133,7 +133,7 @@ class ChromeSyncControllerBuilder {
   void SetWifiConfigurationSyncService(
       ash::sync_wifi::WifiConfigurationSyncService*
           wifi_configuration_sync_service);
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
   // Actually builds the controllers. All setters above must have been called
   // beforehand (null may or may not be allowed).
@@ -187,7 +187,7 @@ class ChromeSyncControllerBuilder {
   SafeOptional<raw_ptr<webapk::WebApkSyncService>> web_apk_sync_service_;
 #endif  // BUILDFLAG(IS_ANDROID)
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   SafeOptional<raw_ptr<app_list::AppListSyncableService>>
       app_list_syncable_service_;
   SafeOptional<raw_ptr<ash::printing::oauth2::AuthorizationZonesManager>>
@@ -206,7 +206,7 @@ class ChromeSyncControllerBuilder {
   SafeOptional<raw_ptr<ash::SyncedPrintersManager>> synced_printer_manager_;
   SafeOptional<raw_ptr<ash::sync_wifi::WifiConfigurationSyncService>>
       wifi_configuration_sync_service_;
-#endif  // BUILDFLAG(IS_CHROMEOS_ASH)
+#endif  // BUILDFLAG(IS_CHROMEOS)
 };
 
 #endif  // CHROME_BROWSER_SYNC_CHROME_SYNC_CONTROLLER_BUILDER_H_
