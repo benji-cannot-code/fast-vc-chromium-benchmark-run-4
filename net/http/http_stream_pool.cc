c400b4be4a2eddb99a57cd29d82aade2b6d70cab
Kenichi Ishibashi <bashi@chromium.org>
2024-10-21 04:28:57 +0000

    HttpStreamPool: Remove DCHECK_IS_ON() for consistency checks
    I actually want to run consistency checks for Canary builds.
    
    Bug: 346835898
    Change-Id: I66d648b295b83f446ef2d885b4e44407658bad4b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938568
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371185}

--- net/http/http_stream_pool.cc
+++ net/http/http_stream_pool.cc
@@ -61,8 +61,6 @@ constexpr base::FeatureParam<bool> kEnableConsistencyCheck{
     &features::kHappyEyeballsV3,
     HttpStreamPool::kEnableConsistencyCheckParamName.data(), false};
 
-#if DCHECK_IS_ON()
-
 // Represents total stream counts in the pool. Only used for consistency check.
 struct StreamCounts {
   size_t handed_out = 0;
@@ -85,8 +83,6 @@ std::ostream& operator<<(std::ostream& os, const StreamCounts& counts) {
             << ", idle: " << counts.idle
             << ", connecting: " << counts.connecting << " }";
 }
-
-#endif  // DCHECK_IS_ON()
 
 }  // namespace
 
@@ -602,7 +598,6 @@ void HttpStreamPool::OnPooledStreamRequestComplete(
 }
 
 void HttpStreamPool::CheckConsistency() {
-#if DCHECK_IS_ON()
   CHECK(kEnableConsistencyCheck.Get());
 
   const StreamCounts pool_total_counts = {
@@ -643,8 +638,6 @@ void HttpStreamPool::CheckConsistency() {
       base::BindOnce(&HttpStreamPool::CheckConsistency,
                      weak_ptr_factory_.GetWeakPtr()),
       base::Seconds(3));
-
-#endif  // DCHECK_IS_ON()
 }
 
 }  // namespace net
