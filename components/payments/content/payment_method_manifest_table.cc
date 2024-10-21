b0fed99fd6bb81994c6aa74eb3357a07129f6ca2
Rouslan Solomakhin <rouslan@chromium.org>
2024-10-21 16:53:53 +0000

    [Payments] Log sqlite errors instead of crashing
    Before this patch, failures in web payments sqlite table creation and
    alteration had a NOTREACHED() macro, because only SQL syntax (which
    developers can control) was expected to be a possible failure reason.
    However, the list of possible failure reasons in
    //sql/sqlite_result_code_values.h shows that sqlite can fail for a large
    number of reasons that are not under developer control, such as lack of
    memory, full disk, disk corruption, and so on. This was not noticed
    until NOTREACHED() started to become CHECK():
    https://groups.google.com/a/chromium.org/g/chromium-dev/c/7PuBcFK2l1s/m/lFCar7pfAQAJ
    
    This patch changes the NOTREACHED() macros to LOG(ERROR) macros in the
    payments sqlite table creation and alteration.
    
    After this patch, failures in payments table creation and alteration are
    logged (locally) and handled by the calling code.
    
    Note:
    There is an ongoing discussion of the best way to improve handling of
    sqlite errors, e.g., should SPC enrollment fail earlier, when it is
    known ahead of time that SPC won't be able to save the credential
    identifier in the table?
    
    Fixed: 40947701
    Change-Id: I6dc44a53733e1fb356fa8695e033257485c8ff78
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939089
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Slobodan Pejic <slobodan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371462}

--- components/payments/content/payment_method_manifest_table.cc
+++ components/payments/content/payment_method_manifest_table.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/feature_list.h"
-#include "base/notreached.h"
+#include "base/logging.h"
 #include "base/time/time.h"
 #include "components/payments/core/secure_payment_confirmation_credential.h"
 #include "components/webdata/common/web_database.h"
@@ -52,7 +52,7 @@ bool PaymentMethodManifestTable::CreateTablesIfNecessary() {
                      "expire_date INTEGER NOT NULL DEFAULT 0, "
                      "method_name VARCHAR, "
                      "web_app_id VARCHAR)")) {
-    NOTREACHED_IN_MIGRATION();
+    LOG(ERROR) << "Cannot create the payment_method_manifest table";
     return false;
   }
 
@@ -65,7 +65,8 @@ bool PaymentMethodManifestTable::CreateTablesIfNecessary() {
           "relying_party_id VARCHAR NOT NULL, "
           "label VARCHAR NOT NULL, "
           "icon BLOB NOT NULL)")) {
-    NOTREACHED_IN_MIGRATION();
+    LOG(ERROR)
+        << "Cannot create the secure_payment_confirmation_instrument table";
     return false;
   }
 
@@ -74,7 +75,8 @@ bool PaymentMethodManifestTable::CreateTablesIfNecessary() {
     if (!db()->Execute(
             "ALTER TABLE secure_payment_confirmation_instrument ADD COLUMN "
             "date_created INTEGER NOT NULL DEFAULT 0")) {
-      NOTREACHED_IN_MIGRATION();
+      LOG(ERROR)
+          << "Cannot alter the secure_payment_confirmation_instrument table";
       return false;
     }
   }
@@ -84,7 +86,8 @@ bool PaymentMethodManifestTable::CreateTablesIfNecessary() {
     if (!db()->Execute(
             "ALTER TABLE secure_payment_confirmation_instrument ADD COLUMN "
             "user_id BLOB")) {
-      NOTREACHED_IN_MIGRATION();
+      LOG(ERROR)
+          << "Cannot alter the secure_payment_confirmation_instrument table";
       return false;
     }
   }
