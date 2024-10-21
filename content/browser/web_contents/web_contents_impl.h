8c906b87c093d7075be5a6d9c2babe143d193ae2
Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
2024-10-21 20:08:20 +0000

    Implement Shell Handwriting API controller
    New class StylusHandwritingControllerWin. Per-thread singleton.
    The class encapsulates the Shell Handwriting API. Receives signals from
    the application to notify the Shell Handwriting API of relevant
    interactions, and forwards information back to the renderer process to
    perform hit testing and focus events.
    
    The controller will be lazily initialized only on the pen mouse move
    event to reduce the performance cost during the startup.
    
    Added initialization of stylus_handwriting_enabled web contents pref
    based on the feature flag value. API availability checks will guard
    against processing handwriting-related touch events in case the
    controller hasn't been able to initialize successfully. Covered
    existing methods and member variables
    (WebContentsImpl::SetStylusHandwritingEnabled() and
    stylus_handwriting_enabled_) with android only buildflag as we don't
    intend to use them on Windows.
    
    More tests will be added in follow-up CLs as more functionality is
    implemented.
    
    Bug: 355578906
    Change-Id: I1c9a849c28e5386265b5e6fa0a9fde3f428bb942
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5805258
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Yaroslav Shalivskyy <yshalivskyy@microsoft.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371604}

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1357,9 +1357,11 @@ class CONTENT_EXPORT WebContentsImpl
   // Sets the spatial navigation state.
   void SetSpatialNavigationDisabled(bool disabled);
 
+#if BUILDFLAG(IS_ANDROID)
   // Sets the Stylus handwriting feature status. This status is updated to web
   // preferences.
   void SetStylusHandwritingEnabled(bool enabled);
+#endif  // BUILDFLAG(IS_ANDROID)
 
   // Called when a file selection is to be done.
   void RunFileChooser(
@@ -2427,9 +2429,8 @@ class CONTENT_EXPORT WebContentsImpl
 
   bool is_spatial_navigation_disabled_ = false;
 
-  bool stylus_handwriting_enabled_ = false;
-
 #if BUILDFLAG(IS_ANDROID)
+  bool stylus_handwriting_enabled_ = false;
   bool long_press_link_select_text_ = false;
 #endif
 
