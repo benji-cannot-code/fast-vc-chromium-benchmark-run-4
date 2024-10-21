c7f6cdfd6483c1d361bafaf58d2f3c4dba847ec8
Peter Boström <pbos@chromium.org>
2024-10-21 05:39:28 +0000

    Use is_null() in Callback::Run's CHECK
    This is more obviously null-checking itself (than `holder_.is_null()`).
    I got confused enough by `holder_.is_null()` to ask on Slack, hopefully
    this helps someone else too, or future myself.
    
    Bug: None
    Change-Id: I276f0fc605efd41610797173862e20943630b031
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937389
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371192}

--- base/functional/callback.h
+++ base/functional/callback.h
@@ -145,7 +145,7 @@ class TRIVIAL_ABI OnceCallback<R(Args...)> {
   //
   // May not be called on a null callback.
   R Run(Args... args) && {
-    CHECK(!holder_.is_null());
+    CHECK(!is_null());
 
     // Move the callback instance into a local variable before the invocation,
     // that ensures the internal state is cleared after the invocation.
@@ -334,7 +334,7 @@ class TRIVIAL_ABI RepeatingCallback<R(Args...)> {
   //
   // May not be called on a null callback.
   R Run(Args... args) const& {
-    CHECK(!holder_.is_null());
+    CHECK(!is_null());
 
     // Keep `bind_state` alive at least until after the invocation to ensure all
     // bound `Unretained` arguments remain protected by MiraclePtr.
