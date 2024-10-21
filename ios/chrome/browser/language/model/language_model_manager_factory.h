ab0cf22faaac2bfd517fed241d8a8b0ae2ccf034
Aliona DANGLA <alionadangla@google.com>
2024-10-21 13:20:03 +0000

    [iOS] Remove usage of profile_ios_forward.h
    This Cl remove the usage of profile_ios_forward.h to be normal
    forward-declarations of ProfileIOS
    
    This CL was uploaded by git cl split.
    
    R=iwells@chromium.org
    
    Bug: 359821785
    Change-Id: I86cce0e1cc96339dd4f91d2b966cab4b3da1bd90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938432
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371331}

--- ios/chrome/browser/language/model/language_model_manager_factory.h
+++ ios/chrome/browser/language/model/language_model_manager_factory.h
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/no_destructor.h"
 #import "components/keyed_service/core/keyed_service.h"
 #import "components/keyed_service/ios/browser_state_keyed_service_factory.h"
-#import "ios/chrome/browser/shared/model/profile/profile_ios_forward.h"
+
+class ProfileIOS;
 
 namespace language {
 class LanguageModelManager;
