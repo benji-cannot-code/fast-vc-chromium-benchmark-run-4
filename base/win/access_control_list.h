65bdae09c317afd790dbdd0010799a4f7f8dae9d
Piotr Bialecki <bialpio@chromium.org>
2024-10-21 22:05:15 +0000

    Spanification: switch AccessControlList to use base::HeapArray
    Additionally, improves const-correctness of `AccessControlList` and
    `SecurityDescriptor`.
    
    Changes:
    - Switches `AccessControlList` to use `HeapArray` as the underlying
      storage for the ACL.
    - Make `AccessControlList::get()` const-correct by fixing up const
      version of the method that vends const pointers, and introducing
      non-const version that vends non-const pointers.
    - Switches `SecurityDescriptor::ToAbsolute()` into non-const method
      as it effectively vends non-const pointers by populating a
      `SECURITY_DESCRIPTOR` struct with them.
    - Introduce non-const variants of `SecurityDescriptor` getters
      (`owner()`, `group()`, `dacl()`, `sacl()`) that return non-const
      references.
    - Where needed, introduce calls to `Clone()` for security descriptor
      and access control list. Those have to happen in methods that accept
      const-references but need to call WinAPI methods that accept
      non-const pointers.
    
    Bug: 40285824
    Change-Id: I989a871414e3980fe7045ddd83531063d2b2ffe9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5935105
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371685}

--- base/win/access_control_list.h
+++ base/win/access_control_list.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base_export.h"
+#include "base/containers/heap_array.h"
 #include "base/win/sid.h"
 #include "base/win/windows_types.h"
 
@@ -103,14 +104,21 @@ class BASE_EXPORT AccessControlList {
   // Returns the AccessControlList as a ACL*. The AccessControlList object
   // retains owenership of the pointer. This can return nullptr if the ACL is
   // null.
-  ACL* get() const { return reinterpret_cast<ACL*>(acl_.get()); }
+  ACL* get() { return reinterpret_cast<ACL*>(acl_.data()); }
+  const ACL* get() const { return reinterpret_cast<const ACL*>(acl_.data()); }
 
   // Returns whether the AccessControlList is considered a null ACL.
-  bool is_null() const { return !acl_; }
+  bool is_null() const {
+    // Note: there is a distinction between null ACL (ACL that occupies 0 bytes)
+    // and an empty ACL (an ACL that occupies sizeof(ACL) bytes and has no ACEs
+    // following it). If the underlying storage is empty, it means that we're
+    // dealing with a null ACL.
+    return acl_.empty();
+  }
 
  private:
   explicit AccessControlList(const ACL* acl);
-  std::unique_ptr<uint8_t[]> acl_;
+  base::HeapArray<uint8_t> acl_;
 };
 
 }  // namespace base::win
