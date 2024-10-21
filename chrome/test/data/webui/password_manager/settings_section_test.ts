70737f8cc2952d118cc61be5f1384730d8df1dd1
Ryan Sultanem <rsult@google.com>
2024-10-21 16:42:45 +0000

    [BatchUpload] Add entry point in the Password settings
    Replace the action of the "Save in account" button in the passwords
    settings page to open the batch upload dialog. It used to open the
    password move dialog.
    
    Fixed: b:374726793
    Change-Id: I2c5abefcb695d9a8937ceb01f7332748a66103a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5947748
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1371451}

--- chrome/test/data/webui/password_manager/settings_section_test.ts
+++ chrome/test/data/webui/password_manager/settings_section_test.ts
@@ -28,6 +28,13 @@ import { PasskeysBrowserProxyImpl } from 'chrome://password-manager/password_man
 
 import {TestPasskeysBrowserProxy} from './test_passkeys_browser_proxy.js';
 // </if>
+
+// <if expr="not is_chromeos">
+import {TestPromoCardsProxy} from './test_promo_cards_browser_proxy.js';
+
+import {PromoCardsProxyImpl} from 'chrome://password-manager/password_manager.js';
+// </if>
+
 // clang-format on
 
 /**
@@ -54,6 +61,9 @@ suite('SettingsSectionTest', function() {
   // <if expr="is_win or is_macosx">
   let passkeysProxy: TestPasskeysBrowserProxy;
   // </if>
+  // <if expr="not is_chromeos">
+  let promoCardsProxy: TestPromoCardsProxy;
+  // </if>
 
   setup(function() {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
@@ -63,6 +73,11 @@ suite('SettingsSectionTest', function() {
     OpenWindowProxyImpl.setInstance(openWindowProxy);
     syncProxy = new TestSyncBrowserProxy();
     SyncBrowserProxyImpl.setInstance(syncProxy);
+    // <if expr="not is_chromeos">
+    promoCardsProxy = new TestPromoCardsProxy();
+    PromoCardsProxyImpl.setInstance(promoCardsProxy);
+    // </if>
+
     // <if expr="is_win or is_macosx">
     passkeysProxy = new TestPasskeysBrowserProxy();
     PasskeysBrowserProxyImpl.setInstance(passkeysProxy);
@@ -541,6 +556,10 @@ suite('SettingsSectionTest', function() {
   test(
       'clicking save passwords in account opens move passwords dialog',
       async function() {
+        loadTimeData.overrideValues({
+          isBatchUploadDesktopEnabled: false,
+        });
+
         passwordManager.data.isAccountStorageEnabled = true;
         syncProxy.syncInfo = {
           isEligibleForAccountStorage: true,
@@ -582,6 +601,53 @@ suite('SettingsSectionTest', function() {
         const dialog = moveDialog!.shadowRoot!.querySelector('#dialog');
         assertTrue(!!dialog);
       });
+
+  // <if expr="not is_chromeos">
+  test(
+      'clicking save passwords in account opens batch upload dialog',
+      async function() {
+        loadTimeData.overrideValues({
+          isBatchUploadDesktopEnabled: true,
+        });
+
+        passwordManager.data.isAccountStorageEnabled = true;
+        syncProxy.syncInfo = {
+          isEligibleForAccountStorage: true,
+          isSyncingPasswords: false,
+        };
+
+        const group = createCredentialGroup({
+          name: 'test.com',
+          credentials: [
+            createPasswordEntry({
+              id: 0,
+              username: 'test1',
+              inProfileStore: true,
+              inAccountStore: false,
+            }),
+          ],
+        });
+
+        passwordManager.data.groups = [group];
+        passwordManager.setRequestCredentialsDetailsResponse(
+            passwordManager.data.groups[0]!.entries);
+
+        const settings = document.createElement('settings-section');
+        document.body.appendChild(settings);
+        await passwordManager.whenCalled('getSavedPasswordList');
+        await flushTasks();
+
+        const movePasswordsButton =
+            settings.shadowRoot!.getElementById('movePasswordsButton');
+        assertTrue(!!movePasswordsButton);
+        assertTrue(isVisible(movePasswordsButton));
+
+        movePasswordsButton!.click();
+        await flushTasks();
+
+        await promoCardsProxy.whenCalled('openBatchUpload');
+      });
+  // </if>
 
   test('Account storage iph', async function() {
     loadTimeData.overrideValues({canAddShortcut: false});
