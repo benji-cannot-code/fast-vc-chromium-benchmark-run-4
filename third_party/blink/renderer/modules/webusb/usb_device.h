ca1445a8dcb73ce6a884b13a65fc7e12c1accb92
Reilly Grant <reillyg@chromium.org>
2024-10-22 18:43:22 +0000

    webusb: Use [PassAsSpan] to handle BufferSource arguments
    This change simplifies the implementation of WebUSB methods which accept
    a BufferSource argument by using the [PassAsSpan] WebIDL attribute.
    
    Using this requires a behavior change: Passing a detached buffer is now
    no longer fatal (except when it causes other argument validation errors)
    because a detached buffer turns into an empty span. I think this is a
    safe change to make because it matches the WebIDL rules for handling
    detached buffers (throwing a specific error was unspecified behavior)
    and this case is developer error, so sites shouldn't be depending on it.
    
    Change-Id: I8af158d51b1760625984f697eac880ecd808d13b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5924242
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Auto-Submit: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372209}

--- third_party/blink/renderer/modules/webusb/usb_device.h
+++ third_party/blink/renderer/modules/webusb/usb_device.h
@@ -101,7 +101,7 @@ class USBDevice : public ScriptWrappable,
   ScriptPromise<USBOutTransferResult> controlTransferOut(
       ScriptState*,
       const USBControlTransferParameters* setup,
-      const DOMArrayPiece& optional_data,
+      base::span<const uint8_t> data,
       ExceptionState&);
   ScriptPromise<IDLUndefined> clearHalt(ScriptState*,
                                         const V8USBDirection& direction,
@@ -111,10 +111,11 @@ class USBDevice : public ScriptWrappable,
                                                 uint8_t endpoint_number,
                                                 unsigned length,
                                                 ExceptionState&);
-  ScriptPromise<USBOutTransferResult> transferOut(ScriptState*,
-                                                  uint8_t endpoint_number,
-                                                  const DOMArrayPiece& data,
-                                                  ExceptionState&);
+  ScriptPromise<USBOutTransferResult> transferOut(
+      ScriptState*,
+      uint8_t endpoint_number,
+      base::span<const uint8_t> data,
+      ExceptionState&);
   ScriptPromise<USBIsochronousInTransferResult> isochronousTransferIn(
       ScriptState*,
       uint8_t endpoint_number,
@@ -123,7 +124,7 @@ class USBDevice : public ScriptWrappable,
   ScriptPromise<USBIsochronousOutTransferResult> isochronousTransferOut(
       ScriptState*,
       uint8_t endpoint_number,
-      const DOMArrayPiece& data,
+      base::span<const uint8_t> data,
       Vector<unsigned> packet_lengths,
       ExceptionState&);
   ScriptPromise<IDLUndefined> reset(ScriptState*, ExceptionState&);
