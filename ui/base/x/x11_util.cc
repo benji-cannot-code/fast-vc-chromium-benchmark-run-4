bfad993fa95cb82bb17e3b0b83f4fc76f702aa65
Maksim Sisov <msisov@igalia.com>
2024-10-22 19:41:45 +0000

    Rename IS_CHROMEOS_ASH to IS_CHROMEOS in //ui/base/x
    IS_CHROMEOS_ASH was used to tell it was chromeos/ash as IS_CHROMEOS
    meant both lacros and ash. Given lacros is gone, it's safe to
    rename that back.
    
    Bug: 373972275, 374122148
    Change-Id: Ieab7500547792fda69258e5b8e2e22aaa99f07f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952967
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1372249}

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/single_thread_task_runner.h"
 #include "base/trace_event/trace_event.h"
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkImageInfo.h"
 #include "ui/base/cursor/mojom/cursor_type.mojom-shared.h"
