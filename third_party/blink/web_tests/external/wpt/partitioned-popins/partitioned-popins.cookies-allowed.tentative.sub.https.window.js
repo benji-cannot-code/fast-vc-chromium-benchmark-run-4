d8426275465a95ffe299769079adc3603cf5fe2c
Ari Chivukula <arichiv@chromium.org>
2024-10-22 16:37:21 +0000

    [Partitioned Popins] Add third-party cookie blocked tests
    Before we go ahead and add requestStorageAccess support, we should
    verify behavior when 3pc are blocked.
    
    Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    
    Bug: 340606651
    Change-Id: I65ce0ff708b8c44f34561fb104fc9e7891eb44e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954365
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Commit-Queue: Sandor «Alex» Major <sandormajor@google.com>
    Reviewed-by: Sandor «Alex» Major <sandormajor@google.com>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372104}

--- third_party/blink/web_tests/external/wpt/partitioned-popins/partitioned-popins.cookies-allowed.tentative.sub.https.window.js
+++ third_party/blink/web_tests/external/wpt/partitioned-popins/partitioned-popins.cookies-allowed.tentative.sub.https.window.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: script=/resources/testdriver.js
 // META: script=/resources/testdriver-vendor.js
+// META: script=/storage-access-api/helpers.js
 
 'use strict';
 
@@ -40,6 +41,8 @@ async_test(t => {
     }
   }));
 
-  // Step 2
-  window.open("https://{{hosts[alt][]}}:{{ports[https][0]}}/partitioned-popins/resources/partitioned-popins.cookies-window.html?id="+id, '_blank', 'popup');
-}, "Verify Partitioned Popins only have access to third-party cookies");
+  MaybeSetStorageAccess("*", "*", "allowed").then(() => {
+    // Step 2
+    window.open("https://{{hosts[alt][]}}:{{ports[https][0]}}/partitioned-popins/resources/partitioned-popins.cookies-window.html?id="+id, '_blank', 'popup');
+  });
+}, "Verify Partitioned Popins cookie access when third-party cookie access allowed");
