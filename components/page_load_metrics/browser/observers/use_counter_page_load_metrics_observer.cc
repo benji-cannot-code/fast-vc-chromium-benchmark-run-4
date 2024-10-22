d2acf95ae407ea90fce91bc8dedd252f712d6d4b
DanielRyanSmith <danielrsmith@google.com>
2024-10-22 23:34:49 +0000

    Add WebDXFeature use counter for Element.checkVisibility
    This adds a mapping of existing WebFeature use counters into a new
    WebDXFeature use counter.
    
    This is an effort to add new telemetry counters to features shipped since 2020.
    
    Bug: 366475997
    Change-Id: Icb6fa19e287e1ee367f7e6b3bce1c5cc27f8ce20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5945186
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Daniel Smith <danielrsmith@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372405}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -472,6 +472,7 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kInertAttribute, WebDXFeature::kInert},
           {WebFeature::kSegmenter, WebDXFeature::kIntlSegmenter},
           {WebFeature::kEyeDropperOpen, WebDXFeature::kEyedropper},
+          {WebFeature::kElementCheckVisibility, WebDXFeature::kCheckVisibility},
       }};
 
   return *kMap;
