f7734a412547a396cbcc2a22d83dd86d39c4ffd3
Steve Kobes <skobes@chromium.org>
2024-10-22 17:26:30 +0000

    Ignore native platform mouse events in web tests.
    For web tests this stops WindowEventDispatcher::SynthesizeMouseMoveEvent
    from sending the renderer a mousemove, which it normally does on startup
    and whenever we swap in a new RenderWidgetHostView.
    
    The benefits of this change are:
    
    - Eliminating potential flakiness by making the web tests more order-
      agnostic.  Tests can behave differently with the mouse at (0,0) versus
      the position-unknown state which is restored in between test runs by
      EventHandler::ResetLastMousePositionForWebTest.  This also helps to
      de-risk upcoming web test isolation changes (go/web-test-isolation).
    
    - Aligning Windows and Linux behavior with Mac.  AppKit sends a native
      mousemove to a new on-screen RWHV, similar to what Aura synthesizes.
      But AppKit does NOT send these mousemoves to headless web tests.
    
    Bug: 374783343
    Change-Id: I6b1d7234410cd6fa799954845bb5b50f5a2b82ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5808379
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372147}

--- content/web_test/browser/web_test_shell_platform_delegate_views.cc
+++ content/web_test/browser/web_test_shell_platform_delegate_views.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/web_test/browser/web_test_shell_platform_delegate.h"
-
 #include "base/containers/contains.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/shell/browser/shell.h"
 #include "content/shell/browser/shell_platform_data_aura.h"
+#include "content/web_test/browser/web_test_shell_platform_delegate.h"
+#include "ui/events/platform/platform_event_source.h"
 
 namespace content {
 
@@ -34,6 +34,11 @@ void WebTestShellPlatformDelegate::Initialize(
     web_test_platform_ = std::make_unique<WebTestPlatformData>();
     web_test_platform_->aura =
         std::make_unique<ShellPlatformDataAura>(default_window_size);
+
+    // This suppresses mousemove events from WindowEventDispatcher, which
+    // matches Mac behavior (AppKit will send a mousemove to a new
+    // RenderWidgetHostView when it is onscreen, but not when headless).
+    ui::PlatformEventSource::SetIgnoreNativePlatformEvents(true);
   }
 }
 
