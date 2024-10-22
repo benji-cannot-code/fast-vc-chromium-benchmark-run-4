d1123f02fd33994be24db9c56ebcb0679df88b3d
Noam Rosenthal <nrosenthal@chromium.org>
2024-10-22 16:55:54 +0000

    View transition layered capture: capture borders separately
    The border shorthand property is insufficient when the borders are
    uneven.
    
    Bug: 373759981
    Change-Id: I06a6a99145c6ff06a4fe78cb738327adbe4db2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5946903
    Commit-Queue: Noam Rosenthal <nrosenthal@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372116}

--- third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
+++ third_party/blink/renderer/core/view_transition/view_transition_style_tracker.cc
@@ -84,7 +84,10 @@ CSSPropertyID kLayeredCaptureProperties[] = {
     // Deliberately capturing the shorthand, to include all the mask-related
     // properties.
     CSSPropertyID::kMask,
-    CSSPropertyID::kBorder,
+    CSSPropertyID::kBorderTop,
+    CSSPropertyID::kBorderRight,
+    CSSPropertyID::kBorderBottom,
+    CSSPropertyID::kBorderLeft,
     CSSPropertyID::kBackground,
     CSSPropertyID::kBorderRadius,
     CSSPropertyID::kBoxShadow,
@@ -99,10 +102,12 @@ CSSPropertyID kLayeredCaptureProperties[] = {
 CSSPropertyID kPropertiesToAnimate[] = {
     CSSPropertyID::kBackdropFilter, CSSPropertyID::kOpacity,
     CSSPropertyID::kClipPath,       CSSPropertyID::kFilter,
-    CSSPropertyID::kMask,           CSSPropertyID::kBorder,
-    CSSPropertyID::kBackground,     CSSPropertyID::kBorderRadius,
-    CSSPropertyID::kBoxShadow,      CSSPropertyID::kOutline,
-    CSSPropertyID::kBorderImage,    CSSPropertyID::kPadding,
+    CSSPropertyID::kMask,           CSSPropertyID::kBorderTop,
+    CSSPropertyID::kBorderRight,    CSSPropertyID::kBorderBottom,
+    CSSPropertyID::kBorderLeft,     CSSPropertyID::kBackground,
+    CSSPropertyID::kBorderRadius,   CSSPropertyID::kBoxShadow,
+    CSSPropertyID::kOutline,        CSSPropertyID::kBorderImage,
+    CSSPropertyID::kPadding,
 };
 
 template <typename K, typename V>
