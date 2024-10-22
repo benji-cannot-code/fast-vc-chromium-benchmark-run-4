d2a49a5b5f7b9ff4b7df496f7fd3cf673e698470
Rohit Rao <rohitrao@chromium.org>
2024-10-22 16:22:54 +0000

    [ios] Histogram to measure time since the last successful Omaha ping.
    Bug: 365549194
    Change-Id: I8c3bedcc3dec82b387f31fc67a89bdf6031f6ccb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953614
    Commit-Queue: Rohit Rao <rohitrao@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372093}

--- ios/chrome/browser/omaha/model/omaha_service.mm
+++ ios/chrome/browser/omaha/model/omaha_service.mm
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/logging.h"
 #import "base/memory/raw_ptr.h"
 #import "base/metrics/field_trial.h"
+#import "base/metrics/histogram_functions.h"
 #import "base/no_destructor.h"
 #import "base/rand_util.h"
 #import "base/strings/stringprintf.h"
@@ -905,6 +906,7 @@ void OmahaService::OnURLLoadComplete(
           ? base::Time::Now()
           : base::Time::Now() + base::Hours(kHoursBetweenRequests);
   current_ping_time_ = next_tries_time_;
+  base::Time original_last_sent_time = last_sent_time_;
   last_sent_time_ = base::Time::Now();
   last_sent_version_ = version_info::GetVersion();
   sending_install_event_ = false;
@@ -912,6 +914,11 @@ void OmahaService::OnURLLoadComplete(
   ClearInstallRetryRequestId();
   PersistStates();
   bool need_to_schedule_ping = true;
+
+  // Log metrics.
+  base::TimeDelta success_delta = last_sent_time_ - original_last_sent_time;
+  base::UmaHistogramCounts1000("IOS.Omaha.HoursSinceLastSuccess",
+                               success_delta.InHours());
 
   // Send notification for updates if needed.
   UpgradeRecommendedDetails* details = [delegate upgradeRecommendedDetails];
