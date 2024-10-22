893d7ee214bddc8f94b62faf7060c0be43ff261d
Jordan Bayles <jophba@chromium.org>
2024-10-22 20:57:58 +0000

    [Cast Streaming] Remove unused VideoFrameFactory feature
    This patch removes an unused VideoFrameFactory feature, paving
    the way for migrating from the media::cast VideoEncoder implementation
    to the media VideoEncoder implementation.
    
    Bug: 282984511
    Change-Id: I2d7cf9166d55abd821c6fd39456ae728bba06003
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5939300
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372308}

--- media/cast/sender/video_sender_unittest.cc
+++ media/cast/sender/video_sender_unittest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/video_frame.h"
 #include "media/cast/cast_environment.h"
 #include "media/cast/common/openscreen_conversion_helpers.h"
-#include "media/cast/common/video_frame_factory.h"
 #include "media/cast/constants.h"
 #include "media/cast/logging/simple_event_subscriber.h"
 #include "media/cast/test/fake_openscreen_clock.h"
@@ -245,12 +244,6 @@ TEST_F(VideoSenderTest, ExternalEncoderInitFails) {
   openscreen_video_sender_ = nullptr;
   video_sender_.reset();
   task_runner_->RunTasks();
-}
-
-TEST_F(VideoSenderTest, CheckVideoFrameFactoryIsNull) {
-  InitEncoder(false, true);
-  ASSERT_EQ(STATUS_INITIALIZED, operational_status_);
-  EXPECT_EQ(nullptr, video_sender_->CreateVideoFrameFactory().get());
 }
 
 }  // namespace media::cast
