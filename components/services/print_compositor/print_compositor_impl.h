3a5b1b2032378dbee15c2cef24683921a5c90835
Nasser Al-shawwa <alshawwa@chromium.org>
2024-10-22 17:37:34 +0000

    Update print compositor interface to accept watermark string.
    Previously, we added watermark rendering to the print compositor
    using a hard-coded string, since the watermark string lives
    in the browser process while the print compositor runs in its
    own service process. This change updates the compositor's
    mojo interface in order for it to be able to receive a the
    watermark text from the browser process.
    
    Bug: 356446812
    Change-Id: I76e92c96f74503431b584d002b393ebc0bbe59b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5907136
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Nasser Al-shawwa <alshawwa@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372154}

--- components/services/print_compositor/print_compositor_impl.h
+++ components/services/print_compositor/print_compositor_impl.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
 #include "build/build_config.h"
+#include "components/enterprise/buildflags/buildflags.h"
 #include "components/services/print_compositor/public/cpp/print_service_mojo_types.h"
 #include "components/services/print_compositor/public/mojom/print_compositor.mojom.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/accessibility/ax_tree_update.h"
 
 class SkDocument;
+struct SkDocumentPage;
 
 namespace base {
 class SingleThreadTaskRunner;
@@ -96,6 +98,14 @@ class PrintCompositorImpl : public mojom::PrintCompositor {
   void SetGenerateDocumentOutline(
       mojom::GenerateDocumentOutline generate_document_outline) override;
   void SetTitle(const std::string& title) override;
+  void SetWatermarkText(const std::string& watermark_text) override;
+
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+  // Print UX requirement for watermarking. Values are in pixels. Constants
+  // exposed for testing.
+  static constexpr int kWatermarkBlockWidth = 350;
+  static constexpr float kWatermarkTextSize = 24.0f;
+#endif
 
  protected:
   // This is the uniform underlying type for both
@@ -121,6 +131,9 @@ class PrintCompositorImpl : public mojom::PrintCompositor {
       mojom::PrintCompositor::DocumentType document_type);
 
   // Make these functions virtual so tests can override them.
+  virtual void DrawPage(SkDocument* doc,
+                        const SkDocumentPage& page,
+                        const std::string& watermark_text);
   virtual void FulfillRequest(
       base::span<const uint8_t> serialized_content,
       const ContentToFrameMap& subframe_content_map,
@@ -133,6 +146,7 @@ class PrintCompositorImpl : public mojom::PrintCompositor {
   FRIEND_TEST_ALL_PREFIXES(PrintCompositorImplTest, IsReadyToComposite);
   FRIEND_TEST_ALL_PREFIXES(PrintCompositorImplTest, MultiLayerDependency);
   FRIEND_TEST_ALL_PREFIXES(PrintCompositorImplTest, DependencyLoop);
+
   friend class MockCompletionPrintCompositorImpl;
 
   // The map needed during content deserialization. It stores the mapping
@@ -281,7 +295,18 @@ class PrintCompositorImpl : public mojom::PrintCompositor {
 
   // The title of the document.
   std::string title_;
+
+  // The watermark text.
+  std::string watermark_text_;
 };
+
+#if BUILDFLAG(ENTERPRISE_WATERMARK)
+// Draw the watermark specified by `watermark_text` using the provided canvas
+// and its size. Exposed for testing.
+void DrawEnterpriseWatermark(SkCanvas* canvas,
+                             SkSize size,
+                             const std::string& watermark_text);
+#endif
 
 }  // namespace printing
 
