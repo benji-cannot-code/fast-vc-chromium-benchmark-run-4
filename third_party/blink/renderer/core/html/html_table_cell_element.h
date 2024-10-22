65e5c5f73b015a70828f258ade60004a338e7120
David Baron <dbaron@chromium.org>
2024-10-22 17:50:05 +0000

    Remove unused IsElementOfType function templates.
    These probably could have been removed in
    https://crrev.com/5a5c8b99807429f49a410f035ea7caa7b09515e2
    
    Change-Id: I803a0435002325e55b071d50bdef6f83cc9c9b72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5949607
    Commit-Queue: David Baron <dbaron@chromium.org>
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372162}

--- third_party/blink/renderer/core/html/html_table_cell_element.h
+++ third_party/blink/renderer/core/html/html_table_cell_element.h
@@ -68,10 +68,6 @@ class CORE_EXPORT HTMLTableCellElement final : public HTMLTablePartElement {
 };
 
 template <>
-inline bool IsElementOfType<const HTMLTableCellElement>(const Node& node) {
-  return IsA<HTMLTableCellElement>(node);
-}
-template <>
 struct DowncastTraits<HTMLTableCellElement> {
   static bool AllowFrom(const Node& node) {
     auto* html_element = DynamicTo<HTMLElement>(node);
