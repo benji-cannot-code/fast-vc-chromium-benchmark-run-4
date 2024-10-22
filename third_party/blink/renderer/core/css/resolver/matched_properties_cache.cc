de6f9528117613e5cfce4b481dc2f495b9f607b2
Steinar H. Gunderson <sesse@chromium.org>
2024-10-22 20:57:01 +0000

    Some cleanups in MPC keying.
    Notably, we remove the notion of “invalid” keys; they can never happen
    anymore, and we don't use them to signal uncacheable states. We also
    deal more methodically with hash collisions (forcibly remove the
    offending entries should it happen), preparing for the future case of
    MPC multimaps. This means we no longer need to update the PropertySets
    of an existing cache item.
    
    It is still hard to get a full overview of all of our “is X cacheable”
    code, but that is out-of-scope for this patch.
    
    There are some minor Speedometer3 changes (in particular, a ~1.3% win
    for TodoMVC-Lit-Complex-DOM), but it's not clear why, so they may be
    spurious.
    
    Change-Id: I35cea90e4e0b59e3c81b9fad13ad13565168f90b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953324
    Reviewed-by: Anders Hartvoll Ruud <andruud@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372307}

--- third_party/blink/renderer/core/css/resolver/matched_properties_cache.cc
+++ third_party/blink/renderer/core/css/resolver/matched_properties_cache.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace blink {
 
 static unsigned ComputeMatchedPropertiesHash(const MatchResult& result) {
+  DCHECK(result.IsCacheable());
   const MatchedPropertiesHashVector& hashes = result.GetMatchedPropertiesHash();
   DCHECK(!std::any_of(hashes.begin(), hashes.end(),
                       [](const MatchedPropertiesHash& hash) {
@@ -52,8 +53,16 @@ static unsigned ComputeMatchedPropertiesHash(const MatchResult& result) {
                                WTF::HashTraits<unsigned>::DeletedValue();
                       }))
       << "This should have been checked in AddMatchedProperties()";
-  return StringHasher::HashMemory(hashes.data(),
-                                  sizeof(hashes[0]) * hashes.size());
+  unsigned hash = StringHasher::HashMemory(hashes.data(),
+                                           sizeof(hashes[0]) * hashes.size());
+
+  // See CSSPropertyValueSet::ComputeHash() for asserts that this is safe.
+  if (hash == WTF::HashTraits<unsigned>::EmptyValue() ||
+      hash == WTF::HashTraits<unsigned>::DeletedValue()) {
+    hash ^= 0x80000000;
+  }
+
+  return hash;
 }
 
 CachedMatchedProperties::CachedMatchedProperties(
@@ -65,21 +74,6 @@ CachedMatchedProperties::CachedMatchedProperties(
       parent_computed_style(parent_style),
       last_used(clock) {
   matched_properties.ReserveInitialCapacity(properties.size());
-  for (const auto& new_matched_properties : properties) {
-    matched_properties.emplace_back(new_matched_properties.properties,
-                                    new_matched_properties.data_);
-  }
-}
-
-void CachedMatchedProperties::Set(const ComputedStyle* style,
-                                  const ComputedStyle* parent_style,
-                                  const MatchedPropertiesVector& properties,
-                                  unsigned clock) {
-  computed_style = style;
-  parent_computed_style = parent_style;
-  last_used = clock;
-
-  matched_properties.clear();
   for (const auto& new_matched_properties : properties) {
     matched_properties.emplace_back(new_matched_properties.properties,
                                     new_matched_properties.data_);
@@ -143,8 +137,6 @@ MatchedPropertiesCache::Key::Key(const MatchResult& result, unsigned hash)
 const CachedMatchedProperties* MatchedPropertiesCache::Find(
     const Key& key,
     const StyleResolverState& style_resolver_state) {
-  DCHECK(key.IsValid());
-
   // Matches the corresponding test in IsStyleCacheable().
   if (style_resolver_state.TextAutosizingMultiplier() != 1.0f) {
     return nullptr;
@@ -155,12 +147,15 @@ const CachedMatchedProperties* MatchedPropertiesCache::Find(
     return nullptr;
   }
   CachedMatchedProperties* cache_item = it->value.Get();
-  if (!cache_item) {
-    return nullptr;
-  }
   if (!cache_item->CorrespondsTo(key.result_.GetMatchedProperties())) {
+    // A hash collision (rare), or the key is not usable anymore.
+    // Take out the existing entry entirely and start anew.
+    // (We could possibly have reused its memory, but for simplicity,
+    // we just treat it as a miss.)
+    cache_.erase(it);
     return nullptr;
   }
+
   if (IsAtShadowBoundary(&style_resolver_state.GetElement()) &&
       cache_item->parent_computed_style->UserModify() !=
           ComputedStyleInitialValues::InitialUserModify()) {
@@ -216,8 +211,6 @@ bool CachedMatchedProperties::CorrespondsTo(
 void MatchedPropertiesCache::Add(const Key& key,
                                  const ComputedStyle* style,
                                  const ComputedStyle* parent_style) {
-  DCHECK(key.IsValid());
-
   Member<CachedMatchedProperties>& cache_item =
       cache_.insert(key.hash_, nullptr).stored_value->value;
 
@@ -225,8 +218,9 @@ void MatchedPropertiesCache::Add(const Key& key,
     cache_item = MakeGarbageCollected<CachedMatchedProperties>(
         style, parent_style, key.result_.GetMatchedProperties(), clock_++);
   } else {
-    cache_item->Set(style, parent_style, key.result_.GetMatchedProperties(),
-                    clock_++);
+    // Note that we don't need to update the properties; they were already
+    // verified to be correct in Find().
+    cache_item->Set(style, parent_style, clock_++);
   }
 }
 
