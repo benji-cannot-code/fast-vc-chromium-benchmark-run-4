2cce12a9e0e2c9801ee88874299271f3276302a8
Sammie Quon <sammiequon@chromium.org>
2024-10-22 19:42:55 +0000

    cros: Improve debugging for WindowInfo
    Json is more standard and looks prettier, and would support nested
    classes better if we wanted to expand in the future.
    
    Bug: None
    Change-Id: Idedc93d73ef712d4e0271de7a1d89b91702f6c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953806
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372251}

--- ash/birch/birch_coral_item.cc
+++ ash/birch/birch_coral_item.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/desks/desk.h"
 #include "ash/wm/desks/desks_controller.h"
 #include "base/barrier_callback.h"
-#include "base/json/json_writer.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_operations.h"
 
@@ -108,7 +107,7 @@ std::string BirchCoralItem::ToString() const {
   auto root = base::Value::Dict().Set(
       "Coral item",
       base::Value::Dict().Set("Title", title()).Set("Subtitle", subtitle()));
-  return base::WriteJson(root).value_or(std::string());
+  return root.DebugString();
 }
 
 void BirchCoralItem::PerformAction(bool is_post_login) {
