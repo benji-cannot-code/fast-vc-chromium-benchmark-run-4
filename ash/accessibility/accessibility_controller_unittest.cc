e197bd9655c51d5cc35945d12ca79f9189eb6abc
LK Kupoluyi <lkupo@chromium.org>
2024-10-22 21:19:26 +0000

    [DisableTrackpad] Implement 30 second timer for confirmation dialog
    Bug: 368059319
    Change-Id: Ie749db3f43df040136b54b98a2ad6230ec517765
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5933784
    Commit-Queue: Jesulayomi Kupoluyi <lkupo@google.com>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372322}

--- ash/accessibility/accessibility_controller_unittest.cc
+++ ash/accessibility/accessibility_controller_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/accessibility/magnifier/docked_magnifier_controller.h"
 #include "ash/accessibility/sticky_keys/sticky_keys_controller.h"
 #include "ash/accessibility/test_accessibility_controller_client.h"
+#include "ash/accessibility/ui/accessibility_confirmation_dialog.h"
 #include "ash/constants/ash_constants.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
@@ -65,8 +66,9 @@ namespace {
 constexpr char kDictationLanguageUpgradedNudgeId[] =
     "dictation_language_upgraded.nudge_id";
 
-const int kUsbMouseDeviceId = 20;
+const int kDialogTimeoutSeconds = 30;
 const int kInternalTouchpadDeviceId = 30;
+const int kUsbMouseDeviceId = 20;
 
 ui::InputDevice GetSampleMouseUsb() {
   return {kUsbMouseDeviceId, ui::INPUT_DEVICE_USB, "SampleMouseUsb"};
@@ -2391,7 +2393,8 @@ TEST_F(AccessibilityControllerSelectToSpeakKeyboardShortcutTest,
 
 class AccessibilityControllerDisableTouchpadTest : public AshTestBase {
  protected:
-  AccessibilityControllerDisableTouchpadTest() = default;
+  AccessibilityControllerDisableTouchpadTest()
+      : AshTestBase(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {}
   AccessibilityControllerDisableTouchpadTest(
       const AccessibilityControllerDisableTouchpadTest&) = delete;
   AccessibilityControllerDisableTouchpadTest& operator=(
@@ -2415,6 +2418,13 @@ class AccessibilityControllerDisableTouchpadTest : public AshTestBase {
 
   PrefService* prefs() {
     return Shell::Get()->session_controller()->GetLastActiveUserPrefService();
+  }
+
+  base::RepeatingTimer& confirmation_dialog_timer() {
+    return Shell::Get()
+        ->accessibility_controller()
+        ->GetConfirmationDialogForTest()
+        ->timer_;
   }
 
   DisableTouchpadEventRewriter* disable_touchpad_event_rewriter() {
@@ -2596,6 +2606,45 @@ TEST_F(AccessibilityControllerDisableTouchpadTest,
   SimulateExternalMouseConnected();
   ASSERT_NE(nullptr, controller()->GetConfirmationDialogForTest());
   ASSERT_TRUE(disable_touchpad_event_rewriter()->IsEnabled());
+}
+
+TEST_F(AccessibilityControllerDisableTouchpadTest, DialogClosesAfter30Seconds) {
+  SimulateOnlyInternalTouchpadConnected();
+  // Ensure confirmation dialog closes after 30 seconds when trackpad mode
+  // is set to "on mouse connected".
+  prefs()->SetInteger(
+      prefs::kAccessibilityDisableTrackpadMode,
+      static_cast<int>(DisableTouchpadMode::kOnExternalMouseConnected));
+
+  ASSERT_FALSE(disable_touchpad_event_rewriter()->IsEnabled());
+
+  SimulateExternalMouseConnected();
+  ASSERT_TRUE(disable_touchpad_event_rewriter()->IsEnabled());
+  ASSERT_NE(nullptr, controller()->GetConfirmationDialogForTest());
+  ASSERT_TRUE(confirmation_dialog_timer().IsRunning());
+
+  for (int i = 0; i < kDialogTimeoutSeconds; ++i) {
+    ASSERT_NE(nullptr, controller()->GetConfirmationDialogForTest());
+    task_environment()->FastForwardBy(base::Seconds(1));
+  }
+
+  ASSERT_FALSE(disable_touchpad_event_rewriter()->IsEnabled());
+  ASSERT_EQ(nullptr, controller()->GetConfirmationDialogForTest());
+
+  // Ensure confirmation dialog closes after 30 seconds when trackpad mode
+  // is set to "always".
+  prefs()->SetInteger(prefs::kAccessibilityDisableTrackpadMode,
+                      static_cast<int>(DisableTouchpadMode::kAlways));
+  ASSERT_TRUE(disable_touchpad_event_rewriter()->IsEnabled());
+  ASSERT_TRUE(confirmation_dialog_timer().IsRunning());
+
+  for (int i = 0; i < kDialogTimeoutSeconds; ++i) {
+    ASSERT_NE(nullptr, controller()->GetConfirmationDialogForTest());
+    task_environment()->FastForwardBy(base::Seconds(1));
+  }
+
+  ASSERT_FALSE(disable_touchpad_event_rewriter()->IsEnabled());
+  ASSERT_EQ(nullptr, controller()->GetConfirmationDialogForTest());
 }
 
 }  // namespace ash
--- ash/accessibility/ui/accessibility_confirmation_dialog.cc
+++ ash/accessibility/ui/accessibility_confirmation_dialog.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "base/functional/bind.h"
+#include "base/strings/strcat.h"
+#include "base/strings/string_util.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/l10n/time_format.h"
 #include "ui/base/mojom/dialog_button.mojom.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
 #include "ui/base/ui_base_types.h"
@@ -25,6 +28,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+namespace {
+
+std::u16string GetMessageWithTimeout(
+    const std::u16string& timeout_message_with_placeholder,
+    int time_remaining) {
+  const std::u16string remaining_time = ui::TimeFormat::Simple(
+      ui::TimeFormat::FORMAT_DURATION, ui::TimeFormat::LENGTH_LONG,
+      base::Seconds(time_remaining));
+  return base::ReplaceStringPlaceholders(timeout_message_with_placeholder,
+                                         remaining_time, /*offset=*/nullptr);
+}
+
+}  // namespace
+
 AccessibilityConfirmationDialog::AccessibilityConfirmationDialog(
     const std::u16string& window_title_text,
     const std::u16string& dialog_text,
@@ -32,7 +49,10 @@ AccessibilityConfirmationDialog::AccessibilityConfirmationDialog(
     const std::u16string& cancel_text,
     base::OnceClosure on_accept_callback,
     base::OnceClosure on_cancel_callback,
-    base::OnceClosure on_close_callback) {
+    base::OnceClosure on_close_callback,
+    std::optional<int> timeout_seconds)
+    : timeout_seconds_(timeout_seconds),
+      timeout_message_with_placeholder_(dialog_text) {
   SetModalType(ui::mojom::ModalType::kSystem);
   SetTitle(window_title_text);
   SetButtonLabel(ui::mojom::DialogButton::kOk, confirm_text);
@@ -47,11 +67,16 @@ AccessibilityConfirmationDialog::AccessibilityConfirmationDialog(
   SetBorder(views::CreateEmptyBorder(
       views::LayoutProvider::Get()->GetDialogInsetsForContentType(
           views::DialogContentType::kText, views::DialogContentType::kText)));
-  std::unique_ptr<views::Label> label =
-      std::make_unique<views::Label>(dialog_text);
+
+  std::u16string dialog_string =
+      timeout_seconds.has_value()
+          ? GetMessageWithTimeout(timeout_message_with_placeholder_,
+                                  time_remaining_)
+          : dialog_text;
+
+  label = AddChildView(std::make_unique<views::Label>(dialog_string));
   label->SetMultiLine(true);
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  AddChildView(std::move(label));
 
   // Parent the dialog widget to the LockSystemModalContainer to ensure that it
   // will get displayed on respective lock/signin or OOBE screen.
@@ -68,12 +93,30 @@ AccessibilityConfirmationDialog::AccessibilityConfirmationDialog(
       this, nullptr,
       Shell::GetContainer(Shell::GetPrimaryRootWindow(), container_id));
   widget->Show();
+  // If a timeout is specified, start the timer.
+  if (timeout_seconds_.has_value()) {
+    time_remaining_ = timeout_seconds_.value();
+    timer_.Start(
+        FROM_HERE, base::Seconds(1),
+        base::BindRepeating(&AccessibilityConfirmationDialog::OnTimerTick,
+                            GetWeakPtr()));
+  }
 }
 
 AccessibilityConfirmationDialog::~AccessibilityConfirmationDialog() = default;
 
 bool AccessibilityConfirmationDialog::ShouldShowCloseButton() const {
   return false;
+}
+
+void AccessibilityConfirmationDialog::OnTimerTick() {
+  if (--time_remaining_ == 0) {
+    CancelDialog();
+    return;
+  }
+
+  label->SetText(GetMessageWithTimeout(timeout_message_with_placeholder_,
+                                       time_remaining_));
 }
 
 base::WeakPtr<AccessibilityConfirmationDialog>
