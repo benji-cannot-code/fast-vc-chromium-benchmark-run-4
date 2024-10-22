8c11aa5a4890eda1f8fc122adb2cbbdc24efc539
Aliona DANGLA <alionadangla@google.com>
2024-10-22 08:21:42 +0000

    [iOS] Remove occurrences of ChromeBrowserState and remove typedef
    Bug: 358053899
    Change-Id: I3332fcf3ac24bf7326b5192d722b65aab184cafb
    Fixed: 358355206
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5947124
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Auto-Submit: Aliona Dangla <alionadangla@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371891}

--- ios/chrome/browser/enterprise/model/idle/idle_service_factory.mm
+++ ios/chrome/browser/enterprise/model/idle/idle_service_factory.mm
@@ -12,11 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace enterprise_idle {
 
 // static
-IdleService* IdleServiceFactory::GetForBrowserState(ProfileIOS* profile) {
-  return GetForProfile(profile);
-}
-
-// static
 IdleService* IdleServiceFactory::GetForProfile(ProfileIOS* profile) {
   return static_cast<IdleService*>(
       GetInstance()->GetServiceForBrowserState(profile, true));
--- ios/chrome/browser/shared/model/profile/test/test_profile_ios_forward.h
+++ ios/chrome/browser/shared/model/profile/test/test_profile_ios_forward.h
@@ -15,6 +15,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // complete.
 
 class TestProfileIOS;
-using TestChromeBrowserState = TestProfileIOS;
 
 #endif  // IOS_CHROME_BROWSER_SHARED_MODEL_PROFILE_TEST_TEST_PROFILE_IOS_FORWARD_H_
