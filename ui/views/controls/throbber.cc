6a1f15eeb877f6c6b15b4238fde100b59b0b6921
Zachary Tan <tanzachary@chromium.org>
2024-10-22 05:16:37 +0000

    Reland "[FedCM] Replace progress bar with spinner buttons on desktop modal"
    This is a reland of commit 06bf7657a056251d5c02e463ce405df4ea8ea08b
    
    Fixed incorrect downcasting which led to failures in linux-ubsan-vptr
    
    Original change's description:
    > [FedCM] Replace progress bar with spinner buttons on desktop modal
    >
    > Demo
    > https://issues.chromium.org/370795251#attachment60130740
    >
    > Bug: 370795251
    > Change-Id: I0d57b9ea344257bac1af75caf1af4e003f2d3a09
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917273
    > Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    > Reviewed-by: Peter Boström <pbos@chromium.org>
    > Reviewed-by: Yi Gu <yigu@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1370599}
    
    Bug: 370795251
    Change-Id: I1dbbfbebaeae2f6bfdb194087548f0188fdd23b5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943800
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371853}

--- ui/views/controls/throbber.cc
+++ ui/views/controls/throbber.cc
@@ -78,7 +78,8 @@ gfx::Size Throbber::CalculatePreferredSize(
 }
 
 void Throbber::OnPaint(gfx::Canvas* canvas) {
-  SkColor color = GetColorProvider()->GetColor(ui::kColorThrobber);
+  SkColor color =
+      GetColorProvider()->GetColor(color_id_.value_or(ui::kColorThrobber));
 
   if (!IsRunning()) {
     if (checked_) {
