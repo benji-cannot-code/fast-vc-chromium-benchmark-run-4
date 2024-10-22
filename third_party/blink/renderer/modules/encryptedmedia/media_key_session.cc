1f6abe681682e518b15106befcf8d2b89c887fa0
Vikram Pasupathy <vpasupathy@chromium.org>
2024-10-22 20:22:24 +0000

    blink: Add UKM for CDM System code
    We want to add a UKM for tracking all CDM system codes to look at all
    problems with keys, and look at the ratio of system error codes to
    success keyed by individual URLs.
    
    testing: played encrypted content, visited chrome://ukm
    https://photos.app.goo.gl/NrCib8H4eumjwCKf8
    
    approval doc: https://docs.google.com/document/d/1brlZAAW3tHF70Sxl3KxqdczZBo8m01O9Zdn4TnootPg/edit?usp=sharing&resourcekey=0-VSQl28fgbXIV2VuUQf5K-A
    
    Bug: 372731553
    Change-Id: Ia6c7b388d5fc952e5faffab372e9e48ed05cdf19
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5935790
    Commit-Queue: Vikram Pasupathy <vpasupathy@chromium.org>
    Reviewed-by: John Rummell <jrummell@chromium.org>
    Reviewed-by: Sun Yueru <yrsun@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372281}

--- third_party/blink/renderer/modules/encryptedmedia/media_key_session.cc
+++ third_party/blink/renderer/modules/encryptedmedia/media_key_session.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "encrypted_media_utils.h"
 #include "media/base/content_decryption_module.h"
 #include "media/base/eme_constants.h"
+#include "services/metrics/public/cpp/ukm_builders.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/public/platform/web_content_decryption_module.h"
 #include "third_party/blink/public/platform/web_content_decryption_module_exception.h"
@@ -1094,6 +1095,9 @@ void MediaKeySession::OnSessionKeysChange(
   // 4.1 Empty statuses.
   key_statuses_map_->Clear();
 
+  auto* ukm_recorder = GetExecutionContext()->UkmRecorder();
+  const ukm::SourceId source_id = GetExecutionContext()->UkmSourceID();
+
   // 4.2 For each pair in input statuses.
   for (size_t i = 0; i < keys.size(); ++i) {
     // 4.2.1 Let pair be the pair.
@@ -1102,6 +1106,10 @@ void MediaKeySession::OnSessionKeysChange(
     //       value of pair's MediaKeyStatus value.
     key_statuses_map_->AddEntry(
         key.Id(), EncryptedMediaUtils::ConvertKeyStatusToString(key.Status()));
+
+    ukm::builders::Media_EME_CdmSystemCode(source_id)
+        .SetCdmSystemCode(key.SystemCode())
+        .Record(ukm_recorder);
   }
 
   // 5. Queue a task to fire a simple event named keystatuseschange
