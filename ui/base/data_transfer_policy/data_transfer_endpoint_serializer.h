31a6921342f8bb39b524d49bff619a1461056ea9
Maksim Sisov <msisov@igalia.com>
2024-10-22 13:25:31 +0000

    Remove ConvertDataTransferEndpointToJson
    This was used by ash/exo and lacros/wayland to convert data
    to json for kMimeTypeDataTransferEndpoint mime type. Given
    it's removed, there are no users of that.
    
    Bug: 373971535, 373972275, 374122148
    Change-Id: I850532d0e15e498b38b6cb8dbdf79452be957cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942731
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1371990}

--- ui/base/data_transfer_policy/data_transfer_endpoint_serializer.h
+++ ui/base/data_transfer_policy/data_transfer_endpoint_serializer.h
@@ -20,11 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-// Encodes DataTransferEndpoints into a JSON string in the format as described
-// above.
-COMPONENT_EXPORT(UI_BASE_DATA_TRANSFER_POLICY)
-std::string ConvertDataTransferEndpointToJson(const DataTransferEndpoint& dte);
-
 // Decodes JSON strings into DataTransferEndpoint objects.
 // If no type or url found, nullptr is returned.
 COMPONENT_EXPORT(UI_BASE_DATA_TRANSFER_POLICY)
