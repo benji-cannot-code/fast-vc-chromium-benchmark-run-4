d9672dc67ed01b34a3c3d436abb0f4286fc61895
Austin Sullivan <asully@chromium.org>
2024-10-22 22:00:55 +0000

    webnn: Support the gatherND operator in CoreML
    Negative and OOB indices are not handled
    
    Bug: 41481333, 366412395
    Cq-Include-Trybots: luci.chromium.try:mac15-blink-rel,mac15.arm64-blink-rel,mac14-blink-rel,mac14.arm64-blink-rel,win11-blink-rel
    Change-Id: I5d1587c1b3c5cb1a2ee3f2255989227c4284ce7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905204
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372359}

--- third_party/blink/web_tests/external/wpt/webnn/conformance_tests/gatherND.https.any.js
+++ third_party/blink/web_tests/external/wpt/webnn/conformance_tests/gatherND.https.any.js
@@ -185,7 +185,7 @@ const gatherNDTests = [
         },
         'gatherNDIndices': {
           'data': [-2, -2, -2],
-          'descriptor': {shape: [3], dataType: 'int64'},
+          'descriptor': {shape: [3], dataType: 'int32'},
           'constant': true
         }
       },
@@ -220,7 +220,7 @@ const gatherNDTests = [
         },
         'gatherNDIndices': {
           'data': [1, 1, 1],
-          'descriptor': {shape: [3], dataType: 'int64'},
+          'descriptor': {shape: [3], dataType: 'int32'},
           'constant': true
         }
       },
