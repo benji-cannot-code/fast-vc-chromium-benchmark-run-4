8b4431490fa98a7309ad1cefd4706699c168b8e3
Petr Cermak <petrcermak@chromium.org>
2024-10-22 18:15:53 +0000

    Convert FrameIntervalMatcherInputsTracedValue to FrameIntervalMatcher::Inputs::WriteIntoTrace
    This makes the code more readable, especially as we plan to add more
    tracing information in the near future. Note that this change is a no-op
    refactoring. All existing trace events should stay the same.
    
    Bug: 371227621
    Change-Id: I11269ed78e477362d104a4b1e178e197eb9a9506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953123
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Auto-Submit: Petr Cermak <petrcermak@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372183}

--- components/viz/service/display/frame_interval_matchers.h
+++ components/viz/service/display/frame_interval_matchers.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/viz/common/surfaces/surface_id.h"
 #include "components/viz/service/viz_service_export.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
+#include "third_party/perfetto/include/perfetto/tracing/traced_value_forward.h"
 
 namespace viz {
 
@@ -117,6 +118,9 @@ class VIZ_SERVICE_EXPORT FrameIntervalMatcher {
 
     Inputs(const Inputs& other);
     Inputs& operator=(const Inputs& other);
+
+    // Serializes this struct into a trace.
+    void WriteIntoTrace(perfetto::TracedValue trace_context) const;
 
     base::raw_ref<const Settings> settings;
     base::TimeTicks aggregated_frame_time;
