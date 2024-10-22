8dacfcffcdcb38a3144c648110d6e1e919f42425
Maksim Sisov <msisov@igalia.com>
2024-10-22 09:13:08 +0000

    Cleanup //ui/base/emoji from LaCros code
    Lacros is deprecated.
    1) Remove obsolete code,
    2) Rename IS_CHROMEOS_ASH to IS_CHROMEOS
    
    Bug: 373971535, 373972275, 374122148
    Change-Id: I17b1cb1bd64556390e683d91f5c6f59791d7f30a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5946743
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Peter Boström <pbos@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1371910}

--- ui/base/emoji/emoji_panel_helper.h
+++ ui/base/emoji/emoji_panel_helper.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/component_export.h"
 #include "base/functional/callback_forward.h"
-#include "build/chromeos_buildflags.h"
+#include "build/build_config.h"
 
 namespace ui {
 
@@ -21,7 +21,7 @@ COMPONENT_EXPORT(UI_BASE_EMOJI) bool IsEmojiPanelSupported();
 // Invokes the commands to show the Emoji Panel.
 COMPONENT_EXPORT(UI_BASE_EMOJI) void ShowEmojiPanel();
 
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
 // Invokes the commands to show the Emoji Panel in tablet mode (ChromeOS only).
 COMPONENT_EXPORT(UI_BASE_EMOJI) void ShowTabletModeEmojiPanel();
 
@@ -55,7 +55,7 @@ void SetShowEmojiKeyboardCallback(EmojiKeyboardCallback callback);
 // Sets a callback to show the emoji panel in tablet mode (ChromeOS only).
 COMPONENT_EXPORT(UI_BASE_EMOJI)
 void SetTabletModeShowEmojiKeyboardCallback(base::RepeatingClosure callback);
-#endif
+#endif  // BUILDFLAG(IS_CHROMEOS)
 
 }  // namespace ui
 
