8c7e5e60d1a286ffe5c0ab7059fd4643c24c65e3
Olivier Li Shing Tat-Dupuis <olivierli@chromium.org>
2024-10-22 18:46:33 +0000

    Add metric to track resource request deferrals
    Bug: 374930286
    Change-Id: Ia60122c1ca57ee1b6ee2693ae9df2ad8df80a7d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954523
    Commit-Queue: Olivier Li Shing Tat-Dupuis <olivierli@google.com>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>
    Commit-Queue: Olivier Li <olivierli@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372210}

--- third_party/blink/renderer/platform/loader/fetch/url_loader/resource_request_sender.cc
+++ third_party/blink/renderer/platform/loader/fetch/url_loader/resource_request_sender.cc
@@ -282,6 +282,7 @@ int ResourceRequestSender::SendAsync(
   }
 #endif
   if (code_cache_host) {
+    used_code_cache_fetcher_ = true;
     code_cache_fetcher_ = CodeCacheFetcher::TryCreateAndStart(
         *request, *code_cache_host,
         WTF::BindOnce(&ResourceRequestSender::DidReceiveCachedCode,
@@ -481,6 +482,7 @@ void ResourceRequestSender::OnReceivedResponse(
   }
 
   if (ShouldDeferTask()) {
+    latency_critical_operation_deferred_ = true;
     pending_tasks_.push_back(WTF::BindOnce(
         &ResourceRequestSender::OnReceivedResponse, weak_factory_.GetWeakPtr(),
         std::move(response_head), std::move(body), std::move(cached_metadata),
@@ -526,6 +528,7 @@ void ResourceRequestSender::OnReceivedRedirect(
     network::mojom::URLResponseHeadPtr response_head,
     base::TimeTicks redirect_ipc_arrival_time) {
   if (ShouldDeferTask()) {
+    latency_critical_operation_deferred_ = true;
     pending_tasks_.emplace_back(WTF::BindOnce(
         &ResourceRequestSender::OnReceivedRedirect, weak_factory_.GetWeakPtr(),
         redirect_info, std::move(response_head), redirect_ipc_arrival_time));
@@ -599,6 +602,7 @@ void ResourceRequestSender::OnRequestComplete(
     const network::URLLoaderCompletionStatus& status,
     base::TimeTicks complete_ipc_arrival_time) {
   if (ShouldDeferTask()) {
+    latency_critical_operation_deferred_ = true;
     pending_tasks_.emplace_back(WTF::BindOnce(
         &ResourceRequestSender::OnRequestComplete, weak_factory_.GetWeakPtr(),
         status, complete_ipc_arrival_time));
@@ -651,6 +655,12 @@ void ResourceRequestSender::OnRequestComplete(
           "Blink.ResourceRequest.CompletionDelay2",
           complete_ipc_arrival_time - renderer_status.completion_time);
     }
+  }
+
+  if (used_code_cache_fetcher_) {
+    base::UmaHistogramBoolean(
+        "Blink.ResourceRequest.DeferedRequestWaitingOnCodeCache",
+        latency_critical_operation_deferred_);
   }
   // The request ID will be removed from our pending list in the destructor.
   // Normally, dispatching this message causes the reference-counted request to
