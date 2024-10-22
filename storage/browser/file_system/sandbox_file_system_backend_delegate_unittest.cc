23c54a3c5e978e8034ab677d5753fab1209145b9
Joel Hockey <joelhockey@chromium.org>
2024-10-22 20:15:31 +0000

    Fix FilePath::ReferencesParent()
    On systems other than Windows, filenames such as "...", "....", " .." or
    ".. " are valid and do not reference the parent directory.
    
    BUG=b:333973312, b:333990417, b:333973299
    TEST=base_unittests --gtest_filter=FilePathTest*
    TEST=storage_unittests --gtest_filter=SandboxFileSystemBackendDelegateTest.*
    TEST=zlib_unittests
    
    Change-Id: Ib5ee017d693f5df018a40b6fb0223663ff489543
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5565285
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: François Degros <fdegros@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372277}

--- storage/browser/file_system/sandbox_file_system_backend_delegate_unittest.cc
+++ storage/browser/file_system/sandbox_file_system_backend_delegate_unittest.cc
@@ -103,6 +103,7 @@ TEST_F(SandboxFileSystemBackendDelegateTest, IsAccessValid) {
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(".")));
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL("..")));
 
+#if BUILDFLAG(IS_WIN)
   // This is also disallowed due to Windows XP parent path handling.
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL("...")));
 
@@ -110,6 +111,11 @@ TEST_F(SandboxFileSystemBackendDelegateTest, IsAccessValid) {
   // on Windows.
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(" ..")));
   EXPECT_FALSE(IsAccessValid(CreateFileSystemURL(".. ")));
+#else
+  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL("...")));
+  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(" ..")));
+  EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(".. ")));
+#endif
 
   // Similar but safe cases.
   EXPECT_TRUE(IsAccessValid(CreateFileSystemURL(" .")));
--- third_party/zlib/google/zip_unittest.cc
+++ third_party/zlib/google/zip_unittest.cc
@@ -750,6 +750,8 @@ TEST_F(ZipTest, UnzipMixedPaths) {
       "Space→",  //
 #else
       " ",                        //
+      "...",                      // Disappears on Windows
+      "....",                     // Disappears on Windows
       "AUX",                      // Disappears on Windows
       "COM1",                     // Disappears on Windows
       "COM2",                     // Disappears on Windows
