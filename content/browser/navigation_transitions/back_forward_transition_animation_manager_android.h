0b7d5bb9047c7d4365b518b1fdb8e5e3ba7ba3cb
William Liu <liuwilliam@chromium.org>
2024-10-23 16:08:52 +0000

    [DNT] Only play cancel animation when the BeforeUnload dialog is shown
    Before this CL, we run the cancel animation if the navigation is
    waiting for the BeforeUnload message. This is very conservative as a
    page can register a BeforeUnload listener but doesn't perform anything.
    
    This CL changes the behavior, in that we only play the cancel animation
    to bring the active page back if a BeforeUnload dialog is shown.
    
    Cq-Include-Trybots: luci.chromium.try:android-12-x64-rel,android-12l-x64-dbg,android-13-x64-rel,android-14-x64-rel,android-15-x64-rel,android-pie-x86-rel,android-oreo-x86-rel
    
    Fixed: 364694410
    Change-Id: Idbe86b5ae5879965fd0559a60ebb47e5b82d3de7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5919513
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372747}

--- content/browser/navigation_transitions/back_forward_transition_animation_manager_android.h
+++ content/browser/navigation_transitions/back_forward_transition_animation_manager_android.h
@@ -130,6 +130,8 @@ class CONTENT_EXPORT BackForwardTransitionAnimationManagerAndroid
 
   void OnPhysicalBackingSizeChanged();
 
+  void OnBeforeUnloadDialogShown(int64_t navigation_id);
+
   WebContentsViewAndroid* web_contents_view_android() const {
     return web_contents_view_android_;
   }
