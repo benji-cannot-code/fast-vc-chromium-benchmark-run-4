a821ba946efb6eef86f965952ac69167ed785738
Dmitry Vykochko <vykochko@google.com>
2024-10-23 10:16:06 +0000

    A11, announce when prediction improvements suggestions are loaded.
    Bug: 372447295
    Change-Id: Ic133864ff7cbcbe6ffa00b7b58ac6911e4d3a17a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5947483
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372593}

--- chrome/browser/ui/views/autofill/popup/popup_view_views.h
+++ chrome/browser/ui/views/autofill/popup/popup_view_views.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
@@ -247,9 +248,6 @@ class PopupViewViews : public PopupBaseView,
   // level up. Returns whether this was successful.
   bool SelectParentPopupContentCell();
 
-  // Announces a string without assertively alerting a user.
-  void AnnouncePolitely(const std::u16string& text);
-
   // The popup can be used for informing the user without providing suggestions
   // to select, e.g. when the suggestions are loading. It has only one
   // suggestion with a special type in this case. This method makes sure
@@ -265,6 +263,11 @@ class PopupViewViews : public PopupBaseView,
   std::optional<base::WeakPtr<ExpandablePopupParentView>> parent_;
 
   std::unique_ptr<PasswordFaviconLoaderImpl> password_favicon_loader_;
+
+  // The implementation of the a11y announcer. When testing the announcements,
+  // it's replaced with a mock function.
+  base::RepeatingCallback<void(const std::u16string& message, bool polite)>
+      a11y_announcer_;
 
   // The index of the row with a selected cell.
   std::optional<size_t> row_with_selected_cell_;
--- chrome/browser/ui/views/autofill/popup/popup_view_views_test_api.h
+++ chrome/browser/ui/views/autofill/popup/popup_view_views_test_api.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/functional/callback_forward.h"
 #include "base/memory/raw_ref.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_view_views.h"
@@ -25,7 +26,14 @@ namespace autofill {
 
 class PopupViewViewsTestApi {
  public:
+  using A11yAnnouncer =
+      base::RepeatingCallback<void(const std::u16string&, bool)>;
+
   explicit PopupViewViewsTestApi(PopupViewViews* view) : view_(*view) {}
+
+  void SetA11yAnnouncer(A11yAnnouncer announcer) {
+    view_->a11y_announcer_ = announcer;
+  }
 
   bool CanShowDropdownInBounds(const gfx::Rect& bounds) const&& {
     return view_->CanShowDropdownInBounds(bounds);
