e9f898c3da48844b373391ab74156966da3b89b7
Yue Zhang <yuezhanggg@chromium.org>
2024-10-23 17:15:38 +0000

    Add price field to commerce internal page
    Change-Id: Ia3397960997f87111c0baef7e480956f6bcdd756
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954022
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372801}

--- components/commerce/core/internals/resources/view_product.ts
+++ components/commerce/core/internals/resources/view_product.ts
@@ -25,6 +25,7 @@ function handleSubmit() {
         getRequiredElement('productClusterId').innerText =
             BigInt.asUintN(64, info.clusterId).toString();
         getRequiredElement('productImageUrl').innerText = info.imageUrl.url;
+        getRequiredElement('productPrice').innerText = info.currentPrice;
         getRequiredElement('productCategories').innerText =
             info.categoryLabels!.join(', \n');
       });
