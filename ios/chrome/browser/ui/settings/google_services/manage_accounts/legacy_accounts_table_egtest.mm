175b47d631a034d246a41d956be32f31c86f3ced
ginnnnnnny <ginnyhuang@chromium.org>
2024-10-23 04:37:40 +0000

    [iOS][gardener] Run testSignInReloadOnRemoveAccount with tab group sync
    For some reason the test fails unless some tab group features are
    enabled. Forcing them on for now and letting the test owners find
    and fix the root cause.
    
    Bug: 375023885
    Change-Id: Ic8cb763660c22d0f04619409ed6748d0175e70d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954133
    Reviewed-by: Chris Lu <thegreenfrog@chromium.org>
    Owners-Override: Ginny Huang <ginnyhuang@chromium.org>
    Commit-Queue: Ginny Huang <ginnyhuang@chromium.org>
    Auto-Submit: Ginny Huang <ginnyhuang@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372503}

--- ios/chrome/browser/ui/settings/google_services/manage_accounts/legacy_accounts_table_egtest.mm
+++ ios/chrome/browser/ui/settings/google_services/manage_accounts/legacy_accounts_table_egtest.mm
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/test/earl_grey/chrome_matchers.h"
 #import "ios/chrome/test/earl_grey/chrome_matchers_app_interface.h"
 #import "ios/chrome/test/earl_grey/web_http_server_chrome_test_case.h"
+#import "ios/testing/earl_grey/app_launch_manager.h"
 #import "ios/testing/earl_grey/earl_grey_test.h"
 #import "net/test/embedded_test_server/embedded_test_server.h"
 #import "ui/base/l10n/l10n_util.h"
@@ -159,6 +160,14 @@ constexpr base::TimeDelta kSyncOperationTimeout = base::Seconds(10);
 // Tests that the Account Settings screen is correctly reloaded when one of
 // the non-primary account is removed.
 - (void)testSignInReloadOnRemoveAccount {
+  // TODO(crbug.com/375023885): Re-enable this test when tab group sync is
+  // disabled.
+  AppLaunchConfiguration config = [self appConfigurationForTestCase];
+  config.relaunch_policy = ForceRelaunchByCleanShutdown;
+  config.features_enabled.push_back(kTabGroupSync);
+  config.features_enabled.push_back(kTabGroupIndicator);
+  [[AppLaunchManager sharedManager] ensureAppLaunchedWithConfiguration:config];
+
   FakeSystemIdentity* fakeIdentity1 = [FakeSystemIdentity fakeIdentity1];
   FakeSystemIdentity* fakeIdentity2 = [FakeSystemIdentity fakeIdentity2];
   [SigninEarlGrey addFakeIdentity:fakeIdentity2];
