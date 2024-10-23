e15380e689b9fcdde5f3c4808d93e38ec414d590
Joe Mason <joenotcharles@google.com>
2024-10-23 16:24:14 +0000

    Fix and re-enable TransferSharedLastForegroundTime test
    On some builders the death test was failing because an assert had an
    unexpected message. Removed the death test: there's no need to test for
    an invalid handle since it's blocked at the Mojo layer.
    
    Bug: 375065072
    Change-Id: I09d0882e9a207b5440d3018edf5b2db8ab579980
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953918
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Auto-Submit: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372758}

--- content/renderer/render_thread_impl_browsertest.cc
+++ content/renderer/render_thread_impl_browsertest.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task/sequenced_task_runner.h"
 #include "base/task/single_thread_task_runner.h"
 #include "base/task/thread_pool/thread_pool_instance.h"
-#include "base/test/gtest_util.h"
 #include "base/test/test_switches.h"
 #include "base/time/time.h"
 #include "cc/base/switches.h"
@@ -305,8 +304,6 @@ class RenderThreadImplBrowserTest : public testing::Test,
   std::unique_ptr<base::RunLoop> run_loop_;
 };
 
-using RenderThreadImplBrowserDeathTest = RenderThreadImplBrowserTest;
-
 #if BUILDFLAG(CONTENT_ENABLE_LEGACY_IPC)
 // Disabled under LeakSanitizer due to memory leaks.
 TEST_F(RenderThreadImplBrowserTest,
@@ -442,26 +439,13 @@ TEST_F(RenderThreadImplBrowserTest, RendererStateTransitionForegrounded) {
   testing::Mock::AllowLeak(main_thread_scheduler_);
 }
 
-// TODO(crbug.com/375065072): Re-enable once not flaky.
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || \
-    BUILDFLAG(IS_FUCHSIA) || BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_TransferSharedLastForegroundTime \
-  DISABLED_TransferSharedLastForegroundTime
-#else
-#define MAYBE_TransferSharedLastForegroundTime TransferSharedLastForegroundTime
-#endif
-TEST_F(RenderThreadImplBrowserDeathTest,
-       MAYBE_TransferSharedLastForegroundTime) {
+TEST_F(RenderThreadImplBrowserTest, TransferSharedLastForegroundTime) {
   auto time_memory = base::AtomicSharedMemory<base::TimeTicks>::Create();
   ASSERT_TRUE(time_memory.has_value());
 
   // No shared memory region mapped by default.
   EXPECT_EQ(base::internal::GetSharedLastForegroundTimeForMetricsForTesting(),
             nullptr);
-
-  // Invalid handles should not be accepted.
-  EXPECT_CHECK_DEATH(thread_->TransferSharedLastForegroundTime(
-      base::ReadOnlySharedMemoryRegion()));
 
   // SharedLastForegroundTimeForMetrics should never be overwritten after it's
   // set, in case a thread is accessing the memory as it's unmapped.
