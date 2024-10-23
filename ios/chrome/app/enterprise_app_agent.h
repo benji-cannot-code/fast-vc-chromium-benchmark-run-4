4c3589e3652ead2d6557078b109e6a5ef5d4cf60
Sylvain Defresne <sdefresne@chromium.org>
2024-10-23 09:37:40 +0000

    [ios] Simplify EnterpriseAppAgent
    Convert it to a SceneObservingAppAgent and introduce helper methods
    to make the code easier to read.
    
    The code is app-specific, so update the description of the class to
    make it clear that is wait for the device enterprise policy, not any
    profile related policies.
    
    Fixed: 364234793
    Low-Coverage-Reason: Improving untested code.
    Change-Id: I3e73255c5d3e75ba0f4fc295ec935ca74270c6e3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953329
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372576}

--- ios/chrome/app/enterprise_app_agent.h
+++ ios/chrome/app/enterprise_app_agent.h
@@ -6,12 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_APP_ENTERPRISE_APP_AGENT_H_
 #define IOS_CHROME_APP_ENTERPRISE_APP_AGENT_H_
 
-#import "ios/chrome/app/application_delegate/app_state_agent.h"
-#import "ios/chrome/app/application_delegate/app_state_observer.h"
+#import "ios/chrome/app/application_delegate/observing_app_state_agent.h"
 
 // App state agent that blocks the app initialization while the
-// enterprise-related data is loaded.
-@interface EnterpriseAppAgent : NSObject <AppStateAgent, AppStateObserver>
+// device enterprise-related data is loaded.
+@interface EnterpriseAppAgent : SceneObservingAppAgent
 
 @end
 
