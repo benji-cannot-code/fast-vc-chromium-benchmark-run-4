8531db1e0c3fa6c72fd7630e8b03b0413e4ec1aa
Li-Yu Yu <aaronyu@google.com>
2024-10-23 19:40:30 +0000

    Remove #audio-suppress-set-rtc-audio-active
    Bug: 330210946
    Test: autoninja -C out/ut unit_tests && out/ut/unit_tests --gtest_filter=AboutFlagsTest.\*
    Change-Id: I2fb697ce80952f10462edf074f982eed76d30465
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955812
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372872}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3488,9 +3488,6 @@ extern const char kAudioStyleTransferDescription[];
 extern const char kAudioSelectionImprovementName[];
 extern const char kAudioSelectionImprovementDescription[];
 
-extern const char kAudioSuppressSetRTCAudioActiveName[];
-extern const char kAudioSuppressSetRTCAudioActiveDescription[];
-
 extern const char kAutoFramingOverrideName[];
 extern const char kAutoFramingOverrideDescription[];
 
