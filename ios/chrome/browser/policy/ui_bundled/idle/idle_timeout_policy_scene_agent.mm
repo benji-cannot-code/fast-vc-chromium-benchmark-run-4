bf1b08f3ecbebf311e1bc0ac2b51c10287393286
Sylvain Defresne <sdefresne@chromium.org>
2024-10-23 14:53:12 +0000

    [ios] Support multi-profile in IdleTimeoutPolicySceneAgent
    Bug: none
    Change-Id: I7e558c874b58b365431db9b345a4b5910096fe85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953788
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1372694}

--- ios/chrome/browser/policy/ui_bundled/idle/idle_timeout_policy_scene_agent.mm
+++ ios/chrome/browser/policy/ui_bundled/idle/idle_timeout_policy_scene_agent.mm
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "components/enterprise/idle/metrics.h"
 #import "components/policy/core/common/policy_pref_names.h"
 #import "components/prefs/pref_service.h"
-#import "ios/chrome/app/application_delegate/app_state.h"
-#import "ios/chrome/app/application_delegate/app_state_observer.h"
+#import "ios/chrome/app/profile/profile_init_stage.h"
 #import "ios/chrome/app/profile/profile_state.h"
 #import "ios/chrome/browser/enterprise/model/idle/idle_service_observer_bridge.h"
 #import "ios/chrome/browser/policy/ui_bundled/idle/constants.h"
@@ -37,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ui/base/l10n/l10n_util.h"
 
 @interface IdleTimeoutPolicySceneAgent () <
-    AppStateObserver,
     IdleServiceObserving,
     IdleTimeoutConfirmationCoordinatorDelegate>
 @end
@@ -96,13 +94,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     _idleService = idleService;
   }
   return self;
-}
-
-#pragma mark - ObservingSceneAgent
-
-- (void)setSceneState:(SceneState*)sceneState {
-  [super setSceneState:sceneState];
-  [self.sceneState.profileState.appState addObserver:self];
 }
 
 #pragma mark - SceneStateObserver
@@ -198,7 +189,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)tearDownObservers {
   _idleServiceObserverBridge.reset();
-  [self.sceneState.profileState.appState removeObserver:self];
 }
 
 - (PrefService*)prefService {
@@ -277,7 +267,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Returns whether the scene and app states allow for the idle timeout
 // confirmation dialog to be shown if it is needed.
 - (BOOL)isUIAvailableToShowDialog {
-  if (self.sceneState.profileState.appState.initStage < AppInitStage::kFinal) {
+  if (self.sceneState.profileState.initStage < ProfileInitStage::kFinal) {
     // Return NO when the app isn't yet fully initialized.
     return NO;
   }
