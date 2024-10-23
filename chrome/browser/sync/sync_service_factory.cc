03a2c453cb9b6f6ad9e89bea750ba58baa7699e6
Mikel Astiz <mastiz@chromium.org>
2024-10-23 11:14:51 +0000

    [sync] Exercise factories from SyncServiceFactory only
    This means the exercising of factories takes place in the same file
    where factory dependencies are declared. ChromeSyncClient now
    doesn't exercise any factory and instead receives all the required
    services injected in the constructor. This patch is very similar to
    the iOS counterpart in https://crrev.com/c/5932688.
    
    Bug: 335688372
    Change-Id: I067c8b73aa050379907288d7d6e7c56b04574c92
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5932387
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1372608}

--- chrome/browser/sync/sync_service_factory.cc
+++ chrome/browser/sync/sync_service_factory.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/chrome_sync_controller_builder.h"
 #include "chrome/browser/sync/data_type_store_service_factory.h"
 #include "chrome/browser/sync/device_info_sync_service_factory.h"
+#include "chrome/browser/sync/glue/extensions_activity_monitor.h"
 #include "chrome/browser/sync/local_or_syncable_bookmark_sync_service_factory.h"
 #include "chrome/browser/sync/send_tab_to_self_sync_service_factory.h"
 #include "chrome/browser/sync/session_sync_service_factory.h"
@@ -345,18 +346,32 @@ syncer::DataTypeController::TypeVector CreateControllers(
   return controllers;
 }
 
+std::unique_ptr<syncer::SyncClient> BuildSyncClient(Profile* profile) {
+  CHECK(profile);
+
+  return std::make_unique<browser_sync::ChromeSyncClient>(
+      profile->GetBaseName(), profile->GetPrefs(),
+      IdentityManagerFactory::GetForProfile(profile),
+      TrustedVaultServiceFactory::GetForProfile(profile),
+      SyncInvalidationsServiceFactory::GetForProfile(profile),
+      DeviceInfoSyncServiceFactory::GetForProfile(profile),
+      DataTypeStoreServiceFactory::GetForProfile(profile),
+      SupervisedUserSettingsServiceFactory::GetForKey(profile->GetProfileKey()),
+      std::make_unique<browser_sync::ExtensionsActivityMonitor>(profile));
+}
+
 std::unique_ptr<KeyedService> BuildSyncService(
     content::BrowserContext* context) {
   syncer::SyncServiceImpl::InitParams init_params;
 
   Profile* profile = Profile::FromBrowserContext(context);
+  CHECK(profile);
 
   // Incognito, guest, or system profiles aren't relevant for Sync, and
   // no SyncService should be created for those types of profiles.
   CHECK(profiles::IsRegularUserProfile(profile));
 
-  init_params.sync_client =
-      std::make_unique<browser_sync::ChromeSyncClient>(profile);
+  init_params.sync_client = BuildSyncClient(profile);
   init_params.url_loader_factory = profile->GetDefaultStoragePartition()
                                        ->GetURLLoaderFactoryForBrowserProcess();
   init_params.network_connection_tracker =
