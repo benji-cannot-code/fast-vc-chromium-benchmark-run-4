36113b35cd5e359d118c4c9538fe64fe385f4b5a
Teresa Mao <temao@chromium.org>
2024-10-23 23:33:05 +0000

    SafeBrowsingUI: Decouple SafeBrowsingLocalStateDelegate from SafeBrowsingUI creation.
    - Introduce ChromeSafeBrowsingUI and AWSafeBrowsingUI, both extending
    SafeBrowsingUI and encapsulating platform specific SafeBrowsingLocalStateDelegate.
    - Change SafeBrowsingUI’s constructor to protected to prevent direct
    instantiation. Users must use either ChromeSafeBrowsingUI or
    AWSafeBrowsingUI to access SafeBrowsingUI functionalities.
    - This is a precursor CL to migrating SafeBrowsingUI from
    ChromeWebUIControllerFactory to ChromeWebUIConfig.
    
    Bug: 40222519
    Change-Id: Iac88b9600d621ca6d58d15dda46e23511271a252
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954036
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Daniel Rubery <drubery@chromium.org>
    Commit-Queue: Teresa Mao <temao@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373028}

--- components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h
+++ components/safe_browsing/content/browser/web_ui/safe_browsing_ui.h
@@ -383,7 +383,7 @@ class SafeBrowsingUIHandler : public content::WebUIMessageHandler {
 
 // The WebUI for chrome://safe-browsing
 class SafeBrowsingUI : public content::WebUIController {
- public:
+ protected:
   SafeBrowsingUI(content::WebUI* web_ui,
                  std::unique_ptr<SafeBrowsingLocalStateDelegate> delegate);
 
