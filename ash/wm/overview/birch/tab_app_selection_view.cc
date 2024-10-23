3aa82b080ab1cdafa17d02122095edce39d3d26d
conniekxu <conniekxu@chromium.org>
2024-10-23 20:11:11 +0000

    coral: Add hover state for thumb button in the user feedback view
    Screen record to show the hover state on thumb buttons:
    https://screencast.googleplex.com/cast/NDY5MjE1NzUyODAxNDg0OHw0ZDUyMjczYi01Mg
    
    Bug=b:374117101
    
    Change-Id: Ia7e980464fe2330d97317a5570f9b3ae88e5f648
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953505
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372897}

--- ash/wm/overview/birch/tab_app_selection_view.cc
+++ ash/wm/overview/birch/tab_app_selection_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/close_button.h"
 #include "ash/style/icon_button.h"
+#include "ash/style/style_util.h"
 #include "ash/style/typography.h"
 #include "base/task/cancelable_task_tracker.h"
 #include "chromeos/ash/services/coral/public/mojom/coral_service.mojom.h"
@@ -327,19 +328,29 @@ class UserFeedbackView : public views::BoxLayoutView {
         AddChildView(std::make_unique<views::BoxLayoutView>());
     thumb_buttons_container->SetOrientation(
         views::BoxLayout::Orientation::kHorizontal);
-    thumb_buttons_container->AddChildView(std::make_unique<IconButton>(
-        base::BindOnce(&UserFeedbackView::OnThumbUpButtonPressed,
-                       base::Unretained(this)),
-        IconButton::Type::kMediumFloating, &kThumbUpIcon,
-        l10n_util::GetStringUTF16(IDS_ASH_BIRCH_CORAL_THUMB_UP_ACCESSIBLE_NAME),
-        /*is_togglable=*/false, /*has_border=*/false));
-    thumb_buttons_container->AddChildView(std::make_unique<IconButton>(
-        base::BindOnce(&UserFeedbackView::OnThumbDownButtonPressed,
-                       base::Unretained(this)),
-        IconButton::Type::kMediumFloating, &kThumbDownIcon,
-        l10n_util::GetStringUTF16(
-            IDS_ASH_BIRCH_CORAL_THUMB_DOWN_ACCESSIBLE_NAME),
-        /*is_togglable=*/false, /*has_border=*/false));
+    auto* thumb_up_button =
+        thumb_buttons_container->AddChildView(std::make_unique<IconButton>(
+            base::BindOnce(&UserFeedbackView::OnThumbUpButtonPressed,
+                           base::Unretained(this)),
+            IconButton::Type::kMediumFloating, &kThumbUpIcon,
+            l10n_util::GetStringUTF16(
+                IDS_ASH_BIRCH_CORAL_THUMB_UP_ACCESSIBLE_NAME),
+            /*is_togglable=*/false, /*has_border=*/false));
+    StyleUtil::SetUpInkDropForButton(thumb_up_button, gfx::Insets(),
+                                     /*highlight_on_hover=*/true,
+                                     /*highlight_on_focus=*/false);
+
+    auto* thumb_down_button =
+        thumb_buttons_container->AddChildView(std::make_unique<IconButton>(
+            base::BindOnce(&UserFeedbackView::OnThumbDownButtonPressed,
+                           base::Unretained(this)),
+            IconButton::Type::kMediumFloating, &kThumbDownIcon,
+            l10n_util::GetStringUTF16(
+                IDS_ASH_BIRCH_CORAL_THUMB_DOWN_ACCESSIBLE_NAME),
+            /*is_togglable=*/false, /*has_border=*/false));
+    StyleUtil::SetUpInkDropForButton(thumb_down_button, gfx::Insets(),
+                                     /*highlight_on_hover=*/true,
+                                     /*highlight_on_focus=*/false);
   }
 
   UserFeedbackView(const UserFeedbackView&) = delete;
