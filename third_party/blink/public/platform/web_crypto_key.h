5f8c04042e58063cad8a1714e8d8ed4fe45afec2
Bruno Pitrus <brunopitrus@hotmail.com>
2024-10-23 22:55:01 +0000

    Correct ODR violation due to compiling same header with different defines
    The WebCryptoAlgorithm instance is particularly insidious as adding or removing a default member function can lead to ABI mismatches.
    I can't think rn of a counterexample for the enum ones, but GCC still warns about them.
    
    See https://devblogs.microsoft.com/oldnewthing/20240802-00/?p=110091 for an explanation of this antipattern.
    
    Change-Id: Ie79008a4e01b99126bd3fa8659061b9c58e951c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5760036
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Auto-Submit: Bruno Pitrus <brunopitrus@hotmail.com>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373004}

--- third_party/blink/public/platform/web_crypto_key.h
+++ third_party/blink/public/platform/web_crypto_key.h
@@ -52,9 +52,7 @@ enum WebCryptoKeyUsage {
   kWebCryptoKeyUsageWrapKey = 1 << 5,
   kWebCryptoKeyUsageUnwrapKey = 1 << 6,
   kWebCryptoKeyUsageDeriveBits = 1 << 7,
-#if INSIDE_BLINK
   kEndOfWebCryptoKeyUsage,
-#endif
 };
 
 // A bitfield of WebCryptoKeyUsage
