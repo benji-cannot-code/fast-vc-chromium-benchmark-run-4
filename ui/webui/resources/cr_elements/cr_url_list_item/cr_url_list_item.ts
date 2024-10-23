09496cea3ec9fa2cfb48faaeddd30ba3c30dabc5
dpapad <dpapad@chromium.org>
2024-10-23 00:50:33 +0000

    WebUI: Fix unresolved side effect TypeScript imports in Desktop code.
    This is in preparation of turning on the noUncheckedSideEffectImports
    flag which was added in TypeScript v5.6 (see [1]) and will help catch
    more programming errors at build time.
    
    In order to be able to resolve these previously unresolved imports
     - Using relative paths when cr_elements/ refer to other cr_elements
       files.
     - Checked in cr_elements/icons.html.ts instead of auto-generating it,
       so that a relative URL to cr_iconset.js can be used.
     - Added mappings for chrome://resources/d3/d3.min.js as needed.
    
    [1] https://www.typescriptlang.org/tsconfig/#noUncheckedSideEffectImports
    
    Bug: 374334459
    Change-Id: I8b746cda71a5245f4ebeb1a33f8d2322cbfcce28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5535769
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372434}

--- ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
+++ ui/webui/resources/cr_elements/cr_url_list_item/cr_url_list_item.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import '//resources/cr_elements/cr_auto_img/cr_auto_img.js';
+import '../cr_auto_img/cr_auto_img.js';
 
 import {assert} from '//resources/js/assert.js';
 import {FocusOutlineManager} from '//resources/js/focus_outline_manager.js';
