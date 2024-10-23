c7943fc17903af3d84696db65a315fcb180df3f8
Jan Keitel <jkeitel@google.com>
2024-10-23 15:24:17 +0000

    Use safe buffer constructs in unsent_log_store.cc
    Bug: 40285824
    Change-Id: Iea1014bf1e5a6b08759092db7c5b45e50a26d371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952470
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372713}

--- components/metrics/unsent_log_store.cc
+++ components/metrics/unsent_log_store.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/metrics/unsent_log_store.h"
 
 #include <cmath>
@@ -313,8 +308,7 @@ void UnsentLogStore::TrimAndPersistUnsentLogs(bool overwrite_in_memory_store) {
           writer.unsent_logs_count() >= log_store_limits_.min_log_count) {
         // The rest of the logs (including the current one) are trimmed.
         if (overwrite_in_memory_store) {
-          NotifyLogsEvent(base::span<std::unique_ptr<LogInfo>>(
-                              list_.begin(), list_.begin() + i + 1),
+          NotifyLogsEvent(base::span(list_).first(static_cast<size_t>(i + 1)),
                           MetricsLogsEventManager::LogEvent::kLogTrimmed);
         }
         break;
