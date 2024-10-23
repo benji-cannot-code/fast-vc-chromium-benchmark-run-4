72047a58edc995de6c073b9d2c93681152dc30ea
Tomasz Wiszkowski <ender@google.com>
2024-10-23 15:14:46 +0000

    Align Omnibox size and font height across CTA and SA.
    ChromeTabbedActivity (specifically: ToolbarPhone) computes the size of
    the LocationBar visuals at runtime to offer smooth transition between
    the focused and unfocused state, which is particularly important when
    transitioning between NTP fakebox and active Omnibox. SearchActivity
    relies on the same layout, however the transformations are not
    applied, producing a smaller font and general misalignment between
    the two activities.
    
    This change ports LocationBar background and sizing from ToolbarPhone
    to SearchActivity.
    
    Note that the change does not align positioning yet - I will try to
    address that in a separate change.
    
    Related code:
    https://crsrc.org/c/chrome/browser/ui/android/toolbar/java/src/org/chromium/chrome/browser/toolbar/top/ToolbarPhone.java;l=907-925
    
    Screens:
    - before: https://screenshot.googleplex.com/9CrBqk4eZ8QHgst.png
    - after: https://screenshot.googleplex.com/5JmvFmLik7jVG8G.png
    
    Bug: b:370570081
    Change-Id: I648d4553ea6b7cb2bb8b6d88e973c32195623e25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955571
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372704}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivityLocationBarLayout.java
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.searchwidget;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -24,6 +26,7 @@ import org.chromium.chrome.browser.omnibox.UrlBarCoordinator;
 import org.chromium.chrome.browser.omnibox.UrlBarCoordinator.SelectionState;
 import org.chromium.chrome.browser.omnibox.UrlBarData;
 import org.chromium.chrome.browser.omnibox.status.StatusCoordinator;
+import org.chromium.chrome.browser.omnibox.styles.OmniboxResourceProvider;
 import org.chromium.chrome.browser.omnibox.suggestions.AutocompleteCoordinator;
 import org.chromium.chrome.browser.omnibox.voice.VoiceRecognitionHandler;
 import org.chromium.chrome.browser.toolbar.top.ToolbarPhone;
@@ -69,7 +72,14 @@ public class SearchActivityLocationBarLayout extends LocationBarLayout {
                 getResources()
                         .getDimensionPixelSize(R.dimen.omnibox_suggestion_bg_round_corner_radius));
 
-        setBackground(backgroundDrawable);
+        // Replicate LocationBarBackground bounds from ToolbarPhone.
+        int verticalInsets =
+                getResources().getDimensionPixelSize(R.dimen.location_bar_vertical_margin)
+                        - OmniboxResourceProvider.getToolbarOnFocusHeightIncrease(getContext()) / 2;
+        LayerDrawable layerDrawable = new LayerDrawable(new Drawable[] {backgroundDrawable});
+        layerDrawable.setLayerInset(0, 0, verticalInsets, 0, verticalInsets); // Adjust padding
+
+        setBackground(layerDrawable);
 
         // Expand status view's left and right space, and expand the vertical padding of the
         // location bar to match the expanded interface on the regular omnibox.
