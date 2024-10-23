1d79e7d5342c1eb810867e54c457a4a895e6082b
Ahmed Nasr <anasr@google.com>
2024-10-23 17:31:01 +0000

    [BabelOrca] Add babel orca producer implementation.
    Bug: b:373692250
    Change-Id: If83f77947091dc493cb871e61a5be8626451d4a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955801
    Reviewed-by: Avynn Donaghe <avynn@google.com>
    Commit-Queue: Ahmed Nasr <anasr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372817}

--- chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_request_data_provider.h
+++ chromeos/ash/components/boca/babelorca/fakes/fake_tachyon_request_data_provider.h
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <string>
 
-#include "base/functional/callback_forward.h"
+#include "base/functional/callback.h"
 #include "chromeos/ash/components/boca/babelorca/tachyon_request_data_provider.h"
 
 namespace ash::babelorca {
 
 class FakeTachyonRequestDataProvider : public TachyonRequestDataProvider {
  public:
+  FakeTachyonRequestDataProvider();
   FakeTachyonRequestDataProvider(std::optional<std::string> session_id,
                                  std::optional<std::string> tachyon_token,
                                  std::optional<std::string> group_id,
@@ -28,15 +29,22 @@ class FakeTachyonRequestDataProvider : public TachyonRequestDataProvider {
 
   ~FakeTachyonRequestDataProvider() override;
 
+  // TachyonRequestDataProvider:
   void SigninToTachyonAndRespond(
       base::OnceCallback<void(bool)> on_response_cb) override;
-
   std::optional<std::string> session_id() const override;
   std::optional<std::string> tachyon_token() const override;
   std::optional<std::string> group_id() const override;
   std::optional<std::string> sender_email() const override;
 
+  void set_tachyon_token(std::optional<std::string> tachyon_token);
+  void set_group_id(std::optional<std::string> group_id);
+
+  base::OnceCallback<void(bool)> TakeSigninCb();
+
  private:
+  base::OnceCallback<void(bool)> signin_cb_;
+
   std::optional<std::string> session_id_;
   std::optional<std::string> tachyon_token_;
   std::optional<std::string> group_id_;
