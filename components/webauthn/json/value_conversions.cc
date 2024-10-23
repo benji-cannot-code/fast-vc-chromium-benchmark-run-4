4c8b106e322fd27f7eb0cbc498cd0212a484d833
Jan Keitel <jkeitel@google.com>
2024-10-23 12:23:21 +0000

    Enable unsafe buffer warnings in webauthn json utils
    No functionality change - this CL just uses some (span) helpers.
    
    Bug: 40285824
    Change-Id: I0049a942745da9e2e0433760922dcfe2e39dd9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952735
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372636}

--- components/webauthn/json/value_conversions.cc
+++ components/webauthn/json/value_conversions.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/webauthn/json/value_conversions.h"
 
 #include <iterator>
@@ -15,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/base64url.h"
+#include "base/containers/span.h"
+#include "base/containers/to_vector.h"
 #include "base/feature_list.h"
 #include "base/ranges/ranges.h"
 #include "base/values.h"
@@ -39,10 +36,8 @@ std::string Base64UrlEncode(base::span<const uint8_t> input) {
   // Byte strings, which appear in the WebAuthn IDL as ArrayBuffer or
   // ByteSource, are base64url-encoded without trailing '=' padding.
   std::string output;
-  base::Base64UrlEncode(
-      std::string_view(reinterpret_cast<const char*>(input.data()),
-                       input.size()),
-      base::Base64UrlEncodePolicy::OMIT_PADDING, &output);
+  base::Base64UrlEncode(input, base::Base64UrlEncodePolicy::OMIT_PADDING,
+                        &output);
   return output;
 }
 
@@ -95,8 +90,7 @@ std::tuple<bool, std::optional<std::string>> Base64UrlDecodeOptionalStringKey(
 }
 
 std::vector<uint8_t> ToByteVector(const std::string& in) {
-  const uint8_t* in_ptr = reinterpret_cast<const uint8_t*>(in.data());
-  return std::vector<uint8_t>(in_ptr, in_ptr + in.size());
+  return base::ToVector(base::as_byte_span(in));
 }
 
 base::Value ToValue(const device::PublicKeyCredentialRpEntity& relying_party) {
