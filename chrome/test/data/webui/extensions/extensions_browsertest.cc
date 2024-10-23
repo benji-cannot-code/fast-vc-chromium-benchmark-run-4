91ae4a06d49b99e93b2ec078af57949da24ab697
rbpotter <rbpotter@chromium.org>
2024-10-23 15:43:26 +0000

    Extensions: Add an asyncMap() directive to replace dom-repeat
    Add a new AsyncMapDirective class in chrome://extensions, along with
    corresponding tests. The new directive internally uses map() to display
    a list of items, and supports |initialCount| and |filter| parameters.
    These parameters are used by some dom-repeats in the extensions list
    currently; these dom-repeats will be migrated to use the new directive
    in a followup.
    
    Also adding a test, and adding the relevant code to the lit bundle.
    lit.rollup.js minified size increases 1.9kB with this change:
    Before: 17557 bytes
    After: 19446 bytes
    
    Bug: 40943652
    Change-Id: I0644fe90da1b584e43ce64eb2bbdebda922fbc2d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941684
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1372729}

--- chrome/test/data/webui/extensions/extensions_browsertest.cc
+++ chrome/test/data/webui/extensions/extensions_browsertest.cc
@@ -39,6 +39,10 @@ IN_PROC_BROWSER_TEST_F(CrExtensionsTest, ActivityLogStreamItem) {
   RunTest("extensions/activity_log_stream_item_test.js", "mocha.run()");
 }
 
+IN_PROC_BROWSER_TEST_F(CrExtensionsTest, AsyncMapDirective) {
+  RunTest("extensions/async_map_directive_test.js", "mocha.run()");
+}
+
 IN_PROC_BROWSER_TEST_F(CrExtensionsTest, ToggleRow) {
   RunTest("extensions/toggle_row_test.js", "mocha.run()");
 }
