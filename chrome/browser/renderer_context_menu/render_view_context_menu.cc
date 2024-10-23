49a81c9175e883dfd33dba38de1497b8174c1b59
Dmitry Vykochko <vykochko@google.com>
2024-10-23 07:59:33 +0000

    Fix adding Generate password entry if kPasswordManualFallbackAvailable.
    The check was lost in this CL crrev.com/c/5776926. This one just
    returns it back.
    
    Bug: 356817239, 316143236
    Change-Id: Icca3323ef581339b6d2a0e8b1a7ebe4094c7dafb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953334
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1372547}

--- chrome/browser/renderer_context_menu/render_view_context_menu.cc
+++ chrome/browser/renderer_context_menu/render_view_context_menu.cc
@@ -2662,6 +2662,11 @@ void RenderViewContextMenu::AppendPasswordItems() {
     return;
   }
 
+  if (base::FeatureList::IsEnabled(
+          password_manager::features::kPasswordManualFallbackAvailable)) {
+    return;
+  }
+
   bool add_separator = false;
 
   // Don't show the item for guest or incognito profiles and also when the
