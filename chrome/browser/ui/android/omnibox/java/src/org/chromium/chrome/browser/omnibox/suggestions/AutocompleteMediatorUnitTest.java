be91d5aee58b8531045f629d5748b247866bcb3d
Tomasz Wiszkowski <ender@google.com>
2024-10-24 18:53:03 +0000

    Preserve the URL, class, and suggestions for a recently visited page.
    This change introduces (conditional) support for additional variants
    of the Jump-Start Omnibox feature, preserving the URL, page
    classification, and suggestions related to the most recently visited
    page before user tabs out of Chrome.
    
    Bug: b:370570081
    Change-Id: Ia193ea4cf8f58745e4748cede7a198e5d9a42f3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962716
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373488}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/AutocompleteMediatorUnitTest.java
@@ -1383,8 +1383,7 @@ public class AutocompleteMediatorUnitTest {
         Set<Integer> eligibleClasses =
                 Set.of(
                         PageClassification.ANDROID_SEARCH_WIDGET_VALUE,
-                        PageClassification.ANDROID_SHORTCUTS_WIDGET_VALUE,
-                        PageClassification.INSTANT_NTP_WITH_OMNIBOX_AS_STARTING_FOCUS_VALUE);
+                        PageClassification.ANDROID_SHORTCUTS_WIDGET_VALUE);
 
         doReturn(mAutocompleteResult)
                 .when(ShadowCachedSuggestionsManager.mock)
@@ -1448,8 +1447,7 @@ public class AutocompleteMediatorUnitTest {
         Set<Integer> eligibleClasses =
                 Set.of(
                         PageClassification.ANDROID_SEARCH_WIDGET_VALUE,
-                        PageClassification.ANDROID_SHORTCUTS_WIDGET_VALUE,
-                        PageClassification.INSTANT_NTP_WITH_OMNIBOX_AS_STARTING_FOCUS_VALUE);
+                        PageClassification.ANDROID_SHORTCUTS_WIDGET_VALUE);
 
         mMediator.onOmniboxSessionStateChange(true);
         doReturn(mAutocompleteResult)
