b3a9f74b3ab1f1fa7729a42c8016ccd422faac87
Marcello Salomao <msalomao@google.com>
2024-10-24 03:37:34 +0000

    help-app: Add message to open app mall to ClientApiDelegate
    Bug: 370383700
    Change-Id: I8b433e78b57f247b1073a1c5b18a7cf2288f51f6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938771
    Reviewed-by: Callistus Tan <callistus@google.com>
    Commit-Queue: Marcello Salomao <msalomao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373108}

--- ash/webui/help_app_ui/resources/receiver.ts
+++ ash/webui/help_app_ui/resources/receiver.ts
@@ -80,6 +80,9 @@ const DELEGATE: ClientApiDelegate = {
   setHasVisitedHowToPage() {
     parentMessagePipe.sendMessage(Message.SET_HAS_VISITED_HOW_TO_PAGE);
   },
+  openAppMallPath(path: string) {
+    parentMessagePipe.sendMessage(Message.OPEN_APP_MALL_PATH, {path});
+  },
 };
 
 window.customLaunchData = {
