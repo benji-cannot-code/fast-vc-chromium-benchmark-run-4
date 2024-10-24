159d35d2604116b01951730b3b4abdd013a6a894
dpapad <dpapad@chromium.org>
2024-10-24 23:38:20 +0000

    WebUI: Turn on noUncheckedSideEffectImports TS flag in Polymer's TS config.
    This flag was added in TypeScript v5.6 (see [1]) and helps catching
    more programming errors at build time, by throwing an error when
    side-effect import URLs don't get resolved.
    
    This flag is already enabled in the default Lit TS config since
    crrev.com/c/5513663. The intention is to eventually enable this flag in
    CrOS's Polymer config as well, and eventually in
    tools/typescript/tsconfig_base.json.
    
    As part of enabling the flag, fixing a newly introduced violation in
    chrome/browser/resources/lens/shared/ where a dependency to
    cr_elements/ is missing. All other violations have been addressed in
    precursor CLs.
    
    [1] https://www.typescriptlang.org/tsconfig/#noUncheckedSideEffectImports
    
    Bug: 374334459
    Change-Id: I6672d08cb8f05ab9359c29446bf2941c6e55e88a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958696
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373654}

--- chrome/browser/resources/lens/shared/searchbox_ghost_loader.ts
+++ chrome/browser/resources/lens/shared/searchbox_ghost_loader.ts
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 import '//resources/cr_elements/cr_spinner_style.css.js';
-import '/lens/shared/searchbox_shared_style.css.js';
+import './searchbox_shared_style.css.js';
 
 import {PolymerElement} from '//resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
