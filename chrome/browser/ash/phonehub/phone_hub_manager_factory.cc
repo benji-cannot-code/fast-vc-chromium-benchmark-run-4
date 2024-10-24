c457623b1494c09b0d665be4f86d927f9964e913
Maksim Moskvitin <mmoskvitin@google.com>
2024-10-24 10:30:25 +0000

    [LacrosRemoval] Delete kChromeOSSyncedSessionSharing flag
    Bug: 372221465, 365742770
    Change-Id: I428a5f0512b91dc9a4070108487021a0b78c7416
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953655
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Maksim Moskvitin <mmoskvitin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373227}

--- chrome/browser/ash/phonehub/phone_hub_manager_factory.cc
+++ chrome/browser/ash/phonehub/phone_hub_manager_factory.cc
@@ -18,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/profiles/profile_helper.h"
 #include "chrome/browser/ash/secure_channel/nearby_connector_factory.h"
 #include "chrome/browser/ash/secure_channel/secure_channel_client_provider.h"
-#include "chrome/browser/ash/sync/sync_mojo_service_ash.h"
-#include "chrome/browser/ash/sync/sync_mojo_service_factory_ash.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/browser/favicon/history_ui_favicon_request_handler_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -104,14 +102,6 @@ PhoneHubManagerFactory::PhoneHubManagerFactory()
   DependsOn(SessionSyncServiceFactory::GetInstance());
   DependsOn(HistoryUiFaviconRequestHandlerFactory::GetInstance());
   DependsOn(SyncServiceFactory::GetInstance());
-
-  // We typically also check crosapi::browser_util::IsAshWebBrowserEnabled() in
-  // relation to this feature flag but this relies on UserManager which is not
-  // initialized at this point. Since this is just a service dependency simply
-  // checking the flag itself is fine.
-  if (base::FeatureList::IsEnabled(syncer::kChromeOSSyncedSessionSharing)) {
-    DependsOn(SyncMojoServiceFactoryAsh::GetInstance());
-  }
 }
 
 PhoneHubManagerFactory::~PhoneHubManagerFactory() = default;
