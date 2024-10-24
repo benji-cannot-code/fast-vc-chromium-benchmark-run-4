7b1029bef8f953a24c75deaa03c9751fa86a4af3
Claire Chambers <clchambers@microsoft.com>
2024-10-24 20:47:47 +0000

    [CompositeClipPathAnimation] Downgrade status NOTREACHED to Dump
    The recent removal of snapshots for composited clip path animations
    caused a bug that occurs when a clip path animation is running on a
    pseudo of an element with a transform animation. In this case, there
    are unnecessary paint invalidations caused by
    RecalcCompositedStatusForKeyframeChange which reset the paint status to
    kNeedsRepaint, but these changes do not cause paint property updates
    as there is no actual style change, thus the pre paint tree walk
    returns early (see linked bug for details).
    
    Importantly, this case does not create a functional issue, only a perf
    one (paint properties are still correct, they've just been incorrectly
    marked for update), so to unblock experimination with the feature, this
    change changes a NOTREACHED check into a dump without crashing.
    
    Bug: 374656290
    Change-Id: Ie579e33c49e54a2d3f841157b5d03c46af41bbfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961180
    Reviewed-by: Olga Gerchikov <gerchiko@microsoft.com>
    Reviewed-by: Kevin Ellis <kevers@chromium.org>
    Commit-Queue: Claire Chambers <clchambers@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1373560}

--- third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
+++ third_party/blink/renderer/core/paint/pre_paint_tree_walk.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/renderer/core/page/chrome_client.h"
 #include "third_party/blink/renderer/core/page/link_highlight.h"
 #include "third_party/blink/renderer/core/page/page.h"
+#include "third_party/blink/renderer/core/paint/clip_path_clipper.h"
 #include "third_party/blink/renderer/core/paint/object_paint_invalidator.h"
 #include "third_party/blink/renderer/core/paint/paint_layer.h"
 #include "third_party/blink/renderer/core/paint/paint_property_tree_printer.h"
@@ -1380,6 +1381,10 @@ void PrePaintTreeWalk::Walk(const LayoutObject& object,
   // Early out from the tree walk if possible.
   if (!needs_tree_builder_context_update && !ObjectRequiresPrePaint(object) &&
       !ContextRequiresChildPrePaint(parent_context)) {
+    if (!ClipPathClipper::ClipPathStatusResolved(object)) {
+      // crbug.com/374656290: Convert to CHECK or DCHECK when fix is confirmed.
+      base::debug::DumpWithoutCrashing();
+    }
     return;
   }
 
