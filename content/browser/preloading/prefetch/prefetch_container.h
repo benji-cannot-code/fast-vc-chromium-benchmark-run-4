41b5801acbe12fdf46ea414bcf3c176ea5e6af04
Taiyo Mizuhashi <taiyo@chromium.org>
2024-10-24 08:44:20 +0000

    Extend PrefetchContainer::referring_origin_ for general browser-initiated prefetch
    This CL is based on wbjacksonjr@'s proposal.
    
    Currently, all prefetch triggers have a concept of referring
    document/origin. Specifically, potential value of PrefetchContainer::referring_origin_ is
    - If CCT (prefetch from App A that has verified associated origin):
      referring_origin == A’s origin
    - If CCT (otherwise): referring_origin == opaque origin [1]
      so that we can treat this request as a cross-site prefetch. Also, if
      nullopt is passed to PrefetchContainer's public ctor for browser-
      initiated prefetch, it is automatically converted to opaque origin [2]
    - If Speculation rules prefetch in document A: referring_origin == A’s
      origin
    
    In order to introduce WebView prefetch, which has no concepts that
    corresponds to referring document/origin and thus has no restriction
    related to it, this CL changes the type of
    PrefetchContainer::referring_origin_ to std::optional so that general
    browser-initiated prefetch can use this.
    Practically this case can be handled like same-site prefetch, in the
    same manner as normal browser-initiated prerender behaves like
    same-site prerender.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:chrome/android/java/src/org/chromium/chrome/browser/WarmupManager.java;l=663;drc=8f3124aee72eb5cc75bec53fd9e7999592767e9a
    [2] https://source.chromium.org/chromium/chromium/src/+/main:content/browser/preloading/prefetch/prefetch_container.cc;l=426;drc=4d3dad034f8aefd3a1c6043eecbd901568546936
    
    Bug: 40946257, 363946909
    Co-authored-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Change-Id: Ic0d1bf1e3350c719020a6a9b8232a50acc170ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942243
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373194}

--- content/browser/preloading/prefetch/prefetch_container.h
+++ content/browser/preloading/prefetch/prefetch_container.h
@@ -112,8 +112,7 @@ class CONTENT_EXPORT PrefetchContainer {
 
   // Ctor used for browser-initiated prefetch.
   // We can pass the referring origin of prefetches via `referring_origin` if
-  // necessary. When `std::nullopt` is passed, the referring origin will be
-  // opaque.
+  // necessary.
   PrefetchContainer(
       WebContents& referring_web_contents,
       const GURL& url,
@@ -127,8 +126,7 @@ class CONTENT_EXPORT PrefetchContainer {
 
   // Ctor used for browser-initiated prefetch that doesn't depend on web
   // contents. We can pass the referring origin of prefetches via
-  // `referrer_origin` if necessary. When `std::nullopt` is passed, the
-  // referring origin will be opaque.
+  // `referrer_origin` if necessary.
   PrefetchContainer(
       BrowserContext* browser_context,
       const GURL& url,
@@ -269,7 +267,9 @@ class CONTENT_EXPORT PrefetchContainer {
   bool IsRendererInitiated() const;
 
   // The origin and that initiates the prefetch request.
-  const url::Origin& GetReferringOrigin() const { return referring_origin_; }
+  const std::optional<url::Origin> GetReferringOrigin() const {
+    return referring_origin_;
+  }
 
   // Whether or not an isolated network context is required to the next
   // prefetch.
@@ -384,6 +384,10 @@ class CONTENT_EXPORT PrefetchContainer {
   // response, and not serve any prefetched resources.
   void SetIsDecoy(bool is_decoy) { is_decoy_ = is_decoy; }
   bool IsDecoy() const { return is_decoy_; }
+
+  // Whether the prefetch request is cross-site/cross-origin for given origin.
+  bool IsCrossSiteRequest(const url::Origin& origin) const;
+  bool IsCrossOriginRequest(const url::Origin& origin) const;
 
   // Whether this prefetch is potentially contaminated by cross-site state.
   // If so, it may need special handling for privacy.
@@ -770,7 +774,7 @@ class CONTENT_EXPORT PrefetchContainer {
  private:
   PrefetchContainer(
       const GlobalRenderFrameHostId& referring_render_frame_host_id,
-      const url::Origin& referring_origin,
+      const std::optional<url::Origin>& referring_origin,
       const std::optional<size_t>& referring_url_hash,
       const PrefetchContainer::Key& key,
       const PrefetchType& prefetch_type,
@@ -838,8 +842,8 @@ class CONTENT_EXPORT PrefetchContainer {
   // The origin and URL that initiates the prefetch request.
   // For renderer-initiated prefetch, this is calculated by referring
   // RenderFrameHost's LastCommittedOrigin. For browser-initiated prefetch, this
-  // is sometimes explicitly passed via ctor, otherwise opaque origin.
-  const url::Origin referring_origin_;
+  // is sometimes explicitly passed via ctor.
+  const std::optional<url::Origin> referring_origin_;
   // Used by metrics for equality checks, only works for renderer-initiated
   // triggers.
   const std::optional<size_t> referring_url_hash_;
--- content/browser/preloading/prefetch/prefetch_container_unittest.cc
+++ content/browser/preloading/prefetch/prefetch_container_unittest.cc
@@ -313,7 +313,7 @@ TEST_P(PrefetchContainerTest, CreatePrefetchContainer_Embedder) {
   PrefetchContainer prefetch_container(
       *web_contents(), GURL("https://test.com"),
       PrefetchType(PreloadingTriggerType::kEmbedder,
-                   /*use_prefetch_proxy=*/true),
+                   /*use_prefetch_proxy=*/false),
       blink::mojom::Referrer(), /*referring_origin=*/std::nullopt,
       /*no_vary_search_expected=*/std::nullopt, /*attempt=*/nullptr);
 
@@ -322,8 +322,8 @@ TEST_P(PrefetchContainerTest, CreatePrefetchContainer_Embedder) {
   EXPECT_EQ(prefetch_container.GetURL(), GURL("https://test.com"));
   EXPECT_EQ(prefetch_container.GetPrefetchType(),
             PrefetchType(PreloadingTriggerType::kEmbedder,
-                         /*use_prefetch_proxy=*/true));
-  EXPECT_TRUE(
+                         /*use_prefetch_proxy=*/false));
+  EXPECT_FALSE(
       prefetch_container.IsIsolatedNetworkContextRequiredForCurrentPrefetch());
 
   EXPECT_EQ(prefetch_container.key(),
@@ -1065,8 +1065,8 @@ TEST_P(PrefetchContainerTest, IsIsolatedNetworkRequired_Embedder) {
   auto prefetch_container_default = CreateEmbedderPrefetchContainer(
       GURL("https://test.com/prefetch"), std::nullopt);
   prefetch_container_default->MakeResourceRequest({});
-  EXPECT_TRUE(prefetch_container_default
-                  ->IsIsolatedNetworkContextRequiredForCurrentPrefetch());
+  EXPECT_FALSE(prefetch_container_default
+                   ->IsIsolatedNetworkContextRequiredForCurrentPrefetch());
 
   auto prefetch_container_same_origin = CreateEmbedderPrefetchContainer(
       GURL("https://test.com/prefetch"),
