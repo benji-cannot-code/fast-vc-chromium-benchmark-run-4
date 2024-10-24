338bc1de0ff7487b94ff6c8ff0fb6f7ec35cff42
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-10-24 06:45:50 +0000

    Import wpt@0312b2b479ca32023a69d57c51677d248155718e
    https://github.com/web-platform-tests/wpt/compare/9c82b9643...0312b2b47
    
    Using wpt-import in Chromium 6b9afc1b2d67309d8904cb0912c9c98d065f6a59.
    With Chromium commits locally applied on WPT:
      8bd2bf04bb "[WebAudio] Re-enable test suspended-context-messageport."
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    emilio@chromium.org:
      external/wpt/css/css-display
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I15fa7168401a12d193776a24c1867b9872efa881
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958208
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1373158}

--- third_party/blink/web_tests/external/wpt/WebCryptoAPI/derive_bits_keys/derived_bits_length_testcases.js
+++ third_party/blink/web_tests/external/wpt/WebCryptoAPI/derive_bits_keys/derived_bits_length_testcases.js
@@ -2,6 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var testCases = {
     "HKDF": [
         {length: 256, expected: algorithms["HKDF"].derivation},
+        {length: 384, expected: algorithms["HKDF"].derivation384},
+        {length: 230, expected: undefined}, // should throw an exception, not multiple of 8
         {length: 0, expected: undefined}, // explicitly disallowed, so should throw
         {length: null, expected: undefined }, // should throw an exception
         {length: undefined, expected: undefined }, // should throw an exception
@@ -9,6 +11,8 @@ var testCases = {
     ],
     "PBKDF2": [
         {length: 256, expected: algorithms["PBKDF2"].derivation},
+        {length: 384, expected: algorithms["PBKDF2"].derivation384},
+        {length: 230, expected: undefined}, // should throw an exception, not multiple of 8
         {length: 0, expected: undefined}, // explicitly disallowed, so should throw
         {length: null, expected: undefined }, // should throw an exception
         {length: undefined, expected: undefined }, // should throw an exception
@@ -16,6 +20,8 @@ var testCases = {
     ],
     "ECDH": [
         {length: 256, expected: algorithms["ECDH"].derivation},
+        {length: 384, expected: undefined}, // should throw an exception, bigger than the output size
+        {length: 230, expected: algorithms["ECDH"].derivation230},
         {length: 0, expected: emptyArray},
         {length: null, expected: algorithms["ECDH"].derivation},
         {length: undefined, expected: algorithms["ECDH"].derivation},
@@ -23,6 +29,8 @@ var testCases = {
     ],
     "X25519": [
         {length: 256, expected: algorithms["X25519"].derivation},
+        {length: 384, expected: undefined}, // should throw an exception, bigger than the output size
+        {length: 230, expected: algorithms["X25519"].derivation230},
         {length: 0, expected: emptyArray},
         {length: null, expected: algorithms["X25519"].derivation},
         {length: undefined, expected: algorithms["X25519"].derivation},
