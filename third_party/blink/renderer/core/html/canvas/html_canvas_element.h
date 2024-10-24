f2347a5d047a092e3ac1db221621a86cf729ca6f
Colin Blundell <blundell@chromium.org>
2024-10-24 13:16:28 +0000

    [Blink] Fold GetOrCreateResourceProvider() into HTMLCanvasElement
    This CL folds Canvas2DLayerBridge::GetOrCreateResourceProvider() into
    HTMLCanvasElement, which is the only caller of this method. We adjust
    the method body as necessary for the change (all functional no-ops).
    
    Note that in the future we have a goal to move CanvasResourceProvider
    creation/ownership to the contexts, but that is for a later phase of
    the overall canvas rearchitecture effort. Consolidating the current
    distributed logic will facilitate that later phase.
    
    Bug: 40280152
    Change-Id: Ibece6f9bf550c6be349565e9de4574545d1dde56
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5946166
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373278}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.h
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.h
@@ -331,6 +331,8 @@ class CORE_EXPORT HTMLCanvasElement final
  private:
   void Dispose();
 
+  CanvasResourceProvider* GetOrCreateCanvasResourceProviderFor2DContext();
+
   void ColorSchemeMayHaveChanged();
 
   void RecordIdentifiabilityMetric(IdentifiableSurface surface,
