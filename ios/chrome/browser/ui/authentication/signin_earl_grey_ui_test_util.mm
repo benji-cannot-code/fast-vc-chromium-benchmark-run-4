7de8a84569e95fbe034a92dfdb5ebc4d7c5432e2
Gauthier Ambard <gambard@chromium.org>
2024-10-24 10:57:17 +0000

    [iOS] Update sign in EG utils for visibility
    This CL makes sure that the EarlGrey sign in utils are ensuring the
    views are visible before tapping on it.
    
    Fixed: 375023885
    Change-Id: I09618cc2a8a92f56ad8b41005937c58cf252d630
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961239
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373234}

--- ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.mm
+++ ios/chrome/browser/ui/authentication/signin_earl_grey_ui_test_util.mm
@@ -255,8 +255,10 @@ id<GREYMatcher> SignOutSnackbarLabelMatcher() {
 
 + (void)openRemoveAccountConfirmationDialogWithFakeIdentity:
     (FakeSystemIdentity*)fakeIdentity {
-  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabel(
-                                          fakeIdentity.userEmail)]
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(ButtonWithAccessibilityLabel(
+                                              fakeIdentity.userEmail),
+                                          grey_sufficientlyVisible(), nil)]
       performAction:grey_tap()];
   [[EarlGrey
       selectElementWithMatcher:ButtonWithAccessibilityLabel(
@@ -268,9 +270,12 @@ id<GREYMatcher> SignOutSnackbarLabelMatcher() {
 + (void)tapRemoveAccountFromDeviceWithFakeIdentity:
     (FakeSystemIdentity*)fakeIdentity {
   [self openRemoveAccountConfirmationDialogWithFakeIdentity:fakeIdentity];
-  [[EarlGrey selectElementWithMatcher:ButtonWithAccessibilityLabel(
-                                          l10n_util::GetNSString(
-                                              IDS_IOS_REMOVE_ACCOUNT_LABEL))]
+  [[EarlGrey
+      selectElementWithMatcher:grey_allOf(
+                                   ButtonWithAccessibilityLabel(
+                                       l10n_util::GetNSString(
+                                           IDS_IOS_REMOVE_ACCOUNT_LABEL)),
+                                   grey_sufficientlyVisible(), nil)]
       performAction:grey_tap()];
   // Wait until the account is removed.
   [ChromeEarlGreyUI waitForAppToIdle];
