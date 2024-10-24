aa7bf707e2f1a7aa3b850a6550470bd7a5d145e8
Adam Langley <agl@chromium.org>
2024-10-24 14:17:17 +0000

    webauthn: pass unhashed PRF inputs to iCloud Keychain.
    Despite being a platform API, iCloud Keychain expects unhashed WebAuthn
    PRF inputs. Also, the `prf_inputs_hashed` field was used for hybrid
    authenticator support and we've deleted that code in Chromium in favour
    of the implementation in Play Services.
    
    Change-Id: I12cf3f111ac29dab9b2b4e90e3d4b6fbdc48067c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955569
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Ken Buchanan <kenrb@chromium.org>
    Auto-Submit: Adam Langley <agl@chromium.org>
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373302}

--- components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2Api.java
+++ components/webauthn/android/java/src/org/chromium/components/webauthn/Fido2Api.java
@@ -437,7 +437,7 @@ public final class Fido2Api {
                 // Two bytestrings for a single PRF input: the null credential ID and then the
                 // hashed salts, concatenated.
                 parcel.writeInt(2);
-                writePrfInput(options.prfInput, /* prfInputsHashed= */ false, parcel);
+                writePrfInput(options.prfInput, parcel);
             } else {
                 // No PRF inputs.
                 parcel.writeInt(0);
@@ -587,7 +587,7 @@ public final class Fido2Api {
             final int d = writeHeader(1, parcel);
             parcel.writeInt(2 * options.extensions.prfInputs.length);
             for (PrfValues input : options.extensions.prfInputs) {
-                writePrfInput(input, options.extensions.prfInputsHashed, parcel);
+                writePrfInput(input, parcel);
             }
             writeLength(d, parcel);
             writeLength(c, parcel);
@@ -607,17 +607,9 @@ public final class Fido2Api {
         writeLength(a, parcel);
     }
 
-    private static void writePrfInput(PrfValues input, boolean prfInputsHashed, Parcel parcel) {
+    private static void writePrfInput(PrfValues input, Parcel parcel) {
         parcel.writeByteArray(input.id);
-        if (prfInputsHashed) {
-            if (input.second == null) {
-                parcel.writeByteArray(input.first);
-            } else {
-                parcel.writeByteArray(concat(input.first, input.second));
-            }
-        } else {
-            parcel.writeByteArray(hashPrfInputs(input));
-        }
+        parcel.writeByteArray(hashPrfInputs(input));
     }
 
     /**
