fe6a88139b983f0d49ce70a230348de2e6bc833a
Dan Polanco <polardz@google.com>
2024-10-24 19:53:13 +0000

    [TabletToolbar] Hide Download Button
    Sets visibility to GONE if the feature flag is enabled.
    
    Bug: 374802010
    Change-Id: I8cb5c201e7c9bc5af2d56376986a55535160f382
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954545
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373522}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/LocationBarMediator.java
@@ -39,6 +39,7 @@ import org.chromium.base.supplier.OneshotSupplier;
 import org.chromium.base.task.PostTask;
 import org.chromium.base.task.TaskTraits;
 import org.chromium.chrome.browser.device.DeviceClassManager;
+import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.lens.LensController;
 import org.chromium.chrome.browser.lens.LensEntryPoint;
@@ -1213,7 +1214,9 @@ class LocationBarMediator
 
     private boolean shouldShowSaveOfflineButton() {
         assert mIsTablet;
-        if (!mNativeInitialized || mLocationBarDataProvider == null) return false;
+        if (hideSaveOfflineButton() || !mNativeInitialized || mLocationBarDataProvider == null) {
+            return false;
+        }
         Tab tab = mLocationBarDataProvider.getTab();
         if (tab == null) return false;
         // The save offline button should not be shown on native pages. Currently, trying to
@@ -1224,6 +1227,10 @@ class LocationBarMediator
     private boolean isSaveOfflineButtonEnabled() {
         if (mLocationBarDataProvider == null) return false;
         return mSaveOfflineButtonState.isEnabled(mLocationBarDataProvider.getTab());
+    }
+
+    private boolean hideSaveOfflineButton() {
+        return ChromeFeatureList.sHideTabletToolbarDownloadButton.isEnabled();
     }
 
     private boolean shouldShowPageActionButtons() {
