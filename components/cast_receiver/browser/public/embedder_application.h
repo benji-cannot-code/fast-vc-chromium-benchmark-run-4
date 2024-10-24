1f562e62b01fa38bdc4c6b700be46a8993e04047
Vigen Issahhanjan <vigeni@google.com>
2024-10-24 20:29:43 +0000

    Sync Cast Core gRPC APis to the latest revision.
    gRPC APIs submitted in
    https://chromium-review.googlesource.com/c/cast_core/public/+/5933643.
    
    Bug: none
    Change-Id: Ic95f67c1ae845e391466f1ce8252259d637c662a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5955550
    Reviewed-by: Antonio Rivera <antoniori@google.com>
    Commit-Queue: Vigen Issahhanjan <vigeni@google.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373547}

--- components/cast_receiver/browser/public/embedder_application.h
+++ components/cast_receiver/browser/public/embedder_application.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 class WebContents;
-class WebUIControllerFactory;
 }  // namespace content
 
 namespace cast_receiver {
@@ -78,13 +77,6 @@ class EmbedderApplication {
   // TODO(crbug.com/1359568): Change default implementation to be based on
   // Chromium state.
   virtual StreamingConfigManager* GetStreamingConfigManager();
-
-  // Creates a new platform-specific WebUIControllerFactory, or nullptr if
-  // this feature is not to be supported. Returns nullptr by default.
-  // |hosts| is the set of hosts for which the custom WebUIController associated
-  // with the returned factory should be used.
-  virtual std::unique_ptr<content::WebUIControllerFactory>
-  CreateWebUIControllerFactory(std::vector<std::string> hosts);
 
   // Loads |url| in the associated WebContents.
   virtual void NavigateToPage(const GURL& url);
