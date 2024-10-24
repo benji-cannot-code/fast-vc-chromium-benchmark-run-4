7f5afa43db0e7a5b7da1ec0346b8fcb77eda2b76
Jan Keitel <jkeitel@google.com>
2024-10-24 05:40:03 +0000

    Use safe buffer constructs in ChunkedByteBuffer
    This CL also removes a method overload that is only used in tests.
    
    Bug: 40285824
    Change-Id: Ib5c2a3f73f5df000f096f61943f0bd576b1fa33e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954269
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Evan Liu <evliu@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373134}

--- components/speech/chunked_byte_buffer.cc
+++ components/speech/chunked_byte_buffer.cc
@@ -3,26 +3,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/speech/chunked_byte_buffer.h"
 
 #include <algorithm>
+#include <cstdint>
 #include <memory>
 #include <string_view>
 #include <utility>
 
 #include "base/check_op.h"
 #include "base/containers/span.h"
+#include "base/containers/span_reader.h"
 #include "base/lazy_instance.h"
 #include "base/numerics/byte_conversions.h"
 
 namespace {
 
-static const size_t kHeaderLength = sizeof(uint32_t);
+constexpr size_t kHeaderLength = sizeof(uint32_t);
 
 static_assert(sizeof(size_t) >= kHeaderLength,
               "chunked byte buffer not supported on this architecture");
@@ -38,11 +35,10 @@ ChunkedByteBuffer::~ChunkedByteBuffer() {
   Clear();
 }
 
-void ChunkedByteBuffer::Append(const uint8_t* start, size_t length) {
-  size_t remaining_bytes = length;
-  const uint8_t* next_data = start;
+void ChunkedByteBuffer::Append(std::string_view data) {
+  base::SpanReader<const uint8_t> reader(base::as_byte_span(data));
 
-  while (remaining_bytes > 0) {
+  while (reader.remaining()) {
     DCHECK(partial_chunk_ != nullptr);
     size_t insert_length = 0;
     bool header_completed = false;
@@ -52,27 +48,24 @@ void ChunkedByteBuffer::Append(const uint8_t* start, size_t length) {
     if (partial_chunk_->header.size() < kHeaderLength) {
       const size_t bytes_to_complete_header =
           kHeaderLength - partial_chunk_->header.size();
-      insert_length = std::min(bytes_to_complete_header, remaining_bytes);
+      insert_length = std::min(bytes_to_complete_header, reader.remaining());
       insert_target = &partial_chunk_->header;
-      header_completed = (remaining_bytes >= bytes_to_complete_header);
+      header_completed = (reader.remaining() >= bytes_to_complete_header);
     } else {
       DCHECK_LT(partial_chunk_->content->size(),
                 partial_chunk_->ExpectedContentLength());
       const size_t bytes_to_complete_chunk =
           partial_chunk_->ExpectedContentLength() -
           partial_chunk_->content->size();
-      insert_length = std::min(bytes_to_complete_chunk, remaining_bytes);
+      insert_length = std::min(bytes_to_complete_chunk, reader.remaining());
       insert_target = partial_chunk_->content.get();
-      content_completed = (remaining_bytes >= bytes_to_complete_chunk);
+      content_completed = (reader.remaining() >= bytes_to_complete_chunk);
     }
 
     DCHECK_GT(insert_length, 0U);
-    DCHECK_LE(insert_length, remaining_bytes);
-    DCHECK_LE(next_data + insert_length, start + length);
-    insert_target->insert(insert_target->end(), next_data,
-                          next_data + insert_length);
-    next_data += insert_length;
-    remaining_bytes -= insert_length;
+    base::span<const uint8_t> next_chunk = reader.Read(insert_length).value();
+    insert_target->insert(insert_target->end(), next_chunk.begin(),
+                          next_chunk.end());
 
     if (header_completed) {
       DCHECK_EQ(partial_chunk_->header.size(), kHeaderLength);
@@ -91,12 +84,7 @@ void ChunkedByteBuffer::Append(const uint8_t* start, size_t length) {
       partial_chunk_ = std::make_unique<Chunk>();
     }
   }
-  DCHECK_EQ(next_data, start + length);
-  total_bytes_stored_ += length;
-}
-
-void ChunkedByteBuffer::Append(std::string_view string) {
-  Append(reinterpret_cast<const uint8_t*>(string.data()), string.size());
+  total_bytes_stored_ += data.size();
 }
 
 bool ChunkedByteBuffer::HasChunks() const {
@@ -121,9 +109,10 @@ void ChunkedByteBuffer::Clear() {
   total_bytes_stored_ = 0;
 }
 
-ChunkedByteBuffer::Chunk::Chunk() : content(new std::vector<uint8_t>()) {}
+ChunkedByteBuffer::Chunk::Chunk()
+    : content(std::make_unique<std::vector<uint8_t>>()) {}
 
-ChunkedByteBuffer::Chunk::~Chunk() {}
+ChunkedByteBuffer::Chunk::~Chunk() = default;
 
 size_t ChunkedByteBuffer::Chunk::ExpectedContentLength() const {
   DCHECK_EQ(header.size(), kHeaderLength);
--- components/speech/chunked_byte_buffer_unittest.cc
+++ components/speech/chunked_byte_buffer_unittest.cc
@@ -3,10 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
+#include "components/speech/chunked_byte_buffer.h"
 
 #include <stdint.h>
 
@@ -14,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "components/speech/chunked_byte_buffer.h"
+#include "base/containers/span.h"
+#include "base/containers/span_reader.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace speech {
@@ -24,27 +22,28 @@ typedef std::vector<uint8_t> ByteVector;
 TEST(ChunkedByteBufferTest, BasicTest) {
   ChunkedByteBuffer buffer;
 
-  const uint8_t kChunks[] = {
+  constexpr auto kChunks = std::to_array<uint8_t>({
       0x00, 0x00, 0x00, 0x04, 0x01, 0x02, 0x03, 0x04,  // Chunk 1: 4 bytes
       0x00, 0x00, 0x00, 0x02, 0x05, 0x06,              // Chunk 2: 2 bytes
       0x00, 0x00, 0x00, 0x01, 0x07                     // Chunk 3: 1 bytes
-  };
+  });
+  base::SpanReader reader{base::span(kChunks)};
 
   EXPECT_EQ(0U, buffer.GetTotalLength());
   EXPECT_FALSE(buffer.HasChunks());
 
   // Append partially chunk 1.
-  buffer.Append(kChunks, 2);
+  buffer.Append(base::as_string_view(*reader.Read(2u)));
   EXPECT_EQ(2U, buffer.GetTotalLength());
   EXPECT_FALSE(buffer.HasChunks());
 
   // Complete chunk 1.
-  buffer.Append(kChunks + 2, 6);
+  buffer.Append(base::as_string_view(*reader.Read(6u)));
   EXPECT_EQ(8U, buffer.GetTotalLength());
   EXPECT_TRUE(buffer.HasChunks());
 
   // Append fully chunk 2.
-  buffer.Append(kChunks + 8, 6);
+  buffer.Append(base::as_string_view(*reader.Read(6u)));
   EXPECT_EQ(14U, buffer.GetTotalLength());
   EXPECT_TRUE(buffer.HasChunks());
 
@@ -53,7 +52,7 @@ TEST(ChunkedByteBufferTest, BasicTest) {
   chunk = buffer.PopChunk();
   EXPECT_TRUE(chunk != nullptr);
   EXPECT_EQ(4U, chunk->size());
-  EXPECT_EQ(0, std::memcmp(kChunks + 4, chunk->data(), chunk->size()));
+  EXPECT_EQ(*chunk, base::span(kChunks).subspan(4, 4));
   EXPECT_EQ(6U, buffer.GetTotalLength());
   EXPECT_TRUE(buffer.HasChunks());
 
@@ -61,12 +60,12 @@ TEST(ChunkedByteBufferTest, BasicTest) {
   chunk = buffer.PopChunk();
   EXPECT_TRUE(chunk != nullptr);
   EXPECT_EQ(2U, chunk->size());
-  EXPECT_EQ(0, std::memcmp(kChunks + 12, chunk->data(), chunk->size()));
+  EXPECT_EQ(*chunk, base::span(kChunks).subspan(12, 2));
   EXPECT_EQ(0U, buffer.GetTotalLength());
   EXPECT_FALSE(buffer.HasChunks());
 
   // Append fully chunk 3.
-  buffer.Append(kChunks + 14, 5);
+  buffer.Append(base::as_string_view(*reader.Read(5u)));
   EXPECT_EQ(5U, buffer.GetTotalLength());
 
   // Remove and check chunk 3.
