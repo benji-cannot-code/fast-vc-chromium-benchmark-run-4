63ef7557f18b8bcdd893fd397cc8f1b780b77259
Daniel Murphy <dmurph@chromium.org>
2024-10-24 21:02:51 +0000

    [PWA/NavCapture] Create factory methods for data classes
    This change creates factory methods for the data classes used to store
    data for the navigation capturing pipeline.
    
    Future patches will work to continue tighten down the data here to only
    provide what is needed in the future, or possibly to make a builder
    pattern here to just populate all data for every case.
    
    This patch intends to be a noop, but likely fixes a few bugs.
    
    Bug: 370856876
    Change-Id: Ideca367cff58a59e255117ded68a03c08847ca25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958373
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373576}

--- chrome/browser/web_applications/navigation_capturing_navigation_handle_user_data.cc
+++ chrome/browser/web_applications/navigation_capturing_navigation_handle_user_data.cc
@@ -11,12 +11,98 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace web_app {
 
-NavigationCapturingRedirectionInfo::NavigationCapturingRedirectionInfo() =
-    default;
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::Disabled() {
+  return NavigationCapturingRedirectionInfo(
+      /*app_id_source_browser=*/std::nullopt,
+      NavigationHandlingInitialResult::kNotHandledByNavigationHandling,
+      /*first_navigation_app_id=*/std::nullopt, WindowOpenDisposition::UNKNOWN,
+      InitialNavigationCapturedBehavior::kNotHandled);
+}
+
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::AuxiliaryContext(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    WindowOpenDisposition disposition) {
+  return NavigationCapturingRedirectionInfo(
+      source_browser_app_id, NavigationHandlingInitialResult::kAuxContext,
+      /*first_navigation_app_id=*/std::nullopt, disposition,
+      InitialNavigationCapturedBehavior::kNotHandled);
+}
+
+// Created for user-modified or capturable navigations that don't have an
+// initial controlling app of the first url.
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::NoInitialActionRedirectionHandlingEligible(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    WindowOpenDisposition disposition) {
+  return NavigationCapturingRedirectionInfo(
+      source_browser_app_id, NavigationHandlingInitialResult::kBrowserTab,
+      /*first_navigation_app_id=*/std::nullopt, disposition,
+      InitialNavigationCapturedBehavior::kNotHandled);
+}
+
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::ForcedNewContext(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    const webapps::AppId controlling_app_id,
+    WindowOpenDisposition disposition) {
+  return NavigationCapturingRedirectionInfo(
+      source_browser_app_id,
+      source_browser_app_id
+          ? NavigationHandlingInitialResult::kForcedNewAppContext
+          : NavigationHandlingInitialResult::kBrowserTab,
+      controlling_app_id, disposition,
+      InitialNavigationCapturedBehavior::kNotHandled);
+}
+
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::CapturedNewContext(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    const webapps::AppId controlling_app_id,
+    WindowOpenDisposition disposition) {
+  return NavigationCapturingRedirectionInfo(
+      source_browser_app_id, NavigationHandlingInitialResult::kNavigateCaptured,
+      controlling_app_id, disposition,
+      InitialNavigationCapturedBehavior::kNavigatedNew);
+}
+
+// static
+NavigationCapturingRedirectionInfo
+NavigationCapturingRedirectionInfo::CapturedNavigateExisting(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    const webapps::AppId controlling_app_id,
+    WindowOpenDisposition disposition) {
+  return NavigationCapturingRedirectionInfo(
+      source_browser_app_id, NavigationHandlingInitialResult::kNavigateCaptured,
+      controlling_app_id, disposition,
+      InitialNavigationCapturedBehavior::kNavigatedExisting);
+}
+
 NavigationCapturingRedirectionInfo::~NavigationCapturingRedirectionInfo() =
     default;
 NavigationCapturingRedirectionInfo::NavigationCapturingRedirectionInfo(
     const NavigationCapturingRedirectionInfo& navigation_info) = default;
+NavigationCapturingRedirectionInfo&
+NavigationCapturingRedirectionInfo::operator=(
+    const NavigationCapturingRedirectionInfo&) = default;
+
+NavigationCapturingRedirectionInfo::NavigationCapturingRedirectionInfo(
+    const std::optional<webapps::AppId>& source_browser_app_id,
+    NavigationHandlingInitialResult initial_nav_handling_result,
+    const std::optional<webapps::AppId>& first_navigation_app_id,
+    WindowOpenDisposition disposition,
+    InitialNavigationCapturedBehavior effective_launch_handling_mode)
+    : app_id_source_browser_(source_browser_app_id),
+      initial_nav_handling_result_(initial_nav_handling_result),
+      first_navigation_app_id_(first_navigation_app_id),
+      disposition_(disposition),
+      effective_launch_handling_mode_(effective_launch_handling_mode) {}
 
 NavigationCapturingNavigationHandleUserData::
     ~NavigationCapturingNavigationHandleUserData() = default;
