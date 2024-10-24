4713013fd0bd491e94c25444728d193e27a10315
Charles Yang <cmyang@google.com>
2024-10-24 19:15:53 +0000

    [Lens] Add unary API with HTTP transport feature flag.
    Bug: b/369159694
    Change-Id: I59c4bd4eb0da53c570332569260fbb3d762faa5c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5959252
    Reviewed-by: Jason Hu <hujasonx@google.com>
    Reviewed-by: Rohit Rao <rohitrao@chromium.org>
    Commit-Queue: Charles Yang <cmyang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373497}

--- ios/chrome/browser/ui/lens/features.h
+++ ios/chrome/browser/ui/lens/features.h
@@ -29,4 +29,7 @@ int LensTranslateToggleMode();
 // Whether to enable the Lens web page load optimization.
 BASE_DECLARE_FEATURE(kLensWebPageLoadOptimizationEnabled);
 
+// Whether to use unary APIs with HTTP transport.
+BASE_DECLARE_FEATURE(kLensUnaryApisWithHttpTransportEnabled);
+
 #endif  // IOS_CHROME_BROWSER_UI_LENS_FEATURES_H_
