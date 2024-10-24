0af7b8aa336f2b0f4ed05d08465fa00435fffdb2
Sophie Wen <sophiewen@google.com>
2024-10-24 17:42:10 +0000

    sunfish: Add "Search" button in default capture mode
    This CL adds the action container and basic "Search" action button in
    default capture mode.
    
    Follow-up CLs will update the action buttons and capture UI.
    
    Demo: https://b.corp.google.com/issues/373896226#comment3
    
    Test: added
    Bug: b/374381937
    Change-Id: I6f9aaa49565b8e939bf1a369ce67d51a69efab45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5949010
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373421}

--- ash/capture_mode/capture_mode_controller.cc
+++ ash/capture_mode/capture_mode_controller.cc
@@ -880,7 +880,7 @@ void CaptureModeController::CaptureScreenshotOfGivenWindow(
       BehaviorType::kGameDashboard);
 }
 
-void CaptureModeController::PerformCapture() {
+void CaptureModeController::PerformCapture(PerformCaptureType capture_type) {
   DCHECK(IsActive());
 
   if (pending_dlp_check_)
@@ -898,12 +898,10 @@ void CaptureModeController::PerformCapture() {
       capture_params->window, capture_params->bounds,
       base::BindOnce(
           &CaptureModeController::OnDlpRestrictionCheckedAtPerformingCapture,
-          weak_ptr_factory_.GetWeakPtr()));
+          weak_ptr_factory_.GetWeakPtr(), capture_type));
 }
 
 void CaptureModeController::PerformImageSearch() {
-  DCHECK_EQ(capture_mode_session_->active_behavior()->behavior_type(),
-            BehaviorType::kSunfish);
   DCHECK(delegate_->IsCaptureAllowedByPolicy());
 
   const std::optional<CaptureParams> capture_params = GetCaptureParams();
@@ -2125,6 +2123,7 @@ void CaptureModeController::InterruptVideoRecording() {
 }
 
 void CaptureModeController::OnDlpRestrictionCheckedAtPerformingCapture(
+    PerformCaptureType capture_type,
     bool proceed) {
   pending_dlp_check_ = false;
 
@@ -2138,6 +2137,7 @@ void CaptureModeController::OnDlpRestrictionCheckedAtPerformingCapture(
   // We don't need to bring capture mode UIs back if `proceed` is false or if
   // the session is about to shutdown. See also
   // `CaptureModeBehavior::ShouldReShowUisAtPerformingCapture()`.
+  // TODO(b/374381937): Determine whether to reshow UIs or end the session.
   auto* active_behavior = capture_mode_session_->active_behavior();
   capture_mode_session_->OnWaitingForDlpConfirmationEnded(
       /*reshow_uis=*/proceed &&
@@ -2158,7 +2158,8 @@ void CaptureModeController::OnDlpRestrictionCheckedAtPerformingCapture(
   }
 
   if (type_ == CaptureModeType::kImage) {
-    if (active_behavior->behavior_type() == BehaviorType::kSunfish) {
+    if (active_behavior->behavior_type() == BehaviorType::kSunfish ||
+        capture_type == PerformCaptureType::kSearch) {
       // Sunfish behavior doesn't need the file path and does specific image
       // capture handling.
       PerformImageSearch();
