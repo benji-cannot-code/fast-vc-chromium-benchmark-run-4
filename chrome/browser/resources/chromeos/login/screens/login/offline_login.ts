88cd5c3f85cbe6cc4c414546784ad09389ec25ea
Yunke Zhou <yunkez@google.com>
2024-10-24 15:19:06 +0000

    [High5] Allow offline login using PIN
    When the user has only a pin factor, allow the user to use pin to login
    offline.
    Screenshot: https://screenshot.googleplex.com/7g9yggSd9WsZjms
    
    Bug: b:370865046
    Change-Id: I64f2560244bfc199a836901c24a8a965a4d7f852
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5936977
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Yunke Zhou <yunkez@google.com>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373331}

--- chrome/browser/resources/chromeos/login/screens/login/offline_login.ts
+++ chrome/browser/resources/chromeos/login/screens/login/offline_login.ts
@@ -103,6 +103,14 @@ export class OfflineLogin extends OfflineLoginBase {
       },
 
       /**
+       * Whether the user should be authenticated by pin or password.
+       */
+      authenticateByPin: {
+        type: Boolean,
+        value: false,
+      },
+
+      /**
        * Proper e-mail with domain, displayed on password page.
        */
       fullEmail: {
@@ -128,6 +136,7 @@ export class OfflineLogin extends OfflineLoginBase {
   private displayDomain: string;
   private email: string;
   private password: string;
+  private authenticateByPin: boolean;
   private fullEmail: string;
   private activeSection: string;
   private animationInProgress: boolean;
@@ -198,6 +207,7 @@ export class OfflineLogin extends OfflineLoginBase {
     this.manager = '';
     this.email = '';
     this.fullEmail = '';
+    this.authenticateByPin = false;
     this.shadowRoot!.querySelector<CrInputElement>('#emailInput')!.invalid =
         false;
     this.shadowRoot!.querySelector<CrInputElement>('#passwordInput')!.invalid =
@@ -205,7 +215,8 @@ export class OfflineLogin extends OfflineLoginBase {
     this.activeSection = LoginSection.EMAIL;
   }
 
-  proceedToPasswordPage(): void {
+  proceedToPasswordPage(authenticateByPin: boolean): void {
+    this.authenticateByPin = authenticateByPin;
     this.switchToPasswordCard(true /* animated */);
   }
 
@@ -343,6 +354,19 @@ export class OfflineLogin extends OfflineLoginBase {
       return;
     }
     this.onNextButtonClicked();
+  }
+
+  private passwordPlaceholderText(locale: string, authenticateByPin: boolean):
+      string {
+    const key = authenticateByPin ? 'offlineLoginPin' : 'offlineLoginPassword';
+    return this.i18nDynamic(locale, key);
+  }
+
+  private passwordErrorText(locale: string, authenticateByPin: boolean):
+      string {
+    const key = authenticateByPin ? 'offlineLoginInvalidPin' :
+                                    'offlineLoginInvalidPassword';
+    return this.i18nDynamic(locale, key);
   }
 }
 
