bc7a2909085611adf584e860b0d889b5edb179b3
Sarah Krakowiak Criel <skrakowi@chromium.org>
2024-10-24 18:08:04 +0000

    Add BertModelHandler class for notification content detection
    This CL implements the class that performs inference on our LiteRT model
    for detecting suspicious notifications. It also includes changes:
    -Log UMA histogram with "suspicious" label's score value
    -Add helper classes and files for testing and storing constants
    
    In follow up CLs, we will create a KeyedService and call on this model
    to perform inference.
    
    Bug: 374936030
    Change-Id: I034238fc49ad2832b0e9c25e87ec0bb11923684a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5948146
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Reviewed-by: Robert Ogden <robertogden@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373436}

---
+++ components/safe_browsing/content/browser/notification_content_detection/notification_content_detection_constants.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safe_browsing/content/browser/notification_content_detection/notification_content_detection_constants.h"
+
+namespace safe_browsing {
+
+const char kSuspiciousVerdictLabel[] = "suspicious";
+
+const char kSuspiciousScoreHistogram[] =
+    "SafeBrowsing.NotificationContentDetection.SuspiciousScore";
+
+}  // namespace safe_browsing
---
+++ components/safe_browsing/content/browser/notification_content_detection/notification_content_detection_constants.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_NOTIFICATION_CONTENT_DETECTION_NOTIFICATION_CONTENT_DETECTION_CONSTANTS_H_
+#define COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_NOTIFICATION_CONTENT_DETECTION_NOTIFICATION_CONTENT_DETECTION_CONSTANTS_H_
+
+namespace safe_browsing {
+
+// Model's label for a suspicious verdict.
+extern const char kSuspiciousVerdictLabel[];
+
+// Histogram names.
+extern const char kSuspiciousScoreHistogram[];
+
+}  // namespace safe_browsing
+
+#endif  // COMPONENTS_SAFE_BROWSING_CONTENT_BROWSER_NOTIFICATION_CONTENT_DETECTION_NOTIFICATION_CONTENT_DETECTION_CONSTANTS_H_
