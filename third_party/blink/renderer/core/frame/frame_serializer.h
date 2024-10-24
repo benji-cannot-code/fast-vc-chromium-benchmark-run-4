3564b9b147cc888cbaadd5f10a223364795dc084
Dan Harrington <harringtond@chromium.org>
2024-10-24 17:31:57 +0000

    MHTML: fix some issues with styles
    This CL makes two changes:
    
    1. For <style> nodes that have unmodified stylesheets, do not
       modify the <style> node. Because round-tripping from text
       to CSS rules back to text is lossy, this sometimes avoids
       problems. See crbug.com/40804066 for why it's lossy.
    2. For <style> nodes that have been modified, serialize them
       inline as a <style> node rather than as a <link> node.
       Serializing styles into another resource changes the
       baseURL, which will break how relative URLs are
       interpreted.
    
    Bug: 363289333
    Change-Id: Id165d21e5ab61f41b60d27e3c030d24da7832615
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5883007
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373409}

--- third_party/blink/renderer/core/frame/frame_serializer.h
+++ third_party/blink/renderer/core/frame/frame_serializer.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_CORE_FRAME_FRAME_SERIALIZER_H_
 
 #include "base/functional/callback.h"
+#include "base/functional/function_ref.h"
 #include "third_party/blink/public/web/web_frame_serializer.h"
 #include "third_party/blink/renderer/core/core_export.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
@@ -46,6 +47,16 @@ class LocalFrame;
 class Frame;
 
 struct SerializedResource;
+
+// Internal functionality exposed for unit testing.
+namespace internal {
+// Returns the result of replacing all case-insensitive occurrences of `from` in
+// `source` with the result of `transform.Run(match)`.
+CORE_EXPORT String
+ReplaceAllCaseInsensitive(String source,
+                          const String& from,
+                          base::FunctionRef<String(const String&)> transform);
+}  // namespace internal
 
 // This class is used to serialize frame's contents to MHTML. It serializes
 // frame's document and resources such as images and CSS stylesheets.
