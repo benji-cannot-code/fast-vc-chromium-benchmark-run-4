7d46ad684f61c2f1931b681b68d5d1a8f15bbfe0
Alex Attar <aattar@google.com>
2024-10-24 17:35:07 +0000

    [Code Health] Replace calls to AbandonAndStop by Stop in IPC
    Part of a series of CLs to remove calls to deprecated function
    AbandonAndStop and replace is by Stop instead.
    
    Bug: 40202541
    Change-Id: I84448eace8bc595b292e4c71eb8c943e16c2ccd5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962280
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373414}

--- ipc/ipc_cpu_perftest.cc
+++ ipc/ipc_cpu_perftest.cc
@@ -183,7 +183,7 @@ class ChannelSteadyPingPongListener : public Listener {
 
   void StopPingPong() {
     cpu_logger_.reset();
-    timer_.AbandonAndStop();
+    timer_.Stop();
     std::move(quit_closure_).Run();
   }
 
@@ -360,7 +360,7 @@ class MojoSteadyPingPongTest : public mojo::core::test::MojoTestBase {
 
   void StopPingPong() {
     cpu_logger_.reset();
-    timer_.AbandonAndStop();
+    timer_.Stop();
     std::move(quit_closure_).Run();
   }
 
