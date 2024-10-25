223c978d14efeeeea72f933a8a3df78d956aae06
Stefano Duo <stefanoduo@google.com>
2024-10-25 13:15:02 +0000

    Refactor and tweak CORS preflight request checks within CorsURLLoaderFactory
    It's not that clear why CorsURLLoaderFactory is checking for
    kURLLoadOptionAsCorsPreflight. This CL attempts to clarify the reasoning
    behind the check, while also fixing an issue related to multi-network
    CCT.
    
    Bug: 40203308, b:366242716
    Change-Id: Ib613f3a9eb323f1bda53c433b8d54724b21d09e2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5899528
    Commit-Queue: Stefano Duo <stefanoduo@google.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373858}

--- net/base/network_change_notifier.cc
+++ net/base/network_change_notifier.cc
@@ -1088,12 +1088,10 @@ void NetworkChangeNotifier::NotifyObserversOfDefaultNetworkActiveImpl() {
 
 NetworkChangeNotifier::DisableForTest::DisableForTest()
     : network_change_notifier_(g_network_change_notifier) {
-  DCHECK(g_network_change_notifier);
   g_network_change_notifier = nullptr;
 }
 
 NetworkChangeNotifier::DisableForTest::~DisableForTest() {
-  DCHECK(!g_network_change_notifier);
   g_network_change_notifier = network_change_notifier_;
 }
 
