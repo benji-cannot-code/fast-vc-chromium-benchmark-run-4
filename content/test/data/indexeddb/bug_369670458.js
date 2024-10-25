ccf054eab832690b499ccdb3b71709ead56e2f5e
Evan Stade <estade@chromium.org>
2024-10-25 15:43:30 +0000

    Blobs: Add test for WriteBlobToFile regression.
    Bug: 369670458
    Change-Id: Ie5119149ead49aae182650c5c039687e0859b958
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961165
    Commit-Queue: Evan Stade <estade@chromium.org>
    Reviewed-by: Brad Triebwasser <btriebw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373932}

---
+++ content/test/data/indexeddb/bug_369670458.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let connection;
+let file;
+
+function populateObjectStore() {
+  connection = event.target.result;
+  let store = connection.createObjectStore('store-name', null);
+}
+
+function doTest() {
+  const txn =
+      connection.transaction('store-name', 'readwrite');
+  txn.objectStore('store-name').add(file.slice(10, -10, file.type), 'key');
+  txn.onabort = unexpectedAbortCallback;
+  txn.onerror = unexpectedErrorCallback;
+  txn.oncomplete = done;
+}
+
+function setUp() {
+  document
+      .getElementById('fileInput')
+      .addEventListener('change', (e) => {
+    file = e.target.files[0];
+    indexedDBTest(populateObjectStore, doTest);
+  });
+}
