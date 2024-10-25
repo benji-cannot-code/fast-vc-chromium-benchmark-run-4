976eb9e68f273b6b4c2a98df40eabdcdf1c4bd2b
Sorin Jianu <sorin@chromium.org>
2024-10-25 21:15:37 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/media_router
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    A few of these have been missed by previous CLs and are being changed
    now.
    
    This CL was uploaded by git cl split.
    
    R=mfoltz@chromium.org, tguilbert@chromium.org
    
    Bug: 374110115
    Change-Id: I650eb1788bce46f75bd68e1ec944adb13135f441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963131
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1374139}

--- components/media_router/browser/android/media_router_android.cc
+++ components/media_router/browser/android/media_router_android.cc
@@ -89,7 +89,7 @@ MediaRouterAndroid::MediaRouteRequest::MediaRouteRequest(
       presentation_id(presentation_id),
       callback(std::move(callback)) {}
 
-MediaRouterAndroid::MediaRouteRequest::~MediaRouteRequest() {}
+MediaRouterAndroid::MediaRouteRequest::~MediaRouteRequest() = default;
 
 MediaRouterAndroid::MediaRouterAndroid()
     : bridge_(new MediaRouterAndroidBridge(this)) {}
