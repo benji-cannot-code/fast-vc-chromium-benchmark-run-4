8f9ee98d048c6f0c27114150f02355232cba1a03
Sergii Bykov <sbykov@google.com>
2024-10-25 08:09:58 +0000

    kiosk: add IWA info to status collector
    Bug: 361016399
    Change-Id: I05b9c12655b3bd4592d6f98cefc550dc48b24dfc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962856
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373777}

--- chrome/browser/ash/policy/status_collector/status_collector.cc
+++ chrome/browser/ash/policy/status_collector/status_collector.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 
 #include "base/time/time.h"
+#include "chrome/browser/ash/app_mode/isolated_web_app/kiosk_iwa_manager.h"
 #include "chrome/browser/ash/app_mode/kiosk_chrome_app_manager.h"
 #include "chrome/browser/ash/app_mode/web_app/web_kiosk_app_manager.h"
 #include "chrome/browser/ash/policy/core/device_local_account.h"
@@ -122,19 +123,25 @@ StatusCollector::GetAutoLaunchedKioskSessionInfo() {
   }
 
   ash::KioskChromeAppManager::App current_app;
-  bool regular_app_auto_launched_with_zero_delay =
+  const bool chrome_app_auto_launched_with_zero_delay =
       ash::KioskChromeAppManager::Get()->GetApp(account->kiosk_app_id,
                                                 &current_app) &&
       current_app.was_auto_launched_with_zero_delay;
 
-  bool web_app_auto_launched_with_zero_delay =
+  const bool web_app_auto_launched_with_zero_delay =
       ash::WebKioskAppManager::Get()
           ->current_app_was_auto_launched_with_zero_delay();
 
-  return regular_app_auto_launched_with_zero_delay ||
-                 web_app_auto_launched_with_zero_delay
-             ? std::move(account)
-             : nullptr;
+  const bool iwa_auto_launched_with_zero_delay =
+      ash::KioskIwaManager::Get()
+          ->current_app_was_auto_launched_with_zero_delay();
+
+  const bool kiosk_auto_launched_with_zero_delay =
+      chrome_app_auto_launched_with_zero_delay ||
+      web_app_auto_launched_with_zero_delay ||
+      iwa_auto_launched_with_zero_delay;
+
+  return kiosk_auto_launched_with_zero_delay ? std::move(account) : nullptr;
 }
 
 std::string StatusCollector::GetDMTokenForProfile(Profile* profile) const {
