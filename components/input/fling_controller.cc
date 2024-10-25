9ecea4d957bb44c4a7aa8dc69d641220e80e3a58
Sorin Jianu <sorin@chromium.org>
2024-10-25 19:10:24 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/input
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    A few of these have been missed by previous CLs and are being changed
    now.
    
    This CL was uploaded by git cl split.
    
    R=bokan@chromium.org
    
    Bug: 374110115
    Change-Id: I33cde16ba72d35042a659a1bccca73bd34adc598
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963207
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1374078}

--- components/input/fling_controller.cc
+++ components/input/fling_controller.cc
@@ -37,7 +37,7 @@ const char* kFlingTraceName = "FlingController::HandlingGestureFling";
 
 namespace input {
 
-FlingController::Config::Config() {}
+FlingController::Config::Config() = default;
 
 FlingController::FlingController(
     FlingControllerEventSenderClient* event_sender_client,
