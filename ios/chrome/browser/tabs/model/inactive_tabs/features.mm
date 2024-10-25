702d988517f870cee44e317ec39b1e3eef46afd0
Gauthier Ambard <gambard@chromium.org>
2024-10-25 13:22:32 +0000

    [iOS] Add fieldtrial for iPad inactive tabs
    Bug: 375190996
    Change-Id: I97fa4c4a240c108a43157a556a2b9ad8827e9784
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5965099
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Reviewed-by: Louis Romero <lpromero@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373861}

--- ios/chrome/browser/tabs/model/inactive_tabs/features.mm
+++ ios/chrome/browser/tabs/model/inactive_tabs/features.mm
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const int kInactiveTabsDisabledByUser = -1;
 
 BASE_FEATURE(kInactiveTabsIPadFeature,
-             "InactiveTabsFeature",
+             "InactiveTabsIPadFeature",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
 bool IsInactiveTabsAvailable() {
