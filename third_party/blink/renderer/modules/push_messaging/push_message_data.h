3b398b0de3efdf60c0e334a5f4a4b50c9a63e676
Peter Beverloo <peter@chromium.org>
2024-10-25 11:05:39 +0000

    Implement and ship PushMessageData::bytes()
    The PushMessageData interface mimics the Body interface, which was
    amended earlier this year with a new bytes() method, following the
    principle that APIs should generally vend byte buffers as Uint8Arrays.
    
    This change implements support for that. Note that PushMessageData does
    not, and cannot, extend from Body (or similar) classes directly, as
    access to data is provided synchronously rather than asynchronously.
    
    ChromeStatus entry:
    https://chromestatus.com/feature/5117729756151808
    
    Intent to Implement and Ship:
    https://groups.google.com/a/chromium.org/g/blink-dev/c/SCWQ0Tm2CFw
    
    Bug: 373336950
    Change-Id: Ideb086d7db7d9053f18e13750b0415ed54b8bcfb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5928888
    Reviewed-by: Daniel Bratell <bratell.d@gmail.com>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Daniel Bratell <bratell.d@gmail.com>
    Auto-Submit: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373826}

--- third_party/blink/renderer/modules/push_messaging/push_message_data.h
+++ third_party/blink/renderer/modules/push_messaging/push_message_data.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_PUSH_MESSAGING_PUSH_MESSAGE_DATA_H_
 
 #include "third_party/blink/renderer/bindings/core/v8/script_value.h"
+#include "third_party/blink/renderer/core/typed_arrays/dom_typed_array.h"
 #include "third_party/blink/renderer/modules/modules_export.h"
 #include "third_party/blink/renderer/platform/bindings/script_wrappable.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
@@ -33,6 +34,7 @@ class MODULES_EXPORT PushMessageData final : public ScriptWrappable {
 
   DOMArrayBuffer* arrayBuffer() const;
   Blob* blob() const;
+  DOMUint8Array* bytes() const;
   ScriptValue json(ScriptState* script_state,
                    ExceptionState& exception_state) const;
   String text() const;
