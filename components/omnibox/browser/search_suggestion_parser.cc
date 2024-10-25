659d62d04b3014ce4ab3e23ca693b80bb36a130e
Sorin Jianu <sorin@chromium.org>
2024-10-25 16:49:06 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/omnibox
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    A few of these have been missed by previous CLs and are being changed
    now.
    
    This CL was uploaded by git cl split.
    
    R=jdonnelly@chromium.org
    
    Bug: 374110115
    Change-Id: Ic0cfb8f6b6560b93ed047029916eca11907f7898
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963210
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373969}

--- components/omnibox/browser/search_suggestion_parser.cc
+++ components/omnibox/browser/search_suggestion_parser.cc
@@ -282,7 +282,7 @@ SearchSuggestionParser::Result::Result(
 
 SearchSuggestionParser::Result::Result(const Result& other) = default;
 
-SearchSuggestionParser::Result::~Result() {}
+SearchSuggestionParser::Result::~Result() = default;
 
 // SearchSuggestionParser::SuggestResult ---------------------------------------
 
@@ -357,7 +357,7 @@ SearchSuggestionParser::SuggestResult::SuggestResult(
 SearchSuggestionParser::SuggestResult::SuggestResult(
     const SuggestResult& result) = default;
 
-SearchSuggestionParser::SuggestResult::~SuggestResult() {}
+SearchSuggestionParser::SuggestResult::~SuggestResult() = default;
 
 SearchSuggestionParser::SuggestResult&
 SearchSuggestionParser::SuggestResult::operator=(const SuggestResult& rhs) =
@@ -476,7 +476,7 @@ SearchSuggestionParser::NavigationResult::NavigationResult(
 SearchSuggestionParser::NavigationResult::NavigationResult(
     const NavigationResult& other) = default;
 
-SearchSuggestionParser::NavigationResult::~NavigationResult() {}
+SearchSuggestionParser::NavigationResult::~NavigationResult() = default;
 
 void SearchSuggestionParser::NavigationResult::
     CalculateAndClassifyMatchContents(const bool allow_bolding_nothing,
@@ -547,7 +547,7 @@ SearchSuggestionParser::Results::Results()
       field_trial_triggered(false),
       relevances_from_server(false) {}
 
-SearchSuggestionParser::Results::~Results() {}
+SearchSuggestionParser::Results::~Results() = default;
 
 void SearchSuggestionParser::Results::Clear() {
   suggest_results.clear();
