e33b01352d6845c7254182b27eb3c5022fc089ec
Samuel Huang <huangs@chromium.org>
2024-10-25 20:10:31 +0000

    [Android FRE] Fix various UI fragility problems.
    This CL fixes multiple issues related to FRE when there are multiple
    fragments. For concreteness let's assume
    * "Page 1" = Sign-In fragment.
    * "Page 2" = Search engine selection fragment.
    * "Advance" = Transition from Page 1 to Page 2, with animation.
    
    1. Chrome crashes in
       HistorySyncCoordinator.maybeRecreateView()
       when transitioning back and forth between Page 1 / Page 2, especially
       if (2), (3), (4) below trigger.
       * Cause: In HistorySyncCoordinator: createViewAndAttachToFragment()
         assumes maybeCreateCoordinator() ensures |mHistorySyncCoordinator|
         is instantiated. This is false: maybeCreateCoordinator() attempts
         to triggers Advance if no primary account set, and leaves the
         Coordinator uninitialized.
       * Non-fix: Ensure |mHistorySyncCoordinator| is instantiated even if
         soon useless: This doesn't work since HistorySyncMediator requires
         account.
       * Fix: Guard against |mHistorySyncCoordinator = null|.
    
    2. With animation, in landscape mode, on advance, 1 Page 2 gets skipped.
      * Cause: FirstRunActivity uses "fake drag" to animate transition,
        using screen width as the drag amount. But in landscape mode, the
        Android Navigation bar takes space, so the drag exceed app width,
        causing transition to overshoot beyond Page 2 and skips it.
      * Fix: Use app width (from DisplayMetrics) for fake drag.
    
    3. Even with (2) fixed, if device changes from landscape to portrait
       during transition (reproduceable by increasing TRANSITION_DELAY_MS)
       then Page 2 still get skipped on advance.
      * Cause: FirstRunActivity transition uses ValueAnimator from 0 to
        (after fixing (2)) app width when animation starts. On resize
        from orientation change, fake drag would exceeds bound.
      * Non-fix: Stop animation when screen width change is detected:
        This is turns out to be complex and unreliable.
      * Fix: Change ValueAnimator to animate screen fraction 0.0 to 1.0,
        then use freshly read screen width in fake drag.
    
    4. Page 1 button (e.g., "Use without account") is visible and clickable
       during animation, causing Page 2 to be skipped if clicked multiple
       times.
      * Cause: Recently-added animation causes button to linger. The old
        assumption of immediate screen change (allowing "fire and forget")
        no longer holds. Also, click causes advanceToNextPage(), which is
        additive.
      * Fix: In FirstRunActivity, store ValueAnimator as member field
        |mAnimator|, and use its presence to debounce page change calls
        (this includes back button) advanceToNextPageInternal() and
        setCurrentItemForPager().
    
    Bug: 374695176
    Change-Id: Ifee1bb0589592ce08a4f613828d794db983786b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5966633
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374108}

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunActivity.java
@@ -11,6 +11,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import androidx.annotation.CallSuper;
@@ -201,6 +202,8 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
 
     private FirstRunPageTransformer mPageTransformer;
     private ViewPager2 mPager;
+
+    private ValueAnimator mAnimator;
 
     /** The pager adapter, which provides the pages to the view pager widget. */
     private FirstRunPagerAdapter mPagerAdapter;
@@ -475,8 +478,12 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
     /**
      * @param {boolean} smoothScroll Whether to animate transition. This should be true for user
      *     triggered transition, and false for quick skips by software.
+     * @return Whether advancing to the next page succeeded.
      */
     private boolean advanceToNextPageInternal(boolean smoothScroll) {
+        // Debounce page changes while animation is in flight.
+        if (mAnimator != null) return false;
+
         mFirstRunFlowSequencer.updateFirstRunProperties(mFreProperties);
 
         int position = mPager.getCurrentItem() + 1;
@@ -560,6 +567,7 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
         if (position < 0) {
             abortFirstRunExperience();
         } else {
+            // Might be debounced if animation is in flight, but consider this SUCCESS anyway.
             setCurrentItemForPager(position, true);
         }
         return BackPressResult.SUCCESS;
@@ -683,7 +691,22 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
         return LocalizationUtils.isLayoutRtl();
     }
 
+    /**
+     * Returns the current window width, which is affected by orientation changes or resizing. For
+     * some usage (e.g., fake drag updates) we'd need to call this function frequently and avoid
+     * using a stale value.
+     */
+    private int getWindowWidth() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
+
+    /** Returns whether the set attempt will lead to transition to an existing Fragment. */
     private boolean setCurrentItemForPager(int position, boolean smoothScroll) {
+        // Debounce page changes while animation is in flight.
+        if (mAnimator != null) return false;
+
         if (sObserver != null) sObserver.onJumpToPage(this, position);
 
         if (position >= mPagerAdapter.getItemCount()) {
@@ -706,15 +729,15 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
 
         if (!sIsAnimationDisabled && smoothScroll) {
             // Use fake drags to control transition time and interpolation in ViewPager2.
-            int width = getWindow().getDecorView().getWidth();
+
             // Direction of content shift: Forward -> negative; backward -> positive (assuming LTR).
             int direction = (position > oldPosition) ? -1 : 1;
             mPageTransformer.setDirection(direction);
             // Direction of drag, which is flipped if RTL.
             float dragSign = direction * (isRtl() ? -1f : 1f);
             // Use linear interpolation to enable custom interpolators usage of various properties.
-            ValueAnimator animator = ValueAnimator.ofInt(0, width);
-            animator.setInterpolator(Interpolators.LINEAR_INTERPOLATOR);
+            mAnimator = ValueAnimator.ofFloat(0f, 1f);
+            mAnimator.setInterpolator(Interpolators.LINEAR_INTERPOLATOR);
 
             class UpdateListener implements ValueAnimator.AnimatorUpdateListener {
                 /** Previous animated value to calculate fake drag delta for transitions. */
@@ -722,16 +745,17 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
 
                 @Override
                 public void onAnimationUpdate(ValueAnimator animation) {
-                    int animatedValue = ((Integer) animation.getAnimatedValue()).intValue();
+                    float frac = ((Float) animation.getAnimatedValue()).floatValue();
+                    int animatedValue = Math.round(frac * getWindowWidth());
                     float deltaPx = dragSign * (animatedValue - mPrevAnimatedValue);
                     mPager.fakeDragBy(deltaPx);
                     mPrevAnimatedValue = animatedValue;
                 }
             }
 
-            animator.addUpdateListener(new UpdateListener());
+            mAnimator.addUpdateListener(new UpdateListener());
 
-            animator.addListener(
+            mAnimator.addListener(
                     new ValueAnimator.AnimatorListener() {
                         @Override
                         public void onAnimationStart(Animator animation) {
@@ -741,12 +765,13 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
                         @Override
                         public void onAnimationEnd(Animator animation) {
                             mPager.endFakeDrag();
+                            mAnimator = null;
                             // No need to call `mPager.setCurrentItem(position, false)`.
                         }
 
                         @Override
                         public void onAnimationCancel(Animator animation) {
-                            /* Ignored */
+                            mAnimator = null;
                         }
 
                         @Override
@@ -755,8 +780,8 @@ public class FirstRunActivity extends FirstRunActivityBase implements FirstRunPa
                         }
                     });
 
-            animator.setDuration(TRANSITION_DELAY_MS);
-            animator.start();
+            mAnimator.setDuration(TRANSITION_DELAY_MS);
+            mAnimator.start();
 
         } else {
             mPager.setCurrentItem(position, false);
