dadc5ca4389b1dcbc7067a02b1146148a1e55f4c
Menghan Yang <myuu@google.com>
2024-10-25 05:24:17 +0000

    [Android][Signin] Re-enable testProfileDataUpdateOnInitialExpandedSheet
    This CL re-enables testProfileDataUpdateOnInitialExpandedSheet, since
    the test is not flaky anymore on revivers following the fix landed
    with https://crrev.com/c/5934372.
    
    Fixed: 342629369
    Change-Id: I9da7813aeaf8c08111fcc3174359a731cf2ec17c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962137
    Commit-Queue: Samar Chehade-Lepleux <samarchehade@google.com>
    Reviewed-by: Samar Chehade-Lepleux <samarchehade@google.com>
    Auto-Submit: Menghan Yang <myuu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373732}

--- chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetTest.java
+++ chrome/browser/ui/android/signin/java/src/org/chromium/chrome/browser/ui/signin/account_picker/AccountPickerBottomSheetTest.java
@@ -68,7 +68,6 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.HistogramWatcher;
@@ -579,7 +578,6 @@ public class AccountPickerBottomSheetTest {
 
     @Test
     @MediumTest
-    @DisabledTest(message = "crbug.com/342629369")
     public void testProfileDataUpdateOnInitialExpandedSheet() {
         buildAndShowBottomSheet(AccountPickerLaunchMode.CHOOSE_ACCOUNT);
         String newFullName = "New Full Name1";
