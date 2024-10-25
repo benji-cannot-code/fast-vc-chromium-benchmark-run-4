e59dcb06eaccb195ff027f389d937b4cbcb210a7
Hyungwook Lee <hyungwook.lee@navercorp.com>
2024-10-25 04:32:01 +0000

    Remove some chrome:: namespace from chrome/browser/win/*
    Bug: 41050580
    Change-Id: I8b2723c9dbdcf2e3e2941875d09fff26a9db323f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5942398
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Hyungwook Lee <hyungwook.lee@navercorp.com>
    Cr-Commit-Position: refs/heads/main@{#1373726}

--- chrome/browser/win/chrome_process_finder.h
+++ chrome/browser/win/chrome_process_finder.h
@@ -14,9 +14,7 @@ namespace base {
 class FilePath;
 }
 
-namespace chrome {
-
-enum NotifyChromeResult {
+enum class NotifyChromeResult {
   NOTIFY_SUCCESS,
   NOTIFY_FAILED,
   NOTIFY_WINDOW_HUNG,
@@ -32,7 +30,5 @@ NotifyChromeResult AttemptToNotifyRunningChrome(HWND remote_window);
 
 // Changes the notification timeout to |new_timeout|, returns the old timeout.
 base::TimeDelta SetNotificationTimeoutForTesting(base::TimeDelta new_timeout);
-
-}  // namespace chrome
 
 #endif  // CHROME_BROWSER_WIN_CHROME_PROCESS_FINDER_H_
