10fba7cf546c2a57ba07687e6e517063971c5c38
Xiaodan Zhu <zxdan@chromium.org>
2024-10-25 20:19:42 +0000

    coral: stop cache embedding when coral disabled
    Bug: b:371221303
    Change-Id: If6ca5d64d5d5747e77ecf133c582d6d18f319e8a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963123
    Commit-Queue: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Howard Yang <hcyang@google.com>
    Reviewed-by: Matthew Mourgos <mmourgos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374112}

--- ash/birch/birch_coral_provider.cc
+++ ash/birch/birch_coral_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/birch/birch_model.h"
 #include "ash/birch/coral_item_remover.h"
 #include "ash/birch/coral_util.h"
+#include "ash/constants/ash_pref_names.h"
 #include "ash/constants/ash_switches.h"
 #include "ash/public/cpp/app_types_util.h"
 #include "ash/public/cpp/saved_desk_delegate.h"
@@ -30,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "chromeos/ash/services/coral/public/mojom/coral_service.mojom.h"
 #include "chromeos/ui/base/window_properties.h"
+#include "components/prefs/pref_service.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "ui/wm/core/window_util.h"
 
@@ -441,7 +443,10 @@ void BirchCoralProvider::FilterCoralContentItems(
 
 void BirchCoralProvider::MaybeCacheTabEmbedding(TabClusterUIItem* tab_item) {
   // Only cache tab embeddings for the primary user.
-  if (Shell::Get()->session_controller()->IsUserPrimary() &&
+  auto* session_controller = Shell::Get()->session_controller();
+  if (session_controller->IsUserPrimary() &&
+      session_controller->GetPrimaryUserPrefService()->GetBoolean(
+          prefs::kBirchUseCoral) &&
       IsValidTab(tab_item) && ShouldCreateEmbedding(tab_item)) {
     CacheTabEmbedding(tab_item);
   }
