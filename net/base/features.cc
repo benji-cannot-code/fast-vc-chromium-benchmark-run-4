04caa2f6e1ea3c084de259af52eed5303da4be7a
Abhi Patel <abhipatel@google.com>
2024-10-25 15:25:45 +0000

    Only create IPP proxy delegate when required.
    Context:
    Currently, we create IPP Proxy Delegate objects and the IPP Core
    even when an `NetworkContext` does not participate in IP Protection.
    This was done in preparation to a webview experiment. This CL will
    once again only create these IPP objects when required. A feature param
    will be added that can be used in any future webview experiments what
    force create IPP objects in the `NetworkContext`.
    
    Bug: 370696608,40947771
    Change-Id: I7fa95d434614524ea5ba1642fad0140980f1a46b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954181
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Abhi Patel <abhipatel@chromium.org>
    Reviewed-by: Maks Orlovich <morlovich@chromium.org>
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373915}

--- net/base/features.cc
+++ net/base/features.cc
@@ -448,6 +448,11 @@ const base::FeatureParam<bool> kIpPrivacyCacheTokensByGeo{
     /*name=*/"IpPrivacyCacheTokensByGeo",
     /*default_value=*/false};
 
+const base::FeatureParam<bool> kIpPrivacyAlwaysCreateCore{
+    &kEnableIpProtectionProxy,
+    /*name=*/"IpPrivacyAlwaysCreateCore",
+    /*default_value=*/false};
+
 // Network-change migration requires NetworkHandle support, which are currently
 // only supported on Android (see
 // NetworkChangeNotifier::AreNetworkHandlesSupported).
