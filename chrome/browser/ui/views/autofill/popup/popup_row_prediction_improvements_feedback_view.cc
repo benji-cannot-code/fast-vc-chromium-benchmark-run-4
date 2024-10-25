ff7993270f1f61f00a69d65257b3235ab8850f3c
Dmitry Vykochko <vykochko@google.com>
2024-10-25 12:49:06 +0000

    Suppress mouse selection of prediction improvements feedback suggestion.
    When selected, the manage predictions link area is highlighted
    and pressing ENTER opens the link in a new tab. This is supported
    for accessibility. When a mouse (or other pointing device)
    is used, there is no need in such highlighting - the link
    can be just clicked, highlighting the area looks weird.
    Fixed by not selecting the suggestion view when it is triggered
    by mouse.
    
    Before:
    http://screencast/cast/NDc3MDk5MTgzNzI4MjMwNHxjYmQxNzBlOC04Yw
    After
    http://screencast/cast/NDg5NTg5MzU5NjY2NzkwNHxmNTljZWVmNC00ZQ
    
    Bug: 372447295
    Change-Id: Ic0b794b1c37b2cec93107b7a79b020e74b0d395a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938437
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Dmitry Vykochko <vykochko@google.com>
    Cr-Commit-Position: refs/heads/main@{#1373849}

--- chrome/browser/ui/views/autofill/popup/popup_row_prediction_improvements_feedback_view.cc
+++ chrome/browser/ui/views/autofill/popup/popup_row_prediction_improvements_feedback_view.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/autofill/popup/popup_base_view.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_cell_utils.h"
 #include "chrome/browser/ui/views/autofill/popup/popup_row_content_view.h"
+#include "chrome/browser/ui/views/autofill/popup/popup_row_view.h"
+#include "chrome/browser/ui/views/autofill/popup/popup_view_utils.h"
 #include "chrome/browser/ui/views/chrome_layout_provider.h"
 #include "components/autofill/core/browser/ui/suggestion_button_action.h"
 #include "components/autofill_prediction_improvements/core/browser/autofill_prediction_improvements_features.h"
@@ -287,6 +289,16 @@ bool PopupRowPredictionImprovementsFeedbackView::HandleKeyPressEvent(
   }
 
   return PopupRowView::HandleKeyPressEvent(event);
+}
+
+void PopupRowPredictionImprovementsFeedbackView::OnCellSelected(
+    std::optional<CellType> type,
+    PopupCellSelectionSource source) {
+  if (source == PopupCellSelectionSource::kMouse) {
+    return;
+  }
+
+  PopupRowView::OnCellSelected(type, source);
 }
 
 views::View&
