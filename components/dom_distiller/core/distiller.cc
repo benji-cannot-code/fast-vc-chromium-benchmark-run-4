1106675ddb2d06a04c0dbd98517da6a0202ec2fc
Sorin Jianu <sorin@chromium.org>
2024-10-25 15:10:32 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/dom_distiller
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    A few of these have been missed by previous CLs and are being changed
    now.
    
    This CL was uploaded by git cl split.
    
    R=mdjones@chromium.org
    
    Bug: 374110115
    Change-Id: Id83d38370848762291649773df4fab4abca7a855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960381
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373906}

--- components/dom_distiller/core/distiller.cc
+++ components/dom_distiller/core/distiller.cc
@@ -48,9 +48,9 @@ std::unique_ptr<Distiller> DistillerFactoryImpl::CreateDistillerForUrl(
   return std::move(distiller);
 }
 
-DistillerImpl::DistilledPageData::DistilledPageData() {}
+DistillerImpl::DistilledPageData::DistilledPageData() = default;
 
-DistillerImpl::DistilledPageData::~DistilledPageData() {}
+DistillerImpl::DistilledPageData::~DistilledPageData() = default;
 
 DistillerImpl::DistillerImpl(
     const DistillerURLFetcherFactory& distiller_url_fetcher_factory,
