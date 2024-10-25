5893efd97e64b4aa9775bed0d0c7769006c06cd9
Amanda Lin Dietz <aldietz@google.com>
2024-10-25 22:16:38 +0000

    [FaceGaze] Fix default threshold in dialog by updating to 50
    * The UI was erroneously populated with a default confidence threshold
    of 60, which is outdated. The default confidence threshold should be
    50 as per the constant in GestureHandler.
    * Update tests.
    
    Bug: 309121742
    Change-Id: I81205802269d86a1c0f54d0dd7cc0b46723ff8a9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5964038
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374163}

--- chrome/test/data/webui/chromeos/settings/os_a11y_page/facegaze_actions_add_dialog_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_a11y_page/facegaze_actions_add_dialog_test.ts
@@ -688,7 +688,7 @@ suite('<facegaze-actions-add-dialog>', () => {
         saveButton.click();
         flush();
 
-        assertTrue(isThresholdValueSetInPref(65));
+        assertTrue(isThresholdValueSetInPref(55));
         assertFalse(faceGazeAddActionDialog.$.dialog.open);
       });
 
@@ -710,7 +710,7 @@ suite('<facegaze-actions-add-dialog>', () => {
         saveButton.click();
         flush();
 
-        assertTrue(isThresholdValueSetInPref(55));
+        assertTrue(isThresholdValueSetInPref(45));
         assertEventContainsCommandPair(new FaceGazeCommandPair(
             MacroName.MOUSE_CLICK_LEFT, FacialGesture.BROW_INNER_UP));
         assertFalse(faceGazeAddActionDialog.$.dialog.open);
@@ -865,12 +865,12 @@ suite('<facegaze-actions-add-dialog>', () => {
 
         webUIListenerCallback('settings.sendGestureInfoToSettings', [
           {gesture: FacialGesture.BROW_INNER_UP, confidence: 70},
-          {gesture: FacialGesture.BROW_INNER_UP, confidence: 50},
+          {gesture: FacialGesture.BROW_INNER_UP, confidence: 40},
         ]);
 
         const gestureCountDiv = getGestureCountDiv();
 
-        // Default confidence threshold is 60, so only one gesture should
+        // Default confidence threshold is 50, so only one gesture should
         // register as detected.
         assertEquals(`Detected 1 time`, gestureCountDiv.innerText);
       });
@@ -889,7 +889,7 @@ suite('<facegaze-actions-add-dialog>', () => {
 
         const gestureCountDiv = getGestureCountDiv();
 
-        // Default confidence threshold is 60, so only one gesture should
+        // Default confidence threshold is 50, so only one gesture should
         // register as detected.
         assertEquals(`Detected 1 time`, gestureCountDiv.innerText);
       });
@@ -926,7 +926,7 @@ suite('<facegaze-actions-add-dialog>', () => {
 
         const gestureCountDiv = getGestureCountDiv();
 
-        // Default confidence threshold is 60, so three gestures should register
+        // Default confidence threshold is 50, so three gestures should register
         // as detected.
         assertEquals(`Detected 3 times`, gestureCountDiv.innerText);
       });
