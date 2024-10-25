44cd91118270edbdf67a5e4b739c64883e3e8d92
S. Ganesh <ganesh@chromium.org>
2024-10-25 01:01:30 +0000

    updater: Lower version integration tests from different architectures
    This CL tests the following scenarios for revision 1371769:
    * 32-bit to 64-bit migration.
    * 64-bit to 32-bit migration.
    
    Specifically, the following builds are tested:
    * older native ARM, x64, and x86 builds to newer Arm64 builds.
    * older x64 and x86 builds to newer x64 builds.
    * older x64 and x86 builds to newer x86 builds.
    
    Bug: 374217027
    Change-Id: I3ed151644a59252e4e83d15bf7b5784279b2bc89
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960176
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1373680}

--- chrome/updater/test/integration_tests_linux.cc
+++ chrome/updater/test/integration_tests_linux.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "base/base_paths.h"
 #include "base/command_line.h"
@@ -186,7 +187,7 @@ void ExpectNotActive(UpdaterScope scope, const std::string& app_id) {
   EXPECT_FALSE(base::PathIsWritable(*path));
 }
 
-base::FilePath GetRealUpdaterLowerVersionPath() {
+std::vector<base::FilePath> GetRealUpdaterLowerVersionPaths() {
   base::FilePath exe_path;
   EXPECT_TRUE(base::PathService::Get(base::DIR_EXE, &exe_path));
   base::FilePath old_updater_path =
@@ -200,8 +201,8 @@ base::FilePath GetRealUpdaterLowerVersionPath() {
 #if BUILDFLAG(CHROMIUM_BRANDING) || BUILDFLAG(GOOGLE_CHROME_BRANDING)
   old_updater_path = old_updater_path.AppendASCII("cipd");
 #endif
-  return old_updater_path.AppendASCII(
-      base::StrCat({kExecutableName, kExecutableSuffix}));
+  return {old_updater_path.AppendASCII(
+      base::StrCat({kExecutableName, kExecutableSuffix}))};
 }
 
 void SetupFakeLegacyUpdater(UpdaterScope scope) {
