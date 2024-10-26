40d6f94f430b3488e55d2dcc2043886fe707ca99
Austin Sullivan <asully@chromium.org>
2024-10-26 04:00:15 +0000

    webnn: Rename some MLTensor error messages to say "tensor"
    They currently say "buffer"
    
    Also updates several variable names while we're at it
    
    Bug: 361372446
    Change-Id: I136d175412a64755d478c53350079789a12ee6fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967040
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Cr-Commit-Position: refs/heads/main@{#1374250}

--- third_party/blink/renderer/modules/ml/ml_context.cc
+++ third_party/blink/renderer/modules/ml/ml_context.cc
@@ -119,7 +119,7 @@ void MLContext::Trace(Visitor* visitor) const {
   visitor->Trace(pending_resolvers_);
   visitor->Trace(graphs_);
   visitor->Trace(graph_builders_);
-  visitor->Trace(buffers_);
+  visitor->Trace(tensors_);
   ScriptWrappable::Trace(visitor);
 }
 
@@ -147,8 +147,8 @@ void MLContext::destroy(ScriptState* script_state,
       graph_builder->OnConnectionError();
     }
 
-    for (const auto& buffer : buffers_) {
-      buffer->destroy();
+    for (const auto& tensor : tensors_) {
+      tensor->destroy();
     }
   }
 }
@@ -1096,13 +1096,13 @@ ScriptPromise<DOMArrayBuffer> MLContext::readTensor(
 
   if (src_tensor->context() != this) {
     exception_state.ThrowTypeError(
-        "The source buffer wasn't created with this context.");
+        "The source tensor wasn't created with this context.");
     return EmptyPromise();
   }
 
   if (!src_tensor->Usage().Has(webnn::MLTensorUsageFlags::kRead)) {
     exception_state.ThrowTypeError(
-        "The source buffer doesn't have read access.");
+        "The source tensor doesn't have read access.");
     return EmptyPromise();
   }
 
@@ -1124,7 +1124,7 @@ ScriptPromise<IDLUndefined> MLContext::readTensor(
 
   if (src_tensor->context() != this) {
     exception_state.ThrowTypeError(
-        "The source buffer wasn't created with this context.");
+        "The source tensor wasn't created with this context.");
     return EmptyPromise();
   }
 
@@ -1146,7 +1146,7 @@ ScriptPromise<IDLUndefined> MLContext::readTensor(
 
   if (src_tensor->context() != this) {
     exception_state.ThrowTypeError(
-        "The source buffer wasn't created with this context.");
+        "The source tensor wasn't created with this context.");
     return EmptyPromise();
   }
 
@@ -1170,13 +1170,13 @@ void MLContext::WriteWebNNTensor(ScriptState* script_state,
 
   if (dst_tensor->context() != this) {
     exception_state.ThrowTypeError(
-        "The destination buffer wasn't created with this context.");
+        "The destination tensor wasn't created with this context.");
     return;
   }
 
   if (!dst_tensor->Usage().Has(webnn::MLTensorUsageFlags::kWrite)) {
     exception_state.ThrowTypeError(
-        "The destination buffer doesn't have write access.");
+        "The destination tensor doesn't have write access.");
     return;
   }
 
@@ -1218,7 +1218,7 @@ void MLContext::WriteWebNNTensor(ScriptState* script_state,
 
   if (write_byte_size > dst_tensor->PackedByteLength()) {
     exception_state.ThrowTypeError(
-        "Number of bytes to write is too large: write size exceeded buffer "
+        "Number of bytes to write is too large: write size exceeded tensor "
         "size.");
     return;
   }
@@ -1278,19 +1278,19 @@ void MLContext::DidCreateWebNNTensor(
   }
 
   if (result->is_error()) {
-    const auto& create_buffer_error = result->get_error();
+    const auto& create_tensor_error = result->get_error();
     resolver->RejectWithDOMException(
-        WebNNErrorCodeToDOMExceptionCode(create_buffer_error->code),
-        create_buffer_error->message);
+        WebNNErrorCodeToDOMExceptionCode(create_tensor_error->code),
+        create_tensor_error->message);
     return;
   }
 
-  auto* buffer = MakeGarbageCollected<MLTensor>(
+  auto* tensor = MakeGarbageCollected<MLTensor>(
       resolver->GetExecutionContext(), this, std::move(validated_descriptor),
       usage, std::move(result->get_success()), base::PassKey<MLContext>());
-  buffers_.insert(buffer);
+  tensors_.insert(tensor);
 
-  resolver->Resolve(buffer);
+  resolver->Resolve(tensor);
 }
 
 }  // namespace blink
