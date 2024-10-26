8337b52c06cbed74496b0241493c69799db8303b
Elijah Hewer <hewer@chromium.org>
2024-10-26 01:09:19 +0000

    Sunfish: Capture image when region adjusted
    Update the Sunfish session behavior so that an image is captured for
    search when the region is adjusted, and not just selected for the first
    time.
    
    Bug: 374169746
    Test: SunfishTest.OnRegionUpdated
    Change-Id: I0903a76d337e11885d363be11fafda695664ff83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5957776
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374227}

--- ash/capture_mode/capture_mode_behavior.cc
+++ ash/capture_mode/capture_mode_behavior.cc
@@ -345,7 +345,7 @@ class SunfishBehavior : public CaptureModeBehavior {
   std::unique_ptr<CaptureModeBarView> CreateCaptureModeBarView() override {
     return std::make_unique<SunfishCaptureBarView>();
   }
-  void OnRegionSelected() override {
+  void OnRegionSelectedOrAdjusted() override {
     // `CaptureModeController` will perform DLP restriction checks and determine
     // whether the image can be sent for search.
     CaptureModeController::Get()->PerformCapture();
@@ -596,7 +596,7 @@ void CaptureModeBehavior::OnAudioRecordingModeChanged() {}
 
 void CaptureModeBehavior::OnDemoToolsSettingsChanged() {}
 
-void CaptureModeBehavior::OnRegionSelected() {}
+void CaptureModeBehavior::OnRegionSelectedOrAdjusted() {}
 
 void CaptureModeBehavior::OnEnterKeyPressed() {
   CaptureModeController::Get()->PerformCapture();
