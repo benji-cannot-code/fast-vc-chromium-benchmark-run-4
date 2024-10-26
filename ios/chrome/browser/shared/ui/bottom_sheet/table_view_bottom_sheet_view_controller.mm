991278e1e8b37233784dd99e663d9afec066cc29
Daniel White <danieltwhite@google.com>
2024-10-26 00:44:03 +0000

    [iOS] Deprecate didTraitCollectionChange in TableViewBottomSheetVC
    Since didTraitCollectionChange is deprecated in iOS 17,
    TableViewBottomSheetVC was updated to use the new alternative system,
    registerForTraitChanges.
    
    Fixed: 349389661
    Change-Id: Iffaa11c9d2565c580a7155fbb0f2b9f3f1080a11
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5908349
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374217}

--- ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller.mm
+++ ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller.mm
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller.h"
 
 #import <Foundation/Foundation.h>
+
 #import <optional>
 #import <ostream>
 
 #import "base/check.h"
 #import "base/notreached.h"
 #import "ios/chrome/browser/shared/ui/bottom_sheet/table_view_bottom_sheet_view_controller+subclassing.h"
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/common/ui/table_view/table_view_cells_constants.h"
 
 namespace {
@@ -174,6 +176,13 @@ NSString* const kCustomDetentIdentifier = @"customDetent";
 
   // Set selection to the first one.
   [self selectFirstRow];
+
+  if (@available(iOS 17, *)) {
+    NSArray<UITrait>* traits = TraitCollectionSetForTraits(
+        @[ UITraitPreferredContentSizeCategory.class ]);
+    [self registerForTraitChanges:traits
+                       withAction:@selector(updateHeightOnTraitChange)];
+  }
 }
 
 - (void)viewIsAppearing:(BOOL)animated {
@@ -184,15 +193,19 @@ NSString* const kCustomDetentIdentifier = @"customDetent";
   [self updateHeight];
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
 
   if (self.traitCollection.preferredContentSizeCategory !=
       previousTraitCollection.preferredContentSizeCategory) {
-    _minimizedStateHeight = std::nullopt;
-    [self updateHeight];
+    [self updateHeightOnTraitChange];
   }
 }
+#endif
 
 #pragma mark - UITableViewDelegate
 
@@ -376,4 +389,12 @@ NSString* const kCustomDetentIdentifier = @"customDetent";
   }
   return _minimizedStateHeight.value();
 }
+
+// Updates the view's height properties when UITraitPreferredContentSizeCategory
+// is modified.
+- (void)updateHeightOnTraitChange {
+  _minimizedStateHeight = std::nullopt;
+  [self updateHeight];
+}
+
 @end
