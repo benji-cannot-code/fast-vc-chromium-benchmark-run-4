9d0006f02bcd670aafd2987fb6b9fd2082c5baee
Xiaoqian Dai <xdai@chromium.org>
2024-10-26 00:35:29 +0000

    float: fix crash caused by recursive bounds update
    It has the same root cause as b:369317333, but with a different code
    path that can trigger the recursive bounds update.
    
    This CL prevents recursive bounds update when updating bounds for a
    floated window.
    
    Bug: b:373340378
    Change-Id: I80be2804ebfdb85d1af4dc3ab118ba113c0af986
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5957729
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374211}

--- ash/wm/float/float_controller.cc
+++ ash/wm/float/float_controller.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/wm_event.h"
 #include "ash/wm/workspace/workspace_event_handler.h"
 #include "ash/wm/workspace/workspace_layout_manager.h"
+#include "base/auto_reset.h"
 #include "base/check.h"
 #include "base/check_op.h"
 #include "base/memory/raw_ptr.h"
@@ -493,6 +494,15 @@ class FloatController::FloatedWindowInfo : public aura::WindowObserver,
     }
 
     if (Shell::Get()->IsInTabletMode()) {
+      // Prevent recursive bounds update calls. The
+      // `UpdateWindowBoundsForTablet` can trigger widget minimum size change
+      // which then trigger the call of `OnWidgetSizeConstraintsChanged`, which
+      // then calls into this function again.
+      if (in_bounds_update_) {
+        return;
+      }
+
+      base::AutoReset<bool> resetter(&in_bounds_update_, true);
       UpdateWindowBoundsForTablet(
           floated_window_, WindowState::BoundsChangeAnimationType::kNone);
     }
@@ -541,6 +551,8 @@ class FloatController::FloatedWindowInfo : public aura::WindowObserver,
 
   gfx::Size last_minimum_size_;
   gfx::Size last_maximum_size_;
+
+  bool in_bounds_update_ = false;
 
   base::WeakPtrFactory<FloatedWindowInfo> weak_ptr_factory_{this};
 };
