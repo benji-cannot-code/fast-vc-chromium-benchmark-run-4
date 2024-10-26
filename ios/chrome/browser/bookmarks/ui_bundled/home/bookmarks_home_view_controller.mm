fe0c016a68e49076de54887cfcbc77402eec1aa3
Daniel White <danieltwhite@google.com>
2024-10-26 00:53:04 +0000

    [iOS] Deprecate didTraitCollectionChange in BookmarksHomeVC
    Since didTraitCollectionChange is deprecated in iOS 17, BookmarksHomeVC
    was updated to use the new alternative system, registerForTraitChanges.
    
    Fixed: 349393935
    Change-Id: I8c4ed803294c339f2af26019c355350b70701eee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905455
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374222}

--- ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
+++ ios/chrome/browser/bookmarks/ui_bundled/home/bookmarks_home_view_controller.mm
@@ -424,6 +424,11 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
   } else {
     [self showLoadingSpinnerBackground];
   }
+
+  if (@available(iOS 17, *)) {
+    [self registerForTraitChanges:TraitCollectionSetForTraits(nil)
+                       withAction:@selector(stopEdittingBookmarkOnTraitChange)];
+  }
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -490,11 +495,16 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
   return NO;
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  // Stop edit of current bookmark folder name, if any.
-  [self.editingFolderCell stopEdit];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
+  [self stopEdittingBookmarkOnTraitChange];
 }
+#endif
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
   return UIStatusBarStyleDefault;
@@ -1204,6 +1214,11 @@ BookmarkNodeIDSet GetBookmarkNodeIDSet(
 
 - (BOOL)isAtTopOfNavigation {
   return (self.navigationController.topViewController == self);
+}
+
+// Stop edit of current bookmark folder name, if any.
+- (void)stopEdittingBookmarkOnTraitChange {
+  [self.editingFolderCell stopEdit];
 }
 
 #pragma mark - BookmarkTableCellTitleEditDelegate
