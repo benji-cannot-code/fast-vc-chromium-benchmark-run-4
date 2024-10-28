454f725108d9b5e6ef1fa9c1271c9861eaa061ae
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-10-28 22:33:40 +0000

    Convert test_transferToGPUTexture_usage_flags to an async function
    This function was manually chaining promises to sequence different test
    variant executions one after the other. This is much more simple to do
    with the async/await semantic.
    
    Bug: 374263392
    Change-Id: I5db28e3ddefeea7f9d7660cb55500440b6c51170
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960864
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374929}

--- third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_transfer/webgpu-helpers.js
+++ third_party/blink/web_tests/wpt_internal/webgpu/canvas_webgpu_transfer/webgpu-helpers.js
@@ -166,19 +166,16 @@ function createBindGroupUsingTexture(device, tex) {
  * transferToGPUTexture() should create a texture which honors the requested
  * usage flags.
  */
-function test_transferToGPUTexture_usage_flags(adapter, adapterInfo, device,
-                                           canvas) {
-  // Create a base-case promise that just resolves immediately.
-  let promise = new Promise((resolve, reject) => { resolve(true); });
-
+async function test_transferToGPUTexture_usage_flags(
+    adapter, adapterInfo, device, canvas) {
   // Skip this test on Mac Swiftshader due to "Invalid Texture" errors.
   if (isMacSwiftShader(adapterInfo)) {
-    return promise;
+    return;
   }
 
   // Declare a helper function which tests individual usage flags.
-  const testOneUsageFlag = function(device, canvas, usageToEnable,
-                                    usageToTest) {
+  const testOneUsageFlag = async function(
+      device, canvas, usageToEnable, usageToTest) {
     const ctx = canvas.getContext('2d');
     const tex = ctx.transferToGPUTexture({device: device,
                                            usage: usageToEnable});
@@ -203,16 +200,15 @@ function test_transferToGPUTexture_usage_flags(adapter, adapterInfo, device,
       clearTextureToColor(device, tex, { r: 1.0, g: 1.0, b: 1.0, a: 1.0 });
     }
 
-    return device.popErrorScope().then((errors) => {
-      if (usageToTest == usageToEnable) {
-        assert_equals(errors, null, 'enabled ' + usageToEnable +
-                                    ', tested ' + usageToTest);
-      } else {
-        assert_not_equals(errors, null, 'enabled ' + usageToEnable +
-                                        ', tested ' + usageToTest);
-      }
-      ctx.transferBackFromGPUTexture();
-    });
+    const errors = await device.popErrorScope();
+    if (usageToTest == usageToEnable) {
+      assert_equals(errors, null, 'enabled ' + usageToEnable +
+                                  ', tested ' + usageToTest);
+    } else {
+      assert_not_equals(errors, null, 'enabled ' + usageToEnable +
+                                      ', tested ' + usageToTest);
+    }
+    ctx.transferBackFromGPUTexture();
   };
 
   // Build up a chain of promises which test each TextureUsage flag.
@@ -223,13 +219,8 @@ function test_transferToGPUTexture_usage_flags(adapter, adapterInfo, device,
 
   for (const usageToEnable of supportedUsageFlags) {
     for (const usageToTest of supportedUsageFlags) {
-      promise = promise.then(() => {
-        return with_webgpu((adapter, adapterInfo, device) => {
-          return testOneUsageFlag(device, canvas, usageToEnable, usageToTest);
-        });
-      });
+      const device = await getWebGPUDevice();
+      await testOneUsageFlag(device, canvas, usageToEnable, usageToTest);
     }
   }
-
-  return promise;
 }
