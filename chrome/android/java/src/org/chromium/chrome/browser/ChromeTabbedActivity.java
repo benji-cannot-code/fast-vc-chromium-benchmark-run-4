91b3dcd01dd444f32368b0e5268ce270ed025d13
ssid <ssid@chromium.org>
2024-10-28 16:56:53 +0000

    [data sharing] Use the new data sharing ui Android APIs
    Remove calls to the old UI, and directly add / update members for
    short term testing. Put the new UI behind a new feature flag.
    
    Change-Id: I0409d0751694f6dd89cd7849231f31e66bc3c64e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960118
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374682}

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeTabbedActivity.java
@@ -3797,7 +3797,7 @@ public class ChromeTabbedActivity extends ChromeActivity<ChromeActivityComponent
                                 intent, DataSharingNotificationManager.DATA_SHARING_EXTRA));
         Runnable showJoinFlowRunnable =
                 () -> {
-                    mRootUiCoordinator.getDataSharingTabManager().initiateJoinFlow(url);
+                    mRootUiCoordinator.getDataSharingTabManager().initiateJoinFlow(this, url);
                 };
 
         OneshotSupplier<TabModelSelector> wrappedSelector =
