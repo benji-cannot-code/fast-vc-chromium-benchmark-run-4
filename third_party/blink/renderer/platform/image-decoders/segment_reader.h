83c82b2a4d732ddcfb9a742c085b66817c7e2358
Fredrik Söderquist <fs@opera.com>
2024-10-28 19:51:03 +0000

    spanification: Add span-based SegmentReader::GetSomeData() overload
    Add a transitional helper for replacing the pointer+size GetSomeData().
    Convert users to the span-based version.
    
    Bug: 351564777
    Change-Id: Icf3b0c5f9222d8c4d5eea499bbae3ce51cc320d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961249
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374816}

--- third_party/blink/renderer/platform/image-decoders/segment_reader.h
+++ third_party/blink/renderer/platform/image-decoders/segment_reader.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef THIRD_PARTY_BLINK_RENDERER_PLATFORM_IMAGE_DECODERS_SEGMENT_READER_H_
 #define THIRD_PARTY_BLINK_RENDERER_PLATFORM_IMAGE_DECODERS_SEGMENT_READER_H_
 
+#include <stdint.h>
+
+#include "base/containers/span.h"
 #include "base/memory/scoped_refptr.h"
 #include "third_party/blink/renderer/platform/image-decoders/rw_buffer.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
@@ -48,6 +51,10 @@ class PLATFORM_EXPORT SegmentReader
   SegmentReader& operator=(const SegmentReader&) = delete;
   virtual size_t size() const = 0;
   virtual size_t GetSomeData(const char*& data, size_t position) const = 0;
+  // Returns a span of however much data is left in the segment containing the
+  // `position`. If there's no data at the specified `position`, an empty span
+  // is returned.
+  base::span<const uint8_t> GetSomeData(size_t position) const;
   virtual sk_sp<SkData> GetAsSkData() const = 0;
   virtual void LockData() {}
   virtual void UnlockData() {}
