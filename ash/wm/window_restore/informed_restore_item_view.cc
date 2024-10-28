80730b87162742ab35c6df83e2e37b6df3f240f7
Sammie Quon <sammiequon@chromium.org>
2024-10-28 22:19:52 +0000

    informed restore: Fix sizing for list items
    They aren't sized correct unless there is an overflow item as well.
    
    Test: manual
    Change-Id: Ice2e269c4ed2f96921f6d31bd5621b3099bfbc8a
    Fixed: 376096171
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973297
    Reviewed-by: Elijah Hewer <hewer@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374925}

--- ash/wm/window_restore/informed_restore_item_view.cc
+++ ash/wm/window_restore/informed_restore_item_view.cc
@@ -83,8 +83,10 @@ InformedRestoreItemView::InformedRestoreItemView(
             .SetBetweenChildSpacing(informed_restore::kScreenshotFaviconSpacing)
             .Build());
   } else {
+    views::BoxLayoutView* inner_box;
     AddChildView(
         views::Builder<views::BoxLayoutView>()
+            .CopyAddressTo(&inner_box)
             .SetOrientation(views::BoxLayout::Orientation::kVertical)
             .SetCrossAxisAlignment(views::BoxLayout::CrossAxisAlignment::kStart)
             .SetBetweenChildSpacing(kTitleFaviconSpacing)
@@ -112,6 +114,7 @@ InformedRestoreItemView::InformedRestoreItemView(
                         views::BoxLayout::CrossAxisAlignment::kCenter)
                     .SetBetweenChildSpacing(kBetweenFaviconSpacing))
             .Build());
+    SetFlexForView(inner_box, 1);
   }
 
   std::vector<GURL> favicons;
