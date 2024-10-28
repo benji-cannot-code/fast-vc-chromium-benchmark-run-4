8e6aea1e55bf7a94d8d79e41c4aeadb7258bf97f
Aashna Sheth <aashnas@google.com>
2024-10-28 15:55:40 +0000

    [N&C] Remove LINT bounds
    This CL removes the LINT bounds in the privacy sandbox service and
    notice service. We re-evaluated conditions under which both services
    are needed. Privacy sandbox service needs to be created for a variety
    of profiles, but the notice service only needs to be created for
    regular profiles.
    
    Bug: b:372479795
    
    Change-Id: Ia094447edf78e328590ab4f0ffd2aad6975ee56e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5921317
    Commit-Queue: Aashna Sheth <aashnas@google.com>
    Reviewed-by: Christine Ying <chrstne@google.com>
    Reviewed-by: Abe Boujane <boujane@google.com>
    Cr-Commit-Position: refs/heads/main@{#1374642}

--- chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.cc
+++ chrome/browser/privacy_sandbox/privacy_sandbox_service_factory.cc
@@ -57,7 +57,6 @@ PrivacySandboxService* PrivacySandboxServiceFactory::GetForProfile(
       GetInstance()->GetServiceForBrowserContext(profile, true));
 }
 
-// LINT.IfChange(PrivacySandboxService)
 PrivacySandboxServiceFactory::PrivacySandboxServiceFactory()
     : ProfileKeyedServiceFactory(
           "PrivacySandboxService",
@@ -85,7 +84,6 @@ PrivacySandboxServiceFactory::PrivacySandboxServiceFactory()
   // eligibility.
   DependsOn(tpcd::experiment::EligibilityServiceFactory::GetInstance());
 }
-// LINT.ThenChange(/chrome/browser/privacy_sandbox/privacy_sandbox_notice_service_factory.cc:PrivacySandboxNoticeService)
 
 std::unique_ptr<KeyedService>
 PrivacySandboxServiceFactory::BuildServiceInstanceForBrowserContext(
