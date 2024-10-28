416b828ab5cc86d74fefb7a0e2a140e304ac1388
Andrew Rayskiy <greengrape@google.com>
2024-10-28 19:15:35 +0000

    iwa: Harden the ApplyUpdate command
    This CL attempts to enforce an invariant that the pending update version
    is always greater or equal than the current version. This is
    meticulously checked in the PrepareStoreUpdate command, and as such
    the assumptions in the ApplyUpdate command can be replaced with CHECK-s
    instead of graceful failures.
    
    Bug: 374933857
    Change-Id: Ia5c0b901a660c1e742181cfc8889bf5434fe1b9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952221
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Robbie McElrath <rmcelrath@chromium.org>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1374791}

--- chrome/browser/web_applications/isolated_web_apps/isolated_web_app_apply_update_command.h
+++ chrome/browser/web_applications/isolated_web_apps/isolated_web_app_apply_update_command.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/commands/web_app_command.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_install_command_helper.h"
 #include "chrome/browser/web_applications/isolated_web_apps/isolated_web_app_url_info.h"
+#include "chrome/browser/web_applications/isolated_web_apps/isolation_data.h"
 #include "chrome/browser/web_applications/locks/app_lock.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/browser/web_applications/web_app_install_info.h"
@@ -118,6 +119,8 @@ class IsolatedWebAppApplyUpdateCommand
 
   void CheckTrustAndSignatures(base::OnceClosure next_step_callback);
 
+  void HandleKeyRotationIfNecessary(base::OnceClosure next_step_callback);
+
   void CreateStoragePartition(base::OnceClosure next_step_callback);
 
   void LoadInstallUrl(base::OnceClosure next_step_callback);
@@ -140,6 +143,13 @@ class IsolatedWebAppApplyUpdateCommand
 
   void CleanupOnFailure(base::OnceClosure next_step_callback);
 
+  // These fields are set in `CheckIfUpdateIsStillPending()`. Calling them
+  // before that will trigger a CHECK-fail.
+  const IsolationData& isolation_data() const { return *isolation_data_; }
+  const IsolationData::PendingUpdateInfo pending_update_info() const {
+    return *isolation_data_->pending_update_info();
+  }
+
   SEQUENCE_CHECKER(sequence_checker_);
 
   std::unique_ptr<AppLock> lock_;
@@ -152,7 +162,7 @@ class IsolatedWebAppApplyUpdateCommand
   const std::unique_ptr<ScopedKeepAlive> optional_keep_alive_;
   const std::unique_ptr<ScopedProfileKeepAlive> optional_profile_keep_alive_;
 
-  std::optional<IsolationData::PendingUpdateInfo> pending_update_info_;
+  std::optional<IsolationData> isolation_data_;
 
   std::unique_ptr<IsolatedWebAppInstallCommandHelper> command_helper_;
 
