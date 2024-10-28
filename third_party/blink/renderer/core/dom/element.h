1ab5c51c5d3bf5f3a5cbfed6ccccd945223be0da
Daniel Vogelheim <vogelheim@chromium.org>
2024-10-28 16:37:28 +0000

    [Sanitizer] Update Sanitizer IDL from current specification.
    This updates the Sanitizer IDL from the current specification draft.
    It also establishes a basic framework to use it, but doesn't actually
    implement any sanitization logic. So this basically implements the
    Framework chapter of the spec, but not the Algorithms part.
    
    The actual sanitization logic will follow shortly. I split this
    out in order to keep the CLs smaller & reviewable.
    
    Spec + IDL ref: https://wicg.github.io/sanitizer-api/#framework
    
    Bug: 356601280
    Change-Id: I55a72af1241a840d9ba2ce0d85bb03ca19754b27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5720931
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374671}

--- third_party/blink/renderer/core/dom/element.h
+++ third_party/blink/renderer/core/dom/element.h
@@ -117,6 +117,7 @@ class ResizeObserverSize;
 class ScrollIntoViewOptions;
 class CheckVisibilityOptions;
 class ScrollToOptions;
+class SetHTMLOptions;
 class ShadowRoot;
 class ShadowRootInit;
 class SpaceSplitString;
@@ -255,6 +256,13 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
   enum class ForceHtml {
     kDontForce = 0,
     kForce = 1,
+  };
+  // SanitizeHTML specifies whether the HTML parser should call into the HTML
+  // sanitizer API, and if so whether in safe or unsafe mode.
+  enum class SanitizeHtml {
+    kDont,
+    kSanitizeSafe,
+    kSanitizeUnsafe,
   };
 
   // Animatable implementation.
@@ -1094,6 +1102,8 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
   // argument to set Sanitizer parameters.
   // See https://github.com/whatwg/html/pull/9538.
   void setHTMLUnsafe(const String& html, ExceptionState& = ASSERT_NO_EXCEPTION);
+  void setHTMLUnsafe(const String& html, SetHTMLOptions*, ExceptionState&);
+  void setHTML(const String& html, SetHTMLOptions*, ExceptionState&);
 
   void setPointerCapture(PointerId poinetr_id, ExceptionState&);
   void releasePointerCapture(PointerId pointer_id, ExceptionState&);
@@ -1919,6 +1929,8 @@ class CORE_EXPORT Element : public ContainerNode, public Animatable {
       const String&,
       ParseDeclarativeShadowRoots parse_declarative_shadows,
       ForceHtml force_html_over_xml,
+      SanitizeHtml sanitize_html,
+      SetHTMLOptions* set_html_options,
       ExceptionState&);
 
   ElementRareDataVector* GetElementRareData() const;
---
+++ third_party/blink/renderer/core/sanitizer/sanitizer.cc
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sanitizer.h"
+
+namespace blink {
+
+Sanitizer* Sanitizer::Create(ExecutionContext* execution_context,
+                             const SanitizerConfig* sanitizer_config,
+                             ExceptionState& exception_state) {
+  Sanitizer* sanitizer = MakeGarbageCollected<Sanitizer>();
+  return sanitizer;
+}
+
+void Sanitizer::allowElement(
+    const V8UnionSanitizerElementNamespaceWithAttributesOrString* element) {}
+
+void Sanitizer::removeElement(
+    const V8UnionSanitizerElementNamespaceOrString* element) {}
+
+void Sanitizer::replaceWithChildrenElement(
+    const V8UnionSanitizerElementNamespaceOrString* element) {}
+
+void Sanitizer::allowAttribute(
+    const V8UnionSanitizerAttributeNamespaceOrString* attribute) {}
+
+void Sanitizer::removeAttribute(
+    const V8UnionSanitizerAttributeNamespaceOrString* attribute) {}
+
+void Sanitizer::setComments(bool comments) {}
+
+void Sanitizer::setDataAttributes(bool data_attributes) {}
+
+void Sanitizer::removeUnsafe() {}
+
+SanitizerConfig* Sanitizer::get() const {
+  return nullptr;
+}
+
+}  // namespace blink
