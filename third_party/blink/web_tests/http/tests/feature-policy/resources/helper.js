e1152844d53118a4b72c1733e0e737f1463b1e7a
Siye Liu <siliu@microsoft.com>
2024-10-28 21:05:17 +0000

    [Permissions Policy] update default for `focus-without-user-activation`
    According to discussion[1] in webappsec WG and WHATWG, we should set
    the default of `focus-without-user-activation` permissions policy to
    `EnableForSelf`.
    
    [1]: https://github.com/w3c/webappsec-permissions-policy/issues/273#issuecomment-2384287101
    
    Bug: 371112534
    Change-Id: I7deb757d0074e1262d07f768e58ea53a99d5faa5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5950528
    Reviewed-by: Sanket Joshi <sajos@microsoft.com>
    Commit-Queue: Siye Liu <siliu@microsoft.com>
    Reviewed-by: Ian Clelland <iclelland@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374868}

--- third_party/blink/web_tests/http/tests/feature-policy/resources/helper.js
+++ third_party/blink/web_tests/http/tests/feature-policy/resources/helper.js
@@ -7,3 +7,7 @@ function loadIframes(srcs) {
     }
   }
 }
+
+function hasAllowAttributeWithValue(frame, feature) {
+  return frame.hasAttribute('allow') && frame.getAttribute('allow').includes(feature);
+}
