f25b65d2be1f8d0da504625d9a257bcac89e7643
Kent Tamura <tkent@chromium.org>
2024-10-28 04:17:37 +0000

    sideways: Basic support for SVG <text>
    Text in the sideways-lr writing mode should grow upward from "current
    text position".  This CL supports `x` `y` `dx` `dy` and `text-anchor`
    for the sideways-lr writing mode.
    
    `textLength` and `<textPath>` will be supported later.
    
    
    Bug: 40501131
    Change-Id: If67215f59b9b623a92a06cbc56457a323362255e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963882
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374467}

--- third_party/blink/renderer/core/layout/svg/svg_text_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/svg/svg_text_layout_algorithm.h
@@ -44,6 +44,15 @@ class SvgTextLayoutAlgorithm {
   PhysicalSize WriteBackToFragmentItems(
       FragmentItemsBuilder::ItemWithOffsetList& items);
 
+  bool IsHorizontal() const {
+    return inline_direction_ == PhysicalDirection::kRight;
+  }
+  bool IsVerticalDownward() const {
+    return inline_direction_ == PhysicalDirection::kDown;
+  }
+  bool IsVerticalUpward() const {
+    return inline_direction_ == PhysicalDirection::kUp;
+  }
   float ScalingFactorAt(const FragmentItemsBuilder::ItemWithOffsetList& items,
                         wtf_size_t addressable_index) const;
   bool IsFirstCharacterInTextPath(wtf_size_t index) const;
@@ -55,7 +64,10 @@ class SvgTextLayoutAlgorithm {
   wtf_size_t addressable_count_;
 
   // "horizontal" flag defined in the specification.
+  // This should be replaced with `inline_direction_`.
   bool horizontal_;
+  // A replacement of the "horizontal" flag above.
+  const PhysicalDirection inline_direction_;
 
   struct SvgPerCharacterInfo {
     std::optional<float> x;
