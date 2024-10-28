0b74727678c05af034333bfd74300661eedfa301
Kenichi Ishibashi <bashi@chromium.org>
2024-10-28 11:16:47 +0000

    HttpStreamPool: Use base::EnumSet for expected application protocols
    This CL introduces NextProtoSet that represents a set of NextProto
    (effectively ALPN). Use it to represent allowed ALPNs for
    HttpStreamPool::Job. In subsequent CLs, I plan to use it to
    restrict H1/H2/H3 connection attempts in AttemptManager to support
    QUIC Alt-Svc destinations.
    
    No behavior changes.
    
    Bug: 346835898
    Change-Id: I0c282a10a54f76f626f8175ef9ab65a21db35df1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5970173
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374552}

--- net/socket/next_proto.h
+++ net/socket/next_proto.h
@@ -6,16 +6,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_SOCKET_NEXT_PROTO_H_
 #define NET_SOCKET_NEXT_PROTO_H_
 
+#include <stdint.h>
+
 #include <string_view>
 #include <vector>
 
+#include "base/containers/enum_set.h"
 #include "net/base/net_export.h"
 
 namespace net {
 
 // This enum is used in Net.SSLNegotiatedAlpnProtocol histogram.
 // Do not change or re-use values.
-enum NextProto {
+enum NextProto : uint8_t {
   kProtoUnknown = 0,
   kProtoHTTP11 = 1,
   kProtoHTTP2 = 2,
@@ -25,6 +28,9 @@ enum NextProto {
 
 // List of protocols to use for ALPN, used for configuring HttpNetworkSessions.
 typedef std::vector<NextProto> NextProtoVector;
+
+using NextProtoSet =
+    base::EnumSet<NextProto, NextProto::kProtoUnknown, NextProto::kProtoLast>;
 
 NET_EXPORT_PRIVATE NextProto NextProtoFromString(std::string_view proto_string);
 
