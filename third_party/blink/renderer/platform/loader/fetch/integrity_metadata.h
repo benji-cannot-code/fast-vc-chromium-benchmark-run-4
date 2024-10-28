c8dcdc339f05b5276307fc47919f26135f6114cb
Mike West <mkwst@chromium.org>
2024-10-28 17:03:06 +0000

    Clean up integrity checking code.
    We check a resource's integrity disposition in a few places, with
    somewhat inconsistent formatting (checking "!passed" vs "failed",
    for instance). To make it simpler to add some functionality here,
    this patch makes a few small changes to align these callsites,
    and centralize the decision about whether or not to force
    integrity checks for cached/prefetched resources.
    
    No change in behavior is intended; the checks should have the same
    outcomes as in the status quo.
    
    Bug: 375343417
    Change-Id: Iea10d3f730cc60359790070fcb06ce62acb97f48
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5965057
    Reviewed-by: Yoav Weiss (@Shopify) <yoavweiss@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374688}

--- third_party/blink/renderer/platform/loader/fetch/integrity_metadata.h
+++ third_party/blink/renderer/platform/loader/fetch/integrity_metadata.h
@@ -46,7 +46,8 @@ class PLATFORM_EXPORT IntegrityMetadata {
 
 enum class ResourceIntegrityDisposition : uint8_t {
   kNotChecked = 0,
-  kFailed,
+  kNetworkError,
+  kFailedIntegrityMetadata,
   kPassed
 };
 
