7091c9ece9945a31875e3e8893b8d7f6017d95bc
Shakti Sahu <shaktisahu@chromium.org>
2024-10-28 19:34:40 +0000

    [TG Sync] Add some CHECKs based on platform implementation
    This CL adds some null checks based on whether coordinator
    exists or not.
    
    Bug: None
    Change-Id: I9dd01dcb7be93a891027a2c8e3e1d7f58c4c6718
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967378
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374810}

--- components/saved_tab_groups/internal/tab_group_sync_service_impl.cc
+++ components/saved_tab_groups/internal/tab_group_sync_service_impl.cc
@@ -43,6 +43,10 @@ namespace tab_groups {
 namespace {
 constexpr base::TimeDelta kDelayBeforeMetricsLogged = base::Seconds(10);
 
+constexpr bool is_android = !!BUILDFLAG(IS_ANDROID);
+constexpr bool is_ios = !!BUILDFLAG(IS_IOS);
+constexpr bool is_desktop = !(is_android || is_ios);
+
 void OnCanApplyOptimizationCompleted(
     TabGroupSyncService::UrlRestrictionCallback callback,
     optimization_guide::OptimizationGuideDecision decision,
@@ -134,6 +138,7 @@ void TabGroupSyncServiceImpl::SetCoordinator(
 
 std::unique_ptr<ScopedLocalObservationPauser>
 TabGroupSyncServiceImpl::CreateScopedLocalObserverPauser() {
+  CHECK(coordinator_);
   return coordinator_->CreateScopedLocalObserverPauser();
 }
 
@@ -421,6 +426,7 @@ void TabGroupSyncServiceImpl::OnTabSelected(const LocalTabGroupID& group_id,
 }
 
 void TabGroupSyncServiceImpl::SaveGroup(SavedTabGroup group) {
+  CHECK(is_desktop);
   const base::Uuid sync_id = group.saved_guid();
   const LocalTabGroupID local_id = group.local_group_id().value();
   AddGroup(std::move(group));
@@ -428,6 +434,7 @@ void TabGroupSyncServiceImpl::SaveGroup(SavedTabGroup group) {
 }
 
 void TabGroupSyncServiceImpl::UnsaveGroup(const LocalTabGroupID& local_id) {
+  CHECK(is_desktop);
   std::optional<SavedTabGroup> group = GetGroup(local_id);
   CHECK(group);
   coordinator_->DisconnectLocalTabGroup(local_id);
@@ -496,6 +503,7 @@ std::vector<LocalTabGroupID> TabGroupSyncServiceImpl::GetDeletedGroupIds()
 void TabGroupSyncServiceImpl::OpenTabGroup(
     const base::Uuid& sync_group_id,
     std::unique_ptr<TabGroupActionContext> context) {
+  CHECK(coordinator_);
   VLOG(2) << __func__;
   coordinator_->HandleOpenTabGroupRequest(sync_group_id, std::move(context));
 }
@@ -570,6 +578,7 @@ void TabGroupSyncServiceImpl::ConnectLocalTabGroup(
     const base::Uuid& sync_id,
     const LocalTabGroupID& local_id,
     OpeningSource opening_source) {
+  CHECK(coordinator_);
   if (!is_initialized_) {
     VLOG(2) << __func__ << " Invoked before init";
     pending_actions_.emplace_back(base::BindOnce(
