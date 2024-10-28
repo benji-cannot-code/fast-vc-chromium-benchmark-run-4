0b428c25443a2b2cd43b8e9d3d13f29dca815eb1
Shu Yang <shuyng@google.com>
2024-10-28 17:28:12 +0000

    Do not update tab title on permanent pdf download complete
    Bug: 367821124
    Change-Id: I7dc4f824b3f6ee0e37a6d0bede647e9f9fee223e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958716
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Commit-Queue: Shu Yang <shuyng@google.com>
    Cr-Commit-Position: refs/heads/main@{#1374713}

--- chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/DownloadInfo.java
+++ chrome/browser/download/android/java/src/org/chromium/chrome/browser/download/DownloadInfo.java
@@ -571,7 +571,8 @@ public final class DownloadInfo {
             long timeRemainingInMs,
             long lastAccessTime,
             boolean isDangerous,
-            @FailState int failState) {
+            @FailState int failState,
+            boolean isTransient) {
         String remappedMimeType = MimeUtils.remapGenericMimeType(mimeType, url.getSpec(), fileName);
 
         Progress progress =
@@ -601,6 +602,7 @@ public final class DownloadInfo {
                 .setIsDangerous(isDangerous)
                 .setUrl(url)
                 .setFailState(failState)
+                .setIsTransient(isTransient)
                 .build();
     }
 }
