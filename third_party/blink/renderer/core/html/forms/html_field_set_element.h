6d9bb68722b887ad51b177669810aba2a1e45b96
David Baron <dbaron@chromium.org>
2024-10-28 22:05:55 +0000

    Cache presence of disabled <fieldset> elements on the document.
    This should eliminate work that is about 0.03% of a profile of
    Speedometer3, which probably means a slightly higher percentage of work
    included in the score, but still too small to measure accurately.
    (There's also a little bit of new work.)
    
    Change-Id: I7a4c4affece4e3b0915e4752853a2fb2da16d9fd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954053
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374914}

--- third_party/blink/renderer/core/html/forms/html_field_set_element.h
+++ third_party/blink/renderer/core/html/forms/html_field_set_element.h
@@ -46,6 +46,7 @@ class CORE_EXPORT HTMLFieldSetElement final : public HTMLFormControlElement {
  protected:
   void DisabledAttributeChanged() override;
   void AncestorDisabledStateWasChanged() override;
+  void DidMoveToNewDocument(Document& old_document) override;
 
  private:
   bool IsEnumeratable() const override { return true; }
