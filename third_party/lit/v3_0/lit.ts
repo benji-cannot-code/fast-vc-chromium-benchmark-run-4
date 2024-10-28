671f3e635d06059e4efc09063bccdf5661cee6e0
rbpotter <rbpotter@chromium.org>
2024-10-28 19:15:14 +0000

    Extensions asyncMap(): Re-render for unchanged items list
    Making several updates to asyncMap() based on testing in the
    extensions page.
    (1) Using setTimeout() to trigger the first call to renderInChunks_(),
        such that another chunk is not rendered while the current Lit
        update is still running. Waiting only for an animation frame was
        causing the browser to paint the first few chunks all at the same
        time and delaying the LCP.
    (2) Re-rendering even if the items list is unchanged, to fix a bug
        where changing other reactive properties that impacted the
        rendering of the items but not the items list itself would cause
        the list to be rendered incorrectly.
    (3) When re-rendering, always initially rendering the number of items
        currently rendered + initialCount (or the length of the new list,
        whatever is less). This is to avoid unnecessarily removing items
        from the DOM and rendering only a small chunk of updated items in
        its place. Replacing many items with a small chunk causes a large
        change in the template, which is slower in Lit and causes UI jank
        when the list is updated frequently.
    (4) Removing filtering functionality. Instead, items should be
        filtered by the client as needed, similar to how filtered lists
        rendered with map() are handled.
    
    These changes ensure the performance of the extensions list using
    asyncMap() is slightly better than the current Polymer version on
    ToT.
    
    Bug: 40943652
    Change-Id: I6c306f0d9813dc93d0b46f5000db213ec442ed22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960760
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1374790}

--- third_party/lit/v3_0/lit.ts
+++ third_party/lit/v3_0/lit.ts
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-export {css, CSSResultGroup, html, LitElement, noChange, nothing, render, PropertyValues, TemplateResult} from 'lit/index.js';
-export {directive, DirectiveParameters, PartInfo, PartType} from 'lit-html/directive.js';
-export {AsyncDirective, ChildPart} from 'lit-html/async-directive.js';
+export {css, CSSResultGroup, html, LitElement, nothing, render, PropertyValues, TemplateResult} from 'lit/index.js';
+export {directive, PartInfo, PartType} from 'lit-html/directive.js';
+export {AsyncDirective} from 'lit-html/async-directive.js';
 export {CrLitElement} from './cr_lit_element.js';
