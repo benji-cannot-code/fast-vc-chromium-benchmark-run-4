29a44d62f2c9c41d848c160e0d3c32b2735a5ed3
Karol Sygiet <sygiet@google.com>
2024-10-28 12:31:20 +0000

    [Japan] Add Alternative name types to the autofill table
    This CL enables alternative name related types for saving in the
    autofill table. Those types have to be stored, since their values
    cannot be obtained by formatting / parsing from already existing
    fields.
    
    Bug: 359768803
    Change-Id: Ic693394ca18489bdcb7c4ac4660d1e4132d68aec
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937925
    Commit-Queue: Karol Sygiet <sygiet@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Cr-Commit-Position: refs/heads/main@{#1374564}

--- components/autofill/core/browser/webdata/addresses/address_autofill_table_unittest.cc
+++ components/autofill/core/browser/webdata/addresses/address_autofill_table_unittest.cc
@@ -72,6 +72,8 @@ INSTANTIATE_TEST_SUITE_P(
 // Tests reading/writing name, email, company, address and phone number
 // information.
 TEST_P(AddressAutofillTableProfileTest, AutofillProfile) {
+  base::test::ScopedFeatureList features{
+      features::kAutofillSupportPhoneticNameForJP};
   AutofillProfile home_profile = CreateAutofillProfile();
 
   home_profile.SetRawInfoWithVerificationStatus(NAME_FIRST, u"John",
@@ -148,7 +150,12 @@ TEST_P(AddressAutofillTableProfileTest, AutofillProfile) {
                                                 VerificationStatus::kObserved);
   home_profile.SetRawInfoWithVerificationStatus(
       ADDRESS_HOME_ADMIN_LEVEL2, u"Oxaca", VerificationStatus::kObserved);
-
+  home_profile.SetRawInfoWithVerificationStatus(
+      ALTERNATIVE_FULL_NAME, u"やまだ たろう", VerificationStatus::kObserved);
+  home_profile.SetRawInfoWithVerificationStatus(
+      ALTERNATIVE_GIVEN_NAME, u"たろう", VerificationStatus::kObserved);
+  home_profile.SetRawInfoWithVerificationStatus(
+      ALTERNATIVE_FAMILY_NAME, u"やまだ", VerificationStatus::kObserved);
   home_profile.SetRawInfo(PHONE_HOME_WHOLE_NUMBER, u"18181234567");
   home_profile.set_language_code("en");
 
@@ -283,6 +290,8 @@ TEST_P(AddressAutofillTableProfileTest, UseDates) {
 }
 
 TEST_P(AddressAutofillTableProfileTest, UpdateAutofillProfile) {
+  base::test::ScopedFeatureList features{
+      features::kAutofillSupportPhoneticNameForJP};
   // Add a profile to the db.
   AutofillProfile profile = CreateAutofillProfile();
   profile.SetRawInfo(NAME_FIRST, u"John");
@@ -300,6 +309,9 @@ TEST_P(AddressAutofillTableProfileTest, UpdateAutofillProfile) {
   profile.SetRawInfo(ADDRESS_HOME_LANDMARK, u"Landmark");
   profile.SetRawInfo(ADDRESS_HOME_BETWEEN_STREETS, u"Marcos y Oliva");
   profile.SetRawInfo(PHONE_HOME_WHOLE_NUMBER, u"18181234567");
+  profile.SetRawInfo(ALTERNATIVE_FULL_NAME, u"やまだ たろう");
+  profile.SetRawInfo(ALTERNATIVE_GIVEN_NAME, u"たろう");
+  profile.SetRawInfo(ALTERNATIVE_FAMILY_NAME, u"やまだ");
   profile.set_language_code("en");
   profile.FinalizeAfterImport();
   table_.AddAutofillProfile(profile);
