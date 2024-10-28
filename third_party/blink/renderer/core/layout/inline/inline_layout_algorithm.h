dc8b3441da22098258f411b046380dc01b6c6ac1
Morten Stenshorne <mstensho@chromium.org>
2024-10-28 13:17:49 +0000

    [text-box-trim] Correct detection of intervening border/padding.
    Replace what crrev.com/c/5905670 did. The detection code is now in
    BlockLayoutAlgorithm().
    
    Border / padding at the container that establishes box trimming
    shouldn't be considered to be intervening. Only check border/padding on
    descendants.
    
    Bug: 40254880
    Change-Id: I45b7c28f5dd37df20eb90c377759642fd3b76ed1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960337
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374580}

--- third_party/blink/renderer/core/layout/inline/inline_layout_algorithm.h
+++ third_party/blink/renderer/core/layout/inline/inline_layout_algorithm.h
@@ -73,9 +73,6 @@ class CORE_EXPORT InlineLayoutAlgorithm final
  private:
   friend class LineWidthsTest;
 
-  bool HasContainerBorderPaddingAtBlockStart() const;
-  bool HasContainerBorderPaddingAtBlockEnd() const;
-
   void PositionLeadingFloats(ExclusionSpace&, LeadingFloats&);
   PositionedFloat PositionFloat(LayoutUnit origin_block_bfc_offset,
                                 LayoutObject* floating_object,
