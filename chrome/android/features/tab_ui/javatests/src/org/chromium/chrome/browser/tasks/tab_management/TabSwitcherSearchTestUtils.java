9b9a2d5a84633c012f5171f1c873ab61d8f495b7
Brandon Fong <bjfong@google.com>
2024-10-28 22:15:00 +0000

    [hub] Implement transparency background on the omnibox for hub search
    Add a translucent background upon entry into hub search.
    
    Preview: https://drive.google.com/file/d/116beRJZEfsZLB89x7ARbBeB_gblwI5JW/view?usp=sharing
    
    Bug: 369687073
    Change-Id: Ieb0ffd962a99066b548b666ac3935dc62e01dd74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889880
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1374919}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchTestUtils.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchTestUtils.java
@@ -14,6 +14,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.chromium.chrome.browser.searchwidget.SearchActivity;
+import org.chromium.chrome.browser.searchwidget.SearchActivityTransparentBackground;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.ActivityTestUtils;
@@ -30,7 +31,7 @@ public class TabSwitcherSearchTestUtils {
         SearchActivity searchActivity =
                 ActivityTestUtils.waitForActivity(
                         InstrumentationRegistry.getInstrumentation(),
-                        SearchActivity.class,
+                        SearchActivityTransparentBackground.class,
                         () -> onView(withId(R.id.search_box_text)).perform(click()));
 
         OmniboxTestUtils omniboxTestUtils = new OmniboxTestUtils(searchActivity);
