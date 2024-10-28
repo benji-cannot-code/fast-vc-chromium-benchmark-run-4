76f504379ee36345e202c27aa362b2e8416f777d
Liza Bipin <mlbipin@google.com>
2024-10-28 10:11:20 +0000

    Remove the first time banner from the interstitial
    This CL removes the code used to show the banner from desktop and iOS.
    The pref and enum used to mark if the banner was shown will be deprecated in a follow up CL.
    
    Bug: 375174041
    Change-Id: I84f4c7873f5e26f56f306325de910ab0d235f3ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5956413
    Reviewed-by: Carlos IL <carlosil@chromium.org>
    Reviewed-by: Anthi Orfanou <anthie@google.com>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Auto-Submit: Liza Bipin <mlbipin@google.com>
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1374531}

--- components/supervised_user/core/browser/resources/supervised_user_block_interstitial_v2.js
+++ components/supervised_user/core/browser/resources/supervised_user_block_interstitial_v2.js
@@ -83,10 +83,6 @@ function initialize() {
     }
   }
 
-  const showBanner = loadTimeData.getBoolean('showBanner');
-  if (!showBanner) {
-    $('banner').style.display = 'none';
-  }
 
   const alreadyRequestedAccessRemote =
       loadTimeData.getBoolean('alreadySentRemoteRequest');
