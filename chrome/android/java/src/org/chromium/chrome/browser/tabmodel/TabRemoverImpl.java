3c7ac68377e3b970519823f40b518bc4b233da8f
Calder Kitagawa <ckitagawa@chromium.org>
2024-10-28 21:24:11 +0000

    [Tab Model] Implement TabModelRemover
    Flush out TabModelRemover to have a doTabRemovalFlow method that takes a
    delegate (per operation) and performs operations depending on which if
    any tab groups are destroyed by the operation.
    
    This may include issuing warning dialogs, performing new tab creation
    and ultimately executing the operations.
    
    TabUngrouperImpl and TabRemoverImpl will be updated in subsequent CLs
    to provide appropriate TabModelRemoverFlowDelegate implementations.
    
    Bug: 345854441
    Change-Id: I5737bf1d1b78f5e35fc032cbbae9d86aef66152e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958907
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Auto-Submit: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374884}

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabRemoverImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabRemoverImpl.java
@@ -9,6 +9,7 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.VisibleForTesting;
 
 import org.chromium.base.supplier.Supplier;
 import org.chromium.chrome.browser.tab.Tab;
@@ -16,7 +17,9 @@ import org.chromium.components.browser_ui.widget.ActionConfirmationResult;
 import org.chromium.ui.modaldialog.ModalDialogManager;
 
 /** Implementation of the {@link TabRemover} interface. */
-public class TabRemoverImpl extends TabModelActor implements TabRemover {
+public class TabRemoverImpl implements TabRemover {
+    private final TabModelRemover mTabModelRemover;
+
     /**
      * @param context The activity context.
      * @param modalDialogManager The manager to use for warning dialogs.
@@ -26,7 +29,12 @@ public class TabRemoverImpl extends TabModelActor implements TabRemover {
             @NonNull Context context,
             @NonNull ModalDialogManager modalDialogManager,
             @NonNull Supplier<TabGroupModelFilter> tabGroupModelFilterSupplier) {
-        super(context, modalDialogManager, tabGroupModelFilterSupplier);
+        this(new TabModelRemover(context, modalDialogManager, tabGroupModelFilterSupplier));
+    }
+
+    @VisibleForTesting
+    TabRemoverImpl(@NonNull TabModelRemover tabModelRemover) {
+        mTabModelRemover = tabModelRemover;
     }
 
     @Override
@@ -36,7 +44,8 @@ public class TabRemoverImpl extends TabModelActor implements TabRemover {
             @Nullable TabModelActionListener listener) {
         // TODO(crbug.com/347981662, crbug.com/347981397, crbug.com/345854441): Show a dialog and
         // create NTPs if needed.
-        PassthroughTabRemover.doCloseTabs(getTabGroupModelFilter(), tabClosureParams);
+        PassthroughTabRemover.doCloseTabs(
+                mTabModelRemover.getTabGroupModelFilter(), tabClosureParams);
         if (listener != null) {
             listener.onConfirmationDialogResult(ActionConfirmationResult.IMMEDIATE_CONTINUE);
         }
@@ -44,7 +53,8 @@ public class TabRemoverImpl extends TabModelActor implements TabRemover {
 
     @Override
     public void forceCloseTabs(@NonNull TabClosureParams tabClosureParams) {
-        getTabGroupModelFilter().closeTabs(tabClosureParams);
+        PassthroughTabRemover.doCloseTabs(
+                mTabModelRemover.getTabGroupModelFilter(), tabClosureParams);
     }
 
     @Override
@@ -52,7 +62,8 @@ public class TabRemoverImpl extends TabModelActor implements TabRemover {
             @NonNull Tab tab, boolean allowDialog, @Nullable TabModelActionListener listener) {
         // TODO(crbug.com/347981662, crbug.com/347981397, crbug.com/345854441): Show a dialog and
         // create NTPs if needed.
-        PassthroughTabRemover.doRemoveTab(getTabGroupModelFilter().getTabModel(), tab);
+        PassthroughTabRemover.doRemoveTab(
+                mTabModelRemover.getTabGroupModelFilter().getTabModel(), tab);
         if (listener != null) {
             listener.onConfirmationDialogResult(ActionConfirmationResult.IMMEDIATE_CONTINUE);
         }
