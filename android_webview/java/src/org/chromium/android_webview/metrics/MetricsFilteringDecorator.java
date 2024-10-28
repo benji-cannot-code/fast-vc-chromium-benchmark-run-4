93c82741c2e241826951eadc11647e681f5b83da
Andrew Grieve <agrieve@chromium.org>
2024-10-28 17:12:21 +0000

    Android: Remove usages of Java stream() in android_webview
    Usage is against style guide, and this CL shows size savings from
    avoiding them (and arguable more readable in many cases).
    
    Also adds a @JniType usage to avoid the need for an array.
    
    Bug: 344943957
    Change-Id: Ib9ca79921c87f290d30c38d99c7b94aa13e944f0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967296
    Auto-Submit: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374700}

--- android_webview/java/src/org/chromium/android_webview/metrics/MetricsFilteringDecorator.java
+++ android_webview/java/src/org/chromium/android_webview/metrics/MetricsFilteringDecorator.java
@@ -13,8 +13,8 @@ import org.chromium.components.metrics.ChromeUserMetricsExtensionProtos.ChromeUs
 import org.chromium.components.metrics.HistogramEventProtos.HistogramEventProto;
 import org.chromium.components.metrics.SystemProfileProtos.SystemProfileProto.MetricsFilteringStatus;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /** Applies metrics filtering before forwarding to the base log uploader. */
 public class MetricsFilteringDecorator implements AndroidMetricsLogConsumer {
@@ -24,7 +24,7 @@ public class MetricsFilteringDecorator implements AndroidMetricsLogConsumer {
 
     public MetricsFilteringDecorator(AndroidMetricsLogConsumer uploader) {
         mLogUploader = uploader;
-        mHistogramsAllowlist = new HistogramsAllowlist();
+        mHistogramsAllowlist = HistogramsAllowlist.load();
     }
 
     @Override
@@ -43,10 +43,12 @@ public class MetricsFilteringDecorator implements AndroidMetricsLogConsumer {
         try {
             ChromeUserMetricsExtension proto = ChromeUserMetricsExtension.parseFrom(data);
             if (shouldApplyMetricsFiltering(proto)) {
-                List<HistogramEventProto> filteredHistograms =
-                        proto.getHistogramEventList().stream()
-                                .filter(mHistogramsAllowlist::contains)
-                                .collect(Collectors.toList());
+                List<HistogramEventProto> filteredHistograms = new ArrayList<>();
+                for (HistogramEventProto event : proto.getHistogramEventList()) {
+                    if (mHistogramsAllowlist.contains(event)) {
+                        filteredHistograms.add(event);
+                    }
+                }
                 ChromeUserMetricsExtension.Builder builder = proto.toBuilder();
                 builder.clearUserActionEvent()
                         .clearHistogramEvent()
