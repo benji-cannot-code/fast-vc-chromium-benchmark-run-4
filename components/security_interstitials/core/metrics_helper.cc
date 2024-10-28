46a6e200ff3e4e4168286dd2da572c38c0b34756
Sorin Jianu <sorin@chromium.org>
2024-10-28 19:20:37 +0000

    components: default the user-defined empty ctor and dtor where possible
    I need an owner's approval please for changes under:
    components/security_interstitials
    
    This is a mechanical change. There are a few instances (but not all)
    of empty user-defined constructors and destructors which can be
    defaulted in the /components code.
    
    A few of these have been missed by previous CLs and are being changed
    now.
    
    This CL was uploaded by git cl split.
    
    R=nparker@chromium.org
    
    Bug: 374110115
    Change-Id: I1b88034ad164106d0e1073fe89e4026fe6a76396
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962677
    Commit-Queue: Nathan Parker <nparker@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Auto-Submit: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1374796}

--- components/security_interstitials/core/metrics_helper.cc
+++ components/security_interstitials/core/metrics_helper.cc
@@ -140,12 +140,12 @@ void MaybeRecordInteractionAsAction(MetricsHelper::Interaction interaction,
 
 MetricsHelper::~MetricsHelper() = default;
 
-MetricsHelper::ReportDetails::ReportDetails() {}
+MetricsHelper::ReportDetails::ReportDetails() = default;
 
 MetricsHelper::ReportDetails::ReportDetails(const ReportDetails& other) =
     default;
 
-MetricsHelper::ReportDetails::~ReportDetails() {}
+MetricsHelper::ReportDetails::~ReportDetails() = default;
 
 MetricsHelper::MetricsHelper(const GURL& request_url,
                              const ReportDetails settings,
