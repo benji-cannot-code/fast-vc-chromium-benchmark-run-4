d5fcb66993be0b4790c2deb18d93697d2b5fba27
Colin Blundell <blundell@chromium.org>
2024-10-29 17:41:44 +0000

    [Blink] Eliminate references to Canvas2DLayerBridge in test comments
    Canvas2DLayerBridge is now an implementation detail in the process of
    being eliminated.
    
    Bug: 40280152
    Change-Id: Ide817d44ccfa7c75b49abb1475fac2de52386c02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975142
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375364}

--- third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
+++ third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d_test.cc
@@ -611,8 +611,7 @@ TEST_P(CanvasRenderingContext2DTest,
   CreateContext(kNonOpaque);
   EXPECT_FALSE(!!CanvasElement().RateLimiter());
 
-  // Install a Canvas2DLayerBridge instance to make the canvas paintable and a
-  // CanvasResourceProvider that does not support direct compositing.
+  // Install a CanvasResourceProvider that does not support direct compositing.
   gfx::Size size = CanvasElement().Size();
   auto provider = std::make_unique<FakeCanvasResourceProvider>(
       SkImageInfo::MakeN32Premul(size.width(), size.height()),
@@ -636,9 +635,8 @@ TEST_P(CanvasRenderingContext2DTest,
   CreateContext(kNonOpaque);
   EXPECT_FALSE(!!CanvasElement().RateLimiter());
 
-  // Install a Canvas2DLayerBridge instance to make the canvas paintable and a
-  // CanvasResourceProvider that supports direct compositing to make the canvas
-  // composited.
+  // Install a CanvasResourceProvider that supports direct compositing to make
+  // the canvas composited.
   gfx::Size size = CanvasElement().Size();
   auto provider = std::make_unique<FakeCanvasResourceProvider>(
       SkImageInfo::MakeN32Premul(size.width(), size.height()),
@@ -660,9 +658,8 @@ TEST_P(CanvasRenderingContext2DTest, HidingCanvasTurnsOffRateLimiting) {
   CreateContext(kNonOpaque);
   EXPECT_FALSE(!!CanvasElement().RateLimiter());
 
-  // Install a Canvas2DLayerBridge instance to make the canvas paintable and a
-  // CanvasResourceProvider that supports direct compositing to make the canvas
-  // composited.
+  // Install a CanvasResourceProvider that supports direct compositing to make
+  // the canvas composited.
   gfx::Size size = CanvasElement().Size();
   auto provider = std::make_unique<FakeCanvasResourceProvider>(
       SkImageInfo::MakeN32Premul(size.width(), size.height()),
@@ -717,8 +714,7 @@ TEST_P(CanvasRenderingContext2DTest, FallbackToSoftwareOnFailedTextureAlloc) {
   // to the raster mode that has been set as preferred.
   EXPECT_EQ(CanvasElement().GetRasterMode(), RasterMode::kGPU);
 
-  // This will cause SkSurface_Gpu creation to fail without
-  // Canvas2DLayerBridge otherwise detecting that anything was disabled.
+  // This will cause SkSurface_Gpu creation to fail.
   SharedGpuContext::ContextProviderWrapper()
       ->ContextProvider()
       ->GetGrContext()
