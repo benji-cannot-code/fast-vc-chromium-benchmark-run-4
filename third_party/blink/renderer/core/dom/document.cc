44fba4c351cb3ce62c2a8489a8596626f5b3d41c
Joey Arhar <jarhar@chromium.org>
2024-10-29 18:09:59 +0000

    Fix <select> exit transition anchoring
    This patch changed the implicit anchoring for <select> such that the
    implicit anchor is gone synchronously when hiding the popover:
    https://chromium-review.googlesource.com/c/chromium/src/+/5846489
    
    This breaks the case where there is a top layer exit animation because
    the implicit anchor is gone during the animation, so the popover jumps
    somewhere else in the page.
    
    This patch fixes this by removing the implicit anchor when removing
    elements from the top layer.
    
    Change-Id: Iaee83fa7b53160db52c10c954450ec4d023837ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5938529
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375386}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7979,6 +7979,13 @@ void Document::RemoveFromTopLayerImmediately(Element* element) {
   }
   element->SetIsInTopLayer(false);
   display_lock_document_state_->ElementRemovedFromTopLayer(element);
+  if (RuntimeEnabledFeatures::PopoverAnchorRelationshipsEnabled()) {
+    if (auto* html_element = DynamicTo<HTMLElement>(element)) {
+      if (html_element->HasPopoverAttribute()) {
+        html_element->SetImplicitAnchor(nullptr);
+      }
+    }
+  }
 
   probe::TopLayerElementsChanged(this);
 
--- third_party/blink/renderer/core/html/html_element.cc
+++ third_party/blink/renderer/core/html/html_element.cc
@@ -1233,6 +1233,7 @@ void HTMLElement::UpdatePopoverAttribute(const AtomicString& value) {
   }
   if (type == PopoverValueType::kNone) {
     if (HasPopoverAttribute()) {
+      SetImplicitAnchor(nullptr);
       // If the popover attribute is being removed, remove the PopoverData.
       RemovePopoverData();
     }
@@ -1803,9 +1804,6 @@ void HTMLElement::HidePopoverInternal(
 
   MarkPopoverInvokersDirty(*this);
   GetPopoverData()->setInvoker(nullptr);
-  if (RuntimeEnabledFeatures::PopoverAnchorRelationshipsEnabled()) {
-    SetImplicitAnchor(nullptr);
-  }
   // Events are only fired in the case that the popover is not being removed
   // from the document.
   if (transition_behavior ==
