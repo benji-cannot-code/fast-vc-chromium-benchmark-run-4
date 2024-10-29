de3a653b5a0a1c13b3111a6ec9488a5780646872
danakj <danakj@chromium.org>
2024-10-29 20:59:06 +0000

    Handle log statements with placeholders
    When placeholders are present the message can't be converted to a &str
    it needs to be made into a String. We were panicking in this case as
    we assumed we can always call as_str() and have it return &str.
    
    See also the docs here:
    https://doc.rust-lang.org/nightly/core/fmt/struct.Arguments.html#method.as_str
    
    R=lukasza@chromium.org
    
    Bug: 339678662
    Change-Id: I6d94944e8236d4fe5c5c932a7292ffa2075da91c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975235
    Commit-Queue: danakj <danakj@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375489}

--- base/test/logging/test_rust_logger_consumer.rs
+++ base/test/logging/test_rust_logger_consumer.rs
@@ -13,6 +13,7 @@ mod ffi {
         fn print_test_error_log();
         fn print_test_debug_log();
         fn print_test_trace_log();
+        fn print_test_error_log_with_placeholder(i: i32);
     }
 }
 
@@ -34,4 +35,8 @@ pub fn print_test_debug_log() {
 
 pub fn print_test_trace_log() {
     trace!("test trace log");
+}
+
+fn print_test_error_log_with_placeholder(i: i32) {
+    error!("test log with placeholder {}", i);
 }
