15f0db357f97a05a513a590099711d41e1dd5d15
Finnur Thorarinsson <finnur@chromium.org>
2024-10-29 17:25:48 +0000

    [NavCapture]: Restructure test framework (part 5)
    The parameterized tests have been getting a bit unwieldy.
    The main action is getting quite long and would benefit
    from pulling code into logical functions.
    
    This CL moves the waiting of NavigationFinished message
    into the utility Launch functions and changes the passing
    of the DomMessageQueue to a ref (instead of pointer).
    
    Bug: None
    Change-Id: I53d736e849fad81b9d75fbf93ef19b4c40752a36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5971313
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375346}

--- chrome/browser/web_applications/web_app_link_capturing_parameterized_browsertest.cc
+++ chrome/browser/web_applications/web_app_link_capturing_parameterized_browsertest.cc
@@ -758,9 +758,9 @@ class WebAppLinkCapturingParameterizedBrowserTest
   //
   // @param message_queue The message queue expected to see the message.
   void WaitForNavigationFinishedMessage(
-      content::DOMMessageQueue* message_queue) {
+      content::DOMMessageQueue& message_queue) {
     std::string message;
-    EXPECT_TRUE(message_queue->WaitForMessage(&message));
+    EXPECT_TRUE(message_queue.WaitForMessage(&message));
     std::string unquoted_message;
     ASSERT_TRUE(base::RemoveChars(message, "\"", &unquoted_message)) << message;
     EXPECT_TRUE(base::StartsWith(unquoted_message, "FinishedNavigating"))
@@ -850,19 +850,25 @@ class WebAppLinkCapturingParameterizedBrowserTest
                            base::WeakPtr<content::WebContents>,
                            apps::LaunchContainer>
         launch_future;
+
+    content::DOMMessageQueue message_queue;
     provider().scheduler().LaunchApp(app_id, url, launch_future.GetCallback());
     EXPECT_TRUE(launch_future.Wait());
     content::WebContents* contents =
         launch_future.Get<base::WeakPtr<content::WebContents>>().get();
     content::WaitForLoadStop(contents);
+    WaitForNavigationFinishedMessage(message_queue);
     return contents;
   }
 
   content::WebContents* LaunchPageInTab(const GURL& url) {
+    content::DOMMessageQueue message_queue;
+    // Note: We do not need to call WaitForLoadStop because NavigateToURL calls
+    // that internally.
     EXPECT_TRUE(ui_test_utils::NavigateToURL(browser(), url));
     content::WebContents* contents =
         browser()->tab_strip_model()->GetActiveWebContents();
-    content::WaitForLoadStop(contents);
+    WaitForNavigationFinishedMessage(message_queue);
     return contents;
   }
 
@@ -1391,8 +1397,6 @@ class WebAppLinkCapturingParameterizedBrowserTest
     Browser* browser_a;
     content::WebContents* contents_a;
     {
-      content::DOMMessageQueue message_queue;
-
       if (StartInAppWindow()) {
         GURL url_a = embedded_test_server()->GetURL(kStartPageScopeA);
         contents_a = LaunchStartPageAsApp(app_a, url_a);
@@ -1401,11 +1405,6 @@ class WebAppLinkCapturingParameterizedBrowserTest
         GURL url_a = embedded_test_server()->GetURL(kStartPageScopeA);
         contents_a = LaunchPageInTab(url_a);
       }
-
-      std::string message;
-      EXPECT_TRUE(message_queue.WaitForMessage(&message));
-      EXPECT_TRUE(base::Contains(message, "FinishedNavigating")) << message;
-      DLOG(INFO) << message;
 
       browser_a = chrome::FindBrowserWithTab(contents_a);
       ASSERT_TRUE(browser_a != nullptr);
@@ -1438,7 +1437,7 @@ class WebAppLinkCapturingParameterizedBrowserTest
       }
 
       if (expect_navigation) {
-        WaitForNavigationFinishedMessage(&message_queue);
+        WaitForNavigationFinishedMessage(message_queue);
       }
     }
 
@@ -1948,7 +1947,7 @@ class NavigationCapturingTestWithAppBLaunched
     ASSERT_TRUE(launch_future.Wait());
     url_observer.Wait();
     // Launching a web app should listen to a single navigation message.
-    WaitForNavigationFinishedMessage(&message_queue);
+    WaitForNavigationFinishedMessage(message_queue);
   }
 
   std::string GetTestClassName() const override {
@@ -2093,7 +2092,7 @@ class NavigationCapturingTestWithBLaunchedAndBrowserTab
     LaunchPageInTab(url_b_dest);
 
     // Launching a web app should listen to a single navigation message.
-    WaitForNavigationFinishedMessage(&message_queue);
+    WaitForNavigationFinishedMessage(message_queue);
   }
 
   std::string GetTestClassName() const override {
@@ -2180,7 +2179,7 @@ class NavigationCapturingTestWithExtraBrowserTabB
     content::WaitForLoadStop(contents_b);
 
     // Launching a web app should listen to a single navigation message.
-    WaitForNavigationFinishedMessage(&message_queue);
+    WaitForNavigationFinishedMessage(message_queue);
   }
 
   std::string GetTestClassName() const override {
