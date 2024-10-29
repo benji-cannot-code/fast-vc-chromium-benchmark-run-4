0ac00e20b63eb40a96d0239826573f5f80d0be00
Federica Germinario <fedegermi@google.com>
2024-10-29 15:51:20 +0000

    [ios] Support multi-profile in IncognitoUsageAppStateAgent
    Fixed: 364235738
    Change-Id: I974d34e3b5e77a756982ca4dc70e913fad5b54cc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953256
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Federica Germinario <fedegermi@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375255}

--- ios/chrome/browser/metrics/model/incognito_usage_app_state_agent.h
+++ ios/chrome/browser/metrics/model/incognito_usage_app_state_agent.h
@@ -6,11 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef IOS_CHROME_BROWSER_METRICS_MODEL_INCOGNITO_USAGE_APP_STATE_AGENT_H_
 #define IOS_CHROME_BROWSER_METRICS_MODEL_INCOGNITO_USAGE_APP_STATE_AGENT_H_
 
-#import "ios/chrome/app/application_delegate/app_state_agent.h"
+#import "ios/chrome/app/application_delegate/observing_app_state_agent.h"
 
 // The agent that logs the length of continuous usage of incognito.
 // Any normal/incognito transition lasting less than 10 seconds will be ignored.
-@interface IncognitoUsageAppStateAgent : NSObject <AppStateAgent>
+@interface IncognitoUsageAppStateAgent : SceneObservingAppAgent
 @end
 
 #endif  // IOS_CHROME_BROWSER_METRICS_MODEL_INCOGNITO_USAGE_APP_STATE_AGENT_H_
--- ios/chrome/browser/metrics/model/incognito_usage_app_state_agent.mm
+++ ios/chrome/browser/metrics/model/incognito_usage_app_state_agent.mm
@@ -15,10 +15,7 @@ namespace {
 constexpr base::TimeDelta kMinimumDelay = base::Seconds(10);
 }
 
-@interface IncognitoUsageAppStateAgent () <AppStateObserver, SceneStateObserver>
-
-// Observed app state.
-@property(nonatomic, weak) AppState* appState;
+@interface IncognitoUsageAppStateAgent ()
 
 @property(nonatomic, assign) BOOL incognitoContentVisible;
 
@@ -28,7 +25,6 @@ constexpr base::TimeDelta kMinimumDelay = base::Seconds(10);
 @end
 
 @implementation IncognitoUsageAppStateAgent
-
 - (instancetype)init {
   self = [super init];
   if (self) {
@@ -112,32 +108,11 @@ constexpr base::TimeDelta kMinimumDelay = base::Seconds(10);
   }
 }
 
-#pragma mark - AppStateAgent
-
-- (void)setAppState:(AppState*)appState {
-  // This should only be called once!
-  DCHECK(!_appState);
-
-  _appState = appState;
-  [appState addObserver:self];
-
-  for (SceneState* scene in appState.connectedScenes) {
-    [scene addObserver:self];
-  }
-  [self updateIncognitoContentVisible];
-}
-
-#pragma mark - AppStateObserver
-
-- (void)appState:(AppState*)appState sceneConnected:(SceneState*)sceneState {
-  [sceneState addObserver:self];
-  [self updateIncognitoContentVisible];
-}
-
 #pragma mark - SceneStateObserver
 
 - (void)sceneState:(SceneState*)sceneState
     transitionedToActivationLevel:(SceneActivationLevel)level {
+  [super sceneState:sceneState transitionedToActivationLevel:level];
   if (sceneState.incognitoContentVisible) {
     [self updateIncognitoContentVisible];
   }
