9e33ef3bf3d517a62870645e84ac65e4aea39611
Hans Wennborg <hans@chromium.org>
2024-10-29 23:27:08 +0000

    Add libclang_rt.asan_ios_dynamic.dylib to the clang-mac-runtime-library package.
    This is needed for iOS ASAN device builds.
    
    Change-Id: I474f7775ed4e2b45b9a7fcc47a0d394593d820ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973137
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Reviewed-by: Arthur Eubanks <aeubanks@google.com>
    Auto-Submit: Justin Cohen <justincohen@chromium.org>
    Commit-Queue: Arthur Eubanks <aeubanks@google.com>
    Commit-Queue: Hans Wennborg <hans@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375575}

--- tools/clang/scripts/package.py
+++ tools/clang/scripts/package.py
@@ -309,6 +309,7 @@ def main():
     runtime_package_name = 'clang-mac-runtime-library'
     runtime_packages = set([
         # AddressSanitizer runtime.
+        'lib/clang/$V/lib/darwin/libclang_rt.asan_ios_dynamic.dylib',
         'lib/clang/$V/lib/darwin/libclang_rt.asan_iossim_dynamic.dylib',
         'lib/clang/$V/lib/darwin/libclang_rt.asan_osx_dynamic.dylib',
 
