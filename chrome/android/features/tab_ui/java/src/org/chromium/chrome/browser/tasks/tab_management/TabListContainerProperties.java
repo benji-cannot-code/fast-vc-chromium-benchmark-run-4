9e060730c309c65fafc20edea784d55b1a0ab110
Theo Cristea <theocristea@google.com>
2024-10-29 22:00:18 +0000

    Follow MVC principles in tab switcher when setting content sensitivity
    The `TabSwitcherPane`s used to set the content sensitivity directly on
    the root view, which violated the MVC principles.
    Now TabSwitcherPaneCoordinator sets the newly added content sensitivity
    property on the model.
    
    As a consequence, the sensitivity is now set on the recycler view,
    instead of the root view. Therefore, tests had to be updated to handle
    this change, and some testing infrastructure became obsolete, such as a
    testing method inside `ChromeTabbedActivity`.
    
    Design doc: go/sensitive-content-tab
    
    Bug: 371547489
    Change-Id: Iafd55e48fd21cf1280ec7eba81aab9b0b5ee264b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974264
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1375526}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerProperties.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabListContainerProperties.java
@@ -66,6 +66,10 @@ class TabListContainerProperties {
     public static final ReadableObjectPropertyKey<Callback<ObservableSupplier<Boolean>>>
             IS_SCROLLING_SUPPLIER_CALLBACK = new WritableObjectPropertyKey<>();
 
+    /** Whether the tab switcher pane has sensitive content. */
+    public static final PropertyModel.WritableBooleanPropertyKey IS_CONTENT_SENSITIVE =
+            new PropertyModel.WritableBooleanPropertyKey();
+
     /** Keys for {@link TabSwitcherPaneCoordinator}. */
     public static final PropertyKey[] ALL_KEYS =
             new PropertyKey[] {
@@ -79,5 +83,6 @@ class TabListContainerProperties {
                 FETCH_VIEW_BY_INDEX_CALLBACK,
                 GET_VISIBLE_RANGE_CALLBACK,
                 IS_SCROLLING_SUPPLIER_CALLBACK,
+                IS_CONTENT_SENSITIVE,
             };
 }
