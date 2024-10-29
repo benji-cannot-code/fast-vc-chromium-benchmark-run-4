61732715a327948201a908c54abd8248f82faea0
Sylvain Defresne <sdefresne@chromium.org>
2024-10-29 17:19:24 +0000

    [ios] Inject the AuthenticationServiceDelegate at construction
    Move the declaration of the AuthenticationServiceDelegate into
    AuthenticationServiceFactory and inject it when the object is
    created which ensures that the object is always initialized.
    
    This removes the need for the CreateAndInitializeForProfile()
    method (it is kept because so many tests use it, it will be
    cleaned up in followup CLs).
    
    Fixed: 325612973
    Change-Id: Ie97ff256ab347d970d1f26f9839291e779f1d091
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960279
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1375339}

--- ios/chrome/browser/signin/model/authentication_service_factory.mm
+++ ios/chrome/browser/signin/model/authentication_service_factory.mm
@@ -8,25 +8,41 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <memory>
 #import <utility>
 
+#import "base/check.h"
+#import "base/functional/callback.h"
+#import "base/functional/callback_helpers.h"
 #import "base/no_destructor.h"
-#import "components/keyed_service/ios/browser_state_dependency_manager.h"
+#import "ios/chrome/browser/browsing_data/model/browsing_data_remover_factory.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
-#import "ios/chrome/browser/signin/model/authentication_service_delegate.h"
+#import "ios/chrome/browser/signin/model/authentication_service_delegate_impl.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/sync/model/sync_service_factory.h"
 
 namespace {
 
+using DelegateFactory =
+    AuthenticationServiceFactory::AuthenticationServiceDelegateFactory;
+
+std::unique_ptr<AuthenticationServiceDelegate>
+BuildAuthenticationServiceDelegate(ProfileIOS* profile) {
+  return std::make_unique<AuthenticationServiceDelegateImpl>(
+      BrowsingDataRemoverFactory::GetForProfile(profile), profile->GetPrefs());
+}
+
 std::unique_ptr<KeyedService> BuildAuthenticationService(
+    DelegateFactory delegate_factory,
     web::BrowserState* context) {
   ProfileIOS* profile = ProfileIOS::FromBrowserState(context);
-  return std::make_unique<AuthenticationService>(
+  auto service = std::make_unique<AuthenticationService>(
       profile->GetPrefs(),
       ChromeAccountManagerServiceFactory::GetForProfile(profile),
       IdentityManagerFactory::GetForProfile(profile),
       SyncServiceFactory::GetForProfile(profile));
+  service->Initialize(std::move(delegate_factory).Run(profile));
+  DCHECK(service->initialized());
+  return service;
 }
 
 }  // namespace
@@ -34,10 +50,8 @@ std::unique_ptr<KeyedService> BuildAuthenticationService(
 // static
 AuthenticationService* AuthenticationServiceFactory::GetForProfile(
     ProfileIOS* profile) {
-  AuthenticationService* service = static_cast<AuthenticationService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
-  CHECK(!service || service->initialized());
-  return service;
+  return GetInstance()->GetServiceForProfileAs<AuthenticationService>(
+      profile, /*create=*/true);
 }
 
 // static
@@ -56,25 +70,42 @@ void AuthenticationServiceFactory::CreateAndInitializeForBrowserState(
 void AuthenticationServiceFactory::CreateAndInitializeForProfile(
     ProfileIOS* profile,
     std::unique_ptr<AuthenticationServiceDelegate> delegate) {
-  AuthenticationService* service = static_cast<AuthenticationService*>(
-      GetInstance()->GetServiceForBrowserState(profile, true));
-  CHECK(service && !service->initialized());
-  service->Initialize(std::move(delegate));
+  GetInstance()->SetTestingFactory(profile,
+                                   GetFactoryWithDelegate(std::move(delegate)));
+  std::ignore = GetForProfile(profile);
 }
 
 // static
 AuthenticationServiceFactory::TestingFactory
 AuthenticationServiceFactory::GetDefaultFactory() {
-  return base::BindRepeating(&BuildAuthenticationService);
+  return GetFactoryWithDelegateFactory(
+      base::BindOnce(&BuildAuthenticationServiceDelegate));
+}
+
+// static
+AuthenticationServiceFactory::TestingFactory
+AuthenticationServiceFactory::GetFactoryWithDelegate(
+    std::unique_ptr<AuthenticationServiceDelegate> delegate) {
+  return GetFactoryWithDelegateFactory(base::IgnoreArgs<ProfileIOS*>(
+      base::ReturnValueOnce(std::move(delegate))));
+}
+
+// static
+AuthenticationServiceFactory::TestingFactory
+AuthenticationServiceFactory::GetFactoryWithDelegateFactory(
+    AuthenticationServiceDelegateFactory delegate_factory) {
+  return base::BindOnce(&BuildAuthenticationService,
+                        std::move(delegate_factory));
 }
 
 AuthenticationServiceFactory::AuthenticationServiceFactory()
-    : BrowserStateKeyedServiceFactory(
-          "AuthenticationService",
-          BrowserStateDependencyManager::GetInstance()) {
+    : ProfileKeyedServiceFactoryIOS("AuthenticationService",
+                                    ProfileSelection::kNoInstanceInIncognito,
+                                    TestingCreation::kNoServiceForTests) {
   DependsOn(ChromeAccountManagerServiceFactory::GetInstance());
   DependsOn(IdentityManagerFactory::GetInstance());
   DependsOn(SyncServiceFactory::GetInstance());
+  DependsOn(BrowsingDataRemoverFactory::GetInstance());
 }
 
 AuthenticationServiceFactory::~AuthenticationServiceFactory() {}
@@ -82,14 +113,11 @@ AuthenticationServiceFactory::~AuthenticationServiceFactory() {}
 std::unique_ptr<KeyedService>
 AuthenticationServiceFactory::BuildServiceInstanceFor(
     web::BrowserState* context) const {
-  return BuildAuthenticationService(context);
+  return BuildAuthenticationService(
+      base::BindOnce(&BuildAuthenticationServiceDelegate), context);
 }
 
 void AuthenticationServiceFactory::RegisterBrowserStatePrefs(
     user_prefs::PrefRegistrySyncable* registry) {
   AuthenticationService::RegisterPrefs(registry);
-}
-
-bool AuthenticationServiceFactory::ServiceIsNULLWhileTesting() const {
-  return true;
 }
