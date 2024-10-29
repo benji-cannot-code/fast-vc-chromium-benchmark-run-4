9da45e03991799e1a6a8b4d5e8cd5bdab24df17b
Arthur Milchior <arthurmilchior@chromium.org>
2024-10-29 01:11:37 +0000

    :a11y: Implement the non-deprecated builder
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implements either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    In order to be usable with std::make_unique, constructor and
    destructor had to became public. This is consistent with most other
    keyed services.
    
    Bug: 1396138
    Change-Id: Id539698bbbb173073affb8cfcc6d1db2a61b4905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4799808
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375003}

--- chrome/browser/accessibility/accessibility_labels_service.h
+++ chrome/browser/accessibility/accessibility_labels_service.h
@@ -47,6 +47,9 @@ class AccessibilityLabelsService
 #endif
 {
  public:
+  // Use |AccessibilityLabelsServiceFactory::GetForProfile(..)| to get
+  // an instance of this service.
+  explicit AccessibilityLabelsService(Profile* profile);
   AccessibilityLabelsService(const AccessibilityLabelsService&) = delete;
   AccessibilityLabelsService& operator=(const AccessibilityLabelsService&) =
       delete;
@@ -83,10 +86,6 @@ class AccessibilityLabelsService
 
  private:
   friend class AccessibilityLabelsServiceFactory;
-
-  // Use |AccessibilityLabelsServiceFactory::GetForProfile(..)| to get
-  // an instance of this service.
-  explicit AccessibilityLabelsService(Profile* profile);
 
   // Returns true if the profile preference is set and, in the case of Android,
   // the device is on Wi-Fi or the "Only on Wi-Fi" preference is not set.
