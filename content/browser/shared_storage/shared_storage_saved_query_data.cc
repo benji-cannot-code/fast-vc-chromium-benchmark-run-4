f77b3ea1529d024af6c15bf67b010dcf949f87c7
Camillia Smith Barnes <cammie@chromium.org>
2024-10-29 21:27:16 +0000

    Shared Storage: Support saved queries in selectURL
    We add support for saved queries, keyed by
    (`data_origin`, `script_url`, `operation_name`, `query_name`).
    
    When a named query is first invoked, the per page budgets are charged
    as usual, the index result is stored for later in case the query is
    reused from the same page.
    
    On query reuse, the previously calculated and stored index result
    will be used to select the URL at that index in the current list, and
    the per page budgets will not be re-charged.
    
    When the page is destroyed, all associated saved queries are also
    destroyed.
    
    See explainer update at
    https://github.com/WICG/shared-storage/pull/176
    and spec updates at
    https://github.com/WICG/shared-storage/pull/188
    https://github.com/WICG/shared-storage/pull/202
    
    Bug: 367440966
    Change-Id: I89fd7e65cd14c9d60d6ac53906f95593a2259a37
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5868889
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Yao Xiao <yaoxia@chromium.org>
    Commit-Queue: Cammie Smith Barnes <cammie@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375507}

---
+++ content/browser/shared_storage/shared_storage_saved_query_data.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/shared_storage/shared_storage_saved_query_data.h"
+
+#include <stdint.h>
+
+#include <queue>
+
+#include "base/functional/callback.h"
+
+namespace content {
+
+SharedStorageSavedQueryData::SharedStorageSavedQueryData() : index(-1) {}
+
+SharedStorageSavedQueryData::SharedStorageSavedQueryData(uint32_t index)
+    : index(index) {}
+
+SharedStorageSavedQueryData::SharedStorageSavedQueryData(
+    SharedStorageSavedQueryData&& other)
+    : index(other.index), callbacks(std::move(other.callbacks)) {}
+
+SharedStorageSavedQueryData::~SharedStorageSavedQueryData() = default;
+
+SharedStorageSavedQueryData& SharedStorageSavedQueryData::operator=(
+    SharedStorageSavedQueryData&&) = default;
+
+}  // namespace content
