bff4152a44779b540e55b8d1e1ea1a225a4c24c7
Christian Xu <christianxu@google.com>
2024-10-29 16:09:32 +0000

    [iOS][LO] Don't show/hide UI if the overlay is already presented/hidden
    Don't show the lens UI if it's already presented as it will freeze the
    UI with the RestorationWindow.
    
    Tested with two consecutive calls to showUI without issue.
    
    Bug: 376236604
    Change-Id: I2f89ae9e82291f6c4e0035c3b2f0fad8e839812b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975448
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Auto-Submit: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1375272}

--- ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
+++ ios/chrome/browser/lens_overlay/coordinator/lens_overlay_coordinator.mm
@@ -111,6 +111,9 @@ const CGFloat kMenuSymbolSize = 18;
                                       LensOverlayConsentViewControllerDelegate,
                                       LensOverlayPanTrackerDelegate>
 
+// Whether the `_containerViewController` is currently presented.
+@property(nonatomic, assign, readonly) BOOL isOverlayPresented;
+
 @end
 
 @implementation LensOverlayCoordinator {
@@ -356,7 +359,7 @@ const CGFloat kMenuSymbolSize = 18;
 }
 
 - (void)showLensUI:(BOOL)animated {
-  if (![self isUICreated]) {
+  if (![self isUICreated] || self.isOverlayPresented) {
     return;
   }
 
@@ -412,7 +415,7 @@ const CGFloat kMenuSymbolSize = 18;
 }
 
 - (void)hideLensUI:(BOOL)animated {
-  if (![self isUICreated]) {
+  if (![self isUICreated] || !self.isOverlayPresented) {
     return;
   }
   [self lockOrientationInPortrait:NO];
@@ -910,6 +913,10 @@ const CGFloat kMenuSymbolSize = 18;
 
 - (BOOL)isUICreated {
   return _containerViewController != nil;
+}
+
+- (BOOL)isOverlayPresented {
+  return _containerViewController.presentingViewController != nil;
 }
 
 - (BOOL)isResultsBottomSheetOpen {
