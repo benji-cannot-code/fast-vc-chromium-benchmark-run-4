483f549f0b01b9a8e8e3c21afe6c45618062efcb
Paul Adedeji <pauladedeji@google.com>
2024-10-29 16:43:23 +0000

    [ntp] Remove unused color `kColorNewTabPageMobilePromoDismissButton`
    It was taken out of the CSS in
    https://chromium-review.googlesource.com/c/chromium/src/+/5953648.
    
    Change-Id: Ia834c9d0eb09878f805aaad7435058416c127bd7
    Bug: 374296306
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960206
    Reviewed-by: Robbie Gibson <rkgibson@google.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375298}

--- chrome/browser/ui/color/chrome_color_id.h
+++ chrome/browser/ui/color/chrome_color_id.h
@@ -267,7 +267,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   E_CPONLY(kColorNewTabPageMenuOuterShadow) \
   E_CPONLY(kColorNewTabPageMicBorderColor) \
   E_CPONLY(kColorNewTabPageMicIconColor) \
-  E_CPONLY(kColorNewTabPageMobilePromoDismissButton) \
   E_CPONLY(kColorNewTabPageModuleControlBorder) \
   E_CPONLY(kColorNewTabPageModuleContextMenuDivider) \
   E_CPONLY(kColorNewTabPageModuleBackground) \
--- chrome/browser/ui/color/material_new_tab_page_color_mixer.cc
+++ chrome/browser/ui/color/material_new_tab_page_color_mixer.cc
@@ -77,9 +77,6 @@ void AddMaterialNewTabPageColorMixer(ui::ColorProvider* provider,
       ui::kColorSysTonalOutline};
   mixer[kColorNewTabPageModuleCalendarDividerColor] = {ui::kColorSysDivider};
 
-  mixer[kColorNewTabPageMobilePromoDismissButton] = {
-      ui::kColorSysOnSurfaceVariant};
-
   mixer[kColorNewTabPagePromoBackground] = {ui::kColorSysBase};
   mixer[kColorNewTabPagePrimaryForeground] = {ui::kColorSysOnSurface};
   mixer[kColorNewTabPageSecondaryForeground] = {ui::kColorSysOnSurfaceSubtle};
