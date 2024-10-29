0077357c7f5d8206de93d76f07c7541c5d729335
Erik Chen <erikchen@chromium.org>
2024-10-29 22:35:13 +0000

    [lensoverlay] Fix crash when screenshot is unavailable.
    Attempting to invoke lens overlay when a screenshot is unavailable
    results in state_ = kOff but non-null results_side_panel_coordinator_.
    This results in a crash when the tab is closed.
    
    This CL fixes some other minor issues in passing: member ordering in
    TabFeatures and ordering of state reset in
    LensOverlayController::ShowUI.
    
    Bug: 373767988
    Change-Id: I65c8bf7910bae80aca61b9453948a3b4cc90f8be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974895
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375542}

--- chrome/browser/ui/tabs/public/tab_features.h
+++ chrome/browser/ui/tabs/public/tab_features.h
@@ -148,7 +148,6 @@ class TabFeatures {
   std::unique_ptr<
       enterprise_data_protection::DataProtectionNavigationController>
       data_protection_controller_;
-  std::unique_ptr<LensOverlayController> lens_overlay_controller_;
   std::unique_ptr<FedCmAccountSelectionViewController>
       fedcm_account_selection_view_controller_;
 
@@ -156,6 +155,7 @@ class TabFeatures {
       permission_indicators_tab_data_;
 
   std::unique_ptr<SidePanelRegistry> side_panel_registry_;
+  std::unique_ptr<LensOverlayController> lens_overlay_controller_;
 
   // Responsible for the customize chrome tab-scoped side panel.
   std::unique_ptr<customize_chrome::SidePanelController>
