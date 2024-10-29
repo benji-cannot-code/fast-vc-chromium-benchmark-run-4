8538eaeb6cedcb401f34aa82c563129d5cc453d3
Theo Cristea <theocristea@google.com>
2024-10-29 11:27:18 +0000

    Follow MVC principles in TabGroupUi when setting the content sensitivity
    The TabGroupUiCoordinator used to set the content sensitivity directly
    on the dialog view, which violates the MVC principles.
    Now TabGridDialog is responsible for setting the sensitivity on the
    dialog view. The TabGridDialogCoordinator calls the
    TabGridDialogMediator, which sets the newly added content sensitivity
    property on the model.
    
    Design doc: go/sensitive-content-tab
    
    Bug: 371547489
    Change-Id: I397c26c9b4f5297ca1420c34478b4ce68fb066c5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5971261
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Theo Cristea <theocristea@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375140}

--- chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
+++ chrome/android/features/tab_ui/java/src/org/chromium/chrome/browser/tasks/tab_management/TabGridDialogCoordinator.java
@@ -537,7 +537,7 @@ public class TabGridDialogCoordinator implements TabGridDialogMediator.DialogCon
     }
 
     @Override
-    public @NonNull TabGridDialogView getDialogView() {
-        return mDialogView;
+    public void setGridContentSensitivity(boolean contentIsSensitive) {
+        mMediator.setGridContentSensitivity(contentIsSensitive);
     }
 }
