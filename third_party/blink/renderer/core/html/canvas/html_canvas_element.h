e62a2b81fb0ea5c290a69e08e607796d1a01d173
Colin Blundell <blundell@chromium.org>
2024-10-29 14:28:14 +0000

    [Blink] Fold SetCanvas2DLayerBridgeInternal() into callsite
    Facilitates followup separation of creation of the bridge from the other
    code in this method at callsites.
    
    Bug: 40280152
    Change-Id: I4faed0d45d09d41549c616f08191e376022b10f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963896
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375209}

--- third_party/blink/renderer/core/html/canvas/html_canvas_element.h
+++ third_party/blink/renderer/core/html/canvas/html_canvas_element.h
@@ -359,8 +359,6 @@ class CORE_EXPORT HTMLCanvasElement final
 
   void Reset();
 
-  void SetCanvas2DLayerBridgeInternal();
-
   void SetSurfaceSize(gfx::Size);
 
   bool SizeChangesAreAllowed(ExceptionState& exception_state);
