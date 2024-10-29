55483e0bf7c48908ddd67fe6d9936ca7fa5a5f20
Juan Mojica <juanmojica@google.com>
2024-10-29 17:36:28 +0000

    [lensoverlay] Fix horizontal padding in background image data.
    We should take into account the original resized screenshot size when
    calculating the horizontal padding. See bug for screenshots.
    
    Change-Id: I36ce67ae2bf9d065b1549185ef856a17be905d71
    Bug: b:373546007
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973493
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Ali Stanfield <stanfield@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375359}

--- chrome/test/data/webui/lens/utils/text_utils.ts
+++ chrome/test/data/webui/lens/utils/text_utils.ts
@@ -36,6 +36,10 @@ export function createTranslatedParagraph(
     writingDirection = WritingDirection.kLeftToRight): TranslatedParagraph {
   return {
     lines,
+    resizedBitmapSize: {
+      width: 1000,
+      height: 1000,
+    },
     alignment,
     contentLanguage,
     writingDirection,
