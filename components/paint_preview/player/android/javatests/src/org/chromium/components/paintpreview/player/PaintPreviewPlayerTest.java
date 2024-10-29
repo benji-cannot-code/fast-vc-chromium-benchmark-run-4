739c354cbf06c49b4ac156aaaf6477975a7208e2
Salvador Guerrero Ramos <salg@google.com>
2024-10-29 17:52:23 +0000

    Disable PaintPreviewPlayerTest.multiFrameDisplayTest_Wide
    This test is flaky in android-12l-x64-dbg-tests and other builders
    
    Bug: 376266736
    Change-Id: Ie2cd076a0d18b868b852cf23448f334cc975096d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5972979
    Commit-Queue: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Sinan Sahin <sinansahin@google.com>
    Owners-Override: Salvador Guerrero Ramos <salg@google.com>
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375373}

--- components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
+++ components/paint_preview/player/android/javatests/src/org/chromium/components/paintpreview/player/PaintPreviewPlayerTest.java
@@ -38,6 +38,7 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.Criteria;
 import org.chromium.base.test.util.CriteriaHelper;
 import org.chromium.base.test.util.DisableIf;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.ui.test.util.BlankUiTestActivityTestCase;
 import org.chromium.ui.test.util.RenderTestRule;
@@ -133,7 +134,9 @@ public class PaintPreviewPlayerTest extends BlankUiTestActivityTestCase {
                 CriteriaHelper.DEFAULT_POLLING_INTERVAL);
     }
 
-    /** Tests the the player correctly initializes and displays a sample paint preview with 1 frame. */
+    /**
+     * Tests the the player correctly initializes and displays a sample paint preview with 1 frame.
+     */
     @Test
     @MediumTest
     @Feature({"RenderTest"})
@@ -174,6 +177,7 @@ public class PaintPreviewPlayerTest extends BlankUiTestActivityTestCase {
     @Test
     @MediumTest
     @Feature({"RenderTest"})
+    @DisabledTest(message = "https://crbug.com/376266736")
     public void multiFrameDisplayTest_Wide() throws Exception {
         makeLayoutWide();
         displayTest(true);
