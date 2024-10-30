d1062ec7c22425f1ef1701ae570f283ed0cdf3f9
April Zhou <aprilzhou@google.com>
2024-10-30 23:06:09 +0000

    boca: Load session when refresh token first becomes available.
    Start a session load when refresh token becomes avaible. This will fix
    the issue that user don't get session upon sign in.
    
    Bug: b:375655322
    Change-Id: I65c71c09abd40ba786b7eb3498c5d30ea42075e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977683
    Reviewed-by: Benjamin Zielinski <bzielinski@google.com>
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375959}

--- chromeos/ash/components/boca/boca_session_manager.h
+++ chromeos/ash/components/boca/boca_session_manager.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/boca/session_api/session_client_impl.h"
 #include "chromeos/services/network_config/public/cpp/cros_network_config_observer.h"
 #include "components/account_id/account_id.h"
+#include "components/signin/public/identity_manager/identity_manager.h"
 #include "google_apis/common/api_error_codes.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/remote.h"
@@ -36,7 +37,8 @@ enum ApiErrorCode;
 namespace ash::boca {
 
 class BocaSessionManager
-    : public chromeos::network_config::CrosNetworkConfigObserver {
+    : public chromeos::network_config::CrosNetworkConfigObserver,
+      public signin::IdentityManager::Observer {
  public:
   inline static constexpr base::TimeDelta kPollingInterval = base::Minutes(5);
   inline static constexpr char kDummyDeviceId[] = "kDummyDeviceId";
@@ -118,6 +120,11 @@ class BocaSessionManager
       chromeos::network_config::mojom::NetworkStatePropertiesPtr network_state)
       override;
 
+  // signin::IdentityManager::Observer
+  void OnRefreshTokenUpdatedForAccount(const CoreAccountInfo& info) override;
+  void OnIdentityManagerShutdown(
+      signin::IdentityManager* identity_manager) override;
+
   void NotifyError(BocaError error);
   void AddObserver(Observer* observer);
   void RemoveObserver(Observer* observer);
@@ -174,6 +181,7 @@ class BocaSessionManager
       cros_network_config_observer_{this};
   AccountId account_id_;
   raw_ptr<SessionClientImpl> session_client_impl_;
+  raw_ptr<signin::IdentityManager> identity_manager_;
   base::WeakPtrFactory<BocaSessionManager> weak_factory_{this};
 };
 }  // namespace ash::boca
