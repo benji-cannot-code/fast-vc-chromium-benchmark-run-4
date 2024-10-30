6ed250cbea9e35cca71958dbca5f0a6110aa621e
Joel Hockey <joelhockey@chromium.org>
2024-10-30 00:25:16 +0000

    Update android directory tests to include DocumentsProvider
    When calling base::File::Stat() with a directory content-URI from a
    DocumentsProvider, the call to OpenContentUri() in File() ctor will
    fail since it is a dir rather than a file. This leaves the File object
    in an invalid state, and ContentUriGetFileInfo() must be called since
    it will not be called via file.GetInfo().
    
    The code is currently ok in this regard and does not need changing, but
    this updates the existing InMemoryContentProvider to be able to handle
    DocumentContracts URIs and implement query() in order to act like a
    DocumentsProvider for testing.
    
    Fixes overflow bug when setting st_mtime_nsec in File::Stat().
    
    Change-Id: I90f22bed219847d06272bb5a8a7a6d1e00308a3d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974375
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375601}

--- base/test/android/content_uri_test_utils.cc
+++ base/test/android/content_uri_test_utils.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/android/path_utils.h"
 #include "base/files/file_path.h"
+#include "base/strings/escape.h"
 
 namespace base::test::android {
 
@@ -33,6 +34,21 @@ std::optional<FilePath> GetInMemoryContentUriFromCacheDirFilePath(
   if (!cache_dir.AppendRelativePath(path, &uri)) {
     return std::nullopt;
   }
+  return uri;
+}
+
+std::optional<FilePath> GetInMemoryContentTreeUriFromCacheDirDirectory(
+    const FilePath& path) {
+  base::FilePath cache_dir;
+  if (!base::android::GetCacheDirectory(&cache_dir)) {
+    return std::nullopt;
+  }
+  base::FilePath document_id;
+  if (!cache_dir.AppendRelativePath(path, &document_id)) {
+    return std::nullopt;
+  }
+  base::FilePath uri("content://org.chromium.native_test.inmemory/tree/" +
+                     base::EscapeAllExceptUnreserved(document_id.value()));
   return uri;
 }
 
