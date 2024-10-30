16675c922df04ca98d7a44c2d9d64b64e60af7ad
Xinyi Ji <xinyiji@google.com>
2024-10-30 22:23:01 +0000

    [EducationalTip] Add default browser promo card into ephemeral module cards platform. (Part 1)
    This CL introduces a default browser promo card to the ephemeral module cards platform by creating the card and registering it.
    
    Bug: 355015904
    Change-Id: I51577ec23a4b4e9b99138e49a23a56b987012548
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962326
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Xi Han <hanxi@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375947}

--- chrome/browser/segmentation_platform/segmentation_platform_service_factory.cc
+++ chrome/browser/segmentation_platform/segmentation_platform_service_factory.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/session_sync_service_factory.h"
 #include "chrome/browser/sync/sync_service_factory.h"
 #include "chrome/common/chrome_constants.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "components/segmentation_platform/embedder/default_model/device_switcher_result_dispatcher.h"
 #include "components/segmentation_platform/embedder/home_modules/home_modules_card_registry.h"
 #include "components/segmentation_platform/embedder/input_delegate/shopping_service_input_delegate.h"
@@ -153,6 +154,11 @@ SegmentationPlatformServiceFactory::SegmentationPlatformServiceFactory()
 
 SegmentationPlatformServiceFactory::~SegmentationPlatformServiceFactory() =
     default;
+
+void SegmentationPlatformServiceFactory::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  home_modules::HomeModulesCardRegistry::RegisterProfilePrefs(registry);
+}
 
 KeyedService* SegmentationPlatformServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
--- components/segmentation_platform/embedder/home_modules/home_modules_card_registry.h
+++ components/segmentation_platform/embedder/home_modules/home_modules_card_registry.h
@@ -17,6 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace segmentation_platform::home_modules {
 
+#if BUILDFLAG(IS_ANDROID)
+// Immpression counter for each card.
+extern const char kDefaultBrowserPromoImpressionCounterPref[];
+#endif
+
 // Registry that manages all ephemeral cards in mobile home modules.
 class HomeModulesCardRegistry : public base::SupportsUserData::Data {
  public:
