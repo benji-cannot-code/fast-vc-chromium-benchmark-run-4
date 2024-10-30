f8285ee0020adc419e2f81124961c84f559e4f26
mlcui <mlcui@google.com>
2024-10-30 22:37:27 +0000

    scanner: Migrate actions to use proto types
    These were introduced in https://crrev.com/c/5960916 but were not
    directly used - instead, we wrapped them in the existing action structs.
    
    Remove the old structs and replace them with the proto types directly.
    
    Bug: b:363101024
    Change-Id: I6a6a2100764baf6f3148543849643d6b349f7e24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974780
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375949}

--- ash/scanner/scanner_action_handler_unittest.cc
+++ ash/scanner/scanner_action_handler_unittest.cc
@@ -243,8 +243,11 @@ TEST(ScannerActionToCommandTest, NewContactWithMultipleFields) {
 }
 
 TEST(ScannerActionToCommandTest, NewGoogleDoc) {
-  ScannerCommand command = ScannerActionToCommand(
-      NewGoogleDocAction("Doc Title", "<span>Contents</span>"));
+  manta::proto::NewGoogleDocAction action;
+  action.set_title("Doc Title");
+  action.set_html_contents("<span>Contents</span>");
+
+  ScannerCommand command = ScannerActionToCommand(std::move(action));
 
   EXPECT_THAT(
       command,
@@ -255,8 +258,11 @@ TEST(ScannerActionToCommandTest, NewGoogleDoc) {
 }
 
 TEST(ScannerActionToCommandTest, NewGoogleSheet) {
-  ScannerCommand command =
-      ScannerActionToCommand(NewGoogleSheetAction("Sheet Title", "a,b\n1,2"));
+  manta::proto::NewGoogleSheetAction action;
+  action.set_title("Sheet Title");
+  action.set_csv_contents("a,b\n1,2");
+
+  ScannerCommand command = ScannerActionToCommand(std::move(action));
 
   EXPECT_THAT(
       command,
@@ -267,8 +273,10 @@ TEST(ScannerActionToCommandTest, NewGoogleSheet) {
 }
 
 TEST(ScannerActionHandlerTest, CopyToClipboardWithPlainText) {
-  ScannerCommand command =
-      ScannerActionToCommand(CopyToClipboardAction("Hello", /*html_text=*/""));
+  manta::proto::CopyToClipboardAction action;
+  action.set_plain_text("Hello");
+
+  ScannerCommand command = ScannerActionToCommand(std::move(action));
 
   EXPECT_THAT(
       command,
@@ -279,8 +287,10 @@ TEST(ScannerActionHandlerTest, CopyToClipboardWithPlainText) {
 }
 
 TEST(ScannerActionHandlerTest, CopyToClipboardWithHtmlText) {
-  ScannerCommand command = ScannerActionToCommand(
-      CopyToClipboardAction(/*plain_text=*/"", "<img />"));
+  manta::proto::CopyToClipboardAction action;
+  action.set_html_text("<img />");
+
+  ScannerCommand command = ScannerActionToCommand(std::move(action));
 
   EXPECT_THAT(
       command,
@@ -292,8 +302,11 @@ TEST(ScannerActionHandlerTest, CopyToClipboardWithHtmlText) {
 }
 
 TEST(ScannerActionHandlerTest, CopyToClipboardWithMultipleFields) {
-  ScannerCommand command =
-      ScannerActionToCommand(CopyToClipboardAction("Hello", "<b>Hello</b>"));
+  manta::proto::CopyToClipboardAction action;
+  action.set_plain_text("Hello");
+  action.set_html_text("<b>Hello</b>");
+
+  ScannerCommand command = ScannerActionToCommand(std::move(action));
 
   EXPECT_THAT(
       command,
