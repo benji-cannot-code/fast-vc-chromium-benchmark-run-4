7e18cf1e78a3f9d44b93e872a26388cfeaa82e09
William Liu <liuwilliam@chromium.org>
2024-10-30 14:39:31 +0000

    [DNT] Do not show the loading bar until commit pending for native pages
    Cq-Include-Trybots: luci.chromium.try:android-12-x64-rel,android-12l-x64-dbg,android-13-x64-rel,android-14-x64-rel,android-15-x64-rel,android-pie-x86-rel,android-oreo-x86-rel
    
    Fixed: 371534496
    Change-Id: I0c4efe22fcea73cd28920ff7adc5497f3aa67e20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967136
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Reviewed-by: Lijin Shen <lazzzis@google.com>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375842}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -2604,12 +2604,11 @@ public class ToolbarManager
 
     private void onBackForwardTransitionAnimationChange() {
         Tab tab = mActivityTabProvider.get();
-        final boolean backGestureInvokeAnimationInProgress =
+        final boolean nativeDrawsProgressBar =
                 tab != null
                         && tab.getWebContents().getCurrentBackForwardTransitionStage()
-                                == AnimationStage.INVOKE_ANIMATION;
-        mToolbar.setShowingProgressBarForBackForwardTransition(
-                backGestureInvokeAnimationInProgress);
+                                == AnimationStage.INVOKE_ANIMATION_WITH_PROGRESS_BAR;
+        mToolbar.setShowingProgressBarForBackForwardTransition(nativeDrawsProgressBar);
     }
 
     public @BackPressResult int handleBackPress() {
