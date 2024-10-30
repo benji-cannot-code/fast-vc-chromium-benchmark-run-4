95fd6b872d4dfadd7400e994e70eb184c7a520af
Sylvain Defresne <sdefresne@chromium.org>
2024-10-30 15:56:58 +0000

    [ios] Record launch metrics once all profile are initialised
    Observe the initialisation of all profiles in MainController and
    report the launch metrics when they are all initialised.
    
    Bug: 333863468
    Change-Id: I6d766851ea1ea201469f32bcaf0c6004f3ef7ab1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973122
    Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1375883}

--- ios/chrome/app/main_controller.h
+++ ios/chrome/app/main_controller.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import <UIKit/UIKit.h>
 
-#import "ios/chrome/app/application_delegate/app_state.h"
 #import "ios/chrome/app/application_delegate/startup_information.h"
 
 @class AppState;
@@ -21,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // By design, it has no public API of its own. Anything interacting with
 // MainController should be doing so through a specific protocol.
-@interface MainController : NSObject <StartupInformation, AppStateObserver>
+@interface MainController : NSObject <StartupInformation>
 
 // Contains information about the application state, for example whether the
 // safe mode is activated.
