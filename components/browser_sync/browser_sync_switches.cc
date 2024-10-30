c4677a5320fbdd7f9a2ce08e4e114a2bb9fa7acf
Rushan Suleymanov <rushans@google.com>
2024-10-30 11:35:30 +0000

    [Sync] Clean up sync invalidation feature toggles
    A new switch is added to disable sync invalidation optimization flags in
    live tests, prior to this CL feature toggles were used for that purpose.
    
    Bug: 357821400
    Change-Id: I4e76a0da6dba1564c3c27352df989f0b5d8ebb7b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975338
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1375775}

--- components/browser_sync/browser_sync_switches.cc
+++ components/browser_sync/browser_sync_switches.cc
@@ -10,14 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-BASE_FEATURE(kSyncUseFCMRegistrationTokensList,
-             "SyncUseFCMRegistrationTokensList",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-BASE_FEATURE(kSyncFilterOutInactiveDevicesForSingleClient,
-             "SyncFilterOutInactiveDevicesForSingleClient",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 BASE_FEATURE(kMigrateSyncingUserToSignedIn,
              "MigrateSyncingUserToSignedIn",
 #if BUILDFLAG(IS_IOS)
