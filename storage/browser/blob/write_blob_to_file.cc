aeeb8c8b7cb55e09892ea6f7428df81f167af8f4
Evan Stade <estade@chromium.org>
2024-10-30 22:53:22 +0000

    Revert "IDB: add histograms to blob system to track down increased error rate"
    This reverts commit 5d2b503f2534b3bdbcc2fb022351fa26472765c7.
    
    Reason for revert: bug found and fixed thanks in part to these histograms
    
    See confirmation of reduced error rate: https://uma.googleplex.com/p/chrome/timeline_v2/?sid=1fd743b28798861ae2434c9b7201ed65
    
    Original change's description:
    > IDB: add histograms to blob system to track down increased error rate
    >
    > NO_IFTTT=new metric enum for existing code enum
    >
    > Bug: 40728654
    > Change-Id: I582eeadeb9cdd6be7eee28518702224089ff26a8
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953813
    > Reviewed-by: Ayu Ishii <ayui@chromium.org>
    > Commit-Queue: Evan Stade <estade@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1372431}
    
    Bug: 40728654
    Change-Id: Iafd0da5cc3a0bc769c4d0cdca5a1206ee5e1a715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980255
    Reviewed-by: Ayu Ishii <ayui@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Evan Stade <estade@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375956}

--- storage/browser/blob/write_blob_to_file.cc
+++ storage/browser/blob/write_blob_to_file.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
-#include "base/metrics/histogram_functions.h"
 #include "base/numerics/checked_math.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/task/task_traits.h"
@@ -46,16 +45,6 @@ struct WriteState {
   std::unique_ptr<FileWriterDelegate> delegate;
   DelegateNoProgressWriteCallback callback;
   base::CheckedNumeric<int64_t> total_bytes;
-};
-
-// Error cases where kInvalidBlob is reported. Used to track down
-// crbug.com/40728654
-enum class InvalidBlobReasons {
-  kConstructionError = 0,
-  kFullCopyTimestampWrong = 1,
-  kPartialCopyTimestampWrong = 2,
-  kUnexpectedBytesCopied = 3,
-  kMaxValue = kUnexpectedBytesCopied,
 };
 
 // Utility function to ignore all progress events returned when running
@@ -170,16 +159,12 @@ mojom::WriteBlobToFileResult CopyFileAndMaybeWriteTimeModified(
     base::GetFileInfo(copy_from, &info);
     if (!FileStreamReader::VerifySnapshotTime(expected_last_modified_copy_from,
                                               info)) {
-      base::UmaHistogramEnumeration(
-          "Storage.Blob.InvalidBlob",
-          InvalidBlobReasons::kFullCopyTimestampWrong);
       return mojom::WriteBlobToFileResult::kInvalidBlob;
     }
     if (!size || info.size == size.value()) {
       bool success = base::CopyFile(copy_from, copy_to);
-      if (!success) {
+      if (!success)
         return mojom::WriteBlobToFileResult::kIOError;
-      }
       if (last_modified && !base::TouchFile(copy_to, last_modified.value(),
                                             last_modified.value())) {
         return mojom::WriteBlobToFileResult::kTimestampError;
@@ -194,17 +179,13 @@ mojom::WriteBlobToFileResult CopyFileAndMaybeWriteTimeModified(
       base::File(copy_from, base::File::FLAG_OPEN | base::File::FLAG_READ);
   base::File outfile(copy_to,
                      base::File::FLAG_WRITE | base::File::FLAG_CREATE_ALWAYS);
-  if (!outfile.IsValid()) {
+  if (!outfile.IsValid())
     return mojom::WriteBlobToFileResult::kIOError;
-  }
 
   base::File::Info info;
   infile.GetInfo(&info);
   if (!FileStreamReader::VerifySnapshotTime(expected_last_modified_copy_from,
                                             info)) {
-    base::UmaHistogramEnumeration(
-        "Storage.Blob.InvalidBlob",
-        InvalidBlobReasons::kPartialCopyTimestampWrong);
     return mojom::WriteBlobToFileResult::kInvalidBlob;
   }
 
@@ -214,11 +195,8 @@ mojom::WriteBlobToFileResult CopyFileAndMaybeWriteTimeModified(
           size.value_or(std::numeric_limits<int64_t>::max()))) {
     return mojom::WriteBlobToFileResult::kIOError;
   }
-  if (size && bytes_copied != size.value()) {
-    base::UmaHistogramEnumeration("Storage.Blob.InvalidBlob",
-                                  InvalidBlobReasons::kUnexpectedBytesCopied);
+  if (size && bytes_copied != size.value())
     return mojom::WriteBlobToFileResult::kInvalidBlob;
-  }
 
   if (last_modified &&
       !outfile.SetTimes(last_modified.value(), last_modified.value())) {
@@ -326,10 +304,6 @@ void WriteConstructedBlobToFile(
   DCHECK(!last_modified || !last_modified.value().is_null());
   if (status != BlobStatus::DONE) {
     DCHECK(BlobStatusIsError(status));
-    base::UmaHistogramEnumeration("Storage.Blob.InvalidBlob",
-                                  InvalidBlobReasons::kConstructionError);
-    base::UmaHistogramEnumeration("Storage.Blob.InvalidBlob.ConstructionError",
-                                  status, BlobStatus::LAST_ERROR);
     std::move(callback).Run(mojom::WriteBlobToFileResult::kInvalidBlob);
     return;
   }
