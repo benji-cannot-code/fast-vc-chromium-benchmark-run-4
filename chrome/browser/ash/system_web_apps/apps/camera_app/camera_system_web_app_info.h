970637e81dcd54e13f36653ad562d2e6c7a177db
Pi-Hsun Shih <pihsun@chromium.org>
2024-10-30 06:32:11 +0000

    CCA: Remove CameraAppUIDelegate::SetLaunchDirectory
    After CL:5974378, for intent that use system dialog we no longer
    requires launch_params to be set, and all cases that requires the
    launch_params goes through SWA launch. Override the standard
    SystemWebAppDelegate::GetLaunchDirectory instead of having our own path
    of SetLaunchDirectory.
    
    Bug: b:374629916
    Test: tast run <dut> camera.CCAUIIntent camera.CCAUISmoke*
    Change-Id: I67fa5868856d356674e68e718e3779090bbbe6af
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974429
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375695}

--- chrome/browser/ash/system_web_apps/apps/camera_app/camera_system_web_app_info.h
+++ chrome/browser/ash/system_web_apps/apps/camera_app/camera_system_web_app_info.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ASH_SYSTEM_WEB_APPS_APPS_CAMERA_APP_CAMERA_SYSTEM_WEB_APP_INFO_H_
 
 #include <memory>
-#include "chrome/browser/ash/system_web_apps/types/system_web_app_delegate.h"
 
+#include "chrome/browser/ash/system_web_apps/types/system_web_app_delegate.h"
 #include "ui/gfx/geometry/rect.h"
 
 class Browser;
@@ -27,6 +27,8 @@ class CameraSystemAppDelegate : public ash::SystemWebAppDelegate {
   gfx::Size GetMinimumWindowSize() const override;
   gfx::Rect GetDefaultBounds(Browser* browser) const override;
   bool UseSystemThemeColor() const override;
+  base::FilePath GetLaunchDirectory(
+      const apps::AppLaunchParams& params) const override;
 };
 
 // Return a WebAppInstallInfo used to install the app.
