efe86c743954f8d627083fb2dbde99ff271a8a0a
Pi-Hsun Shih <pihsun@chromium.org>
2024-10-30 06:31:00 +0000

    camera: Don't open CCA in dialog for ARC intent that don't need results
    For INTENT_ACTION_* intents from ARC, the caller app just request the
    camera app to be opened without the result. In this case we can open CCA
    in a normal window with normal SWA launch flow.
    
          and check the opened CCA isn't in a dialog.
          and check CCA is opened in video mode.
    
    Bug: b:374629916
    Test: `adb shell am start -a android.media.action.STILL_IMAGE_CAMERA`
    Test: `adb shell am start -a android.media.action.VIDEO_CAMERA`
    Change-Id: I9bd17114b19c8b4b8ac452441056825d41c071d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974377
    Reviewed-by: David Jacobo <djacobo@chromium.org>
    Commit-Queue: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Wei Lee <wtlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375693}

--- chrome/browser/ash/system_web_apps/apps/camera_app/chrome_camera_app_ui_delegate.cc
+++ chrome/browser/ash/system_web_apps/apps/camera_app/chrome_camera_app_ui_delegate.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/pdf/pdf_service.h"
 #include "chrome/browser/policy/policy_util.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/screen_ai/public/optical_character_recognizer.h"
 #include "chrome/browser/ui/ash/system_web_apps/system_web_app_ui_utils.h"
 #include "chrome/browser/ui/settings_window_manager_chromeos.h"
@@ -107,10 +108,19 @@ constexpr int kPdfDpi = 72;
 // static
 void ChromeCameraAppUIDelegate::CameraAppDialog::ShowIntent(
     const std::string& queries,
+    bool launch_in_dialog,
     gfx::NativeWindow parent) {
   std::string url = ash::kChromeUICameraAppMainURL + queries;
-  CameraAppDialog* dialog = new CameraAppDialog(url);
-  dialog->ShowSystemDialog(parent);
+  if (launch_in_dialog) {
+    CameraAppDialog* dialog = new CameraAppDialog(url);
+    dialog->ShowSystemDialog(parent);
+  } else {
+    ash::SystemAppLaunchParams params;
+    params.url = GURL(url);
+    params.launch_source = apps::LaunchSource::kFromArc;
+    ash::LaunchSystemWebAppAsync(ProfileManager::GetActiveUserProfile(),
+                                 ash::SystemWebAppType::CAMERA, params);
+  }
 }
 
 ChromeCameraAppUIDelegate::CameraAppDialog::CameraAppDialog(
