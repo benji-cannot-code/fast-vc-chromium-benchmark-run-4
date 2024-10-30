10441fc20e6bc32dc7c002765b8a89cf35818a28
Mike Dougherty <michaeldo@chromium.org>
2024-10-30 15:13:03 +0000

    Extend metrics
    Additionally fix the spelling of two IOS.JavaScript metrics.
    
    Bug: None
    Change-Id: I1259475d7a35e60f779702b382c5d78324fa527e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977030
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Commit-Queue: Mike Dougherty <michaeldo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375859}

--- ios/web/js_features/window_error/script_error_message_handler_java_script_feature.mm
+++ ios/web/js_features/window_error/script_error_message_handler_java_script_feature.mm
@@ -93,7 +93,7 @@ void ScriptErrorMessageHandlerJavaScriptFeature::ScriptMessageReceived(
   }
 
   bool has_scriptname = details.filename && details.filename.length > 0;
-  UMA_HISTOGRAM_BOOLEAN("IOS.Javascript.ErrorHasFilename", has_scriptname);
+  UMA_HISTOGRAM_BOOLEAN("IOS.JavaScript.ErrorHasFilename", has_scriptname);
 
   if (base::FeatureList::IsEnabled(features::kLogJavaScriptErrors) &&
       !has_scriptname) {
