880f5f43531cef4669e69e8bb6e53cafeea5e91e
Morten Stenshorne <mstensho@chromium.org>
2024-10-31 09:45:17 +0000

    [CodeHealth] Remove stale base::Feature ArcGhostWindowNewStyle
    It's been enabled since M110.
    
    Bug: 356234336
    Change-Id: I160def310a7dc48df183b638c516d299008bf896
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5971635
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376190}

--- chrome/browser/ash/app_restore/arc_ghost_window_view.cc
+++ chrome/browser/ash/app_restore/arc_ghost_window_view.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/app_restore/arc_ghost_window_view.h"
 
-#include "ash/components/arc/arc_features.h"
 #include "ash/public/cpp/app_list/app_list_config.h"
 #include "ash/public/cpp/style/dark_light_mode_controller.h"
 #include "base/metrics/histogram_functions.h"
@@ -70,10 +69,6 @@ enum class GhostWindowType {
   kIconSpinningWithFixupText = 1,
   kMaxValue = kIconSpinningWithFixupText,
 };
-
-bool IsGhostWindowNewStyleEnabled() {
-  return base::FeatureList::IsEnabled(arc::kGhostWindowNewStyle);
-}
 
 std::u16string GetGhostWindowAppLaunchString(const std::string& app_name) {
   return l10n_util::GetStringFUTF16(IDS_ARC_GHOST_WINDOW_APP_LAUNCHING_MESSAGE,
@@ -149,7 +144,7 @@ void ArcGhostWindowView::SetGhostWindowViewType(arc::GhostWindowType type) {
 
   // DarkLightModeController maybe null in test env.
   if (type != arc::GhostWindowType::kFullRestore &&
-      IsGhostWindowNewStyleEnabled() && DarkLightModeController::Get()) {
+      DarkLightModeController::Get()) {
     // New style use ChromeOS system provided background color.
     auto color = cros_styles::ResolveColor(
         cros_styles::ColorName::kBgColor,
@@ -162,8 +157,7 @@ void ArcGhostWindowView::SetGhostWindowViewType(arc::GhostWindowType type) {
     SetBackground(views::CreateSolidBackground(theme_color_));
   }
 
-  if (type == arc::GhostWindowType::kFullRestore ||
-      !IsGhostWindowNewStyleEnabled()) {
+  if (type == arc::GhostWindowType::kFullRestore) {
     // If not enabled new style flag, all types will use original UI.
     AddChildView(views::Builder<views::ImageView>()
                      .SetImage(icon_raw_data_)
@@ -197,8 +191,7 @@ void ArcGhostWindowView::SetGhostWindowViewType(arc::GhostWindowType type) {
 void ArcGhostWindowView::OnThemeChanged() {
   views::View::OnThemeChanged();
   // DarkLightModeController maybe null in test env.
-  if (!IsGhostWindowNewStyleEnabled() ||
-      ghost_window_type_ == arc::GhostWindowType::kFullRestore ||
+  if (ghost_window_type_ == arc::GhostWindowType::kFullRestore ||
       !DarkLightModeController::Get()) {
     return;
   }
