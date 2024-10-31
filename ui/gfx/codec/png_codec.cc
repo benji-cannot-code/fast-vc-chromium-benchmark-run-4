d67fc3e31453122e53abd7fb94341d5bb11ea05d
Tom Sepez <tsepez@chromium.org>
2024-10-31 22:06:09 +0000

    Make PNGCodec::DecodeOutput a move-only type.
    It contains a std::vector of pixels, so copying it is expensive.
    This currently results in a new/memcpy in PNGCoded::Decode() when
    placing `output` into the optional<> for return, since the move
    operations are implicitly deleted by the presence of user-provided
    copy operations.
    
    -- fix else-after-return while at it.
    
    Change-Id: Ibb6aab96ef3613a773f4c7a9021d1bbd0c114f72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983379
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376627}

--- ui/gfx/codec/png_codec.cc
+++ ui/gfx/codec/png_codec.cc
@@ -31,11 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 
 PNGCodec::DecodeOutput::DecodeOutput() = default;
-PNGCodec::DecodeOutput::~DecodeOutput() = default;
-PNGCodec::DecodeOutput::DecodeOutput(const PNGCodec::DecodeOutput& other) =
-    default;
+PNGCodec::DecodeOutput::DecodeOutput(PNGCodec::DecodeOutput&& other) = default;
 PNGCodec::DecodeOutput& PNGCodec::DecodeOutput::operator=(
-    const PNGCodec::DecodeOutput& other) = default;
+    PNGCodec::DecodeOutput&& other) = default;
+PNGCodec::DecodeOutput::~DecodeOutput() = default;
 
 // Decoder --------------------------------------------------------------------
 
@@ -136,11 +135,10 @@ std::optional<PNGCodec::DecodeOutput> PNGCodec::Decode(
   SkCodec::Result result = preparation_output->codec->getPixels(
       info_srgb, output.output.data(),
       preparation_output->image_info.minRowBytes());
-  if (result == SkCodec::kSuccess) {
-    return output;
-  } else {
+  if (result != SkCodec::kSuccess) {
     return std::nullopt;
   }
+  return output;
 }
 
 SkBitmap PNGCodec::Decode(base::span<const uint8_t> input) {
