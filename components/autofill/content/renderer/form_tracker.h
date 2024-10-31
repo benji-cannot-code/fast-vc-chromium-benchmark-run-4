1ff0407a653f3dc2d6dce982099f56a4355a3373
Jihad Hanna <jihadghanna@google.com>
2024-10-31 11:35:57 +0000

    Cleanup feature AutofillUnifyAndFixFormTracking
    Follow-up CLs will aim at moving submission logic from AutofillAgent to
    FormTracker. This CL takes care of removing references to the feature.
    
    Bug: 40281981
    Change-Id: I7064b9e61ef7ded3cb1bf4a1be0da7003aea87c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5970953
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376231}

--- components/autofill/content/renderer/form_tracker.h
+++ components/autofill/content/renderer/form_tracker.h
@@ -78,7 +78,7 @@ class FormTracker : public content::RenderFrameObserver,
     enum class SaveFormReason {
       kTextFieldChanged,
       // TODO(crbug.com/40281981): Remove after launching the feature
-      // kAutofillUnifyAndFixFormTracking.
+      // kAutofillPreferSavedFormAsSubmittedForm.
       kWillSendSubmitEvent,
       kSelectChanged,
     };
@@ -179,7 +179,7 @@ class FormTracker : public content::RenderFrameObserver,
   }
 
   // content::WebLocalFrameObserver:
-  void OnFrameDetached() override;
+  void OnFrameDetached() override {}
   void WillSendSubmitEvent(const blink::WebFormElement& form) override;
 
   // Called in a posted task by textFieldDidChange() to work-around a WebKit bug
--- components/autofill/content/renderer/password_autofill_agent.h
+++ components/autofill/content/renderer/password_autofill_agent.h
@@ -108,10 +108,7 @@ class RendererSavePasswordProgressLogger;
 class PasswordGenerationAgent;
 
 // This class is responsible for filling password forms.
-// TODO(crbug.com/40281981): Remove FormTracker::Observer after launching
-// kAutofillUnifyAndFixFormTracking.
 class PasswordAutofillAgent : public content::RenderFrameObserver,
-                              public FormTracker::Observer,
                               public mojom::PasswordAutofillAgent {
  public:
   using EnableHeavyFormDataScraping =
@@ -169,13 +166,7 @@ class PasswordAutofillAgent : public content::RenderFrameObserver,
   void TriggerFormSubmission() override;
 #endif
 
-  // FormTracker::Observer
-  void OnProvisionallySaveForm(const blink::WebFormElement& form,
-                               const blink::WebFormControlElement& element,
-                               SaveFormReason source) override;
-  void OnProbablyFormSubmitted() override;
-  void OnFormSubmitted(const blink::WebFormElement& form) override;
-  void OnInferredFormSubmission(mojom::SubmissionSource source) override;
+  void OnFormSubmitted(const blink::WebFormElement& form);
 
   // WebLocalFrameClient editor related calls forwarded by AutofillAgent.
   // If they return true, it indicates the event was consumed and should not
