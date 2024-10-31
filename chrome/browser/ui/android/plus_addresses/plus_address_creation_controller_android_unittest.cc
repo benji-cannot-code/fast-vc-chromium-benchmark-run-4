6b9917c2a51343fd94040e4729d8d64cb2c69675
Timofey Chudakov <tchudakov@google.com>
2024-10-31 13:21:02 +0000

    [Android] Set syncable pref for the first plus address creation time.
    This CL sets the syncable pref plus_addresses.creation.first.time, when
    the user accepts the plus address creation bottom sheet for the
    first time, i.e. when the user creates their first plus address.
    
    This pref will be used to attach additional data to the plus address
    HaTS surveys.
    
    Bug: 348139343
    Change-Id: I709d4d703c706e613b08a6176d4b492ba94d60ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975138
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376256}

--- chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
+++ chrome/browser/ui/android/plus_addresses/plus_address_creation_controller_android_unittest.cc
@@ -21,8 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/fake_plus_address_service.h"
 #include "components/plus_addresses/features.h"
 #include "components/plus_addresses/metrics/plus_address_metrics.h"
+#include "components/plus_addresses/plus_address_prefs.h"
 #include "components/plus_addresses/plus_address_types.h"
 #include "components/plus_addresses/settings/mock_plus_address_setting_service.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/test/browser_task_environment.h"
 #include "content/public/test/web_contents_tester.h"
@@ -154,6 +156,11 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, AcceptCreation) {
           metrics::PlusAddressModalCompletionStatus::kModalConfirmed,
           /*notice_shown=*/false),
       0, 1);
+  // The pref is not set if the first time onboarding notice has been already
+  // shown.
+  EXPECT_EQ(
+      profile()->GetPrefs()->GetTime(prefs::kFirstPlusAddressCreationTime),
+      base::Time());
 }
 
 // Tests that no notice is shown if the onboarding feature is disabled.
@@ -221,6 +228,10 @@ TEST_F(PlusAddressCreationControllerAndroidEnabledTest, ShowNoticeAccept) {
           metrics::PlusAddressModalCompletionStatus::kModalConfirmed,
           /*notice_shown=*/true),
       0, 1);
+  // The pref is set only when the first time onboarding notice is shown.
+  EXPECT_EQ(profile()->GetTestingPrefService()->GetTime(
+                prefs::kFirstPlusAddressCreationTime),
+            base::Time::Now());
 }
 
 // Tests that the notice is shown if the  `kPlusAddressUserOnboardingEnabled`,
