d163ec98caef9e8107e3e9333839c41e914e89d9
Chris Thompson <cthomp@chromium.org>
2024-10-31 23:07:22 +0000

    HTTPS-First Mode: Update tests in chrome/browser/extensions
    This CL updates tests in c/b/extensions that fail when the new feature
    flag kHttpsFirstBalancedModeAutoEnable is enabled in the fieldtrial
    testing config. Due to the large number of affected files, this change
    is being split into multiple CLs -- see https://crrev.com/c/5787951/19
    for the full set of affected files.
    
    Bug: 349860796
    Change-Id: Ib8097a8a23d8be59cf3b00359644516ff64e4040
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980877
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Auto-Submit: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: David Bertoni <dbertoni@chromium.org>
    Commit-Queue: David Bertoni <dbertoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376657}

--- chrome/test/data/extensions/api_test/runtime/uninstall_url/test.js
+++ chrome/test/data/extensions/api_test/runtime/uninstall_url/test.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // browser_tests.exe --gtest_filter=ExtensionApiTest.ChromeRuntimeUninstallURL
 
 var pass = chrome.test.callbackPass;
-var uninstall_url = 'http://www.google.com/';
+var uninstall_url = 'https://www.google.com/';
 var sets_uninstall_url = 'Sets Uninstall Url';
 var uninstalled = false;
 chrome.test.runTests([
