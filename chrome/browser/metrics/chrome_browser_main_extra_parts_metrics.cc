490de29344d7ba00a2d140359e0a0cb784c07c62
lauren n. liberda <lauren@selfisekai.rocks>
2024-10-31 16:19:39 +0000

    metrics: fix missing imports on non-glibc linux builds
    Bug: 40244829
    Change-Id: Ie9f16c2fc9230e472846f506fb3ad0c2685105e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804816
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376364}

--- chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
+++ chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.cc
@@ -90,8 +90,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // TODO(crbug.com/40118868): Revisit the macro expression once build flag switch
 // of lacros-chrome is complete.
-#if defined(__GLIBC__) && (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS))
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS_LACROS)
+#if defined(__GLIBC__)
 #include <gnu/libc-version.h>
+#endif  // defined(__GLIBC__)
 
 #include "base/linux_util.h"
 #include "base/strings/string_split.h"
