493708eda59c7de47af917c2d05cd0930b08fcde
Thomas Lukaszewicz <tluk@chromium.org>
2024-10-31 21:44:34 +0000

    [webui] Set and update the emebdding browser for tabs
    This CL ensures browser tabs set the appropriate embedding
    browser window interface when created and keep it up to date
    when the tab is moved between windows.
    
    Bug: 360724768
    Change-Id: Id05b8255eef71d7b9cb49f694690a269d5edf4ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981891
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376616}

--- chrome/browser/ui/webui/webui_embedding_context.cc
+++ chrome/browser/ui/webui/webui_embedding_context.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/webui_embedding_context.h"
 
 #include "chrome/browser/ui/browser_window/public/browser_window_interface.h"
+#include "chrome/browser/ui/tabs/public/tab_interface.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_user_data.h"
 
@@ -79,6 +80,19 @@ class EmbedderContextData
 WEB_CONTENTS_USER_DATA_KEY_IMPL(EmbedderContextData);
 
 }  // namespace
+
+base::CallbackListSubscription InitEmbeddingContext(
+    tabs::TabInterface* tab_interface) {
+  // Set the initial browser context and configure updates for browser changes.
+  const auto update_browser_context = [](tabs::TabInterface* tab) {
+    CHECK(tab->GetBrowserWindowInterface());
+    SetBrowserWindowInterface(tab->GetContents(),
+                              tab->GetBrowserWindowInterface());
+  };
+  update_browser_context(tab_interface);
+  return tab_interface->RegisterDidInsert(
+      base::BindRepeating(update_browser_context));
+}
 
 void SetBrowserWindowInterface(
     content::WebContents* host_contents,
