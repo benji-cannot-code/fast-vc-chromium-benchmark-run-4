19c881f65a0ac1c56e425693256cbe19e358bd92
Tom Anderson <thomasanderson@chromium.org>
2024-10-31 16:20:43 +0000

    Only create status icon windows on supported platforms (currently X11)
    The X11 platform has a codepath for handling XEmbed status icons
    which is not available on other platforms like Wayland.  This may
    result in a detached status icon window showing.  This CL avoids
    creating a status icon window on non-X11 platforms.
    
    R=nickdiego
    
    Change-Id: I25ccb34a23fbe69105be108bccf4866dcd7700e5
    Fixed: 370688305
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980289
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376365}

--- chrome/browser/ui/views/status_icons/status_icon_button_linux.cc
+++ chrome/browser/ui/views/status_icons/status_icon_button_linux.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/transform.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/scoped_canvas.h"
+#include "ui/ozone/public/ozone_platform.h"
 
 namespace {
 
@@ -54,6 +55,12 @@ void StatusIconButtonLinux::UpdatePlatformContextMenu(ui::MenuModel* model) {
 }
 
 void StatusIconButtonLinux::OnSetDelegate() {
+  if (!ui::OzonePlatform::GetInstance()
+           ->GetPlatformRuntimeProperties()
+           .supports_system_tray_windowing) {
+    return;
+  }
+
   widget_ = std::make_unique<StatusIconWidget>();
 
   const int width = std::max(1, delegate_->GetImage().width());
