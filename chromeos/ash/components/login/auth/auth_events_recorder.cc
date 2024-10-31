c0b37e4beb7866e67ab4a5eecffd96252ded2629
Istvan Nagy <iscsi@google.com>
2024-10-31 17:28:48 +0000

    [High5] Track all successful logins made by passwordless users.
    Bug: b:376672605
    Change-Id: Ic2f0a5c81acb9154ed084d3a7efd790acc9b6905
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979856
    Commit-Queue: Istvan Nagy <iscsi@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376422}

--- chromeos/ash/components/login/auth/auth_events_recorder.cc
+++ chromeos/ash/components/login/auth/auth_events_recorder.cc
@@ -72,6 +72,9 @@ constexpr int kManyUserLimit = 5;
 constexpr char kConfiguredAuthFactorsHistogramPrefix[] =
     "Ash.OSAuth.Login.ConfiguredAuthFactors.";
 
+// Histogram for recording the passwordless user login.
+constexpr char kPasswordlessLoginHistogram[] = "Ash.OSAuth.Login.Passwordless";
+
 // Histogram prefix for recording duration of various login flow phases.
 // Format: "Ash.OSAuth.Login.Times.{...}"
 constexpr char kLoginTimeHistogramPrefix[] = "Ash.OSAuth.Login.Times.";
@@ -412,6 +415,8 @@ void AuthEventsRecorder::RecordSessionAuthFactors(
   DCHECK(auth_surface_.has_value());
   DCHECK_EQ(auth_surface_.value(), AuthenticationSurface::kLogin);
 
+  bool passwordless = true;
+
   const auto factor_types = auth_factors.GetSessionFactors();
   for (const auto factor : kTrackedAuthFactors) {
     if (factor == cryptohome::AuthFactorType::kPassword) {
@@ -423,12 +428,17 @@ void AuthEventsRecorder::RecordSessionAuthFactors(
       base::UmaHistogramBoolean(GetConfiguredPasswordFactorHistogramName(
                                     ConfiguredPasswordType::kLocal),
                                 local_password != nullptr);
+      if (local_password || online_password) {
+        passwordless = false;
+      }
       continue;
     }
 
     base::UmaHistogramBoolean(GetConfiguredAuthFactorsHistogramName(factor),
                               base::Contains(factor_types, factor));
   }
+
+  base::UmaHistogramBoolean(kPasswordlessLoginHistogram, passwordless);
 }
 
 void AuthEventsRecorder::OnRecoveryDone(CryptohomeRecoveryResult result,
--- chromeos/ash/components/login/auth/auth_events_recorder_unittest.cc
+++ chromeos/ash/components/login/auth/auth_events_recorder_unittest.cc
@@ -296,6 +296,9 @@ TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactors) {
       "Ash.OSAuth.Login.ConfiguredAuthFactors.SmartCard", 0, 1);
   histogram_tester.ExpectBucketCount(
       "Ash.OSAuth.Login.ConfiguredAuthFactors.LocalPassword", 0, 1);
+
+  // The logged-in user has a password factor.
+  histogram_tester.ExpectBucketCount("Ash.OSAuth.Login.Passwordless", 0, 1);
 }
 
 TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactorsLocalPassword) {
@@ -320,6 +323,9 @@ TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactorsLocalPassword) {
       "Ash.OSAuth.Login.ConfiguredAuthFactors.SmartCard", 0, 1);
   histogram_tester.ExpectBucketCount(
       "Ash.OSAuth.Login.ConfiguredAuthFactors.GaiaPassword", 0, 1);
+
+  // The logged-in user has a password factor.
+  histogram_tester.ExpectBucketCount("Ash.OSAuth.Login.Passwordless", 0, 1);
 }
 
 TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactorsLegacyPassword) {
@@ -344,6 +350,35 @@ TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactorsLegacyPassword) {
       "Ash.OSAuth.Login.ConfiguredAuthFactors.SmartCard", 0, 1);
   histogram_tester.ExpectBucketCount(
       "Ash.OSAuth.Login.ConfiguredAuthFactors.LocalPassword", 0, 1);
+
+  // The logged-in user has a password factor.
+  histogram_tester.ExpectBucketCount("Ash.OSAuth.Login.Passwordless", 0, 1);
+}
+
+TEST_F(AuthEventsRecorderTest, RecordSessionAuthFactorsCryptohomePin) {
+  base::HistogramTester histogram_tester;
+
+  SessionAuthFactors factors({MakePinAuthFactor(), MakeRecoveryFactor()});
+  recorder_->OnAuthenticationSurfaceChange(
+      AuthEventsRecorder::AuthenticationSurface::kLogin);
+  recorder_->RecordSessionAuthFactors(factors);
+
+  // The following factors are recorded with `true`.
+  histogram_tester.ExpectBucketCount(
+      "Ash.OSAuth.Login.ConfiguredAuthFactors.CryptohomePin", 1, 1);
+  histogram_tester.ExpectBucketCount(
+      "Ash.OSAuth.Login.ConfiguredAuthFactors.Recovery", 1, 1);
+
+  // The following factors are recorded with `false`.
+  histogram_tester.ExpectBucketCount(
+      "Ash.OSAuth.Login.ConfiguredAuthFactors.SmartCard", 0, 1);
+  histogram_tester.ExpectBucketCount(
+      "Ash.OSAuth.Login.ConfiguredAuthFactors.GaiaPassword", 0, 1);
+  histogram_tester.ExpectBucketCount(
+      "Ash.OSAuth.Login.ConfiguredAuthFactors.LocalPassword", 0, 1);
+
+  // The logged-in user has no password factor.
+  histogram_tester.ExpectBucketCount("Ash.OSAuth.Login.Passwordless", 1, 1);
 }
 
 TEST_F(AuthEventsRecorderTest, OnRecoveryDone) {
