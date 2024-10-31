52303ad19047fa007dc267712e089424bba70c54
Zheda Chen <zheda.chen@intel.com>
2024-10-31 03:19:04 +0000

    [PM] Throttle begin frame rate for unimportant frames
    This CL throttles begin frame rate for unimportant frames (cross origin
    frames with small proportion of the page's visible area and no user
    activation) on foreground pages. Frame rate would be reduced to half of
    normal frame rate.
    
    Bug: 40942028
    Change-Id: I772498058be52bafe86ba396be36f1a6a38cf988
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5682484
    Reviewed-by: Patrick Monette <pmonette@chromium.org>
    Commit-Queue: Zheda Chen <zheda.chen@intel.com>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376077}

--- chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
+++ chrome/browser/performance_manager/chrome_browser_main_extra_parts_performance_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/performance_manager/metrics/metrics_provider_desktop.h"
 #include "chrome/browser/performance_manager/observers/page_load_metrics_observer.h"
 #include "chrome/browser/performance_manager/policies/background_tab_loading_policy.h"
+#include "chrome/browser/performance_manager/policies/frame_throttling_policy.h"
 #include "chrome/browser/performance_manager/policies/policy_features.h"
 #include "chrome/browser/performance_manager/policies/working_set_trimmer_policy.h"
 #include "chrome/browser/performance_manager/user_tuning/profile_discard_opt_out_list_helper.h"
@@ -217,6 +218,12 @@ void ChromeBrowserMainExtraPartsPerformanceManager::CreatePoliciesAndDecorators(
 
   graph->PassToGraph(
       std::make_unique<performance_manager::metrics::PageResourceMonitor>());
+
+  if (base::FeatureList::IsEnabled(
+          performance_manager::features::kThrottleUnimportantFrameRate)) {
+    graph->PassToGraph(std::make_unique<
+                       performance_manager::policies::FrameThrottlingPolicy>());
+  }
 
   if (base::FeatureList::IsEnabled(
           performance_manager::features::kBFCachePerformanceManagerPolicy)) {
--- components/performance_manager/features.cc
+++ components/performance_manager/features.cc
@@ -199,6 +199,10 @@ BASE_FEATURE(kUnimportantFramesPriority,
              "UnimportantFramesPriority",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kThrottleUnimportantFrameRate,
+             "ThrottleUnimportantFrameRate",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
 BASE_FEATURE(kLoadingPerformanceScenario,
              "LoadingPerformanceScenario",
              base::FEATURE_DISABLED_BY_DEFAULT);
