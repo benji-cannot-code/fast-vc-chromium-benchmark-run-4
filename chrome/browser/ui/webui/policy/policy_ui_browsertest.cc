f047a9aa9e8eddb4ffa5e774ecd24a7d9d1fb4e7
Austin Zhang <austinzzr@google.com>
2024-10-31 01:20:58 +0000

    [PLG] Add CEC promotion banner feature to the chrome://management page
    Refactor EnablePolicyBanner for chrome://policy page so that all banner features are in the same place, also rename it to EnablePolicyPromotionBanner
    
    NO_IFTTT=this change will not be published on iOS
    
    Change-Id: Ie5d2316b6d4cc499c249d53c8f6fd98279f15672
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5914192
    Reviewed-by: Yann Dago <ydago@chromium.org>
    Commit-Queue: Austin Zhang <austinzzr@google.com>
    Reviewed-by: Salma Elmahallawy <esalma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376019}

--- chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
+++ chrome/browser/ui/webui/policy/policy_ui_browsertest.cc
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/policy/core/browser/webui/policy_status_provider.h"
 #include "components/policy/core/common/cloud/cloud_policy_refresh_scheduler.h"
 #include "components/policy/core/common/external_data_fetcher.h"
-#include "components/policy/core/common/features.h"
 #include "components/policy/core/common/management/scoped_management_service_override_for_testing.h"
 #include "components/policy/core/common/mock_configuration_policy_provider.h"
 #include "components/policy/core/common/policy_map.h"
@@ -70,6 +69,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/install_verifier.h"
 #include "chrome/browser/extensions/test_extension_system.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/ui_features.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/account_id/account_id.h"
 #include "extensions/common/extension_builder.h"
@@ -999,10 +999,10 @@ class PolicyUIManagedStatusTest : public PolicyUITest,
   PolicyUIManagedStatusTest() {
     if (GetParam()) {
       scoped_feature_list_.InitAndEnableFeature(
-          policy::features::kEnablePolicyBanner);
+          features::kEnablePolicyPromotionBanner);
     } else {
       scoped_feature_list_.InitAndDisableFeature(
-          policy::features::kEnablePolicyBanner);
+          features::kEnablePolicyPromotionBanner);
     }
   }
   bool isFeatureEnabled() { return GetParam(); }
@@ -1157,4 +1157,4 @@ IN_PROC_BROWSER_TEST_P(PolicyUIManagedStatusTest, HandleLocaleNotEnUSHidden) {
 INSTANTIATE_TEST_SUITE_P(PolicyManagedUITestInstance,
                          PolicyUIManagedStatusTest,
                          ::testing::Values(false, true));
-#endif
+#endif // !BUILDFLAG(IS_ANDROID)
