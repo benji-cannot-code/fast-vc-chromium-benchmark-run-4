5bc136181f822d6395b083e8334c5183bac0a557
Brandon Wylie <wylieb@google.com>
2024-10-31 01:23:37 +0000

    Revert "[hub] Implement transparency background on the omnibox for hub search"
    This reverts commit 75007c9ab2c5fe52db1be177280e43d1543ebe63.
    
    Reason for revert: Regressed switch-to-tab opening functionality as described in crbug.com/376350086.
    
    Original change's description:
    > [hub] Implement transparency background on the omnibox for hub search
    >
    > Add a translucent background upon entry into hub search.
    >
    > Preview: https://drive.google.com/file/d/116beRJZEfsZLB89x7ARbBeB_gblwI5JW/view?usp=sharing
    >
    > Bug: 369687073
    > Change-Id: Ieb0ffd962a99066b548b666ac3935dc62e01dd74
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5889880
    > Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    > Commit-Queue: Brandon Fong <bjfong@google.com>
    > Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1374919}
    
    Bug: 369687073
    Change-Id: I48b6a57b51f303c6f78298b92fc5b1427d2a06d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980248
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Patrick Noland <pnoland@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Patrick Noland <pnoland@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376022}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchTestUtils.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchTestUtils.java
@@ -13,7 +13,6 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.chromium.chrome.browser.searchwidget.SearchActivity;
-import org.chromium.chrome.browser.searchwidget.SearchActivityTransparentBackground;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
 import org.chromium.chrome.test.util.ActivityTestUtils;
@@ -29,7 +28,7 @@ public class TabSwitcherSearchTestUtils {
         SearchActivity searchActivity =
                 ActivityTestUtils.waitForActivity(
                         InstrumentationRegistry.getInstrumentation(),
-                        SearchActivityTransparentBackground.class,
+                        SearchActivity.class,
                         () -> onView(withId(R.id.search_box_text)).perform(click()));
 
         OmniboxTestUtils omniboxTestUtils = new OmniboxTestUtils(searchActivity);
