9319317444f6d6867690cec9bdc15c9f10afc438
Peter Kotwicz <pkotwicz@chromium.org>
2024-10-31 02:26:13 +0000

    Fix v8 optimizer in site settings "Recent Activity" list.
    Fixed: 375219955
    
    Change-Id: Ib9b636a094cd273d6611eb22e3fd67a148742df7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5959429
    Reviewed-by: Rainhard Findling <rainhard@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376054}

--- chrome/browser/resources/settings/site_settings_page/site_settings_page_util.ts
+++ chrome/browser/resources/settings/site_settings_page/site_settings_page_util.ts
@@ -54,6 +54,8 @@ export function getLocalizationStringForContentType(
       return 'siteSettingsImagesMidSentence';
     case ContentSettingsTypes.JAVASCRIPT:
       return 'siteSettingsJavascriptMidSentence';
+    case ContentSettingsTypes.JAVASCRIPT_OPTIMIZER:
+      return 'siteSettingsJavascriptOptimizerMidsentence';
     case ContentSettingsTypes.KEYBOARD_LOCK:
       return 'siteSettingsKeyboardLockMidSentence';
     case ContentSettingsTypes.LOCAL_FONTS:
@@ -98,7 +100,6 @@ export function getLocalizationStringForContentType(
       return 'siteSettingsZoomLevelsMidSentence';
     // The following members do not have a mid-sentence localization.
     case ContentSettingsTypes.ANTI_ABUSE:
-    case ContentSettingsTypes.JAVASCRIPT_OPTIMIZER:
     case ContentSettingsTypes.PDF_DOCUMENTS:
     case ContentSettingsTypes.PERFORMANCE:
     case ContentSettingsTypes.PRIVATE_NETWORK_DEVICES:
