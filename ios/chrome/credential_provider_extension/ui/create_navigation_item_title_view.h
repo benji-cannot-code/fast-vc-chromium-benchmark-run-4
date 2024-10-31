f5f4211a887545ac41276406e470454e83fa7b14
Alexis Hetu <sugoi@chromium.org>
2024-10-31 00:17:33 +0000

    [iOS] BrandedNavigationItemTitleView in CPE
    This CL adds a utility function to create a navigation item title
    view for the passkey keychain provider to use in its UI. It is
    complete with icon and font. The font defaults to system font in
    Chromium, but will use the branded font as soon as it is available
    in the ChromeExtensionKeychain framework.
    
    Bug: 375390807
    Change-Id: I31dafbf9da0f295a21e13e8ecd0d2311420dd919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5966695
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Alexis Hétu <sugoi@chromium.org>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1375982}

---
+++ ios/chrome/credential_provider_extension/ui/create_navigation_item_title_view.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREATE_NAVIGATION_ITEM_TITLE_VIEW_H_
+#define IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREATE_NAVIGATION_ITEM_TITLE_VIEW_H_
+
+#import <UIKit/UIKit.h>
+
+namespace credential_provider_extension {
+
+// Creates a branded title view for the keychain navigation controller.
+UIView* CreateNavigationItemTitleView(UIFont* font);
+
+}  // namespace credential_provider_extension
+
+#endif  // IOS_CHROME_CREDENTIAL_PROVIDER_EXTENSION_UI_CREATE_NAVIGATION_ITEM_TITLE_VIEW_H_
