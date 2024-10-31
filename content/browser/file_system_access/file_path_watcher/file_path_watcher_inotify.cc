e1622669f8207685d16c87690d8392260212e4ee
Nathan Memmott <memmott@chromium.org>
2024-10-31 23:12:25 +0000

    FilePathWatcher: Report Usage Changes
    Update the FilePathWatcher interface to support watching usage changes.
    It adds:
    - current_usage - returns the number of underlying OS resources being
      used. The number is relative to the quota_limit. This CL implements it
      on every platform.
    - quota_limit - returns the max amount of resources the FilePathWatcher
      is allowed to use. This is to be determined and currently implemented
      by just returning zero.
    - usage_callback - passed to the FilePathWatcher constructor and called
      any time the current_usage changes. Currently unimplemented and will
      be implemented in a follow up CL.
    
    These additions will be used to implement change observer_limits.
    
    Bug: 338457523
    Change-Id: Ic71421ff692749563020fdada08039386f15f502
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5968293
    Reviewed-by: Daseul Lee <dslee@chromium.org>
    Commit-Queue: Nathan Memmott <memmott@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376661}

--- content/browser/file_system_access/file_path_watcher/file_path_watcher_inotify.cc
+++ content/browser/file_system_access/file_path_watcher/file_path_watcher_inotify.cc
@@ -196,11 +196,20 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate {
   // Returns a WatcherEntry for this, must be called on the original sequence.
   InotifyReader::WatcherEntry GetWatcherEntry();
 
+  size_t current_usage() const override {
+    // `watches_` contains inotify watches of all dir components of `target_`.
+    // `recursive_paths_by_watch_` contains inotify watches for sub dirs under
+    // `target_` of a Type::kRecursive watcher and keyed by inotify watches. All
+    // inotify watches used by this FilePathWatcherImpl are either in `watches_`
+    // or as a key in `recursive_paths_by_watch_`. As a result, the two provide
+    // a good estimate on the number of inotify watches used by this
+    // FilePathWatcherImpl.
+    return watches_.size() + recursive_watches_by_path_.size();
+  }
+
   void UpdateInotifyCountHighWaterMark() {
-    int current_inotify_count =
-        watches_.size() + recursive_watches_by_path_.size();
     inotify_count_high_water_mark_ =
-        std::max(inotify_count_high_water_mark_, current_inotify_count);
+        std::max(inotify_count_high_water_mark_, current_usage());
   }
 
  private:
@@ -218,7 +227,8 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate {
   bool WatchWithChangeInfo(
       const base::FilePath& path,
       const WatchOptions& options,
-      const FilePathWatcher::CallbackWithChangeInfo& callback) override;
+      const FilePathWatcher::CallbackWithChangeInfo& callback,
+      const FilePathWatcher::UsageChangeCallback& usage_callback) override;
 
   // Cancel the watch. This unregisters the instance with InotifyReader.
   void Cancel() override;
@@ -310,7 +320,7 @@ class FilePathWatcherImpl : public FilePathWatcher::PlatformDelegate {
       recursive_paths_by_watch_;
   std::map<base::FilePath, InotifyReader::Watch> recursive_watches_by_path_;
 
-  int inotify_count_high_water_mark_ = 0;
+  size_t inotify_count_high_water_mark_ = 0;
 
   base::WeakPtrFactory<FilePathWatcherImpl> weak_factory_{this};
 };
@@ -830,16 +840,7 @@ void FilePathWatcherImpl::CancelAndRunCallbackOnExceededLimit() {
 bool FilePathWatcherImpl::WouldExceedWatchLimit() const {
   DUMP_WILL_BE_CHECK(task_runner()->RunsTasksInCurrentSequence());
 
-  // `watches_` contains inotify watches of all dir components of `target_`.
-  // `recursive_paths_by_watch_` contains inotify watches for sub dirs under
-  // `target_` of a Type::kRecursive watcher and keyed by inotify watches.
-  // All inotify watches used by this FilePathWatcherImpl are either in
-  // `watches_` or as a key in `recursive_paths_by_watch_`. As a result, the
-  // two provide a good estimate on the number of inofiy watches used by this
-  // FilePathWatcherImpl.
-  const size_t number_of_inotify_watches =
-      watches_.size() + recursive_paths_by_watch_.size();
-  return number_of_inotify_watches >= GetMaxNumberOfInotifyWatches();
+  return current_usage() >= GetMaxNumberOfInotifyWatches();
 }
 
 InotifyReader::WatcherEntry FilePathWatcherImpl::GetWatcherEntry() {
@@ -853,7 +854,8 @@ bool FilePathWatcherImpl::Watch(const base::FilePath& path,
   return WatchWithChangeInfo(
       path, WatchOptions{.type = type},
       base::IgnoreArgs<const FilePathWatcher::ChangeInfo&>(
-          base::BindRepeating(std::move(callback))));
+          base::BindRepeating(std::move(callback))),
+      base::DoNothingAs<void(size_t, size_t)>());
 }
 
 bool FilePathWatcherImpl::WatchWithOptions(
@@ -863,13 +865,15 @@ bool FilePathWatcherImpl::WatchWithOptions(
   return WatchWithChangeInfo(
       path, options,
       base::IgnoreArgs<const FilePathWatcher::ChangeInfo&>(
-          base::BindRepeating(std::move(callback))));
+          base::BindRepeating(std::move(callback))),
+      base::DoNothingAs<void(size_t, size_t)>());
 }
 
 bool FilePathWatcherImpl::WatchWithChangeInfo(
     const base::FilePath& path,
     const WatchOptions& options,
-    const FilePathWatcher::CallbackWithChangeInfo& callback) {
+    const FilePathWatcher::CallbackWithChangeInfo& callback,
+    const FilePathWatcher::UsageChangeCallback& usage_callback) {
   DUMP_WILL_BE_CHECK(target_.empty());
 
   set_task_runner(base::SequencedTaskRunner::GetCurrentDefault());
