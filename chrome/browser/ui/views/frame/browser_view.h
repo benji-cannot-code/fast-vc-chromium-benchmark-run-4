a788226fac3e8cd64e406da67aa36cdd10dd023a
Nasko Oskov <nasko@chromium.org>
2024-10-31 16:57:50 +0000

    Revert "[views-ax] Migration of kName attribute in Tab"
    This reverts commit 1eadd223cce221615fa18db6647451a26a0315a2.
    
    Reason for revert: This CL has regressed window name availability to a11y APIs - https://crbug.com/374947846.
    
    Original change's description:
    > [views-ax] Migration of kName attribute in Tab
    >
    > This CL migrates the kName attribute in views to be updated
    > whenever its value should change, rather than querying the value
    > and computing it only when needed.
    >
    > As part of this change, the following views are updated:
    > Tab, BrowserView, PermissionChipView, and TabSlotView. The Tab
    > class extends TabSlotView. BrowserView::GetAccessibleTabLabel
    >  constructs the accessible name for a tab by gathering various
    > properties such as the title, group, memory usage, and possibly
    > other elements like permission chips, to create a comprehensive
    > and accurate label.
    >
    > This CL is part of the ViewsAX project:
    > https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    >
    > Bug: 325137417
    > Change-Id: Id1ca1da2e970c82575dff12bd48434f716d4548f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5836477
    > Reviewed-by: Jacques Newman <janewman@microsoft.com>
    > Commit-Queue: Gaurav Kumar <gauravkum@microsoft.com>
    > Reviewed-by: Elly FJ <ellyjones@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1367340}
    
    Bug: 325137417
    Change-Id: Ica54d78299b40a43ec93b48355da31c618d4e85f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983223
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Benjamin Beaudry <benjamin.beaudry@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1376398}

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -832,7 +832,6 @@ class BrowserView : public BrowserWindow,
   gfx::Point GetThemeOffsetFromBrowserView() const;
 
   void ApplyWatermarkSettings(const std::string& watermark_text);
-  void UpdateAccessibleNameForAllTabs();
 
 #if BUILDFLAG(ENTERPRISE_SCREENSHOT_PROTECTION)
   void ApplyScreenshotSettings(bool allow);
@@ -873,7 +872,6 @@ class BrowserView : public BrowserWindow,
   friend class TopControlsSlideControllerTest;
   FRIEND_TEST_ALL_PREFIXES(BrowserViewTest, BrowserView);
   FRIEND_TEST_ALL_PREFIXES(BrowserViewTest, AccessibleWindowTitle);
-  FRIEND_TEST_ALL_PREFIXES(PermissionChipUnitTest, AccessibleName);
   class AccessibilityModeObserver;
 
   // BrowserUserEducationInterface private methods:
@@ -1086,8 +1084,6 @@ class BrowserView : public BrowserWindow,
 
   // The Browser object we are associated with.
   std::unique_ptr<Browser> browser_;
-
-  base::CallbackListSubscription chip_visibility_subscription_;
 
   // BrowserView layout (LTR one is pictured here).
   //
