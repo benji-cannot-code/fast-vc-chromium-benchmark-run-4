5e14b7d516e82e69596d439b3018de070a246604
Karol Sygiet <sygiet@google.com>
2024-10-31 16:59:20 +0000

    [Japan] Make alternative name types syncable
    Recently the support for autofill stored alternative name types have
    been added. The support for syncing them should be added.
    
    Bug: 359768803
    Change-Id: I2589c68b8e6af11dbfa447b9caa2d7949396019d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975735
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Auto-Submit: Karol Sygiet <sygiet@google.com>
    Reviewed-by: Mahmoud Rashad <mmrashad@google.com>
    Commit-Queue: Mahmoud Rashad <mmrashad@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376401}

--- components/autofill/core/browser/webdata/addresses/contact_info_sync_util_unittest.cc
+++ components/autofill/core/browser/webdata/addresses/contact_info_sync_util_unittest.cc
@@ -62,6 +62,12 @@ AutofillProfile ConstructBaseProfile(
                                            VerificationStatus::kParsed);
   profile.SetRawInfoWithVerificationStatus(NAME_FULL, u"John K. Doe",
                                            VerificationStatus::kUserVerified);
+  profile.SetRawInfoWithVerificationStatus(ALTERNATIVE_FAMILY_NAME, u"Doe",
+                                           VerificationStatus::kParsed);
+  profile.SetRawInfoWithVerificationStatus(ALTERNATIVE_GIVEN_NAME, u"John",
+                                           VerificationStatus::kParsed);
+  profile.SetRawInfoWithVerificationStatus(ALTERNATIVE_FULL_NAME, u"John Doe",
+                                           VerificationStatus::kUserVerified);
 
   // Set address-related values and statuses.
   profile.SetRawInfoWithVerificationStatus(ADDRESS_HOME_CITY, u"Mountain View",
@@ -353,6 +359,12 @@ ContactInfoSpecifics ConstructBaseSpecifics() {
   SetToken(specifics.mutable_name_last_second(), "e",
            ContactInfoSpecifics::PARSED);
   SetToken(specifics.mutable_name_full(), "John K. Doe",
+           ContactInfoSpecifics::USER_VERIFIED);
+  SetToken(specifics.mutable_alternative_family_name(), "Doe",
+           ContactInfoSpecifics::PARSED);
+  SetToken(specifics.mutable_alternative_given_name(), "John",
+           ContactInfoSpecifics::PARSED);
+  SetToken(specifics.mutable_alternative_full_name(), "John Doe",
            ContactInfoSpecifics::USER_VERIFIED);
 
   // Set address-related values and statuses.
@@ -685,7 +697,8 @@ class ContactInfoSyncUtilTest
                                 features::kAutofillUseINAddressModel,
                                 features::kAutofillUseITAddressModel,
                                 features::kAutofillUseNLAddressModel,
-                                features::kAutofillTrackMultipleUseDates},
+                                features::kAutofillTrackMultipleUseDates,
+                                features::kAutofillSupportPhoneticNameForJP},
                                {});
   }
 
