57ca2133937be14b9ee97de339a56b00dbd24996
hsuanling <hsuanling@google.com>
2024-10-31 15:58:30 +0000

    Reland "recorder: Always install SODA before soda session starts"
    Explicitly mark async function `installSoda` as ignored with the
    `void`.
    
    This is a reland of commit 9d622cdfd5d2b34b86f84d9263c3f1856a705821
    
    Original change's description:
    > recorder: Always install SODA before soda session starts
    >
    > This is a workaround to make local soda state updated before
    > `OnSodaUninstalled` event is implemented. When Soda library is removed
    > by other process, the local state is still `installed`, so we should
    > always try installing it to get real state from browser process.
    >
    > Also, to avoid soda install hanging the recording, remove `await` so
    > that the recording can start without soda session.
    >
    > Bug: b:371464701
    > Test: manually - start recording after removing english package
    > Change-Id: I849413316167b253f986809502f10e62b68ed209
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981450
    > Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    > Commit-Queue: Jennifer Ling <hsuanling@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1376098}
    
    Bug: b:371464701
    Change-Id: I4359062825e18e06e4379eb657a28b83fc2368b9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981455
    Reviewed-by: Pi-Hsun Shih <pihsun@chromium.org>
    Commit-Queue: Jennifer Ling <hsuanling@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376348}

--- ash/webui/recorder_app_ui/resources/core/test_helper.ts
+++ ash/webui/recorder_app_ui/resources/core/test_helper.ts
@@ -119,7 +119,7 @@ export class TestHelper {
    */
   static installTranscriptionModel(): void {
     // TODO(hsuanling): Support testing different languages
-    usePlatformHandler().installSoda(LanguageCode.EN_US);
+    void usePlatformHandler().installSoda(LanguageCode.EN_US);
   }
 
   /**
