7624e85619d61472c27ba7624fd69ce4ab5d38e1
Sarah Krakowiak Criel <skrakowi@chromium.org>
2024-10-31 14:51:42 +0000

    Use PlatformNotificationData in the model handler
    The original implementation took in a string for prompting the
    notification content detection model. However, we actually should
    construct the string from within the NotificationContentDetectionModel
    class. This will also make it easier when we want to change the
    notification_data member values from within
    NotificationContentDetectionModel::PostprocessCategories when
    implementing the UI enforcement on suspicious verdicts in the future.
    
    Bug: 374936030
    Change-Id: I7372ddab54f7723b2ed650d48a9a2fee2287f41e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5976688
    Reviewed-by: Joe Mason <joenotcharles@google.com>
    Commit-Queue: Sarah Krakowiak <skrakowi@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376302}

--- components/safe_browsing/content/browser/notification_content_detection/notification_content_detection_model.h
+++ components/safe_browsing/content/browser/notification_content_detection/notification_content_detection_model.h
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/task/sequenced_task_runner.h"
 #include "components/optimization_guide/core/bert_model_handler.h"
+#include "third_party/blink/public/common/notifications/platform_notification_data.h"
+#include "third_party/blink/public/mojom/notifications/notification.mojom.h"
 #include "url/gurl.h"
 
 namespace optimization_guide {
@@ -38,9 +40,9 @@ class NotificationContentDetectionModel
       scoped_refptr<base::SequencedTaskRunner> background_task_runner);
   ~NotificationContentDetectionModel() override;
 
-  // Perform inference on the model with `contents` as input. Pass
-  // `PostprocessCategories` as the `ExecuteModelWithInput` callback.
-  void Execute(const std::u16string& contents);
+  // Perform inference on the model with the provided notification contents.
+  // Pass `PostprocessCategories` as the `ExecuteModelWithInput` callback.
+  void Execute(blink::PlatformNotificationData& notification_data);
 
  private:
   // Log UMA metrics, given the `output` result of model inference.
