f25cbd604e2fe5f4e0c8369691a05057d42fc242
Addison Luh <aluh@chromium.org>
2024-10-31 22:52:25 +0000

    [filter-keys] Implement bounce keys logic
    Once enabled, bounced keys will be filtered. Key presses within a given
    delay since the last key release will be discarded. The delay applies to
    one key at a time, and it can be overridden by pressing a different key.
    Basically, if a different key is pressed within the delay of an earlier
    key, the delay is reset and the new key is accepted immediately, at
    which point a delay is applied to the new key. This makes it possible to
    type words like "repetitive" faster without waiting between the
    repeating "titi".
    
    Keys that are synthetic, such as the auto repeated keys when a key is
    held down or virtual keyboard inputs, are not filtered.
    
    Bug: b:375218304
    Change-Id: I22a8d40c362a74fd20d89323f5c3cbe57e5bb671
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974440
    Commit-Queue: Addison Luh <aluh@chromium.org>
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376651}

--- ash/accessibility/filter_keys_event_rewriter.h
+++ ash/accessibility/filter_keys_event_rewriter.h
@@ -6,7 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_ACCESSIBILITY_FILTER_KEYS_EVENT_REWRITER_H_
 #define ASH_ACCESSIBILITY_FILTER_KEYS_EVENT_REWRITER_H_
 
+#include <optional>
+
 #include "ash/ash_export.h"
+#include "base/containers/flat_set.h"
+#include "base/time/time.h"
 #include "ui/events/event_rewriter.h"
 
 namespace ash {
@@ -14,13 +18,19 @@ namespace ash {
 // EventRewriter that delays or cancels some keyboard events.
 class ASH_EXPORT FilterKeysEventRewriter : public ui::EventRewriter {
  public:
+  static constexpr base::TimeDelta kDefaultBounceKeysDelay =
+      base::Milliseconds(500);
+
   FilterKeysEventRewriter();
   FilterKeysEventRewriter(const FilterKeysEventRewriter&) = delete;
   FilterKeysEventRewriter& operator=(const FilterKeysEventRewriter&) = delete;
   ~FilterKeysEventRewriter() override;
 
   void SetBounceKeysEnabled(bool enabled);
-  bool IsBounceKeysEnabled();
+  bool IsBounceKeysEnabled() const;
+
+  void SetBounceKeysDelay(base::TimeDelta delay);
+  const base::TimeDelta& GetBounceKeysDelay() const;
 
  private:
   // ui::EventRewriter:
@@ -28,7 +38,27 @@ class ASH_EXPORT FilterKeysEventRewriter : public ui::EventRewriter {
       const ui::Event& event,
       const Continuation continuation) override;
 
+  // Returns true if a key entry was erased.
+  bool EraseNextKeyReleaseToDiscard(ui::DomCode dom_code);
+
+  // Resets bounce keys state, used when disabling bounce keys functionality.
+  void ResetBounceKeysState();
+
+  // Enables bounce keys functionality.
   bool bounce_keys_enabled_ = false;
+  // Delay until subsequent key strokes are accepted for bounce keys.
+  base::TimeDelta bounce_keys_delay_ = kDefaultBounceKeysDelay;
+
+  // The last key that had a key pressed event.
+  // Used to reset bounce keys delay when a different key is pressed.
+  std::optional<ui::DomCode> last_pressed_key_;
+  // The time of the last key released event.
+  // Used to calculate whether bounce keys delay has passed.
+  std::optional<base::TimeTicks> last_released_time_;
+  // Set of keys where the key released event should be discarded.
+  // Used to pair key released events with discarded key pressed events,
+  // e.g. for when key press and release events cross the delay boundary.
+  base::flat_set<ui::DomCode> next_key_release_to_discard_;
 };
 
 }  // namespace ash
