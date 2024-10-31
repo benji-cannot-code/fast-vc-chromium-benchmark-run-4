ddadba128401f1b115b4d8a0ab46da86ff2a6e5a
Morten Stenshorne <mstensho@chromium.org>
2024-10-31 21:59:20 +0000

    [CodeHealth] Remove stale base::Feature ChromeOSAmbientModePhotoPreview
    This flag didn't control anything.
    
    Bug: 356234180
    Change-Id: I038681521d0017d79955208aae4a36bf0c55cf35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979809
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376619}

--- ash/constants/ash_features.h
+++ ash/constants/ash_features.h
@@ -37,8 +37,6 @@ BASE_DECLARE_FEATURE(kAlwaysReinstallSystemWebApps);
 COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kAmbientModeDevUseProdFeature);
 COMPONENT_EXPORT(ASH_CONSTANTS)
-BASE_DECLARE_FEATURE(kAmbientModePhotoPreviewFeature);
-COMPONENT_EXPORT(ASH_CONSTANTS)
 BASE_DECLARE_FEATURE(kAmbientModeManagedScreensaver);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kAnnotatorMode);
 COMPONENT_EXPORT(ASH_CONSTANTS) BASE_DECLARE_FEATURE(kApnRevamp);
@@ -1084,7 +1082,6 @@ bool IsEapDefaultCasWithoutSubjectVerificationAllowed();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAltClickAndSixPackCustomizationEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAmbientModeDevUseProdEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAmbientModeManagedScreensaverEnabled();
-COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAmbientModePhotoPreviewEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAnnotatorModeEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsApnRevampAndPoliciesEnabled();
 COMPONENT_EXPORT(ASH_CONSTANTS) bool IsAllowApnModificationPolicyEnabled();
