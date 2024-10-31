2a8ff9f978eea686830510328ff4d2eea1adce96
Harsh Shukla <hvs@google.com>
2024-10-31 18:25:26 +0000

    Populating CVC returned in runtime retrieval
    Design doc:go/jp-ewallet-fronting-card-chrome-design
    
    Bug: 369472865
    Change-Id: Ic573e3741730b354331f1d92b13c33a41067de39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5969757
    Reviewed-by: Siyu An <siyua@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Vinny Persky <vinnypersky@google.com>
    Commit-Queue: Harsh Shukla <hvs@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376473}

--- components/autofill/core/browser/payments/credit_card_risk_based_authenticator.cc
+++ components/autofill/core/browser/payments/credit_card_risk_based_authenticator.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/core/browser/payments/payments_network_interface.h"
 #include "components/autofill/core/browser/payments/payments_util.h"
 #include "components/autofill/core/browser/personal_data_manager.h"
+#include "components/autofill/core/common/autofill_payments_features.h"
 
 namespace autofill {
 namespace {
@@ -132,6 +133,12 @@ void CreditCardRiskBasedAuthenticator::OnUnmaskResponseReceived(
           RiskBasedAuthenticationResponse::Result::kNoAuthenticationRequired;
       card_.SetNumber(base::UTF8ToUTF16(response_details.real_pan));
       card_.set_record_type(CreditCard::RecordType::kFullServerCard);
+      if (!response_details.dcvv.empty() &&
+          (unmask_request_details_->card
+               .card_info_retrieval_enrollment_state() ==
+           CreditCard::CardInfoRetrievalEnrollmentState::kRetrievalEnrolled)) {
+        card_.set_cvc(base::UTF8ToUTF16(response_details.dcvv));
+      }
       response.card = card_;
 
       autofill_metrics::LogRiskBasedAuthResult(
