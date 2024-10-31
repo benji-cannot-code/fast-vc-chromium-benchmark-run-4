57b30f35e18a4fcb15193bc8ce1f111b37a31381
Jan Keitel <jkeitel@google.com>
2024-10-31 07:26:44 +0000

    Avoid unsafe buffer usage in feature_entry.cc
    Bug: 40285824
    Change-Id: I0c98dce1408e4a46ee1bbe61b13a85c61994c2d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961456
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376141}

--- components/flags_ui/feature_entry.cc
+++ components/flags_ui/feature_entry.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "components/flags_ui/feature_entry.h"
 
 #include "base/check_op.h"
@@ -119,11 +114,9 @@ std::u16string FeatureEntry::DescriptionForOption(int index) const {
       || type == FeatureEntry::PLATFORM_FEATURE_NAME_VALUE
 #endif
   ) {
-    const char* const kEnableDisableDescriptions[] = {
-        kGenericExperimentChoiceDefault,
-        kGenericExperimentChoiceEnabled,
-        kGenericExperimentChoiceDisabled,
-    };
+    const auto kEnableDisableDescriptions = std::to_array<const char*>(
+        {kGenericExperimentChoiceDefault, kGenericExperimentChoiceEnabled,
+         kGenericExperimentChoiceDisabled});
     description = kEnableDisableDescriptions[index];
   } else if (type == FeatureEntry::FEATURE_WITH_PARAMS_VALUE
 #if BUILDFLAG(IS_CHROMEOS_ASH)
