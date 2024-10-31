b1d77e3c283a51644fd483025e02b34ecac40596
vikas soni <vikassoni@chromium.org>
2024-10-31 19:49:28 +0000

    Revert "Import/Export AcceleratedStaticBitmapImage via ClientSharedImage"
    This reverts commit 92a848ae7ac4c561f7d6e027d3b53181a5d654c9.
    
    Reason for revert: This CL is one of the reason for causing meet hang
    when using speaker spotlight.
    
    Original change's description:
    > Import/Export AcceleratedStaticBitmapImage via ClientSharedImage
    >
    > This CL refactors the AcceleratedStaticBitmapImage mojom type to use
    > ClientSharedImage instead of MailboxHolder, and also refactors
    > AcceleratedImageInfo and CreateFromExternalMailbox() accordingly.
    >
    > Bug: 1494911
    > Change-Id: I17fad839db8ba65d2f2c2b1acf7184178920de2e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900826
    > Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    > Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    > Reviewed-by: Joe Mason <joenotcharles@google.com>
    > Commit-Queue: Mingjing Zhang <mjzhang@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1364235}
    
    Bug: 1494911, 374812177
    Change-Id: I647302cead96412d3f8ea8ee4195ef789ebf4c5a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982193
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376531}

--- third_party/blink/public/common/messaging/accelerated_image_info.h
+++ third_party/blink/public/common/messaging/accelerated_image_info.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define THIRD_PARTY_BLINK_PUBLIC_COMMON_MESSAGING_ACCELERATED_IMAGE_INFO_H_
 
 #include "base/functional/callback.h"
-#include "gpu/command_buffer/client/client_shared_image.h"
 #include "gpu/command_buffer/common/mailbox_holder.h"
 #include "gpu/command_buffer/common/shared_image_usage.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
@@ -21,7 +20,8 @@ namespace blink {
 // See third_party/blink/public/mojom/messaging/static_bitmap_image.mojom
 // for details.
 struct BLINK_COMMON_EXPORT AcceleratedImageInfo {
-  gpu::ExportedSharedImage shared_image;
+  gpu::MailboxHolder mailbox_holder;
+  gpu::SharedImageUsageSet usage;
   SkImageInfo image_info;
   bool is_origin_top_left;
   bool supports_display_compositing;
