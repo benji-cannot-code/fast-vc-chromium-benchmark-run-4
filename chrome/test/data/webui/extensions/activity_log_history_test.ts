4213b0f39b1149557c049fabc383eb6e257e069f
rbpotter <rbpotter@chromium.org>
2024-10-31 18:31:33 +0000

    Extensions: Migrate activity-log-*-item elements to Lit
    Bug: 40943652
    Change-Id: I4ef65114b8f91304e5ca809a90080f4a379e7f36
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5976702
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376481}

--- chrome/test/data/webui/extensions/activity_log_history_test.ts
+++ chrome/test/data/webui/extensions/activity_log_history_test.ts
@@ -9,6 +9,7 @@ import type {ActivityLogHistoryElement} from 'chrome://extensions/extensions.js'
 import {ActivityLogPageState} from 'chrome://extensions/extensions.js';
 import {flush} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 import {assertEquals, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 import {TestService} from './test_service.js';
 import {testVisible} from './test_util.js';
@@ -330,6 +331,7 @@ suite('ExtensionsActivityLogHistoryTest', function() {
     await setupActivityLogHistory();
 
     flush();
+    await microtasksFinished();
 
     const expandableItems = Array.from(getHistoryItems())
                                 .filter(
@@ -347,6 +349,7 @@ suite('ExtensionsActivityLogHistoryTest', function() {
         .querySelector<HTMLElement>('#expand-all-button')!.click();
 
     flush();
+    await microtasksFinished();
     assertEquals(2, getExpandedItems().length);
 
     // Collapse all items.
@@ -356,6 +359,7 @@ suite('ExtensionsActivityLogHistoryTest', function() {
         .querySelector<HTMLElement>('#collapse-all-button')!.click();
 
     flush();
+    await microtasksFinished();
     assertEquals(0, getExpandedItems().length);
   });
 
