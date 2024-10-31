15a0545544e2c743ae73f40d97e479fd934134b8
mlcui <mlcui@google.com>
2024-10-31 23:35:38 +0000

    scanner: Populate actions from server before executing
    We previously assumed that the server responded with fully populated
    actions on the initial actions response, but with recent proposals (see
    attached bug) the server may respond with unpopulated actions on the
    initial actions response.
    
    Re-send a request to the server to populate the action before executing
    actions.
    
    This CL also changes `ScannerActionViewModel::ToCallback` to return a
    repeating callback, which fixes the issue of users not being able to
    retry actions after they have been clicked in the UI.
    
    Bug: b:376165802
    Change-Id: I6a6a2100d40c32b10bedb34f0111eccc2062ca28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981410
    Commit-Queue: Michael Cui <mlcui@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376671}

--- ash/scanner/scanner_session.h
+++ ash/scanner/scanner_session.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "ash/scanner/scanner_action_view_model.h"
 #include "ash/scanner/scanner_command_delegate.h"
+#include "ash/scanner/scanner_unpopulated_action.h"
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/ref_counted_memory.h"
@@ -53,9 +54,19 @@ class ASH_EXPORT ScannerSession : public ScannerCommandDelegate {
   google_apis::RequestSender* GetGoogleApisRequestSender() override;
 
  private:
-  void OnActionsReturned(FetchActionsCallback callback,
-                         std::unique_ptr<manta::proto::ScannerOutput> output,
-                         manta::MantaStatus status);
+  void OnActionsReturned(
+      scoped_refptr<base::RefCountedMemory> downscaled_jpeg_bytes,
+      FetchActionsCallback callback,
+      std::unique_ptr<manta::proto::ScannerOutput> output,
+      manta::MantaStatus status);
+
+  // Populates the selected action. Used as a
+  // `ScannerUnpopulatedAction::PopulateToProtoCallback` once bound with the
+  // possibly-downscaled JPEG bytes.
+  void PopulateAction(
+      scoped_refptr<base::RefCountedMemory> downscaled_jpeg_bytes,
+      manta::proto::ScannerAction unpopulated_action,
+      ScannerUnpopulatedAction::PopulatedProtoCallback callback);
 
   const raw_ptr<ScannerProfileScopedDelegate> delegate_;
 
