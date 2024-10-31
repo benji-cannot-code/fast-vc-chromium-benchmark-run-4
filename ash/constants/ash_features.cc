70ea824012542db08b5668df65783f37d4007608
Curtis McMullan <curtismcmullan@chromium.org>
2024-10-31 01:05:26 +0000

    scanner: enable with dogfood flag
    The feature should be enabled if the dogfood flag is enabled.
    
    BUG=b:369272173
    
    Change-Id: I9b1812072c4370e067c3f296e869a47b32103e70
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977623
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376009}

--- ash/constants/ash_features.cc
+++ ash/constants/ash_features.cc
@@ -4693,7 +4693,8 @@ bool IsScalableShelfPodsEnabled() {
 }
 
 bool IsScannerEnabled() {
-  return base::FeatureList::IsEnabled(kScannerUpdate);
+  return base::FeatureList::IsEnabled(kScannerUpdate) ||
+         base::FeatureList::IsEnabled(kScannerDogfood);
 }
 
 bool IsSeaPenDemoModeEnabled() {
