9bea109de3efdd1d56d1b95ae426408235eeb9f7
Jan Keitel <jkeitel@google.com>
2024-10-31 07:31:21 +0000

    Do not override OnThemeChanged in RichAnswersView
    It is not necessary as long as all colors are set using ColorIds.
    
    Change-Id: Id81663a816c296282e293e54e84bcad9d92d1507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973004
    Reviewed-by: Jason Thai <jasontt@chromium.org>
    Auto-Submit: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376143}

--- chrome/browser/ui/ash/quick_answers/ui/rich_answers_view.h
+++ chrome/browser/ui/ash/quick_answers/ui/rich_answers_view.h
@@ -55,7 +55,6 @@ class RichAnswersView : public views::View, public views::WidgetObserver {
   // views::View:
   void AddedToWidget() override;
   void OnKeyEvent(ui::KeyEvent* event) override;
-  void OnThemeChanged() override;
 
   // views::WidgetObserver:
   void OnWidgetActivationChanged(views::Widget* widget, bool active) override;
@@ -98,7 +97,6 @@ class RichAnswersView : public views::View, public views::WidgetObserver {
   raw_ptr<views::BoxLayoutView> content_view_ = nullptr;
   raw_ptr<views::ImageButton> settings_button_ = nullptr;
   raw_ptr<views::ImageView> vector_icon_ = nullptr;
-  raw_ptr<views::Link> search_link_label_ = nullptr;
 
   base::ScopedObservation<views::Widget, views::WidgetObserver>
       widget_observation_{this};
