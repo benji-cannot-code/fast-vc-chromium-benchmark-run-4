c6e74bd59dd30c15406f9bededaa4036300b0b46
Wes Okuhara <wesokuhara@google.com>
2024-10-31 22:04:18 +0000

    CrOS Settings: Fix consistent-type-import ESLint rule for people page
    This change complies with the top-level ESLint spec, specifically the
    "consistent-type-import" rule. It helps identify which import lines are
    only used for type-checking and thus can be excluded from the JS output.
    Also it helps to remove unintentional side effects.
    
    Bug: b:372733928
    Test: git cl presubmit --force
    Change-Id: Ie03ebe4f034e9967923976e516f79d6fa1da6ca2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980921
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376625}

--- chrome/browser/resources/ash/settings/os_people_page/graduation/mojo_interface_provider.ts
+++ chrome/browser/resources/ash/settings/os_people_page/graduation/mojo_interface_provider.ts
@@ -3,7 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {GraduationHandler, GraduationHandlerInterface} from '../../mojom-webui/graduation_handler.mojom-webui.js';
+import type {GraduationHandlerInterface} from '../../mojom-webui/graduation_handler.mojom-webui.js';
+import {GraduationHandler} from '../../mojom-webui/graduation_handler.mojom-webui.js';
 
 let testHandlerProvider: GraduationHandlerInterface|null = null;
 
