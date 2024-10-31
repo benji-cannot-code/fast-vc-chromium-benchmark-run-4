786a280d8eb2e2c2192938369f5356cc168e0d0c
Amanda Lin Dietz <aldietz@google.com>
2024-10-31 22:42:33 +0000

    [FaceGaze] Send repeat keydown events while gesture is held for KeyPressMacro
    * Previous behavior was:
    1. Send a key down event when gesture is detected.
    2. Send a key up event when gesture is no longer detected.
    
    This lead to only one key press being generated on desktop ChromeOS,
    even if the gesture were being held and a user might expect to
    simulate a physical key press.
    
    See key event viewer:
    https://screenshot.googleplex.com/6ZHQsWTP7A7vo8d
    
    * New behavior is:
    1. Send a key down event when gesture is detected.
    2. Send repeated key down events when gesture is still detected
    on a regular interval.
    3. Send a key up event when gesture is no longer detected.
    
    This leads to a behavior like a physical key press that is held as
    long as the gesture is held:
    https://screenshot.googleplex.com/85enCYFvGrXAEtm
    
    * Fix tests that were impacted by change to key event API.
    * Add test for EventGenerator.
    * Fix test base and add FaceGazeTest for repeat key events.
    
    Fixed: 376113229
    Change-Id: I1495db842bed941d3ef52efdb73d4f95b9a6d211
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974635
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Reviewed-by: Gina Domergue <gdomergue@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376647}

--- chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_input_handler_test.js
+++ chrome/browser/resources/chromeos/accessibility/chromevox/background/braille/braille_input_handler_test.js
@@ -435,8 +435,8 @@ ChromeVoxBrailleInputHandlerTest = class extends ChromeVoxE2ETest {
     chrome.runtime.onConnectExternal = new FakeChromeEvent();
     this.port = new FakePort();
     chrome.accessibilityPrivate.sendSyntheticKeyEvent =
-        (event, useRewriters, opt_callback) =>
-            this.storeKeyEvent(event, useRewriters, opt_callback);
+        (event, useRewriters, isRepeat, opt_callback) =>
+            this.storeKeyEvent(event, useRewriters, isRepeat, opt_callback);
     chrome.accessibilityPrivate.SyntheticKeyboardEventType = {};
     chrome.accessibilityPrivate.SyntheticKeyboardEventType.KEYDOWN = 'keydown';
     chrome.accessibilityPrivate.SyntheticKeyboardEventType.KEYUP = 'keyup';
@@ -510,7 +510,7 @@ ChromeVoxBrailleInputHandlerTest = class extends ChromeVoxE2ETest {
         this.inputHandler.getExpansionType());
   }
 
-  storeKeyEvent(event, useRewriters, opt_callback) {
+  storeKeyEvent(event, useRewriters, isRepeat, opt_callback) {
     const storedCopy = {keyCode: event.keyCode};
     if (event.type === 'keydown') {
       this.keyEvents.push(storedCopy);
--- chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
+++ chrome/browser/resources/chromeos/accessibility/definitions/accessibility_private_mv2.d.ts
@@ -411,7 +411,8 @@ declare global {
           callback: SetNativeChromeVoxArcSupportForCurrentAppCallback): void;
 
       export function sendSyntheticKeyEvent(
-          keyEvent: SyntheticKeyboardEvent, useRewriters?: boolean): void;
+          keyEvent: SyntheticKeyboardEvent, useRewriters?: boolean,
+          isRepeat?: boolean): void;
 
       export function enableMouseEvents(enabled: boolean): void;
 
