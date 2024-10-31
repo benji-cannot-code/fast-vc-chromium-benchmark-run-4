c54b0baa3335c36788c30d9ff785ee2883b5fa12
Tsuyoshi Horo <horo@chromium.org>
2024-10-31 00:59:17 +0000

    Add more tests for translate-kit command lines
    Bug: 374631433
    Change-Id: Ied38ec3285c7bced4f0d534a825886936c4358d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974039
    Reviewed-by: Ming-Ying Chung <mych@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376005}

--- chrome/browser/on_device_translation/component_manager.cc
+++ chrome/browser/on_device_translation/component_manager.cc
@@ -227,7 +227,7 @@ ComponentManager::GetLanguagePackInfoFromCommandLine() {
 #endif  // BUILDFLAG(IS_WIN)
                         base::KEEP_WHITESPACE, base::SPLIT_WANT_ALL);
   if (splitted_strings.size() % 3 != 0) {
-    LOG(ERROR) << "Invalid --translate-kit-packages flag";
+    LOG(ERROR) << "Invalid --" << kTranslateKitPackagePaths << " flag.";
     return std::nullopt;
   }
 
@@ -235,7 +235,7 @@ ComponentManager::GetLanguagePackInfoFromCommandLine() {
   auto it = splitted_strings.begin();
   while (it != splitted_strings.end()) {
     if (!base::IsStringASCII(*it) || !base::IsStringASCII(*(it + 1))) {
-      LOG(ERROR) << "Invalid --translate-kit-packages flag";
+      LOG(ERROR) << "Invalid --" << kTranslateKitPackagePaths << " flag.";
       return std::nullopt;
     }
     LanguagePackInfo package;
