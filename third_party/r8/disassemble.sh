d8cf306b8183c16e5aeaf01b6cc853bace3fa937
Sam Maier <smaier@chromium.org>
2024-10-31 14:32:19 +0000

    Update r8.jar references to cipd/lib folder
    Change-Id: I5bb9dec0c72c93fe5ecc9b6b4d0bbbdcdda74e45
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5978894
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Auto-Submit: Sam Maier <smaier@chromium.org>
    Commit-Queue: Sam Maier <smaier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376291}

--- third_party/r8/disassemble.sh
+++ third_party/r8/disassemble.sh
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 R8DIR=$(dirname $0)
 set -x
-exec $R8DIR/../jdk/current/bin/java -cp $R8DIR/lib/r8.jar \
+exec $R8DIR/../jdk/current/bin/java -cp $R8DIR/cipd/lib/r8.jar \
   com.android.tools.r8.Disassemble "${@:---help}"
