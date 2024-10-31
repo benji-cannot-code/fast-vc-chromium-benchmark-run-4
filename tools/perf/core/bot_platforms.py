80259dc8027d6887606143ef424c25159e94a6bf
Farid (Mojtaba) Faridzad <faridzad@google.com>
2024-10-31 05:33:53 +0000

    Revert "Run Crossbench loadline-phone on an Android Pixel 6 in shard# 9."
    This reverts commit a2c0ff5119acae7581f0ad1d0f3eb7e1c6edf50c.
    
    Reason for revert: b/376543980
    
    Original change's description:
    > Run Crossbench loadline-phone on an Android Pixel 6 in shard# 9.
    >
    > Run Crossbench loadline-phone benchmark in Waterfall on an Android Pixel 6 phone.
    >
    > Bug: 371988599
    > Change-Id: Ic54eee593b557d58f611182491f39d0dfc39fc06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977356
    > Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    > Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1375810}
    
    Bug: 371988599
    Change-Id: Iee3f5c987c933f8e66801fcf5eea80865ac35e82
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981014
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Reviewed-by: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376116}

--- tools/perf/core/bot_platforms.py
+++ tools/perf/core/bot_platforms.py
@@ -395,13 +395,6 @@ def _crossbench_jetstream2_1(estimated_runtime=180):
                           estimated_runtime=estimated_runtime)
 
 
-def _crossbench_loadline_phone(estimated_runtime=60, arguments=None):
-  return CrossbenchConfig('loadline_phone.crossbench',
-                          'loadline-phone',
-                          estimated_runtime=estimated_runtime,
-                          arguments=arguments)
-
-
 _CROSSBENCH_JETSTREAM_SPEEDOMETER = frozenset([
     _crossbench_jetstream2_1(),
     _crossbench_speedometer3_0(),
@@ -418,9 +411,9 @@ _CROSSBENCH_BENCHMARKS_ALL = frozenset([
     _crossbench_jetstream2_1(),
 ])
 
-_CROSSBENCH_ANDROID = frozenset([
+# TODO(b/338630584): Remove it when other benchmarks can be run on Android.
+_CROSSBENCH_SPEEDOMETER = frozenset([
     _crossbench_speedometer3_0(arguments=['--fileserver']),
-    _crossbench_loadline_phone(),
 ])
 
 _CHROME_HEALTH_BENCHMARK_CONFIGS_DESKTOP = PerfSuite(
@@ -890,7 +883,7 @@ ANDROID_PIXEL6 = PerfPlatform('android-pixel6-perf',
                               14,
                               'android',
                               executables=_ANDROID_PIXEL6_EXECUTABLE_CONFIGS,
-                              crossbench=_CROSSBENCH_ANDROID)
+                              crossbench=_CROSSBENCH_SPEEDOMETER)
 ANDROID_PIXEL6_PGO = PerfPlatform(
     'android-pixel6-perf-pgo',
     'Android U',
@@ -898,7 +891,7 @@ ANDROID_PIXEL6_PGO = PerfPlatform(
     8,
     'android',
     executables=_ANDROID_PIXEL6_PGO_EXECUTABLE_CONFIGS,
-    crossbench=_CROSSBENCH_ANDROID)
+    crossbench=_CROSSBENCH_SPEEDOMETER)
 ANDROID_PIXEL6_PRO = PerfPlatform(
     'android-pixel6-pro-perf',
     'Android T',
