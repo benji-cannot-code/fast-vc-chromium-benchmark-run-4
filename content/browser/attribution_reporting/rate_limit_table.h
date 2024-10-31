240c3ce099e5732e14a7a977ca93c79f97fc6f56
Andrew Paseltiner <apaseltiner@chromium.org>
2024-10-31 15:46:37 +0000

    Remove unnecessary flat_set creation from AllowedForReportingOriginLimit
    Change-Id: Ibff440904e2b6b42c10e336bf02f95f408427b55
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982450
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Auto-Submit: Andrew Paseltiner <apaseltiner@chromium.org>
    Commit-Queue: Nan Lin <linnan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376338}

--- content/browser/attribution_reporting/rate_limit_table.h
+++ content/browser/attribution_reporting/rate_limit_table.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <vector>
 
-#include "base/containers/flat_set.h"
 #include "base/containers/span.h"
 #include "base/memory/raw_ref.h"
 #include "base/sequence_checker.h"
@@ -186,7 +185,7 @@ class CONTENT_EXPORT RateLimitTable {
       bool is_source,
       const CommonSourceInfo& common_info,
       base::Time time,
-      const base::flat_set<net::SchemefulSite>& destination_sites)
+      base::span<const net::SchemefulSite> destination_sites)
       VALID_CONTEXT_REQUIRED(sequence_checker_);
 
   // Returns false on failure.
