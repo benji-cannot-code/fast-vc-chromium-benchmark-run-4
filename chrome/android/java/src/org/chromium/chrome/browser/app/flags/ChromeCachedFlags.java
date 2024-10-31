53acfef55d943e3c4bc9608506a4eeefe34aff15
Jinsuk Kim <jinsukkim@chromium.org>
2024-10-31 17:00:29 +0000

    [MIM] Do not cache CctSignInPrompt feature flag
    The cached flag was introduced for some tests to pass. They are not
    related to the feature itself, but the feature-related class was
    being instiated and an error was thrown due to the native layer
    not yet initialized.
    
    This CL fixed the issue simply by switching the order of some condition
    checks so those test won't attempt to run the feature. In this way,
    the cached version of the feature flag can be safely removed.
    
    Bug: crbug.com/365544095
    Change-Id: Ib278d9b855f27ef29c8e9b0f8d1d4a962f48b46f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973045
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Commit-Queue: Jinsuk Kim <jinsukkim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376403}

--- chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
+++ chrome/android/java/src/org/chromium/chrome/browser/app/flags/ChromeCachedFlags.java
@@ -52,7 +52,6 @@ import org.chromium.components.cached_flags.CachedFlag;
 import org.chromium.components.cached_flags.CachedFlagUtils;
 import org.chromium.components.cached_flags.CachedFlagsSafeMode;
 import org.chromium.components.omnibox.OmniboxFeatures;
-import org.chromium.components.signin.SigninFeatureMap;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -89,8 +88,7 @@ public class ChromeCachedFlags {
         CachedFlagUtils.cacheNativeFlags(
                 ChromeFeatureList.sFlagsCachedFullBrowser,
                 OmniboxFeatures.getFieldTrialsToCache(),
-                ModalDialogFeatureMap.sCachedFlags,
-                SigninFeatureMap.sCachedFlags);
+                ModalDialogFeatureMap.sCachedFlags);
         cacheAdditionalNativeFlags();
 
         List<CachedFieldTrialParameter<?>> fieldTrialsToCache =
