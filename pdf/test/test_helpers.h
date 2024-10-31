4c6e0c1bf4e2877864a211ba88e1cb8fbf83f095
Alan Screen <awscreen@chromium.org>
2024-10-31 14:53:20 +0000

    [PDF Ink Signatures] Move CheckPdfRendering() into helpers
    Move the CheckPdfRendering() helper function out of
    pdf_ink_module_unittest.cc and into test_helpers.cc, so that it is
    available for use by other tests.
    
    Change-Id: Ifbf9f211b15d1327696e7fe22df7bf7be9840354
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963119
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376304}

--- pdf/test/test_helpers.h
+++ pdf/test/test_helpers.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PDF_TEST_TEST_HELPERS_H_
 #define PDF_TEST_TEST_HELPERS_H_
 
+#include "base/containers/span.h"
 #include "base/files/file_path.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -35,6 +36,14 @@ testing::AssertionResult MatchesPngFile(
 testing::AssertionResult FuzzyMatchesPngFile(
     const SkImage* actual_image,
     const base::FilePath& expected_png_file);
+
+// Takes `pdf_data` and loads it using PDFium. Then renders the page at
+// `page_index` to a bitmap of `size_in_points` and checks if it matches
+// `expected_png_file`.
+void CheckPdfRendering(base::span<const uint8_t> pdf_data,
+                       int page_index,
+                       const gfx::Size& size_in_points,
+                       const base::FilePath& expected_png_file);
 
 // Creates a Skia surface with dimensions `size` and filled with `color`.
 sk_sp<SkSurface> CreateSkiaSurfaceForTesting(const gfx::Size& size,
