cb6c19997a9bc37a7a94762e2557a58f581cf26e
Federica Germinario <fedegermi@google.com>
2024-10-31 15:39:48 +0000

    [ios] Improve logic in AppMetricsAppStateAgent for mim
    Note that profile specific logic in AppMetricsAppStateAgent will
    be moved to a profileAgent in a different CL.
    
    Bug: 364233323
    Change-Id: I9514415c348585a362738a81c1ccafb51325fcf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979813
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376335}

--- ios/chrome/app/app_metrics_app_state_agent.mm
+++ ios/chrome/app/app_metrics_app_state_agent.mm
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/app/tests_hook.h"
 #import "ios/chrome/browser/metrics/model/ios_profile_session_durations_service.h"
 #import "ios/chrome/browser/metrics/model/ios_profile_session_durations_service_factory.h"
-#import "ios/chrome/browser/shared/coordinator/scene/scene_controller.h"
 #import "ios/chrome/browser/shared/coordinator/scene/scene_state.h"
 #import "ios/chrome/browser/shared/model/application_context/application_context.h"
 #import "ios/chrome/browser/shared/model/profile/profile_manager_ios.h"
@@ -28,10 +27,7 @@ NSString* const kDeferredInitializationBlocksComplete =
     @"DeferredInitializationBlocksComplete";
 }  // namespace
 
-@interface AppMetricsAppStateAgent () <SceneStateObserver>
-
-// Observed app state.
-@property(nonatomic, weak) AppState* appState;
+@interface AppMetricsAppStateAgent ()
 
 // This flag is set when the first scene has activated since the startup, and
 // never reset during the app's lifetime.
@@ -45,24 +41,12 @@ NSString* const kDeferredInitializationBlocksComplete =
 
 @implementation AppMetricsAppStateAgent
 
-#pragma mark - AppStateAgent
-
-- (void)setAppState:(AppState*)appState {
-  // This should only be called once!
-  DCHECK(!_appState);
-
-  _appState = appState;
-  [appState addObserver:self];
-}
-
 #pragma mark - AppStateObserver
-
-- (void)appState:(AppState*)appState sceneConnected:(SceneState*)sceneState {
-  [sceneState addObserver:self];
-}
 
 - (void)appState:(AppState*)appState
     didTransitionFromInitStage:(AppInitStage)previousInitStage {
+  [super appState:appState didTransitionFromInitStage:previousInitStage];
+
   if (appState.initStage ==
       AppInitStage::kBrowserObjectsForBackgroundHandlers) {
     // Log session start if the app is already foreground.
@@ -76,6 +60,8 @@ NSString* const kDeferredInitializationBlocksComplete =
 
 - (void)sceneState:(SceneState*)sceneState
     transitionedToActivationLevel:(SceneActivationLevel)level {
+  [super sceneState:sceneState transitionedToActivationLevel:level];
+
   if (!self.firstSceneHasConnected) {
     self.appState.startupInformation.firstSceneConnectionTime =
         base::TimeTicks::Now();
@@ -131,7 +117,7 @@ NSString* const kDeferredInitializationBlocksComplete =
   }
 }
 
-#pragma mark - private
+#pragma mark - Private
 
 - (void)handleSessionStart {
   self.appState.lastTimeInForeground = base::TimeTicks::Now();
