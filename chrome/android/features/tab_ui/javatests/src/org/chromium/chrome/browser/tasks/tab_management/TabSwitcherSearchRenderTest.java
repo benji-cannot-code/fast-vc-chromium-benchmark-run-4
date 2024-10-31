94bd8ae4575c5de14c8b7d591da584887e1c9acd
Brandon Fong <bjfong@google.com>
2024-10-31 17:35:51 +0000

    [hub] Introduce a back arrow in place of status icon hub search activity
    Add a back button as the status icon for hub's search activity.
    
    Bug: 376264282
    Change-Id: Ide4f790776b74eb95a79c3d9c8a600d7dd9d4e5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973070
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Brandon Wylie <wylieb@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376430}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
@@ -50,7 +50,7 @@ public class TabSwitcherSearchRenderTest {
     @Rule
     public ChromeRenderTestRule mRenderTestRule =
             ChromeRenderTestRule.Builder.withPublicCorpus()
-                    .setRevision(3)
+                    .setRevision(4)
                     .setBugComponent(Component.UI_BROWSER_MOBILE_TAB_SWITCHER)
                     .build();
 
--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -672,7 +672,13 @@ public class SearchActivity extends AsyncInitializationActivity
     }
 
     private void setHubSearchBoxVisualElements() {
-        mLocationBarCoordinator.getStatusCoordinator().setShowStatusView(false);
+        mLocationBarCoordinator
+                .getStatusCoordinator()
+                .setOnStatusIconNavigateBackButtonPress(
+                        (View v) ->
+                                finish(
+                                        TerminationReason.BACK_KEY_PRESSED,
+                                        /* loadUrlParams= */ null));
     }
 
     private void setIncognitoColorScheme() {
