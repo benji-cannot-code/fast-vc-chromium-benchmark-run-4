a0a234970933f18439a388a4c880f3a2f5584b35
Noémie St-Onge <noemies@google.com>
2024-10-31 19:10:30 +0000

    [iOS] Show temp welcome screen for fixing the degraded recoverability
    This CL adds the temporary "fix degraded recoverability" and welcome
    screen to the SDS fetching flow. This screen only contains a button
    for now. A follow-up CL will take care of updating its content to match
    the mocks.
    
    Video:
    https://drive.google.com/file/d/1yM8Mk7Kk_y9sIL8AyL8Vw-cXi9Ogivl5/view?usp=sharing&resourcekey=0-FPkI-qsX4bkXMOSJ1-ZIjA
    
    Bug: 355042392
    Change-Id: I175f3ef7186753f4571edbababae6b8d5665a5bc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981673
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376503}

--- ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.h
+++ ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.h
@@ -16,6 +16,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Presents the passkey enrollment welcome screen.
 - (void)showEnrollmentWelcomeScreen:(ProceduralBlock)enrollBlock;
 
+// Presents the passkey "fix degraded recoverability state" welcome screen.
+- (void)showFixDegradedRecoverabilityWelcomeScreen:
+    (ProceduralBlock)fixDegradedRecoverabilityBlock;
+
 // Presents the passkey reauthentication weclome screen.
 - (void)showReauthenticationWelcomeScreen:(ProceduralBlock)reauthenticateBlock;
 
--- ios/chrome/credential_provider_extension/ui/passkey_welcome_screen_view_controller.h
+++ ios/chrome/credential_provider_extension/ui/passkey_welcome_screen_view_controller.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Possible purposes for showing the passkey welcome screen.
 enum class PasskeyWelcomeScreenPurpose {
   kEnroll,
+  kFixDegradedRecoverability,
   kReauthenticate,
 };
 
