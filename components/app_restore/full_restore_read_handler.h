9c0e1020fd759a124152dcc69b7b4065398071f7
Sammie Quon <sammiequon@chromium.org>
2024-10-31 19:48:38 +0000

    Reland "Reland "coral: Use the app restore data to launch apps""
    This is a reland of commit 086c66e776e7d7fbef4c40b15ff0d5e8e62fc36e
    
    Disabled the flaky test since this CL is needed for coral dogfood.
    Added a TODO to follow up soon.
    
    Original change's description:
    > Reland "coral: Use the app restore data to launch apps"
    >
    > This is a reland of commit 3448a68ec0a44945bc004aa2a212c64e43035376
    >
    > It seems sometimes files SWA doesn't get launched, and test was flaky
    > before the original patch. Just kept settings SWA in the test.
    >
    > Original change's description:
    > > coral: Use the app restore data to launch apps
    > >
    > > This allows apps to launch in their old window states, bounds, etc.
    > > Apps can have multiple windows, so currently this just takes the first
    > > window, as multi instance apps are quite rare. The window id should
    > > be passed to the coral backend in a follow up and be returned with the
    > > app id, as it also an identifier.
    > >
    > > Bug: 365839465
    > > Change-Id: I3c62a386135f0216fad965cf055efd4524802d61
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5941179
    > > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > > Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1374775}
    >
    > Bug: 365839465
    > Change-Id: I8c74d826703aa9f809506a61ed9cd63fd0416d5d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975499
    > Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    > Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1375916}
    
    Bug: 365839465
    Change-Id: I2ce52d67729a0ac95e94e473a7cc181b7e9ab6be
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982004
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376530}

--- components/app_restore/full_restore_read_handler.h
+++ components/app_restore/full_restore_read_handler.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace app_restore {
 struct AppLaunchInfo;
+struct AppRestoreData;
 class RestoreData;
 struct WindowInfo;
 }  // namespace app_restore
@@ -93,6 +94,14 @@ class COMPONENT_EXPORT(APP_RESTORE) FullRestoreReadHandler
                      int32_t task_id,
                      int32_t session_id) override;
   void OnTaskDestroyed(int32_t task_id) override;
+
+  // Returns the first restore app data (first window) associated with `app_id`.
+  // This is used for the coral feature.
+  // TODO(http://crbug.com/365839465): The window id should also be passed
+  // through the pipeline.
+  app_restore::AppRestoreData* GetFirstAppRestoreDataForActiveProfile(
+      const std::string& app_id,
+      int32_t& out_window_id);
 
   void SetPrimaryProfilePath(const base::FilePath& profile_path);
 
@@ -170,6 +179,14 @@ class COMPONENT_EXPORT(APP_RESTORE) FullRestoreReadHandler
   bool IsFullRestoreRunning() const;
 
   void AddChromeBrowserLaunchInfoForTesting(const base::FilePath& profile_path);
+
+  void SetRestoreDataForTesting(
+      const base::FilePath& path,
+      std::unique_ptr<app_restore::RestoreData> restore_data);
+
+  // This will CHECK-fail if there is no restore data associated with `path`.
+  std::unique_ptr<app_restore::RestoreData> GetRestoreDataForTesting(
+      const base::FilePath& path);
 
  private:
   friend class ash::AppLaunchInfoSaveWaiter;
