8b45059af9a8108dac0b252d274e475163f18fb1
Hiroki Nakagawa <nhiroki@chromium.org>
2024-11-01 15:22:14 +0000

    Add AwGWSPageLoadMetricsObserver for WebView
    This CL adds AwGWSPageLoadMetricsObserver that is a
    GWSPageLoadMetricsObserver for WebView. As mentioned in the previous CL
    (https://crrev.com/c/5973517), this corresponds to
    ChromeGWSPageLoadMetricsObserver, but doesn't record metrics with
    kSuffixIsBrowserStarting and kSuffixFromNewTabPage. See the CL
    description of the previous CL for details.
    
    Bug: 376094913
    Change-Id: I068e10652e474508d20f62614e762554b93fdd81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977063
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376877}

--- android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
+++ android_webview/browser/page_load_metrics/page_load_metrics_initialize.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "android_webview/browser/page_load_metrics/page_load_metrics_initialize.h"
 
+#include "android_webview/browser/page_load_metrics/aw_gws_page_load_metrics_observer.h"
 #include "android_webview/browser/page_load_metrics/aw_page_load_metrics_memory_tracker_factory.h"
 #include "components/page_load_metrics/browser/metrics_web_contents_observer.h"
 #include "components/page_load_metrics/browser/observers/abandoned_page_load_metrics_observer.h"
@@ -64,6 +65,7 @@ void PageLoadMetricsEmbedder::RegisterObservers(
   tracker->AddObserver(std::make_unique<ThirdPartyMetricsObserver>());
   tracker->AddObserver(std::make_unique<AbandonedPageLoadMetricsObserver>());
   tracker->AddObserver(std::make_unique<GWSAbandonedPageLoadMetricsObserver>());
+  tracker->AddObserver(std::make_unique<AwGWSPageLoadMetricsObserver>());
 }
 
 bool PageLoadMetricsEmbedder::IsNewTabPageUrl(const GURL& url) {
