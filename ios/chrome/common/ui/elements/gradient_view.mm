10a353707fdd0571e62e8aa91782c1f0250f41c9
Daniel White <danieltwhite@google.com>
2024-11-01 06:10:12 +0000

    [iOS] Deprecate didTraitCollectionChange in GradientView
    Since didTraitCollectionChange is deprecated in iOS 17, GradientView was
    updated to use the new alternative system, registerForTraitChanges.
    
    Fixed: 349389660
    Change-Id: I4b0576592fd726195c05684c7d3d15c8931f7af0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5905743
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376760}

--- ios/chrome/common/ui/elements/gradient_view.mm
+++ ios/chrome/common/ui/elements/gradient_view.mm
@@ -37,6 +37,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     self.gradientLayer.endPoint = endPoint;
     self.userInteractionEnabled = NO;
     [self updateColors];
+
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits = @[
+        UITraitUserInterfaceIdiom.class, UITraitUserInterfaceStyle.class,
+        UITraitDisplayGamut.class, UITraitAccessibilityContrast.class,
+        UITraitUserInterfaceLevel.class
+      ];
+      __weak __typeof(self) weakSelf = self;
+      UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
+                                       UITraitCollection* previousCollection) {
+        [weakSelf updateColorsOnTraitChange:previousCollection];
+      };
+      [self registerForTraitChanges:traits withHandler:handler];
+    }
   }
   return self;
 }
@@ -53,20 +67,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   return base::apple::ObjCCastStrict<CAGradientLayer>(self.layer);
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
-  if ([self.traitCollection
-          hasDifferentColorAppearanceComparedToTraitCollection:
-              previousTraitCollection]) {
-    [CATransaction begin];
-    // If this isn't set, the changes here are automatically animated. The other
-    // color changes for dark mode don't animate, however, so there ends up
-    // being visual desyncing.
-    [CATransaction setDisableActions:YES];
-    [self updateColors];
-    [CATransaction commit];
+  if (@available(iOS 17, *)) {
+    return;
   }
+
+  [self updateColorsOnTraitChange:previousTraitCollection];
 }
+#endif
 
 - (void)setStartColor:(UIColor*)startColor endColor:(UIColor*)endColor {
   self.startColor = startColor;
@@ -81,6 +91,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     (id)self.startColor.CGColor,
     (id)self.endColor.CGColor,
   ];
+}
+
+// Animate and update the view's color when its appearance has been modified via
+// changes in UITraits.
+- (void)updateColorsOnTraitChange:(UITraitCollection*)previousTraitCollection {
+  if ([self.traitCollection
+          hasDifferentColorAppearanceComparedToTraitCollection:
+              previousTraitCollection]) {
+    [CATransaction begin];
+    // If this isn't set, the changes here are automatically animated. The other
+    // color changes for dark mode don't animate, however, so there ends up
+    // being visual desyncing.
+    [CATransaction setDisableActions:YES];
+    [self updateColors];
+    [CATransaction commit];
+  }
 }
 
 @end
