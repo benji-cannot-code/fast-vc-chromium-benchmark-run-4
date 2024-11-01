a3129f56c715cf6739651ec742ef2d22e1a1126e
Orko Garai <orko@igalia.com>
2024-11-01 15:13:13 +0000

    Add wayland-linux-drm-syncobj chrome flag
    Add new chrome flag to be able to enable explicit sync in wayland.
    
    Bug: 367623923
    Change-Id: I7ff70f80c3c78e902e86930c60a74d41a5436174
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980164
    Commit-Queue: Orko Garai <orko@igalia.com>
    Reviewed-by: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Nick Yamane <nickdiego@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1376871}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4575,6 +4575,9 @@ extern const char kPulseaudioLoopbackForScreenShareDescription[];
 extern const char kSimplifiedTabDragUIName[];
 extern const char kSimplifiedTabDragUIDescription[];
 
+extern const char kWaylandLinuxDrmSyncobjName[];
+extern const char kWaylandLinuxDrmSyncobjDescription[];
+
 extern const char kWaylandPerWindowScalingName[];
 extern const char kWaylandPerWindowScalingDescription[];
 
