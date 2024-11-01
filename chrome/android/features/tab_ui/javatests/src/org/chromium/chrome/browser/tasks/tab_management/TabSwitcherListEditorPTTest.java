5b17676ec1a32fe697f5486efb751c42ac59510b
Fiaz Muhammad <mfiaz@google.com>
2024-11-01 22:09:27 +0000

    [Tab Groups] Disable bugged test in TabSwitcherListEditorPTTest
    - Just used a @DisabledTest annotation
    
    Bug: 376897518
    Change-Id: Ie3de2cc3c5f19126a1a3bd5736b06a11e5ef6ce4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985211
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377128}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherListEditorPTTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherListEditorPTTest.java
@@ -60,6 +60,7 @@ public class TabSwitcherListEditorPTTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "crbug.com/376897518")
     public void testCreateTabGroupOf1() {
         WebPageStation firstPage = mInitialStateRule.startOnBlankPage();
         int firstTabId = firstPage.getLoadedTab().getId();
