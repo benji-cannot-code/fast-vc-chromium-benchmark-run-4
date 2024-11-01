8307fad8abe6fe6b68047818b327a13260e1b540
Jean-Philippe Gravel <jpgravel@chromium.org>
2024-11-01 21:27:54 +0000

    Fix potential nullptr dereference in OffscreenCanvas::EnableAcceleration
    Here, there is a theoretical code path where the provider pointer
    could be nullptr. We better check the pointer to be on the safe side.
    This code is relatively new and only used in the unreleased
    transferToGPUTexture API.
    
    Change-Id: I64b68d161690069c1c8b56eb7f5146cd1d345ff9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979548
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377093}

--- third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
+++ third_party/blink/renderer/core/offscreencanvas/offscreen_canvas.cc
@@ -479,6 +479,9 @@ bool OffscreenCanvas::EnableAcceleration() {
   // will be an accelerated canvas once it has been created.
   CanvasResourceProvider* provider =
       GetOrCreateCanvasResourceProvider(RasterModeHint::kPreferGPU);
+  if (!provider) {
+    return false;
+  }
   return provider->IsAccelerated();
 }
 
