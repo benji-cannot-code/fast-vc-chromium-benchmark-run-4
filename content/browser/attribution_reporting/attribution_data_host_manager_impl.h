de5da270b0ed42b9739a75749d07439e6034fbe1
Andrew Paseltiner <apaseltiner@chromium.org>
2024-11-01 19:17:44 +0000

    Clean up AttributionDataHostManagerImpl
    - Remove unnecessary namespace prefixes
    - Move internal-only method from .h to .cc
    - Simplify issue handling
    
    This CL contains no intentional user-visible behavioral changes.
    
    Change-Id: If032e6a650aaed3ab77865c662c1684aac2d4b4f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979380
    Reviewed-by: Nan Lin <linnan@chromium.org>
    Commit-Queue: Andrew Paseltiner <apaseltiner@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377027}

--- content/browser/attribution_reporting/attribution_data_host_manager_impl.h
+++ content/browser/attribution_reporting/attribution_data_host_manager_impl.h
@@ -195,11 +195,6 @@ class CONTENT_EXPORT AttributionDataHostManagerImpl final
       const RegistrationContext*,
       attribution_reporting::mojom::RegistrationType);
 
-  [[nodiscard]] static bool CheckRegistrarSupport(
-      attribution_reporting::Registrar,
-      attribution_reporting::mojom::RegistrationType,
-      const RegistrationContext&);
-
   void OnReceiverDisconnected();
 
   struct HeaderPendingDecode;
