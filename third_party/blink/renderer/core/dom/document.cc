f3e4b3e794b937940fb50cb70d43cedbe84d32f8
Ari Chivukula <arichiv@chromium.org>
2024-11-01 13:35:14 +0000

    [Partitioned Popins] Cleanup Blink Invariants
    We should be using CHECKs here and verify if the page is a popin before
    trying to read popin related data.
    
    Explainer: https://explainers-by-googlers.github.io/partitioned-popins/
    I2P: https://groups.google.com/a/chromium.org/g/blink-dev/c/ApU_zUmpQ2g/
    
    Bug: 340606651
    Change-Id: Ibc5f4362583ab7a6d6dcc72f48ffc1b0849108bd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980155
    Reviewed-by: Sandor «Alex» Major <sandormajor@google.com>
    Commit-Queue: Ari Chivukula <arichiv@chromium.org>
    Auto-Submit: Ari Chivukula <arichiv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376846}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -6427,8 +6427,8 @@ scoped_refptr<const SecurityOrigin> Document::TopFrameOrigin() const {
   // If this window was opened as a new partitioned popin we need to use the
   // origin of the opener's top-frame as our top-frame.
   // See https://explainers-by-googlers.github.io/partitioned-popins/
-  if (GetPage()->GetPartitionedPopinOpenerTopFrameOrigin()) {
-    return GetPage()->GetPartitionedPopinOpenerTopFrameOrigin();
+  if (GetPage()->IsPartitionedPopin()) {
+    return GetPage()->GetPartitionedPopinOpenerProperties().top_frame_origin;
   }
 
   return GetFrame()->Tree().Top().GetSecurityContext()->GetSecurityOrigin();
@@ -6466,8 +6466,9 @@ net::SiteForCookies Document::SiteForCookies() const {
   // If this window was opened as a new partitioned popin we need to use the
   // site for cookies of the opener as our initial candidate.
   // See https://explainers-by-googlers.github.io/partitioned-popins/
-  if (GetPage()->GetPartitionedPopinOpenerSiteForCookies()) {
-    candidate = *GetPage()->GetPartitionedPopinOpenerSiteForCookies();
+  if (GetPage()->IsPartitionedPopin()) {
+    candidate =
+        GetPage()->GetPartitionedPopinOpenerProperties().site_for_cookies;
     // We can only skip comparisons when using the SiteForCookies from the
     // top frame. Because we reset `candidate`, we need to call
     // CompareWithFrameTreeOriginAndRevise() regardless of whether a frame
