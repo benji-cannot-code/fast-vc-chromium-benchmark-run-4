634c8d3d266513ea00ccfd464ce3b45a4428b89a
Chris Thompson <cthomp@chromium.org>
2024-11-01 16:01:42 +0000

    HTTPS-First Mode: Update tests in chrome/browser/ui/webui/commerce
    This CL updates commerce tests that fail when the new feature flag
    kHttpsFirstBalancedModeAutoEnable is enabled in the fieldtrial testing
    config. Due to the large number of affected files, this change is being
    split into multiple CLs -- see https://crrev.com/c/5787951/19 for the
    full set of affected files.
    
    Bug: 349860796
    Change-Id: I66d44937e1c811419c3523b95f55beb9410553b1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979605
    Reviewed-by: Yue Zhang <yuezhanggg@chromium.org>
    Commit-Queue: Chris Thompson <cthomp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376888}

--- chrome/browser/ui/webui/commerce/shopping_ui_handler_delegate_browsertest.cc
+++ chrome/browser/ui/webui/commerce/shopping_ui_handler_delegate_browsertest.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_navigation_observer.h"
 
 namespace {
-const char kExampleUrl[] = "http://example.com/";
+const char kExampleUrl[] = "https://example.com/";
 }  // namespace
 
 // Tests ShoppingUiHandlerDelegate.
