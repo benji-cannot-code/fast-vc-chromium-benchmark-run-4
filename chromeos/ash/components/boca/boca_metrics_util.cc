ebd1fa23dd1dc4f49b17f372c68aefb2a3d988c5
zhumatthew <zhumatthew@google.com>
2024-11-01 23:06:04 +0000

    Boca: Fix var name for metrics to match histogram
    Change-Id: Id2954708073077f2f0e5e0dd6f2bb0992a070817
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984600
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377162}

--- chromeos/ash/components/boca/boca_metrics_util.cc
+++ chromeos/ash/components/boca/boca_metrics_util.cc
@@ -9,8 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash::boca {
 
-void RecordOnTaskLockedStateDuration(base::TimeDelta unlocked_state_duration,
-                                     base::TimeDelta locked_state_duration) {
+void RecordOnTaskLockedStateDurationPercentage(
+    base::TimeDelta unlocked_state_duration,
+    base::TimeDelta locked_state_duration) {
   const base::TimeDelta total_time =
       unlocked_state_duration + locked_state_duration;
   if (total_time.is_zero()) {
@@ -18,8 +19,9 @@ void RecordOnTaskLockedStateDuration(base::TimeDelta unlocked_state_duration,
   }
   const double percentage_locked = 100.0 * (locked_state_duration / total_time);
   const double percentage_unlocked = 100.0 - percentage_locked;
-  UMA_HISTOGRAM_PERCENTAGE(kBocaOnTaskLockedSessionDuration, percentage_locked);
-  UMA_HISTOGRAM_PERCENTAGE(kBocaOnTaskUnlockedSessionDuration,
+  UMA_HISTOGRAM_PERCENTAGE(kBocaOnTaskLockedSessionDurationPercentage,
+                           percentage_locked);
+  UMA_HISTOGRAM_PERCENTAGE(kBocaOnTaskUnlockedSessionDurationPercentage,
                            percentage_unlocked);
 }
 
--- chromeos/ash/components/boca/boca_metrics_util.h
+++ chromeos/ash/components/boca/boca_metrics_util.h
@@ -9,15 +9,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time/time.h"
 
 namespace ash::boca {
-inline constexpr char kBocaOnTaskLockedSessionDuration[] =
-    "Ash.Boca.OnTask.LockedSessionDuration";
-inline constexpr char kBocaOnTaskUnlockedSessionDuration[] =
-    "Ash.Boca.OnTask.UnlockedSessionDuration";
+inline constexpr char kBocaOnTaskLockedSessionDurationPercentage[] =
+    "Ash.Boca.OnTask.LockedSessionDurationPercentage";
+inline constexpr char kBocaOnTaskUnlockedSessionDurationPercentage[] =
+    "Ash.Boca.OnTask.UnlockedSessionDurationPercentage";
 
 // Records the percentage of the duration that a session was in a particular
 // locked or unlocked state.
-void RecordOnTaskLockedStateDuration(base::TimeDelta unlocked_state_duration,
-                                     base::TimeDelta locked_state_duration);
+void RecordOnTaskLockedStateDurationPercentage(
+    base::TimeDelta unlocked_state_duration,
+    base::TimeDelta locked_state_duration);
 
 }  // namespace ash::boca
 
