67009d1f53a1c39715c72de99aeaa3f43302c8ef
Jocelyn Tran <jocelyntran@google.com>
2024-11-01 17:05:47 +0000

    [A11y] Support aria-actions property on ATK/AT-SPI
    `aria-actions` is a new proposed property that identifies secondary
    action elements inside a composite interactive widget. An example of
    this pattern is close button in tabs. This property allows authors to
    surface the actions directly for discoverability. In the example, when
    a screen reader user focuses on a tab, they should know that the close
    button exists.
    
    This CL implements the mapping for this property for ATK/AT-SPI using
    the AtkAction interface.
    
    Future CLs will handle
    - the remaining API mappings
    - IDL related updates
    - restrictions on edge cases
    - has-actions attribute
    
    https://chromestatus.com/feature/5161589307867136
    
    https://github.com/w3c/aria/issues/1440
    https://github.com/w3c/aria/pull/1805
    
    Change-Id: I8702d279fcf0961c84360d0609b0f2a20d9c049c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5925796
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Dominic Farolino <dom@chromium.org>
    Commit-Queue: Jocelyn Tran <jocelyntran@google.com>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Scott Haseley <shaseley@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376934}

--- ui/accessibility/ax_enum_util.cc
+++ ui/accessibility/ax_enum_util.cc
@@ -1863,6 +1863,8 @@ const char* ToString(ax::mojom::IntListAttribute int_list_attribute) {
       return "none";
     case ax::mojom::IntListAttribute::kIndirectChildIds:
       return "indirectChildIds";
+    case ax::mojom::IntListAttribute::kActionsIds:
+      return "actionsIds";
     case ax::mojom::IntListAttribute::kControlsIds:
       return "controlsIds";
     case ax::mojom::IntListAttribute::kDetailsIds:
