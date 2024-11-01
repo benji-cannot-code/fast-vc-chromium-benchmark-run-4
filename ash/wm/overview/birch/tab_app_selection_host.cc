9a18f3995244ebe0502a6693e27c92be5c6c7a43
Sammie Quon <sammiequon@chromium.org>
2024-11-01 21:49:30 +0000

    coral: Icon reload on the coral chip when item removed
    Adds mechanism for selection view to notify the coral chip to reload its
    icon when a tab or app is closed.
    
    Fixed an issue with the initial add on view tooltip being incorrect.
    
    Test: manual
    Change-Id: I9056ea0480e9b9f765210c1c23eab418e4569487
    Fixed: 376545184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985050
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377110}

--- ash/wm/overview/birch/tab_app_selection_host.cc
+++ ash/wm/overview/birch/tab_app_selection_host.cc
@@ -93,7 +93,9 @@ TabAppSelectionHost::TabAppSelectionHost(BirchChipButton* coral_chip)
   Init(std::move(params));
   SetVisibilityAnimationTransition(views::Widget::ANIMATE_NONE);
   SetContentsView(std::make_unique<TabAppSelectionView>(
-      static_cast<BirchCoralItem*>(coral_chip->GetItem())->group_id()));
+      static_cast<BirchCoralItem*>(coral_chip->GetItem())->group_id(),
+      base::BindRepeating(&TabAppSelectionHost::OnItemRemoved,
+                          base::Unretained(this))));
   widget_delegate()->set_desired_bounds_delegate(base::BindRepeating(
       &TabAppSelectionHost::GetDesiredBoundsInScreen, base::Unretained(this)));
   SetBounds(GetDesiredBoundsInScreen());
@@ -119,6 +121,10 @@ void TabAppSelectionHost::ProcessKeyEvent(ui::KeyEvent* event) {
   }
   views::AsViewClass<TabAppSelectionView>(GetContentsView())
       ->ProcessKeyEvent(event);
+}
+
+void TabAppSelectionHost::OnItemRemoved() {
+  owner_->ReloadIcon();
 }
 
 void TabAppSelectionHost::SlideOut() {
