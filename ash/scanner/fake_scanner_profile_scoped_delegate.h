1e3e692c05405e81d85bec78dcba414792819173
mlcui <mlcui@google.com>
2024-11-01 01:33:37 +0000

    scanner: Fix issues with Google APIs server in fake profile delegate
    There were a few issues when this was introduced:
    
    - `request_callback` did not have a trailing underscore as per the style
      guide.
    - There was no setter for the aforementioned request callback.
    - The comment for the HTTP server-related members referred to Google
      Drive, not Google APIs.
    
    Fix all of these issues.
    
    Bug: b:374624760
    Change-Id: I6a6a210071e4e5e00f51673af68f0f462e395e4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981655
    Commit-Queue: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Auto-Submit: Michael Cui <mlcui@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376711}

--- ash/scanner/fake_scanner_profile_scoped_delegate.h
+++ ash/scanner/fake_scanner_profile_scoped_delegate.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SCANNER_FAKE_SCANNER_PROFILE_SCOPED_DELEGATE_H_
 #define ASH_SCANNER_FAKE_SCANNER_PROFILE_SCOPED_DELEGATE_H_
 
+#include <utility>
+
 #include "ash/public/cpp/scanner/scanner_profile_scoped_delegate.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/memory/scoped_refptr.h"
@@ -36,6 +38,14 @@ class FakeScannerProfileScopedDelegate : public ScannerProfileScopedDelegate {
   FakeScannerProfileScopedDelegate& operator=(
       const FakeScannerProfileScopedDelegate&) = delete;
   ~FakeScannerProfileScopedDelegate() override;
+
+  // Sets the callback called when the HTTP server for the Google APIs request
+  // sender is called.
+  void SetRequestCallback(
+      net::test_server::EmbeddedTestServer::HandleRequestCallback
+          request_callback) {
+    request_callback_ = std::move(request_callback);
+  }
 
   // ScannerProfileScopedDelegate:
   ScannerSystemState GetSystemState() const override;
@@ -72,13 +82,13 @@ class FakeScannerProfileScopedDelegate : public ScannerProfileScopedDelegate {
 
   // Initialising these members will *start an HTTP server*, which is not needed
   // for most tests. These members are only initialised upon the first
-  // `GetGoogleDriveService` call. These members should either be all null, or
-  // all non-null.
+  // `GetGoogleApisRequestSender` call. These members should either be all null,
+  // or all non-null.
   std::unique_ptr<google_apis::RequestSender> request_sender_;
   std::unique_ptr<net::test_server::EmbeddedTestServer> test_server_;
   std::unique_ptr<GaiaUrlsOverriderForTesting> gaia_urls_overrider_;
 
-  net::test_server::EmbeddedTestServer::HandleRequestCallback request_callback;
+  net::test_server::EmbeddedTestServer::HandleRequestCallback request_callback_;
 };
 
 }  // namespace ash
