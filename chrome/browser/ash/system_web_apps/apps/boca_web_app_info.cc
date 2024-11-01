3a213c30463a988b186e0a9669831ed14f995c91
April Zhou <aprilzhou@google.com>
2024-11-01 06:54:07 +0000

    boca: Update app name.
    Bug: b:376771910
    Change-Id: I3b124de5ae1a31c1b2515ef396b908732c1f8545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979260
    Commit-Queue: April Zhou <aprilzhou@google.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376770}

--- chrome/browser/ash/system_web_apps/apps/boca_web_app_info.cc
+++ chrome/browser/ash/system_web_apps/apps/boca_web_app_info.cc
@@ -19,6 +19,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/boca/boca_role_util.h"
 #include "chromeos/ash/components/boca/boca_session_manager.h"
 #include "chromeos/constants/chromeos_features.h"
+#include "chromeos/strings/grit/chromeos_strings.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 std::unique_ptr<web_app::WebAppInstallInfo> CreateWebAppInfoForBocaApp() {
@@ -26,8 +28,7 @@ std::unique_ptr<web_app::WebAppInstallInfo> CreateWebAppInfoForBocaApp() {
   auto info =
       web_app::CreateSystemWebAppInstallInfoWithStartUrlAsIdentity(start_url);
   info->scope = GURL(ash::boca::kChromeBocaAppUntrustedURL);
-  // TODO(aprilzhou): Convert the title to a localized string
-  info->title = u"BOCA";
+  info->title = l10n_util::GetStringUTF16(IDS_SCHOOL_TOOLS_TITLE);
   web_app::CreateIconInfoForSystemWebApp(
       info->start_url(),
       {{"app_icon_120.png", 120, IDR_ASH_BOCA_UI_APP_ICON_120_PNG}}, *info);
