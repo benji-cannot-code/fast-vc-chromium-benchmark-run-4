8eae362744425d6123fd002db0988af9d4a572a9
Fredrik Söderquist <fs@opera.com>
2024-11-01 17:19:02 +0000

    spanification: //third_party/blink/renderer/platform/graphics/color.cc
    Bug: 351564777
    Change-Id: I5db54c3a8e7d94fe2a9f4e5913e3d8a931a57998
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979517
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376945}

--- third_party/blink/renderer/platform/graphics/color.h
+++ third_party/blink/renderer/platform/graphics/color.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <optional>
 #include <tuple>
 
+#include "base/containers/span.h"
 #include "base/gtest_prod_util.h"
 #include "third_party/blink/renderer/platform/platform_export.h"
 #include "third_party/blink/renderer/platform/wtf/allocator/allocator.h"
@@ -358,8 +359,8 @@ class PLATFORM_EXPORT Color {
   Color BlendWithWhite() const;
 
   static bool ParseHexColor(const StringView&, Color&);
-  static bool ParseHexColor(const LChar*, unsigned, Color&);
-  static bool ParseHexColor(const UChar*, unsigned, Color&);
+  static bool ParseHexColor(base::span<const LChar>, Color&);
+  static bool ParseHexColor(base::span<const UChar>, Color&);
 
   static const Color kBlack;
   static const Color kWhite;
