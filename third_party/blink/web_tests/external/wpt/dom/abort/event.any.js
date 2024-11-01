5e1dda5df731a74d4eb9f5647cd04e0538fdd362
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-01 16:52:16 +0000

    Import wpt@e047128d24353688dd5b51c6831e7243aae4d81e
    https://github.com/web-platform-tests/wpt/compare/a5e377152...e047128d2
    
    Using wpt-import in Chromium c4f80a33b1bb2fe8b97766e934cac91c65e4c222.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    foolip@chromium.org:
      external/wpt/infrastructure
    kojii@chromium.org:
      external/wpt/css/css-scoping
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I53696bd13dc69679667232fddfe31308670386dc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979023
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1376926}

--- third_party/blink/web_tests/external/wpt/dom/abort/event.any.js
+++ third_party/blink/web_tests/external/wpt/dom/abort/event.any.js
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: global=window,dedicatedworker,shadowrealm
+
 test(t => {
   const c = new AbortController(),
         s = c.signal;
