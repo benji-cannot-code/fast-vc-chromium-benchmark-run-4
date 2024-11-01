2e205eb9114de085daa41a8a8fcf4fd7fb848517
Hung-Hsien Chen <hunghsienchen@chromium.org>
2024-11-01 21:26:42 +0000

    audio_devices_pref_handler: Add voice isolation state
    The state will reflect the ui toggle state which will be added in
    later CLs.
    
    Setting kInputVoiceIsolationEnabled to allowlist in prefs_util.cc so
    that HTML elements can access the pref.
    
    BUG: b:353627012
    TEST: autoninja -C out/Default chromeos_unittests && \
      out/Default/chromeos_unittests \
      --gtest_filter='*AudioDevicesPrefHandlerTest*'
    
    Change-Id: Id9320a285847298749d7e8a6497eef620292bdbe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5912313
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Commit-Queue: Hung-Hsien Chen <hunghsienchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377091}

--- chromeos/ash/components/audio/audio_devices_pref_handler_impl.cc
+++ chromeos/ash/components/audio/audio_devices_pref_handler_impl.cc
@@ -426,6 +426,16 @@ double AudioDevicesPrefHandlerImpl::GetDeviceDefaultOutputVolume(
   }
 }
 
+bool AudioDevicesPrefHandlerImpl::GetVoiceIsolationState() const {
+  return local_state_->GetBoolean(prefs::kInputVoiceIsolationEnabled);
+}
+
+void AudioDevicesPrefHandlerImpl::SetVoiceIsolationState(
+    bool voice_isolation_state) {
+  local_state_->SetBoolean(prefs::kInputVoiceIsolationEnabled,
+                           voice_isolation_state);
+}
+
 bool AudioDevicesPrefHandlerImpl::GetNoiseCancellationState() {
   return local_state_->GetBoolean(prefs::kInputNoiseCancellationEnabled);
 }
@@ -660,6 +670,7 @@ void AudioDevicesPrefHandlerImpl::RegisterPrefs(PrefRegistrySimple* registry) {
   registry->RegisterDictionaryPref(prefs::kAudioDevicesGainPercent);
   registry->RegisterDictionaryPref(prefs::kAudioDevicesMute);
   registry->RegisterDictionaryPref(prefs::kAudioDevicesState);
+  registry->RegisterBooleanPref(prefs::kInputVoiceIsolationEnabled, false);
   registry->RegisterBooleanPref(prefs::kInputNoiseCancellationEnabled, false);
   registry->RegisterBooleanPref(prefs::kInputStyleTransferEnabled, false);
   registry->RegisterBooleanPref(prefs::kHandsFreeProfileInputSuperResolution,
--- chromeos/ash/components/audio/audio_devices_pref_handler_stub.h
+++ chromeos/ash/components/audio/audio_devices_pref_handler_stub.h
@@ -60,6 +60,9 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_AUDIO)
   void AddAudioPrefObserver(AudioPrefObserver* observer) override;
   void RemoveAudioPrefObserver(AudioPrefObserver* observer) override;
 
+  bool GetVoiceIsolationState() const override;
+  void SetVoiceIsolationState(bool voice_isolation_state) override;
+
   bool GetNoiseCancellationState() override;
   void SetNoiseCancellationState(bool noise_cancellation_state) override;
 
@@ -102,6 +105,7 @@ class COMPONENT_EXPORT(CHROMEOS_ASH_COMPONENTS_AUDIO)
   base::ObserverList<AudioPrefObserver>::Unchecked observers_;
 
   bool is_audio_output_allowed_ = true;
+  bool voice_isolation_state_ = false;
   bool noise_cancellation_state_ = true;
   bool style_transfer_state_ = false;
   bool force_respect_ui_gains_ = false;
