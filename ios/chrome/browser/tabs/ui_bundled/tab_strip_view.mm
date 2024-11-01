b14428fb1ebc787152daef49529b6dc78128e7f1
Daniel White <danieltwhite@google.com>
2024-11-01 06:06:29 +0000

    [iOS] Deprecate traitCollectionDidChange in TabStripView
    Since traitCollectionDidChange is deprecated in iOS 17 TabStripView was
    updated to use the new alternative system, registerForTraitChanges.
    
    Fixed: 349391928
    Change-Id: I9eceb2c87817b92155929d5152f13d7d8ed4398e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5906492
    Commit-Queue: Daniel White <danieltwhite@google.com>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1376758}

--- ios/chrome/browser/tabs/ui_bundled/tab_strip_view.mm
+++ ios/chrome/browser/tabs/ui_bundled/tab_strip_view.mm
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/tabs/ui_bundled/tab_strip_view.h"
 
+#import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
+
 @implementation TabStripView
 
 @synthesize layoutDelegate = _layoutDelegate;
@@ -13,6 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if ((self = [super initWithFrame:frame])) {
     self.showsHorizontalScrollIndicator = NO;
     self.showsVerticalScrollIndicator = NO;
+    if (@available(iOS 17, *)) {
+      NSArray<UITrait>* traits =
+          TraitCollectionSetForTraits(@[ UITraitHorizontalSizeClass.class ]);
+      __weak __typeof(self) weakSelf = self;
+      UITraitChangeHandler handler = ^(id<UITraitEnvironment> traitEnvironment,
+                                       UITraitCollection* previousCollection) {
+        [weakSelf notifyDelegateOfTraitChange:previousCollection];
+      };
+      [self registerForTraitChanges:traits withHandler:handler];
+    }
   }
   return self;
 }
@@ -28,8 +40,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self.layoutDelegate layoutTabStripSubviews];
 }
 
+#if !defined(__IPHONE_17_0) || __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_17_0
 - (void)traitCollectionDidChange:(UITraitCollection*)previousTraitCollection {
   [super traitCollectionDidChange:previousTraitCollection];
+  if (@available(iOS 17, *)) {
+    return;
+  }
+
+  [self notifyDelegateOfTraitChange:previousTraitCollection];
+}
+#endif
+
+// Notifies `layoutDelegate` that a change in the collection of UITraits has
+// been observed.
+- (void)notifyDelegateOfTraitChange:
+    (UITraitCollection*)previousTraitCollection {
   [self.layoutDelegate traitCollectionDidChange:previousTraitCollection];
   [self.layoutDelegate layoutTabStripSubviews];
 }
