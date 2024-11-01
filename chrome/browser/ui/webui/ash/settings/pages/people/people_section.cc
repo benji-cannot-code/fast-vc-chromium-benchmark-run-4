8696d5b5d4d4f9b64e86af0e5ae8561a77b0cb7e
Daniel Cheng <dcheng@chromium.org>
2024-11-01 23:10:50 +0000

    [settings] Remove base::NoDestructor use from PeopleSection
    The arrays of SearchConcepts are static data and do not need to be
    stored in a std::vector. One tricky bit is that these arrays were
    initialized based on the result of checking a base::Feature; luckily the
    base::Feature has been launched for some time and the feature check can
    simply be removed.
    
    Bug: 370837212
    Bug: 375364966
    Change-Id: Ic2ca06ebb35a2f0872897f5fd09382b82870ad22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984254
    Reviewed-by: Xiaohui Chen <xiaohuic@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377170}

--- chrome/browser/ui/webui/ash/settings/pages/people/people_section.cc
+++ chrome/browser/ui/webui/ash/settings/pages/people/people_section.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/ash/settings/pages/people/people_section.h"
 
+#include <array>
 #include <vector>
 
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/edusumer/graduation_utils.h"
+#include "base/containers/span.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/i18n/number_formatting.h"
@@ -74,17 +76,10 @@ using ::chromeos::settings::mojom::Subpage;
 
 namespace {
 
-const char* GetAccountsPath() {
-  return ash::features::IsOsSettingsRevampWayfindingEnabled()
-             ? mojom::kPeopleSectionPath
-             : mojom::kMyAccountsSubpagePath;
-}
-
-const std::vector<SearchConcept>& GetPeopleSearchConcepts() {
-  const char* kAccountsPath = GetAccountsPath();
-  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+base::span<const SearchConcept> GetPeopleSearchConcepts() {
+  static constexpr auto tags = std::to_array<SearchConcept>({
       {IDS_OS_SETTINGS_TAG_PEOPLE_ACCOUNTS,
-       kAccountsPath,
+       mojom::kPeopleSectionPath,
        mojom::SearchResultIcon::kAvatar,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSubpage,
@@ -96,31 +91,30 @@ const std::vector<SearchConcept>& GetPeopleSearchConcepts() {
        mojom::SearchResultType::kSection,
        {.section = mojom::Section::kPeople}},
       {IDS_OS_SETTINGS_TAG_PEOPLE_ACCOUNTS_ADD_V2,
-       kAccountsPath,
+       mojom::kPeopleSectionPath,
        mojom::SearchResultIcon::kAvatar,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
        {.setting = mojom::Setting::kAddAccount}},
   });
 
-  return *tags;
+  return tags;
 }
 
-const std::vector<SearchConcept>& GetRemoveAccountSearchConcepts(
-    const char* accounts_path) {
-  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+base::span<const SearchConcept> GetRemoveAccountSearchConcepts() {
+  static constexpr auto tags = std::to_array<SearchConcept>({
       {IDS_OS_SETTINGS_TAG_PEOPLE_ACCOUNTS_REMOVE,
-       accounts_path,
+       mojom::kPeopleSectionPath,
        mojom::SearchResultIcon::kAvatar,
        mojom::SearchResultDefaultRank::kMedium,
        mojom::SearchResultType::kSetting,
        {.setting = mojom::Setting::kRemoveAccount}},
   });
-  return *tags;
+  return tags;
 }
 
-const std::vector<SearchConcept>& GetParentalSearchConcepts() {
-  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+base::span<const SearchConcept> GetParentalSearchConcepts() {
+  static constexpr auto tags = std::to_array<SearchConcept>({
       {IDS_OS_SETTINGS_TAG_PARENTAL_CONTROLS,
        mojom::kPeopleSectionPath,
        mojom::SearchResultIcon::kAvatar,
@@ -130,11 +124,11 @@ const std::vector<SearchConcept>& GetParentalSearchConcepts() {
        {IDS_OS_SETTINGS_TAG_PARENTAL_CONTROLS_ALT1,
         IDS_OS_SETTINGS_TAG_PARENTAL_CONTROLS_ALT2, SearchConcept::kAltTagEnd}},
   });
-  return *tags;
+  return tags;
 }
 
-const std::vector<SearchConcept>& GetGraduationSearchConcepts() {
-  static const base::NoDestructor<std::vector<SearchConcept>> tags({
+base::span<const SearchConcept> GetGraduationSearchConcepts() {
+  static constexpr auto tags = std::to_array<SearchConcept>({
       {IDS_OS_SETTINGS_TAG_GRADUATION,
        mojom::kPeopleSectionPath,
        mojom::SearchResultIcon::kGraduation,
@@ -142,7 +136,7 @@ const std::vector<SearchConcept>& GetGraduationSearchConcepts() {
        mojom::SearchResultType::kSetting,
        {.setting = mojom::Setting::kGraduation}},
   });
-  return *tags;
+  return tags;
 }
 
 void AddAccountManagerPageStrings(content::WebUIDataSource* html_source,
@@ -747,7 +741,7 @@ void PeopleSection::UpdateAccountManagerSearchTags(
 
   // Start with no Account Manager search tags.
   SearchTagRegistry::ScopedTagUpdater updater = registry()->StartUpdate();
-  updater.RemoveSearchTags(GetRemoveAccountSearchConcepts(GetAccountsPath()));
+  updater.RemoveSearchTags(GetRemoveAccountSearchConcepts());
 
   user_manager::User* user = ProfileHelper::Get()->GetUserByProfile(profile());
   DCHECK(user);
@@ -758,7 +752,7 @@ void PeopleSection::UpdateAccountManagerSearchTags(
     }
 
     // If a non-device account exists, add the "Remove Account" search tag.
-    updater.AddSearchTags(GetRemoveAccountSearchConcepts(GetAccountsPath()));
+    updater.AddSearchTags(GetRemoveAccountSearchConcepts());
     return;
   }
 }
