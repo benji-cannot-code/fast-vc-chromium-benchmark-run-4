22b5119c54d1889e0f2effcfff8a489403655598
Mustafa Emre Acer <meacer@chromium.org>
2024-11-01 18:05:46 +0000

    [Lookalikes] Add confusable Cyrillic letter
    Cyrillic letter т can be confused by Latin uppercase t. This CL adds
    it to the whole script confusables set so that hostnames that contain
    т can't be used in spoofs.
    
    Running this patch over a large number of IDN revealed a mix of domains.
    This CL also allowlists some Russian words that don't look like any
    English words.
    
    Bug: 40051657
    Change-Id: I389b9a518b014ee646226f42381497e071e30516
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979230
    Reviewed-by: Joe DeBlasio <jdeblasio@chromium.org>
    Commit-Queue: Joe DeBlasio <jdeblasio@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376985}

--- components/url_formatter/spoof_checks/idn_spoof_checker.cc
+++ components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -156,8 +156,8 @@ IDNSpoofChecker::HuffmanTrieParams g_trie_params{
 // Allow these common words that are whole script confusables. They aren't
 // confusable with any words in Latin scripts.
 const char16_t* kAllowedWholeScriptConfusableWords[] = {
-    u"секс",  u"как",   u"коса",    u"курс",    u"парк",
-    u"такий", u"укроп", u"сахарок", u"покраска"};
+    u"секс",  u"как",     u"коса",     u"курс",  u"парк",  u"такий",
+    u"укроп", u"сахарок", u"покраска", u"театр", u"астро", u"пхукет"};
 
 }  // namespace
 
@@ -232,7 +232,7 @@ IDNSpoofChecker::IDNSpoofChecker() {
        {"am"}},
       {// Cyrillic
        "[[:Cyrl:]]",
-       "[аысԁеԍһіюкјӏорԗԛѕԝхуъьҽпгѵѡ]",
+       "[аысԁеԍһіюкјӏорԗԛѕтԝхуъьҽпгѵѡ]",
        // TLDs containing most of the Cyrillic domains.
        {"bg", "by", "kz", "pyc", "ru", "su", "ua", "uz"}},
       {// Ethiopic (Ge'ez). Variants of these characters such as ሁ and ሡ could
