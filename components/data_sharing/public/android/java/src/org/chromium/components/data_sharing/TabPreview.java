97c77430d95d61182b3b99ec2c26fa247651ace3
Min Qin <qinmin@chromium.org>
2024-11-01 18:28:17 +0000

    Expose preview for shared tab groups in preview API
    This CL simplifies the preview API so  different platforms doesn't
    need to parse the proto message
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 376744402
    Change-Id: I882dc43f9e9add76e13a64c11c94258c121d513f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5977545
    Reviewed-by: Chris Thompson <cthomp@chromium.org>
    Reviewed-by: pengchao Cai <pengchaocai@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376994}

---
+++ components/data_sharing/public/android/java/src/org/chromium/components/data_sharing/TabPreview.java
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.components.data_sharing;
+
+import androidx.annotation.VisibleForTesting;
+
+import org.jni_zero.CalledByNative;
+import org.jni_zero.JNINamespace;
+
+import org.chromium.url.GURL;
+
+/** Preview about a shared tab group. */
+@JNINamespace("data_sharing")
+public class TabPreview {
+    public final GURL url;
+    public final String displayUrl;
+
+    @VisibleForTesting(otherwise = VisibleForTesting.PACKAGE_PRIVATE)
+    public TabPreview(GURL url, String displayUrl) {
+        this.url = url;
+        this.displayUrl = displayUrl;
+    }
+
+    @CalledByNative
+    private static TabPreview createTabPreview(GURL gurl, String displayUrl) {
+        return new TabPreview(gurl, displayUrl);
+    }
+}
