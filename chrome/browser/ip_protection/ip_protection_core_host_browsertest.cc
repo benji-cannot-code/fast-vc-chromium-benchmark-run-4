60588423d374e29a514013e8e3731cbb6dfa62c4
Matt Mueller <mattm@chromium.org>
2024-11-01 19:34:08 +0000

    Cert Management UI V2: flip fieldtrial_testing_config flags on Linux
    Enables EnableCertManagementUIV2 and EnableCertManagementUIV2Write
    flags in fieldtrial_testing_config.json for Linux.
    
    Bug: 40928765
    Change-Id: I14b3fcce7f62b92aaceb7ede7c243bdb02e44af2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983485
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Hubert Chao <hchao@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377033}

--- chrome/browser/ip_protection/ip_protection_core_host_browsertest.cc
+++ chrome/browser/ip_protection/ip_protection_core_host_browsertest.cc
@@ -47,9 +47,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/signin/public/identity_manager/primary_account_change_event.h"
 #endif
 
-#if BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 #include "chrome/common/chrome_features.h"
-#endif  // BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
 using ::ip_protection::BlindSignedAuthToken;
 using ::ip_protection::GeoHint;
@@ -60,7 +60,7 @@ class ScopedIpProtectionFeatureList {
   ScopedIpProtectionFeatureList() {
     feature_list_.InitWithFeatures(
         {
-#if BUILDFLAG(IS_MAC) || BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
             // Use of IpProtectionCoreHostFactory::GetInstance() in the test
             // constructor means that the KeyedService Factory instances get
             // created before feature overrides in
@@ -71,7 +71,7 @@ class ScopedIpProtectionFeatureList {
             //
             // See http://g/chrome-secure-web-and-net/Qre0HqS0hgA for more info.
             ::features::kEnableCertManagementUIV2Write,
-#endif  // BUILDFLAG(CHROME_ROOT_STORE_CERT_MANAGEMENT_UI)
+#endif  // BUILDFLAG(IS_WIN) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_LINUX)
 
             net::features::kEnableIpProtectionProxy,
             network::features::kMaskedDomainList},
