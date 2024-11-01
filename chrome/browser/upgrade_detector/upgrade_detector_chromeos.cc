3e84d8d7fed2e2d9863a93003a74e1ba4be78f4a
Jae Hoon Kim <kimjae@google.com>
2024-11-01 19:22:34 +0000

    Clean up stale base::Feature "AllowRepeatedUpdates"
    .. default value is "on" within platform code.
    We can purge this now.
    
    BUG=b:219067273, 356233710
    TEST=CQ
    
    Change-Id: If9390330faa8ec107c861e28bdc014367c6eaf0d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980589
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Auto-Submit: Jae Hoon Kim <kimjae@chromium.org>
    Commit-Queue: Jae Hoon Kim <kimjae@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377030}

--- chrome/browser/upgrade_detector/upgrade_detector_chromeos.cc
+++ chrome/browser/upgrade_detector/upgrade_detector_chromeos.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <optional>
 
-#include "ash/constants/ash_features.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/no_destructor.h"
@@ -58,7 +57,6 @@ UpgradeDetectorChromeos::UpgradeDetectorChromeos(
     : UpgradeDetector(clock, tick_clock),
       upgrade_notification_timer_(tick_clock),
       initialized_(false),
-      toggled_update_flag_(false),
       update_in_progress_(false) {}
 
 UpgradeDetectorChromeos::~UpgradeDetectorChromeos() {}
@@ -224,14 +222,6 @@ void UpgradeDetectorChromeos::UpdateStatusChanged(
     update_in_progress_ = true;
     if (!upgrade_detected_time().is_null())
       NotifyOnUpgrade();
-  }
-  // TODO(b/219067273): Cleanup toggling from ash into platform code.
-  if (!toggled_update_flag_) {
-    // Only send feature flag status one time.
-    toggled_update_flag_ = true;
-    UpdateEngineClient::Get()->ToggleFeature(
-        update_engine::kFeatureRepeatedUpdates,
-        base::FeatureList::IsEnabled(ash::features::kAllowRepeatedUpdates));
   }
 }
 
