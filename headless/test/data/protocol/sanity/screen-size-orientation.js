1231383eb1af48fbb3eadf3e3115b4eb29023f77
Peter Kvitek <kvitekp@chromium.org>
2024-11-01 17:06:57 +0000

    [headless] Add screen size orientation test
    https://crrev.com/c/5949433 added screen orientation handling to
    headless shell. This CL adds a test to verify that specifying the
    portrait window size using --window-size results in portrait screen
    orientation.
    
    Bug: 368938318
    Change-Id: I6dac36d60c56a52a1aabb723e4d6ad04f2ccb3b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984594
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Peter Kvitek <kvitekp@chromium.org>
    Commit-Queue: Peter Kvitek <kvitekp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376936}

---
+++ headless/test/data/protocol/sanity/screen-size-orientation.js
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(async function(testRunner) {
+  const {session, dp} =
+      await testRunner.startBlank('Tests screen size orientation.');
+
+  const result = await session.evaluate('window.screen.orientation.type');
+  testRunner.log('orientation=' + result);
+
+  testRunner.completeTest();
+})
