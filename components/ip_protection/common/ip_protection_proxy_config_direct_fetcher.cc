03b899ab5d766e0ba2abad9b40c631a5a00ed25d
Xinan Lin <linxinan@chromium.org>
2024-11-01 16:50:29 +0000

    Clear backoff for proxy config fetcher if profile status changes
    If the user signed in, fetcher does not have to wait for backoff to
    retry GetProxyConfig.
    
    BUG=375253808
    TEST=run e2e test locally
    
    Change-Id: Ie19ab0ad2814f0a43b4c698455c48d703304348d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5976530
    Reviewed-by: Dustin Mitchell <djmitche@chromium.org>
    Commit-Queue: Xinan Lin <linxinan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1376924}

--- components/ip_protection/common/ip_protection_proxy_config_direct_fetcher.cc
+++ components/ip_protection/common/ip_protection_proxy_config_direct_fetcher.cc
@@ -114,8 +114,7 @@ void IpProtectionProxyConfigDirectFetcher::OnGetProxyConfigCompleted(
   }
 
   // Cancel any backoff on success.
-  no_get_proxy_config_until_ = base::Time();
-  next_get_proxy_config_backoff_ = kGetProxyConfigFailureTimeout;
+  ClearNoGetProxyConfigUntilTime();
 
   std::vector<net::ProxyChain> proxy_list =
       GetProxyListFromProxyConfigResponse(response.value());
@@ -325,5 +324,10 @@ void IpProtectionProxyConfigDirectFetcher::Retriever::OnGetProxyConfigCompleted(
   }
 
   std::move(callback).Run(std::move(response_proto));
+}
+
+void IpProtectionProxyConfigDirectFetcher::ClearNoGetProxyConfigUntilTime() {
+  no_get_proxy_config_until_ = base::Time();
+  next_get_proxy_config_backoff_ = kGetProxyConfigFailureTimeout;
 }
 }  // namespace ip_protection
