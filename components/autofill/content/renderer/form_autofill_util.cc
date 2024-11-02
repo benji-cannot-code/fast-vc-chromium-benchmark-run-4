215828b1da767a42c9f15c4f438fb9996dadafa8
Christoph Schwering <schwering@google.com>
2024-11-02 01:01:38 +0000

    [Autofill] Fix ownership for associated forms in invalid HTML
    Blink's HTML parser associates form controls with the last unclosed
    form, if there is one. That is, in
      <div> <form> </div> <input>
    the `input` is associated with the `form`.
    
    blink::HTMLFormElement::ListedElements() discovers the associated
    input by extending the scope of its traversal if some form control has
    been associated to the form by the HTML parser.
    
    blink::HTMLFormElement::CollectListedElements() supports flattening
    nested forms (used by Autofill). As in the above example, nested
    forms may have associated fields due to the HTML parser's special
    handling of unclosed form element. To discover these elements, we
    need to extend the scope of the traversal, as in ListedElements().
    
    Before this CL, this scope extension was missing. This CL fixes that.
    
    The fix should have no performance implications on well-formed
    documents for two independent reasons:
    - The changed code is called only on <form>s that have a nested <form>.
    - The scope is expanded only on unclosed <form>.
    
    Bug: 347059988
    Change-Id: I25769e616bf72c52eb1da208cc86d959fed148d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5972637
    Reviewed-by: Mason Freed <masonf@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377204}

--- components/autofill/content/renderer/form_autofill_util.cc
+++ components/autofill/content/renderer/form_autofill_util.cc
@@ -2478,7 +2478,7 @@ FindFormAndFieldForFormControlElement(
   SCOPED_CRASH_KEYS_FOR_FORM(owng, owning_form);
 #undef FORM_CRASH_KEYS
   // clang-format on
-  NOTREACHED(base::NotFatalUntil::M132);
+  NOTREACHED(base::NotFatalUntil::M134);
   return std::nullopt;
 }
 
