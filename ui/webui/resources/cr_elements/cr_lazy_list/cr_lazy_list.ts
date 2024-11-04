c4b632f1ef7b7d64988b6abd2309d6d5784e2109
rbpotter <rbpotter@chromium.org>
2024-11-04 19:17:39 +0000

    cr-lazy-list: Throw an error if list length changes in place
    List length changes in place are not currently supported by
    cr-lazy-list, so throw an error if we detect them to prevent accidental
    misuse.
    
    Bug: 40943652
    Change-Id: I527cf093a694d80cf7e3661966751a033d5b1dc7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980188
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377797}

--- ui/webui/resources/cr_elements/cr_lazy_list/cr_lazy_list.ts
+++ ui/webui/resources/cr_elements/cr_lazy_list/cr_lazy_list.ts
@@ -98,6 +98,7 @@ export class CrLazyListElement<T = object> extends CrLitElement {
   private numItemsDisplayed_: number = 0;
 
   // Internal state
+  private lastItemsLength_: number = 0;
   private lastRenderedHeight_: number = 0;
   private resizeObserver_: ResizeObserver|null = null;
   private scrollListener_: EventListener = () => this.onScroll_();
@@ -106,9 +107,14 @@ export class CrLazyListElement<T = object> extends CrLitElement {
     super.willUpdate(changedProperties);
 
     if (changedProperties.has('items')) {
+      this.lastItemsLength_ = this.items.length;
       this.numItemsDisplayed_ = this.items.length === 0 ?
           0 :
           Math.min(this.numItemsDisplayed_, this.items.length);
+    } else {
+      assert(
+          this.items.length === this.lastItemsLength_,
+          'Items array changed in place; rendered result may be incorrect.');
     }
 
     if (changedProperties.has('itemSize')) {
