c01941eea5121b2748940cc3bed18fff76b7a99c
William Liu <liuwilliam@chromium.org>
2024-11-04 16:44:39 +0000

    [DNT] Record kFinished state
    Without tracking kFinished, we don't know the portion of transitions
    that are aborted.
    
    Cq-Include-Trybots: luci.chromium.try:android-12-x64-rel,android-12l-x64-dbg,android-13-x64-rel,android-14-x64-rel,android-15-x64-rel,android-pie-x86-rel,android-oreo-x86-rel
    
    Fixed: 376091532
    Change-Id: I8ff06e1fce073153c41fc5fcdba5a2040978e299
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984810
    Reviewed-by: Baran Erfani <baranerf@google.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377696}

--- content/browser/navigation_transitions/back_forward_transition_animator.cc
+++ content/browser/navigation_transitions/back_forward_transition_animator.cc
@@ -46,6 +46,9 @@ using IgnoringInputReason = BackForwardTransitionAnimator::IgnoringInputReason;
 using AnimationAbortReason =
     BackForwardTransitionAnimator::AnimationAbortReason;
 
+static constexpr char kAnimationAbortedReason[] =
+    "Navigation.GestureTransition.AnimationAbortReason";
+
 static constexpr base::TimeDelta kDismissScreenshotAfter = base::Seconds(4);
 
 void ResetTransformForLayer(cc::slim::Layer* layer) {
@@ -149,6 +152,9 @@ const char* AnimationAbortReasonToString(AnimationAbortReason abort_reason) {
     case BackForwardTransitionAnimator::AnimationAbortReason::
         kPhysicalSizeChanged:
       return "kPhysicalSizeChanged";
+    case BackForwardTransitionAnimator::AnimationAbortReason::
+        kAnimationFinished:
+      return "kAnimationFinished";
   }
   NOTREACHED();
 }
@@ -338,6 +344,11 @@ BackForwardTransitionAnimator::~BackForwardTransitionAnimator() {
               "BackForwardTransitionAnimator::~BackForwardTransitionAnimator");
 
   CHECK(IsTerminalState()) << StateToString(state_);
+
+  if (state_ == State::kAnimationFinished) {
+    base::UmaHistogramEnumeration(kAnimationAbortedReason,
+                                  AnimationAbortReason::kAnimationFinished);
+  }
 
   switch (ignoring_input_reason_) {
     case IgnoringInputReason::kAnimationInvokedOccurred: {
@@ -1126,8 +1137,7 @@ void BackForwardTransitionAnimator::AbortAnimation(
   TRACE_EVENT("browser,navigation",
               "BackForwardTransitionAnimator::AbortAnimation", "abort_reason",
               AnimationAbortReasonToString(abort_reason));
-  base::UmaHistogramEnumeration(
-      "Navigation.GestureTransition.AnimationAbortReason", abort_reason);
+  base::UmaHistogramEnumeration(kAnimationAbortedReason, abort_reason);
   AdvanceAndProcessState(State::kAnimationAborted);
 }
 
