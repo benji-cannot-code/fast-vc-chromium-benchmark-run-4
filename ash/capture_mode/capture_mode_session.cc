2f7c1e6e4333c00f9391358c64e9e7f61f14887f
Elijah Hewer <hewer@chromium.org>
2024-11-04 21:27:03 +0000

    Sunfish: Add callback for feedback button
    Add a callback to the Sunfish feedback button that opens a feedback
    dialog, set to send responses to a sunfish bucket.
    
    Bug: 375508606
    Test: manual
    Change-Id: I795babcc2b28d56af1ce9a7e2693620d2fd789e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984961
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377895}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
+#include "ash/shell_delegate.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/style/ash_color_id.h"
 #include "ash/style/color_util.h"
@@ -3174,9 +3175,7 @@ void CaptureModeSession::RemoveAllActionButtons() {
 }
 
 void CaptureModeSession::UpdateFeedbackButtonWidget() {
-  // TODO: crbug.com/374831944 - The feedback button should be created if
-  // either Sunfish or Scanner are enabled.
-  if (!features::IsSunfishFeatureEnabled()) {
+  if (!features::CanStartSunfishSession()) {
     return;
   }
 
@@ -3190,8 +3189,10 @@ void CaptureModeSession::UpdateFeedbackButtonWidget() {
     feedback_button_ =
         feedback_button_widget_->SetContentsView(std::make_unique<PillButton>(
             // TODO(hewer): Add callback to open a feedback page.
-            views::Button::PressedCallback(), u"Send Feedback",
-            PillButton::Type::kDefaultWithIconLeading, &kFeedbackIcon));
+            base::BindRepeating(&CaptureModeSession::ShowFeedbackPage,
+                                base::Unretained(this)),
+            u"Send Feedback", PillButton::Type::kDefaultWithIconLeading,
+            &kFeedbackIcon));
     feedback_button_widget_->ShowInactive();
   }
 
@@ -3228,6 +3229,18 @@ bool CaptureModeSession::ShouldHideFeedbackWidget(views::Widget* widget) const {
   auto* controller = CaptureModeController::Get();
   return controller->type() != CaptureModeType::kImage ||
          controller->source() != CaptureModeSource::kRegion;
+}
+
+void CaptureModeSession::ShowFeedbackPage() {
+  Shell::Get()->shell_delegate()->OpenFeedbackDialog(
+      ShellDelegate::FeedbackSource::kSunfish,
+      /*description_template=*/std::string(),
+      /*category_tag=*/"sunfish");
+
+  // The session will still be active when the feedback dialog appears,
+  // preventing the user from interacting with the dialog, so we need to stop
+  // the session. `this` is destroyed here.
+  controller_->Stop();
 }
 
 void CaptureModeSession::InitInternal() {
