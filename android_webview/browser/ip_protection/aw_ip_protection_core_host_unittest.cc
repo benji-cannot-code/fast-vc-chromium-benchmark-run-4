b305a05ff6855ca5374bc6c9ba518596c7707099
Ho Cheung <hocheung@chromium.org>
2024-11-04 20:00:43 +0000

    [Code Health] Remove underscores from test names in //android_webview
    In the GoogleTest framework, underscores are not
    allowed. [1] Chromium has underscores in many test
    names, and those should be changed.
    
    [1]
    https://github.com/google/googletest/blob/main/docs/faq.md#why-should-test-suite-names-and-test-names-not-contain-underscore
    
    Bug: 377144451
    Change-Id: I8ea9999e6e9ebaf8c6419c2b5d3679ba15a001ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981525
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377831}

--- android_webview/browser/ip_protection/aw_ip_protection_core_host_unittest.cc
+++ android_webview/browser/ip_protection/aw_ip_protection_core_host_unittest.cc
@@ -338,8 +338,7 @@ TEST_F(AwIpProtectionCoreHostTest, BlindSignedTokenErrorOther) {
 }
 
 // TryGetAuthTokens() fails because IP Protection is disabled.
-TEST_F(AwIpProtectionCoreHostTest,
-       TryGetAuthTokens_IpProtectionDisabled) {
+TEST_F(AwIpProtectionCoreHostTest, TryGetAuthTokensIpProtectionDisabled) {
   scoped_feature_list_.InitAndDisableFeature(
       net::features::kEnableIpProtectionProxy);
 
@@ -407,7 +406,7 @@ TEST_F(AwIpProtectionCoreHostTest, GetProxyConfigFailure) {
   EXPECT_FALSE(geo_hint.has_value());
 }
 
-TEST_F(AwIpProtectionCoreHostTest, GetProxyConfig_IpProtectionDisabled) {
+TEST_F(AwIpProtectionCoreHostTest, GetProxyConfigIpProtectionDisabled) {
   scoped_feature_list_.InitAndDisableFeature(
       net::features::kEnableIpProtectionProxy);
 
