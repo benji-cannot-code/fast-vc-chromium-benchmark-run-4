2c075e640d85ec0d307467f54df83bb4bee02e19
jhtin <jhtin@google.com>
2024-11-04 21:15:20 +0000

    scanner: Add histogram helper for scanner metrics.
    Bug: b:363101363
    Change-Id: I65eed1e796b4ebeb640b8f1c0370d8a260eba006
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981437
    Auto-Submit: Jon Htin <jhtin@google.com>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Jon Htin <jhtin@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377882}

---
+++ ash/scanner/scanner_metrics.cc
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/scanner/scanner_metrics.h"
+
+#include "base/metrics/histogram_functions.h"
+
+namespace ash {
+
+constexpr char kScannerFeatureUserStateHistogramName[] =
+    "Ash.ScannerFeature.UserState";
+
+void RecordScannerFeatureUserState(ScannerFeatureUserState state) {
+  base::UmaHistogramEnumeration(kScannerFeatureUserStateHistogramName, state);
+}
+
+}  // namespace ash
