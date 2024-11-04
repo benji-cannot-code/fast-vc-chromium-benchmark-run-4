84cd4f63ab1a99b72ba2e840a4da248776a40604
Aaron leventhal <aleventhal@google.com>
2024-11-04 18:55:33 +0000

    [A11y] Debug failed check when loading inline textboxes
    Stack should only come from a <br> or text node, both of which
    should return IsText() == true.
    
    Bug: 377304036
    Change-Id: Ia9c916b5b1a62b56a052d4626012afdb575c5c74
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987436
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Auto-Submit: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Mark Schillaci <mschillaci@google.com>
    Reviewed-by: Mark Schillaci <mschillaci@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377785}

--- third_party/blink/renderer/modules/accessibility/ax_node_object.cc
+++ third_party/blink/renderer/modules/accessibility/ax_node_object.cc
@@ -5416,7 +5416,7 @@ void AXNodeObject::AddInlineTextBoxChildren() {
   CHECK(ShouldLoadInlineTextBoxes());
   CHECK(GetLayoutObject());
   GetLayoutObject()->CheckIsNotDestroyed();
-  CHECK(GetLayoutObject()->IsText());
+  CHECK(GetLayoutObject()->IsText()) << GetLayoutObject() << " " << this;
   CHECK(!GetLayoutObject()->NeedsLayout());
   CHECK(AXObjectCache().GetAXMode().has_mode(ui::AXMode::kInlineTextBoxes));
   CHECK(!AXObjectCache().GetAXMode().HasExperimentalFlags(
