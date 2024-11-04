e9b313a2c1321cd54ca1bc56e9d7f60b6bcfb97c
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-04 16:45:45 +0000

    [Global error service]Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 40249337
    Change-Id: I5ddf9eded233460dc50942d9a9988031a12aef61
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801393
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Elly FJ <ellyjones@chromium.org>
    Commit-Queue: Elly FJ <ellyjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377697}

--- chrome/browser/ui/global_error/global_error_service_factory.cc
+++ chrome/browser/ui/global_error/global_error_service_factory.cc
@@ -35,7 +35,8 @@ GlobalErrorServiceFactory::GlobalErrorServiceFactory()
 
 GlobalErrorServiceFactory::~GlobalErrorServiceFactory() = default;
 
-KeyedService* GlobalErrorServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+GlobalErrorServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* profile) const {
-  return new GlobalErrorService();
+  return std::make_unique<GlobalErrorService>();
 }
