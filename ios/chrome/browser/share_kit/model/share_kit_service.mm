fc835654e0006644aac378aff46fd6e7b2a1430e
Gauthier Ambard <gambard@chromium.org>
2024-11-04 17:18:00 +0000

    [iOS] Cleanup Share Kit
    This is implemented downstream.
    
    Bug: 375609463
    Change-Id: I499e9c9a1c4b408dfce63cccdf2ba90983e38457
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987774
    Reviewed-by: Louis Romero <lpromero@google.com>
    Commit-Queue: Louis Romero <lpromero@google.com>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377715}

--- ios/chrome/browser/share_kit/model/share_kit_service.mm
+++ ios/chrome/browser/share_kit/model/share_kit_service.mm
@@ -8,5 +8,3 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ShareKitService::ShareKitService() = default;
 
 ShareKitService::~ShareKitService() = default;
-
-void ShareKitService::ReadGroups(ShareKitReadConfiguration* config) {}
