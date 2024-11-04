7b406ef8f745cd4e454d6e57cbe738f987f66d32
Joel Hockey <joelhockey@chromium.org>
2024-11-04 22:38:49 +0000

    Functions to get and create content-URIs by display-name
    JS FileSystemAccess APIs have some functions where we look up and
    optionally create child files by providing the parent file handle,
    and the child display-name.
    
    When the file already exists, ContentUriGetChildDocumentOrQuery() will
    query DocumentProviders and return the URI of the matching document.
    
    When the file does not exist, it is not desirable to create the
    document until all access checks have been done. So to handle this, a
    query URI is created which encodes the relevant information. This URI
    will eventually be used to call ContentUriGetDocumentFromQuery() to
    create the document.
    
    New TestDocumentsProvider is created to test this code, and
    InMemoryContentProvider is no longer required to handle tree URIs
    
    Bug: 376097631
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED tests from c++ exercise Java
    Change-Id: I8a569dea218ae0ebe429f4536afd24f1ccb957ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5986890
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377953}

--- base/test/android/content_uri_test_utils.cc
+++ base/test/android/content_uri_test_utils.cc
@@ -47,7 +47,7 @@ std::optional<FilePath> GetInMemoryContentTreeUriFromCacheDirDirectory(
   if (!cache_dir.AppendRelativePath(path, &document_id)) {
     return std::nullopt;
   }
-  base::FilePath uri("content://org.chromium.native_test.inmemory/tree/" +
+  base::FilePath uri("content://org.chromium.native_test.docprov/tree/" +
                      base::EscapeAllExceptUnreserved(document_id.value()));
   return uri;
 }
