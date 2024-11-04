1de376b8451f3e451afe776ffcac7b332be7185b
Shakti Sahu <shaktisahu@chromium.org>
2024-11-04 22:37:38 +0000

    [TG Sync] Some cleanups on the desktop STG listener layer
    This CL adds some minor cleanups:
    1. Replaces the usage of hardcoded base::Token with LocalTabID to make
    it flexible for future refactors.
    2. Renamed SavedTabGroupWebContentsListener's member function
    saved_tab_group_tab_id() with more appropriate local_tab_id().
    3. In LocalTabGroupListener::MaybeRemoveWebContentsFromLocal,
    refactored a block with comments to make it more readable.
    
    Bug: 359982911
    Change-Id: I600aacc44acfb417141139a53b20f3a38c8d4f04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985167
    Commit-Queue: Shakti Sahu <shaktisahu@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rushan Suleymanov <rushans@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377951}

--- chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_web_contents_listener.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/saved_tab_group_web_contents_listener.cc
@@ -80,11 +80,9 @@ void SavedTabGroupWebContentsListener::OnTabDiscarded(
 
 SavedTabGroupWebContentsListener::SavedTabGroupWebContentsListener(
     TabGroupSyncService* service,
-    const LocalTabID& saved_tab_group_tab_id,
+    const LocalTabID& local_tab_id,
     tabs::TabModel* local_tab)
-    : service_(service),
-      saved_tab_group_tab_id_(saved_tab_group_tab_id),
-      local_tab_(local_tab) {
+    : service_(service), local_tab_id_(local_tab_id), local_tab_(local_tab) {
   tab_discard_subscription_ = local_tab->RegisterWillDiscardContents(
       base::BindRepeating(&SavedTabGroupWebContentsListener::OnTabDiscarded,
                           base::Unretained(this)));
@@ -101,7 +99,7 @@ void SavedTabGroupWebContentsListener::NavigateToUrl(const GURL& url) {
   }
 
   std::optional<SavedTabGroup> group = saved_group();
-  SavedTabGroupTab* saved_tab = group->GetTab(saved_tab_group_tab_id_);
+  SavedTabGroupTab* saved_tab = group->GetTab(local_tab_id());
   CHECK(saved_tab);
 
   // If the URL is inside current tab URL's redirect chain, there is no need to
@@ -135,7 +133,7 @@ void SavedTabGroupWebContentsListener::DidFinishNavigation(
   std::optional<SavedTabGroup> group = saved_group();
   if (group) {
     TabGroupSyncUtils::RecordSavedTabGroupNavigationUkmMetrics(
-        saved_tab_group_tab_id_,
+        local_tab_id(),
         group->collaboration_id() ? SavedTabGroupType::SHARED
                                   : SavedTabGroupType::SYNCED,
         navigation_handle, service_);
@@ -158,13 +156,13 @@ void SavedTabGroupWebContentsListener::DidFinishNavigation(
     return;
   }
 
-  SavedTabGroupTab* tab = group->GetTab(saved_tab_group_tab_id_);
+  SavedTabGroupTab* tab = group->GetTab(local_tab_id());
   CHECK(tab);
 
   if (!tab_groups::IsTabGroupSyncServiceDesktopMigrationEnabled()) {
     // TODO(crbug.com/359715038): Implement in TGSS then remove cast.
     static_cast<TabGroupSyncServiceProxy*>(service_)->SetFaviconForTab(
-        group->local_group_id().value(), saved_tab_group_tab_id_,
+        group->local_group_id().value(), local_tab_id(),
         favicon::TabFaviconFromWebContents(contents()));
   }
 
@@ -172,7 +170,7 @@ void SavedTabGroupWebContentsListener::DidFinishNavigation(
   tab_builder.SetURL(contents()->GetURL());
   tab_builder.SetTitle(contents()->GetTitle());
 
-  service_->UpdateTab(group->local_group_id().value(), saved_tab_group_tab_id_,
+  service_->UpdateTab(group->local_group_id().value(), local_tab_id(),
                       std::move(tab_builder));
 }
 
@@ -190,7 +188,7 @@ void SavedTabGroupWebContentsListener::UpdateTabRedirectChain(
   std::optional<SavedTabGroup> group = saved_group();
   SavedTabGroupTabBuilder tab_builder;
   tab_builder.SetRedirectURLChain(navigation_handle->GetRedirectChain());
-  service_->UpdateTab(group->local_group_id().value(), saved_tab_group_tab_id_,
+  service_->UpdateTab(group->local_group_id().value(), local_tab_id(),
                       tab_builder);
 }
 
@@ -198,7 +196,7 @@ const SavedTabGroup SavedTabGroupWebContentsListener::saved_group() {
   std::vector<SavedTabGroup> all_groups = service_->GetAllGroups();
   auto iter = base::ranges::find_if(
       all_groups, [&](const SavedTabGroup& potential_group) {
-        return potential_group.ContainsTab(saved_tab_group_tab_id_);
+        return potential_group.ContainsTab(local_tab_id());
       });
   CHECK(iter != all_groups.end());
 
--- chrome/browser/ui/tabs/saved_tab_groups/tab_group_sync_service_proxy_unittest.cc
+++ chrome/browser/ui/tabs/saved_tab_groups/tab_group_sync_service_proxy_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/saved_tab_groups/public/features.h"
 #include "components/saved_tab_groups/public/tab_group_sync_service.h"
 #include "components/saved_tab_groups/public/types.h"
+#include "components/saved_tab_groups/test_support/saved_tab_group_test_utils.h"
 #include "components/tab_groups/tab_group_color.h"
 #include "components/tab_groups/tab_group_id.h"
 #include "components/tab_groups/tab_group_visual_data.h"
@@ -104,9 +105,9 @@ class TabGroupSyncServiceProxyUnitTest
   const base::Uuid kFirstTabId = base::Uuid::GenerateRandomV4();
   const base::Uuid kSecondTabId = base::Uuid::GenerateRandomV4();
   const base::Uuid kThirdTabId = base::Uuid::GenerateRandomV4();
-  const LocalTabID kFirstTabToken = base::Token::CreateRandom();
-  const LocalTabID kSecondTabToken = base::Token::CreateRandom();
-  const LocalTabID kThirdTabToken = base::Token::CreateRandom();
+  const LocalTabID kFirstTabToken = test::GenerateRandomTabID();
+  const LocalTabID kSecondTabToken = test::GenerateRandomTabID();
+  const LocalTabID kThirdTabToken = test::GenerateRandomTabID();
   const std::u16string kFirstTabTitle = u"first tab";
   const std::u16string kSecondTabTitle = u"second tab";
   const std::u16string kThirdTabTitle = u"third tab";
@@ -522,7 +523,7 @@ TEST_P(TabGroupSyncServiceProxyUnitTest, UpdateLocalTabId) {
   const base::Uuid tab_1_guid =
       retrieved_group->saved_tabs()[0].saved_tab_guid();
 
-  LocalTabID new_local_tab_id = base::Token::CreateRandom();
+  LocalTabID new_local_tab_id = test::GenerateRandomTabID();
   service()->UpdateLocalTabId(local_id, tab_1_guid, new_local_tab_id);
 
   retrieved_group = service()->GetGroup(local_id);
