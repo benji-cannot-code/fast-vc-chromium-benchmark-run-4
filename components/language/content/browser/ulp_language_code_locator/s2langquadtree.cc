37527d98aa9f8c342787f58a87c445b8d19e686b
Tom Sepez <tsepez@chromium.org>
2024-11-04 20:41:37 +0000

    Make S2LangQuadTreeNode a move-only type.
    Then move the child node into the children_ vector to avoid deep
    recursive copies in Deserialize().
    
    Change-Id: I335ed5e0909bf45b276f43f94036bdc0016e9144
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980920
    Reviewed-by: Ian Wells <iwells@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377859}

--- components/language/content/browser/ulp_language_code_locator/s2langquadtree.cc
+++ components/language/content/browser/ulp_language_code_locator/s2langquadtree.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/s2cellid/src/s2/s2cellid.h"
 
 S2LangQuadTreeNode::S2LangQuadTreeNode() = default;
-S2LangQuadTreeNode::S2LangQuadTreeNode(const S2LangQuadTreeNode& other) =
+S2LangQuadTreeNode::S2LangQuadTreeNode(S2LangQuadTreeNode&& other) = default;
+S2LangQuadTreeNode& S2LangQuadTreeNode::operator=(S2LangQuadTreeNode&& other) =
     default;
 S2LangQuadTreeNode::~S2LangQuadTreeNode() = default;
 
@@ -58,7 +59,7 @@ size_t S2LangQuadTreeNode::DeserializeSubtree(
       subtree_size +=
           DeserializeSubtree(serialized_langtree, bits_per_lang_index,
                              bit_offset + subtree_size, &child);
-      root->children_.push_back(child);
+      root->children_.push_back(std::move(child));
     }
     return subtree_size;
   }
