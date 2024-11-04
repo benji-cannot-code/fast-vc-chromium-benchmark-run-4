8fad652ffdab6162003aab25ace2a138bec4baad
Wes Okuhara <wesokuhara@google.com>
2024-11-04 20:11:12 +0000

    Remove consistent-type-imports ESLint rule for CrOS Settings WebUI
    CrOS Settings WebUI used to have a config for the ESLint rule
    consistent-type-imports which deviated from the top-level config. This
    CL removes that deviation to comply with the top-level config.
    
    Bug: b:372733928, b:370730323
    Test: git cl presubmit --files "chrome/browser/resources/ash/settings/*" --force
    Change-Id: Ie0ca020d01fc6993463a61d1761330f92117acd1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983266
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377841}

--- tools/web_dev_style/eslint.config.mjs
+++ tools/web_dev_style/eslint.config.mjs
@@ -445,8 +445,6 @@ export default [
     files: ['chrome/browser/resources/ash/settings/**/*.[jt]s'],
 
     rules: {
-      '@typescript-eslint/consistent-type-imports': 'off',
-
       '@typescript-eslint/explicit-function-return-type': [
         'error', {
           allowExpressions: true,
