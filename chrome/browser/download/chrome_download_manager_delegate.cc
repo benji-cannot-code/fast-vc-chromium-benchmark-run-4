2c820899b906de664096a005476cdc5068a2bd32
Min Qin <qinmin@chromium.org>
2024-11-04 18:28:58 +0000

    Fix an issue that a null callback is passed when hitting back/forward button on PDF
    the callback is moved, so changing the code order to avoid using a null cb
    
    Low-Coverage-Reason: TRIVIAL_CHANGE
    Bug: 377301543
    Change-Id: I296466d6873e7f2edf9746717585273670e0ce5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5988331
    Reviewed-by: Shu Yang <shuyng@google.com>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377757}

--- chrome/browser/download/chrome_download_manager_delegate.cc
+++ chrome/browser/download/chrome_download_manager_delegate.cc
@@ -2022,9 +2022,6 @@ void ChromeDownloadManagerDelegate::CheckDownloadAllowed(
     return;
   }
 
-  CanDownloadCallback cb = base::BindOnce(
-      &ChromeDownloadManagerDelegate::OnCheckDownloadAllowedComplete,
-      weak_ptr_factory_.GetWeakPtr(), std::move(check_download_allowed_cb));
 #if BUILDFLAG(IS_ANDROID)
   if (ShouldOpenPdfInline() && mime_type == pdf::kPDFMimeType) {
     // If this is a forward/back navigation, the native page should trigger a
@@ -2037,6 +2034,13 @@ void ChromeDownloadManagerDelegate::CheckDownloadAllowed(
     }
     NewNavigationObserver::GetInstance()->StartObserving(web_contents);
   }
+#endif
+
+  CanDownloadCallback cb = base::BindOnce(
+      &ChromeDownloadManagerDelegate::OnCheckDownloadAllowedComplete,
+      weak_ptr_factory_.GetWeakPtr(), std::move(check_download_allowed_cb));
+
+#if BUILDFLAG(IS_ANDROID)
   DownloadControllerBase::Get()->AcquireFileAccessPermission(
       web_contents_getter,
       base::BindOnce(&OnDownloadAcquireFileAccessPermissionDone,
