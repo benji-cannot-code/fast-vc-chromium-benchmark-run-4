e88a5d254e12ef684421483e7d343f73289c088b
Ryan Sultanem <rsult@google.com>
2024-11-04 17:10:30 +0000

    [BatchUpload] Remove BatchUploadDataProvider
    Fixed: b:372827385
    Change-Id: I92bb090605a936b7a81168958b1ff19f58bf58f1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974262
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377708}

--- chrome/browser/profiles/batch_upload/batch_upload_controller.h
+++ chrome/browser/profiles/batch_upload/batch_upload_controller.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 
 #include "base/functional/callback.h"
-#include "chrome/browser/profiles/batch_upload/batch_upload_data_provider.h"
 #include "chrome/browser/profiles/batch_upload/batch_upload_delegate.h"
 #include "components/sync/service/local_data_description.h"
 
