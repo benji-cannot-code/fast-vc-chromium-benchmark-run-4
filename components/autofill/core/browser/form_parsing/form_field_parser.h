8172d082c80586500efc25ea3ed199b409c64630
Florian Leimgruber <fleimgruber@google.com>
2024-11-04 14:11:10 +0000

    Add FormFieldParser::MatchInfo
    Local heuristics are implemented as different parsers that match various
    regexes against fields. Based on the matches, each parser adds
    classifications for the fields with a certain score. In the end, the
    type with the highest scoring classification(s) wins.
    
    The current APIs don't return any "metadata" about the matches, such as
    "was the label or name matched?", "how well did the regex match?", etc.
    
    As a result:
    - The scores of classifications are solely on a parser level, rather
      than a regex or match level (e.g. an address classification always
      beats a name classification).
    - Multi-label support is difficult, since local heuristics don't have a
      way to prioritize based on the type of match.
    - It is impossible to gather label-specific prediction quality metrics,
      since the heuristic type can potentially be derived from the name
      attribute.
    
    This chain of CLs attempts to solve this problem by:
    - (This CL) Defining MatchInfo that is returned as part of
      FormFieldParser::Match(). It currently only describes whether name or
      label matched, but can be easily be extended in the future.
    - (crrev.com/c/5980157) Refactors the parsers to store pairs of (field,
      MatchInfo) for fields that the parsers wants to classify (instead of
      just the fields as is the case right now). This way, the MatchInfo is
      available by the time the parser makes a classification.
    - (crrev.com/c/5987711) Utilizes the MatchInfo to emit a metric that
      measures whether heuristic types are derived from the label or name
      attribute. This is just a sample application - more applications
      solving the problems mentioned above will follow.
    
    Bug: 320965828
    Change-Id: I487ef07ada81bcd08faa20ba9f73330d4bfefc29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979815
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Commit-Queue: Florian Leimgruber <fleimgruber@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377605}

--- components/autofill/core/browser/form_parsing/form_field_parser.h
+++ components/autofill/core/browser/form_parsing/form_field_parser.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_AUTOFILL_CORE_BROWSER_FORM_PARSING_FORM_FIELD_PARSER_H_
 
 #include <memory>
+#include <optional>
 #include <string>
 #include <string_view>
 #include <utility>
@@ -123,6 +124,12 @@ struct ParsingContext {
 // name, phone number, or address field.
 class FormFieldParser {
  public:
+  struct MatchInfo {
+    MatchAttribute matched_attribute;
+    // TODO(crbug.com/320965828): Add other details such as the regex that
+    // matched or how well the regex matched to improve match prioritisation.
+  };
+
   FormFieldParser(const FormFieldParser&) = delete;
   FormFieldParser& operator=(const FormFieldParser&) = delete;
 
@@ -165,8 +172,8 @@ class FormFieldParser {
       const std::vector<std::unique_ptr<AutofillField>>& fields,
       FieldCandidatesMap& field_candidates);
 
-  // Returns true if `field` matches one of the the passed `patterns`.
-  static bool FieldMatchesMatchPatternRef(
+  // Returns a MatchInfo if `field` matches one of the the passed `patterns`.
+  static std::optional<MatchInfo> FieldMatchesMatchPatternRef(
       ParsingContext& context,
       base::span<const MatchPatternRef> patterns,
       const AutofillField& field,
@@ -181,7 +188,8 @@ class FormFieldParser {
                               DenseSet<MatchAttribute> match_attributes,
                               const char* regex_name = "") {
     return FormFieldParser::Match(context, field, pattern, match_attributes,
-                                  regex_name);
+                                  regex_name)
+        .has_value();
   }
 
   static bool ParseInAnyOrderForTesting(
@@ -308,11 +316,12 @@ class FormFieldParser {
 
   // Matches the regular expression |pattern| against the components of
   // |field| as specified in |match_type|.
-  static bool Match(ParsingContext& context,
-                    const AutofillField* field,
-                    std::u16string_view pattern,
-                    DenseSet<MatchAttribute> match_attributes,
-                    const char* regex_name = "");
+  static std::optional<MatchInfo> Match(
+      ParsingContext& context,
+      const AutofillField* field,
+      std::u16string_view pattern,
+      DenseSet<MatchAttribute> match_attributes,
+      const char* regex_name = "");
 
   // Perform a "pass" over the |fields| where each pass uses the supplied
   // |parse| method to match content to a given field type.
