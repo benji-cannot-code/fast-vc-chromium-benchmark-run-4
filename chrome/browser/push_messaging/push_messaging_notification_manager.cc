909fc4a30615765ee2373061dc29f02137410a91
Justin Lulejian <jlulejian@chromium.org>
2024-11-04 12:13:59 +0000

    [Extensions] Allow silent push without notification permission.
    Before this change, extensions with service workers could subscribe to
    a push server without the user seeing a default notification when a
    message is received (see crrev.com/c/4705862) if they passed
    `userVisibleOnly: false`.
    
    However, this didn't bypass the notification permission requirement for
    subscribing or receiving messages.
    
    After this change, such extensions can fully use the Push API without
    the notification permission. This allows them to "silently" (without
    requiring a notification) receive push messages without escalating their
    permissions.
    
    The requirement for the notification permission is still enforced if an
    extension attempts to show a notification while using the Push API.This
    is enforced in
    `NotificationPermissionContext::GetPermissionStatusInternal()`.
    
    Fixed: 41492640
    Change-Id: Idf544ddc22ae38885eb6f5d39364d9660d06cc98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980884
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377556}

--- chrome/browser/push_messaging/push_messaging_notification_manager.cc
+++ chrome/browser/push_messaging/push_messaging_notification_manager.cc
@@ -115,8 +115,8 @@ void PushMessagingNotificationManager::EnforceUserVisibleOnlyRequirements(
     bool requested_user_visible_only) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
-  if (ShouldSkipUserVisibleOnlyRequirements(origin,
-                                            requested_user_visible_only)) {
+  if (ShouldBypassUserVisibleOnlyRequirement(origin,
+                                             requested_user_visible_only)) {
     std::move(message_handled_callback)
         .Run(/* did_show_generic_notification= */ false);
     LogSilentPushEvent(SilentPushEvent::kNotificationEnforcementSkipped);
@@ -276,12 +276,12 @@ void PushMessagingNotificationManager::DidWriteNotificationData(
   LogSilentPushEvent(SilentPushEvent::kAllowedWithGenericNotification);
 }
 
-bool PushMessagingNotificationManager::ShouldSkipUserVisibleOnlyRequirements(
+bool PushMessagingNotificationManager::ShouldBypassUserVisibleOnlyRequirement(
     const GURL& origin,
     bool requested_user_visible_only) {
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   if (origin.SchemeIs(extensions::kExtensionScheme)) {
-    return ShouldSkipExtensionUserVisibleOnlyRequirements(
+    return ShouldExtensionsBypassUserVisibleOnlyRequirement(
         origin, requested_user_visible_only);
   }
 #endif  // BUILDFLAG(ENABLE_EXTENSIONS)
@@ -298,7 +298,7 @@ void PushMessagingNotificationManager::LogSilentPushEvent(
 
 #if BUILDFLAG(ENABLE_EXTENSIONS)
 bool PushMessagingNotificationManager::
-    ShouldSkipExtensionUserVisibleOnlyRequirements(
+    ShouldExtensionsBypassUserVisibleOnlyRequirement(
         const GURL& origin,
         bool requested_user_visible_only) {
   // Worker based extensions are exempt from the user visible requirement only
