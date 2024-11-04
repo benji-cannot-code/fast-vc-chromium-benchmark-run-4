5fbbd1adb84a71f22f1872fa61783e5cd5fa8929
Adem Derinel <derinel@google.com>
2024-11-04 15:45:44 +0000

    Enable unsafe buffer warnings in content/browser/webauth
    Also move the Base64UrlEncode method to own file as it's reused in 2
    places. Unfortunately the usage in the unittest file cannot be moved -
    the linker cannot see the definition, and I couldn't figure out why.
    
    Bug: 40285824
    Change-Id: Ie13a85613ac56349d51d6d5780eaa2c7029f6717
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5973916
    Reviewed-by: Adam Langley <agl@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377658}

---
+++ content/browser/webauth/common_utils.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/webauth/common_utils.h"
+
+#include <string>
+
+#include "base/base64url.h"
+#include "base/containers/span.h"
+
+namespace content {
+
+std::string Base64UrlEncodeChallenge(
+    const base::span<const uint8_t> challenge) {
+  std::string ret;
+  base::Base64UrlEncode(challenge, base::Base64UrlEncodePolicy::OMIT_PADDING,
+                        &ret);
+  return ret;
+}
+
+}  // namespace content
---
+++ content/browser/webauth/common_utils.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_WEBAUTH_COMMON_UTILS_H_
+#define CONTENT_BROWSER_WEBAUTH_COMMON_UTILS_H_
+
+#include <string>
+
+#include "base/containers/span.h"
+
+namespace content {
+
+std::string Base64UrlEncodeChallenge(const base::span<const uint8_t> challenge);
+
+}  // namespace content
+
+#endif  // CONTENT_BROWSER_WEBAUTH_COMMON_UTILS_H_
