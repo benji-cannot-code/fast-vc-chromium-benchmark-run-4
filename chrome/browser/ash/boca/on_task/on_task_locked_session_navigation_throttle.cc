fa182bf605eedbfef0fb46aae0e501a872f10d6c
zhumatthew <zhumatthew@google.com>
2024-11-04 21:46:09 +0000

    OnTask: Ctrl+click is opening new tabs
    Fix issue where ctrl+click will open new tabs and always allow
    navigations in those new tabs.
    
    Bug: b:376928459
    Change-Id: I5dd25e1bbff68e41eecbbf0a4ad13a589969fa58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5984016
    Reviewed-by: Vignesh Shenvi <vshenvi@google.com>
    Commit-Queue: Matthew Zhu <zhumatthew@google.com>
    Cr-Commit-Position: refs/heads/main@{#1377913}

--- chrome/browser/ash/boca/on_task/on_task_locked_session_navigation_throttle.cc
+++ chrome/browser/ash/boca/on_task/on_task_locked_session_navigation_throttle.cc
@@ -106,8 +106,10 @@ OnTaskLockedSessionNavigationThrottle::MaybeCreateThrottleFor(
 
   Browser* const content_browser =
       LockedSessionWindowTracker::GetBrowserWithTab(handle->GetWebContents());
-  if (!content_browser || (content_browser != window_tracker->browser() &&
-                           !content_browser->is_type_app_popup())) {
+
+  // Ensure we only apply the nav throttle on OnTask SWA navigations.
+  if (content_browser && (content_browser != window_tracker->browser() &&
+                          !content_browser->is_type_app_popup())) {
     return nullptr;
   }
   window_tracker->ObserveWebContents(handle->GetWebContents());
@@ -161,6 +163,25 @@ bool OnTaskLockedSessionNavigationThrottle::
           !url.SchemeIsHTTPOrHTTPS());
 }
 
+bool OnTaskLockedSessionNavigationThrottle::IsOutsideOnTaskAppNavigation() {
+  // TODO(b/377347487): Add test for Navigations that happen outside the OnTask
+  // SWA but attach the tab to the OnTask SWA subsequently.
+  Browser* const content_browser =
+      LockedSessionWindowTracker::GetBrowserWithTab(
+          navigation_handle()->GetWebContents());
+  LockedSessionWindowTracker* const window_tracker =
+      LockedSessionWindowTrackerFactory::GetForBrowserContext(
+          navigation_handle()->GetWebContents()->GetBrowserContext());
+  // Handle the case where the creation of the tab is in the OnTask app
+  // context, but is moved to a different browser right after (such as open link
+  // in chrome window context menu).
+  if (!content_browser || (content_browser != window_tracker->browser() &&
+                           !content_browser->is_type_app_popup())) {
+    return true;
+  }
+  return false;
+}
+
 content::NavigationThrottle::ThrottleCheckResult
 OnTaskLockedSessionNavigationThrottle::CheckRestrictions() {
   // If there is a client side redirect, let those through.
@@ -176,11 +197,7 @@ OnTaskLockedSessionNavigationThrottle::CheckRestrictions() {
       LockedSessionWindowTracker::GetBrowserWithTab(
           navigation_handle()->GetWebContents());
 
-  // Handle the case where the creation of the tab is in the OnTask app
-  // context, but is moved to a different browser right after (such as open link
-  // in chrome window context menu).
-  if (!content_browser || (content_browser != window_tracker->browser() &&
-                           !content_browser->is_type_app_popup())) {
+  if (IsOutsideOnTaskAppNavigation()) {
     return PROCEED;
   }
 
@@ -336,12 +353,15 @@ OnTaskLockedSessionNavigationThrottle::WillProcessResponse() {
   LockedSessionWindowTracker* const window_tracker =
       LockedSessionWindowTrackerFactory::GetForBrowserContext(
           navigation_handle()->GetWebContents()->GetBrowserContext());
+  if (should_redirects_pass_) {
+    return PROCEED;
+  }
   if (ShouldBlockSensitiveUrlNavigation() &&
       !window_tracker->oauth_in_progress()) {
     MaybeShowBlockedURLToast();
     return CANCEL;
   }
-  return PROCEED;
+  return CheckRestrictions();
 }
 
 content::NavigationThrottle::ThrottleCheckResult
