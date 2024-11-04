fd835a3e43b4a5755e9976b4e182d61fc76bef43
Pengchao Cai <pengchaocai@chromium.org>
2024-11-04 21:11:28 +0000

    [data sharing] Hook up ShareKit with Chrome OpenTabGroup
    Users pressing "Join and Open" from the Join dialog triggers Chrome to
    open the tab group to the tab strip.
    
    Bug: 376348679
    Change-Id: Iba5074ac93317cd823640d1f1f25b48ef8bbc98c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983265
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: pengchao Cai <pengchaocai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377877}

--- chrome/browser/ui/webui/data_sharing/data_sharing_page_handler.cc
+++ chrome/browser/ui/webui/data_sharing/data_sharing_page_handler.cc
@@ -9,6 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/branding_buildflags.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/identity_manager_factory.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/browser_window/public/browser_window_features.h"
+#include "chrome/browser/ui/views/data_sharing/data_sharing_open_group_helper.h"
 #include "chrome/browser/ui/views/data_sharing/data_sharing_utils.h"
 #include "chrome/browser/ui/webui/data_sharing/data_sharing_ui.h"
 #include "components/signin/public/identity_manager/access_token_info.h"
@@ -76,6 +80,14 @@ void DataSharingPageHandler::AssociateTabGroupWithGroupId(
     const std::string& group_id) {
   data_sharing::AssociateTabGroupWithGroupId(tab_group_id, group_id,
                                              GetProfile());
+}
+
+void DataSharingPageHandler::OpenTabGroup(const std::string& group_id) {
+  Browser* const browser = chrome::FindLastActiveWithProfile(GetProfile());
+  CHECK(browser);
+  browser->browser_window_features()
+      ->data_sharing_open_group_helper()
+      ->OpenTabGroupWhenAvailable(group_id);
 }
 
 Profile* DataSharingPageHandler::GetProfile() {
