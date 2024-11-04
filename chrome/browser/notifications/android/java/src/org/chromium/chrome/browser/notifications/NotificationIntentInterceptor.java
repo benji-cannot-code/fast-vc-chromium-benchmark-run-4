45c6cebf9b1b989e73d4ffc45a941b06ad08748d
Min Qin <qinmin@chromium.org>
2024-11-04 22:42:18 +0000

    Add a toast message when clicking on trampoline activity
    This CL adds a toast when user clicks on the trampoline activity,
    and also fixes the status/navigation bar issue.
    Will work with UX on the final string for the toast.
    
    Bug: 365727857
    Change-Id: I48d4089cc4fe8c302d697ab30294f701fa4c8935
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5972862
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1377959}

--- chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptor.java
+++ chrome/browser/notifications/android/java/src/org/chromium/chrome/browser/notifications/NotificationIntentInterceptor.java
@@ -13,6 +13,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import androidx.annotation.IntDef;
 import androidx.annotation.Nullable;
@@ -25,6 +26,7 @@ import org.chromium.base.Log;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.components.browser_ui.notifications.NotificationMetadata;
 import org.chromium.components.browser_ui.notifications.PendingIntentProvider;
+import org.chromium.ui.widget.Toast;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -155,6 +157,18 @@ public class NotificationIntentInterceptor {
             if (!handleNotificationIntent()) {
                 TrampolineActivityTracker.getInstance().finishTrackedActivity();
             }
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                Toast.makeText(
+                                this,
+                                R.string.notification_trampoline_toast_message,
+                                Toast.LENGTH_SHORT)
+                        .show();
+            }
+            return super.onTouchEvent(event);
         }
 
         private static boolean hasVisibleActivities() {
