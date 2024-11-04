9dec3b0272f6558e74a316fb87ceb8cf9fedbfd9
Marc Treib <treib@chromium.org>
2024-11-04 12:35:44 +0000

    [iOS] Feature kSeparateProfilesForManagedAccounts: Check min iOS version
    This feature depends on multi-profile infrastructure, which is only
    available on iOS 17+.
    So this CL adds a helper AreSeparateProfilesForManagedAccountsEnabled()
    to check that the feature flag is enabled and the required iOS version
    is available.
    
    Bug: 331783685
    Change-Id: Ie727475f158e3b5c158bc6d6cc0f6aab6cad04bd
    Low-Coverage-Reason: HARD_TO_TEST The uncovered lines are for iOS <= 16 which apparently isn't covered by bots anymore. They're also entirely trivial, just an early-out into the feature-disabled code path.
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980213
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377562}

--- ios/chrome/browser/signin/model/account_profile_mapper.mm
+++ ios/chrome/browser/signin/model/account_profile_mapper.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/signin/model/account_profile_mapper.h"
 
 #import "base/check_is_test.h"
-#import "base/feature_list.h"
 #import "base/functional/bind.h"
 #import "base/functional/callback.h"
 #import "base/strings/sys_string_conversions.h"
@@ -31,7 +30,7 @@ ProfileNameToGaiaIds GetMappingFromProfileAttributes(
     const ProfileAttributesStorageIOS* profile_attributes_storage) {
   ProfileNameToGaiaIds result;
 
-  if (!base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (!AreSeparateProfilesForManagedAccountsEnabled()) {
     system_identity_manager->IterateOverIdentities(base::BindRepeating(
         [](std::map<std::string, std::set<std::string, std::less<>>,
                     std::less<>>& result,
@@ -259,7 +258,7 @@ void AccountProfileMapper::Assigner::OnIdentityListChanged() {
       std::ref(processed_gaia_ids)));
 
   // Check if any of the previously-assigned Gaia IDs have been removed.
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     for (const auto& [profile_name, gaia_ids] : profile_to_gaia_ids_) {
       for (const std::string& gaia_id : gaia_ids) {
         if (!processed_gaia_ids.contains(gaia_id)) {
@@ -322,7 +321,7 @@ AccountProfileMapper::Assigner::ProcessIdentityForAssignmentToProfile(
     id<SystemIdentity> identity) {
   processed_gaia_ids.insert(base::SysNSStringToUTF8(identity.gaiaID));
 
-  if (!base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (!AreSeparateProfilesForManagedAccountsEnabled()) {
     // With the feature flag disabled, no actual assignment is necessary.
     return SystemIdentityManager::IteratorResult::kContinueIteration;
   }
@@ -349,7 +348,7 @@ void AccountProfileMapper::Assigner::HostedDomainedFetched(
     id<SystemIdentity> identity,
     NSString* hosted_domain,
     NSError* error) {
-  CHECK(base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts));
+  CHECK(AreSeparateProfilesForManagedAccountsEnabled());
 
   if (error) {
     // TODO(crbug.com/331783685): Need to retry.
@@ -368,7 +367,7 @@ void AccountProfileMapper::Assigner::HostedDomainedFetched(
 void AccountProfileMapper::Assigner::AssignIdentityToProfile(
     id<SystemIdentity> identity,
     bool is_managed_account) {
-  CHECK(base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts));
+  CHECK(AreSeparateProfilesForManagedAccountsEnabled());
 
   const std::string gaia_id = base::SysNSStringToUTF8(identity.gaiaID);
 
@@ -549,7 +548,7 @@ AccountProfileMapper::FilterIdentitiesForProfile(
   // by enterprise policy, and remove that filter done by
   // ChromeAccountManagerService.
 
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     ProfileAttributesIOS attr =
         profile_manager_->GetProfileAttributesStorage()
             ->GetAttributesForProfileWithName(profile_name);
@@ -574,7 +573,7 @@ void AccountProfileMapper::MappingUpdated(
   std::set<std::string> profiles_to_notify;
   // Note: If the feature flag is disabled, all profiles are notified, so no
   // need to find the affected profiles.
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     std::set<std::string> all_profiles;
     for (const auto& [name, gaia_ids] : old_mapping) {
       all_profiles.insert(name);
@@ -598,7 +597,7 @@ void AccountProfileMapper::MappingUpdated(
 void AccountProfileMapper::NotifyIdentityListChanged(
     const std::set<std::string>& profile_names_to_notify) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     for (const std::string& profile_name : profile_names_to_notify) {
       auto it = observer_lists_per_profile_name_.find(profile_name);
       if (it == observer_lists_per_profile_name_.end()) {
@@ -622,7 +621,7 @@ void AccountProfileMapper::NotifyIdentityUpdated(
     id<SystemIdentity> identity,
     std::string_view profile_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     if (profile_name.empty()) {
       return;
     }
@@ -648,7 +647,7 @@ void AccountProfileMapper::NotifyAccessTokenRefreshFailed(
     id<RefreshAccessTokenError> error,
     std::string_view profile_name) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  if (base::FeatureList::IsEnabled(kSeparateProfilesForManagedAccounts)) {
+  if (AreSeparateProfilesForManagedAccountsEnabled()) {
     if (profile_name.empty()) {
       return;
     }
