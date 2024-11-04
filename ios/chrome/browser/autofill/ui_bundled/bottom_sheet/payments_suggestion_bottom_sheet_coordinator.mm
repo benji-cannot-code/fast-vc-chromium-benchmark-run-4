f4979179071af257c2d0c1b71f9d61147687b231
vincb <vincb@google.com>
2024-11-04 20:06:30 +0000

    [ios][pbsV2] Do not reattach listeners for payments sheet
    In http://shortn/_ENhcizSeE5 we made the decision that reattaching
    the listeners after using one of the longpress actions wasn't
    worth since they are used rarely.
    
    This will simplify the logic and probably makes it more robust.
    
    Change-Id: I3a13a2e577cae18700f67ee7250f93e4414e49ba
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985572
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377835}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_coordinator.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_coordinator.mm
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_coordinator.h"
 
-#import "base/feature_list.h"
-#import "components/autofill/ios/common/features.h"
 #import "components/autofill/ios/form_util/form_activity_params.h"
 #import "ios/chrome/browser/autofill/model/personal_data_manager_factory.h"
 #import "ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_exit_reason.h"
@@ -139,7 +137,6 @@ using PaymentsSuggestionBottomSheetExitReason::kUsePaymentsSuggestion;
       dismissViewControllerAnimated:NO
                          completion:^{
                            [weakSelf.settingsHandler showCreditCardSettings];
-                           [weakSelf reattachListenersIfNeeded];
                            [weakSelf.browserCoordinatorCommandsHandler
                                    dismissPaymentSuggestions];
                          }];
@@ -158,7 +155,6 @@ using PaymentsSuggestionBottomSheetExitReason::kUsePaymentsSuggestion;
         [](__weak __typeof(self) weak_self, autofill::CreditCard credit_card) {
           [weak_self.settingsHandler showCreditCardDetails:credit_card
                                                 inEditMode:NO];
-          [weak_self reattachListenersIfNeeded];
           [weak_self
                   .browserCoordinatorCommandsHandler dismissPaymentSuggestions];
         },
@@ -212,13 +208,6 @@ using PaymentsSuggestionBottomSheetExitReason::kUsePaymentsSuggestion;
 - (void)setInitialVoiceOverFocus {
   UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification,
                                   self.viewController.image);
-}
-
-// Reattaches the listeners for the latest focused form if deemed needed.
-- (void)reattachListenersIfNeeded {
-  if (base::FeatureList::IsEnabled(kAutofillPaymentsSheetV2Ios)) {
-    [self.mediator reattachListeners];
-  }
 }
 
 @end
