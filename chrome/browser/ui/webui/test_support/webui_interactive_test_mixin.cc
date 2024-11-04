fd784643c214fefe4f8fea026b608bff81040c37
Dana Fried <dfried@chromium.org>
2024-11-04 20:44:14 +0000

    Fix incorrect declaration of identifiers.
    Identifiers cannot be defined in header files.
    But they also cannot be declared in template classes.
    Both will result in "duplicate" identifiers, and runtime crashes.
    
    This CL moves some globally-defined identifiers that were incorrect into
    into a shared base class.
    
    Change-Id: Icc3d96202b2d56afa5f62e025f959b6e5795b96e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987540
    Auto-Submit: Dana Fried <dfried@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Eshwar Stalin <estalin@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377860}

---
+++ chrome/browser/ui/webui/test_support/webui_interactive_test_mixin.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/test_support/webui_interactive_test_mixin.h"
+
+#include "ui/base/interaction/element_identifier.h"
+
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(WebUiInteractiveTestMixinBase,
+                                      kElementRenders);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(WebUiInteractiveTestMixinBase,
+                                      kButtonWasClicked);
+DEFINE_CLASS_ELEMENT_IDENTIFIER_VALUE(WebUiInteractiveTestMixinBase,
+                                      kIronCollapseContentShows);
