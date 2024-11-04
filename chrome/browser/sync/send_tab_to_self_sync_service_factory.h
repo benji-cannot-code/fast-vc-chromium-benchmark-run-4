0613716b64e0ad8f7ab433c5af57125d391b9453
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-04 12:47:20 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 40249337
    Change-Id: Ife49cbc53d33e5708b66b72c7432c6e5b3bade47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4801330
    Reviewed-by: Victor Vianna <victorvianna@google.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377567}

--- chrome/browser/sync/send_tab_to_self_sync_service_factory.h
+++ chrome/browser/sync/send_tab_to_self_sync_service_factory.h
@@ -37,7 +37,7 @@ class SendTabToSelfSyncServiceFactory : public ProfileKeyedServiceFactory {
   ~SendTabToSelfSyncServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
