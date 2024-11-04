549997bf3e718a855e2d962eab34362623c3383a
Tommy Nyquist <nyquist@chromium.org>
2024-11-04 17:20:23 +0000

    [messaging] Expand CollaborationEvent enum to more event types
    The current CollaborationEvent enum does not support all the types
    we expect we need when we will use it for storage.
    
    This expands the enum values based on recent offline discussions.
    
    Bug: 345856704, 370897655
    Change-Id: I0317a0a26d8d89a0e1eccf14bcdeddfb22ef18b8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980263
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Aishwarya Rajesh <aishwaryarj@google.com>
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377718}

--- chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImpl.java
+++ chrome/browser/data_sharing/android/java/src/org/chromium/chrome/browser/data_sharing/InstantMessageDelegateImpl.java
@@ -115,11 +115,11 @@ public class InstantMessageDelegateImpl implements InstantMessageDelegate {
             @CollaborationEvent int collaborationEvent = message.collaborationEvent;
 
             if (message.level == InstantNotificationLevel.SYSTEM) {
-                if (collaborationEvent == CollaborationEvent.COLLABORATION_USER_JOINED) {
+                if (collaborationEvent == CollaborationEvent.COLLABORATION_MEMBER_ADDED) {
                     @NonNull
                     DataSharingNotificationManager dataSharingNotificationManager =
                             attachedWindowInfo.dataSharingNotificationManager;
-                    showCollaborationUserJoinedSystemNotification(
+                    showCollaborationMemberAddedSystemNotification(
                             message, context, dataSharingNotificationManager, tabGroupModelFilter);
                 }
                 success = true;
@@ -130,10 +130,10 @@ public class InstantMessageDelegateImpl implements InstantMessageDelegate {
 
                 if (collaborationEvent == CollaborationEvent.TAB_REMOVED) {
                     showTabRemoved(message, context, messageDispatcher);
-                } else if (collaborationEvent == CollaborationEvent.TAB_NAVIGATED) {
+                } else if (collaborationEvent == CollaborationEvent.TAB_UPDATED) {
                     showTabChange(message, context, messageDispatcher);
-                } else if (collaborationEvent == CollaborationEvent.COLLABORATION_USER_JOINED) {
-                    showCollaborationUserJoined(
+                } else if (collaborationEvent == CollaborationEvent.COLLABORATION_MEMBER_ADDED) {
+                    showCollaborationMemberAdded(
                             message, context, messageDispatcher, tabGroupModelFilter);
                 } else if (collaborationEvent == CollaborationEvent.COLLABORATION_REMOVED) {
                     showCollaborationRemoved(
@@ -214,7 +214,7 @@ public class InstantMessageDelegateImpl implements InstantMessageDelegate {
                 () -> {});
     }
 
-    private void showCollaborationUserJoined(
+    private void showCollaborationMemberAdded(
             InstantMessage message,
             Context context,
             MessageDispatcher messageDispatcher,
@@ -231,7 +231,7 @@ public class InstantMessageDelegateImpl implements InstantMessageDelegate {
         // TODO(https://crbug.com/369163940): Action should open manage sheet.
         showGenericMessage(
                 messageDispatcher,
-                MessageIdentifier.COLLABORATION_USER_JOINED,
+                MessageIdentifier.COLLABORATION_MEMBER_ADDED,
                 title,
                 buttonText,
                 icon,
@@ -258,7 +258,7 @@ public class InstantMessageDelegateImpl implements InstantMessageDelegate {
                 () -> {});
     }
 
-    private void showCollaborationUserJoinedSystemNotification(
+    private void showCollaborationMemberAddedSystemNotification(
             InstantMessage message,
             Context context,
             DataSharingNotificationManager dataSharingNotificationManager,
