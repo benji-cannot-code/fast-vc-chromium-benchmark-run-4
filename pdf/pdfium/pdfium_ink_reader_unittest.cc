9589ba2a7ac18cb21a849a9ea9a534c2a5a76327
Alan Screen <awscreen@chromium.org>
2024-11-04 22:23:38 +0000

    [PDF Ink Signatures] Add helper to count PDF mark objects
    Introduce a test helper to count the number of PDF marked content
    objects that are identified by a particular name.  This will be useful
    to verify logic for applying Ink strokes into PDF documents.
    
    Bug: 335517469
    Change-Id: I7c863ada06c2125dee6a0a546dbe1cb8d619f891
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980804
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1377937}

--- pdf/pdfium/pdfium_ink_reader_unittest.cc
+++ pdf/pdfium/pdfium_ink_reader_unittest.cc
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <vector>
 
+#include "pdf/pdf_ink_constants.h"
 #include "pdf/pdfium/pdfium_engine.h"
 #include "pdf/pdfium/pdfium_page.h"
 #include "pdf/pdfium/pdfium_test_base.h"
+#include "pdf/pdfium/pdfium_test_helpers.h"
 #include "pdf/test/test_client.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/ink/src/ink/geometry/affine_transform.h"
@@ -63,5 +65,45 @@ TEST_P(PDFiumInkReaderTest, NoPage) {
 }
 
 INSTANTIATE_TEST_SUITE_P(All, PDFiumInkReaderTest, testing::Bool());
+
+class PDFiumInkReaderStrokeMarkedObjectsTests : public PDFiumInkReaderTest {
+ public:
+  void ValidateStrokeMarkedObjectsCount(
+      const base::FilePath::CharType* pdf_name,
+      int expected_count) {
+    TestClient client;
+    std::unique_ptr<PDFiumEngine> engine = InitializeEngine(&client, pdf_name);
+    ASSERT_TRUE(engine);
+
+    std::vector<uint8_t> saved_pdf_data = engine->GetSaveData();
+    ASSERT_FALSE(saved_pdf_data.empty());
+
+    TestClient saved_client;
+    std::unique_ptr<PDFiumEngine> saved_engine =
+        InitializeEngineFromData(&saved_client, std::move(saved_pdf_data));
+    ASSERT_TRUE(saved_engine);
+
+    ASSERT_TRUE(saved_engine->doc());
+    EXPECT_EQ(GetPdfMarkObjCountForTesting(saved_engine->doc(),
+                                           kInkAnnotationIdentifierKeyV2),
+              expected_count);
+  }
+};
+
+TEST_P(PDFiumInkReaderStrokeMarkedObjectsTests, MarkedObjectsNoStrokeData) {
+  ValidateStrokeMarkedObjectsCount(FILE_PATH_LITERAL("blank.pdf"),
+                                   /*expected_count=*/0);
+}
+
+TEST_P(PDFiumInkReaderStrokeMarkedObjectsTests, MarkedObjectsHasStrokeData) {
+  ValidateStrokeMarkedObjectsCount(FILE_PATH_LITERAL("ink_v2.pdf"),
+                                   /*expected_count=*/1);
+}
+
+// There are no rendering concerns for counting marked objects, so only one
+// variation need be run.
+INSTANTIATE_TEST_SUITE_P(All,
+                         PDFiumInkReaderStrokeMarkedObjectsTests,
+                         testing::Values(false));
 
 }  // namespace chrome_pdf
---
+++ pdf/pdfium/pdfium_test_helpers.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PDF_PDFIUM_PDFIUM_TEST_HELPERS_H_
+#define PDF_PDFIUM_PDFIUM_TEST_HELPERS_H_
+
+#include <string_view>
+
+#include "third_party/pdfium/public/fpdfview.h"
+
+namespace chrome_pdf {
+
+// Scans PDF and returns the count of content mark objects it contains which
+// match the specified name.  Will return 0 if `document` is null.
+int GetPdfMarkObjCountForTesting(FPDF_DOCUMENT document,
+                                 std::string_view mark_name);
+
+}  // namespace chrome_pdf
+
+#endif  // PDF_PDFIUM_PDFIUM_TEST_HELPERS_H_
