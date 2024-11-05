3891a62f358f502f730730fcb451b649994770af
Nihar Majmudar <niharm@google.com>
2024-11-05 20:13:01 +0000

    [lensoverlay] Add transition to searchbox width.
    Also adds transition to translate button when searchbox becomes
    unfocused.
    
    Screencast: screencast/cast/NDg4MDQzNTg1ODM3NDY1Nnw2NGQ1YTJlMS1lNw
    
    Change-Id: I354b0e5d0a6701f7d5565bb930c30b4c88a500cd
    Bug: 372971592
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987394
    Reviewed-by: Juan Mojica <juanmojica@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378550}

--- chrome/browser/resources/lens/overlay/lens_overlay_app.ts
+++ chrome/browser/resources/lens/overlay/lens_overlay_app.ts
@@ -286,10 +286,12 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
 
   private handleSearchboxFocused() {
     this.isSearchboxFocused = true;
+    this.$.translateButtonContainer.classList.remove('searchbox-unfocused');
   }
 
   private handleSearchboxBlurred() {
     this.isSearchboxFocused = false;
+    this.$.translateButtonContainer.classList.add('searchbox-unfocused');
   }
 
   private handleLanguagePickersOpened() {
