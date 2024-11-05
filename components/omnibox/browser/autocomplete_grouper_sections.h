d63432646d914e01132204a1e5eff188bb456017
Ameur Hosni <ameurhosni@google.com>
2024-11-05 10:34:55 +0000

    [IOS][Omnibox] Increase ZPS limit on Ipad via experimental flags
    This Cl prepares experimental flags to increase the web,srp and ntp zps
    suggestions limit. As described in the phase2 of the ipadOS UI revamp.
    http://shortn/_ddWnFa1zBa
    
    Bug: None
    Change-Id: I8459413e933baf3455529081613558a33c0fb3ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975441
    Commit-Queue: Ameur Hosni <ameurhosni@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378204}

--- components/omnibox/browser/autocomplete_grouper_sections.h
+++ components/omnibox/browser/autocomplete_grouper_sections.h
@@ -283,7 +283,9 @@ class IOSLensMultimodalZpsSection : public ZpsSection {
 //  - up to 10 personalized suggestions.
 class IOSIpadNTPZpsSection : public ZpsSection {
  public:
-  explicit IOSIpadNTPZpsSection(omnibox::GroupConfigMap& group_configs);
+  explicit IOSIpadNTPZpsSection(size_t trends_count,
+                                size_t total_count,
+                                omnibox::GroupConfigMap& group_configs);
 };
 
 // Section expressing the iPad ZPS limits and grouping for the SRP.
@@ -295,7 +297,8 @@ class IOSIpadNTPZpsSection : public ZpsSection {
 //  - up to 10 personalized suggestions.
 class IOSIpadSRPZpsSection : public ZpsSectionWithMVTiles {
  public:
-  explicit IOSIpadSRPZpsSection(omnibox::GroupConfigMap& group_configs);
+  explicit IOSIpadSRPZpsSection(size_t total_count,
+                                omnibox::GroupConfigMap& group_configs);
 };
 
 // Section expressing the iPad ZPS limits and grouping for the Web.
@@ -307,7 +310,8 @@ class IOSIpadSRPZpsSection : public ZpsSectionWithMVTiles {
 //  - up to 10 personalized suggestions.
 class IOSIpadWebZpsSection : public ZpsSectionWithMVTiles {
  public:
-  explicit IOSIpadWebZpsSection(omnibox::GroupConfigMap& group_configs);
+  explicit IOSIpadWebZpsSection(size_t total_count,
+                                omnibox::GroupConfigMap& group_configs);
 };
 
 #endif  // COMPONENTS_OMNIBOX_BROWSER_AUTOCOMPLETE_GROUPER_SECTIONS_H_
