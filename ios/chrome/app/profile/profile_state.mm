ae5af949278af59f7d4641a17b6c507c12f08cec
Aliona DANGLA <alionadangla@google.com>
2024-11-05 17:41:02 +0000

    [iOS] Listen for profile instead of app state in SigninPolicySceneAgent
    Fixed: 364234804
    Change-Id: Ifeba1290cbfbbc1b693c134d2ce6f92e95975b2f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5953785
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378432}

--- ios/chrome/app/profile/profile_state.mm
+++ ios/chrome/app/profile/profile_state.mm
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ProfileState
 
 @interface ProfileState () <SceneStateObserver>
-
 @end
 
 @implementation ProfileState {
@@ -267,6 +266,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (id<UIBlockerTarget>)currentUIBlocker {
+  if (_appState.currentUIBlocker) {
+    return _appState.currentUIBlocker;
+  }
   return _uiBlockerTarget;
 }
 
