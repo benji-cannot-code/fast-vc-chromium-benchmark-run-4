bcbd4c8f68434e99e2de246927ed7eb0afcc2296
ahmyam01 <ahmed.yamout@arm.com>
2024-11-05 17:42:24 +0000

    Fix blink_platform_perftests for android
    Updated BUILD file and mojo init to fix 65 failing tests.
    
    Change-Id: Ia7d8e322f6e4d571e5e7f4a4effbb206156ac504
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5963961
    Reviewed-by: Richard Townsend <richard.townsend@arm.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Richard Townsend <richard.townsend@arm.com>
    Cr-Commit-Position: refs/heads/main@{#1378433}

--- mojo/public/cpp/bindings/tests/wtf_types_unittest.cc
+++ mojo/public/cpp/bindings/tests/wtf_types_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/bind.h"
 #include "base/run_loop.h"
 #include "base/test/task_environment.h"
+#include "mojo/core/embedder/embedder.h"
 #include "mojo/public/cpp/bindings/lib/message_fragment.h"
 #include "mojo/public/cpp/bindings/lib/serialization.h"
 #include "mojo/public/cpp/bindings/lib/wtf_serialization.h"
@@ -61,7 +62,7 @@ class TestWTFImpl : public TestWTF {
 
 class WTFTypesTest : public testing::Test {
  public:
-  WTFTypesTest() {}
+  WTFTypesTest() { mojo::core::Init(); }
 
  private:
   base::test::SingleThreadTaskEnvironment task_environment_;
