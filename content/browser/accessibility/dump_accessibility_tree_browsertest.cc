e954757d0711f4d238db4a558cb93f268ef492bc
Valerie Young <spectranaut@igalia.com>
2024-11-05 00:54:18 +0000

    Reland "[AX Mac] Pretty print AXCustomContent from ax_dump_tree external macOS dumps"
    This reverts commit 038e0a2893512d9a5fb2a5321afd452912565ef0.
    
    Reason for revert: The original patch revealed a bug that existed in macOS 11, specifically, that the AX API attribute AXCustomContent was not yet supported.
    
    This patch turns off the tests which test AXCustomContent for macOS 11.
    
    Original change's description:
    > Revert "[AX Mac] Pretty print AXCustomContent from ax_dump_tree external macOS dumps"
    >
    > This reverts commit 95ea439983ec79832f6f2e12c924f2a54928cd3c.
    >
    > Reason for revert: Suspected of causing test failures:
    > https://ci.chromium.org/ui/p/chromium/builders/ci/mac11-arm64-rel-tests/47888/overview
    >
    > Original change's description:
    > > [AX Mac] Pretty print AXCustomContent from ax_dump_tree external macOS dumps
    > >
    > > Bug: 363275809
    > > Change-Id: Id65789495fa28f2599ccee9ca9602842263d7ada
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5926150
    > > Reviewed-by: Jayson Adams <shrike@chromium.org>
    > > Commit-Queue: Valerie Young <spectranaut@igalia.com>
    > > Reviewed-by: David Tseng <dtseng@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1375503}
    >
    > Bug: 363275809
    > Change-Id: I2f380d3ca8dbf228ab1f9b47dc9a9d045e5adf32
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974676
    > Owners-Override: Takumi Fujimoto <takumif@chromium.org>
    > Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    > Auto-Submit: Takumi Fujimoto <takumif@chromium.org>
    > Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1375578}
    
    Bug: 363275809
    Cq-Include-Trybots: luci.chromium.try:mac11.0-blink-rel,mac11.0.arm64-blink-rel
    Change-Id: I27e711cd5a2a44b35c190f66f137b17cac213e22
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5976533
    Commit-Queue: Valerie Young <spectranaut@igalia.com>
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Reviewed-by: David Tseng <dtseng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378035}

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -45,6 +45,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NOT_ANDROID(x) x
 #endif
 
+namespace {
+
+bool IsNotMacOS11() {
+#if BUILDFLAG(IS_MAC)
+  return base::mac::MacOSMajorVersion() > 11;
+#else
+  return true;
+#endif
+}
+
+}  // namespace
+
 namespace content {
 
 using ui::AXPropertyFilter;
@@ -886,7 +898,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaDefinition) {
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilityAriaDescribedBy) {
-  RunAriaTest(FILE_PATH_LITERAL("aria-describedby.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunAriaTest(FILE_PATH_LITERAL("aria-describedby.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
@@ -906,17 +923,14 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 
 // TODO(crbug.com/40853220): disabled on UIA
-// TODO(crbug.com/372866272): disabled temporarily on macOS
-#if BUILDFLAG(IS_MAC)
-#define MAYBE_AccessibilityAriaDescribedByUpdates \
-  DISABLED_AccessibilityAriaDescribedByUpdates
-#else
-#define MAYBE_AccessibilityAriaDescribedByUpdates \
-  AccessibilityAriaDescribedByUpdates
-#endif  // BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTestExceptUIA,
-                       MAYBE_AccessibilityAriaDescribedByUpdates) {
-  RunAriaTest(FILE_PATH_LITERAL("aria-describedby-updates.html"));
+                       AccessibilityAriaDescribedByUpdates) {
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunAriaTest(FILE_PATH_LITERAL("aria-describedby-updates.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
@@ -1746,7 +1760,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaToolbar) {
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaTooltip) {
-  RunAriaTest(FILE_PATH_LITERAL("aria-tooltip.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunAriaTest(FILE_PATH_LITERAL("aria-tooltip.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityAriaTree) {
@@ -1817,7 +1836,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilityInputTextARIAPlaceholder) {
-  RunAriaTest(FILE_PATH_LITERAL("input-text-aria-placeholder.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunAriaTest(FILE_PATH_LITERAL("input-text-aria-placeholder.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
@@ -1963,7 +1987,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityButtonNameCalc) {
-  RunHtmlTest(FILE_PATH_LITERAL("button-name-calc.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunHtmlTest(FILE_PATH_LITERAL("button-name-calc.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCanvas) {
@@ -1971,12 +2000,22 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCanvas) {
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCanvasFallback) {
-  RunHtmlTest(FILE_PATH_LITERAL("canvas-fallback.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunHtmlTest(FILE_PATH_LITERAL("canvas-fallback.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(YieldingParserDumpAccessibilityTreeTest,
                        AccessibilityCanvasFallback) {
-  RunHtmlTest(FILE_PATH_LITERAL("canvas-fallback.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunHtmlTest(FILE_PATH_LITERAL("canvas-fallback.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCaption) {
@@ -1990,7 +2029,12 @@ IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilityCheckboxNameCalc) {
-  RunHtmlTest(FILE_PATH_LITERAL("checkbox-name-calc.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunHtmlTest(FILE_PATH_LITERAL("checkbox-name-calc.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest, AccessibilityCite) {
@@ -2179,7 +2223,12 @@ IN_PROC_BROWSER_TEST_P(
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
                        AccessibilityContenteditableWithNoDescendants) {
-  RunHtmlTest(FILE_PATH_LITERAL("contenteditable-with-no-descendants.html"));
+  if (IsNotMacOS11()) {
+    // TODO(crbug.com/376720477)
+    // AXCustomContent is not returned until macOS 12, so this test
+    // will fail on macOS 11
+    RunHtmlTest(FILE_PATH_LITERAL("contenteditable-with-no-descendants.html"));
+  }
 }
 
 IN_PROC_BROWSER_TEST_P(DumpAccessibilityTreeTest,
