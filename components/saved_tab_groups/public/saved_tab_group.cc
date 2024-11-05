7141ec692e5f97c1a96b7ae30995f690ff65f734
Min Qin <qinmin@chromium.org>
2024-11-05 04:32:11 +0000

    Add is_pending_sanitization to SavedTabGroupTab
    Will move the title sanitization process into model class. A tab may
    require sanitization in 3 conditions:
    1. URL/title changed
    2. added to a shared tabgroup
    3. when its tabgroup becomes shared
    will follow up on CLs to clean the sanitization flag
    
    Bug: 374221675
    Change-Id: Ibac665a718f03fe8202a4ef0394a702f8431ec72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979298
    Commit-Queue: Min Qin <qinmin@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378112}

--- components/saved_tab_groups/public/saved_tab_group.cc
+++ components/saved_tab_groups/public/saved_tab_group.cc
@@ -419,6 +419,15 @@ SavedTabGroup SavedTabGroup::CloneAsSharedTabGroup(
   return shared_group;
 }
 
+bool SavedTabGroup::IsPendingSanitization() const {
+  for (const auto& tab : saved_tabs()) {
+    if (tab.is_pending_sanitization()) {
+      return true;
+    }
+  }
+  return false;
+}
+
 void SavedTabGroup::RemoveTabImpl(const base::Uuid& saved_tab_guid) {
   std::optional<size_t> index = GetIndexOfTab(saved_tab_guid);
   CHECK(index.has_value());
