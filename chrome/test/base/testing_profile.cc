96c05c4bdd21bb19ad017b53a007227f429c6e8a
Mitsuru Oshima <oshima@chromium.org>
2024-11-05 14:10:59 +0000

    Allow only one guest otr profile on CrOS
    Bug: 374351946
    Change-Id: I764f4e45fa88ac0861905467cc8accda8325fe01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5988291
    Reviewed-by: David Black <dmblack@google.com>
    Reviewed-by: Ryan Sultanem <rsult@google.com>
    Commit-Queue: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378287}

--- chrome/test/base/testing_profile.cc
+++ chrome/test/base/testing_profile.cc
@@ -668,6 +668,12 @@ Profile* TestingProfile::GetOffTheRecordProfile(
     if (!create_if_needed)
       return nullptr;
 
+#if BUILDFLAG(IS_CHROMEOS)
+    if (IsGuestSession()) {
+      CHECK_EQ(otr_profile_id, OTRProfileID::PrimaryID());
+    }
+#endif
+
     TestingProfile::Builder builder;
     if (IsGuestSession() && otr_profile_id == OTRProfileID::PrimaryID())
       builder.SetGuestSession();
@@ -1233,6 +1239,13 @@ TestingProfile* TestingProfile::Builder::BuildOffTheRecord(
   DCHECK(!build_called_);
   DCHECK(original_profile);
   build_called_ = true;
+
+#if BUILDFLAG(IS_CHROMEOS)
+  if (original_profile->IsGuestSession()) {
+    // ChromeOS Guest Session has only one primary OTR.
+    CHECK_EQ(otr_profile_id, OTRProfileID::PrimaryID());
+  }
+#endif
 
   // Note: Owned by |original_profile|.
   return new TestingProfile(
