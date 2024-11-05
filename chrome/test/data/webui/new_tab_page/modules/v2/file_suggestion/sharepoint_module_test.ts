be63f899f9651284462c2debba734c0efc5aada4
Paul Adedeji <pauladedeji@google.com>
2024-11-05 18:35:13 +0000

    [ntp][enterprise] Add ability to disable Sharepoint Card.
    screencast/cast/NTQ5MjUwNzg4OTYzMTIzMnw5OTM1YjgwMS1kZg
    
    Change-Id: I0ac7d8888696ecad2b48cb447929a5591b5dab24
    Bug: 370052288
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5990151
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378467}

--- chrome/test/data/webui/new_tab_page/modules/v2/file_suggestion/sharepoint_module_test.ts
+++ chrome/test/data/webui/new_tab_page/modules/v2/file_suggestion/sharepoint_module_test.ts
@@ -3,14 +3,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type {SharepointModuleElement} from 'chrome://new-tab-page/lazy_load.js';
+import type {DisableModuleEvent, SharepointModuleElement} from 'chrome://new-tab-page/lazy_load.js';
 import {sharepointModuleDescriptor} from 'chrome://new-tab-page/lazy_load.js';
 import {$$} from 'chrome://new-tab-page/new_tab_page.js';
-import {assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
-import {microtasksFinished} from 'chrome://webui-test/test_util.js';
+import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
+import {assertEquals, assertFalse, assertTrue} from 'chrome://webui-test/chai_assert.js';
+import {eventToPromise, microtasksFinished} from 'chrome://webui-test/test_util.js';
 
 suite('SharepointModule', () => {
-  setup(() => {
+  setup(async () => {
     document.body.innerHTML = window.trustedTypes!.emptyHTML;
   });
 
@@ -31,5 +32,28 @@ suite('SharepointModule', () => {
 
     // Assert.
     assertTrue(!!$$(sharepointModule, 'ntp-info-dialog'));
+  });
+
+  test('clicking the disable button fires a disable module event', async () => {
+    // Arrange.
+    const modulesSharepointName = 'SharePoint';
+    loadTimeData.overrideValues({modulesSharepointName: modulesSharepointName});
+    const sharepointModule = await sharepointModuleDescriptor.initialize(0) as
+        SharepointModuleElement;
+    document.body.append(sharepointModule);
+    await microtasksFinished();
+
+    // Act.
+    const whenFired = eventToPromise('disable-module', sharepointModule);
+    const disableButton = sharepointModule.$.moduleHeaderElementV2.shadowRoot!
+                              .querySelector<HTMLElement>('#disable');
+    assertTrue(!!disableButton);
+    disableButton.click();
+
+    // Assert.
+    const event: DisableModuleEvent = await whenFired;
+    assertEquals(
+        ('You won\'t see ' + modulesSharepointName + ' on this page again'),
+        event.detail.message);
   });
 });
