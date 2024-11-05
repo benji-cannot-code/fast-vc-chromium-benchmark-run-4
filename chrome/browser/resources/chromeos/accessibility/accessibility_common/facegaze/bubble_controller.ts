9913b551c521ddbb40bce5fab25e62e1b76d9cf0
Amanda Lin Dietz <aldietz@google.com>
2024-11-05 17:55:20 +0000

    [FaceGaze] Persist strings for held macros in bubble UI
    * Persist strings for macros that are held.
    * Due to complexities with tracking state, if new actions are triggered
    while the macro is held, the strings for the new actions will populate
    the bubble UI immediately, but not include the held macros. When the
    bubble UI resets after a timeout, the held macros will populate the
    bubble UI again at that time.
    * Add tests.
    
    Fixed: 375235384
    Change-Id: I22dcd68229dc1da033668051ac615ff09f378f0f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985158
    Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378443}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/bubble_controller.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/bubble_controller.ts
@@ -45,7 +45,13 @@ export class BubbleController {
       scrollMode,
       longClick,
       dictation,
+      heldActions,
     } = this.getStateGesture_();
+
+    if (heldActions) {
+      heldActions.forEach((displayText) => {this.baseText_.push(displayText)});
+    }
+
     if (paused) {
       this.baseText_.push(chrome.i18n.getMessage(
           'facegaze_state_paused',
@@ -242,6 +248,7 @@ export namespace BubbleController {
     scrollMode: FacialGesture|undefined;
     longClick: FacialGesture|undefined;
     dictation: FacialGesture|undefined;
+    heldActions: string[];
   }
 }
 
