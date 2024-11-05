99a2b54866f0c1dfa90915efc3f15bb02c61aa6f
mlcui <mlcui@google.com>
2024-11-05 16:44:04 +0000

    sunfish: Hide search box button entry point if feature is disabled
    See attached bug for more details. This uses a similar model to the
    Assistant button.
    
    Bug: b:368184169
    Change-Id: I6a6a2100862b0a25a64cf93b5589e543d24a3bcd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982018
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Michael Cui <mlcui@google.com>
    Commit-Queue: Toni Barzic <tbarzic@google.com>
    Reviewed-by: Toni Barzic <tbarzic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378388}

--- ash/app_list/app_list_controller_impl.cc
+++ ash/app_list/app_list_controller_impl.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/assistant/ui/assistant_view_delegate.h"
 #include "ash/assistant/util/assistant_util.h"
 #include "ash/assistant/util/deep_link_util.h"
+#include "ash/capture_mode/capture_mode_controller.h"
 #include "ash/constants/ash_features.h"
 #include "ash/constants/ash_pref_names.h"
 #include "ash/keyboard/ui/keyboard_ui_controller.h"
@@ -1765,8 +1766,10 @@ SearchModel* AppListControllerImpl::GetSearchModel() {
 }
 
 void AppListControllerImpl::UpdateSearchBoxUiVisibilities() {
-  GetSearchModel()->search_box()->SetShowAssistantButton(
-      IsAssistantAllowedAndEnabled());
+  SearchBoxModel* search_box_model = GetSearchModel()->search_box();
+  search_box_model->SetShowAssistantButton(IsAssistantAllowedAndEnabled());
+  search_box_model->SetShowSunfishButton(
+      CaptureModeController::IsSunfishAllowedAndEnabled());
 
   if (!client_) {
     return;
