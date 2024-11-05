bb266bcd779404fe6ad1e917fa962b9cc0ea4dae
Finnur Thorarinsson <finnur@chromium.org>
2024-11-05 11:55:28 +0000

    [NavCapture]: Switch to WebAppBrowserTestBase::InstallWebApp.
    (Follow-up from previous review)
    
    Bug: 374807552
    Change-Id: I7e35f70132501084f96dd950a4f8547ef05d1656
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987730
    Commit-Queue: Finnur Thorarinsson <finnur@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378242}

--- chrome/browser/web_applications/web_app_navigation_capturing_intent_picker_browsertest.cc
+++ chrome/browser/web_applications/web_app_navigation_capturing_intent_picker_browsertest.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/views/web_apps/web_app_link_capturing_test_utils.h"
+#include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
+#include "chrome/browser/web_applications/web_app_install_info.h"
 #include "chrome/browser/web_applications/web_app_navigation_capturing_browsertest_base.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
@@ -44,8 +46,11 @@ class WebAppNavigationCapturingIntentPickerBrowserTest
 #endif  // BUILDFLAG(IS_MAC)
 IN_PROC_BROWSER_TEST_F(WebAppNavigationCapturingIntentPickerBrowserTest,
                        MAYBE_FocusExisting) {
-  webapps::AppId app_id =
-      InstallApp(GetAppUrl(), ManifestLaunchHandler_ClientMode::kFocusExisting);
+  webapps::AppId app_id = test::InstallWebApp(
+      profile(), WebAppInstallInfo::CreateForTesting(
+                     GetAppUrl(), blink::mojom::DisplayMode::kMinimalUi,
+                     mojom::UserDisplayMode::kStandalone,
+                     ManifestLaunchHandler_ClientMode::kFocusExisting));
 
   Browser* app_browser = LaunchWebAppBrowserAndWait(app_id);
   EXPECT_NE(app_browser, browser());
@@ -82,8 +87,10 @@ IN_PROC_BROWSER_TEST_F(WebAppNavigationCapturingIntentPickerBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(WebAppNavigationCapturingIntentPickerBrowserTest,
                        NavigateExisting) {
-  webapps::AppId app_id = InstallApp(
-      GetAppUrl(), ManifestLaunchHandler_ClientMode::kNavigateExisting);
+  webapps::AppId app_id = InstallWebApp(WebAppInstallInfo::CreateForTesting(
+      GetAppUrl(), blink::mojom::DisplayMode::kMinimalUi,
+      mojom::UserDisplayMode::kStandalone,
+      ManifestLaunchHandler_ClientMode::kNavigateExisting));
 
   Browser* app_browser = LaunchWebAppBrowserAndWait(app_id);
   EXPECT_NE(app_browser, browser());
