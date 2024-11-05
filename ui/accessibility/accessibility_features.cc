1af2e723cc343a6b6ce2120bbab67e29f3f7ed8f
Kevin Ellis <kevers@google.com>
2024-11-05 14:02:16 +0000

    [a11y] Enable pruning AXInlineTextBoxText by default
    Last step before 100% launch on Finch.
    
    
    Bug: 336294737
    Change-Id: I14984e1f874027edff8f30a7ad4c19ef3cab3c90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5989130
    Commit-Queue: Kevin Ellis <kevers@chromium.org>
    Reviewed-by: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378283}

--- ui/accessibility/accessibility_features.cc
+++ ui/accessibility/accessibility_features.cc
@@ -47,7 +47,7 @@ bool IsAccessibilityBlockFlowIteratorEnabled() {
 
 BASE_FEATURE(kAccessibilityPruneRedundantInlineText,
              "AccessibilityPruneRedundantInlineText",
-             base::FEATURE_DISABLED_BY_DEFAULT);
+             base::FEATURE_ENABLED_BY_DEFAULT);
 bool IsAccessibilityPruneRedundantInlineTextEnabled() {
   return base::FeatureList::IsEnabled(
       ::features::kAccessibilityPruneRedundantInlineText);
