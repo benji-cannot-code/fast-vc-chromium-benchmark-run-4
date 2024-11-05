4c5ca9edd7a12fd122cb7043ce1d0e7d701c6350
anthie@google.com <anthie@google.com>
2024-11-05 16:20:14 +0000

    Add test support for the SU IPH on FRE flows
    The FRE tests that accept or decline the Sync consent
    now check whether the IPH for supervised user has
    been displayed.
    
    Bug: 374730442
    Change-Id: I0680da4ba841ed1e0b91a58aae8145796b5876ea
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983144
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378362}

--- chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog_browsertest.cc
+++ chrome/browser/ui/profiles/signin_intercept_first_run_experience_dialog_browsertest.cc
@@ -117,7 +117,10 @@ class SigninInterceptFirstRunExperienceDialogBrowserTest : public TestBase {
                      TrackerInitializationMode::kDoNotWait),
                  ClockMode::kUseTestClock,
                  InitialSessionState::kOutsideGracePeriod,
-                 /*use_main_profile=*/true) {}
+                 /*use_main_profile=*/true),
+        scoped_iph_delay_(
+            AvatarToolbarButton::SetScopedIPHMinDelayAfterCreationForTesting(
+                base::Seconds(0))) {}
 
   ~SigninInterceptFirstRunExperienceDialogBrowserTest() override = default;
 
@@ -144,10 +147,6 @@ class SigninInterceptFirstRunExperienceDialogBrowserTest : public TestBase {
   void SetUpOnMainThread() override {
     TestBase::SetUpOnMainThread();
     identity_test_env()->SetAutomaticIssueOfAccessTokens(true);
-
-    // Needed for profile switch IPH testing.
-    AvatarToolbarButton::SetIPHMinDelayAfterCreationForTesting(
-        base::Seconds(0));
   }
 
   // Returns true if the profile switch IPH has been shown.
@@ -251,7 +250,8 @@ class SigninInterceptFirstRunExperienceDialogBrowserTest : public TestBase {
 
  private:
   testing::NiceMock<policy::MockConfigurationPolicyProvider> policy_provider_;
-
+  // Needed for profile switch IPH testing.
+  base::AutoReset<base::TimeDelta> scoped_iph_delay_;
   base::HistogramTester histogram_tester_;
   base::UserActionTester user_action_tester_;
 
