9aad87d2b71d59f10207209909a3cae1d474a14b
Brandon Fong <bjfong@google.com>
2024-11-05 00:23:06 +0000

    [hub] Make the suggestions background opaque with same toolbar color
    Make the suggestions background area have the same color as the toolbar
    since transparency is not yet landed.
    
    Inspiration taken from: https://chromium-review.googlesource.com/c/chromium/src/+/5984533
    
    Bug: 369687073
    Change-Id: I21d96241f038222069f372e75b47be6506cd8919
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979912
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378015}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionListProperties.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/SuggestionListProperties.java
@@ -65,6 +65,13 @@ import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
     /** Whether the dropdown should draw over top of the anchor view. */
     static final WritableBooleanPropertyKey DRAW_OVER_ANCHOR = new WritableBooleanPropertyKey();
 
+    /**
+     * Whether the dropdown container should always be visible, even if there's no suggestions to
+     * show.
+     */
+    static final WritableBooleanPropertyKey CONTAINER_ALWAYS_VISIBLE =
+            new WritableBooleanPropertyKey();
+
     static final PropertyKey[] ALL_KEYS =
             new PropertyKey[] {
                 ALPHA,
@@ -78,6 +85,7 @@ import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
                 DROPDOWN_SCROLL_LISTENER,
                 DROPDOWN_SCROLL_TO_TOP_LISTENER,
                 LIST_IS_FINAL,
-                DRAW_OVER_ANCHOR
+                DRAW_OVER_ANCHOR,
+                CONTAINER_ALWAYS_VISIBLE
             };
 }
