4299947872e31a2247310df68a868d1f32ed85f7
Timofey Chudakov <tchudakov@google.com>
2024-11-05 11:43:31 +0000

    [Android] Set syncable pref for the last plus address filling time.
    This pref will be used to attach additional data to the plus address
    HaTS surveys.
    
    Bug: 348139343
    Change-Id: I04ecc02f8c374a3bb0e0d31dceaa78e83671c2fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5974272
    Reviewed-by: Jan Keitel <jkeitel@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378237}

--- chrome/browser/keyboard_accessory/android/address_accessory_controller_impl_unittest.cc
+++ chrome/browser/keyboard_accessory/android/address_accessory_controller_impl_unittest.cc
@@ -35,10 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/plus_addresses/fake_plus_address_service.h"
 #include "components/plus_addresses/features.h"
 #include "components/plus_addresses/grit/plus_addresses_strings.h"
+#include "components/plus_addresses/plus_address_prefs.h"
 #include "components/plus_addresses/plus_address_test_utils.h"
 #include "components/plus_addresses/plus_address_types.h"
 #include "components/prefs/pref_service.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -513,6 +515,23 @@ TEST_F(AddressAccessoryControllerTest, TriggersPlusAddressCreationBottomSheet) {
                                base::UTF8ToUTF16(plus_address)));
   controller()->OnOptionSelected(
       AccessoryAction::CREATE_PLUS_ADDRESS_FROM_ADDRESS_SHEET);
+}
+
+TEST_F(AddressAccessoryControllerTest, FillsPlusAddressSuggestion) {
+  FieldGlobalId field_id = test::MakeFieldGlobalId();
+  const std::u16string plus_address = u"example@gmail.com";
+
+  EXPECT_CALL(main_frame_autofill_driver(),
+              ApplyFieldAction(mojom::FieldActionType::kReplaceAll,
+                               mojom::ActionPersistence::kFill, field_id,
+                               plus_address));
+  controller()->OnFillingTriggered(
+      field_id, AccessorySheetField::Builder()
+                    .SetSuggestionType(AccessorySuggestionType::kPlusAddress)
+                    .SetDisplayText(plus_address)
+                    .SetSelectable(true)
+                    .Build());
+  EXPECT_TRUE(plus_address_service().was_plus_address_suggestion_filled());
 }
 
 }  // namespace autofill
