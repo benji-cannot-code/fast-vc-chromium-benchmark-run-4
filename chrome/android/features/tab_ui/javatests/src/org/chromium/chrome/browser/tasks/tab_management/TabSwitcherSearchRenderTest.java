6123cc8b5239c3bf1759a4ee2af4e67ec3f8f8c6
Brandon Wylie <wylieb@chromium.org>
2024-11-05 18:53:25 +0000

    [hub] Show search loupe for phones in landscape
    Bug: 377375743
    Change-Id: I4de282c7dcdb513b9be27c7eac01e98bf9d66038
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5991571
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378479}

--- chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
+++ chrome/android/features/tab_ui/javatests/src/org/chromium/chrome/browser/tasks/tab_management/TabSwitcherSearchRenderTest.java
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tasks.tab_management;
 
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
+
 import static org.chromium.chrome.browser.tasks.tab_management.TabUiTestHelper.enterTabSwitcher;
 import static org.chromium.ui.base.DeviceFormFactor.PHONE;
 import static org.chromium.ui.base.DeviceFormFactor.TABLET;
@@ -30,6 +32,7 @@ import org.chromium.chrome.browser.searchwidget.SearchActivity;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.chrome.test.R;
+import org.chromium.chrome.test.util.ActivityTestUtils;
 import org.chromium.chrome.test.util.ChromeRenderTestRule;
 import org.chromium.chrome.test.util.OmniboxTestUtils;
 import org.chromium.net.test.EmbeddedTestServer;
@@ -86,6 +89,20 @@ public class TabSwitcherSearchRenderTest {
 
         mRenderTestRule.render(
                 cta.findViewById(R.id.tab_switcher_view_holder), "hub_searchbox_phone");
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"RenderTest"})
+    @Restriction(PHONE)
+    public void testHubSearchBox_PhoneLandscape() throws IOException {
+        ChromeTabbedActivity cta = mActivityTestRule.getActivity();
+        ActivityTestUtils.rotateActivityToOrientation(cta, ORIENTATION_LANDSCAPE);
+        enterTabSwitcher(cta);
+
+        mRenderTestRule.render(
+                cta.findViewById(R.id.tab_switcher_view_holder), "hub_searchbox_phone_landscape");
+        ActivityTestUtils.clearActivityOrientation(cta);
     }
 
     @Test
