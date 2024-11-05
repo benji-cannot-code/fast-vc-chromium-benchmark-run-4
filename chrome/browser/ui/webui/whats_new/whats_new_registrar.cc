c8c757a3dfb3dcddf6f6883dd25a932fcbcfee15
Alison Gale <agale@google.com>
2024-11-05 18:11:38 +0000

    Whats new module for performance detection
    Change-Id: Iff463709ed813f21382dbe6fa0975e8080c64d99
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983938
    Commit-Queue: Alison Gale <agale@chromium.org>
    Reviewed-by: Mickey Burks <mickeyburks@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378452}

--- chrome/browser/ui/webui/whats_new/whats_new_registrar.cc
+++ chrome/browser/ui/webui/whats_new/whats_new_registrar.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/webui/whats_new/whats_new_storage_service_impl.h"
+#include "components/performance_manager/public/features.h"
 #include "components/user_education/webui/whats_new_registry.h"
 #include "pdf/buildflags.h"
 #include "ui/webui/resources/js/browser_command/browser_command.mojom.h"
@@ -42,6 +43,10 @@ void RegisterWhatsNewModules(whats_new::WhatsNewRegistry* registry) {
   registry->RegisterModule(
       WhatsNewModule(::features::kToolbarPinning, "corising@google.com",
                      BrowserCommand::kShowCustomizeChromeToolbar));
+
+  registry->RegisterModule(
+      WhatsNewModule(performance_manager::features::kPerformanceInterventionUI,
+                     "agale@google.com"));
 }
 
 void RegisterWhatsNewEditions(whats_new::WhatsNewRegistry* registry) {
