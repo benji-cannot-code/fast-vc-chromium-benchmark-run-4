875efcda85879e21a108479218575b7ad929c8fe
Hubert Chao <hchao@chromium.org>
2024-11-05 20:32:32 +0000

    Cert Management UI V2: show constraints in cert viewer
    * add lit dependency for cert viewer
    * add <constraint-list> element
    * pipe constraints through cert_metadata to be visible in
      modifications tab
    * change strings.m.js import as per https://groups.google.com/a/google.com/g/chrome-webui/c/Zej6BsN9_YA/m/jBt2v1CmBAAJ
    
    Bug: 40928765
    Change-Id: I7673787890b5310005e5c60f0bab0b32962ebded
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982979
    Commit-Queue: Hubert Chao <hchao@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378574}

---
+++ chrome/browser/resources/certificate_viewer/constraint_list.html.ts
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {html} from '//resources/lit/v3_0/lit.rollup.js';
+
+import type {ConstraintListElement} from './constraint_list.js';
+
+export function getHtml(this: ConstraintListElement) {
+  return html`
+  <div>
+  ${this.constraints.map(constraint => html`<div>${constraint}</div>`)}
+  </div>`;
+}
---
+++ chrome/browser/resources/certificate_viewer/constraint_list.ts
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {CrLitElement} from '//resources/lit/v3_0/lit.rollup.js';
+
+import {getHtml} from './constraint_list.html.js';
+
+
+export class ConstraintListElement extends CrLitElement {
+  static get is() {
+    return 'constraint-list';
+  }
+
+  override render() {
+    return getHtml.bind(this)();
+  }
+
+  static override get properties() {
+    return {
+      constraints: {type: Array},
+    };
+  }
+
+  constraints: string[] = [];
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'constraint-list': ConstraintListElement;
+  }
+}
+
+customElements.define(ConstraintListElement.is, ConstraintListElement);
