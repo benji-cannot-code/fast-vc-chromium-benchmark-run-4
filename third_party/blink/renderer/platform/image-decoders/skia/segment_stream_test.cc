f9c16e2614759c7bcd2c84791c0595193c79d4ca
Helmut Januschka <helmut@januschka.com>
2024-11-05 20:56:46 +0000

    use c++20 starts_/ends_-with //blink
    Bug: 40256235
    Change-Id: If297502ad749bd92360116c3bde90051d81176d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982559
    Reviewed-by: danakj <danakj@chromium.org>
    Commit-Queue: Helmut Januschka <helmut@januschka.com>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378592}

--- third_party/blink/renderer/platform/image-decoders/skia/segment_stream_test.cc
+++ third_party/blink/renderer/platform/image-decoders/skia/segment_stream_test.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/blink/renderer/platform/image-decoders/skia/segment_stream.h"
 
+#include <string_view>
+
 #include "base/memory/scoped_refptr.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/blink/renderer/platform/image-decoders/segment_reader.h"
