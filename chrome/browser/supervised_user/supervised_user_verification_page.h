b9cca3ea28b11ec70e9d0ef15cf6a428f1689957
anthie@google.com <anthie@google.com>
2024-11-05 13:43:31 +0000

    Add UMA metrics for Youtube subframe re-auth interstitial
    NO_IFTTT=Fixing a typo in FamilyLinkUserReauthenticationInterstitialState, no functional change.
    
    Bug: 374254038
    Change-Id: Id75da9967f760e27c586ce0eef53a2ada13082d8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5966313
    Reviewed-by: James Lee <ljjlee@google.com>
    Commit-Queue: Anthi Orfanou <anthie@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Duong Dac <ddac@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378277}

--- chrome/browser/supervised_user/supervised_user_verification_page.h
+++ chrome/browser/supervised_user/supervised_user_verification_page.h
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class GURL;
 
 // LINT.IfChange(FamilyLinkUserReauthenticationInterstitialState)
-// State of the re-authentication interstitial indicatins if the user
+// State of the re-authentication interstitial indicating if the user
 // has interacted with the sign-in flow.
 enum class FamilyLinkUserReauthenticationInterstitialState : int {
   kInterstitialShown = 0,
@@ -41,6 +41,10 @@ class SupervisedUserVerificationPage
   // Whether the user is in a suitable auth state for this page to be shown.
   static bool ShouldShowPage(
       const supervised_user::ChildAccountService& child_account_service);
+
+  // Helper method for getting the right histogram bucket from a given status.
+  static FamilyLinkUserReauthenticationInterstitialState
+  GetReauthenticationInterstitialStateFromStatus(Status status);
 
   // `request_url` is the URL which triggered the interstitial page. It can be
   // a main frame or a subresource URL.
--- chrome/browser/supervised_user/supervised_user_verification_page_youtube.cc
+++ chrome/browser/supervised_user/supervised_user_verification_page_youtube.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
+#include "base/metrics/histogram_functions.h"
 #include "components/grit/components_resources.h"
 #include "components/security_interstitials/content/security_interstitial_controller_client.h"
 #include "components/security_interstitials/core/common_string_util.h"
@@ -15,6 +16,38 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_recorder.h"
 #include "ui/base/l10n/l10n_util.h"
+
+namespace {
+constexpr char kSubframeYoutubeReauthenticationInterstitiaHistogramName[] =
+    "FamilyLinkUser.SubframeYoutubeReauthenticationInterstitial";
+
+void RecordUkmForMainFrame(SupervisedUserVerificationPage::Status status,
+                           ukm::SourceId source_id) {
+  auto builder =
+      ukm::builders::FamilyLinkUser_ReauthenticationInterstitial(source_id);
+  switch (status) {
+    case SupervisedUserVerificationPage::Status::SHOWN:
+      builder.SetInterstitialShown(true);
+      break;
+    case SupervisedUserVerificationPage::Status::REAUTH_STARTED:
+      builder.SetReauthenticationStarted(true);
+      break;
+    case SupervisedUserVerificationPage::Status::REAUTH_COMPLETED:
+      builder.SetReauthenticationCompleted(true);
+      break;
+    default:
+      NOTREACHED();
+  }
+  builder.Record(ukm::UkmRecorder::Get());
+}
+
+void RecordUmaForSubFrame(SupervisedUserVerificationPage::Status status) {
+  base::UmaHistogramEnumeration(
+      kSubframeYoutubeReauthenticationInterstitiaHistogramName,
+      SupervisedUserVerificationPage::
+          GetReauthenticationInterstitialStateFromStatus(status));
+}
+}  // namespace
 
 // static
 const security_interstitials::SecurityInterstitialPage::TypeID
@@ -88,25 +121,9 @@ void SupervisedUserVerificationPageForYouTube::PopulateInterstitialStrings(
 
 void SupervisedUserVerificationPageForYouTube::RecordReauthStatusMetrics(
     Status status) {
-  if (!is_main_frame_) {
-    // Do not record metrics for subframe interstitials.
-    return;
+  if (is_main_frame_) {
+    RecordUkmForMainFrame(status, source_id_);
+  } else {
+    RecordUmaForSubFrame(status);
   }
-
-  auto builder =
-      ukm::builders::FamilyLinkUser_ReauthenticationInterstitial(source_id_);
-  switch (status) {
-    case Status::SHOWN:
-      builder.SetInterstitialShown(true);
-      break;
-    case Status::REAUTH_STARTED:
-      builder.SetReauthenticationStarted(true);
-      break;
-    case Status::REAUTH_COMPLETED:
-      builder.SetReauthenticationCompleted(true);
-      break;
-    default:
-      NOTREACHED();
-  }
-  builder.Record(ukm::UkmRecorder::Get());
 }
