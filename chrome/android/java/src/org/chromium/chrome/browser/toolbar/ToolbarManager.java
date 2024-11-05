c3a080b25a0953807888b874896677de9ae138d2
Lijin Shen <lazzzis@google.com>
2024-11-05 02:19:44 +0000

    [DNT] Add logs and speculative fix for non-navigatable page on start
    Add logs and sepcualtive fix to track whey the onBackStarted is
    triggered even when the page is not navigatable.
    
    Bug: 376928606
    Change-Id: Ie5976329cb1318a54be5778a39e3df4ecc056652
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5990245
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Lijin Shen <lazzzis@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378074}

--- chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/toolbar/ToolbarManager.java
@@ -463,12 +463,18 @@ public class ToolbarManager
         public void handleOnBackProgressed(@NonNull BackEventCompat backEvent) {
             if (mHandler == null) return;
             if (!mIsInProgress) {
+                assert mActivityTabProvider.get().canGoBack()
+                        : "Tab should be navigable when starting a new onStarted";
+                assert mHandler == null : "No active handler when starting a new onStarted";
+                mHandler = null;
                 handleOnBackStarted(backEvent);
                 return;
             }
 
             if (mInitialEdge != backEvent.getSwipeEdge()) {
                 handleOnBackCancelled();
+                assert mActivityTabProvider.get().canGoBack()
+                        : "Tab should be navigable when restarting gesture";
                 handleOnBackStarted(backEvent);
                 return;
             }
@@ -484,6 +490,8 @@ public class ToolbarManager
 
         @Override
         public void handleOnBackStarted(@NonNull BackEventCompat backEvent) {
+            assert mActivityTabProvider.get().canGoBack()
+                    : "Tab should be navigable when gesture starts";
             mIsInProgress = true;
             mIsGestureMode = UiUtils.isGestureNavigationMode(mActivity.getWindow());
             mInitialEdge = backEvent.getSwipeEdge();
@@ -507,8 +515,6 @@ public class ToolbarManager
             if (!mIsGestureMode) return;
             if (!GestureNavigationUtils.allowTransition(mActivityTabProvider.get(), false)) return;
 
-            mHandler = TabOnBackGestureHandler.from(mActivityTabProvider.get());
-
             boolean navigatesForward = isForward();
             if (TabOnBackGestureHandler.shouldAnimateNavigationTransition(
                     navigatesForward, backEvent.getSwipeEdge())) {
@@ -517,14 +523,15 @@ public class ToolbarManager
                         mLocationBarModel.getTab(),
                         BrowserControlsState.SHOWN,
                         /* animate= */ true);
+                mHandler = TabOnBackGestureHandler.from(mActivityTabProvider.get());
+                mHandler.onBackStarted(
+                        backEvent.getProgress(),
+                        backEvent.getSwipeEdge() == BackEventCompat.EDGE_LEFT
+                                ? BackGestureEventSwipeEdge.LEFT
+                                : BackGestureEventSwipeEdge.RIGHT,
+                        navigatesForward,
+                        mIsGestureMode);
             }
-            mHandler.onBackStarted(
-                    backEvent.getProgress(),
-                    backEvent.getSwipeEdge() == BackEventCompat.EDGE_LEFT
-                            ? BackGestureEventSwipeEdge.LEFT
-                            : BackGestureEventSwipeEdge.RIGHT,
-                    navigatesForward,
-                    mIsGestureMode);
         }
     }
 
