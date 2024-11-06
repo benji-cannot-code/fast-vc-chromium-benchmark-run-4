4ef6b56c17539b812f847c25b178bb9f8342162d
dpapad <dpapad@chromium.org>
2024-11-06 01:11:25 +0000

    WebUI: Infer TS path_mappings entry from deps for third_party/d3.
    This fixes an issue with siso remote execution that was previously fixed
    with a workaround at crrev.com/c/5964076, by ensuring that the file
    needed to properly resolve d3.min.js imports is declared as a GN input.
    
    Effectively the fix allows correctly resolving the following
    side-effect import
    
    import 'chrome://resources/d3/d3.min.js';
    
    as long as the //third_party/d3:library target is declared as a
    dependency.
    
    Bug: 326005022
    Change-Id: I58d64541bba7a3604910494baa91fb99482e3b82
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994953
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378690}

---
+++ third_party/d3/src/d3.d.ts
@@ -0,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Intentionally empty dummy file. This is used as a path_mapping for
+// '//resources/d3/d3.min.js' import URLs encountered by the TS compiler.
