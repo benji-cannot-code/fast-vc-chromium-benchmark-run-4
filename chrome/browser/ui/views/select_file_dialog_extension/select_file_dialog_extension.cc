64a5dd7a221ba5c5199d1becd2858b5d7132636d
Antonio Gomes <tonikitoo@igalia.com>
2024-11-06 13:03:30 +0000

    Remove SelectFileDialogExtension::Owner::lacros_window_id
    ... and related logic now that lacros project is terminated.
    
    In summary, the CL removes lacros-specific logic in the touched files,
    as well as an unneeded include.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 377475532, 373971535
    Change-Id: I53d089f6e4946744eb0c4a6d5d1eb1db3f7c5013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993442
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378940}

--- chrome/browser/ui/views/select_file_dialog_extension/select_file_dialog_extension.cc
+++ chrome/browser/ui/views/select_file_dialog_extension/select_file_dialog_extension.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/no_destructor.h"
 #include "base/strings/stringprintf.h"
-#include "build/chromeos_buildflags.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/apps/platform_apps/app_window_registry_util.h"
 #include "chrome/browser/ash/extensions/file_manager/select_file_dialog_extension_user_data.h"
@@ -174,10 +173,6 @@ SelectFileDialogExtension::RoutingID GetRoutingID(
     const SelectFileDialogExtension::Owner& owner) {
   if (owner.android_task_id.has_value())
     return base::StringPrintf("android.%d", *owner.android_task_id);
-
-  // Lacros ids are already prefixed with "lacros".
-  if (owner.lacros_window_id.has_value())
-    return *owner.lacros_window_id;
 
   if (web_contents) {
     return base::StringPrintf(
@@ -454,9 +449,8 @@ void SelectFileDialogExtension::SelectFileWithFileManagerParams(
       owner.window->GetId() ==
           ash::kShellWindowId_CaptureModeFolderSelectionDialogOwner;
 
-  const bool skip_finding_browser = is_for_capture_mode ||
-                                    owner.android_task_id.has_value() ||
-                                    owner.lacros_window_id.has_value();
+  const bool skip_finding_browser =
+      is_for_capture_mode || owner.android_task_id.has_value();
 
   can_resize_ =
       !display::Screen::GetScreen()->InTabletMode() && !is_for_capture_mode;
@@ -482,12 +476,7 @@ void SelectFileDialogExtension::SelectFileWithFileManagerParams(
     return;
 
   std::vector<std::string> volume_filter;
-  if (owner.is_lacros) {
-    // SelectFileAsh (Lacros) is opening the dialog: only show fusebox volumes
-    // in File Manager UI to return real file descriptors to SelectFileAsh.
-    // TODO(crbug.com/369851375): Delete this; Lacros has sunset.
-    volume_filter.push_back("fusebox-only");
-  } else if (use_media_store_filter) {
+  if (use_media_store_filter) {
     // ArcSelectFile is opening the dialog: add 'media-store-files-only' filter
     // to only show volumes in File Manager UI that are indexed by the Android
     // MediaStore.
