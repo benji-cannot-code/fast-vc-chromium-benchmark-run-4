e7f7aa301762c945b7fc0ea88f872cb16acad104
W. James MacLean <wjmaclean@chromium.org>
2024-11-06 20:05:21 +0000

    Add pointer crashkeys, bug number.
    This CL is a follow-on to
    https://chromium-review.googlesource.com/c/chromium/src/+/5979009 to
    add crashkeys for the various target pointers, so we can see which ones
    are populated. It also adds the bug number to the crashkey names.
    
    This CL also adds a feature that can be used to implement a killswitch
    in case the DumpWithoutCrashing volumes get too high.
    
    Bug: 346629231
    Change-Id: I09ea2a32a690bb35994ec8a7ea2c9109aedac1fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5992371
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Commit-Queue: James Maclean <wjmaclean@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379197}

--- components/input/features.h
+++ components/input/features.h
@@ -19,6 +19,8 @@ namespace input::features {
 
 COMPONENT_EXPORT(INPUT) BASE_DECLARE_FEATURE(kInputOnViz);
 #endif
+COMPONENT_EXPORT(INPUT)
+BASE_DECLARE_FEATURE(kLogBubblingTouchscreenGesturesForDebug);
 COMPONENT_EXPORT(INPUT) BASE_DECLARE_FEATURE(kScrollBubblingFix);
 
 }  // namespace input::features
