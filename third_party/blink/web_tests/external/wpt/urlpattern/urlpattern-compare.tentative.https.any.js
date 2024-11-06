98ac22e9e34bf996964cb7ebbeecf1cf0ff95102
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-06 06:31:51 +0000

    Import wpt@1b56d89a261b86dedfd2854b53c1732e435f1f57
    https://github.com/web-platform-tests/wpt/compare/a6c717293...1b56d89a2
    
    Using wpt-import in Chromium d4631ac72e5b6fac2dc454ff15a9d162b8c7fc4f.
    With Chromium commits locally applied on WPT:
      272e26eb5c "[CodeHealth] Misc cleanup in PointerEvent wpts"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/wpt/css/css-shapes
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I6cbaf94b1380b2f3095a03eb89bc0f670a05c8b0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5997191
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378818}

--- third_party/blink/web_tests/external/wpt/urlpattern/urlpattern-compare.tentative.https.any.js
+++ third_party/blink/web_tests/external/wpt/urlpattern/urlpattern-compare.tentative.https.any.js
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // META: global=window,worker
-// META: script=resources/urlpattern-compare-tests.js
+// META: script=resources/urlpattern-compare-tests.tentative.js
