c41868793e39a82b644f4c966d112de904a66b51
Etienne Pierre-doray <etiennep@chromium.org>
2024-11-06 16:54:52 +0000

    [tracing] Implement clone session in mojo backend
    Following up on
    https://android-review.git.corp.google.com/c/platform/external/perfetto/+/3312680
    This CL adds support for clone session in mojo backend
    
    Bug: 374302723
    Change-Id: I9fbe924fc842b79a6928b6b7c730713b50451820
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987833
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379053}

--- services/tracing/perfetto/consumer_host.h
+++ services/tracing/perfetto/consumer_host.h
@@ -52,6 +52,11 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
         const perfetto::TraceConfig& trace_config,
         perfetto::base::ScopedFile output_file,
         mojom::TracingClientPriority priority);
+    TracingSession(
+        ConsumerHost* host,
+        mojo::PendingReceiver<mojom::TracingSessionHost> tracing_session_host,
+        mojo::PendingRemote<mojom::TracingSessionClient> tracing_session_client,
+        mojom::TracingClientPriority priority);
 
     TracingSession(const TracingSession&) = delete;
     TracingSession& operator=(const TracingSession&) = delete;
@@ -62,8 +67,11 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
     void OnTraceData(std::vector<perfetto::TracePacket> packets, bool has_more);
     void OnTraceStats(bool success, const perfetto::TraceStats&);
     void OnTracingDisabled(const std::string& error);
+    void OnSessionCloned(const OnSessionClonedArgs&);
     void OnConsumerClientDisconnected();
     void Flush(uint32_t timeout, base::OnceCallback<void(bool)> callback);
+    void CloneSession(const base::UnguessableToken& uuid,
+                      CloneSessionCallback callback);
 
     mojom::TracingClientPriority tracing_priority() const {
       return tracing_priority_;
@@ -104,6 +112,7 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
     bool convert_to_legacy_json_ = false;
     base::SequenceBound<StreamWriter> read_buffers_stream_writer_;
     RequestBufferUsageCallback request_buffer_usage_callback_;
+    CloneSessionCallback on_session_cloned_callback_;
     std::unique_ptr<perfetto::trace_processor::TraceProcessorStorage>
         trace_processor_;
     std::string json_agent_label_filter_;
@@ -149,6 +158,11 @@ class ConsumerHost : public perfetto::Consumer, public mojom::ConsumerHost {
       mojo::PendingRemote<mojom::TracingSessionClient> tracing_session_client,
       const perfetto::TraceConfig& config,
       base::File output_file) override;
+  void CloneSession(
+      mojo::PendingReceiver<mojom::TracingSessionHost> tracing_session_host,
+      mojo::PendingRemote<mojom::TracingSessionClient> tracing_session_client,
+      const base::UnguessableToken& uuid,
+      CloneSessionCallback callback) override;
 
   // perfetto::Consumer implementation.
   // This gets called by the Perfetto service as control signals,
--- services/tracing/public/mojom/trace_config_mojom_traits.cc
+++ services/tracing/public/mojom/trace_config_mojom_traits.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "mojo/public/cpp/base/token_mojom_traits.h"
+#include "mojo/public/cpp/base/unguessable_token_mojom_traits.h"
 #include "services/tracing/public/mojom/data_source_config_mojom_traits.h"
 
 namespace mojo {
@@ -79,12 +80,14 @@ bool StructTraits<tracing::mojom::TraceConfigDataView, perfetto::TraceConfig>::
   std::vector<perfetto::TraceConfig::DataSource> data_sources;
   std::vector<perfetto::TraceConfig::BufferConfig> buffers;
   std::optional<base::Token> trace_uuid;
+  std::optional<base::UnguessableToken> unique_session_name;
   if (!data.ReadDataSources(&data_sources) || !data.ReadBuffers(&buffers) ||
       !data.ReadPerfettoBuiltinDataSource(
           out->mutable_builtin_data_sources()) ||
       !data.ReadIncrementalStateConfig(
           out->mutable_incremental_state_config()) ||
-      !data.ReadTraceUuid(&trace_uuid)) {
+      !data.ReadTraceUuid(&trace_uuid) ||
+      !data.ReadUniqueSessionName(&unique_session_name)) {
     return false;
   }
 
@@ -102,6 +105,9 @@ bool StructTraits<tracing::mojom::TraceConfigDataView, perfetto::TraceConfig>::
   if (trace_uuid) {
     out->set_trace_uuid_msb(trace_uuid->high());
     out->set_trace_uuid_lsb(trace_uuid->low());
+  }
+  if (unique_session_name) {
+    out->set_unique_session_name(unique_session_name->ToString());
   }
   return true;
 }
