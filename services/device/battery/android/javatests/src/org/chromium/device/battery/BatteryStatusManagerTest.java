c0f0e2c2b828113fb14057a1b7f96f1fd03e36c2
Nafis Abedin <nafisabedin@google.com>
2024-11-06 18:35:20 +0000

    [Reland] Move BatteryStatusManager BroadcastReceiver registration/unregistration to the background.
    This is a reland of commit b7fc93939544f1f319d96235bf91cc36c0156256
    
    The original CL was reverted due to a build failure in android-cast-arm-rel. The reland is rebased on top of @agrieve's latest changes that address the problem (https://crrev.com/c/5995610). The device_feature_list target is merged into the device_features component for easier inclusion.
    
    Original change's description:
    > [Clank Startup] Move BatteryStatusManager BroadcastReceiver
    > registration/unregistration to the background.
    >
    > This CL adds the logic for an experiment posting the broadcast receiver registration/unregistration to a background thread in an effort to reduce Binder calls made on the UI thread.
    >
    > Testing: Previous CLs have utilized the following website https://output.jsbin.com/battery-status-test.
    >
    > Bug: 376251730
    > Change-Id: I1272376a5712b41a05ede2c0e9c58703276fa9a9
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979229
    > Reviewed-by: Reilly Grant <reillyg@chromium.org>
    > Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    > Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1377642}
    
    Bug: 376251730
    Change-Id: I9ace15c89aeec8021da647a145d959c1aa94944b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5989250
    Commit-Queue: Nafis Abedin <nafisabedin@google.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379127}

--- services/device/battery/android/javatests/src/org/chromium/device/battery/BatteryStatusManagerTest.java
+++ services/device/battery/android/javatests/src/org/chromium/device/battery/BatteryStatusManagerTest.java
@@ -180,7 +180,7 @@ public class BatteryStatusManagerTest {
     @Test
     @SmallTest
     public void testStartStopSucceeds() {
-        Assert.assertTrue(mManager.start());
+        mManager.start();
         mManager.stop();
     }
 
