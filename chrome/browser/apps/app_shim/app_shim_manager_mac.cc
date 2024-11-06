7afa06f7607b0f1ff3c77ea0061031e681d51c12
Morten Stenshorne <mstensho@chromium.org>
2024-11-06 01:12:56 +0000

    [CodeHealth] Remove stale base::Feature AppShimProfileMenuIcons
    Enabled by default since M113.
    
    Bug: 356235375, 40206692
    Change-Id: I5a246bd1f9d1693f5142b9a15cc211c852e057d7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993414
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378693}

--- chrome/browser/apps/app_shim/app_shim_manager_mac.cc
+++ chrome/browser/apps/app_shim/app_shim_manager_mac.cc
@@ -84,13 +84,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// A feature to control whether or not the profile icons are sent over mojo.
-// This is used to debug crashes that are only seen in release builds.
-// https://crbug.com/1274236
-BASE_FEATURE(kAppShimProfileMenuIcons,
-             "AppShimProfileMenuIcons",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
 // A crash key that is used when dumping because of errors when building and
 // verifying the app shim requirement.
 crash_reporter::CrashKeyString<256> app_shim_requirement_crash_key(
@@ -1789,10 +1782,8 @@ void AppShimManager::RebuildProfileMenuItemsFromAvatarMenu() {
     mojo_item->menu_index = item.menu_index;
     mojo_item->active = item.active;
     mojo_item->profile_path = item.profile_path;
-    if (base::FeatureList::IsEnabled(kAppShimProfileMenuIcons)) {
-      mojo_item->icon =
-          profiles::GetAvatarIconForNSMenu(item.profile_path).ToImageSkia()[0];
-    }
+    mojo_item->icon =
+        profiles::GetAvatarIconForNSMenu(item.profile_path).ToImageSkia()[0];
     profile_menu_items_.push_back(std::move(mojo_item));
   }
 }
