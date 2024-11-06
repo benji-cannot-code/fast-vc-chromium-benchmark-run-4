5645b7fdeb23598fdbe3832da2ffdac5eae5603f
Chris Lu <thegreenfrog@chromium.org>
2024-11-06 23:30:33 +0000

    [ios] Show prominence notif setting alert upon app notif setting toggle
    When the user toggles a feature notification on in the in-app settings
    page, IsProvisionalNotificationAlertEnabled is true, and the user
    currently doesn't have the app's OS-level notification enabled to show
    as alerts and in the lockscreen, an alert modal will show informing the
    user of that and giving them a button to go to those settings.
    
    Bug: 370956441
    Change-Id: Ie06bffc315eb344f6a880ed63ea33dbd8589ad98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994895
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Chris Lu <thegreenfrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379336}

--- ios/chrome/browser/ui/push_notification/notifications_opt_in_alert_coordinator.mm
+++ ios/chrome/browser/ui/push_notification/notifications_opt_in_alert_coordinator.mm
@@ -24,8 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/public/commands/command_dispatcher.h"
 #import "ios/chrome/browser/shared/public/commands/settings_commands.h"
 #import "ios/chrome/browser/shared/public/commands/snackbar_commands.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/sync/model/device_info_sync_service_factory.h"
 #import "ios/chrome/browser/ui/push_notification/metrics.h"
+#import "ios/chrome/browser/ui/push_notification/prominence_notification_setting_alert_coordinator.h"
+#import "ios/chrome/browser/ui/push_notification/prominence_notification_setting_alert_coordinator_delegate.h"
 #import "ios/chrome/grit/ios_branded_strings.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ui/base/l10n/l10n_util_mac.h"
@@ -45,11 +48,16 @@ NSString* GetGaiaIdForProfile(ProfileIOS* profile) {
 
 }  // namespace
 
+@interface NotificationsOptInAlertCoordinator () <
+    ProminenceNotificationSettingAlertCoordinatorDelegate>
+@end
+
 @implementation NotificationsOptInAlertCoordinator {
   SEQUENCE_CHECKER(sequence_checker_);
   // The coordinator used to present the alert used when permission has
   // previously been denied.
   AlertCoordinator* _alertCoordinator;
+  ProminenceNotificationSettingAlertCoordinator* _prominenceAlertCoordinator;
 }
 
 - (void)start {
@@ -61,6 +69,16 @@ NSString* GetGaiaIdForProfile(ProfileIOS* profile) {
 - (void)stop {
   [_alertCoordinator stop];
   _alertCoordinator = nil;
+}
+
+#pragma mark - ProminenceNotificationSettingAlertCoordinatorDelegate
+
+- (void)prominenceNotificationSettingAlertCoordinatorIsDone:
+    (ProminenceNotificationSettingAlertCoordinator*)coordinator {
+  DCHECK(coordinator == _prominenceAlertCoordinator);
+  [_prominenceAlertCoordinator stop];
+  _prominenceAlertCoordinator = nil;
+  [self setResult:NotificationsOptInAlertResult::kPermissionGranted];
 }
 
 #pragma mark - Private methods
@@ -102,6 +120,28 @@ NSString* GetGaiaIdForProfile(ProfileIOS* profile) {
     if (self.confirmationMessage) {
       [self showConfirmationSnackbar];
     }
+    if (IsProvisionalNotificationAlertEnabled()) {
+      __weak __typeof(self) weakSelf = self;
+      [PushNotificationUtil
+          getPermissionSettings:^(UNNotificationSettings* settings) {
+            [weakSelf onPermissionSettingResult:settings];
+          }];
+    } else {
+      [self setResult:NotificationsOptInAlertResult::kPermissionGranted];
+    }
+  }
+}
+
+- (void)onPermissionSettingResult:(UNNotificationSettings*)settings {
+  if (settings.lockScreenSetting == UNNotificationSettingDisabled ||
+      settings.alertSetting == UNNotificationSettingDisabled) {
+    _prominenceAlertCoordinator =
+        [[ProminenceNotificationSettingAlertCoordinator alloc]
+            initWithBaseViewController:self.baseViewController
+                               browser:self.browser];
+    _prominenceAlertCoordinator.delegate = self;
+    [_prominenceAlertCoordinator start];
+  } else {
     [self setResult:NotificationsOptInAlertResult::kPermissionGranted];
   }
 }
