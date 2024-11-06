480a9b9ada75da601c5d3f861ba6a7d973e1572b
Rune Lillesveen <futhark@chromium.org>
2024-11-06 13:53:25 +0000

    WebDX mapping for white-space-collapse
    Change-Id: Ie5e475520145b62e1d9273d56c02c74e1435ebfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5991431
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Commit-Queue: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378955}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -571,6 +571,7 @@ UseCounterMetricsRecorder::GetCSSProperties2WebDXFeatureMap() {
           {CSSSampleId::kCounterSet, WebDXFeature::kCounterSet},
           {CSSSampleId::kObjectViewBox, WebDXFeature::kObjectViewBox},
           {CSSSampleId::kForcedColorAdjust, WebDXFeature::kForcedColors},
+          {CSSSampleId::kWhiteSpaceCollapse, WebDXFeature::kWhiteSpaceCollapse},
       }};
 
   return *kMap;
