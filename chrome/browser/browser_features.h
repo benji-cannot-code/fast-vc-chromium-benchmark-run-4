ae353f6df0d7b2b18b4d58b1dd3c1e74cd167786
Morten Stenshorne <mstensho@chromium.org>
2024-11-06 09:44:01 +0000

    [CodeHealth] Remove stale base::Feature ClosedTabCache
    The flag didn't control anything. There is an `id` member in
    DetachedWebContents, though, which seems to be left behind by this
    feature. But it's not clear if it can be removed. Add a TODO for that.
    
    Bug: 356234804
    Change-Id: I3d522662d25c02e2c11cbe3cdc323bb857ce5783
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993416
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378885}

--- chrome/browser/browser_features.h
+++ chrome/browser/browser_features.h
@@ -37,7 +37,6 @@ BASE_DECLARE_FEATURE(kBookmarksTreeView);
 BASE_DECLARE_FEATURE(kBookmarkTriggerForPrerender2);
 BASE_DECLARE_FEATURE(kCertificateTransparencyAskBeforeEnabling);
 BASE_DECLARE_FEATURE(kCertVerificationNetworkTime);
-BASE_DECLARE_FEATURE(kClosedTabCache);
 
 #if BUILDFLAG(IS_LINUX)
 BASE_DECLARE_FEATURE(kDbusSecretPortal);
