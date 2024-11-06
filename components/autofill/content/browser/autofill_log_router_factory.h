b396010f3970d66ca2208840454e0bb86a1d177d
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-06 15:00:04 +0000

    [Autofill log]Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 1396138
    Change-Id: I835ffb838ba45b00a63fc730c80c71abe834357f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802084
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378996}

--- components/autofill/content/browser/autofill_log_router_factory.h
+++ components/autofill/content/browser/autofill_log_router_factory.h
@@ -36,7 +36,7 @@ class AutofillLogRouterFactory : public BrowserContextKeyedServiceFactory {
   ~AutofillLogRouterFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
