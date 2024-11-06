71898e85b6406ed6c8af1cb81d6d50685592df1d
Jayden Zangari <jaydenz@google.com>
2024-11-06 01:45:48 +0000

    Improved performance of the Mahi Q&A view
    Improved performance of the Mahi Q&A view. Full explanation of the
    performance improvements can be found in go/pompano-ui-performance. This CL implements solution 2 in the document.
    
    Also limit the size of the Mahi panel to 540, 600 pixels.
    
    Before:  https://screencast.googleplex.com/cast/NTgxNDQzMjIxNDAyNDE5MnwxMDk5MjJhNC00Zg
    After: https://screencast.googleplex.com/cast/NjY4NjA2Njc1MTgzMjA2NHxmMGQ4YmE2OC0xYg
    
    bugs: b:374174232
    Change-Id: If553f72ca92b8aa64d9759ab7ab719b9964f176b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5981818
    Commit-Queue: Andre Le <leandre@chromium.org>
    Auto-Submit: Jayden Zangari <jaydenz@google.com>
    Reviewed-by: Andre Le <leandre@chromium.org>
    Reviewed-by: Michelle Chen <michellegc@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378709}

--- ash/system/mahi/mahi_widget_delegate.cc
+++ ash/system/mahi/mahi_widget_delegate.cc
@@ -31,6 +31,12 @@ class MahiFrameView : public NonClientFrameViewAsh {
     return gfx::Size(mahi_constants::kPanelDefaultWidth,
                      mahi_constants::kPanelDefaultHeight);
   }
+
+  // views::NonClientFrameView:
+  gfx::Size GetMaximumSize() const override {
+    return gfx::Size(mahi_constants::kPanelMaximumWidth,
+                     mahi_constants::kPanelMaximumHeight);
+  }
 };
 
 }  // namespace
