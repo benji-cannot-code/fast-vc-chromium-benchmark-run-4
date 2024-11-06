caa45fc172f58f7c9d34ca42007d2171a77d6dd2
Steinar H. Gunderson <sesse@chromium.org>
2024-11-06 09:34:23 +0000

    Add WebDX feature mappings for nesting and nth-child-of.
    Change-Id: I52856bfd961a2e43fd9fd368fd6d53fa7c0ff949
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993494
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378878}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -505,6 +505,9 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kHasRicUnits, WebDXFeature::kRic},
           {WebFeature::kHasLhUnits, WebDXFeature::kLh},
           {WebFeature::kHasRlhUnits, WebDXFeature::kRlh},
+          {WebFeature::kCSSNesting, WebDXFeature::kNesting},
+          {WebFeature::kCSSSelectorNthChildOfSelector,
+           WebDXFeature::kNthChildOf},
       }};
 
   return *kMap;
