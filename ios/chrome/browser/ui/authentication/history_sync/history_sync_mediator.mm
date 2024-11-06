3aa90ad3a4ac9e51ec1a5f0f660e6d5ba5950bae
Marc Treib <treib@chromium.org>
2024-11-06 09:39:26 +0000

    [iOS] HistorySyncMediator: Only care about primary account
    Before this CL: HistorySyncMediator updates its avatar image on every
    `identityUpdated` call, even if the updated identity is different from
    the primary one (for which this UI is shown). This could, in rare cases,
    lead to wrong avatar images showing up.
    After this CL: Notifications for non-primary accounts are ignored.
    
    Bug: 377467350
    Change-Id: Ie7ab5d325ffcf15807ad81f05d4f88ee98687c81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993737
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378881}

--- ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
+++ ios/chrome/browser/ui/authentication/history_sync/history_sync_mediator.mm
@@ -142,7 +142,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - ChromeAccountManagerServiceObserver
 
 - (void)identityUpdated:(id<SystemIdentity>)identity {
-  [self updateAvatarImageWithIdentity:identity];
+  if ([identity isEqual:_authenticationService->GetPrimaryIdentity(
+                            signin::ConsentLevel::kSignin)]) {
+    [self updateAvatarImageWithIdentity:identity];
+  }
 }
 
 - (void)onChromeAccountManagerServiceShutdown:
