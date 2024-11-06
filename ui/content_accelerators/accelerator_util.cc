65c0e3159c7a904bfbb69d1c7947de77360ff2ab
Maksim Sisov <msisov@igalia.com>
2024-11-06 11:48:04 +0000

    Cleanup //ui/content_accelerators from LaCros support code.
    No functional changes are done.
    
    1) Renamed IS_CHROMEOS_ASH to IS_CHROMEOS
    2) Removed chromeos_buildflags.h includes and deps
    3) General clean ups.
    
    Bug: 373972275, 377410076
    Change-Id: I722e0e0aed8aa38c79d4d6cf30844fb2b21bc22a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993581
    Reviewed-by: Georg Neis <neis@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378911}

--- ui/content_accelerators/accelerator_util.cc
+++ ui/content_accelerators/accelerator_util.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/content_accelerators/accelerator_util.h"
 
 #include "build/build_config.h"
-#include "build/chromeos_buildflags.h"
 #include "third_party/blink/public/common/input/web_input_event.h"
 #include "ui/base/ui_base_features.h"
 #include "ui/events/blink/blink_event_util.h"
@@ -17,7 +16,7 @@ namespace ui {
 
 ui::Accelerator GetAcceleratorFromNativeWebKeyboardEvent(
     const input::NativeWebKeyboardEvent& event) {
-#if BUILDFLAG(IS_CHROMEOS_ASH)
+#if BUILDFLAG(IS_CHROMEOS)
   if (::features::IsImprovedKeyboardShortcutsEnabled()) {
     // TODO: This must be the same as below and it's simpler.
     // Cleanup if this change sticks.
