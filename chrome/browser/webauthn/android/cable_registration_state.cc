2c04e8fc1f04c0b80d94f52367e0530daf687a90
Peter Kasting <pkasting@chromium.org>
2024-11-06 01:51:02 +0000

    Remove EXTENT.
    This isn't necessary; everywhere it's used can use alternatives like
    constexpr size() (which is clearer), or in the limit std::tuple_size_v.
    
    Bug: 364987728
    Change-Id: I5f3a97490765aca2fbb80963902071910b5ad397
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994531
    Reviewed-by: Martin Kreichgauer <martinkr@google.com>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Martin Kreichgauer <martinkr@google.com>
    Cr-Commit-Position: refs/heads/main@{#1378718}

--- chrome/browser/webauthn/android/cable_registration_state.cc
+++ chrome/browser/webauthn/android/cable_registration_state.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/webauthn/android/cable_registration_state.h"
 
+#include <array>
+
 #include "base/base64.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback.h"
@@ -157,7 +159,7 @@ void RegistrationState::MaybeFlushPendingEvent() {
     // and we piggyback on that to transmit fresh keys. Therefore syncing
     // peers should have reasonably recent information.
     uint64_t id;
-    static_assert(EXTENT(event->pairing_id) == sizeof(id), "");
+    static_assert(std::tuple_size_v<decltype(event->pairing_id)> == sizeof(id));
     memcpy(&id, event->pairing_id.data(), sizeof(id));
 
     // A maximum age is enforced for sync secrets so that any leak of
