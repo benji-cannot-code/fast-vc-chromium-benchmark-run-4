46bceef2d6a5c36757bfec77dfce5f1cde98fdf2
Maria Kazinova <kazinova@google.com>
2024-11-06 09:55:51 +0000

    Add a new HeuristicSource enum value for Password predictions.
    This CL adds a new enum value to allow distinguishing field types
    set in the FormStructure's fields by Password Manager model, from
    types predicted by Autofill model.
    
    Bug: 371933424
    Change-Id: Id09bc157b645029f2dc41095fc4f69978c9f092e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5992795
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Reviewed-by: Florian Leimgruber <fleimgruber@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378891}

--- components/autofill/core/browser/heuristic_source.cc
+++ components/autofill/core/browser/heuristic_source.cc
@@ -19,7 +19,7 @@ HeuristicSource GetActiveHeuristicSource() {
     static bool model_predictions_active =
         features::kAutofillModelPredictionsAreActive.Get();
     if (model_predictions_active) {
-      return HeuristicSource::kMachineLearning;
+      return HeuristicSource::kAutofillMachineLearning;
     }
   }
 #if BUILDFLAG(USE_INTERNAL_AUTOFILL_PATTERNS)
@@ -45,7 +45,8 @@ std::optional<PatternFile> HeuristicSourceToPatternFile(
     case HeuristicSource::kPredictionImprovementRegexes:
       return PatternFile::kPredictionImprovements;
 #endif
-    case autofill::HeuristicSource::kMachineLearning:
+    case autofill::HeuristicSource::kAutofillMachineLearning:
+    case HeuristicSource::kPasswordManagerMachineLearning:
       return std::nullopt;
   }
   NOTREACHED();
--- components/autofill/core/browser/heuristic_source.h
+++ components/autofill/core/browser/heuristic_source.h
@@ -25,8 +25,9 @@ enum class HeuristicSource {
   kExperimentalRegexes,
   kPredictionImprovementRegexes,
 #endif
-  kMachineLearning,
-  kMaxValue = kMachineLearning
+  kAutofillMachineLearning,
+  kPasswordManagerMachineLearning,
+  kMaxValue = kPasswordManagerMachineLearning
 };
 
 // The active heuristic sources depend on the build config and Finch configs.
