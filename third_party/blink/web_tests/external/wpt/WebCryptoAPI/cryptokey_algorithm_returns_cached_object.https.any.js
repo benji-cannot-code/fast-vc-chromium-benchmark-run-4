3dca1def0e659657fe94af73be76b3a5e15a363a
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-06 01:52:50 +0000

    Import wpt@a6c717293ac291bd8085ede7af88717141d6ee69
    https://github.com/web-platform-tests/wpt/compare/db9f0ab6e...a6c717293
    
    Using wpt-import in Chromium 4cd21c9009163dbccbf545d9e7bcfc6d2a782360.
    With Chromium commits locally applied on WPT:
      4c883e5e24 "Fix wpt flaky tests"
      a5048548de "[layout] 2 tests for stretch keyword"
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    bjonesbe@adobe.com:
      external/wpt/css/css-shapes
    cbiesinger@chromium.org:
      external/wpt/css/css-sizing
    
    NOAUTOREVERT=true
    R=rubber-stamper@appspot.gserviceaccount.com
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I69d20862c537acff6bb9d9f7150e991ce12bc228
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5995490
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378720}

---
+++ third_party/blink/web_tests/external/wpt/WebCryptoAPI/cryptokey_algorithm_returns_cached_object.https.any.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// META: title=WebCryptoAPI: CryptoKey.algorithm getter returns cached object
+
+// https://w3c.github.io/webcrypto/#dom-cryptokey-algorithm
+// https://github.com/servo/servo/issues/33908
+
+promise_test(function() {
+    return self.crypto.subtle.generateKey(
+        {
+          name: "AES-CTR",
+          length: 256,
+        },
+        true,
+        ["encrypt"],
+      ).then(
+        function(key) {
+          let a = key.algorithm;
+          let b = key.algorithm;
+          assert_true(a === b);
+        },
+        function(err) {
+            assert_unreached("generateKey threw an unexpected error: " + err.toString());
+        }
+    );
+}, "CryptoKey.algorithm getter returns cached object");
