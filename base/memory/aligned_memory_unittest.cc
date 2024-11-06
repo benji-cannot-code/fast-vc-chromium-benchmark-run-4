0f18a05494357b39d0428519db029330ffcbe1c2
danakj <danakj@chromium.org>
2024-11-06 20:54:09 +0000

    Provide aligned allocation methods that return HeapArray
    There are two methods. The basic one returns a HeapArray<T> that
    is allocated with a runtime alignment. The complex one returns a
    HeapArray<char> that is allocated with a runtime alignment, along
    with a span<T> for accessing the buffer as T*. This is used for
    code that wants to control the lifetime of objects in the buffer
    with in-place construction.
    
    An AlignedHeapArray<T> alias is provided to avoid having to write
    out the aligned deleter in the type name.
    
    R=tsepez@chromium.org
    
    Bug: 40284755
    Change-Id: Ibe03c817732bc04976a1ce15868d61a2751275c1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994775
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379225}

--- base/memory/aligned_memory_unittest.cc
+++ base/memory/aligned_memory_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/aligned_memory.h"
 
+#include <stdint.h>
 #include <string.h>
 
 #include <memory>
@@ -13,6 +14,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
+
+TEST(AlignedMemoryTest, AlignedUninit) {
+  {
+    base::AlignedHeapArray<char> h = AlignedUninit<char>(8, 32);
+    EXPECT_EQ(h.size(), 8u);
+    EXPECT_TRUE(IsAligned(h.data(), 32));
+  }
+  {
+    base::AlignedHeapArray<int16_t> h = AlignedUninit<int16_t>(8, 32);
+    EXPECT_EQ(h.size(), 8u);
+    EXPECT_TRUE(IsAligned(h.data(), 32));
+  }
+}
+
+TEST(AlignedMemoryTest, AlignedUninitCharArray) {
+  auto [h, s] = AlignedUninitCharArray<int16_t>(8, 32);
+  static_assert(std::same_as<base::AlignedHeapArray<char>, decltype(h)>);
+  static_assert(std::same_as<base::span<int16_t>, decltype(s)>);
+  EXPECT_EQ(h.size(), 8u * sizeof(int16_t));
+  EXPECT_TRUE(IsAligned(h.data(), 32));
+  EXPECT_EQ(s.size(), 8u);
+  EXPECT_TRUE(IsAligned(s.data(), 32));
+}
 
 TEST(AlignedMemoryTest, DynamicAllocation) {
   void* p = AlignedAlloc(8, 8);
