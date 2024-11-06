a38f6bdb9a0ab7bf3262ab18a52ae41f76c2ab0b
Sammie Quon <sammiequon@chromium.org>
2024-11-06 19:04:00 +0000

    full restore: Remove factory test file checker
    It was a speculative fix that did not work, so remove the async
    threading logic. We will explore different approaches.
    
    Test: none
    Bug: 366607503
    Change-Id: If4519955a54965a1da129271a2f5f41930bb8e02
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994453
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379153}

--- chrome/browser/ash/app_restore/full_restore_service.cc
+++ chrome/browser/ash/app_restore/full_restore_service.cc
@@ -29,11 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_restore/window_restore_util.h"
 #include "base/barrier_callback.h"
 #include "base/command_line.h"
-#include "base/files/file_util.h"
 #include "base/metrics/histogram_functions.h"
 #include "base/ranges/algorithm.h"
 #include "base/strings/string_util.h"
-#include "base/task/thread_pool.h"
 #include "base/trace_event/trace_event.h"
 #include "base/version.h"
 #include "base/version_info/version_info.h"
@@ -154,10 +152,6 @@ CollectRestoreIDsForNormalBrowserWindows(
     }
   }
   return app_restore_ids;
-}
-
-bool IsFactoryTestRunningMayBlock() {
-  return base::PathExists(base::FilePath("/usr/local/factory/enabled"));
 }
 
 }  // namespace
@@ -1007,22 +1001,10 @@ void FullRestoreService::MaybeShowInformedRestoreOnboarding(bool restore_on) {
     return;
   }
 
-  base::ThreadPool::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::MayBlock()},
-      base::BindOnce(&IsFactoryTestRunningMayBlock),
-      base::BindOnce(&FullRestoreService::OnShouldShowInformedRestoreOnboarding,
-                     weak_ptr_factory_.GetWeakPtr(), restore_on));
-}
-
-void FullRestoreService::OnShouldShowInformedRestoreOnboarding(
-    bool restore_on,
-    bool factory_test_running) {
-  if (!factory_test_running) {
-    CHECK(Shell::Get()->informed_restore_controller());
-    Shell::Get()
-        ->informed_restore_controller()
-        ->MaybeShowInformedRestoreOnboarding(restore_on);
-  }
+  auto* informed_restore_controller =
+      Shell::Get()->informed_restore_controller();
+  CHECK(informed_restore_controller);
+  informed_restore_controller->MaybeShowInformedRestoreOnboarding(restore_on);
 }
 
 ScopedRestoreForTesting::ScopedRestoreForTesting() {
