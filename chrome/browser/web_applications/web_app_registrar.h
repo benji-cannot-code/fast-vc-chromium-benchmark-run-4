a43a7efe35029786245b26e19918e129944034f7
Daniel Murphy <dmurph@chromium.org>
2024-11-06 05:35:00 +0000

    [PWA/NavCapture] Add flag to force the default setting for preinstalled
    This change allows us to force the default setting of preinstalled
    (only) apps to be 'off by default', as a hedge against potential issues
    during launch.
    
    Bug: b:376936377
    Change-Id: Ib79a365a1397231db2a9f40cc123d7ee84f639e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994731
    Reviewed-by: Marijn Kruisselbrink <mek@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378797}

--- chrome/browser/web_applications/web_app_registrar.h
+++ chrome/browser/web_applications/web_app_registrar.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/check_op.h"
 #include "base/containers/flat_map.h"
 #include "base/containers/flat_set.h"
+#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
@@ -72,6 +73,11 @@ using DiyAppCount = base::StrongAlias<class DiyAppCountTag, int>;
 using InstallableAppCount =
     base::StrongAlias<class InstallableAppCountTag, int>;
 using NonSyncingAppCount = base::StrongAlias<class NonSyncingAppCountTag, int>;
+
+// Enabling this will force all apps that are exclusively preinstalled and open
+// in a browser tab to have the default link capturing setting be 'off'. This is
+// a safety switch in case something goes wrong with link capturing launch.
+BASE_DECLARE_FEATURE(kPreinstalledBrowserTabWebAppsForcedDefaultCaptureOff);
 
 // A registry model. This is a read-only container, which owns WebApp objects.
 class WebAppRegistrar {
