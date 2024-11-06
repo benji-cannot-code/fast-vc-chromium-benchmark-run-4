e746f0cad2c4901e15f07b959ce45fe3c6beb7c2
Simon Hangl <simonha@google.com>
2024-11-06 11:57:27 +0000

    Remove Lacros include from media stream manager
    Change-Id: I58328008b95ccceb778b9e5c5dd12ff3161f3513
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999530
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Elad Alon <eladalon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378925}

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -102,10 +102,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/capture/video/chromeos/video_capture_device_factory_chromeos.h"
 #endif
 
-#if BUILDFLAG(IS_CHROMEOS_LACROS)
-#include "chromeos/lacros/lacros_service.h"
-#endif
-
 #if !BUILDFLAG(IS_ANDROID)
 #include "content/browser/media/captured_surface_controller.h"
 #endif
