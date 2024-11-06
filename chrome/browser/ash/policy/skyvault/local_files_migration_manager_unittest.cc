7c40adcf7591b83ead155dad7830a311de020712
Aida Zolic <aidazolic@chromium.org>
2024-11-06 15:01:07 +0000

    [skyvault] Check migration misconfigured on init
    Also checks if migration is misconfigured a.k.a. the respective cloud
    provider isn't allowed by policy, when initializing the migration
    manager.
    
    R=poromov@chromium.org
    
    Change-Id: I0e74b3fbe3190c7f5f2f37ee4692af0709506013
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5987795
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Aida Zolic <aidazolic@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378997}

--- chrome/browser/ash/policy/skyvault/local_files_migration_manager_unittest.cc
+++ chrome/browser/ash/policy/skyvault/local_files_migration_manager_unittest.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/ash/components/system/statistics_provider.h"
 #include "components/user_manager/scoped_user_manager.h"
 #include "content/public/test/browser_task_environment.h"
+#include "profile.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -77,6 +78,9 @@ class LocalFilesMigrationManagerTest : public testing::Test {
     ash::system::StatisticsProvider::SetTestProvider(&statistics_provider_);
 
     ash::UserDataAuthClient::OverrideGlobalInstanceForTesting(&userdataauth_);
+
+    drive::DriveIntegrationServiceFactory::GetForProfile(profile())->SetEnabled(
+        true);
   }
 
   void TearDown() override {
