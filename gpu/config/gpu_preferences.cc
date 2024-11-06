3c8515eb8055bf2ad8210e781bee073bff2da9a0
Minoru Chikamune <chikamune@chromium.org>
2024-11-06 14:00:11 +0000

    Revert MiracleParameter code for GpuDiskCacheSize
    This CL reverts the following changes because this MiracleParameter
    experiment didn't show any improvements.
    
    https://chromium-review.googlesource.com/c/chromium/src/+/4840758
    https://chromium-review.googlesource.com/c/chromium/src/+/4896068
    
    Bug: 40279826
    Change-Id: I5c5d1269a35cda4132942d2c6da46e7af9e205ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5997951
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Minoru Chikamune <chikamune@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378958}

--- gpu/config/gpu_preferences.cc
+++ gpu/config/gpu_preferences.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/feature_list.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/system/sys_info.h"
-#include "components/miracle_parameter/common/public/miracle_parameter.h"
 #include "gpu/config/gpu_finch_features.h"
 #include "gpu/config/gpu_switches.h"
 #include "gpu/ipc/common/gpu_preferences.mojom.h"
@@ -37,22 +36,6 @@ size_t GetCustomGpuCacheSizeBytesIfExists(std::string_view switch_string) {
 }
 #endif
 
-BASE_FEATURE(kDefaultGpuDiskCacheSize,
-             "DefaultGpuDiskCacheSize",
-             base::FEATURE_ENABLED_BY_DEFAULT);
-
-MIRACLE_PARAMETER_FOR_INT(GetGpuDefaultMaxProgramCacheMemoryBytes,
-                          kDefaultGpuDiskCacheSize,
-                          "GpuDefaultMaxProgramCacheMemoryBytes",
-                          kDefaultMaxProgramCacheMemoryBytes)
-
-#if BUILDFLAG(IS_ANDROID)
-MIRACLE_PARAMETER_FOR_INT(GetGpuLowEndMaxProgramCacheMemoryBytes,
-                          kDefaultGpuDiskCacheSize,
-                          "GpuLowEndMaxProgramCacheMemoryBytes",
-                          kLowEndMaxProgramCacheMemoryBytes)
-#endif
-
 }  // namespace
 
 size_t GetDefaultGpuDiskCacheSize() {
@@ -63,12 +46,12 @@ size_t GetDefaultGpuDiskCacheSize() {
   if (custom_cache_size) {
     cache_size = custom_cache_size;
   } else {
-    cache_size = GetGpuDefaultMaxProgramCacheMemoryBytes();
+    cache_size = kDefaultMaxProgramCacheMemoryBytes;
   }
 #else   // !BUILDFLAG(IS_ANDROID)
   cache_size = base::SysInfo::IsLowEndDevice()
-                   ? GetGpuLowEndMaxProgramCacheMemoryBytes()
-                   : GetGpuDefaultMaxProgramCacheMemoryBytes();
+                   ? kLowEndMaxProgramCacheMemoryBytes
+                   : kDefaultMaxProgramCacheMemoryBytes;
 #endif  // !BUILDFLAG(IS_ANDROID)
 
   if (base::FeatureList::IsEnabled(::features::kAggressiveShaderCacheLimits)) {
