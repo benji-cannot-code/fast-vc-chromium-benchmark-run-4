89b4ca897fc7ba2e42ff365a22ea199ff84cb377
Jay Zhou <zhouzj@google.com>
2024-11-06 17:52:57 +0000

    Add scheduler to DwaService and log rotation
    Add MetricsRotationScheduler to DwaService to rotate logs. Logs
    are built from populated DWA protos and stored into the log store
    during log rotation. High level population of DWA proto is also
    added in this CL.
    
    Bug: 369384758
    Change-Id: Id2f48ae9721f1c73243829dec25aa4c968f0da3c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5890104
    Reviewed-by: Zainab Rizvi <rizvis@google.com>
    Commit-Queue: Jay Zhou <zhouzj@google.com>
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379098}

--- components/metrics/dwa/dwa_pref_names.cc
+++ components/metrics/dwa/dwa_pref_names.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace metrics::dwa::prefs {
 
-// Preference which stores serialized DWA logs to be uploaded.
 const char kUnsentLogStoreName[] = "dwa.persistent_logs";
 
 const char kDwaClientId[] = "dwa.client_id";
--- components/metrics/dwa/dwa_pref_names.h
+++ components/metrics/dwa/dwa_pref_names.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace metrics::dwa::prefs {
 
 // Preference which stores serialized DWA logs to be uploaded.
+COMPONENT_EXPORT(DWA)
 extern const char kUnsentLogStoreName[];
 
 // Client ID which changes daily. Note that this is a local state pref which is
