1fa02b1a734e52dca7d82072c9ffda121d645930
Joey Arhar <jarhar@chromium.org>
2024-11-06 20:10:59 +0000

    Remove LabelEventHandlerCallSuper flag
    This flag has been enabled by default for long enough so it is safe to
    remove.
    
    Change-Id: Iba6849bcd32f42dd54124f2013711f61cdd7f161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000615
    Commit-Queue: Joey Arhar <jarhar@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379201}

--- third_party/blink/renderer/core/html/forms/html_label_element.h
+++ third_party/blink/renderer/core/html/forms/html_label_element.h
@@ -55,7 +55,7 @@ class CORE_EXPORT HTMLLabelElement final : public HTMLElement {
 
   // Overridden to either click() or focus() the corresponding control.
   void DefaultEventHandler(Event&) override;
-  bool DefaultEventHandlerInternal(Event&);
+  void DefaultEventHandlerInternal(Event&);
   bool HasActivationBehavior() const override;
 
   void Focus(const FocusParams&) override;
