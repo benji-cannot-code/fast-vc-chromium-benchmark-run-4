71acafe85c942e90ca0025b55ad6e157ae714694
Benjamin Williams <bwwilliams@google.com>
2024-11-06 15:14:37 +0000

    [iOS] Improve UI for Save Passwords and Use Autofill Magic Stack Tips
    This update introduces two UI improvements for the Tips (Magic Stack)
    project:
    
    - The instructional overlays for Save Passwords & Use Autofill now
      occupy half of the screen in portrait mode and the entire screen in
      landscape mode:
      Before:
    https://drive.google.com/file/d/1kxHIzofv8OpxeXjYFgWJ9OtNkjGNgvSB/view?usp=drive_link&resourcekey=0-QAABuxFu-oFFR1lKQSfL3A
      After:
    https://drive.google.com/file/d/1_PdYiF5tishg0f80yKX93BuIJ2xljuUx/view?usp=drive_link&resourcekey=0-Uf8UYJ0cQhjjgrQDYgcR_Q
    
    - The spacing between the image, title, and subtitle has been updated:
      Before:
    https://drive.google.com/file/d/13c7DtEsIQW2FIXE_w2fSaZ93ovPsxof-/view?usp=drive_link&resourcekey=0-mad6IsEIPEOXsiKvOiH1DA
      After:
    https://drive.google.com/file/d/1Is90oYib98OoXjHvjo7Qn08sNk3ssnpm/view?usp=drive_link&resourcekey=0-g9s53iDOLQskT5_cc0rspw
    
    Change-Id: I94c054691c67cc7a85d165eb0b0e891456f1f85a
    Bug: 376749707
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983039
    Owners-Override: Benjamin Williams <bwwilliams@google.com>
    Commit-Queue: Benjamin Williams <bwwilliams@google.com>
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379005}

--- ios/chrome/browser/docking_promo/ui/animated_promo_view_controller.mm
+++ ios/chrome/browser/docking_promo/ui/animated_promo_view_controller.mm
@@ -13,9 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/public/provider/chrome/browser/lottie/lottie_animation_configuration.h"
 
 namespace {
-constexpr CGFloat kCustomSpacingAtTopIfNoNavigationBar = 24;
+constexpr CGFloat kCustomSpacingAtTopIfNoNavigationBar = 32;
 constexpr CGFloat kCustomSpacingAfterImageWithoutAnimation = 0;
-constexpr CGFloat kCustomSpacing = 20;
+constexpr CGFloat kCustomSpacing = 8;
 }  // namespace
 
 @interface AnimatedPromoViewController ()
@@ -229,8 +229,11 @@ constexpr CGFloat kCustomSpacing = 20;
   _animationViewWrapper.animationView.hidden = hidden || darkModeEnabled;
   _animationViewWrapperDarkMode.animationView.hidden =
       hidden || !darkModeEnabled;
+
   if (_animationBackgroundColor) {
     _animationViewWrapper.animationView.backgroundColor =
+        _animationBackgroundColor;
+    _animationViewWrapperDarkMode.animationView.backgroundColor =
         _animationBackgroundColor;
   }
 
