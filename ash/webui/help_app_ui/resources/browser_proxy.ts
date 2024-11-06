bca576c9b69a2e991f94f8c34515e946881ce318
Morten Stenshorne <mstensho@chromium.org>
2024-11-06 09:58:53 +0000

    [CodeHealth] Remove stale base::Feature EnableLocalSearchService
    Enabled by default since M89.
    
    Bug: 356234181
    Change-Id: I612166b08a42302e13579b5c07fe5e7706d14bc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983187
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Thanh Nguyen <thanhdng@chromium.org>
    Reviewed-by: Zufeng Wang <zufeng@google.com>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378893}

--- ash/webui/help_app_ui/resources/browser_proxy.ts
+++ ash/webui/help_app_ui/resources/browser_proxy.ts
@@ -39,10 +39,6 @@ const guestFrame = document.createElement('iframe');
 guestFrame.src = `${GUEST_ORIGIN}${location.pathname}${location.search}`;
 document.body.appendChild(guestFrame);
 
-// Cached result whether Local Search Service is enabled.
-const isLssEnabled =
-    helpApp.handler.isLssEnabled().then(result => result.enabled);
-
 // Cached result of whether Launcher Search is enabled.
 const isLauncherSearchEnabled =
     helpApp.handler.isLauncherSearchEnabled().then(result => result.enabled);
@@ -95,9 +91,6 @@ guestMessagePipe.registerHandler(
 
 guestMessagePipe.registerHandler(
     Message.ADD_OR_UPDATE_SEARCH_INDEX, async (data: SearchableItem[]) => {
-      if (!(await isLssEnabled)) {
-        return;
-      }
       const dataToSend = data.map(searchableItem => {
         const contents: Content[] = [
           {
@@ -150,18 +143,12 @@ guestMessagePipe.registerHandler(
     });
 
 guestMessagePipe.registerHandler(Message.CLEAR_SEARCH_INDEX, async () => {
-  if (!(await isLssEnabled)) {
-    return;
-  }
   return indexRemote.clearIndex();
 });
 
 guestMessagePipe.registerHandler(
   Message.FIND_IN_SEARCH_INDEX,
   async (dataFromApp: {query: string, maxResults: number}) => {
-      if (!(await isLssEnabled)) {
-        return {results: null};
-      }
       const response = await indexRemote.find(
           toTruncatedString16(dataFromApp.query), dataFromApp.maxResults || 50);
 
