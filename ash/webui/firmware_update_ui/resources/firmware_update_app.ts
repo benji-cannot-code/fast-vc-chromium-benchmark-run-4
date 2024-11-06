1779991a9f6594bdcc43d795d5f34bd0f3266d2c
Andre Braga <andrebraga@google.com>
2024-11-06 16:24:37 +0000

    firmware_update: change header if on Flex device with flag enabled
    This CL is part of a small chain that will implement some remaining UI
    changes for go/reven-firmware-update-dd, outlined in [1].  While fwupd
    normally only updates external devices on CrOS, Flex devices with the
    firmware updates flag enabled can also update their internal
    firmware. So, we change the header to reflect that.
    
    I've also added a way to check for the Flex firmware update flag, which
    is used in this and later CLs.
    
    [1]: http://slides/1v_n0JTRUQ0g2P3kEQtjW9uobKlGKz-ezkZBTzkahcBg#slide=id.p
    
    Test: browser_tests --gtest_filter=FirmwareUpdateAppBrowserTest.FirmwareUpdateApp
    Bug: b:374145678
    
    Change-Id: I6536f453d755ade79c669d9e0a16f531badfebd8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5967935
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Commit-Queue: Andre Braga <andrebraga@google.com>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379039}

--- ash/webui/firmware_update_ui/resources/firmware_update_app.ts
+++ ash/webui/firmware_update_ui/resources/firmware_update_app.ts
@@ -15,6 +15,7 @@ import {ColorChangeUpdater} from 'chrome://resources/cr_components/color_change_
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {getTemplate} from './firmware_update_app.html.js';
+import {IsFlexFirmwareUpdateEnabled} from './firmware_update_utils.js';
 
 /**
  * @fileoverview
@@ -40,6 +41,13 @@ export class FirmwareUpdateAppElement extends FirmwareUpdateAppElementBase {
     (function() {
       ColorChangeUpdater.forDocument().start();
     })();
+  }
+
+  protected computeHeaderText(): string {
+    if (IsFlexFirmwareUpdateEnabled()) {
+      return this.i18n('appTitleOnFlex');
+    }
+    return this.i18n('appTitle');
   }
 }
 
