0f3a53b45d8a3bab8e1a70a15224e5a47fd77bc3
Jihad Hanna <jihadghanna@google.com>
2024-11-06 18:16:22 +0000

    Fix test setups of AutofillDriver::ApplyFormAction
    In production, the returned IDs by AutofillDriver::ApplyFormAction is a
    subset of the fields that are passed to it.
    
    In tests, this was false and inconsistent. This CL fixes that.
    
    Bug: 40227071
    Change-Id: Id3c8d0304c1bb7f28c92f31b3a1d4946b80f6970
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999736
    Commit-Queue: Jihad Hanna <jihadghanna@google.com>
    Reviewed-by: Christoph Schwering <schwering@google.com>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1379109}

--- components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
+++ components/autofill/core/browser/metrics/autofill_metrics_unittest.cc
@@ -5486,7 +5486,7 @@ TEST_F(AutofillMetricsSeamlessnessTest,
   EXPECT_THAT(SamplesOf({kFillable, kBefore, kAll, kBitmask}),
               BucketsAre(Bucket(kName | kNumber | kExp | kCvc, 2)));
   EXPECT_THAT(SamplesOf({kFillable, kAfter, kAll, kBitmask}),
-              BucketsAre(Bucket(kName | kExp, 1), Bucket(kNumber | kCvc, 1)));
+              BucketsAre(Bucket(kName | kExp, 1), Bucket(kNumber, 1)));
   EXPECT_THAT(
       SamplesOf({kFills, kBefore, kAll, kBitmask}),
       BucketsAre(Bucket(kName | kNumber | kExp, 1), Bucket(kNumber, 1)));
@@ -5575,7 +5575,7 @@ TEST_F(AutofillMetricsSeamlessnessTest,
 
            {UkmBuilder::kFillable_BeforeSecurity_BitmaskName,
             kName | kNumber | kExp | kCvc},
-           {UkmBuilder::kFillable_AfterSecurity_BitmaskName, kNumber | kCvc},
+           {UkmBuilder::kFillable_AfterSecurity_BitmaskName, kNumber},
            {UkmBuilder::kFilled_BeforeSecurity_BitmaskName, kNumber},
            {UkmBuilder::kFilled_AfterSecurity_BitmaskName, kNumber},
 
