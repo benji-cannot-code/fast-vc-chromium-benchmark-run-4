39de9c3587c6204308a2ed90c452ebc8df275cec
vincb <vincb@google.com>
2024-11-06 19:17:38 +0000

    [ios][pbsV2] Field trial config for payments sheet V2
    Change-Id: I0f378eb582ca13f87120f19683650c54d4307f6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994750
    Reviewed-by: Tommy Martino <tmartino@chromium.org>
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379160}

--- ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
+++ ios/chrome/browser/autofill/ui_bundled/bottom_sheet/payments_suggestion_bottom_sheet_egtest.mm
@@ -10,10 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/test/ios/wait_util.h"
 #import "components/autofill/core/browser/autofill_test_utils.h"
 #import "components/autofill/core/common/autofill_payments_features.h"
+#import "components/autofill/ios/common/features.h"
 #import "components/url_formatter/elide_url.h"
+#import "ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h"
 #import "ios/chrome/browser/metrics/model/metrics_app_interface.h"
 #import "ios/chrome/browser/shared/public/features/features.h"
-#import "ios/chrome/browser/autofill/ui_bundled/autofill_app_interface.h"
 #import "ios/chrome/browser/ui/settings/settings_root_table_constants.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_actions.h"
@@ -75,6 +76,17 @@ const char kFormCardExpirationYear[] = "CCExpiresYear";
   GREYAssertNil([MetricsAppInterface releaseHistogramTester],
                 @"Failed to release histogram tester.");
   [super tearDownHelper];
+}
+
+- (AppLaunchConfiguration)appConfigurationForTestCase {
+  AppLaunchConfiguration config;
+  if ([self isRunningTest:@selector
+            (testOpenPaymentsBottomSheetShowDetailsEditNickname)]) {
+    // Disable V2 for that test case as it doesn't support the flow tested by
+    // that test case.
+    config.features_disabled.push_back(kAutofillPaymentsSheetV2Ios);
+  }
+  return config;
 }
 
 // Matcher for the bottom sheet's "Continue" button.
@@ -479,6 +491,49 @@ void CheckAutofillSuggestionAcceptedIndexMetricsCount(
       [nickname stringByAppendingString:[_lastDigits substringFromIndex:4]];
   id<GREYMatcher> nicknamedCreditCard = grey_text(nicknameAndCardNumber);
   [ChromeEarlGrey waitForUIElementToAppearWithMatcher:nicknamedCreditCard];
+}
+
+// Verify that the Payments Bottom Sheet "Show Details" button opens the proper
+// menu and allows the nickname to be edited. For any version of the sheet.
+- (void)testOpenPaymentsBottomSheetShowDetailsEditNicknameOnAnyVersion {
+  [self loadPaymentsPage];
+
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::WebViewMatcher()]
+      performAction:chrome_test_util::TapWebElementWithId(kFormCardName)];
+
+  WaitOnResponsiveContinueButton();
+
+  // Long press to open context menu.
+  id<GREYMatcher> creditCardEntry = grey_text(_lastDigits);
+
+  [[EarlGrey selectElementWithMatcher:creditCardEntry]
+      performAction:grey_longPress()];
+
+  [ChromeEarlGreyUI waitForAppToIdle];
+
+  [[EarlGrey
+      selectElementWithMatcher:
+          grey_allOf(chrome_test_util::ContextMenuItemWithAccessibilityLabelId(
+                         IDS_IOS_PAYMENT_BOTTOM_SHEET_SHOW_DETAILS),
+                     grey_interactable(), nullptr)] performAction:grey_tap()];
+
+  [ChromeEarlGreyUI waitForAppToIdle];
+
+  // Edit the card's nickname.
+  [[EarlGrey selectElementWithMatcher:SettingToolbarEditButton()]
+      performAction:grey_tap()];
+
+  NSString* nickname = @"Card Nickname";
+  [[EarlGrey selectElementWithMatcher:NicknameTextField()]
+      performAction:grey_replaceText(nickname)];
+
+  [[EarlGrey selectElementWithMatcher:SettingToolbarDoneButton()]
+      performAction:grey_tap()];
+
+  // Close the context menu.
+  [[EarlGrey
+      selectElementWithMatcher:chrome_test_util::NavigationBarDoneButton()]
+      performAction:grey_tap()];
 }
 
 // Verify that the Payments Bottom Sheet "Manage Payments Methods" button opens
