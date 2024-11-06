001dcf5c686597cb7057e62098cb6195e503f365
Chuong Ho <hdchuong@google.com>
2024-11-06 06:26:35 +0000

    lobster: pass lobster availability into editor menu card
    This CL passes the lobster availability from lobster service into editor menu card to decide when to show lobster artifact in the menu card.
    
    Bug: b:348280474
    
    Change-Id: Ie4198714ce71f31b0523f061ab0bccc587ca4720
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5900444
    Commit-Queue: Chuong Ho <hdchuong@chromium.org>
    Reviewed-by: Curtis McMullan <curtismcmullan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378814}

---
+++ chrome/browser/ui/ash/editor_menu/lobster_manager.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/ash/editor_menu/lobster_manager.h"
+
+#include "ash/lobster/lobster_controller.h"
+#include "base/check.h"
+
+namespace chromeos::editor_menu {
+
+LobsterManager::LobsterManager(
+    std::unique_ptr<ash::LobsterController::Trigger> trigger)
+    : trigger_(std::move(trigger)) {
+  CHECK(trigger_);
+}
+
+LobsterManager::~LobsterManager() = default;
+
+void LobsterManager::StartFlowWithFreeformText(
+    const std::string& freeform_text) {
+  trigger_->Fire(freeform_text);
+}
+
+}  // namespace chromeos::editor_menu
--- chrome/browser/ui/ash/read_write_cards/read_write_cards_manager_impl.cc
+++ chrome/browser/ui/ash/read_write_cards/read_write_cards_manager_impl.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/constants/ash_features.h"
 #include "base/command_line.h"
 #include "base/functional/callback.h"
 #include "base/hash/sha1.h"
@@ -42,7 +43,8 @@ ReadWriteCardsManagerImpl::ReadWriteCardsManagerImpl()
           g_browser_process->shared_url_loader_factory(),
           quick_answers_controller_->GetQuickAnswersDelegate()));
 
-  if (chromeos::features::IsOrcaEnabled()) {
+  if (chromeos::features::IsOrcaEnabled() ||
+      ash::features::IsLobsterEnabled()) {
     editor_menu_controller_ =
         std::make_unique<editor_menu::EditorMenuControllerImpl>();
   }
