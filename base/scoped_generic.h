89a512b39eaff7da75b70834af5b44f990a55b16
Tom Sepez <tsepez@chromium.org>
2024-11-06 23:09:18 +0000

    Avoid a copy in base::ScopedGeneric<>::release(), if possible.
    Try to move() the old generic rather than copying it. If the generic is
    not movable, this will silently fall-back to a copy.
    
    Change-Id: Ie0be6a8aaf967b65a60f1337659ece200c1f3823
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994452
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379329}

--- base/scoped_generic.h
+++ base/scoped_generic.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <concepts>
 #include <type_traits>
+#include <utility>
 
 #include "base/check.h"
 #include "base/compiler_specific.h"
@@ -149,8 +150,8 @@ class ScopedGeneric {
   // object. After this operation, this object will hold a null value, and
   // will not own the object any more.
   [[nodiscard]] element_type release() {
-    element_type old_generic = data_.generic;
-    data_.generic = traits_type::InvalidValue();
+    element_type old_generic =
+        std::exchange(data_.generic, traits_type::InvalidValue());
     TrackRelease(old_generic);
     return old_generic;
   }
