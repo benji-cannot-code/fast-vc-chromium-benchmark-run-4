24f334e95a36bcd56d10dbdcfaa55e8a968670eb
Daniil Sakhapov <sakhapov@chromium.org>
2024-11-06 08:54:41 +0000

    [carousel] Check for ::scroll-marker-group style in scroller
    To determine if a scroller needs ::scroll-marker-group, check if
    it has ::scroll-marker-group style in addition to checking
    scroll-marker-group property.
    
    Bug: 332396355
    Change-Id: I2c569f1d97cc5e7ca95a1fa8f7354fb7610faca3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5994432
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1378865}

--- third_party/blink/renderer/core/style/computed_style.h
+++ third_party/blink/renderer/core/style/computed_style.h
@@ -2288,11 +2288,13 @@ class ComputedStyle final : public ComputedStyleBase {
     }
     if (pseudo == kPseudoIdScrollMarkerGroupBefore) {
       return ScrollMarkerGroup() == EScrollMarkerGroup::kBefore &&
-             IsScrollContainer();
+             IsScrollContainer() &&
+             HasPseudoElementStyle(kPseudoIdScrollMarkerGroup);
     }
     if (pseudo == kPseudoIdScrollMarkerGroupAfter) {
       return ScrollMarkerGroup() == EScrollMarkerGroup::kAfter &&
-             IsScrollContainer();
+             IsScrollContainer() &&
+             HasPseudoElementStyle(kPseudoIdScrollMarkerGroup);
     }
     if (!HasPseudoElementStyle(pseudo)) {
       return false;
