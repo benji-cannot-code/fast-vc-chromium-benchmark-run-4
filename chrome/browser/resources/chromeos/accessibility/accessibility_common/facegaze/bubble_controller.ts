47e84ae3ab13aec0ed3dbb46cb0e5d7af7975a74
Keishi Hattori <keishi@chromium.org>
2024-11-06 14:17:47 +0000

    Revert "[FaceGaze] Persist strings for held macros in bubble UI"
    This reverts commit d598953e4edcfda9cebb26262d6c74666c66d6ba.
    
    Reason for revert: Caused failure.
    https://ci.chromium.org/ui/p/chromium/builders/ci/linux-presubmit/20867/overview
    
    Original change's description:
    > [FaceGaze] Persist strings for held macros in bubble UI
    >
    > * Persist strings for macros that are held.
    > * Due to complexities with tracking state, if new actions are triggered
    > while the macro is held, the strings for the new actions will populate
    > the bubble UI immediately, but not include the held macros. When the
    > bubble UI resets after a timeout, the held macros will populate the
    > bubble UI again at that time.
    > * Add tests.
    >
    > Fixed: 375235384
    > Change-Id: I22dcd68229dc1da033668051ac615ff09f378f0f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985158
    > Reviewed-by: Akihiro Ota <akihiroota@chromium.org>
    > Commit-Queue: Amanda Lin Dietz <aldietz@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1378443}
    
    Change-Id: Ic16118875d967b8e25d54ef83e6ae98d4dcd9579
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5998051
    Commit-Queue: Ravjit Uppal <ravjit@chromium.org>
    Owners-Override: Keishi Hattori <keishi@chromium.org>
    Auto-Submit: Keishi Hattori <keishi@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1378971}

--- chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/bubble_controller.ts
+++ chrome/browser/resources/chromeos/accessibility/accessibility_common/facegaze/bubble_controller.ts
@@ -45,13 +45,7 @@ export class BubbleController {
       scrollMode,
       longClick,
       dictation,
-      heldActions,
     } = this.getStateGesture_();
-
-    if (heldActions) {
-      heldActions.forEach((displayText) => {this.baseText_.push(displayText)});
-    }
-
     if (paused) {
       this.baseText_.push(chrome.i18n.getMessage(
           'facegaze_state_paused',
@@ -248,7 +242,6 @@ export namespace BubbleController {
     scrollMode: FacialGesture|undefined;
     longClick: FacialGesture|undefined;
     dictation: FacialGesture|undefined;
-    heldActions: string[];
   }
 }
 
