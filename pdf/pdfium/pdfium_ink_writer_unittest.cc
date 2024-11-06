ca27019bd3a7621ae6571d7218609efbb5355adc
Lei Zhang <thestig@chromium.org>
2024-11-06 19:00:02 +0000

    [PDF Ink Signatures] Disable PDFiumInk{Read,Write}Tests under CFI
    These tests call into third_party/libtess2 code, and this third part
    code uses function pointers in a way that makes CFI unhappy. Disable the
    failing tests for now.
    
    Bug: 377704081
    Change-Id: I2ec7dbb5d4b2feb6a631a829120a9605b1821ab0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999363
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379150}

--- pdf/pdfium/pdfium_ink_writer_unittest.cc
+++ pdf/pdfium/pdfium_ink_writer_unittest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <vector>
 
+#include "base/cfi_buildflags.h"
 #include "base/files/file_path.h"
 #include "base/time/time.h"
 #include "pdf/pdf_ink_brush.h"
@@ -83,7 +84,13 @@ std::unique_ptr<PdfInkBrush> CreateTestBrush() {
 
 using PDFiumInkWriterTest = PDFiumTestBase;
 
-TEST_P(PDFiumInkWriterTest, BasicWriteAndRead) {
+// TODO(crbug.com/377704081): Enable test for CFI.
+#if BUILDFLAG(CFI_ICALL_CHECK)
+#define MAYBE_BasicWriteAndRead DISABLED_BasicWriteAndRead
+#else
+#define MAYBE_BasicWriteAndRead BasicWriteAndRead
+#endif
+TEST_P(PDFiumInkWriterTest, MAYBE_BasicWriteAndRead) {
   TestClient client;
   std::unique_ptr<PDFiumEngine> engine =
       InitializeEngine(&client, FILE_PATH_LITERAL("blank.pdf"));
