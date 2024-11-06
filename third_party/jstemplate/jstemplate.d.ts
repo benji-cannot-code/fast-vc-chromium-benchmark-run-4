f75f3036b660552fdc34318800ce6ee54b8467d2
dpapad <dpapad@chromium.org>
2024-11-06 19:22:40 +0000

    WebUI: Infer TS path_mappings entry from deps for third_party/jstemplate
    This fixes an issue with siso remote execution by ensuring that the file
    needed to properly resolve jstemplate_compiled.js imports is declared as
    a GN input.
    
    Effectively the fix allows correctly resolving the following
    side-effect import
    
    import 'chrome://resources/jstemplate/jstemplate_compiled.js';
    
    as long as the //third_party/jstemplate:library target is declared as a
    dependency.
    
    TBR=thakis@chromium.org
    
    Fixed: 377617674
    Change-Id: Ice49382571a58220456828a38e4a8574a6a3a021
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001253
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379166}

--- third_party/jstemplate/jstemplate.d.ts
+++ third_party/jstemplate/jstemplate.d.ts
@@ -6,5 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 declare class JsEvalContext {
   constructor(data: any);
 }
+
 declare function jstProcess(
     context: JsEvalContext, template: HTMLElement): void;
+
+declare function jstGetTemplate(templateName: string): HTMLElement;
