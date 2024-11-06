ecd77f5e678442ddc00a7b3613eebb6c5007de47
manukh <manukh@chromium.org>
2024-11-06 21:25:44 +0000

    [omnibox][history-embeddings] Hide loading answers after `Stop()`
    The history embeddings answers are allowed to ignore inactivity
    `Stop()`s because:
    1) Answers are expected to take longer to load than the 1.5s stop timer.
    2) Answers are sorted 2nd-to-last above the disclaimer IPH, and changes
       lower in the popup are less disruptive than changes higher up.
    3) Answers reserve their slot with a 'loading' placeholder, and
       replacing the 'loading' placeholder with an answer is less disruptive
       than showing a brand new match.
    
    Allowing this stop-exception is delicate. There are 3 `Stop()` scenarios
    we need handle:
    
    1) Inactivity `Stop()`
      a) User types '@history query'
      b) 'loading' answer is displayed
      c) 1.5s later the stop timeout triggers causing `Stop()` to be called
      d) when the real answer is ready, it replaces the 'loading' answer
    
    2) Popup closure `Stop()`
      a) User types '@history query'
      b) 'loading' answer is displayed
      c) user closes the omnibox causing `Stop()` to be called
      d) when the real answer is ready, it should be discarded and not
         re-open the closed popup
    
    3) Other `Stop()`
      a) User types '@history query'
      b) 'loading' answer is displayed
      c) user presses the down arrow causing `Stop()` to be called
      d) the 'loading' placeholder is removed
      e) when the real answer is ready, it shouldn't update the popup
         disrupting the user's selection
    
    With this CL, all 3 cases work correctly. But it's still a temporary
    solution; see crrev.com/c/5948144 & crrev.com/c/5980091 .
    
    Bug: 364303536, 376329281
    Change-Id: I8e1fc96858df999715e636d2f60805f5a3099c47
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5998150
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: manuk hovanesian <manukh@chromium.org>
    Commit-Queue: manuk hovanesian <manukh@chromium.org>
    Reviewed-by: Orin Jaworski <orinj@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379248}

--- components/omnibox/browser/history_embeddings_provider.cc
+++ components/omnibox/browser/history_embeddings_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <optional>
 #include <string>
+#include <vector>
 
 #include "base/check.h"
 #include "base/functional/bind.h"
@@ -103,8 +104,15 @@ void HistoryEmbeddingsProvider::Stop(bool clear_cached_results,
   //   controller that they expect a slow response and the controller to
   //   accommodate it by updating its stop, debounce, and cache timers'
   //   behaviors.
-  if (!due_to_user_inactivity)
+  if (!due_to_user_inactivity && !done_) {
     done_ = true;
+    size_t erased_count = std::erase_if(matches_, [&](const auto& match) {
+      return match.type == AutocompleteMatchType::HISTORY_EMBEDDINGS_ANSWER;
+    });
+    CHECK_LE(erased_count, 1u);
+    if (erased_count)
+      NotifyListeners(!matches_.empty());
+  }
 
   // TODO(b/333770460): Once `HistoryEmbeddingsService` has a stop API, we
   //   should call it here.
