adb63255b95870cdc2299f3580feb59b8fa4ee24
Paul Adedeji <pauladedeji@google.com>
2024-11-06 18:39:55 +0000

    [ntp][code-health] Re-enable ClickingEntryNavigatesToCorrectPage.
    Re-enables NewTabPageModulesInteractiveLinkUiTest.ClickingEntryNavigatesToCorrectPage as the bug causing it to fail was most likely addressed [1].
    
    Also:
    1. Removes the embedded test server, which isn't used by any of these
    tests.
    2. Removes const kGooglePageUrl, and writes out its value in
    the one place it is used, for clarity.
    
    [1]https://chromium-review.googlesource.com/c/chromium/src/+/5996210
    
    Change-Id: Id2907ba2b2cb769dd9c4d80b4123817b676d83f8
    Bug: 347914816
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5995690
    Commit-Queue: Paul Adedeji <pauladedeji@google.com>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1379131}

--- chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
+++ chrome/browser/new_tab_page/modules/new_tab_page_modules_interactive_uitest.cc
@@ -20,8 +20,6 @@ namespace {
 
 DEFINE_LOCAL_ELEMENT_IDENTIFIER_VALUE(kNewTabPageElementId);
 
-const char kGooglePageUrl[] = "https://www.google.com/";
-
 using DeepQuery = WebContentsInteractionTestUtil::DeepQuery;
 const DeepQuery kModulesV2Container = {"ntp-app", "ntp-modules-v2",
                                        "#container"};
@@ -63,7 +61,7 @@ ModuleDetails kMostRelevantTabResumptionModuleDetails = {
      "ntp-most-relevant-tab-resumption", "ntp-module-header-v2", "#disable"},
     {{{"ntp-app", "ntp-modules-v2", "ntp-module-wrapper",
        "ntp-most-relevant-tab-resumption", "#urlVisits", "a"},
-      kGooglePageUrl}},
+      "https://www.google.com"}},
 };
 
 ModuleDetails kGoogleCalendarModuleDetails = {
@@ -131,16 +129,6 @@ class NewTabPageModulesInteractiveUiBaseTest : public InteractiveBrowserTest {
     InteractiveBrowserTest::SetUpCommandLine(command_line);
     base::CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kSignedOutNtpModulesSwitch);
-  }
-
-  void SetUpOnMainThread() override {
-    InteractiveBrowserTest::SetUpOnMainThread();
-    embedded_test_server()->StartAcceptingConnections();
-  }
-
-  void TearDownOnMainThread() override {
-    EXPECT_TRUE(embedded_test_server()->ShutdownAndWaitUntilComplete());
-    InteractiveBrowserTest::TearDownOnMainThread();
   }
 
   InteractiveTestApi::MultiStep LoadNewTabPage() {
@@ -221,7 +209,6 @@ class NewTabPageModulesInteractiveUiTest
   void operator=(const NewTabPageModulesInteractiveUiTest&) = delete;
 
   void SetUp() override {
-    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
     features.InitWithFeaturesAndParameters(
         ModuleDetails().features,
         /*disabled_features=*/ntp::ComputeDisabledFeaturesList(
@@ -313,7 +300,6 @@ class NewTabPageModulesInteractiveLinkUiTest
   void operator=(const NewTabPageModulesInteractiveLinkUiTest&) = delete;
 
   void SetUp() override {
-    ASSERT_TRUE(embedded_test_server()->InitializeAndListen());
     features.InitWithFeaturesAndParameters(GetParam().first, {});
     InteractiveBrowserTest::SetUp();
   }
@@ -325,17 +311,8 @@ INSTANTIATE_TEST_SUITE_P(All,
                          NewTabPageModulesInteractiveLinkUiTest,
                          ::testing::ValuesIn(GetAllModuleLinks(kAllModules)));
 
-// TODO(crbug.com/347914816): Fix test failure on Mac.
-#if BUILDFLAG(IS_CHROMEOS_LACROS) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC)
-#define MAYBE_ClickingEntryNavigatesToCorrectPage \
-  DISABLED_ClickingEntryNavigatesToCorrectPage
-#else
-#define MAYBE_ClickingEntryNavigatesToCorrectPage \
-  ClickingEntryNavigatesToCorrectPage
-#endif
 IN_PROC_BROWSER_TEST_P(NewTabPageModulesInteractiveLinkUiTest,
-                       MAYBE_ClickingEntryNavigatesToCorrectPage) {
+                       ClickingEntryNavigatesToCorrectPage) {
   RunTestSequence(
       // 1. Wait for new tab page to load.
       LoadNewTabPage(),
