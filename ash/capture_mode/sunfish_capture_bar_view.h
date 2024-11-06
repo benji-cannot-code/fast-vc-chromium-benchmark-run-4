ff793e63ce8f3241fba41a9f7cd50894d51a2f45
Sophie Wen <sophiewen@google.com>
2024-11-06 21:42:09 +0000

    sunfish: Fix tab crash
    This CL fixes the crash when pressing tab on the sunfish capture bar.
    
    Test: added, verified it crashes w/o fix in Patchset 1
    Fixed: b:377579953, b:362311598
    Change-Id: Ic1ca6594e67d5438bfcc50314f26bd6144e6c9b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000595
    Commit-Queue: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Auto-Submit: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379266}

--- ash/capture_mode/sunfish_capture_bar_view.h
+++ ash/capture_mode/sunfish_capture_bar_view.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-// TODO(http://b/362311598): Fix how tabbing works when only the close button is
-// available.
 // A view that acts as the content view of the capture mode bar widget for a
 // sunfish capture session. It only contains the close button.
 class ASH_EXPORT SunfishCaptureBarView : public CaptureModeBarView {
