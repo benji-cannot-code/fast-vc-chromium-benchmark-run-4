444c488b098bed4b6f715bef035207d61f8f3912
Steinar H. Gunderson <sesse@chromium.org>
2024-11-07 11:54:16 +0000

    Add WebDX feature mapping for gradient-interpolation.
    Change-Id: Ibea5174df8003739e16a68ef9c7fad25ed575654
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000451
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379603}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -510,6 +510,8 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
            WebDXFeature::kNthChildOf},
           {WebFeature::kForcedColorsMediaFeature, WebDXFeature::kForcedColors},
           {WebFeature::kCSSTrigFunctions, WebDXFeature::kTrigFunctions},
+          {WebFeature::kCSSColorGradientColorSpace,
+           WebDXFeature::kGradientInterpolation},
       }};
 
   return *kMap;
