a006e0e2a6fb98d4234de81598c094146d8a7038
Daniel Rubery <drubery@chromium.org>
2024-11-07 23:01:53 +0000

    Add observer interface for device bound sessions
    In order to clear device bound sessions from the browser process, we
    need a way to notify the browser that a session was used. This is
    analogous to CookieAccessObserver or SharedDictionaryAccessDetails. This
    CL adds that Mojo interface and calls it from the SessionServiceImpl.
    
    Future CLs will plumb the interface from the browser process to the
    URLRequest.
    
    Bug: 353774427
    Change-Id: I6ea5f2c9bce4bedd8144f0ba82bf13308435d971
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5983816
    Reviewed-by: Anunoy Ghosh <anunoy@chromium.org>
    Reviewed-by: Mustafa Emre Acer <meacer@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Daniel Rubery <drubery@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379998}

---
+++ net/device_bound_sessions/session_key.h
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_DEVICE_BOUND_SESSIONS_SESSION_KEY_H_
+#define NET_DEVICE_BOUND_SESSIONS_SESSION_KEY_H_
+
+#include "net/base/schemeful_site.h"
+#include "net/device_bound_sessions/session.h"
+
+namespace net::device_bound_sessions {
+
+// Unique identifier for a `Session`.
+struct NET_EXPORT SessionKey {
+  SessionKey();
+  SessionKey(SchemefulSite site, Session::Id id);
+  ~SessionKey();
+
+  SessionKey(const SessionKey&);
+  SessionKey& operator=(const SessionKey&);
+
+  SessionKey(SessionKey&&);
+  SessionKey& operator=(SessionKey&&);
+
+  SchemefulSite site;
+  Session::Id id;
+};
+
+}  // namespace net::device_bound_sessions
+
+#endif  // NET_DEVICE_BOUND_SESSIONS_SESSION_KEY_H_
--- net/device_bound_sessions/session_service_impl.h
+++ net/device_bound_sessions/session_service_impl.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/device_bound_sessions/registration_fetcher.h"
 #include "net/device_bound_sessions/registration_fetcher_param.h"
 #include "net/device_bound_sessions/session.h"
+#include "net/device_bound_sessions/session_key.h"
 #include "net/device_bound_sessions/session_service.h"
 
 namespace net {
@@ -41,7 +42,8 @@ class NET_EXPORT SessionServiceImpl : public SessionService {
   // during construction. Otherwise, it is a no-op.
   void LoadSessionsAsync();
 
-  void RegisterBoundSession(RegistrationFetcherParam registration_params,
+  void RegisterBoundSession(OnAccessCallback on_access_callback,
+                            RegistrationFetcherParam registration_params,
                             const IsolationInfo& isolation_info) override;
 
   std::optional<Session::Id> GetAnySessionRequiringDeferral(
@@ -53,7 +55,8 @@ class NET_EXPORT SessionServiceImpl : public SessionService {
       RefreshCompleteCallback restart_callback,
       RefreshCompleteCallback continue_callback) override;
 
-  void SetChallengeForBoundSession(const GURL& request_url,
+  void SetChallengeForBoundSession(OnAccessCallback on_access_callback,
+                                   const GURL& request_url,
                                    const SessionChallengeParam& param) override;
 
   Session* GetSessionForTesting(const SchemefulSite& site,
@@ -68,6 +71,7 @@ class NET_EXPORT SessionServiceImpl : public SessionService {
   void OnLoadSessionsComplete(SessionsMap sessions);
 
   void OnRegistrationComplete(
+      OnAccessCallback on_access_callback,
       std::optional<RegistrationFetcher::RegistrationCompleteParams> params);
 
   // Get all the unexpired sessions for a given site. This also removes
