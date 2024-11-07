63028da00ee01d99d5d5e8b813da47e36dce8326
Jennifer Serrano <jennserrano@google.com>
2024-11-07 00:40:01 +0000

    [ntp][enterprise] Move calendar fake data creation to separate file
    Google and Outlook calendar use the same mojom structs so fake data
    creation for the most part remains the same.
    
    Bug: 357102078
    Change-Id: Ic014bb67a48eeba33e72d7c7b3616467c306c1e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001093
    Commit-Queue: Jennifer Serrano <jennserrano@google.com>
    Reviewed-by: Riley Tatum <rtatum@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379382}

---
+++ chrome/browser/new_tab_page/modules/v2/calendar/calendar_fake_data_helper.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NEW_TAB_PAGE_MODULES_V2_CALENDAR_CALENDAR_FAKE_DATA_HELPER_H_
+#define CHROME_BROWSER_NEW_TAB_PAGE_MODULES_V2_CALENDAR_CALENDAR_FAKE_DATA_HELPER_H_
+
+#include <vector>
+
+#include "chrome/browser/new_tab_page/modules/v2/calendar/calendar_data.mojom.h"
+
+namespace calendar::calendar_fake_data_helper {
+
+enum class CalendarType {
+  GOOGLE_CALENDAR = 0,
+  OUTLOOK_CALENDAR = 1,
+};
+
+std::vector<ntp::calendar::mojom::CalendarEventPtr> GetFakeEvents(
+    CalendarType calendar_type);
+
+}  // namespace calendar::calendar_fake_data_helper
+
+#endif  // CHROME_BROWSER_NEW_TAB_PAGE_MODULES_V2_CALENDAR_CALENDAR_FAKE_DATA_HELPER_H_
