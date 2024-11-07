c011c5a31720593014496f01e5917be523b81ae1
Nate Chapin <japhet@chromium.org>
2024-11-07 09:51:18 +0000

    Use CallWith=Isolate instead of CallWith=ScriptState in trusted_type_policy.idl
    The implementation just delegates to an internal version that takes an
    Isolate instead of a ScriptState, this allows the bindings to call that
    version directly
    
    Change-Id: Id5dd8df0daeffe8e38e176bff47154fee339ff32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001294
    Reviewed-by: Daniel Vogelheim <vogelheim@chromium.org>
    Auto-Submit: Nate Chapin <japhet@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379555}

--- third_party/blink/renderer/core/trustedtypes/trusted_types_util.cc
+++ third_party/blink/renderer/core/trustedtypes/trusted_types_util.cc
@@ -286,7 +286,7 @@ String GetStringFromScriptHelper(
     return script;
   }
 
-  TrustedScript* result = default_policy->CreateScript(
+  TrustedScript* result = default_policy->createScript(
       context->GetIsolate(), script,
       GetDefaultCallbackArgs(context->GetIsolate(), "TrustedScript",
                              interface_name, property_name, script),
@@ -345,7 +345,7 @@ String TrustedTypesCheckForHTML(const String& html,
   // TODO(ajwong): This can be optimized to avoid a AddRef in the
   // StringCache::CreateStringAndInsertIntoCache() also, but it's a hard mess.
   // Punt for now.
-  TrustedHTML* result = default_policy->CreateHTML(
+  TrustedHTML* result = default_policy->createHTML(
       execution_context->GetIsolate(), html,
       GetDefaultCallbackArgs(execution_context->GetIsolate(), "TrustedHTML",
                              interface_name, property_name),
@@ -399,7 +399,7 @@ String TrustedTypesCheckForScript(const String& script,
   // TODO(ajwong): This can be optimized to avoid a AddRef in the
   // StringCache::CreateStringAndInsertIntoCache() also, but it's a hard mess.
   // Punt for now.
-  TrustedScript* result = default_policy->CreateScript(
+  TrustedScript* result = default_policy->createScript(
       execution_context->GetIsolate(), script,
       GetDefaultCallbackArgs(execution_context->GetIsolate(), "TrustedScript",
                              interface_name, property_name, script),
@@ -454,7 +454,7 @@ String TrustedTypesCheckForScriptURL(const String& script_url,
   // TODO(ajwong): This can be optimized to avoid a AddRef in the
   // StringCache::CreateStringAndInsertIntoCache() also, but it's a hard mess.
   // Punt for now.
-  TrustedScriptURL* result = default_policy->CreateScriptURL(
+  TrustedScriptURL* result = default_policy->createScriptURL(
       execution_context->GetIsolate(), script_url,
       GetDefaultCallbackArgs(execution_context->GetIsolate(),
                              "TrustedScriptURL", interface_name, property_name),
