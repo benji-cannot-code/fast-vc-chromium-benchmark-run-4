46d6fd429de3dcb14b9b6c144efb046745f04284
Siyu <siyua@chromium.org>
2024-11-07 19:15:22 +0000

    [Pix] Add metrics to Pix flow (6)
    Adding metric to record event when risk data is not available.
    
    go/pix-in-chrome-metrics-revised
    
    Bug: 367751320, 377126728
    Change-Id: I8c1044cb8d071abf3ba4310b103c53248944a9ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001753
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379842}

--- components/facilitated_payments/core/browser/facilitated_payments_manager.h
+++ components/facilitated_payments/core/browser/facilitated_payments_manager.h
@@ -108,7 +108,7 @@ class FacilitatedPaymentsManager {
   FRIEND_TEST_ALL_PREFIXES(FacilitatedPaymentsManagerTest,
                            PixPaymentPromptAccepted_TriggersLoadRiskData);
   FRIEND_TEST_ALL_PREFIXES(FacilitatedPaymentsManagerTest,
-                           PaymentNotOfferedReason_RiskDataEmpty);
+                           PayflowExitedReason_RiskDataEmpty);
   FRIEND_TEST_ALL_PREFIXES(
       FacilitatedPaymentsManagerTest,
       RiskDataEmpty_GetClientTokenNotCalled_ErrorScreenShown);
