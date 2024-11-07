ca4f7e7b104603c71263e2f26f1b159759fee340
Lukasz Anforowicz <lukasza@chromium.org>
2024-11-07 22:54:48 +0000

    [rust png] Check `IsAllDataReceived` in `FrameIsReceivedAtIndex`.
    This CL tweaks `SkiaImageDecoderBase::FrameIsReceivedAtIndex` so that it
    takes into account `IsAllDataReceived`.  When all data has been
    received, then (by definition) all data for all individual frames has
    also been received (the default `ImageDecoder::FrameIsReceivedAtIndex`
    implementation just returns `IsAllDataReceived()`).  This is true, even
    if an `SkCodec` may not have parsed the whole stream and didn't yet
    realize that all the data is indeed available.
    
    Fixed: chromium:377555843
    Change-Id: Ic8ce140edf42ceefc45d2b81a848b97ec8c8f573
    Cq-Include-Trybots: chromium/try:android-rust-arm32-rel
    Cq-Include-Trybots: chromium/try:android-rust-arm64-dbg
    Cq-Include-Trybots: chromium/try:android-rust-arm64-rel
    Cq-Include-Trybots: chromium/try:linux-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:linux-rust-x64-rel
    Cq-Include-Trybots: chromium/try:win-rust-x64-dbg
    Cq-Include-Trybots: chromium/try:win-rust-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5962743
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379986}

--- third_party/blink/renderer/platform/image-decoders/skia/skia_image_decoder_base.cc
+++ third_party/blink/renderer/platform/image-decoders/skia/skia_image_decoder_base.cc
@@ -149,6 +149,14 @@ int SkiaImageDecoderBase::RepetitionCount() const {
 }
 
 bool SkiaImageDecoderBase::FrameIsReceivedAtIndex(wtf_size_t index) const {
+  // When all input data has been received, then (by definition) it means that
+  // all data for all individual frames has also been received.  (Note that the
+  // default `ImageDecoder::FrameIsReceivedAtIndex` implementation just returns
+  // `IsAllDataReceived()`.)
+  if (IsAllDataReceived()) {
+    return true;
+  }
+
   SkCodec::FrameInfo frame_info;
   if (!codec_ || !codec_->getFrameInfo(index, &frame_info)) {
     return false;
@@ -345,7 +353,7 @@ void SkiaImageDecoderBase::Decode(wtf_size_t index) {
       }
       case SkCodec::kIncompleteInput:
         frame.SetPixelsChanged(true);
-        if (FrameIsReceivedAtIndex(current_frame_index) || IsAllDataReceived()) {
+        if (FrameIsReceivedAtIndex(current_frame_index)) {
           SetFailedFrameIndex(current_frame_index);
         }
         break;
