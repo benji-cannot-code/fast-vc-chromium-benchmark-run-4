3294a9b2e78c531f690d0527f596ac4c61b7a99d
Jan Keitel <jkeitel@google.com>
2024-11-07 07:59:21 +0000

    Do not close toast while menu is showing
    This CL adds a functionality to ToastView to delay closing if the
    menu is currently showing. Once the menu closes and there was a prior
    attempt to close the ToastView, the ToastView is then closed.
    
    Screencast: https://screencast.googleplex.com/cast/NTIyNjM2NDYwMzk5MDAxNnwwOWYzY2ZlYy0wNg
    
    Bug: 358618479
    Change-Id: I7dda503b4ed9fddb6a9d0ba015b5d4e5ee2b7aa3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999264
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Jan Keitel <jkeitel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379507}

--- chrome/browser/ui/toasts/toast_view.cc
+++ chrome/browser/ui/toasts/toast_view.cc
@@ -332,8 +332,12 @@ void ToastView::AnimateIn() {
 }
 
 void ToastView::Close(ToastCloseReason reason) {
-  // TODO(crbug.com/358618479): Only close if menu is not showing - otherwise,
-  // delay the close.
+  // Do not close if the menu is open - instead, remember the call to close it.
+  if (menu_runner_) {
+    pending_close_reason_ = reason;
+    return;
+  }
+
   // TODO(crbug.com/358610872): Log toast close reason metric.
   views::Widget::ClosedReason widget_closed_reason =
       views::Widget::ClosedReason::kUnspecified;
@@ -451,6 +455,9 @@ void ToastView::OnMenuClosed() {
   menu_runner_.reset();
   menu_button_highlight_.reset();
   menu_button_->SetState(views::Button::ButtonState::STATE_NORMAL);
+  if (pending_close_reason_) {
+    Close(pending_close_reason_.value());
+  }
 }
 
 BEGIN_METADATA(ToastView)
