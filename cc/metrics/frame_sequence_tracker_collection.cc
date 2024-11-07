d4338b1b5d984fb8f56a8a3576cbfa46f13e5a2f
Matthew Jones <mdjones@chromium.org>
2024-11-07 22:54:36 +0000

    Revert "Propagate scroll state for backfill PRs."
    This reverts commit 36b0a4fd7dac5a3c9c02e7a720c45087598a24e3.
    
    Reason for revert: Causing failures on the android bots:
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20WebView%20O%20(dbg)/25699/overview
    
    https://ci.chromium.org/ui/p/chromium/builders/ci/Android%20WebView%20P%20(dbg)/17875/overview
    
    Original change's description:
    > Propagate scroll state for backfill PRs.
    >
    > Set scroll state to fetch from active scrolling tracker
    > when making pipeline reporters for backfilled frames.
    >
    > Presented frame pr:
    > https://screenshot.googleplex.com/AAds6BJNYfCHPtX
    > Backfill pr:
    > https://screenshot.googleplex.com/6iwvCBgqPhAuhNb
    >
    > Bug: 40213368
    > Change-Id: Ic3f0ff90d564b6d640a7440ce6999856a135e9ce
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5952948
    > Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    > Reviewed-by: Jonathan Ross <jonross@chromium.org>
    > Commit-Queue: Stacy Gaikovaia <gaiko@google.com>
    > Auto-Submit: Stacy Gaikovaia <gaiko@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1379708}
    
    Bug: 40213368
    Change-Id: I478faa20978a9bc74a37d5bf1934d91347c1e064
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000839
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1379985}

--- cc/metrics/frame_sequence_tracker_collection.cc
+++ cc/metrics/frame_sequence_tracker_collection.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/containers/contains.h"
 #include "base/memory/ptr_util.h"
 #include "cc/metrics/compositor_frame_reporting_controller.h"
-#include "cc/metrics/frame_info.h"
 #include "cc/metrics/frame_sequence_tracker.h"
 
 namespace cc {
@@ -336,16 +335,6 @@ void FrameSequenceTrackerCollection::AddSortedFrame(
   }
 
   DestroyTrackers();
-}
-
-FrameInfo::SmoothEffectDrivingThread
-FrameSequenceTrackerCollection::GetSmoothEffectDrivingThread() {
-  for (auto const& tracker : frame_trackers_) {
-    if (IsScrollType(tracker.first.first)) {
-      return tracker.first.second;
-    }
-  }
-  return FrameInfo::SmoothEffectDrivingThread::kUnknown;
 }
 
 }  // namespace cc
