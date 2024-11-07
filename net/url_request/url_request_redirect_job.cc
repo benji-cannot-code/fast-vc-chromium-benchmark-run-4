52f6f6bfd9b52f16738462b948257fe41dc3ce36
Marja Hölttä <marja@chromium.org>
2024-11-07 10:07:09 +0000

    [v8 code cache] Fix timestamp mismatch issue
    When we get a HTTP 304 ("Not Modified") response, we update the
    response_time in the HTTP cache. But we don't update the code cache
    response time. Later on, we check that the code cache associated with a
    response has the same response time, but they won't match. This results
    in discarding valid code caches.
    
    Design doc: https://docs.google.com/document/d/1ndVOb-GUCCHiVT4uO-NyOQ3N3R4f8iDKtMU8zurnc-U/edit?usp=sharing
    
    Bug: 373703283
    Change-Id: I2d1ada339535736fc09a5df151cb01282b9a0972
    Fixed: 373703283
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937821
    Reviewed-by: Nidhi Jaju <nidhijaju@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379562}

--- net/url_request/url_request_redirect_job.cc
+++ net/url_request/url_request_redirect_job.cc
@@ -51,6 +51,7 @@ void URLRequestRedirectJob::GetResponseInfo(HttpResponseInfo* info) {
   info->headers = fake_headers_;
   info->request_time = response_time_;
   info->response_time = response_time_;
+  info->original_response_time = response_time_;
 }
 
 void URLRequestRedirectJob::GetLoadTimingInfo(
