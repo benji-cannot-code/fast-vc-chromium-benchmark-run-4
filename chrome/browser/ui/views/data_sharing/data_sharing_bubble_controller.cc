47c77f41e532d4599b74489a44c0add2c9df329e
Pengchao Cai <pengchaocai@chromium.org>
2024-11-07 19:17:58 +0000

    [data sharing] Open WebUI learn more links to browser
    This CL added the override of WebContentsDelegate::AddNewContents in
    WebUIContentsWrapper which will be invoked (by WebContentsImpl
    ::CreateNewWindow) when left clicking a href link from the data sharing
    webui webcontents. The call is then forwarded to WebUIContentsWrapper
    ::Host.
    
    Change-Id: Ib7366380bef6bb8f5447052334a7f7106578d2a1
    Bug: 373898207
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5996011
    Reviewed-by: Yuheng Huang <yuhengh@chromium.org>
    Commit-Queue: pengchao Cai <pengchaocai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379844}

--- chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.cc
+++ chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/data_sharing/data_sharing_bubble_controller.h"
 
+#include "base/memory/raw_ptr.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_element_identifiers.h"
+#include "chrome/browser/ui/browser_navigator.h"
 #include "chrome/browser/ui/views/data_sharing/data_sharing_utils.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/frame/top_container_view.h"
@@ -25,17 +27,47 @@ class DataSharingBubbleDialogView : public WebUIBubbleDialogView {
   METADATA_HEADER(DataSharingBubbleDialogView, WebUIBubbleDialogView)
  public:
   explicit DataSharingBubbleDialogView(
+      Browser* browser,
       views::View* anchor_view,
       std::unique_ptr<WebUIContentsWrapper> contents_wrapper)
       : WebUIBubbleDialogView(anchor_view,
                               contents_wrapper->GetWeakPtr(),
                               std::nullopt,
                               views::BubbleBorder::Arrow::TOP_LEFT),
-        contents_wrapper_(std::move(contents_wrapper)) {}
+        contents_wrapper_(std::move(contents_wrapper)),
+        browser_(browser) {}
+
+  // WebUIContentsWrapper::Host override. Handle opening WebUI href links into
+  // browser.
+  content::WebContents* AddNewContents(
+      content::WebContents* source,
+      std::unique_ptr<content::WebContents> new_contents,
+      const GURL& target_url,
+      WindowOpenDisposition disposition,
+      const blink::mojom::WindowFeatures& window_features,
+      bool user_gesture,
+      bool* was_blocked) override;
 
  private:
   std::unique_ptr<WebUIContentsWrapper> contents_wrapper_;
+  raw_ptr<Browser> browser_;
 };
+
+content::WebContents* DataSharingBubbleDialogView::AddNewContents(
+    content::WebContents* source,
+    std::unique_ptr<content::WebContents> new_contents,
+    const GURL& target_url,
+    WindowOpenDisposition disposition,
+    const blink::mojom::WindowFeatures& window_features,
+    bool user_gesture,
+    bool* was_blocked) {
+  NavigateParams params(browser_, target_url,
+                        ui::PageTransition::PAGE_TRANSITION_LINK);
+  params.tabstrip_index = browser_->tab_strip_model()->count();
+  params.disposition = disposition;
+  Navigate(&params);
+  return params.navigated_or_inserted_contents;
+}
 
 BEGIN_METADATA(DataSharingBubbleDialogView)
 END_METADATA
@@ -86,7 +118,7 @@ void DataSharingBubbleController::Show(
   }
 
   auto bubble_view = std::make_unique<DataSharingBubbleDialogView>(
-      anchor_view_for_share, std::move(contents_wrapper));
+      &GetBrowser(), anchor_view_for_share, std::move(contents_wrapper));
   bubble_view->SetProperty(views::kElementIdentifierKey,
                            kDataSharingBubbleElementId);
   bubble_view_ = bubble_view->GetWeakPtr();
