207b6b234656e09b56993f4ae1822a913c9944eb
Monica Basta <msalama@chromium.org>
2024-11-07 09:29:59 +0000

    [B4B-D] Update BookmarkMenuDelegate::GetDropParams
    Update `GetDropParams` to act as if the key to `menu_id_to_node_map_`
    either contains a `BookmarkParentFolder` or a bookmark URL node. This is
    a fair representation as an item in the bookmark menu can either be a
    bookmark url node, bookmark permanent folder including both local and
    account bookmarks or non permanent bookmark folder.
    
    Bug: 369304373
    Change-Id: I52f2a420a97ca3f36a67febbdfdd905763f7a97e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999352
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Monica Basta <msalama@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379547}

--- chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.h
+++ chrome/browser/ui/views/bookmarks/bookmark_menu_delegate.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/functional/callback.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
+#include "chrome/browser/bookmarks/bookmark_merged_surface_service.h"
 #include "chrome/browser/ui/bookmarks/bookmark_stats.h"
 #include "chrome/browser/ui/toolbar/app_menu_model.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_context_menu.h"
@@ -150,7 +151,7 @@ class BookmarkMenuDelegate : public bookmarks::BaseBookmarkModelObserver,
       NodeToMenuMap;
 
   struct DropParams {
-    raw_ptr<const bookmarks::BookmarkNode> drop_parent = nullptr;
+    BookmarkParentFolder drop_parent;
     size_t index_to_drop_at = 0;
   };
 
