6b939f4349d71295d1860b699043cc04a886e6f0
Maksim Moskvitin <mmoskvitin@google.com>
2024-11-07 23:24:09 +0000

    [DataSharing] Record DB init status histogram
    Bug: 370897286
    Change-Id: I4721c363650e09a34145c23140088162d5830d96
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999119
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Auto-Submit: Maksim Moskvitin <mmoskvitin@google.com>
    Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380016}

--- components/data_sharing/internal/group_data_model.cc
+++ components/data_sharing/internal/group_data_model.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_forward.h"
+#include "base/metrics/histogram_functions.h"
 #include "base/observer_list.h"
 #include "components/data_sharing/internal/group_data_proto_utils.h"
 #include "components/data_sharing/internal/group_data_store.h"
@@ -140,9 +141,12 @@ void GroupDataModel::OnCollaborationGroupSyncDataLoaded() {
 
 void GroupDataModel::OnGroupDataStoreLoaded(
     GroupDataStore::DBInitStatus status) {
+  base::UmaHistogramBoolean("DataSharing.GroupDBInitSuccess",
+                            status == GroupDataStore::DBInitStatus::kSuccess);
+  if (db_loaded_callback_) {
+    std::move(db_loaded_callback_).Run();
+  }
   if (status != GroupDataStore::DBInitStatus::kSuccess) {
-    // TODO(crbug.com/301390275): perhaps some error handling is needed in this
-    // case (at least metrics).
     return;
   }
 
@@ -327,6 +331,11 @@ void GroupDataModel::NotifyObserversAboutChangedMembers(
 
 GroupDataStore& GroupDataModel::GetGroupDataStoreForTesting() {
   return group_data_store_;
+}
+
+void GroupDataModel::SetGroupDataStoreLoadedCallbackForTesting(
+    base::OnceClosure db_loaded_callback) {
+  db_loaded_callback_ = std::move(db_loaded_callback);
 }
 
 }  // namespace data_sharing
