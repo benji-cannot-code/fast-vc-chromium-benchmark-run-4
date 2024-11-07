451fe707d3e34e4f299ddd6cbab653d605e81204
Wan-Teh Chang <wtc@google.com>
2024-11-07 01:37:57 +0000

    Roll src/third_party/dav1d/libdav1d/ ef4aff75b..93f12c117 (12 commits)
    https://chromium.googlesource.com/external/github.com/videolan/dav1d.git/+log/ef4aff75b0b5..93f12c117a4e
    
    $ git log ef4aff75b..93f12c117 --date=short --first-parent --format='%ad %ae %s'
    2024-10-16 brad Provide dav1d_getauxval() wrapper for getauxvaul() and elf_aux_info()
    2024-11-04 unlord riscv64/mc: Only process w*3/4 elements in blend_v
    2024-11-04 unlord riscv64/mc16: Unroll 16bpc RVV blend_v 2x
    2024-11-04 unlord riscv64/mc16: Branchless vsetvl in blend_v function
    2024-11-04 unlord riscv64/mc16: Add VLEN=256 8bpc RVV blend_v function
    2024-11-04 unlord riscv64/mc16: Add 16bpc RVV blend_v function
    2024-10-29 unlord riscv64/mc16: Unroll 16bpc RVV blend 2x
    2024-10-30 unlord riscv64/mc16: Branchless vsetvl in blend function
    2024-10-30 unlord riscv64/mc16: Add VLEN=256 8bpc RVV blend function
    2024-10-29 unlord meson: Move riscv64 8bpc only files into bitdepth sources
    2024-10-29 unlord riscv64/mc16: Add 16bpc RVV blend function
    2024-10-28 unlord riscv64/ipred16: Fix build error with -Dbitdepths=16
    
    Created with:
      roll-dep src/third_party/dav1d/libdav1d
    R=eugene@chromium.org
    
    Change-Id: I16098d02c83db3dd01f0300e1508bb16e203bfd6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002091
    Commit-Queue: Wan-Teh Chang <wtc@google.com>
    Auto-Submit: Wan-Teh Chang <wtc@google.com>
    Reviewed-by: Eugene Zemtsov <eugene@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379406}

--- third_party/dav1d/version/vcs_version.h
+++ third_party/dav1d/version/vcs_version.h
@@ -1,3 +1,3 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* auto-generated, do not edit */
-#define DAV1D_VERSION "1.5.0-4-gef4aff7"
+#define DAV1D_VERSION "1.5.0-16-g93f12c1"
