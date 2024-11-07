40797681e9e3e87bcf0e3f471e568d2aa9f6acac
Hyungwook Lee <hyungwook.lee@navercorp.com>
2024-11-07 12:28:00 +0000

    Remove some chrome:: namespace from chrome/browser/ui/browser_otr_state*
    Bug: 41050580
    Change-Id: I162b99a389dd6ef7b5a4c97952073027f5248855
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5937221
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: Hyungwook Lee <hyungwook.lee@navercorp.com>
    Cr-Commit-Position: refs/heads/main@{#1379622}

--- chrome/browser/ui/browser_otr_state.h
+++ chrome/browser/ui/browser_otr_state.h
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BROWSER_OTR_STATE_H_
 #define CHROME_BROWSER_UI_BROWSER_OTR_STATE_H_
 
-namespace chrome {
-
 bool IsOffTheRecordSessionActive();
-
-}  // namespace chrome
 
 #endif  // CHROME_BROWSER_UI_BROWSER_OTR_STATE_H_
--- chrome/browser/ui/browser_otr_state_android.cc
+++ chrome/browser/ui/browser_otr_state_android.cc
@@ -6,10 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 #include "chrome/browser/ui/browser_otr_state.h"
 
-namespace chrome {
-
 bool IsOffTheRecordSessionActive() {
   return TabModelList::IsOffTheRecordSessionActive();
 }
-
-}  // namespace chrome
