7117df99e434e8f4b5e1aeec21f777b7d4b121ac
dpapad <dpapad@chromium.org>
2024-11-07 00:27:41 +0000

    Add //third_party/jstemplate:library dependency in a few more places.
    These places previously used a <script> tag to fetch the
    jstemplate_compiled.js file and therefore were not type checked via TS.
    Moving the import to the TS side seeems better, as it increases type
    checking coverage.
    
    This also allows removing custom definitions for jstemplate in favor
    of the canonical one in third_party/jstemplate/jstemplate.d.ts, as well
    as possibly preventing future usages of jstemplate (which is considered
    deprecated) by adding visibility checks for the
    third_party/jstemplate:library target in a follow-up.
    
    Also removing a TS path mapping that should have been removed in
    crrev.com/c/6001253.
    
    Fixed: 377617674
    Change-Id: I9d0fbe04ad83edb6cfe0647d238bc6e88c3b073c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001173
    Reviewed-by: Rebekah Potter <rbpotter@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Rebekah Potter <rbpotter@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379375}

--- chrome/browser/resources/components/components.ts
+++ chrome/browser/resources/components/components.ts
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'chrome://resources/js/jstemplate_compiled.js';
 import '/strings.m.js';
 
 import {assert} from 'chrome://resources/js/assert.js';
@@ -10,15 +11,6 @@ import {addWebUiListener, sendWithPromise} from 'chrome://resources/js/cr.js';
 import {loadTimeData} from 'chrome://resources/js/load_time_data.js';
 import {isChromeOS} from 'chrome://resources/js/platform.js';
 import {$, getRequiredElement} from 'chrome://resources/js/util.js';
-
-declare global {
-  class JsEvalContext {
-    constructor(data: any);
-  }
-  function jstProcess(context: JsEvalContext, template: HTMLElement): void;
-
-  const trustedTypes: {emptyHTML: string};
-}
 
 interface Component {
   id: string;
@@ -53,7 +45,7 @@ function renderTemplate(componentsData: ComponentsData) {
       document.body.querySelector<HTMLElement>(
                        '#component-template')!.cloneNode(true) as HTMLElement;
   getRequiredElement('component-placeholder').innerHTML =
-      trustedTypes.emptyHTML;
+      window.trustedTypes!.emptyHTML;
   getRequiredElement('component-placeholder').appendChild(output);
   jstProcess(input, output);
   output.removeAttribute('hidden');
--- components/download/resources/download_internals/download_internals.ts
+++ components/download/resources/download_internals/download_internals.ts
@@ -5,22 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // <if expr="is_ios">
 import 'chrome://resources/js/ios/web_ui.js';
-
 // </if>
+
+import 'chrome://resources/js/jstemplate_compiled.js';
+import './download_internals_visuals.js';
 
 import {addWebUiListener} from 'chrome://resources/js/cr.js';
 import {getRequiredElement} from 'chrome://resources/js/util.js';
 
 import type {DownloadInternalsBrowserProxy, ServiceEntry, ServiceRequest, ServiceStatus} from './download_internals_browser_proxy.js';
 import {DownloadInternalsBrowserProxyImpl, ServiceEntryState} from './download_internals_browser_proxy.js';
-
-declare global {
-  class JsEvalContext {
-    constructor(data: any);
-  }
-
-  function jstProcess(context: JsEvalContext, template: HTMLElement): void;
-}
 
 const browserProxy: DownloadInternalsBrowserProxy =
     DownloadInternalsBrowserProxyImpl.getInstance();
