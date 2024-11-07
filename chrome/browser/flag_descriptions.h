6b562ad3b00204cdd7f50b6a36d63f4ded294e53
Igor Kraskevich <kraskevich@google.com>
2024-11-07 11:57:14 +0000

    [Android EMEA Gardener] Revert "Add PartitionAllocMemoryTagging to chrome://flags"
    This reverts commit a83b2d80713c4b1cce2c235cbe72cca87adc371f.
    
    Reason for revert: this CL breaks AboutFlagsTest.EveryFlagHasMetadata test on some bots, for example https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests/20950/test-results?sortby=&groupby=
    
    Original change's description:
    > Add PartitionAllocMemoryTagging to chrome://flags
    >
    > Bug: 372508438
    > Change-Id: Ic17d47aaf1db67c1672b223ed4d5157c38865d90
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5997853
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Reviewed-by: Takashi Sakamoto <tasak@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1379420}
    
    Bug: 372508438
    Change-Id: Ifa18fdbb10acec66a11040a67be932d8d22653c7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999716
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379605}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1667,9 +1667,6 @@ extern const char kPageVisibilityPageContentAnnotationsDescription[];
 extern const char kParallelDownloadingName[];
 extern const char kParallelDownloadingDescription[];
 
-extern const char kPartitionAllocMemoryTaggingName[];
-extern const char kPartitionAllocMemoryTaggingDescription[];
-
 extern const char kPartitionAllocWithAdvancedChecksName[];
 extern const char kPartitionAllocWithAdvancedChecksDescription[];
 
