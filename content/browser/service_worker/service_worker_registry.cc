dc3579accef963787b8c404899559f2073eecae1
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2024-11-07 08:03:01 +0000

    Deprecate ServiceWorker.Storage.RetryCountForRecovery
    As far as I checked the recent UMA, the count is almost 0.
    I can see several times out of more than 10 million times on Windows.
    
    I believe we can delete the metrics.
    
    OBSOLETE_HISTOGRAM[ServiceWorker.Storage.RetryCountForRecovery]=The metric is no longer useful.
    
    Fixed: 377305338, 40150911
    Change-Id: I95227e90d80a9e1744a79f745f329dd64a340a05
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002453
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Keita Suzuki <suzukikeita@chromium.org>
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379510}

--- content/browser/service_worker/service_worker_registry.cc
+++ content/browser/service_worker/service_worker_registry.cc
@@ -94,11 +94,6 @@ void CompleteFindSoon(
                                     status, std::move(callback)));
 }
 
-void RecordRetryCount(size_t retries, size_t queue_size) {
-  base::UmaHistogramCounts100("ServiceWorker.Storage.RetryCountForRecovery",
-                              retries);
-}
-
 // Notifies quota manager that a disk write operation failed so that it can
 // check for storage pressure.
 void CheckForClientWriteFailure(
@@ -1881,9 +1876,7 @@ void ServiceWorkerRegistry::OnRemoteStorageDisconnected() {
   if (connection_state_ == ConnectionState::kRecovering) {
     ++recovery_retry_counts_;
     if (recovery_retry_counts_ > kMaxRetryCounts) {
-      RecordRetryCount(kMaxRetryCounts, inflight_calls_.size());
-      CHECK(false) << "The Storage Service consistently crashes.";
-      return;
+      NOTREACHED() << "The Storage Service consistently crashes.";
     }
   }
   connection_state_ = ConnectionState::kRecovering;
@@ -1903,8 +1896,6 @@ void ServiceWorkerRegistry::OnRemoteStorageDisconnected() {
 
 void ServiceWorkerRegistry::DidRecover() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-
-  RecordRetryCount(recovery_retry_counts_, inflight_calls_.size());
 
   recovery_retry_counts_ = 0;
   connection_state_ = ConnectionState::kNormal;
