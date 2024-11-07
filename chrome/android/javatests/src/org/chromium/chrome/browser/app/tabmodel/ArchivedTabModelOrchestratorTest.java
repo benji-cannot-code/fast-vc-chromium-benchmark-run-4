a1825174dd00fc71e5f18e348ee4de4c1148cf9f
Igor Kraskevich <kraskevich@google.com>
2024-11-07 12:16:50 +0000

    [Android EMEA Gardener] Disable testOpenArchivedTabFromHubSearch
    The test is broken on several bots, for example https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm-tests and https://ci.chromium.org/ui/p/chrome/builders/ci/android-arm64-tests
    
    Bug: 377800167
    Change-Id: I23e7ac52be3c8453914d145b1a3a62f430b2576a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000357
    Commit-Queue: Igor Kraskevich <kraskevich@google.com>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Owners-Override: Igor Kraskevich <kraskevich@google.com>
    Auto-Submit: Igor Kraskevich <kraskevich@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379616}

--- chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestratorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/app/tabmodel/ArchivedTabModelOrchestratorTest.java
@@ -42,6 +42,7 @@ import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.task.TaskRunner;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.CriteriaHelper;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.DisableFeatures;
 import org.chromium.base.test.util.Features.EnableFeatures;
@@ -379,6 +380,7 @@ public class ArchivedTabModelOrchestratorTest {
 
     @Test
     @MediumTest
+    @DisabledTest(message = "https://crbug.com/377800167")
     public void testOpenArchivedTabFromHubSearch() {
         finishLoading();
         mActivityTestRule.loadUrl(mActivityTestRule.getTestServer().getURL(TEST_PATH));
