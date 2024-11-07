88699e8daa2205dffcad62e273df8f299d94e711
Igor Kraskevich <kraskevich@google.com>
2024-11-07 11:08:18 +0000

    Revert "[messaging] Add change delta observer shell for TabGroupSyncService"
    This reverts commit 136ff32af269394f2ca1dfc950596d4724a6300b.
    
    Reason for revert: this causes crashes for multiple tests on multiple bots, for example https://ci.chromium.org/ui/test/chrome/org.chromium.chrome.browser.collaboration.CollaborationServiceFactoryTest%23testServiceCreation_RealService?q=V%3Abuilder%3Dandroid-arm64-tests+V%3Adevice_os%3DR+V%3Adevice_type%3Dflame+V%3Aos%3DAndroid+V%3Atest_suite%3Dchrome_public_test_apk
    
    Original change's description:
    > [messaging] Add change delta observer shell for TabGroupSyncService
    >
    > This CL adds a shell for a new TabGroupChangeNotifier that will act as
    > a delta observer for changes from TabGroupSyncService.
    >
    > For now it is only a shell, but in later CLs we will add support for
    > in fact converting the changes from TabGroupSyncService to what we
    > need and matches CollaborationEvent type changes.
    >
    > Since we with this CL is starting to fill in the
    > MessagingBackendServiceImpl, this CL also adds a test suite for this
    > class.
    >
    > Bug: 345856704
    > Change-Id: Ie7a3788d05f94baa131d373b9127ef7cbbeb309d
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5985139
    > Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    > Commit-Queue: Tommy Nyquist <nyquist@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1379270}
    
    Bug: 345856704
    Change-Id: I0b4b0e7627c41120a56b2f91aa588b1143beabb7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999715
    Owners-Override: Igor Kraskevich <kraskevich@google.com>
    Auto-Submit: Igor Kraskevich <kraskevich@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1379584}

--- chrome/browser/collaboration/messaging/messaging_backend_service_factory.cc
+++ chrome/browser/collaboration/messaging/messaging_backend_service_factory.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tab_group_sync/tab_group_sync_service_factory.h"
 #include "components/collaboration/internal/messaging/messaging_backend_service_impl.h"
-#include "components/collaboration/internal/messaging/tab_group_change_notifier_impl.h"
 #include "components/data_sharing/public/features.h"
 #include "components/saved_tab_groups/public/features.h"
 #include "components/saved_tab_groups/public/tab_group_sync_service.h"
@@ -63,12 +62,9 @@ MessagingBackendServiceFactory::BuildServiceInstanceForBrowserContext(
       tab_groups::TabGroupSyncServiceFactory::GetForProfile(profile);
   auto* data_sharing_service =
       data_sharing::DataSharingServiceFactory::GetForProfile(profile);
-  auto tab_group_change_notifier =
-      std::make_unique<TabGroupChangeNotifierImpl>(tab_group_sync_service);
 
   auto service = std::make_unique<MessagingBackendServiceImpl>(
-      std::move(tab_group_change_notifier), tab_group_sync_service,
-      data_sharing_service);
+      tab_group_sync_service, data_sharing_service);
 
   return std::move(service);
 }
