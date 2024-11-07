0a2fdbd42b23e93c45760583350565b727972164
Mike Taylor <miketaylr@chromium.org>
2024-11-07 15:15:30 +0000

    Add a flag to control partitioned visited links, w/ 'self-links'
    In addition, replace PartitionVisitedLinkDatabase with
    PartitionVisitedLinkDatabaseWithSelfLinks as the relevant
    experimental feature.
    
    Bug: 377647715
    Change-Id: I5815ff7e9e6764c957db2df96e7586f63d842253
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000694
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Mike Taylor <miketaylr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379691}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1673,6 +1673,9 @@ extern const char kPartitionAllocWithAdvancedChecksDescription[];
 extern const char kPartitionVisitedLinkDatabaseName[];
 extern const char kPartitionVisitedLinkDatabaseDescription[];
 
+extern const char kPartitionVisitedLinkDatabaseWithSelfLinksName[];
+extern const char kPartitionVisitedLinkDatabaseWithSelfLinksDescription[];
+
 extern const char kPartitionedPopinsName[];
 extern const char kPartitionedPopinsDescription[];
 
