c6f34bdcf56eb6bf9710a1b261a47f49c19781f3
Morten Stenshorne <mstensho@chromium.org>
2024-11-07 12:27:50 +0000

    [CodeHealth] Remove stale base::Feature ArcMediaView
    Enabled by default since M57.
    
    Bug: 356235266
    Change-Id: I73e17477b08a7ab96ce37b52afa324adbfd4be08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993512
    Reviewed-by: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379621}

--- chrome/browser/ash/arc/fileapi/arc_media_view_util.h
+++ chrome/browser/ash/arc/fileapi/arc_media_view_util.h
@@ -10,12 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/feature_list.h"
-
 namespace arc {
-
-// base::FeatureList feature for ARC media view.
-BASE_DECLARE_FEATURE(kMediaViewFeature);
 
 // Authority of MediaDocumentsProvider in Android.
 extern const char kMediaDocumentsProviderAuthority[];
