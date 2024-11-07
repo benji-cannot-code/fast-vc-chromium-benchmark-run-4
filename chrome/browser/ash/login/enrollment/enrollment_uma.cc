9c563c7d7343745881f74d6d46fd3358cc38fe99
Jackson Tadie <jacksontadie@google.com>
2024-11-07 19:54:46 +0000

    Use new FRD flavors during enrollment
    Add new enrollment modes to mirror the Flex Remote Deployment forced and
    manual fallback flavors, and trigger token-based enrollment with them
    like the existing token enrollment modes. The enrollment flavor is
    included in the registration request, informing the server whether the
    token-based enrollment was FRD or non-FRD auto-enrollment.
    
    Test: unit_tests --test_filter=EnrollmentConfigTest.TokenEnrollment.*
    Test: unit_tests
    --test_filter=EnrollmentScreenTokenBasedEnrollmentTest.*
    Test: Manually verify FAE succeeds with REMOTE_DEPLOYMENT source
    Bug: b/374118628
    
    Change-Id: I73b9431fadcb30e86f6cbab3f5af5be4f847daac
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5960482
    Reviewed-by: Yves Arrouye <drcrash@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Commit-Queue: Jackson Tadie <jacksontadie@google.com>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379870}

--- chrome/browser/ash/login/enrollment/enrollment_uma.cc
+++ chrome/browser/ash/login/enrollment/enrollment_uma.cc
@@ -37,6 +37,10 @@ const char* const kMetricEnrollmentTokenBased =
     "Enterprise.EnrollmentTokenBased";
 const char* const kMetricEnrollmentTokenBasedManualFallback =
     "Enterprise.EnrollmentTokenBasedManualFallback";
+const char* const kMetricEnrollmentRemoteDeployment =
+    "Enterprise.EnrollmentRemoteDeployment";
+const char* const kMetricEnrollmentRemoteDeploymentManualFallback =
+    "Enterprise.EnrollmentRemoteDeploymentManualFallback";
 
 }  // namespace
 
@@ -89,6 +93,13 @@ void EnrollmentUMA(policy::MetricEnrollment sample,
     case policy::EnrollmentConfig::
         MODE_ENROLLMENT_TOKEN_INITIAL_MANUAL_FALLBACK:
       base::UmaHistogramSparse(kMetricEnrollmentTokenBasedManualFallback,
+                               sample);
+      break;
+    case policy::EnrollmentConfig::MODE_REMOTE_DEPLOYMENT_SERVER_FORCED:
+      base::UmaHistogramSparse(kMetricEnrollmentRemoteDeployment, sample);
+      break;
+    case policy::EnrollmentConfig::MODE_REMOTE_DEPLOYMENT_MANUAL_FALLBACK:
+      base::UmaHistogramSparse(kMetricEnrollmentRemoteDeploymentManualFallback,
                                sample);
       break;
     case policy::EnrollmentConfig::MODE_NONE:
