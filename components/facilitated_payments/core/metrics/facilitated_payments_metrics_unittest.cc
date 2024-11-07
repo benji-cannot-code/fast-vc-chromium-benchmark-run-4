911ff6ae8ffbe5d5b6d18d912aea0b1338c48442
Siyu <siyua@chromium.org>
2024-11-07 19:16:12 +0000

    [Pix] Add metrics to Pix flow (7)
    Add metrics regarding client token:
    
    Add a payflow exited reason due to client token missing;
    Remove GetClientToken result histogram;
    Update GetClientToken latency histogram to add a result breakdown.
    
    go/payments-autofill-uma has been updated
    
    OBSOLETE_HISTOGRAM[FacilitatedPayments.Pix.GetClientToken.Latency]=Replaced by FacilitatedPayments.Pix.GetClientToken.Success/Failure.Latency.
    
    OBSOLETE_HISTOGRAM[FacilitatedPayments.Pix.GetClientToken.Result]=Merged to above one as a subcategory.
    
    go/pix-in-chrome-metrics-revised
    
    Bug: 367751320, 377126728
    Change-Id: I3b48af2316de7cf92706f5d98729a6aa3f10ac35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001755
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Vishwas Uppoor <vishwasuppoor@google.com>
    Commit-Queue: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379843}

--- components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
@@ -97,17 +97,13 @@ TEST(FacilitatedPaymentsMetricsTest, LogLoadRiskDataResult) {
       /*expected_bucket_count=*/1);
 }
 
-TEST(FacilitatedPaymentsMetricsTest, LogGetClientTokenResult) {
+TEST(FacilitatedPaymentsMetricsTest, LogGetClientTokenResultAndLatency) {
   base::HistogramTester histogram_tester;
 
-  LogGetClientTokenResult(/*result=*/true, base::Milliseconds(10));
+  LogGetClientTokenResultAndLatency(/*result=*/true, base::Milliseconds(10));
 
   histogram_tester.ExpectUniqueSample(
-      "FacilitatedPayments.Pix.GetClientToken.Result",
-      /*sample=*/true,
-      /*expected_bucket_count=*/1);
-  histogram_tester.ExpectUniqueSample(
-      "FacilitatedPayments.Pix.GetClientToken.Latency",
+      "FacilitatedPayments.Pix.GetClientToken.Success.Latency",
       /*sample=*/10,
       /*expected_bucket_count=*/1);
 }
@@ -227,7 +223,8 @@ INSTANTIATE_TEST_SUITE_P(
                     PayflowExitedReason::kNoLinkedAccount,
                     PayflowExitedReason::kLandscapeScreenOrientation,
                     PayflowExitedReason::kApiClientNotAvailable,
-                    PayflowExitedReason::kRiskDataNotAvailable));
+                    PayflowExitedReason::kRiskDataNotAvailable,
+                    PayflowExitedReason::kClientTokenNotAvailable));
 
 class FacilitatedPaymentsMetricsUkmTest : public testing::Test {
  public:
