fe69614d257c6c51b09ab77be1682c59e61ee6d7
Ramin Halavati <rhalavati@google.com>
2024-11-07 17:37:13 +0000

    Don't show PDF Searchify promo before ScreenAI component download.
    ScreenAI component download is triggered when Chrome considers it useful
    for the user. Therefore the component may be unavailable when user opens
    a PDF without text.
    This CL blocks showing the promo for PDF Searchify until when the
    component is available and searchify can be done.
    
    This changed can be followed up by triggering searchify when ScreenAI
    component is downloaded (crbug.com/377820463).
    
    Bug: 360803943
    Change-Id: Ie7d015200e2784f8c14678e6949e7a0b4bd18b24
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001952
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Ramin Halavati <rhalavati@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379778}

--- chrome/browser/ui/pdf/chrome_pdf_document_helper_client.cc
+++ chrome/browser/ui/pdf/chrome_pdf_document_helper_client.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/pdf_features.h"
 
 #if BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
+#include "chrome/browser/screen_ai/screen_ai_install_state.h"
 #include "chrome/browser/ui/user_education/browser_user_education_interface.h"
 #include "components/user_education/common/feature_promo/feature_promo_controller.h"
 #endif  // BUILDFLAG(ENABLE_SCREEN_AI_SERVICE)
@@ -108,7 +109,10 @@ void ChromePDFDocumentHelperClient::SetPluginCanSave(
 void ChromePDFDocumentHelperClient::OnSearchifyStateChange(
     bool busy,
     content::WebContents* contents) {
-  if (busy) {
+  // Show the promo only when ScreenAI component is available and OCR can be
+  // done.
+  if (busy &&
+      screen_ai::ScreenAIInstallState::GetInstance()->IsComponentAvailable()) {
     MaybeShowFeaturePromo(contents);
   }
 
