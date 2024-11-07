bf9011dc50c523f8b5d5f4e8eaecf81adbb524d1
Cristian Lopez <cristiandlopez@google.com>
2024-11-07 21:09:34 +0000

    Delete seed file upon clearing seed for clients outside treatment group.
    This CL implements deleting a seed file if one is present for clients
    not in the seed files treatment group. This is to avoid the unexpected
    existence of seed files in clients not using them.
    
    Bug: 377918164
    Change-Id: Ifae5da82cbeddbbe20d4e7ab99e58f417ee93506
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001527
    Reviewed-by: Luc Nguyen <lucnguyen@google.com>
    Reviewed-by: Caitlin Fischer <caitlinfischer@google.com>
    Commit-Queue: Cristian Lopez <cristiandlopez@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379922}

--- components/variations/seed_reader_writer.h
+++ components/variations/seed_reader_writer.h
@@ -26,8 +26,8 @@ namespace variations {
 // Trial and group names for the seed file experiment.
 const char kSeedFileTrial[] = "SeedFileTrial";
 const char kDefaultGroup[] = "Default";
-const char kControlGroup[] = "Control_V1";
-const char kSeedFilesGroup[] = "SeedFiles_V1";
+const char kControlGroup[] = "Control_V2";
+const char kSeedFilesGroup[] = "SeedFiles_V2";
 
 // Handles reading and writing seeds to disk.
 class COMPONENT_EXPORT(VARIATIONS) SeedReaderWriter
@@ -76,6 +76,9 @@ class COMPONENT_EXPORT(VARIATIONS) SeedReaderWriter
 
   // Schedules `seed_data` to be written using `seed_writer_`.
   void ScheduleSeedFileWrite(const std::string& seed_data);
+
+  // Schedules the deletion of a seed file.
+  void DeleteSeedFile();
 
   // Pref service used to persist seeds.
   raw_ptr<PrefService> local_state_;
