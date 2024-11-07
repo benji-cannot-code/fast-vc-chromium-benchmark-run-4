610779344ee693385901fe3576e58263c3fae105
Zaina Al-Mashni <zalmashni@google.com>
2024-11-07 10:41:57 +0000

    [Gardner] Disable failing TimeTicks.LowRes test for Windows
    Bug: 377836577
    Change-Id: I2ab615848015a7ed51688b110b27329f80b4b4aa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999546
    Reviewed-by: Mariam Ali <alimariam@google.com>
    Commit-Queue: Zaina Al-Mashni <zalmashni@google.com>
    Commit-Queue: Mariam Ali <alimariam@google.com>
    Owners-Override: Zaina Al-Mashni <zalmashni@google.com>
    Auto-Submit: Zaina Al-Mashni <zalmashni@google.com>
    Cr-Commit-Position: refs/heads/main@{#1379577}

--- base/time/time_unittest.cc
+++ base/time/time_unittest.cc
@@ -1471,7 +1471,7 @@ TEST(TimeTicks, MaybeHighRes) {
 }
 #endif
 
-#if !BUILDFLAG(IS_APPLE)
+#if !BUILDFLAG(IS_APPLE) && !BUILDFLAG(IS_WIN)
 // Check that the low resolution tick value is "close" to the high resolution
 // tick value across platforms. This property holds on non-Apple platforms.
 TEST(TimeTicks, LowRes) {
