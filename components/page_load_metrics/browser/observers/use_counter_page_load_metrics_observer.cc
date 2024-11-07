5720745c5f3077a628641947dd30ea3e8b3501c3
Steinar H. Gunderson <sesse@chromium.org>
2024-11-07 11:54:02 +0000

    Add use-counter and WebDX feature mapping for trig-functions.
    Change-Id: I93be3a23b57c7c1881c40ba258872ac86ad1a6f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999271
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379602}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -509,6 +509,7 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kCSSSelectorNthChildOfSelector,
            WebDXFeature::kNthChildOf},
           {WebFeature::kForcedColorsMediaFeature, WebDXFeature::kForcedColors},
+          {WebFeature::kCSSTrigFunctions, WebDXFeature::kTrigFunctions},
       }};
 
   return *kMap;
