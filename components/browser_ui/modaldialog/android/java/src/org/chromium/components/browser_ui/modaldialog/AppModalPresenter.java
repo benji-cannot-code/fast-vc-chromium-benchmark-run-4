4aeae05c8a30553491a32627c375ea3412d74f10
Sky Malice <skym@chromium.org>
2024-11-07 23:20:10 +0000

    Clean up memory leaks in AppModalPresenter.java
    mDialogView was leaking callbacks to external caller. While the
    mWindowInsetsListener nulling is very minor.
    
    Bug: 377577352
    Change-Id: I73611ed1e671adcd8baf32b5c06851cd9de7e826
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5996770
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380013}

--- components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/AppModalPresenter.java
+++ components/browser_ui/modaldialog/android/java/src/org/chromium/components/browser_ui/modaldialog/AppModalPresenter.java
@@ -157,8 +157,10 @@ public class AppModalPresenter extends ModalDialogManager.Presenter {
         }
 
         mDialog.setOnShowListener(
-                (dialogInterface) -> {
-                    mDialogView.onEnterAnimationStarted(ENTER_ANIMATION_ESTIMATION_MS);
+                (ignored) -> {
+                    if (mDialogView != null) {
+                        mDialogView.onEnterAnimationStarted(ENTER_ANIMATION_ESTIMATION_MS);
+                    }
                 });
 
         if (onDialogCreatedCallback != null) {
@@ -181,10 +183,15 @@ public class AppModalPresenter extends ModalDialogManager.Presenter {
         }
 
         if (mDialog != null) {
+            if (mWindowInsetsListener != null) {
+                mWindowInsetsListener = null;
+                ViewCompat.setOnApplyWindowInsetsListener(
+                        getWindow().getDecorView().getRootView(), null);
+            }
             mDialog.dismiss();
             mDialog = null;
+            mDialogView = null;
             mModel = null;
-            mWindowInsetsListener = null;
         }
 
         if (mEdgeToEdgeStateSupplier != null) {
