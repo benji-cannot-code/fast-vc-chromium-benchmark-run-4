ab18b45662cfb2015f149caed572647fac07465c
Javier Contreras Tenorio (from Dev Box) <javiercon@microsoft.com>
2024-11-07 00:40:57 +0000

    [views-ax] Migrate ArcNotificationView a11y data
    This CL migrates the a11y data for ArcNotificationView from the old
    "pull" system to the new "pull" system.
    
    In this case, when the contents are not focusable we don't want to
    expose a11y data for this view. As such, we mark it as ignored.
    
    This CL is part of the ViewsAX project:
    https://docs.google.com/document/d/1Ku7HOyDsiZem1yaV6ccZ-tz3lO2XR2NEcm8HjR6d-VY/edit#heading=h.ke1u3utej413
    
    Bug: 325137417
    Change-Id: Ie84f98fe37a161715ac7754d64aa04929380f1eb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5931270
    Commit-Queue: Javier Contreras <javiercon@microsoft.com>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1379383}

--- ash/public/cpp/external_arc/message_center/arc_notification_view.cc
+++ ash/public/cpp/external_arc/message_center/arc_notification_view.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/geometry/size.h"
 #include "ui/message_center/public/cpp/message_center_constants.h"
 #include "ui/message_center/views/notification_view_base.h"
+#include "ui/views/accessibility/view_accessibility.h"
 #include "ui/views/animation/animation_builder.h"
 #include "ui/views/background.h"
 #include "ui/views/controls/button/image_button.h"
@@ -133,6 +134,10 @@ ArcNotificationView::ArcNotificationView(
         CHECK(v);
         return v->HasFocus();
       }));
+
+  // There is no a11y data for this view since it is never focused when the
+  // content view is focusable.
+  GetViewAccessibility().SetIsIgnored(true);
 }
 
 ArcNotificationView::~ArcNotificationView() {
@@ -188,16 +193,6 @@ void ArcNotificationView::UpdateBackgroundPainter() {
 
 void ArcNotificationView::UpdateControlButtonsVisibility() {
   content_view_->UpdateControlButtonsVisibility();
-}
-
-void ArcNotificationView::GetAccessibleNodeData(ui::AXNodeData* node_data) {
-  if (content_view_->IsFocusable()) {
-    // This data is never used since this view is never focused when the content
-    // view is focusable.
-    return;
-  }
-  CHECK(features::IsRenderArcNotificationsByChromeEnabled());
-  message_center::MessageView::GetAccessibleNodeData(node_data);
 }
 
 message_center::NotificationControlButtonsView*
