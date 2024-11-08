a7c1ea990400d8615aad08ae513d1df9accb5a5d
Trevor Perrier <perrier@google.com>
2024-11-08 00:22:26 +0000

    [Compose] Split out enabled countried for proactive nudge
    Fixed: b:377353604
    Change-Id: I8ef8636614fa4cd7884e1400e31518e1da00b8c2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001515
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Commit-Queue: Trevor Perrier <perrier@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380042}

--- components/compose/core/browser/config.h
+++ components/compose/core/browser/config.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/containers/flat_set.h"
 #include "base/time/time.h"
 
 namespace compose {
@@ -105,6 +106,10 @@ struct Config {
   // nudge.
   int proactive_nudge_text_change_count = 10;
 
+  // List of countries where the proactive nudge is enabled. The default value
+  // is set in the constructor.
+  base::flat_set<std::string> proactive_nudge_countries;
+
   // Whether the nudge on selection should be enabled.
   bool selection_nudge_enabled = false;
 
@@ -146,11 +151,8 @@ struct Config {
   base::TimeDelta request_latency_timeout = base::Seconds(20);
 
   // Finch-controllable list of countries where Compose should be enabled. The
-  // default value contains countries where it was already fully launched.
-  std::vector<std::string> enabled_countries = {
-      "bd", "ca", "gh", "in", "ke", "my", "ng", "ph",
-      "pk", "sg", "tz", "ug", "us", "zm", "zw",
-  };
+  // default value is set in the constructor.
+  base::flat_set<std::string> enabled_countries;
 
   // The threshold for the lifetime of a Compose session. A session whose
   // lifetime has exceeded this threshold will be replaced with a new session
