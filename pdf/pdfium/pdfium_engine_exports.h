398f55ff0b0f14eeb65092d7f72fd1c5ef274aae
Lei Zhang <thestig@chromium.org>
2024-11-08 00:05:22 +0000

    Add wrappers for FPDF_RenderPageBitmap() to pdfium_api_wrappers.h
    Move some rendering code out of pdfium_engine_exports.cc and into
    pdfium_api_wrappers.cc. Create two new wrappers:
    
    - RenderPageToBitmap() for general use.
    - RenderPageToDC() for use on Windows.
    
    Then call the new wrapper functions from pdfium_engine_exports.cc, and
    remove the RenderPDFPageToBitmap() overload in PDFiumEngineExports.
    
    This mostly just moves code around. The only addition are sanity checks
    to make sure the bitmap buffer and the HDC are not null. By reorganizing
    the code in this fashion, both test and non-test code can utilize these
    wrappers going forward, and the hierarchy of callers in //pdf always
    eventually point to pdfium_api_wrappers.h.
    
    Change-Id: Ie01be079dd4d41576bf2c0cf6af16e1b68737f07
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002606
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380033}

--- pdf/pdfium/pdfium_engine_exports.h
+++ pdf/pdfium/pdfium_engine_exports.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/chromeos_buildflags.h"
 #include "pdf/document_metadata.h"
 #include "services/screen_ai/buildflags/buildflags.h"
-#include "third_party/pdfium/public/fpdfview.h"
 #include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size.h"
 #include "ui/gfx/geometry/size_f.h"
@@ -95,9 +94,6 @@ class PDFiumEngineExports {
 #endif  // BUILDFLAG(IS_WIN)
 
   // See the definition of RenderPDFPageToBitmap in pdf.cc for details.
-  bool RenderPDFPageToBitmap(FPDF_PAGE page,
-                             const RenderingSettings& settings,
-                             void* bitmap_buffer);
   bool RenderPDFPageToBitmap(base::span<const uint8_t> pdf_buffer,
                              int page_index,
                              const RenderingSettings& settings,
