2a97005c8353fd9ded3af9df96ffb0a2b7c20c8c
Dave Tapuska <dtapuska@chromium.org>
2024-11-08 22:30:35 +0000

    [ios blink] Fix ios build failure
    The compositor_unittests were failing to link. Move the implementation
    of the interface class beside the definition.
    
    Change-Id: I0968cf3d17b4eba7e584bc64542506f87f3cd4d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6005995
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380655}

--- content/browser/renderer_host/render_widget_host_view_ios_uiview.mm
+++ content/browser/renderer_host/render_widget_host_view_ios_uiview.mm
@@ -12,12 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 static void* kObservingContext = &kObservingContext;
 
-@implementation CALayerFrameSinkProvider
-- (ui::CALayerFrameSink*)frameSink {
-  return nil;
-}
-@end
-
 @interface BETextPosition : UITextPosition {
   CGRect rect_;
 }
