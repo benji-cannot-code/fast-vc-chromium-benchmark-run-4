1fa461aa0de751f36809278f9c87c01d0609f0de
Benjamin Keen <bkeen@google.com>
2024-11-08 16:25:30 +0000

    [CodeHealth] Remove stale base::Feature PrepopulatedSearchEngineOverrideRollout
    This feature has been enabled by default since M121.
    
    Fixed: 362247135
    Change-Id: I89628e40ef31bfbbc12146fd8b97319fa9fdb538
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999120
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Benjamin Keen <bkeen@google.com>
    Cr-Commit-Position: refs/heads/main@{#1380401}

--- chrome/browser/extensions/api/settings_overrides/settings_overrides_api.cc
+++ chrome/browser/extensions/api/settings_overrides/settings_overrides_api.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <memory>
 #include <utility>
 
-#include "base/feature_list.h"
 #include "base/lazy_instance.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/prefs/session_startup_pref.h"
@@ -62,16 +61,8 @@ std::unique_ptr<TemplateURLData> ConvertSearchProvider(
     const std::string& install_parameter) {
   std::unique_ptr<TemplateURLData> data;
   if (search_provider.prepopulated_id) {
-    if (base::FeatureList::IsEnabled(
-            kPrepopulatedSearchEngineOverrideRollout)) {
-      data = TemplateURLPrepopulateData::GetPrepopulatedEngineFromFullList(
-          prefs, search_engine_choice_service,
-          *search_provider.prepopulated_id);
-    } else {
-      data = TemplateURLPrepopulateData::GetPrepopulatedEngine(
-          prefs, search_engine_choice_service,
-          *search_provider.prepopulated_id);
-    }
+    data = TemplateURLPrepopulateData::GetPrepopulatedEngineFromFullList(
+        prefs, search_engine_choice_service, *search_provider.prepopulated_id);
 
     if (data) {
       // We need to override the prepopulate_id and Sync GUID of the generated
@@ -141,13 +132,6 @@ std::unique_ptr<TemplateURLData> ConvertSearchProvider(
 }
 
 }  // namespace
-
-// Kill-switch for the updated logic to fetch the prepopulated search engine
-// for settings override.
-// Exposed for tests. To be removed in M122.
-BASE_FEATURE(kPrepopulatedSearchEngineOverrideRollout,
-             "PrepopulatedSearchEngineOverrideRollout",
-             base::FEATURE_ENABLED_BY_DEFAULT);
 
 SettingsOverridesAPI::SettingsOverridesAPI(content::BrowserContext* context)
     : profile_(Profile::FromBrowserContext(context)),
--- chrome/browser/extensions/api/settings_overrides/settings_overrides_api.h
+++ chrome/browser/extensions/api/settings_overrides/settings_overrides_api.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 #include <string>
 
-#include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "base/scoped_observation.h"
 #include "components/search_engines/template_url_service.h"
@@ -22,8 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class Profile;
 
 namespace extensions {
-
-BASE_DECLARE_FEATURE(kPrepopulatedSearchEngineOverrideRollout);
 
 class SettingsOverridesAPI : public BrowserContextKeyedAPI,
                              public ExtensionRegistryObserver {
