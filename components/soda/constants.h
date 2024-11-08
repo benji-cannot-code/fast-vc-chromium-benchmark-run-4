e1ed1a8433073bfdb09cb4eb58d3adbfa7f84fbd
Adam Rice <ricea@chromium.org>
2024-11-08 05:28:39 +0000

    Make speech::kLanguageComponentConfigs inline
    The speech::kLanguageComponentConfigs constant was marked as `constexpr`
    in components/soda/constants.h but was not marked as `inline`. This
    resulted in it being included 5 times in the chrome binary.
    
    Mark it inline.
    
    Change-Id: Iebc1cf28d8a797ebfec478c19775b8eeb281e662
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002244
    Reviewed-by: Abigail Klein <abigailbklein@google.com>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380166}

--- components/soda/constants.h
+++ components/soda/constants.h
@@ -88,7 +88,7 @@ struct SodaLanguagePackComponentConfig {
   const uint8_t public_key_sha[32] = {};
 };
 
-constexpr SodaLanguagePackComponentConfig kLanguageComponentConfigs[] = {
+inline constexpr SodaLanguagePackComponentConfig kLanguageComponentConfigs[] = {
     {LanguageCode::kEnUs,
      "en-US",
      prefs::kSodaEnUsConfigPath,
