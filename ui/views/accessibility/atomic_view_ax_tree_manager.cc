7e7f769df626f9b7fe9173f6cac6eb41c6d1b1b0
Kurt Catti-Schmidt <kschmi@microsoft.com>
2024-11-08 03:22:33 +0000

    [a11y] Remove const on value parameters in AXTreeManager and related
    According to the Google C++ Coding Guidelines, value params are not
    supposed to be const. A few methods in AXTreeManager and
    AXPlatformTreeManager were not obeying this guideline, which this CL
    addresses.
    
    There was also an organization/ordering issue in
    atomic_view_ax_tree_manager.cc/h addressed.
    
    Change-Id: I26b81dcdd7a9fbea5a277af3fac428ce1bf4cac1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001156
    Reviewed-by: Jacques Newman <janewman@microsoft.com>
    Commit-Queue: Kurt Catti-Schmidt <kschmi@microsoft.com>
    Cr-Commit-Position: refs/heads/main@{#1380127}

--- ui/views/accessibility/atomic_view_ax_tree_manager.cc
+++ ui/views/accessibility/atomic_view_ax_tree_manager.cc
@@ -55,7 +55,7 @@ bool AtomicViewAXTreeManager::IsView() const {
   return true;
 }
 
-ui::AXNode* AtomicViewAXTreeManager::GetNode(const ui::AXNodeID node_id) const {
+ui::AXNode* AtomicViewAXTreeManager::GetNode(ui::AXNodeID node_id) const {
   // This here is the key to the whole thing. The AtomicViewAXTreeManager is
   // fetching and updating the AXNodeData from the View itself whenever this
   // function gets called.
@@ -64,20 +64,6 @@ ui::AXNode* AtomicViewAXTreeManager::GetNode(const ui::AXNodeID node_id) const {
       << "The AtomicViewAXTreeManager should only allow callers to get the "
          "root node as it is the only managed node.";
   return ax_tree_->root();
-}
-
-ui::AXPlatformNode* AtomicViewAXTreeManager::GetPlatformNodeFromTree(
-    const ui::AXNodeID node_id) const {
-  return delegate_->GetFromNodeID(node_id);
-}
-
-ui::AXPlatformNode* AtomicViewAXTreeManager::GetPlatformNodeFromTree(
-    const ui::AXNode& node) const {
-  return GetPlatformNodeFromTree(node.id());
-}
-
-ui::AXPlatformNodeDelegate* AtomicViewAXTreeManager::RootDelegate() const {
-  return delegate_;
 }
 
 ui::AXTreeID AtomicViewAXTreeManager::GetParentTreeID() const {
@@ -95,6 +81,20 @@ ui::AXNode* AtomicViewAXTreeManager::GetRoot() const {
 
 ui::AXNode* AtomicViewAXTreeManager::GetParentNodeFromParentTree() const {
   return nullptr;
+}
+
+ui::AXPlatformNode* AtomicViewAXTreeManager::GetPlatformNodeFromTree(
+    ui::AXNodeID node_id) const {
+  return delegate_->GetFromNodeID(node_id);
+}
+
+ui::AXPlatformNode* AtomicViewAXTreeManager::GetPlatformNodeFromTree(
+    const ui::AXNode& node) const {
+  return GetPlatformNodeFromTree(node.id());
+}
+
+ui::AXPlatformNodeDelegate* AtomicViewAXTreeManager::RootDelegate() const {
+  return delegate_;
 }
 
 void AtomicViewAXTreeManager::ClearComputedRootData() {
