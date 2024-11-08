0182525fda95fc0dabbe72e3532dbcc401875926
apoorvaverma <apoorvaverma@google.com>
2024-11-08 16:23:30 +0000

    Add metadata.json file in WebView components directory
    This file is currently just being used to store Omaha cohort IDs.
    Implemented as discussed in go/wv-cohort-hash. Tested locally to
    check the files are created in the correct directory and populated
    as expected.
    
    e.g. running the following command in adb shell
    cat data/user/0/com.google.android.webview.debug/
           app_webview/components/cps/
           cffplpkejcbdpfnfabnjikeicbedmifn/1_2023.7.28.7/metadata.json
    
    returns
    {"cohortId":"1:1vi9:"}
    
    A follow up CL will read this file and use it to populate the cohort
    hash in UMA logs.
    
    Change-Id: I0d7ca46cad38539ef8463705d771dade88758e9d
    Bug: 338547250
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5920443
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Commit-Queue: Apoorva Verma <apoorvaverma@google.com>
    Cr-Commit-Position: refs/heads/main@{#1380398}

--- android_webview/nonembedded/component_updater/aw_component_installer_policy.h
+++ android_webview/nonembedded/component_updater/aw_component_installer_policy.h
@@ -31,10 +31,11 @@ class AwComponentInstallerPolicy
   void OnCustomUninstall() final;
 
  private:
-  base::FilePath GetComponentsProviderServiceDirectory();
 
   // Virtual for testing.
   virtual void IncrementComponentsUpdatedCount();
+  virtual base::FilePath GetComponentsProviderServiceDirectory();
+  virtual int GetHighestSequenceNumber(base::FilePath cps_component_base_path);
 };
 
 }  // namespace android_webview
--- android_webview/nonembedded/component_updater/aw_component_update_service.h
+++ android_webview/nonembedded/component_updater/aw_component_update_service.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 class TimeTicks;
+class FilePath;
 }
 
 namespace component_updater {
@@ -53,6 +54,13 @@ class AwComponentUpdateService {
   void CheckForUpdates(UpdateCallback on_finished, bool on_demand_update);
 
   void IncrementComponentsUpdatedCount();
+
+  // Gets the highest sequence number for a given component path.
+  int GetHighestSequenceNumber(base::FilePath cps_component_base_path);
+
+  // Returns the directory path for a given component hash.
+  base::FilePath GetComponentsProviderServiceDirectory(
+      const std::vector<uint8_t>& hash);
 
  private:
   SEQUENCE_CHECKER(sequence_checker_);
@@ -91,6 +99,7 @@ class AwComponentUpdateService {
                                   base::OnceClosure on_finished);
 
   scoped_refptr<update_client::UpdateClient> update_client_;
+  scoped_refptr<update_client::Configurator> configurator_;
 
   // A collection of every registered component.
   base::flat_map<std::string, component_updater::ComponentRegistration>
@@ -105,6 +114,14 @@ class AwComponentUpdateService {
 
   void RecordComponentsUpdated(UpdateCallback on_finished,
                                update_client::Error error);
+
+  void UpdateMetadataFiles(UpdateCallback on_finished, int32_t count_of_comps);
+
+  // Gets the highest sequence number directory for a given component path.
+  std::string GetHighestSequenceNumberDirectory(
+      base::FilePath cps_component_base_path);
+
+  std::string GetCohortId(const std::string& component_id);
 
   // Counts how many components were updated, for UMA logging.
   int32_t components_updated_count_ = 0;
