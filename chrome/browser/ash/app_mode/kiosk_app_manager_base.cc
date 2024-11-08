f8d0826e58fe51ba19aaf8897408fa1d751dcef2
Sergii Bykov <sbykov@google.com>
2024-11-08 15:15:02 +0000

    kiosk: Impl removing old apps in IWA manager
    Bug: 361017701
    Change-Id: I5194edcd712732126f5980216a4bdd7eb7e0c108
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999535
    Commit-Queue: Sergii Bykov <sbykov@google.com>
    Reviewed-by: Polina Bondarenko <pbond@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380368}

--- chrome/browser/ash/app_mode/kiosk_app_manager_base.cc
+++ chrome/browser/ash/app_mode/kiosk_app_manager_base.cc
@@ -5,11 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ash/app_mode/kiosk_app_manager_base.h"
 
-#include <map>
-#include <utility>
+#include <string>
+#include <vector>
 
+#include "base/check.h"
 #include "base/files/file_path.h"
 #include "base/functional/bind.h"
+#include "base/notreached.h"
 #include "base/path_service.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_data_base.h"
 #include "chrome/browser/ash/app_mode/kiosk_app_manager_observer.h"
@@ -17,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/policy/core/device_local_account.h"
 #include "chrome/common/chrome_paths.h"
 #include "chromeos/ash/components/settings/cros_settings_names.h"
+#include "components/account_id/account_id.h"
 
 namespace ash {
 
@@ -97,6 +100,12 @@ void KioskAppManagerBase::NotifySessionInitialized() const {
   }
 }
 
+void KioskAppManagerBase::NotifyAppRemoved(const std::string& app_id) const {
+  for (auto& observer : observers_) {
+    observer.OnKioskAppDataRemoved(app_id);
+  }
+}
+
 void KioskAppManagerBase::AddObserver(KioskAppManagerObserver* observer) {
   observers_.AddObserver(observer);
 }
@@ -106,7 +115,7 @@ void KioskAppManagerBase::RemoveObserver(KioskAppManagerObserver* observer) {
 }
 
 void KioskAppManagerBase::ClearRemovedApps(
-    const std::vector<KioskAppDataBase*>& old_apps) {
+    const std::vector<KioskAppDataBase*>& old_apps) const {
   std::vector<AccountId> account_ids_to_remove;
   account_ids_to_remove.reserve(old_apps.size());
   for (KioskAppDataBase* entry : old_apps) {
@@ -115,6 +124,9 @@ void KioskAppManagerBase::ClearRemovedApps(
   }
   KioskCryptohomeRemover::RemoveCryptohomesAndExitIfNeeded(
       account_ids_to_remove);
+  for (const KioskAppDataBase* entry : old_apps) {
+    NotifyAppRemoved(entry->app_id());
+  }
 }
 
 }  // namespace ash
