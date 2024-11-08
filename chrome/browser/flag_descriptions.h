726812d24f562bab8055de2cca63b16a094a9571
Keishi Hattori <keishi@chromium.org>
2024-11-08 03:56:05 +0000

    Retry: Add PartitionAllocMemoryTagging to chrome://flags
    This contains the reland of crrev.com/c/5997853,
    the fix change to chrome/browser/flag-metadata.json,
    and crrev.com/c/6002452
    
    Bug: 372508438
    Change-Id: Ib176ff34c4116622d84e23a23c52fbf6f7bfc979
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002696
    Reviewed-by: Takashi Sakamoto <tasak@google.com>
    Commit-Queue: Keishi Hattori <keishi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380135}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1670,6 +1670,9 @@ extern const char kPageVisibilityPageContentAnnotationsDescription[];
 extern const char kParallelDownloadingName[];
 extern const char kParallelDownloadingDescription[];
 
+extern const char kPartitionAllocMemoryTaggingName[];
+extern const char kPartitionAllocMemoryTaggingDescription[];
+
 extern const char kPartitionAllocWithAdvancedChecksName[];
 extern const char kPartitionAllocWithAdvancedChecksDescription[];
 
