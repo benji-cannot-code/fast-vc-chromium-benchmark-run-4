9af80c0148212c233aeebe41754afa76d5d2bb9f
Steinar H. Gunderson <sesse@chromium.org>
2024-11-08 10:48:19 +0000

    Add use-counter and WebDX feature mapping for color-function.
    Change-Id: Ia0a342cbe9c7f9dbccb565fe984b5d93420f9871
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999891
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380256}

--- third_party/blink/renderer/core/css/properties/css_color_function_parser.cc
+++ third_party/blink/renderer/core/css/properties/css_color_function_parser.cc
@@ -525,6 +525,10 @@ CSSValue* ColorFunctionParser::ConsumeFunctionalSyntaxColor(
     return nullptr;
   }
 
+  if (function_id == CSSValueID::kColor) {
+    context.Count(WebFeature::kCSSColorFunction);
+  }
+
   std::optional<Color> resolved_color;
   bool has_alpha = false;
   {
