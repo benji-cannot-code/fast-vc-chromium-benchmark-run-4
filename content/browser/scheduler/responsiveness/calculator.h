dbd83cc55a6b75d296a9be34ed7bedac1ebbda6d
Etienne Pierre-doray <etiennep@chromium.org>
2024-11-08 13:20:38 +0000

    [tracing] Refactor congestion events trace events
    - Splits Initial and Periodic intervals
    - Emit both RunningOnly and QueueingAndRunning
    - Use perfetto::NamedTrack
    
    Bug: 374302723
    Change-Id: I49dd59589d77ee836ff1a664f16d63ac2e4da482
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000654
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380321}

--- content/browser/scheduler/responsiveness/calculator.h
+++ content/browser/scheduler/responsiveness/calculator.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 #include "content/common/content_export.h"
 #include "content/public/browser/responsiveness_calculator_delegate.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 #if BUILDFLAG(IS_ANDROID)
 #include "base/android/application_status_listener.h"
@@ -105,18 +106,20 @@ class CONTENT_EXPORT Calculator {
   // the specific congested slices.
   // Exposed for testing.
   void EmitResponsivenessTraceEvents(CongestionType congestion_type,
+                                     StartupStage startup_stage,
                                      base::TimeTicks start_time,
                                      base::TimeTicks end_time,
                                      const std::set<int>& congested_slices);
 
   // Exposed for testing.
   virtual void EmitCongestedIntervalsMeasurementTraceEvent(
+      StartupStage startup_stage,
       base::TimeTicks start_time,
-      base::TimeTicks end_time,
-      size_t amount_of_slices);
+      base::TimeTicks end_time);
 
   // Exposed for testing.
-  virtual void EmitCongestedIntervalTraceEvent(base::TimeTicks start_time,
+  virtual void EmitCongestedIntervalTraceEvent(CongestionType congestion_type,
+                                               base::TimeTicks start_time,
                                                base::TimeTicks end_time);
 
   // Exposed for testing.
@@ -215,6 +218,8 @@ class CONTENT_EXPORT Calculator {
 
   // Used to record embedder-specific responsiveness metrics.
   std::unique_ptr<ResponsivenessCalculatorDelegate> delegate_;
+
+  perfetto::NamedTrack congestion_track_;
 
 #if BUILDFLAG(IS_ANDROID)
   // Listener for changes in application state, unregisters itself when
