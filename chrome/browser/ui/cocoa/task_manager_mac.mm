bd8ab6dda8c3e917cfac89939a8deb003a020a37
Michael Wojcicka <mwoj@google.com>
2024-11-08 01:08:31 +0000

    Use MacViews for Task Manager Desktop Refresh
    - When the TaskManagerDesktopRefresh flag is enabled, the macOS Chrome
      Task Manager will use Views instead of the Cocoa implementation.
    
    Bug: https://crbug.com/375063408
    Change-Id: Ie5561fce29a34ec209c25972384d8390dd227068
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5958667
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Michael Wojcicka <mwoj@google.com>
    Cr-Commit-Position: refs/heads/main@{#1380077}

--- chrome/browser/ui/cocoa/task_manager_mac.mm
+++ chrome/browser/ui/cocoa/task_manager_mac.mm
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/buildflag.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/lifetime/termination_notification.h"
+#include "chrome/browser/task_manager/common/task_manager_features.h"
 #include "chrome/browser/task_manager/task_manager_interface.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
@@ -807,11 +808,15 @@ namespace chrome {
 
 // Declared in browser_dialogs.h.
 task_manager::TaskManagerTableModel* ShowTaskManager(Browser* browser) {
-  return task_manager::TaskManagerMac::Show();
+  return base::FeatureList::IsEnabled(features::kTaskManagerDesktopRefresh)
+             ? ShowTaskManagerViews(browser)
+             : task_manager::TaskManagerMac::Show();
 }
 
 void HideTaskManager() {
-  task_manager::TaskManagerMac::Hide();
+  base::FeatureList::IsEnabled(features::kTaskManagerDesktopRefresh)
+      ? HideTaskManagerViews()
+      : task_manager::TaskManagerMac::Hide();
 }
 
 }  // namespace chrome
