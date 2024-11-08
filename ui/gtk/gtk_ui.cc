6c05168cd1c1dc24df04d83b5621eac11663087e
Kalvin Lee <kdlee@chromium.org>
2024-11-08 05:26:03 +0000

    spanify: Array-ify `ui::writing_system_key_domcodes`
    This CL is trivial. Because `spanify` wants to turn this C-style array
    into a `std::array` but is unable to determine its size, this CL fixes
    it up manually. It
    
    *   makes the array an `inline constexpr std::array`, consistent with
        https://abseil.io/tips/168.
    
    *   renames it in compliance with the style guide.
    
    *   reworks all usages to simply use ranged `for`, which incidentally
        more or less obviates the need for spanification.
    
    Bug: 364338808
    Change-Id: Iabf6bf1de8b0e310fea95bd82ad01190ca92b6c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000580
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380165}

--- ui/gtk/gtk_ui.cc
+++ ui/gtk/gtk_ui.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/color/color_provider_manager.h"
 #include "ui/display/display.h"
 #include "ui/events/keycodes/dom/dom_code.h"
+#include "ui/events/keycodes/dom/dom_keyboard_layout.h"
 #include "ui/events/keycodes/dom/dom_keyboard_layout_manager.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
 #include "ui/gfx/animation/animation.h"
@@ -616,9 +617,7 @@ base::flat_map<std::string, std::string> GtkUi::GetKeyboardLayoutMap() {
   auto layouts = std::make_unique<ui::DomKeyboardLayoutManager>();
   auto map = base::flat_map<std::string, std::string>();
 
-  for (unsigned int i_domcode = 0;
-       i_domcode < ui::kWritingSystemKeyDomCodeEntries; ++i_domcode) {
-    ui::DomCode domcode = ui::writing_system_key_domcodes[i_domcode];
+  for (const ui::DomCode domcode : ui::kWritingSystemKeyDomCodes) {
     guint16 keycode = ui::KeycodeConverter::DomCodeToNativeKeycode(domcode);
     GdkKeymapKey* keys = nullptr;
     guint* keyvals = nullptr;
