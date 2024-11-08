3a91e894ab1e23d04ef3cd5ed3a2183523b42e6f
Sylvain Defresne <sdefresne@chromium.org>
2024-11-08 11:36:26 +0000

    Reland "Reland "[ios] Move part of Profile deferred init to ProfileController""
    This is a reland of commit f17d9a31b20c1e2fa0b98a8e0c3be3387161e820
    
    The CL was reverted because it failed `gn check` when the CPE is
    disabled (i.e. ios_enable_credential_provider_extension=false) which
    is only the case when building for macOS catalyst.
    
    Fix the `gn check` failure by adding `// nogncheck` on the files that
    are included conditionally (since `gn check` does not understand the
    pre-processor #if directives).
    
    Original change's description:
    > Reland "[ios] Move part of Profile deferred init to ProfileController"
    >
    > This is a reland of commit 686edbcf2509644d0f0a5a19c31e14d3da542e10
    >
    > The original CLs was reverted because it did not compiler on device
    > (which does not happen on CQ). This version fixes the typos in the
    > code that is only compiled for devices (introduced when introducing
    > an helper function).
    >
    > Original change's description:
    > > [ios] Move part of Profile deferred init to ProfileController
    > >
    > > Move some of the Profile deferred initialization from MainController
    > > to ProfileController. This ensure the code is run even for Profiles
    > > that are loaded dynamically after the MainController is initialized.
    > >
    > > Bug: 353683675, 40769058, 333863468
    > > Change-Id: Ie22a5e2b68f127da6ace5715abd2ac1d8157b42a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999921
    > > Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    > > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1379635}
    >
    > Bug: 353683675, 40769058, 333863468
    > Change-Id: I75008ce46788e94233d2402c90a2d4179d97cc89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999324
    > Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1380223}
    
    Cq-Include-Trybots: luci.chromium.try:ios-catalyst
    Bug: 353683675, 40769058, 333863468
    Change-Id: I67e6ab931dba2d3af22740d43a4c4248136a2020
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003470
    Reviewed-by: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380279}

--- ios/chrome/app/profile/application_storage_metrics.mm
+++ ios/chrome/app/profile/application_storage_metrics.mm
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/app/application_storage_metrics.h"
+#import "ios/chrome/app/profile/application_storage_metrics.h"
 
 #import <Foundation/Foundation.h>
 
---
+++ ios/chrome/app/profile/features.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2023 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_PROFILE_FEATURES_H_
+#define IOS_CHROME_APP_PROFILE_FEATURES_H_
+
+#import "base/feature_list.h"
+
+BASE_DECLARE_FEATURE(kLogApplicationStorageSizeMetrics);
+
+#endif  // IOS_CHROME_APP_PROFILE_FEATURES_H_
