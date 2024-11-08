c0f71532e83c48f7404d045c14987ee7a8b96451
Vasiliy Telezhnikov <vasilyt@chromium.org>
2024-11-08 15:24:06 +0000

    Remove texture target from RasterInterface::CopySharedImage
    The param is no longer used.
    
    Change-Id: I8c673a856c4b767f872e8c9b17d534e9da106441
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979551
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Jeffrey Kardatzke <jkardatzke@google.com>
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Kramer Ge <fangzhoug@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380371}

--- third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
+++ third_party/blink/renderer/platform/graphics/gpu/drawing_buffer.h
@@ -284,8 +284,6 @@ class PLATFORM_EXPORT DrawingBuffer : public cc::TextureLayerClient,
 
   bool CopyToPlatformMailbox(gpu::raster::RasterInterface*,
                              gpu::Mailbox dst_mailbox,
-                             GLenum dst_texture_target,
-                             bool flip_y,
                              const gfx::Point& dst_texture_offset,
                              const gfx::Rect& src_sub_rectangle,
                              SourceDrawingBuffer src_buffer);
@@ -293,7 +291,6 @@ class PLATFORM_EXPORT DrawingBuffer : public cc::TextureLayerClient,
   bool CopyToVideoFrame(
       WebGraphicsContext3DVideoFramePool* frame_pool,
       SourceDrawingBuffer src_buffer,
-      bool src_origin_is_top_left,
       const gfx::ColorSpace& dst_color_space,
       WebGraphicsContext3DVideoFramePool::FrameReadyCallback callback);
 
