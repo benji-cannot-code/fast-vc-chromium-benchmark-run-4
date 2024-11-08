e5ff3f7dddd0edf797663b42960cea6ccd2382a2
William Liu <liuwilliam@chromium.org>
2024-11-08 19:41:54 +0000

    [DNT] Dump state transfer and the navigation request when crashing
    Change-Id: Id544dbd88c7a41dc08142dfc926510fd226999cf
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000399
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380544}

--- content/browser/navigation_transitions/back_forward_transition_animator.cc
+++ content/browser/navigation_transitions/back_forward_transition_animator.cc
@@ -325,6 +325,10 @@ static constexpr float kFloatTolerance = 0.001f;
   return a > b || AlmostEqual(a, b);
 }
 
+#define CREATE_SCOPED_CRASH_KEYS()                                          \
+  SCOPED_CRASH_KEY_STRING1024("DNT", "States", serialized_states_.c_str()); \
+  SCOPED_CRASH_KEY_STRING1024("DNT", "Request", serialized_request_.c_str());
+
 }  // namespace
 
 std::unique_ptr<BackForwardTransitionAnimator>
@@ -344,6 +348,7 @@ BackForwardTransitionAnimator::Factory::Create(
 }
 
 BackForwardTransitionAnimator::~BackForwardTransitionAnimator() {
+  CREATE_SCOPED_CRASH_KEYS();
   TRACE_EVENT("browser,navigation",
               "BackForwardTransitionAnimator::~BackForwardTransitionAnimator");
 
@@ -478,12 +483,14 @@ void BackForwardTransitionAnimator::OnGestureProgressed(
 }
 
 void BackForwardTransitionAnimator::OnGestureCancelled() {
+  CREATE_SCOPED_CRASH_KEYS();
   CHECK_EQ(state_, State::kStarted);
   StartInputSuppression(IgnoringInputReason::kAnimationCanceledOccurred);
   AdvanceAndProcessState(State::kDisplayingCancelAnimation);
 }
 
 void BackForwardTransitionAnimator::OnGestureInvoked() {
+  CREATE_SCOPED_CRASH_KEYS();
   CHECK_EQ(state_, State::kStarted);
 
   StartInputSuppression(IgnoringInputReason::kAnimationInvokedOccurred);
@@ -513,6 +520,7 @@ void BackForwardTransitionAnimator::OnGestureInvoked() {
 }
 
 void BackForwardTransitionAnimator::OnContentForNavigationEntryShown() {
+  CREATE_SCOPED_CRASH_KEYS();
   // Might be called multiple times if user swipes again before NTP fade
   // has finished.
   if (state_ != State::kWaitingForContentForNavigationEntryShown) {
@@ -626,6 +634,7 @@ void BackForwardTransitionAnimator::OnAnimate(
 
 void BackForwardTransitionAnimator::OnRenderWidgetHostDestroyed(
     RenderWidgetHost* widget_host) {
+  CREATE_SCOPED_CRASH_KEYS();
   if (widget_host != new_render_widget_host_) {
     return;
   }
@@ -641,6 +650,9 @@ void BackForwardTransitionAnimator::OnRenderWidgetHostDestroyed(
 // cancelled.
 void BackForwardTransitionAnimator::OnRenderFrameMetadataChangedAfterActivation(
     base::TimeTicks activation_time) {
+  AppendToSerializeStates("FrameMetadataChanged");
+  CREATE_SCOPED_CRASH_KEYS();
+
   CHECK(tracked_request_);
   // We shouldn't get this notification for subframe navigations because we
   // never subscribe to the `RenderWidgetHost` for subframes.
@@ -704,6 +716,11 @@ void BackForwardTransitionAnimator::DidStartNavigation(
   TRACE_EVENT("browser,navigation",
               "BackForwardTransitionAnimator::DidStartNavigation",
               "navigation_id", navigation_handle->GetNavigationId());
+  AppendToSerializeStates(
+      "DidStartNav " +
+      base::NumberToString(navigation_handle->GetNavigationId()));
+  CREATE_SCOPED_CRASH_KEYS();
+
   // We need to set this state here since for same-document navigations, the
   // commit message is sent before the animator starts tracking the navigation.
   if (is_starting_navigation_) {
@@ -754,6 +771,10 @@ void BackForwardTransitionAnimator::ReadyToCommitNavigation(
   TRACE_EVENT("browser,navigation",
               "BackForwardTransitionAnimator::ReadyToCommitNavigation",
               "navigation_id", navigation_handle->GetNavigationId());
+  AppendToSerializeStates(
+      "ReadyToCommitNav " +
+      base::NumberToString(navigation_handle->GetNavigationId()));
+  CREATE_SCOPED_CRASH_KEYS();
 
   CHECK(!navigation_handle->IsSameDocument());
 
@@ -806,6 +827,11 @@ void BackForwardTransitionAnimator::DidFinishNavigation(
   TRACE_EVENT("browser,navigation",
               "BackForwardTransitionAnimator::DidFinishNavigation",
               "navigation_id", navigation_handle->GetNavigationId());
+  AppendToSerializeStates(
+      "DidFinishNav " +
+      base::NumberToString(navigation_handle->GetNavigationId()));
+  CREATE_SCOPED_CRASH_KEYS();
+
   // If we haven't started tracking a navigation, or if `navigation_handle`
   // isn't what we tracked, or if this `navigation_handle` has committed, ignore
   // it.
@@ -862,6 +888,10 @@ void BackForwardTransitionAnimator::OnDidNavigatePrimaryMainFramePreCommit(
   TRACE_EVENT(
       "browser,navigation",
       "BackForwardTransitionAnimator::OnDidNavigatePrimaryMainFramePreCommit");
+  AppendToSerializeStates(
+      "PreCommit " +
+      base::NumberToString(navigation_request->GetNavigationId()));
+  CREATE_SCOPED_CRASH_KEYS();
 
   // If a navigation commits in the primary main frame while we are tracking the
   // subframe requests, abort the animation immediately.
@@ -1004,6 +1034,11 @@ void BackForwardTransitionAnimator::OnDidNavigatePrimaryMainFramePreCommit(
 
 void BackForwardTransitionAnimator::OnNavigationCancelledBeforeStart(
     NavigationHandle* navigation_handle) {
+  AppendToSerializeStates(
+      "CancelledBeforeStart " +
+      base::NumberToString(navigation_handle->GetNavigationId()));
+  CREATE_SCOPED_CRASH_KEYS();
+
   if (!tracked_request_ ||
       tracked_request_->navigation_id != navigation_handle->GetNavigationId()) {
     // A unrelated request is cancelled before start.
@@ -1108,6 +1143,9 @@ void BackForwardTransitionAnimator::MaybeRecordIgnoredInput(
 
 void BackForwardTransitionAnimator::OnBeforeUnloadDialogShown(
     int64_t navigation_id) {
+  AppendToSerializeStates("BUShown " + base::NumberToString(navigation_id));
+  CREATE_SCOPED_CRASH_KEYS();
+
   if (!tracked_request_ || tracked_request_->navigation_id != navigation_id) {
     return;
   }
@@ -1126,6 +1164,7 @@ void BackForwardTransitionAnimator::AbortAnimation(
               "BackForwardTransitionAnimator::AbortAnimation", "abort_reason",
               AnimationAbortReasonToString(abort_reason));
   base::UmaHistogramEnumeration(kAnimationAbortedReason, abort_reason);
+  abort_reason_ = abort_reason;
   AdvanceAndProcessState(State::kAnimationAborted);
 }
 
@@ -1194,6 +1233,7 @@ void BackForwardTransitionAnimator::OnTransformAnimated(
 }
 
 void BackForwardTransitionAnimator::OnCancelAnimationDisplayed() {
+  CREATE_SCOPED_CRASH_KEYS();
   if (navigation_state_ == NavigationState::kBeforeUnloadDispatched) {
     if (effect_.keyframe_models().empty()) {
       // http://crbug.com/377341853: We occasionally exhaust the scrim model and
@@ -1214,6 +1254,7 @@ void BackForwardTransitionAnimator::OnCancelAnimationDisplayed() {
 }
 
 void BackForwardTransitionAnimator::OnInvokeAnimationDisplayed() {
+  CREATE_SCOPED_CRASH_KEYS();
   ResetLiveOverlayLayer();
 
   if (progress_bar_) {
@@ -1235,6 +1276,7 @@ void BackForwardTransitionAnimator::OnInvokeAnimationDisplayed() {
 }
 
 void BackForwardTransitionAnimator::OnCrossFadeAnimationDisplayed() {
+  CREATE_SCOPED_CRASH_KEYS();
   CHECK(effect_.keyframe_models().empty());
   AdvanceAndProcessState(State::kAnimationFinished);
 }
@@ -1364,6 +1406,7 @@ void BackForwardTransitionAnimator::
 }
 
 void BackForwardTransitionAnimator::InitializeEffectForCrossfadeAnimation() {
+  CREATE_SCOPED_CRASH_KEYS();
   // Before we add the cross-fade model, the scrim model must have finished.
   CHECK(effect_.keyframe_models().empty());
 
@@ -1386,10 +1429,12 @@ void BackForwardTransitionAnimator::AdvanceAndProcessState(State state) {
   if (previous_animation_stage != GetCurrentAnimationStage()) {
     animation_manager_->OnAnimationStageChanged();
   }
+  AppendToSerializeStates(FormatStateAndNavigationState());
   ProcessState();
 }
 
 void BackForwardTransitionAnimator::ProcessState() {
+  CREATE_SCOPED_CRASH_KEYS();
   switch (state_) {
     case State::kStarted: {
       DeferDialogs();
@@ -1698,6 +1743,7 @@ void BackForwardTransitionAnimator::TrackRequest(
       .navigation_id = created_request->GetNavigationId(),
       .is_primary_main_frame = created_request->IsInPrimaryMainFrame(),
   };
+  SerializeNavigationRequest(created_request.get());
 
   if (created_request->IsNavigationStarted()) {
     navigation_state_ = NavigationState::kStarted;
@@ -1864,6 +1910,7 @@ void BackForwardTransitionAnimator::SetUpEmbedderContentLayerIfNeeded(
 // `OnRenderFrameMetadataChangedAfterActivation` to the manager
 void BackForwardTransitionAnimator::SubscribeToNewRenderWidgetHost(
     NavigationRequest* navigation_request) {
+  CREATE_SCOPED_CRASH_KEYS();
   CHECK(!new_render_widget_host_);
 
   if (!navigation_request->GetNavigationEntry()) {
@@ -2047,12 +2094,14 @@ void BackForwardTransitionAnimator::InsertLayersInOrder() {
 }
 
 void BackForwardTransitionAnimator::OnPostNavigationFirstFrameTimeout() {
+  AppendToSerializeStates("ScreenshotTimeout");
   CHECK_EQ(state_, State::kWaitingForNewRendererToDraw);
   CHECK_EQ(navigation_state_, NavigationState::kCommitted);
   PostNavigationFirstFrameActivated();
 }
 
 void BackForwardTransitionAnimator::PostNavigationFirstFrameActivated() {
+  AppendToSerializeStates("PostNavigationFirstFrameActivated");
   if (viz_has_activated_first_frame_) {
     // Viz has already activated the first frame post-navigation and has already
     // notified the browser.
@@ -2231,5 +2280,41 @@ void BackForwardTransitionAnimator::ResumeDialogs() {
   }
   deferred_dialog_token_ = ui::ModalDialogManagerBridge::kInvalidDialogToken;
 }
+
+void BackForwardTransitionAnimator::AppendToSerializeStates(
+    const std::string& state) {
+  if (!serialized_states_.empty()) {
+    serialized_states_.append(" ");
+  }
+  serialized_states_.append(state);
+}
+
+std::string BackForwardTransitionAnimator::FormatStateAndNavigationState()
+    const {
+  std::stringstream value;
+  value << StateToString(state_) << "("
+        << NavigationStateToString(navigation_state_);
+  if (state_ == State::kAnimationAborted && abort_reason_.has_value()) {
+    value << "," << AnimationAbortReasonToString(abort_reason_.value());
+  }
+  value << ")";
+  return value.str();
+}
+
+void BackForwardTransitionAnimator::SerializeNavigationRequest(
+    NavigationRequest* request) {
+  std::stringstream value;
+  value << "Id " << request->GetNavigationId() << " PrimaryMain "
+        << request->IsInPrimaryMainFrame() << " State "
+        << static_cast<int>(request->state());
+  if (auto* current_rfh = RenderFrameHostImpl::FromID(
+          request->GetPreviousRenderFrameHostId())) {
+    value << " from " << current_rfh->GetLastCommittedURL();
+  }
+  value << " to " << request->GetURL();
+  serialized_request_ = value.str();
+}
+
+#undef CREATE_SCOPED_CRASH_KEYS
 
 }  // namespace content
