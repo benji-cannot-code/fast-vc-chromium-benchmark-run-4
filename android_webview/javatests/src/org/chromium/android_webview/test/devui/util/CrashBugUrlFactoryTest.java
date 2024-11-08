02bbc47d77413eaa15f6555455f24ac5001232d8
Nate Fischer <ntfschr@chromium.org>
2024-11-08 10:38:12 +0000

    AW: fix up the user-reported crash template
    No change to logic. This fixes a few issues I noticed for the user
    reported crashes:
    
    * Remove a redundant double-quote character.
    * Give a generic default for the title (users can still replace the
      title).
    * Document the supported query parameters.
    
    Test: run_webview_instrumentation_test_apk -f CrashBugUrlFactoryTest.*
    Test: Compile with official gn args, load chrome://crash, file report
    Test: Resulted in filing http://b/377948398
    Change-Id: Ic8ef030e315b412efa1d6d92477ff588b51aec1b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003981
    Auto-Submit: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380253}

--- android_webview/javatests/src/org/chromium/android_webview/test/devui/util/CrashBugUrlFactoryTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/devui/util/CrashBugUrlFactoryTest.java
@@ -79,6 +79,7 @@ public class CrashBugUrlFactoryTest {
                 hasData(
                         hasParamWithValue(
                                 "template", BugTrackerConstants.DEFAULT_WEBVIEW_TEMPLATE)));
+        assertThat(intent, hasData(hasParamWithValue("title", "User reported crash")));
         assertThat(intent, hasData(hasParamWithValue("priority", "P3")));
         assertThat(intent, hasData(hasParamWithValue("type", "BUG")));
         assertThat(
