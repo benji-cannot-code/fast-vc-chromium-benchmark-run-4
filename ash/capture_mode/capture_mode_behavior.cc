8d17f664ef22ffb85dfc1b90c0a899e35725574e
Sophie Wen <sophiewen@google.com>
2024-11-08 05:14:58 +0000

    sunfish: Update action widget visibility
    This CL does the following:
    - Move `action_container_widget_` opacity changes out of
    `MaybeUpdateCaptureUisOpacity()`
    - Always create `action_container_widget_` but set its visibility
    - Change tests to check `IsVisible()` instead of opacity
    
    Bug: b:377570562
    Change-Id: I659f9e1bd4ffbac1e57ec21ec0b052ec4a869d90
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004051
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380161}

--- ash/capture_mode/capture_mode_behavior.cc
+++ ash/capture_mode/capture_mode_behavior.cc
@@ -114,6 +114,7 @@ class DefaultBehavior : public CaptureModeBehavior {
     auto* scanner_controller = Shell::Get()->scanner_controller();
     return scanner_controller && scanner_controller->CanStartSession();
   }
+  bool CanShowActionButtons() const override { return true; }
   void OnRegionSelectedOrAdjusted() override {
     if (ShouldShowDefaultActionButtonsAfterRegionSelected() &&
         features::IsScannerEnabled()) {
@@ -366,6 +367,7 @@ class SunfishBehavior : public CaptureModeBehavior {
   bool ShouldShowCaptureButtonAfterRegionSelected() const override {
     return false;
   }
+  bool CanShowActionButtons() const override { return true; }
   bool ShouldEndSessionOnSearchResultClicked() const override { return true; }
   const std::u16string GetCaptureLabelRegionText() const override {
     return l10n_util::GetStringUTF16(IDS_ASH_SUNFISH_CAPTURE_LABEL);
@@ -541,6 +543,10 @@ bool CaptureModeBehavior::ShouldShowDefaultActionButtonsAfterRegionSelected()
 }
 
 bool CaptureModeBehavior::CanShowSmartActionsButton() const {
+  return false;
+}
+
+bool CaptureModeBehavior::CanShowActionButtons() const {
   return false;
 }
 
