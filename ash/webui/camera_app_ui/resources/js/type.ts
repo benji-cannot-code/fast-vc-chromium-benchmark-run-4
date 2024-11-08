0132ebb0f75bb990ca639aab8113d860bcb5a2d1
Kam Kwankajornkiet <kamchonlathorn@google.com>
2024-11-08 05:07:18 +0000

    CCA: Remove super resolution intro dialog
    The feature launches in M126. So, it's time to remove the feature
    introduction dialog.
    
    Bug: 360924316
    Test: Manually check UI, tast run $brya camera.CCAUISmoke.fake_hal
    Change-Id: I310416a8452e067495d8ac78eaaab2751b5c77ae
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5998844
    Commit-Queue: Kam Kwankajornkiet <kamchonlathorn@chromium.org>
    Reviewed-by: Shik Chen <shik@chromium.org>
    Reviewed-by: Chu-Hsuan Yang <chuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380158}

--- ash/webui/camera_app_ui/resources/js/type.ts
+++ ash/webui/camera_app_ui/resources/js/type.ts
@@ -142,7 +142,6 @@ export enum ViewName {
   REVIEW = 'view-review',
   SETTINGS = 'view-settings',
   SPLASH = 'view-splash',
-  SUPER_RES_INTRO_DIALOG = 'view-super-res-intro-dialog',
   VIDEO_RESOLUTION_SETTINGS = 'view-video-resolution-settings',
   WARNING = 'view-warning',
 }
@@ -552,7 +551,6 @@ export enum LocalStorageKey {
   SAVE_METADATA = 'saveMetadata',
   SHOW_ALL_RESOLUTIONS = 'showAllResolutions',
   SHOW_METADATA = 'showMetadata',
-  SUPER_RES_DIALOG_SHOWN = 'superResDialogShown',
   TOGGLE_MIC = 'toggleMic',
 }
 
