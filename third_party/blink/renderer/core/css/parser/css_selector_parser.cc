f96be607fedd9e032de7a8df439cca44d876e480
Steinar H. Gunderson <sesse@chromium.org>
2024-11-08 14:24:36 +0000

    Add use-counter and WebDX feature mapping for file-selector-button.
    Change-Id: I0e1faf02c931bc5b25f78bc71b2cfa4b872ec44a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003165
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Noam Rosenthal <nrosenthal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380348}

--- third_party/blink/renderer/core/css/parser/css_selector_parser.cc
+++ third_party/blink/renderer/core/css/parser/css_selector_parser.cc
@@ -2555,6 +2555,9 @@ static void RecordUsageAndDeprecationsOneSelector(
     case CSSSelector::kPseudoModal:
       feature = WebFeature::kCSSSelectorPseudoModal;
       break;
+    case CSSSelector::kPseudoFileSelectorButton:
+      feature = WebFeature::kCSSSelectorPseudoFileSelectorButton;
+      break;
     default:
       break;
   }
