6af1e31d95858378fd139f6f95bd17694067db6f
Lei Zhang <thestig@chromium.org>
2024-11-08 05:13:09 +0000

    Revert "[PDF Ink Signatures] Rename PdfInkUndoRedoModel types and methods"
    This reverts commit 0c09e97488a2edac41d55aea832fb210b5872f96.
    
    Reason for revert: Not needed. Landed prematurely.
    
    Original change's description:
    > [PDF Ink Signatures] Rename PdfInkUndoRedoModel types and methods
    >
    > In PdfInkUndoRedoModel, rename types and methods that have "Draw" in the
    > name to "DrawStroke". Do the same for "Erase". This clarifies what is
    > being drawn/erased, and makes room for other types of draw/erase
    > operations. This is just a rename and has no behavior changes.
    >
    > Bug: 377820805
    > Change-Id: Id44e434f88f758c1391c22c301a712bb60a66e18
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001998
    > Commit-Queue: Lei Zhang <thestig@chromium.org>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: Andy Phan <andyphan@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1379855}
    
    Bug: 377820805
    Change-Id: Ife50ea089feb6f2ddc3eee85414d0241bd6feafc
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004053
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Lei Zhang <thestig@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1380159}

--- pdf/pdf_ink_module.cc
+++ pdf/pdf_ink_module.cc
@@ -419,8 +419,8 @@ bool PdfInkModule::StartStroke(const gfx::PointF& position,
   // Invalidate area around this one point.
   client_->Invalidate(GetDrawingBrush().GetInvalidateArea(position, position));
 
-  std::optional<PdfInkUndoRedoModel::DiscardedDrawStrokeCommands> discards =
-      undo_redo_model_.StartDrawStroke();
+  std::optional<PdfInkUndoRedoModel::DiscardedDrawCommands> discards =
+      undo_redo_model_.StartDraw();
   CHECK(discards.has_value());
   ApplyUndoRedoDiscards(discards.value());
 
@@ -523,7 +523,7 @@ bool PdfInkModule::FinishStroke(const gfx::PointF& position,
       InkStrokeId id = stroke_id_generator_.GetIdAndAdvance();
       strokes_[state.page_index].push_back(
           FinishedStrokeState(segment.CopyToStroke(), id));
-      bool undo_redo_success = undo_redo_model_.DrawStroke(id);
+      bool undo_redo_success = undo_redo_model_.Draw(id);
       CHECK(undo_redo_success);
     }
   }
@@ -531,7 +531,7 @@ bool PdfInkModule::FinishStroke(const gfx::PointF& position,
   client_->StrokeFinished();
   client_->UpdateThumbnail(state.page_index);
 
-  bool undo_redo_success = undo_redo_model_.FinishDrawStroke();
+  bool undo_redo_success = undo_redo_model_.FinishDraw();
   CHECK(undo_redo_success);
 
   // Reset `state` now that the stroke operation is done.
@@ -554,8 +554,8 @@ bool PdfInkModule::StartEraseStroke(const gfx::PointF& position) {
   CHECK(!state.erasing);
   state.erasing = true;
 
-  std::optional<PdfInkUndoRedoModel::DiscardedDrawStrokeCommands> discards =
-      undo_redo_model_.StartEraseStroke();
+  std::optional<PdfInkUndoRedoModel::DiscardedDrawCommands> discards =
+      undo_redo_model_.StartErase();
   CHECK(discards.has_value());
   ApplyUndoRedoDiscards(discards.value());
 
@@ -593,7 +593,7 @@ bool PdfInkModule::FinishEraseStroke(const gfx::PointF& position) {
     return false;
   }
 
-  bool undo_redo_success = undo_redo_model_.FinishEraseStroke();
+  bool undo_redo_success = undo_redo_model_.FinishErase();
   CHECK(undo_redo_success);
 
   CHECK(is_erasing_stroke());
@@ -641,7 +641,7 @@ bool PdfInkModule::EraseHelper(const gfx::PointF& position, int page_index) {
 
     invalidate_envelope.Add(shape.Bounds());
 
-    bool undo_redo_success = undo_redo_model_.EraseStroke(stroke.id);
+    bool undo_redo_success = undo_redo_model_.Erase(stroke.id);
     CHECK(undo_redo_success);
   }
 
@@ -848,15 +848,15 @@ void PdfInkModule::ApplyUndoRedoCommands(
     case PdfInkUndoRedoModel::CommandsType::kNone: {
       return;
     }
-    case PdfInkUndoRedoModel::CommandsType::kDrawStroke: {
+    case PdfInkUndoRedoModel::CommandsType::kDraw: {
       ApplyUndoRedoCommandsHelper(
-          PdfInkUndoRedoModel::GetDrawStrokeCommands(commands).value(),
+          PdfInkUndoRedoModel::GetDrawCommands(commands).value(),
           /*should_draw=*/true);
       return;
     }
-    case PdfInkUndoRedoModel::CommandsType::kEraseStroke: {
+    case PdfInkUndoRedoModel::CommandsType::kErase: {
       ApplyUndoRedoCommandsHelper(
-          PdfInkUndoRedoModel::GetEraseStrokeCommands(commands).value(),
+          PdfInkUndoRedoModel::GetEraseCommands(commands).value(),
           /*should_draw=*/false);
       return;
     }
@@ -912,7 +912,7 @@ void PdfInkModule::ApplyUndoRedoCommandsHelper(std::set<InkStrokeId> ids,
 }
 
 void PdfInkModule::ApplyUndoRedoDiscards(
-    const PdfInkUndoRedoModel::DiscardedDrawStrokeCommands& discards) {
+    const PdfInkUndoRedoModel::DiscardedDrawCommands& discards) {
   if (discards.empty()) {
     return;
   }
