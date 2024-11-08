5ef6e8a2b4dad33c11517aa726ba7222cf61fad8
Fiaz Muhammad <mfiaz@google.com>
2024-11-08 20:12:15 +0000

    [Tab Groups] Add Test Mode Functionality for TabGroupFeatureUtils
    - Two one new Boolean fields and setter method
    - Prevents SharedPreferencesManager from needing to be mocked
    	(difficult since it requires many layers upon layers of mocks)
    
    Low-Coverage-Reason: TESTS_IN_SEPARATE_CL
    
    Bug: 377176969
    Change-Id: Iee4869b8e287ce18bef3b5f3566aa2f17ca3dc04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5995511
    Reviewed-by: Brandon Fong <bjfong@google.com>
    Commit-Queue: Fiaz Muhammad <mfiaz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Reviewed-by: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380563}

--- chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabGroupFeatureUtils.java
+++ chrome/browser/tabmodel/android/java/src/org/chromium/chrome/browser/tabmodel/TabGroupFeatureUtils.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.tabmodel;
 
+import androidx.annotation.Nullable;
+
+import org.chromium.base.ResettersForTesting;
 import org.chromium.base.shared_preferences.SharedPreferencesManager;
 import org.chromium.chrome.browser.flags.ChromeFeatureList;
 import org.chromium.chrome.browser.preferences.ChromePreferenceKeys;
@@ -20,6 +23,7 @@ public class TabGroupFeatureUtils {
                     ChromeFeatureList.TAB_GROUP_CREATION_DIALOG_ANDROID,
                     SHOW_TAB_GROUP_CREATION_DIALOG_SETTING_PARAM,
                     false);
+    private static @Nullable Boolean sTestValueShowTabGroupCreationDialog;
 
     /**
      * Returns whether the group creation dialog should be shown based on the setting switch for
@@ -27,7 +31,9 @@ public class TabGroupFeatureUtils {
      * case for all callsites.
      */
     public static boolean shouldShowGroupCreationDialogViaSettingsSwitch() {
-        if (SHOW_TAB_GROUP_CREATION_DIALOG_SETTING.getValue()) {
+        if (sTestValueShowTabGroupCreationDialog != null) {
+            return sTestValueShowTabGroupCreationDialog;
+        } else if (SHOW_TAB_GROUP_CREATION_DIALOG_SETTING.getValue()) {
             SharedPreferencesManager prefsManager = ChromeSharedPreferences.getInstance();
             return prefsManager.readBoolean(
                     ChromePreferenceKeys.SHOW_TAB_GROUP_CREATION_DIALOG, true);
@@ -49,6 +55,21 @@ public class TabGroupFeatureUtils {
         } else {
             return true;
         }
+    }
+
+    /**
+     * Sets the the value to be returned by {@link
+     * #shouldShowGroupCreationDialogViaSettingsSwitch()}. When {@link
+     * #sTestValueShowTabGroupCreationDialog} is not null, prefsManager will not be called in {@link
+     * #shouldShowGroupCreationDialogViaSettingsSwitch()}, allowing for ease in testing different
+     * logical branches.
+     *
+     * @param returnValue The value to be returned by {@link
+     *     #shouldShowGroupCreationDialogViaSettingsSwitch()}.
+     */
+    public static void setsTestValueShowTabGroupCreationDialog(@Nullable Boolean returnValue) {
+        sTestValueShowTabGroupCreationDialog = returnValue;
+        ResettersForTesting.register(() -> sTestValueShowTabGroupCreationDialog = null);
     }
 
     /** All statics. */
