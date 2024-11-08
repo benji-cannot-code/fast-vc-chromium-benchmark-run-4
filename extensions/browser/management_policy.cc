cf7ce2546fb8811c4dfefa2a65e5906ba6c5dafb
Devlin Cronin <rdevlin.cronin@chromium.org>
2024-11-08 19:11:47 +0000

    [Extensions][Cleanup] No longer pass `error` into `UserMayLoad()`
    ManagementPolicy::UserMayLoad() takes in a string to populate with
    the reason an extension cannot be loaded. However, no production callers
    ever populate the error out-param and this is never shown in any UI.
    
    Remove this out-param.
    
    This CL has no behavior change.
    
    Bug: None
    Change-Id: Ib118f58a749c133fd96171dc5d3d74b605edaa25
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001293
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380518}

--- extensions/browser/management_policy.cc
+++ extensions/browser/management_policy.cc
@@ -87,10 +87,9 @@ void ManagementPolicy::RegisterProviders(
     providers_.insert(provider.get());
 }
 
-bool ManagementPolicy::UserMayLoad(const Extension* extension,
-                                   std::u16string* error) const {
-  return ApplyToProviderList(
-      &Provider::UserMayLoad, "Installation", true, extension, error);
+bool ManagementPolicy::UserMayLoad(const Extension* extension) const {
+  return ApplyToProviderList(&Provider::UserMayLoad, "Installation", true,
+                             extension, /*error=*/nullptr);
 }
 
 bool ManagementPolicy::UserMayInstall(const Extension* extension,
@@ -132,7 +131,7 @@ bool ManagementPolicy::MustRemainEnabled(const Extension* extension,
 bool ManagementPolicy::MustRemainDisabled(
     const Extension* extension,
     disable_reason::DisableReason* reason) const {
-  if (!UserMayLoad(extension, /*error=*/nullptr)) {
+  if (!UserMayLoad(extension)) {
     if (reason) {
       *reason = disable_reason::DISABLE_BLOCKED_BY_POLICY;
     }
