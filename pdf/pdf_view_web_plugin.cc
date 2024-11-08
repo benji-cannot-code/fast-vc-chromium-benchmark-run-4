c9fb5abb2973932a56224e59b558ba780a023496
Andy Phan <andyphan@chromium.org>
2024-11-08 21:05:04 +0000

    [PDF Ink Signatures] Set save request type to annotation after edit
    There are two ways to save a PDF in the PDF viewer: using the save
    button in the toolbar, or using a save browser command.
    
    For the toolbar button, set the save request type to ANNOTATION only
    if there are existing Ink2 edits, rather than always after the user
    enters annotation mode. This matches the behavior of CrOS Gallery more
    closely.
    
    For the save browser command, do the same by informing the browser
    that the plugin will handle the save after drawing a stroke. Note that
    in the edge case where the user undos all strokes, then the plugin will
    have the browser handle the save instead.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED This CL adds tests that
    cover new code.
    
    Bug: 353904284, 377488931
    Change-Id: Iad31cd4907456de1acfb2e35a4fdda343d34981f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003789
    Commit-Queue: Andy Phan <andyphan@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380605}

--- pdf/pdf_view_web_plugin.cc
+++ pdf/pdf_view_web_plugin.cc
@@ -313,6 +313,7 @@ class PdfViewWebPlugin::PdfInkModuleClientImpl : public PdfInkModuleClient {
     base::Value::Dict message;
     message.Set("type", "finishInkStroke");
     plugin_->client_->PostMessage(std::move(message));
+    plugin_->SetPluginCanSave(true);
   }
 
   void UpdateInkCursorImage(SkBitmap bitmap) override {
