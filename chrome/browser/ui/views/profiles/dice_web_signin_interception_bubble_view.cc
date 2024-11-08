6d8914bcfc4861a82b817fae3632a8626c0cfd77
Ryan Sultanem <rsult@google.com>
2024-11-08 14:57:15 +0000

    [Uno-D] Record signin offered histograms for uno related access points
    Record `Signin.SignIn.Offered` for the ProfileMenu and password bubble
    entry point, for all states except SigninPending.
    
    Extended existing histogram `Signin.SignIn.Offered` to have variants
    for `.WithDefault` and `.NewAccountNoExistingAccount`.
    `.WithDefault`: defaulted web signed in account.
    `.NewAccountNoExistingAccount`: fully signed out users.
    
    Bug: b:369834715
    Change-Id: Ica4ef81c4d259f3e8501bf5e7d7b293ce6c4a568
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5993556
    Commit-Queue: Ryan Sultanem <rsult@google.com>
    Reviewed-by: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380360}

--- chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
+++ chrome/browser/ui/views/profiles/dice_web_signin_interception_bubble_view.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/grit/generated_resources.h"
 #include "components/input/native_web_keyboard_event.h"
 #include "components/signin/public/base/consent_level.h"
+#include "components/signin/public/base/signin_metrics.h"
 #include "components/signin/public/base/signin_switches.h"
 #include "components/signin/public/identity_manager/identity_manager.h"
 #include "components/strings/grit/components_strings.h"
@@ -127,7 +128,8 @@ void RecordMetricsChromeSigninInterceptStarted() {
   auto access_point =
       signin_metrics::AccessPoint::ACCESS_POINT_CHROME_SIGNIN_INTERCEPT_BUBBLE;
   RecordSigninImpressionUserActionForAccessPoint(access_point);
-  signin_metrics::LogSignInOffered(access_point);
+  signin_metrics::LogSignInOffered(
+      access_point, signin_metrics::PromoAction::PROMO_ACTION_WITH_DEFAULT);
 }
 
 std::string_view GetChromeSigninReactionString(
