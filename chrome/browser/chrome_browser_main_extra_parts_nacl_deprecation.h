5c051661000f29e8195047f16b9bf3956b549cee
Fabian Sommer <fabiansommer@chromium.org>
2024-11-08 11:53:01 +0000

    Implement NativeClientDeviceForceAllowed policy
    The old NativeClientForceAllowed policy does not work for ChromeOS -
    we need the policy value early during startup, and user policies are not
    loaded for some scenarios (login screen, managed guest session).
    Instead, implement a new device policy that takes the place of the old
    user policy, while deprecating the old policy.
    
    Since the value needs to be present very early in startup, generally
    before device policies are loaded, add a second pref that is used to
    cache the last known value of the policy. During startup, use this
    cached value to potentially re-enable NaCl.
    This means that Chrome needs to be restarted once for the policy to be
    applied correctly.
    
    Fixed: 374790919
    Change-Id: I6cb3d904d1627d18233c7f3e73374ad7c84fbaf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5988133
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Maksim Ivanov <emaxx@chromium.org>
    Commit-Queue: Fabian Sommer <fabiansommer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380287}

--- chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.h
+++ chrome/browser/chrome_browser_main_extra_parts_nacl_deprecation.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/feature_list.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
+#include "components/prefs/pref_change_registrar.h"
 
 // Controls whether any NaCl plugins are allowed to be loaded.
 // This can be overridden by enterprise policy. Exposed for testing.
@@ -24,6 +25,14 @@ class ChromeBrowserMainExtraPartsNaclDeprecation
   // signal early enough to child processes. This is necessary since plugins are
   // loaded before field trial initialization.
   void PostEarlyInitialization() override;
+
+  // Used for cleanup of a pref observer.
+  void PostMainMessageLoopRun() override;
+
+ private:
+  void NaclAllowedChanged();
+
+  PrefChangeRegistrar pref_change_registrar_;
 };
 
 #endif  // CHROME_BROWSER_CHROME_BROWSER_MAIN_EXTRA_PARTS_NACL_DEPRECATION_H_
