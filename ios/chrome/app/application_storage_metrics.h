426503b6baa7b0bc6d64074f77d28c0e9ad0def6
Kush Sinha <sinhak@chromium.org>
2024-11-08 09:56:49 +0000

    Revert "Reland "[ios] Move part of Profile deferred init to ProfileController""
    This reverts commit f17d9a31b20c1e2fa0b98a8e0c3be3387161e820.
    
    Reason for revert: Compile failure https://ci.chromium.org/ui/p/chromium/builders/ci/ios-catalyst/147881/overview
    
    Original change's description:
    > Reland "[ios] Move part of Profile deferred init to ProfileController"
    >
    > This is a reland of commit 686edbcf2509644d0f0a5a19c31e14d3da542e10
    >
    > The original CLs was reverted because it did not compiler on device
    > (which does not happen on CQ). This version fixes the typos in the
    > code that is only compiled for devices (introduced when introducing
    > an helper function).
    >
    > Original change's description:
    > > [ios] Move part of Profile deferred init to ProfileController
    > >
    > > Move some of the Profile deferred initialization from MainController
    > > to ProfileController. This ensure the code is run even for Profiles
    > > that are loaded dynamically after the MainController is initialized.
    > >
    > > Bug: 353683675, 40769058, 333863468
    > > Change-Id: Ie22a5e2b68f127da6ace5715abd2ac1d8157b42a
    > > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999921
    > > Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    > > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > > Cr-Commit-Position: refs/heads/main@{#1379635}
    >
    > Bug: 353683675, 40769058, 333863468
    > Change-Id: I75008ce46788e94233d2402c90a2d4179d97cc89
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999324
    > Reviewed-by: Aliona Dangla <alionadangla@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1380223}
    
    Bug: 353683675, 40769058, 333863468
    Change-Id: Ib6a0c5f296d7d3457716b30e61ff73741d2e06f4
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999466
    Owners-Override: Kush Sinha <sinhak@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Kush Sinha <sinhak@chromium.org>
    Commit-Queue: Kush Sinha <sinhak@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380242}

--- ios/chrome/app/application_storage_metrics.h
+++ ios/chrome/app/application_storage_metrics.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_APP_PROFILE_APPLICATION_STORAGE_METRICS_H_
-#define IOS_CHROME_APP_PROFILE_APPLICATION_STORAGE_METRICS_H_
+#ifndef IOS_CHROME_APP_APPLICATION_STORAGE_METRICS_H_
+#define IOS_CHROME_APP_APPLICATION_STORAGE_METRICS_H_
 
 #import <Foundation/Foundation.h>
 
@@ -16,4 +16,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 void LogApplicationStorageMetrics(base::FilePath profile_path,
                                   base::FilePath off_the_record_state_path);
 
-#endif  // IOS_CHROME_APP_PROFILE_APPLICATION_STORAGE_METRICS_H_
+#endif  // IOS_CHROME_APP_APPLICATION_STORAGE_METRICS_H_
