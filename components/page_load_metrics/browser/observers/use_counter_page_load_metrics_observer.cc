f571f09845032e53402fd1408d0170741fe6a454
Steinar H. Gunderson <sesse@chromium.org>
2024-11-08 10:53:39 +0000

    Add use-counter and WebDX feature mapping for exp-functions.
    Change-Id: Iee62acf223f9bcb39eff27d0f5ae491f4e389f69
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001394
    Commit-Queue: Steinar H Gunderson <sesse@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Shunya Shishido <sisidovski@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380262}

--- components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
+++ components/page_load_metrics/browser/observers/use_counter_page_load_metrics_observer.cc
@@ -526,6 +526,7 @@ UseCounterMetricsRecorder::GetWebFeatureToWebDXFeatureMap() {
           {WebFeature::kPrefersReducedTransparencyMediaFeature,
            WebDXFeature::kPrefersReducedTransparency},
           {WebFeature::kCSSRoundModRemFunctions, WebDXFeature::kRoundModRem},
+          {WebFeature::kCSSExponentialFunctions, WebDXFeature::kExpFunctions},
       }};
 
   return *kMap;
