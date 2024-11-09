1d86146bca89466b77cf80a58bf7b36badf6a0fc
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-09 03:31:21 +0000

    Import wpt@7b542308b66f77878714f851a8baac291ee2a39e
    https://github.com/web-platform-tests/wpt/compare/af915520c...7b542308b
    
    Using wpt-import in Chromium 24c4edf09f4867595ed5806e022538113004fcdd.
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    cbiesinger@chromium.org:
      external/wpt/css/css-flexbox
    nzolghadr@chromium.org, mustaq@chromium.org:
      external/wpt/pointerevents
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: Ie92b008697f80e90d8db15776a5a8071fd7670ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6007041
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Commit-Queue: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1380784}

---
+++ third_party/blink/web_tests/external/wpt/webdriver/tests/bidi/external/bluetooth/simulate_adapter/__init__.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+from webdriver.bidi.modules.script import ContextTarget
+
+
+async def get_bluetooth_availability(bidi_session, context):
+    result = await bidi_session.script.evaluate(
+        expression="navigator.bluetooth.getAvailability()",
+        target=ContextTarget(context["context"]), await_promise=True, )
+    return result['value']
+
+
+async def set_simulate_adapter(bidi_session, context, test_page, state):
+    # Navigate to a page, as bluetooth is not guaranteed to work on
+    # `about:blank`.
+    await bidi_session.browsing_context.navigate(context=context['context'],
+                                                 url=test_page, wait="complete")
+
+    await bidi_session.bluetooth.simulate_adapter(context=context["context"],
+                                                  state=state)
