473c1f2729337c8a3111ebda399d4da042c281d1
Noah Rose Ledesma <noahrose@google.com>
2024-11-09 00:53:06 +0000

    Spanify GetLastOSVersion
    GetLastOSVersion relied on a reinterpret cast from deserialized
    bytes. Refactor the code to construct the os info struct from its
    constituent parts using a span reader.
    
    Bug: 374320451
    Change-Id: I23d51b8889f01afde0a112da3e3659dfb5daa161
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006735
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Noah Rose Ledesma <noahrose@google.com>
    Commit-Queue: Noah Rose Ledesma <noahrose@google.com>
    Reviewed-by: Xiaoling Bao <xiaolingbao@chromium.org>
    Commit-Queue: Xiaoling Bao <xiaolingbao@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380738}

--- chrome/updater/persisted_data.cc
+++ chrome/updater/persisted_data.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/374320451): Fix and remove.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/updater/persisted_data.h"
 
 #include <optional>
@@ -36,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if BUILDFLAG(IS_WIN)
 #include <windows.h>
 
+#include "base/containers/span.h"
+#include "base/containers/span_reader.h"
+#include "base/numerics/byte_conversions.h"
 #include "base/win/registry.h"
 #include "chrome/updater/util/win_util.h"
 #include "chrome/updater/win/win_constants.h"
@@ -665,7 +663,28 @@ std::optional<OSVERSIONINFOEX> PersistedData::GetLastOSVersion() const {
     return std::nullopt;
   }
 
-  return *reinterpret_cast<const OSVERSIONINFOEX*>(decoded_os_version->data());
+  auto reader = base::SpanReader(base::make_span(*decoded_os_version));
+  OSVERSIONINFOEX info;
+  info.dwOSVersionInfoSize =
+      base::U32FromNativeEndian(*reader.Read<sizeof(DWORD)>());
+  info.dwMajorVersion =
+      base::U32FromNativeEndian(*reader.Read<sizeof(DWORD)>());
+  info.dwMinorVersion =
+      base::U32FromNativeEndian(*reader.Read<sizeof(DWORD)>());
+  info.dwBuildNumber = base::U32FromNativeEndian(*reader.Read<sizeof(DWORD)>());
+  info.dwPlatformId = base::U32FromNativeEndian(*reader.Read<sizeof(DWORD)>());
+  base::as_writable_byte_span(info.szCSDVersion)
+      .copy_from(*reader.Read<sizeof((OSVERSIONINFOEX){}.szCSDVersion)>());
+  info.wServicePackMajor =
+      base::U16FromNativeEndian(*reader.Read<sizeof(WORD)>());
+  info.wServicePackMinor =
+      base::U16FromNativeEndian(*reader.Read<sizeof(WORD)>());
+  info.wSuiteMask = base::U16FromNativeEndian(*reader.Read<sizeof(WORD)>());
+  info.wProductType = base::U8FromNativeEndian(*reader.Read<sizeof(BYTE)>());
+  info.wReserved = base::U8FromNativeEndian(*reader.Read<sizeof(BYTE)>());
+
+  CHECK_EQ(reader.remaining(), 0u);
+  return info;
 }
 
 void PersistedData::SetLastOSVersion() {
