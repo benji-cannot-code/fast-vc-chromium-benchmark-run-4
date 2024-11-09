734f267baadcc619a2757c5fe3a95088c6b3c043
Lei Zhang <thestig@chromium.org>
2024-11-09 02:34:08 +0000

    [PDF Ink Signatures] Set the tool type appropriately for ink::Strokes
    Change input event handlers in PdfInkModule to set the tool type as
    either mouse or touch, instead of always hardcoding the tool type to
    mouse. Though it is not clear how to differentiate between touch events
    and pen events, so there is a TODO to investigate that later.
    
    Bug: 353942909
    Change-Id: I4da5ac5ab81dcc46d66f776c0a6029ce8121bbdc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001860
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380773}

--- pdf/pdf_ink_module.h
+++ pdf/pdf_ink_module.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/ink/src/ink/geometry/modeled_shape.h"
 #include "third_party/ink/src/ink/strokes/in_progress_stroke.h"
+#include "third_party/ink/src/ink/strokes/input/stroke_input.h"
 #include "third_party/ink/src/ink/strokes/input/stroke_input_batch.h"
 #include "third_party/ink/src/ink/strokes/stroke.h"
 #include "ui/gfx/geometry/point_f.h"
@@ -170,6 +171,13 @@ class PdfInkModule {
   DocumentStrokeInputPointsMap GetVisibleStrokesInputPositionsForTesting()
       const;
 
+  // For testing only. Returns the number of stroke inputs of a particular
+  // `tool_type` for a given page at `page_index`. The `page_index` must be
+  // non-negative.
+  int GetInputOfTypeCountForPageForTesting(
+      int page_index,
+      ink::StrokeInput::ToolType tool_type) const;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(PdfInkModuleTest, HandleSetAnnotationModeMessage);
 
@@ -261,12 +269,18 @@ class PdfInkModule {
   bool OnTouchEnd(const blink::WebTouchEvent& event);
   bool OnTouchMove(const blink::WebTouchEvent& event);
 
-  // Return values have the same semantics as OnMouse()* above.
-  bool StartStroke(const gfx::PointF& position, base::TimeTicks timestamp);
-  bool ContinueStroke(const gfx::PointF& position, base::TimeTicks timestamp);
-  bool FinishStroke(const gfx::PointF& position, base::TimeTicks timestamp);
+  // Return values have the same semantics as On{Mouse,Touch}*() above.
+  bool StartStroke(const gfx::PointF& position,
+                   base::TimeTicks timestamp,
+                   ink::StrokeInput::ToolType tool_type);
+  bool ContinueStroke(const gfx::PointF& position,
+                      base::TimeTicks timestamp,
+                      ink::StrokeInput::ToolType tool_type);
+  bool FinishStroke(const gfx::PointF& position,
+                    base::TimeTicks timestamp,
+                    ink::StrokeInput::ToolType tool_type);
 
-  // Return values have the same semantics as OnMouse*() above.
+  // Return values have the same semantics as On{Mouse,Touch}*() above.
   bool StartEraseStroke(const gfx::PointF& position);
   bool ContinueEraseStroke(const gfx::PointF& position);
   bool FinishEraseStroke(const gfx::PointF& position);
@@ -321,10 +335,11 @@ class PdfInkModule {
       int page_index);
 
   // Helper to convert `position` to a canonical position and record it into
-  // `current_tool_state_` for the indicated time. Can only be called when
-  // drawing.
+  // `current_tool_state_` for the indicated `timestamp` and `tool_type`.
+  // Can only be called when drawing.
   void RecordStrokePosition(const gfx::PointF& position,
-                            base::TimeTicks timestamp);
+                            base::TimeTicks timestamp,
+                            ink::StrokeInput::ToolType tool_type);
 
   void ApplyUndoRedoCommands(const PdfInkUndoRedoModel::Commands& commands);
   void ApplyUndoRedoCommandsHelper(std::set<PdfInkUndoRedoModel::IdType> ids,
