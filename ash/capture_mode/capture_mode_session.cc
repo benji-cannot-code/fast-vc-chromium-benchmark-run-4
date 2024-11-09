f3e5f59a7b803bb330f129ad5450c47f5bc4ca52
Elijah Hewer <hewer@chromium.org>
2024-11-09 00:36:04 +0000

    Sunfish: Record search button shown and pressed
    Add metrics to record when the "Search with Lens" action button is both
    shown and pressed during a default capture session.
    
    Bug: 377718305
    Test: SunfishTest.RecordSearchButtonShownAndPressed
    Change-Id: If26a45f8a37c272dc8c6c3d70ceeb3cb595fdf59
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001470
    Commit-Queue: Elijah Hewer <hewer@chromium.org>
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Reviewed-by: Sophie Wen <sophiewen@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1380727}

--- ash/capture_mode/capture_mode_session.cc
+++ ash/capture_mode/capture_mode_session.cc
@@ -1983,6 +1983,7 @@ void CaptureModeSession::DoPerformCapture() {
 }
 
 void CaptureModeSession::OnSearchButtonPressed() {
+  RecordSearchButtonPressed();
   // See if we can move this to `PerformImageSearch()`.
   controller_->PerformCapture(
       PerformCaptureType::kSearch);  // `this` can be deleted after this.
@@ -2620,6 +2621,7 @@ void CaptureModeSession::OnLocatedEventReleased(
   // TODO(b/367882127): May also need to check if the user has opted in.
   if (active_behavior_->ShouldShowDefaultActionButtonsAfterRegionSelected()) {
     if (IsSunfishFeatureEnabledWithFeatureKey()) {
+      RecordSearchButtonShown();
       capture_mode_util::AddActionButton(
           base::BindRepeating(&CaptureModeSession::OnSearchButtonPressed,
                               weak_ptr_factory_.GetWeakPtr()),
