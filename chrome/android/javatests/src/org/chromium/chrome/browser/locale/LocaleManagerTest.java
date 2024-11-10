9dd438e1332b2eb80754594f8b254d77892d093c
Tanmoy Mollik <triploblastic@google.com>
2024-11-10 16:08:32 +0000

    [Clay] Show a snackbar when default search engine changes
    Screenshot: http://crbug.com/355182385#attachment60625412
    
    Bug: 355182385
    Change-Id: Ie2b9e385a988b7f46d64762911223e31b07480fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5980757
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alex Ilin <alexilin@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Tanmoy Mollik <triploblastic@google.com>
    Cr-Commit-Position: refs/heads/main@{#1380949}

--- chrome/android/javatests/src/org/chromium/chrome/browser/locale/LocaleManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/locale/LocaleManagerTest.java
@@ -5,18 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.locale;
 
+import androidx.test.filters.MediumTest;
 import androidx.test.filters.SmallTest;
 
+import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnit;
-import org.mockito.junit.MockitoRule;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.Batch;
@@ -24,10 +21,11 @@ import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.browser.search_engines.SearchEnginePromoType;
+import org.chromium.chrome.browser.ui.messages.snackbar.Snackbar;
+import org.chromium.chrome.browser.ui.messages.snackbar.SnackbarManager;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
 import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.components.policy.test.annotations.Policies;
-import org.chromium.components.search_engines.TemplateUrl;
 
 import java.util.concurrent.ExecutionException;
 
@@ -36,23 +34,21 @@ import java.util.concurrent.ExecutionException;
 @Batch(Batch.PER_CLASS)
 @CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
 public class LocaleManagerTest {
-    public @Rule MockitoRule mockitoRule = MockitoJUnit.rule();
     public static @ClassRule ChromeTabbedActivityTestRule sActivityTestRule =
             new ChromeTabbedActivityTestRule();
 
-    private @Mock TemplateUrl mMockTemplateUrl;
-
     @BeforeClass
     public static void setUpClass() throws ExecutionException {
-        sActivityTestRule.setFinishActivity(true);
-    }
-
-    @Before
-    public void setUp() {
         // Launch any activity as an Activity ref is required to attempt to show the activity.
         sActivityTestRule.startMainActivityOnBlankPage();
         sActivityTestRule.waitForActivityNativeInitializationComplete();
         sActivityTestRule.waitForDeferredStartup();
+    }
+
+    @After
+    public void tearDown() {
+        ThreadUtils.runOnUiThreadBlocking(
+                sActivityTestRule.getActivity().getSnackbarManager()::dismissAllSnackbars);
     }
 
     @Policies.Add({@Policies.Item(key = "DefaultSearchProviderEnabled", string = "false")})
@@ -84,5 +80,48 @@ public class LocaleManagerTest {
                                         }));
         searchEnginesFinalizedCallback.waitForCallback(0);
         Assert.assertEquals(0, getShowTypeCallback.getCallCount());
+    }
+
+    @MediumTest
+    @Test
+    public void testSnackbarForDeviceSearchEngineUpdate() {
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    LocaleManager localeManager = LocaleManager.getInstance();
+                    SnackbarManager snackbarManager =
+                            sActivityTestRule.getActivity().getSnackbarManager();
+                    localeManager.setSnackbarManager(snackbarManager);
+
+                    localeManager.showSnackbarForDeviceSearchEngineUpdate();
+
+                    Assert.assertTrue(snackbarManager.isShowing());
+                    Snackbar currentSnackbar = snackbarManager.getCurrentSnackbarForTesting();
+                    Assert.assertEquals(
+                            currentSnackbar.getIdentifierForTesting(),
+                            Snackbar.UMA_SEARCH_ENGINE_CHANGED_NOTIFICATION);
+                });
+    }
+
+    @MediumTest
+    @Test
+    public void testSnackbarForDeviceSearchEngineUpdateShownAfterSnackbarManagerSet() {
+        ThreadUtils.runOnUiThreadBlocking(
+                () -> {
+                    LocaleManager localeManager = LocaleManager.getInstance();
+                    SnackbarManager snackbarManager =
+                            sActivityTestRule.getActivity().getSnackbarManager();
+                    // Simulate the case when snackbar manager is not set.
+                    localeManager.setSnackbarManager(null);
+                    localeManager.showSnackbarForDeviceSearchEngineUpdate();
+                    Assert.assertFalse(snackbarManager.isShowing());
+
+                    localeManager.setSnackbarManager(snackbarManager);
+
+                    Assert.assertTrue(snackbarManager.isShowing());
+                    Snackbar currentSnackbar = snackbarManager.getCurrentSnackbarForTesting();
+                    Assert.assertEquals(
+                            currentSnackbar.getIdentifierForTesting(),
+                            Snackbar.UMA_SEARCH_ENGINE_CHANGED_NOTIFICATION);
+                });
     }
 }
