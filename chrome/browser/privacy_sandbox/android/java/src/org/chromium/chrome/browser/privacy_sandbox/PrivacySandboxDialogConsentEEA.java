328e931bd5f0fd6e8af2453d8858ef02ac03df2a
Jacob Stanley <jacobstanley@google.com>
2024-11-11 20:26:58 +0000

    Cleaning up TODO comment after it was already fixed.
    Bug was already fixed in this CL: https://chromium-review.googlesource.com/c/chromium/src/+/5876180, but the TODO was never removed.
    
    Change-Id: Idd6bbdb5c09585bcaf0c1e9449b61d14f90d56e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012923
    Auto-Submit: Jacob Stanley <jacobstanley@google.com>
    Reviewed-by: Andrey Zaytsev <andzaytsev@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381357}

--- chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogConsentEEA.java
+++ chrome/browser/privacy_sandbox/android/java/src/org/chromium/chrome/browser/privacy_sandbox/PrivacySandboxDialogConsentEEA.java
@@ -273,7 +273,6 @@ public class PrivacySandboxDialogConsentEEA extends ChromeDialog
                                         errorCode);
                             }
                         };
-                // TODO(crbug.com/366010532): Add in functionality to add language code to the url
                 mThinWebView =
                         PrivacySandboxDialogController.createThinWebView(
                                 mWebContents, mProfile, mActivityWindowAndroid, privacyPolicyUrl);
