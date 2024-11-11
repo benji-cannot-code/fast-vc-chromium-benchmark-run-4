f70852a75d3fb19cce57dc8f3643561a1fbc6ebb
Mason Freed <masonf@chromium.org>
2024-11-11 20:55:01 +0000

    Fix crash for parseparts without DOMPartsAPI enabled
    Fixed: 372736019
    Change-Id: Ic8c36b1a5b98ebbbc4eac55db9b7105fdd03a910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006198
    Commit-Queue: Mason Freed <masonf@chromium.org>
    Reviewed-by: Di Zhang <dizhangg@chromium.org>
    Commit-Queue: Di Zhang <dizhangg@chromium.org>
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381378}

--- third_party/blink/renderer/core/dom/element.cc
+++ third_party/blink/renderer/core/dom/element.cc
@@ -7245,6 +7245,7 @@ void Element::SetInnerHTMLInternal(
       if (auto* template_element = DynamicTo<HTMLTemplateElement>(*this)) {
         container = template_element->content();
         swap_dom_parts =
+            RuntimeEnabledFeatures::DOMPartsAPIEnabled() &&
             template_element->hasAttribute(html_names::kParsepartsAttr);
       }
       ReplaceChildrenWithFragment(container, fragment, exception_state);
