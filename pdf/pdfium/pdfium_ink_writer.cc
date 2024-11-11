6e6a4aad4cb74cb21106ba97da2782aa044fe4f7
Lei Zhang <thestig@chromium.org>
2024-11-11 19:41:42 +0000

    [PDF Ink Signatures] Fix in-memory page object opacity
    When writing ink::Strokes into an in-memory page as FPDF_PAGEOBJECTs,
    SetBrushPropertiesForPath() tries to set the alpha value from the
    ink::Brush's color. However, the alpha value is actually stored in the
    ink::BrushTip's opacity_multiplier field, as seen in various unit tests.
    
    Use that as the alpha value, instead of the brush color's alpha value,
    which is always opaque.
    
    Bug: 378464305
    Change-Id: I542b6523402b33eb5ecfded2e5a30a059b91d772
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6013099
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Reviewed-by: Andy Phan <andyphan@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alan Screen <awscreen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381320}

--- pdf/pdfium/pdfium_ink_writer.cc
+++ pdf/pdfium/pdfium_ink_writer.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "pdf/pdf_ink_constants.h"
 #include "pdf/pdf_ink_conversions.h"
 #include "pdf/pdf_ink_transform.h"
+#include "third_party/ink/src/ink/brush/brush_coat.h"
+#include "third_party/ink/src/ink/brush/brush_tip.h"
 #include "third_party/ink/src/ink/geometry/mesh.h"
 #include "third_party/ink/src/ink/geometry/modeled_shape.h"
 #include "third_party/ink/src/ink/geometry/point.h"
@@ -138,11 +140,20 @@ std::vector<ScopedFPDFPageObject> WriteShapeToNewPathsOnPage(
 }
 
 void SetBrushPropertiesForPath(const ink::Brush& brush, FPDF_PAGEOBJECT path) {
-  // TODO(crbug.com/353942910) Write out the brush type and size.
-  SkColor color = GetSkColorFromInkBrush(brush);
-  bool result =
-      FPDFPageObj_SetFillColor(path, SkColorGetR(color), SkColorGetG(color),
-                               SkColorGetB(color), SkColorGetA(color));
+  const SkColor color = GetSkColorFromInkBrush(brush);
+  CHECK_EQ(SkColorGetA(color), SK_AlphaOPAQUE);
+
+  CHECK_EQ(brush.CoatCount(), 1u);
+  const ink::BrushCoat& coat = brush.GetCoats()[0];
+  CHECK_EQ(coat.tips.size(), 1u);
+  // third_party/ink/src/ink/brush/brush_tip.h says this can have a value up to
+  // 2.0f, but that should never be the case, as //pdf code never sets it that
+  // high.
+  CHECK_LE(coat.tips[0].opacity_multiplier, 1.0f);
+
+  bool result = FPDFPageObj_SetFillColor(path, SkColorGetR(color),
+                                         SkColorGetG(color), SkColorGetB(color),
+                                         coat.tips[0].opacity_multiplier * 255);
   CHECK(result);
 }
 
