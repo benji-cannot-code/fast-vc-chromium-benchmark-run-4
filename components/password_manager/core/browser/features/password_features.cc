f3af28cca1564587409d030be802ba3061c7d0ac
Ioana Pandele <ioanap@chromium.org>
2024-11-11 18:07:30 +0000

    [UPMLocalPwd] Drop the login db rename requirement for syncing users
    When migrating healthy syncing users to UPM with split stores, the
    profile login DB was renamed to the account login DB  and kept around
    as a fallback, in case of feature rollback. The feature rolled out a
    while ago, and in the meantime the login DBs for all migrated users
    have been cleared, so the rename step no longer makes sense.
    
    Bug: 378421883
    Change-Id: I6fd163b478b5d9eb059d885a8c6785cae0b37661
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012360
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381277}

--- components/password_manager/core/browser/features/password_features.cc
+++ components/password_manager/core/browser/features/password_features.cc
@@ -158,6 +158,9 @@ BASE_FEATURE(kBiometricAuthIdentityCheck,
 BASE_FEATURE(kClearLoginDatabaseForAllMigratedUPMUsers,
              "ClearLoginDatabaseForAllMigratedUPMUsers",
              base::FEATURE_ENABLED_BY_DEFAULT);
+BASE_FEATURE(kDropLoginDbRenameForUpmSyncingUsers,
+             "DropLoginDbRenameForUpmSyncingUsers",
+             base::FEATURE_ENABLED_BY_DEFAULT);
 #endif
 
 BASE_FEATURE(kUsernameFirstFlowFallbackCrowdsourcing,
