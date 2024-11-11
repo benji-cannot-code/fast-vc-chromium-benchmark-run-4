baf7b35b7b3b6989f309e649f1161e6f67f5a412
Zachary Tan <tanzachary@chromium.org>
2024-11-11 18:36:25 +0000

    [FedCM] Make account rows appear disabled in active mode verify sheet
    Screenshot
    https://issues.chromium.org/action/issues/377921411/attachments/60715885?download=false
    
    Bug: 377921411
    Change-Id: I4c6a8d4618b0b1c32cad11978cd586173aa85d23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6007564
    Commit-Queue: Zachary Tan <tanzachary@chromium.org>
    Reviewed-by: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381291}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -467,7 +467,9 @@ void AccountSelectionModalView::ShowVerifyingSheet(
     // chooser.
     if (std::string(child->GetClassName()) == "HoverButton") {
       is_single_account_chooser = true;
-      has_spinner_ |= static_cast<AccountHoverButton*>(child)->HasSpinner();
+      AccountHoverButton* button = static_cast<AccountHoverButton*>(child);
+      has_spinner_ |= button->HasSpinner();
+      button->SetDisabledOpacity();
     }
     child->SetEnabled(false);
   }
@@ -480,7 +482,9 @@ void AccountSelectionModalView::ShowVerifyingSheet(
     views::View* contents = wrapper->children()[0];
     for (const auto& child : contents->children()) {
       if (std::string(child->GetClassName()) == "HoverButton") {
-        has_spinner_ |= static_cast<AccountHoverButton*>(child)->HasSpinner();
+        AccountHoverButton* button = static_cast<AccountHoverButton*>(child);
+        has_spinner_ |= button->HasSpinner();
+        button->SetDisabledOpacity();
       }
       child->SetEnabled(false);
     }
