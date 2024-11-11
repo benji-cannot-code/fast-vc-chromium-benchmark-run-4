6dc4474710b8fe8605296f5f0071a52a41b5f89a
Oksana Zhuravlova <oksamyt@chromium.org>
2024-11-11 20:57:13 +0000

    [tracing] Add task_start_time to chrome_input_pipeline_steps
    This CL adds a task_start_time column to the chrome_input_pipeline_steps
    table. This column will be used in the creation of the timeline for
    scroll jank analysis.
    
    PERFETTO_TESTS=`autoninja -C out/Default perfetto_diff_tests && out/Default/bin/run_perfetto_diff_tests`
    
    Bug: b:369829860, b:376679369
    Change-Id: I0a8aa995952b3d96ae4112ac62cd46e333dcfea6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001555
    Reviewed-by: Alexander Timin <altimin@chromium.org>
    Reviewed-by: Tushar Agarwal <agarwaltushar@google.com>
    Commit-Queue: Oksana Zhuravlova <oksamyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381379}

--- base/tracing/stdlib/chrome/input.sql
+++ base/tracing/stdlib/chrome/input.sql
@@ -20,7 +20,9 @@ CREATE PERFETTO TABLE _chrome_input_pipeline_steps_no_input_type(
   -- Step name (ChromeLatencyInfo.step).
   step STRING,
   -- Input type.
-  input_type STRING
+  input_type STRING,
+  -- Start time of the parent Chrome scheduler task (if any) of this step.
+  task_start_time_ts INT
 ) AS
 SELECT
   EXTRACT_ARG(thread_slice.arg_set_id, 'chrome_latency_info.trace_id') AS latency_id,
@@ -29,7 +31,8 @@ SELECT
   dur,
   utid,
   EXTRACT_ARG(thread_slice.arg_set_id, 'chrome_latency_info.step') AS step,
-  EXTRACT_ARG(thread_slice.arg_set_id, 'chrome_latency_info.input_type') AS input_type
+  EXTRACT_ARG(thread_slice.arg_set_id, 'chrome_latency_info.input_type') AS input_type,
+  ts - (EXTRACT_ARG(thread_slice.arg_set_id, 'current_task.event_offset_from_task_start_time_us') * 1000) AS task_start_time_ts
 FROM
   thread_slice
 WHERE
@@ -70,7 +73,9 @@ CREATE PERFETTO TABLE chrome_input_pipeline_steps(
   -- Step name (ChromeLatencyInfo.step).
   step STRING,
   -- Input type.
-  input_type STRING
+  input_type STRING,
+  -- Start time of the parent Chrome scheduler task (if any) of this step.
+  task_start_time_ts INT
 ) AS
 SELECT
   latency_id,
@@ -79,7 +84,8 @@ SELECT
   dur,
   utid,
   step,
-  chrome_inputs.input_type AS input_type
+  chrome_inputs.input_type AS input_type,
+  task_start_time_ts
 FROM
   chrome_inputs
 LEFT JOIN
