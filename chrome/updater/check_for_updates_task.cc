55d902cfc212ef69914eefba332f1ff483222f9f
S. Ganesh <ganesh@chromium.org>
2024-11-11 14:28:26 +0000

    updater: reduce jitter to [0, 1) seconds, jitter all background tasks
    Since the scheduled tasks on mac and Windows both support second
    granularity, this CL reduces the jitter on background tasks to [0, 1)
    seconds instead of [0, 60) seconds.
    
    In addition, this CL also moves the jitter out of the update-check task
    and into every task in the `RunPeriodicTasks` pipeline, so that domain
    enrollment, policy fetching, etc are also jittered.
    
    Bug: 373413318
    Change-Id: I8aa582bb1f0eaf3b499831da4937e4eb292c5f09
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006341
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381157}

--- chrome/updater/check_for_updates_task.cc
+++ chrome/updater/check_for_updates_task.cc
@@ -79,7 +79,7 @@ void CheckForUpdatesTask::Run(base::OnceClosure callback) {
     return;
   }
 
-  base::SequencedTaskRunner::GetCurrentDefault()->PostDelayedTask(
+  base::SequencedTaskRunner::GetCurrentDefault()->PostTask(
       FROM_HERE,
       base::BindOnce(
           std::move(update_checker_),
@@ -89,8 +89,7 @@ void CheckForUpdatesTask::Run(base::OnceClosure callback) {
                 VLOG(0) << task_name << " task complete: " << result;
                 std::move(closure).Run();
               },
-              std::move(callback), task_name_)),
-      config_->InitialDelay());
+              std::move(callback), task_name_)));
 }
 
 }  // namespace updater
