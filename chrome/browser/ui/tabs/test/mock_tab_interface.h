c65ada5914f619542589027cab533ce31e424dbc
Thomas Lukaszewicz <tluk@chromium.org>
2024-11-11 17:06:11 +0000

    [tabs] Make MockTabInterface a NiceMock
    TabInterface can incur a high degree of uninteresting calls during
    testing. This CL makes this mock a NiceMock by default to suppress
    excessive warnings.
    
    Bug: None
    Change-Id: I926986441130ad47e2a5e3b45057ba69056c3ef1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010328
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Thomas Lukaszewicz <tluk@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381230}

--- chrome/browser/ui/tabs/test/mock_tab_interface.h
+++ chrome/browser/ui/tabs/test/mock_tab_interface.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace tabs {
 
-class MockTabInterface : public TabInterface {
+class MockTabInterface : public testing::NiceMock<TabInterface> {
  public:
   MockTabInterface();
   ~MockTabInterface();
