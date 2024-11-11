6446e326725339309be817870bcbe85207af35f6
Timofey Chudakov <tchudakov@google.com>
2024-11-11 19:28:23 +0000

    HaTS survey after the user has created 3rd+ plus address.
    This CL adds functionality to show the feature perception survey after
    the user has created their 3rd+ plus address.
    
    Bug: 348139343
    Change-Id: I5a34358454f7a30b000edb47cc1995c0f079092d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5991436
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381314}

--- chrome/browser/ui/hats/survey_config.h
+++ chrome/browser/ui/hats/survey_config.h
@@ -77,6 +77,7 @@ extern const char kHatsSurveyTriggerSafetyHubAndroid[];
 
 extern const char kHatsSurveyTriggerPermissionsPrompt[];
 extern const char kHatsSurveyTriggerPlusAddressAcceptedFirstTimeCreate[];
+extern const char kHatsSurveyTriggerPlusAddressCreatedMultiplePlusAddresses[];
 extern const char kHatsSurveyTriggerPlusAddressDeclinedFirstTimeCreate[];
 extern const char kHatsSurveyTriggerPrivacySandboxSentimentSurvey[];
 
