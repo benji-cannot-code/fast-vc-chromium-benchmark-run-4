8821b23bdb7cd12583e88af850d372bb1349f55d
Morten Stenshorne <mstensho@chromium.org>
2024-11-11 11:00:51 +0000

    [CodeHealth] Remove stale base::Feature DeviceForceScheduledReboot
    Enabled by default since M104.
    
    Bug: 356234162
    Change-Id: Ia4b3e7da45c33649fc6a7b3eab4b04a5ec1d516d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5982552
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381108}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -734,9 +734,6 @@ extern const char kDesktopPWAsScopeExtensionsDescription[];
 extern const char kDesktopPWAsBorderlessName[];
 extern const char kDesktopPWAsBorderlessDescription[];
 
-extern const char kDeviceForceScheduledRebootName[];
-extern const char kDeviceForceScheduledRebootDescription[];
-
 extern const char kDevicePostureName[];
 extern const char kDevicePostureDescription[];
 
