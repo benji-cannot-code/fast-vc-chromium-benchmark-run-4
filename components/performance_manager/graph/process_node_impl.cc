967f7eb9cb2e4213e3d020dbf0826841517578c5
Joe Mason <joenotcharles@google.com>
2024-11-11 17:06:38 +0000

    Add CreateProcessTracingTrack helper function
    This function can create named sub-process tracks in the browser
    process.
    
    Bug: 374302723
    Change-Id: I35e716e0f44cb15fbc7c45f4364871d3542930d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000793
    Reviewed-by: Etienne Pierre-Doray <etiennep@chromium.org>
    Commit-Queue: Joe Mason <joenotcharles@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381231}

--- components/performance_manager/graph/process_node_impl.cc
+++ components/performance_manager/graph/process_node_impl.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "components/performance_manager/graph/process_node_impl.h"
 
+#include <optional>
 #include <utility>
 
 #include "base/check_op.h"
@@ -23,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace performance_manager {
 
@@ -43,7 +45,9 @@ content::ProcessType ValidateBrowserChildProcessType(
 ProcessNodeImpl::ProcessNodeImpl(BrowserProcessNodeTag tag)
     : ProcessNodeImpl(content::PROCESS_TYPE_BROWSER,
                       AnyChildProcessHostProxy{},
-                      base::TaskPriority::HIGHEST) {}
+                      base::TaskPriority::HIGHEST) {
+  tracing_track_.emplace(perfetto::ProcessTrack::Current());
+}
 
 ProcessNodeImpl::ProcessNodeImpl(RenderProcessHostProxy proxy,
                                  base::TaskPriority priority)
@@ -182,6 +186,15 @@ void ProcessNodeImpl::RequestSharedPerformanceScenarioRegions(
     uint64_t process_track_id,
     RequestSharedPerformanceScenarioRegionsCallback callback) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+
+  // Should not be called for the Browser process, which already has a track.
+  // Otherwise, it's ok to overwrite `tracing_track_`, since processes can be
+  // re-initialized for the same ProcessNode (eg. after a crash).
+  // TODO(crbug.com/374302723): Rename the Mojo message that sets this, since
+  // the track ID is used for more than PerformanceScenarios now.
+  CHECK_NE(process_type_, content::PROCESS_TYPE_BROWSER);
+  tracing_track_.emplace(perfetto::Track::Global(process_track_id));
+
   if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kSingleProcess)) {
     // The "child process" is actually running in the same process space. The
@@ -192,9 +205,8 @@ void ProcessNodeImpl::RequestSharedPerformanceScenarioRegions(
                             base::ReadOnlySharedMemoryRegion());
     return;
   }
-  std::move(callback).Run(
-      GetGlobalSharedScenarioRegion(),
-      GetSharedScenarioRegionForProcessNode(this, process_track_id));
+  std::move(callback).Run(GetGlobalSharedScenarioRegion(),
+                          GetSharedScenarioRegionForProcessNode(this));
 }
 
 content::ProcessType ProcessNodeImpl::GetProcessType() const {
@@ -294,6 +306,11 @@ ProcessNode::NodeSetView<WorkerNodeImpl*> ProcessNodeImpl::worker_nodes()
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK_EQ(process_type_, content::PROCESS_TYPE_RENDERER);
   return NodeSetView<WorkerNodeImpl*>(worker_nodes_);
+}
+
+std::optional<perfetto::Track> ProcessNodeImpl::tracing_track() const {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return tracing_track_;
 }
 
 void ProcessNodeImpl::SetProcessExitStatus(int32_t exit_status) {
--- components/performance_manager/graph/process_node_impl.h
+++ components/performance_manager/graph/process_node_impl.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/abseil-cpp/absl/types/variant.h"
 #include "third_party/blink/public/common/tokens/tokens.h"
+#include "third_party/perfetto/include/perfetto/tracing/track.h"
 
 namespace performance_manager {
 
@@ -131,6 +132,7 @@ class ProcessNodeImpl
   // Private implementation properties.
   NodeSetView<FrameNodeImpl*> frame_nodes() const;
   NodeSetView<WorkerNodeImpl*> worker_nodes() const;
+  std::optional<perfetto::Track> tracing_track() const;
 
   void SetProcessExitStatus(int32_t exit_status);
   void SetProcessMetricsName(const std::string& metrics_name);
@@ -250,6 +252,10 @@ class ProcessNodeImpl
   // A bit field that indicates which type of content this process has hosted,
   // either currently or in the past.
   ContentTypes hosted_content_types_ GUARDED_BY_CONTEXT(sequence_checker_);
+
+  // The Perfetto ProcessTrack for this process.
+  std::optional<perfetto::Track> tracing_track_
+      GUARDED_BY_CONTEXT(sequence_checker_);
 
   NodeSet frame_nodes_ GUARDED_BY_CONTEXT(sequence_checker_);
 
