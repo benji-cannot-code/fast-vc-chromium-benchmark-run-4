82434a245124fba29b8510d6739a502803d8cb49
Duncan Mercer <mercerd@google.com>
2024-11-11 14:31:25 +0000

    [lensoverlay] Hide searchbox on image context menu entry
    Bug: 378127207
    Change-Id: Iee0878ac0b458452485e58a241da92d041b9c8d9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006675
    Auto-Submit: Duncan Mercer <mercerd@google.com>
    Reviewed-by: Nihar Majmudar <niharm@google.com>
    Commit-Queue: Nihar Majmudar <niharm@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381158}

--- chrome/browser/resources/lens/overlay/lens_overlay_app.ts
+++ chrome/browser/resources/lens/overlay/lens_overlay_app.ts
@@ -88,6 +88,8 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
       },
       searchBoxHidden: {
         type: Boolean,
+        computed:
+            'computeShouldHideSearchBox(isTranslateModeActive, sidePanelOpened, forceHideSearchBox)',
         reflectToAttribute: true,
       },
       isClosing: {
@@ -147,9 +149,11 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
   private initialFlashAnimationHasEnded: boolean = false;
   // Whether the side panel has been opened.
   private sidePanelOpened: boolean = false;
-  // Whether the search box should be hidden. Updated on overlay selection and
-  // translate mode state change.
+  // Whether the search box should be hidden.
   private searchBoxHidden: boolean = false;
+  // Whether the search box should be forced to hide. Used to prevent the search
+  // box from showing when we know the side panel will be opened.
+  private forceHideSearchBox: boolean = false;
   // Whether the overlay is being shut down.
   private isClosing: boolean = false;
   // Whether more options menu should be shown.
@@ -221,8 +225,6 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
     this.eventTracker_.add(
         document, 'translate-mode-state-changed', (e: CustomEvent) => {
           this.isTranslateModeActive = e.detail.translateModeEnabled;
-          this.searchBoxHidden =
-              this.isTranslateModeActive || this.sidePanelOpened;
         });
     this.eventTracker_.add(document, 'text-copied', () => {
       this.showToast(this.i18n('copyToastMessage'));
@@ -311,13 +313,13 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
     const newSearchboxWidth = areSuggestionsShowing ?
         suggestionsContainer.offsetWidth :
         (this.$.searchboxGhostLoader.offsetWidth > 0 ?
-        this.$.searchboxGhostLoader.offsetWidth:
-        this.$.searchbox.offsetWidth);
+             this.$.searchboxGhostLoader.offsetWidth :
+             this.$.searchbox.offsetWidth);
     const newSearchboxHeight = areSuggestionsShowing ?
         suggestionsContainer.offsetHeight :
         (this.$.searchboxGhostLoader.offsetHeight > 0 ?
-          this.$.searchboxGhostLoader.offsetHeight:
-          this.$.searchbox.offsetHeight);
+             this.$.searchboxGhostLoader.offsetHeight :
+             this.$.searchbox.offsetHeight);
 
     // Get the top and left position of the searchbox relative to the selection
     // overlay.
@@ -431,7 +433,7 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
   private handleSelectionStarted() {
     this.$.cursorTooltip.setPauseTooltipChanges(true);
     this.isPointerDown = true;
-    this.searchBoxHidden = true;
+    this.forceHideSearchBox = true;
   }
 
   // The user finished making their selection on the selection overlay.
@@ -452,6 +454,11 @@ export class LensOverlayAppElement extends LensOverlayAppElementBase {
 
   private computeShouldFadeOutButtons(): boolean {
     return !this.isTranslateModeActive && this.isPointerDown;
+  }
+
+  private computeShouldHideSearchBox(): boolean {
+    return this.isTranslateModeActive || this.sidePanelOpened ||
+        this.forceHideSearchBox;
   }
 
   private async showToast(message: string) {
