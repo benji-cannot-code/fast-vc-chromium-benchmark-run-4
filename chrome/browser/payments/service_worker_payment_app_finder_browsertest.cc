5155fa49458e1b84f2a464103d6e17368cca4c7e
Stephen McGruer <smcgruer@chromium.org>
2024-11-11 16:46:39 +0000

    Clean up launched kPaymentHandlerAlwaysRefreshIcon flag
    This feature was fully launched in M117, and so its time to clean up the
    flag and related code. This isn't a pure cleanup - it also removes the
    synchronous refetch of missing icons for payment apps. There really
    shouldn't be any of those left in the wild anyway, but for any that do
    exist we now rely on the asynchronous icon-refresh path there too.
    
    Bug: 40259220
    Change-Id: Id1196ea00b4faec9237498c574d8e051c842c98e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5013984
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Stephen McGruer <smcgruer@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381222}

--- chrome/browser/payments/service_worker_payment_app_finder_browsertest.cc
+++ chrome/browser/payments/service_worker_payment_app_finder_browsertest.cc
@@ -864,10 +864,7 @@ IN_PROC_BROWSER_TEST_F(ServiceWorkerPaymentAppFinderBrowserTest,
 class ServiceWorkerPaymentAppFinderIconRefreshBrowserTest
     : public ServiceWorkerPaymentAppFinderBrowserTest {
  public:
-  ServiceWorkerPaymentAppFinderIconRefreshBrowserTest() {
-    scoped_feature_list_.InitAndEnableFeature(
-        features::kPaymentHandlerAlwaysRefreshIcon);
-  }
+  ServiceWorkerPaymentAppFinderIconRefreshBrowserTest() = default;
   ~ServiceWorkerPaymentAppFinderIconRefreshBrowserTest() override = default;
 
   content::InstalledPaymentAppsFinder::PaymentApps GetInstalledPaymentApps() {
@@ -884,9 +881,6 @@ class ServiceWorkerPaymentAppFinderIconRefreshBrowserTest
 
     return apps;
   }
-
- private:
-  base::test::ScopedFeatureList scoped_feature_list_;
 };
 
 IN_PROC_BROWSER_TEST_F(ServiceWorkerPaymentAppFinderIconRefreshBrowserTest,
