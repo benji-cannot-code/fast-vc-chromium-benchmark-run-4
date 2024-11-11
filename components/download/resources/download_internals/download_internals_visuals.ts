57af9499f6b7c695d89bbba6bf9d97d68652825f
dpapad <dpapad@chromium.org>
2024-11-11 21:19:38 +0000

    DownloadInternals: Migrate from jstemplate to Lit templates.
    This is part of the effort to fully remove the obsolete
    third_party/jstemplate dependency from the repository.
    
    Bug: 40433145
    Change-Id: I229456b74023e7901aea189ecb76f9fb1f2db75c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6005935
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1381392}

--- components/download/resources/download_internals/download_internals_visuals.ts
+++ components/download/resources/download_internals/download_internals_visuals.ts
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import type {ServiceEntry, ServiceRequest} from './download_internals_browser_proxy.js';
 import {DriverEntryState, ServiceEntryResult, ServiceEntryState, ServiceRequestResult} from './download_internals_browser_proxy.js';
 
-function getOngoingServiceEntryClass(entry: ServiceEntry) {
+export function getOngoingServiceEntryClass(entry: ServiceEntry) {
   switch (entry.state) {
     case ServiceEntryState.NEW:
       return 'service-entry-new';
@@ -28,7 +28,7 @@ function getOngoingServiceEntryClass(entry: ServiceEntry) {
   }
 }
 
-function getFinishedServiceEntryClass(entry: ServiceEntry) {
+export function getFinishedServiceEntryClass(entry: ServiceEntry) {
   switch (entry.result) {
     case ServiceEntryResult.SUCCEED:
       return 'service-entry-success';
@@ -37,7 +37,7 @@ function getFinishedServiceEntryClass(entry: ServiceEntry) {
   }
 }
 
-function getServiceRequestClass(request: ServiceRequest) {
+export function getServiceRequestClass(request: ServiceRequest) {
   switch (request.result) {
     case ServiceRequestResult.ACCEPTED:
       return 'service-entry-success';
@@ -48,13 +48,3 @@ function getServiceRequestClass(request: ServiceRequest) {
       return 'service-entry-fail';
   }
 }
-
-// Expose these on |window| because they need to be accessed by jstemplate code
-// in download_internals.html
-Object.assign(window, {
-  downloadInternalsVisuals: {
-    getServiceRequestClass,
-    getFinishedServiceEntryClass,
-    getOngoingServiceEntryClass,
-  },
-});
