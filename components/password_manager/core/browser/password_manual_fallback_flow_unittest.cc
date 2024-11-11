b5c42de6ca1d494aeeba92fd95b8edf46a0f4f26
Bruno Braga <brunobraga@google.com>
2024-11-11 10:10:35 +0000

    [mulhet] Log type of suggestion accepted
    Bug: 330304589
    Change-Id: I60f1e4a0502f3c25657c69f647c8f7c06a10da1e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003474
    Reviewed-by: Maria Kazinova <kazinova@google.com>
    Commit-Queue: Maria Kazinova <kazinova@google.com>
    Auto-Submit: Bruno Braga <brunobraga@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381087}

--- components/password_manager/core/browser/password_manual_fallback_flow_unittest.cc
+++ components/password_manager/core/browser/password_manual_fallback_flow_unittest.cc
@@ -1398,6 +1398,9 @@ TEST_P(PasswordManualFallbackFlowFillAfterSuggestionMetricsTest,
                         .row = 0,
                         .sub_popup_level = 0,
                         .from_search_result = SuggestionFromSearchResult()});
+    histograms.ExpectUniqueSample("Autofill.Suggestions.AcceptedType",
+                                  SuggestionType::kPasswordFieldByFieldFilling,
+                                  1);
     histograms.ExpectUniqueSample(
         "PasswordManager.ManualFallback.AcceptedSuggestion.SearchInputUsed",
         SuggestionFromSearchResult(), 1);
