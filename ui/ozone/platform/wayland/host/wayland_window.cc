f34a16b252706ae8b4fac00fa9057d096f632d0d
Maksim Sisov <msisov@igalia.com>
2024-11-12 07:58:20 +0000

    Remove zaura popup from //ui/ozone/platform/wayland
    Lacros has sunset. ZAuraPopup was a Wayland object used to support
    lacros. It has no use for Linux/Wayland as the protocol is only
    implemented by exo. Remove this and all the related code.
    
    Bug: 374244479, 373971535
    Change-Id: Id75d1a864939a1d21064b9667bc157f708d01e87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004960
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1381629}

--- ui/ozone/platform/wayland/host/wayland_window.cc
+++ ui/ozone/platform/wayland/host/wayland_window.cc
@@ -183,6 +183,8 @@ void WaylandWindow::OnEnteredOutputScaleChanged() {
   UpdateWindowScale(/*update_bounds=*/true);
 }
 
+void WaylandWindow::PropagateBufferScale(float new_scale) {}
+
 WaylandZAuraSurface* WaylandWindow::GetZAuraSurface() {
   return root_surface_ ? root_surface_->zaura_surface() : nullptr;
 }
--- ui/ozone/platform/wayland/test/test_zaura_shell.cc
+++ ui/ozone/platform/wayland/test/test_zaura_shell.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/ozone/platform/wayland/test/mock_xdg_surface.h"
 #include "ui/ozone/platform/wayland/test/server_object.h"
 #include "ui/ozone/platform/wayland/test/test_output.h"
-#include "ui/ozone/platform/wayland/test/test_zaura_popup.h"
 #include "ui/ozone/platform/wayland/test/test_zaura_surface.h"
 #include "ui/ozone/platform/wayland/test/test_zaura_toplevel.h"
 
@@ -48,21 +47,12 @@ void GetAuraToplevelForXdgToplevel(wl_client* client,
       GetUserDataAs<TestZAuraToplevel>(zaura_toplevel_resource));
 }
 
-void GetAuraPopupForXdgPopup(wl_client* client,
-                             wl_resource* resource,
-                             uint32_t id,
-                             wl_resource* popup) {
-  CreateResourceWithImpl<TestZAuraPopup>(client, &zaura_popup_interface,
-                                         kZAuraShellVersion,
-                                         &kTestZAuraPopupImpl, id);
-}
-
 const struct zaura_shell_interface kTestZAuraShellImpl = {
     &GetAuraSurface,
     nullptr,
     &SurfaceSubmissionInPixelCoordinates,
     &GetAuraToplevelForXdgToplevel,
-    &GetAuraPopupForXdgPopup,
+    nullptr,
     &DestroyResource,
 };
 
