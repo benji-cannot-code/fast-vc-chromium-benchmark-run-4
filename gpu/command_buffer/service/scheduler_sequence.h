2b4ddbd370fbd5f1cdf05e00ed4d0d1e99a1048c
Yuzhu Shen <yzshen@chromium.org>
2024-11-12 19:38:02 +0000

    InProcessCommandBuffer: update sync point wait and release.
    The updated code moves away from using SyncPointManager and
    SyncPointClientState API. Instead, it goes through SingleTaskSequence
    (and therefore the underlying TaskGraph).
    
    Bug: 324276400
    Change-Id: Ib35aa70b73777f9eba1dce8c76703c1bc29b4dc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006409
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381919}

--- gpu/command_buffer/service/scheduler_sequence.h
+++ gpu/command_buffer/service/scheduler_sequence.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/raw_ptr.h"
 #include "base/task/single_thread_task_runner.h"
 #include "gpu/command_buffer/common/sync_token.h"
+#include "gpu/command_buffer/service/scheduler.h"
 #include "gpu/command_buffer/service/sequence_id.h"
 #include "gpu/command_buffer/service/single_task_sequence.h"
 #include "gpu/gpu_gles2_export.h"
@@ -28,7 +29,6 @@ class OverlayProcessorAndroid;
 }  // namespace viz
 
 namespace gpu {
-class Scheduler;
 
 // Selectively allow ScheduleTask if DefaultDisallowScheduleTaskOnCurrentThread
 // is used for a thread.
@@ -93,6 +93,11 @@ class GPU_GLES2_EXPORT SchedulerSequence : public SingleTaskSequence {
   SequenceId GetSequenceId() override;
   bool ShouldYield() override;
   void ScheduleTask(
+      gpu::TaskCallback task,
+      std::vector<SyncToken> sync_token_fences,
+      const SyncToken& release,
+      ReportingCallback report_callback = ReportingCallback()) override;
+  void ScheduleTask(
       base::OnceClosure task,
       std::vector<SyncToken> sync_token_fences,
       const SyncToken& release,
@@ -102,12 +107,15 @@ class GPU_GLES2_EXPORT SchedulerSequence : public SingleTaskSequence {
       std::vector<SyncToken> sync_token_fences,
       const SyncToken& release,
       ReportingCallback report_callback = ReportingCallback()) override;
+  void ContinueTask(gpu::TaskCallback task) override;
   void ContinueTask(base::OnceClosure task) override;
   [[nodiscard]] ScopedSyncPointClientState CreateSyncPointClientState(
       CommandBufferNamespace namespace_id,
       CommandBufferId command_buffer_id) override;
 
  private:
+  void ScheduleTaskImpl(Scheduler::Task task);
+
   const raw_ptr<Scheduler> scheduler_;
   const SequenceId sequence_id_;
   const bool target_thread_is_always_available_;
