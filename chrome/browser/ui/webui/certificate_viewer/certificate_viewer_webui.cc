e1d2be7f491ebc93576fe7fe9bcedf49f6d35762
Carlos IL <carlosil@chromium.org>
2024-11-12 18:08:16 +0000

    Cert Management UI V2: Plumb constraints to viewer for enterprise certs
    This also changes the CIDR representation in
    server_certificate_database.proto to use an IP (in bytes format) and a
    prefix length instead of a formatted string.
    
    Bug: 40928765
    Change-Id: If9a22a1f20c1f1120053488684841b0614ca07fb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002422
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: Carlos IL <carlosil@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381863}

--- chrome/browser/ui/webui/certificate_viewer/certificate_viewer_webui.cc
+++ chrome/browser/ui/webui/certificate_viewer/certificate_viewer_webui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string_view>
 #include <utility>
 
+#include "base/containers/span.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/i18n/time_formatting.h"
@@ -36,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/host_zoom_map.h"
 #include "content/public/browser/web_contents.h"
+#include "net/base/ip_address.h"
 #include "net/cert/x509_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/mojom/ui_base_types.mojom-shared.h"
@@ -601,7 +603,14 @@ void CertificateViewerDialogHandler::HandleGetCertificateMetadata(
       constraints.Append(base::Value(dns_constraint));
     }
     for (auto& cidr_constraint : cert_metadata_->constraints().cidrs()) {
-      constraints.Append(base::Value(cidr_constraint));
+      net::IPAddress ip(base::as_byte_span(cidr_constraint.ip()));
+      if (!ip.IsValid()) {
+        continue;
+      }
+      std::string cidr_string = ip.ToString();
+      cidr_string += "/";
+      cidr_string += base::NumberToString(cidr_constraint.prefix_length());
+      constraints.Append(base::Value(std::move(cidr_string)));
     }
     dict.Set("constraints", std::move(constraints));
   }
