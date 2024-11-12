22f6668bedeae14f18a76c685f1511b84574dee3
Brandon Wylie <wylieb@chromium.org>
2024-11-12 18:05:13 +0000

    [hub] Add user action for hub search widget
    Bug: 378554652
    Change-Id: I1aa1e2eda9cc28ed03f02c448a22097851a0c123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001140
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381862}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -109,6 +109,9 @@ public class SearchActivity extends AsyncInitializationActivity
             "QuickActionSearchWidget.LensQuery";
 
     @VisibleForTesting
+    /* package */ static final String USED_TEXT_FROM_HUB_WIDGET = "HubSearchWidget.Query";
+
+    @VisibleForTesting
     /* package */ static final String HISTOGRAM_LAUNCHED_WITH_QUERY =
             "Android.Omnibox.SearchActivity.LaunchedWithQuery";
 
@@ -773,6 +776,7 @@ public class SearchActivity extends AsyncInitializationActivity
 
                         // Tracked by Custom Tabs.
                     case IntentOrigin.CUSTOM_TAB -> null;
+                    case IntentOrigin.HUB -> USED_TEXT_FROM_HUB_WIDGET;
                     default -> null;
                 };
 
@@ -832,6 +836,7 @@ public class SearchActivity extends AsyncInitializationActivity
                         case IntentOrigin.CUSTOM_TAB -> ".CustomTab";
                         case IntentOrigin.QUICK_ACTION_SEARCH_WIDGET -> ".ShortcutsWidget";
                         case IntentOrigin.LAUNCHER -> ".Launcher";
+                        case IntentOrigin.HUB -> ".Hub";
                         default -> ".SearchWidget";
                     };
             RecordHistogram.recordEnumeratedHistogram(histogramName + suffix, sample, max);
