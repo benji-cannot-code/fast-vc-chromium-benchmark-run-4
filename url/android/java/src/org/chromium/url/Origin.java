2a15e8bc680e4a38a528560b5626af998d8185fb
Peter Birk Pakkenberg <pbirk@chromium.org>
2024-11-12 13:20:53 +0000

    Fix Origin.toString for large port numbers
    The old toString method would output negative port numbers.
    
    Fixed: 378635575
    Change-Id: I1fe0bf3527e8005fbda79c7c9f0d2ad8a9c12f58
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012447
    Auto-Submit: Peter Pakkenberg <pbirk@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381725}

--- url/android/java/src/org/chromium/url/Origin.java
+++ url/android/java/src/org/chromium/url/Origin.java
@@ -5,11 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.url;
 
+import androidx.annotation.NonNull;
+
 import org.jni_zero.CalledByNative;
 import org.jni_zero.JNINamespace;
 import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
+import java.util.Locale;
 import java.util.Objects;
 
 /** An origin is either a (scheme, host, port) tuple or is opaque. */
@@ -123,8 +126,11 @@ public class Origin {
      * "null" if it's opaque.
      */
     @Override
+    @NonNull
     public String toString() {
-        return isOpaque() ? "null" : String.format("%s://%s:%s", mScheme, mHost, mPort);
+        return isOpaque()
+                ? "null"
+                : String.format(Locale.ROOT, "%s://%s:%d", mScheme, mHost, getPort());
     }
 
     @CalledByNative
