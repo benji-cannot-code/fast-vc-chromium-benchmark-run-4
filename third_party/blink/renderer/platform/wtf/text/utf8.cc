adfc073e398cb9d9195a9b4d04f0afa6a7fc018d
Peter Boström <pbos@chromium.org>
2024-11-12 23:37:58 +0000

    Fix bad DUMP_WILL_BE_CHECKs in wtf/text/utf8.cc
    These should've been NOTREACHED(base::NotFatalUntil..)s, but
    unfortunately the current implementation of CHECKs contains a
    static_cast<bool>(condition) which instead casted a base::NotFatalUntil
    to always-true and therefore passing the CHECK rather than dumping due
    to should-be-unreachable code.
    
    Bug: 329702346
    Change-Id: I4a58dbf4648eec8d9f060df86ae5049c29ecd010
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011824
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382042}

--- third_party/blink/renderer/platform/wtf/text/utf8.cc
+++ third_party/blink/renderer/platform/wtf/text/utf8.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/check.h"
 #include "base/not_fatal_until.h"
+#include "base/notreached.h"
 #include "third_party/blink/renderer/platform/wtf/text/ascii_ctype.h"
 #include "third_party/blink/renderer/platform/wtf/text/character_names.h"
 #include "third_party/blink/renderer/platform/wtf/text/string_hasher.h"
@@ -166,7 +167,7 @@ ConversionResult ConvertUTF16ToUTF8(const UChar** source_start,
     } else {
       // TODO(crbug.com/329702346): Surrogate pairs cannot represent codepoints
       // higher than 0x10FFFF, so this should not be reachable.
-      DUMP_WILL_BE_CHECK(base::NotFatalUntil::M127);
+      NOTREACHED(base::NotFatalUntil::M134);
       bytes_to_write = 3;
       ch = kReplacementCharacter;
     }
@@ -350,7 +351,7 @@ ConversionResult ConvertUTF8ToUTF16(const char** source_start,
       // TODO(crbug.com/329702346): This should never happen;
       // InlineUTF8SequenceLength() can never return a value higher than 4, and
       // a 4-byte UTF-8 sequence can never encode anything higher than 0x10FFFF.
-      DUMP_WILL_BE_CHECK(base::NotFatalUntil::M127);
+      NOTREACHED(base::NotFatalUntil::M134);
       if (strict) {
         source -= utf8_sequence_length;  // return to the start
         result = kSourceIllegal;
