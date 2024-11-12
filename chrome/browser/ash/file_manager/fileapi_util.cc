4e89a9487e709467f3ba8e8067c1b8c16cfab0bd
Joel Hockey <joelhockey@chromium.org>
2024-11-12 00:23:47 +0000

    Add base_subdirs to file_chooser.mojom NativeFileInfo
    Android content-URIs do not contain the names of subdirs like paths in
    posix or windows do.
    
    This CL is a nop to add the field to mojom and update any affected
    clients. It is part of a series of CLs to get webkitRelativePath
    working for android content-URIs.
    
    Bug: 377716464
    Change-Id: Idaa6de86d81a705c4d7758929b34ea5ef2101a83
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000058
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381493}

--- chrome/browser/ash/file_manager/fileapi_util.cc
+++ chrome/browser/ash/file_manager/fileapi_util.cc
@@ -285,7 +285,8 @@ class ConvertSelectedFileInfoListToFileChooserFileInfoListImpl {
         chooser_info_list_.push_back(
             FileChooserFileInfo::NewNativeFile(NativeFileInfo::New(
                 selected_info_list[i].file_path,
-                base::UTF8ToUTF16(selected_info_list[i].display_name))));
+                base::UTF8ToUTF16(selected_info_list[i].display_name),
+                std::vector<std::u16string>())));
         continue;
       }
 
@@ -294,7 +295,8 @@ class ConvertSelectedFileInfoListToFileChooserFileInfoListImpl {
         chooser_info_list_.push_back(
             FileChooserFileInfo::NewNativeFile(NativeFileInfo::New(
                 selected_info_list[i].local_path,
-                base::UTF8ToUTF16(selected_info_list[i].display_name))));
+                base::UTF8ToUTF16(selected_info_list[i].display_name),
+                std::vector<std::u16string>())));
         continue;
       }
 
