11d296c65ff6a21fab167b980d48f451adbb3a0b
Nicolás Peña <npm@chromium.org>
2024-11-12 19:41:54 +0000

    [FedCM] Do not show spinner if input is protected try 2
    This CL moves the logic to show the spinner in the account button to the
    method in modal which shows the verifying sheet. We rely on a bool we
    store on the account button, which is set whenever the account button is
    clicked. Note that this means that the spinner might show if the user
    does something like select an account while input is protected and then
    choose a different account. This could be addressed by also storing the
    account in the account button and comparing it to the selected account,
    but we consider it to be a very particular edge case not worth the extra
    storage complexity. Now, if the input is ignored, no account button will
    show a spinner since it is only introduced when showing the verifying
    sheet.
    
    Fixed: 378691634, 377730706
    Change-Id: Ic5eb4559c365a9ec617601938a8f6815a67cc5ee
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015176
    Reviewed-by: Zachary Tan <tanzachary@chromium.org>
    Commit-Queue: Nicolás Peña <npm@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381922}

--- chrome/browser/ui/views/webid/account_selection_modal_view.cc
+++ chrome/browser/ui/views/webid/account_selection_modal_view.cc
@@ -468,7 +468,10 @@ void AccountSelectionModalView::ShowVerifyingSheet(
     if (std::string(child->GetClassName()) == "HoverButton") {
       is_single_account_chooser = true;
       AccountHoverButton* button = static_cast<AccountHoverButton*>(child);
-      has_spinner_ |= button->HasSpinner();
+      if (button->HasBeenClicked()) {
+        has_spinner_ = true;
+        button->ReplaceSecondaryViewWithSpinner();
+      }
       button->SetDisabledOpacity();
     }
     child->SetEnabled(false);
@@ -483,7 +486,10 @@ void AccountSelectionModalView::ShowVerifyingSheet(
     for (const auto& child : contents->children()) {
       if (std::string(child->GetClassName()) == "HoverButton") {
         AccountHoverButton* button = static_cast<AccountHoverButton*>(child);
-        has_spinner_ |= button->HasSpinner();
+        if (button->HasBeenClicked()) {
+          has_spinner_ = true;
+          button->ReplaceSecondaryViewWithSpinner();
+        }
         button->SetDisabledOpacity();
       }
       child->SetEnabled(false);
--- chrome/browser/ui/views/webid/account_selection_view_base.cc
+++ chrome/browser/ui/views/webid/account_selection_view_base.cc
@@ -461,18 +461,14 @@ void AccountHoverButton::OnPressed(const ui::Event& event) {
                                  button_position_,
                                  /*min=*/0,
                                  /*exclusive_max=*/10, /*buckets=*/11);
-  if (secondary_view()) {
-    has_spinner_ = true;
-    static_cast<AccountHoverButtonSecondaryView*>(secondary_view())
-        ->ReplaceWithSpinner();
-  }
+  has_been_clicked_ = true;
   if (callback_) {
     callback_.Run(event);
   }
 }
 
-bool AccountHoverButton::HasSpinner() {
-  return has_spinner_;
+bool AccountHoverButton::HasBeenClicked() {
+  return has_been_clicked_;
 }
 
 void AccountHoverButton::SetDisabledOpacity() {
@@ -500,6 +496,12 @@ void AccountHoverButton::SetDisabledOpacity() {
 
 bool AccountHoverButton::HasDisabledOpacity() {
   return is_appear_disabled_;
+}
+
+void AccountHoverButton::ReplaceSecondaryViewWithSpinner() {
+  has_spinner_ = true;
+  static_cast<AccountHoverButtonSecondaryView*>(secondary_view())
+      ->ReplaceWithSpinner();
 }
 
 AccountSelectionViewBase::AccountSelectionViewBase(
