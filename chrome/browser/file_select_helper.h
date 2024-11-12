66f38b3ee5106479629770129c0f4d3917544f26
Joel Hockey <joelhockey@chromium.org>
2024-11-12 00:23:58 +0000

    Migrate FolderUploadConfirmationView to DialogModel
    Using ui::DialogModel allows this dialog to be used by android.
    
    Bug: 377716464
    Change-Id: I7d3fa35e7c1e12d2d12030760817f191e24711b4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5998849
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381494}

--- chrome/browser/file_select_helper.h
+++ chrome/browser/file_select_helper.h
@@ -36,6 +36,7 @@ class WebContents;
 }
 
 namespace ui {
+class DialogModel;
 struct SelectedFileInfo;
 }
 
@@ -76,6 +77,7 @@ class FileSelectHelper : public base::RefCountedThreadSafe<
   friend class base::RefCountedThreadSafe<FileSelectHelper>;
   friend class base::DeleteHelper<FileSelectHelper>;
   friend class FileSelectHelperContactsAndroid;
+  friend class FileSelectHelperTest;
   friend struct content::BrowserThread::DeleteOnThread<
       content::BrowserThread::UI>;
 
@@ -110,6 +112,7 @@ class FileSelectHelper : public base::RefCountedThreadSafe<
       ContentAnalysisCompletionCallback_FolderUpload_OKBad);
   FRIEND_TEST_ALL_PREFIXES(FileSelectHelperTest, GetFileTypesFromAcceptType);
   FRIEND_TEST_ALL_PREFIXES(FileSelectHelperTest, MultipleFileExtensionsForMime);
+  FRIEND_TEST_ALL_PREFIXES(FileSelectHelperTest, ConfirmationDialog);
   FRIEND_TEST_ALL_PREFIXES(policy::DlpFilesControllerAshBrowserTest,
                            FilesUploadCallerPassed);
 
@@ -164,9 +167,11 @@ class FileSelectHelper : public base::RefCountedThreadSafe<
       const net::DirectoryLister::DirectoryListerData& data) override;
   void OnListDone(int error) override;
 
-  void LaunchConfirmationDialog(
+  std::unique_ptr<ui::DialogModel> CreateConfirmationDialog(
       const base::FilePath& path,
-      std::vector<ui::SelectedFileInfo> selected_files);
+      std::vector<blink::mojom::FileChooserFileInfoPtr> selected_files,
+      base::OnceCallback<
+          void(std::vector<blink::mojom::FileChooserFileInfoPtr>)> callback);
 
   // Cleans up and releases this instance. This must be called after the last
   // callback is received from the enumeration code.
