2431a734608a973d00285fe1da591befad9a347b
Aliona DANGLA <alionadangla@google.com>
2024-11-12 14:31:33 +0000

    [iOS] Reset the UI blocker of first run when the scene is disconnected
    The UI has been destroyed and the app is shutting down. First run
    profile agent been informed that the UI is about to be destroyed, but it
    didn't remove the ScopedUIBlocker causing shutdown crash. This CL solve
    this by removing the blocker when the UI is disabled.
    
    Fixed: 378468270
    Change-Id: Iab1234161669e5f94da88950658f58de9e9bcee5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011707
    Commit-Queue: Aliona Dangla <alionadangla@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381745}

--- ios/chrome/app/profile/first_run_profile_agent.mm
+++ ios/chrome/app/profile/first_run_profile_agent.mm
@@ -43,6 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark - SceneStateObserver
 
 - (void)sceneStateDidDisableUI:(SceneState*)sceneState {
+  _firstRunUIBlocker.reset();
+
   [_firstRunCoordinator stop];
   _firstRunCoordinator = nil;
 
