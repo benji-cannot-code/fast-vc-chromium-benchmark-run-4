d52c9602ca01edfa7594039ce8562101a0320d7f
Dana Fried <dfried@chromium.org>
2024-11-12 22:36:44 +0000

    [Test Automation] Create InSameContextAs() helper.
    This works like InContext() but instead of explicitly specifying the
    context, an element is located and its context is used.
    
    This can be used with a named element, or an element that is unique
    across contexts, such as an instrumented WebContents in a browser test.
    
    Bug: 378691113
    Change-Id: I816cc441326f266e23c7e6b15a7a50f2c4fc3bab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015420
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382008}

--- ui/base/interaction/interactive_test.h
+++ ui/base/interaction/interactive_test.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <variant>
 #include <vector>
 
+#include "base/functional/callback_helpers.h"
 #include "base/strings/strcat.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/bind.h"
@@ -416,9 +417,23 @@ class InteractiveTestApi {
   template <typename T>
   [[nodiscard]] static StepBuilder InSameContext(T&& step);
 
+  // Specifies that test step(s) should be executed in a specific context.
   [[nodiscard]] MultiStep InContext(ElementContext context, MultiStep steps);
   template <typename T>
   [[nodiscard]] StepBuilder InContext(ElementContext context, T&& step);
+
+  // Specifies that test step(s) should be executed in the same context as a
+  // specific `element`, which should be unique across contexts or a specific
+  // named element.
+  //
+  // NOTE: If the previous step already references the element, prefer
+  // `InSameContext()` as it has fewer limitations and handles elements that may
+  // be present in multiple contexts.
+  [[nodiscard]] static MultiStep InSameContextAs(ElementSpecifier element,
+                                                 MultiStep steps);
+  template <typename T>
+  [[nodiscard]] static MultiStep InSameContextAs(ElementSpecifier element,
+                                                 T&& step);
 
   // Specifies that these test step(s) should be executed as soon as they are
   // eligible to trigger, one after the other. By default, once a step is
@@ -782,6 +797,14 @@ InteractionSequence::StepBuilder InteractiveTestApi::InContext(
   const auto fmt = base::StringPrintf("InContext( %p, %%s )",
                                       static_cast<const void*>(context));
   return std::move(step.SetContext(context).FormatDescription(fmt));
+}
+
+// static
+template <typename T>
+InteractiveTestApi::MultiStep InteractiveTestApi::InSameContextAs(
+    ElementSpecifier element,
+    T&& step) {
+  return InSameContextAs(element, Steps(std::forward<T>(step)));
 }
 
 // static
