70e8f0c236cccf2316ee164a5f9f00841d8b5978
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-12 04:17:24 +0000

    Implement the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one was deprecated,
    this CL replaces it with the first one.
    
    Bug: 40249337
    Change-Id: I1fb3152b25957861184f47bf0b5c36219c90c278
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4802083
    Reviewed-by: Theresa Sullivan <twellington@chromium.org>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381583}

--- chrome/browser/undo/bookmark_undo_service_factory.cc
+++ chrome/browser/undo/bookmark_undo_service_factory.cc
@@ -48,7 +48,8 @@ BookmarkUndoServiceFactory::BookmarkUndoServiceFactory()
 
 BookmarkUndoServiceFactory::~BookmarkUndoServiceFactory() = default;
 
-KeyedService* BookmarkUndoServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+BookmarkUndoServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
-  return new BookmarkUndoService;
+  return std::make_unique<BookmarkUndoService>();
 }
--- chrome/browser/undo/bookmark_undo_service_factory.h
+++ chrome/browser/undo/bookmark_undo_service_factory.h
@@ -31,7 +31,7 @@ class BookmarkUndoServiceFactory : public ProfileKeyedServiceFactory {
   ~BookmarkUndoServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
