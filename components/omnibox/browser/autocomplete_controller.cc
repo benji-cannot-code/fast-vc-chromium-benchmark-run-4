1626565a97379ccf2a5e30652d8c1642e1d2e6e2
Brandon Wylie <wylieb@chromium.org>
2024-11-12 19:56:19 +0000

    [hub] Include history suggestions for typed queries
    * Add new feature param to include the history provider.
    * Add new section for history results.
    * Add integration test for e2e flow.
    * Add about_flags entries for both extra providers.
    
    Bug: 377751604
    Change-Id: I40e7f9cfa0c8ae8865897252b2644033eff77ac3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001584
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381929}

--- components/omnibox/browser/autocomplete_controller.cc
+++ components/omnibox/browser/autocomplete_controller.cc
@@ -1053,8 +1053,10 @@ bool AutocompleteController::ShouldRunProvider(
   if (omnibox::IsAndroidHub(input_.current_page_classification())) {
     return provider->type() == AutocompleteProvider::TYPE_SEARCH ||
            provider->type() == AutocompleteProvider::TYPE_OPEN_TAB ||
-           (omnibox::kAndroidHubSearchEnableBookmarkProvider.Get() &&
-            provider->type() == AutocompleteProvider::TYPE_BOOKMARK);
+           (OmniboxFieldTrial::kAndroidHubSearchEnableBookmarkProvider.Get() &&
+            provider->type() == AutocompleteProvider::TYPE_BOOKMARK) ||
+           (OmniboxFieldTrial::kAndroidHubSearchEnableHistoryProvider.Get() &&
+            provider->type() == AutocompleteProvider::TYPE_HISTORY_QUICK);
   }
 #endif
 
