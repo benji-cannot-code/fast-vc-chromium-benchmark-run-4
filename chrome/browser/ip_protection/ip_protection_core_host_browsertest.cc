afd7280254619165d47479c665a5b4526fde82f0
Dustin J. Mitchell <djmitche@chromium.org>
2024-11-12 00:39:07 +0000

    Add logging information for flaky test
    Test IpProtectionCoreHostBrowserTest.NetworkServiceCanRequestTokens is
    flaky, but the flakes do not reproduce locally. This adds some logging
    to help diagnose the issue in logs available from the flake dashboards.
    These dashboards appear to only show WARNING and above log messages.
    
    The new log messages are only in tests and /.*ForTesting/ methods, and
    will not appear in a running browser.
    
    Bug: 376827614
    Change-Id: Ib728736032c357c19ce6fa272ecc37ae0506b05e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011093
    Reviewed-by: Andrew Williams <awillia@chromium.org>
    Commit-Queue: Dustin Mitchell <djmitche@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381506}

--- chrome/browser/ip_protection/ip_protection_core_host_browsertest.cc
+++ chrome/browser/ip_protection/ip_protection_core_host_browsertest.cc
@@ -119,6 +119,9 @@ class IpProtectionCoreHostInterceptor
   void TryGetAuthTokens(uint32_t batch_size,
                         ip_protection::mojom::ProxyLayer proxy_layer,
                         TryGetAuthTokensCallback callback) override {
+    // TODO(crbug.com/376827614): remove this logging once flakiness is fixed.
+    LOG(WARNING) << "IpProtectionCoreHostInterceptor::TryGetAuthTokens(" << this
+                 << ")";
     if (should_intercept_) {
       std::vector<BlindSignedAuthToken> tokens;
       for (uint32_t i = 0; i < batch_size; i++) {
@@ -269,6 +272,9 @@ IN_PROC_BROWSER_TEST_F(IpProtectionCoreHostBrowserTest,
                          std::optional<base::Time>>
       future;
   auto* ipp_control = getter->last_remote_for_testing();
+  // TODO(crbug.com/376827614): remove this logging once flakiness is fixed.
+  LOG(WARNING)
+      << "First ipp_control->VerifyIpProtectionCoreHostForTesting call";
   ipp_control->VerifyIpProtectionCoreHostForTesting(future.GetCallback());
   const std::optional<BlindSignedAuthToken>& result =
       future.Get<std::optional<BlindSignedAuthToken>>();
@@ -292,6 +298,9 @@ IN_PROC_BROWSER_TEST_F(IpProtectionCoreHostBrowserTest,
   future.Clear();
   auto* incognito_ipp_control = getter->last_remote_for_testing();
   ASSERT_NE(incognito_ipp_control, ipp_control);
+  LOG(WARNING)
+      << "Second incognito_ipp_control->VerifyIpProtectionCoreHostForTesting "
+         "call";
   incognito_ipp_control->VerifyIpProtectionCoreHostForTesting(
       future.GetCallback());
   const std::optional<BlindSignedAuthToken>& incognito_result =
@@ -302,6 +311,8 @@ IN_PROC_BROWSER_TEST_F(IpProtectionCoreHostBrowserTest,
 
   // Ensure that we can still get tokens from the main profile.
   future.Clear();
+  LOG(WARNING)
+      << "Third ipp_control->VerifyIpProtectionCoreHostForTesting call";
   ipp_control->VerifyIpProtectionCoreHostForTesting(future.GetCallback());
   const std::optional<BlindSignedAuthToken>& second_attempt_result =
       future.Get<std::optional<BlindSignedAuthToken>>();
