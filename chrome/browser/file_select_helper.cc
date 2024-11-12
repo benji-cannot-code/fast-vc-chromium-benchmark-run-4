cb420520fbf7f0e43d78d6a6f54856fea7733eeb
Joel Hockey <joelhockey@chromium.org>
2024-11-12 00:29:31 +0000

    Show correct display name for android upload dialog
    Content-URIs do not usually have the display-name as the last segment
    of the path like posix and windows. We must query Java code to get
    the display name.
    
    Bug: 377716464
    Change-Id: I59b7d4a2b0927e149765ba48f052d0b844be0198
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6005649
    Commit-Queue: Joel Hockey <joelhockey@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1381499}

--- chrome/browser/file_select_helper.cc
+++ chrome/browser/file_select_helper.cc
@@ -57,10 +57,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #endif
 
-#if !BUILDFLAG(IS_ANDROID)
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/content_uri_utils.h"
+#else
 #include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
 #include "chrome/browser/picture_in_picture/scoped_disallow_picture_in_picture.h"
-#endif  // !BUILDFLAG(IS_ANDROID)
+#endif  // BUILDFLAG(IS_ANDROID)
 
 using blink::mojom::FileChooserFileInfo;
 using blink::mojom::FileChooserFileInfoPtr;
@@ -138,14 +140,24 @@ void InterpretSafeBrowsingVerdict(base::OnceCallback<void(bool)> recipient,
 
 #endif
 
+#if BUILDFLAG(IS_ANDROID)
+std::u16string GetDisplayName(const base::FilePath& content_uri) {
+  std::u16string display_name;
+  if (!base::MaybeGetFileDisplayName(content_uri, &display_name)) {
+    display_name = content_uri.BaseName().AsUTF16Unsafe();
+  }
+  return display_name;
+}
+#endif
+
 }  // namespace
 
 struct FileSelectHelper::ActiveDirectoryEnumeration {
-  explicit ActiveDirectoryEnumeration(const base::FilePath& path)
-      : path_(path) {}
+  explicit ActiveDirectoryEnumeration(const std::u16string& display_name)
+      : display_name_(display_name) {}
 
   std::unique_ptr<net::DirectoryLister> lister_;
-  const base::FilePath path_;
+  const std::u16string display_name_;
   std::vector<blink::mojom::NativeFileInfoPtr> results_;
 };
 
@@ -179,9 +191,9 @@ void FileSelectHelper::FileSelected(const ui::SelectedFileInfo& file,
     return;
   }
 
-  const base::FilePath& path = file.local_path;
   if (dialog_type_ == ui::SelectFileDialog::SELECT_UPLOAD_FOLDER) {
-    StartNewEnumeration(path);
+    StartNewEnumeration(file.local_path,
+                        base::FilePath(file.display_name).AsUTF16Unsafe());
     return;
   }
 
@@ -220,9 +232,10 @@ void FileSelectHelper::FileSelectionCanceled() {
   RunFileChooserEnd();
 }
 
-void FileSelectHelper::StartNewEnumeration(const base::FilePath& path) {
+void FileSelectHelper::StartNewEnumeration(const base::FilePath& path,
+                                           const std::u16string& display_name) {
   base_dir_ = path;
-  auto entry = std::make_unique<ActiveDirectoryEnumeration>(path);
+  auto entry = std::make_unique<ActiveDirectoryEnumeration>(display_name);
   entry->lister_ = base::WrapUnique(new net::DirectoryLister(
       path, net::DirectoryLister::NO_SORT_RECURSIVE, this));
   entry->lister_->Start();
@@ -246,7 +259,7 @@ void FileSelectHelper::OnListFile(
 }
 
 std::unique_ptr<ui::DialogModel> FileSelectHelper::CreateConfirmationDialog(
-    const base::FilePath& path,
+    const std::u16string& display_name,
     std::vector<FileChooserFileInfoPtr> selected_files,
     base::OnceCallback<void(std::vector<blink::mojom::FileChooserFileInfoPtr>)>
         callback) {
@@ -262,7 +275,7 @@ std::unique_ptr<ui::DialogModel> FileSelectHelper::CreateConfirmationDialog(
       .SetTitle(l10n_util::GetPluralStringFUTF16(IDS_CONFIRM_FILE_UPLOAD_TITLE,
                                                  selected_files.size()))
       .AddParagraph(ui::DialogModelLabel(l10n_util::GetStringFUTF16(
-          IDS_CONFIRM_FILE_UPLOAD_TEXT, path.BaseName().LossyDisplayName())))
+          IDS_CONFIRM_FILE_UPLOAD_TEXT, display_name)))
       .AddOkButton(
           base::BindOnce(std::move(ok_callback), std::move(selected_files)),
           ui::DialogModel::Button::Params().SetLabel(
@@ -302,7 +315,7 @@ void FileSelectHelper::OnListDone(int error) {
 
   if (dialog_type_ == ui::SelectFileDialog::SELECT_UPLOAD_FOLDER) {
     auto model = CreateConfirmationDialog(
-        entry->path_, std::move(chooser_files),
+        entry->display_name_, std::move(chooser_files),
         base::BindOnce(&FileSelectHelper::PerformContentAnalysisIfNeeded,
                        this));
     chrome::ShowTabModal(std::move(model), web_contents_);
@@ -803,7 +816,15 @@ void FileSelectHelper::EnumerateDirectoryImpl(
   // to the caller, until the last callback is received from the enumeration
   // code. At that point, we must call EnumerateDirectoryEnd().
   AddRef();
-  StartNewEnumeration(path);
+#if BUILDFLAG(IS_ANDROID)
+  if (path.IsContentUri()) {
+    base::ThreadPool::PostTaskAndReplyWithResult(
+        FROM_HERE, {base::MayBlock()}, base::BindOnce(&GetDisplayName, path),
+        base::BindOnce(&FileSelectHelper::StartNewEnumeration, this, path));
+    return;
+  }
+#endif
+  StartNewEnumeration(path, path.BaseName().AsUTF16Unsafe());
 }
 
 // This method is called when we receive the last callback from the enumeration
