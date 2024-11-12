cc5418e7ad3a563fcc011d0f3ded960e67218940
Filipa Senra <fsenra@google.com>
2024-11-12 12:53:15 +0000

    [iOS][QD] Not run the tabs animation from recent tabs
    We received from crash reports from a CHECK with NotFatalUntil in
    BrowserCoordinator#stopQuickDeleteForAnimationWithCompletion. In
    practise, these check failures mean that we were triggering the
    animation in a flow where the regular tab grid was not accessible from.
    This is because you can open History on Recent Tabs. As such, this CL
    disables the animation for flows that start from the recent tabs.
    
    Before: http://dr/file/d/1fZ4mLohW0zkKGHHXiYo9tIMUhLj4o0UN
    After: http://dr/file/d/1_kO0RWKjuCKmYFf-KLyZne0ajMzTTLn5
    
    Fixed: 377567780
    Change-Id: Ie865a7456c6180bf5996dc54d76320d6c0c00868
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004713
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Filipa Senra <fsenra@google.com>
    Cr-Commit-Position: refs/heads/main@{#1381712}

--- ios/chrome/browser/history/ui_bundled/history_table_view_controller.mm
+++ ios/chrome/browser/history/ui_bundled/history_table_view_controller.mm
@@ -542,7 +542,8 @@ const CGFloat kButtonHorizontalPadding = 30.0;
         self.browser->GetCommandDispatcher(), QuickDeleteCommands);
     [quickDeleteHandler
         showQuickDeleteAndCanPerformTabsClosureAnimation:
-            ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET];
+            ui::GetDeviceFormFactor() == ui::DEVICE_FORM_FACTOR_TABLET &&
+            self.canPerformTabsClosureAnimation];
     return;
   }
 
