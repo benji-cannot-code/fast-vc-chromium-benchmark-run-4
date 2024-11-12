5e9ed204bf239f28b137a5b43a5f3eaef00a1d7a
Mohamed Heikal <mheikal@chromium.org>
2024-11-12 23:12:09 +0000

    Disable flakey SensitiveContentTest case
    Bug: 378742136
    Change-Id: I39ece04244ad69c6bd0af7d7cf2b488cff59036f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6014497
    Commit-Queue: Mohamed Heikal <mheikal@chromium.org>
    Owners-Override: Mohamed Heikal <mheikal@chromium.org>
    Reviewed-by: Henrique Nakashima <hnakashima@chromium.org>
    Auto-Submit: Mohamed Heikal <mheikal@chromium.org>
    Commit-Queue: Henrique Nakashima <hnakashima@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382024}

--- chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sensitive_content/SensitiveContentTest.java
@@ -42,6 +42,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.supplier.ObservableSupplierImpl;
 import org.chromium.base.supplier.OneshotSupplierImpl;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.DoNotBatch;
 import org.chromium.base.test.util.Features.EnableFeatures;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
@@ -379,6 +380,7 @@ public class SensitiveContentTest {
 
     @Test
     @LargeTest
+    @DisabledTest(message = "Flakey test. See https://crbug.com/378742136")
     @EnableFeatures(SensitiveContentFeatures.SENSITIVE_CONTENT_WHILE_SWITCHING_TABS)
     public void testSwipingBetweenTabsIsSensitive() {
         // Set up.
