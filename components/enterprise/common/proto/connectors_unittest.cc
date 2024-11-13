a0359e7476bf561588035b374f7873fe9cb0ff95
Nancy Xiao <nancylanxiao@google.com>
2024-11-13 16:27:11 +0000

    Remove redundant import
    Change-Id: I0db9517f3de8337c42b67d882a2db4672b74a968
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018365
    Reviewed-by: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Auto-Submit: Nancy Xiao <nancylanxiao@google.com>
    Commit-Queue: Dominique Fauteux-Chapleau <domfc@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382371}

--- components/enterprise/common/proto/connectors_unittest.cc
+++ components/enterprise/common/proto/connectors_unittest.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// #include "third_party/content_analysis_sdk/src/proto/content_analysis/sdk/
-
 #include "components/enterprise/common/proto/connectors.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/content_analysis_sdk/src/proto/content_analysis/sdk/analysis.pb.h"
