bfdae6fed62a528d9beafd1f79be96ec648ba988
Maksim Sisov <msisov@igalia.com>
2024-11-13 13:25:41 +0000

    Remove lacros' zaura_toplevel code from Ozone/Wayland 4/*
    This CL removes Lock/Unlock support functionality from Ozone/Wayland,
    which was controlled via zaura_toplevel.
    
    Bug: 374244479, 373971535
    Change-Id: I6456eb4e25b411b57b6b287d36a13b4434e5b222
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011917
    Commit-Queue: Maksim Sisov <msisov@igalia.com>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382285}

--- ui/platform_window/extensions/wayland_extension.h
+++ ui/platform_window/extensions/wayland_extension.h
@@ -25,17 +25,6 @@ enum class WaylandFloatStartLocation {
   kBottomLeft,
 };
 
-enum class WaylandOrientationLockType {
-  kAny,
-  kNatural,
-  kPortrait,
-  kLandscape,
-  kPortraitPrimary,
-  kLandscapePrimary,
-  kPortraitSecondary,
-  kLandscapeSecondary,
-};
-
 class COMPONENT_EXPORT(PLATFORM_WINDOW) WaylandExtension {
  public:
   // Waits for a Wayland roundtrip to ensure all side effects have been
@@ -103,10 +92,6 @@ class COMPONENT_EXPORT(PLATFORM_WINDOW) WaylandToplevelExtension {
   // Whether or not the underlying platform supports native pointer locking.
   virtual bool SupportsPointerLock() = 0;
   virtual void LockPointer(bool enabled) = 0;
-
-  // Lock and unlock the window rotation.
-  virtual void Lock(WaylandOrientationLockType lock_Type) = 0;
-  virtual void Unlock() = 0;
 
   // Retrieve current layout state.
   virtual bool GetTabletMode() = 0;
