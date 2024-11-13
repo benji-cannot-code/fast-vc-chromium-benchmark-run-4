e28d9f34facc7f233d4710f8830156d11a90661b
Khalid Peer <khalidpeer@chromium.org>
2024-11-13 19:37:54 +0000

    Revert "[Extensions] Remove ExtensionHostDelegate::GetJavaScriptDialogManager()"
    This reverts commit a4d86fc3495e8a08218996c31c9038ec9777a1f3.
    
    Reason for revert: The original CL resulted in a tree-blocking failure (https://cr-buildbucket.appspot.com/build/8731358160175787633).
    
    Original change's description:
    > [Extensions] Remove ExtensionHostDelegate::GetJavaScriptDialogManager()
    >
    > ExtensionHostDelegate::GetJavaScriptDialogManager() is used by
    > ExtensionHost (in //extensions) to retrieve the
    > JavaScriptDialogManager instance to use for the web contents owned
    > by the ExtensionHost. In the //chrome implementation (the only real
    > implementation), this forwards to
    > javascript_dialogs::AppModalDialogManager::GetInstance().
    >
    > This can be done directly at the //extensions layer, since
    > //extensions can depend directly upon //components/javascript_dialogs.
    >
    > Remove ExtensionHostDelegate::GetJavaScriptDialogManager() and inline
    > the usage directly in ExtensionHost.
    >
    > Bug: None
    > Change-Id: I378084110ae1c219749186bdd5572adc2a0e358e
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6013243
    > Commit-Queue: Devlin Cronin <rdevlin.cronin@chromium.org>
    > Reviewed-by: Avi Drissman <avi@chromium.org>
    > Reviewed-by: Tim <tjudkins@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1382491}
    
    Bug: None
    Change-Id: I5a09a6ae4ded82e83456375dfa8c66e5467db173
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019307
    Auto-Submit: Khalid Peer <khalidpeer@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Owners-Override: Khalid Peer <khalidpeer@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382516}

--- chrome/browser/extensions/chrome_extension_host_delegate.cc
+++ chrome/browser/extensions/chrome_extension_host_delegate.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc/media_capture_devices_dispatcher.h"
 #include "chrome/browser/picture_in_picture/picture_in_picture_window_manager.h"
 #include "chrome/browser/ui/prefs/prefs_tab_helper.h"
+#include "components/javascript_dialogs/app_modal_dialog_manager.h"
 #include "extensions/browser/extension_host.h"
 #include "extensions/browser/extension_system.h"
 #include "extensions/browser/extensions_browser_client.h"
@@ -37,6 +38,11 @@ void ChromeExtensionHostDelegate::OnMainFrameCreatedForBackgroundPage(
       ExtensionSystem::Get(host->browser_context())->extension_service();
   if (service)
     service->DidCreateMainFrameForBackgroundPage(host);
+}
+
+content::JavaScriptDialogManager*
+ChromeExtensionHostDelegate::GetJavaScriptDialogManager() {
+  return javascript_dialogs::AppModalDialogManager::GetInstance();
 }
 
 void ChromeExtensionHostDelegate::CreateTab(
