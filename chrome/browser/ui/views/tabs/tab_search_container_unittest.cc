2fe486df3d262369be5cc44820681dce5cb5b2d0
Emily Shack <emshack@chromium.org>
2024-11-13 23:12:27 +0000

    Add skeleton GLIC button
    Also creates features::IsGlicEnabled() method which factors in tab strip
    combo button feature flag.
    
    There are likely some edge case tab strip layout bugs still present in
    this CL - the goal here is for this button to be a starting point, that
    other teams can iterate on and fix any layout bugs moving forward.
    
    TabStripRegionView have DanglingUntriaged, these should likely be
    addressed wholesale.
    
    DanglingUntriaged-notes: All other buttons treated in the same way by
    Low-Coverage-Reason: TRIVIAL_CHANGE. tab_strip_region_view has low coverage, but only adds a placeholder button behind a flag. Tests should be added as the button gains functionality.
    Bug: 377584745
    Change-Id: If4671d3cd837373ccd064ede87d08278c8993ebc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6014687
    Commit-Queue: Emily Shack <emshack@chromium.org>
    Reviewed-by: Eshwar Stalin <estalin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382650}

--- chrome/browser/ui/views/tabs/tab_search_container_unittest.cc
+++ chrome/browser/ui/views/tabs/tab_search_container_unittest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/views/frame/tab_strip_region_view.h"
 #include "chrome/browser/ui/views/tabs/tab_organization_button.h"
 #include "chrome/browser/ui/views/tabs/tab_search_button.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/views/chrome_views_test_base.h"
 #include "components/optimization_guide/core/model_execution/model_execution_features.h"
--- chrome/browser/ui/views/tabs/tab_strip_control_button.cc
+++ chrome/browser/ui/views/tabs/tab_strip_control_button.cc
@@ -7,11 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <utility>
 
-#include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/ui/views/frame/browser_non_client_frame_view.h"
 #include "chrome/browser/ui/views/tabs/tab_strip.h"
 #include "chrome/browser/ui/views/tabs/tab_strip_controller.h"
 #include "chrome/browser/ui/views/toolbar/toolbar_ink_drop_util.h"
+#include "chrome/common/chrome_features.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/base/metadata/metadata_impl_macros.h"
 #include "ui/gfx/geometry/rounded_corners_f.h"
