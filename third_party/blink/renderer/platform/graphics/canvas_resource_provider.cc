dcda823b131f1eadcdce5c0f086574f91d6b1d17
Vasiliy Telezhnikov <vasilyt@chromium.org>
2024-11-13 16:41:34 +0000

    Make CanvasResourceProvider::IsOriginTopLeft virtual
    Only CanvasResourceProviderPassthrough can have bottom left origin.
    This CL makes it virtual and override in Passthrough, so it's easier
    to eliminate call-sites and the function all together.
    
    Bug: 378688985
    Change-Id: I5dcfcd7d1cab3e918562547dffa9e17c1f2e1cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6000557
    Commit-Queue: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Jean-Philippe Gravel <jpgravel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382382}

--- third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_provider.cc
@@ -153,7 +153,6 @@ class CanvasResourceProviderBitmap : public CanvasResourceProvider {
       : CanvasResourceProvider(kBitmap,
                                info,
                                filter_quality,
-                               /*is_origin_top_left=*/true,
                                /*context_provider_wrapper=*/nullptr,
                                std::move(resource_dispatcher),
                                resource_host) {}
@@ -286,7 +285,6 @@ class CanvasResourceProviderSharedImage : public CanvasResourceProvider {
       : CanvasResourceProvider(kSharedImage,
                                info,
                                filter_quality,
-                               /*is_origin_top_left=*/true,
                                std::move(context_provider_wrapper),
                                /*resource_dispatcher=*/nullptr,
                                resource_host),
@@ -649,9 +647,10 @@ class CanvasResourceProviderSharedImage : public CanvasResourceProvider {
     const auto props = GetSkSurfaceProps();
     if (is_accelerated_) {
       return SkSurfaces::WrapBackendTexture(
-          GetGrContext(), CreateGrTextureForResource(), GetGrSurfaceOrigin(),
-          0 /* msaa_sample_count */, GetSkImageInfo().colorType(),
-          GetSkImageInfo().refColorSpace(), &props);
+          GetGrContext(), CreateGrTextureForResource(),
+          kTopLeft_GrSurfaceOrigin, 0 /* msaa_sample_count */,
+          GetSkImageInfo().colorType(), GetSkImageInfo().refColorSpace(),
+          &props);
     }
 
     // For software raster path, we render into cpu memory managed internally
@@ -823,16 +822,17 @@ class CanvasResourceProviderPassThrough final : public CanvasResourceProvider {
       : CanvasResourceProvider(kPassThrough,
                                info,
                                filter_quality,
-                               is_origin_top_left,
                                std::move(context_provider_wrapper),
                                std::move(resource_dispatcher),
-                               resource_host) {}
+                               resource_host),
+        is_origin_top_left_(is_origin_top_left) {}
 
   ~CanvasResourceProviderPassThrough() override = default;
   bool IsValid() const final { return true; }
   bool IsAccelerated() const final { return true; }
   bool SupportsDirectCompositing() const override { return true; }
   bool SupportsSingleBuffering() const override { return true; }
+  bool IsOriginTopLeft() const override { return is_origin_top_left_; }
 
  private:
   scoped_refptr<CanvasResource> CreateResource() final {
@@ -854,6 +854,8 @@ class CanvasResourceProviderPassThrough final : public CanvasResourceProvider {
       return nullptr;
     return resource->Bitmap();
   }
+
+  const bool is_origin_top_left_;
 };
 
 // * Renders to back buffer of a shared image swap chain.
@@ -872,7 +874,6 @@ class CanvasResourceProviderSwapChain final : public CanvasResourceProvider {
       : CanvasResourceProvider(kSwapChain,
                                info,
                                filter_quality,
-                               /*is_origin_top_left=*/true,
                                std::move(context_provider_wrapper),
                                std::move(resource_dispatcher),
                                resource_host),
@@ -1415,7 +1416,6 @@ CanvasResourceProvider::CanvasResourceProvider(
     const ResourceProviderType& type,
     const SkImageInfo& info,
     cc::PaintFlags::FilterQuality filter_quality,
-    bool is_origin_top_left,
     base::WeakPtr<WebGraphicsContext3DProviderWrapper> context_provider_wrapper,
     base::WeakPtr<CanvasResourceDispatcher> resource_dispatcher,
     CanvasResourceHost* resource_host)
@@ -1424,7 +1424,6 @@ CanvasResourceProvider::CanvasResourceProvider(
       resource_dispatcher_(resource_dispatcher),
       info_(info),
       filter_quality_(filter_quality),
-      is_origin_top_left_(is_origin_top_left),
       resource_host_(resource_host),
       recorder_(std::make_unique<MemoryManagedPaintRecorder>(Size(), this)),
       snapshot_paint_image_id_(cc::PaintImage::GetNextId()) {
