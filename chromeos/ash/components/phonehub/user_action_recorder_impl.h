cf77071844b08118595a1acdcbc3208be57f193b
Crisrael Lucero <crisrael@google.com>
2024-11-13 18:33:24 +0000

    [Phone Hub] Include IFTTT lint blocks on histogram enums
    Allow files to stay in sync using IFTTT linter blocks for enums used in
    histograms. This change only includes new lint blocks and does not
    change any implementation.
    
    Context: https://www.chromium.org/chromium-os/developer-library/guides/development/keep-files-in-sync/
    
    NO_IFTTT=Adding IFTTT to already existing blocks ironically trigger it
    Test: N/a, no-op only adding lint blocks in comments
    Bug: b:378518282
    
    Change-Id: I0422b27e8edabd746af816ce94cfba49cd02ea28
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011606
    Reviewed-by: Pu Shi <pushi@google.com>
    Commit-Queue: Crisrael Lucero <crisrael@google.com>
    Cr-Commit-Position: refs/heads/main@{#1382463}

--- chromeos/ash/components/phonehub/user_action_recorder_impl.h
+++ chromeos/ash/components/phonehub/user_action_recorder_impl.h
@@ -27,7 +27,10 @@ class UserActionRecorderImpl : public UserActionRecorder {
   FRIEND_TEST_ALL_PREFIXES(UserActionRecorderImplTest, RecordActions);
 
   // Types of user actions; numerical value should not be reused or reordered
-  // since this enum is used in metrics.
+  // since this enum is used in metrics. Keep in sync with PhoneHubUserAction
+  // enum in //tools/metrics/histograms/metadata/phonehub/enums.xml.
+  //
+  // LINT.IfChange(PhoneHubUserAction)
   enum class UserAction {
     kUiOpened = 0,
     kTether = 1,
@@ -40,6 +43,7 @@ class UserActionRecorderImpl : public UserActionRecorder {
     kAppStreamLauncherOpened = 8,
     kMaxValue = kAppStreamLauncherOpened,
   };
+  // LINT.ThenChange(//tools/metrics/histograms/metadata/phonehub/enums.xml:PhoneHubUserAction)
 
   // UserActionRecorder:
   void RecordUiOpened() override;
