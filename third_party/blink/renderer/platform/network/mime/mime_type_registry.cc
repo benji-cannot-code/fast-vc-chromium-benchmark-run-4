b095bd253faa7e5e9e55a4a4cf710644ccc69193
Fredrik Söderquist <fs@opera.com>
2024-11-13 22:19:28 +0000

    spanification: //third_party/blink/renderer/platform/network/mime/mime_type_registry.cc
    Bug: 351564777
    Change-Id: Idc0afc557bc63bfe8b205dac2416cb859f9a41e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018564
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/main@{#1382614}

--- third_party/blink/renderer/platform/network/mime/mime_type_registry.cc
+++ third_party/blink/renderer/platform/network/mime/mime_type_registry.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/351564777): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "third_party/blink/renderer/platform/network/mime/mime_type_registry.h"
 
 #include "base/files/file_path.h"
@@ -22,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/blink/public/platform/file_path_conversion.h"
 #include "third_party/blink/public/platform/platform.h"
 #include "third_party/blink/renderer/platform/wtf/assertions.h"
+#include "third_party/blink/renderer/platform/wtf/text/character_visitor.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
 namespace blink {
@@ -43,12 +39,13 @@ std::string ToASCIIOrEmpty(const WebString& string) {
   return string.ContainsOnlyASCII() ? string.Ascii() : std::string();
 }
 
-template <typename CHARTYPE, typename SIZETYPE>
-std::string ToLowerASCIIInternal(CHARTYPE* str, SIZETYPE length) {
+template <typename CharType>
+std::string ToLowerASCIIInternal(base::span<const CharType> chars) {
   std::string lower_ascii;
-  lower_ascii.reserve(length);
-  for (CHARTYPE* p = str; p < str + length; p++)
-    lower_ascii.push_back(base::ToLowerASCII(static_cast<char>(*p)));
+  lower_ascii.reserve(chars.size());
+  for (size_t i = 0; i < chars.size(); i++) {
+    lower_ascii.push_back(base::ToLowerASCII(static_cast<char>(chars[i])));
+  }
   return lower_ascii;
 }
 
@@ -56,9 +53,8 @@ std::string ToLowerASCIIInternal(CHARTYPE* str, SIZETYPE length) {
 std::string ToLowerASCIIOrEmpty(const String& str) {
   if (str.empty() || !str.ContainsOnlyASCIIOrEmpty())
     return std::string();
-  if (str.Is8Bit())
-    return ToLowerASCIIInternal(str.Characters8(), str.length());
-  return ToLowerASCIIInternal(str.Characters16(), str.length());
+  return WTF::VisitCharacters(
+      str, [](auto chars) { return ToLowerASCIIInternal(chars); });
 }
 
 STATIC_ASSERT_ENUM(MIMETypeRegistry::kNotSupported,
