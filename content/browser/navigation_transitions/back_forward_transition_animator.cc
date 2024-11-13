f81ca1ce28e5e2f286c9a3cc834553f0f9d88b84
William Liu <liuwilliam@chromium.org>
2024-11-13 21:21:10 +0000

    [DNT] Fix same-doc nav restarts as cross-doc
    By adding a `was_reset_for_cross_document_restart()` on the request, we
    abort the transition whenever this happens.
    
    R=khushalsagar@chromium.org
    
    Bug: 378504116
    Change-Id: I36a41c56cf514adb8f7ed11a838f64ec13b6d3d4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011318
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Khushal Sagar <khushalsagar@chromium.org>
    Commit-Queue: William Liu <liuwilliam@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382583}

--- content/browser/navigation_transitions/back_forward_transition_animator.cc
+++ content/browser/navigation_transitions/back_forward_transition_animator.cc
@@ -159,6 +159,8 @@ const char* AnimationAbortReasonToString(AnimationAbortReason abort_reason) {
       return "kAnimationFinished";
     case AnimationAbortReason::kPrimaryMainFrameRenderProcessDestroyed:
       return "kPrimaryMainFrameRenderProcessDestroyed";
+    case AnimationAbortReason::kSameDocNavRestarts:
+      return "kSameDocNavRestarts";
   }
   NOTREACHED();
 }
@@ -841,6 +843,12 @@ void BackForwardTransitionAnimator::DidFinishNavigation(
   // ignored completely. We should decide what to do before launch.
   if (!tracked_request_ ||
       tracked_request_->navigation_id != navigation_handle->GetNavigationId()) {
+    return;
+  }
+
+  if (static_cast<NavigationRequest*>(navigation_handle)
+          ->was_reset_for_cross_document_restart()) {
+    AbortAnimation(AnimationAbortReason::kSameDocNavRestarts);
     return;
   }
 
