127d93de15c8f963e444a4e19168f58c3a7d2eee
Peter Kasting <pkasting@chromium.org>
2024-11-13 22:54:55 +0000

    Pass size_t to span functions: base/
    In preparation for upgrading various span args from size_t to
    StrictNumeric<size_t>, this fixes type mismatches that would cause
    compile errors with that change.
    
    Bug: none
    Change-Id: I38821090e22e717702ecaa402c483de2a09511c6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020576
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1382637}

--- base/metrics/histogram_threadsafe_unittest.cc
+++ base/metrics/histogram_threadsafe_unittest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/persistent_histogram_allocator.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/no_destructor.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/scoped_feature_list.h"
 #include "base/threading/simple_thread.h"
@@ -117,7 +118,8 @@ class SnapshotDeltaThread : public SimpleThread {
         break;
       }
       case SPARSE_HISTOGRAM:
-        subtle::NoBarrier_AtomicIncrement(&real_bucket_counts_[sample], 1);
+        subtle::NoBarrier_AtomicIncrement(
+            &real_bucket_counts_[checked_cast<size_t>(sample)], 1);
         break;
       case LINEAR_HISTOGRAM:
       case BOOLEAN_HISTOGRAM:
@@ -142,7 +144,8 @@ class SnapshotDeltaThread : public SimpleThread {
       // This is to ensure SnapshotDelta() is fully thread-safe, not just
       // "eventually consistent".
       ASSERT_GE(count, 0);
-      subtle::NoBarrier_AtomicIncrement(&snapshots_bucket_counts_[min], count);
+      subtle::NoBarrier_AtomicIncrement(
+          &snapshots_bucket_counts_[checked_cast<size_t>(min)], count);
     }
   }
 
