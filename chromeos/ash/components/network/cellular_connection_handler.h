11f5ec93cad548d2bdb3bdf261aa80edef560640
Jason Zhang <jiajunz@google.com>
2024-11-13 01:02:37 +0000

    [CrOS Cellular] Do not connect to cellular network if SIM is locked
    Currently, when switching to a cellular profile on a locked SIM, we
    don't check the SIM lock status and directly initiate a connect request.
    And the connection would fail after 2 min because it tries to wait for
    the auto-connection first.
    This CL saves user 2 mins by checking the lock status before initiate a
    connection request.
    
    Bug: 308853903
    Change-Id: Ia591316ad9b7864b46de67450fa29589c35ab2a1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5943709
    Commit-Queue: Jason Zhang <jiajunz@google.com>
    Reviewed-by: Chad Duffin <chadduffin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382087}

--- chromeos/ash/components/network/cellular_connection_handler.h
+++ chromeos/ash/components/network/cellular_connection_handler.h
@@ -71,7 +71,8 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularConnectionHandler
     kCouldNotFindRelevantESimProfile = 5,
     kEnableProfileFailed = 6,
     kTimeoutWaitingForConnectable = 7,
-    kMaxValue = kTimeoutWaitingForConnectable
+    kSimLocked = 8,
+    kMaxValue = kSimLocked
   };
 
   CellularConnectionHandler();
@@ -170,6 +171,7 @@ class COMPONENT_EXPORT(CHROMEOS_NETWORK) CellularConnectionHandler
                                      const std::string& old_guid,
                                      const std::string& new_guid) override;
   void NetworkConnectionStateChanged(const NetworkState* network) override;
+  void DevicePropertiesUpdated(const DeviceState* device) override;
 
   void ProcessRequestQueue();
   void TransitionToConnectionState(ConnectionState state);
