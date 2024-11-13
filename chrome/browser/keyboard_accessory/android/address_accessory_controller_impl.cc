c60cb7c11dd7a078caae1f741f18baa49506b082
Timofey Chudakov <tchudakov@google.com>
2024-11-13 16:40:25 +0000

    [Android] HaTS survey for manual fallback plus address creation.
    This CL adds logic to trigger a HaTS survey after the user opens the
    plus address creation bottom sheet from the keyboard accessory manual
    filling sheet and successfully creates a plus address.
    
    Bug: 348139343
    Change-Id: I9ec5b487aec27d64c15322de2b19940a6e5a16b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011706
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1382380}

--- chrome/browser/keyboard_accessory/android/address_accessory_controller_impl.cc
+++ chrome/browser/keyboard_accessory/android/address_accessory_controller_impl.cc
@@ -174,7 +174,7 @@ void AddressAccessoryControllerImpl::OnOptionSelected(
               ContentAutofillClient::FromWebContents(&GetWebContents())) {
         client->OfferPlusAddressCreation(
             client->GetLastCommittedPrimaryMainFrameOrigin(),
-            /*is_manual_fallback=*/false,
+            /*is_manual_fallback=*/true,
             base::BindOnce(
                 &AddressAccessoryControllerImpl::OnPlusAddressCreated,
                 weak_ptr_factory_.GetWeakPtr(),
