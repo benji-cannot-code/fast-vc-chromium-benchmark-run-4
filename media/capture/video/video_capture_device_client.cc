b824c76d773f7b954d4aa16f9eede30ca649ad13
Piotr Bialecki <bialpio@chromium.org>
2024-11-13 19:40:13 +0000

    Remove overzealous DFAKE_SCOPED_RECURSIVE_LOCK in VCDC
    CL https://crrev.com/c/5599239 introduced more debug checks to guard
    against multiple threads calling into VideoCaptureDeviceClient. Turns
    out that VCDC is supposed to be sometimes-thread-safe, and therefore
    the added `DFAKE_SCOPED_RECURSIVE_LOCK`s are tripping even in
    legitimate scenarios.
    
    This CL removes those overzealous `DFAKE_SCOPED_RECURSIVE_LOCK`s from
    methods that are explicitly called out as thread-safe by declaration
    of the `VideoCaptureDevice::Client` interface.
    
    Change-Id: Ib1b587cd717fb9108478cba5ea3f8ec54bf0aac4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6017070
    Commit-Queue: Piotr Bialecki <bialpio@chromium.org>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382520}

--- media/capture/video/video_capture_device_client.cc
+++ media/capture/video/video_capture_device_client.cc
@@ -1004,8 +1004,6 @@ void VideoCaptureDeviceClient::OnIncomingCapturedBufferExt(
 void VideoCaptureDeviceClient::OnError(VideoCaptureError error,
                                        const base::Location& from_here,
                                        const std::string& reason) {
-  DFAKE_SCOPED_RECURSIVE_LOCK(call_from_producer_);
-
   const std::string log_message = base::StringPrintf(
       "error@ %s, %s, OS message: %s", from_here.ToString().c_str(),
       reason.c_str(),
@@ -1024,8 +1022,6 @@ void VideoCaptureDeviceClient::OnFrameDropped(
 }
 
 void VideoCaptureDeviceClient::OnLog(const std::string& message) {
-  DFAKE_SCOPED_RECURSIVE_LOCK(call_from_producer_);
-
   receiver_->OnLog(message);
 }
 
@@ -1036,8 +1032,6 @@ double VideoCaptureDeviceClient::GetBufferPoolUtilization() const {
 }
 
 void VideoCaptureDeviceClient::OnStarted() {
-  DFAKE_SCOPED_RECURSIVE_LOCK(call_from_producer_);
-
   receiver_->OnStarted();
 }
 
