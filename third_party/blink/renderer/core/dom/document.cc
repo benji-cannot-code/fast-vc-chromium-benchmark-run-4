39a6dac7492c43daee874a845b2a7a2a8b1ac845
Lingqi Chi <lingqi@chromium.org>
2024-11-13 03:13:11 +0000

    Prerender: Only dry run paint phase based on prerender optimization config
    This CL adds a flag that will be used to control the behavior of the
    new prerendering optimization that prepares the paint tree during
    prerendering.
    And document will decide whether to trigger prerendering based on this flag.
    
    Follow-ups will:
    1. add more metrics on the renderer side to verify the correctness.
    2. add more checks for painting related metrics.
    3. report the distribution of paint result reusability
    
    Bug: 40240492
    Change-Id: I448af0706db29ead32cc10566717afe1d3df2c7a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5954407
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Lingqi Chi <lingqi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382121}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -7369,7 +7369,8 @@ void Document::FinishedParsing() {
         UpdateStyleAndLayoutTree();
         if (base::FeatureList::IsEnabled(
                 features::kPrerender2EarlyDocumentLifecycleUpdate) &&
-            IsPrerendering() && GetFrame()->IsLocalRoot()) {
+            IsPrerendering() && GetFrame()->IsLocalRoot() &&
+            GetPage()->ShouldPreparePaintTreeOnPrerender()) {
           View()->DryRunPaintingForPrerender();
         }
       }
