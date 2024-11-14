8b7cb5689d6ca467597dab1b90fba4dbf8e3d7b9
Saifuddin Hitawala <hitawala@chromium.org>
2024-11-14 00:17:49 +0000

    [gpu] Add GL performance log suppression
    Suppress GL_DEBUG_TYPE_PERFORMANCE log messages for web tests that can
    get sent to the JS console and cause unnecessary test failures due to
    test output log expectation comparisons.
    
    Tested by removing suppressions for tests due to such failures in PS5
    and adding mac-skia-alt-arm64-blink-rel bot.
    
    Bug: 374090995
    Change-Id: Id211e17e0f92f29af2faaadb9f806323162d9437
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018367
    Commit-Queue: Saifuddin Hitawala <hitawala@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382695}

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -291,6 +291,7 @@ static const char* const kSwitchNames[] = {
     switches::kUseGpuInTests,
     switches::kWatchDirForScrollJankReport,
     switches::kWebViewDrawFunctorUsesVulkan,
+    switches::kSuppressPerformanceLogs,
 #if BUILDFLAG(IS_MAC)
     sandbox::policy::switches::kEnableSandboxLogging,
     sandbox::policy::switches::kDisableMetalShaderCache,
