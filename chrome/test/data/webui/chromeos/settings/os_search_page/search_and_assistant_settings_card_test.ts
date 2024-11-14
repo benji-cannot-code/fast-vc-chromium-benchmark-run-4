84dc6eb01e9b8abe972fc49f57b4b2bc04444cba
Michael Cui <mlcui@google.com>
2024-11-14 04:35:42 +0000

    Revert "sunfish: Use internal settings strings and icon"
    This reverts commit 16f8be63753efb4e8a817f017f3dabe4a8610a8d.
    
    Reason for revert: b/378780331
    
    Original change's description:
    > sunfish: Use internal settings strings and icon
    >
    > See https://crrev.com/i/7811368 for the internal CL which adds the
    > strings and icon.
    >
    > Bug: b:368184169
    > Change-Id: I6a6a21003d5ffddb9f8d4439a7f1b96a0af58c26
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6007263
    > Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    > Auto-Submit: Michael Cui <mlcui@google.com>
    > Commit-Queue: Wes Okuhara <wesokuhara@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1381240}
    
    Bug: b:368184169, b:378780331
    Change-Id: Ie6dc34fdef7c89d9936b1bf567cb5b86e4752f35
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6013325
    Commit-Queue: Michael Cui <mlcui@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Nikki Moteva <moteva@google.com>
    Reviewed-by: Wes Okuhara <wesokuhara@google.com>
    Cr-Commit-Position: refs/heads/main@{#1382803}

--- chrome/test/data/webui/chromeos/settings/os_search_page/search_and_assistant_settings_card_test.ts
+++ chrome/test/data/webui/chromeos/settings/os_search_page/search_and_assistant_settings_card_test.ts
@@ -261,7 +261,6 @@ suite('<search-and-assistant-settings-card>', () => {
                       })})});
   });
 
-  // <if expr="_google_chrome">
   test(
       'when isSunfishSettingsToggleVisible flag is false, ' +
           'Sunfish toggle is hidden',
@@ -333,7 +332,6 @@ suite('<search-and-assistant-settings-card>', () => {
               `Element should be focused for settingId=${setting}.'`);
         });
       });
-  // </if>
 
   suite('when Quick Answers is not supported', () => {
     test('Search engine row should be visible', () => {
