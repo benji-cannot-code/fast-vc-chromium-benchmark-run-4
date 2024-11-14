400be32d7472b65c404da510201cd283d15d1d73
Austin Sullivan <asully@chromium.org>
2024-11-14 03:20:09 +0000

    webnn: Remove unused functions in graph_validation_utils.h
    This is dead code which should have been removed alongside its last
    callers, respectively
    
    Change-Id: I1d0dd5027a766c38bf723b2f6d684f7fda7dde29
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011570
    Reviewed-by: Phillis Tang <phillis@chromium.org>
    Auto-Submit: Austin Sullivan <asully@chromium.org>
    Commit-Queue: Phillis Tang <phillis@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382779}

--- services/webnn/public/cpp/graph_validation_utils.h
+++ services/webnn/public/cpp/graph_validation_utils.h
@@ -19,9 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace webnn {
 
-std::string COMPONENT_EXPORT(WEBNN_PUBLIC_CPP)
-    DataTypeConstraintToString(const SupportedDataTypes& constraint_set);
-
 // Represents the `MLConv2dFilterOperandLayout` that specifies the layout format
 // of the filter tensor. O is output channels, I is input channels / groups, H
 // is height and W is the width of filter.
@@ -762,9 +759,6 @@ base::expected<uint32_t, std::string> COMPONENT_EXPORT(WEBNN_PUBLIC_CPP)
                                        const uint32_t stride,
                                        const uint32_t dilation,
                                        const uint32_t output_padding);
-
-bool COMPONENT_EXPORT(WEBNN_PUBLIC_CPP)
-    IsFloatingPointType(OperandDataType data_type);
 
 // A depthwise conv2d operation is a variant of grouped convolution where the
 // options.groups == input_channels == output_channels according to WebNN conv2d
