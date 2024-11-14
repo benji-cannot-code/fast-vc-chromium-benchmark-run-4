cdef6edd335499821e3caa1a1608015a84316132
Sammie Quon <sammiequon@chromium.org>
2024-11-14 02:45:55 +0000

    coral: Add max saved group toast when limit is hit
    At the moment, the rest of feature is not implemented, so this will not
    show up in production. It does get the ball rolling on some piping
    work and creating a test fixture though.
    
    Test: ash_unittests CoralControllerTest.MaxCoralSavedGroupLimit
    Change-Id: I4a2f28b7070de1b38bc76128fa6595abdd6b9929
    Fixed: 366264124
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012939
    Reviewed-by: Michele Fan <michelefan@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Reviewed-by: Daniel Andersson <dandersson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382768}

--- components/desks_storage/core/desk_model_wrapper.cc
+++ components/desks_storage/core/desk_model_wrapper.cc
@@ -128,6 +128,10 @@ size_t DeskModelWrapper::GetDeskTemplateEntryCount() const {
          policy_entries_.size();
 }
 
+size_t DeskModelWrapper::GetCoralEntryCount() const {
+  return 0u;
+}
+
 size_t DeskModelWrapper::GetMaxSaveAndRecallDeskEntryCount() const {
   return save_and_recall_desks_model_->GetMaxSaveAndRecallDeskEntryCount();
 }
@@ -135,6 +139,10 @@ size_t DeskModelWrapper::GetMaxSaveAndRecallDeskEntryCount() const {
 size_t DeskModelWrapper::GetMaxDeskTemplateEntryCount() const {
   return GetDeskTemplateModel()->GetMaxDeskTemplateEntryCount() +
          policy_entries_.size();
+}
+
+size_t DeskModelWrapper::GetMaxCoralEntryCount() const {
+  return 0u;
 }
 
 std::set<base::Uuid> DeskModelWrapper::GetAllEntryUuids() const {
--- components/desks_storage/core/desk_sync_bridge.cc
+++ components/desks_storage/core/desk_sync_bridge.cc
@@ -476,6 +476,10 @@ size_t DeskSyncBridge::GetDeskTemplateEntryCount() const {
   return template_count + policy_entries_.size();
 }
 
+size_t DeskSyncBridge::GetCoralEntryCount() const {
+  return 0u;
+}
+
 // Chrome sync does not support save and recall desks yet. Return 0 for max
 // count.
 size_t DeskSyncBridge::GetMaxSaveAndRecallDeskEntryCount() const {
@@ -484,6 +488,10 @@ size_t DeskSyncBridge::GetMaxSaveAndRecallDeskEntryCount() const {
 
 size_t DeskSyncBridge::GetMaxDeskTemplateEntryCount() const {
   return kMaxTemplateCount + policy_entries_.size();
+}
+
+size_t DeskSyncBridge::GetMaxCoralEntryCount() const {
+  return 0u;
 }
 
 std::set<base::Uuid> DeskSyncBridge::GetAllEntryUuids() const {
