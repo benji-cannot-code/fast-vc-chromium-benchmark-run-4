8d04b1005e7d4e678e5134cd5db112e05ec8f6ed
Vishwas Uppoor <vishwasuppoor@google.com>
2024-11-14 23:14:57 +0000

    [PIX] Log user actions on the FOP selector
    Log users actions like FOP selected, links clicked, etc on the FOP
    selector.
    
    go/pix-in-chrome-metrics-revised
    
    Bug: 375089558, 375088986
    Change-Id: I74137db4f98aa9ebc13bb4df0975a80260d370b6
    Fixed: 378555947
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020219
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383308}

--- chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsMediator.java
+++ chrome/browser/facilitated_payments/ui/android/internal/java/src/org/chromium/chrome/browser/facilitated_payments/FacilitatedPaymentsPaymentMethodsMediator.java
@@ -42,6 +42,7 @@ import android.graphics.Bitmap;
 
 import androidx.annotation.VisibleForTesting;
 
+import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.autofill.AutofillUiUtils;
 import org.chromium.chrome.browser.autofill.PersonalDataManagerFactory;
 import org.chromium.chrome.browser.facilitated_payments.FacilitatedPaymentsPaymentMethodsComponent.Delegate;
@@ -56,6 +57,7 @@ import org.chromium.components.autofill.payments.AccountType;
 import org.chromium.components.autofill.payments.BankAccount;
 import org.chromium.components.autofill.payments.Ewallet;
 import org.chromium.components.browser_ui.bottomsheet.BottomSheetController.StateChangeReason;
+import org.chromium.components.facilitated_payments.core.ui_utils.FopSelectorAction;
 import org.chromium.components.facilitated_payments.core.ui_utils.UiEvent;
 import org.chromium.components.payments.InputProtector;
 import org.chromium.ui.modelutil.MVCListAdapter.ListItem;
@@ -71,6 +73,12 @@ import java.util.Optional;
  */
 class FacilitatedPaymentsPaymentMethodsMediator {
     static final String PIX_BANK_ACCOUNT_TRANSACTION_LIMIT = "500";
+
+    // This histogram name should be in sync with the one in
+    // components/facilitated_payments/core/metrics/facilitated_payments_metrics.cc:LogFopSelected.
+    @VisibleForTesting
+    static final String FOP_SELECTOR_USER_ACTION_HISTOGRAM =
+            "FacilitatedPayments.Pix.FopSelector.UserAction";
 
     private Context mContext;
     private PropertyModel mModel;
@@ -195,7 +203,7 @@ class FacilitatedPaymentsPaymentMethodsMediator {
                 new PropertyModel.Builder(FooterProperties.ALL_KEYS)
                         .with(
                                 FooterProperties.SHOW_PAYMENT_METHOD_SETTINGS_CALLBACK,
-                                () -> mDelegate.showManagePaymentMethodsSettings(mContext))
+                                () -> this.onManagePaymentMethodsOptionSelected())
                         .build());
     }
 
@@ -209,7 +217,7 @@ class FacilitatedPaymentsPaymentMethodsMediator {
                                 R.string.pix_payment_additional_info)
                         .with(
                                 SHOW_PAYMENT_METHOD_SETTINGS_CALLBACK,
-                                () -> mDelegate.showFinancialAccountsManagementSettings(mContext))
+                                () -> this.onTurnOffPaymentPromptLinkClicked())
                         .build());
     }
 
@@ -276,6 +284,24 @@ class FacilitatedPaymentsPaymentMethodsMediator {
     public void onEwalletSelected(Ewallet eWallet) {
         if (!mInputProtector.shouldInputBeProcessed()) return;
         mDelegate.onEwalletSelected(eWallet.getInstrumentId());
+    }
+
+    private void onManagePaymentMethodsOptionSelected() {
+        mDelegate.showManagePaymentMethodsSettings(mContext);
+
+        RecordHistogram.recordEnumeratedHistogram(
+                FOP_SELECTOR_USER_ACTION_HISTOGRAM,
+                FopSelectorAction.MANAGE_PAYMENT_METHODS_OPTION_SELECTED,
+                FopSelectorAction.MAX_VALUE + 1);
+    }
+
+    private void onTurnOffPaymentPromptLinkClicked() {
+        mDelegate.showFinancialAccountsManagementSettings(mContext);
+
+        RecordHistogram.recordEnumeratedHistogram(
+                FOP_SELECTOR_USER_ACTION_HISTOGRAM,
+                FopSelectorAction.TURN_OFF_PAYMENT_PROMPT_LINK_CLICKED,
+                FopSelectorAction.MAX_VALUE + 1);
     }
 
     @VisibleForTesting
--- components/facilitated_payments/core/browser/facilitated_payments_manager.cc
+++ components/facilitated_payments/core/browser/facilitated_payments_manager.cc
@@ -195,7 +195,7 @@ void FacilitatedPaymentsManager::OnPixPaymentPromptResult(
                          ukm_source_id_);
     return;
   }
-
+  LogFopSelected();
   ShowProgressScreen();
 
   initiate_payment_request_details_->instrument_id_ = selected_instrument_id;
