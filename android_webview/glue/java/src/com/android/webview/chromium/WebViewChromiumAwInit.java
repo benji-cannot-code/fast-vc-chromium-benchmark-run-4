f91bbd02c6d4b5f63fe7498f3aee23855f8f3023
Abhijith Nair <abhijithnair@google.com>
2024-11-14 16:59:52 +0000

    Remove redundant reference from AwInit
    We held multiple references to the same
    AwTracingController object in WebViewChromiumAwInit.
    This CL removes the redundant reference.
    
    Bug: n/a
    Change-Id: Ia006d340aeb897ad7ac065e204e398c5bdc86aa0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018489
    Commit-Queue: Abhijith Nair <abhijithnair@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383053}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumAwInit.java
@@ -83,7 +83,6 @@ public class WebViewChromiumAwInit {
     // TODO(gsennton): store aw-objects instead of adapters here
     // Initialization guarded by mLock.
     private AwBrowserContext mDefaultBrowserContext;
-    private AwTracingController mTracingController;
     private SharedStatics mSharedStatics;
     private GeolocationPermissionsAdapter mDefaultGeolocationPermissions;
     private CookieManagerAdapter mDefaultCookieManager;
@@ -291,7 +290,7 @@ public class WebViewChromiumAwInit {
                 mDefaultWebStorage =
                         new WebStorageAdapter(
                                 mFactory, defaultBrowserContext.getQuotaManagerBridge());
-                mAwTracingController = getTracingController();
+                mAwTracingController = new AwTracingController();
                 mDefaultServiceWorkerController =
                         defaultBrowserContext.getServiceWorkerController();
                 mAwProxyController = new AwProxyController();
@@ -491,13 +490,6 @@ public class WebViewChromiumAwInit {
                         new AwNetworkChangeNotifierRegistrationPolicy(), forceUpdateNetworkState);
             }
         }
-    }
-
-    public AwTracingController getTracingController() {
-        if (mTracingController == null) {
-            mTracingController = new AwTracingController();
-        }
-        return mTracingController;
     }
 
     // Only on UI thread.
