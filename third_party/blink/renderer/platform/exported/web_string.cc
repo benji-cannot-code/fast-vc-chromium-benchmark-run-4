0bbc7e64c6fe7eaf613d93d94f31bef408c04b12
Kent Tamura <tkent@chromium.org>
2024-11-14 22:26:32 +0000

    Spanify some variants of WTF::Equal()
    This CL spanifies:
    
      `WTF::Equal(const StringImpl*, const LChar*, wtf_size_t)`
      `WTF::Equal(const StringImpl*, const UChar*, wtf_size_t)`
      `WTF::Equal(const StringImpl*, const char*, wtf_size_t)`
    
    This CL should have no behavior changes.
    
    Bug: 351564777
    Change-Id: I79687da4aef66e91898b4c3d53fc8cfa3e5789d0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022437
    Auto-Submit: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383276}

--- third_party/blink/renderer/platform/exported/web_string.cc
+++ third_party/blink/renderer/platform/exported/web_string.cc
@@ -142,8 +142,7 @@ bool WebString::Equals(const WebString& s) const {
 }
 
 bool WebString::Equals(std::string_view characters) const {
-  return Equal(impl_.get(), characters.data(),
-               base::checked_cast<wtf_size_t>(characters.length()));
+  return Equal(impl_.get(), characters);
 }
 
 size_t WebString::Find(const WebString& s) const {
--- third_party/blink/renderer/platform/wtf/text/atomic_string_table.cc
+++ third_party/blink/renderer/platform/wtf/text/atomic_string_table.cc
@@ -56,7 +56,7 @@ class UCharBuffer {
         hash_(ComputeHashAndMaskTop8Bits(chars, len, encoding)),
         encoding_(encoding) {}
 
-  const UChar* characters() const { return characters_; }
+  base::span<const UChar> characters() const { return {characters_, length_}; }
   unsigned length() const { return length_; }
   unsigned hash() const { return hash_; }
   AtomicStringUCharEncoding encoding() const { return encoding_; }
@@ -84,7 +84,7 @@ struct UCharBufferTranslator {
   static unsigned GetHash(const UCharBuffer& buf) { return buf.hash(); }
 
   static bool Equal(StringImpl* const& str, const UCharBuffer& buf) {
-    return WTF::Equal(str, buf.characters(), buf.length());
+    return WTF::Equal(str, buf.characters());
   }
 
   static void Store(StringImpl*& location,
@@ -362,8 +362,7 @@ struct LCharBufferTranslator {
   static unsigned GetHash(const LCharBuffer& buf) { return buf.hash(); }
 
   static bool Equal(StringImpl* const& str, const LCharBuffer& buf) {
-    auto chars = buf.characters();
-    return WTF::Equal(str, chars.data(), chars.size());
+    return WTF::Equal(str, buf.characters());
   }
 
   static void Store(StringImpl*& location,
