8864034f96b56b1f56aa82ac9a12eeba09e17ca6
Kenichi Ishibashi <bashi@chromium.org>
2024-11-14 08:50:24 +0000

    HttpStreamPool: Fix QUIC only attempt failure
    Before this CL, HttpStreamPool::AttemptManager didn't check
    allowed alpns in OnQuicTaskComplete() when failed. As a result,
    the AttemptManager would not complete forever. Check whether QUIC is
    the only allowed protocol in OnQuicTaskComplete() when failed. If so,
    complete AttemptManager with the failure.
    
    Bug: 346835898
    Change-Id: Ica905e0d1741cd9bd799bfd88a533ce900842715
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022199
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382868}

--- net/http/http_stream_pool_attempt_manager.cc
+++ net/http/http_stream_pool_attempt_manager.cc
@@ -588,7 +588,7 @@ void HttpStreamPool::AttemptManager::OnQuicTaskComplete(
 
   if (rv != OK &&
       (tcp_based_attempt_state_ == TcpBasedAttemptState::kAllAttemptsFailed ||
-       group_->force_quic())) {
+       group_->force_quic() || !CanUseTcpBasedProtocols())) {
     error_to_notify_ = rv;
     NotifyFailure();
     return;
