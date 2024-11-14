9a2e6ffdc5e628ee44dc939944d79bdf84f6076c
Alex Rudenko <alexrudenko@chromium.org>
2024-11-14 19:45:25 +0000

    Roll inspector_protocol to ed67c428c14e7b2617388327052dbd7824c26c3b
    Bug: 40276207
    Change-Id: I7c7be634c2d40a4cf7530c518c960dd5c6310ea9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6017963
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383158}

--- third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
+++ third_party/inspector_protocol/crdtp/chromium/protocol_traits.h
@@ -131,7 +131,7 @@ struct CRDTP_EXPORT ProtocolTypeTraits<Binary> {
 
 template <>
 struct detail::MaybeTypedef<Binary> {
-  typedef ValueMaybe<Binary> type;
+  typedef std::optional<Binary> type;
 };
 
 }  // namespace crdtp
--- third_party/inspector_protocol/crdtp/maybe_test.cc
+++ third_party/inspector_protocol/crdtp/maybe_test.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace crdtp {
 
 // =============================================================================
-// detail::PtrMaybe, detail::ValueMaybe, templates for optional
+// detail::PtrMaybe, templates for optional
 // pointers / values which are used in ../lib/Forward_h.template.
 // =============================================================================
 TEST(PtrMaybeTest, SmokeTest) {
@@ -29,17 +29,6 @@ TEST(PtrMaybeTest, SmokeTest) {
   EXPECT_TRUE(example.has_value());
   EXPECT_THAT(*example, testing::IsEmpty());
   EXPECT_THAT(out, testing::ElementsAre(42, 21));
-}
-
-TEST(ValueMaybeTest, SmokeTest) {
-  detail::ValueMaybe<int32_t> example;
-  EXPECT_FALSE(example.has_value());
-  EXPECT_EQ(-1, example.value_or(-1));
-  example = 42;
-  EXPECT_TRUE(example.has_value());
-  EXPECT_EQ(42, example.value());
-  int32_t out = *std::move(example);
-  EXPECT_EQ(out, 42);
 }
 
 }  // namespace crdtp
