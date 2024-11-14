ba8bd5ab71488be5c6d70dde60eccbf7e5c311b1
Vishwas Uppoor <vishwasuppoor@google.com>
2024-11-14 17:15:40 +0000

    [PIX] Log histograms when a UI screen is shown
    go/pix-ui-events
    go/pix-in-chrome-metrics-revised
    
    NO_IFTTT= They are already in sync.
    
    Bug: 377126728, 367751320
    Fixed: 378984733
    Change-Id: I5b59ea41338c6a98ec0b69ea55c48467d7d3dd66
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5986752
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383066}

--- components/facilitated_payments/core/metrics/facilitated_payments_metrics.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/strcat.h"
 #include "base/time/time.h"
+#include "components/facilitated_payments/core/ui_utils/facilitated_payments_ui_utils.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 
 namespace payments::facilitated {
@@ -137,6 +138,11 @@ void LogTransactionResult(TransactionResult result,
       .SetResult(static_cast<uint8_t>(result))
       .SetTriggerSource(static_cast<uint8_t>(trigger_source))
       .Record(ukm::UkmRecorder::Get());
+}
+
+void LogUiScreenShown(UiState ui_screen) {
+  base::UmaHistogramEnumeration("FacilitatedPayments.Pix.UiScreenShown",
+                                ui_screen);
 }
 
 }  // namespace payments::facilitated
--- components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
+++ components/facilitated_payments/core/metrics/facilitated_payments_metrics_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/metrics/histogram_tester.h"
 #include "base/test/task_environment.h"
 #include "base/types/expected.h"
+#include "components/facilitated_payments/core/ui_utils/facilitated_payments_ui_utils.h"
 #include "components/ukm/test_ukm_recorder.h"
 #include "services/metrics/public/cpp/ukm_builders.h"
 #include "services/metrics/public/cpp/ukm_source_id.h"
@@ -264,6 +265,28 @@ TEST_F(FacilitatedPaymentsMetricsUkmTest, LogTransactionResult_UkmLogged) {
             static_cast<uint8_t>(TransactionResult::kSuccess));
   EXPECT_EQ(ukm_entries[0].metrics.at("TriggerSource"),
             static_cast<uint8_t>(TriggerSource::kDOMSearch));
+}
+
+class FacilitatedPaymentsMetricsTestForUiScreens
+    : public testing::TestWithParam<UiState> {
+ public:
+  UiState ui_screen() { return GetParam(); }
+};
+
+INSTANTIATE_TEST_SUITE_P(FacilitatedPaymentsMetricsTest,
+                         FacilitatedPaymentsMetricsTestForUiScreens,
+                         testing::Values(UiState::kFopSelector,
+                                         UiState::kProgressScreen,
+                                         UiState::kErrorScreen));
+
+TEST_P(FacilitatedPaymentsMetricsTestForUiScreens, LogUiScreenShown) {
+  base::HistogramTester histogram_tester;
+
+  LogUiScreenShown(ui_screen());
+
+  histogram_tester.ExpectUniqueSample("FacilitatedPayments.Pix.UiScreenShown",
+                                      /*sample=*/ui_screen(),
+                                      /*expected_bucket_count=*/1);
 }
 
 }  // namespace payments::facilitated
