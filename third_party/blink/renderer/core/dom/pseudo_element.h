99a5e927061e92e87bcd3faa77fbb2f048b73404
Morten Stenshorne <mstensho@chromium.org>
2024-11-14 19:28:26 +0000

    Blockify scroll markers if needed on style change.
    We only blockified during box attachment, but if ::scroll-marker style
    is recalculated, we may also need to apply it then.
    
    Bug: 376834376
    Change-Id: I0ca72333ebccd4c3673b5d52520c934fb4a0bb1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023118
    Commit-Queue: Morten Stenshorne <mstensho@chromium.org>
    Reviewed-by: Rune Lillesveen <futhark@chromium.org>
    Reviewed-by: Daniil Sakhapov <sakhapov@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383148}

--- third_party/blink/renderer/core/dom/pseudo_element.h
+++ third_party/blink/renderer/core/dom/pseudo_element.h
@@ -71,7 +71,15 @@ class CORE_EXPORT PseudoElement : public Element {
   // unresolved = alias, kPseudoScrollMarkerGroup is resolved.
   // For styling and selector matching, return resolved version.
   PseudoId GetPseudoIdForStyling() const override;
-  const ComputedStyle* LayoutStyleForDisplayContents(const ComputedStyle&);
+
+  // Return the adjusted style needed by layout. In some cases computed style
+  // cannot be used as-is by layout. display:contents needs to be adjusted to
+  // display:inline. Scroll marker pseudo elements may need to blockify the
+  // display type (depending on the parent). Returns nullptr if no adjustment is
+  // necessary.
+  const ComputedStyle* AdjustedLayoutStyle(
+      const ComputedStyle& style,
+      const ComputedStyle& layout_parent_style);
 
   static AtomicString PseudoElementNameForEvents(Element*);
   static bool IsWebExposed(PseudoId, const Node*);
@@ -98,8 +106,6 @@ class CORE_EXPORT PseudoElement : public Element {
    public:
     AttachLayoutTreeScope(PseudoElement*, const AttachContext&);
     ~AttachLayoutTreeScope();
-
-    void OverrideComputedStyle(const ComputedStyle& new_style);
 
    private:
     PseudoElement* element_;
