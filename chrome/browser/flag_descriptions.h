f6050ac2e907e41dcbe1b1f3d8b4644f0f441d97
Bruno Braga <brunobraga@google.com>
2024-11-14 16:04:08 +0000

    Remove mullet related flags from chrome://flags
    Bug: 378835293
    Change-Id: I193554568d50cb26fb438063659ff148136c4244
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023158
    Commit-Queue: Bruno Braga <brunobraga@google.com>
    Reviewed-by: Theo Cristea <theocristea@google.com>
    Reviewed-by: Timofey Chudakov <tchudakov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383015}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -328,9 +328,6 @@ extern const char
 extern const char kAutofillEnableFpanRiskBasedAuthenticationName[];
 extern const char kAutofillEnableFpanRiskBasedAuthenticationDescription[];
 
-extern const char kAutofillEnableManualFallbackIPHName[];
-extern const char kAutofillEnableManualFallbackIPHDescription[];
-
 extern const char kAutofillEnableCardArtImageName[];
 extern const char kAutofillEnableCardArtImageDescription[];
 
@@ -354,12 +351,6 @@ extern const char kAutofillEnableCardInfoRuntimeRetrievalDescription[];
 
 extern const char kAutofillEnableCardProductNameName[];
 extern const char kAutofillEnableCardProductNameDescription[];
-
-extern const char kAutofillGranularFillingAvailableName[];
-extern const char kAutofillGranularFillingAvailableDescription[];
-
-extern const char kAutofillForUnclassifiedFieldsAvailableName[];
-extern const char kAutofillForUnclassifiedFieldsAvailableDescription[];
 
 extern const char kAutofillEnableLocalIbanName[];
 extern const char kAutofillEnableLocalIbanDescription[];
