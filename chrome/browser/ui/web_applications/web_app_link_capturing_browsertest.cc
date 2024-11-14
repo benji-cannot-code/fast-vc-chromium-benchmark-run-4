9fbdb986a5fc63cc9deb587188d7fe225ac52ebd
Dibyajyoti Pal <dibyapal@google.com>
2024-11-14 18:20:22 +0000

    [PWA/Nav Capturing] Migrate IntentChipButtonBrowserTest to use v2
    This CL migrates the IntentChipButtonBrowserTest to use the v2 default
    on and off flags on Windows, Mac and Linux, and the v1 default off
    flag on CrOS. The v1 default on flag is not used on CrOS since CrOS
    doesn't allow apps to capture links by default.
    
    As a small side change, the LinkCapturingVersionToString() function is
    moved closer to the test support functions for reusability.
    
    Fixed: 378711984
    Bug: 351775835
    Change-Id: Idf93b423982b3b06bd6a154605e5e7466e728450
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015825
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Mike Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383104}

--- chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
+++ chrome/browser/ui/web_applications/web_app_link_capturing_browsertest.cc
@@ -62,13 +62,6 @@ using ui_test_utils::BrowserChangeObserver;
 namespace web_app {
 namespace {
 using ClientMode = LaunchHandler::ClientMode;
-
-std::string LinkCapturingVersionToString(
-    const testing::TestParamInfo<apps::test::LinkCapturingFeatureVersion>&
-        version) {
-  return apps::test::ToString(version.param);
-}
-
 // Tests that links are captured correctly into an installed WebApp using the
 // 'tabbed' display mode, which allows the webapp window to have multiple tabs.
 class WebAppLinkCapturingBrowserTest
@@ -720,7 +713,7 @@ INSTANTIATE_TEST_SUITE_P(
     testing::Values(apps::test::LinkCapturingFeatureVersion::kV2DefaultOff,
                     apps::test::LinkCapturingFeatureVersion::kV2DefaultOn),
 #endif
-    LinkCapturingVersionToString);
+    apps::test::LinkCapturingVersionToString);
 
 // TODO(crbug.com/376922620): Add tabbed mode support for navigation capturing.
 #if BUILDFLAG(IS_CHROMEOS)
@@ -798,7 +791,7 @@ INSTANTIATE_TEST_SUITE_P(
     ,
     WebAppTabStripLinkCapturingBrowserTest,
     testing::Values(apps::test::LinkCapturingFeatureVersion::kV1DefaultOff),
-    LinkCapturingVersionToString);
+    apps::test::LinkCapturingVersionToString);
 #endif
 
 }  // namespace
