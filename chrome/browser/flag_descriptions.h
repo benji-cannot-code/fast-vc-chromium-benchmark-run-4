af3dbe8b3e68517607ff5f7dd1ec98181245b091
Jayson Adams <shrike@chromium.org>
2024-11-14 22:11:27 +0000

    [Mac A11y] Add kMacAccessibilityAPIMigration to chrome://flags
    Per https://chromium.googlesource.com/chromium/src/+/main/docs/how_to_add_your_feature_flag.md#step-2_adding-the-feature-flag-to-the-chrome_flags-ui
    
    Bug: 363275809
    Change-Id: I6555e2982a161659e9c2b281e8184f13e13655a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021357
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383266}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -3234,6 +3234,9 @@ extern const char kEnableExtensibleEnterpriseSSODescription[];
 extern const char kImmersiveFullscreenName[];
 extern const char kImmersiveFullscreenDescription[];
 
+extern const char kMacAccessibilityAPIMigrationName[];
+extern const char kMacAccessibilityAPIMigrationDescription[];
+
 extern const char kMacImeLiveConversionFixName[];
 extern const char kMacImeLiveConversionFixDescription[];
 
