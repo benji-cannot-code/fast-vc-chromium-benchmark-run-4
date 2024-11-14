f37c1112a683260e27725533217eeea31d62f3a5
Daniel Cheng <dcheng@chromium.org>
2024-11-14 09:52:59 +0000

    Extend base::Extend to support a generic range as input
    This replaces the overload that takes a `base::span` and adds support
    for projections, similar to `base::ToVector`. Note that this does not
    update the overload that consumes a `std::vector<T>&&`, and that the
    legacy `std::vector<T>&&` overload does not support projections. The
    right way to implement this is to probably add an adapter to treat the
    values in the range as rvalues.
    
    Bug: 378623811
    Change-Id: I95bbcd99ad12c5bccc991c1aac48f4b2bc2eba4e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015246
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382886}

--- base/containers/extend_unittest.cc
+++ base/containers/extend_unittest.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/containers/adapters.h"
+#include "base/containers/circular_deque.h"
 #include "base/containers/span.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -137,6 +139,34 @@ TEST(ExtendTest, ExtendWithSpan) {
   Extend(dst, kRawArray);
   EXPECT_THAT(dst,
               ElementsAre(3, 4, 5, 6, 7, 8, 9, 10, 11, 9, 10, 11, 3, 4, 5));
+}
+
+TEST(ExtendTest, ExtendWithRanges) {
+  std::set<int> set = {1, 2, 3};
+  circular_deque<int> deque = {4, 5, 6};
+  {
+    std::vector<int> dst;
+    Extend(dst, Reversed(set));
+    EXPECT_THAT(dst, ElementsAre(3, 2, 1));
+    Extend(dst, Reversed(deque));
+    EXPECT_THAT(dst, ElementsAre(3, 2, 1, 6, 5, 4));
+  }
+
+  {
+    std::vector<int> dst;
+    Extend(dst, set, [](int val) { return -val; });
+    EXPECT_THAT(dst, ElementsAre(-1, -2, -3));
+    Extend(dst, deque, [](int val) { return val + 1; });
+    EXPECT_THAT(dst, ElementsAre(-1, -2, -3, 5, 6, 7));
+  }
+
+  {
+    std::vector<int> dst;
+    Extend(dst, Reversed(set), [](int val) { return val * val; });
+    EXPECT_THAT(dst, ElementsAre(9, 4, 1));
+    Extend(dst, Reversed(deque), [](int val) { return val - 1; });
+    EXPECT_THAT(dst, ElementsAre(9, 4, 1, 5, 4, 3));
+  }
 }
 
 }  // namespace base
