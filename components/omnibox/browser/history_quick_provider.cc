98adb0df3d4898187ff3ba6a00631cc9f73c27f3
Brandon Wylie <wylieb@chromium.org>
2024-11-14 19:45:40 +0000

    [hub] Prevent max_matches logic from applying to hub search
    Ignore artificial limits imposed on bookmark/history providers for
    android hub page classifications.
    
    Bug: 378964498
    Change-Id: Ic658433947d969789793827b17e758d4d4e95a7f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022316
    Reviewed-by: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Brandon Wylie <wylieb@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383159}

--- components/omnibox/browser/history_quick_provider.cc
+++ components/omnibox/browser/history_quick_provider.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/omnibox/browser/history_url_provider.h"
 #include "components/omnibox/browser/in_memory_url_index.h"
 #include "components/omnibox/browser/keyword_provider.h"
+#include "components/omnibox/browser/match_compare.h"
 #include "components/omnibox/browser/omnibox_field_trial.h"
 #include "components/omnibox/browser/omnibox_triggered_feature_service.h"
 #include "components/omnibox/browser/url_prefix.h"
@@ -46,6 +47,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/page_transition_types.h"
 #include "url/third_party/mozilla/url_parse.h"
 #include "url/url_util.h"
+
+namespace {
+constexpr int kAndroidHubMaxMatches = 5;
+}  // namespace
 
 bool HistoryQuickProvider::disabled_ = false;
 
@@ -92,6 +97,12 @@ void HistoryQuickProvider::DoAutocomplete() {
   size_t max_matches = autocomplete_input_.InKeywordMode()
                            ? provider_max_matches_in_keyword_mode_
                            : provider_max_matches_;
+  if (autocomplete_input_.current_page_classification() ==
+      metrics::OmniboxEventProto::ANDROID_HUB) {
+    // LINT.IfChange(HubHistoryMaxMatches)
+    max_matches = kAndroidHubMaxMatches;
+    // LINT.ThenChange(//components/omnibox/browser/autocomplete_grouper_sections.cc:HubHistorySectionSlots)
+  }
 
   // Get the matching URLs from the DB.
   ScoredHistoryMatches matches = in_memory_url_index_->HistoryItemsForTerms(
@@ -127,12 +138,16 @@ void HistoryQuickProvider::DoAutocomplete() {
 
   add_matches(matches);
 
-  // If ML scoring is enabled, mark all "extra" matches as `culled_by_provider`.
-  // If ML scoring is disabled, this is effectively a no-op as the matches will
-  // already be resized in the above call to `HistoryItemsForTerms()`.
-  ResizeMatches(
-      max_matches,
-      OmniboxFieldTrial::IsMlUrlScoringUnlimitedNumCandidatesEnabled());
+  if (autocomplete_input_.current_page_classification() !=
+      PageClassification::OmniboxEventProto_PageClassification_ANDROID_HUB) {
+    // If ML scoring is enabled, mark all "extra" matches as
+    // `culled_by_provider`. If ML scoring is disabled, this is effectively a
+    // no-op as the matches will already be resized in the above call to
+    // `HistoryItemsForTerms()`.
+    ResizeMatches(
+        max_matches,
+        OmniboxFieldTrial::IsMlUrlScoringUnlimitedNumCandidatesEnabled());
+  }
 
   // Add suggestions from the user's highly visited domains bypassing
   // `provider_max_matches_`.
