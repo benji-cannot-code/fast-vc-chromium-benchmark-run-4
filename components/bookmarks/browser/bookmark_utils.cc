c2ef54d2c8f47ccc25302db84fb46441b5835d59
Mikel Astiz <mastiz@chromium.org>
2024-11-14 13:54:59 +0000

    [bookmarks] Replace pipes with backticks in comments
    The patch modernizes comments under components/bookmarks to follow the
    latest guidance in
    https://chromium.googlesource.com/chromium/src/+/main/styleguide/c++/c++-dos-and-donts.md#comment-style
    
    Change-Id: I17d35f963345d8fe409c5c27201583ecdc067230
    Bug: None
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023019
    Reviewed-by: Marc Treib <treib@chromium.org>
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382938}

--- components/bookmarks/browser/bookmark_utils.cc
+++ components/bookmarks/browser/bookmark_utils.cc
@@ -82,7 +82,7 @@ bool MoreRecentlyModified(const BookmarkNode* n1, const BookmarkNode* n2) {
   return n1->date_folder_modified() > n2->date_folder_modified();
 }
 
-// Returns true if |text| contains each string in |words|. This is used when
+// Returns true if `text` contains each string in `words`. This is used when
 // searching for bookmarks.
 bool DoesBookmarkTextContainWords(const std::u16string& text,
                                   const std::vector<std::u16string>& words) {
@@ -117,7 +117,7 @@ bool HasSelectedAncestor(
   return HasSelectedAncestor(model, selected_nodes, node->parent());
 }
 
-// Recursively searches for a node satisfying the functor |pred| . Returns
+// Recursively searches for a node satisfying the functor `pred` . Returns
 // nullptr if not found.
 template <typename Predicate>
 const BookmarkNode* FindNode(const BookmarkNode* node, Predicate pred) {
@@ -282,8 +282,8 @@ void CopyToClipboard(
   }
 }
 
-// Updates |title| such that |url| and |title| pair are unique among the
-// children of |parent|.
+// Updates `title` such that `url` and `title` pair are unique among the
+// children of `parent`.
 void MakeTitleUnique(const BookmarkModel* model,
                      const BookmarkNode* parent,
                      const GURL& url,
@@ -477,7 +477,7 @@ std::vector<std::u16string> ParseBookmarkQuery(
   return query_words;
 }
 
-// Returns true if |node|s title or url contains the strings in |words|.
+// Returns true if `node`s title or url contains the strings in `words`.
 bool DoesBookmarkContainWords(const std::u16string& title,
                               const GURL& url,
                               const std::vector<std::u16string>& words) {
