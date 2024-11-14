5c59905f1eef573e97fbdebc42d3e49325f0f3bc
S. Ganesh <ganesh@chromium.org>
2024-11-14 18:24:34 +0000

    updater: implement a Configurator and PersistedData for standalone pings
    This CL implements versions of the `Configurator` and `PersistedData`
    for use with standalone pings outside of UpdateService.
    
    Low-Coverage-Reason: COVERAGE_UNDERREPORTED updater integration tests
    Bug: 40944565,374957472
    Change-Id: Ibc97750636db1c417a560100a416bba7350f86ce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022615
    Auto-Submit: S Ganesh <ganesh@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Commit-Queue: S Ganesh <ganesh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383109}

--- chrome/updater/net/network.h
+++ chrome/updater/net/network.h
@@ -12,11 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_refptr.h"
 #include "base/sequence_checker.h"
 #include "build/build_config.h"
+#include "chrome/updater/policy/service.h"
 #include "components/update_client/network.h"
 
 namespace updater {
-
-struct PolicyServiceProxyConfiguration;
 
 // Creates instances of `NetworkFetcher`. Because of idiosyncrasies of how
 // the Windows implementation works, the instance of the factory class must
---
+++ chrome/updater/ping_configurator.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_UPDATER_PING_CONFIGURATOR_H_
+#define CHROME_UPDATER_PING_CONFIGURATOR_H_
+
+#include "base/memory/scoped_refptr.h"
+#include "components/update_client/configurator.h"
+
+namespace updater {
+
+// This is a lightweight `Configurator` that has very few dependencies. The
+// implementation has `NOTREACHED` for many methods, and is solely expected to
+// be used for standalone pings.
+scoped_refptr<update_client::Configurator> CreatePingConfigurator();
+
+}  // namespace updater
+
+#endif  // CHROME_UPDATER_PING_CONFIGURATOR_H_
