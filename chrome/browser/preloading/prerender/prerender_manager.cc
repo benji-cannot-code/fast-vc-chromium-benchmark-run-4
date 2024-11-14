69a97fa008d0fb086899b22c6b90c3c8efb264d9
Hiroki Nakagawa <nhiroki@chromium.org>
2024-11-14 16:16:23 +0000

    WebView Prerender: Support NoVarySearch hint
    This CL implements NoVarySearch hint for WebView prerender API (Note
    that the API itself hasn't been implemented yet).
    
    - The WebView prefetch API has already implemented the NoVarySearch
      hint. To reuse helper functions, this CL moves
      GetPrefetchExpectedNoVarySearch() etc from aw_browser_context to
      aw_preloading_utils. The prerender API takes AwPrefetchParameters and
      retrievs the hint from the parameter using the functions.
    
    - This CL also passes the additional request headers param for
      prerendering navigation from Java to C++, but actually it's not used
      in this CL. Follow-up CLs will use it.
    
    Bug: 41490450
    Change-Id: I231c0fc2406a6f413d2441230562f98722908a54
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6002156
    Reviewed-by: Wayne Jackson Jr. <wbjacksonjr@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Rakina Zata Amni <rakina@chromium.org>
    Reviewed-by: Richard (Torne) Coles <torne@chromium.org>
    Reviewed-by: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Taiyo Mizuhashi <taiyo@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383024}

--- chrome/browser/preloading/prerender/prerender_manager.cc
+++ chrome/browser/preloading/prerender/prerender_manager.cc
@@ -236,6 +236,7 @@ PrerenderManager::StartPrerenderBookmark(const GURL& prerendering_url) {
   bookmark_prerender_handle_ = web_contents()->StartPrerendering(
       prerendering_url, content::PreloadingTriggerType::kEmbedder,
       prerender_utils::kBookmarkBarMetricSuffix,
+      /*no_vary_search_expected=*/std::nullopt,
       ui::PageTransitionFromInt(ui::PAGE_TRANSITION_AUTO_BOOKMARK),
       // Considering the characteristics of triggers (e.g., the duration from
       // trigger to activation), warm-up is not enabled for now on this trigger.
@@ -300,6 +301,7 @@ PrerenderManager::StartPrerenderNewTabPage(
   new_tab_page_prerender_handle_ = web_contents()->StartPrerendering(
       prerendering_url, content::PreloadingTriggerType::kEmbedder,
       prerender_utils::kNewTabPageMetricSuffix,
+      /*no_vary_search_expected=*/std::nullopt,
       ui::PageTransitionFromInt(ui::PAGE_TRANSITION_AUTO_BOOKMARK),
       // Considering the characteristics of triggers (e.g., the duration from
       // trigger to activation), warm-up is not enabled for now on this trigger.
@@ -364,6 +366,7 @@ PrerenderManager::StartPrerenderDirectUrlInput(
   direct_url_input_prerender_handle_ = web_contents()->StartPrerendering(
       prerendering_url, content::PreloadingTriggerType::kEmbedder,
       prerender_utils::kDirectUrlInputMetricSuffix,
+      /*no_vary_search_expected=*/std::nullopt,
       ui::PageTransitionFromInt(ui::PAGE_TRANSITION_TYPED |
                                 ui::PAGE_TRANSITION_FROM_ADDRESS_BAR),
       /*should_warm_up_compositor=*/true,
@@ -406,6 +409,7 @@ void PrerenderManager::StartPrerenderSearchResult(
       web_contents()->StartPrerendering(
           prerendering_url, content::PreloadingTriggerType::kEmbedder,
           prerender_utils::kDefaultSearchEngineMetricSuffix,
+          /*no_vary_search_expected=*/std::nullopt,
           ui::PageTransitionFromInt(ui::PAGE_TRANSITION_GENERATED |
                                     ui::PAGE_TRANSITION_FROM_ADDRESS_BAR),
           /*should_warm_up_compositor=*/true,
