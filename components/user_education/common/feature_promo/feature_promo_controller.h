0c282b14d23f90c5f44d656b11f8e343efb2a6a9
Dana Fried <dfried@chromium.org>
2024-11-14 01:28:01 +0000

    [User Education] Push more UE2.0 implementation down into 2.0 code
    The way that Feature Engagement demo mode is handled will be different
    in User Education 2.5 vs. 2.0 promo controller, so push that code down
    into the implementation.
    
    Bug: 369887098
    Change-Id: Iad72d145ad3e122da939bb50245c027b445cdd42
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020755
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Alison Gale <agale@chromium.org>
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382739}

--- components/user_education/common/feature_promo/feature_promo_controller.h
+++ components/user_education/common/feature_promo/feature_promo_controller.h
@@ -361,8 +361,6 @@ class FeaturePromoControllerCommon : public FeaturePromoController {
   const FeaturePromoRegistry* registry() const { return registry_; }
   FeaturePromoRegistry* registry() { return registry_; }
 
-  bool in_iph_demo_mode() const { return in_iph_demo_mode_; }
-
   // Removes a promo from the queue and returns whether the promo was found and
   // canceled.
   virtual bool MaybeUnqueuePromo(const base::Feature& iph_feature) = 0;
@@ -480,9 +478,6 @@ class FeaturePromoControllerCommon : public FeaturePromoController {
       FeaturePromoSpecification::CustomActionCallback custom_action_callback,
       bool custom_action_is_default,
       int custom_action_dismiss_string_id);
-
-  // Whether the IPH Demo Mode flag has been set at startup.
-  const bool in_iph_demo_mode_;
 
   // The feature promo registry to use.
   const raw_ptr<FeaturePromoRegistry> registry_;
