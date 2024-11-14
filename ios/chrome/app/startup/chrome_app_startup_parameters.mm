cc84dcd9437d55cfee502434c0c1c7fb9165b7ab
Federica Germinario <fedegermi@google.com>
2024-11-14 15:16:10 +0000

    Revert "[ios] Remove call to NSUserDefaults synchronize"
    This reverts commit d3778f410c36db17987a83dcfe2bfa5e11539e4b.
    
    Reason for revert: It's safer to keep that code for now
    
    Original change's description:
    > [ios] Remove call to NSUserDefaults synchronize
    >
    > Apple documentation discourages use of synchronize because unnecessary.
    > https://developer.apple.com/documentation/foundation/nsuserdefaults/1414005-synchronize
    >
    > Bug: none
    > Change-Id: I769e11b43cf40f6588d1e0263fe759e41d684aa2
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5975529
    > Auto-Submit: Federica Germinario <fedegermi@google.com>
    > Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1377637}
    
    Bug: none
    Change-Id: I62ba5db9390c336cb61b135f73a686a9db9dff5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018308
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Auto-Submit: Federica Germinario <fedegermi@google.com>
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382985}

--- ios/chrome/app/startup/chrome_app_startup_parameters.mm
+++ ios/chrome/app/startup/chrome_app_startup_parameters.mm
@@ -547,6 +547,10 @@ TabOpeningPostOpeningAction XCallbackPoaToPostOpeningAction(
 
   [sharedDefaults removeObjectForKey:commandDictionaryPreference];
 
+  // `sharedDefaults` is used for communication between apps. Synchronize to
+  // avoid synchronization issues (like removing the next order).
+  [sharedDefaults synchronize];
+
   if (!commandDictionary) {
     return nil;
   }
