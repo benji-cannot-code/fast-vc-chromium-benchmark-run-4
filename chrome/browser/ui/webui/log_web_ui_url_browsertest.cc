62664abc428dcba006040da2e115bd07c36e87f2
Keren Zhu <kerenzhu@chromium.org>
2024-11-14 17:16:07 +0000

    Add metrics for preloaded WebUIs
    This CL adds two metrics:
    - WebUI.PreloadedUrl: This metric logs the URL of a preloaded WebUI.
    - WebUI.PreloadedAndShownUrl: This metric logs the URL of a preloaded
    WebUI on its first non-empty paint.
    
    Bug: 375209495
    Change-Id: I560a4ab0ea493ab7da80354be92e2f0fa2429c39
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015280
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Roman Arora <romanarora@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Keren Zhu <kerenzhu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383067}

--- chrome/browser/ui/webui/log_web_ui_url_browsertest.cc
+++ chrome/browser/ui/webui/log_web_ui_url_browsertest.cc
@@ -35,29 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::Bucket;
 using testing::ElementsAre;
 
-namespace {
-
-class WebContentsNonEmptyPaintWaiter : public content::WebContentsObserver {
- public:
-  explicit WebContentsNonEmptyPaintWaiter(content::WebContents* web_contents)
-      : WebContentsObserver(web_contents) {}
-
-  void Wait() {
-    if (web_contents()->CompletedFirstVisuallyNonEmptyPaint()) {
-      return;
-    }
-    run_loop_.Run();
-  }
-
- private:
-  // WebContentsObserver:
-  void DidFirstVisuallyNonEmptyPaint() override { run_loop_.Quit(); }
-
-  base::RunLoop run_loop_;
-};
-
-}  // namespace
-
 namespace webui {
 
 class LogWebUIUrlTest : public InProcessBrowserTest {
@@ -145,7 +122,7 @@ IN_PROC_BROWSER_TEST_F(LogWebUIUrlTest, ShownWebUI) {
   browser()->tab_strip_model()->InsertWebContentsAt(0, std::move(web_contents),
                                                     AddTabTypes::ADD_ACTIVE);
 
-  WebContentsNonEmptyPaintWaiter(web_contents_ptr).Wait();
+  content::WaitForFirstNonEmptyPaint(web_contents_ptr);
   EXPECT_THAT(histogram_tester().GetAllSamples(webui::kWebUIShownUrl),
               ElementsAre(Bucket(origin_hash, 1)));
 }
