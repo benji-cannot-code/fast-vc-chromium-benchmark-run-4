25ee05451d106c2bc62706fcafe9e433eb024d34
Koji Ishii <kojii@chromium.org>
2024-11-14 06:49:07 +0000

    [text-box-trim] Extract common test values
    This patch extraccts common test values to an external file.
    
    This patch has no behavior changes.
    
    Bug: 40254880
    Change-Id: I58ce558bb40831a92a2f5c8fcd066ed1f2ff4700
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6018683
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Auto-Submit: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382828}

---
+++ third_party/blink/web_tests/external/wpt/css/css-inline/text-box-trim/parsing/values.js
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+const text_box_edge_values = [
+  "auto",
+  "text",
+  "ideographic",
+  "ideographic-ink",
+
+  ["text text", "text"],
+  "text alphabetic",
+  "text ideographic",
+  "text ideographic-ink",
+
+  "cap text",
+  "cap alphabetic",
+  "cap ideographic",
+  "cap ideographic-ink",
+
+  "ex text",
+  "ex alphabetic",
+  "ex ideographic",
+  "ex ideographic-ink",
+
+  "ideographic text",
+  "ideographic alphabetic",
+  ["ideographic ideographic", "ideographic"],
+  "ideographic ideographic-ink",
+
+  "ideographic-ink text",
+  "ideographic-ink alphabetic",
+  "ideographic-ink ideographic",
+  ["ideographic-ink ideographic-ink", "ideographic-ink"],
+];
