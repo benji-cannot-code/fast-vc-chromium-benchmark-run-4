7a4f96e692c3ccf629b2ae5829fb3b79ffded423
Jacob Stanley <jacobstanley@google.com>
2024-11-14 18:50:54 +0000

    Enale flaky NoticeEEA test
    Overriding isPrivacySandboxAdsApiUxEnhancementsEnabled to false for
    NoticeEEA and created a new NoticeEEAAdsApiUxEnhancement suite.
    
    Bug: 378958535
    Change-Id: If0266f9bdbbdbcee18e7c55f9ab7d68e296e56a2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023625
    Reviewed-by: Abe Boujane <boujane@google.com>
    Commit-Queue: Jacob Stanley <jacobstanley@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383125}

--- chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.cc
+++ chrome/test/data/webui/privacy_sandbox/privacy_sandbox_dialog_browsertest.cc
@@ -68,13 +68,7 @@ IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest, Combined) {
   RunTestSuite("Combined");
 }
 
-// TODO(crbug.com/378958535): Flaky on Windows & ChromeOS.
-#if BUILDFLAG(IS_WIN) || BUILDFLAG(IS_CHROMEOS)
-#define MAYBE_NoticeEEA DISABLED_NoticeEEA
-#else
-#define MAYBE_NoticeEEA NoticeEEA
-#endif
-IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest, MAYBE_NoticeEEA) {
+IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest, NoticeEEA) {
   RunTestSuite("NoticeEEA");
 }
 
@@ -97,4 +91,8 @@ IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest,
 
 IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest, CombinedAdsApiUxEnhancement) {
   RunTestSuite("CombinedAdsApiUxEnhancement");
+}
+
+IN_PROC_BROWSER_TEST_P(PrivacySandboxDialogTest, NoticeEEAAdsApiUxEnhancement) {
+  RunTestSuite("NoticeEEAAdsApiUxEnhancement");
 }
