188644293cfda85fb7dc7c4e55d0836b552f375e
Nina Satragno <nsatragno@chromium.org>
2024-11-14 17:51:00 +0000

    [webauthn] Mark more tests as needing linking
    Mark some WebAuthn tests as needing the WebAuthnHybridLinking feature.
    These tests will be removed once support for hybrid linking is dropped.
    
    Bug: 372493822
    Change-Id: I45cc4ea2b2a0c0ee26c28ea913d1bd378786c507
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6001038
    Reviewed-by: Adam Langley <agl@chromium.org>
    Auto-Submit: Nina Satragno <nsatragno@chromium.org>
    Commit-Queue: Adam Langley <agl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383079}

--- chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
+++ chrome/browser/webauthn/chrome_authenticator_request_delegate_unittest.cc
@@ -655,9 +655,18 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, MaybeGetRelyingPartyIdOverride) {
   }
 }
 
+// TODO(crbug.com/372493822): remove these tests when hybrid linking is cleaned
+// up.
+class HybridLinkingChromeAuthenticatorRequestDelegateTest
+    : public ChromeAuthenticatorRequestDelegateTest {
+ protected:
+  base::test::ScopedFeatureList scoped_feature_list_{
+      device::kWebAuthnHybridLinking};
+};
+
 // Tests that synced GPM passkeys are injected in the transport availability
-// info.
-TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys) {
+// info if there's a phone paired.
+TEST_F(HybridLinkingChromeAuthenticatorRequestDelegateTest, GpmPasskeys) {
   std::string relying_party = "example.com";
   GURL url("https://example.com");
   content::WebContentsTester::For(web_contents())->NavigateAndCommit(url);
@@ -729,7 +738,8 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys) {
 
 // Tests that synced GPM passkeys are not discovered if there are no sync paired
 // phones.
-TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys_NoSyncPairedPhones) {
+TEST_F(HybridLinkingChromeAuthenticatorRequestDelegateTest,
+       GpmPasskeys_NoSyncPairedPhones) {
   GURL url("https://example.com");
   content::WebContentsTester::For(web_contents())->NavigateAndCommit(url);
   ChromeWebAuthnCredentialsDelegateFactory::CreateForWebContents(
@@ -775,7 +785,8 @@ TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys_NoSyncPairedPhones) {
 }
 
 // Tests that shadowed GPM passkeys are not discovered.
-TEST_F(ChromeAuthenticatorRequestDelegateTest, GpmPasskeys_ShadowedPasskeys) {
+TEST_F(HybridLinkingChromeAuthenticatorRequestDelegateTest,
+       GpmPasskeys_ShadowedPasskeys) {
   GURL url("https://example.com");
   content::WebContentsTester::For(web_contents())->NavigateAndCommit(url);
   ChromeWebAuthnCredentialsDelegateFactory::CreateForWebContents(
--- chrome/browser/webauthn/chrome_webauthn_autofill_interactive_uitest.cc
+++ chrome/browser/webauthn/chrome_webauthn_autofill_interactive_uitest.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/password_manager/core/browser/password_store/password_store_interface.h"
 #include "components/strings/grit/components_strings.h"
+#include "components/sync/base/features.h"
 #include "components/sync/test/test_sync_service.h"
 #include "components/sync_device_info/device_info.h"
 #include "components/sync_device_info/fake_device_info_sync_service.h"
@@ -253,7 +254,7 @@ class WebAuthnAutofillIntegrationTest : public CertVerifierBrowserTest {
 
   void SetUp() override {
     scoped_feature_list_.InitWithFeatures(
-        {},
+        {device::kWebAuthnHybridLinking},
         /*disabled_features=*/{
             // Disable this feature explicitly, as it can cause unexpected email
             // fields to be parsed in these tests.
@@ -581,6 +582,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthnDevtoolsAutofillIntegrationTest,
   RunSelectAccountTest(kConditionalUIRequestFiltered);
 }
 
+// TODO(crbug.com/372493822): remove when hybrid linking flag is removed.
 IN_PROC_BROWSER_TEST_F(WebAuthnDevtoolsAutofillIntegrationTest,
                        GPMPasskeys) {
   // Have the virtual device masquerade as a phone.
@@ -659,6 +661,7 @@ IN_PROC_BROWSER_TEST_F(WebAuthnDevtoolsAutofillIntegrationTest,
 
 // Tests that downloading passkeys from sync during a conditional UI also
 // updates the autofill popup with the newly downloaded credentials.
+// TODO(crbug.com/372493822): remove when hybrid linking flag is removed.
 IN_PROC_BROWSER_TEST_F(WebAuthnDevtoolsAutofillIntegrationTest,
                        GPMPasskeys_UpdatePasskeys) {
   // Have the virtual device masquerade as a phone.
