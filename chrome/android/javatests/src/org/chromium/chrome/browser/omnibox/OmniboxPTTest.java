9bd6234f291d1280e7c2b0b9c7868ed14c0997c2
Henrique Nakashima <hnakashima@chromium.org>
2024-11-14 20:12:30 +0000

    [Android] Disable OmniboxPTTest#testOpenTypeDelete_fromWebPage in 12
    Bug: 372709072
    Change-Id: I247951dcde71e16780429cc03b4d2e07671b20c3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020801
    Auto-Submit: Henrique Nakashima <hnakashima@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Owners-Override: Henrique Nakashima <hnakashima@chromium.org>
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383172}

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/OmniboxPTTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser.omnibox;
 
+import android.os.Build;
 import android.util.Pair;
 
 import androidx.test.filters.LargeTest;
@@ -19,6 +20,7 @@ import org.chromium.base.test.transit.BatchedPublicTransitRule;
 import org.chromium.base.test.transit.TransitAsserts;
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisableIf;
 import org.chromium.base.test.util.JniMocker;
 import org.chromium.chrome.browser.flags.ChromeSwitches;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -60,6 +62,10 @@ public class OmniboxPTTest {
 
     @LargeTest
     @Test
+    @DisableIf.Build(
+            sdk_is_greater_than = Build.VERSION_CODES.R,
+            sdk_is_less_than = Build.VERSION_CODES.TIRAMISU,
+            message = "Flaky in S, crbug.com/372709072")
     public void testOpenTypeDelete_fromWebPage() {
         WebPageStation blankPage = mEntryPoints.startOnBlankPage(mBatchedRule);
         var omniboxAndKeyboard = blankPage.openOmnibox(sFakeSuggestions);
