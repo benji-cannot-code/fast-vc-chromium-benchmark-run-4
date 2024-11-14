94c5e4ae4db47791510996024a3b439a916872e8
Ioana Pandele <ioanap@chromium.org>
2024-11-14 13:32:38 +0000

    [UPMLoginDbSunset]Flag for deprecating the login DB for unmigrated users
    Bug: 378676341
    Change-Id: Ic6cc11144b68ae54f8cfbcaeb90e1e7f6069ef04
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012426
    Reviewed-by: Viktor Semeniuk <vsemeniuk@google.com>
    Commit-Queue: Viktor Semeniuk <vsemeniuk@google.com>
    Auto-Submit: Ioana Pandele <ioanap@chromium.org>
    Commit-Queue: Ioana Pandele <ioanap@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382899}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -1419,6 +1419,11 @@ extern const char kLocationBarModelOptimizationsDescription[];
 extern const char kLogJsConsoleMessagesName[];
 extern const char kLogJsConsoleMessagesDescription[];
 
+#if BUILDFLAG(IS_ANDROID)
+extern const char kLoginDbDeprecationAndroidName[];
+extern const char kLoginDbDeprecationAndroidDescription[];
+#endif  // BUILDFLAG(IS_ANDROID)
+
 extern const char kMediaRouterCastAllowAllIPsName[];
 extern const char kMediaRouterCastAllowAllIPsDescription[];
 
