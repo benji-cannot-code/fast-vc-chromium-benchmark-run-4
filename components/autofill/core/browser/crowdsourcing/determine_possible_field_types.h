f7660f0737fbd584ee19748b7b18e999d793c42e
Christoph Schwering <schwering@google.com>
2024-11-14 23:04:21 +0000

    [Autofill] Move PreProcessStateMatchingTypes() out of BAM
    This CL pulls PreProcessStateMatchingTypes() out of
    BrowserAutofillManager and moves it to
    determine_possible_field_types.{h,cc}.
    
    Bug: 374086145
    Change-Id: I22626ed5212fa848caf5fe38a5a4f0ce72952872
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023140
    Commit-Queue: Christoph Schwering <schwering@google.com>
    Reviewed-by: Dominic Battré <battre@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1383302}

--- components/autofill/core/browser/crowdsourcing/determine_possible_field_types.h
+++ components/autofill/core/browser/crowdsourcing/determine_possible_field_types.h
@@ -13,9 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
+class AutofillClient;
 class AutofillProfile;
 class CreditCard;
 class FormStructure;
+
+// For each submitted field in the `form_structure`, determines whether
+// `ADDRESS_HOME_STATE` is a possible matching type.
+// This function is intended to run before
+// DeterminePossibleFieldTypesForUpload() on the browser UI thread.
+void PreProcessStateMatchingTypes(const AutofillClient& client,
+                                  const std::vector<AutofillProfile>& profiles,
+                                  FormStructure& form_structure);
 
 // Uses the existing personal data in |profiles| and |credit_cards| to
 // determine possible field types for the |form|.  This is
