aaa71877c34a311849c5bcaa4e0886c3eacdd720
Christopher Cameron <ccameron@chromium.org>
2024-11-14 13:29:21 +0000

    gfx::ColorTransform: Remove obsolete features
    Remove features that have been enabled by default for a very long time.
    
    Bug: 378966966
    Change-Id: I479427267a59502edca674b8972b43ec3fa53390
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023018
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382894}

--- ui/gfx/color_transform.h
+++ ui/gfx/color_transform.h
@@ -20,9 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 
-COLOR_SPACE_EXPORT BASE_DECLARE_FEATURE(kHlgPqUnifiedTonemap);
-COLOR_SPACE_EXPORT BASE_DECLARE_FEATURE(kHlgPqSdrRelative);
-
 class COLOR_SPACE_EXPORT ColorTransform {
  public:
   // Parameters that must be specified at creation time. Changing these
