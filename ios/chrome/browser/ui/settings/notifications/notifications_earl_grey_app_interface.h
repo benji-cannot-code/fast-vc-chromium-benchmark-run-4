c9efa38bc2073053daf30bce2fe587bf2bd3b83f
Matt Jones <mdjones@chromium.org>
2024-11-14 22:36:41 +0000

    [commerce] Fix iOS notification tests
    This patch fixes two tests that were missing the mock ShoppingService
    set up. These tests previously depended on more bespoke utils and
    field trial configs to accomplish the same thing. The tests are now
    set up deterministially.
    
    Bug: 378039633
    Change-Id: I4ffb0d4da37b03b2bb7bd90bd06b3b24a3b30224
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022063
    Reviewed-by: David Maunder <davidjm@chromium.org>
    Reviewed-by: Guillem Perez <guiperez@google.com>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383282}

---
+++ ios/chrome/browser/ui/settings/notifications/notifications_earl_grey_app_interface.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_UI_SETTINGS_NOTIFICATIONS_NOTIFICATIONS_EARL_GREY_APP_INTERFACE_H_
+#define IOS_CHROME_BROWSER_UI_SETTINGS_NOTIFICATIONS_NOTIFICATIONS_EARL_GREY_APP_INTERFACE_H_
+
+#import <UIKit/UIKit.h>
+
+// The app interface for notifications, facilitating test setup.
+@interface NotificationsEarlGreyAppInterface : NSObject
+
+// Replaces the shopping service with one where all features are enabled.
++ (void)setUpMockShoppingService;
+
+@end
+
+#endif  // IOS_CHROME_BROWSER_UI_SETTINGS_NOTIFICATIONS_NOTIFICATIONS_EARL_GREY_APP_INTERFACE_H_
--- ios/chrome/browser/ui/settings/notifications/tracking_price/tracking_price_egtest.mm
+++ ios/chrome/browser/ui/settings/notifications/tracking_price/tracking_price_egtest.mm
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #import "components/commerce/core/commerce_feature_list.h"
+#import "ios/chrome/browser/ui/settings/notifications/notifications_earl_grey_app_interface.h"
 #import "ios/chrome/grit/ios_strings.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey.h"
 #import "ios/chrome/test/earl_grey/chrome_earl_grey_ui.h"
@@ -37,8 +38,9 @@ using chrome_test_util::SettingsTrackingPriceTableView;
 }
 
 // Tests that the settings page is dismissed by swiping down from the top.
-// TODO(crbug.com/378039633): Reenable the test.
-- (void)DISABLED_testTrackingPriceSwipeDown_FromUpdatedSettingsView {
+- (void)testTrackingPriceSwipeDown_FromUpdatedSettingsView {
+  [NotificationsEarlGreyAppInterface setUpMockShoppingService];
+
   [self openTrackingPriceSettingsFromUpdatedSettingsView];
 
   // Check that Tracking Price TableView is presented.
