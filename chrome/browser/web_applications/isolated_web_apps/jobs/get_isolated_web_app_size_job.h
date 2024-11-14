20ae8ff1bfea842b5b956c51d6d7ccfaf14fa2c3
Simon Hangl <simonha@google.com>
2024-11-14 15:26:41 +0000

    Use web bundle size for computing app size of isolated web apps
    Bug: 371603351
    Change-Id: Id2f515295d2806cc52bbdff2481432b44ad58c4a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010874
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Simon Hangl <simonha@google.com>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382994}

--- chrome/browser/web_applications/isolated_web_apps/jobs/get_isolated_web_app_size_job.h
+++ chrome/browser/web_applications/isolated_web_apps/jobs/get_isolated_web_app_size_job.h
@@ -30,8 +30,8 @@ struct GetIsolatedWebAppSizeJobResult {
 
 class GetIsolatedWebAppSizeJob {
  public:
-  using ResultCallback =
-      base::OnceCallback<void(std::optional<GetIsolatedWebAppSizeJobResult>)>;
+  using ResultCallback = base::OnceCallback<void(
+      std::optional<GetIsolatedWebAppSizeJobResult> result)>;
 
   GetIsolatedWebAppSizeJob(Profile* profile,
                            const webapps::AppId& app_id,
@@ -43,7 +43,9 @@ class GetIsolatedWebAppSizeJob {
 
  private:
   void StoragePartitionSizeFetched(int64_t size);
-  void MaybeCompleteCommand();
+  void MaybeComputeBundleSize();
+  void OnBundleSizeComputed(std::optional<int64_t> bundle_size);
+  void CompleteJobWithError();
 
   const webapps::AppId app_id_;
   url::Origin iwa_origin_;
