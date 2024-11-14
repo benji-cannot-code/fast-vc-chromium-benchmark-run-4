09e1ccea1fdbf2433d2be3cc20c72053b5a2fc67
Alison Gale <agale@chromium.org>
2024-11-14 00:10:30 +0000

    Clean up toast flag
    Bug: 378700923
    Change-Id: I0b70935eaec74dd7f2e059ae1617b622d5b0b549
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6014763
    Auto-Submit: Alison Gale <agale@chromium.org>
    Reviewed-by: Charles Meng <charlesmeng@chromium.org>
    Commit-Queue: Charles Meng <charlesmeng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1382686}

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/task_manager/common/task_manager_features.h"
 #include "chrome/browser/tpcd/experiment/tpcd_experiment_features.h"
 #include "chrome/browser/ui/browser_navigator_params.h"
-#include "chrome/browser/ui/toasts/toast_features.h"
 #include "chrome/browser/ui/ui_features.h"
 #include "chrome/browser/unexpire_flags.h"
 #include "chrome/browser/unexpire_flags_gen.h"
@@ -880,31 +879,6 @@ const FeatureEntry::FeatureParam kCctSignInPromptAlways[] = {
 const FeatureEntry::FeatureVariation kCctSignInPromptVariations[] = {
     {"always show", kCctSignInPromptAlways, std::size(kCctSignInPromptAlways),
      nullptr}};
-#endif
-
-#if !BUILDFLAG(IS_ANDROID)
-const FeatureEntry::FeatureParam kToastWith6Seconds[] = {
-    {"toast_timeout", "6s"},
-    {"toast_without_action_timeout", "6s"},
-    {"toast_demo_mode", "true"}};
-const FeatureEntry::FeatureParam kToastWith8Seconds[] = {
-    {"toast_timeout", "8s"},
-    {"toast_without_action_timeout", "8s"},
-    {"toast_demo_mode", "true"}};
-const FeatureEntry::FeatureParam kToastWith10Seconds[] = {
-    {"toast_timeout", "10s"},
-    {"toast_without_action_timeout", "10s"},
-    {"toast_demo_mode", "true"}};
-const FeatureEntry::FeatureParam kToastWith12Seconds[] = {
-    {"toast_timeout", "12s"},
-    {"toast_without_action_timeout", "12s"},
-    {"toast_demo_mode", "true"}};
-
-const FeatureEntry::FeatureVariation kToastVariations[] = {
-    {"with 6s", kToastWith6Seconds, std::size(kToastWith6Seconds), nullptr},
-    {"with 8s", kToastWith8Seconds, std::size(kToastWith8Seconds), nullptr},
-    {"with 10s", kToastWith10Seconds, std::size(kToastWith10Seconds), nullptr},
-    {"with 12s", kToastWith12Seconds, std::size(kToastWith12Seconds), nullptr}};
 #endif
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
@@ -4539,14 +4513,6 @@ const FeatureEntry kFeatureEntries[] = {
     {"top-chrome-touch-ui", flag_descriptions::kTopChromeTouchUiName,
      flag_descriptions::kTopChromeTouchUiDescription, kOsDesktop,
      MULTI_VALUE_TYPE(kTopChromeTouchUiChoices)},
-
-#if !BUILDFLAG(IS_ANDROID)
-    {"top-chrome-toasts", flag_descriptions::kTopChromeToastsName,
-     flag_descriptions::kTopChromeToastsDescription, kOsDesktop,
-     FEATURE_WITH_PARAMS_VALUE_TYPE(toast_features::kToastFramework,
-                                    kToastVariations,
-                                    "ToastFramework")},
-#endif
 
 #if BUILDFLAG(ENABLE_WEBUI_TAB_STRIP)
     {flag_descriptions::kWebUITabStripFlagId,
