1868433356893d369a79ad259e93f48e4e5842c6
rajendrant <rajendrant@chromium.org>
2024-11-14 23:22:21 +0000

    Deflake ModelExecutionValidationBrowserTest.ModelExecutionSuccess
    From local repro, the test server request handler was not invoked, but the model execution request was sent. The RetryForHistogramUntilCountReached() seems to be getting stuck forever.
    
    This CL explicitly adds runloop wait for test server request to be retrieved.
    
    Bug: 378122992
    Change-Id: I2195634debc2bee20e5e58b2d832105ca3d052b6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023812
    Reviewed-by: Sophie Chang <sophiechang@chromium.org>
    Commit-Queue: Raj T <rajendrant@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383315}

--- chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
+++ chrome/browser/optimization_guide/model_execution/model_execution_validation_browsertest.cc
@@ -109,6 +109,7 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
     EXPECT_NE(request.headers.end(), request.headers.find("X-Client-Data"));
     EXPECT_TRUE(base::Contains(request.headers,
                                net::HttpRequestHeaders::kAuthorization));
+    std::move(model_execution_request_closure_).Run();
 
     if (should_server_fail_model_execution_) {
       response->set_code(net::HTTP_NOT_FOUND);
@@ -141,6 +142,8 @@ class ModelExecutionValidationBrowserTestBase : public InProcessBrowserTest {
 
   bool should_server_fail_model_execution_ = false;
 
+  base::OnceClosure model_execution_request_closure_;
+
   // Identity test support.
   std::unique_ptr<IdentityTestEnvironmentProfileAdaptor>
       identity_test_env_adaptor_;
@@ -165,7 +168,11 @@ class ModelExecutionValidationBrowserTest
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionSuccess) {
+  base::RunLoop run_loop;
+  model_execution_request_closure_ = run_loop.QuitClosure();
+
   EnableSignin();
+  run_loop.Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
@@ -185,8 +192,12 @@ IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
 #endif
 IN_PROC_BROWSER_TEST_F(ModelExecutionValidationBrowserTest,
                        MAYBE_ModelExecutionFailsServerFailure) {
+  base::RunLoop run_loop;
+  model_execution_request_closure_ = run_loop.QuitClosure();
+
   EnableServerModelExecutionFailure();
   EnableSignin();
+  run_loop.Run();
   RetryForHistogramUntilCountReached(
       &histogram_tester_, "OptimizationGuide.ModelExecution.Result.Test", 1);
 
