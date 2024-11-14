d0854947c1dae8cb84cdda65ee20f4add647afa4
Daniel Murphy <dmurph@chromium.org>
2024-11-14 17:51:08 +0000

    [PWA] Cleanups, populating window_app_id in tab_helper, crash debug.
    This change:
    - Does some cleanup of unnecessary spaghetti in WebAppInstallFinalizer.
    - Populates the app_id of app windows in the WebAppTabHelpers of their
      tabs.
    - Uses that information for a new crash key to help debug
      https://crbug.com/378755300
    - A small cleanup where we look up the app controlling a url, which
      can simply use the WebAppTabHelper.
    
    Bug: 378755300
    Change-Id: I2a8388aee3477cdec90a5cc1bfa73e3be929ba14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019257
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Daniel Murphy <dmurph@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383080}

--- chrome/browser/web_applications/commands/fetch_manifest_and_install_command.cc
+++ chrome/browser/web_applications/commands/fetch_manifest_and_install_command.cc
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/web_app_install_params.h"
 #include "chrome/browser/web_applications/web_app_install_utils.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
+#include "chrome/browser/web_applications/web_app_tab_helper.h"
 #include "chrome/browser/web_applications/web_app_utils.h"
 #include "chrome/browser/web_applications/web_contents/web_app_data_retriever.h"
 #include "chrome/browser/web_applications/web_contents/web_contents_manager.h"
@@ -689,8 +690,9 @@ void FetchManifestAndInstallCommand::OnInstallFinalizedMaybeReparentTab(
 
   bool is_shortcut_created = IsShortcutCreated(app_lock_->registrar(), app_id);
   DCHECK(app_lock_);
-  const bool can_reparent_tab = app_lock_->install_finalizer().CanReparentTab(
-      app_id, is_shortcut_created);
+  const bool can_reparent_tab =
+      app_lock_->ui_manager().CanReparentAppTabToWindow(app_id,
+                                                        is_shortcut_created);
 
   SCOPED_CRASH_KEY_NUMBER("PWA", "install_surface",
                           static_cast<int>(install_surface_));
@@ -698,12 +700,18 @@ void FetchManifestAndInstallCommand::OnInstallFinalizedMaybeReparentTab(
       "PWA", "install_url",
       web_contents_->GetLastCommittedURL().possibly_invalid_spec());
   SCOPED_CRASH_KEY_STRING64("PWA", "install_app_id", app_id);
+  WebAppTabHelper* tab_helper =
+      WebAppTabHelper::FromWebContents(web_contents_.get());
+  if (tab_helper) {
+    SCOPED_CRASH_KEY_STRING64("PWA", "source_window_app_id",
+                              tab_helper->window_app_id().value_or("<none>"));
+  }
 
   if (can_reparent_tab &&
       (web_app_info_->user_display_mode != mojom::UserDisplayMode::kBrowser) &&
       (install_surface_ != webapps::WebappInstallSource::DEVTOOLS)) {
-    app_lock_->install_finalizer().ReparentTab(app_id, is_shortcut_created,
-                                               web_contents_.get());
+    app_lock_->ui_manager().ReparentAppTabToWindow(web_contents_.get(), app_id,
+                                                   is_shortcut_created);
   }
 
   OnInstallCompleted(app_id, code);
