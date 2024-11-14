e039e872d90097a90f06bce69436304b05818510
Timofey Chudakov <tchudakov@google.com>
2024-11-14 15:38:15 +0000

    Show HaTS when an email suggestion is chosen over a plus address one.
    This CL adds the logic to trigger the HaTS survey after the user chooses
    an email suggestion from the Autofill popup which offers to fill a plus
    address as well.
    
    Bug: 348139343
    Change-Id: Ic221d78576dd1b81611dc21ed0776846cb2d7545
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011912
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Florian Jacky <fjacky@chromium.org>
    Reviewed-by: Norge Vizcay <vizcay@google.com>
    Commit-Queue: Timofey Chudakov <tchudakov@google.com>
    Reviewed-by: Rubin Deliallisi <rubindl@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383004}

--- components/plus_addresses/fake_plus_address_service.h
+++ components/plus_addresses/fake_plus_address_service.h
@@ -55,6 +55,7 @@ class FakePlusAddressService : public PlusAddressService {
       autofill::SuggestionType suggestion_type) override;
   void DidFillPlusAddress(bool did_show_email_suggestion,
                           bool is_manual_fallback) override;
+  void DidChooseEmailOverPlusAddress() override;
   void OnClickedRefreshInlineSuggestion(
       const url::Origin& last_committed_primary_main_frame_origin,
       base::span<const autofill::Suggestion> current_suggestions,
@@ -168,6 +169,10 @@ class FakePlusAddressService : public PlusAddressService {
     return did_fill_plus_address_suggestion_;
   }
 
+  bool was_email_chosen_over_plus_address() {
+    return was_email_chosen_over_plus_address_;
+  }
+
   std::optional<hats::SurveyType> get_triggered_survey_type() {
     return triggered_survey_;
   }
@@ -189,6 +194,7 @@ class FakePlusAddressService : public PlusAddressService {
   bool should_return_quota_error_ = false;
   bool should_return_timeout_error_ = false;
   bool did_fill_plus_address_suggestion_ = false;
+  bool was_email_chosen_over_plus_address_ = false;
 };
 
 }  // namespace plus_addresses
