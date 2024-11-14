7841f25fde4da3d5936b2d5c4a76214ca3bc3c0e
Gauthier Ambard <gambard@chromium.org>
2024-11-14 13:45:44 +0000

    Revert "[iOS] Add initial UI for Default Browser banner promo"
    This reverts commit f7e1e2a0b2ee1587623310d574184ac952854f51.
    
    Reason for revert: Broke the static initializer check
    
    Original change's description:
    > [iOS] Add initial UI for Default Browser banner promo
    >
    > This CL adds the initial iPhone UI for the banner promo. iPad UI will
    > be added in a separate CL. The model code controlling when the promo
    > will actually display will also be added later.
    >
    > Like the location bar itself, the promo has a fixed height (depending on
    > content size category) and does not expand beyond that.
    >
    > This CL also refactors/adds new methods to dynamic_type_util to make
    > calculating the correct font size easier.
    >
    > Bug: 374118934
    > Change-Id: I93a565988f4307b21f6bad8e6d684e21e27bc15c
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6006298
    > Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Auto-Submit: Robbie Gibson <rkgibson@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1382882}
    
    Bug: 374118934
    Change-Id: I25b34952496cc3918c35ae94360334f337a51245
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023119
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Auto-Submit: Gauthier Ambard <gambard@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1382922}

--- ios/chrome/browser/ui/toolbar/primary_toolbar_view.mm
+++ ios/chrome/browser/ui/toolbar/primary_toolbar_view.mm
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/browser/shared/ui/util/uikit_ui_util.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 #import "ios/chrome/browser/ui/omnibox/omnibox_ui_features.h"
-#import "ios/chrome/browser/ui/toolbar/banner_promo_view.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_button.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_button_factory.h"
 #import "ios/chrome/browser/ui/toolbar/buttons/toolbar_configuration.h"
@@ -27,11 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "ios/chrome/common/ui/util/constraints_ui_util.h"
 #import "ios/chrome/common/ui/util/ui_util.h"
 #import "ui/gfx/ios/uikit_util.h"
-
-namespace {
-// Extra vertical spacing when the banner promo is active.
-const CGFloat kBannerPromoVerticalSpacing = 8;
-}  // namespace
 
 @interface PrimaryToolbarView ()
 
@@ -107,13 +101,7 @@ const CGFloat kBannerPromoVerticalSpacing = 8;
 
 @end
 
-@implementation PrimaryToolbarView {
-  // Background and container for the banner promo.
-  UIView* _bannerPromoBackground;
-
-  // The actual banner promo view.
-  BannerPromoView* _bannerPromo;
-}
+@implementation PrimaryToolbarView
 
 @synthesize fakeOmniboxTarget = _fakeOmniboxTarget;
 @synthesize locationBarBottomConstraint = _locationBarBottomConstraint;
@@ -167,7 +155,6 @@ const CGFloat kBannerPromoVerticalSpacing = 8;
   [self setUpProgressBar];
   [self setUpCollapsedToolbarButton];
   [self setUpSeparator];
-  [self setUpBannerPromo];
 
   [self setUpConstraints];
 }
@@ -265,13 +252,6 @@ const CGFloat kBannerPromoVerticalSpacing = 8;
                   ? content_suggestions::FakeToolbarHeight()
                   : ToolbarExpandedHeight(
                         self.traitCollection.preferredContentSizeCategory);
-  }
-
-  if (IsDefaultBrowserBannerPromoEnabled()) {
-    height += _bannerPromo.intrinsicContentSize.height;
-    if (isTopOmnibox) {
-      height += kBannerPromoVerticalSpacing;
-    }
   }
 
   // If the tab group indicator is visible, add its height to the total height.
@@ -394,22 +374,6 @@ const CGFloat kBannerPromoVerticalSpacing = 8;
   [self addSubview:self.separator];
 }
 
-// Sets the banner promo up.
-- (void)setUpBannerPromo {
-  if (!IsDefaultBrowserBannerPromoEnabled()) {
-    return;
-  }
-
-  _bannerPromoBackground = [[UIView alloc] init];
-  _bannerPromoBackground.translatesAutoresizingMaskIntoConstraints = NO;
-  _bannerPromoBackground.backgroundColor = [UIColor colorNamed:kBlueHaloColor];
-  [self addSubview:_bannerPromoBackground];
-
-  _bannerPromo = [[BannerPromoView alloc] init];
-  _bannerPromo.translatesAutoresizingMaskIntoConstraints = NO;
-  [_bannerPromoBackground addSubview:_bannerPromo];
-}
-
 // Sets the constraints up.
 - (void)setUpConstraints {
   id<LayoutGuideProvider> safeArea = self.safeAreaLayoutGuide;
@@ -474,25 +438,6 @@ const CGFloat kBannerPromoVerticalSpacing = 8;
         constraintEqualToAnchor:safeArea.leadingAnchor
                        constant:kExpandedLocationBarHorizontalMargin]
   ]];
-
-  if (IsDefaultBrowserBannerPromoEnabled()) {
-    [NSLayoutConstraint activateConstraints:@[
-      [_bannerPromoBackground.leadingAnchor
-          constraintEqualToAnchor:self.leadingAnchor],
-      [_bannerPromoBackground.trailingAnchor
-          constraintEqualToAnchor:self.trailingAnchor],
-      [_bannerPromoBackground.topAnchor constraintEqualToAnchor:self.topAnchor],
-
-      [_bannerPromo.leadingAnchor
-          constraintEqualToAnchor:_bannerPromoBackground.leadingAnchor],
-      [_bannerPromo.trailingAnchor
-          constraintEqualToAnchor:_bannerPromoBackground.trailingAnchor],
-      [_bannerPromo.bottomAnchor
-          constraintEqualToAnchor:_bannerPromoBackground.bottomAnchor],
-      [_bannerPromo.topAnchor
-          constraintEqualToAnchor:self.safeAreaLayoutGuide.topAnchor],
-    ]];
-  }
 
   // Trailing StackView constraints.
   [NSLayoutConstraint activateConstraints:@[
