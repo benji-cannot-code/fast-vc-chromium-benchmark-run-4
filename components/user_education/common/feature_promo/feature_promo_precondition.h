5bc9863a50450153906435c4264cc644919ac4e4
Dana Fried <dfried@chromium.org>
2024-11-15 18:48:29 +0000

    [User Education] Expose cached data in precondition lists.
    This is required in order to be able to extract promos along with their
    data from a promo queue for User Education 2.5.
    
    Bug: 369887050
    Change-Id: I48d6ff2e78edc79708c7b38faed4ea5197097245
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022279
    Commit-Queue: Dana Fried <dfried@chromium.org>
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1383726}

--- components/user_education/common/feature_promo/feature_promo_precondition.h
+++ components/user_education/common/feature_promo/feature_promo_precondition.h
@@ -110,6 +110,8 @@ class FeaturePromoPreconditionBase : public FeaturePromoPrecondition {
   FRIEND_TEST_ALL_PREFIXES(FeaturePromoPreconditionTest, ExtractCachedData);
   FRIEND_TEST_ALL_PREFIXES(FeaturePromoPreconditionTest,
                            GetAfterExtractCachedDataFails);
+  FRIEND_TEST_ALL_PREFIXES(FeaturePromoPreconditionTest,
+                           FeaturePromoPreconditionList_ExtractCachedData);
 
   const Identifier identifier_;
   FeaturePromoResult::Failure failure_;
@@ -241,6 +243,9 @@ class FeaturePromoPreconditionList {
   // either the `failure()` and `identifier()` of the first that does not pass,
   // or `FeaturePromoResult::Success()` if all preconditions pass.
   CheckResult CheckPreconditions() const;
+
+  // Extracts cached data from all preconditions into `to_add_to`.
+  void ExtractCachedData(internal::PreconditionData::Collection& to_add_to);
 
  private:
   ListType preconditions_;
