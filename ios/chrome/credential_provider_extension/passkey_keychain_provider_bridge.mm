659e193ff1bfe4691e58c5069ad27ed8a3d7ee40
Noémie St-Onge <noemies@google.com>
2024-11-15 20:49:32 +0000

    [iOS] Guard against infinite `reauth <-> mark keys as stale` loop
    Although very unlikely, the current code makes it possible to fall in an
    infinite loop following this pattern:
    1. User is asked to reauthenticate.
    2. Invalid keys are received.
    3. Keys are marked as stale.
    4. User is asked to reauthenticate.
    [...]
    
    This is possible because the current call to `-fetchKeysForGaia` in the
    completion of `-markKeysAsStaleForGaia` doesn't account for the fact
    that reauthentication might already have happened, allowing it to happen
    again the next time `-onKeysFetchedForGaia` is called.
    
    Bug: 378846287
    Change-Id: I7a0938ec057be74e4a7b21d29f6186ed34485d75
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024433
    Commit-Queue: Noémie St-Onge <noemies@google.com>
    Reviewed-by: Alexis Hétu <sugoi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383798}

--- ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.mm
+++ ios/chrome/credential_provider_extension/passkey_keychain_provider_bridge.mm
@@ -96,6 +96,7 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
                 credential:credential
         canMarkKeysAsStale:YES
                    purpose:purpose
+         canReauthenticate:YES
                 completion:fetchSecurityDomainSecretCompletion
                      error:nil];
   }
@@ -144,6 +145,7 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
                 credential:credential
         canMarkKeysAsStale:YES
                    purpose:purpose
+         canReauthenticate:YES
                 completion:fetchSecurityDomainSecretCompletion
                      error:nil];
   } else {
@@ -153,6 +155,7 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
                       credential:credential
               canMarkKeysAsStale:YES
                          purpose:purpose
+               canReauthenticate:NO
                       completion:fetchSecurityDomainSecretCompletion
                            error:enroll_error];
     };
@@ -174,11 +177,15 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
 }
 
 // Attempts to fetch the keys for the account associated with the provided gaia
-// ID if no error occured at the previous stage.
+// ID if no error occured at the previous stage. `canReauthenticate` indicates
+// whether the user can be asked to reauthenticate by entering their GPM PIN.
+// This argument should only be set to `NO` if the user has already been asked
+// to reauthenticate.
 - (void)fetchKeysForGaia:(NSString*)gaia
               credential:(id<Credential>)credential
       canMarkKeysAsStale:(BOOL)canMarkKeysAsStale
                  purpose:(PasskeyKeychainProvider::ReauthenticatePurpose)purpose
+       canReauthenticate:(BOOL)canReauthenticate
               completion:(FetchSecurityDomainSecretCompletionBlock)
                              fetchSecurityDomainSecretCompletion
                    error:(NSError*)error {
@@ -197,7 +204,7 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
                                purpose:purpose
                             completion:fetchSecurityDomainSecretCompletion
                                keyList:key_list
-                     canReauthenticate:YES];
+                     canReauthenticate:canReauthenticate];
       };
   [self fetchKeysForGaia:gaia purpose:purpose completion:fetchKeysCompletion];
 }
@@ -230,15 +237,15 @@ bool ContainsValidKey(const PasskeyKeychainProvider::SharedKeyList keys,
     if (purpose == PasskeyKeychainProvider::ReauthenticatePurpose::kDecrypt &&
         canMarkKeysAsStale && credential &&
         !ContainsValidKey(keyList, credential)) {
-      // Mark keys as stale and try again. Note that "credential" is only used
-      // here, so we set "credential" to nil in the following call to avoid
-      // getting into an infinite loop.
+      // Mark keys as stale and try again. `canMarkKeysAsStale` is set to `NO`
+      // to avoid getting into an infinite loop.
       [self markKeysAsStaleForGaia:gaia
                         completion:^() {
                           [weakSelf fetchKeysForGaia:gaia
                                           credential:credential
                                   canMarkKeysAsStale:NO
                                              purpose:purpose
+                                   canReauthenticate:canReauthenticate
                                           completion:completion
                                                error:nil];
                         }];
