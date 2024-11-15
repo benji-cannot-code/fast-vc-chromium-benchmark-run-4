c9f0a8bb0baaf6ba53c234ad3e7b37e3afb051aa
conniekxu <conniekxu@chromium.org>
2024-11-15 18:49:40 +0000

    coral: Stay in overview mode
    The initial CL to stay in overview mode for coral group. The new desk
    is activated.
    
    For now, there're two issues I found for it:
    1. Desk preview view is not updated at the end for both of the new desk
    and the previous desk unless the mouse is moved to the desks bar. Bug
    filed for it.
    2. If there's a floating window in the group, after it's moved to the
    new desk, the overview item is not created for it.
    
    I filed bugs for both of them and added TODOs.
    
    Bug=b:375043063
    
    Change-Id: I5f0460006393b6a8e7d2ee9f5e4f2161f2ba7a7d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6013584
    Reviewed-by: Xiaodan Zhu <zxdan@chromium.org>
    Reviewed-by: Sammie Quon <sammiequon@chromium.org>
    Commit-Queue: Connie Xu <conniekxu@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383728}

--- ash/public/cpp/test/test_coral_delegate.h
+++ ash/public/cpp/test/test_coral_delegate.h
@@ -20,8 +20,8 @@ class TestCoralDelegate : public CoralDelegate {
 
   // CoralDelegate:
   void LaunchPostLoginGroup(coral::mojom::GroupPtr group) override;
-  void MoveTabsInGroupToNewDesk(
-      const std::vector<coral::mojom::Tab>& tabs) override;
+  void MoveTabsInGroupToNewDesk(const std::vector<coral::mojom::Tab>& tabs,
+                                size_t src_desk_index) override;
   void CreateSavedDeskFromGroup(coral::mojom::GroupPtr group) override;
 };
 
