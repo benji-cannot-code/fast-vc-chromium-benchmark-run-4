c41c2797d1de754aa4952e65b2865f568b10e03d
Marc Treib <treib@chromium.org>
2024-11-15 14:26:47 +0000

    [iOS] Migrate SigninAndHistorySync to accounts-on-device
    If AreSeparateProfilesForManagedAccountsEnabled() is true,
    SigninAndHistorySyncCoordinator now uses APIs from IdentityManager to
    query all accounts on the device, and listen for account changes.
    
    Bug: 377467350
    Change-Id: Ia31fcfb443bad5c4ce8a776b61e76dcfc870b08a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010735
    Commit-Queue: Marc Treib <treib@chromium.org>
    Reviewed-by: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383589}

--- ios/chrome/browser/ui/authentication/signin/signin_history_sync/signin_and_history_sync_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/signin_history_sync/signin_and_history_sync_coordinator.mm
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/strings/sys_string_conversions.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
 #import "ios/chrome/browser/shared/model/profile/profile_ios.h"
+#import "ios/chrome/browser/shared/public/features/features.h"
 #import "ios/chrome/browser/signin/model/authentication_service.h"
 #import "ios/chrome/browser/signin/model/authentication_service_factory.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
+#import "ios/chrome/browser/signin/model/identity_manager_factory.h"
 #import "ios/chrome/browser/ui/authentication/history_sync/history_sync_popup_coordinator.h"
 #import "ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.h"
 #import "ios/chrome/browser/ui/authentication/signin/consistency_promo_signin/consistency_promo_signin_coordinator.h"
@@ -211,10 +213,18 @@ enum class SignInHistorySyncStep {
 - (SignInHistorySyncStep)nextStep {
   switch (_currentStep) {
     case SignInHistorySyncStep::kStart: {
-      ChromeAccountManagerService* accountManagerService =
-          ChromeAccountManagerServiceFactory::GetForProfile(
-              self.browser->GetProfile());
-      if (accountManagerService->HasIdentities()) {
+      bool hasIdentitiesOnDevice = false;
+      if (AreSeparateProfilesForManagedAccountsEnabled()) {
+        signin::IdentityManager* identityManager =
+            IdentityManagerFactory::GetForProfile(self.browser->GetProfile());
+        hasIdentitiesOnDevice = !identityManager->GetAccountsOnDevice().empty();
+      } else {
+        ChromeAccountManagerService* accountManagerService =
+            ChromeAccountManagerServiceFactory::GetForProfile(
+                self.browser->GetProfile());
+        hasIdentitiesOnDevice = accountManagerService->HasIdentities();
+      }
+      if (hasIdentitiesOnDevice) {
         return SignInHistorySyncStep::kBottomSheetSignin;
       }
       return SignInHistorySyncStep::kInstantSignin;
