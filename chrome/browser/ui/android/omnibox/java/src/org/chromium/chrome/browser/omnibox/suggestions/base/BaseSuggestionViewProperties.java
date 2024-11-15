123d346820d11194e52aa08bbb7e63f29056a6d1
Tomasz Wiszkowski <ender@google.com>
2024-11-15 22:10:22 +0000

    Remove obsolete DENSITY property.
    This property is no longer used.
    
    Change-Id: If22d9613e8a7ed0a57561c3a4289d2e7bb2501f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026688
    Auto-Submit: Tomasz Wiszkowski <ender@google.com>
    Commit-Queue: Gang Wu <gangwu@chromium.org>
    Reviewed-by: Gang Wu <gangwu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383837}

--- chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProperties.java
+++ chrome/browser/ui/android/omnibox/java/src/org/chromium/chrome/browser/omnibox/suggestions/base/BaseSuggestionViewProperties.java
@@ -7,7 +7,6 @@ package org.chromium.chrome.browser.omnibox.suggestions.base;
 
 import android.content.Context;
 
-import androidx.annotation.IntDef;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
@@ -22,20 +21,10 @@ import org.chromium.ui.modelutil.PropertyModel.WritableBooleanPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableIntPropertyKey;
 import org.chromium.ui.modelutil.PropertyModel.WritableObjectPropertyKey;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.util.List;
 
 /** The base set of properties for most omnibox suggestions. */
 public @interface BaseSuggestionViewProperties {
-
-    /** Describes density of the suggestions. */
-    @IntDef({Density.DEFAULT, Density.COMPACT})
-    @Retention(RetentionPolicy.SOURCE)
-    public @interface Density {
-        int DEFAULT = 0;
-        int COMPACT = 1;
-    }
 
     /** Describes the content and behavior of the interactive Action Icon. */
     public static final class Action {
@@ -110,10 +99,6 @@ public @interface BaseSuggestionViewProperties {
     public static final WritableObjectPropertyKey<Runnable> ON_FOCUS_VIA_SELECTION =
             new WritableObjectPropertyKey<>();
 
-    /** Specifies how densely suggestions should be packed. */
-    @VisibleForTesting
-    public static final WritableIntPropertyKey DENSITY = new WritableIntPropertyKey();
-
     /** Callback invoked when user clicks the suggestion. */
     @VisibleForTesting
     public static final WritableObjectPropertyKey<Runnable> ON_CLICK =
@@ -147,7 +132,6 @@ public @interface BaseSuggestionViewProperties {
                 ICON,
                 ACTION_BUTTONS,
                 ON_FOCUS_VIA_SELECTION,
-                DENSITY,
                 ON_CLICK,
                 ON_LONG_CLICK,
                 ON_TOUCH_DOWN_EVENT,
