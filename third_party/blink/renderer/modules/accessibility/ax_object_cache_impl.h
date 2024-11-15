92e8c14cf9607f6c0f6b6443eb41de43d8f6645b
Aaron Leventhal <aleventhal@google.com>
2024-11-15 21:29:25 +0000

    [A11y] Update implicit selection rules to be spec-compliant
    Implicit selection is where currently focused descendant or active
    descendant is treated as selected, in terms of the state and
    selection events. It is supported only in single selection containers.
    
    Changes to the ARIA spec have affected when it is supported:
    * tree/listbox: only support if none of the treeitems/options have an
      explicit aria-selected/aria-checked
    * tabpanel: only support if none of the tabs have an explicit
      aria-selected/aria-expanded
    * grid/treegrid: not supported (same as before)
    
    Tree updates: If aria-multiselectable changes on the container, or
    aria-selected/aria-checked changes on an option/treeitem, or
    aria-expanded/aria-selected changes on a tab, then mark their container
    as not supporting implicit selection, and mark everything dirty where
    the implicit selected state may have changed. Note: this is tricky in
    the tab case because when focus is inside a tabpanel controlled by a
    tab, implicit selection is on the tab, which has a somewhat indirect
    relation with the focus in the tree.
    
    Fixed: 1143451
    Bug: https://github.com/nvaccess/nvda/issues/14352
    Change-Id: I117cb6210b1c35e976f3962cf17ea39ace7901e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021043
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Reviewed-by: Jocelyn Tran <jocelyntran@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383813}

--- third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
+++ third_party/blink/renderer/modules/accessibility/ax_object_cache_impl.h
@@ -180,10 +180,6 @@ class MODULES_EXPORT AXObjectCacheImpl : public AXObjectCacheBase {
   }
   bool IsFrozen() const override { return frozen_count_; }
 
-  //
-  // Iterators.
-  //
-
   void SelectionChanged(Node*) override;
 
   // Uses the relation cache to check whether the current element is pointed to
@@ -218,6 +214,11 @@ class MODULES_EXPORT AXObjectCacheImpl : public AXObjectCacheBase {
   void DiscardBadAriaHiddenBecauseOfFocus(AXObject& focus);
   // Mark an aria-hidden usage as bad/discarded when used on <body>/<html>/etc.
   void DiscardBadAriaHiddenBecauseOfElement(const AXObject& obj);
+
+  // Implicit selection aka "selection follows focus" is not allowed on
+  // containers with subwidgets that have had checked or selected, or expanded
+  // in the case of tabs.
+  bool IsImplicitSelectionAllowed(const AXObject* container);
 
   void ImageLoaded(const LayoutObject*) override;
 
@@ -710,6 +711,7 @@ class MODULES_EXPORT AXObjectCacheImpl : public AXObjectCacheBase {
     kEditableTextContentChanged,
     kFocusableChanged,
     kIdChanged,
+    kMaybeDisallowImplicitSelection,
     kNodeIsAttached,
     kNodeGainedFocus,
     kNodeLostFocus,
@@ -953,6 +955,11 @@ class MODULES_EXPORT AXObjectCacheImpl : public AXObjectCacheBase {
   // Helper that clears children up to the first included ancestor and returns
   // the ancestor if a children changed notification should be fired on it.
   AXObject* InvalidateChildren(AXObject* obj);
+
+  // Implicit selection aka "selection follows focus" is not allowed on
+  // containers with subwidgets that have had checked or selected, or expanded
+  // in the case of tabs.
+  void MaybeDisallowImplicitSelectionWithCleanLayout(AXObject* subwidget);
 
   // Helper method for `ComputeNodesOnLine()`. Given a `line_object` which is
   // the last LayoutObject of a line and that is a child of `block_flow`,
@@ -1254,6 +1261,9 @@ class MODULES_EXPORT AXObjectCacheImpl : public AXObjectCacheBase {
   Vector<ui::AXEvent> pending_events_to_serialize_;
 
   HashMap<DOMNodeId, bool> whitespace_ignored_map_;
+
+  // Any tree, tab or listbox that disallows implicit "selection from focus".
+  HashSet<AXID> containers_disallowing_implicit_selection_;
 
   // Make sure the next serialization sends everything.
   bool mark_all_dirty_ = false;
