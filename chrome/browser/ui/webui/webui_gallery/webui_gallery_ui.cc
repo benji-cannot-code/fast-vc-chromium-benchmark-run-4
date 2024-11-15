2c56f16b73da44685a42d43eb571ab4f8f48112b
Adem Derinel <derinel@google.com>
2024-11-15 07:26:04 +0000

    Enable unsafe buffer warnings in webui_gallery_ui.cc
    Bug: 40285824
    Change-Id: I408c5b6814c3a69e566dba1dab6e0a4e67dea619
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022108
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Adem Derinel <derinel@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383454}

--- chrome/browser/ui/webui/webui_gallery/webui_gallery_ui.cc
+++ chrome/browser/ui/webui/webui_gallery/webui_gallery_ui.cc
@@ -3,11 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifdef UNSAFE_BUFFERS_BUILD
-// TODO(crbug.com/40285824): Remove this and convert code to safer constructs.
-#pragma allow_unsafe_buffers
-#endif
-
 #include "chrome/browser/ui/webui/webui_gallery/webui_gallery_ui.h"
 
 #include "chrome/browser/profiles/profile.h"
@@ -35,7 +30,7 @@ void CreateAndAddWebuiGalleryUIHtmlSource(Profile* profile) {
 
   webui::SetupWebUIDataSource(
       source,
-      base::make_span(kWebuiGalleryResources, kWebuiGalleryResourcesSize),
+      base::span(kWebuiGalleryResources),
       IDR_WEBUI_GALLERY_WEBUI_GALLERY_HTML);
 
   source->OverrideContentSecurityPolicy(
@@ -51,8 +46,7 @@ void CreateAndAddWebuiGalleryUIHtmlSource(Profile* profile) {
 
   // Add shared SidePanel resources so that those elements can be demonstrated
   // as well.
-  source->AddResourcePaths(base::make_span(kSidePanelSharedResources,
-                                           kSidePanelSharedResourcesSize));
+  source->AddResourcePaths(base::span(kSidePanelSharedResources));
 
   content::URLDataSource::Add(
       profile, std::make_unique<FaviconSource>(
