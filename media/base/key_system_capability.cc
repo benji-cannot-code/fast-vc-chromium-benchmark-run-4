839b98628ea334f589cc999917789fb0a9dbb712
Sangbaek Park <sangbaekpark@chromium.org>
2024-11-15 23:17:20 +0000

    media: Clean-up on helper functions of media::KeySystemCapability
    This is a follow-up change from crrev.com/c/5624435.
    - Removed helper functions in `media::KeySystemCapability`
    - Moved the conversion logics into its StructTraits
    
    Bug: 40071197 , b/333947314 , b/340887578
    Change-Id: I9cadadba6a99e8b056a3b4ddb1ce7e19b7725330
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6025358
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Sangbaek Park <sangbaekpark@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383867}

--- media/base/key_system_capability.cc
+++ media/base/key_system_capability.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/key_system_capability.h"
 
-#include "base/types/expected.h"
 #include "media/base/cdm_capability.h"
 
 namespace media {
@@ -22,26 +21,6 @@ KeySystemCapability::KeySystemCapability(const KeySystemCapability& other) =
     default;
 
 KeySystemCapability::~KeySystemCapability() = default;
-
-CdmCapabilityOrStatus KeySystemCapability::ToCdmCapabilityOrStatus(
-    bool is_hw_secure) const {
-  if (is_hw_secure) {
-    return hw_cdm_capability_or_status;
-  }
-  return sw_cdm_capability_or_status;
-}
-
-std::optional<CdmCapabilityQueryStatus>
-KeySystemCapability::ToCdmCapabilityQueryStatus(bool is_hw_secure) const {
-  if (is_hw_secure) {
-    return hw_cdm_capability_or_status.has_value()
-               ? std::nullopt
-               : std::optional(hw_cdm_capability_or_status.error());
-  }
-  return sw_cdm_capability_or_status.has_value()
-             ? std::nullopt
-             : std::optional(sw_cdm_capability_or_status.error());
-}
 
 bool operator==(const KeySystemCapability& lhs,
                 const KeySystemCapability& rhs) {
