df49cdc4d7afcd9854457c64900e9f0882ac65ea
Neehit Goyal <neehit.goyal@samsung.com>
2024-11-15 09:35:01 +0000

    Remove unused #includes of variations_params_manager.h
    Bug: 374967542
    Change-Id: I7ad0b14473c415c4933f2dab5179b4ccf80be2df
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6012265
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Commit-Queue: Mikhail Khokhlov <khokhlov@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383494}

--- chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
+++ chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/prefs/pref_service.h"
 #include "components/tracing/common/background_tracing_state_manager.h"
 #include "components/tracing/common/pref_names.h"
-#include "components/variations/variations_params_manager.h"
 #include "content/public/browser/background_tracing_manager.h"
 #include "content/public/browser/browser_task_traits.h"
 #include "content/public/browser/browser_thread.h"
