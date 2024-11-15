e3dbed6fe0cb135617da2c69dd1fd91a7fbcd3ed
thefrog <thefrog@chromium.org>
2024-11-15 18:38:57 +0000

    Fix various bugs for the min time for referrer chains in URT lookups
    Changes in this CL:
    1. Previously if URL lookups were disabled on startup, the min timestamp
       would not be set the moment the pref changes made the lookups be
       enabled. That's because the lookup service was not created until it
       was needed for checking a specific URL navigation.
    
       This CL moves the pref tracking to SafeBrowsingServiceImpl, which
       allows tracking at profile startup.
    2. Previously any time a pref changed and the service was enabled, the
       min timestamp allowed for referrer chains would get reset to the
       current time. This is overly restrictive, because the pref changing
       might not have impacted whether or not the service was enabled.
    
       This CL resets the min timestamp only when the prefs change such
       that the disabled service becomes enabled.
    3. CanPerformFullURLLookup() depends on pref kVariationsCountry.
       Previously, changes to this pref were not tracked and thus would not
       trigger changes to the minimum allowed timestamp for referrer chains.
       This caused incorrect behavior if the user's pref ESB/MBB selections
       changed to permit the lookup while the kVariationsCountry was
       ineligible, since the timestamp would not be set at that point.
    
       This CL changes the call into CanPerformFullURLLookup() to instead be
       a call into HasPrefPermissionsToPerformFullURLLookup(). The new
       function only checks ESB and MBB. There is no reason to update the
       timestamp based on kVariationsCountry, because that's not related to
       user permissions for sending referrer chains.
    
    Fixed: 378666653, 378453256, 377641155
    Change-Id: Ibbabbb509ef2f922c64cb0f10fd3f37ee0176dc5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020795
    Commit-Queue: thefrog <thefrog@chromium.org>
    Reviewed-by: Ali Juma <ajuma@chromium.org>
    Reviewed-by: Xinghui Lu <xinghuilu@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383719}

--- chrome/browser/safe_browsing/url_lookup_service_factory.cc
+++ chrome/browser/safe_browsing/url_lookup_service_factory.cc
@@ -92,6 +92,9 @@ RealTimeUrlLookupServiceFactory::BuildServiceInstanceForBrowserContext(
       profile->IsOffTheRecord(),
       base::BindRepeating(
           &RealTimeUrlLookupServiceFactory::GetVariationsService),
+      base::BindRepeating(&RealTimeUrlLookupServiceFactory::
+                              GetMinAllowedTimestampForReferrerChains,
+                          profile),
       SafeBrowsingNavigationObserverManagerFactory::GetForBrowserContext(
           profile),
       WebUIInfoSingleton::GetInstance());
@@ -115,6 +118,14 @@ RealTimeUrlLookupServiceFactory::GetURLLoaderFactory(
 variations::VariationsService*
 RealTimeUrlLookupServiceFactory::GetVariationsService() {
   return g_browser_process->variations_service();
+}
+
+// static
+base::Time
+RealTimeUrlLookupServiceFactory::GetMinAllowedTimestampForReferrerChains(
+    Profile* profile) {
+  return g_browser_process->safe_browsing_service()
+      ->GetMinAllowedTimestampForReferrerChains(profile);
 }
 
 void RealTimeUrlLookupServiceFactory::SetURLLoaderFactoryForTesting(
--- components/safe_browsing/core/browser/realtime/policy_engine.cc
+++ components/safe_browsing/core/browser/realtime/policy_engine.cc
@@ -65,8 +65,9 @@ bool RealTimePolicyEngine::CanPerformFullURLLookup(
     PrefService* pref_service,
     bool is_off_the_record,
     variations::VariationsService* variations_service) {
-  if (is_off_the_record)
+  if (is_off_the_record) {
     return false;
+  }
 
   // |variations_service| can be nullptr during shutdown and in tests.
   if (variations_service &&
@@ -74,7 +75,7 @@ bool RealTimePolicyEngine::CanPerformFullURLLookup(
     return false;
   }
 
-  return IsUserEpOptedIn(pref_service) || IsUserMbbOptedIn(pref_service);
+  return HasPrefPermissionsToPerformFullURLLookup(pref_service);
 }
 
 // static
@@ -89,6 +90,12 @@ bool RealTimePolicyEngine::CanPerformFullURLLookupWithToken(
   }
 
   return std::move(client_callback).Run(IsUserEpOptedIn(pref_service));
+}
+
+// static
+bool RealTimePolicyEngine::HasPrefPermissionsToPerformFullURLLookup(
+    PrefService* pref_service) {
+  return IsUserEpOptedIn(pref_service) || IsUserMbbOptedIn(pref_service);
 }
 
 // static
