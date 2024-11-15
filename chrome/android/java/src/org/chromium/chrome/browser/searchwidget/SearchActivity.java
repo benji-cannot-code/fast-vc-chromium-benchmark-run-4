b1152d34dd5f0794844f5b6cdc6eaec3ee7c05cd
Brandon Fong <bjfong@google.com>
2024-11-15 23:40:31 +0000

    [hub] Add metrics for hub search usage V2
    Add metrics for hub search second attempt.
    Comments addressed from: https://chromium-review.googlesource.com/c/chromium/src/+/6019935
    
    Bug: 378723318
    Change-Id: I55f4eb15ae4289fd0a200c8217ae0ee7508361f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6019897
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Commit-Queue: Brandon Fong <bjfong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383873}

--- chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
+++ chrome/android/java/src/org/chromium/chrome/browser/searchwidget/SearchActivity.java
@@ -161,6 +161,7 @@ public class SearchActivity extends AsyncInitializationActivity
 
     // NOTE: This is used to capture HISTOGRAM_SESSION_TERMINATION_REASON.
     // Do not shuffle or reassign values.
+    // LINT.IfChange(TerminationReason)
     @IntDef({
         TerminationReason.NAVIGATION,
         TerminationReason.UNSPECIFIED,
@@ -169,6 +170,7 @@ public class SearchActivity extends AsyncInitializationActivity
         TerminationReason.OMNIBOX_FOCUS_LOST,
         TerminationReason.ACTIVITY_FOCUS_LOST,
         TerminationReason.FRE_NOT_COMPLETED,
+        TerminationReason.CUSTOM_BACK_ARROW,
         TerminationReason.COUNT
     })
     @Retention(RetentionPolicy.SOURCE)
@@ -180,8 +182,11 @@ public class SearchActivity extends AsyncInitializationActivity
         int OMNIBOX_FOCUS_LOST = 4;
         int ACTIVITY_FOCUS_LOST = 5;
         int FRE_NOT_COMPLETED = 6;
-        int COUNT = 7;
+        int CUSTOM_BACK_ARROW = 7;
+        int COUNT = 8;
     }
+
+    // LINT.ThenChange(/tools/metrics/histograms/metadata/android/enums.xml:SearchActivityTerminationReason)
 
     @VisibleForTesting /* package */ static final String CCT_CLIENT_PACKAGE_PREFIX = "app-cct-";
 
@@ -689,10 +694,9 @@ public class SearchActivity extends AsyncInitializationActivity
         mLocationBarCoordinator
                 .getStatusCoordinator()
                 .setOnStatusIconNavigateBackButtonPress(
-                        (View v) ->
-                                finish(
-                                        TerminationReason.BACK_KEY_PRESSED,
-                                        /* loadUrlParams= */ null));
+                        (View v) -> {
+                            finish(TerminationReason.CUSTOM_BACK_ARROW, /* loadUrlParams= */ null);
+                        });
     }
 
     private void setIncognitoColorScheme() {
