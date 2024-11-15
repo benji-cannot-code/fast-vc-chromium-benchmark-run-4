e5ca5ff80b7f954e59dd5f374386874e15de1f7d
Marke Hallowell <wry@chromium.org>
2024-11-15 22:35:18 +0000

    Add Keyed Service and Keyed Service Factory for glic
    Bug: 377935152
    Change-Id: I5a7a00b3b67f37191bc530e5fc5379a4f25e6877
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021462
    Reviewed-by: Carlos Knippschild <carlosk@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Marke Hallowell <wry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383846}

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -409,6 +409,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/desktop_android/desktop_android_extension_system.h"
 #endif
 
+#if BUILDFLAG(ENABLE_GLIC)
+#include "chrome/browser/glic/glic_keyed_service_factory.h"
+#endif  // BUILDFLAG(ENABLE_GLIC)
+
 #if BUILDFLAG(ENABLE_SESSION_SERVICE)
 #include "chrome/browser/sessions/app_session_service_factory.h"
 #include "chrome/browser/sessions/exit_type_service_factory.h"
@@ -907,6 +911,13 @@ void ChromeBrowserMainExtraPartsProfiles::
 #endif
 #if !BUILDFLAG(IS_ANDROID)
   GalleryWatchManager::EnsureFactoryBuilt();
+#endif
+#if BUILDFLAG(ENABLE_GLIC)
+  if (base::FeatureList::IsEnabled(features::kGlic)) {
+    GlicKeyedServiceFactory::GetInstance();
+  }
+#endif  // BUILDFLAG(ENABLE_GLIC)
+#if !BUILDFLAG(IS_ANDROID)
   GlobalErrorServiceFactory::GetInstance();
 #endif
   GoogleGroupsManagerFactory::GetInstance();
