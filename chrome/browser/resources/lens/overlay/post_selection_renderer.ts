9805ed670c34987b3a3c685bc445265a0bba9261
Juan Mojica <juanmojica@google.com>
2024-11-15 21:50:59 +0000

    [lensoverlay] Shimmer should not come back after it disappears.
    This is accomplished by never unfocusing the cursor from the shimmer.
    See bug for demo.
    
    Change-Id: If9a77ca11fa885e7afcfd934f711547f517ae6fa
    Bug: b:369322932, 379338047
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026632
    Reviewed-by: Duncan Mercer <mercerd@google.com>
    Commit-Queue: Juan Mojica <juanmojica@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383826}

--- chrome/browser/resources/lens/overlay/post_selection_renderer.ts
+++ chrome/browser/resources/lens/overlay/post_selection_renderer.ts
@@ -160,7 +160,7 @@ export class PostSelectionRendererElement extends PolymerElement {
       this.browserProxy.callbackRouter.clearAllSelections.addListener(
           this.clearSelection.bind(this)),
       this.browserProxy.callbackRouter.clearRegionSelection.addListener(
-          this.clearSelection.bind(this)),
+          this.clearRegionSelection.bind(this)),
       this.browserProxy.callbackRouter.setPostRegionSelection.addListener(
           this.setSelection.bind(this)),
     ];
@@ -181,6 +181,12 @@ export class PostSelectionRendererElement extends PolymerElement {
     this.canvasHeight = height;
     this.canvasPhysicalWidth = width * window.devicePixelRatio;
     this.canvasPhysicalHeight = height * window.devicePixelRatio;
+  }
+
+  clearRegionSelection() {
+    unfocusShimmer(this, ShimmerControlRequester.CURSOR);
+    unfocusShimmer(this, ShimmerControlRequester.MANUAL_REGION);
+    this.clearSelection();
   }
 
   clearSelection() {
