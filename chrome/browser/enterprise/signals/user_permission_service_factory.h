95b7c0a250bfa1710ff5ea94142405330aaae2f6
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-15 16:06:38 +0000

    Implements the non-deprecated builder.
    Any class inheriting `BrowserContextKeyedServiceFactory` should
    implement either `BuildServiceInstanceForBrowserContext` or
    `BuildServiceInstanceFor`. The second one being deprecated,
    this CL replaces it by the first one.
    
    Bug: 40249337
    Change-Id: I9b6793758d983d495b8bdea79bb141f5468ea9bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4798505
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Hamda Mare <hmare@google.com>
    Reviewed-by: Hamda Mare <hmare@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383632}

--- chrome/browser/enterprise/signals/user_permission_service_factory.h
+++ chrome/browser/enterprise/signals/user_permission_service_factory.h
@@ -34,7 +34,7 @@ class UserPermissionServiceFactory : public ProfileKeyedServiceFactory {
   ~UserPermissionServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory implementation:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
