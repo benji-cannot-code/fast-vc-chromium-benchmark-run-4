6d8ac4157f8edd1a5fedbc48f2aebef2ed0a0f0a
Chase Phillips <cmp@chromium.org>
2024-11-15 23:55:21 +0000

    controlledframe: Update Context Menus to require promises API form
    Bug: 40191772, 327431749
    Change-Id: I13b9b15f4c9f06947d74f85e2f6c06baa95436ed
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004027
    Commit-Queue: Chase Phillips <cmp@chromium.org>
    Reviewed-by: Kevin McNee <mcnee@chromium.org>
    Auto-Submit: Chase Phillips <cmp@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383881}

--- chrome/browser/controlled_frame/controlled_frame_apitest.cc
+++ chrome/browser/controlled_frame/controlled_frame_apitest.cc
@@ -102,7 +102,7 @@ const content::EvalJsResult UpdateContextMenuItemTitle(
     const std::string& id,
     const std::string& new_title) {
   return content::EvalJs(app_frame, content::JsReplace(R"(
-      new Promise((resolve, reject) => {
+      new Promise(async (resolve, reject) => {
         const frame = document.getElementsByTagName('controlledframe')[0];
         if (!frame || !frame.contextMenus || !frame.contextMenus.update) {
           reject('FAIL: frame, frame.contextMenus, or ' +
@@ -110,10 +110,8 @@ const content::EvalJsResult UpdateContextMenuItemTitle(
           return;
         }
 
-        frame.contextMenus.update(
-            /*id=*/$1,
-            { title: $2 },
-            () => { resolve('SUCCESS'); });
+        await frame.contextMenus.update(/*id=*/$1, { title: $2 });
+        resolve('SUCCESS');
       });
   )",
                                                        id, new_title));
@@ -123,7 +121,7 @@ const content::EvalJsResult RemoveContextMenuItem(
     content::RenderFrameHost* app_frame,
     const std::string& id) {
   return content::EvalJs(app_frame, content::JsReplace(R"(
-      new Promise((resolve, reject) => {
+      new Promise(async (resolve, reject) => {
         const frame = document.getElementsByTagName('controlledframe')[0];
         if (!frame || !frame.contextMenus || !frame.contextMenus.remove) {
           reject('FAIL: frame, frame.contextMenus, or ' +
@@ -131,9 +129,8 @@ const content::EvalJsResult RemoveContextMenuItem(
           return;
         }
 
-        frame.contextMenus.remove(
-            /*id=*/$1,
-            () => { resolve('SUCCESS'); });
+        await frame.contextMenus.remove(/*id=*/$1);
+        resolve('SUCCESS');
       });
   )",
                                                        id));
@@ -142,7 +139,7 @@ const content::EvalJsResult RemoveContextMenuItem(
 const content::EvalJsResult RemoveAllContextMenuItems(
     content::RenderFrameHost* app_frame) {
   return content::EvalJs(app_frame, R"(
-      new Promise((resolve, reject) => {
+      new Promise(async (resolve, reject) => {
         const frame = document.getElementsByTagName('controlledframe')[0];
         if (!frame || !frame.contextMenus || !frame.contextMenus.removeAll) {
           reject('FAIL: frame, frame.contextMenus, or ' +
@@ -150,7 +147,8 @@ const content::EvalJsResult RemoveAllContextMenuItems(
           return;
         }
 
-        frame.contextMenus.removeAll(() => { resolve('SUCCESS'); });
+        await frame.contextMenus.removeAll();
+        resolve('SUCCESS');
       });
   )");
 }
