f455c9f9cb80b0e92184d9079cd4711a7c4bb0a1
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-15 08:28:02 +0000

    `BuildServiceInstanceFor` is deprecated. This CL ensures that the new method `BuildServiceInstanceForBrowserContext` is used instead.
    In particular it ensures that services are created with a unique_ptr.
    
    This CL was uploaded by git cl split.
    
    R=greengrape@google.com
    
    Bug: 40249337
    Change-Id: I48b4cc30ca1eee4b02d63b24df9fa150c5b09062
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023575
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Andrew Rayskiy <greengrape@google.com>
    Commit-Queue: Andrew Rayskiy <greengrape@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383474}

--- chrome/browser/chromeos/extensions/vpn_provider/vpn_service_factory.h
+++ chrome/browser/chromeos/extensions/vpn_provider/vpn_service_factory.h
@@ -43,7 +43,7 @@ class VpnServiceFactory : public ProfileKeyedServiceFactory {
   // BrowserContextKeyedServiceFactory:
   bool ServiceIsCreatedWithBrowserContext() const override;
   bool ServiceIsNULLWhileTesting() const override;
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
