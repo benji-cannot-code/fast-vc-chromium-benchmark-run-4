b8d4b30c1b2342265f9e6695ac4f244f259a9fe7
Jérôme Lebel <jlebel@chromium.org>
2024-11-15 16:46:23 +0000

    [iOS] Fix FRE when force incognito
    This patch fixes the FRE when the enterprise policy forces incognito
    mode, and the user adds an identity.
    
    This patch moves regular EGTests testIncognitoForcedByPolicy to
    first_run_search_engine_choice_egtest.mm so the search engine screen
    is also tested with the enterprise policy.
    
    And this patch adds an EGTest for the add identity in the FRE
    with the enterprise policy.
    
    This fix should be cherry-pick in M131, with crrev.com/c/6012687.
    
    Fixed: 378476916
    Change-Id: Iec4542b8b5d4e3b569fdc21762a1e9a0331fef14
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023129
    Commit-Queue: Vincent Boisselle <vincb@google.com>
    Auto-Submit: Jérôme Lebel <jlebel@chromium.org>
    Reviewed-by: Vincent Boisselle <vincb@google.com>
    Commit-Queue: Jérôme Lebel <jlebel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383661}

--- ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_coordinator.mm
+++ ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_coordinator.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/metrics/user_metrics_action.h"
 #import "base/notreached.h"
 #import "ios/chrome/browser/shared/model/browser/browser.h"
+#import "ios/chrome/browser/shared/model/profile/profile_ios.h"
 #import "ios/chrome/browser/signin/model/chrome_account_manager_service_factory.h"
 #import "ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_coordinator_delegate.h"
 #import "ios/chrome/browser/ui/authentication/identity_chooser/identity_chooser_mediator.h"
@@ -78,10 +79,11 @@ typedef NS_ENUM(NSInteger, IdentityChooserCoordinatorState) {
       self.transitionController;
 
   // Creates the mediator.
+  ChromeAccountManagerService* accountManagerService =
+      ChromeAccountManagerServiceFactory::GetForProfile(
+          self.browser->GetProfile()->GetOriginalProfile());
   self.identityChooserMediator = [[IdentityChooserMediator alloc]
-      initWithAccountManagerService:ChromeAccountManagerServiceFactory::
-                                        GetForProfile(
-                                            self.browser->GetProfile())];
+      initWithAccountManagerService:accountManagerService];
 
   self.identityChooserMediator.consumer = self.identityChooserViewController;
   // Setups.
--- ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
+++ ios/chrome/browser/ui/authentication/signin/add_account_signin/add_account_signin_coordinator.mm
@@ -109,7 +109,7 @@ using signin_metrics::PromoAction;
 
 - (void)start {
   [super start];
-  ProfileIOS* profile = self.browser->GetProfile();
+  ProfileIOS* profile = self.browser->GetProfile()->GetOriginalProfile();
   _accountManagerService =
       ChromeAccountManagerServiceFactory::GetForProfile(profile);
   _identityManager = IdentityManagerFactory::GetForProfile(profile);
@@ -325,7 +325,8 @@ using signin_metrics::PromoAction;
   self.historySyncPopupCoordinator = nil;
 
   AuthenticationService* authService =
-      AuthenticationServiceFactory::GetForProfile(self.browser->GetProfile());
+      AuthenticationServiceFactory::GetForProfile(
+          self.browser->GetProfile()->GetOriginalProfile());
   // Even if `result` is not "success" for the history opt-in step, the sign-in
   // step did succeed, so pass SigninCoordinatorResultSuccess.
   [self addAccountDoneWithSigninResult:SigninCoordinatorResultSuccess
