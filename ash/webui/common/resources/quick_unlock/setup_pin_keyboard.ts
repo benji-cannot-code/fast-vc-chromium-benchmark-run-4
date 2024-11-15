41b17cd0e3925d756e91252b765ae778bbe6cf00
Renato Silva <rrsilva@google.com>
2024-11-15 14:12:28 +0000

    [High5] Add visibility toggle for PIN setup
    Add a visibility button for showing and hiding the user's PIN into
    the keyboard element used by OOBE and Settings.
    
    https://screenshot.googleplex.com/3h6i4hhu4FVabTF
    https://screenshot.googleplex.com/7Bd4ubPFdmGv6BU
    
    Bug: 365059362, 348326316
    Change-Id: I15cc824cdf4e3f411e0dca21699ab4d28cbb6eb7
    Fixed: 360178965
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6010752
    Commit-Queue: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Hardik Goyal <hardikgoyal@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383580}

--- ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
+++ ash/webui/common/resources/quick_unlock/setup_pin_keyboard.ts
@@ -160,6 +160,14 @@ export class SetupPinKeyboardElement extends SetupPinKeyboardElementBase {
         value: false,
       },
 
+      /**
+       * Enables the visibility icon for showing/hiding the PIN
+       */
+      enableVisibilityIcon: {
+        type: Boolean,
+        value: false,
+      },
+
       isSetPinCallPending_: {
         notify: true,
         type: Boolean,
@@ -182,6 +190,7 @@ export class SetupPinKeyboardElement extends SetupPinKeyboardElementBase {
   useRecoveryModeApi: boolean;
   quickUnlockPrivate: typeof chrome.quickUnlockPrivate;
   enablePlaceholder: boolean;
+  enableVisibilityIcon: boolean;
 
   override focus(): void {
     this.$.pinKeyboard.focusInput();
@@ -354,6 +363,7 @@ export class SetupPinKeyboardElement extends SetupPinKeyboardElementBase {
       this.initialPin_ = this.pinKeyboardValue_;
       this.pinKeyboardValue_ = '';
       this.isConfirmStep = true;
+      this.$.pinKeyboard.resetPinVisibility();
       this.onPinChange_(new CustomEvent(
           'pin-change', {detail: {pin: this.pinKeyboardValue_}}));
       this.$.pinKeyboard.focusInput();
