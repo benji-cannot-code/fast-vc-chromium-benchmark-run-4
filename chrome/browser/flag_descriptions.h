927d3743140df23ca08cee5ef5ff865642cb8fdb
Alexander Cooper <alcooper@chromium.org>
2024-11-15 18:21:20 +0000

    Add runtime flag to use smooth depth data
    The OpenXR Depth extension provides both a raw and a smooth depth image.
    This change adds a flag to toggle which one is exposed to the page to
    enable investigation of potenital use cases for either and to help
    evaluate if there's a need for a change to the spec.
    
    Bug: 379076349
    Change-Id: I09f3745fe961d82c85a5264fadc95cc246d876cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022269
    Reviewed-by: Piotr Bialecki <bialpio@chromium.org>
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383702}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2881,6 +2881,9 @@ extern const char kOpenXRExtendedFeaturesDescription[];
 
 extern const char kOpenXRName[];
 extern const char kOpenXRDescription[];
+
+extern const char kOpenXRAndroidSmoothDepthName[];
+extern const char kOpenXRAndroidSmoothDepthDescription[];
 #endif
 
 // Non-Android ----------------------------------------------------------------
