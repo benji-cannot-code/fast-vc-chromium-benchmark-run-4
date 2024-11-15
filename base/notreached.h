01b86680c36ec46f504422c51af787c6716cf566
Peter Boström <pbos@chromium.org>
2024-11-15 00:46:51 +0000

    Remove NOTREACHED_IN_MIGRATION()
    Bug: 40580068
    Change-Id: I203fa8a9f02ca9ee06a8e7c976657aee21bfe7ca
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021996
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Commit-Queue: Peter Boström <pbos@chromium.org>
    Commit-Queue: Lei Zhang <thestig@chromium.org>
    Owners-Override: Lei Zhang <thestig@chromium.org>
    Auto-Submit: Peter Boström <pbos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383355}

--- base/notreached.h
+++ base/notreached.h
@@ -17,20 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace logging {
 
-// Migration in progress: For new code use NOTREACHED() or
-// NOTREACHED(base::NotFatalUntil::M*). NOTREACHED_IN_MIGRATION() is equally
-// fatal to NOTREACHED() without parameters but not annotated as [[noreturn]].
-#if CHECK_WILL_STREAM()
-#define NOTREACHED_IN_MIGRATION() \
-  LOGGING_CHECK_FUNCTION_IMPL(::logging::NotReachedError::NotReached(), false)
-#else
-#define NOTREACHED_IN_MIGRATION()                          \
-  (true) ? ::logging::NotReachedError::TriggerNotReached() \
-         : EAT_CHECK_STREAM_PARAMS()
-#endif
-
-// Migration in progress: Use NOTREACHED() directly without parameters instead.
-// TODO(crbug.com/40580068): Merge this with NOTREACHED().
 #if CHECK_WILL_STREAM()
 #define NOTREACHED_INTERNAL_IMPL() ::logging::NotReachedNoreturnError()
 #else
