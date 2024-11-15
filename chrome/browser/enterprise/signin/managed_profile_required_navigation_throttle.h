55ca2f34a42b143d40346538b75c48013cd8d438
Yann Dago <ydago@chromium.org>
2024-11-15 14:19:04 +0000

    [Managed profile creation] Close interstitial after profile creation
    Closes interstitial when user creates a managed profile, leave
    interstitial open if user cancels profile creation.
    
    Bug: 372187106
    Change-Id: I07059043713d814ced90190610f40bd81e831cbd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023395
    Reviewed-by: David Roger <droger@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1383583}

--- chrome/browser/enterprise/signin/managed_profile_required_navigation_throttle.h
+++ chrome/browser/enterprise/signin/managed_profile_required_navigation_throttle.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_ENTERPRISE_SIGNIN_MANAGED_PROFILE_REQUIRED_NAVIGATION_THROTTLE_H_
 
 #include <memory>
+#include <vector>
 
 #include "base/containers/flat_map.h"
 #include "base/functional/callback_helpers.h"
@@ -59,7 +60,15 @@ class ManagedProfileRequiredNavigationThrottle
   [[nodiscard]] static base::ScopedClosureRunner
   BlockNavigationUntilEnterpriseActionTaken(
       content::BrowserContext* browser_context,
+      content::WebContents* enterprise_action_web_contents,
       content::WebContents* allowed_web_contents = nullptr);
+
+  static void ShowBlockedWindow(content::BrowserContext* browser_context);
+  static void SetReloadRequired(
+      content::BrowserContext* browser_context,
+      bool success,
+      base::OnceCallback<void(content::NavigationHandle&)> on_reload_triggered =
+          base::OnceCallback<void(content::NavigationHandle&)>());
 
  private:
   ThrottleCheckResult ProcessThrottleEvent();
