e2b0ee42ad92cd0e44ad0267d00fa3f0ac8af38a
Zijie He <zijiehe@google.com>
2024-11-15 21:03:25 +0000

    Revert "[Media Remoting] Remove RemotePlaybackBackend feature and flag."
    This reverts commit 3b4babc23c835039d112ed1c69601baddd23ff5a.
    
    Reason for revert: After the change, web-engine started showing the
    cast button on the video stream. See an example at,
    https://chrome-gold.skia.org/detail?grouping=name%3DCastStreaming_VP8_1Frame%26source_type%3Dchrome-gpu&digest=add51219264a51163f45119e1921b42e&changelist_id=6024003&crs=gerrit, or the breakage https://ci.chromium.org/ui/p/chrome/builders/ci/fuchsia-cast-sherlock/10380/overview
    
    Original change's description:
    > [Media Remoting] Remove RemotePlaybackBackend feature and flag.
    >
    > This feature shipped to stable in M121.
    >
    > Fixed: 361121891
    > Change-Id: If46024acf34a31232fff28727baf8e12e06b94fb
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015927
    > Reviewed-by: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Muyao Xu <muyaoxu@google.com>
    > Commit-Queue: Mark Foltz <mfoltz@chromium.org>
    > Cr-Commit-Position: refs/heads/main@{#1383049}
    
    Cq-Include-Trybots: luci.chrome.try:fuchsia-cast-sherlock,fuchsia-fyi-sherlock
    Change-Id: I77f484385cd72dad57faa330a49ed7968fe8b218
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021512
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Mark Foltz <mfoltz@chromium.org>
    Commit-Queue: Zijie He <zijiehe@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383802}

--- third_party/blink/renderer/modules/remoteplayback/remote_playback.cc
+++ third_party/blink/renderer/modules/remoteplayback/remote_playback.cc
@@ -243,6 +243,13 @@ ScriptPromise<IDLUndefined> RemotePlayback::prompt(
     return EmptyPromise();
   }
 
+  if (!RuntimeEnabledFeatures::RemotePlaybackBackendEnabled()) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kNotSupportedError,
+        "The RemotePlayback API is disabled on this platform.");
+    return EmptyPromise();
+  }
+
   if (availability_ == mojom::ScreenAvailability::UNAVAILABLE) {
     exception_state.ThrowDOMException(DOMExceptionCode::kNotFoundError,
                                       "No remote playback devices found.");
@@ -308,7 +315,8 @@ void RemotePlayback::PromptInternal() {
 
 int RemotePlayback::WatchAvailabilityInternal(
     AvailabilityCallbackWrapper* callback) {
-  if (IsBackgroundAvailabilityMonitoringDisabled()) {
+  if (RuntimeEnabledFeatures::RemotePlaybackBackendEnabled() &&
+      IsBackgroundAvailabilityMonitoringDisabled()) {
     return kWatchAvailabilityNotSupported;
   }
 
@@ -442,7 +450,8 @@ void RemotePlayback::SourceChanged(const KURL& source,
 
 void RemotePlayback::UpdateAvailabilityUrlsAndStartListening() {
   if (is_background_availability_monitoring_disabled_for_testing_ ||
-      IsBackgroundAvailabilityMonitoringDisabled()) {
+      IsBackgroundAvailabilityMonitoringDisabled() ||
+      !RuntimeEnabledFeatures::RemotePlaybackBackendEnabled()) {
     return;
   }
 
@@ -521,6 +530,7 @@ void RemotePlayback::StateChangedForTesting(bool is_connected) {
 
 bool RemotePlayback::RemotePlaybackAvailable() const {
   if (IsBackgroundAvailabilityMonitoringDisabled() &&
+      RuntimeEnabledFeatures::RemotePlaybackBackendEnabled() &&
       !media_element_->currentSrc().IsEmpty()) {
     return true;
   }
