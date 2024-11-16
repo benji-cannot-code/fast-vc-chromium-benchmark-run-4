8484141d960b2aec29aef7a936fc11b6623adfa6
Clark DuVall <cduvall@chromium.org>
2024-11-16 01:07:19 +0000

    Update on device benchmark to only check after version change
    This avoids checking too often if the browser restarts a lot, but we can
    still refresh the perf class on a version change.
    
    Bug: 301660696
    Change-Id: Icfdb9407516a830f5e0a0c01222b53a6a3265805
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024066
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Clark DuVall <cduvall@chromium.org>
    Auto-Submit: Clark DuVall <cduvall@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383913}

--- components/optimization_guide/core/model_execution/on_device_model_component.cc
+++ components/optimization_guide/core/model_execution/on_device_model_component.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/optimization_guide/core/optimization_guide_enums.h"
 #include "components/optimization_guide/core/optimization_guide_features.h"
 #include "components/prefs/pref_service.h"
+#include "components/version_info/version_info.h"
 
 namespace optimization_guide {
 namespace {
@@ -194,7 +195,19 @@ void OnDeviceModelComponentStateManager::DevicePerformanceClassChanged(
     OnDeviceModelPerformanceClass performance_class) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   UpdatePerformanceClassPref(local_state_, performance_class);
+  local_state_->SetString(
+      model_execution::prefs::localstate::kOnDevicePerformanceClassVersion,
+      version_info::GetVersionNumber());
   BeginUpdateRegistration();
+}
+
+bool OnDeviceModelComponentStateManager::NeedsPerformanceClassUpdate() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  return base::FeatureList::IsEnabled(
+             features::kOnDeviceModelFetchPerformanceClassEveryStartup) ||
+         local_state_->GetString(model_execution::prefs::localstate::
+                                     kOnDevicePerformanceClassVersion) !=
+             version_info::GetVersionNumber();
 }
 
 void OnDeviceModelComponentStateManager::OnStartup() {
--- components/optimization_guide/core/model_execution/on_device_model_component.h
+++ components/optimization_guide/core/model_execution/on_device_model_component.h
@@ -109,6 +109,9 @@ class OnDeviceModelComponentStateManager
   void DevicePerformanceClassChanged(
       OnDeviceModelPerformanceClass performance_class);
 
+  // Whether the performance class needs to be fetched.
+  bool NeedsPerformanceClassUpdate();
+
   // Returns the current state. Null if the component is not available.
   const OnDeviceModelComponentState* GetState();
 
