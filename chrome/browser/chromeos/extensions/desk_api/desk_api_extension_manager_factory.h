cad0850bf092fbb7b10cf4a27dbbe80f39b0c638
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-16 00:55:59 +0000

    [desk api]Migrate to BuildServiceInstanceForBrowserContext
    `BuildServiceInstanceFor` is deprecated. This CL ensures that the new
    method `BuildServiceInstanceForBrowserContext` is used instead.
    
    In particular it ensures that services are created with a unique_ptr.
    
    This CL was uploaded by git cl split.
    
    R=yzd@google.com
    
    Bug: 40249337
    Change-Id: If39a15f47edde0f923f6de8bb11e36914dbdc428
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021105
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Yanzhu Du <yzd@google.com>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1383906}

--- chrome/browser/chromeos/extensions/desk_api/desk_api_extension_manager_factory.h
+++ chrome/browser/chromeos/extensions/desk_api/desk_api_extension_manager_factory.h
@@ -32,7 +32,7 @@ class DeskApiExtensionManagerFactory : public ProfileKeyedServiceFactory {
   DeskApiExtensionManagerFactory();
   ~DeskApiExtensionManagerFactory() override;
 
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
 };
 
