1351a8d1fd7107b01cd3177b78c1e0a3d77c257d
Ken Rockot <rockot@google.com>
2024-11-16 01:48:10 +0000

    Introduce ipcz fuzzer
    Adds a new ipcz_fuzzer binary which runs a test case to
    deterministically cover most interesting ipcz code, injecting
    fuzz data during a single internode transmission. Specific
    injection timing and location are selected by interpreting the
    leading bytes of fuzz data as a configuration.
    
    Bug: 370880241
    Change-Id: I3585a7e1e204e85ebdbcd326df238327b20d6622
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6004442
    Commit-Queue: Ken Rockot <rockot@google.com>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1383929}

---
+++ third_party/ipcz/src/fuzzer/ipcz_fuzzer.cc
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cstddef>
+
+#include "fuzzer/fuzzer.h"
+#include "fuzzer/ipcz_fuzzer_testcase.h"
+#include "third_party/abseil-cpp/absl/types/span.h"
+
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data, size_t size) {
+  if (size <= sizeof(ipcz::fuzzer::Fuzzer::FuzzConfig)) {
+    return 0;
+  }
+
+  const auto& config =
+      *reinterpret_cast<const ipcz::fuzzer::Fuzzer::FuzzConfig*>(data);
+  ipcz::fuzzer::Fuzzer fuzzer(
+      config, absl::MakeSpan(data, size).subspan(sizeof(config)));
+  ipcz::fuzzer::IpczFuzzerTestcase testcase(fuzzer);
+  testcase.Run();
+  return 0;
+}
