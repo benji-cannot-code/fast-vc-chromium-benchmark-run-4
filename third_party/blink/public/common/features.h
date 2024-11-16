067e933f0da8201001b289bd379bcbd6d3968402
Maggie Chen <magchen@chromium.org>
2024-11-16 19:12:11 +0000

    Remove kCanvasSharedBitmapToSharedImage
    This feature is enabled by default after M130.
    
    Bug:40064122
    
    Change-Id: I9052c002de2ed20e5afd79ad4cd39d172e19d0cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6024347
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Maggie Chen <magchen@chromium.org>
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384024}

--- third_party/blink/public/common/features.h
+++ third_party/blink/public/common/features.h
@@ -243,9 +243,6 @@ BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kCanvas2DHibernation);
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(
     kCanvas2DHibernationReleaseTransferMemory);
 
-// Enable the Shared Bitmap to Shared Image conversion for Canvas.
-BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kCanvasSharedBitmapToSharedImage);
-
 BLINK_COMMON_EXPORT BASE_DECLARE_FEATURE(kCaptureJSExecutionLocation);
 
 // If enabled, the HTMLDocumentParser will only check its budget after parsing a
@@ -1894,8 +1891,6 @@ BLINK_COMMON_EXPORT bool IsKeepAliveURLLoaderServiceEnabled();
 // Returns true if Link Preview and the given trigger type is enabled.
 BLINK_COMMON_EXPORT bool IsLinkPreviewTriggerTypeEnabled(
     LinkPreviewTriggerType type);
-
-BLINK_COMMON_EXPORT bool IsCanvasSharedBitmapConversionEnabled();
 
 // DO NOT ADD NEW FEATURES HERE.
 //
--- third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource_host.cc
@@ -283,9 +283,8 @@ bool CanvasResourceHost::IsResourceValid() {
     return true;
   }
 
-  if (!features::IsCanvasSharedBitmapConversionEnabled() ||
-      (resource_provider_ &&
-       resource_provider_->GetType() == CanvasResourceProvider::kBitmap)) {
+  if (resource_provider_ &&
+      resource_provider_->GetType() == CanvasResourceProvider::kBitmap) {
     if (preferred_2d_raster_mode_ == RasterModeHint::kPreferCPU) {
       return true;
     }
