c82959dcb759b72b05ace15bddc137e43a95ffcf
Jenny Zhang <jennyz@chromium.org>
2024-11-17 07:08:37 +0000

    [Desktop Android CRX] Enable system.memory API.
    This cl enables system.memory on desktop android, and
    add an API test for MV3 extension.
    
    Bug: 379169420
    Change-Id: I7966a10c9ee228a5ad43a9986f4d2fcc35dc332b
    Cq-Include-Trybots: luci.chromium.try:android-desktop-arm64-compile-rel,android-desktop-x64-compile-rel,android-desktop-14-x64-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021558
    Reviewed-by: Emilia Paz <emiliapaz@chromium.org>
    Commit-Queue: Jenny Zhang <jennyz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384072}

--- chrome/browser/extensions/system_memory_apitest.cc
+++ chrome/browser/extensions/system_memory_apitest.cc
@@ -3,9 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/extension_apitest.h"
 #include "content/public/test/browser_test.h"
 #include "extensions/browser/api/system_memory/memory_info_provider.h"
+
+#if BUILDFLAG(IS_ANDROID)
+#include "chrome/browser/extensions/extension_platform_apitest.h"
+#else
+#include "chrome/browser/extensions/extension_apitest.h"
+#endif  // BUILDFLAG(IS_ANDROID)
 
 namespace extensions {
 
@@ -28,6 +33,9 @@ class MockMemoryInfoProviderImpl : public MemoryInfoProvider {
   ~MockMemoryInfoProviderImpl() override = default;
 };
 
+// Desktop android only supports manifest v3 and later, don't need to run
+// tests for MV2 extensions.
+#if !BUILDFLAG(IS_ANDROID)
 using ContextType = ExtensionBrowserTest::ContextType;
 
 class SystemMemoryApiTest : public ExtensionApiTest,
@@ -51,6 +59,28 @@ IN_PROC_BROWSER_TEST_P(SystemMemoryApiTest, Memory) {
   // The provider is owned by the single MemoryInfoProvider instance.
   MemoryInfoProvider::InitializeForTesting(provider);
   ASSERT_TRUE(RunExtensionTest("system_memory")) << message_;
+}
+#endif
+
+#if BUILDFLAG(IS_ANDROID)
+using ExtensionApiTestBase = ExtensionPlatformApiTest;
+#else
+using ExtensionApiTestBase = ExtensionApiTest;
+#endif
+class SystemMemoryApiMV3Test : public ExtensionApiTestBase {
+ public:
+  SystemMemoryApiMV3Test() = default;
+  ~SystemMemoryApiMV3Test() override = default;
+  SystemMemoryApiMV3Test(const SystemMemoryApiMV3Test&) = delete;
+  SystemMemoryApiMV3Test& operator=(const SystemMemoryApiMV3Test&) = delete;
+};
+
+// Test memory api for MV3 extension.
+IN_PROC_BROWSER_TEST_F(SystemMemoryApiMV3Test, Memory) {
+  scoped_refptr<MemoryInfoProvider> provider = new MockMemoryInfoProviderImpl;
+  // The provider is owned by the single MemoryInfoProvider instance.
+  MemoryInfoProvider::InitializeForTesting(provider);
+  ASSERT_TRUE(RunExtensionTest("system_memory/mv3")) << message_;
 }
 
 }  // namespace extensions
