ea65c73920f0cbb988d79cc6c49284535c436608
Peter Kasting <pkasting@chromium.org>
2024-11-18 05:32:46 +0000

    Cleanups from stricter span usage of size_t: third_party/zlib
    More use of first(), compile-time sizes, etc.
    
    Bug: none
    Change-Id: Ic93c3c1a7bbc2c3aead9ff77272e442a5388a1f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6027098
    Reviewed-by: Satoru Takabayashi <satorux@chromium.org>
    Commit-Queue: Satoru Takabayashi <satorux@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1384167}

--- third_party/zlib/google/zip_writer.cc
+++ third_party/zlib/google/zip_writer.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/zlib/google/zip_writer.h"
 
 #include <algorithm>
+#include <tuple>
 
 #include "base/files/file.h"
 #include "base/logging.h"
@@ -194,8 +195,8 @@ bool ZipWriter::AddMixedEntries(Paths paths) {
   while (!paths.empty()) {
     // Work with chunks of 50 paths at most.
     const size_t n = std::min<size_t>(paths.size(), 50);
-    const Paths relative_paths = paths.subspan(0, n);
-    paths = paths.subspan(n, paths.size() - n);
+    Paths relative_paths;
+    std::tie(relative_paths, paths) = paths.split_at(n);
 
     files.clear();
     if (!file_accessor_->Open(relative_paths, &files) || files.size() != n)
@@ -234,8 +235,8 @@ bool ZipWriter::AddFileEntries(Paths paths) {
   while (!paths.empty()) {
     // Work with chunks of 50 paths at most.
     const size_t n = std::min<size_t>(paths.size(), 50);
-    const Paths relative_paths = paths.subspan(0, n);
-    paths = paths.subspan(n, paths.size() - n);
+    Paths relative_paths;
+    std::tie(relative_paths, paths) = paths.split_at(n);
 
     DCHECK_EQ(relative_paths.size(), n);
 
