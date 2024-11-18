53f4ae68fc42376c08a38d96255dae87059d0afb
Shiyi Zou <shiyi.zou@intel.com>
2024-11-18 03:33:33 +0000

    Reland "webnn: implement reverse operator"
    This is a reland of commit 0459ac1a9b94c1dd3e11410f7fb74f0a8e2ddeaf
    
    The previous CL got reverted because another CL[1] changed the function
    name `buildGraphAndCompute` to `buildAndExecuteGraph` in WPT. This CL
    fixes the name.
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5556220
    
    Original change's description:
    > webnn: implement reverse operator
    >
    > This CL adds IDL and mojo definition of reverse operator according to
    > the spec issue [1] and implements it on DirectML backend.
    >
    > [1] https://github.com/webmachinelearning/webnn/issues/773
    >
    > Bug: 376707210
    > Change-Id: I0d42b49b87ce243db9d44512e6000c7ee901077b
    > Cq-Include-Trybots: luci.chromium.try:win11-blink-rel, mac14.arm64-blink-rel, mac15.arm64-blink-rel, linux-blink-rel
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5979825
    > Commit-Queue: ningxin hu <ningxin.hu@intel.com>
    > Auto-Submit: Shiyi Zou <shiyi.zou@intel.com>
    > Reviewed-by: Austin Sullivan <asully@chromium.org>
    > Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    > Cr-Commit-Position: refs/heads/main@{#1382078}
    
    Bug: 376707210
    Change-Id: I604de69bcc5f8932ac47f9f34a9b4c58e589193d
    Cq-Include-Trybots: luci.chromium.try:mac14.arm64-blink-rel, mac14-blink-rel, mac15.arm64-blink-rel, mac15-blink-rel, linux-blink-rel
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015938
    Reviewed-by: ningxin hu <ningxin.hu@intel.com>
    Commit-Queue: Shiyi Zou <shiyi.zou@intel.com>
    Auto-Submit: Shiyi Zou <shiyi.zou@intel.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Austin Sullivan <asully@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384152}

--- services/webnn/tflite/graph_builder_tflite.cc
+++ services/webnn/tflite/graph_builder_tflite.cc
@@ -459,6 +459,8 @@ ContextProperties GraphBuilderTflite::GetContextProperties() {
        /*relu_input=*/DataTypeConstraint::kFloat16To32,
        /*resample2d_input=*/DataTypeConstraint::kFloat16To32,
        /*reshape_input=*/kAllDataTypesExceptUint4,
+       // TODO(crbug.com/377366939): Implement Reverse.
+       /*reverse_input=*/{},
        // TODO(crbug.com/370538329): Implement scatterElements.
        /*scatter_elements_input=*/{},
        /*scatter_elements_indices=*/{},
@@ -885,6 +887,7 @@ base::expected<void, std::string> GraphBuilderTflite::SerializeOperation(
       break;
     }
     case mojom::Operation::Tag::kGatherElements:
+    case mojom::Operation::Tag::kReverse:
     case mojom::Operation::Tag::kScatterElements:
       return base::unexpected(NotSupportedOperatorError(op));
   }
