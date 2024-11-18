3387dae4292e3c53a498fab38ad6eb1e064acfa5
Antonio Gomes <tonikitoo@igalia.com>
2024-11-18 09:24:05 +0000

    Remove calls to IsLacrosWindow() in chrome/browser/ui/ash/shelf/
    With the termination of the Lacros project, this function always return
    `false`. This CL removes the calls in c/b/ui/ash/shelf and simplifies
    its call sites.
    
    R=hidehiko@chromium.org, neis@chromium.org
    
    Bug: 378932949, 373971535
    Change-Id: I95002334f79054526d675c8e406d954b965183f7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022988
    Reviewed-by: Georg Neis <neis@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/main@{#1384217}

--- chrome/browser/ui/ash/shelf/app_service/app_service_app_window_crostini_tracker.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_app_window_crostini_tracker.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/apps/app_service/app_service_proxy.h"
 #include "chrome/browser/apps/app_service/app_service_proxy_factory.h"
 #include "chrome/browser/ash/borealis/borealis_window_manager.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/crostini/crostini_features.h"
 #include "chrome/browser/ash/crostini/crostini_force_close_watcher.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
@@ -78,7 +77,6 @@ void MoveWindowFromOldDisplayToNewDisplay(aura::Window* window,
 bool ShouldSkipWindow(aura::Window* window) {
   return wm::GetTransientParent(window) ||
          arc::GetWindowTaskOrSessionId(window).has_value() ||
-         crosapi::browser_util::IsLacrosWindow(window) ||
          plugin_vm::IsPluginVmAppWindow(window) ||
          ash::borealis::IsBorealisWindow(window);
 }
--- chrome/browser/ui/ash/shelf/app_service/app_service_app_window_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/app_service/app_service_app_window_shelf_controller.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/borealis/borealis_service.h"
 #include "chrome/browser/ash/borealis/borealis_service_factory.h"
 #include "chrome/browser/ash/borealis/borealis_window_manager.h"
-#include "chrome/browser/ash/crosapi/browser_util.h"
 #include "chrome/browser/ash/crostini/crostini_features.h"
 #include "chrome/browser/ash/crostini/crostini_util.h"
 #include "chrome/browser/ash/guest_os/guest_os_shelf_utils.h"
@@ -632,9 +631,6 @@ void AppServiceAppWindowShelfController::OnItemDelegateDiscarded(
 
 ash::ShelfID AppServiceAppWindowShelfController::GetShelfId(
     aura::Window* window) const {
-  if (crosapi::browser_util::IsLacrosWindow(window))
-    return ash::ShelfID(app_constants::kLacrosAppId);
-
   std::string shelf_app_id;
   if (ash::borealis::IsBorealisWindow(window)) {
     for (Profile* profile : profile_list_) {
