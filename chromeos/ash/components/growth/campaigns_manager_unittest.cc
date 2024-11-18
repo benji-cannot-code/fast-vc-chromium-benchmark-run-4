92d7f06dd2d392aaf3503930e91352675fb6ed02
ThuHuong <thv@chromium.org>
2024-11-18 18:29:59 +0000

    Update pref targeting test case.
    Update the pref targeting unittest to test the case pref value is
    boolean.
    
    Bug: b:361180907
    Change-Id: I33d72050f90196e904f39948c9a906fc9485cb3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5804377
    Reviewed-by: Tao Wu <wutao@chromium.org>
    Commit-Queue: Thu Huong Vu <thv@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384458}

--- chromeos/ash/components/growth/campaigns_manager_unittest.cc
+++ chromeos/ash/components/growth/campaigns_manager_unittest.cc
@@ -624,7 +624,7 @@ class CampaignsManagerTest : public testing::Test {
     pref_->registry()->RegisterListPref(
         kTestPref1, base::Value::List().Append("v0").Append("v1"));
     pref_->registry()->RegisterStringPref(kTestPref2, "v2");
-    pref_->registry()->RegisterStringPref(kTestPref3, "v3");
+    pref_->registry()->RegisterBooleanPref(kTestPref3, true);
   }
 
   void InitializeUserManager() {
@@ -2315,10 +2315,11 @@ TEST_F(CampaignsManagerTest, GetCampaignMatchMultiTargetingsMismatch) {
 }
 
 TEST_F(CampaignsManagerTest, GetCampaignWithInvalidPrefTargeting) {
+  // Pref values: pref1=[v0,v1], pref2=v2, pref3=true
   LoadComponentWithUserPrefTargeting(R"(
     [
       {
-        "name": "pref1",
+        "name": "pref_1",
         "value": "v0"
       }
     ])");
@@ -2327,6 +2328,7 @@ TEST_F(CampaignsManagerTest, GetCampaignWithInvalidPrefTargeting) {
 }
 
 TEST_F(CampaignsManagerTest, GetCampaignWithPrefTargetingMismatch) {
+  // Pref values: pref1=[v0,v1], pref2=v2, pref3=true
   LoadComponentWithUserPrefTargeting(R"(
     [
       {
@@ -2345,7 +2347,8 @@ TEST_F(CampaignsManagerTest, GetCampaignWithPrefTargetingMismatch) {
 }
 
 TEST_F(CampaignsManagerTest, GetCampaignWithPrefTargetingMatch) {
-  // This 2nd set of considtion match pref1 = value_0 and pref2 = value_2.
+  // Pref values: pref1=[v0,v1], pref2=v2, pref3=true
+  // This 2 sets of condition matched with pref1 = v1 and pref3 = true.
   LoadComponentWithUserPrefTargeting(R"(
     [
       {
@@ -2354,13 +2357,13 @@ TEST_F(CampaignsManagerTest, GetCampaignWithPrefTargetingMatch) {
       },
       {
         "name": "pref2",
-        "value": ["v2"]
+        "value": ["v3"]
       }
     ],
     [
       {
         "name": "pref3",
-        "value": ["v3"]
+        "value": [true]
       }
     ])");
 
