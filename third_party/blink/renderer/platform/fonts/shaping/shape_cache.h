aabfaa932e911bc71f32e0c4b88766a09b3bded9
Kent Tamura <tkent@chromium.org>
2024-11-18 23:52:00 +0000

    Spanify some variants of WTF::Equal(), part 2
    This CL spanifiy pairs of the second and the third arguments of the
    following functions:
    
    - Equal(const CharType* a, const CharType* b, wtf_size_t length)
    - Equal(const LChar* a, const UChar* b, wtf_size_t length)
    - Equal(const UChar* a, const LChar* b, wtf_size_t length)
    
    * Add Span8() and Span16() to CSSParserToken for ease of base::span
      creation.
    * Add begin() and end() to ShapeCache::SmallStringKey for ease of
      base::span creation.
    
    This CL should have no behavior changes.
    
    Bug: 351564777
    Change-Id: Iccea15a89e123119a59b3614ff14f1f146759193
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023587
    Reviewed-by: Fredrik Söderquist <fs@opera.com>
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384652}

--- third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
+++ third_party/blink/renderer/platform/fonts/shaping/shape_cache.h
@@ -73,8 +73,7 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
         characters_[i] = characters[i];
       }
 
-      hash_ = static_cast<unsigned>(base::FastHash(
-          base::as_bytes(base::make_span(characters_, length_))));
+      hash_ = static_cast<unsigned>(base::FastHash(base::as_byte_span(*this)));
     }
 
     SmallStringKey(base::span<const UChar> characters, TextDirection direction)
@@ -82,12 +81,13 @@ class ShapeCache : public GarbageCollected<ShapeCache> {
           direction_(static_cast<unsigned>(direction)) {
       DCHECK(characters.size() <= kCapacity);
       memcpy(characters_, characters.data(), characters.size_bytes());
-      hash_ = static_cast<unsigned>(base::FastHash(
-          base::as_bytes(base::make_span(characters_, length_))));
+      hash_ = static_cast<unsigned>(base::FastHash(base::as_byte_span(*this)));
     }
 
     const UChar* Characters() const { return characters_; }
     uint16_t length() const { return length_; }
+    const UChar* begin() const { return characters_; }
+    const UChar* end() const { return characters_ + length_; }
     TextDirection Direction() const {
       return static_cast<TextDirection>(direction_);
     }
@@ -231,7 +231,7 @@ inline bool operator==(const ShapeCache::SmallStringKey& a,
                        const ShapeCache::SmallStringKey& b) {
   if (a.length() != b.length() || a.Direction() != b.Direction())
     return false;
-  return WTF::Equal(a.Characters(), b.Characters(), a.length());
+  return WTF::Equal(a.Characters(), base::span(b));
 }
 
 }  // namespace blink
