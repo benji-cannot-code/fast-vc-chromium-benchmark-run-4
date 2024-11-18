58e508646f379d416479a7d786d736237ecdfccd
thefrog <thefrog@chromium.org>
2024-11-18 19:23:22 +0000

    Make TriggerManager::GetSBErrorDisplayOptions() return smaller struct
    Currently GetSBErrorDisplayOptions() returns a large struct of type
    security_interstitials::BaseSafeBrowsingErrorUI::SBErrorDisplayOptions.
    However, the calls of that method only end up using 3 of the struct
    fields.
    
    This CL replaces that function with GetDataCollectionPermissions(),
    which returns a smaller struct DataCollectionPermissions that only
    contains the 3 relevant fields. TriggerManager methods that used the
    struct SBErrorDisplayOptions now use DataCollectionPermissions instead.
    
    The two blocking page classes used a separate BaseSafeBrowsingErrorUI
    helper method to thread through a SBErrorDisplayOptions to methods on
    the TriggerManager. These helper method calls are left as-is, and the
    returned struct is converted into a DataCollectionPermissions after.
    
    Fixed: 345125741
    Change-Id: Icf93c4c344fb774b66d93f9e8507fc46772c8387
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026188
    Reviewed-by: Zack Han <zackhan@chromium.org>
    Reviewed-by: Nate Fischer <ntfschr@chromium.org>
    Commit-Queue: thefrog <thefrog@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384499}

--- components/safe_browsing/content/browser/triggers/mock_trigger_manager.h
+++ components/safe_browsing/content/browser/triggers/mock_trigger_manager.h
@@ -31,7 +31,7 @@ class MockTriggerManager : public TriggerManager {
            scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
            history::HistoryService* history_service,
            ReferrerChainProvider* referrer_chain_provider,
-           const SBErrorOptions& error_display_options));
+           const DataCollectionPermissions& data_collection_permissions));
   MOCK_METHOD8(
       StartCollectingThreatDetailsWithReason,
       bool(TriggerType trigger_type,
@@ -40,7 +40,7 @@ class MockTriggerManager : public TriggerManager {
            scoped_refptr<network::SharedURLLoaderFactory> url_loader_factory,
            history::HistoryService* history_service,
            ReferrerChainProvider* referrer_chain_provider,
-           const SBErrorOptions& error_display_options,
+           const DataCollectionPermissions& data_collection_permissions,
            TriggerManagerReason* out_reason));
 
   MOCK_METHOD8(FinishCollectingThreatDetails,
@@ -50,7 +50,7 @@ class MockTriggerManager : public TriggerManager {
                    const base::TimeDelta& delay,
                    bool did_proceed,
                    int num_visits,
-                   const SBErrorOptions& error_display_options,
+                   const DataCollectionPermissions& data_collection_permissions,
                    std::optional<int64_t> warning_shown_ts,
                    bool is_hats_candidate));
 };
