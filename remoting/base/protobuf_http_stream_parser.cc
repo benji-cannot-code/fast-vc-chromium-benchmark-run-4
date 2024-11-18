5fac35443660d752d200cee0bac7a9fe79e94334
Peter Kasting <pkasting@chromium.org>
2024-11-18 16:58:06 +0000

    Pass size_t to span functions: remoting/
    In preparation for upgrading various span args from size_t to
    StrictNumeric<size_t>, this fixes type mismatches that would cause
    compile errors with that change.
    
    Bug: none
    Change-Id: I8934a4fc8fe7efa5750ab48ac83d2603419f9b7e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020502
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joe Downing <joedow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384392}

--- remoting/base/protobuf_http_stream_parser.cc
+++ remoting/base/protobuf_http_stream_parser.cc
@@ -85,11 +85,12 @@ void ProtobufHttpStreamParser::ParseStreamIfAvailable() {
     }
   }
 
-  if (bytes_consumed == 0) {
+  if (bytes_consumed <= 0) {
     return;
   }
   base::span<const uint8_t> bytes_not_consumed =
-      read_buffer_->span_before_offset().subspan(bytes_consumed);
+      read_buffer_->span_before_offset().subspan(
+          static_cast<size_t>(bytes_consumed));
   read_buffer_->everything().copy_prefix_from(bytes_not_consumed);
   read_buffer_->set_offset(bytes_not_consumed.size());
 }
