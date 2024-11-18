ce861e8b4121643b028bdc0db99d37615a84a514
Mason Freed <masonf@chromium.org>
2024-11-18 15:40:57 +0000

    Fix dialog.requestClose() in two ways
    1. The prior implementation ignored the argument to requestClose.
       With this CL, requestClose(string) stores away string and then
       returns it from the CloseWatcher close event.
    2. If the dialog's closedBy state is None (either explicitly or
       through the "Auto" state), requestClose throws an exception.
    
    I also added a more significant set of tests for requestClose() in
    all states.
    
    This goes along with recent discussion about throwing exceptions if
    requestClose() is called when the dialog isn't in the right state,
    relative to closedBy:
    
      https://github.com/whatwg/html/issues/10164#issuecomment-2479792309
    
    The spec PR has been updated accordingly:
    
      https://github.com/whatwg/html/pull/10737
    
    Bug: 376516550
    Change-Id: I023845844e6afb4da9a71637d517ac78d2861329
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026242
    Auto-Submit: Mason Freed <masonf@chromium.org>
    Reviewed-by: David Baron <dbaron@chromium.org>
    Commit-Queue: David Baron <dbaron@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384345}

--- third_party/blink/renderer/core/html/html_dialog_element.cc
+++ third_party/blink/renderer/core/html/html_dialog_element.cc
@@ -137,6 +137,7 @@ HTMLDialogElement::HTMLDialogElement(Document& document)
     : HTMLElement(html_names::kDialogTag, document),
       is_modal_(false),
       return_value_(""),
+      request_close_return_value_(""),
       previously_focused_element_(nullptr) {
   UseCounter::Count(document, WebFeature::kDialogElement);
 }
@@ -203,13 +204,20 @@ void HTMLDialogElement::close(const String& return_value,
   }
 }
 
-void HTMLDialogElement::requestClose(const String& return_value) {
+void HTMLDialogElement::requestClose(const String& return_value,
+                                     ExceptionState& exception_state) {
   CHECK(RuntimeEnabledFeatures::HTMLDialogLightDismissEnabled());
   if (!IsOpen()) {
     return;
   }
-  // The close watcher might be disabled if this is a non-modal dialog.
-  close_watcher_->setEnabled(true);
+  if (ClosedBy() == ClosedByState::kNone) {
+    exception_state.ThrowDOMException(
+        DOMExceptionCode::kInvalidStateError,
+        "To use requestClose, the dialog's closedBy state must not be 'none'.");
+    return;
+  }
+  CHECK(close_watcher_);
+  request_close_return_value_ = return_value;
   close_watcher_->requestClose();
   SetCloseWatcherEnabledState();
 }
@@ -318,7 +326,7 @@ void HTMLDialogElement::HandleDialogLightDismiss(const Event& event,
       auto& dialog = dialog_list.at(index);
       if (dialog != ancestor_dialog &&
           dialog->ClosedBy() == ClosedByState::kAny) {
-        dialog->requestClose();
+        dialog->requestClose(String(), ASSERT_NO_EXCEPTION);
       }
     }
   }
@@ -470,8 +478,7 @@ void HTMLDialogElement::SetCloseWatcherEnabledState() {
   }
   CHECK(close_watcher_);
   ClosedByState closed_by = ClosedBy();
-  close_watcher_->setEnabled(closed_by == ClosedByState::kAny ||
-                             closed_by == ClosedByState::kCloseRequest);
+  close_watcher_->setEnabled(closed_by != ClosedByState::kNone);
 }
 
 void HTMLDialogElement::CreateCloseWatcher() {
@@ -480,6 +487,7 @@ void HTMLDialogElement::CreateCloseWatcher() {
   if (!window) {
     return;
   }
+  CHECK(IsOpen());
   close_watcher_ = CloseWatcher::Create(*window);
   if (!close_watcher_) {
     return;
@@ -595,7 +603,7 @@ void HTMLDialogElement::CloseWatcherFiredCancel(Event* close_watcher_event) {
 void HTMLDialogElement::CloseWatcherFiredClose() {
   // https://wicg.github.io/close-watcher/#patch-dialog closeAction
 
-  close();
+  close(request_close_return_value_);
 }
 
 // https://html.spec.whatwg.org#dialog-focusing-steps
