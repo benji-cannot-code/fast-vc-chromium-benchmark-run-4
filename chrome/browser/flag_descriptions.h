e0de209dad007d6118bf8a98d8163a0240b13b03
Li-Yu Yu <aaronyu@google.com>
2024-11-18 02:36:48 +0000

    Remove #audio-flexible-loopback-for-system-loopback
    Fixed: b:361119439
    Test: autoninja -C out/ut unit_tests && out/ut/unit_tests --gtest_filter=AboutFlagsTest.\*
    Change-Id: Id429fd157005ebc299eb11b30b13a4f2bcae5184
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6029410
    Commit-Queue: Li-Yu Yu <aaronyu@google.com>
    Reviewed-by: Yu-Hsuan Hsu <yuhsuan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384145}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -4499,9 +4499,6 @@ extern const char kShowForceRespectUiGainsToggleDescription[];
 extern const char kCrOSSystemVoiceIsolationOptionName[];
 extern const char kCrOSSystemVoiceIsolationOptionDescription[];
 
-extern const char kAudioFlexibleLoopbackForSystemLoopbackName[];
-extern const char kAudioFlexibleLoopbackForSystemLoopbackDescription[];
-
 extern const char kShowSpatialAudioToggleName[];
 extern const char kShowSpatialAudioToggleDescription[];
 #endif  // BUILDFLAG(IS_CHROMEOS)
