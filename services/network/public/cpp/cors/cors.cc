c67a161828bd9bc2a3ca89c6ddf4a003494b84d6
Kalvin Lee <kdlee@chromium.org>
2024-11-18 11:11:25 +0000

    spanify: Array-ify `kWebEffectiveConnectionTypeMapping`
    `spanify` wants to rewrite this constant but fails to find the size.
    This CL does so manually.
    
    While we're at it, clean up a number of `DCHECK()`s that use only
    compile-time constants, deduplicating them, upgrading them to
    `static_assert()`, and moving them right under the declaration of
    `kWebEffectiveConnectionTypeMapping`.
    
    Additionally, remove a legacy `DCHECK()` from
    `network_state_notifier.cc`, since converting this constant to a
    `std::array` also gives us bound checks, which will surface a crash just
    as reliably as said `DCHECK()`.
    
    Bug: 364338808
    Change-Id: I8ae7ca4f70045f397cb41179a264e0f15413348c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023055
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Kalvin Lee <kdlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384245}

--- services/network/public/cpp/cors/cors.cc
+++ services/network/public/cpp/cors/cors.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "services/network/public/cpp/cors/cors.h"
 
+#include <algorithm>
 #include <set>
 #include <string_view>
 #include <vector>
@@ -422,10 +423,10 @@ bool IsCorsSafelistedHeader(const std::string& name, const std::string& value) {
              lower_name == "rtt") {
     return IsSimilarToIntABNF(value);
   } else if (lower_name == "ect") {
-    auto* const* begin = network::kWebEffectiveConnectionTypeMapping;
-    auto* const* end = network::kWebEffectiveConnectionTypeMapping +
-                       network::kWebEffectiveConnectionTypeMappingCount;
-    return std::find(begin, end, value) != end;
+    return std::find(network::kWebEffectiveConnectionTypeMapping.begin(),
+                     network::kWebEffectiveConnectionTypeMapping.end(),
+                     value) !=
+           network::kWebEffectiveConnectionTypeMapping.end();
   } else if (lower_name == "save-data") {
     return lower_value == "on";
   }
