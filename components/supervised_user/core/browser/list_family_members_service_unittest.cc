94f007fd540d788570942a6beb094d745e07a2f2
Liza Bipin <mlbipin@google.com>
2024-11-18 10:17:47 +0000

    Revert "Enumerate and cache the identity states of a Family Link user."
    This reverts commit 16f032abbb1bb641510e7737fe4da91b7d6addd8.
    
    Reason for revert: This enum will no longer be used. 
    
    Original change's description:
    > Enumerate and cache the identity states of a Family Link user.
    >
    > Maps the identity state of a user into a format that can be used to
    > determine Family Link properties associated to the account.
    >
    > NO_IFTTT=Adding FamilyLinkUserState to linter
    >
    > Bug: 372607761
    > Change-Id: Ie46f24b644ae0b8fd3ef63ec4484c6a12232fb66
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5929845
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Reviewed-by: James Lee <ljjlee@google.com>
    > Commit-Queue: Liza Bipin <mlbipin@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1376219}
    
    Bug: 372607761
    Change-Id: I911f5a74f95e2999dac4a1213b086c168aa84efa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6023176
    Commit-Queue: Liza Bipin <mlbipin@google.com>
    Reviewed-by: Nohemi Fernandez <fernandex@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Cr-Commit-Position: refs/heads/main@{#1384227}

--- components/supervised_user/core/browser/list_family_members_service_unittest.cc
+++ components/supervised_user/core/browser/list_family_members_service_unittest.cc
@@ -182,9 +182,6 @@ TEST_F(ListFamilyMembersServiceTest,
   ASSERT_EQ(0, test_url_loader_factory_.NumPending());
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             supervised_user::kDefaultEmptyFamilyMemberRole);
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kNotFamilyLink));
 
   test_list_family_members_service_->Shutdown();
 }
@@ -227,9 +224,6 @@ TEST_F(ListFamilyMembersServiceTest, FamilyRolePrefReflectsAccountCapability) {
 
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             "family_manager");
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kHeadOfHousehold));
 
   test_list_family_members_service_->Shutdown();
 }
@@ -443,8 +437,6 @@ TEST_F(ListFamilyMembersServiceTest,
   SimulateErrorResponseForPendingRequest();
   ASSERT_EQ(0, test_url_loader_factory_.NumPending());
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole), "");
-  EXPECT_EQ(pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-            static_cast<int>(supervised_user::FamilyLinkUserState::kUnknown));
 
   test_list_family_members_service_->Shutdown();
 }
@@ -490,17 +482,11 @@ TEST_F(ListFamilyMembersServiceTest, ListFamilyFetcherClearsResponseOnSignout) {
   EXPECT_EQ(hoh_username, "username_hoh");
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             "family_manager");
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kHeadOfHousehold));
 
   identity_test_env_.ClearPrimaryAccount();
   EXPECT_EQ(hoh_username, "");
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             supervised_user::kDefaultEmptyFamilyMemberRole);
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kNotFamilyLink));
 
   test_list_family_members_service_->Shutdown();
 }
@@ -544,17 +530,11 @@ TEST_F(ListFamilyMembersServiceTest, ListFamilyFetcherResetsPrefOnSignout) {
   EXPECT_EQ(hoh_username, "username_hoh");
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             "family_manager");
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kHeadOfHousehold));
 
   identity_test_env_.ClearPrimaryAccount();
   EXPECT_EQ(hoh_username, "");
   EXPECT_EQ(pref_service_.GetString(prefs::kFamilyLinkUserMemberRole),
             supervised_user::kDefaultEmptyFamilyMemberRole);
-  EXPECT_EQ(
-      pref_service_.GetInteger(prefs::kFamilyLinkUserState),
-      static_cast<int>(supervised_user::FamilyLinkUserState::kNotFamilyLink));
 
   test_list_family_members_service_->Shutdown();
 }
