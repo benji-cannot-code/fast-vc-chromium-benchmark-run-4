7c67a649550f355064645e33db5e4bdd83f784ec
Christian Xu <christianxu@google.com>
2024-11-18 11:20:57 +0000

    [iOS][LO][RemoveSelection][1/N] Expose hide selection UI
    Bug: 375194155
    Change-Id: Idaf1d8d1aaf9517606fda03e5f23fb7e27960b08
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6011022
    Commit-Queue: Christian Xu <christianxu@chromium.org>
    Reviewed-by: Radu Nitescu <radunitescu@google.com>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384247}

--- ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
+++ ios/chrome/browser/lens_overlay/coordinator/fake_chrome_lens_overlay.mm
@@ -71,6 +71,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // NO-OP
 }
 
+- (void)hideUserSelection {
+  // NO-OP
+}
+
 - (void)setTopIconsHidden:(BOOL)hidden {
   // NO-OP
 }
