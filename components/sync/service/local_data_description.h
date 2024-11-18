ab7f65d39894d7e380aa02b0e201519da895f21c
Ivan Murashov <ivan.murashov@lge.com>
2024-11-18 20:26:50 +0000

    IWYU: Add missing include for std::variant usage in local_data_description.h
    In the CL https://crrev.com/c/5934368 used std::variant in the
    components/sync/service/local_data_description.h, but required
    include was missed. Added missing include for std::variant usage.
    
    Bug: 41455655
    Change-Id: If67c513d3b9b89775afb2e36a9913ea67a284e0c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6028535
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384535}

--- components/sync/service/local_data_description.h
+++ components/sync/service/local_data_description.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPONENTS_SYNC_SERVICE_LOCAL_DATA_DESCRIPTION_H_
 
 #include <string>
+#include <variant>
 #include <vector>
 
 #include "components/sync/base/data_type.h"
