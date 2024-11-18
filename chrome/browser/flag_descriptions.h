963df079f7d14d1069bc161d05ffc123f9936282
Dan Polanco <polardz@google.com>
2024-11-18 21:32:06 +0000

    [New Tab Animations] Add Chrome feature flag
    Feature flag to guard the visibility of the new tab animations
    
    Bug: 40933120
    Change-Id: I680f764ebfe9502e389d9df8f325f993bb153cce
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032249
    Reviewed-by: Sky Malice <skym@chromium.org>
    Commit-Queue: Dan Polanco <polardz@google.com>
    Reviewed-by: Calder Kitagawa <ckitagawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384567}

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -2756,6 +2756,9 @@ extern const char kSecurePaymentConfirmationAndroidDescription[];
 extern const char kShareCustomActionsInCCTName[];
 extern const char kShareCustomActionsInCCTDescription[];
 
+extern const char kShowNewTabAnimationsName[];
+extern const char kShowNewTabAnimationsDescription[];
+
 extern const char kShowReadyToPayDebugInfoName[];
 extern const char kShowReadyToPayDebugInfoDescription[];
 
