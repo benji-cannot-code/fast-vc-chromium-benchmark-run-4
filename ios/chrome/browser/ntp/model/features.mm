9fd088466c577036777321de064bae1dbcac0ae9
Hira Mahmood <hiramahmood@google.com>
2024-11-18 23:55:22 +0000

    [iOS] Create Set Up List in First Run feature flag
    This CL creates the feature flag for the Set Up List in First Run
    feature.
    
    Bug: 379305809
    Change-Id: Ibde978a6e89362af9dfb56843491275a217c6cf8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032335
    Reviewed-by: Scott Yoder <scottyoder@google.com>
    Commit-Queue: Hira Mahmood <hiramahmood@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384657}

---
+++ ios/chrome/browser/ntp/model/features.mm
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2024 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ntp/model/features.h"
+
+#import "base/metrics/field_trial_params.h"
+
+namespace set_up_list {
+
+BASE_FEATURE(kSetUpListInFirstRun,
+             "SetUpListInFirstRun",
+             base::FEATURE_DISABLED_BY_DEFAULT);
+
+const char kSetUpListInFirstRunParam[] = "SetUpListInFirstRunParam";
+
+int GetSetUpListInFirstRunVariation() {
+  if (!base::FeatureList::IsEnabled(kSetUpListInFirstRun)) {
+    return 0;
+  }
+  return base::GetFieldTrialParamByFeatureAsInt(kSetUpListInFirstRun,
+                                                kSetUpListInFirstRunParam, 1);
+}
+
+}  // namespace set_up_list
