90e00a7d109d7afb132f82e598f8aef9da9fff15
Hidehiko Abe <hidehiko@chromium.org>
2024-11-18 04:19:17 +0000

    boca: Do not depend on global active user.
    The Boca webui is tied to a BrowserContext instance already, so the
    User corresponding to the BrowserContext should be used,
    instead of depending on the global active User.
    
    Along with the fix, now WebUI needs to be instantiated for unittests.
    So, switches the TestSuite to use content::UnitTestTestRunner to
    use global code injection for WebUI instances.
    
    BUG=278643115
    TEST=Tryjob
    
    Change-Id: I6b3aecfd2b9785f75162ec58f2639e150a0978e6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6022046
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: April Zhou <aprilzhou@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384158}

--- ash/webui/run_all_unittests.cc
+++ ash/webui/run_all_unittests.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/test/launcher/unit_test_launcher.h"
 #include "base/test/test_suite.h"
 #include "build/chromeos_buildflags.h"
+#include "content/public/test/unittest_test_suite.h"
 #include "mojo/core/embedder/embedder.h"
 
 #if BUILDFLAG(IS_CHROMEOS_DEVICE)
@@ -16,11 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 int main(int argc, char** argv) {
-  AshWebUITestSuite test_suite(argc, argv);
+  content::UnitTestTestSuite test_suite(
+      new AshWebUITestSuite(argc, argv),
+      base::BindRepeating(
+          &content::UnitTestTestSuite::CreateTestContentClients));
 
   // Some tests use mojo
   mojo::core::Init();
-  return base::LaunchUnitTests(
-      argc, argv,
-      base::BindOnce(&AshWebUITestSuite::Run, base::Unretained(&test_suite)));
+  return base::LaunchUnitTests(argc, argv,
+                               base::BindOnce(&content::UnitTestTestSuite::Run,
+                                              base::Unretained(&test_suite)));
 }
