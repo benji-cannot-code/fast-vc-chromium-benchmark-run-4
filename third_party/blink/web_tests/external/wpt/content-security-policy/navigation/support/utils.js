6d0e4b918af5c62a5e63feb8af6da194ac20880b
Chromium WPT Sync <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
2024-11-18 23:25:00 +0000

    Import wpt@bfd44a5a07a419124c9a9bbdb095510b6ecdb509
    https://github.com/web-platform-tests/wpt/compare/b15ef9181...bfd44a5a0
    
    Using wpt-import in Chromium 435f43c75e6953f46276cecb95ee2bdd6cdc9d3e.
    With Chromium commits locally applied on WPT:
      435f43c75e "Implement new behavior for popovers contained within [...]
    
    Note to gardeners: This CL imports external tests and adds expectations
    for those tests; if this CL is large and causes a few new failures,
    please fix the failures by adding new lines to TestExpectations rather
    than reverting. See:
    https://chromium.googlesource.com/chromium/src/+/main/docs/testing/web_platform_tests.md
    
    Directory owners for changes in this CL:
    antoniosartori@chromium.org, arthursonzogni@chromium.org:
      external/wpt/content-security-policy
    
    NOAUTOREVERT=true
    
    No-Export: true
    Cq-Include-Trybots: luci.chromium.try:linux-blink-rel
    Change-Id: I00c5c2f6bee2567d86a1d6cfacfb1b3a3ff9e1e1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032312
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Auto-Submit: WPT Autoroller <wpt-autoroller@chops-service-accounts.iam.gserviceaccount.com>
    Reviewed-by: Weizhong Xia <weizhong@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384639}

---
+++ third_party/blink/web_tests/external/wpt/content-security-policy/navigation/support/utils.js
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+function determineInjectionSinkDescription(testCase) {
+    const targetWindowDescription = ("targetWindow" in testCase) ?
+      testCase.targetWindow.name : "";
+
+    const element = ("elementId" in testCase) ?
+      window.document.getElementById(testCase.elementId) : null;
+
+    const elementDescription = element ? (element.localName +
+        (element.target ? ("[target=" + element.target + "]") : "")) : null;
+
+    return ((elementDescription ? (elementDescription + ".") :
+      (targetWindowDescription ? (targetWindowDescription + ".") : ""))) +
+      testCase.propertySequence.join(".");
+}
+
+function assignJavascriptURLToInjectionSink(testCase) {
+  const element = ("elementId" in testCase) ?
+    document.getElementById(testCase.elementId) : null;
+
+  let currentObject = element ? element : testCase.targetWindow;
+
+  const propertySequence = testCase.propertySequence;
+  for (let i = 0; i < propertySequence.length - 1; ++i) {
+    currentObject = currentObject[propertySequence[i]];
+  }
+
+  currentObject[propertySequence.at(-1)] =
+    "javascript:parent.postMessage('executed', '*')";
+
+  if ("navigationFunction" in testCase) {
+    element[testCase.navigationFunction]();
+  }
+}
