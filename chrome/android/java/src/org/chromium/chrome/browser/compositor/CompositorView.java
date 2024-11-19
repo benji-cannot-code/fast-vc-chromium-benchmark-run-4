a9f5b9f3d45acb9693f426eda45c182fa50be477
Bo Liu <boliu@chromium.org>
2024-11-19 17:19:36 +0000

    android: Revert invalid surface size report
    This reverts r1350166 and r1350252.
    
    Have gotten reports that this does happen so it has provided all the
    info it can. Also it's not clear if this is causing an actual crash vs a
    dump without crash, so just reverting to be safe.
    
    Bug: 346635511, 369374760
    Change-Id: I387793fed95514f9125fdcd545211275a19e9ef3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033635
    Commit-Queue: Bo Liu <boliu@chromium.org>
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385036}

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/CompositorView.java
@@ -26,7 +26,6 @@ import org.jni_zero.JniType;
 import org.jni_zero.NativeMethods;
 
 import org.chromium.base.ContextUtils;
-import org.chromium.base.JavaExceptionReporter;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.chrome.browser.compositor.layouts.Layout;
@@ -48,8 +47,6 @@ import org.chromium.ui.base.WindowAndroid;
 import org.chromium.ui.resources.AndroidResourceType;
 import org.chromium.ui.resources.ResourceManager;
 
-import java.util.Locale;
-
 /**
  * The is the {@link View} displaying the ui compositor results; including webpages and tabswitcher.
  */
@@ -57,8 +54,6 @@ import java.util.Locale;
 public class CompositorView extends FrameLayout
         implements CompositorSurfaceManager.SurfaceManagerCallbackTarget,
                 WindowAndroid.SelectionHandlesObserver {
-    private static final int EXCESSIVE_SURFACE_SIZE = 1000000;
-
     // Cache objects that should not be created every frame
     private final Rect mCacheAppRect = new Rect();
 
@@ -457,20 +452,6 @@ public class CompositorView extends FrameLayout
 
     @Override
     public void surfaceChanged(Surface surface, int format, int width, int height) {
-        if (width >= EXCESSIVE_SURFACE_SIZE
-                || height >= EXCESSIVE_SURFACE_SIZE
-                || width < 0
-                || height < 0) {
-            JavaExceptionReporter.reportException(
-                    new RuntimeException(
-                            String.format(
-                                    Locale.US,
-                                    "w:%d h:%d vw:%d vh:%d)",
-                                    width,
-                                    height,
-                                    getWidth(),
-                                    getHeight())));
-        }
         if (mNativeCompositorView == 0) return;
 
         InputTransferToken browserInputToken = null;
