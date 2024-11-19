4e0ee1110c189f59e7096b7b57ab5af92f96e4e2
Ivan Murashov <ivan.murashov@lge.com>
2024-11-19 13:15:10 +0000

    IWYU: Add missing include for std::vector usage in ax_location_and_scroll_updates.h
    In the CL https://crrev.com/c/5872830 used std::vector in the
    ui/accessibility/ax_location_and_scroll_updates.h, but required include
    was missed. Added missing include for std::vector usage.
    
    Bug: 41455655
    Change-Id: Ib237a05e81a60cc2c5ef123a7cfc5bfd862edf32
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6029049
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Ivan Murashov <ivan.murashov@lge.com>
    Cr-Commit-Position: refs/heads/main@{#1384903}

--- ui/accessibility/ax_location_and_scroll_updates.h
+++ ui/accessibility/ax_location_and_scroll_updates.h
@@ -6,8 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_ACCESSIBILITY_AX_LOCATION_AND_SCROLL_UPDATES_H_
 #define UI_ACCESSIBILITY_AX_LOCATION_AND_SCROLL_UPDATES_H_
 
+#include <vector>
+
 #include "ui/accessibility/ax_base_export.h"
 #include "ui/accessibility/ax_relative_bounds.h"
+
 namespace ui {
 struct AX_BASE_EXPORT AXLocationChange {
  public:
