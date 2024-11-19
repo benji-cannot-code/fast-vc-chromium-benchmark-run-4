02a9413b0f7fc753123ba345f42753562ffe4e6e
Manuel Rego Casasnovas <rego@igalia.com>
2024-11-19 14:46:07 +0000

    Move window.onmove event to its own feature and mark as experimental
    This used to be part of DesktopPWAsAdditionalWindowingControls
    which includes more things than window.onmove:
    https://github.com/explainers-by-googlers/additional-windowing-controls/blob/main/README.md#proposal
    
    The idea is to have it in its own runtime feature
    so it can be enabled independently of the rest.
    
    This patch moves it to a feature called WindowOnMoveEvent
    and marks it as experimental.
    
    Bug: 379542213
    Change-Id: I29728225371fc0b127aa7b030a6abab7f4cdbd73
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035050
    Commit-Queue: Manuel Rego <rego@igalia.com>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384932}

--- third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
+++ third_party/blink/renderer/core/frame/web_frame_widget_impl.cc
@@ -4815,8 +4815,7 @@ bool WebFrameWidgetImpl::UpdateScreenRects(
 }
 
 void WebFrameWidgetImpl::EnqueueMoveEvent() {
-  if (!RuntimeEnabledFeatures::
-          DesktopPWAsAdditionalWindowingControlsEnabled()) {
+  if (!RuntimeEnabledFeatures::WindowOnMoveEventEnabled()) {
     return;
   }
 
