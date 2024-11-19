47923191b072410cd42f0262665c27bc461ff3bb
Peter Kasting <pkasting@chromium.org>
2024-11-19 00:16:43 +0000

    Cleanups from stricter span usage of size_t: third_party/blink
    More use of first(), compile-time sizes, etc.
    
    Bug: none
    Change-Id: I1c228e338583acec4c9f64493a814685e049e3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026978
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Auto-Submit: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384669}

--- third_party/blink/renderer/modules/webcodecs/image_decoder_external_test.cc
+++ third_party/blink/renderer/modules/webcodecs/image_decoder_external_test.cc
@@ -548,7 +548,7 @@ TEST_F(ImageDecoderTest, DecoderReadableStream) {
 
   v8::Local<v8::Value> v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(
       v8_scope.GetScriptState(),
-      DOMUint8Array::Create(data_span.subspan(0, chunk_size)));
+      DOMUint8Array::Create(data_span.first(chunk_size)));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
 
   underlying_source->Enqueue(ScriptValue(v8_scope.GetIsolate(), v8_data_array));
@@ -648,10 +648,9 @@ TEST_F(ImageDecoderTest, DecoderReadableStreamAvif) {
   EXPECT_EQ(decoder->type(), kImageType);
 
   // Enqueue a single byte and ensure nothing breaks.
-  base::span<const uint8_t> data_span = base::as_byte_span(data);
+  const auto [first, rest] = base::as_byte_span(data).split_at<1>();
   v8::Local<v8::Value> v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(
-      v8_scope.GetScriptState(),
-      DOMUint8Array::Create(data_span.subspan(0, 1)));
+      v8_scope.GetScriptState(), DOMUint8Array::Create(first));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
 
   underlying_source->Enqueue(ScriptValue(v8_scope.GetIsolate(), v8_data_array));
@@ -672,9 +671,8 @@ TEST_F(ImageDecoderTest, DecoderReadableStreamAvif) {
   EXPECT_FALSE(decode_tester.IsRejected());
 
   // Append the rest of the data.
-  v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(
-      v8_scope.GetScriptState(),
-      DOMUint8Array::Create(data_span.subspan(1, data.size() - 1)));
+  v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(v8_scope.GetScriptState(),
+                                                  DOMUint8Array::Create(rest));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
 
   underlying_source->Enqueue(ScriptValue(v8_scope.GetIsolate(), v8_data_array));
@@ -793,7 +791,7 @@ TEST_F(ImageDecoderTest, DecodePartialImage) {
   Vector<char> data = ReadFile("images/resources/dice.png");
   auto* array_buffer = DOMArrayBuffer::Create(128, 1);
   array_buffer->ByteSpan().copy_from(
-      base::as_byte_span(data).subspan(0, array_buffer->ByteLength()));
+      base::as_byte_span(data).first(array_buffer->ByteLength()));
 
   init->setData(MakeGarbageCollected<V8ImageBufferSource>(array_buffer));
   auto* decoder = ImageDecoderExternal::Create(v8_scope.GetScriptState(), init,
@@ -852,7 +850,7 @@ TEST_F(ImageDecoderTest, DecodeClosedDuringReadableStream) {
 
   v8::Local<v8::Value> v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(
       v8_scope.GetScriptState(),
-      DOMUint8Array::Create(data_span.subspan(0, data.size() / 2)));
+      DOMUint8Array::Create(data_span.first(data.size() / 2)));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
 
   underlying_source->Enqueue(ScriptValue(v8_scope.GetIsolate(), v8_data_array));
@@ -902,7 +900,7 @@ TEST_F(ImageDecoderTest, DecodeInvalidFileViaReadableStream) {
 
   v8::Local<v8::Value> v8_data_array = ToV8Traits<DOMUint8Array>::ToV8(
       v8_scope.GetScriptState(),
-      DOMUint8Array::Create(data_span.subspan(0, data.size() / 2)));
+      DOMUint8Array::Create(data_span.first(data.size() / 2)));
   ASSERT_FALSE(v8_scope.GetExceptionState().HadException());
 
   underlying_source->Enqueue(ScriptValue(v8_scope.GetIsolate(), v8_data_array));
