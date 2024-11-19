aa8d683619813209344ca4446825760db9caf4d0
Elmehdi Rahmaoui <erahmaoui@google.com>
2024-11-19 12:02:38 +0000

    Reland "[iOS] URL Opening refactoring 2"
    This is a reland of commit dcf3ba81af8ae04c6701f1f0e33d2439cc62f653 the
    issue was that the ChromeStartUpParam updates were not reported in the
    fuzzer. The change is now made to the fuzzer test as well to avoid
    compilation failure.
    
    Original change's description:
    > [iOS] URL Opening refactoring 2
    >
    > The aim of this CL is to add a new param to the app startup params
    > class, in order to check the application mode was forced based on
    > the profile prefs.
    >
    > Bug: 374935368
    > Change-Id: I0ae7985c835bf664217bd5a0b986f4a273f23620
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6003007
    > Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    > Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1384403}
    
    Bug: 374935368
    Change-Id: Ifc6ee0afa76378e6b2e00c39c4202e6e4c39887b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032133
    Commit-Queue: Elmehdi Rahmaoui <erahmaoui@google.com>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384870}

--- ios/chrome/app/startup/chrome_app_startup_parameters.h
+++ ios/chrome/app/startup/chrome_app_startup_parameters.h
@@ -45,13 +45,14 @@ enum MobileSessionCallerApp {
 - (instancetype)initWithExternalURL:(const GURL&)externalURL
                         completeURL:(const GURL&)completeURL
                     applicationMode:(ApplicationModeForTabOpening)mode
-    NS_UNAVAILABLE;
+               forceApplicationMode:(BOOL)forceApplicationMode NS_UNAVAILABLE;
 
 - (instancetype)initWithExternalURL:(const GURL&)externalURL
                   declaredSourceApp:(NSString*)declaredSourceApp
                     secureSourceApp:(NSString*)secureSourceApp
                         completeURL:(NSURL*)completeURL
                     applicationMode:(ApplicationModeForTabOpening)mode
+               forceApplicationMode:(BOOL)forceApplicationMode
     NS_DESIGNATED_INITIALIZER;
 
 // Returns a ChromeAppStartupParameters instance containing the URL to
@@ -98,7 +99,8 @@ enum MobileSessionCallerApp {
 // If the string for the parsed URL is malformed (according to RFC 2396),
 // returns nil.
 + (instancetype)startupParametersWithURL:(NSURL*)URL
-                       sourceApplication:(NSString*)appID;
+                       sourceApplication:(NSString*)appID
+                    forceApplicationMode:(BOOL)forceApplicationMode;
 
 // Returns the MobileSessionCallerApp for the given bundle ID.
 - (MobileSessionCallerApp)callerApp;
@@ -118,7 +120,8 @@ enum MobileSessionCallerApp {
                                       index:(NSNumber*)index
                                         URL:(NSURL*)URL
                           sourceApplication:(NSString*)appID
-                    secureSourceApplication:(NSString*)secureAppID;
+                    secureSourceApplication:(NSString*)secureAppID
+                       forceApplicationMode:(BOOL)forceApplicationMode;
 
 @end
 
--- ios/chrome/browser/intents/user_activity_browser_agent_unittest.mm
+++ ios/chrome/browser/intents/user_activity_browser_agent_unittest.mm
@@ -423,9 +423,10 @@ TEST_F(UserActivityBrowserAgentTest, ContinueUserActivityForeground) {
   [user_activity setWebpageURL:net::NSURLWithGURL(gurl)];
 
   AppStartupParameters* startup_params = [[AppStartupParameters alloc]
-      initWithExternalURL:gurl
-              completeURL:gurl
-          applicationMode:ApplicationModeForTabOpening::NORMAL];
+       initWithExternalURL:gurl
+               completeURL:gurl
+           applicationMode:ApplicationModeForTabOpening::NORMAL
+      forceApplicationMode:NO];
   [connection_information_ setStartupParameters:startup_params];
 
   // Action.
@@ -627,8 +628,9 @@ TEST_F(UserActivityBrowserAgentTest, ContinueUserActivityIntentForeground) {
   }
 
   AppStartupParameters* startup_params = [[AppStartupParameters alloc]
-         initWithURLs:URLs
-      applicationMode:ApplicationModeForTabOpening::NORMAL];
+              initWithURLs:URLs
+           applicationMode:ApplicationModeForTabOpening::NORMAL
+      forceApplicationMode:NO];
   [connection_information_ setStartupParameters:startup_params];
 
   // Action.
@@ -651,9 +653,10 @@ TEST_F(UserActivityBrowserAgentTest, HandleStartupParamsWithExternalFile) {
   GURL complete_url("file://test.pdf");
 
   AppStartupParameters* startup_params = [[AppStartupParameters alloc]
-      initWithExternalURL:external_url
-              completeURL:complete_url
-          applicationMode:ApplicationModeForTabOpening::INCOGNITO];
+       initWithExternalURL:external_url
+               completeURL:complete_url
+           applicationMode:ApplicationModeForTabOpening::INCOGNITO
+      forceApplicationMode:NO];
   [connection_information_ setStartupParameters:startup_params];
 
   // Action.
