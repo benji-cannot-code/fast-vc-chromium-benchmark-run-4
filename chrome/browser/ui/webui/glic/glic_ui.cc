5e94b7392709464aed76290994dce3f5512c78ae
Nick Birnie <birnie@google.com>
2024-11-19 00:29:20 +0000

    Add CSP override finch param and cli flag
    Also added a finch param for guest url. These will allow us to relax the CSP and serve different guest urls via Finch experiment.
    
    Bug: 378750400
    Change-Id: Ib502c89d0d01cbd2c7864bad91d8a88c8812f3fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032389
    Commit-Queue: Nick Birnie <birnie@google.com>
    Reviewed-by: Dan H <harringtond@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384677}

--- chrome/browser/ui/webui/glic/glic_ui.cc
+++ chrome/browser/ui/webui/glic/glic_ui.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/glic/glic_ui.h"
 
+#include <string>
+
 #include "base/command_line.h"
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/ui/webui/glic/glic_page_handler.h"
@@ -40,20 +42,30 @@ GlicUI::GlicUI(content::WebUI* web_ui) : ui::MojoWebUIController(web_ui) {
   webui::SetupWebUIDataSource(source, base::make_span(kGlicResources),
                               IDR_GLIC_GLIC_HTML);
 
-  // TODO(crbug.com/378951332): Configure an approved CSP.
-  source->OverrideContentSecurityPolicy(
-      network::mojom::CSPDirectiveName::ChildSrc,
-      "child-src 'self'"
-      " https://*.google.com/"
-      " https://*.googleplex.com/;");
-
   auto* command_line = base::CommandLine::ForCurrentProcess();
-  source->AddString("glicGuestURL", command_line->GetSwitchValueASCII(
-                                        ::switches::kGlicGuestURL));
+
+  // Set up guest URL via cli flag or default to finch param value.
+  bool hasGlicGuestURL = command_line->HasSwitch(::switches::kGlicGuestURL);
+  source->AddString("glicGuestURL", hasGlicGuestURL
+                                        ? command_line->GetSwitchValueASCII(
+                                              ::switches::kGlicGuestURL)
+                                        : features::kGlicGuestURL.Get());
   source->AddString(
       "glicGuestAPISource",
       ui::ResourceBundle::GetSharedInstance().LoadDataResourceString(
           IDR_GENERATED_GLIC_API_IMPL_ROLLUP_JS));
+  source->AddString("glicGuestURL", command_line->GetSwitchValueASCII(
+                                        ::switches::kGlicGuestURL));
+  // TODO(crbug.com/378951332): Configure an approved CSP.
+  // Set up csp override by cli flag or default to finch param value. This will
+  // be removed when we go to canary since it will no longer be needed once
+  // crbug.com/378951332 is addressed.
+  bool hasCSPOverride = command_line->HasSwitch(::switches::kCSPOverride);
+  source->OverrideContentSecurityPolicy(
+      network::mojom::CSPDirectiveName::ChildSrc,
+      hasCSPOverride
+          ? command_line->GetSwitchValueASCII(::switches::kCSPOverride)
+          : features::kGlicWebUICSPOverride.Get());
 
   extensions::TabHelper::CreateForWebContents(web_ui->GetWebContents());
 }
