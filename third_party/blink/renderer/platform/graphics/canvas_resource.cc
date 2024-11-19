7ce797e8e4ec15fbeeb696733d11b654169aa900
Colin Blundell <blundell@chromium.org>
2024-11-19 16:09:20 +0000

    [Blink] Have CanvasResource store gfx::ColorSpace internally
    This CL changes CanvasResource to convert the SkColorSpace that it
    receives to a gfx::ColorSpace on reception and store that internally,
    converting it back only when necessary to create an SkImageInfo.
    
    The concrete change here is that when the passed-in SkColorSpace is
    null, we will now pass an SRGB colorspace when creating the SkImageInfo.
    This is a functional no-op as SRGB is the colorspace that is used by
    SkImageInfo if null is passed in [1].
    
    Followup CLs will propagate the usage of gfx::ColorSpace outward.
    
    [1] https://source.chromium.org/chromium/chromium/src/+/main:third_party/skia/include/core/SkImageInfo.h;l=227-228;drc=a2ed1bfa50bdef7869f102cb1436f0c95cca812f;bpv=1;bpt=1
    
    Bug: 371227617
    Change-Id: Ia2c54bb5ee8961b44c6113bf27ae022bc96eec12
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6033733
    Reviewed-by: Vasiliy Telezhnikov <vasilyt@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384995}

--- third_party/blink/renderer/platform/graphics/canvas_resource.cc
+++ third_party/blink/renderer/platform/graphics/canvas_resource.cc
@@ -67,7 +67,8 @@ CanvasResource::CanvasResource(base::WeakPtr<CanvasResourceProvider> provider,
       size_(size),
       sk_color_type_(sk_color_type),
       sk_alpha_type_(sk_alpha_type),
-      sk_color_space_(std::move(sk_color_space)),
+      color_space_(sk_color_space ? gfx::ColorSpace(*sk_color_space)
+                                  : gfx::ColorSpace::CreateSRGB()),
       filter_quality_(filter_quality) {}
 
 CanvasResource::~CanvasResource() {}
@@ -239,7 +240,8 @@ bool CanvasResource::PrepareAcceleratedTransferableResourceFromClientSI(
 
 SkImageInfo CanvasResource::CreateSkImageInfo() const {
   return SkImageInfo::Make(SkISize::Make(Size().width(), Size().height()),
-                           sk_color_type_, sk_alpha_type_, sk_color_space_);
+                           sk_color_type_, sk_alpha_type_,
+                           color_space_.ToSkColorSpace());
 }
 
 viz::SharedImageFormat CanvasResource::GetSharedImageFormat() const {
@@ -247,9 +249,7 @@ viz::SharedImageFormat CanvasResource::GetSharedImageFormat() const {
 }
 
 gfx::ColorSpace CanvasResource::GetColorSpace() const {
-  SkColorSpace* color_space = sk_color_space_.get();
-  return color_space ? gfx::ColorSpace(*color_space)
-                     : gfx::ColorSpace::CreateSRGB();
+  return color_space_;
 }
 
 // CanvasResourceSharedBitmap
