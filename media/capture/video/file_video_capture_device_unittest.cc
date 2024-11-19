cefef69fe4bc98fad0a8114071a500392355dac2
vikas soni <vikassoni@chromium.org>
2024-11-19 17:35:19 +0000

    [MappableSI] Update FileVideoCaptureDevice to use MappableSI.
    Update FileVideoCaptureDevice to use MappableSI instead of
    GpuMemoryBuffer.
    
    Bug: 40264379
    Change-Id: I7559b34fbfc23b040f9c9cb36d5d139719130b3e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5797865
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: vikas soni <vikassoni@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385048}

--- media/capture/video/file_video_capture_device_unittest.cc
+++ media/capture/video/file_video_capture_device_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "media/capture/video/file_video_capture_device.h"
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -13,9 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/task/bind_post_task.h"
 #include "base/test/task_environment.h"
+#include "gpu/command_buffer/client/test_shared_image_interface.h"
 #include "media/base/test_data_util.h"
-#include "media/capture/video/file_video_capture_device.h"
 #include "media/capture/video/mock_video_capture_device_client.h"
+#include "media/capture/video/video_capture_gpu_channel_host.h"
 #include "media/video/fake_gpu_memory_buffer.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -83,6 +86,10 @@ class FileVideoCaptureDeviceTest : public ::testing::Test {
         GetTestDataFilePath("bear.mjpeg"),
         std::make_unique<FakeGpuMemoryBufferSupport>());
     device_->AllocateAndStart(VideoCaptureParams(), std::move(client_));
+    test_sii_ = base::MakeRefCounted<gpu::TestSharedImageInterface>();
+    test_sii_->UseTestGMBInSharedImageCreationWithBufferUsage();
+    VideoCaptureGpuChannelHost::GetInstance().SetSharedImageInterface(
+        test_sii_);
   }
 
   void TearDown() override { device_->StopAndDeAllocate(); }
@@ -124,6 +131,7 @@ class FileVideoCaptureDeviceTest : public ::testing::Test {
   MockImageCaptureClient image_capture_client_;
   std::unique_ptr<VideoCaptureDevice> device_;
   VideoCaptureFormat last_format_;
+  scoped_refptr<gpu::TestSharedImageInterface> test_sii_;
   base::test::TaskEnvironment task_environment_;
   std::unique_ptr<base::RunLoop> run_loop_;
 };
