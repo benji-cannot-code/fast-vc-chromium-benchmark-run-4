a4360557fd769ce576b4d82ee43293a3e817c884
Antonio Gomes <tonikitoo@igalia.com>
2024-11-19 09:42:32 +0000

    Remove BrowserAppShelfItemController
    .. now that Lacros project has been terminated.
    
    BrowserAppShelfItemController used to be instantiated from a single
    place in the code:
    `ChromeShelfItemFactory::CreateShelfItemDelegateForAppId()`, and only if
    `BrowserAppShelfControllerShouldHandleApp()` returned true.
    
    However, with the Lacros project being terminated,
    BrowserAppShelfControllerShouldHandleApp() became no-op, always returning
    false. What is more, it was in fact removed altogether as part of [1].
    
    This is a follow up CL that also removes BrowserAppShelfItemController,
    which became an orphan in the tree.
    
    [1] https://crrev.com/c/6030549
    
    R=hidehiko@chromium.org
    
    Bug: 378932949, 373971535
    Change-Id: I8754fa71fd934002dd6c4ad899273c090ee5c3f8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6031789
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384837}

--- chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_controller.cc
@@ -71,7 +71,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/app_window_shelf_controller.h"
 #include "chrome/browser/ui/ash/shelf/app_window_shelf_item_controller.h"
-#include "chrome/browser/ui/ash/shelf/browser_app_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/browser_shortcut_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/browser_status_monitor.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
--- chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.cc
+++ chrome/browser/ui/ash/shelf/chrome_shelf_item_factory.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ash/app_list/arc/arc_app_utils.h"
 #include "chrome/browser/ui/ash/shelf/app_shortcut_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/arc_playstore_shortcut_shelf_item_controller.h"
-#include "chrome/browser/ui/ash/shelf/browser_app_shelf_item_controller.h"
 #include "chrome/browser/ui/ash/shelf/chrome_shelf_controller_util.h"
 #include "chrome/browser/ui/ash/shelf/shelf_controller_helper.h"
 #include "chrome/browser/ui/ash/shelf/standalone_browser_extension_app_shelf_item_controller.h"
