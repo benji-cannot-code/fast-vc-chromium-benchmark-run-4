4fb92e280b6e67cd7638b774c54219d8e9c600ce
Moteva <moteva@google.com>
2024-11-19 20:04:34 +0000

    CrOS Settings: cleanup revamp code in personalization page
    Bug: 370837128
    Change-Id: I30f37115a28e1e5be424803670dad3f161a8db76
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026055
    Reviewed-by: Connie Xu <conniekxu@chromium.org>
    Commit-Queue: Nikki Moteva <moteva@google.com>
    Reviewed-by: Jimmy Gong <jimmyxgong@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385156}

--- chrome/browser/resources/ash/settings/personalization_page/personalization_page.ts
+++ chrome/browser/resources/ash/settings/personalization_page/personalization_page.ts
@@ -20,7 +20,7 @@ import {I18nMixin} from 'chrome://resources/ash/common/cr_elements/i18n_mixin.js
 import {PolymerElement} from 'chrome://resources/polymer/v3_0/polymer/polymer_bundled.min.js';
 
 import {DeepLinkingMixin} from '../common/deep_linking_mixin.js';
-import {isRevampWayfindingEnabled, shouldShowMultitaskingInPersonalization} from '../common/load_time_booleans.js';
+import {shouldShowMultitaskingInPersonalization} from '../common/load_time_booleans.js';
 import {RouteObserverMixin} from '../common/route_observer_mixin.js';
 import {Section} from '../mojom-webui/routes.mojom-webui.js';
 import {Setting} from '../mojom-webui/setting.mojom-webui.js';
@@ -52,13 +52,6 @@ export class SettingsPersonalizationPageElement extends
         readOnly: true,
       },
 
-      isRevampWayfindingEnabled_: {
-        type: Boolean,
-        value() {
-          return isRevampWayfindingEnabled();
-        },
-      },
-
       shouldShowMultitaskingInPersonalization_: {
         type: Boolean,
         value() {
@@ -78,7 +71,6 @@ export class SettingsPersonalizationPageElement extends
     };
   }
 
-  private isRevampWayfindingEnabled_: boolean;
   private readonly shouldShowMultitaskingInPersonalization_: boolean;
   private personalizationHubBrowserProxy_: PersonalizationHubBrowserProxy;
   private section_: Section;
@@ -88,12 +80,6 @@ export class SettingsPersonalizationPageElement extends
 
     this.personalizationHubBrowserProxy_ =
         PersonalizationHubBrowserProxyImpl.getInstance();
-  }
-
-  private getPersonalizationRowIcon_(): string {
-    return this.isRevampWayfindingEnabled_ ?
-        'os-settings:personalization-revamp' :
-        '';
   }
 
   private openPersonalizationHub_(): void {
