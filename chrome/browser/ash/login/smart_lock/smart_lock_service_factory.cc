583ac722c6e62c093af8e5c2a5bb856f0cb13895
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-19 14:10:05 +0000

    [Smart lock]Migrate to BuildServiceInstanceForBrowserContext
    `BuildServiceInstanceFor` is deprecated. This CL ensures that the new
    method `BuildServiceInstanceForBrowserContext` is used instead.
    
    In particular it ensures that services are created with a unique_ptr.
    
    This CL was uploaded by git cl split.
    
    R=hansberry@chromium.org
    
    Bug: 40249337
    Change-Id: I3d06eda98ab24d2e9578008444d86a52bb9ba6ab
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6021380
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Arthur Milchior <arthurmilchior@chromium.org>
    Reviewed-by: Ryan Hansberry <hansberry@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384918}

--- chrome/browser/ash/login/smart_lock/smart_lock_service_factory.cc
+++ chrome/browser/ash/login/smart_lock/smart_lock_service_factory.cc
@@ -71,7 +71,8 @@ SmartLockServiceFactory::SmartLockServiceFactory()
 
 SmartLockServiceFactory::~SmartLockServiceFactory() = default;
 
-KeyedService* SmartLockServiceFactory::BuildServiceInstanceFor(
+std::unique_ptr<KeyedService>
+SmartLockServiceFactory::BuildServiceInstanceForBrowserContext(
     content::BrowserContext* context) const {
   if (!context) {
     return nullptr;
@@ -92,7 +93,7 @@ KeyedService* SmartLockServiceFactory::BuildServiceInstanceFor(
     return nullptr;
   }
 
-  SmartLockService* service = new SmartLockService(
+  auto service = std::make_unique<SmartLockService>(
       Profile::FromBrowserContext(context),
       secure_channel::SecureChannelClientProvider::GetInstance()->GetClient(),
       device_sync::DeviceSyncClientFactory::GetForProfile(profile),
--- chrome/browser/ash/login/smart_lock/smart_lock_service_factory.h
+++ chrome/browser/ash/login/smart_lock/smart_lock_service_factory.h
@@ -44,7 +44,7 @@ class SmartLockServiceFactory : public ProfileKeyedServiceFactory {
   ~SmartLockServiceFactory() override;
 
   // BrowserContextKeyedServiceFactory:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override;
   void RegisterProfilePrefs(
       user_prefs::PrefRegistrySyncable* registry) override;
