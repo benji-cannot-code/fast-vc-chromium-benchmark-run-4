91fe4360254bc9a9f095a9bc19a9b06151b262f9
Sammie Quon <sammiequon@chromium.org>
2024-11-19 05:09:15 +0000

    coral: Save apps from group into coral desk
    Uses the existing restore data collector to save the apps from the
    active desk. Passes a map as an allow list so we only save the apps in
    the group. The restore data collector is built to be async (although
    with lacros gone, in theory it should be sync), so we need to shuffle
    some existing logic to follow this pattern.
    
    Test: manual
    Test: manually checked disk contents
    Test: ash_unittests CoralSavedGroupTest.SaveAppsInGroup
    Bug: 365839564
    Change-Id: I8bf0aa220291c037131a8550b014a1fd7d10b449
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6032298
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384766}

--- ash/public/cpp/test/test_saved_desk_delegate.cc
+++ ash/public/cpp/test/test_saved_desk_delegate.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/public/cpp/test/test_saved_desk_delegate.h"
 
 #include "ash/public/cpp/desk_template.h"
+#include "ash/public/cpp/window_properties.h"
 #include "base/containers/contains.h"
 #include "components/app_restore/app_launch_info.h"
 #include "ui/aura/client/aura_constants.h"
@@ -22,6 +23,15 @@ TestSavedDeskDelegate::~TestSavedDeskDelegate() = default;
 void TestSavedDeskDelegate::GetAppLaunchDataForSavedDesk(
     aura::Window* window,
     GetAppLaunchDataCallback callback) const {
+  if (window) {
+    if (const std::string* app_id = window->GetProperty(kAppIDKey)) {
+      if (app_ids_with_app_launch_info_.contains(*app_id)) {
+        std::move(callback).Run(std::make_unique<app_restore::AppLaunchInfo>(
+            *app_id, /*window_id=*/0));
+        return;
+      }
+    }
+  }
   std::move(callback).Run({});
 }
 
--- ash/wm/coral/coral_controller.h
+++ ash/wm/coral/coral_controller.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
+class DeskTemplate;
 class FakeCoralService;
 
 class ASH_EXPORT CoralRequest {
@@ -137,6 +138,14 @@ class ASH_EXPORT CoralController {
   void HandleCacheEmbeddingsResult(
       base::OnceCallback<void(bool)> callback,
       coral::mojom::CacheEmbeddingsResultPtr result);
+
+  // Callback that is run when we call
+  // `DesksController::CaptureActiveDeskAsSavedDesk()`. May also be called
+  // directly if a group has no apps in it. If `desk_template` is nullptr, then
+  // we create one if `tab_urls` is not empty, otherwise this function does
+  // nothing.
+  void OnTemplateCreated(const std::vector<GURL>& tab_urls,
+                         std::unique_ptr<DeskTemplate> desk_template);
 
   mojo::Remote<CoralService> coral_service_;
 
