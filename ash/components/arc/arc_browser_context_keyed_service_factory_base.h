923e39e9c5fa85285a8c1f06bc5d2d9aa0d997d2
Arthur Milchior <arthurmilchior@chromium.org>
2024-11-19 18:22:23 +0000

    [Arc browser]Migrate to BuildServiceInstanceForBrowserContext
    `BuildServiceInstanceFor` is deprecated. This CL ensures that the new
    method `BuildServiceInstanceForBrowserContext` is used instead.
    
    In particular it ensures that services are created with a unique_ptr.
    
    This CL was uploaded by git cl split.
    
    R=khmel@chromium.org
    
    Bug: 40249337
    Change-Id: Idc99225e4a3996e5b8b22a6e75950acbf5333a5d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6020600
    Auto-Submit: Arthur Milchior <arthurmilchior@chromium.org>
    Commit-Queue: Yury Khmel <khmel@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385087}

--- ash/components/arc/arc_browser_context_keyed_service_factory_base.h
+++ ash/components/arc/arc_browser_context_keyed_service_factory_base.h
@@ -118,7 +118,7 @@ class ArcBrowserContextKeyedServiceFactoryBase
   ~ArcBrowserContextKeyedServiceFactoryBase() override = default;
 
   // BrowserContextKeyedServiceFactory override:
-  KeyedService* BuildServiceInstanceFor(
+  std::unique_ptr<KeyedService> BuildServiceInstanceForBrowserContext(
       content::BrowserContext* context) const override {
     auto* arc_service_manager = arc::ArcServiceManager::Get();
 
@@ -133,7 +133,8 @@ class ArcBrowserContextKeyedServiceFactoryBase
       return nullptr;
     }
 
-    return new Service(context, arc_service_manager->arc_bridge_service());
+    return std::make_unique<Service>(context,
+                                     arc_service_manager->arc_bridge_service());
   }
 };
 
