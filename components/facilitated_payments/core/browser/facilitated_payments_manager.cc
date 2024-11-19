8880bfcda0d3fca6752ffc1eeb0bf730c5afb4f6
Vishwas Uppoor <vishwasuppoor@google.com>
2024-11-19 00:54:20 +0000

    [PIX] Setup UI event listener on page loads
    The manager resets on page navigations invalidating weak pointers
    effectively disabling the UI event listener.
    
    Setup the UI event listener every time the payflow starts.
    
    Bug: 375089558, 375088986
    Change-Id: I083de4cca134682c399fd0b687846eb41df71629
    Fixed: 378984733
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6027214
    Commit-Queue: Vishwas Uppoor <vishwasuppoor@google.com>
    Reviewed-by: Siyu An <siyua@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384689}

--- components/facilitated_payments/core/browser/facilitated_payments_manager.cc
+++ components/facilitated_payments/core/browser/facilitated_payments_manager.cc
@@ -36,8 +36,6 @@ FacilitatedPaymentsManager::FacilitatedPaymentsManager(
               FacilitatedPaymentsInitiatePaymentRequestDetails>()) {
   DCHECK(optimization_guide_decider_);
   RegisterPixAllowlist();
-  client_->SetUiEventListener(base::BindRepeating(
-      &FacilitatedPaymentsManager::OnUiEvent, weak_ptr_factory_.GetWeakPtr()));
 }
 
 FacilitatedPaymentsManager::~FacilitatedPaymentsManager() {
@@ -62,6 +60,8 @@ void FacilitatedPaymentsManager::OnPixCodeCopiedToClipboard(
     return;
   }
   has_payflow_started_ = true;
+  client_->SetUiEventListener(base::BindRepeating(
+      &FacilitatedPaymentsManager::OnUiEvent, weak_ptr_factory_.GetWeakPtr()));
   ukm_source_id_ = ukm_source_id;
   trigger_source_ = TriggerSource::kCopyEvent;
   // Check whether the domain for the render_frame_host_url is allowlisted.
