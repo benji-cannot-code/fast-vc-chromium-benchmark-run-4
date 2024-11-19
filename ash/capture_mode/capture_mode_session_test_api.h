81e603c24cd5514229cf85ebef8d9590c4575cfc
Sophie Wen <sophiewen@google.com>
2024-11-19 02:41:21 +0000

    sunfish: Move `disclaimer_` to `CaptureModeSession`
    This CL moves `disclaimer_` from `CaptureModeController` to
    `CaptureModeSession`. The only difference in behavior is when the
    session is ended, e.g. via the Escape key, the dialog will also be
    closed. Re-entering the session will re-show the disclaimer.
    
    Fixed: b:378781691
    Test: updated
    Change-Id: I34dea9e10748ca280c6b3fa8537b5366a09b22d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6015931
    Reviewed-by: Ahmed Fakhry <afakhry@chromium.org>
    Commit-Queue: Sophie Wen <sophiewen@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384733}

--- ash/capture_mode/capture_mode_session_test_api.h
+++ ash/capture_mode/capture_mode_session_test_api.h
@@ -52,6 +52,8 @@ class CaptureModeSessionTestApi {
 
   views::Widget* GetActionContainerWidget();
 
+  views::Widget* GetDisclaimerWidget();
+
   views::Widget* GetRecordingTypeMenuWidget();
 
   views::Widget* GetDimensionsLabelWidget();
