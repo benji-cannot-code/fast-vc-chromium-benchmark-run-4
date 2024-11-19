6f561eda01560316c0bc563c934d1ad45b3383b7
Mickey Burks <mickeyburks@chromium.org>
2024-11-19 19:56:16 +0000

    [SavedTabGroups] Add attention indicator to TabGroupHeader
    Render the attention indicator in TabGroupHeader and provide the
    functionality for setting attention on a tab group. The actual
    API will likely reside on a browser interface somewhere, similar to
    `BrowserTabStripController::SetTabNeedsAttentionAt`. How attention is
    set/removed from the message service is left to be determined.
    
    Bug: 370924179
    Change-Id: If987d89ea7b38b2f6dc2b1236a33d6766729d8a7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6026954
    Reviewed-by: Darryl James <dljames@chromium.org>
    Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    Commit-Queue: Mickey Burks <mickeyburks@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385151}

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1394,6 +1394,11 @@ void TabStrip::SetTabNeedsAttention(int model_index, bool attention) {
   tab_at(model_index)->SetTabNeedsAttention(attention);
 }
 
+void TabStrip::SetTabGroupNeedsAttention(const tab_groups::TabGroupId& id,
+                                         bool attention) {
+  group_header(id)->SetTabGroupNeedsAttention(attention);
+}
+
 std::optional<int> TabStrip::GetModelIndexOf(const TabSlotView* view) const {
   const std::optional<int> viewmodel_index =
       tab_container_->GetModelIndexOf(view);
