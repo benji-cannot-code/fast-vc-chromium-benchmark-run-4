a224c4a9f0dfb36778b913c9b3378a39a8298071
Weizhong Xia <weizhong@google.com>
2024-11-19 01:41:08 +0000

    Revert "Enable swiftshade for chrome new headless"
    This reverts commit a1a366d142241da4911bd8abd13be4f76d4db421.
    
    Reason for revert: Likely caused run time hike in chrome_wpt_tests. Run time on chrome_wpt_tests on win-rel here is 17min.
    
    Original change's description:
    > Enable swiftshade for chrome new headless
    >
    > crrev.com/c/5955968 enabled swiftshader for headless shell. This
    > change enable that for chrome new headless to make the configuration
    > consistent.
    >
    > Bug: 339041663
    > Change-Id: I19e542d954d66961bca06803a79f629a0e02faf6
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6016209
    > Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    > Commit-Queue: Weizhong Xia <weizhong@google.com>
    > Cr-Commit-Position: refs/heads/main@{#1382416}
    
    Bug: 339041663
    Change-Id: Ieb9bd8f720d2be20321b74d7a6266437b28eef81
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6034309
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Weizhong Xia <weizhong@google.com>
    Reviewed-by: Jonathan Lee <jonathanjlee@google.com>
    Cr-Commit-Position: refs/heads/main@{#1384712}

--- third_party/blink/tools/blinkpy/wpt_tests/product.py
+++ third_party/blink/tools/blinkpy/wpt_tests/product.py
@@ -118,7 +118,6 @@ class DesktopProduct(Product):
         super().update_runner_options(options)
         options.binary = self._port.path_to_driver()
         options.binary_args.extend(self.additional_binary_args())
-        options.enable_swiftshader = True
 
     def additional_binary_args(self) -> List[str]:
         # Base args applicable to all embedders.
@@ -135,6 +134,10 @@ class Chrome(DesktopProduct):
 
 class HeadlessShell(DesktopProduct):
     name = 'headless_shell'
+
+    def update_runner_options(self, options: argparse.Namespace):
+        super().update_runner_options(options)
+        options.enable_swiftshader = True
 
     def additional_binary_args(self):
         # TODO(crbug.com/40887057): Support `--enable-leak-detection` and plumb
