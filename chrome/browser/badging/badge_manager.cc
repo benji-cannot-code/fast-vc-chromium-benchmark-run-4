fea3c91957bd459cee097730b10ae0ade99217e9
Finnur Thorarinsson <finnur@chromium.org>
2024-11-19 17:43:52 +0000

    [NavCapture]: Migrate away from Find[Installed]AppWithUrlInScopes.
    This is an intermediary no-op step where we switch calls to
    Find[Installed]AppWithUrlInScopes over to calls to
    FindBestAppWithUrlInScopes (which is what these functions do
    internally).
    
    This change is for directory:
    ['chrome/browser/badging', 'chrome/browser/ui/web_applications']
    
    Planned (but beyond the scope of this CL) is an effort to
    evaluate all call sites of FindBestAppWithUrlInScopes for
    correctness and introduce new helpers, that hopefully will
    be more resistant to incorrect use.
    
    Design doc: https://docs.google.com/document/d/14JPgYUVX8ECAMdXlp6GwXP-ucXnIIPXjTfxWt-WINLo/edit?usp=sharing
    
    This CL was uploaded by git cl split.
    
    R=dibyapal@chromium.org
    
    Bug: 340952100
    Change-Id: Id93506f42f3b23d94550e8be547e63ed52077aa2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035905
    Commit-Queue: Dibyajyoti Pal <dibyapal@chromium.org>
    Auto-Submit: Finnur Thorarinsson <finnur@chromium.org>
    Reviewed-by: Dibyajyoti Pal <dibyapal@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385059}

--- chrome/browser/badging/badge_manager.cc
+++ chrome/browser/badging/badge_manager.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/web_applications/app_browser_controller.h"
+#include "chrome/browser/web_applications/proto/web_app_install_state.pb.h"
 #include "chrome/browser/web_applications/web_app_provider.h"
 #include "chrome/browser/web_applications/web_app_registrar.h"
 #include "chrome/browser/web_applications/web_app_sync_bridge.h"
@@ -267,8 +268,15 @@ BadgeManager::FrameBindingContext::GetAppIdsAndUrlsForBadging() const {
     return std::vector<std::tuple<webapps::AppId, GURL>>{};
 
   const web_app::WebAppRegistrar& registrar = provider->registrar_unsafe();
+  // TODO(crbug.com/340952100): Evaluate call sites of FindBestAppWithUrlInScope
+  // for correctness.
   const std::optional<webapps::AppId> app_id =
-      registrar.FindAppWithUrlInScope(frame->GetLastCommittedURL());
+      registrar.FindBestAppWithUrlInScope(
+          frame->GetLastCommittedURL(),
+          {
+              web_app::proto::InstallState::INSTALLED_WITH_OS_INTEGRATION,
+              web_app::proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
+          });
   if (!app_id)
     return std::vector<std::tuple<webapps::AppId, GURL>>{};
   return std::vector<std::tuple<webapps::AppId, GURL>>{std::make_tuple(
--- chrome/browser/ui/web_applications/web_app_browsertest_base.cc
+++ chrome/browser/ui/web_applications/web_app_browsertest_base.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/web_applications/mojom/user_display_mode.mojom.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_manager.h"
 #include "chrome/browser/web_applications/os_integration/os_integration_test_override.h"
+#include "chrome/browser/web_applications/proto/web_app_install_state.pb.h"
 #include "chrome/browser/web_applications/test/debug_info_printer.h"
 #include "chrome/browser/web_applications/test/os_integration_test_override_impl.h"
 #include "chrome/browser/web_applications/test/web_app_install_test_utils.h"
@@ -152,7 +153,13 @@ WebAppBrowserTestBase::NavigateInNewWindowAndAwaitInstallabilityCheck(
 
 std::optional<webapps::AppId>
 WebAppBrowserTestBase::FindAppWithUrlInScope(const GURL& url) {
-  return provider().registrar_unsafe().FindAppWithUrlInScope(url);
+  // TODO(crbug.com/340952100): Evaluate call sites of FindBestAppWithUrlInScope
+  // for correctness.
+  return provider().registrar_unsafe().FindBestAppWithUrlInScope(
+      url, {
+               proto::InstallState::INSTALLED_WITH_OS_INTEGRATION,
+               proto::InstallState::INSTALLED_WITHOUT_OS_INTEGRATION,
+           });
 }
 
 Browser* WebAppBrowserTestBase::OpenPopupAndWait(
