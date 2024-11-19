55c8bf71ec5631213e6cdaf3b27bb096ef358870
Michael Wilson <mjwilson@chromium.org>
2024-11-19 18:24:59 +0000

    Revert "Handle extension installations for SU without custodian info"
    This reverts commit 13fe1cfb53a7ec3c393a813a4d968034a9defab5.
    
    Reason for revert: Failing on UBSan builder
    
    Original change's description:
    > Handle extension installations for SU without custodian info
    >
    > Today when the custodian info is missing (due to failure to fetch
    > the family info or due to a household missing the custodian), the
    > deprecated "Blocked extensions" screens is shown to supervised user.
    >
    > This CL fixes the behavior in this case: we well always invoke the
    > Parent Approval dialog and exit early with a failure (transmitted to
    > the webstore) if the custodian info is missing.
    >
    > Bug: 350716375
    > Change-Id: I5ab523879e8405bbd6e152af6cf27254394b4c06
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5961603
    > Reviewed-by: James Lee <ljjlee@google.com>
    > Commit-Queue: James Lee <ljjlee@google.com>
    > Commit-Queue: Anthi Orfanou <anthie@google.com>
    > Code-Coverage: findit-for-me@appspot.gserviceaccount.com <findit-for-me@appspot.gserviceaccount.com>
    > Cr-Commit-Position: refs/heads/main@{#1384975}
    
    Bug: 350716375
    Change-Id: Ia99a3083e4df3a32f6126d5743e742f4e37fa925
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6035903
    Auto-Submit: Michael Wilson <mjwilson@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Michael Wilson <mjwilson@chromium.org>
    Owners-Override: Michael Wilson <mjwilson@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1385091}

--- chrome/browser/ui/views/supervised_user/parent_permission_dialog_view.cc
+++ chrome/browser/ui/views/supervised_user/parent_permission_dialog_view.cc
@@ -604,22 +604,11 @@ void ParentPermissionDialogView::CreateContents() {
 }
 
 void ParentPermissionDialogView::ShowDialog() {
-  if (is_showing_) {
+  if (is_showing_)
     return;
-  }
 
   is_showing_ = true;
   LoadParentEmailAddresses();
-
-  if (parent_permission_email_addresses_.empty()) {
-    SendResultOnce(ParentPermissionDialog::Result::kParentPermissionFailed);
-    CloseDialog();
-    // Record metrics.
-    supervised_user_metrics_recorder_.RecordParentPermissionDialogUmaMetrics(
-        SupervisedUserExtensionsMetricsRecorder::ParentPermissionDialogState::
-            kNoParentError);
-    return;
-  }
 
   supervised_user_metrics_recorder_.RecordParentPermissionDialogUmaMetrics(
       SupervisedUserExtensionsMetricsRecorder::ParentPermissionDialogState::
@@ -692,6 +681,8 @@ void ParentPermissionDialogView::ShowDialogInternal() {
 }
 
 void ParentPermissionDialogView::LoadParentEmailAddresses() {
+  // Get the parents' email addresses.  There can be a max of 2 parent email
+  // addresses, the primary and the secondary.
   supervised_user::SupervisedUserService* service =
       SupervisedUserServiceFactory::GetForProfile(params_->profile);
 
@@ -704,6 +695,13 @@ void ParentPermissionDialogView::LoadParentEmailAddresses() {
       base::UTF8ToUTF16(service->GetSecondCustodianEmailAddress());
   if (!secondary_parent_email.empty())
     parent_permission_email_addresses_.push_back(secondary_parent_email);
+
+  if (parent_permission_email_addresses_.empty()) {
+    supervised_user_metrics_recorder_.RecordParentPermissionDialogUmaMetrics(
+        SupervisedUserExtensionsMetricsRecorder::ParentPermissionDialogState::
+            kNoParentError);
+    SendResultOnce(ParentPermissionDialog::Result::kParentPermissionFailed);
+  }
 }
 
 void ParentPermissionDialogView::CloseWithReason(
