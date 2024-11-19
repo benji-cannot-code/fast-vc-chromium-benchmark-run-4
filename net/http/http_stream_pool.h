7966da9fdbf857f0120a243a9a822c77b655a21d
Kenichi Ishibashi <bashi@chromium.org>
2024-11-19 09:01:54 +0000

    Refactor HttpStreamPoolSwitchingInfo and other classes
    This CL includes following changes as preparations for supporting QUIC
    alt-svc endpoints:
    * Rename HttpStreamPoolSwitchingInfo to HttpStreamPoolRequestInfo.
    * Don't use HttpStreamKey in HttpStreamPoolRequestInfo because
      we need to calculate another HttpStreamKey when an alternative
      service exists for the request.
    * Move some common parameters from RequestStream() and Preconnect()
      to the constructor of HttpStreamPool::JobController to avoid
      multiple calculations of HttpStreamKey and QuicSessionAliasKey.
    
    This CL has no behavior changes.
    
    Bug: 346835898
    Change-Id: Iddff226a982072fd470fffc0a32aad7985acc3fe
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5999135
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1384811}

--- net/http/http_stream_pool.h
+++ net/http/http_stream_pool.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/network_change_notifier.h"
 #include "net/base/request_priority.h"
 #include "net/http/alternative_service.h"
-#include "net/http/http_stream_pool_switching_info.h"
+#include "net/http/http_stream_pool_request_info.h"
 #include "net/http/http_stream_request.h"
 #include "net/socket/next_proto.h"
 #include "net/socket/ssl_client_socket.h"
@@ -126,7 +126,7 @@ class NET_EXPORT_PRIVATE HttpStreamPool
   // Requests an HttpStream.
   std::unique_ptr<HttpStreamRequest> RequestStream(
       HttpStreamRequest::Delegate* delegate,
-      HttpStreamPoolSwitchingInfo switching_info,
+      HttpStreamPoolRequestInfo request_info,
       RequestPriority priority,
       const std::vector<SSLConfig::CertAndStatus>& allowed_bad_certs,
       bool enable_ip_based_pooling,
@@ -135,7 +135,7 @@ class NET_EXPORT_PRIVATE HttpStreamPool
 
   // Requests that enough connections/sessions for `num_streams` be opened.
   // `callback` is only invoked when the return value is `ERR_IO_PENDING`.
-  int Preconnect(HttpStreamPoolSwitchingInfo switching_info,
+  int Preconnect(HttpStreamPoolRequestInfo request_info,
                  size_t num_streams,
                  CompletionOnceCallback callback);
 
